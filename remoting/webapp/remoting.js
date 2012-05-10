221695fbe2484750dac77bd9a22218e22878d2c1
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 18:40:48 +0000

    Constrain keyboard navigation while a modal dialog is active.
    BUG=126200
    TEST=Open and close various modal dialogs (IT2Me client and host, Me2Me host config) and make sure that the control tab ordering is what you would expect.
    
    Review URL: https://chromiumcodereview.appspot.com/10384097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136351 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/remoting.js
+++ remoting/webapp/remoting.js
@@ -60,6 +60,8 @@ remoting.init = function() {
   window.addEventListener('paste', pluginGotPaste_, false);
   window.addEventListener('copy', pluginGotCopy_, false);
 
+  remoting.initModalDialogs();
+
   if (isHostModeSupported_()) {
     var noShare = document.getElementById('chrome-os-no-share');
     noShare.parentNode.removeChild(noShare);
