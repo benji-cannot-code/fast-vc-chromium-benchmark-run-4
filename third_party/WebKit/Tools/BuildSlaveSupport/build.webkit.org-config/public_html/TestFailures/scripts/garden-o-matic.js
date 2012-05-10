a1cc62fc2c8d02f4336dc689006219cd0cad3e07
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-10 04:34:08 +0000

    Don't show partytime if there are non-layout test failures. https://bugs.webkit.org/show_bug.cgi?id=86056
    Reviewed by Dimitri Glazkov.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/controllers.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/controllers_unittests.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/garden-o-matic.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116606 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/garden-o-matic.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/garden-o-matic.js
@@ -37,6 +37,14 @@ var g_failuresController = null;
 
 var g_nonLayoutTestFailureBuilders = null;
 
+function updatePartyTime()
+{
+    if (!g_unexpectedFailuresController.length() && !g_nonLayoutTestFailureBuilders.hasFailures())
+        $('#onebar').addClass('partytime');
+    else
+        $('#onebar').removeClass('partytime');
+}
+
 function update()
 {
     if (g_revisionHint)
@@ -52,7 +60,10 @@ function update()
 
     g_info.add(updating);
 
-    builders.buildersFailingNonLayoutTests(g_nonLayoutTestFailureBuilders.update.bind(g_nonLayoutTestFailureBuilders));
+    builders.buildersFailingNonLayoutTests(function(failuresList) {
+        g_nonLayoutTestFailureBuilders.update(failuresList);
+        updatePartyTime();
+    });
 
     base.callInParallel([model.updateRecentCommits, model.updateResultsByBuilder], function() {
         if (g_failuresController)
@@ -64,10 +75,7 @@ function update()
             updating.update('Analyzing test failures ... ' + ++numberOfTestsAnalyzed + ' tests analyzed.');
             g_unexpectedFailuresController.update(failureAnalysis);
         }, function() {
-            if (!g_unexpectedFailuresController.length())
-                $('#onebar').addClass('partytime');
-            else
-                $('#onebar').removeClass('partytime');
+            updatePartyTime();
             g_unexpectedFailuresController.purge();
 
             updating.dismiss();
