64941825fd1347c4f69b8949414c42654c6126e2
rakuco@webkit.org <rakuco@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-10 14:04:55 +0000

    [EFL] Add OwnPtr specialization for Eina_Hash. https://bugs.webkit.org/show_bug.cgi?id=85046
    Patch by Michał Pakuła vel Rutka <m.pakula@samsung.com> on 2012-05-10
    Reviewed by Andreas Kling.
    
    Add an override for Eina_Hash for EFL port.
    
    * wtf/OwnPtrCommon.h:
    (WTF):
    * wtf/efl/OwnPtrEfl.cpp:
    (WTF::deleteOwnedPtr):
    (WTF):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/OwnPtrCommon.h
+++ third_party/WebKit/Source/WTF/wtf/OwnPtrCommon.h
@@ -43,6 +43,7 @@ typedef struct HRGN__* HRGN;
 typedef struct _Ecore_Evas Ecore_Evas;
 typedef struct _Ecore_Pipe Ecore_Pipe;
 typedef struct _Ecore_Timer Ecore_Timer;
+typedef struct _Eina_Hash Eina_Hash;
 typedef struct _Eina_Module Eina_Module;
 typedef struct _Evas_Object Evas_Object;
 #endif
@@ -70,6 +71,7 @@ namespace WTF {
     void deleteOwnedPtr(Ecore_Evas*);
     void deleteOwnedPtr(Ecore_Pipe*);
     void deleteOwnedPtr(Ecore_Timer*);
+    void deleteOwnedPtr(Eina_Hash*);
     void deleteOwnedPtr(Eina_Module*);
     void deleteOwnedPtr(Evas_Object*);
 #endif
