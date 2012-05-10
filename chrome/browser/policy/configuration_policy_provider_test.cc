10d43e8cb2608a4d6eee53fe63443cd2326ef6a3
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 13:19:17 +0000

    Load dictionary policies on Windows.
    BUG=108994
    TEST=unit_tests are green; ProxySettings can be directly configured as a policy on Windows.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10384054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136287 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_provider_test.cc
+++ chrome/browser/policy/configuration_policy_provider_test.cc
@@ -140,8 +140,6 @@ TEST_P(ConfigurationPolicyProviderTest, StringListValue) {
                         &expected_value));
 }
 
-// TODO(joaodasilva): implement this for windows. http://crbug.com/108994
-#if defined(OS_POSIX)
 TEST_P(ConfigurationPolicyProviderTest, DictionaryValue) {
   base::DictionaryValue expected_value;
   expected_value.SetBoolean("bool", true);
@@ -174,7 +172,6 @@ TEST_P(ConfigurationPolicyProviderTest, DictionaryValue) {
                         test_policy_definitions::kKeyDictionary,
                         &expected_value));
 }
-#endif
 
 TEST_P(ConfigurationPolicyProviderTest, RefreshPolicies) {
   PolicyMap policy_map;
