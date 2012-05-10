7f80cea23316e9a0a09bcde23f822f301386c1f6
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 05:11:33 +0000

    Add a WebDriver API function HeapProrilerDump.
    BUG=123751
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10161020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/commands/chrome_commands.h
+++ chrome/test/webdriver/commands/chrome_commands.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -83,6 +83,21 @@ class ViewsCommand : public WebDriverCommand {
  private:
   DISALLOW_COPY_AND_ASSIGN(ViewsCommand);
 };
+
+#if !defined(NO_TCMALLOC) && (defined(OS_LINUX) || defined(OS_CHROMEOS))
+class HeapProfilerDumpCommand : public WebDriverCommand {
+ public:
+  HeapProfilerDumpCommand(const std::vector<std::string>& path_segments,
+                          const base::DictionaryValue* const parameters);
+  virtual ~HeapProfilerDumpCommand();
+
+  virtual bool DoesPost() OVERRIDE;
+  virtual void ExecutePost(Response* const response) OVERRIDE;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(HeapProfilerDumpCommand);
+};
+#endif  // !defined(NO_TCMALLOC) && (defined(OS_LINUX) || defined(OS_CHROMEOS))
 
 }  // namespace webdriver
 
