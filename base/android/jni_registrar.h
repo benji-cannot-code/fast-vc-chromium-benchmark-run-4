1e47a68972d0d5dd9977cc5bf6efe1bf958f9bc2
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 15:16:35 +0000

    Fix forward-declaration of RegistrationMethod in jni_registrar.h
    See http://codereview.chromium.org/10035034
    
    Review URL: https://chromiumcodereview.appspot.com/10392025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136306 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_registrar.h
+++ base/android/jni_registrar.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace android {
 
-class RegistrationMethod;
+struct RegistrationMethod;
 
 // Registers the JNI bindings for the specified |method| definition containing
 // |count| elements.  Returns whether the registration of the given methods
