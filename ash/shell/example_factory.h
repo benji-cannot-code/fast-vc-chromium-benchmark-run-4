0aa98281a4e3444725abd636d21c81e518e37700
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 23:17:44 +0000

    ash: Remove unused declaration of BuildAppListModel() function.
    R=xiyuan@chromium.org
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10377097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136437 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/example_factory.h
+++ ash/shell/example_factory.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 namespace app_list {
-class AppListModel;
 class AppListViewDelegate;
 }
 
@@ -25,8 +24,6 @@ void CreateLockScreen();
 
 // Creates a window showing samples of commonly used widgets.
 void CreateWidgetsWindow();
-
-void BuildAppListModel(app_list::AppListModel* model);
 
 app_list::AppListViewDelegate* CreateAppListViewDelegate();
 
