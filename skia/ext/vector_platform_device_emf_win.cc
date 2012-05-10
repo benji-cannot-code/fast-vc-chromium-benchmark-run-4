455c068e5b66481cdb13cdccdd610ae8f59a242a
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 15:30:20 +0000

    roll skia to 3891 Review URL: https://chromiumcodereview.appspot.com/10377084
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136308 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/vector_platform_device_emf_win.cc
+++ skia/ext/vector_platform_device_emf_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "skia/ext/bitmap_platform_device.h"
 #include "skia/ext/skia_utils_win.h"
+#include "third_party/skia/include/core/SkPathEffect.h"
 #include "third_party/skia/include/core/SkTemplates.h"
 #include "third_party/skia/include/core/SkUtils.h"
 #include "third_party/skia/include/ports/SkTypeface_win.h"
