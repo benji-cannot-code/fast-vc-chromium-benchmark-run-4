273e5977c158b60ae1aeb843ea04d16bf07f0b8d
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 01:55:59 +0000

    [Mac] Get various fullscreen tests passing/not crashing on Lion.
    This required changing when the NOTIFICATION_FULLSCREEN_CHANGED gets sent,
    which appears to fix a TODO.
    
    BUG=93925,111097,113521
    TEST=unit_tests:BrowserWindowFullScreenControllerTest, browser_tests:BrowserTest.TestFullscreenBubbleMouseLockState
    TEST=Fullscreen continues to work in the UI on 10.5-10.8.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10383076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller_private.mm
+++ chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -757,6 +757,8 @@ willPositionSheet:(NSWindow*)sheet
   fullscreenWindow_.reset();
   [self layoutSubviews];
 
+  [self windowDidExitFullScreen:nil];
+
   // Fade back in.
   if (didFadeOut) {
     CGDisplayFade(token, kFadeDurationSeconds / 2, kCGDisplayBlendSolidColor,
@@ -862,6 +864,7 @@ willPositionSheet:(NSWindow*)sheet
     [self deregisterForContentViewResizeNotifications];
   enteringFullscreen_ = NO;
   [self showFullscreenExitBubbleIfNecessary];
+  browser_->WindowFullscreenStateChanged();
 }
 
 - (void)windowWillExitFullScreen:(NSNotification*)notification {
@@ -872,7 +875,9 @@ willPositionSheet:(NSWindow*)sheet
 }
 
 - (void)windowDidExitFullScreen:(NSNotification*)notification {
-  [self deregisterForContentViewResizeNotifications];
+  if (base::mac::IsOSLionOrLater())
+    [self deregisterForContentViewResizeNotifications];
+  browser_->WindowFullscreenStateChanged();
 }
 
 - (void)windowDidFailToEnterFullScreen:(NSWindow*)window {
