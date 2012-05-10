f4d5577f9ba9358287a8a9af07577050b8bf5fda
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-10 22:49:48 +0000

    Revert 136117 - Disable the hang dialog detector when showModalDialog is running.
    Temporarily reverting due to dependence on 132407.  Will fix and reland.
    
    BUG=122153
    
    Review URL: https://chromiumcodereview.appspot.com/10377017
    
    TBR=jam@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10378097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136417 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2088,8 +2088,7 @@ void RenderViewImpl::runModal() {
   if (RenderThreadImpl::current())  // Will be NULL during unit tests.
     RenderThreadImpl::current()->DoNotSuspendWebKitSharedTimer();
 
-  SendAndRunNestedMessageLoop(new ViewHostMsg_RunModal(
-      routing_id_, opener_id_));
+  SendAndRunNestedMessageLoop(new ViewHostMsg_RunModal(routing_id_));
 }
 
 bool RenderViewImpl::enterFullScreen() {
