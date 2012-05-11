1651f468573002943cb20159fc625bca58f30f73
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-11 23:24:37 +0000

    Log message class/line in IPC::ChannelProxy::Context::OnDispatchMessage
    BUG=none
    TEST=about:tracing
    
    
    Review URL: https://chromiumcodereview.appspot.com/10380046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136703 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel_proxy.cc
+++ ipc/ipc_channel_proxy.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_logging.h"
+#include "ipc/ipc_message_macros.h"
 #include "ipc/ipc_message_utils.h"
 
 namespace IPC {
@@ -226,8 +227,9 @@ void ChannelProxy::Context::OnDispatchMessage(const Message& message) {
   TRACE_EVENT1("task", "ChannelProxy::Context::OnDispatchMessage",
                "name", name);
 #else
-  TRACE_EVENT1("task", "ChannelProxy::Context::OnDispatchMessage",
-               "type", message.type());
+  TRACE_EVENT2("task", "ChannelProxy::Context::OnDispatchMessage",
+               "class", IPC_MESSAGE_ID_CLASS(message.type()),
+               "line", IPC_MESSAGE_ID_LINE(message.type()));
 #endif
 
   if (!listener_)
