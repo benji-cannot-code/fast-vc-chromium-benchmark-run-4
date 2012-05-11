d09f13697ccb6037b73efe4983deff740aefdf25
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-11 09:35:00 +0000

    Web Inspector: use div, not span as a parent element for ElementsTreeOutline in Audits panel https://bugs.webkit.org/show_bug.cgi?id=86188
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    We need to use <div>, not <span> as a container for ElementsTreeOutline, as latter accesses its parent offsetWidth
    within _treeElementFromEvent(), which returns 0 for inline elements.
    
    * inspector/front-end/AuditFormatters.js:
    (WebInspector.AuditFormatters.node.onNodeAvailable):
    (WebInspector.AuditFormatters.node):
    
    LayoutTests:
    
    * inspector/extensions/extensions-audits-expected.txt: rebaseline to account for extra <div>.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/AuditFormatters.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/AuditFormatters.js
@@ -122,7 +122,7 @@ WebInspector.AuditFormatters = {
      */
     node: function(expression)
     {
-        var treeOutline = new WebInspector.ElementsTreeOutline(false, false, true);
+        var parentElement = document.createElement("div");
         /**
          * @param {?number} nodeId
          */
@@ -130,9 +130,11 @@ WebInspector.AuditFormatters = {
         {
             if (!nodeId)
                 return;
+            var treeOutline = new WebInspector.ElementsTreeOutline(false, false, true);
             treeOutline.rootDOMNode = WebInspector.domAgent.nodeForId(nodeId);
             treeOutline.element.addStyleClass("outline-disclosure");
             treeOutline.setVisible(true);
+            parentElement.appendChild(treeOutline.element);
         }
         /**
          * @param {WebInspector.RemoteObject} remoteObject
@@ -142,7 +144,7 @@ WebInspector.AuditFormatters = {
             remoteObject.pushNodeToFrontend(onNodeAvailable);
         }
         WebInspector.AuditFormatters.Utilities.evaluate(expression, onEvaluate);
-        return treeOutline.element;
+        return parentElement;
     }
 };
 
