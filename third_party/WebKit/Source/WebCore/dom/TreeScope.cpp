58ba9d21283e8ac7a0adc069c7475570d4e8d365
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-11 01:45:47 +0000

    [Refactoring] Move Selection from DOMWindow to TreeScope. https://bugs.webkit.org/show_bug.cgi?id=82699
    Reviewed by Ryosuke Niwa.
    
    Since ShadowRoot will also manage its own version of DOMSelection, we would like to
    share the code among Document and DOMSelection. This patch moves DOMSelection from DOMWindow to TreeScope
    so that ShadowRoot can also use it.
    
    No new tests, should covered by existing tests.
    
    * dom/Document.cpp:
    (WebCore::Document::updateFocusAppearanceTimerFired):
    * dom/Document.h:
    (Document):
    * dom/ShadowRoot.cpp:
    (WebCore::ShadowRoot::selection):
    * do/mTreeScope.cpp:
    (WebCore::TreeScope::~TreeScope):
    (WebCore::TreeScope::getSelection):
    (WebCore):
    * dom/TreeScope.h:
    (WebCore):
    (TreeScope):
    * page/DOMSelection.cpp:
    (WebCore::DOMSelection::DOMSelection):
    (WebCore::DOMSelection::clearTreeScope):
    (WebCore):
    * page/DOMSelection.h:
    (WebCore):
    (WebCore::DOMSelection::create):
    (DOMSelection):
    (WebCore::DOMSelection::frame):
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::~DOMWindow):
    (WebCore::DOMWindow::clearDOMWindowProperties):
    (WebCore::DOMWindow::getSelection):
    * page/DOMWindow.h:
    (DOMWindow):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116715 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/TreeScope.cpp
+++ third_party/WebKit/Source/WebCore/dom/TreeScope.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TreeScope.h"
 
 #include "ContainerNode.h"
+#include "DOMSelection.h"
 #include "Document.h"
 #include "Element.h"
 #include "FocusController.h"
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLMapElement.h"
 #include "HTMLNames.h"
 #include "Page.h"
+#include "RuntimeEnabledFeatures.h"
 #include "TreeScopeAdopter.h"
 #include <wtf/text/AtomicString.h>
 #include <wtf/text/CString.h>
@@ -55,6 +57,10 @@ TreeScope::TreeScope(ContainerNode* rootNode)
 
 TreeScope::~TreeScope()
 {
+    if (m_selection) {
+        m_selection->clearTreeScope();
+        m_selection = 0;
+    }
 }
 
 void TreeScope::destroyTreeScopeData()
@@ -115,6 +121,18 @@ HTMLMapElement* TreeScope::getImageMap(const String& url) const
     if (rootNode()->document()->isHTMLDocument())
         return static_cast<HTMLMapElement*>(m_imageMapsByName.getElementByLowercasedMapName(AtomicString(name.lower()).impl(), this));
     return static_cast<HTMLMapElement*>(m_imageMapsByName.getElementByMapName(AtomicString(name).impl(), this));
+}
+
+DOMSelection* TreeScope::getSelection() const
+{
+    if (!rootNode()->document()->frame())
+        return 0;
+
+    if (m_selection)
+        return m_selection.get();
+
+    m_selection = DOMSelection::create(rootNode()->document());
+    return m_selection.get();
 }
 
 Element* TreeScope::findAnchor(const String& name)
--- third_party/WebKit/Source/WebCore/page/DOMSelection.h
+++ third_party/WebKit/Source/WebCore/page/DOMSelection.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2007 Apple Inc.  All rights reserved.
+ * Copyright (C) 2012 Google Inc.  All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -39,15 +40,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
     class Frame;
-    class Range;
     class Node;
+    class Range;
+    class TreeScope;
     class VisibleSelection;
 
     typedef int ExceptionCode;
 
-    class DOMSelection : public RefCounted<DOMSelection>, public DOMWindowProperty {
+    class DOMSelection : public RefCounted<DOMSelection> {
     public:
-        static PassRefPtr<DOMSelection> create(Frame* frame) { return adoptRef(new DOMSelection(frame)); }
+        static PassRefPtr<DOMSelection> create(const TreeScope* treeScope) { return adoptRef(new DOMSelection(treeScope)); }
+
+        void clearTreeScope();
 
         // Safari Selection Object API
         // These methods return the valid equivalents of internal editing positions.
@@ -85,11 +89,16 @@ namespace WebCore {
 
         String toString();
 
+        Frame* frame() const { return m_frame; }
+
         // Microsoft Selection Object API
         void empty();
 
     private:
-        explicit DOMSelection(Frame*);
+        const TreeScope* m_treeScope;
+        Frame* m_frame;
+
+        explicit DOMSelection(const TreeScope*);
 
         // Convenience method for accessors, does not NULL check m_frame.
         const VisibleSelection& visibleSelection() const;
