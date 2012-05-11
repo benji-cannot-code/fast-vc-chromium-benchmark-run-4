8311881cf1baa64600627f1d9f07648d2ecd78a7
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-11 20:12:37 +0000

    Add the ability to skip a sub folder of webgl conformance tests Skip conformance/OGLES and conformance/more.
    The reasons we want to skip these two subsets are:
    1) The bot cycles are too long at the moment (just for gpu_tests, it's 45+ on debug)
    2) for more/, most likely it's been covered already in the basic tests
    3) for ogles/, we almost never touch any code that might break these tests
    
    The mechanism is to support * in test names matching.
    
    In this CL we only add support for * at the end of a test name, which should be enough for this skip-a-folder purpose.
    
    BUG=
    TEST=content_unittests
    Review URL: https://chromiumcodereview.appspot.com/10388093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136647 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/gpu_test_expectations_parser.h
+++ content/test/gpu/gpu_test_expectations_parser.h
@@ -21,6 +21,7 @@ class GPUTestExpectationsParser {
     kGpuTestFail = 1 << 1,
     kGpuTestFlaky = 1 << 2,
     kGpuTestTimeout = 1 << 3,
+    kGpuTestSkip = 1 << 4,
   };
 
   enum GPUTestProfile {
