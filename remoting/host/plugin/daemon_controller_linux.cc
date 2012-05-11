e8047ba5f6971a59f028af7bf9c2704aa64c8fd1
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-11 00:44:11 +0000

    Implemented getVersion framework.
    BUG=125998
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10356118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136465 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/daemon_controller_linux.cc
+++ remoting/host/plugin/daemon_controller_linux.cc
@@ -65,6 +65,7 @@ class DaemonControllerLinux : public remoting::DaemonController {
                             const CompletionCallback& done_callback) OVERRIDE;
   virtual void Stop(const CompletionCallback& done_callback) OVERRIDE;
   virtual void SetWindow(void* window_handle) OVERRIDE;
+  virtual void GetVersion(const GetVersionCallback& done_callback) OVERRIDE;
 
  private:
   FilePath GetConfigPath();
@@ -184,6 +185,12 @@ void DaemonControllerLinux::Stop(const CompletionCallback& done_callback) {
 
 void DaemonControllerLinux::SetWindow(void* window_handle) {
   // noop
+}
+
+void DaemonControllerLinux::GetVersion(
+    const GetVersionCallback& done_callback) {
+  NOTIMPLEMENTED();
+  done_callback.Run("");
 }
 
 FilePath DaemonControllerLinux::GetConfigPath() {
