f843b2ed012d0f4ad59ab33ab3aae22a7ba45fb4
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-11 22:23:36 +0000

    Switch client to Pepper UDP API instead of Transport API.
    BUG=109630
    
    
    Review URL: https://chromiumcodereview.appspot.com/10284008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136692 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/chromoting_client.cc
+++ remoting/client/chromoting_client.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/negotiating_authenticator.h"
 #include "remoting/protocol/v1_authenticator.h"
 #include "remoting/protocol/session_config.h"
+#include "remoting/protocol/transport.h"
 
 namespace remoting {
 
@@ -47,7 +48,9 @@ ChromotingClient::ChromotingClient(const ClientConfig& config,
 ChromotingClient::~ChromotingClient() {
 }
 
-void ChromotingClient::Start(scoped_refptr<XmppProxy> xmpp_proxy) {
+void ChromotingClient::Start(
+    scoped_refptr<XmppProxy> xmpp_proxy,
+    scoped_ptr<protocol::TransportFactory> transport_factory) {
   DCHECK(message_loop()->BelongsToCurrentThread());
 
   scoped_ptr<protocol::Authenticator> authenticator;
@@ -61,8 +64,8 @@ void ChromotingClient::Start(scoped_refptr<XmppProxy> xmpp_proxy) {
   }
 
   connection_->Connect(xmpp_proxy, config_.local_jid, config_.host_jid,
-                       config_.host_public_key, authenticator.Pass(),
-                       this, this, this, this);
+                       config_.host_public_key, transport_factory.Pass(),
+                       authenticator.Pass(), this, this, this, this);
 
   if (!view_->Initialize()) {
     ClientDone();
