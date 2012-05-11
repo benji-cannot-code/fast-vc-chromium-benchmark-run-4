12cac5aaec6d649b014137539268319de4e4c16e
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-11 21:24:09 +0000

    Unreviewed Chromium build fix after http://trac.webkit.org/changeset/116799.
    * rendering/RenderLayerBacking.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerBacking.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerBacking.cpp
@@ -55,6 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StyleResolver.h"
 #include "TiledBacking.h"
 
+#include <wtf/CurrentTime.h>
+
 #if ENABLE(CSS_FILTERS)
 #include "FilterEffectRenderer.h"
 #endif
