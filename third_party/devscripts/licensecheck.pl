1017e27a0f7fd1307bf8c022d52e9df5f73d261b
slamm@google.com <slamm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-11 01:43:24 +0000

    Match ISC license with optional phrase ("and its documentation") used by dnspython.
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10310044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136479 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/devscripts/licensecheck.pl
+++ third_party/devscripts/licensecheck.pl
@@ -435,7 +435,7 @@ sub parselicense($) {
 	$license = "MIT/X11 (BSD like) $license";
     }
 
-    if ($licensetext  =~ /Permission to use, copy, modify, and(\/or)? distribute this software for any purpose (with or )?without fee is hereby granted, provided.*(copyright|entire) notice.*all copies/) {
+    if ($licensetext  =~ /Permission to use, copy, modify, and(\/or)? distribute this software (and its documentation )?for any purpose (with or )?without fee is hereby granted, provided.*(copyright|entire) notice.*all copies/) {
 	$license = "ISC $license";
     }
 
