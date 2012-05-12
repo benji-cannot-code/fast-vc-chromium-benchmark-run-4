5ed051a0ab409e2bf4f8c01d6a9e6070ecf49ffe
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-12 13:39:09 +0000

    Web Inspector: Turn HelpScreen to be View. https://bugs.webkit.org/show_bug.cgi?id=85711
    Patch by Eugene Klyuchnikov <eustas.bug@gmail.com> on 2012-05-12
    Reviewed by Yury Semikhatsky.
    
    Motivation: for further UI changes, HelpSceen needs to be View.
    It is planned to combine Settings Screen and Shortcuts Screen in one
    tabbed screen.
    Bonus: "helpScreen.css" will be lazy-loaded.
    Additional changes: move settingsScreen logic out of inspector.js
    
    UI change, no test required.
    
    * WebCore.gypi: Change "helpScreen.css" file group.
    * inspector/front-end/HelpScreen.js:
    (WebInspector.HelpScreen): Turned to View subclass.
    (WebInspector.HelpScreen.prototype.showModal): Remove "onHide" param
    (WebInspector.HelpScreen.prototype.hide): Ditto.
    (WebInspector.HelpScreen.prototype._onKeyDown): Adopt View members.
    (WebInspector.HelpScreen.prototype._onBlur): Ditto.
    * inspector/front-end/SettingsScreen.js: Adopt new workflow.
    (WebInspector.SettingsScreen): Put onHide function to member
    (WebInspector.SettingsScreen.prototype.willHide): Invoke onHide
    (WebInspector.SettingsController): Mediator pattern - this class
    takes care of status bar button - settings screen relationship.
    (WebInspector.SettingsController.prototype.get statusBarItem):
    Getter fot representative element.
    (WebInspector.SettingsController.prototype._buttonClicked):
    Classifies user action.
    (WebInspector.SettingsController.prototype._onHideSettingsScreen):
    Cleanup after settings screen is hidden.
    (WebInspector.SettingsController.prototype._showSettingsScreen):
    Presents settings screen.
    (WebInspector.SettingsController.prototype._hideSettingsScreen):
    Hides settings screen.
    * inspector/front-end/ShortcutsScreen.js: Adopt new workflow.
    (WebInspector.ShortcutsScreen.prototype.wasShown): Lazy initialization.
    * inspector/front-end/WorkerManager.js: Adopt new workflow.
    (WebInspector.WorkerManager.prototype._disconnectedFromWorker): Ditto.
    (WebInspector.WorkerTerminatedScreen.prototype.willHide): Ditto.
    * inspector/front-end/helpScreen.css: Fix spacing.
    (.help-window-outer): Ditto.
    (body.compact .help-window-outer): Ditto.
    (body.compact .help-window-main): Ditto.
    (body.compact .help-window-caption): Ditto.
    (.help-content): Ditto.
    (body.compact .help-content): Ditto.
    (.help-content select): Ditto.
    * inspector/front-end/inspector.html: Ditto.
    * inspector/front-end/inspector.js: Ditto.
    (WebInspector._createGlobalStatusBarItems): Create SettingsController.
    (WebInspector.documentKeyDown): Adopt new workflow.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/WorkerManager.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/WorkerManager.js
@@ -206,13 +206,9 @@ WebInspector.WorkerManager.prototype = {
 
     _disconnectedFromWorker: function()
     {
-        function onHide()
-        {
-            WebInspector.debuggerModel.removeEventListener(WebInspector.DebuggerModel.Events.GlobalObjectCleared, screen.hide, screen);
-        }
         var screen = new WebInspector.WorkerTerminatedScreen();
         WebInspector.debuggerModel.addEventListener(WebInspector.DebuggerModel.Events.GlobalObjectCleared, screen.hide, screen);
-        screen.show(onHide.bind(this));
+        screen.showModal();
     }
 }
 
@@ -271,6 +267,17 @@ WebInspector.WorkerTerminatedScreen = function()
     var p = this.contentElement.createChild("p");
     p.addStyleClass("help-section");
     p.textContent = WebInspector.UIString("Inspected worker has terminated. Once it restarts we will attach to it automatically.");
+}
+
+WebInspector.WorkerTerminatedScreen.prototype = {
+    /**
+     * @override
+     */
+    willHide: function()
+    {
+        WebInspector.debuggerModel.removeEventListener(WebInspector.DebuggerModel.Events.GlobalObjectCleared, this.hide, this);
+        WebInspector.HelpScreen.prototype.willHide.call(this);
+    }
 }
 
 WebInspector.WorkerTerminatedScreen.prototype.__proto__ = WebInspector.HelpScreen.prototype;
