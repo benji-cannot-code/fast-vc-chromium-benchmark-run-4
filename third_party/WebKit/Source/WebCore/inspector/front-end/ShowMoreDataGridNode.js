c3696131a351cc6f0a36e7f04cb90d5115067b6f
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-12 10:17:18 +0000

    Web Inspector: allow showing arbitrary range of nodes in heap snapshot view https://bugs.webkit.org/show_bug.cgi?id=86230
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Test: inspector/profiler/heap-snapshot-summary-show-ranges.html
    
    It was only possible to expand heap snapshot node children sequentially starting
    from the first child and then pressing either "Show next X items" or "Show all X items".
    This change makes it possible to show any range of children.
    
    * inspector/front-end/HeapSnapshot.js:
    (WebInspector.HeapSnapshotFilteredOrderedIterator):
    (WebInspector.HeapSnapshotFilteredOrderedIterator.prototype.serializeItemsRange):
    (WebInspector.HeapSnapshotFilteredOrderedIterator.prototype.sortAll):
    (WebInspector.HeapSnapshotFilteredOrderedIterator.prototype.sortAndRewind):
    (WebInspector.HeapSnapshotEdgesProvider.prototype.serializeItem):
    (WebInspector.HeapSnapshotNodesProvider.prototype.serializeItem):
    * inspector/front-end/HeapSnapshotGridNodes.js:
    (WebInspector.HeapSnapshotGridNode):
    (WebInspector.HeapSnapshotGridNode.prototype._populate.sorted):
    (WebInspector.HeapSnapshotGridNode.prototype._populate):
    (WebInspector.HeapSnapshotGridNode.prototype._populateChildren.serializeNextChunk):
    (WebInspector.HeapSnapshotGridNode.prototype._populateChildren.insertRetrievedChild):
    (WebInspector.HeapSnapshotGridNode.prototype._populateChildren.insertShowMoreButton):
    (WebInspector.HeapSnapshotGridNode.prototype._populateChildren.childrenRetrieved):
    (WebInspector.HeapSnapshotGridNode.prototype._populateChildren):
    (WebInspector.HeapSnapshotGridNode.prototype._saveChildren):
    (WebInspector.HeapSnapshotGridNode.prototype.sort.afterSort):
    (WebInspector.HeapSnapshotGridNode.prototype.sort):
    (WebInspector.HeapSnapshotDiffNodesProvider):
    (WebInspector.HeapSnapshotDiffNodesProvider.prototype.serializeItemsRange):
    (WebInspector.HeapSnapshotDiffNodesProvider.prototype.serializeItemsRange.didReceiveDeletedItems):
    (WebInspector.HeapSnapshotDiffNodesProvider.prototype.serializeItemsRange.didReceiveAddedItems):
    (WebInspector.HeapSnapshotDiffNodesProvider.prototype.sortAndRewind):
    * inspector/front-end/HeapSnapshotProxy.js:
    (WebInspector.HeapSnapshotWorker):
    (WebInspector.HeapSnapshotProviderProxy.prototype.serializeItemsRange):
    * inspector/front-end/ShowMoreDataGridNode.js:
    (WebInspector.ShowMoreDataGridNode):
    (WebInspector.ShowMoreDataGridNode.prototype._showNextChunk):
    (WebInspector.ShowMoreDataGridNode.prototype._showAll):
    (WebInspector.ShowMoreDataGridNode.prototype._updateLabels):
    (WebInspector.ShowMoreDataGridNode.prototype.setStartPosition):
    (WebInspector.ShowMoreDataGridNode.prototype.setEndPosition):
    * inspector/front-end/utilities.js:
    
    LayoutTests:
    
    * inspector/profiler/heap-snapshot-comparison-show-all.html:
    * inspector/profiler/heap-snapshot-containment-show-all.html:
    * inspector/profiler/heap-snapshot-dominators-show-all.html:
    * inspector/profiler/heap-snapshot-summary-show-all.html:
    * inspector/profiler/heap-snapshot-summary-show-ranges-expected.txt: Added.
    * inspector/profiler/heap-snapshot-summary-show-ranges.html: Added. Test that
    showing overlapping and disjoint ranges of node children will leave the node
    in a valid state.
    * platform/mac/Skipped: Skip the test on platforms that don't support heap profiling.
    * platform/qt/Skipped:
    * platform/win/Skipped:
    * platform/wincairo/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ShowMoreDataGridNode.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ShowMoreDataGridNode.js
@@ -32,33 +32,50 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * @constructor
  * @extends {WebInspector.DataGridNode}
+ * @param {function(number, number)} callback
+ * @param {number} startPosition
+ * @param {number} endPosition
+ * @param {number} chunkSize
  */
-WebInspector.ShowMoreDataGridNode = function(callback, nextCount, allCount)
+WebInspector.ShowMoreDataGridNode = function(callback, startPosition, endPosition, chunkSize)
 {
-    function populate(count)
-    {
-        var index = this.parent.children.indexOf(this);
-        this.parent.removeChild(this);
-        callback(count, index);
-    }
+    WebInspector.DataGridNode.call(this, {summaryRow:true}, false);
+    this._callback = callback;
+    this._startPosition = startPosition;
+    this._endPosition = endPosition;
+    this._chunkSize = chunkSize;
 
     this.showNext = document.createElement("button");
     this.showNext.setAttribute("type", "button");
-    this.showNext.textContent = WebInspector.UIString("Show next %d", nextCount);
-    this.showNext.addEventListener("click", populate.bind(this, nextCount), false);
+    this.showNext.addEventListener("click", this._showNextChunk.bind(this), false);
 
-    if (allCount) {
-        this.showAll = document.createElement("button");
-        this.showAll.setAttribute("type", "button");
-        this.showAll.textContent = WebInspector.UIString("Show all %d", allCount);
-        this.showAll.addEventListener("click", populate.bind(this, allCount), false);
-    }
+    this.showAll = document.createElement("button");
+    this.showAll.setAttribute("type", "button");
+    this.showAll.addEventListener("click", this._showAll.bind(this), false);
 
-    WebInspector.DataGridNode.call(this, {summaryRow:true}, false);
+    this._updateLabels();
     this.selectable = false;
 }
 
 WebInspector.ShowMoreDataGridNode.prototype = {
+    _showNextChunk: function()
+    {
+        this._callback(this._startPosition, this._startPosition + this._chunkSize);
+    },
+
+    _showAll: function()
+    {
+        this._callback(this._startPosition, this._endPosition);
+    },
+
+    _updateLabels: function()
+    {
+        var totalSize = this._endPosition - this._startPosition;
+        var nextChunkSize = Math.min(this._chunkSize, totalSize);
+        this.showNext.textContent = WebInspector.UIString("Show next %d", nextChunkSize);
+        this.showAll.textContent = WebInspector.UIString("Show all %d", totalSize);
+    },
+
     createCells: function()
     {
         var cell = document.createElement("td");
@@ -77,6 +94,24 @@ WebInspector.ShowMoreDataGridNode.prototype = {
             cell = document.createElement("td");
             this._element.appendChild(cell);
         }
+    },
+
+    /**
+     * @param {number} from
+     */
+    setStartPosition: function(from)
+    {
+        this._startPosition = from;
+        this._updateLabels();
+    },
+
+    /**
+     * @param {number} to
+     */
+    setEndPosition: function(to)
+    {
+        this._endPosition = to;
+        this._updateLabels();
     },
 
     /**
--- third_party/WebKit/Source/WebCore/inspector/front-end/utilities.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/utilities.js
@@ -219,6 +219,10 @@ Object.defineProperty(Array.prototype, "upperBound",
     }
 });
 
+Object.defineProperty(Uint32Array.prototype, "sort", {
+   value: Array.prototype.sort
+});
+
 (function() {
 var partition = {
     /**
