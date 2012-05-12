1464b8256de44621a42c55801888a723a1be56df
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-12 09:01:00 +0000

    Revert 136679 - Delete MockDataSource, remove IsLocalSource & IsSeekable from Demuxer/Pipeline.
    It broke LayoutTests/http/tests/media/media-source/webm/video-media-source-seek.html
    and video-media-source-state-changes.html.
    http://test-results.appspot.com/dashboards/flakiness_dashboard.html#showExpectations=true&showLargeExpectations=true&tests=http%2Ftests%2Fmedia%2Fmedia-source%2Fwebm%2Fvideo-media-source-seek.html%2Chttp%2Ftests%2Fmedia%2Fmedia-source%2Fwebm%2Fvideo-media-source-state-changes.html
    
    Review URL: https://chromiumcodereview.appspot.com/10378100
    TBR=fischman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10378119
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136753 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline_unittest.cc
+++ media/base/pipeline_unittest.cc
@@ -37,6 +37,8 @@ namespace media {
 static const int kTotalBytes = 1024;
 static const int kBufferedBytes = 1024;
 static const int kBitrate = 1234;
+static const bool kLocalSource = false;
+static const bool kSeekable = true;
 
 ACTION_P(InitializeDemuxerWithError, error) {
   arg1.Run(error);
@@ -116,6 +118,10 @@ class PipelineTest : public ::testing::Test {
     // Demuxer properties.
     EXPECT_CALL(*mocks_->demuxer(), GetBitrate())
         .WillRepeatedly(Return(kBitrate));
+    EXPECT_CALL(*mocks_->demuxer(), IsLocalSource())
+        .WillRepeatedly(Return(kLocalSource));
+    EXPECT_CALL(*mocks_->demuxer(), IsSeekable())
+        .WillRepeatedly(Return(kSeekable));
 
     // Configure the demuxer to return the streams.
     for (size_t i = 0; i < streams->size(); ++i) {
@@ -793,6 +799,24 @@ TEST_F(PipelineTest, StartTimeIsNonZero) {
   EXPECT_TRUE(pipeline_->HasVideo());
 
   EXPECT_EQ(kStartTime, pipeline_->GetCurrentTime());
+}
+
+TEST_F(PipelineTest, DemuxerProperties) {
+  CreateAudioStream();
+  CreateVideoStream();
+  MockDemuxerStreamVector streams;
+  streams.push_back(audio_stream());
+  streams.push_back(video_stream());
+
+  InitializeDemuxer(&streams);
+  InitializeAudioDecoder(audio_stream());
+  InitializeAudioRenderer();
+  InitializeVideoDecoder(video_stream());
+  InitializeVideoRenderer();
+  InitializePipeline(PIPELINE_OK);
+
+  EXPECT_EQ(kLocalSource, pipeline_->IsLocalSource());
+  EXPECT_NE(kSeekable, pipeline_->IsStreaming());
 }
 
 class FlexibleCallbackRunner : public base::DelegateSimpleThread::Delegate {
