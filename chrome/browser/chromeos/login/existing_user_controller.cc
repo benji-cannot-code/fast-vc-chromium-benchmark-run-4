dbc4ced651a062f6f6a8fdf88962ebc83d98d06b
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-12 10:46:44 +0000

    [cros] Convert Login.CreateAccount to user action metric.
    BUG=127099
    TEST=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/10389092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136764 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller.cc
+++ chrome/browser/chromeos/login/existing_user_controller.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
+#include "content/public/browser/user_metrics.h"
 #include "grit/generated_resources.h"
 #include "net/http/http_auth_cache.h"
 #include "net/http/http_network_session.h"
@@ -284,7 +285,7 @@ ExistingUserController::~ExistingUserController() {
 //
 
 void ExistingUserController::CreateAccount() {
-  UMA_HISTOGRAM_CUSTOM_COUNTS("Login.CreateAccount", 1, 1, 2, 3);
+  content::RecordAction(content::UserMetricsAction("Login.CreateAccount"));
   guest_mode_url_ =
       google_util::AppendGoogleLocaleParam(GURL(kCreateAccountURL));
   LoginAsGuest();
