011e0cb02e2c6d3c944dfff15f869de96ed850e9
mitchellwrosen@chromium.org <mitchellwrosen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-12 01:57:21 +0000

    "index" property should be optional with "chrome.tabs.move"
    Made the "index" property of "chrome.tabs.move" optional
    
    BUG=33048
    TEST=ExtensionApiTest.TabMove
    
    
    Review URL: https://chromiumcodereview.appspot.com/9699065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136741 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_module.cc
+++ chrome/browser/extensions/extension_tabs_module.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_tabs_module.h"
 
 #include <algorithm>
+#include <limits>
 #include <vector>
 
 #include "base/base64.h"
@@ -1410,12 +1411,10 @@ bool MoveTabsFunction::RunImpl() {
   EXTENSION_FUNCTION_VALIDATE(ReadOneOrMoreIntegers(tab_value, &tab_ids));
 
   DictionaryValue* update_props = NULL;
+  int new_index;
   EXTENSION_FUNCTION_VALIDATE(args_->GetDictionary(1, &update_props));
-
-  int new_index = -1;
-  EXTENSION_FUNCTION_VALIDATE(update_props->GetInteger(
-      keys::kIndexKey, &new_index));
-  EXTENSION_FUNCTION_VALIDATE(new_index >= 0);
+  EXTENSION_FUNCTION_VALIDATE(update_props->GetInteger(keys::kIndexKey,
+      &new_index));
 
   ListValue tab_values;
   for (size_t i = 0; i < tab_ids.size(); ++i) {
@@ -1442,6 +1441,7 @@ bool MoveTabsFunction::RunImpl() {
       int window_id = extension_misc::kUnknownWindowId;
       EXTENSION_FUNCTION_VALIDATE(update_props->GetInteger(
           keys::kWindowIdKey, &window_id));
+
       if (!GetBrowserFromWindowID(this, window_id, &target_browser))
         return false;
 
@@ -1473,7 +1473,8 @@ bool MoveTabsFunction::RunImpl() {
 
         // Clamp move location to the last position.
         // This is ">" because it can append to a new index position.
-        if (new_index > target_tab_strip->count())
+        // -1 means set the move location to the last position.
+        if (new_index > target_tab_strip->count() || new_index < 0)
           new_index = target_tab_strip->count();
 
         target_tab_strip->InsertTabContentsAt(
@@ -1490,7 +1491,8 @@ bool MoveTabsFunction::RunImpl() {
     // Perform a simple within-window move.
     // Clamp move location to the last position.
     // This is ">=" because the move must be to an existing location.
-    if (new_index >= source_tab_strip->count())
+    // -1 means set the move location to the last position.
+    if (new_index >= source_tab_strip->count() || new_index < 0)
       new_index = source_tab_strip->count() - 1;
 
     if (new_index != tab_index)
