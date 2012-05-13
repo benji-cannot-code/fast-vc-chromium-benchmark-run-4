ef0b5dae161dd5e70a519ad279356d0c23501f60
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-13 23:36:03 +0000

    FractionalLayoutUnit class has unneeded redundant uses of "inline" keyword https://bugs.webkit.org/show_bug.cgi?id=86301
    Reviewed by Andreas Kling.
    
    * platform/FractionalLayoutUnit.h: Removed uses of inline for functions
    defined inside a class definition. The C++ language defines that all such
    functions are treated as if specified with inline, and explicitly stating
    inline in addition does not add anything or change behavior.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cg/ImageCG.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cg/ImageCG.cpp
@@ -195,7 +195,7 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& destRect, const F
 {
     startAnimation();
 
-    RetainPtr<CGImageRef> image = frameAtIndex(m_currentFrame);
+    CGImageRef image = frameAtIndex(m_currentFrame);
     if (!image) // If it's too early we won't have an image yet.
         return;
     
@@ -210,7 +210,7 @@ void BitmapImage::draw(GraphicsContext* ctxt, const FloatRect& destRect, const F
     if (shouldRespectImageOrientation == RespectImageOrientation)
         orientation = frameOrientationAtIndex(m_currentFrame);
 
-    ctxt->drawNativeImage(image.get(), selfSize, styleColorSpace, destRect, srcRect, compositeOp, orientation);
+    ctxt->drawNativeImage(image, selfSize, styleColorSpace, destRect, srcRect, compositeOp, orientation);
 
     if (imageObserver())
         imageObserver()->didDraw(this);
