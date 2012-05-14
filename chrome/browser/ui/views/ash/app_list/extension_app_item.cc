abd273705572a8fc4b5e7f98334c3d2986242c7f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 15:16:34 +0000

    app_list: No need to include app_list_item_view.h in extension_app_item.cc
    R=xiyuan@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10310138
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136875 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/app_list/extension_app_item.cc
+++ chrome/browser/ui/views/ash/app_list/extension_app_item.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_icon_set.h"
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
-#include "ui/app_list/app_list_item_view.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/image/image.h"
 
