fa8e29328bb77798c447bbdd575717c559b38b8e
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 13:38:59 +0000

    Mark MediaLayoutTest.Tests DISABLED on Mac, crashes flakily.
    BUG=chromium:128021
    TEST=None
    TBR=jam@chromium.org, scherkus@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10386114
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136862 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media_browsertest.cc
+++ content/browser/media_browsertest.cc
@@ -153,7 +153,8 @@ class MediaLayoutTest : public InProcessBrowserLayoutTest {
 };
 
 // Timing out, see http://crbug.com/127526.
-#if defined(ADDRESS_SANITIZER)
+// Flakily crashing on Mac, see http://crbug.com/128021.
+#if defined(ADDRESS_SANITIZER) || defined(OS_MAC)
 #define MAYBE_Tests DISABLED_Tests
 #else
 #define MAYBE_Tests Tests
