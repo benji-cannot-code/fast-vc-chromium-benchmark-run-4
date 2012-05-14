87c56ea1b3515d49191824e4ede10410c1d6318e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-14 19:24:16 +0000

    [chromium] Compute the best target node on a GestureTap event https://bugs.webkit.org/show_bug.cgi?id=85101
    Patch by Terry Anderson <tdanderson@chromium.org> on 2012-05-14
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Using tests in the touchadjustment/ directory.
    
    * WebCore.gypi:
    Include TouchAdjustment.* files.
    * page/TouchAdjustment.cpp:
    Included math.h because of an 'Undeclared identifier: INFINITY' when
    this patch was landed before.
    
    Source/WebKit/chromium:
    
    * features.gypi:
    Enable the touch adjustment code by default.
    * src/WebInputEventConversion.cpp:
    (WebKit::PlatformGestureEventBuilder::PlatformGestureEventBuilder):
    In the case of a GestureTap, deltaX represents the horizontal padding of the touch
    area and deltaY represents the vertical padding of the touch area.
    
    LayoutTests:
    
    * platform/chromium/test_expectations.txt:
    The flaky tests in touchadjustment/ have been addressed in WK86218.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116983 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/TouchAdjustment.cpp
+++ third_party/WebKit/Source/WebCore/page/TouchAdjustment.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderBox.h"
 #include "RenderObject.h"
 #include "RenderStyle.h"
+#include <math.h>
 
 namespace WebCore {
 
