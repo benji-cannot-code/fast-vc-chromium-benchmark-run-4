c3b947f36e196a722b15017564093a083067aa56
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-14 12:31:07 +0000

    webkit-patch land should automatically add svn:mime-type for .png files https://bugs.webkit.org/show_bug.cgi?id=75825 I refactored the png.py to avoid code duplication.
    Patch by Balazs Ankes <bank@inf.u-szeged.hu> on 2012-05-14
    Reviewed by Dirk Pranke.
    
    * Scripts/webkitpy/common/checksvnconfigfile.py: Added.
    (check):
    (_config_file_path):
    (errorstr_autoprop):
    (errorstr_png):
    * Scripts/webkitpy/style/checkers/png.py:
    (PNGChecker.check):
    * Scripts/webkitpy/tool/commands/download.py:
    (Land):
    * Scripts/webkitpy/tool/steps/__init__.py:
    * Scripts/webkitpy/tool/steps/addsvnmimetypeforpng.py: Added.
    (AddSvnMimetypeForPng):
    (AddSvnMimetypeForPng.__init__):
    (AddSvnMimetypeForPng.run):
    (AddSvnMimetypeForPng._check_pngs):
    * Scripts/webkitpy/tool/steps/addsvnmimetypeforpng_unittest.py: Added.
    (MockSCMDetector):
    (MockSCMDetector.__init__):
    (MockSCMDetector.display_name):
    (AddSvnMimetypeForPngTest):
    (AddSvnMimetypeForPngTest.test_run):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/__init__.py
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # FIXME: Is this the right way to do this?
+from webkitpy.tool.steps.addsvnmimetypeforpng import AddSvnMimetypeForPng
 from webkitpy.tool.steps.applypatch import ApplyPatch
 from webkitpy.tool.steps.applypatchwithlocalcommit import ApplyPatchWithLocalCommit
 from webkitpy.tool.steps.applywatchlist import ApplyWatchList
