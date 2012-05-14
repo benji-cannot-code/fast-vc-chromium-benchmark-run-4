f45720c1d0b5f799bf3facfa2e7e957e85f17e57
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 21:21:16 +0000

    Turn off simple scrolling by default. This is necessary for OOBE.
    BUG=122403
    TEST=simple scrolling should be off on oobe screen.
    Review URL: https://chromiumcodereview.appspot.com/10382152
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136985 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/x/events_x.cc
+++ ui/base/x/events_x.cc
@@ -291,7 +291,7 @@ class UI_EXPORT CMTEventData {
 
   };
 
-  CMTEventData() : natural_scroll_enabled_(true) {
+  CMTEventData() : natural_scroll_enabled_(false) {
     UpdateDeviceList(ui::GetXDisplay());
   }
 
