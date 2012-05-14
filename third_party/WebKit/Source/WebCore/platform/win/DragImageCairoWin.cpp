1f052e2491794183a67e73b57e25526cc14d0420
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-14 23:19:09 +0000

    [WinCairo] Unreviewed build correction.
    * platform/win/DragImageCairoWin.cpp: Add missing include for
    new NativeImageCairo.h type.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117011 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/DragImageCairoWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/win/DragImageCairoWin.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "GraphicsContextPlatformPrivateCairo.h"
 #include "Image.h"
+#include "NativeImageCairo.h"
 #include <wtf/RetainPtr.h>
 #include <cairo-win32.h>
 #include <windows.h>
