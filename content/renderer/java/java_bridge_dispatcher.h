14beeb9c5bece4e0212863deb0fac71068e76b25
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 20:22:25 +0000

    Fix JavaBridgeDispatcher to ref-count JavaBridgeChannel
    JavaBridgeChannel inherits from NPChannelBase, which is ref-counted, so we
    should use scoped_refptr, not scoped_ptr.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10356149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136965 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/java/java_bridge_dispatcher.h
+++ content/renderer/java/java_bridge_dispatcher.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -39,7 +39,7 @@ class JavaBridgeDispatcher : public content::RenderViewObserver {
   // cleared. We hold a ref to these.
   typedef std::map<string16, NPVariant> ObjectMap;
   ObjectMap objects_;
-  scoped_ptr<JavaBridgeChannel> channel_;
+  scoped_refptr<JavaBridgeChannel> channel_;
 };
 
 #endif  // CONTENT_RENDERER_JAVA_JAVA_BRIDGE_DISPATCHER_H_
