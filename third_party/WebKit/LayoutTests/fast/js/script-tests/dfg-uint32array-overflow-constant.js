9ae576e8c45f1ba3a6651eb93650350b1a8495d9
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-14 06:47:24 +0000

    DFG performs incorrect constant folding on double-to-uint32 conversion in Uint32Array PutByVal https://bugs.webkit.org/show_bug.cgi?id=86330
    Source/JavaScriptCore: 
    
    Reviewed by Darin Adler.
            
    static_cast<int>(d) is wrong, since JS semantics require us to use toInt32(d).
    In particular, C++ casts on typical hardware (like x86 and similar) will
    return 0x80000000 for double values that are out of range of the int32 domain
    (i.e. less than -2^31 or greater than or equal to 2^31). But JS semantics call
    for wrap-around; for example the double value 4294967297 ought to become the
    int32 value 1, not 0x80000000.
    
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compilePutByValForIntTypedArray):
    
    LayoutTests: 
    
    Rubber stamped by Darin Adler.
    
    * fast/js/dfg-uint32array-overflow-constant-expected.txt: Added.
    * fast/js/dfg-uint32array-overflow-constant.html: Added.
    * fast/js/script-tests/dfg-uint32array-overflow-constant.js: Added.
    (foo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/dfg-uint32array-overflow-constant.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Tests that storing a value that is outside of the int32 range into a Uint32Array results in correct wrap-around."
+);
+
+function foo(a) {
+    a[0] = 0x8005465c;
+}
+
+var array = new Uint32Array(1);
+
+for (var i = 0; i < 200; ++i) {
+    foo(array);
+    shouldBe("array[0]", "0x8005465c");
+}
+
