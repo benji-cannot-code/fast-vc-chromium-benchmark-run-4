5cb0bd50c92530b0ea07b6552965d783cd24e7c1
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-14 20:11:23 +0000

    garden-o-matic should specify which types of baselines to rebaseline https://bugs.webkit.org/show_bug.cgi?id=86242
    Reviewed by Adam Barth.
    
    This change modifies garden-o-matic so that we are explicit
    about whether to rebaseline the text file, the image file, or
    both. This should prevent us from pulling incorrect or
    unexpected baselines off the bots.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/checkout.js:
    (.):
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/checkout_unittests.js:
    (.):
    * Scripts/webkitpy/tool/commands/rebaseline.py:
    (AbstractRebaseliningCommand):
    (AbstractRebaseliningCommand.__init__):
    (RebaselineTest):
    (RebaselineTest._rebaseline_test_and_update_expectations):
    (RebaselineTest.execute):
    (OptimizeBaselines):
    (OptimizeBaselines._optimize_baseline):
    (OptimizeBaselines.execute):
    (AnalyzeBaselines):
    (AnalyzeBaselines._analyze_baseline):
    (AnalyzeBaselines.execute):
    (RebaselineExpectations._rebaseline_port):
    * Scripts/webkitpy/tool/servers/gardeningserver.py:
    (GardeningHTTPRequestHandler.rebaseline):
    (GardeningHTTPRequestHandler.optimizebaselines):
    * Scripts/webkitpy/tool/servers/gardeningserver_unittest.py:
    (GardeningServerTest.test_rebaseline):
    (GardeningServerTest.test_rebaseline_new_port):
    (GardeningServerTest.test_optimizebaselines):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/checkout.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/checkout.js
@@ -76,11 +76,12 @@ checkout.updateExpectations = function(failureInfoList, callback, checkoutUnavai
     }, checkoutUnavailable);
 };
 
-checkout.optimizeBaselines = function(testName, callback, checkoutUnavailable)
+checkout.optimizeBaselines = function(testName, suffixes, callback, checkoutUnavailable)
 {
     callIfCheckoutAvailable(function() {
         net.post(config.kLocalServerURL + '/optimizebaselines?' + $.param({
             'test': testName,
+            'suffixes': suffixes,
         }), function() {
             callback();
         });
@@ -103,9 +104,12 @@ checkout.rebaseline = function(failureInfoList, callback, progressCallback, chec
 {
     callIfCheckoutAvailable(function() {
         base.callInSequence(function(failureInfo, callback) {
+            var suffixes = base.uniquifyArray(base.flattenArray(failureInfo.failureInfoList.map(results.failureTypeToExtensionList))).join(',');
+
             net.post(config.kLocalServerURL + '/rebaseline?' + $.param({
                 'builder': failureInfo.builderName,
                 'test': failureInfo.testName,
+                'suffixes': suffixes,
             }), function() {
                 if (progressCallback)
                     progressCallback(failureInfo);
@@ -113,7 +117,11 @@ checkout.rebaseline = function(failureInfoList, callback, progressCallback, chec
             });
         }, failureInfoList, function() {
             var testNameList = base.uniquifyArray(failureInfoList.map(function(failureInfo) { return failureInfo.testName; }));
-            base.callInSequence(checkout.optimizeBaselines, testNameList, callback);
+            var suffixes = base.uniquifyArray(base.flattenArray(
+                failureInfoList.map(function(failureInfo) {
+                    return base.flattenArray(failureInfo.failureInfoList.map(results.failureTypeToExtensionList))
+                }))).join(',');
+            base.callInSequence(function(testName, callback) { checkout.optimizeBaselines(testName, suffixes, callback)}, testNameList, callback);
         });
     }, checkoutUnavailable);
 };
--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/gardeningserver.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/gardeningserver.py
@@ -140,8 +140,10 @@ class GardeningHTTPRequestHandler(ReflectionHandler):
         builder = self.query['builder'][0]
         command = [
             'rebaseline-test',
+            '--suffixes', self.query['suffixes'][0],
             builder,
             self.query['test'][0],
+
         ]
         command.extend(builders.fallback_port_names_for_new_port(builder))
         self._run_webkit_patch(command)
@@ -151,6 +153,7 @@ class GardeningHTTPRequestHandler(ReflectionHandler):
         test = self.query['test'][0]
         self._run_webkit_patch([
             'optimize-baselines',
+            '--suffixes', self.query['suffixes'][0],
             test,
         ])
         self._serve_text('success')
