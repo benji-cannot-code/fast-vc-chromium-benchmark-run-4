ab343c82b00bc64a131e8bc92866165e1f78d99b
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 21:19:24 +0000

    Changing default for calibration from enable to disable
    BUG=127616
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10332133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136983 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ui_base_switches.h
+++ ui/base/ui_base_switches.h
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 UI_EXPORT extern const char kDefaultDeviceScaleFactor[];
-UI_EXPORT extern const char kDisableTouchCalibration[];
 UI_EXPORT extern const char kEnableTextSubpixelPositioning[];
+UI_EXPORT extern const char kEnableTouchCalibration[];
 UI_EXPORT extern const char kEnableTouchEvents[];
 UI_EXPORT extern const char kLang[];
 UI_EXPORT extern const char kLocalePak[];
