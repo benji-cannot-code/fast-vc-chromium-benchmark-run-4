496be33f8444d9463dc949ba432acb240d421bab
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-14 15:35:38 +0000

    Source/JavaScriptCore: DFG JIT didn't work with ARM EABI. https://bugs.webkit.org/show_bug.cgi?id=84449
    Patch by Yong Li <yoli@rim.com> on 2012-05-14
    Reviewed by Filip Pizlo.
    
    Add a 32-bit dummy argument for some callOperation()
    methods to make it work for ARM EABI.
    
    * dfg/DFGCCallHelpers.h:
    (JSC::DFG::CCallHelpers::setupArgumentsWithExecState):
    (CCallHelpers):
    * dfg/DFGOperations.cpp:
    * dfg/DFGSpeculativeJIT.h:
    (SpeculativeJIT):
    (JSC::DFG::SpeculativeJIT::callOperation):
    
    Source/WTF: DFG JIT is not ARM EABI compatible
    https://bugs.webkit.org/show_bug.cgi?id=84449
    
    Patch by Yong Li <yoli@rim.com> on 2012-05-14
    Reviewed by Filip Pizlo.
    
    Add COMPILER_SUPPORTS(EABI) when __ARM_EABI__
    or __EABI__ is defined.
    
    * wtf/Compiler.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@116951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Compiler.h
+++ third_party/WebKit/Source/WTF/wtf/Compiler.h
@@ -253,4 +253,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
+/* ABI */
+#if defined(__ARM_EABI__) || defined(__EABI__)
+#define WTF_COMPILER_SUPPORTS_EABI 1
+#endif
+
 #endif /* WTF_Compiler_h */
