168e3e5bb528a6098230cc074aa041a8be8404d9
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-14 18:47:45 +0000

    Revert 136762 - Bump Breakpad revision to r963 Reason: symupload got broken
    BUG=127776
    TBR=mark
    Review URL: https://chromiumcodereview.appspot.com/10377108
    
    TBR=glider@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10398022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@136925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/crash_service/crash_service.cc
+++ chrome/tools/crash_service/crash_service.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -247,8 +247,6 @@ bool CrashService::Initialize(const std::wstring& command_line) {
                                       &CrashService::OnClientConnected, this,
                                       &CrashService::OnClientDumpRequest, this,
                                       &CrashService::OnClientExited, this,
-                                      /*upload_request_callback*/ NULL,
-                                      /* upload_context */ NULL,
                                       true, &dumps_path.value());
 
   if (!dumper_) {
