bc863fc472ec6bc889b52edfd76664569f06478e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-15 01:10:52 +0000

    [chromium] Prevent KeyCodeConversionAndroid from breaking on next NDK roll https://bugs.webkit.org/show_bug.cgi?id=86415
    Patch by Alexandre Elias <aelias@google.com> on 2012-05-14
    Reviewed by Adam Barth.
    
    The new enum values added in this file have been added in recent
    versions of the NDK, so this file will fail to compile with duplicate
    enum error when we roll NDK to r7c or higher.  A workaround is
    to move our enum values to the WebCore namespace.  When the NDK
    upgrade is complete, we can delete them.
    
    No new tests. (No-op change.)
    
    * platform/chromium/KeyCodeConversionAndroid.cpp:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117022 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/KeyCodeConversionAndroid.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/KeyCodeConversionAndroid.cpp
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <android/keycodes.h>
 
+namespace WebCore {
+
 // The Android NDK does not provide values for these yet:
 enum {
     AKEYCODE_ESCAPE          = 111,
@@ -80,8 +82,6 @@ enum {
     AKEYCODE_CHANNEL_UP      = 166,
     AKEYCODE_CHANNEL_DOWN    = 167,
 };
-
-namespace WebCore {
 
 int windowsKeyCodeForKeyEvent(unsigned int keyCode)
 {
