6d7b6d9f14e09253e2c0e4064c93350d1ccbda7b
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 21:50:04 +0000

    Missed a file in crrev.com/137247 (Moving libxml_utils). Fixing Chrome ChromeOS bot.
    TBR=dtu@chromium.org
    BUG=None.
    TEST=None.
    
    Review URL: https://chromiumcodereview.appspot.com/10386149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/bluetooth/bluetooth_service_record.cc
+++ chrome/browser/chromeos/bluetooth/bluetooth_service_record.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/string_number_conversions.h"
-#include "chrome/common/libxml_utils.h"
+#include "third_party/libxml/chromium/libxml_utils.h"
 
 namespace {
 
