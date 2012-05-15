d424b455ca78b56b8211e60b15fabbdd0063f775
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-15 01:06:42 +0000

    https://bugs.webkit.org/show_bug.cgi?id=86420 ScrollbarPainter should support expansionTransitionProgress
    Reviewed by Sam Weinig.
    
    expansionTransitionProgress works the same as 
    uiStateTransitionProgress. This code just echoes that code, but for 
    expansion instead of uiState.
    * platform/mac/NSScrollerImpDetails.h:
    * platform/mac/ScrollAnimatorMac.mm:
    (supportsExpansionTransitionProgress):
    (-[WebScrollbarPartAnimation setCurrentProgress:]):
    (-[WebScrollbarPainterDelegate cancelAnimations]):
    (-[WebScrollbarPainterDelegate scrollerImp:animateExpansionTransitionWithDuration:]):
    (-[WebScrollbarPainterDelegate invalidate]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ScrollAnimatorMac.mm
@@ -54,6 +54,12 @@ static bool supportsUIStateTransitionProgress()
     return globalSupportsUIStateTransitionProgress;
 }
 
+static bool supportsExpansionTransitionProgress()
+{
+    static bool globalSupportsExpansionTransitionProgress = [NSClassFromString(@"NSScrollerImp") instancesRespondToSelector:@selector(expansionTransitionProgress)];
+    return globalSupportsExpansionTransitionProgress;
+}
+
 static ScrollbarThemeMac* macScrollbarTheme()
 {
     ScrollbarTheme* scrollbarTheme = ScrollbarTheme::theme();
@@ -279,7 +285,8 @@ static NSSize abs(NSSize size)
 enum FeatureToAnimate {
     ThumbAlpha,
     TrackAlpha,
-    UIStateTransition
+    UIStateTransition,
+    ExpansionTransition
 };
 
 @interface WebScrollbarPartAnimation : NSAnimation
@@ -352,6 +359,9 @@ enum FeatureToAnimate {
     case UIStateTransition:
         [_scrollbarPainter.get() setUiStateTransitionProgress:currentValue];
         break;
+    case ExpansionTransition:
+        [_scrollbarPainter.get() setExpansionTransitionProgress:currentValue];
+        break;
     }
 
     _scrollbar->invalidate();
@@ -374,6 +384,7 @@ enum FeatureToAnimate {
     RetainPtr<WebScrollbarPartAnimation> _knobAlphaAnimation;
     RetainPtr<WebScrollbarPartAnimation> _trackAlphaAnimation;
     RetainPtr<WebScrollbarPartAnimation> _uiStateTransitionAnimation;
+    RetainPtr<WebScrollbarPartAnimation> _expansionTransitionAnimation;
 }
 - (id)initWithScrollbar:(WebCore::Scrollbar*)scrollbar;
 - (void)cancelAnimations;
@@ -397,6 +408,7 @@ enum FeatureToAnimate {
     [_knobAlphaAnimation.get() stopAnimation];
     [_trackAlphaAnimation.get() stopAnimation];
     [_uiStateTransitionAnimation.get() stopAnimation];
+    [_expansionTransitionAnimation.get() stopAnimation];
     END_BLOCK_OBJC_EXCEPTIONS;
 }
 
@@ -531,6 +543,36 @@ enum FeatureToAnimate {
     [_uiStateTransitionAnimation.get() startAnimation];
 }
 
+- (void)scrollerImp:(id)scrollerImp animateExpansionTransitionWithDuration:(NSTimeInterval)duration
+{
+    if (!_scrollbar)
+        return;
+
+    if (!supportsExpansionTransitionProgress())
+        return;
+
+    ASSERT(scrollerImp == scrollbarPainterForScrollbar(_scrollbar));
+
+    ScrollbarPainter scrollbarPainter = (ScrollbarPainter)scrollerImp;
+
+    // ExpansionTransition always animates to 1. In case an animation is in progress this avoids a hard transition.
+    [scrollbarPainter setExpansionTransitionProgress:1 - [scrollerImp expansionTransitionProgress]];
+
+    if (!_expansionTransitionAnimation) {
+        _expansionTransitionAnimation.adoptNS([[WebScrollbarPartAnimation alloc] initWithScrollbar:_scrollbar
+                                                                                  featureToAnimate:ExpansionTransition
+                                                                                       animateFrom:[scrollbarPainter expansionTransitionProgress]
+                                                                                         animateTo:1.0
+                                                                                          duration:duration]);
+    } else {
+        // If we don't need to initialize the animation, just reset the values in case they have changed.
+        [_expansionTransitionAnimation.get() setStartValue:[scrollbarPainter uiStateTransitionProgress]];
+        [_expansionTransitionAnimation.get() setEndValue:1.0];
+        [_expansionTransitionAnimation.get() setDuration:duration];
+    }
+    [_expansionTransitionAnimation.get() startAnimation];
+}
+
 - (void)scrollerImp:(id)scrollerImp overlayScrollerStateChangedTo:(NSUInteger)newOverlayScrollerState
 {
     UNUSED_PARAM(scrollerImp);
@@ -544,6 +586,7 @@ enum FeatureToAnimate {
     [_knobAlphaAnimation.get() invalidate];
     [_trackAlphaAnimation.get() invalidate];
     [_uiStateTransitionAnimation.get() invalidate];
+    [_expansionTransitionAnimation.get() invalidate];
     END_BLOCK_OBJC_EXCEPTIONS;
 }
 
