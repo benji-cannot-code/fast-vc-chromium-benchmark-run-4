3886fe1d99e076c238375bcb97510bb339c9b10e
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 02:27:18 +0000

    Better fix for closing uber tray when clicking on Desktop
    Bug=None
    Test=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/9877015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137059 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/widget.cc
+++ ui/views/widget/widget.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/views/widget/widget.h"
 
+#include "base/bind.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/utf_string_conversions.h"
@@ -108,6 +109,7 @@ Widget::InitParams::InitParams()
                   ViewsDelegate::views_delegate->UseTransparentWindows()),
       accept_events(true),
       can_activate(true),
+      close_on_deactivate(false),
       keep_on_top(false),
       ownership(NATIVE_WIDGET_OWNS_WIDGET),
       mirror_origin_in_rtl(false),
@@ -132,6 +134,7 @@ Widget::InitParams::InitParams(Type type)
                   ViewsDelegate::views_delegate->UseTransparentWindows()),
       accept_events(true),
       can_activate(type != TYPE_POPUP && type != TYPE_MENU),
+      close_on_deactivate(false),
       keep_on_top(type == TYPE_MENU),
       ownership(NATIVE_WIDGET_OWNS_WIDGET),
       mirror_origin_in_rtl(false),
@@ -154,7 +157,8 @@ gfx::NativeView Widget::InitParams::GetParent() const {
 // Widget, public:
 
 Widget::Widget()
-    : native_widget_(NULL),
+    : ALLOW_THIS_IN_INITIALIZER_LIST(set_capture_factory_(this)),
+      native_widget_(NULL),
       widget_delegate_(NULL),
       non_client_view_(NULL),
       dragged_view_(NULL),
@@ -166,6 +170,7 @@ Widget::Widget()
       widget_closed_(false),
       saved_show_state_(ui::SHOW_STATE_DEFAULT),
       focus_on_creation_(true),
+      close_on_deactivate_(false),
       is_top_level_(false),
       native_widget_initialized_(false),
       native_widget_destroyed_(false),
@@ -317,6 +322,7 @@ void Widget::Init(const InitParams& params) {
     SetContentsView(params.delegate->GetContentsView());
     SetInitialBoundsForFramelessWindow(params.bounds);
   }
+  close_on_deactivate_ = params.close_on_deactivate;
   native_widget_initialized_ = true;
 }
 
@@ -511,6 +517,14 @@ void Widget::Show() {
     saved_show_state_ = ui::SHOW_STATE_NORMAL;
   } else {
     native_widget_->Show();
+  }
+
+  if (close_on_deactivate_) {
+    // Set mouse capture on timeout in case this is called from a
+    // mouse pressed handler.
+    MessageLoopForUI::current()->PostTask(FROM_HERE, base::Bind(
+           &Widget::SetMouseCapture, set_capture_factory_.GetWeakPtr(),
+           static_cast<View*>(NULL)));
   }
 }
 
@@ -1051,6 +1065,8 @@ bool Widget::OnMouseEvent(const MouseEvent& event) {
           native_widget_->SetCapture();
         return true;
       }
+      if (close_on_deactivate_ && !GetRootView()->HitTest(event.location()))
+        Close();
       return false;
     case ui::ET_MOUSE_RELEASED:
       last_mouse_event_was_move_ = false;
@@ -1091,6 +1107,11 @@ void Widget::OnMouseCaptureLost() {
   if (is_mouse_button_pressed_)
     GetRootView()->OnMouseCaptureLost();
   is_mouse_button_pressed_ = false;
+
+  // Without mouse capture, the widget doesn't process all events so can miss
+  // the user clicking outside the root view's bounds.
+  if (close_on_deactivate_)
+    Close();
 }
 
 ui::TouchStatus Widget::OnTouchEvent(const TouchEvent& event) {
@@ -1158,7 +1179,7 @@ void Widget::DestroyRootView() {
 // Widget, private:
 
 bool Widget::ShouldReleaseCaptureOnMouseReleased() const {
-  return true;
+  return !close_on_deactivate_;
 }
 
 void Widget::SetInactiveRenderingDisabled(bool value) {
