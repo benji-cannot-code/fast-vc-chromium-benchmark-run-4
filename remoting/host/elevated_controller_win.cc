942839af479842a2a0a26ca25fa60125a869417d
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 01:49:18 +0000

    Chromoting: Implemented GetVersion for Windows.
    BUG=125998
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10310154
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137050 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/elevated_controller_win.cc
+++ remoting/host/elevated_controller_win.cc
@@ -8,11 +8,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sddl.h>
 
 #include "base/file_util.h"
+#include "base/file_version_info.h"
 #include "base/logging.h"
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/path_service.h"
+#include "base/process_util.h"
 #include "base/stringize_macros.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
@@ -328,7 +330,24 @@ STDMETHODIMP ElevatedControllerWin::GetConfig(BSTR* config_out) {
 }
 
 STDMETHODIMP ElevatedControllerWin::GetVersion(BSTR* version_out) {
-  return E_NOTIMPL;
+  // Report the product version number of the daemon controller binary as
+  // the host version.
+  HMODULE binary = base::GetModuleFromAddress(
+      reinterpret_cast<void*>(&ReadConfig));
+  scoped_ptr<FileVersionInfo> version_info(
+      FileVersionInfo::CreateFileVersionInfoForModule(binary));
+
+  string16 version;
+  if (version_info.get()) {
+    version = version_info->product_version();
+  }
+
+  *version_out = ::SysAllocString(version.c_str());
+  if (version_out == NULL) {
+    return E_OUTOFMEMORY;
+  }
+
+  return S_OK;
 }
 
 STDMETHODIMP ElevatedControllerWin::SetConfig(BSTR config) {
