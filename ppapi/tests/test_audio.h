d7cd53ab5cc91d485b2b8cf602642cac3777496b
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 23:23:48 +0000

    Added audio callback tests. This also puts in place a check and only runs the audio tests if audio devices are available on the machine. This means that were failing before on some platforms should no longer fail (they just won't be run).
    BUG=none
    TEST=Ran tests on various platforms. Ran trybots and checked that if tests ran, they succeeded.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10182008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137299 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_audio.h
+++ ppapi/tests/test_audio.h
@@ -15,7 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class TestAudio : public TestCase {
  public:
-  explicit TestAudio(TestingInstance* instance) : TestCase(instance) {}
+  explicit TestAudio(TestingInstance* instance);
+  ~TestAudio();
 
   // TestCase implementation.
   virtual bool Init();
@@ -25,7 +26,35 @@ class TestAudio : public TestCase {
   std::string TestCreation();
   std::string TestDestroyNoStop();
   std::string TestFailures();
+  std::string TestAudioCallback1();
+  std::string TestAudioCallback2();
 
+  // Calls |audio_callback_method_| (where |user_data| is "this").
+  static void AudioCallbackTrampoline(void* sample_buffer,
+                                      uint32_t buffer_size_in_bytes,
+                                      void* user_data);
+
+  typedef void (TestAudio::*AudioCallbackMethod)(void* sample_buffer,
+                                                 uint32_t buffer_size_in_bytes);
+
+  // Method called by |AudioCallbackTrampoline()|. Set only when the callback
+  // can't be running (before |StartPlayback()|, after |StopPlayback()| or
+  // releasing the last reference to the audio resource).
+  AudioCallbackMethod audio_callback_method_;
+
+  // An |AudioCallbackMethod| that just clears |sample_buffer|.
+  void AudioCallbackTrivial(void* sample_buffer, uint32_t buffer_size_in_bytes);
+
+  // |AudioCallbackMethod| used by |TestAudioCallbackN()|.
+  void AudioCallbackTest(void* sample_buffer, uint32_t buffer_size_in_bytes);
+
+  // Used by |TestAudioCallbackN()|.
+  PP_CompletionCallback test_callback_;
+
+  bool test_done_;
+  bool callback_fired_;
+
+  // Raw C-level interfaces, set in |Init()|; do not modify them elsewhere.
   const PPB_Audio* audio_interface_;
   const PPB_AudioConfig* audio_config_interface_;
   const PPB_Core* core_interface_;
