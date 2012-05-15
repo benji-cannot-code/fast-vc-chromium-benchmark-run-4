1d534083e160dbf611fa629877464529a772d8b2
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 18:00:10 +0000

    Coverity: Fix pass by value error in SpeechRecognitionHypothesis constructor.
    CID=103455
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10386130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137171 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/speech_recognition_result.h
+++ content/public/common/speech_recognition_result.h
@@ -21,7 +21,7 @@ struct SpeechRecognitionHypothesis {
 
   SpeechRecognitionHypothesis() : confidence(0.0) {}
 
-  SpeechRecognitionHypothesis(const string16 utterance_value,
+  SpeechRecognitionHypothesis(const string16& utterance_value,
                               double confidence_value)
       : utterance(utterance_value),
         confidence(confidence_value) {
