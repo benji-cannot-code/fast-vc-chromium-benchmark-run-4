fb768fb8059062ebcb1a26772bb49baffc179f92
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-15 17:41:34 +0000

    Factor HitTestPoint out of HitTestResult. https://bugs.webkit.org/show_bug.cgi?id=85965
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-05-15
    Reviewed by Simon Fraser.
    
    In preparation for handling transformations on area-based hit-testing,
    we need to factor test-point and test-area out of HitTestResult so that
    it can be transformed independently.
    
    To maintain the current API as closely as possible HitTestResult now
    inherit from HitTestPoint, representing the original untransformed
    hitTestPoint.
    
    No change in functionality. No new tests.
    
    * rendering/HitTestResult.cpp:
    (WebCore::HitTestPoint::HitTestPoint):
    (WebCore::HitTestPoint::~HitTestPoint):
    (WebCore::HitTestPoint::operator=):
    (WebCore::HitTestPoint::rectForPoint):
    (WebCore::HitTestResult::HitTestResult):
    (WebCore::HitTestResult::operator=):
    (WebCore::HitTestResult::isSelected):
    (WebCore::HitTestResult::spellingToolTip):
    (WebCore::HitTestResult::replacedString):
    * rendering/HitTestResult.h:
    (HitTestPoint):
    (WebCore::HitTestPoint::point):
    (WebCore::HitTestPoint::roundedPoint):
    (WebCore::HitTestPoint::setPoint):
    (WebCore::HitTestPoint::isRectBasedTest):
    (WebCore::HitTestPoint::topPadding):
    (WebCore::HitTestPoint::rightPadding):
    (WebCore::HitTestPoint::bottomPadding):
    (WebCore::HitTestPoint::leftPadding):
    (HitTestResult):
    (WebCore::HitTestResult::hitTestPoint):
    (WebCore::HitTestPoint::rectForPoint):
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::hitTestLayer):
    (WebCore::RenderLayer::hitTestList):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -3574,7 +3574,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
     // Next we want to see if the mouse pos is inside the child RenderObjects of the layer.
     if (fgRect.intersects(hitTestArea) && isSelfPaintingLayer()) {
         // Hit test with a temporary HitTestResult, because we only want to commit to 'result' if we know we're frontmost.
-        HitTestResult tempResult(result.point(), result.topPadding(), result.rightPadding(), result.bottomPadding(), result.leftPadding(), result.shadowContentFilterPolicy());
+        HitTestResult tempResult(result.hitTestPoint(), result.shadowContentFilterPolicy());
         if (hitTestContents(request, tempResult, layerBounds, hitTestPoint, HitTestDescendants) &&
             isHitCandidate(this, false, zOffsetForContentsPtr, unflattenedTransformState.get())) {
             if (result.isRectBasedTest())
@@ -3603,7 +3603,7 @@ RenderLayer* RenderLayer::hitTestLayer(RenderLayer* rootLayer, RenderLayer* cont
         return candidateLayer;
 
     if (bgRect.intersects(hitTestArea) && isSelfPaintingLayer()) {
-        HitTestResult tempResult(result.point(), result.topPadding(), result.rightPadding(), result.bottomPadding(), result.leftPadding(), result.shadowContentFilterPolicy());
+        HitTestResult tempResult(result.hitTestPoint(), result.shadowContentFilterPolicy());
         if (hitTestContents(request, tempResult, layerBounds, hitTestPoint, HitTestSelf) &&
             isHitCandidate(this, false, zOffsetForContentsPtr, unflattenedTransformState.get())) {
             if (result.isRectBasedTest())
@@ -3659,7 +3659,7 @@ RenderLayer* RenderLayer::hitTestList(Vector<RenderLayer*>* list, RenderLayer* r
     for (int i = list->size() - 1; i >= 0; --i) {
         RenderLayer* childLayer = list->at(i);
         RenderLayer* hitLayer = 0;
-        HitTestResult tempResult(result.point(), result.topPadding(), result.rightPadding(), result.bottomPadding(), result.leftPadding(), result.shadowContentFilterPolicy());
+        HitTestResult tempResult(result.hitTestPoint(), result.shadowContentFilterPolicy());
         if (childLayer->isPaginated())
             hitLayer = hitTestPaginatedChildLayer(childLayer, rootLayer, request, tempResult, hitTestRect, hitTestPoint, transformState, zOffsetForDescendants);
         else
