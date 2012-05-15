0f429cb180e6e194e4fba25b978403e4138140ee
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 15:27:06 +0000

    Add flag to specify if explicitly requested download is from web.
    This allows interposing the DownloadResourceThrottle on all web 
    downloads, not just those occuring because of navigations.
    
    BUG=127522
    R=darin@chromium.org
    TEST=Retry example in referenced issue; look for throttling message.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10381122
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137137 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/buffered_resource_handler.cc
+++ content/browser/renderer_host/buffered_resource_handler.cc
@@ -301,6 +301,7 @@ bool BufferedResourceHandler::CompleteResponseStarted(int request_id) {
             info->GetChildID(),
             info->GetRouteID(),
             info->GetRequestID(),
+            true,  // is_content_initiated
             DownloadSaveInfo(),
             DownloadResourceHandler::OnStartedCallback()));
 
--- content/browser/renderer_host/resource_dispatcher_host_impl.cc
+++ content/browser/renderer_host/resource_dispatcher_host_impl.cc
@@ -466,6 +466,7 @@ void ResourceDispatcherHostImpl::CancelRequestsForContext(
 
 net::Error ResourceDispatcherHostImpl::BeginDownload(
     scoped_ptr<net::URLRequest> request,
+    bool is_content_initiated,
     ResourceContext* context,
     int child_id,
     int route_id,
@@ -514,7 +515,8 @@ net::Error ResourceDispatcherHostImpl::BeginDownload(
   // |started_callback|.
   scoped_refptr<ResourceHandler> handler(
       CreateResourceHandlerForDownload(request.get(), context, child_id,
-                                       route_id, request_id_, save_info,
+                                       route_id, request_id_,
+                                       is_content_initiated, save_info,
                                        started_callback));
 
   if (!request_context->job_factory()->IsHandledURL(url)) {
@@ -576,6 +578,7 @@ ResourceDispatcherHostImpl::CreateResourceHandlerForDownload(
     int child_id,
     int route_id,
     int request_id,
+    bool is_content_initiated,
     const DownloadSaveInfo& save_info,
     const DownloadResourceHandler::OnStartedCallback& started_cb) {
   scoped_refptr<ResourceHandler> handler(
@@ -585,7 +588,7 @@ ResourceDispatcherHostImpl::CreateResourceHandlerForDownload(
   if (delegate_) {
     ScopedVector<ResourceThrottle> throttles;
     delegate_->DownloadStarting(request, context, child_id, route_id,
-                                request_id, !request->is_pending(), &throttles);
+                                request_id, is_content_initiated, &throttles);
     if (!throttles.empty()) {
       handler = new ThrottlingResourceHandler(this, handler, child_id,
                                               request_id, throttles.Pass());
