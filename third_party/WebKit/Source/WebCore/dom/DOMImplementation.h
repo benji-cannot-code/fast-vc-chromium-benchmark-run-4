5ae9eb9c05e9465c398e5ae214d11ece6ecd3aab
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-15 20:54:18 +0000

    Site-specific hack: Disclaim WebM as a supported type on Mac for YouTube. https://bugs.webkit.org/show_bug.cgi?id=86409
    Reviewed by Darin Adler.
    
    No new tests; site specific hack.
    
    Add a Mac-only site-specific hack which disclaims both video/webm and video/x-flv
    as supported types when the media element's document has a host of youtube.com.
    
    Add a new, pure-virtual prototype class for use by MediaPlayer::supportsType:
    * platform/graphics/MediaPlayer.h:
    (MediaPlayerSupportsTypeClient):
    (WebCore::MediaPlayerSupportsTypeClient::~MediaPlayerSupportsTypeClient):
    (WebCore::MediaPlayerSupportsTypeClient::mediaPlayerNeedsSiteSpecificHacks):
    (WebCore::MediaPlayerSupportsTypeClient::mediaPlayerDocumentHost):
    
    Use these new client calls to determine whether to apply the site-specific
    hack:
    * platform/graphics/MediaPlayer.cpp:
    (WebCore::MediaPlayer::supportsType):
    
    Add this prototype class as a superclass of HTMLMediaElement.  Pass in the
    HTMLMediaElement's this pointer when calling MediaPlayer::supportsType():
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::canPlayType):
    (WebCore::HTMLMediaElement::selectNextSourceChild):
    (WebCore::HTMLMediaElement::mediaPlayerNeedsSiteSpecificHacks):
    (WebCore::HTMLMediaElement::mediaPlayerDocumentHost):
    * html/HTMLMediaElement.h:
    
    As is HTMLMediaElement, a MediaPlayerSupportsTypeClient class is needed. Add a
    new class DOMImplementationSupportsTypeClient, an instance of which will be
    passed to MediaPlayer::supportsType():
    * dom/DOMImplementation.cpp:
    (DOMImplementationSupportsTypeClient):
    (WebCore::DOMImplementationSupportsTypeClient::DOMImplementationSupportsTypeClient):
    (WebCore::DOMImplementation::createDocument):
    (WebCore::DOMImplementation::mediaPlayerNeedsSiteSpecificHacks):
    (WebCore::DOMImplementation::mediaPlayerDocumentHost):
    * dom/DOMImplementation.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117147 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DOMImplementation.h
+++ third_party/WebKit/Source/WebCore/dom/DOMImplementation.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DOMImplementation_h
 
 #include "Document.h"
+#include "MediaPlayer.h"
 #include <wtf/Forward.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
