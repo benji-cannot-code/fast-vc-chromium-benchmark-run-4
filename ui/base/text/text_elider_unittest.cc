d65f385351fa8b80283b54e34084579b2902a647
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 02:43:10 +0000

    Modify ui::ElideRectangleText() to honor trailing newlines.
    Trailing whitespace without a \n character is still trimmed away.
    Adds additional tests for this.
    
    Also, makes the code use ContainsOnlyWhitespace() from base
    and makes the implementation of ContainsOnlyWhitespace()
    shorter (based on the code that was in text_elider.cc.)
    
    BUG=126297
    TEST=New tests in text_elider_unittest.cc
    
    Review URL: https://chromiumcodereview.appspot.com/10386016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137061 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/text/text_elider_unittest.cc
+++ ui/base/text/text_elider_unittest.cc
@@ -531,6 +531,14 @@ TEST(TextEliderTest, ElideRectangleText) {
     { "\nTest", test_width, line_height, true, "" },
     { "\n\nTest", test_width, line_height * 3, false, "||Test" },
     { "\n\nTest", test_width, line_height * 2, true, "|" },
+    { "Test\n", 2 * test_width, line_height * 5, false, "Test|" },
+    { "Test\n\n", 2 * test_width, line_height * 5, false, "Test||" },
+    { "Test\n\n\n", 2 * test_width, line_height * 5, false, "Test|||" },
+    { "Test\nTest\n\n", 2 * test_width, line_height * 5, false, "Test|Test||" },
+    { "Test\n\nTest\n", 2 * test_width, line_height * 5, false, "Test||Test|" },
+    { "Test\n\n\nTest", 2 * test_width, line_height * 5, false, "Test|||Test" },
+    { "Te ", test_width, line_height, false, "Te" },
+    { "Te  Te Test", test_width, 3 * line_height, false, "Te|Te|Test" },
   };
 
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(cases); ++i) {
@@ -542,10 +550,12 @@ TEST(TextEliderTest, ElideRectangleText) {
                                  cases[i].available_pixel_height,
                                  TRUNCATE_LONG_WORDS,
                                  &lines));
-    if (cases[i].output)
-      EXPECT_EQ(cases[i].output, UTF16ToUTF8(JoinString(lines, '|')));
-    else
-      EXPECT_TRUE(lines.empty());
+    if (cases[i].output) {
+      const std::string result = UTF16ToUTF8(JoinString(lines, '|'));
+      EXPECT_EQ(cases[i].output, result) << "Case " << i << " failed!";
+    } else {
+      EXPECT_TRUE(lines.empty()) << "Case " << i << " failed!";
+    }
   }
 }
 
@@ -580,10 +590,12 @@ TEST(TextEliderTest, ElideRectangleTextPunctuation) {
                                  cases[i].available_pixel_height,
                                  wrap_behavior,
                                  &lines));
-    if (cases[i].output)
-      EXPECT_EQ(cases[i].output, UTF16ToUTF8(JoinString(lines, '|')));
-    else
-      EXPECT_TRUE(lines.empty());
+    if (cases[i].output) {
+      const std::string result = UTF16ToUTF8(JoinString(lines, '|'));
+      EXPECT_EQ(cases[i].output, result) << "Case " << i << " failed!";
+    } else {
+      EXPECT_TRUE(lines.empty()) << "Case " << i << " failed!";
+    }
   }
 }
 
@@ -639,7 +651,8 @@ TEST(TextEliderTest, ElideRectangleTextLongWords) {
                        &lines);
     std::string expected_output(cases[i].output);
     ReplaceSubstringsAfterOffset(&expected_output, 0, "...", kEllipsis);
-    EXPECT_EQ(expected_output, UTF16ToUTF8(JoinString(lines, '|')));
+    const std::string result = UTF16ToUTF8(JoinString(lines, '|'));
+    EXPECT_EQ(expected_output, result) << "Case " << i << " failed!";
   }
 }
 
