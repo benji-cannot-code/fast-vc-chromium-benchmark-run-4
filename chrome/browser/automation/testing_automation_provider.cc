8ca4da994602246f4620f3edfea2d4827dcdc359
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-15 05:13:18 +0000

    [chromeos] Fix Logout automation when no browser windows are present.
    Logout automation used to apply the IDC_EXIT accelerator to the first
    browser window. This would not work in the case of aura if no browser
    windows are present.
    
    Fix automation to use ash system tray calls.
    
    BUG=chromium:127859
    TEST=new pyauto test: chromeos_login.ChromeosLogin.testLogoutWithNoWindows
    
    R=sadrul@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10388107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137078 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -2298,6 +2298,7 @@ void TestingAutomationProvider::SendJSONRequest(int handle,
   handler_map["LoginAsGuest"] = &TestingAutomationProvider::LoginAsGuest;
   handler_map["AddLoginEventObserver"] =
       &TestingAutomationProvider::AddLoginEventObserver;
+  handler_map["SignOut"] = &TestingAutomationProvider::SignOut;
 
   handler_map["LockScreen"] = &TestingAutomationProvider::LockScreen;
   handler_map["UnlockScreen"] = &TestingAutomationProvider::UnlockScreen;
--- chrome/browser/automation/testing_automation_provider.h
+++ chrome/browser/automation/testing_automation_provider.h
@@ -1441,7 +1441,7 @@ class TestingAutomationProvider : public AutomationProvider,
                        IPC::Message* reply_message);
 
 #if defined(OS_CHROMEOS)
-  // Login.
+  // Login / Logout.
   void GetLoginInfo(base::DictionaryValue* args, IPC::Message* reply_message);
 
   void ShowCreateAccountUI(base::DictionaryValue* args,
@@ -1463,6 +1463,8 @@ class TestingAutomationProvider : public AutomationProvider,
   //   output: { "result": "My Window Name" }
   void ExecuteJavascriptInOOBEWebUI(
       base::DictionaryValue* args, IPC::Message* reply_message);
+
+  void SignOut(base::DictionaryValue* args, IPC::Message* reply_message);
 
   // Screen locker.
   void LockScreen(base::DictionaryValue* args, IPC::Message* reply_message);
