6bebf795fca0817e6f5be7166a095008de4152ce
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 16:00:54 +0000

    [EFL] Memory leak in RenderThemeEfl https://bugs.webkit.org/show_bug.cgi?id=86609
    Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-05-16
    Reviewed by Martin Robinson.
    
    Fix a memory leak by freeing the cairo surface with
    cairo_surface_destroy.
    
    * platform/efl/RenderThemeEfl.cpp:
    (WebCore::RenderThemeEfl::cacheThemePartFlush):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.cpp
@@ -277,7 +277,7 @@ void RenderThemeEfl::cacheThemePartFlush()
     end = m_partCache.end();
     for (; itr != end; itr++) {
         struct ThemePartCacheEntry *entry = *itr;
-        cairo_surface_finish(entry->surface);
+        cairo_surface_destroy(entry->surface);
         evas_object_del(entry->o);
         ecore_evas_free(entry->ee);
         delete entry;
