6ece178462db1178067238cd2ed1500ea8c6d456
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 20:16:45 +0000

    gdata: Fix a bug that caused google drive not to load
    There were two subtle changes in how we stored the file system metadata
    in a proto file, and each change broke compatibility in a subtle way.
    We should discard the proto file if old formats are detected.
    
    Note that the protobuf definitions in gdata.proto were kept compatible.
    The problem was in how we stored data in the protobuf, and the changes were
    subtle.
    
    BUG=128133
    TEST=confirmed that the google drive loaded with the stale GCache directory from josh
    
    Review URL: https://chromiumcodereview.appspot.com/10384204
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_files_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_files_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <utility>
 #include <vector>
+#include "chrome/browser/chromeos/gdata/gdata.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace gdata {
@@ -47,6 +48,39 @@ TEST(GDataRootDirectoryTest, RemoveTemporaryFilesFromCacheMap) {
   ASSERT_TRUE(root->GetCacheEntry("<resource_id_3>", "") != NULL);
   ASSERT_TRUE(root->GetCacheEntry("<resource_id_4>", "") == NULL);
 
+}
+
+TEST(GDataRootDirectoryTest, ParseFromString_DetectBadTitle) {
+  GDataRootDirectoryProto proto;
+  GDataEntryProto* mutable_entry =
+      proto.mutable_gdata_directory()->mutable_gdata_entry();
+  mutable_entry->mutable_file_info()->set_is_directory(true);
+
+  std::string serialized_proto;
+  ASSERT_TRUE(proto.SerializeToString(&serialized_proto));
+
+  GDataRootDirectory root;
+  // This should fail as the title is empty.
+  // root.title() should be unchanged.
+  ASSERT_FALSE(root.ParseFromString(serialized_proto));
+  ASSERT_EQ("drive", root.title());
+
+  // Setting the title to "gdata".
+  mutable_entry->set_title("gdata");
+  ASSERT_TRUE(proto.SerializeToString(&serialized_proto));
+
+  // This should fail as the title is not "drive".
+  // root.title() should be unchanged.
+  ASSERT_FALSE(root.ParseFromString(serialized_proto));
+  ASSERT_EQ("drive", root.title());
+
+  // Setting the title to "drive".
+  mutable_entry->set_title("drive");
+  ASSERT_TRUE(proto.SerializeToString(&serialized_proto));
+
+  // This should succeed as the title is "drive".
+  ASSERT_TRUE(root.ParseFromString(serialized_proto));
+  ASSERT_EQ("drive", root.title());
 }
 
 }  // namespace gdata
