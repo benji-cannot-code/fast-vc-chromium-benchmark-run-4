b0a31a37424ecbded92e93e9d862b231798879f4
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 22:58:46 +0000

    ash: Add a heads-up display to track touch-point states and positions.
    The command-line flag to turn on the HUD is --ash-touch-hud. It can also be
    turned on from about:flags
    
    BUG=none
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10386178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -1076,6 +1076,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ::switches::kFlingTapSuppressMaxGap,
       ::switches::kTouchDevices,
       ::switches::kTouchOptimizedUI,
+      ash::switches::kAshTouchHud,
       ash::switches::kAuraLegacyPowerButton,
       ash::switches::kAuraNoShadows,
       ash::switches::kAuraPanelManager,
