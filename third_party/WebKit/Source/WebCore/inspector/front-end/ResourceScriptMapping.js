03db2b93b40da2582728c566dfc9ef21f0313268
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 12:48:50 +0000

    Web Inspector: follow up to r117267 - fixing typo and removing unnecessary dispatch. https://bugs.webkit.org/show_bug.cgi?id=86613
    Reviewed by Vsevolod Vlasov.
    
    * inspector/front-end/ResourceScriptMapping.js:
    (WebInspector.ResourceScriptMapping.prototype._uiSourceCodeAdded):
    (WebInspector.ResourceScriptMapping.prototype._uiSourceCodeReplaced):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117270 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourceScriptMapping.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourceScriptMapping.js
@@ -145,10 +145,6 @@ WebInspector.ResourceScriptMapping.prototype = {
     _uiSourceCodeAdded: function(rawSourceCode, uiSourceCode)
     {
         this._rawSourceCodeForUISourceCode.put(uiSourceCode, rawSourceCode);
-
-        for (var i = 0; i < rawSourceCode._scripts.length; ++i)
-            rawSourceCode._scripts[i].setSourceMapping(this);
-
         this.dispatchEventToListeners(WebInspector.ScriptMapping.Events.UISourceCodeAdded, uiSourceCode);
     },
 
@@ -165,7 +161,7 @@ WebInspector.ResourceScriptMapping.prototype = {
         for (var i = 0; i < rawSourceCode._scripts.length; ++i)
             rawSourceCode._scripts[i].setSourceMapping(this);
 
-        var data = { oldUISourceCode: uiSourceCode, uiSourceCode: uiSourceCode };
+        var data = { oldUISourceCode: oldUISourceCode, uiSourceCode: uiSourceCode };
         this.dispatchEventToListeners(WebInspector.ScriptMapping.Events.UISourceCodeReplaced, data);
     },
 
