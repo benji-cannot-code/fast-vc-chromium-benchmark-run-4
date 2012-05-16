6c041d1a9eb83d76d4386b9138b5aec7506245b8
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 13:35:30 +0000

    Add managedModePrivate.onChange event
    BUG=120635
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10391011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137412 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_managed_mode_api.h
+++ chrome/browser/extensions/extension_managed_mode_api.h
@@ -11,6 +11,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include "chrome/browser/extensions/extension_function.h"
+#include "chrome/browser/prefs/pref_change_registrar.h"
+#include "content/public/browser/notification_observer.h"
+
+class Profile;
+
+class ExtensionManagedModeEventRouter : public content::NotificationObserver {
+ public:
+  explicit ExtensionManagedModeEventRouter(Profile* profile);
+  virtual ~ExtensionManagedModeEventRouter();
+
+  void Init();
+
+  // content::NotificationObserver implementation:
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
+
+ private:
+  PrefChangeRegistrar registrar_;
+  Profile* profile_;
+
+  DISALLOW_COPY_AND_ASSIGN(ExtensionManagedModeEventRouter);
+};
 
 class GetManagedModeFunction : public SyncExtensionFunction {
  public:
