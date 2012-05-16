2a6a3011d0b6e364e209864f9ca25f2e23563dc8
jsalz@chromium.org <jsalz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 03:52:07 +0000

    Add localhost check to IsRemote.
    BUG=None
    TEST=Installed on Alex, disabled network connectivity, and loaded a page from localhost
    
    Change-Id: I318fbf050f80f65618bed90924d8a1283f03fdf5
    
    
    Review URL: https://chromiumcodereview.appspot.com/10342007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137356 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/offline_resource_throttle.cc
+++ chrome/browser/renderer_host/offline_resource_throttle.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/resource_context.h"
 #include "content/public/browser/resource_throttle_controller.h"
 #include "net/base/net_errors.h"
+#include "net/base/net_util.h"
 #include "net/base/network_change_notifier.h"
 #include "net/url_request/url_request.h"
 #include "net/url_request/url_request_context.h"
@@ -120,9 +121,10 @@ void OfflineResourceThrottle::OnBlockingPageComplete(bool proceed) {
 }
 
 bool OfflineResourceThrottle::IsRemote(const GURL& url) const {
-  return url.SchemeIs(chrome::kFtpScheme) ||
-         url.SchemeIs(chrome::kHttpScheme) ||
-         url.SchemeIs(chrome::kHttpsScheme);
+  return !net::IsLocalhost(url.host()) &&
+    (url.SchemeIs(chrome::kFtpScheme) ||
+     url.SchemeIs(chrome::kHttpScheme) ||
+     url.SchemeIs(chrome::kHttpsScheme));
 }
 
 bool OfflineResourceThrottle::ShouldShowOfflinePage(const GURL& url) const {
