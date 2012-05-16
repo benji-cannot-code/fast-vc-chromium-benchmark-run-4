584c161308e4b05035845509a13d39dcd6e7d390
sonnyrao@chromium.org <sonnyrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 13:35:49 +0000

    Add a autotest dependency for Page Cycler data
    This creates a new dependency which will be used by ChromeOS
    to package up the latest set of Page Cycler data from the
    Chrome source tree.  The Chrome ebuild does the heavy lifting
    of getting this packaged up, but this provides the script
    which will get called on the target that puts a symlink in
    the correct place to the pc data files.
    
    BUG=chromium-os:23990
    TEST=none
    
    R=nirnimesh@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10381167
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137413 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/chromeos/autotest/files/client/deps/page_cycler_dep/page_cycler_dep.py
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Install the page_cycler_dep dependency.
+"""
+
+import os
+from autotest_lib.client.bin import utils
+
+version = 1
+
+def setup(top_dir):
+    return
+
+
+pwd = os.getcwd()
+utils.update_version(pwd + '/src', False, version, setup, pwd)
