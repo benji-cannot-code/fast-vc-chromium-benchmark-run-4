a043c8118e3bc13496a61c182594bd0526b9f416
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 03:35:12 +0000

    remove chrome_shared.css from task manager
    BUG=103688
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10332158
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/task_manager/includes.js
+++ chrome/browser/resources/task_manager/includes.js
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // reference the resources.
 
 var css = [
-  'chrome_shared.css',
+  'chrome_shared2.css',
   'list.css',
   'table.css',
   'menu.css',
