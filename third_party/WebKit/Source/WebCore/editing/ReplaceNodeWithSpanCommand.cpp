ca673c5e0e53f3d56142ec22fa20939c12677e8a
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 19:31:40 +0000

    Avoid reparsing the style attribute when cloning elements. <http://webkit.org/b/86574>
    Reviewed by Antti Koivisto.
    
    Refactor cloning of attributes a bit to dodge the styleAttr reparse previously
    caused by ElementAttributeData::setAttributes().
    
    Introduced Element::cloneDataFromElement() which takes care of cloning the
    ElementAttributeData as well as "non-attribute properties" (which is currently
    specific to HTMLInputElement.)
    
    Also includes some additional dodging of attribute vector traversal to find
    old/new 'id' and 'name' attributes.
    
    I'm seeing a ~10% improvement on PerformanceTests/DOM/CloneNodes locally.
    
    * dom/Document.cpp:
    (WebCore::Document::importNode):
    * dom/Element.cpp:
    (WebCore::Element::cloneElementWithoutChildren):
    (WebCore::Element::cloneAttributesFromElement):
    (WebCore::Element::cloneDataFromElement):
    * dom/Element.h:
    (WebCore::Element::copyNonAttributePropertiesFromElement):
    * dom/ElementAttributeData.cpp:
    (WebCore::ElementAttributeData::cloneDataFrom):
    * dom/ElementAttributeData.h:
    (ElementAttributeData):
    * dom/Node.h:
    * dom/StyledElement.cpp:
    (WebCore::StyledElement::styleAttributeChanged):
    (WebCore::StyledElement::parseAttribute):
    * dom/StyledElement.h:
    * editing/ReplaceNodeWithSpanCommand.cpp:
    (WebCore::swapInNodePreservingAttributesAndChildren):
    * html/HTMLElement.cpp:
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::copyNonAttributePropertiesFromElement):
    * html/HTMLInputElement.h:
    * inspector/DOMPatchSupport.cpp:
    (WebCore::DOMPatchSupport::innerPatchNode):
    * inspector/InspectorDOMAgent.cpp:
    (WebCore::InspectorDOMAgent::setNodeName):
    * svg/SVGUseElement.cpp:
    (WebCore::SVGUseElement::expandSymbolElementsInShadowTree):
    (WebCore::SVGUseElement::transferUseAttributesToReplacedElement):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/ReplaceNodeWithSpanCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/ReplaceNodeWithSpanCommand.cpp
@@ -65,7 +65,7 @@ static void swapInNodePreservingAttributesAndChildren(HTMLElement* newNode, HTML
     }
 
     // FIXME: Fix this to send the proper MutationRecords when MutationObservers are present.
-    newNode->setAttributesFromElement(*nodeToReplace);
+    newNode->cloneDataFromElement(*nodeToReplace);
 
     parentNode->removeChild(nodeToReplace, ec);
     ASSERT(!ec);
