a665a4c44dada6e6475b6bd17e61e3c7a14dd415
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 00:21:00 +0000

    Make error information available even if all we have is line number information. https://bugs.webkit.org/show_bug.cgi?id=86547
    Reviewed by Filip Pizlo.
    
    We don't need expression information to generate useful line, file, and stack information,
    so only require that we have line number info available.
    
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::throwException):
    * runtime/Executable.h:
    (JSC):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
@@ -44,8 +44,6 @@ namespace JSC {
     class LLIntOffsetsExtractor;
     class ProgramCodeBlock;
     class ScopeChainNode;
-
-    struct ExceptionInfo;
     
     enum CompilationKind { FirstCompilation, OptimizingCompilation };
 
