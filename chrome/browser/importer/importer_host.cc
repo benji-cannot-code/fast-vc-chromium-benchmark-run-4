11d1e1e983ad3c32f9c24ada02f34c4714761bc5
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 19:18:19 +0000

    importer: Make use of ShowMessageBox() function in ImporterHost.
    This makes ImporterHost use the cross platform function instead of using the one
    that is Windows specific.
    
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10391097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/importer_host.cc
+++ chrome/browser/importer/importer_host.cc
@@ -22,17 +22,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/simple_message_box.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_source.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
-
-#if defined(OS_WIN)
-// TODO(port): Port this file.
-#include "ui/base/win/message_box_win.h"
-#endif
 
 using content::BrowserThread;
 
@@ -162,12 +158,9 @@ void ImporterHost::StartImportSettings(
 void ImporterHost::OnGoogleGAIACookieChecked(bool result) {
 #if defined(OS_WIN)
   if (!result) {
-    ui::MessageBox(
-        NULL,
-        UTF16ToWide(l10n_util::GetStringUTF16(
-            IDS_IMPORTER_GOOGLE_LOGIN_TEXT)).c_str(),
-        L"",
-        MB_OK | MB_TOPMOST);
+    browser::ShowMessageBox(NULL,
+        l10n_util::GetStringUTF16(IDS_IMPORTER_GOOGLE_LOGIN_TEXT), string16(),
+        browser::MESSAGE_BOX_TYPE_INFORMATION);
 
     GURL url("https://accounts.google.com/ServiceLogin");
     DCHECK(profile_);
