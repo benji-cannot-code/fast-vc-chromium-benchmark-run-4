d3d6fb4584758816f2609c069d71e066f6866cad
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 14:22:30 +0000

    [Chromium] Web Inspector: debugger browser tests are broken https://bugs.webkit.org/show_bug.cgi?id=86624
    Reviewed by Vsevolod Vlasov.
    
    Use WebInspector.panels.scripts._scriptMapping instead of
    WebInspector.panels.scripts._presentationModel as the latter
    has been deleted.
    
    * src/js/Tests.js:
    (.TestSuite.prototype.nonAnonymousUISourceCodes_):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/js/Tests.js
+++ third_party/WebKit/Source/WebKit/chromium/src/js/Tests.js
@@ -650,7 +650,7 @@ TestSuite.prototype.nonAnonymousUISourceCodes_ = function()
         return !!uiSourceCode.url;
     }
 
-    var uiSourceCodes = WebInspector.panels.scripts._presentationModel.uiSourceCodes();
+    var uiSourceCodes = WebInspector.panels.scripts._scriptMapping.uiSourceCodes();
     return uiSourceCodes.filter(filterOutAnonymous);
 };
 
