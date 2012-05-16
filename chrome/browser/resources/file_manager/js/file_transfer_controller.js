60a8076b1aaf97b79dae187c45a0203f087e4ef9
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 22:14:10 +0000

    Add gdata content search to file_manager
    Search results will be shown under virtual path gdata/.search/<query>.
    Content search will be triggered whenever we try to enumerate directory with
    the mentioned file path format.
    Entries returned by search results will have file names formatted as
    <resource_id>.<real_file_name>, so we have to strip resource_id part before
    displaying name or selecting target path for copy/move operations.
    
    When user enters some input x in search box(on gdata only),
     file manager will change directory to /gdata/.search/x in
    which search results will be displayed
    
    TEST = manual
    BUG=chromium-os:27539
    
    Review URL: https://chromiumcodereview.appspot.com/10342010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137536 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_transfer_controller.js
+++ chrome/browser/resources/file_manager/js/file_transfer_controller.js
@@ -429,6 +429,8 @@ FileTransferController.prototype = {
     if (this.directoryModel_.isPathReadOnly(destinationPath)) {
       return false;
     }
+    if (this.directoryModel_.isSearching())
+      return false;
 
     if (!dataTransfer.types || dataTransfer.types.indexOf('fs/tag') == -1)
       return false;  // Unsupported type of content.
@@ -510,7 +512,7 @@ FileTransferController.prototype = {
   },
 
   get currentDirectory() {
-    return this.directoryModel_.getCurrentDirEntry();
+    return this.directoryModel_.getSearchOrCurrentDirEntry();
   },
 
   get readonly() {
