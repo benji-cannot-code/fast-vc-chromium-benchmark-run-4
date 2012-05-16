69c07db86f70dc4ede02a7a6d229e427a0849fd8
gene@chromium.org <gene@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 23:07:30 +0000

    Adding functionality to specify TLS encryption CUPS print servers for cloudprint connector.
    BUG=127858
    TEST=add 'cups_encryption' filed to 'Service State' file in print settings section. Value should be 0..4 (see http_encryption_t for possible values).
    
    Review URL: https://chromiumcodereview.appspot.com/10399050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137550 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/cups_helper.cc
+++ printing/backend/cups_helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,7 +15,8 @@ static const int kDefaultIPPServerPort = 631;
 
 // Helper wrapper around http_t structure, with connection and cleanup
 // functionality.
-HttpConnectionCUPS::HttpConnectionCUPS(const GURL& print_server_url)
+HttpConnectionCUPS::HttpConnectionCUPS(const GURL& print_server_url,
+                                       http_encryption_t encryption)
     : http_(NULL) {
   // If we have an empty url, use default print server.
   if (print_server_url.is_empty())
@@ -26,7 +27,7 @@ HttpConnectionCUPS::HttpConnectionCUPS(const GURL& print_server_url)
     port = kDefaultIPPServerPort;
 
   http_ = httpConnectEncrypt(print_server_url.host().c_str(), port,
-                             HTTP_ENCRYPT_NEVER);
+                             encryption);
   if (http_ == NULL) {
     LOG(ERROR) << "CP_CUPS: Failed connecting to print server: " <<
                print_server_url;
