ae936eb067b975839921f4733320189e316abb5d
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-16 12:35:46 +0000

    Move declarative API into events API
    The motivation of this CL is to bring the declarative API and the rest of the
    events mechanism closer together in the API documentation. To achive this, we
    move the content of declarative.json and everything related to events from
    extension.json into the events.json API definition. The new "events" namespace
    is automatically provided to all extensions.
    
    This is a second attempt after
    http://src.chromium.org/viewvc/chrome?view=rev&revision=137388 failed in
    ExecuteScriptApiTest.ExecuteScriptInFrame. TBR'ing as this is a tiny change from the previous CL.
    
    BUG=126497,112155
    TEST=no
    TBR=aa@chromium.org, kalman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10392127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/declarative_webrequest_custom_bindings.js
+++ chrome/renderer/resources/extensions/declarative_webrequest_custom_bindings.js
@@ -6,16 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Custom bindings for the declarativeWebRequest API.
 
 var chromeHidden = requireNative('chrome_hidden').GetChromeHidden();
+var utils = require('utils');
 
 chromeHidden.registerCustomHook('declarativeWebRequest', function(api) {
   // Returns the schema definition of type |typeId| defined in |namespace|.
   function getSchema(namespace, typeId) {
-    var filterNamespace = function(val) {return val.namespace === namespace;};
-    var apiSchema = api.apiDefinitions.filter(filterNamespace)[0];
-    var filterTypeId = function (val) {
-      return val.id === namespace + "." + typeId;
-    };
-    var resultSchema = apiSchema.types.filter(filterTypeId)[0];
+    var apiSchema = utils.lookup(api.apiDefinitions, 'namespace', namespace);
+    var resultSchema = utils.lookup(
+        apiSchema.types, 'id', namespace + '.' + typeId);
     return resultSchema;
   }
 
