ebc28faef9b677ffa80ba9df756c2ae7adff20e1
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 23:35:15 +0000

    [chromium] Clear the m_private pointer when destroying WebFilterOperations to avoid assert in WebPrivateOwnPtr https://bugs.webkit.org/show_bug.cgi?id=86654
    Reviewed by James Robinson.
    
    Source/Platform:
    
    * chromium/public/WebFilterOperations.h:
    (WebKit::WebFilterOperations::~WebFilterOperations):
    (WebFilterOperations):
    
    Source/WebCore:
    
    * platform/chromium/support/WebFilterOperations.cpp:
    (WebKit::WebFilterOperations::reset):
    (WebKit::WebFilterOperations::destroy):
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/support/WebFilterOperations.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/support/WebFilterOperations.cpp
@@ -36,9 +36,14 @@ using namespace WebCore;
 
 namespace WebKit {
 
-void WebFilterOperations::initialize()
+void WebFilterOperations::reset()
 {
     m_private.reset(new FilterOperations());
+}
+
+void WebFilterOperations::destroy()
+{
+    m_private.reset(0);
 }
 
 void WebFilterOperations::append(const WebFilterOperation& filter)
@@ -65,11 +70,6 @@ void WebFilterOperations::append(const WebFilterOperation& filter)
         break;
     }
     }
-}
-
-void WebFilterOperations::clear()
-{
-    m_private->operations().clear();
 }
 
 const FilterOperations& WebFilterOperations::toFilterOperations() const
