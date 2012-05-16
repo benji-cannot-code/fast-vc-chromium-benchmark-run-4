e6926deb61ab27babf57ba862c98e114f93f7960
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-16 11:43:14 +0000

    [BlackBerry] Cookies should be checked during parsing to improve performance. https://bugs.webkit.org/show_bug.cgi?id=85028
    Patch by Jason Liu <jason.liu@torchmobile.com.cn> on 2012-05-16
    Reviewed by George Staikos.
    
    We shouldn't waste time and memery on invalid cookies. It is better to drop them during parsing.
    We shouldn't check the default domain since it is set with host. So we only check domains which are parsed
    from response headers.
    
    No new tests. No functionality change.
    
    * platform/blackberry/CookieManager.cpp:
    (WebCore::CookieManager::setCookies):
    * platform/blackberry/CookieManager.h:
    * platform/blackberry/CookieParser.cpp:
    (WebCore::CookieParser::parseOneCookie):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/blackberry/CookieManager.h
+++ third_party/WebKit/Source/WebCore/platform/blackberry/CookieManager.h
@@ -120,8 +120,6 @@ private:
 
     void checkAndTreatCookie(ParsedCookie*, BackingStoreRemovalPolicy);
 
-    bool shouldRejectForSecurityReason(const ParsedCookie*, const KURL&);
-
     void addCookieToMap(CookieMap* targetMap, ParsedCookie* candidateCookie, BackingStoreRemovalPolicy postToBackingStore);
 
     CookieMap* findOrCreateCookieMap(CookieMap* protocolMap, const String& domain, bool findOnly);
