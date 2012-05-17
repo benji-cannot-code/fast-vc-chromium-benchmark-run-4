ddb6898b43314cc7c89f55389639aa98a3b34d85
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-17 18:09:37 +0000

    [EFL] Add API for Web Database handling https://bugs.webkit.org/show_bug.cgi?id=85178
    Patch by Thiago Marcos P. Santos <thiago.santos@intel.com> on 2012-05-17
    Reviewed by Antonio Gomes.
    
    Source/WebKit:
    
    Added the new Web Database API to the buildsystem.
    
    * PlatformEfl.cmake:
    
    Source/WebKit/efl:
    
    Add API for Web Database. This API will allow a browser
    to query information about size, name, filename and remove
    databases based on the origin. It will be also possible to
    list all databases for a given origin.
    
    * ewk/EWebKit.h:
    * ewk/ewk_security_origin.cpp:
    (ewk_security_origin_web_database_get_all):
    * ewk/ewk_security_origin.h:
    * ewk/ewk_settings.cpp:
    * ewk/ewk_settings.h:
    * ewk/ewk_web_database.cpp: Added.
    (_Ewk_Web_Database):
    (ewk_web_database_display_name_get):
    (ewk_web_database_expected_size_get):
    (ewk_web_database_filename_get):
    (ewk_web_database_name_get):
    (ewk_web_database_security_origin_get):
    (ewk_web_database_size_get):
    (ewk_web_database_remove):
    (ewk_web_database_remove_all):
    (ewk_web_database_free):
    (ewk_web_database_list_free):
    (ewk_web_database_new):
    * ewk/ewk_web_database.h: Added.
    * ewk/ewk_web_database_private.h: Copied from Source/WebKit/efl/ewk/EWebKit.h.
    (WebCore):
    
    Tools:
    
    Use the newly introduced Web Database API in EFL's DRT.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (DumpRenderTreeChrome::resetDefaultsToConsistentValues):
    * DumpRenderTree/efl/LayoutTestControllerEfl.cpp:
    (LayoutTestController::clearAllDatabases):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_security_origin.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_security_origin.cpp
@@ -25,8 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DatabaseTracker.h"
 #include "SecurityOrigin.h"
 #include "ewk_private.h"
+#include "ewk_security_origin_private.h"
+#include "ewk_web_database.h"
+#include "ewk_web_database_private.h"
 #include <Eina.h>
 #include <wtf/RefPtr.h>
+#include <wtf/UnusedParam.h>
 #include <wtf/text/CString.h>
 
 struct _Ewk_Security_Origin {
@@ -84,6 +88,25 @@ void ewk_security_origin_web_database_quota_set(const Ewk_Security_Origin* origi
 void ewk_security_origin_application_cache_quota_set(const Ewk_Security_Origin* origin, int64_t quota)
 {
     WebCore::cacheStorage().storeUpdatedQuotaForOrigin(origin->securityOrigin.get(), quota);
+}
+
+Eina_List* ewk_security_origin_web_database_get_all(const Ewk_Security_Origin* origin)
+{
+    Eina_List* databases = 0;
+#if ENABLE(SQL_DATABASE)
+    Vector<WTF::String> names;
+
+    if (!WebCore::DatabaseTracker::tracker().databaseNamesForOrigin(origin->securityOrigin.get(), names))
+        return 0;
+
+    for (unsigned i = 0; i < names.size(); i++) {
+        Ewk_Web_Database* database = ewk_web_database_new(origin->securityOrigin.get(), names[i].utf8().data());
+        databases = eina_list_append(databases, database);
+    }
+#else
+    UNUSED_PARAM(origin);
+#endif
+    return databases;
 }
 
 void ewk_security_origin_free(Ewk_Security_Origin* origin)
--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_web_database_private.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_web_database_private.h
@@ -1,8 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
-    Copyright (C) 2008-2009 INdT - Instituto Nokia de Tecnologia
-    Copyright (C) 2009-2010 ProFUSION embedded systems
-    Copyright (C) 2009-2010 Samsung Electronics
+    Copyright (C) 2012 Intel Corporation
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
@@ -20,31 +18,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     Boston, MA 02110-1301, USA.
 */
 
-/**
- * @file    EWebKit.h
- * @brief   Contains the header files that are required by WebKit-EFL.
- *
- * It includes the all header files that are exported to public API and Evas header.
- */
+#ifndef ewk_web_database_private_h
+#define ewk_web_database_private_h
 
-#ifndef EWebKit_h
-#define EWebKit_h
+#include <wtf/text/WTFString.h>
 
-#include "ewk_auth.h"
-#include "ewk_contextmenu.h"
-#include "ewk_cookies.h"
-#include "ewk_frame.h"
-#include "ewk_history.h"
-#include "ewk_js.h"
-#include "ewk_main.h"
-#include "ewk_network.h"
-#include "ewk_security_origin.h"
-#include "ewk_security_policy.h"
-#include "ewk_settings.h"
-#include "ewk_view.h"
-#include "ewk_web_database.h"
-#include "ewk_window_features.h"
+namespace WebCore {
+class SecurityOrigin;
+}
 
-#include <Evas.h>
+Ewk_Web_Database* ewk_web_database_new(WebCore::SecurityOrigin* coreOrigin, const WTF::String& coreName);
 
-#endif // EWebKit_h
+#endif // ewk_web_database_private_h
