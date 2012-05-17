e05ec668121426bd05eb631b3472dc875c02a57e
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 01:37:11 +0000

    Add GetIOMessageLoopProxy method to RenderThread interface. This is needed to set up a PPAPI HostDispatcher in Chrome code, so we can work on the NaCl IPC proxy outside of src/content. BUG=116317 TEST=compiles
    Review URL: https://chromiumcodereview.appspot.com/10407009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137604 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/render_thread.h
+++ content/public/renderer/render_thread.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -44,6 +44,7 @@ class CONTENT_EXPORT RenderThread : public IPC::Message::Sender {
   virtual IPC::SyncChannel* GetChannel() = 0;
   virtual std::string GetLocale() = 0;
   virtual IPC::SyncMessageFilter* GetSyncMessageFilter() = 0;
+  virtual scoped_refptr<base::MessageLoopProxy> GetIOMessageLoopProxy() = 0;
 
   // Called to add or remove a listener for a particular message routing ID.
   // These methods normally get delegated to a MessageRouter.
