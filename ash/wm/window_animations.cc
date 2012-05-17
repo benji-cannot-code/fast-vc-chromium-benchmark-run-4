234ecfac749b5bc34c70c5da982ede15f7350531
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 23:08:30 +0000

    Use Layer's target visibility instead of the current visibility in Window::SetVisible.
    Widget::Show does not take effect if it's called while the hide animation is running.
    This CL modifies Window::SetVisible to check Layer's target visibility instead of Layer's current visibility so that Window::Show takes effect even when Window::Hide followed immediately by Window::Show.
    Also modified the widnow animation code so that Layer's target visibility is correctly set.
    
    BUG=127863
    TEST=Tried repro steps (see BUG) and didn't crash. Passed aura_unittests and aura_shell_unittests.
    
    Review URL: https://chromiumcodereview.appspot.com/10387146
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137764 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_animations.cc
+++ ash/wm/window_animations.cc
@@ -142,9 +142,6 @@ class HidingWindowAnimationObserver : public ui::ImplicitAnimationObserver,
  private:
   // Overridden from ui::ImplicitAnimationObserver:
   virtual void OnImplicitAnimationsCompleted() OVERRIDE {
-    // Restore the correct visibility value (overridden for the duration of the
-    // animation in AnimateHideWindow()).
-    layer()->SetVisible(false);
     // Window may have been destroyed by this point.
     if (window_)
       window_->RemoveObserver(this);
@@ -204,8 +201,9 @@ class WorkspaceHidingWindowAnimationObserver :
   DISALLOW_COPY_AND_ASSIGN(WorkspaceHidingWindowAnimationObserver);
 };
 
-// Shows a window using an animation, animating its opacity from 0.f to 1.f, and
-// its transform from |start_transform| to |end_transform|.
+// Shows a window using an animation, animating its opacity from 0.f to 1.f,
+// its visibility to true, and its transform from |start_transform| to
+// |end_transform|.
 void AnimateShowWindowCommon(aura::Window* window,
                              const ui::Transform& start_transform,
                              const ui::Transform& end_transform) {
@@ -220,13 +218,14 @@ void AnimateShowWindowCommon(aura::Window* window,
     if (duration.ToInternalValue() > 0)
       settings.SetTransitionDuration(duration);
 
+    window->layer()->SetVisible(true);
     window->layer()->SetTransform(end_transform);
     window->layer()->SetOpacity(kWindowAnimation_ShowOpacity);
   }
 }
 
-// Hides a window using an animation, animating its opacity from 1.f to 0.f, and
-// its transform to |end_transform|.
+// Hides a window using an animation, animating its opacity from 1.f to 0.f,
+// its visibility to false, and its transform to |end_transform|.
 void AnimateHideWindowCommon(aura::Window* window,
                              const ui::Transform& end_transform) {
   window->layer()->set_delegate(NULL);
@@ -241,6 +240,7 @@ void AnimateHideWindowCommon(aura::Window* window,
 
   window->layer()->SetOpacity(kWindowAnimation_HideOpacity);
   window->layer()->SetTransform(end_transform);
+  window->layer()->SetVisible(false);
 }
 
 // Show/Hide windows using a shrink animation.
@@ -442,6 +442,7 @@ void AnimateHideWindow_Minimize(aura::Window* window) {
   // Property sets within this scope will be implicitly animated.
   ui::ScopedLayerAnimationSettings settings(window->layer()->GetAnimator());
   settings.AddObserver(new HidingWindowAnimationObserver(window));
+  window->layer()->SetVisible(false);
 
   AddLayerAnimationsForMinimize(window, false);
 }
--- ash/wm/window_animations_unittest.cc
+++ ash/wm/window_animations_unittest.cc
@@ -33,6 +33,7 @@ TEST_F(WindowAnimationsWorkspaceTest, HideShow) {
   ash::internal::AnimateOnChildWindowVisibilityChanged(
       window.get(), false);
   EXPECT_EQ(0.0f, window->layer()->GetTargetOpacity());
+  EXPECT_FALSE(window->layer()->GetTargetVisibility());
   EXPECT_FALSE(window->layer()->visible());
   // Showing.
   SetWindowVisibilityAnimationType(
@@ -41,6 +42,7 @@ TEST_F(WindowAnimationsWorkspaceTest, HideShow) {
   ash::internal::AnimateOnChildWindowVisibilityChanged(
       window.get(), true);
   EXPECT_EQ(1.0f, window->layer()->GetTargetOpacity());
+  EXPECT_TRUE(window->layer()->GetTargetVisibility());
   EXPECT_TRUE(window->layer()->visible());
   // Stays showing.
   ui::AnimationContainerElement* element =
@@ -49,6 +51,7 @@ TEST_F(WindowAnimationsWorkspaceTest, HideShow) {
   element->Step(base::TimeTicks::Now() +
                 base::TimeDelta::FromSeconds(5));
   EXPECT_EQ(1.0f, window->layer()->GetTargetOpacity());
+  EXPECT_TRUE(window->layer()->GetTargetVisibility());
   EXPECT_TRUE(window->layer()->visible());
 }
 
@@ -67,6 +70,7 @@ TEST_F(WindowAnimationsWorkspaceTest, ShowHide) {
   ash::internal::AnimateOnChildWindowVisibilityChanged(
       window.get(), true);
   EXPECT_EQ(1.0f, window->layer()->GetTargetOpacity());
+  EXPECT_TRUE(window->layer()->GetTargetVisibility());
   EXPECT_TRUE(window->layer()->visible());
   // Hiding.
   SetWindowVisibilityAnimationType(
@@ -75,6 +79,7 @@ TEST_F(WindowAnimationsWorkspaceTest, ShowHide) {
   ash::internal::AnimateOnChildWindowVisibilityChanged(
       window.get(), false);
   EXPECT_EQ(0.0f, window->layer()->GetTargetOpacity());
+  EXPECT_FALSE(window->layer()->GetTargetVisibility());
   EXPECT_FALSE(window->layer()->visible());
   // Stays hidden.
   ui::AnimationContainerElement* element =
@@ -83,7 +88,24 @@ TEST_F(WindowAnimationsWorkspaceTest, ShowHide) {
   element->Step(base::TimeTicks::Now() +
                 base::TimeDelta::FromSeconds(5));
   EXPECT_EQ(0.0f, window->layer()->GetTargetOpacity());
+  EXPECT_FALSE(window->layer()->GetTargetVisibility());
   EXPECT_FALSE(window->layer()->visible());
+}
+
+TEST_F(WindowAnimationsWorkspaceTest, LayerTargetVisibility) {
+  aura::Window* default_container =
+      ash::Shell::GetInstance()->GetContainer(
+          internal::kShellWindowId_DefaultContainer);
+  scoped_ptr<aura::Window> window(
+      aura::test::CreateTestWindowWithId(0, default_container));
+
+  // Layer target visibility changes according to Show/Hide.
+  window->Show();
+  EXPECT_TRUE(window->layer()->GetTargetVisibility());
+  window->Hide();
+  EXPECT_FALSE(window->layer()->GetTargetVisibility());
+  window->Show();
+  EXPECT_TRUE(window->layer()->GetTargetVisibility());
 }
 
 }  // namespace internal
