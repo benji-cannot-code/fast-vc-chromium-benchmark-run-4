4191340a05e5455c9e8316090dc15c3e5898b41d
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 17:30:25 +0000

    chromeos: Ignore power button presses when screen is off.
    Make PowerButtonController ignore power button events when
    the screen is turned off -- if the user hits the power
    button to turn the screen back on, we don't want to lock or
    shutdown if they hold it a bit too long.
    
    BUG=128451
    TEST=added, also did manual testing
    
    
    Review URL: https://chromiumcodereview.appspot.com/10382211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137679 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/power/brightness_observer.cc
+++ chrome/browser/chromeos/power/brightness_observer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/power/brightness_observer.h"
 
+#include "ash/shell.h"
+#include "ash/wm/power_button_controller.h"
 #include "chrome/browser/extensions/system/system_api.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 
@@ -20,6 +22,8 @@ BrightnessObserver::~BrightnessObserver() {
 
 void BrightnessObserver::BrightnessChanged(int level, bool user_initiated) {
   extensions::DispatchBrightnessChangedEvent(level, user_initiated);
+  ash::Shell::GetInstance()->power_button_controller()->
+      OnScreenBrightnessChanged(static_cast<double>(level));
 }
 
 }  // namespace chromeos
--- chrome/browser/chromeos/power/brightness_observer.h
+++ chrome/browser/chromeos/power/brightness_observer.h
@@ -13,8 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-// This observer displays a bubble at the bottom of the screen showing the
-// current brightness level whenever the user changes it.
+// This observer listens for changes to the screen brightness and notifies
+// extensions and ash::PowerButtonController about them.
 class BrightnessObserver : public PowerManagerClient::Observer {
  public:
   // This class registers/unregisters itself as an observer in ctor/dtor.
