5e91f57f90978b51d03e16d24efea70a6cea77eb
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 01:48:37 +0000

    Fix uninitialized member in ctor.
    TBR=darin@chromium.org
    R=jhawkins@chromium.org
    
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10377180
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137606 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/intents/internal_web_intents_dispatcher_unittest.cc
+++ content/browser/intents/internal_web_intents_dispatcher_unittest.cc
@@ -15,8 +15,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class InternalWebIntentsDispatcherTest
     : public content::RenderViewHostTestHarness {
  public:
-  InternalWebIntentsDispatcherTest() {
-    replied_ = 0;
+  InternalWebIntentsDispatcherTest()
+      : replied_(0),
+        notified_reply_type_(webkit_glue::WEB_INTENT_REPLY_INVALID) {
   }
 
   ~InternalWebIntentsDispatcherTest() {}
