b8a42aac476dd726faf206a1f10a4cf9c060834f
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 09:24:25 +0000

    [Photo Editor] Save edited images immediately.
    BUG=126898
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10399047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137643 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/image_editor/image_view.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_view.js
@@ -123,6 +123,10 @@ ImageView.prototype.getVideo = function() { return this.videoElement_ };
 
 ImageView.prototype.getThumbnail = function() { return this.thumbnailCanvas_ };
 
+ImageView.prototype.getContentRevision = function() {
+  return this.contentRevision_;
+};
+
 /**
  * Copy an image fragment from a full resolution canvas to a device resolution
  * canvas.
@@ -220,6 +224,7 @@ ImageView.prototype.load = function(
   var self = this;
 
   this.contentID_ = id;
+  this.contentRevision_ = -1;
 
   var loadingVideo = FileType.getMediaType(source) == 'video';
   if (loadingVideo) {
@@ -371,9 +376,8 @@ ImageView.prototype.getReadyContent = function(id, source) {
  *
  * @param {number} id Unique image id for caching purposes
  * @param {string|HTMLCanvasElement} source
- * @param {Object} metadata
  */
-ImageView.prototype.prefetch = function(id, source, metadata) {
+ImageView.prototype.prefetch = function(id, source) {
   var self = this;
   function prefetchDone(canvas) {
     if (canvas.width)
@@ -438,6 +442,7 @@ ImageView.prototype.replaceContent_ = function(
     // Once we implement zoom/pan we should pass contentCanvas_ instead.
     this.updateThumbnail_(this.screenImage_);
 
+    this.contentRevision_++;
     for (var i = 0; i != this.contentCallbacks_.length; i++) {
       try {
         this.contentCallbacks_[i]();
