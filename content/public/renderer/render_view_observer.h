13480ce324590d3ffd391df8fe177d1ecf19bffe
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 23:07:27 +0000

    Hide RenderViewObserver::RenderViewGone. I made it public initially because I didn't want to have the forward declaration of internal content classes in public headers. But later on I got convinced that this doesn't matter. Review URL: https://chromiumcodereview.appspot.com/10377185
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137762 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/render_view_observer.h
+++ content/public/renderer/render_view_observer.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_channel.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebIconURL.h"
 
+class RenderViewImpl;
+
 namespace WebKit {
 class WebDataSource;
 class WebFrame;
@@ -84,10 +86,6 @@ class CONTENT_EXPORT RenderViewObserver : public IPC::Channel::Listener,
   // IPC::Channel::Listener implementation.
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
 
-  // This is called by the RenderView when it's going away so that this object
-  // can null out its pointer.
-  void RenderViewGone();
-
  protected:
   explicit RenderViewObserver(RenderView* render_view);
   virtual ~RenderViewObserver();
@@ -99,6 +97,12 @@ class CONTENT_EXPORT RenderViewObserver : public IPC::Channel::Listener,
   int routing_id() { return routing_id_; }
 
  private:
+  friend class ::RenderViewImpl;
+
+  // This is called by the RenderView when it's going away so that this object
+  // can null out its pointer.
+  void RenderViewGone();
+
   RenderView* render_view_;
   // The routing ID of the associated RenderView.
   int routing_id_;
