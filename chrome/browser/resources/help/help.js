d4236a9650c2f75a1bcd207e3797c583fb8eb2ef
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 00:25:47 +0000

    Revert 137554 - convert help page to loadTimeData
    BUG=122753
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10386076
    
    TBR=estade@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10383228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137581 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/help/help.js
+++ chrome/browser/resources/help/help.js
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <include src="../uber/uber_utils.js">
 
 cr.define('help', function() {
+  var localStrings = new LocalStrings();
+
   /**
    * Encapsulated handling of the help page.
    */
@@ -23,18 +25,17 @@ cr.define('help', function() {
       uber.onContentFrameLoaded();
 
       // Set the title.
-      var title = loadTimeData.getString('helpTitle');
+      var title = localStrings.getString('helpTitle');
       uber.invokeMethodOnParent('setTitle', {title: title});
 
-      $('product-license').innerHTML = loadTimeData.getString('productLicense');
-      if (cr.isChromeOS) {
+      $('product-license').innerHTML = localStrings.getString('productLicense');
+      if (cr.isChromeOS)
         $('product-os-license').innerHTML =
-            loadTimeData.getString('productOsLicense');
-      }
+            localStrings.getString('productOsLicense');
 
       var productTOS = $('product-tos');
       if (productTOS)
-        productTOS.innerHTML = loadTimeData.getString('productTOS');
+        productTOS.innerHTML = localStrings.getString('productTOS');
 
       $('get-help').onclick = chrome.send.bind(chrome, 'openHelpPage');
       $('report-issue').onclick =
@@ -71,8 +72,8 @@ cr.define('help', function() {
       moreInfo.style.height = visible ? '' : moreInfo.scrollHeight + 'px';
       moreInfo.addEventListener('webkitTransitionEnd', function(event) {
         $('more-info-expander').textContent = visible ?
-            loadTimeData.getString('showMoreInfo') :
-            loadTimeData.getString('hideMoreInfo');
+            localStrings.getString('showMoreInfo') :
+                localStrings.getString('hideMoreInfo');
       });
     },
 
@@ -99,17 +100,17 @@ cr.define('help', function() {
       if (status == 'checking') {
         this.setUpdateImage_('working');
         $('update-status').innerHTML =
-            loadTimeData.getString('updateCheckStarted');
+            localStrings.getString('updateCheckStarted');
       } else if (status == 'updating') {
         this.setUpdateImage_('working');
-        $('update-status').innerHTML = loadTimeData.getString('updating');
+        $('update-status').innerHTML = localStrings.getString('updating');
       } else if (status == 'nearly_updated') {
         this.setUpdateImage_('up-to-date');
         $('update-status').innerHTML =
-            loadTimeData.getString('updateAlmostDone');
+            localStrings.getString('updateAlmostDone');
       } else if (status == 'updated') {
         this.setUpdateImage_('up-to-date');
-        $('update-status').innerHTML = loadTimeData.getString('upToDate');
+        $('update-status').innerHTML = localStrings.getString('upToDate');
       } else if (status == 'failed') {
         this.setUpdateImage_('failed');
         $('update-status').innerHTML = message;
