4067283bc15228cf90336dcee75345d84931775a
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-17 00:07:16 +0000

    Crash if SVG gradient stop has display: none set https://bugs.webkit.org/show_bug.cgi?id=86686 <rdar://problem/10751905>
    Reviewed by Dean Jackson.
    
    Source/WebCore:
    
    Create a renderer for SVGStopElement regardless of the "display" property.
    This matches the behavior of Opera and the SVG specification.
    
    Test: svg/custom/gradient-stop-display-none-crash.svg
    
    * svg/SVGStopElement.cpp:
    (WebCore::SVGStopElement::rendererIsNeeded):
    (WebCore):
    * svg/SVGStopElement.h:
    (SVGStopElement):
    
    LayoutTests:
    
    Add a test that ensures that we don't crash if "display: none" is set on an SVGStopElement.
    
    * svg/custom/gradient-stop-display-none-crash-expected.txt: Added.
    * svg/custom/gradient-stop-display-none-crash.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGStopElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGStopElement.cpp
@@ -107,6 +107,11 @@ RenderObject* SVGStopElement::createRenderer(RenderArena* arena, RenderStyle*)
     return new (arena) RenderSVGGradientStop(this);
 }
 
+bool SVGStopElement::rendererIsNeeded(const NodeRenderingContext&)
+{
+    return true;
+}
+
 Color SVGStopElement::stopColorIncludingOpacity() const
 {
     ASSERT(renderer());
