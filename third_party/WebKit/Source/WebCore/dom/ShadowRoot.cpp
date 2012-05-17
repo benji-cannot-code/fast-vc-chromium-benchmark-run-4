6e8d3fb433dc9e82f1753b87fbefe747e6640926
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-17 09:55:59 +0000

    ShadowRoot.selection should be ShadowRoot.getSelection() https://bugs.webkit.org/show_bug.cgi?id=86598
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    In the latest spec, ShadowRoot.selection is changed to ShadowRoot.getSelection().
    So update the implementation.
    
    * dom/ShadowRoot.cpp:
    * dom/ShadowRoot.h:
    (ShadowRoot):
    * dom/ShadowRoot.idl:
    
    LayoutTests:
    
    In the latest spec, ShadowRoot.selection is changed to ShadowRoot.getSelection().
    So update the tests.
    
    * editing/shadow/selection-of-orphan-shadowroot.html:
    * editing/shadow/selection-of-shadowroot.html: Removed unnecessary code to
    call ShadowRoot.selection by using ShadowRoot.getSelection(). Now ShadowRoot.getSelection()
    can be called directly.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
@@ -150,11 +150,6 @@ void ShadowRoot::setInnerHTML(const String& markup, ExceptionCode& ec)
         replaceChildrenWithFragment(this, fragment.release(), ec);
 }
 
-DOMSelection* ShadowRoot::selection() const
-{
-    return getSelection();
-}
-
 bool ShadowRoot::childTypeAllowed(NodeType type) const
 {
     switch (type) {
--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.h
@@ -77,8 +77,6 @@ public:
 
     Element* activeElement() const;
 
-    DOMSelection* selection() const;
-
     ShadowRoot* youngerShadowRoot() const { return prev(); }
     ShadowRoot* olderShadowRoot() const { return next(); }
 
