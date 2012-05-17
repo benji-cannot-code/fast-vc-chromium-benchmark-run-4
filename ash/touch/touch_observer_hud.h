aab457dd504d12d88d2ff0b80892e5ea6cc8180d
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 17:36:53 +0000

    ash: Make the touchy heads-up display more colorful/useful.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10399061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137680 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/touch/touch_observer_hud.h
+++ ash/touch/touch_observer_hud.h
@@ -25,6 +25,8 @@ class Widget;
 namespace ash {
 namespace internal {
 
+class TouchHudCanvas;
+
 // An event filter which handles system level gesture events.
 class TouchObserverHUD : public aura::EventFilter {
  public:
@@ -46,7 +48,9 @@ class TouchObserverHUD : public aura::EventFilter {
       aura::GestureEvent* event) OVERRIDE;
 
   static const int kMaxTouchPoints = 32;
+
   scoped_ptr<views::Widget> widget_;
+  TouchHudCanvas* canvas_;
   views::Label* touch_labels_[kMaxTouchPoints];
   gfx::Point touch_positions_[kMaxTouchPoints];
   ui::EventType touch_status_[kMaxTouchPoints];
