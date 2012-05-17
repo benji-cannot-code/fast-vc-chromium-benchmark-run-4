d442b67f756bfa1877802d5dc33e1a0c960a0ded
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-17 14:21:41 +0000

    Web Inspector: [Extensions API] pages shown in sidebar are limited in height to 150px https://bugs.webkit.org/show_bug.cgi?id=86659
    Reviewed by Vsevolod Vlasov.
    
    * inspector/front-end/ExtensionPanel.js:
    (WebInspector.ExtensionView):
    (WebInspector.ExtensionSidebarPane.prototype.setPage): set default iframe height to 150px, unless already set by user.
    * inspector/front-end/inspector.css: set extension view and iframe elements to "fill", iframe to 100% height.
    (iframe.extension):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ExtensionPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ExtensionPanel.js
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2011 Google Inc. All rights reserved.
+ * Copyright (C) 2012 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 WebInspector.ExtensionView = function(id, parent, src, className)
 {
     WebInspector.View.call(this);
+    this.element.className = "fill";
 
     this._id = id;
     this._iframe = document.createElement("iframe");
@@ -264,7 +265,9 @@ WebInspector.ExtensionSidebarPane.prototype = {
         if (this._extensionView)
             this._extensionView.detach(true);
 
-        this._extensionView = new WebInspector.ExtensionView(this._id, this.bodyElement, url, "extension");
+        this._extensionView = new WebInspector.ExtensionView(this._id, this.bodyElement, url, "extension fill");
+        if (!this.bodyElement.style.height)
+            this.setHeight("150px");
     },
 
     /**
