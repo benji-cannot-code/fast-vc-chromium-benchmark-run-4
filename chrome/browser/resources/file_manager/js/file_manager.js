d321a8dd778eff02eee597c29c7e06895a29329f
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 18:22:18 +0000

    convert chromeos file manager to loadTimeData
    BUG=122753
    TEST=manual,unit
    
    Review URL: https://chromiumcodereview.appspot.com/10391075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -114,11 +114,6 @@ FileManager.prototype = {
   var MINIMUM_BUTTER_DISPLAY_TIME_MS = 1300;
 
   /**
-   * Translated strings.
-   */
-  var localStrings;
-
-  /**
    * Item for the Grid View.
    * @constructor
    */
@@ -149,27 +144,27 @@ FileManager.prototype = {
    * Return a translated string.
    *
    * Wrapper function to make dealing with translated strings more concise.
-   * Equivilant to localStrings.getString(id).
+   * Equivalent to loadTimeData.getString(id).
    *
    * @param {string} id The id of the string to return.
    * @return {string} The translated string.
    */
   function str(id) {
-    return localStrings.getString(id) || ('UNLOCALIZED STRING ' + id);
+    return loadTimeData.getString(id);
   }
 
   /**
    * Return a translated string with arguments replaced.
    *
    * Wrapper function to make dealing with translated strings more concise.
-   * Equivilant to localStrings.getStringF(id, ...).
+   * Equivilant to loadTimeData.getStringF(id, ...).
    *
    * @param {string} id The id of the string to return.
    * @param {...string} The values to replace into the string.
    * @return {string} The translated string with replaced values.
    */
   function strf(id, var_args) {
-    return localStrings.getStringF.apply(localStrings, arguments);
+    return loadTimeData.getStringF.apply(loadTimeData, arguments);
   }
 
   /**
@@ -185,8 +180,8 @@ FileManager.prototype = {
         break;
       }
     }
-    return localStrings.getString('FILE_ERROR_' + code) ||
-        localStrings.getStringF('FILE_ERROR_GENERIC', code);
+    return loadTimeData.getString('FILE_ERROR_' + code) ||
+        loadTimeData.getStringF('FILE_ERROR_GENERIC', code);
   }
 
   /**
@@ -260,7 +255,7 @@ FileManager.prototype = {
    */
   FileManager.initStrings = function(callback) {
     chrome.fileBrowserPrivate.getStrings(function(strings) {
-      localStrings = new LocalStrings(strings);
+      loadTimeData.data = strings;
       if (callback)
         callback();
     });
@@ -597,7 +592,7 @@ FileManager.prototype = {
     this.dialogDom_.ownerDocument.defaultView.addEventListener(
         'resize', this.onResize_.bind(this));
 
-    if (str('ASH') == '1')
+    if (loadTimeData.getBoolean('ASH'))
       this.dialogDom_.setAttribute('ash', 'true');
 
     this.filePopup_ = null;
@@ -606,7 +601,7 @@ FileManager.prototype = {
         'input', this.onSearchBoxUpdate_.bind(this));
 
     // Populate the static localized strings.
-    i18nTemplate.process(this.document_, localStrings.templateData);
+    i18nTemplate.process(this.document_, loadTimeData);
   };
 
   /**
@@ -2439,7 +2434,9 @@ FileManager.prototype = {
           task.title = str('ACTION_OPEN');
         } else if (task_parts[1] == 'view-pdf') {
           // Do not render this task if disabled.
-          if (str('PDF_VIEW_ENABLED') == 'false') continue;
+          if (!loadTimeData.getBoolean('PDF_VIEW_ENABLED'))
+            continue;
+
           task.iconUrl =
               chrome.extension.getURL('images/filetype_pdf.png');
           task.title = str('ACTION_VIEW');
@@ -2613,7 +2610,7 @@ FileManager.prototype = {
 
   FileManager.prototype.getGDataPreferences_ = function() {
     return this.gdataPreferences_ ||
-      { driveEnabled: str('ENABLE_GDATA') == '1' };
+      { driveEnabled: loadTimeData.getBoolean('ENABLE_GDATA') };
   };
 
   FileManager.prototype.getNetworkConnectionState_ = function() {
