5e80dcd11eeb4a26d1c04ba2d087275ae1dc6c1d
mifenton@rim.com <mifenton@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-17 17:20:07 +0000

    [BlackBerry] Build fix, SelectionHandler extra debug logs https://bugs.webkit.org/show_bug.cgi?id=86743
    Reviewed by Antonio Gomes.
    
    Add include for BlackBerryPlatformLog to SelectionHandler.cpp
    
    * WebKitSupport/SelectionHandler.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117458 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/SelectionHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/SelectionHandler.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "visible_units.h"
 
 #include <BlackBerryPlatformKeyboardEvent.h>
+#include <BlackBerryPlatformLog.h>
 
 #include <sys/keycodes.h>
 
