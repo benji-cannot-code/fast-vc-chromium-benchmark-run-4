b1d072c65cffe4ba174514a2e4e8a98389d20f37
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-17 17:49:45 +0000

    Revert 137445 - CHECK for that we are not seeing user-visible SPDY_PING_FAILED errors.
    BUG=107048
    
    
    Review URL: https://chromiumcodereview.appspot.com/10243016
    
    TBR=rch@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10408010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137683 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction.cc
+++ net/http/http_network_transaction.cc
@@ -165,7 +165,6 @@ int HttpNetworkTransaction::Start(const HttpRequestInfo* request_info,
   int rv = DoLoop(OK);
   if (rv == ERR_IO_PENDING)
     callback_ = callback;
-  CHECK_NE(rv, ERR_SPDY_PING_FAILED);
   return rv;
 }
 
@@ -472,7 +471,6 @@ bool HttpNetworkTransaction::is_https_request() const {
 }
 
 void HttpNetworkTransaction::DoCallback(int rv) {
-  CHECK_NE(rv, ERR_SPDY_PING_FAILED);
   DCHECK_NE(rv, ERR_IO_PENDING);
   DCHECK(!callback_.is_null());
 
