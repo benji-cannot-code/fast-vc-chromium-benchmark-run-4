e4c3eaa8630ed7c2d717898fcad9b2b42afe58a7
robert@webkit.org <robert@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-17 19:07:57 +0000

    CSS 2.1 failure: table-height-algorithm-023 and -024 fail https://bugs.webkit.org/show_bug.cgi?id=85405
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Treat vertical-align: [length] the same as vertical-align: baseline.
    
    Tests: css2.1/20110323/table-height-algorithm-023.htm
           css2.1/20110323/table-height-algorithm-024.htm
           fast/table/mozilla-bug10296-vertical-align-1.html
           fast/table/mozilla-bug10296-vertical-align-2.html
    
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::calcRowLogicalHeight):
    (WebCore::RenderTableSection::layoutRows):
    
    LayoutTests:
    
    * css2.1/20110323/table-height-algorithm-023.htm: Added.
    * css2.1/20110323/table-height-algorithm-024.htm: Added.
        Tests from the CSS test suite.
    * fast/table/mozilla-bug10296-vertical-align-1.html: Added.
    * fast/table/mozilla-bug10296-vertical-align-2.html: Added.
        I split out the part of bug10296-1.html that gets rebaselined here so that the new results
        are visible. I also changed the text so that the expected result is less mysterious.
    
    * platform/chromium-linux-x86/css2.1/20110323/table-height-algorithm-023-expected.png: Added.
    * platform/chromium-linux-x86/css2.1/20110323/table-height-algorithm-023-expected.txt: Added.
    * platform/chromium-linux-x86/css2.1/20110323/table-height-algorithm-024-expected.png: Added.
    * platform/chromium-linux-x86/css2.1/20110323/table-height-algorithm-024-expected.txt: Added.
    * platform/chromium-linux-x86/fast/table/mozilla-bug10296-vertical-align-1-expected.png: Added.
    * platform/chromium-linux-x86/fast/table/mozilla-bug10296-vertical-align-1-expected.txt: Added.
    * platform/chromium-linux-x86/fast/table/mozilla-bug10296-vertical-align-2-expected.png: Added.
    * platform/chromium-linux-x86/fast/table/mozilla-bug10296-vertical-align-2-expected.txt: Added.
    * platform/chromium-win/tables/mozilla/bugs/bug10296-1-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTableSection.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTableSection.cpp
@@ -371,7 +371,7 @@ int RenderTableSection::calcRowLogicalHeight()
 
             // find out the baseline
             EVerticalAlign va = cell->style()->verticalAlign();
-            if (va == BASELINE || va == TEXT_BOTTOM || va == TEXT_TOP || va == SUPER || va == SUB) {
+            if (va == BASELINE || va == TEXT_BOTTOM || va == TEXT_TOP || va == SUPER || va == SUB || va == LENGTH) {
                 LayoutUnit baselinePosition = cell->cellBaselinePosition();
                 if (baselinePosition > cell->borderBefore() + cell->paddingBefore()) {
                     m_grid[r].baseline = max(m_grid[r].baseline, baselinePosition - cell->intrinsicPaddingBefore());
@@ -610,7 +610,7 @@ void RenderTableSection::layoutRows()
 
                 // If the baseline moved, we may have to update the data for our row. Find out the new baseline.
                 EVerticalAlign va = cell->style()->verticalAlign();
-                if (va == BASELINE || va == TEXT_BOTTOM || va == TEXT_TOP || va == SUPER || va == SUB) {
+                if (va == BASELINE || va == TEXT_BOTTOM || va == TEXT_TOP || va == SUPER || va == SUB || va == LENGTH) {
                     LayoutUnit baseline = cell->cellBaselinePosition();
                     if (baseline > cell->borderBefore() + cell->paddingBefore())
                         m_grid[r].baseline = max(m_grid[r].baseline, baseline);
@@ -627,6 +627,7 @@ void RenderTableSection::layoutRows()
                 case SUPER:
                 case TEXT_TOP:
                 case TEXT_BOTTOM:
+                case LENGTH:
                 case BASELINE: {
                     LayoutUnit b = cell->cellBaselinePosition();
                     if (b > cell->borderBefore() + cell->paddingBefore())
