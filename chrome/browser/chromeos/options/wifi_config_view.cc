25b9f97dd826de224681bfc3deb4ba123b49d16c
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 03:06:36 +0000

    Picked up stevenjb@: Added WimaxConfigView.
    BUG=128429
    TEST=Run chrome ash/chromeos in a stub environment. It should be possible to 'connect' to 'Fake Wimax1'
    
    
    Review URL: https://chromiumcodereview.appspot.com/10407034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137809 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/wifi_config_view.cc
+++ chrome/browser/chromeos/options/wifi_config_view.cc
@@ -1238,6 +1238,7 @@ void WifiConfigView::InitFocus() {
     view_to_focus->RequestFocus();
 }
 
+// static
 void WifiConfigView::ParseWiFiUIProperty(
     NetworkPropertyUIData* property_ui_data,
     Network* network,
@@ -1249,6 +1250,7 @@ void WifiConfigView::ParseWiFiUIProperty(
       base::StringPrintf("%s.%s", onc::kWiFi, key.c_str()));
 }
 
+// static
 void WifiConfigView::ParseWiFiEAPUIProperty(
     NetworkPropertyUIData* property_ui_data,
     Network* network,
