18129ed5b343fcd536db4529595b81a18f76e957
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 19:47:28 +0000

    ash: Increase the outside bounds of window frames to make it easier to resize with touch.
    BUG=113397
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10389211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137931 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/frame_painter.cc
+++ ash/wm/frame_painter.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/base/animation/slide_animation.h"
 #include "ui/base/hit_test.h"
+#include "ui/base/layout.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/theme_provider.h"
 #include "ui/gfx/canvas.h"
@@ -36,10 +37,11 @@ const int kBorderThickness = 0;
 // In the window corners, the resize areas don't actually expand bigger, but the
 // 16 px at the end of each edge triggers diagonal resizing.
 const int kResizeAreaCornerSize = 16;
-// Ash windows do not have a traditional visible window frame.  Window content
-// extends to the edge of the window.  We consider a small region outside the
+// Ash windows do not have a traditional visible window frame. Window content
+// extends to the edge of the window. We consider a small region outside the
 // window bounds and an even smaller region overlapping the window to be the
 // "non-client" area and use it for resizing.
+const int kResizeOutsideBoundsSizeTouch = 30;
 const int kResizeOutsideBoundsSize = 6;
 const int kResizeInsideBoundsSize = 1;
 // Space between left edge of window and popup window icon.
@@ -202,9 +204,12 @@ void FramePainter::Init(views::Widget* frame,
 
   window_ = frame->GetNativeWindow();
   // Ensure we get resize cursors for a few pixels outside our bounds.
+  int outside_bounds = ui::DisplayLayout() == ui::LAYOUT_TOUCH ?
+      kResizeOutsideBoundsSizeTouch :
+      kResizeOutsideBoundsSize;
   window_->set_hit_test_bounds_override_outer(
-      gfx::Insets(-kResizeOutsideBoundsSize, -kResizeOutsideBoundsSize,
-                  -kResizeOutsideBoundsSize, -kResizeOutsideBoundsSize));
+      gfx::Insets(-outside_bounds, -outside_bounds,
+                  -outside_bounds, -outside_bounds));
   // Ensure we get resize cursors just inside our bounds as well.
   window_->set_hit_test_bounds_override_inner(
       gfx::Insets(kResizeInsideBoundsSize, kResizeInsideBoundsSize,
@@ -238,7 +243,10 @@ gfx::Rect FramePainter::GetWindowBoundsForClientBounds(
 int FramePainter::NonClientHitTest(views::NonClientFrameView* view,
                                    const gfx::Point& point) {
   gfx::Rect expanded_bounds = view->bounds();
-  expanded_bounds.Inset(-kResizeOutsideBoundsSize, -kResizeOutsideBoundsSize);
+  int outside_bounds = ui::DisplayLayout() == ui::LAYOUT_TOUCH ?
+      kResizeOutsideBoundsSizeTouch :
+      kResizeOutsideBoundsSize;
+  expanded_bounds.Inset(-outside_bounds, -outside_bounds);
   if (!expanded_bounds.Contains(point))
     return HTNOWHERE;
 
