bdc5435f7a09749d46eef46ac0842f20a8bd6ea6
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 16:37:05 +0000

    Extends DBusThreadManager to connect ibus-bus.
    Introduce IBusUtil::GetIBusAddressTest to get ibus-daemon address.
    
    BUG=chromium-os:26334
    TEST=chromes_unittest, unit_tests, dbus_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10159004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137875 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/dbus_thread_manager.h
+++ chromeos/dbus/dbus_thread_manager.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROMEOS_DBUS_DBUS_THREAD_MANAGER_H_
 #pragma once
 
+#include <string>
+
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "chromeos/chromeos_export.h"
@@ -85,8 +87,16 @@ class CHROMEOS_EXPORT DBusThreadManager {
   // Gets the global instance. Initialize() must be called first.
   static DBusThreadManager* Get();
 
+  // Creates new IBusBus instance to communicate with ibus-daemon with specified
+  // ibus address. Must be called before using ibus related clients.
+  // TODO(nona): Support shutdown to enable dynamical ibus-daemon shutdown.
+  virtual void InitIBusBus(const std::string &ibus_address) = 0;
+
   // Returns the D-Bus system bus instance, owned by DBusThreadManager.
   virtual dbus::Bus* GetSystemBus() = 0;
+
+  // Returns the IBus bus instance, owned by DBusThreadManager.
+  virtual dbus::Bus* GetIBusBus() = 0;
 
   // Returns the bluetooth adapter client, owned by DBusThreadManager.
   // Do not cache this pointer and use it after DBusThreadManager is shut
