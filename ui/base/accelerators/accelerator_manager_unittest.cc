54e73de27cfaf38f8d93b4a13fca26d072a7a7af
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 21:08:00 +0000

    accelerators: Remove deprecated Accelerator ctor that takes booleans.
    BUG=128242
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10399085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137957 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/accelerators/accelerator_manager_unittest.cc
+++ ui/base/accelerators/accelerator_manager_unittest.cc
@@ -48,7 +48,7 @@ bool TestTarget::CanHandleAccelerators() const {
 }
 
 Accelerator GetAccelerator(KeyboardCode code, int mask) {
-  return Accelerator(code, mask & (1 << 0), mask & (1 << 1), mask & (1 << 2));
+  return Accelerator(code, mask);
 }
 
 }  // namespace
@@ -62,7 +62,7 @@ class AcceleratorManagerTest : public testing::Test {
 };
 
 TEST_F(AcceleratorManagerTest, Register) {
-  const Accelerator accelerator_a(VKEY_A, false, false, false);
+  const Accelerator accelerator_a(VKEY_A, EF_NONE);
   TestTarget target;
   manager_.Register(accelerator_a, AcceleratorManager::kNormalPriority,
                     &target);
@@ -73,7 +73,7 @@ TEST_F(AcceleratorManagerTest, Register) {
 }
 
 TEST_F(AcceleratorManagerTest, RegisterMultipleTarget) {
-  const Accelerator accelerator_a(VKEY_A, false, false, false);
+  const Accelerator accelerator_a(VKEY_A, EF_NONE);
   TestTarget target1;
   manager_.Register(accelerator_a, AcceleratorManager::kNormalPriority,
                     &target1);
@@ -89,11 +89,11 @@ TEST_F(AcceleratorManagerTest, RegisterMultipleTarget) {
 }
 
 TEST_F(AcceleratorManagerTest, Unregister) {
-  const Accelerator accelerator_a(VKEY_A, false, false, false);
+  const Accelerator accelerator_a(VKEY_A, EF_NONE);
   TestTarget target;
   manager_.Register(accelerator_a, AcceleratorManager::kNormalPriority,
                     &target);
-  const Accelerator accelerator_b(VKEY_B, false, false, false);
+  const Accelerator accelerator_b(VKEY_B, EF_NONE);
   manager_.Register(accelerator_b, AcceleratorManager::kNormalPriority,
                     &target);
 
@@ -111,14 +111,14 @@ TEST_F(AcceleratorManagerTest, Unregister) {
 }
 
 TEST_F(AcceleratorManagerTest, UnregisterAll) {
-  const Accelerator accelerator_a(VKEY_A, false, false, false);
+  const Accelerator accelerator_a(VKEY_A, EF_NONE);
   TestTarget target1;
   manager_.Register(accelerator_a, AcceleratorManager::kNormalPriority,
                     &target1);
-  const Accelerator accelerator_b(VKEY_B, false, false, false);
+  const Accelerator accelerator_b(VKEY_B, EF_NONE);
   manager_.Register(accelerator_b, AcceleratorManager::kNormalPriority,
                     &target1);
-  const Accelerator accelerator_c(VKEY_C, false, false, false);
+  const Accelerator accelerator_c(VKEY_C, EF_NONE);
   TestTarget target2;
   manager_.Register(accelerator_c, AcceleratorManager::kNormalPriority,
                     &target2);
