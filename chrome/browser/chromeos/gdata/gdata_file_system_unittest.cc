d92133c8ab1ceabc1ee0616e7737cf7f96622ff1
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 19:42:15 +0000

    gdata: Define the resource ID for the root directory
    Per the spec, the resource ID for the root directory is defined
    as "folder:root". Add the resource ID to the root directory in our
    file system representation so we can look up the root directory by
    the resource ID.
    
    BUG=127697
    TEST=add unit tests
    
    Review URL: https://chromiumcodereview.appspot.com/10332253
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
@@ -1001,6 +1001,7 @@ class GDataFileSystemTest : public testing::Test {
     GDataEntryProto* file_base = root_dir->mutable_gdata_entry();
     PlatformFileInfoProto* platform_info = file_base->mutable_file_info();
     file_base->set_title("drive");
+    file_base->set_resource_id(kGDataRootDirectoryResourceId);
     platform_info->set_is_directory(true);
 
     // drive/File1
