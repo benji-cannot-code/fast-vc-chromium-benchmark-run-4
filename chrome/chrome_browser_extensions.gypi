8f70a17fab03c7041f6514da446b161ab0ead615
penghuang@chromium.org <penghuang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 01:50:18 +0000

    Add schema chrome-extension-resource:// for extension resources. It may be used for bundle JS libraries in future like Google Analytics JS library.
    BUG=122987
    TEST=unit_tests --gtest_filter=ExtensionFileUtil.*
    TEST=browser_tests --gtest_filter=ExtensionResourceRequestPolicyTest*
    
    Review URL: https://chromiumcodereview.appspot.com/9909019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137794 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_browser_extensions.gypi
+++ chrome/chrome_browser_extensions.gypi
@@ -335,6 +335,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/extensions/extension_protocols.h',
         'browser/extensions/extension_record_api.cc',
         'browser/extensions/extension_record_api.h',
+        'browser/extensions/extension_resource_protocols.cc',
+        'browser/extensions/extension_resource_protocols.h',
         'browser/extensions/extension_scoped_prefs.h',
         'browser/extensions/extension_service.cc',
         'browser/extensions/extension_service.h',
--- chrome/common/extensions/csp_validator.cc
+++ chrome/common/extensions/csp_validator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -44,7 +44,8 @@ bool HasOnlySecureTokens(StringTokenizer& tokenizer) {
         source == "'none'" ||
         StartsWithASCII(source, "https://", true) ||
         StartsWithASCII(source, "chrome://", true) ||
-        StartsWithASCII(source, "chrome-extension://", true)) {
+        StartsWithASCII(source, "chrome-extension://", true) ||
+        StartsWithASCII(source, "chrome-extension-resource:", true)) {
       continue;
     }
 
