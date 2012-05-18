bb798f481711c99577fd7740f54144a70556c500
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 14:26:43 +0000

    Implemented declarative redirects to transparent image and empty document.
    BUG=112155
    TEST=no
    
    
    Review URL: https://chromiumcodereview.appspot.com/10386172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137858 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/declarative_webrequest_custom_bindings.js
+++ chrome/renderer/resources/extensions/declarative_webrequest_custom_bindings.js
@@ -43,4 +43,11 @@ chromeHidden.registerCustomHook('declarativeWebRequest', function(api) {
   chrome.declarativeWebRequest.RedirectRequest = function(parameters) {
     setupInstance(this, parameters, 'RedirectRequest');
   };
+  chrome.declarativeWebRequest.RedirectToTransparentImage =
+      function(parameters) {
+    setupInstance(this, parameters, 'RedirectToTransparentImage');
+  };
+  chrome.declarativeWebRequest.RedirectToEmptyDocument = function(parameters) {
+    setupInstance(this, parameters, 'RedirectToEmptyDocument');
+  };
 });
