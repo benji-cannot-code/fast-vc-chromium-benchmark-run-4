5bb4063a88805cf211142960b65227a44b72bf9c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-18 19:12:27 +0000

    Script elements inserted while fragment parsing should have their "already started" flag set. https://bugs.webkit.org/show_bug.cgi?id=86376
    Patch by Pablo Flouret <pablof@motorola.com> on 2012-05-18
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Step 3 of:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/tree-construction.html#scriptTag
    
    Test: fast/parser/script-already-started-flag-in-fragment-parsing-mode.html
    
    * html/HTMLScriptElement.cpp:
    (WebCore::HTMLScriptElement::create):
    * html/HTMLScriptElement.h:
    (HTMLScriptElement):
    * html/parser/HTMLConstructionSite.cpp:
    (WebCore::HTMLConstructionSite::insertScriptElement):
    
    LayoutTests:
    
    * fast/parser/script-already-started-flag-in-fragment-parsing-mode-expected.txt: Added.
    * fast/parser/script-already-started-flag-in-fragment-parsing-mode.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117611 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLScriptElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLScriptElement.h
@@ -32,7 +32,7 @@ namespace WebCore {
 
 class HTMLScriptElement : public HTMLElement, public ScriptElement {
 public:
-    static PassRefPtr<HTMLScriptElement> create(const QualifiedName&, Document*, bool wasInsertedByParser);
+    static PassRefPtr<HTMLScriptElement> create(const QualifiedName&, Document*, bool wasInsertedByParser, bool alreadyStarted = false);
 
     String text() const { return scriptContent(); }
     void setText(const String&);
