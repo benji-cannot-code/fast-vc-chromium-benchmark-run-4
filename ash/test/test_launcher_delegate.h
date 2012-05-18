a251ec1b029184b6e80efbb55436684a33d99bda
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 22:03:38 +0000

    Revert 137968 - Adding four bezel gestures
    Reverted due to compile failure on Linux CrOS
    
    BUG=126061, 126060, 123167
    TEST=Unit test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10383108
    
    TBR=skuhne@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10412012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137970 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/test/test_launcher_delegate.h
+++ ash/test/test_launcher_delegate.h
@@ -29,7 +29,6 @@ class TestLauncherDelegate : public LauncherDelegate,
   virtual ~TestLauncherDelegate();
 
   void AddLauncherItem(aura::Window* window);
-  void AddLauncherItem(aura::Window* window, LauncherItemStatus status);
 
   static TestLauncherDelegate* instance() { return instance_; }
 
