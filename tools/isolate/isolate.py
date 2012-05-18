33d67f0688108c964a36a396ce1b1113956a1bbc
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 18:30:08 +0000

    An empty .results file would throw ValueError, eat it.
    R=mad@chromium.org
    BUG=
    TEST=Switching from test_isolate_mode=noop to hashtable works
    
    Review URL: https://chromiumcodereview.appspot.com/10386211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137910 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/isolate.py
+++ tools/isolate/isolate.py
@@ -299,8 +299,8 @@ class Flattenable(object):
       with open(filename, 'r') as f:
         out = cls.load(json.load(f))
       logging.debug('Loaded %s(%s)' % (cls.__name__, filename))
-    except IOError:
-      pass
+    except (IOError, ValueError):
+      logging.warn('Failed to load %s' % filename)
     return out
 
 
