4c8526fdd75f9f2d06ed946176455f93d9c62809
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-18 23:06:05 +0000

    Build fix after r117607.
    * platform/mac/WebCoreNSCellExtras.m:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117652 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/WebCoreNSCellExtras.m
+++ third_party/WebKit/Source/WebCore/platform/mac/WebCoreNSCellExtras.m
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#import "config.h"
 #import "WebCoreNSCellExtras.h"
 
 #if !BUTTON_CELL_DRAW_WITH_FRAME_DRAWS_FOCUS_RING
