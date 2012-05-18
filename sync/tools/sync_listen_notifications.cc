fcaafe80f3439a60486fc22f7b1e6103f37ecf79
munjal@chromium.org <munjal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-18 19:24:41 +0000

    Revive revision 137685 and fix the DEPs file in chrome/browser/sync
    BUG=124140
    Review URL: https://chromiumcodereview.appspot.com/10408011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@137922 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/tools/sync_listen_notifications.cc
+++ sync/tools/sync_listen_notifications.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "jingle/notifier/base/notifier_options.h"
 #include "net/base/host_port_pair.h"
 #include "net/url_request/url_request_test_util.h"
-#include "sync/notifier/invalidation_version_tracker.h"
+#include "sync/notifier/invalidation_state_tracker.h"
 #include "sync/notifier/sync_notifier.h"
 #include "sync/notifier/sync_notifier_factory.h"
 #include "sync/notifier/sync_notifier_observer.h"
@@ -66,12 +66,12 @@ class NotificationPrinter : public sync_notifier::SyncNotifierObserver {
   DISALLOW_COPY_AND_ASSIGN(NotificationPrinter);
 };
 
-class NullInvalidationVersionTracker
-    : public base::SupportsWeakPtr<NullInvalidationVersionTracker>,
-      public sync_notifier::InvalidationVersionTracker {
+class NullInvalidationStateTracker
+    : public base::SupportsWeakPtr<NullInvalidationStateTracker>,
+      public sync_notifier::InvalidationStateTracker {
  public:
-  NullInvalidationVersionTracker() {}
-  virtual ~NullInvalidationVersionTracker() {}
+  NullInvalidationStateTracker() {}
+  virtual ~NullInvalidationStateTracker() {}
 
   virtual sync_notifier::InvalidationVersionMap
       GetAllMaxVersions() const OVERRIDE {
@@ -172,10 +172,10 @@ int main(int argc, char* argv[]) {
           command_line,
           new TestURLRequestContextGetter(io_thread.message_loop_proxy()));
   const char kClientInfo[] = "sync_listen_notifications";
-  NullInvalidationVersionTracker null_invalidation_version_tracker;
+  NullInvalidationStateTracker null_invalidation_state_tracker;
   sync_notifier::SyncNotifierFactory sync_notifier_factory(
       notifier_options, kClientInfo,
-      null_invalidation_version_tracker.AsWeakPtr());
+      null_invalidation_state_tracker.AsWeakPtr());
   scoped_ptr<sync_notifier::SyncNotifier> sync_notifier(
       sync_notifier_factory.CreateSyncNotifier());
   NotificationPrinter notification_printer;
