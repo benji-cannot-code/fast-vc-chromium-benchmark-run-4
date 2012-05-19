d3742867bc0fe7a82830fd0a9d7533b97be7ddcb
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-19 01:45:23 +0000

    Revert r137984 "Fix for broken CertificateViewerUITest.testDetails."
    TBR=kgr@chromium.org
    BUG=127732
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10407051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138009 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/certificate_viewer_dialog_test.js
+++ chrome/test/data/webui/certificate_viewer_dialog_test.js
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Test fixture for generated tests.
  * @extends {testing.Test}
  */
-function CertificateViewerUITest() {}
+function CertificateViewerUITest() {};
 
 CertificateViewerUITest.prototype = {
   __proto__: testing.Test.prototype,
@@ -32,7 +32,7 @@ CertificateViewerUITest.prototype = {
  * Test fixture for asynchronous tests.
  * @extends {CertificateViewerUITest}
  */
-function CertificateViewerUITestAsync() {}
+function CertificateViewerUITestAsync() {};
 
 CertificateViewerUITestAsync.prototype = {
   __proto__: CertificateViewerUITest.prototype,
@@ -45,7 +45,7 @@ CertificateViewerUITestAsync.prototype = {
 // Certificate viewer UI tests are disabled on platforms with native certificate
 // viewers.
 GEN('#include "chrome/test/data/webui/certificate_viewer_ui_test-inl.h"');
-GEN('');
+GEN('')
 
 // Constructors and destructors must be provided in .cc to prevent clang errors.
 GEN('CertificateViewerUITest::CertificateViewerUITest() {}');
@@ -69,8 +69,9 @@ TEST_F('CertificateViewerUITest', 'testCN', function() {
  * Test the details pane of the certificate viewer. This verifies that a
  * certificate in the chain can be selected to view the fields. And that fields
  * can be selected to view their values.
+ * TODO(flackr,kgr): Re-enable this (http://crbug.com/127732).
  */
-TEST_F('CertificateViewerUITestAsync', 'testDetails', function() {
+TEST_F('CertificateViewerUITestAsync', 'DISABLED_testDetails', function() {
   var certHierarchy = $('hierarchy');
   var certFields = $('cert-fields');
   var certFieldVal = $('cert-field-value');
@@ -78,13 +79,9 @@ TEST_F('CertificateViewerUITestAsync', 'testDetails', function() {
   // There must be at least one certificate in the hierarchy.
   assertLT(0, certHierarchy.childNodes.length);
 
-  // Select the second tab, causing its data to be loaded if needed.
-  $('tabbox').selectedIndex = 1;
-
   // Select the first certificate on the chain and ensure the details show up.
   // Override the receive certificate function to catch when fields are
   // loaded.
-  certHierarchy.selectedItem = null;
   var getCertificateFields = cert_viewer.getCertificateFields;
   cert_viewer.getCertificateFields = this.continueTest(WhenTestDone.ALWAYS,
       function(certFieldDetails) {
