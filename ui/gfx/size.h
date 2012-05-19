24aed84af87491b19667f00c976efe09a38b6992
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-19 02:12:02 +0000

    Add instantiation declarations for PointBase, SizeBase, RectBase
    These templates have explicit instantiations in point.cc, point_f.cc, size.cc,
    size_f.cc, rect.cc, rect_f.cc. This suggests that the intent was to have these
    instantiations in a single translation unit. Without an explicit instantiation
    declaration, they would still be weakly emitted to all translation units that
    use point.h (etc). Add an explicit instantiation declaration to stop this from
    happening.
    
    Thanks to Rafael Espindola for help with this patch.
    
    BUG=90078
    TEST=do a components debug build on mac. observe fewer linker errors.
    Normal builds get a bit faster too.
    
    Review URL: https://chromiumcodereview.appspot.com/10386220
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138012 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/size.h
+++ ui/gfx/size.h
@@ -45,6 +45,10 @@ class UI_EXPORT Size : public SizeBase<Size, int> {
   std::string ToString() const;
 };
 
+#if !defined(COMPILER_MSVC)
+extern template class SizeBase<Size, int>;
+#endif
+
 }  // namespace gfx
 
 #endif  // UI_GFX_SIZE_H_
