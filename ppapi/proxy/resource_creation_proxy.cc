1c824d4c1dc546f98a66ed57dcee35f1f225b22b
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-19 01:39:05 +0000

    Get PPB_Audio interface building as untrusted code.
    Add audio source files needed for PPB_Audio to build, and #ifdef out
    host side code. Flesh out the SyncSocket and CancelableSyncSocket stubs
    a bit more to compile and link.
    
    BUG=116317
    TEST=compiles
    
    Review URL: https://chromiumcodereview.appspot.com/10412014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138007 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/resource_creation_proxy.cc
+++ ppapi/proxy/resource_creation_proxy.cc
@@ -173,7 +173,6 @@ PP_Resource ResourceCreationProxy::CreateWheelInputEvent(
                                     instance, data))->GetReference();
 }
 
-#if !defined(OS_NACL)
 PP_Resource ResourceCreationProxy::CreateAudio(
     PP_Instance instance,
     PP_Resource config_id,
@@ -191,8 +190,9 @@ PP_Resource ResourceCreationProxy::CreateAudioConfig(
       OBJECT_IS_PROXY, instance, sample_rate, sample_frame_count);
 }
 
+#if !defined(OS_NACL)
 PP_Resource ResourceCreationProxy::CreateAudioTrusted(PP_Instance instance) {
-  // Proxied plugins can't created trusted audio devices.
+  // Proxied plugins can't create trusted audio devices.
   return 0;
 }
 
