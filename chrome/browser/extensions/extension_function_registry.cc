95a3d9c9e795f967a800e8e0f22f89aab6d10bc4
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-20 06:09:41 +0000

    gdata: Add requestDirectoryRefresh to file_browser_private.
    The new API is used to fetch the metadata of files in a particular
    directory. More specifically, the new API is used to fix the stale
    thumbnail URL issue.
    
    BUG=127697
    TEST=added a unit test.
    
    Review URL: https://chromiumcodereview.appspot.com/10386226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138055 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -383,6 +383,7 @@ void ExtensionFunctionRegistry::ResetFunctions() {
   RegisterFunction<SetGDataPreferencesFunction>();
   RegisterFunction<GetPathForDriveSearchResultFunction>();
   RegisterFunction<GetNetworkConnectionStateFunction>();
+  RegisterFunction<RequestDirectoryRefreshFunction>();
 
   // Mediaplayer
   RegisterFunction<PlayMediaplayerFunction>();
