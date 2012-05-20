32e4f7e8ff550aaf7580d4e417fe5f0585318170
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-20 02:33:31 +0000

    Fix for GLInProcessContext
    TEST=DRT no longer crashes
    BUG=none
    TBR=apatrick@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10407055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138049 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
+++ webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.cc
@@ -314,6 +314,9 @@ GLInProcessContext::Error GLInProcessContext::GetError() {
 }
 
 bool GLInProcessContext::IsCommandBufferContextLost() {
+  if (!command_buffer_.get()) {
+    return true;
+  }
   CommandBuffer::State state = command_buffer_->GetState();
   return state.error == ::gpu::error::kLostContext;
 }
@@ -511,7 +514,7 @@ void GLInProcessContext::Destroy() {
   command_buffer_.reset();
 
   if (decoder_.get()) {
-    decoder_->Destroy(context_lost);
+    decoder_->Destroy(!context_lost);
   }
 }
 
