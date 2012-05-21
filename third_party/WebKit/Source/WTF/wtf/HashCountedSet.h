8e78fd51667288857beba8efd85c064168f3f6fb
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 08:14:46 +0000

    REGRESSION(r117501): IconDatabase asserts on startup in synchronousIconForPageURL(). <http://webkit.org/b/86935> <rdar://problem/11480012>
    Reviewed by Anders Carlsson.
    
    Source/WebCore:
    
    - Correctly set m_retainOrReleaseIconRequested to true in retainIconForPageURL().
      This was causing the assertions, as we would end up doing nothing until the first
      icon release request came in.
    
    - Require that m_urlsToRetainOrReleaseLock be held when accessing m_retainOrReleaseIconRequested.
      This removes a possible race condition in double checked locking.
    
    - Swap over the retain/release work queues while holding m_urlsToRetainOrReleaseLock
      and release it right away to avoid sitting on the lock while updating the database.
    
    * loader/icon/IconDatabase.cpp:
    (WebCore::IconDatabase::synchronousIconForPageURL):
    (WebCore::IconDatabase::retainIconForPageURL):
    (WebCore::IconDatabase::releaseIconForPageURL):
    (WebCore::IconDatabase::retainedPageURLCount):
    (WebCore::IconDatabase::performURLImport):
    (WebCore::IconDatabase::syncThreadMainLoop):
    (WebCore::IconDatabase::performPendingRetainAndReleaseOperations):
    * loader/icon/IconDatabase.h:
    (IconDatabase):
    
    Source/WTF:
    
    Added a swap() to HashCountedSet.
    
    * wtf/HashCountedSet.h:
    (HashCountedSet::swap):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117744 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/HashCountedSet.h
+++ third_party/WebKit/Source/WTF/wtf/HashCountedSet.h
@@ -40,6 +40,8 @@ namespace WTF {
         typedef typename ImplType::AddResult AddResult;
         
         HashCountedSet() {}
+
+        void swap(HashCountedSet&);
         
         int size() const;
         int capacity() const;
@@ -76,6 +78,12 @@ namespace WTF {
     private:
         ImplType m_impl;
     };
+
+    template<typename Value, typename HashFunctions, typename Traits>
+    inline void HashCountedSet<Value, HashFunctions, Traits>::swap(HashCountedSet& other)
+    {
+        m_impl.swap(other.m_impl);
+    }
     
     template<typename Value, typename HashFunctions, typename Traits>
     inline int HashCountedSet<Value, HashFunctions, Traits>::size() const
--- third_party/WebKit/Source/WebCore/loader/icon/IconDatabase.h
+++ third_party/WebKit/Source/WebCore/loader/icon/IconDatabase.h
@@ -166,7 +166,6 @@ private:
     bool m_iconURLImportComplete;
     bool m_syncThreadHasWorkToDo;
     bool m_disabledSuddenTerminationForSyncThread;
-    bool m_retainOrReleaseIconRequested;
 
     Mutex m_urlAndIconLock;
     // Holding m_urlAndIconLock is required when accessing any of the following data structures or the objects they contain
@@ -189,6 +188,7 @@ private:
     // Holding m_urlsToRetainOrReleaseLock is required when accessing any of the following data structures.
     HashCountedSet<String> m_urlsToRetain;
     HashCountedSet<String> m_urlsToRelease;
+    bool m_retainOrReleaseIconRequested;
 
 // *** Sync Thread Only ***
 public:
