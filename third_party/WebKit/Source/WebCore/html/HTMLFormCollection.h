64d2017e95963ae2c7281eb87cdbec189d07e1d5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 10:29:37 +0000

    [Forms] Refactor HTMLFormCollection https://bugs.webkit.org/show_bug.cgi?id=86602
    Patch by Rakesh KN <rakesh.kn@motorola.com> on 2012-05-21
    Reviewed by Kent Tamura.
    
    Modify HTMLFormCollection to be independent of HTMLFormElement which is needed
    for implementing HTMLFieldSetElement's element attribute.
    
    Covered by existing tests.
    
    * html/HTMLFormCollection.cpp:
    (WebCore::HTMLFormCollection::HTMLFormCollection):
    Modified to take more generic HTMLElement* instead of HTMLFormElement* so that
    HTMLFormCollection for HTMLFieldSetElement can also be created.
    (WebCore::HTMLFormCollection::create): Ditto.
    (WebCore::HTMLFormCollection::formControlElements):
    Helper function for getting the array of FormAssociatedElements for this form.
    (WebCore::HTMLFormCollection::formImageElements):
    Helper function for getting the array of image elements for this form.
    (WebCore::HTMLFormCollection::numberOfFormControlElements):
    Helper function for getting the number of elements in this form.
    (WebCore::HTMLFormCollection::calcLength):
    Modified to use new helper functions defined for getting the FormAssociatedElements
    and image elements array instead of static_cast to HTMLFormElement.
    (WebCore::HTMLFormCollection::item): Ditto.
    (WebCore::HTMLFormCollection::getNamedFormItem): Ditto.
    (WebCore::HTMLFormCollection::updateNameCache): Ditto.
    * html/HTMLFormCollection.h:
    (WebCore):
    (HTMLFormCollection):
    * html/HTMLFormElement.h:
    (WebCore::HTMLFormElement::imageElements):
    New accessor for image elements array of form element.
    Also HTMLFormCollection is not needed to be friend of HTMLFormElement as collection
    does not access the form element memebers directly now.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFormCollection.h
+++ third_party/WebKit/Source/WebCore/html/HTMLFormCollection.h
@@ -28,15 +28,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class HTMLFormElement;
+class FormAssociatedElement;
+class HTMLElement;
+class HTMLImageElement;
 class QualifiedName;
-
 // This class is just a big hack to find form elements even in malformed HTML elements.
 // The famous <table><tr><form><td> problem.
 
 class HTMLFormCollection : public HTMLCollection {
 public:
-    static PassOwnPtr<HTMLFormCollection> create(HTMLFormElement*);
+    static PassOwnPtr<HTMLFormCollection> create(HTMLElement*);
 
     virtual ~HTMLFormCollection();
 
@@ -46,13 +47,17 @@ public:
     virtual Node* namedItem(const AtomicString& name) const;
 
 private:
-    HTMLFormCollection(HTMLFormElement*);
+    HTMLFormCollection(HTMLElement*);
 
     virtual void updateNameCache() const;
     virtual unsigned calcLength() const;
 
     Element* getNamedItem(const QualifiedName& attrName, const AtomicString& name) const;
     Element* getNamedFormItem(const QualifiedName& attrName, const String& name, int duplicateNumber) const;
+
+    const Vector<FormAssociatedElement*>& formControlElements() const;
+    const Vector<HTMLImageElement*>& formImageElements() const;
+    unsigned numberOfFormControlElements() const;
 
     mutable int currentPos;
 };
