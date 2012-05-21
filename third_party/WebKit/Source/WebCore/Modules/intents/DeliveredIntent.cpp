57ac38d0fe465d496d250a173f0ce948ab2ade44
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 16:24:06 +0000

    Add suggestions field to web intents API. https://bugs.webkit.org/show_bug.cgi?id=86791
    Patch by Greg Billock <gbillock@google.com> on 2012-05-21
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    The |suggestions| field is used by the client to avoid an empty
    web intents selection window (picker). The UA can add the given
    suggestions to the picker if it would otherwise be empty. See
    http://dvcs.w3.org/hg/web-intents/raw-file/tip/spec/Overview.html#intent-parameters-dictionary
    
    * Modules/intents/DeliveredIntent.cpp:
    (WebCore::DeliveredIntent::DeliveredIntent):
    * Modules/intents/Intent.cpp:
    (WebCore::Intent::create):
    (WebCore::Intent::Intent):
    * Modules/intents/Intent.h:
    (WebCore::Intent::suggestions):
    (Intent):
    
    Source/WebKit/chromium:
    
    * public/WebIntent.h:
    (WebIntent):
    * src/WebIntent.cpp:
    (WebKit::WebIntent::suggestions):
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/intents/DeliveredIntent.cpp
+++ third_party/WebKit/Source/WebCore/Modules/intents/DeliveredIntent.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ExceptionCode.h"
 #include "Frame.h"
 #include "SerializedScriptValue.h"
+#include <wtf/Vector.h>
 
 namespace WebCore {
 
@@ -49,7 +50,7 @@ PassRefPtr<DeliveredIntent> DeliveredIntent::create(Frame* frame, PassOwnPtr<Del
 DeliveredIntent::DeliveredIntent(Frame* frame, PassOwnPtr<DeliveredIntentClient> client, const String& action, const String& type,
                                  PassRefPtr<SerializedScriptValue> data, PassOwnPtr<MessagePortArray> ports,
                                  const HashMap<String, String>& extras)
-    : Intent(action, type, data, PassOwnPtr<MessagePortChannelArray>(), extras, KURL())
+    : Intent(action, type, data, PassOwnPtr<MessagePortChannelArray>(), extras, KURL(), Vector<KURL>())
     , FrameDestructionObserver(frame)
     , m_client(client)
     , m_ports(ports)
