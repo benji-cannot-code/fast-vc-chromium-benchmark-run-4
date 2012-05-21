78a1f48cf20c710d0b9d48ef01cb41c0c414cef6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 16:06:42 +0000

    webkitdirs.pm: Fix undefined CXXFLAGS variable https://bugs.webkit.org/show_bug.cgi?id=87000
    Patch by Thiago Marcos P. Santos <thiago.santos@intel.com> on 2012-05-21
    Reviewed by Martin Robinson.
    
    Fixes a warning on Autotools and CMake buildsystem when
    CXXFLAGS is not defined.
    
    * Scripts/webkitdirs.pm:
    (runAutogenForAutotoolsProjectIfNecessary):
    (generateBuildSystemFromCMakeProject):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117787 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -1870,7 +1870,7 @@ sub runAutogenForAutotoolsProjectIfNecessary($@)
     # used on Chromium build.
     determineArchitecture();
     if ($architecture ne "x86_64" && !isARM()) {
-        $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse " . $ENV{'CXXFLAGS'};
+        $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse " . ($ENV{'CXXFLAGS'} || "");
     }
 
     # Prefix the command with jhbuild run.
@@ -2094,7 +2094,7 @@ sub generateBuildSystemFromCMakeProject
     # between 32-bit and 64-bit architectures.
     determineArchitecture();
     if ($architecture ne "x86_64" && !isARM()) {
-        $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse " . $ENV{'CXXFLAGS'};
+        $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse " . ($ENV{'CXXFLAGS'} || "");
     }
 
     # We call system("cmake @args") instead of system("cmake", @args) so that @args is
