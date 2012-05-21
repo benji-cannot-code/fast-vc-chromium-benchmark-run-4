39af94d8990d7f96d2768f45d8a3cf33a9697e17
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 07:11:00 +0000

    [EFL] Minor style fixes in web intents code are needed https://bugs.webkit.org/show_bug.cgi?id=86986
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-05-21
    Reviewed by Adam Barth.
    
    Source/WebKit:
    
    Fix minor ordering issues in CMake files.
    
    * CMakeLists.txt:
    * PlatformEfl.cmake:
    
    Source/WebKit/efl:
    
    * ewk/EWebKit.h: Add new intent headers.
    * ewk/ewk_frame.h: Add missing underscores to EwkIntentRequest type.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117737 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/EWebKit.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/EWebKit.h
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ewk_cookies.h"
 #include "ewk_frame.h"
 #include "ewk_history.h"
+#include "ewk_intent.h"
+#include "ewk_intent_request.h"
 #include "ewk_js.h"
 #include "ewk_main.h"
 #include "ewk_network.h"
