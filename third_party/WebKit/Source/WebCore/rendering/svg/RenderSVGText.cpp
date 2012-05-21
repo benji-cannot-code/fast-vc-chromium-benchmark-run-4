5b395ca5f72f305af5a4a6b57a1aba5c26e71027
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 16:12:10 +0000

    [Chromium] REGRESSION: Assertion failure on svg/custom/acid3-test-77.html https://bugs.webkit.org/show_bug.cgi?id=86715
    Reviewed by Nikolas Zimmermann.
    
    Source/WebCore: 
    
    Calls to clear the text positioning elements in RenderSVGText must be
    protected with a FontCachePurgeProtector so that fonts are not removed
    before the SVG metrics are rebuilt. The protection must cover both the
    cache clearing and the reconstruction calls.
    
    Patch fixes existing tests.
    
    * rendering/svg/RenderSVGText.cpp:
    (WebCore::RenderSVGText::subtreeChildWasAdded):
    (WebCore::RenderSVGText::subtreeChildWasRemoved):
    (WebCore::RenderSVGText::subtreeTextDidChange):
    (WebCore::RenderSVGText::removeChild):
    
    LayoutTests: 
    
    * platform/chromium/test_expectations.txt: Removing expectation for acid3 test.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGText.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGText.cpp
@@ -172,6 +172,9 @@ void RenderSVGText::subtreeChildWasAdded(RenderObject* child)
     if (!shouldHandleSubtreeMutations() || documentBeingDestroyed())
         return;
 
+    // Always protect the cache before clearing text positioning elements when the cache will subsequently be rebuilt.
+    FontCachePurgePreventer fontCachePurgePreventer;
+
     // The positioning elements cache doesn't include the new 'child' yet. Clear the
     // cache, as the next buildLayoutAttributesForTextRenderer() call rebuilds it.
     m_layoutAttributesBuilder.clearTextPositioningElements();
@@ -306,6 +309,9 @@ void RenderSVGText::subtreeTextDidChange(RenderSVGInlineText* text)
         ASSERT(!m_layoutAttributesBuilder.numberOfTextPositioningElements());
         return;
     }
+
+    // Always protect the cache before clearing text positioning elements when the cache will subsequently be rebuilt.
+    FontCachePurgePreventer fontCachePurgePreventer;
 
     // The positioning elements cache depends on the size of each text renderer in the
     // subtree. If this changes, clear the cache. It's going to be rebuilt below.
@@ -527,6 +533,7 @@ void RenderSVGText::removeChild(RenderObject* child)
 
     RenderSVGInlineText* text = toRenderSVGInlineText(child);
     Vector<SVGTextLayoutAttributes*, 2> affectedAttributes;
+    FontCachePurgePreventer fontCachePurgePreventer;
     subtreeChildWillBeRemoved(text, affectedAttributes);
     RenderSVGBlock::removeChild(child);
     subtreeChildWasRemoved(affectedAttributes);
