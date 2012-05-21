64fd1ab0adcec3381951b1dfcc60d68ee71ea01f
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 06:28:54 +0000

    [V8] Pass Isolate to throwError()s in V8 custom bindings (Part 1) https://bugs.webkit.org/show_bug.cgi?id=86980
    Reviewed by Adam Barth.
    
    The objective is to pass Isolate around in V8 bindings.
    This patch passes Isolate to throwError()s in V8 custom bindings.
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8ArrayBufferCustom.cpp:
    (WebCore::V8ArrayBuffer::constructorCallback):
    * bindings/v8/custom/V8ArrayBufferViewCustom.h:
    (WebCore::constructWebGLArrayWithArrayBufferArgument):
    (WebCore::constructWebGLArray):
    * bindings/v8/custom/V8AudioContextCustom.cpp:
    (WebCore::V8AudioContext::constructorCallback):
    * bindings/v8/custom/V8BlobCustom.cpp:
    (WebCore::V8Blob::constructorCallback):
    * bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp:
    (WebCore::V8CSSStyleDeclaration::namedPropertySetter):
    * bindings/v8/custom/V8ClipboardCustom.cpp:
    (WebCore::V8Clipboard::clearDataCallback):
    (WebCore::V8Clipboard::setDragImageCallback):
    * bindings/v8/custom/V8DOMFormDataCustom.cpp:
    (WebCore::V8DOMFormData::appendCallback):
    * bindings/v8/custom/V8DOMStringMapCustom.cpp:
    (WebCore::V8DOMStringMap::namedPropertySetter):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8DedicatedWorkerContextCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8DirectoryEntryCustom.cpp:
    (WebCore::V8DirectoryEntry::getDirectoryCallback):
    (WebCore::V8DirectoryEntry::getFileCallback):
    * bindings/v8/custom/V8DocumentCustom.cpp:
    (WebCore::V8Document::evaluateCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8BlobCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8BlobCustom.cpp
@@ -69,7 +69,7 @@ v8::Handle<v8::Value> V8Blob::constructorCallback(const v8::Arguments& args)
     // Get the script execution context.
     ScriptExecutionContext* context = getScriptExecutionContext();
     if (!context)
-        return V8Proxy::throwError(V8Proxy::ReferenceError, "Blob constructor associated document is unavailable");
+        return V8Proxy::throwError(V8Proxy::ReferenceError, "Blob constructor associated document is unavailable", args.GetIsolate());
 
     if (!args.Length()) {
         RefPtr<Blob> blob = Blob::create();
@@ -92,7 +92,7 @@ v8::Handle<v8::Value> V8Blob::constructorCallback(const v8::Arguments& args)
         v8::TryCatch tryCatchEndings;
         bool containsEndings = dictionary.get("endings", endings);
         if (tryCatchEndings.HasCaught())
-            return throwError(tryCatchEndings.Exception());
+            return throwError(tryCatchEndings.Exception(), args.GetIsolate());
 
         if (containsEndings) {
             if (endings != "transparent" && endings != "native")
@@ -102,7 +102,7 @@ v8::Handle<v8::Value> V8Blob::constructorCallback(const v8::Arguments& args)
         v8::TryCatch tryCatchType;
         dictionary.get("type", type);
         if (tryCatchType.HasCaught())
-            return throwError(tryCatchType.Exception());
+            return throwError(tryCatchType.Exception(), args.GetIsolate());
     }
 
     ASSERT(endings == "transparent" || endings == "native");
