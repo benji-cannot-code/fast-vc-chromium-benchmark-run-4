ec511296953e04f8e5c7ba9fb0d2d13c75e6fb62
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-21 13:02:57 +0000

    Web Inspector: declare setFormatted on UISourceCode (otherwise fails for styles).
    Not reviewed: front-end fix as a follow up to r117751.
    
    * inspector/front-end/UISourceCode.js:
    (WebInspector.UISourceCode.prototype.consoleMessagesCleared):
    (WebInspector.UISourceCode.prototype.setFormatted):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/UISourceCode.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/UISourceCode.js
@@ -307,6 +307,16 @@ WebInspector.UISourceCode.prototype = {
     {
         this._consoleMessages = [];
         this.dispatchEventToListeners(WebInspector.UISourceCode.Events.ConsoleMessagesCleared);
+    },
+
+    /**
+     * @param {boolean} formatted
+     * @param {function()=} callback
+     */
+    setFormatted: function(formatted, callback)
+    {
+        if (callback)
+            callback();
     }
 }
 
