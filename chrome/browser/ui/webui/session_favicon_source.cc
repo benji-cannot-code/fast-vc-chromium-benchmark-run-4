7b5afd769d5d05c7e10d4397262e0c4e3185d585
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-21 18:03:12 +0000

    NTP: Prevent tab sync icons from being cached.
    BUG=127463
    TEST=Manual.
    
    Review URL: https://chromiumcodereview.appspot.com/10407066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138112 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/session_favicon_source.cc
+++ chrome/browser/ui/webui/session_favicon_source.cc
@@ -50,3 +50,9 @@ bool SessionFaviconSource::ShouldReplaceExistingSource() const {
   // requests on the floor.
   return false;
 }
+
+bool SessionFaviconSource::AllowCaching() const {
+  // Prevent responses from being cached, otherwise session favicons won't
+  // update in a timely manner.
+  return false;
+}
