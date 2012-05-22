81303d8c76886d099409ed48baa5b6ed797c6ee4
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 20:53:16 +0000

    Don't pass class types across EXE/DLL boundaries for SetCommandLine() and SetExperimentList().
    BUG=128541
    
    Review URL: https://chromiumcodereview.appspot.com/10411059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/child_process_logging_win.cc
+++ chrome/common/child_process_logging_win.cc
@@ -18,6 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 
 namespace child_process_logging {
+
+namespace {
+
 // exported in breakpad_win.cc: void __declspec(dllexport) __cdecl SetActiveURL.
 typedef void (__cdecl *MainSetActiveURL)(const wchar_t*);
 
@@ -47,11 +50,22 @@ typedef void (__cdecl *MainSetNumberOfViews)(int);
 
 // exported in breakpad_win.cc:
 //   void __declspec(dllexport) __cdecl SetCommandLine
-typedef void (__cdecl *MainSetCommandLine)(const CommandLine*);
+typedef void (__cdecl *MainSetCommandLine)(const wchar_t**, size_t);
 
 // exported in breakpad_field_trial_win.cc:
 //   void __declspec(dllexport) __cdecl SetExperimentList
-typedef void (__cdecl *MainSetExperimentList)(const std::vector<string16>&);
+typedef void (__cdecl *MainSetExperimentList)(const wchar_t**, size_t);
+
+// Copied from breakpad_win.cc.
+void StringVectorToCStringVector(const std::vector<std::wstring>& wstrings,
+                                 std::vector<const wchar_t*>* cstrings) {
+  cstrings->clear();
+  cstrings->reserve(wstrings.size());
+  for (size_t i = 0; i < wstrings.size(); ++i)
+    cstrings->push_back(wstrings[i].c_str());
+}
+
+}  // namespace
 
 void SetActiveURL(const GURL& url) {
   static MainSetActiveURL set_active_url = NULL;
@@ -190,7 +204,10 @@ void SetCommandLine(const CommandLine* command_line) {
     if (!set_command_line)
       return;
   }
-  (set_command_line)(command_line);
+
+  std::vector<const wchar_t*> cstrings;
+  StringVectorToCStringVector(command_line->argv(), &cstrings);
+  (set_command_line)(&cstrings[0], cstrings.size());
 }
 
 void SetExperimentList(const std::vector<string16>& state) {
@@ -205,7 +222,10 @@ void SetExperimentList(const std::vector<string16>& state) {
     if (!set_experiment_list)
       return;
   }
-  (set_experiment_list)(state);
+
+  std::vector<const wchar_t*> cstrings;
+  StringVectorToCStringVector(state, &cstrings);
+  (set_experiment_list)(&cstrings[0], cstrings.size());
 }
 
 void SetNumberOfViews(int number_of_views) {
