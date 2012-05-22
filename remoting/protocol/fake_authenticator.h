cb739114c9ceab2be7f1a6aaca2e196e7507dbab
dharani@chromium.org <dharani@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 03:16:02 +0000

    This broke M20 build.
    Revert 137824 - Properly handle accounts that don't have GMail account.
    
    1. Me2MeHostAuthenticatorFactory now verifies that the bare 
    JID of the remote client matches bare JID of the host. 
    Previously it was comparing it with the user's email, 
    which may be different from JID.
    2. GaiaOAuthClient now fetches user's email.
    3. SignalingConnector verifies that user's email matches 
    the expected value stored in xmpp_login. If it doesn't, 
    then the auth token was generated for a different account 
    and the host treats it as an authentication error.
    
    BUG=128102
    
    Review URL: https://chromiumcodereview.appspot.com/10332187
    
    TBR=sergeyu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10388226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138204 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/fake_authenticator.h
+++ remoting/protocol/fake_authenticator.h
@@ -90,7 +90,6 @@ class FakeHostAuthenticatorFactory : public AuthenticatorFactory {
 
   // AuthenticatorFactory interface.
   virtual scoped_ptr<Authenticator> CreateAuthenticator(
-      const std::string& local_jid,
       const std::string& remote_jid,
       const buzz::XmlElement* first_message) OVERRIDE;
 
