3d935213755fd7bd0de954fcd3760ab7f6bb38c4
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 08:46:00 +0000

    GCC 4.7 and C++11 support. https://bugs.webkit.org/show_bug.cgi?id=86465
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-05-22
    Reviewed by Darin Adler.
    
    Source/WTF:
    
    Detect C++11 mode in GCC 4.7 and set appropiate compiler feature flags.
    Turn C++11 override control into a compiler feature flag.
    Fix non-clang support of compiler feature CXX_DELETED_FUNCTIONS.
    
    * wtf/Compiler.h:
    * wtf/Noncopyable.h:
    
    Tools:
    
    Update detection of C++11 mode in GCC.
    
    * qmake/mkspecs/features/unix/default_post.prf:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117934 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Compiler.h
+++ third_party/WebKit/Source/WTF/wtf/Compiler.h
@@ -46,6 +46,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define __has_extension __has_feature /* Compatibility with older versions of clang */
 #endif
 
+#define CLANG_PRAGMA(PRAGMA) _Pragma(PRAGMA)
+
 /* Specific compiler features */
 #define WTF_COMPILER_SUPPORTS_CXX_VARIADIC_TEMPLATES __has_feature(cxx_variadic_templates)
 #define WTF_COMPILER_SUPPORTS_CXX_RVALUE_REFERENCES __has_feature(cxx_rvalue_references)
@@ -53,9 +55,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_COMPILER_SUPPORTS_CXX_NULLPTR __has_feature(cxx_nullptr)
 #define WTF_COMPILER_SUPPORTS_BLOCKS __has_feature(blocks)
 #define WTF_COMPILER_SUPPORTS_C_STATIC_ASSERT __has_extension(c_static_assert)
+#define WTF_COMPILER_SUPPORTS_CXX_OVERRIDE_CONTROL __has_extension(cxx_override_control)
 
 #define WTF_COMPILER_SUPPORTS_HAS_TRIVIAL_DESTRUCTOR __has_extension(has_trivial_destructor)
 
+#endif
+
+#ifndef CLANG_PRAGMA
+#define CLANG_PRAGMA(PRAGMA)
 #endif
 
 /* COMPILER(MSVC) - Microsoft Visual C++ */
@@ -72,6 +79,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* Specific compiler features */
 #if !COMPILER(CLANG) && _MSC_VER >= 1600
 #define WTF_COMPILER_SUPPORTS_CXX_NULLPTR 1
+#endif
+
+#if !COMPILER(CLANG)
+#define WTF_COMPILER_SUPPORTS_CXX_OVERRIDE_CONTROL 1
+#define WTF_COMPILER_QUIRK_FINAL_IS_CALLED_SEALED 1
 #endif
 
 #endif
@@ -107,7 +119,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* Specific compiler features */
 #if COMPILER(GCC) && !COMPILER(CLANG)
 #if GCC_VERSION_AT_LEAST(4, 7, 0) && __cplusplus >= 201103L
+#define WTF_COMPILER_SUPPORTS_CXX_RVALUE_REFERENCES 1
+#define WTF_COMPILER_SUPPORTS_CXX_DELETED_FUNCTIONS 1
 #define WTF_COMPILER_SUPPORTS_CXX_NULLPTR 1
+#define WTF_COMPILER_SUPPORTS_CXX_OVERRIDE_CONTROL 1
 #define WTF_COMPILER_QUIRK_GCC11_GLOBAL_ISINF_ISNAN 1
 
 #elif GCC_VERSION_AT_LEAST(4, 6, 0) && defined(__GXX_EXPERIMENTAL_CXX0X__)
@@ -221,35 +236,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WARN_UNUSED_RETURN
 #endif
 
-/* OVERRIDE */
+/* OVERRIDE and FINAL */
 
-#ifndef OVERRIDE
-#if COMPILER(CLANG)
-#if __has_extension(cxx_override_control)
+#if COMPILER_SUPPORTS(CXX_OVERRIDE_CONTROL)
 #define OVERRIDE override
-#endif
-#elif COMPILER(MSVC)
-#define OVERRIDE override
-#endif
-#endif
 
-#ifndef OVERRIDE
-#define OVERRIDE
-#endif
-
-/* FINAL */
-
-#ifndef FINAL
-#if COMPILER(CLANG)
-#if __has_extension(cxx_override_control)
+#if COMPILER_QUIRK(FINAL_IS_CALLED_SEALED)
+#define FINAL sealed
+#else
 #define FINAL final
 #endif
-#elif COMPILER(MSVC)
-#define FINAL sealed
-#endif
-#endif
 
-#ifndef FINAL
+#else
+#define OVERRIDE
 #define FINAL
 #endif
 
--- third_party/WebKit/Source/WTF/wtf/Noncopyable.h
+++ third_party/WebKit/Source/WTF/wtf/Noncopyable.h
@@ -26,13 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if COMPILER_SUPPORTS(CXX_DELETED_FUNCTIONS)
     #define WTF_MAKE_NONCOPYABLE(ClassName) \
-        _Pragma("clang diagnostic push") \
-        _Pragma("clang diagnostic ignored \"-Wunknown-pragmas\"") \
-        _Pragma("clang diagnostic ignored \"-Wc++0x-extensions\"") \
+        CLANG_PRAGMA("clang diagnostic push") \
+        CLANG_PRAGMA("clang diagnostic ignored \"-Wunknown-pragmas\"") \
+        CLANG_PRAGMA("clang diagnostic ignored \"-Wc++0x-extensions\"") \
         private: \
             ClassName(const ClassName&) = delete; \
             ClassName& operator=(const ClassName&) = delete; \
-        _Pragma("clang diagnostic pop")
+        CLANG_PRAGMA("clang diagnostic pop")
 #else
     #define WTF_MAKE_NONCOPYABLE(ClassName) \
         private: \
