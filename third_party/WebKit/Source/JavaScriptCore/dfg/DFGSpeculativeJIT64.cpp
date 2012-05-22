9670eb41f53661d292989327e8dbee4c6813e5d3
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 00:49:31 +0000

    Cleanup of Calls to operationStrCat and operationNewArray and Use Constructor after r117729 https://bugs.webkit.org/show_bug.cgi?id=87027
    Reviewed by Oliver Hunt.
    
    Change calls to operationStrCat and operationNewArray to provide the
    pointer to the EncodedJSValue* data buffer instead of the ScratchBuffer
    that contains it.  Added a ScratchBuffer::create() function.
    This is a clean-up to r117729.
    
    * dfg/DFGOperations.cpp:
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * runtime/JSGlobalData.h:
    (JSC::ScratchBuffer::create):
    (JSC::ScratchBuffer::dataBuffer):
    (JSC::JSGlobalData::scratchBufferForSize):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT64.cpp
@@ -2980,7 +2980,7 @@ void SpeculativeJIT::compile(Node& node)
 
         GPRResult result(this);
         
-        callOperation(op == StrCat ? operationStrCat : operationNewArray, result.gpr(), static_cast<void *>(scratchBuffer), node.numChildren());
+        callOperation(op == StrCat ? operationStrCat : operationNewArray, result.gpr(), static_cast<void *>(buffer), node.numChildren());
 
         if (scratchSize) {
             GPRTemporary scratch(this);
