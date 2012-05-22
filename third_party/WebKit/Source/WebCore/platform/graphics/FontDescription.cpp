5ad03f961e6a9fd1f940aa7be7a5323b294928fc
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 01:53:10 +0000

    Shrink FontDescription object size https://bugs.webkit.org/show_bug.cgi?id=86950
    Reviewed by Alexey Proskuryakov.
    
    Reduce object size of FontDescription by 8 bytes on 64-bit, 4 bytes on 32-bit systems.
    Note that this means Font, StyleInheritedData and RenderSVGInlineText shrink too.
    
    * platform/graphics/FontDescription.cpp:
    * platform/graphics/FontDescription.h:
    (WebCore::FontDescription::script):
    (FontDescription):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/FontDescription.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/FontDescription.cpp
@@ -35,7 +35,6 @@ namespace WebCore {
 
 struct SameSizeAsFontDescription {
     FontFamily familyList;
-    void* settings;
     RefPtr<FontFeatureSettings> m_featureSettings;
     float sizes[2];
     // FXIME: Make them fit into one word.
