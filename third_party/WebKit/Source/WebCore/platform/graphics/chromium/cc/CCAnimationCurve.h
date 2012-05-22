1cbf3e596e5f5cab9f9bf7cd4be5f42bee4740a5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 17:04:23 +0000

    add OVERRIDE annotations to classes derived from CCAnimationCurve https://bugs.webkit.org/show_bug.cgi?id=87073
    Patch by Lu Guanqun <guanqun.lu@intel.com> on 2012-05-22
    Reviewed by James Robinson.
    
    No new tests required.
    
    * platform/graphics/chromium/cc/CCAnimationCurve.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCAnimationCurve.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCAnimationCurve.h
@@ -59,7 +59,7 @@ public:
     virtual float getValue(double t) const = 0;
 
     // Partial CCAnimation implementation.
-    virtual Type type() const { return Float; }
+    virtual Type type() const OVERRIDE { return Float; }
 };
 
 class CCTransformAnimationCurve : public CCAnimationCurve {
@@ -69,7 +69,7 @@ public:
     virtual TransformationMatrix getValue(double t, const IntSize& layerSize) const = 0;
 
     // Partial CCAnimation implementation.
-    virtual Type type() const { return Transform; }
+    virtual Type type() const OVERRIDE { return Transform; }
 };
 
 } // namespace WebCore
