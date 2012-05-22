35c487e8905344b6a7ddaa7ba442e1094d73321b
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 22:58:53 +0000

    [V8] Replace 'throwTypeError(); return Undefined();' with 'return throwTypeError();' https://bugs.webkit.org/show_bug.cgi?id=87106
    Reviewed by Adam Barth.
    
    This patch replaces
    
        throwTypeError();
        return v8::Undefined();
    
    with
    
        return throwTypeError();
    
    No tests. No change in behavior.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateOverloadedFunctionCallback):
    (GenerateParametersCheck):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::overloadedMethodCallback):
    (WebCore::TestObjV8Internal::overloadedMethod1Callback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
@@ -1814,8 +1814,7 @@ static v8::Handle<v8::Value> overloadedMethodCallback(const v8::Arguments& args)
         return overloadedMethod6Callback(args);
     if ((args.Length() == 1 && (args[0]->IsNull() || args[0]->IsArray())))
         return overloadedMethod7Callback(args);
-    V8Proxy::throwTypeError(0, args.GetIsolate());
-    return v8::Handle<v8::Value>();
+    return V8Proxy::throwTypeError(0, args.GetIsolate());
 }
 
 static v8::Handle<v8::Value> classMethodCallback(const v8::Arguments& args)
@@ -1872,8 +1871,7 @@ static v8::Handle<v8::Value> overloadedMethod1Callback(const v8::Arguments& args
         return overloadedMethod11Callback(args);
     if ((args.Length() == 1 && (args[0]->IsNull() || args[0]->IsUndefined() || args[0]->IsString() || args[0]->IsObject())))
         return overloadedMethod12Callback(args);
-    V8Proxy::throwTypeError(0, args.GetIsolate());
-    return v8::Handle<v8::Value>();
+    return V8Proxy::throwTypeError(0, args.GetIsolate());
 }
 
 #endif // ENABLE(Condition1)
