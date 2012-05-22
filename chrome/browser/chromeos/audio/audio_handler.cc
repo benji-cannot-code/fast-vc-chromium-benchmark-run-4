3871be1781faa231e980b48653df7346f729baac
vollick@google.com <vollick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 18:24:01 +0000

    Revert 138278 - Fix the volume controlling behaviors.
    Based on the description of crbug.com/128644:
    - volume up from muted should be normal up (4%), not 25%
    - but if the volume up happens after the mute button, it should go back to the original volume
    
    BUG=128644
    TEST=browser_test passed, manually checked on lumpy
    
    Review URL: https://chromiumcodereview.appspot.com/10411028
    
    TBR=mukai@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10421010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138299 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/audio/audio_handler.cc
+++ chrome/browser/chromeos/audio/audio_handler.cc
@@ -41,11 +41,7 @@ static AudioHandler* g_audio_handler = NULL;
 // static
 void AudioHandler::Initialize() {
   CHECK(!g_audio_handler);
-#if defined(USE_CRAS)
-  g_audio_handler = new AudioHandler(new AudioMixerCras());
-#else
-  g_audio_handler = new AudioHandler(new AudioMixerAlsa());
-#endif
+  g_audio_handler = new AudioHandler();
 }
 
 // static
@@ -55,12 +51,6 @@ void AudioHandler::Shutdown() {
     delete g_audio_handler;
     g_audio_handler = NULL;
   }
-}
-
-// static
-void AudioHandler::InitializeForTesting(AudioMixer* mixer) {
-  CHECK(!g_audio_handler);
-  g_audio_handler = new AudioHandler(mixer);
 }
 
 // static
@@ -126,8 +116,12 @@ void AudioHandler::RemoveVolumeObserver(VolumeObserver* observer) {
   volume_observers_.RemoveObserver(observer);
 }
 
-AudioHandler::AudioHandler(AudioMixer* mixer)
-    : mixer_(mixer),
+AudioHandler::AudioHandler()
+#if defined(USE_CRAS)
+    : mixer_(new AudioMixerCras()),
+#else
+    : mixer_(new AudioMixerAlsa()),
+#endif
       prefs_(g_browser_process->local_state()) {
   mixer_->SetVolumePercent(prefs_->GetDouble(prefs::kAudioVolumePercent));
   mixer_->SetMuted(prefs_->GetInteger(prefs::kAudioMute) == kPrefMuteOn);
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -2833,7 +2833,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/views/ash/browser_non_client_frame_view_ash_browsertest.cc',
         'browser/ui/views/ash/caps_lock_handler_browsertest.cc',
         'browser/ui/views/ash/launcher/launcher_favicon_loader_browsertest.cc',
-        'browser/ui/views/ash/volume_controller_chromeos_browsertest.cc',
         'browser/ui/views/browser_actions_container_browsertest.cc',
         'browser/ui/views/constrained_window_views_browsertest.cc',
         'browser/ui/views/frame/app_non_client_frame_view_aura_browsertest.cc',
