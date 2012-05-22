49870194610a3cc3659cd942afa1f32b32825acd
zimmermann@webkit.org <zimmermann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 15:41:19 +0000

    2012-05-22  Nikolas Zimmermann  <nzimmermann@rim.com>
            Not reviewed.
    
            Follow-up patch after r117975, as the strict approach fires assertions.
            CSS generated content can violate the ASSERT(child->isSVGInline() || child->isSVGInlineText()) logic.
    
            * rendering/svg/RenderSVGText.cpp:
            (WebCore::RenderSVGText::subtreeChildWasAdded):
            (WebCore::RenderSVGText::subtreeChildWillBeRemoved):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGText.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGText.cpp
@@ -169,7 +169,6 @@ inline bool RenderSVGText::shouldHandleSubtreeMutations() const
 void RenderSVGText::subtreeChildWasAdded(RenderObject* child)
 {
     ASSERT(child);
-    ASSERT(child->isSVGInlineText() || child->isSVGInline());
     if (!shouldHandleSubtreeMutations() || documentBeingDestroyed())
         return;
 
@@ -179,6 +178,9 @@ void RenderSVGText::subtreeChildWasAdded(RenderObject* child)
     // The positioning elements cache doesn't include the new 'child' yet. Clear the
     // cache, as the next buildLayoutAttributesForTextRenderer() call rebuilds it.
     m_layoutAttributesBuilder.clearTextPositioningElements();
+
+    if (!child->isSVGInlineText() && !child->isSVGInline())
+        return;
 
     // Detect changes in layout attributes and only measure those text parts that have changed!
     Vector<SVGTextLayoutAttributes*> newLayoutAttributes;
@@ -242,7 +244,6 @@ void RenderSVGText::willBeDestroyed()
 void RenderSVGText::subtreeChildWillBeRemoved(RenderObject* child, Vector<SVGTextLayoutAttributes*, 2>& affectedAttributes)
 {
     ASSERT(child);
-    ASSERT(child->isSVGInlineText() || child->isSVGInline());
     if (!shouldHandleSubtreeMutations())
         return;
 
@@ -251,7 +252,7 @@ void RenderSVGText::subtreeChildWillBeRemoved(RenderObject* child, Vector<SVGTex
     // The positioning elements cache depends on the size of each text renderer in the
     // subtree. If this changes, clear the cache. It's going to be rebuilt below.
     m_layoutAttributesBuilder.clearTextPositioningElements();
-    if (m_layoutAttributes.isEmpty() || child->isSVGInline())
+    if (m_layoutAttributes.isEmpty() || !child->isSVGInlineText())
         return;
 
     // This logic requires that the 'text' child is still inserted in the tree.
