cfde73b80cf44c1a1e0624ae21901bddf52c6b34
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 20:58:21 +0000

    Use browser::ShowWebDialog() directly in MobileSetup rather than using BrowserList.
    http://crbug.com/129187
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10409068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138355 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/mobile_setup_dialog.cc
+++ chrome/browser/ui/webui/chromeos/mobile_setup_dialog.cc
@@ -9,13 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/platform_util.h"
-#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser_dialogs.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/webui/web_dialog_delegate.h"
 #include "chrome/common/url_constants.h"
+#include "content/public/browser/browser_thread.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+#include "ui/gfx/size.h"
 
 using content::BrowserThread;
 using content::WebContents;
@@ -73,10 +74,10 @@ MobileSetupDialogDelegate::~MobileSetupDialogDelegate() {
 }
 
 void MobileSetupDialogDelegate::ShowDialog() {
-  Browser* browser = BrowserList::GetLastActive();
-  if (!browser)
-    return;
-  browser->BrowserShowWebDialog(this, NULL);
+  browser::ShowWebDialog(NULL,
+                         ProfileManager::GetDefaultProfileOrOffTheRecord(),
+                         NULL,
+                         this);
 }
 
 ui::ModalType MobileSetupDialogDelegate::GetDialogModalType() const {
