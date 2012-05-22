180273a840dde4b0f53e5382a8913d7c9d70cbed
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 16:56:16 +0000

    Use existing model for initial layout, if we can.
    BUG=128880
    TEST=Try to repro the bug (with an empty profile), see it doesn't happen.
    
    Review URL: https://chromiumcodereview.appspot.com/10413044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138283 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/web_intent_picker_cocoa.h
+++ chrome/browser/ui/cocoa/web_intent_picker_cocoa.h
@@ -34,6 +34,8 @@ class WebIntentPickerCocoa : public WebIntentPicker,
 
   void OnSheetDidEnd(NSWindow* sheet);
 
+  WebIntentPickerModel* model() { return model_; }
+
   // WebIntentPickerDelegate forwarding API.
   void OnCancelled();
   void OnServiceChosen(size_t index);
