cd312634ed21ab37b1e5854ca78303cc53825f70
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 16:36:20 +0000

    Aura/ash split: Look for the ScreenPositionClient on the Window, not the RootWindow.
    BUG=125106
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10416017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138273 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -76,6 +76,16 @@ void SetRestoreBounds(aura::Window* window, const gfx::Rect& bounds) {
   window->SetProperty(aura::client::kRestoreBoundsKey, new gfx::Rect(bounds));
 }
 
+void AdjustScreenBounds(aura::Window* window, gfx::Rect* bounds) {
+  aura::client::ScreenPositionClient* screen_position_client =
+      aura::client::GetScreenPositionClient(window);
+  if (screen_position_client) {
+    gfx::Point origin = bounds->origin();
+    screen_position_client->ConvertToScreenPoint(&origin);
+    bounds->set_origin(origin);
+  }
+}
+
 }  // namespace
 
 // Used when SetInactiveRenderingDisabled() is invoked to track when active
@@ -421,15 +431,7 @@ void NativeWidgetAura::InitModalType(ui::ModalType modal_type) {
 
 gfx::Rect NativeWidgetAura::GetWindowScreenBounds() const {
   gfx::Rect bounds = window_->GetBoundsInRootWindow();
-
-  aura::client::ScreenPositionClient* screen_position_client =
-      aura::client::GetScreenPositionClient(window_->GetRootWindow());
-  if (screen_position_client) {
-    gfx::Point origin = bounds.origin();
-    screen_position_client->ConvertToScreenPoint(&origin);
-    bounds.set_origin(origin);
-  }
-
+  AdjustScreenBounds(window_, &bounds);
   return bounds;
 }
 
@@ -437,15 +439,7 @@ gfx::Rect NativeWidgetAura::GetClientAreaScreenBounds() const {
   // View-to-screen coordinate system transformations depend on this returning
   // the full window bounds, for example View::ConvertPointToScreen().
   gfx::Rect bounds = window_->GetBoundsInRootWindow();
-
-  aura::client::ScreenPositionClient* screen_position_client =
-      aura::client::GetScreenPositionClient(window_->GetRootWindow());
-  if (screen_position_client) {
-    gfx::Point origin = bounds.origin();
-    screen_position_client->ConvertToScreenPoint(&origin);
-    bounds.set_origin(origin);
-  }
-
+  AdjustScreenBounds(window_, &bounds);
   return bounds;
 }
 
