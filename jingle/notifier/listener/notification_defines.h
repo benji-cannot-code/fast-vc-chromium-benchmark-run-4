e02e14069194a5b717de6d28388abc3a87ac56a2
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-22 05:25:29 +0000

    Revert 138216 - [Sync] Turn notifier::PushClient into an interface
    Split the previous implementation into two pieces:
    XmppPushClient and NonBlockingPushClient.
    
    Add FakePushClient and FakePushClientObserver.
    
    Remove use of ThreadSafeObserverList.
    
    Add PushClient::CreateDefault() function, which creates a
    NonBlockingPushClient for an XmppPushClient.
    
    Dep-inject PushClient into P2PNotifier.
    
    Add some helper functions to notification_defines.{h,cc}.
    
    BUG=76764
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10413014
    
    TBR=akalin@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10388227
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138221 0039d316-1c4b-4281-b951-d872f2087c98

--- jingle/notifier/listener/notification_defines.h
+++ jingle/notifier/listener/notification_defines.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace notifier {
 
 struct Subscription {
-  Subscription();
-  ~Subscription();
-  bool Equals(const Subscription& other) const;
-
   // The name of the channel to subscribe to; usually but not always
   // a URL.
   std::string channel;
@@ -26,15 +22,8 @@ struct Subscription {
 
 typedef std::vector<Subscription> SubscriptionList;
 
-bool SubscriptionListsEqual(const SubscriptionList& subscriptions1,
-                            const SubscriptionList& subscriptions2);
-
 // A structure representing a <recipient/> block within a push message.
 struct Recipient {
-  Recipient();
-  ~Recipient();
-  bool Equals(const Recipient& other) const;
-
   // The bare jid of the recipient.
   std::string to;
   // User-specific data for the recipient.
@@ -42,9 +31,6 @@ struct Recipient {
 };
 
 typedef std::vector<Recipient> RecipientList;
-
-bool RecipientListsEqual(const RecipientList& recipients1,
-                         const RecipientList& recipients2);
 
 struct Notification {
   Notification();
@@ -57,7 +43,6 @@ struct Notification {
   // The notification data payload.
   std::string data;
 
-  bool Equals(const Notification& other) const;
   std::string ToString() const;
 };
 
