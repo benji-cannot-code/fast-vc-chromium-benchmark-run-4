fff9eccecf37a4f6f23c946e6b393c2e18ab7249
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-22 00:28:58 +0000

    GeneratorGeneratedImage should cache intermediate images https://bugs.webkit.org/show_bug.cgi?id=86906 <rdar://problem/11484852>
    Reviewed by Dean Jackson.
    
    Cache generated images in GeneratorGeneratedImage. The cache is invalidated:
        a) if the Generator's hash changes.
        b) if the rendered size changes.
        c) if the cached image is incompatible with the destination context (acceleration state or CTM scale).
        d) after one second of disuse.
    
    Add a hash() function to Generator, which should be implemented in subclasses
    to provide a straightforward way to determine if Generators have been mutated
    in a way that will affect the resultant rendering.
    
    No new tests, performance optimization. Correctness is covered by the multitude of existing gradient tests.
    
    * platform/graphics/Generator.h:
    (Generator):
    * platform/graphics/GeneratorGeneratedImage.cpp:
    (WebCore::GeneratorGeneratedImage::drawPattern):
    * platform/graphics/GeneratorGeneratedImage.h:
    (WebCore):
    (GeneratorGeneratedImage):
    (WebCore::GeneratorGeneratedImage::~GeneratorGeneratedImage):
    (WebCore::GeneratorGeneratedImage::GeneratorGeneratedImage):
    (GeneratedImageCacheTimer):
    (WebCore::GeneratorGeneratedImage::GeneratedImageCacheTimer::GeneratedImageCacheTimer):
    (WebCore::GeneratorGeneratedImage::GeneratedImageCacheTimer::restart):
    * platform/graphics/Gradient.cpp:
    (WebCore::Gradient::Gradient):
    (WebCore::Gradient::addColorStop):
    (WebCore::Gradient::sortStopsIfNecessary):
    (WebCore::Gradient::setSpreadMethod):
    (WebCore::Gradient::setGradientSpaceTransform):
    (WebCore::Gradient::hash):
    * platform/graphics/Gradient.h:
    (WebCore::Gradient::setP0):
    (WebCore::Gradient::setP1):
    (WebCore::Gradient::setStartRadius):
    (WebCore::Gradient::setEndRadius):
    (Gradient):
    (WebCore::Gradient::clearHashCache):
    * platform/graphics/GraphicsContext.cpp:
    (WebCore::GraphicsContext::isCompatibleWithBuffer):
    (WebCore):
    * platform/graphics/GraphicsContext.h:
    (GraphicsContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@117858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GeneratorGeneratedImage.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/GeneratorGeneratedImage.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2008, 2009, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2008, 2009, 2010, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FloatRect.h"
 #include "GraphicsContext.h"
-#include "ImageBuffer.h"
 #include "Length.h"
 
 namespace WebCore {
@@ -62,17 +61,28 @@ void GeneratorGeneratedImage::drawPattern(GraphicsContext* destContext, const Fl
     adjustedPatternCTM.scale(1.0 / xScale, 1.0 / yScale);
     adjustedSrcRect.scale(xScale, yScale);
 
-    // Create a BitmapImage and call drawPattern on it.
-    OwnPtr<ImageBuffer> imageBuffer = destContext->createCompatibleBuffer(adjustedSize);
-    if (!imageBuffer)
-        return;
+    unsigned generatorHash = m_generator->hash();
 
-    // Fill with the generated image.
-    GraphicsContext* graphicsContext = imageBuffer->context();
-    graphicsContext->fillRect(FloatRect(FloatPoint(), adjustedSize), *m_generator.get());
+    if (!m_cachedImageBuffer
+        || m_cachedGeneratorHash != generatorHash
+        || m_cachedAdjustedSize != adjustedSize
+        || !destContext->isCompatibleWithBuffer(m_cachedImageBuffer.get())) {
+        // Create a BitmapImage and call drawPattern on it.
+        m_cachedImageBuffer = destContext->createCompatibleBuffer(adjustedSize);
+        if (!m_cachedImageBuffer)
+            return;
+
+        // Fill with the generated image.
+        GraphicsContext* graphicsContext = m_cachedImageBuffer->context();
+        graphicsContext->fillRect(FloatRect(FloatPoint(), adjustedSize), *m_generator.get());
+
+        m_cachedGeneratorHash = generatorHash;
+        m_cachedAdjustedSize = adjustedSize;
+    }
 
     // Tile the image buffer into the context.
-    imageBuffer->drawPattern(destContext, adjustedSrcRect, adjustedPatternCTM, phase, styleColorSpace, compositeOp, destRect);
+    m_cachedImageBuffer->drawPattern(destContext, adjustedSrcRect, adjustedPatternCTM, phase, styleColorSpace, compositeOp, destRect);
+    m_cacheTimer.restart();
 }
 
 void GeneratedImage::computeIntrinsicDimensions(Length& intrinsicWidth, Length& intrinsicHeight, FloatSize& intrinsicRatio)
--- third_party/WebKit/Source/WebCore/platform/graphics/Gradient.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/Gradient.cpp
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Color.h"
 #include "FloatRect.h"
+#include <wtf/HashFunctions.h>
+#include <wtf/StringHasher.h>
 #include <wtf/UnusedParam.h>
 
 namespace WebCore {
@@ -44,6 +46,7 @@ Gradient::Gradient(const FloatPoint& p0, const FloatPoint& p1)
     , m_stopsSorted(false)
     , m_lastStop(0)
     , m_spreadMethod(SpreadMethodPad)
+    , m_hashCache(0)
 {
     platformInit();
 }
@@ -58,6 +61,7 @@ Gradient::Gradient(const FloatPoint& p0, float r0, const FloatPoint& p1, float r
     , m_stopsSorted(false)
     , m_lastStop(0)
     , m_spreadMethod(SpreadMethodPad)
+    , m_hashCache(0)
 {
     platformInit();
 }
@@ -100,6 +104,8 @@ void Gradient::addColorStop(float value, const Color& color)
 
     m_stopsSorted = false;
     platformDestroy();
+
+    clearHashCache();
 }
 
 void Gradient::addColorStop(const Gradient::ColorStop& stop)
@@ -108,6 +114,8 @@ void Gradient::addColorStop(const Gradient::ColorStop& stop)
 
     m_stopsSorted = false;
     platformDestroy();
+
+    clearHashCache();
 }
 
 static inline bool compareStops(const Gradient::ColorStop& a, const Gradient::ColorStop& b)
@@ -126,6 +134,8 @@ void Gradient::sortStopsIfNecessary()
         return;
 
     std::stable_sort(m_stops.begin(), m_stops.end(), compareStops);
+
+    clearHashCache();
 }
 
 void Gradient::getColor(float value, float* r, float* g, float* b, float* a) const
@@ -209,13 +219,24 @@ void Gradient::setSpreadMethod(GradientSpreadMethod spreadMethod)
 {
     // FIXME: Should it become necessary, allow calls to this method after m_gradient has been set.
     ASSERT(m_gradient == 0);
+
+    if (m_spreadMethod == spreadMethod)
+        return;
+
     m_spreadMethod = spreadMethod;
+
+    clearHashCache();
 }
 
 void Gradient::setGradientSpaceTransform(const AffineTransform& gradientSpaceTransformation)
-{ 
+{
+    if (m_gradientSpaceTransformation == gradientSpaceTransformation)
+        return;
+
     m_gradientSpaceTransformation = gradientSpaceTransformation;
     setPlatformGradientSpaceTransform(gradientSpaceTransformation);
+
+    clearHashCache();
 }
 
 #if !USE(SKIA) && !USE(CAIRO)
@@ -224,5 +245,45 @@ void Gradient::setPlatformGradientSpaceTransform(const AffineTransform&)
 }
 #endif
 
+unsigned Gradient::hash()
+{
+    if (m_hashCache)
+        return m_hashCache;
+
+    struct {
+        AffineTransform gradientSpaceTransformation;
+        FloatPoint p0;
+        FloatPoint p1;
+        float r0;
+        float r1;
+        float aspectRatio;
+        int lastStop;
+        GradientSpreadMethod spreadMethod;
+        bool radial;
+    } parameters;
+    
+    memset(&parameters, 0, sizeof(parameters));
+    
+    parameters.gradientSpaceTransformation = m_gradientSpaceTransformation;
+    parameters.p0 = m_p0;
+    parameters.p1 = m_p1;
+    parameters.r0 = m_r0;
+    parameters.r1 = m_r1;
+    parameters.aspectRatio = m_aspectRatio;
+    parameters.lastStop = m_lastStop;
+    parameters.spreadMethod = m_spreadMethod;
+    parameters.radial = m_radial;
+
+    unsigned parametersHash = StringHasher::hashMemory(&parameters, sizeof(parameters));
+    
+    ColorStop * stopData = m_stops.data();
+    unsigned stopHash = StringHasher::hashMemory(stopData, m_stops.size() * sizeof(ColorStop));
+    
+    std::pair<unsigned, unsigned> hashes(parametersHash, stopHash);
+    
+    m_hashCache = DefaultHash<std::pair<unsigned, unsigned> >::Hash::hash(hashes);
+
+    return m_hashCache;
+}
 
 } //namespace
