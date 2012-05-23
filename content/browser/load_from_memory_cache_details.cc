573fcb96a85ff6f069d8686d433b033a3d788fbb
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 22:53:18 +0000

    Passing mime type in DidLoadResourceFromMemoryCache and more info in LoadFromMemoryCacheDetails.
    The mime_type is needed for speculative resource prefetching.
    
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10413064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138638 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/load_from_memory_cache_details.cc
+++ content/browser/load_from_memory_cache_details.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,11 +9,17 @@ LoadFromMemoryCacheDetails::LoadFromMemoryCacheDetails(
     const GURL& url,
     int pid,
     int cert_id,
-    net::CertStatus cert_status)
+    net::CertStatus cert_status,
+    std::string http_method,
+    std::string mime_type,
+    ResourceType::Type resource_type)
     : url_(url),
       pid_(pid),
       cert_id_(cert_id),
-      cert_status_(cert_status) {
+      cert_status_(cert_status),
+      http_method_(http_method),
+      mime_type_(mime_type),
+      resource_type_(resource_type) {
 }
 
 LoadFromMemoryCacheDetails::~LoadFromMemoryCacheDetails() {
