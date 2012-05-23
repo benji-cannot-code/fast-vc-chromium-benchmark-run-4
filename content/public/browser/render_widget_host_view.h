dffa73a921673fd71eeca53b5e5297836a981b07
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 02:12:55 +0000

    Fixes lack of forward declaration of GdkEvent in render_widget_host_view. This fixes bot redness caused by 10411086
    Bug=None
    Test=Bots go green
    
    Review URL: https://chromiumcodereview.appspot.com/10421030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138418 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/render_widget_host_view.h
+++ content/public/browser/render_widget_host_view.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebInputEvent.h"
 #include "ui/gfx/native_widget_types.h"
 
+#if defined(TOOLKIT_GTK)
+class GdkEventButton;
+#endif
+
 class BrowserAccessibilityManager;
 
 namespace gfx {
