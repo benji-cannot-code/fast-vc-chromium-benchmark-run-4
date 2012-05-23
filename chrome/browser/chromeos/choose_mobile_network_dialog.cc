875a77af0fe5b1ba1afce1336aa4a9b94ab52454
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 21:00:05 +0000

    Don't use BrowserList::GetLastActive in choose mobile network dialog. It's not necessary.
    http://crbug.com/129187
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10332299
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138599 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/choose_mobile_network_dialog.cc
+++ chrome/browser/chromeos/choose_mobile_network_dialog.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/views/web_dialog_view.h"
 #include "chrome/common/url_constants.h"
 #include "ui/views/view.h"
@@ -31,7 +29,7 @@ namespace chromeos {
 void ChooseMobileNetworkDialog::ShowDialog(gfx::NativeWindow owning_window) {
   views::Widget::CreateWindowWithParent(
       new WebDialogView(ProfileManager::GetDefaultProfileOrOffTheRecord(),
-                        BrowserList::GetLastActive(),
+                        NULL,
                         new ChooseMobileNetworkDialog),
       owning_window)->Show();
 }
