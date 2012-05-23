388a3ebd832aabda81e479a9518378d88c41244e
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 16:01:19 +0000

    fix bustage. bad conflict merge.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138487 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_dialog_cloud.cc
+++ chrome/browser/printing/print_dialog_cloud.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/printing/cloud_print/cloud_print_url.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/print_messages.h"
