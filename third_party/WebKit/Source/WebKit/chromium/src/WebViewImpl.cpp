7a6d5539744151e7e14f8dbcc172afdaacd04083
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-23 05:03:16 +0000

    [chromium] Apply viewport tag initial-scale only once https://bugs.webkit.org/show_bug.cgi?id=82949
    Patch by Alexandre Elias <aelias@google.com> on 2012-05-22
    Reviewed by Adam Barth.
    
    First, check that isPageScaleFactorSet return false before setting
    initial-scale. We need to call dispatchViewportPropertiesDidChange()
    when the viewport width changes, since that's an input to the viewport
    tag calculation. When this happens, we shouldn't pop back to initial
    scale.
    
    Second, check that isNewNavigation is true when deciding to clear
    isPageScaleFactorIsSet in didCommitLoad. We only want to clear it on
    the very first commit, otherwise we'll pop back to initial scale if
    the user zooms in before the load is complete.
    
    New test WebFrameTest::FixedLayoutInitializeAtMinimumPageScale.
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::dispatchViewportPropertiesDidChange):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::didCommitLoad):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118121 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -3103,7 +3103,7 @@ void WebViewImpl::didCommitLoad(bool* isNewNavigation, bool isNavigationWithinPa
     m_newNavigationLoader = 0;
 #endif
     m_observedNewNavigation = false;
-    if (!isNavigationWithinPage)
+    if (*isNewNavigation && !isNavigationWithinPage)
         m_pageScaleFactorIsSet = false;
 
     m_gestureAnimation.clear();
