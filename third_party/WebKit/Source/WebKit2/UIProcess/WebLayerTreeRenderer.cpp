1da6dedca6930259c84cbe270c8d56e7a6bdfddf
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-23 10:54:01 +0000

    [Qt] Race condition in LayerTreeHost/Proxy can cause blank rendering https://bugs.webkit.org/show_bug.cgi?id=87239
    Reviewed by Kenneth Rohde Christiansen.
    
    On the UI process side the WebLayerTreeRenderer/LayerTreeHostProxy pair is
    created with m_active = false, which makes it ignore any incoming layer updates.
    It remains inactive until the ContentsSGNode is created and setActive(true) is called.
    
    On the web process side the LayerTreeHost is created with m_waitingForUIProcess = false.
    
    Those two variables must be in sync, otherwise a race condition can
    happen: The web process sends updates to the ui process before the
    ContentsSGNode is created. This can happen for example when re-using an
    existing WebProcess to create a QQuickWebView and load a page very fast
    from the disk cache. The update arrives on the ui process side, but it
    is discarded because setActive(true) hasn't been called yet. As a
    result any updates that would for example create and populate the tiles
    required for rendering are ignored.
    
    This patch ensures that the variables are initialized with the same values and that calling
    setActive(true) will trigger the rendering of the next frame.
    
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::setActive):
    * WebProcess/WebPage/qt/LayerTreeHostQt.cpp:
    (WebKit::LayerTreeHostQt::LayerTreeHostQt):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.cpp
@@ -461,6 +461,8 @@ void WebLayerTreeRenderer::setActive(bool active)
     // and cannot be applied to the newly created instance.
     m_renderQueue.clear();
     m_isActive = active;
+    if (m_isActive)
+        renderNextFrame();
 }
 
 } // namespace WebKit
