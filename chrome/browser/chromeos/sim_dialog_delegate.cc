c4f15c350cb973f4dba779554513aff01584ec3d
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 22:15:44 +0000

    Remove BrowserList usage from SimDialogDelegate. Just pass NULL for the browser. Does not matter.
    http://crbug.com/129187
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10408063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138614 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/sim_dialog_delegate.cc
+++ chrome/browser/chromeos/sim_dialog_delegate.cc
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/views/web_dialog_view.h"
 #include "chrome/common/url_constants.h"
 #include "ui/views/view.h"
@@ -50,7 +48,7 @@ void SimDialogDelegate::ShowDialog(gfx::NativeWindow owning_window,
                                    SimDialogMode mode) {
   views::Widget::CreateWindowWithParent(
       new WebDialogView(ProfileManager::GetDefaultProfileOrOffTheRecord(),
-                        BrowserList::GetLastActive(),
+                        NULL,
                         new SimDialogDelegate(mode)),
       owning_window)->Show();
 }
