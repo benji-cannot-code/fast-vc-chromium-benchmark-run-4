5dc4e8465870d61fe0ec4a23d8cfda7b8d6aba2d
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 17:07:10 +0000

    Convert printing code to use base::TimeDelta in PostDelayedTask interfaces.
    R=abodenha@chromium.org
    BUG=108171
    
    
    Review URL: https://chromiumcodereview.appspot.com/10412028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138500 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_job.h
+++ chrome/browser/printing/print_job.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -79,7 +79,7 @@ class PrintJob : public PrintJobWorkerOwner,
   // Synchronously wait for the job to finish. It is mainly useful when the
   // process is about to be shut down and we're waiting for the spooler to eat
   // our data.
-  bool FlushJob(int timeout_ms);
+  bool FlushJob(base::TimeDelta timeout);
 
   // Disconnects the PrintedPage source (PrintedPagesSource). It is done when
   // the source is being destroyed.
