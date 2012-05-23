7ac47f607523316c987b29f1078d6ca84749ec8b
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 19:23:51 +0000

    Make cloud print dialog creation function not use browser list, instead obtain profile and parent window from invocation context.
    http://crbug.com/129053
    TEST=none
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=138485
    Review URL: https://chromiumcodereview.appspot.com/10388214
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview/print_preview_handler.cc
+++ chrome/browser/ui/webui/print_preview/print_preview_handler.cc
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_view.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_ui.h"
 #include "printing/backend/print_backend.h"
@@ -548,7 +549,11 @@ void PrintPreviewHandler::OnSigninComplete(
 }
 
 void PrintPreviewHandler::HandleSignin(const ListValue* /*args*/) {
+  gfx::NativeWindow modal_parent =
+      web_ui()->GetWebContents()->GetView()->GetTopLevelNativeWindow();
   print_dialog_cloud::CreateCloudPrintSigninDialog(
+      web_ui()->GetWebContents()->GetBrowserContext(),
+      modal_parent,
       base::Bind(&PrintPreviewHandler::OnSigninComplete, AsWeakPtr()));
 }
 
@@ -567,11 +572,16 @@ void PrintPreviewHandler::HandlePrintWithCloudPrint() {
     return;
   }
   DCHECK_GT(data->size(), 0U);
-  print_dialog_cloud::CreatePrintDialogForBytes(data,
+
+  gfx::NativeWindow modal_parent =
+      web_ui()->GetWebContents()->GetView()->GetTopLevelNativeWindow();
+  print_dialog_cloud::CreatePrintDialogForBytes(
+      web_ui()->GetWebContents()->GetBrowserContext(),
+      modal_parent,
+      data,
       string16(print_preview_ui->initiator_tab_title()),
       string16(),
-      std::string("application/pdf"),
-      true);
+      std::string("application/pdf"));
 
   // Once the cloud print dialog comes up we're no longer in a background
   // printing situation.  Close the print preview.
--- chrome/renderer/chrome_mock_render_thread.cc
+++ chrome/renderer/chrome_mock_render_thread.cc
@@ -86,7 +86,8 @@ void ChromeMockRenderThread::OnAllocateTempFileForPrinting(
   }
 }
 
-void ChromeMockRenderThread::OnTempFileForPrintingWritten(int browser_fd) {
+void ChromeMockRenderThread::OnTempFileForPrintingWritten(int render_view_id,
+                                                          int browser_fd) {
   close(browser_fd);
 }
 #endif  // defined(OS_CHROMEOS)
