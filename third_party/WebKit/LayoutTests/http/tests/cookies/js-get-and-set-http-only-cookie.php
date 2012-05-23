5efd3b6639b0f7a69b8ce759fb5e9e1ab30584ca
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-23 02:49:58 +0000

    [BlackBerry] Possible to clobber httponly cookie. https://bugs.webkit.org/show_bug.cgi?id=86067
    Patch by Jason Liu <jason.liu@torchmobile.com.cn> on 2012-05-22
    Reviewed by Rob Buis.
    
    Source/WebCore:
    
    If a cookie is set by javaScript and there is already a same httpOnly cookie in cookieManager,
    we should reject it. If it has a httpOnly property, we reject it, too.
    
    Test: http/tests/cookies/js-get-and-set-http-only-cookie.php
    
    * platform/blackberry/CookieJarBlackBerry.cpp:
    (WebCore::setCookies):
    * platform/blackberry/CookieManager.cpp:
    (WebCore::CookieManager::setCookies):
    (WebCore::CookieManager::shouldRejectNotHttpCookie):
    (WebCore):
    * platform/blackberry/CookieManager.h:
    
    LayoutTests:
    
    * http/tests/cookies/js-get-and-set-http-only-cookie-expected.txt: Added.
    * http/tests/cookies/js-get-and-set-http-only-cookie.php: Added.
    * http/tests/cookies/script-tests/js-get-and-set-http-only-cookie.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/cookies/js-get-and-set-http-only-cookie.php
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+    setcookie("httpOnlyCookie", "value", time()+36000000, "", "", 0, 1);
+?>
+<!DCTYPE HTML PUBLIC "-//IETF//DTD HTML//EN">
+<html>
+<head>
+<link rel="stylesheet" href="resources/cookies-test-style.css">
+<script src="resources/cookies-test-pre.js"></script>
+</head>
+<body>
+<p id="description"></p>
+<div id="console"></div>
+<script src="script-tests/js-get-and-set-http-only-cookie.js"></script>
+<script src="resources/cookies-test-post.js"></script>
+</body>
+</html>
