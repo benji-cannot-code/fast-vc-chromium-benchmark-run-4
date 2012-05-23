bb49f3067c55f6835fa1def13941d5e698edc949
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 20:53:34 +0000

    Revert 138588 - Fix bug 105043: Panels [WIN]: For minimize panels, taskbar hover preview show the 4-pixel representation
    See: http://build.chromium.org/p/chromium/builders/Win%20Aura/builds/13340/steps/compile/logs/stdio#error1
    
    BUG=105043
    TEST=Manual test by minimizing panels and moving mouse over their taskbar icons
    
    
    Review URL: https://chromiumcodereview.appspot.com/10408047
    
    TBR=jianli@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10429007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138597 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_browser_window_cocoa.mm
+++ chrome/browser/ui/panels/panel_browser_window_cocoa.mm
@@ -355,10 +355,6 @@ void PanelBrowserWindowCocoa::Observe(
   [controller_ updateWindowLevel];
 }
 
-void PanelBrowserWindowCocoa::PanelExpansionStateChanging(
-    Panel::ExpansionState old_state, Panel::ExpansionState new_state) {
-}
-
 // NativePanelTesting implementation.
 class NativePanelTestingCocoa : public NativePanelTesting {
  public:
