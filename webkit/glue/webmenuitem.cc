9b2e9931a1893d02be29909f980479d68a87c8fa
apavlov@chromium.org <apavlov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 18:39:51 +0000

    Implement the Chromium-specific part of WebCore context menus with submenus. The WebKit counterpart is tracked at https://bugs.webkit.org/show_bug.cgi?id=86625
    BUG=128351
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10411002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138540 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webmenuitem.cc
+++ webkit/glue/webmenuitem.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -23,6 +23,8 @@ WebMenuItem::WebMenuItem(const WebKit::WebMenuItemInfo& item)
       has_directional_override(item.hasTextDirectionOverride),
       enabled(item.enabled),
       checked(item.checked) {
+  for (size_t i = 0; i < item.subMenuItems.size(); ++i)
+    submenu.push_back(WebMenuItem(item.subMenuItems[i]));
 }
 
 WebMenuItem::WebMenuItem(const WebMenuItem& item)
