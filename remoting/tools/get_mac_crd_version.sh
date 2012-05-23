693a9a021480757bfdb1d6bcfab0aec5d8577049
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 00:04:44 +0000

    [Chromoting] Simple script to print out installed mac version info.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10332303
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138401 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/tools/get_mac_crd_version.sh
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh
+
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+ME2ME_HOST="/Library/PrivilegedHelperTools/org.chromium.chromoting.me2me_host.app"
+UNINSTALLER="/Applications/Chrome Remote Desktop Host Uninstaller.app"
+PREFPANE="/Library/PreferencePanes/org.chromium.chromoting.prefPane"
+KEYSTONE="/Library/Google/GoogleSoftwareUpdate/GoogleSoftwareUpdate.bundle"
+
+INFO_PLIST="Contents/Info.plist"
+
+set -e -u
+
+function print_plist_version {
+  local name="${1}"
+  local file="${2}"
+  set `PlistBuddy -c 'Print CFBundleVersion' "${file}/${INFO_PLIST}"`
+  echo "${name} version = ${1}"
+}
+
+print_plist_version "Me2me host" "${ME2ME_HOST}"
+print_plist_version "Uninstaller" "${UNINSTALLER}"
+print_plist_version "PreferencePane" "${PREFPANE}"
+print_plist_version "Keystone" "${KEYSTONE}"
