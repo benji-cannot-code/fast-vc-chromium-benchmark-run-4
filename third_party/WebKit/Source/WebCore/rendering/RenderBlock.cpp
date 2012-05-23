67f56d7db89e0490dad3ca0624b8547c468af314
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-23 20:41:11 +0000

    ASSERT failure toRenderProgress in HTMLProgressElement::didElementStateChange https://bugs.webkit.org/show_bug.cgi?id=87274
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Progress bar can't run-in. Prevent it from becoming a run-in, leading to an
    unworkable RenderInline.
    
    Test: fast/runin/progress-run-in-crash.html
    
    * html/HTMLProgressElement.cpp:
    (WebCore::HTMLProgressElement::didElementStateChange):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::moveRunInUnderSiblingBlockIfNeeded):
    
    LayoutTests:
    
    * fast/runin/progress-run-in-crash-expected.txt: Added.
    * fast/runin/progress-run-in-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -1847,6 +1847,9 @@ void RenderBlock::moveRunInUnderSiblingBlockIfNeeded(RenderObject* runIn)
     if (runInNode && runInNode->hasTagName(selectTag))
         return;
 
+    if (runInNode && runInNode->hasTagName(progressTag))
+        return;
+
     RenderObject* curr = runIn->nextSibling();
     if (!curr || !curr->isRenderBlock() || !curr->childrenInline())
         return;
