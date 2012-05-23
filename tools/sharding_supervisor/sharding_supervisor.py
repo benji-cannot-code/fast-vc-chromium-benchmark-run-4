03b2d75e01b4bc926ac26bb61bd6c1cb392f878e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 22:16:05 +0000

    Valgrind: Tool changes to support running browser tests with memcheck in sharding_supervisor.
    - Add ability to override CPU count in sharding_supervisor via an envvar.
    - Add a dummy gtest compatibility flag to the Valgrind test runner script.
    
    BUG=118609
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10417048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138615 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/sharding_supervisor/sharding_supervisor.py
+++ tools/sharding_supervisor/sharding_supervisor.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -55,6 +55,9 @@ def DetectNumCores():
     be found.
   """
   try:
+    # Override on some Chromium Valgrind bots.
+    if "CHROME_VALGRIND_NUMCPUS" in os.environ:
+      return int(os.environ["CHROME_VALGRIND_NUMCPUS"])
     # Linux, Unix, MacOS
     if hasattr(os, "sysconf"):
       if "SC_NPROCESSORS_ONLN" in os.sysconf_names:
