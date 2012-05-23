697ac2c31745c58d70ddb618b78ccb79f6d36430
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 22:20:21 +0000

    Honor the session.restore_on_startup_pref on first start
    This CL fixes a regression introduced by CL 9481003 that made the browser ignore the session.restore_on_startup pref on first start.
    
    BUG=125467
    TEST=session.restore_on_startup, session.urls_to_restore_on_startup
    TEST=and RestoreOnStartup policy are honored on first start as well
    
    Review URL: https://chromiumcodereview.appspot.com/10417032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138617 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator.cc
+++ chrome/browser/ui/startup/startup_browser_creator.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/environment.h"
 #include "base/file_path.h"
 #include "base/lazy_instance.h"
+#include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/histogram.h"
 #include "base/path_service.h"
@@ -231,10 +232,16 @@ bool StartupBrowserCreator::WasRestarted() {
 SessionStartupPref StartupBrowserCreator::GetSessionStartupPref(
     const CommandLine& command_line,
     Profile* profile) {
-  SessionStartupPref pref = SessionStartupPref::GetStartupPref(profile);
+  DCHECK(profile);
+  PrefService* prefs = profile->GetPrefs();
+  SessionStartupPref pref = SessionStartupPref::GetStartupPref(prefs);
 
-  // Session restore should be avoided on the first run.
-  if (first_run::IsChromeFirstRun())
+  // The pref has an OS-dependent default value. For the first run only, this
+  // default is overridden with SessionStartupPref::DEFAULT so that first run
+  // behavior (sync promo, welcome page) is consistently invoked.
+  // This applies only if the pref is still at its default and has not been
+  // set by the user, managed prefs or policy.
+  if (first_run::IsChromeFirstRun() && SessionStartupPref::TypeIsDefault(prefs))
     pref.type = SessionStartupPref::DEFAULT;
 
   if (command_line.HasSwitch(switches::kRestoreLastSession) ||
@@ -242,8 +249,7 @@ SessionStartupPref StartupBrowserCreator::GetSessionStartupPref(
     pref.type = SessionStartupPref::LAST;
   }
   if (pref.type == SessionStartupPref::LAST &&
-      IncognitoModePrefs::ShouldLaunchIncognito(command_line,
-                                                profile->GetPrefs())) {
+      IncognitoModePrefs::ShouldLaunchIncognito(command_line, prefs)) {
     // We don't store session information when incognito. If the user has
     // chosen to restore last session and launched incognito, fallback to
     // default launch behavior.
