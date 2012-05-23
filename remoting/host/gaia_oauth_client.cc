98db7e7b9d5977de69a5557ecc7fde15713b442b
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 22:13:24 +0000

    Add missing initialisation in GaiaOAuthClient::Core.
    The changes to address crbug.com/128102 contained a bug whereby the caller-supplied OAuth URLs were not stored to the Core, leading to empty URLs being passed to the URLFetcher.  Confusingly, this provoked a URLFetcher response code of net::IO_PENDING, so this CL also adds a CHECK() to remoting/host/url_fetcher.cc to ensure that the supplied URL is valid.
    
    BUG=129449
    
    Review URL: https://chromiumcodereview.appspot.com/10431005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138611 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/gaia_oauth_client.cc
+++ remoting/host/gaia_oauth_client.cc
@@ -49,7 +49,8 @@ class GaiaOAuthClient::Core
  public:
   Core(const OAuthProviderInfo& info,
        net::URLRequestContextGetter* request_context_getter)
-      : request_context_getter_(request_context_getter),
+      : provider_info_(info),
+        request_context_getter_(request_context_getter),
         delegate_(NULL) {
   }
 
