c509bebf6c8239f03f8dcb3b4d0142c460620706
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 08:40:38 +0000

    Multiple dispatchers for root windows This allows ash to inject Dispatcher for root window without a dispatcher being dependent of aura.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10407081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138451 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/dispatcher_linux.h
+++ ui/aura/dispatcher_linux.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <map>
+#include <vector>
 #include <X11/Xlib.h>
 // Get rid of a macro from Xlib.h that conflicts with Aura's RootWindow class.
 #undef RootWindow
@@ -24,9 +25,15 @@ class DispatcherLinux : public MessageLoop::Dispatcher,
   DispatcherLinux();
   virtual ~DispatcherLinux();
 
-  void WindowDispatcherCreated(::Window window,
-                               MessageLoop::Dispatcher* dispatcher);
-  void WindowDispatcherDestroying(::Window window);
+  // Adds/Removes |dispatcher| for the |x_window|.
+  void AddDispatcherForWindow(MessageLoop::Dispatcher* dispatcher,
+                                   ::Window x_window);
+  void RemoveDispatcherForWindow(::Window x_window);
+
+  // Adds/Removes |dispatcher| to receive all events sent to the X
+  // root window.
+  void AddDispatcherForRootWindow(MessageLoop::Dispatcher* dispatcher);
+  void RemoveDispatcherForRootWindow(MessageLoop::Dispatcher* dispatcher);
 
   // Overridden from MessageLoop::Dispatcher:
   virtual bool Dispatch(const base::NativeEvent& event) OVERRIDE;
@@ -38,10 +45,14 @@ class DispatcherLinux : public MessageLoop::Dispatcher,
 
  private:
   typedef std::map< ::Window, MessageLoop::Dispatcher* > DispatchersMap;
+  typedef std::vector<MessageLoop::Dispatcher*> Dispatchers;
 
   MessageLoop::Dispatcher* GetDispatcherForXEvent(XEvent* xev) const;
 
   DispatchersMap dispatchers_;
+  Dispatchers root_window_dispatchers_;
+
+  ::Window x_root_window_;
 
   DISALLOW_COPY_AND_ASSIGN(DispatcherLinux);
 };
