b53d69184807198ad887700f19ef783574f133df
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 05:08:52 +0000

    Browser Plugin: Fix multi-platform use of FilePath.
    TBR=fsamuel@chromium.org
    TEST=compiles
    
    Review URL: https://chromiumcodereview.appspot.com/10413068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138429 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/browser_plugin/browser_plugin_constants.cc
+++ content/renderer/browser_plugin/browser_plugin_constants.cc
@@ -7,5 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 const char kBrowserPluginName[] = "Browser Plugin";
 const char kBrowserPluginMimeType[] = "application/browser-plugin";
-const char kBrowserPluginPath[] = "internal/browser-plugin";
+const FilePath::CharType kBrowserPluginPath[] =
+    FILE_PATH_LITERAL("internal/browser-plugin");
 const char kBrowserPluginDescription[] = "Out-of-Process Browser Plugin";
--- content/renderer/browser_plugin/browser_plugin_constants.h
+++ content/renderer/browser_plugin/browser_plugin_constants.h
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifndef CONTENT_RENDERER_BROWSER_PLUGIN_BROWSER_PLUGIN_CONSTANTS_H_
 #define CONTENT_RENDERER_BROWSER_PLUGIN_BROWSER_PLUGIN_CONSTANTS_H_
+#pragma once
+
+#include "base/file_path.h"
 
 extern const char kBrowserPluginName[];
 extern const char kBrowserPluginMimeType[];
-extern const char kBrowserPluginPath[];
+extern const FilePath::CharType kBrowserPluginPath[];
 extern const char kBrowserPluginDescription[];
 
 #endif  // CONTENT_RENDERER_BROWSER_PLUGIN_BROWSER_PLUGIN_CONSTANTS_H_
