fa384c25508cf79e3e7dd92a9a1b546c126d0ca8
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-23 13:50:06 +0000

    Streamline Element teardown for the no-Attr-objects case. <http://webkit.org/b/87228>
    Reviewed by Antti Koivisto.
    
    Dodge one function call in ~Element() if the element has no Attr objects
    associated (the typical case.)
    
    Renamed detachAttributes to detachAttrObjectsFromElement to reduce ambiguity.
    
    * dom/Element.cpp:
    (WebCore::Element::~Element):
    * dom/ElementAttributeData.cpp:
    (WebCore::ElementAttributeData::detachAttrObjectsFromElement):
    (WebCore::ElementAttributeData::clearAttributes):
    * dom/ElementAttributeData.h:
    (ElementAttributeData):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118173 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ElementAttributeData.cpp
+++ third_party/WebKit/Source/WebCore/dom/ElementAttributeData.cpp
@@ -221,10 +221,9 @@ bool ElementAttributeData::isEquivalent(const ElementAttributeData* other) const
     return true;
 }
 
-void ElementAttributeData::detachAttributesFromElement(Element* element)
+void ElementAttributeData::detachAttrObjectsFromElement(Element* element)
 {
-    if (!element->hasAttrList())
-        return;
+    ASSERT(element->hasAttrList());
 
     for (unsigned i = 0; i < m_attributes.size(); ++i) {
         if (RefPtr<Attr> attr = attrIfExists(element, m_attributes[i].name()))
@@ -287,8 +286,10 @@ void ElementAttributeData::cloneDataFrom(const ElementAttributeData& sourceData,
 
 void ElementAttributeData::clearAttributes(Element* element)
 {
+    if (element->hasAttrList())
+        detachAttrObjectsFromElement(element);
+
     clearClass();
-    detachAttributesFromElement(element);
     m_attributes.clear();
 }
 
