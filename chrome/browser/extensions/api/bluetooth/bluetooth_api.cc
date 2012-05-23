e40469358ab677b1307621d7a6b45023d419c191
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 22:06:27 +0000

    Clean-up logic for getDevciesWithServiceName.
    This simplifies the callback logic, eliminating a potential bug if
    ProvidesServiceWithName is called immediately on the UI thread.
    
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10388241
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138606 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
+++ chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
@@ -111,15 +111,21 @@ void BluetoothGetDevicesWithServiceNameFunction::AddDeviceIfTrue(
 bool BluetoothGetDevicesWithServiceNameFunction::RunImpl() {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 
-  scoped_ptr<GetDevicesWithServiceName::Params> params(
-      GetDevicesWithServiceName::Params::Create(*args_));
-
   ListValue* matches = new ListValue;
   result_.reset(matches);
 
   BluetoothAdapter::DeviceList devices =
       GetMutableAdapter(profile())->GetDevices();
-  callbacks_pending_ = 0;
+  if (devices.empty()) {
+    SendResponse(true);
+    return true;
+  }
+
+  callbacks_pending_ = devices.size();
+  AddRef();  // Released in AddDeviceIfTrue when callbacks_pending_ == 0
+
+  scoped_ptr<GetDevicesWithServiceName::Params> params(
+      GetDevicesWithServiceName::Params::Create(*args_));
   for (BluetoothAdapter::DeviceList::iterator i = devices.begin();
       i != devices.end(); ++i) {
     (*i)->ProvidesServiceWithName(params->name,
@@ -127,13 +133,7 @@ bool BluetoothGetDevicesWithServiceNameFunction::RunImpl() {
                    this,
                    matches,
                    *i));
-    callbacks_pending_++;
   }
-
-  if (callbacks_pending_)
-    AddRef();  // Released in AddDeviceIfTrue when callbacks_pending_ == 0
-  else
-    SendResponse(true);
 
   return true;
 }
