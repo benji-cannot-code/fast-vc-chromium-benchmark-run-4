261eb84cdff3de07648a6d024ee4d108cb4f291e
kochi@google.com <kochi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 05:20:12 +0000

    Add GL_EXPORT for GLContextGLX
    This fixes breakage of component_build on ChromeOS
    
    BUG=129131
    TEST=emerge and run chrome on ChromeOS
    
    Review URL: https://chromiumcodereview.appspot.com/10399120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138430 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_context_glx.h
+++ ui/gl/gl_context_glx.h
@@ -12,13 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "ui/base/x/x11_util.h"
 #include "ui/gl/gl_context.h"
+#include "ui/gl/gl_export.h"
 
 namespace gfx {
 
 class GLSurface;
 
 // Encapsulates a GLX OpenGL context.
-class GLContextGLX : public GLContext {
+class GL_EXPORT GLContextGLX : public GLContext {
  public:
   explicit GLContextGLX(GLShareGroup* share_group);
 
