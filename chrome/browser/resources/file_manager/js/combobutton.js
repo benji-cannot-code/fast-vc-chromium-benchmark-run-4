201be79e7d9bcee92c2ca81c244308e2077884fd
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 19:56:30 +0000

    Revert 138476 - [FileBrowser] Added DefaultAction dialog to choose default action.
    BUG=126927
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10411018
    
    TBR=zvorygin@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10412062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138566 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/combobutton.js
+++ chrome/browser/resources/file_manager/js/combobutton.js
@@ -36,13 +36,6 @@ cr.define('cr.ui', function() {
     },
 
     /**
-     * Adds separator to drop-down list.
-     */
-    addSeparator: function() {
-      this.menu.addSeparator();
-    },
-
-    /**
      * Default item to fire on combobox click
      */
     get defaultItem() {
