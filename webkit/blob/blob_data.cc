74de210d34ea39b1c6fb27adfccf665894bdf87f
ericu@google.com <ericu@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 20:32:32 +0000

    Revert 138554 - Prevent zero-length items from being appended to a blob.
    BUG=128266
    TEST=as in the bug
    
    
    Review URL: https://chromiumcodereview.appspot.com/10386183
    
    TBR=ericu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10428003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138584 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/blob/blob_data.cc
+++ webkit/blob/blob_data.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -84,36 +84,5 @@ BlobData::BlobData(const WebBlobData& data) {
 }
 
 BlobData::~BlobData() {}
-
-void BlobData::AppendData(const char* data, size_t length) {
-  DCHECK(length > 0);
-  items_.push_back(Item());
-  items_.back().SetToData(data, length);
-}
-
-void BlobData::AppendFile(const FilePath& file_path, uint64 offset,
-                          uint64 length,
-                          const base::Time& expected_modification_time) {
-  DCHECK(length > 0);
-  items_.push_back(Item());
-  items_.back().SetToFile(file_path, offset, length,
-                          expected_modification_time);
-}
-
-void BlobData::AppendBlob(const GURL& blob_url, uint64 offset, uint64 length) {
-  DCHECK(length > 0);
-  items_.push_back(Item());
-  items_.back().SetToBlob(blob_url, offset, length);
-}
-
-int64 BlobData::GetMemoryUsage() const {
-  int64 memory = 0;
-  for (std::vector<Item>::const_iterator iter = items_.begin();
-       iter != items_.end(); ++iter) {
-    if (iter->type == TYPE_DATA)
-      memory += iter->data.size();
-  }
-  return memory;
-}
 
 }  // namespace webkit_blob
