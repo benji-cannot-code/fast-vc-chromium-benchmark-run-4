e53f7ba96c86bcce87f8aff2cf32945946b2738e
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-23 19:07:16 +0000

    Refactor out study validation function in VariationsService and add a unit test.
    BUG=none
    TEST=VariationsServiceTest.ValidateStudy
    
    Review URL: https://chromiumcodereview.appspot.com/10411095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138550 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/variations_service.h
+++ chrome/browser/metrics/variations_service.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/metrics/field_trial.h"
 #include "base/time.h"
 #include "chrome/browser/metrics/proto/study.pb.h"
 #include "chrome/browser/metrics/proto/trials_seed.pb.h"
@@ -51,6 +52,7 @@ class VariationsService : public net::URLFetcherDelegate {
   FRIEND_TEST_ALL_PREFIXES(VariationsServiceTest, CheckStudyVersion);
   FRIEND_TEST_ALL_PREFIXES(VariationsServiceTest, CheckStudyVersionWildcards);
   FRIEND_TEST_ALL_PREFIXES(VariationsServiceTest, CheckStudyDate);
+  FRIEND_TEST_ALL_PREFIXES(VariationsServiceTest, ValidateStudy);
 
   // Store the given seed data to the given local prefs. Note that |seed_data|
   // is assumed to be the raw serialized protobuf data stored in a string. It
@@ -73,6 +75,11 @@ class VariationsService : public net::URLFetcherDelegate {
   // Checks whether |study| is applicable for the given date/time.
   static bool CheckStudyDate(const chrome_variations::Study& study,
                              const base::Time& date_time);
+
+  // Validates the sanity of |study| and computes the total probability.
+  static bool ValidateStudyAndComputeTotalProbability(
+      const chrome_variations::Study& study,
+      base::FieldTrial::Probability* total_probability);
 
   // Loads the Variations seed data from the given local prefs into |seed|. If
   // there is a problem with loading, the pref value is cleared and false is
