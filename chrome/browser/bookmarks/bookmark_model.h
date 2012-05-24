bb8359343c6eba7e70e5d1bb055150aa050c1b78
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 19:46:45 +0000

    Converting BookmarkModel and HistoryService to ProfileKeyedServices. This just performs the initial conversion. Separate CLs to take care of the removal of profile_->Get<Serivce> will follow.
    BUG=97804,112525
    TEST=no new, passes existing unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10399087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138867 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model.h
+++ chrome/browser/bookmarks/bookmark_model.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/cancelable_request.h"
 #include "chrome/browser/favicon/favicon_service.h"
 #include "chrome/browser/history/history.h"
+#include "chrome/browser/profiles/profile_keyed_service.h"
 #include "content/public/browser/notification_registrar.h"
 #include "googleurl/src/gurl.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -193,19 +194,18 @@ class BookmarkPermanentNode : public BookmarkNode {
 // An observer may be attached to observe relevant events.
 //
 // You should NOT directly create a BookmarkModel, instead go through the
-// Profile.
+// BookmarkModelFactory.
 class BookmarkModel : public content::NotificationObserver,
-                      public BookmarkService {
+                      public BookmarkService,
+                      public ProfileKeyedService {
  public:
   explicit BookmarkModel(Profile* profile);
   virtual ~BookmarkModel();
 
-  static void RegisterUserPrefs(PrefService* prefs);
-
   // Invoked prior to destruction to release any necessary resources.
-  void Cleanup();
+  virtual void Shutdown() OVERRIDE;
 
-  // Loads the bookmarks. This is called by Profile upon creation of the
+  // Loads the bookmarks. This is called upon creation of the
   // BookmarkModel. You need not invoke this directly.
   void Load();
 
