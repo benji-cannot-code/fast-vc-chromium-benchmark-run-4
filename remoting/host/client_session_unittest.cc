8160ecf2d7b575d3eeee7c5e4b0a324f9270817f
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 01:13:33 +0000

    [Chromoting] Tighten up unit tests.
    The result is that GMock stops complaining about uninteresting
    mock function calls.
    
    This CL also prevents a NOTIMPLEMENTED error in fake_session.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10430006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138681 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/client_session_unittest.cc
+++ remoting/host/client_session_unittest.cc
@@ -38,7 +38,8 @@ class ClientSessionTest : public testing::Test {
     protocol::MockSession* session = new MockSession();
     EXPECT_CALL(*session, jid()).WillRepeatedly(ReturnRef(client_jid_));
     EXPECT_CALL(*session, SetStateChangeCallback(_));
-
+    EXPECT_CALL(*session, SetRouteChangeCallback(_));
+    EXPECT_CALL(*session, Close());
     scoped_ptr<protocol::ConnectionToClient> connection(
         new protocol::ConnectionToClient(session));
     client_session_.reset(new ClientSession(
@@ -253,6 +254,7 @@ TEST_F(ClientSessionTest, ClampMouseEvents) {
 
   EXPECT_CALL(session_event_handler_, OnSessionAuthenticated(_));
   EXPECT_CALL(session_event_handler_, OnSessionChannelsConnected(_));
+  EXPECT_CALL(session_event_handler_, OnSessionClosed(_));
 
   client_session_->OnConnectionAuthenticated(client_session_->connection());
   client_session_->OnConnectionChannelsConnected(client_session_->connection());
@@ -272,6 +274,8 @@ TEST_F(ClientSessionTest, ClampMouseEvents) {
       client_session_->InjectMouseEvent(event);
     }
   }
+
+  DisconnectClientSession();
 }
 
 }  // namespace remoting
