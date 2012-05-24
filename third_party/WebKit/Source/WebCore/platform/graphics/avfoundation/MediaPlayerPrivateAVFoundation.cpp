7a1950669b8d27eeaaef7c7af12a3bad1938437b
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-24 02:44:14 +0000

    REGRESSION: compositing/video/video-poster.html fails on Mac https://bugs.webkit.org/show_bug.cgi?id=87199
    Reviewed by Maciej Stachowiak.
    
    No new tests; fixes failing compositing/video/video-poster.html test.
    
    Instead of creating the video layer directly, simply allow the layer
    to be created in updateStates() by changing the definition of
    isReadyForVideoSetup() to bypass the m_isAllowedToRender check if
    the player reports a video track is present.  This causes the video layer
    to be created and for future calls to prepareForRendering() to result
    in calls to mediaPlayerRenderingModeChanged().
    
    * platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp:
    (WebCore::MediaPlayerPrivateAVFoundation::isReadyForVideoSetup):
    (WebCore::MediaPlayerPrivateAVFoundation::metadataLoaded):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118314 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/avfoundation/MediaPlayerPrivateAVFoundation.cpp
@@ -379,7 +379,10 @@ unsigned MediaPlayerPrivateAVFoundation::bytesLoaded() const
 
 bool MediaPlayerPrivateAVFoundation::isReadyForVideoSetup() const
 {
-    return m_isAllowedToRender && m_readyState >= MediaPlayer::HaveMetadata && m_player->visible();
+    // AVFoundation will not return true for firstVideoFrameAvailable until
+    // an AVPlayerLayer has been added to the AVPlayerItem, so allow video setup
+    // here if a video track to trigger allocation of a AVPlayerLayer.
+    return (m_isAllowedToRender || m_cachedHasVideo) && m_readyState >= MediaPlayer::HaveMetadata && m_player->visible();
 }
 
 void MediaPlayerPrivateAVFoundation::prepareForRendering()
@@ -531,14 +534,6 @@ void MediaPlayerPrivateAVFoundation::metadataLoaded()
 {
     m_loadingMetadata = false;
     tracksChanged();
-
-#if USE(ACCELERATED_COMPOSITING)
-    // AVFoundation will not return true for firstVideoFrameAvailable until
-    // an AVPlayerLayer has been added to the AVPlayerItem, so call createVideoLayer()
-    // here to trigger allocation of a AVPlayerLayer.
-    if (m_cachedHasVideo)
-        createVideoLayer();
-#endif
 }
 
 void MediaPlayerPrivateAVFoundation::rateChanged()
