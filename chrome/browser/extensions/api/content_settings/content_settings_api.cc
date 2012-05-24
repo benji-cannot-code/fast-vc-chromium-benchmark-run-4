7cf5959495fa816568d2bf59a9d562e4fc8f9f8c
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 03:24:46 +0000

    Remove IsWebIntentsEnabledForActiveBrowser and change HostContentSettingsMap to use a variant that supplies a PrefService instead.
    http://crbug.com/129187
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10432005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138721 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/content_settings/content_settings_api.cc
+++ chrome/browser/extensions/api/content_settings/content_settings_api.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_preference_api_constants.h"
 #include "chrome/browser/extensions/extension_preference_helpers.h"
 #include "chrome/browser/extensions/extension_service.h"
+#include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_error_utils.h"
@@ -223,7 +224,9 @@ bool SetContentSettingFunction::RunImpl() {
   EXTENSION_FUNCTION_VALIDATE(
       helpers::StringToContentSetting(setting_str, &setting));
   EXTENSION_FUNCTION_VALIDATE(
-      HostContentSettingsMap::IsSettingAllowedForType(setting, content_type));
+      HostContentSettingsMap::IsSettingAllowedForType(profile()->GetPrefs(),
+                                                      setting,
+                                                      content_type));
 
   ExtensionPrefsScope scope = kExtensionPrefsScopeRegular;
   if (details->HasKey(pref_keys::kScopeKey)) {
