d2de48324209c7068b4753a6ca55460b876e2dcf
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 19:52:44 +0000

    NaCl: Remove unused #include of nrd_xfer_effector.h
    This will enable me to remove this header from the NaCl side, since it is no
    longer used there.
    
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2550
    TEST=build
    
    Review URL: https://chromiumcodereview.appspot.com/10440006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138869 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/service_runtime.cc
+++ ppapi/native_client/src/trusted/plugin/service_runtime.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // remove when we no longer need to cast the DescWrapper below.
 #include "native_client/src/trusted/desc/nacl_desc_io.h"
 #include "native_client/src/trusted/desc/nrd_xfer.h"
-#include "native_client/src/trusted/desc/nrd_xfer_effector.h"
 #include "native_client/src/trusted/nonnacl_util/sel_ldr_launcher.h"
 #include "native_client/src/trusted/plugin/manifest.h"
 
