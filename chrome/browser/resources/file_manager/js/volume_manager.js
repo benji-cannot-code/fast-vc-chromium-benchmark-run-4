c62ee04bc85993a22accc07762f06a52f1314914
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 12:34:27 +0000

    [File Manager] Updated volume icons
    BUG=127913
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10399062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138786 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/volume_manager.js
+++ chrome/browser/resources/file_manager/js/volume_manager.js
@@ -47,7 +47,7 @@ VolumeManager.Error = {
   UNKNOWN: 'error_unknown',
   INTERNAL: 'error_internal',
   UNKNOWN_FILESYSTEM: 'error_unknown_filesystem',
-  UNSUPPORTED_FILESYSTEM: 'error_unsuported_filesystem',
+  UNSUPPORTED_FILESYSTEM: 'error_unsupported_filesystem',
   INVALID_ARCHIVE: 'error_invalid_archive',
   LIBCROS_MISSING: 'error_libcros_missing',
   AUTHENTICATION: 'error_authentication',
@@ -214,6 +214,7 @@ VolumeManager.prototype.makeVolumeInfo_ = function(
    callback({
      mountPath: mountPath,
      error: error,
+     deviceType: metadata && metadata.deviceType,
      readonly: !!metadata && metadata.isReadOnly
    });
   }
@@ -313,6 +314,15 @@ VolumeManager.prototype.isUnreadable = function(mountPath) {
   var error = this.getMountError(mountPath);
   return error == VolumeManager.Error.UNKNOWN_FILESYSTEM ||
          error == VolumeManager.Error.UNSUPPORTED_FILESYSTEM;
+};
+
+/**
+ * @param {string} mountPath Volume mounted path.
+ * @return {string} Device type ('usb'|'sd'|'optical'|'mobile'|'unknown')
+ *   (as defined in chrome/browser/chromeos/disks/disk_mount_manager.cc).
+ */
+VolumeManager.prototype.getDeviceType = function(mountPath) {
+  return this.getVolumeInfo_(mountPath).deviceType;
 };
 
 /**
