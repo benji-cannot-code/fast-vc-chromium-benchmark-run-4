ba28894bc091d37cc60b320fd15cb9fb9f11f7b1
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 21:22:04 +0000

    [chromeos] Fix testDefaultVolume
    Float values should never be done an exact match. Precision errors will make tests fail
    such tat 69.999999 and 70 won't match, even though they are equal for all practical
    purposes. Uses assertAlmostEqual() for float values
    
    TEST=None
    BUG=None
    
    R=rohitbm@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10451019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138892 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/chromeos_volume.py
+++ chrome/test/functional/chromeos_volume.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -48,12 +48,13 @@ class ChromeosVolume(pyauto.PyUITest):
     default_volume = self.GetPrivateInfo()['default_volume']
     assert default_volume.get(board_name), \
            'No volume settings available for %s.' % board_name
-    volume_dict = {u'volume': default_volume[board_name],
-                   u'is_mute': default_volume['is_mute']}
+    expected = {u'volume': default_volume[board_name],
+                u'is_mute': default_volume['is_mute']}
     volume = self.GetVolumeInfo()
-    self.assertEqual(volume, volume_dict, 
-        msg='Volume settings are set to %s, not matching with default ' \
-        'volume settings %s.' % (volume, volume_dict))
+    self.assertEqual(volume.get('is_mute'), expected.get('is_mute'))
+    self.assertAlmostEqual(volume.get('volume'), expected.get('volume'),
+        msg='Volume settings are set to %s, not matching with default '
+            'volume settings %s.' % (volume, expected))
 
   def testLoginLogoutVolume(self):
     """Test that volume settings are preserved after login and logout"""
@@ -61,13 +62,13 @@ class ChromeosVolume(pyauto.PyUITest):
     self._Login()
     after_login = self.GetVolumeInfo()
     self.assertEqual(before_login, after_login,
-        msg='Before login : %s and after login : %s, volume states are not '\
-        'matching' % (before_login, after_login))
+        msg='Before login : %s and after login : %s, volume states are not '
+            'matching' % (before_login, after_login))
     self.Logout()
     after_logout = self.GetVolumeInfo()
     self.assertEqual(after_login, after_logout,
-        msg='Before logout : %s and after logout : %s, volume states are not '\
-        'matching' % (after_login, after_logout))
+        msg='Before logout : %s and after logout : %s, volume states are not '
+            'matching' % (after_login, after_logout))
     # For successive tests
     self._Login()
 
