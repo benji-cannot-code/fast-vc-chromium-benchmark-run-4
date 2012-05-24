4bfc3f9e9e57306f100f1aa86c0caf7c0212f1b5
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-24 16:29:22 +0000

    [Chromium] Move queryLocalizedString to Platform.h https://bugs.webkit.org/show_bug.cgi?id=85762
    Reviewed by Adam Barth.
    
    Part of a refactoring series. See tracking bug 82948.
    
    Source/Platform:
    
    * Platform.gypi:
    * chromium/public/Platform.h:
    (WebKit):
    (Platform):
    (WebKit::Platform::queryLocalizedString):
    * chromium/public/WebLocalizedString.h: Added.
    (WebKit):
    
    Source/WebKit/chromium:
    
    * WebKit.gyp:
    * public/platform/WebKitPlatformSupport.h:
    (WebKit::WebKitPlatformSupport::getPluginList):
    * public/platform/WebLocalizedString.h:
    * src/LocalizedStrings.cpp:
    (WebCore::query):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/Platform.h
+++ third_party/WebKit/Source/Platform/chromium/public/Platform.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebData.h"
 #include "WebGamepads.h"
 #include "WebGraphicsContext3D.h"
+#include "WebLocalizedString.h"
 #include "WebString.h"
 
 namespace WebKit {
@@ -60,6 +61,7 @@ class WebSocketStreamHandle;
 class WebThemeEngine;
 class WebThread;
 class WebWorkerRunLoop;
+struct WebLocalizedString;
 
 class Platform {
 public:
@@ -164,6 +166,14 @@ public:
 
     // A suggestion to cache this metadata in association with this URL.
     virtual void cacheMetadata(const WebURL&, double responseTime, const char* data, size_t dataSize) { }
+
+
+    // Resources -----------------------------------------------------------
+
+    // Returns a localized string resource (with substitution parameters).
+    virtual WebString queryLocalizedString(WebLocalizedString::Name) { return WebString(); }
+    virtual WebString queryLocalizedString(WebLocalizedString::Name, const WebString& parameter) { return WebString(); }
+    virtual WebString queryLocalizedString(WebLocalizedString::Name, const WebString& parameter1, const WebString& parameter2) { return WebString(); }
 
 
     // Threads -------------------------------------------------------
