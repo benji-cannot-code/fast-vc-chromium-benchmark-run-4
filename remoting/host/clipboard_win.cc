e656f6139ad7831b367201acd6e93fe57100fcaa
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-24 10:07:17 +0000

    Add missing base/threading/platform_thread import.
    TBR=ajwong@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10447011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138761 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/clipboard_win.cc
+++ remoting/host/clipboard_win.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/process_util.h"
 #include "base/string16.h"
+#include "base/threading/platform_thread.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/scoped_hglobal.h"
 #include "base/win/windows_version.h"
