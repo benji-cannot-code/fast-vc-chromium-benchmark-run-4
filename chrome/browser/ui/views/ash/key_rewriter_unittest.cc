c05850c0d5b67f0923e4c44272a94347c5ba59b5
yusukes@google.com <yusukes@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 04:36:21 +0000

    Fix win_aura build failure caused by r138988.
    BUG=None
    TEST=try
    TBR=derat@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10445030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138990 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/key_rewriter_unittest.cc
+++ chrome/browser/ui/views/ash/key_rewriter_unittest.cc
@@ -143,6 +143,12 @@ class KeyRewriterTest : public testing::Test {
 };
 
 }  // namespace
+#else
+class KeyRewriterTest : public testing::Test {
+ public:
+  KeyRewriterTest() {}
+  virtual ~KeyRewriterTest() {}
+};
 #endif
 
 TEST_F(KeyRewriterTest, TestGetDeviceType) {
