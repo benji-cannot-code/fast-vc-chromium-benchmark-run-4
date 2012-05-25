fd3ebab6e8d9657a2b47adcdabaabeb4a3e30696
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 00:08:09 +0000

    Rename some chrome.exe exports: SetCommandLine --> SetCommandLine2, SetExperimentList --> SetExperimentList2.
    This will avoid mismatched parameter expectations when there is version shear, since their implementations were recently changed.
    BUG=129390,128541
    
    
    Review URL: https://chromiumcodereview.appspot.com/10422019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@138935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_field_trial_win.cc
+++ chrome/app/breakpad_field_trial_win.cc
@@ -13,7 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/breakpad_win.h"
 #include "chrome/common/child_process_logging.h"
 
-extern "C" void __declspec(dllexport) __cdecl SetExperimentList(
+// Note that this is suffixed with "2" due to a parameter change that was made
+// to the predecessor "SetExperimentList()". If the signature changes again, use
+// a new name.
+extern "C" void __declspec(dllexport) __cdecl SetExperimentList2(
       const wchar_t** experiment_strings, size_t experiment_strings_size) {
   // Make sure we were initialized before we start writing data
   if (breakpad_win::g_experiment_chunks_offset == 0)
@@ -69,7 +72,7 @@ namespace testing {
 void SetExperimentList(const std::vector<string16>& experiment_strings) {
   std::vector<const wchar_t*> cstrings;
   StringVectorToCStringVector(experiment_strings, &cstrings);
-  ::SetExperimentList(&cstrings[0], cstrings.size());
+  ::SetExperimentList2(&cstrings[0], cstrings.size());
 }
 
 }  // namespace testing
