cfe6f9d80da0fb83e8c4c3879f65efa417f63754
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 21:37:32 +0000

    browser: Fix typo in comment referring to nonexistent classes.
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10444037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/repost_form_warning_controller.h
+++ chrome/browser/repost_form_warning_controller.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,8 +15,8 @@ class NavigationController;
 }
 
 // This class is used to continue or cancel a pending reload when the
-// repost form warning is shown. It is owned by the platform-dependent
-// |RepostFormWarning{Gtk,Mac,View}| classes.
+// repost form warning is shown. It is owned by the platform-specific
+// |TabModalConfirmDialog{Gtk, Mac, Views, WebUI}| classes.
 class RepostFormWarningController : public TabModalConfirmDialogDelegate {
  public:
   explicit RepostFormWarningController(content::WebContents* web_contents);
