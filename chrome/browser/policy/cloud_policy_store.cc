cedb2a44daf1781450f93c6edc0db31d28562162
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 09:40:19 +0000

    Add CloudPolicyService
    CloudPolicyService glues together a cloud policy client and a CloudPolicyStore, taking care of pushing policy to the store when new policy from the cloud arrived. It also handles coordination for policy refresh operations.
    
    BUG=chromium:108928
    TEST=Unit tests.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10092010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139014 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/policy/cloud_policy_store.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/policy/cloud_policy_store.h"
+
+namespace policy {
+
+CloudPolicyStore::Observer::~Observer() {}
+
+CloudPolicyStore::CloudPolicyStore()
+    : status_(STATUS_OK),
+      is_initialized_(false) {}
+
+CloudPolicyStore::~CloudPolicyStore() {}
+
+void CloudPolicyStore::AddObserver(CloudPolicyStore::Observer* observer) {
+  observers_.AddObserver(observer);
+}
+
+void CloudPolicyStore::RemoveObserver(CloudPolicyStore::Observer* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+void CloudPolicyStore::NotifyStoreLoaded() {
+  is_initialized_ = true;
+  FOR_EACH_OBSERVER(Observer, observers_, OnStoreLoaded(this));
+}
+
+void CloudPolicyStore::NotifyStoreError() {
+  is_initialized_ = true;
+  FOR_EACH_OBSERVER(Observer, observers_, OnStoreError(this));
+}
+
+}  // namespace
