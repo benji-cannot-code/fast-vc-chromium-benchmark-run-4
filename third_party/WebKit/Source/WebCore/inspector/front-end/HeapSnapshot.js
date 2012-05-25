fdd7cf1876a864a7bbd30f2deb583e4fbafa3bc2
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-25 15:32:52 +0000

    Web Inspector: Speed up edges iteration in heap profiler https://bugs.webkit.org/show_bug.cgi?id=87286
    Add an extra node to nodes array that points to the end of edges array.
    It allows to eliminate a check for the last node in iteration code.
    
    Patch by Alexei Filippov <alexeif@chromium.org> on 2012-05-25
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    * inspector/front-end/HeapSnapshot.js:
    (WebInspector.HeapSnapshotNode.prototype._edgeIndexesStart):
    (WebInspector.HeapSnapshotNode.prototype._edgeIndexesEnd):
    (WebInspector.HeapSnapshotNodeIterator):
    (WebInspector.HeapSnapshot.prototype._buildRetainers):
    (WebInspector.HeapSnapshot.prototype._bfs):
    (WebInspector.HeapSnapshot.prototype._buildAggregates):
    (WebInspector.HeapSnapshot.prototype._buildPostOrderIndex):
    (WebInspector.HeapSnapshot.prototype._buildDominatorTree):
    (WebInspector.HeapSnapshot.prototype._markQueriableHeapObjects):
    
    LayoutTests:
    
    * inspector/profiler/heap-snapshot-test.js:
    (initialize_HeapSnapshotTest.InspectorTest.createHeapSnapshotMockObject):
    (initialize_HeapSnapshotTest.InspectorTest._postprocessHeapSnapshotMock):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118528 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/HeapSnapshot.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/HeapSnapshot.js
@@ -536,16 +536,14 @@ WebInspector.HeapSnapshotNode.prototype = {
 
     _edgeIndexesStart: function()
     {
-        return this._snapshot._nodes[this.nodeIndex + this._snapshot._firstEdgeIndexOffset];
+        var snapshot = this._snapshot;
+        return snapshot._nodes[this.nodeIndex + snapshot._firstEdgeIndexOffset];
     },
 
     _edgeIndexesEnd: function()
     {
-        var nextNodeIndex = this._nextNodeIndex();
         var snapshot = this._snapshot;
-        if (nextNodeIndex < snapshot._nodes.length)
-            return snapshot._nodes[nextNodeIndex + snapshot._firstEdgeIndexOffset]
-        return snapshot._containmentEdges.length;
+        return snapshot._nodes[this._nextNodeIndex() + snapshot._firstEdgeIndexOffset]
     },
 
     _nextNodeIndex: function()
@@ -566,7 +564,7 @@ WebInspector.HeapSnapshotNode.prototype = {
 WebInspector.HeapSnapshotNodeIterator = function(node)
 {
     this.node = node;
-    this._nodesLength = node._snapshot._nodes.length;
+    this._nodesLength = node._snapshot._realNodesLength;
 }
 
 WebInspector.HeapSnapshotNodeIterator.prototype = {
@@ -691,8 +689,15 @@ WebInspector.HeapSnapshot.prototype = {
             visitedMarker:     0x10000  // bits: 1,0000,0000,0000,0000
         };
 
-        this.nodeCount = this._nodes.length / this._nodeFieldCount;
+        this._realNodesLength = this._nodes.length;
+        this.nodeCount = this._realNodesLength / this._nodeFieldCount;
         this._edgeCount = this._containmentEdges.length / this._edgeFieldsCount;
+
+        // Add an extra node and make its first edge field point to the end of edges array.
+        var nodes = this._nodes;
+        this._nodes = new Uint32Array(this._realNodesLength + this._nodeFieldCount);
+        this._nodes.set(nodes);
+        this._nodes[this._realNodesLength + this._firstEdgeIndexOffset] = this._containmentEdges.length;
 
         this._markInvisibleEdges();
         this._buildRetainers();
@@ -736,13 +741,11 @@ WebInspector.HeapSnapshot.prototype = {
 
         var srcNodeIndex = 0;
         var nextNodeFirstEdgeIndex = nodes[firstEdgeIndexOffset];
-        var nodesLength = nodes.length;
+        var nodesLength = this._realNodesLength;
         while (srcNodeIndex < nodesLength) {
             var firstEdgeIndex = nextNodeFirstEdgeIndex;
             var nextNodeIndex = srcNodeIndex + nodeFieldCount;
-            nextNodeFirstEdgeIndex = nextNodeIndex < nodesLength
-                                   ? nodes[nextNodeIndex + firstEdgeIndexOffset]
-                                   : containmentEdges.length;
+            nextNodeFirstEdgeIndex = nodes[nextNodeIndex + firstEdgeIndexOffset];
             for (var edgeIndex = firstEdgeIndex; edgeIndex < nextNodeFirstEdgeIndex; edgeIndex += edgeFieldsCount) {
                 var toNodeIndex = containmentEdges[edgeIndex + edgeToNodeOffset];
                 if (toNodeIndex % nodeFieldCount)
@@ -925,9 +928,7 @@ WebInspector.HeapSnapshot.prototype = {
             }
             var distance = distances[nodeOrdinal] + 1;
             var firstEdgeIndex = nodes[nodeIndex + firstEdgeIndexOffset];
-            var edgesEnd = nodeOrdinal < nodeCount - 1
-                         ? nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount]
-                         : containmentEdges.length;
+            var edgesEnd = nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount];
             for (var edgeToNodeIndex = firstEdgeIndex + edgeToNodeOffset; edgeToNodeIndex < edgesEnd; edgeToNodeIndex += edgeFieldsCount) {
                 var childNodeIndex = containmentEdges[edgeToNodeIndex];
                 var childNodeOrdinal = childNodeIndex / nodeFieldCount;
@@ -946,7 +947,7 @@ WebInspector.HeapSnapshot.prototype = {
         var classIndexes = [];
         var nodes = this._nodes;
         var flags = this._flags;
-        var nodesLength = nodes.length;
+        var nodesLength = this._realNodesLength;
         var nodeNativeType = this._nodeNativeType;
         var nodeFieldCount = this._nodeFieldCount;
         var selfSizeOffset = this._nodeSelfSizeOffset;
@@ -1098,9 +1099,7 @@ WebInspector.HeapSnapshot.prototype = {
                 painted[nodeOrdinal] = black;
                 var nodeFlag = flags[nodeOrdinal] & flag;
                 var beginEdgeIndex = nodes[nodeIndex + firstEdgeIndexOffset];
-                var endEdgeIndex =  nodeOrdinal < nodeCount - 1
-                                    ? nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount]
-                                    : containmentEdgesLength;
+                var endEdgeIndex = nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount];
                 for (var edgeIndex = beginEdgeIndex; edgeIndex < endEdgeIndex; edgeIndex += edgeFieldsCount) {
                     if (nodeIndex !== rootNodeIndex && containmentEdges[edgeIndex + edgeTypeOffset] === edgeShortcutType)
                         continue;
@@ -1231,9 +1230,7 @@ WebInspector.HeapSnapshot.prototype = {
                     nodeOrdinal = postOrderIndex2NodeOrdinal[postOrderIndex];
                     nodeIndex = nodeOrdinal * nodeFieldCount;
                     beginEdgeToNodeFieldIndex = nodes[nodeIndex + firstEdgeIndexOffset] + edgeToNodeOffset;
-                    endEdgeToNodeFieldIndex = nodeOrdinal < nodesCount - 1
-                                                  ? nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount]
-                                                  : containmentEdgesLength;
+                    endEdgeToNodeFieldIndex = nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount];
                     for (var toNodeFieldIndex = beginEdgeToNodeFieldIndex;
                          toNodeFieldIndex < endEdgeToNodeFieldIndex;
                          toNodeFieldIndex += edgeFieldsCount) {
@@ -1459,9 +1456,7 @@ WebInspector.HeapSnapshot.prototype = {
                 continue;
             flags[nodeOrdinal] |= flag;
             var beginEdgeIndex = nodes[nodeIndex + firstEdgeIndexOffset];
-            var endEdgeIndex = nodeOrdinal < nodeCount - 1
-                               ? nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount]
-                               : containmentEdges.length;
+            var endEdgeIndex = nodes[nodeIndex + firstEdgeIndexOffset + nodeFieldCount];
             for (var edgeIndex = beginEdgeIndex; edgeIndex < endEdgeIndex; edgeIndex += edgeFieldsCount) {
                 var childNodeIndex = containmentEdges[edgeIndex + edgeToNodeOffset];
                 if (flags[childNodeIndex / nodeFieldCount] & flag)
