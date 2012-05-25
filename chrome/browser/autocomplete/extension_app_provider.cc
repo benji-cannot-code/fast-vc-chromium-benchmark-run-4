25d37435516e5135ffeb7168b15d54af3bd91ad6
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 19:32:25 +0000

    Omnibox: Make providers respect FORCED_QUERY
    The HistoryContentsProvider, ExtensionProvider, and ShortcutsProvider are erroneously returning results for FORCED_QUERY omnibox inputs such as ?fac.  This means URLs show up as suggestions when they shouldn't.  This fixes it.
    
    BUG=
    TEST=type ?fac in the omnibox
    
    Review URL: https://chromiumcodereview.appspot.com/10443012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/extension_app_provider.cc
+++ chrome/browser/autocomplete/extension_app_provider.cc
@@ -96,7 +96,8 @@ void ExtensionAppProvider::Start(const AutocompleteInput& input,
                                  bool minimal_changes) {
   matches_.clear();
 
-  if (input.type() == AutocompleteInput::INVALID)
+  if ((input.type() == AutocompleteInput::INVALID) ||
+      (input.type() == AutocompleteInput::FORCED_QUERY))
     return;
 
   if (input.text().empty())
