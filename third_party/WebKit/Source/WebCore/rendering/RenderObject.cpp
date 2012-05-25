20714ebdb67332f40c676c85bbe4eeb64cb76501
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-25 01:49:40 +0000

    Unreviewed, rolling out r118452. http://trac.webkit.org/changeset/118452 https://bugs.webkit.org/show_bug.cgi?id=87446
    Causes many tests to assert on Mac, NRWT bails out (Requested
    by sundiamonde on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-05-24
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::~RenderObject):
    (WebCore::RenderObject::clearLayoutRootIfNeeded):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -220,7 +220,7 @@ RenderObject::RenderObject(Node* node)
 
 RenderObject::~RenderObject()
 {
-    ASSERT(documentBeingDestroyed() || !frame()->view() || frame()->view()->layoutRoot() != this);
+    ASSERT(!node() || documentBeingDestroyed() || !frame()->view() || frame()->view()->layoutRoot() != this);
 #ifndef NDEBUG
     ASSERT(!m_hasAXObject);
     renderObjectCounter.decrement();
@@ -2261,7 +2261,7 @@ bool RenderObject::isSelectionBorder() const
 
 inline void RenderObject::clearLayoutRootIfNeeded() const
 {
-    if (!documentBeingDestroyed() && frame()) {
+    if (node() && !documentBeingDestroyed() && frame()) {
         if (FrameView* view = frame()->view()) {
             if (view->layoutRoot() == this) {
                 ASSERT_NOT_REACHED();
