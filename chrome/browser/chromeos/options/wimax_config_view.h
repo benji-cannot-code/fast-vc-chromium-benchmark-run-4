f3c4219f091ab6bbe7f410d67cdbc0ab891b25cf
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 23:41:07 +0000

    Obey Wimax save credentials setting.
    Note: the default for wimax->save_credentials() is false, so for a new wimax network the checkbox will be unchecked initially.
    Also, it doesn't make any sense to "share" a network if credentials are not saved, so that will be hidden also when "save credentials" is hidden.
    
    BUG=128429
    TEST=Ensure that save credentials + share checkboxes are hidden during login and visible after login for secure Wimax networks.
    
    Review URL: https://chromiumcodereview.appspot.com/10453010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139154 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/wimax_config_view.h
+++ chrome/browser/chromeos/options/wimax_config_view.h
@@ -67,9 +67,6 @@ class WimaxConfigView : public ChildNetworkConfigView,
   // Updates state of the Login button.
   void UpdateDialogButtons();
 
-  // Enable/Disable "share this network" checkbox.
-  void RefreshShareCheckbox();
-
   // Updates the error text label.
   void UpdateErrorLabel();
 
