175cc76e2bb83a0943dbded7b201fe9ddee22cc7
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-25 20:02:24 +0000

    Unreviewed, rolling out r112155. http://trac.webkit.org/changeset/112155 https://bugs.webkit.org/show_bug.cgi?id=79389 Hitch (due to style recalc?) when starting CSS3 animation
    This caused a number of issues, including:
    https://bugs.webkit.org/show_bug.cgi?id=87146
    https://bugs.webkit.org/show_bug.cgi?id=84194
    <rdar://problem/11506629>
    <rdar://problem/11267408>
    <rdar://problem/11531859>
    
    * dom/Element.cpp:
    (WebCore::Element::recalcStyle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -1116,7 +1116,7 @@ void Element::recalcStyle(StyleChange change)
         }
 
         if (change != Force) {
-            if (styleChangeType() == FullStyleChange)
+            if (styleChangeType() >= FullStyleChange)
                 change = Force;
             else
                 change = ch;
