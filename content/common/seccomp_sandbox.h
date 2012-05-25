98903026f877768e8059a155f71ba74faa08e8c9
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 16:31:43 +0000

    Build seccomp sandbox code even when Clang is used
    There used to be an issue with the seccomp sandbox when Clang was used. Since
    this is no longer the case, there is no reason to undefine SECCOMP_SANDBOX
    when building on Clang.
    
    BUG=
    TEST=
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/10448017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139056 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/seccomp_sandbox.h
+++ content/common/seccomp_sandbox.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,8 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 
 #if defined(ARCH_CPU_X86_FAMILY) && !defined(CHROMIUM_SELINUX) && \
-  !defined(__clang__) && !defined(OS_CHROMEOS) && !defined(TOOLKIT_VIEWS) && \
-  !defined(OS_OPENBSD)
+  !defined(OS_CHROMEOS) && !defined(TOOLKIT_VIEWS) && !defined(OS_OPENBSD)
 #define SECCOMP_SANDBOX
 #include "seccompsandbox/sandbox.h"
 #endif
