0bd9e413d2bc295a231b0283389bde8bcca8b534
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-25 23:18:26 +0000

    browser: Rename js_modal_* to javascript_app_modal_*
    And also rename the classes from JSModal* to JavaScriptAppModal*.
    
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10441036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/javascript_app_modal_dialog_cocoa.h
+++ chrome/browser/ui/cocoa/javascript_app_modal_dialog_cocoa.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_COCOA_JS_MODAL_DIALOG_COCOA_H_
-#define CHROME_BROWSER_UI_COCOA_JS_MODAL_DIALOG_COCOA_H_
+#ifndef CHROME_BROWSER_UI_COCOA_JAVASCRIPT_APP_MODAL_DIALOG_COCOA_H_
+#define CHROME_BROWSER_UI_COCOA_JAVASCRIPT_APP_MODAL_DIALOG_COCOA_H_
 #pragma once
 
 #include "chrome/browser/ui/app_modal_dialogs/native_app_modal_dialog.h"
@@ -21,10 +21,10 @@ class NSAlert;
 class JavaScriptAppModalDialogHelper;
 #endif
 
-class JSModalDialogCocoa : public NativeAppModalDialog {
+class JavaScriptAppModalDialogCocoa : public NativeAppModalDialog {
  public:
-  explicit JSModalDialogCocoa(JavaScriptAppModalDialog* dialog);
-  virtual ~JSModalDialogCocoa();
+  explicit JavaScriptAppModalDialogCocoa(JavaScriptAppModalDialog* dialog);
+  virtual ~JavaScriptAppModalDialogCocoa();
 
   // Overridden from NativeAppModalDialog:
   virtual int GetAppModalDialogButtons() const OVERRIDE;
@@ -42,8 +42,7 @@ class JSModalDialogCocoa : public NativeAppModalDialog {
   scoped_nsobject<JavaScriptAppModalDialogHelper> helper_;
   NSAlert* alert_; // weak, owned by |helper_|.
 
-  DISALLOW_COPY_AND_ASSIGN(JSModalDialogCocoa);
+  DISALLOW_COPY_AND_ASSIGN(JavaScriptAppModalDialogCocoa);
 };
 
-#endif  // CHROME_BROWSER_UI_COCOA_JS_MODAL_DIALOG_COCOA_H_
-
+#endif  // CHROME_BROWSER_UI_COCOA_JAVASCRIPT_APP_MODAL_DIALOG_COCOA_H_
--- chrome/browser/ui/views/javascript_app_modal_dialog_views.h
+++ chrome/browser/ui/views/javascript_app_modal_dialog_views.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_JS_MODAL_DIALOG_VIEWS_H_
-#define CHROME_BROWSER_UI_VIEWS_JS_MODAL_DIALOG_VIEWS_H_
+#ifndef CHROME_BROWSER_UI_VIEWS_JAVASCRIPT_APP_MODAL_DIALOG_VIEWS_H_
+#define CHROME_BROWSER_UI_VIEWS_JAVASCRIPT_APP_MODAL_DIALOG_VIEWS_H_
 #pragma once
 
 #include "base/memory/scoped_ptr.h"
@@ -17,11 +17,11 @@ namespace views {
 class MessageBoxView;
 }
 
-class JSModalDialogViews : public NativeAppModalDialog,
-                           public views::DialogDelegate {
+class JavaScriptAppModalDialogViews : public NativeAppModalDialog,
+                                      public views::DialogDelegate {
  public:
-  explicit JSModalDialogViews(JavaScriptAppModalDialog* parent);
-  virtual ~JSModalDialogViews();
+  explicit JavaScriptAppModalDialogViews(JavaScriptAppModalDialog* parent);
+  virtual ~JavaScriptAppModalDialogViews();
 
   // Overridden from NativeAppModalDialog:
   virtual int GetAppModalDialogButtons() const OVERRIDE;
@@ -56,7 +56,7 @@ class JSModalDialogViews : public NativeAppModalDialog,
   // The message box view whose commands we handle.
   views::MessageBoxView* message_box_view_;
 
-  DISALLOW_COPY_AND_ASSIGN(JSModalDialogViews);
+  DISALLOW_COPY_AND_ASSIGN(JavaScriptAppModalDialogViews);
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_JS_MODAL_DIALOG_VIEWS_H_
+#endif  // CHROME_BROWSER_UI_VIEWS_JAVASCRIPT_APP_MODAL_DIALOG_VIEWS_H_
