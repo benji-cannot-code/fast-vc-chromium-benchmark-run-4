60419dd0ab1a11be2ea1747464db0fbe80c05b46
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-26 05:28:10 +0000

    Avoid the need for a valgrind suppression by initializing memory fully.  The use of memcmp to compare two WebMouseEvent objects causes us to need to worry about how the structure padding is initialized.
    R=jamesr@chromium.org
    BUG=102622
    
    Review URL: https://chromiumcodereview.appspot.com/10445048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139190 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/input_event_filter_unittest.cc
+++ content/renderer/gpu/input_event_filter_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -94,6 +94,9 @@ class IPCMessageRecorder : public IPC::Channel::Listener {
 
 void InitMouseEvent(WebMouseEvent* event, WebInputEvent::Type type,
                     int x, int y) {
+  // Avoid valgrind false positives by initializing memory completely.
+  memset(event, 0, sizeof(*event));
+
   new (event) WebMouseEvent();
   event->type = type;
   event->x = x;
