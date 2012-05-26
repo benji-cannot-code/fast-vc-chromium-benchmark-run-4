e41631728edeb752a29e96365313df1aa9e0a450
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-26 00:36:56 +0000

    When BUNDLE is enabled in libjingle, there will be a possibility of sending multiple stun queries. That because PortAllocator will create only one real PortAlloctorSession and many proxies for that session. Proxies can come and go but the real impl of PortAllocatorSession will remain. Due to this P2PTC channel will call GetInitialPorts and StartGetAllPorts many times.
    Review URL: https://chromiumcodereview.appspot.com/10444032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139166 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/p2p/host_address_request.cc
+++ content/renderer/p2p/host_address_request.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -29,6 +29,7 @@ P2PHostAddressRequest::~P2PHostAddressRequest() {
 void P2PHostAddressRequest::Request(const std::string& host_name,
                                     const DoneCallback& done_callback) {
   DCHECK(delegate_message_loop_->BelongsToCurrentThread());
+  DCHECK_EQ(STATE_CREATED, state_);
 
   state_ = STATE_SENT;
   ipc_message_loop_->PostTask(FROM_HERE, base::Bind(
