455a91b22e8ae27fa4de9921437711263a0494df
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-26 00:55:13 +0000

    Remove Pepper Transport API transport.
    BUG=109630
    
    Review URL: https://chromiumcodereview.appspot.com/10442040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139170 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/connection_to_host.cc
+++ remoting/protocol/connection_to_host.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/clipboard_stub.h"
 #include "remoting/protocol/errors.h"
 #include "remoting/protocol/jingle_session_manager.h"
-#include "remoting/protocol/pepper_transport_factory.h"
+#include "remoting/protocol/transport.h"
 #include "remoting/protocol/video_reader.h"
 #include "remoting/protocol/video_stub.h"
 #include "remoting/protocol/util.h"
