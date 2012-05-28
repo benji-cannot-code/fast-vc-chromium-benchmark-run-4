6bae784edc40067fabf913db5e99cb0c4e482185
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-28 08:21:07 +0000

    media/audio: Update doc comment to refer to the right function name.
    Instead of set_factory, it's set_factory_for_testing.
    
    R=tommi@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10441061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139227 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/test_audio_input_controller_factory.h
+++ media/audio/test_audio_input_controller_factory.h
@@ -26,7 +26,7 @@ class TestAudioInputControllerFactory;
 // Typical usage:
 //   // Create and register factory.
 //   TestAudioInputControllerFactory factory;
-//   AudioInputController::set_factory(&factory);
+//   AudioInputController::set_factory_for_testing(&factory);
 //
 //   // Do something that triggers creation of an AudioInputController.
 //   TestAudioInputController* controller = factory.last_controller();
@@ -42,7 +42,7 @@ class TestAudioInputControllerFactory;
 //   // Make sure consumer of AudioInputController does the right thing.
 //   ...
 //   // Reset factory.
-//   AudioInputController::set_factory(NULL);
+//   AudioInputController::set_factory_for_testing(NULL);
 
 class TestAudioInputController : public AudioInputController {
  public:
