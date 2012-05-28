f64d0d3151fd27f242c02f40d0e35dcfc39f1dc0
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-28 12:40:29 +0000

    Revert 139238 - Fix content detector nits.
    Fix the nits detected in https://chromiumcodereview.appspot.com/10187020/ after
    the patch was landed and the bug closed.
    
    BUG=125390
    TEST=Unit tests of each component (address/phone number/email).
    
    Review URL: https://chromiumcodereview.appspot.com/10443031
    
    TBR=leandrogracia@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10451053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139239 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/android/content_detector.h
+++ content/renderer/android/content_detector.h
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_ANDROID_CONTENT_DETECTOR_H_
 #pragma once
 
+#include "base/string_util.h"
 #include "googleurl/src/gurl.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebNode.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebRange.h"
 
 namespace WebKit {
@@ -19,21 +21,23 @@ namespace content {
 // Base class for text-based content detectors.
 class ContentDetector {
  public:
+
   // Holds the content detection results.
   struct Result {
+    bool valid; // Flag indicating if the result is valid.
+    WebKit::WebRange range; // Range describing the content boundaries.
+    std::string text; // Processed text of the content.
+    GURL intent_url; // URL of the intent that should process this content.
+
     Result() : valid(false) {}
-    Result(const WebKit::WebRange& content_boundaries,
+
+    Result(const WebKit::WebRange& range,
            const std::string& text,
            const GURL& intent_url)
         : valid(true),
-          content_boundaries(content_boundaries),
+          range(range),
           text(text),
           intent_url(intent_url) {}
-
-    bool valid;
-    WebKit::WebRange content_boundaries;
-    std::string text; // Processed text of the content.
-    GURL intent_url; // URL of the intent that should process this content.
   };
 
   virtual ~ContentDetector() {}
@@ -43,8 +47,6 @@ class ContentDetector {
   Result FindTappedContent(const WebKit::WebHitTestResult& hit_test);
 
  protected:
-  ContentDetector() {}
-
   // Parses the input string defined by the begin/end iterators returning true
   // if the desired content is found. The start and end positions relative to
   // the input iterators are returned in start_pos and end_pos.
@@ -62,6 +64,7 @@ class ContentDetector {
   // position in order to search for content.
   virtual size_t GetMaximumContentLength() = 0;
 
+  ContentDetector() {}
   WebKit::WebRange FindContentRange(const WebKit::WebHitTestResult& hit_test,
                                     std::string* content_text);
 
