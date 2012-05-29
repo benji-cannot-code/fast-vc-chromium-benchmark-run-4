2f99d98e2b81e2da768f252f1f4964ca787a8504
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-29 23:41:04 +0000

    uber frame: localStrings->loadTimeData
    BUG=122753
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10455015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139425 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/uber/uber.js
+++ chrome/browser/resources/uber/uber.js
@@ -4,8 +4,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 cr.define('uber', function() {
-  var localStrings = new LocalStrings();
-
   /**
    * Options for how web history should be handled.
    */
@@ -101,7 +99,7 @@ cr.define('uber', function() {
    * @return {Object} The default iframe container.
    */
   function getDefaultIframe() {
-    return $(localStrings.getString('helpHost'));
+    return $(loadTimeData.getString('helpHost'));
   }
 
   /**
--- chrome/browser/ui/webui/uber/uber_ui.cc
+++ chrome/browser/ui/webui/uber/uber_ui.cc
@@ -63,6 +63,8 @@ ChromeWebUIDataSource* CreateUberHTMLSource() {
   source->AddString("settingsHost",
                     ASCIIToUTF16(chrome::kChromeUISettingsHost));
 
+  source->set_use_json_js_format_v2();
+
   return source;
 }
 
