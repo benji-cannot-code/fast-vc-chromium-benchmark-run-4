f4491cc888330dfc407de7479933e949b278ffbf
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-29 23:43:16 +0000

    DFG should keep captured variables alive until the (inline) return. https://bugs.webkit.org/show_bug.cgi?id=87205
    Source/JavaScriptCore: 
    
    Reviewed by Gavin Barraclough.
            
    Changes the way we do flushing for captured variables and arguments. Instead of flushing
    each SetLocal immediately, we flush at kill points. So a SetLocal will cause a Flush of
    whatever was live in the variable previously, and a return will cause a Flush of all
    captured variables and all arguments.
    
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::setDirect):
    (JSC::DFG::ByteCodeParser::set):
    (JSC::DFG::ByteCodeParser::setLocal):
    (JSC::DFG::ByteCodeParser::getArgument):
    (JSC::DFG::ByteCodeParser::setArgument):
    (JSC::DFG::ByteCodeParser::findArgumentPositionForArgument):
    (ByteCodeParser):
    (JSC::DFG::ByteCodeParser::findArgumentPositionForLocal):
    (JSC::DFG::ByteCodeParser::findArgumentPosition):
    (JSC::DFG::ByteCodeParser::flush):
    (JSC::DFG::ByteCodeParser::flushDirect):
    (JSC::DFG::ByteCodeParser::flushArgumentsAndCapturedVariables):
    (JSC::DFG::ByteCodeParser::handleInlining):
    (JSC::DFG::ByteCodeParser::parseBlock):
    (JSC::DFG::ByteCodeParser::InlineStackEntry::InlineStackEntry):
    * dfg/DFGCSEPhase.cpp:
    (JSC::DFG::CSEPhase::setLocalStoreElimination):
    (JSC::DFG::CSEPhase::performNodeCSE):
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::SpeculativeJIT::forwardSpeculationCheck):
    
    LayoutTests: 
    
    Reviewed by Gavin Barraclough.
    
    * fast/js/dfg-inline-arguments-osr-exit-and-capture-expected.txt: Added.
    * fast/js/dfg-inline-arguments-osr-exit-and-capture.html: Added.
    * fast/js/script-tests/dfg-inline-arguments-osr-exit-and-capture.js: Added.
    (foo):
    (bar):
    (baz):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGCSEPhase.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGCSEPhase.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -621,7 +621,7 @@ private:
     }
     
     // This returns the Flush node that is keeping a SetLocal alive.
-    NodeIndex setLocalStoreElimination(VirtualRegister local)
+    NodeIndex setLocalStoreElimination(VirtualRegister local, NodeIndex expectedNodeIndex)
     {
         for (unsigned i = m_indexInBlock; i--;) {
             NodeIndex index = m_currentBlock->at(i);
@@ -630,7 +630,7 @@ private:
                 continue;
             switch (node.op()) {
             case GetLocal:
-            case SetLocal:
+            case Flush:
                 if (node.local() == local)
                     return NoNode;
                 break;
@@ -640,19 +640,13 @@ private:
                     return NoNode;
                 break;
                 
-            case Flush: {
+            case SetLocal: {
                 if (node.local() != local)
                     break;
-                if (!i)
-                    break;
-                NodeIndex prevIndex = m_currentBlock->at(i - 1);
-                if (prevIndex != node.child1().index())
-                    break;
-                ASSERT(m_graph[prevIndex].local() == local);
-                ASSERT(m_graph[prevIndex].variableAccessData() == node.variableAccessData());
-                ASSERT(m_graph[prevIndex].shouldGenerate());
-                if (m_graph[prevIndex].refCount() > 1)
-                    break;
+                if (index != expectedNodeIndex)
+                    return NoNode;
+                if (m_graph[index].refCount() > 1)
+                    return NoNode;
                 return index;
             }
                 
@@ -660,6 +654,14 @@ private:
                 if (m_graph.uncheckedActivationRegisterFor(node.codeOrigin) == local)
                     return NoNode;
                 break;
+                
+            case TearOffActivation:
+            case TearOffArguments:
+                // If an activation is being torn off then it means that captured variables
+                // are live. We could be clever here and check if the local qualifies as an
+                // argument register. But that seems like it would buy us very little since
+                // any kind of tear offs are rare to begin with.
+                return NoNode;
                 
             default:
                 if (m_graph.clobbersWorld(index))
@@ -856,27 +858,26 @@ private:
             break;
         }
             
-        case SetLocal: {
+        case Flush: {
             if (m_fixpointState == FixpointNotConverged)
                 break;
             VariableAccessData* variableAccessData = node.variableAccessData();
             if (!variableAccessData->isCaptured())
                 break;
             VirtualRegister local = variableAccessData->local();
-            NodeIndex replacementIndex = setLocalStoreElimination(local);
+            NodeIndex replacementIndex = setLocalStoreElimination(local, node.child1().index());
             if (replacementIndex == NoNode)
                 break;
             Node& replacement = m_graph[replacementIndex];
-            ASSERT(replacement.op() == Flush);
+            ASSERT(replacement.op() == SetLocal);
             ASSERT(replacement.refCount() == 1);
             ASSERT(replacement.shouldGenerate());
-            ASSERT(replacement.mustGenerate());
-            replacement.setOpAndDefaultFlags(Phantom);
-            NodeIndex setLocalIndex = replacement.child1().index();
-            ASSERT(m_graph[setLocalIndex].op() == SetLocal);
-            m_graph.clearAndDerefChild1(replacement);
-            replacement.children.child1() = m_graph[setLocalIndex].child1();
-            m_graph.ref(replacement.child1());
+            node.setOpAndDefaultFlags(Phantom);
+            NodeIndex dataNodeIndex = replacement.child1().index();
+            ASSERT(m_graph[dataNodeIndex].hasResult());
+            m_graph.clearAndDerefChild1(node);
+            node.children.child1() = Edge(dataNodeIndex);
+            m_graph.ref(dataNodeIndex);
             break;
         }
             
