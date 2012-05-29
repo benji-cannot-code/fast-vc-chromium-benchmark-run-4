5f7775d2571b646e698608365fe81160b6cc32b6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-29 17:50:56 +0000

    [BlackBerry] Make DefaultTapHighlight use the new WebOverlay API https://bugs.webkit.org/show_bug.cgi?id=87604
    Patch by Arvid Nilsson <anilsson@rim.com> on 2012-05-29
    Reviewed by Antonio Gomes.
    
    Also add a new method to allow the embedder to use the default tap
    highlight instead of replacing it with a custom one just to keep track
    of the "shouldHideAfterScroll" flag.
    
    PR #160262.
    
    * Api/WebTapHighlight.h:
    * WebKitSupport/DefaultTapHighlight.cpp:
    (BlackBerry::WebKit::DefaultTapHighlight::DefaultTapHighlight):
    (BlackBerry::WebKit::DefaultTapHighlight::draw):
    (BlackBerry::WebKit::DefaultTapHighlight::hide):
    (BlackBerry::WebKit::DefaultTapHighlight::paintContents):
    * WebKitSupport/DefaultTapHighlight.h:
    (BlackBerry::WebKit::DefaultTapHighlight::shouldHideAfterScroll):
    (DefaultTapHighlight):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DefaultTapHighlight.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DefaultTapHighlight.h
@@ -25,10 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 
 #include "Color.h"
-#include "GraphicsLayer.h"
 #include "GraphicsLayerClient.h"
+#include "WebOverlay.h"
 #include "WebTapHighlight.h"
+
 #include <BlackBerryPlatformIntRectRegion.h>
+#include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
 namespace BlackBerry {
@@ -48,26 +50,26 @@ public:
     virtual void draw(const Platform::IntRectRegion&, int red, int green, int blue, int alpha, bool hideAfterScroll);
     virtual void hide();
 
+    virtual bool shouldHideAfterScroll() const { return m_shouldHideAfterScroll; }
+
     // GraphicsLayerClient
     virtual void notifyAnimationStarted(const WebCore::GraphicsLayer*, double time) { }
     virtual void notifySyncRequired(const WebCore::GraphicsLayer*);
     virtual void paintContents(const WebCore::GraphicsLayer*, WebCore::GraphicsContext&, WebCore::GraphicsLayerPaintingPhase, const WebCore::IntRect& inClip);
     virtual bool showDebugBorders(const WebCore::GraphicsLayer*) const { return false; }
     virtual bool showRepaintCounter(const WebCore::GraphicsLayer*) const { return false; }
-
-#if PLATFORM(BLACKBERRY)
     virtual bool contentsVisible(const WebCore::GraphicsLayer*, const WebCore::IntRect& contentRect) const { return true; }
-#endif
 
 
 private:
     DefaultTapHighlight(WebPagePrivate*);
 
     WebPagePrivate* m_page;
-    OwnPtr<WebCore::GraphicsLayer> m_layer;
+    OwnPtr<WebOverlay> m_overlay;
     BlackBerry::Platform::IntRectRegion m_region;
     WebCore::Color m_color;
     bool m_visible;
+    bool m_shouldHideAfterScroll;
 };
 
 } // namespace WebKit
