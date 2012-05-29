04b386733239cfc99c333de5530451890b099ff4
philn@webkit.org <philn@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-29 16:27:08 +0000

    [GStreamer] Video sink doesn't respect WebKit code style https://bugs.webkit.org/show_bug.cgi?id=87693
    Reviewed by Alexis Menard.
    
    No change in behavior of the sink, this patch only adapts the code
    style to WebKit's.
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    * platform/graphics/gstreamer/VideoSinkGStreamer.cpp:
    (_WebKitVideoSinkPrivate):
    (webkitVideoSinkTimeoutCallback):
    (webkitVideoSinkRender):
    (webkitVideoSinkDispose):
    (unlockBufferMutex):
    (webkitVideoSinkUnlock):
    (webkitVideoSinkUnlockStop):
    (webkitVideoSinkStop):
    (webkitVideoSinkStart):
    (webkitVideoSinkMarshalVoidAndMiniObject):
    (webkit_video_sink_class_init):
    (webkitVideoSinkNew):
    * platform/graphics/gstreamer/VideoSinkGStreamer.h:
    (_WebKitVideoSink):
    (_WebKitVideoSinkClass):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/VideoSinkGStreamer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/VideoSinkGStreamer.h
@@ -26,46 +26,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GStreamerGWorld.h"
 #include <glib-object.h>
 #include <gst/video/gstvideosink.h>
-
-G_BEGIN_DECLS
+#include <gst/video/video.h>
 
 #define WEBKIT_TYPE_VIDEO_SINK webkit_video_sink_get_type()
 
-#define WEBKIT_VIDEO_SINK(obj) \
-    (G_TYPE_CHECK_INSTANCE_CAST((obj), \
-    WEBKIT_TYPE_VIDEO_SINK, WebKitVideoSink))
+#define WEBKIT_VIDEO_SINK(obj) (G_TYPE_CHECK_INSTANCE_CAST((obj), WEBKIT_TYPE_VIDEO_SINK, WebKitVideoSink))
+#define WEBKIT_VIDEO_SINK_CLASS(klass) (G_TYPE_CHECK_CLASS_CAST((klass), WEBKIT_TYPE_VIDEO_SINK, WebKitVideoSinkClass))
+#define WEBKIT_IS_VIDEO_SINK(obj) (G_TYPE_CHECK_INSTANCE_TYPE((obj), WEBKIT_TYPE_VIDEO_SINK))
+#define WEBKIT_IS_VIDEO_SINK_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE((klass), WEBKIT_TYPE_VIDEO_SINK))
+#define WEBKIT_VIDEO_SINK_GET_CLASS(obj) (G_TYPE_INSTANCE_GET_CLASS((obj), WEBKIT_TYPE_VIDEO_SINK, WebKitVideoSinkClass))
 
-#define WEBKIT_VIDEO_SINK_CLASS(klass) \
-    (G_TYPE_CHECK_CLASS_CAST((klass), \
-    WEBKIT_TYPE_VIDEO_SINK, WebKitVideoSinkClass))
-
-#define WEBKIT_IS_VIDEO_SINK(obj) \
-    (G_TYPE_CHECK_INSTANCE_TYPE((obj), \
-    WEBKIT_TYPE_VIDEO_SINK))
-
-#define WEBKIT_IS_VIDEO_SINK_CLASS(klass) \
-    (G_TYPE_CHECK_CLASS_TYPE((klass), \
-    WEBKIT_TYPE_VIDEO_SINK))
-
-#define WEBKIT_VIDEO_SINK_GET_CLASS(obj) \
-    (G_TYPE_INSTANCE_GET_CLASS((obj), \
-    WEBKIT_TYPE_VIDEO_SINK, WebKitVideoSinkClass))
-
-typedef struct _WebKitVideoSink        WebKitVideoSink;
-typedef struct _WebKitVideoSinkClass   WebKitVideoSinkClass;
+typedef struct _WebKitVideoSink WebKitVideoSink;
+typedef struct _WebKitVideoSinkClass WebKitVideoSinkClass;
 typedef struct _WebKitVideoSinkPrivate WebKitVideoSinkPrivate;
 
 struct _WebKitVideoSink {
-    /*< private >*/
     GstVideoSink parent;
-    WebKitVideoSinkPrivate *priv;
+    WebKitVideoSinkPrivate* priv;
 };
 
 struct _WebKitVideoSinkClass {
-    /*< private >*/
     GstVideoSinkClass parent_class;
 
-    /* Future padding */
+    // Future padding
     void (* _webkit_reserved1)(void);
     void (* _webkit_reserved2)(void);
     void (* _webkit_reserved3)(void);
@@ -74,11 +57,9 @@ struct _WebKitVideoSinkClass {
     void (* _webkit_reserved6)(void);
 };
 
+GType webkit_video_sink_get_type() G_GNUC_CONST;
 
-GType       webkit_video_sink_get_type(void) G_GNUC_CONST;
-GstElement *webkit_video_sink_new(WebCore::GStreamerGWorld*);
-
-G_END_DECLS
+GstElement* webkitVideoSinkNew(WebCore::GStreamerGWorld*);
 
 #endif // USE(GSTREAMER)
 #endif
