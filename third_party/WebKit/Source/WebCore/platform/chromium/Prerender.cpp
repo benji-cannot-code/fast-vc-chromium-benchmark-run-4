bd7ae51672ca98d8ba69d326232b99d823933b54
gavinp@chromium.org <gavinp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-29 23:07:49 +0000

    [Chromium] Remove assertions on state in Prerender.cpp https://bugs.webkit.org/show_bug.cgi?id=87746
    Chrome was crashing in the new Prerender API on Google Web Search; this was because the DOM would stop(), setting the
    state of the Prerenders in the document to Inactive, but an asyncronous GC would later come come along and trigger the
    removedFromDocument behaviour on each element, including the <link> element launching the prerender.  This causes an
    assertion failure.
    
    I believe the late-deletion of the DOM, and the HTMLElement::removedFromDocument calls are legal, and it was the
    overzealous assertions in Prerender that were inappropriate.
    
    Reviewed by Adam Barth.
    
    No new automated tests, working on a Chromium browser test. Manually test by entering many prerender and non-prerendered
    queries into Google Web Search.
    
    * platform/chromium/Prerender.cpp:
    (WebCore::Prerender::Prerender):
    (WebCore::Prerender::add):
    (WebCore::Prerender::cancel):
    (WebCore::Prerender::abandon):
    * platform/chromium/Prerender.h:
    (Prerender):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/Prerender.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/Prerender.cpp
@@ -48,9 +48,6 @@ Prerender::Prerender(const KURL& url, const String& referrer, ReferrerPolicy pol
     : m_url(url)
     , m_referrer(referrer)
     , m_referrerPolicy(policy)
-#ifndef NDEBUG
-    , m_state(Inactive)
-#endif
 {
 }
 
@@ -58,24 +55,13 @@ Prerender::~Prerender()
 {
 }
 
-void Prerender::setState(State state)
-{
-#ifdef NDEBUG
-    UNUSED_PARAM(state);
-#else
-    m_state = state;
-#endif
-}
-
 void Prerender::add()
 {
-    ASSERT(m_state == Inactive);
     WebKit::WebPrerenderingSupport* platform = WebKit::WebPrerenderingSupport::current();
     if (!platform)
         return;
     WebKit::WebPrerender webPrerender(this);
     platform->add(webPrerender);
-    setState(Active);
 }
 
 void Prerender::cancel()
@@ -84,10 +70,8 @@ void Prerender::cancel()
     WebKit::WebPrerenderingSupport* platform = WebKit::WebPrerenderingSupport::current();
     if (!platform)
         return;
-    ASSERT(m_state == Active);
     WebKit::WebPrerender webPrerender(this);
     platform->cancel(webPrerender);
-    setState(Inactive);
 }
 
 void Prerender::abandon()
@@ -95,12 +79,8 @@ void Prerender::abandon()
     WebKit::WebPrerenderingSupport* platform = WebKit::WebPrerenderingSupport::current();
     if (!platform)
         return;
-    // FIXME: Assert on the state as Inactive here. It is currently common to call abandon() on an Inactive
-    // prerender, as the Prerenderer doesn't keep track of which prerenders are active, and so any page that
-    // ever had a now-canceled Prerender will get this bogus stop() call.
     WebKit::WebPrerender webPrerender(this);
     platform->abandon(webPrerender);
-    setState(Inactive);
 }
 
 void Prerender::suspend()
