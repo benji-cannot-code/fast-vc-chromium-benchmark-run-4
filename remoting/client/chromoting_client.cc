d388c17709eba8fb524a4f5de1f66cf4562fabc5
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-29 23:40:33 +0000

    Remove V1 authenticators.
    All currently used versions of chromoting should support V2 auth, so
    we no longer need V1 auth support.
    
    BUG=110483
    
    
    Review URL: https://chromiumcodereview.appspot.com/10453064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139424 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/chromoting_client.cc
+++ remoting/client/chromoting_client.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/authentication_method.h"
 #include "remoting/protocol/connection_to_host.h"
 #include "remoting/protocol/negotiating_authenticator.h"
-#include "remoting/protocol/v1_authenticator.h"
 #include "remoting/protocol/session_config.h"
 #include "remoting/protocol/transport.h"
 
@@ -54,14 +53,9 @@ void ChromotingClient::Start(
   DCHECK(message_loop()->BelongsToCurrentThread());
 
   scoped_ptr<protocol::Authenticator> authenticator;
-  if (config_.use_v1_authenticator) {
-    authenticator.reset(new protocol::V1ClientAuthenticator(
-        config_.local_jid, config_.shared_secret));
-  } else {
-    authenticator = protocol::NegotiatingAuthenticator::CreateForClient(
-        config_.authentication_tag,
-        config_.shared_secret, config_.authentication_methods);
-  }
+  authenticator = protocol::NegotiatingAuthenticator::CreateForClient(
+      config_.authentication_tag,
+      config_.shared_secret, config_.authentication_methods);
 
   connection_->Connect(xmpp_proxy, config_.local_jid, config_.host_jid,
                        config_.host_public_key, transport_factory.Pass(),
