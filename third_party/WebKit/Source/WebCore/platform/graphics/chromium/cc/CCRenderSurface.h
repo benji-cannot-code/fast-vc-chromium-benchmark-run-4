eb19896afb9d42ebaf44bb7938fe121436fd64da
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-29 17:46:03 +0000

    [chromium] Unoccluded area in surfaces should always be clipped to the rootScissorRect https://bugs.webkit.org/show_bug.cgi?id=87677
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    When a contributing surface is visited, and we have not visited another
    surface, then we know the surface is completely unoccluded. However, the
    root scissor rect should still be applied, and anything outside of it
    should be considered occluded.
    
    Unit test: WebKitTests::CCOcclusionTrackerTestTopmostSurfaceIsClippedToScissor
    
    * platform/graphics/chromium/RenderSurfaceChromium.cpp:
    (WebCore::RenderSurfaceChromium::targetRenderSurface):
    (WebCore):
    * platform/graphics/chromium/RenderSurfaceChromium.h:
    (RenderSurfaceChromium):
    * platform/graphics/chromium/cc/CCOcclusionTracker.cpp:
    (WebCore::::unoccludedContributingSurfaceContentRect):
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::targetRenderSurface):
    (WebCore):
    * platform/graphics/chromium/cc/CCRenderSurface.h:
    (CCRenderSurface):
    
    Source/WebKit/chromium:
    
    * tests/CCOcclusionTrackerTest.cpp:
    (CCOcclusionTrackerTestTopmostSurfaceIsClippedToScissor):
    (WebKitTests::CCOcclusionTrackerTestTopmostSurfaceIsClippedToScissor::runMyTest):
    (WebKitTests):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
@@ -139,6 +139,7 @@ public:
     ManagedTexture* backgroundTexture() const { return m_backgroundTexture.get(); }
 
     int owningLayerId() const;
+    CCRenderSurface* targetRenderSurface() const;
 
     bool hasReplica() const;
 
