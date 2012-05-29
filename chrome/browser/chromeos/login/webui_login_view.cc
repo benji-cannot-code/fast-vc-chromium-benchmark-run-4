a5166b241ba76e60fb71cad2db689c5fc4334960
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-29 18:23:33 +0000

    browser: Remove chrome_shell_delegate.h include from webui_login_view.cc
    BUG=125846
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10443041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139325 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_login_view.cc
+++ chrome/browser/chromeos/login/webui_login_view.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/renderer_preferences_util.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
-#include "chrome/browser/ui/views/ash/chrome_shell_delegate.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
