57d0526e2bd84780403313d48172a3e88ec2116f
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-29 23:30:20 +0000

    rename chrome_shared2.css to chrome_shared.css
    BUG=103688
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10444043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139423 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/task_manager/includes.js
+++ chrome/browser/resources/task_manager/includes.js
@@ -8,11 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // reference the resources.
 
 var css = [
-  'chrome_shared2.css',
+  'chrome_shared.css',
   'list.css',
   'table.css',
   'menu.css',
-  'widgets.css',
 ];
 
 var script = [
