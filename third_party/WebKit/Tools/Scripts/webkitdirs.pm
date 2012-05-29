d0d8720bd35fabe6844fee5ecdb0386363c27aea
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-29 14:56:43 +0000

    [BlackBerry] OpenGL content is not rendered on simulator https://bugs.webkit.org/show_bug.cgi?id=87721
    Patch by Arvid Nilsson <anilsson@rim.com> on 2012-05-29
    Reviewed by George Staikos.
    
    Fixed by enabling GLES2 support in simulator builds.
    
    To experiment with building without OpenGL support, you can set the
    DISABLE_GLES2 environment variable to a non-empty string and rebuild.
    
    PR #150695
    
    * Scripts/webkitdirs.pm:
    (blackberryCMakeArguments):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118777 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -940,7 +940,6 @@ sub blackberryCMakeArguments()
     if ($cpu eq "a9") {
         $cpu = $arch . "v7le";
         push @cmakeExtraOptions, '-DTARGETING_PLAYBOOK=1';
-        push @cmakeExtraOptions, '-DENABLE_GLES2=1';
     }
 
     my $stageDir = $ENV{"STAGE_DIR"};
@@ -967,7 +966,7 @@ sub blackberryCMakeArguments()
 
     push @cmakeExtraOptions, "-DCMAKE_SKIP_RPATH='ON'" if isDarwin();
     push @cmakeExtraOptions, "-DENABLE_DRT=1" if $ENV{"ENABLE_DRT"};
-    push @cmakeExtraOptions, "-DENABLE_GLES2=1" if $ENV{"ENABLE_GLES2"};
+    push @cmakeExtraOptions, "-DENABLE_GLES2=1" unless $ENV{"DISABLE_GLES2"};
 
     my @includeSystemDirectories;
     push @includeSystemDirectories, File::Spec->catdir($stageInc, "grskia", "skia");
