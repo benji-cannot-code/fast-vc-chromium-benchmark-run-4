1d3a7ca3d70aab5e9914a8e5e9fd61b3529fa1db
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 07:39:02 +0000

    Modify Browser::IsReservedCommandOrKey() to make sure it returns false when an Ash shortcut is pressed.
    BUG=129978,123856
    TEST=ran unit_tests + manual test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10444047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139476 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_unittest.cc
+++ chrome/browser/ui/browser_unittest.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser.h"
 
+#include "chrome/app/chrome_command_ids.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
+#include "content/public/browser/native_web_keyboard_event.h"
 
 // Various assertions around setting show state.
 TEST_F(BrowserWithTestWindowTest, GetSavedWindowShowState) {
@@ -21,4 +23,18 @@ TEST_F(BrowserWithTestWindowTest, GetSavedWindowShowState) {
   EXPECT_EQ(ui::SHOW_STATE_MINIMIZED, browser()->GetSavedWindowShowState());
   browser()->set_show_state(ui::SHOW_STATE_FULLSCREEN);
   EXPECT_EQ(ui::SHOW_STATE_FULLSCREEN, browser()->GetSavedWindowShowState());
+}
+
+TEST_F(BrowserWithTestWindowTest, IsReservedCommandOrKey) {
+#if defined(OS_CHROMEOS)
+  const content::NativeWebKeyboardEvent event(ui::ET_KEY_PRESSED,
+                                              false,
+                                              ui::VKEY_F1,
+                                              0,
+                                              base::Time::Now().ToDoubleT());
+  // F1-4 keys are reserved on Chrome OS.
+  EXPECT_TRUE(browser()->IsReservedCommandOrKey(IDC_BACK, event));
+  // ..unless |command_id| is -1. crbug.com/122978
+  EXPECT_FALSE(browser()->IsReservedCommandOrKey(-1, event));
+#endif
 }
