8d761ef5fbe2d3d417f33376ad20c0c98bb3950e
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-30 18:57:28 +0000

    WebProcessMainMac.mm doesn't need to import WKView.h.
    Rubber-stamped by Anders Carlsson.
    
    * WebProcess/mac/WebProcessMainMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/mac/WebProcessMainMac.mm
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebProcess.h"
 #import "WebSystemInterface.h"
 #import <WebCore/RunLoop.h>
-#import <WebKit2/WKView.h>
 #import <WebKitSystemInterface.h>
 #import <mach/mach_error.h>
 #import <objc/objc-auto.h>
