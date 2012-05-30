23a060904b9d0010f33f74654df292ca4199e816
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 09:54:35 +0000

    Very minor typo fixes for GData/FileAPI code.
    BUG=None
    TEST=None. Just changing comments and spaces.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10441086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -516,7 +516,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/gdata/gdata_cache.cc',
         'browser/chromeos/gdata/gdata_cache.h',
         'browser/chromeos/gdata/gdata_db.h',
-        'browser/chromeos/gdata/gdata_db_factory.cc', 
+        'browser/chromeos/gdata/gdata_db_factory.cc',
         'browser/chromeos/gdata/gdata_db_factory.h',
         'browser/chromeos/gdata/gdata_documents_service.cc',
         'browser/chromeos/gdata/gdata_documents_service.h',
--- webkit/chromeos/fileapi/memory_file_util.h
+++ webkit/chromeos/fileapi/memory_file_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -146,6 +146,6 @@ class MemoryFileUtil : public FileUtilAsync {
   DISALLOW_COPY_AND_ASSIGN(MemoryFileUtil);
 };
 
-}  // fileapi
+}  // namespace fileapi
 
 #endif  // WEBKIT_CHROMEOS_FILEAPI_FILE_SYSTEM_FILE_UTIL_ASYNC_H_
