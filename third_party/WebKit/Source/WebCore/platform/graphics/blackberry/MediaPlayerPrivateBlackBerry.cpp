13790553215646823113d29a8874d5414fc4e2a9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-30 03:51:23 +0000

    [BlackBerry] Loading media data with http authentication https://bugs.webkit.org/show_bug.cgi?id=84214
    Patch by Jonathan Dong <jonathan.dong@torchmobile.com.cn> on 2012-05-29
    Reviewed by George Staikos.
    
    .:
    
    Added a manual test case which needs user to provide a HTTP server
    with HTTP authentication support when loading the specified media
    resource. The test case will test if the media resource is successfully
    loaded.
    
    * ManualTests/blackberry/video-load-with-authentication.html: Added.
    
    Source/WebCore:
    
    RIM PR: 117618
    Implemented http authentication feature for media by implementing
    two interface functions in class MediaPlayerPrivate:
    onAuthenticationNeeded(): this function is triggered when MMR
    engine requires http authentication. We search the CredentialStorage
    to see if we have already stored existing credential information,
    or challenge user to provide it.
    OnAuthenticationAccepted(): this function is triggered when MMR
    engine accepts the credential information, and we need to save
    it in CredentialStorage for later use.
    
    Internally reviewed by Max Feil <mfeil@qnx.com>.
    
    Manual test case: blackberry/video-load-with-authentication.html
    
    * platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp:
    (WebCore::generateProtectionSpaceFromMMRAuthChallenge):
    (WebCore):
    (WebCore::MediaPlayerPrivate::onAuthenticationNeeded):
    (WebCore::MediaPlayerPrivate::onAuthenticationAccepted):
    * platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.h:
    (MediaPlayerPrivate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118887 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MediaPlayerPrivateBlackBerry.h"
 
 #include "CookieManager.h"
+#include "Credential.h"
+#include "CredentialStorage.h"
 #include "Frame.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
@@ -31,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HostWindow.h"
 #include "NotImplemented.h"
 #include "PlatformContextSkia.h"
+#include "ProtectionSpace.h"
 #include "RenderBox.h"
 #include "TimeRanges.h"
 #include "WebPageClient.h"
@@ -657,6 +660,50 @@ void MediaPlayerPrivate::onBuffering(bool flag)
     setBuffering(flag);
 }
 #endif
+
+static ProtectionSpace generateProtectionSpaceFromMMRAuthChallenge(const MMRAuthChallenge& authChallenge)
+{
+    KURL url(ParsedURLString, String(authChallenge.url().c_str()));
+    ASSERT(url.isValid());
+
+    return ProtectionSpace(url.host(), url.port(),
+                           static_cast<ProtectionSpaceServerType>(authChallenge.serverType()),
+                           authChallenge.realm().c_str(),
+                           static_cast<ProtectionSpaceAuthenticationScheme>(authChallenge.authScheme()));
+}
+
+bool MediaPlayerPrivate::onAuthenticationNeeded(MMRAuthChallenge& authChallenge)
+{
+    KURL url(ParsedURLString, String(authChallenge.url().c_str()));
+    if (!url.isValid())
+        return false;
+
+    ProtectionSpace protectionSpace = generateProtectionSpaceFromMMRAuthChallenge(authChallenge);
+    Credential credential = CredentialStorage::get(protectionSpace);
+    bool isConfirmed = false;
+    if (credential.isEmpty()) {
+        if (frameView() && frameView()->hostWindow())
+            isConfirmed = frameView()->hostWindow()->platformPageClient()->authenticationChallenge(url, protectionSpace, credential);
+    } else
+        isConfirmed = true;
+
+    if (isConfirmed)
+        authChallenge.setCredential(credential.user().utf8().data(), credential.password().utf8().data(), static_cast<MMRAuthChallenge::CredentialPersistence>(credential.persistence()));
+
+    return isConfirmed;
+}
+
+void MediaPlayerPrivate::onAuthenticationAccepted(const MMRAuthChallenge& authChallenge) const
+{
+    KURL url(ParsedURLString, String(authChallenge.url().c_str()));
+    if (!url.isValid())
+        return;
+
+    ProtectionSpace protectionSpace = generateProtectionSpaceFromMMRAuthChallenge(authChallenge);
+    Credential savedCredential = CredentialStorage::get(protectionSpace);
+    if (savedCredential.isEmpty())
+        CredentialStorage::set(Credential(authChallenge.username().c_str(), authChallenge.password().c_str(), static_cast<CredentialPersistence>(authChallenge.persistence())), protectionSpace, url);
+}
 
 int MediaPlayerPrivate::showErrorDialog(MMRPlayer::Error type)
 {
