93563ce17ab74f7f36da55570905072310b74396
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-30 16:07:41 +0000

    Unreviewed WinCE build fix after r115926.
    * WebView.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118929 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/wince/WebView.cpp
+++ third_party/WebKit/Source/WebKit/wince/WebView.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameLoaderClientWinCE.h"
 #include "FrameView.h"
 #include "GraphicsContext.h"
+#include "HTMLFormElement.h"
 #include "InitializeThreading.h"
 #include "InspectorClientWinCE.h"
 #include "IntSize.h"
