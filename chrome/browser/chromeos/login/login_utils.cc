e5702be30a4a4b58091c70bcafc1380feceb00f7
vollick@google.com <vollick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 00:03:55 +0000

    Disable accelerated animation
    With this change accelerated animation will be disabled by default.
    
    BUG=129934
    TEST=manual
    TBR=sky@chromium.org,avayvod@chromium.org,ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10459016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139433 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -1054,7 +1054,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ::switches::kDisableLoginAnimations,
       ::switches::kDisableSeccompFilterSandbox,
       ::switches::kDisableSeccompSandbox,
-      ::switches::kDisableThreadedAnimation,
+      ::switches::kEnableThreadedAnimation,
       ::switches::kEnableDevicePolicy,
       ::switches::kEnableGView,
       ::switches::kEnableLogging,
