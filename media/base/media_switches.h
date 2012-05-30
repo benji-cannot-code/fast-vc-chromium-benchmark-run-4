c6a3caa2a5c104ef87afec2e9b4bba0b39c6e162
kochi@google.com <kochi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 02:20:56 +0000

    Export kUseCras
    In ChromeOS's login_utils, it uses kUseCras, and for component build,
    it has to be exported.
    
    BUG=none
    TEST=emerge chromeos-chrome with USE=component_build
    
    Review URL: https://chromiumcodereview.appspot.com/10442060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139455 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/media_switches.h
+++ media/base/media_switches.h
@@ -19,7 +19,7 @@ extern const char kAlsaInputDevice[];
 #endif
 
 #if defined(OS_LINUX)
-extern const char kUseCras[];
+MEDIA_EXPORT extern const char kUseCras[];
 #endif
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
