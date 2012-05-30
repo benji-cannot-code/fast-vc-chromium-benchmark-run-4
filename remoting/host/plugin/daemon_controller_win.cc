fb26bee76a5c4225f93ff700fe95fadb9798f7ac
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 07:58:56 +0000

    Return NOT_IMPLEMENTED on unsupported OSes.
    BUG=130149
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10441083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139488 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/daemon_controller_win.cc
+++ remoting/host/plugin/daemon_controller_win.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "base/win/scoped_bstr.h"
 #include "base/win/scoped_comptr.h"
+#include "base/win/windows_version.h"
 #include "remoting/base/scoped_sc_handle_win.h"
 #include "remoting/host/branding.h"
 #include "remoting/host/plugin/daemon_installer_win.h"
@@ -184,6 +185,9 @@ DaemonControllerWin::~DaemonControllerWin() {
 }
 
 remoting::DaemonController::State DaemonControllerWin::GetState() {
+  if (base::win::GetVersion() < base::win::VERSION_VISTA) {
+    return STATE_NOT_IMPLEMENTED;
+  }
   // TODO(alexeypa): Make the thread alertable, so we can switch to APC
   // notifications rather than polling.
   ScopedScHandle service;
