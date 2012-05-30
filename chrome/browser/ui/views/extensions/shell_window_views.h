da532d9d4bfe5d2be421a2e5657d472bed6a281f
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 07:59:51 +0000

    Allow specification of minimum size for app windows on creation.
    Currently only has an effect on windows.
    
    BUG=129962
    R=mihaip@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10444045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139489 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/shell_window_views.h
+++ chrome/browser/ui/views/extensions/shell_window_views.h
@@ -72,6 +72,8 @@ class ShellWindowViews : public ShellWindow,
   virtual void OnBoundsChanged(const gfx::Rect& previous_bounds) OVERRIDE;
 
  private:
+  friend class ShellWindowFrameView;
+
   virtual ~ShellWindowViews();
 
   void OnViewWasResized();
@@ -82,6 +84,7 @@ class ShellWindowViews : public ShellWindow,
   gfx::ScopedSkRegion caption_region_;
 
   bool use_custom_frame_;
+  gfx::Size minimum_size_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellWindowViews);
 };
