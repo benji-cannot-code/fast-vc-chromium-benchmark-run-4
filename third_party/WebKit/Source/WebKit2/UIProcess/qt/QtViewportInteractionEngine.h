e1abbb07636dfd166c84db5a779669370e3a6468
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-30 10:28:07 +0000

    [Qt] Variable renaming as preparation of further refactoring.
    Rubberstamped by Simon Hausmann.
    
    Change m_viewport to m_viewportItem and m_content to m_pageItem.
    
    * UIProcess/qt/QtViewportInteractionEngine.cpp:
    (WebKit::QtViewportInteractionEngine::QtViewportInteractionEngine):
    (WebKit::QtViewportInteractionEngine::setItemRectVisible):
    (WebKit::QtViewportInteractionEngine::animateItemRectVisible):
    (WebKit::QtViewportInteractionEngine::flickableMoveStarted):
    (WebKit::QtViewportInteractionEngine::flickableMoveEnded):
    (WebKit::QtViewportInteractionEngine::flickableMovingPositionUpdate):
    (WebKit::QtViewportInteractionEngine::scaleAnimationStateChanged):
    (WebKit::QtViewportInteractionEngine::pagePositionRequest):
    (WebKit::QtViewportInteractionEngine::computePosRangeForItemAtScale):
    (WebKit::QtViewportInteractionEngine::focusEditableArea):
    (WebKit::QtViewportInteractionEngine::zoomToAreaGestureEnded):
    (WebKit::QtViewportInteractionEngine::nearestValidBounds):
    (WebKit::QtViewportInteractionEngine::reset):
    (WebKit::QtViewportInteractionEngine::setCSSScale):
    (WebKit::QtViewportInteractionEngine::currentCSSScale):
    (WebKit::QtViewportInteractionEngine::scrollAnimationActive):
    (WebKit::QtViewportInteractionEngine::panGestureActive):
    (WebKit::QtViewportInteractionEngine::panGestureStarted):
    (WebKit::QtViewportInteractionEngine::panGestureRequestUpdate):
    (WebKit::QtViewportInteractionEngine::panGestureEnded):
    (WebKit::QtViewportInteractionEngine::panGestureCancelled):
    (WebKit::QtViewportInteractionEngine::cancelScrollAnimation):
    (WebKit::QtViewportInteractionEngine::pinchGestureStarted):
    (WebKit::QtViewportInteractionEngine::pinchGestureRequestUpdate):
    (WebKit::QtViewportInteractionEngine::scaleContent):
    * UIProcess/qt/QtViewportInteractionEngine.h:
    (QtViewportInteractionEngine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportInteractionEngine.h
@@ -117,8 +117,8 @@ private:
     friend class ViewportUpdateDeferrer;
     friend class ::QWebKitTest;
 
-    QQuickWebView* const m_viewport;
-    QQuickWebPage* const m_content;
+    QQuickWebView* const m_viewportItem;
+    QQuickWebPage* const m_pageItem;
 
     qreal cssScaleFromItem(qreal) const;
     qreal itemScaleFromCSS(qreal) const;
