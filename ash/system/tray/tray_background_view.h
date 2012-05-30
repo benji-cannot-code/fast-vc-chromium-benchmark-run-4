f34f39791775358c5b3c5104fdf0ee9840c533e7
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 23:25:49 +0000

    Add ASH_EXPORT to TrayBackgroundView
    BUG=124914
    TEST=win_aura compiles
    TBR=sadrul@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10448091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139679 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_background_view.h
+++ ash/system/tray/tray_background_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_SYSTEM_TRAY_TRAY_BACKGROUND_VIEW_H_
 #pragma once
 
+#include "ash/ash_export.h"
 #include "ash/launcher/background_animator.h"
 #include "ash/system/tray/tray_views.h"
 
@@ -20,8 +21,8 @@ class TrayBackground;
 // his shown/hidden. It also inherits from ActionableView so that the tray
 // items can override PerformAction when clicked on.
 
-class TrayBackgroundView : public internal::ActionableView,
-                           public BackgroundAnimatorDelegate {
+class ASH_EXPORT TrayBackgroundView : public internal::ActionableView,
+                                      public BackgroundAnimatorDelegate {
  public:
   TrayBackgroundView();
   virtual ~TrayBackgroundView();
