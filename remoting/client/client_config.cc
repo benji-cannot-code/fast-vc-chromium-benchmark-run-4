f3191465db7b2203ab1053f890910e0a8e07d616
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 18:16:54 +0000

    Revert 139424 - Remove V1 authenticators.
    All currently used versions of chromoting should support V2 auth, so
    we no longer need V1 auth support.
    
    BUG=110483
    
    
    Review URL: https://chromiumcodereview.appspot.com/10453064
    
    TBR=sergeyu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10453080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139578 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/client_config.cc
+++ remoting/client/client_config.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-ClientConfig::ClientConfig() {
+ClientConfig::ClientConfig()
+    : use_v1_authenticator(false) {
 }
 
 ClientConfig::~ClientConfig() {
