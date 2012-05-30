ef90e19ce1d31c5df5dfb3d29675a983e5f18acd
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-30 16:41:00 +0000

    [Coverity] Uninit ctor
    CID=104031
    
    R=jhawkins@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10392153
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/nacl_types.cc
+++ chrome/common/nacl_types.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace nacl {
 
-NaClStartParams::NaClStartParams() : enable_exception_handling(false) {
+NaClStartParams::NaClStartParams()
+    : validation_cache_enabled(false),
+      enable_exception_handling(false) {
 }
 
 NaClStartParams::~NaClStartParams() {
