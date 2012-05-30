ef6e5b6c79fbe63b6c271aac5b46355d84b9d1b0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-30 17:17:15 +0000

    [EFL][DRT] http/tests/navigation/new-window-redirect-history.html does not pass https://bugs.webkit.org/show_bug.cgi?id=87848
    Patch by Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com> on 2012-05-30
    Reviewed by Csaba Osztrogonác.
    
    Tools:
    
    The reason was that DumpRenderTreeChrome::extraViews() returned copy of the vector
    and dumpBackForwardListForWebViews() used iterators belonging actually to different vectors
    (browser->extraViews().begin() and browser->extraViews().end()) as if they had belonged to the same vector.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (DumpRenderTreeChrome::extraViews):
    * DumpRenderTree/efl/DumpRenderTreeChrome.h:
    (DumpRenderTreeChrome):
    
    LayoutTests:
    
    * platform/efl/Skipped: Unskipped http/tests/navigation/new-window-redirect-history.html.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@118933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTreeChrome.cpp
@@ -175,7 +175,7 @@ bool DumpRenderTreeChrome::initialize()
     return true;
 }
 
-Vector<Evas_Object*> DumpRenderTreeChrome::extraViews() const
+const Vector<Evas_Object*>& DumpRenderTreeChrome::extraViews() const
 {
     return m_extraViews;
 }
