8bb34001c5a322be2f35eaa5d453a3d1550502de
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 08:00:35 +0000

    [V8] Pass Isolate to v8String() in custom bindings https://bugs.webkit.org/show_bug.cgi?id=87825
    Reviewed by Adam Barth.
    
    The objective is to pass Isolate around in V8 bindings.
    This patch passes Isolate to v8String() in custom bindings.
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8ArrayBufferViewCustom.cpp:
    (WebCore::installFastSet):
    * bindings/v8/custom/V8ArrayBufferViewCustom.h:
    (WebCore):
    (WebCore::setWebGLArrayHelper):
    * bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp:
    (WebCore::V8CSSStyleDeclaration::namedPropertyEnumerator):
    (WebCore::V8CSSStyleDeclaration::namedPropertyGetter):
    * bindings/v8/custom/V8CanvasRenderingContext2DCustom.cpp:
    (WebCore::toV8Object):
    * bindings/v8/custom/V8ClipboardCustom.cpp:
    (WebCore::V8Clipboard::typesAccessorGetter):
    * bindings/v8/custom/V8DOMStringMapCustom.cpp:
    (WebCore::V8DOMStringMap::namedPropertyGetter):
    (WebCore::V8DOMStringMap::namedPropertyEnumerator):
    * bindings/v8/custom/V8FileReaderCustom.cpp:
    (WebCore::V8FileReader::resultAccessorGetter):
    * bindings/v8/custom/V8HTMLCanvasElementCustom.cpp:
    (WebCore::V8HTMLCanvasElement::toDataURLCallback):
    * bindings/v8/custom/V8HTMLElementCustom.cpp:
    (WebCore::toV8Object):
    * bindings/v8/custom/V8HTMLInputElementCustom.cpp:
    (WebCore::V8HTMLInputElement::selectionDirectionAccessorGetter):
    * bindings/v8/custom/V8IDBAnyCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8IDBKeyCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8InjectedScriptHostCustom.cpp:
    (WebCore::V8InjectedScriptHost::databaseIdCallback):
    (WebCore::V8InjectedScriptHost::storageIdCallback):
    * bindings/v8/custom/V8InspectorFrontendHostCustom.cpp:
    (WebCore::V8InspectorFrontendHost::platformCallback):
    * bindings/v8/custom/V8JavaScriptCallFrameCustom.cpp:
    (WebCore::V8JavaScriptCallFrame::typeAccessorGetter):
    * bindings/v8/custom/V8LocationCustom.cpp:
    (WebCore::V8Location::toStringCallback):
    * bindings/v8/custom/V8SQLResultSetRowListCustom.cpp:
    (WebCore::V8SQLResultSetRowList::itemCallback):
    * bindings/v8/custom/V8StorageCustom.cpp:
    (WebCore::V8Storage::namedPropertyEnumerator):
    (WebCore::storageGetter):
    * bindings/v8/custom/V8XMLHttpRequestCustom.cpp:
    (WebCore::V8XMLHttpRequest::responseTextAccessorGetter):
    * bindings/v8/custom/V8XSLTProcessorCustom.cpp:
    (WebCore::V8XSLTProcessor::getParameterCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8InspectorFrontendHostCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8InspectorFrontendHostCustom.cpp
@@ -45,22 +45,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8InspectorFrontendHost::platformCallback(const v8::Arguments&)
+v8::Handle<v8::Value> V8InspectorFrontendHost::platformCallback(const v8::Arguments& args)
 {
 #if defined(OS_MACOSX)
-    return v8String("mac");
+    return v8String("mac", args.GetIsolate());
 #elif defined(OS_LINUX)
-    return v8String("linux");
+    return v8String("linux", args.GetIsolate());
 #elif defined(OS_FREEBSD)
-    return v8String("freebsd");
+    return v8String("freebsd", args.GetIsolate());
 #elif defined(OS_OPENBSD)
-    return v8String("openbsd");
+    return v8String("openbsd", args.GetIsolate());
 #elif defined(OS_SOLARIS)
-    return v8String("solaris");
+    return v8String("solaris", args.GetIsolate());
 #elif defined(OS_WIN)
-    return v8String("windows");
+    return v8String("windows", args.GetIsolate());
 #else
-    return v8String("unknown");
+    return v8String("unknown", args.GetIsolate());
 #endif
 }
 
