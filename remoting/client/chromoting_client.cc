e504488f2e96d611833094cdc0f8050be91f041b
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 17:35:12 +0000

    Disable V1 auth support in remoting client and Me2Me host.
    BUG=110483
    
    Review URL: https://chromiumcodereview.appspot.com/10446084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139815 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/chromoting_client.cc
+++ remoting/client/chromoting_client.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/protocol/authentication_method.h"
 #include "remoting/protocol/connection_to_host.h"
 #include "remoting/protocol/negotiating_authenticator.h"
-#include "remoting/protocol/v1_authenticator.h"
 #include "remoting/protocol/session_config.h"
 #include "remoting/protocol/transport.h"
 
@@ -53,15 +52,10 @@ void ChromotingClient::Start(
     scoped_ptr<protocol::TransportFactory> transport_factory) {
   DCHECK(message_loop()->BelongsToCurrentThread());
 
-  scoped_ptr<protocol::Authenticator> authenticator;
-  if (config_.use_v1_authenticator) {
-    authenticator.reset(new protocol::V1ClientAuthenticator(
-        config_.local_jid, config_.shared_secret));
-  } else {
-    authenticator = protocol::NegotiatingAuthenticator::CreateForClient(
-        config_.authentication_tag,
-        config_.shared_secret, config_.authentication_methods);
-  }
+  scoped_ptr<protocol::Authenticator> authenticator(
+      protocol::NegotiatingAuthenticator::CreateForClient(
+          config_.authentication_tag,
+          config_.shared_secret, config_.authentication_methods));
 
   connection_->Connect(xmpp_proxy, config_.local_jid, config_.host_jid,
                        config_.host_public_key, transport_factory.Pass(),
