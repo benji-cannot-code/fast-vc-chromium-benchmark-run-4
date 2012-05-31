cbd1b8e379bdfb38ac3c612946c655afc2362f36
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 16:00:20 +0000

    The difference between a column and a column group renderer is badly drawn https://bugs.webkit.org/show_bug.cgi?id=87556
    Reviewed by Abhishek Arya.
    
    No expected change in behavior.
    
    This patch tries to draw a better line between those 3 concepts:
    - renderer is a RenderTableCol (RenderObject::isRenderTableCol).
    - renderer is a column (RenderTableCol::isTableColumn()).
    - renderer is a column group (RenderTableCol::isTableColumnGroup()).
    (most of the code that needs to know the difference manipulate a RenderTableCol
    so there was no need to move the 2 last helpers into RenderObject)
    
    * rendering/AutoTableLayout.cpp:
    (WebCore::AutoTableLayout::recalcColumn):
    Factored the code to use the column / column group iteration we
    set up in the rest of the code. Made the code more self-describing
    by using more RenderTableCol functions.
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::addChild):
    Factored the code to use the isTableColumn() and isTableColumnGroup() to
    better underline what we check.
    
    * rendering/RenderObject.h:
    (WebCore::RenderObject::isRenderTableCol):
    (WebCore::RenderObject::isTablePart):
    Renamed isTableCol to isRenderTableCol to match the class as this doesn't
    imply that the renderer is actually a column.
    
    * html/HTMLTableColElement.cpp:
    (WebCore::HTMLTableColElement::parseAttribute):
    * rendering/AutoTableLayout.cpp:
    (WebCore::AutoTableLayout::fullRecalc):
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::addChild):
    (WebCore::RenderTable::layout):
    (WebCore::RenderTable::firstColumn):
    Updated after the isRenderTableCol rename.
    
    * dom/Text.cpp:
    (WebCore::Text::rendererIsNeeded):
    * editing/htmlediting.cpp:
    (WebCore::isTableStructureNode):
    Ditto and renamed some variables.
    
    * html/HTMLFormElement.cpp:
    (WebCore::HTMLFormElement::rendererIsNeeded):
    Ditto and added a FIXME as the logic doesn't seem very bullet proof.
    
    * rendering/RenderTableCell.cpp:
    (WebCore::RenderTableCell::computeCollapsedStartBorder):
    (WebCore::RenderTableCell::computeCollapsedEndBorder):
    (WebCore::RenderTableCell::computeCollapsedBeforeBorder):
    (WebCore::RenderTableCell::computeCollapsedAfterBorder):
    Changed to use the enclosingColumnGroup* helpers.
    
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::paintCell):
    * rendering/RenderTableCol.cpp:
    (WebCore::RenderTableCol::isChildAllowed):
    (WebCore::RenderTableCol::canHaveChildren):
    (WebCore::RenderTableCol::enclosingColumnGroup):
    (WebCore::RenderTableCol::nextColumn):
    Updated to use the new functions.
    
    * rendering/RenderTableCol.h:
    (WebCore::RenderTableCol::isTableColumn):
    (WebCore::RenderTableCol::isTableColumnGroup):
    (WebCore::RenderTableCol::enclosingColumnGroupIfAdjacentBefore):
    (WebCore::RenderTableCol::enclosingColumnGroupIfAdjacentAfter):
    Added the following new helpers.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
@@ -120,7 +120,7 @@ void RenderTable::addChild(RenderObject* child, RenderObject* beforeChild)
     if (child->isTableCaption()) {
         m_captions.append(toRenderTableCaption(child));
         wrapInAnonymousSection = false;
-    } else if (child->isTableCol()) {
+    } else if (child->isRenderTableCol()) {
         m_hasColElements = true;
         wrapInAnonymousSection = false;
     } else if (child->isTableSection()) {
@@ -361,7 +361,7 @@ void RenderTable::layout()
             if (collapsing)
                 section->recalcOuterBorder();
             ASSERT(!section->needsLayout());
-        } else if (child->isTableCol()) {
+        } else if (child->isRenderTableCol()) {
             child->layoutIfNeeded();
             ASSERT(!child->needsLayout());
         }
@@ -730,7 +730,7 @@ void RenderTable::appendColumn(unsigned span)
 RenderTableCol* RenderTable::firstColumn() const
 {
     for (RenderObject* child = firstChild(); child; child = child->nextSibling()) {
-        if (child->isTableCol())
+        if (child->isRenderTableCol())
             return toRenderTableCol(child);
 
         // We allow only table-captions before columns or column-groups.
