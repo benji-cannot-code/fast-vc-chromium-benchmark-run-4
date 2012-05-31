7a38a9d5b8c8aa46703a333b0a29294a529e680f
slewis@apple.com <slewis@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 01:51:26 +0000

    https://bugs.webkit.org/show_bug.cgi?id=87714 Mac crash logs can take a really long time to be written out.
    Reviewed by Dirk Pranke.
    
    Make a second pass looking for crash logs after the tests have completed running.
    
    * Scripts/webkitpy/layout_tests/controllers/manager.py:
    (use_trac_links_in_results_html):
    (Manager.run):
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.repository_paths):
    (Port.look_for_new_crash_logs):
    * Scripts/webkitpy/layout_tests/port/mac.py:
    (MacPort.look_for_new_crash_logs):
    * Scripts/webkitpy/layout_tests/port/mac_unittest.py:
    (test_get_crash_log):
    (test_look_for_new_crash_logs):
    (test_look_for_new_crash_logs.fake_time_cb):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119017 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/manager_unittest.py
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import StringIO
 import sys
+import time
 import unittest
 
 from webkitpy.common.system.filesystem_mock import MockFileSystem
@@ -326,6 +327,21 @@ class ManagerTest(unittest.TestCase):
 
             manager = get_manager_with_tests(['http\\tests\\mime'])
             self.assertTrue(manager.needs_servers())
+
+    def test_look_for_new_crash_logs(self):
+        def get_manager_with_tests(test_names):
+            host = MockHost()
+            port = host.port_factory.get('test-mac-leopard')
+            manager = Manager(port, options=MockOptions(test_list=None, http=True), printer=Mock())
+            manager.collect_tests(test_names)
+            return manager
+        host = MockHost()
+        port = host.port_factory.get('test-mac-leopard')
+        tests = ['failures/expected/crash.html']
+        expectations = test_expectations.TestExpectations(port, tests)
+        rs = result_summary.ResultSummary(expectations, tests)
+        manager = get_manager_with_tests(tests)
+        manager._look_for_new_crash_logs(rs, time.time())
 
 
 class NaturalCompareTest(unittest.TestCase):
