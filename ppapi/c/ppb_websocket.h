026bc03744a62a6f3711199e8660479efd7388ed
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 09:25:45 +0000

    WebSocket Pepper API: allow to close connection without code and reason
     - define PP_WEBSOCKETSTATUSCODE_NOT_SPECIFIED to close without status code
     - omit reason for WebKit API explicitly when code is not specified
    
    BUG=none
    TEST=browser_tests --gtest_filter'PPAPI*WebSocket_*Close'
    
    
    Review URL: https://chromiumcodereview.appspot.com/10332138
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139764 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_websocket.h
+++ ppapi/c/ppb_websocket.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_websocket.idl modified Wed Apr 25 11:48:30 2012. */
+/* From ppb_websocket.idl modified Thu May 31 15:47:38 2012. */
 
 #ifndef PPAPI_C_PPB_WEBSOCKET_H_
 #define PPAPI_C_PPB_WEBSOCKET_H_
@@ -72,6 +72,14 @@ PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_WebSocketReadyState, 4);
  * <code>PP_WEBSOCKETSTATUSCODE_USER_PRIVATE_MAX</code> are valid for Close().
  */
 typedef enum {
+  /**
+   * Indicates to request closing connection without status code and reason.
+   *
+   * (Note that the code 1005 is forbidden to send in actual close frames by
+   * the RFC. PP_WebSocket reuses this code internally and the code will never
+   * appear in the actual close frames.)
+   */
+  PP_WEBSOCKETSTATUSCODE_NOT_SPECIFIED = 1005,
   /**
    * Status codes in the range 0-999 are not used.
    */
@@ -247,7 +255,8 @@ struct PPB_WebSocket_1_0 {
    * @param[in] web_socket A <code>PP_Resource</code> corresponding to a
    * WebSocket.
    *
-   * @param[in] code The WebSocket close code. This is ignored if it is 0.
+   * @param[in] code The WebSocket close code. This is ignored if it is
+   * <code>PP_WEBSOCKETSTATUSCODE_NOT_SPECIFIED</code>.
    * <code>PP_WEBSOCKETSTATUSCODE_NORMAL_CLOSURE</code> must be used for the
    * usual case. To indicate some specific error cases, codes in the range
    * <code>PP_WEBSOCKETSTATUSCODE_USER_REGISTERED_MIN</code> to
