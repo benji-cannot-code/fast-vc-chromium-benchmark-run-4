b8db6d2e89039b43f16a70403a8a26b19cbf665b
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 18:57:29 +0000

    Re-enable the touch UI about:flags thing now that touch resources are always included on Windows.
    BUG=128260
    
    
    Review URL: https://chromiumcodereview.appspot.com/10468002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -640,7 +640,7 @@ const Experiment kExperiments[] = {
     "touch-optimized-ui",
     IDS_FLAGS_TOUCH_OPTIMIZED_UI_NAME,
     IDS_FLAGS_TOUCH_OPTIMIZED_UI_DESCRIPTION,
-    kOsCrOS,
+    kOsWin | kOsCrOS,
     MULTI_VALUE_TYPE(kTouchOptimizedUIChoices)
   },
   {
