cb356fd8c46eaef6e618796b60e5eeb51fd5646d
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 22:06:38 +0000

    Create a focus manager per desktop RootWindow.
    BUG=none
    TEST=linux_aura bot cycles green
    R=oshima
    TBR=beng
    
    Review URL: https://chromiumcodereview.appspot.com/10447127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139887 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_native_widget_helper_aura.cc
+++ ui/views/widget/desktop_native_widget_helper_aura.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/screen_position_client.h"
 #include "ui/aura/desktop/desktop_activation_client.h"
 #include "ui/aura/desktop/desktop_dispatcher_client.h"
+#include "ui/aura/focus_manager.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/shared/input_method_event_filter.h"
 #include "ui/aura/shared/root_window_event_filter.h"
@@ -111,6 +112,7 @@ void DesktopNativeWidgetHelperAura::PreInitialize(
   }
   root_window_.reset(new aura::RootWindow(bounds));
   root_window_->Init();
+  root_window_->set_focus_manager(new aura::FocusManager);
 
   root_window_event_filter_ =
       new aura::shared::RootWindowEventFilter(root_window_.get());
