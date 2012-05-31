7bf6e0a58ba82d058ff0a9ecd640c70edbc936ad
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 00:28:00 +0000

    [V8] Implement V8Binding::v8BooleanWithCheck(isolate) https://bugs.webkit.org/show_bug.cgi?id=87814
    Reviewed by Adam Barth.
    
    The objective is to pass Isolate to v8::True() and v8::False().
    Similarly to v8NullWithCheck() (bug 87713), this patch implements
    V8Binding::v8BooleanWithCheck(isolate), which calls v8::True()/v8::False()
    or v8::True(isolate)/v8::False(isolate) depending on whether the
    passed isolate is null or not.
    
    This patch also passes Isolate to v8Boolean() in CodeGeneratorV8.pm.
    
    No tests. No behavior change.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (NativeToJSValue):
    * bindings/scripts/test/V8/V8TestEventTarget.cpp:
    (WebCore::TestEventTargetV8Internal::dispatchEventCallback):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::createAttrGetter):
    (WebCore::TestObjV8Internal::reflectedBooleanAttrAttrGetter):
    (WebCore::TestObjV8Internal::reflectedCustomBooleanAttrAttrGetter):
    * bindings/v8/V8Binding.h:
    (WebCore::v8Boolean):
    (WebCore):
    (WebCore::v8BooleanWithCheck):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119003 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
@@ -465,7 +465,7 @@ static v8::Handle<v8::Value> createAttrGetter(v8::Local<v8::String> name, const 
 {
     INC_STATS("DOM.TestObj.create._get");
     TestObj* imp = V8TestObj::toNative(info.Holder());
-    return v8Boolean(imp->isCreate());
+    return v8Boolean(imp->isCreate(), info.GetIsolate());
 }
 
 static void createAttrSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
@@ -529,7 +529,7 @@ static v8::Handle<v8::Value> reflectedBooleanAttrAttrGetter(v8::Local<v8::String
 {
     INC_STATS("DOM.TestObj.reflectedBooleanAttr._get");
     TestObj* imp = V8TestObj::toNative(info.Holder());
-    return v8Boolean(imp->hasAttribute(WebCore::HTMLNames::reflectedbooleanattrAttr));
+    return v8Boolean(imp->hasAttribute(WebCore::HTMLNames::reflectedbooleanattrAttr), info.GetIsolate());
 }
 
 static void reflectedBooleanAttrAttrSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
@@ -593,7 +593,7 @@ static v8::Handle<v8::Value> reflectedCustomBooleanAttrAttrGetter(v8::Local<v8::
 {
     INC_STATS("DOM.TestObj.reflectedCustomBooleanAttr._get");
     TestObj* imp = V8TestObj::toNative(info.Holder());
-    return v8Boolean(imp->hasAttribute(WebCore::HTMLNames::customContentBooleanAttrAttr));
+    return v8Boolean(imp->hasAttribute(WebCore::HTMLNames::customContentBooleanAttrAttr), info.GetIsolate());
 }
 
 static void reflectedCustomBooleanAttrAttrSetter(v8::Local<v8::String> name, v8::Local<v8::Value> value, const v8::AccessorInfo& info)
