8a98b59607c867dae3ae3a343f819f9e8bc2bef4
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 22:43:25 +0000

    [chromium] Assertion failures during compositor startup in lost context situations https://bugs.webkit.org/show_bug.cgi?id=87912
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    getShaderiv and getProgramiv may return 0 if the context is lost. We correctly recover in this case, so it's
    bogus to fail an ASSERT in debug in this case.
    
    Added new unit test in LayerRendererChromiumTest to cover this.
    
    * platform/graphics/chromium/ProgramBinding.cpp:
    (WebCore::contextLost):
    (WebCore):
    (WebCore::ProgramBindingBase::init):
    (WebCore::ProgramBindingBase::createShaderProgram):
    
    Source/WebKit/chromium:
    
    Adds a unit test verifying that even if we lose our context during or before initialization we get through the
    rest of the path without failing ASSERT()s.
    
    * tests/LayerRendererChromiumTest.cpp:
    (LoseContextOnFirstGetContext):
    (LoseContextOnFirstGetContext::LoseContextOnFirstGetContext):
    (TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119160 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ProgramBinding.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ProgramBinding.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ProgramBinding.h"
 
+#include "Extensions3D.h"
 #include "GeometryBinding.h"
 #include "GraphicsContext.h"
 #include "GraphicsContext3D.h"
@@ -51,10 +52,16 @@ ProgramBindingBase::~ProgramBindingBase()
     ASSERT(!m_initialized);
 }
 
+static bool contextLost(GraphicsContext3D* context)
+{
+    return (context->getExtensions()->getGraphicsResetStatusARB() != GraphicsContext3D::NO_ERROR);
+}
+
+
 void ProgramBindingBase::init(GraphicsContext3D* context, const String& vertexShader, const String& fragmentShader)
 {
     m_program = createShaderProgram(context, vertexShader, fragmentShader);
-    ASSERT(m_program);
+    ASSERT(m_program || contextLost(context));
 }
 
 void ProgramBindingBase::cleanup(GraphicsContext3D* context)
@@ -92,20 +99,23 @@ unsigned ProgramBindingBase::createShaderProgram(GraphicsContext3D* context, con
     TRACE_EVENT("ProgramBindingBase::createShaderProgram", this, 0);
     unsigned vertexShader = loadShader(context, GraphicsContext3D::VERTEX_SHADER, vertexShaderSource);
     if (!vertexShader) {
-        LOG_ERROR("Failed to create vertex shader");
+        if (!contextLost(context))
+            LOG_ERROR("Failed to create vertex shader");
         return 0;
     }
 
     unsigned fragmentShader = loadShader(context, GraphicsContext3D::FRAGMENT_SHADER, fragmentShaderSource);
     if (!fragmentShader) {
         GLC(context, context->deleteShader(vertexShader));
-        LOG_ERROR("Failed to create fragment shader");
+        if (!contextLost(context))
+            LOG_ERROR("Failed to create fragment shader");
         return 0;
     }
 
     unsigned programObject = context->createProgram();
     if (!programObject) {
-        LOG_ERROR("Failed to create shader program");
+        if (!contextLost(context))
+            LOG_ERROR("Failed to create shader program");
         return 0;
     }
 
@@ -121,7 +131,8 @@ unsigned ProgramBindingBase::createShaderProgram(GraphicsContext3D* context, con
     int linked = 0;
     GLC(context, context->getProgramiv(programObject, GraphicsContext3D::LINK_STATUS, &linked));
     if (!linked) {
-        LOG_ERROR("Failed to link shader program");
+        if (!contextLost(context))
+            LOG_ERROR("Failed to link shader program");
         GLC(context, context->deleteProgram(programObject));
         return 0;
     }
