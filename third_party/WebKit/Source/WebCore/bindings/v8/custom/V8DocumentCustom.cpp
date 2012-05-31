6ff7991f5b7cdb0b6a67361926ce16b8b267fd4b
benm@google.com <benm@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 22:34:01 +0000

    Fix crash in V8Document::createTouchListCallback. https://bugs.webkit.org/show_bug.cgi?id=87085
    Source/WebCore:
    
    Reviewed by Abhishek Arya.
    
    Test: fast/events/touch/document-create-touch-list-crash.html
    
    * bindings/v8/custom/V8DocumentCustom.cpp:
    (WebCore::V8Document::createTouchListCallback): Verify the native type
    of the arguments passed to createTouchList are Touches before appending
    them to the TouchList. In the case of a non-Touch argument, insert
    null into the TouchList. This consolidates V8 and JSC bindings
    behaviour.
    * dom/Document.cpp: Remove dead code.
    * dom/Document.h: ditto.
    
    LayoutTests:
    
    Test for crash in document.createTouchList when it
    is supplied with non-touch parameters.
    
    Reviewed by Abhishek Arya.
    
    * fast/events/touch/document-create-touch-list-crash-expected.txt: Added.
    * fast/events/touch/document-create-touch-list-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119158 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8DocumentCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8DocumentCustom.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8CanvasRenderingContext2D.h"
 #include "V8CustomXPathNSResolver.h"
 #include "V8DOMImplementation.h"
+#include "V8DOMWrapper.h"
 #include "V8HTMLDocument.h"
 #include "V8IsolatedContext.h"
 #include "V8Node.h"
@@ -143,9 +144,8 @@ v8::Handle<v8::Value> V8Document::createTouchListCallback(const v8::Arguments& a
     RefPtr<TouchList> touchList = TouchList::create();
 
     for (int i = 0; i < args.Length(); i++) {
-        if (!args[i]->IsObject())
-            return v8::Undefined();
-        touchList->append(V8Touch::toNative(args[i]->ToObject()));
+        Touch* touch = V8DOMWrapper::isWrapperOfType(args[i], &V8Touch::info) ? V8Touch::toNative(args[i]->ToObject()) : 0;
+        touchList->append(touch);
     }
 
     return toV8(touchList.release(), args.GetIsolate());
