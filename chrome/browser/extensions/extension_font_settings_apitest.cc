c8fdfac612cde532bac52bf39dfee733505901fc
falken@chromium.org <falken@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-05-31 06:49:41 +0000

    [Font Settings API]: CL for readability review.
    BUG=
    TEST=
    TBR=mpcomplete
    
    
    Review URL: https://chromiumcodereview.appspot.com/10447013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139746 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_font_settings_apitest.cc
+++ chrome/browser/extensions/extension_font_settings_apitest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// Font Settings Extension API browser tests.
+
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -10,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 
+// Test of extension API on a standard profile.
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FontSettings) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
@@ -25,6 +28,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FontSettings) {
   EXPECT_TRUE(RunExtensionTest("font_settings/standard")) << message_;
 }
 
+// Test of extension API in incognito split mode.
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, FontSettingsIncognito) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableExperimentalExtensionApis);
