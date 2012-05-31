18eab75b48c9744f9eb93a4da36b455d1ddbccdc
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-05-31 22:30:03 +0000

    DataLog should be usable outside of JSC https://bugs.webkit.org/show_bug.cgi?id=88015
    Reviewed by Oliver Hunt.
    
    * wtf/DataLog.h:
    (WTF):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119157 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/DataLog.h
+++ third_party/WebKit/Source/WTF/wtf/DataLog.h
@@ -34,10 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-FILE* dataFile();
+WTF_EXPORT_PRIVATE FILE* dataFile();
 
-void dataLogV(const char* format, va_list) WTF_ATTRIBUTE_PRINTF(1, 0);
-void dataLog(const char* format, ...) WTF_ATTRIBUTE_PRINTF(1, 2);
+WTF_EXPORT_PRIVATE void dataLogV(const char* format, va_list) WTF_ATTRIBUTE_PRINTF(1, 0);
+WTF_EXPORT_PRIVATE void dataLog(const char* format, ...) WTF_ATTRIBUTE_PRINTF(1, 2);
 
 } // namespace WTF
 
