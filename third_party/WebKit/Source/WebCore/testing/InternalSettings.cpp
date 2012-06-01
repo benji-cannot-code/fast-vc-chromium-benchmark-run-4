3c3fc74e92cc3707d70a9d95930ff0e2429c1b53
mihaip@chromium.org <mihaip@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-01 20:23:53 +0000

    Improve synchronous XHR disabling https://bugs.webkit.org/show_bug.cgi?id=88032
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    * testing/InternalSettings.cpp:
    (WebCore::InternalSettings::setSyncXHRInDocumentsEnabled): Fix a
    copy-and-paste error
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::open): Improves the disabled message, as
    suggested in a comment after r118599 was put in the commit queue.
    
    LayoutTests:
    
    Update with new error message.
    
    * fast/xmlhttprequest/xmlhttprequest-sync-disabled-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119276 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/InternalSettings.cpp
+++ third_party/WebKit/Source/WebCore/testing/InternalSettings.cpp
@@ -372,10 +372,10 @@ void InternalSettings::setFixedPositionCreatesStackingContext(bool creates, Exce
     settings()->setFixedPositionCreatesStackingContext(creates);
 }
 
-void InternalSettings::setSyncXHRInDocumentsEnabled(bool creates, ExceptionCode& ec)
+void InternalSettings::setSyncXHRInDocumentsEnabled(bool enabled, ExceptionCode& ec)
 {
-    InternalSettingsGuardForFrameView();
-    settings()->setSyncXHRInDocumentsEnabled(creates);
+    InternalSettingsGuardForSettings();
+    settings()->setSyncXHRInDocumentsEnabled(enabled);
 }
 
 void InternalSettings::setJavaScriptProfilingEnabled(bool enabled, ExceptionCode& ec)
