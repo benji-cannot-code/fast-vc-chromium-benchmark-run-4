0f957abe04af85779c4892e0fe88d239ad9459e4
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-01 12:30:09 +0000

    Frame::willDetachPage() shouldn't be called more than once. https://bugs.webkit.org/show_bug.cgi?id=88056
    Reviewed by Ryosuke Niwa.
    
    This change moved willDetachPage() to private and calls it from
    detachFromParent().  Also, it checks m_page and call
    willDetachPage() only once, if m_page is available.
    
    No new tests. Covered by existing suites.
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::closeAndRemoveChild):
    (WebCore::FrameLoader::detachFromParent):
    * page/Frame.cpp:
    (WebCore::Frame::detachFromPage):
    (WebCore):
    (WebCore::Frame::willDetachPage):
    * page/Frame.h:
    (Frame):
    * page/Page.cpp:
    (WebCore::Page::~Page):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119219 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Frame.h
+++ third_party/WebKit/Source/WebCore/page/Frame.h
@@ -92,7 +92,6 @@ namespace WebCore {
         void addDestructionObserver(FrameDestructionObserver*);
         void removeDestructionObserver(FrameDestructionObserver*);
 
-        void willDetachPage();
         void detachFromPage();
         void disconnectOwnerElement();
 
@@ -208,6 +207,7 @@ namespace WebCore {
         Frame(Page*, HTMLFrameOwnerElement*, FrameLoaderClient*);
 
         void injectUserScriptsForWorld(DOMWrapperWorld*, const UserScriptVector&, UserScriptInjectionTime);
+        void willDetachPage();
 
         HashSet<FrameDestructionObserver*> m_destructionObservers;
 
@@ -350,11 +350,6 @@ namespace WebCore {
     inline Page* Frame::page() const
     {
         return m_page;
-    }
-
-    inline void Frame::detachFromPage()
-    {
-        m_page = 0;
     }
 
     inline EventHandler* Frame::eventHandler() const
