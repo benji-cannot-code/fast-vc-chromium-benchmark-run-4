a8565d1f57ba1c574f41051ffe18aeb30506766c
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 20:20:34 +0000

    Disable gpu.GpuTest.testNoGpuProcessOnStart pyauto test.
    BUG=130647
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/10448112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/gpu.py
+++ chrome/test/functional/gpu.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -55,11 +55,6 @@ class GpuTest(pyauto.PyUITest):
     self.NavigateToURL(url)
     self.assertTrue(self.WaitUntil(
       lambda: self._GetGpuPID() is not None), msg='No process for GPU')
-
-  def testNoGpuProcessOnStart(self):
-    """Verify that no gpu process is spawned when first starting browser."""
-    self.AppendTab(pyauto.GURL('about:blank'))
-    self.assertFalse(self._GetGpuPID())
 
   def test2dCanvas(self):
     """Verify that gpu process is spawned when viewing a 2D canvas."""
