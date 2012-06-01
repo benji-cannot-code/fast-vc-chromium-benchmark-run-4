18e8597f179f6d723284077f0745948f0be64286
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 12:32:15 +0000

    Fix gcc 4.6 compile issues; set but unused variable and pointer/FALSE fubar.
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10444119
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140019 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/external_metrics_unittest.cc
+++ chrome/browser/chromeos/external_metrics_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,10 +25,11 @@ class ExternalMetricsTest : public testing::Test {
 static void SendMessage(const char* path, const char* name, const char* value) {
   int fd = open(path, O_CREAT | O_APPEND | O_WRONLY, 0666);
   int32 l = strlen(name) + strlen(value) + 2 + sizeof(l);
-  int num_bytes;
-  num_bytes = write(fd, &l, sizeof(l));
-  num_bytes = write(fd, name, strlen(name) + 1);
-  num_bytes = write(fd, value, strlen(value) + 1);
+  size_t num_bytes = 0;
+  num_bytes += write(fd, &l, sizeof(l));
+  num_bytes += write(fd, name, strlen(name) + 1);
+  num_bytes += write(fd, value, strlen(value) + 1);
+  EXPECT_EQ(num_bytes, sizeof(l) + strlen(name) + strlen(value) + 2);
   close(fd);
 }
 
