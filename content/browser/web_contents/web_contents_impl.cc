d30cb3968d4b6e5aa59a7c33162e2d446e6c758f
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 02:55:16 +0000

    FilterURL is a static method, so we don't need to call it as an instance method.
    R=jam@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10440124
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139960 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1764,16 +1764,14 @@ void WebContentsImpl::DidStartProvisionalLoadForFrame(
   bool is_error_page = (url.spec() == content::kUnreachableWebDataURL);
   GURL validated_url(url);
   GURL validated_opener_url(opener_url);
-  RenderViewHostImpl* render_view_host_impl =
-      static_cast<RenderViewHostImpl*>(render_view_host);
   content::RenderProcessHost* render_process_host =
       render_view_host->GetProcess();
-  render_view_host_impl->FilterURL(
+  RenderViewHostImpl::FilterURL(
       ChildProcessSecurityPolicyImpl::GetInstance(),
       render_process_host->GetID(),
       false,
       &validated_url);
-  render_view_host_impl->FilterURL(
+  RenderViewHostImpl::FilterURL(
       ChildProcessSecurityPolicyImpl::GetInstance(),
       render_process_host->GetID(),
       true,
@@ -1805,21 +1803,19 @@ void WebContentsImpl::DidRedirectProvisionalLoad(
   GURL validated_source_url(source_url);
   GURL validated_target_url(target_url);
   GURL validated_opener_url(opener_url);
-  RenderViewHostImpl* render_view_host_impl =
-      static_cast<RenderViewHostImpl*>(render_view_host);
   content::RenderProcessHost* render_process_host =
       render_view_host->GetProcess();
-  render_view_host_impl->FilterURL(
+  RenderViewHostImpl::FilterURL(
       ChildProcessSecurityPolicyImpl::GetInstance(),
       render_process_host->GetID(),
       false,
       &validated_source_url);
-  render_view_host_impl->FilterURL(
+  RenderViewHostImpl::FilterURL(
       ChildProcessSecurityPolicyImpl::GetInstance(),
       render_process_host->GetID(),
       false,
       &validated_target_url);
-  render_view_host_impl->FilterURL(
+  RenderViewHostImpl::FilterURL(
       ChildProcessSecurityPolicyImpl::GetInstance(),
       render_process_host->GetID(),
       true,
@@ -1852,11 +1848,9 @@ void WebContentsImpl::DidFailProvisionalLoadWithError(
             params.showing_repost_interstitial
           << ", frame_id: " << params.frame_id;
   GURL validated_url(params.url);
-  RenderViewHostImpl* render_view_host_impl =
-      static_cast<RenderViewHostImpl*>(render_view_host);
   content::RenderProcessHost* render_process_host =
       render_view_host->GetProcess();
-  render_view_host_impl->FilterURL(
+  RenderViewHostImpl::FilterURL(
       ChildProcessSecurityPolicyImpl::GetInstance(),
       render_process_host->GetID(),
       false,
