78caec95580e65a50174757245b1e1d30d519651
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 05:47:45 +0000

    Move IDR_THROBBER from ui_resources to ui_resources_standard
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10456076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@139976 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/themes/theme_service.cc
+++ chrome/browser/themes/theme_service.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/theme_resources.h"
 #include "grit/theme_resources_standard.h"
 #include "grit/ui_resources.h"
+#include "grit/ui_resources_standard.h"
 #include "ui/base/layout.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image_skia.h"
--- chrome/browser/ui/panels/panel_window_controller_cocoa.mm
+++ chrome/browser/ui/panels/panel_window_controller_cocoa.mm
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/encoding_menu_controller.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
-#include "grit/ui_resources.h"
+#include "grit/ui_resources_standard.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/mac/nsimage_cache.h"
