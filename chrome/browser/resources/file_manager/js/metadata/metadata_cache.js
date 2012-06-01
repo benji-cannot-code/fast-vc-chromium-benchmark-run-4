82937ee5495ec468689dae43b42d5111aa74f2d6
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 12:43:41 +0000

    [filemanager] Remove old metadata code.
    BUG=129347
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10441149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/metadata/metadata_cache.js
+++ chrome/browser/resources/file_manager/js/metadata/metadata_cache.js
@@ -511,33 +511,31 @@ MetadataCache.prototype.mergeProperties_ = function(url, data) {
 /**
  * Base class for metadata providers.
  * @constructor
- * TODO(dgozman): rename to MetadataProvider (now it conflicts with another
- *     entity).
  */
-function MetadataProvider2() {
+function MetadataProvider() {
 }
 
 /**
  * @param {string} url The url.
  * @return {boolean} Whether this provider supports the url.
  */
-MetadataProvider2.prototype.supportsUrl = function(url) { return false; };
+MetadataProvider.prototype.supportsUrl = function(url) { return false; };
 
 /**
  * @param {string} type The metadata type.
  * @return {boolean} Whether this provider provides this metadata.
  */
-MetadataProvider2.prototype.providesType = function(type) { return false; };
+MetadataProvider.prototype.providesType = function(type) { return false; };
 
 /**
  * @return {string} Unique provider id.
  */
-MetadataProvider2.prototype.getId = function() { return ''; };
+MetadataProvider.prototype.getId = function() { return ''; };
 
 /**
  * @return {boolean} Whether provider is ready.
  */
-MetadataProvider2.prototype.isInitialized = function() { return true; };
+MetadataProvider.prototype.isInitialized = function() { return true; };
 
 /**
  * Fetches the metadata. It's suggested to return all the metadata this provider
@@ -548,7 +546,7 @@ MetadataProvider2.prototype.isInitialized = function() { return true; };
  *     to metadata value.
  * @param {Entry=} opt_entry The file entry if present.
  */
-MetadataProvider2.prototype.fetch = function(url, type, callback, opt_entry) {
+MetadataProvider.prototype.fetch = function(url, type, callback, opt_entry) {
   throw new Error('Default metadata provider cannot fetch.');
 };
 
@@ -560,11 +558,11 @@ MetadataProvider2.prototype.fetch = function(url, type, callback, opt_entry) {
  * @constructor
  */
 function FilesystemProvider() {
-  MetadataProvider2.call(this);
+  MetadataProvider.call(this);
 }
 
 FilesystemProvider.prototype = {
-  __proto__: MetadataProvider2.prototype
+  __proto__: MetadataProvider.prototype
 };
 
 /**
@@ -629,7 +627,7 @@ FilesystemProvider.prototype.fetch = function(url, type, callback, opt_entry) {
  * @constructor
  */
 function GDataProvider() {
-  MetadataProvider2.call(this);
+  MetadataProvider.call(this);
 
   // We batch metadata fetches into single API call.
   this.urls_ = [];
@@ -640,7 +638,7 @@ function GDataProvider() {
 }
 
 GDataProvider.prototype = {
-  __proto__: MetadataProvider2.prototype
+  __proto__: MetadataProvider.prototype
 };
 
 /**
@@ -774,7 +772,7 @@ GDataProvider.prototype.convert_ = function(data) {
  * @constructor
  */
 function ContentProvider() {
-  MetadataProvider2.call(this);
+  MetadataProvider.call(this);
 
   // Pass all URLs to the metadata reader until we have a correct filter.
   this.urlFilter_ = /.*/;
@@ -798,7 +796,7 @@ function ContentProvider() {
 }
 
 ContentProvider.prototype = {
-  __proto__: MetadataProvider2.prototype
+  __proto__: MetadataProvider.prototype
 };
 
 /**
