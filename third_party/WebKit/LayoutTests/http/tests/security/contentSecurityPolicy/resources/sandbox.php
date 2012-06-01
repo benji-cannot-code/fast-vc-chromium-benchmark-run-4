6d4b341e92a40209944eb8ac9aa4a902fc40f3f8
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-01 09:26:27 +0000

    sandbox directive in X-WebKit-CSP header unable to create a unique origin https://bugs.webkit.org/show_bug.cgi?id=88014
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    We process the HTTP headers for a response after we create a document
    object for the response. Previously, the SecurityOrigin of a document
    was determined when the document was created, which meant that the
    sandbox directive in CSP couldn't create a unique origin.
    
    In this patch, we transition to a unique origin when we start enforcing
    the SandboxOrigin bit. This patch is more complicated than you might
    expect because we redundantly store the SecurityOrigin object in
    DOMWindow. Removing that redundant state is
    https://bugs.webkit.org/show_bug.cgi?id=75793.
    
    The CSP sandbox directive is defined in
    http://dvcs.w3.org/hg/content-security-policy/raw-file/tip/csp-1.0-specification.html
    by reference to
    http://www.whatwg.org/specs/web-apps/current-work/#forced-sandboxing-flag-set.
    The relation between sandbox and unique origin arises from
    http://www.whatwg.org/specs/web-apps/current-work/#origin-0.
    
    Test: http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-in-http-header-control.html
          http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-in-http-header.html
          http/tests/security/contentSecurityPolicy/sandbox-in-http-header-control.html
          http/tests/security/contentSecurityPolicy/sandbox-in-http-header.html
    
    * dom/Document.cpp:
    (WebCore::Document::didUpdateSecurityOrigin):
    (WebCore::Document::initContentSecurityPolicy):
    * dom/Document.h:
    (Document):
    * dom/SecurityContext.cpp:
    (WebCore::SecurityContext::enforceSandboxFlags):
    (WebCore):
    (WebCore::SecurityContext::didUpdateSecurityOrigin):
    * dom/SecurityContext.h:
    (SecurityContext):
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-in-http-header-control.html: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-in-http-header-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-in-http-header.html: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-in-http-header-control-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-in-http-header-control.html: Added.
    * http/tests/security/contentSecurityPolicy/resources/sandbox.php: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-in-http-header-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-in-http-header.html: Added.
    * http/tests/security/contentSecurityPolicy/sandbox-allow-scripts-in-http-header-control-expected.txt: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/sandbox.php
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("X-WebKit-CSP: sandbox " . $_GET["sandbox"]);
+?>
+<!DOCTYPE html>
+<p>Ready</p>
+<script>
+alert("Script executed in iframe.");
+window.secret = "I am a secret";
+</script>
