2409bd081f2f0c5b4e67b866ab60928c680422f9
zeno.albisser@nokia.com <zeno.albisser@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-01 17:02:00 +0000

    Make TextureMapper work with GraphicsSurface. https://bugs.webkit.org/show_bug.cgi?id=87738
    Add TextureMapperSurfaceBackingStore, a new backing store
    that allows to import textures from a GraphicsSurface.
    On Mac the GraphicsSurface is backed by an IOSurface
    which must be used with a GL_TEXTURE_RECTANGLE_ARB texture.
    Therefore it is also necessary to add new shader programs
    for directly painting these textures on screen.
    
    Reviewed by Noam Rosenthal.
    
    * platform/graphics/texmap/TextureMapperBackingStore.cpp:
    Add a new TextureMapperBackingStore implementation that can directly
    reuse textures as they are being passed from the GraphicsSurface.
    (WebCore::TextureMapperSurfaceBackingStore::setGraphicsSurface):
    (WebCore::TextureMapperSurfaceBackingStore::texture):
    (WebCore::TextureMapperSurfaceBackingStore::paintToTextureMapper):
    * platform/graphics/texmap/TextureMapperBackingStore.h:
    (GraphicsSurfaceData):
    (WebCore::GraphicsSurfaceData::setSurface):
    (WebCore::GraphicsSurfaceData::GraphicsSurfaceData):
    (TextureMapperSurfaceBackingStore):
    (WebCore::TextureMapperSurfaceBackingStore::create):
    (WebCore::TextureMapperSurfaceBackingStore::~TextureMapperSurfaceBackingStore):
    (WebCore::TextureMapperSurfaceBackingStore::TextureMapperSurfaceBackingStore):
    
    * platform/graphics/texmap/TextureMapperGL.cpp:
    Add a drawing function for textures of type GL_TEXTURE_RECTANGLE_ARB.
    (WebCore::TextureMapperGL::drawTextureRectangleARB):
    * platform/graphics/texmap/TextureMapperShaderManager.cpp:
    (WebCore::TextureMapperShaderManager::getShaderProgram):
    (WebCore::TextureMapperShaderProgramSimple::create):
    (WebCore::TextureMapperShaderProgramSimple::initialize):
    (WebCore::TextureMapperShaderProgramSolidColor::create):
    (WebCore::TextureMapperShaderProgramSolidColor::initialize):
    (WebCore::TextureMapperShaderProgramRectSimple::create):
    (WebCore::TextureMapperShaderProgramRectSimple::fragmentShaderSource):
    (WebCore::TextureMapperShaderProgramOpacityAndMask::create):
    (WebCore::TextureMapperShaderProgramOpacityAndMask::initialize):
    (WebCore::TextureMapperShaderProgramRectOpacityAndMask::create):
    (WebCore::TextureMapperShaderProgramRectOpacityAndMask::fragmentShaderSource):
    
    * platform/graphics/texmap/TextureMapperShaderManager.h:
    Add new shader programs that can be used with GL_TEXTURE_RECTANGLE_ARB textures.
    This is mainly necessary, because this type of texture uses non-normalized coordinates.
    Further move the calls to initializeProgram() from the constructor into a separate
    initialize() function, as initializeProgram() calls vertexShaderSource() (and friends)
    and we should not call virtual functions in the constructor.
    (WebCore::TextureMapperShaderProgram::initialize):
    (TextureMapperShaderProgramSimple):
    (WebCore::TextureMapperShaderProgramSimple::TextureMapperShaderProgramSimple):
    (TextureMapperShaderProgramRectSimple):
    (WebCore::TextureMapperShaderProgramRectSimple::TextureMapperShaderProgramRectSimple):
    (TextureMapperShaderProgramOpacityAndMask):
    (WebCore::TextureMapperShaderProgramOpacityAndMask::TextureMapperShaderProgramOpacityAndMask):
    (TextureMapperShaderProgramRectOpacityAndMask):
    (WebCore::TextureMapperShaderProgramRectOpacityAndMask::TextureMapperShaderProgramRectOpacityAndMask):
    (TextureMapperShaderProgramSolidColor):
    (WebCore::TextureMapperShaderProgramSolidColor::TextureMapperShaderProgramSolidColor):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperGL.cpp
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TextureMapperGL.h"
 
 #include "GraphicsContext.h"
+#include "GraphicsSurface.h"
 #include "Image.h"
 #include "TextureMapperShaderManager.h"
 #include "Timer.h"
@@ -365,6 +366,34 @@ void TextureMapperGL::drawTexture(const BitmapTexture& texture, const FloatRect&
 
     const BitmapTextureGL& textureGL = static_cast<const BitmapTextureGL&>(texture);
     drawTexture(textureGL.id(), textureGL.isOpaque() ? 0 : SupportsBlending, textureGL.size(), targetRect, matrix, opacity, mask);
+}
+
+void TextureMapperGL::drawTextureRectangleARB(uint32_t texture, Flags flags, const IntSize& textureSize, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, const BitmapTexture* maskTexture)
+{
+    RefPtr<TextureMapperShaderProgram> shaderInfo;
+    if (maskTexture)
+        shaderInfo = data().sharedGLData().textureMapperShaderManager.getShaderProgram(TextureMapperShaderManager::RectOpacityAndMask);
+    else
+        shaderInfo = data().sharedGLData().textureMapperShaderManager.getShaderProgram(TextureMapperShaderManager::RectSimple);
+    GL_CMD(glUseProgram(shaderInfo->id()));
+
+    GL_CMD(glEnableVertexAttribArray(shaderInfo->vertexAttrib()));
+    GL_CMD(glActiveTexture(GL_TEXTURE0));
+    GL_CMD(glBindTexture(GL_TEXTURE_RECTANGLE_ARB, texture));
+    GL_CMD(glUniform1i(shaderInfo->sourceTextureVariable(), 0));
+
+    const GLfloat m4src[] = {
+        targetRect.width(), 0, 0, 0,
+        0, (flags & ShouldFlipTexture) ? -targetRect.height() : targetRect.height(), 0, 0,
+        0, 0, 1, 0,
+        0, (flags & ShouldFlipTexture) ? 1 : 0, 0, 1};
+
+    GL_CMD(glUniformMatrix4fv(shaderInfo->sourceMatrixVariable(), 1, GL_FALSE, m4src));
+
+    shaderInfo->prepare(opacity, maskTexture);
+
+    bool needsBlending = (flags & SupportsBlending) || opacity < 0.99 || maskTexture;
+    drawRect(targetRect, modelViewMatrix, shaderInfo.get(), GL_TRIANGLE_FAN, needsBlending);
 }
 
 void TextureMapperGL::drawTexture(uint32_t texture, Flags flags, const IntSize& textureSize, const FloatRect& targetRect, const TransformationMatrix& modelViewMatrix, float opacity, const BitmapTexture* maskTexture)
--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperShaderManager.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperShaderManager.cpp
@@ -56,6 +56,20 @@ static const char* fragmentShaderSourceOpacityAndMask =
         }
     );
 
+static const char* fragmentShaderSourceRectOpacityAndMask =
+    FRAGMENT_SHADER(
+        uniform sampler2DRect SourceTexture, MaskTexture;
+        uniform lowp float Opacity;
+        varying highp vec2 OutTexCoordSource, OutTexCoordMask;
+        void main(void)
+        {
+            lowp vec4 color = texture2DRect(SourceTexture, OutTexCoordSource);
+            lowp vec4 maskColor = texture2DRect(MaskTexture, OutTexCoordMask);
+            lowp float fragmentAlpha = Opacity * maskColor.a;
+            gl_FragColor = vec4(color.rgb * fragmentAlpha, color.a * fragmentAlpha);
+        }
+    );
+
 static const char* vertexShaderSourceOpacityAndMask =
     VERTEX_SHADER(
         uniform mat4 InMatrix, InSourceMatrix;
@@ -77,6 +91,18 @@ static const char* fragmentShaderSourceSimple =
         void main(void)
         {
             lowp vec4 color = texture2D(SourceTexture, OutTexCoordSource);
+            gl_FragColor = vec4(color.rgb * Opacity, color.a * Opacity);
+        }
+    );
+
+static const char* fragmentShaderSourceRectSimple =
+    FRAGMENT_SHADER(
+        uniform sampler2DRect SourceTexture;
+        uniform lowp float Opacity;
+        varying highp vec2 OutTexCoordSource;
+        void main(void)
+        {
+            lowp vec4 color = texture2DRect(SourceTexture, OutTexCoordSource);
             gl_FragColor = vec4(color.rgb * Opacity, color.a * Opacity);
         }
     );
@@ -132,8 +158,14 @@ PassRefPtr<TextureMapperShaderProgram> TextureMapperShaderManager::getShaderProg
     case Simple:
         program = TextureMapperShaderProgramSimple::create();
         break;
+    case RectSimple:
+        program = TextureMapperShaderProgramRectSimple::create();
+        break;
     case OpacityAndMask:
         program = TextureMapperShaderProgramOpacityAndMask::create();
+        break;
+    case RectOpacityAndMask:
+        program = TextureMapperShaderProgramRectOpacityAndMask::create();
         break;
     case SolidColor:
         program = TextureMapperShaderProgramSolidColor::create();
@@ -187,10 +219,13 @@ TextureMapperShaderProgram::~TextureMapperShaderProgram()
 
 PassRefPtr<TextureMapperShaderProgramSimple> TextureMapperShaderProgramSimple::create()
 {
-    return adoptRef(new TextureMapperShaderProgramSimple());
+    RefPtr<TextureMapperShaderProgramSimple> program = adoptRef(new TextureMapperShaderProgramSimple());
+    // Avoid calling virtual functions in constructor.
+    program->initialize();
+    return program.release();
 }
 
-TextureMapperShaderProgramSimple::TextureMapperShaderProgramSimple()
+void TextureMapperShaderProgramSimple::initialize()
 {
     initializeProgram();
     getUniformLocation(m_sourceMatrixVariable, "InSourceMatrix");
@@ -216,10 +251,13 @@ void TextureMapperShaderProgramSimple::prepare(float opacity, const BitmapTextur
 
 PassRefPtr<TextureMapperShaderProgramSolidColor> TextureMapperShaderProgramSolidColor::create()
 {
-    return adoptRef(new TextureMapperShaderProgramSolidColor());
+    RefPtr<TextureMapperShaderProgramSolidColor> program = adoptRef(new TextureMapperShaderProgramSolidColor());
+    // Avoid calling virtual functions in constructor.
+    program->initialize();
+    return program.release();
 }
 
-TextureMapperShaderProgramSolidColor::TextureMapperShaderProgramSolidColor()
+void TextureMapperShaderProgramSolidColor::initialize()
 {
     initializeProgram();
     getUniformLocation(m_matrixVariable, "InMatrix");
@@ -236,12 +274,28 @@ const char* TextureMapperShaderProgramSolidColor::fragmentShaderSource() const
     return fragmentShaderSourceSolidColor;
 }
 
-PassRefPtr<TextureMapperShaderProgramOpacityAndMask> TextureMapperShaderProgramOpacityAndMask::create()
+PassRefPtr<TextureMapperShaderProgramRectSimple> TextureMapperShaderProgramRectSimple::create()
 {
-    return adoptRef(new TextureMapperShaderProgramOpacityAndMask());
+    RefPtr<TextureMapperShaderProgramRectSimple> program = adoptRef(new TextureMapperShaderProgramRectSimple());
+    // Avoid calling virtual functions in constructor.
+    program->initialize();
+    return program.release();
 }
 
-TextureMapperShaderProgramOpacityAndMask::TextureMapperShaderProgramOpacityAndMask()
+const char* TextureMapperShaderProgramRectSimple::fragmentShaderSource() const
+{
+    return fragmentShaderSourceRectSimple;
+}
+
+PassRefPtr<TextureMapperShaderProgramOpacityAndMask> TextureMapperShaderProgramOpacityAndMask::create()
+{
+    RefPtr<TextureMapperShaderProgramOpacityAndMask> program = adoptRef(new TextureMapperShaderProgramOpacityAndMask());
+    // Avoid calling virtual functions in constructor.
+    program->initialize();
+    return program.release();
+}
+
+void TextureMapperShaderProgramOpacityAndMask::initialize()
 {
     initializeProgram();
     getUniformLocation(m_matrixVariable, "InMatrix");
@@ -272,6 +326,19 @@ void TextureMapperShaderProgramOpacityAndMask::prepare(float opacity, const Bitm
     glBindTexture(GL_TEXTURE_2D, maskTextureGL->id());
     glUniform1i(m_maskTextureVariable, 1);
     glActiveTexture(GL_TEXTURE0);
+}
+
+PassRefPtr<TextureMapperShaderProgramRectOpacityAndMask> TextureMapperShaderProgramRectOpacityAndMask::create()
+{
+    RefPtr<TextureMapperShaderProgramRectOpacityAndMask> program = adoptRef(new TextureMapperShaderProgramRectOpacityAndMask());
+    // Avoid calling virtual functions in constructor.
+    program->initialize();
+    return program.release();
+}
+
+const char* TextureMapperShaderProgramRectOpacityAndMask::fragmentShaderSource() const
+{
+    return fragmentShaderSourceRectOpacityAndMask;
 }
 
 TextureMapperShaderManager::TextureMapperShaderManager()
