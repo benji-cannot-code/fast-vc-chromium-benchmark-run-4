6d220bd9b2a3465595c7df6f51b45d845ffd8b83
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-01 22:43:11 +0000

    Use fully qualified name for Fixed to avoid the collision with "typedef SInt32 Fixed" in MacTypes.h
    * tests/GraphicsLayerChromiumTest.cpp:
    (WebKitTests::TEST_F):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/GraphicsLayerChromiumTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/GraphicsLayerChromiumTest.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerChromium.h"
 #include "Matrix3DTransformOperation.h"
 #include "RotateTransformOperation.h"
+#include "Length.h"
 #include "TranslateTransformOperation.h"
 #include "WebCompositor.h"
 
@@ -210,11 +211,11 @@ TEST_F(GraphicsLayerChromiumTest, createTransformAnimation)
     WebCore::KeyframeValueList values(AnimatedPropertyWebkitTransform);
 
     TransformOperations operations1;
-    operations1.operations().append(TranslateTransformOperation::create(Length(2, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations1.operations().append(TranslateTransformOperation::create(Length(2, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(0, &operations1));
 
     TransformOperations operations2;
-    operations2.operations().append(TranslateTransformOperation::create(Length(4, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations2.operations().append(TranslateTransformOperation::create(Length(4, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(duration, &operations2));
 
     RefPtr<Animation> animation = Animation::create();
@@ -293,11 +294,11 @@ TEST_F(GraphicsLayerChromiumTest, createReversedAnimation)
     WebCore::KeyframeValueList values(AnimatedPropertyWebkitTransform);
 
     TransformOperations operations1;
-    operations1.operations().append(TranslateTransformOperation::create(Length(2, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations1.operations().append(TranslateTransformOperation::create(Length(2, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(0, &operations1));
 
     TransformOperations operations2;
-    operations2.operations().append(TranslateTransformOperation::create(Length(4, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations2.operations().append(TranslateTransformOperation::create(Length(4, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(duration, &operations2));
 
     RefPtr<Animation> animation = Animation::create();
@@ -330,11 +331,11 @@ TEST_F(GraphicsLayerChromiumTest, createAlternatingAnimation)
     WebCore::KeyframeValueList values(AnimatedPropertyWebkitTransform);
 
     TransformOperations operations1;
-    operations1.operations().append(TranslateTransformOperation::create(Length(2, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations1.operations().append(TranslateTransformOperation::create(Length(2, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(0, &operations1));
 
     TransformOperations operations2;
-    operations2.operations().append(TranslateTransformOperation::create(Length(4, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations2.operations().append(TranslateTransformOperation::create(Length(4, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(duration, &operations2));
 
     RefPtr<Animation> animation = Animation::create();
@@ -369,11 +370,11 @@ TEST_F(GraphicsLayerChromiumTest, createReversedAlternatingAnimation)
     WebCore::KeyframeValueList values(AnimatedPropertyWebkitTransform);
 
     TransformOperations operations1;
-    operations1.operations().append(TranslateTransformOperation::create(Length(2, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations1.operations().append(TranslateTransformOperation::create(Length(2, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(0, &operations1));
 
     TransformOperations operations2;
-    operations2.operations().append(TranslateTransformOperation::create(Length(4, Fixed), Length(0, Fixed), TransformOperation::TRANSLATE_X));
+    operations2.operations().append(TranslateTransformOperation::create(Length(4, WebCore::Fixed), Length(0, WebCore::Fixed), TransformOperation::TRANSLATE_X));
     values.insert(new TransformAnimationValue(duration, &operations2));
 
     RefPtr<Animation> animation = Animation::create();
