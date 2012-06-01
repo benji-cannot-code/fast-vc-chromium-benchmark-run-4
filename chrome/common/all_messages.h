80d0e8e7586e03a30ffdc2307e8760bd684090ce
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-01 20:46:52 +0000

    Revert 140093 - Open pepper files directly in browser.
    This CL merges in the changes for the per-profile plugin process from the
    previously retired CL.
    Review URL: https://chromiumcodereview.appspot.com/10387195
    
    TBR=tsepez@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10477006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/all_messages.h
+++ chrome/common/all_messages.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -18,5 +18,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/common_message_generator.h"
 #include "chrome/common/nacl_messages.h"
 #include "content/common/content_message_generator.h"
-#include "ppapi/proxy/pepper_file_messages.h"
 #include "ppapi/proxy/ppapi_messages.h"
