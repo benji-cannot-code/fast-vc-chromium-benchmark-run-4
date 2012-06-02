1badaa905b082190ead2b60c1bb6ab4b983182a8
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-02 02:57:40 +0000

    Remove old MIME type for client plugin, and cleanup unused include.
    BUG=86353,112532
    
    
    Review URL: https://chromiumcodereview.appspot.com/10478002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140168 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/chromoting_instance.cc
+++ remoting/client/plugin/chromoting_instance.cc
@@ -25,8 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/completion_callback.h"
 #include "ppapi/cpp/input_event.h"
 #include "ppapi/cpp/rect.h"
-// TODO(wez): Remove this when crbug.com/86353 is complete.
-#include "ppapi/cpp/private/var_private.h"
 #include "remoting/base/constants.h"
 #include "remoting/base/util.h"
 #include "remoting/client/client_config.h"
