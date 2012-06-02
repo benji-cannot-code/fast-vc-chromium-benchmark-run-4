32eda1bec3af987160c91ce7adc6200af34189b9
jamiewalch@google.com <jamiewalch@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-02 00:39:53 +0000

    Refactored innerText vs. innerHTML in l10n.js and removed other uses of innerHTML.
    BUG=130582
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/10501005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140141 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_table_entry.js
+++ remoting/webapp/host_table_entry.js
@@ -222,7 +222,7 @@ remoting.HostTableEntry.prototype.beginRename_ = function() {
   var editBox = /** @type {HTMLInputElement} */ document.createElement('input');
   editBox.type = 'text';
   editBox.value = this.host.hostName;
-  this.hostNameCell_.innerHTML = '';
+  this.hostNameCell_.innerText = '';
   this.hostNameCell_.appendChild(editBox);
   editBox.select();
 
@@ -344,7 +344,7 @@ remoting.HostTableEntry.prototype.setHostName_ = function() {
                                                     this.host.hostName);
   }
   hostNameNode.classList.add('host-list-label');
-  this.hostNameCell_.innerHTML = '';  // Remove previous contents (if any).
+  this.hostNameCell_.innerText = '';  // Remove previous contents (if any).
   this.hostNameCell_.appendChild(hostNameNode);
 };
 
