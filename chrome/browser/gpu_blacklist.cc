077d0195bae305b97c667a6fec066d6ac14a8299
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-02 01:46:20 +0000

    Add support for blacklisting 3d and stage3d in Flapper
    This allows the software rendering list to blacklist all 3d in flash, or just 3d used for stage3d (arbitrary 3d content).
    
    It's not actually hooked up to flash yet.
    
    
    BUG=129847
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10453092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140155 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_blacklist.cc
+++ chrome/browser/gpu_blacklist.cc
@@ -643,6 +643,8 @@ bool GpuBlacklist::GpuBlacklistEntry::SetBlacklistedFeatures(
       case content::GPU_FEATURE_TYPE_ACCELERATED_COMPOSITING:
       case content::GPU_FEATURE_TYPE_WEBGL:
       case content::GPU_FEATURE_TYPE_MULTISAMPLING:
+      case content::GPU_FEATURE_TYPE_FLASH3D:
+      case content::GPU_FEATURE_TYPE_FLASH_STAGE3D:
       case content::GPU_FEATURE_TYPE_ALL:
         feature_type |= type;
         break;
--- content/public/common/gpu_feature_type.h
+++ content/public/common/gpu_feature_type.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,10 +17,14 @@ enum GpuFeatureType {
   GPU_FEATURE_TYPE_ACCELERATED_COMPOSITING = 1 << 1,
   GPU_FEATURE_TYPE_WEBGL = 1 << 2,
   GPU_FEATURE_TYPE_MULTISAMPLING = 1 << 3,
+  GPU_FEATURE_TYPE_FLASH3D = 1 << 4,
+  GPU_FEATURE_TYPE_FLASH_STAGE3D = 1 << 5,
   GPU_FEATURE_TYPE_ALL = GPU_FEATURE_TYPE_ACCELERATED_2D_CANVAS |
                          GPU_FEATURE_TYPE_ACCELERATED_COMPOSITING |
                          GPU_FEATURE_TYPE_WEBGL |
-                         GPU_FEATURE_TYPE_MULTISAMPLING,
+                         GPU_FEATURE_TYPE_MULTISAMPLING |
+                         GPU_FEATURE_TYPE_FLASH3D |
+                         GPU_FEATURE_TYPE_FLASH_STAGE3D,
   // All flags initialized to false, i.e., no feature is blacklisted.
   GPU_FEATURE_TYPE_UNKNOWN = 0
 };
