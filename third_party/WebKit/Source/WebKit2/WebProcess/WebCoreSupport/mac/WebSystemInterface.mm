b6f18ac9b7db3522c60a126d8726440519a0f74d
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-03 02:33:02 +0000

    Remove a duplicate INIT() from WebSystemInterface.mm https://bugs.webkit.org/show_bug.cgi?id=88181
    Reviewed by Anders Carlsson.
    
    * WebProcess/WebCoreSupport/mac/WebSystemInterface.mm:
    (InitWebCoreSystemInterface):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119347 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/mac/WebSystemInterface.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/mac/WebSystemInterface.mm
@@ -157,8 +157,6 @@ void InitWebCoreSystemInterface(void)
         INIT(CopyCFURLResponseSuggestedFilename);
         INIT(SetCFURLResponseMIMEType);
 
-        INIT(SetMetadataURL);
-
 #if !defined(BUILDING_ON_SNOW_LEOPARD)
         INIT(CreateVMPressureDispatchOnMainQueue);
 #endif
