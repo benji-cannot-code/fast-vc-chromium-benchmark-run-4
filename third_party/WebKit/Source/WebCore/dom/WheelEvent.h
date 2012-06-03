84495b854c562f9c0c47ad8a70abe9a3dc7df4a8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-03 15:51:08 +0000

    Unreviewed, rolling out r119359. http://trac.webkit.org/changeset/119359 https://bugs.webkit.org/show_bug.cgi?id=88189
    This broke platform/mac/fast/dom/wrapper-classes-objc.html.
    (Requested by dominicc on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-06-03
    
    Source/WebCore:
    
    * bindings/objc/PublicDOMInterfaces.h:
    * bindings/scripts/CodeGeneratorObjC.pm:
    (GetBaseClass):
    * dom/MouseEvent.h:
    (MouseEvent):
    * dom/WheelEvent.cpp:
    (WebCore::WheelEvent::WheelEvent):
    * dom/WheelEvent.h:
    (WheelEvent):
    * dom/WheelEvent.idl:
    
    LayoutTests:
    
    * fast/events/event-creation-expected.txt:
    * fast/events/event-creation.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/WheelEvent.h
+++ third_party/WebKit/Source/WebCore/dom/WheelEvent.h
@@ -27,12 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "EventDispatchMediator.h"
 #include "FloatPoint.h"
-#include "MouseEvent.h"
+#include "MouseRelatedEvent.h"
 
 namespace WebCore {
 
     // extension: mouse wheel event
-    class WheelEvent : public MouseEvent {
+    class WheelEvent : public MouseRelatedEvent {
     public:
         enum { tickMultiplier = 120 };
 
@@ -75,7 +75,7 @@ namespace WebCore {
     private:
         WheelEvent();
         WheelEvent(const FloatPoint& wheelTicks, const FloatPoint& rawDelta,
-                   Granularity, PassRefPtr<AbstractView>,
+                   Granularity granularity, PassRefPtr<AbstractView>,
                    const IntPoint& screenLocation, const IntPoint& pageLocation,
                    bool ctrlKey, bool altKey, bool shiftKey, bool metaKey, bool directionInvertedFromDevice);
 
