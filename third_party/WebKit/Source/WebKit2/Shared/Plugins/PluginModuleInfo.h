8fe6999ca717e4f663c628cfbc25780925416736
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-03 00:53:38 +0000

    Ensure cpu_type_t is defined by including <mach/machine.h> https://bugs.webkit.org/show_bug.cgi?id=88178
    Reviewed by Anders Carlsson.
    
    * Shared/Plugins/PluginModuleInfo.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/Plugins/PluginModuleInfo.h
+++ third_party/WebKit/Source/WebKit2/Shared/Plugins/PluginModuleInfo.h
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <WebCore/PluginData.h>
 
+#if PLATFORM(MAC)
+#include <mach/machine.h>
+#endif
+
 namespace WebKit {
 
 struct PluginModuleInfo {
