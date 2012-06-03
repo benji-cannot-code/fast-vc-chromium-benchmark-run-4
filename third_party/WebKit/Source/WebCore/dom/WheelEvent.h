904dcfb162694702c69a99d7f0f55a5cd09c6ea7
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-03 14:33:27 +0000

    WheelEvent should inherit from MouseEvent https://bugs.webkit.org/show_bug.cgi?id=76104
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    The spec for WheelEvent is
    <http://www.w3.org/TR/DOM-Level-3-Events/#webidl-events-WheelEvent>
    
    Tests: fast/events/event-creation.html
           http://samples.msdn.microsoft.com/ietestcenter/dominheritance/showdominheritancetest.htm?Prototype_WheelEvent
    
    * bindings/objc/PublicDOMInterfaces.h: Remove redundant MouseEvent API from WheelEvent
    * bindings/scripts/CodeGeneratorObjC.pm: MouseEvents are Events
    * dom/MouseEvent.h: Expose no-arg constructor to WheelEvent
    * dom/WheelEvent.cpp:
    (WebCore::WheelEvent::WheelEvent): Call MouseEvent superconstructor
    * dom/WheelEvent.h: Extend MouseEvent
    * dom/WheelEvent.idl: "
    
    LayoutTests:
    
    * fast/events/event-creation-expected.txt:
    * fast/events/event-creation.html: Also check WheelEvent instanceof MouseEvent as well as UIEvent, Event
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119359 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/WheelEvent.h
+++ third_party/WebKit/Source/WebCore/dom/WheelEvent.h
@@ -27,12 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "EventDispatchMediator.h"
 #include "FloatPoint.h"
-#include "MouseRelatedEvent.h"
+#include "MouseEvent.h"
 
 namespace WebCore {
 
     // extension: mouse wheel event
-    class WheelEvent : public MouseRelatedEvent {
+    class WheelEvent : public MouseEvent {
     public:
         enum { tickMultiplier = 120 };
 
@@ -75,7 +75,7 @@ namespace WebCore {
     private:
         WheelEvent();
         WheelEvent(const FloatPoint& wheelTicks, const FloatPoint& rawDelta,
-                   Granularity granularity, PassRefPtr<AbstractView>,
+                   Granularity, PassRefPtr<AbstractView>,
                    const IntPoint& screenLocation, const IntPoint& pageLocation,
                    bool ctrlKey, bool altKey, bool shiftKey, bool metaKey, bool directionInvertedFromDevice);
 
