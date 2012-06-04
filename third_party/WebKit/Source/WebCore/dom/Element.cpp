cfb1288d46a4a6c85238cf9763b76d09e2781a28
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 08:52:08 +0000

    Use SelectorQuery in Element::webkitMatchesSelector() https://bugs.webkit.org/show_bug.cgi?id=88209
    Reviewed by Antti Koivisto.
    
    By using SelectorQuery in Element::webkitMatchesSelector(),
    we can not only simplify the code but also improve the performance
    thanks to the cache mechanism of SelectorQuery which was implemented
    in bug 87942.
    
    Tests: fast/dom/SelectorAPI/*. No change in test results.
    
    * dom/Element.cpp:
    (WebCore::Element::webkitMatchesSelector):
    * dom/SelectorQuery.cpp:
    (WebCore::SelectorQuery::matches):
    (WebCore):
    * dom/SelectorQuery.h:
    (SelectorQuery):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119375 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderRegion.h"
 #include "RenderView.h"
 #include "RenderWidget.h"
+#include "SelectorQuery.h"
 #include "Settings.h"
 #include "ShadowRoot.h"
 #include "StyleResolver.h"
@@ -1765,29 +1766,11 @@ bool Element::webkitMatchesSelector(const String& selector, ExceptionCode& ec)
         ec = SYNTAX_ERR;
         return false;
     }
-    CSSParserContext parserContext(document());
-    CSSParser parser(parserContext);
-    CSSSelectorList selectorList;
-    parser.parseSelector(selector, selectorList);
 
-    if (!selectorList.first()) {
-        ec = SYNTAX_ERR;
+    SelectorQuery* selectorQuery = document()->selectorQueryCache()->add(selector, document(), ec);
+    if (!selectorQuery)
         return false;
-    }
-
-    // Throw a NAMESPACE_ERR if the selector includes any namespace prefixes.
-    if (selectorList.selectorsNeedNamespaceResolution()) {
-        ec = NAMESPACE_ERR;
-        return false;
-    }
-
-    SelectorChecker selectorChecker(document(), parserContext.mode == CSSStrictMode);
-    for (CSSSelector* selector = selectorList.first(); selector; selector = CSSSelectorList::next(selector)) {
-        if (selectorChecker.checkSelector(selector, this))
-            return true;
-    }
-
-    return false;
+    return selectorQuery->matches(this);
 }
 
 DOMTokenList* Element::classList()
