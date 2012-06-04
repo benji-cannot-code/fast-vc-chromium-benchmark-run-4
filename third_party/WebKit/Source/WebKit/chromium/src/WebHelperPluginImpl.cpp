0da4c963bb32b507b6d4d47161b33d80b09312f9
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 20:14:56 +0000

    Fix attempt after r119411.
    * src/WebHelperPluginImpl.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebHelperPluginImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebHelperPluginImpl.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "EmptyClients.h"
 #include "FocusController.h"
+#include "FileChooser.h"
 #include "FrameView.h"
 #include "Page.h"
 #include "PageWidgetDelegate.h"
