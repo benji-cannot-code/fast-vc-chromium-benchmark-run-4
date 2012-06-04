1542ea7a0767c4da62f1454b55ed9d64c03b249f
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-04 22:17:42 +0000

    Strip trailing path separators from XDG util results.
    TEST=NONE
    BUG=NONE
    
    
    Review URL: https://chromiumcodereview.appspot.com/10496007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140398 0039d316-1c4b-4281-b951-d872f2087c98

--- base/nix/xdg_util.cc
+++ base/nix/xdg_util.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,9 +25,9 @@ FilePath GetXDGUserDirectory(Environment* env, const char* dir_name,
                              const char* fallback_dir) {
   char* xdg_dir = xdg_user_dir_lookup(dir_name);
   if (xdg_dir) {
-    FilePath rv(xdg_dir);
+    FilePath path(xdg_dir);
     free(xdg_dir);
-    return rv;
+    return path.StripTrailingSeparators();
   }
   return file_util::GetHomeDir().Append(fallback_dir);
 }
