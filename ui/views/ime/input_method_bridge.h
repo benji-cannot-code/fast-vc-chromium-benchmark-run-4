00b3ebaff317812cdd4e3c73ce535a3f993ef24f
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-04 02:44:52 +0000

    Introduce GetCompositionCharacterBounds.
    This function retrieves character boundaries in composition text.
    This is necessary for Japanese input method to show suggest window which is aligned with composition text.
    
    This CL only adds function for native widget but it isn't used yet.
    This CL is not ready for RenderWidgetHostViewAura because the webkit is not ready for this feature.
    https://bugs.webkit.org/show_bug.cgi?id=87911
    
    This function is similar to IMR_QUERYCHARPOSITION message in Windows or firstRectForCharacterRange in MacOSX.
    IMR_QUERYCHARPOSITION: http://msdn.microsoft.com/en-us/library/windows/desktop/dd318634(v=vs.85).aspx
    firstRectForCharacterRange: https://developer.apple.com/library/mac/#documentation/Cocoa/Reference/NSTextInputClient_Protocol/Reference/Reference.html
    
    
    BUG=120597
    TEST=run unit_tests,ui_unittests,views_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10451002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140259 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/ime/input_method_bridge.h
+++ ui/views/ime/input_method_bridge.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "ui/base/ime/text_input_client.h"
+#include "ui/gfx/rect.h"
 #include "ui/views/ime/input_method_base.h"
 
 namespace ui {
@@ -54,6 +55,8 @@ class InputMethodBridge : public InputMethodBase,
   virtual ui::TextInputType GetTextInputType() const OVERRIDE;
   virtual bool CanComposeInline() const OVERRIDE;
   virtual gfx::Rect GetCaretBounds() OVERRIDE;
+  virtual bool GetCompositionCharacterBounds(uint32 index,
+                                             gfx::Rect* rect) OVERRIDE;
   virtual bool HasCompositionText() OVERRIDE;
   virtual bool GetTextRange(ui::Range* range) OVERRIDE;
   virtual bool GetCompositionTextRange(ui::Range* range) OVERRIDE;
@@ -72,6 +75,10 @@ class InputMethodBridge : public InputMethodBase,
 
  private:
   void UpdateViewFocusState();
+
+  // Returns a rectangle converted from |rect| from a focused View's coordinate
+  // system to that of the screen.
+  gfx::Rect ConvertRectToFocusedView(const gfx::Rect& rect);
 
   ui::InputMethod* const host_;
   bool context_focused_;
