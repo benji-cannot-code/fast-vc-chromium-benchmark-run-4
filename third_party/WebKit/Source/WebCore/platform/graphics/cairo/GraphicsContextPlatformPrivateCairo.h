79f51dad32faf1058de7d1d27a54605ac7336159
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 13:31:14 +0000

    [Cairo] Implementation of GraphicsContext does not save/restore ImageInterpolation correctly https://bugs.webkit.org/show_bug.cgi?id=87985
    Patch by Dominik Röttsches <dominik.rottsches@intel.com> on 2012-06-04
    Reviewed by Gustavo Noronha Silva.
    
    Source/WebCore:
    
    Moving image interpolation quality setting into state so that it's saved and restored.
    
    Covered by existing test fast/canvas/canvas-imageSmoothingEnabled.html which is now passing.
    
    * platform/graphics/cairo/GraphicsContextPlatformPrivateCairo.h:
    (GraphicsContextPlatformPrivate): Removing stale member that was never used.
    * platform/graphics/cairo/PlatformContextCairo.cpp:
    (WebCore::PlatformContextCairo::State::State):
    (PlatformContextCairo::State):
    (WebCore::PlatformContextCairo::PlatformContextCairo):
    (WebCore::PlatformContextCairo::drawSurfaceToContext):
    (WebCore::PlatformContextCairo::setImageInterpolationQuality):
    (WebCore):
    (WebCore::PlatformContextCairo::imageInterpolationQuality):
    * platform/graphics/cairo/PlatformContextCairo.h:
    (PlatformContextCairo): Moving implementations for image interpolation quality setter into implementation file.
    
    LayoutTests:
    
    Unskipping fast/canvas/canvas-imageSmoothingEnabled.html now that it passes.
    
    * platform/efl/TestExpectations:
    * platform/gtk/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/GraphicsContextPlatformPrivateCairo.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/GraphicsContextPlatformPrivateCairo.h
@@ -95,7 +95,6 @@ public:
 
     PlatformContextCairo* platformContext;
     Vector<float> layers;
-    InterpolationQuality imageInterpolationQuality;
 
 #if PLATFORM(GTK)
     GdkEventExpose* expose;
--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/PlatformContextCairo.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/PlatformContextCairo.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Igalia S.L.
  * Copyright (c) 2008, Google Inc. All rights reserved.
+ * Copyright (c) 2012, Intel Corporation
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -63,11 +64,13 @@ class PlatformContextCairo::State {
 public:
     State()
         : m_globalAlpha(1)
+        , m_imageInterpolationQuality(InterpolationDefault)
     {
     }
 
     State(const State& state)
         : m_globalAlpha(state.m_globalAlpha)
+        , m_imageInterpolationQuality(state.m_imageInterpolationQuality)
     {
         // We do not copy m_imageMaskInformation because otherwise it would be applied
         // more than once during subsequent calls to restore().
@@ -75,11 +78,11 @@ public:
 
     ImageMaskInformation m_imageMaskInformation;
     float m_globalAlpha;
+    InterpolationQuality m_imageInterpolationQuality;
 };
 
 PlatformContextCairo::PlatformContextCairo(cairo_t* cr)
     : m_cr(cr)
-    , m_imageInterpolationQuality(InterpolationDefault)
 {
     m_stateStack.append(State());
     m_state = &m_stateStack.last();
@@ -159,7 +162,8 @@ void PlatformContextCairo::drawSurfaceToContext(cairo_surface_t* surface, const 
     // Test using example site at http://www.meyerweb.com/eric/css/edge/complexspiral/demo.html
     RefPtr<cairo_pattern_t> pattern = adoptRef(cairo_pattern_create_for_surface(surface));
 
-    switch (m_imageInterpolationQuality) {
+    ASSERT(m_state);
+    switch (m_state->m_imageInterpolationQuality) {
     case InterpolationNone:
     case InterpolationLow:
         cairo_pattern_set_filter(pattern.get(), CAIRO_FILTER_FAST);
@@ -191,6 +195,19 @@ void PlatformContextCairo::drawSurfaceToContext(cairo_surface_t* surface, const 
     drawPatternToCairoContext(m_cr.get(), pattern.get(), destRect, globalAlpha());
     cairo_restore(m_cr.get());
 }
+
+void PlatformContextCairo::setImageInterpolationQuality(InterpolationQuality quality)
+{
+    ASSERT(m_state);
+    m_state->m_imageInterpolationQuality = quality;
+}
+
+InterpolationQuality PlatformContextCairo::imageInterpolationQuality() const
+{
+    ASSERT(m_state);
+    return m_state->m_imageInterpolationQuality;
+}
+
 
 float PlatformContextCairo::globalAlpha() const
 {
