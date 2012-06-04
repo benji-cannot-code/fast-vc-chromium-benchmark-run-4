8e13cde3c42c421e91b3bee9558e4e2f54bda96c
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 05:38:04 +0000

    [chromium] DataTransferItem.webkitGetAsEntry() should synchronously return entry https://bugs.webkit.org/show_bug.cgi?id=87969
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    Per discussion on whatwg we should make webkitGetAsEntry synchronous.
    http://lists.w3.org/Archives/Public/public-whatwg-archive/2012Apr/0079.html
    
    Tests: editing/pasteboard/data-transfer-items-drag-drop-entry.html
           fast/filesystem/cross-filesystem-op.html
    
    * Modules/filesystem/DataTransferItemFileSystem.h:
    (DataTransferItemFileSystem):
    * Modules/filesystem/DataTransferItemFileSystem.idl:
    * Modules/filesystem/chromium/DataTransferItemFileSystemChromium.cpp:
    (WebCore::DataTransferItemFileSystem::webkitGetAsEntry): Changed it to synchronously return entry.
    
    LayoutTests:
    
    * editing/pasteboard/data-transfer-items-drag-drop-entry-expected.txt:
    * editing/pasteboard/data-transfer-items-drag-drop-entry.html:
    * fast/filesystem/cross-filesystem-op.html:
    * platform/chromium/test_expectations.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/filesystem/DataTransferItemFileSystem.h
+++ third_party/WebKit/Source/WebCore/Modules/filesystem/DataTransferItemFileSystem.h
@@ -39,12 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class DataTransferItem;
-class EntryCallback;
+class Entry;
 class ScriptExecutionContext;
 
 class DataTransferItemFileSystem {
 public:
-    static void webkitGetAsEntry(DataTransferItem*, ScriptExecutionContext*, PassRefPtr<EntryCallback>);
+    static PassRefPtr<Entry> webkitGetAsEntry(DataTransferItem*, ScriptExecutionContext*);
 
 private:
     DataTransferItemFileSystem();
