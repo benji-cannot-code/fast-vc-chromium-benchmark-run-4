0a7a2ae92cbbfe018839190d8ccd882299769a5f
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-04 23:21:24 +0000

    file manager extensions: Clean up OS_CHROMEOS guards.
    Remove the unnecessary guards "#if defined(OS_CHROMEOS)" in file_browser_private_api.cc
    and file_manager_util.cc. Per chrome_browser.gypi, the source files related to file manager
    extensions are included only for Chrome OS.
    
    BUG=127846
    TEST=unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10520009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_manager_util.cc
+++ chrome/browser/chromeos/extensions/file_manager_util.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/gdata/gdata_operation_registry.h"
 #include "chrome/browser/chromeos/gdata/gdata_system_service.h"
 #include "chrome/browser/chromeos/gdata/gdata_util.h"
+#include "chrome/browser/chromeos/media/media_player.h"
 #include "chrome/browser/extensions/crx_installer.h"
 #include "chrome/browser/extensions/extension_install_ui.h"
 #include "chrome/browser/extensions/extension_service.h"
@@ -47,10 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/file_system_mount_point_provider.h"
 #include "webkit/fileapi/file_system_util.h"
 #include "webkit/plugins/webplugininfo.h"
-
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/media/media_player.h"
-#endif
 
 using base::DictionaryValue;
 using base::ListValue;
@@ -602,7 +599,6 @@ bool ExecuteBuiltinHandler(Profile* profile, const FilePath& path,
   if (IsSupportedBrowserExtension(file_extension.data()) ||
       ShouldBeOpenedWithPdfPlugin(profile, file_extension.data())) {
     GURL page_url = net::FilePathToFileURL(path);
-#if defined(OS_CHROMEOS)
     // Override gdata resource to point to internal handler instead of file:
     // URL.
     if (gdata::util::GetSpecialRemoteRootPath().IsParent(path)) {
@@ -617,7 +613,6 @@ bool ExecuteBuiltinHandler(Profile* profile, const FilePath& path,
           base::Bind(&OnGDataFileFound, profile, path, gdata::REGULAR_FILE));
       return true;
     }
-#endif
     OpenNewTab(page_url, (Profile*)NULL);
     return true;
   }
@@ -643,7 +638,6 @@ bool ExecuteBuiltinHandler(Profile* profile, const FilePath& path,
     return true;
   }
 
-#if defined(OS_CHROMEOS)
   if (internal_task_id == kFileBrowserPlayTaskId) {
     GURL url;
     if (!ConvertFileToFileSystemUrl(profile, path,
@@ -674,7 +668,6 @@ bool ExecuteBuiltinHandler(Profile* profile, const FilePath& path,
         GetVideoPlayerUrl(url), NEW_FOREGROUND_TAB, NULL);
     return true;
   }
-#endif  // OS_CHROMEOS
 
   if (IsCRXFile(file_extension.data())) {
     InstallCRX(profile, path);
