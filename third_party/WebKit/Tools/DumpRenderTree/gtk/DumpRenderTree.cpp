df5b99348b1cc0b3258ac12c8941b039dc4f3a9b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 19:30:52 +0000

    [GTK] Memory leaks in DumpRenderTree.cpp https://bugs.webkit.org/show_bug.cgi?id=88213
    Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-06-04
    Reviewed by Martin Robinson.
    
    Fixed memory leaks in DumpRenderTree.
    
    * DumpRenderTree/gtk/DumpRenderTree.cpp:
    (appendString): Accepts const gchar* as the second argument.
    (dumpFramesAsText): Free the string returned by g_strdup_printf().
    (dumpHistoryItem): Free the string returned by g_uri_parse_scheme().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
@@ -132,7 +132,7 @@ void displayWebView()
     gtk_widget_queue_draw(GTK_WIDGET(webView));
 }
 
-static void appendString(gchar*& target, gchar* string)
+static void appendString(gchar*& target, const gchar* string)
 {
     gchar* oldString = target;
     target = g_strconcat(target, string, NULL);
@@ -247,8 +247,10 @@ static gchar* dumpFramesAsText(WebKitWebFrame* frame)
 
     if (gLayoutTestController->dumpChildFramesAsText()) {
         GSList* children = DumpRenderTreeSupportGtk::getFrameChildren(frame);
-        for (GSList* child = children; child; child = g_slist_next(child))
-            appendString(result, dumpFramesAsText(static_cast<WebKitWebFrame* >(child->data)));
+        for (GSList* child = children; child; child = g_slist_next(child)) {
+            GOwnPtr<gchar> childData(dumpFramesAsText(static_cast<WebKitWebFrame*>(child->data)));
+            appendString(result, childData.get());
+        }
         g_slist_free(children);
     }
 
@@ -279,8 +281,10 @@ static void dumpHistoryItem(WebKitWebHistoryItem* item, int indent, bool current
     gchar* uriScheme = g_uri_parse_scheme(uri);
     if (g_strcmp0(uriScheme, "file") == 0) {
         gchar* pos = g_strstr_len(uri, -1, "/LayoutTests/");
-        if (!pos)
+        if (!pos) {
+            g_free(uriScheme);
             return;
+        }
 
         GString* result = g_string_sized_new(strlen(uri));
         result = g_string_append(result, "(file test):");
