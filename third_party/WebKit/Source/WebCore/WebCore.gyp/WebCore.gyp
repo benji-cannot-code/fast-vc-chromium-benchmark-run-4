73fc4fee7a16aac7226c2dc3b27204752a221946
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 03:06:19 +0000

    EventTarget.idl should generate JS bindings https://bugs.webkit.org/show_bug.cgi?id=88120
    This is one step in a larger change to expose a constructor for
    EventTarget and put EventTarget on the prototype chain of DOM
    objects that are event targets, in line with DOM Core. See
    <https://bugs.webkit.org/show_bug.cgi?id=67312>
    
    Reviewed by Adam Barth.
    
    No new tests. This change does not change any functionality.
    
    * CMakeLists.txt: Generate JSEventTarget.
    * DerivedSources.cpp:
    * DerivedSources.make:
    * DerivedSources.pri:
    * GNUmakefile.list.am:
    * Target.pri:
    * UseJSC.cmake:
    * WebCore.gyp/WebCore.gyp:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/js/JSBindingsAllInOne.cpp: JSEventTarget.cpp was renamed.
    * bindings/js/JSEventTarget.h: Removed. Generated now.
    * bindings/js/JSEventTargetCustom.cpp: Renamed from Source/WebCore/bindings/js/JSEventTarget.cpp.
    * bindings/scripts/CodeGeneratorV8.pm:
    (GetInternalFields): EventTarget is an EventTarget.
    * dom/EventTarget.idl: Defer to toJS, toEventTarget in JSEventTargetCustom.cpp.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119367 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCore.gyp/WebCore.gyp
+++ third_party/WebKit/Source/WebCore/WebCore.gyp/WebCore.gyp
@@ -157,7 +157,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'bindings_idl_files!': [
       # Custom bindings in bindings/v8/custom exist for these.
       '../dom/EventListener.idl',
-      '../dom/EventTarget.idl',
       '../html/VoidCallback.idl',
 
       # Bindings with custom Objective-C implementations.
--- third_party/WebKit/Source/WebCore/bindings/js/JSBindingsAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSBindingsAllInOne.cpp
@@ -78,7 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSErrorHandler.cpp"
 #include "JSEventCustom.cpp"
 #include "JSEventListener.cpp"
-#include "JSEventTarget.cpp"
+#include "JSEventTargetCustom.cpp"
 #include "JSExceptionBase.cpp"
 #include "JSFloat32ArrayCustom.cpp"
 #include "JSFloat64ArrayCustom.cpp"
