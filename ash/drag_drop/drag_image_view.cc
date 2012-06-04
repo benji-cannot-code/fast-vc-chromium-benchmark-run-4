e0ad05b209fbeba9965cab6a051c74e4c207840f
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-04 17:53:28 +0000

    Revert 140315 - ash: Stack drag widget at the right order and fix its shadow.
    BUG=118022
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10482002
    
    TBR=varunjain@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10483009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140320 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/drag_drop/drag_image_view.cc
+++ ash/drag_drop/drag_image_view.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/drag_drop/drag_image_view.h"
 
-#include "ash/wm/shadow_types.h"
 #include "ui/views/widget/widget.h"
 
 namespace ash {
@@ -17,14 +16,13 @@ using views::Widget;
 Widget* CreateDragWidget() {
   Widget* drag_widget = new Widget;
   Widget::InitParams params;
-  params.type = Widget::InitParams::TYPE_TOOLTIP;
+  params.type = Widget::InitParams::TYPE_WINDOW_FRAMELESS;
   params.keep_on_top = true;
   params.accept_events = false;
   params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   params.transparent = true;
   drag_widget->Init(params);
   drag_widget->SetOpacity(0xFF);
-  SetShadowType(drag_widget->GetNativeView(), SHADOW_TYPE_NONE);
   return drag_widget;
 }
 }
