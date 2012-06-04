69789a21984070fb182ca0cc3e15506f222f0f24
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 23:25:18 +0000

    [BlackBerry] Split AnimationFrameRateController into its own file https://bugs.webkit.org/show_bug.cgi?id=88242
    Patch by Andrew Lo <anlo@rim.com> on 2012-06-04
    Reviewed by Antonio Gomes.
    
    Include new header file to reflect re-organization of platform animation
    header files.
    
    Source/WebCore:
    
    * platform/graphics/DisplayRefreshMonitor.h:
    
    Source/WebKit/blackberry:
    
    * Api/WebPageCompositor_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPageCompositor_p.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPageCompositor_p.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerCompositingThread.h"
 #include "LayerRenderer.h"
 
-#include <BlackBerryPlatformAnimation.h>
+#include <BlackBerryPlatformAnimationFrameRateController.h>
 #include <BlackBerryPlatformGLES2Context.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/RefCounted.h>
