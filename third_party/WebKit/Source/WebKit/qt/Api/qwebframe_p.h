dcf3081478d350d4695db6abca0e09ff66097d89
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-04 10:24:38 +0000

    [Qt] Don't use WebKit HAVE() macro in public Qt headers
    Reviewed by Kenneth Rohde Christiansen.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/Api/qwebframe_p.h
+++ third_party/WebKit/Source/WebKit/qt/Api/qwebframe_p.h
@@ -31,7 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PlatformString.h"
 #if ENABLE(ORIENTATION_EVENTS)
 #include "qorientationsensor.h"
-#if !(defined(HAVE_QT5) && HAVE_QT5)
+#if QT_VERSION < QT_VERSION_CHECK(5, 0, 0)
 using QTM_NAMESPACE::QOrientationSensor;
 #endif
 #endif // ENABLE(ORIENTATION_EVENTS).
--- third_party/WebKit/Source/WebKit/qt/Api/qwebpage.h
+++ third_party/WebKit/Source/WebKit/qt/Api/qwebpage.h
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QtCore/qobject.h>
 #include <QtCore/qurl.h>
-#if defined(HAVE_QT5) && HAVE_QT5
+#if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
 #include <QtWidgets/qwidget.h>
 #else
 #include <QtGui/qwidget.h>
