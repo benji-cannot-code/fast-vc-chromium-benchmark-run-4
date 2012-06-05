4f8dbd28dae80b3692377ed76023f52ca08bfa17
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-05 10:25:59 +0000

    Remove unused methods from SelectorQuery.h https://bugs.webkit.org/show_bug.cgi?id=88289
    Reviewed by Antti Koivisto.
    
    This patch removes SelectorDataList::size() and
    SelectorDataList::SelectorDataList().
    SelectorDataList::size() is not used by anybody.
    SelectorDataList::SelectorDataList() can be auto-generated
    by a compiler.
    
    No tests. No change in behavior.
    
    * dom/SelectorQuery.cpp:
    * dom/SelectorQuery.h:
    (SelectorDataList):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/SelectorQuery.h
+++ third_party/WebKit/Source/WebCore/dom/SelectorQuery.h
@@ -42,12 +42,7 @@ class NodeList;
 
 class SelectorDataList {
 public:
-    SelectorDataList();
-
     void initialize(const CSSSelectorList&);
-
-    int size() const { return m_selectors.size(); }
-
     bool matches(const SelectorChecker&, Element*) const;
     PassRefPtr<NodeList> queryAll(const SelectorChecker&, Node* rootNode) const;
     PassRefPtr<Element> queryFirst(const SelectorChecker&, Node* rootNode) const;
