c842cff10577a3726d1312789520c4a2d9711434
dcaiafa@chromium.org <dcaiafa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 03:28:17 +0000

    Clipboard support for Mac host.
    BUG=117473
    TEST=clipboard operations over me2me and it2me (linux -> mac)
    
    
    Review URL: https://chromiumcodereview.appspot.com/10500007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140484 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/event_executor_mac.cc
+++ remoting/host/event_executor_mac.cc
@@ -9,9 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Carbon/Carbon.h>
 
 #include "base/basictypes.h"
+#include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/message_loop.h"
+#include "remoting/host/clipboard.h"
 #include "remoting/proto/internal.pb.h"
 #include "remoting/protocol/message_decoder.h"
 #include "third_party/skia/include/core/SkPoint.h"
@@ -51,13 +53,15 @@ class EventExecutorMac : public EventExecutor {
   MessageLoop* message_loop_;
   SkIPoint mouse_pos_;
   uint32 mouse_button_state_;
+  scoped_ptr<Clipboard> clipboard_;
 
   DISALLOW_COPY_AND_ASSIGN(EventExecutorMac);
 };
 
-EventExecutorMac::EventExecutorMac(
-    MessageLoop* message_loop)
-    : message_loop_(message_loop), mouse_button_state_(0) {
+EventExecutorMac::EventExecutorMac(MessageLoop* message_loop)
+    : message_loop_(message_loop),
+      mouse_button_state_(0),
+      clipboard_(Clipboard::Create()) {
   // Ensure that local hardware events are not suppressed after injecting
   // input events.  This allows LocalInputMonitor to detect if the local mouse
   // is being moved whilst a remote user is connected.
@@ -252,7 +256,16 @@ const int kUsVkeyToKeysym[256] = {
 };
 
 void EventExecutorMac::InjectClipboardEvent(const ClipboardEvent& event) {
-  // TODO(simonmorris): Implement clipboard injection.
+  if (MessageLoop::current() != message_loop_) {
+    message_loop_->PostTask(
+        FROM_HERE,
+        base::Bind(&EventExecutorMac::InjectClipboardEvent,
+                   base::Unretained(this),
+                   event));
+    return;
+  }
+
+  clipboard_->InjectClipboardEvent(event);
 }
 
 void EventExecutorMac::InjectKeyEvent(const KeyEvent& event) {
@@ -339,11 +352,28 @@ void EventExecutorMac::InjectMouseEvent(const MouseEvent& event) {
 
 void EventExecutorMac::OnSessionStarted(
     scoped_ptr<protocol::ClipboardStub> client_clipboard) {
-  return;
+  if (MessageLoop::current() != message_loop_) {
+    message_loop_->PostTask(
+        FROM_HERE,
+        base::Bind(&EventExecutorMac::OnSessionStarted,
+                   base::Unretained(this),
+                   base::Passed(&client_clipboard)));
+    return;
+  }
+
+  clipboard_->Start(client_clipboard.Pass());
 }
 
 void EventExecutorMac::OnSessionFinished() {
-  return;
+  if (MessageLoop::current() != message_loop_) {
+    message_loop_->PostTask(
+        FROM_HERE,
+        base::Bind(&EventExecutorMac::OnSessionFinished,
+                   base::Unretained(this)));
+    return;
+  }
+
+  clipboard_->Stop();
 }
 
 }  // namespace
