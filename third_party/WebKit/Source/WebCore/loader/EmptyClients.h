5e7d1ef7a5973ebcf772739ce8ecf86c1b5c8ddd
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-05 09:08:04 +0000

    Move some function definitions in EmptyClients.h to EmptyClients.cpp https://bugs.webkit.org/show_bug.cgi?id=88285
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    If we have definitions of functions with RefPtr<> in EmptyClients.h like
    
        virtual void runOpenPanel(Frame*, PassRefPtr<FileChooser>) { }
    
    and a source file includes EmptyClient.h, the source file needs to
    include FileChooser.h though the source file doesn't use FileChooser
    class explicitly because the definition of runOpenPanel() needs to call
    FileChooser::deref().
    To avoid this, we had better move such functions to EmptyClients.cpp.
    
    No new tests. No behavior changes.
    
    * loader/EmptyClients.cpp: Move such functions from EmptyClients.h to
    here, and move EmptyPopupMenu and EmptySearchPopupMenu too because
    they're used only by EmptyChromeClient.
    (WebCore):
    (EmptyPopupMenu):
    (WebCore::EmptyPopupMenu::show):
    (WebCore::EmptyPopupMenu::hide):
    (WebCore::EmptyPopupMenu::updateFromElement):
    (WebCore::EmptyPopupMenu::disconnectClient):
    (EmptySearchPopupMenu):
    (WebCore::EmptySearchPopupMenu::popupMenu):
    (WebCore::EmptySearchPopupMenu::saveRecentSearches):
    (WebCore::EmptySearchPopupMenu::loadRecentSearches):
    (WebCore::EmptySearchPopupMenu::enabled):
    (WebCore::EmptyChromeClient::createPopupMenu):
    (WebCore::EmptyChromeClient::createSearchPopupMenu):
    (WebCore::EmptyChromeClient::createColorChooser):
    (WebCore::EmptyChromeClient::runOpenPanel):
    (WebCore::EmptyFrameLoaderClient::dispatchDecidePolicyForNewWindowAction):
    (WebCore::EmptyFrameLoaderClient::dispatchDecidePolicyForNavigationAction):
    (WebCore::EmptyFrameLoaderClient::dispatchWillSendSubmitEvent):
    (WebCore::EmptyFrameLoaderClient::dispatchWillSubmitForm):
    (WebCore::EmptyFrameLoaderClient::createDocumentLoader):
    (WebCore::EmptyFrameLoaderClient::createFrame):
    (WebCore::EmptyFrameLoaderClient::createPlugin):
    (WebCore::EmptyFrameLoaderClient::createJavaAppletWidget):
    (WebCore::EmptyFrameLoaderClient::createMediaPlayerProxyPlugin):
    (WebCore::EmptyFrameLoaderClient::createNetworkingContext):
    (WebCore::EmptyFrameLoaderClient::dispatchIntent):
    (WebCore::EmptyTextCheckerClient::requestCheckingOfString):
    (WebCore::EmptyEditorClient::registerUndoStep):
    (WebCore::EmptyEditorClient::registerRedoStep):
    (WebCore::EmptyContextMenuClient::customizeMenu):
    * loader/EmptyClients.h: Move such function definitions to
    EmptyClient.cpp, and append OVERRIDE.
    (EmptyChromeClient):
    (EmptyFrameLoaderClient):
    (EmptyTextCheckerClient):
    (EmptyEditorClient):
    (EmptyContextMenuClient):
    * svg/graphics/SVGImage.cpp: Remove unnecessary #includes.
    
    Source/WebKit/chromium:
    
    * src/WebHelperPluginImpl.cpp: Remove unnecessary #includes, and add necessary #includes.
    * src/WebPagePopupImpl.cpp: ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/EmptyClients.h
+++ third_party/WebKit/Source/WebCore/loader/EmptyClients.h
@@ -33,31 +33,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContextMenuClient.h"
 #include "DeviceMotionClient.h"
 #include "DeviceOrientationClient.h"
-#include "DocumentLoader.h"
 #include "DragClient.h"
-#include "EditCommand.h"
 #include "EditorClient.h"
 #include "TextCheckerClient.h"
 #include "FloatRect.h"
 #include "FocusDirection.h"
 #include "FrameLoaderClient.h"
-#include "FrameNetworkingContext.h"
 #include "InspectorClient.h"
 #include "Page.h"
-#include "PopupMenu.h"
 #include "ResourceError.h"
-#include "SearchPopupMenu.h"
 
 #if USE(V8)
 #include <v8.h>
-#endif
-
-#if ENABLE(INPUT_TYPE_COLOR)
-#include "ColorChooser.h"
-#endif
-
-#if ENABLE(WEB_INTENTS)
-#include "IntentRequest.h"
 #endif
 
 /*
@@ -76,25 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class GraphicsContext3D;
-
-class EmptyPopupMenu : public PopupMenu {
-public:
-    virtual void show(const IntRect&, FrameView*, int) {}
-    virtual void hide() {}
-    virtual void updateFromElement() {}
-    virtual void disconnectClient() {}
-};
-
-class EmptySearchPopupMenu : public SearchPopupMenu {
-public:
-    virtual PopupMenu* popupMenu() { return m_popup.get(); }
-    virtual void saveRecentSearches(const AtomicString&, const Vector<String>&) {}
-    virtual void loadRecentSearches(const AtomicString&, Vector<String>&) {}
-    virtual bool enabled() { return false; }
-
-private:
-    RefPtr<EmptyPopupMenu> m_popup;
-};
 
 class EmptyChromeClient : public ChromeClient {
 public:
@@ -151,8 +119,8 @@ public:
     virtual bool selectItemWritingDirectionIsNatural() { return false; }
     virtual bool selectItemAlignmentFollowsMenuWritingDirection() { return false; }
     virtual bool hasOpenedPopup() const OVERRIDE { return false; }
-    virtual PassRefPtr<PopupMenu> createPopupMenu(PopupMenuClient*) const { return adoptRef(new EmptyPopupMenu()); }
-    virtual PassRefPtr<SearchPopupMenu> createSearchPopupMenu(PopupMenuClient*) const { return adoptRef(new EmptySearchPopupMenu()); }
+    virtual PassRefPtr<PopupMenu> createPopupMenu(PopupMenuClient*) const OVERRIDE;
+    virtual PassRefPtr<SearchPopupMenu> createSearchPopupMenu(PopupMenuClient*) const OVERRIDE;
 #if ENABLE(PAGE_POPUP)
     virtual PagePopup* openPagePopup(PagePopupClient*, const IntRect&) OVERRIDE { return 0; }
     virtual void closePagePopup(PagePopup*) OVERRIDE { }
@@ -203,10 +171,10 @@ public:
 #endif
 
 #if ENABLE(INPUT_TYPE_COLOR)
-    virtual PassOwnPtr<ColorChooser> createColorChooser(ColorChooserClient*, const Color&) { return nullptr; }
+    virtual PassOwnPtr<ColorChooser> createColorChooser(ColorChooserClient*, const Color&) OVERRIDE;
 #endif
 
-    virtual void runOpenPanel(Frame*, PassRefPtr<FileChooser>) { }
+    virtual void runOpenPanel(Frame*, PassRefPtr<FileChooser>) OVERRIDE;
     virtual void loadIconForFiles(const Vector<String>&, FileIconLoader*) { }
 
     virtual void formStateDidChange(const Node*) { }
@@ -298,14 +266,14 @@ public:
     virtual void dispatchShow() { }
 
     virtual void dispatchDecidePolicyForResponse(FramePolicyFunction, const ResourceResponse&, const ResourceRequest&) { }
-    virtual void dispatchDecidePolicyForNewWindowAction(FramePolicyFunction, const NavigationAction&, const ResourceRequest&, PassRefPtr<FormState>, const String&) { }
-    virtual void dispatchDecidePolicyForNavigationAction(FramePolicyFunction, const NavigationAction&, const ResourceRequest&, PassRefPtr<FormState>) { }
+    virtual void dispatchDecidePolicyForNewWindowAction(FramePolicyFunction, const NavigationAction&, const ResourceRequest&, PassRefPtr<FormState>, const String&) OVERRIDE;
+    virtual void dispatchDecidePolicyForNavigationAction(FramePolicyFunction, const NavigationAction&, const ResourceRequest&, PassRefPtr<FormState>) OVERRIDE;
     virtual void cancelPolicyCheck() { }
 
     virtual void dispatchUnableToImplementPolicy(const ResourceError&) { }
 
-    virtual void dispatchWillSendSubmitEvent(PassRefPtr<FormState>) { }
-    virtual void dispatchWillSubmitForm(FramePolicyFunction, PassRefPtr<FormState>) { }
+    virtual void dispatchWillSendSubmitEvent(PassRefPtr<FormState>) OVERRIDE;
+    virtual void dispatchWillSubmitForm(FramePolicyFunction, PassRefPtr<FormState>) OVERRIDE;
 
     virtual void revertToProvisionalState(DocumentLoader*) { }
     virtual void setMainDocumentError(DocumentLoader*, const ResourceError&) { }
@@ -350,7 +318,7 @@ public:
     virtual void didFinishLoad() { }
     virtual void prepareForDataSourceReplacement() { }
 
-    virtual PassRefPtr<DocumentLoader> createDocumentLoader(const ResourceRequest& request, const SubstituteData& substituteData) { return DocumentLoader::create(request, substituteData); }
+    virtual PassRefPtr<DocumentLoader> createDocumentLoader(const ResourceRequest&, const SubstituteData&) OVERRIDE;
     virtual void setTitle(const StringWithDirection&, const KURL&) { }
 
     virtual String userAgent(const KURL&) { return ""; }
@@ -374,11 +342,11 @@ public:
     virtual void didDisplayInsecureContent() { }
     virtual void didRunInsecureContent(SecurityOrigin*, const KURL&) { }
     virtual void didDetectXSS(const KURL&, bool) { }
-    virtual PassRefPtr<Frame> createFrame(const KURL&, const String&, HTMLFrameOwnerElement*, const String&, bool, int, int) { return 0; }
-    virtual PassRefPtr<Widget> createPlugin(const IntSize&, HTMLPlugInElement*, const KURL&, const Vector<String>&, const Vector<String>&, const String&, bool) { return 0; }
-    virtual PassRefPtr<Widget> createJavaAppletWidget(const IntSize&, HTMLAppletElement*, const KURL&, const Vector<String>&, const Vector<String>&) { return 0; }
+    virtual PassRefPtr<Frame> createFrame(const KURL&, const String&, HTMLFrameOwnerElement*, const String&, bool, int, int) OVERRIDE;
+    virtual PassRefPtr<Widget> createPlugin(const IntSize&, HTMLPlugInElement*, const KURL&, const Vector<String>&, const Vector<String>&, const String&, bool) OVERRIDE;
+    virtual PassRefPtr<Widget> createJavaAppletWidget(const IntSize&, HTMLAppletElement*, const KURL&, const Vector<String>&, const Vector<String>&) OVERRIDE;
 #if ENABLE(PLUGIN_PROXY_FOR_VIDEO)
-    virtual PassRefPtr<Widget> createMediaPlayerProxyPlugin(const IntSize&, HTMLMediaElement*, const KURL&, const Vector<String>&, const Vector<String>&, const String&) { return 0; }
+    virtual PassRefPtr<Widget> createMediaPlayerProxyPlugin(const IntSize&, HTMLMediaElement*, const KURL&, const Vector<String>&, const Vector<String>&, const String&) OVERRIDE;
     virtual void hideMediaPlayerProxyPlugin(Widget*) { }
     virtual void showMediaPlayerProxyPlugin(Widget*) { }
 #endif
@@ -408,10 +376,10 @@ public:
     virtual bool shouldCacheResponse(DocumentLoader*, unsigned long, const ResourceResponse&, const unsigned char*, unsigned long long) { return true; }
 #endif
 
-    virtual PassRefPtr<FrameNetworkingContext> createNetworkingContext() { return PassRefPtr<FrameNetworkingContext>(); }
+    virtual PassRefPtr<FrameNetworkingContext> createNetworkingContext() OVERRIDE;
 
 #if ENABLE(WEB_INTENTS)
-    virtual void dispatchIntent(PassRefPtr<IntentRequest>) { }
+    virtual void dispatchIntent(PassRefPtr<IntentRequest>) OVERRIDE;
 #endif
 };
 
@@ -429,7 +397,7 @@ public:
 #endif
 
     virtual void getGuessesForWord(const String&, const String&, Vector<String>&) { }
-    virtual void requestCheckingOfString(PassRefPtr<TextCheckingRequest>) { }
+    virtual void requestCheckingOfString(PassRefPtr<TextCheckingRequest>) OVERRIDE;
 };
 
 class EmptyEditorClient : public EditorClient {
@@ -469,8 +437,8 @@ public:
     virtual void didWriteSelectionToPasteboard() { }
     virtual void didSetSelectionTypesForPasteboard() { }
 
-    virtual void registerUndoStep(PassRefPtr<UndoStep>) { }
-    virtual void registerRedoStep(PassRefPtr<UndoStep>) { }
+    virtual void registerUndoStep(PassRefPtr<UndoStep>) OVERRIDE;
+    virtual void registerRedoStep(PassRefPtr<UndoStep>) OVERRIDE;
     virtual void clearUndoRedoOperations() { }
 
     virtual bool canCopyCut(Frame*, bool defaultValue) const { return defaultValue; }
@@ -547,7 +515,7 @@ public:
     virtual void contextMenuDestroyed() { }
 
 #if USE(CROSS_PLATFORM_CONTEXT_MENUS)
-    virtual PassOwnPtr<ContextMenu> customizeMenu(PassOwnPtr<ContextMenu>) { return nullptr; }
+    virtual PassOwnPtr<ContextMenu> customizeMenu(PassOwnPtr<ContextMenu>) OVERRIDE;
 #else
     virtual PlatformMenuDescription getCustomMenuFromDefaultItems(ContextMenu*) { return 0; }
 #endif
