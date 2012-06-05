eb26525b2dc3bc09f891d2c07ea4ccfd62f36cba
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 02:26:22 +0000

    [mac] Fix badly positioned webview in shell window.
    BUG=130669
    R=sail@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10512002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140477 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/shell_window_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/shell_window_cocoa.mm
@@ -43,7 +43,7 @@ ShellWindowCocoa::ShellWindowCocoa(Profile* profile,
   [window setTitle:base::SysUTF8ToNSString(extension->name())];
 
   NSView* view = web_contents()->GetView()->GetNativeView();
-  [view setFrame:rect];
+  [view setFrame:[[window contentView] bounds]];
   [view setAutoresizingMask:NSViewWidthSizable | NSViewHeightSizable];
   [[window contentView] addSubview:view];
 
