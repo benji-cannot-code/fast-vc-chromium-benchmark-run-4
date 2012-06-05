9c5e7f36ef6b51ac2e621cfa2c5f4d3bcc239dfd
nirnimesh@chromium.org <nirnimesh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 19:41:11 +0000

    [chromeos] PyAuto automation for crosh.
    Add automation hook to open crosh, and fetch output from a given hterm.
    Add a test to verify the flow.
    
    
    BUG=128108
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10518004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140587 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -1847,6 +1847,8 @@ void TestingAutomationProvider::SendJSONRequest(int handle,
   handler_map["SetVolume"] = &TestingAutomationProvider::SetVolume;
   handler_map["SetMute"] = &TestingAutomationProvider::SetMute;
 
+  handler_map["OpenCrosh"] = &TestingAutomationProvider::OpenCrosh;
+
 #endif  // defined(OS_CHROMEOS)
 
   std::map<std::string, BrowserJsonHandler> browser_handler_map;
--- chrome/browser/automation/testing_automation_provider_chromeos.cc
+++ chrome/browser/automation/testing_automation_provider_chromeos.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/automation/testing_automation_provider.h"
 
 #include "ash/shell.h"
+#include "ash/shell_delegate.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "base/command_line.h"
 #include "base/i18n/time_formatting.h"
@@ -1395,6 +1396,13 @@ void TestingAutomationProvider::SetMute(DictionaryValue* args,
   }
   audio_handler->SetMuted(mute);
   reply.SendSuccess(NULL);
+}
+
+void TestingAutomationProvider::OpenCrosh(DictionaryValue* args,
+                                        IPC::Message* reply_message) {
+  new NavigationNotificationObserver(
+      NULL, this, reply_message, 1, false, true);
+  ash::Shell::GetInstance()->delegate()->OpenCrosh();
 }
 
 void TestingAutomationProvider::CaptureProfilePhoto(
