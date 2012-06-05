919acaa1d3806f1ecb66e707871c7a523b17d0b3
hebert.christopherj@chromium.org <hebert.christopherj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 22:21:57 +0000

    Add histogram for time it takes to access Windows registry for external extensions.
    BUG=110425
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10456062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/external_registry_extension_loader_win.cc
+++ chrome/browser/extensions/external_registry_extension_loader_win.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -9,7 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/memory/scoped_handle.h"
+#include "base/metrics/histogram.h"
 #include "base/string_util.h"
+#include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "base/version.h"
@@ -46,6 +48,7 @@ void ExternalRegistryExtensionLoader::StartLoading() {
 
 void ExternalRegistryExtensionLoader::LoadOnFileThread() {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
+  base::TimeTicks start_time = base::TimeTicks::Now();
   scoped_ptr<DictionaryValue> prefs(new DictionaryValue);
 
   // A map of IDs, to weed out duplicates between HKCU and HKLM.
@@ -145,6 +148,8 @@ void ExternalRegistryExtensionLoader::LoadOnFileThread() {
   }
 
   prefs_.reset(prefs.release());
+  HISTOGRAM_TIMES("Extensions.ExternalRegistryLoaderWin",
+                  base::TimeTicks::Now() - start_time);
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::Bind(&ExternalRegistryExtensionLoader::LoadFinished, this));
