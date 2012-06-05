e9a89d698aac0172e7e39edd809b0acee3765b7b
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 07:59:54 +0000

    Change "session only" content settings to "clear on exit", and add code that migrates from the "clear on exit" checkbox to content settings
    BUG=129349
    TEST=HostContentSettingsMapTest.MigrateClearOnExit
    
    Review URL: https://chromiumcodereview.appspot.com/10515005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -309,6 +309,7 @@ extern const char kDesktopNotificationAllowedOrigins[];  // OBSOLETE
 extern const char kDesktopNotificationDeniedOrigins[];  // OBSOLETE
 extern const char kDesktopNotificationPosition[];
 extern const char kDefaultContentSettings[];
+extern const char kContentSettingsClearOnExitMigrated[];
 extern const char kContentSettingsVersion[];
 extern const char kContentSettingsPatterns[];  // OBSOLETE
 extern const char kContentSettingsPatternPairs[];
