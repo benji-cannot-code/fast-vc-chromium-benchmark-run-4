7cef780fac1a48bd1a0eceaae2fb9714a1773fe6
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 08:05:54 +0000

    Make up and down arrows open the network menu on the OOBE
    BUG=122313
    TEST=Boot a Chromebook to the OOBE screen.  Tab to the network selection dialog.  Press up or down.
    
    Review URL: https://chromiumcodereview.appspot.com/10502012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140500 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/network_dropdown.js
+++ chrome/browser/resources/chromeos/login/network_dropdown.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -79,6 +79,13 @@ cr.define('cr.ui', function() {
   var DropDown = cr.ui.define('div');
 
   DropDown.ITEM_DIVIDER_ID = -2;
+
+  DropDown.KEYCODE_DOWN = 40;
+  DropDown.KEYCODE_ENTER = 13;
+  DropDown.KEYCODE_ESC = 27;
+  DropDown.KEYCODE_SPACE = 32;
+  DropDown.KEYCODE_TAB = 9;
+  DropDown.KEYCODE_UP = 38;
 
   DropDown.prototype = {
     __proto__: HTMLDivElement.prototype,
@@ -287,8 +294,10 @@ cr.define('cr.ui', function() {
 
       el.addEventListener('keydown', function f(e) {
         if (this.inFocus && !this.controller.isShown &&
-            (e.keyCode == 13 || e.keyCode == 32)) {
-          // Enter or space has been pressed.
+            (e.keyCode == DropDown.KEYCODE_ENTER ||
+             e.keyCode == DropDown.KEYCODE_SPACE ||
+             e.keyCode == DropDown.KEYCODE_UP ||
+             e.keyCode == DropDown.KEYCODE_DOWN)) {
           this.opening = true;
           this.controller.isShown = true;
         }
@@ -306,7 +315,7 @@ cr.define('cr.ui', function() {
         return;
       var selected = this.container.selectedItem;
       switch (e.keyCode) {
-        case 38: {  // Key up.
+        case DropDown.KEYCODE_UP: {
           do {
             selected = selected.previousSibling;
             if (!selected)
@@ -315,7 +324,7 @@ cr.define('cr.ui', function() {
           this.container.selectItem(selected, false);
           break;
         }
-        case 40: {  // Key down.
+        case DropDown.KEYCODE_DOWN: {
           do {
             selected = selected.nextSibling;
             if (!selected)
@@ -324,15 +333,15 @@ cr.define('cr.ui', function() {
           this.container.selectItem(selected, false);
           break;
         }
-        case 27: {  // Esc.
+        case DropDown.KEYCODE_ESC: {
           this.isShown = false;
           break;
         }
-        case 9: {  // Tab.
+        case DropDown.KEYCODE_TAB: {
           this.isShown = false;
           break;
         }
-        case 13: {  // Enter.
+        case DropDown.KEYCODE_ENTER: {
           var button = this.titleButton;
           if (!button.opening) {
             button.focus();
