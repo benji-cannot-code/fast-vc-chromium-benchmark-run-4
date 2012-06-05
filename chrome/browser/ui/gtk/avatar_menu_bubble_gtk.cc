f0303059c2498012db6625a3e354fa5d7bfdbef3
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 00:03:02 +0000

    Fix crash in AvatarMenuBubbleGtk
    From the crash report, it looks like the AvatarMenuBubbleGtk object is valid
    (it is deleted using DeleteSoon which keeps it around longer than the
    BubbleGtk) but the BubbleGtk is already deleted.
    
    BUG=130632
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10482003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140437 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/avatar_menu_bubble_gtk.cc
+++ chrome/browser/ui/gtk/avatar_menu_bubble_gtk.cc
@@ -80,6 +80,7 @@ void AvatarMenuBubbleGtk::OnDestroy(GtkWidget* widget) {
 
 void AvatarMenuBubbleGtk::BubbleClosing(BubbleGtk* bubble,
                                         bool closed_by_escape) {
+  bubble_ = NULL;
 }
 
 void AvatarMenuBubbleGtk::OnAvatarMenuModelChanged(
