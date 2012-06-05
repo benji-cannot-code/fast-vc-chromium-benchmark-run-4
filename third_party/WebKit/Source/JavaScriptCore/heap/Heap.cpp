c1bf2269ff3699024ad021cb18d8678857e2f1bd
mhahnenberg@apple.com <mhahnenberg@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-05 20:38:21 +0000

    Entry into JSC should CRASH() if the Heap is busy https://bugs.webkit.org/show_bug.cgi?id=88355
    Reviewed by Geoffrey Garen.
    
    Interpreter::execute() returns jsNull() right now if we try to enter it while 
    the Heap is busy (e.g. with a collection), which is okay, but some code paths 
    that call Interpreter::execute() allocate objects before checking if the Heap 
    is busy. Attempting to execute JS code while the Heap is busy should not be 
    allowed and should be enforced by a release-mode CRASH() to prevent vague, 
    unhelpful backtraces later on if somebody makes a mistake. Normally, recursively 
    executing JS code is okay, e.g. for evals, but it should not occur during a 
    Heap allocation or collection because the Heap is not guaranteed to be in a 
    consistent state (especially during collections). We are protected from 
    executing JS on the same Heap concurrently on two separate threads because 
    they must each take a JSLock first. However, we are not protected from reentrant 
    execution of JS on the same thread because JSLock allows reentrancy. Therefore, 
    we should fail early if we detect an entrance into JS code while the Heap is busy.
    
    * heap/Heap.cpp: Changed Heap::collect so that it sets the m_operationInProgress field 
    at the beginning of collection and then unsets it at the end so that it is set at all 
    times throughout the duration of a collection rather than sporadically during various 
    phases. There is no reason to unset during a collection because our collector does 
    not currently support running additional JS between the phases of a collection.
    (JSC::Heap::getConservativeRegisterRoots):
    (JSC::Heap::markRoots):
    (JSC::Heap::collect):
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::execute): Crash if the Heap is busy.
    * runtime/Completion.cpp: Crash if the Heap is busy. We do it here before we call 
    Interpreter::execute() because we do some allocation prior to calling execute() which 
    could cause Heap corruption if, for example, that allocation caused a collection.
    (JSC::evaluate):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119518 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
@@ -404,9 +404,6 @@ inline RegisterFile& Heap::registerFile()
 void Heap::getConservativeRegisterRoots(HashSet<JSCell*>& roots)
 {
     ASSERT(isValidThreadState(m_globalData));
-    if (m_operationInProgress != NoOperation)
-        CRASH();
-    m_operationInProgress = Collection;
     ConservativeRoots registerFileRoots(&m_objectSpace.blocks(), &m_storageSpace);
     registerFile().gatherConservativeRoots(registerFileRoots);
     size_t registerFileRootCount = registerFileRoots.size();
@@ -415,7 +412,6 @@ void Heap::getConservativeRegisterRoots(HashSet<JSCell*>& roots)
         setMarked(registerRoots[i]);
         roots.add(registerRoots[i]);
     }
-    m_operationInProgress = NoOperation;
 }
 
 void Heap::markRoots(bool fullGC)
@@ -425,9 +421,6 @@ void Heap::markRoots(bool fullGC)
     COND_GCPHASE(fullGC, MarkFullRoots, MarkYoungRoots);
     UNUSED_PARAM(fullGC);
     ASSERT(isValidThreadState(m_globalData));
-    if (m_operationInProgress != NoOperation)
-        CRASH();
-    m_operationInProgress = Collection;
 
     void* dummy;
     
@@ -583,7 +576,6 @@ void Heap::markRoots(bool fullGC)
     m_sharedData.reset();
     m_storageSpace.doneCopying();
 
-    m_operationInProgress = NoOperation;
 }
 
 size_t Heap::objectCount()
@@ -655,6 +647,9 @@ void Heap::collect(SweepToggle sweepToggle)
     ASSERT(globalData()->identifierTable == wtfThreadData().currentIdentifierTable());
     ASSERT(m_isSafeToCollect);
     JAVASCRIPTCORE_GC_BEGIN();
+    if (m_operationInProgress != NoOperation)
+        CRASH();
+    m_operationInProgress = Collection;
 
     m_activityCallback->willCollect();
 
@@ -729,6 +724,9 @@ void Heap::collect(SweepToggle sweepToggle)
     m_bytesAllocated = 0;
     double lastGCEndTime = WTF::currentTime();
     m_lastGCLength = lastGCEndTime - lastGCStartTime;
+    if (m_operationInProgress != Collection)
+        CRASH();
+    m_operationInProgress = NoOperation;
     JAVASCRIPTCORE_GC_END();
 }
 
