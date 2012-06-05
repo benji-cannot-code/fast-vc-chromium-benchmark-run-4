73c3c512d2c24baa588b9ae33c722628b0475444
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-05 03:49:11 +0000

    Gesture related changes for views:
    . Makes capture send both gesture and mouse events to the same view.
    . Makes gesture handling consistant with mouse handling. We only
      handle targetting a single view and reset the target on touch up.
    
    BUG=124277
    TEST=none
    R=ben@chromium.org,sadrul@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10479010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140487 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab_drag_controller.cc
+++ chrome/browser/ui/views/tabs/tab_drag_controller.cc
@@ -779,7 +779,7 @@ TabDragController::DragBrowserToNewTabStrip(
     target_tabstrip->OwnDragController(this);
     // Disable animations so that we don't see a close animation on aero.
     browser_widget->SetVisibilityChangedAnimationsEnabled(false);
-    browser_widget->ReleaseMouseCapture();
+    browser_widget->ReleaseCapture();
     // EndMoveLoop is going to snap the window back to its original location.
     // Hide it so users don't see this.
     browser_widget->Hide();
@@ -1119,7 +1119,7 @@ void TabDragController::Attach(TabStrip* attached_tabstrip,
   // window has mouse capture. This is important so that if activation changes
   // the drag isn't prematurely canceled.
   if (detach_into_browser_) {
-    attached_tabstrip_->GetWidget()->SetMouseCapture(attached_tabstrip_);
+    attached_tabstrip_->GetWidget()->SetCapture(attached_tabstrip_);
     attached_tabstrip_->OwnDragController(this);
   }
 
@@ -1137,7 +1137,7 @@ void TabDragController::Detach() {
   // reattach ownership is transfered.
   if (detach_into_browser_) {
     attached_tabstrip_->ReleaseDragController();
-    attached_tabstrip_->GetWidget()->ReleaseMouseCapture();
+    attached_tabstrip_->GetWidget()->ReleaseCapture();
   }
 
   mouse_move_direction_ = kMovedMouseLeft | kMovedMouseRight;
@@ -1254,7 +1254,7 @@ void TabDragController::RunMoveLoop() {
   // destroying the drag loop. Release mouse capture ourself before this while
   // the DragController isn't owned by the TabStrip.
   attached_tabstrip_->ReleaseDragController();
-  attached_tabstrip_->GetWidget()->ReleaseMouseCapture();
+  attached_tabstrip_->GetWidget()->ReleaseCapture();
   attached_tabstrip_->OwnDragController(this);
   views::Widget::MoveLoopResult result = move_loop_widget_->RunMoveLoop();
   content::NotificationService::current()->Notify(
@@ -1285,7 +1285,7 @@ void TabDragController::RunMoveLoop() {
       tab_strip_to_attach_to_after_exit_ = NULL;
     }
     DCHECK(attached_tabstrip_);
-    attached_tabstrip_->GetWidget()->SetMouseCapture(attached_tabstrip_);
+    attached_tabstrip_->GetWidget()->SetCapture(attached_tabstrip_);
   } else if (active_) {
     EndDrag(result == views::Widget::MOVE_LOOP_CANCELED);
   }
--- ui/views/view.cc
+++ ui/views/view.cc
@@ -830,8 +830,8 @@ ui::GestureStatus View::OnGestureEvent(const GestureEvent& event) {
   return ui::GESTURE_STATUS_UNKNOWN;
 }
 
-void View::SetMouseHandler(View *new_mouse_handler) {
-  // It is valid for new_mouse_handler to be NULL
+void View::SetMouseHandler(View* new_mouse_handler) {
+  // |new_mouse_handler| may be NULL.
   if (parent_)
     parent_->SetMouseHandler(new_mouse_handler);
 }
@@ -855,12 +855,6 @@ ui::TextInputClient* View::GetTextInputClient() {
 InputMethod* View::GetInputMethod() {
   Widget* widget = GetWidget();
   return widget ? widget->GetInputMethod() : NULL;
-}
-
-ui::GestureStatus View::ProcessGestureEvent(const GestureEvent& event) {
-  // TODO(Gajen): Implement a grab scheme similar to as as is found in
-  //              MousePressed.
-  return OnGestureEvent(event);
 }
 
 // Accelerators ----------------------------------------------------------------
