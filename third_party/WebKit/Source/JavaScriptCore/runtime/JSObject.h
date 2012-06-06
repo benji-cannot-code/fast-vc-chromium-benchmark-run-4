b5e07d2fba4e0af43f603425947e09f4ebc93117
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-06 07:37:05 +0000

    Reverted r119567, the fix for <http://webkit.org/b/88378>, because it broke the 32-bit build.
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::SpeculativeJIT::emitAllocateBasicJSObject):
    * jit/JITInlineMethods.h:
    (JSC::JIT::emitAllocateBasicJSObject):
    * llint/LowLevelInterpreter.asm:
    * runtime/JSGlobalData.h:
    (JSGlobalData):
    * runtime/JSGlobalThis.cpp:
    (JSC::JSGlobalThis::setUnwrappedObject):
    * runtime/JSObject.cpp:
    (JSC::JSObject::visitChildren):
    (JSC::JSObject::createInheritorID):
    * runtime/JSObject.h:
    (JSObject):
    (JSC::JSObject::resetInheritorID):
    (JSC):
    (JSC::JSObject::offsetOfInheritorID):
    (JSC::JSObject::inheritorID):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
@@ -237,6 +237,7 @@ namespace JSC {
         
         static size_t offsetOfInlineStorage();
         static size_t offsetOfPropertyStorage();
+        static size_t offsetOfInheritorID();
 
         static JS_EXPORTDATA const ClassInfo s_info;
 
@@ -264,9 +265,9 @@ namespace JSC {
         // To create derived types you likely want JSNonFinalObject, below.
         JSObject(JSGlobalData&, Structure*, PropertyStorage inlineStorage);
         
-        void resetInheritorID(JSGlobalData& globalData)
+        void resetInheritorID()
         {
-            removeDirect(globalData, globalData.m_inheritorIDKey);
+            m_inheritorID.clear();
         }
 
     private:
@@ -303,6 +304,7 @@ namespace JSC {
         Structure* createInheritorID(JSGlobalData&);
 
         StorageBarrier m_propertyStorage;
+        WriteBarrier<Structure> m_inheritorID;
     };
 
 
@@ -310,8 +312,8 @@ namespace JSC {
 #define JSNonFinalObject_inlineStorageCapacity 4
 #define JSFinalObject_inlineStorageCapacity 6
 #else
-#define JSNonFinalObject_inlineStorageCapacity 3
-#define JSFinalObject_inlineStorageCapacity 5
+#define JSNonFinalObject_inlineStorageCapacity 2
+#define JSFinalObject_inlineStorageCapacity 4
 #endif
 
 COMPILE_ASSERT((JSFinalObject_inlineStorageCapacity >= JSNonFinalObject_inlineStorageCapacity), final_storage_is_at_least_as_large_as_non_final);
@@ -416,6 +418,11 @@ inline size_t JSObject::offsetOfPropertyStorage()
     return OBJECT_OFFSETOF(JSObject, m_propertyStorage);
 }
 
+inline size_t JSObject::offsetOfInheritorID()
+{
+    return OBJECT_OFFSETOF(JSObject, m_inheritorID);
+}
+
 inline bool JSObject::isGlobalObject() const
 {
     return structure()->typeInfo().type() == GlobalObjectType;
@@ -508,10 +515,9 @@ inline void JSObject::setPrototype(JSGlobalData& globalData, JSValue prototype)
 
 inline Structure* JSObject::inheritorID(JSGlobalData& globalData)
 {
-    if (WriteBarrierBase<Unknown>* location = getDirectLocation(globalData, globalData.m_inheritorIDKey)) {
-        Structure* inheritorID = jsCast<Structure*>(location->get());
-        ASSERT(inheritorID->isEmpty());
-        return inheritorID;
+    if (m_inheritorID) {
+        ASSERT(m_inheritorID->isEmpty());
+        return m_inheritorID.get();
     }
     return createInheritorID(globalData);
 }
