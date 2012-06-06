99c562bd1e5cd96194c8f17b646fa431e645841a
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 23:16:19 +0000

    Rename DrawBitmapInt to DrawImageInt
    Bug=None
    Test=Compiles
    
    Review URL: https://chromiumcodereview.appspot.com/10512021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140877 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/canvas_win.cc
+++ ui/gfx/canvas_win.cc
@@ -456,7 +456,7 @@ void Canvas::DrawStringWithHalo(const string16& text,
   }
 
   // Draw the halo bitmap with blur.
-  DrawBitmapInt(text_bitmap, x - 1, y - 1);
+  DrawImageInt(text_bitmap, x - 1, y - 1);
 }
 
 void Canvas::DrawFadeTruncatingString(
