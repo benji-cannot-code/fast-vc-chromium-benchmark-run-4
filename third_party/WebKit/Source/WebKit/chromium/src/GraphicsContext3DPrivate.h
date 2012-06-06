59501b92d85df67085c96163ac1f0053772d99a7
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-06 23:21:49 +0000

    [chromium] Unreviewed build fix, add an apparently used include of OwnArrayPtr.h back to GraphicsContext3DPrivate.h https://bugs.webkit.org/show_bug.cgi?id=88468
    * src/GraphicsContext3DPrivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/GraphicsContext3DPrivate.h
+++ third_party/WebKit/Source/WebKit/chromium/src/GraphicsContext3DPrivate.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext3D.h"
 #include "SkBitmap.h"
 #include <wtf/HashSet.h>
+#include <wtf/OwnArrayPtr.h>
 #include <wtf/OwnPtr.h>
 
 class GrContext;
