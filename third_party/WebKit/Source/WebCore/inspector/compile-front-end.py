125ea0105117ba00f6245d150a8df350eded1fb4
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-06 11:37:02 +0000

    Web Inspector: [Compiler] Remove compile-front-end.sh and fix InjectedScriptSource errors. https://bugs.webkit.org/show_bug.cgi?id=87512
    Reviewed by Yury Semikhatsky.
    
    Removed compile-front-end.sh, compile-front-end.py should be used instead now.
    Made generate_protocol_externs a python module to call it from compile-front-end.py.
    Temporary copy of InjectedScriptSource.js is created and modified to
    avoid "This code lacks side effects" warning.
    
    * inspector/compile-front-end.py:
    * inspector/compile-front-end.sh: Removed.
    * inspector/generate-protocol-externs: Removed.
    * inspector/generate_protocol_externs.py: Added.
    (full_qualified_type_id):
    (param_type):
    (generate_protocol_externs):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119585 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/compile-front-end.py
+++ third_party/WebKit/Source/WebCore/inspector/compile-front-end.py
@@ -29,8 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import os
+import os.path
+import generate_protocol_externs
 
-inspector_frontend_path = "Source/WebCore/inspector/front-end"
+inspector_path = "Source/WebCore/inspector"
+inspector_frontend_path = inspector_path + "/front-end"
+
+generate_protocol_externs.generate_protocol_externs(inspector_frontend_path + "/protocol-externs.js", inspector_path + "/Inspector.json")
+
 externs = ["externs.js", "protocol-externs.js"]
 jsmodule_name_prefix = "jsmodule_"
 modules = [
@@ -331,7 +337,9 @@ modules = [
 # JavaScriptFormatter
 # ScriptFormatterWorker
 
-command = "java -jar ~/closure/compiler.jar --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --warning_level VERBOSE --language_in ECMASCRIPT5 --accept_const_keyword \\\n"
+compiler_command = "java -jar ~/closure/compiler.jar --summary_detail_level 3 --compilation_level SIMPLE_OPTIMIZATIONS --warning_level VERBOSE --language_in ECMASCRIPT5 --accept_const_keyword \\\n"
+
+command = compiler_command
 for extern in externs:
     command += "    --externs " + inspector_frontend_path + "/" + extern
     command += " \\\n"
@@ -351,5 +359,15 @@ for module in modules:
         command += "        --js " + inspector_frontend_path + "/" + script
         command += " \\\n"
 command += "\n"
-
 os.system(command)
+
+print "Compiling InjectedScriptSource.js..."
+os.system("echo \"var injectedScriptValue = \" > " + inspector_path + "/" + "InjectedScriptSourceTmp.js")
+os.system("cat  " + inspector_path + "/" + "InjectedScriptSource.js" + " >> " + inspector_path + "/" + "InjectedScriptSourceTmp.js")
+command = compiler_command
+command += "    --externs " + inspector_path + "/" + "InjectedScriptExterns.js" + " \\\n"
+command += "    --module " + jsmodule_name_prefix + "injected_script" + ":" + "1" + " \\\n"
+command += "        --js " + inspector_path + "/" + "InjectedScriptSourceTmp.js" + " \\\n"
+command += "\n"
+os.system(command)
+os.system("rm " + inspector_path + "/" + "InjectedScriptSourceTmp.js")
