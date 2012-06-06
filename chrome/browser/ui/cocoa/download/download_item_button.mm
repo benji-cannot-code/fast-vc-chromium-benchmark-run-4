85910384a2cc312fb2e0bce98142c67183c2665e
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 22:39:34 +0000

    Retain the controller in -[DownloadItemButton rightMouseDown:] before opening the menu that targets it.
    BUG=129826
    TEST=See comment #18 in bug.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10543033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140870 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/download/download_item_button.mm
+++ chrome/browser/ui/cocoa/download/download_item_button.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,6 +42,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                    withEvent:[NSApp currentEvent]
                      forView:self];
   }
+}
+
+// Override to retain the controller, in case a closure is pumped that deletes
+// the DownloadItemController while the menu is open <http://crbug.com/129826>.
+- (void)rightMouseDown:(NSEvent*)event {
+  scoped_nsobject<DownloadItemController> ref([controller_ retain]);
+  [super rightMouseDown:event];
 }
 
 - (void)menuDidClose:(NSMenu*)menu {
