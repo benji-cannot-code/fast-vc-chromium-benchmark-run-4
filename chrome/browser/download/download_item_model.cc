0a7b89f2d70617be64ab484afea079c54bdbae94
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 00:33:01 +0000

    Canceled becomes Cancelled (as spelled pretty much everywhere else).
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10535025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140672 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_item_model.cc
+++ chrome/browser/download/download_item_model.cc
@@ -127,7 +127,7 @@ string16 DownloadItemModel::GetStatusText() {
       }
       break;
     case DownloadItem::CANCELLED:
-      status_text = l10n_util::GetStringUTF16(IDS_DOWNLOAD_STATUS_CANCELED);
+      status_text = l10n_util::GetStringUTF16(IDS_DOWNLOAD_STATUS_CANCELLED);
       break;
     case DownloadItem::REMOVING:
       break;
@@ -299,7 +299,7 @@ string16 BaseDownloadItemModel::InterruptReasonStatusMessage(int reason) {
       string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS_SHUTDOWN;
       break;
     case content::DOWNLOAD_INTERRUPT_REASON_USER_CANCELED:
-      string_id = IDS_DOWNLOAD_STATUS_CANCELED;
+      string_id = IDS_DOWNLOAD_STATUS_CANCELLED;
       break;
     default:
       string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS;
@@ -358,7 +358,7 @@ string16 BaseDownloadItemModel::InterruptReasonMessage(int reason) {
       string_id = IDS_DOWNLOAD_INTERRUPTED_DESCRIPTION_SHUTDOWN;
       break;
     case content::DOWNLOAD_INTERRUPT_REASON_USER_CANCELED:
-      string_id = IDS_DOWNLOAD_STATUS_CANCELED;
+      string_id = IDS_DOWNLOAD_STATUS_CANCELLED;
       break;
     default:
       string_id = IDS_DOWNLOAD_INTERRUPTED_STATUS;
