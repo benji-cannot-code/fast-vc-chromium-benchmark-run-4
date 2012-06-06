da2e91f218938be0d2e13d6fdffd50db93a78c88
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 17:01:24 +0000

    Add CORS-awareness to HTML5 media elements.
    This CL is dead code until WebMediaPlayerClientImpl starts calling the new 2-arg
    load() method, but this needs to land (and roll into WebKit DEPS) before WMPCI
    can be updated to call this 2-arg load().
    
    BUG=123369
    
    
    Review URL: https://chromiumcodereview.appspot.com/10543007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140779 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_impl.cc
+++ webkit/media/webmediaplayer_impl.cc
@@ -83,6 +83,15 @@ const float kMaxRate = 16.0f;
 
 namespace webkit_media {
 
+#define COMPILE_ASSERT_MATCHING_ENUM(name)                              \
+  COMPILE_ASSERT(static_cast<int>(WebKit::WebMediaPlayer::CORSMode ## name) == \
+                 static_cast<int>(BufferedResourceLoader::k ## name),   \
+                 mismatching_enums)
+COMPILE_ASSERT_MATCHING_ENUM(Unspecified);
+COMPILE_ASSERT_MATCHING_ENUM(Anonymous);
+COMPILE_ASSERT_MATCHING_ENUM(UseCredentials);
+#undef COMPILE_ASSERT_MATCHING_ENUM
+
 WebMediaPlayerImpl::WebMediaPlayerImpl(
     WebKit::WebFrame* frame,
     WebKit::WebMediaPlayerClient* client,
@@ -202,6 +211,10 @@ URLSchemeForHistogram URLScheme(const GURL& url) {
 }  // anonymous namespace
 
 void WebMediaPlayerImpl::load(const WebKit::WebURL& url) {
+  load(url, CORSModeUnspecified);
+}
+
+void WebMediaPlayerImpl::load(const WebKit::WebURL& url, CORSMode cors_mode) {
   DCHECK_EQ(main_loop_, MessageLoop::current());
 
   GURL gurl(url);
@@ -238,9 +251,11 @@ void WebMediaPlayerImpl::load(const WebKit::WebURL& url) {
   // Otherwise it's a regular request which requires resolving the URL first.
   proxy_->set_data_source(
       new BufferedDataSource(main_loop_, frame_, media_log_));
-  proxy_->data_source()->Initialize(url, base::Bind(
-      &WebMediaPlayerImpl::DataSourceInitialized,
-      base::Unretained(this), gurl));
+  proxy_->data_source()->Initialize(
+      url, static_cast<BufferedResourceLoader::CORSMode>(cors_mode),
+      base::Bind(
+          &WebMediaPlayerImpl::DataSourceInitialized,
+          base::Unretained(this), gurl));
 
   is_local_source_ = !gurl.SchemeIs("http") && !gurl.SchemeIs("https");
 
@@ -368,6 +383,7 @@ void WebMediaPlayerImpl::setVisible(bool visible) {
 COMPILE_ASSERT_MATCHING_ENUM(PreloadNone, NONE);
 COMPILE_ASSERT_MATCHING_ENUM(PreloadMetaData, METADATA);
 COMPILE_ASSERT_MATCHING_ENUM(PreloadAuto, AUTO);
+#undef COMPILE_ASSERT_MATCHING_ENUM
 
 void WebMediaPlayerImpl::setPreload(WebMediaPlayer::Preload preload) {
   DCHECK_EQ(main_loop_, MessageLoop::current());
