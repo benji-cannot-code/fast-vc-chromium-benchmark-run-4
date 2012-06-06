c6b51972619be45f7e39cc5c673d8970d25c38f0
mcgrathr@chromium.org <mcgrathr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 16:47:44 +0000

    IWYU nit in ppapi_proxy
    Technically one is always supposed to do #include <sys/types.h> before
    doing #include <sys/mman.h>.  For the newlib <sys/mman.h> this
    actually matters, as otherwise off_t will be undefined when it's used
    in the mmap prototype.  Newer libstdc++ no longer implicitly includes
    <sys/types.h> in places that it used to, so this nit is newly noticed.
    
    BUG= none
    TEST= none
    
    R=sehr@google.com
    
    Review URL: https://chromiumcodereview.appspot.com/10540020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140777 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/shared/ppapi_proxy/command_buffer_nacl.cc
+++ ppapi/native_client/src/shared/ppapi_proxy/command_buffer_nacl.cc
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "native_client/src/shared/ppapi_proxy/command_buffer_nacl.h"
 
+#include <sys/types.h>
 #include <sys/mman.h>
 #include "gpu/command_buffer/common/logging.h"
 #include "native_client/src/shared/ppapi_proxy/plugin_globals.h"
