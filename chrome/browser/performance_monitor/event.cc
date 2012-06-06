161590c4a19ab1b70b5e801e1f88a68593d07177
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 01:09:30 +0000

    CPM Event Construction
    This provides code for cpm event construction, which will be used by other cpm
    classes.
    
    BUG=130212
    TEST=NONE
    
    
    Review URL: https://chromiumcodereview.appspot.com/10542013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140682 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/performance_monitor/event.cc
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/performance_monitor/event.h"
+
+namespace performance_monitor {
+
+Event::Event(const EventType& type,
+             const base::Time& time,
+             scoped_ptr<base::DictionaryValue> data)
+    : type_(type), time_(time), data_(data.release()) {
+}
+
+Event::~Event() {
+}
+
+scoped_ptr<Event> Event::FromValue(scoped_ptr<base::DictionaryValue> data) {
+  int type = 0;
+  if (!data->GetInteger(std::string("type"), &type))
+    return scoped_ptr<Event>();
+  double time = 0.0;
+  if (!data->GetDouble(std::string("time"), &time))
+    return scoped_ptr<Event>();
+  return scoped_ptr<Event>(new Event(static_cast<EventType>(type),
+                                     base::Time::FromInternalValue((int64)time),
+                                     data.Pass()));
+}
+
+}  // namespace performance_monitor
