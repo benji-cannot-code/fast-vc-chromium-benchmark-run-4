c74677b983483ad606dc4eab571a036c3321f2e4
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 17:06:19 +0000

    Treat all navigations from automation as being typed from the address bar.
    This allows special debug URLs to be used by automated tests.
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10533017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140782 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.cc
+++ chrome/browser/automation/testing_automation_provider.cc
@@ -586,7 +586,11 @@ void TestingAutomationProvider::NavigateToURLBlockUntilNavigationsComplete(
 
       // TODO(darin): avoid conversion to GURL.
       OpenURLParams params(
-          url, Referrer(), CURRENT_TAB, content::PAGE_TRANSITION_TYPED, false);
+          url, Referrer(), CURRENT_TAB,
+          content::PageTransitionFromInt(
+              content::PAGE_TRANSITION_TYPED |
+              content::PAGE_TRANSITION_FROM_ADDRESS_BAR),
+          false);
       browser->OpenURL(params);
       return;
     }
@@ -613,7 +617,11 @@ void TestingAutomationProvider::NavigationAsync(int handle,
       // Don't add any listener unless a callback mechanism is desired.
       // TODO(vibhor): Do this if such a requirement arises in future.
       OpenURLParams params(
-          url, Referrer(), CURRENT_TAB, content::PAGE_TRANSITION_TYPED, false);
+          url, Referrer(), CURRENT_TAB,
+          content::PageTransitionFromInt(
+              content::PAGE_TRANSITION_TYPED |
+              content::PAGE_TRANSITION_FROM_ADDRESS_BAR),
+          false);
       browser->OpenURL(params);
       *status = true;
     }
@@ -6058,9 +6066,13 @@ void TestingAutomationProvider::NavigateToURL(
   new NavigationNotificationObserver(
       &web_contents->GetController(), this, reply_message,
       navigation_count, false, true);
-  browser->OpenURLFromTab(web_contents, OpenURLParams(
+  OpenURLParams params(
       GURL(url), content::Referrer(), CURRENT_TAB,
-      content::PAGE_TRANSITION_TYPED, false));
+      content::PageTransitionFromInt(
+          content::PAGE_TRANSITION_TYPED |
+          content::PAGE_TRANSITION_FROM_ADDRESS_BAR),
+      false);
+  browser->OpenURLFromTab(web_contents, params);
 }
 
 void TestingAutomationProvider::WaitUntilNavigationCompletes(
