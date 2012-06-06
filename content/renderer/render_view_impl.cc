bb42b8cc42aa083a3ac5716aa88c83f7dbaf871d
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 18:31:21 +0000

    content: A couple of device-scale-factor fixes:
     * Implement WebWidgetClient::deviceScaleFactor
     * Set the device-scale-factor on WebView.
    
    BUG=127795
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10481005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140801 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2862,6 +2862,8 @@ void RenderViewImpl::ProcessViewLayoutFlags(const CommandLine& command_line) {
         webview()->setFixedLayoutSize(WebSize(width,height));
     }
   }
+  webview()->setDeviceScaleFactor(
+      webkit_preferences_.default_device_scale_factor);
 }
 
 void RenderViewImpl::didStartProvisionalLoad(WebFrame* frame) {
