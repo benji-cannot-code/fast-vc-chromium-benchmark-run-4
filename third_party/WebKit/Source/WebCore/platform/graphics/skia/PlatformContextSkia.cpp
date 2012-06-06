dc4274b15cc99832f2485fce6985ba51144b6f16
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-06 15:17:20 +0000

    [Chromium] imageSmoothingEnabled should apply to patterns https://bugs.webkit.org/show_bug.cgi?id=88103
    Patch by Keyar Hood <keyar@chromium.org> on 2012-06-06
    Reviewed by Stephen White.
    
    Source/WebCore:
    
    Test: fast/canvas/canvas-imageSmoothingEnabled-patterns.html
    
    When drawing patterns on a Canvas, the imageSmoothingEnabled flag is
    respected.
    * platform/graphics/skia/PlatformContextSkia.cpp:
    (WebCore::PlatformContextSkia::setupShader):
    
    LayoutTests:
    
    Added tests for imageSmoothingEnabled flag applying to patterns.
    
    Modified the TestExpectations file as tests are failing due to the new
    change. It makes the pictures prettier (and it agrees with other
    browsers) so we have decided that the new results are to be considered
    correct.
    * fast/canvas/canvas-imageSmoothingEnabled-patterns.html: Added.
    * platform/chromium-linux/fast/canvas/canvas-imageSmoothingEnabled-patterns-expected.png: Added.
    * platform/chromium-linux/fast/canvas/canvas-imageSmoothingEnabled-patterns-expected.txt: Added.
    * platform/chromium/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/PlatformContextSkia.cpp
@@ -373,6 +373,7 @@ void PlatformContextSkia::setupShader(SkPaint* paint, Gradient* grad, Pattern* p
     } else if (pat) {
         shader = pat->platformPattern(m_gc->getCTM());
         color = SK_ColorBLACK;
+        paint->setFilterBitmap(interpolationQuality() != InterpolationNone);
     }
 
     paint->setColor(m_state->applyAlpha(color));
