76be09255b419b8f039170ff9a4ab1a23dedb9f1
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 02:17:05 +0000

    Move the core state machine of SSLClientSocketNSS into a thread-safe Core
    NSS SSL functions may block on the underlying PKCS#11 modules or on
    user input. On ChromeOS, which has a hardware TPM, calls may take upwards
    of several seconds, preventing any IPC due to the I/O thread being
    blocked.
    
    To avoid blocking the I/O thread on ChromeOS, move the core SSL
    implementation to a dedicated worker thread, so that only SSL sockets
    are blocked.
    
    BUG=122355
    TEST=existing net_unittests + see bug.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10454066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140697 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/authenticator_test_base.cc
+++ remoting/protocol/authenticator_test_base.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
+#include "base/test/test_timeouts.h"
+#include "base/timer.h"
 #include "crypto/rsa_private_key.h"
 #include "net/base/cert_test_util.h"
 #include "remoting/protocol/authenticator.h"
@@ -22,15 +24,24 @@ using testing::SaveArg;
 namespace remoting {
 namespace protocol {
 
-AuthenticatorTestBase::MockChannelDoneCallback::MockChannelDoneCallback() {
-}
-AuthenticatorTestBase::MockChannelDoneCallback::~MockChannelDoneCallback() {
+namespace {
+
+ACTION_P(QuitThreadOnCounter, counter) {
+  --(*counter);
+  EXPECT_GE(*counter, 0);
+  if (*counter == 0)
+    MessageLoop::current()->Quit();
 }
 
-AuthenticatorTestBase::AuthenticatorTestBase() {
-}
-AuthenticatorTestBase::~AuthenticatorTestBase() {
-}
+}  // namespace
+
+AuthenticatorTestBase::MockChannelDoneCallback::MockChannelDoneCallback() {}
+
+AuthenticatorTestBase::MockChannelDoneCallback::~MockChannelDoneCallback() {}
+
+AuthenticatorTestBase::AuthenticatorTestBase() {}
+
+AuthenticatorTestBase::~AuthenticatorTestBase() {}
 
 void AuthenticatorTestBase::SetUp() {
   FilePath certs_dir(net::GetTestCertsDirectory());
@@ -100,22 +111,40 @@ void AuthenticatorTestBase::RunChannelAuth(bool expected_fail) {
   net::StreamSocket* client_socket = NULL;
   net::StreamSocket* host_socket = NULL;
 
+  // Expect two callbacks to be called - the client callback and the host
+  // callback.
+  int callback_counter = 2;
+
   EXPECT_CALL(client_callback_, OnDone(net::OK, _))
-      .WillOnce(SaveArg<1>(&client_socket));
+      .WillOnce(DoAll(SaveArg<1>(&client_socket),
+                      QuitThreadOnCounter(&callback_counter)));
   if (expected_fail) {
-    EXPECT_CALL(host_callback_, OnDone(net::ERR_FAILED, NULL));
+    EXPECT_CALL(host_callback_, OnDone(net::ERR_FAILED, NULL))
+         .WillOnce(QuitThreadOnCounter(&callback_counter));
   } else {
     EXPECT_CALL(host_callback_, OnDone(net::OK, _))
-        .WillOnce(SaveArg<1>(&host_socket));
+        .WillOnce(DoAll(SaveArg<1>(&host_socket),
+                        QuitThreadOnCounter(&callback_counter)));
   }
 
-  message_loop_.RunAllPending();
+  // Ensure that .Run() does not run unbounded if the callbacks are never
+  // called.
+  base::Timer shutdown_timer(false, false);
+  shutdown_timer.Start(FROM_HERE, TestTimeouts::action_timeout(),
+                       MessageLoop::QuitClosure());
+  message_loop_.Run();
+  shutdown_timer.Stop();
 
   testing::Mock::VerifyAndClearExpectations(&client_callback_);
   testing::Mock::VerifyAndClearExpectations(&host_callback_);
 
   client_socket_.reset(client_socket);
   host_socket_.reset(host_socket);
+
+  if (!expected_fail) {
+    ASSERT_TRUE(client_socket_.get() != NULL);
+    ASSERT_TRUE(host_socket_.get() != NULL);
+  }
 }
 
 void AuthenticatorTestBase::OnHostConnected(
