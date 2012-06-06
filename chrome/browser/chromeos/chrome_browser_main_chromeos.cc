1784c91001ef0c2db63eabde10546543b8fd7173
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-06 21:51:11 +0000

    chromeos: Notify power manager about user activity.
    This makes Chrome call powerd's HandleUserActivity D-Bus
    method in response to input events (but no more often than
    once every five seconds).
    
    BUG=chromium-os:31351
    TEST=manual: tailed powerd's log and saw that it's receiving HandleUserActivity calls every five seconds as i type or move the mouse
    
    
    Review URL: https://chromiumcodereview.appspot.com/10544011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -49,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/power/resume_observer.h"
 #include "chrome/browser/chromeos/power/screen_dimming_observer.h"
 #include "chrome/browser/chromeos/power/screen_lock_observer.h"
+#include "chrome/browser/chromeos/power/user_activity_notifier.h"
 #include "chrome/browser/chromeos/power/video_activity_notifier.h"
 #include "chrome/browser/chromeos/system/statistics_provider.h"
 #include "chrome/browser/chromeos/system_key_event_listener.h"
@@ -452,6 +453,7 @@ void ChromeBrowserMainPartsChromeos::PostBrowserStart() {
   // These are dependent on the ash::Shell singleton already having been
   // initialized.
   power_button_observer_.reset(new chromeos::PowerButtonObserver);
+  user_activity_notifier_.reset(new chromeos::UserActivityNotifier);
   video_activity_notifier_.reset(new chromeos::VideoActivityNotifier);
   screen_dimming_observer_.reset(new chromeos::ScreenDimmingObserver);
 
@@ -501,8 +503,9 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
 
   chromeos::WebSocketProxyController::Shutdown();
 
-  // Let VideoActivityNotifier unregister itself as an observer of the
-  // ash::Shell singleton before the shell is destroyed.
+  // Let classes unregister themselves as observers of the ash::Shell singleton
+  // before the shell is destroyed.
+  user_activity_notifier_.reset();
   video_activity_notifier_.reset();
 
   // Detach D-Bus clients before DBusThreadManager is shut down.
