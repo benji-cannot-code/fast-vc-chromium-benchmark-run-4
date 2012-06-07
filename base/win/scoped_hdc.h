a80f9709c3503cf31c073774103c6f09e7d0d2ed
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-07 21:42:23 +0000

    Make ScopedGetDC() CHECK fail if GetDC(NULL) returns NULL.
    Use scoping objects more in the omnibox code.
    
    BUG=110105
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10545045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141087 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/scoped_hdc.h
+++ base/win/scoped_hdc.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -22,8 +22,15 @@ class ScopedGetDC {
   explicit ScopedGetDC(HWND hwnd)
       : hwnd_(hwnd),
         hdc_(GetDC(hwnd)) {
-    DCHECK(!hwnd_ || IsWindow(hwnd_));
-    DCHECK(hdc_);
+    if (hwnd_) {
+      DCHECK(IsWindow(hwnd_));
+      DCHECK(hdc_);
+    } else {
+      // If GetDC(NULL) returns NULL, something really bad has happened, like
+      // GDI handle exhaustion.  In this case Chrome is going to behave badly no
+      // matter what, so we may as well just force a crash now.
+      CHECK(hdc_);
+    }
   }
 
   ~ScopedGetDC() {
