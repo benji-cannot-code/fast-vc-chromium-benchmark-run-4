f75d89ff57e4230836e96b6e3dc825f7799a9db6
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-07 00:18:43 +0000

    Map the certificate related SSL alerts to ERR_BAD_SSL_CLIENT_AUTH_CERT on the client side.
    Move MapNSSHandshakeError to ssl_client_socket_nss.cc (and rename it
    MapNSSClientHandshakeError) because it is specific to the client side.
    
    R=rsleevi@chromium.org
    BUG=129209
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10332300
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140897 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/nss_ssl_util.h
+++ net/socket/nss_ssl_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -28,9 +28,6 @@ PRErrorCode MapErrorToNSS(int result);
 
 // Map NSS error code to network error code.
 int MapNSSError(PRErrorCode err);
-
-// Map NSS error code from the first SSL handshake to network error code.
-int MapNSSHandshakeError(PRErrorCode err);
 
 }  // namespace net
 
