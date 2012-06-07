1b9605caa00b921209171f11591bdf66006fb054
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-07 22:37:50 +0000

    Unreviewed, rolling out r119744. http://trac.webkit.org/changeset/119744 https://bugs.webkit.org/show_bug.cgi?id=88584
    Fails assertions in debug builds (Requested by jamesr_ on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-06-07
    
    Source/WebCore:
    
    * platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp:
    (WebCore::CCIOSurfaceLayerImpl::~CCIOSurfaceLayerImpl):
    (WebCore::CCIOSurfaceLayerImpl::willDraw):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostImplTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext3D.h"
 #include "LayerRendererChromium.h"
 #include "cc/CCIOSurfaceDrawQuad.h"
-#include "cc/CCLayerTreeHostImpl.h"
 #include "cc/CCProxy.h"
 #include "cc/CCQuadCuller.h"
 
@@ -50,14 +49,9 @@ CCIOSurfaceLayerImpl::CCIOSurfaceLayerImpl(int id)
 
 CCIOSurfaceLayerImpl::~CCIOSurfaceLayerImpl()
 {
-    if (!m_ioSurfaceTextureId)
-        return;
-
-    CCGraphicsContext* context = layerTreeHostImpl()->context();
-    // FIXME: Implement this path for software compositing.
-    GraphicsContext3D* context3d = context->context3D();
-    if (context3d)
-        context3d->deleteTexture(m_ioSurfaceTextureId);
+    // FIXME: it seems there is no layer renderer / GraphicsContext3D available here. Ideally we
+    // would like to delete m_ioSurfaceTextureId.
+    m_ioSurfaceTextureId = 0;
 }
 
 void CCIOSurfaceLayerImpl::willDraw(CCRenderer* layerRenderer, CCGraphicsContext* context)
@@ -74,7 +68,6 @@ void CCIOSurfaceLayerImpl::willDraw(CCRenderer* layerRenderer, CCGraphicsContext
         ASSERT(extensions->supports("GL_CHROMIUM_iosurface"));
         ASSERT(extensions->supports("GL_ARB_texture_rectangle"));
 
-        // FIXME: Do this in a way that we can track memory usage.
         if (!m_ioSurfaceTextureId)
             m_ioSurfaceTextureId = context3d->createTexture();
 
