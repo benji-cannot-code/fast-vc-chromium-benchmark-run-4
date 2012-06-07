c61acdfa9170b8f0ddcd11ac10144f9b011bf204
scheib@chromium.org <scheib@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-07 22:40:19 +0000

    Add new Pointer Lock spec events webkitpointerlockchange and webkitpointerlockerror https://bugs.webkit.org/show_bug.cgi?id=88377
    Reviewed by Dimitri Glazkov.
    
    Part of a series of refactoring changes to update pointer lock API to
    the fullscreen locking style. https://bugs.webkit.org/show_bug.cgi?id=84402
    
    New change and error events added. Follow up patches will remove
    the previous callbacks and pointerlocklost event. Tests updated
    to include the new events, and a new test added specifically for
    the new events, pointerlockchange-pointerlockerror-events.html.
    
    Source/WebCore:
    
    Test: pointer-lock/pointerlockchange-pointerlockerror-events.html
    
    * dom/Document.h:
    (Document):
    * dom/Document.idl:
    * dom/EventNames.h:
    (WebCore):
    * page/PointerLockController.cpp:
    (WebCore::PointerLockController::requestPointerLock):
    (WebCore::PointerLockController::didAcquirePointerLock):
    (WebCore::PointerLockController::didNotAcquirePointerLock):
    (WebCore::PointerLockController::didLosePointerLock):
    (WebCore::PointerLockController::enqueueEvent):
    (WebCore):
    * page/PointerLockController.h:
    (PointerLockController):
    
    LayoutTests:
    
    * pointer-lock/lock-already-locked-expected.txt:
    * pointer-lock/lock-already-locked.html:
    * pointer-lock/pointer-lock-api-expected.txt:
    * pointer-lock/pointer-lock-api.html:
    * pointer-lock/pointerlockchange-pointerlockerror-events-expected.txt: Added.
    * pointer-lock/pointerlockchange-pointerlockerror-events.html: Added.
    * pointer-lock/pointerlocklost-event-expected.txt:
    * pointer-lock/pointerlocklost-event.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/EventNames.h
+++ third_party/WebKit/Source/WebCore/dom/EventNames.h
@@ -221,6 +221,8 @@ namespace WebCore {
     macro(show) \
     \
     macro(webkitpointerlocklost) \
+    macro(webkitpointerlockchange) \
+    macro(webkitpointerlockerror) \
     \
     macro(webkitRegionLayoutUpdate) \
     \
