e2709eef094aa44047790ac9308e81f73372b2c2
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-07 13:29:20 +0000

    Rename DrawBitmapInt to DrawImageInt
    Bug=None
    Test=Compiles
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=140877
    
    Review URL: https://chromiumcodereview.appspot.com/10512021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141002 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_item_view_views.cc
+++ ui/views/controls/menu/menu_item_view_views.cc
@@ -51,7 +51,7 @@ void MenuItemView::PaintButton(gfx::Canvas* canvas, PaintButtonMode mode) {
     // Don't use config.check_width here as it's padded to force more padding.
     gfx::Rect check_bounds(icon_x, icon_y, check->width(), icon_height);
     AdjustBoundsForRTLUI(&check_bounds);
-    canvas->DrawBitmapInt(*check, check_bounds.x(), check_bounds.y());
+    canvas->DrawImageInt(*check, check_bounds.x(), check_bounds.y());
   } else if (type_ == RADIO) {
     const gfx::ImageSkia* image =
         GetRadioButtonImage(GetDelegate()->IsItemChecked(GetCommand()));
@@ -62,7 +62,7 @@ void MenuItemView::PaintButton(gfx::Canvas* canvas, PaintButtonMode mode) {
                            image->width(),
                            image->height());
     AdjustBoundsForRTLUI(&radio_bounds);
-    canvas->DrawBitmapInt(*image, radio_bounds.x(), radio_bounds.y());
+    canvas->DrawImageInt(*image, radio_bounds.x(), radio_bounds.y());
   }
 
   // Render the foreground.
@@ -97,9 +97,9 @@ void MenuItemView::PaintButton(gfx::Canvas* canvas, PaintButtonMode mode) {
     if (!enabled()) {
       SkPaint paint;
       paint.setAlpha(120);
-      canvas->DrawBitmapInt(icon_, icon_bounds.x(), icon_bounds.y(), paint);
+      canvas->DrawImageInt(icon_, icon_bounds.x(), icon_bounds.y(), paint);
     } else {
-      canvas->DrawBitmapInt(icon_, icon_bounds.x(), icon_bounds.y());
+      canvas->DrawImageInt(icon_, icon_bounds.x(), icon_bounds.y());
     }
   }
 
@@ -111,8 +111,8 @@ void MenuItemView::PaintButton(gfx::Canvas* canvas, PaintButtonMode mode) {
                                          config.arrow_width) / 2,
                            config.arrow_width, height());
     AdjustBoundsForRTLUI(&arrow_bounds);
-    canvas->DrawBitmapInt(*GetSubmenuArrowImage(),
-                          arrow_bounds.x(), arrow_bounds.y());
+    canvas->DrawImageInt(*GetSubmenuArrowImage(),
+                         arrow_bounds.x(), arrow_bounds.y());
   }
 }
 
