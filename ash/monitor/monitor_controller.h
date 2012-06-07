e8ad22fbb80a5202e982951dc87358eee44237d9
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-07 03:48:41 +0000

    Allow taking screenshots for all of minitors at the same time.
    It contains a fix of screenshot filename too.  Because multiple
    screenshot taking happens in a very short time period, the filename
    could be same for all of the monitors.  Thus we need to rename them
    by specifying some suffix.  Here I just added numbers (2), (3), ...
    
    BUG=130718
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10514002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140939 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/monitor/monitor_controller.h
+++ ash/monitor/monitor_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma once
 
 #include <map>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
@@ -28,6 +29,9 @@ class MonitorController : public aura::MonitorObserver {
  public:
   MonitorController();
   virtual ~MonitorController();
+
+  // Gets all of the root windows.
+  void GetAllRootWindows(std::vector<aura::RootWindow*>* windows);
 
   // aura::MonitorObserver overrides:
   virtual void OnMonitorBoundsChanged(
