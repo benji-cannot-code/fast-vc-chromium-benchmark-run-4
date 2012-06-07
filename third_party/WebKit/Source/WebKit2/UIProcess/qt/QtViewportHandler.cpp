5fc0ad3d2f8c8f7f1ed167d10687fe9a79682bf0
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-07 09:38:45 +0000

    [Qt][WK2] setPageItemRectVisible() is not guarded by ViewportUpdateDeferrer at scaleAnimationValueChanged() https://bugs.webkit.org/show_bug.cgi?id=87954
    Reviewed by Kenneth Rohde Christiansen.
    
    Resetting the end value, the easing curve or the duration of the scale animation triggers
    a recalculation of the animation interval. This might change the current value of the
    animated property.
    Since guarding of setPageItemRectVisible() relies on animation state change we have to
    make sure we only act on animation value changes if the animation is in active state.
    
    * UIProcess/qt/QtViewportHandler.cpp:
    (WebKit::QtViewportHandler::QtViewportHandler):
    (WebKit::QtViewportHandler::animatePageItemRectVisible):
    (WebKit::QtViewportHandler::scaleAnimationValueChanged):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportHandler.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/QtViewportHandler.cpp
@@ -164,6 +164,9 @@ QtViewportHandler::QtViewportHandler(WebKit::WebPageProxy* proxy, QQuickWebView*
     , m_lastCommittedScale(-1)
     , m_zoomOutScale(0.0)
 {
+    m_scaleAnimation->setDuration(kScaleAnimationDurationMillis);
+    m_scaleAnimation->setEasingCurve(QEasingCurve::OutCubic);
+
     connect(m_viewportItem, SIGNAL(movementStarted()), SLOT(flickMoveStarted()), Qt::DirectConnection);
     connect(m_viewportItem, SIGNAL(movementEnded()), SLOT(flickMoveEnded()), Qt::DirectConnection);
 
@@ -293,9 +296,6 @@ void QtViewportHandler::animatePageItemRectVisible(const QRectF& itemRect)
     if (itemRect == currentPageItemRectVisible)
         return;
 
-    m_scaleAnimation->setDuration(kScaleAnimationDurationMillis);
-    m_scaleAnimation->setEasingCurve(QEasingCurve::OutCubic);
-
     m_scaleAnimation->setStartValue(currentPageItemRectVisible);
     m_scaleAnimation->setEndValue(itemRect);
 
@@ -367,6 +367,13 @@ void QtViewportHandler::scaleAnimationStateChanged(QAbstractAnimation::State new
 
 void QtViewportHandler::scaleAnimationValueChanged(QVariant value)
 {
+    // Resetting the end value, the easing curve or the duration of the scale animation
+    // triggers a recalculation of the animation interval. This might change the current
+    // value of the animated property.
+    // Make sure we only act on animation value changes if the animation is active.
+    if (!scaleAnimationActive())
+        return;
+
     setPageItemRectVisible(value.toRectF());
 }
 
