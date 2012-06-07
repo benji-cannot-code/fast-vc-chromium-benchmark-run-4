d6f37a195c683ca0a6f83964a738a39d9d6e04b5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-07 05:00:00 +0000

    [chromium] Stop dropping texture limits when the layer tree host becomes invisible, and initialize with 0 allocation. https://bugs.webkit.org/show_bug.cgi?id=87747
    Patch by Michal Mocny <mmocny@google.com> on 2012-06-06
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    GpuMemoryManager manages texture memory allocation limits. It will send a 0 allocation when a renderer becomes
    invisible, and an appropriate allocation after a renderer is initialized.  These numbers will change
    depending on the state of memory across devices and other renderers, so we would like to remove all explicit
    management by the renderers themselves.
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::initializeLayerRenderer):
    (WebCore::CCLayerTreeHost::setVisible):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostTest.cpp:
    (WTF::MockLayerTreeHost::create):
    * tests/Canvas2DLayerChromiumTest.cpp:
    (Canvas2DLayerChromiumTest::fullLifecycleTest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
@@ -228,6 +228,7 @@ public:
 
         // LayerTreeHostImpl won't draw if it has 1x1 viewport.
         layerTreeHost->setViewportSize(IntSize(1, 1));
+        layerTreeHost->setContentsMemoryAllocationLimitBytes(TextureManager::highLimitBytes(layerTreeHost->viewportSize()));
 
         layerTreeHost->rootLayer()->setLayerAnimationDelegate(testHooks);
 
