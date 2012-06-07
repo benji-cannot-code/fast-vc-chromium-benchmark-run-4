2b94fea45ceb4c5bf638a1f226695681b306f440
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-07 18:53:58 +0000

    Removed kEnableSyncSignin switch since it's obsolete.
    BUG=131077
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10532044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141052 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -658,13 +658,6 @@ const Experiment kExperiments[] = {
   },
 #endif
   {
-    "enable-sync-signin",
-    IDS_ENABLE_SYNC_SIGNIN_NAME,
-    IDS_ENABLE_SYNC_SIGNIN_DESCRIPTION,
-    kOsAll,
-    SINGLE_VALUE_TYPE(switches::kEnableSyncSignin)
-  },
-  {
     "enable-platform-apps",
     IDS_FLAGS_ENABLE_PLATFORM_APPS_NAME,
     IDS_FLAGS_ENABLE_PLATFORM_APPS_DESCRIPTION,
