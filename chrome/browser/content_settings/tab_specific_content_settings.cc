5abe2130b69407a5a09f1e74b6a6cecb4aaf8270
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-07 01:45:02 +0000

    Fix regression from r138369 where the IPC to tell content settings for the RenderView arrives before the RenderView is created.
    BUG=96137
    Review URL: https://chromiumcodereview.appspot.com/10539034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@140929 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/tab_specific_content_settings.cc
+++ chrome/browser/content_settings/tab_specific_content_settings.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_view_host.h"
+#include "content/public/browser/render_view_host_observer.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "webkit/fileapi/file_system_types.h"
@@ -43,6 +44,19 @@ namespace {
 typedef std::list<TabSpecificContentSettings*> TabSpecificList;
 static base::LazyInstance<TabSpecificList> g_tab_specific =
     LAZY_INSTANCE_INITIALIZER;
+
+class InterstitialHostObserver : public content::RenderViewHostObserver {
+ public:
+  explicit InterstitialHostObserver(RenderViewHost* rvh)
+      : content::RenderViewHostObserver(rvh) {}
+
+  // content::RenderViewHostObserver overrides.
+  virtual void RenderViewHostInitialized() OVERRIDE {
+    Send(new ChromeViewMsg_SetAsInterstitial(routing_id()));
+    delete this;
+  }
+};
+
 }
 
 TabSpecificContentSettings::SiteDataObserver::SiteDataObserver(
@@ -436,8 +450,9 @@ void TabSpecificContentSettings::ClearGeolocationContentSettings() {
 
 void TabSpecificContentSettings::RenderViewForInterstitialPageCreated(
     RenderViewHost* render_view_host) {
-  render_view_host->Send(new ChromeViewMsg_SetAsInterstitial(
-      render_view_host->GetRoutingID()));
+  // We want to tell the renderer-side code to ignore content settings for this
+  // page but we must wait until the RenderView is created.
+  new InterstitialHostObserver(render_view_host);
 }
 
 bool TabSpecificContentSettings::OnMessageReceived(
