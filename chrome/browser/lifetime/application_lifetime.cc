9e74768bc17812a475853ccca293cad705cd2e7d
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 19:34:43 +0000

    TabContentsWrapper -> TabContents, part 20.
    Lifetime.
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10537079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/lifetime/application_lifetime.cc
+++ chrome/browser/lifetime/application_lifetime.cc
@@ -18,7 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_window.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -197,7 +197,7 @@ void CloseAllBrowsers() {
       // DestroyBrowser to make sure the browser is deleted and cleanup can
       // happen.
       while (browser->tab_count())
-        delete browser->GetTabContentsWrapperAt(0);
+        delete browser->GetTabContentsAt(0);
       browser->window()->DestroyBrowser();
       i = BrowserList::begin();
       if (i != BrowserList::end() && browser == *i) {
