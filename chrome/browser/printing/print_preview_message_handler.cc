86e3a30829b0c3e9e7b02277a0078b7ae165be69
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 21:46:03 +0000

    TabContentsWrapper -> TabContents, part 24.
    Printing.
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10538059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_preview_message_handler.cc
+++ chrome/browser/printing/print_preview_message_handler.cc
@@ -85,7 +85,7 @@ TabContents* PrintPreviewMessageHandler::GetPrintPreviewTab() {
 }
 
 TabContents* PrintPreviewMessageHandler::tab_contents() {
-  return TabContents::GetCurrentWrapperForContents(web_contents());
+  return TabContents::FromWebContents(web_contents());
 }
 
 PrintPreviewUI* PrintPreviewMessageHandler::GetPrintPreviewUI() {
