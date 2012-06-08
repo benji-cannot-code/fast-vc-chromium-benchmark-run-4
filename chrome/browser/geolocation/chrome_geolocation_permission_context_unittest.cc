2c1e51da31b549eaa1cb376ea3c5f74f5b81a865
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 21:10:41 +0000

    TabContentsWrapper -> TabContents, part 14.
    Geolocation.
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10543079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141285 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/chrome_geolocation_permission_context_unittest.cc
+++ chrome/browser/geolocation/chrome_geolocation_permission_context_unittest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/infobars/infobar.h"
 #include "chrome/browser/infobars/infobar_tab_helper.h"
 #include "chrome/browser/tab_contents/confirm_infobar_delegate.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/browser/ui/tab_contents/test_tab_contents.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/test/base/testing_profile.h"
@@ -134,7 +134,7 @@ class GeolocationPermissionContextTests : public TabContentsTestHarness {
   scoped_refptr<ChromeGeolocationPermissionContext>
       geolocation_permission_context_;
   ClosedDelegateTracker closed_delegate_tracker_;
-  ScopedVector<TabContentsWrapper> extra_tabs_;
+  ScopedVector<TabContents> extra_tabs_;
 
  private:
   // TabContentsTestHarness:
@@ -212,7 +212,7 @@ void GeolocationPermissionContextTests::AddNewTab(const GURL& url) {
       url, content::Referrer(), content::PAGE_TRANSITION_TYPED, std::string());
   RenderViewHostTester::For(new_tab->GetRenderViewHost())->
       SendNavigate(extra_tabs_.size() + 1, url);
-  extra_tabs_.push_back(new TabContentsWrapper(new_tab));
+  extra_tabs_.push_back(new TabContents(new_tab));
 }
 
 void GeolocationPermissionContextTests::CheckTabContentsState(
