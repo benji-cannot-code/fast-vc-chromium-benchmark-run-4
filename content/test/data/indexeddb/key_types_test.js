7e762fea3ef37c361d681dddb907b995d85649e4
alecflett@chromium.org <alecflett@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 23:36:50 +0000

    Add name checks to browser exception tests (Similar to what we already added to the webkit tests)
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10533069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141322 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/data/indexeddb/key_types_test.js
+++ content/test/data/indexeddb/key_types_test.js
@@ -160,6 +160,7 @@ function testInvalidKeys() {
       } catch (e) {
         window.ex = e;
         shouldBe("ex.code", "IDBDatabaseException.DATA_ERR");
+        shouldBe("ex.name", "'DataError'");
       }
     });
   testKeyOrdering();
--- content/test/data/indexeddb/value_size_test.js
+++ content/test/data/indexeddb/value_size_test.js
@@ -117,8 +117,9 @@ function testOverLimit()
       fail('store.add - Expected exception, but none was raised');
     } catch (e) {
       debug('Exception (expected)');
-      code = e.code;
-      shouldBe("code", "IDBDatabaseException.DATA_ERR");
+      ex = e;
+      shouldBe("ex.code", "IDBDatabaseException.DATA_ERR");
+      shouldBe("ex.name", "'DataError'");
     }
 
     try {
@@ -128,8 +129,9 @@ function testOverLimit()
       fail('store.add - Expected exception, but none was raised');
     } catch (e) {
       debug('Exception (expected)');
-      code = e.code;
-      shouldBe("code", "IDBDatabaseException.DATA_ERR");
+      ex = e;
+      shouldBe("ex.code", "IDBDatabaseException.DATA_ERR");
+      shouldBe("ex.name", "'DataError'");
     }
 
     request = store.openCursor();
@@ -143,8 +145,9 @@ function testOverLimit()
         fail('cursor.update - Expected exception, but none was raised');
       } catch (e) {
         debug('Exception (expected)');
-        code = e.code;
-        shouldBe("code", "IDBDatabaseException.DATA_ERR");
+        ex = e;
+        shouldBe("ex.code", "IDBDatabaseException.DATA_ERR");
+        shouldBe("ex.name", "'DataError'");
 
         transaction.abort();
       }
