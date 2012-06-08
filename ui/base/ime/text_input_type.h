69e74fcbb2a693280beb738d03b00487b4d0e2d8
yosin@chromium.org <yosin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 04:48:00 +0000

    Add text input types newly introduced in WebKit
    BUG=128954
    TEST=No need. This patch doesn't change behavior.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10534037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141167 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/text_input_type.h
+++ ui/base/ime/text_input_type.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -27,6 +27,12 @@ enum TextInputType {
   TEXT_INPUT_TYPE_NUMBER,
   TEXT_INPUT_TYPE_TELEPHONE,
   TEXT_INPUT_TYPE_URL,
+  TEXT_INPUT_TYPE_DATE,
+  TEXT_INPUT_TYPE_DATE_TIME,
+  TEXT_INPUT_TYPE_DATE_TIME_LOCAL,
+  TEXT_INPUT_TYPE_MONTH,
+  TEXT_INPUT_TYPE_TIME,
+  TEXT_INPUT_TYPE_WEEK,
 };
 
 }  // namespace ui
