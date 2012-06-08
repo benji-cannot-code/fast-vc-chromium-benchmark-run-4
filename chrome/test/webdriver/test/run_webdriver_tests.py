0d09e66c12c8482f3fbb64c563dc4d186ef5bdbe
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 01:24:29 +0000

    Fix python chromedriver tests after DEPS update. BUG=none TBR=zori TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10544065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/webdriver/test/run_webdriver_tests.py
+++ chrome/test/webdriver/test/run_webdriver_tests.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -204,6 +204,7 @@ class Main(object):
     sys.modules['pytest'] = imp.new_module('pytest')
     sys.modules['pytest'].mark = imp.new_module('mark')
     sys.modules['pytest'].mark.ignore_chrome = lambda x: x
+    sys.modules['pytest'].mark.ignore_opera = lambda x: lambda y: None
 
   def _Run(self):
     """Run the tests."""
