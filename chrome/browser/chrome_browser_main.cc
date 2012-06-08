5184e14d9031c7b7a5f1acee70ea490158b8627c
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 21:01:27 +0000

    Revert "Add field trial for domain bound certificates."  The field trial is done.
    This reverts commit r133151.
    
    BUG=124105
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10544015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141284 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/platform_locale_settings.h"
 #include "net/base/net_module.h"
 #include "net/base/sdch_manager.h"
-#include "net/base/ssl_config_service.h"
 #include "net/cookies/cookie_monster.h"
 #include "net/http/http_basic_stream.h"
 #include "net/http/http_network_layer.h"
@@ -1014,25 +1013,6 @@ void ChromeBrowserMainParts::AutoLaunchChromeFieldTrial() {
   }
 }
 
-void ChromeBrowserMainParts::DomainBoundCertsFieldTrial() {
-  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
-  if (channel == chrome::VersionInfo::CHANNEL_CANARY) {
-    net::SSLConfigService::EnableDomainBoundCertsTrial();
-  } else if (channel == chrome::VersionInfo::CHANNEL_DEV &&
-             base::FieldTrialList::IsOneTimeRandomizationEnabled()) {
-    const base::FieldTrial::Probability kDivisor = 100;
-    // 10% probability of being in the enabled group.
-    const base::FieldTrial::Probability kEnableProbability = 10;
-    scoped_refptr<base::FieldTrial> trial =
-        base::FieldTrialList::FactoryGetFieldTrial(
-            "DomainBoundCerts", kDivisor, "disable", 2012, 5, 31, NULL);
-    trial->UseOneTimeRandomization();
-    int enable_group = trial->AppendGroup("enable", kEnableProbability);
-    if (trial->group() == enable_group)
-      net::SSLConfigService::EnableDomainBoundCertsTrial();
-  }
-}
-
 void ChromeBrowserMainParts::SetupUniformityFieldTrials() {
   // One field trial will be created for each entry in this array. The i'th
   // field trial will have |trial_sizes[i]| groups in it, including the default
@@ -1104,7 +1084,6 @@ void ChromeBrowserMainParts::SetupFieldTrials(bool metrics_recording_enabled,
   PredictorFieldTrial();
   DefaultAppsFieldTrial();
   AutoLaunchChromeFieldTrial();
-  DomainBoundCertsFieldTrial();
   gpu_util::InitializeForceCompositingModeFieldTrial();
   SetupUniformityFieldTrials();
   AutocompleteFieldTrial::Activate();
--- net/base/ssl_config_service.cc
+++ net/base/ssl_config_service.cc
@@ -75,7 +75,6 @@ SSLConfigService::SSLConfigService()
 }
 
 static bool g_cached_info_enabled = false;
-static bool g_domain_bound_certs_trial = false;
 
 // GlobalCRLSet holds a reference to the global CRLSet. It simply wraps a lock
 // around a scoped_refptr so that getting a reference doesn't race with
@@ -134,11 +133,6 @@ uint16 SSLConfigService::default_version_max() {
   return g_default_version_max;
 }
 
-// static
-void SSLConfigService::EnableDomainBoundCertsTrial() {
-  g_domain_bound_certs_trial = true;
-}
-
 void SSLConfigService::AddObserver(Observer* observer) {
   observer_list_.AddObserver(observer);
 }
@@ -153,8 +147,6 @@ SSLConfigService::~SSLConfigService() {
 // static
 void SSLConfigService::SetSSLConfigFlags(SSLConfig* ssl_config) {
   ssl_config->cached_info_enabled = g_cached_info_enabled;
-  if (g_domain_bound_certs_trial)
-    ssl_config->domain_bound_certs_enabled = true;
 }
 
 void SSLConfigService::ProcessConfigUpdate(const SSLConfig& orig_config,
