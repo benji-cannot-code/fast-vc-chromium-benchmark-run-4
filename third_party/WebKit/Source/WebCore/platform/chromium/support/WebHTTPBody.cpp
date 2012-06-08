e28e17bfd133985c27301a9f4ef1091e8114f2ed
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-08 11:50:03 +0000

    File::lastModifiedDate should use NaN or separate boolean flag for null Date value https://bugs.webkit.org/show_bug.cgi?id=87826
    Reviewed by Kent Tamura.
    
    Test: http/tests/local/fileapi/file-last-modified-after-delete.html
    
    * fileapi/File.cpp:
    (WebCore::File::File):
    (WebCore::File::captureSnapshot):
    (WebCore::File::lastModifiedDate):
    (WebCore::File::lastModifiedDateForBinding): Removed.
    * fileapi/File.h:
    (File):
    * fileapi/File.idl:
    * platform/FileMetadata.h:
    (WebCore::FileMetadata::FileMetadata):
    * platform/FileSystem.h:
    * platform/chromium/support/WebHTTPBody.cpp:
    (WebKit::WebHTTPBody::elementAt):
    * platform/network/BlobData.h:
    (WebCore::BlobDataItem::doNotCheckFileChange): Removed.
    * platform/network/BlobData.cpp:
    * platform/network/cf/FormDataStreamCFNet.cpp:
    (WebCore::advanceCurrentStream):
    * platform/network/soup/ResourceHandleSoup.cpp:
    (WebCore::blobIsOutOfDate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119821 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/support/WebHTTPBody.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/support/WebHTTPBody.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include <public/WebHTTPBody.h>
 
+#include "FileSystem.h"
 #include "FormData.h"
 
 using namespace WebCore;
@@ -78,7 +79,7 @@ bool WebHTTPBody::elementAt(size_t index, Element& result) const
     result.filePath.reset();
     result.fileStart = 0;
     result.fileLength = 0;
-    result.modificationTime = 0.0;
+    result.modificationTime = invalidFileTime();
     result.blobURL = KURL();
 
     switch (element.m_type) {
--- third_party/WebKit/Source/WebCore/platform/network/BlobData.h
+++ third_party/WebKit/Source/WebCore/platform/network/BlobData.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BlobData_h
 #define BlobData_h
 
+#include "FileSystem.h"
 #include "KURL.h"
 #include "PlatformString.h"
 #include <wtf/Forward.h>
@@ -60,14 +61,13 @@ private:
 
 struct BlobDataItem {
     static const long long toEndOfFile;
-    static const double doNotCheckFileChange;
 
     // Default constructor.
     BlobDataItem()
         : type(Data)
         , offset(0)
         , length(toEndOfFile)
-        , expectedModificationTime(doNotCheckFileChange)
+        , expectedModificationTime(invalidFileTime())
     {
     }
 
@@ -77,7 +77,7 @@ struct BlobDataItem {
         , data(data)
         , offset(0)
         , length(toEndOfFile)
-        , expectedModificationTime(doNotCheckFileChange)
+        , expectedModificationTime(invalidFileTime())
     {
     }
 
@@ -87,7 +87,7 @@ struct BlobDataItem {
         , path(path)
         , offset(0)
         , length(toEndOfFile)
-        , expectedModificationTime(doNotCheckFileChange)
+        , expectedModificationTime(invalidFileTime())
     {
     }
 
@@ -100,14 +100,14 @@ struct BlobDataItem {
         , expectedModificationTime(expectedModificationTime)
     {
     }
-    
+
     // Constructor for Blob type.
     BlobDataItem(const KURL& url, long long offset, long long length)
         : type(Blob)
         , url(url)
         , offset(offset)
         , length(length)
-        , expectedModificationTime(doNotCheckFileChange)
+        , expectedModificationTime(invalidFileTime())
     {
     }
 
@@ -115,7 +115,7 @@ struct BlobDataItem {
     void detachFromCurrentThread();
 
     enum { Data, File, Blob } type;
-    
+
     // For Data type.
     RefPtr<RawData> data;
 
@@ -138,7 +138,7 @@ private:
         , data(data)
         , offset(offset)
         , length(length)
-        , expectedModificationTime(doNotCheckFileChange)
+        , expectedModificationTime(invalidFileTime())
     {
     }
 };
