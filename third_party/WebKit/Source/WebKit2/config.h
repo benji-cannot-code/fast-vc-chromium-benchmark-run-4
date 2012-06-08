851d70a813cfdc8775ef03c67f7b18f1e05fd964
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-08 05:48:07 +0000

    [WebKit2] Explicitly include CoreFoundation.h in config.h https://bugs.webkit.org/show_bug.cgi?id=88619
    Reviewed by Dan Bernstein.
    
    WebKit2 relies (probably unintentionally) on CoreFoundation.h being
    included by CoreGraphics.h (via CGSession.h), but some versions of
    CoreGraphics headers don't include CoreFoundation.h. WebKit2 should
    include it explicitly in config.h.
    
    * config.h: Import CoreFoundation.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/config.h
+++ third_party/WebKit/Source/WebKit2/config.h
@@ -74,6 +74,7 @@ static const type& name() \
 #define ENABLE_MEMORY_SAMPLER 1
 #endif
 
+#import <CoreFoundation/CoreFoundation.h>
 #import <CoreGraphics/CoreGraphics.h>
 
 #include <WebCore/EmptyProtocolDefinitions.h>
