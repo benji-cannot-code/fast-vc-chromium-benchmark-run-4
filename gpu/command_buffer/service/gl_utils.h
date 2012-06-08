12d7dcbe517c73ab2ec8a2a46d0dac9d5b878243
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 00:04:00 +0000

    Plum through ANGLE_depth_texture
    TEST=unit tests
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10441087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141117 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gl_utils.h
+++ gpu/command_buffer/service/gl_utils.h
@@ -87,7 +87,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define GL_COMMANDS_ISSUED_CHROMIUM            0x84F2
 
 // GL_OES_texure_3D
-#define GL_SAMPLER_3D_OES                                       0x8B5F
+#define GL_SAMPLER_3D_OES                      0x8B5F
+
+// GL_OES_depth24
+#define GL_DEPTH_COMPONENT24_OES               0x81A6
+
+// GL_OES_depth32
+#define GL_DEPTH_COMPONENT32_OES               0x81A7
+
+// GL_OES_packed_depth_stencil
+#define GL_DEPTH24_STENCIL8_OES                0x88F0
 
 #define GL_GLEXT_PROTOTYPES 1
 
