f597912158fc59a7636686eb14394b88807edb05
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-08 16:37:48 +0000

    [GTK] Add API to get the library version to WebKit2 GTK+ https://bugs.webkit.org/show_bug.cgi?id=88426
    Reviewed by Martin Robinson.
    
    .:
    
    * configure.ac: Generate WebKitVersion.h from WebKitVersion.h.in
    file.
    
    Source/WebKit2:
    
    * GNUmakefile.am:
    * GNUmakefile.list.am:
    * UIProcess/API/gtk/WebKitVersion.cpp: Added.
    (webkit_get_major_version):
    (webkit_get_minor_version):
    (webkit_get_micro_version):
    * UIProcess/API/gtk/WebKitVersion.h.in: Added.
    * UIProcess/API/gtk/docs/webkit2gtk-docs.sgml: Add new symbols.
    * UIProcess/API/gtk/docs/webkit2gtk-sections.txt: Add new section
    for WebKitVersion.
    * UIProcess/API/gtk/tests/GNUmakefile.am:
    * UIProcess/API/gtk/tests/TestWebKitVersion.cpp: Added.
    (testWebKitVersion):
    (testWebKitCheckVersion):
    (beforeAll):
    (afterAll):
    * UIProcess/API/gtk/webkit2.h: Include WebKitVersion.h.
    
    Tools:
    
    * gtk/generate-gtkdoc:
    (get_webkit2_options): Add built sources to the list of files, but
    ignore enum types and marshallers.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/webkit2.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/webkit2.h
@@ -48,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <webkit2/WebKitURIRequest.h>
 #include <webkit2/WebKitURIResponse.h>
 #include <webkit2/WebKitURISchemeRequest.h>
+#include <webkit2/WebKitVersion.h>
 #include <webkit2/WebKitWebContext.h>
 #include <webkit2/WebKitWebInspector.h>
 #include <webkit2/WebKitWebResource.h>
