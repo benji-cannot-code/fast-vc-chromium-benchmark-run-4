a33594edec2653313101594fb21dc4231f38e1a9
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-08 20:06:46 +0000

    Coverity: Fix pass by value error in source_buffer_stream::DeleteAfter() function.
    BUG=none
    TEST=none
    CID=104188
    
    
    Review URL: https://chromiumcodereview.appspot.com/10546075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141268 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -113,7 +113,7 @@ class SourceBufferRange {
  private:
   // Helper method to delete buffers in |buffers_| starting from
   // |starting_point|, an iterator in |buffers_|.
-  void DeleteAfter(BufferQueue::iterator starting_point,
+  void DeleteAfter(const BufferQueue::iterator& starting_point,
                    BufferQueue* deleted_buffers,
                    BufferQueue::iterator* next_buffer);
 
@@ -609,7 +609,7 @@ void SourceBufferRange::DeleteAfter(
 }
 
 void SourceBufferRange::DeleteAfter(
-    BufferQueue::iterator starting_point,
+    const BufferQueue::iterator& starting_point,
     BufferQueue* removed_buffers,
     BufferQueue::iterator* next_buffer) {
   // Return if we're not deleting anything.
