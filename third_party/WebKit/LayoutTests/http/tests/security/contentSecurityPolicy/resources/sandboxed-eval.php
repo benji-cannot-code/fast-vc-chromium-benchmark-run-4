e229f1249cf534faf817f0a446e05bf7125ce2f1
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-09 19:35:02 +0000

    Can't use eval in iframes sanbdoxed via CSP header https://bugs.webkit.org/show_bug.cgi?id=88450
    Reviewed by Mihai Parparita.
    
    Source/WebCore:
    
    The initial empty document in a frame inherits the security context of
    its parent (including the CSP policy).  When we load the real document,
    in some cases we'll do a "secure transition" to the new document.  That
    means that we leave the global object in place in case the parent
    document has created any properties that it expects will be visible to
    the new document.
    
    If the parent document has a CSP policy that blocks eval, the "no eval"
    bit will be set on the global object of the initial document.  When we
    perform a "secure transition" to the new document, we'll keep the bit,
    which is wrong.  In this patch, we reset the bit by always enabling
    eval when clearing the context, regardless of whether we're performing
    a "secure transition".
    
    Test: http/tests/security/contentSecurityPolicy/iframe-inside-csp.html
    
    * bindings/js/ScriptController.cpp:
    (WebCore::ScriptController::enableEval):
    (WebCore):
    * bindings/js/ScriptController.h:
    (ScriptController):
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::enableEval):
    (WebCore):
    (WebCore::ScriptController::disableEval):
    * bindings/v8/ScriptController.h:
    (ScriptController):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::clear):
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/iframe-inside-csp-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/iframe-inside-csp.html: Added.
    * http/tests/security/contentSecurityPolicy/resources/sandboxed-eval.php: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/sandboxed-eval.php
@@ -0,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+header("X-WebKit-CSP: sandbox allow-scripts");
+?>
+<script>
+alert('PASS (1/2): Script can execute');
+</script>
+<script>
+eval("alert('PASS (2/2): Eval works')");
+</script>
+Done.
--- third_party/WebKit/Source/WebCore/bindings/js/ScriptController.h
+++ third_party/WebKit/Source/WebCore/bindings/js/ScriptController.h
@@ -105,6 +105,7 @@ public:
 
     WTF::TextPosition eventHandlerPosition() const;
 
+    void enableEval();
     void disableEval();
 
     static bool processingUserGesture();
