d29b11f73926a388b02c72842ce03641fe8d6290
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-10 21:41:00 +0000

    browser: Move login and speech files into cocoa/ directory.
    Now that the views version and the gtk version of these files were moved in
    r138041 and r140216, respectively, makes sense to also move the cocoa version.
    
    R=rsesek@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10483016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/login_prompt_cocoa.mm
+++ chrome/browser/ui/cocoa/login_prompt_cocoa.mm
@@ -3,8 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/login/login_prompt.h"
-#import "chrome/browser/ui/login/login_prompt_mac.h"
+#import "chrome/browser/ui/cocoa/login_prompt_cocoa.h"
 
 #include "base/mac/bundle_locations.h"
 #include "base/mac/mac_util.h"
@@ -16,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/cocoa/constrained_window_mac.h"
 #include "chrome/browser/ui/login/login_model.h"
+#include "chrome/browser/ui/login/login_prompt.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_contents.h"
