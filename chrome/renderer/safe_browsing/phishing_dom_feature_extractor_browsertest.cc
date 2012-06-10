08c8c0f8d2ec79691dc969bf312c69ca4bae2e50
nsylvain@chromium.org <nsylvain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-10 16:10:26 +0000

    Disable failing phishing tests.
    BUG=131999
    TBR=bryner
    Review URL: https://chromiumcodereview.appspot.com/10535100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141376 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/safe_browsing/phishing_dom_feature_extractor_browsertest.cc
+++ chrome/renderer/safe_browsing/phishing_dom_feature_extractor_browsertest.cc
@@ -93,7 +93,8 @@ class PhishingDOMFeatureExtractorTest
   base::WeakPtrFactory<PhishingDOMFeatureExtractorTest> weak_factory_;
 };
 
-TEST_F(PhishingDOMFeatureExtractorTest, FormFeatures) {
+// This test is disabled. See bug 131999
+TEST_F(PhishingDOMFeatureExtractorTest, DISABLED_FormFeatures) {
   // This test doesn't exercise the extraction timing.
   EXPECT_CALL(clock_, Now()).WillRepeatedly(Return(base::TimeTicks::Now()));
   responses_["http://host.com/"] =
@@ -193,7 +194,8 @@ TEST_F(PhishingDOMFeatureExtractorTest, LinkFeatures) {
   EXPECT_THAT(features.features(), ContainerEq(expected_features.features()));
 }
 
-TEST_F(PhishingDOMFeatureExtractorTest, ScriptAndImageFeatures) {
+// This test is disabled. See bug 131999
+TEST_F(PhishingDOMFeatureExtractorTest, DISABLED_ScriptAndImageFeatures) {
   // This test doesn't exercise the extraction timing.
   EXPECT_CALL(clock_, Now()).WillRepeatedly(Return(base::TimeTicks::Now()));
   responses_["http://host.com/"] =
@@ -224,7 +226,8 @@ TEST_F(PhishingDOMFeatureExtractorTest, ScriptAndImageFeatures) {
   EXPECT_THAT(features.features(), ContainerEq(expected_features.features()));
 }
 
-TEST_F(PhishingDOMFeatureExtractorTest, SubFrames) {
+// This test is disabled. See bug 131999
+TEST_F(PhishingDOMFeatureExtractorTest, DISABLED_SubFrames) {
   // This test doesn't exercise the extraction timing.
   EXPECT_CALL(clock_, Now()).WillRepeatedly(Return(base::TimeTicks::Now()));
 
