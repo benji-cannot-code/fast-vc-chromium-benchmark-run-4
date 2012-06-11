64855d978b0cff02117a4470cb8549a9a3d47119
smckay@chromium.org <smckay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 19:27:51 +0000

    Add "scheme" column to web_intents and web_intents_defaults tables. Includes support for recovering from at least some bad schema states. Added test to verify the new recovery behavior in addition to standard migration coverage.
    BUG=129199, 129200
    TEST=WebDatabaseMigrationTest.MigrateVersion45ToCurrent, WebDatabaseMigrationTest.MigrateVersion45InvalidToCurrent
    
    
    Review URL: https://chromiumcodereview.appspot.com/10543059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/web_database.cc
+++ chrome/browser/webdata/web_database.cc
@@ -22,11 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // corresponding changes must happen in the unit tests, and new migration test
 // added.  See |WebDatabaseMigrationTest::kCurrentTestedVersionNumber|.
 // static
-const int WebDatabase::kCurrentVersionNumber = 45;
+const int WebDatabase::kCurrentVersionNumber = 46;
 
 namespace {
 
-const int kCompatibleVersionNumber = 45;
+const int kCompatibleVersionNumber = 46;
 
 // Change the version number and possibly the compatibility version of
 // |meta_table_|.
@@ -328,6 +328,13 @@ sql::InitStatus WebDatabase::MigrateOldVersionsAsNeeded() {
         return FailedMigrationTo(45);
 
       ChangeVersion(&meta_table_, 45, true);
+      // FALL THROUGH
+
+    case 45:
+      if (!web_intents_table_->MigrateToVersion46AddSchemeColumn())
+        return FailedMigrationTo(46);
+
+      ChangeVersion(&meta_table_, 46, true);
       // FALL THROUGH
 
     // Add successive versions here.  Each should set the version number and
