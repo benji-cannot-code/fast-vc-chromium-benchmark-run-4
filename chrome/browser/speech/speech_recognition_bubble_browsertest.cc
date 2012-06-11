9700a1b4dd3088b2184b6728f96dcc2466343559
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 15:46:52 +0000

    TabContentsWrapper -> TabContents, part 27.
    Speech.
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10537083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/speech/speech_recognition_bubble_browsertest.cc
+++ chrome/browser/speech/speech_recognition_bubble_browsertest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/speech/speech_recognition_bubble.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/rect.h"
@@ -25,14 +25,14 @@ class SpeechRecognitionBubbleTest : public SpeechRecognitionBubbleDelegate,
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, CreateAndDestroy) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
 }
 
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndDestroy) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
   bubble->Show();
 }
@@ -40,7 +40,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndDestroy) {
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndHide) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
   bubble->Show();
   bubble->Hide();
@@ -49,7 +49,7 @@ IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndHide) {
 IN_PROC_BROWSER_TEST_F(SpeechRecognitionBubbleTest, ShowAndHideTwice) {
   gfx::Rect element_rect(100, 100, 100, 100);
   scoped_ptr<SpeechRecognitionBubble> bubble(SpeechRecognitionBubble::Create(
-      browser()->GetSelectedWebContents(), this, element_rect));
+      browser()->GetActiveWebContents(), this, element_rect));
   EXPECT_TRUE(bubble.get());
   bubble->Show();
   bubble->Hide();
