b71d0b52431c93ce4eff65b7404037743abbbda7
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 19:35:20 +0000

    touch: Add touch/gesture support for the star-icon in the location bar.
    BUG=124277
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10545117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141464 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/star_view.h
+++ chrome/browser/ui/views/location_bar/star_view.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,6 +31,8 @@ class StarView : public views::ImageView {
                               string16* tooltip) const OVERRIDE;
   virtual bool OnMousePressed(const views::MouseEvent& event) OVERRIDE;
   virtual void OnMouseReleased(const views::MouseEvent& event) OVERRIDE;
+  virtual ui::GestureStatus OnGestureEvent(
+      const views::GestureEvent& event) OVERRIDE;
   virtual bool OnKeyPressed(const views::KeyEvent& event) OVERRIDE;
 
   // The CommandUpdater for the Browser object that owns the location bar.
