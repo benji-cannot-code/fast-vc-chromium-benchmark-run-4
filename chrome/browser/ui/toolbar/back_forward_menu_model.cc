f1b960f51c5ce636b6428815039408b913b252c8
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 21:19:29 +0000

    TabContentsWrapper -> TabContents, part 47.
    browser/ui/toolbar 
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10541100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/back_forward_menu_model.cc
+++ chrome/browser/ui/toolbar/back_forward_menu_model.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/favicon_status.h"
@@ -412,7 +412,7 @@ WebContents* BackForwardMenuModel::GetWebContents() const {
   // We use the test web contents if the unit test has specified it.
   return test_web_contents_ ?
       test_web_contents_ :
-      browser_->GetSelectedTabContentsWrapper()->web_contents();
+      browser_->GetActiveWebContents();
 }
 
 int BackForwardMenuModel::MenuIndexToNavEntryIndex(int index) const {
