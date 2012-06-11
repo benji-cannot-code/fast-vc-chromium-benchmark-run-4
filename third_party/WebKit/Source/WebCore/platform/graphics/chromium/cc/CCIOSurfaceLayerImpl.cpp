177c187de205ad1661b1678a33ac9a817c481492
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-11 16:49:22 +0000

    [chromium] Free texture from CCIOSurfaceLayerImpl when it is destroyed https://bugs.webkit.org/show_bug.cgi?id=88371
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Unit test: CCLayerTreeHostImplTest.layersFreeTextures
    
    * platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp:
    (WebCore::CCIOSurfaceLayerImpl::~CCIOSurfaceLayerImpl):
    (WebCore::CCIOSurfaceLayerImpl::willDraw):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostImplTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext3D.h"
 #include "LayerRendererChromium.h"
 #include "cc/CCIOSurfaceDrawQuad.h"
+#include "cc/CCLayerTreeHostImpl.h"
 #include "cc/CCProxy.h"
 #include "cc/CCQuadCuller.h"
 
@@ -49,9 +50,14 @@ CCIOSurfaceLayerImpl::CCIOSurfaceLayerImpl(int id)
 
 CCIOSurfaceLayerImpl::~CCIOSurfaceLayerImpl()
 {
-    // FIXME: it seems there is no layer renderer / GraphicsContext3D available here. Ideally we
-    // would like to delete m_ioSurfaceTextureId.
-    m_ioSurfaceTextureId = 0;
+    if (!m_ioSurfaceTextureId)
+        return;
+
+    CCGraphicsContext* context = layerTreeHostImpl()->context();
+    // FIXME: Implement this path for software compositing.
+    GraphicsContext3D* context3d = context->context3D();
+    if (context3d)
+        context3d->deleteTexture(m_ioSurfaceTextureId);
 }
 
 void CCIOSurfaceLayerImpl::willDraw(CCRenderer* layerRenderer, CCGraphicsContext* context)
@@ -68,6 +74,7 @@ void CCIOSurfaceLayerImpl::willDraw(CCRenderer* layerRenderer, CCGraphicsContext
         ASSERT(extensions->supports("GL_CHROMIUM_iosurface"));
         ASSERT(extensions->supports("GL_ARB_texture_rectangle"));
 
+        // FIXME: Do this in a way that we can track memory usage.
         if (!m_ioSurfaceTextureId)
             m_ioSurfaceTextureId = context3d->createTexture();
 
