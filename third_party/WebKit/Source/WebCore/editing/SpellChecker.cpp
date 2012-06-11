cd8decaebd0e3b2a10a7d1b868908a0eacc51bab
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-11 08:17:14 +0000

    Spellchecker crash in async scenario. https://bugs.webkit.org/show_bug.cgi?id=88617
    Reviewed by Tony Chang.
    
    SpellCheckRequest::invokeRequest() can be reached after the frame is detached.
    SpellCheckRequest should ignore such a request since TextCheckerClient is not available
    for detached Frames.
    
    No new tests. It's hard to reproduce this.
    
    * editing/SpellChecker.cpp:
    (WebCore::SpellChecker::invokeRequest):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/SpellChecker.cpp
+++ third_party/WebKit/Source/WebCore/editing/SpellChecker.cpp
@@ -179,6 +179,10 @@ void SpellChecker::requestCheckingFor(PassRefPtr<SpellCheckRequest> request)
 void SpellChecker::invokeRequest(PassRefPtr<SpellCheckRequest> request)
 {
     ASSERT(!m_processingRequest);
+    if (!client()) {
+        request->didCancel();
+        return;
+    }
 
     m_processingRequest = request;
     client()->requestCheckingOfString(m_processingRequest);
