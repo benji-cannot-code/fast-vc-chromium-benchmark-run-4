cc080ca9f965a73ecf03f7e69dbc6f8dad62b1ac
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 19:28:33 +0000

    TabContentsWrapper -> TabContents, part 21.
    Notifications.
    
    BUG=131026
    TEST=no change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10536071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141459 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notification_service.cc
+++ chrome/browser/notifications/desktop_notification_service.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/confirm_infobar_delegate.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/content_settings.h"
 #include "chrome/common/content_settings_pattern.h"
@@ -337,7 +337,7 @@ void DesktopNotificationService::RequestPermission(
   if (!tab) {
     Browser* browser = browser::FindLastActiveWithProfile(profile_);
     if (browser)
-      tab = browser->GetSelectedWebContents();
+      tab = browser->GetActiveWebContents();
   }
 
   if (!tab)
@@ -350,12 +350,12 @@ void DesktopNotificationService::RequestPermission(
   ContentSetting setting = GetContentSetting(origin);
   if (setting == CONTENT_SETTING_ASK) {
     // Show an info bar requesting permission.
-    TabContentsWrapper* wrapper =
-        TabContentsWrapper::GetCurrentWrapperForContents(tab);
-    InfoBarTabHelper* infobar_helper = wrapper->infobar_tab_helper();
+    TabContents* tab_contents = TabContents::FromWebContents(tab);
+    InfoBarTabHelper* infobar_helper = tab_contents->infobar_tab_helper();
     infobar_helper->AddInfoBar(new NotificationPermissionInfoBarDelegate(
         infobar_helper,
-        DesktopNotificationServiceFactory::GetForProfile(wrapper->profile()),
+        DesktopNotificationServiceFactory::GetForProfile(
+            tab_contents->profile()),
         origin,
         DisplayNameForOrigin(origin),
         process_id,
--- chrome/browser/notifications/notification_browsertest.cc
+++ chrome/browser/notifications/notification_browsertest.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/infobars/infobar_tab_helper.h"
 #include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/tab_contents/tab_contents_wrapper.h"
+#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/web_contents.h"
@@ -32,13 +32,13 @@ IN_PROC_BROWSER_TEST_F(NotificationsPermissionTest, TestUserGestureInfobar) {
   // That's considered a user gesture to webkit, and should produce an infobar.
   bool result;
   ASSERT_TRUE(ui_test_utils::ExecuteJavaScriptAndExtractBool(
-      browser()->GetSelectedWebContents()->GetRenderViewHost(),
+      browser()->GetActiveWebContents()->GetRenderViewHost(),
       L"",
       L"window.domAutomationController.send(request());",
       &result));
   EXPECT_TRUE(result);
 
-  EXPECT_EQ(1U, browser()->GetTabContentsWrapperAt(0)->infobar_tab_helper()->
+  EXPECT_EQ(1U, browser()->GetTabContentsAt(0)->infobar_tab_helper()->
             infobar_count());
 }
 
@@ -53,6 +53,6 @@ IN_PROC_BROWSER_TEST_F(NotificationsPermissionTest, TestNoUserGestureInfobar) {
       test_server()->GetURL(
           "files/notifications/notifications_request_inline.html"));
 
-  EXPECT_EQ(0U, browser()->GetTabContentsWrapperAt(0)->infobar_tab_helper()->
+  EXPECT_EQ(0U, browser()->GetTabContentsAt(0)->infobar_tab_helper()->
             infobar_count());
 }
