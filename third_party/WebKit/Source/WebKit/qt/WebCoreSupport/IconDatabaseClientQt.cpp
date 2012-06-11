66e0017967f0d0c622ab2ed17b1e838518937984
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-11 10:29:09 +0000

    [Qt][Win] Add missing includes to fix the build https://bugs.webkit.org/show_bug.cgi?id=88538
    Reviewed by Simon Hausmann.
    
    * Api/qwebframe.cpp:
    * WebCoreSupport/ChromeClientQt.cpp:
    * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
    * WebCoreSupport/GeolocationClientQt.cpp:
    * WebCoreSupport/IconDatabaseClientQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/IconDatabaseClientQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/IconDatabaseClientQt.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IconDatabaseClientQt.h"
 
 #include "FrameLoaderClientQt.h"
+#include "HTMLFormElement.h"
 #include "IconDatabaseBase.h"
 #include <wtf/text/CString.h>
 
