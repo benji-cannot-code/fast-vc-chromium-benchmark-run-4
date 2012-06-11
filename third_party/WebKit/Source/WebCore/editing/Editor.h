badd50bc5fd568775f63a9bbb13720e3f640aa59
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-11 20:52:14 +0000

    Remove support for disconnected/excluded from search frames, they are not used by Safari anymore https://bugs.webkit.org/show_bug.cgi?id=88723
    Reviewed by Dan Bernstein.
    
    Source/WebCore: 
    
    * WebCore.exp.in:
    Update export.
    * editing/Editor.cpp:
    (WebCore::Editor::rangeOfString):
    (WebCore::Editor::countMatchesForText):
    * editing/Editor.h:
    (Editor):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::findFrameForNavigation):
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::parent):
    (WebCore::DOMWindow::top):
    * page/EventHandler.cpp:
    (WebCore::EventHandler::hitTestResultAtPoint):
    * page/Frame.cpp:
    (WebCore::Frame::Frame):
    * page/Frame.h:
    (Frame):
    * page/FrameTree.cpp:
    (WebCore::FrameTree::parent):
    (WebCore::FrameTree::top):
    * page/FrameTree.h:
    (FrameTree):
    * page/Location.cpp:
    (WebCore::Location::ancestorOrigins):
    * xml/XMLTreeViewer.cpp:
    (WebCore::XMLTreeViewer::hasNoStyleInformation):
    Update for the removal of disconnected frames and text search exclusions concepts.
    
    Source/WebKit/blackberry: 
    
    * WebKitSupport/InPageSearchManager.cpp:
    (BlackBerry::WebKit::InPageSearchManager::scopeStringMatches):
    
    Source/WebKit/chromium: 
    
    * src/WebFrameImpl.cpp:
    (WebKit::WebFrameImpl::scopeStringMatches):
    
    Source/WebKit/mac: 
    
    * WebView/WebFrame.mm:
    * WebView/WebFramePrivate.h:
    Remove -[WebFrame _setIsDisconnected:] and -[WebFrame _setExcludeFromTextSearch:]
    
    Source/WebKit/win: 
    
    * WebFrame.cpp:
    (WebFrame::setIsDisconnected):
    (WebFrame::setExcludeFromTextSearch):
    Stop doing anything in these functions.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.h
+++ third_party/WebKit/Source/WebCore/editing/Editor.h
@@ -323,10 +323,6 @@ public:
 
     PassRefPtr<Range> selectedRange();
     
-    // We should make these functions private when their callers in Frame are moved over here to Editor
-    bool insideVisibleArea(const LayoutPoint&) const;
-    bool insideVisibleArea(Range*) const;
-
     void addToKillRing(Range*, bool prepend);
 
     void startAlternativeTextUITimer();
@@ -433,10 +429,6 @@ private:
     void selectComposition();
     enum SetCompositionMode { ConfirmComposition, CancelComposition };
     void setComposition(const String&, SetCompositionMode);
-
-    PassRefPtr<Range> firstVisibleRange(const String&, FindOptions);
-    PassRefPtr<Range> lastVisibleRange(const String&, FindOptions);
-    PassRefPtr<Range> nextVisibleRange(Range*, const String&, FindOptions);
 
     void changeSelectionAfterCommand(const VisibleSelection& newSelection, bool closeTyping, bool clearTypingStyle);
 
--- third_party/WebKit/Source/WebCore/page/Frame.h
+++ third_party/WebKit/Source/WebCore/page/Frame.h
@@ -120,10 +120,6 @@ namespace WebCore {
 
     // ======== All public functions below this point are candidates to move out of Frame into another class. ========
 
-        bool isDisconnected() const;
-        void setIsDisconnected(bool);
-        bool excludeFromTextSearch() const;
-        void setExcludeFromTextSearch(bool);
         bool inScope(TreeScope*) const;
 
         void injectUserScripts(UserScriptInjectionTime);
@@ -238,8 +234,6 @@ namespace WebCore {
 #endif
 
         bool m_inViewSourceMode;
-        bool m_isDisconnected;
-        bool m_excludeFromTextSearch;
 
 #if USE(TILED_BACKING_STORE)
     // FIXME: The tiled backing store belongs in FrameView, not Frame.
@@ -311,26 +305,6 @@ namespace WebCore {
     inline HTMLFrameOwnerElement* Frame::ownerElement() const
     {
         return m_ownerElement;
-    }
-
-    inline bool Frame::isDisconnected() const
-    {
-        return m_isDisconnected;
-    }
-
-    inline void Frame::setIsDisconnected(bool isDisconnected)
-    {
-        m_isDisconnected = isDisconnected;
-    }
-
-    inline bool Frame::excludeFromTextSearch() const
-    {
-        return m_excludeFromTextSearch;
-    }
-
-    inline void Frame::setExcludeFromTextSearch(bool exclude)
-    {
-        m_excludeFromTextSearch = exclude;
     }
 
     inline bool Frame::inViewSourceMode() const
