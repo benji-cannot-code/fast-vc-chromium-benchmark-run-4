0686329250f574fcea715212dafbbeba8fb428da
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 15:33:06 +0000

    Fix subscript out of range error when rolling back an empty value.
    BUG=132037
    TEST=installer_util_unittests.exe --gtest_filter=DeleteRegValueWorkItemTest.DeleteExistingValue
    
    Review URL: https://chromiumcodereview.appspot.com/10535104
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141412 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/delete_reg_value_work_item.cc
+++ chrome/installer/util/delete_reg_value_work_item.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -88,9 +88,11 @@ void DeleteRegValueWorkItem::Rollback() {
                          KEY_READ | KEY_WRITE);
   if (result == ERROR_SUCCESS) {
     // try to restore the previous value
-    result = key.WriteValue(value_name_.c_str(), &previous_value_[0],
-                            static_cast<DWORD>(previous_value_.size()),
-                            previous_type_);
+    DWORD previous_size = static_cast<DWORD>(previous_value_.size());
+    const char* previous_value =
+        previous_size ? &previous_value_[0] : NULL;
+    result = key.WriteValue(value_name_.c_str(), previous_value,
+                            previous_size, previous_type_);
     VLOG_IF(1, result != ERROR_SUCCESS) << "rollback: restoring "
                                         << value_name_ << " error: " << result;
   } else {
