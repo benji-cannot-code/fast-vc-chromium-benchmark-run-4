6f756e7315cb03232d87a717f381d32d3eaa6da0
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 23:36:54 +0000

    Remove gtk_util.h from jankometer.cc
    R=erg@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10539083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141560 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/jankometer.cc
+++ chrome/browser/jankometer.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <limits>
-
 #include "chrome/browser/jankometer.h"
+
+#include <limits>
 
 #include "base/basictypes.h"
 #include "base/bind.h"
@@ -22,10 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
-
-#if defined(TOOLKIT_GTK)
-#include "chrome/browser/ui/gtk/gtk_util.h"
-#endif
 
 using base::TimeDelta;
 using base::TimeTicks;
