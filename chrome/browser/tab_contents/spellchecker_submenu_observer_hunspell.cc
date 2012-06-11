46a3762bab5859896ce29659794747777dc9eceb
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-11 06:36:29 +0000

    Fix a contextual typo and a confusing resource ID used by the spellchecker menu. This change fixes a contexual typo 'Spell-checker Options' with 'Spell-checker options' on Windows and Linux. Also it replaces IDC_CHECK_SPELLING_OF_THIS_FIELD with IDC_CHECK_SPELLING_WHILE_TYPING for compatibility with Safari. (This option enables or disables spell-checking globally as Safari does, not enables or disables spell-checking in the selected text field.)
    BUG=131724,131712
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10544067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/spellchecker_submenu_observer_hunspell.cc
+++ chrome/browser/tab_contents/spellchecker_submenu_observer_hunspell.cc
@@ -65,11 +65,11 @@ void SpellCheckerSubMenuObserver::InitMenu(
       IDC_CONTENT_CONTEXT_LANGUAGE_SETTINGS,
       IDS_CONTENT_CONTEXT_LANGUAGE_SETTINGS);
 
-  // Add a 'Check the spelling of this field' item in the sub menu.
+  // Add a 'Check spelling while typing' item in the sub menu.
   submenu_model_.AddCheckItem(
-      IDC_CHECK_SPELLING_OF_THIS_FIELD,
+      IDC_CHECK_SPELLING_WHILE_TYPING,
       l10n_util::GetStringUTF16(
-          IDS_CONTENT_CONTEXT_CHECK_SPELLING_OF_THIS_FIELD));
+          IDS_CONTENT_CONTEXT_CHECK_SPELLING_WHILE_TYPING));
 
   // Add a check item "Ask Google for spelling suggestions" item. (This class
   // does not handle this item because the SpellingMenuObserver class handles it
@@ -96,7 +96,7 @@ bool SpellCheckerSubMenuObserver::IsCommandIdSupported(int command_id) {
       // is hard for this class to handle it.
       return false;
 
-    case IDC_CHECK_SPELLING_OF_THIS_FIELD:
+    case IDC_CHECK_SPELLING_WHILE_TYPING:
     case IDC_SPELLPANEL_TOGGLE:
     case IDC_SPELLCHECK_MENU:
       return true;
@@ -113,8 +113,8 @@ bool SpellCheckerSubMenuObserver::IsCommandIdChecked(int command_id) {
     return language_selected_ == command_id - IDC_SPELLCHECK_LANGUAGES_FIRST;
   }
 
-  // Check box for 'Check the Spelling of this field'.
-  if (command_id == IDC_CHECK_SPELLING_OF_THIS_FIELD) {
+  // Check box for 'Check Spelling while typing'.
+  if (command_id == IDC_CHECK_SPELLING_WHILE_TYPING) {
     Profile* profile = proxy_->GetProfile();
     if (!profile || !profile->GetPrefs()->GetBoolean(prefs::kEnableSpellCheck))
       return false;
@@ -138,7 +138,7 @@ bool SpellCheckerSubMenuObserver::IsCommandIdEnabled(int command_id) {
   }
 
   switch (command_id) {
-    case IDC_CHECK_SPELLING_OF_THIS_FIELD:
+    case IDC_CHECK_SPELLING_WHILE_TYPING:
       return pref->GetBoolean(prefs::kEnableSpellCheck);
 
     case IDC_SPELLPANEL_TOGGLE:
@@ -169,7 +169,7 @@ void SpellCheckerSubMenuObserver::ExecuteCommand(int command_id) {
 
   content::RenderViewHost* rvh = proxy_->GetRenderViewHost();
   switch (command_id) {
-    case IDC_CHECK_SPELLING_OF_THIS_FIELD:
+    case IDC_CHECK_SPELLING_WHILE_TYPING:
       rvh->Send(new SpellCheckMsg_ToggleSpellCheck(rvh->GetRoutingID()));
       break;
   }
