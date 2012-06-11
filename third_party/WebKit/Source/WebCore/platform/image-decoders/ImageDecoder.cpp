f03d06b5ca753d552cfa5f19b07e5bbb02eedde9
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-11 14:05:59 +0000

    Source/WebCore: [Qt] Add config tests for WEBP imagedecoder library, modify HAVE(decoderlibrary) to USE(...)$ https://bugs.webkit.org/show_bug.cgi?id=87841
    Reviewed by Simon Hausmann.
    
    No new tests were needed.
    
    * Target.pri:
    * WebCore.pri:
    * platform/image-decoders/ImageDecoder.cpp:
    (WebCore::ImageDecoder::create):
    
    Tools: [Qt] Add config tests for WEBP imagedecoder library, modify HAVE(decoderlibrary) to USE(...)
    https://bugs.webkit.org/show_bug.cgi?id=87841
    
    Reviewed by Simon Hausmann.
    
    * qmake/config.tests/libwebp/libwebp.cpp: Added.
    (main):
    * qmake/config.tests/libwebp/libwebp.pro: Added.
    * qmake/sync.profile:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@119977 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-decoders/ImageDecoder.cpp
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/ImageDecoder.cpp
@@ -110,7 +110,7 @@ ImageDecoder* ImageDecoder::create(const SharedBuffer& data, ImageSource::AlphaO
     if (matchesGIFSignature(contents))
         return new GIFImageDecoder(alphaOption, gammaAndColorProfileOption);
 
-#if !PLATFORM(QT) || (PLATFORM(QT) && HAVE(LIBPNG))
+#if !PLATFORM(QT) || (PLATFORM(QT) && USE(LIBPNG))
     if (matchesPNGSignature(contents))
         return new PNGImageDecoder(alphaOption, gammaAndColorProfileOption);
 
@@ -118,7 +118,7 @@ ImageDecoder* ImageDecoder::create(const SharedBuffer& data, ImageSource::AlphaO
         return new ICOImageDecoder(alphaOption, gammaAndColorProfileOption);
 #endif
 
-#if !PLATFORM(QT) || (PLATFORM(QT) && HAVE(LIBJPEG))
+#if !PLATFORM(QT) || (PLATFORM(QT) && USE(LIBJPEG))
     if (matchesJPEGSignature(contents))
         return new JPEGImageDecoder(alphaOption, gammaAndColorProfileOption);
 #endif
--- third_party/WebKit/Tools/qmake/sync.profile
+++ third_party/WebKit/Tools/qmake/sync.profile
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     gccdepends => {},
     libpng => {},
     libjpeg => {},
+    libwebp => {},
     libxml2 => {},
     libxslt => {},
 );
