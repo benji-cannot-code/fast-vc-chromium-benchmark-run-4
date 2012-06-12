6a50fb5dd597ffef2b4d51a28bb05c5ff685b0f5
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 19:52:34 +0000

    Added support for animated/nonanimated user image. There are no optimizations (for instance, image is still decoded to PNG at each request) and bug with replaying animation at each click on already active user pod, but CL is big enough, so we should start review.
    BUG=114083
    TEST=none
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=139779
    
    Review URL: https://chromiumcodereview.appspot.com/10454044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager_impl.h
+++ chrome/browser/chromeos/login/user_manager_impl.h
@@ -33,6 +33,7 @@ class ProfileSyncService;
 namespace chromeos {
 
 class RemoveUserDelegate;
+class UserImage;
 
 // Implementation of the UserManager.
 class UserManagerImpl : public UserManager,
@@ -76,7 +77,7 @@ class UserManagerImpl : public UserManager,
   virtual void SaveUserDefaultImageIndex(const std::string& username,
                                          int image_index) OVERRIDE;
   virtual void SaveUserImage(const std::string& username,
-                             const SkBitmap& image) OVERRIDE;
+                             const UserImage& user_image) OVERRIDE;
   virtual void SetLoggedInUserCustomWallpaperLayout(
       ash::WallpaperLayout layout) OVERRIDE;
   virtual void SaveUserImageFromFile(const std::string& username,
@@ -179,7 +180,7 @@ class UserManagerImpl : public UserManager,
   // If |image| is empty, sets a stub image for the user.
   void SetUserImage(const std::string& username,
                     int image_index,
-                    const SkBitmap& image);
+                    const UserImage& user_image);
 
   void GetUserWallpaperProperties(const std::string& username,
                                  User::WallpaperType* type,
@@ -192,7 +193,7 @@ class UserManagerImpl : public UserManager,
   // and sends LOGIN_USER_IMAGE_CHANGED notification.
   void SaveUserImageInternal(const std::string& username,
                              int image_index,
-                             const SkBitmap& image);
+                             const UserImage& user_image);
 
   // Saves wallpaper to file, post task to generate thumbnail and updates local
   // state preferences.
@@ -200,17 +201,17 @@ class UserManagerImpl : public UserManager,
                                  ash::WallpaperLayout layout,
                                  User::WallpaperType type,
                                  WallpaperDelegate* delegate,
-                                 const SkBitmap& image);
+                                 const UserImage& user_image);
 
   // Sets desktop background to custom wallpaper and loads wallpaper thumbnail
   // asynchronously.
   void OnCustomWallpaperLoaded(const std::string& email,
                                ash::WallpaperLayout layout,
-                               const SkBitmap& wallpaper);
+                               const UserImage& wallpaper);
 
   // Caches the loaded wallpaper for the given user.
   void OnCustomWallpaperThumbnailLoaded(const std::string& email,
-                                        const SkBitmap& wallpaper);
+                                        const UserImage& user_image);
 
   // Updates the custom wallpaper thumbnail in wallpaper picker UI.
   void OnThumbnailUpdated(WallpaperDelegate* delegate);
@@ -225,7 +226,7 @@ class UserManagerImpl : public UserManager,
   // notification. Runs on FILE thread. Posts task for saving image info to
   // Local State on UI thread.
   void SaveImageToFile(const std::string& username,
-                       const SkBitmap& image,
+                       const UserImage& user_image,
                        const FilePath& image_path,
                        int image_index);
 
@@ -252,7 +253,7 @@ class UserManagerImpl : public UserManager,
                                  User::WallpaperType type);
 
   // Saves |image| to the specified |image_path|. Runs on FILE thread.
-  bool SaveBitmapToFile(const SkBitmap& image,
+  bool SaveBitmapToFile(const UserImage& user_image,
                         const FilePath& image_path);
 
   // Initializes |downloaded_profile_image_| with the picture of the logged-in
