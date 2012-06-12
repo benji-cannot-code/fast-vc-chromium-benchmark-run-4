a4c4655050e2378998a3397c2e514431bc273466
james.wei@intel.com <james.wei@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 03:25:44 +0000

    replace string16 with StringPiece16 in ConvertUTF16ToJavaString()
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10532081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141607 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_string.cc
+++ base/android/jni_string.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Internal version that does not use a scoped local pointer.
-jstring ConvertUTF16ToJavaStringImpl(JNIEnv* env, const string16& str) {
+jstring ConvertUTF16ToJavaStringImpl(JNIEnv* env,
+                                     const base::StringPiece16& str) {
   jstring result = env->NewString(str.data(), str.length());
   base::android::CheckException(env);
   return result;
@@ -34,7 +35,8 @@ std::string ConvertJavaStringToUTF8(const JavaRef<jstring>& str) {
 }
 
 ScopedJavaLocalRef<jstring> ConvertUTF8ToJavaString(
-    JNIEnv* env, const base::StringPiece& str) {
+    JNIEnv* env,
+    const base::StringPiece& str) {
   // JNI's NewStringUTF expects "modified" UTF8 so instead create the string
   // via our own UTF16 conversion utility.
   // Further, Dalvik requires the string passed into NewStringUTF() to come from
@@ -61,9 +63,9 @@ string16 ConvertJavaStringToUTF16(const JavaRef<jstring>& str) {
   return ConvertJavaStringToUTF16(AttachCurrentThread(), str.obj());
 }
 
-// TODO(joth): change this to accept const StringPiece16&.
-ScopedJavaLocalRef<jstring> ConvertUTF16ToJavaString(JNIEnv* env,
-                                                     const string16& str) {
+ScopedJavaLocalRef<jstring> ConvertUTF16ToJavaString(
+    JNIEnv* env,
+    const base::StringPiece16& str) {
   return ScopedJavaLocalRef<jstring>(env,
                                      ConvertUTF16ToJavaStringImpl(env, str));
 }
