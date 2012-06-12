6fd4a2cce06593574594176e079b605c0fddebe7
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-12 18:24:27 +0000

    <rdar://problem/11593686> and https://bugs.webkit.org/show_bug.cgi?id=88683 Garbage collection of an <img> element can cause reentrant event dispatch.
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    The most straightforward solution is for ImageLoader to keep its Element alive
    with ref/deref any time the Image is actually loading.
    
    ImageLoader should always do this for all Elements, and if those Elements want/need
    different behavior for when they are detached then they need to manually stop their
    loads.
    
    Tests: http/tests/loading/embed-image-load-outlives-gc-without-crashing.html
           http/tests/loading/image-input-type-outlives-gc-without-crashing.html
           http/tests/loading/image-load-outlives-gc-without-crashing.html
           http/tests/loading/object-image-load-outlives-gc-without-crashing.html
           http/tests/loading/svg-image-load-outlives-gc-without-crashing.html
           http/tests/loading/video-poster-image-load-outlives-gc-without-crashing.html
    
    * loader/ImageLoader.cpp:
    (WebCore::ImageLoader::ImageLoader):
    (WebCore::ImageLoader::~ImageLoader):
    (WebCore::ImageLoader::setImage):
    (WebCore::ImageLoader::updateFromElement):
    (WebCore::ImageLoader::notifyFinished):
    (WebCore::ImageLoader::updatedHasPendingLoadEvent):
    (WebCore::ImageLoader::dispatchPendingBeforeLoadEvent):
    (WebCore::ImageLoader::dispatchPendingLoadEvent):
    * loader/ImageLoader.h:
    (ImageLoader):
    
    LayoutTests:
    
    New tests for all users of ImageLoader:
    * http/tests/loading/embed-image-load-outlives-gc-without-crashing-expected.txt: Added.
    * http/tests/loading/embed-image-load-outlives-gc-without-crashing.html: Added.
    * http/tests/loading/image-input-type-outlives-gc-without-crashing-expected.txt: Added.
    * http/tests/loading/image-input-type-outlives-gc-without-crashing.html: Added.
    * http/tests/loading/image-load-outlives-gc-without-crashing-expected.txt: Added.
    * http/tests/loading/image-load-outlives-gc-without-crashing.html: Added.
    * http/tests/loading/object-image-load-outlives-gc-without-crashing-expected.txt: Added.
    * http/tests/loading/object-image-load-outlives-gc-without-crashing.html: Added.
    * http/tests/loading/resources/slowimage.php: Added.
    * http/tests/loading/svg-image-load-outlives-gc-without-crashing-expected.txt: Added.
    * http/tests/loading/svg-image-load-outlives-gc-without-crashing.html: Added.
    * http/tests/loading/video-poster-image-load-outlives-gc-without-crashing-expected.txt: Added.
    * http/tests/loading/video-poster-image-load-outlives-gc-without-crashing.html: Added.
    
    Shared sub resource for all of those tests:
    * http/tests/loading/resources/slowimage.php: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/ImageLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/ImageLoader.cpp
@@ -90,6 +90,7 @@ ImageLoader::ImageLoader(Element* element)
     , m_hasPendingErrorEvent(false)
     , m_imageComplete(true)
     , m_loadManually(false)
+    , m_elementIsProtected(false)
 {
 }
 
@@ -109,6 +110,11 @@ ImageLoader::~ImageLoader()
     ASSERT(m_hasPendingErrorEvent || !errorEventSender().hasPendingEvents(this));
     if (m_hasPendingErrorEvent)
         errorEventSender().cancelEvent(this);
+
+    // If the ImageLoader is being destroyed but it is still protecting its image-loading Element,
+    // remove that protection here.
+    if (m_elementIsProtected)
+        m_element->deref();
 }
 
 void ImageLoader::setImage(CachedImage* newImage)
@@ -138,6 +144,10 @@ void ImageLoader::setImage(CachedImage* newImage)
 
     if (RenderImageResource* imageResource = renderImageResource())
         imageResource->resetAnimation();
+
+    // Only consider updating the protection ref-count of the Element immediately before returning
+    // from this function as doing so might result in the destruction of this ImageLoader.
+    updatedHasPendingLoadEvent();
 }
 
 void ImageLoader::updateFromElement()
@@ -219,6 +229,10 @@ void ImageLoader::updateFromElement()
 
     if (RenderImageResource* imageResource = renderImageResource())
         imageResource->resetAnimation();
+
+    // Only consider updating the protection ref-count of the Element immediately before returning
+    // from this function as doing so might result in the destruction of this ImageLoader.
+    updatedHasPendingLoadEvent();
 }
 
 void ImageLoader::updateFromElementIgnoringPreviousError()
@@ -258,6 +272,9 @@ void ImageLoader::notifyFinished(CachedResource* resource)
 
     if (resource->wasCanceled()) {
         m_hasPendingLoadEvent = false;
+        // Only consider updating the protection ref-count of the Element immediately before returning
+        // from this function as doing so might result in the destruction of this ImageLoader.
+        updatedHasPendingLoadEvent();
         return;
     }
 
@@ -304,6 +321,24 @@ void ImageLoader::updateRenderer()
         imageResource->setCachedImage(m_image.get());
 }
 
+void ImageLoader::updatedHasPendingLoadEvent()
+{
+    // If an Element that does image loading is removed from the DOM the load event for the image is still observable.
+    // As long as the ImageLoader is actively loading, the Element itself needs to be ref'ed to keep it from being
+    // destroyed by DOM manipulation or garbage collection.
+    // If such an Element wishes for the load to stop when removed from the DOM it needs to stop the ImageLoader explicitly.
+
+    if (m_hasPendingLoadEvent == m_elementIsProtected)
+        return;
+
+    m_elementIsProtected = m_hasPendingLoadEvent;
+
+    if (m_elementIsProtected)
+        m_element->ref();
+    else
+        m_element->deref();
+}
+
 void ImageLoader::dispatchPendingEvent(ImageEventSender* eventSender)
 {
     ASSERT(eventSender == &beforeLoadEventSender() || eventSender == &loadEventSender() || eventSender == &errorEventSender());
@@ -339,6 +374,10 @@ void ImageLoader::dispatchPendingBeforeLoadEvent()
     
     if (m_element->hasTagName(HTMLNames::objectTag))
         static_cast<HTMLObjectElement*>(m_element)->renderFallbackContent();
+
+    // Only consider updating the protection ref-count of the Element immediately before returning
+    // from this function as doing so might result in the destruction of this ImageLoader.
+    updatedHasPendingLoadEvent();
 }
 
 void ImageLoader::dispatchPendingLoadEvent()
@@ -351,6 +390,10 @@ void ImageLoader::dispatchPendingLoadEvent()
         return;
     m_hasPendingLoadEvent = false;
     dispatchLoadEvent();
+
+    // Only consider updating the protection ref-count of the Element immediately before returning
+    // from this function as doing so might result in the destruction of this ImageLoader.
+    updatedHasPendingLoadEvent();
 }
 
 void ImageLoader::dispatchPendingErrorEvent()
--- third_party/WebKit/Source/WebCore/loader/ImageLoader.h
+++ third_party/WebKit/Source/WebCore/loader/ImageLoader.h
@@ -76,6 +76,8 @@ private:
     virtual void dispatchLoadEvent() = 0;
     virtual String sourceURI(const AtomicString&) const = 0;
 
+    void updatedHasPendingLoadEvent();
+
     void dispatchPendingBeforeLoadEvent();
     void dispatchPendingLoadEvent();
     void dispatchPendingErrorEvent();
@@ -91,6 +93,7 @@ private:
     bool m_hasPendingErrorEvent : 1;
     bool m_imageComplete : 1;
     bool m_loadManually : 1;
+    bool m_elementIsProtected : 1;
 };
 
 }
