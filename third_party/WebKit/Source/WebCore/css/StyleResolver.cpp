13e5f4790a4b31526757df59fa6539836f9553a7
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-12 22:39:01 +0000

    Replaced items in a flexbox should be coerced to display:block https://bugs.webkit.org/show_bug.cgi?id=87068
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    The flexbox spec lists HTML tags that should automatically be converted
    to display:block when a flex child. It also says that atomic inline-level
    children should become block (e.g., inline-table should be treated as table).
    http://dev.w3.org/csswg/css3-flexbox/#flex-items
    
    Test: css3/flexbox/flexitem.html
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::collectMatchingRulesForList):
    
    LayoutTests:
    
    * css3/flexbox/flexitem-expected.txt: Added.
    * css3/flexbox/flexitem.html: Added.
    * css3/flexbox/resources/flexbox.js:
    (checkExpectedValues):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120132 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KeyframeList.h"
 #include "LinkHash.h"
 #include "LocaleToScriptMapping.h"
+#include "MathMLNames.h"
 #include "Matrix3DTransformOperation.h"
 #include "MatrixTransformOperation.h"
 #include "MediaList.h"
@@ -1920,6 +1921,36 @@ static void addIntrinsicMargins(RenderStyle* style)
     }
 }
 
+static bool shouldBecomeBlockWhenParentIsFlexbox(const Element* element)
+{
+    return element->hasTagName(imgTag)
+        || element->hasTagName(canvasTag)
+#if ENABLE(SVG)
+        || element->hasTagName(SVGNames::svgTag)
+#endif
+#if ENABLE(MATHML)
+        || element->hasTagName(MathMLNames::mathTag)
+#endif
+#if ENABLE(VIDEO)
+        || element->hasTagName(audioTag)
+        || element->hasTagName(videoTag)
+#endif
+        || element->hasTagName(iframeTag)
+        || element->hasTagName(objectTag)
+        || element->hasTagName(embedTag)
+        || element->hasTagName(appletTag)
+#if ENABLE(PROGRESS_TAG)
+        || element->hasTagName(progressTag)
+#endif
+#if ENABLE(METER_TAG)
+        || element->hasTagName(meterTag)
+#endif
+        || element->hasTagName(inputTag)
+        || element->hasTagName(buttonTag)
+        || element->hasTagName(selectTag)
+        || element->hasTagName(textareaTag);
+}
+
 static EDisplay equivalentBlockDisplay(EDisplay display, bool isFloating, bool strictParsing)
 {
     switch (display) {
@@ -2062,6 +2093,13 @@ void StyleResolver::adjustRenderStyle(RenderStyle* style, RenderStyle* parentSty
         // https://bugs.webkit.org/show_bug.cgi?id=46418 - Flexible box support.
         if (style->writingMode() != TopToBottomWritingMode && (style->display() == BOX || style->display() == INLINE_BOX))
             style->setWritingMode(TopToBottomWritingMode);
+
+        if (e && e->parentNode() && e->parentNode()->renderer() && e->parentNode()->renderer()->isFlexibleBox()) {
+            if (shouldBecomeBlockWhenParentIsFlexbox(e))
+                style->setDisplay(BLOCK);
+            else if (style->display() != INLINE)
+                style->setDisplay(equivalentBlockDisplay(style->display(), style->isFloating(), m_checker.strictParsing()));
+        }
     }
 
     // Make sure our z-index value is only applied if the object is positioned.
