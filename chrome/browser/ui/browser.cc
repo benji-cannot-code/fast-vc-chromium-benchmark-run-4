cc772e7e633916e0d7f254374856bfa9fb060cdf
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 12:02:43 +0000

    Adding "Clear hosted app data" checkbox to browsing data removal UI.
    Original patch from battre@chromium.org, I'm just sheparding it along as he's
    out on vacation.
    
    BUG=116372
    TBR=rsimha@chromium.org, timsteele@chromium.org
    TEST=Install a hosted application, then clear browsing data with the new checkbox checked: the hosted application's data should be cleared. Without the checkbox, the data should remain untouched.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10502015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -2134,6 +2134,9 @@ void Browser::RegisterUserPrefs(PrefService* prefs) {
   prefs->RegisterBooleanPref(prefs::kDeleteFormData,
                              false,
                              PrefService::SYNCABLE_PREF);
+  prefs->RegisterBooleanPref(prefs::kDeleteHostedAppsData,
+                             false,
+                             PrefService::SYNCABLE_PREF);
   prefs->RegisterIntegerPref(prefs::kDeleteTimePeriod,
                              0,
                              PrefService::SYNCABLE_PREF);
