99f2cfe7850b74a70c71cd45ff598b228cba0b06
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-12 14:39:24 +0000

    [chromium] Use TextureDrawQuad for video layer output when it produces a single GL texture https://bugs.webkit.org/show_bug.cgi?id=88814
    Reviewed by Adrienne Walker.
    
    When a CCVideLayerImpl produces quads for a frame that is contained
    in a single GL texture, then use the TextureDrawQuad instead of the
    VideoDrawQuad. Moves the computation for the RGBA(software) and
    NativeTexture (hardware) cases from LayerRendererChromium into
    CCVideoLayerImpl, so it can embed these values in the TextureDrawQuad.
    
    No new tests, behaviour should be the same.
    
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::drawStreamTexture):
    (WebCore::LayerRendererChromium::drawVideoQuad):
    * platform/graphics/chromium/LayerRendererChromium.h:
    (LayerRendererChromium):
    * platform/graphics/chromium/cc/CCVideoLayerImpl.cpp:
    (WebCore::CCVideoLayerImpl::appendQuads):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCVideoLayerImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCVideoLayerImpl.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/CCLayerTreeHostImpl.h"
 #include "cc/CCProxy.h"
 #include "cc/CCQuadCuller.h"
+#include "cc/CCTextureDrawQuad.h"
 #include "cc/CCVideoDrawQuad.h"
 #include <public/WebVideoFrame.h>
 #include <wtf/text/WTFString.h>
@@ -184,12 +185,44 @@ void CCVideoLayerImpl::appendQuads(CCQuadCuller& quadList, const CCSharedQuadSta
     IntRect quadRect(IntPoint(), bounds());
     WebKit::WebTransformationMatrix matrix;
 
-    if (m_format == Extensions3DChromium::GL_TEXTURE_EXTERNAL_OES)
+    switch (m_format) {
+    case GraphicsContext3D::LUMINANCE: {
+        // YUV software decoder.
+        OwnPtr<CCVideoDrawQuad> videoQuad = CCVideoDrawQuad::create(sharedQuadState, quadRect, m_framePlanes, 0, m_format, matrix);
+        quadList.append(videoQuad.release());
+        break;
+    }
+    case GraphicsContext3D::RGBA: {
+        // RGBA software decoder.
+        const FramePlane& plane = m_framePlanes[WebKit::WebVideoFrame::rgbPlane];
+        float widthScaleFactor = static_cast<float>(plane.visibleSize.width()) / plane.size.width();
+
+        bool premultipliedAlpha = true;
+        FloatRect uvRect(0, 0, widthScaleFactor, 1);
+        bool flipped = false;
+        OwnPtr<CCTextureDrawQuad> textureQuad = CCTextureDrawQuad::create(sharedQuadState, quadRect, plane.textureId, premultipliedAlpha, uvRect, flipped);
+        quadList.append(textureQuad.release());
+        break;
+    }
+    case GraphicsContext3D::TEXTURE_2D: {
+        // NativeTexture hardware decoder.
+        bool premultipliedAlpha = true;
+        FloatRect uvRect(0, 0, 1, 1);
+        bool flipped = false;
+        OwnPtr<CCTextureDrawQuad> textureQuad = CCTextureDrawQuad::create(sharedQuadState, quadRect, m_frame->textureId(), premultipliedAlpha, uvRect, flipped);
+        quadList.append(textureQuad.release());
+        break;
+    }
+    case Extensions3DChromium::GL_TEXTURE_EXTERNAL_OES: {
+        // StreamTexture hardware decoder.
         matrix = m_streamTextureMatrix;
-
-    OwnPtr<CCVideoDrawQuad> videoQuad = CCVideoDrawQuad::create(sharedQuadState, quadRect, m_framePlanes, m_frame->textureId(), m_format, matrix);
-
-    quadList.append(videoQuad.release());
+        OwnPtr<CCVideoDrawQuad> videoQuad = CCVideoDrawQuad::create(sharedQuadState, quadRect, m_framePlanes, m_frame->textureId(), m_format, matrix);
+        quadList.append(videoQuad.release());
+        break;
+    }
+    default:
+        CRASH(); // Someone updated convertVFCFormatToGC3DFormat above but update this!
+    }
 }
 
 void CCVideoLayerImpl::didDraw()
