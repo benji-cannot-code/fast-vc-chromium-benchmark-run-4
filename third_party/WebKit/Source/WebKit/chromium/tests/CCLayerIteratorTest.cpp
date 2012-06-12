25ce8f32c34aa3374a050c3e37900c91a9354e85
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-12 19:17:54 +0000

    [chromium] Don't crash in CCLayerIterator if the root layer doesn't have a render surface https://bugs.webkit.org/show_bug.cgi?id=88886
    Patch by Sami Kyostila <skyostil@chromium.org> on 2012-06-12
    Reviewed by James Robinson.
    
    If we are iterating over a render render surface layer list where the
    root layer does not have a render surface, fail gracefully instead of
    crashing.
    
    Tests: CCLayerIteratorTest.{emptyTree,rootLayerWithoutRenderSurface}
    
    * platform/graphics/chromium/cc/CCLayerIterator.h:
    (WebCore::CCLayerIterator::CCLayerIterator):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerIteratorTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerIteratorTest.cpp
@@ -83,6 +83,8 @@ void resetCounts(Vector<RefPtr<LayerChromium> >& renderSurfaceLayerList)
         renderSurfaceLayer->m_countRepresentingContributingSurface = -1;
         renderSurfaceLayer->m_countRepresentingItself = -1;
 
+        if (!renderSurface)
+            continue;
         for (unsigned layerIndex = 0; layerIndex < renderSurface->layerList().size(); ++layerIndex) {
             TestLayerChromium* layer = static_cast<TestLayerChromium*>(renderSurface->layerList()[layerIndex].get());
 
@@ -121,6 +123,14 @@ void iterateBackToFront(Vector<RefPtr<LayerChromium> >* renderSurfaceLayerList)
         if (it.representsItself())
             layer->m_countRepresentingItself = count;
     }
+}
+
+TEST(CCLayerIteratorTest, emptyTree)
+{
+    Vector<RefPtr<LayerChromium> > renderSurfaceLayerList;
+
+    iterateBackToFront(&renderSurfaceLayerList);
+    iterateFrontToBack(&renderSurfaceLayerList);
 }
 
 TEST(CCLayerIteratorTest, simpleTree)
@@ -276,6 +286,19 @@ TEST(CCLayerIteratorTest, complexTreeMultiSurface)
     EXPECT_COUNT(root23, 3, 4, 2);
     EXPECT_COUNT(root231, -1, -1, 1);
     EXPECT_COUNT(root3, -1, -1, 0);
+}
+
+TEST(CCLayerIteratorTest, rootLayerWithoutRenderSurface)
+{
+    RefPtr<TestLayerChromium> rootLayer = TestLayerChromium::create();
+    Vector<RefPtr<LayerChromium> > renderSurfaceLayerList;
+    renderSurfaceLayerList.append(rootLayer.get());
+
+    iterateBackToFront(&renderSurfaceLayerList);
+    EXPECT_COUNT(rootLayer, -1, -1, -1);
+
+    iterateFrontToBack(&renderSurfaceLayerList);
+    EXPECT_COUNT(rootLayer, -1, -1, -1);
 }
 
 } // namespace
