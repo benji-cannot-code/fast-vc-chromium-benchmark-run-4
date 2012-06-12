be9cf61459da276ad19e0e48b88c25c368ed2212
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-12 03:04:28 +0000

    [EFL] Remove CMakeCache.txt before building https://bugs.webkit.org/show_bug.cgi?id=88717
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-06-11
    Reviewed by Antonio Gomes.
    
    Remove CMake cache file before every build to avoid using outdated
    build flags when new features get enabled. This will avoid unexpected
    failures on the bots due to outdated build flags.
    
    * Scripts/webkitdirs.pm:
    (generateBuildSystemFromCMakeProject):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -2096,6 +2096,12 @@ sub generateBuildSystemFromCMakeProject
         $ENV{'CXXFLAGS'} = "-march=pentium4 -msse2 -mfpmath=sse " . ($ENV{'CXXFLAGS'} || "");
     }
 
+    # Remove CMakeCache.txt to avoid using outdated build flags
+    if (isEfl()) {
+        my $cacheFilePath = File::Spec->catdir($buildPath, "CMakeCache.txt");
+        unlink($cacheFilePath) if -e $cacheFilePath;
+    }
+
     # We call system("cmake @args") instead of system("cmake", @args) so that @args is
     # parsed for shell metacharacters.
     my $wrapper = jhbuildWrapperPrefixIfNeeded() . " ";
