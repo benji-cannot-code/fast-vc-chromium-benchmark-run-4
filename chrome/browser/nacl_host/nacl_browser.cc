f52efdffe7ce79bad7aff3d8b2cb0f2ed7d515bb
ncbray@google.com <ncbray@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 01:57:04 +0000

    Fix an incorrect reference to a pre-processor define.
    The typo originated from: https://chromiumcodereview.appspot.com/10532018
    and caused the CL to not function as intended.
    
    BUG= http://code.google.com/p/chromium/issues/detail?id=131218
    TEST= none
    
    Review URL: https://chromiumcodereview.appspot.com/10536107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141592 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/nacl_browser.cc
+++ chrome/browser/nacl_host/nacl_browser.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/pickle.h"
 #include "base/win/windows_version.h"
+#include "build/build_config.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_paths_internal.h"
 #include "content/public/browser/browser_thread.h"
@@ -22,7 +23,7 @@ const char kValidationCacheSequenceName[] = "NaClValidationCache";
 const FilePath::CharType kValidationCacheFileName[] =
     FILE_PATH_LITERAL("nacl_validation_cache.bin");
 
-#if defined(CHROMEOS)
+#if defined(OS_CHROMEOS)
 // TODO(ncbray) enable on ChromeOS.
 // http://code.google.com/p/chromium/issues/detail?id=131218
 const bool kValidationCacheEnabledByDefault = false;
