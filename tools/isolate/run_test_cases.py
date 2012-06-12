f20f9875306f4771104e94162e1bff1b2455ef63
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 18:07:00 +0000

    Add a unit test for run_test_cases.py.
    Make Progress to output to stderr, it automatically flushes and it doesn't
    interfere with the output, making testing easier and stdout more usable in
    general, like if piping the results.
    
    NOTRY=true
    R=cmp@chromium.org
    BUG=
    TEST=new integration test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10533085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141686 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/run_test_cases.py
+++ tools/isolate/run_test_cases.py
@@ -118,7 +118,8 @@ def get_test_cases(executable, whitelist, blacklist):
   return tests
 
 
-def run_test_cases(executable, whitelist, blacklist, jobs, timeout, stats_only):
+def run_test_cases(
+    executable, whitelist, blacklist, jobs, timeout, stats_only, no_dump):
   """Traces test cases one by one."""
   test_cases = get_test_cases(executable, whitelist, blacklist)
   if not test_cases:
@@ -132,8 +133,9 @@ def run_test_cases(executable, whitelist, blacklist, jobs, timeout, stats_only):
     results = pool.join(progress, 0.1)
     duration = time.time() - progress.start
   results = dict((item[0]['test_case'], item) for item in results)
-  trace_inputs.write_json('%s.run_test_cases' % executable, results, False)
-  print ''
+  if not no_dump:
+    trace_inputs.write_json('%s.run_test_cases' % executable, results, False)
+  sys.stderr.write('\n')
   total = len(results)
   if not total:
     return 1
@@ -208,6 +210,10 @@ def main():
       action='count',
       default=int(os.environ.get('ISOLATE_DEBUG', 0)),
       help='Use multiple times')
+  parser.add_option(
+      '--no-dump',
+      action='store_true',
+      help='do not generate a .test_cases file')
   options, args = parser.parse_args()
   levels = [logging.ERROR, logging.WARNING, logging.INFO, logging.DEBUG]
   logging.basicConfig(
@@ -225,7 +231,8 @@ def main():
       options.blacklist,
       options.jobs,
       options.timeout,
-      options.stats)
+      options.stats,
+      options.no_dump)
 
 
 if __name__ == '__main__':
