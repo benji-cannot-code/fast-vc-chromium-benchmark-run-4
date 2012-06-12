6042c87dbd132e5430ef3a21bae2c9ca5367c1dd
zhajiang@rim.com <zhajiang@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-12 21:44:54 +0000

    [BlackBerry] Scale was incorrect when reloading a simple web page after initial load https://bugs.webkit.org/show_bug.cgi?id=88889
    Reviewed by Adam Treat.
    Patch by Jacky Jiang <zhajiang@rim.com>
    
    PR: 164442
    When we were reloading a web page with the load type FrameLoadTypeSame
    after the initial load, the first layout timer was fired after the load
    Finished state, in which case the web page would have no chance to zoom
    to the initial scale during the layout as it was only for load
    Committed state. This patch takes care of it.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::layoutFinished):
    (BlackBerry::WebKit::WebPagePrivate::shouldZoomToInitialScaleOnLoad):
    (WebKit):
    (BlackBerry::WebKit::WebPagePrivate::zoomToInitialScaleOnLoad):
    * Api/WebPage_p.h:
    (WebPagePrivate):
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::dispatchDidFirstVisuallyNonEmptyLayout):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -1555,7 +1555,7 @@ void WebPagePrivate::layoutFinished()
 
     m_nestedLayoutFinishedCount++;
 
-    if (loadState() == Committed)
+    if (shouldZoomToInitialScaleOnLoad())
         zoomToInitialScaleOnLoad();
     else if (loadState() != None)
         notifyTransformedContentsSizeChanged();
@@ -1590,6 +1590,18 @@ void WebPagePrivate::layoutFinished()
             }
         }
     }
+}
+
+bool WebPagePrivate::shouldZoomToInitialScaleOnLoad() const
+{
+    // For FrameLoadTypeSame load, the first layout timer can be fired after the load Finished state. We should
+    // zoom to initial scale for this case as well, otherwise the scale of the web page can be incorrect.
+    FrameLoadType frameLoadType = FrameLoadTypeStandard;
+    if (m_mainFrame && m_mainFrame->loader())
+        frameLoadType = m_mainFrame->loader()->loadType();
+    if (m_loadState == Committed || (m_loadState == Finished && frameLoadType == FrameLoadTypeSame))
+        return true;
+    return false;
 }
 
 void WebPagePrivate::zoomToInitialScaleOnLoad()
@@ -1629,7 +1641,7 @@ void WebPagePrivate::zoomToInitialScaleOnLoad()
     if (m_mainFrame && m_mainFrame->loader() && m_mainFrame->loader()->shouldRestoreScrollPositionAndViewState())
         shouldZoom = false;
 
-    if (shouldZoom && loadState() == Committed) {
+    if (shouldZoom && shouldZoomToInitialScaleOnLoad()) {
         // Preserve at top and at left position, to avoid scrolling
         // to a non top-left position for web page with viewport meta tag
         // that specifies an initial-scale that is zoomed in.
