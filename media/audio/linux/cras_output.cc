8c511c82f606939dfe42c36b387f816691917755
dgreid@chromium.org <dgreid@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 09:17:07 +0000

    CrasOutput: Handle Close() being called after a failed Open().
    If Open() fails due to the pipe() or pthread_create() system calls
    failing, client_ will be non-null but will have been freed. To fix this,
    set client_ to NULL on failures in Open(), and check before using it in
    Close().
    
    BUG=132040
    TEST=Change the cras library to return errors from the client_connect() or client_run() functions, then watch as the errors cause a crash when you add an audio stream.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10546090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141638 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/linux/cras_output.cc
+++ media/audio/linux/cras_output.cc
@@ -121,6 +121,7 @@ bool CrasOutputStream::Open() {
   int err = cras_client_create(&client_);
   if (err < 0) {
     LOG(WARNING) << "Couldn't create CRAS client.\n";
+    client_ = NULL;
     TransitionTo(kInError);
     return false;
   }
@@ -128,6 +129,7 @@ bool CrasOutputStream::Open() {
   if (err) {
     LOG(WARNING) << "Couldn't connect CRAS client.\n";
     cras_client_destroy(client_);
+    client_ = NULL;
     TransitionTo(kInError);
     return false;
   }
@@ -136,6 +138,7 @@ bool CrasOutputStream::Open() {
   if (err) {
     LOG(WARNING) << "Couldn't run CRAS client.\n";
     cras_client_destroy(client_);
+    client_ = NULL;
     TransitionTo(kInError);
     return false;
   }
@@ -150,8 +153,11 @@ void CrasOutputStream::Close() {
     return;
   }
 
-  cras_client_stop(client_);
-  cras_client_destroy(client_);
+  if (client_) {
+    cras_client_stop(client_);
+    cras_client_destroy(client_);
+    client_ = NULL;
+  }
 
   // Signal to the manager that we're closed and can be removed.
   // Should be last call in the method as it deletes "this".
