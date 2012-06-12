1ccf49e2b4eb4bbe9b1a0667306da84cfdc9e0e8
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 19:06:43 +0000

    Add trace-viewer to third party and redirect about:tracing to that implementation.
    Review URL: https://chromiumcodereview.appspot.com/10546068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141695 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/tracing_ui.cc
+++ chrome/browser/ui/webui/tracing_ui.cc
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents_view.h"
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_message_handler.h"
-#include "grit/browser_resources.h"
+#include "grit/tracing_resources.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
