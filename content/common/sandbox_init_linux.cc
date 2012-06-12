eb03d2ff885854a786b780176e71ab47b48c99ec
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 17:55:00 +0000

    Disable the seccomp filter GPU process sandbox by default on Chrome OS.
    We don't want the GPU process sandbox on by default on Chrome OS.
    Since we control Chrome's command line, we can make sure it's enabled
    for users, but disabled for developers.
    
    Add an '--enable-chromeos-gpu-sandbox' flag, and remove the #ifdef that
    disabled the sandbox on Chrome OS. Until we manually add the flag to
    Chrome OS, the sandbox will continue to be disabled.
    
    Moreover, even with the flag on, we only enable the sandbox on 64 bits and
    for Intel GPUs.
    
    BUG=127664
    TEST=unit tests, and WebGL test websites on Lumpy.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10534049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141683 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_init_linux.cc
+++ content/common/sandbox_init_linux.cc
@@ -390,6 +390,27 @@ static void InstallFilter(const std::vector<struct sock_filter>& program) {
   PLOG_IF(FATAL, ret != 0) << "Failed to install filter.";
 }
 
+static bool ShouldEnableGPUSandbox() {
+  // Default setting is: enabled for Linux, disabled for Chrome OS.
+  // '--disable-gpu-sandbox' takes precedence over '--enable-gpu-sandbox'.
+#if defined(OS_CHROMEOS)
+  bool res = false;
+#else
+  bool res = true;
+#endif
+
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+
+  if (command_line.HasSwitch(switches::kEnableGpuSandbox)) {
+    res = true;
+  }
+  if (command_line.HasSwitch(switches::kDisableGpuSandbox)) {
+    res = false;
+  }
+
+  return res;
+}
+
 }  // anonymous namespace
 
 namespace content {
@@ -403,7 +424,7 @@ void InitializeSandbox() {
   std::string process_type =
       command_line.GetSwitchValueASCII(switches::kProcessType);
   if (process_type == switches::kGpuProcess &&
-      command_line.HasSwitch(switches::kDisableGpuSandbox))
+      !ShouldEnableGPUSandbox())
     return;
 
   if (!CanUseSeccompFilters())
@@ -444,4 +465,3 @@ void InitializeSandbox() {
 }  // namespace content
 
 #endif
-
