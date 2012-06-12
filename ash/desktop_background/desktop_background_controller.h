bafb5f064ad537d6dbf5dc3d43af0f7badbfd673
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-12 23:06:38 +0000

    Remove root_window_ member from DesktopBackgroundController.   This will be changed to set the wallpaper on all root windows.
    BUG=123160
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10545142
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141777 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/desktop_background_controller.h
+++ ash/desktop_background/desktop_background_controller.h
@@ -47,7 +47,7 @@ class ASH_EXPORT DesktopBackgroundController {
     BACKGROUND_SOLID_COLOR
   };
 
-  explicit DesktopBackgroundController(aura::RootWindow* root_window);
+  DesktopBackgroundController();
   virtual ~DesktopBackgroundController();
 
   // Gets the desktop background mode.
@@ -91,8 +91,6 @@ class ASH_EXPORT DesktopBackgroundController {
   // Creates a new background widget and sets the background mode to image mode.
   // Called after wallpaper loaded successfully.
   void OnWallpaperLoadCompleted(scoped_refptr<WallpaperOperation> wo);
-
-  aura::RootWindow* root_window_;
 
   // Can change at runtime.
   BackgroundMode desktop_background_mode_;
