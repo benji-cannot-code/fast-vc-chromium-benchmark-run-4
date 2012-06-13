cde685b0ee074f824ecca9882e68ed1ffc7aa78d
scottmg@google.com <scottmg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 01:05:42 +0000

    Change timeouts for renderer and plugin hang detection
    Some crash detection work points to the the renderer hang dialog coming up when it should be the plugin hung dialog. To see if this is the cause of elevated renderer crash rates, make the plugin timeout lower than the renderer temporarily. This will probably be reverted after testing.
    
    R=jam@chromium.org
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10536134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141819 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_view_prefs.cc
+++ chrome/browser/ui/browser_view_prefs.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // How long do we wait before we consider a window hung (in ms).
-const int kDefaultPluginMessageResponseTimeout = 30000;
+const int kDefaultPluginMessageResponseTimeout = 25000;
 
 // How frequently we check for hung plugin windows.
 const int kDefaultHungPluginDetectFrequency = 2000;
