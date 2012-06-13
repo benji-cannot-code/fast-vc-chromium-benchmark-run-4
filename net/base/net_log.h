9468be0e558b7cc3be97399df517350003b66f1c
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 22:14:33 +0000

    A pair of NetLog changes for ongoing refactoring:
    SourceEventParametersCallback returns NULL on invalid
    sources, like the old corresponding function.
    
    Add a NetLog::StringCallback overload that takes in a
    string16.
    
    R=eroman@chromium.org
    BUG=126243
    
    Review URL: https://chromiumcodereview.appspot.com/10546151
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141993 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/net_log.h
+++ net/base/net_log.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/string16.h"
 #include "net/base/net_export.h"
 
 namespace base {
@@ -306,6 +307,10 @@ class NET_EXPORT NetLog {
   // Warning: |name| and |value| must remain valid for the life of the callback.
   static ParametersCallback StringCallback(const char* name,
                                            const std::string* value);
+
+  // Same as above, but takes in a UTF16 string.
+  static ParametersCallback StringCallback(const char* name,
+                                           const string16* value);
 
  protected:
   // Child classes should respond to the new entry here.  This includes
