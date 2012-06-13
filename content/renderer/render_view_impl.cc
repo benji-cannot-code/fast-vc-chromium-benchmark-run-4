c7ec36075b93175a8e7d4235c5bfd9d5992ab543
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 02:56:59 +0000

    Remove magic iframe load transfer support. Basically manual revert of r97048, due to files changing locations etc.
    Review URL: https://chromiumcodereview.appspot.com/9922001
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141841 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -180,7 +180,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/glue/web_intent_service_data.h"
 #include "webkit/glue/webkit_constants.h"
 #include "webkit/glue/webkit_glue.h"
-#include "webkit/glue/weburlloader_impl.h"
 #include "webkit/glue/weburlresponse_extradata_impl.h"
 #include "webkit/gpu/webgraphicscontext3d_in_process_impl.h"
 #include "webkit/media/webmediaplayer_impl.h"
@@ -3412,12 +3411,6 @@ void RenderViewImpl::didRunInsecureContent(
       routing_id_,
       origin.toString().utf8(),
       target));
-}
-
-void RenderViewImpl::didAdoptURLLoader(WebKit::WebURLLoader* loader) {
-  webkit_glue::WebURLLoaderImpl* loader_impl =
-      static_cast<webkit_glue::WebURLLoaderImpl*>(loader);
-  loader_impl->UpdateRoutingId(routing_id_);
 }
 
 void RenderViewImpl::didExhaustMemoryAvailableForScript(WebFrame* frame) {
--- webkit/glue/weburlloader_impl.h
+++ webkit/glue/weburlloader_impl.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -30,8 +30,6 @@ class WebURLLoaderImpl : public WebKit::WebURLLoader {
       WebKit::WebURLLoaderClient* client);
   virtual void cancel();
   virtual void setDefersLoading(bool value);
-
-  WEBKIT_GLUE_EXPORT void UpdateRoutingId(int new_routing_id);
 
  private:
   class Context;
