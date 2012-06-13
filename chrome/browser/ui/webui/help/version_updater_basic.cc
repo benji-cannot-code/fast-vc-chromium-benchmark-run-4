55c50ce106aae6a83a3057b92578f7df8e542dc2
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 03:51:55 +0000

    retry r141693 (reverted by r141711) Add relaunch prompt to linux help page.
    This doesn't allow you to check for updates or show progress, it just notifies you if a new version is ready. As such, I didn't see a reason to limit it to Google Chrome builds.
    
    This also fixes a bug where a 'disabled' state permanently hides the status text.
    
    BUG=117403
    TEST=manual with --check-for-update-interval=1 and manually editing the version
    
    Review URL: https://chromiumcodereview.appspot.com/10532120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141850 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/webui/help/version_updater_basic.cc
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/webui/help/version_updater_basic.h"
+
+#include "base/string16.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
+#include "chrome/browser/upgrade_detector.h"
+
+void VersionUpdaterBasic::CheckForUpdate(
+    const StatusCallback& status_callback) {
+  if (UpgradeDetector::GetInstance()->notify_upgrade())
+    status_callback.Run(NEARLY_UPDATED, 0, string16());
+  else
+    status_callback.Run(DISABLED, 0, string16());
+}
+
+void VersionUpdaterBasic::RelaunchBrowser() const {
+  browser::AttemptRestart();
+}
+
+VersionUpdater* VersionUpdater::Create() {
+  return new VersionUpdaterBasic;
+}
