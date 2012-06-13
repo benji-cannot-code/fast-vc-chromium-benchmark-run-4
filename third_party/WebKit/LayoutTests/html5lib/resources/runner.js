75a1da38b51b5557d72e21da74ed4b9481385ab1
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-13 12:34:26 +0000

    Fonts, fullscreen, gamepad, and html5lib tests should use testRunner instead of layoutTestController https://bugs.webkit.org/show_bug.cgi?id=88959
    Reviewed by Darin Adler.
    
    * fonts/valid-standard-font.html:
    * fullscreen/anonymous-block-merge-crash.html:
    * fullscreen/full-screen-element-stack.html:
    * fullscreen/full-screen-frameset.html:
    * fullscreen/full-screen-iframe-zIndex.html:
    * fullscreen/full-screen-keyboard-disabled.html:
    * fullscreen/full-screen-keyboard-enabled.html:
    * fullscreen/full-screen-remove-ancestor-during-transition.html:
    * fullscreen/full-screen-remove-sibling.html:
    * fullscreen/full-screen-render-inline.html:
    * fullscreen/full-screen-request-rejected.html:
    * fullscreen/full-screen-request-removed.html:
    * fullscreen/full-screen-restrictions.html:
    * fullscreen/full-screen-stacking-context.html:
    * fullscreen/full-screen-test.js:
    (runWithKeyDown):
    (endTest):
    * fullscreen/full-screen-zIndex-after.html:
    * fullscreen/full-screen-zIndex.html:
    * fullscreen/non-ancestor-iframe.html:
    * fullscreen/parent-flow-inline-with-block-child.html:
    * fullscreen/video-controls-override.html:
    * html5lib/resources/runner.js:
    (run):
    * html5lib/webkit-resumer.html:
    * platform/gtk/fonts/custom-font-missing-glyphs.html:
    * platform/gtk/fonts/font-face-with-complex-text.html:
    * platform/gtk/fonts/font-family-fallback.html:
    * platform/gtk/fonts/synthetic-oblique-positioning.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/html5lib/resources/runner.js
+++ third_party/WebKit/LayoutTests/html5lib/resources/runner.js
@@ -31,8 +31,8 @@ document.writeln("</style>");
 document.writeln("<p>Script did not run</p>");
 document.writeln("<iframe></iframe>");
 
-if (window.layoutTestController)
-    layoutTestController.waitUntilDone();
+if (window.testRunner)
+    testRunner.waitUntilDone();
 
 Markup.noAutoDump();
 Markup.useHTML5libOutputFormat();
@@ -78,8 +78,8 @@ function run()
         }
         xhr.send(null);
     } else {
-        if (window.layoutTestController)
-            layoutTestController.notifyDone();
+        if (window.testRunner)
+            testRunner.notifyDone();
     }
 }
 
