3a7a1db6afcda6c4b0436905a5e8d970b343eaea
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 19:16:38 +0000

    The server bound cert store might have a NULL persistent store, so check before accessing it
    This can happen for Incognito profiles for example
    
    BUG=131855
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10543134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141928 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/default_server_bound_cert_store.cc
+++ net/base/default_server_bound_cert_store.cc
@@ -123,7 +123,8 @@ void DefaultServerBoundCertStore::SetForceKeepSessionState() {
   base::AutoLock autolock(lock_);
   InitIfNecessary();
 
-  store_->SetForceKeepSessionState();
+  if (store_)
+    store_->SetForceKeepSessionState();
 }
 
 DefaultServerBoundCertStore::~DefaultServerBoundCertStore() {
