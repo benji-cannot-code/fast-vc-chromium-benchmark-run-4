733aff45164afa1c403d797764a9fb55ff24eeb0
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 07:01:35 +0000

    Add IPC argument to handle composition character bounds into ViewHostMsg_ImeCompositionRangeChanged.
    What for:
    Using this argument, browser can acquire the composition character boundary rectangles.
    This information is used by Japanese input method editor to show its window at the correct position.
    This is used not only for fixing http://crbug.com/120597 but also for supporting IMR_QUERYCHARPOSITION message in Windows Chrome.
    To support IMR_QUERYCHARPOSITION(Ref 1) message, input method editor can also show its window at the correct position(Ref 2).
    
    Performance:
    There is no performance concern for latin language users because this IPC is only emitted if the user uses an input method editor.
    Even if the user uses an input method editor, the cost of this IPC is small because this IPC will be emitted at the same frequency as key typing.
    
    Data size:
    The data size to be transferred is small: it is the composition string length (typically less than a few dozen characters) times sizeof(gfx::Rect).
    
    Ref 1. http://msdn.microsoft.com/en-us/library/windows/desktop/dd318634(v=vs.85).aspx
    Ref 2. https://bug-87911-attachments.webkit.org/attachment.cgi?id=144954
    (In Ref2, Safari works well for all IMEs. Firefox works well except ATOK. Chrome doesn't work for all IMEs.)
    
    BUG=120597
    TEST=try bots.
    
    Review URL: https://chromiumcodereview.appspot.com/10543024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141866 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1508,9 +1508,10 @@ void RenderWidgetHostImpl::OnMsgTextInputStateChanged(
 }
 
 void RenderWidgetHostImpl::OnMsgImeCompositionRangeChanged(
-    const ui::Range& range) {
+    const ui::Range& range,
+    const std::vector<gfx::Rect>& character_bounds) {
   if (view_)
-    view_->ImeCompositionRangeChanged(range);
+    view_->ImeCompositionRangeChanged(range, character_bounds);
 }
 
 void RenderWidgetHostImpl::OnMsgImeCancelComposition() {
