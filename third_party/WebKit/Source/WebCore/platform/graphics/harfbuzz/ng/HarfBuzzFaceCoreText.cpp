0790cb7e57ab446853f0fdcabd9d617e2667f2eb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-13 19:21:18 +0000

    [chromium mac] Don't include things in subframeworks of ApplicationServices.framework.
    A CoreText #include (with quotes instead of brackets, even) sprung up
    in r120506, bug 69826.
    
    https://bugs.webkit.org/show_bug.cgi?id=88569
    
    Patch by Mark Mentovai <mark@chromium.org> on 2012-06-13
    Reviewed by Stephen White.
    
    No new tests.
    
    * platform/graphics/harfbuzz/ng/HarfBuzzFaceCoreText.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120234 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzFaceCoreText.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzFaceCoreText.cpp
@@ -32,12 +32,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "HarfBuzzFace.h"
 
-#include "CoreText/CoreText.h"
 #include "FontPlatformData.h"
 #include "HarfBuzzShaper.h"
 #include "SimpleFontData.h"
 
 #include "hb.h"
+#include <ApplicationServices/ApplicationServices.h>
 
 namespace WebCore {
 
