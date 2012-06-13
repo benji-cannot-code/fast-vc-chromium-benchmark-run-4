59a091dc7b26a99791002d18564274f7a5995f54
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-13 17:38:37 +0000

    mac: Pipe backing scale factor to renderer on change.
    Not used for anything yet, so no visible effect.
    
    BUG=128267
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10542135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@141906 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -116,7 +116,8 @@ static NSString* const NSBackingPropertyOldScaleFactorKey =
 // -windowDidChangeBackingProperties:) uses
 // @"NSWindowBackingPropertiesChangeOldBackingScaleFactorKey", but that always
 // returns an old scale of 0. @"NSBackingPropertyOldScaleFactorKey" seems to
-// work in practice, and it's what's used in Apple's WebKit port.
+// work in practice, and it's what's used in Apple's WebKit port
+// (WebKit/mac/WebView/WebView.mm).
 
 #endif  // 10.7
 
@@ -1899,8 +1900,11 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
       [[notification userInfo] objectForKey:NSBackingPropertyOldScaleFactorKey])
       doubleValue];
   if (newBackingScaleFactor != oldBackingScaleFactor) {
-    // TODO(thakis): Tell renderer and backing store about new DPI,
-    // schedule repaint.
+    // TODO(thakis): Tell backing store about new DPI, schedule repaint.
+    if (renderWidgetHostView_->render_widget_host_) {
+      renderWidgetHostView_->render_widget_host_->SetDeviceScaleFactor(
+          newBackingScaleFactor);
+    }
   }
 }
 
