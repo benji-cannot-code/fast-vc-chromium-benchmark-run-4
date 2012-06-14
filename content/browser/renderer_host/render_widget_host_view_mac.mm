5e22a4aecec4ed08a93cc595bc55b231be54f752
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 05:21:24 +0000

    mac: Make dynamic DPI changes work.
    This fixes the software case completely, and makes everything
    look good on the hardware path after a few frames. In the
    hardware path, the IOSurfaces aren't tagged with their own DPI,
    so they are rendered at the wrong scale for a few frames, until
    the "DPI changed" message made it from the browser to the
    renderer.
    
    This CL adds three things that happen in response to DPI changes:
    1.) It reallocates the browser-side BackingStoreMac at the new DPI.
    2.) It lets the renderer do a full repaint after DPI changed.
    3.) It passes the new DPI on to WebKit (in render_view_impl.cc)
    
    (3) won't have an effect for hardware pages until
    https://bugs.webkit.org/show_bug.cgi?id=88916 lands.
    
    BUG=128267
    TEST=change from lodpi and hidpi and back, check that software- and
    hardware-rendered pages look good after the change in both foreground
    and background tabs.
    
    Review URL: https://chromiumcodereview.appspot.com/10548026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142099 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -160,6 +160,7 @@ float ScaleFactor(NSView* view) {
 - (void)cancelChildPopups;
 - (void)windowDidChangeBackingProperties:(NSNotification*)notification;
 - (void)checkForPluginImeCancellation;
+- (void)updateTabBackingStoreScaleFactor;
 @end
 
 // NSEvent subtype for scroll gestures events.
@@ -399,6 +400,10 @@ RenderWidgetHost* RenderWidgetHostViewMac::GetRenderWidgetHost() const {
 void RenderWidgetHostViewMac::DidBecomeSelected() {
   if (!is_hidden_)
     return;
+
+  // Check if the backing scale factor changed while the tab was in the
+  // background.
+  [cocoa_view_ updateTabBackingStoreScaleFactor];
 
   if (web_contents_switch_paint_time_.is_null())
     web_contents_switch_paint_time_ = base::TimeTicks::Now();
@@ -821,8 +826,6 @@ bool RenderWidgetHostViewMac::IsPopup() const {
 
 BackingStore* RenderWidgetHostViewMac::AllocBackingStore(
     const gfx::Size& size) {
-  // TODO(thakis): Register for backing scale factor change events and pass
-  // that on.
   float scale = ScaleFactor(cocoa_view_);
   return new BackingStoreMac(render_widget_host_, size, scale);
 }
@@ -1891,6 +1894,20 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
   }
 }
 
+- (void)updateTabBackingStoreScaleFactor {
+  if (!renderWidgetHostView_->render_widget_host_)
+    return;
+
+  float scaleFactor = ScaleFactor(self);
+  BackingStoreMac* backingStore = static_cast<BackingStoreMac*>(
+      renderWidgetHostView_->render_widget_host_->GetBackingStore(false));
+  if (backingStore)  // NULL in hardware path.
+    backingStore->ScaleFactorChanged(scaleFactor);
+
+  renderWidgetHostView_->render_widget_host_->SetDeviceScaleFactor(
+      scaleFactor);
+}
+
 // http://developer.apple.com/library/mac/#documentation/GraphicsAnimation/Conceptual/HighResolutionOSX/CapturingScreenContents/CapturingScreenContents.html#//apple_ref/doc/uid/TP40012302-CH10-SW4
 - (void)windowDidChangeBackingProperties:(NSNotification*)notification {
   NSWindow* window = (NSWindow*)[notification object];
@@ -1900,11 +1917,16 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
       [[notification userInfo] objectForKey:NSBackingPropertyOldScaleFactorKey])
       doubleValue];
   if (newBackingScaleFactor != oldBackingScaleFactor) {
-    // TODO(thakis): Tell backing store about new DPI, schedule repaint.
-    if (renderWidgetHostView_->render_widget_host_) {
-      renderWidgetHostView_->render_widget_host_->SetDeviceScaleFactor(
-          newBackingScaleFactor);
-    }
+    // Background tabs check if their scale factor changed when they become
+    // active, in DidBecomeSelected().
+
+    // Allocating a CGLayerRef with the current scale factor immediately from
+    // this handler doesn't work. Schedule the backing store update on the
+    // next runloop cycle, then things are read for CGLayerRef allocations to
+    // work.
+    [self performSelector:@selector(updateTabBackingStoreScaleFactor)
+               withObject:nil
+               afterDelay:0];
   }
 }
 
