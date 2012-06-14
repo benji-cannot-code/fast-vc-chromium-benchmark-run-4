568d7b14b5d879525009217245c03cb52744828b
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 15:23:57 +0000

    Fixed Chrome OS Audio Player
    BUG=132445
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10546148
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142147 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
+++ chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // included file but that's all right since any javascript file should start
 // with a copyright comment anyway.
 
+//<include src="../file_type.js"/>
 //<include src="audio_player.js"/>
 //<include src="media_controls.js"/>
 //<include src="../metadata/metadata_cache.js"/>
