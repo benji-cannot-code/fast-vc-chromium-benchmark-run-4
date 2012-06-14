7c7ec53634a2dfddcc2194d6e143bd6596206e44
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 01:37:17 +0000

    Expose video memory usage estimation (textures, renderbuffers, buffers) in command buffer.
    This will be used by GpuMemoryManager
    
    BUG=129739
    TEST=bots
    R=gman
    
    Review URL: https://chromiumcodereview.appspot.com/10548034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142061 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/context_group.h
+++ gpu/command_buffer/service/context_group.h
@@ -123,6 +123,8 @@ class GPU_EXPORT ContextGroup : public base::RefCounted<ContextGroup> {
 
   IdAllocatorInterface* GetIdAllocator(unsigned namespace_id);
 
+  uint32 GetMemRepresented() const;
+
  private:
   friend class base::RefCounted<ContextGroup>;
   ~ContextGroup();
