d3e08f7ae539c23a6745343b131303b157da4350
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 07:22:09 +0000

    [WebSocket] WebSocket object in Worker Context should fire error event when it fails connection. https://bugs.webkit.org/show_bug.cgi?id=88744
    Patch by Li Yin <li.yin@intel.com> on 2012-06-14
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Spec:http://dev.w3.org/html5/websockets/#feedback-from-the-protocol
    The behavior of firing error event in Document has been implemented in Committed r117944.
    WebSocket in WorkerContext is also required to fire error event.
    
    Test: http/tests/websocket/tests/hybi/workers/close.html
    
    * Modules/websockets/ThreadableWebSocketChannelClientWrapper.cpp:
    (WebCore::ThreadableWebSocketChannelClientWrapper::didReceiveMessageError):
    (WebCore):
    (WebCore::ThreadableWebSocketChannelClientWrapper::didReceiveMessageErrorCallback):
    * Modules/websockets/ThreadableWebSocketChannelClientWrapper.h:
    (ThreadableWebSocketChannelClientWrapper):
    * Modules/websockets/WorkerThreadableWebSocketChannel.cpp:
    (WebCore::workerContextDidReceiveMessageError):
    (WebCore):
    (WebCore::WorkerThreadableWebSocketChannel::Peer::didReceiveMessageError):
    * Modules/websockets/WorkerThreadableWebSocketChannel.h:
    (Peer):
    
    LayoutTests:
    
    * http/tests/websocket/tests/hybi/workers/close-expected.txt:
    * http/tests/websocket/tests/hybi/workers/resources/close.js:
    (testPassed):
    (runCodeTest.else.ws.onerror):
    (runCodeTest):
    (runReasonTest.else.ws.onerror):
    (runReasonTest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/websocket/tests/hybi/workers/resources/close.js
+++ third_party/WebKit/LayoutTests/http/tests/websocket/tests/hybi/workers/resources/close.js
@@ -9,6 +9,11 @@ function postResult(result, actual, expected)
     postMessage(message);
 }
 
+function testPassed(message)
+{
+    postMessage("PASS: " + message);
+}
+
 function testFailed(message)
 {
     postMessage("FAIL: " + message);
@@ -111,6 +116,10 @@ function runCodeTest()
                 if (closeEvent.code == abnormalClosure)
                     runInvalidStringTest();
             };
+            ws.onerror = function ()
+            {
+                testPassed("onerror() was called.");
+            };
         }
         try {
             ws.close(codeTestCodes[id]);
@@ -147,6 +156,10 @@ function runReasonTest()
                 shouldBe("closeEvent.code", "abnormalClosure");
                 if (closeEvent.code == abnormalClosure)
                     runCodeAndReasonTest();
+            };
+            ws.onerror = function ()
+            {
+                testPassed("onerror() was called.");
             };
         }
         try {
