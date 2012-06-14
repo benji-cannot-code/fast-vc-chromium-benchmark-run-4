8623331ad6d9cba9a88e0edba017493b323beec4
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 22:33:20 +0000

    Fix bit packing of StyleRareInheritedData on Windows https://bugs.webkit.org/show_bug.cgi?id=89112
    Reviewed by Ryosuke Niwa.
    
    On my machine (64 bit Chromium Linux), StyleRareInheritedData is 168 bytes.
    
    No new tests, added a compile assert to ensure correctness going forward.
    
    * rendering/style/StyleRareInheritedData.cpp:
    (SameSizeAsStyleRareInheritedData):
    (WebCore):
    * rendering/style/StyleRareInheritedData.h:
    (StyleRareInheritedData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/style/StyleRareInheritedData.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleRareInheritedData.cpp
@@ -31,6 +31,35 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+struct SameSizeAsStyleRareInheritedData : public RefCounted<SameSizeAsStyleRareInheritedData> {
+    Color firstColor;
+    float firstFloat;
+    Color colors[5];
+    void* ownPtrs[1];
+    AtomicString atomicStrings[5];
+    void* refPtrs[2];
+    Length lengths[1];
+    float secondFloat;
+    unsigned m_bitfields[2];
+    short pagedMediaShorts[2];
+    unsigned unsigneds[1];
+    short hyphenationShorts[3];
+
+#if ENABLE(CSS_IMAGE_RESOLUTION)
+    float imageResolutionFloats;
+#endif
+
+#if ENABLE(TOUCH_EVENTS)
+    Color touchColors;
+#endif
+
+#if ENABLE(CSS_VARIABLES)
+    void* variableDataRefs[1];
+#endif
+};
+
+COMPILE_ASSERT(sizeof(StyleRareInheritedData) == sizeof(SameSizeAsStyleRareInheritedData), StyleRareInheritedData_should_bit_pack);
+
 StyleRareInheritedData::StyleRareInheritedData()
     : textStrokeWidth(RenderStyle::initialTextStrokeWidth())
     , indent(RenderStyle::initialTextIndent())
