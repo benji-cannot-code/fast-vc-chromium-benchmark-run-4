6478ac1ac92d933b10a4ef1f946a7363f2ecb862
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 17:42:18 +0000

    [CFNetwork] XMLHttpRequest incorrectly returns cached responses even when there is a Vary header field         https://bugs.webkit.org/show_bug.cgi?id=88925
            Reviewed by David Kilzer.
    
            Test: http/tests/cache/xhr-vary-header.html
    
            * platform/network/cf/ResourceHandleCFNet.cpp: (WebCore::willCacheResponse): Don't
            cache the response at all if there is a Vary header field. CFNetwork won't look at
            it if we allowed the response cached.
    
            * platform/network/mac/ResourceHandleMac.mm:
            (WebCore::ResourceHandle::createNSURLConnection): [NSURLResponse copy] is mutable
            in practice, but we should be nice, and use -mutableCopy.
            (-[WebCoreResourceHandleAsDelegate connection:willCacheResponse:]): Same fix as in
            CFNet code.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120339 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/mac/ResourceHandleMac.mm
+++ third_party/WebKit/Source/WebCore/platform/network/mac/ResourceHandleMac.mm
@@ -210,7 +210,7 @@ void ResourceHandle::createNSURLConnection(id delegate, bool shouldUseCredential
 
     NSURLRequest *nsRequest = firstRequest().nsURLRequest();
     if (!shouldContentSniff) {
-        NSMutableURLRequest *mutableRequest = [[nsRequest copy] autorelease];
+        NSMutableURLRequest *mutableRequest = [[nsRequest mutableCopy] autorelease];
         wkSetNSURLRequestShouldContentSniff(mutableRequest, NO);
         nsRequest = mutableRequest;
     }
@@ -886,6 +886,12 @@ String ResourceHandle::privateBrowsingStorageSessionIdentifierDefaultBase()
 #endif
 
     if (!m_handle || !m_handle->client())
+        return nil;
+
+    // Workaround for <rdar://problem/6300990> Caching does not respect Vary HTTP header.
+    // FIXME: WebCore cache has issues with Vary, too (bug 58797, bug 71509).
+    if ([[cachedResponse response] isKindOfClass:[NSHTTPURLResponse class]]
+        && [[(NSHTTPURLResponse *)[cachedResponse response] allHeaderFields] objectForKey:@"Vary"])
         return nil;
 
     NSCachedURLResponse *newResponse = m_handle->client()->willCacheResponse(m_handle, cachedResponse);
