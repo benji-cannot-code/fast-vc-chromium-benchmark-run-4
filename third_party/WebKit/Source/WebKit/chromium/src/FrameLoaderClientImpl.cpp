15ed62dd6845aa49f3fcaf4960379dd923ede861
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 07:36:17 +0000

    [chromium] Rename WebFrameClient::userAgent() to better reflect what it does https://bugs.webkit.org/show_bug.cgi?id=89028
    Patch by Dan Alcantara <dfalcantara@chromium.org> on 2012-06-14
    Reviewed by Adam Barth.
    
    Renames WebFrameClient::userAgent() to userAgentOverride() and makes it return
    the user agent override string directly.
    
    * public/WebFrameClient.h:
    (WebFrameClient):
    (WebKit::WebFrameClient::userAgentOverride):
    * src/FrameLoaderClientImpl.cpp:
    (WebKit::FrameLoaderClientImpl::userAgent):
    * tests/FrameLoaderClientImplTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120292 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -1384,8 +1384,8 @@ void FrameLoaderClientImpl::setTitle(const StringWithDirection& title, const KUR
 
 String FrameLoaderClientImpl::userAgent(const KURL& url)
 {
-    WebString override;
-    if (m_webFrame->client()->userAgent(WebURL(url), &override))
+    WebString override = m_webFrame->client()->userAgentOverride(WebURL(url));
+    if (!override.isEmpty())
         return override;
 
     return WebKit::Platform::current()->userAgent(url);
