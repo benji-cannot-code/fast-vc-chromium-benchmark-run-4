d3fce8d725b2dc077f1a9d6b720fa6d0ce5be325
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 11:53:43 +0000

    [Form] Replace InputNumber type to Decimal type and drop InputNumber https://bugs.webkit.org/show_bug.cgi?id=88746
    Reviewed by Kent Tamura.
    
    This patch removes InputNumber introduced by r119540 by replacing
    InputNumber type to Decimal type and changing convertDoubleToDecimal
    and convertDecimalToDouble to Decimal::fromDouble and
    Decimal::toDouble, respectively.
    
    No new tests. This patch doesn't change behavior.
    
    * html/BaseDateAndTimeInputType.cpp:
    (WebCore::BaseDateAndTimeInputType::setValueAsDecimal):
    (WebCore::BaseDateAndTimeInputType::defaultValueForStepUp):
    (WebCore::BaseDateAndTimeInputType::parseToNumber):
    (WebCore::BaseDateAndTimeInputType::serialize):
    (WebCore::BaseDateAndTimeInputType::serializeWithComponents):
    (WebCore::BaseDateAndTimeInputType::serializeWithMilliseconds):
    * html/BaseDateAndTimeInputType.h:
    (BaseDateAndTimeInputType):
    * html/DateInputType.cpp:
    (WebCore::DateInputType::createStepRange):
    * html/DateTimeInputType.cpp:
    (WebCore::DateTimeInputType::defaultValueForStepUp):
    (WebCore::DateTimeInputType::createStepRange):
    * html/DateTimeInputType.h:
    (DateTimeInputType):
    * html/DateTimeLocalInputType.cpp:
    (WebCore::DateTimeLocalInputType::createStepRange):
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::getAllowedValueStep):
    * html/HTMLInputElement.h:
    (HTMLInputElement):
    * html/InputType.cpp:
    (WebCore::InputType::setValueAsDouble):
    (WebCore::InputType::setValueAsDecimal):
    (WebCore::InputType::rangeUnderflow):
    (WebCore::InputType::rangeOverflow):
    (WebCore::InputType::defaultValueForStepUp):
    (WebCore::InputType::minimum):
    (WebCore::InputType::maximum):
    (WebCore::InputType::isInRange):
    (WebCore::InputType::isOutOfRange):
    (WebCore::InputType::stepMismatch):
    (WebCore::InputType::validationMessage):
    (WebCore::InputType::parseToNumber):
    (WebCore::InputType::parseToNumberOrNaN):
    (WebCore::InputType::serialize):
    (WebCore::InputType::applyStep):
    (WebCore::InputType::getAllowedValueStep):
    (WebCore::InputType::stepUpFromRenderer):
    * html/InputType.h:
    (InputType):
    * html/MonthInputType.cpp:
    (WebCore::MonthInputType::defaultValueForStepUp):
    (WebCore::MonthInputType::createStepRange):
    (WebCore::MonthInputType::parseToNumber):
    * html/MonthInputType.h:
    (MonthInputType):
    * html/NumberInputType.cpp:
    (WebCore::NumberInputType::setValueAsDecimal):
    (WebCore::NumberInputType::createStepRange):
    (WebCore::NumberInputType::parseToNumber):
    (WebCore::NumberInputType::serialize):
    * html/NumberInputType.h:
    (NumberInputType):
    * html/RangeInputType.cpp:
    (WebCore::ensureMaximum):
    (WebCore::RangeInputType::setValueAsDecimal):
    (WebCore::RangeInputType::createStepRange):
    (WebCore::RangeInputType::handleKeydownEvent):
    (WebCore::RangeInputType::parseToNumber):
    (WebCore::RangeInputType::serialize):
    (WebCore::RangeInputType::sanitizeValue):
    * html/RangeInputType.h:
    (RangeInputType):
    * html/StepRange.cpp:
    (WebCore::StepRange::StepRange):
    (WebCore::StepRange::acceptableError):
    (WebCore::StepRange::alignValueForStep):
    (WebCore::StepRange::clampValue):
    (WebCore::StepRange::parseStep):
    (WebCore::StepRange::roundByStep):
    (WebCore::StepRange::stepMismatch):
    * html/StepRange.h:
    (WebCore::StepRange::StepDescription::defaultValue):
    (StepRange):
    (WebCore::StepRange::maximum):
    (WebCore::StepRange::minimum):
    (WebCore::StepRange::step):
    (WebCore::StepRange::stepBase):
    (WebCore::StepRange::defaultValue):
    (WebCore::StepRange::proportionFromValue):
    (WebCore::StepRange::valueFromProportion):
    * html/TimeInputType.cpp:
    (WebCore::TimeInputType::defaultValueForStepUp):
    (WebCore::TimeInputType::createStepRange):
    * html/TimeInputType.h:
    (TimeInputType):
    * html/WeekInputType.cpp:
    (WebCore::WeekInputType::createStepRange):
    * html/shadow/CalendarPickerElement.cpp:
    (WebCore::CalendarPickerElement::writeDocument):
    * html/shadow/SliderThumbElement.cpp:
    (WebCore::sliderPosition):
    (WebCore::RenderSliderThumb::layout):
    (WebCore::SliderThumbElement::setPositionFromPoint):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120313 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/BaseDateAndTimeInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/BaseDateAndTimeInputType.cpp
@@ -66,7 +66,7 @@ double BaseDateAndTimeInputType::valueAsDouble() const
     return parseToDouble(element()->value());
 }
 
-void BaseDateAndTimeInputType::setValueAsInputNumber(const InputNumber& newValue, TextFieldEventBehavior eventBehavior, ExceptionCode&) const
+void BaseDateAndTimeInputType::setValueAsDecimal(const Decimal& newValue, TextFieldEventBehavior eventBehavior, ExceptionCode&) const
 {
     element()->setValue(serialize(newValue), eventBehavior);
 }
@@ -81,13 +81,13 @@ bool BaseDateAndTimeInputType::typeMismatch() const
     return typeMismatchFor(element()->value());
 }
 
-InputNumber BaseDateAndTimeInputType::defaultValueForStepUp() const
+Decimal BaseDateAndTimeInputType::defaultValueForStepUp() const
 {
     double ms = currentTimeMS();
     double utcOffset = calculateUTCOffset();
     double dstOffset = calculateDSTOffset(ms, utcOffset);
     int offset = static_cast<int>((utcOffset + dstOffset) / msPerMinute);
-    return convertDoubleToInputNumber(ms + (offset * msPerMinute));
+    return Decimal::fromDouble(ms + (offset * msPerMinute));
 }
 
 bool BaseDateAndTimeInputType::isSteppable() const
@@ -119,10 +119,10 @@ double BaseDateAndTimeInputType::parseToDouble(const String& source) const
     return msec;
 }
 
-InputNumber BaseDateAndTimeInputType::parseToNumber(const String& source, const InputNumber& defaultValue) const
+Decimal BaseDateAndTimeInputType::parseToNumber(const String& source, const Decimal& defaultValue) const
 {
     const double doubleValue = parseToDouble(source);
-    return isfinite(doubleValue) ? convertDoubleToInputNumber(doubleValue) : defaultValue;
+    return isfinite(doubleValue) ? Decimal::fromDouble(doubleValue) : defaultValue;
 }
 
 bool BaseDateAndTimeInputType::parseToDateComponents(const String& source, DateComponents* out) const
@@ -135,19 +135,19 @@ bool BaseDateAndTimeInputType::parseToDateComponents(const String& source, DateC
     return parseToDateComponentsInternal(source.characters(), source.length(), out);
 }
 
-String BaseDateAndTimeInputType::serialize(const InputNumber& value) const
+String BaseDateAndTimeInputType::serialize(const Decimal& value) const
 {
     if (!value.isFinite())
         return String();
     DateComponents date;
-    if (!setMillisecondToDateComponents(convertInputNumberToDouble(value), &date))
+    if (!setMillisecondToDateComponents(value.toDouble(), &date))
         return String();
     return serializeWithComponents(date);
 }
 
 String BaseDateAndTimeInputType::serializeWithComponents(const DateComponents& date) const
 {
-    InputNumber step;
+    Decimal step;
     if (!element()->getAllowedValueStep(&step))
         return date.toString();
     if (step.remainder(msecPerMinute).isZero())
@@ -159,7 +159,7 @@ String BaseDateAndTimeInputType::serializeWithComponents(const DateComponents& d
 
 String BaseDateAndTimeInputType::serializeWithMilliseconds(double value) const
 {
-    return serialize(convertDoubleToInputNumber(value));
+    return serialize(Decimal::fromDouble(value));
 }
 
 String BaseDateAndTimeInputType::localizeValue(const String& proposedValue) const
