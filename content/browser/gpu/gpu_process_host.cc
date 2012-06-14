0af3b561b4c8222497e0ce7680dddb6ad2b6b6bd
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 23:55:25 +0000

    Forward --enable-gpu-sandbox flag.
    Now that we added the flag, we need to forward it.
    Flag will be off by default, and is not included in the
    Chrome OS cmdline.
    
    Context: http://codereview.chromium.org/10534049/
    
    BUG=127664
    TEST=unit tests, and WebGL test websites on Lumpy.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10533161
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142281 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -735,6 +735,7 @@ bool GpuProcessHost::LaunchGpuProcess(const std::string& channel_id) {
     switches::kDisableGpuSandbox,
     switches::kReduceGpuSandbox,
     switches::kDisableSeccompFilterSandbox,
+    switches::kEnableGpuSandbox,
     switches::kDisableGpuSwitching,
     switches::kDisableGpuVsync,
     switches::kDisableGpuWatchdog,
