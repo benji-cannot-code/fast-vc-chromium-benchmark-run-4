e7c21bca37fec379631b20ee09457a88fbf2ebbe
falken@google.com <falken@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 04:04:19 +0000

    [Font Settings API]: Remove default charset from API.
    As suggested by Jungshik. Although charset is currently one of the settings in the built-in font options UI, it is not really a font setting and doesn't belong there or in the API.
    
    BUG=114148
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10535142
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -434,9 +434,6 @@ void ExtensionFunctionRegistry::ResetFunctions() {
   RegisterFunction<ClearMinimumFontSizeFunction>();
   RegisterFunction<GetMinimumFontSizeFunction>();
   RegisterFunction<SetMinimumFontSizeFunction>();
-  RegisterFunction<ClearDefaultCharacterSetFunction>();
-  RegisterFunction<GetDefaultCharacterSetFunction>();
-  RegisterFunction<SetDefaultCharacterSetFunction>();
 
   // ChromeAuth settings.
   RegisterFunction<SetCloudPrintCredentialsFunction>();
