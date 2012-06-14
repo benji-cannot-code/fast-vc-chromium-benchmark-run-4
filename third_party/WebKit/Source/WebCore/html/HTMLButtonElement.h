996e1a062888496019600bf3d9ffadaaed368de6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 08:31:25 +0000

    el.getAttribute('type') returns 'null' when setting el.type to null https://bugs.webkit.org/show_bug.cgi?id=89030
    Patch by Edaena Salinas Jasso <edaena@apple.com> on 2012-06-14
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    Test: fast/dom/HTMLButtonElement/change-type.html
    
    * html/HTMLButtonElement.cpp:
    (WebCore::HTMLButtonElement::setType): Changed parameter to be AtomicString to
    address an unrelated review comment from bug 14439 for slightly better performance.
    * html/HTMLButtonElement.h:
    (HTMLButtonElement): Changed parameter to be AtomicString to address an unrelated
    review comment from bug 14439 for slightly better performance.
    * html/HTMLButtonElement.idl: Specified TreatNullAs NullString for the type attribute.
    
    LayoutTests:
    
    * fast/dom/HTMLButtonElement/change-type-expected.txt: Updated the expected output
    so that it matches the null return type of getAttribute('type').
    * fast/dom/HTMLButtonElement/change-type.html: Change test of getAttribute('type')
    to be null instead of 'null'. Addressed and unrelated review comment from bug 14439
    about how 'undefined' is specified in the shouldBe function.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120296 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLButtonElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLButtonElement.h
@@ -33,7 +33,7 @@ class HTMLButtonElement : public HTMLFormControlElement {
 public:
     static PassRefPtr<HTMLButtonElement> create(const QualifiedName&, Document*, HTMLFormElement*);
 
-    void setType(const String&);
+    void setType(const AtomicString&);
     
     String value() const;
 
