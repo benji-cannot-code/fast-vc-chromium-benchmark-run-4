26056365762df502522e003ac8e42abc1661fbb7
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 18:08:45 +0000

    Fix a compilation error caused by some variables being unused under ASan on Mac. Review URL: https://chromiumcodereview.appspot.com/10536173
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142170 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_mac.mm
+++ base/process_util_mac.mm
@@ -879,6 +879,10 @@ void EnableTerminationOnOutOfMemory() {
         !g_old_valloc_purgeable && !g_old_realloc_purgeable &&
         !g_old_memalign_purgeable) << "Old allocators unexpectedly non-null";
 
+#if !defined(ADDRESS_SANITIZER)
+  // Don't do anything special on OOM for the malloc zones replaced by
+  // AddressSanitizer, as modifying or protecting them may not work correctly.
+
   // See http://trac.webkit.org/changeset/53362/trunk/Tools/DumpRenderTree/mac
   bool zone_allocators_protected = base::mac::IsOSLionOrLater();
 
@@ -887,9 +891,6 @@ void EnableTerminationOnOutOfMemory() {
   ChromeMallocZone* purgeable_zone =
       reinterpret_cast<ChromeMallocZone*>(GetPurgeableZone());
 
-#ifndef ADDRESS_SANITIZER
-  // Don't do anything special on OOM for the malloc zones replaced by
-  // AddressSanitizer, as modifying or protecting them may not work correctly.
   vm_address_t page_start_default = 0;
   vm_address_t page_start_purgeable = 0;
   vm_size_t len_default = 0;
