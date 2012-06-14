2b11a4d5237c7a5069fdfb5dd0e71691a165da58
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 01:56:18 +0000

    Revert 142048 - Move thread checking from autofill_country.cc to browser_process_impl.cc
    DCHECK(CalledOnValidThread()) is better than BrowserThread::CurrentlyOn(...)
    because it automatically supports unit test without requiring setting up
    specific threads.
    
    BUG=132643
    
    TEST=Run Autofill tests in debug build
    
    
    Review URL: https://chromiumcodereview.appspot.com/10545169
    
    TBR=wangxianzhu@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10533143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142066 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/address_unittest.cc
+++ chrome/browser/autofill/address_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -15,8 +15,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::BrowserThread;
 
+class AddressTest : public testing::Test {
+ public:
+  // In order to access the application locale -- which the tested functions do
+  // internally -- this test must run on the UI thread.
+  AddressTest() : ui_thread_(BrowserThread::UI, &message_loop_) {}
+
+ private:
+  MessageLoopForUI message_loop_;
+  content::TestBrowserThread ui_thread_;
+
+  DISALLOW_COPY_AND_ASSIGN(AddressTest);
+};
+
 // Test that the getters and setters for country code are working.
-TEST(AddressTest, CountryCode) {
+TEST_F(AddressTest, CountryCode) {
   Address address;
   EXPECT_EQ(std::string(), address.country_code());
 
@@ -28,7 +41,7 @@ TEST(AddressTest, CountryCode) {
 }
 
 // Test that country codes are properly decoded as country names.
-TEST(AddressTest, GetCountry) {
+TEST_F(AddressTest, GetCountry) {
   Address address;
   EXPECT_EQ(std::string(), address.country_code());
 
@@ -46,7 +59,7 @@ TEST(AddressTest, GetCountry) {
 }
 
 // Test that we properly detect country codes appropriate for each country.
-TEST(AddressTest, SetCountry) {
+TEST_F(AddressTest, SetCountry) {
   Address address;
   EXPECT_EQ(std::string(), address.country_code());
 
@@ -82,7 +95,7 @@ TEST(AddressTest, SetCountry) {
 }
 
 // Test that we properly match typed values to stored country data.
-TEST(AddressTest, IsCountry) {
+TEST_F(AddressTest, IsCountry) {
   Address address;
   address.set_country_code("US");
 
