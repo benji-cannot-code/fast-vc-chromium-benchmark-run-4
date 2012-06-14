9ec871f8ffb7adc3cd98570b2ea8252b23f707e6
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-14 09:31:22 +0000

    Web Inspector: clear execution live before the text edit. https://bugs.webkit.org/show_bug.cgi?id=89016
    Reviewed by Vsevolod Vlasov.
    
    Otherwise we can't remove them after committing the edit.
    
    * inspector/front-end/JavaScriptSourceFrame.js:
    (WebInspector.JavaScriptSourceFrame.prototype.beforeTextChanged):
    (WebInspector.JavaScriptSourceFrame.prototype.clearExecutionLine):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120299 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/JavaScriptSourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/JavaScriptSourceFrame.js
@@ -167,6 +167,7 @@ WebInspector.JavaScriptSourceFrame.prototype = {
                 // Re-adding decoration only.
                 this._addBreakpointDecoration(breakpointLocations[i].uiLocation.lineNumber, breakpoint.condition(), breakpoint.enabled(), true);
             }
+            this.clearExecutionLine();
         }
 
         WebInspector.SourceFrame.prototype.beforeTextChanged.call(this);
@@ -441,7 +442,7 @@ WebInspector.JavaScriptSourceFrame.prototype = {
 
     clearExecutionLine: function()
     {
-        if (this.loaded)
+        if (this.loaded && typeof this._executionLineNumber === "number")
             this.textViewer.removeDecoration(this._executionLineNumber, "webkit-execution-line");
         delete this._executionLineNumber;
     },
