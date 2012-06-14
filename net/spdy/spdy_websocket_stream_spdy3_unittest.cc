11074322aff69b880f42dd8414eb54c0a1ff027b
rvargas@chromium.org <rvargas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-14 00:32:09 +0000

    Disk cache: Make sure that references to IO buffers are released before invoking the callbacks.
    BUG=131272
    TEST=net_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10542068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142039 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_websocket_stream_spdy3_unittest.cc
+++ net/spdy/spdy_websocket_stream_spdy3_unittest.cc
@@ -178,7 +178,7 @@ class SpdyWebSocketStreamSpdy3Test : public testing::Test {
   }
 
   void DoSync(SpdyWebSocketStreamEvent* event) {
-    sync_callback_.SetResult(OK);
+    sync_callback_.callback().Run(OK);
   }
 
  protected:
