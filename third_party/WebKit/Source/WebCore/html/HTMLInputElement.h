2e56efffe8625572d0b58766eedbb599eea19956
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-15 06:38:25 +0000

    Support file extensions in HTMLInputElement::accept https://bugs.webkit.org/show_bug.cgi?id=88298
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    The WHATWG specification added file extensions supoprt for
    HTMLInputElement::accept recently.
    
    In the WebCore FileChooser interface, we rejected invalid MIME type
    strings in HTMLInputElement::acceptMIMETypes(), and platform file
    choosers don't expect that it containts file extensions. So, this patch
    introduces additional member "acceptFileExtensions" to
    FileChooserSettings, and it contains only file extensions specified by
    an accept attribute.
    
    * html/FileInputType.cpp:
    Sets HTMLInputElement::acceptFileExtensions() to
    FileChooserSettings::acceptFileExtensions.
    (WebCore::FileInputType::handleDOMActivateEvent):
    (WebCore::FileInputType::receiveDropForDirectoryUpload):
    * html/HTMLInputElement.cpp:
    (WebCore::isValidFileExtension): Added.
    (WebCore::parseAcceptAttribute):
    Common part for acceptMIMETyps() and acceptFileExtensions().
    (WebCore::HTMLInputElement::acceptMIMETypes):
    Uses parseAcceptAttribute() with isValidMIMEType().
    (WebCore::HTMLInputElement::acceptFileExtensions):
    Added. Uses parseAcceptAttribute() with isValidFileExtension().
    * html/HTMLInputElement.h:
    (HTMLInputElement): Add acceptFileExtensions().
    * platform/FileChooser.h:
    (FileChooserSettings): Add acceptFileExtensions and acceptTypes().
    * platform/FileChooser.cpp:
    (WebCore::FileChooserSettings::acceptTypes): Added.
    
    Source/WebKit/chromium:
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::runOpenPanel):
    Use FileChooserSettings::acceptTypes() to get both of MIME types and file extensions.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLInputElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLInputElement.h
@@ -198,6 +198,7 @@ public:
     void setDefaultValue(const String&);
 
     Vector<String> acceptMIMETypes();
+    Vector<String> acceptFileExtensions();
     String accept() const;
     String alt() const;
 
