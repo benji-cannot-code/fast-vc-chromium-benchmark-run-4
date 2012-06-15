0f29564afc139b6b2680da659b7308455e351761
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 01:09:56 +0000

    mac: Make tab close buttons look good in HiDPI
    BUG=none
    TEST=close buttons look good
    
    Review URL: https://chromiumcodereview.appspot.com/10542172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142304 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/molokocacao/NSBezierPath+MCAdditions.m
+++ third_party/molokocacao/NSBezierPath+MCAdditions.m
@@ -93,7 +93,7 @@ static void CGPathCallback(void *info, const CGPathElement *element)
 #endif  // MCBEZIER_USE_PRIVATE_FUNCTION
 }
 
-- (void)fillWithInnerShadow:(NSShadow *)shadow
+- (void)fillWithInnerShadow:(NSShadow *)shadow scale:(float)scale
 {
 	[NSGraphicsContext saveGraphicsState];
 	
@@ -101,7 +101,9 @@ static void CGPathCallback(void *info, const CGPathElement *element)
 	NSSize originalOffset = offset;
 	CGFloat radius = shadow.shadowBlurRadius;
 	NSRect bounds = NSInsetRect(self.bounds, -(ABS(offset.width) + radius), -(ABS(offset.height) + radius));
-	offset.height += bounds.size.height;
+
+	// The context's user transform isn't automatically applied to shadow offsets.
+	offset.height += scale * bounds.size.height;
 	shadow.shadowOffset = offset;
 	NSAffineTransform *transform = [NSAffineTransform transform];
 	if ([[NSGraphicsContext currentContext] isFlipped])
