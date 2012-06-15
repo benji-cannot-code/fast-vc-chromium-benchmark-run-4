a5022aac383a35cb93644715bfed96e4b86c7878
strobe@google.com <strobe@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 23:33:28 +0000

    Allow any AVC level in Media Source ISO BMFF codec strings.
    BUG=129072
    TEST=ChunkDemuxerTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10544166
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142520 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/chunk_demuxer.cc
+++ media/filters/chunk_demuxer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
+#include "base/string_util.h"
 #include "media/base/audio_decoder_config.h"
 #include "media/base/stream_parser_buffer.h"
 #include "media/base/video_decoder_config.h"
@@ -18,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 struct CodecInfo {
-  const char* name;
+  const char* pattern;
   DemuxerStream::Type type;
 };
 
@@ -33,10 +34,8 @@ struct SupportedTypeInfo {
 static const CodecInfo kVP8CodecInfo = { "vp8", DemuxerStream::VIDEO };
 static const CodecInfo kVorbisCodecInfo = { "vorbis", DemuxerStream::AUDIO };
 
-// TODO(strobe): Perform matching against supported profiles and levels, or
-// simply accept codecs that match a prefix.
-static const CodecInfo kH264CodecInfo = { "avc1.4D4041", DemuxerStream::VIDEO };
-static const CodecInfo kAACCodecInfo = { "mp4a.40.2", DemuxerStream::AUDIO };
+static const CodecInfo kH264CodecInfo = { "avc1.*", DemuxerStream::VIDEO };
+static const CodecInfo kAACCodecInfo = { "mp4a.40.*", DemuxerStream::AUDIO };
 
 static const CodecInfo* kVideoWebMCodecs[] = {
   &kVP8CodecInfo,
@@ -104,7 +103,7 @@ static bool IsSupported(const std::string& type,
         DemuxerStream::Type codec_type = DemuxerStream::UNKNOWN;
 
         for (int k = 0; type_info.codecs[k]; ++k) {
-          if (codecs[j] == type_info.codecs[k]->name) {
+          if (MatchPattern(codecs[j], type_info.codecs[k]->pattern)) {
             found_codec = true;
             codec_type = type_info.codecs[k]->type;
             break;
