14db3e5dd0a2fbd758b06cdaa4741b2290af2ad6
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 16:05:50 +0000

    Retry: add test for RemoteFileSystemOperation::Write() and fix revealed bugs.
    * Add a browser test for remote file writing.
    * Fix a confusion of remote and local path in CloseWritableSnapshotFile.
    * Fix a wrong ownership relation between FileWriterDelegate and RemoteFileStreamWriter.
    * Made CommitDirtyInCache update entry metadata.
    
    BUG=132051, 127097
    TEST=browser_tests --gtest_filter='*RemoteFileSystem*'
    TEST=unit_tests --gtest_filter='*GData*'
    
    First Commit: http://src.chromium.org/viewvc/chrome?view=rev&revision=142373
    Reverted:  http://src.chromium.org/viewvc/chrome?view=rev&revision=142380
    
    The first commit broke GDataFileSystemTest::OpenAndCloseFile. Changing
    the behavior of CloseFile began to require some more IO operations.
    
    Review URL: https://chromiumcodereview.appspot.com/10562003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_unittest.cc
@@ -1221,10 +1221,12 @@ class GDataFileSystemTest : public testing::Test {
                                   const FilePath& file_path) {
       last_error_ = error;
       opened_file_path_ = file_path;
+      MessageLoop::current()->Quit();
     }
 
     virtual void CloseFileCallback(base::PlatformFileError error) {
       last_error_ = error;
+      MessageLoop::current()->Quit();
     }
 
     virtual void GetEntryInfoCallback(
@@ -3828,10 +3830,7 @@ TEST_F(GDataFileSystemTest, OpenAndCloseFile) {
 
   // Open kFileInRoot ("drive/File 1.txt").
   file_system_->OpenFile(kFileInRoot, callback);
-  RunAllPendingForIO();  // Try to get from the cache.
-  RunAllPendingForIO();  // Check if we have space before downloading.
-  RunAllPendingForIO();  // Check if we have space after downloading.
-  RunAllPendingForIO();  // Mark dirty in cache.
+  message_loop_.Run();
 
   // Verify that the file was properly opened.
   EXPECT_EQ(base::PLATFORM_FILE_OK, callback_helper_->last_error_);
@@ -3850,7 +3849,7 @@ TEST_F(GDataFileSystemTest, OpenAndCloseFile) {
 
   // Close kFileInRoot ("drive/File 1.txt").
   file_system_->CloseFile(kFileInRoot, close_file_callback);
-  RunAllPendingForIO();  // Commit dirty in cache.
+  message_loop_.Run();
 
   // Verify that the file was properly closed.
   EXPECT_EQ(base::PLATFORM_FILE_OK, callback_helper_->last_error_);
