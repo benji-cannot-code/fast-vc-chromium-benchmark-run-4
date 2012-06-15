4c712e8e6ff7e276d4ef021d2912058dd2513394
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-15 02:50:55 +0000

    Unreviewed, rolling out r120384. http://trac.webkit.org/changeset/120384 https://bugs.webkit.org/show_bug.cgi?id=89157
    breaks create-blob-url-from-data-url.html (Requested by
    morrita on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-06-14
    
    Source/WebCore:
    
    * fileapi/Blob.cpp:
    (WebCore::Blob::Blob):
    * fileapi/BlobURL.cpp:
    (WebCore::BlobURL::createBlobURL):
    * fileapi/BlobURL.h:
    (BlobURL):
    * fileapi/FileReaderLoader.cpp:
    (WebCore::FileReaderLoader::start):
    * fileapi/ThreadableBlobRegistry.cpp:
    (WebCore):
    (WebCore::ThreadableBlobRegistry::registerBlobURL):
    (WebCore::ThreadableBlobRegistry::unregisterBlobURL):
    * fileapi/ThreadableBlobRegistry.h:
    (WebCore):
    (ThreadableBlobRegistry):
    * html/DOMURL.cpp:
    (WebCore::DOMURL::createObjectURL):
    * page/SecurityOrigin.cpp:
    (WebCore::SecurityOrigin::create):
    
    LayoutTests:
    
    * fast/files/file-reader-file-url-expected.txt: Removed.
    * fast/files/file-reader-file-url.html: Removed.
    * fast/files/resources/file-reader-file-url-iframe.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/SecurityOrigin.cpp
+++ third_party/WebKit/Source/WebCore/page/SecurityOrigin.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "KURL.h"
 #include "SchemeRegistry.h"
 #include "SecurityPolicy.h"
-#include "ThreadableBlobRegistry.h"
 #include <wtf/MainThread.h>
 #include <wtf/StdLibExtras.h>
 #include <wtf/text/StringBuilder.h>
@@ -89,15 +88,6 @@ static KURL extractInnerURL(const KURL& url)
     // FIXME: Update this callsite to use the innerURL member function when
     // we finish implementing it.
     return KURL(ParsedURLString, decodeURLEscapeSequences(url.path()));
-}
-
-static PassRefPtr<SecurityOrigin> getCachedOrigin(const KURL& url)
-{
-#if ENABLE(BLOB)
-    if (url.protocolIs("blob"))
-        return ThreadableBlobRegistry::cachedOrigin(url);
-#endif
-    return 0;
 }
 
 static bool shouldTreatAsUniqueOrigin(const KURL& url)
@@ -182,10 +172,6 @@ SecurityOrigin::SecurityOrigin(const SecurityOrigin* other)
 
 PassRefPtr<SecurityOrigin> SecurityOrigin::create(const KURL& url)
 {
-    RefPtr<SecurityOrigin> cachedOrigin = getCachedOrigin(url);
-    if (cachedOrigin.get())
-        return cachedOrigin;
-
     if (shouldTreatAsUniqueOrigin(url)) {
         RefPtr<SecurityOrigin> origin = adoptRef(new SecurityOrigin());
 
