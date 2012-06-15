adc7977b14f0af386f9c4e12102c6cca7b75ff9e
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 03:48:38 +0000

    Add placeholder methods around displays, for option UI.
    Following methods are added to OutputConfigurator interface:
    - GetDisplayMode(): used to show the options UI.
    - SetDisplayMode(State): should be called when the user changes display mode in the options UI.
    
    Following field is added to gfx::Display:
    - name: to denote which monitors is which, in the option UI.
      see the mock image of crbug.com/130385
    
    BUG=130385
    TEST=compilation succeeded.  Make sure Ctrl-F4 behavior is not changed.
    
    Review URL: https://chromiumcodereview.appspot.com/10539129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142336 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/monitor/output_configurator.cc
+++ chromeos/monitor/output_configurator.cc
@@ -631,13 +631,6 @@ bool OutputConfigurator::CycleDisplayMode() {
   VLOG(1) << "CycleDisplayMode";
   bool did_change = false;
   if (is_running_on_chrome_os_) {
-    Display* display = base::MessagePumpAuraX11::GetDefaultXDisplay();
-    CHECK(display != NULL);
-    XGrabServer(display);
-    Window window = DefaultRootWindow(display);
-    XRRScreenResources* screen = XRRGetScreenResources(display, window);
-    CHECK(screen != NULL);
-
     // Rules:
     // - if there are 0 or 1 displays, do nothing and return false.
     // - use y-coord of CRTCs to determine if we are mirror, primary-first, or
@@ -660,16 +653,8 @@ bool OutputConfigurator::CycleDisplayMode() {
         // Do nothing - we aren't in a mode which we can rotate.
         break;
     }
-    if (STATE_INVALID != new_state) {
-      UpdateCacheAndXrandrToState(display,
-                                  screen,
-                                  window,
-                                  new_state);
-      did_change = true;
-    }
-
-    XRRFreeScreenResources(screen);
-    XUngrabServer(display);
+    if (STATE_INVALID != new_state)
+      did_change = SetDisplayMode(new_state);
   }
   return did_change;
 }
@@ -731,6 +716,28 @@ bool OutputConfigurator::ScreenPowerSet(bool power_on, bool all_displays) {
   return success;
 }
 
+bool OutputConfigurator::SetDisplayMode(State new_state) {
+  if (output_state_ == STATE_INVALID ||
+      output_state_ == STATE_HEADLESS ||
+      output_state_ == STATE_SINGLE)
+    return false;
+
+  Display* display = base::MessagePumpAuraX11::GetDefaultXDisplay();
+  CHECK(display != NULL);
+  XGrabServer(display);
+  Window window = DefaultRootWindow(display);
+  XRRScreenResources* screen = XRRGetScreenResources(display, window);
+  CHECK(screen != NULL);
+
+  UpdateCacheAndXrandrToState(display,
+                              screen,
+                              window,
+                              new_state);
+  XRRFreeScreenResources(screen);
+  XUngrabServer(display);
+  return true;
+}
+
 bool OutputConfigurator::Dispatch(const base::NativeEvent& event) {
   // Ignore this event if the Xrandr extension isn't supported.
   if (is_running_on_chrome_os_ &&
@@ -784,4 +791,3 @@ void OutputConfigurator::CheckIsProjectingAndNotify() {
 }
 
 }  // namespace chromeos
-
