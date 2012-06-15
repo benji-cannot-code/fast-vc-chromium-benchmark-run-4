72de52b00adc5c0f7427a9cc3b5442e60d3fad57
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-15 07:19:53 +0000

    Add fullscreen button to Chrome video controls for video. https://bugs.webkit.org/show_bug.cgi?id=88818
    Patch by Silvia Pfeiffer <silviapf@chromium.org> on 2012-06-15
    Reviewed by Eric Carlson.
    
    No new tests, final patch will contain the rebaselined tests.
    
    The Chrome video controls are receiving a visual update.
    This patch includes a fullscreen button for video elements and the rendering of the controls
    in fullscreen including hiding them after 2 seconds when the mouse is out of the controls
    and not moved.
    
    * css/fullscreen.css:
    (video:-webkit-full-screen, audio:-webkit-full-screen):
    Add audio to the default fullscreen styling rules.
    * css/mediaControlsChromium.css:
    (video:-webkit-full-page-media::-webkit-media-controls-panel):
    Align controls to the bottom of the fullscreen page.
    (audio::-webkit-media-controls-fullscreen-button, video::-webkit-media-controls-fullscreen-button):
    Include styling for the fullscreen button.
    * html/shadow/MediaControlRootElementChromium.cpp:
    (WebCore):
    Add a constant for when to hide the controls in fullscreen.
    (WebCore::MediaControlRootElementChromium::MediaControlRootElementChromium):
    Add member fields for fullscreen button, hiding timer and tracking of whether we are in fullscreen.
    (WebCore::MediaControlRootElementChromium::create):
    Add fullscreen button element to the visual layout.
    (WebCore::MediaControlRootElementChromium::setMediaController):
    Add fullscreen button element to the media controller.
    (WebCore::MediaControlRootElementChromium::reset):
    Show the fullscreen button if the controller supports fullscreen.
    (WebCore::MediaControlRootElementChromium::playbackStarted):
    Start the timer to hide the controls in fullscreen.
    (WebCore::MediaControlRootElementChromium::playbackStopped):
    Don't hide the controls when the video is paused in fullscreen.
    (WebCore::MediaControlRootElementChromium::reportedError):
    Hide the fullscreen button when we hit an error.
    (WebCore::MediaControlRootElementChromium::defaultEventHandler):
    Add logic for mouse events in fullscreen to start/stop the hiding timer.
    (WebCore::MediaControlRootElementChromium::startHideFullscreenControlsTimer):
    Start the fullscreen hiding timer.
    (WebCore::MediaControlRootElementChromium::hideFullscreenControlsTimerFired):
    Timer fired: hide the video controls in fullscreen.
    (WebCore::MediaControlRootElementChromium::stopHideFullscreenControlsTimer):
    Reset the fullscreen hiding timer.
    (WebCore::MediaControlRootElementChromium::enteredFullscreen):
    Add logic to enter fullscreen.
    (WebCore::MediaControlRootElementChromium::exitedFullscreen):
    Add logic to exit fullscreen.
    * html/shadow/MediaControlRootElementChromium.h:
    (MediaControlRootElementChromium):
    Add declaration of member functions and fields for fullscreen.
    * rendering/RenderMediaControlsChromium.cpp:
    (WebCore::paintMediaFullscreenButton):
    Use the new image for the fullscreen button.
    * rendering/RenderThemeChromiumMac.h:
    (RenderThemeChromiumMac):
    Declare the fullscreen painting function for Chrome Mac.
    * rendering/RenderThemeChromiumMac.mm:
    (WebCore::RenderThemeChromiumMac::paintMediaFullscreenButton):
    Hook up the fullscreen paining function for Chrome Mac.
    * rendering/RenderThemeChromiumSkia.cpp:
    (WebCore::RenderThemeChromiumSkia::paintMediaFullscreenButton):
    Hook up the fullscreen paining function for Chrome Skia.
    * rendering/RenderThemeChromiumSkia.h:
    (RenderThemeChromiumSkia):
    Declare the fullscreen painting function for Chrome Skia.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElementChromium.h
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElementChromium.h
@@ -127,6 +127,9 @@ private:
     MediaControlRootElementChromium(Document*);
 
     virtual void defaultEventHandler(Event*);
+    void hideFullscreenControlsTimerFired(Timer<MediaControlRootElementChromium>*);
+    void startHideFullscreenControlsTimer();
+    void stopHideFullscreenControlsTimer();
 
     virtual const AtomicString& shadowPseudoId() const;
 
@@ -151,7 +154,9 @@ private:
 #endif
 
     bool m_opaque;
+    Timer<MediaControlRootElementChromium> m_hideFullscreenControlsTimer;
     bool m_isMouseOverControls;
+    bool m_isFullscreen;
 };
 
 }
