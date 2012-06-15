03f3e9b12f61a8a706c2f53813029a2668cb7f48
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 09:44:55 +0000

    Clean up for GDataOperationRegistry.
    Fixing several style issues.
    
    BUG=none
    TEST=unit_tests --gtest_filter='*GData*'
    
    Review URL: https://chromiumcodereview.appspot.com/10453022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142366 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_operation_registry.cc
+++ chrome/browser/chromeos/gdata/gdata_operation_registry.cc
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/gdata/gdata_operation_registry.h"
 
-#include <sstream>
-
+#include "base/string_number_conversions.h"
 #include "content/public/browser/browser_thread.h"
 
 using content::BrowserThread;
@@ -50,16 +49,21 @@ GDataOperationRegistry::ProgressStatus::ProgressStatus(OperationType type,
       progress_total(-1) {
 }
 
-std::string GDataOperationRegistry::ProgressStatus::ToString() const {
-  std::stringstream stream;
-  stream << "id=" << operation_id
-         << "type="
-         << GDataOperationRegistry::OperationTypeToString(operation_type)
-         << "path=" << file_path.AsUTF8Unsafe()
-         << "state=" << OperationTransferStateToString(transfer_state)
-         << ", progress=" << progress_current
-         << "/ " << progress_total;
-  return stream.str();
+std::string GDataOperationRegistry::ProgressStatus::DebugString() const {
+  std::string str;
+  str += "id=";
+  str += base::IntToString(operation_id);
+  str += " type=";
+  str += OperationTypeToString(operation_type);
+  str += " path=";
+  str += file_path.AsUTF8Unsafe();
+  str += " state=";
+  str += OperationTransferStateToString(transfer_state);
+  str += " progress=";
+  str += base::Int64ToString(progress_current);
+  str += "/";
+  str += base::Int64ToString(progress_total);
+  return str;
 }
 
 GDataOperationRegistry::Operation::Operation(GDataOperationRegistry* registry)
@@ -198,8 +202,8 @@ void GDataOperationRegistry::OnOperationProgress(OperationID id) {
   Operation* operation = in_flight_operations_.Lookup(id);
   DCHECK(operation);
 
-  DVLOG(1) << "GDataOperation[" << id << "] " <<
-      operation->progress_status().ToString();
+  DVLOG(1) << "GDataOperation[" << id << "] "
+           << operation->progress_status().DebugString();
   if (IsFileTransferOperation(operation)) {
     FOR_EACH_OBSERVER(Observer, observer_list_,
                       OnProgressUpdate(GetProgressStatusList()));
