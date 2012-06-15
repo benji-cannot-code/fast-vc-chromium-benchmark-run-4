b9c09413e20070079b30bc2def8e495f846e4278
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 19:40:23 +0000

    Show source of proxy settings in net-internals.
    The source of proxy settings is now recorded in the NetLog. Display the source in the Proxy tab and in PROXY_CONFIG_CHANGED events.
    
    BUG=none
    TEST=browser_tests --gtest_filter=NetInternalsTest.netInternalsLogViewPainterPrintAsText
    
    Review URL: https://chromiumcodereview.appspot.com/10535176
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/net_internals/log_view_painter.js
+++ chrome/test/data/webui/net_internals/log_view_painter.js
@@ -1168,7 +1168,8 @@ function painterTestProxyConfig() {
              '<local>'
            ],
            'pac_url': 'https://config/wpad.dat',
-           'single_proxy': 'cache-proxy:3128'
+           'single_proxy': 'cache-proxy:3128',
+           'source': 'SYSTEM'
          },
          'old_config': {
            'auto_detect': true
@@ -1195,7 +1196,8 @@ function painterTestProxyConfig() {
     '                                   Bypass list: \n' +
     '                                     *.local\n' +
     '                                     foo\n' +
-    '                                     <local>';
+    '                                     <local>\n' +
+    '                               Source: SYSTEM';
 
   return testCase;
 }
