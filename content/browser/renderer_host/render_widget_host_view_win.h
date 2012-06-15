25b48710aef28fc7954f53f9bf5ca1cb104484d9
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 02:57:25 +0000

    Windows: Do not fast tab switch if the tab has resized.
    This looks bad visually because when the tab is initially selected, one frame of the page rendered at a different size is presented.
    
    Also, got rid of the compositor host window on Vista and later. Still needed for XP unfortunately.
    
    BUG=131736
    
    Review URL: https://chromiumcodereview.appspot.com/10540136
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142331 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_win.h
+++ content/browser/renderer_host/render_widget_host_view_win.h
@@ -90,7 +90,7 @@ class RenderWidgetHostViewWin
 
   CONTENT_EXPORT void CreateWnd(HWND parent);
 
-  void ScheduleComposite();
+  void AcceleratedPaint(HDC dc);
 
   DECLARE_WND_CLASS_EX(kRenderWidgetHostHWNDClass, CS_DBLCLKS, 0);
 
