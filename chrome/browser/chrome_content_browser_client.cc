27b15e9606eff652f47af74b7875dd7105d93fdb
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 22:06:07 +0000

    Restore PPAPI Flash in-process file access.
    This behaviour is under control of the --ppapi-flash-in-process flag.  There
    will be degraded protection of the pepper flash data directories from
    compromised renderers under this option (hence the flag).
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=142449
    
    Review URL: https://chromiumcodereview.appspot.com/10533137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1494,7 +1494,7 @@ std::string ChromeContentBrowserClient::GetDefaultDownloadName() {
   return l10n_util::GetStringUTF8(IDS_DEFAULT_DOWNLOAD_FILENAME);
 }
 
-bool ChromeContentBrowserClient::AllowSocketAPI(
+bool ChromeContentBrowserClient::AllowPepperSocketAPI(
     content::BrowserContext* browser_context, const GURL& url) {
   if (!url.is_valid())
     return false;
@@ -1535,6 +1535,11 @@ bool ChromeContentBrowserClient::AllowSocketAPI(
     return true;
 
   return false;
+}
+
+bool ChromeContentBrowserClient::AllowPepperPrivateFileAPI() {
+  return CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kPpapiFlashInProcess);
 }
 
 #if defined(OS_POSIX) && !defined(OS_MACOSX)
