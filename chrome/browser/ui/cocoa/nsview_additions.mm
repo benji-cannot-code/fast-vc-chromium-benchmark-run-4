923ff942217b3f181819d155cf77ae25e9130609
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 19:41:26 +0000

    Cleaned up pixel alignment code.
    BUG=132984
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10557009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142450 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/nsview_additions.mm
+++ chrome/browser/ui/cocoa/nsview_additions.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 @implementation NSView (ChromeAdditions)
 
 - (CGFloat)cr_lineWidth {
-  return 1.0 / [self convertSizeToBase:NSMakeSize(1, 1)].width;
+  // NOTE: This is a correct use of base coordinate system, to perform pixel
+  // alignment.
+  return [self convertSizeFromBase:NSMakeSize(1, 1)].width;
 }
 
 - (BOOL)cr_isMouseInView {
