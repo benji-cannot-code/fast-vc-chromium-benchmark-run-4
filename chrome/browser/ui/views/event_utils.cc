ae93fac4a686f19066995157971663121bf7afb5
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 15:48:56 +0000

    Only treat left and middle mouse buttons as possible disposition events.
    BUG=None
    TEST=Right click on a bookmark in the bookmark bar should not navigate to the bookmark.
    
    Review URL: https://chromiumcodereview.appspot.com/10558004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/event_utils.cc
+++ chrome/browser/ui/views/event_utils.cc
@@ -11,7 +11,7 @@ namespace event_utils {
 
 bool IsPossibleDispositionEvent(const views::Event& event) {
   return event.IsMouseEvent() && (event.flags() &
-             (ui::EF_LEFT_MOUSE_BUTTON | ui::EF_RIGHT_MOUSE_BUTTON));
+             (ui::EF_LEFT_MOUSE_BUTTON | ui::EF_MIDDLE_MOUSE_BUTTON));
 }
 
 }  // namespace event_utils
