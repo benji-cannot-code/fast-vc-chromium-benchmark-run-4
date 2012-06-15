e9e5dad1bce21798e3cf8af193512f8fd61fc168
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 07:38:23 +0000

    Add Extended Desktop mode behind --ash-extended-desktop flag.  A lot of stuff still doesn't work but you can create and use a window on 2nd display.  MonitorController now owns all root windows.
    BUG=123160
    TEST=see http://codereview.chromium.org/10536187/
    
    Review URL: https://chromiumcodereview.appspot.com/10535149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142350 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_manager.cc
+++ ash/wm/workspace/workspace_manager.cc
@@ -134,7 +134,8 @@ void WorkspaceManager::SetActiveWorkspaceByWindow(aura::Window* window) {
 }
 
 void WorkspaceManager::UpdateShelfVisibility() {
-  shelf_->UpdateVisibilityState();
+  if (shelf_)
+    shelf_->UpdateVisibilityState();
 }
 
 WorkspaceManager::WindowState WorkspaceManager::GetWindowState() {
