e637f03624795466730019657aaafd50ca5ab2d7
rltoscano@google.com <rltoscano@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-15 20:19:14 +0000

    Reduces height of search widget.
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10532165
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142464 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/search/destination_list.js
+++ chrome/browser/resources/print_preview/search/destination_list.js
@@ -128,7 +128,7 @@ cr.define('print_preview', function() {
    * @const
    * @private
    */
-  DestinationList.SHORT_LIST_SIZE_ = 5;
+  DestinationList.SHORT_LIST_SIZE_ = 4;
 
   DestinationList.prototype = {
     __proto__: print_preview.Component.prototype,
