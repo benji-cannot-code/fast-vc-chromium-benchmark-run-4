d248eef31bd707d6b45e1f0298f127f965cb8d3c
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-15 07:13:35 +0000

    Unreviewed, rolling out r110340. http://trac.webkit.org/changeset/110340 https://bugs.webkit.org/show_bug.cgi?id=88749
    Source/WebCore:
    
    r110340 added a broken implementation of
    HTMLSelectElement::selectedOptions. This roll out removes
    HTMLSelectElement::selectedOptions.
    
    * html/CollectionType.h:
    * html/HTMLCollection.cpp:
    (WebCore::HTMLCollection::shouldIncludeChildren):
    (WebCore::HTMLCollection::isAcceptableElement):
    * html/HTMLSelectElement.cpp:
    * html/HTMLSelectElement.h:
    (HTMLSelectElement):
    * html/HTMLSelectElement.idl:
    
    LayoutTests:
    
    * fast/dom/HTMLSelectElement/select-selectedOptions-expected.txt: Removed.
    * fast/dom/HTMLSelectElement/select-selectedOptions.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLCollection.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLCollection.cpp
@@ -62,7 +62,6 @@ bool HTMLCollection::shouldIncludeChildren(CollectionType type)
     case MapAreas:
     case OtherCollection:
     case SelectOptions:
-    case SelectedOptions:
     case DataListOptions:
     case WindowNamedItems:
 #if ENABLE(MICRODATA)
@@ -120,13 +119,6 @@ inline bool HTMLCollection::isAcceptableElement(Element* element) const
         return element->hasLocalName(trTag);
     case SelectOptions:
         return element->hasLocalName(optionTag);
-    case SelectedOptions:
-        if (element->hasLocalName(optionTag)) {
-            HTMLOptionElement* option = static_cast<HTMLOptionElement*>(element);
-            if (option->selected())
-                return true;
-        }
-        return false;
     case DataListOptions:
         if (element->hasLocalName(optionTag)) {
             HTMLOptionElement* option = static_cast<HTMLOptionElement*>(element);
--- third_party/WebKit/Source/WebCore/html/HTMLSelectElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLSelectElement.h
@@ -65,7 +65,6 @@ public:
     void setValue(const String&);
 
     HTMLOptionsCollection* options();
-    HTMLCollection* selectedOptions();
 
     void optionElementChildrenChanged();
 
@@ -181,7 +180,6 @@ private:
     virtual void childrenChanged(bool changedByParser = false, Node* beforeChange = 0, Node* afterChange = 0, int childCountDelta = 0);
 
     OwnPtr<HTMLOptionsCollection> m_optionsCollection;
-    OwnPtr<HTMLCollection> m_selectedOptionsCollection;
 
     // m_listItems contains HTMLOptionElement, HTMLOptGroupElement, and HTMLHRElement objects.
     mutable Vector<HTMLElement*> m_listItems;
