22668e54970aadf6c7e22b80a2b712ff8f1defb9
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-16 00:44:59 +0000

    Detect if user closes the pref-pane without disabling Chromoting
    BUG=129616
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10566013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142544 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/me2me_preference_pane.mm
+++ remoting/host/me2me_preference_pane.mm
@@ -328,8 +328,8 @@ std::string JsonHostConfig::GetSerializedData() const {
   [service_status_timer_ invalidate];
   [service_status_timer_ release];
   service_status_timer_ = nil;
-  if (have_new_config_)
-    [self notifyPlugin:kUpdateFailedNotificationName];
+
+  [self notifyPlugin:kUpdateFailedNotificationName];
 }
 
 - (void)applyConfiguration:(id)sender
@@ -754,8 +754,6 @@ std::string JsonHostConfig::GetSerializedData() const {
       NSLog(@"Failed to get path of configuration data.");
       return;
     }
-    if (access(file.c_str(), F_OK) != 0)
-      return;
 
     remove(file.c_str());
     [self notifyPlugin:kUpdateFailedNotificationName];
