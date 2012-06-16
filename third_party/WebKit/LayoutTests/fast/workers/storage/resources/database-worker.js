7cd2ea7b5ade341a261f86264fb6697e8f73444d
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-16 03:54:33 +0000

    Use testRunner instead of layoutTestController in fast/workers/storage tests https://bugs.webkit.org/show_bug.cgi?id=89271
    Reviewed by Adam Barth.
    
    * fast/workers/storage/change-version-sync.html:
    * fast/workers/storage/interrupt-database-sync.html-disabled:
    * fast/workers/storage/interrupt-database.html:
    * fast/workers/storage/resources/database-worker.js:
    (onmessage):
    * fast/workers/storage/resources/multiple-transactions-sync.js:
    (checkCompletion):
    * fast/workers/storage/resources/test-inputs-common.js:
    (finishTest):
    * fast/workers/storage/use-same-database-in-page-and-workers.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120522 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/workers/storage/resources/database-worker.js
+++ third_party/WebKit/LayoutTests/fast/workers/storage/resources/database-worker.js
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var window = {
-    layoutTestController: {
+    testRunner: {
         notifyDone: function() { postMessage("notifyDone"); }
     }
 };
-var layoutTestController = window.layoutTestController;
+var testRunner = window.testRunner;
 
 function log(s) {
     postMessage("log:" + s);
@@ -23,7 +23,7 @@ onmessage = function(event) {
             log("Received unexpected message: " + event.data);
     } catch (ex) {
         log("Worker caught exception: " + ex);
-        window.layoutTestController.notifyDone();
+        window.testRunner.notifyDone();
     }
 };
 
--- third_party/WebKit/LayoutTests/fast/workers/storage/resources/multiple-transactions-sync.js
+++ third_party/WebKit/LayoutTests/fast/workers/storage/resources/multiple-transactions-sync.js
@@ -3,8 +3,8 @@ var complete = 0;
 
 function checkCompletion()
 {
-    if (++complete == 2 && window.layoutTestController)
-        layoutTestController.notifyDone();
+    if (++complete == 2 && window.testRunner)
+        testRunner.notifyDone();
 }
 
 function runTest()
