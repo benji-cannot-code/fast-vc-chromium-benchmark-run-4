3ed52341a92b6f1e89bca1d7e0408b759291b28f
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-16 00:54:03 +0000

    Changes to aura or the gtk2 shim should trigger linux_aura tryjobs.
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10556021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/libgtk2ui/PRESUBMIT.py
+++ chrome/browser/ui/libgtk2ui/PRESUBMIT.py
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
@@ -10,4 +12,4 @@ for more details on the presubmit API built into gcl.
 """
 
 def GetPreferredTrySlaves():
-  return ['linux_chromeos', 'linux_aura']
+  return ['linux_aura']
