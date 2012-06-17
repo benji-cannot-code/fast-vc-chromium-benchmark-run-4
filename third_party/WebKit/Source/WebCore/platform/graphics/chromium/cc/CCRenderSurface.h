cf063ea3ddbbfa3312bdc759c87828919803a3fb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-17 00:26:26 +0000

    [Chromium] Compositor should avoid drawing quads when cached textures are available and contents unchanged https://bugs.webkit.org/show_bug.cgi?id=88482
    Patch by Zeev Lieber <zlieber@chromium.org> on 2012-06-16
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    Post-processing CCRenderPassList after it's been generated and
    removing all render surface quads for which there are
    cached textures, and whose content didn't change. Added a new
    flag to CCLayerImpl to differentiate surface property change and
    layer property change.
    
    The changes are covered by new unit tests that check that
    the removal algorithm functions propertly. No rendering
    behaviour change, so no new layout tests.
    
    * platform/graphics/chromium/cc/CCDamageTracker.cpp:
    (WebCore::layerNeedsToRedrawOntoItsTargetSurface):
    (WebCore):
    (WebCore::CCDamageTracker::extendDamageForLayer):
    (WebCore::CCDamageTracker::extendDamageForRenderSurface):
    * platform/graphics/chromium/cc/CCLayerImpl.cpp:
    (WebCore::CCLayerImpl::CCLayerImpl):
    (WebCore::CCLayerImpl::layerSurfacePropertyChanged):
    (WebCore):
    (WebCore::CCLayerImpl::resetAllChangeTrackingForSubtree):
    (WebCore::CCLayerImpl::setOpacity):
    (WebCore::CCLayerImpl::setTransform):
    * platform/graphics/chromium/cc/CCLayerImpl.h:
    (CCLayerImpl):
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::calculateRenderPasses):
    (WebCore::CCLayerTreeHostImpl::removeRenderPassesRecursive):
    (WebCore):
    (WebCore::CCLayerTreeHostImpl::removePassesWithCachedTextures):
    (WebCore::CCLayerTreeHostImpl::prepareToDraw):
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.h:
    (FrameData):
    (CCLayerTreeHostImpl):
    * platform/graphics/chromium/cc/CCRenderPass.h:
    (CCRenderPass):
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::prepareContentsTexture):
    (WebCore::CCRenderSurface::hasCachedContentsTexture):
    (WebCore):
    (WebCore::CCRenderSurface::contentsChanged):
    * platform/graphics/chromium/cc/CCRenderSurface.h:
    (CCRenderSurface):
    
    Source/WebKit/chromium:
    
    Added tests to verify quad removal algorithm. Added infrastructure
    code to generate custom render passes out of a script
    encoded as C string.
    
    * tests/CCDamageTrackerTest.cpp:
    (WebKitTests::TEST_F):
    * tests/CCLayerImplTest.cpp:
    (WebCore):
    (WebCore::TEST):
    * tests/CCLayerTreeHostImplTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
@@ -54,9 +54,10 @@ public:
     explicit CCRenderSurface(CCLayerImpl*);
     ~CCRenderSurface();
 
-    bool prepareContentsTexture(LayerRendererChromium*);
+    virtual bool prepareContentsTexture(LayerRendererChromium*);
     void releaseContentsTexture();
     bool hasValidContentsTexture() const;
+    virtual bool hasCachedContentsTexture() const;
 
     bool prepareBackgroundTexture(LayerRendererChromium*);
     void releaseBackgroundTexture();
@@ -114,6 +115,8 @@ public:
 
     void setScissorRect(const IntRect& scissorRect) { m_scissorRect = scissorRect; }
     const IntRect& scissorRect() const { return m_scissorRect; }
+
+    virtual bool contentsChanged() const;
 
     void setContentRect(const IntRect&);
     const IntRect& contentRect() const { return m_contentRect; }
--- third_party/WebKit/Source/WebKit/chromium/tests/CCDamageTrackerTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCDamageTrackerTest.cpp
@@ -935,7 +935,8 @@ TEST_F(CCDamageTrackerTest, verifyDamageForMask)
 
     // Then test the property change.
     clearDamageForAllSurfaces(root.get());
-    maskLayer->setOpacity(0.5);
+    maskLayer->setStackingOrderChanged(true);
+
     emulateDrawingOneFrame(root.get());
     childDamageRect = child->renderSurface()->damageTracker()->currentDamageRect();
     EXPECT_FLOAT_RECT_EQ(FloatRect(0, 0, 30, 30), childDamageRect);
@@ -1002,7 +1003,7 @@ TEST_F(CCDamageTrackerTest, verifyDamageForReplicaMask)
 
     // CASE 1: a property change on the mask should damage only the reflected region on the target surface.
     clearDamageForAllSurfaces(root.get());
-    replicaMaskLayer->setOpacity(0.6f);
+    replicaMaskLayer->setStackingOrderChanged(true);
     emulateDrawingOneFrame(root.get());
 
     FloatRect grandChildDamageRect = grandChild1->renderSurface()->damageTracker()->currentDamageRect();
@@ -1066,7 +1067,8 @@ TEST_F(CCDamageTrackerTest, verifyDamageForReplicaMaskWithAnchor)
 
     // A property change on the replicaMask should damage the reflected region on the target surface.
     clearDamageForAllSurfaces(root.get());
-    replicaMaskLayer->setOpacity(0.6f);
+    replicaMaskLayer->setStackingOrderChanged(true);
+
     emulateDrawingOneFrame(root.get());
 
     FloatRect childDamageRect = child1->renderSurface()->damageTracker()->currentDamageRect();
