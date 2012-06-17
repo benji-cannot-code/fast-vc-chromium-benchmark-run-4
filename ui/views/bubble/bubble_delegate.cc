90d1fd543351dd942210a296081981ae31ce06ce
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-17 22:48:44 +0000

    views: Fix a case of user-after-free in bubbles.
    If the anchor is destroyed while the bubble-widget is invisible, then the
    bubble-widget holds a reference to the destroyed anchor-widget without
    realizing that it has been destroyed, causing the user-after-free error.
    
    BUG=127539
    TEST=aura_shell_unittests:SystemTrayTest tests don't use-after-free.
    
    Review URL: https://chromiumcodereview.appspot.com/10563017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142655 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/bubble/bubble_delegate.cc
+++ ui/views/bubble/bubble_delegate.cc
@@ -139,7 +139,10 @@ BubbleDelegateView::BubbleDelegateView(
   AddAccelerator(ui::Accelerator(ui::VKEY_ESCAPE, ui::EF_NONE));
 }
 
-BubbleDelegateView::~BubbleDelegateView() {}
+BubbleDelegateView::~BubbleDelegateView() {
+  if (anchor_widget_)
+    anchor_widget_->RemoveObserver(this);
+}
 
 // static
 Widget* BubbleDelegateView::CreateBubble(BubbleDelegateView* bubble_delegate) {
@@ -147,6 +150,9 @@ Widget* BubbleDelegateView::CreateBubble(BubbleDelegateView* bubble_delegate) {
   // Determine the anchor widget from the anchor view at bubble creation time.
   bubble_delegate->anchor_widget_ = bubble_delegate->anchor_view() ?
       bubble_delegate->anchor_view()->GetWidget() : NULL;
+  if (bubble_delegate->anchor_widget_)
+    bubble_delegate->anchor_widget_->AddObserver(bubble_delegate);
+
   Widget* bubble_widget = CreateBubbleWidget(bubble_delegate);
 
 #if defined(OS_WIN) && !defined(USE_AURA)
@@ -200,16 +206,12 @@ void BubbleDelegateView::OnWidgetVisibilityChanged(Widget* widget,
   if (visible) {
     if (border_widget_)
       border_widget_->Show();
-    if (anchor_widget())
-      anchor_widget()->AddObserver(this);
     GetFocusManager()->SetFocusedView(GetInitiallyFocusedView());
     if (anchor_widget() && anchor_widget()->GetTopLevelWidget())
       anchor_widget()->GetTopLevelWidget()->DisableInactiveRendering();
   } else {
     if (border_widget_)
       border_widget_->Hide();
-    if (anchor_widget())
-      anchor_widget()->RemoveObserver(this);
   }
 }
 
