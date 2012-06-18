f1cc63438b91714ba0c0767e2c0e930cf0dfeeac
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-18 04:35:21 +0000

    GC copy phase spends needless cycles zero-filling blocks https://bugs.webkit.org/show_bug.cgi?id=89128
    Reviewed by Gavin Barraclough.
    
    We only need to zero-fill when we're allocating memory that might not
    get fully initialized before GC.
    
    * heap/CopiedBlock.h:
    (JSC::CopiedBlock::createNoZeroFill):
    (JSC::CopiedBlock::create): Added a way to create without zero-filling.
    This is our optimization.
    
    (JSC::CopiedBlock::zeroFillToEnd):
    (JSC::CopiedBlock::CopiedBlock): Split zero-filling out from creation,
    so we can sometimes create without zero-filling.
    
    * heap/CopiedSpace.cpp:
    (JSC::CopiedSpace::init):
    (JSC::CopiedSpace::tryAllocateSlowCase):
    (JSC::CopiedSpace::doneCopying): Renamed addNewBlock to allocateBlock()
    to clarify that the new block is always newly-allocated.
    
    (JSC::CopiedSpace::doneFillingBlock): Make sure to zero-fill to the end
    of a block that might be used in the future for allocation. (Most of the
    time, this is a no-op, since we've already filled the block completely.)
    
    (JSC::CopiedSpace::getFreshBlock): Removed this function because the
    abstraction of "allocation must succeed" is no longer useful.
    
    * heap/CopiedSpace.h: Updated declarations to match.
    
    * heap/CopiedSpaceInlineMethods.h:
    (JSC::CopiedSpace::allocateBlockForCopyingPhase): New function, which
    knows that it can skip zero-filling.
    
    Added tighter scoping to our lock, to improve parallelism.
    
    (JSC::CopiedSpace::allocateBlock): Folded getFreshBlock functionality
    into this function, for simplicity.
    
    * heap/MarkStack.cpp:
    (JSC::SlotVisitor::startCopying):
    (JSC::SlotVisitor::allocateNewSpace): Use our new zero-fill-free helper
    function for great good.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/CopiedSpace.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/CopiedSpace.cpp
@@ -59,8 +59,7 @@ void CopiedSpace::init()
     m_toSpace = &m_blocks1;
     m_fromSpace = &m_blocks2;
     
-    if (!addNewBlock())
-        CRASH();
+    allocateBlock();
 }   
 
 CheckedBoolean CopiedSpace::tryAllocateSlowCase(size_t bytes, void** outPtr)
@@ -70,10 +69,8 @@ CheckedBoolean CopiedSpace::tryAllocateSlowCase(size_t bytes, void** outPtr)
     
     m_heap->didAllocate(m_allocator.currentCapacity());
 
-    if (!addNewBlock()) {
-        *outPtr = 0;
-        return false;
-    }
+    allocateBlock();
+
     *outPtr = m_allocator.allocate(bytes);
     ASSERT(*outPtr);
     return true;
@@ -169,6 +166,8 @@ void CopiedSpace::doneFillingBlock(CopiedBlock* block)
         return;
     }
 
+    block->zeroFillToEnd();
+
     {
         SpinLockHolder locker(&m_toSpaceLock);
         m_toSpace->push(block);
@@ -224,33 +223,10 @@ void CopiedSpace::doneCopying()
         curr = next;
     }
 
-    if (!m_toSpace->head()) {
-        if (!addNewBlock())
-            CRASH();
-    } else
+    if (!m_toSpace->head())
+        allocateBlock();
+    else
         m_allocator.resetCurrentBlock(static_cast<CopiedBlock*>(m_toSpace->head()));
-}
-
-CheckedBoolean CopiedSpace::getFreshBlock(AllocationEffort allocationEffort, CopiedBlock** outBlock)
-{
-    CopiedBlock* block = 0;
-    if (allocationEffort == AllocationMustSucceed)
-        block = CopiedBlock::create(m_heap->blockAllocator().allocate());
-    else {
-        ASSERT(allocationEffort == AllocationCanFail);
-        if (m_heap->shouldCollect())
-            m_heap->collect(Heap::DoNotSweep);
-        
-        if (!getFreshBlock(AllocationMustSucceed, &block)) {
-            *outBlock = 0;
-            ASSERT_NOT_REACHED();
-            return false;
-        }
-    }
-    ASSERT(block);
-    ASSERT(is8ByteAligned(block->m_offset));
-    *outBlock = block;
-    return true;
 }
 
 size_t CopiedSpace::size()
