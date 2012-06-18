34e758d3bc14193a09ddec0ed1d2a1ae174203a5
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 14:31:05 +0000

    Whitelist all previously loaded extensions when registering the ExtensionService with the HostContentSettingsMap.
    We used to only listen to EXTENSION_LOADED notifications, but now the PlatformAppProvider is created after extensions have been loaded, so we need to go through already loaded extensions and whitelist them as well.
    
    BUG=131811
    TEST=see bug for manual test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10535172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142708 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_platform_app_provider.cc
+++ chrome/browser/content_settings/content_settings_platform_app_provider.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/content_settings.h"
 #include "chrome/common/content_settings_pattern.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_set.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_details.h"
@@ -23,6 +24,13 @@ namespace content_settings {
 
 PlatformAppProvider::PlatformAppProvider(ExtensionService* extension_service)
     : registrar_(new content::NotificationRegistrar) {
+  // Whitelist all extensions loaded so far.
+  const ExtensionSet* extensions = extension_service->extensions();
+  for (ExtensionSet::const_iterator it = extensions->begin();
+       it != extensions->end(); ++it) {
+    if ((*it)->plugins().size() > 0)
+      SetContentSettingForExtension(*it, CONTENT_SETTING_ALLOW);
+  }
   Profile* profile = extension_service->profile();
   registrar_->Add(this, chrome::NOTIFICATION_EXTENSION_HOST_CREATED,
                   content::Source<Profile>(profile));
