1c3aad2f7592fbd58ef00fdd12a8c6907c12766b
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 08:26:41 +0000

    chromeos: Small clean up of gdata::GDataFileSystem
    BUG=None
    TEST=unit_tests --gtest_filter="GData*"
    
    Review URL: https://chromiumcodereview.appspot.com/10559027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/bind.h"
-#include "base/eintr_wrapper.h"
 #include "base/file_util.h"
 #include "base/json/json_file_value_serializer.h"
 #include "base/json/json_reader.h"
@@ -19,8 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/platform_file.h"
 #include "base/threading/sequenced_worker_pool.h"
-#include "base/threading/thread_restrictions.h"
-#include "base/synchronization/waitable_event.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/gdata/drive_webapps_registry.h"
 #include "chrome/browser/chromeos/gdata/gdata.pb.h"
@@ -713,9 +710,8 @@ GDataFileSystem::GDataFileSystem(
       documents_service_(documents_service),
       update_timer_(true /* retain_user_task */, true /* is_repeating */),
       hide_hosted_docs_(false),
-      ui_weak_ptr_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(
-          new base::WeakPtrFactory<GDataFileSystem>(this))),
-      ui_weak_ptr_(ui_weak_ptr_factory_->GetWeakPtr()),
+      ui_weak_ptr_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+      ui_weak_ptr_(ui_weak_ptr_factory_.GetWeakPtr()),
       sequence_token_(sequence_token) {
   // Should be created from the file browser extension API on UI thread.
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
--- chrome/browser/chromeos/gdata/gdata_file_system.h
+++ chrome/browser/chromeos/gdata/gdata_file_system.h
@@ -16,16 +16,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/platform_file.h"
 #include "base/timer.h"
-#include "base/synchronization/lock.h"
 #include "base/threading/sequenced_worker_pool.h"
 #include "chrome/browser/chromeos/gdata/gdata_cache.h"
 #include "chrome/browser/chromeos/gdata/gdata_files.h"
 #include "chrome/browser/prefs/pref_change_registrar.h"
 #include "content/public/browser/notification_observer.h"
-
-namespace base {
-class WaitableEvent;
-}
 
 namespace gdata {
 
@@ -35,7 +30,7 @@ struct UploadFileInfo;
 
 namespace {
 struct LoadRootFeedParams;
-}
+}  // namespace
 
 // Information about search result returned by Search Async callback.
 // This is data needed to create a file system entry that will be used by file
@@ -1148,7 +1143,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
   scoped_ptr<PrefChangeRegistrar> pref_registrar_;
 
   // WeakPtrFactory and WeakPtr bound to the UI thread.
-  scoped_ptr<base::WeakPtrFactory<GDataFileSystem> > ui_weak_ptr_factory_;
+  base::WeakPtrFactory<GDataFileSystem> ui_weak_ptr_factory_;
   base::WeakPtr<GDataFileSystem> ui_weak_ptr_;
 
   ObserverList<Observer> observers_;
