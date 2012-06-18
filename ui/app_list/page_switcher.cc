a86fd147e7a4756076673a4ec726297ad565abcc
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 23:22:41 +0000

    app_list: Change page switch button size based on width.
    Change button's width to as low as 44px to make them fit better for 5-6 pages.
    
    BUG=none.
    TEST=Verify page switcher buttons could fit well until 6 pages.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10572002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142854 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/page_switcher.cc
+++ ui/app_list/page_switcher.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/page_switcher.h"
 
+#include <algorithm>
+
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/app_list/pagination_model.h"
 #include "ui/base/animation/throb_animation.h"
@@ -18,7 +20,8 @@ namespace {
 const int kPreferredHeight = 57;
 
 const int kButtonSpacing = 18;
-const int kButtonWidth = 68;
+const int kMaxButtonWidth = 68;
+const int kMinButtonWidth = 44;
 const int kButtonHeight = 6;
 const int kButtonCornerRadius = 2;
 
@@ -32,6 +35,7 @@ class PageSwitcherButton : public views::CustomButton {
  public:
   explicit PageSwitcherButton(views::ButtonListener* listener)
       : views::CustomButton(listener),
+        button_width_(kMaxButtonWidth),
         selected_range_(0) {
   }
   virtual ~PageSwitcherButton() {}
@@ -44,9 +48,11 @@ class PageSwitcherButton : public views::CustomButton {
     SchedulePaint();
   }
 
+  void set_button_width(int button_width) { button_width_ = button_width; }
+
   // Overridden from views::View:
   virtual gfx::Size GetPreferredSize() OVERRIDE {
-    return gfx::Size(kButtonWidth, kButtonHeight);
+    return gfx::Size(button_width_, kButtonHeight);
   }
 
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE {
@@ -61,7 +67,7 @@ class PageSwitcherButton : public views::CustomButton {
   // Paints a button that has two rounded corner at bottom.
   void PaintButton(gfx::Canvas* canvas, SkColor base_color) {
     gfx::Rect rect(GetContentsBounds().Center(
-            gfx::Size(kButtonWidth, kButtonHeight)));
+            gfx::Size(button_width_, kButtonHeight)));
 
     SkPath path;
     path.addRoundRect(gfx::RectToSkRect(rect),
@@ -96,6 +102,8 @@ class PageSwitcherButton : public views::CustomButton {
       canvas->DrawPath(selected_path, paint);
     }
   }
+
+  int button_width_;
 
   // [-1, 1] range that represents the portion of the button that should be
   // painted with kSelectedColor. Positive range starts from left side and
@@ -140,6 +148,9 @@ gfx::Size PageSwitcher::GetPreferredSize() {
 
 void PageSwitcher::Layout() {
   gfx::Rect rect(GetContentsBounds());
+
+  CalculateButtonWidth(rect.width());
+
   // Makes |buttons_| horizontally center and vertically fill.
   gfx::Size buttons_size(buttons_->GetPreferredSize());
   gfx::Rect buttons_bounds(rect.CenterPoint().x() - buttons_size.width() / 2,
@@ -147,6 +158,22 @@ void PageSwitcher::Layout() {
                            buttons_size.width(),
                            rect.height());
   buttons_->SetBoundsRect(rect.Intersect(buttons_bounds));
+}
+
+void PageSwitcher::CalculateButtonWidth(int contents_width) {
+  int button_count = buttons_->child_count();
+  if (!button_count)
+    return;
+
+  int button_width = contents_width / button_count - kButtonSpacing;
+  button_width = std::min(kMaxButtonWidth,
+                          std::max(kMinButtonWidth, button_width));
+
+  for (int i = 0; i < button_count; ++i) {
+    PageSwitcherButton* button =
+        static_cast<PageSwitcherButton*>(buttons_->child_at(i));
+    button->set_button_width(button_width);
+  }
 }
 
 void PageSwitcher::ButtonPressed(views::Button* sender,
