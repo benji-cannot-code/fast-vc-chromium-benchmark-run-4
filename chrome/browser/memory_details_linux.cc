c002b440eeb155e678f0af5664fc8fcb1b4b303f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 17:34:23 +0000

    Fix build error with gcc-4.7
    BUG=120724
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10553031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142750 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/memory_details_linux.cc
+++ chrome/browser/memory_details_linux.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/memory_details.h"
 
+#include <sys/types.h>
+#include <unistd.h>
+
 #include <map>
 #include <set>
 
