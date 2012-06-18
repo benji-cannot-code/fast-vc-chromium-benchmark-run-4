8a56dc2e54abb7d426d9caca002bd4be841494ec
petarj@mips.com <petarj@mips.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 02:47:05 +0000

    [MIPS] Adding atomic ops for MIPS architecture.
    Initial commit for atomic operations on MIPS architecture.
    
    BUG=https://code.google.com/p/chromium/issues/detail?id=130022
    TEST=make chrome
    
    
    Review URL: https://chromiumcodereview.appspot.com/10448043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142663 0039d316-1c4b-4281-b951-d872f2087c98

--- base/atomicops.h
+++ base/atomicops.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -138,6 +138,8 @@ Atomic64 Release_Load(volatile const Atomic64* ptr);
 #include "base/atomicops_internals_x86_gcc.h"
 #elif defined(COMPILER_GCC) && defined(ARCH_CPU_ARM_FAMILY)
 #include "base/atomicops_internals_arm_gcc.h"
+#elif defined(COMPILER_GCC) && defined(ARCH_CPU_MIPS_FAMILY)
+#include "base/atomicops_internals_mips_gcc.h"
 #else
 #error "Atomic operations are not supported on your platform"
 #endif
