2336d9e3eb67964864e48d1df27027bb01742a67
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 20:14:58 +0000

    Revert 142563 - Handle sync database corruption with re-download
    A recent change allowed us to handle a missing sync database by creating
    a new directory and repopulating it by requesting an initial sync from
    the server.
    
    This commit builds on that work by intentionally deleting the sync
    database if it is found to be corrupt or unusable.  The client will then
    recognize that the database is empty and repopulate it, just as it would
    had the directory not existed in the first place.
    
    BUG=109668
    TEST=Manual.  Three cases, see below:
    - Delete 'Sync Data/SyncData.sqlite3' from the profile directory.  Start
      chromium and observe that all data is redownloaded from the server.
    - Corrupt an existing sync directory.  The SQL command
      'DELETE FROM metas WHERE metahandle = 1' should be sufficient.  Start
      chromium and observe that the directory is deleted then recreated
      and all data is redownloaded from the server.
    - Ensure that no directory can be created.  You could do this by marking
      the 'Sync Data' folder as read-only.  Start chrome, and observe that
      sync is unusable, but the browser still works.
      NOTE: This only works in release builds, debug builds will DCHECK.
      It's debatable whether or not this scenario is worth testing.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10554016
    
    TBR=rlarocque@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10578003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142793 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/sync_manager.cc
+++ sync/internal_api/sync_manager.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
-#include "base/file_util.h"
 #include "base/json/json_writer.h"
 #include "base/memory/ref_counted.h"
 #include "base/metrics/histogram.h"
@@ -906,6 +905,10 @@ bool SyncManager::SyncInternal::Init(
   encryptor_ = encryptor;
   unrecoverable_error_handler_ = unrecoverable_error_handler;
   report_unrecoverable_error_function_ = report_unrecoverable_error_function;
+  share_.directory.reset(
+      new syncable::Directory(encryptor_,
+                              unrecoverable_error_handler_,
+                              report_unrecoverable_error_function_));
 
   connection_manager_.reset(new SyncAPIServerConnectionManager(
       sync_server_and_path, port, use_ssl, user_agent, post_factory));
@@ -914,8 +917,6 @@ bool SyncManager::SyncInternal::Init(
   observing_ip_address_changes_ = true;
 
   connection_manager()->AddListener(this);
-
-  bool signed_in = SignIn(credentials);
 
   // Test mode does not use a syncer context or syncer thread.
   if (testing_mode_ == NON_TEST) {
@@ -937,6 +938,8 @@ bool SyncManager::SyncInternal::Init(
     session_context()->set_account_name(credentials.email);
     scheduler_.reset(new SyncScheduler(name_, session_context(), new Syncer()));
   }
+
+  bool signed_in = SignIn(credentials);
 
   if (signed_in) {
     if (scheduler()) {
@@ -1126,10 +1129,6 @@ void SyncManager::SyncInternal::StartSyncingNormally(
 
 bool SyncManager::SyncInternal::OpenDirectory() {
   DCHECK(!initialized_) << "Should only happen once";
-  share_.directory.reset(
-      new syncable::Directory(encryptor_,
-                              unrecoverable_error_handler_,
-                              report_unrecoverable_error_function_));
 
   // Set before Open().
   change_observer_ =
@@ -1144,16 +1143,6 @@ bool SyncManager::SyncInternal::OpenDirectory() {
   } else {
     open_result = directory()->Open(
         database_path_, username_for_share(), this, transaction_observer);
-    // If at first we don't succeed, delete the DB and try again.
-    if (open_result != syncable::OPENED) {
-      file_util::Delete(database_path_, false);
-      share_.directory.reset(
-          new syncable::Directory(encryptor_,
-                                  unrecoverable_error_handler_,
-                                  report_unrecoverable_error_function_));
-      open_result = directory()->Open(
-          database_path_, username_for_share(), this, transaction_observer);
-    }
   }
   if (open_result != syncable::OPENED) {
     LOG(ERROR) << "Could not open share for:" << username_for_share();
