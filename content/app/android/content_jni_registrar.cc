aab55fd5e14f3861183aa56eb5a046dcc14a5086
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 22:32:42 +0000

    Adding java TouchPoint class for android
    Upstream the java TouchPoint class before we upstream all the touch related stuff for chrome on android.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10532148
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142835 0039d316-1c4b-4281-b951-d872f2087c98

--- content/app/android/content_jni_registrar.cc
+++ content/app/android/content_jni_registrar.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/android/device_info.h"
 #include "content/browser/android/download_controller.h"
 #include "content/browser/android/sandboxed_process_launcher.h"
+#include "content/browser/android/touch_point.h"
 #include "content/common/android/command_line.h"
 #include "content/common/android/surface_callback.h"
 #include "content/common/android/trace_event_binding.h"
@@ -33,6 +34,7 @@ base::android::RegistrationMethod kContentRegisteredMethods[] = {
   { "SandboxedProcessLauncher", content::RegisterSandboxedProcessLauncher },
   { "SandboxedProcessService", content::RegisterSandboxedProcessService },
   { "SurfaceCallback", content::RegisterSurfaceCallback },
+  { "TouchPoint", content::RegisterTouchPoint },
   { "TraceEvent", RegisterTraceEvent },
 };
 
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -202,6 +202,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'browser/android/download_controller.h',
     'browser/android/sandboxed_process_launcher.cc',
     'browser/android/sandboxed_process_launcher.h',
+    'browser/android/touch_point.cc',
+    'browser/android/touch_point.h',
     'browser/appcache/appcache_dispatcher_host.cc',
     'browser/appcache/appcache_dispatcher_host.h',
     'browser/appcache/appcache_frontend_proxy.cc',
