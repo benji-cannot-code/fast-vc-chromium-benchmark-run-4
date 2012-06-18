ec0e372c1f69b15e347966f1a0629cef7d1b0fd5
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 17:55:51 +0000

    Fix coordinates in speech recognition.
    BUG=133101
    TEST=speech input works well in HiDPI
    
    
    Review URL: https://chromiumcodereview.appspot.com/10557031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/info_bubble_view.h
+++ chrome/browser/ui/cocoa/info_bubble_view.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace info_bubble {
 
+// These values are in view coordinates.
 const CGFloat kBubbleArrowHeight = 8.0;
 const CGFloat kBubbleArrowWidth = 15.0;
 const CGFloat kBubbleCornerRadius = 2.0;
