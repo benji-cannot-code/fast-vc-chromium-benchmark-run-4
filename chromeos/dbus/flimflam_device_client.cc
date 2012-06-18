13c47ae276c95d5bce10d015ccc55f24629b70b6
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-18 19:20:53 +0000

    Revert 142765 - Use DBus.ServiceProperty to watch for SMS signals
    Use DBus.ServiceProperty instead of DBus.ConnectionProperty to watch
    for SMS signals from ModemManager. The allows Chrome to continue
    monitoring for signals even if ModemManager is restarted.
    
    Contributed by jglasgow@chromium.org
    
    Requires updates to shill in https://gerrit.chromium.org/gerrit/#change,24613
    
    BUG=chromium-os:31575
    TEST=unittests, run /usr/local/lib/flimflam/test/fake-modem
    
    Change-Id: Ia811eff852ac02328e1b105e2378ce4400c60315
    
    
    Review URL: https://chromiumcodereview.appspot.com/10536104
    
    TBR=jglasgow@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10563031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142780 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/flimflam_device_client.cc
+++ chromeos/dbus/flimflam_device_client.cc
@@ -223,9 +223,6 @@ class FlimflamDeviceClientStubImpl : public FlimflamDeviceClient {
         flimflam::kDBusConnectionProperty,
         base::Value::CreateStringValue("/stub"));
     cellular_properties->SetWithoutPathExpansion(
-        flimflam::kDBusServiceProperty,
-        base::Value::CreateStringValue("org.freedesktop.ModemManager"));
-    cellular_properties->SetWithoutPathExpansion(
         flimflam::kDBusObjectProperty,
         base::Value::CreateStringValue("/device/cellular1"));
     stub_devices_.Set(kStubCellular1, cellular_properties);
@@ -241,9 +238,6 @@ class FlimflamDeviceClientStubImpl : public FlimflamDeviceClient {
     cellular_properties->SetWithoutPathExpansion(
         flimflam::kDBusConnectionProperty,
         base::Value::CreateStringValue(":stub.0"));
-    cellular_properties->SetWithoutPathExpansion(
-        flimflam::kDBusServiceProperty,
-        base::Value::CreateStringValue("org.freedesktop.ModemManager1"));
     cellular_properties->SetWithoutPathExpansion(
         flimflam::kDBusObjectProperty,
         base::Value::CreateStringValue(
