3f7697a93ff79e795b762ea2ad85c437dc4ce001
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-18 13:46:26 +0000

    Unreviewed, rolling out r120485 and r120582. http://trac.webkit.org/changeset/120485 http://trac.webkit.org/changeset/120582 https://bugs.webkit.org/show_bug.cgi?id=89345
    chromeos tree breakage (Requested by morrita on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-06-18
    
    Source/WebCore:
    
    * WebCore.gyp/WebCore.gyp:
    * platform/image-decoders/ImageDecoder.h:
    (ImageFrame):
    (ImageDecoder):
    * platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
    (turboSwizzled):
    (WebCore::JPEGImageReader::JPEGImageReader):
    (WebCore::JPEGImageReader::close):
    (WebCore::JPEGImageReader::decode):
    (JPEGImageReader):
    (WebCore::JPEGImageDecoder::outputScanlines):
    * platform/image-decoders/png/PNGImageDecoder.cpp:
    (WebCore::PNGImageReader::PNGImageReader):
    (WebCore::PNGImageReader::close):
    (WebCore::PNGImageReader::currentBufferSize):
    (PNGImageReader):
    (WebCore::PNGImageReader::decodingSizeOnly):
    (WebCore::PNGImageReader::interlaceBuffer):
    (WebCore::PNGImageReader::hasAlpha):
    (WebCore::PNGImageReader::setHasAlpha):
    (WebCore::PNGImageDecoder::headerAvailable):
    (WebCore::PNGImageDecoder::rowAvailable):
    * platform/image-decoders/skia/ImageDecoderSkia.cpp:
    (WebCore):
    (WebCore::resolveColorSpace):
    (WebCore::createColorSpace):
    (WebCore::ImageFrame::setColorProfile):
    (WebCore::ImageFrame::setStatus):
    
    Source/WTF:
    
    * wtf/Platform.h:
    
    LayoutTests:
    
    * platform/chromium/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -468,18 +468,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* USE(SKIA) for Win/Linux/Mac/Android */
 #if PLATFORM(CHROMIUM)
 #if OS(DARWIN)
+#if USE(SKIA_ON_MAC_CHROMIUM)
 #define WTF_USE_SKIA 1
+#else
+#define WTF_USE_CG 1
+#endif
 #define WTF_USE_ATSUI 1
 #define WTF_USE_CORE_TEXT 1
 #define WTF_USE_ICCJPEG 1
-#define WTF_USE_QCMSLIB 1
 #elif OS(ANDROID)
 #define WTF_USE_SKIA 1
 #else
 #define WTF_USE_SKIA 1
 #define WTF_USE_CHROMIUM_NET 1
-#define WTF_USE_ICCJPEG 1
-#define WTF_USE_QCMSLIB 1
 #endif
 #endif
 
