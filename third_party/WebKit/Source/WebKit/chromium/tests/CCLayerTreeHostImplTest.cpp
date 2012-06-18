2e0bc212b74c9dae59bf1538b48ae55189b6c5a0
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-18 02:43:33 +0000

    Unreviewed build fix.
    Source/WebCore:
    
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::removePassesWithCachedTextures):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostImplTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120564 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostImplTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostImplTest.cpp
@@ -1627,6 +1627,8 @@ TEST_F(CCLayerTreeHostImplTest, partialSwapReceivesDamageRect)
     EXPECT_EQ(expectedSwapRect.height(), actualSwapRect.height());
 }
 
+} // namespace
+
 class FakeLayerWithQuads : public CCLayerImpl {
 public:
     static PassOwnPtr<FakeLayerWithQuads> create(int id) { return adoptPtr(new FakeLayerWithQuads(id)); }
@@ -1645,6 +1647,8 @@ private:
     {
     }
 };
+
+namespace {
 
 class MockContext : public FakeWebGraphicsContext3D {
 public:
