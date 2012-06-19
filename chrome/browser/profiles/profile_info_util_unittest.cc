812e4495398217d69216254942e221d53db356ab
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 23:11:39 +0000

    Make avatar images HiDPI.
    BUG=133542
    TEST=as in bug
    
    
    Review URL: https://chromiumcodereview.appspot.com/10584022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143074 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_info_util_unittest.cc
+++ chrome/browser/profiles/profile_info_util_unittest.cc
@@ -1,12 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "chrome/browser/profiles/profile_info_util.h"
 
 #include "grit/generated_resources.h"
-#include "grit/theme_resources.h"
+#include "grit/theme_resources_standard.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image_unittest_util.h"
