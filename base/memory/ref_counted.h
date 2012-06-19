4659dd0aec0c053b5ea6dda76680ff98e95a4d33
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 18:33:38 +0000

    Add element_type in scoped_refptr.
    This will enable ::testing::IsNull and ::testing::NotNull on scoped_refptr. It
    was not working because GetRawPointer needs element_type to be defined in the
    Pointer type.
    
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10558017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142997 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/ref_counted.h
+++ base/memory/ref_counted.h
@@ -220,6 +220,8 @@ class RefCountedData : public base::RefCounted< base::RefCountedData<T> > {
 template <class T>
 class scoped_refptr {
  public:
+  typedef T element_type;
+
   scoped_refptr() : ptr_(NULL) {
   }
 
