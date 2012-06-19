d4b2f82b71397c7fbb82c298157acdb768a85b6c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-19 21:33:56 +0000

    [cairo] Fix LayoutTests/fast/canvas/patternfill-repeat.html https://bugs.webkit.org/show_bug.cgi?id=53085
    Patch by Dominik Röttsches <dominik.rottsches@intel.com> on 2012-06-19
    Reviewed by Martin Robinson.
    
    Source/WebCore:
    
    Clipping previously unlimited vertical and horizontal pattern repeats with
    a clip rectangle similar to the Qt Graphics Context.
    
    No new tests, this patch fixes
    canvas/philip/tests/2d.pattern.paint.repeat* tests.
    
    * platform/graphics/Pattern.h:
    (WebCore::Pattern::getPatternSpaceTransform): Adding constant getter method to be able to map to pattern space externally, needed by PlatformContextCairo::clipForPatternFilling.
    * platform/graphics/cairo/PlatformContextCairo.cpp:
    (WebCore::PlatformContextCairo::prepareForFilling):
    (WebCore::PlatformContextCairo::clipForPatternFilling): Clip pattern repeats if needed.
    * platform/graphics/cairo/PlatformContextCairo.h:
    
    LayoutTests:
    
    canvas/philip/tests/2d.pattern.paint.repeat* tests now passing.
    
    * platform/efl/Skipped:
    * platform/gtk/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/PlatformContextCairo.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/PlatformContextCairo.h
@@ -69,6 +69,8 @@ public:
     void prepareForStroking(const GraphicsContextState&, AlphaPreservation = PreserveAlpha);
 
 private:
+    void clipForPatternFilling(const GraphicsContextState&);
+
     RefPtr<cairo_t> m_cr;
 
     class State;
