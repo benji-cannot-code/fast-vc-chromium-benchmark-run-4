65625f7f8af2163a1bcf1ab4968dd09c634ee7c2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-19 19:09:49 +0000

    Fix link error with fillWithEmptyClients https://bugs.webkit.org/show_bug.cgi?id=89484
    Patch by Alexandre Elias <aelias@google.com> on 2012-06-19
    Reviewed by Ryosuke Niwa.
    
    fillWithEmptyClients() was within #if ENABLE() guard in the .cpp file
    but not in the header.  There is now a caller unrelated to these ENABLEs
    in WebHelperPluginImpl, so linking fails if both SVG and PAGE_POPUP
    are disabled.
    
    This function looks generic, so just remove the ENABLE guards.
    
    No new tests. (Build fix.)
    
    * loader/EmptyClients.cpp:
    (WebCore::fillWithEmptyClients):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/EmptyClients.cpp
+++ third_party/WebKit/Source/WebCore/loader/EmptyClients.cpp
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-#if ENABLE(SVG) || ENABLE(PAGE_POPUP)
 void fillWithEmptyClients(Page::PageClients& pageClients)
 {
 #if ENABLE(CONTEXT_MENUS)
@@ -65,7 +64,6 @@ void fillWithEmptyClients(Page::PageClients& pageClients)
     static InspectorClient* dummyInspectorClient = adoptPtr(new EmptyInspectorClient).leakPtr();
     pageClients.inspectorClient = dummyInspectorClient;
 }
-#endif
 
 class EmptyPopupMenu : public PopupMenu {
 public:
