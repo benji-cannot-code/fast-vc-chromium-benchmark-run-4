1228733bcdf2bcf1a276173dfdebfb82eb2e52e9
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 21:51:01 +0000

    [NaCl SDK] In update_nacl_manifest.py, catch errors from "gsutil ls".
    The behavior in gsutil 3.10 is different from 3.3. gsutil ls of a non-existent
    directory used to return nothing, now it throws an exception.
    
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10580017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143059 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/update_nacl_manifest.py
+++ native_client_sdk/src/build_tools/update_nacl_manifest.py
@@ -185,7 +185,10 @@ class RealDelegate(Delegate):
 
   def GsUtil_ls(self, url):
     """See Delegate.GsUtil_ls"""
-    stdout = self._RunGsUtil(None, 'ls', url)
+    try:
+      stdout = self._RunGsUtil(None, 'ls', url)
+    except subprocess.CalledProcessError:
+      return []
 
     # filter out empty lines
     return filter(None, stdout.split('\n'))
@@ -217,7 +220,8 @@ class RealDelegate(Delegate):
     else:
       stdin_pipe = None
 
-    process = subprocess.Popen(cmd, stdin=stdin_pipe, stdout=subprocess.PIPE)
+    process = subprocess.Popen(cmd, stdin=stdin_pipe, stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
     stdout, _ = process.communicate(stdin)
 
     if process.returncode != 0:
