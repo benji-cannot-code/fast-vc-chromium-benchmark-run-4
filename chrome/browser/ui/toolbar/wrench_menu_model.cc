87d6592293a41710317192cebd98313a6b24411f
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 14:32:49 +0000

    Only show Pin to Start Screen command when running in metro mode.
    This command can only work in metro mode.
    
    BUG=129598
    TEST=Check pin to start screen is only visible in metro mode.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10548040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/wrench_menu_model.cc
+++ chrome/browser/ui/toolbar/wrench_menu_model.cc
@@ -60,7 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if defined(OS_WIN)
-#include "base/win/windows_version.h"
+#include "base/win/metro.h"
 #include "chrome/browser/enumerate_modules_model_win.h"
 #endif
 
@@ -360,19 +360,23 @@ bool WrenchMenuModel::IsCommandIdEnabled(int command_id) const {
 }
 
 bool WrenchMenuModel::IsCommandIdVisible(int command_id) const {
-  if (command_id == IDC_UPGRADE_DIALOG) {
-    return UpgradeDetector::GetInstance()->notify_upgrade();
-  } else if (command_id == IDC_VIEW_INCOMPATIBILITIES) {
 #if defined(OS_WIN)
+  if (command_id == IDC_VIEW_INCOMPATIBILITIES) {
     EnumerateModulesModel* loaded_modules =
         EnumerateModulesModel::GetInstance();
     if (loaded_modules->confirmed_bad_modules_detected() <= 0)
       return false;
     loaded_modules->AcknowledgeConflictNotification();
     return true;
+  } else if (command_id == IDC_PIN_TO_START_SCREEN) {
+    return base::win::IsMetroProcess();
 #else
+  if (command_id == IDC_VIEW_INCOMPATIBILITIES ||
+      command_id == IDC_PIN_TO_START_SCREEN) {
     return false;
 #endif
+  } else if (command_id == IDC_UPGRADE_DIALOG) {
+    return UpgradeDetector::GetInstance()->notify_upgrade();
   } else if (command_id == IDC_VIEW_BACKGROUND_PAGES) {
     return TaskManager::GetBackgroundPageCount() > 0;
   }
@@ -441,11 +445,7 @@ void WrenchMenuModel::Build() {
   AddItemWithStringId(IDC_NEW_INCOGNITO_WINDOW, IDS_NEW_INCOGNITO_WINDOW);
 #endif
 
-#if defined(OS_WIN)
-  if (base::win::GetVersion() >= base::win::VERSION_WIN8)
-    AddItemWithStringId(IDC_PIN_TO_START_SCREEN, IDS_PIN_TO_START_SCREEN);
-#endif
-
+  AddItemWithStringId(IDC_PIN_TO_START_SCREEN, IDS_PIN_TO_START_SCREEN);
   bookmark_sub_menu_model_.reset(new BookmarkSubMenuModel(this, browser_));
   AddSubMenuWithStringId(IDC_BOOKMARKS_MENU, IDS_BOOKMARKS_MENU,
                          bookmark_sub_menu_model_.get());
