ed581af8145e577bb1624031a5b13a539deb5778
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 16:04:34 +0000

    fix win_aura build
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/stubs_aura.cc
+++ chrome/browser/ui/views/stubs_aura.cc
@@ -25,6 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class SSLClientAuthHandler;
 class TabContents;
 
+namespace content {
+class WebContents;
+}
+
 namespace net {
 class HttpNetworkSession;
 class SSLCertRequestInfo;
