3bd09ec4c246aca31821552f297a8906c8c3ae2c
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 02:07:08 +0000

    Enforce 3-ways merge style on merge_isolate.py and isolate.py by default.
    Otherwise merge_isolate.py mangles .isolate files that only have 2 OSes
    defined, like base_unittests.isolate.
    
    Make its output in binary mode, otherwise CRLF is used on Windows.
    
    R=cmp@chromium.org
    NOTRY=true
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10563029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142898 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/merge_isolate.py
+++ tools/isolate/merge_isolate.py
@@ -20,6 +20,8 @@ import sys
 
 from isolate_common import pretty_print, KEY_TRACKED, KEY_UNTRACKED
 
+DEFAULT_OSES = ['linux', 'mac', 'win']
+
 
 def union(lhs, rhs):
   """Merges two compatible datastructures composed of dict/list/set."""
@@ -304,7 +306,7 @@ def convert_map_to_gyp(values, oses):
   return out
 
 
-def load_gyp(value, file_comment):
+def load_gyp(value, file_comment, default_oses):
   """Parses one gyp skeleton and returns a Configs() instance.
 
   |value| is the loaded dictionary that was defined in the gyp file.
@@ -343,6 +345,7 @@ def load_gyp(value, file_comment):
   # Scan to get the list of OSes.
   conditions = value.get('conditions', [])
   oses = set(re.match(r'OS==\"([a-z]+)\"', c[0]).group(1) for c in conditions)
+  oses = oses.union(default_oses)
   configs = Configs(oses, file_comment)
 
   # Global level variables.
@@ -358,7 +361,7 @@ def load_gyp(value, file_comment):
   return configs
 
 
-def load_gyps(items):
+def load_gyps(items, default_oses):
   """Parses each gyp file and returns the merged results.
 
   It only loads what load_gyp() can process.
@@ -368,12 +371,13 @@ def load_gyps(items):
     dirs:  dict(dirame, set(OS where this dirname is a dependency))
     oses:  set(all the OSes referenced)
     """
-  configs = Configs([], None)
+  configs = Configs(default_oses, None)
   for item in items:
     logging.debug('loading %s' % item)
     with open(item, 'r') as f:
       content = f.read()
-    new_config = load_gyp(eval_content(content), extract_comment(content))
+    new_config = load_gyp(
+        eval_content(content), extract_comment(content), default_oses)
     logging.debug('has OSes: %s' % ','.join(k for k in new_config.per_os if k))
     configs = union(configs, new_config)
   logging.debug('Total OSes: %s' % ','.join(k for k in configs.per_os if k))
@@ -396,6 +400,9 @@ def main(args=None):
       '-v', '--verbose', action='count', default=0, help='Use multiple times')
   parser.add_option(
       '-o', '--output', help='Output to file instead of stdout')
+  parser.add_option(
+      '--os', default=','.join(DEFAULT_OSES),
+      help='Inject the list of OSes, default: %default')
 
   options, args = parser.parse_args(args)
   level = [logging.ERROR, logging.INFO, logging.DEBUG][min(2, options.verbose)]
@@ -403,10 +410,10 @@ def main(args=None):
         level=level,
         format='%(levelname)5s %(module)15s(%(lineno)3d):%(message)s')
 
-  configs = load_gyps(args)
+  configs = load_gyps(args, options.os.split(','))
   data = convert_map_to_gyp(*reduce_inputs(*invert_map(configs.flatten())))
   if options.output:
-    with open(options.output, 'w') as f:
+    with open(options.output, 'wb') as f:
       print_all(configs.file_comment, data, f)
   else:
     print_all(configs.file_comment, data, sys.stdout)
--- tools/isolate/merge_isolate_test.py
+++ tools/isolate/merge_isolate_test.py
@@ -54,7 +54,7 @@ class MergeGyp(unittest.TestCase):
       pass
 
   def test_load_gyp_empty(self):
-    self.assertEquals({}, merge_isolate.load_gyp({}, None).flatten())
+    self.assertEquals({}, merge_isolate.load_gyp({}, None, []).flatten())
 
   def test_load_gyp(self):
     value = {
@@ -118,7 +118,8 @@ class MergeGyp(unittest.TestCase):
         KEY_UNTRACKED: ['b', 'f', 'h'],
       },
     }
-    self.assertEquals(expected, merge_isolate.load_gyp(value, None).flatten())
+    self.assertEquals(
+        expected, merge_isolate.load_gyp(value, None, []).flatten())
 
   def test_load_gyp_duplicate_command(self):
     value = {
@@ -134,7 +135,7 @@ class MergeGyp(unittest.TestCase):
       ],
     }
     try:
-      merge_isolate.load_gyp(value, None)
+      merge_isolate.load_gyp(value, None, [])
       self.fail()
     except AssertionError:
       pass
@@ -150,7 +151,7 @@ class MergeGyp(unittest.TestCase):
       KEY_TRACKED: ['a'],
       KEY_UNTRACKED: ['b'],
     }
-    actual = merge_isolate.load_gyp(value, None)
+    actual = merge_isolate.load_gyp(value, None, [])
     # Flattening the whole config will discard 'None'.
     self.assertEquals({}, actual.flatten())
     self.assertEquals([None], actual.per_os.keys())
@@ -333,8 +334,8 @@ class MergeGyp(unittest.TestCase):
     actual = merge_isolate.union(
         merge_isolate.union(
           merge_isolate.Configs([], None),
-          merge_isolate.load_gyp({}, None)),
-        merge_isolate.load_gyp({}, None)).flatten()
+          merge_isolate.load_gyp({}, None, [])),
+        merge_isolate.load_gyp({}, None, [])).flatten()
     self.assertEquals({}, actual)
 
   def test_merge_empty(self):
@@ -380,8 +381,8 @@ class MergeGyp(unittest.TestCase):
     configs = merge_isolate.union(
         merge_isolate.union(
           merge_isolate.Configs([], None),
-          merge_isolate.load_gyp(linux, None)),
-        merge_isolate.load_gyp(mac, None)).flatten()
+          merge_isolate.load_gyp(linux, None, [])),
+        merge_isolate.load_gyp(mac, None, [])).flatten()
     self.assertEquals(expected, configs)
 
   def test_load_three_conditions(self):
@@ -438,9 +439,9 @@ class MergeGyp(unittest.TestCase):
         merge_isolate.union(
           merge_isolate.union(
             merge_isolate.Configs([], None),
-            merge_isolate.load_gyp(linux, None)),
-          merge_isolate.load_gyp(mac, None)),
-        merge_isolate.load_gyp(win, None)).flatten()
+            merge_isolate.load_gyp(linux, None, [])),
+          merge_isolate.load_gyp(mac, None, [])),
+        merge_isolate.load_gyp(win, None, [])).flatten()
     self.assertEquals(expected, configs)
 
   def test_merge_three_conditions(self):
@@ -493,19 +494,19 @@ class MergeGyp(unittest.TestCase):
     # Pylint is confused with merge_isolate.union() return type.
     # pylint: disable=E1103
     configs = merge_isolate.union(
-        merge_isolate.load_gyp({}, '# Yo dawg!\n# Chill out.\n'),
-        merge_isolate.load_gyp({}, None))
+        merge_isolate.load_gyp({}, '# Yo dawg!\n# Chill out.\n', []),
+        merge_isolate.load_gyp({}, None, []))
     self.assertEquals('# Yo dawg!\n# Chill out.\n', configs.file_comment)
 
     configs = merge_isolate.union(
-        merge_isolate.load_gyp({}, None),
-        merge_isolate.load_gyp({}, '# Yo dawg!\n# Chill out.\n'))
+        merge_isolate.load_gyp({}, None, []),
+        merge_isolate.load_gyp({}, '# Yo dawg!\n# Chill out.\n', []))
     self.assertEquals('# Yo dawg!\n# Chill out.\n', configs.file_comment)
 
     # Only keep the first one.
     configs = merge_isolate.union(
-        merge_isolate.load_gyp({}, '# Yo dawg!\n'),
-        merge_isolate.load_gyp({}, '# Chill out.\n'))
+        merge_isolate.load_gyp({}, '# Yo dawg!\n', []),
+        merge_isolate.load_gyp({}, '# Chill out.\n', []))
     self.assertEquals('# Yo dawg!\n', configs.file_comment)
 
   def test_extract_comment(self):
