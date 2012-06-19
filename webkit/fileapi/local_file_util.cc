dd041b060f2c54a01886c1a9555448f85183b2c7
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 02:17:55 +0000

    Fix symbolic link handling in fileapi
    In isolated file system:
    - disallows following symlinks in a directory / subdirectories
    - do not list symlinks in a directory / subdirectories
    - allows symlink access if the symlink file itself is selected (i.e. it's a top-level entry)
    
    In any other sandboxed file systems:
    - disallow symlink access at all (not only in ReadDirectory)
    
    Removes AbstractFileEnumerator::IsLink.
    
    BUG=none
    TEST=manually tested for isolated files (if a symlink file is dropped it should be viewable, but if a directory is dropped which contains symlinks those symlinks should not be listed/viewable).
    
    Review URL: https://chromiumcodereview.appspot.com/10557035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142899 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/local_file_util.cc
+++ webkit/fileapi/local_file_util.cc
@@ -37,7 +37,6 @@ class LocalFileEnumerator : public FileSystemFileUtil::AbstractFileEnumerator {
   virtual int64 Size() OVERRIDE;
   virtual base::Time LastModifiedTime() OVERRIDE;
   virtual bool IsDirectory() OVERRIDE;
-  virtual bool IsLink() OVERRIDE;
 
  private:
   file_util::FileEnumerator file_enum_;
@@ -48,6 +47,9 @@ class LocalFileEnumerator : public FileSystemFileUtil::AbstractFileEnumerator {
 
 FilePath LocalFileEnumerator::Next() {
   FilePath next = file_enum_.Next();
+  // Don't return symlinks.
+  while (!next.empty() && file_util::IsLink(next))
+    next = file_enum_.Next();
   if (next.empty())
     return next;
   file_enum_.GetFindInfo(&file_util_info_);
@@ -67,10 +69,6 @@ base::Time LocalFileEnumerator::LastModifiedTime() {
 
 bool LocalFileEnumerator::IsDirectory() {
   return file_util::FileEnumerator::IsDirectory(file_util_info_);
-}
-
-bool LocalFileEnumerator::IsLink() {
-  return file_util::FileEnumerator::IsLink(file_util_info_);
 }
 
 LocalFileUtil::LocalFileUtil() {
@@ -128,6 +126,9 @@ PlatformFileError LocalFileUtil::GetFileInfo(
   PlatformFileError error = GetLocalFilePath(context, path, &file_path);
   if (error != base::PLATFORM_FILE_OK)
     return error;
+  // We should not follow symbolic links in sandboxed file system.
+  if (file_util::IsLink(file_path))
+    return base::PLATFORM_FILE_ERROR_NOT_FOUND;
   error = NativeFileUtil::GetFileInfo(file_path, file_info);
   if (error == base::PLATFORM_FILE_OK)
     *platform_file_path = file_path;
