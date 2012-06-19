0bceb401abb57a5e9bf06011c187d1943fa8085d
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 14:50:34 +0000

    Revert 142944 - [cros] Fake a stub user login when no --login-manager and --login-user are given.
    Reverting due to failures on Linux ChromiumOS Tests (2), see http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%282%29/builds/3745:
    
    BrowserActionsContainerTest.Basic
    ExistingUserControllerTest.AutoEnrollAfterSignIn
    ExistingUserControllerTest.ExistingUserLogin
    ExistingUserControllerTest.NewUserDontAutoEnrollAfterSignIn
    ExtensionsStartupTest.NoFileAccess
    ExtensionsStartupTest.Test
    LoginProfileTest.UserNotPassed
    PrefsTabHelperBrowserTest.PrefsAreMigratedToFontMap
    PrefsTabHelperBrowserTest2.GlobalPrefsAreMigrated
    
    
    This is a reland of 142372 with fixes for tests.
    
    BUG=126765
    TEST=Manual on Linux without --login-manager and --login-user
    
    Review URL: https://chromiumcodereview.appspot.com/10561005
    
    TBR=ivankr@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10579016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142952 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager_impl.cc
+++ chrome/browser/chromeos/login/user_manager_impl.cc
@@ -69,6 +69,9 @@ namespace {
 // depends on that and it's hard to figure out what).
 const char kGuestUser[] = "";
 
+// Stub user email (for test paths).
+const char kStubUser[] = "stub-user@example.com";
+
 // Names of nodes with info about user image.
 const char kImagePathNodeName[] = "path";
 const char kImageIndexNodeName[] = "index";
@@ -78,6 +81,9 @@ const char kWallpaperIndexNodeName[] = "index";
 
 const int kThumbnailWidth = 128;
 const int kThumbnailHeight = 80;
+
+// Index of the default image used for the |kStubUser| user.
+const int kStubDefaultImageIndex = 0;
 
 // Delay betweeen user login and attempt to update user's profile image.
 const long kProfileImageDownloadDelayMs = 10000;
@@ -198,7 +204,18 @@ UserManagerImpl::UserManagerImpl()
       observed_sync_service_(NULL),
       last_image_set_async_(false),
       downloaded_profile_image_data_url_(chrome::kAboutBlankURL) {
+  // If we're not running on ChromeOS, and are not showing the login manager
+  // or attempting a command line login? Then login the stub user.
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (!base::chromeos::IsRunningOnChromeOS() &&
+      !command_line->HasSwitch(switches::kLoginManager) &&
+      !command_line->HasSwitch(switches::kLoginPassword) &&
+      !command_line->HasSwitch(switches::kGuestSession)) {
+    StubUserLoggedIn();
+  }
+
   MigrateWallpaperData();
+
   registrar_.Add(this, chrome::NOTIFICATION_OWNER_KEY_FETCH_ATTEMPT_SUCCEEDED,
       content::NotificationService::AllSources());
   registrar_.Add(this, chrome::NOTIFICATION_PROFILE_ADDED,
@@ -222,6 +239,14 @@ const UserList& UserManagerImpl::GetUsers() const {
 
 void UserManagerImpl::UserLoggedIn(const std::string& email,
                                    bool browser_restart) {
+  // Remove the stub user if it is still around.
+  if (logged_in_user_) {
+    DCHECK(IsLoggedInAsStub());
+    delete logged_in_user_;
+    logged_in_user_ = NULL;
+    is_current_user_ephemeral_ = false;
+  }
+
   if (email == kGuestUser) {
     GuestUserLoggedIn();
     return;
@@ -334,6 +359,13 @@ void UserManagerImpl::EphemeralUserLoggedIn(const std::string& email) {
   NotifyOnLogin();
 }
 
+void UserManagerImpl::StubUserLoggedIn() {
+  is_current_user_ephemeral_ = true;
+  logged_in_user_ = new User(kStubUser, false);
+  logged_in_user_->SetImage(UserImage(GetDefaultImage(kStubDefaultImageIndex)),
+                            kStubDefaultImageIndex);
+}
+
 void UserManagerImpl::InitializeWallpaper() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
@@ -349,6 +381,10 @@ void UserManagerImpl::InitializeWallpaper() {
             SetDefaultWallpaper(ash::GetSolidColorIndex());
       }
     }
+    return;
+  } else if (IsLoggedInAsStub()) {
+    ash::Shell::GetInstance()->desktop_background_controller()->
+        SetDefaultWallpaper(ash::GetInvalidWallpaperIndex());
     return;
   }
   UserSelected(GetLoggedInUser().email());
@@ -629,7 +665,7 @@ void UserManagerImpl::Observe(int type,
           base::Unretained(this)));
       break;
     case chrome::NOTIFICATION_PROFILE_ADDED:
-      if (IsUserLoggedIn() && !IsLoggedInAsGuest()) {
+      if (IsUserLoggedIn() && !IsLoggedInAsGuest() && !IsLoggedInAsStub()) {
         Profile* profile = content::Source<Profile>(source).ptr();
         if (!profile->IsOffTheRecord() &&
             profile == ProfileManager::GetDefaultProfile()) {
@@ -647,7 +683,7 @@ void UserManagerImpl::Observe(int type,
 }
 
 void UserManagerImpl::OnStateChanged() {
-  DCHECK(IsUserLoggedIn() && !IsLoggedInAsGuest());
+  DCHECK(IsUserLoggedIn() && !IsLoggedInAsGuest() && !IsLoggedInAsStub());
   AuthError::State state = observed_sync_service_->GetAuthError().state();
   if (state != AuthError::NONE &&
       state != AuthError::CONNECTION_FAILED &&
@@ -890,8 +926,8 @@ bool UserManagerImpl::AreEphemeralUsersEnabled() const {
 }
 
 bool UserManagerImpl::IsEphemeralUser(const std::string& email) const {
-  // The guest and stub user always are ephemeral.
-  if (email == kGuestUser || email == kStubUser)
+  // The guest user always is ephemeral.
+  if (email == kGuestUser)
     return true;
 
   // The currently logged-in user is ephemeral iff logged in as ephemeral.
@@ -1009,6 +1045,13 @@ void UserManagerImpl::GetLoggedInUserWallpaperProperties(
     int* index) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(logged_in_user_);
+
+  if (IsLoggedInAsStub()) {
+    *type = current_user_wallpaper_type_ = User::DEFAULT;
+    *index = current_user_wallpaper_index_ = ash::GetInvalidWallpaperIndex();
+    return;
+  }
+
   GetUserWallpaperProperties(GetLoggedInUser().email(), type, index);
 }
 
