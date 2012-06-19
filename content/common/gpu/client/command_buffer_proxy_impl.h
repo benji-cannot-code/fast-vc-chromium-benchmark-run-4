6f6977efb6f193af1635c0985b6ec3031260517b
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-19 00:03:51 +0000

    gpu: add SignalSyncPoint to complement WaitSyncPoint
    BUG=112299
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10541185
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142869 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/command_buffer_proxy_impl.h
+++ content/common/gpu/client/command_buffer_proxy_impl.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
+#include "base/hash_tables.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/weak_ptr.h"
 #include "content/common/gpu/client/gpu_video_decode_accelerator_host.h"
@@ -68,6 +69,8 @@ class CommandBufferProxyImpl
   virtual bool EnsureBackbuffer() OVERRIDE;
   virtual uint32 InsertSyncPoint() OVERRIDE;
   virtual void WaitSyncPoint(uint32 sync_point) OVERRIDE;
+  virtual bool SignalSyncPoint(uint32 sync_point,
+                               const base::Closure& callback) OVERRIDE;
   virtual void SetMemoryAllocationChangedCallback(
       const base::Callback<void(const GpuMemoryAllocationForRenderer&)>&
           callback) OVERRIDE;
@@ -106,6 +109,10 @@ class CommandBufferProxyImpl
   GpuChannelHost* channel() const { return channel_; }
 
  private:
+  typedef std::map<int32, gpu::Buffer> TransferBufferMap;
+  typedef std::map<int, scoped_refptr<GpuVideoDecodeAcceleratorHost> > Decoders;
+  typedef base::hash_map<uint32, base::Closure> SignalTaskMap;
+
   // Send an IPC message over the GPU channel. This is private to fully
   // encapsulate the channel; all callers of this function must explicitly
   // verify that the context has not been lost.
@@ -118,17 +125,16 @@ class CommandBufferProxyImpl
   void OnEchoAck();
   void OnConsoleMessage(const GPUCommandBufferConsoleMessage& message);
   void OnSetMemoryAllocation(const GpuMemoryAllocationForRenderer& allocation);
+  void OnSignalSyncPointAck(uint32 id);
 
   // Try to read an updated copy of the state from shared memory.
   void TryUpdateState();
 
   // Local cache of id to transfer buffer mapping.
-  typedef std::map<int32, gpu::Buffer> TransferBufferMap;
   TransferBufferMap transfer_buffers_;
 
   // Zero or more video decoder hosts owned by this proxy, keyed by their
   // decoder_route_id.
-  typedef std::map<int, scoped_refptr<GpuVideoDecodeAcceleratorHost> > Decoders;
   Decoders video_decoder_hosts_;
 
   // The last cached state received from the service.
@@ -155,6 +161,10 @@ class CommandBufferProxyImpl
       memory_allocation_changed_callback_;
 
   GpuConsoleMessageCallback console_message_callback_;
+
+  // Tasks to be invoked in SignalSyncPoint responses.
+  uint32 next_signal_id_;
+  SignalTaskMap signal_tasks_;
 
   DISALLOW_COPY_AND_ASSIGN(CommandBufferProxyImpl);
 };
