aaa783e49e3478b6b464b9f49a3e1770008475a5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-19 13:59:20 +0000

    Replaced printf with fprintf in DEBUG_AUDIONODE_REFERENCES
    Change printf to fprintf in DEBUG_AUDIONODE_REFERENCES
    https://bugs.webkit.org/show_bug.cgi?id=86564
    
    Patch by Andrei Poenaru <poenaru@adobe.com> on 2012-06-19
    Reviewed by Csaba Osztrogonác.
    
    There is no need for new tests: writing data to stderr, instead of stdout.
    
    * Modules/webaudio/AudioContext.cpp:
    (WebCore::AudioContext::~AudioContext):
    * Modules/webaudio/AudioNode.cpp:
    (WebCore::AudioNode::~AudioNode):
    (WebCore::AudioNode::ref):
    (WebCore::AudioNode::finishDeref):
    (WebCore::AudioNode::printNodeCounts):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120707 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioNode.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioNode.cpp
@@ -63,7 +63,7 @@ AudioNode::~AudioNode()
 {
 #if DEBUG_AUDIONODE_REFERENCES
     --s_nodeCount[nodeType()];
-    printf("%p: %d: AudioNode::~AudioNode() %d %d\n", this, nodeType(), m_normalRefCount, m_connectionRefCount);
+    fprintf(stderr, "%p: %d: AudioNode::~AudioNode() %d %d\n", this, nodeType(), m_normalRefCount, m_connectionRefCount);
 #endif
 }
 
@@ -317,7 +317,7 @@ void AudioNode::ref(RefType refType)
     }
 
 #if DEBUG_AUDIONODE_REFERENCES
-    printf("%p: %d: AudioNode::ref(%d) %d %d\n", this, nodeType(), refType, m_normalRefCount, m_connectionRefCount);
+    fprintf(stderr, "%p: %d: AudioNode::ref(%d) %d %d\n", this, nodeType(), refType, m_normalRefCount, m_connectionRefCount);
 #endif
 
     // See the disabling code in finishDeref() below. This handles the case where a node
@@ -380,7 +380,7 @@ void AudioNode::finishDeref(RefType refType)
     }
     
 #if DEBUG_AUDIONODE_REFERENCES
-    printf("%p: %d: AudioNode::deref(%d) %d %d\n", this, nodeType(), refType, m_normalRefCount, m_connectionRefCount);
+    fprintf(stderr, "%p: %d: AudioNode::deref(%d) %d %d\n", this, nodeType(), refType, m_normalRefCount, m_connectionRefCount);
 #endif
 
     if (!m_connectionRefCount) {
@@ -406,15 +406,15 @@ int AudioNode::s_nodeCount[NodeTypeEnd];
 
 void AudioNode::printNodeCounts()
 {
-    printf("\n\n");
-    printf("===========================\n");
-    printf("AudioNode: reference counts\n");
-    printf("===========================\n");
+    fprintf(stderr, "\n\n");
+    fprintf(stderr, "===========================\n");
+    fprintf(stderr, "AudioNode: reference counts\n");
+    fprintf(stderr, "===========================\n");
 
     for (unsigned i = 0; i < NodeTypeEnd; ++i)
-        printf("%d: %d\n", i, s_nodeCount[i]);
+        fprintf(stderr, "%d: %d\n", i, s_nodeCount[i]);
 
-    printf("===========================\n\n\n");
+    fprintf(stderr, "===========================\n\n\n");
 }
 
 #endif // DEBUG_AUDIONODE_REFERENCES
