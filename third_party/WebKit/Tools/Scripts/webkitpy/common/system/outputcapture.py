5f1124f6ac371c7010ab5cb65d22620b1a72627f
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-20 04:01:29 +0000

    test-webkitpy: add a -p flag to pass through captured output to enable debugging https://bugs.webkit.org/show_bug.cgi?id=89158
    Reviewed by Eric Seidel.
    
    Normally when outputcapture runs it intercepts stdout/stderr,
    which makes running under the debugger difficult and mostly
    pointless (since you can't see any output). This change adds a
    flag to test-webkitpy (-p) that will cause outputcapture to pass
    through the output as well as capture it.
    
    * Scripts/webkitpy/common/system/outputcapture.py:
    (OutputCapture.stream_wrapper):
    (OutputCapture._capture_output_with_name):
    * Scripts/webkitpy/common/system/outputcapture_unittest.py:
    (OutputCaptureTest.setUp):
    * Scripts/webkitpy/test/main.py:
    (Tester._parse_args):
    (Tester._run_tests):
    (Tester._log_exception):
    (_CaptureAndPassThroughStream):
    (_CaptureAndPassThroughStream.__init__):
    (_CaptureAndPassThroughStream.write):
    (_CaptureAndPassThroughStream._message_is_from_pdb):
    (_CaptureAndPassThroughStream.flush):
    (_CaptureAndPassThroughStream.getvalue):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/outputcapture.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/outputcapture.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -36,6 +36,13 @@ from StringIO import StringIO
 
 
 class OutputCapture(object):
+    # By default we capture the output to a stream. Other modules may override
+    # this function in order to do things like pass through the output. See
+    # webkitpy.test.main for an example.
+    @staticmethod
+    def stream_wrapper(stream):
+        return StringIO()
+
     def __init__(self):
         self.saved_outputs = dict()
         self._log_level = logging.INFO
@@ -46,8 +53,9 @@ class OutputCapture(object):
             self._logs_handler.setLevel(self._log_level)
 
     def _capture_output_with_name(self, output_name):
-        self.saved_outputs[output_name] = getattr(sys, output_name)
-        captured_output = StringIO()
+        stream = getattr(sys, output_name)
+        captured_output = self.stream_wrapper(stream)
+        self.saved_outputs[output_name] = stream
         setattr(sys, output_name, captured_output)
         return captured_output
 
