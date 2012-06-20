9c14c31ca4e193a0f7d2765510a45195e4a69797
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-20 19:17:43 +0000

    [chromium] webkit-backface-visibility doesn't work with video https://bugs.webkit.org/show_bug.cgi?id=88908
    When determining a contents layer's backface culling, use the parent
    layer's transform and backface-visibility properties. Track which
    layers need this special treatment with useParentBackfaceVisibility
    and setUseParentBackfaceVisibility functions in WebCore::LayerChromium,
    WebKit::WebLayer, and WebCore::CCLayerImpl.
    
    Patch by Christopher Cameron <ccameron@chromium.org> on 2012-06-20
    Reviewed by Adrienne Walker.
    
    Source/Platform:
    
    * chromium/public/WebLayer.h:
        (WebLayer):
            Add an accessor to specify that a layer should use its parent's
            backface culling behavior.
    
    Source/WebCore:
    
    Tests: compositing/backface-visibility/backface-visibility-image.html
           compositing/backface-visibility/backface-visibility-webgl.html
    
    * platform/graphics/chromium/GraphicsLayerChromium.cpp:
    (WebCore::GraphicsLayerChromium::setupContentsLayer):
        When a contents layer is added, tag it as inheriting its backface
        culling from its parent.
    * platform/graphics/chromium/LayerChromium.cpp:
        (WebCore::LayerChromium::LayerChromium):
            Initialize new m_useParentBackfaceVisibility member variable.
        (WebCore::LayerChromium::pushPropertiesTo):
            Propagate m_useParentBackfaceVisibility to CCLayerImpl.
    * platform/graphics/chromium/LayerChromium.h:
        (WebCore::LayerChromium::setUseParentBackfaceVisibility):
        (WebCore::LayerChromium::useParentBackfaceVisibility):
        (LayerChromium):
            Add m_useParentBackfaceVisibility member variable and modify
            and query accessors.
    * platform/graphics/chromium/cc/CCLayerImpl.cpp:
        (WebCore::CCLayerImpl::CCLayerImpl):
            Initialize new m_useParentBackfaceVisibility member variable.
    * platform/graphics/chromium/cc/CCLayerImpl.h:
        (WebCore::CCLayerImpl::setUseParentBackfaceVisibility):
        (WebCore::CCLayerImpl::useParentBackfaceVisibility):
        (CCLayerImpl):
            Add m_useParentBackfaceVisibility member variable and modify
            and query accessors.
    * platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp:
        (WebCore::layerShouldBeSkipped):
            When examining a layer, if the layer has
            useParentBackfaceVisibility set then use the layer's parent
            layer to determine backface culling.
    
    Source/WebKit/chromium:
    
    * src/WebLayer.cpp:
        (WebKit::WebLayer::setUseParentBackfaceVisibility):
            Add an accessor to specify that a layer should use its parent's
            backface culling behavior.
    
    LayoutTests:
    
    * compositing/backface-visibility/backface-visibility-image-expected.png: Added.
    * compositing/backface-visibility/backface-visibility-image-expected.txt: Added.
    * compositing/backface-visibility/backface-visibility-image.html: Added.
    * compositing/backface-visibility/backface-visibility-webgl-expected.png: Added.
    * compositing/backface-visibility/backface-visibility-webgl-expected.txt: Added.
    * compositing/backface-visibility/backface-visibility-webgl.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LayerChromium.h
@@ -193,6 +193,9 @@ public:
     void setPreserves3D(bool preserve3D) { m_preserves3D = preserve3D; }
     bool preserves3D() const { return m_preserves3D; }
 
+    void setUseParentBackfaceVisibility(bool useParentBackfaceVisibility) { m_useParentBackfaceVisibility = useParentBackfaceVisibility; }
+    bool useParentBackfaceVisibility() const { return m_useParentBackfaceVisibility; }
+
     void setUsesLayerClipping(bool usesLayerClipping) { m_usesLayerClipping = usesLayerClipping; }
     bool usesLayerClipping() const { return m_usesLayerClipping; }
 
@@ -375,6 +378,7 @@ private:
     bool m_usesLayerClipping;
     bool m_isNonCompositedContent;
     bool m_preserves3D;
+    bool m_useParentBackfaceVisibility;
     bool m_alwaysReserveTextures;
     bool m_drawCheckerboardForMissingTiles;
     bool m_forceRenderSurface;
