b1ba4894f4d4a0fb5e9be7edc210a11726a7a891
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 02:03:33 +0000

    create a theme source when starting the downloads page
    BUG=132964
    TEST=start downloading something large, watch the progress bar on the downloads page (note: you must avoid going to the NTP, which creates a theme source)
    
    Review URL: https://chromiumcodereview.appspot.com/10582025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143122 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/downloads_ui.cc
+++ chrome/browser/ui/webui/downloads_ui.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
 #include "chrome/browser/ui/webui/downloads_dom_handler.h"
+#include "chrome/browser/ui/webui/theme_source.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
@@ -100,6 +101,10 @@ DownloadsUI::DownloadsUI(content::WebUI* web_ui) : WebUIController(web_ui) {
   ChromeWebUIDataSource* source = CreateDownloadsUIHTMLSource();
   source->set_use_json_js_format_v2();
   ChromeURLDataManager::AddDataSource(profile, source);
+#if defined(ENABLE_THEMES)
+  ThemeSource* theme = new ThemeSource(profile);
+  ChromeURLDataManager::AddDataSource(profile, theme);
+#endif
 }
 
 // static
