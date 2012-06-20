2839f64d1c3ee528ed321dfef834fb6853261c57
dcaiafa@chromium.org <dcaiafa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 00:03:34 +0000

    Fix crash on it2me disconnect on the mac.
    In it2me, the destructor of EventExecutor does not get called in the same thread
    from where it's used. This is causing the Timer object used in EventExecutorMac
    to CHECK in the destructor because it requires that it be destroyed in the same
    thread where it was first started - even if it has already been stopped.
    
    So instead of reusing the Timer and starting and stopping it on demand,
    EventExecutorMac will now create and destroy the timer on session start and
    stop, respectively.
    
    BUG=133017
    TEST=Connect and disconnect with it2me. Repeat with me2me.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10557012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143089 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/clipboard_mac.mm
+++ remoting/host/clipboard_mac.mm
@@ -28,6 +28,7 @@ namespace remoting {
 class ClipboardMac : public Clipboard {
  public:
   ClipboardMac();
+  virtual ~ClipboardMac();
 
   // Must be called on the UI thread.
   virtual void Start(
@@ -40,13 +41,19 @@ class ClipboardMac : public Clipboard {
   void CheckClipboardForChanges();
 
   scoped_ptr<protocol::ClipboardStub> client_clipboard_;
-  base::RepeatingTimer<ClipboardMac> clipboard_polling_timer_;
+  scoped_ptr<base::RepeatingTimer<ClipboardMac> > clipboard_polling_timer_;
   NSInteger current_change_count_;
 
   DISALLOW_COPY_AND_ASSIGN(ClipboardMac);
 };
 
 ClipboardMac::ClipboardMac() : current_change_count_(0) {
+}
+
+ClipboardMac::~ClipboardMac() {
+  // In it2me the destructor is not called in the same thread that the timer is
+  // created. Thus the timer must have already been destroyed by now.
+  DCHECK(clipboard_polling_timer_.get() == NULL);
 }
 
 void ClipboardMac::Start(scoped_ptr<protocol::ClipboardStub> client_clipboard) {
@@ -58,7 +65,8 @@ void ClipboardMac::Start(scoped_ptr<protocol::ClipboardStub> client_clipboard) {
 
   // OS X doesn't provide a clipboard-changed notification. The only way to
   // detect clipboard changes is by polling.
-  clipboard_polling_timer_.Start(FROM_HERE,
+  clipboard_polling_timer_.reset(new base::RepeatingTimer<ClipboardMac>());
+  clipboard_polling_timer_->Start(FROM_HERE,
       base::TimeDelta::FromMilliseconds(kClipboardPollingIntervalMs),
       this, &ClipboardMac::CheckClipboardForChanges);
 }
@@ -82,7 +90,7 @@ void ClipboardMac::InjectClipboardEvent(const protocol::ClipboardEvent& event) {
 }
 
 void ClipboardMac::Stop() {
-  clipboard_polling_timer_.Stop();
+  clipboard_polling_timer_.reset();
   client_clipboard_.reset();
 }
 
