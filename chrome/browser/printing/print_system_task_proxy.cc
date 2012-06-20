cd217e5d06b7cdc3ba6b85a2e766ca747d9a758e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 21:59:51 +0000

    Get rid of UNIT_TEST in a cc file. Review URL: https://chromiumcodereview.appspot.com/10583041
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143269 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_system_task_proxy.cc
+++ chrome/browser/printing/print_system_task_proxy.cc
@@ -316,12 +316,6 @@ PrintSystemTaskProxy::PrintSystemTaskProxy(
       has_logged_printers_count_(has_logged_printers_count) {
 }
 
-#if defined(UNIT_TEST) && defined(USE_CUPS)
-// Only used for testing.
-PrintSystemTaskProxy::PrintSystemTaskProxy() {
-}
-#endif
-
 PrintSystemTaskProxy::~PrintSystemTaskProxy() {
 }
 
