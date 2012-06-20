1d64a8e959778d9b66d5b492f4ec7fd7371894e7
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 23:30:36 +0000

    inject platform_apps.js into iframes in platform apps
    BUG=none
    TEST=included
    
    Review URL: https://chromiumcodereview.appspot.com/10581009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143295 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/user_script_slave.h
+++ chrome/renderer/extensions/user_script_slave.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -37,7 +37,7 @@ class UserScriptSlave {
  public:
   // Utility to get the URL we will match against for a frame. If the frame has
   // committed, this is the commited URL. Otherwise it is the provisional URL.
-  static GURL GetDataSourceURLForFrame(WebKit::WebFrame* frame);
+  static GURL GetDataSourceURLForFrame(const WebKit::WebFrame* frame);
 
   explicit UserScriptSlave(const ExtensionSet* extensions);
   ~UserScriptSlave();
---
+++ chrome/test/data/extensions/platform_apps/restrictions/sandboxed_iframe.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.onload = function() {
+  try {
+    window.alert('should throw');
+    window.parent.postMessage({'success': false,
+                               'reason' : 'should have thrown'},
+                              '*');
+  } catch(e) {
+    var message = e.message || e;
+    var succ = message.indexOf('is not available in packaged apps') != -1;
+    window.parent.postMessage({'success': succ,
+                               'reason' : 'got wrong error: ' + message},
+                              '*');
+
+  }
+};
