74182185417a61e16a8b81b3ee056867b86faa97
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 05:31:12 +0000

    [net] Implement MappedHostResolver::ResolveFromCache.
    BUG=133608
    TEST=./net_unittests --gtest_filter=MappedHostResolverTest.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/10581032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143148 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mapped_host_resolver.cc
+++ net/base/mapped_host_resolver.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -25,21 +25,14 @@ int MappedHostResolver::Resolve(const RequestInfo& info,
                                 const CompletionCallback& callback,
                                 RequestHandle* out_req,
                                 const BoundNetLog& net_log) {
-  DCHECK(addresses);
-  DCHECK_EQ(false, callback.is_null());
-  // Modify the request before forwarding it to |impl_|.
-  RequestInfo modified_info = info;
-  HostPortPair host_port(info.host_port_pair());
-  if (rules_.RewriteHost(&host_port))
-    modified_info.set_host_port_pair(host_port);
-  return impl_->Resolve(modified_info, addresses, callback, out_req, net_log);
+  return impl_->Resolve(ApplyRules(info), addresses, callback, out_req,
+                        net_log);
 }
 
 int MappedHostResolver::ResolveFromCache(const RequestInfo& info,
                                          AddressList* addresses,
                                          const BoundNetLog& net_log) {
-  NOTIMPLEMENTED();
-  return ERR_UNEXPECTED;
+  return impl_->ResolveFromCache(ApplyRules(info), addresses, net_log);
 }
 
 void MappedHostResolver::CancelRequest(RequestHandle req) {
@@ -52,6 +45,15 @@ void MappedHostResolver::ProbeIPv6Support() {
 
 HostCache* MappedHostResolver::GetHostCache() {
   return impl_->GetHostCache();
+}
+
+HostResolver::RequestInfo MappedHostResolver::ApplyRules(
+    const RequestInfo& info) const {
+  RequestInfo modified_info = info;
+  HostPortPair host_port(info.host_port_pair());
+  if (rules_.RewriteHost(&host_port))
+    modified_info.set_host_port_pair(host_port);
+  return modified_info;
 }
 
 }  // namespace net
