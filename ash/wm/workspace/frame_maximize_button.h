663b6a602f3c2d481842938a26cfe955828432c5
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 00:35:56 +0000

    Revert 143092 - Make ImageButton and NetworkMenuIcon work in high DPI
    Test=Manual
    Bug=None
    
    Testing steps
    1) Run chromium using --force-device-scale-factor=2 --highlight-missing-2x-resources --load-2x-resources
    2) Ensure that the hot image is not pixelated
    
    Review URL: https://chromiumcodereview.appspot.com/10546160
    
    TBR=pkotwicz@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10574035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143096 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/frame_maximize_button.h
+++ ash/wm/workspace/frame_maximize_button.h
@@ -41,7 +41,7 @@ class ASH_EXPORT FrameMaximizeButton : public views::ImageButton {
 
  protected:
   // ImageButton overrides:
-  virtual gfx::ImageSkia GetImageToPaint(float scale) OVERRIDE;
+  virtual gfx::ImageSkia GetImageToPaint() OVERRIDE;
 
  private:
   class EscapeEventFilter;
