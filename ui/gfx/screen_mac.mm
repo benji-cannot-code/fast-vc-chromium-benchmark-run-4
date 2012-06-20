7e672eddb099f829d64f044a51dbaa3f3357cc3f
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 23:45:20 +0000

    Fix for screen_mac::GetDisplayNearestPoint. The input point may not always be inside the display bounds.
    BUG=133692
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10580042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143300 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/screen_mac.mm
+++ ui/gfx/screen_mac.mm
@@ -158,8 +158,7 @@ gfx::Display Screen::GetDisplayNearestPoint(const gfx::Point& point) {
     if (NSMouseInRect(ns_point, [screen frame], NO))
       return GetDisplayForScreen(screen, screen == primary);
   }
-  NOTREACHED();
-  return gfx::Display();
+  return GetPrimaryDisplay();
 }
 
 }  // namespace gfx
