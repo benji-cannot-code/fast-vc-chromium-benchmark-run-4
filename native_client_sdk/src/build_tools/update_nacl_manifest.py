ed56096bc98feff4b2c392970d4d4022508b497d
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-20 19:22:22 +0000

    [NaCl SDK] Add option to update_nacl_manifest to provide gsutil.
    BUG=none
    TEST=none
    TBR=noelallen@chromium.org
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/10575038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143227 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/update_nacl_manifest.py
+++ native_client_sdk/src/build_tools/update_nacl_manifest.py
@@ -11,6 +11,7 @@ in manifest.
 import buildbot_common
 import csv
 import manifest_util
+import optparse
 import os
 import posixpath
 import re
@@ -160,8 +161,11 @@ class Delegate(object):
 
 
 class RealDelegate(Delegate):
-  def __init__(self):
-    pass
+  def __init__(self, gsutil=None):
+    if gsutil:
+      self.gsutil = gsutil
+    else:
+      self.gsutil = buildbot_common.GetGsutil()
 
   def GetRepoManifest(self):
     """See Delegate.GetRepoManifest"""
@@ -214,7 +218,7 @@ class RealDelegate(Delegate):
           operation such as ls, cp or cat.
     Returns:
       The stdout from the process."""
-    cmd = [buildbot_common.GetGsutil()] + list(args)
+    cmd = [self.gsutil] + list(args)
     if stdin:
       stdin_pipe = subprocess.PIPE
     else:
@@ -538,7 +542,12 @@ def Run(delegate, platforms):
 
 
 def main(args):
-  delegate = RealDelegate()
+  parser = optparse.OptionParser()
+  parser.add_option('--gsutil', help='path to gsutil', dest='gsutil',
+      default=None)
+  options, args = parser.parse_args(args[1:])
+
+  delegate = RealDelegate(options.gsutil)
   Run(delegate, ('mac', 'win', 'linux'))
 
 
