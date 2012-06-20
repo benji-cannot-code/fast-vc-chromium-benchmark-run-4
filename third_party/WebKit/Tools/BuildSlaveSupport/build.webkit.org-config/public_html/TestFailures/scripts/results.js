8ae40f5caf656db229fa4719a9a119ce7a944b11
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-20 02:12:29 +0000

    garden-o-matic fails to rebaseline tests with MISSING results https://bugs.webkit.org/show_bug.cgi?id=89521
    Reviewed by Kenneth Russell.
    
    If the bots report "MISSING", we should try to find PNG and TXT results on the server.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js
@@ -158,6 +158,7 @@ results.failureTypeToExtensionList = function(failureType)
         return [kWAVExtension];
     case TEXT:
         return [kTXTExtension];
+    case MISSING:
     case IMAGE_TEXT:
         return [kTXTExtension, kPNGExtension];
     default:
