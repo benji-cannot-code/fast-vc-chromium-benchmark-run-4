2c13cc20b2e2e3c97f6c0adcf89b700e4ad51297
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-20 00:09:05 +0000

    Source/WebCore: Make DOMFileSystem selectively structure clonable so it can be the body of a web intent. https://bugs.webkit.org/show_bug.cgi?id=88267
    Patch by Steve VanDeBogart <vandebo@chromium.org> on 2012-06-19
    Reviewed by David Levin.
    
    This functionality will allow FileSystem API objects to be dispatched to
    web content in order to enable, for instance, an intent notifying web
    content that a particular filesystem has been mounted by the local
    system. The goal at this point is not to enable across-the-board
    clonability, but just for handling the particular use case.
    
    * bindings/v8/SerializedScriptValue.cpp:
    (WebCore::V8ObjectMap::Writer::writeDOMFileSystem):
    (WebCore::V8ObjectMap::Serializer::writeDOMFileSystem):
    (WebCore::V8ObjectMap::Serializer::doSerialize):
    (WebCore::V8ObjectMap::Reader::read):
    (WebCore::V8ObjectMap::Reader::readDOMFileSystem):
    
    LayoutTests: Add filesystem regression test to check that the filesystem object is
    not serializable as created by javascript.
    https://bugs.webkit.org/show_bug.cgi?id=88267
    
    Patch by Greg Billock <gbillock@google.com> on 2012-06-19
    Reviewed by David Levin.
    
    * fast/filesystem/filesystem-unserializable-expected.html: Added.
    * fast/filesystem/filesystem-unserializable.html: Added.
    * fast/filesystem/script-tests/filesystem-unserializable.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/filesystem/DOMFileSystemBase.cpp
+++ third_party/WebKit/Source/WebCore/Modules/filesystem/DOMFileSystemBase.cpp
@@ -63,6 +63,7 @@ DOMFileSystemBase::DOMFileSystemBase(ScriptExecutionContext* context, const Stri
     , m_name(name)
     , m_type(type)
     , m_filesystemRootURL(rootURL)
+    , m_clonable(false)
     , m_asyncFileSystem(asyncFileSystem)
 {
 }
