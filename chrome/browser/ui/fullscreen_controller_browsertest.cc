29ade37d952f6406c3e8fcfdc4d66a1c1a936a49
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 04:57:05 +0000

    FullscreenControllerBrowserTest.FullscreenMouseLockContentSettings is flaky.
    BUG=133831
    TEST=none
    TBR=scheib@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10600014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143347 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/fullscreen_controller_browsertest.cc
+++ chrome/browser/ui/fullscreen_controller_browsertest.cc
@@ -166,9 +166,18 @@ void FullscreenControllerBrowserTest::TestFullscreenMouseLockContentSettings() {
   ASSERT_FALSE(IsMouseLockPermissionRequested());
 }
 
+#if defined(OS_MACOSX) || defined(OS_LINUX)
+// http://crbug.com/133831
+#define MAYBE_FullscreenMouseLockContentSettings \
+    FLAKY_FullscreenMouseLockContentSettings
+#else
+#define MAYBE_FullscreenMouseLockContentSettings \
+    FullscreenMouseLockContentSettings
+#endif
+
 // Tests fullscreen and Mouse Lock with varying content settings ALLOW & BLOCK.
 IN_PROC_BROWSER_TEST_F(FullscreenControllerBrowserTest,
-                       FullscreenMouseLockContentSettings) {
+                       MAYBE_FullscreenMouseLockContentSettings) {
   TestFullscreenMouseLockContentSettings();
 }
 
@@ -331,4 +340,3 @@ IN_PROC_BROWSER_TEST_F(
   }
 }
 #endif
-
