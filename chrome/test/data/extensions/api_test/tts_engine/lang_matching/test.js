4cba355b7ce7679efd2f5b977ffe0821ae765d0a
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 01:00:02 +0000

    Fix TTS extension API language matching.
    Now if an extension registers a voice with lang 'fr-FR', a speech
    request with lang 'fr' will match, and vice versa. Strict matches
    still take precedence.
    
    BUG=133470
    TEST=adds new browser test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10581011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143320 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/tts_engine/lang_matching/test.js
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TTS api test for Chrome on ChromeOS.
+// browser_tests.exe --gtest_filter="TtsApiTest.*"
+
+chrome.test.runTests([
+  function testWordCallbacks() {
+    // Register listeners for speech functions, enabling this extension
+    // to be a TTS engine.
+    var speakListener = function(utterance, options, sendTtsEvent) {
+      chrome.test.assertNoLastError();
+      sendTtsEvent({'type': 'end', 'charIndex': utterance.length});
+    };
+    var stopListener = function() {};
+    chrome.ttsEngine.onSpeak.addListener(speakListener);
+    chrome.ttsEngine.onStop.addListener(stopListener);
+
+    // Make sure that a lang of 'fr-FR' goes to our engine,
+    // even though the engine only registered 'fr'.
+    chrome.tts.speak(
+        'dummy utterance',
+        {
+         'lang': 'fr-FR',
+         'onEvent': function(event) {
+           chrome.test.assertNoLastError();
+           chrome.test.succeed();
+         }
+        },
+        function() {
+          chrome.test.assertNoLastError();
+        });
+  }
+]);
