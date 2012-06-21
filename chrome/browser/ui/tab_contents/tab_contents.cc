4d37a4324f8f21db238ee23970b9f0f7121608bf
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 21:43:49 +0000

    Infobar for NPAPI plugins required in metro mode
    The infobar offers to switch to desktop chrome, where previously we would
    not say anthing or before that we would pop the plugin installer infobar.
    
    BUG=133111
    TEST=see bug
    Review URL: https://chromiumcodereview.appspot.com/10619002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143459 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents.cc
+++ chrome/browser/ui/tab_contents/tab_contents.cc
@@ -54,10 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
 
-#if defined(OS_WIN)
-#include "base/win/metro.h"
-#endif
-
 using content::WebContents;
 
 namespace {
@@ -140,14 +136,7 @@ TabContents::TabContents(WebContents* contents)
   safe_browsing_tab_observer_.reset(
       new safe_browsing::SafeBrowsingTabObserver(this));
 
-#if defined(OS_WIN)
-  // Metro mode Chrome on Windows does not support plugins. Avoid registering
-  // the PluginObserver so we don't popup plugin-related infobars.
-  if (!base::win::IsMetroProcess())
-    plugin_observer_.reset(new PluginObserver(this));
-#else
   plugin_observer_.reset(new PluginObserver(this));
-#endif
 
 #if !defined(OS_ANDROID)
   if (OmniboxSearchHint::IsEnabled(profile()))
