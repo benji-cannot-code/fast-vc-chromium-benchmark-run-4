63fe5e882bee3858f3b6c09be98426bb2f5a05ec
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 21:06:04 +0000

    Expose more BaseWindow functionality to apps via chrome.appWindow.
    R=aa@chromium.org,mihaip@chromium.org
    
    BUG=131912,130464
    
    
    Review URL: https://chromiumcodereview.appspot.com/10581048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143451 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/app_window_custom_bindings.js
+++ chrome/renderer/resources/extensions/app_window_custom_bindings.js
@@ -21,4 +21,10 @@ chromeHidden.registerCustomHook('appWindow', function(bindingsAPI) {
       delete request.callback;
     }
   })
+  apiFunctions.setHandleRequest('moveTo', function(x, y) {
+    window.moveTo(x, y);
+  })
+  apiFunctions.setHandleRequest('resizeTo', function(width, height) {
+    window.resizeTo(width, height);
+  })
 });
