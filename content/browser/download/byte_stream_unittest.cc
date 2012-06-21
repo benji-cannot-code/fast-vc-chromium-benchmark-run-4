44724810e28236c7bb8d21abb7834f0530ce3047
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 17:31:56 +0000

    Remove old Sleep and PostDelayedTask interfaces that use int ms instead of TimeDelta.
    The previous version of this patch was reverted due to crashing cros_x86 and cros_tegra2 builds.  See: http://codereview.chromium.org/9703053/
    
    BUG=108171
    
    Review URL: https://chromiumcodereview.appspot.com/10572030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143401 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/byte_stream_unittest.cc
+++ content/browser/download/byte_stream_unittest.cc
@@ -34,14 +34,7 @@ class MockTaskRunner : public base::SequencedTaskRunner {
 
   // TaskRunner functions.
   MOCK_METHOD3(PostDelayedTask, bool(const tracked_objects::Location&,
-                                     const base::Closure&, int64));
-  MOCK_METHOD3(PostDelayedTask, bool(const tracked_objects::Location&,
                                      const base::Closure&, base::TimeDelta));
-
-  MOCK_METHOD3(PostNonNestableDelayedTask, bool(
-      const tracked_objects::Location&,
-      const base::Closure&,
-      int64 delay_ms));
 
   MOCK_METHOD3(PostNonNestableDelayedTask, bool(
       const tracked_objects::Location&,
@@ -338,7 +331,7 @@ TEST_F(ByteStreamTest, ByteStream_SinkCallback) {
   int num_callbacks = 0;
   byte_stream_output->RegisterCallback(
       base::Bind(CountCallbacks, &num_callbacks));
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
 
@@ -412,7 +405,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceCallback) {
   EXPECT_TRUE(ValidateIOBuffer(output_io_buffer, output_length));
 
   // Setup expectations.
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
 
@@ -431,7 +424,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceCallback) {
   EXPECT_EQ(1, num_callbacks);
 
   // Same drill with final buffer.
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
   EXPECT_EQ(content::ByteStreamReader::STREAM_HAS_DATA,
@@ -470,7 +463,7 @@ TEST_F(ByteStreamTest, ByteStream_SinkInterrupt) {
   int num_callbacks = 0;
   byte_stream_output->RegisterCallback(
       base::Bind(CountCallbacks, &num_callbacks));
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
 
@@ -536,7 +529,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceInterrupt) {
   message_loop_.RunAllPending();
 
   // Setup expectations.
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
 
@@ -557,7 +550,7 @@ TEST_F(ByteStreamTest, ByteStream_SourceInterrupt) {
   EXPECT_EQ(1, num_alt_callbacks);
 
   // Third get should also trigger callback.
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
   EXPECT_EQ(content::ByteStreamReader::STREAM_HAS_DATA,
@@ -589,7 +582,7 @@ TEST_F(ByteStreamTest, ByteStream_ZeroCallback) {
   int num_callbacks = 0;
   byte_stream_output->RegisterCallback(
       base::Bind(CountCallbacks, &num_callbacks));
-  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, 0))
+  EXPECT_CALL(*task_runner.get(), PostDelayedTask(_, _, base::TimeDelta()))
       .WillOnce(DoAll(SaveArg<1>(&intermediate_callback),
                       Return(true)));
 
