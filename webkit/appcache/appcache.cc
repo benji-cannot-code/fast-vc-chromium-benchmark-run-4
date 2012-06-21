02157275437a03726b80449e901957496871b318
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 17:59:48 +0000

    Don't ignore network namespace entries when '*' is in the list.
    BUG=133816
    TEST=appcache_unittest + i need to update a layout test
    Review URL: https://chromiumcodereview.appspot.com/10575044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143410 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/appcache/appcache.cc
+++ webkit/appcache/appcache.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -153,11 +153,8 @@ void AppCache::InitializeWithDatabaseRecords(
   std::sort(fallback_namespaces_.begin(), fallback_namespaces_.end(),
             SortNamespacesByLength);
 
-  if (!online_whitelist_all_) {
-    for (size_t i = 0; i < whitelists.size(); ++i) {
-      online_whitelist_namespaces_.push_back(whitelists.at(i).namespace_url);
-    }
-  }
+  for (size_t i = 0; i < whitelists.size(); ++i)
+    online_whitelist_namespaces_.push_back(whitelists.at(i).namespace_url);
 }
 
 void AppCache::ToDatabaseRecords(
@@ -210,13 +207,11 @@ void AppCache::ToDatabaseRecords(
     record.target_url = fallback_namespaces_[i].target_url;
   }
 
-  if (!online_whitelist_all_) {
-    for (size_t i = 0; i < online_whitelist_namespaces_.size(); ++i) {
-      whitelists->push_back(AppCacheDatabase::OnlineWhiteListRecord());
-      AppCacheDatabase::OnlineWhiteListRecord& record = whitelists->back();
-      record.cache_id = cache_id_;
-      record.namespace_url = online_whitelist_namespaces_[i];
-    }
+  for (size_t i = 0; i < online_whitelist_namespaces_.size(); ++i) {
+    whitelists->push_back(AppCacheDatabase::OnlineWhiteListRecord());
+    AppCacheDatabase::OnlineWhiteListRecord& record = whitelists->back();
+    record.cache_id = cache_id_;
+    record.namespace_url = online_whitelist_namespaces_[i];
   }
 }
 
