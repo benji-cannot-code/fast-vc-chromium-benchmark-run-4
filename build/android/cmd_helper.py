55be7b6969ee9175fe0ee3c32101ac47a0cbae1c
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 01:57:32 +0000

    Revert "Revert 143261 - Upstreaming android tools"
    - fixing permissions and relanding. CQ does not run
      check_perms apparently.
    
    - original review:
      https://chromiumcodereview.appspot.com/10578032/
    
    - failing bot log:
      http://build.chromium.org/p/chromium/builders/Linux/builds/26592/steps/check_perms/logs/stdio
    
    This reverts commit 1ad94575470ccb61f9a64915638aafac3269a28f.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10598004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143329 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/cmd_helper.py
+++ build/android/cmd_helper.py
@@ -1,7 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+#!/usr/bin/python
+#
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+"""A wrapper for subprocess to make calling shell commands easier."""
 
 
 import logging
@@ -16,13 +20,16 @@ def RunCmd(args, cwd=None):
       the string or the first item in the args sequence.
     cwd: If not None, the subprocess's current directory will be changed to
       |cwd| before it's executed.
+
+  Returns:
+    Return code from the command execution.
   """
   logging.info(str(args) + ' ' + (cwd or ''))
   p = subprocess.Popen(args=args, cwd=cwd)
   return p.wait()
 
 
-def GetCmdOutput(args, cwd=None):
+def GetCmdOutput(args, cwd=None, shell=False):
   """Open a subprocess to execute a program and returns its output.
 
   Args:
@@ -30,10 +37,15 @@ def GetCmdOutput(args, cwd=None):
       the string or the first item in the args sequence.
     cwd: If not None, the subprocess's current directory will be changed to
       |cwd| before it's executed.
+    shell: Whether to execute args as a shell command.
+
+  Returns:
+    Captures and returns the command's stdout.
+    Prints the command's stderr to logger (which defaults to stdout).
   """
   logging.info(str(args) + ' ' + (cwd or ''))
   p = subprocess.Popen(args=args, cwd=cwd, stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
+                       stderr=subprocess.PIPE, shell=shell)
   stdout, stderr = p.communicate()
   if stderr:
     logging.critical(stderr)
