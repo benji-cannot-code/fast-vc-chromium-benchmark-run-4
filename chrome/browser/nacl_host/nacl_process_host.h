d9d1a3db9dd943fa152455ea88934a18dd5d0a33
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 23:07:04 +0000

    Add an IPC channel between the NaCl loader process and the renderer. BUG=116317 TEST=manual Review URL: https://chromiumcodereview.appspot.com/10214007
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143483 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/nacl_process_host.h
+++ chrome/browser/nacl_host/nacl_process_host.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/nacl_types.h"
 #include "content/public/browser/browser_child_process_host_delegate.h"
 #include "googleurl/src/gurl.h"
+#include "ipc/ipc_channel_handle.h"
 
 class ChromeRenderMessageFilter;
 class CommandLine;
@@ -87,7 +88,7 @@ class NaClProcessHost : public content::BrowserChildProcessHostDelegate {
 
   // Sends the reply message to the renderer who is waiting for the plugin
   // to load. Returns true on success.
-  bool ReplyToRenderer();
+  bool ReplyToRenderer(const IPC::ChannelHandle& channel_handle);
 
   // Sends the message to the NaCl process to load the plugin. Returns true
   // on success.
@@ -113,6 +114,8 @@ class NaClProcessHost : public content::BrowserChildProcessHostDelegate {
   bool AttachDebugExceptionHandler(const std::string& info,
                                    IPC::Message* reply_msg);
 #endif
+
+  void OnPpapiChannelCreated(const IPC::ChannelHandle& channel_handle);
 
   GURL manifest_url_;
 
@@ -152,6 +155,8 @@ class NaClProcessHost : public content::BrowserChildProcessHostDelegate {
   bool enable_exception_handling_;
 
   bool off_the_record_;
+
+  bool enable_ipc_proxy_;
 
   DISALLOW_COPY_AND_ASSIGN(NaClProcessHost);
 };
--- ppapi/native_client/src/trusted/plugin/nacl_entry_points.h
+++ ppapi/native_client/src/trusted/plugin/nacl_entry_points.h
@@ -20,12 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef bool (*LaunchNaClProcessFunc)(PP_Instance instance,
                                       const char* url,
                                       int socket_count,
-                                      nacl::Handle* result_sockets);
-
-typedef bool (*StartPpapiProxyFunc)(PP_Instance instance);
+                                      nacl::Handle* result_sockets,
+                                      void** ipc_channel_handle);
 
 
 extern LaunchNaClProcessFunc launch_nacl_process;
-extern StartPpapiProxyFunc start_ppapi_proxy;
 
 #endif  // NATIVE_CLIENT_SRC_TRUSTED_PLUGIN_NACL_ENTRY_POINTS_H_
