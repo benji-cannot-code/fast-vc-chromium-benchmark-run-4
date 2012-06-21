2a50f083ee80c8bbd3d507c323d5f60618ebc2c8
ronghuawu@chromium.org <ronghuawu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-21 21:10:37 +0000

    Fix a hang when the RtcVideoDecoder was given a MediaStream without video track, in which case there won't be any frame from RtcVideoDecoder. The hang happened when the page tried to destroy the WebMediaPlayer but the pipeline can't be stopped because it's still in the seeking state, which in turn becauses the RtcVideoDecoder never returned a frame.
    This cl override the PrepareForShutdownHack, set the shutting_down flag and fullfill any pending read in it.
    
    BUG=http://code.google.com/p/webrtc/issues/detail?id=508
    TEST=
    http://code.google.com/p/webrtc/issues/attachmentText?id=508&aid=5080004000&name=apprtc_loopback_audio_only.html&token=ghcCq45Vu7SH7HwcKsfjSo8ldqA%3A1340221943627
    https://s3.amazonaws.com/webrtc-test/freeze_test.html
    Review URL: https://chromiumcodereview.appspot.com/10581044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143452 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/rtc_video_decoder.h
+++ content/renderer/media/rtc_video_decoder.h
@@ -37,6 +37,7 @@ class CONTENT_EXPORT RTCVideoDecoder
   virtual void Reset(const base::Closure& clusure) OVERRIDE;
   virtual void Stop(const base::Closure& clusure) OVERRIDE;
   virtual const gfx::Size& natural_size() OVERRIDE;
+  virtual void PrepareForShutdownHack() OVERRIDE;
 
   // cricket::VideoRenderer implementation
   virtual bool SetSize(int width, int height, int reserved) OVERRIDE;
@@ -58,12 +59,15 @@ class CONTENT_EXPORT RTCVideoDecoder
     kStopped
   };
 
+  void CancelPendingRead();
+
   MessageLoop* message_loop_;
   gfx::Size visible_size_;
   std::string url_;
   DecoderState state_;
   ReadCB read_cb_;
   bool got_first_frame_;
+  bool shutting_down_;
   base::TimeDelta last_frame_timestamp_;
   base::TimeDelta start_time_;
 
