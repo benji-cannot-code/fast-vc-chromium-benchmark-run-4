a2389e7f918a1a01705aa4c6e583cb1213d22c61
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-21 00:31:41 +0000

    IndexedDB: Remove redundant IDBObjectStore.delete() overloads https://bugs.webkit.org/show_bug.cgi?id=89587
    Reviewed by Darin Fisher.
    
    Source/WebCore:
    
    The IDBObjectStore.delete(IDBKey) overload can delegate to the delete(IDBKeyRange)
    overload. Also cleaned up redundant checks in related overloads.
    
    Test: storage/indexeddb/deleted-objects.html
    
    * Modules/indexeddb/IDBCursorBackendImpl.cpp:
    (WebCore::IDBCursorBackendImpl::deleteFunction): Call keyRange variant instead.
    * Modules/indexeddb/IDBIndex.cpp:
    (WebCore::IDBIndex::get): Remove redundant deleted/invalid IDBKey checks.
    (WebCore::IDBIndex::getKey): Remove redundant deleted/invalid IDBKey checks.
    * Modules/indexeddb/IDBKeyRange.cpp: Null IDBKeys are not acceptable; currently
    not passed in by any callers.
    (WebCore::IDBKeyRange::only):
    (WebCore::IDBKeyRange::lowerBound):
    (WebCore::IDBKeyRange::upperBound):
    (WebCore::IDBKeyRange::bound):
    * Modules/indexeddb/IDBObjectStore.cpp:
    (WebCore::IDBObjectStore::get): Was missing deleted check in IDBKeyRange overload.
    Removed redundant checks in IDBKey overload.
    (WebCore::IDBObjectStore::deleteFunction): Delegate to IDBKeyRange overload.
    * Modules/indexeddb/IDBObjectStoreBackendImpl.cpp: Remove IDBKey overload.
    * Modules/indexeddb/IDBObjectStoreBackendImpl.h: Remove IDBKey overload.
    (IDBObjectStoreBackendImpl):
    * Modules/indexeddb/IDBObjectStoreBackendInterface.h: Remove IDBKey overload.
    
    Source/WebKit/chromium:
    
    * public/WebIDBObjectStore.h: Add note to remove overload when Chromium is updated.
    (WebIDBObjectStore):
    * src/IDBObjectStoreBackendProxy.cpp: Delete IDBKey overload.
    * src/IDBObjectStoreBackendProxy.h: Delete IDBKey overload.
    (IDBObjectStoreBackendProxy):
    * src/WebIDBObjectStoreImpl.cpp: Delete IDBKey overload.
    * src/WebIDBObjectStoreImpl.h: Delete IDBKey overload.
    (WebIDBObjectStoreImpl):
    
    LayoutTests:
    
    * storage/indexeddb/deleted-objects-expected.txt:
    * storage/indexeddb/resources/deleted-objects.js: Test IDBKeyRange overloads.
    (openDatabase.request.onsuccess.request.onsuccess):
    (openDatabase.request.onsuccess):
    (openDatabase):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@120888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebIDBObjectStoreImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebIDBObjectStoreImpl.h
@@ -53,7 +53,6 @@ public:
 
     void get(const WebIDBKeyRange&, WebIDBCallbacks*, const WebIDBTransaction&, WebExceptionCode&);
     void put(const WebSerializedScriptValue&, const WebIDBKey&, PutMode, WebIDBCallbacks*, const WebIDBTransaction&, WebExceptionCode&);
-    void deleteFunction(const WebIDBKey&, WebIDBCallbacks*, const WebIDBTransaction&, WebExceptionCode&);
     void deleteFunction(const WebIDBKeyRange&, WebIDBCallbacks*, const WebIDBTransaction&, WebExceptionCode&);
     void clear(WebIDBCallbacks*, const WebIDBTransaction&, WebExceptionCode&);
 
