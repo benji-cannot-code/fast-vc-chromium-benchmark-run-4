31564d72fcac45168909ba30a8273a9dc96e8fda
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 08:08:38 +0000

    Handle Escape key on constrained window.
    Escape key is used to be handled by JS-side in each dialog implementation. This CL removes JS code and change to handle Escape key by C++-side in constrained window implementation globally.
    
    This CL is based with based on http://crrev.com/127012 (reverted).
    
    BUG=119684
    TEST=manual, trybots pass on win and linux_chromeos
    
    Review URL: https://chromiumcodereview.appspot.com/10446076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143560 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/certificate_viewer.js
+++ chrome/browser/resources/certificate_viewer.js
@@ -47,12 +47,6 @@ cr.define('cert_viewer', function() {
     stripGtkAccessorKeys();
 
     $('export').onclick = exportCertificate;
-
-    // TODO(kochi): ESC key should be handled in the views window side.
-    document.addEventListener('keydown', function(e) {
-      if (e.keyCode == 27)  // ESC
-        chrome.send('DialogClose');
-    });
   }
 
   /**
