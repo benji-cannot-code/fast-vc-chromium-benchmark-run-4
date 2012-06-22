34d2674c15076b7281fb4024bdcf539238f460e3
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 23:50:03 +0000

    Add support for basic web_contents methods in ContentView (android).
    This adds the basic navigation functionality and some other basic calls
    that were simply calling into WebContents.
    
    Again, this is for ContentShell upstreaming in android.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10581051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143741 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/navigation_controller_impl.cc
+++ content/browser/web_contents/navigation_controller_impl.cc
@@ -429,13 +429,17 @@ NavigationEntry* NavigationControllerImpl::GetEntryAtIndex(
 
 NavigationEntry* NavigationControllerImpl::GetEntryAtOffset(
     int offset) const {
-  int index = (transient_entry_index_ != -1) ?
-                  transient_entry_index_ + offset :
-                  last_committed_entry_index_ + offset;
+  int index = GetIndexForOffset(offset);
   if (index < 0 || index >= GetEntryCount())
     return NULL;
 
   return entries_[index].get();
+}
+
+int NavigationControllerImpl::GetIndexForOffset(int offset) const {
+  return (transient_entry_index_ != -1) ?
+             transient_entry_index_ + offset :
+             last_committed_entry_index_ + offset;
 }
 
 bool NavigationControllerImpl::CanGoBack() const {
@@ -445,6 +449,11 @@ bool NavigationControllerImpl::CanGoBack() const {
 bool NavigationControllerImpl::CanGoForward() const {
   int index = GetCurrentEntryIndex();
   return index >= 0 && index < (static_cast<int>(entries_.size()) - 1);
+}
+
+bool NavigationControllerImpl::CanGoToOffset(int offset) const {
+  int index = GetIndexForOffset(offset);
+  return index >= 0 && index < GetEntryCount();
 }
 
 void NavigationControllerImpl::GoBack() {
@@ -520,13 +529,10 @@ void NavigationControllerImpl::GoToIndex(int index) {
 }
 
 void NavigationControllerImpl::GoToOffset(int offset) {
-  int index = (transient_entry_index_ != -1) ?
-                  transient_entry_index_ + offset :
-                  last_committed_entry_index_ + offset;
-  if (index < 0 || index >= GetEntryCount())
+  if (!CanGoToOffset(offset))
     return;
 
-  GoToIndex(index);
+  GoToIndex(GetIndexForOffset(offset));
 }
 
 void NavigationControllerImpl::RemoveEntryAtIndex(int index) {
