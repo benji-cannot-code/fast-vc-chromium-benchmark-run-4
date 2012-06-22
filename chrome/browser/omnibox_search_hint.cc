063f5562cd49c3235e4442589fa8e3f6449b6ba6
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 04:18:47 +0000

    Simplify overriding ShouldExpire() a bit by allowing classes to override ShouldExpireInternal() instead, which fits most uses.
    Also simplifies the geolocation infobar by adding a protected setter to the base class.
    
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10584038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143529 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/omnibox_search_hint.cc
+++ chrome/browser/omnibox_search_hint.cc
@@ -66,8 +66,6 @@ class HintInfoBar : public ConfirmInfoBarDelegate {
   void AllowExpiry() { should_expire_ = true; }
 
   // ConfirmInfoBarDelegate:
-  virtual bool ShouldExpire(
-      const content::LoadCommittedDetails& details) const OVERRIDE;
   virtual void InfoBarDismissed() OVERRIDE;
   virtual gfx::Image* GetIcon() const OVERRIDE;
   virtual Type GetInfoBarType() const OVERRIDE;
@@ -75,6 +73,8 @@ class HintInfoBar : public ConfirmInfoBarDelegate {
   virtual int GetButtons() const OVERRIDE;
   virtual string16 GetButtonLabel(InfoBarButton button) const OVERRIDE;
   virtual bool Accept() OVERRIDE;
+  virtual bool ShouldExpireInternal(
+      const content::LoadCommittedDetails& details) const OVERRIDE;
 
   // The omnibox hint that shows us.
   OmniboxSearchHint* omnibox_hint_;
@@ -108,11 +108,6 @@ HintInfoBar::HintInfoBar(OmniboxSearchHint* omnibox_hint)
 HintInfoBar::~HintInfoBar() {
   if (!action_taken_)
     UMA_HISTOGRAM_COUNTS("OmniboxSearchHint.Ignored", 1);
-}
-
-bool HintInfoBar::ShouldExpire(
-    const content::LoadCommittedDetails& details) const {
-  return details.is_navigation_to_different_page() && should_expire_;
 }
 
 void HintInfoBar::InfoBarDismissed() {
@@ -151,6 +146,11 @@ bool HintInfoBar::Accept() {
   omnibox_hint_->DisableHint();
   omnibox_hint_->ShowEnteringQuery();
   return true;
+}
+
+bool HintInfoBar::ShouldExpireInternal(
+    const content::LoadCommittedDetails& details) const {
+  return should_expire_;
 }
 
 
--- chrome/browser/tab_contents/simple_alert_infobar_delegate.cc
+++ chrome/browser/tab_contents/simple_alert_infobar_delegate.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,11 +21,6 @@ SimpleAlertInfoBarDelegate::SimpleAlertInfoBarDelegate(
 SimpleAlertInfoBarDelegate::~SimpleAlertInfoBarDelegate() {
 }
 
-bool SimpleAlertInfoBarDelegate::ShouldExpire(
-      const content::LoadCommittedDetails& details) const {
-  return auto_expire_ && ConfirmInfoBarDelegate::ShouldExpire(details);
-}
-
 gfx::Image* SimpleAlertInfoBarDelegate::GetIcon() const {
   return icon_;
 }
@@ -36,4 +31,9 @@ string16 SimpleAlertInfoBarDelegate::GetMessageText() const {
 
 int SimpleAlertInfoBarDelegate::GetButtons() const {
   return BUTTON_NONE;
+}
+
+bool SimpleAlertInfoBarDelegate::ShouldExpireInternal(
+      const content::LoadCommittedDetails& details) const {
+  return auto_expire_ && ConfirmInfoBarDelegate::ShouldExpireInternal(details);
 }
