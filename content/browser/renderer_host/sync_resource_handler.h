91b5a3c291099fba33eaf09a4367f08d7e14d1d4
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 20:49:13 +0000

    Revert r143458 (which re-applies r143341 and r142979).  This CL also includes unit tests for DataReceived and DataReceived_ACK messaging.
    Make ResourceHandlers that defer loading also be responsible for calling Resume
    on their ResourceController.
    
    What this does is avoid out-of-band calls to Resume the ResourceLoader, and
    that means that an intermediate ResourceHandler (for example the
    BufferedResourceHandler) will be able to implement ResourceController.
    
    Consider the problematic scenerio that happens today:
    
    1- The BufferedResourceHandler consumes response headers and buffers network
    data, delaying notifications to its downstream ResourceHandler.
    2- The BufferedResourceHandler decides if it is dealing with a download or
    not, and optionally installs a DownloadResourceHandler as the downstream
    handler.
    3- The BufferedResourceHandler replays the OnResponseStarted and
    OnReadCompleted events to push data to its downstream handler.
    4- The downstream handler might decide to defer processing.
    5- When the downstream handler wants to resume processing, it needs to
    tell someone to Resume.
    
    At step 5, the complicated thing we do to ourselves today is we make that
    Resume step tell the ResourceLoader to resume instead of the
    BufferedResourceHandler.
    
    This causes problems for the BufferedResourceHandler as it may start receiving
    more network events before it has finished flushing its existing buffers to its
    downstream handler.
    
    This patch is a step toward fixing the above problem. It makes all handlers
    call Resume on their own controller when it is time to resume. A subsequent
    patch will leverage this to cleanup and simplify the BufferedResourceHandler
    as well as the overall pause/resume semantics of the ResourceLoader.
    
    Other cleanup in this patch: The {Async,Sync,CrossSite}ResourceHandler
    classes are now all constructed with an URLRequest. This avoids messy
    code to call ResourceDispatcherHostImpl::GetURLRequest(). Since the
    lifetime of ResourceHandlers is now bound to the lifetime of their
    associated URLRequest, this is a safe and simple change to make. Other
    ResourceHandlers, like the BufferedResourceHandler, were already holding
    onto their associated URLRequest.
    
    R=jam@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10644011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143702 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/sync_resource_handler.h
+++ content/browser/renderer_host/sync_resource_handler.h
@@ -18,6 +18,7 @@ class Message;
 
 namespace net {
 class IOBuffer;
+class URLRequest;
 }
 
 namespace content {
@@ -29,7 +30,7 @@ class ResourceMessageFilter;
 class SyncResourceHandler : public ResourceHandler {
  public:
   SyncResourceHandler(ResourceMessageFilter* filter,
-                      const GURL& url,
+                      net::URLRequest* request,
                       IPC::Message* result_message,
                       ResourceDispatcherHostImpl* resource_dispatcher_host);
   virtual ~SyncResourceHandler();
@@ -65,6 +66,7 @@ class SyncResourceHandler : public ResourceHandler {
 
   SyncLoadResult result_;
   scoped_refptr<ResourceMessageFilter> filter_;
+  net::URLRequest* request_;
   IPC::Message* result_message_;
   ResourceDispatcherHostImpl* rdh_;
 };
