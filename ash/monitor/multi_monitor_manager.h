bd0de7d13a2e5f190e2d07ddc7dc3194ee08b16a
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 22:09:08 +0000

    Update capture on all root windows  This is necessary to properly forward all events on all root windows to the captured window and release when capture is cleared.
    Original plan was is to do native capture, but it turns out to be difficult
    (at least on chromeos/X11) because we need to warp the pointer to other window and handle mouse event there to support multiple displays layouts (see http://codereview.chromium.org/10581008/)
    
     This also update Window::ConvertPointToWindow so that it accepts
    windows on different root windows.
    
    BUG=123160
    TEST=ExtendedDesktopText.{Capture|ConvertPoint}
    
    Review URL: https://chromiumcodereview.appspot.com/10638008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143722 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/monitor/multi_monitor_manager.h
+++ ash/monitor/multi_monitor_manager.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "ui/aura/monitor_manager.h"
 #include "ui/aura/root_window_observer.h"
 #include "ui/aura/window.h"
@@ -62,13 +63,14 @@ class ASH_EXPORT MultiMonitorManager : public aura::MonitorManager,
                                    const gfx::Size& new_size) OVERRIDE;
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(ExtendedDesktopTest, ConvertPoint);
   typedef std::vector<gfx::Display> Displays;
 
   void Init();
   void AddRemoveMonitorImpl();
   void CycleMonitorImpl();
   void ScaleMonitorImpl();
-  gfx::Display& FindDisplayById(int id);
+  gfx::Display& FindDisplayForRootWindow(const aura::RootWindow* root);
 
   Displays displays_;
 
--- ui/aura/test/test_window_delegate.h
+++ ui/aura/test/test_window_delegate.h
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_AURA_TEST_TEST_WINDOW_DELEGATE_H_
 #pragma once
 
+#include <string>
+
 #include "base/compiler_specific.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/aura/window_delegate.h"
@@ -90,6 +92,39 @@ class MaskedWindowDelegate : public TestWindowDelegate {
   gfx::Rect mask_rect_;
 
   DISALLOW_COPY_AND_ASSIGN(MaskedWindowDelegate);
+};
+
+// Keeps track of mouse/key events.
+class EventCountDelegate : public TestWindowDelegate {
+ public:
+  EventCountDelegate();
+
+  // Overridden from TestWindowDelegate:
+  virtual bool OnMouseEvent(MouseEvent* event) OVERRIDE;
+  virtual bool OnKeyEvent(KeyEvent* event) OVERRIDE;
+
+  // Returns the counts of mouse motion events in the
+  // form of "<enter> <move> <leave>".
+  std::string GetMouseMotionCountsAndReset();
+
+  // Returns the counts of mouse button events in the
+  // form of "<press> <release>".
+  std::string GetMouseButtonCountsAndReset();
+
+  // Returns the counts of key events in the form of
+  // "<press> <release>".
+  std::string GetKeyCountsAndReset();
+
+ private:
+  int mouse_enter_count_;
+  int mouse_move_count_;
+  int mouse_leave_count_;
+  int mouse_press_count_;
+  int mouse_release_count_;
+  int key_press_count_;
+  int key_release_count_;
+
+  DISALLOW_COPY_AND_ASSIGN(EventCountDelegate);
 };
 
 }  // namespace test
