9e52a001131756907e6659efb282d92fccb2d25e
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 04:10:08 +0000

    Remove redundant dependency from chromeos::CryptohomeLibrary to src/chrome and src/content
    BUG=None
    TEST=build success
    
    Review URL: https://chromiumcodereview.appspot.com/10611002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cryptohome_library.cc
+++ chrome/browser/chromeos/cros/cryptohome_library.cc
@@ -5,18 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/cros/cryptohome_library.h"
 
-#include "base/command_line.h"
-#include "base/message_loop.h"
+#include <map>
+
+#include "base/memory/weak_ptr.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
-#include "chrome/common/chrome_switches.h"
 #include "chromeos/dbus/cryptohome_client.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
-#include "content/public/browser/browser_thread.h"
-#include "crypto/encryptor.h"
 #include "crypto/sha2.h"
-
-using content::BrowserThread;
 
 namespace {
 
