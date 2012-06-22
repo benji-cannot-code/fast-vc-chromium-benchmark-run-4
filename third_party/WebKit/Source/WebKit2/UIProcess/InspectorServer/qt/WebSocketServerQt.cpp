ad551dfff8d576cff0a06b85ec6922eb78d5f733
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-22 13:24:31 +0000

    [Qt] Fix the remote inspector loading problems on Mac https://bugs.webkit.org/show_bug.cgi?id=89747
    Patch by Jocelyn Turcotte <turcotte.j@gmail.com> on 2012-06-22
    Reviewed by Simon Hausmann.
    
    Source/WebCore:
    
    Make sure that the state of a SocketStreamHandle is set properly
    when created from an existing QTcpSocket.
    
    This fixes the inspectorserver Qt api auto test on Mac.
    
    * platform/network/qt/SocketStreamHandleQt.cpp:
    (WebCore::SocketStreamHandle::SocketStreamHandle):
    
    Source/WebKit2:
    
    Reverse the creation order of the inter-dependent WebSocketServerConnection
    and SocketStreamHandle to make sure that the later has a client properly
    set on construction.
    
    This is to work around the assert on m_state in SocketStreamHandle::setClient.
    
    * UIProcess/InspectorServer/WebSocketServer.cpp:
    (WebKit::WebSocketServer::didAcceptConnection):
    * UIProcess/InspectorServer/WebSocketServer.h:
    (WebKit::WebSocketServer::client):
    (WebSocketServer):
    * UIProcess/InspectorServer/WebSocketServerConnection.cpp:
    (WebKit::WebSocketServerConnection::WebSocketServerConnection):
    (WebKit::WebSocketServerConnection::setSocketHandle):
    (WebKit):
    * UIProcess/InspectorServer/WebSocketServerConnection.h:
    (WebSocketServerConnection):
    * UIProcess/InspectorServer/qt/WebSocketServerQt.cpp:
    (WebKit::QtTcpServerHandler::handleNewConnection):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/InspectorServer/qt/WebSocketServerQt.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/InspectorServer/qt/WebSocketServerQt.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebSocketServerQt.h"
 
 #include "WebSocketServer.h"
+#include "WebSocketServerConnection.h"
 #include <WebCore/SocketStreamHandle.h>
 #include <wtf/PassOwnPtr.h>
 
@@ -56,7 +57,9 @@ void QtTcpServerHandler::handleNewConnection()
 {
     QTcpSocket* socket = m_serverSocket.nextPendingConnection();
     ASSERT(socket);
-    m_webSocketServer->didAcceptConnection(SocketStreamHandle::create(socket, 0));
+    OwnPtr<WebSocketServerConnection> conection = adoptPtr(new WebSocketServerConnection(m_webSocketServer->client(), m_webSocketServer));
+    conection->setSocketHandle(SocketStreamHandle::create(socket, conection.get()));
+    m_webSocketServer->didAcceptConnection(conection.release());
 }
 
 bool QtTcpServerHandler::listen(const String& bindAddress, unsigned short port)
