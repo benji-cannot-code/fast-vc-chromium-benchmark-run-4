6dcf1a30a235f0dec3940d949c1c63a22cca0087
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-22 16:46:28 +0000

    Web Inspector: timeline event details popup misses CPU time https://bugs.webkit.org/show_bug.cgi?id=89765
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel.Record.prototype.generatePopupContent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/English.lproj/localizedStrings.js
+++ third_party/WebKit/Source/WebCore/English.lproj/localizedStrings.js
@@ -703,3 +703,4 @@ localizedStrings["Local modifications..."] = "Local modifications...";
 localizedStrings["apply original content"] = "apply original content";
 localizedStrings["apply revision content"] = "apply revision content";
 localizedStrings["revert"] = "revert";
+localizedStrings["CPU Time"] = "CPU Time";
