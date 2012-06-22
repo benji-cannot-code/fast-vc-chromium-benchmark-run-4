b3e442c63363e6a3e06b073badc7194da7e159a3
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-22 05:23:04 +0000

    Fix edit profile link in NTP
    currently the manage profile overlay is dimissed when new profile data comes in --- instead, just update the overlay.
    
    There's a lot of simplification here which is possible because only one profile (the currently active one) may be managed.
    
    BUG=132343
    TEST=Sign in to Chrome, click username on NTP, edit the current profile.
    
    Review URL: https://chromiumcodereview.appspot.com/10579026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143541 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options2/browser_options.js
+++ chrome/browser/resources/options2/browser_options.js
@@ -249,9 +249,7 @@ cr.define('options', function() {
           chrome.send('createProfile');
         };
         $('profiles-manage').onclick = function(event) {
-          var selectedProfile = self.getSelectedProfileItem_();
-          if (selectedProfile)
-            ManageProfileOverlay.showManageDialog(selectedProfile);
+          ManageProfileOverlay.showManageDialog();
         };
         $('profiles-delete').onclick = function(event) {
           var selectedProfile = self.getSelectedProfileItem_();
@@ -1020,9 +1018,33 @@ cr.define('options', function() {
       // add it to the list, even if the list is hidden so we can access it
       // later.
       $('profiles-list').dataModel = new ArrayDataModel(profiles);
-      // We got new data, close the open overlay if it's open.
-      ManageProfileOverlay.getInstance().visible = false;
+
+      // Received new data. If showing the "manage" overlay, keep it up to
+      // date. If showing the "delete" overlay, close it.
+      if (ManageProfileOverlay.getInstance().visible &&
+          !$('manage-profile-overlay-manage').hidden) {
+        ManageProfileOverlay.showManageDialog();
+      } else {
+        ManageProfileOverlay.getInstance().visible = false;
+      }
+
       this.setProfileViewButtonsStatus_();
+    },
+
+    /**
+     * Returns the currently active profile for this browser window.
+     * @return {Object} A profile info object.
+     * @private
+     */
+    getCurrentProfile_: function() {
+      for (var i = 0; i < $('profiles-list').dataModel.length; i++) {
+        var profile = $('profiles-list').dataModel.item(i);
+        if (profile.isCurrentProfile)
+          return profile;
+      }
+
+      assert(false,
+             'There should always be a current profile, but none found.');
     },
 
     setGtkThemeButtonEnabled_: function(enabled) {
@@ -1371,6 +1393,7 @@ cr.define('options', function() {
   //Forward public APIs to private implementations.
   [
     'addBluetoothDevice',
+    'getCurrentProfile',
     'getStartStopSyncButton',
     'hideBluetoothSettings',
     'removeCloudPrintConnectorSection',
