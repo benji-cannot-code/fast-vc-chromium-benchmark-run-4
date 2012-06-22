100225ca6dffa87242aedb72ccf1818293de5e41
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-22 09:14:01 +0000

    Web Inspector: InspectorState::updateCookie should not do JSON serialization if unsupported https://bugs.webkit.org/show_bug.cgi?id=89743
    Source/WebCore:
    
    Since all InspectorClient's are InspectorStateClient's provide a
    virtual accessor that determines whether or not InspectorClient updates
    are supported or not.
    
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2012-06-22
    Reviewed by Yury Semikhatsky.
    
    * inspector/InspectorState.cpp:
    (WebCore::InspectorState::updateCookie):
    Don't serialize and message the client if the client doesn't do anything with it.
    
    * inspector/InspectorStateClient.h:
    * inspector/WorkerInspectorController.cpp:
    (WebCore::InspectorStateClient::supportsInspectorStateUpdates):
    Let the client say whether or not supports updates or not.
    
    Source/WebKit/blackberry:
    
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2012-06-22
    Reviewed by Yury Semikhatsky.
    
    * WebCoreSupport/InspectorClientBlackBerry.cpp:
    (WebCore::InspectorClientBlackBerry::updateInspectorStateCookie):
    
    Source/WebKit/chromium:
    
    The Chromium port does want InspectorState updates.
    
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2012-06-22
    Reviewed by Yury Semikhatsky.
    
    * src/InspectorClientImpl.h:
    (WebKit::InspectorClientImpl::supportsInspectorStateUpdates):
    * src/WebDevToolsAgentImpl.h:
    (WebKit::WebDevToolsAgentImpl::supportsInspectorStateUpdates):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorState.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorState.cpp
@@ -66,7 +66,7 @@ void InspectorState::unmute()
 
 void InspectorState::updateCookie()
 {
-    if (m_client && !m_isOnMute)
+    if (m_client && !m_isOnMute && m_client->supportsInspectorStateUpdates())
         m_client->updateInspectorStateCookie(m_properties->toJSONString());
 }
 
--- third_party/WebKit/Source/WebCore/inspector/WorkerInspectorController.cpp
+++ third_party/WebKit/Source/WebCore/inspector/WorkerInspectorController.cpp
@@ -78,6 +78,7 @@ public:
     virtual ~WorkerStateClient() { }
 
 private:
+    virtual bool supportsInspectorStateUpdates() const { return true; }
     virtual void updateInspectorStateCookie(const String& cookie)
     {
         m_workerContext->thread()->workerReportingProxy().updateInspectorStateCookie(cookie);
