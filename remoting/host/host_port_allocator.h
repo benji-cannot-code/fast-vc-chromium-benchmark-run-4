f8637bb6775a72a003308653161d3c9fb2c1996c
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-23 08:35:26 +0000

    Remove UrlFetcher from remoting and use the one in net instead.
    BUG=133790
    TEST=Stop and restart the Me2Me host. It should still work.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10637008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143798 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/host_port_allocator.h
+++ remoting/host/host_port_allocator.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <set>
 
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "remoting/host/url_fetcher.h"
 #include "third_party/libjingle/source/talk/p2p/client/httpportallocator.h"
 
 namespace net {
