dd3b01793151e7ef6e910ef1b044da0521263b1c
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-23 08:31:08 +0000

    gdata: Add GDataUploader::UploadUpdatedFile()
    This function will be used in a separate patch.
    Along the way, rename UploadFile() to UploadNewFile() to make it clearer.
    
    BUG=127080
    TEST=Saving to Drive works as before
    
    Review URL: https://chromiumcodereview.appspot.com/10592026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_params.h
+++ chrome/browser/chromeos/gdata/gdata_params.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/platform_file.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/gdata/gdata_parser.h"
+#include "chrome/browser/chromeos/gdata/gdata_upload_file_info.h"
 #include "net/base/io_buffer.h"
 #include "googleurl/src/gurl.h"
 
@@ -41,7 +42,8 @@ struct ResumeUploadResponse {
 
 // Struct for passing params needed for DocumentsService::ResumeUpload() calls.
 struct ResumeUploadParams {
-  ResumeUploadParams(int64 start_range,
+  ResumeUploadParams(UploadMode upload_mode,
+                     int64 start_range,
                      int64 end_range,
                      int64 content_length,
                      const std::string& content_type,
@@ -50,6 +52,7 @@ struct ResumeUploadParams {
                      const FilePath& virtual_path);
   ~ResumeUploadParams();
 
+  UploadMode upload_mode;  // Mode of the upload.
   int64 start_range;  // Start of range of contents currently stored in |buf|.
   int64 end_range;  // End of range of contents currently stored in |buf|.
   int64 content_length;  // File content-Length.
@@ -64,18 +67,28 @@ struct ResumeUploadParams {
 
 // Struct for passing params needed for DocumentsService::InitiateUpload()
 // calls.
+//
+// When uploading a new file (UPLOAD_NEW_FILE):
+// - |title| should be set.
+// - |upload_location| should be the upload_url() of the parent directory.
+//
+// When updating an existing file (UPLOAD_EXISTING_FILE):
+// - |title| should be empty
+// - |upload_location| should be the upload_url() of the existing file.
 struct InitiateUploadParams {
-  InitiateUploadParams(const std::string& title,
+  InitiateUploadParams(UploadMode upload_mode,
+                       const std::string& title,
                        const std::string& content_type,
                        int64 content_length,
-                       const GURL& resumable_create_media_link,
+                       const GURL& upload_location,
                        const FilePath& virtual_path);
   ~InitiateUploadParams();
 
+  UploadMode upload_mode;
   std::string title;
   std::string content_type;
   int64 content_length;
-  GURL resumable_create_media_link;
+  GURL upload_location;
   const FilePath& virtual_path;
 };
 
