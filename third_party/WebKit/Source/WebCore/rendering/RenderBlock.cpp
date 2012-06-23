a5dd9bf63619716e47a2010f5571fab551c386b3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-23 12:21:28 +0000

    Store hit-test rect in HitTestPoint as Rect. https://bugs.webkit.org/show_bug.cgi?id=89454
    Patch by Allan Sandfeld Jensen <allan.jensen@nokia.com> on 2012-06-23
    Reviewed by Darin Adler.
    
    Calculate the rectangle early and store that instead of padding
    to avoid recalculating the rectangle for every single element
    hit tested.
    
    No change in functionality. No new tests.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::bestClickableNodeForTouchPoint):
    (WebCore::EventHandler::bestZoomableAreaForTouchPoint):
    * rendering/HitTestResult.cpp:
    (WebCore::HitTestPoint::HitTestPoint):
    (WebCore::HitTestPoint::operator=):
    (WebCore::HitTestPoint::setPoint):
    (WebCore::hitTestPointIntersects):
    (WebCore::HitTestPoint::intersects):
    (WebCore::HitTestResult::addNodeToRectBasedTestResult):
    * rendering/HitTestResult.h:
    (HitTestPoint):
    (WebCore::HitTestPoint::boundingBox):
    (WebCore::HitTestPoint::topPadding):
    (WebCore::HitTestPoint::rightPadding):
    (WebCore::HitTestPoint::bottomPadding):
    (WebCore::HitTestPoint::leftPadding):
    (HitTestResult):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::hitTestColumns):
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::nodeAtPoint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121096 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -4707,7 +4707,7 @@ bool RenderBlock::hitTestColumns(const HitTestRequest& request, HitTestResult& r
         return false;
 
     for (ColumnRectIterator it(*this); it.hasMore(); it.advance()) {
-        LayoutRect hitRect = result.rectForPoint(pointInContainer.point());
+        LayoutRect hitRect = pointInContainer.boundingBox();
         LayoutRect colRect = it.columnRect();
         colRect.moveBy(accumulatedOffset);
         if (pointInContainer.intersects(colRect)) {
