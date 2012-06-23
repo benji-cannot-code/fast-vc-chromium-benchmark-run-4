3beccc8478d4bd31dff64ef2440d38c0228d1143
dgrogan@chromium.org <dgrogan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-23 01:16:20 +0000

    IndexedDB: Avoid infinite loop if we try to encode -1 for leveldb https://bugs.webkit.org/show_bug.cgi?id=89625
    Source/WebCore:
    
    It gets worse, memory is allocated inside the loop so the process is
    eventually killed by the OS.
    
    Reviewed by Tony Chang.
    
    Added tests to Source/WebKit/chromium/tests/IDBLevelDBCodingTest.cpp
    
    * Modules/indexeddb/IDBLevelDBCoding.cpp:
    (WebCore::IDBLevelDBCoding::encodeInt):
    (WebCore::IDBLevelDBCoding::encodeVarInt):
    
    Source/WebKit/chromium:
    
    Reviewed by Tony Chang.
    
    * tests/IDBLevelDBCodingTest.cpp:
    (IDBLevelDBCoding::TEST):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/IDBLevelDBCodingTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/IDBLevelDBCodingTest.cpp
@@ -140,6 +140,9 @@ TEST(IDBLevelDBCodingTest, EncodeInt)
     EXPECT_EQ(static_cast<size_t>(1), encodeInt(255).size());
     EXPECT_EQ(static_cast<size_t>(2), encodeInt(256).size());
     EXPECT_EQ(static_cast<size_t>(4), encodeInt(0xffffffff).size());
+#ifdef NDEBUG
+    EXPECT_EQ(static_cast<size_t>(8), encodeInt(-1).size());
+#endif
 }
 
 TEST(IDBLevelDBCodingTest, DecodeBool)
@@ -167,6 +170,9 @@ TEST(IDBLevelDBCodingTest, DecodeInt)
     testCases.append(655536);
     testCases.append(7711192431755665792ll);
     testCases.append(0x7fffffffffffffffll);
+#ifdef NDEBUG
+    testCases.append(-3);
+#endif
 
     for (size_t i = 0; i < testCases.size(); ++i) {
         int64_t n = testCases[i];
@@ -182,6 +188,9 @@ TEST(IDBLevelDBCodingTest, EncodeVarInt)
     EXPECT_EQ(static_cast<size_t>(2), encodeVarInt(255).size());
     EXPECT_EQ(static_cast<size_t>(2), encodeVarInt(256).size());
     EXPECT_EQ(static_cast<size_t>(5), encodeVarInt(0xffffffff).size());
+#ifdef NDEBUG
+    EXPECT_EQ(static_cast<size_t>(8), encodeInt(-100).size());
+#endif
 }
 
 TEST(IDBLevelDBCodingTest, DecodeVarInt)
@@ -195,6 +204,9 @@ TEST(IDBLevelDBCodingTest, DecodeVarInt)
     testCases.append(655536);
     testCases.append(7711192431755665792ll);
     testCases.append(0x7fffffffffffffffll);
+#ifdef NDEBUG
+    testCases.append(-3);
+#endif
 
     for (size_t i = 0; i < testCases.size(); ++i) {
         int64_t n = testCases[i];
