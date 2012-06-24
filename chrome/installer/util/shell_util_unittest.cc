d0847253ebceb5d260bd7e0ee344043fa916544c
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-24 13:14:12 +0000

    Revert 143782 - Use a better registration suffix that will always be unique while respecting the MSDN rules. (Triggered assertion failures in browser_tests blocking WebKit rolls.)
    The suffix will now be the base32 encoding of the md5 hash of the user's username. This is always unique and can be determined many times deterministically without I/O access (as opposed to other methods we considered).
    
    It respects all MSDN constraint for properties onto which it is appended.
    
    This replaces the prior-style username suffixes for new installs.
    
    Old-style suffixes (i.e. unsuffixed and username suffixed) will however be kept as is through future updates.
    
    Design doc: https://docs.google.com/a/chromium.org/document/d/1qmcV3uYBh3JwvXhYkI7asg0nN7KfVMWVOzND4p0jQ3E/edit
    
    BUG=133810
    TEST= http://goo.gl/ZZ7gE
    installer_util_unittests.exe --gtest_filter=ShellUtilTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/10617002
    
    TBR=gab@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10667006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143833 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/shell_util_unittest.cc
+++ chrome/installer/util/shell_util_unittest.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/path_service.h"
-#include "base/md5.h"
 #include "base/scoped_temp_dir.h"
 #include "base/string16.h"
 #include "base/win/scoped_comptr.h"
@@ -90,7 +89,7 @@ bool VerifyChromeShortcut(const std::wstring& exe_path,
   return true;
 }
 
-class ShellUtilTestWithDirAndDist : public testing::Test {
+class ShellUtilTest : public testing::Test {
  protected:
   virtual void SetUp() {
     ASSERT_TRUE(temp_dir_.CreateUniqueTempDir());
@@ -105,7 +104,7 @@ class ShellUtilTestWithDirAndDist : public testing::Test {
 };
 
 // Test that we can open archives successfully.
-TEST_F(ShellUtilTestWithDirAndDist, UpdateChromeShortcutTest) {
+TEST_F(ShellUtilTest, UpdateChromeShortcutTest) {
   // Create an executable in test path by copying ourself to it.
   wchar_t exe_full_path_str[MAX_PATH];
   EXPECT_FALSE(::GetModuleFileName(NULL, exe_full_path_str, MAX_PATH) == 0);
@@ -172,7 +171,7 @@ TEST_F(ShellUtilTestWithDirAndDist, UpdateChromeShortcutTest) {
                                    description2, 1));
 }
 
-TEST_F(ShellUtilTestWithDirAndDist, CreateChromeDesktopShortcutTest) {
+TEST_F(ShellUtilTest, CreateChromeDesktopShortcutTest) {
   // Run this test on Vista+ only if we are running elevated.
   if (base::win::GetVersion() > base::win::VERSION_XP && !IsUserAnAdmin()) {
     LOG(ERROR) << "Must be admin to run this test on Vista+";
@@ -365,14 +364,14 @@ TEST_F(ShellUtilTestWithDirAndDist, CreateChromeDesktopShortcutTest) {
       profile_names));
 }
 
-TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdBasic) {
+TEST_F(ShellUtilTest, BuildAppModelIdBasic) {
   std::vector<string16> components;
   const string16 base_app_id(dist_->GetBaseAppId());
   components.push_back(base_app_id);
   ASSERT_EQ(base_app_id, ShellUtil::BuildAppModelId(components));
 }
 
-TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdManySmall) {
+TEST_F(ShellUtilTest, BuildAppModelIdManySmall) {
   std::vector<string16> components;
   const string16 suffixed_app_id(dist_->GetBaseAppId().append(L".gab"));
   components.push_back(suffixed_app_id);
@@ -382,7 +381,7 @@ TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdManySmall) {
             ShellUtil::BuildAppModelId(components));
 }
 
-TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdLongUsernameNormalProfile) {
+TEST_F(ShellUtilTest, BuildAppModelIdLongUsernameNormalProfile) {
   std::vector<string16> components;
   const string16 long_appname(
       L"Chrome.a_user_who_has_a_crazy_long_name_with_some_weird@symbols_in_it_"
@@ -393,7 +392,7 @@ TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdLongUsernameNormalProfile) {
             ShellUtil::BuildAppModelId(components));
 }
 
-TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdLongEverything) {
+TEST_F(ShellUtilTest, BuildAppModelIdLongEverything) {
   std::vector<string16> components;
   const string16 long_appname(
       L"Chrome.a_user_who_has_a_crazy_long_name_with_some_weird@symbols_in_it_"
@@ -405,26 +404,4 @@ TEST_F(ShellUtilTestWithDirAndDist, BuildAppModelIdLongEverything) {
   ASSERT_LE(constructed_app_id.length(), installer::kMaxAppModelIdLength);
   ASSERT_EQ(L"Chrome.a_user_wer_64_characters.A_crazy_profilethat_is_possible",
             constructed_app_id);
-}
-
-TEST(ShellUtilTest, MD5DigestToBase32) {
-  const base::MD5Digest digest = { 'f', 'o', 'o', 'b', 'a', 'f', 'o', 'o', 'b',
-                                   'a', 'f', 'o', 'o', 'b', 'a', 'f' };
-  EXPECT_EQ(L"MZXW6YTBMZXW6YTBMZXW6YTBMY",
-            ShellUtil::MD5DigestToBase32(digest));
-}
-
-TEST(ShellUtilTest, ByteArrayToBase32) {
-  // Tests from http://tools.ietf.org/html/rfc4648#section-10.
-  const unsigned char test_array[] = { 'f', 'o', 'o', 'b', 'a', 'r' };
-
-  const string16 expected[] = { L"", L"MY", L"MZXQ", L"MZXW6", L"MZXW6YQ",
-                                L"MZXW6YTB", L"MZXW6YTBOI"};
-
-  // Run the tests, with one more letter in the input every pass.
-  for (int i = 0; i < arraysize(expected); ++i) {
-    EXPECT_EQ(expected[i],
-              ShellUtil::ByteArrayToBase32(test_array, i));
-  }
-
 }
