4bfb8bc06bb9e6363d5984fa54cb7ab16851615d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-24 07:27:03 +0000

    Web Inspector: speed up Timeline and Elements panel rendering https://bugs.webkit.org/show_bug.cgi?id=89771
    Reviewed by Yury Semikhatsky.
    
    I used timeline panel to find unnecessary style operations.
    
    * inspector/front-end/ElementsTreeOutline.js:
    (WebInspector.ElementsTreeElement.prototype.updateTitle):
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._updateEventDividers):
    (WebInspector.TimelinePanel.prototype._updateFrames):
    (WebInspector.TimelinePanel.prototype.sidebarResized):
    (WebInspector.TimelinePanel.prototype.onResize):
    (WebInspector.TimelinePanel.prototype._onScroll):
    (WebInspector.TimelinePanel.prototype._refresh):
    (WebInspector.TimelinePanel.prototype._refreshRecords):
    (WebInspector.TimelinePanel.prototype._adjustScrollPosition):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ElementsTreeOutline.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ElementsTreeOutline.js
@@ -1471,7 +1471,8 @@ WebInspector.ElementsTreeElement.prototype = {
         }
 
         delete this.selectionElement;
-        this.updateSelection();
+        if (this.selected)
+            this.updateSelection();
         this._preventFollowingLinksOnDoubleClick();
         this._highlightSearchResults();
     },
@@ -1846,6 +1847,9 @@ WebInspector.ElementsTreeUpdater.prototype = {
         }
 
         var updatedParentTreeElements = [];
+        var treeOutlineContainerElement = this._treeOutline.element.parentNode;
+        var originalScrollTop = treeOutlineContainerElement ? treeOutlineContainerElement.scrollTop : 0;
+        this._treeOutline.element.addStyleClass("hidden");
 
         for (var i = 0; i < this._recentlyModifiedNodes.length; ++i) {
             var parent = this._recentlyModifiedNodes[i].parent;
@@ -1853,6 +1857,7 @@ WebInspector.ElementsTreeUpdater.prototype = {
             if (parent === this._treeOutline._rootDOMNode) {
                 // Document's children have changed, perform total update.
                 this._treeOutline.update();
+                this._treeOutline.element.removeStyleClass("hidden");
                 return;
             }
 
@@ -1879,6 +1884,9 @@ WebInspector.ElementsTreeUpdater.prototype = {
         for (var i = 0; i < updatedParentTreeElements.length; ++i)
             delete updatedParentTreeElements[i].alreadyUpdatedChildren;
 
+        this._treeOutline.element.removeStyleClass("hidden");
+        if (originalScrollTop)
+            treeOutlineContainerElement.scrollTop = originalScrollTop;
         this._recentlyModifiedNodes = [];
     },
 
