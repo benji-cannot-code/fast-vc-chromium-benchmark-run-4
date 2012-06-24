3f4b4b2c765e38692a22aacf9f87237541352d9c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-24 23:02:28 +0000

    app_list: Remove unnecessary using declaration from drop_shadow_label.cc.
    And a few unused includes.
    
    TBR=xiyuan@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10657014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143842 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/drop_shadow_label.cc
+++ ui/app_list/drop_shadow_label.cc
@@ -5,14 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/app_list/drop_shadow_label.h"
 
-#include "base/utf_string_conversions.h"
-#include "third_party/skia/include/effects/SkGradientShader.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/color_utils.h"
 #include "ui/gfx/insets.h"
-#include "ui/gfx/skbitmap_operations.h"
-
-using views::Label;
 
 namespace app_list {
 
@@ -55,8 +49,8 @@ void DropShadowLabel::PaintText(gfx::Canvas* canvas,
 
   if (HasFocus() || paint_as_focused()) {
     gfx::Rect focus_bounds = text_bounds;
-    focus_bounds.Inset(-Label::kFocusBorderPadding,
-                       -Label::kFocusBorderPadding);
+    focus_bounds.Inset(-views::Label::kFocusBorderPadding,
+                       -views::Label::kFocusBorderPadding);
     canvas->DrawFocusRect(focus_bounds);
   }
 }
