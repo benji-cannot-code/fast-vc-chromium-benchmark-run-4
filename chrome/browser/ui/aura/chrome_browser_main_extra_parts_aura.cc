5603ca5137dd6b63b57f33089ba82d35b06107e8
tony@chromium.org <tony@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 22:24:41 +0000

    Fix a clang style checker warnings when building with -Duse_aura=1 on Linux.
    ../../chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h:21:1: error: [chromium-style] Complex class/struct needs an explicit out-of-line destructor.
    class ChromeBrowserMainExtraPartsAura : public ChromeBrowserMainExtraParts {
    
    TBR=sky
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10659008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144033 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.cc
+++ chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.cc
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 ChromeBrowserMainExtraPartsAura::ChromeBrowserMainExtraPartsAura() {
 }
 
+ChromeBrowserMainExtraPartsAura::~ChromeBrowserMainExtraPartsAura() {
+}
+
 void ChromeBrowserMainExtraPartsAura::PreProfileInit() {
 #if !defined(USE_ASH)
   gfx::Screen::SetInstance(aura::CreateDesktopScreen());
--- chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h
+++ chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h
@@ -22,6 +22,7 @@ class DesktopStackingClient;
 class ChromeBrowserMainExtraPartsAura : public ChromeBrowserMainExtraParts {
  public:
   ChromeBrowserMainExtraPartsAura();
+  virtual ~ChromeBrowserMainExtraPartsAura();
 
   // Overridden from ChromeBrowserMainExtraParts:
   virtual void PreProfileInit() OVERRIDE;
