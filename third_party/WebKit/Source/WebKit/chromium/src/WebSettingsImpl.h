c332aaf53e09c64a9b76ee15127e91e5227c5584
macpherson@chromium.org <macpherson@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-25 02:03:45 +0000

    Add runtime flag to enable/disable CSS variables (in addition to existing compile-time flag). https://bugs.webkit.org/show_bug.cgi?id=89542
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Added fast/css/variables/build-supports-variables.html to test runtime flag in test environment.
    Updated all other css variables tests to enable the runtime flag from JS.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParserContext::CSSParserContext):
    (WebCore::operator==):
    (WebCore::CSSParser::cssVariablesEnabled):
    (WebCore):
    (WebCore::CSSParser::detectDashToken):
    (WebCore::CSSParser::lex):
    * css/CSSParser.h:
    (CSSParser):
    * css/CSSParserMode.h:
    (CSSParserContext):
    * page/Settings.h:
    (Settings):
    (WebCore::Settings::setCSSVariablesEnabled):
    (WebCore::Settings::cssVariablesEnabled):
    
    Source/WebKit/chromium:
    
    * features.gypi:
    * public/WebSettings.h:
    * src/WebSettingsImpl.cpp:
    (WebKit::WebSettingsImpl::setExperimentalCSSVariablesEnabled):
    (WebKit):
    * src/WebSettingsImpl.h:
    (WebSettingsImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
@@ -98,6 +98,7 @@ public:
     virtual void setExperimentalCSSRegionsEnabled(bool);
     virtual void setExperimentalCSSGridLayoutEnabled(bool);
     virtual void setExperimentalCSSCustomFilterEnabled(bool);
+    virtual void setExperimentalCSSVariablesEnabled(bool);
     virtual void setOpenGLMultisamplingEnabled(bool);
     virtual void setPrivilegedWebGLExtensionsEnabled(bool);
     virtual void setWebGLErrorsToConsoleEnabled(bool);
