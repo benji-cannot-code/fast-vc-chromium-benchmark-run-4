60376a04811a7e58aef3071d41f692c60afa24f6
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 18:08:45 +0000

    Compile fail on ChromiumOS.
    Revert 143946 - Fixes bug in SearchTabHelper not correctly updating state. Also
    removes NTP_LOADING since we don't need it anymore.
    
    BUG=none
    TEST=none
    R=dhollowa@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10657007
    
    TBR=sky@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10666030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/search_types.h
+++ chrome/browser/ui/search/search_types.h
@@ -18,6 +18,9 @@ struct Mode {
     // The default state means anything but the following states.
     MODE_DEFAULT,
 
+    // On the NTP page but the NTP isn't in a state that we should show it.
+    MODE_NTP_LOADING,
+
     // On the NTP page and the NTP is ready to be displayed.
     MODE_NTP,
 
@@ -44,7 +47,7 @@ struct Mode {
   }
 
   bool is_ntp() const {
-    return mode == MODE_NTP;
+    return mode == MODE_NTP || mode == MODE_NTP_LOADING;
   }
 
   bool is_search() const {
