9c5ee27b18b554a34bcb3a64443eb35d20d94fb6
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 18:14:50 +0000

    Make third-party cookie blocking take precedence over any rules that match all hosts.
    Previously, it would only take precedence over rules that match all origins ("*").
    
    BUG=133768
    TEST=see bug for manual test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10574045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143953 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/content_settings_pattern.cc
+++ chrome/common/content_settings_pattern.cc
@@ -479,6 +479,10 @@ bool ContentSettingsPattern::Matches(
   return true;
 }
 
+bool ContentSettingsPattern::MatchesAllHosts() const {
+  return parts_.has_domain_wildcard && parts_.host.empty();
+}
+
 const std::string ContentSettingsPattern::ToString() const {
   if (IsValid())
     return content_settings::PatternParser::ToString(parts_);
