2f316702cfe4adeb580c13bd6514629d2bfe0349
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 19:15:23 +0000

    Move some RegisterXxxPref out of Browser
    Move the RegisterXxxPref calls from Browser::RegisterUserPrefs that
    are used on Android to a relevant common place, either
    ProfileImpl::RegisterUserPrefs or
    BrowserProcessImpl::RegisterUserPrefs.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10580020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143974 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/background/background_mode_manager.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/browser_shutdown.h"
+#include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/debugger/devtools_window.h"
@@ -183,6 +184,7 @@ void RegisterUserPrefs(PrefService* user_prefs) {
   AlternateErrorPageTabObserver::RegisterUserPrefs(user_prefs);
   AutofillManager::RegisterUserPrefs(user_prefs);
   bookmark_utils::RegisterUserPrefs(user_prefs);
+  chrome::ChromeContentBrowserClient::RegisterUserPrefs(user_prefs);
   ChromeVersionService::RegisterUserPrefs(user_prefs);
   chrome_browser_net::HttpServerPropertiesManager::RegisterPrefs(user_prefs);
   chrome_browser_net::Predictor::RegisterUserPrefs(user_prefs);
