c8956222656bc75db86a73c7edbd84925fcc39be
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-25 00:41:05 +0000

    Unreviewed, rolling out r121106. http://trac.webkit.org/changeset/121106 https://bugs.webkit.org/show_bug.cgi?id=89841
    Caused 85% performance regressions on Dromaeo/jslib-modify-
    jquery.html (Requested by rniwa on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-06-24
    
    * dom/Document.cpp:
    (WebCore::Document::clearNodeListCaches):
    * dom/Document.h:
    (Document):
    * dom/DynamicNodeList.cpp:
    (WebCore::DynamicSubtreeNodeList::~DynamicSubtreeNodeList):
    * dom/DynamicNodeList.h:
    (WebCore::DynamicSubtreeNodeList::DynamicSubtreeNodeList):
    * dom/Node.cpp:
    (WebCore::Node::clearRareData):
    (WebCore::Node::invalidateNodeListsCacheAfterAttributeChanged):
    (WebCore::Node::invalidateNodeListsCacheAfterChildrenChanged):
    (WebCore::Node::getElementsByTagName):
    (WebCore::Node::getElementsByTagNameNS):
    (WebCore::Node::getElementsByName):
    (WebCore::Node::getElementsByClassName):
    (WebCore::Node::radioNodeList):
    (WebCore::NodeListsNodeData::invalidateCaches):
    (WebCore):
    (WebCore::NodeRareData::createNodeLists):
    * dom/NodeRareData.h:
    (NodeListsNodeData):
    (WebCore::NodeListsNodeData::adoptTreeScope):
    (WebCore::NodeRareData::ensureNodeLists):
    (NodeRareData):
    * dom/TreeScope.cpp:
    (WebCore::TreeScope::TreeScope):
    * dom/TreeScope.h:
    (WebCore::TreeScope::addNodeListCache):
    (WebCore::TreeScope::removeNodeListCache):
    (WebCore::TreeScope::hasNodeListCaches):
    (TreeScope):
    * dom/TreeScopeAdopter.cpp:
    (WebCore::TreeScopeAdopter::moveTreeToNewScope):
    * html/LabelableElement.cpp:
    (WebCore::LabelableElement::labels):
    * html/LabelsNodeList.cpp:
    (WebCore::LabelsNodeList::LabelsNodeList):
    (WebCore::LabelsNodeList::~LabelsNodeList):
    * html/RadioNodeList.cpp:
    (WebCore::RadioNodeList::RadioNodeList):
    (WebCore::RadioNodeList::~RadioNodeList):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121126 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DynamicNodeList.cpp
+++ third_party/WebKit/Source/WebCore/dom/DynamicNodeList.cpp
@@ -31,7 +31,6 @@ namespace WebCore {
 
 DynamicSubtreeNodeList::~DynamicSubtreeNodeList()
 {
-    document()->unregisterDynamicSubtreeNodeList(this);
 }
 
 unsigned DynamicSubtreeNodeList::length() const
