0bf401eb63e9d9fa16e492f212931db4df11b5ce
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 21:23:18 +0000

    Add tests for chrome.fileSystem app API.
    BUG=132945
    TEST=No testing required; automated tests added.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10663014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144017 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/file_system/save_existing/background.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function onLaunched() {
+  chrome.appWindow.create('test.html');
+}
+
+chrome.experimental.app.onLaunched.addListener(onLaunched);
