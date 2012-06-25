b5b1ead78be486019ebd165c4b6fd7106ed2e9f4
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-25 22:19:03 +0000

    Unreviewed, rolling out r121129. http://trac.webkit.org/changeset/121129 https://bugs.webkit.org/show_bug.cgi?id=89542
    Chromium ASan failure: crbug.com/134402
    
    Source/WebCore:
    
    * css/CSSParser.cpp:
    (WebCore::CSSParserContext::CSSParserContext):
    (WebCore::operator==):
    (WebCore):
    (WebCore::CSSParser::detectDashToken):
    (WebCore::CSSParser::lex):
    * css/CSSParser.h:
    (CSSParser):
    * css/CSSParserMode.h:
    (CSSParserContext):
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::collectMatchingRulesForList):
    * page/Settings.h:
    (Settings):
    * testing/InternalSettings.cpp:
    * testing/InternalSettings.h:
    (InternalSettings):
    * testing/InternalSettings.idl:
    
    Source/WebKit/chromium:
    
    * features.gypi:
    * public/WebSettings.h:
    * src/WebSettingsImpl.cpp:
    * src/WebSettingsImpl.h:
    (WebSettingsImpl):
    
    LayoutTests:
    
    * fast/css/variables/border-width.html:
    * fast/css/variables/build-supports-variables-expected.txt: Removed.
    * fast/css/variables/build-supports-variables.html: Removed.
    * fast/css/variables/colors-test.html:
    * fast/css/variables/complex-cycle.html:
    * fast/css/variables/computed-style.html:
    * fast/css/variables/inherited-values.html:
    * fast/css/variables/inline-styles.html:
    * fast/css/variables/invalid-shorthand.html:
    * fast/css/variables/invalid-variable-value.html:
    * fast/css/variables/multi-level-cycle.html:
    * fast/css/variables/redefinition.html:
    * fast/css/variables/shorthand.html:
    * fast/css/variables/simple-cycle.html:
    * fast/css/variables/transform-test.html:
    * fast/css/variables/undefined.html:
    * fast/css/variables/use-before-defined.html:
    * fast/css/variables/var-inside-shorthand.html:
    * fast/css/variables/variable-chain.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121187 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/InternalSettings.h
+++ third_party/WebKit/Source/WebCore/testing/InternalSettings.h
@@ -76,8 +76,6 @@ public:
     void setEnableScrollAnimator(bool enabled, ExceptionCode&);
     bool scrollAnimatorEnabled(ExceptionCode&);
     void setCSSExclusionsEnabled(bool enabled, ExceptionCode&);
-    void setCSSVariablesEnabled(bool enabled, ExceptionCode&);
-    bool cssVariablesEnabled(ExceptionCode&);
     void setMediaPlaybackRequiresUserGesture(bool, ExceptionCode&);
     void setEditingBehavior(const String&, ExceptionCode&);
     void setFixedPositionCreatesStackingContext(bool, ExceptionCode&);
