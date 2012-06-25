61930768215cc01b61fef5b652014701162a79ac
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-25 17:14:41 +0000

    [EFL] Support screenDepth() functionality. https://bugs.webkit.org/show_bug.cgi?id=89423
    Reviewed by Kenneth Rohde Christiansen.
    
    Support screenDepth() based on ecore_x_window_depth_get(). This functionality may be used by
    both WebKit1 and WebKit2. So, concrete function is placed into EflScreenUtilities.cpp.
    
    This patch can't support existing test cases. Because, EFL DumpRenderTree works without X11 window
    by default.
    
    * platform/efl/EflScreenUtilities.cpp:
    (WebCore::getPixelDepth):
    (WebCore):
    * platform/efl/EflScreenUtilities.h:
    (WebCore):
    * platform/efl/PlatformScreenEfl.cpp:
    (WebCore::screenDepth):
    (WebCore::screenDepthPerComponent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/PlatformScreenEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/PlatformScreenEfl.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PlatformScreen.h"
 
+#include "EflScreenUtilities.h"
 #include "NotImplemented.h"
 #include "PlatformString.h"
 #include "Widget.h"
@@ -58,14 +59,19 @@ int screenVerticalDPI(Widget* widget)
 
 int screenDepth(Widget* widget)
 {
-    notImplemented();
-    return 8;
+    if (!widget->evas())
+        return 0;
+
+    return getPixelDepth(widget->evas());
 }
 
-int screenDepthPerComponent(Widget*)
+int screenDepthPerComponent(Widget* widget)
 {
-    notImplemented();
-    return 8;
+    if (!widget->evas())
+        return 0;
+
+    // FIXME: How to support this functionality based on EFL library ?
+    return getPixelDepth(widget->evas());
 }
 
 bool screenIsMonochrome(Widget*)
