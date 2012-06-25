ac2abaec121311a32872a0775037c56b8e3122e8
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 17:45:26 +0000

    Exit mouse lock permision prompt on tab switch or close.
    BUG=131702
    
    Review URL: https://chromiumcodereview.appspot.com/10642008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143941 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/fullscreen_controller.cc
+++ chrome/browser/ui/fullscreen_controller.cc
@@ -247,7 +247,9 @@ void FullscreenController::LostMouseLock() {
 }
 
 void FullscreenController::OnTabClosing(WebContents* web_contents) {
-  if (IsFullscreenForTabOrPending(web_contents)) {
+  const TabContents* contents = TabContents::FromWebContents(web_contents);
+  if (contents &&
+      (contents == fullscreened_tab_ || contents == mouse_lock_tab_)) {
     ExitTabFullscreenOrMouseLockIfNecessary();
     // The call to exit fullscreen may result in asynchronous notification of
     // fullscreen state change (e.g., on Linux). We don't want to rely on it
@@ -259,7 +261,8 @@ void FullscreenController::OnTabClosing(WebContents* web_contents) {
 }
 
 void FullscreenController::OnTabDeactivated(TabContents* contents) {
-  if (contents == fullscreened_tab_)
+  if (contents &&
+      (contents == fullscreened_tab_ || contents == mouse_lock_tab_))
     ExitTabFullscreenOrMouseLockIfNecessary();
 }
 
