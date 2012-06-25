aeec58f548bea87d481f9b8948019b84499c34de
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-25 23:07:01 +0000

    Valgrind: Fix memory leaks in RenderViewHostTest.ExecuteJavascriptAndGetValue
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10664021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144045 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_view_host_browsertest.cc
+++ content/browser/renderer_host/render_view_host_browsertest.cc
@@ -37,8 +37,8 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
       browser()->GetActiveWebContents()->GetRenderViewHost());
 
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-                                                     ASCIIToUTF16("!false;"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(), ASCIIToUTF16("!false;")));
     EXPECT_EQ(Value::TYPE_BOOLEAN, value->GetType());
     bool bool_value;
     EXPECT_TRUE(value->GetAsBoolean(&bool_value));
@@ -47,8 +47,8 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // Execute the script 'true' and make sure we get back true.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-                                                     ASCIIToUTF16("true;"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(), ASCIIToUTF16("true;")));
     EXPECT_EQ(Value::TYPE_BOOLEAN, value->GetType());
     bool bool_value;
     EXPECT_TRUE(value->GetAsBoolean(&bool_value));
@@ -57,8 +57,8 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // Execute the script 'false' and make sure we get back false.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-                                                     ASCIIToUTF16("false;"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(), ASCIIToUTF16("false;")));
     EXPECT_EQ(Value::TYPE_BOOLEAN, value->GetType());
     bool bool_value;
     EXPECT_TRUE(value->GetAsBoolean(&bool_value));
@@ -67,8 +67,8 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // And now, for something completely different, try a number.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-                                                     ASCIIToUTF16("42;"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(), ASCIIToUTF16("42;")));
     EXPECT_EQ(Value::TYPE_INTEGER, value->GetType());
     int int_value;
     EXPECT_TRUE(value->GetAsInteger(&int_value));
@@ -77,8 +77,8 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // Try a floating point number.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-                                                     ASCIIToUTF16("42.2;"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(), ASCIIToUTF16("42.2;")));
     EXPECT_EQ(Value::TYPE_DOUBLE, value->GetType());
     double double_value;
     EXPECT_TRUE(value->GetAsDouble(&double_value));
@@ -87,8 +87,9 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // Let's check out string.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-        ASCIIToUTF16("\"something completely different\";"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(),
+            ASCIIToUTF16("\"something completely different\";")));
     EXPECT_EQ(Value::TYPE_STRING, value->GetType());
     std::string string_value;
     EXPECT_TRUE(value->GetAsString(&string_value));
@@ -97,8 +98,9 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // Regular expressions might be fun.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-        ASCIIToUTF16("/finder.*foo/g;"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(),
+            ASCIIToUTF16("/finder.*foo/g;")));
     EXPECT_EQ(Value::TYPE_STRING, value->GetType());
     std::string string_value;
     EXPECT_TRUE(value->GetAsString(&string_value));
@@ -108,8 +110,9 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
   // Let's test some date conversions.  First up, epoch.  Can't use 0 because
   // that means uninitialized, so use the next best thing.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-        ASCIIToUTF16("new Date(1);"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(),
+            ASCIIToUTF16("new Date(1);")));
     EXPECT_EQ(Value::TYPE_DOUBLE, value->GetType());
     double date_seconds;
     EXPECT_TRUE(value->GetAsDouble(&date_seconds));
@@ -128,8 +131,9 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // Test date with a real date input.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-        ASCIIToUTF16("new Date(Date.UTC(2006, 7, 16, 12, 0, 15));"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(),
+            ASCIIToUTF16("new Date(Date.UTC(2006, 7, 16, 12, 0, 15));")));
     EXPECT_EQ(Value::TYPE_DOUBLE, value->GetType());
     double date_seconds;
     EXPECT_TRUE(value->GetAsDouble(&date_seconds));
@@ -149,8 +153,9 @@ IN_PROC_BROWSER_TEST_F(RenderViewHostTest,
 
   // And something more complicated - get an array back as a list.
   {
-    Value* value = rvh->ExecuteJavascriptAndGetValue(string16(),
-        ASCIIToUTF16("new Array(\"one\", 2, false);"));
+    scoped_ptr<Value> value(
+        rvh->ExecuteJavascriptAndGetValue(string16(),
+            ASCIIToUTF16("new Array(\"one\", 2, false);")));
     EXPECT_EQ(Value::TYPE_LIST, value->GetType());
     ListValue* list_value;
     EXPECT_TRUE(value->GetAsList(&list_value));
