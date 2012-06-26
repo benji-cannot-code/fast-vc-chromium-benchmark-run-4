benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit b8885794066eb876a3b6f8f9197c116c2d90f263

SPDY: Set initial max concurrent streams to 100
Currently we default to 10 concurrent client initiated streams, due to lame SPDY servers. Firefox 13 has gone default with with a much higher default, so we're going to raise ours too since SPDY servers seem to be able to handle them (the lame server in question just sends a SETTINGS frame after the first roundtrip that lowers the stream limit to 10).

BUG=132799
TEST=none


Review URL: https://chromiumcodereview.appspot.com/10535171

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@142228 0039d316-1c4b-4281-b951-d872f2087c98

net/spdy/spdy_session.cc
@@ -172,7 +172,7 @@ Value* NetLogSpdyGoAwayCallback(SpdyStreamId last_stream_id,
 }
 
 NextProto g_default_protocol = kProtoUnknown;
-size_t g_init_max_concurrent_streams = 10;
+size_t g_init_max_concurrent_streams = 100;
 size_t g_max_concurrent_stream_limit = 256;
 size_t g_default_initial_rcv_window_size = 10 * 1024 * 1024;  // 10MB
 bool g_enable_ping_based_connection_checking = true;

commit 21d15b90b378db74f26deb9a7e7bff6aeebbd25e

Rename SpdySession::kInitialMaxConcurrentStreams to    SpdySession::kMaxPushedConcurrentStreams
Add comment to SpdySession global variables:
   g_init_max_concurrent_streams = 100;
   g_max_concurrent_stream_limit = 256;

Fix bug in SpdySession::ResetStaticSettingsToInit introduced in r142228.


Review URL: https://chromiumcodereview.appspot.com/10676003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144307 0039d316-1c4b-4281-b951-d872f2087c98

net/spdy/spdy_session.cc
@@ -173,7 +173,11 @@ Value* NetLogSpdyGoAwayCallback(SpdyStreamId last_stream_id,
 }
 
 NextProto g_default_protocol = kProtoUnknown;
+// Maximum number of concurrent streams we will create, unless the server
+// sends a SETTINGS frame with a different value.
 size_t g_init_max_concurrent_streams = 100;
+// The maximum number of concurrent streams we will ever create.  Even if
+// the server permits more, we will never exceed this limit.
 size_t g_max_concurrent_stream_limit = 256;
 size_t g_default_initial_rcv_window_size = 10 * 1024 * 1024;  // 10MB
 bool g_enable_ping_based_connection_checking = true;
@@ -210,7 +214,7 @@ void SpdySession::set_default_initial_recv_window_size(size_t value) {
 void SpdySession::ResetStaticSettingsToInit() {
   // WARNING: These must match the initializers above.
   g_default_protocol = kProtoUnknown;
-  g_init_max_concurrent_streams = 10;
+  g_init_max_concurrent_streams = 100;
   g_max_concurrent_stream_limit = 256;
   g_default_initial_rcv_window_size = kSpdyStreamInitialWindowSize;
   g_enable_ping_based_connection_checking = true;
@@ -1590,7 +1594,7 @@ void SpdySession::SendInitialSettings() {
     // Create a new settings frame notifying the sever of our
     // max_concurrent_streams_ and initial window size.
     settings_map[SETTINGS_MAX_CONCURRENT_STREAMS] =
-        SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kInitialMaxConcurrentStreams);
+        SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kMaxConcurrentPushedStreams);
     if (GetProtocolVersion() > 2 &&
         initial_recv_window_size_ != kSpdyStreamInitialWindowSize) {
       settings_map[SETTINGS_INITIAL_WINDOW_SIZE] =

net/spdy/spdy_session.h
@@ -41,8 +41,8 @@ namespace net {
 const int kMss = 1430;
 const int kMaxSpdyFrameChunkSize = (2 * kMss) - SpdyFrame::kHeaderSize;
 
-// Specifies the maxiumum concurrent streams server could send.
-const int kInitialMaxConcurrentStreams = 1000;
+// Specifies the maxiumum concurrent streams server could send (via push).
+const int kMaxConcurrentPushedStreams = 1000;
 
 class BoundNetLog;
 class SpdyStream;

net/spdy/spdy_session_spdy2_unittest.cc
@@ -680,7 +680,7 @@ TEST_F(SpdySessionSpdy2Test, SendInitialSettingsOnNewSession) {
   SettingsMap settings;
   const SpdySettingsIds kSpdySettingsIds1 = SETTINGS_MAX_CONCURRENT_STREAMS;
   settings[kSpdySettingsIds1] =
-      SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kInitialMaxConcurrentStreams);
+      SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kMaxConcurrentPushedStreams);
   MockConnect connect_data(SYNCHRONOUS, OK);
   scoped_ptr<SpdyFrame> settings_frame(ConstructSpdySettings(settings));
   MockWrite writes[] = {

net/spdy/spdy_session_spdy3_unittest.cc
@@ -682,7 +682,7 @@ TEST_F(SpdySessionSpdy3Test, SendInitialSettingsOnNewSession) {
   const SpdySettingsIds kSpdySettingsIds2 = SETTINGS_INITIAL_WINDOW_SIZE;
   const uint32 kInitialRecvWindowSize = 10 * 1024 * 1024;
   settings[kSpdySettingsIds1] =
-      SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kInitialMaxConcurrentStreams);
+      SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kMaxConcurrentPushedStreams);
   settings[kSpdySettingsIds2] =
       SettingsFlagsAndValue(SETTINGS_FLAG_NONE, kInitialRecvWindowSize);
   MockConnect connect_data(SYNCHRONOUS, OK);