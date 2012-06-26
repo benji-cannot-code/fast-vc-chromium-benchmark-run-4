d719134b547aac4545eeb4e5c4b67eab5c0c0a03
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 21:29:59 +0000

    net-internals:  Switch to a visible view when the active one is hidden.
    R=eroman@chromium.org
    BUG=115571
    
    Review URL: https://chromiumcodereview.appspot.com/10665025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144282 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/net_internals/net_internals_test.js
+++ chrome/test/data/webui/net_internals/net_internals_test.js
@@ -217,6 +217,14 @@ var NetInternalsTest = (function() {
   };
 
   /**
+   * Returns the id of the currently active tab.
+   * @return {string} ID of the active tab.
+   */
+  NetInternalsTest.getActiveTabId = function() {
+    return MainView.getInstance().categoryTabSwitcher().findActiveTab().id;
+  };
+
+  /**
    * Returns the tab id of a tab, given its associated URL hash value.  Asserts
    *     if |hash| has no associated tab.
    * @param {string}: hash Hash associated with the tab to return the id of.
@@ -304,6 +312,10 @@ var NetInternalsTest = (function() {
    */
   NetInternalsTest.checkTabHandleVisibility = function(tabVisibilityState,
                                                        tourTabs) {
+    // The currently active tab should have a handle that is visible.
+    expectTrue(NetInternalsTest.tabHandleIsVisible(
+                   NetInternalsTest.getActiveTabId()));
+
     // Check visibility state of all tabs.
     var tabCount = 0;
     for (var hash in tabVisibilityState) {
