eaf40861e165188d7b7e8d2ba2174d96eefb4082
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-26 09:22:45 +0000

    New fast/js/dfg-store-unexpected-value-into-argument-and-osr-exit.html fails on 32 bit https://bugs.webkit.org/show_bug.cgi?id=89953
    Reviewed by Zoltan Herczeg.
    
    DFG 32-bit JIT was confused about the difference between a predicted type and a
    proven type. This is easy to get confused about, since a local that is predicted int32
    almost always means that the local must be an int32 since speculations are hoisted to
    stores to locals. But that is less likely to be the case for arguments, where there is
    an additional least-upper-bounding step: any store to an argument with a weird type
    may force the argument to be any type.
    
    This patch basically duplicates the functionality in DFGSpeculativeJIT64.cpp for
    GetLocal: the decision of whether to load a local as an int32 (or as an array, or as
    a boolean) is made based on the AbstractValue::m_type, which is a type proof, rather
    than the VariableAccessData::prediction(), which is a predicted type.
    
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGSpeculativeJIT32_64.cpp
@@ -1892,7 +1892,7 @@ void SpeculativeJIT::compile(Node& node)
                 break;
             }
         
-            if (isInt32Speculation(prediction)) {
+            if (isInt32Speculation(value.m_type)) {
                 GPRTemporary result(this);
                 m_jit.load32(JITCompiler::payloadFor(node.local()), result.gpr());
 
@@ -1904,7 +1904,7 @@ void SpeculativeJIT::compile(Node& node)
                 break;
             }
 
-            if (isArraySpeculation(prediction)) {
+            if (isArraySpeculation(value.m_type)) {
                 GPRTemporary result(this);
                 m_jit.load32(JITCompiler::payloadFor(node.local()), result.gpr());
 
@@ -1916,7 +1916,7 @@ void SpeculativeJIT::compile(Node& node)
                 break;
             }
 
-            if (isBooleanSpeculation(prediction)) {
+            if (isBooleanSpeculation(value.m_type)) {
                 GPRTemporary result(this);
                 m_jit.load32(JITCompiler::payloadFor(node.local()), result.gpr());
 
