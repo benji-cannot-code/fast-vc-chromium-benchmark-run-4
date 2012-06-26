d89ad32fe9ad151eef1f86e544d548e99238e57a
jpu@apple.com <jpu@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-26 18:38:56 +0000

    On Mac, autocorrection sometimes fails to take place in Safari. https://bugs.webkit.org/show_bug.cgi?id=89982
    Reviewed by Darin Adler.
    
    Existing test was turned off due to intermittent failure, which is caused by autocorrection result depending on user data
    that may be altered by previous test runs. Hopefully we can turn the test back on once we have a way to make autocorrection
    behave consistently.
    
    Basically we should check the value of shouldCheckForCorrection, not shouldShowCorrectionPanel, to determine if we should
    early return in markAndReplaceFor().
    
    * editing/Editor.cpp:
    (WebCore::Editor::markAndReplaceFor):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.cpp
+++ third_party/WebKit/Source/WebCore/editing/Editor.cpp
@@ -2086,7 +2086,7 @@ void Editor::markAndReplaceFor(PassRefPtr<SpellCheckRequest> request, const Vect
             if (result->type == TextCheckingTypeLink && selectionOffset > resultLocation + resultLength + 1)
                 continue;
 
-            if (!(shouldPerformReplacement || shouldShowCorrectionPanel || shouldMarkLink) || !doReplacement)
+            if (!(shouldPerformReplacement || shouldCheckForCorrection || shouldMarkLink) || !doReplacement)
                 continue;
 
             String replacedString = plainText(rangeToReplace.get());
