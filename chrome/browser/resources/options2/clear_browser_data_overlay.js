benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit cf218548bd70ce6e8e1ef8be26ee26fb2c6debbf

Fix id attributes of clear_browser_overlay
BUG=116372
TEST=Check that chrome://settings/clearBrowserData continues to work as intended.


Review URL: https://chromiumcodereview.appspot.com/10628018

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@143885 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/options2/clear_browser_data_overlay.js
@@ -12,9 +12,8 @@ cr.define('options', function() {
    * @class
    */
   function ClearBrowserDataOverlay() {
-    OptionsPage.call(this, 'clearBrowserData',
-                     loadTimeData.getString('clearBrowserDataOverlayTabTitle'),
-                     'clearBrowserDataOverlay');
+    OptionsPage.call(this, 'clearBrowserData', '',
+                     'clear-browser-data-overlay');
   }
 
   cr.addSingletonGetter(ClearBrowserDataOverlay);
@@ -44,16 +43,16 @@ cr.define('options', function() {
       });
 
       var checkboxes = document.querySelectorAll(
-          '#cbdContentArea input[type=checkbox]');
+          '#cbd-content-area input[type=checkbox]');
       for (var i = 0; i < checkboxes.length; i++) {
         checkboxes[i].onclick = f;
       }
       this.updateCommitButtonState_();
 
-      $('clearBrowserDataDismiss').onclick = function(event) {
+      $('clear-browser-data-dismiss').onclick = function(event) {
         ClearBrowserDataOverlay.dismiss();
       };
-      $('clearBrowserDataCommit').onclick = function(event) {
+      $('clear-browser-data-commit').onclick = function(event) {
         ClearBrowserDataOverlay.setClearingState(true);
         chrome.send('performClearBrowserData');
       };
@@ -62,7 +61,7 @@ cr.define('options', function() {
     // Set the enabled state of the commit button.
     updateCommitButtonState_: function() {
       var checkboxes = document.querySelectorAll(
-          '#cbdContentArea input[type=checkbox]');
+          '#cbd-content-area input[type=checkbox]');
       var isChecked = false;
       for (var i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
@@ -70,7 +69,7 @@ cr.define('options', function() {
           break;
         }
       }
-      $('clearBrowserDataCommit').disabled = !isChecked;
+      $('clear-browser-data-commit').disabled = !isChecked;
     },
   };
 
@@ -78,20 +77,20 @@ cr.define('options', function() {
   // Chrome callbacks
   //
   ClearBrowserDataOverlay.setClearingState = function(state) {
-    $('deleteBrowsingHistoryCheckbox').disabled = state;
-    $('deleteDownloadHistoryCheckbox').disabled = state;
-    $('deleteCacheCheckbox').disabled = state;
-    $('deleteCookiesCheckbox').disabled = state;
-    $('deletePasswordsCheckbox').disabled = state;
-    $('deleteFormDataCheckbox').disabled = state;
-    $('deleteHostedAppsDataCheckbox').disabled = state;
-    $('deauthorizeContentLicensesCheckbox').disabled = state;
-    $('clearBrowserDataTimePeriod').disabled = state;
-    $('cbdThrobber').style.visibility = state ? 'visible' : 'hidden';
-    $('clearBrowserDataDismiss').disabled = state;
+    $('delete-browsing-history-checkbox').disabled = state;
+    $('delete-download-history-checkbox').disabled = state;
+    $('delete-cache-checkbox').disabled = state;
+    $('delete-cookies-checkbox').disabled = state;
+    $('delete-passwords-checkbox').disabled = state;
+    $('delete-form-data-checkbox').disabled = state;
+    $('delete-hosted-apps-data-checkbox').disabled = state;
+    $('deauthorize-content-licenses-checkbox').disabled = state;
+    $('clear-browser-data-time-period').disabled = state;
+    $('cbd-throbber').style.visibility = state ? 'visible' : 'hidden';
+    $('clear-browser-data-dismiss').disabled = state;
 
     if (state)
-      $('clearBrowserDataCommit').disabled = true;
+      $('clear-browser-data-commit').disabled = true;
     else
       ClearBrowserDataOverlay.getInstance().updateCommitButtonState_();
   };

chrome/browser/resources/extensions/pack_extension_overlay.html
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <div id="packExtensionOverlay" class="page">
   <h1 i18n-content="packExtensionOverlay"></h1>
-  <div id="cbdContentArea" class="content-area">
+  <div id="cbd-content-area" class="content-area">
     <div class="pack-extension-heading" i18n-content="packExtensionHeading">
     </div>
     <div class="pack-extension-text-boxes">

chrome/browser/resources/options2/clear_browser_data_overlay.css
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Use of this source code is governed by a BSD-style license that can be
  * found in the LICENSE file. */
 
-#clearBrowserDataOverlay {
+#clear-browser-data-overlay {
   min-width: 500px;
 }
 
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   margin: 5px 0;
 }
 
-#cbdThrobber {
+#cbd-throbber {
   margin: 4px 10px;
   vertical-align: middle;
   visibility: hidden;

chrome/browser/resources/options2/clear_browser_data_overlay.html
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-<div id="clearBrowserDataOverlay" class="page" hidden>
+<div id="clear-browser-data-overlay" class="page" hidden>
   <div class="close-button"></div>
   <h1 i18n-content="clearBrowserDataOverlay"></h1>
-  <div id="cbdContentArea" class="content-area">
+  <div id="cbd-content-area" class="content-area">
     <span i18n-content="clearBrowserDataLabel"></span>
-    <select id="clearBrowserDataTimePeriod"
+    <select id="clear-browser-data-time-period"
         i18n-options="clearBrowserDataTimeList"
         pref="browser.clear_data.time_period"
         data-type="number">
@@ -12,28 +12,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     <div id="clear-data-checkboxes">
       <div class="checkbox">
         <label>
-          <input id="deleteBrowsingHistoryCheckbox"
+          <input id="delete-browsing-history-checkbox"
               pref="browser.clear_data.browsing_history" type="checkbox">
           <span i18n-content="deleteBrowsingHistoryCheckbox"></span>
         </label>
       </div>
       <div class="checkbox">
         <label>
-          <input id="deleteDownloadHistoryCheckbox"
+          <input id="delete-download-history-checkbox"
               pref="browser.clear_data.download_history" type="checkbox">
           <span i18n-content="deleteDownloadHistoryCheckbox"></span>
         </label>
       </div>
       <div class="checkbox">
         <label>
-          <input id="deleteCacheCheckbox"
+          <input id="delete-cache-checkbox"
               pref="browser.clear_data.cache" type="checkbox">
           <span i18n-content="deleteCacheCheckbox"></span>
         </label>
       </div>
       <div class="checkbox">
         <label>
-          <input id="deleteCookiesCheckbox"
+          <input id="delete-cookies-checkbox"
               pref="browser.clear_data.cookies" type="checkbox">
           <span i18n-content="deleteCookiesFlashCheckbox"
               class="clear-plugin-lso-data-enabled"></span>
@@ -43,28 +43,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       </div>
       <div class="checkbox">
         <label>
-          <input id="deletePasswordsCheckbox"
+          <input id="delete-passwords-checkbox"
               pref="browser.clear_data.passwords" type="checkbox">
           <span i18n-content="deletePasswordsCheckbox"></span>
         </label>
       </div>
       <div class="checkbox">
         <label>
-          <input id="deleteFormDataCheckbox"
+          <input id="delete-form-data-checkbox"
               pref="browser.clear_data.form_data" type="checkbox">
           <span i18n-content="deleteFormDataCheckbox"></span>
         </label>
       </div>
       <div class="checkbox">
         <label>
-          <input id="deleteHostedAppsDataCheckbox"
+          <input id="delete-hosted-apps-data-checkbox"
               pref="browser.clear_data.hosted_apps_data" type="checkbox">
           <span i18n-content="deleteHostedAppsDataCheckbox"></span>
         </label>
       </div>
       <div class="checkbox pepper-flash-settings">
         <label>
-          <input id="deauthorizeContentLicensesCheckbox"
+          <input id="deauthorize-content-licenses-checkbox"
               pref="browser.clear_data.content_licenses" type="checkbox">
           <span i18n-content="deauthorizeContentLicensesCheckbox"></span>
         </label>
@@ -81,10 +81,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           i18n-values="href:clearBrowsingDataLearnMoreUrl"></a>
     </div>
     <div class="action-area-right">
-      <div id="cbdThrobber" class="throbber"></div>
+      <div id="cbd-throbber" class="throbber"></div>
       <div class="button-strip">
-        <button id="clearBrowserDataDismiss" i18n-content="cancel"></button>
-        <button id="clearBrowserDataCommit"
+        <button id="clear-browser-data-dismiss" i18n-content="cancel"></button>
+        <button id="clear-browser-data-commit"
             i18n-content="clearBrowserDataCommit">
         </button>
       </div>

commit 0051bb2b763e34a4d478ea70a49472c4fedd0b78

Fix tab label for chrome://settings/clearBrowserData
This got broken in r143885.

BUG=134473
TEST=no


Review URL: https://chromiumcodereview.appspot.com/10656023

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144197 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/resources/options2/clear_browser_data_overlay.js
@@ -12,7 +12,8 @@ cr.define('options', function() {
    * @class
    */
   function ClearBrowserDataOverlay() {
-    OptionsPage.call(this, 'clearBrowserData', '',
+    OptionsPage.call(this, 'clearBrowserData',
+                     loadTimeData.getString('clearBrowserDataOverlayTabTitle'),
                      'clear-browser-data-overlay');
   }
 

chrome/test/functional/special_tabs.py
@@ -61,9 +61,8 @@ class SpecialTabsTest(pyauto.PyUITest):
     'chrome://plugins': { 'title': 'Plug-ins' },
     'chrome://settings': { 'title': 'Settings' },
     'chrome://settings/autofill': { 'title': 'Settings - Autofill settings' },
-    # Broken by r143885. crbug.com/134473
-    # 'chrome://settings/clearBrowserData':
-    #  { 'title': 'Settings - Clear browsing data' },
+    'chrome://settings/clearBrowserData':
+      { 'title': 'Settings - Clear browsing data' },
     'chrome://settings/content': { 'title': 'Settings - Content settings' },
     'chrome://settings/languages':
       { 'title': 'Settings - Languages' },