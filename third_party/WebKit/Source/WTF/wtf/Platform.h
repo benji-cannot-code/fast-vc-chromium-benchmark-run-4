02102fc6f7e4796b0f0a36575865e7b2d18454b3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-26 00:08:19 +0000

    [BlackBerry] Add JSC statistics into about:memory https://bugs.webkit.org/show_bug.cgi?id=89779
    Patch by Yong Li <yoli@rim.com> on 2012-06-25
    Reviewed by Rob Buis.
    
    Source/JavaScriptCore:
    
    Add MemoryStatistics.cpp into build, and fill JITBytes for BlackBerry port.
    
    * PlatformBlackBerry.cmake:
    * runtime/MemoryStatistics.cpp:
    (JSC::globalMemoryStatistics):
    
    Source/WebKit/blackberry:
    
    Add detailed JS memory statistics to about:memory page.
    
    * WebCoreSupport/AboutData.cpp:
    (WebCore::dumpJSCTypeCountSetToTableHTML):
    (WebCore):
    (WebCore::memoryPage):
    
    Source/WTF:
    
    Turn on WTF_USE_EXPORT_MACROS for BlackBerry port.
    This will make macros like JS_EXPORT_PRIVATE work without
    extra porting.
    
    * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -1067,7 +1067,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
    since most ports try to support sub-project independence, adding new headers
    to WTF causes many ports to break, and so this way we can address the build
    breakages one port at a time. */
-#if !defined(WTF_USE_EXPORT_MACROS) && (PLATFORM(MAC) || PLATFORM(QT) || PLATFORM(WX))
+#if !defined(WTF_USE_EXPORT_MACROS) && (PLATFORM(MAC) || PLATFORM(QT) || PLATFORM(WX) || PLATFORM(BLACKBERRY))
 #define WTF_USE_EXPORT_MACROS 1
 #endif
 
