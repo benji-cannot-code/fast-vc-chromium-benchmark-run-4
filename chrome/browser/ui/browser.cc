0d0e55899623ae5cafdbf7df9f328fb63e81f911
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 00:11:55 +0000

    Add special visibility rules for bookmarks under Instant Extended API
    Adds special visibility rules for bookmarks under Instant Extended API.
    
    BUG=134518
    TEST=Manual: Open NTP -> no bookmarks, Open SRP -> no bookmarks, Open foo.com -> bookmarks.
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10662026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144065 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -4047,6 +4047,11 @@ void Browser::UpdateBookmarkBarState(BookmarkBarStateChangeReason reason) {
     else
       state = BookmarkBar::HIDDEN;
   }
+
+  // Only allow the bookmark bar to be shown in default mode.
+  if (!search_model_->mode().is_default())
+    state = BookmarkBar::HIDDEN;
+
   if (state == bookmark_bar_state_)
     return;
 
