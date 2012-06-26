dac4b87f7b9d044ed93b6f0c1a1e05a5a96d6ae0
zysxqn@google.com <zysxqn@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 00:28:46 +0000

    Add a regenerate button to regenerate the password in Windows.
    BUG=120480
    TEST=Not tested.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10642009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144071 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/password_generation_bubble_view.h
+++ chrome/browser/ui/views/password_generation_bubble_view.h
@@ -25,6 +25,7 @@ class RenderViewHost;
 }
 
 namespace views {
+class ImageButton;
 class TextButton;
 class Textfield;
 }
@@ -45,7 +46,8 @@ class PasswordGenerationBubbleView : public views::BubbleDelegateView,
                                content::RenderViewHost* render_view_host,
                                autofill::PasswordGenerator* password_generator,
                                content::PageNavigator* navigator,
-                               PasswordManager* password_manager);
+                               PasswordManager* password_manager,
+                               ui::ThemeProvider* theme_provider);
   virtual ~PasswordGenerationBubbleView();
 
  private:
@@ -65,6 +67,7 @@ class PasswordGenerationBubbleView : public views::BubbleDelegateView,
 
   // Subviews
   views::TextButton* accept_button_;
+  views::ImageButton* regenerate_button_;
   views::Textfield* text_field_;
 
   // Location that the bubble points to
@@ -85,6 +88,9 @@ class PasswordGenerationBubbleView : public views::BubbleDelegateView,
 
   // PasswordManager associated with this tab.
   PasswordManager* password_manager_;
+
+  // Theme provider used to draw the regenerate button.
+  ui::ThemeProvider* theme_provider_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordGenerationBubbleView);
 };
