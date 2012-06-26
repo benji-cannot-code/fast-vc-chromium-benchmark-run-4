4b04218a9e12e5fbcaa299bf0725049047d567af
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-26 04:47:14 +0000

    [Qt] Make it possible to build WebKit without QtWidgets https://bugs.webkit.org/show_bug.cgi?id=78109
    Reviewed by Tor Arne Vestbø.
    
    .:
    
    * Source/QtWebKit.pro: Don't build WK1 tests and examples if WK1 is disabled.
    * Source/api.pri: Move WK1 sources away from here and use WEBKIT += webkit1 instead
    * WebKit.pro: Add WK1 to SUBDIRS unless no_webkit1 is set.
    
    Source/WebKit:
    
    Moved WebKit1 sources into a separate static library. Only WK1
    depends on QtWidgets.
    
    * WebKit1.pri: Added.
    * WebKit1.pro: Added.
    
    Source/WebKit2:
    
    * WebKit2.pri: Add missing WK2 specific dependencies.
    * qt/MainQt.cpp: Do the QStyle initialization hook only if we are
    compiling with QtWidgets support.
    (WebKit):
    (main):
    
    Tools:
    
    * Tools.pro: Disable various tools that depend on WK1.
    * qmake/mkspecs/features/default_post.prf: Add WK1 as supported static lib in WEBKIT
    * qmake/mkspecs/features/default_pre.prf: Disable WK1 if QtWidgets is not built.
    * qmake/mkspecs/modules/webkit1.prf: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/qt/MainQt.cpp
+++ third_party/WebKit/Source/WebKit2/qt/MainQt.cpp
@@ -35,7 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 Q_DECL_IMPORT int WebProcessMainQt(QGuiApplication*);
+#if !defined(QT_NO_WIDGETS)
 Q_DECL_IMPORT void initializeWebKit2Theme();
+#endif
 }
 
 #if !defined(NDEBUG) && defined(Q_OS_UNIX)
@@ -73,7 +75,9 @@ int main(int argc, char** argv)
     }
 #endif
 
+#if !defined(QT_NO_WIDGETS)
     WebKit::initializeWebKit2Theme();
+#endif
 
     // Has to be done before QApplication is constructed in case
     // QApplication itself produces debug output.
