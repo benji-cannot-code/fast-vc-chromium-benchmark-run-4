ec7239ab9d2d6eef459d108f81b322404326dec5
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 23:14:07 +0000

    Removes unused thumbnail and favicon fields from PageUsageData.
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10673005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144316 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/page_usage_data.cc
+++ chrome/browser/history/page_usage_data.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -7,37 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "third_party/skia/include/core/SkBitmap.h"
-
 PageUsageData::PageUsageData(history::URLID id)
     : id_(id),
-      thumbnail_(NULL),
-      thumbnail_set_(false),
-      thumbnail_pending_(false),
-      favicon_(NULL),
-      favicon_set_(false),
-      favicon_pending_(false),
       score_(0.0) {
 }
 
 PageUsageData::~PageUsageData() {
-  delete thumbnail_;
-  delete favicon_;
-}
-
-void PageUsageData::SetThumbnail(SkBitmap* img) {
-  if (thumbnail_ && thumbnail_ != img)
-    delete thumbnail_;
-
-  thumbnail_ = img;
-  thumbnail_set_ = true;
-}
-
-void PageUsageData::SetFavicon(SkBitmap* img) {
-  if (favicon_ && favicon_ != img)
-    delete favicon_;
-  favicon_ = img;
-  favicon_set_ = true;
 }
 
 // static
