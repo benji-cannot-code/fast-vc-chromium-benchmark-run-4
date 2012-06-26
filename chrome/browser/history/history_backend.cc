aff3f3ff968956a5c3b946869a432bcedcd64fe0
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 12:08:26 +0000

    When adding a no-visit-history item for a bookmark URL, also give that history item a title.
    Android's content provider needs to provide a title for the bookmark
    even if it hasn't been visited yet. By default, we use the bookmark
    title as the history item title, and fall back to the URL if there
    is no bookmark title.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10592019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_backend.cc
+++ chrome/browser/history/history_backend.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/string_util.h"
 #include "base/time.h"
+#include "base/utf_string_conversions.h"
 #include "chrome/browser/autocomplete/history_url_provider.h"
 #include "chrome/browser/bookmarks/bookmark_service.h"
 #include "chrome/browser/cancelable_request.h"
@@ -962,7 +963,8 @@ void HistoryBackend::SetPageTitle(const GURL& url,
   }
 }
 
-void HistoryBackend::AddPageNoVisitForBookmark(const GURL& url) {
+void HistoryBackend::AddPageNoVisitForBookmark(const GURL& url,
+                                               const string16& title) {
   if (!db_.get())
     return;
 
@@ -972,6 +974,13 @@ void HistoryBackend::AddPageNoVisitForBookmark(const GURL& url) {
     // URL is already known, nothing to do.
     return;
   }
+
+  if (!title.empty()) {
+    url_info.set_title(title);
+  } else {
+    url_info.set_title(UTF8ToUTF16(url.spec()));
+  }
+
   url_info.set_last_visit(Time::Now());
   // Mark the page hidden. If the user types it in, it'll unhide.
   url_info.set_hidden(true);
