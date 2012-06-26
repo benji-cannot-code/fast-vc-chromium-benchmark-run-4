af18b817535e4b34b650456e4bee55e5d654cbdb
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 23:37:02 +0000

    Make SplitString() and variants clear their outparam vector.  (Note that SplitStringIntoKeyValues() and SplitStringIntoKeyValuePairs() already did this.)  This is more in line with what other APIs that take outparams do.
    I audited all callers, and the only ones affected by this are the buggy ones in bug 134695 that already wanted this behavior, as well as the couple of places in this CL that were manually calling clear() and now don't have to.
    
    BUG=134695
    TEST=none
    TBR=brettw,phajdan.jr
    Review URL: https://chromiumcodereview.appspot.com/10684003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144323 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_vms.cc
+++ net/ftp/ftp_directory_listing_parser_vms.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -218,7 +218,6 @@ bool ParseFtpDirectoryListingVms(
         return false;
 
       // Join the current and next line and split them into columns.
-      columns.clear();
       base::SplitString(
           CollapseWhitespace(lines[i] + ASCIIToUTF16(" ") + lines[i + 1],
                              false),
