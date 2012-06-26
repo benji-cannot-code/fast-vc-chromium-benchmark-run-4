736e73ad6b2fa6d5e3758aac442626ef33d0d5ff
jcivelli@chromium.org <jcivelli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-26 01:12:06 +0000

    Fix progress load reporting for MHTML documents.
    MHTML documents containing references to resources they don't include
    never finish loading.
    https://bugs.webkit.org/show_bug.cgi?id=71859
    
    Reviewed by Nate Chapin.
    
    Source/WebCore:
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::scheduleArchiveLoad):
    
    Source/WebKit/chromium:
    
    * tests/EventListenerTest.cpp:
    * tests/FrameTestHelpers.cpp:
    (FrameTestHelpers):
    (WebKit::FrameTestHelpers::registerMockedURLLoad):
    (WebKit::FrameTestHelpers::registerMockedURLLoadAsHTML):
    (WebKit::FrameTestHelpers::createWebViewAndLoad):
    * tests/FrameTestHelpers.h:
    (FrameTestHelpers):
    * tests/ListenerLeakTest.cpp:
    (WebKit::ListenerLeakTest::RunTest):
    * tests/WebFrameTest.cpp:
    (WebKit::WebFrameTest::registerMockedHttpURLLoad):
    (WebKit::WebFrameTest::registerMockedChromeURLLoad):
    * tests/WebViewTest.cpp:
    (WebKit::TEST_F):
    (WebKit::WebViewTest::testAutoResize):
    (WebKit::WebViewTest::testTextInputType):
    (TestWebViewClient):
    (WebKit::TestWebViewClient::TestWebViewClient):
    (WebKit::TestWebViewClient::didStartLoading):
    (WebKit::TestWebViewClient::didStopLoading):
    (WebKit::TestWebViewClient::didChangeLoadProgress):
    (WebKit::TestWebViewClient::loadingStarted):
    (WebKit::TestWebViewClient::loadingStopped):
    (WebKit::TestWebViewClient::loadProgress):
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121206 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -74,12 +74,12 @@ public:
 
     void registerMockedHttpURLLoad(const std::string& fileName)
     {
-        FrameTestHelpers::registerMockedURLLoad(m_baseURL, fileName);
+        FrameTestHelpers::registerMockedURLLoadAsHTML(m_baseURL, fileName);
     }
 
     void registerMockedChromeURLLoad(const std::string& fileName)
     {
-        FrameTestHelpers::registerMockedURLLoad(m_chromeURL, fileName);
+        FrameTestHelpers::registerMockedURLLoadAsHTML(m_chromeURL, fileName);
     }
 
 protected:
