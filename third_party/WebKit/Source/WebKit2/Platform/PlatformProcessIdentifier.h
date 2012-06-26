6c3890a09e7d08647b68c8921e041de81841db1b
rakuco@webkit.org <rakuco@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-26 03:46:14 +0000

    [EFL][WK2] Include unistd.h in PlatformProcessIdentifier.h. https://bugs.webkit.org/show_bug.cgi?id=89804
    Reviewed by Hajime Morita.
    
    The pid_t typedef ends up coming from stdlib.h (indirectly via WTF
    includes) on Mac and Linux, even though that is not mandated by
    POSIX and breaks other platforms (such as FreeBSD). Fix that by
    explicitly including unistd.h, which is where the typedef is
    supposed to come from.
    
    * Platform/PlatformProcessIdentifier.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/PlatformProcessIdentifier.h
+++ third_party/WebKit/Source/WebKit2/Platform/PlatformProcessIdentifier.h
@@ -30,6 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if PLATFORM(QT)
 class QProcess;
+#elif PLATFORM(EFL)
+#include <unistd.h>
 #endif
 
 namespace WebKit {
