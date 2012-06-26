da5bc33bc9e4662f21c8ecd0a5980d30c5a08152
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 22:22:16 +0000

    Moved URL from .grd file into source code to create smaller localization files
    BUG=116372
    TEST=chrome://settings/clearBrowserData has a working "Learn more" link.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10656051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options2/clear_browser_data_handler2.cc
+++ chrome/browser/ui/webui/options2/clear_browser_data_handler2.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/browsing_data_helper.h"
 #include "chrome/browser/browsing_data_remover.h"
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
@@ -22,6 +23,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
 #include "ui/base/l10n/l10n_util.h"
+
+namespace {
+const char kClearBrowsingDataLearnMoreUrl[] =
+    "https://support.google.com/chrome/?p=settings_clear_browsing_data";
+}
 
 namespace options2 {
 
@@ -59,12 +65,15 @@ void ClearBrowserDataHandler::GetLocalizedValues(
       IDS_DEAUTHORIZE_CONTENT_LICENSES_CHKBOX },
     { "clearBrowserDataCommit", IDS_CLEAR_BROWSING_DATA_COMMIT },
     { "flash_storage_url", IDS_FLASH_STORAGE_URL },
-    { "clearBrowsingDataLearnMoreUrl", IDS_CLEAR_BROWSING_DATA_LEARN_MORE_URL },
   };
 
   RegisterStrings(localized_strings, resources, arraysize(resources));
   RegisterTitle(localized_strings, "clearBrowserDataOverlay",
                 IDS_CLEAR_BROWSING_DATA_TITLE);
+  localized_strings->SetString(
+      "clearBrowsingDataLearnMoreUrl",
+      google_util::StringAppendGoogleLocaleParam(
+          kClearBrowsingDataLearnMoreUrl));
 
   ListValue* time_list = new ListValue;
   for (int i = 0; i < 5; i++) {
