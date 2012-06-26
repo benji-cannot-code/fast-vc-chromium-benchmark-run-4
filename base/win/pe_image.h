d9d0b11de6b24a03a780962ba0927c90e616d0d0
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 21:19:05 +0000

    Better fix for http://codereview.chromium.org/10661041/
    While the previous works, this one actually makes sense.
    
    BUG=127799
    TEST=it builds
    Review URL: https://chromiumcodereview.appspot.com/10662040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144277 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/pe_image.h
+++ base/win/pe_image.h
@@ -15,12 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <windows.h>
 
-#if _MSC_VER <= 1600
-// The windows SDK now defines in FACILITY_VISUALCPP winerror.h which
-// clashes with the older crt's definition of the same.
+#if defined(_WIN32_WINNT_WIN8)
+// The Windows 8 SDK defines FACILITY_VISUALCPP in winerror.h.
 #undef FACILITY_VISUALCPP
-#include <DelayIMP.h>
 #endif
+#include <DelayIMP.h>
 
 namespace base {
 namespace win {
