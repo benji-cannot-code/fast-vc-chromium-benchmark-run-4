d356859a40ac32f5355308579238ec8d545b400f
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-26 16:04:40 +0000

    Change the parent container for the IME candidate window.
    This CL creates a new container (InputMethodContainer) and use it.
    
    BUG=130713
    TEST=manually checked with app-list and network dialog.  Made sure doesn't break alt-tab.  No additional breaks on aura_shell_unittest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10629013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/candidate_window.cc
+++ chrome/browser/chromeos/input_method/candidate_window.cc
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/non_client_view.h"
 
 #if defined(USE_ASH)
+#include "ash/shell.h"
+#include "ash/shell_window_ids.h"
 #include "ash/wm/window_animations.h"
 #endif  // USE_ASH
 
@@ -1592,7 +1594,13 @@ void CandidateWindowControllerImpl::CreateView() {
   // they should use WIDGET_OWNS_NATIVE_WIDGET ownership.
   params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   // Show the candidate window always on top
+#if defined(USE_ASH)
+  params.parent = ash::Shell::GetContainer(
+      ash::Shell::GetActiveRootWindow(),
+      ash::internal::kShellWindowId_InputMethodContainer);
+#else
   params.keep_on_top = true;
+#endif
   frame_->Init(params);
 #if defined(USE_ASH)
   ash::SetWindowVisibilityAnimationType(
