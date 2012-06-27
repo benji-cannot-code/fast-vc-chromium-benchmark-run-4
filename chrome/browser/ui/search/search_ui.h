220ed4f1906639488b578d8bcddf2732bdd6c9b4
alicet@chromium.org <alicet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 17:17:43 +0000

    Add placeholder text left spacing for alternate ntp page.
    Per mocks, the placeholder text and blinking cursor of the edit box on
    alternate ntp page should start 11px right of where it is now. Added
    background so that the inset will be colored as the edit box.
    
    BUG=134365
    TEST=manual on linux aura.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10678008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/search_ui.h
+++ chrome/browser/ui/search/search_ui.h
@@ -24,6 +24,9 @@ extern const SkColor kNTPBackgroundColor;
 // Color for the placeholder text on NTP.
 extern const SkColor kNTPPlaceholderTextColor;
 
+// Color for the omnibox background
+extern const SkColor kOmniboxBackgroundColor;
+
 // Color for the separator between results and the page.
 extern const SkColor kResultsSeparatorColor;
 
