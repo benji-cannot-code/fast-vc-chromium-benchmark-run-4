9c5eb90c8dc27847326a656a183d450a35108f70
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 00:31:18 +0000

    build fix
    BUG=134372
    TEST=none
    TBR=rlp
    
    Review URL: https://chromiumcodereview.appspot.com/10678006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/icon_label_bubble_view.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
-#include "grit/theme_resources.h"
+#include "grit/theme_resources_standard.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/views/controls/image_view.h"
