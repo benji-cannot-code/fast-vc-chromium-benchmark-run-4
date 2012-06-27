420f04e8fa8b4a5d23941e439c74ebfbec830bf6
sque@chromium.org <sque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 02:42:50 +0000

    webkit: Include stdio.h in cpp_binding_example
    In order to use printf(), <stdio.h> should be explicitly included.
    
    BUG=chromium:134180
    TEST=build cpp_binding_example successfully
    
    Change-Id: I372d726672b0f252649afa94862a829e29245126
    Signed-off-by: Mandeep Singh Baines <msb@chromium.org>
    Signed-off-by: Simon Que <sque@chromium.org>
    
    
    Review URL: https://chromiumcodereview.appspot.com/10634037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144381 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/cpp_binding_example.cc
+++ webkit/glue/cpp_binding_example.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cpp_binding_example.h"
 
+#include <stdio.h>
+
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 
