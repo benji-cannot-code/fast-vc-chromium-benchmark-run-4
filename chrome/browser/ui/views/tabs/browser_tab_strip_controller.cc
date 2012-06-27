33e1b8a04b986b3d665ac487fb18031859758725
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 23:47:45 +0000

    views: Be consistent in our for loop usage.
    This fixes all the occurrences I could find with:
    
    $ g grep -n "for (" chrome/browser/ui/views/  | grep "iter =" | grep iterator | grep -v chrome/browser/ui/views/ash/
    $ g grep -n "for (" chrome/browser/ui/views/  | grep "it =" | grep iterator | grep -v chrome/browser/ui/views/ash/
    $ g grep -n "for (" chrome/browser/ui/views/  | grep "i =" | grep iterator | grep -v chrome/browser/ui/views/ash/
    
    This will help us to push others to follow the same style everywhere under this
    directory in codereviews. So we can tell them to do a quick grep and follow the
    convention.
    
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10695002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144609 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/browser_tab_strip_controller.cc
@@ -557,7 +557,7 @@ void BrowserTabStripController::StartHighlightTabsForCommand(
     if (IsValidIndex(model_index)) {
       std::vector<int> indices =
           model_->GetIndicesClosedByCommand(model_index, command_id);
-      for (std::vector<int>::const_iterator i = indices.begin();
+      for (std::vector<int>::const_iterator i(indices.begin());
            i != indices.end(); ++i) {
         tabstrip_->StartHighlight(*i);
       }
