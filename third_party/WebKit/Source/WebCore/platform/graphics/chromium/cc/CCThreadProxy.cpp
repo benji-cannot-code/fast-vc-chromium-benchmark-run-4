9d9424942b016ba07e90e220eaa63cce47adfda5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-27 01:49:20 +0000

    [chromium] Remove WebView::graphicsContext3D getter https://bugs.webkit.org/show_bug.cgi?id=89916
    Patch by James Robinson <jamesr@chromium.org> on 2012-06-26
    Reviewed by Adrienne Walker.
    
    Source/Platform:
    
    Remove unused getter.
    
    * chromium/public/WebLayerTreeView.h:
    (WebLayerTreeView):
    
    Source/WebCore:
    
    Deletes code supporting compositor context getter.
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    (CCLayerTreeHost):
    * platform/graphics/chromium/cc/CCProxy.h:
    (CCProxy):
    * platform/graphics/chromium/cc/CCSingleThreadProxy.cpp:
    * platform/graphics/chromium/cc/CCSingleThreadProxy.h:
    (CCSingleThreadProxy):
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    * platform/graphics/chromium/cc/CCThreadProxy.h:
    (CCThreadProxy):
    
    Source/WebKit/chromium:
    
    Removes WebView::graphicsContext3D getter. This getter was used to access the compositor's context, which is an
    inherently dangerous operation since the compositor context may not be safe to use on the main thread and has
    somewhat complicated creation / recreation semantics. A shared context is exposed
    (WebView::sharedGraphicsContext3D) for callers who may want access to a context in a share group with the
    compositor.
    
    * public/WebView.h:
    (WebView):
    * src/WebLayerTreeView.cpp:
    * src/WebViewImpl.cpp:
    * src/WebViewImpl.h:
    (WebViewImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121310 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.cpp
@@ -158,11 +158,6 @@ void CCThreadProxy::requestStartPageScaleAnimationOnImplThread(IntSize targetPos
         m_layerTreeHostImpl->startPageScaleAnimation(targetPosition, useAnchor, scale, monotonicallyIncreasingTime(), duration);
 }
 
-CCGraphicsContext* CCThreadProxy::context()
-{
-    return 0;
-}
-
 void CCThreadProxy::finishAllRendering()
 {
     ASSERT(CCProxy::isMainThread());
