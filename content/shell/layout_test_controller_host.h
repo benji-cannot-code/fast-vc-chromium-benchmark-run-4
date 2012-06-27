6985e2e8fd14b0dda449ad2c9ce856ed9fb6ac8c
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 10:58:51 +0000

    Add basic support to dump pixel results
    BUG=111316
    TEST="webkit/tools/layout_tests/run_webkit_tests.py --chromium --debug --driver-name=content_shell --additional-drt-flag=--dump-render-tree css3/css3-modsel-33.html" should pass
    
    Review URL: https://chromiumcodereview.appspot.com/10656044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144436 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/layout_test_controller_host.h
+++ content/shell/layout_test_controller_host.h
@@ -13,12 +13,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/cancelable_callback.h"
 #include "content/public/browser/render_view_host_observer.h"
 
+class SkBitmap;
+
 namespace content {
 
 class LayoutTestControllerHost : public RenderViewHostObserver {
  public:
   static LayoutTestControllerHost* FromRenderViewHost(
       RenderViewHost* render_view_host);
+
+  // Initialize the LayoutTestControllerHost for a given test.
+  static void Init(const std::string& expected_pixel_hash);
 
   explicit LayoutTestControllerHost(RenderViewHost* render_view_host);
   virtual ~LayoutTestControllerHost();
@@ -37,6 +42,7 @@ class LayoutTestControllerHost : public RenderViewHostObserver {
   // Message handlers.
   void OnDidFinishLoad();
   void OnTextDump(const std::string& dump);
+  void OnImageDump(const std::string& actual_pixel_hash, const SkBitmap& image);
 
   // layoutTestController handlers.
   void OnNotifyDone();
@@ -47,6 +53,7 @@ class LayoutTestControllerHost : public RenderViewHostObserver {
   void OnWaitUntilDone();
 
   static std::map<RenderViewHost*, LayoutTestControllerHost*> controllers_;
+  static std::string expected_pixel_hash_;
 
   bool captured_dump_;
 
--- content/shell/shell_messages.h
+++ content/shell/shell_messages.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Multiply-included file, no traditional include guard.
 #include <string>
 
+#include "content/public/common/common_param_traits.h"
 #include "ipc/ipc_message_macros.h"
+#include "third_party/skia/include/core/SkBitmap.h"
 
 #define IPC_MESSAGE_START ShellMsgStart
 
@@ -17,9 +19,19 @@ IPC_MESSAGE_ROUTED3(ShellViewMsg_CaptureTextDump,
                     bool /* printing */,
                     bool /* recursive */)
 
+// Tells the render view to capture an image of the page. The render view
+// responds with a ShelLViewHostMsg_ImageDump.
+IPC_MESSAGE_ROUTED1(ShellViewMsg_CaptureImageDump,
+                    std::string /* expected pixel hash */)
+
 // Send a text dump of the WebContents to the render host.
 IPC_MESSAGE_ROUTED1(ShellViewHostMsg_TextDump,
                     std::string /* dump */)
+
+// Send an image dump of the WebContents to the render host.
+IPC_MESSAGE_ROUTED2(ShellViewHostMsg_ImageDump,
+                    std::string /* actual pixel hash */,
+                    SkBitmap /* image */)
 
 // The main frame of the render view finished loading.
 IPC_MESSAGE_ROUTED0(ShellViewHostMsg_DidFinishLoad)
