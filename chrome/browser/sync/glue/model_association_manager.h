be6dd82705c296617a09d8a7708899cb0ebe1f6a
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 22:19:29 +0000

    sync: fix reentrancy crash in ModelAssociationManager
    Don't call OnTypesLoaded unless we're IDLE, since we could be in the middle of some 
    other process.  
    
    Add a CHECK to ensure we don't re-enter ModelAssociationManager::Initialize.
    
    BUG=134550
    TEST=trigger unrecoverable error with data type while data type configuration is in progress, don't crash.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10687002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144583 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/model_association_manager.h
+++ chrome/browser/sync/glue/model_association_manager.h
@@ -81,7 +81,7 @@ class ModelAssociationManager {
  private:
   enum State {
     // This is the state after |Initialize| is called.
-    INITIAILIZED_TO_CONFIGURE,
+    INITIALIZED_TO_CONFIGURE,
     // Starting a new configuration.
     CONFIGURING,
     // A stop command was issued.
