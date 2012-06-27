211e2f8e50d1fa475552df39348bf4d342cc6e7d
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 04:59:03 +0000

    Remove browser::FindLastActiveWithProfile from app launcher on the NTP. Browser* is accessible through the NTP's WebContents.
    BUG=129187
    Review URL: https://chromiumcodereview.appspot.com/10675013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144401 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/app_launcher_handler.cc
+++ chrome/browser/ui/webui/ntp/app_launcher_handler.cc
@@ -567,7 +567,8 @@ void AppLauncherHandler::HandleLaunchApp(const ListValue* args) {
 
     // To give a more "launchy" experience when using the NTP launcher, we close
     // it automatically.
-    Browser* browser = browser::FindLastActiveWithProfile(profile);
+    Browser* browser = browser::FindBrowserWithWebContents(
+        web_ui()->GetWebContents());
     WebContents* old_contents = NULL;
     if (browser)
       old_contents = browser->GetActiveWebContents();
@@ -640,10 +641,8 @@ void AppLauncherHandler::HandleCreateAppShortcut(const ListValue* args) {
   if (!extension)
     return;
 
-  Browser* browser = browser::FindLastActiveWithProfile(
-      extension_service_->profile());
-  if (!browser)
-    return;
+  Browser* browser = browser::FindBrowserWithWebContents(
+        web_ui()->GetWebContents());
   browser->window()->ShowCreateChromeAppShortcutsDialog(
       browser->profile(), extension);
 }
