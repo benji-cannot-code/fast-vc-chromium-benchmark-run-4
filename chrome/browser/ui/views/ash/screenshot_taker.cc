2c162f31a88b5a49328e7907731902c3fe20800b
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 18:45:05 +0000

    Use capturing with XGetImage() only in screenshot_taker.
    GrabWindowSnapshot() happens in the "Report Issue" (c/b/ui/webui/feedback_ui.cc)
    and XGetImage runs earlier than closing the wrench menu.  This could delay the
    response on "Report Issue" but it's much more important to work correctly for this specific item.
    
    R=derat@chromium.org
    BUG=134818
    TEST=manually done on lumpy / selecting "Report Issue" and Ctrl-F5
    
    
    Review URL: https://chromiumcodereview.appspot.com/10694003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144497 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/screenshot_taker.cc
+++ chrome/browser/ui/views/ash/screenshot_taker.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/window_snapshot/window_snapshot.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
+#include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
 
 #if defined(OS_CHROMEOS)
@@ -109,6 +110,20 @@ void SaveScreenshot(const FilePath& screenshot_directory,
   }
 }
 
+bool GrabWindowSnapshot(aura::Window* window,
+                        const gfx::Rect& snapshot_bounds,
+                        std::vector<unsigned char>* png_data) {
+#if defined(OS_LINUX)
+  // We use XGetImage() for Linux/ChromeOS for performance reasons.
+  // See crbug.com/119492
+  // TODO(mukai): remove this when the performance issue has been fixed.
+  if (window->GetRootWindow()->GrabSnapshot(snapshot_bounds, png_data))
+    return true;
+#endif  // OS_LINUX
+
+  return browser::GrabWindowSnapshot(window, png_data, snapshot_bounds);
+}
+
 // How opaque should the layer that we flash onscreen to provide visual
 // feedback after the screenshot is taken be?
 const float kVisualFeedbackLayerOpacity = 0.25f;
@@ -149,7 +164,7 @@ void ScreenshotTaker::HandleTakePartialScreenshot(
 
   bool use_24hour_clock = ShouldUse24HourClock();
 
-  if (browser::GrabWindowSnapshot(window, &png_data->data(), rect)) {
+  if (GrabWindowSnapshot(window, rect, &png_data->data())) {
     DisplayVisualFeedback(rect);
     content::BrowserThread::PostTask(
         content::BrowserThread::FILE, FROM_HERE,
