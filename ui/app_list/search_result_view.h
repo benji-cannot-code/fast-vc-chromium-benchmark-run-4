2d507de62c566dca99ed76026010c5f3eaf480ca
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 19:11:18 +0000

    app_list: Document the ownership of the remaining data members.
    R=xiyuan@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10664031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144505 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/search_result_view.h
+++ ui/app_list/search_result_view.h
@@ -58,12 +58,12 @@ class SearchResultView : public views::CustomButton,
   // SearchResultObserver overrides:
   virtual void OnIconChanged() OVERRIDE;
 
-  SearchResult* result_;
+  SearchResult* result_;  // Owned by AppListModel::SearchResults.
 
   // Parent list view. Owned by views hierarchy.
   SearchResultListView* list_view_;
 
-  views::ImageView* icon_;  // Owned by views hierarchy
+  views::ImageView* icon_;  // Owned by views hierarchy.
   scoped_ptr<gfx::RenderText> title_text_;
   scoped_ptr<gfx::RenderText> details_text_;
 
