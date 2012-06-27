112c4aca50933f43c9f08ee989f46ed0054b6376
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 22:07:20 +0000

    Remove --do-not-create-shortcuts flag from setup.exe
    As a precursor to extracting the install logic in subfunctions, might as well remove this flag which we decided was never used and is no longer needed (it was originally meant for Chrome Frame, but the installer path for CF now does this correctly without this flag).
    
    BUG=132825,127544
    TEST=Install CF with "setup.exe --chrome-frame --do-not-register-for-update-launch --do-not-launch-chrome" and ensure no shortcuts are created.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10700008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144580 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/master_preferences.cc
+++ chrome/installer/util/master_preferences.cc
@@ -147,8 +147,6 @@ void MasterPreferences::InitializeFromCommandLine(const CommandLine& cmd_line) {
       installer::master_preferences::kCreateAllShortcuts },
     { installer::switches::kDisableLogging,
       installer::master_preferences::kDisableLogging },
-    { installer::switches::kDoNotCreateShortcuts,
-      installer::master_preferences::kDoNotCreateShortcuts },
     { installer::switches::kMsi,
       installer::master_preferences::kMsi },
     { installer::switches::kMultiInstall,
