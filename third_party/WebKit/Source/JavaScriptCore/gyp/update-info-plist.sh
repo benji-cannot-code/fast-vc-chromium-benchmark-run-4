69779b1bb689d0f89db3d64b499d7c624e584734
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-27 09:44:31 +0000

    [Chromium] Remove unused build scripts and empty folders for JavaScriptCore w/ gyp https://bugs.webkit.org/show_bug.cgi?id=90029
    Source/JavaScriptCore: 
    
    Reviewed by Adam Barth.
    
    * gyp: Removed.
    * gyp/generate-derived-sources.sh: Removed.
    * gyp/generate-dtrace-header.sh: Removed.
    * gyp/run-if-exists.sh: Removed.
    * gyp/update-info-plist.sh: Removed.
    
    Source/WebCore: 
    
    * gyp: Removed empty dir.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121334 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/gyp/update-info-plist.sh
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Touch Info.plist to let Xcode know it needs to copy it into the built product
-if [[ "${CONFIGURATION}" != "Production" ]]; then
-    touch "$1";
-fi
