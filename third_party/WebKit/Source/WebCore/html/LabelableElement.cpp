9401c31431a68332867cb5603c65c3ca9e5b09ab
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-27 07:48:24 +0000

    LabelableElement.cpp should include not ElementRareData.h but NodeRareData.h https://bugs.webkit.org/show_bug.cgi?id=90047
    Reviewed by Kent Tamura.
    
    This is a simple refactoring. What LabelableElement uses is
    not ElementRareData but NodeRareData.
    
    No tests. No change in behavior.
    
    * html/LabelableElement.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/LabelableElement.cpp
+++ third_party/WebKit/Source/WebCore/html/LabelableElement.cpp
@@ -26,8 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "LabelableElement.h"
 
-#include "ElementRareData.h"
 #include "LabelsNodeList.h"
+#include "NodeRareData.h"
 #include "RenderStyle.h"
 
 namespace WebCore {
