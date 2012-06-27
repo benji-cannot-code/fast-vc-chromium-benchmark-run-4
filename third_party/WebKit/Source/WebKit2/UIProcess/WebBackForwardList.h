9d848de1b66f1fe9c705826bcba17bee82b27003
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-27 18:28:52 +0000

    https://bugs.webkit.org/show_bug.cgi?id=87513 WebBackForwardList needs an overhaul to consistently and clearly handle error conditions.
    Reviewed by Darin Adler.
    
    - We think a null entry might somehow be getting in the list so we now try to prevent that.
    - We think a null entry might somehow be in the list so we now null check when indexing into m_entries.
    - A lot of index math - especially tracking "no current index" - was implicit or wrong.
    - Operating on a WebBackForwardList whose page has been closed is now an explicit no-op.
    - The session state data reading and writing code was fragile and needed an overhaul.
    - This includes adding a new V1 format of the session data that is easier to validate when reading back in.
    
    * UIProcess/WebBackForwardList.cpp:
    (WebKit::WebBackForwardList::~WebBackForwardList):
    (WebKit::WebBackForwardList::pageClosed):
    (WebKit::WebBackForwardList::addItem):
    (WebKit::WebBackForwardList::goToItem):
    (WebKit::WebBackForwardList::backListCount):
    (WebKit::WebBackForwardList::forwardListCount):
    (WebKit::WebBackForwardList::backListAsImmutableArrayWithLimit):
    (WebKit::WebBackForwardList::forwardListAsImmutableArrayWithLimit):
    (WebKit::WebBackForwardList::clear):
    
    * UIProcess/WebBackForwardList.h:
    (WebBackForwardList):
    
    * UIProcess/cf/WebBackForwardListCF.cpp:
    (WebKit::createEmptySessionHistoryDictionary):
    (WebKit::WebBackForwardList::createCFDictionaryRepresentation):
    (WebKit::WebBackForwardList::restoreFromCFDictionaryRepresentation):
    (WebKit::WebBackForwardList::restoreFromV0CFDictionaryRepresentation):
    (WebKit::WebBackForwardList::restoreFromV1CFDictionaryRepresentation):
    (WebKit::extractBackForwardListEntriesFromArray):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebBackForwardList.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebBackForwardList.h
@@ -81,6 +81,8 @@ public:
 #if USE(CF)
     CFDictionaryRef createCFDictionaryRepresentation(WebPageProxy::WebPageProxySessionStateFilterCallback, void* context) const;
     bool restoreFromCFDictionaryRepresentation(CFDictionaryRef);
+    bool restoreFromV0CFDictionaryRepresentation(CFDictionaryRef);
+    bool restoreFromV1CFDictionaryRepresentation(CFDictionaryRef);
 #endif
 
 private:
