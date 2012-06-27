7cb04e40ee4fcbc96296d231149b6d571a6e85b4
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 11:58:57 +0000

    Revert 144382 - [Sync] Make Login class listen to connection and DNS changes
    Reason:
    http://build.chromium.org/p/chromium/builders/Mac10.6%20Sync/builds/19046
    
    Make sync_listen_notifications listen for network changes.
    
    Add a scoped NSAutoreleasePool to sync_listen_notifications.
    
    Relax DEPS for sync/tools a bit.
    
    BUG=106034
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10675012
    
    TBR=akalin@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10690007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144439 0039d316-1c4b-4281-b951-d872f2087c98

--- jingle/notifier/communicator/login.cc
+++ jingle/notifier/communicator/login.cc
@@ -44,19 +44,15 @@ Login::Login(Delegate* delegate,
                       try_ssltcp_first,
                       auth_mechanism) {
   net::NetworkChangeNotifier::AddIPAddressObserver(this);
-  net::NetworkChangeNotifier::AddConnectionTypeObserver(this);
-  net::NetworkChangeNotifier::AddDNSObserver(this);
   ResetReconnectState();
 }
 
 Login::~Login() {
-  net::NetworkChangeNotifier::RemoveDNSObserver(this);
-  net::NetworkChangeNotifier::RemoveConnectionTypeObserver(this);
   net::NetworkChangeNotifier::RemoveIPAddressObserver(this);
 }
 
 void Login::StartConnection() {
-  DVLOG(1) << "Starting connection...";
+  VLOG(1) << "Starting connection...";
   single_attempt_.reset(new SingleLoginAttempt(login_settings_, this));
 }
 
@@ -92,22 +88,7 @@ void Login::OnSettingsExhausted() {
 }
 
 void Login::OnIPAddressChanged() {
-  DVLOG(1) << "Detected IP address change";
-  OnNetworkEvent();
-}
-
-void Login::OnConnectionTypeChanged(
-    net::NetworkChangeNotifier::ConnectionType type) {
-  DVLOG(1) << "Detected connection type change";
-  OnNetworkEvent();
-}
-
-void Login::OnDNSChanged(unsigned detail) {
-  DVLOG(1) << "Detected DNS change";
-  OnNetworkEvent();
-}
-
-void Login::OnNetworkEvent() {
+  VLOG(1) << "Detected IP address change";
   // Reconnect in 1 to 9 seconds (vary the time a little to try to
   // avoid spikey behavior on network hiccups).
   reconnect_interval_ = base::TimeDelta::FromSeconds(base::RandInt(1, 9));
@@ -125,8 +106,8 @@ void Login::TryReconnect() {
   DCHECK_GT(reconnect_interval_.InSeconds(), 0);
   single_attempt_.reset();
   reconnect_timer_.Stop();
-  DVLOG(1) << "Reconnecting in "
-           << reconnect_interval_.InSeconds() << " seconds";
+  VLOG(1) << "Reconnecting in "
+          << reconnect_interval_.InSeconds() << " seconds";
   reconnect_timer_.Start(
       FROM_HERE, reconnect_interval_, this, &Login::DoReconnect);
 }
@@ -138,7 +119,7 @@ void Login::DoReconnect() {
   reconnect_interval_ *= 2;
   if (reconnect_interval_ > kMaxReconnectInterval)
     reconnect_interval_ = kMaxReconnectInterval;
-  DVLOG(1) << "Reconnecting...";
+  VLOG(1) << "Reconnecting...";
   StartConnection();
 }
 
