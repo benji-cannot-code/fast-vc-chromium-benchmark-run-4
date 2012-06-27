14d03d11190b3f7f102d9e90d71663fd308b4822
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 19:59:14 +0000

    Relanding r144496 which was reverted in r144503.
    Fix several shutdown/error-path bugs in VAVDA and avoid unnecessary MakeCurrent() calls
    
    - GpuVideoDecodeAcceleratorHost is already AddRoute'd to
    GpuChannelHost on creation, so
    CommandBufferProxyImpl::OnChannelError doesn't have to call
    GVDAH::OnChannelError as well (was causing double-notifications
    of errors to GVDAH).
    
    - GpuVideoDecodeAcceleratorHost::Send() would DCHECK that no
    error had previously been reported on the channel, but that's
    incorrect because its client will likely still have async
    posted tasks from before the error notifications. Now instead
    of DCHECK'ing simply early-return after an error was seen.
    
    - gpu_video_decode_accelerator.cc:MakeDecoderContextCurrent()
    used to unconditionally bind the GpuCommandBufferStub* into its
    closure, but nothing guarantees the lifetime of the stub; in
    fact context loss will cause the stub to be deleted so this
    could crash as well. Now take a WeakPtr to the stub and check
    its validity before every use (and return false if invalid to
    allow clients to avoid assuming context currency).
    
    - GpuVideoDecodeAccelerator is now a
    GpuCommandBufferStub::DestructionObserver to avoid attempting
    further work once the stub is about to be deleted.
    
    - Removed a bunch of unnecessary MakeCurrent() calls; VAAPI_*
    calls don't need it, and are sometimes made on the decoder (not
    main) thread, from which it is incorrect to call GL-related
    methods such as MakeCurrent() anyway.
    
    BUG=134020
    TEST=manual playback of multiple HW-decoded .mp4's doesn't crash anything when different tabs are closed.
    TBR=posciak,piman
    
    Review URL: https://chromiumcodereview.appspot.com/10692016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144528 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/gpu_messages.h"
 #include "content/common/gpu/gpu_watchdog.h"
 #include "content/common/gpu/image_transport_surface.h"
+#include "content/common/gpu/media/gpu_video_decode_accelerator.h"
 #include "content/common/gpu/sync_point_manager.h"
 #include "gpu/command_buffer/common/constants.h"
 #include "gpu/command_buffer/common/gles2_cmd_utils.h"
