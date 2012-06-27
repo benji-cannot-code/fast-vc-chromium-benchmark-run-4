27b6114376923b3bde522840d1095586b50f2cc8
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 10:46:02 +0000

    Fix crash properly, don't call SetCurrentUserIsOwner() from file thread.
    http://src.chromium.org/viewvc/chrome?view=rev&revision=143967 fixed
    the crash, but the fix didn't look correct to me.
    The code was still calling SetCurrentUserIsOwner() from file thread,
    then posting a task to call it from UI thread.
    
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10680002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144434 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager_impl.cc
+++ chrome/browser/chromeos/login/user_manager_impl.cc
@@ -754,6 +754,7 @@ bool UserManagerImpl::IsCurrentUserOwner() const {
 }
 
 void UserManagerImpl::SetCurrentUserIsOwner(bool is_current_user_owner) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   base::AutoLock lk(is_current_user_owner_lock_);
   is_current_user_owner_ = is_current_user_owner;
 }
@@ -1471,8 +1472,6 @@ void UserManagerImpl::CheckOwnership() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
   bool is_owner = OwnershipService::GetSharedInstance()->IsCurrentUserOwner();
   VLOG(1) << "Current user " << (is_owner ? "is owner" : "is not owner");
-
-  SetCurrentUserIsOwner(is_owner);
 
   // UserManagerImpl should be accessed only on UI thread.
   BrowserThread::PostTask(
