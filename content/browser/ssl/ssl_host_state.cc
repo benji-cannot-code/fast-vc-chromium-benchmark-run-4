9713a487143ec9a0670ac08bea9b089fefa2aa70
jgreenwald@chromium.org <jgreenwald@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-27 00:42:02 +0000

    Add SSlHostState::Clear()
    This method is used by Chrome on Android.  It allows SSL warnings to be
    reset so that SSL error prompts will start getting displayed against
    even if the user previously clicked through the warning page.
    
    BUG=115372
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10384211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144340 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/ssl/ssl_host_state.cc
+++ content/browser/ssl/ssl_host_state.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -49,6 +49,12 @@ void SSLHostState::AllowCertForHost(net::X509Certificate* cert,
   DCHECK(CalledOnValidThread());
 
   cert_policy_for_host_[host].Allow(cert);
+}
+
+void SSLHostState::Clear() {
+  DCHECK(CalledOnValidThread());
+
+  cert_policy_for_host_.clear();
 }
 
 net::CertPolicy::Judgment SSLHostState::QueryPolicy(
