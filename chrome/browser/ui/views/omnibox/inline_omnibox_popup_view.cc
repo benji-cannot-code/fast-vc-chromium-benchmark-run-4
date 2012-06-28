e0edc38178a526b3852b5365a0645b6688d1d03a
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 22:41:41 +0000

    cros alternate ntp: modify suggest background to same as toolbar bottom edge.
    - also change color of infobar separator for instant extended api
    
    BUG=134909
    TEST=verify per bug rpt
    
    
    Review URL: https://chromiumcodereview.appspot.com/10704029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/inline_omnibox_popup_view.cc
+++ chrome/browser/ui/views/omnibox/inline_omnibox_popup_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/omnibox/omnibox_view.h"
+#include "chrome/browser/ui/search/search_ui.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "chrome/browser/ui/views/omnibox/omnibox_result_view.h"
 #include "grit/chromium_strings.h"
@@ -59,9 +60,8 @@ InlineOmniboxPopupView::InlineOmniboxPopupView(
   set_owned_by_client();
   // Our visibility determines whether the popup is open. Start out closed.
   SetVisible(false);
-  // TODO(sky): replace with constant kNTPBackgroundColor.
   set_background(views::Background::CreateSolidBackground(
-                     SkColorSetRGB(0xF5, 0xF5, 0xF5)));
+                     chrome::search::kSuggestBackgroundColor));
 }
 
 InlineOmniboxPopupView::~InlineOmniboxPopupView() {
