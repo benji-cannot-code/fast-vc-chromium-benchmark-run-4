c72888e330c4e257b69a0e825480f57a907096f6
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-28 22:34:25 +0000

    [GTK] Add unit tests for GtkInputMethodFilter https://bugs.webkit.org/show_bug.cgi?id=88698
    Reviewed by Carlos Garcia Campos.
    
    Add unit tests for GtkInputMethodFilter in the WebCore platform layer.
    This change adds the TestGtk test suite which will be used for all non-API
    layer GTK unit tests.
    
    * TestWebKitAPI/GNUmakefile.am: Update the build to include the new tests.
    * TestWebKitAPI/Tests/gtk/InputMethodFilter.cpp: Added.
    (TestWebKitAPI::PlatformWebView::PlatformWebView): Remove the call to gtk_init here
    as it's now in main.cpp.
    * TestWebKitAPI/gtk/main.cpp: Change the g_type_init call to gtk_init, because now
    a majority of all unit tests depend on GTK+ being initialized.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121475 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/gtk/main.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/gtk/main.cpp
@@ -27,11 +27,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "TestsController.h"
 
-#include <glib-object.h>
+#include <gtk/gtk.h>
 
 int main(int argc, char** argv)
 {
-    g_type_init();
+    gtk_init(&argc, &argv);
 
     return TestWebKitAPI::TestsController::shared().run(argc, argv) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
