1ecbbbce970c5ddd36b13d55c3273610a72349a2
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 02:21:41 +0000

    Instead of enqueueing SPDY frames, instead enqueue SPDY streams that are ready to produce data.  This allows us to lazily allocate a stream id.
    BUG=111708
    
    Review URL: https://chromiumcodereview.appspot.com/10448083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144649 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.h
+++ net/spdy/spdy_stream.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define NET_SPDY_SPDY_STREAM_H_
 #pragma once
 
+#include <list>
 #include <string>
 #include <vector>
 
@@ -25,12 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/ssl_client_socket.h"
 #include "net/spdy/spdy_framer.h"
 #include "net/spdy/spdy_protocol.h"
+#include "net/spdy/spdy_session.h"
 
 namespace net {
 
 class AddressList;
 class IPEndPoint;
-class SpdySession;
 class SSLCertRequestInfo;
 class SSLInfo;
 
@@ -96,7 +97,6 @@ class NET_EXPORT_PRIVATE SpdyStream
 
   // SpdyStream constructor
   SpdyStream(SpdySession* session,
-             SpdyStreamId stream_id,
              bool pushed,
              const BoundNetLog& net_log);
 
@@ -261,6 +261,8 @@ class NET_EXPORT_PRIVATE SpdyStream
   int GetProtocolVersion() const;
 
  private:
+  class SpdyStreamIOBufferProducer;
+
   enum State {
     STATE_NONE,
     STATE_GET_DOMAIN_BOUND_CERT,
@@ -309,6 +311,14 @@ class NET_EXPORT_PRIVATE SpdyStream
   // the MessageLoop to replay all the data that the server has already sent.
   void PushedStreamReplayData();
 
+  // Informs the SpdySession that this stream has a write available.
+  void SetHasWriteAvailable();
+
+  // Returns a newly created SPDY frame owned by the called that contains
+  // the next frame to be sent by this frame.  May return NULL if this
+  // stream has become stalled on flow control.
+  SpdyFrame* ProduceNextFrame();
+
   // There is a small period of time between when a server pushed stream is
   // first created, and the pushed data is replayed. Any data received during
   // this time should continue to be buffered.
@@ -343,6 +353,8 @@ class NET_EXPORT_PRIVATE SpdyStream
 
   linked_ptr<SpdyHeaderBlock> response_;
   base::Time response_time_;
+
+  std::list<SpdyFrame*> pending_data_frames_;
 
   State io_state_;
 
