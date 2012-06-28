eadfd8ad8604f52a12d5b95afa639a436e617a2f
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 05:21:09 +0000

    Revert 144578 - Disable core animation plugins when compositing is unavailable.
    When compositing is disabled or blacklisted we won't be able to composite core animation plugins, so disable them.
    
    BUG=122430
    TEST=flash works on nytimes.com with --disable-accelerated-compositing
    
    Review URL: https://chromiumcodereview.appspot.com/10542112
    
    TBR=jbauman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10698028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144665 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/plugin_process_host.cc
+++ content/browser/plugin_process_host.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "content/browser/browser_child_process_host_impl.h"
-#include "content/browser/gpu/gpu_data_manager_impl.h"
 #include "content/browser/plugin_service_impl.h"
 #include "content/common/child_process_host_impl.h"
 #include "content/common/plugin_messages.h"
@@ -222,7 +221,6 @@ bool PluginProcessHost::Init(const webkit::WebPluginInfo& info) {
     switches::kDisableBreakpad,
 #if defined(OS_MACOSX)
     switches::kDisableCompositedCoreAnimationPlugins,
-    switches::kDisableCoreAnimationPlugins,
 #endif
     switches::kDisableLogging,
     switches::kEnableDCHECK,
@@ -242,8 +240,6 @@ bool PluginProcessHost::Init(const webkit::WebPluginInfo& info) {
 
   cmd_line->CopySwitchesFrom(browser_command_line, kSwitchNames,
                              arraysize(kSwitchNames));
-
-  GpuDataManagerImpl::GetInstance()->AppendPluginCommandLine(cmd_line);
 
   // If specified, prepend a launcher program to the command line.
   if (!plugin_launcher.empty())
