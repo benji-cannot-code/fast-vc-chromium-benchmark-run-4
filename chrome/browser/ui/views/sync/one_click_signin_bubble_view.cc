4dacd9d7fa4574fdf0bf77e3ce95c49b25f4aa7a
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 02:01:25 +0000

    Only BookmarkBubbleView::ShowEditor with a valid parent, etc.
    NULL check anchor_widget in BookmarkBubbleView::ShowEditor.
    If NULL, don't show the editor, just apply edits and close.
    Avoids a NULL AV and bringing up a modeless dialog.
    Minor whitespace and comment updates.
    
    Add debugging code to find the root cause of this state.
    CRASHES on debug values reflect some possible culprits.
    TODO(msw): Follow up to determine crash cause(s).
    
    TODO(msw): Nix BubbleDelegateView::WindowClosing???
    (overridden by subclasses... my bad advice from http://codereview.chromium.org/10204014/)
    
    BUG=134504,105151,124310
    TEST=none, less BookmarkBubbleView::ShowEditor crashes.
    
    Review URL: https://chromiumcodereview.appspot.com/10662031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/sync/one_click_signin_bubble_view.cc
+++ chrome/browser/ui/views/sync/one_click_signin_bubble_view.cc
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 const int kMinimumLabelWidth = 240;
 
 
-// BookmarkBubbleView ---------------------------------------------------------
+// OneClickSigninBubbleView ----------------------------------------------------
 
 // static
 OneClickSigninBubbleView* OneClickSigninBubbleView::bubble_view_ = NULL;
