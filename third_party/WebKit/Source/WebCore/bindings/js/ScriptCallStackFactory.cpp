f1b54e70d4f782a3aa327181fed846bcfc58d12b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-28 01:09:22 +0000

    Unreviewed, rolling out r121359. http://trac.webkit.org/changeset/121359 https://bugs.webkit.org/show_bug.cgi?id=90115
    Broke many inspector tests (Requested by jpfau on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-06-27
    
    Source/JavaScriptCore:
    
    * interpreter/Interpreter.h:
    (JSC::StackFrame::toString):
    
    Source/WebCore:
    
    * bindings/js/ScriptCallStackFactory.cpp:
    (WebCore::createScriptCallStack):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/ScriptCallStackFactory.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/ScriptCallStackFactory.cpp
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (c) 2010 Google Inc. All rights reserved.
- * Copyright (C) 2012 Research In Motion Limited. All rights reserved.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -35,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "InspectorInstrumentation.h"
 #include "JSDOMBinding.h"
-#include "JSMainThreadExecState.h"
 #include "ScriptArguments.h"
 #include "ScriptCallFrame.h"
 #include "ScriptCallStack.h"
@@ -54,26 +52,9 @@ namespace WebCore {
 
 class ScriptExecutionContext;
 
-PassRefPtr<ScriptCallStack> createScriptCallStack(size_t maxStackSize, bool emptyIsAllowed)
+PassRefPtr<ScriptCallStack> createScriptCallStack(size_t, bool)
 {
-    Vector<ScriptCallFrame> frames;
-    if (JSC::ExecState* exec = JSMainThreadExecState::currentState()) {
-        Vector<StackFrame> stackTrace;
-        Interpreter::getStackTrace(&exec->globalData(), stackTrace);
-        for (Vector<StackFrame>::const_iterator iter = stackTrace.begin(); iter < stackTrace.end(); iter++) {
-            StackFrame level = *iter;
-            frames.append(ScriptCallFrame(level.friendlyFunctionName(exec), level.friendlySourceURL(), level.friendlyLineNumber()));
-            if (frames.size() >= maxStackSize)
-                break;
-        }
-    }
-    if (frames.isEmpty() && !emptyIsAllowed) {
-        // No frames found. It may happen in the case where
-        // a bound function is called from native code for example.
-        // Fallback to setting lineNumber to 0, and source and function name to "undefined".
-        frames.append(ScriptCallFrame("undefined", "undefined", 0));
-    }
-    return ScriptCallStack::create(frames);
+    return 0;
 }
 
 PassRefPtr<ScriptCallStack> createScriptCallStack(JSC::ExecState* exec, size_t maxStackSize)
