babb62befc5623e38781bc1bb218d55a788680f3
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 23:25:14 +0000

    Fix latency tests.
    This moves the SwapBuffers complete trace event from the Echo command to platform-specific locations that should be less prone to breakage in the future.
    
    BUG=135023
    TBR=shawnsingh
    
    Review URL: https://chromiumcodereview.appspot.com/10729002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144832 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -2079,6 +2079,8 @@ void RenderWidgetHostViewMac::SetTextInputActive(bool active) {
     // that on.
     renderWidgetHostView_->compositing_iosurface_->DrawIOSurface(
         self, ScaleFactor(self));
+    // For latency_tests.cc:
+    UNSHIPPED_TRACE_EVENT_INSTANT0("test_gpu", "CompositorSwapBuffersComplete");
     renderWidgetHostView_->AckPendingSwapBuffers();
     return;
   }
