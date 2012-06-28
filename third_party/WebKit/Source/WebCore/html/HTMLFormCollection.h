e8227c0d5cbafb3af407024e5e3d5183967fcf95
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-28 23:01:06 +0000

    Cleanup HTMLFormCollection https://bugs.webkit.org/show_bug.cgi?id=90111
    Reviewed by Andreas Kling.
    
    Source/WebCore: 
    
    Got rid of getNamedItem and enamed getNamedFormItem to firstNamedItem and got rid of duplicateNumber argument since
    it's always 0. Also made it a static local function. In addition, removed nextItem() since it's not used anywhere.
    
    WebKit API Test: WebKit1.HTMLFormCollectionNamedItemTest
    
    * html/HTMLFormCollection.cpp:
    (WebCore::firstNamedItem):
    (WebCore):
    (WebCore::HTMLFormCollection::namedItem):
    * html/HTMLFormCollection.h:
    (HTMLFormCollection):
    
    Tools: 
    
    Add a WebKit API test using copy-paste design pattern per kling's request.
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/mac/HTMLFormCollectionNamedItem.html: Added.
    * TestWebKitAPI/Tests/mac/HTMLFormCollectionNamedItem.mm: Added.
    (-[HTMLFormCollectionNamedItemTest webView:didFinishLoadForFrame:]):
    (TestWebKitAPI):
    (TestWebKitAPI::TEST):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121478 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFormCollection.h
+++ third_party/WebKit/Source/WebCore/html/HTMLFormCollection.h
@@ -42,8 +42,6 @@ public:
     virtual ~HTMLFormCollection();
 
     virtual Node* item(unsigned index) const;
-    virtual Node* nextItem() const;
-
     virtual Node* namedItem(const AtomicString& name) const;
 
 private:
@@ -51,9 +49,6 @@ private:
 
     virtual void updateNameCache() const;
     virtual unsigned calcLength() const;
-
-    Element* getNamedItem(const QualifiedName& attrName, const AtomicString& name) const;
-    Element* getNamedFormItem(const QualifiedName& attrName, const String& name, int duplicateNumber) const;
 
     const Vector<FormAssociatedElement*>& formControlElements() const;
     const Vector<HTMLImageElement*>& formImageElements() const;
