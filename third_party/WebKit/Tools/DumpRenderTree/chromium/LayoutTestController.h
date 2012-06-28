c7c241997c43837721068be1d2522d437e0319c4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-28 19:45:09 +0000

    [Chromium] Chromium's LayoutTestController is missing setBackingScaleFactor https://bugs.webkit.org/show_bug.cgi?id=83635
    Patch by Terry Anderson <tdanderson@chromium.org> on 2012-06-28
    Reviewed by Adam Barth.
    
    Added InvokeCallbackTask, a new derived class of MethodTask. When
    setBackingScaleFactor is called, a call to setDeviceScaleFactor in
    WebView is made and then postTask is used to invoke the callback
    function specified in testRunner.setBackingScaleFactor.
    
    * DumpRenderTree/chromium/LayoutTestController.cpp:
    (LayoutTestController::LayoutTestController):
    (InvokeCallbackTask):
    (InvokeCallbackTask::InvokeCallbackTask):
    (InvokeCallbackTask::runIfValid):
    (LayoutTestController::setBackingScaleFactor):
    * DumpRenderTree/chromium/LayoutTestController.h:
    (LayoutTestController):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/LayoutTestController.h
@@ -454,6 +454,9 @@ public:
     // resulting in the changed setting being ignored.
     void setTextSubpixelPositioning(const CppArgumentList&, CppVariant*);
 
+    // Used to set the device scale factor.
+    void setBackingScaleFactor(const CppArgumentList&, CppVariant*);
+
 public:
     // The following methods are not exposed to JavaScript.
     void setWorkQueueFrozen(bool frozen) { m_workQueue.setFrozen(frozen); }
