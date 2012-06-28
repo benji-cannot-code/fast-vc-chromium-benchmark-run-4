94d78082eee268dcefc89d1cd12a3d92b310f166
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 00:07:27 +0000

    Add libffmpegsumo.so to Chrome OS PyAuto staged binaries.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10705007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144617 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromeos/autotest/files/client/deps/pyauto_dep/setup_test_links.sh
+++ chrome/test/chromeos/autotest/files/client/deps/pyauto_dep/setup_test_links.sh
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# A script to setup symbolic links needed for Chrome's PyAuto framwork.
+# A script to setup symbolic links needed for Chrome's PyAuto framework.
 
 ln -f -s /opt/google/chrome/chrome $(dirname $0)/chrome
 [ -L $(dirname $0)/locales ] || ln -f -s /opt/google/chrome/locales \
@@ -13,3 +13,4 @@ ln -f -s /opt/google/chrome/chrome $(dirname $0)/chrome
 [ -L $(dirname $0)/resources ] || ln -f -s /opt/google/chrome/resources \
     $(dirname $0)/resources
 ln -f -s /opt/google/chrome/*.pak $(dirname $0)/
+ln -f -s /opt/google/chrome/libffmpegsumo.so $(dirname $0)/libffmpegsumo.so
