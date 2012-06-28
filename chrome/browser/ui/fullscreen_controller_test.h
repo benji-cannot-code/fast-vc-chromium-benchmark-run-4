4145ec12146c6bb7b9a477b8f9206e19a477fc5a
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-28 23:11:31 +0000

    ToggleTabFullscreen made robust to parallel fullscreen transitions on bots.
    Repeat ToggleFullscreenModeForTab until the correct state is entered.
    This addresses flakiness on test bots running many fullscreen
    tests in parallel.
    
    BUG=133831
    
    Review URL: https://chromiumcodereview.appspot.com/10636032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/fullscreen_controller_test.h
+++ chrome/browser/ui/fullscreen_controller_test.h
@@ -41,6 +41,8 @@ class FullscreenControllerTest : public InProcessBrowserTest {
 
   virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE;
   void ToggleTabFullscreen(content::WebContents* tab, bool enter_fullscreen);
+  void ToggleTabFullscreenNoRetries(content::WebContents* tab,
+                                    bool enter_fullscreen);
   void ToggleBrowserFullscreen(bool enter_fullscreen);
   void RequestToLockMouse(content::WebContents* tab,
                           bool user_gesture,
@@ -62,6 +64,9 @@ class FullscreenControllerTest : public InProcessBrowserTest {
   void Reload();
   static const char kFullscreenMouseLockHTML[];
  private:
+  void ToggleTabFullscreenInternal(content::WebContents* tab,
+                                    bool enter_fullscreen,
+                                    bool retry_until_success);
   DISALLOW_COPY_AND_ASSIGN(FullscreenControllerTest);
 };
 
