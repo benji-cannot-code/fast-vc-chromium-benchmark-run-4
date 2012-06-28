41240d5cdcd05d7f7dd234c56913b5f96bbdc53b
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-28 21:10:12 +0000

    Unreviewed, rolling out r121463. http://trac.webkit.org/changeset/121463 https://bugs.webkit.org/show_bug.cgi?id=90094
    Broke Windows build.
    
    * WebKit.gypi:
    * WebKitUnitTests.gyp:
    * public/WebDOMMessageEvent.h:
    * tests/AssociatedURLLoaderTest.cpp:
    (WebKit::AssociatedURLLoaderTest::AssociatedURLLoaderTest):
    (WebKit::AssociatedURLLoaderTest::SetUp):
    (WebKit::AssociatedURLLoaderTest::CheckMethodFails):
    (WebKit::AssociatedURLLoaderTest::CheckHeaderFails):
    (WebKit::AssociatedURLLoaderTest::CheckAccessControlHeaders):
    (WebKit::TEST_F):
    * tests/EventListenerTest.cpp:
    * tests/FrameTestHelpers.cpp:
    (WebKit::FrameTestHelpers::registerMockedURLLoad):
    (FrameTestHelpers):
    (WebKit::FrameTestHelpers::loadFrame):
    * tests/FrameTestHelpers.h:
    (FrameTestHelpers):
    * tests/ListenerLeakTest.cpp:
    (WebKit::ListenerLeakTest::RunTest):
    * tests/PopupMenuTest.cpp:
    (WebKit::SelectPopupMenuTest::registerMockedURLLoad):
    (WebKit::SelectPopupMenuTest::loadFrame):
    (WebKit::TEST_F):
    * tests/RunAllTests.cpp:
    * tests/URLTestHelpers.cpp: Removed.
    * tests/URLTestHelpers.h: Removed.
    * tests/WebFrameTest.cpp:
    (WebKit::WebFrameTest::registerMockedHttpURLLoad):
    (WebKit::WebFrameTest::registerMockedChromeURLLoad):
    (WebKit::TEST_F):
    * tests/WebPageNewSerializerTest.cpp:
    (WebKit::WebPageNewSerializeTest::registerMockedURLLoad):
    (WebPageNewSerializeTest):
    (WebKit::WebPageNewSerializeTest::setUpCSSTestPage):
    (WebKit::WebPageNewSerializeTest::loadURLInTopFrame):
    (WebKit::WebPageNewSerializeTest::resourceVectorContains):
    (WebKit::TEST_F):
    * tests/WebPageSerializerTest.cpp:
    (WebKit::WebPageSerializerTest::registerMockedURLLoad):
    (WebKit::WebPageSerializerTest::loadURLInTopFrame):
    (WebKit::WebPageSerializerTest::webVectorContains):
    (WebKit::TEST_F):
    * tests/WebViewTest.cpp:
    (WebKit::TEST_F):
    (WebKit::WebViewTest::testAutoResize):
    (WebKit::WebViewTest::testTextInputType):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/PopupMenuTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/PopupMenuTest.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PopupMenuClient.h"
 #include "PopupMenuChromium.h"
 #include "RuntimeEnabledFeatures.h"
-#include "URLTestHelpers.h"
 #include "WebDocument.h"
 #include "WebElement.h"
 #include "WebFrame.h"
@@ -65,7 +64,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 using namespace WebKit;
-using WebKit::URLTestHelpers::toKURL;
 
 namespace {
 
@@ -252,7 +250,15 @@ protected:
 
     void registerMockedURLLoad(const std::string& fileName)
     {
-        URLTestHelpers::registerMockedURLLoad(toKURL(baseURL + fileName), WebString::fromUTF8(fileName.c_str()), WebString::fromUTF8("popup/"), WebString::fromUTF8("text/html"));
+        WebURLResponse response;
+        response.initialize();
+        response.setMIMEType("text/html");
+
+        std::string filePath = webkit_support::GetWebKitRootDir().utf8();
+        filePath += "/Source/WebKit/chromium/tests/data/popup/";
+        filePath += fileName;
+
+        webkit_support::RegisterMockedURL(WebURL(GURL(baseURL + fileName)), response, WebString::fromUTF8(filePath));
     }
 
     void serveRequests()
@@ -264,7 +270,7 @@ protected:
     {
         WebURLRequest urlRequest;
         urlRequest.initialize();
-        urlRequest.setURL(WebURL(toKURL(baseURL + fileName)));
+        urlRequest.setURL(WebURL(GURL(baseURL + fileName)));
         frame->loadRequest(urlRequest);
     }
 
@@ -422,7 +428,7 @@ TEST_F(SelectPopupMenuTest, DISABLED_SelectItemEventFire)
 
     // mousedown event is held by select node, and we don't simulate the event for the node.
     // So we can only see mouseup and click event.
-    EXPECT_STREQ("upclick", element.innerText().utf8().data());
+    EXPECT_STREQ("upclick", std::string(element.innerText().utf8()).c_str());
 
     // Disable the item at index 1.
     m_popupMenuClient.setDisabledIndex(1);
@@ -434,7 +440,7 @@ TEST_F(SelectPopupMenuTest, DISABLED_SelectItemEventFire)
     simulateLeftMouseUpEvent(row1Point);
 
     // The item at index 1 is disabled, so the text should not be changed.
-    EXPECT_STREQ("upclick", element.innerText().utf8().data());
+    EXPECT_STREQ("upclick", std::string(element.innerText().utf8()).c_str());
 
     showPopup();
     // menuItemHeight * 2.5 means the Y position on the item at index 2.
@@ -443,7 +449,7 @@ TEST_F(SelectPopupMenuTest, DISABLED_SelectItemEventFire)
     simulateLeftMouseUpEvent(row1Point);
 
     // The item is changed to the item at index 2, from index 0, so change event is fired.
-    EXPECT_STREQ("upclickchangeupclick", element.innerText().utf8().data());
+    EXPECT_STREQ("upclickchangeupclick", std::string(element.innerText().utf8()).c_str());
 }
 
 TEST_F(SelectPopupMenuTest, FLAKY_SelectItemKeyEvent)
@@ -464,7 +470,7 @@ TEST_F(SelectPopupMenuTest, FLAKY_SelectItemKeyEvent)
 
     WebElement element = m_webView->mainFrame()->document().getElementById("message");
     // We only can see change event but no other mouse related events.
-    EXPECT_STREQ("change", element.innerText().utf8().data());
+    EXPECT_STREQ("change", std::string(element.innerText().utf8()).c_str());
 }
 
 TEST_F(SelectPopupMenuTest, SelectItemRemoveSelectOnChange)
@@ -486,7 +492,7 @@ TEST_F(SelectPopupMenuTest, SelectItemRemoveSelectOnChange)
     simulateLeftMouseUpEvent(row1Point);
 
     WebElement element = m_webView->mainFrame()->document().getElementById("message");
-    EXPECT_STREQ("change", element.innerText().utf8().data());
+    EXPECT_STREQ("change", std::string(element.innerText().utf8()).c_str());
 }
 
 TEST_F(SelectPopupMenuTest, SelectItemRemoveSelectOnClick)
@@ -508,7 +514,7 @@ TEST_F(SelectPopupMenuTest, SelectItemRemoveSelectOnClick)
     simulateLeftMouseUpEvent(row1Point);
 
     WebElement element = m_webView->mainFrame()->document().getElementById("message");
-    EXPECT_STREQ("click", element.innerText().utf8().data());
+    EXPECT_STREQ("click", std::string(element.innerText().utf8()).c_str());
 }
 
 } // namespace
