8ac4cdb1ebcc8d02642bc7e4f8bbd68c8821d7c4
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-29 09:50:26 +0000

    [V8] Replace v8::Integer::New() with v8Integer() in custom bindings https://bugs.webkit.org/show_bug.cgi?id=90242
    Reviewed by Yury Semikhatsky.
    
    v8Integer() is a fast wrapper of v8::Integer::New().
    This patch replaces v8::Integer::New() with v8Integer() in custom bindings,
    and pass isolates.
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp:
    (WebCore::V8CSSStyleDeclaration::namedPropertyEnumerator):
    (WebCore::V8CSSStyleDeclaration::namedPropertyQuery):
    * bindings/v8/custom/V8ClipboardCustom.cpp:
    (WebCore::V8Clipboard::typesAccessorGetter):
    * bindings/v8/custom/V8DOMStringMapCustom.cpp:
    (WebCore::V8DOMStringMap::namedPropertyQuery):
    (WebCore::V8DOMStringMap::namedPropertyEnumerator):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::WindowSetTimeoutImpl):
    * bindings/v8/custom/V8DataViewCustom.cpp:
    (WebCore::V8DataView::getInt8Callback):
    (WebCore::V8DataView::getUint8Callback):
    * bindings/v8/custom/V8HTMLInputElementCustom.cpp:
    (WebCore::V8HTMLInputElement::selectionStartAccessorGetter):
    (WebCore::V8HTMLInputElement::selectionEndAccessorGetter):
    * bindings/v8/custom/V8HTMLOptionsCollectionCustom.cpp:
    (WebCore::V8HTMLOptionsCollection::lengthAccessorGetter):
    * bindings/v8/custom/V8InjectedScriptHostCustom.cpp:
    (WebCore::V8InjectedScriptHost::functionDetailsCallback):
    * bindings/v8/custom/V8MessageEventCustom.cpp:
    (WebCore::V8MessageEvent::portsAccessorGetter):
    * bindings/v8/custom/V8MutationCallbackCustom.cpp:
    (WebCore::V8MutationCallback::handleEvent):
    * bindings/v8/custom/V8NodeListCustom.cpp:
    (WebCore::V8NodeList::namedPropertyGetter):
    * bindings/v8/custom/V8SQLTransactionCustom.cpp:
    (WebCore::V8SQLTransaction::executeSqlCallback):
    * bindings/v8/custom/V8SQLTransactionSyncCustom.cpp:
    (WebCore::V8SQLTransactionSync::executeSqlCallback):
    * bindings/v8/custom/V8StorageCustom.cpp:
    (WebCore::V8Storage::namedPropertyEnumerator):
    (WebCore::V8Storage::indexedPropertyGetter):
    (WebCore::V8Storage::namedPropertyQuery):
    (WebCore::V8Storage::indexedPropertySetter):
    (WebCore::V8Storage::indexedPropertyDeleter):
    * bindings/v8/custom/V8WebGLRenderingContextCustom.cpp:
    (WebCore::toV8Object):
    (WebCore::V8WebGLRenderingContext::getAttachedShadersCallback):
    (WebCore::V8WebGLRenderingContext::getSupportedExtensionsCallback):
    * bindings/v8/custom/V8WorkerContextCustom.cpp:
    (WebCore::SetTimeoutOrInterval):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp
@@ -175,7 +175,7 @@ v8::Handle<v8::Array> V8CSSStyleDeclaration::namedPropertyEnumerator(const v8::A
     for (unsigned i = 0; i < propertyNamesLength; ++i) {
         String key = propertyNames.at(i);
         ASSERT(!key.isNull());
-        properties->Set(v8::Integer::New(i), v8String(key, info.GetIsolate()));
+        properties->Set(v8Integer(i, info.GetIsolate()), v8String(key, info.GetIsolate()));
     }
 
     return properties;
@@ -186,7 +186,7 @@ v8::Handle<v8::Integer> V8CSSStyleDeclaration::namedPropertyQuery(v8::Local<v8::
     INC_STATS("DOM.CSSStyleDeclaration.NamedPropertyQuery");
 
     if (cssPropertyInfo(v8Name))
-        return v8::Integer::New(v8::None);
+        return v8Integer(0, info.GetIsolate());
 
     return v8::Handle<v8::Integer>();
 }
--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
@@ -126,19 +126,19 @@ static v8::Handle<v8::Value> toV8Object(const WebGLGetInfo& info, v8::Isolate* i
         const Vector<bool>& value = info.getBoolArray();
         v8::Local<v8::Array> array = v8::Array::New(value.size());
         for (size_t ii = 0; ii < value.size(); ++ii)
-            array->Set(v8::Integer::New(ii), v8::Boolean::New(value[ii]));
+            array->Set(v8Integer(ii, isolate), v8::Boolean::New(value[ii]));
         return array;
     }
     case WebGLGetInfo::kTypeFloat:
         return v8::Number::New(info.getFloat());
     case WebGLGetInfo::kTypeInt:
-        return v8::Integer::New(info.getInt());
+        return v8Integer(info.getInt(), isolate);
     case WebGLGetInfo::kTypeNull:
         return v8::Null(isolate);
     case WebGLGetInfo::kTypeString:
         return v8::String::New(fromWebCoreString(info.getString()), info.getString().length());
     case WebGLGetInfo::kTypeUnsignedInt:
-        return v8::Integer::NewFromUnsigned(info.getUnsignedInt());
+        return v8UnsignedInteger(info.getUnsignedInt(), isolate);
     case WebGLGetInfo::kTypeWebGLBuffer:
         return toV8(info.getWebGLBuffer(), isolate);
     case WebGLGetInfo::kTypeWebGLFloatArray:
@@ -290,7 +290,7 @@ v8::Handle<v8::Value> V8WebGLRenderingContext::getAttachedShadersCallback(const 
         return v8::Null(args.GetIsolate());
     v8::Local<v8::Array> array = v8::Array::New(shaders.size());
     for (size_t ii = 0; ii < shaders.size(); ++ii)
-        array->Set(v8::Integer::New(ii), toV8(shaders[ii].get(), args.GetIsolate()));
+        array->Set(v8Integer(ii, args.GetIsolate()), toV8(shaders[ii].get(), args.GetIsolate()));
     return array;
 }
 
@@ -399,7 +399,7 @@ v8::Handle<v8::Value> V8WebGLRenderingContext::getSupportedExtensionsCallback(co
     Vector<String> value = imp->getSupportedExtensions();
     v8::Local<v8::Array> array = v8::Array::New(value.size());
     for (size_t ii = 0; ii < value.size(); ++ii)
-        array->Set(v8::Integer::New(ii), v8::String::New(fromWebCoreString(value[ii]), value[ii].length()));
+        array->Set(v8Integer(ii, args.GetIsolate()), v8::String::New(fromWebCoreString(value[ii]), value[ii].length()));
     return array;
 }
 
