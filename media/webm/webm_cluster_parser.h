367e9457f607a88d378905c646a46fecf2d5916d
vrk@chromium.org <vrk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-29 23:35:01 +0000

    Add support to WebMClusterParser to accept clusters without blocks
    BUG=NONE
    TEST=media_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10673012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145030 0039d316-1c4b-4281-b951-d872f2087c98

--- media/webm/webm_cluster_parser.h
+++ media/webm/webm_cluster_parser.h
@@ -37,6 +37,7 @@ class MEDIA_EXPORT WebMClusterParser : public WebMParserClient {
   // Returns the number of bytes parsed on success.
   int Parse(const uint8* buf, int size);
 
+  base::TimeDelta cluster_start_time() const { return cluster_start_time_; }
   const BufferQueue& audio_buffers() const { return audio_.buffers(); }
   const BufferQueue& video_buffers() const { return video_.buffers(); }
 
@@ -95,6 +96,7 @@ class MEDIA_EXPORT WebMClusterParser : public WebMParserClient {
   int64 block_duration_;
 
   int64 cluster_timecode_;
+  base::TimeDelta cluster_start_time_;
 
   Track audio_;
   Track video_;
