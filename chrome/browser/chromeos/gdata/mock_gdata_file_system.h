effc3351114228f1422bc47baf02b187dc77638f
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-29 11:50:17 +0000

    Implement CreateFile() for GData file system.
    This CL is the last piece for implementing the File API
    getFile(..., {create: true}, ...) for GDataFileSytem.
    
    Currently the file creation is implemented as uploading of
    an empty file to the server. The Drive server assigns the
    resource_id, and further read or write operation just
    treats it as a non-pinned file (e.g., when it is read or
    written it is downloaded to GCache/v1/tmp/<resource_id>.<md5>).
    
    BUG=132968
    TEST=Manual test.
    1) Be sure prerequisites patches (10636053, 10656043, and 10556003) are applied.
    2) Edit image files on Google Drive folder by photo editor, and with "overwrite original" unchecked)
    3) Verify a new file is created on the Drive.
    
    Review URL: https://chromiumcodereview.appspot.com/10568012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144898 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/mock_gdata_file_system.h
+++ chrome/browser/chromeos/gdata/mock_gdata_file_system.h
@@ -58,6 +58,10 @@ class MockGDataFileSystem : public GDataFileSystemInterface {
                     bool is_exclusive,
                     bool is_recursive,
                     const FileOperationCallback& callback));
+  MOCK_METHOD3(CreateFile,
+               void(const FilePath& file_path,
+                    bool is_exclusive,
+                    const FileOperationCallback& callback));
   MOCK_METHOD3(GetFileByPath,
                void(const FilePath& file_path,
                     const GetFileCallback& get_file_callback,
