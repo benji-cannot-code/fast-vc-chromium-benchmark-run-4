c490ecad8ef27de30f849459f7104da4f6793523
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-29 04:41:42 +0000

    Allow to release WebSocket resource in completion callbacks invoked by Close().
     - Protect |this| around invoking plugin callbacks
     - Add unit tests to check the case
    
    BUG=
    TEST=browser_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10661026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144872 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_utils.cc
+++ ppapi/tests/test_utils.cc
@@ -99,7 +99,8 @@ TestCompletionCallback::TestCompletionCallback(PP_Instance instance)
       callback_type_(PP_OPTIONAL),
       post_quit_task_(false),
       run_count_(0),  // TODO(dmichael): Remove when all tests are updated.
-      instance_(instance) {
+      instance_(instance),
+      delegate_(NULL) {
 }
 
 TestCompletionCallback::TestCompletionCallback(PP_Instance instance,
@@ -109,7 +110,8 @@ TestCompletionCallback::TestCompletionCallback(PP_Instance instance,
       result_(PP_OK_COMPLETIONPENDING),
       callback_type_(force_async ? PP_REQUIRED : PP_OPTIONAL),
       post_quit_task_(false),
-      instance_(instance) {
+      instance_(instance),
+      delegate_(NULL) {
 }
 
 TestCompletionCallback::TestCompletionCallback(PP_Instance instance,
@@ -119,7 +121,8 @@ TestCompletionCallback::TestCompletionCallback(PP_Instance instance,
       result_(PP_OK_COMPLETIONPENDING),
       callback_type_(callback_type),
       post_quit_task_(false),
-      instance_(instance) {
+      instance_(instance),
+      delegate_(NULL) {
 }
 
 int32_t TestCompletionCallback::WaitForResult() {
@@ -201,6 +204,7 @@ void TestCompletionCallback::Reset() {
   have_result_ = false;
   post_quit_task_ = false;
   run_count_ = 0;  // TODO(dmichael): Remove when all tests are updated.
+  delegate_ = NULL;
   errors_.clear();
 }
 
@@ -214,6 +218,8 @@ void TestCompletionCallback::Handler(void* user_data, int32_t result) {
   callback->result_ = result;
   callback->have_result_ = true;
   callback->run_count_++;  // TODO(dmichael): Remove when all tests are updated.
+  if (callback->delegate_)
+    callback->delegate_->OnCallback(user_data, result);
   if (callback->post_quit_task_) {
     callback->post_quit_task_ = false;
     GetTestingInterface()->QuitMessageLoop(callback->instance_);
