180fbf17e53bab8ba683910d256a1f6222fe8497
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-29 22:10:43 +0000

    [Chromium] WebFontRendering.cpp requires some OS(ANDROID) ifdefs to build downstream https://bugs.webkit.org/show_bug.cgi?id=90292
    Reviewed by Nate Chapin.
    
    These ifdefs are required to build this file downstream. There's some
    sublte difference between how the OS flags are set upstream and
    downstream. This is on our list of issues to resolve, but in the
    meantime, this patch makes these files identical upstream and
    downstream to reduce noise in the upstreaming queue.
    
    * src/linux/WebFontRendering.cpp:
    (WebKit::WebFontRendering::setSubpixelPositioning):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/linux/WebFontRendering.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/linux/WebFontRendering.cpp
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "FontPlatformData.h"
 
-#if OS(LINUX)
+#if OS(LINUX) && !OS(ANDROID)
 #include "WebFontInfo.h"
 #endif
 
@@ -76,7 +76,7 @@ void WebFontRendering::setSubpixelRendering(bool useSubpixelRendering)
 void WebFontRendering::setSubpixelPositioning(bool useSubpixelPositioning)
 {
     FontPlatformData::setSubpixelPositioning(useSubpixelPositioning);
-#if OS(LINUX)
+#if OS(LINUX) && !OS(ANDROID)
     WebFontInfo::setSubpixelPositioning(useSubpixelPositioning);
 #endif
 }
