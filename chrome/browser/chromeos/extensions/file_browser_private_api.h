9b533b4fadfffbb740ad9f41f15430aecda34f25
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-29 05:32:48 +0000

    gdata: Get rid of GDataFileSystem::GetCacheState()
    Instead, add GDataCache::GetCacheEntryOnUIThread().
    
    BUG=133552
    TEST=the existing testis covered this functionality
    
    Review URL: https://chromiumcodereview.appspot.com/10690028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144878 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_private_api.h
+++ chrome/browser/chromeos/extensions/file_browser_private_api.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/platform_file.h"
 #include "chrome/browser/chromeos/extensions/file_browser_event_router.h"
+#include "chrome/browser/chromeos/gdata/gdata_cache.h"
 #include "chrome/browser/extensions/extension_function.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "googleurl/src/url_util.h"
@@ -451,8 +452,8 @@ class GetGDataFilePropertiesFunction : public FileBrowserFunction {
                      scoped_ptr<gdata::GDataFileProto> file_proto);
 
   void CacheStateReceived(base::DictionaryValue* property_dict,
-                          base::PlatformFileError error,
-                          int cache_state);
+                          bool success,
+                          const gdata::GDataCache::CacheEntry& cache_entry);
 
   size_t current_index_;
   base::ListValue* path_list_;
