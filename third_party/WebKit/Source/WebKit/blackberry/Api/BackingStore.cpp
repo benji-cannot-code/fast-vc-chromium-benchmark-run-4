5efe1a33b37aa3045a76ab05ab78b66303e3e057
jpetsovits@rim.com <jpetsovits@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-29 18:27:21 +0000

    Add blitVisibleContents() as public API. https://bugs.webkit.org/show_bug.cgi?id=90211
    Reviewed by Adam Treat.
    
    We keep blitContents() (with src/dst rectangles)
    for compatibility with older Cascades sprints for now,
    but want to switch to always blitting the full viewport
    and this is a good first step.
    
    * Api/BackingStore.cpp:
    (BlackBerry::WebKit::BackingStorePrivate::blitVisibleContents):
    (BlackBerry::WebKit::BackingStore::blitVisibleContents):
    (WebKit):
    * Api/BackingStore.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009, 2010, 2011 Research In Motion Limited. All rights reserved.
+ * Copyright (C) 2009, 2010, 2011, 2012 Research In Motion Limited. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -1160,8 +1160,11 @@ void BackingStorePrivate::blitVisibleContents(bool force)
 {
     // Blitting must never happen for direct rendering case.
     ASSERT(!shouldDirectRenderingToWindow());
-    if (shouldDirectRenderingToWindow())
+    if (shouldDirectRenderingToWindow()) {
+        BlackBerry::Platform::logAlways(BlackBerry::Platform::LogLevelCritical,
+            "BackingStore::blitVisibleContents operation not supported in direct rendering mode");
         return;
+    }
 
     if (m_suspendScreenUpdates) {
         // Avoid client going into busy loop while updates suspended.
@@ -2809,6 +2812,11 @@ bool BackingStore::isScrollingOrZooming() const
 void BackingStore::setScrollingOrZooming(bool scrollingOrZooming)
 {
     d->setScrollingOrZooming(scrollingOrZooming);
+}
+
+void BackingStore::blitVisibleContents()
+{
+    d->blitVisibleContents(false /*force*/);
 }
 
 void BackingStore::blitContents(const BlackBerry::Platform::IntRect& dstRect, const BlackBerry::Platform::IntRect& contents)
