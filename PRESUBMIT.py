f9a500535438149142e3115dcbe733878ef7abaa
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-06-29 00:09:03 +0000

    Change PRESUBMIT to try more cls on android
    - android doesn't currently build anything in chrome/
      so we're changing try job logic to send builds to android
      unless the CL was exclusively in chrome/
    
    Previously we would have built on roughly 20% of cls with 'git try'
    After this change we will build on roughly 60% of cls (based on last 10k
    commits)
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10694029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@144841 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -481,6 +481,9 @@ def CheckChangeOnCommit(input_api, output_api):
 def GetPreferredTrySlaves(project, change):
   files = change.LocalPaths()
 
+  if not files:
+    return []
+
   if all(re.search('\.(m|mm)$|[/_]mac[/_.]', f) for f in files):
     return ['mac_rel']
   if all(re.search('[/_]win[/_.]', f) for f in files):
@@ -493,9 +496,7 @@ def GetPreferredTrySlaves(project, change):
   if any(re.search('[/_]aura', f) for f in files):
     trybots.append('linux_chromeos')
 
-  # Ensure CL contains some relevant files (i.e. not purely ^chrome)
-  if any(re.search('^(base|build|content|ipc|jingle|media|net|sql)/', f)
-         for f in files):
+  if not all(f.startswith('chrome/') for f in files):
     trybots.append('android')
 
   return trybots
