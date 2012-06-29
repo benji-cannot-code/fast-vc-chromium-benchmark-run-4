3c4c26a480d40f5c8ca0ffb3f7702048e5e8e50f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-06-29 21:57:24 +0000

    Source/WebCore: Web Inspector: Add data length to resource events on timeline to keep track of the amount of data loaded and the total data length https://bugs.webkit.org/show_bug.cgi?id=89244
    Patch by Hanna Ma <Hanma@rim.com> on 2012-06-29
    Reviewed by Pavel Feldman.
    
    Added data length to inspector timeline popup
    content for resources to keep track of the amount of data loaded.
    Tests: inspector/timeline/timeline-network-received-data.html
    
    * English.lproj/localizedStrings.js:
    * inspector/InspectorInstrumentation.cpp:
    (WebCore::InspectorInstrumentation::willReceiveResourceDataImpl):
    * inspector/InspectorInstrumentation.h:
    (InspectorInstrumentation):
    (WebCore::InspectorInstrumentation::willReceiveResourceData):
    * inspector/InspectorTimelineAgent.cpp:
    (WebCore::InspectorTimelineAgent::willReceiveResourceData):
    * inspector/InspectorTimelineAgent.h:
    (InspectorTimelineAgent):
    * inspector/TimelineRecordFactory.cpp:
    (WebCore::TimelineRecordFactory::createReceiveResourceData):
    * inspector/TimelineRecordFactory.h:
    (TimelineRecordFactory):
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._showPopover):
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel.prototype.reset):
    (WebInspector.TimelinePresentationModel.Record):
    (WebInspector.TimelinePresentationModel.Record.prototype.generatePopupContent):
    * loader/ResourceLoader.cpp:
    (WebCore::ResourceLoader::didReceiveData):
    * inspector/timeline/timeline-network-received-data.html: Added.
    * inspector/timeline/timeline-network-received-data-expected.txt: Added.
    * inspector/timeline/timeline-test.js:
    
    LayoutTests: Web Inspector: Add data length to resource events on timeline to
    keep track of the amount of data loaded and the total data length.
    https://bugs.webkit.org/show_bug.cgi?id=89244
    
    Patch by Hanna Ma <Hanma@rim.com> on 2012-06-29
    Reviewed by Pavel Feldman.
    
    Added tests for resource receive data events.
    
    * inspector/timeline/timeline-network-received-data-expected.txt: Added.
    * inspector/timeline/timeline-network-received-data.html: Added.
    * inspector/timeline/timeline-test.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121592 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/inspector/timeline/timeline-test.js
+++ third_party/WebKit/LayoutTests/inspector/timeline/timeline-test.js
@@ -20,7 +20,8 @@ InspectorTest.timelinePropertyFormatters = {
     timerId: "formatAsTypeName",
     scriptLine: "formatAsTypeName",
     lineNumber: "formatAsTypeName",
-    frameId: "formatAsTypeName"
+    frameId: "formatAsTypeName",
+    encodedDataLength: "formatAsTypeName"
 };
 
 InspectorTest.startTimeline = function(callback)
