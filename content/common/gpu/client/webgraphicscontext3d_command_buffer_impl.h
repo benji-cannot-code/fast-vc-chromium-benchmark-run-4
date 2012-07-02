f6fc3959a8befeac2b72ad749ba2f6646ad86d6f
alexst@chromium.org <alexst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-02 20:18:10 +0000

    Adding necessary plumbing for bindUniformLocationCHROMIUM extension.
    BUG=133438
    TEST=unit tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10708002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145180 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
+++ content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
@@ -571,6 +571,9 @@ class WebGraphicsContext3DCommandBufferImpl
                                    WebGLId dest_id, WGC3Dint level,
                                    WGC3Denum internal_format);
 
+  virtual void bindUniformLocationCHROMIUM(WebGLId program, WGC3Dint location,
+                                           const WGC3Dchar* uniform);
+
  protected:
   virtual GrGLInterface* onCreateGrGLInterface();
 
