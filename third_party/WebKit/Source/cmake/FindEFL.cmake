2b69d63bd0bae5084c0ab9a1fbb5f42aa0f90bba
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-02 06:22:14 +0000

    [EFL] Add Gamepad support https://bugs.webkit.org/show_bug.cgi?id=90170
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-01
    Reviewed by Kenneth Rohde Christiansen.
    
    .:
    
    * Source/cmake/FindEFL.cmake: Bump EFL libs dependencies.
    * Source/cmake/OptionsEfl.cmake: Turn on GAMEPAD flag on EFL port.
    * Source/cmakeconfig.h.cmake: Add GAMEPAD flag to CMake.
    
    Source/WebCore:
    
    Add support for the Gamepad feature on the EFL port.
    
    The implementation of this class relies on the Linux
    kernel joystick API.
    
    Gamepad devices are recognized through the GamepadsEfl
    class, of which implementation is based on Eeze
    library. This way devices are properly registered on
    connection as objects of the GamepadDeviceEfl class
    which inherits GamepadDeviceLinux. GamepadDeviceEfl
    reads the joystick data through an Ecore_Fd_Handler
    and updates the device state accordingly. The
    GamepadsEfl object is then polled for gamepads data
    through the sampleGamepads method.
    
    No new tests - already tested by gamepad/*
    
    * CMakeLists.txt:
    * PlatformEfl.cmake:
    * platform/efl/GamepadsEfl.cpp: Added.
    (WebCore):
    (GamepadDeviceEfl):
    (WebCore::GamepadDeviceEfl::create):
    (WebCore::GamepadDeviceEfl::GamepadDeviceEfl):
    (WebCore::GamepadDeviceEfl::~GamepadDeviceEfl):
    (WebCore::GamepadDeviceEfl::readCallback):
    (GamepadsEfl):
    (WebCore::GamepadsEfl::onGamePadChange):
    (WebCore::GamepadsEfl::GamepadsEfl):
    (WebCore::GamepadsEfl::~GamepadsEfl):
    (WebCore::GamepadsEfl::registerDevice):
    (WebCore::GamepadsEfl::unregisterDevice):
    (WebCore::GamepadsEfl::updateGamepadList):
    (WebCore::sampleGamepads):
    
    Tools:
    
    * Scripts/webkitperl/FeatureList.pm: Turn on GAMEPAD flag
    by default for EFL port.
    * efl/jhbuild.modules: Bump dependency versions of EFL libs
    since the latest Eeze is required for gamepad support.
    
    LayoutTests:
    
    Unskip gamepad/gamepad-api.html now that Gamepad
    support is enabled on EFL port.
    
    gamepad/gamepad-polling-access.html is still
    skipped because it requires additional test
    infrastructure.
    
    * platform/efl/Skipped:
    * platform/efl/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmake/FindEFL.cmake
+++ third_party/WebKit/Source/cmake/FindEFL.cmake
@@ -2,20 +2,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 INCLUDE(FindPkgConfig)
 
 PKG_CHECK_MODULES (EDJE REQUIRED edje>=1.0.0)
-PKG_CHECK_MODULES (ECORE REQUIRED ecore>=1.0.0)
+PKG_CHECK_MODULES (ECORE REQUIRED ecore>=1.2.0)
 PKG_CHECK_MODULES (ECORE_EVAS REQUIRED ecore-evas>=1.0.0)
 PKG_CHECK_MODULES (EFLDEPS REQUIRED
-  eina>=1.0.999.63568
+  eina>=1.2.0
   evas>=1.0.0
-  ecore>=1.0.999.59763
+  ecore>=1.2.0
   ecore-file>=1.0.0
   ecore-evas>=1.0.999.59763
   edje>=1.0.0
   eukit>=1.1.0
   edbus>=1.1.0
   ecore-input>=1.0.0
-  eeze>=1.1.99)
-PKG_CHECK_MODULES (EINA REQUIRED eina>=1.0.0)
+  eeze>=1.2.99)
+PKG_CHECK_MODULES (EINA REQUIRED eina>=1.2.0)
 PKG_CHECK_MODULES (ECORE_X ecore-x>=1.0.0)
 PKG_CHECK_MODULES (EVAS REQUIRED evas>=1.0.0)
 PKG_CHECK_MODULES (EUKIT REQUIRED eukit>=1.1.0)
