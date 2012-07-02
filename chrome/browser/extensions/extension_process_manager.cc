9a6500d947a4f8ec7788af7dd76812f9e0e05cda
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-02 21:11:05 +0000

    One possible fix.
    BUG=133296
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10703036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.cc
+++ chrome/browser/extensions/extension_process_manager.cc
@@ -488,7 +488,6 @@ void ExtensionProcessManager::OnShouldUnloadAck(
   ExtensionHost* host = GetBackgroundHostForExtension(extension_id);
   if (host &&
       sequence_id == background_page_data_[extension_id].close_sequence_id) {
-    background_page_data_[extension_id].is_closing = true;
     host->render_view_host()->Send(new ExtensionMsg_Unload(extension_id));
   }
 }
@@ -503,6 +502,7 @@ void ExtensionProcessManager::OnUnloadAck(const std::string& extension_id) {
 
 void ExtensionProcessManager::CloseLazyBackgroundPageNow(
     const std::string& extension_id) {
+  background_page_data_[extension_id].is_closing = true;
   ExtensionHost* host = GetBackgroundHostForExtension(extension_id);
   if (host)
     CloseBackgroundHost(host);
