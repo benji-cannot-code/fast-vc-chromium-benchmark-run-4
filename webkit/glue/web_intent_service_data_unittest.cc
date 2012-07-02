18e578bcd914fc2f5c6689512ac00a3e6f39897a
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-02 03:17:23 +0000

    Fix compile errors on gcc 4.6. This changes prevents calling constructors webkit_glue::WebIntentServiceData::WebIntentServiceData() directly since it is prohibited by gcc 4.6 (-fpermissive).
    BUG=none
    TEST=build on gcc 4.6.
    Review URL: https://chromiumcodereview.appspot.com/10690036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145118 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/web_intent_service_data_unittest.cc
+++ webkit/glue/web_intent_service_data_unittest.cc
@@ -43,7 +43,7 @@ TEST(WebIntentServiceDataTest, Defaults) {
 TEST(WebIntentServiceDataTest, ActionServicesEqual) {
 
   // with default disposition...
-  WebIntentServiceData intent = WebIntentServiceData::WebIntentServiceData(
+  WebIntentServiceData intent(
       ASCIIToUTF16("http://webintents.org/share"),
       ASCIIToUTF16("image/png"),
       string16(),
@@ -63,7 +63,7 @@ TEST(WebIntentServiceDataTest, ActionServicesEqual) {
 TEST(WebIntentServiceDataTest, SchemeServicesEqual) {
 
   // with default disposition...
-  WebIntentServiceData intent = WebIntentServiceData::WebIntentServiceData(
+  WebIntentServiceData intent(
       string16(),
       string16(),
       ASCIIToUTF16("mailto"),
