ab1f0a92dd8fdc12de14da5afb57aee9a4191049
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 20:23:41 +0000

    Have webkit-patch rebaseline use rebaseline-test-internal https://bugs.webkit.org/show_bug.cgi?id=90491
    Reviewed by Dirk Pranke.
    
    This lets it handle new files, reduces duplicate code and lays the
    groundwork for making rebaseline have a richer interface.
    
    * Scripts/webkitpy/common/net/buildbot/buildbot_mock.py:
    (MockBuild):
    (MockBuild.__init__):
    (MockBuilder.build):
    * Scripts/webkitpy/tool/commands/rebaseline.py:
    (AbstractParallelRebaselineCommand._files_to_add):
    (Rebaseline):
    (Rebaseline._builder_to_pull_from):
    (Rebaseline._tests_to_update):
    (Rebaseline.execute):
    * Scripts/webkitpy/tool/commands/rebaseline_unittest.py:
    (test_overrides_are_included_correctly):
    (test_rebaseline):
    (test_rebaseline.mock_builder_to_pull_from):
    (test_rebaseline.mock_tests_to_update):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/net/buildbot/buildbot_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/net/buildbot/buildbot_mock.py
@@ -28,12 +28,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
+class MockBuild(object):
+    def __init__(self, build_number, revision, is_green):
+        self._number = build_number
+        self._revision = revision
+        self._is_green = is_green
+
 class MockBuilder(object):
     def __init__(self, name):
         self._name = name
 
     def name(self):
         return self._name
+
+    def build(self, build_number):
+        return MockBuild(build_number=build_number, revision=1234, is_green=False)
 
     def results_url(self):
         return "http://example.com/builders/%s/results" % self.name()
