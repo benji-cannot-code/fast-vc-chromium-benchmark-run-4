f1ea471b26af541b6e0f850822cf0f5ad66fa519
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 16:15:24 +0000

    -Wunused-private-field cleanup, linux edition https://bugs.webkit.org/show_bug.cgi?id=90463
    Reviewed by Stephen White.
    
    * DumpRenderTree/chromium/DRTDevToolsAgent.h:
    * DumpRenderTree/chromium/GamepadController.h:
    (GamepadController):
    * DumpRenderTree/chromium/NotificationPresenter.h:
    (NotificationPresenter::NotificationPresenter):
    (NotificationPresenter):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/GamepadController.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/GamepadController.cpp
@@ -36,8 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebKit;
 
-GamepadController::GamepadController(TestShell* shell)
-    : m_shell(shell)
+GamepadController::GamepadController(TestShell*)
 {
     bindMethod("connect", &GamepadController::connect);
     bindMethod("disconnect", &GamepadController::disconnect);
--- third_party/WebKit/Tools/DumpRenderTree/chromium/NotificationPresenter.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/NotificationPresenter.h
@@ -44,7 +44,7 @@ class TestShell;
 // A class that implements WebNotificationPresenter for DRT.
 class NotificationPresenter : public WebKit::WebNotificationPresenter {
 public:
-    explicit NotificationPresenter(TestShell* shell) : m_shell(shell) { }
+    explicit NotificationPresenter(TestShell*) { }
     virtual ~NotificationPresenter();
 
     // Called by the LayoutTestController to simulate a user granting permission.
@@ -63,9 +63,6 @@ public:
     void reset() { m_allowedOrigins.clear(); }
 
 private:
-    // Non-owned pointer. The NotificationPresenter is owned by the test shell.
-    TestShell* m_shell;
-
     // Set of allowed origins.
     HashSet<WTF::String> m_allowedOrigins;
 
