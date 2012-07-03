08ac4637253ae9c5a1eb64baa81902a8a92eb43e
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 09:50:42 +0000

    Reduce log spam from things which use Deterministic/OrderedSocketData
    When running net_unittests, don't DLOG all of the state machine transitions.
    It is really only meant for local testing.
    
    BUG=none
    TEST=smaller build logs
    
    
    Review URL: https://chromiumcodereview.appspot.com/10694044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145278 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/socket_test_util.cc
+++ net/socket/socket_test_util.cc
@@ -26,7 +26,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/socket.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-#define NET_TRACE(level, s)   DLOG(level) << s << __FUNCTION__ << "() "
+// Socket events are easier to debug if you log individual reads and writes.
+// Enable these if locally debugging, but they are too noisy for the waterfall.
+#if 0
+#define NET_TRACE(level, s) DLOG(level) << s << __FUNCTION__ << "() "
+#else
+#define NET_TRACE(level, s) EAT_STREAM_PARAMETERS
+#endif
 
 namespace net {
 
