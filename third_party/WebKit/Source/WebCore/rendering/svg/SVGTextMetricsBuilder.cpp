15fc40c583152be5d6fa10a23996eb1f5f3aca10
pdr@google.com <pdr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 10:29:08 +0000

    Fix text positioning with non-bmp characters. https://bugs.webkit.org/show_bug.cgi?id=87681
    Reviewed by Nikolas Zimmermann.
    
    Source/WebCore:
    
    Previously when constructing metrics for tspans with non-bmp characters,
    each non-bmp character treated as a skipped character in the same way that
    spaces are ignored.
    This made sense because the initial SVGCharacterDataMap for <text> is
    indexed by character index (not string length) so the high portion of a
    non-bmp character was treated as a skipped space. Unfortunately, this
    led to a bug because skipped spaces lead to an offset in the positioning
    values list but non-bmp characters do not.
    
    This change switches the code to use a new offset for non-bmp characters,
    surrogatePairCharacters, which does not affect the positioning values list.
    
    Tests: svg/text/non-bmp-tspans-expected.svg
           svg/text/non-bmp-tspans.svg
    
    * rendering/svg/SVGTextMetricsBuilder.cpp:
    (WebCore::SVGTextMetricsBuilder::measureTextRenderer):
    
    LayoutTests:
    
    * svg/text/non-bmp-tspans-expected.svg: Added.
    * svg/text/non-bmp-tspans.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/SVGTextMetricsBuilder.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/SVGTextMetricsBuilder.cpp
@@ -156,6 +156,7 @@ void SVGTextMetricsBuilder::measureTextRenderer(RenderSVGInlineText* text, Measu
 
     initializeMeasurementWithTextRenderer(text);
     bool preserveWhiteSpace = text->style()->whiteSpace() == PRE;
+    int surrogatePairCharacters = 0;
 
     while (advance()) {
         const UChar* currentCharacter = m_run.data(m_textPosition);
@@ -169,7 +170,7 @@ void SVGTextMetricsBuilder::measureTextRenderer(RenderSVGInlineText* text, Measu
 
         if (data->processRenderer) {
             if (data->allCharactersMap) {
-                const SVGCharacterDataMap::const_iterator it = data->allCharactersMap->find(data->valueListPosition + m_textPosition - data->skippedCharacters + 1);
+                const SVGCharacterDataMap::const_iterator it = data->allCharactersMap->find(data->valueListPosition + m_textPosition - data->skippedCharacters - surrogatePairCharacters + 1);
                 if (it != data->allCharactersMap->end())
                     attributes->characterDataMap().set(m_textPosition + 1, it->second);
             }
@@ -177,7 +178,7 @@ void SVGTextMetricsBuilder::measureTextRenderer(RenderSVGInlineText* text, Measu
         }
 
         if (data->allCharactersMap && currentCharacterStartsSurrogatePair())
-            data->skippedCharacters += m_currentMetrics.length() - 1;
+            surrogatePairCharacters++;
 
         data->lastCharacter = currentCharacter;
     }
