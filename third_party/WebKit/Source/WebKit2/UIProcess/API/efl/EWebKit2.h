3ddd46f65588575ff85067c0b75d52fbf4d119e7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 06:27:10 +0000

    [EFL][WK2] Add API to inspect a Web Intent service https://bugs.webkit.org/show_bug.cgi?id=90066
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-02
    Reviewed by Kenneth Rohde Christiansen.
    
    Add EFL API to inspect a Web Intent Service and emit
    a signal on the view when a new intent service
    registers.
    
    * PlatformEfl.cmake:
    * UIProcess/API/efl/EWebKit2.h:
    * UIProcess/API/efl/ewk_intent_service.cpp: Added.
    (_Ewk_Intent_Service):
    (ewk_intent_service_ref):
    (ewk_intent_service_unref):
    (ewk_intent_service_action_get):
    (ewk_intent_service_type_get):
    (ewk_intent_service_href_get):
    (ewk_intent_service_title_get):
    (ewk_intent_service_disposition_get):
    (ewk_intent_service_new):
    * UIProcess/API/efl/ewk_intent_service.h: Added.
    * UIProcess/API/efl/ewk_intent_service_private.h: Copied from Source/WebKit2/UIProcess/API/efl/ewk_view_loader_client.cpp.
    * UIProcess/API/efl/ewk_view.cpp:
    (ewk_view_intent_service_register):
    * UIProcess/API/efl/ewk_view.h:
    * UIProcess/API/efl/ewk_view_loader_client.cpp:
    (registerIntentServiceForFrame):
    (ewk_view_loader_client_attach):
    * UIProcess/API/efl/ewk_view_private.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EWebKit2.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EWebKit2.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ewk_context.h"
 #include "ewk_intent.h"
+#include "ewk_intent_service.h"
 #include "ewk_view.h"
 
 #endif // EWebKit2_h
