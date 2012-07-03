05b1f9a774b442e15fd01aca70fd4bb21cbb308d
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 10:24:23 +0000

    Update dynamic_annotations to r4384.
    TBR=darin,timurrrr
    Review URL: https://chromiumcodereview.appspot.com/10696089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145281 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/dynamic_annotations/dynamic_annotations.c
+++ base/third_party/dynamic_annotations/dynamic_annotations.c
@@ -43,10 +43,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # include "base/third_party/valgrind/valgrind.h"
 #endif
 
+/* Compiler-based ThreadSanitizer defines
+   DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL = 1
+   and provides its own definitions of the functions. */
+
+#ifndef DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL
+# define DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL 0
+#endif
+
 /* Each function is empty and called (via a macro) only in debug mode.
    The arguments are captured by dynamic tools at runtime. */
 
-#if DYNAMIC_ANNOTATIONS_ENABLED == 1
+#if DYNAMIC_ANNOTATIONS_ENABLED == 1 \
+    && DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL == 0
 
 /* Identical code folding(-Wl,--icf=all) countermeasures.
    This makes all Annotate* functions different, which prevents the linker from
@@ -215,9 +224,11 @@ void DYNAMIC_ANNOTATIONS_NAME(AnnotateFlushState)(
     const char *file, int line)
 {DYNAMIC_ANNOTATIONS_IMPL}
 
-#endif  /* DYNAMIC_ANNOTATIONS_ENABLED == 1 */
+#endif  /* DYNAMIC_ANNOTATIONS_ENABLED == 1
+    && DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL == 0 */
 
-#if DYNAMIC_ANNOTATIONS_PROVIDE_RUNNING_ON_VALGRIND == 1
+#if DYNAMIC_ANNOTATIONS_PROVIDE_RUNNING_ON_VALGRIND == 1 \
+    && DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL == 0
 static int GetRunningOnValgrind(void) {
 #ifdef RUNNING_ON_VALGRIND
   if (RUNNING_ON_VALGRIND) return 1;
@@ -255,4 +266,5 @@ int RunningOnValgrind(void) {
   return local_running_on_valgrind;
 }
 
-#endif /* DYNAMIC_ANNOTATIONS_PROVIDE_RUNNING_ON_VALGRIND == 1 */
+#endif /* DYNAMIC_ANNOTATIONS_PROVIDE_RUNNING_ON_VALGRIND == 1
+    && DYNAMIC_ANNOTATIONS_EXTERNAL_IMPL == 0 */
