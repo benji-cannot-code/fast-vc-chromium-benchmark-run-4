benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 253f2fcfa38f6ed8a4c3eed7a7822e7f0be7deed

Don't hardcode target dpi of 160 (it should be 96 on desktop) https://bugs.webkit.org/show_bug.cgi?id=88114
Patch by Konrad Piascik <kpiascik@rim.com> on 2012-06-29
Reviewed by Adam Barth.

Source/WebCore:

No behavioural change, current tests in fast/viewport cover all
functionality.

* WebCore.exp.in: Updated symbol for computeViewportAttributes.
* dom/ViewportArguments.cpp: Use new parameter for devicePixelRatio
                             and don't calculate it anymore.
(WebCore::computeViewportAttributes):
* dom/ViewportArguments.h: Change the deviceDPI parameter to
                           devicePixelRatio and put the onus
                           on the embedder to supply the
                           correct value.  Add temporary constant.
(WebCore):

Source/WebKit/blackberry:

Added new WebSetting to specify what the devicePixelRatio should be.
Updated the call to computeViewportAttributes.

* Api/WebPage.cpp:
(BlackBerry::WebKit::WebPagePrivate::recomputeVirtualViewportFromViewportArguments):
* Api/WebSettings.cpp:
(WebKit):
(BlackBerry::WebKit::WebSettings::standardSettings):
(BlackBerry::WebKit::WebSettings::devicePixelRatio):
(BlackBerry::WebKit::WebSettings::setDevicePixelRatio):
* Api/WebSettings.h:
* WebKitSupport/DumpRenderTreeSupport.cpp:
(DumpRenderTreeSupport::dumpConfigurationForViewport):

Source/WebKit/chromium:

Updated the call to computeViewportAttributes.

* src/ChromeClientImpl.cpp:
(WebKit::ChromeClientImpl::dispatchViewportPropertiesDidChange):

Source/WebKit/efl:

Updated the call to computeViewportAttributes.

* WebCoreSupport/DumpRenderTreeSupportEfl.cpp:
(DumpRenderTreeSupportEfl::dumpConfigurationForViewport):
* ewk/ewk_view.cpp:
(_ewk_view_viewport_attributes_compute):

Source/WebKit/gtk:

Updated the call to computeViewportAttributes.

* WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
(DumpRenderTreeSupportGtk::dumpConfigurationForViewport):
* webkit/webkitviewportattributes.cpp:
(webkitViewportAttributesRecompute):

third_party/WebKit/Source/WebCore/WebCore.exp.in
@@ -607,7 +607,7 @@ __ZN7WebCore25HistoryPropertyListWriter16writeHistoryItemERNS_30BinaryPropertyLi
 __ZN7WebCore25HistoryPropertyListWriter6bufferEm
 __ZN7WebCore25HistoryPropertyListWriterC2Ev
 __ZN7WebCore25addLanguageChangeObserverEPvPFvS0_E
-__ZN7WebCore25computeViewportAttributesENS_17ViewportArgumentsEiiiiNS_7IntSizeE
+__ZN7WebCore25computeViewportAttributesENS_17ViewportArgumentsEiiifNS_7IntSizeE
 __ZN7WebCore26UserTypingGestureIndicator27processingUserTypingGestureEv
 __ZN7WebCore26UserTypingGestureIndicator28focusedElementAtGestureStartEv
 __ZN7WebCore26stopObservingCookieChangesEv

third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -919,7 +919,7 @@ void WebPage::sendViewportAttributesChanged()
     int deviceWidth = (settings->deviceWidth() > 0) ? settings->deviceWidth() : m_viewportSize.width();
     int deviceHeight = (settings->deviceHeight() > 0) ? settings->deviceHeight() : m_viewportSize.height();
 
-    ViewportAttributes attr = computeViewportAttributes(m_page->viewportArguments(), minimumLayoutFallbackWidth, deviceWidth, deviceHeight, static_cast<int>(160 * m_page->deviceScaleFactor()), m_viewportSize);
+    ViewportAttributes attr = computeViewportAttributes(m_page->viewportArguments(), minimumLayoutFallbackWidth, deviceWidth, deviceHeight, m_page->deviceScaleFactor(), m_viewportSize);
 
     setResizesToContentsUsingLayoutSize(IntSize(static_cast<int>(attr.layoutSize.width()), static_cast<int>(attr.layoutSize.height())));
     send(Messages::WebPageProxy::DidChangeViewportProperties(attr));
@@ -3123,7 +3123,7 @@ void WebPage::simulateMouseMotion(WebCore::IntPoint position, double time)
 String WebPage::viewportConfigurationAsText(int deviceDPI, int deviceWidth, int deviceHeight, int availableWidth, int availableHeight)
 {
     ViewportArguments arguments = mainFrame()->document()->viewportArguments();
-    ViewportAttributes attrs = WebCore::computeViewportAttributes(arguments, /* default layout width for non-mobile pages */ 980, deviceWidth, deviceHeight, deviceDPI, IntSize(availableWidth, availableHeight));
+    ViewportAttributes attrs = WebCore::computeViewportAttributes(arguments, /* default layout width for non-mobile pages */ 980, deviceWidth, deviceHeight, m_page->deviceScaleFactor(), IntSize(availableWidth, availableHeight));
     WebCore::restrictMinimumScaleFactorToViewportSize(attrs, IntSize(availableWidth, availableHeight));
     WebCore::restrictScaleFactorToInitialScaleIfNotUserScalable(attrs);
     return String::format("viewport size %dx%d scale %f with limits [%f, %f] and userScalable %f\n", static_cast<int>(attrs.layoutSize.width()), static_cast<int>(attrs.layoutSize.height()), attrs.initialScale, attrs.minimumScale, attrs.maximumScale, attrs.userScalable);

third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -3445,9 +3445,7 @@ IntSize WebPagePrivate::recomputeVirtualViewportFromViewportArguments()
     int desktopWidth = defaultMaxLayoutSize().width();
     int deviceWidth = Platform::Graphics::Screen::primaryScreen()->width();
     int deviceHeight = Platform::Graphics::Screen::primaryScreen()->height();
-    FloatSize currentPPI = Platform::Graphics::Screen::primaryScreen()->pixelsPerInch(-1);
-    int deviceDPI = int(roundf((currentPPI.width() + currentPPI.height()) / 2));
-    ViewportAttributes result = computeViewportAttributes(m_viewportArguments, desktopWidth, deviceWidth, deviceHeight, deviceDPI, m_defaultLayoutSize);
+    ViewportAttributes result = computeViewportAttributes(m_viewportArguments, desktopWidth, deviceWidth, deviceHeight, m_webSettings->devicePixelRatio(), m_defaultLayoutSize);
     m_page->setDeviceScaleFactor(result.devicePixelRatio);
     return IntSize(result.layoutSize.width(), result.layoutSize.height());
 }

third_party/WebKit/Source/WebCore/dom/ViewportArguments.cpp
@@ -42,11 +42,7 @@ using namespace std;
 
 namespace WebCore {
 
-// FIXME: We shouldn't hardcode the targetDPI to 160.
-// See https://bugs.webkit.org/show_bug.cgi?id=88114
-static float targetDPI = 160;
-
-ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktopWidth, int deviceWidth, int deviceHeight, int deviceDPI, IntSize visibleViewport)
+ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktopWidth, int deviceWidth, int deviceHeight, float devicePixelRatio, IntSize visibleViewport)
 {
     ViewportAttributes result;
 
@@ -55,7 +51,7 @@ ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktop
 
     ASSERT(availableWidth > 0 && availableHeight > 0);
 
-    result.devicePixelRatio = deviceDPI / targetDPI;
+    result.devicePixelRatio = devicePixelRatio;
 
     // Resolve non-'auto' width and height to pixel values.
     if (result.devicePixelRatio != 1.0) {

third_party/WebKit/Source/WebCore/dom/ViewportArguments.h
@@ -73,10 +73,6 @@ struct ViewportArguments {
         ValueDesktopWidth = -2,
         ValueDeviceWidth = -3,
         ValueDeviceHeight = -4,
-        ValueDeviceDPI = -5,
-        ValueLowDPI = -6,
-        ValueMediumDPI = -7,
-        ValueHighDPI = -8
     };
 
     ViewportArguments(Type type = Implicit)
@@ -113,9 +109,13 @@ struct ViewportArguments {
     {
         return !(*this == other);
     }
+    // FIXME: We're going to keep this constant around until all embedders
+    // refactor their code to no longer need it.
+    static const int deprecatedTargetDPI = 160;
 };
 
-ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktopWidth, int deviceWidth, int deviceHeight, int deviceDPI, IntSize visibleViewport);
+ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktopWidth, int deviceWidth, int deviceHeight, float devicePixelRatio, IntSize visibleViewport);
+
 void restrictMinimumScaleFactorToViewportSize(ViewportAttributes& result, IntSize visibleViewport);
 void restrictScaleFactorToInitialScaleIfNotUserScalable(ViewportAttributes& result);
 float computeMinimumScaleFactorForContentContained(const ViewportAttributes& result, const IntSize& viewportSize, const IntSize& contentSize);

third_party/WebKit/Source/WebKit/blackberry/Api/WebSettings.cpp
@@ -25,9 +25,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebString.h"
 #include <Base64.h>
+#include <BlackBerryPlatformDeviceInfo.h>
 #include <BlackBerryPlatformFontInfo.h>
+#include <BlackBerryPlatformScreen.h>
 #include <Color.h>
+#include <FloatSize.h>
 #include <PageCache.h>
+#include <ViewportArguments.h>
 #include <wtf/HashSet.h>
 #include <wtf/Vector.h>
 
@@ -59,6 +63,7 @@ DEFINE_STATIC_LOCAL(String, BlackBerryZoomToFitOnLoadEnabled, ("BlackBerryZoomTo
 DEFINE_STATIC_LOCAL(String, BlackBerryFullScreenVideoCapable, ("BlackBerryFullScreenVideoCapable"));
 DEFINE_STATIC_LOCAL(String, BlackBerryCredentialAutofillEnabled, ("BlackBerryCredentialAutofillEnabled"));
 DEFINE_STATIC_LOCAL(String, BlackBerryFormAutofillEnabled, ("BlackBerryFormAutofillEnabled"));
+DEFINE_STATIC_LOCAL(String, BlackBerryDevicePixelRatio, ("BlackBerryDevicePixelRatio"));
 DEFINE_STATIC_LOCAL(String, SpatialNavigationEnabled, ("SpatialNavigationEnabled"));
 DEFINE_STATIC_LOCAL(String, WebKitDatabasePath, ("WebKitDatabasePath"));
 DEFINE_STATIC_LOCAL(String, WebKitDatabasesEnabled, ("WebKitDatabasesEnabled"));
@@ -173,6 +178,13 @@ WebSettings* WebSettings::standardSettings()
     settings->m_private->setBoolean(BlackBerryFullScreenVideoCapable, false);
     settings->m_private->setBoolean(BlackBerryCredentialAutofillEnabled, false);
     settings->m_private->setBoolean(BlackBerryFormAutofillEnabled, false);
+
+    if (BlackBerry::Platform::DeviceInfo::instance()->isMobile()) {
+        WebCore::FloatSize currentPPI = Platform::Graphics::Screen::primaryScreen()->pixelsPerInch(-1);
+        int deviceDPI = int(roundf((currentPPI.width() + currentPPI.height()) / 2));
+        settings->m_private->setDouble(BlackBerryDevicePixelRatio, deviceDPI / WebCore::ViewportArguments::deprecatedTargetDPI);
+    } else
+        settings->m_private->setDouble(BlackBerryDevicePixelRatio, 1);
 
     settings->m_private->setInteger(WebKitDefaultFontSize, 16);
     settings->m_private->setInteger(WebKitDefaultFixedFontSize, 13);
@@ -824,6 +836,16 @@ bool WebSettings::isFormAutofillEnabled() const
 void WebSettings::setFormAutofillEnabled(bool enable)
 {
     return m_private->setBoolean(BlackBerryFormAutofillEnabled, enable);
+}
+
+double WebSettings::devicePixelRatio() const
+{
+    return m_private->getDouble(BlackBerryDevicePixelRatio);
+}
+
+void WebSettings::setDevicePixelRatio(double ratio)
+{
+    m_private->setDouble(BlackBerryDevicePixelRatio, ratio);
 }
 
 } // namespace WebKit

third_party/WebKit/Source/WebKit/blackberry/Api/WebSettings.h
@@ -238,6 +238,9 @@ public:
     bool isFormAutofillEnabled() const;
     void setFormAutofillEnabled(bool);
 
+    double devicePixelRatio() const;
+    void setDevicePixelRatio(double);
+
 private:
     WebSettingsPrivate* m_private;
     WebSettings();

third_party/WebKit/Source/WebKit/blackberry/Api/WebViewportArguments.cpp
@@ -86,12 +86,12 @@ void WebViewportArguments::setHeight(float height)
     d->height = height;
 }
 
-float WebViewportArguments::targetDensityDpi() const
+float WebViewportArguments::devicePixelRatio() const
 {
     return 0;
 }
 
-void WebViewportArguments::setTargetDensityDpi(float)
+void WebViewportArguments::setDevicePixelRatio(float)
 {
 }
 

commit 11840f74fc838d98ff7afc6044612236100a3f76

[Qt][WK2] fast/viewport/viewport-91.html still fails after r121555 and r121661 https://bugs.webkit.org/show_bug.cgi?id=90376
Reviewed by Csaba Osztrogonác.

Source/WebCore:

No new tests, this is only a build alignment.

Export ViewportArguments::deprecatedTargetDPI since it is used in WebKit2.
* WebCore.exp.in:

Source/WebKit2:

layoutTestController.dumpConfigurationForViewport still need
to use the constant target DPI of 160.
* WebProcess/WebPage/WebPage.cpp:
(WebKit::WebPage::viewportConfigurationAsText):

LayoutTests:

Unskip passing test.
* platform/qt-5.0-wk2/Skipped:

git-svn-id: svn://svn.chromium.org/blink/trunk@121740 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/WebCore.exp.in
@@ -457,6 +457,7 @@ __ZN7WebCore17HistoryController33restoreScrollPositionAndViewStateEv
 __ZN7WebCore17JSDOMGlobalObject6s_infoE
 __ZN7WebCore17RegularExpressionC1ERKN3WTF6StringENS1_19TextCaseSensitivityE
 __ZN7WebCore17RegularExpressionD1Ev
+__ZN7WebCore17ViewportArguments19deprecatedTargetDPIE
 __ZN7WebCore17cacheDOMStructureEPNS_17JSDOMGlobalObjectEPN3JSC9StructureEPKNS2_9ClassInfoE
 __ZN7WebCore17openTemporaryFileERKN3WTF6StringERi
 __ZN7WebCore17userVisibleStringEP5NSURL

third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -3123,7 +3123,8 @@ void WebPage::simulateMouseMotion(WebCore::IntPoint position, double time)
 String WebPage::viewportConfigurationAsText(int deviceDPI, int deviceWidth, int deviceHeight, int availableWidth, int availableHeight)
 {
     ViewportArguments arguments = mainFrame()->document()->viewportArguments();
-    ViewportAttributes attrs = WebCore::computeViewportAttributes(arguments, /* default layout width for non-mobile pages */ 980, deviceWidth, deviceHeight, m_page->deviceScaleFactor(), IntSize(availableWidth, availableHeight));
+    ViewportAttributes attrs = WebCore::computeViewportAttributes(arguments, /* default layout width for non-mobile pages */ 980,
+                                                                  deviceWidth, deviceHeight, deviceDPI / ViewportArguments::deprecatedTargetDPI, IntSize(availableWidth, availableHeight));
     WebCore::restrictMinimumScaleFactorToViewportSize(attrs, IntSize(availableWidth, availableHeight));
     WebCore::restrictScaleFactorToInitialScaleIfNotUserScalable(attrs);
     return String::format("viewport size %dx%d scale %f with limits [%f, %f] and userScalable %f\n", static_cast<int>(attrs.layoutSize.width()), static_cast<int>(attrs.layoutSize.height()), attrs.initialScale, attrs.minimumScale, attrs.maximumScale, attrs.userScalable);

third_party/WebKit/LayoutTests/platform/qt-5.0-wk2/Skipped
@@ -393,7 +393,3 @@ http/tests/cache/xhr-vary-header.html
 # https://bugs.webkit.org/show_bug.cgi?id=89473
 editing/pasteboard/copy-in-password-field.html
 editing/pasteboard/copy-element-with-conflicting-background-color-from-rule.html
-
-# [Qt][WK2] fast/viewport/viewport-91.html still fails after r121555 and r121661
-# https://bugs.webkit.org/show_bug.cgi?id=90376
-fast/viewport/viewport-91.html