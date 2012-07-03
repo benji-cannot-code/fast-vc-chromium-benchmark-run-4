33376cd9ddb36d0300e307d555c08b214732664f
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 19:55:17 +0000

    ash: Touch-optimize the window-resize region for touch-events only.
    Both mouse and touch-events are possible in touch-optimized
    UI. But for hit-testing, use touch-optimization only for
    touch/gesture-events, and not for mouse-events.
    
    BUG=135187
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10703069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145359 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/frame_painter.cc
+++ ash/wm/frame_painter.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPaint.h"
 #include "third_party/skia/include/core/SkPath.h"
 #include "ui/aura/client/aura_constants.h"
+#include "ui/aura/env.h"
 #include "ui/aura/window.h"
 #include "ui/base/animation/slide_animation.h"
 #include "ui/base/hit_test.h"
@@ -230,10 +231,14 @@ gfx::Rect FramePainter::GetWindowBoundsForClientBounds(
 int FramePainter::NonClientHitTest(views::NonClientFrameView* view,
                                    const gfx::Point& point) {
   gfx::Rect expanded_bounds = view->bounds();
-  int outside_bounds = ui::GetDisplayLayout() == ui::LAYOUT_TOUCH ?
-      kResizeOutsideBoundsSizeTouch :
-      kResizeOutsideBoundsSize;
+  int outside_bounds = kResizeOutsideBoundsSize;
+
+  if (ui::GetDisplayLayout() == ui::LAYOUT_TOUCH &&
+      aura::Env::GetInstance()->is_touch_down()) {
+    outside_bounds = kResizeOutsideBoundsSizeTouch;
+  }
   expanded_bounds.Inset(-outside_bounds, -outside_bounds);
+
   if (!expanded_bounds.Contains(point))
     return HTNOWHERE;
 
