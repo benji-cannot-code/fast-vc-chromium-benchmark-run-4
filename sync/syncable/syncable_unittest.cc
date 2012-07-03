2d57ccc791a773367ed11a43bab949d10a2f2c75
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 21:11:09 +0000

    sync: Fix and test delete of server-unknown items
    The fix we settled on for issue 125381 was to unset the unsynced bit for
    items which were created and deleted locally, before the server was ever
    iuformed of their existence.  The server doesn't like being notified of
    deletions to items it never knew existed, as we learned with
    issue 132905.
    
    It turns out that the syncapi was deleting items in a way that bypassed
    the originally attempted fix for issue 125381 in r137476.  This shows
    that the testing of the original fix was inadequate.  This commit fixes
    the bug and improves testing, with the hope of preventing these bugs
    from reoccuring in the future.  The fix is so subtle that I doubt
    anything other than lots and lots of tests will be able to prevent
    regressions.
    
    BUG=134934,125381
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10703060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145380 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/syncable/syncable_unittest.cc
+++ sync/syncable/syncable_unittest.cc
@@ -1284,6 +1284,10 @@ TEST_F(SyncableDirectoryTest, OldClientLeftUnsyncedDeletedLocalItem) {
   // The ID of the entry which will be unsynced, is_del and !ServerKnows().
   Id zombie_id = id_factory.NewLocalId();
 
+  // We're about to do some bad things.  Tell the directory verification
+  // routines to look the other way.
+  dir_->SetInvariantCheckLevel(OFF);
+
   {
     WriteTransaction trans(FROM_HERE, UNITTEST, dir_.get());
 
@@ -1318,6 +1322,10 @@ TEST_F(SyncableDirectoryTest, OldClientLeftUnsyncedDeletedLocalItem) {
 
   {
     ReadTransaction trans(FROM_HERE, dir_.get());
+
+    // The directory loading routines should have cleaned things up, making it
+    // safe to check invariants once again.
+    dir_->FullyCheckTreeInvariants(&trans);
 
     Entry server_knows(&trans, GET_BY_ID, server_knows_id);
     EXPECT_TRUE(server_knows.good());
@@ -1863,8 +1871,8 @@ class SyncableClientTagTest : public SyncableDirectoryTest {
     if (id.ServerKnows()) {
       me.Put(BASE_VERSION, kBaseVersion);
     }
-    me.Put(IS_DEL, deleted);
     me.Put(IS_UNSYNCED, true);
+    me.Put(IS_DEL, deleted);
     me.Put(IS_DIR, false);
     return me.Put(UNIQUE_CLIENT_TAG, tag);
   }
@@ -1878,7 +1886,11 @@ class SyncableClientTagTest : public SyncableDirectoryTest {
     EXPECT_EQ(me.Get(ID), id);
     EXPECT_EQ(me.Get(UNIQUE_CLIENT_TAG), test_tag_);
     EXPECT_EQ(me.Get(IS_DEL), deleted);
-    EXPECT_EQ(me.Get(IS_UNSYNCED), true);
+
+    // We only sync deleted items that the server knew about.
+    if (me.Get(ID).ServerKnows() || !me.Get(IS_DEL)) {
+      EXPECT_EQ(me.Get(IS_UNSYNCED), true);
+    }
   }
 
  protected:
