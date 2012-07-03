5d7c034d51ea4b16f0071930c6f4a6e71bfe7352
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 03:23:43 +0000

    [BlackBerry] Update DumpRenderTree to have it work interactively in parallel https://bugs.webkit.org/show_bug.cgi?id=88326
    Patch by Xiaobo Wang <xbwang@torchmobile.com.cn> on 2012-07-02
    Reviewed by Rob Buis.
    
    1. Test name is sent to torch-launcher via PPS message(from host machine).
       So we get test list by monitoring and receiving PPS message instead of
       reading file index.drt.
    2. Torch-launcher create a <test file>.done file when it finished a test.
    3. We don't need to search for Ref-tests in DumpRenderTree.cpp any more. NRWT
       will get them for us.
    
    * DumpRenderTree/blackberry/DumpRenderTree.cpp:
    (BlackBerry::WebKit::DumpRenderTree::DumpRenderTree):
    (BlackBerry::WebKit::DumpRenderTree::doneDrt):
    (BlackBerry::WebKit::DumpRenderTree::runRemainingTests):
    (BlackBerry::WebKit::DumpRenderTree::ensurePPS):
    (WebKit):
    (BlackBerry::WebKit::DumpRenderTree::handlePPSData):
    (BlackBerry::WebKit::DumpRenderTree::waitForTest):
    (BlackBerry::WebKit::DumpRenderTree::runTests):
    (BlackBerry::WebKit::DumpRenderTree::dump):
    * DumpRenderTree/blackberry/DumpRenderTreeBlackBerry.h:
    (DumpRenderTree):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121723 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/blackberry/DumpRenderTreeBlackBerry.h
+++ third_party/WebKit/Tools/DumpRenderTree/blackberry/DumpRenderTreeBlackBerry.h
@@ -22,9 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "BlackBerryGlobal.h"
 
+
 #include "DumpRenderTreeClient.h"
 #include "PlatformString.h"
 #include "Timer.h"
+#include <BlackBerryPlatformLayoutTest.h>
 #include <FindOptions.h>
 #include <wtf/Vector.h>
 
@@ -46,7 +48,7 @@ namespace BlackBerry {
 namespace WebKit {
 class WebPage;
 
-class DumpRenderTree : public BlackBerry::WebKit::DumpRenderTreeClient {
+class DumpRenderTree : public BlackBerry::WebKit::DumpRenderTreeClient, public BlackBerry::Platform::LayoutTestClient {
 public:
     DumpRenderTree(WebPage*);
     virtual ~DumpRenderTree();
@@ -105,11 +107,12 @@ public:
     void setSelectTrailingWhitespaceEnabled(bool enabled) { s_selectTrailingWhitespaceEnabled = enabled; }
     bool didReceiveAuthenticationChallenge(WebCore::Credential&);
 
+    // BlackBerry::Platform::BlackBerryPlatformLayoutTestClient method
+    virtual void addTest(const char* testFile);
 private:
     void runTest(const String& url);
     void runTests();
     void runCurrentTest();
-    void getRefTests(const String& testName);
 
     void processWork(WebCore::Timer<DumpRenderTree>*);
 
@@ -120,7 +123,6 @@ private:
     void locationChangeForFrame(WebCore::Frame*);
 
     void doneDrt();
-    void getTestsToRun();
     bool isHTTPTest(const String& test);
     String renderTreeDump() const;
     void resetToConsistentStateBeforeTesting();
@@ -130,10 +132,9 @@ private:
 
     Vector<String> m_tests;
     Vector<String>::iterator m_currentTest;
-    Vector<String> m_refTests; // Reference tests for current test
+    Vector<String> m_bufferedTests;
 
     String m_resultsDir;
-    String m_indexFile;
     String m_doneFile;
     String m_currentHttpTest;
     String m_currentTestFile;
@@ -147,7 +148,6 @@ private:
 
     bool m_acceptsEditing;
     bool m_loadFinished;
-    bool m_runningRefTests;
     static bool s_selectTrailingWhitespaceEnabled;
 };
 }
