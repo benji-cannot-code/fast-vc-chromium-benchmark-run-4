2bc8071d9da1f6fd6b1a983ede82b91b197c650b
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 09:42:13 +0000

    Treat numbers as word characters used by our spellchecker. This change adds Arabic numbers to the $ALeffter variable to prevent SpellcheckWordIterator from removing them.
    BUG=123290
    TEST=SpellCheckTest.SpellCheckStrings_EN_US
    
    Review URL: https://chromiumcodereview.appspot.com/10689035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145277 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/spellchecker/spellcheck_worditerator.cc
+++ chrome/renderer/spellchecker/spellcheck_worditerator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -65,7 +65,7 @@ void SpellcheckCharAttribute::CreateRuleSets(const std::string& language) {
       // For instance, U+05F4 is MidLetter. So, this may be
       // better, but it leads to an empty set error in Thai.
       // "$ALetter   = [[\\p{script=%s}] & [\\p{Word_Break = ALetter}]];"
-      "$ALetter      = [\\p{script=%s}];"
+      "$ALetter      = [\\p{script=%s} [0123456789]];"
       "$MidNumLet    = [\\p{Word_Break = MidNumLet}];"
       "$MidLetter    = [\\p{Word_Break = MidLetter}%s];"
       "$MidNum       = [\\p{Word_Break = MidNum}];"
