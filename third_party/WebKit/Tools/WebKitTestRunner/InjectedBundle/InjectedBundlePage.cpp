1a9d6fafae9bd2fc870cb106e1d798aa2c554156
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 17:36:27 +0000

    [Qt][WTR] Get rid of using DumpRenderTreeSupportQt https://bugs.webkit.org/show_bug.cgi?id=90262
    Unreviewed build fix.
    
    Reverting r121550 manually because it caused build
    break on ARM and Mac. It turned out that the Qt build
    system is too broken for this change now, first we have
    to address two issues:
        * symbol hiding was forbidden accidentally on x86 Linux
        * Qt lacks a separate library for test support
    
    * Tools.pro:
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    (WTR::InjectedBundlePage::resetAfterTest):
    (WTR::InjectedBundlePage::didClearWindowForFrame):
    * WebKitTestRunner/InjectedBundle/qt/ActivateFontsQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -46,6 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/text/CString.h>
 #include <wtf/text/StringBuilder.h>
 
+#if PLATFORM(QT)
+#include "DumpRenderTreeSupportQt.h"
+#endif
+
 using namespace std;
 
 namespace WTR {
@@ -348,7 +352,11 @@ void InjectedBundlePage::resetAfterTest()
 {
     WKBundleFrameRef frame = WKBundlePageGetMainFrame(m_page);
     JSGlobalContextRef context = WKBundleFrameGetJavaScriptContext(frame);
+#if PLATFORM(QT)
+    DumpRenderTreeSupportQt::injectInternalsObject(context);
+#else
     WebCoreTestSupport::resetInternalsObject(context);
+#endif
 }
 
 // Loader Client Callbacks
@@ -770,7 +778,11 @@ void InjectedBundlePage::didClearWindowForFrame(WKBundleFrameRef frame, WKBundle
     InjectedBundle::shared().textInputController()->makeWindowObject(context, window, &exception);
     InjectedBundle::shared().accessibilityController()->makeWindowObject(context, window, &exception);
 
+#if PLATFORM(QT)
+    DumpRenderTreeSupportQt::injectInternalsObject(context);
+#else
     WebCoreTestSupport::injectInternalsObject(context);
+#endif
 }
 
 void InjectedBundlePage::didCancelClientRedirectForFrame(WKBundleFrameRef frame)
