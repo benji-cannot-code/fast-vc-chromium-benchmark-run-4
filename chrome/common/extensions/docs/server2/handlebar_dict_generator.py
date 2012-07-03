0cf29e5f15b954f7c1d25ccef98540fdfa23ef3b
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-03 17:26:55 +0000

    Extensions Docs Server: IDL files
    The server can now handle IDL files as well as JSON files.
    
    BUG=131095
    
    Review URL: https://chromiumcodereview.appspot.com/10689077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145326 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/handlebar_dict_generator.py
+++ chrome/common/extensions/docs/server2/handlebar_dict_generator.py
@@ -74,6 +74,8 @@ class HandlebarDictGenerator(object):
     return function_dict
 
   def _GenerateCallback(self, callback):
+    if not callback:
+      return {}
     callback_dict = {
       'name': 'callback',
       'parameters': []
