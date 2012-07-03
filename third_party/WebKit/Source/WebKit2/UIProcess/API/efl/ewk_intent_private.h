4de90f5f69cc740db36713c3ddf02f6a7af14dba
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-03 06:54:42 +0000

    [EFL][WK2] Add API to deliver a Web Intent to a frame https://bugs.webkit.org/show_bug.cgi?id=90067
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-03
    Reviewed by Kenneth Rohde Christiansen.
    
    Add ewk_view_intent_deliver() method on the Ewk_View
    to deliver a Web Intent to the view's main frame.
    
    * UIProcess/API/efl/ewk_view.cpp:
    (ewk_view_intent_deliver):
    * UIProcess/API/efl/ewk_view.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_intent_private.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_intent_private.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 typedef struct _Ewk_Intent Ewk_Intent;
 
 Ewk_Intent* ewk_intent_new(WKIntentDataRef intentData);
+WKIntentDataRef ewk_intent_WKIntentDataRef_get(const Ewk_Intent* intent);
 
 #endif // ENABLE(WEB_INTENTS)
 
--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKURL.h"
 #include "ewk_context.h"
 #include "ewk_context_private.h"
+#include "ewk_intent_private.h"
 #include "ewk_view_loader_client_private.h"
 #include "ewk_view_private.h"
 #include <wtf/text/CString.h>
@@ -615,6 +616,21 @@ Eina_Bool ewk_view_forward(Evas_Object* ewkView)
         return true;
     }
     return false;
+}
+
+Eina_Bool ewk_view_intent_deliver(Evas_Object* ewkView, Ewk_Intent* intent)
+{
+#if ENABLE(WEB_INTENTS)
+    EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData, false);
+    EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv, false);
+    EINA_SAFETY_ON_NULL_RETURN_VAL(intent, false);
+
+    WKPageRef wkPage = toAPI(priv->pageClient->page());
+    WKPageDeliverIntentToFrame(wkPage, WKPageGetMainFrame(wkPage), ewk_intent_WKIntentDataRef_get(intent));
+    return true;
+#else
+    return false;
+#endif
 }
 
 Eina_Bool ewk_view_back_possible(Evas_Object* ewkView)
