df42ef66661b167835f86f0fb54a1d7e64e4a9b2
wingo@igalia.com <wingo@igalia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-04 20:32:26 +0000

    [GTK] Enable parallel GC https://bugs.webkit.org/show_bug.cgi?id=90568
    Reviewed by Martin Robinson.
    
    Source/JavaScriptCore:
    
    * runtime/Options.cpp: Include <algorithm.h> for std::min.
    
    Source/WTF:
    
    * wtf/Platform.h: Add GTK to the ENABLE_PARALLEL_GC party.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Options.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Options.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Options.h"
 
+#include <algorithm>
 #include <limits>
 #include <stdio.h>
 #include <stdlib.h>
