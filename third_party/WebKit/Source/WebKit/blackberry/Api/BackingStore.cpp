43f19ce13b49fc34e2424e1f1da6ab91682f101a
leo.yang@torchmobile.com.cn <leo.yang@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-04 02:59:30 +0000

    [BlackBerry] Checkerboard shown when clicking on error page buttons https://bugs.webkit.org/show_bug.cgi?id=90152 RIM PR #161867
    Reviewed by George Staikos.
    
    r121514 just made the race occur in lower possibility but can't avoid it.
    This path is using another approach to fix the problem. When we are resetting
    backingstore while committing a page we need to suspend backingstore and
    screen update to make sure no other threads can update backingstore and screen.
    
    * Api/BackingStore.cpp:
    (BlackBerry::WebKit::BackingStorePrivate::resetTiles):
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::setLoadState):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -1891,10 +1891,6 @@ void BackingStorePrivate::clearVisibleZoom()
 
 void BackingStorePrivate::resetTiles(bool resetBackground)
 {
-    // We need to reset m_hasBlitJobs to prevent ui thread from
-    // drawing checkerboard unintentionally. See RIM PR #161867.
-    m_hasBlitJobs = false;
-
     BackingStoreGeometry* currentState = frontState();
     TileMap currentMap = currentState->tileMap();
 
