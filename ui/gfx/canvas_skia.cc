3677a2c6c054fa7920df8a4c91111a25e2629fa4
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-04 04:40:39 +0000

    Fix eliding of LTR tab title in RTL UI under non-Windows Views.
    Use DrawFadeTruncatingString() on non-Windows views platforms too, now
    that the implementation is no longer platform specific.
    
    BUG=135058
    TEST=Launch chromeos chrome with --lang=he and go to amazon.com.
    Check that the tab title is painted LTR and elided (via fade
    truncation) on the right end. Now, go to google.com. Verify
    that the "Google" string is right-aligned in the tab title.
    TBR=sky@chromium.org,xji@chromium.org
    R=msw@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10701024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145432 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/canvas_skia.cc
+++ ui/gfx/canvas_skia.cc
@@ -400,8 +400,6 @@ void Canvas::DrawStringWithHalo(const string16& text,
   DrawImageInt(text_bitmap, x - 1, y - 1);
 }
 
-// TODO(asvitkine): Remove the ifdef once all platforms use canvas_skia.cc.
-#if defined(OS_WIN)
 void Canvas::DrawFadeTruncatingString(
       const string16& text,
       TruncateFadeMode truncate_mode,
@@ -468,6 +466,5 @@ void Canvas::DrawFadeTruncatingString(
   render_text->Draw(this);
   canvas_->restore();
 }
-#endif
 
 }  // namespace gfx
