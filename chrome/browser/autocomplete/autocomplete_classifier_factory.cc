711cf97046d7dc6bcc0ab871b3d4d753451b62aa
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-04 01:19:15 +0000

    Enable dependency of AutocompleteClassifierFactory on the ShortcutsBackendFactory.
    BUG=112558
    TEST=None
    Review URL: https://chromiumcodereview.appspot.com/10689098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_classifier_factory.cc
+++ chrome/browser/autocomplete/autocomplete_classifier_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
+#include "chrome/browser/history/shortcuts_backend_factory.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 
@@ -36,7 +37,7 @@ AutocompleteClassifierFactory::AutocompleteClassifierFactory()
   DependsOn(TemplateURLServiceFactory::GetInstance());
   // TODO(pkasting): Uncomment these once they exist.
   //   DependsOn(PrefServiceFactory::GetInstance());
-  //   DependsOn(ShortcutsBackendFactory::GetInstance());
+  DependsOn(ShortcutsBackendFactory::GetInstance());
 }
 
 AutocompleteClassifierFactory::~AutocompleteClassifierFactory() {
