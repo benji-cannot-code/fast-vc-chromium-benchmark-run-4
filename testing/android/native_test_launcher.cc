7cb685bf29458ca812c150c24d1464a8f7a64bdd
shouqun.liu@intel.com <shouqun.liu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-05 15:55:25 +0000

    Add WaitForDebugger for android native tests
    For convenience of debugging APKs of native tests (*_unittests, DumpRenderTree)
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10697083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145538 0039d316-1c4b-4281-b951-d872f2087c98

--- testing/android/native_test_launcher.cc
+++ testing/android/native_test_launcher.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/path_utils.h"
 #include "base/android/scoped_java_ref.h"
 #include "base/at_exit.h"
+#include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
@@ -178,6 +179,7 @@ static void RunTests(JNIEnv* env,
                      jobject app_context) {
   base::AtExitManager exit_manager;
 
+  // Command line initialized basically, will be fully initialized later.
   static const char* const kInitialArgv[] = { "ChromeTestActivity" };
   CommandLine::Init(arraysize(kInitialArgv), kInitialArgv);
 
@@ -203,6 +205,18 @@ static void RunTests(JNIEnv* env,
   // This object is owned by gtest.
   AndroidLogPrinter* log = new AndroidLogPrinter();
   log->Init(&argc, &argv[0]);
+
+  // Fully initialize command line with arguments.
+  CommandLine::ForCurrentProcess()->AppendArguments(
+      CommandLine(argc, &argv[0]), false);
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+  if (command_line.HasSwitch(switches::kWaitForDebugger)) {
+    std::string msg = StringPrintf("Native test waiting for GDB because "
+                                   "flag %s was supplied",
+                                   switches::kWaitForDebugger);
+    log_write(ANDROID_LOG_VERBOSE, msg.c_str());
+    base::debug::WaitForDebugger(24 * 60 * 60, false);
+  }
 
   main(argc, &argv[0]);
 }
