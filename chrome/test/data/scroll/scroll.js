f274043982b7c84dba6ab482038e403fc4ce8b32
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-05 18:33:59 +0000

    Add scroll test for real world web sites.
    For now it uses the same sites list as the page cycler and it is disabled.
    I plan to change that.
    
    BUG=134057
    TEST=chrome/test/functional/perf.py perf.PopularSitesScrollTest.test2012Q2
    
    
    Review URL: https://chromiumcodereview.appspot.com/10702032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/scroll/scroll.js
+++ chrome/test/data/scroll/scroll.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // USAGE:
 //   1. To start the scrolling, invoke __scroll_test().
 //   2. Wait for __scrolling_complete to be true
-//   3. Read __initial_frame_times and __repeat_frame_times arrays.
+//   3. Read __frame_times array.
 
 
 // Function that sets a callback called when the next frame is rendered.
