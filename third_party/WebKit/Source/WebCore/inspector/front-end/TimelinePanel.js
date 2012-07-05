053e0723e5d7681addb0658475c762d80ae5af4f
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-05 15:17:43 +0000

    Web Inspector: incorrect height of main timeline pane after switching to memory mode https://bugs.webkit.org/show_bug.cgi?id=90387
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._splitterDragging):
    (WebInspector.TimelinePanel.prototype.set _setSplitterPosition):
    (WebInspector.TimelinePanel.prototype._overviewModeChanged):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePanel.js
@@ -158,7 +158,6 @@ WebInspector.TimelinePanel.prototype = {
         var top = event.pageY + this._dragOffset
         this._setSplitterPosition(top);
         event.preventDefault();
-        this._refresh();
     },
 
     /**
@@ -182,6 +181,7 @@ WebInspector.TimelinePanel.prototype = {
         this._timelineMemorySplitter.style.top = (top - 2) + "px";
         this._memoryStatistics.setTopPosition(top);
         this._containerElementHeight = this._containerElement.clientHeight;
+        this.onResize();
     },
 
     get calculator()
@@ -438,13 +438,13 @@ WebInspector.TimelinePanel.prototype = {
             this._memoryStatistics.hide();
             this.splitView.element.style.height = "auto";
             this.splitView.element.style.bottom = "0";
+            this.onResize();
         } else {
             this._timelineMemorySplitter.removeStyleClass("hidden");
             this._memoryStatistics.show();
             this.splitView.element.style.bottom = "auto";
             this._setSplitterPosition(WebInspector.settings.memoryCounterGraphsHeight.get());
         }
-        this._refresh();
     },
 
     _toggleTimelineButtonClicked: function()
