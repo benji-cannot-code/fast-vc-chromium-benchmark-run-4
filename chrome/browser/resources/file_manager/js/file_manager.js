b470287fe7799f261a51f2f761b546ceb9ded173
oleg@chromium.org <oleg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-05 11:45:02 +0000

    Fix style warnings in file_manager.js. Review URL: https://chromiumcodereview.appspot.com/10689112
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145524 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1088,10 +1088,10 @@ FileManager.prototype = {
       // Hack goes below, since we don't receive beforepaste event, but receive
       // beforecut and beforecopy events.
       case 'paste':
-        return this.isRenamingInProgress()
-                   ? this.document_.queryCommandEnabled(commandId)
-                   : (!!this.fileTransferController_ &&
-                      this.fileTransferController_.queryPasteCommandEnabled());
+        return this.isRenamingInProgress() ?
+               this.document_.queryCommandEnabled(commandId) :
+               (!!this.fileTransferController_ &&
+                this.fileTransferController_.queryPasteCommandEnabled());
 
       case 'rename':
         return (// Initialized to the point where we have a current directory
@@ -1103,11 +1103,9 @@ FileManager.prototype = {
                 this.selection.totalCount == 1);
 
       case 'delete':
-        return (this.isRenamingInProgress()
-                ? this.document_.queryCommandEnabled(commandId)
-                : !readonly &&
-                  this.selection &&
-                  this.selection.totalCount > 0);
+        return this.isRenamingInProgress() ?
+               this.document_.queryCommandEnabled(commandId) :
+               !readonly && this.selection && this.selection.totalCount > 0;
 
       case 'newfolder':
         return !readonly &&
