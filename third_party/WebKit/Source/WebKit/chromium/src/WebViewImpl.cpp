1a6049571b19ff5976f08193d55f5e5bbe48f7dc
scheib@chromium.org <scheib@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-05 16:21:43 +0000

    [Chromium] Clear m_currentInputEvent after handled by pointerLockMouseEvent(). https://bugs.webkit.org/show_bug.cgi?id=90391
    Source/WebKit/chromium:
    
    WebViewImpl::handleInputEvent was keeping a pointer to an input event that would
    later be accessed. When in pointer lock, that pointer was not being cleared.
    Code modified to use TemporaryChange to automatically clear the pointer at all
    method exit points.
    
    Reviewed by Abhishek Arya.
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::handleInputEvent):
    
    LayoutTests:
    
    Test that reproduces bug 90391:
    Enable pointer lock, receive mouse move, call window.open, don't crash.
    
    Reviewed by Abhishek Arya.
    
    * pointer-lock/bug90391-move-then-window-open-crash-expected.txt: Added.
    * pointer-lock/bug90391-move-then-window-open-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -165,6 +165,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/CurrentTime.h>
 #include <wtf/MainThread.h>
 #include <wtf/RefPtr.h>
+#include <wtf/TemporaryChange.h>
 #include <wtf/Uint8ClampedArray.h>
 
 #if ENABLE(GESTURE_EVENTS)
@@ -1761,7 +1762,7 @@ bool WebViewImpl::handleInputEvent(const WebInputEvent& inputEvent)
     if (m_ignoreInputEvents)
         return false;
 
-    m_currentInputEvent = &inputEvent;
+    TemporaryChange<const WebInputEvent*>(m_currentInputEvent, &inputEvent);
 
 #if ENABLE(POINTER_LOCK)
     if (isPointerLocked() && WebInputEvent::isMouseEventType(inputEvent.type)) {
@@ -1799,12 +1800,10 @@ bool WebViewImpl::handleInputEvent(const WebInputEvent& inputEvent)
         node->dispatchMouseEvent(
               PlatformMouseEventBuilder(mainFrameImpl()->frameView(), *static_cast<const WebMouseEvent*>(&inputEvent)),
               eventType, static_cast<const WebMouseEvent*>(&inputEvent)->clickCount);
-        m_currentInputEvent = 0;
         return true;
     }
 
     bool handled = PageWidgetDelegate::handleInputEvent(m_page.get(), *this, inputEvent);
-    m_currentInputEvent = 0;
     return handled;
 }
 
