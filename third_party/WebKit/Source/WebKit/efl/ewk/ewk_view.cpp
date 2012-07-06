eb25f152c349bd299364c8f7a15d6340ba6f289a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-06 07:17:22 +0000

    [EFL] Add Web Inspector to WebKit-EFL https://bugs.webkit.org/show_bug.cgi?id=83865
    Patch by Seokju Kwon <seokju.kwon@samsung.com> on 2012-07-06
    Reviewed by Pavel Feldman.
    
    Source/WebKit:
    
    EFL port implementation for Web Inspector.
    Prepare Web Inspector resources.
    
    * PlatformEfl.cmake:
    
    Source/WebKit/efl:
    
    EFL port implementation for Web Inspector.
    Implementation of InspectorClientEfl and InspectorFrontendClientEfl
    for using the Web Inspector in WebKit-EFL with new EWK APIs.
    
    * WebCoreSupport/InspectorClientEfl.cpp:
    (WebCore::notifyWebInspectorDestroy):
    (WebCore):
    (InspectorFrontendSettingsEfl):
    (WebCore::InspectorFrontendSettingsEfl::getProperty):
    (WebCore::InspectorFrontendSettingsEfl::setProperty):
    (WebCore::InspectorClientEfl::InspectorClientEfl):
    (WebCore::InspectorClientEfl::~InspectorClientEfl):
    (WebCore::InspectorClientEfl::inspectorDestroyed):
    (WebCore::InspectorClientEfl::openInspectorFrontend):
    (WebCore::InspectorClientEfl::closeInspectorFrontend):
    (WebCore::InspectorClientEfl::bringFrontendToFront):
    (WebCore::InspectorClientEfl::sendMessageToFrontend):
    (WebCore::InspectorClientEfl::releaseFrontendPage):
    (WebCore::InspectorClientEfl::inspectorFilesPath):
    (WebCore::InspectorFrontendClientEfl::InspectorFrontendClientEfl):
    (WebCore::InspectorFrontendClientEfl::~InspectorFrontendClientEfl):
    (WebCore::InspectorFrontendClientEfl::localizedStringsURL):
    (WebCore::InspectorFrontendClientEfl::hiddenPanels):
    (WebCore::InspectorFrontendClientEfl::bringToFront):
    (WebCore::InspectorFrontendClientEfl::closeWindow):
    (WebCore::InspectorFrontendClientEfl::inspectedURLChanged):
    (WebCore::InspectorFrontendClientEfl::attachWindow):
    (WebCore::InspectorFrontendClientEfl::detachWindow):
    (WebCore::InspectorFrontendClientEfl::setAttachedWindowHeight):
    (WebCore::InspectorFrontendClientEfl::destroyInspectorWindow):
    * WebCoreSupport/InspectorClientEfl.h:
    (WebCore):
    (InspectorClientEfl):
    (InspectorFrontendClientEfl):
    (WebCore::InspectorFrontendClientEfl::disconnectInspectorClient):
    * ewk/ewk_view.cpp:
    (_Ewk_View_Private_Data):
    (_ewk_view_priv_new):
    (ewk_view_web_inspector_show):
    (ewk_view_web_inspector_close):
    (ewk_view_web_inspector_view_get):
    (ewk_view_web_inspector_view_set):
    * ewk/ewk_view.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLInputElement.h"
 #include "HTMLNames.h"
 #include "InspectorClientEfl.h"
+#include "InspectorController.h"
 #include "IntSize.h"
 #include "JSDOMBinding.h"
 #include "JSDOMWindow.h"
@@ -345,6 +346,9 @@ struct _Ewk_View_Private_Data {
     SoupSession* soupSession;
     const char* cursorGroup;
     Evas_Object* cursorObject;
+#if ENABLE(INSPECTOR)
+    Evas_Object* inspectorView;
+#endif
 #ifdef HAVE_ECORE_X
     bool isUsingEcoreX;
 #endif
@@ -731,8 +735,9 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* smartData)
     pageClients.chromeClient = new WebCore::ChromeClientEfl(smartData->self);
     pageClients.editorClient = new WebCore::EditorClientEfl(smartData->self);
     pageClients.dragClient = new WebCore::DragClientEfl;
-    pageClients.inspectorClient = new WebCore::InspectorClientEfl;
-
+#if ENABLE(INSPECTOR)
+    pageClients.inspectorClient = new WebCore::InspectorClientEfl(smartData->self);
+#endif
     priv->page = adoptPtr(new WebCore::Page(pageClients));
 
 #if ENABLE(DEVICE_ORIENTATION)
@@ -4360,6 +4365,48 @@ void ewk_view_setting_should_display_text_descriptions_set(Evas_Object *ewkView,
         priv->pageSettings->setShouldDisplayTextDescriptions(enable);
         priv->settings.shouldDisplayTextDescriptions = enable;
     }
+#endif
+}
+
+void ewk_view_web_inspector_show(const Evas_Object* ewkView)
+{
+#if ENABLE(INSPECTOR)
+    EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData);
+    EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv);
+
+    priv->page->inspectorController()->show();
+#endif
+}
+
+void ewk_view_web_inspector_close(const Evas_Object* ewkView)
+{
+#if ENABLE(INSPECTOR)
+    EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData);
+    EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv);
+
+    priv->page->inspectorController()->close();
+#endif
+}
+
+Evas_Object* ewk_view_web_inspector_view_get(const Evas_Object* ewkView)
+{
+#if ENABLE(INSPECTOR)
+    EWK_VIEW_SD_GET_OR_RETURN(ewkView, smartData, 0);
+    EWK_VIEW_PRIV_GET_OR_RETURN(smartData, priv, 0);
+
+    return priv->inspectorView;
+#else
+    return 0;
+#endif
+}
+
+void ewk_view_web_inspector_view_set(Evas_Object* ewkView, Evas_Object* inspectorView)
+{
+#if ENABLE(INSPECTOR)
+    EWK_VIEW_SD_GET(ewkView, smartData);
+    EWK_VIEW_PRIV_GET(smartData, priv);
+
+    priv->inspectorView = inspectorView;
 #endif
 }
 
