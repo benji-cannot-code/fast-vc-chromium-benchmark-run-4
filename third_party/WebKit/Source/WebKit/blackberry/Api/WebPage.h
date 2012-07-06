2b8ef459433289b405c76e3b64eba083e7fd6ec2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-06 20:43:20 +0000

    Add an API to explicitly call a JavaScript function with args. https://bugs.webkit.org/show_bug.cgi?id=90694
    Currently the Blackberry port doesn't expose the JavaScript
    engine to 3rd parties so they rely upon executeJavaScript
    which can be slower than necessary and unsafe as eval is used.
    This new API provides a way to explicitly call a specific
    JavaScript function with a list of args preventing the case
    where an argument comes from a untrusted source and tries to
    escape the arg list to take control of the JavaScript engine.
    
    In the future if the Blackberry port introduces a formal
    way to interact with the JavaScript engine this function should
    be removed.
    
    PR 149294
    
    Patch by Benjamin C Meyer <bmeyer@rim.com> on 2012-07-06
    Reviewed by Unreviewed
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::executeJavaScriptFunction):
    (WebKit):
    * Api/WebPage.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <BlackBerryPlatformWebContext.h>
 #include <imf/input_data.h>
 #include <network/NetworkRequest.h>
+#include <string>
+#include <vector>
 
 struct OpaqueJSContext;
 typedef const struct OpaqueJSContext* JSContextRef;
@@ -103,6 +105,8 @@ public:
 
     // Takes a UTF16 encoded script that is used explicitly by the pattern matching code
     bool executeJavaScriptInIsolatedWorld(const std::wstring& script, JavaScriptDataType& returnType, WebString& returnValue);
+
+    bool executeJavaScriptFunction(const std::vector<std::string> &script, const std::vector<std::string> &args, JavaScriptDataType& returnType, WebString& returnValue);
 
     void initializeIconDataBase();
 
