91c5a1dd2749517772d615bbaa014410f864cfc8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-06 06:07:52 +0000

    [WK2][EFL] Ewk_View needs to report load status changes https://bugs.webkit.org/show_bug.cgi?id=90566
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-05
    Reviewed by Kenneth Rohde Christiansen.
    
    Add new "load,finished", "load,provisional,failed",
    "load,provisional,redirect" and "load,provisional,started"
    signals on the Ewk_View to notify the client of different
    load state changes.
    
    * UIProcess/API/efl/ewk_view.cpp:
    (ewk_view_load_finished):
    (ewk_view_load_provisional_failed):
    (ewk_view_load_provisional_redirect):
    (ewk_view_load_provisional_started):
    * UIProcess/API/efl/ewk_view.h:
    * UIProcess/API/efl/ewk_view_loader_client.cpp:
    (didFinishLoadForFrame):
    (didFailLoadWithErrorForFrame):
    (didStartProvisionalLoadForFrame):
    (didReceiveServerRedirectForProvisionalLoadForFrame):
    (didFailProvisionalLoadWithErrorForFrame):
    (ewk_view_loader_client_attach):
    * UIProcess/API/efl/ewk_view_private.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.h
@@ -30,7 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * - "intent,request,new", Ewk_Intent_Request*: reports new Web intent request.
  * - "intent,service,register", Ewk_Intent_Service*: reports new Web intent service registration.
  * - "load,error", const Ewk_Web_Error*: reports main frame load failed.
+ * - "load,finished", void: reports load finished.
  * - "load,progress", double*: load progress has changed (value from 0.0 to 1.0).
+ * - "load,provisional,failed", const Ewk_Web_Error*: view provisional load failed.
+ * - "load,provisional,redirect", void: view received redirect for provisional load.
+ * - "load,provisional,started", void: view started provisional load.
  * - "resource,request,new", const Ewk_Web_Resource_Request*: a resource request was initiated.
  * - "title,changed", const char*: title of the main frame was changed.
  */
