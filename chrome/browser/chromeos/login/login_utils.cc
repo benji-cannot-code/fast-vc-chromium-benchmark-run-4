452dd7987bf854095c7b4fe17fd999e5199a06dd
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-06 16:29:09 +0000

    Add --load-2x-resources to list of allowed switches in guest mode
    Bug=136014
    Test=Manual, 2x resources loaded in guest mode
    
    Review URL: https://chromiumcodereview.appspot.com/10698109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145598 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -739,6 +739,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ::switches::kDisableThreadedCompositing,
       ::switches::kForceCompositingMode,
       ::switches::kGpuStartupDialog,
+      ::switches::kLoad2xResources,
       ::switches::kLoginProfile,
       ::switches::kScrollPixels,
       ::switches::kNoFirstRun,
