6bc134f16167289bcf7bee6783d6214ce818b3da
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-06 02:50:57 +0000

    Multiple Content Security Policy headers are correctly processed as separate headers. https://bugs.webkit.org/show_bug.cgi?id=90629
    Source/WebCore:
    
    Headers of the same name are normalized into a single, comma-separated
    string as per RFC2616, section 4.2. We didn't correctly account for this
    in ContentSecurityPolicy::didReceiveHeader. Now we do by walking through
    the header string, looking for commas and processing each block in turn.
    
    This oversight bit Firefox as well, and was patched in February:
    https://bugzilla.mozilla.org/show_bug.cgi?id=717511
    
    Patch by Mike West <mkwst@chromium.org> on 2012-07-05
    Reviewed by Adam Barth.
    
    Test: http/tests/security/contentSecurityPolicy/directive-parsing-multiple-headers.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::ContentSecurityPolicy::didReceiveHeader):
    
    LayoutTests:
    
    Patch by Mike West <mkwst@chromium.org> on 2012-07-05
    Reviewed by Adam Barth.
    
    * http/tests/security/contentSecurityPolicy/directive-parsing-multiple-headers-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/directive-parsing-multiple-headers.html: Added.
    * http/tests/security/contentSecurityPolicy/resources/echo-multiple-headers.pl: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@121936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/echo-multiple-headers.pl
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/perl -wT
+use strict;
+use CGI;
+
+my $cgi = new CGI;
+
+print "Content-Type: text/html; charset=UTF-8\n";
+print "X-WebKit-CSP: ".$cgi->param('csp1')."\n";
+print "X-WebKit-CSP: ".$cgi->param('csp2')."\n\n";
+
+my ($text, $replacement) = ("FAIL", "PASS");
+($text, $replacement) = ($replacement, $text) if $cgi->param('should_run') eq 'no';
+
+print "<!DOCTYPE html>\n";
+print "<html>\n";
+print "<body>\n";
+print "<div id=\"result\" text=\"$replacement\">\n";
+print "$text\n";
+print "</div>\n";
+print "<script src=\"".$cgi->param('q')."\"></script>\n";
+print "</body>\n";
+print "</html>\n";
