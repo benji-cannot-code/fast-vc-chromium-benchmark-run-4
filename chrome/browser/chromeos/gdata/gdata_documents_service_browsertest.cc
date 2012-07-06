ece9dba695f86b6f2e7fac1b7a68774e27d4f1fd
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-06 21:32:08 +0000

    Revert r145517 "Change GetDocuments to use a struct to hold the arguments"
    TBR=zork@chromium.org
    BUG=none
    TEST=RemoteFileSystemExtensionApiTest.ContentSearch on chromeos bot cycles green
    
    Review URL: https://chromiumcodereview.appspot.com/10700126
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_documents_service_browsertest.cc
+++ chrome/browser/chromeos/gdata/gdata_documents_service_browsertest.cc
@@ -111,10 +111,9 @@ IN_PROC_BROWSER_TEST_F(GDataTest, GetDocuments) {
   base::Value* result_data = NULL;
   service_->GetDocuments(
       gdata_test_server_.GetURL("files/chromeos/gdata/root_feed.json"),
-      gdata::DocumentsServiceInterface::GetDocumentsOptions(
-          0,  // start_changestamp
-          std::string(),  // search string
-          std::string()),  // directory resource ID
+      0,  // start_changestamp
+      std::string(),  // search string
+      std::string(),  // directory resource ID
       base::Bind(&TestGetDocumentsCallback, &result, &result_data));
   ui_test_utils::RunMessageLoop();
 
@@ -137,10 +136,9 @@ IN_PROC_BROWSER_TEST_F(GDataTest, GetDocumentsFailure) {
   base::Value* result_data = NULL;
   service_->GetDocuments(
       gdata_test_server_.GetURL("files/chromeos/gdata/testfile.txt"),
-      gdata::DocumentsServiceInterface::GetDocumentsOptions(
-          0,  // start_changestamp
-          std::string(),  // search string
-          std::string()),  // directory resource ID
+      0,  // start_changestamp
+      std::string(),  // search string
+      std::string(),  // directory resource ID
       base::Bind(&TestGetDocumentsCallback, &result, &result_data));
   ui_test_utils::RunMessageLoop();
 
--- chrome/browser/chromeos/gdata/gdata_file_system.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system.cc
@@ -969,9 +969,9 @@ void GDataFileSystem::LoadFeedFromServer(
   const base::TimeTicks start_time = base::TimeTicks::Now();
   documents_service_->GetDocuments(
       GURL(),   // root feed start.
-      DocumentsServiceInterface::GetDocumentsOptions(start_changestamp,
-                                                     search_query,
-                                                     directory_resource_id),
+      start_changestamp,
+      search_query,
+      directory_resource_id,
       base::Bind(&GDataFileSystem::OnGetDocuments,
                  ui_weak_ptr_,
                  initial_origin,
@@ -2655,10 +2655,9 @@ void GDataFileSystem::OnGetDocuments(ContentOrigin initial_origin,
     // Kick of the remaining part of the feeds.
     documents_service_->GetDocuments(
         next_feed_url,
-        DocumentsServiceInterface::GetDocumentsOptions(
-            params->start_changestamp,
-            params->search_query,
-            params->directory_resource_id),
+        params->start_changestamp,
+        params->search_query,
+        params->directory_resource_id,
         base::Bind(&GDataFileSystem::OnGetDocuments,
                    ui_weak_ptr_,
                    initial_origin,
