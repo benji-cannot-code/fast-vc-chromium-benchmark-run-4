a0e8d4092f29a0a0eaebce03d57bfab749347434
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-06 14:29:05 +0000

    Prints useful error message for invalid arguments to trace_test_cases.py.
    Make sure to normalize --cwd argument and do not default to 'chrome' anymore.
    
    TBR=cmp@chromium.org
    NOTRY=true
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10694102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145591 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/trace_test_cases_smoke_test.py
+++ tools/isolate/trace_test_cases_smoke_test.py
@@ -99,7 +99,6 @@ class TraceTestCases(unittest.TestCase):
         '--timeout', '0',
         '--out', self.temp_file,
         '--root-dir', ROOT_DIR,
-        '--cwd', ROOT_DIR,
         '--variable', 'PRODUCT_DIR', 'data',
         TARGET_PATH,
     ]
@@ -109,7 +108,7 @@ class TraceTestCases(unittest.TestCase):
     proc = subprocess.Popen(
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     out, err = proc.communicate() or ('', '')  # pylint is confused.
-    self.assertEquals(0, proc.returncode)
+    self.assertEquals(0, proc.returncode, (out, err))
     if sys.platform == 'win32':
       # TODO(maruel): Figure out why replace('\r\n', '\n') doesn't work.
       out = out.replace('\r', '')
