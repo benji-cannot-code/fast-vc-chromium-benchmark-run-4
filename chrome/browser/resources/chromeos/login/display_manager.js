4ad5f28967ca2d2b07608aa246a279c55fde8191
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-06 11:37:01 +0000

    Replace the link to opt-out of auto-enrollment with a keyboard shortcut.
    BUG=chromium-os:31897
    TEST=Mark a device for auto-enrollment; go through OOBE and sign-in with an account from another domain; opt-out using the shortcut
    
    
    Review URL: https://chromiumcodereview.appspot.com/10694099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145582 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/display_manager.js
+++ chrome/browser/resources/chromeos/login/display_manager.js
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(xiyuan): Find a better to share those constants.
 /** @const */ var SCREEN_OOBE_NETWORK = 'connect';
 /** @const */ var SCREEN_OOBE_EULA = 'eula';
+/** @const */ var SCREEN_OOBE_ENROLLMENT = 'oauth-enrollment';
 /** @const */ var SCREEN_GAIA_SIGNIN = 'gaia-signin';
 /** @const */ var SCREEN_ACCOUNT_PICKER = 'account-picker';
 
@@ -96,6 +97,10 @@ cr.define('cr.ui.login', function() {
           // In this case update check will be skipped and OOBE will
           // proceed straight to enrollment screen when EULA is accepted.
           chrome.send('skipUpdateEnrollAfterEula');
+        } else if (currentStepId == SCREEN_OOBE_ENROLLMENT) {
+          // This accelerator is also used to manually cancel auto-enrollment.
+          if (this.currentScreen.cancelAutoEnrollment)
+            this.currentScreen.cancelAutoEnrollment();
         }
       } else if (name == ACCELERATOR_VERSION) {
         if (this.allowToggleVersion_)
