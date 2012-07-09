bdde7feb34ef78bc0fd3f2fb1ccce9196da36755
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-09 23:32:45 +0000

    Extensions Docs Server: Support APIs with properties
    Added a template for API properties. I also added templates for the privacy API,
    which has properties.
    
    BUG=131095
    
    Review URL: https://chromiumcodereview.appspot.com/10689117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145786 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/json_schema_compiler/model.py
+++ tools/json_schema_compiler/model.py
@@ -112,9 +112,8 @@ class Callback(object):
   def __init__(self, parent, json):
     params = json['parameters']
     self.parent = parent
-    self.optional = False
-    if 'optional' in json:
-      self.optional = json['optional'] == True
+    self.description = json.get('description')
+    self.optional = json.get('optional', False)
     self.params = []
     if len(params) == 0:
       return
@@ -185,6 +184,7 @@ class Property(object):
     self._unix_name = UnixName(self.name)
     self._unix_name_used = False
     self.optional = json.get('optional', False)
+    self.functions = []
     self.has_value = False
     self.description = json.get('description')
     self.parent = parent
