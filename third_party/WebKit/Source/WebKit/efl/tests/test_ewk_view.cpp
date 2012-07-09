7182771b765b3b22b945a7c956b7f3a00f63a214
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-09 11:36:37 +0000

    [EFL] Simplify runTest function by defining default arguments. https://bugs.webkit.org/show_bug.cgi?id=90525
    Patch by Krzysztof Czech <k.czech@samsung.com> on 2012-07-09
    Reviewed by Chang Shu.
    
    Default arguments will simplify macro RUN_TEST.
    Function will only have one mandatory argument, callback test function.
    Added static modifiers to callback functions.
    
    * tests/UnitTestUtils/EWKTestBase.h:
    (EWKTestBase):
    * tests/test_ewk_view.cpp:
    (ewkViewEditableGetCb):
    (TEST):
    (ewkViewUriGetCb):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/tests/test_ewk_view.cpp
+++ third_party/WebKit/Source/WebKit/efl/tests/test_ewk_view.cpp
@@ -28,7 +28,7 @@ using namespace EWKUnitTests;
 /**
 * @brief Checking whether function properly returns correct value.
 */
-void ewkViewEditableGetCb(void* eventInfo, Evas_Object* o, void* data)
+static void ewkViewEditableGetCb(void* eventInfo, Evas_Object* o, void* data)
 {
     ewk_view_editable_set(o, EINA_FALSE);
     EXPECT_EQ(EINA_FALSE, ewk_view_editable_get(o));
@@ -37,13 +37,13 @@ void ewkViewEditableGetCb(void* eventInfo, Evas_Object* o, void* data)
 
 TEST(test_ewk_view, ewk_view_editable_get)
 {
-    RUN_TEST(ewkViewEditableGetCb, "load,finished", 0);
+    RUN_TEST(ewkViewEditableGetCb);
 }
 
 /**
 * @brief Checking whether function returns correct uri string.
 */
-void ewkViewUriGetCb(void* eventInfo, Evas_Object* o, void* data)
+static void ewkViewUriGetCb(void* eventInfo, Evas_Object* o, void* data)
 {
     EXPECT_STREQ("http://www.webkit.org/", ewk_view_uri_get(o));
     END_TEST();
@@ -51,5 +51,5 @@ void ewkViewUriGetCb(void* eventInfo, Evas_Object* o, void* data)
 
 TEST(test_ewk_view, ewk_view_uri_get)
 {
-    RUN_TEST("http://www.webkit.org", ewkViewUriGetCb, "load,finished", 0);
+    RUN_TEST("http://www.webkit.org", ewkViewUriGetCb);
 }
