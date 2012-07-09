60c80d8d1386910c23843dfc8bab2a1f894bb0f0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-09 04:59:12 +0000

    Unreviewed, rolling out r121968. http://trac.webkit.org/changeset/121968 https://bugs.webkit.org/show_bug.cgi?id=90749
    Caused a link error on chromium Win dbg. (Requested by hayato
    on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-07-08
    
    * inspector/InspectorMemoryAgent.cpp:
    (WebCore::InspectorMemoryAgent::getProcessMemoryDistribution):
    * inspector/front-end/NativeMemorySnapshotView.js:
    (WebInspector.MemoryBlockViewProperties._initialize):
    * platform/MemoryUsageSupport.cpp:
    * platform/MemoryUsageSupport.h:
    (MemoryUsageSupport):
    * platform/chromium/MemoryUsageSupportChromium.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/MemoryUsageSupport.cpp
+++ third_party/WebKit/Source/WebCore/platform/MemoryUsageSupport.cpp
@@ -64,8 +64,4 @@ bool MemoryUsageSupport::processMemorySizesInBytes(size_t*, size_t*)
     return false;
 }
 
-void MemoryUsageSupport::memoryUsageByComponents(Vector<ComponentInfo>&)
-{
-}
-
 } // namespace WebCore
--- third_party/WebKit/Source/WebCore/platform/chromium/MemoryUsageSupportChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/MemoryUsageSupportChromium.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryUsageSupport.h"
 
 #include <public/Platform.h>
-#include <third_party/skia/src/core/SkGlyphCache.h>
 
 namespace WebCore {
 
@@ -65,20 +64,6 @@ int MemoryUsageSupport::highUsageDeltaMB()
 bool MemoryUsageSupport::processMemorySizesInBytes(size_t* privateBytes, size_t* sharedBytes)
 {
     return WebKit::Platform::current()->processMemorySizesInBytes(privateBytes, sharedBytes);
-}
-
-static bool glyphCacheVisitor(SkGlyphCache* cache, void* context)
-{
-    size_t* sizePtr = reinterpret_cast<size_t*>(context);
-    *sizePtr += sizeof(*cache);
-    return false;
-}
-
-void MemoryUsageSupport::memoryUsageByComponents(Vector<ComponentInfo>& components)
-{
-    size_t size = 0;
-    SkGlyphCache::VisitAllCaches(glyphCacheVisitor, &size);
-    components.append(ComponentInfo("GlyphCache", size));
 }
 
 } // namespace WebCore
