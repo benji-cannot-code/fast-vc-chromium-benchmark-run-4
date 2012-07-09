78e5ff4230d4c45bd3f5d3793fb734534258f126
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-09 03:53:32 +0000

    Cleanup: make ExtensionRequestSender manage its IPC responses directly, rather than going via ExtensionHelper and ExtensionDispatcher. Also make it generate its request IDs internally.
    A cleanup that leads from this is making ExtensionMsg_Reponse a CONTROL message
    rather than ROUTED, simplifying some browser-side code.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10703111
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145642 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/chrome_render_view_test.h
+++ chrome/test/base/chrome_render_view_test.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/autofill/autofill_agent.h"
 #include "chrome/renderer/chrome_mock_render_thread.h"
 #include "chrome/renderer/chrome_content_renderer_client.h"
+#include "content/public/test/mock_render_thread.h"
 #include "content/public/test/render_view_test.h"
 
 class ExtensionDispatcher;
