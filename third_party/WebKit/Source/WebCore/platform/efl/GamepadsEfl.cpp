0e1969518e746d0547e174d4a1f0542178ec0b74
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-09 19:59:02 +0000

    [EFL] Log significant Gamepad API-related events https://bugs.webkit.org/show_bug.cgi?id=90595
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-09
    Reviewed by Antonio Gomes.
    
    Log significant Gamepad API-related events in
    GamepadsEfl.
    
    No new tests, no behavior change.
    
    * platform/efl/GamepadsEfl.cpp:
    (WebCore::GamepadDeviceEfl::deviceFile):
    (GamepadDeviceEfl):
    (WebCore::GamepadDeviceEfl::GamepadDeviceEfl):
    (WebCore::GamepadDeviceEfl::readCallback):
    (WebCore::GamepadsEfl::registerDevice):
    (WebCore::GamepadsEfl::unregisterDevice):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/GamepadsEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/GamepadsEfl.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "GamepadDeviceLinux.h"
 #include "GamepadList.h"
+#include "Logging.h"
 #include <Ecore.h>
 #include <Eeze.h>
 #include <Eina.h>
@@ -52,17 +53,20 @@ public:
     }
     ~GamepadDeviceEfl();
     void resetFdHandler() { m_fdHandler = 0; }
+    const String& deviceFile() const { return m_deviceFile; }
 
 private:
     GamepadDeviceEfl(const String& deviceFile);
     static Eina_Bool readCallback(void* userData, Ecore_Fd_Handler*);
 
     Ecore_Fd_Handler* m_fdHandler;
+    String m_deviceFile;
 };
 
 GamepadDeviceEfl::GamepadDeviceEfl(const String& deviceFile)
     : GamepadDeviceLinux(deviceFile)
     , m_fdHandler(0)
+    , m_deviceFile(deviceFile)
 {
     if (m_fileDescriptor < 0)
         return;
@@ -83,7 +87,7 @@ Eina_Bool GamepadDeviceEfl::readCallback(void* userData, Ecore_Fd_Handler* fdHan
     GamepadDeviceEfl* gamepadDevice = static_cast<GamepadDeviceEfl*>(userData);
 
     if (ecore_main_fd_handler_active_get(fdHandler, ECORE_FD_ERROR)) {
-        LOG_ERROR("An error occurred while watching the joystick file descriptor, aborting.");
+        LOG_ERROR("An error occurred while watching the joystick file descriptor at %s, aborting.", gamepadDevice->deviceFile().utf8().data());
         gamepadDevice->resetFdHandler();
         return ECORE_CALLBACK_CANCEL;
     }
@@ -93,12 +97,12 @@ Eina_Bool GamepadDeviceEfl::readCallback(void* userData, Ecore_Fd_Handler* fdHan
     const ssize_t len = read(fdDevice, &event, sizeof(event));
 
     if (len <= 0) {
-        LOG_ERROR("Failed to read joystick file descriptor, aborting.");
+        LOG_ERROR("Failed to read joystick file descriptor at %s, aborting.", gamepadDevice->deviceFile().utf8().data());
         gamepadDevice->resetFdHandler();
         return ECORE_CALLBACK_CANCEL;
     }
     if (len != sizeof(event)) {
-        LOG_ERROR("Wrong js_event size read on file descriptor, ignoring.");
+        LOG_ERROR("Wrong js_event size read on file descriptor at %s, ignoring.", gamepadDevice->deviceFile().utf8().data());
         return ECORE_CALLBACK_RENEW;
     }
 
@@ -180,10 +184,13 @@ void GamepadsEfl::registerDevice(const String& syspath)
     if (!deviceFile || !eina_str_has_prefix(deviceFile, joystickPrefix))
         return;
 
+    LOG(Gamepad, "Registering gamepad at %s", deviceFile);
+
     const size_t slotCount = m_slots.size();
     for (size_t index = 0; index < slotCount; ++index) {
         if (!m_slots[index]) {
             m_slots[index] = GamepadDeviceEfl::create(String::fromUTF8(deviceFile));
+            LOG(Gamepad, "Gamepad device name is %s", m_slots[index]->id().utf8().data());
             m_deviceMap.add(syspath, m_slots[index].get());
             break;
         }
@@ -196,6 +203,7 @@ void GamepadsEfl::unregisterDevice(const String& syspath)
         return;
 
     GamepadDeviceEfl* gamepadDevice = m_deviceMap.take(syspath);
+    LOG(Gamepad, "Unregistering gamepad at %s", gamepadDevice->deviceFile().utf8().data());
     const size_t index = m_slots.find(gamepadDevice);
     ASSERT(index != notFound);
 
