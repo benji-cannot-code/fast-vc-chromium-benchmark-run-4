d3a350c56744b962379376325e44baf18a9c4e7b
ronghuawu@chromium.org <ronghuawu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-09 19:15:04 +0000

    Fix a potential hang when trying to hook up the CaptureVideoDecoder with a camera that won't output any frame, for example a disabled camera. Add unittests for CaptureVideoDecoder and RTCVideoDecoder for this scenario.
    Similar fix has been done to the RTCVideoDecoder in http://codereview.chromium.org/10581044/.
    
    TEST=on a mac laptop with lid closed - can open the camera but there will be no frames.
    
    Review URL: https://chromiumcodereview.appspot.com/10661040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145710 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/capture_video_decoder.h
+++ content/renderer/media/capture_video_decoder.h
@@ -41,6 +41,7 @@ class CONTENT_EXPORT CaptureVideoDecoder
   virtual void Reset(const base::Closure& closure) OVERRIDE;
   virtual void Stop(const base::Closure& closure) OVERRIDE;
   virtual const gfx::Size& natural_size() OVERRIDE;
+  virtual void PrepareForShutdownHack() OVERRIDE;
 
   // VideoCapture::EventHandler implementation.
   virtual void OnStarted(media::VideoCapture* capture) OVERRIDE;
@@ -75,6 +76,7 @@ class CONTENT_EXPORT CaptureVideoDecoder
   void ReadOnDecoderThread(const ReadCB& read_cb);
   void ResetOnDecoderThread(const base::Closure& closure);
   void StopOnDecoderThread(const base::Closure& closure);
+  void PrepareForShutdownHackOnDecoderThread();
 
   void OnStoppedOnDecoderThread(media::VideoCapture* capture);
   void OnPausedOnDecoderThread(media::VideoCapture* capture);
@@ -94,6 +96,7 @@ class CONTENT_EXPORT CaptureVideoDecoder
   gfx::Size natural_size_;
   DecoderState state_;
   bool got_first_frame_;
+  bool shutting_down_;
   ReadCB read_cb_;
   base::Closure pending_stop_cb_;
   media::StatisticsCB statistics_cb_;
