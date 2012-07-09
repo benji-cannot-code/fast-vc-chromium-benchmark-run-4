b00bd2d31f0c72ac27846758a5da49734098fde0
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-09 08:36:51 +0000

    Unreviewed chromium inspector test fix.
    * src/js/Tests.js:
    (.TestSuite.prototype.nonAnonymousUISourceCodes_):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/js/Tests.js
+++ third_party/WebKit/Source/WebKit/chromium/src/js/Tests.js
@@ -650,7 +650,7 @@ TestSuite.prototype.nonAnonymousUISourceCodes_ = function()
         return !!uiSourceCode.url;
     }
 
-    var uiSourceCodes = WebInspector.panels.scripts._uiSourceCodeProvider.uiSourceCodes();
+    var uiSourceCodes = WebInspector.workspace.uiSourceCodes();
     return uiSourceCodes.filter(filterOutAnonymous);
 };
 
