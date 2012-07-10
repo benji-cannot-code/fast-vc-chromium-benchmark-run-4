1343fdb59734a93047b6cbbeadcd881eb33d49e5
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-10 07:38:58 +0000

    [Chromium-Mac] Form related layout tests cause timeout https://bugs.webkit.org/show_bug.cgi?id=90851
    Reviewed by Kent Tamura.
    
    This patch changed to pass a valid argument to LocaleMac
    constructor.
    
    No new tests. No more timeout in tests listed in the bug.
    
    * platform/text/mac/LocaleMac.mm:
    (WebCore::LocaleMac::currentLocale): Changed to pass local identifier string
    instead of NSLocale object.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/mac/LocaleMac.mm
+++ third_party/WebKit/Source/WebCore/platform/text/mac/LocaleMac.mm
@@ -72,7 +72,7 @@ PassOwnPtr<LocaleMac> LocaleMac::create(const String& localeIdentifier)
 
 LocaleMac* LocaleMac::currentLocale()
 {
-    static LocaleMac* currentLocale = LocaleMac::create([NSLocale currentLocale]).leakPtr();
+    static LocaleMac* currentLocale = LocaleMac::create([[NSLocale currentLocale] localeIdentifier]).leakPtr();
     return currentLocale;
 }
 
