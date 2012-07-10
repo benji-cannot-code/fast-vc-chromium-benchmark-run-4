2d5a7b2e0ce15b3ec326911f06d51149bccc7589
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 22:36:17 +0000

    Get the audio proxy building as untrusted code for NaCl. BUG=116317 TEST=compiles Review URL: https://chromiumcodereview.appspot.com/10694115
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145971 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/resource_creation_proxy.cc
+++ ppapi/proxy/resource_creation_proxy.cc
@@ -169,6 +169,11 @@ PP_Resource ResourceCreationProxy::CreateAudio(
                                               audio_callback, user_data);
 }
 
+PP_Resource ResourceCreationProxy::CreateAudioTrusted(PP_Instance instance) {
+  // Proxied plugins can't create trusted audio devices.
+  return 0;
+}
+
 PP_Resource ResourceCreationProxy::CreateAudioConfig(
     PP_Instance instance,
     PP_AudioSampleRate sample_rate,
@@ -193,11 +198,6 @@ PP_Resource ResourceCreationProxy::CreateGraphics2D(PP_Instance instance,
 }
 
 #if !defined(OS_NACL)
-PP_Resource ResourceCreationProxy::CreateAudioTrusted(PP_Instance instance) {
-  // Proxied plugins can't create trusted audio devices.
-  return 0;
-}
-
 PP_Resource ResourceCreationProxy::CreateAudioInput0_1(
     PP_Instance instance,
     PP_Resource config_id,
