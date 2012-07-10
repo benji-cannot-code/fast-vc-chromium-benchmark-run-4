c17b80443da9bfd868bb83814519f43018bde4f4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-10 02:29:15 +0000

    [chromium] Merge updates and idle updates into one pass https://bugs.webkit.org/show_bug.cgi?id=90324
    Patch by Eric Penner <epenner@google.com> on 2012-07-09
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    We no longer need to do painting is passes in order
    to allocate memory in the right order. So this merges
    updates and idle updates into one pass.
    
    Covered by existing unit tests (some of which are
    updated with this patch).
    
    * platform/graphics/chromium/ContentLayerChromium.cpp:
    (WebCore::ContentLayerChromium::needMoreUpdates):
    * platform/graphics/chromium/ContentLayerChromium.h:
    (ContentLayerChromium):
    * platform/graphics/chromium/LayerChromium.h:
    (WebCore::LayerChromium::needMoreUpdates):
    * platform/graphics/chromium/TiledLayerChromium.cpp:
    (WebCore::TiledLayerChromium::updateTiles):
    (WebCore::TiledLayerChromium::updateLayerRect):
    * platform/graphics/chromium/TiledLayerChromium.h:
    (TiledLayerChromium):
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::CCLayerTreeHost):
    (WebCore::CCLayerTreeHost::compositeAndReadback):
    (WebCore::CCLayerTreeHost::updateLayers):
    (WebCore::CCLayerTreeHost::paintMasksForRenderSurface):
    (WebCore::CCLayerTreeHost::paintLayerContents):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    (CCLayerTreeHost):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostTest.cpp:
    (WTF::ContentLayerChromiumWithUpdateTracking::resetPaintContentsCount):
    (WTF::ContentLayerChromiumWithUpdateTracking::ContentLayerChromiumWithUpdateTracking):
    (ContentLayerChromiumWithUpdateTracking):
    (WTF::CCLayerTreeHostTestOpacityChange::afterTest):
    * tests/CCTiledLayerTestCommon.h:
    (FakeTiledLayerChromium):
    * tests/TiledLayerChromiumTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/ContentLayerChromium.h
@@ -79,7 +79,7 @@ public:
     virtual bool drawsContent() const OVERRIDE;
     virtual void setTexturePriorities(const CCPriorityCalculator&) OVERRIDE;
     virtual void update(CCTextureUpdater&, const CCOcclusionTracker*) OVERRIDE;
-    virtual void idleUpdate(CCTextureUpdater&, const CCOcclusionTracker*) OVERRIDE;
+    virtual bool needMoreUpdates() OVERRIDE;
 
     virtual void setOpaque(bool) OVERRIDE;
 
