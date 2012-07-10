51b4dedb366b842b84973f68754e42b88f57ff27
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 22:15:17 +0000

    Fix incorrect usage of BrowserContext on the FILE thread. Review URL: https://chromiumcodereview.appspot.com/10701128
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_private_api.h
+++ chrome/browser/chromeos/extensions/file_browser_private_api.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <queue>
 #include <vector>
 
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/platform_file.h"
 #include "chrome/browser/chromeos/extensions/file_browser_event_router.h"
@@ -21,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/url_util.h"
 
 class GURL;
+
+namespace fileapi {
+class FileSystemContext;
+}
 
 namespace ui {
 struct SelectedFileInfo;
@@ -175,8 +180,10 @@ class FileBrowserFunction
   // Converts virtual paths to local paths and call |callback| (on the UI
   // thread) with the results.
   // This method must be called from the file thread.
-  void GetLocalPathsOnFileThread(const UrlList& file_urls,
-                                 GetLocalPathsCallback callback);
+  void GetLocalPathsOnFileThread(
+      scoped_refptr<fileapi::FileSystemContext> file_system_context,
+      const UrlList& file_urls,
+      GetLocalPathsCallback callback);
 };
 
 // Select a single file.  Closes the dialog window.
