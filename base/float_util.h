5ba5f1aaabc5185821384f5c85c7da2c551f7a4a
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 14:11:38 +0000

    Use isfinite instead of finite on Mac
    According to math.h in the 10.6 SDK, finite is deprecated in favor of isfinite, and finite isn't available on iOS.
    
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10704126
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145876 0039d316-1c4b-4281-b951-d872f2087c98

--- base/float_util.h
+++ base/float_util.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2006-2008 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -19,7 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 
 inline bool IsFinite(const double& number) {
-#if defined(OS_POSIX)
+#if defined(OS_MACOSX)
+  // C99 says isfinite() replaced finite(), and iOS does not provide the
+  // older call.
+  return isfinite(number) != 0;
+#elif defined(OS_POSIX)
   return finite(number) != 0;
 #elif defined(OS_WIN)
   return _finite(number) != 0;
