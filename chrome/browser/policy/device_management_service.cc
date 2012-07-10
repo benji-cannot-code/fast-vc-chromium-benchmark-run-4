3b0ffe22a0d04c551e5fce05fe49c63d7a54fd71
pastarmovj@chromium.org <pastarmovj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 23:17:17 +0000

    Remove legacy DM Server http error codes.
    BUG=83199
    TEST=still compiling.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10748008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145983 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/device_management_service.cc
+++ chrome/browser/policy/device_management_service.cc
@@ -69,11 +69,6 @@ const int kInternalServerError = 500;
 const int kServiceUnavailable = 503;
 const int kPolicyNotFound = 902; // This error is not sent as HTTP status code.
 
-// TODO(pastarmovj): Legacy error codes are here for compatibility only. They
-// should be removed once the DM Server has been updated.
-const int kPendingApprovalLegacy = 491;
-const int kDeviceNotFoundLegacy = 901;
-
 #if defined(OS_CHROMEOS)
 // Machine info keys.
 const char kMachineInfoHWClass[] = "hardware_class";
@@ -346,7 +341,6 @@ void DeviceManagementRequestJobImpl::HandleResponse(
     case kDeviceManagementNotAllowed:
       ReportError(DM_STATUS_SERVICE_MANAGEMENT_NOT_SUPPORTED);
       return;
-    case kPendingApprovalLegacy:
     case kPendingApproval:
       ReportError(DM_STATUS_SERVICE_ACTIVATION_PENDING);
       return;
@@ -355,7 +349,6 @@ void DeviceManagementRequestJobImpl::HandleResponse(
     case kServiceUnavailable:
       ReportError(DM_STATUS_TEMPORARY_UNAVAILABLE);
       return;
-    case kDeviceNotFoundLegacy:
     case kDeviceNotFound:
       ReportError(DM_STATUS_SERVICE_DEVICE_NOT_FOUND);
       return;
