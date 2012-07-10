35553734349f3b911ee612c1a38a21ad64d99f70
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-10 20:30:01 +0000

    [Chromium-Android] Use default font rendering styles in FontPlatformData::querySystemForRenderStyle() https://bugs.webkit.org/show_bug.cgi?id=90826
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    No new tests. The change to this file is refactoring only.
    
    * platform/graphics/harfbuzz/FontPlatformDataHarfBuzz.cpp:
    (WebCore::FontPlatformData::querySystemForRenderStyle): Moved the "NoPreference" initialization into PlatformSupport::PlatformSupport::getRenderStyleForStrike().
    
    Source/WebKit/chromium:
    
    * WebKit.gyp:
    * src/PlatformSupport.cpp:
    (WebCore::PlatformSupport::getRenderStyleForStrike): Call style.setDefault() on Android or when the font is not specified.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/PlatformSupport.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/PlatformSupport.cpp
@@ -69,13 +69,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if OS(DARWIN)
 #include <public/mac/WebThemeEngine.h>
-#elif OS(UNIX) && !OS(ANDROID)
-#include "WebFontInfo.h"
+#elif OS(UNIX)
 #include "WebFontRenderStyle.h"
-#include <public/linux/WebThemeEngine.h>
-#elif OS(ANDROID)
+#if OS(ANDROID)
 #include <public/android/WebThemeEngine.h>
-#endif
+#else
+#include "WebFontInfo.h"
+#include <public/linux/WebThemeEngine.h>
+#endif // OS(ANDROID)
+#endif // elif OS(UNIX)
 
 #include "NativeImageSkia.h"
 
@@ -258,16 +260,20 @@ void PlatformSupport::getFontFamilyForCharacters(const UChar* characters, size_t
 
 void PlatformSupport::getRenderStyleForStrike(const char* font, int sizeAndStyle, FontRenderStyle* result)
 {
-#if !OS(ANDROID)
     WebFontRenderStyle style;
 
-    if (WebKit::Platform::current()->sandboxSupport())
+#if OS(ANDROID)
+    style.setDefaults();
+#else
+    if (!font || !*font)
+        style.setDefaults(); // It's probably a webfont. Take the system defaults.
+    else if (WebKit::Platform::current()->sandboxSupport())
         WebKit::Platform::current()->sandboxSupport()->getRenderStyleForStrike(font, sizeAndStyle, &style);
     else
         WebFontInfo::renderStyleForStrike(font, sizeAndStyle, &style);
+#endif
 
     style.toFontRenderStyle(result);
-#endif
 }
 #endif
 
