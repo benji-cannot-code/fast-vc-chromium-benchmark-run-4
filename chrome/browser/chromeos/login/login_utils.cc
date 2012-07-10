c1b214e830c55be0a1d1b6fb1ed2bd7f51cf222f
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-10 22:35:49 +0000

    Removing last usage of default request context. Fixing up login_utils to not need it by adding a notification so that login_utils only accesses it when the request context getter has been initialized.
    BUG=125292, 127693
    TEST=unittest, manual
    
    Review URL: https://chromiumcodereview.appspot.com/10657009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@145969 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -74,6 +74,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/session_manager_client.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/browser/notification_observer.h"
 #include "googleurl/src/gurl.h"
 #include "media/base/media_switches.h"
 #include "net/base/network_change_notifier.h"
@@ -227,6 +228,7 @@ class LoginUtilsImpl
       public OAuth1TokenFetcher::Delegate,
       public OAuthLoginVerifier::Delegate,
       public net::NetworkChangeNotifier::ConnectionTypeObserver,
+      public content::NotificationObserver,
       public base::SupportsWeakPtr<LoginUtilsImpl> {
  public:
   LoginUtilsImpl()
@@ -235,8 +237,13 @@ class LoginUtilsImpl
         has_cookies_(false),
         delegate_(NULL),
         job_restart_request_(NULL),
-        should_restore_auth_session_(false) {
+        should_restore_auth_session_(false),
+        url_request_context_getter_(NULL) {
     net::NetworkChangeNotifier::AddConnectionTypeObserver(this);
+    registrar_.Add(
+        this,
+        chrome::NOTIFICATION_PROFILE_URL_REQUEST_CONTEXT_GETTER_INITIALIZED,
+        content::Source<Profile>(ProfileManager::GetDefaultProfile()));
   }
 
   virtual ~LoginUtilsImpl() {
@@ -286,6 +293,11 @@ class LoginUtilsImpl
   // net::NetworkChangeNotifier::ConnectionTypeObserver overrides.
   virtual void OnConnectionTypeChanged(
       net::NetworkChangeNotifier::ConnectionType type) OVERRIDE;
+
+  // content::NotificationObserver overrides.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
  protected:
   virtual std::string GetOffTheRecordCommandLine(
@@ -354,6 +366,12 @@ class LoginUtilsImpl
   // True if should restore authentication session when notified about
   // online state change.
   bool should_restore_auth_session_;
+
+  content::NotificationRegistrar registrar_;
+
+  // This is set via a notification after the profile has initialized the
+  // getter.
+  net::URLRequestContextGetter* url_request_context_getter_;
 
   DISALLOW_COPY_AND_ASSIGN(LoginUtilsImpl);
 };
@@ -863,9 +881,11 @@ scoped_refptr<Authenticator> LoginUtilsImpl::CreateAuthenticator(
 // We use a special class for this so that it can be safely leaked if we
 // never connect. At shutdown the order is not well defined, and it's possible
 // for the infrastructure needed to unregister might be unstable and crash.
-class WarmingObserver : public NetworkLibrary::NetworkManagerObserver {
+class WarmingObserver : public NetworkLibrary::NetworkManagerObserver,
+                        public content::NotificationObserver {
  public:
-  WarmingObserver() {
+  WarmingObserver()
+      : url_request_context_getter_(NULL) {
     NetworkLibrary *netlib = CrosLibrary::Get()->GetNetworkLibrary();
     netlib->AddNetworkManagerObserver(this);
   }
@@ -880,11 +900,28 @@ class WarmingObserver : public NetworkLibrary::NetworkManagerObserver {
           GURL(GaiaUrls::GetInstance()->client_login_url()),
           chrome_browser_net::UrlInfo::EARLY_LOAD_MOTIVATED,
           kConnectionsNeeded,
-          make_scoped_refptr(Profile::GetDefaultRequestContextDeprecated()));
+          url_request_context_getter_);
       netlib->RemoveNetworkManagerObserver(this);
       delete this;
     }
   }
+
+  // content::NotificationObserver overrides.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) {
+  switch (type) {
+    case chrome::NOTIFICATION_PROFILE_URL_REQUEST_CONTEXT_GETTER_INITIALIZED: {
+      Profile* profile = content::Source<Profile>(source).ptr();
+      url_request_context_getter_ = profile->GetRequestContext();
+      break;
+    }
+    default:
+      NOTREACHED();
+  }
+}
+ private:
+  net::URLRequestContextGetter* url_request_context_getter_;
 };
 
 void LoginUtilsImpl::PrewarmAuthentication() {
@@ -895,7 +932,7 @@ void LoginUtilsImpl::PrewarmAuthentication() {
         GURL(GaiaUrls::GetInstance()->client_login_url()),
         chrome_browser_net::UrlInfo::EARLY_LOAD_MOTIVATED,
         kConnectionsNeeded,
-        make_scoped_refptr(Profile::GetDefaultRequestContextDeprecated()));
+        url_request_context_getter_);
   } else {
     new WarmingObserver();
   }
@@ -1106,6 +1143,20 @@ void LoginUtilsImpl::OnConnectionTypeChanged(
       Profile* user_profile = ProfileManager::GetDefaultProfile();
       KickStartAuthentication(user_profile);
     }
+  }
+}
+
+void LoginUtilsImpl::Observe(int type,
+                             const content::NotificationSource& source,
+                             const content::NotificationDetails& details) {
+  switch (type) {
+    case chrome::NOTIFICATION_PROFILE_URL_REQUEST_CONTEXT_GETTER_INITIALIZED: {
+      Profile* profile = content::Source<Profile>(source).ptr();
+      url_request_context_getter_ = profile->GetRequestContext();
+      break;
+    }
+    default:
+      NOTREACHED();
   }
 }
 
