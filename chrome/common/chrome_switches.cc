f7f7beedeeb147dd1f77a47295ba63411a64b4a9
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 02:25:48 +0000

    Add --enable-drive-api for Chrome OS
    Use this flag for the period of migration from Document List API
    to Drive v2 API for file browser / filesystem APIs etc.
    Code for using this flag will follow separately.
    
    BUG=chromium:127728
    TEST=compile
    
    Review URL: https://chromiumcodereview.appspot.com/10753024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146033 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1333,6 +1333,9 @@ const char kSkipOAuthLogin[]                = "skip-oauth-login";
 // Enables device policy support on ChromeOS.
 const char kEnableDevicePolicy[]            = "enable-device-policy";
 
+// Enables Drive v2 API instead of Google Documents List API.
+const char kEnableDriveV2Api[]              = "enable-drive-v2-api";
+
 // Enables the redirection of viewable document requests to the Google Document
 // Viewer.
 const char kEnableGView[]                   = "enable-gview";
