fa653637181e71a868a4a983848d477d02ad1828
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 01:57:44 +0000

    Protect references to ThemeService* with ENABLE_THEMES
    On android, ENABLE_THEMES and ENABLE_PROTECTOR_SERVICE are not defined.
    This fixes some link errors for unit_tests.
    
    BUG=117407
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10704148
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146028 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -415,7 +415,9 @@ void ExtensionServiceTestBase::InitializeExtensionService(
   chrome::RegisterUserPrefs(prefs);
   profile->SetPrefService(prefs);
 
+#if defined(ENABLE_THEMES)
   ThemeServiceFactory::GetInstance()->ForceRegisterPrefsForTest(prefs);
+#endif
   PluginPrefsFactory::GetInstance()->ForceRegisterPrefsForTest(prefs);
 
   profile_.reset(profile);
