555f526af7a3a4c4db26e8faf5f98c8ab725771b
rjkroege@chromium.org <rjkroege@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-11 23:01:58 +0000

    [chromium] [regression] Don't use ScrollByPrecisePixels on Chromium Mac. https://bugs.webkit.org/show_bug.cgi?id=91020
    Reviewed by Adam Barth.
    
    A change in https://bugs.webkit.org/show_bug.cgi?id=87535 to
    improve the operation of smooth scrolling incorrectly caused
    Chromium Mac to use the wrong scroll granularity on
    hasPreciseScrollingDeltas() == true wheelevent scrolls.
    Exclude the change on the Chromium Mac platform.
    
    * platform/ScrollAnimator.cpp:
    (WebCore::ScrollAnimator::handleWheelEvent): Adjusted #ifdef to exclude Chromium
    Mac from ScrollByPrecisePixels change.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122382 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ScrollAnimator.cpp
+++ third_party/WebKit/Source/WebCore/platform/ScrollAnimator.cpp
@@ -92,7 +92,7 @@ bool ScrollAnimator::handleWheelEvent(const PlatformWheelEvent& e)
 
     bool handled = false;
 
-#if PLATFORM(CHROMIUM)
+#if PLATFORM(CHROMIUM) && !OS(DARWIN)
     ScrollGranularity granularity = e.hasPreciseScrollingDeltas() ? ScrollByPrecisePixel : ScrollByPixel;
 #else
     ScrollGranularity granularity = ScrollByPixel;
