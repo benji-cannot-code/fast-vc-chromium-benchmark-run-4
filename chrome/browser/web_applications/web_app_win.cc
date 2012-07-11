781bf9d55fb8a3b4263b022034aee32b6268999e
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 09:50:47 +0000

    Remove app shortcuts when app is uninstalled on Linux.
    To support this, shortcut creation on Linux for extensions has been modified so that the filename encodes the extension ID and the profile. Also, when creating shortcuts any existing shortcuts are removed first.
    
    Web page shortcuts are not affected.
    
    BUG=130456
    TEST=Test uninstalling apps removes their shortcuts; test uninstalling
    apps is not broken in any way; test shortcuts for web apps
    are not broken in any way.
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=146065
    
    Review URL: https://chromiumcodereview.appspot.com/10698114
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146094 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_win.cc
+++ chrome/browser/web_applications/web_app_win.cc
@@ -256,6 +256,11 @@ bool CreatePlatformShortcut(
   return success;
 }
 
+void DeletePlatformShortcuts(const FilePath& profile_path,
+                             const std::string& extension_id) {
+  // TODO(benwells): Implement this.
+}
+
 }  // namespace internals
 
 }  // namespace web_app
