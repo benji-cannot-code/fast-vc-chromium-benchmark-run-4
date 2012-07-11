0766ea7054f7bdee172621dbaf094e68ab6b6c49
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-11 20:14:28 +0000

    [chromium] Minimum size used for occlusion tracking should be a setting on CCLayerTreeHost https://bugs.webkit.org/show_bug.cgi?id=90993
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    Move the default minimum size used for occlusion tracking from the
    CCOcclusionTracker class into CCLayerTreeSettings. This value is then
    used on both threads as the lower limit for any occlusion to be
    remembered.
    
    This allows us to use (0, 0) as the minimum size for tests, allowing all
    occlusion to be tracked.
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::paintLayerContents):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    (WebCore::CCLayerTreeSettings::CCLayerTreeSettings):
    (CCLayerTreeSettings):
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::calculateRenderPasses):
    * platform/graphics/chromium/cc/CCOcclusionTracker.h:
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostImplTest.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostImplTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostImplTest.cpp
@@ -71,6 +71,8 @@ public:
         , m_didRequestRedraw(false)
     {
         CCLayerTreeSettings settings;
+        settings.minimumOcclusionTrackingSize = IntSize();
+
         m_hostImpl = CCLayerTreeHostImpl::create(settings, this);
         m_hostImpl->initializeLayerRenderer(createContext(), UnthrottledUploader);
         m_hostImpl->setViewportSize(IntSize(10, 10));
@@ -87,6 +89,8 @@ public:
         CCSettings::setPartialSwapEnabled(partialSwap);
 
         CCLayerTreeSettings settings;
+        settings.minimumOcclusionTrackingSize = IntSize();
+
         OwnPtr<CCLayerTreeHostImpl> myHostImpl = CCLayerTreeHostImpl::create(settings, this);
 
         myHostImpl->initializeLayerRenderer(graphicsContext, UnthrottledUploader);
@@ -685,7 +689,6 @@ TEST_F(CCLayerTreeHostImplTest, didDrawNotCalledOnHiddenLayer)
 
 TEST_F(CCLayerTreeHostImplTest, willDrawNotCalledOnOccludedLayer)
 {
-    // Make the viewport large so that we can have large layers that get considered for occlusion (small layers do not).
     IntSize bigSize(1000, 1000);
     m_hostImpl->setViewportSize(bigSize);
 
@@ -1185,10 +1188,8 @@ private:
     IntRect m_quadVisibleRect;
 };
 
-// https://bugs.webkit.org/show_bug.cgi?id=75783
 TEST_F(CCLayerTreeHostImplTest, blendingOffWhenDrawingOpaqueLayers)
 {
-
     {
         OwnPtr<CCLayerImpl> root = CCLayerImpl::create(1);
         root->setAnchorPoint(FloatPoint(0, 0));
@@ -1201,6 +1202,7 @@ TEST_F(CCLayerTreeHostImplTest, blendingOffWhenDrawingOpaqueLayers)
 
     root->addChild(BlendStateCheckLayer::create(2));
     BlendStateCheckLayer* layer1 = static_cast<BlendStateCheckLayer*>(root->children()[0].get());
+    layer1->setPosition(FloatPoint(2, 2));
 
     CCLayerTreeHostImpl::FrameData frame;
 
@@ -1253,6 +1255,7 @@ TEST_F(CCLayerTreeHostImplTest, blendingOffWhenDrawingOpaqueLayers)
 
     layer1->addChild(BlendStateCheckLayer::create(3));
     BlendStateCheckLayer* layer2 = static_cast<BlendStateCheckLayer*>(layer1->children()[0].get());
+    layer2->setPosition(FloatPoint(4, 4));
 
     // 2 opaque layers, drawn without blending.
     layer1->setOpaque(true);
