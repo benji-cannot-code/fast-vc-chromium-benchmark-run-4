645deb2f877ca6e48b3533dd9a393f62b35d23a0
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 19:28:48 +0000

    Adding a field trial to enable speculative resource prefetching learning.
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10752006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146170 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/predictors/resource_prefetch_predictor.cc
+++ chrome/browser/predictors/resource_prefetch_predictor.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/url_database.h"
 #include "chrome/browser/predictors/predictor_database.h"
 #include "chrome/browser/predictors/predictor_database_factory.h"
+#include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
@@ -115,10 +116,8 @@ ResourcePrefetchPredictor::~ResourcePrefetchPredictor() {
 }
 
 // static
-bool ResourcePrefetchPredictor::IsEnabled() {
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  return command_line->HasSwitch(
-      switches::kEnableSpeculativeResourcePrefetching);
+bool ResourcePrefetchPredictor::IsEnabled(Profile* profile) {
+  return prerender::IsSpeculativeResourcePrefetchingLearningEnabled(profile);
 }
 
 void ResourcePrefetchPredictor::LazilyInitialize() {
