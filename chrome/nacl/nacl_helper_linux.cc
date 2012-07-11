08ac77fb40004f1c788accb626bc7499b32efd38
halyavin@google.com <halyavin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-11 11:33:19 +0000

    Better error reporting in nacl helper.
    BUG= https://code.google.com/p/chromium/issues/detail?id=136793
    TEST= none
    
    Review URL: https://chromiumcodereview.appspot.com/10695077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146103 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_helper_linux.cc
+++ chrome/nacl/nacl_helper_linux.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/eintr_wrapper.h"
 #include "base/global_descriptors_posix.h"
+#include "base/json/string_escape.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
 #include "base/posix/unix_domain_socket.h"
@@ -236,9 +237,11 @@ int main(int argc, char *argv[]) {
     if (msglen == 0 || (msglen == -1 && errno == ECONNRESET)) {
       // EOF from the browser. Goodbye!
       _exit(0);
-    }
-    if (msglen == sizeof(kNaClForkRequest) - 1 &&
-        memcmp(buf, kNaClForkRequest, msglen) == 0) {
+    } else if (msglen < 0) {
+      LOG(ERROR) << "nacl_helper: receive from zygote failed, errno = "
+                 << errno;
+    } else if (msglen == sizeof(kNaClForkRequest) - 1 &&
+               memcmp(buf, kNaClForkRequest, msglen) == 0) {
       if (kNaClParentFDIndex + 1 == fds.size()) {
         HandleForkRequest(fds, prereserved_sandbox_size);
         continue;  // fork succeeded. Note: child does not return
@@ -247,10 +250,9 @@ int main(int argc, char *argv[]) {
                    << fds.size();
       }
     } else {
-      if (msglen != 0) {
-        LOG(ERROR) << "nacl_helper unrecognized request: %s";
-        _exit(-1);
-      }
+      LOG(ERROR) << "nacl_helper unrecognized request: "
+                 << base::GetDoubleQuotedJson(std::string(buf, buf + msglen));
+      _exit(-1);
     }
     // if fork fails, send PID=-1 to zygote
     if (!UnixDomainSocket::SendMsg(kNaClZygoteDescriptor, &badpid,
