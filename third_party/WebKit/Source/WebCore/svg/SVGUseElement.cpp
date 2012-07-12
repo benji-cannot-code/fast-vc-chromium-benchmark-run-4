aeae258bb5a85230509af47d73d8086e9dc9ca85
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 12:26:03 +0000

    Incorrect handling of chained pending resources in SVGUseElement https://bugs.webkit.org/show_bug.cgi?id=89686
    Reviewed by Nikolas Zimmermann.
    
    Source/WebCore:
    
    Currently SVGUseElement builds the shadow tree when the target first
    becomes available. This is normally OK, but if the target itself (or
    one of its children) is a <use> element with pending resources, then
    the shadow expansion only captures the current state of the tree and
    never gets updated when the pending resource becomes available.
    
    In order to support arbitrary <use>-on-<use> constructs, this patch
    tracks nested <use> dependencies and rebuilds the dependent trees
    whenever the target gets updated.
    
    Tests: svg/custom/use-nested-expected.svg
           svg/custom/use-nested.svg
    
    * svg/SVGElement.cpp:
    (WebCore::SVGElement::removedFrom): removedFrom needs to be called up the inheritance chain
    before invoking removeAllElementReferencesForTarget. Otherwise we could end up finding the
    element being removed as a valid target in SVGUseElement::buildInstanceTree because its
    InDocument flag is not cleared yet.
    * svg/SVGUseElement.cpp:
    (WebCore::SVGUseElement::~SVGUseElement):
    (WebCore::SVGUseElement::clearResourceReferences):
    (WebCore::SVGUseElement::buildPendingResource):
    (WebCore::SVGUseElement::buildShadowAndInstanceTree):
    (WebCore::SVGUseElement::buildInstanceTree):
    * svg/SVGUseElement.h:
    (SVGUseElement):
    Track <use> -> <use> dependencies using SVGDocumentExtensions'
    m_elementDependencies framework and ensure dependent trees are rebuilt
    when the target itself gets rebuilt.
    
    LayoutTests:
    
    Added reftest to verify that nested/indirect <use> constructs are
    handled correctly WRT pending resource tracking.
    
    Also updated the svg/hixie/error/017-expected.txt to match the current
    results. Per comments in SVGUseElement::buildShadowAndInstanceTree, the
    new behavior is correct: <use> cycles are to be ignored.
    
    * platform/chromium-win/svg/hixie/error/017-expected.txt:
    * platform/efl/svg/hixie/error/017-expected.txt:
    * platform/gtk/svg/hixie/error/017-expected.txt:
    * platform/mac/svg/hixie/error/017-expected.txt:
    * platform/qt/svg/hixie/error/017-expected.txt:
    * svg/custom/use-nested-expected.svg: Added.
    * svg/custom/use-nested.svg: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGUseElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGUseElement.cpp
@@ -108,6 +108,8 @@ SVGUseElement::~SVGUseElement()
 {
     if (m_cachedDocument)
         m_cachedDocument->removeClient(this);
+
+    clearResourceReferences();
 }
 
 void SVGUseElement::createShadowSubtree()
@@ -408,6 +410,9 @@ void SVGUseElement::clearResourceReferences()
     }
 
     m_needsShadowTreeRecreation = false;
+
+    ASSERT(document());
+    document()->accessSVGExtensions()->removeAllTargetReferencesForElement(this);
 }
 
 void SVGUseElement::buildPendingResource()
@@ -415,12 +420,12 @@ void SVGUseElement::buildPendingResource()
     if (!referencedDocument())
         return;
     clearResourceReferences();
-    if (!inDocument())
+    if (!inDocument() || isInShadowTree())
         return;
 
     String id;
     Element* target = SVGURIReference::targetElementFromIRIString(href(), document(), &id, externalDocument());
-    if (!target) {
+    if (!target || !target->inDocument()) {
         // If we can't find the target of an external element, just give up.
         // We can't observe if the target somewhen enters the external document, nor should we do it.
         if (externalDocument())
@@ -466,7 +471,7 @@ void SVGUseElement::buildShadowAndInstanceTree(SVGElement* target)
 
     // Eventually enter recursion to build SVGElementInstance objects for the sub-tree children
     bool foundProblem = false;
-    buildInstanceTree(target, m_targetElementInstance.get(), foundProblem);
+    buildInstanceTree(target, m_targetElementInstance.get(), foundProblem, false);
 
     if (instanceTreeIsLoading(m_targetElementInstance.get()))
         return;
@@ -519,6 +524,10 @@ void SVGUseElement::buildShadowAndInstanceTree(SVGElement* target)
 
     // Update relative length information.
     updateRelativeLengthsInformation();
+
+    // Rebuild all dependent use elements.
+    ASSERT(document());
+    document()->accessSVGExtensions()->removeAllElementReferencesForTarget(this);
 
     // Eventually dump instance tree
 #ifdef DUMP_INSTANCE_TREE
@@ -587,7 +596,7 @@ RenderObject* SVGUseElement::rendererClipChild() const
     return 0;
 }
 
-void SVGUseElement::buildInstanceTree(SVGElement* target, SVGElementInstance* targetInstance, bool& foundProblem)
+void SVGUseElement::buildInstanceTree(SVGElement* target, SVGElementInstance* targetInstance, bool& foundProblem, bool foundUse)
 {
     ASSERT(target);
     ASSERT(targetInstance);
@@ -600,6 +609,14 @@ void SVGUseElement::buildInstanceTree(SVGElement* target, SVGElementInstance* ta
         foundProblem = hasCycleUseReferencing(static_cast<SVGUseElement*>(target), targetInstance, newTarget);
         if (foundProblem)
             return;
+
+        // We only need to track fist degree <use> dependencies. Indirect references are handled
+        // as the invalidation bubbles up the dependency chain.
+        if (!foundUse) {
+            ASSERT(document());
+            document()->accessSVGExtensions()->addElementReferencingTarget(this, target);
+            foundUse = true;
+        }
     } else if (isDisallowedElement(target)) {
         foundProblem = true;
         return;
@@ -627,7 +644,7 @@ void SVGUseElement::buildInstanceTree(SVGElement* target, SVGElementInstance* ta
         targetInstance->appendChild(instance.release());
 
         // Enter recursion, appending new instance tree nodes to the "instance" object.
-        buildInstanceTree(element, instancePtr, foundProblem);
+        buildInstanceTree(element, instancePtr, foundProblem, foundUse);
         if (foundProblem)
             return;
     }
@@ -638,7 +655,7 @@ void SVGUseElement::buildInstanceTree(SVGElement* target, SVGElementInstance* ta
     RefPtr<SVGElementInstance> newInstance = SVGElementInstance::create(this, static_cast<SVGUseElement*>(target), newTarget);
     SVGElementInstance* newInstancePtr = newInstance.get();
     targetInstance->appendChild(newInstance.release());
-    buildInstanceTree(newTarget, newInstancePtr, foundProblem);
+    buildInstanceTree(newTarget, newInstancePtr, foundProblem, foundUse);
 }
 
 bool SVGUseElement::hasCycleUseReferencing(SVGUseElement* use, SVGElementInstance* targetInstance, SVGElement*& newTarget)
--- third_party/WebKit/Source/WebCore/svg/SVGUseElement.h
+++ third_party/WebKit/Source/WebCore/svg/SVGUseElement.h
@@ -85,7 +85,7 @@ private:
     virtual bool selfHasRelativeLengths() const;
 
     // Instance tree handling
-    void buildInstanceTree(SVGElement* target, SVGElementInstance* targetInstance, bool& foundCycle);
+    void buildInstanceTree(SVGElement* target, SVGElementInstance* targetInstance, bool& foundCycle, bool foundUse);
     bool hasCycleUseReferencing(SVGUseElement*, SVGElementInstance* targetInstance, SVGElement*& newTarget);
 
     // Shadow tree handling
