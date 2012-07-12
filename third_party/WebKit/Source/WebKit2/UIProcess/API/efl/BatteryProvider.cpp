9c2528f23bd4ccee19fc250e9ae078acd0a80cbb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 13:44:25 +0000

    [EFL][WK2] Browser crashes running BatteryStatus tests. https://bugs.webkit.org/show_bug.cgi?id=91065
    Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-07-12
    Reviewed by Kentaro Hara.
    
    clientInfo was incorrectly casted to BatteryProviderEfl in helper
    function. This patch fixes the issue.
    
    * UIProcess/API/efl/BatteryProvider.cpp:
    (toBatteryProvider):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/BatteryProvider.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/BatteryProvider.cpp
@@ -36,9 +36,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace WebCore;
 using namespace WebKit;
 
-static inline BatteryProviderEfl* toBatteryProvider(const void* clientInfo)
+static inline BatteryProvider* toBatteryProvider(const void* clientInfo)
 {
-    return static_cast<BatteryProviderEfl*>(const_cast<void*>(clientInfo));
+    return static_cast<BatteryProvider*>(const_cast<void*>(clientInfo));
 }
 
 static void startUpdatingCallback(WKBatteryManagerRef batteryManager, const void* clientInfo)
