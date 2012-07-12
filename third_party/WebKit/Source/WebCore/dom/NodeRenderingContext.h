2d086dd82ac74e21c9aa1ff7a55758a66333b2cd
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 11:54:26 +0000

    Typo: ParentTranversalDetails should be ParentTraversalDetails https://bugs.webkit.org/show_bug.cgi?id=91059
    Reviewed by Andreas Kling.
    
    Did a bare rename from ParentTranversalDetails to ParentTraversalDetails
    
    No new tests, just a rename.
    
    * dom/ComposedShadowTreeWalker.cpp:
    (WebCore::ComposedShadowTreeWalker::ParentTraversalDetails::didTraverseInsertionPoint):
    (WebCore::ComposedShadowTreeWalker::ParentTraversalDetails::didTraverseShadowRoot):
    (WebCore::ComposedShadowTreeWalker::ParentTraversalDetails::didFindNode):
    (WebCore::ComposedShadowTreeWalker::findParent):
    (WebCore::ComposedShadowTreeWalker::traverseNodeEscapingFallbackContents):
    (WebCore::ComposedShadowTreeWalker::traverseParent):
    (WebCore::ComposedShadowTreeWalker::traverseParentInCurrentTree):
    (WebCore::ComposedShadowTreeWalker::traverseParentBackToYoungerShadowRootOrHost):
    * dom/ComposedShadowTreeWalker.h:
    (WebCore::ComposedShadowTreeWalker::ParentTraversalDetails::ParentTraversalDetails):
    (ComposedShadowTreeWalker):
    * dom/NodeRenderingContext.h:
    (NodeRenderingContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/NodeRenderingContext.h
+++ third_party/WebKit/Source/WebCore/dom/NodeRenderingContext.h
@@ -71,7 +71,7 @@ public:
 
 private:
     Node* m_node;
-    ComposedShadowTreeWalker::ParentTranversalDetails m_parentDetails;
+    ComposedShadowTreeWalker::ParentTraversalDetails m_parentDetails;
     RefPtr<RenderStyle> m_style;
     RenderNamedFlowThread* m_parentFlowRenderer;
     AtomicString m_flowThread;
