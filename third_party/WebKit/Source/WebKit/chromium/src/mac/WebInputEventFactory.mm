71cd5b68c0c09ffd5986960f9d87fa4a7213c707
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 17:11:39 +0000

    [chromium][Mac] r122400 broke 10.6 build https://bugs.webkit.org/show_bug.cgi?id=91103
    Patch by Robert Sesek <rsesek@chromium.org> on 2012-07-12
    Reviewed by Tony Chang.
    
    Use the right availability macros for forward-declaring methods and
    defining constants.
    
    * src/mac/WebInputEventFactory.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/mac/WebInputEventFactory.mm
+++ third_party/WebKit/Source/WebKit/chromium/src/mac/WebInputEventFactory.mm
@@ -29,13 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebInputEventFactory.h"
 
 #include <ApplicationServices/ApplicationServices.h>
+#include <Availability.h>
 #import <Cocoa/Cocoa.h>
 
 #import "KeyEventCocoa.h"
 #include "WebInputEvent.h"
 #include <wtf/ASCIICType.h>
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED <= 1060
+#if __MAC_OS_X_VERSION_MAX_ALLOWED < 1070
 
 // Additional Lion APIs.
 enum {
@@ -54,9 +55,9 @@ typedef NSUInteger NSEventPhase;
 - (NSEventPhase)momentumPhase;
 @end
 
-#endif  // __MAC_OS_X_VERSION_MIN_REQUIRED <= 1060
+#endif  // __MAC_OS_X_VERSION_MAX_ALLOWED < 1070
 
-#if __MAC_OS_X_VERSION_MIN_REQUIRED == 1050
+#if __MAC_OS_X_VERSION_MAX_ALLOWED <= 1050
 
 // These are not defined in the 10.5 SDK but are defined in later SDKs inside
 // a MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_5 #ifdef.
@@ -65,7 +66,7 @@ enum {
     NSEventTypeEndGesture       = 20
 };
 
-#endif  // __MAC_OS_X_VERSION_MIN_REQUIRED == 1050
+#endif  // __MAC_OS_X_VERSION_MAX_ALLOWED <= 1050
 
 namespace WebKit {
 
