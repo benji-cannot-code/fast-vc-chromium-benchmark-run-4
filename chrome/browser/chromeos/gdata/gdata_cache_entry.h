275116208ccef0509aa631aed88010d0090c2a64
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-12 18:23:24 +0000

    gdata: Remove GDataCacheEntry::ToString()
    The function was used for debug logging, but these are not so useful.
    
    BUG=136921
    TEST=out/Release/unit_tests --gtest_filter=GData*
    
    Review URL: https://chromiumcodereview.appspot.com/10694161
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_cache_entry.h
+++ chrome/browser/chromeos/gdata/gdata_cache_entry.h
@@ -79,10 +79,6 @@ class GDataCacheEntry {
       cache_state_ &= ~CACHE_STATE_PERSISTENT;
   }
 
-  // For debugging purposes.
-  // TODO(satorux): Remove this: crrev.com/136921
-  std::string ToString() const;
-
  private:
   std::string md5_;
   int cache_state_;
