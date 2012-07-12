bda3efd1d56d41d86c02b5d1230f084a08b820dc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 15:01:08 +0000

    InspectorFileSystemAgent.cpp have to include File.h explicitly https://bugs.webkit.org/show_bug.cgi?id=91078
    Patch by Dongwoo Im <dw.im@samsung.com> on 2012-07-12
    Reviewed by Vsevolod Vlasov.
    
    Make sure the InspectorFileSystemAgent.cpp include the File.h by including explicitly.
    
    No new tests. Covered by existing tests.
    
    * inspector/InspectorFileSystemAgent.cpp: Include File.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122459 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorFileSystemAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorFileSystemAgent.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EntryArray.h"
 #include "EntryCallback.h"
 #include "ErrorCallback.h"
+#include "File.h"
 #include "FileCallback.h"
 #include "FileEntry.h"
 #include "FileError.h"
