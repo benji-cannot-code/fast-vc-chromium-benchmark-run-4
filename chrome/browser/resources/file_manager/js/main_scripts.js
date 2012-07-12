908cdfa14f3a2b706033498fdfe0d66ce9ad93bd
oleg@chromium.org <oleg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-12 16:24:03 +0000

    Refactor DirectoryModel.
    BUG=126887,135041
    TEST=Changing root, directory, copying files.
    Review URL: https://chromiumcodereview.appspot.com/10442061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146360 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="combobutton.js"/>
 //
 //<include src="util.js"/>
+//<include src="directory_contents.js">
 //<include src="volume_manager.js"/>
 //<include src="directory_model.js"/>
 //<include src="file_copy_manager.js"/>
