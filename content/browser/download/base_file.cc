76e5ab14f986062db7920d1c780e2a47646184e5
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-12 22:59:57 +0000

    Add test for failing rename in BaseFileTest.
    Also clean up temporary directory in BaseFileTest.ReadonlyBaseFile.
    
    BUG=none
    TEST=content_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10702130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146477 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/base_file.cc
+++ content/browser/download/base_file.cc
@@ -190,7 +190,7 @@ net::Error RenameFileAndResetSecurityDescriptor(
   int result = SHFileOperation(&move_info);
 
   if (result == 0)
-    return net::OK;
+    return (move_info.fAnyOperationsAborted) ? net::ERR_ABORTED : net::OK;
 
   return MapShFileOperationCodes(result);
 }
