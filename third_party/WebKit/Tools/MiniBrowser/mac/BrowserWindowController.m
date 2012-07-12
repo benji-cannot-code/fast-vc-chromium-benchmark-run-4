f2d8a240f7cd7435403235814d3d9010b6471868
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 01:22:05 +0000

    Fix the build by declaring -isPaginated before use.
    * MiniBrowser/mac/BrowserWindowController.m:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/mac/BrowserWindowController.m
+++ third_party/WebKit/Tools/MiniBrowser/mac/BrowserWindowController.m
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 - (void)didFailProvisionalLoadWithErrorForFrame:(WKFrameRef)frame;
 - (void)didFailLoadWithErrorForFrame:(WKFrameRef)frame;
 - (void)didSameDocumentNavigationForFrame:(WKFrameRef)frame;
+- (BOOL)isPaginated;
 @end
 
 @implementation BrowserWindowController
