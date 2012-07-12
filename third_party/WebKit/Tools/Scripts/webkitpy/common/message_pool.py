e39aeb0f682e6c7562cad80e72049aea90c802f9
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 21:55:37 +0000

    webkitpy: rename manager_worker_broker to message_pool https://bugs.webkit.org/show_bug.cgi?id=91145
    Reviewed by Ojan Vafai.
    
    Since the MessagePool interface is more generic (and simpler)
    now and will be reused by test-webkitpy, I'm renaming it and
    moving it to webkitpy.common.
    
    * Scripts/webkitpy/common/message_pool.py: Renamed from Tools/Scripts/webkitpy/layout_tests/controllers/manager_worker_broker.py.
    * Scripts/webkitpy/layout_tests/controllers/manager.py:
    (TestRunInterruptedException.__reduce__):
    (Manager._run_tests.worker_factory):
    (Manager._run_tests):
    * Scripts/webkitpy/layout_tests/run_webkit_tests_integrationtest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/message_pool.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/message_pool.py
@@ -27,7 +27,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-"""Module for handling messages and concurrency for run-webkit-tests."""
+"""Module for handling messages and concurrency for run-webkit-tests
+and test-webkitpy. This module follows the design for multiprocessing.Pool
+and concurrency.futures.ProcessPoolExecutor, with the following differences:
+
+* Tasks are executed in stateful subprocesses via objects that implement the
+  Worker interface - this allows the workers to share state across tasks.
+* The pool provides an asynchronous event-handling interface so the caller
+  may receive events as tasks are processed.
+
+If you don't need these features, use multiprocessing.Pool or concurrency.futures
+intead.
+
+"""
 
 import cPickle
 import logging
@@ -40,7 +52,6 @@ import traceback
 
 from webkitpy.common.host import Host
 from webkitpy.common.system import stack_utils
-from webkitpy.layout_tests.views import metered_stream
 
 
 _log = logging.getLogger(__name__)
@@ -282,8 +293,8 @@ class _Worker(multiprocessing.Process):
         # into the child process, so we need to remove it to avoid duplicate logging.
         for h in self._logger.handlers:
             # log handlers don't have names until python 2.7.
-            # FIXME: get webkitpy.test.printer from a constant as well.
-            if getattr(h, 'name', '') in (metered_stream.LOG_HANDLER_NAME, 'webkitpy.test.printer'):
+            # FIXME: log handler names should be passed in.
+            if getattr(h, 'name', '') in ('MeteredStreamLogHandler', 'webkitpy.test.printer'):
                 self._logger.removeHandler(h)
                 break
 
