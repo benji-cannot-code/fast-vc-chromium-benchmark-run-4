822f6705534af48a2c9ca9697ec1422b5cf94230
allan.jensen@nokia.com <allan.jensen@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 17:07:16 +0000

    Unreviewed. Build fix for r122462.
    * platform/qt/MemoryUsageSupportQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122474 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/MemoryUsageSupportQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/qt/MemoryUsageSupportQt.cpp
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryUsageSupport.h"
 
 #include "JSDOMWindow.h"
-#include <JavaScriptCore/MemoryStatistics.h>
+#include <runtime/MemoryStatistics.h>
 #include <wtf/FastMalloc.h>
 
 #if OS(LINUX)
