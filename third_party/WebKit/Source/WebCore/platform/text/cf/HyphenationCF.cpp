d4967e52f75282098c7decce6837070243d53f33
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 03:23:38 +0000

    Attempt to fix the Chromium Mac build after r122400.
    * platform/text/cf/HyphenationCF.cpp: Ensure that Chromium only compiles the body of this file if
    building for a new enough version of OS X.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122412 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/cf/HyphenationCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/cf/HyphenationCF.cpp
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "Hyphenation.h"
 
-#if !PLATFORM(MAC) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 1070
+#if (!PLATFORM(MAC) && !PLATFORM(CHROMIUM)) || __MAC_OS_X_VERSION_MIN_REQUIRED >= 1070
 
 #include "AtomicStringKeyedMRUCache.h"
 #include "TextBreakIteratorInternalICU.h"
