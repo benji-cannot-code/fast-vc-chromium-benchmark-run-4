1ca2b7dd493c6db96955f6ff2c3b668301f8e686
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-12 06:47:09 +0000

    - add client cert authentication support in test websocket server - add new browser test which access to a page which requires client cert
    BUG=136950
    TEST=browser_tests
    
    Review URL: https://chromiumcodereview.appspot.com/10541168
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/ui_test_utils.h
+++ chrome/test/base/ui_test_utils.h
@@ -360,6 +360,9 @@ class TestWebSocketServer {
   // Serves with TLS.
   void UseTLS();
 
+  // Requests client cert authentication.
+  void UseClientAuthentication();
+
   // Starts the python websocket server using |root_directory|. Returns whether
   // the server was successfully started.
   bool Start(const FilePath& root_directory);
@@ -397,6 +400,9 @@ class TestWebSocketServer {
 
   // If the python websocket server serves with TLS.
   bool secure_;
+
+  // If the python websocket server requests client cert authentication.
+  bool client_authentication_;
 
   DISALLOW_COPY_AND_ASSIGN(TestWebSocketServer);
 };
