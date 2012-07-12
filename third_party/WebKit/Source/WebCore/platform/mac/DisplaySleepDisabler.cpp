1ea5592d6952fb1684a17ab172e92b3b12a3642a
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-12 06:18:09 +0000

    <http://webkit.org/b/91051> Fix the Qt Mac build after r122400.
    Qt on Mac builds with a deployment target of OS X 10.5. However, it was not been setting
    BUILDING_ON_LEOPARD / TARGETING_LEOPARD and thus was falling down code paths in DisplaySleepDisabler.cpp
    that were specific to Snow Leopard and newer. After the removal of BUILDING_ON_LEOPARD
    and TARGETING_LEOPARD it ended up falling down the correct Leopard-compatible code path,
    which revealed that the code made assumptions about which headers had already been included.
    
    * platform/mac/DisplaySleepDisabler.cpp: Include CoreServices/CoreServices.h to pull in
    a declaration of UpdateSystemActivity when targeting Leopard.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/DisplaySleepDisabler.cpp
+++ third_party/WebKit/Source/WebCore/platform/mac/DisplaySleepDisabler.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if __MAC_OS_X_VERSION_MIN_REQUIRED == 1050
 #include <wtf/UnusedParam.h>
+#include <CoreServices/CoreServices.h>
 #endif
 
 namespace WebCore {
@@ -58,7 +59,7 @@ DisplaySleepDisabler::~DisplaySleepDisabler()
 {
     IOPMAssertionRelease(m_disableDisplaySleepAssertion);
 }
-    
+
 #if __MAC_OS_X_VERSION_MIN_REQUIRED == 1050
 void DisplaySleepDisabler::systemActivityTimerFired(Timer<DisplaySleepDisabler>*)
 {
