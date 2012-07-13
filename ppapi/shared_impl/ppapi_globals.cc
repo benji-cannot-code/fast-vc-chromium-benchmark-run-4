870f9435a8ef2f29d780024cd206a5bc36a4a020
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 22:19:59 +0000

    Fix unit test to allow repeated successful runs by avoiding a function static variable in tests.
    BUG=137145
    TEST=run unit_tests with --gtest_filter=ComponentInstallerTest.PepperFlashCheck --gtest_repeat=2 successfully.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10702188
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146670 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/shared_impl/ppapi_globals.cc
+++ ppapi/shared_impl/ppapi_globals.cc
@@ -24,10 +24,12 @@ PpapiGlobals* PpapiGlobals::ppapi_globals_ = NULL;
 PpapiGlobals::PpapiGlobals() {
   DCHECK(!ppapi_globals_);
   ppapi_globals_ = this;
+  message_loop_proxy_ = base::MessageLoopProxy::current();
 }
 
 PpapiGlobals::PpapiGlobals(ForTest) {
   DCHECK(!ppapi_globals_);
+  message_loop_proxy_ = base::MessageLoopProxy::current();
 }
 
 PpapiGlobals::~PpapiGlobals() {
@@ -44,9 +46,7 @@ void PpapiGlobals::SetPpapiGlobalsOnThreadForTest(PpapiGlobals* ptr) {
 }
 
 base::MessageLoopProxy* PpapiGlobals::GetMainThreadMessageLoop() {
-  CR_DEFINE_STATIC_LOCAL(scoped_refptr<base::MessageLoopProxy>, proxy,
-      (base::MessageLoopProxy::current()));
-  return proxy.get();
+  return message_loop_proxy_.get();
 }
 
 bool PpapiGlobals::IsHostGlobals() const {
