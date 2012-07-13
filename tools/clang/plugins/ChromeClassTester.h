6b00d04896f097f0ae49a37b49c56221d1700ceb
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 01:11:32 +0000

    clang: Check if namespace string is empty using empty() accessor.
    R=erg@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10735067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146497 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/plugins/ChromeClassTester.h
+++ tools/clang/plugins/ChromeClassTester.h
@@ -22,7 +22,7 @@ class ChromeClassTester : public clang::ASTConsumer {
   explicit ChromeClassTester(clang::CompilerInstance& instance);
   virtual ~ChromeClassTester();
 
-  // ASTConsumer:
+  // clang::ASTConsumer:
   virtual void HandleTagDeclDefinition(clang::TagDecl* tag);
 
  protected:
