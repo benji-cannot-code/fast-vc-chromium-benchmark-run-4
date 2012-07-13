d74b441533a12c2bd1c0e1adf3989c91e39334d4
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-13 00:34:26 +0000

    Build fix. Initialize unused const member variables to make compilers happy.
    * dom/DynamicNodeList.h:
    (WebCore::DynamicNodeListCacheBase::DynamicNodeListCacheBase):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DynamicNodeList.h
+++ third_party/WebKit/Source/WebCore/dom/DynamicNodeList.h
@@ -48,7 +48,9 @@ public:
     }
 
     DynamicNodeListCacheBase(CollectionType collectionType)
-        : m_collectionType(collectionType)
+        : m_rootedAtDocument(false) // Ignored
+        , m_invalidationType(DoNotInvalidateOnAttributeChanges) // Ignored
+        , m_collectionType(collectionType)
     {
         ASSERT(m_collectionType == static_cast<unsigned>(collectionType));
         clearCache();
