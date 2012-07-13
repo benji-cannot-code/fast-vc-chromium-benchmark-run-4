4f4c5172878fd1f7eff33c664dd2c4cb9329c3c9
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 17:57:26 +0000

    Remove reference to ExtensionsUI if extensions are not enabled.
    BUG=117407
    
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10704164
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -191,8 +191,6 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<DevToolsUI>;
   if (url.host() == chrome::kChromeUIDialogHost)
     return &NewWebUI<ConstrainedWebDialogUI>;
-  if (url.host() == chrome::kChromeUIExtensionsFrameHost)
-    return &NewWebUI<ExtensionsUI>;
   if (url.host() == chrome::kChromeUIFlashHost)
     return &NewWebUI<FlashUI>;
   if (url.host() == chrome::kChromeUIGpuInternalsHost)
@@ -250,6 +248,10 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
   // Android does not support plugins for now.
   if (url.host() == chrome::kChromeUIPluginsHost)
     return &NewWebUI<PluginsUI>;
+#endif
+#if defined(ENABLE_EXTENSIONS)
+  if (url.host() == chrome::kChromeUIExtensionsFrameHost)
+    return &NewWebUI<ExtensionsUI>;
 #endif
 #if defined(ENABLE_PRINTING)
   if (url.host() == chrome::kChromeUIPrintHost &&
