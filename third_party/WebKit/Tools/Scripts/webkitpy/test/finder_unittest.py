33ad643559e59b7ac6fcbc382525b797124e2f67
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-13 02:01:09 +0000

    test-webkitpy: rename test_finder to finder https://bugs.webkit.org/show_bug.cgi?id=91175
    Reviewed by Adam Barth.
    
    Rename test_finder -> finder, TestFinder -> Finder to remove
    some of the stutter in the names.
    
    * Scripts/webkitpy/test/finder.py: Renamed from Tools/Scripts/webkitpy/test/test_finder.py.
    * Scripts/webkitpy/test/finder_unittest.py: Renamed from Tools/Scripts/webkitpy/test/test_finder_unittest.py.
    * Scripts/webkitpy/test/main.py:
    (Tester.__init__):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/test/finder_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/test/finder_unittest.py
@@ -26,10 +26,10 @@ import unittest
 
 from webkitpy.common.system.filesystem_mock import MockFileSystem
 from webkitpy.common.system.outputcapture import OutputCapture
-from webkitpy.test.test_finder import TestFinder
+from webkitpy.test.finder import Finder
 
 
-class TestFinderTest(unittest.TestCase):
+class FinderTest(unittest.TestCase):
     def setUp(self):
         files = {
           '/foo/bar/baz.py': '',
@@ -41,7 +41,7 @@ class TestFinderTest(unittest.TestCase):
           '/tmp/another_unittest.py': '',
         }
         self.fs = MockFileSystem(files)
-        self.finder = TestFinder(self.fs)
+        self.finder = Finder(self.fs)
         self.finder.add_tree('/foo', 'bar')
         self.finder.add_tree('/foo2')
 
