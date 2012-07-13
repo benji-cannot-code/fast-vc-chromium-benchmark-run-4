aaeef8cb8c4b0b51d3348689b6ce7bbf2833d5e5
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 10:51:01 +0000

    Re-enable AppNotificationManagerTest.ExtensionUninstall under ASan.
    BUG=100156
    TBR=asargent
    Review URL: https://chromiumcodereview.appspot.com/10704200
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/app_notification_manager_unittest.cc
+++ chrome/browser/extensions/app_notification_manager_unittest.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -98,13 +98,7 @@ TEST_F(AppNotificationManagerTest, Simple) {
 
 // Test that AppNotificationManager correctly listens to EXTENSION_UNINSTALLED
 // notifications and removes associated data when that happens.
-#ifdef ADDRESS_SANITIZER
-// This test crashes under ASan, see http://crbug.com/100156
-#define MAYBE_ExtensionUninstall DISABLED_ExtensionUninstall
-#else
-#define MAYBE_ExtensionUninstall ExtensionUninstall
-#endif
-TEST_F(AppNotificationManagerTest, MAYBE_ExtensionUninstall) {
+TEST_F(AppNotificationManagerTest, ExtensionUninstall) {
   // Add some items from two test extension ids.
   std::string id1 = extension_test_util::MakeId("id1");
   std::string id2 = extension_test_util::MakeId("id2");
