4cfdfb11642789b0be266b8809917760eae158bc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-13 09:05:21 +0000

    CodeGeneratorJS.pm : SetterExpression should use 'push' rather than 'unshift' https://bugs.webkit.org/show_bug.cgi?id=91198
    Patch by Dongwoo Im <dw.im@samsung.com> on 2012-07-13
    Reviewed by Kentaro Hara.
    
    'SetterExpression' should use 'push' to make arguments, rather than 'unshift'.
    
    No new tests. Covered by existing tests.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateImplementation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -2020,7 +2020,7 @@ sub GenerateImplementation
                                     $functionName = "impl->${functionName}";
                                 }
 
-                                unshift(@arguments, GenerateCallWith($attribute->signature->extendedAttributes->{"CallWith"}, \@implContent, ""));
+                                push(@arguments, GenerateCallWith($attribute->signature->extendedAttributes->{"CallWith"}, \@implContent, ""));
 
                                 push(@arguments, "ec") if @{$attribute->setterExceptions};
                                 push(@implContent, "    ${functionName}(" . join(", ", @arguments) . ");\n");
