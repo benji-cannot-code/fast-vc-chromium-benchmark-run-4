06dfb3b13b4db8fc82ac0413918d516d533f223a
mtytel@chromium.org <mtytel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 22:57:34 +0000

    Chrome Performance Monitor: Metric API Functions.
    Implements the two metric functions specified here:
    https://docs.google.com/a/chromium.org/document/d/1GhiAly7eRlkH5wfuGAEy53soLIiWVc-3p7E2phSWlJ0/edit#heading=h.9lk03sdde2hp
    
    
    BUG=130212
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10702117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/performance_monitor/metric_details.h
+++ chrome/browser/performance_monitor/metric_details.h
@@ -10,6 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_monitor {
 
+enum MetricType {
+  METRIC_SAMPLE,
+  METRIC_NUMBER_OF_METRICS
+};
+
+const char* MetricTypeToString(MetricType event_type);
+
 struct MetricDetails {
   MetricDetails();
   MetricDetails(const std::string& metric_name,
