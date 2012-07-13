c40bccd45947b244c49218a6e5b1b2cdc27c76e3
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-13 10:11:24 +0000

    Add a PyAuto test for retail mode
    This CL fakes enrollment into retail mode, checks that the retail mode
    login screen is shown and that a single click triggers login.
    
    Further retail mode tests will be added to chromeos_retail_mode.py in
    the future that verify that the various policies affecting retail mode
    work as expected.
    
    The CL depends on https://chromiumcodereview.appspot.com/10702115/.
    
    BUG=127163
    TEST=PyAuto chromeos_device_policy
    
    
    Review URL: https://chromiumcodereview.appspot.com/10696140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146547 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_notification_types.h
+++ chrome/common/chrome_notification_types.h
@@ -947,6 +947,10 @@ enum NotificationType {
   // notification.
   NOTIFICATION_LOGIN_WEBUI_LOADED,
 
+  // Sent when the login screen has loaded in retail mode. The first paint event
+  // after this fires NOTIFICATION_LOGIN_WEBUI_VISIBLE.
+  NOTIFICATION_DEMO_WEBUI_LOADED,
+
   // Sent when the user images on the WebUI login screen have all been loaded.
   // "Normal boot" i.e. for the device with at least one user would generate
   // this one on boot.
@@ -968,6 +972,7 @@ enum NotificationType {
   // 2. NOTIFICATION_LOGIN_WEBUI_LOADED
   // 3. NOTIFICATION_LOGIN_NETWORK_ERROR_SHOWN
   // 4. NOTIFICATION_WIZARD_FIRST_SCREEN_SHOWN
+  // 5. NOTIFICATION_DEMO_WEBUI_LOADED
   //
   // Possible series of notifications:
   // 1. Boot into fresh OOBE
@@ -981,6 +986,9 @@ enum NotificationType {
   //    (NOTIFICATION_LOGIN_NETWORK_ERROR_SHOWN +
   //     NOTIFICATION_LOGIN_WEBUI_VISIBLE)
   //    NOTIFICATION_LOGIN_WEBUI_LOADED
+  //    NOTIFICATION_LOGIN_WEBUI_VISIBLE
+  // 4. Boot into retail mode
+  //    NOTIFICATION_DEMO_WEBUI_LOADED
   //    NOTIFICATION_LOGIN_WEBUI_VISIBLE
   NOTIFICATION_LOGIN_WEBUI_VISIBLE,
 
