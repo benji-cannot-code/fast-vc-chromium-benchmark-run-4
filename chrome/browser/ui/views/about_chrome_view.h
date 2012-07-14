5dfa1904fa0ae9ff93052ca48b3824b976592f79
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-14 15:49:42 +0000

    Switch about box to web ui on Windows.
    This CLs adds proper support for update check on Windows so that we can turn on the WebUI support and eventually remove the code for about box in a modal dialog.
    
    BUG=115123
    TEST=Make sure about works and properly checks for updates.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10698106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146738 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/about_chrome_view.h
+++ chrome/browser/ui/views/about_chrome_view.h
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/dialog_delegate.h"
 
 #if defined(OS_WIN) && !defined(USE_AURA)
-#include "chrome/browser/google/google_update.h"
+#include "chrome/browser/google/google_update_win.h"
 #endif
 
 namespace views {
