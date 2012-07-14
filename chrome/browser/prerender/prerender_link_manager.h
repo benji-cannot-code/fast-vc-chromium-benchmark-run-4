51bda8ef623d8da75e47a0eee6181e4104b9985a
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-14 15:11:02 +0000

    Handle interface to prerenders.
    The prerender_manager now returns a PrerenderHandle* when creating a prerender; this is a useful object for canceling the prerender, as well as signaling navigation, etc...
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/10553029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146735 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_link_manager.h
+++ chrome/browser/prerender/prerender_link_manager.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 
 #include "base/basictypes.h"
-#include "base/gtest_prod_util.h"
 #include "chrome/browser/profiles/profile_keyed_service.h"
 #include "googleurl/src/gurl.h"
 
@@ -26,6 +25,7 @@ class Size;
 
 namespace prerender {
 
+class PrerenderHandle;
 class PrerenderManager;
 
 // PrerenderLinkManager implements the API on Link elements for all documents
@@ -70,14 +70,20 @@ class PrerenderLinkManager : public ProfileKeyedService {
   friend class PrerenderTest;
 
   typedef std::pair<int, int> ChildAndPrerenderIdPair;
-  typedef std::map<ChildAndPrerenderIdPair, GURL> IdPairToUrlMap;
+  typedef std::map<ChildAndPrerenderIdPair, PrerenderHandle*>
+      IdPairToPrerenderHandleMap;
 
-  void RemovePrerender(const IdPairToUrlMap::iterator& id_url_iter);
+  void RemovePrerender(
+      const IdPairToPrerenderHandleMap::iterator& id_to_handle_iter);
 
   bool IsEmpty() const;
 
   PrerenderManager* manager_;
-  IdPairToUrlMap ids_to_url_map_;
+
+  // A map from child process id and prerender id to PrerenderHandles. We map
+  // from this pair because the prerender ids are only unique within their
+  // renderer process.
+  IdPairToPrerenderHandleMap ids_to_handle_map_;
 
   DISALLOW_COPY_AND_ASSIGN(PrerenderLinkManager);
 };
