d8c1826fcf20d9893feac032e2a0d3c6c0574458
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-14 21:34:10 +0000

    Fix the Windows build.
    * platform/network/cf/DNSCFNet.cpp: Fix the condition to take Windows in to account.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122671 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/DNSCFNet.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/DNSCFNet.cpp
@@ -43,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <CFNetwork/CFNetwork.h>
 #endif
 
-#if !PLATFORM(IOS) || __MAC_OS_X_VERSION_MIN_REQUIRED == 1050
+#if PLATFORM(MAC) && !PLATFORM(IOS) && __MAC_OS_X_VERSION_MIN_REQUIRED == 1050
 #include <SystemConfiguration/SystemConfiguration.h>
 #endif
 
