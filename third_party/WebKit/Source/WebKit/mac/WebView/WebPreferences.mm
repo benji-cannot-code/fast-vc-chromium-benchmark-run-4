bf3dce5a293c8c2581c9b9dae511acb17357d8f8
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-14 09:03:20 +0000

    [Mac] Do not try to update the cache model for every WebPreferences change https://bugs.webkit.org/show_bug.cgi?id=91302
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-07-13
    Reviewed by Joseph Pecoraro.
    
    Source/WebKit/mac: 
    
    WebView was listening to any change on any WebPreferences in order to update the cache model. This is unneeded in most cases,
    we can just listen to updates of the CacheModel property.
    
    * WebView/WebPreferences.mm:
    (-[WebPreferences initWithIdentifier:]):
    (-[WebPreferences _postCacheModelChangedNotification]):
    (-[WebPreferences setCacheModel:]):
    * WebView/WebPreferencesPrivate.h:
    * WebView/WebView.mm:
    (+[WebView initialize]):
    (+[WebView _cacheModelChangedNotification:]):
    
    Tools: 
    
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/mac/SetAndUpdateCacheModel.mm: Added.
    (TestWebKitAPI):
    (TestWebKitAPI::TEST):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122665 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
@@ -48,6 +48,7 @@ using namespace WebCore;
 NSString *WebPreferencesChangedNotification = @"WebPreferencesChangedNotification";
 NSString *WebPreferencesRemovedNotification = @"WebPreferencesRemovedNotification";
 NSString *WebPreferencesChangedInternalNotification = @"WebPreferencesChangedInternalNotification";
+NSString *WebPreferencesCacheModelChangedInternalNotification = @"WebPreferencesCacheModelChangedInternalNotification";
 
 #define KEY(x) (_private->identifier ? [_private->identifier stringByAppendingString:(x)] : (x))
 
@@ -223,6 +224,7 @@ static WebCacheModel cacheModelForMainBundle(void)
     [[self class] _setInstance:self forIdentifier:_private->identifier];
 
     [self _postPreferencesChangedNotification];
+    [self _postCacheModelChangedNotification];
 
     return self;
 }
@@ -803,10 +805,21 @@ static WebCacheModel cacheModelForMainBundle(void)
     return [self _boolValueForKey:WebKitUsesPageCachePreferenceKey];
 }
 
+- (void)_postCacheModelChangedNotification
+{
+    if (!pthread_main_np()) {
+        [self performSelectorOnMainThread:_cmd withObject:nil waitUntilDone:NO];
+        return;
+    }
+
+    [[NSNotificationCenter defaultCenter] postNotificationName:WebPreferencesCacheModelChangedInternalNotification object:self userInfo:nil];
+}
+
 - (void)setCacheModel:(WebCacheModel)cacheModel
 {
     [self _setIntegerValue:cacheModel forKey:WebKitCacheModelPreferenceKey];
     [self setAutomaticallyDetectsCacheModel:NO];
+    [self _postCacheModelChangedNotification];
 }
 
 - (WebCacheModel)cacheModel
