1023948b40cdf6ca80b6661893295ffdb35fe037
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-14 02:35:16 +0000

    Add DPI information to video packets.
    The intention is that capturers can add this information and the client can
    make use of it to up-scale the desktop if it DPI is larger than that of the
    host by an integer multiple. For example, a standard-DPI host viewed on a
    retina-display Macbook Pro would no longer look tiny.
    
    BUG=135089
    TEST=EncoderVp8Test.TestDpiPropagation
    
    
    Review URL: https://chromiumcodereview.appspot.com/10736046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146715 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/base/capture_data.cc
+++ remoting/base/capture_data.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,7 +21,8 @@ CaptureData::CaptureData(const DataPlanes &data_planes,
       size_(size),
       pixel_format_(format),
       capture_time_ms_(0),
-      client_sequence_number_(0) {
+      client_sequence_number_(0),
+      dpi_(SkIPoint::Make(0, 0)) {
 }
 
 CaptureData::~CaptureData() {}
--- remoting/base/capture_data.h
+++ remoting/base/capture_data.h
@@ -60,6 +60,10 @@ class CaptureData : public base::RefCountedThreadSafe<CaptureData> {
     client_sequence_number_ = client_sequence_number;
   }
 
+  SkIPoint dpi() const { return dpi_; }
+
+  void set_dpi(const SkIPoint& dpi) { dpi_ = dpi; }
+
  private:
   friend class base::RefCountedThreadSafe<CaptureData>;
   virtual ~CaptureData();
@@ -74,6 +78,9 @@ class CaptureData : public base::RefCountedThreadSafe<CaptureData> {
 
   // Sequence number supplied by client for performance tracking.
   int64 client_sequence_number_;
+
+  // DPI for this frame.
+  SkIPoint dpi_;
 };
 
 }  // namespace remoting
