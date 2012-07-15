a118aa43efd54d5fb97ec4a2667446919f22ba01
joepeck@webkit.org <joepeck@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-15 23:01:11 +0000

    Fix case sensitive build issue.
    Unreviewed build fix.
    
    * WebView/WebPreferences.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebPreferences.mm
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/ApplicationCacheStorage.h>
 #import <WebCore/CookieStorageCFNet.h>
 #import <WebCore/ResourceHandle.h>
-#import <WTF/RetainPtr.h>
+#import <wtf/RetainPtr.h>
 
 using namespace WebCore;
 
