df963f7e441a064425c9c8df8674257a7510c5f6
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-16 21:30:28 +0000

    [chromium] Remove non-ephemeral data from RenderSurface as it duplicates data from the owning layer https://bugs.webkit.org/show_bug.cgi?id=91418
    Reviewed by Adrienne Walker.
    
    This removes the filters and masks from render surfaces, and makes them
    used directly from the owning layer. Also removes skipsDraw from
    surfaces as it was just not used at all.
    
    Covered by existing tests.
    
    * platform/graphics/chromium/LayerChromium.h:
    (WebCore::LayerChromium::filters):
    (WebCore::LayerChromium::backgroundFilters):
    (WebCore::LayerChromium::hasMask):
    (WebCore::LayerChromium::hasReplica):
    (WebCore::LayerChromium::replicaHasMask):
    (LayerChromium):
    * platform/graphics/chromium/RenderSurfaceChromium.cpp:
    (WebCore::RenderSurfaceChromium::RenderSurfaceChromium):
    * platform/graphics/chromium/RenderSurfaceChromium.h:
    (RenderSurfaceChromium):
    * platform/graphics/chromium/cc/CCLayerImpl.h:
    (WebCore::CCLayerImpl::hasMask):
    (WebCore::CCLayerImpl::hasReplica):
    (WebCore::CCLayerImpl::replicaHasMask):
    (CCLayerImpl):
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::calculateMemoryForRenderSurfaces):
    * platform/graphics/chromium/cc/CCLayerTreeHostCommon.cpp:
    (WebCore::calculateDrawTransformsInternal):
    * platform/graphics/chromium/cc/CCOcclusionTracker.cpp:
    (WebCore::::finishedRenderTarget):
    (WebCore::reduceOcclusionBelowSurface):
    (WebCore::::leaveToRenderTarget):
    * platform/graphics/chromium/cc/CCRenderPass.cpp:
    (WebCore::CCRenderPass::appendQuadsForRenderSurfaceLayer):
    * platform/graphics/chromium/cc/CCRenderSurface.cpp:
    (WebCore::CCRenderSurface::drawableContentRect):
    (WebCore::CCRenderSurface::appendQuads):
    * platform/graphics/chromium/cc/CCRenderSurface.h:
    (CCRenderSurface):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCRenderSurface.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatRect.h"
 #include "IntRect.h"
 #include "cc/CCSharedQuadState.h"
-#include <public/WebFilterOperations.h>
 #include <public/WebTransformationMatrix.h>
 #include <wtf/Noncopyable.h>
 #include <wtf/text/WTFString.h>
@@ -63,12 +62,6 @@ public:
 
     float drawOpacity() const { return m_drawOpacity; }
     void setDrawOpacity(float opacity) { m_drawOpacity = opacity; }
-
-    void setFilters(const WebKit::WebFilterOperations& filters) { m_filters = filters; }
-    const WebKit::WebFilterOperations& filters() const { return m_filters; }
-
-    void setBackgroundFilters(const WebKit::WebFilterOperations& filters) { m_backgroundFilters = filters; }
-    const WebKit::WebFilterOperations& backgroundFilters() const { return m_backgroundFilters; }
 
     void setNearestAncestorThatMovesPixels(CCRenderSurface* surface) { m_nearestAncestorThatMovesPixels = surface; }
     const CCRenderSurface* nearestAncestorThatMovesPixels() const { return m_nearestAncestorThatMovesPixels; }
@@ -116,11 +109,6 @@ public:
 
     int owningLayerId() const;
 
-    bool hasReplica() const;
-
-    bool hasMask() const;
-    bool replicaHasMask() const;
-
     void resetPropertyChangedFlag() { m_surfacePropertyChanged = false; }
     bool surfacePropertyChanged() const;
     bool surfacePropertyChangedOnlyFromDescendant() const;
@@ -151,8 +139,6 @@ private:
     WebKit::WebTransformationMatrix m_replicaScreenSpaceTransform;
     bool m_targetSurfaceTransformsAreAnimating;
     bool m_screenSpaceTransformsAreAnimating;
-    WebKit::WebFilterOperations m_filters;
-    WebKit::WebFilterOperations m_backgroundFilters;
 
     // Uses the space of the surface's target surface.
     IntRect m_clipRect;
