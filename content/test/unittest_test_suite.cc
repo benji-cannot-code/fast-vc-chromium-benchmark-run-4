ceab286756c6556bc326f198b605b8eaa9d1395a
eaugusti@chromium.org <eaugusti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 19:12:49 +0000

    Forces V8ValueConverter to switch context when converting objects.
    Objects passed into the V8ValueConverter may have fields that reference
    objects in a v8::Context different than the one that was passed in.
    This is very common for objects that were created in an isolated world and
    reference globals in that world (such as window). This fix will check to see if
    the current v8::Context is the same as the converting objects CreationContext().
    If they are different, they will be swaped during the object's conversion.
    
    In addition, the V8ValueConverter has been expanded to have the ability to omit duplicate
    fileds in an object. This is very important when something with a graph structure (like a DOM
    node) is converted.
    
    Extra checks for integer/string fields have been added as in respone to Jakob's observation:
    https://code.google.com/p/chromium/issues/detail?id=134661#c5
    
    BUG=134661, 135104
    TEST=content_unittests::V8ValueConverterImplTest.RecursiveObject
    
    Review URL: https://chromiumcodereview.appspot.com/10704030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146852 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/unittest_test_suite.cc
+++ content/test/unittest_test_suite.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/unittest_test_suite.h"
 
 #include "base/logging.h"
+#include "base/rand_util.h"
 #include "base/test/test_suite.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h"
@@ -20,7 +21,7 @@ class UnitTestTestSuite::UnitTestWebKitPlatformSupport
   virtual ~UnitTestWebKitPlatformSupport() {}
   virtual void cryptographicallyRandomValues(unsigned char* buffer,
                                              size_t length) OVERRIDE {
-    memset(buffer, 0, length);
+    base::RandBytes(buffer, length);
   }
   virtual const unsigned char* getTraceCategoryEnabledFlag(
       const char* categoryName) {
