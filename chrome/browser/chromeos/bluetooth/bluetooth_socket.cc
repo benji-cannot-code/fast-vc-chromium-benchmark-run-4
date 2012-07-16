452de6c70d33eab5a2e075a8f7ecab472826817a
shenhan@google.com <shenhan@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 19:26:07 +0000

    Fix gcc 4.7 building problems.
    Fixes include -
    - added static_cast for narrowing conversion in initiliazation lists;
    - added explicit <unistd.h> inclusion
    
    BUG=None
    TEST=Built with GCC-4.7 under linux
    
    
    Review URL: https://chromiumcodereview.appspot.com/10758012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146856 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/bluetooth/bluetooth_socket.cc
+++ chrome/browser/chromeos/bluetooth/bluetooth_socket.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "chrome/browser/chromeos/bluetooth/bluetooth_service_record.h"
 #include "chrome/browser/chromeos/bluetooth/bluetooth_utils.h"
