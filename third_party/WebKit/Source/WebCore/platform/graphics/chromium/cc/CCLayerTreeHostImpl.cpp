aee6fb7e126fddef771cff052c771e8e4a3d6ed6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-16 19:06:00 +0000

    [chromium] Only apply page scale delta to root scroll layer https://bugs.webkit.org/show_bug.cgi?id=91374
    Patch by Sami Kyostila <skyostil@chromium.org> on 2012-07-16
    Reviewed by Adrienne Walker.
    
    Source/WebCore:
    
    When the user pinch-zooms the web page though the Chromium compositor, the
    per-layer page scale delta is used to keep track of the difference between the
    page scale on the compositor thread versus the main thread. On the next
    commit to the main thread these values are reset to 1.
    
    When calculating layer positions, the compositor applies a layer's page scale
    delta both to the layer itself as well as all of its children. Since we are
    currently updating the page scale delta on all scrollable layers, this results
    in scrollable child layers getting scaled multiple times.
    
    This patch changes the compositor to only apply the page scale delta on the
    root scroll layer.
    
    New unit test: CCLayerTreeHostImplTest.pageScaleDeltaAppliedToRootScrollLayerOnly
    
    * platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp:
    (WebCore::CCLayerTreeHostImpl::setPageScaleFactorAndLimits):
    (WebCore::CCLayerTreeHostImpl::setPageScaleDelta):
    
    Source/WebKit/chromium:
    
    New unit test
    CCLayerTreeHostImplTest.pageScaleDeltaAppliedToRootScrollLayerOnly to verify
    the transformation of child layer while pinch zooming.
    
    * tests/CCLayerTreeHostImplTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122746 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHostImpl.cpp
@@ -762,18 +762,6 @@ static void adjustScrollsForPageScaleChange(CCLayerImpl* layerImpl, float pageSc
         adjustScrollsForPageScaleChange(layerImpl->children()[i].get(), pageScaleChange);
 }
 
-static void applyPageScaleDeltaToScrollLayers(CCLayerImpl* layerImpl, float pageScaleDelta)
-{
-    if (!layerImpl)
-        return;
-
-    if (layerImpl->scrollable())
-        layerImpl->setPageScaleDelta(pageScaleDelta);
-
-    for (size_t i = 0; i < layerImpl->children().size(); ++i)
-        applyPageScaleDeltaToScrollLayers(layerImpl->children()[i].get(), pageScaleDelta);
-}
-
 void CCLayerTreeHostImpl::setDeviceScaleFactor(float deviceScaleFactor)
 {
     if (deviceScaleFactor == m_deviceScaleFactor)
@@ -808,7 +796,8 @@ void CCLayerTreeHostImpl::setPageScaleFactorAndLimits(float pageScale, float min
     // Clamp delta to limits and refresh display matrix.
     setPageScaleDelta(m_pageScaleDelta / m_sentPageScaleDelta);
     m_sentPageScaleDelta = 1;
-    applyPageScaleDeltaToScrollLayers(m_rootScrollLayerImpl, m_pageScaleDelta);
+    if (m_rootScrollLayerImpl)
+        m_rootScrollLayerImpl->setPageScaleDelta(m_pageScaleDelta);
 }
 
 void CCLayerTreeHostImpl::setPageScaleDelta(float delta)
@@ -826,7 +815,8 @@ void CCLayerTreeHostImpl::setPageScaleDelta(float delta)
     m_pageScaleDelta = delta;
 
     updateMaxScrollPosition();
-    applyPageScaleDeltaToScrollLayers(m_rootScrollLayerImpl, m_pageScaleDelta);
+    if (m_rootScrollLayerImpl)
+        m_rootScrollLayerImpl->setPageScaleDelta(m_pageScaleDelta);
 }
 
 void CCLayerTreeHostImpl::updateMaxScrollPosition()
