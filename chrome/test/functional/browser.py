f6b9d4c066bb153bf8cdbc2b61d6390c6c1e4b9e
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 21:32:59 +0000

    Switch Flash tests to look for the Pepper Plugin on Windows.
    BUG=None
    TEST=flash.FlashTest
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/10784022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146890 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/browser.py
+++ chrome/test/functional/browser.py
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/env python
-# Copyright (c) 2011 The Chromium Authors. All rights reserved.
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
@@ -24,13 +24,6 @@ class BrowserTest(pyauto.PyUITest):
     while True:
       raw_input('Hit <enter> to dump info.. ')
       self.pprint(self.GetBrowserInfo())
-
-  def setUp(self):
-    pyauto.PyUITest.setUp(self)
-    self._flash_plugin_type = 'Plug-in'
-    if (self.IsChromeOS() and
-        self.GetBrowserInfo()['properties']['branding'] == 'Google Chrome'):
-      self._flash_plugin_type = 'Pepper Plugin'
 
   def _GetUniqProcesses(self, total_tabs, renderer_processes):
     """ Returns a count of uniq processes of opened tabs
