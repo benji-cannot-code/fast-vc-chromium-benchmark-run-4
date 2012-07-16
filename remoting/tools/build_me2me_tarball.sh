91ca48636b162a8406a73340c25248871d5e8783
lambroslambrou@google.com <lambroslambrou@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 17:40:27 +0000

    Fix svn revision number in Me2Me tarball.
    The recent git layout changes broke the svn revision detection in this script.
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/10703074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146829 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/tools/build_me2me_tarball.sh
+++ remoting/tools/build_me2me_tarball.sh
@@ -14,7 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 set -eu
 
-REVISION="$(git svn find-rev HEAD)"
+MERGE_BASE="$(git merge-base HEAD origin/git-svn)"
+REVISION="$(git svn find-rev "$MERGE_BASE")"
 if [ -z "$REVISION" ]; then
   echo "svn revision number not found"
   REVISION_SUFFIX=""
