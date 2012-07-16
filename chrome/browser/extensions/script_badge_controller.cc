ef9a938cf4985936a41868d7a66801e6afc909e3
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 08:33:18 +0000

    Make script badges clear when the document of the main frame loads, not when there's a navigation. This fixes a bug where script badges were incorrectly being cleared on fragment navigation, for example google.com -> google.com#foo, which counts as a navigation but doesn't re-inject the content script therefore shoudn't clear the script badge.
    BUG=137167
    TEST=Go to a page with a content script. Change the URL fragment to something
         arbitrary but different. Script badges shouldn't clear. Reload page or
         do a real navigation and the script badge should clear (until added again).
    
    Review URL: https://chromiumcodereview.appspot.com/10690158
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146774 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/script_badge_controller.cc
+++ chrome/browser/extensions/script_badge_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_set.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "content/public/browser/navigation_controller.h"
+#include "content/public/browser/navigation_details.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
@@ -102,6 +103,8 @@ void ScriptBadgeController::NotifyChange() {
 void ScriptBadgeController::DidNavigateMainFrame(
     const content::LoadCommittedDetails& details,
     const content::FrameNavigateParams& params) {
+  if (details.is_in_page)
+    return;
   extensions_executing_scripts_.clear();
   current_actions_.clear();
 }
