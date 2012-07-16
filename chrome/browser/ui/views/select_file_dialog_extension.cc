15c6619188f9565a63d6a09990250f8cfabb7de3
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 21:02:54 +0000

    Making the Open/Save file dialogs resizable.
    BUG=127172
    TEST=Manual test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10694112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146880 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/select_file_dialog_extension.cc
+++ chrome/browser/ui/views/select_file_dialog_extension.cc
@@ -39,6 +39,9 @@ namespace {
 const int kFileManagerWidth = 954;  // pixels
 const int kFileManagerHeight = 640;  // pixels
 
+const int kFileManagerMinimumWidth = kFileManagerWidth * 2 / 3;  // pixels
+const int kFileManagerMinimumHeight = kFileManagerHeight * 2 / 3;  // pixels
+
 // Holds references to file manager dialogs that have callbacks pending
 // to their listeners.
 class PendingDialog {
@@ -342,6 +345,9 @@ ExtensionDialog* dialog = ExtensionDialog::Show(file_browser_url,
     LOG(ERROR) << "Unable to create extension dialog";
     return;
   }
+
+  dialog->SetMinimumContentsSize(kFileManagerMinimumWidth,
+                                 kFileManagerMinimumHeight);
 
   // Connect our listener to FileDialogFunction's per-tab callbacks.
   AddPending(tab_id);
