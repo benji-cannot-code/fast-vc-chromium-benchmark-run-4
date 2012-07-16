1d04007d164c5d6acc1da7b8773e192f156bb992
erg@google.com <erg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 21:45:27 +0000

    Revert 146792 - Lock-free GamepadSeqLock (try 2)
    [Reverted due to crashing in ChromeBot with following stack:
    content::internal::GamepadSeqLockBase::ReadTo+0x1a
    content::GamepadSharedMemoryReader::SampleGamepads+0xaa
    RendererWebKitPlatformSupportImpl::sampleGamepads+0x57
    WebCore::sampleGamepads+0x3f
    WebCore::NavigatorGamepad::gamepads+0x71
    WebCore::NavigatorV8Internal::webkitGamepadsAttrGetter+0x45
    v8::internal::JSObject::GetPropertyWithCallback+0x252]
    
    The change
    - provides an improved lock-free SeqLock implementation which eliminates any potential blocking of readers.
    - provides a higher-level and simpler API as was suggested by Darin.
    - ThreadSanitizer report suppressions are replaced with correct synchronization. 
    - eliminates nasty kMaximumContentionCount and associated histogram.
    Oritignal review URL:
    http://codereview.chromium.org/8772004
    TBR=darin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10704041
    
    TBR=dvyukov@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10785024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146896 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gamepad/gamepad_provider_unittest.cc
+++ content/browser/gamepad/gamepad_provider_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process_util.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/system_monitor/system_monitor.h"
-#include "base/threading/platform_thread.h"
 #include "content/browser/gamepad/data_fetcher.h"
 #include "content/browser/gamepad/gamepad_provider.h"
 #include "content/common/gamepad_hardware_buffer.h"
@@ -24,14 +23,19 @@ using WebKit::WebGamepads;
 class MockDataFetcher : public GamepadDataFetcher {
  public:
   explicit MockDataFetcher(const WebGamepads& test_data)
-      : test_data_(test_data) {
+      : test_data_(test_data),
+        read_data_(false, false) {
   }
   virtual void GetGamepadData(WebGamepads* pads,
                               bool devices_changed_hint) OVERRIDE {
     *pads = test_data_;
+    read_data_.Signal();
   }
 
+  void WaitForDataRead() { return read_data_.Wait(); }
+
   WebGamepads test_data_;
+  base::WaitableEvent read_data_;
 };
 
 // Main test fixture
@@ -74,6 +78,8 @@ TEST_F(GamepadProviderTest, DISABLED_PollingAccess) {
 
   main_message_loop_.RunAllPending();
 
+  mock_data_fetcher_->WaitForDataRead();
+
   // Renderer-side, pull data out of poll buffer.
   base::SharedMemoryHandle handle =
       provider->GetRendererSharedMemoryHandle(base::GetCurrentProcessHandle());
@@ -83,14 +89,15 @@ TEST_F(GamepadProviderTest, DISABLED_PollingAccess) {
   void* mem = shared_memory->memory();
 
   GamepadHardwareBuffer* hwbuf = static_cast<GamepadHardwareBuffer*>(mem);
+  // See gamepad_hardware_buffer.h for details on the read discipline.
   WebGamepads output;
-  output.length = 0;
-  for (;;) {
-    hwbuf->gamepads.ReadTo(&output);
-    if (output.length == 1)
-      break;
-    base::PlatformThread::YieldCurrentThread();
-  }
+
+  base::subtle::Atomic32 version;
+  do {
+    version = hwbuf->sequence.ReadBegin();
+    memcpy(&output, &hwbuf->buffer, sizeof(output));
+  } while (hwbuf->sequence.ReadRetry(version));
+
   EXPECT_EQ(1u, output.length);
   EXPECT_EQ(1u, output.items[0].buttonsLength);
   EXPECT_EQ(1.f, output.items[0].buttons[0]);
