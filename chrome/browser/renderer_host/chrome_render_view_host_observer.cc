2870b9bf6059e3a7a37380c13458e475bf4a94f1
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 20:23:52 +0000

    Attempt to fix bug where extension APIs not always defined.
    This is not a great fix, it would be better to understand the race better, but oh well.
    
    BUG=132148
    
    Review URL: https://chromiumcodereview.appspot.com/10735084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146872 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_render_view_host_observer.cc
+++ chrome/browser/renderer_host/chrome_render_view_host_observer.cc
@@ -34,6 +34,7 @@ ChromeRenderViewHostObserver::ChromeRenderViewHostObserver(
       site_instance->GetBrowserContext());
 
   InitRenderViewHostForExtensions();
+  InitRenderViewForExtensions();
 }
 
 ChromeRenderViewHostObserver::~ChromeRenderViewHostObserver() {
@@ -42,6 +43,8 @@ ChromeRenderViewHostObserver::~ChromeRenderViewHostObserver() {
 }
 
 void ChromeRenderViewHostObserver::RenderViewHostInitialized() {
+  // This reinitializes some state in the case where a render process crashes
+  // but we keep the same RenderViewHost instance.
   InitRenderViewForExtensions();
 }
 
@@ -81,14 +84,6 @@ void ChromeRenderViewHostObserver::InitRenderViewHostForExtensions() {
 
   // TODO(creis): Use this to replace SetInstalledAppForRenderer.
   process_manager->RegisterRenderViewHost(render_view_host(), extension);
-
-  if (extension->is_app()) {
-    // Record which, if any, installed app is associated with this process.
-    // TODO(aa): Totally lame to store this state in a global map in extension
-    // service. Can we get it from EPM instead?
-    profile_->GetExtensionService()->SetInstalledAppForRenderer(
-        render_view_host()->GetProcess()->GetID(), extension);
-  }
 }
 
 void ChromeRenderViewHostObserver::InitRenderViewForExtensions() {
