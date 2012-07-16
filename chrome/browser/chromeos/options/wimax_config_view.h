0b5b70510511fe67f0ecd5b3566a2e1c2555a446
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-16 22:23:04 +0000

    Move wifi/wimax/vpn config title from titlebar to dialog body
    BUG=130195
    TEST=Heading for connect to wifi/vpn/wimax should appear in bold in the dialog body, not in the titlebar (see issue).
    
    
    Review URL: https://chromiumcodereview.appspot.com/10659017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146904 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/wimax_config_view.h
+++ chrome/browser/chromeos/options/wimax_config_view.h
@@ -46,7 +46,6 @@ class WimaxConfigView : public ChildNetworkConfigView,
                              const views::Event& event) OVERRIDE;
 
   // ChildNetworkConfigView:
-  virtual string16 GetTitle() OVERRIDE;
   virtual views::View* GetInitiallyFocusedView() OVERRIDE;
   virtual bool CanLogin() OVERRIDE;
   virtual bool Login() OVERRIDE;
