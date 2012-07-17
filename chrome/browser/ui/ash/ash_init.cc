c3c9975c4d2702e9c0f5f5802dd602250b0e2a20
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 22:58:51 +0000

    Implement ash::KeyboardBrightnessControlDelegate.
    BUG=135482
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10689166
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147124 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/ash_init.cc
+++ chrome/browser/ui/ash/ash_init.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/volume_controller_chromeos.h"
 #include "chrome/browser/ui/ash/brightness_controller_chromeos.h"
 #include "chrome/browser/ui/ash/ime_controller_chromeos.h"
+#include "chrome/browser/ui/ash/keyboard_brightness_controller_chromeos.h"
 #endif
 
 
@@ -84,6 +85,9 @@ void OpenAsh() {
       scoped_ptr<ash::CapsLockDelegate>(new CapsLockHandler(xkeyboard)).Pass());
   shell->accelerator_controller()->SetImeControlDelegate(
       scoped_ptr<ash::ImeControlDelegate>(new ImeController).Pass());
+  shell->accelerator_controller()->SetKeyboardBrightnessControlDelegate(
+      scoped_ptr<ash::KeyboardBrightnessControlDelegate>(
+          new KeyboardBrightnessController).Pass());
   shell->accelerator_controller()->SetVolumeControlDelegate(
       scoped_ptr<ash::VolumeControlDelegate>(new VolumeController).Pass());
 
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2400,6 +2400,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/ash/event_rewriter.h',
         'browser/ui/ash/ime_controller_chromeos.cc',
         'browser/ui/ash/ime_controller_chromeos.h',
+        'browser/ui/ash/keyboard_brightness_controller_chromeos.cc',
+        'browser/ui/ash/keyboard_brightness_controller_chromeos.h',
         'browser/ui/ash/screenshot_taker.cc',
         'browser/ui/ash/screenshot_taker.h',
         'browser/ui/ash/tabs/dock_info_ash.cc',
