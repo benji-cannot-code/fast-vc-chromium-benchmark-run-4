c9ae990d9ee603ade7dd482e54f8b3bf01337d8b
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 16:19:11 +0000

    [filemanager] Moved tasks-related code to a separate entity.
    BUG=137503
    TEST=Nothing changes for the user.
    
    Review URL: https://chromiumcodereview.appspot.com/10704226
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -56,6 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="file_copy_manager.js"/>
 //<include src="file_manager.js"/>
 //<include src="file_manager_pyauto.js"/>
+//<include src="file_tasks.js"/>
 //<include src="file_type.js"/>
 //<include src="file_transfer_controller.js"/>
 //<include src="media/media_util.js"/>
