883f2da59e911474edffeeaddfd51ba162184b15
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 18:09:56 +0000

    Bring up a content settings icon for ungestured registerProtocolHandler call.
    R=benwells@chromium.org
    BUG=122020
    TEST=None
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=144957
    
    Review URL: https://chromiumcodereview.appspot.com/10584042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/character_encoding.h"
 #include "chrome/browser/chrome_page_zoom.h"
 #include "chrome/browser/content_settings/host_content_settings_map.h"
+#include "chrome/browser/content_settings/tab_specific_content_settings.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/custom_handlers/register_protocol_handler_infobar_delegate.h"
 #include "chrome/browser/debugger/devtools_toggle_action.h"
@@ -853,7 +854,8 @@ void Browser::RegisterProtocolHandlerHelper(WebContents* web_contents,
                                             const std::string& protocol,
                                             const GURL& url,
                                             const string16& title,
-                                            bool user_gesture) {
+                                            bool user_gesture,
+                                            BrowserWindow* window) {
   TabContents* tab_contents = TabContents::FromWebContents(web_contents);
   if (!tab_contents || tab_contents->profile()->IsOffTheRecord())
     return;
@@ -863,11 +865,23 @@ void Browser::RegisterProtocolHandlerHelper(WebContents* web_contents,
 
   ProtocolHandlerRegistry* registry =
       tab_contents->profile()->GetProtocolHandlerRegistry();
+  TabSpecificContentSettings* content_settings =
+      tab_contents->content_settings();
 
-  // TODO(gbillock): Replace this policy with one that shows the content
-  // settings icon.
-  if (!user_gesture)
+  if (!user_gesture && window) {
+    content_settings->set_pending_protocol_handler(handler);
+    content_settings->set_previous_protocol_handler(
+        registry->GetHandlerFor(handler.protocol()));
+    window->GetLocationBar()->UpdateContentSettingsIcons();
     return;
+  }
+
+  // Make sure content-setting icon is turned off in case the page does
+  // ungestured and gestured RPH calls.
+  if (window) {
+    content_settings->ClearPendingProtocolHandler();
+    window->GetLocationBar()->UpdateContentSettingsIcons();
+  }
 
   if (!registry->SilentlyHandleRegisterHandlerRequest(handler)) {
     content::RecordAction(
@@ -1589,7 +1603,7 @@ void Browser::RegisterProtocolHandler(WebContents* web_contents,
                                       const string16& title,
                                       bool user_gesture) {
   RegisterProtocolHandlerHelper(
-      web_contents, protocol, url, title, user_gesture);
+      web_contents, protocol, url, title, user_gesture, window());
 }
 
 void Browser::RegisterIntentHandler(
--- chrome/chrome_dll.gypi
+++ chrome/chrome_dll.gypi
@@ -250,6 +250,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 'app/nibs/ContentBlockedPlugins.xib',
                 'app/nibs/ContentBlockedPopups.xib',
                 'app/nibs/ContentBlockedGeolocation.xib',
+                'app/nibs/ContentProtocolHandlers.xib',
                 'app/nibs/DownloadItem.xib',
                 'app/nibs/DownloadShelf.xib',
                 'app/nibs/EditSearchEngine.xib',
