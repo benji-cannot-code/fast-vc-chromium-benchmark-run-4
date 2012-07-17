76a8c1a482d1ec9a6e08627788982c2828ee5d7e
battre@chromium.org <battre@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 16:57:39 +0000

    Support host permissions in decl. WebRequest API
    This CL adds checks, whether actions may be executed on requests based on the extensions' host permissions. This does not consider incognito mode permissions, yet.
    
    BUG=112155
    TEST=no
    
    
    Review URL: https://chromiumcodereview.appspot.com/10790027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_request/web_request_api_helpers.h
+++ chrome/browser/extensions/api/web_request/web_request_api_helpers.h
@@ -27,6 +27,10 @@ class ListValue;
 class Value;
 }
 
+namespace extensions {
+class Extension;
+}
+
 namespace net {
 class BoundNetLog;
 class URLRequest;
@@ -170,7 +174,7 @@ bool MergeOnAuthRequiredResponses(
     const net::BoundNetLog* net_log);
 
 // Returns true if the request shall not be reported to extensions.
-bool HideRequest(net::URLRequest* request);
+bool HideRequest(const net::URLRequest* request);
 
 // Returns whether |type| is a ResourceType that is handled by the web request
 // API.
@@ -185,6 +189,12 @@ const char* ResourceTypeToString(ResourceType::Type type);
 // success.
 bool ParseResourceType(const std::string& type_str,
                        ResourceType::Type* type);
+
+// Returns whether |extension| may access |url| based on host permissions.
+// In addition to that access is granted to about: URLs and extension URLs
+// that are in the scope of |extension|.
+bool CanExtensionAccessURL(const extensions::Extension* extension,
+                           const GURL& url);
 
 }  // namespace extension_web_request_api_helpers
 
