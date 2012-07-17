3a3cbc16c1b5b2390b5d756d8f5737a19ce8d6e8
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-17 03:01:29 +0000

    [EFL][DRT] Implement dumpFrameScrollPosition https://bugs.webkit.org/show_bug.cgi?id=87638
    Reviewed by Hajime Morita.
    
    Tools:
    
    * DumpRenderTree/efl/DumpRenderTree.cpp:
    (dumpFrameScrollPosition): Implemented.
    (shouldDumpFrameScrollPosition): Fixed wrong condition like other ports.
    
    LayoutTests:
    
    Updated expected results to dump scrolled position.
    
    * platform/efl/compositing/geometry/fixed-in-composited-expected.txt:
    * platform/efl/compositing/geometry/horizontal-scroll-composited-expected.txt:
    * platform/efl/compositing/geometry/video-fixed-scrolling-expected.txt:
    * platform/efl/compositing/overflow/fixed-position-ancestor-clip-expected.txt:
    * platform/efl/css2.1/20110323/abspos-containing-block-initial-001-expected.txt:
    * platform/efl/fast/block/positioning/rtl-fixed-positioning-expected.txt:
    * platform/efl/fast/block/positioning/vertical-rl/fixed-positioning-expected.txt:
    * platform/efl/fast/dom/focus-contenteditable-expected.txt:
    * platform/efl/fast/dom/scroll-reveal-left-overflow-expected.txt:
    * platform/efl/fast/dom/scroll-reveal-top-overflow-expected.txt:
    * platform/efl/fast/dynamic/anchor-lock-expected.txt:
    * platform/efl/fast/events/autoscroll-expected.txt:
    * platform/efl/fast/events/reveal-link-when-focused-expected.txt:
    * platform/efl/fast/layers/scroll-rect-to-visible-expected.txt:
    * platform/efl/fast/multicol/scrolling-overflow-expected.txt: Added.
    * platform/efl/fast/overflow/clip-rects-fixed-ancestor-expected.txt: Added.
    * platform/efl/fast/overflow/overflow_hidden-expected.txt:
    * platform/efl/fast/overflow/position-fixed-transform-clipping-expected.txt:
    * platform/efl/fast/overflow/scrollRevealButton-expected.txt:
    * platform/efl/fast/repaint/fixed-child-move-after-scroll-expected.txt:
    * platform/efl/fast/repaint/fixed-child-of-fixed-move-after-scroll-expected.txt:
    * platform/efl/fast/repaint/fixed-child-of-transformed-move-after-scroll-expected.txt:
    * platform/efl/fast/repaint/fixed-expected.txt:
    * platform/efl/fast/repaint/fixed-move-after-scroll-expected.txt:
    * platform/efl/fast/repaint/fixed-tranformed-expected.txt:
    * platform/efl/fast/repaint/repaint-during-scroll-expected.txt:
    * platform/efl/http/tests/navigation/postredirect-goback1-expected.txt:
    * platform/efl/transforms/2d/transform-fixed-container-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTree.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/efl/DumpRenderTree.cpp
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/text/CString.h>
+#include <wtf/text/StringBuilder.h>
 
 OwnPtr<DumpRenderTreeChrome> browser;
 Evas_Object* topLoadingFrame = 0;
@@ -102,9 +103,40 @@ static String dumpFramesAsText(Evas_Object* frame)
     return result;
 }
 
-static void dumpFrameScrollPosition(Evas_Object*)
+static void dumpFrameScrollPosition(Evas_Object* frame)
 {
-    notImplemented();
+    int x, y;
+    ewk_frame_scroll_pos_get(frame, &x, &y);
+    if (abs(x) > 0 || abs(y) > 0) {
+        StringBuilder result;
+
+        Evas_Object* parent = evas_object_smart_parent_get(frame);
+
+        // smart parent of main frame is view object.
+        if (parent != browser->mainView()) {
+            result.append("frame '");
+            result.append(ewk_frame_name_get(frame));
+            result.append("' ");
+        }
+
+        result.append("scrolled to ");
+        result.append(WTF::String::number(x));
+        result.append(",");
+        result.append(WTF::String::number(y));
+        result.append("\n");
+
+        printf("%s", result.toString().utf8().data());
+    }
+
+    if (gLayoutTestController->dumpChildFrameScrollPositions()) {
+        Eina_List* children = DumpRenderTreeSupportEfl::frameChildren(frame);
+        void* iterator;
+
+        EINA_LIST_FREE(children, iterator) {
+            Evas_Object* currentFrame = static_cast<Evas_Object*>(iterator);
+            dumpFrameScrollPosition(currentFrame);
+        }
+    }
 }
 
 static bool shouldLogFrameLoadDelegates(const String& pathOrURL)
@@ -307,7 +339,7 @@ static void dumpFrameContentsAsText(Evas_Object* frame)
 
 static bool shouldDumpFrameScrollPosition()
 {
-    return gLayoutTestController->dumpAsText() && !gLayoutTestController->dumpDOMAsWebArchive() && !gLayoutTestController->dumpSourceAsWebArchive();
+    return !gLayoutTestController->dumpAsText() && !gLayoutTestController->dumpDOMAsWebArchive() && !gLayoutTestController->dumpSourceAsWebArchive();
 }
 
 static bool shouldDumpPixelsAndCompareWithExpected()
