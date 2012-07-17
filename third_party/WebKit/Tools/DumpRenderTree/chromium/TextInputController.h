f3db60150a81e7fd7b6fe770a58e7f119c3cf6f8
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-17 08:10:50 +0000

    [Chromium] TextInputController shouldn't know about TestShell https://bugs.webkit.org/show_bug.cgi?id=91455
    Reviewed by Kent Tamura.
    
    This patch uses the same pattern we used for the
    AccessibilityController to remove the dependency on TestShell.
    
    * DumpRenderTree/chromium/TestShell.cpp:
    (TestShell::initialize):
    (TestShell::createMainWindow):
    (TestShell::~TestShell):
    * DumpRenderTree/chromium/TextInputController.cpp:
    (TextInputController::TextInputController):
    (TextInputController::insertText):
    (TextInputController::doCommand):
    (TextInputController::setMarkedText):
    (TextInputController::unmarkText):
    (TextInputController::hasMarkedText):
    (TextInputController::markedRange):
    (TextInputController::selectedRange):
    (TextInputController::firstRectForCharacterRange):
    (TextInputController::validAttributesForMarkedText):
    (TextInputController::setComposition):
    * DumpRenderTree/chromium/TextInputController.h:
    (WebKit):
    (TextInputController):
    (TextInputController::setWebView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122821 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TextInputController.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TextInputController.h
@@ -38,15 +38,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CppBoundClass.h"
 
-class TestShell;
-
 namespace WebKit {
-class WebFrame;
+class WebView;
 }
 
 class TextInputController : public CppBoundClass {
 public:
-    TextInputController(TestShell*);
+    TextInputController();
+
+    void setWebView(WebKit::WebView* webView) { m_webView = webView; }
 
     void insertText(const CppArgumentList&, CppVariant*);
     void doCommand(const CppArgumentList&, CppVariant*);
@@ -65,11 +65,7 @@ public:
     void setComposition(const CppArgumentList&, CppVariant*);
 
 private:
-    // Returns the test shell's main WebFrame.
-    static WebKit::WebFrame* getMainFrame();
-
-    // Non-owning pointer. The TextInputController is owned by the TestShell.
-    static TestShell* testShell;
+    WebKit::WebView* m_webView;
 };
 
 #endif // TextInputController_h
