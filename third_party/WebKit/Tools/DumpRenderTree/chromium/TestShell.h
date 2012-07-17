c9bc592c5e41a6ded2d98c959e24447c437ab34c
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-17 17:19:18 +0000

    Unreviewed, rolling out r122828. http://trac.webkit.org/changeset/122828 https://bugs.webkit.org/show_bug.cgi?id=91516
    DumpRenderTree crashes after printing test results. (Requested
    by vsevik on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-07-17
    
    * DumpRenderTree/chromium/TestRunner/TestInterfaces.cpp:
    (TestInterfaces::TestInterfaces):
    (TestInterfaces::~TestInterfaces):
    (TestInterfaces::bindTo):
    (TestInterfaces::resetAll):
    * DumpRenderTree/chromium/TestRunner/TestInterfaces.h:
    (TestInterfaces):
    * DumpRenderTree/chromium/TestShell.cpp:
    (TestShell::initialize):
    (TestShell::createMainWindow):
    (TestShell::~TestShell):
    (TestShell::resetTestController):
    (TestShell::bindJSObjectsToWindow):
    * DumpRenderTree/chromium/TestShell.h:
    (TestShell::accessibilityController):
    (TestShell):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NotificationPresenter.h"
 #include "TestEventPrinter.h"
 #include "TestInterfaces.h"
+#include "TextInputController.h"
 #include "WebPreferences.h"
 #include "WebViewHost.h"
 #include <string>
@@ -95,7 +96,7 @@ public:
     WebViewHost* webViewHost() const { return m_webViewHost.get(); }
     LayoutTestController* layoutTestController() const { return m_layoutTestController.get(); }
     EventSender* eventSender() const { return m_eventSender.get(); }
-    AccessibilityController* accessibilityController() const { return m_testInterfaces->accessibilityController(); }
+    AccessibilityController* accessibilityController() const { return m_accessibilityController.get(); }
 #if ENABLE(NOTIFICATIONS)
     NotificationPresenter* notificationPresenter() const { return m_notificationPresenter.get(); }
 #endif
@@ -215,9 +216,11 @@ private:
     OwnPtr<WebPermissions> m_webPermissions;
     OwnPtr<DRTDevToolsAgent> m_drtDevToolsAgent;
     OwnPtr<DRTDevToolsClient> m_drtDevToolsClient;
+    OwnPtr<AccessibilityController> m_accessibilityController;
     OwnPtr<TestInterfaces> m_testInterfaces;
     OwnPtr<EventSender> m_eventSender;
     OwnPtr<LayoutTestController> m_layoutTestController;
+    OwnPtr<TextInputController> m_textInputController;
 #if ENABLE(NOTIFICATIONS) || ENABLE(LEGACY_NOTIFICATIONS)
     OwnPtr<NotificationPresenter> m_notificationPresenter;
 #endif
