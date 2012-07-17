e88809aa49f5bf2215c87b977e68c5556d7fbcaa
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 20:27:35 +0000

    Revert 147045 - Bring up a content settings icon for ungestured registerProtocolHandler call.
    R=benwells@chromium.org
    BUG=122020
    TEST=None
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=144957
    
    Review URL: https://chromiumcodereview.appspot.com/10584042
    
    TBR=gbillock@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10782038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_content_setting_bubble_model_delegate.cc
+++ chrome/browser/ui/browser_content_setting_bubble_model_delegate.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/chrome_pages.h"
-#include "chrome/common/url_constants.h"
 
 // The URL for when the user clicks "learn more" on the mixed scripting page
 // icon bubble.
@@ -38,13 +37,7 @@ void BrowserContentSettingBubbleModelDelegate::ShowContentSettingsPage(
     GURL url(google_util::AppendGoogleLocaleParam(
         GURL(kInsecureScriptHelpUrl)));
     chrome::AddSelectedTabWithURL(browser_, url, content::PAGE_TRANSITION_LINK);
-    return;
+  } else {
+    chrome::ShowContentSettings(browser_, type);
   }
-
-  if (type == CONTENT_SETTINGS_TYPE_PROTOCOL_HANDLERS) {
-    chrome::ShowSettingsSubPage(browser_, chrome::kHandlerSettingsSubPage);
-    return;
-  }
-
-  chrome::ShowContentSettings(browser_, type);
 }
--- chrome/chrome_dll.gypi
+++ chrome/chrome_dll.gypi
@@ -250,7 +250,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 'app/nibs/ContentBlockedPlugins.xib',
                 'app/nibs/ContentBlockedPopups.xib',
                 'app/nibs/ContentBlockedGeolocation.xib',
-                'app/nibs/ContentProtocolHandlers.xib',
                 'app/nibs/DownloadItem.xib',
                 'app/nibs/DownloadShelf.xib',
                 'app/nibs/EditSearchEngine.xib',
