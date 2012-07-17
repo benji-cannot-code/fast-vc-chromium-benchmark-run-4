4f3d08f63bad3127910eb06a078080d5589bd6d5
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 23:46:44 +0000

    Bring up a content settings icon for ungestured registerProtocolHandler call.
    Reverted by shess@chromium.org, turned out not to be related to the problem.
    Revert CL: https://chromiumcodereview.appspot.com/10782038/
    Original CL: https://chromiumcodereview.appspot.com/10584042
    
    R=benwells@chromium.org
    BUG=122020
    TEST=None
    TBR=gbillock@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10789053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147135 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_content_setting_bubble_model_delegate.cc
+++ chrome/browser/ui/browser_content_setting_bubble_model_delegate.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/chrome_pages.h"
+#include "chrome/common/url_constants.h"
 
 // The URL for when the user clicks "learn more" on the mixed scripting page
 // icon bubble.
@@ -37,7 +38,13 @@ void BrowserContentSettingBubbleModelDelegate::ShowContentSettingsPage(
     GURL url(google_util::AppendGoogleLocaleParam(
         GURL(kInsecureScriptHelpUrl)));
     chrome::AddSelectedTabWithURL(browser_, url, content::PAGE_TRANSITION_LINK);
-  } else {
-    chrome::ShowContentSettings(browser_, type);
+    return;
   }
+
+  if (type == CONTENT_SETTINGS_TYPE_PROTOCOL_HANDLERS) {
+    chrome::ShowSettingsSubPage(browser_, chrome::kHandlerSettingsSubPage);
+    return;
+  }
+
+  chrome::ShowContentSettings(browser_, type);
 }
