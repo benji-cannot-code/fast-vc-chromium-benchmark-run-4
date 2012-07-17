a49cf6a9a79897cd03e89402938e5e5829d45025
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 22:28:59 +0000

    Removing usage of profile_->GetHistory() from the codebase in order to remove that function.
    This was previously reverted, found not to be the cause of the issue, now I am reinstating the changes.
    
    BUG=97804
    TEST=existing unittests 
    
    TBR=sky@chromium.org 
    
    Review URL: https://chromiumcodereview.appspot.com/10782004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147112 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/memory_purger.cc
+++ chrome/browser/memory_purger.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/history/history.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/safe_browsing/safe_browsing_service.h"
 #include "chrome/browser/ui/browser_list.h"
@@ -111,7 +112,7 @@ void MemoryPurger::PurgeBrowser() {
     // Spinning up the history service is expensive, so we avoid doing it if it
     // hasn't been done already.
     HistoryService* history_service =
-        profiles[i]->GetHistoryServiceWithoutCreating();
+        HistoryServiceFactory::GetForProfileWithoutCreating(profiles[i]);
     if (history_service)
       history_service->UnloadBackend();
 
