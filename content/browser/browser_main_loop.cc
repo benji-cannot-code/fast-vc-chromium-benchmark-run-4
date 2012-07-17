b6485e6b2662411917fd6b5de87398d95c1d7247
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 19:01:01 +0000

    Histograms - Initialize HistogramSynchronizer in browser process. Moved initialization of BrowserGpuChannelHostFactory and ImageTransportFactory into BrowserThreadsStarted.
    R=jar@chromium.org, jam@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10783010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147058 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/gpu/browser_gpu_channel_host_factory.h"
 #include "content/browser/gpu/gpu_process_host.h"
 #include "content/browser/gpu/gpu_process_host_ui_shim.h"
+#include "content/browser/histogram_synchronizer.h"
 #include "content/browser/in_process_webkit/webkit_thread.h"
 #include "content/browser/net/browser_online_state_observer.h"
 #include "content/browser/plugin_service_impl.h"
@@ -432,11 +433,6 @@ void BrowserMainLoop::CreateThreads() {
     }
   }
 
-  BrowserGpuChannelHostFactory::Initialize();
-#if defined(USE_AURA)
-  ImageTransportFactory::Initialize();
-#endif
-
   BrowserThreadsStarted();
 
   if (parts_.get())
@@ -601,6 +597,13 @@ void BrowserMainLoop::InitializeMainThread() {
 
 
 void BrowserMainLoop::BrowserThreadsStarted() {
+  HistogramSynchronizer::GetInstance();
+
+  content::BrowserGpuChannelHostFactory::Initialize();
+#if defined(USE_AURA)
+  ImageTransportFactory::Initialize();
+#endif
+
   // RDH needs the IO thread to be created.
   resource_dispatcher_host_.reset(new ResourceDispatcherHostImpl());
 
