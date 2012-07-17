3694ba1163345c2ee1eb86fc8ce7352128ceaf4c
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 11:39:50 +0000

    Android: further simplication for test runners.
    Overtime, there has been a few changes in the way we run:
    - individual test suite versus all suites.
    - single device versus sharded.
    - debug info.
    
    This patch simplifies the many different code paths and ensures
    we'll have less breakages (and a more consistent way to add features).
    
    BUG=
    TEST=existing android tests (single device, multiple devices, emulator).
    
    
    Review URL: https://chromiumcodereview.appspot.com/10777017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146985 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/single_test_runner.py
+++ build/android/pylib/single_test_runner.py
@@ -338,4 +338,6 @@ class SingleTestRunner(BaseTestRunner):
       self.dump_debug_info.StopRecordingLog()
     if self.test_package.performance_test:
       self.adb.TearDownPerformanceTest()
+    if self.dump_debug_info:
+      self.dump_debug_info.ArchiveNewCrashFiles()
     super(SingleTestRunner, self).TearDown()
