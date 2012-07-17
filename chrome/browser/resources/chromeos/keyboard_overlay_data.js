b9e2945e7e01215891347160620b16a4b8c46144
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 23:14:20 +0000

    Add Alt+W and Alt+Shift+W to the keyboard overlay.
    BUG=137708
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10786042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147127 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -15866,6 +15866,8 @@ var keyboardOverlayData = {
     'up<>ALT<>CTRL': 'keyboardOverlayHome',
     'v<>CTRL': 'keyboardOverlayPaste',
     'v<>CTRL<>SHIFT': 'keyboardOverlayPasteAsPlainText',
+    'w<>ALT': 'keyboardOverlayNextWindow',
+    'w<>ALT<>SHIFT': 'keyboardOverlayPreviousWindow',
     'w<>CTRL': 'keyboardOverlayCloseTab',
     'w<>CTRL<>SHIFT': 'keyboardOverlayCloseWindow',
     'x<>CTRL': 'keyboardOverlayCut',
