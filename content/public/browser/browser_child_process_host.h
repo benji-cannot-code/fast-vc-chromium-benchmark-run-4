401a1d3577c22fc320c39897125bbae9746121c4
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-17 01:25:34 +0000

    Cleanup: Add a note to BrowserChildProcessHost::Launch().
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10787027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@146943 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/browser_child_process_host.h
+++ content/public/browser/browser_child_process_host.h
@@ -34,6 +34,7 @@ class CONTENT_EXPORT BrowserChildProcessHost : public IPC::Sender {
   virtual ~BrowserChildProcessHost() {}
 
   // Derived classes call this to launch the child process asynchronously.
+  // Takes ownership of |cmd_line|.
   virtual void Launch(
 #if defined(OS_WIN)
       const FilePath& exposed_dir,
