36f965ff49c5e8569dd20f8ce3277bc6f336b31b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 20:25:11 +0000

    Chromium Mac: Add TEXTURE_RECTANGLE_ARB support to CCVideoLayerImpl https://bugs.webkit.org/show_bug.cgi?id=91169
    Patch by Sailesh Agrawal <sail@chromium.org> on 2012-07-18
    Reviewed by Adrienne Walker.
    
    Source/Platform:
    
    Added a "orientation" field to WebCompositorIOSurfaceQuad. This is used at draw time to flip the texture if necessary.
    
    * chromium/public/WebCompositorIOSurfaceQuad.h: Added orientation field to the constructor.
    (WebKit::WebCompositorIOSurfaceQuad::orientation):
    (WebCompositorIOSurfaceQuad): The new orientation field should be set to Flipped if the texture should be flipped when drawing.
    
    Source/WebCore:
    
    This extends CCVideoLayerImpl to support TEXTURE_RECTANGLE_ARB. This texture target is used by the Mac hardware accelerated video decoder.
    
    No new tests (HW video decode on Mac is being tested manually.).
    
    * platform/chromium/support/WebCompositorIOSurfaceQuad.cpp:
    (WebKit::WebCompositorIOSurfaceQuad::create): Added an orientation argument.
    (WebKit::WebCompositorIOSurfaceQuad::WebCompositorIOSurfaceQuad): Added an orientation argument.
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::drawIOSurfaceQuad): Added support for non-flipped IOSurface textures.
    * platform/graphics/chromium/LayerRendererChromium.h:
    (LayerRendererChromium): Changed TextureIOSurfaceProgram to be non-flipped. To draw flipped textures drawIOSurfaceQuad sets a different value for texTransformLocation.
    * platform/graphics/chromium/cc/CCIOSurfaceLayerImpl.cpp:
    (WebCore::CCIOSurfaceLayerImpl::appendQuads): Updated call to CCIOSurfaceDrawQuad constructor.
    * platform/graphics/chromium/cc/CCVideoLayerImpl.cpp:
    (WebCore::CCVideoLayerImpl::appendQuads): Added support for drawing TEXTURE_RECTANGLE_ARB textures.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCVideoLayerImpl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCVideoLayerImpl.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TextStream.h"
 #include "TextureAllocator.h"
 #include "cc/CCGraphicsContext.h"
+#include "cc/CCIOSurfaceDrawQuad.h"
 #include "cc/CCLayerTreeHostImpl.h"
 #include "cc/CCProxy.h"
 #include "cc/CCQuadCuller.h"
@@ -221,6 +222,12 @@ void CCVideoLayerImpl::appendQuads(CCQuadCuller& quadList, const CCSharedQuadSta
 #endif
         OwnPtr<CCTextureDrawQuad> textureQuad = CCTextureDrawQuad::create(sharedQuadState, quadRect, m_frame->textureId(), premultipliedAlpha, uvRect, flipped);
         quadList.append(textureQuad.release());
+        break;
+    }
+    case Extensions3D::TEXTURE_RECTANGLE_ARB: {
+        IntSize textureSize(m_frame->width(), m_frame->height()); 
+        OwnPtr<CCIOSurfaceDrawQuad> ioSurfaceQuad = CCIOSurfaceDrawQuad::create(sharedQuadState, quadRect, textureSize, m_frame->textureId(), CCIOSurfaceDrawQuad::Unflipped);
+        quadList.append(ioSurfaceQuad.release());
         break;
     }
     case Extensions3DChromium::GL_TEXTURE_EXTERNAL_OES: {
