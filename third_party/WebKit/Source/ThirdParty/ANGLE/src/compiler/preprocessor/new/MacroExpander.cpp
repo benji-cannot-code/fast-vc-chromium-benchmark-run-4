31f0a1eac283c142039e99ed3b3c7d242cff7b67
kkristof@inf.u-szeged.hu <kkristof@inf.u-szeged.hu@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 07:51:41 +0000

    [Qt] Buildfix after r122870. https://bugs.webkit.org/show_bug.cgi?id=89039
    Reviewed by Csaba Osztrogonác
    
    * src/compiler/preprocessor/new/MacroExpander.cpp:
    (pp::MacroExpander::ungetToken):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/ThirdParty/ANGLE/src/compiler/preprocessor/new/MacroExpander.cpp
+++ third_party/WebKit/Source/ThirdParty/ANGLE/src/compiler/preprocessor/new/MacroExpander.cpp
@@ -127,7 +127,6 @@ void MacroExpander::ungetToken(const Token& token)
     {
         MacroContext* context = mContextStack.back();
         context->unget();
-        assert(context->index >= 0);
         assert(context->replacements[context->index] == token);
     }
     else
