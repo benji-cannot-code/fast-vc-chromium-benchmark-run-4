240ba1926efb9e411cd64cfc1ddc18743454a98e
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 01:08:39 +0000

    Record metrics to measure the usage of Blob([ArrayBuffer]) to eventually deprecate it https://bugs.webkit.org/show_bug.cgi?id=90534
    Reviewed by Jian Li.
    
    We are removing ArrayBuffer support in Blob constructor (in favor of
    ArrayBufferView) but before doing that we should record its use relative to ArrayBufferView.
    http://dev.w3.org/2006/webapi/FileAPI/#dfn-Blob
    
    No new tests as this has no functional changes.
    
    * fileapi/WebKitBlobBuilder.cpp:
    (WebCore::WebKitBlobBuilder::append):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122910 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/fileapi/WebKitBlobBuilder.cpp
+++ third_party/WebKit/Source/WebCore/fileapi/WebKitBlobBuilder.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Blob.h"
 #include "ExceptionCode.h"
 #include "File.h"
+#include "HistogramSupport.h"
 #include "LineEnding.h"
 #include "ScriptExecutionContext.h"
 #include "TextEncoding.h"
@@ -47,6 +48,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/text/CString.h>
 
 namespace WebCore {
+
+enum BlobConstructorArrayBufferOrView {
+    BlobConstructorArrayBuffer,
+    BlobConstructorArrayBufferView,
+    BlobConstructorArrayBufferOrViewMax,
+};
 
 // static
 PassRefPtr<WebKitBlobBuilder> WebKitBlobBuilder::create(ScriptExecutionContext* context)
@@ -102,22 +109,23 @@ void WebKitBlobBuilder::append(ScriptExecutionContext* context, ArrayBuffer* arr
 {
     String consoleMessage("ArrayBuffer values are deprecated in Blob Constructor. Use ArrayBufferView instead.");
     context->addConsoleMessage(JSMessageSource, LogMessageType, WarningMessageLevel, consoleMessage);
+
+    HistogramSupport::histogramEnumeration("WebCore.Blob.constructor.ArrayBufferOrView", BlobConstructorArrayBuffer, BlobConstructorArrayBufferOrViewMax);
+
     if (!arrayBuffer)
         return;
-    Vector<char>& buffer = getBuffer();
-    size_t oldSize = buffer.size();
-    buffer.append(static_cast<const char*>(arrayBuffer->data()), arrayBuffer->byteLength());
-    m_size += buffer.size() - oldSize;
+
+    appendBytesData(arrayBuffer->data(), arrayBuffer->byteLength());
 }
 
 void WebKitBlobBuilder::append(ArrayBufferView* arrayBufferView)
 {
+    HistogramSupport::histogramEnumeration("WebCore.Blob.constructor.ArrayBufferOrView", BlobConstructorArrayBufferView, BlobConstructorArrayBufferOrViewMax);
+
     if (!arrayBufferView)
         return;
-    Vector<char>& buffer = getBuffer();
-    size_t oldSize = buffer.size();
-    buffer.append(static_cast<const char*>(arrayBufferView->baseAddress()), arrayBufferView->byteLength());
-    m_size += buffer.size() - oldSize;
+
+    appendBytesData(arrayBufferView->baseAddress(), arrayBufferView->byteLength());
 }
 #endif
 
@@ -140,6 +148,15 @@ void WebKitBlobBuilder::append(Blob* blob)
         m_size += blobSize;
         m_items.append(BlobDataItem(blob->url(), 0, blobSize));
     }
+}
+
+
+void WebKitBlobBuilder::appendBytesData(const void* data, size_t length)
+{
+    Vector<char>& buffer = getBuffer();
+    size_t oldSize = buffer.size();
+    buffer.append(static_cast<const char*>(data), length);
+    m_size += buffer.size() - oldSize;
 }
 
 PassRefPtr<Blob> WebKitBlobBuilder::getBlob(const String& contentType)
