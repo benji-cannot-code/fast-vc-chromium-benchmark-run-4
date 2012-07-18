c4843ca526e929a94a18459fcf0e0fca190b1f4c
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 04:11:48 +0000

    Implement IBusEngineFactoryService
    With this CL, these classes are compiled and tested but not in use production binary at this moment.
    
    BUG=126947
    TEST=chromeos_unittests,ui_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10693144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147176 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chromeos/dbus/ibus/mock_ibus_engine_factory_service.cc
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/dbus/ibus/mock_ibus_engine_factory_service.h"
+
+namespace chromeos {
+
+MockIBusEngineFactoryService::MockIBusEngineFactoryService() {
+}
+
+MockIBusEngineFactoryService::~MockIBusEngineFactoryService() {
+}
+
+void MockIBusEngineFactoryService::SetCreateEngineHandler(
+    const CreateEngineHandler& create_engine_handler) {
+}
+
+void MockIBusEngineFactoryService::UnsetCreateEngineHandler() {
+}
+
+}  // namespace chromeos
---
+++ chromeos/dbus/ibus/mock_ibus_engine_factory_service.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROMEOS_DBUS_IBUS_MOCK_IBUS_ENGINE_FACTORY_SERVICE_H_
+#define CHROMEOS_DBUS_IBUS_MOCK_IBUS_ENGINE_FACTORY_SERVICE_H_
+
+#include "base/basictypes.h"
+#include "chromeos/dbus/ibus/ibus_engine_factory_service.h"
+
+namespace chromeos {
+class MockIBusEngineFactoryService : public IBusEngineFactoryService {
+ public:
+  MockIBusEngineFactoryService();
+  virtual ~MockIBusEngineFactoryService();
+
+  virtual void SetCreateEngineHandler(
+      const CreateEngineHandler& create_engine_handler) OVERRIDE;
+  virtual void UnsetCreateEngineHandler() OVERRIDE;
+
+  DISALLOW_COPY_AND_ASSIGN(MockIBusEngineFactoryService);
+};
+
+}  // namespace chromeos
+
+#endif  // CHROMEOS_DBUS_IBUS_MOCK_IBUS_ENGINE_FACTORY_SERVICE_H_
