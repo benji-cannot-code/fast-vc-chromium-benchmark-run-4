8422a4ff5c55abe25ce6bd24871824e2c1f641ce
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 16:25:54 +0000

    Coverity: Initialize a member variable.
    CID_COUNT=1
    CID=104455
    BUG=none
    TEST=none
    R=groby
    TBR=sky
    
    
    Review URL: https://chromiumcodereview.appspot.com/10787042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147247 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/histogram_message_filter.cc
+++ content/browser/histogram_message_filter.cc
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-HistogramMessageFilter::HistogramMessageFilter() {
-}
+HistogramMessageFilter::HistogramMessageFilter() {}
 
 void HistogramMessageFilter::OnChannelConnected(int32 peer_pid) {
   BrowserMessageFilter::OnChannelConnected(peer_pid);
