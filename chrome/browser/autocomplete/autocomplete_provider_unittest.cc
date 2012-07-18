105bc62e4f3c58b9a0edd69b2a2d682e7b0e46d2
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 02:09:29 +0000

    autocomplete: Rename autocomplete_unittest.cc to autocomplete_provider_unittest.cc
    This is the last patch in this series.
    
    BUG=135128
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10703169
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147163 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_provider_unittest.cc
+++ chrome/browser/autocomplete/autocomplete_provider_unittest.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/autocomplete/autocomplete_provider.h"
+
 #include "base/bind.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
@@ -13,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_controller.h"
 #include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
-#include "chrome/browser/autocomplete/autocomplete_provider.h"
 #include "chrome/browser/autocomplete/autocomplete_provider_listener.h"
 #include "chrome/browser/autocomplete/keyword_provider.h"
 #include "chrome/browser/autocomplete/search_provider.h"
