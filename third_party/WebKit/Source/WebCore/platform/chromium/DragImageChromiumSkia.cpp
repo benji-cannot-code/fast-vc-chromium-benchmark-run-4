c04e78e1a04901fbe17b12e36c9378d60bec5da7
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 19:51:12 +0000

    [chromium] Fix crash in DragImageTest caused by r122996 https://bugs.webkit.org/show_bug.cgi?id=91653
    Patch by Varun Jain <varunjain@chromium.org> on 2012-07-18
    Reviewed by Tony Chang.
    
    Covered by existing DragImageTest.
    
    * platform/chromium/DragImageChromiumSkia.cpp:
    (WebCore::deleteDragImage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/DragImageChromiumSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/DragImageChromiumSkia.cpp
@@ -53,7 +53,8 @@ IntSize dragImageSize(DragImageRef image)
 
 void deleteDragImage(DragImageRef image)
 {
-    delete image->bitmap;
+    if (image)
+        delete image->bitmap;
     delete image;
 }
 
