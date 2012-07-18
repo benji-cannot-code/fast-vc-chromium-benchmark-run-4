3bedaeac104cfc959af6f6d9ccccda6b4c81d96a
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 14:20:47 +0000

    Turn on fragments for prerender.
    R=dominich@chromium.org
    BUG=118224
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10778022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147235 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_browsertest.cc
+++ chrome/browser/prerender/prerender_browsertest.cc
@@ -1706,15 +1706,24 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest, PrerenderRendererCrash) {
   ui_test_utils::RunMessageLoop();
 }
 
+IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
+                       PrerenderPageWithFragment) {
+  PrerenderTestURL("files/prerender/prerender_page.html#fragment",
+                   FINAL_STATUS_USED,
+                   1);
+  NavigateToDestURL();
+  ASSERT_TRUE(IsEmptyPrerenderLinkManager());
+}
+
 // Checks that we correctly use a prerendered page when navigating to a
 // fragment.
 // DISABLED: http://crbug.com/84154
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                        DISABLED_PrerenderPageNavigateFragment) {
-  PrerenderTestURL("files/prerender/prerender_fragment.html",
+  PrerenderTestURL("files/prerender/no_prerender_page.html",
                    FINAL_STATUS_APP_TERMINATING,
                    1);
-  NavigateToURL("files/prerender/prerender_fragment.html#fragment");
+  NavigateToURL("files/prerender/no_prerender_page.html#fragment");
 }
 
 // Checks that we correctly use a prerendered page when we prerender a fragment
@@ -1722,10 +1731,10 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
 // http://crbug.com/83901
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                        DISABLED_PrerenderFragmentNavigatePage) {
-  PrerenderTestURL("files/prerender/prerender_fragment.html#fragment",
+  PrerenderTestURL("files/prerender/no_prerender_page.html#fragment",
                    FINAL_STATUS_APP_TERMINATING,
                    1);
-  NavigateToURL("files/prerender/prerender_fragment.html");
+  NavigateToURL("files/prerender/no_prerender_page.html");
 }
 
 // Checks that we correctly use a prerendered page when we prerender a fragment
@@ -1733,10 +1742,10 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
 // DISABLED: http://crbug.com/84154
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                        DISABLED_PrerenderFragmentNavigateFragment) {
-  PrerenderTestURL("files/prerender/prerender_fragment.html#other_fragment",
+  PrerenderTestURL("files/prerender/no_prerender_page.html#other_fragment",
                    FINAL_STATUS_APP_TERMINATING,
                    1);
-  NavigateToURL("files/prerender/prerender_fragment.html#fragment");
+  NavigateToURL("files/prerender/no_prerender_page.html#fragment");
 }
 
 // Checks that we correctly use a prerendered page when the page uses a client
@@ -1745,10 +1754,10 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                        DISABLED_PrerenderClientRedirectFromFragment) {
   PrerenderTestURL(
-      CreateClientRedirect("files/prerender/prerender_fragment.html#fragment"),
+      CreateClientRedirect("files/prerender/no_prerender_page.html#fragment"),
       FINAL_STATUS_APP_TERMINATING,
       2);
-  NavigateToURL("files/prerender/prerender_fragment.html");
+  NavigateToURL("files/prerender/no_prerender_page.html");
 }
 
 // Checks that we correctly use a prerendered page when the page uses a client
@@ -1757,10 +1766,10 @@ IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
 IN_PROC_BROWSER_TEST_F(PrerenderBrowserTest,
                        DISABLED_PrerenderClientRedirectToFragment) {
   PrerenderTestURL(
-      CreateClientRedirect("files/prerender/prerender_fragment.html"),
+      CreateClientRedirect("files/prerender/no_prerender_page.html"),
       FINAL_STATUS_APP_TERMINATING,
       2);
-  NavigateToURL("files/prerender/prerender_fragment.html#fragment");
+  NavigateToURL("files/prerender/no_prerender_page.html#fragment");
 }
 
 // Checks that we correctly use a prerendered page when the page uses JS to set
