0c394b96a16676e3cda06cddaaf2d6a103d344d8
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 19:02:14 +0000

    Add flag to Java -> JavaScript to disable exposing inherited methods.
    Add a flag that allows Java Object injection into JavaScript to specify whether
    or not only publicly declared methods of the specified class can be injected or
    whether all public methods of a class should be injected (the current default).
    This allows the specific APIs to only inject the few methods that should
    be injected and not unknowingly expose any other methods (those include
    getClass, getField, getMethod, getAnnotation, etc.. anything from Object or other
    super classes).  In addition, this flag is passed down to other Java Object
    injections that happen as the result of Objects being returned from method calls.
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10793035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147281 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/java/java_bound_object.h
+++ content/browser/renderer_host/java/java_bound_object.h
@@ -24,9 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // created and destroyed on different threads.
 class JavaBoundObject {
  public:
-  // Takes a Java object and creates a JavaBoundObject around it. Returns an
-  // NPObject with a ref count of one which owns the JavaBoundObject.
-  static NPObject* Create(const base::android::JavaRef<jobject>& object);
+  // Takes a Java object and creates a JavaBoundObject around it. Also takes
+  // a boolean that determines whether or not inherited methods are allowed
+  // to be called as well.  This property propagates to all Objects that get
+  // implicitly exposed as return values as well. Returns an NPObject with
+  // a ref count of one which owns the JavaBoundObject.
+  static NPObject* Create(const base::android::JavaRef<jobject>& object,
+                          bool allow_inherited_methods);
 
   virtual ~JavaBoundObject();
 
@@ -41,7 +45,8 @@ class JavaBoundObject {
               NPVariant* result);
 
  private:
-  explicit JavaBoundObject(const base::android::JavaRef<jobject>& object);
+  explicit JavaBoundObject(const base::android::JavaRef<jobject>& object,
+                           bool allow_inherited_methods);
 
   void EnsureMethodsAreSetUp() const;
 
@@ -54,6 +59,9 @@ class JavaBoundObject {
   // scoped_ptr in STL containers as we can't copy it.
   typedef std::multimap<std::string, linked_ptr<JavaMethod> > JavaMethodMap;
   mutable JavaMethodMap methods_;
+  mutable bool are_methods_set_up_;
+
+  bool allow_inherited_methods_;
 
   DISALLOW_IMPLICIT_CONSTRUCTORS(JavaBoundObject);
 };
