aef50eae5b9a2e13cb13c6b1e6ac2b3ecebdb947
fgalligan@chromium.org <fgalligan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 02:58:09 +0000

    Add support for encrypted WebM files as defined in the RFC.
    The WebM parser now reads the HMAC and IV from every encrypted
    Block and stores that information in the DecryptorConfig object.
    Added two new elements ContentEncAESSettings and
    AESSettingsCipherMode used by encrypted WebM files.
    
    BUG=119845
    TEST=media_unittests --gtest_filter=HmacAesDecryptorTest
    
    Review URL: https://chromiumcodereview.appspot.com/10535029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147169 0039d316-1c4b-4281-b951-d872f2087c98

--- media/webm/webm_parser.cc
+++ media/webm/webm_parser.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This file contains code to parse WebM file elements. It was created
 // from information in the Matroska spec.
 // http://www.matroska.org/technical/specs/index.html
+// This file contains code for encrypted WebM. Current WebM
+// encrypted request for comments specification is here
+// http://wiki.webmproject.org/encryption/webm-encryption-rfc
 
 #include <iomanip>
 
@@ -233,12 +236,17 @@ static const ElementIdInfo kContentCompressionIds[] = {
 };
 
 static const ElementIdInfo kContentEncryptionIds[] = {
+  {LIST, kWebMIdContentEncAESSettings},
   {UINT, kWebMIdContentEncAlgo},
   {BINARY, kWebMIdContentEncKeyID},
   {BINARY, kWebMIdContentSignature},
   {BINARY, kWebMIdContentSigKeyID},
   {UINT, kWebMIdContentSigAlgo},
   {UINT, kWebMIdContentSigHashAlgo},
+};
+
+static const ElementIdInfo kContentEncAESSettingsIds[] = {
+  {UINT, kWebMIdAESSettingsCipherMode},
 };
 
 static const ElementIdInfo kCuesIds[] = {
@@ -377,6 +385,7 @@ static const ListElementInfo kListElementInfo[] = {
   LIST_ELEMENT_INFO(kWebMIdContentEncoding, 4, kContentEncodingIds),
   LIST_ELEMENT_INFO(kWebMIdContentCompression, 5, kContentCompressionIds),
   LIST_ELEMENT_INFO(kWebMIdContentEncryption, 5, kContentEncryptionIds),
+  LIST_ELEMENT_INFO(kWebMIdContentEncAESSettings, 6, kContentEncAESSettingsIds),
   LIST_ELEMENT_INFO(kWebMIdCues, 1, kCuesIds),
   LIST_ELEMENT_INFO(kWebMIdCuePoint, 2, kCuePointIds),
   LIST_ELEMENT_INFO(kWebMIdCueTrackPositions, 3, kCueTrackPositionsIds),
