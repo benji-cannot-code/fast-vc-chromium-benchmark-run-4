e98927de93c48c9f29fd317582999a17255809f0
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 18:21:33 +0000

    Unreviewed, rolling out r122984. http://trac.webkit.org/changeset/122984 https://bugs.webkit.org/show_bug.cgi?id=91171
    Broken the shared build, need to export a SkData in skia
    
    * platform/graphics/skia/GraphicsContextSkia.cpp:
    (WebCore::GraphicsContext::setURLForRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/skia/GraphicsContextSkia.cpp
@@ -38,17 +38,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Gradient.h"
 #include "ImageBuffer.h"
 #include "IntRect.h"
-#include "KURL.h"
 #include "NativeImageSkia.h"
 #include "NotImplemented.h"
 #include "PlatformContextSkia.h"
 
-#include "SkAnnotation.h"
 #include "SkBitmap.h"
 #include "SkBlurMaskFilter.h"
 #include "SkColorFilter.h"
 #include "SkCornerPathEffect.h"
-#include "SkData.h"
 #include "SkLayerDrawLooper.h"
 #include "SkShader.h"
 #include "SkiaUtils.h"
@@ -998,11 +995,6 @@ void GraphicsContext::setPlatformTextDrawingMode(TextDrawingModeFlags mode)
 
 void GraphicsContext::setURLForRect(const KURL& link, const IntRect& destRect)
 {
-    if (paintingDisabled())
-        return;
-
-    SkAutoDataUnref url(SkData::NewWithCString(link.string().utf8().data()));
-    SkAnnotateRectWithURL(platformContext()->canvas(), destRect, url.get());
 }
 
 void GraphicsContext::setPlatformShouldAntialias(bool enable)
