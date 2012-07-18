9e58e83e772f928250bc248d93ffc4546e54bab7
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 01:16:14 +0000

    Teach CodeGenerator to support for static, readonly, attributes https://bugs.webkit.org/show_bug.cgi?id=88920 <rdar://problem/11650330>
    Reviewed by Oliver Hunt.
    
    Update the parser to be able to accept the static keyword for attribute. We will treat static attributes
    like custom static functions. They call the implementing class directly, and pass in the ExecState as a script context.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GetAttributeGetterName): Factor out the construction of the attribute getter function name.
    (GetAttributeSetterName): Factor out the construction of the attribute setter function name.
    (GenerateHeader): Determine that a class has read-write properties only if there is a read-write attribute that
    is not static.
    (GenerateAttributesHashTable): Skip static attributes in the object hash table. They will be added to the constructor
    hash table.
    (GenerateImplementation): Look for static attributes to add to the constructor hash table. Make a call to the static
    function in the class.
    * bindings/scripts/IDLParser.pm:
    (ParseInterface): Update the processing because of the regex change.
    * bindings/scripts/IDLStructure.pm: Update the attribute regex.
    * bindings/scripts/test/JS/JSTestObj.cpp: Update test results.
    * bindings/scripts/test/JS/JSTestObj.h: Update test results.
    * bindings/scripts/test/TestObj.idl: Add test cases.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/IDLParser.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/IDLParser.pm
@@ -338,11 +338,12 @@ sub ParseInterface
             if ($line =~ /\Wattribute\W/) {
                 $line =~ /$IDLStructure::interfaceAttributeSelector/;
 
-                my $attributeType = (defined($1) ? $1 : die("Parsing error!\nSource:\n$line\n)"));
-                my $attributeExtendedAttributes = (defined($2) ? $2 : " "); chop($attributeExtendedAttributes);
+                my $isStatic = defined($1);
+                my $attributeType = (defined($2) ? $2 : die("Parsing error!\nSource:\n$line\n)"));
+                my $attributeExtendedAttributes = (defined($3) ? $3 : " "); chop($attributeExtendedAttributes);
 
-                my $attributeDataType = (defined($3) ? $3 : die("Parsing error!\nSource:\n$line\n)"));
-                my $attributeDataName = (defined($4) ? $4 : die("Parsing error!\nSource:\n$line\n)"));
+                my $attributeDataType = (defined($4) ? $4 : die("Parsing error!\nSource:\n$line\n)"));
+                my $attributeDataName = (defined($5) ? $5 : die("Parsing error!\nSource:\n$line\n)"));
   
                 ('' =~ /^/); # Reset variables needed for regexp matching
 
@@ -354,6 +355,7 @@ sub ParseInterface
 
                 my $newDataNode = new domAttribute();
                 $newDataNode->type($attributeType);
+                $newDataNode->isStatic($isStatic);
                 $newDataNode->signature(new domSignature());
 
                 $newDataNode->signature->name($attributeDataName);
@@ -363,7 +365,7 @@ sub ParseInterface
                 my $arrayRef = $dataNode->attributes;
                 push(@$arrayRef, $newDataNode);
 
-                print "  |  |>  Attribute; TYPE \"$attributeType\" DATA NAME \"$attributeDataName\" DATA TYPE \"$attributeDataType\" GET EXCEPTION? \"$getterException\" SET EXCEPTION? \"$setterException\"" .
+                print "  |  |>  Attribute; STATIC? \"$isStatic\" TYPE \"$attributeType\" DATA NAME \"$attributeDataName\" DATA TYPE \"$attributeDataType\" GET EXCEPTION? \"$getterException\" SET EXCEPTION? \"$setterException\"" .
                     dumpExtendedAttributes("\n  |                 ", $newDataNode->signature->extendedAttributes) . "\n" unless $beQuiet;
 
                 $getterException =~ s/\s+//g;
@@ -392,7 +394,7 @@ sub ParseInterface
                 $newDataNode->signature->type($methodType);
                 $newDataNode->signature->extendedAttributes(parseExtendedAttributes($methodExtendedAttributes));
 
-                print "  |  |-  Method; TYPE \"$methodType\" NAME \"$methodName\" EXCEPTION? \"$methodException\"" .
+                print "  |  |-  Method; STATIC? \"$isStatic\" TYPE \"$methodType\" NAME \"$methodName\" EXCEPTION? \"$methodException\"" .
                     dumpExtendedAttributes("\n  |              ", $newDataNode->signature->extendedAttributes) . "\n" unless $beQuiet;
 
                 $methodException =~ s/\s+//g;
