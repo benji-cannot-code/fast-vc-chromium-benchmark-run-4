36e6528f68102e100249322d2039f96945e30fdc
alexis.menard@openbossa.org <alexis.menard@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 08:57:47 +0000

    [EFL] Build fix in WebKitTestRunner. https://bugs.webkit.org/show_bug.cgi?id=91567
    Reviewed by Kentaro Hara.
    
    sleep() is defined in unistd.h, we need to include it.
    
    * WebKitTestRunner/efl/TestControllerEfl.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122936 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/efl/TestControllerEfl.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/efl/TestControllerEfl.cpp
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Ecore.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <wtf/Platform.h>
 #include <wtf/text/WTFString.h>
 
