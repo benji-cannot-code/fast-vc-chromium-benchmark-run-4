30ac055da06bb16a7599cc92d74c193dedc1f5f4
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 17:58:59 +0000

    Convert svg/css/scientific-numbers.html to a text only test https://bugs.webkit.org/show_bug.cgi?id=91641
    Reviewed by Dirk Schulze.
    
    This is a JS test so we don't need the pixel dump.
    
    * svg/css/script-tests/scientific-numbers.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/svg/css/script-tests/scientific-numbers.js
+++ third_party/WebKit/LayoutTests/svg/css/script-tests/scientific-numbers.js
@@ -1,5 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Test scientific numbers on <length> values for SVG presentation attributes.")
+if (window.testRunner)
+    testRunner.dumpAsText();
 createSVGTestCase();
 
 var text = createSVGElement("text");
