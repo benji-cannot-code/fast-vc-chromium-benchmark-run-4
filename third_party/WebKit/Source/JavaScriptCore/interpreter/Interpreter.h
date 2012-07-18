10abe12a449c42e0de6d026b07d1f409bf310868
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 23:26:06 +0000

    dumpCallFrame is broken in ToT https://bugs.webkit.org/show_bug.cgi?id=91444
    Reviewed by Gavin Barraclough.
    
    Various changes have been made to the SF calling convention, but
    dumpCallFrame has not been updated to reflect these changes.
    That resulted in both bogus information, as well as numerous
    assertions of sadness.
    
    This patch makes dumpCallFrame actually work again and adds the
    wonderful feature of telling you the name of the variable that a
    register reflects, or what value it contains.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::nameForRegister):
        A really innefficient mechanism for finding the name of a local register.
        This should only ever be used by debug code so this should be okay.
    * bytecode/CodeBlock.h:
    (CodeBlock):
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::generate):
        Debug builds no longer throw away a functions symbol table, this allows
        us to actually perform a register# to name mapping
    * dfg/DFGJITCompiler.cpp:
    (JSC::DFG::JITCompiler::link):
        We weren't propogating the bytecode offset here leading to assertions
        in debug builds when dumping bytecode of DFG compiled code.
    * interpreter/Interpreter.cpp:
    (JSC):
    (JSC::Interpreter::dumpRegisters):
         Rework to actually be correct.
    (JSC::getCallerInfo):
         Return the byteocde offset as well now, given we have to determine it
         anyway.
    (JSC::Interpreter::getStackTrace):
    (JSC::Interpreter::retrieveCallerFromVMCode):
    * interpreter/Interpreter.h:
    (Interpreter):
    * jsc.cpp:
    (GlobalObject::finishCreation):
    (functionDumpCallFrame):
         Give debug builds of JSC a method for calling dumpCallFrame so we can
         inspect a callframe without requiring us to break in a debugger.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.h
+++ third_party/WebKit/Source/JavaScriptCore/interpreter/Interpreter.h
@@ -249,6 +249,9 @@ namespace JSC {
         void dumpSampleData(ExecState* exec);
         void startSampling();
         void stopSampling();
+
+        JS_EXPORT_PRIVATE void dumpCallFrame(CallFrame*);
+
     private:
         enum ExecutionFlag { Normal, InitializeAndReturn };
 
@@ -280,7 +283,6 @@ namespace JSC {
 
         JSValue privateExecute(ExecutionFlag, RegisterFile*, CallFrame*);
 
-        void dumpCallFrame(CallFrame*);
         void dumpRegisters(CallFrame*);
         
         bool isCallBytecode(Opcode opcode) { return opcode == getOpcode(op_call) || opcode == getOpcode(op_construct) || opcode == getOpcode(op_call_eval); }
