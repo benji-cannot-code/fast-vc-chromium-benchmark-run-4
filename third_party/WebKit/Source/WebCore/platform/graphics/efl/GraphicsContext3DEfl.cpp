f09a79de6e9332a0539be4723903283ef438757f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 13:19:13 +0000

    [WK2][EFL] Divide ENABLE(WEBGL) into ENABLE(WEBGL) and USE(3D_GRAPHICS) in CMakeLists.txt https://bugs.webkit.org/show_bug.cgi?id=91584
    Patch by YoungTaeck Song <youngtaeck.song@samsung.com> on 2012-07-18
    Reviewed by Noam Rosenthal.
    
    This patch is a subset of Efl's UI_SIDE_COMPOSITING implementation.
    Modified CMakeLists.txt so that the basic 3D-graphics sources can be compiled even when WebGL is disabled.
    
    * CMakeLists.txt:
    * PlatformEfl.cmake:
    * platform/graphics/GraphicsContext3D.h:
    (GraphicsContext3D):
    * platform/graphics/efl/GraphicsContext3DEfl.cpp:
    (WebCore::GraphicsContext3D::platformGraphicsContext3D):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@122958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/efl/GraphicsContext3DEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/efl/GraphicsContext3DEfl.cpp
@@ -52,7 +52,7 @@ GraphicsContext3D::~GraphicsContext3D()
 {
 }
 
-PlatformGraphicsContext3D GraphicsContext3D::platformGraphicsContext3D() const
+PlatformGraphicsContext3D GraphicsContext3D::platformGraphicsContext3D()
 {
     return m_private->platformGraphicsContext3D();
 }
