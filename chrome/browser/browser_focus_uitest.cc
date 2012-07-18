32aef98089c154bfb7caa5c61f9ee82c1c4e6a83
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 13:44:38 +0000

    browser: Remove location_bar_view.h from browser_focus_uitest.cc
    BUG=125846
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10787032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147226 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_focus_uitest.cc
+++ chrome/browser/browser_focus_uitest.cc
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(TOOLKIT_VIEWS)
 #include "chrome/browser/ui/views/frame/browser_view.h"
-#include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #endif
 
 #if defined(TOOLKIT_GTK)
