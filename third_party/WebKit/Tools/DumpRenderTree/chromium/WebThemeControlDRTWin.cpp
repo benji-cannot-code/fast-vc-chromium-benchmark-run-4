a020447ae54aa41f64644cf73519201cc1248cb0
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 20:21:34 +0000

    [chromium] Unreviewed, more compile fixes on Chromium Win.
    Source/WebKit/chromium:
    
    * tests/WebTransformationMatrixTest.cpp:
    (WebKit::TEST):
    
    Tools:
    
    * DumpRenderTree/chromium/TestShellWin.cpp:
    (TestShell::waitTestFinished):
    * DumpRenderTree/chromium/WebThemeControlDRTWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebThemeControlDRTWin.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebThemeControlDRTWin.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkPath.h"
 #include "third_party/skia/include/core/SkRect.h"
 
+#include <algorithm>
 #include <wtf/Assertions.h>
 
 using namespace std;
@@ -75,8 +76,8 @@ static SkIRect validate(const SkIRect& rect, WebThemeControlDRTWin::Type ctype)
         // The maximum width and height is 13.
         // Center the square in the passed rectangle.
         const int maxControlSize = 13;
-        int controlSize = min(rect.width(), rect.height());
-        controlSize = min(controlSize, maxControlSize);
+        int controlSize = std::min(rect.width(), rect.height());
+        controlSize = std::min(controlSize, maxControlSize);
 
         retval.fLeft   = rect.fLeft + (rect.width() / 2) - (controlSize / 2);
         retval.fRight  = retval.fLeft + controlSize - 1;
