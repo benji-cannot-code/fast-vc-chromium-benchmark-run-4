ed1041595088c7a6a31630c71156b4f97da1333e
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-18 00:16:41 +0000

    [NaCl SDK] Use .tar.bz2 for bundle extension instead of .bz2
    BUG=none
    TEST=none
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/10690186
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147140 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/build_sdk.py
+++ native_client_sdk/src/build_tools/build_sdk.py
@@ -535,7 +535,7 @@ def main(args):
   if not skip_examples:
     CopyExamples(pepperdir, toolchains)
 
-  tarname = 'naclsdk_' + platform + '.bz2'
+  tarname = 'naclsdk_' + platform + '.tar.bz2'
   if 'pnacl' in toolchains:
     tarname = 'p' + tarname
   tarfile = os.path.join(OUT_DIR, tarname)
