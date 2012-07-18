6e75d600e55a2244c5b192ca1126f380458c5db1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-18 20:01:46 +0000

    [EFL] Add central error management to EFL port https://bugs.webkit.org/show_bug.cgi?id=91598
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-18
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    Define possible error types in ErrorsEfl so
    that we can reuse the header in both WebKit1
    and WebKit2. This is inspired from the GTK
    port.
    
    No new tests, no behavior change.
    
    * PlatformEfl.cmake:
    * platform/efl/ErrorsEfl.cpp: Added.
    (WebCore):
    (WebCore::cancelledError):
    (WebCore::blockedError):
    (WebCore::cannotShowURLError):
    (WebCore::interruptedForPolicyChangeError):
    (WebCore::cannotShowMIMETypeError):
    (WebCore::fileDoesNotExistError):
    (WebCore::pluginWillHandleLoadError):
    (WebCore::downloadNetworkError):
    (WebCore::downloadCancelledByUserError):
    (WebCore::downloadDestinationError):
    (WebCore::printError):
    (WebCore::printerNotFoundError):
    (WebCore::invalidPageRangeToPrint):
    * platform/efl/ErrorsEfl.h: Added.
    (WebCore):
    
    Source/WebKit/efl:
    
    Make use of ErrorsEfl header from WebCore in
    EFL's FrameLoaderClient now that we have
    a central place for errors.
    
    * WebCoreSupport/FrameLoaderClientEfl.cpp:
    (WebCore::FrameLoaderClientEfl::cancelledError):
    (WebCore::FrameLoaderClientEfl::blockedError):
    (WebCore::FrameLoaderClientEfl::cannotShowURLError):
    (WebCore::FrameLoaderClientEfl::interruptedForPolicyChangeError):
    (WebCore::FrameLoaderClientEfl::cannotShowMIMETypeError):
    (WebCore::FrameLoaderClientEfl::fileDoesNotExistError):
    (WebCore::FrameLoaderClientEfl::pluginWillHandleLoadError):
    (WebCore::FrameLoaderClientEfl::shouldFallBack):
    
    Source/WebKit2:
    
    Make use of ErrorsEfl header from WebCore in
    WebKit2, for Ewk_Web_Error and WebErrorsEfl.
    
    * UIProcess/API/efl/ewk_web_error.cpp:
    (ewk_web_error_type_get):
    * UIProcess/API/efl/ewk_web_error.h:
    * WebProcess/WebCoreSupport/efl/WebErrorsEfl.cpp:
    (WebKit::cancelledError):
    (WebKit::blockedError):
    (WebKit::cannotShowURLError):
    (WebKit::interruptedForPolicyChangeError):
    (WebKit::cannotShowMIMETypeError):
    (WebKit::fileDoesNotExistError):
    (WebKit::pluginWillHandleLoadError):
    
    Tools:
    
    Map WebKitNetworkError to NSURLErrorDomain when
    printing in DumpRenderTree so that the output
    matches the expected one.
    
    * DumpRenderTree/efl/DumpRenderTreeChrome.cpp:
    (descriptionSuitableForTestResult):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/efl/WebErrorsEfl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/efl/WebErrorsEfl.cpp
@@ -27,8 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebErrors.h"
 
-#include "WKError.h"
-#include "WebError.h"
+#include <WebCore/ErrorsEfl.h>
 #include <WebCore/ResourceError.h>
 #include <WebCore/ResourceRequest.h>
 #include <WebCore/ResourceResponse.h>
@@ -37,47 +36,39 @@ using namespace WebCore;
 
 namespace WebKit {
 
-// FIXME: Export following error codes so that application can understand.
-// We should establish Efl port's error system because application cannot understand those local define.
-// See https://bugs.webkit.org/show_bug.cgi?id=90783 for detail.
-enum {
-    kWKErrorCodeCancelled =                             300,
-    kWKErrorCodeFileDoesNotExist =                      301,
-};
-
 ResourceError cancelledError(const ResourceRequest& request)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodeCancelled, request.url().string(), "Request cancelled");
+    return WebCore::cancelledError(request);
 }
 
 ResourceError blockedError(const ResourceRequest& request)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodeCannotUseRestrictedPort, request.url().string(), "Request blocked");
+    return WebCore::blockedError(request);
 }
 
 ResourceError cannotShowURLError(const ResourceRequest& request)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodeCannotShowURL, request.url().string(), "Cannot show URL");
+    return WebCore::cannotShowURLError(request);
 }
 
 ResourceError interruptedForPolicyChangeError(const ResourceRequest& request)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodeFrameLoadInterruptedByPolicyChange, request.url().string(), "Frame load interrupted by policy change");
+    return WebCore::interruptedForPolicyChangeError(request);
 }
 
 ResourceError cannotShowMIMETypeError(const ResourceResponse& response)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodeCannotShowMIMEType, response.url().string(), "Cannot show mimetype");
+    return WebCore::cannotShowMIMETypeError(response);
 }
 
 ResourceError fileDoesNotExistError(const ResourceResponse& response)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodeFileDoesNotExist, response.url().string(), "File does not exist");
+    return WebCore::fileDoesNotExistError(response);
 }
 
 ResourceError pluginWillHandleLoadError(const ResourceResponse& response)
 {
-    return ResourceError(WebError::webKitErrorDomain(), kWKErrorCodePlugInWillHandleLoad, response.url().string(), "Plugin will handle load");
+    return WebCore::pluginWillHandleLoadError(response);
 }
 
 } // namespace WebKit
