bd610671919d7a881195783f70f29e9262fe11fd
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 07:12:21 +0000

    Fix NetInternalsTest.netInternalsDnsViewIncognitoClears after r147404. It turns out that content::RunThisRunLoop can be called recursively.
    TBR=sky
    Review URL: https://chromiumcodereview.appspot.com/10806029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147405 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/chrome_test_launcher.cc
+++ chrome/test/base/chrome_test_launcher.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/test/test_launcher.h"
 
+#include <stack>
+
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/memory/linked_ptr.h"
 #include "base/run_loop.h"
 #include "base/scoped_temp_dir.h"
 #include "base/test/test_file_util.h"
@@ -115,17 +118,21 @@ class ChromeTestLauncherDelegate : public test_launcher::TestLauncherDelegate {
 
   virtual void PreRunMessageLoop(base::RunLoop* run_loop) OVERRIDE {
 #if !defined(USE_AURA) && defined(TOOLKIT_VIEWS)
-    DCHECK(!handler_.get());
     if (content::BrowserThread::CurrentlyOn(content::BrowserThread::UI)) {
-      handler_.reset(new views::AcceleratorHandler);
-      run_loop->set_dispatcher(handler_.get());
+      linked_ptr<views::AcceleratorHandler> handler(
+          new views::AcceleratorHandler);
+      handlers_.push(handler);
+      run_loop->set_dispatcher(handler.get());
     }
 #endif
   }
 
   virtual void PostRunMessageLoop() {
 #if !defined(USE_AURA) && defined(TOOLKIT_VIEWS)
-    handler_.reset();
+    if (content::BrowserThread::CurrentlyOn(content::BrowserThread::UI)) {
+      DCHECK_EQ(handlers_.empty(), false);
+      handlers_.pop();
+    }
 #endif
   }
 
@@ -133,7 +140,7 @@ class ChromeTestLauncherDelegate : public test_launcher::TestLauncherDelegate {
   ScopedTempDir temp_dir_;
 
 #if !defined(USE_AURA) && defined(TOOLKIT_VIEWS)
-  scoped_ptr<views::AcceleratorHandler> handler_;
+  std::stack<linked_ptr<views::AcceleratorHandler> > handlers_;
 #endif
 
   DISALLOW_COPY_AND_ASSIGN(ChromeTestLauncherDelegate);
