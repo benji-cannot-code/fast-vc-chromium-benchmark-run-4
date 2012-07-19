62bbcd503d45d6c21e922517ff2104267d8148f8
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 02:41:47 +0000

    Don't send a DragLeave to the renderer if we didn't send a DragEnter.
    BUG=136834
    TEST=dragging over an interstitial doesn't crash the renderer
    
    
    Review URL: https://chromiumcodereview.appspot.com/10783037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147371 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_drag_dest_mac.mm
+++ content/browser/web_contents/web_drag_dest_mac.mm
@@ -143,7 +143,8 @@ int GetModifierFlags() {
   if (currentRVH_ != webContents_->GetRenderViewHost())
     return;
 
-  // Nothing to do in the interstitial case.
+  if ([self onlyAllowsNavigation])
+    return;
 
   if (delegate_)
     delegate_->OnDragLeave();
@@ -188,7 +189,6 @@ int GetModifierFlags() {
     [self draggingEntered:info view:view];
 
   // Check if we only allow navigation and navigate to a url on the pasteboard.
-  BOOL result = YES;
   if ([self onlyAllowsNavigation]) {
     NSPasteboard* pboard = [info draggingPasteboard];
     if ([pboard containsURLData]) {
@@ -197,13 +197,14 @@ int GetModifierFlags() {
       webContents_->OpenURL(OpenURLParams(
           url, Referrer(), CURRENT_TAB, content::PAGE_TRANSITION_AUTO_BOOKMARK,
           false));
+      return YES;
     } else {
-      result = NO;
+      return NO;
     }
-  } else {
-    if (delegate_)
-      delegate_->OnDrop();
   }
+
+  if (delegate_)
+    delegate_->OnDrop();
 
   currentRVH_ = NULL;
 
@@ -219,7 +220,7 @@ int GetModifierFlags() {
 
   dropData_.reset();
 
-  return result;
+  return YES;
 }
 
 // Given |data|, which should not be nil, fill it in using the contents of the
