f63ad7951c2318059723f5cce219e7642942d0f8
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 09:39:59 +0000

    PyAuto: Fix race in wait for login form reload after policy refresh
    Refreshing device policy with the login form visible causes the form to
    reload. It seems that on some of the autotest bots, the reload is already
    finished by the time _WaitForLoginFormToReload() is called, causing the
    wait to miss the reload and time out eventually.
    
    This CL introduces a sentinel variable that gets updated when the form
    reloads, allowing _WaitForLoginFormToReload() to reliably check for a
    reload, whether it happened before the function was called or during its
    runtime.
    
    BUG=None (bugtracker currently down)
    TEST=PyAuto policy tests on Chrome OS
    
    
    Review URL: https://chromiumcodereview.appspot.com/10790049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147418 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/chromeos_ephemeral.py
+++ chrome/test/functional/chromeos_ephemeral.py
@@ -126,8 +126,9 @@ class ChromeosEphemeral(policy_base.PolicyTestBase):
 
   def testEnablingBeforeSession(self):
     """Checks that a new session can be made ephemeral."""
+    self.PrepareToWaitForLoginFormReload()
     self._SetEphemeralUsersEnabled(True)
-    self._WaitForLoginFormReload()
+    self.WaitForLoginFormReload()
 
     self.Login(user_index=0)
     self._AssertLocalStatePrefsEmpty()
@@ -140,8 +141,9 @@ class ChromeosEphemeral(policy_base.PolicyTestBase):
 
   def testEnablingDuringSession(self):
     """Checks that an existing non-ephemeral session is not made ephemeral."""
+    self.PrepareToWaitForLoginFormReload()
     self._SetEphemeralUsersEnabled(False)
-    self._WaitForLoginFormReload()
+    self.WaitForLoginFormReload()
 
     self.Login(user_index=0)
     self._AssertLocalStatePrefsSet(user_indexes=[0])
@@ -157,8 +159,9 @@ class ChromeosEphemeral(policy_base.PolicyTestBase):
 
   def testDisablingDuringSession(self):
     """Checks that an existing ephemeral session is not made non-ephemeral."""
+    self.PrepareToWaitForLoginFormReload()
     self._SetEphemeralUsersEnabled(True)
-    self._WaitForLoginFormReload()
+    self.WaitForLoginFormReload()
 
     self.Login(user_index=0)
     # TODO(bartfab): Remove this when crosbug.com/20709 is fixed.
@@ -177,8 +180,9 @@ class ChromeosEphemeral(policy_base.PolicyTestBase):
 
   def testEnablingEphemeralUsersCleansUp(self):
     """Checks that persistent information is cleared."""
+    self.PrepareToWaitForLoginFormReload()
     self._SetEphemeralUsersEnabled(False)
-    self._WaitForLoginFormReload()
+    self.WaitForLoginFormReload()
 
     self.Login(user_index=0)
     # TODO(bartfab): Remove this when crosbug.com/20709 is fixed.
