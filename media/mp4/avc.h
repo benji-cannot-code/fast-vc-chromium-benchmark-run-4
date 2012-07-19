42f47c8facc3dd674d780ee208b3f916cba4fe74
xiaomings@google.com <xiaomings@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 19:43:45 +0000

    Add HE AAC support to ISO BMFF.
    Was https://chromiumcodereview.appspot.com/10753005/.
    
    The original cl also includes abstraction of BitReader class used by H264Parser. It includes bugs that break the H264Parser.
    
    In this cl I removed the abstraction and make the BitReader used by H264Parser independent to the one used by esds parser because of:
    
    1. The original cl introduce bugs.
    2. Even if we fix the bugs, we may not be able to make the generalized class as fast as the old one while keeping the abstraction.
    3. The H264 bit stream use very special syntax on escaping and trailing zero bits, which might not be a good candidate for abstraction.
    
    BUG=134445
    TEST=BitReaderTest, AACTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10780026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147511 0039d316-1c4b-4281-b951-d872f2087c98

--- media/mp4/avc.h
+++ media/mp4/avc.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
-#include "media/base/channel_layout.h"
 #include "media/base/media_export.h"
 
 namespace media {
@@ -24,10 +23,7 @@ class MEDIA_EXPORT AVC {
   static bool InsertParameterSets(
       const AVCDecoderConfigurationRecord& avc_config,
       std::vector<uint8>* buffer);
-
-  static ChannelLayout ConvertAACChannelCountToChannelLayout(int count);
 };
-
 
 }  // namespace mp4
 }  // namespace media
