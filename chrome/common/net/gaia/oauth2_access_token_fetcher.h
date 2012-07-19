050b04c31522551b1dafccd2cd9eedd9daba53df
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 09:37:18 +0000

    Update API of OAuth2AccessTokenFetcher to return an expiration date.
     OAuth2 access token have an expiration date that is returned by the
    server.  Until now, we were not retrieving this information and had no
    way to reuse the tokens correctly. This change updates the API so this
    information is available to the caller.
    
     This is a reland of revision 147199.
     Original review: https://chromiumcodereview.appspot.com/10694154
    
    TEST=None
    BUG=None
    TBR=munjal, kalman, Miranda Callahan, Ben Goodger (Google)
    R=tbarzic
    
    
    Review URL: https://chromiumcodereview.appspot.com/10796006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/net/gaia/oauth2_access_token_fetcher.h
+++ chrome/common/net/gaia/oauth2_access_token_fetcher.h
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class OAuth2AccessTokenFetcherTest;
 
+namespace base {
+class Time;
+}
+
 namespace net {
 class URLFetcher;
 class URLRequestContextGetter;
@@ -76,7 +80,8 @@ class OAuth2AccessTokenFetcher : public net::URLFetcherDelegate {
   void EndGetAccessToken(const net::URLFetcher* source);
 
   // Helper mehtods for reporting back results.
-  void OnGetTokenSuccess(const std::string& access_token);
+  void OnGetTokenSuccess(const std::string& access_token,
+                         const base::Time& expiration_time);
   void OnGetTokenFailure(const GoogleServiceAuthError& error);
 
   // Other helpers.
@@ -87,7 +92,8 @@ class OAuth2AccessTokenFetcher : public net::URLFetcherDelegate {
       const std::string& refresh_token,
       const std::vector<std::string>& scopes);
   static bool ParseGetAccessTokenResponse(const net::URLFetcher* source,
-                                          std::string* access_token);
+                                          std::string* access_token,
+                                          int* expires_in);
 
   // State that is set during construction.
   OAuth2AccessTokenConsumer* const consumer_;
