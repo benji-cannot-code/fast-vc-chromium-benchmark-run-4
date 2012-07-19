a3176faa72fe46ea640bd0bf1cbfa0f7750ec3dd
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 20:48:10 +0000

    Revert 147517 - there are outstanding review comments.
    Basic hooks for web intents pyauto tests.
    
    
    TEST=python chrome/test/functional/webintents.py
    
    
    Review URL: https://chromiumcodereview.appspot.com/10692143
    
    TBR=danhn@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10796046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147525 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_controller.cc
+++ chrome/browser/ui/intents/web_intent_picker_controller.cc
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_controller.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_intents_dispatcher.h"
@@ -172,10 +171,8 @@ void WebIntentPickerController::SetIntentsDispatcher(
                  weak_ptr_factory_.GetWeakPtr()));
 }
 
-void WebIntentPickerController::ShowDialog(
-    const string16& action,
-    const string16& type,
-    const AutomationCallback* set_extensions) {
+void WebIntentPickerController::ShowDialog(const string16& action,
+                                           const string16& type) {
   // Only show a picker once.
   // TODO(gbillock): There's a hole potentially admitting multiple
   // in-flight dispatches since we don't create the picker
@@ -247,15 +244,11 @@ void WebIntentPickerController::ShowDialog(
                    weak_ptr_factory_.GetWeakPtr()));
   }
 
-  if (set_extensions) {
-    set_extensions->Run();
-  } else {
     GetCWSIntentsRegistry(tab_contents_)->GetIntentServices(
         action, type,
         base::Bind(&WebIntentPickerController::OnCWSIntentServicesAvailable,
                    weak_ptr_factory_.GetWeakPtr()));
   }
-}
 
 void WebIntentPickerController::Observe(
     int type,
@@ -579,7 +572,26 @@ void WebIntentPickerController::OnCWSIntentServicesAvailable(
       continue;
     }
 
-    AddSuggestedExtension(info);
+    picker_model_->AddSuggestedExtension(
+        info.name,
+        info.id,
+        info.average_rating);
+
+    pending_async_count_++;
+    net::URLFetcher* icon_url_fetcher = net::URLFetcher::Create(
+        0,
+        info.icon_url,
+        net::URLFetcher::GET,
+        new URLFetcherTrampoline(
+            base::Bind(
+                &WebIntentPickerController::OnExtensionIconURLFetchComplete,
+                weak_ptr_factory_.GetWeakPtr(), info.id)));
+
+    icon_url_fetcher->SetLoadFlags(
+        net::LOAD_DO_NOT_SEND_COOKIES | net::LOAD_DO_NOT_SAVE_COOKIES);
+    icon_url_fetcher->SetRequestContext(
+        tab_contents_->profile()->GetRequestContext());
+    icon_url_fetcher->Start();
   }
 
   AsyncOperationFinished();
@@ -686,34 +698,9 @@ void WebIntentPickerController::OnExtensionInstallServiceAvailable(
   AsyncOperationFinished();
 }
 
-void WebIntentPickerController::AddSuggestedExtension(
-    const CWSIntentsRegistry::IntentExtensionInfo& info) {
-  picker_model_->AddSuggestedExtension(info.name, info.id, info.average_rating);
-
-  pending_async_count_++;
-  net::URLFetcher* icon_url_fetcher = net::URLFetcher::Create(
-      0,
-      info.icon_url,
-      net::URLFetcher::GET,
-      new URLFetcherTrampoline(
-          base::Bind(
-              &WebIntentPickerController::OnExtensionIconURLFetchComplete,
-              weak_ptr_factory_.GetWeakPtr(), info.id)));
-
-  icon_url_fetcher->SetLoadFlags(
-      net::LOAD_DO_NOT_SEND_COOKIES | net::LOAD_DO_NOT_SAVE_COOKIES);
-  icon_url_fetcher->SetRequestContext(
-      tab_contents_->profile()->GetRequestContext());
-  icon_url_fetcher->Start();
-}
-
 void WebIntentPickerController::AsyncOperationFinished() {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
   if (--pending_async_count_ == 0) {
-    content::NotificationService::current()->Notify(
-        chrome::NOTIFICATION_WEB_INTENT_PICKER_LOADED,
-        content::Source<content::WebContents>(tab_contents_->web_contents()),
-        content::Details<std::string>(NULL));
     if (picker_)
       picker_->OnPendingAsyncCompleted();
   }
