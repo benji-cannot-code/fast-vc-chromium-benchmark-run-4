bddfdc955328d12f315922c962449488219e046b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-19 18:41:25 +0000

    Crash in WebCore::StyleResolver::collectMatchingRulesForList https://bugs.webkit.org/show_bug.cgi?id=90803
    Patch by Douglas Stockwell <dstockwell@google.com> on 2012-07-19
    Reviewed by Andreas Kling.
    
    Source/WebCore:
    
    When a ProcessingInstruction was removed from the document the owner
    was removed, but the style resolver was not guaranteed to be updated.
    It was then possible for an inconsistent version of the stylesheet to
    remain visible in the DOM. Fixed by removing an invalid condition and
    mirroring the logic from StyleElement.
    
    Test: fast/css/xml-stylesheet-removed.xhtml
    
    * dom/ProcessingInstruction.cpp:
    (WebCore::ProcessingInstruction::removedFrom): Mirror the logic from
    StyleElement -- always update the style resolver.
    
    LayoutTests:
    
    * fast/css/xml-stylesheet-removed-expected.txt: Added.
    * fast/css/xml-stylesheet-removed.xhtml: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123128 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ProcessingInstruction.cpp
+++ third_party/WebKit/Source/WebCore/dom/ProcessingInstruction.cpp
@@ -313,7 +313,8 @@ void ProcessingInstruction::removedFrom(ContainerNode* insertionPoint)
         m_sheet = 0;
     }
 
-    if (m_cachedSheet)
+    // If we're in document teardown, then we don't need to do any notification of our sheet's removal.
+    if (document()->renderer())
         document()->styleResolverChanged(DeferRecalcStyle);
 }
 
