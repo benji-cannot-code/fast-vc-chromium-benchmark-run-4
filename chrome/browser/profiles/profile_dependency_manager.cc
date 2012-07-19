e105e7a9d7cd78886a6ce7761d764f6ebe435dfb
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 15:17:12 +0000

    Add PrerenderLinkManager to the ProfileDependancyManager.
    It turns out, this is important to do. Who knew?
    
    R=mirandac@chromium.org
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10800034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_dependency_manager.cc
+++ chrome/browser/profiles/profile_dependency_manager.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/autocomplete_action_predictor_factory.h"
 #include "chrome/browser/predictors/predictor_database_factory.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor_factory.h"
+#include "chrome/browser/prerender/prerender_link_manager_factory.h"
 #include "chrome/browser/prerender/prerender_manager_factory.h"
 #include "chrome/browser/printing/cloud_print/cloud_print_proxy_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -233,6 +234,7 @@ void ProfileDependencyManager::AssertFactoriesBuilt() {
   predictors::PredictorDatabaseFactory::GetInstance();
   predictors::ResourcePrefetchPredictorFactory::GetInstance();
   prerender::PrerenderManagerFactory::GetInstance();
+  prerender::PrerenderLinkManagerFactory::GetInstance();
   ProfileSyncServiceFactory::GetInstance();
 #if defined(ENABLE_PROTECTOR_SERVICE)
   protector::ProtectorServiceFactory::GetInstance();
