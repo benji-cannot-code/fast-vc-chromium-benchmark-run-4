09a59e0f60970684c9fa36b9b2398d79154c6bd6
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 20:59:10 +0000

    Allow blacklisting just sharing textures between processes.
    This will allow us to move some of the hardcoded blacklisting logic into the blacklist.
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10698172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_util.cc
+++ chrome/browser/gpu_util.cc
@@ -39,6 +39,7 @@ const char kGpuFeatureNameWebgl[] = "webgl";
 const char kGpuFeatureNameMultisampling[] = "multisampling";
 const char kGpuFeatureNameFlash3d[] = "flash_3d";
 const char kGpuFeatureNameFlashStage3d[] = "flash_stage3d";
+const char kGpuFeatureNameTextureSharing[] = "texture_sharing";
 const char kGpuFeatureNameAll[] = "all";
 const char kGpuFeatureNameUnknown[] = "unknown";
 
@@ -238,6 +239,8 @@ GpuFeatureType StringToGpuFeatureType(const std::string& feature_string) {
     return content::GPU_FEATURE_TYPE_FLASH3D;
   else if (feature_string == kGpuFeatureNameFlashStage3d)
     return content::GPU_FEATURE_TYPE_FLASH_STAGE3D;
+  else if (feature_string == kGpuFeatureNameTextureSharing)
+    return content::GPU_FEATURE_TYPE_TEXTURE_SHARING;
   else if (feature_string == kGpuFeatureNameAll)
     return content::GPU_FEATURE_TYPE_ALL;
   return content::GPU_FEATURE_TYPE_UNKNOWN;
@@ -260,6 +263,8 @@ std::string GpuFeatureTypeToString(GpuFeatureType type) {
       matches.push_back(kGpuFeatureNameFlash3d);
     if (type & content::GPU_FEATURE_TYPE_FLASH_STAGE3D)
       matches.push_back(kGpuFeatureNameFlashStage3d);
+    if (type & content::GPU_FEATURE_TYPE_TEXTURE_SHARING)
+      matches.push_back(kGpuFeatureNameTextureSharing);
     if (!matches.size())
       matches.push_back(kGpuFeatureNameUnknown);
   }
@@ -336,6 +341,14 @@ Value* GetFeatureStatus() {
           command_line.HasSwitch(switches::kDisableFlashStage3d),
           "Using Stage3d in Flash has been disabled, either via about:flags or"
           " command line.",
+          false
+      },
+      {
+          "texture_sharing",
+          flags & content::GPU_FEATURE_TYPE_TEXTURE_SHARING,
+          command_line.HasSwitch(switches::kDisableImageTransportSurface),
+          "Sharing textures between processes has been disabled, either via"
+          " about:flags or command line.",
           false
       }
   };
