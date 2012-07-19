e671735339f7b4ee0109ba52efc45772fdafb2f5
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 18:15:11 +0000

    Merge worker_pool into run_test_cases.
    This makes run_test_cases.py completely standalone so it can be easily used on
    the swarm slave.
    
    R=cmp@chromium.org
    NOTRY=true
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10803030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147492 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/trace_inputs_smoke_test.py
+++ tools/isolate/trace_inputs_smoke_test.py
@@ -13,7 +13,7 @@ import sys
 import tempfile
 import unittest
 
-import worker_pool
+import run_test_cases
 
 FULLNAME = os.path.abspath(__file__)
 ROOT_DIR = os.path.dirname(FULLNAME)
@@ -433,7 +433,7 @@ class TraceInputsImport(TraceInputsBase):
           cmd, cwd, tracename, True)
       return (tracename, resultcode, output)
 
-    with worker_pool.ThreadPool(PARALLEL) as pool:
+    with run_test_cases.ThreadPool(PARALLEL) as pool:
       api = self.trace_inputs.get_api()
       with api.get_tracer(self.log) as tracer:
         pool.add_task(
