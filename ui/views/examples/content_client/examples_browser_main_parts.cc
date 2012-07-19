338dc7d9a2a5efb07cb9c595cf2fdad50f3bc681
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-19 03:22:06 +0000

    views: Remove unused net include from examples_browser_main_parts.cc
    BUG=137800
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10798004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147386 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/content_client/examples_browser_main_parts.cc
+++ ui/views/examples/content_client/examples_browser_main_parts.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/shell/shell_browser_context.h"
 #include "googleurl/src/gurl.h"
-#include "net/base/net_module.h"
 #include "ui/views/examples/examples_window.h"
 #include "ui/views/focus/accelerator_handler.h"
 #include "ui/views/test/test_views_delegate.h"
