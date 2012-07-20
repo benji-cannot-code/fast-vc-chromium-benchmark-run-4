ab33ab9f5b3dc5e2f15c70f7a698a668bc8dc108
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 03:47:09 +0000

    A Speculative Revert for r147597 - Convert ProtocolHandlerRegistry to be a ProfileKeyedService.
    BUG=129200
    TEST=protocol_handler_registry_browsertest.cc,protocol_handler_registry_unittest.cc
    
    willchan@ -> profile_io changes
    +bauerb -> content_settings changes
    
    
    Review URL: https://chromiumcodereview.appspot.com/10546083
    
    TBR=smckay@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10815026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147612 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.cc
+++ base/message_loop.cc
@@ -323,10 +323,6 @@ void MessageLoop::QuitNow() {
   }
 }
 
-bool MessageLoop::IsType(Type type) const {
-  return type_ == type;
-}
-
 static void QuitCurrentWhenIdle() {
   MessageLoop::current()->QuitWhenIdle();
 }
--- chrome/browser/ui/content_settings/content_setting_bubble_model.h
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class ContentSettingBubbleModelDelegate;
 class Profile;
-class ProtocolHandlerRegistry;
 class TabContents;
 
 // This model provides data for ContentSettingBubble, and also controls
@@ -160,7 +159,6 @@ class ContentSettingRPHBubbleModel : public ContentSettingTitleAndLinkModel {
   ContentSettingRPHBubbleModel(Delegate* delegate,
                                TabContents* tab_contents,
                                Profile* profile,
-                               ProtocolHandlerRegistry* registry,
                                ContentSettingsType content_type);
 
   virtual void OnRadioClicked(int radio_index) OVERRIDE;
@@ -180,7 +178,6 @@ class ContentSettingRPHBubbleModel : public ContentSettingTitleAndLinkModel {
   void ClearOrSetPreviousHandler();
 
   int selected_item_;
-  ProtocolHandlerRegistry* registry_;
   ProtocolHandler pending_handler_;
   ProtocolHandler previous_handler_;
 };
