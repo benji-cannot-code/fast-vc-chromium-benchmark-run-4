26ce95ea6ff07996be4d85bb8af44755825b96c6
dimich@chromium.org <dimich@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-20 00:21:16 +0000

    Unreviewed, reverting http://trac.webkit.org/changeset/123149. It broke an IDB test on Chromium page cycler.
    * bindings/v8/SerializedScriptValue.cpp:
    (WebCore::SerializedScriptValue::SerializedScriptValue):
    (WebCore):
    * bindings/v8/SerializedScriptValue.h:
    * dom/MessageEvent.cpp:
    (WebCore::MessageEvent::MessageEvent):
    (WebCore::MessageEvent::initMessageEvent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123166 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/SerializedScriptValue.cpp
@@ -2202,7 +2202,6 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::release()
 }
 
 SerializedScriptValue::SerializedScriptValue()
-    : m_externallyAllocatedMemory(0)
 {
 }
 
@@ -2254,7 +2253,6 @@ SerializedScriptValue::SerializedScriptValue(v8::Handle<v8::Value> value,
                                              MessagePortArray* messagePorts, ArrayBufferArray* arrayBuffers,
                                              bool& didThrow,
                                              v8::Isolate* isolate)
-    : m_externallyAllocatedMemory(0)
 {
     didThrow = false;
     Writer writer(isolate);
@@ -2301,7 +2299,6 @@ SerializedScriptValue::SerializedScriptValue(v8::Handle<v8::Value> value,
 }
 
 SerializedScriptValue::SerializedScriptValue(const String& wireData)
-    : m_externallyAllocatedMemory(0)
 {
     m_data = wireData.isolatedCopy();
 }
@@ -2325,18 +2322,5 @@ ScriptValue SerializedScriptValue::deserializeForInspector(ScriptState* scriptSt
     return ScriptValue(deserialize(0, isolate));
 }
 #endif
-
-void SerializedScriptValue::registerMemoryAllocatedWithCurrentScriptContext()
-{
-    if (m_externallyAllocatedMemory)
-        return;
-    m_externallyAllocatedMemory = static_cast<intptr_t>(m_data.length());
-    v8::V8::AdjustAmountOfExternalAllocatedMemory(m_externallyAllocatedMemory);
-}
-
-SerializedScriptValue::~SerializedScriptValue()
-{
-    v8::V8::AdjustAmountOfExternalAllocatedMemory(-m_externallyAllocatedMemory);
-}
 
 } // namespace WebCore
