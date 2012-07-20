6b120d6a4cca945a54fc92d8e2fbcf54d14c1383
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-20 13:55:02 +0000

    SVG Filter Effect sub-region not applied for some filters https://bugs.webkit.org/show_bug.cgi?id=89767
    Reviewed by Dirk Schulze.
    
    Source/WebCore: 
    
    The filter effect region for SVG feGaussian and feDropShadow filters
    was incorrectly set, failing to account for the radius of the blur.
    This patch fixes the problem and removes an unneccessary clip
    operation when the filter result is blitted into the target. That clip
    was clipping the shadow from webkit-shadow.
    
    Tests: svg/filters/feDropShadow-subregion.svg
           svg/filters/feGaussianBlur-subregion.svg
    
    * platform/graphics/filters/FEDropShadow.cpp:
    (WebCore::FEDropShadow::determineAbsolutePaintRect): Moved the code
    that clips the paint rect by the filter effect subregion to come after
    the paint is modified by the filter radius.
    * platform/graphics/filters/FEGaussianBlur.cpp:
    (WebCore::FEGaussianBlur::determineAbsolutePaintRect):
    Moved the code that clips the paint rect by the filter effect subregion to
    come after the paint is modified by the filter radius.
    * rendering/svg/RenderSVGResourceFilter.cpp:
    (WebCore::RenderSVGResourceFilter::postApplyResource):
    Removed a clip that is unnecessary and that was incorrectly clipping drop shadows.
    
    LayoutTests: 
    
    The filter effect region for SVG feGaussian and feDropShadow filters was incorrectly set,
    failing to account for the radius of the blur. This patch fixes the problem and removes an
    unneccessary clip operation when the filter result is blitted into the target. That clip
    was clipping the shadow from webkit-shadow.
    
    * platform/chromium/TestExpectations: Added temp entries for affected
    tests. Will rebaseline later (not closing the bug in the meantime).
    * svg/filters/feDropShadow-subregion-expected.png: Added.
    * svg/filters/feDropShadow-subregion-expected.txt: Added.
    * svg/filters/feDropShadow-subregion.svg: This verifies the subregion for feDropShadow.
    * svg/filters/feGaussianBlur-subregion-expected.png: Added.
    * svg/filters/feGaussianBlur-subregion-expected.txt: Added.
    * svg/filters/feGaussianBlur-subregion.svg: This verifies the subregion for feGaussianBlur.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/filters/FEDropShadow.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/filters/FEDropShadow.cpp
@@ -62,10 +62,6 @@ void FEDropShadow::determineAbsolutePaintRect()
     FloatRect absoluteOffsetPaintRect(absolutePaintRect);
     absoluteOffsetPaintRect.move(filter->applyHorizontalScale(m_dx), filter->applyVerticalScale(m_dy));
     absolutePaintRect.unite(absoluteOffsetPaintRect);
-    if (clipsToBounds())
-        absolutePaintRect.intersect(maxEffectRect());
-    else
-        absolutePaintRect.unite(maxEffectRect());
     
     unsigned kernelSizeX = 0;
     unsigned kernelSizeY = 0;
@@ -74,6 +70,12 @@ void FEDropShadow::determineAbsolutePaintRect()
     // We take the half kernel size and multiply it with three, because we run box blur three times.
     absolutePaintRect.inflateX(3 * kernelSizeX * 0.5f);
     absolutePaintRect.inflateY(3 * kernelSizeY * 0.5f);
+
+    if (clipsToBounds())
+        absolutePaintRect.intersect(maxEffectRect());
+    else
+        absolutePaintRect.unite(maxEffectRect());
+
     setAbsolutePaintRect(enclosingIntRect(absolutePaintRect));
 }
 
