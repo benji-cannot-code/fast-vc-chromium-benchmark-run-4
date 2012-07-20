996089ec064e79315806ac7e9e5a15cb0215659d
fgalligan@chromium.org <fgalligan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 14:54:10 +0000

    Add support for encrypted WebM files as defined in the RFC.
    The WebM parser now reads the HMAC and IV from every encrypted
    Block and stores that information in the DecryptorConfig object.
    Added two new elements ContentEncAESSettings and
    AESSettingsCipherMode used by encrypted WebM files.
    
    This is a new code review of
    https://chromiumcodereview.appspot.com/10535029/ patch set 17 after
    patch set 16 was reverted.
    
    BUG=119845
    TEST=media_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10810026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147654 0039d316-1c4b-4281-b951-d872f2087c98

--- media/webm/webm_content_encodings.h
+++ media/webm/webm_content_encodings.h
@@ -43,6 +43,11 @@ class MEDIA_EXPORT ContentEncoding {
     kEncAlgoAes = 5,
   };
 
+  enum CipherMode {
+    kCipherModeInvalid = 0,
+    kCipherModeCtr = 1,
+  };
+
   ContentEncoding();
   ~ContentEncoding();
 
@@ -65,6 +70,9 @@ class MEDIA_EXPORT ContentEncoding {
 
   void SetEncryptionKeyId(const uint8* encryption_key_id, int size);
 
+  CipherMode cipher_mode() const { return cipher_mode_; }
+  void set_cipher_mode(CipherMode mode) { cipher_mode_ = mode; }
+
  private:
   int64 order_;
   Scope scope_;
@@ -72,6 +80,7 @@ class MEDIA_EXPORT ContentEncoding {
   EncryptionAlgo encryption_algo_;
   scoped_array<uint8> encryption_key_id_;
   int encryption_key_id_size_;
+  CipherMode cipher_mode_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentEncoding);
 };
