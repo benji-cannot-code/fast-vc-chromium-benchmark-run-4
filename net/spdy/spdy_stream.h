e03f6357e584c0bc6ec43c338cc292be8fc387cd
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 20:12:41 +0000

    Revert 147692 - Instead of enqueueing SPDY frames, instead enqueue SPDY streams that are ready to produce data. This allows us to lazily allocate a stream id.
    This patch still brakes net_unittests
    
    The CL was reverted because of memory leaks.  Both SpdyIOBufferProducers leaked the SpdyFrame they owned.  This version of the CL fixes this defect.
    
    Attempting to re-land 144649 
    
    Revert 144655 - Revert 144649
    
    BUG=111708
    
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10803041
    
    TBR=rch@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10807057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147706 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.h
+++ net/spdy/spdy_stream.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_SPDY_SPDY_STREAM_H_
 #define NET_SPDY_SPDY_STREAM_H_
 
-#include <list>
 #include <string>
 #include <vector>
 
@@ -24,12 +23,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/ssl_client_socket.h"
 #include "net/spdy/spdy_framer.h"
 #include "net/spdy/spdy_protocol.h"
-#include "net/spdy/spdy_session.h"
 
 namespace net {
 
 class AddressList;
 class IPEndPoint;
+class SpdySession;
 class SSLCertRequestInfo;
 class SSLInfo;
 
@@ -91,6 +90,7 @@ class NET_EXPORT_PRIVATE SpdyStream
 
   // SpdyStream constructor
   SpdyStream(SpdySession* session,
+             SpdyStreamId stream_id,
              bool pushed,
              const BoundNetLog& net_log);
 
@@ -252,8 +252,6 @@ class NET_EXPORT_PRIVATE SpdyStream
   int GetProtocolVersion() const;
 
  private:
-  class SpdyStreamIOBufferProducer;
-
   enum State {
     STATE_NONE,
     STATE_GET_DOMAIN_BOUND_CERT,
@@ -303,14 +301,6 @@ class NET_EXPORT_PRIVATE SpdyStream
   // the MessageLoop to replay all the data that the server has already sent.
   void PushedStreamReplayData();
 
-  // Informs the SpdySession that this stream has a write available.
-  void SetHasWriteAvailable();
-
-  // Returns a newly created SPDY frame owned by the called that contains
-  // the next frame to be sent by this frame.  May return NULL if this
-  // stream has become stalled on flow control.
-  SpdyFrame* ProduceNextFrame();
-
   // There is a small period of time between when a server pushed stream is
   // first created, and the pushed data is replayed. Any data received during
   // this time should continue to be buffered.
@@ -345,8 +335,6 @@ class NET_EXPORT_PRIVATE SpdyStream
 
   scoped_ptr<SpdyHeaderBlock> response_;
   base::Time response_time_;
-
-  std::list<SpdyFrame*> pending_data_frames_;
 
   State io_state_;
 
