19560cd8c0dbebf5c100599c525cf4b3c2894ec2
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-20 08:03:59 +0000

    REGRESSION(r121420): Performance regression of form state saving for pages with multiple forms https://bugs.webkit.org/show_bug.cgi?id=91804
    Reviewed by Hajime Morita.
    
    The complexity of FormKeyGenerator::formKey() was O(N) where N is the
    number of form elements with an identical action URL, and formKey() is
    called for every form. So, it's O(N^2). A page in www.reddit.com
    contains hundreds of form elements with action="#". So FormController::
    formElementsState() took a few seconds on a slow machine.
    
    In order to avoid O(N^2) operation, storing a map from form signatures
    to next index numbers, instead of storing existing formKey strings.
    
    No new tests. Just a performance improvement.
    
    * html/FormController.cpp:
    (FormKeyGenerator): Remove m_existingKeys. Add a map from a form
    signature string to the next index number.
    (WebCore::formSignature): Returns a signature string for a form, without
    an index number. This is like "actionURL [name1 name2 ]"
    (WebCore::FormKeyGenerator::formKey):
    Creates a formKey string by concatenating a formSignature and #n. N is
    obtained from m_formSignatureToNextIndexMap in O(1).
    (WebCore::FormKeyGenerator::willDeleteForm):
    Remove the code for m_existingKeys.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/FormController.cpp
+++ third_party/WebKit/Source/WebCore/html/FormController.cpp
@@ -279,8 +279,9 @@ private:
     FormKeyGenerator() { }
 
     typedef HashMap<HTMLFormElement*, AtomicString> FormToKeyMap;
+    typedef HashMap<String, unsigned> FormSignatureToNextIndexMap;
     FormToKeyMap m_formToKeyMap;
-    HashSet<AtomicString> m_existingKeys;
+    FormSignatureToNextIndexMap m_formSignatureToNextIndexMap;
 };
 
 static inline void recordFormStructure(const HTMLFormElement& form, StringBuilder& builder)
@@ -305,10 +306,9 @@ static inline void recordFormStructure(const HTMLFormElement& form, StringBuilde
     builder.append("]");
 }
 
-static inline AtomicString createKey(HTMLFormElement* form, unsigned index)
+static inline String formSignature(const HTMLFormElement& form)
 {
-    ASSERT(form);
-    KURL actionURL = form->getURLAttribute(actionAttr);
+    KURL actionURL = form.getURLAttribute(actionAttr);
     // Remove the query part because it might contain volatile parameters such
     // as a session key.
     actionURL.setQuery(String());
@@ -316,11 +316,8 @@ static inline AtomicString createKey(HTMLFormElement* form, unsigned index)
     if (!actionURL.isEmpty())
         builder.append(actionURL.string());
 
-    recordFormStructure(*form, builder);
-
-    builder.append(" #");
-    builder.append(String::number(index));
-    return builder.toAtomicString();
+    recordFormStructure(form, builder);
+    return builder.toString();
 }
 
 AtomicString FormKeyGenerator::formKey(const HTMLFormControlElementWithState& control)
@@ -334,13 +331,18 @@ AtomicString FormKeyGenerator::formKey(const HTMLFormControlElementWithState& co
     if (it != m_formToKeyMap.end())
         return it->second;
 
-    AtomicString candidateKey;
-    unsigned index = 0;
-    do {
-        candidateKey = createKey(form, index++);
-    } while (!m_existingKeys.add(candidateKey).isNewEntry);
-    m_formToKeyMap.add(form, candidateKey);
-    return candidateKey;
+    String signature = formSignature(*form);
+    ASSERT(!signature.isNull());
+    FormSignatureToNextIndexMap::AddResult result = m_formSignatureToNextIndexMap.add(signature, 0);
+    unsigned nextIndex = result.iterator->second++;
+
+    StringBuilder builder;
+    builder.append(signature);
+    builder.append(" #");
+    builder.append(String::number(nextIndex));
+    AtomicString formKey = builder.toAtomicString();
+    m_formToKeyMap.add(form, formKey);
+    return formKey;
 }
 
 void FormKeyGenerator::willDeleteForm(HTMLFormElement* form)
@@ -351,7 +353,6 @@ void FormKeyGenerator::willDeleteForm(HTMLFormElement* form)
     FormToKeyMap::iterator it = m_formToKeyMap.find(form);
     if (it == m_formToKeyMap.end())
         return;
-    m_existingKeys.remove(it->second);
     m_formToKeyMap.remove(it);
 }
 
