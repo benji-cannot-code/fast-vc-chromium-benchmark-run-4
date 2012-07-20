3a19ac212e7c17b56b471f9fb0342b4b5fc4cd1a
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 02:06:01 +0000

    Revert 147594 - NaCl: Remove two uses of '#include "nacl/nacl_log.h"'
    "git cl dcommit" went crazy and appears to have reverted the 3
    previous commits at the same time as committing my change.  (This
    might be due to race conditions with running "git fetch"
    concurrently.)
    
    TBR=mseaborn@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10815024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147601 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/tests/ppapi_example_audio/audio.cc
+++ ppapi/native_client/tests/ppapi_example_audio/audio.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 //
 
+#include <nacl/nacl_log.h>
+
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 #include <string>
 #include "native_client/src/include/nacl/nacl_inttypes.h"
-#include "native_client/src/shared/platform/nacl_log.h"
 #include "native_client/src/shared/ppapi_proxy/utility.h"
 #include "ppapi/c/pp_bool.h"
 #include "ppapi/c/pp_errors.h"
