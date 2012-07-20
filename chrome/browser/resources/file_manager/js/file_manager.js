05da844a870aac72fb654728d7bb0d052f007cfa
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 12:26:47 +0000

    [filemanager] Implemented import process in photo import dialog.
    Photos are imported to /drive/My Photos/album-name.
    Added a context menu item for roots to import all photos from the root.
    No strings yet, only placeholders.
    Review URL: https://chromiumcodereview.appspot.com/10803035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147644 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1100,11 +1100,14 @@ FileManager.prototype = {
                (this.dialogType_ == FileManager.DialogType.SELECT_SAVEAS_FILE ||
                 this.dialogType_ == FileManager.DialogType.FULL_PAGE);
 
+      // TODO(dgozman): current root may be not yet updated here due
+      // to async issues.
       case 'unmount':
-        return true;
-
       case 'format':
         return this.directoryModel_.getCurrentRootType() == RootType.REMOVABLE;
+
+      case 'import-photos':
+        return this.directoryModel_.getCurrentRootType() != RootType.GDATA;
 
       case 'gdata-help':
       case 'gdata-buy-more-space':
@@ -1414,6 +1417,11 @@ FileManager.prototype = {
         this.confirm.show(
             str('FORMATTING_WARNING'),
             chrome.fileBrowserPrivate.formatDevice.bind(null, url));
+        return;
+
+      case 'import-photos':
+        chrome.tabs.create({url: chrome.extension.getURL('photo_import.html') +
+            '#' + this.directoryModel_.getCurrentRootPath()});
         return;
 
       case 'gdata-buy-more-space':
@@ -1863,7 +1871,9 @@ FileManager.prototype = {
       eject.addEventListener('mouseup', function(e) { e.stopPropagation() });
       eject.addEventListener('mousedown', function(e) { e.stopPropagation() });
       li.appendChild(eject);
+    }
 
+    if (rootType != RootType.GDATA) {
       cr.ui.contextMenuHandler.setContextMenu(li, this.rootsContextMenu_);
     }
 
