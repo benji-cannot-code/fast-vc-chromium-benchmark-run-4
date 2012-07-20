fce035e1a4ded16656fb6e21e71cb3d63fa2c69b
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 21:52:37 +0000

    Move content settings data files that are in chrome\test\data\npapi up one level. That makes the only files in that directory those needed by plugin_browsertests.cc, so we can move it to content\test\data\npapi. Also remove click_to_play.html since it wasn't used.
    BUG=90448
    Review URL: https://chromiumcodereview.appspot.com/10808062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/content_settings_browsertest.cc
+++ chrome/browser/content_settings/content_settings_browsertest.cc
@@ -104,8 +104,7 @@ IN_PROC_BROWSER_TEST_F(ClickToPlayPluginTest, Basic) {
       CONTENT_SETTINGS_TYPE_PLUGINS, CONTENT_SETTING_BLOCK);
 
   GURL url = ui_test_utils::GetTestUrl(
-      FilePath().AppendASCII("npapi"),
-      FilePath().AppendASCII("clicktoplay.html"));
+      FilePath(), FilePath().AppendASCII("clicktoplay.html"));
   ui_test_utils::NavigateToURL(browser(), url);
 
   string16 expected_title(ASCIIToUTF16("OK"));
@@ -125,8 +124,7 @@ IN_PROC_BROWSER_TEST_F(ClickToPlayPluginTest, LoadAllBlockedPlugins) {
       CONTENT_SETTINGS_TYPE_PLUGINS, CONTENT_SETTING_BLOCK);
 
   GURL url = ui_test_utils::GetTestUrl(
-      FilePath().AppendASCII("npapi"),
-      FilePath().AppendASCII("load_all_blocked_plugins.html"));
+      FilePath(), FilePath().AppendASCII("load_all_blocked_plugins.html"));
   ui_test_utils::NavigateToURL(browser(), url);
 
   string16 expected_title1(ASCIIToUTF16("1"));
