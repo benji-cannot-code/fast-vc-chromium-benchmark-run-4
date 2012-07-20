280124eb1354b153caf3e113f703037b0cfc21cd
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-20 08:06:36 +0000

    Needs pseudo id to point the inner element of an img elemnet for styling https://bugs.webkit.org/show_bug.cgi?id=91590
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    We introduce pseudo id to point the element in UserAgentShadowDOM.
    This enables us to style the inner element of an img element shadow.
    Please see also http://trac.webkit.org/wiki/ShadowDOMForReplacedElements
    
    Test: fast/dom/shadow/shadowdom-for-image-with-pseudo-id.html
    
    * html/shadow/ImageInnerElement.cpp:
    (WebCore::ImageInnerElement::shadowPseudoId):
    (WebCore):
    * html/shadow/ImageInnerElement.h:
    (ImageInnerElement):
    
    LayoutTests:
    
    Uses pseudo id to confirm the style is applied to the inner element of an img element shadow.
    
    * fast/dom/shadow/shadowdom-for-image-with-pseudo-id-expected.html: Added.
    * fast/dom/shadow/shadowdom-for-image-with-pseudo-id.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/ImageInnerElement.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/ImageInnerElement.cpp
@@ -69,4 +69,10 @@ RenderObject* ImageInnerElement::createRenderer(RenderArena* arena, RenderStyle*
     return createRendererForImage(this, arena);
 }
 
+const AtomicString& ImageInnerElement::shadowPseudoId() const
+{
+    DEFINE_STATIC_LOCAL(AtomicString, pseudoId, ("-webkit-image-inner-element"));
+    return pseudoId;
+}
+
 }
