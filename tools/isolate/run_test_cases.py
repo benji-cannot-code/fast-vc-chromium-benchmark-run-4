f707510ee2de3136bc20643406690a0b0294e90c
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 18:14:43 +0000

    Fix exception on OSX 10.6 due to incomplete multiprocessing implementation.
    multiprocessing.cpu_count() raises NotImplementedError on *some* python version
    found in this OS. Fallback to get the number of processors manually.
    
    TBR=cmp@chromium.org
    NOTRY=true
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10810042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147685 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/run_test_cases.py
+++ tools/isolate/run_test_cases.py
@@ -13,7 +13,6 @@ parallel.
 import fnmatch
 import json
 import logging
-import multiprocessing
 import optparse
 import os
 import Queue
@@ -21,6 +20,20 @@ import subprocess
 import sys
 import threading
 import time
+
+
+def num_processors():
+  """Returns the number of processors.
+
+  Python on OSX 10.6 raises a NotImplementedError exception.
+  """
+  try:
+    # Multiprocessing
+    import multiprocessing
+    return multiprocessing.cpu_count()
+  except:  # pylint: disable=W0702
+    # Mac OS 10.6
+    return int(os.sysconf('SC_NPROCESSORS_ONLN'))
 
 
 if subprocess.mswindows:
@@ -478,7 +491,7 @@ def get_test_cases(executable, whitelist, blacklist, index, shards):
 def run_test_cases(executable, test_cases, jobs, timeout, no_dump):
   """Traces test cases one by one."""
   progress = Progress(len(test_cases))
-  with ThreadPool(jobs or multiprocessing.cpu_count()) as pool:
+  with ThreadPool(jobs) as pool:
     function = Runner(executable, os.getcwd(), timeout, progress).map
     for test_case in test_cases:
       pool.add_task(function, test_case)
@@ -536,7 +549,8 @@ def main():
   parser.add_option(
       '-j', '--jobs',
       type='int',
-      help='number of parallel jobs')
+      default=num_processors(),
+      help='number of parallel jobs; default=%default')
   parser.add_option(
       '-t', '--timeout',
       type='int',
