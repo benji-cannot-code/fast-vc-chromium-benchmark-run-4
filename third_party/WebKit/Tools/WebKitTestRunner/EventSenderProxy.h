4ebbeb4cc48b5ec0dcb0bbd3fd8625ddb4369353
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-20 11:31:06 +0000

    [EFL][WK2][WTR] Implement EventSenderProxy https://bugs.webkit.org/show_bug.cgi?id=91731
    Patch by Kangil Han <kangil.han@samsung.com> on 2012-07-20
    Reviewed by Kentaro Hara.
    
    This is an initial EFL port implementation of EventSenderProxy.
    
    * WebKitTestRunner/EventSenderProxy.h:
    (WTR):
    (EventSenderProxy):
    * WebKitTestRunner/InjectedBundle/EventSendingController.h:
    * WebKitTestRunner/PlatformEfl.cmake:
    * WebKitTestRunner/TestController.cpp:
    (WTR::TestController::TestController):
    (WTR::TestController::didReceiveSynchronousMessageFromInjectedBundle):
    * WebKitTestRunner/efl/EventSenderProxyEfl.cpp: Added.
    (WTR):
    (WTREvent):
    (WTR::WTREvent::WTREvent):
    (WTR::KeyEventInfo::KeyEventInfo):
    (KeyEventInfo):
    (WTR::evasMouseButton):
    (WTR::setEvasModifiers):
    (WTR::dispatchMouseDownEvent):
    (WTR::dispatchMouseUpEvent):
    (WTR::dispatchMouseMoveEvent):
    (WTR::dispatchMouseScrollByEvent):
    (WTR::keyPadName):
    (WTR::keyName):
    (WTR::EventSenderProxy::EventSenderProxy):
    (WTR::EventSenderProxy::updateClickCountForButton):
    (WTR::EventSenderProxy::dispatchEvent):
    (WTR::EventSenderProxy::replaySavedEvents):
    (WTR::EventSenderProxy::sendOrQueueEvent):
    (WTR::EventSenderProxy::mouseDown):
    (WTR::EventSenderProxy::mouseUp):
    (WTR::EventSenderProxy::mouseMoveTo):
    (WTR::EventSenderProxy::mouseScrollBy):
    (WTR::EventSenderProxy::leapForward):
    (WTR::EventSenderProxy::keyDown):
    (WTR::EventSenderProxy::addTouchPoint):
    (WTR::EventSenderProxy::updateTouchPoint):
    (WTR::EventSenderProxy::setTouchModifier):
    (WTR::EventSenderProxy::touchStart):
    (WTR::EventSenderProxy::touchMove):
    (WTR::EventSenderProxy::touchEnd):
    (WTR::EventSenderProxy::touchCancel):
    (WTR::EventSenderProxy::clearTouchPoints):
    (WTR::EventSenderProxy::releaseTouchPoint):
    (WTR::EventSenderProxy::cancelTouchPoint):
    (WTR::EventSenderProxy::setTouchPointRadius):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123203 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/EventSenderProxy.h
+++ third_party/WebKit/Tools/WebKitTestRunner/EventSenderProxy.h
@@ -34,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif PLATFORM(GTK)
 #include <gdk/gdk.h>
 #include <wtf/Vector.h>
+#elif PLATFORM(EFL)
+#include <wtf/Deque.h>
 #endif
 
 namespace WTR {
@@ -42,6 +44,8 @@ class TestController;
 
 #if PLATFORM(GTK)
 struct WTREventQueueItem;
+#elif PLATFORM(EFL)
+struct WTREvent;
 #endif
 
 class EventSenderProxy {
@@ -78,7 +82,7 @@ private:
     double currentEventTime() { return m_time; }
     void updateClickCountForButton(int button);
 
-#if PLATFORM(QT) || PLATFORM(GTK)
+#if PLATFORM(QT) || PLATFORM(GTK) || PLATFORM(EFL)
     void replaySavedEvents();
 #endif
 
@@ -90,6 +94,9 @@ private:
 #elif PLATFORM(GTK)
     void sendOrQueueEvent(GdkEvent*);
     GdkEvent* createMouseButtonEvent(GdkEventType, unsigned button, WKEventModifiers);
+#elif PLATFORM(EFL)
+    void sendOrQueueEvent(const WTREvent&);
+    void dispatchEvent(const WTREvent&);
 #endif
 
     double m_time;
@@ -113,6 +120,9 @@ private:
     QPoint m_touchPointRadius;
     bool m_touchActive;
 #endif
+#elif PLATFORM(EFL)
+    Deque<WTREvent> m_eventQueue;
+    unsigned m_mouseButton;
 #endif
 };
 
