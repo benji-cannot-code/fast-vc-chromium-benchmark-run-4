2fc6137b500623acd0b0ef247ee1537524050240
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 22:54:45 +0000

    Add model control of more suggestions link visibility.
    R=groby@chromium.org
    BUG=128230
    TEST=WebIntentPickerModel.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/10766019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147731 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_model.cc
+++ chrome/browser/ui/intents/web_intent_picker_model.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "chrome/browser/ui/intents/web_intent_picker_model_observer.h"
+#include "grit/generated_resources.h"
 #include "grit/ui_resources.h"
+#include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/image/image.h"
 
@@ -115,6 +117,15 @@ const WebIntentPickerModel::SuggestedExtension&
 
 size_t WebIntentPickerModel::GetSuggestedExtensionCount() const {
   return std::min(suggested_extensions_.size(), kMaxSuggestionCount);
+}
+
+string16 WebIntentPickerModel::GetSuggestionsLinkText() const {
+  if (suggested_extensions_.size() <= kMaxSuggestionCount)
+    return string16();
+
+  return l10n_util::GetStringUTF16(GetInstalledServiceCount() == 0 ?
+      IDS_INTENT_PICKER_GET_MORE_SERVICES_NONE_INSTALLED :
+      IDS_INTENT_PICKER_GET_MORE_SERVICES);
 }
 
 void WebIntentPickerModel::SetSuggestedExtensionIconWithId(
