6d008d81d876c62badd60bebd60630f1a4bc7a5f
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-20 22:09:02 +0000

    Remove AURA ifdefs around AcceleratedSurfaceNew/Release.
    Use stubs in RWHV instead. This avoids DCHECK failures from
    unknown messages when using TextureImageTransportSurface.
    
    BUG=136923
    
    
    Review URL: https://chromiumcodereview.appspot.com/10795049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147726 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host_ui_shim.cc
+++ content/browser/gpu/gpu_process_host_ui_shim.cc
@@ -204,19 +204,13 @@ bool GpuProcessHostUIShim::OnControlMessageReceived(
                         OnAcceleratedSurfaceSuspend)
     IPC_MESSAGE_HANDLER(GpuHostMsg_GraphicsInfoCollected,
                         OnGraphicsInfoCollected)
+    IPC_MESSAGE_HANDLER(GpuHostMsg_AcceleratedSurfaceNew,
+                        OnAcceleratedSurfaceNew)
+    IPC_MESSAGE_HANDLER(GpuHostMsg_AcceleratedSurfaceRelease,
+                        OnAcceleratedSurfaceRelease)
 
 #if defined(TOOLKIT_GTK) || defined(OS_WIN)
     IPC_MESSAGE_HANDLER(GpuHostMsg_ResizeView, OnResizeView)
-#endif
-
-#if defined(USE_AURA)
-    IPC_MESSAGE_HANDLER(GpuHostMsg_AcceleratedSurfaceNew,
-                        OnAcceleratedSurfaceNew)
-#endif
-
-#if defined(USE_AURA)
-    IPC_MESSAGE_HANDLER(GpuHostMsg_AcceleratedSurfaceRelease,
-                        OnAcceleratedSurfaceRelease)
 #endif
 
     IPC_MESSAGE_UNHANDLED_ERROR()
@@ -290,8 +284,6 @@ void GpuProcessHostUIShim::OnResizeView(int32 surface_id,
 
 #endif
 
-#if defined(USE_AURA)
-
 void GpuProcessHostUIShim::OnAcceleratedSurfaceNew(
     const GpuHostMsg_AcceleratedSurfaceNew_Params& params) {
   RenderWidgetHostViewPort* view = GetRenderWidgetHostViewFromSurfaceID(
@@ -301,8 +293,6 @@ void GpuProcessHostUIShim::OnAcceleratedSurfaceNew(
   view->AcceleratedSurfaceNew(
       params.width, params.height, params.surface_handle);
 }
-
-#endif
 
 static base::TimeDelta GetSwapDelay() {
   CommandLine* cmd_line = CommandLine::ForCurrentProcess();
@@ -370,8 +360,6 @@ void GpuProcessHostUIShim::OnAcceleratedSurfaceSuspend(int32 surface_id) {
   view->AcceleratedSurfaceSuspend();
 }
 
-#if defined(USE_AURA)
-
 void GpuProcessHostUIShim::OnAcceleratedSurfaceRelease(
     const GpuHostMsg_AcceleratedSurfaceRelease_Params& params) {
   RenderWidgetHostViewPort* view = GetRenderWidgetHostViewFromSurfaceID(
@@ -380,5 +368,3 @@ void GpuProcessHostUIShim::OnAcceleratedSurfaceRelease(
     return;
   view->AcceleratedSurfaceRelease(params.identifier);
 }
-
-#endif
