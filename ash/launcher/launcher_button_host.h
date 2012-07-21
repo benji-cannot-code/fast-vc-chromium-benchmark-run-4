fc705fb4929a33e1dc5d63caabba576533796c03
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-21 06:02:01 +0000

    ash: Support touch drag in launcher.
    This CL uses ET_GESTURE_SCROLL_BEGIN etc on launcher button for touch drag.
    
    BUG=113400
    TEST=Verify can drag app shortcut around using touch.
    
    Review URL: https://chromiumcodereview.appspot.com/10807017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147783 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_button_host.h
+++ ash/launcher/launcher_button_host.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string16.h"
 
 namespace views {
-class MouseEvent;
+class LocatedEvent;
 class View;
 }
 
@@ -22,17 +22,26 @@ namespace internal {
 // This interface is used to enable reordering the items on the launcher.
 class ASH_EXPORT LauncherButtonHost {
  public:
-  // Invoked when the mouse is pressed on a view.
-  virtual void MousePressedOnButton(views::View* view,
-                                    const views::MouseEvent& event) = 0;
+  enum Pointer {
+    NONE,
+    MOUSE,
+    TOUCH,
+  };
 
-  // Invoked when the mouse is dragged over a view.
-  virtual void MouseDraggedOnButton(views::View* view,
-                                    const views::MouseEvent& event) = 0;
+  // Invoked when a pointer device is pressed on a view.
+  virtual void PointerPressedOnButton(views::View* view,
+                                      Pointer pointer,
+                                      const views::LocatedEvent& event) = 0;
 
-  // Invoked either if the mouse is released or mouse capture canceled.
-  virtual void MouseReleasedOnButton(views::View* view,
-                                     bool canceled) = 0;
+  // Invoked when a pointer device is dragged over a view.
+  virtual void PointerDraggedOnButton(views::View* view,
+                                      Pointer pointer,
+                                      const views::LocatedEvent& event) = 0;
+
+  // Invoked either if a pointer device is released or mouse capture canceled.
+  virtual void PointerReleasedOnButton(views::View* view,
+                                       Pointer pointer,
+                                       bool canceled) = 0;
 
   // Invoked when the mouse moves on the item.
   virtual void MouseMovedOverButton(views::View* view) = 0;
