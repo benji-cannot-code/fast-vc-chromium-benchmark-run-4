8b4fdee7e8cc9b758862f4984b273f715be3d209
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-22 18:39:35 +0000

    Make disk cache tests ETags more HTTP standard conformant.
    ETags shuld use quotes.
    
    For a special treat, do check out HttpCache.ConditionalizedGet_PreserveRequestHeaders, previously it asserted on the ETags as being "\"foopy\"" when the ETags were created they were just "foppy". So that test looked like a matching ETags unless you read the code very carefully.
    
    R=rvargas@chromium.org
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/10802053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147823 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_cache_unittest.cc
+++ net/http/http_cache_unittest.cc
@@ -3792,7 +3792,7 @@ TEST(HttpCache, DoomOnDestruction3) {
       "Last-Modified: Wed, 28 Nov 2007 00:40:09 GMT\n"
       "Content-Length: 22\n"
       "Accept-Ranges: none\n"
-      "Etag: foopy\n";
+      "Etag: \"foopy\"\n";
   AddMockTransaction(&transaction);
   MockHttpRequest request(transaction);
 
@@ -3835,7 +3835,7 @@ TEST(HttpCache, SetTruncatedFlag) {
   transaction.response_headers =
       "Last-Modified: Wed, 28 Nov 2007 00:40:09 GMT\n"
       "Content-Length: 22\n"
-      "Etag: foopy\n";
+      "Etag: \"foopy\"\n";
   AddMockTransaction(&transaction);
   MockHttpRequest request(transaction);
 
@@ -3895,7 +3895,7 @@ TEST(HttpCache, DontSetTruncatedFlag) {
   transaction.response_headers =
       "Last-Modified: Wed, 28 Nov 2007 00:40:09 GMT\n"
       "Content-Length: 22\n"
-      "Etag: foopy\n";
+      "Etag: \"foopy\"\n";
   AddMockTransaction(&transaction);
   MockHttpRequest request(transaction);
 
@@ -4822,7 +4822,7 @@ TEST(HttpCache, StopCachingSavesEntry) {
     AddMockTransaction(&mock_transaction);
     mock_transaction.response_headers = "Cache-Control: max-age=10000\n"
                                         "Content-Length: 42\n"
-                                        "Etag: foo\n";
+                                        "Etag: \"foo\"\n";
 
     rv = trans->Start(&request, callback.callback(), net::BoundNetLog());
     EXPECT_EQ(net::OK, callback.GetResult(rv));
@@ -4937,7 +4937,7 @@ TEST(HttpCache, TruncatedByContentLength2) {
   AddMockTransaction(&transaction);
   transaction.response_headers = "Cache-Control: max-age=10000\n"
                                  "Content-Length: 100\n"
-                                 "Etag: foo\n";
+                                 "Etag: \"foo\"\n";
   RunTransactionTest(cache.http_cache(), transaction);
   RemoveMockTransaction(&transaction);
 
