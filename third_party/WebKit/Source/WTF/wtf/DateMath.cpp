4874af0aa0e9240f44385d543dbb7196ee1a241a
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-23 09:47:59 +0000

    Build fix for Windows after r123317.
    * wtf/DateMath.cpp: Added missing header include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123322 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/DateMath.cpp
+++ third_party/WebKit/Source/WTF/wtf/DateMath.cpp
@@ -87,6 +87,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <time.h>
 #include <wtf/text/StringBuilder.h>
 
+#if OS(WINDOWS)
+#include <windows.h>
+#endif
+
 #if HAVE(ERRNO_H)
 #include <errno.h>
 #endif
