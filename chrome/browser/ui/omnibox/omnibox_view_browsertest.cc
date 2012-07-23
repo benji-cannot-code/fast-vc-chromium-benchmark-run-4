003b737203c21cdb7ac5485377806149b4f72ff3
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 22:11:19 +0000

    Removing usage of profile_->GetHistory() from the codebase in order to remove that function.
    This was previously reverted, found not to be the cause of the issue, now I am reinstating the changes. 
    
    BUG=97804
    TEST=existing unittests
    
    TBR=sky@chromium.org,shess@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10806022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147962 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/history/history.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url.h"
 #include "chrome/browser/search_engines/template_url_service.h"
@@ -299,8 +300,8 @@ class OmniboxViewTest : public InProcessBrowserTest,
 
   void AddHistoryEntry(const TestHistoryEntry& entry, const Time& time) {
     Profile* profile = browser()->profile();
-    HistoryService* history_service =
-        profile->GetHistoryService(Profile::EXPLICIT_ACCESS);
+    HistoryService* history_service = HistoryServiceFactory::GetForProfile(
+        profile, Profile::EXPLICIT_ACCESS);
     ASSERT_TRUE(history_service);
 
     if (!history_service->BackendLoaded()) {
