2454791915f4b96ea29039da00d39382dbaba7c6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-23 22:34:11 +0000

    [BlackBerry] Move about: URL handling out of WebCore https://bugs.webkit.org/show_bug.cgi?id=91541
    Patch by Yong Li <yoli@rim.com> on 2012-07-23
    Reviewed by Rob Buis.
    
    PR# 181304.
    Move about: URL handling to the right place (FrameLoaderClientBlackBerry::createDocumentLoader), so
    reload and history navigation can work.
    Other changes: Remove about:version which makes little sense. Make about:memory partially visible.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::load): Remove the call to loadAbout()
    * Api/WebPage_p.h: Remove loadAbout()
    (WebPagePrivate):
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::createDocumentLoader): Construct about: data here.
    * WebKitSupport/AboutData.cpp:
    (BlackBerry::WebKit::numberToHTMLTr): Make it static
    (BlackBerry::WebKit::configPage): Make it static
    (BlackBerry::WebKit::memoryPage): Make it static
    (BlackBerry::WebKit::cachePage):
    (BlackBerry::WebKit::buildPage):
    (BlackBerry::WebKit::creditsPage):
    (BlackBerry::WebKit::cookiePage):
    (BlackBerry::WebKit::aboutData): The only export function that returns HTML source for a given about: URL.
    (WebKit):
    * WebKitSupport/AboutData.h:
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/AboutData.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/AboutData.h
@@ -25,8 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace BlackBerry {
 namespace WebKit {
 
-String configPage();
-String memoryPage();
+String aboutData(String aboutWhat);
 
 } // namespace WebKit
 } // namespace BlackBerry
