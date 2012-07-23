041a6bf495981a9dbffe0a63a0d7834b23a0c4e8
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 17:41:00 +0000

    Make NativeViewGLSurfaceEGL directly usable on Android.
    Define AcceleratedWidget as ANativeWindow* which can be
    used to create an EGLSurface.
    Also do allow direct initialization of NativeViewGLSurfaceEGL.
    
    BUG=136923
    
    
    Review URL: https://chromiumcodereview.appspot.com/10815041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147879 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_surface_egl.cc
+++ ui/gl/gl_surface_egl.cc
@@ -9,16 +9,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "build/build_config.h"
-#if !defined(OS_ANDROID)
 #include "third_party/angle/include/EGL/egl.h"
 #include "third_party/angle/include/EGL/eglext.h"
-#endif
 #include "ui/gl/egl_util.h"
 #include "ui/gl/gl_context.h"
-
-#if defined(OS_ANDROID)
-#include <EGL/egl.h>
-#endif
 
 // This header must come after the above third-party include, as
 // it brings in #defines that cause conflicts.
@@ -173,10 +167,6 @@ NativeViewGLSurfaceEGL::NativeViewGLSurfaceEGL(bool software,
 }
 
 bool NativeViewGLSurfaceEGL::Initialize() {
-#if defined(OS_ANDROID)
-  NOTREACHED();
-  return false;
-#else
   DCHECK(!surface_);
 
   if (!GetDisplay()) {
@@ -212,7 +202,6 @@ bool NativeViewGLSurfaceEGL::Initialize() {
   supports_post_sub_buffer_ = (surfaceVal && retVal) == EGL_TRUE;
 
   return true;
-#endif
 }
 
 void NativeViewGLSurfaceEGL::Destroy() {
