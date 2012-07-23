96906234a2aa334e5fcc16652604ff0faf9e6a02
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-23 21:39:19 +0000

    MediaController.currentTime should be kept stable during script execution. https://bugs.webkit.org/show_bug.cgi?id=88555
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Test: media/media-controller-time-constant.html
    
    To keep MediaController.currentTime stable, add a new m_position variable and
    a new m_clearPositionTimer timer.  Both must be mutable variables as they will
    be updated from within const functions.  Calls to currentTime() will result in
    stable values until the next runloop iteration.
    
    * html/MediaController.cpp:
    (MediaController::MediaController):
    (MediaController::currentTime):
    (MediaController::setCurrentTime):
    (MediaController::clearPositionTimerFired):
    * html/MediaController.h:
    
    LayoutTests:
    
    * media/media-controller-time-constant-expected.txt: Added.
    * media/media-controller-time-constant.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123386 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/MediaController.cpp
+++ third_party/WebKit/Source/WebCore/html/MediaController.cpp
@@ -48,10 +48,12 @@ MediaController::MediaController(ScriptExecutionContext* context)
     : m_paused(false)
     , m_defaultPlaybackRate(1)
     , m_volume(1)
+    , m_position(MediaPlayer::invalidTime())
     , m_muted(false)
     , m_readyState(HAVE_NOTHING)
     , m_playbackState(WAITING)
     , m_asyncEventTimer(this, &MediaController::asyncEventTimerFired)
+    , m_clearPositionTimer(this, &MediaController::clearPositionTimerFired)
     , m_closedCaptionsVisible(false)
     , m_clock(Clock::create())
     , m_scriptExecutionContext(context)
@@ -144,8 +146,13 @@ float MediaController::currentTime() const
     if (m_mediaElements.isEmpty())
         return 0;
 
-    // Some clocks may return times outside the range of [0..duration].
-    return max(0.0f, min(duration(), m_clock->currentTime()));
+    if (m_position == MediaPlayer::invalidTime()) {
+        // Some clocks may return times outside the range of [0..duration].
+        m_position = max(0.0f, min(duration(), m_clock->currentTime()));
+        m_clearPositionTimer.startOneShot(0);
+    }
+
+    return m_position;
 }
 
 void MediaController::setCurrentTime(float time, ExceptionCode& code)
@@ -492,6 +499,11 @@ void MediaController::asyncEventTimerFired(Timer<MediaController>*)
     size_t count = pendingEvents.size();
     for (size_t index = 0; index < count; ++index)
         dispatchEvent(pendingEvents[index].release(), ec);
+}
+
+void MediaController::clearPositionTimerFired(Timer<MediaController>*)
+{
+    m_position = MediaPlayer::invalidTime();
 }
 
 bool MediaController::hasAudio() const
--- third_party/WebKit/Source/WebCore/html/MediaController.h
+++ third_party/WebKit/Source/WebCore/html/MediaController.h
@@ -124,6 +124,7 @@ private:
     void bringElementUpToSpeed(HTMLMediaElement*);
     void scheduleEvent(const AtomicString& eventName);
     void asyncEventTimerFired(Timer<MediaController>*);
+    void clearPositionTimerFired(Timer<MediaController>*);
     bool hasEnded() const;
 
     // EventTarget
@@ -141,11 +142,13 @@ private:
     bool m_paused;
     float m_defaultPlaybackRate;
     float m_volume;
+    mutable float m_position;
     bool m_muted;
     ReadyState m_readyState;
     PlaybackState m_playbackState;
     Vector<RefPtr<Event> > m_pendingEvents;
     Timer<MediaController> m_asyncEventTimer;
+    mutable Timer<MediaController> m_clearPositionTimer;
     String m_mediaGroup;
     bool m_closedCaptionsVisible;
     PassRefPtr<Clock> m_clock;
