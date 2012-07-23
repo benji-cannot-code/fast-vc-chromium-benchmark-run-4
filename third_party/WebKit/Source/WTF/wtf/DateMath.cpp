9b3af86038def319431bdb133e1e4529786f42b7
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-23 08:34:19 +0000

    [WIN] Use GetTimeZoneInformation() for calculateUTCOffset() https://bugs.webkit.org/show_bug.cgi?id=91935
    Reviewed by Ryosuke Niwa.
    
    GetTimeZoneInformation() returns the offset directly. Using it
    avoids unnecessary calculations and remove dependencies on
    other time related function, which do not exist on WinCE.
    
    * wtf/DateMath.cpp:
    (WTF::calculateUTCOffset):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/DateMath.cpp
+++ third_party/WebKit/Source/WTF/wtf/DateMath.cpp
@@ -357,6 +357,12 @@ int equivalentYearForDST(int year)
 
 int32_t calculateUTCOffset()
 {
+#if OS(WINDOWS)
+    TIME_ZONE_INFORMATION timeZoneInformation;
+    GetTimeZoneInformation(&timeZoneInformation);
+    int32_t bias = timeZoneInformation.Bias + timeZoneInformation.StandardBias;
+    return -bias * 60 * 1000;
+#else
     time_t localTime = time(0);
     tm localt;
     getLocalTime(&localTime, &localt);
@@ -377,7 +383,7 @@ int32_t calculateUTCOffset()
 #if HAVE(TM_ZONE)
     localt.tm_zone = 0;
 #endif
-    
+
 #if HAVE(TIMEGM)
     time_t utcOffset = timegm(&localt) - mktime(&localt);
 #else
@@ -387,6 +393,7 @@ int32_t calculateUTCOffset()
 #endif
 
     return static_cast<int32_t>(utcOffset * 1000);
+#endif
 }
 
 /*
