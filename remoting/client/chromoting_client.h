d1451c280af2733b9c5861adb1c74c0adc62e3c1
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 02:41:31 +0000

    Rename ChromotingView to ClientUserInterface. Clean it up.
    ChromotingView wasn't really doing what it was initially meant to do, 
    now FrameProducer and FrameConsumer interfaces are used to process video 
    stream. Renamed and simplified that interface. Also moved implementation 
    from PepperView to ChromotingInstance.
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=147595
    
    Review URL: https://chromiumcodereview.appspot.com/10703144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147828 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/chromoting_client.h
+++ remoting/client/chromoting_client.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "remoting/client/client_config.h"
 #include "remoting/client/chromoting_stats.h"
-#include "remoting/client/chromoting_view.h"
 #include "remoting/protocol/audio_stub.h"
 #include "remoting/protocol/client_stub.h"
 #include "remoting/protocol/clipboard_stub.h"
@@ -36,6 +35,7 @@ class TransportFactory;
 }  // namespace protocol
 
 class AudioPlayer;
+class ClientUserInterface;
 class RectangleUpdateDecoder;
 
 // TODO(sergeyu): Move VideoStub implementation to RectangleUpdateDecoder.
@@ -48,7 +48,7 @@ class ChromotingClient : public protocol::ConnectionToHost::HostEventCallback,
   ChromotingClient(const ClientConfig& config,
                    scoped_refptr<base::SingleThreadTaskRunner> task_runner,
                    protocol::ConnectionToHost* connection,
-                   ChromotingView* view,
+                   ClientUserInterface* user_interface,
                    RectangleUpdateDecoder* rectangle_decoder,
                    AudioPlayer* audio_player);
 
@@ -111,7 +111,7 @@ class ChromotingClient : public protocol::ConnectionToHost::HostEventCallback,
   ClientConfig config_;
   scoped_refptr<base::SingleThreadTaskRunner> task_runner_;
   protocol::ConnectionToHost* connection_;
-  ChromotingView* view_;
+  ClientUserInterface* user_interface_;
   RectangleUpdateDecoder* rectangle_decoder_;
   AudioPlayer* audio_player_;
 
