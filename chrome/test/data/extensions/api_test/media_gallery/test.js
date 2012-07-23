afc50f120259defa828a42bbe575d4bf864dce58
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 19:48:58 +0000

    Revert 147895 - switch mediaGalleries to .idl api definition
    This is part 2 of the revert. First part in 147897.
    
    Does not compile.
    
    BUG=none
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10806023
    
    TBR=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10816024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147902 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/media_gallery/test.js
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var mediaGalleries = chrome.experimental.mediaGalleries;
+var mediaFileSystemsListCallback = function(results) {
+  // There should be a "Pictures" directory on all desktop platforms.
+  var expectedFileSystems = 1;
+  // But not on Android and ChromeOS.
+  if (/Android/.test(navigator.userAgent) || /CrOS/.test(navigator.userAgent)) {
+    expectedFileSystems = 0;
+  }
+  chrome.test.assertEq(expectedFileSystems, results.length);
+};
+var nullCallback = function(result) {
+  chrome.test.assertEq(null, result);
+};
+
+function runTests(tests) {
+  chrome.test.getConfig(function(config) {
+    operatingSystem = config.osName;
+    chrome.test.runTests(tests);
+  });
+}
+
+chrome.test.runTests([
+  function getGalleries() {
+    mediaGalleries.getMediaFileSystems(
+        chrome.test.callbackPass(mediaFileSystemsListCallback));
+  },
+
+  function openMediaGalleryManager() {
+    // Just confirm that the function exists.
+    chrome.test.assertTrue(mediaGalleries.openMediaGalleryManager !== null);
+    chrome.test.succeed()
+  },
+
+  function extractEmbeddedThumbnails() {
+    var result = mediaGalleries.extractEmbeddedThumbnails({});
+    chrome.test.assertEq(null, result);
+    chrome.test.succeed()
+  },
+
+  function assembleMediaFile() {
+    mediaGalleries.assembleMediaFile(
+        {}, {}, chrome.test.callbackPass(nullCallback));
+  },
+]);
