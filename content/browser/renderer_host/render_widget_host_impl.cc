131f9b9204da2d0a5982ec1fe19c386cc7de615c
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 19:57:06 +0000

    Add threaded compositing mode to field trial
    This splits the force-compositing-mode field trial into 3 groups:
    - disabled
    - force-compositing-mode
    - force-compositing-mode + threaded-compositing
    
    
    Review URL: https://chromiumcodereview.appspot.com/10811002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147907 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/trace_event.h"
 #include "base/i18n/rtl.h"
 #include "base/message_loop.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
@@ -35,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/user_metrics.h"
+#include "content/public/common/content_constants.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/result_codes.h"
 #include "skia/ext/image_operations.h"
@@ -1900,11 +1902,17 @@ void RenderWidgetHostImpl::AcknowledgeBufferPresent(
 }
 
 void RenderWidgetHostImpl::AcknowledgeSwapBuffersToRenderer() {
+  base::FieldTrial* trial =
+      base::FieldTrialList::Find(content::kGpuCompositingFieldTrialName);
+  bool is_thread_trial = trial && trial->group_name() ==
+      content::kGpuCompositingFieldTrialThreadEnabledName;
+  bool has_enable = CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableThreadedCompositing);
+  bool has_disable = CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kDisableThreadedCompositing);
+  DCHECK(!is_thread_trial || !has_disable);
   bool enable_threaded_compositing =
-      CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableThreadedCompositing) &&
-      !CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kDisableThreadedCompositing);
+      is_thread_trial || (has_enable && !has_disable);
   if (!enable_threaded_compositing)
     Send(new ViewMsg_SwapBuffers_ACK(routing_id_));
 }
