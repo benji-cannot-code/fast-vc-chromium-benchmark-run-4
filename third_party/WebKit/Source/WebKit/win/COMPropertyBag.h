e03385b3a49785a3d9790f4401c6af339676539e
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-23 20:23:47 +0000

    Build fix if NOMINMAX is defined by the build system.
    * COMEnumVariant.h: Added #ifndef NOMINMAX around #define NOMINMAX.
    * COMPropertyBag.h: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/win/COMPropertyBag.h
+++ third_party/WebKit/Source/WebKit/win/COMPropertyBag.h
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPropertyBag_h
 #define COMPropertyBag_h
 
+#ifndef NOMINMAX
 #define NOMINMAX
+#endif
 #include <ocidl.h>
 #include <unknwn.h>
 
