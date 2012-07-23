4ea74459ef6e9844ed91be6c593f2b64de295c09
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-23 19:31:38 +0000

    Add WebTextInputType enum values for text areas and content-editable. https://bugs.webkit.org/show_bug.cgi?id=91654
    Reviewed by Adam Barth.
    
    No new tests, no behavior change without corresponding chromium.org changes.
    
    * public/WebTextInputType.h:
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::textInputType):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebTextInputType.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebTextInputType.h
@@ -55,6 +55,10 @@ enum WebTextInputType {
     WebTextInputTypeMonth,
     WebTextInputTypeTime,
     WebTextInputTypeWeek,
+    WebTextInputTypeTextArea,
+
+    // Input caret is in a contenteditable node (not an INPUT field).
+    WebTextInputTypeContentEditable,
 };
 
 } // namespace WebKit
--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -2101,11 +2101,11 @@ WebTextInputType WebViewImpl::textInputType()
         if (textarea->readOnly() || textarea->disabled())
             return WebTextInputTypeNone;
 
-        return WebTextInputTypeText;
+        return WebTextInputTypeTextArea;
     }
 
     if (node->shouldUseInputMethod())
-        return WebTextInputTypeText;
+        return WebTextInputTypeContentEditable;
 
     return WebTextInputTypeNone;
 }
