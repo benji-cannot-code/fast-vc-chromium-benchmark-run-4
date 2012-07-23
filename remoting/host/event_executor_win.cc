77cc4cde9925a61728d707d15d535d13ee6ebfc1
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-23 19:56:46 +0000

    Remove EventExecutor dependency on VideoFrameCapturer.
    BUG=137044
    TEST=Manual verification that Windows host input injection works correctly.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10795053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@147906 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/event_executor_win.cc
+++ remoting/host/event_executor_win.cc
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/location.h"
 #include "base/single_thread_task_runner.h"
-#include "remoting/host/video_frame_capturer.h"
 #include "remoting/host/clipboard.h"
 #include "remoting/proto/event.pb.h"
+// SkSize.h assumes that stdint.h-style types are already defined.
+#include "third_party/skia/include/core/SkTypes.h"
+#include "third_party/skia/include/core/SkSize.h"
 
 namespace remoting {
 
@@ -32,8 +34,7 @@ using protocol::MouseEvent;
 class EventExecutorWin : public EventExecutor {
  public:
   EventExecutorWin(scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
-                   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-                   VideoFrameCapturer* capturer);
+                   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner);
   virtual ~EventExecutorWin() {}
 
   // ClipboardStub interface.
@@ -55,7 +56,6 @@ class EventExecutorWin : public EventExecutor {
 
   scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner_;
-  VideoFrameCapturer* capturer_;
   scoped_ptr<Clipboard> clipboard_;
 
   DISALLOW_COPY_AND_ASSIGN(EventExecutorWin);
@@ -63,11 +63,9 @@ class EventExecutorWin : public EventExecutor {
 
 EventExecutorWin::EventExecutorWin(
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
-    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-    VideoFrameCapturer* capturer)
+    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner)
     : main_task_runner_(main_task_runner),
       ui_task_runner_(ui_task_runner),
-      capturer_(capturer),
       clipboard_(Clipboard::Create()) {
 }
 
@@ -222,8 +220,11 @@ void EventExecutorWin::HandleMouse(const MouseEvent& event) {
     INPUT input;
     input.type = INPUT_MOUSE;
     input.mi.time = 0;
-    SkISize screen_size = capturer_->size_most_recent();
+    SkISize screen_size(SkISize::Make(GetSystemMetrics(SM_CXVIRTUALSCREEN),
+                                      GetSystemMetrics(SM_CYVIRTUALSCREEN)));
     if ((screen_size.width() > 1) && (screen_size.height() > 1)) {
+      x = std::max(0, std::min(screen_size.width(), x));
+      y = std::max(0, std::min(screen_size.height(), y));
       input.mi.dx = static_cast<int>((x * 65535) / (screen_size.width() - 1));
       input.mi.dy = static_cast<int>((y * 65535) / (screen_size.height() - 1));
       input.mi.dwFlags =
@@ -291,10 +292,9 @@ void EventExecutorWin::HandleMouse(const MouseEvent& event) {
 
 scoped_ptr<EventExecutor> EventExecutor::Create(
     scoped_refptr<base::SingleThreadTaskRunner> main_task_runner,
-    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner,
-    VideoFrameCapturer* capturer) {
+    scoped_refptr<base::SingleThreadTaskRunner> ui_task_runner) {
   return scoped_ptr<EventExecutor>(
-      new EventExecutorWin(main_task_runner, ui_task_runner, capturer));
+      new EventExecutorWin(main_task_runner, ui_task_runner));
 }
 
 }  // namespace remoting
