16795e6f0190955228d4e6254347ddb34fbfdcca
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 09:21:37 +0000

    [EFL][WK2] Fix possible crash in didSendRequestForResource https://bugs.webkit.org/show_bug.cgi?id=92077
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-24
    Reviewed by Kenneth Rohde Christiansen.
    
    Add NULL check for wkRedirectResponse in
    didSendRequestForResource() before using it. The
    argument may be NULL if there was no redirection.
    
    * UIProcess/API/efl/ewk_view.h:
    * UIProcess/API/efl/ewk_view_resource_load_client.cpp:
    (didSendRequestForResource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view_resource_load_client.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view_resource_load_client.cpp
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ewk_web_resource_private.h"
 #include <wtf/text/CString.h>
 
+using namespace WebCore;
 using namespace WebKit;
 
 static inline Evas_Object* toEwkView(const void* clientInfo)
@@ -67,10 +68,11 @@ static void didInitiateLoadForResource(WKPageRef, WKFrameRef wkFrame, uint64_t r
 static void didSendRequestForResource(WKPageRef, WKFrameRef, uint64_t resourceIdentifier, WKURLRequestRef wkRequest, WKURLResponseRef wkRedirectResponse, const void* clientInfo)
 {
     Ewk_Url_Request* request = ewk_url_request_new(wkRequest);
-    Ewk_Url_Response* redirectResponse = ewk_url_response_new(toImpl(wkRedirectResponse)->resourceResponse());
+    Ewk_Url_Response* redirectResponse = wkRedirectResponse ? ewk_url_response_new(toImpl(wkRedirectResponse)->resourceResponse()) : 0;
     ewk_view_resource_request_sent(toEwkView(clientInfo), resourceIdentifier, request, redirectResponse);
     ewk_url_request_unref(request);
-    ewk_url_response_unref(redirectResponse);
+    if (redirectResponse)
+        ewk_url_response_unref(redirectResponse);
 }
 
 static void didReceiveResponseForResource(WKPageRef, WKFrameRef, uint64_t resourceIdentifier, WKURLResponseRef wkResponse, const void* clientInfo)
