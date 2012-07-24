08b82ef7913c34765ed1ccf014677b0ce3eba389
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 11:18:12 +0000

    [EFL][WK2] Remove unneeded WebURLRequestEfl and WebURLResponseEfl https://bugs.webkit.org/show_bug.cgi?id=92087
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-07-24
    Reviewed by Kenneth Rohde Christiansen.
    
    Remove WebURLRequestEfl and WebURLResponseEfl classes
    and their corresponding C APIs since we don't need / use
    them.
    
    * Shared/API/c/efl/WKURLRequestEfl.cpp: Removed.
    * Shared/API/c/efl/WKURLRequestEfl.h: Removed.
    * Shared/API/c/efl/WKURLResponseEfl.cpp: Removed.
    * Shared/API/c/efl/WKURLResponseEfl.h: Removed.
    * Shared/efl/WebURLRequestEfl.cpp: Removed.
    * Shared/efl/WebURLRequestEfl.h: Removed.
    * Shared/efl/WebURLResponseEfl.cpp: Removed.
    * Shared/efl/WebURLResponseEfl.h: Removed.
    * UIProcess/API/efl/ewk_url_request.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_url_request.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_url_request.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKURL.h"
 #include "WKURLRequest.h"
 #include "WebURLRequest.h"
-#include "WebURLRequestEfl.h"
 #include "ewk_url_request_private.h"
 #include <wtf/text/CString.h>
 
