eaeadf5e48ce6f27a460a742d57426d6038c3a23
leandrogracia@chromium.org <leandrogracia@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 19:48:07 +0000

    [Chromium] Enable conversion between WebFloatRect and gfx::RectF. https://bugs.webkit.org/show_bug.cgi?id=92117
    Reviewed by Adam Barth.
    
    Introduce WebFloatRect <--> gfx::RectF conversion as we already do with WebRect <--> gfx::Rect.
    
    * chromium/public/WebFloatRect.h:
    (WebKit::WebFloatRect::WebFloatRect):
    (WebFloatRect):
    (WebKit::WebFloatRect::operator=):
    (WebKit::WebFloatRect::operator gfx::RectF):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123511 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebFloatRect.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebFloatRect.h
@@ -36,6 +36,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if WEBKIT_IMPLEMENTATION
 #include "FloatRect.h"
+#else
+#include <ui/gfx/rect_f.h>
 #endif
 
 namespace WebKit {
@@ -49,10 +51,10 @@ struct WebFloatRect {
     bool isEmpty() const { return width <= 0 || height <= 0; }
 
     WebFloatRect()
-        : x(0.0f)
-        , y(0.0f)
-        , width(0.0f)
-        , height(0.0f)
+        : x(0)
+        , y(0)
+        , width(0)
+        , height(0)
     {
     }
 
@@ -85,6 +87,28 @@ struct WebFloatRect {
     operator WebCore::FloatRect() const
     {
         return WebCore::FloatRect(x, y, width, height);
+    }
+#else
+    WebFloatRect(const gfx::RectF& r)
+        : x(r.x())
+        , y(r.y())
+        , width(r.width())
+        , height(r.height())
+    {
+    }
+
+    WebFloatRect& operator=(const gfx::RectF& r)
+    {
+        x = r.x();
+        y = r.y();
+        width = r.width();
+        height = r.height();
+        return *this;
+    }
+
+    operator gfx::RectF() const
+    {
+        return gfx::RectF(x, y, width, height);
     }
 #endif
 };
