f4977e21d1cbcf4a46b3a4edcb2bcfc462e3e915
zvorygin@chromium.org <zvorygin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 16:59:50 +0000

    Fixed context menu handling for archives.
    BUG=138650
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10802083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148127 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1093,6 +1093,9 @@ FileManager.prototype = {
       // TODO(dgozman): current root may be not yet updated here due
       // to async issues.
       case 'unmount':
+        return this.directoryModel_.getCurrentRootType() == RootType.ARCHIVE ||
+               this.directoryModel_.getCurrentRootType() == RootType.REMOVABLE;
+
       case 'format':
         return this.directoryModel_.getCurrentRootType() == RootType.REMOVABLE;
 
