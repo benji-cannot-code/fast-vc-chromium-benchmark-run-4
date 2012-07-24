92bab330d3b836cdf370da46d7ff3eb03efb52b4
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 23:51:23 +0000

    [V8] Correct WebIDL type should be used in the testing IDL file https://bugs.webkit.org/show_bug.cgi?id=92168
    Reviewed by Kentaro Hara.
    
    Updated bindings test.
    
    * bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
    (webkit_dom_test_obj_strict_function):
    * bindings/scripts/test/GObject/WebKitDOMTestObj.h:
    * bindings/scripts/test/JS/JSTestObj.cpp:
    (WebCore::jsTestObjPrototypeFunctionStrictFunction):
    * bindings/scripts/test/TestObj.idl:
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::enabledAtRuntimeMethod1Callback):
    (WebCore::TestObjV8Internal::enabledAtRuntimeMethod2Callback):
    (WebCore::TestObjV8Internal::enabledPerContextMethod1Callback):
    (WebCore::TestObjV8Internal::enabledPerContextMethod2Callback):
    (WebCore::TestObjV8Internal::strictFunctionCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
@@ -1729,8 +1729,8 @@ static v8::Handle<v8::Value> enabledAtRuntimeMethod1Callback(const v8::Arguments
     if (args.Length() < 1)
         return V8Proxy::throwNotEnoughArgumentsError(args.GetIsolate());
     TestObj* imp = V8TestObj::toNative(args.Holder());
-    EXCEPTION_BLOCK(int, intArg, V8int::HasInstance(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)) ? V8int::toNative(v8::Handle<v8::Object>::Cast(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined))) : 0);
-    imp->enabledAtRuntimeMethod1(intArg);
+    EXCEPTION_BLOCK(long long, Arg, toInt64(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)));
+    imp->enabledAtRuntimeMethod1(Arg);
     return v8::Handle<v8::Value>();
 }
 
@@ -1740,8 +1740,8 @@ static v8::Handle<v8::Value> enabledAtRuntimeMethod2Callback(const v8::Arguments
     if (args.Length() < 1)
         return V8Proxy::throwNotEnoughArgumentsError(args.GetIsolate());
     TestObj* imp = V8TestObj::toNative(args.Holder());
-    EXCEPTION_BLOCK(int, intArg, V8int::HasInstance(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)) ? V8int::toNative(v8::Handle<v8::Object>::Cast(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined))) : 0);
-    imp->enabledAtRuntimeMethod2(intArg);
+    EXCEPTION_BLOCK(long long, Arg, toInt64(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)));
+    imp->enabledAtRuntimeMethod2(Arg);
     return v8::Handle<v8::Value>();
 }
 
@@ -1751,8 +1751,8 @@ static v8::Handle<v8::Value> enabledPerContextMethod1Callback(const v8::Argument
     if (args.Length() < 1)
         return V8Proxy::throwNotEnoughArgumentsError(args.GetIsolate());
     TestObj* imp = V8TestObj::toNative(args.Holder());
-    EXCEPTION_BLOCK(int, intArg, V8int::HasInstance(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)) ? V8int::toNative(v8::Handle<v8::Object>::Cast(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined))) : 0);
-    imp->enabledPerContextMethod1(intArg);
+    EXCEPTION_BLOCK(long long, Arg, toInt64(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)));
+    imp->enabledPerContextMethod1(Arg);
     return v8::Handle<v8::Value>();
 }
 
@@ -1762,8 +1762,8 @@ static v8::Handle<v8::Value> enabledPerContextMethod2Callback(const v8::Argument
     if (args.Length() < 1)
         return V8Proxy::throwNotEnoughArgumentsError(args.GetIsolate());
     TestObj* imp = V8TestObj::toNative(args.Holder());
-    EXCEPTION_BLOCK(int, intArg, V8int::HasInstance(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)) ? V8int::toNative(v8::Handle<v8::Object>::Cast(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined))) : 0);
-    imp->enabledPerContextMethod2(intArg);
+    EXCEPTION_BLOCK(long long, Arg, toInt64(MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined)));
+    imp->enabledPerContextMethod2(Arg);
     return v8::Handle<v8::Value>();
 }
 
@@ -1889,7 +1889,7 @@ static v8::Handle<v8::Value> strictFunctionCallback(const v8::Arguments& args)
     {
     STRING_TO_V8PARAMETER_EXCEPTION_BLOCK(V8Parameter<>, str, MAYBE_MISSING_PARAMETER(args, 0, DefaultIsUndefined));
     EXCEPTION_BLOCK(float, a, static_cast<float>(MAYBE_MISSING_PARAMETER(args, 1, DefaultIsUndefined)->NumberValue()));
-    EXCEPTION_BLOCK(int, b, V8int::HasInstance(MAYBE_MISSING_PARAMETER(args, 2, DefaultIsUndefined)) ? V8int::toNative(v8::Handle<v8::Object>::Cast(MAYBE_MISSING_PARAMETER(args, 2, DefaultIsUndefined))) : 0);
+    EXCEPTION_BLOCK(int, b, toInt32(MAYBE_MISSING_PARAMETER(args, 2, DefaultIsUndefined)));
     RefPtr<bool> result = imp->strictFunction(str, a, b, ec);
     if (UNLIKELY(ec))
         goto fail;
