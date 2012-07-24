96400da0543c57a5e66cd84f6d09c8c37f02e768
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 00:42:17 +0000

    Preferences support for multiple promos.
    * Add promo_type_. For now this is hardcoded to ntp_notification_promo, but we'll also allow bubble_promo soon.
    * Store promo preferences in a dictionary, in a list, in a promo object. This reflects the structure of the promo as we receive it over the wire.
    * Add ClearDeprecatedPrefs.
    * Fix logic of CheckForNewNotification, HandleViewed and HandleClosed. These now call InitFromPreferences.
    * Add better tests for HandleViewed and HandleClosed. Get rid of TestPrefs.
    
    BUG=123061
    TEST=unit tests.
    
    Review URL: https://chromiumcodereview.appspot.com/10790089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/notification_promo.h
+++ chrome/browser/web_resource/notification_promo.h
@@ -28,6 +28,9 @@ class NotificationPromo {
  public:
   static GURL PromoServerURL();
 
+  static const char kNtpNotificationPromoType[];
+  static const char kBubblePromoType[];
+
   explicit NotificationPromo(Profile* profile);
   ~NotificationPromo();
 
@@ -80,6 +83,7 @@ class NotificationPromo {
   Profile* profile_;
   PrefService* prefs_;
 
+  std::string promo_type_;
   std::string promo_text_;
 #if defined(OS_ANDROID)
   std::string promo_text_long_;
