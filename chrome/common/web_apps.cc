c3cda32d75f5d84f21e6325b0d6d19dcb4231738
grv@chromium.org <grv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 03:38:03 +0000

    Add offline_mode support in crx-less apps
    Added a key in the manifest file "offline_enabled", for crx-less apps to support offline_mode.
    
    BUG=101782
    TEST=unit_test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10704092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148039 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/web_apps.cc
+++ chrome/common/web_apps.cc
@@ -104,6 +104,7 @@ const char WebApplicationInfo::kInvalidIconURL[] =
 
 WebApplicationInfo::WebApplicationInfo() {
   is_bookmark_app = false;
+  is_offline_enabled = false;
 }
 
 WebApplicationInfo::~WebApplicationInfo() {
@@ -316,6 +317,9 @@ bool ParseWebAppFromDefinitionFile(Value* definition_value,
       icons.push_back(icon);
     }
   }
+
+  // Parse if offline mode is enabled.
+  definition->GetBoolean("offline_enabled", &web_app->is_offline_enabled);
 
   CHECK(definition->GetString("name", &web_app->title));
   definition->GetString("description", &web_app->description);
--- chrome/common/web_apps.h
+++ chrome/common/web_apps.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -75,6 +75,9 @@ struct WebApplicationInfo {
   // The type of launch container to use with the app. Currently supported
   // values are 'tab' and 'panel'. Only supported with manifest-based apps.
   std::string launch_container;
+
+  // This indicates if the app is functional in offline mode or not.
+  bool is_offline_enabled;
 };
 
 
