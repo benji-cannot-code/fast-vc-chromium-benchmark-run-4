04c66b83b25014142ebf708b4521fe1153a91b0a
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 08:44:58 +0000

    [JSC] REGRESSION(r122912): CodeGeneratorJS.pm generates wrong code for static attributes in supplemental IDL files https://bugs.webkit.org/show_bug.cgi?id=91925
    Reviewed by Adam Barth.
    
    r122912 implemented static attributes in CodeGeneratorJS.pm.
    However, it generates wrong code for static attributes in supplemental
    IDL files. This patch fixes it as shown in JSTestInterface.cpp.
    This fix aligns with the fix in CodeGeneratorV8.pm (r123308).
    
    Test: bindings/scripts/test/TestSupplemental.idl
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateImplementation):
    * bindings/scripts/test/JS/JSTestInterface.cpp:
    (WebCore::jsTestInterfaceConstructorSupplementalStaticReadOnlyAttr):
    (WebCore::jsTestInterfaceConstructorSupplementalStaticAttr):
    (WebCore::setJSTestInterfaceConstructorSupplementalStaticAttr):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123439 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestInterface.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestInterface.cpp
@@ -246,7 +246,7 @@ bool JSTestInterface::getOwnPropertyDescriptor(JSObject* object, ExecState* exec
 JSValue jsTestInterfaceConstructorSupplementalStaticReadOnlyAttr(ExecState* exec, JSValue, PropertyName)
 {
     UNUSED_PARAM(exec);
-    JSValue result = jsNumber(TestSupplemental::supplementalStaticReadOnlyAttr(impl));
+    JSValue result = jsNumber(TestSupplemental::supplementalStaticReadOnlyAttr());
     return result;
 }
 
@@ -256,7 +256,7 @@ JSValue jsTestInterfaceConstructorSupplementalStaticReadOnlyAttr(ExecState* exec
 JSValue jsTestInterfaceConstructorSupplementalStaticAttr(ExecState* exec, JSValue, PropertyName)
 {
     UNUSED_PARAM(exec);
-    JSValue result = jsString(exec, TestSupplemental::supplementalStaticAttr(impl));
+    JSValue result = jsString(exec, TestSupplemental::supplementalStaticAttr());
     return result;
 }
 
@@ -326,7 +326,7 @@ void JSTestInterface::put(JSCell* cell, ExecState* exec, PropertyName propertyNa
 void setJSTestInterfaceConstructorSupplementalStaticAttr(ExecState* exec, JSObject*, JSValue value)
 {
     UNUSED_PARAM(exec);
-    TestSupplemental::setSupplementalStaticAttr(impl, ustringToString(value.isEmpty() ? UString() : value.toString(exec)->value(exec)));
+    TestSupplemental::setSupplementalStaticAttr(ustringToString(value.isEmpty() ? UString() : value.toString(exec)->value(exec)));
 }
 
 #endif
