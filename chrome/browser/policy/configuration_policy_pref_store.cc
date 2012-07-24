afc9286e7b2d38e4b2caad3a8dbc585ac9473fd2
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 11:19:32 +0000

    Modified PolicyService to register observers by domain only, instead of domain and component.
    BUG=108999
    TEST=Nothing breaks
    
    
    Review URL: https://chromiumcodereview.appspot.com/10805057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148082 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_pref_store.cc
+++ chrome/browser/policy/configuration_policy_pref_store.cc
@@ -46,7 +46,7 @@ ConfigurationPolicyPrefStore::ConfigurationPolicyPrefStore(
       level_(level) {
   // Read initial policy.
   prefs_.reset(CreatePreferencesFromPolicies());
-  policy_service_->AddObserver(POLICY_DOMAIN_CHROME, "", this);
+  policy_service_->AddObserver(POLICY_DOMAIN_CHROME, this);
 }
 
 void ConfigurationPolicyPrefStore::AddObserver(PrefStore::Observer* observer) {
@@ -110,7 +110,7 @@ ConfigurationPolicyPrefStore::CreateRecommendedPolicyPrefStore(
 }
 
 ConfigurationPolicyPrefStore::~ConfigurationPolicyPrefStore() {
-  policy_service_->RemoveObserver(POLICY_DOMAIN_CHROME, "", this);
+  policy_service_->RemoveObserver(POLICY_DOMAIN_CHROME, this);
 }
 
 void ConfigurationPolicyPrefStore::Refresh() {
--- chrome/browser/policy/policy_service_impl.h
+++ chrome/browser/policy/policy_service_impl.h
@@ -35,10 +35,8 @@ class PolicyServiceImpl : public PolicyService,
 
   // PolicyService overrides:
   virtual void AddObserver(PolicyDomain domain,
-                           const std::string& component_id,
                            PolicyService::Observer* observer) OVERRIDE;
   virtual void RemoveObserver(PolicyDomain domain,
-                              const std::string& component_id,
                               PolicyService::Observer* observer) OVERRIDE;
   virtual const PolicyMap& GetPolicies(
       PolicyDomain domain,
@@ -48,7 +46,7 @@ class PolicyServiceImpl : public PolicyService,
 
  private:
   typedef ObserverList<PolicyService::Observer, true> Observers;
-  typedef std::map<PolicyBundle::PolicyNamespace, Observers*> ObserverMap;
+  typedef std::map<PolicyDomain, Observers*> ObserverMap;
   typedef std::vector<ConfigurationPolicyObserverRegistrar*> RegistrarList;
 
   // ConfigurationPolicyProvider::Observer overrides:
@@ -84,7 +82,7 @@ class PolicyServiceImpl : public PolicyService,
   // Maps each policy namespace to its current policies.
   PolicyBundle policy_bundle_;
 
-  // Maps each policy namespace to its observer list.
+  // Maps each policy domain to its observer list.
   ObserverMap observers_;
 
   // True if all the providers are initialized.
