6d485aab56e247353ec7f5baa20d924b2960c9d0
kxing@chromium.org <kxing@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 21:22:57 +0000

    Support for both 44.1 kHz and 48 kHz on the client.
    Review URL: https://chromiumcodereview.appspot.com/10795066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148203 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/pepper_audio_player.h
+++ remoting/client/plugin/pepper_audio_player.h
@@ -16,25 +16,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/audio.h"
 #include "ppapi/cpp/instance.h"
 #include "remoting/client/audio_player.h"
+#include "remoting/proto/audio.pb.h"
 
 namespace remoting {
-
-class AudioPacket;
 
 class PepperAudioPlayer : public AudioPlayer {
  public:
   explicit PepperAudioPlayer(pp::Instance* instance);
   virtual ~PepperAudioPlayer();
 
-  // Returns true if successful, false otherwise.
-  virtual bool Start() OVERRIDE;
-
   virtual void ProcessAudioPacket(scoped_ptr<AudioPacket> packet) OVERRIDE;
-
-  virtual bool IsRunning() const OVERRIDE;
 
  private:
   typedef std::list<AudioPacket*> AudioPacketQueue;
+
+  // Resets the audio player and starts the playback.
+  // Returns true on success.
+  bool ResetAudioPlayer(AudioPacket::SamplingRate sampling_rate);
 
   // Function called by the browser when it needs more audio samples.
   static void PepperAudioPlayerCallback(void* samples,
@@ -43,7 +41,10 @@ class PepperAudioPlayer : public AudioPlayer {
 
   void FillWithSamples(void* samples, uint32_t buffer_size);
 
+  pp::Instance* instance_;
   pp::Audio audio_;
+
+  AudioPacket::SamplingRate sampling_rate_;
 
   // The count of sample frames per channel in an audio buffer.
   uint32_t samples_per_frame_;
@@ -58,7 +59,7 @@ class PepperAudioPlayer : public AudioPlayer {
   // The number of bytes from |queued_packets_| that have been consumed.
   size_t bytes_consumed_;
 
-  bool running_;
+  bool start_failed_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperAudioPlayer);
 };
