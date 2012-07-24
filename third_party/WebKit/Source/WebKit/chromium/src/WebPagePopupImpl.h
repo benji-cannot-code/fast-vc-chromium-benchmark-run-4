180970d7ef219603a604e9620e4eb78e50102973
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 13:42:56 +0000

    Unreviewed, rolling out r123463. http://trac.webkit.org/changeset/123463 https://bugs.webkit.org/show_bug.cgi?id=92110
    Broke Android build (Requested by keishi on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-07-24
    
    * src/WebPagePopupImpl.h:
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::handleMouseDown):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123470 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPagePopupImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPagePopupImpl.h
@@ -64,7 +64,6 @@ public:
     bool handleKeyEvent(const WebCore::PlatformKeyboardEvent&);
     void closePopup();
     WebWidgetClient* widgetClient() const { return m_widgetClient; }
-    bool hasSamePopupClient(WebPagePopupImpl* other) { return other && m_popupClient == other->m_popupClient; }
 
 private:
     // WebWidget functions
