2eb26156a7b4089710a6ce423e9b1ef5388b24c2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 21:31:31 +0000

    Copy worker test data to content in preparation for worker tests moving to content_browsertests
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148205 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/test/data/workers/worker_utils.js
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var shared_worker_count = 0;
+function getWorker(worker_url)
+{
+  // Create either a dedicated or shared worker, depending on flags
+  var url = document.location.toString();
+  if (url.search("shared") >= 0) {
+    // Make a shared worker that looks like a worker
+    var worker = new SharedWorker(worker_url, "name" + ++shared_worker_count);
+    worker.port.onmessage = function(evt) {
+      worker.onmessage(evt);
+    };
+    worker.postMessage = function(msg, port) {
+      worker.port.postMessage(msg, port);
+    };
+    return worker;
+  } else {
+    return new Worker(worker_url);
+  }
+}
+
+function onSuccess()
+{
+  setTimeout(onFinished, 0, "OK");
+}
+
+function onFailure() {
+  setTimeout(onFinished, 0, "FAIL");
+}
+
+function onFinished(result) {
+  var statusPanel = document.getElementById("statusPanel");
+  if (statusPanel) {
+    statusPanel.innerHTML = result;
+  }
+
+  document.title = result;
+}
