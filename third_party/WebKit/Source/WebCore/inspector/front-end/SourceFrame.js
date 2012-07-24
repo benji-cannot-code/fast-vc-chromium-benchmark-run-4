d436b37008f39d2d208ec5144e7bd8a9cc3585b0
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 18:05:14 +0000

    Web Inspector: Snippets UI polish https://bugs.webkit.org/show_bug.cgi?id=92142
    Reviewed by Pavel Feldman.
    
    Changed snippets context menu items.
    Added run snippet status bar button.
    Removed unused edit source button.
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/Images/statusbarButtonGlyphs.png:
    * inspector/front-end/ScriptSnippetModel.js:
    (WebInspector.SnippetJavaScriptSourceFrame):
    (WebInspector.SnippetJavaScriptSourceFrame.prototype.statusBarItems):
    (WebInspector.SnippetJavaScriptSourceFrame.prototype._runButtonClicked):
    * inspector/front-end/ScriptsNavigator.js:
    (WebInspector.SnippetsNavigatorView.prototype.getOrCreateFolderTreeElement):
    (WebInspector.SnippetsNavigatorView.prototype.handleContextMenu):
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype._updateScriptViewStatusBarItems):
    (WebInspector.ScriptsPanel.prototype._createSourceFrame):
    * inspector/front-end/SourceFrame.js:
    (WebInspector.SourceFrame.prototype.statusBarItems):
    * inspector/front-end/inspector.css:
    * inspector/front-end/scriptsPanel.css:
    (.evaluate-snippet-status-bar-item .glyph):
    (.evaluate-snippet-status-bar-item.toggled .glyph):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123494 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -104,6 +104,14 @@ WebInspector.SourceFrame.prototype = {
         this._clearLineToReveal();
     },
 
+    /**
+     * @return {Array.<Element>}
+     */
+    statusBarItems: function()
+    {
+        return [];
+    },
+
     defaultFocusedElement: function()
     {
         return this._textEditor.defaultFocusedElement();
