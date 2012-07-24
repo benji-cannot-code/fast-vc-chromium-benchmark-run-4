4af39eae5fd941c43d2dcc252f618dac05834236
smckay@chromium.org <smckay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 20:15:19 +0000

    Cleanup URLInterceptors in test methods.
    BUG=138518,138520
    TEST=ProtocolHandlerRegistryTest
    
    Review URL: https://chromiumcodereview.appspot.com/10796091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/custom_handlers/protocol_handler_registry_unittest.cc
+++ chrome/browser/custom_handlers/protocol_handler_registry_unittest.cc
@@ -748,10 +748,10 @@ TEST_F(ProtocolHandlerRegistryTest, TestMaybeCreateTaskWorksFromIOThread) {
   ProtocolHandler ph1 = CreateProtocolHandler("mailto", "test1");
   registry()->OnAcceptRegisterProtocolHandler(ph1);
   GURL url("mailto:someone@something.com");
-  net::URLRequestJobFactory::Interceptor* interceptor =
-      registry()->CreateURLInterceptor();
 
-  AssertIntercepted(url, interceptor);
+  scoped_ptr<net::URLRequestJobFactory::Interceptor> interceptor(
+      registry()->CreateURLInterceptor());
+  AssertIntercepted(url, interceptor.get());
 }
 
 TEST_F(ProtocolHandlerRegistryTest,
@@ -759,10 +759,10 @@ TEST_F(ProtocolHandlerRegistryTest,
   std::string scheme("mailto");
   ProtocolHandler ph1 = CreateProtocolHandler(scheme, "test1");
   registry()->OnAcceptRegisterProtocolHandler(ph1);
-  net::URLRequestJobFactory::Interceptor* interceptor =
-      registry()->CreateURLInterceptor();
 
-  AssertWillHandle(scheme, true, interceptor);
+  scoped_ptr<net::URLRequestJobFactory::Interceptor> interceptor(
+      registry()->CreateURLInterceptor());
+  AssertWillHandle(scheme, true, interceptor.get());
 }
 
 TEST_F(ProtocolHandlerRegistryTest, TestRemovingDefaultFallsBackToOldDefault) {
@@ -806,10 +806,10 @@ TEST_F(ProtocolHandlerRegistryTest, MAYBE_TestClearDefaultGetsPropagatedToIO) {
   ProtocolHandler ph1 = CreateProtocolHandler(scheme, "test1");
   registry()->OnAcceptRegisterProtocolHandler(ph1);
   registry()->ClearDefault(scheme);
-  net::URLRequestJobFactory::Interceptor* interceptor =
-      registry()->CreateURLInterceptor();
 
-  AssertWillHandle(scheme, false, interceptor);
+  scoped_ptr<net::URLRequestJobFactory::Interceptor> interceptor(
+      registry()->CreateURLInterceptor());
+  AssertWillHandle(scheme, false, interceptor.get());
 }
 
 TEST_F(ProtocolHandlerRegistryTest, TestLoadEnabledGetsPropogatedToIO) {
@@ -817,12 +817,11 @@ TEST_F(ProtocolHandlerRegistryTest, TestLoadEnabledGetsPropogatedToIO) {
   ProtocolHandler ph1 = CreateProtocolHandler(mailto, "MailtoHandler");
   registry()->OnAcceptRegisterProtocolHandler(ph1);
 
-  net::URLRequestJobFactory::Interceptor* interceptor =
-      registry()->CreateURLInterceptor();
-  AssertWillHandle(mailto, true, interceptor);
+  scoped_ptr<net::URLRequestJobFactory::Interceptor> interceptor(
+      registry()->CreateURLInterceptor());
+  AssertWillHandle(mailto, true, interceptor.get());
   registry()->Disable();
-  AssertWillHandle(mailto, false, interceptor);
-  delete interceptor;
+  AssertWillHandle(mailto, false, interceptor.get());
 }
 
 TEST_F(ProtocolHandlerRegistryTest, TestReplaceHandler) {
