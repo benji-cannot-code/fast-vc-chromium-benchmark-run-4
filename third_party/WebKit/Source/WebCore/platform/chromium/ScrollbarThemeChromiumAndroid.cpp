75d95440f84b0ff39b394db5b209bc241ad3b167
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 08:55:18 +0000

    [Chromium] Build fix for Android after r123424 https://bugs.webkit.org/show_bug.cgi?id=92082
    Unreviewed build fix.
    
    Mark removed the PlatformSupport.h header from ScrollbarThemeChromiumAndroid.cpp
    in r123424, while there still are two references left to another method and a
    constant. Re-add the include directive.
    
    * platform/chromium/ScrollbarThemeChromiumAndroid.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/ScrollbarThemeChromiumAndroid.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/ScrollbarThemeChromiumAndroid.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayoutTestSupport.h"
 #include "PlatformContextSkia.h"
 #include "PlatformMouseEvent.h"
+#include "PlatformSupport.h"
 #include "Scrollbar.h"
 #include "TransformationMatrix.h"
 
