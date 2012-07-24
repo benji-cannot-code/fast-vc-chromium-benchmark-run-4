32c9f76fff4621b1e57087382bc5416f053a74bc
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 11:36:23 +0000

    Make audio volume icon HDC Update the image for 2x which only had 4 icons Show mute icon when volume is 0 (+ some simplification)
    BUG=122992,137763
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10781021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148089 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/audio/tray_volume.cc
+++ ash/system/audio/tray_volume.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/audio/tray_volume.h"
 
+#include <cmath>
+
 #include "ash/shell.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/tray/tray_constants.h"
@@ -19,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image.h"
+#include "ui/gfx/image/image_skia_operations.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/image_view.h"
 #include "ui/views/controls/label.h"
@@ -32,7 +35,11 @@ namespace internal {
 namespace {
 const int kVolumeImageWidth = 25;
 const int kVolumeImageHeight = 25;
-const int kVolumeLevel = 4;
+
+// IDR_AURA_UBER_TRAY_VOLUME_LEVELS contains 5 images,
+// The one for mute is at the 0 index and the other
+// four are used for ascending volume levels.
+const int kVolumeLevels = 4;
 }
 
 namespace tray {
@@ -54,24 +61,16 @@ class VolumeButton : public views::ToggleImageButton {
   void Update() {
     ash::SystemTrayDelegate* delegate =
         ash::Shell::GetInstance()->tray_delegate();
-    int level = static_cast<int>(delegate->GetVolumeLevel() * 100);
-    int image_index = level / (100 / kVolumeLevel);
-    if (level > 0 && image_index == 0)
-      ++image_index;
-    if (level == 100)
-      image_index = kVolumeLevel - 1;
-    else if (image_index == kVolumeLevel - 1)
-      --image_index;
-    // Index 0 is reserved for mute.
-    if (delegate->IsAudioMuted())
-      image_index = 0;
-    else
-      ++image_index;
+    float level = delegate->GetVolumeLevel();
+    int image_index = delegate->IsAudioMuted() ?
+        0 : (level == 1.0 ?
+             kVolumeLevels : std::ceil(level * (kVolumeLevels - 1)));
+
     if (image_index != image_index_) {
-      SkIRect region = SkIRect::MakeXYWH(0, image_index * kVolumeImageHeight,
-          kVolumeImageWidth, kVolumeImageHeight);
-      gfx::ImageSkia image_skia;
-      image_.ToImageSkia()->extractSubset(&image_skia, region);
+      gfx::Rect region(0, image_index * kVolumeImageHeight,
+                       kVolumeImageWidth, kVolumeImageHeight);
+      gfx::ImageSkia image_skia = gfx::ImageSkiaOperations::ExtractSubset(
+          *(image_.ToImageSkia()), region);
       SetImage(views::CustomButton::BS_NORMAL, &image_skia);
       image_index_ = image_index;
     }
