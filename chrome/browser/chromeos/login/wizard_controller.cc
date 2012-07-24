f1d45b87ee4f79e647e2beaaac113fdc3e04d235
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 08:21:43 +0000

    [cros] Flip enable-new-oobe switch.
    BUG=129799
    TEST=Manual + autotests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10808061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148061 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wizard_controller.cc
+++ chrome/browser/chromeos/login/wizard_controller.cc
@@ -124,6 +124,9 @@ WizardController* WizardController::default_controller_ = NULL;
 // static
 bool WizardController::skip_user_image_selection_ = false;
 
+// static
+bool WizardController::zero_delay_enabled_ = false;
+
 ///////////////////////////////////////////////////////////////////////////////
 // WizardController, public:
 
@@ -790,8 +793,15 @@ bool WizardController::GetUsageStatisticsReporting() const {
   return usage_statistics_reporting_;
 }
 
+// static
+bool WizardController::IsZeroDelayEnabled() {
+  return zero_delay_enabled_;
+}
+
+// static
 void WizardController::SetZeroDelays() {
   kShowDelayMs = 0;
+  zero_delay_enabled_ = true;
 }
 
 }  // namespace chromeos
--- chrome/browser/chromeos/login/wizard_in_process_browser_test.cc
+++ chrome/browser/chromeos/login/wizard_in_process_browser_test.cc
@@ -23,6 +23,11 @@ WizardInProcessBrowserTest::WizardInProcessBrowserTest(const char* screen_name)
       host_(NULL) {
 }
 
+void WizardInProcessBrowserTest::SetUp() {
+  WizardController::SetZeroDelays();
+  InProcessBrowserTest::SetUp();
+}
+
 void WizardInProcessBrowserTest::SetUpCommandLine(CommandLine* command_line) {
   command_line->AppendSwitch(switches::kNoStartupWindow);
   command_line->AppendSwitch(switches::kLoginManager);
@@ -30,9 +35,6 @@ void WizardInProcessBrowserTest::SetUpCommandLine(CommandLine* command_line) {
 
 void WizardInProcessBrowserTest::SetUpOnMainThread() {
   SetUpWizard();
-
-  WizardController::SetZeroDelays();
-
   if (!screen_name_.empty()) {
     ShowLoginWizard(screen_name_, gfx::Size(1024, 600));
     host_ = BaseLoginDisplayHost::default_host();
