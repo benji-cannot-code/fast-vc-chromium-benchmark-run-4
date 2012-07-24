1cf73e64bb2ddc386551694e8cbd3d7209fcbfb5
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-24 00:12:13 +0000

    A FocusScope for a distributed node should not be its TreeScope. https://bugs.webkit.org/show_bug.cgi?id=91829
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Current implementation of FocusScope::focusScopeOf(Node*) returns
    the given node's treeScope().  That does not apply if the node is
    a distributed node.  We should calculate a FocusScope for a
    distributed node by traversing ancestor nodes in Composed Shadow
    Tree.
    
    Test: fast/dom/shadow/focus-navigation-with-distributed-nodes.html
    
    * page/FocusController.cpp:
    (WebCore::FocusScope::focusScopeOf):
    
    LayoutTests:
    
    * fast/dom/shadow/focus-navigation-with-distributed-nodes-expected.txt: Added.
    * fast/dom/shadow/focus-navigation-with-distributed-nodes.html: Added.
    * fast/dom/shadow/resources/shadow-dom.js:
    (shouldNavigateFocus):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/shadow/resources/shadow-dom.js
+++ third_party/WebKit/LayoutTests/fast/dom/shadow/resources/shadow-dom.js
@@ -125,6 +125,10 @@ function shouldNavigateFocus(from, to, direction)
       return;
     }
     fromElement.focus();
+    if (!isInnermostActiveElement(from)) {
+        debug('FAIL: Can not be focused: '+ from);
+        return;
+    }
     if (direction == 'forward')
         navigateFocusForward();
     else
