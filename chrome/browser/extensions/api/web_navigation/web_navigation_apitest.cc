f8112be75c481b4f821520853f6201ca1e0bee71
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-24 11:25:23 +0000

    Revert 148074 - Pass the render process id to the FrameNavigationState.
    It's used in addition to frame ids to identify frames correctly.
    
    Also expose the processId via webNavigation.getFrame and getAllFrames
    
    BUG=136090
    TEST=ExtensionApiTest.WebNavigationGetFrame
    
    Review URL: https://chromiumcodereview.appspot.com/10805066
    
    TBR=jochen@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10808097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148086 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -190,7 +190,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WebNavigationUserAction) {
   params.media_type = WebKit::WebContextMenuData::MediaTypeNone;
   params.page_url = url;
   params.frame_id = WebNavigationTabObserver::Get(tab)->
-      frame_navigation_state().GetMainFrameID().frame_num;
+      frame_navigation_state().GetMainFrameID();
   params.link_url = extension->GetResourceURL("userAction/b.html");
 
   TestRenderViewContextMenu menu(tab, params);
