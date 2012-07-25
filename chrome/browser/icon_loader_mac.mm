0b3453243388ed33c1060185185f71386765927e
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 16:13:12 +0000

    Added support for Hidpi in download UI
    BUG=136949
    TEST=Manual, see instructions below
    
    Run chrome with --force-device-scale-factor=2 --highlight-missing-2x-resources
    Ensure that the file type icon in the download bar shows up red
    
    Review URL: https://chromiumcodereview.appspot.com/10703173
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148334 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/icon_loader_mac.mm
+++ chrome/browser/icon_loader_mac.mm
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/threading/thread.h"
 #include "base/sys_string_conversions.h"
-#include "skia/ext/skia_utils_mac.h"
-#include "third_party/skia/include/core/SkBitmap.h"
+#include "ui/gfx/image/image_skia.h"
+#include "ui/gfx/image/image_skia_util_mac.h"
 
 void IconLoader::ReadIcon() {
   NSString* group = base::SysUTF8ToNSString(group_);
@@ -34,7 +34,7 @@ void IconLoader::ReadIcon() {
       default:
         NOTREACHED();
     }
-    image_.reset(new gfx::Image(gfx::NSImageToSkBitmap(icon, size, false)));
+    image_.reset(new gfx::Image(gfx::ImageSkiaFromResizedNSImage(icon, size)));
   }
 
   target_message_loop_->PostTask(FROM_HERE,
