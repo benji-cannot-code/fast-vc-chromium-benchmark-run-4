0e0807b8c8f6603bba3aa4b8aff775dd6dbf4e3f
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 21:26:12 +0000

    Add a clarifying comment to the memory layout navigation. (Inspired by Coverity CID 100022)
    R=tommi@chromium.org
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10826014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148412 0039d316-1c4b-4281-b951-d872f2087c98

--- courgette/memory_allocator.cc
+++ courgette/memory_allocator.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -136,6 +136,8 @@ bool TempMapping::Initialize(size_t size) {
 
 void* TempMapping::memory() const {
   uint8* mem = reinterpret_cast<uint8*>(mapping_.view());
+  // The 'this' pointer is written at the start of mapping_.view(), so
+  // go past it. (See Initialize()).
   if (mem)
     mem += sizeof(this);
   DCHECK(mem);
