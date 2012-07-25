9853ea81df9cc1b0158fcec6eb8c752b03a7ad3b
husky@chromium.org <husky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 18:20:07 +0000

    Disable WebGL on Android by default (but allow it to be enabled)
    WebGL can run on Android but we need to assess how robust and secure
    it is. This patch adds an --enable-experimental-webgl flag to allow
    us to run conformance tests on various devices.
    
    BUG=138958
    TEST=Manual testing with flag, no change if flag is not used
    
    
    Review URL: https://chromiumcodereview.appspot.com/10830011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148368 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu_util.cc
+++ chrome/browser/gpu_util.cc
@@ -333,7 +333,11 @@ Value* GetFeatureStatus() {
       {
           "webgl",
           flags & content::GPU_FEATURE_TYPE_WEBGL,
+#if defined(OS_ANDROID)
+          !command_line.HasSwitch(switches::kEnableExperimentalWebGL),
+#else
           command_line.HasSwitch(switches::kDisableExperimentalWebGL),
+#endif
           "WebGL has been disabled, either via about:flags or command line.",
           false
       },
@@ -578,7 +582,11 @@ void UpdateStats() {
   const bool kGpuFeatureUserFlags[] = {
       command_line.HasSwitch(switches::kDisableAccelerated2dCanvas),
       command_line.HasSwitch(switches::kDisableAcceleratedCompositing),
+#if defined(OS_ANDROID)
+      !command_line.HasSwitch(switches::kEnableExperimentalWebGL)
+#else
       command_line.HasSwitch(switches::kDisableExperimentalWebGL)
+#endif
   };
 #if defined(OS_WIN)
   const std::string kGpuBlacklistFeatureHistogramNamesWin[] = {
