0daba66a5243624bcb88ed00cd3ad95f366ea339
orenb@chromium.org <orenb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 22:48:27 +0000

    Fixed coverity defects pertaining to pass-by-value use where pass-by-reference is preferred
    CID_COUNT=3
    CID=(104210, 104215),104237,104238
    BUG=
    TEST=
    TBR=jln,bolms,brettw
    
    Review URL: https://chromiumcodereview.appspot.com/10825016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148437 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/seccomp-bpf/sandbox_bpf.h
+++ sandbox/linux/seccomp-bpf/sandbox_bpf.h
@@ -326,7 +326,7 @@ class Sandbox {
   friend class Util;
   friend class Verifier;
   struct Range {
-    Range(uint32_t f, uint32_t t, ErrorCode e) :
+    Range(uint32_t f, uint32_t t, const ErrorCode& e) :
       from(f),
       to(t),
       err(e) {
