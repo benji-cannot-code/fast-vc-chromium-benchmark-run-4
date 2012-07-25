8acfef5cf580f3f2cf54d3ddd116d159207ea65a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 14:47:20 +0000

    views: Put bounds_animation_unittest.cc into views namespace.
    TEST=views_unittests
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10790115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148324 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/focus/focus_manager_factory.cc
+++ ui/views/focus/focus_manager_factory.cc
@@ -8,17 +8,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "ui/views/focus/focus_manager.h"
 
+namespace views {
+
 namespace {
 
-using views::FocusManager;
-
-class DefaultFocusManagerFactory : public views::FocusManagerFactory {
+class DefaultFocusManagerFactory : public FocusManagerFactory {
  public:
-  DefaultFocusManagerFactory() : views::FocusManagerFactory() {}
+  DefaultFocusManagerFactory() : FocusManagerFactory() {}
   virtual ~DefaultFocusManagerFactory() {}
 
  protected:
-  virtual FocusManager* CreateFocusManager(views::Widget* widget) OVERRIDE {
+  virtual FocusManager* CreateFocusManager(Widget* widget) OVERRIDE {
     return new FocusManager(widget, NULL /* delegate */);
   }
 
@@ -26,11 +26,9 @@ class DefaultFocusManagerFactory : public views::FocusManagerFactory {
   DISALLOW_COPY_AND_ASSIGN(DefaultFocusManagerFactory);
 };
 
-views::FocusManagerFactory* focus_manager_factory = NULL;
+FocusManagerFactory* focus_manager_factory = NULL;
 
 }  // namespace
-
-namespace views {
 
 FocusManagerFactory::FocusManagerFactory() {
 }
