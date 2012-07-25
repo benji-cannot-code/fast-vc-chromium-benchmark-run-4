744c4791cbb9c84b856a03568e6fc0e0bd7d45b0
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-25 02:35:01 +0000

    Move PagePopupClient helper functions so they can be shared https://bugs.webkit.org/show_bug.cgi?id=92096
    Reviewed by Kent Tamura.
    
    Move PagePopupClient helper functions to a separate file,
    so they can be shared with new PagePopupClients.
    
    No new tests. Covered by fast/forms/date/calendar-picker-appearance.html
    
    * WebCore.gypi: Added PagePopupClient.cpp
    * html/shadow/CalendarPickerElement.cpp:
    (WebCore::CalendarPickerElement::writeDocument): Modified to use PagePopupClient.
    * page/PagePopupClient.cpp: Added.
    (WebCore):
    (WebCore::PagePopupClient::addJavaScriptString):
    (WebCore::PagePopupClient::addProperty):
    * page/PagePopupClient.h:
    (WebCore::PagePopupClient::addString):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/PagePopupClient.h
+++ third_party/WebKit/Source/WebCore/page/PagePopupClient.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(PAGE_POPUP)
 
+#include "DocumentWriter.h"
 #include "IntSize.h"
 #include <wtf/text/WTFString.h>
 
@@ -60,7 +61,21 @@ public:
     virtual void didClosePopup() = 0;
 
     virtual ~PagePopupClient() { }
+
+    // Helper functions to be used in PagePopupClient::writeDocument().
+    static void addString(const String&, DocumentWriter&);
+    static void addJavaScriptString(const String&, DocumentWriter&);
+    static void addProperty(const char* name, const String& value, DocumentWriter&);
+    static void addProperty(const char* name, unsigned value, DocumentWriter&);
+    static void addProperty(const char* name, bool value, DocumentWriter&);
+    static void addProperty(const char* name, const Vector<String>& values, DocumentWriter&);
 };
+
+inline void PagePopupClient::addString(const String& str, DocumentWriter& writer)
+{
+    CString str8 = str.utf8();
+    writer.addData(str8.data(), str8.length());
+}
 
 }
 #endif
