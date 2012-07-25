fd068976f8eecff6b6e7d8ace372fbc94b7e905c
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-25 22:15:05 +0000

    Source/JavaScriptCore: Create a specialized pair for use in HashMap iterators https://bugs.webkit.org/show_bug.cgi?id=92137
    Reviewed by Ryosuke Niwa.
    
    Update a couple of sites that relied on the fact that "contents" of iterators were
    std::pairs.
    
    * profiler/Profile.cpp:
    (JSC): This code kept a vector of the pairs that were the "contents" of the iterators. This
    is changed to use a KeyValuePair. We make use HashCount's ValueType (which represents only
    the key) to get the proper key parameter for KeyValuePair.
    * tools/ProfileTreeNode.h:
    (ProfileTreeNode): Use HashMap::ValueType to declare the type of the contents of the hash
    instead of declaring it manually. This will make use of the new KeyValuePair.
    
    Source/WebKit2: Create a specialized struct for use in HashMap iterators
    https://bugs.webkit.org/show_bug.cgi?id=92137
    
    Reviewed by Ryosuke Niwa.
    
    * Platform/CoreIPC/ArgumentCoders.h: Add encode/decode for KeyValuePair.
    
    Source/WTF: Create a specialized pair for use in HashMap iterators
    https://bugs.webkit.org/show_bug.cgi?id=92137
    
    Reviewed by Ryosuke Niwa.
    
    The type used for internal storage in HashMap is exposed in its interface as iterator
    "contents". Currently HashMap uses std::pair<>, which this patch replaces with
    KeyValuePair.
    
    Having this specialized structure will allow us to customize the members to be called
    key/value, improving readability in code using HashMap iterators. They are still called
    first/second to separate this change from the mechanical change of updating callsites.
    
    * wtf/HashIterators.h:
    (HashTableConstKeysIterator):
    (HashTableConstValuesIterator):
    (HashTableKeysIterator):
    (HashTableValuesIterator):
    Use KeyValuePair instead of std::pair when defining the iterators.
    
    * wtf/HashMap.h:
    (WTF):
    (WTF::KeyValuePairKeyExtractor::extract):
    (HashMap):
    Remove PairFirstExtractor. Add and use the KeyValuePair corresponding extractor.
    
    (WTF::HashMapValueTraits::isEmptyValue): Use KeyValuePairHashTraits for HashMaps.
    (WTF::HashMapTranslator::translate):
    (WTF::HashMapTranslatorAdapter::translate):
    The traits of the mapped value is now called ValueTraits instead of SecondTraits.
    
    * wtf/HashTable.h:
    (WTF::hashTableSwap): Add specialization for swapping KeyValuePairs.
    (WTF): Remove now unneeded specialization for std::pairs.
    
    * wtf/HashTraits.h:
    (KeyValuePair):
    (WTF::KeyValuePair::KeyValuePair):
    (WTF):
    Specialized pair. In the future difference from pair should be the member names.
    
    (KeyValuePairHashTraits):
    (WTF::KeyValuePairHashTraits::emptyValue):
    (WTF::KeyValuePairHashTraits::constructDeletedValue):
    (WTF::KeyValuePairHashTraits::isDeletedValue):
    These traits are analogous to PairHashTraits but for KeyValuePair.
    
    * wtf/RefPtrHashMap.h: Use KeyValuePairHashTraits.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/HashIterators.h
+++ third_party/WebKit/Source/WTF/wtf/HashIterators.h
@@ -34,9 +34,9 @@ namespace WTF {
     template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableKeysIterator;
     template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableValuesIterator;
 
-    template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableConstIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > {
+    template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableConstIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > {
     private:
-        typedef std::pair<KeyType, MappedType> ValueType;
+        typedef KeyValuePair<KeyType, MappedType> ValueType;
     public:
         typedef HashTableConstKeysIterator<HashTableType, KeyType, MappedType> Keys;
         typedef HashTableConstValuesIterator<HashTableType, KeyType, MappedType> Values;
@@ -57,9 +57,9 @@ namespace WTF {
         typename HashTableType::const_iterator m_impl;
     };
 
-    template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > {
+    template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > {
     private:
-        typedef std::pair<KeyType, MappedType> ValueType;
+        typedef KeyValuePair<KeyType, MappedType> ValueType;
     public:
         typedef HashTableKeysIterator<HashTableType, KeyType, MappedType> Keys;
         typedef HashTableValuesIterator<HashTableType, KeyType, MappedType> Values;
@@ -87,7 +87,7 @@ namespace WTF {
 
     template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableConstKeysIterator {
     private:
-        typedef HashTableConstIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > ConstIterator;
+        typedef HashTableConstIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > ConstIterator;
 
     public:
         HashTableConstKeysIterator(const ConstIterator& impl) : m_impl(impl) {}
@@ -104,7 +104,7 @@ namespace WTF {
 
     template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableConstValuesIterator {
     private:
-        typedef HashTableConstIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > ConstIterator;
+        typedef HashTableConstIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > ConstIterator;
 
     public:
         HashTableConstValuesIterator(const ConstIterator& impl) : m_impl(impl) {}
@@ -121,8 +121,8 @@ namespace WTF {
 
     template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableKeysIterator {
     private:
-        typedef HashTableIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > Iterator;
-        typedef HashTableConstIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > ConstIterator;
+        typedef HashTableIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > Iterator;
+        typedef HashTableConstIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > ConstIterator;
 
     public:
         HashTableKeysIterator(const Iterator& impl) : m_impl(impl) {}
@@ -144,8 +144,8 @@ namespace WTF {
 
     template<typename HashTableType, typename KeyType, typename MappedType> struct HashTableValuesIterator {
     private:
-        typedef HashTableIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > Iterator;
-        typedef HashTableConstIteratorAdapter<HashTableType, std::pair<KeyType, MappedType> > ConstIterator;
+        typedef HashTableIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > Iterator;
+        typedef HashTableConstIteratorAdapter<HashTableType, KeyValuePair<KeyType, MappedType> > ConstIterator;
 
     public:
         HashTableValuesIterator(const Iterator& impl) : m_impl(impl) {}
--- third_party/WebKit/Source/WTF/wtf/RefPtrHashMap.h
+++ third_party/WebKit/Source/WTF/wtf/RefPtrHashMap.h
@@ -35,7 +35,7 @@ namespace WTF {
     private:
         typedef KeyTraitsArg KeyTraits;
         typedef MappedTraitsArg MappedTraits;
-        typedef PairHashTraits<KeyTraits, MappedTraits> ValueTraits;
+        typedef KeyValuePairHashTraits<KeyTraits, MappedTraits> ValueTraits;
 
     public:
         typedef typename KeyTraits::TraitType KeyType;
@@ -52,7 +52,7 @@ namespace WTF {
         
         typedef HashArg HashFunctions;
 
-        typedef HashTable<KeyType, ValueType, PairFirstExtractor<ValueType>,
+        typedef HashTable<KeyType, ValueType, KeyValuePairKeyExtractor<ValueType>,
             HashFunctions, ValueTraits, KeyTraits> HashTableType;
 
         typedef HashMapTranslator<ValueTraits, HashFunctions>
