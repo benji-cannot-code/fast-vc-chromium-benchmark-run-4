cd10722dd396f700b62aecd0d03d21b44d499d63
allan.jensen@nokia.com <allan.jensen@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-25 16:45:17 +0000

    Incorrect rounding in ceiledLayoutUnit and roundedLayoutUnit. https://bugs.webkit.org/show_bug.cgi?id=91061
    Reviewed by Adam Barth.
    
    The two functions are currently both returning the flooredLayoutUnit when subpixel
    layout is enabled. This patch changes them to return the correct fractional rounded
    value.
    
    * platform/FractionalLayoutUnit.h:
    (WebCore::FractionalLayoutUnit::fromFloatCeil):
    (FractionalLayoutUnit):
    (WebCore::FractionalLayoutUnit::fromFloatRound):
    * rendering/LayoutTypes.h:
    (WebCore::roundedLayoutUnit):
    (WebCore::ceiledLayoutUnit):
    * rendering/RenderBlock.cpp:
    (WebCore::updatePreferredWidth):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -5594,7 +5594,7 @@ static inline void stripTrailingSpace(float& inlineMax, float& inlineMin,
 
 static inline void updatePreferredWidth(LayoutUnit& preferredWidth, float& result)
 {
-    LayoutUnit snappedResult = LayoutUnit::fromFloatCeil(result);
+    LayoutUnit snappedResult = ceiledLayoutUnit(result);
     preferredWidth = max(snappedResult, preferredWidth);
 }
 
