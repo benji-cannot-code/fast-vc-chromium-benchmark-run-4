8ea5b59c2d2d9ad67e9475e36b0f0dc118b534f7
kgr@chromium.org <kgr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-25 21:09:20 +0000

    Added hover feedback on touch to launcher buttons.
    BUG=131186
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10702102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148408 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_button.cc
+++ ash/launcher/launcher_button.cc
@@ -10,9 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/launcher/launcher_button_host.h"
 #include "grit/ui_resources.h"
+#include "skia/ext/image_operations.h"
+#include "ui/aura/event.h"
 #include "ui/base/accessibility/accessible_view_state.h"
 #include "ui/base/animation/animation_delegate.h"
 #include "ui/base/animation/throb_animation.h"
+#include "ui/base/events.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/compositor/layer.h"
 #include "ui/compositor/layer_animation_element.h"
@@ -367,6 +370,12 @@ void LauncherButton::OnMouseExited(const views::MouseEvent& event) {
 ui::GestureStatus LauncherButton::OnGestureEvent(
     const views::GestureEvent& event) {
   switch (event.type()) {
+    case ui::ET_GESTURE_TAP_DOWN:
+      AddState(STATE_HOVERED);
+      return CustomButton::OnGestureEvent(event);
+    case ui::ET_GESTURE_END:
+      ClearState(STATE_HOVERED);
+      return CustomButton::OnGestureEvent(event);
     case ui::ET_GESTURE_SCROLL_BEGIN:
       host_->PointerPressedOnButton(this, LauncherButtonHost::TOUCH, event);
       return ui::GESTURE_STATUS_CONSUMED;
