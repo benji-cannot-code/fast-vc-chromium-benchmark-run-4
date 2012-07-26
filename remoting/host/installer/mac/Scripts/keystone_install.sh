c6739888bc664cf096d14b5b41d40b5df16bd30a
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 23:40:48 +0000

    [Chromoting] Fix keystone install script to use .pkg instead of .mpkg.
    Iceberg builds .mpkgs, but Packages builds flat .pkgs. We switch to using
    Packages because it can create "flag packages" that we can sign and 
    signing is required for Mac OS X 10.8.
    
    When we switched from .mpkg to .pkg, the keystone installer script was
    not updated. So keystone installs are not happening.
    Review URL: https://chromiumcodereview.appspot.com/10831041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148655 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/installer/mac/Scripts/keystone_install.sh
+++ remoting/host/installer/mac/Scripts/keystone_install.sh
@@ -7,4 +7,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Version = @@VERSION@@
 
-/usr/sbin/installer -pkg "$1/@@HOST_PKG@@.mpkg" -target /
+/usr/sbin/installer -pkg "$1/@@HOST_PKG@@.pkg" -target /
