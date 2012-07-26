12ae7ad17f9059b5583a9d9f9c3df91a7bd8f2c8
charles.wei@torchmobile.com.cn <charles.wei@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-26 12:21:09 +0000

    Remove redundant include of PageGroup.h in FrameLoader.cpp https://bugs.webkit.org/show_bug.cgi?id=92357
    Reviewed by Kentaro Hara.
    
    No new tests, just remove redundant include file.
    
    * loader/FrameLoader.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -81,7 +81,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryInstrumentation.h"
 #include "Page.h"
 #include "PageCache.h"
-#include "PageGroup.h"
 #include "PageTransitionEvent.h"
 #include "PluginData.h"
 #include "PluginDatabase.h"
