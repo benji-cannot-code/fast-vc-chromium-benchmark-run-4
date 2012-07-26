e96ec937d4039c71b47166ddfd56f3f7af5c65da
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 05:44:05 +0000

    Support copying a partial rectangle region from the compositing surface on Aura and GTK.
    BUG=118571
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10815070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148520 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/test_render_view_host.cc
+++ content/browser/renderer_host/test_render_view_host.cc
@@ -101,7 +101,8 @@ BackingStore* TestRenderWidgetHostView::AllocBackingStore(
 }
 
 void TestRenderWidgetHostView::CopyFromCompositingSurface(
-    const gfx::Size& size,
+    const gfx::Rect& src_subrect,
+    const gfx::Size& dst_size,
     const base::Callback<void(bool)>& callback,
     skia::PlatformCanvas* output) {
   callback.Run(false);
