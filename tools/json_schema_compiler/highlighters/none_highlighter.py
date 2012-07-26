7f10f28568e56a8a73dd60f94b9c3aa19db7a7a4
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 02:22:18 +0000

    Fix syntax highlighting in JSON Schema Compiler's preview server: don't crash if Pygments isn't installed, and escape HTML (especially obvious with generics) when syntax highlighting is turned off.
    Review URL: https://chromiumcodereview.appspot.com/10833024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148482 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/json_schema_compiler/highlighters/none_highlighter.py
+++ tools/json_schema_compiler/highlighters/none_highlighter.py
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import cgi
+
 class NoneHighlighter(object):
   """Highlighter that just wraps code in a <pre>.
   """
@@ -10,7 +12,7 @@ class NoneHighlighter(object):
     return ''
 
   def GetCodeElement(self, code, style):
-    return '<pre>' + code + '</pre>'
+    return '<pre>' + cgi.escape(code) + '</pre>'
 
   def DisplayName(self):
     return 'none'
