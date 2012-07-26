c1b9274c25c93df782bbb79e7ea5591d66aaa36f
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 00:49:29 +0000

    Remove blur on bubble windows
    Bubble windows sometimes had drawing artifacts. It looks like this was caused by the blur filter we added to those windows.
    
    This CL removes the blur filter (partial revert of r134795 and r147628).
    
    BUG=138761
    TEST=Ran and verified that bubble windows looked ok on external display.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10833009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148451 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/info_bubble_view.mm
+++ chrome/browser/ui/cocoa/info_bubble_view.mm
@@ -63,7 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [bezier lineToPoint:NSMakePoint(arrowStart.x + info_bubble::kBubbleArrowWidth,
                                   arrowStart.y)];
   [bezier closePath];
-  [[NSColor colorWithCalibratedWhite:1.0 alpha:0.9] set];
+  [[NSColor colorWithCalibratedWhite:1.0 alpha:0.96] set];
   [bezier fill];
 }
 
