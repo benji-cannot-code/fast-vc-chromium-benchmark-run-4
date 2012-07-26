a8a1ec3c3b4c55266eebfce91847074861a4328c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-26 11:32:27 +0000

    [EFL] [WK2] Memory leak in ewk_view_resource_load_client.cpp https://bugs.webkit.org/show_bug.cgi?id=92099
    Patch by Sudarsana Nagineni <sudarsana.nagineni@linux.intel.com> on 2012-07-26
    Reviewed by Kentaro Hara.
    
    Fix a memory leak by clearing the loadingResources HashMap on exit.
    
    * UIProcess/API/efl/ewk_view.cpp:
    (_Ewk_View_Private_Data::~_Ewk_View_Private_Data):
    (_ewk_view_priv_loading_resources_clear):
    (ewk_view_load_provisional_started):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
@@ -52,6 +52,7 @@ static const char EWK_VIEW_TYPE_STR[] = "EWK2_View";
 static const int defaultCursorSize = 16;
 
 typedef HashMap<uint64_t, Ewk_Web_Resource*> LoadingResourcesMap;
+static void _ewk_view_priv_loading_resources_clear(LoadingResourcesMap& loadingResourcesMap);
 
 struct _Ewk_View_Private_Data {
     OwnPtr<PageClientImpl> pageClient;
@@ -85,6 +86,7 @@ struct _Ewk_View_Private_Data {
         eina_stringshare_del(title);
         eina_stringshare_del(theme);
         eina_stringshare_del(customEncoding);
+        _ewk_view_priv_loading_resources_clear(loadingResourcesMap);
 
         if (cursorObject)
             evas_object_del(cursorObject);
@@ -310,6 +312,17 @@ static Ewk_View_Private_Data* _ewk_view_priv_new(Ewk_View_Smart_Data* smartData)
 #endif
 
     return priv;
+}
+
+static void _ewk_view_priv_loading_resources_clear(LoadingResourcesMap& loadingResourcesMap)
+{
+    // Clear the loadingResources HashMap.
+    LoadingResourcesMap::iterator it = loadingResourcesMap.begin();
+    LoadingResourcesMap::iterator end = loadingResourcesMap.end();
+    for ( ; it != end; ++it)
+        ewk_web_resource_unref(it->second);
+
+    loadingResourcesMap.clear();
 }
 
 static void _ewk_view_priv_del(Ewk_View_Private_Data* priv)
@@ -999,11 +1012,7 @@ void ewk_view_load_provisional_started(Evas_Object* ewkView)
 
     // The main frame started provisional load, we should clear
     // the loadingResources HashMap to start clean.
-    LoadingResourcesMap::iterator it = priv->loadingResourcesMap.begin();
-    LoadingResourcesMap::iterator end = priv->loadingResourcesMap.end();
-    for ( ; it != end; ++it)
-        ewk_web_resource_unref(it->second);
-    priv->loadingResourcesMap.clear();
+    _ewk_view_priv_loading_resources_clear(priv->loadingResourcesMap);
 
     ewk_view_uri_update(ewkView);
     evas_object_smart_callback_call(ewkView, "load,provisional,started", 0);
