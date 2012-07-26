3905fa267c27e9e15c7f01e90f9ec690cf36a5d1
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 15:49:50 +0000

    Fixed ExtensionApiTest.Bookmarks and enabled it again.
    Also fixed a little nit in the chrome.bookmarks.create() call, which would return the wrong error code when given an invalid URL.
    
    BUG=18979,89762
    TEST=ExtensionApiTest.Bookmarks in the browser_tests is green
    
    
    Review URL: https://chromiumcodereview.appspot.com/10831012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148556 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_extension_apitest.cc
+++ chrome/browser/bookmarks/bookmark_extension_apitest.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_apitest.h"
 
-// Flaky test, http://crbug.com/89762.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Bookmarks) {
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Bookmarks) {
   ASSERT_TRUE(RunExtensionTest("bookmarks")) << message_;
 }
