8ee5ae0ffd32b6186b764a5e8a119ab4e799fd20
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-26 07:48:58 +0000

    Unreviewed attempt to fix the chromium-win build.
    * DumpRenderTree/chromium/TestRunner/TestRunner.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/TestRunner.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/TestRunner.cpp
@@ -63,7 +63,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "v8/include/v8.h"
 #include "webkit/support/webkit_support.h"
 #include <algorithm>
-#include <cctype>
 #include <clocale>
 #include <cstdlib>
 #include <limits>
