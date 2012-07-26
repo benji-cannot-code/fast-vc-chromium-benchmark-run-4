78a81891f35476319a44c0666da52378dbf90191
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 00:49:40 +0000

    Flip HW video decode from being behind an "enable" flag to being behind a "disable" flag with per-platform default settings (currently off for all but cros).
    This is in preparation for turning on HW decode by default one platform at a time.
    
    BUG=133827,133828
    
    
    Review URL: https://chromiumcodereview.appspot.com/10805070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148452 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -120,7 +120,7 @@ extern const char kEnableStrictSiteIsolation[];
 CONTENT_EXPORT extern const char kEnableThreadedCompositing[];
 CONTENT_EXPORT extern const char kDisableThreadedCompositing[];
 CONTENT_EXPORT extern const char kEnableTcpFastOpen[];
-CONTENT_EXPORT extern const char kEnableAcceleratedVideoDecode[];
+CONTENT_EXPORT extern const char kDisableAcceleratedVideoDecode[];
 CONTENT_EXPORT extern const char kEnableVideoTrack[];
 extern const char kEnableViewport[];
 CONTENT_EXPORT extern const char kExperimentalLocationFeatures[];
