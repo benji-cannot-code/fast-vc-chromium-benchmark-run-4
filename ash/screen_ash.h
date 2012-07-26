f946ef5268d6326e40bd5b9a706db08b76f8572d
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-26 02:17:23 +0000

    Fix coordinates in tab detach/drag and multi window resizer.
    ToplevelWindowEventFilter:
    * Use location in root window for drag_location
    
    DocInfo::GetLocalProcessWindowAtPoint
    * Use ScreenPositionClient to convert a point in screen to window
    * Use correct root window at the position.
    
    MultiWindowResizer
    * Use ScreenPositionClient to convert a point in screen to window.
    * show_bounds_ should be screen coordinates.
    
    I also renamed {screen|parent}_{location|position| to
    {location|position}_in_{screen|parent} to be more explicit and consistent with the rest of the code.
    
    I'll work on the test separately as we discussed offline.
    
    BUG=138997,138868
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10827022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148478 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/screen_ash.h
+++ ash/screen_ash.h
@@ -8,9 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "base/compiler_specific.h"
-#include "ui/gfx/insets.h"
-#include "ui/gfx/rect.h"
 #include "ui/gfx/screen_impl.h"
+
+namespace gfx {
+class Rect;
+}
 
 namespace ash {
 
