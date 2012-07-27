15de5bb5f3f6abe2d5beaa648a5f41fcf00f4903
posciak@chromium.org <posciak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 22:09:11 +0000

    Extract Scoped{FrameBuffer,Texture}Binder and clean up TFP in VAVDA.
    Clean up TFP in VAVDA:
    - properly restore currently bound texture after binding our own
    - use the extracted Binders for the above
    - use glXDestroyPixmap instead of glXDestroyPixmapGLX
    - do not bind texture twice
    
    Also modify TextureImageTransportSurface to use the extracted Binders.
    
    BUG=
    TEST=manual runs of VAVDA
    
    
    Review URL: https://chromiumcodereview.appspot.com/10827052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148827 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/common/gpu/gl_scoped_binders.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_COMMON_GPU_GL_SCOPED_BINDERS_H_
+#define CONTENT_COMMON_GPU_GL_SCOPED_BINDERS_H_
+
+namespace content {
+
+class ScopedFrameBufferBinder {
+ public:
+  explicit ScopedFrameBufferBinder(unsigned int fbo);
+  ~ScopedFrameBufferBinder();
+
+ private:
+  int old_fbo_;
+};
+
+class ScopedTextureBinder {
+ public:
+  explicit ScopedTextureBinder(unsigned int id);
+  ~ScopedTextureBinder();
+
+ private:
+  int old_id_;
+};
+
+}  // namespace content
+
+#endif  // CONTENT_COMMON_GPU_GL_SCOPED_BINDERS_H_
--- content/common/gpu/media/vaapi_h264_decoder.cc
+++ content/common/gpu/media/vaapi_h264_decoder.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/stl_util.h"
+#include "content/common/gpu/gl_scoped_binders.h"
 #include "content/common/gpu/media/vaapi_h264_decoder.h"
 #include "third_party/libva/va/va.h"
 #include "third_party/libva/va/va_x11.h"
@@ -256,14 +257,14 @@ VaapiH264Decoder::DecodeSurface::DecodeSurface(
   if (!make_context_current_.Run())
     return;
 
-  glEnable(GL_TEXTURE_2D);
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
+  content::ScopedTextureBinder texture_binder(texture_id_);
   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
   XWindowAttributes win_attr;
   int screen = DefaultScreen(x_display_);
   XGetWindowAttributes(x_display_, RootWindow(x_display_, screen), &win_attr);
+  //TODO(posciak): pass the depth required by libva, not the RootWindow's depth
   x_pixmap_ = XCreatePixmap(x_display_, RootWindow(x_display_, screen),
                             width_, height_, win_attr.depth);
   if (!x_pixmap_) {
@@ -284,7 +285,6 @@ VaapiH264Decoder::DecodeSurface::DecodeSurface(
     return;
   }
 
-  glBindTexture(GL_TEXTURE_2D, texture_id_);
   glXBindTexImageEXT(x_display_, glx_pixmap_, GLX_FRONT_LEFT_EXT, NULL);
 
   available_ = true;
@@ -294,7 +294,7 @@ VaapiH264Decoder::DecodeSurface::~DecodeSurface() {
   // Unbind surface from texture and deallocate resources.
   if (glx_pixmap_ && make_context_current_.Run()) {
     glXReleaseTexImageEXT(x_display_, glx_pixmap_, GLX_FRONT_LEFT_EXT);
-    glXDestroyGLXPixmap(x_display_, glx_pixmap_);
+    glXDestroyPixmap(x_display_, glx_pixmap_);
   }
 
   if (x_pixmap_)
