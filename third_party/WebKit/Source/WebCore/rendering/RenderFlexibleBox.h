f1f7c9ff64fcb01ebe824cac0104d6e708f64d34
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-27 07:27:44 +0000

    changing -webkit-order should change the paint order of flex items https://bugs.webkit.org/show_bug.cgi?id=92041
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Override paintChildren and use the flex order iterator to determine the order to paint the children.
    
    Test: css3/flexbox/order-painting.html
    
    * rendering/RenderFlexibleBox.cpp:
    (WebCore::RenderFlexibleBox::layoutBlock): Save a reference to the order iterator.
    (WebCore::RenderFlexibleBox::paintChildren):
    * rendering/RenderFlexibleBox.h:
    (RenderFlexibleBox): Hold a reference to the order iterator so we don't have to recreate it at paint time.
        Also mark all the virtual methods with OVERRIDE.
    
    LayoutTests:
    
    Use a ref test since this is testing paint behavior.
    
    * css3/flexbox/order-painting-expected.html: Added.
    * css3/flexbox/order-painting.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123842 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlexibleBox.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define RenderFlexibleBox_h
 
 #include "RenderBlock.h"
+#include <wtf/OwnPtr.h>
 
 namespace WebCore {
 
@@ -41,11 +42,13 @@ public:
     RenderFlexibleBox(Node*);
     virtual ~RenderFlexibleBox();
 
-    virtual const char* renderName() const;
+    virtual const char* renderName() const OVERRIDE;
 
-    virtual bool isFlexibleBox() const { return true; }
-    virtual void computePreferredLogicalWidths();
-    virtual void layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0);
+    virtual bool isFlexibleBox() const OVERRIDE { return true; }
+    virtual void computePreferredLogicalWidths() OVERRIDE;
+    virtual void layoutBlock(bool relayoutChildren, LayoutUnit pageLogicalHeight = 0) OVERRIDE;
+
+    virtual void paintChildren(PaintInfo& forSelf, const LayoutPoint&, PaintInfo& forChild, bool usePrintRect) OVERRIDE;
 
     bool isHorizontalFlow() const;
 
@@ -133,6 +136,8 @@ private:
     void applyStretchAlignmentToChild(RenderBox*, LayoutUnit lineCrossAxisExtent);
     void flipForRightToLeftColumn(OrderIterator&);
     void flipForWrapReverse(OrderIterator&, const WTF::Vector<LineContext>&, LayoutUnit crossAxisStartEdge);
+
+    OwnPtr<OrderIterator> m_orderIterator;
 };
 
 } // namespace WebCore
