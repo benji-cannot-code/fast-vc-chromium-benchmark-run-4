5b7ad36c50669f856e194df868e3183ec8b287cf
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 21:16:58 +0000

    Chrome-side implementation of media source timestamp offset
    Adds functionality to signal an offset to be applied to the
    buffers in ChunkDemuxer. Is not triggerable from Chrome yet.
    
    BUG=139044
    TEST=media_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10803019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148810 0039d316-1c4b-4281-b951-d872f2087c98

--- media/mp4/mp4_stream_parser.h
+++ media/mp4/mp4_stream_parser.h
@@ -32,7 +32,8 @@ class MEDIA_EXPORT MP4StreamParser : public StreamParser {
                     const NewBuffersCB& audio_cb,
                     const NewBuffersCB& video_cb,
                     const NeedKeyCB& need_key_cb,
-                    const NewMediaSegmentCB& new_segment_cb) OVERRIDE;
+                    const NewMediaSegmentCB& new_segment_cb,
+                    const base::Closure& end_of_segment_cb) OVERRIDE;
   virtual void Flush() OVERRIDE;
   virtual bool Parse(const uint8* buf, int size) OVERRIDE;
 
@@ -71,6 +72,7 @@ class MEDIA_EXPORT MP4StreamParser : public StreamParser {
   NewBuffersCB video_cb_;
   NeedKeyCB need_key_cb_;
   NewMediaSegmentCB new_segment_cb_;
+  base::Closure end_of_segment_cb_;
 
   OffsetByteQueue queue_;
 
