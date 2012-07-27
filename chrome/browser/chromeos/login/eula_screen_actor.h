f8866e414f5ab848cba9daf0cb48396e2b8d7b63
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 05:26:10 +0000

    Remove unused chromeos::EulaScreenActor::IsTpmEnabled
    BUG=126674
    TEST=build success
    
    Review URL: https://chromiumcodereview.appspot.com/10829032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/eula_screen_actor.h
+++ chrome/browser/chromeos/login/eula_screen_actor.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -21,9 +21,6 @@ class EulaScreenActor {
   class Delegate {
    public:
     virtual ~Delegate() {}
-
-    // Returns true if TPM is enabled.
-    virtual bool IsTpmEnabled() const = 0;
 
     // Returns URL of the OEM EULA page that should be displayed using current
     // locale and manifest. Returns empty URL otherwise.
