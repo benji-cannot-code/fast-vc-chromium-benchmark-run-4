e53b16648ca78ae5efacafb336e1135e3406e7c5
mathp@google.com <mathp@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 16:56:14 +0000

    Address bug where the one-click sign-in bar would never show again once a user (with associated profile) had clicked "Cancel". New behavior is that we now remember for which email the bar was dismissed, and show it again in case the user signs in with a different email. If the user accepts the one-click sign-in at one point, the bar never shows again for this profile
    BUG=118280
    TEST=Cancel the one-click sign-in bar, log-in with another Google account,
    see that it still shows up.
    
    Review URL: https://chromiumcodereview.appspot.com/10555005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/password_manager_delegate_impl.cc
+++ chrome/browser/password_manager/password_manager_delegate_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/singleton.h"
 #include "base/metrics/histogram.h"
+#include "base/utf_string_conversions.h"
 #include "chrome/browser/autofill/autofill_manager.h"
 #include "chrome/browser/infobars/infobar_tab_helper.h"
 #include "chrome/browser/password_manager/password_form_manager.h"
@@ -140,8 +141,12 @@ void PasswordManagerDelegateImpl::AddSavePasswordInfoBarIfPermitted(
   // For now, one-click signin is fully implemented only on windows.
 #if defined(ENABLE_ONE_CLICK_SIGNIN)
   GURL realm(form_to_save->realm());
-  if (realm == GURL(GaiaUrls::GetInstance()->gaia_login_form_realm()) &&
-      OneClickSigninHelper::CanOffer(tab_contents_->web_contents(), true)) {
+  // TODO(mathp): Checking only against associated_username() causes a bug
+  // referenced here: crbug.com/133275
+  if ((realm == GURL(GaiaUrls::GetInstance()->gaia_login_form_realm()) ||
+      realm == GURL("https://www.google.com/")) &&
+      OneClickSigninHelper::CanOffer(tab_contents_->web_contents(),
+          UTF16ToUTF8(form_to_save->associated_username()), true)) {
     return;
   }
 #endif
