15f46869a3c8dc26f036994b53d75b3e0fb763e2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-27 00:01:08 +0000

    [BlackBerry] Remove synchronous spellchecking code https://bugs.webkit.org/show_bug.cgi?id=92415
    Removing synchronous spellchecking code path.
    
    Patch by Nima Ghanavatian <nghanavatian@rim.com> on 2012-07-26
    Reviewed by Rob Buis.
    
    Internally reviewed by Mike Fenton.
    
    * Api/WebPageClient.h:
    * WebCoreSupport/EditorClientBlackBerry.cpp:
    (WebCore::EditorClientBlackBerry::checkSpellingOfString):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123815 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/EditorClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/EditorClientBlackBerry.cpp
@@ -557,7 +557,7 @@ void EditorClientBlackBerry::learnWord(const WTF::String&)
 
 void EditorClientBlackBerry::checkSpellingOfString(const UChar* text, int textLength, int* misspellLocation, int* misspellLength)
 {
-    m_webPagePrivate->m_client->checkSpellingOfString(text, textLength, *misspellLocation, *misspellLength);
+    notImplemented();
 }
 
 WTF::String EditorClientBlackBerry::getAutoCorrectSuggestionForMisspelledWord(const WTF::String& misspelledWord)
