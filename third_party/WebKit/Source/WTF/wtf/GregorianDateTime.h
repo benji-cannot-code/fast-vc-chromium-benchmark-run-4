2b7fd8fc11931865aa189f64e3f17d7f14e5fdcf
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-27 22:20:59 +0000

    [Qt] Fix build after r123917 https://bugs.webkit.org/show_bug.cgi?id=92555
    Unreviewed.
    
    * wtf/GregorianDateTime.h: Needs string.h for memset().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123925 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/GregorianDateTime.h
+++ third_party/WebKit/Source/WTF/wtf/GregorianDateTime.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_GregorianDateTime_h
 #define WTF_GregorianDateTime_h
 
+#include <string.h>
 #include <time.h>
 #include <wtf/Noncopyable.h>
 
