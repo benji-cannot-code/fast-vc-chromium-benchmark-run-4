5116247db9abd279840881ab240be0d8cec1b6d7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-27 19:02:47 +0000

    Web Inspector: Allow front_end to be loaded into an iframe https://bugs.webkit.org/show_bug.cgi?id=92437
    Patch by John J. Barton <johnjbarton@johnjbarton.com> on 2012-07-27
    Reviewed by Pavel Feldman.
    
    This change only affects 'embedders' of the inspector/front_end. No change of function for WebKit ports.
    
    * inspector/front-end/ExtensionAPI.js:
    Replace window.top with window.parent two places. When WebInspector is loaded as the main window content,
    then extension iframes have window.top === window.parent; when WebInspector is loaded as an iframe, then
    extensions are iframes in iframes and we need to use a relative address window.parent.
    (injectedExtensionAPI.ExtensionViewImpl.dispatchShowEvent):
    (injectedExtensionAPI.ExtensionViewImpl):
    (injectedExtensionAPI.ExtensionServerClient):
    WebKit/chromium/scripts/generate_devtools_extension_api.py:
    The current number of frames is used in a dynamically created identifier. Again we replace 'top' by
    window.parent.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/scripts/generate_devtools_extension_api.py
+++ third_party/WebKit/Source/WebKit/chromium/scripts/generate_devtools_extension_api.py
@@ -42,7 +42,7 @@ def write_devtools_extension_api(output, input_names):
     output.write("""
         var tabId;
         var extensionInfo = {};
-        platformExtensionAPI(injectedExtensionAPI("remote-" + top.frames.length));
+        platformExtensionAPI(injectedExtensionAPI("remote-" + window.parent.frames.length));
     })();""")
 
 
