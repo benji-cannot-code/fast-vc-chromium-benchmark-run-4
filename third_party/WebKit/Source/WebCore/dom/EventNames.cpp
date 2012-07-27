8cc44138e5d5a3e1650d404b9a4b763da466683c
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-27 06:35:38 +0000

    Unreviewed, rolling out r123820. http://trac.webkit.org/changeset/123820 https://bugs.webkit.org/show_bug.cgi?id=92465
    It made all tests crash on all port (Requested by Ossy_home on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-07-26
    
    * dom/EventNames.cpp:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/EventNames.cpp
+++ third_party/WebKit/Source/WebCore/dom/EventNames.cpp
@@ -25,10 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 #define INITIALIZE_EVENT_NAME(name) \
-    , name##Event(#name, AtomicString::ConstructFromLiteral)
+    , name##Event(#name)
 
 #define INITIALIZE_EVENT_INTERFACE(name) \
-    , interfaceFor##name(#name, AtomicString::ConstructFromLiteral)
+    , interfaceFor##name(#name)
 
 EventNames::EventNames()
     : dummy(0)
