3d77fd5baf4d295da05bbade0735f129f4091cb8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-27 07:56:34 +0000

    [EFL] Change prototype of run_open_panel https://bugs.webkit.org/show_bug.cgi?id=91956
    Patch by Kihong Kwon <kihong.kwon@samsung.com> on 2012-07-27
    Reviewed by Kentaro Hara.
    
    In order to support all of the file chooser attributes,
    change the types of parameters in run_open_panel method.
    i.e., change "Eina_Bool allows_multiple_files, Eina_List *accept_types"
    to "Ewk_File_Chooser *file_chooser"
    In addition, Efl can support the capture attribute for HTML media capture.
    
    * WebCoreSupport/ChromeClientEfl.cpp:
    (WebCore::ChromeClientEfl::runOpenPanel):
    * ewk/ewk_view.cpp:
    (ewk_view_run_open_panel):
    * ewk/ewk_view.h:
    * ewk/ewk_view_private.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.h
@@ -114,6 +114,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ewk_view_h
 #define ewk_view_h
 
+#include "ewk_file_chooser.h"
 #include "ewk_frame.h"
 #include "ewk_history.h"
 #include "ewk_js.h"
@@ -188,7 +189,7 @@ struct _Ewk_View_Smart_Class {
     int64_t (*exceeded_application_cache_quota)(Ewk_View_Smart_Data *sd, Ewk_Security_Origin* origin, int64_t defaultOriginQuota, int64_t totalSpaceNeeded);
     uint64_t (*exceeded_database_quota)(Ewk_View_Smart_Data *sd, Evas_Object *frame, const char *databaseName, uint64_t current_size, uint64_t expected_size);
 
-    Eina_Bool (*run_open_panel)(Ewk_View_Smart_Data *sd, Evas_Object *frame, Eina_Bool allows_multiple_files, Eina_List *accept_types, Eina_List **selected_filenames);
+    Eina_Bool (*run_open_panel)(Ewk_View_Smart_Data *sd, Evas_Object *frame, Ewk_File_Chooser *file_chooser, Eina_List **selected_filenames);
 
     Eina_Bool (*navigation_policy_decision)(Ewk_View_Smart_Data *sd, Ewk_Frame_Resource_Request *request, Ewk_Navigation_Type navigation_type);
     Eina_Bool (*focus_can_cycle)(Ewk_View_Smart_Data *sd, Ewk_Focus_Direction direction);
@@ -198,7 +199,7 @@ struct _Ewk_View_Smart_Class {
  * The version you have to put into the version field
  * in the @a Ewk_View_Smart_Class structure.
  */
-#define EWK_VIEW_SMART_CLASS_VERSION 5UL
+#define EWK_VIEW_SMART_CLASS_VERSION 6UL
 
 /**
  * Initializes a whole @a Ewk_View_Smart_Class structure.
