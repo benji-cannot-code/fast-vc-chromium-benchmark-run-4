bead1da51a8664430f78b84333d0c8aeab6730ba
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 07:59:34 +0000

    Describing the `sandbox` workflow for extension developers.
    BUG=117154
    TEST=Just docs, no tests.
    
    Review URL: https://chromiumcodereview.appspot.com/10810054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148728 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/examples/howto/sandbox/eventpage.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.browserAction.onClicked.addListener(function() {
+  var iframe = document.getElementById('theFrame');
+  var message = {
+    command: 'render',
+    context: {thing: 'world'}
+  };
+  iframe.contentWindow.postMessage(message, '*');
+});
+
+
+window.addEventListener('message', function(event) {
+  if (event.data.html) {
+    var notification = webkitNotifications.createNotification(
+      'icon.png',
+      'Templated!',
+      'HTML Received for "' + event.data.name + '": `' + event.data.html + '`'
+    );
+    notification.show();
+  }
+});
