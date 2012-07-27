218a297ce8d8245b4710a4903fece3aff935847e
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 00:36:32 +0000

    Move PROFILE_DESTROYED notification to ProfileDestroyer and observe it in ExtensionProcessManager.
    1. EPM was not observing the correct notification to shut down its ExtensionHosts.
    2. PROFILE_DESTROYED has to be sent early enough that the EPM can shut down ExtensionHosts before the ProfileDestroyer checks that all renderers are gone.
    
    BUG=138843
    TEST=In Debug build, have a split mode incognito extension installed. Open and close an incognito window; no crash. Close the regular profile window; no crash.
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10824020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148674 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -11,7 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/sync_prefs.h"
+#include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/pref_names.h"
+#include "content/public/browser/notification_service.h"
+#include "content/public/browser/notification_source.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
 
@@ -22,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 Profile::Profile()
     : restored_last_session_(false),
+      sent_destroyed_notification_(false),
       accessibility_pause_level_(0) {
 }
 
@@ -117,4 +121,14 @@ bool Profile::IsGuestSession() {
 bool Profile::IsSyncAccessible() {
   browser_sync::SyncPrefs prefs(GetPrefs());
   return ProfileSyncService::IsSyncEnabled() && !prefs.IsManaged();
+}
+
+void Profile::MaybeSendDestroyedNotification() {
+  if (!sent_destroyed_notification_) {
+    sent_destroyed_notification_ = true;
+    content::NotificationService::current()->Notify(
+        chrome::NOTIFICATION_PROFILE_DESTROYED,
+        content::Source<Profile>(this),
+        content::NotificationService::NoDetails());
+  }
 }
