d9d58dd653ff45b62ed7ebb65116493b0105e621
yongsheng.zhu@intel.com <yongsheng.zhu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 04:08:57 +0000

    Add support for android in gpu test expections parser
    So the parser can know 'Android' platform. Also the failed case
    'GPUTestExpectationsParserTest.WebGLTestExpectationsValidation'
    is passed.
    
    BUG=118542
    TEST=content_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10833003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148719 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/gpu/gpu_test_expectations_parser.cc
+++ content/test/gpu/gpu_test_expectations_parser.cc
@@ -38,6 +38,7 @@ enum Token {
   kConfigMac,
   kConfigLinux,
   kConfigChromeOS,
+  kConfigAndroid,
   // gpu vendor
   kConfigNVidia,
   kConfigAMD,
@@ -80,6 +81,7 @@ const TokenInfo kTokenData[] = {
   { "mac", GPUTestConfig::kOsMac },
   { "linux", GPUTestConfig::kOsLinux },
   { "chromeos", GPUTestConfig::kOsChromeOS },
+  { "android", GPUTestConfig::kOsAndroid },
   { "nvidia", 0x10DE },
   { "amd", 0x1002 },
   { "intel", 0x8086 },
@@ -235,6 +237,7 @@ bool GPUTestExpectationsParser::ParseConfig(
       case kConfigMac:
       case kConfigLinux:
       case kConfigChromeOS:
+      case kConfigAndroid:
       case kConfigNVidia:
       case kConfigAMD:
       case kConfigIntel:
@@ -282,6 +285,7 @@ bool GPUTestExpectationsParser::ParseLine(
       case kConfigMac:
       case kConfigLinux:
       case kConfigChromeOS:
+      case kConfigAndroid:
       case kConfigNVidia:
       case kConfigAMD:
       case kConfigIntel:
@@ -392,6 +396,7 @@ bool GPUTestExpectationsParser::UpdateTestConfig(
     case kConfigMac:
     case kConfigLinux:
     case kConfigChromeOS:
+    case kConfigAndroid:
       if ((config->os() & kTokenData[token].flag) != 0) {
         PushErrorMessage(kErrorMessage[kErrorEntryWithOsConflicts],
                          line_number);
