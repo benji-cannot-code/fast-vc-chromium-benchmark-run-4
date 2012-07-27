c92b076b5b4884bdbc3854e53fc47eb5b030bb4b
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 17:52:07 +0000

    Extensions Docs Server: use addEventListener with branch.js
    Clean up branch.js to use addEventListener.
    
    BUG=131095
    
    Review URL: https://chromiumcodereview.appspot.com/10828027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/static/js/branch.js
+++ chrome/common/extensions/docs/server2/static/js/branch.js
@@ -3,18 +3,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-/**
- * Redirects to the same page on a different branch.
- *
- * @param {String} The path to redirect to.
- */
 (function() {
-  function redirectToBranch(value) {
+  function redirectToBranch() {
+    var value = event.target.value;
     if (!value)
       return;
-    var path = window.location.pathname.split('/')
-    window.location = value + '/' + path[path.length - 1];
+    var current_branch = window.bootstrap.branchInfo.current;
+    var path = window.location.pathname.split('/');
+    var index = path.indexOf(current_branch);
+    if (index != -1)
+      path[index] = value;
+    else
+      path.splice(path.length - 1, 0, value);
+    window.location = path.join('/');
   }
 
-  window.redirectToBranch = redirectToBranch;
+  document.getElementById('branchChooser').addEventListener(
+      'change',
+      redirectToBranch);
 })()
