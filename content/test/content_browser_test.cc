7948035bb28fa526641e72e0a3d0fe53208ae6c9
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-27 01:17:20 +0000

    Add the RenderProcessHost::FastShutdownIfPossible calls to ContentBrowserTest that I removed in r148507. It looks like this may be preventing the browser from shutting down child processes. Review URL: https://chromiumcodereview.appspot.com/10833043
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148686 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/content_browser_test.cc
+++ content/test/content_browser_test.cc
@@ -121,6 +121,11 @@ void ContentBrowserTest::RunTestOnMainThreadLoop() {
   pool.Recycle();
 #endif
 
+  for (RenderProcessHost::iterator i(RenderProcessHost::AllHostsIterator());
+       !i.IsAtEnd(); i.Advance()) {
+    i.GetCurrentValue()->FastShutdownIfPossible();
+  }
+
   Shell::CloseAllWindows();
 }
 
