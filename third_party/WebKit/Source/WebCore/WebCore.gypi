4304367a92282ebad10338efd1fc865fbc1eb710
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-28 05:48:46 +0000

    Unreviewed, rolling out r123908. http://trac.webkit.org/changeset/123908 https://bugs.webkit.org/show_bug.cgi?id=92572
    Broke Chromium Linux compile (Requested by pkasting on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-07-27
    
    * WebCore.gypi:
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::CSSPrimitiveValue):
    * css/CSSValueKeywords.in:
    * css/mediaControls.css:
    * css/mediaControlsChromiumAndroid.css:
    (audio::-webkit-media-controls-play-button, video::-webkit-media-controls-play-button):
    * html/shadow/MediaControlElements.cpp:
    (WebCore):
    * html/shadow/MediaControlElements.h:
    (WebCore):
    * html/shadow/MediaControlRootElementChromium.cpp:
    (WebCore::MediaControlChromiumEnclosureElement::create):
    (WebCore):
    (WebCore::MediaControlChromiumEnclosureElement::shadowPseudoId):
    (WebCore::MediaControls::create):
    (WebCore::MediaControlRootElementChromium::create):
    * html/shadow/MediaControlRootElementChromium.h:
    (MediaControlChromiumEnclosureElement):
    (MediaControlRootElementChromium):
    * html/shadow/MediaControlRootElementChromiumAndroid.cpp: Removed.
    * html/shadow/MediaControlRootElementChromiumAndroid.h: Removed.
    * platform/ThemeTypes.h:
    * rendering/RenderMediaControlsChromium.cpp:
    (WebCore::RenderMediaControlsChromium::paintMediaControlsPart):
    * rendering/RenderTheme.cpp:
    (WebCore::RenderTheme::paint):
    * rendering/RenderTheme.h:
    * rendering/RenderThemeChromiumAndroid.cpp:
    * rendering/RenderThemeChromiumAndroid.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCore.gypi
+++ third_party/WebKit/Source/WebCore/WebCore.gypi
@@ -5741,8 +5741,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'html/shadow/MediaControlElements.h',
             'html/shadow/MediaControlRootElementChromium.cpp',
             'html/shadow/MediaControlRootElementChromium.h',
-            'html/shadow/MediaControlRootElementChromiumAndroid.cpp',
-            'html/shadow/MediaControlRootElementChromiumAndroid.h',
             'html/shadow/MeterShadowElement.cpp',
             'html/shadow/MeterShadowElement.h',
             'html/shadow/ProgressShadowElement.cpp',
--- third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElementChromium.h
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControlRootElementChromium.h
@@ -64,19 +64,13 @@ class MediaControlTextTrackDisplayElement;
 #endif
 
 class MediaControlChromiumEnclosureElement : public MediaControlElement {
-protected:
+public:
+    static PassRefPtr<MediaControlChromiumEnclosureElement> create(Document*);
+
+private:
     explicit MediaControlChromiumEnclosureElement(Document*);
 
-private:
     virtual MediaControlElementType displayType() const;
-};
-
-class MediaControlPanelEnclosureElement : public MediaControlChromiumEnclosureElement {
-public:
-    static PassRefPtr<MediaControlPanelEnclosureElement> create(Document*);
-
-private:
-    explicit MediaControlPanelEnclosureElement(Document*);
     virtual const AtomicString& shadowPseudoId() const;
 };
 
@@ -85,7 +79,7 @@ public:
     static PassRefPtr<MediaControlRootElementChromium> create(Document*);
 
     // MediaControls implementation.
-    virtual void setMediaController(MediaControllerInterface*);
+    void setMediaController(MediaControllerInterface*);
 
     void show();
     void hide();
@@ -94,9 +88,9 @@ public:
 
     void reset();
 
-    virtual void playbackProgressed();
-    virtual void playbackStarted();
-    virtual void playbackStopped();
+    void playbackProgressed();
+    void playbackStarted();
+    void playbackStopped();
 
     void changedMute();
     void changedVolume();
@@ -123,13 +117,9 @@ public:
 
     virtual bool shouldHideControls();
 
-protected:
-    explicit MediaControlRootElementChromium(Document*);
-
-    // Returns true if successful, otherwise return false.
-    bool initializeControls(Document*);
-
 private:
+    MediaControlRootElementChromium(Document*);
+
     virtual void defaultEventHandler(Event*);
     void hideFullscreenControlsTimerFired(Timer<MediaControlRootElementChromium>*);
     void startHideFullscreenControlsTimer();
@@ -149,8 +139,7 @@ private:
     MediaControlVolumeSliderElement* m_volumeSlider;
     MediaControlFullscreenButtonElement* m_fullscreenButton;
     MediaControlPanelElement* m_panel;
-    MediaControlPanelEnclosureElement* m_enclosure;
-
+    MediaControlChromiumEnclosureElement* m_enclosure;
 #if ENABLE(VIDEO_TRACK)
     MediaControlTextTrackContainerElement* m_textDisplayContainer;
 #endif
