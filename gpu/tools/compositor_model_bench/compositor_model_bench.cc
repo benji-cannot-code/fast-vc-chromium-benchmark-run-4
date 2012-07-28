ce7c9a849f392000c493ef747656df05f9a7c618
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-28 23:16:25 +0000

    Delete unused member variable.
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148909 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/tools/compositor_model_bench/compositor_model_bench.cc
+++ gpu/tools/compositor_model_bench/compositor_model_bench.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -62,8 +62,7 @@ void _update_loop(Simulator* sim);
 class Simulator {
  public:
   Simulator(int seconds_per_test, const FilePath& output_path)
-     : running_(false),
-       current_sim_(NULL),
+     : current_sim_(NULL),
        output_path_(output_path),
        seconds_per_test_(seconds_per_test),
        ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)),
@@ -342,7 +341,6 @@ class Simulator {
   }
 
   // Simulation task list for this execution
-  bool running_;
   RenderModelSimulator* current_sim_;
   queue<SimulationSpecification> sims_remaining_;
   queue<SimulationSpecification> sims_completed_;
