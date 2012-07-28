1f7687f70d3a4ae2d00203e648ed611405b324a1
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-28 02:13:17 +0000

    login-webui: Close any bubble when window loses focus.
    If there is an error bubble on the login screen, and the user clicks
    on the status-tray, then the bubble should close. Instead of sending
    complicated messages from the UI to the login webui code, it is much
    simpler to just close the bubbles when the login window loses focus.
    
    BUG=127092
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10827015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148866 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/bubble.js
+++ chrome/browser/resources/chromeos/login/bubble.js
@@ -39,6 +39,7 @@ cr.define('cr.ui', function() {
                                           this.handleDocClick_.bind(this));
       this.ownerDocument.addEventListener('keydown',
                                           this.handleDocKeyDown_.bind(this));
+      window.addEventListener('blur', this.handleWindowBlur_.bind(this));
       this.addEventListener('webkitTransitionEnd',
                             this.handleTransitionEnd_.bind(this));
     },
@@ -214,6 +215,15 @@ cr.define('cr.ui', function() {
      * @private
      */
     handleDocKeyDown_: function(e) {
+      if (!this.hidden)
+        this.hide();
+    },
+
+    /**
+     * Handler of window blur event.
+     * @private
+     */
+    handleWindowBlur_: function(e) {
       if (!this.hidden)
         this.hide();
     }
