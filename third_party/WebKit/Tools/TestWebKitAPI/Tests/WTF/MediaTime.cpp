55b9a3f6f6ef4f77fdf767ae85728dffc58cbcbf
pkasting@chromium.org <pkasting@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-28 19:22:46 +0000

    Fix Chromium/Win compile. https://bugs.webkit.org/show_bug.cgi?id=88787
    Unreviewed, build fix.
    
    Chromium/Win needs _USE_MATH_DEFINES but doesn't define PLATFORM(WIN).
    The other files in WebKit that define this macro (both in
    WebCore/platform/graphics/cg) do it by unconditionally #defining it to 1
    above the #include of "config.h", so copy that pattern here.
    
    * TestWebKitAPI/Tests/WTF/MediaTime.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123968 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/Tests/WTF/MediaTime.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/Tests/WTF/MediaTime.cpp
@@ -27,11 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+#define _USE_MATH_DEFINES 1
 #include "config.h"
-
-#if PLATFORM(WIN)
-#define _USE_MATH_DEFINES
-#endif
 
 #include <wtf/MediaTime.h>
 
