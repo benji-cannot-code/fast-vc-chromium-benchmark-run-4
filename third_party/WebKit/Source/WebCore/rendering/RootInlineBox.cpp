7a8b23e9076f34c9a7bef9fca008920c658fea54
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-29 22:55:51 +0000

    In flipped blocks, a point on the top edge of a text box is considered outside the box (and vice versa) https://bugs.webkit.org/show_bug.cgi?id=92597
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore: 
    
    This is similar to <http://webkit.org/b/92593> except for RenderText. Fixing this required
    fixing some code that creates a point inside a box by setting its y coordinate to the
    logical top of the box. In a flipped block, this would be just outside the box.
    
    Enhanced fast/writing-mode/flipped-blocks-hit-test-line-edges.html to cover this.
    
    * editing/visible_units.cpp:
    (WebCore::absoluteLineDirectionPointToLocalPointInBlock): Changed to use
    RootInlineBox::blockDirectionPointInLine in the vertical case as well.
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::positionForPointWithInlineChildren): Changed to use
    RootInlineBox::blockDirectionPointInLine.
    * rendering/RenderText.cpp:
    (WebCore::RenderText::positionForPoint): Changed the strictness of y inequalities based on
    the flippedness of the block. This fixes the bug.
    * rendering/RootInlineBox.cpp:
    (WebCore::RootInlineBox::blockDirectionPointInLine): Moved the implementation here from the
    header, and made it return the bottom in the flipped block case, so that the returned point
    is inside the line in that case as well.
    * rendering/RootInlineBox.h:
    
    LayoutTests: 
    
    Added hits inside the text to this test.
    
    * fast/writing-mode/flipped-blocks-hit-test-line-edges-expected.txt:
    * fast/writing-mode/flipped-blocks-hit-test-line-edges.html:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RootInlineBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RootInlineBox.cpp
@@ -602,6 +602,11 @@ LayoutUnit RootInlineBox::selectionBottom() const
     return nextTop;
 }
 
+int RootInlineBox::blockDirectionPointInLine() const
+{
+    return !block()->style()->isFlippedBlocksWritingMode() ? max(lineTop(), selectionTop()) : min(lineBottom(), selectionBottom());
+}
+
 RenderBlock* RootInlineBox::block() const
 {
     return toRenderBlock(renderer());
