fc8d6df6985de00c8f764192177281fa181929c2
mmocny@chromium.org <mmocny@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-30 23:18:16 +0000

    Removing duplicate IsHidden function from RWHI.
    is_hidden() was already added to RWHI recently and they duplicate exact functionality.
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10833052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149070 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -276,7 +276,7 @@ RenderWidgetHost* RenderWidgetHostViewAura::GetRenderWidgetHost() const {
 }
 
 void RenderWidgetHostViewAura::WasShown() {
-  if (!host_->IsHidden())
+  if (!host_->is_hidden())
     return;
   host_->WasShown();
 
@@ -289,7 +289,7 @@ void RenderWidgetHostViewAura::WasShown() {
 }
 
 void RenderWidgetHostViewAura::WasHidden() {
-  if (host_->IsHidden())
+  if (host_->is_hidden())
     return;
   host_->WasHidden();
 
@@ -616,7 +616,7 @@ void RenderWidgetHostViewAura::AcceleratedSurfaceBuffersSwapped(
   surface_route_id_ = params_in_pixel.route_id;
   // If protection state changed, then this swap is stale. We must still ACK but
   // do not update current_surface_ since it may have been discarded.
-  if (host_->IsHidden() ||
+  if (host_->is_hidden() ||
       (params_in_pixel.protection_state_id &&
           params_in_pixel.protection_state_id != protection_state_id_)) {
     DCHECK(!current_surface_);
@@ -687,7 +687,7 @@ void RenderWidgetHostViewAura::AcceleratedSurfacePostSubBuffer(
   surface_route_id_ = params_in_pixel.route_id;
   // If visible state changed, then this PSB is stale. We must still ACK but
   // do not update current_surface_.
-  if (host_->IsHidden() ||
+  if (host_->is_hidden() ||
       (params_in_pixel.protection_state_id &&
           params_in_pixel.protection_state_id != protection_state_id_)) {
     DCHECK(!current_surface_);
@@ -796,7 +796,7 @@ void RenderWidgetHostViewAura::AcceleratedSurfaceRelease(
 }
 
 void RenderWidgetHostViewAura::SetSurfaceNotInUseByCompositor(ui::Compositor*) {
-  if (current_surface_ || !host_->IsHidden())
+  if (current_surface_ || !host_->is_hidden())
     return;
   current_surface_in_use_by_compositor_ = false;
   AdjustSurfaceProtection();
@@ -808,7 +808,7 @@ void RenderWidgetHostViewAura::AdjustSurfaceProtection() {
   // Otherwise, change to not proctected once done thumbnailing and compositing.
   bool surface_is_protected =
       current_surface_ ||
-      !host_->IsHidden() ||
+      !host_->is_hidden() ||
       (current_surface_is_protected_ &&
           (!pending_thumbnail_tasks_.empty() ||
               current_surface_in_use_by_compositor_));
