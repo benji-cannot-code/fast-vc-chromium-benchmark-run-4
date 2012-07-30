688f0d7ab2e5039217419615325487603bfd7142
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-30 23:47:52 +0000

    Enable Verizon Activation from OOBE/Login screen. With Verizon's activation now coming in a separate modal dialog, there is no reason for us to restrict this to only once the user has logged in. Allows users to activate Verizon from the login screen or OOBE. The user should still not be able to activate from the lock screen though.
    Also make the modal activation dialog the default option.
    
    Review requested,
    zelidrag@ - main review
    sky@ - OWNER's review for,
      chrome/browser/ui/views/ash
      chrome/browser/ui/webui/chromeos/
    sadrul@ - OWNER's review for,
      chrome/browser/chromeos/status
    
    R=zelidrag@chromium.org,sky@chromium.org,sadrul@chromium.org
    BUG=134475
    TEST=The activation option is available at OOBE and the Login screens but not the lock screen. On selecting the activation option, the 3g activation dialog comes up.
    
    Review URL: https://chromiumcodereview.appspot.com/10790133
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149082 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1367,9 +1367,6 @@ const char kEnableGView[]                   = "enable-gview";
 // Enable Kiosk mode for ChromeOS
 const char kEnableKioskMode[]               = "enable-kiosk-mode";
 
-// Enables mobile setup in a dialog.
-const char kEnableMobileSetupDialog[]       = "enable-mobile-dialog";
-
 // Enables support for policy-configured networks.
 const char kEnableONCPolicy[]               = "enable-onc-policy";
 
