6373de3619c438530dab2afea491d873f643239d
steveblock@chromium.org <steveblock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-30 14:23:41 +0000

    Revert "Hook up ContentViewCore.add/removeJavascriptInterface()"
    This reverts commit 4c4c2a2efc00cdea3b62c5ff4baf8563ed19711f.
    
    This commit included inintended changes not present in code review.
    
    BUG=110637
    
    Review URL: https://chromiumcodereview.appspot.com/10827085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148949 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_view_core_impl.h
+++ content/browser/android/content_view_core_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/process.h"
 #include "content/browser/renderer_host/render_widget_host_view_android.h"
-#include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/browser/android/content_view_core.h"
 #include "content/public/browser/notification_observer.h"
 #include "googleurl/src/gurl.h"
@@ -35,8 +34,6 @@ class ContentViewCoreImpl : public ContentViewCore,
   ContentViewCoreImpl(JNIEnv* env,
                       jobject obj,
                       WebContents* web_contents);
-
-  // ContentViewCore overrides
   virtual void Destroy(JNIEnv* env, jobject obj) OVERRIDE;
 
   // --------------------------------------------------------------------------
@@ -112,12 +109,6 @@ class ContentViewCoreImpl : public ContentViewCore,
   jboolean NeedsReload(JNIEnv* env, jobject obj);
   void ClearHistory(JNIEnv* env, jobject obj);
   void SetClient(JNIEnv* env, jobject obj, jobject jclient);
-  void AddJavascriptInterface(JNIEnv* env,
-                              jobject obj,
-                              jobject object,
-                              jstring name,
-                              jboolean allow_inherited_methods);
-  void RemoveJavascriptInterface(JNIEnv* env, jobject obj, jstring name);
 
   // --------------------------------------------------------------------------
   // Public methods that call to Java via JNI
@@ -151,7 +142,7 @@ class ContentViewCoreImpl : public ContentViewCore,
   void OnAcceleratedCompositingStateChange(RenderWidgetHostViewAndroid* rwhva,
                                            bool activated,
                                            bool force);
-  void StartContentIntent(const GURL& content_url);
+  virtual void StartContentIntent(const GURL& content_url) OVERRIDE;
 
   // --------------------------------------------------------------------------
   // Methods called from native code
@@ -196,7 +187,7 @@ class ContentViewCoreImpl : public ContentViewCore,
 
   // Reference to the current WebContents used to determine how and what to
   // display in the ContentViewCore.
-  WebContentsImpl* web_contents_;
+  WebContents* web_contents_;
 
   // We only set this to be the delegate of the web_contents if we own it.
   scoped_ptr<ContentViewClient> content_view_client_;
