b94abb5c0a8ac4e48dd4633a9976188a7bce444f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-30 16:32:57 +0000

    https://bugs.webkit.org/show_bug.cgi?id=92642 [Bindings]Remove custom JS/V8 bindings for WebSocket::close() using [Clamp]
    Patch by Vineet Chaudhary <rgf748@motorola.com> on 2012-07-30
    Reviewed by Kentaro Hara.
    
    With support of [Clamp] as extended attribute we can replace custom bindings from WebSocket::close().
    Spec for WebSocket::close() : http://www.whatwg.org/specs/web-apps/current-work/multipage/network.html#websocket
    
    No new tests. There should be no behavioural changes
    Existing tests under http/websocket/tests/hybi/ should cover this.
    
    * Modules/websockets/WebSocket.h:
    (WebCore::WebSocket::close):
    * Modules/websockets/WebSocket.idl: Used [Clamp].
    * bindings/js/JSWebSocketCustom.cpp: Removed custom call close().
    (WebCore):
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateParametersCheck):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateParametersCheck):
    * bindings/scripts/test/JS/JSTestObj.cpp:
    (WebCore::jsTestObjPrototypeFunctionClassMethodWithClamp):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::classMethodWithClampCallback):
    * bindings/v8/custom/V8WebSocketCustom.cpp: Removed custom call close().
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124034 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/websockets/WebSocket.h
+++ third_party/WebKit/Source/WebCore/Modules/websockets/WebSocket.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventNames.h"
 #include "EventTarget.h"
 #include "KURL.h"
+#include "WebSocketChannel.h"
 #include "WebSocketChannelClient.h"
 #include <wtf/Forward.h>
 #include <wtf/OwnPtr.h>
@@ -74,6 +75,8 @@ public:
     bool send(Blob*, ExceptionCode&);
 
     void close(int code, const String& reason, ExceptionCode&);
+    void close(ExceptionCode& ec) { close(WebSocketChannel::CloseEventCodeNotSpecified, String(), ec); }
+    void close(int code, ExceptionCode& ec) { close(code, String(), ec); }
 
     const KURL& url() const;
     State readyState() const;
