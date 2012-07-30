c052009712a3967ed88bc2e08a5d4df8f35e2472
jar@chromium.org <jar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-30 18:41:53 +0000

    Support profiling of tasks run as sequenced_tasks
    Mimic code seen in message_loop.cc (since these tasks are run on named
    threads) to support tracking of sequenced worker pool runs of tasks.
    We surround the *.Run() method with a call to get the time before we
    start, and then call to tally the time it took to run after task.Run()
    returns.
    
    
    r=brettw
    BUG=139035
    
    Review URL: https://chromiumcodereview.appspot.com/10825022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@148986 0039d316-1c4b-4281-b951-d872f2087c98

--- base/tracking_info.cc
+++ base/tracking_info.cc
@@ -1,13 +1,18 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #include "base/tracking_info.h"
 
+#include <stddef.h>
 #include "base/tracked_objects.h"
 
 namespace base {
+
+TrackingInfo::TrackingInfo()
+    : birth_tally(NULL) {
+}
 
 TrackingInfo::TrackingInfo(
     const tracked_objects::Location& posted_from,
