2c360dd9b8369da2ed0548f249d0c8b0dee91374
jocelyn.turcotte@nokia.com <jocelyn.turcotte@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-30 11:33:41 +0000

    [Qt] Remove accurateVisibleContentsPosition and use a FloatRect for visibleContentsRect instead https://bugs.webkit.org/show_bug.cgi?id=92536
    Reviewed by Noam Rosenthal.
    
    AccurateVisibleContentsPosition carries the same information as visibleContentsRect.location() and
    is needed for fixed layer position updates.
    This patch instead uses a FloatRect for the visible contents rect from the web view down to the
    LayerCoordinatorProxy, and converts it to an IntRect at this moment, before sending it to the
    web process.
    This also moves the check to prevent sending the same rect to the moment of the conversion
    in LayerTreeCoordinatorProxy to fix a bug where the fixed layer deltas wouldn't be updated
    unless the integer part of position components changed.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewLegacyPrivate::updateViewportSize):
    * UIProcess/DrawingAreaProxy.h:
    (WebKit::DrawingAreaProxy::setVisibleContentsRect):
    * UIProcess/DrawingAreaProxyImpl.cpp:
    (WebKit::DrawingAreaProxyImpl::setVisibleContentsRect):
    * UIProcess/DrawingAreaProxyImpl.h:
    (DrawingAreaProxyImpl):
    * UIProcess/LayerTreeCoordinatorProxy.cpp:
    (WebKit::LayerTreeCoordinatorProxy::LayerTreeCoordinatorProxy):
    (WebKit::LayerTreeCoordinatorProxy::setVisibleContentsRect):
    * UIProcess/LayerTreeCoordinatorProxy.h:
    (LayerTreeCoordinatorProxy):
    * UIProcess/WebLayerTreeRenderer.cpp:
    (WebKit::WebLayerTreeRenderer::WebLayerTreeRenderer):
    (WebKit::WebLayerTreeRenderer::setVisibleContentsRect):
    (WebKit::WebLayerTreeRenderer::adjustPositionForFixedLayers):
    * UIProcess/WebLayerTreeRenderer.h:
    (WebLayerTreeRenderer):
    * UIProcess/qt/QtViewportHandler.cpp:
    (WebKit::QtViewportHandler::visibleContentsRect):
    (WebKit::QtViewportHandler::informVisibleContentChange):
    * UIProcess/qt/QtViewportHandler.h:
    (QtViewportHandler):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124013 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebLayerTreeRenderer.h
@@ -67,7 +67,7 @@ public:
     void paintToGraphicsContext(BackingStore::PlatformGraphicsContext);
     void syncRemoteContent();
     void setContentsSize(const WebCore::FloatSize&);
-    void setVisibleContentsRect(const WebCore::IntRect&, float scale, const WebCore::FloatPoint& accurateVisibleContentsPosition);
+    void setVisibleContentsRect(const WebCore::FloatRect&);
     void didChangeScrollPosition(const WebCore::IntPoint& position);
     void syncCanvas(uint32_t id, const WebCore::IntSize& canvasSize, uint32_t graphicsSurfaceToken);
 
@@ -108,9 +108,7 @@ private:
 
     typedef HashMap<WebLayerID, WebCore::GraphicsLayer*> LayerMap;
     WebCore::FloatSize m_contentsSize;
-    WebCore::IntRect m_visibleContentsRect;
-    WebCore::FloatPoint m_accurateVisibleContentsPosition;
-    float m_contentsScale;
+    WebCore::FloatRect m_visibleContentsRect;
 
     // Render queue can be accessed ony from main thread or updatePaintNode call stack!
     Vector<Function<void()> > m_renderQueue;
