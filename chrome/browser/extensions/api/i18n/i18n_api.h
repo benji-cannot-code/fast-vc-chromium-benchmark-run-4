cca77a27ecec4ec3d862ca5d6c3ab1a32a47a235
mitchellwrosen@chromium.org <mitchellwrosen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-30 22:10:18 +0000

    Use JSON schema compiler in i18n API code
    TBR=ben@chromium.org
    
    BUG=121174
    
    Review URL: https://chromiumcodereview.appspot.com/10701026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/i18n/i18n_api.h
+++ chrome/browser/extensions/api/i18n/i18n_api.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_EXTENSIONS_EXTENSION_I18N_API_H__
-#define CHROME_BROWSER_EXTENSIONS_EXTENSION_I18N_API_H__
+#ifndef CHROME_BROWSER_EXTENSIONS_API_I18N_I18N_API_H__
+#define CHROME_BROWSER_EXTENSIONS_API_I18N_I18N_API_H__
 
 #include "chrome/browser/extensions/extension_function.h"
 
@@ -14,4 +14,4 @@ class GetAcceptLanguagesFunction : public SyncExtensionFunction {
   DECLARE_EXTENSION_FUNCTION_NAME("i18n.getAcceptLanguages")
 };
 
-#endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_I18N_API_H__
+#endif  // CHROME_BROWSER_EXTENSIONS_API_I18N_I18N_API_H__
--- chrome/common/extensions/api/api.gyp
+++ chrome/common/extensions/api/api.gyp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'events.json',
           'experimental_record.json',
           'file_browser_handler_internal.json',
+          'i18n.json',
           'font_settings.json',
           'history.json',
           'permissions.json',
