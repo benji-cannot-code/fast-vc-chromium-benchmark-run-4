04039511d491ad93f51330ac227ad9da2e311b0a
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-30 01:46:58 +0000

    [Chromium] HarfBuzzShaper can't handle segmented text run https://bugs.webkit.org/show_bug.cgi?id=92445
    Reviewed by Tony Chang.
    
    Pass the range to be displayed to HarfBuzzShaper. The shaper calculates positions and advances of each glyph
    regardless of the range, but only add glyphs which are in the range.
    
    No new tests. platform/chromium-linux/fast/text/international/draw-complex-text-from-to.html should
    work as expected once we move to use harfbuzz-ng on linux. Note that the current expectation will need to be rebaselined
    because the old hb shaper (ComplexTextController) mishandles the range. |to| should be exclusive. I'll rebaseline
    the expectation later.
    
    * platform/graphics/harfbuzz/FontHarfBuzz.cpp:
    (WebCore::Font::drawComplexText): Call shaper.setDrawRange().
    * platform/graphics/harfbuzz/ng/HarfBuzzShaper.cpp:
    (WebCore::HarfBuzzShaper::HarfBuzzShaper):
    (WebCore::HarfBuzzShaper::setDrawRange): Added.
    (WebCore):
    (WebCore::HarfBuzzShaper::shouldDrawCharacterAt): Added.
    (WebCore::HarfBuzzShaper::shapeHarfBuzzRuns): Added variables that hold pending advances.
    (WebCore::HarfBuzzShaper::setGlyphPositionsForHarfBuzzRun): Add only glyphs which are in the given range to glyphBuffer.
    * platform/graphics/harfbuzz/ng/HarfBuzzShaper.h:
    (HarfBuzzShaper):
    * platform/graphics/mac/FontComplexTextMac.cpp:
    (WebCore::Font::drawComplexText): Call shaper.setDrawRange().
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@123991 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzShaper.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzShaper.h
@@ -52,6 +52,7 @@ public:
     HarfBuzzShaper(const Font*, const TextRun&);
     virtual ~HarfBuzzShaper();
 
+    void setDrawRange(int from, int to);
     bool shape(GlyphBuffer* = 0);
     FloatPoint adjustStartPoint(const FloatPoint&);
     float totalWidth() { return m_totalWidth; }
@@ -97,11 +98,12 @@ private:
         float m_width;
     };
 
+    bool shouldDrawCharacterAt(int index);
     void setFontFeatures();
 
     bool collectHarfBuzzRuns();
     bool shapeHarfBuzzRuns(GlyphBuffer*);
-    void setGlyphPositionsForHarfBuzzRun(HarfBuzzRun*, unsigned runIndexInVisualOrder, hb_buffer_t*, GlyphBuffer*);
+    void setGlyphPositionsForHarfBuzzRun(HarfBuzzRun*, unsigned runIndexInVisualOrder, hb_buffer_t*, GlyphBuffer*, float& pendingGlyphAdvanceX, float& pendingGlyphAdvanceY);
 
     GlyphBufferAdvance createGlyphBufferAdvance(float, float);
 
@@ -109,6 +111,9 @@ private:
     Vector<OwnPtr<HarfBuzzRun>, 16> m_harfbuzzRuns;
 
     FloatPoint m_startOffset;
+
+    int m_fromIndex;
+    int m_toIndex;
 
     float m_totalWidth;
 };
