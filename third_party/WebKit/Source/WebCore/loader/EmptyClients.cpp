f7af44223dacc31c50b168b4dc0817887dab05fe
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-31 03:15:20 +0000

    fillWithEmptyClients method should also initialize chromeClient with EmptyChromeClient https://bugs.webkit.org/show_bug.cgi?id=92703
    Patch by Vivek Galatage <vivekgalatage@gmail.com> on 2012-07-30
    Reviewed by Kent Tamura.
    
    fillWithEmptyClients should also intialize PageClients.chromeClient with EmptyChromeClient
    as its required for creating a Empty page and to minimize the exported symbols.
    
    No new tests as no change in functionality.
    
    * loader/EmptyClients.cpp:
    (WebCore::fillWithEmptyClients):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/EmptyClients.cpp
+++ third_party/WebKit/Source/WebCore/loader/EmptyClients.cpp
@@ -48,6 +48,9 @@ namespace WebCore {
 
 void fillWithEmptyClients(Page::PageClients& pageClients)
 {
+    static ChromeClient* dummyChromeClient = adoptPtr(new EmptyChromeClient).leakPtr();
+    pageClients.chromeClient = dummyChromeClient;
+
 #if ENABLE(CONTEXT_MENUS)
     static ContextMenuClient* dummyContextMenuClient = adoptPtr(new EmptyContextMenuClient).leakPtr();
     pageClients.contextMenuClient = dummyContextMenuClient;
