bc6069b58fd67be29d339da69dcd37e707c26710
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-31 22:07:20 +0000

    Add stub implementation for MediaStreamAudioSourceNode https://bugs.webkit.org/show_bug.cgi?id=90110
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    Test: webaudio/mediastreamaudiosourcenode.html
    
    * CMakeLists.txt:
    * DerivedSources.make:
    * GNUmakefile.list.am:
    * WebCore.gypi:
    * WebCore.xcodeproj/project.pbxproj:
    Add MediaStreamAudioSourceNode files to make files.
    
    * Modules/webaudio/AudioContext.cpp:
    * Modules/webaudio/AudioContext.h:
    * Modules/webaudio/AudioContext.idl:
    (WebCore::AudioContext::createMediaStreamSource):
    Add new createMediaStreamSource() method.
    
    * Modules/webaudio/AudioNode.h:
    Add NodeTypeMediaStreamAudioSource.
    
    * Modules/webaudio/MediaStreamAudioSourceNode.cpp: Added.
    * Modules/webaudio/MediaStreamAudioSourceNode.h: Added.
    (WebCore::MediaStreamAudioSourceNode::create):
    (WebCore::MediaStreamAudioSourceNode::MediaStreamAudioSourceNode):
    (WebCore::MediaStreamAudioSourceNode::~MediaStreamAudioSourceNode):
    (WebCore::MediaStreamAudioSourceNode::setFormat):
    (WebCore::MediaStreamAudioSourceNode::process):
    (WebCore::MediaStreamAudioSourceNode::reset):
    (WebCore::MediaStreamAudioSourceNode::mediaStream):
    (WebCore::MediaStreamAudioSourceNode::audioSourceProvider):
    Basic MediaStreamAudioSourceNode implementation.
    
    * Modules/webaudio/MediaStreamAudioSourceNode.idl: Added.
    
    LayoutTests:
    
    * webaudio/mediastreamaudiosourcenode-expected.txt: Added.
    * webaudio/mediastreamaudiosourcenode.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioContext.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioContext.cpp
@@ -60,6 +60,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WaveShaperNode.h"
 #include "WaveTable.h"
 
+#if ENABLE(MEDIA_STREAM)
+#include "MediaStream.h"
+#include "MediaStreamAudioSourceNode.h"
+#endif
+
 #if ENABLE(VIDEO)
 #include "HTMLMediaElement.h"
 #include "MediaElementAudioSourceNode.h"
@@ -379,6 +384,26 @@ PassRefPtr<MediaElementAudioSourceNode> AudioContext::createMediaElementSource(H
     RefPtr<MediaElementAudioSourceNode> node = MediaElementAudioSourceNode::create(this, mediaElement);
 
     mediaElement->setAudioSourceNode(node.get());
+
+    refNode(node.get()); // context keeps reference until node is disconnected
+    return node;
+}
+#endif
+
+#if ENABLE(MEDIA_STREAM)
+PassRefPtr<MediaStreamAudioSourceNode> AudioContext::createMediaStreamSource(MediaStream* mediaStream, ExceptionCode& ec)
+{
+    ASSERT(mediaStream);
+    if (!mediaStream) {
+        ec = INVALID_STATE_ERR;
+        return 0;
+    }
+
+    ASSERT(isMainThread());
+    lazyInitialize();
+
+    // FIXME: For now we don't give it an AudioSourceProvider, so it will output silence.
+    RefPtr<MediaStreamAudioSourceNode> node = MediaStreamAudioSourceNode::create(this, mediaStream, 0);
 
     refNode(node.get()); // context keeps reference until node is disconnected
     return node;
--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioNode.h
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioNode.h
@@ -64,6 +64,7 @@ public:
         NodeTypeOscillator,
         NodeTypeAudioBufferSource,
         NodeTypeMediaElementAudioSource,
+        NodeTypeMediaStreamAudioSource,
         NodeTypeJavaScript,
         NodeTypeBiquadFilter,
         NodeTypePanner,
