4a1e69ede63d5f90a57e89d785fdd3ed7e270d5e
tedvessenes@gmail.com <tedvessenes@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 23:22:48 +0000

    Fix CleanupProcesses() interface to use TimeDelta.
    BUG=108171
    
    
    Review URL: https://chromiumcodereview.appspot.com/10821062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/mini_installer_test/installer_test_util.cc
+++ chrome/test/mini_installer_test/installer_test_util.cc
@@ -180,7 +180,7 @@ bool Install(const FilePath& installer, const SwitchBuilder& switches) {
 }
 
 bool LaunchChrome(bool close_after_launch, bool system_level) {
-  base::CleanupProcesses(installer::kChromeExe, 0,
+  base::CleanupProcesses(installer::kChromeExe, base::TimeDelta(),
                          content::RESULT_CODE_HUNG, NULL);
   FilePath install_path;
   if (!GetChromeInstallDirectory(
@@ -219,9 +219,9 @@ bool LaunchIE(const std::string& url) {
 }
 
 bool UninstallAll() {
-  base::CleanupProcesses(installer::kChromeExe, 0,
+  base::CleanupProcesses(installer::kChromeExe, base::TimeDelta(),
                          content::RESULT_CODE_HUNG, NULL);
-  base::CleanupProcesses(installer::kChromeFrameHelperExe, 0,
+  base::CleanupProcesses(installer::kChromeFrameHelperExe, base::TimeDelta(),
                          content::RESULT_CODE_HUNG, NULL);
   std::vector<installer_test::InstalledProduct> installed;
   if (!GetInstalledProducts(&installed)) {
@@ -274,7 +274,8 @@ bool Uninstall(bool system_level,
   LOG(INFO) << "Uninstall command: " << uninstall_cmd.GetCommandLineString();
   bool ret_val = RunAndWaitForCommandToFinish(uninstall_cmd);
   // Close IE notification when uninstalling Chrome Frame.
-  base::CleanupProcesses(mini_installer_constants::kIEProcessName, 0,
+  base::CleanupProcesses(mini_installer_constants::kIEProcessName,
+                         base::TimeDelta(),
                          content::RESULT_CODE_HUNG, NULL);
   return ret_val;
 }
