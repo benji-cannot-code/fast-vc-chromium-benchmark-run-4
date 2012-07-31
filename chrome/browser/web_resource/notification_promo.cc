250cab2f33ce5c3a550b4af2a668d68667a2d5d0
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 20:21:09 +0000

    Fix regression due to deprecated promo preference.
    148013 introduced a regression where the promo text was not being set, because it referred to a deprecated promo pref.
    
    BUG=123061
    TEST=manual.
    
    Review URL: https://chromiumcodereview.appspot.com/10825093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149264 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/notification_promo.cc
+++ chrome/browser/web_resource/notification_promo.cc
@@ -356,6 +356,7 @@ void NotificationPromo::CheckForNewNotification() {
 }
 
 void NotificationPromo::OnNewNotification() {
+  DVLOG(1) << "OnNewNotification";
   // Create a new promo group.
   group_ = base::RandInt(0, num_groups_ - 1);
   WritePrefs();
