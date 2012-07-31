49db9fa636b797f2ce9b9aee17eddb83414e9d91
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-07-31 16:05:11 +0000

    Unreviewed, rolling out r124208. http://trac.webkit.org/changeset/124208 https://bugs.webkit.org/show_bug.cgi?id=92765
    it broke compilation on mac (Requested by loislo on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-07-31
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * inspector/InspectorAllInOne.cpp:
    * inspector/InspectorMemoryAgent.cpp:
    (WebCore):
    * inspector/MemoryInstrumentationImpl.cpp: Removed.
    * inspector/MemoryInstrumentationImpl.h: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorAllInOne.cpp
@@ -70,7 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorWebGLAgent.cpp"
 #include "InspectorWorkerAgent.cpp"
 #include "InstrumentingAgents.cpp"
-#include "MemoryInstrumentationImpl.cpp"
 #include "NetworkResourcesData.cpp"
 #include "PageConsoleAgent.cpp"
 #include "PageDebuggerAgent.cpp"
