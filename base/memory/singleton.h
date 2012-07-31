196b8ed36a1905c5a7399e954fc63adef0594b81
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 00:53:06 +0000

    base: Suggest forward declare DefaultSingletonTraits in header files.
    Instead of including singleton.h in header files we can forward declare it,
    and include only in the source file.
    
    There is also a presubmit check for this, I didn't know ;)
    
    R=willchan@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10835029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149095 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/singleton.h
+++ base/memory/singleton.h
@@ -150,7 +150,7 @@ template <typename Type> base::subtle::Atomic32
 // Example usage:
 //
 // In your header:
-//   #include "base/memory/singleton.h"
+//   template <typename T> struct DefaultSingletonTraits;
 //   class FooClass {
 //    public:
 //     static FooClass* GetInstance();  <-- See comment below on this.
@@ -163,6 +163,7 @@ template <typename Type> base::subtle::Atomic32
 //   };
 //
 // In your source file:
+//  #include "base/memory/singleton.h"
 //  FooClass* FooClass::GetInstance() {
 //    return Singleton<FooClass>::get();
 //  }
