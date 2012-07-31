43361097eaa76baafb5ac4b750b6b4c07c08bea6
mitchellwrosen@chromium.org <mitchellwrosen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 22:47:50 +0000

    Added JSON schema compiler support for serialized types
    Currently supports string->int, string->int64, and int->string.
    
    BUG=139076
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149302 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/json_schema_compiler/cpp_type_generator_test.py
+++ tools/json_schema_compiler/cpp_type_generator_test.py
@@ -49,6 +49,7 @@ class CppTypeGeneratorTest(unittest.TestCase):
     manager = CppTypeGenerator('', self.windows, self.windows.unix_name)
     manager.AddNamespace(self.tabs, self.tabs.unix_name)
     self.assertEquals('#include "path/to/tabs.h"\n'
+                      '#include "base/string_number_conversions.h"\n'
                       '#include "base/json/json_writer.h"',
                       manager.GenerateIncludes().Render())
     self.assertEquals('namespace tabs {\n'
@@ -59,7 +60,8 @@ class CppTypeGeneratorTest(unittest.TestCase):
                       '}  // windows',
                       manager.GenerateForwardDeclarations().Render())
     manager = CppTypeGenerator('', self.permissions, self.permissions.unix_name)
-    self.assertEquals('#include "base/json/json_writer.h"',
+    self.assertEquals('#include "base/string_number_conversions.h"\n'
+                      '#include "base/json/json_writer.h"',
                       manager.GenerateIncludes().Render())
     self.assertEquals('namespace permissions {\n'
                       'struct Permissions;\n'
@@ -67,7 +69,8 @@ class CppTypeGeneratorTest(unittest.TestCase):
                       manager.GenerateForwardDeclarations().Render())
     manager = CppTypeGenerator('', self.content_settings,
                                self.content_settings.unix_name)
-    self.assertEquals('', manager.GenerateIncludes().Render())
+    self.assertEquals('#include "base/string_number_conversions.h"',
+                      manager.GenerateIncludes().Render())
 
 
   def testGenerateIncludesAndForwardDeclarationsMultipleTypes(self):
@@ -84,6 +87,7 @@ class CppTypeGeneratorTest(unittest.TestCase):
     manager = CppTypeGenerator('', windows, self.windows.unix_name)
     manager.AddNamespace(tabs_namespace, self.tabs.unix_name)
     self.assertEquals('#include "path/to/tabs.h"\n'
+                      '#include "base/string_number_conversions.h"\n'
                       '#include "base/json/json_writer.h"',
                       manager.GenerateIncludes().Render())
     self.assertEquals('namespace tabs {\n'
@@ -110,7 +114,8 @@ class CppTypeGeneratorTest(unittest.TestCase):
     manager.AddNamespace(browser_action_namespace,
                          self.browser_action.unix_name)
     self.assertEquals('#include "path/to/browser_action.h"\n'
-                      '#include "path/to/font_settings.h"',
+                      '#include "path/to/font_settings.h"\n'
+                      '#include "base/string_number_conversions.h"',
                       manager.GenerateIncludes().Render())
     self.assertEquals('namespace browserAction {\n'
                       'typedef std::vector<int> ColorArray;\n'
