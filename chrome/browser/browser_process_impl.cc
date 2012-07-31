702e442f1ab83694cecac77e89112f9544165c70
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 17:48:12 +0000

    Extract ChromeBrowserFieldTrials out of ChromeBrowserMainParts.
    BUG=none
    TEST=compiles
    
    Review URL: https://chromiumcodereview.appspot.com/10827080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_process_impl.cc
+++ chrome/browser/browser_process_impl.cc
@@ -21,7 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "chrome/browser/automation/automation_provider_list.h"
 #include "chrome/browser/background/background_mode_manager.h"
-#include "chrome/browser/browser_trial.h"
 #include "chrome/browser/chrome_browser_main.h"
 #include "chrome/browser/chrome_content_browser_client.h"
 #include "chrome/browser/chrome_plugin_service_filter.h"
