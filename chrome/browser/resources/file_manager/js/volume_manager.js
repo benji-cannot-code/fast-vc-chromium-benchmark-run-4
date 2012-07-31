18b0ba1177961ecfc0f8dae9a38aa161a10ccf77
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 17:06:15 +0000

    Fixed race condition on mounting dgrive while file manager initialization.
    BUG=139680
    TEST=Manual test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/volume_manager.js
+++ chrome/browser/resources/file_manager/js/volume_manager.js
@@ -116,6 +116,7 @@ VolumeManager.prototype.initMountPoints_ = function() {
   var mountedVolumes = [];
   var self = this;
   var index = 0;
+  this.deferredQueue_ = [];
   function step(mountPoints) {
     if (index < mountPoints.length) {
       var info = mountPoints[index];
@@ -137,6 +138,12 @@ VolumeManager.prototype.initMountPoints_ = function() {
       // Subscribe to the mount completed event when mount points initialized.
       chrome.fileBrowserPrivate.onMountCompleted.addListener(
           self.onMountCompleted_.bind(self));
+
+      var deferredQueue = self.deferredQueue_;
+      self.deferredQueue_ = null;
+      for (var i = 0; i < deferredQueue.length; i++) {
+        deferredQueue[i]();
+      }
 
       if (mountedVolumes.length > 0)
         cr.dispatchSimpleEvent(self, 'change');
@@ -303,6 +310,12 @@ VolumeManager.prototype.unmount = function(mountPath,
                                            successCallback,
                                            errorCallback) {
   this.validateMountPath_(mountPath);
+  if (this.deferredQueue_) {
+    this.deferredQueue_.push(this.unmount.bind(this,
+        mountPath, successCallback, errorCallback));
+    return;
+  }
+
   var volumeInfo = this.mountedVolumes_[mountPath];
   if (!volumeInfo) {
     errorCallback(VolumeManager.Error.NOT_MOUNTED);
@@ -371,6 +384,12 @@ VolumeManager.prototype.getVolumeInfo_ = function(mountPath) {
  */
 VolumeManager.prototype.mount_ = function(url, mountType,
                                           successCallback, errorCallback) {
+  if (this.deferredQueue_) {
+    this.deferredQueue_.push(this.mount_.bind(this,
+        url, mountType, successCallback, errorCallback));
+    return;
+  }
+
   chrome.fileBrowserPrivate.addMount(url, mountType, {},
                                      function(sourcePath) {
     console.log('Mount request: url=' + url + '; mountType=' + mountType +
