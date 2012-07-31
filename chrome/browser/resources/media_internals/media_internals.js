057df3844822fe5269869dd1975ab6924763acdf
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-07-31 18:16:41 +0000

    Handle no volume in chrome://media-internals
    When using Pepper Flash chrome://media-internals displays NaN for volume.
    
    Volume may never be set by plugins like Pepper Flash. Fix is to only display volume information if it exists.
    
    (Also fixed presubmit errors.)
    
    BUG=64215
    TEST=Ran on youtube and verified that volume wasn't displayed. Ran on music.google.com and verified that volume was displayed.
    
    Review URL: https://chromiumcodereview.appspot.com/10821058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/media_internals/media_internals.js
+++ chrome/browser/resources/media_internals/media_internals.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -42,8 +42,8 @@ cr.define('media', function() {
    * Initialize variables and ask MediaInternals for all its data.
    */
   function initialize() {
-    audioStreamDiv = document.getElementById('audio-streams');
-    cacheDiv = document.getElementById('cache-entries');
+    audioStreamDiv = $('audio-streams');
+    cacheDiv = $('cache-entries');
 
     // Get information about all currently active media.
     chrome.send('getEverything');
@@ -67,14 +67,16 @@ cr.define('media', function() {
 
       out.textContent += 'Audio stream ' + stream.id.split('.')[1];
       out.textContent += ' is ' + (stream.playing ? 'playing' : 'paused');
-      out.textContent += ' at ' + (stream.volume * 100).toFixed(0);
-      out.textContent += '% volume.';
+      if (typeof stream.volume != 'undefined') {
+        out.textContent += ' at ' + (stream.volume * 100).toFixed(0);
+        out.textContent += '% volume.';
+      }
       return out;
     }
 
     var out = document.createElement('ul');
     audioStreams.map(printStream).forEach(function(s) {
-      out.appendChild(s)
+      out.appendChild(s);
     });
 
     audioStreamDiv.textContent = '';
@@ -148,7 +150,7 @@ cr.define('media', function() {
   function onRendererTerminated(renderer) {
     for (var key in mediaPlayers) {
       if (mediaPlayers[key].renderer == renderer) {
-        document.getElementById('media-players').removeChild(mediaPlayers[key]);
+        $('media-players').removeChild(mediaPlayers[key]);
         delete mediaPlayers[key];
         break;
       }
