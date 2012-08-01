180fe05b8aaadea16f4cfa33ff9f4f168ad0cf9f
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 19:41:58 +0000

    mac: Fix a few compile errors with the 10.8 SDK
    BUG=139138
    TBR=tony
    
    
    Review URL: https://chromiumcodereview.appspot.com/10831108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149465 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/test_webview_delegate_mac.mm
+++ webkit/tools/test_shell/test_webview_delegate_mac.mm
@@ -270,7 +270,7 @@ void TestWebViewDelegate::ShowJavaScriptAlert(const string16& message) {
                                    defaultButton:@"OK"
                                  alternateButton:nil
                                      otherButton:nil
-                       informativeTextWithFormat:text];
+                       informativeTextWithFormat:@"%@", text];
   [alert runModal];
 }
 
