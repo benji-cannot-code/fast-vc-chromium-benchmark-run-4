ba9c734632651b9c8d5280e5294ce99f00cba28f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 03:01:29 +0000

    chrome: Move browser_otr_state.h into chrome namespace.
    BUG=133088
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10831084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149364 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_otr_state_android.cc
+++ chrome/browser/ui/browser_otr_state_android.cc
@@ -7,10 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
 
-namespace browser {
+namespace chrome {
 
 bool IsOffTheRecordSessionActive() {
   return TabModelList::IsOffTheRecordSessionActive();
 }
 
-}
+}  // namespace chrome
