0a557803b9db2a7575f424ef985536b2b812c955
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-01 09:24:34 +0000

    Unreviewed. Fix GTK+ build with recent version of GTK+.
    Use g_threads_enter/leave instead of the wrapper macros. This
    gives a compile warning because they are now deprecated but at
    least it builds.
    
    Source/WebCore:
    
    * plugins/gtk/gtk2xtbin.c:
    (xt_event_prepare):
    (xt_event_check):
    (xt_event_dispatch):
    
    Source/WebKit2:
    
    * UIProcess/API/gtk/WebKitWebView.cpp:
    (webkitWebViewRunAsModal): Fix enter/leave order.
    * UIProcess/gtk/WebPopupMenuProxyGtk.cpp:
    (WebKit::WebPopupMenuProxyGtk::showPopupMenu):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124318 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/gtk/gtk2xtbin.c
+++ third_party/WebKit/Source/WebCore/plugins/gtk/gtk2xtbin.c
@@ -133,9 +133,9 @@ xt_event_prepare (GSource*  source_data,
 {   
   int mask;
 
-  GDK_THREADS_ENTER();
+  gdk_threads_enter();
   mask = XPending(xtdisplay);
-  GDK_THREADS_LEAVE();
+  gdk_threads_leave();
 
   return (gboolean)mask;
 }
@@ -143,16 +143,16 @@ xt_event_prepare (GSource*  source_data,
 static gboolean
 xt_event_check (GSource*  source_data)
 {
-  GDK_THREADS_ENTER ();
+  gdk_threads_enter();
 
   if (xt_event_poll_fd.revents & G_IO_IN) {
     int mask;
     mask = XPending(xtdisplay);
-    GDK_THREADS_LEAVE ();
+    gdk_threads_leave();
     return (gboolean)mask;
   }
 
-  GDK_THREADS_LEAVE ();
+  gdk_threads_leave();
   return FALSE;
 }   
 
@@ -166,7 +166,7 @@ xt_event_dispatch (GSource*  source_data,
 
   ac = XtDisplayToApplicationContext(xtdisplay);
 
-  GDK_THREADS_ENTER ();
+  gdk_threads_enter();
 
   /* Process only real X traffic here.  We only look for data on the
    * pipe, limit it to XTBIN_MAX_EVENTS and only call
@@ -177,7 +177,7 @@ xt_event_dispatch (GSource*  source_data,
     XtAppProcessEvent(ac, XtIMXEvent);
   }
 
-  GDK_THREADS_LEAVE ();
+  gdk_threads_leave();
 
   return TRUE;  
 }
