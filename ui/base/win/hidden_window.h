1c59f59d20875c51bd13e3fade0b1ede2220b3cc
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 19:24:02 +0000

    Makes NativeViewHostWin change the parent of the hwnd when detaching. Without this the hwnd would remain parented so that if the widget containing the NativeViewHost was destroyed then the hwnd would get destroyed by virtue of being a child.
    Previously we had this logic in NativeTabContentsViewWin (lost in
    WebView refactoring), but it seems better to promote to
    NativeViewHostWin.
    
    BUG=132090
    TEST=none
    R=ben@chromium.org,jam@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10832011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149459 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/base/win/hidden_window.h
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_BASE_WIN_HIDDEN_WINDOW_H_
+#define UI_BASE_WIN_HIDDEN_WINDOW_H_
+
+#include <windows.h>
+
+#include "ui/base/ui_export.h"
+
+namespace ui {
+
+// Returns an HWND that can be used as a temporary parent. The returned HWND is
+// never destroyed.
+UI_EXPORT HWND GetHiddenWindow();
+
+}  // namespace ui
+
+#endif  // UI_BASE_WIN_HIDDEN_WINDOW_H_
