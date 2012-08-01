f0dec3ae1030758880e16e2be017db0441bbcb92
dschuff@chromium.org <dschuff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 19:36:02 +0000

    Simplify Pnacl translation thread code Primarily a merge of the streaming-related code into PnaclTranslationThread
    R=jvoung@chromium.org,sehr@chromium.org
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2195
    TEST=nacl_integration
    
    
    Review URL: https://chromiumcodereview.appspot.com/10827109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149464 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/trusted/plugin/pnacl_coordinator.cc
+++ ppapi/native_client/src/trusted/plugin/pnacl_coordinator.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "native_client/src/trusted/plugin/manifest.h"
 #include "native_client/src/trusted/plugin/plugin.h"
 #include "native_client/src/trusted/plugin/plugin_error.h"
-#include "native_client/src/trusted/plugin/pnacl_streaming_translate_thread.h"
 #include "native_client/src/trusted/plugin/pnacl_translate_thread.h"
 #include "native_client/src/trusted/plugin/service_runtime.h"
 #include "native_client/src/trusted/plugin/temporary_file.h"
@@ -441,7 +440,7 @@ void PnaclCoordinator::CachedFileDidOpen(int32_t pp_error) {
   // Create the translation thread object immediately. This ensures that any
   // pieces of the file that get downloaded before the compilation thread
   // is accepting SRPCs won't get dropped.
-  translate_thread_.reset(new PnaclStreamingTranslateThread());
+  translate_thread_.reset(new PnaclTranslateThread());
   if (translate_thread_ == NULL) {
     ReportNonPpapiError("could not allocate translation thread.");
     return;
@@ -485,10 +484,8 @@ void PnaclCoordinator::BitcodeStreamGotData(int32_t pp_error,
                                             FileStreamData data) {
   PLUGIN_PRINTF(("PnaclCoordinator::BitcodeStreamGotData (pp_error=%"
                  NACL_PRId32", data=%p)\n", pp_error, data ? &(*data)[0] : 0));
-  PnaclStreamingTranslateThread* thread =
-    static_cast<PnaclStreamingTranslateThread*>(translate_thread_.get());
-  DCHECK(thread);
-  thread->PutBytes(data, pp_error);
+  DCHECK(translate_thread_.get());
+  translate_thread_->PutBytes(data, pp_error);
 }
 
 StreamCallback PnaclCoordinator::GetCallback() {
