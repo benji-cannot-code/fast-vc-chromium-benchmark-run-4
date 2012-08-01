17c23142725b6ee02a04b7fe3a7ae17705bdcc45
piman@chromium.org <piman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-01 22:27:51 +0000

    [chromium] remove unused fields from LayerRendererCapabilities https://bugs.webkit.org/show_bug.cgi?id=92906
    Reviewed by James Robinson.
    
    No new tests (just removing dead code).
    
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::LayerRendererChromium::initialize):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    (WebCore::LayerRendererCapabilities::LayerRendererCapabilities):
    (LayerRendererCapabilities):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHost.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCLayerTreeHost.h
@@ -126,12 +126,9 @@ struct LayerRendererCapabilities {
         : bestTextureFormat(0)
         , contextHasCachedFrontBuffer(false)
         , usingPartialSwap(false)
-        , usingMapSub(false)
         , usingAcceleratedPainting(false)
         , usingSetVisibility(false)
         , usingSwapCompleteCallback(false)
-        , usingTextureUsageHint(false)
-        , usingTextureStorageExtension(false)
         , usingGpuMemoryManager(false)
         , usingDiscardFramebuffer(false)
         , usingEglImage(false)
@@ -140,12 +137,9 @@ struct LayerRendererCapabilities {
     GC3Denum bestTextureFormat;
     bool contextHasCachedFrontBuffer;
     bool usingPartialSwap;
-    bool usingMapSub;
     bool usingAcceleratedPainting;
     bool usingSetVisibility;
     bool usingSwapCompleteCallback;
-    bool usingTextureUsageHint;
-    bool usingTextureStorageExtension;
     bool usingGpuMemoryManager;
     bool usingDiscardFramebuffer;
     bool usingEglImage;
