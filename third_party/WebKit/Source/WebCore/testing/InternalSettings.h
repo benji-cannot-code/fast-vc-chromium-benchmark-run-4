936df44b2dd4153bd3783e93c09e551db75df920
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-01 22:12:23 +0000

    [Text Autosizing] Provide an API for influencing the font scale factor https://bugs.webkit.org/show_bug.cgi?id=92882
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Add the font scale factor to settings, and provide an API in
    window.internal.settings to change it from layout tests.
    
    The font scale factor applied to Text Autosizing influences the sizing
    of text, and will influence the scaling of boosted blocks once the
    implementation progresses. For Android, it will be set to the font size
    chosen in the user's system-wide preferences.
    
    Test: fast/text-autosizing/font-scale-factor.html
    
    * page/Settings.cpp:
    (WebCore::Settings::Settings):
    (WebCore):
    (WebCore::Settings::setTextAutosizingFontScaleFactor):
    * page/Settings.h:
    (Settings):
    (WebCore::Settings::textAutosizingFontScaleFactor):
    * rendering/TextAutosizer.cpp:
    (WebCore::TextAutosizer::processBlock):
    * testing/InternalSettings.cpp:
    (WebCore::InternalSettings::Backup::Backup):
    (WebCore::InternalSettings::Backup::restoreTo):
    (WebCore::InternalSettings::setTextAutosizingFontScaleFactor):
    (WebCore):
    * testing/InternalSettings.h:
    (Backup):
    (InternalSettings):
    * testing/InternalSettings.idl:
    
    Source/WebKit/chromium:
    
    Add an API to change Text Autosizing's font scale factor.
    
    The font scale factor applied to Text Autosizing influences the sizing
    of text, and will influence the scaling of boosted blocks once the
    implementation progresses. For Android, it will be set to the font size
    chosen in the user's system-wide preferences.
    
    * public/WebSettings.h:
    * src/WebSettingsImpl.cpp:
    (WebKit::WebSettingsImpl::setTextAutosizingFontScaleFactor):
    (WebKit):
    * src/WebSettingsImpl.h:
    (WebSettingsImpl):
    
    LayoutTests:
    
    Add a basic test. More usable tests should be added later, as it's
    really hard to test this accurately in this phase of the implementation,
    and Text Autosizing is still subject to various bugs.
    
    The font scale factor applied to Text Autosizing influences the sizing
    of text, and will influence the scaling of boosted blocks once the
    implementation progresses. For Android, it will be set to the font size
    chosen in the user's system-wide preferences.
    
    * fast/text-autosizing/font-scale-factor-expected.html: Added.
    * fast/text-autosizing/font-scale-factor.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/InternalSettings.h
+++ third_party/WebKit/Source/WebCore/testing/InternalSettings.h
@@ -74,6 +74,7 @@ public:
 #if ENABLE(TEXT_AUTOSIZING)
         bool m_originalTextAutosizingEnabled;
         IntSize m_originalTextAutosizingWindowSizeOverride;
+        float m_originalTextAutosizingFontScaleFactor;
 #endif
 #if ENABLE(DIALOG_ELEMENT)
         bool m_originalDialogElementEnabled;
@@ -116,6 +117,7 @@ public:
     void setPictographFontFamily(const String& family, const String& script, ExceptionCode&);
     void setTextAutosizingEnabled(bool enabled, ExceptionCode&);
     void setTextAutosizingWindowSizeOverride(int width, int height, ExceptionCode&);
+    void setTextAutosizingFontScaleFactor(float fontScaleFactor, ExceptionCode&);
     void setEnableScrollAnimator(bool enabled, ExceptionCode&);
     bool scrollAnimatorEnabled(ExceptionCode&);
     void setCSSExclusionsEnabled(bool enabled, ExceptionCode&);
--- third_party/WebKit/Source/WebKit/chromium/public/WebSettings.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebSettings.h
@@ -146,6 +146,7 @@ public:
     virtual void setSyncXHRInDocumentsEnabled(bool) = 0;
     virtual void setTextAreasAreResizable(bool) = 0;
     virtual void setTextAutosizingEnabled(bool) = 0;
+    virtual void setTextAutosizingFontScaleFactor(float) = 0;
     virtual void setTextDirectionSubmenuInclusionBehaviorNeverIncluded() = 0;
     virtual void setUnifiedTextCheckerEnabled(bool) = 0;
     virtual void setUserStyleSheetLocation(const WebURL&) = 0;
