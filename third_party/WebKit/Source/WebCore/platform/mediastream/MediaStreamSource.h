25544c40ff94b6f8953580643ecfbf208b52909c
tommyw@google.com <tommyw@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-01 20:33:39 +0000

    MediaStream API: Add ExtraData capability to MediaStreamSource https://bugs.webkit.org/show_bug.cgi?id=92860
    Reviewed by Adam Barth.
    
    The Chromium port needs to store some related data in MediaStreamSource objects.
    
    Source/Platform:
    
    * chromium/public/WebMediaStreamSource.h:
    (ExtraData):
    (WebKit::WebMediaStreamSource::ExtraData::~ExtraData):
    (WebMediaStreamSource):
    
    Source/WebCore:
    
    Not testable from a LayoutTest.
    
    * platform/chromium/support/WebMediaStreamSource.cpp:
    (ExtraDataContainer):
    (WebKit::ExtraDataContainer::ExtraDataContainer):
    (WebKit::ExtraDataContainer::extraData):
    (WebKit):
    (WebKit::WebMediaStreamSource::extraData):
    (WebKit::WebMediaStreamSource::setExtraData):
    * platform/mediastream/MediaStreamSource.h:
    (ExtraData):
    (WebCore::MediaStreamSource::ExtraData::~ExtraData):
    (MediaStreamSource):
    (WebCore::MediaStreamSource::extraData):
    (WebCore::MediaStreamSource::setExtraData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mediastream/MediaStreamSource.h
+++ third_party/WebKit/Source/WebCore/platform/mediastream/MediaStreamSource.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Ericsson AB. All rights reserved.
+ * Copyright (C) 2012 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -48,6 +49,11 @@ public:
         virtual void sourceChangedState() = 0;
     };
 
+    class ExtraData : public RefCounted<ExtraData> {
+    public:
+        virtual ~ExtraData() { }
+    };
+
     enum Type {
         TypeAudio,
         TypeVideo
@@ -71,6 +77,9 @@ public:
     void addObserver(Observer*);
     void removeObserver(Observer*);
 
+    PassRefPtr<ExtraData> extraData() const { return m_extraData; }
+    void setExtraData(PassRefPtr<ExtraData> extraData) { m_extraData = extraData; }
+
 private:
     MediaStreamSource(const String& id, Type, const String& name, ReadyState);
 
@@ -79,6 +88,7 @@ private:
     String m_name;
     ReadyState m_readyState;
     Vector<Observer*> m_observers;
+    RefPtr<ExtraData> m_extraData;
 };
 
 typedef Vector<RefPtr<MediaStreamSource> > MediaStreamSourceVector;
