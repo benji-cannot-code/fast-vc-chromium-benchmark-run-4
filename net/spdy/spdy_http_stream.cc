9de25f731ba09122d217601d4358b67f23d008fd
pauljensen@chromium.org <pauljensen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-01 20:25:32 +0000

    Log SPDY request headers in URL_REQUEST events in net-internals.
    BUG=135203
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149482 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_http_stream.cc
+++ net/spdy/spdy_http_stream.cc
@@ -13,9 +13,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/message_loop.h"
+#include "base/stringprintf.h"
+#include "base/values.h"
 #include "net/base/address_list.h"
 #include "net/base/host_port_pair.h"
 #include "net/base/load_flags.h"
+#include "net/base/net_log.h"
 #include "net/base/net_util.h"
 #include "net/http/http_request_headers.h"
 #include "net/http/http_request_info.h"
@@ -25,6 +28,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/spdy_session.h"
 
 namespace net {
+
+namespace {
+
+Value* NetLogSpdySendRequestCallback(const SpdyHeaderBlock* headers,
+                                     NetLog::LogLevel /* log_level */) {
+  DictionaryValue* dict = new DictionaryValue();
+  ListValue* headers_list = new ListValue();
+  for (SpdyHeaderBlock::const_iterator it = headers->begin();
+       it != headers->end(); ++it) {
+    headers_list->Append(new StringValue(base::StringPrintf(
+        "%s: %s", it->first.c_str(), it->second.c_str())));
+  }
+  dict->Set("headers", headers_list);
+  return dict;
+}
+
+}  // namespace
 
 SpdyHttpStream::SpdyHttpStream(SpdySession* spdy_session,
                                bool direct)
@@ -199,6 +219,9 @@ int SpdyHttpStream::SendRequest(const HttpRequestHeaders& request_headers,
   CreateSpdyHeadersFromHttpRequest(*request_info_, request_headers,
                                    headers.get(), stream_->GetProtocolVersion(),
                                    direct_);
+  stream_->net_log().AddEvent(
+      NetLog::TYPE_HTTP_TRANSACTION_SPDY_SEND_REQUEST_HEADERS,
+      base::Bind(&NetLogSpdySendRequestCallback, headers.get()));
   stream_->set_spdy_headers(headers.Pass());
 
   stream_->SetRequestTime(request_time);
