0133ccd9872d475ffd9a0fb48b4b7df4dddc411e
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-01 22:16:08 +0000

    Generalize SheriffIRCBot to prepare for PerfBot https://bugs.webkit.org/show_bug.cgi?id=92912
    Reviewed by Adam Barth.
    
    Renamed SheriffIRCBot to IRCBot and generalized to support non-sheriffbot IRC bot in the future.
    This will be useful when I add an experimental perf EWS IRC bot.
    
    * Scripts/webkitpy/tool/bot/irc_command.py:
    (Whois.execute):
    * Scripts/webkitpy/tool/bot/irc_command_unittest.py:
    (IRCCommandTest):
    * Scripts/webkitpy/tool/bot/ircbot.py: Moved from Tools/Scripts/webkitpy/tool/bot/sheriffircbot.py.
    (Eliza): Moved from irc_command.
    (Eliza.__init__):
    (Eliza.execute):
    (IRCBot):
    (IRCBot.__init__):
    (IRCBot.irc_delegate):
    (IRCBot._parse_command_and_args):
    (IRCBot.process_message):
    * Scripts/webkitpy/tool/bot/ircbot_unittest.py: Moved from Tools/Scripts/webkitpy/tool/bot/sheriffircbot_unittest.py.
    (run):
    (IRCBotTest):
    (IRCBotTest.test_eliza): Moved from IRCCommandTest.
    (IRCBotTest.test_parse_command_and_args):
    (IRCBotTest.test_exception_during_command):
    * Scripts/webkitpy/tool/bot/sheriffircbot.py: Removed.
    * Scripts/webkitpy/tool/bot/sheriffircbot_unittest.py: Removed.
    * Scripts/webkitpy/tool/commands/sheriffbot.py:
    (SheriffBot.begin_work_queue):
    * Scripts/webkitpy/webkitpy.pyproj:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/ircbot_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/bot/ircbot_unittest.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -34,7 +34,8 @@ from webkitpy.common.system.outputcapture import OutputCapture
 from webkitpy.tool.bot import irc_command
 from webkitpy.tool.bot.queueengine import TerminateQueue
 from webkitpy.tool.bot.sheriff import Sheriff
-from webkitpy.tool.bot.sheriffircbot import SheriffIRCBot
+from webkitpy.tool.bot.ircbot import IRCBot
+from webkitpy.tool.bot.ircbot import Eliza
 from webkitpy.tool.bot.sheriff_unittest import MockSheriffBot
 from webkitpy.tool.mocktool import MockTool
 
@@ -42,24 +43,29 @@ from webkitpy.tool.mocktool import MockTool
 def run(message):
     tool = MockTool()
     tool.ensure_irc_connected(None)
-    bot = SheriffIRCBot(tool, Sheriff(tool, MockSheriffBot()))
+    bot = IRCBot("sheriffbot", tool, Sheriff(tool, MockSheriffBot()), irc_command.commands)
     bot._message_queue.post(["mock_nick", message])
     bot.process_pending_messages()
 
 
-class SheriffIRCBotTest(unittest.TestCase):
+class IRCBotTest(unittest.TestCase):
+    def test_eliza(self):
+        eliza = Eliza()
+        eliza.execute("tom", "hi", None, None)
+        eliza.execute("tom", "bye", None, None)
+
     def test_parse_command_and_args(self):
         tool = MockTool()
-        bot = SheriffIRCBot(tool, Sheriff(tool, MockSheriffBot()))
-        self.assertEqual(bot._parse_command_and_args(""), (irc_command.Eliza, [""]))
-        self.assertEqual(bot._parse_command_and_args("   "), (irc_command.Eliza, [""]))
+        bot = IRCBot("sheriffbot", tool, Sheriff(tool, MockSheriffBot()), irc_command.commands)
+        self.assertEqual(bot._parse_command_and_args(""), (Eliza, [""]))
+        self.assertEqual(bot._parse_command_and_args("   "), (Eliza, [""]))
         self.assertEqual(bot._parse_command_and_args(" hi "), (irc_command.Hi, []))
         self.assertEqual(bot._parse_command_and_args(" hi there "), (irc_command.Hi, ["there"]))
 
     def test_exception_during_command(self):
         tool = MockTool()
         tool.ensure_irc_connected(None)
-        bot = SheriffIRCBot(tool, Sheriff(tool, MockSheriffBot()))
+        bot = IRCBot("sheriffbot", tool, Sheriff(tool, MockSheriffBot()), irc_command.commands)
 
         class CommandWithException(object):
             def execute(self, nick, args, tool, sheriff):
