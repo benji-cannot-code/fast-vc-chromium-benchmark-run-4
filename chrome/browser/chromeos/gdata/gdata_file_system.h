c4df6ae88ed96a34d82ce91223ba7a245ba60a12
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 09:48:05 +0000

    Comment objectType field of ApplicationResource, resolving TODO
    With some comment fixes for style nits.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10827135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system.h
+++ chrome/browser/chromeos/gdata/gdata_file_system.h
@@ -788,6 +788,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
       const std::string& resource_id,
       const std::string& md5,
       const FilePath& cache_file_path);
+
   // Callback for getting the size of the cache file in the blocking pool.
   void OnGetFileSizeCompleteForUpdateFile(
       const FileOperationCallback& callback,
@@ -796,6 +797,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
       const FilePath& cache_file_path,
       GDataFileError* error,
       int64* file_size);
+
   // Callback for GDataRootDirectory::GetEntryByResourceIdAsync.
   void OnGetFileCompleteForUpdateFileByEntry(
     const FileOperationCallback& callback,
@@ -803,7 +805,6 @@ class GDataFileSystem : public GDataFileSystemInterface,
     int64 file_size,
     const FilePath& cache_file_path,
     GDataEntry* entry);
-
 
   // Called when GDataUploader::UploadUpdatedFile() is completed for
   // UpdateFileByResourceId().
