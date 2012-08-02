11383078784a2451809134ad24120b2cb6f5936c
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 02:01:52 +0000

    gdata cleanup: Fix mismatched comments and remove unnecessary forward declarations.
    BUG=none.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10821127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149562 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/chromeos/fileapi/remote_file_system_proxy.h
+++ webkit/chromeos/fileapi/remote_file_system_proxy.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "webkit/fileapi/file_system_operation_interface.h"
 
-class GURL;
-
 namespace fileapi {
 
 typedef base::Callback<
