b39d3c73d08dcacd092ecc556e3c1445a5e4dd5a
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 02:59:57 +0000

    Use JavaObjectWeakGlobalRef in ContentViewCore.
    Java ContentViewCore object may be garbage
    collected without first destroying the native object. Because of this,
    all C++->Java calls to ContentViewCore must be guarded with a null
    check.
    
    BUG=139386,138237
    
    
    Review URL: https://chromiumcodereview.appspot.com/10827125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149570 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_helper.h
+++ base/android/jni_helper.h
@@ -11,12 +11,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/android/scoped_java_ref.h"
 
 // Manages WeakGlobalRef lifecycle.
+// This class is not thread-safe w.r.t. get() and reset(). Multiple threads may
+// safely use get() concurrently, but if the user calls reset() (or of course,
+// calls the destructor) they'll need to provide their own synchronization.
 class JavaObjectWeakGlobalRef {
  public:
   JavaObjectWeakGlobalRef(JNIEnv* env, jobject obj);
   virtual ~JavaObjectWeakGlobalRef();
 
   base::android::ScopedJavaLocalRef<jobject> get(JNIEnv* env) const;
+
+  void reset();
 
  private:
   jweak obj_;
@@ -27,6 +32,6 @@ class JavaObjectWeakGlobalRef {
 // Get the real object stored in the weak reference returned as a
 // ScopedJavaLocalRef.
 base::android::ScopedJavaLocalRef<jobject> GetRealObject(
-    JNIEnv* env, jobject obj);
+    JNIEnv* env, jweak obj);
 
 #endif  // BASE_ANDROID_JNI_HELPER_H_
