41f3642fe0187193551f6c7716aa0a8a60b86f0f
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 23:14:21 +0000

    Create a LinuxSandbox class.
    The LinuxSandbox class aims to become the central place for Linux
    sandboxing inside content/.
    
    For now, this refactors mostly code from the Zygote.
    
    (Note: this is a re-land of https://chromiumcodereview.appspot.com/10826093/
     with a trivial fix for ARM architectures).
    
    BUG=
    TBR=piman@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/10843059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149734 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_init_linux.cc
+++ content/common/sandbox_init_linux.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/time.h"
+#include "content/common/sandbox_linux.h"
 #include "content/public/common/content_switches.h"
 #include "sandbox/linux/seccomp-bpf/sandbox_bpf.h"
 
@@ -539,23 +540,6 @@ bool InitializeBpfSandbox_x86(const CommandLine& command_line,
   return true;
 }
 
-bool InitializeLegacySandbox_x86(const CommandLine& command_line,
-                                  const std::string& process_type) {
-#if defined(SECCOMP_SANDBOX)
-  // Start the old seccomp mode 1 (sandbox/linux/seccomp-legacy).
-  if (process_type == switches::kRendererProcess && SeccompSandboxEnabled()) {
-    // N.b. SupportsSeccompSandbox() returns a cached result, as we already
-    // called it earlier in the zygote. Thus, it is OK for us to not pass in
-    // a file descriptor for "/proc".
-    if (SupportsSeccompSandbox(-1)) {
-      StartSeccompSandbox();
-      return true;
-    }
-  }
-#endif
-  return false;
-}
-
 }  // anonymous namespace
 
 #endif  // defined(__i386__) || defined(__x86_64__)
@@ -572,7 +556,7 @@ void InitializeSandbox() {
 
   // First, try to enable seccomp-legacy.
   seccomp_legacy_started =
-      InitializeLegacySandbox_x86(command_line, process_type);
+      LinuxSandbox::GetInstance()->StartSeccompLegacy(process_type);
   if (seccomp_legacy_started)
     LogSandboxStarted("seccomp-legacy", process_type);
 
