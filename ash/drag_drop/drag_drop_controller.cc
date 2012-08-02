59bb9a94f0b2b771232acda9c8666da1ccd68fa3
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 19:19:58 +0000

    aura: Fix a couple of drag drop issues: 1. Only DragDropController should be allowed to set the cursor while a drag-drop session is in progress. 2. For some wierd reason I was cancelling drag-drop on MOUSE_EXITED event. That is clearly wrong.
    BUG=138694
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149670 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/drag_drop/drag_drop_controller.cc
+++ ash/drag_drop/drag_drop_controller.cc
@@ -65,6 +65,7 @@ int DragDropController::StartDragAndDrop(const ui::OSExchangeData& data,
   if (capture_window)
     capture_window->ReleaseCapture();
   drag_drop_in_progress_ = true;
+  drag_cursor_ = ui::kCursorPointer;
 
   drag_data_ = &data;
   drag_operation_ = operation;
@@ -129,6 +130,7 @@ void DragDropController::DragUpdate(aura::Window* target,
         cursor = ui::kCursorAlias;
       else if (op & ui::DragDropTypes::DRAG_MOVE)
         cursor = ui::kCursorMove;
+      drag_cursor_ = cursor;
       ash::Shell::GetInstance()->cursor_manager()->SetCursor(cursor);
     }
   }
@@ -142,6 +144,7 @@ void DragDropController::DragUpdate(aura::Window* target,
 
 void DragDropController::Drop(aura::Window* target,
                               const aura::LocatedEvent& event) {
+  drag_cursor_ = ui::kCursorPointer;
   ash::Shell::GetInstance()->cursor_manager()->SetCursor(ui::kCursorPointer);
   aura::client::DragDropDelegate* delegate = NULL;
 
@@ -171,6 +174,7 @@ void DragDropController::Drop(aura::Window* target,
 }
 
 void DragDropController::DragCancel() {
+  drag_cursor_ = ui::kCursorPointer;
   ash::Shell::GetInstance()->cursor_manager()->SetCursor(ui::kCursorPointer);
 
   // |drag_window_| can be NULL if we have just started the drag and have not
@@ -192,6 +196,10 @@ bool DragDropController::IsDragDropInProgress() {
   return drag_drop_in_progress_;
 }
 
+gfx::NativeCursor DragDropController::GetDragCursor() {
+  return drag_cursor_;
+}
+
 bool DragDropController::PreHandleKeyEvent(aura::Window* target,
                                            aura::KeyEvent* event) {
   if (drag_drop_in_progress_ && event->key_code() == ui::VKEY_ESCAPE) {
@@ -211,9 +219,6 @@ bool DragDropController::PreHandleMouseEvent(aura::Window* target,
       break;
     case ui::ET_MOUSE_RELEASED:
       Drop(target, *event);
-      break;
-    case ui::ET_MOUSE_EXITED:
-      DragCancel();
       break;
     default:
       // We could reach here if the user drops outside the root window.
--- ui/aura/root_window.cc
+++ ui/aura/root_window.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/aura_switches.h"
 #include "ui/aura/client/activation_client.h"
 #include "ui/aura/client/capture_client.h"
+#include "ui/aura/client/drag_drop_client.h"
 #include "ui/aura/client/event_client.h"
 #include "ui/aura/client/screen_position_client.h"
 #include "ui/aura/env.h"
@@ -219,6 +220,11 @@ gfx::Point RootWindow::GetHostOrigin() const {
 }
 
 void RootWindow::SetCursor(gfx::NativeCursor cursor) {
+  // If a drag is in progress, the DragDropClient should override the cursor.
+  client::DragDropClient* dnd_client = client::GetDragDropClient(this);
+  if (dnd_client && dnd_client->IsDragDropInProgress())
+    cursor = dnd_client->GetDragCursor();
+
   last_cursor_ = cursor;
   // A lot of code seems to depend on NULL cursors actually showing an arrow,
   // so just pass everything along to the host.
