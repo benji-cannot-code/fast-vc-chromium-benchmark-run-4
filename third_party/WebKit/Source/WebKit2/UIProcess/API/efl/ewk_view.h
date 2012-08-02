22f7df2d523e5387bd58aedb3d81a8351d6035fd
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-02 16:03:46 +0000

    [EFL][WK2]Add ewk_view_ui_client. https://bugs.webkit.org/show_bug.cgi?id=89864
    Patch by Hyerim Bae <hyerim.bae@samsung.com> on 2012-08-02
    Reviewed by Kentaro Hara.
    
    Add ewk_view_ui_client.h / cpp files for wrapping WKPageSetPageUIClient,
    add close, createNewPage callback member to WKPageSetPageUIClient.
    
    * PlatformEfl.cmake:
    * UIProcess/API/efl/ewk_view.cpp:
    (ewk_view_base_add):
    (ewk_view_page_close):
    (ewk_view_page_create):
    * UIProcess/API/efl/ewk_view.h:
    * UIProcess/API/efl/ewk_view_private.h:
    * UIProcess/API/efl/ewk_view_ui_client.cpp: Added.
    (toEwkView):
    (closePage):
    (createNewPage):
    (ewk_view_ui_client_attach):
    * UIProcess/API/efl/ewk_view_ui_client_private.h: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *
  * The following signals (see evas_object_smart_callback_add()) are emitted:
  *
+ * - "close,window", void: window is closed.
+ * - "create,window", Evas_Object**: a new window is created.
  * - "download,cancelled", Ewk_Download_Job*: reports that a download was effectively cancelled.
  * - "download,failed", Ewk_Download_Job_Error*: reports that a download failed with the given error.
  * - "download,finished", Ewk_Download_Job*: reports that a download completed successfully.
