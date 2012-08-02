d7b42e4f6bccc3e6e6ddd525f3ce932d9c85121e
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 11:32:45 +0000

    SpeechRecognitionDispatcher: break out translation of error codes to separate function
    Also remove stale comment from speech_recognition_error.h.
    
    BUG=116954
    
    
    Review URL: https://chromiumcodereview.appspot.com/10831110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149612 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/speech_recognition_error.h
+++ content/public/common/speech_recognition_error.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-// This enumeration follows the values described here:
-// http://www.w3.org/2005/Incubator/htmlspeech/2010/10/google-api-draft.html#speech-input-error
 enum SpeechRecognitionErrorCode {
   // There was no error.
   SPEECH_RECOGNITION_ERROR_NONE = 0,
