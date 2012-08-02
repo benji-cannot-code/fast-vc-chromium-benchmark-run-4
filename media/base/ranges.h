b647af5e54dc9427cbd889c6e91908ac3952a83b
vrk@google.com <vrk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 21:24:38 +0000

    Cap sourceBuffered() on duration and truncate duration on EoS
    Adds logic + test to truncate the duration on Media Source streams when
    EndOfStream is signalled. Also adds some logic to make sure buffered streams
    never exceed the reported duration.
    
    BUG=139899
    
    Review URL: https://chromiumcodereview.appspot.com/10829108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149702 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/ranges.h
+++ media/base/ranges.h
@@ -40,7 +40,7 @@ class Ranges {
   void clear();
 
   // Computes the intersection between this range and |other|.
-  Ranges<T> IntersectionWith(const Ranges<T>& other);
+  Ranges<T> IntersectionWith(const Ranges<T>& other) const;
 
  private:
   // Wrapper around DCHECK_LT allowing comparisons of operator<<'able T's.
@@ -134,7 +134,7 @@ void Ranges<T>::clear() {
 }
 
 template<class T>
-Ranges<T> Ranges<T>::IntersectionWith(const Ranges<T>& other) {
+Ranges<T> Ranges<T>::IntersectionWith(const Ranges<T>& other) const {
   Ranges<T> result;
 
   size_t i = 0;
