626eb2d8cd8ac0a2e14579bf49de7895b18f9395
oleg@chromium.org <oleg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 15:56:29 +0000

    Move all the butter bar code to a separate class. Fix some style, method & variable names and simplify code.
    BUG=134601
    TEST=Progress is shown while copying files. Error messages in butter bar are shown and hidden appropriately.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -52,6 +52,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="path_util.js"/>
 //<include src="util.js"/>
 //<include src="breadcrumbs_controller.js"/>
+//<include src="butter_bar.js"/>
 //<include src="directory_contents.js">
 //<include src="directory_model.js"/>
 //<include src="file_copy_manager_wrapper.js"/>
