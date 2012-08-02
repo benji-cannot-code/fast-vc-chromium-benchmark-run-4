c5f6c1512229ba0be4cc862f5e454321bf703637
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-02 12:56:16 +0000

    [EFL][WK2] WTR is failing when X server is not running https://bugs.webkit.org/show_bug.cgi?id=92719
    Patch by Alexander Shalamov <alexander.shalamov@intel.com> on 2012-08-02
    Reviewed by Hajime Morita.
    
    EFL's WebKitTestRunner doesn't execute tests when X server is not running.
    This patch fixes the problem by checking environment variable before ecore x initialization.
    
    * Scripts/webkitpy/layout_tests/port/efl.py:
    (EflPort.setup_environ_for_server):
    * WebKitTestRunner/efl/main.cpp:
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124445 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/efl/main.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/efl/main.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EWebKit2.h"
 #include "TestController.h"
 #include <wtf/Assertions.h>
+#include <stdlib.h>
 
 #ifdef HAVE_ECORE_X
 #include <Ecore_X.h>
@@ -36,9 +37,14 @@ int main(int argc, char** argv)
         return 1;
 
 #ifdef HAVE_ECORE_X
-    if (!ecore_x_init(0)) {
-        ewk_shutdown();
-        return 1;
+    const char* display = getenv("DISPLAY");
+    int intialized = 0;
+    if (display) {
+        intialized = ecore_x_init(0);
+        if (!intialized) {
+            ewk_shutdown();
+            return 1;
+        }
     }
 #endif
 
@@ -46,7 +52,8 @@ int main(int argc, char** argv)
     WTR::TestController controller(argc, const_cast<const char**>(argv));
 
 #ifdef HAVE_ECORE_X
-    ecore_x_shutdown();
+    if (intialized)
+        ecore_x_shutdown();
 #endif
 
     ewk_shutdown();
