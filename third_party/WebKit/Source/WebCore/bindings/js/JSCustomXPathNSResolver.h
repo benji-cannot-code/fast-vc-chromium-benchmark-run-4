12d793622caadd3ccfb7a1575be56f3255c291bb
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-02 22:24:08 +0000

    A few objects aren't being safely protected from GC in all cases https://bugs.webkit.org/show_bug.cgi?id=93031
    Reviewed by Filip Pizlo.
    
    I haven't seen evidence that anyone is hitting bugs due to this, but any
    GC error can lead to later -- hard to diagnose -- bugs if they result in
    resurrecting dead objects.
    
    * bindings/js/JSCustomXPathNSResolver.cpp:
    (WebCore::JSCustomXPathNSResolver::create):
    (WebCore::JSCustomXPathNSResolver::JSCustomXPathNSResolver):
    (WebCore::JSCustomXPathNSResolver::lookupNamespaceURI):
    * bindings/js/JSCustomXPathNSResolver.h:
    (JSCustomXPathNSResolver):
    * bindings/js/JSDictionary.cpp:
    (WebCore::JSDictionary::tryGetProperty):
    * bindings/js/JSDictionary.h:
    (WebCore::JSDictionary::JSDictionary):
    (WebCore::JSDictionary::initializerObject):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSCustomXPathNSResolver.h
+++ third_party/WebKit/Source/WebCore/bindings/js/JSCustomXPathNSResolver.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define JSCustomXPathNSResolver_h
 
 #include "XPathNSResolver.h"
+#include <heap/Strong.h>
+#include <heap/StrongInlines.h>
 #include <runtime/JSValue.h>
 #include <wtf/Forward.h>
 #include <wtf/RefPtr.h>
@@ -51,11 +53,11 @@ namespace WebCore {
         virtual String lookupNamespaceURI(const String& prefix);
 
     private:
-        JSCustomXPathNSResolver(JSC::JSObject*, JSDOMWindow*);
+        JSCustomXPathNSResolver(JSC::ExecState*, JSC::JSObject*, JSDOMWindow*);
 
-        // JSCustomXPathNSResolvers are always temporary, thus no need to GC protect the objects.
-        JSC::JSObject* m_customResolver;
-        JSDOMWindow* m_globalObject;
+        // JSCustomXPathNSResolvers are always temporary so using a Strong reference is safe here.
+        JSC::Strong<JSC::JSObject> m_customResolver;
+        JSC::Strong<JSDOMWindow> m_globalObject;
     };
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebCore/bindings/js/JSDictionary.h
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDictionary.h
@@ -28,6 +28,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define JSDictionary_h
 
 #include "MessagePort.h"
+#include <heap/Strong.h>
+#include <heap/StrongInlines.h>
 #include <interpreter/CallFrame.h>
 #include <wtf/Forward.h>
 
@@ -47,7 +49,7 @@ class JSDictionary {
 public:
     JSDictionary(JSC::ExecState* exec, JSC::JSObject* initializerObject)
         : m_exec(exec)
-        , m_initializerObject(initializerObject)
+        , m_initializerObject(exec->globalData(), initializerObject)
     {
     }
 
@@ -63,7 +65,7 @@ public:
     bool getWithUndefinedOrNullCheck(const String& propertyName, String& value) const;
 
     JSC::ExecState* execState() const { return m_exec; }
-    JSC::JSObject* initializerObject() const { return m_initializerObject; }
+    JSC::JSObject* initializerObject() const { return m_initializerObject.get(); }
     bool isValid() const { return m_exec && m_initializerObject; }
 
 private:
@@ -110,7 +112,7 @@ private:
     static void convertValue(JSC::ExecState*, JSC::JSValue, ArrayValue& result);
 
     JSC::ExecState* m_exec;
-    JSC::JSObject* m_initializerObject;
+    JSC::Strong<JSC::JSObject> m_initializerObject;
 };
 
 template <typename T, typename Result>
