268ff14fc801d1619c603ee70eecb927bee1f1a3
piman@chromium.org <piman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-02 00:22:05 +0000

    [chromium] factor out the optimization pass in CCRenderSurfaceFilters::apply https://bugs.webkit.org/show_bug.cgi?id=92453
    Reviewed by James Robinson.
    
    This separates the "optimization" pass in CCRenderSurfaceFilters::apply
    to resolve a succession of color matrix filters into a single operation.
    This allows testing of that code.
    This introduces a new generic color matrix WebFilterOperation, which can
    also be used on its own.
    
    Source/Platform:
    
    * Platform.gypi:
    * chromium/public/WebFilterOperation.h:
    (WebKit::WebFilterOperation::matrix):
    (WebKit::WebFilterOperation::createColorMatrixFilter):
    (WebFilterOperation):
    (WebKit::WebFilterOperation::WebFilterOperation):
    * chromium/src/WebFilterOperation.cpp: Copied from Source/WebCore/platform/graphics/chromium/cc/CCRenderSurfaceFilters.h.
    (WebKit):
    (WebKit::WebFilterOperation::equals):
    (WebKit::WebFilterOperation::WebFilterOperation):
    * chromium/src/WebFilterOperations.cpp:
    (WebKit::WebFilterOperations::hasFilterThatAffectsOpacity):
    
    Source/WebCore:
    
    New test: CCRenderSurfaceFiltersTest.
    
    * platform/graphics/chromium/cc/CCRenderSurfaceFilters.cpp:
    (WebCore::CCRenderSurfaceFilters::optimize):
    (WebCore):
    (WebCore::CCRenderSurfaceFilters::apply):
    * platform/graphics/chromium/cc/CCRenderSurfaceFilters.h:
    (CCRenderSurfaceFilters):
    
    Source/WebKit/chromium:
    
    * WebKit.gypi:
    * tests/CCRenderSurfaceFiltersTest.cpp: Added.
    (WebKit):
    (WebKit::isCombined):
    (WebKit::TEST):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebFilterOperation.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebFilterOperation.h
@@ -27,8 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebFilterOperation_h
 #define WebFilterOperation_h
 
+#include "SkScalar.h"
 #include "WebCommon.h"
-
 #include "WebColor.h"
 #include "WebPoint.h"
 
@@ -47,6 +47,7 @@ public:
         FilterTypeOpacity,
         FilterTypeBlur,
         FilterTypeDropShadow,
+        FilterTypeColorMatrix,
     };
 
     FilterType type() const { return m_type; }
@@ -65,6 +66,10 @@ public:
         WEBKIT_ASSERT(m_type == FilterTypeDropShadow);
         return m_dropShadowColor;
     }
+    const SkScalar* matrix() const
+    {
+        return m_matrix;
+    }
 
 #define WEBKIT_HAS_NEW_WEBFILTEROPERATION_API 1
     static WebFilterOperation createGrayscaleFilter(float amount) { return WebFilterOperation(FilterTypeGrayscale, amount); }
@@ -77,13 +82,9 @@ public:
     static WebFilterOperation createOpacityFilter(float amount) { return WebFilterOperation(FilterTypeOpacity, amount); }
     static WebFilterOperation createBlurFilter(float amount) { return WebFilterOperation(FilterTypeBlur, amount); }
     static WebFilterOperation createDropShadowFilter(WebPoint offset, float stdDeviation, WebColor color) { return WebFilterOperation(FilterTypeDropShadow, offset, stdDeviation, color); }
+    static WebFilterOperation createColorMatrixFilter(SkScalar matrix[20]) { return WebFilterOperation(FilterTypeColorMatrix, matrix); }
 
-    bool equals(const WebFilterOperation& other) const
-    {
-        return m_amount == other.m_amount
-            && m_dropShadowOffset == other.m_dropShadowOffset
-            && m_dropShadowColor == other.m_dropShadowColor;
-    }
+    bool equals(const WebFilterOperation& other) const;
 
 private:
     FilterType m_type;
@@ -91,10 +92,11 @@ private:
     float m_amount;
     WebPoint m_dropShadowOffset;
     WebColor m_dropShadowColor;
+    SkScalar m_matrix[20];
 
     WebFilterOperation(FilterType type, float amount)
     {
-        WEBKIT_ASSERT(type != FilterTypeDropShadow);
+        WEBKIT_ASSERT(type != FilterTypeDropShadow && type != FilterTypeColorMatrix);
         m_type = type;
         m_amount = amount;
         m_dropShadowColor = 0;
@@ -108,6 +110,8 @@ private:
         m_dropShadowOffset = offset;
         m_dropShadowColor = color;
     }
+
+    WebFilterOperation(FilterType, SkScalar matrix[20]);
 };
 
 inline bool operator==(const WebFilterOperation& a, const WebFilterOperation& b)
--- third_party/WebKit/Source/WebKit/chromium/WebKit.gypi
+++ third_party/WebKit/Source/WebKit/chromium/WebKit.gypi
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'tests/CCOcclusionTrackerTest.cpp',
             'tests/CCOcclusionTrackerTestCommon.h',
             'tests/CCQuadCullerTest.cpp',
+            'tests/CCRenderSurfaceFiltersTest.cpp',
             'tests/CCRenderSurfaceTest.cpp',
             'tests/CCResourceProviderTest.cpp',
             'tests/CCSchedulerStateMachineTest.cpp',
