64a0e014875818b17a16af767b05f131c740400f
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-02 16:08:48 +0000

    Inline stylesheets can confuse style sharing https://bugs.webkit.org/show_bug.cgi?id=92970
    Reviewed by Dan Bernstein.
    
    Source/WebCore: 
    
    Consider document 
            
    <div class="i30"></div>
    <style>.i30 { background-color:green; }</style>
    <div class="i30"></div>
            
    When processing the <style> element the scope optimization marks the first div as needing style recalc. 
    Next the parser adds the second div to the tree and immediately calculates its style. Since it looks exactly 
    like the first div the style sharing optimization copies the style from there. The pending recalc of the
    first div is resolved by a timer but the second div is left with the old style.
            
    Fix by disallowing style sharing from elements with pending style recalc.
    
    Test: fast/css/style-sharing-inline-stylesheet.html
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::canShareStyleWithElement):
    
    LayoutTests: 
    
    * fast/css/style-sharing-inline-stylesheet-expected.txt: Added.
    * fast/css/style-sharing-inline-stylesheet.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
@@ -1394,6 +1394,8 @@ bool StyleResolver::canShareStyleWithElement(StyledElement* element) const
         return false;
     if (element->inlineStyle())
         return false;
+    if (element->needsStyleRecalc())
+        return false;
 #if ENABLE(SVG)
     if (element->isSVGElement() && static_cast<SVGElement*>(element)->animatedSMILStyleProperties())
         return false;
