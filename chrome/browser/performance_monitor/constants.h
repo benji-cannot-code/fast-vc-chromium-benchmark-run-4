2ea0ed50a388d4d740cc1856e814a1c436aa4c84
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-02 21:15:08 +0000

    CPM: revised unclean exit watching, revised renderer crash test
    Fixed unclean exit watching to work on CrOS
    Added a cleaner waiting structure to renderer crash to see if it clears up the
    CrOS ASAN error.
    
    BUG=137726, 130212
    
    
    Review URL: https://chromiumcodereview.appspot.com/10815079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149697 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/performance_monitor/constants.h
+++ chrome/browser/performance_monitor/constants.h
@@ -8,25 +8,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_monitor {
 
-// TODO(chebert): i18n on all constants.
+// Constants which are used by the PerformanceMonitor and its related classes.
+// The constants should be documented alongside the definition of their values
+// in the .cc file.
+
 extern const char kMetricNotFoundError[];
-
-// Any metric that is not associated with a specific activity will use this as
-// its activity.
 extern const char kProcessChromeAggregate[];
+extern const int kGatherIntervalInMinutes;
 
-// Metrics keys for statistics gathering.
+// State tokens
+extern const char kStateChromeVersion[];
+extern const char kStateProfilePrefix[];
+
+// Metric details
 extern const char kMetricCPUUsageName[];
 extern const char kMetricCPUUsageDescription[];
 extern const char kMetricCPUUsageUnits[];
 extern const double kMetricCPUUsageTickSize;
-
 extern const char kMetricPrivateMemoryUsageName[];
 extern const char kMetricPrivateMemoryUsageDescription[];
 extern const char kMetricPrivateMemoryUsageUnits[];
 extern const double kMetricPrivateMemoryUsageTickSize;
-
-extern const char kStateChromeVersion[];
 
 }  // namespace performance_monitor
 
