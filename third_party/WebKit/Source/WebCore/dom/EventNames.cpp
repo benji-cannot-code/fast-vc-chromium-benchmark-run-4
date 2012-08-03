c2735abd6b73fa38996ab3ca98638b0780f953a8
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-03 15:22:29 +0000

    Initialize the Event Names' string from read only memory https://bugs.webkit.org/show_bug.cgi?id=92435
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-08-03
    Reviewed by Anders Carlsson.
    
    Similarily to r123689, we can initialize the event names' string from memory without copying the data.
    This saves us memory and initialization time.
    
    * dom/EventNames.cpp:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/EventNames.cpp
+++ third_party/WebKit/Source/WebCore/dom/EventNames.cpp
@@ -25,10 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 #define INITIALIZE_EVENT_NAME(name) \
-    , name##Event(#name)
+    , name##Event(#name, AtomicString::ConstructFromLiteral)
 
 #define INITIALIZE_EVENT_INTERFACE(name) \
-    , interfaceFor##name(#name)
+    , interfaceFor##name(#name, AtomicString::ConstructFromLiteral)
 
 EventNames::EventNames()
     : dummy(0)
