1675431113b0fa4f7c3b5e54e1433a3f9840d847
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 23:33:33 +0000

    Delete both regular and Metro user data dirs on uninstall.
    Also:
    - The uninstall survey will use the metro Chrome local state if there is no desktop Chrome local state (although in the future we maybe want to aggregate them).
    - Chrome inactivity for toast purposes is now based on the most-recently used of both desktop and metro Chrome.
    
    BUG=125793
    TEST=install chrome, make it the default, launch into metro at least once, quit, then uninstall and check the "also clear..." box.  Metro data dir should be gone.
    
    Review URL: https://chromiumcodereview.appspot.com/10826144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149968 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/helper.cc
+++ chrome/installer/util/helper.cc
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/file_path.h"
 #include "base/path_service.h"
+#include "base/win/windows_version.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/installer/util/browser_distribution.h"
 #include "chrome/installer/util/installation_state.h"
 #include "chrome/installer/util/install_util.h"
@@ -41,8 +43,22 @@ FilePath GetChromeInstallPath(bool system_install, BrowserDistribution* dist) {
   return GetChromeInstallBasePath(system_install, dist, kInstallBinaryDir);
 }
 
-FilePath GetChromeUserDataPath(BrowserDistribution* dist) {
-  return GetChromeInstallBasePath(false, dist, kInstallUserDataDir);
+void GetChromeUserDataPaths(BrowserDistribution* dist,
+                            std::vector<FilePath>* paths) {
+  const bool has_metro_data = dist->CanSetAsDefault() &&
+      base::win::GetVersion() >= base::win::VERSION_WIN8;
+  FilePath data_dir(GetChromeInstallBasePath(false, dist, kInstallUserDataDir));
+  if (data_dir.empty()) {
+    paths->clear();
+  } else {
+    paths->resize(has_metro_data ? 2 : 1);
+    (*paths)[0] = data_dir;
+    if (has_metro_data) {
+      (*paths)[1] = data_dir.DirName().Append(
+          chrome::kMetroChromeUserDataSubDir);
+    }
+  }
+  DCHECK(!paths->empty());
 }
 
 BrowserDistribution* GetBinariesDistribution(bool system_install) {
--- chrome/installer/util/product_unittest.cc
+++ chrome/installer/util/product_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/test/test_reg_util_win.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/installer/util/chrome_frame_distribution.h"
 #include "chrome/installer/util/google_update_constants.h"
 #include "chrome/installer/util/installation_state.h"
@@ -71,10 +72,19 @@ TEST_F(ProductTest, MAYBE_ProductInstallBasic) {
   BrowserDistribution* distribution = product->distribution();
   EXPECT_EQ(BrowserDistribution::CHROME_BROWSER, distribution->GetType());
 
-  FilePath user_data(product->GetUserDataPath());
-  EXPECT_FALSE(user_data.empty());
+  std::vector<FilePath> user_data_paths;
+  product->GetUserDataPaths(&user_data_paths);
+  EXPECT_LE(static_cast<size_t>(1), user_data_paths.size());
+  const FilePath& user_data = user_data_paths[0];
+  EXPECT_FALSE(user_data_paths[0].empty());
   EXPECT_NE(std::wstring::npos,
-            user_data.value().find(installer::kInstallUserDataDir));
+            user_data_paths[0].value().find(installer::kInstallUserDataDir));
+  if (user_data_paths.size() > 1) {
+    EXPECT_FALSE(user_data_paths[1].empty());
+    EXPECT_NE(
+        std::wstring::npos,
+        user_data_paths[1].value().find(chrome::kMetroChromeUserDataSubDir));
+  }
 
   FilePath program_files;
   PathService::Get(base::DIR_PROGRAM_FILES, &program_files);
