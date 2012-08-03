e221e1978af79d19075a0d4e69b81437d1a4c851
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 21:29:28 +0000

    [Sync] Set transport_security_state for sync_{client,listen_notifications}
    Currently, the DCHECK added by r148251 fires.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10830164
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149928 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/tools/sync_client.cc
+++ sync/tools/sync_client.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/host_port_pair.h"
 #include "net/base/host_resolver.h"
 #include "net/base/network_change_notifier.h"
+#include "net/base/transport_security_state.h"
 #include "net/url_request/url_request_test_util.h"
 #include "sync/internal_api/public/base/model_type.h"
 #include "sync/internal_api/public/base_node.h"
@@ -103,6 +104,8 @@ class MyTestURLRequestContext : public TestURLRequestContext {
             net::HostResolver::kDefaultParallelism,
             net::HostResolver::kDefaultRetryAttempts,
             NULL));
+    context_storage_.set_transport_security_state(
+        new net::TransportSecurityState());
     Init();
   }
 
