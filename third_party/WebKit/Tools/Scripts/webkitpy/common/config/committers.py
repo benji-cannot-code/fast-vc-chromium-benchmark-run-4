3af98853cb827547d02c3b7e25d85eeea9c8fec2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-03 19:18:06 +0000

    Unreviewed, adding self to committers.py after becoming committer.
    Patch by Arvid Nilsson <anilsson@rim.com> on 2012-08-03
    
    * Scripts/webkitpy/common/config/committers.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124637 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/config/committers.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/config/committers.py
@@ -202,6 +202,7 @@ committers_unable_to_review = [
     Committer("Anton D'Auria", "adauria@apple.com", "antonlefou"),
     Committer("Anton Muhin", "antonm@chromium.org", "antonm"),
     Committer("Arko Saha", "arko@motorola.com", "arkos"),
+    Committer("Arvid Nilsson", "anilsson@rim.com", "anilsson"),
     Committer("Balazs Kelemen", "kbalazs@webkit.org", "kbalazs"),
     Committer("Ben Murdoch", "benm@google.com", "benm"),
     Committer("Ben Wells", "benwells@chromium.org", "benwells"),
