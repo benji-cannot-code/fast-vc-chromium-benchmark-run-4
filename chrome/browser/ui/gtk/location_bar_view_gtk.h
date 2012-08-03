4dd25092bd9ab8556a8bb45f1dae266e41509267
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 12:21:32 +0000

    GTK implementation of the action box button.
    Does nothing so far.
    
    TBR=ben@chromium.org
    BUG=125307
    
    Review URL: https://chromiumcodereview.appspot.com/10830140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/location_bar_view_gtk.h
+++ chrome/browser/ui/gtk/location_bar_view_gtk.h
@@ -39,13 +39,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/gtk/owned_widget_gtk.h"
 #include "webkit/glue/window_open_disposition.h"
 
-class OmniboxViewGtk;
+class ActionBoxButtonGtk;
 class Browser;
 class CommandUpdater;
 class ContentSettingImageModel;
 class ContentSettingBubbleGtk;
 class ExtensionAction;
 class GtkThemeService;
+class OmniboxViewGtk;
 class SkBitmap;
 class ToolbarModel;
 
@@ -482,6 +483,8 @@ class LocationBarViewGtk : public OmniboxEditController,
 
   // Alignment used to wrap |location_entry_|.
   GtkWidget* location_entry_alignment_;
+
+  scoped_ptr<ActionBoxButtonGtk> action_box_button_;
 
   CommandUpdater* command_updater_;
   ToolbarModel* toolbar_model_;
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -3026,6 +3026,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/global_error/global_error_service_factory.h',
         'browser/ui/gtk/accelerators_gtk.cc',
         'browser/ui/gtk/accelerators_gtk.h',
+        'browser/ui/gtk/action_box_button_gtk.cc',
+        'browser/ui/gtk/action_box_button_gtk.h',
         'browser/ui/gtk/autofill/autofill_popup_view_gtk.cc',
         'browser/ui/gtk/autofill/autofill_popup_view_gtk.h',
         'browser/ui/gtk/avatar_menu_bubble_gtk.cc',
