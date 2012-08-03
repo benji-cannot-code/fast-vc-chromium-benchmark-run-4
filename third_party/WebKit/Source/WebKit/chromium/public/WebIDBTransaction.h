f9b01706a0cdf36c0caa033d820aa2788175744c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-03 05:07:13 +0000

    IndexedDB: remove leftover chromium stubs from putIndexWithKeys https://bugs.webkit.org/show_bug.cgi?id=93032
    Patch by Alec Flett <alecflett@chromium.org> on 2012-08-02
    Reviewed by Darin Fisher.
    
    Remove old chromium APIs that are no longer called by chrome.
    
    * public/WebIDBCursor.h:
    * public/WebIDBObjectStore.h:
    * public/WebIDBTransaction.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebIDBTransaction.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebIDBTransaction.h
@@ -42,11 +42,6 @@ class WebIDBTransaction {
 public:
     virtual ~WebIDBTransaction() { }
 
-    virtual int mode() const
-    {
-        WEBKIT_ASSERT_NOT_REACHED();
-        return 0;
-    }
     virtual WebIDBObjectStore* objectStore(const WebString& name, WebExceptionCode&)
     {
         WEBKIT_ASSERT_NOT_REACHED();
