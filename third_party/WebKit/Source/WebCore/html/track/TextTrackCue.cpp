f6e830d1f060797158aa8e40ca57d6850f5540a6
annacc@chromium.org <annacc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-03 20:08:53 +0000

    Negative timestamps for TextTrackCues should not be allowed. https://bugs.webkit.org/show_bug.cgi?id=92939
    Reviewed by Eric Carlson.
    
    Make sure cues added in JavaScript are not allowed negative timestamps.
    Attempting to add a cue with a negative timestamp is not successful
    and setting a timestamp to a negative value has no effect.
    
    Source/WebCore:
    
    Test: media/track/track-cue-negative-timestamp.html
    
    * html/track/TextTrack.cpp:
    (WebCore::TextTrack::addCue): If the cue's startTime or endTime is
        negative, do not add the cue.
    * html/track/TextTrackCue.cpp:
    (WebCore::TextTrackCue::setStartTime): Ignore negative values.
    (WebCore::TextTrackCue::setEndTime): Ignore negative values.
    * html/track/TextTrackCueList.cpp:
    (WebCore::TextTrackCueList::add): Add ASSERTs to check startTime and
        endTime are positive.
    
    LayoutTests:
    
    * media/track/track-cue-negative-timestamp-expected.txt: Added.
    * media/track/track-cue-negative-timestamp.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/track/TextTrackCue.cpp
+++ third_party/WebKit/Source/WebCore/html/track/TextTrackCue.cpp
@@ -169,7 +169,8 @@ void TextTrackCue::setId(const String& id)
 
 void TextTrackCue::setStartTime(double value)
 {
-    if (m_startTime == value)
+    // TODO(93143): Add spec-compliant behavior for negative time values.
+    if (m_startTime == value || value < 0)
         return;
     
     cueWillChange();
@@ -179,7 +180,8 @@ void TextTrackCue::setStartTime(double value)
     
 void TextTrackCue::setEndTime(double value)
 {
-    if (m_endTime == value)
+    // TODO(93143): Add spec-compliant behavior for negative time values.
+    if (m_endTime == value || value < 0)
         return;
     
     cueWillChange();
