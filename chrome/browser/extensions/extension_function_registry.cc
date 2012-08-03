72bd9498b340ff337f5004895b5cfe2a9d9e5c42
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 22:35:29 +0000

    Drive: Adds clearDriveCache API to file_browser_private.
    This API is to clear all the local cache of Drive.
    
    BUG=135197
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10837091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -377,6 +377,7 @@ void ExtensionFunctionRegistry::ResetFunctions() {
   RegisterFunction<GetGDataPreferencesFunction>();
   RegisterFunction<SetGDataPreferencesFunction>();
   RegisterFunction<SearchDriveFunction>();
+  RegisterFunction<ClearDriveCacheFunction>();
   RegisterFunction<GetNetworkConnectionStateFunction>();
   RegisterFunction<RequestDirectoryRefreshFunction>();
 
