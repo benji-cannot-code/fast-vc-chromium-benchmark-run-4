0ced4598390d7acd678661a99b332129d01a46fe
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 02:53:51 +0000

    Revert 149782 - Sandbox: Fix CreateProcess policy tests.
    BUG=6944
    TEST=sbox_integration_tests
    Review URL: https://chromiumcodereview.appspot.com/10823134
    
    TBR=rvargas@google.com
    Review URL: https://chromiumcodereview.appspot.com/10828142
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149790 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/win/tests/common/controller.h
+++ sandbox/win/tests/common/controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SANDBOX_WIN_TESTS_COMMON_CONTROLLER_H_
-#define SANDBOX_WIN_TESTS_COMMON_CONTROLLER_H_
+#ifndef SANDBOX_TESTS_COMMON_CONTROLLER_H_
+#define SANDBOX_TESTS_COMMON_CONTROLLER_H__
 
 #include <windows.h>
 #include <string>
@@ -30,8 +30,6 @@ enum SboxTestResult {
   SBOX_TEST_DENIED,     // Access was denied.
   SBOX_TEST_NOT_FOUND,  // The resource was not found.
   SBOX_TEST_FIRST_ERROR = SBOX_TEST_FIRST_RESULT | SEVERITY_ERROR_FLAGS,
-  SBOX_TEST_SECOND_ERROR,
-  SBOX_TEST_THIRD_ERROR,
   SBOX_TEST_INVALID_PARAMETER,
   SBOX_TEST_FAILED_TO_RUN_TEST,
   SBOX_TEST_FAILED_TO_EXECUTE_COMMAND,
@@ -145,4 +143,4 @@ int DispatchCall(int argc, wchar_t **argv);
 
 }  // namespace sandbox
 
-#endif  // SANDBOX_WIN_TESTS_COMMON_CONTROLLER_H_
+#endif  // SANDBOX_TESTS_COMMON_CONTROLLER_H_
