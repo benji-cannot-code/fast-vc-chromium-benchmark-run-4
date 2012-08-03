f18e2c751d0ace740652cff6150428139d51952d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-03 12:55:30 +0000

    bookmarks: Forward declare DefaultSingletonTraits in bookmark_model_factory.h
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10836050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149847 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model_factory.cc
+++ chrome/browser/bookmarks/bookmark_model_factory.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 
+#include "base/memory/singleton.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/prefs/pref_service.h"
