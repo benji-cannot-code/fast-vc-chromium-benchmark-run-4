6745b9b55a2b5264d32bc52472642483446894d0
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-03 01:51:35 +0000

    V8 bindings code that tries to find its context should use BindingState functions rather than V8Proxy https://bugs.webkit.org/show_bug.cgi?id=93038
    Reviewed by Eric Seidel.
    
    After this patch, most code in the V8 bindings that tries to determine
    it's "context" goes through BindingState.h rather than V8Proxy. This
    patch is a step towards unifying all these code paths.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateNamedConstructorCallback):
    * bindings/v8/BindingState.cpp:
    (WebCore::currentFrame):
    (WebCore):
    * bindings/v8/BindingState.h:
    (WebCore):
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::retrieveFrameForEnteredContext):
    (WebCore::ScriptController::retrieveFrameForCurrentContext):
    * bindings/v8/V8DOMWindowShell.cpp:
    (WebCore::v8UncaughtExceptionHandler):
    * bindings/v8/V8Proxy.cpp:
    (WebCore::V8Proxy::reportUnsafeAccessTo):
    * bindings/v8/V8Proxy.h:
    (V8Proxy):
    * bindings/v8/V8Utilities.cpp:
    (WebCore::getScriptExecutionContext):
    * bindings/v8/custom/V8AudioContextCustom.cpp:
    (WebCore::V8AudioContext::constructorCallback):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
    (WebCore::V8HTMLDocument::writeCallback):
    (WebCore::V8HTMLDocument::writelnCallback):
    (WebCore::V8HTMLDocument::openCallback):
    * bindings/v8/custom/V8HTMLImageElementConstructor.cpp:
    (WebCore::v8HTMLImageElementConstructorCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.h
@@ -216,7 +216,6 @@ namespace WebCore {
         // linking time.
         static Frame* retrieveFrameForEnteredContext();
         static Frame* retrieveFrameForCurrentContext();
-        static DOMWindow* retrieveWindowForCallingContext();
         static Frame* retrieveFrameForCallingContext();
 
         // Returns V8 Context of a frame. If none exists, creates
--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLImageElementConstructor.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLImageElementConstructor.cpp
@@ -32,15 +32,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8HTMLImageElementConstructor.h"
 
-#include "HTMLImageElement.h"
+#include "BindingState.h"
 #include "Document.h"
 #include "Frame.h"
+#include "HTMLImageElement.h"
 #include "HTMLNames.h"
 #include "V8Binding.h"
 #include "V8Document.h"
 #include "V8HTMLImageElement.h"
 #include "V8Proxy.h"
-
 #include <wtf/RefPtr.h>
 
 namespace WebCore {
@@ -57,7 +57,7 @@ static v8::Handle<v8::Value> v8HTMLImageElementConstructorCallback(const v8::Arg
     if (ConstructorMode::current() == ConstructorMode::WrapExistingObject)
         return args.Holder();
 
-    Frame* frame = V8Proxy::retrieveFrameForCurrentContext();
+    Frame* frame = currentFrame(BindingState::instance());
     if (!frame)
         return V8Proxy::throwError(V8Proxy::ReferenceError, "Image constructor associated frame is unavailable", args.GetIsolate());
 
