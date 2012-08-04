7cf2f4d9253fd7c800f113abd3607cc1a25e892d
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-04 02:58:19 +0000

    Add support for suppressing error logging, and don't log errors due to cached host information or client device suspend.
    BUG=139389
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825187
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150004 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/remoting.gyp
+++ remoting/remoting.gyp
@@ -173,6 +173,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'webapp/server_log_entry.js',
       'webapp/spinner.gif',
       'webapp/stats_accumulator.js',
+      'webapp/suspend_monitor.js',
       'webapp/toolbar.css',
       'webapp/toolbar.js',
       'webapp/ui_mode.js',
---
+++ remoting/webapp/suspend_monitor.js
@@ -0,0 +1,52 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * @fileoverview
+ * Class to detect when the device is suspended, for example when a laptop's
+ * lid is closed.
+ */
+
+
+'use strict';
+
+/** @suppress {duplicate} */
+var remoting = remoting || {};
+
+/**
+ * @param {function():void} callback Callback function to invoke when a
+ * suspend+resume operation has been detected.
+ *
+ * @constructor
+ */
+remoting.SuspendMonitor = function (callback) {
+  /** @type {function():void} @private */
+  this.callback_ = callback;
+  /** @type {number} @private */
+  this.timerIntervalMs_ = 60 * 1000;
+  /** @type {number} @private */
+  this.lateToleranceMs_ = 60 * 1000;
+  /** @type {number} @private */
+  this.callbackExpectedTime_ = 0;
+  this.start_();
+};
+
+/** @private */
+remoting.SuspendMonitor.prototype.start_ = function() {
+  window.setTimeout(this.checkSuspend_.bind(this), this.timerIntervalMs_);
+  this.callbackExpectedTime_ = new Date().getTime() + this.timerIntervalMs_;
+};
+
+/** @private */
+remoting.SuspendMonitor.prototype.checkSuspend_ = function() {
+  var lateByMs = new Date().getTime() - this.callbackExpectedTime_;
+  if (lateByMs > this.lateToleranceMs_) {
+    this.callback_();
+  }
+  this.start_();
+};
+
+/** @type {remoting.SuspendMonitor?} */
+remoting.suspendMonitor = null;
