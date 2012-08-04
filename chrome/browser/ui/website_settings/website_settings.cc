d8b29dd6fc20f2c9a5a57514802a5e455ecaf555
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-04 00:12:55 +0000

    (Views only) Disable drop-down buttons for managed permissions and display the source of the managed setting (e.g. policy, extension).
    BUG=113688
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10831128
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149981 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/website_settings/website_settings.cc
+++ chrome/browser/ui/website_settings/website_settings.cc
@@ -427,6 +427,7 @@ void WebsiteSettings::PresentSitePermissions() {
     DCHECK(value.get());
     permission_info.setting =
         content_settings::ValueToContentSetting(value.get());
+    permission_info.source = info.source;
 
     if (info.primary_pattern == ContentSettingsPattern::Wildcard() &&
         info.secondary_pattern == ContentSettingsPattern::Wildcard()) {
