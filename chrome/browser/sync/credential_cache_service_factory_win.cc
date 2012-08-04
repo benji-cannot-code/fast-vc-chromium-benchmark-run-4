06b23602453ee33b440f2e314a148a14bca8a937
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-04 02:27:30 +0000

    [sync] Add a polling mechanism to CredentialCacheService for on-the-fly sign in / sign out / reconfigure detection
    With sync credential caching on Win 8, if Metro and Desktop are both
    running at the same time, and the user signs in / signs out / reconfigures
    sync in one mode, the other mode gets updated only after restart.
    
    This patch implements a polling mechanism to enable first-time
    auto-sign-in, first time auto-sign-out, and sync setting / credential
    reconfigurations without the need for browser restart.
    
    BUG=139200
    TEST=Launch clean Desktop and Metro chrome, sign in / sign out / reconfigure one mode, watch for a minute and the other mode should catch up
    
    Review URL: https://chromiumcodereview.appspot.com/10828108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150001 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/credential_cache_service_factory_win.cc
+++ chrome/browser/sync/credential_cache_service_factory_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/signin/token_service_factory.h"
 #include "chrome/browser/sync/credential_cache_service_win.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
@@ -38,6 +39,7 @@ CredentialCacheServiceFactory::CredentialCacheServiceFactory()
   // TODO(rsimha): Uncomment this once it exists.
   // DependsOn(PrefServiceFactory::GetInstance());
   DependsOn(ProfileSyncServiceFactory::GetInstance());
+  DependsOn(SigninManagerFactory::GetInstance());
   DependsOn(TokenServiceFactory::GetInstance());
 }
 
