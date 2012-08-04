38aab5ba4a2cc9758fbca078b49a00106faa6032
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-04 00:39:58 +0000

    aura: Fix launcher tooltips: 1. Refactor out app_list_bubble_border so that it can be used for launcher tooltip border. 2. UI polish: increase padding, make tooltip single line, reduce maximum tooltip width.
    BUG=133292
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10834140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@149987 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/bubble/bubble_border.cc
+++ ui/views/bubble/bubble_border.cc
@@ -213,7 +213,7 @@ void BubbleBorder::GetInsetsForArrowLocation(gfx::Insets* insets,
   insets->Set(top, left, bottom, right);
 }
 
-int BubbleBorder::border_thickness() const {
+int BubbleBorder::GetBorderThickness() const {
   return images_->border_thickness;
 }
 
@@ -506,7 +506,7 @@ void BubbleBackground::Paint(gfx::Canvas* canvas, views::View* view) const {
   paint.setColor(border_->background_color());
   SkPath path;
   gfx::Rect bounds(view->GetContentsBounds());
-  bounds.Inset(-border_->border_thickness(), -border_->border_thickness());
+  bounds.Inset(-border_->GetBorderThickness(), -border_->GetBorderThickness());
   SkScalar radius = SkIntToScalar(BubbleBorder::GetCornerRadius());
   path.addRoundRect(gfx::RectToSkRect(bounds), radius, radius);
   canvas->DrawPath(path, paint);
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -63,6 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'bubble/bubble_delegate.h',
         'bubble/bubble_frame_view.cc',
         'bubble/bubble_frame_view.h',
+        'bubble/bubble_border_2.cc',
+        'bubble/bubble_border_2.h',
         'button_drag_utils.cc',
         'button_drag_utils.h',
         'color_chooser/color_chooser_listener.h',
