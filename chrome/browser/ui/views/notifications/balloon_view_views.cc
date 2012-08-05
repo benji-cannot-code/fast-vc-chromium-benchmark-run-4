684847cd71e8988725b29868335461ca459cdd20
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-05 16:44:20 +0000

    views: Forward declare more classes in balloon_view_views.h.
    R=pkasting@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10832107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150051 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/notifications/balloon_view_views.cc
+++ chrome/browser/ui/views/notifications/balloon_view_views.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/message_loop.h"
 #include "base/utf_string_conversions.h"
-#include "chrome/browser/notifications/balloon.h"
 #include "chrome/browser/notifications/balloon_collection.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
 #include "chrome/browser/notifications/notification.h"
@@ -31,10 +30,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/insets.h"
 #include "ui/gfx/native_widget_types.h"
+#include "ui/gfx/path.h"
 #include "ui/views/bubble/bubble_border.h"
-#include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/image_button.h"
+#include "ui/views/controls/button/menu_button.h"
 #include "ui/views/controls/button/text_button.h"
+#include "ui/views/controls/label.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/menu_model_adapter.h"
 #include "ui/views/controls/menu/menu_runner.h"
@@ -47,8 +48,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #include "chrome/browser/ui/views/notifications/balloon_view_host.h"
 #endif
-
-using views::Widget;
 
 namespace {
 
@@ -359,14 +358,14 @@ void BalloonViewImpl::Show(Balloon* balloon) {
   if (enable_web_ui_)
     html_contents_->EnableWebUI();
 
-  html_container_ = new Widget;
-  Widget::InitParams params(Widget::InitParams::TYPE_POPUP);
+  html_container_ = new views::Widget;
+  views::Widget::InitParams params(views::Widget::InitParams::TYPE_POPUP);
   params.bounds = contents_rect;
   html_container_->Init(params);
   html_container_->SetContentsView(html_contents_->view());
 
   gfx::Rect balloon_rect(x(), y(), GetTotalWidth(), GetTotalHeight());
-  frame_container_ = new Widget;
+  frame_container_ = new views::Widget;
   params.delegate = this;
   params.transparent = true;
   params.bounds = balloon_rect;
