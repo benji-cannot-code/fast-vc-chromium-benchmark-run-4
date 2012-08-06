49ba7867843b53edc01837ea474e1cd9f4b16554
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-06 13:35:09 +0000

    [WK2] REGRESSION(124596) lot of web process unresponsiveness appears on Apple bots https://bugs.webkit.org/show_bug.cgi?id=93120
    Reviewed by Csaba Osztrogonác.
    
    Set the short timeout back to 15 seconds and add more time to WebKitTestRunner
    so it will be able to detect an unresponsive web process.
    
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.default_timeout_ms):
    * WebKitTestRunner/TestController.cpp:
    (WTR):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
@@ -53,11 +53,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTR {
 
-// defaultLongTimeout + defaultShortTimeout should be less than 35,
+// defaultLongTimeout + defaultShortTimeout should be less than 50,
 // the default timeout value of the test harness so we can detect an
 // unresponsive web process.
 static const double defaultLongTimeout = 30;
-static const double defaultShortTimeout = 3;
+static const double defaultShortTimeout = 15;
 static const double defaultNoTimeout = -1;
 
 static WKURLRef blankURL()
