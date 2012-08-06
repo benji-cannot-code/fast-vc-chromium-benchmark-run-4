6f4adef0ae59c1549be7483de404766296f6e63f
arko@motorola.com <arko@motorola.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-06 10:29:39 +0000

    Microdata: document.getItems() must treat no token as no parameter. https://bugs.webkit.org/show_bug.cgi?id=93071
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore: 
    
    document.getItems() must return all top-level microdata items in the
    document when no tokens specified in the argument.
    Spec: http://www.whatwg.org/specs/web-apps/current-work/multipage/microdata.html#dom-document-getitems
    
    Test: fast/dom/MicroData/getitems-empty-string-as-arg.html
    
    * dom/Document.cpp:
    (WebCore::Document::getItems): Create an empty string identifier when typeNames is null string
    to map such request in the cache.
    * dom/MicroDataItemList.cpp:
    (WebCore::MicroDataItemList::undefinedItemType): Changed undefinedItemType() to empty string.
    (WebCore::MicroDataItemList::nodeMatches): Return true if m_typeNames contains no token.
    
    LayoutTests: 
    
    Added test to verify that document.getItems() returns all top-level
    microdata items in the document when no tokens specified in the argement.
    
    * fast/dom/MicroData/getitems-empty-string-as-arg-expected.txt: Added.
    * fast/dom/MicroData/getitems-empty-string-as-arg.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/MicroDataItemList.cpp
+++ third_party/WebKit/Source/WebCore/dom/MicroDataItemList.cpp
@@ -41,7 +41,7 @@ using namespace HTMLNames;
 
 const String& MicroDataItemList::undefinedItemType()
 {
-    DEFINE_STATIC_LOCAL(String, undefinedItemTypeString, ("http://webkit.org/microdata/undefinedItemType"));
+    DEFINE_STATIC_LOCAL(String, undefinedItemTypeString, (""));
     return undefinedItemTypeString;
 }
 
@@ -66,7 +66,7 @@ bool MicroDataItemList::nodeMatches(Element* testNode) const
     if (!testElement->fastHasAttribute(itemscopeAttr) || testElement->fastHasAttribute(itempropAttr))
         return false;
 
-    if (m_originalTypeNames == undefinedItemType())
+    if (!m_typeNames.size())
         return true;
 
     return testElement->itemType()->tokens().containsAll(m_typeNames);
