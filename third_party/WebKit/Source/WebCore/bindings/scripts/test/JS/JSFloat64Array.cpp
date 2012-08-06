5a8832ba0be7ad4ae42def33f98fe113258280b8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-06 12:02:34 +0000

    [JSC] Remove custom JSBindings for constructArrayBufferView() https://bugs.webkit.org/show_bug.cgi?id=93097
    Patch by Vineet Chaudhary <rgf748@motorola.com> on 2012-08-06
    Reviewed by Kentaro Hara.
    
    Currently TypedArray files have the custom implementation of constructArrayBufferView().
    We can can remove these modifying CodeGeneratorJS.pm
    
    Tests: TestTypedArray.idl
    
    * bindings/js/JSFloat32ArrayCustom.cpp: Removed custom call constructArrayBufferView().
    * bindings/js/JSFloat64ArrayCustom.cpp: Ditto.
    * bindings/js/JSInt16ArrayCustom.cpp: Ditto.
    * bindings/js/JSInt32ArrayCustom.cpp: Ditto.
    * bindings/js/JSUint16ArrayCustom.cpp: Ditto.
    * bindings/js/JSUint32ArrayCustom.cpp: Ditto.
    * bindings/scripts/CodeGeneratorJS.pm: Modified Generator code to generate bindings for
    constructArrayBufferView()
    (GenerateConstructorDefinition):
    * bindings/scripts/IDLAttributes.txt: Added [TypedArray] attribute.
    * bindings/scripts/test/JS/JSFloat64Array.cpp: Tests result.
    (WebCore::JSFloat64ArrayConstructor::getConstructData):
    (WebCore::JSFloat64ArrayConstructor::constructJSFloat64Array):
    * bindings/scripts/test/TestTypedArray.idl: Test.
    * html/canvas/Float32Array.idl: Using [TypedArray].
    * html/canvas/Float64Array.idl: Ditto.
    * html/canvas/Int16Array.idl: Ditto.
    * html/canvas/Int32Array.idl: Ditto.
    * html/canvas/Uint16Array.idl: Ditto.
    * html/canvas/Uint32Array.idl: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSFloat64Array.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSFloat64Array.cpp
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSFloat64Array.h"
 
 #include "ExceptionCode.h"
+#include "JSArrayBufferViewHelper.h"
 #include "JSDOMBinding.h"
 #include "JSFloat32Array.h"
 #include "JSInt32Array.h"
@@ -83,6 +84,16 @@ ConstructType JSFloat64ArrayConstructor::getConstructData(JSCell*, ConstructData
 {
     constructData.native.function = constructJSFloat64Array;
     return ConstructTypeHost;
+}
+
+EncodedJSValue JSC_HOST_CALL JSFloat64ArrayConstructor::constructJSFloat64Array(ExecState* exec)
+{
+    JSFloat64ArrayConstructor* jsConstructor = jsCast<JSFloat64ArrayConstructor*>(exec->callee());
+    RefPtr<Float64Array> array = constructArrayBufferView<Float64Array, double>(exec);
+    if (!array.get())
+        // Exception has already been thrown.
+        return JSValue::encode(JSValue());
+    return JSValue::encode(asObject(toJS(exec, jsConstructor->globalObject(), array.get())));
 }
 
 /* Hash table for prototype */
