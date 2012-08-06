7f8cee63bef5c771595e9c41275a166011d9a271
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 16:35:01 +0000

    Add message to Ash message center for no notifications
    Also hides tray before login.
    
    BUG=137155
    TBR=sky@chromium.org (for ash_strings.grd)
    
    Review URL: https://chromiumcodereview.appspot.com/10834164
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150101 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -326,6 +326,7 @@ void StatusAreaWidget::AddSystemTray(SystemTray* system_tray,
     system_tray_delegate_.reset(new DummySystemTrayDelegate());
 
   system_tray->CreateItems();  // Called after delegate is created.
+  UpdateAfterLoginStatusChange(system_tray_delegate_->GetUserLoginStatus());
 }
 
 void StatusAreaWidget::AddWebNotificationTray(
@@ -368,6 +369,8 @@ bool StatusAreaWidget::ShouldShowNonSystemNotifications() {
 
 void StatusAreaWidget::UpdateAfterLoginStatusChange(
     user::LoginStatus login_status) {
+  if (login_status_ == login_status)
+    return;
   login_status_ = login_status;
   if (system_tray_)
     system_tray_->UpdateAfterLoginStatusChange(login_status);
