5323f1322ccb6399eda493c37f084197f7acf3b6
pdr@google.com <pdr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-06 06:31:39 +0000

    Fix assertion during detach of SVG wrappers without baseVal https://bugs.webkit.org/show_bug.cgi?id=93063
    Reviewed by Nikolas Zimmermann.
    
    Source/WebCore:
    
    r131583 introduced a change where SVGAnimatedListPropertyTearOff required
    a baseVal to be set before detaching wrappers. This caused an assertion
    to be hit if no baseVal was set.
    This patch changes this behavior so that wrappers are detached even if
    no baseVal is set.
    
    Test: svg/animations/dynamic-modify-transform-without-baseval.html
    
    * svg/properties/SVGAnimatedListPropertyTearOff.h:
    (WebCore::SVGAnimatedListPropertyTearOff::detachListWrappers):
    * svg/properties/SVGListProperty.h:
    (WebCore::SVGListProperty::detachListWrappersAndResize): Extracted this static method for detaching wrappers without needing an SVGListProperty.
    (SVGListProperty):
    (WebCore::SVGListProperty::detachListWrappers):
    
    LayoutTests:
    
    * svg/animations/dynamic-modify-transform-without-baseval-expected.txt: Added.
    * svg/animations/dynamic-modify-transform-without-baseval.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/properties/SVGAnimatedListPropertyTearOff.h
+++ third_party/WebKit/Source/WebCore/svg/properties/SVGAnimatedListPropertyTearOff.h
@@ -67,8 +67,7 @@ public:
 
     void detachListWrappers(unsigned newListSize)
     {
-        if (m_baseVal)
-            static_cast<ListProperty*>(m_baseVal.get())->detachListWrappers(newListSize);
+        ListProperty::detachListWrappersAndResize(&m_wrappers, newListSize);
     }
 
     PropertyType& currentAnimatedValue()
