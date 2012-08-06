f60171c21ad749f4bc8662dfca089c1cde0246ea
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 21:48:04 +0000

    chromeos: Open app using profile returned from GetProfileForNewWindows.
    BUG=138860
    TEST=Verify fix for 138860 and in non-guest mode apps run under the same profile as before.
    
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10829194
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/views/ash/launcher/chrome_launcher_controller.cc
@@ -305,7 +305,9 @@ void ChromeLauncherController::OpenAppID(
   } else {
     const Extension* extension =
         profile_->GetExtensionService()->GetInstalledExtension(app_id);
-    extension_utils::OpenExtension(profile_, extension, event_flags);
+    extension_utils::OpenExtension(GetProfileForNewWindows(),
+                                   extension,
+                                   event_flags);
   }
 }
 
