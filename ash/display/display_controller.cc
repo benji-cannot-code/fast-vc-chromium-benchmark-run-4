5928ee33b3bedb33fbe04641b36b8af315e5a6ee
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 22:53:13 +0000

    This is a verbatim revert of https://chromiumcodereview.appspot.com/10800095/ (except for file ash/accelerators/accelerator_table.cc which was an unrelated fix)
    BUG=140822
    
    Review URL: https://chromiumcodereview.appspot.com/10827184
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150161 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_controller.cc
+++ ash/display/display_controller.cc
@@ -23,6 +23,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 namespace internal {
+namespace {
+// True if the extended desktop mode is enabled.
+bool extended_desktop_enabled = false;
+}  // namespace
 
 DisplayController::DisplayController()
     : secondary_display_layout_(RIGHT),
@@ -208,10 +212,14 @@ void DisplayController::OnDisplayRemoved(const gfx::Display& display) {
 
 // static
 bool DisplayController::IsExtendedDesktopEnabled(){
-  static bool extended_desktop_disabled =
+  return extended_desktop_enabled ||
       CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kAshExtendedDesktopDisabled);
-  return !extended_desktop_disabled;
+          switches::kAshExtendedDesktop);
+}
+
+// static
+void DisplayController::SetExtendedDesktopEnabled(bool enabled) {
+  extended_desktop_enabled = enabled;
 }
 
 aura::RootWindow* DisplayController::AddRootWindowForDisplay(
