55df5c0b478ddb3219c020be511e273715e3bd66
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 15:36:10 +0000

    Add WebSocketError to indicate decoding failure reason
    BUG=none
    TEST=net_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10824081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150095 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/websockets/websocket_errors.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_WEBSOCKETS_WEBSOCKET_ERRORS_H_
+#define NET_WEBSOCKETS_WEBSOCKET_ERRORS_H_
+
+#include "net/base/net_errors.h"
+
+namespace net {
+
+// Error values for WebSocket framing.
+// This values are compatible to RFC6455 defined status codes.
+enum WebSocketError {
+  WEB_SOCKET_OK = 1000,
+  WEB_SOCKET_ERR_PROTOCOL_ERROR = 1002,
+  WEB_SOCKET_ERR_MESSAGE_TOO_BIG = 1009
+};
+
+// Convert WebSocketError to net::Error defined in net/base/net_errors.h.
+Error WebSocketErrorToNetError(WebSocketError error);
+
+}  // namespace net
+
+#endif  // NET_WEBSOCKETS_WEBSOCKET_FRAME_H_
