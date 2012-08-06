f5d66adfed9b286705a77310df9aa9bde02ce952
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-06 13:03:42 +0000

    [Qt][WK2] Remove workarounds from input event handling https://bugs.webkit.org/show_bug.cgi?id=93125
    Reviewed by Jocelyn Turcotte.
    
    Since input event propagation in Qt5 has been fixed we can remove
    the workarounds introduced to prevent the QML WebView from receiving
    input events targeting a child dialog.
    To do this the childMouseEventFilter function has to be changed to
    force touch and mouse events through the default propagation path.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewPrivate::QQuickWebViewPrivate):
    (QQuickWebViewLegacyPrivate::initialize):
    (QQuickWebView::childMouseEventFilter):
    (QQuickWebView::touchEvent):
    * UIProcess/API/qt/qquickwebview_p_p.h:
    (QQuickWebViewPrivate):
    (QQuickWebViewLegacyPrivate):
    * UIProcess/qt/QtDialogRunner.cpp:
    (WebKit::QtDialogRunner::run):
    * UIProcess/qt/WebColorChooserProxyQt.cpp:
    (WebKit::WebColorChooserProxyQt::createItem):
    (WebKit::WebColorChooserProxyQt::endChooser):
    * UIProcess/qt/WebPopupMenuProxyQt.cpp:
    (WebKit::WebPopupMenuProxyQt::showPopupMenu):
    (WebKit::WebPopupMenuProxyQt::hidePopupMenu):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124758 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qquickwebview_p_p.h
@@ -72,9 +72,6 @@ public:
 
     virtual void onComponentComplete();
 
-    virtual void enableMouseEvents() { }
-    virtual void disableMouseEvents() { }
-
     virtual void provisionalLoadDidStart(const WTF::String& url);
     virtual void didReceiveServerRedirectForProvisionalLoad(const WTF::String& url);
     virtual void loadDidCommit();
@@ -125,8 +122,6 @@ public:
 
     QPointF contentPos() const;
     void setContentPos(const QPointF&);
-
-    void setDialogActive(bool active) { m_dialogActive = active; }
 
     void updateIcon();
 
@@ -195,7 +190,6 @@ protected:
     bool m_useDefaultContentItemSize;
     bool m_navigatorQtObjectEnabled;
     bool m_renderToOffscreenBuffer;
-    bool m_dialogActive;
     bool m_allowAnyHTTPSCertificateForLocalHost;
     WTF::String m_iconUrl;
     int m_loadProgress;
@@ -209,8 +203,6 @@ public:
     virtual void initialize(WKContextRef contextRef = 0, WKPageGroupRef pageGroupRef = 0);
 
     virtual void updateViewportSize();
-    virtual void enableMouseEvents();
-    virtual void disableMouseEvents();
 
     qreal zoomFactor() const;
     void setZoomFactor(qreal);
--- third_party/WebKit/Source/WebKit2/UIProcess/qt/WebColorChooserProxyQt.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/WebColorChooserProxyQt.cpp
@@ -96,7 +96,6 @@ void WebColorChooserProxyQt::createItem(QObject* contextObject)
     m_colorChooser->setParentItem(m_webView);
 
     component->completeCreate();
-    QQuickWebViewPrivate::get(m_webView)->setDialogActive(true);
 }
 
 void WebColorChooserProxyQt::createContext(QQmlComponent* component, QObject* contextObject)
@@ -134,8 +133,6 @@ void WebColorChooserProxyQt::notifyColorSelected(const QColor& color)
 
 void WebColorChooserProxyQt::endChooser()
 {
-    QQuickWebViewPrivate::get(m_webView)->setDialogActive(false);
-
     m_colorChooser.clear();
     m_context.clear();
 
