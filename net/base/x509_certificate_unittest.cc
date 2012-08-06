7687988cc209b4fa60eac64e95b67a8c488af8f7
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-06 19:04:21 +0000

    Implement SHA-256 fingerprint support.
    The HTTP-based Public Key Pinning Internet Draft
    (tools.ietf.org/html/draft-ietf-websec-key-pinning) requires this.
    
    Per wtc, give the *Fingeprint* types more meaningful *HashValue* names.
    
    Cleaning up lint along the way.
    
    This CL reverts 149268, which reverted 149261 the previous version of this
    CL. It includes a fix to the compile problem that necessitated 149268.
    
    BUG=117914
    TEST=net_unittests, unit_tests TransportSecurityPersisterTest
    Review URL: https://chromiumcodereview.appspot.com/10836062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150124 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/x509_certificate_unittest.cc
+++ net/base/x509_certificate_unittest.cc
@@ -187,7 +187,7 @@ void CheckGoogleCert(const scoped_refptr<X509Certificate>& google_cert,
   const Time& valid_expiry = google_cert->valid_expiry();
   EXPECT_EQ(valid_to, valid_expiry.ToDoubleT());
 
-  const SHA1Fingerprint& fingerprint = google_cert->fingerprint();
+  const SHA1HashValue& fingerprint = google_cert->fingerprint();
   for (size_t i = 0; i < 20; ++i)
     EXPECT_EQ(expected_fingerprint[i], fingerprint.data[i]);
 
@@ -244,7 +244,7 @@ TEST(X509CertificateTest, WebkitCertParsing) {
   const Time& valid_expiry = webkit_cert->valid_expiry();
   EXPECT_EQ(1300491319, valid_expiry.ToDoubleT());  // Mar 18 23:35:19 2011 GMT
 
-  const SHA1Fingerprint& fingerprint = webkit_cert->fingerprint();
+  const SHA1HashValue& fingerprint = webkit_cert->fingerprint();
   for (size_t i = 0; i < 20; ++i)
     EXPECT_EQ(webkit_fingerprint[i], fingerprint.data[i]);
 
@@ -299,7 +299,7 @@ TEST(X509CertificateTest, ThawteCertParsing) {
   const Time& valid_expiry = thawte_cert->valid_expiry();
   EXPECT_EQ(1263772799, valid_expiry.ToDoubleT());  // Jan 17 23:59:59 2010 GMT
 
-  const SHA1Fingerprint& fingerprint = thawte_cert->fingerprint();
+  const SHA1HashValue& fingerprint = thawte_cert->fingerprint();
   for (size_t i = 0; i < 20; ++i)
     EXPECT_EQ(thawte_fingerprint[i], fingerprint.data[i]);
 
@@ -889,7 +889,7 @@ TEST_P(X509CertificateParseTest, CanParseFormat) {
     // Compare the parsed certificate with the expected certificate, by
     // comparing fingerprints.
     const X509Certificate* cert = certs[i];
-    const SHA1Fingerprint& actual_fingerprint = cert->fingerprint();
+    const SHA1HashValue& actual_fingerprint = cert->fingerprint();
     uint8* expected_fingerprint = test_data_.chain_fingerprints[i];
 
     for (size_t j = 0; j < 20; ++j)
