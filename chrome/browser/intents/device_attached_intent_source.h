3c7f2a7d4cef8786fe312b27c78e5557cd3acf0d
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 01:17:02 +0000

    Show device attach web intent picker dialog only if we have registered services.
    BUG=140093
    TEST=none
    TBR=gbillock@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10831147
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intents/device_attached_intent_source.h
+++ chrome/browser/intents/device_attached_intent_source.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_INTENTS_DEVICE_ATTACHED_INTENT_SOURCE_H_
 #define CHROME_BROWSER_INTENTS_DEVICE_ATTACHED_INTENT_SOURCE_H_
 
+#include "base/memory/weak_ptr.h"
 #include "base/system_monitor/system_monitor.h"
 
 class Browser;
@@ -22,7 +23,8 @@ class WebContentsDelegate;
 //   root_path = the File Path at which the device is accessible
 //   filesystem_id = registered isolated file system identifier
 class DeviceAttachedIntentSource
-    : public base::SystemMonitor::DevicesChangedObserver {
+    : public base::SystemMonitor::DevicesChangedObserver,
+      public base::SupportsWeakPtr<DeviceAttachedIntentSource> {
  public:
   DeviceAttachedIntentSource(Browser* browser,
                              content::WebContentsDelegate* delegate);
@@ -35,6 +37,11 @@ class DeviceAttachedIntentSource
       base::SystemMonitor::MediaDeviceType type,
       const FilePath::StringType& location) OVERRIDE;
   virtual void OnMediaDeviceDetached(const std::string& id) OVERRIDE;
+
+  // Dispatches web intents for the attached media device specified by
+  // |device_info|.
+  void DispatchIntentsForService(
+      const base::SystemMonitor::MediaDeviceInfo& device_info);
 
  private:
   typedef std::map<std::string, base::SystemMonitor::MediaDeviceInfo>
