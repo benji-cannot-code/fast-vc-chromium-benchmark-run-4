f7233873d934b2bcb37faf0afd36895e8543b147
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 01:41:40 +0000

    Add support for PNG representation in gfx::Image
    Currently all conversions to or from PNG will request (and so possibly
    create and cache) an ImageSkia intermediate representation.
    
    Depends on http://codereview.chromium.org/10799014/
    
    BUG=123865
    
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/10799014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tests/ui_gfx_image_unittest.mm
+++ chrome/browser/ui/tests/ui_gfx_image_unittest.mm
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -31,8 +31,8 @@ TEST_F(UiGfxImageTest, CheckColor) {
   CGFloat components[4] = { 0 };
   [color getComponents:components];
 
-  EXPECT_GT(components[0], 0.95);
-  EXPECT_LT(components[1], 0.05);
+  EXPECT_LT(components[0], 0.05);
+  EXPECT_GT(components[1], 0.95);
   EXPECT_LT(components[2], 0.05);
   EXPECT_GT(components[3], 0.95);
 }
