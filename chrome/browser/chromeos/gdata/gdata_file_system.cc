885e4bda4ac3ba11bf0f95fa55b189cd667997b7
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 23:44:50 +0000

    Simplify implementation of FindEntryByPathSync
    In addition, RemoveEntry and RemoveChild now return void.
    
    BUG=137725
    TEST=unit tests.
    Review URL: https://chromiumcodereview.appspot.com/10825218
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system.cc
@@ -2861,8 +2861,7 @@ GDataFileError GDataFileSystem::RemoveEntryFromGData(
     *resource_id = entry->AsGDataFile()->resource_id();
 
   GDataDirectory* parent_dir = entry->parent();
-  if (!parent_dir->RemoveEntry(entry))
-    return GDATA_FILE_ERROR_NOT_FOUND;
+  parent_dir->RemoveEntry(entry);
 
   FOR_EACH_OBSERVER(GDataFileSystemInterface::Observer, observers_,
                     OnDirectoryChanged(parent_dir->GetFilePath()));
