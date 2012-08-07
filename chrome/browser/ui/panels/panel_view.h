5484c6e1f35c644ca2ecbf9fd526754b85857b57
kaiwang@chromium.org <kaiwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 21:50:23 +0000

    Revert 150420 - Accelerator support for browserless panels on Windows.
    BUG=127323
    TEST=New test
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825210
    
    TBR=jianli@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10827206
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150425 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_view.h
+++ chrome/browser/ui/panels/panel_view.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_PANELS_PANEL_VIEW_H_
 #define CHROME_BROWSER_UI_PANELS_PANEL_VIEW_H_
 
-#include <map>
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/ui/panels/native_panel.h"
 #include "ui/base/animation/animation_delegate.h"
@@ -192,9 +191,6 @@ class PanelView : public NativePanel,
   // The last view that had focus in the panel. This is saved so that focus can
   // be restored properly when a drag ends.
   views::View* old_focused_view_;
-
-  // A mapping between accelerators and commands.
-  static std::map<ui::Accelerator, int> accelerator_table_;
 
 #if defined(OS_WIN) && !defined(USE_ASH) && !defined(USE_AURA)
   // Used to provide custom taskbar thumbnail for Windows 7 and later.
