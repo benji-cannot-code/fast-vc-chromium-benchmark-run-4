e34b003411c15746c9972f36e05f024a75d8be96
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 02:18:00 +0000

    app_list: Fix the header include guards.
    BUG=125846
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10823197
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/app_list/extension_app_item.h
+++ chrome/browser/ui/ash/app_list/extension_app_item.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_ASH_APP_LIST_EXTENSION_APP_ITEM_H_
-#define CHROME_BROWSER_UI_VIEWS_ASH_APP_LIST_EXTENSION_APP_ITEM_H_
+#ifndef CHROME_BROWSER_UI_ASH_APP_LIST_EXTENSION_APP_ITEM_H_
+#define CHROME_BROWSER_UI_ASH_APP_LIST_EXTENSION_APP_ITEM_H_
 
 #include <string>
 
@@ -72,4 +72,4 @@ class ExtensionAppItem : public ChromeAppListItem,
   DISALLOW_COPY_AND_ASSIGN(ExtensionAppItem);
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_ASH_APP_LIST_EXTENSION_APP_ITEM_H_
+#endif  // CHROME_BROWSER_UI_ASH_APP_LIST_EXTENSION_APP_ITEM_H_
