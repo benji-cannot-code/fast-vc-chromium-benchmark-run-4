b2658c7822505ed0b6b7a89bd0805dd21b9ea697
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 21:04:17 +0000

    Clip the bitmap for creating the thumbnail on GPU when accelerated compositing is active.
    BUG=118571
    
    
    Review URL: https://chromiumcodereview.appspot.com/10831063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/thumbnail_generator.h
+++ chrome/browser/tab_contents/thumbnail_generator.h
@@ -46,6 +46,8 @@ class ThumbnailGenerator : public content::NotificationObserver,
   // The result of clipping. This can be used to determine if the
   // generated thumbnail is good or not.
   enum ClipResult {
+    // Clipping is not done yet.
+    kUnprocessed,
     // The source image is smaller.
     kSourceIsSmaller,
     // Wider than tall by twice or more, clip horizontally.
@@ -114,7 +116,7 @@ class ThumbnailGenerator : public content::NotificationObserver,
   // Update the thumbnail of the given tab.
   void UpdateThumbnail(content::WebContents* web_contents,
                        const SkBitmap& bitmap,
-                       const ThumbnailGenerator::ClipResult& clip_result);
+                       const ClipResult& clip_result);
 
   // Returns true if we should update the thumbnail of the given URL.
   static bool ShouldUpdateThumbnail(Profile* profile,
@@ -142,12 +144,14 @@ class ThumbnailGenerator : public content::NotificationObserver,
   // AsyncUpdateThumbnail invocation. This runs on the UI thread.
   void UpdateThumbnailWithBitmap(
       content::WebContents* web_contents,
+      ClipResult clip_result,
       const SkBitmap& bitmap);
 
   // Called when the canvas for generating a thumbnail is ready after the
   // AsyncUpdateThumbnail invocation. This runs on the UI thread.
   void UpdateThumbnailWithCanvas(
       content::WebContents* web_contents,
+      ClipResult clip_result,
       skia::PlatformCanvas* temp_canvas,
       bool result);
 
