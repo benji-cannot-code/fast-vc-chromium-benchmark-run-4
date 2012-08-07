360a45796a9a97135270c1c609a3047185c52dda
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 04:08:28 +0000

    Copy the !details.is_in_page restriction from TabHelper to PageActionController::DidNavigateMainFrame
    Oops, r145054 forgot to do this.
    
    BUG=139410
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834184
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/page_action_controller.cc
+++ chrome/browser/extensions/page_action_controller.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_set.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "content/public/browser/invalidate_type.h"
+#include "content/public/browser/navigation_details.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/web_contents.h"
 
@@ -87,6 +88,9 @@ void PageActionController::NotifyChange() {
 void PageActionController::DidNavigateMainFrame(
     const content::LoadCommittedDetails& details,
     const content::FrameNavigateParams& params) {
+  if (details.is_in_page)
+    return;
+
   const std::vector<ExtensionAction*> current_actions = GetCurrentActions();
 
   if (current_actions.empty())
