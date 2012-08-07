43d690bbc2fa4f7078e8468674c8bffef85e89cf
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 11:47:47 +0000

    Revert r150326 - "Breakpad support for AddressSanitizer."
    Causes compile failures on the official builders.
    
    BUG=None
    TBR=glider@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10836131
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150331 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/crash_handler_host_linux.h
+++ chrome/browser/crash_handler_host_linux.h
@@ -93,10 +93,6 @@ class CrashHandlerHostLinux : public MessageLoopForIO::Watcher,
   bool shutting_down_;
 #endif
 
-#if defined(ADDRESS_SANITIZER)
-  char* asan_report_str_;
-#endif
-
   DISALLOW_COPY_AND_ASSIGN(CrashHandlerHostLinux);
 };
 
