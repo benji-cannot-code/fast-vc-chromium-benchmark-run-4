7ff0b38963d46948112bae127aa4fb8db081ee86
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 07:11:43 +0000

    Showing the promo on release channel only.
    BUG=138954
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10829192
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_private_api.cc
+++ chrome/browser/chromeos/extensions/file_browser_private_api.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/views/select_file_dialog_extension.h"
 #include "chrome/browser/ui/webui/extensions/extension_icon_source.h"
+#include "chrome/common/chrome_version_info.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_icon_set.h"
 #include "chrome/common/extensions/file_browser_handler.h"
@@ -1723,6 +1724,9 @@ bool FileDialogStringsFunction::RunImpl() {
   if (!provider->GetMachineStatistic(kMachineInfoBoard, &board))
     board = "unknown";
   dict->SetString(kMachineInfoBoard, board);
+
+  dict->SetString("BROWSER_VERSION_MODIFIER",
+                  chrome::VersionInfo::GetVersionStringModifier());
 
   return true;
 }
