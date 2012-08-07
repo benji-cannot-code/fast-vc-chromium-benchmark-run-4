9db205d1cf4f43516a98ca8ab89058f8a04cace3
motek@chromium.org <motek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 17:21:12 +0000

    The updated (simplified) look of set-as-default dialog on Windows8.
    BUG=140357
    
    Review URL: https://chromiumcodereview.appspot.com/10837084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150365 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/set_as_default_browser.js
+++ chrome/browser/resources/set_as_default_browser.js
@@ -11,10 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     $('launch-button').addEventListener('click', function() {
       chrome.send('SetAsDefaultBrowser:LaunchSetDefaultBrowserFlow');
     });
-
-    $('return-button').addEventListener('click', function() {
-      chrome.send('SetAsDefaultBrowser:ReturnToBrowser');
-    });
   }
 
   window.addEventListener('DOMContentLoaded', wireUpWindow);
