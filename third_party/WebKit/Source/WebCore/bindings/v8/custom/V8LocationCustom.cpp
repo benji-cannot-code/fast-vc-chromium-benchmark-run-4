06a0b576bd87645655922ba16db574fc4f86a875
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-07 04:08:10 +0000

    BindingSecurity::shouldAllowAccessToFrame shouldn't use a raw boolean parameter https://bugs.webkit.org/show_bug.cgi?id=93323
    Reviewed by Eric Seidel.
    
    As requested by Eric. This patch also renames the function to match its
    counterpart in the JavaScriptCore bindings.
    
    * bindings/generic/BindingSecurity.cpp:
    (WebCore::canAccessDocument):
    (WebCore::BindingSecurity::shouldAllowAccessToFrame):
    (WebCore::BindingSecurity::shouldAllowAccessToNode):
    (WebCore::BindingSecurity::allowSettingFrameSrcToJavascriptUrl):
    * bindings/generic/BindingSecurity.h:
    (BindingSecurity):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateDomainSafeFunctionGetter):
    (GenerateReplaceableAttrSetter):
    (GenerateFunctionCallback):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.cpp:
    (WebCore::TestActiveDOMObjectV8Internal::excitingFunctionCallback):
    (WebCore::TestActiveDOMObjectV8Internal::postMessageAttrGetter):
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::canAccessFromCurrentOrigin):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::WindowSetTimeoutImpl):
    (WebCore::V8DOMWindow::eventAccessorGetter):
    (WebCore::V8DOMWindow::eventAccessorSetter):
    (WebCore::V8DOMWindow::openerAccessorSetter):
    (WebCore::V8DOMWindow::addEventListenerCallback):
    (WebCore::V8DOMWindow::removeEventListenerCallback):
    (WebCore::V8DOMWindow::showModalDialogCallback):
    (WebCore::V8DOMWindow::openCallback):
    (WebCore::V8DOMWindow::namedSecurityCheck):
    (WebCore::V8DOMWindow::indexedSecurityCheck):
    * bindings/v8/custom/V8HistoryCustom.cpp:
    (WebCore::V8History::indexedSecurityCheck):
    (WebCore::V8History::namedSecurityCheck):
    * bindings/v8/custom/V8InjectedScriptManager.cpp:
    (WebCore::InjectedScriptManager::canAccessInspectedWindow):
    * bindings/v8/custom/V8LocationCustom.cpp:
    (WebCore::V8Location::reloadAccessorGetter):
    (WebCore::V8Location::replaceAccessorGetter):
    (WebCore::V8Location::assignAccessorGetter):
    (WebCore::V8Location::toStringCallback):
    (WebCore::V8Location::indexedSecurityCheck):
    (WebCore::V8Location::namedSecurityCheck):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8LocationCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8LocationCustom.cpp
@@ -157,7 +157,7 @@ v8::Handle<v8::Value> V8Location::reloadAccessorGetter(v8::Local<v8::String> nam
         return privateTemplate->GetFunction();
     }
     Location* imp = V8Location::toNative(holder);
-    if (!BindingSecurity::canAccessFrame(BindingState::instance(), imp->frame(), false)) {
+    if (!BindingSecurity::shouldAllowAccessToFrame(BindingState::instance(), imp->frame(), DoNotReportSecurityError)) {
         static v8::Persistent<v8::FunctionTemplate> sharedTemplate = v8::Persistent<v8::FunctionTemplate>::New(v8::FunctionTemplate::New(V8Location::reloadCallback, v8::Handle<v8::Value>(), v8::Signature::New(V8Location::GetRawTemplate())));
         return sharedTemplate->GetFunction();
     }
@@ -175,7 +175,7 @@ v8::Handle<v8::Value> V8Location::replaceAccessorGetter(v8::Local<v8::String> na
         return privateTemplate->GetFunction();
     }
     Location* imp = V8Location::toNative(holder);
-    if (!BindingSecurity::canAccessFrame(BindingState::instance(), imp->frame(), false)) {
+    if (!BindingSecurity::shouldAllowAccessToFrame(BindingState::instance(), imp->frame(), DoNotReportSecurityError)) {
         static v8::Persistent<v8::FunctionTemplate> sharedTemplate = v8::Persistent<v8::FunctionTemplate>::New(v8::FunctionTemplate::New(V8Location::replaceCallback, v8::Handle<v8::Value>(), v8::Signature::New(V8Location::GetRawTemplate())));
         return sharedTemplate->GetFunction();
     }
@@ -194,7 +194,7 @@ v8::Handle<v8::Value> V8Location::assignAccessorGetter(v8::Local<v8::String> nam
         return privateTemplate->GetFunction();
     }
     Location* imp = V8Location::toNative(holder);
-    if (!BindingSecurity::canAccessFrame(BindingState::instance(), imp->frame(), false)) {
+    if (!BindingSecurity::shouldAllowAccessToFrame(BindingState::instance(), imp->frame(), DoNotReportSecurityError)) {
         static v8::Persistent<v8::FunctionTemplate> sharedTemplate = v8::Persistent<v8::FunctionTemplate>::New(v8::FunctionTemplate::New(V8Location::assignCallback, v8::Handle<v8::Value>(), v8::Signature::New(V8Location::GetRawTemplate())));
         return sharedTemplate->GetFunction();
     }
@@ -252,7 +252,7 @@ v8::Handle<v8::Value> V8Location::toStringCallback(const v8::Arguments& args)
     INC_STATS("DOM.Location.toString");
     v8::Handle<v8::Object> holder = args.Holder();
     Location* imp = V8Location::toNative(holder);
-    if (!BindingSecurity::canAccessFrame(BindingState::instance(), imp->frame(), true))
+    if (!BindingSecurity::shouldAllowAccessToFrame(BindingState::instance(), imp->frame()))
         return v8::Undefined();
     String result = imp->href();
     return v8String(result, args.GetIsolate());
@@ -262,14 +262,14 @@ bool V8Location::indexedSecurityCheck(v8::Local<v8::Object> host, uint32_t index
 {
     // Only allow same origin access
     Location* imp =  V8Location::toNative(host);
-    return BindingSecurity::canAccessFrame(BindingState::instance(), imp->frame(), false);
+    return BindingSecurity::shouldAllowAccessToFrame(BindingState::instance(), imp->frame(), DoNotReportSecurityError);
 }
 
 bool V8Location::namedSecurityCheck(v8::Local<v8::Object> host, v8::Local<v8::Value> key, v8::AccessType type, v8::Local<v8::Value>)
 {
     // Only allow same origin access
     Location* imp = V8Location::toNative(host);
-    return BindingSecurity::canAccessFrame(BindingState::instance(), imp->frame(), false);
+    return BindingSecurity::shouldAllowAccessToFrame(BindingState::instance(), imp->frame(), DoNotReportSecurityError);
 }
 
 v8::Handle<v8::Value> toV8(Location* impl, v8::Isolate* isolate)
