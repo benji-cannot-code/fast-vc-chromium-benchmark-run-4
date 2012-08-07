cec08c4851b27400c71c0cddd727a9cea3d9137a
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 02:20:26 +0000

    Add GL_CHROMIUM_texture_mailbox entrypoints to wgc3d implementations
    BUG=131275
    
    
    Review URL: https://chromiumcodereview.appspot.com/10829209
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150241 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
+++ content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
@@ -42,6 +42,7 @@ class GLES2Implementation;
 
 using WebKit::WebGLId;
 
+using WebKit::WGC3Dbyte;
 using WebKit::WGC3Dchar;
 using WebKit::WGC3Denum;
 using WebKit::WGC3Dboolean;
@@ -248,7 +249,6 @@ class WebGraphicsContext3DCommandBufferImpl
   virtual void enableVertexAttribArray(WGC3Duint index);
   virtual void finish();
   virtual void flush();
-  virtual void shallowFlushCHROMIUM();
   virtual void framebufferRenderbuffer(WGC3Denum target,
                                        WGC3Denum attachment,
                                        WGC3Denum renderbuffertarget,
@@ -574,6 +574,14 @@ class WebGraphicsContext3DCommandBufferImpl
 
   virtual void bindUniformLocationCHROMIUM(WebGLId program, WGC3Dint location,
                                            const WGC3Dchar* uniform);
+
+  virtual void shallowFlushCHROMIUM();
+
+  virtual void genMailboxCHROMIUM(WGC3Dbyte* mailbox);
+  virtual void produceTextureCHROMIUM(WGC3Denum target,
+                                      const WGC3Dbyte* mailbox);
+  virtual void consumeTextureCHROMIUM(WGC3Denum target,
+                                      const WGC3Dbyte* mailbox);
 
  protected:
   virtual GrGLInterface* onCreateGrGLInterface();
--- webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.h
+++ webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.h
@@ -27,6 +27,7 @@ class GLES2Implementation;
 
 using WebKit::WebGLId;
 
+using WebKit::WGC3Dbyte;
 using WebKit::WGC3Dchar;
 using WebKit::WGC3Denum;
 using WebKit::WGC3Dboolean;
@@ -483,6 +484,12 @@ class WebGraphicsContext3DInProcessCommandBufferImpl
                                            const WGC3Dchar* uniform);
 
   virtual void shallowFlushCHROMIUM();
+
+  virtual void genMailboxCHROMIUM(WGC3Dbyte* mailbox);
+  virtual void produceTextureCHROMIUM(WGC3Denum target,
+                                      const WGC3Dbyte* mailbox);
+  virtual void consumeTextureCHROMIUM(WGC3Denum target,
+                                      const WGC3Dbyte* mailbox);
 
  protected:
   virtual GrGLInterface* onCreateGrGLInterface();
