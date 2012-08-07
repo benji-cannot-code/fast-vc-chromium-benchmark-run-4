23e605b5c66d88548827430b2edfab0a21ed7702
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 10:53:51 +0000

    Add GetChangelist operation for Drive V2.
    This CL adds GetChangelist operation and its corresponding interface
    for DocumentsService.  No code uses this interface yet.
    
    BUG=chromium:127728
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10836125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/drive_api_operations.cc
+++ chrome/browser/chromeos/gdata/drive_api_operations.cc
@@ -5,10 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/gdata/drive_api_operations.h"
 
+#include "base/string_number_conversions.h"
+#include "chrome/common/net/url_util.h"
+
 namespace {
 
 const char kDriveV2AboutURL[] = "https://www.googleapis.com/drive/v2/about";
 const char kDriveV2ApplistURL[] = "https://www.googleapis.com/drive/v2/apps";
+const char kDriveV2ChangelistURL[] =
+    "https://www.googleapis.com/drive/v2/changes";
 
 }  // namespace
 
@@ -37,6 +42,29 @@ GetApplistOperation::~GetApplistOperation() {}
 
 GURL GetApplistOperation::GetURL() const {
   return GURL(kDriveV2ApplistURL);
+}
+
+//============================ GetChangelistOperation ==========================
+
+GetChangelistOperation::GetChangelistOperation(
+    GDataOperationRegistry* registry,
+    const GURL& url,
+    int64 start_changestamp,
+    const GetDataCallback& callback)
+    : GetDataOperation(registry, callback),
+      url_(kDriveV2ChangelistURL),
+      start_changestamp_(start_changestamp) {
+  if (!url.is_empty())
+    url_ = url;
+}
+
+GetChangelistOperation::~GetChangelistOperation() {}
+
+GURL GetChangelistOperation::GetURL() const {
+  if (start_changestamp_)
+    return chrome_common_net::AppendOrReplaceQueryParameter(
+        url_, "startChangeId", base::Int64ToString(start_changestamp_));
+  return url_;
 }
 
 }  // namespace gdata
--- chrome/browser/chromeos/gdata/mock_gdata_documents_service.h
+++ chrome/browser/chromeos/gdata/mock_gdata_documents_service.h
@@ -35,6 +35,9 @@ class MockDocumentsService : public DocumentsServiceInterface {
                                   const std::string& search_string,
                                   const std::string& directory_resource_id,
                                   const GetDataCallback& callback));
+  MOCK_METHOD3(GetChangelist, void(const GURL& feed_url,
+                                   int64 start_changestamp,
+                                   const GetDataCallback& callback));
   MOCK_METHOD2(GetDocumentEntry, void(const std::string& resource_id,
                                       const GetDataCallback& callback));
   MOCK_METHOD1(GetAccountMetadata, void(const GetDataCallback& callback));
