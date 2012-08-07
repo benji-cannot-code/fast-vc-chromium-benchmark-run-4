1fd27a3a7554a814128635fc67e237402378820c
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 00:50:51 +0000

    Don't delete print system on connector stop. It may contains cached printers list.
    BUG=139001
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10825172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/cloud_print/cloud_print_connector.h
+++ chrome/service/cloud_print/cloud_print_connector.h
@@ -159,6 +159,7 @@ class CloudPrintConnector
       const printing::PrinterCapsAndDefaults& caps_and_defaults);
 
   bool IsSamePrinter(const std::string& name1, const std::string& name2) const;
+  bool InitPrintSystem();
 
   // CloudPrintConnector client.
   Client* client_;
@@ -179,7 +180,7 @@ class CloudPrintConnector
   JobHandlerMap job_handler_map_;
   // Next response handler.
   ResponseHandler next_response_handler_;
-  // The list of peding tasks to be done in the background.
+  // The list of pending tasks to be done in the background.
   std::list<PendingTask> pending_tasks_;
   // The CloudPrintURLFetcher instance for the current request.
   scoped_refptr<CloudPrintURLFetcher> request_;
