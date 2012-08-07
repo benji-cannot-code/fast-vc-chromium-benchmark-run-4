d0c48de443030216c36287b2331e711e5af1d0fd
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-07 23:31:50 +0000

    [V8] Replace all V8 undefined values in the rest of custom bindings with v8Undefined() https://bugs.webkit.org/show_bug.cgi?id=93220
    Reviewed by Eric Seidel.
    
    We should use v8Undefined() everywhere in V8 bindings.
    This patch replaces all V8 undefined values in the rest of custom bindings
    with v8Undefined().
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8DOMStringMapCustom.cpp:
    (WebCore::V8DOMStringMap::namedPropertyQuery): Simple refactoring not related to
    this patch. Since other bindings are using 0 instead of v8::None, we should use 0 here too.
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8ScriptProfileCustom.cpp:
    (WebCore::toV8):
    * bindings/v8/custom/V8ScriptProfileNodeCustom.cpp:
    (WebCore::toV8):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.h
@@ -379,7 +379,7 @@ namespace WebCore {
     {
         if (!value->IsObject()) {
             V8Proxy::throwTypeError();
-            return v8::Local<v8::Value>();
+            return v8Undefined();
         }
 
         v8::Local<v8::Value> v8Value(v8::Local<v8::Value>::New(value));
@@ -389,7 +389,7 @@ namespace WebCore {
 
         if (lengthValue->IsUndefined() || lengthValue->IsNull()) {
             V8Proxy::throwTypeError();
-            return v8::Local<v8::Value>();
+            return v8Undefined();
         }
 
         EXCEPTION_BLOCK(uint32_t, sequenceLength, lengthValue->Int32Value());
