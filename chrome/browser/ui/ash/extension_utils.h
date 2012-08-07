260642e53676f7af2f3267f53d6316086217a273
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 16:01:45 +0000

    extension_utils: Fix include path and header guards.
    BUG=125846
    TBR=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834206
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150356 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/extension_utils.h
+++ chrome/browser/ui/ash/extension_utils.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_ASH_EXTENSION_UTILS_H_
-#define CHROME_BROWSER_UI_VIEWS_ASH_EXTENSION_UTILS_H_
+#ifndef CHROME_BROWSER_UI_ASH_EXTENSION_UTILS_H_
+#define CHROME_BROWSER_UI_ASH_EXTENSION_UTILS_H_
 
 class Profile;
 
@@ -22,4 +22,4 @@ void OpenExtension(Profile* profile,
 
 }  // namespace extension_utils
 
-#endif  // CHROME_BROWSER_UI_VIEWS_ASH_EXTENSION_UTILS_H_
+#endif  // CHROME_BROWSER_UI_ASH_EXTENSION_UTILS_H_
