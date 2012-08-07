7906b9e8dbc58f9f7afb423d9d49619e1bb8da82
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 02:05:53 +0000

    Revert 148511 - Add pin icon to the omnibar in windows 8 metro mode.
    The pin icon allows pinning and unpinning the start screen. The icon used changes depending on whether the page is currently pinned or not.
    
    BUG=129598
    TEST=Test pages can be pinned and unpinned from metro mode
    
    
    Review URL: https://chromiumcodereview.appspot.com/10800054
    
    TBR=sky@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10828193
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150229 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/metro_pin_tab_helper.cc
+++ chrome/browser/ui/metro_pin_tab_helper.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/utf_string_conversions.h"
-#include "chrome/browser/ui/metro_pinned_state_observer.h"
 #include "content/public/browser/web_contents.h"
 
 #if defined(OS_WIN)
@@ -16,8 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 MetroPinTabHelper::MetroPinTabHelper(content::WebContents* web_contents)
     : content::WebContentsObserver(web_contents),
-      is_pinned_(false),
-      observer_(NULL) {}
+      is_pinned_(false) {}
 
 MetroPinTabHelper::~MetroPinTabHelper() {}
 
@@ -43,7 +41,8 @@ void MetroPinTabHelper::TogglePinnedToStartScreen() {
     // TODO(benwells): This will update the state incorrectly if the user
     // cancels. To fix this some sort of callback needs to be introduced as
     // the pinning happens on another thread.
-    SetIsPinned(!is_pinned_);
+    is_pinned_ = !is_pinned_;
+    return;
   }
 #endif
 }
@@ -68,16 +67,9 @@ void MetroPinTabHelper::UpdatePinnedStateForCurrentURL() {
     }
 
     GURL url = web_contents()->GetURL();
-    SetIsPinned(metro_is_pinned_to_start_screen(UTF8ToUTF16(url.spec())) != 0);
+    is_pinned_ = metro_is_pinned_to_start_screen(UTF8ToUTF16(url.spec())) != 0;
     VLOG(1) << __FUNCTION__ << " with url " << UTF8ToUTF16(url.spec())
             << " result: " << is_pinned_;
   }
 #endif
-}
-
-void MetroPinTabHelper::SetIsPinned(bool is_pinned) {
-  bool was_pinned = is_pinned_;
-  is_pinned_ = is_pinned;
-  if (observer_ && is_pinned_ != was_pinned)
-    observer_->MetroPinnedStateChanged(web_contents(), is_pinned_);
 }
