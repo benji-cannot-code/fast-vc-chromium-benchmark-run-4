57d7f01f5e76c67c63483234b5667ed56c576e39
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 03:55:22 +0000

    Set locale for extension tests, keep it from affecting global state
    R=gbillock@chromium.org,mpcomplete@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10837130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150254 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service_unittest.cc
+++ chrome/browser/extensions/extension_service_unittest.cc
@@ -60,6 +60,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_l10n_util.h"
 #include "chrome/common/extensions/extension_manifest_constants.h"
 #include "chrome/common/extensions/extension_resource.h"
 #include "chrome/common/extensions/permissions/permission_set.h"
@@ -4010,6 +4011,9 @@ TEST_F(ExtensionServiceTest, ExternalPrefProvider) {
 
 // Test loading good extensions from the profile directory.
 TEST_F(ExtensionServiceTest, LoadAndRelocalizeExtensions) {
+  // Ensure we're testing in "en" and leave global state untouched.
+  extension_l10n_util::ScopedLocaleForTest testLocale("en");
+
   // Initialize the test dir with a good Preferences/extensions.
   FilePath source_install_dir = data_dir_
       .AppendASCII("l10n");
