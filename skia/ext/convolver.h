49c396c664564c8dec93c084c61637298e6aec29
tomhudson@chromium.org <tomhudson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-07 12:51:51 +0000

    Temporarily disable buggy SSE2 code.
    Review URL: https://chromiumcodereview.appspot.com/10823186
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150335 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/convolver.h
+++ skia/ext/convolver.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -16,7 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(ARCH_CPU_X86_FAMILY)
 #if defined(__x86_64__) || defined(_M_X64) || defined(__SSE2__) || _M_IX86_FP==2
 // This is where we had compiler support for SSE2 instructions.
-#define SIMD_SSE2 1
+// FIXME: Known buggy, so disabling for M22.
+// #define SIMD_SSE2 1
 #endif
 #endif
 
