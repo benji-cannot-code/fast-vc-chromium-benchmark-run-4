3f1a258330fc03c30378d65dc0a5340928c00580
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-07 20:15:26 +0000

    [BlackBerry][FullScreen] Remove the set/reset of touch modes code when entering/leaving fullscreen https://bugs.webkit.org/show_bug.cgi?id=92520 PR #184511
    Reviewed by Yong Li.
    Patch by Antonio Gomes <agomes@rim.com>
    
    Internally reviewed by Gen Mak.
    
    Remove touch mode set/reset when entering/leaving fullscreen
    for media elements through the new FULLSCREEN_API code path.
    The client now handles it.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::WebPagePrivate):
    (BlackBerry::WebKit::WebPagePrivate::enterFullScreenForElement):
    (BlackBerry::WebKit::WebPagePrivate::exitFullScreenForElement):
    * Api/WebPage_p.h:
    (WebPagePrivate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -372,9 +372,6 @@ WebPagePrivate::WebPagePrivate(WebPage* webPage, WebPageClient* client, const In
 #if ENABLE(EVENT_MODE_METATAGS)
     , m_cursorEventMode(ProcessedCursorEvents)
     , m_touchEventMode(ProcessedTouchEvents)
-#if ENABLE(FULLSCREEN_API)
-    , m_touchEventModePriorGoingFullScreen(ProcessedTouchEvents)
-#endif
 #endif
 #if ENABLE(FULLSCREEN_API) && ENABLE(VIDEO)
     , m_scaleBeforeFullScreen(-1.0)
@@ -6096,10 +6093,6 @@ void WebPagePrivate::enterFullScreenForElement(Element* element)
         m_xScrollOffsetBeforeFullScreen = scrollPosition.x();
         m_mainFrame->view()->setScrollPosition(WebCore::IntPoint(0, scrollPosition.y()));
 
-#if ENABLE(EVENT_MODE_METATAGS)
-        m_touchEventModePriorGoingFullScreen = m_touchEventMode;
-        didReceiveTouchEventMode(PureTouchEventsWithMouseConversion);
-#endif
         // The current scale can be clamped to a greater minimum scale when we relayout contents during
         // the change of the viewport size. Cache the current scale so that we can restore it when
         // leaving fullscreen. Otherwise, it is possible that we will use the wrong scale.
@@ -6134,10 +6127,6 @@ void WebPagePrivate::exitFullScreenForElement(Element* element)
             WebCore::IntPoint(m_xScrollOffsetBeforeFullScreen, scrollPosition.y()));
         m_xScrollOffsetBeforeFullScreen = -1;
 
-#if ENABLE(EVENT_MODE_METATAGS)
-        didReceiveTouchEventMode(m_touchEventModePriorGoingFullScreen);
-        m_touchEventModePriorGoingFullScreen = ProcessedTouchEvents;
-#endif
         if (m_scaleBeforeFullScreen > 0) {
             // Restore the scale when leaving fullscreen. We can't use TransformationMatrix::scale(double) here, as it
             // will multiply the scale rather than set the scale.
