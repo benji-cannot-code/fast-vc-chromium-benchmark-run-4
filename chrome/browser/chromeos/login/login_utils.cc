57cdda9da96af306212d37f485b32e65ec4a0340
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 16:01:25 +0000

    Whitelist the flag --enable-fixed-position-compositing
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10820052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -757,6 +757,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ::switches::kDisableSeccompFilterSandbox,
       ::switches::kDisableSeccompSandbox,
       ::switches::kDisableThreadedAnimation,
+      ::switches::kEnableCompositingForFixedPosition,
       ::switches::kEnableDevicePolicy,
       ::switches::kEnableGView,
       ::switches::kEnableHighDPIPDFPlugin,
