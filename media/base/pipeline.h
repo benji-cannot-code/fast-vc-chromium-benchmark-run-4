5979b6ba8562c23efeee8e3669286eb77e24e0c1
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 18:30:34 +0000

    Remove Pipeline::kEnded state as it is redundant with Pipeline::kStarted.
    Review URL: https://chromiumcodereview.appspot.com/10855041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150591 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/pipeline.h
+++ media/base/pipeline.h
@@ -81,9 +81,6 @@ class MEDIA_EXPORT PipelineStatusNotification {
 //         |                                              |
 //         V      Seek()/Stop()                           |
 //   [ Started ] -------------------------> [ Pausing (for each filter) ]
-//         |                                              ^
-//         |   OnRendererEnded()             Seek()/Stop()    |
-//         `-------------> [ Ended ] ---------------------'
 //                                                        ^  SetError()
 //                                                        |
 //                                         [ Any State Other Than InitXXX ]
@@ -247,7 +244,6 @@ class MEDIA_EXPORT Pipeline
     kFlushing,
     kStarting,
     kStarted,
-    kEnded,
     kStopping,
     kStopped,
     kError,
