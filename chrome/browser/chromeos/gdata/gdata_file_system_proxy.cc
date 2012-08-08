17ccaecfde530da557ed9fd09dd4c57d7ece82de
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 08:41:29 +0000

    Revert 150436 - gdata: Remove hide_hosted_documents from ReadDirectoryCallback
    Background: We plan to Add ReadDirectoryByPath() to GDataDirectoryService
    and use the same ReadDirectoryCallback, and hide_hosted_documents  turned
    out to be an obstacle. It's cleaner to check the setting in the caller,
    where this setting matters anyway.
    
    BUG=141147
    TEST=confirm that the "show hosted documents" setting works as before
    
    Review URL: https://chromiumcodereview.appspot.com/10836137
    
    TBR=satorux@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10830211
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150520 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system_proxy.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system_proxy.cc
@@ -16,9 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/gdata/gdata_file_system_interface.h"
 #include "chrome/browser/chromeos/gdata/gdata_system_service.h"
 #include "chrome/browser/chromeos/gdata/gdata_util.h"
-#include "chrome/browser/prefs/pref_service.h"
-#include "chrome/browser/profiles/profile.h"
-#include "chrome/common/pref_names.h"
 #include "content/public/browser/browser_thread.h"
 #include "webkit/blob/shareable_file_reference.h"
 #include "webkit/fileapi/file_system_file_util_proxy.h"
@@ -181,10 +178,8 @@ base::FileUtilProxy::Entry GDataEntryProtoToFileUtilProxyEntry(
 // GDataFileSystemProxy class implementation.
 
 GDataFileSystemProxy::GDataFileSystemProxy(
-    GDataFileSystemInterface* file_system,
-    Profile* profile)
-    : file_system_(file_system),
-      profile_(profile) {
+    GDataFileSystemInterface* file_system)
+    : file_system_(file_system) {
   // Should be created from the file browser extension API (AddMountFunction)
   // on UI thread.
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
@@ -682,12 +677,9 @@ void GDataFileSystemProxy::OnReadDirectory(
     const FileSystemOperationInterface::ReadDirectoryCallback&
     callback,
     GDataFileError error,
+    bool hide_hosted_documents,
     scoped_ptr<gdata::GDataEntryProtoVector> proto_entries) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-
-  PrefService* pref_service = profile_->GetPrefs();
-  const bool hide_hosted_documents =
-      pref_service->GetBoolean(prefs::kDisableGDataHostedFiles);
 
   if (error != GDATA_FILE_OK) {
     callback.Run(util::GDataFileErrorToPlatformError(error),
--- chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
+++ chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
@@ -171,6 +171,7 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler {
   // Called when ReadDirectoryByPath() is complete.
   void OnReadDirectoryByPath(const FilePath& parent_path,
                              gdata::GDataFileError error,
+                             bool hide_hosted_documents,
                              scoped_ptr<gdata::GDataEntryProtoVector> entries);
 
   // Called when GetResourceIdsOfAllFilesOnUIThread() is complete.
@@ -263,6 +264,7 @@ void DriveInternalsWebUIHandler::OnGetGCacheContents(
 void DriveInternalsWebUIHandler::OnReadDirectoryByPath(
     const FilePath& parent_path,
     gdata::GDataFileError error,
+    bool hide_hosted_documents,
     scoped_ptr<gdata::GDataEntryProtoVector> entries) {
   --num_pending_reads_;
   if (error == gdata::GDATA_FILE_OK) {
