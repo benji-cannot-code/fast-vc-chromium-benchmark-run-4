9808d96de966741d0f41339be8a005c8699bc45c
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 08:13:11 +0000

    Remove Element::ensureShadowRoot https://bugs.webkit.org/show_bug.cgi?id=77608
    Reviewed by Ryosuke Niwa.
    
    Since Element::ensureShadowRoot is not used anymore, we can remove this safely.
    
    No new tests, no change in behavior.
    
    * WebCore.order:
    * dom/Element.cpp:
    * dom/Element.h:
    (Element):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -1206,14 +1206,6 @@ ShadowRoot* Element::userAgentShadowRoot() const
     return 0;
 }
 
-ShadowRoot* Element::ensureShadowRoot()
-{
-    if (ElementShadow* shadow = this->shadow())
-        return shadow->oldestShadowRoot();
-
-    return ShadowRoot::create(this, ShadowRoot::UserAgentShadowRoot).get();
-}
-
 const AtomicString& Element::shadowPseudoId() const
 {
     return hasRareData() ? elementRareData()->m_shadowPseudoId : nullAtom;
