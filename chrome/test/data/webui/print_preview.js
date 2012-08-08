6a4bfb55088128272b23550b9a95fa83b18343ea
rltoscano@google.com <rltoscano@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 02:02:28 +0000

    Fix duplex defaults.
    BUG=138312
    
    
    Review URL: https://chromiumcodereview.appspot.com/10829229
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150482 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/print_preview.js
+++ chrome/test/data/webui/print_preview.js
@@ -649,7 +649,8 @@ TEST_F('PrintPreviewWebUITest', 'TestDuplexSettings', function() {
     'setColorAsDefault': true,
     'disableCopiesOption': false,
     'disableLandscapeOption': true,
-    'printerDefaultDuplexValue': 0
+    'printerDefaultDuplexValue': 0,
+    'setDuplexAsDefault': false
   };
   this.nativeLayer_.dispatchEvent(capsSetEvent);
 
@@ -682,7 +683,8 @@ TEST_F('PrintPreviewWebUITest', 'TestDuplexSettings', function() {
     'setColorAsDefault': false,
     'disableCopiesOption': false,
     'disableLandscapeOption': false,
-    'printerDefaultDuplexValue': 1
+    'printerDefaultDuplexValue': 1,
+    'setDuplexAsDefault': true
   };
   this.nativeLayer_.dispatchEvent(capsSetEvent);
 
