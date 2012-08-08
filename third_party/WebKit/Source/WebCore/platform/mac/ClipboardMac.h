02e2c6771980d32a88ca9a214a32489df1f77fc6
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 18:55:07 +0000

    Another unreviewed build fix after http://trac.webkit.org/changeset/125051
    git-svn-id: svn://svn.chromium.org/blink/trunk@125063 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ClipboardMac.h
+++ third_party/WebKit/Source/WebCore/platform/mac/ClipboardMac.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ClipboardMac_h
 
 #include "CachedImage.h"
+#include "CachedImageClient.h"
 #include "Clipboard.h"
 #include <wtf/RetainPtr.h>
 
