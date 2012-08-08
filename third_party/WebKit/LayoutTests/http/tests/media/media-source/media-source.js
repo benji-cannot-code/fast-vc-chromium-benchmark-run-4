85496943da0f59be3433afd2e85bd9aa204d943b
annacc@chromium.org <annacc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 18:38:43 +0000

    Update HTMLMediaElement to the new OO MediaSource API. https://bugs.webkit.org/show_bug.cgi?id=91775
    Reviewed by Eric Carlson.
    
    This patch rips out the old-style MediaSource API and allows a
    MediaSource object to be attached to HTMLMediaElement.
    http://dvcs.w3.org/hg/html-media/raw-file/tip/media-source/media-source.html
    
    Source/WebCore:
    
    Tests: updates to http/tests/media/media-source/*
           http/tests/media/media-source/video-media-source-add-and-remove-buffers.html
           http/tests/media/media-source/video-media-source-objects.html
    
    * html/HTMLMediaElement.cpp:
    (WebCore):
    (WebCore::HTMLMediaElement::HTMLMediaElement): Remove old style API resources.
    (WebCore::HTMLMediaElement::parseAttribute): Remove old style event attribute.
    (WebCore::HTMLMediaElement::prepareForLoad): Set source state to "closed".
    (WebCore::HTMLMediaElement::loadResource): Get MediaSource object from blob registry
        look up and, if found, set it's MediaPlayer pointer to the current MediaPlayer.
    (WebCore::HTMLMediaElement::noneSupported): Set source state to "closed".
    (WebCore::HTMLMediaElement::mediaEngineError): Set source state to "closed".
    (WebCore::HTMLMediaElement::mediaPlayerSourceOpened): Set source state to "open".
    (WebCore::HTMLMediaElement::mediaPlayerSourceURL): Change to new blob URL.
    (WebCore::HTMLMediaElement::seek): Check if source state is "closed".
    (WebCore::HTMLMediaElement::setSourceState): Helper function so that we don't have to
        keep checking for m_mediaSource.
    (WebCore::HTMLMediaElement::userCancelledLoad): Set source state to "closed".
    (WebCore::HTMLMediaElement::createMediaPlayer): If the current MediaPlayer is re-
         created, notify the MediaSource and reset its MediaPlayer.
    
    * html/HTMLMediaElement.h: Remove old style API code and add a MediaSource object.
    (HTMLMediaElement):
    * html/HTMLMediaElement.idl: Remove old style API.
    
    LayoutTests:
    
    * http/tests/media/media-source/media-source.js:
    (MediaSourceTest.SegmentHelper):
    (MediaSourceTest.SegmentHelper.prototype.addSourceBuffer):
    (MediaSourceTest.SegmentHelper.prototype.appendInitSegment):
    (MediaSourceTest.SegmentHelper.prototype.appendMediaSegment):
    (MediaSourceTest.SegmentHelper.prototype.appendUntilEndOfStream):
    (MediaSourceTest.setSrcToMediaSourceTestURL):
    (MediaSourceTest.defaultOnErrorChecks):
    (MediaSourceTest.runOnSourceOpen.eventHandlerFunction):
    (MediaSourceTest.runOnSourceOpen):
    (MediaSourceTest.logSourceState):
    (MediaSourceTest.expectSourceState):
    * http/tests/media/media-source/video-media-source-abort-expected.txt:
    * http/tests/media/media-source/video-media-source-abort.html:
    * http/tests/media/media-source/video-media-source-add-and-remove-buffers-expected.txt: Renamed from LayoutTests/http/tests/media/media-source/video-media-source-add-and-remove-ids-expected.txt.
    * http/tests/media/media-source/video-media-source-add-and-remove-buffers.html: Added.
    * http/tests/media/media-source/video-media-source-add-and-remove-ids.html: Removed.
    * http/tests/media/media-source/video-media-source-errors-expected.txt:
    * http/tests/media/media-source/video-media-source-errors.html:
    * http/tests/media/media-source/video-media-source-event-attributes-expected.txt:
    * http/tests/media/media-source/video-media-source-event-attributes.html:
    * http/tests/media/media-source/video-media-source-objects-expected.txt: Added.
    * http/tests/media/media-source/video-media-source-objects.html: Added.
    * http/tests/media/media-source/video-media-source-play-expected.txt:
    * http/tests/media/media-source/video-media-source-play.html:
    * http/tests/media/media-source/video-media-source-seek-expected.txt:
    * http/tests/media/media-source/video-media-source-seek.html:
    * http/tests/media/media-source/video-media-source-state-changes-expected.txt:
    * http/tests/media/media-source/video-media-source-state-changes.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125059 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/media/media-source/media-source.js
+++ third_party/WebKit/LayoutTests/http/tests/media/media-source/media-source.js
@@ -1,10 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var MediaSourceTest = {};
+var mediaSource = new MediaSource();
 
 MediaSourceTest.SegmentHelper = function(segmentInfo)
 {
     this.MediaSegmentsToLoad = 0;
-    this.SourceID = 'sourceId';
+    this.sourceBuffer = null;
 
     this.videoTag = null;
     this.segmentInfo = segmentInfo;
@@ -122,32 +123,32 @@ MediaSourceTest.SegmentHelper.prototype.getData_ = function (url, start, length,
     request.send();
 };
 
-MediaSourceTest.SegmentHelper.prototype.addSourceId = function()
+MediaSourceTest.SegmentHelper.prototype.addSourceBuffer = function()
 {
-    this.videoTag.webkitSourceAddId(this.SourceID, this.segmentInfo.type);
+    this.sourceBuffer = mediaSource.addSourceBuffer(this.segmentInfo.type);
 };
 
 MediaSourceTest.SegmentHelper.prototype.appendInitSegment = function()
 {
-    this.videoTag.webkitSourceAppend(this.SourceID, this.initSegment);
+    this.sourceBuffer.append(this.initSegment);
 };
 
 MediaSourceTest.SegmentHelper.prototype.appendMediaSegment = function(index)
 {
-    this.videoTag.webkitSourceAppend(this.SourceID, this.mediaSegments[index]);
+    this.sourceBuffer.append(this.mediaSegments[index]);
 };
 
 MediaSourceTest.SegmentHelper.prototype.appendUntilEndOfStream = function(startIndex)
 {
-    if (!this.videoTag.webkitSourceAppend) {
-        failTest("webkitSourceAppend() is not available");
+    if (!this.sourceBuffer) {
+        failTest("MediaSource API is not available");
         return;
     }
 
     for (var index = startIndex; index < this.mediaSegments.length; index++)
         this.appendMediaSegment(index);
 
-    this.videoTag.webkitSourceEndOfStream(this.videoTag.EOS_NO_ERROR);
+    mediaSource.endOfStream();
 };
 
 MediaSourceTest.SegmentHelper.prototype.getTimeForMediaSegment = function(index)
@@ -177,7 +178,7 @@ MediaSourceTest.setSrcToMediaSourceTestURL = function(videoTag)
         return;
     }
 
-    videoTag.src = videoTag.webkitMediaSourceURL;
+    videoTag.src = webkitURL.createObjectURL(mediaSource);
 };
 
 MediaSourceTest.mediaErrorString = function(videoTag)
@@ -207,9 +208,9 @@ MediaSourceTest.defaultOnErrorChecks = function(event)
 
     consoleWrite("EVENT(error) : " + MediaSourceTest.mediaErrorString(videoTag));
 
-    if (videoTag.webkitSourceState != HTMLMediaElement.SOURCE_CLOSED) {
-        consoleWrite("Unexpected source state. Expected SOURCE_CLOSED" +
-                     " got " + getSourceStateName(videoTag.webkitSourceState));
+    if (mediaSource.readyState != "closed") {
+        consoleWrite("Unexpected source state. Expected 'closed'" +
+                     " got " + mediaSource.readyState);
     }
 };
 
@@ -271,42 +272,32 @@ MediaSourceTest.runOnSourceOpen = function(videoTag, onOpenFunction)
 {
     var eventHandlerFunction = function (event)
     {
+        if (videoTag.networkState != HTMLMediaElement.NETWORK_LOADING &&
+            videoTag.networkState != HTMLMediaElement.NETWORK_IDLE) {
+            failTest("Unexpected network state. Expected " +
+                     HTMLMediaElement.NETWORK_LOADING + " or " +
+                     HTMLMediaElement.NETWORK_IDLE +
+                     " got " + videoTag.readyState);
+        }
         event.target.removeEventListener('webkitsourceopen', eventHandlerFunction);
         onOpenFunction();
     };
-    videoTag.addEventListener('webkitsourceopen', eventHandlerFunction);
+
+    mediaSource.addEventListener('webkitsourceopen', eventHandlerFunction);
     MediaSourceTest.setSrcToMediaSourceTestURL(videoTag);
 };
 
-MediaSourceTest.logSourceState = function(videoTag)
+MediaSourceTest.logSourceState = function(mediaSource)
 {
-    consoleWrite("webkitSourceState : " + MediaSourceTest.getSourceStateName(videoTag.webkitSourceState));
+    consoleWrite("webkitSourceState : " + mediaSource.readyState);
 };
 
-MediaSourceTest.expectSourceState = function(videoTag, expected)
+MediaSourceTest.expectSourceState = function(mediaSource, expected)
 {
-    if (videoTag.webkitSourceState != expected) {
-        failTest("Unexpected source state. Expected " +
-                 MediaSourceTest.getSourceStateName(expected) +
-                 " got " + MediaSourceTest.getSourceStateName(videoTag.webkitSourceState));
+    if (mediaSource.readyState != expected) {
+        failTest("Unexpected source state. Expected " + expected +
+                 " got " + mediaSource.readyState);
     }
-};
-
-MediaSourceTest.getSourceStateName = function(state)
-{
-    var stateName = "UNKNOWN";
-    switch (state) {
-        case HTMLMediaElement.SOURCE_CLOSED:
-            stateName = "SOURCE_CLOSED";
-            break;
-        case HTMLMediaElement.SOURCE_OPEN:
-            stateName = "SOURCE_OPEN";
-            break;
-        case HTMLMediaElement.SOURCE_ENDED:
-            stateName = "SOURCE_ENDED";
-            break;
-    }
-    return stateName;
 };
 
 MediaSourceTest.expectReadyState = function(videoTag, expected)
