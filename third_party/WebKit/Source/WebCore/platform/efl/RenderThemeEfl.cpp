25006b00bd6c11f797e6a1e0d45018213f3bcfad
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 12:53:18 +0000

    [EFL] Support DataList for <input type="range"> https://bugs.webkit.org/show_bug.cgi?id=92634
    Patch by KwangYong Choi <ky0.choi@samsung.com> on 2012-08-08
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    DataList theme for EFL is implemented. DataList feature of
    <input type="range"> is supported.
    
    Test: fast/forms/datalist/input-list.html
    
    * platform/efl/RenderThemeEfl.cpp:
    (WebCore::RenderThemeEfl::paintSliderTrack):
    (WebCore::RenderThemeEfl::sliderTickSize):
    (WebCore::RenderThemeEfl::sliderTickOffsetFromTrackCenter):
    (WebCore::RenderThemeEfl::supportsDataListUI):
    (WebCore):
    * platform/efl/RenderThemeEfl.h:
    (RenderThemeEfl):
    
    LayoutTests:
    
    Update platform/efl/fast/forms/datalist/input-list-expected.txt because
    range type of data list feature is passed.
    
    * platform/efl/fast/forms/datalist/input-list-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125026 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSValueKeywords.h"
 #include "GraphicsContext.h"
 #include "HTMLInputElement.h"
+#include "InputType.h"
 #include "NotImplemented.h"
 #include "Page.h"
 #include "PaintInfo.h"
@@ -748,8 +749,15 @@ LayoutUnit RenderThemeEfl::baselinePosition(const RenderObject* object) const
 bool RenderThemeEfl::paintSliderTrack(RenderObject* object, const PaintInfo& info, const IntRect& rect)
 {
     if (object->style()->appearance() == SliderHorizontalPart)
-        return paintThemePart(object, SliderHorizontal, info, rect);
-    return paintThemePart(object, SliderVertical, info, rect);
+        paintThemePart(object, SliderHorizontal, info, rect);
+    else
+        paintThemePart(object, SliderVertical, info, rect);
+
+#if ENABLE(DATALIST_ELEMENT)
+    paintSliderTicks(object, info, rect);
+#endif
+
+    return false;
 }
 
 void RenderThemeEfl::adjustSliderTrackStyle(StyleResolver* styleResolver, RenderStyle* style, Element* element) const
@@ -793,16 +801,26 @@ void RenderThemeEfl::adjustSliderThumbSize(RenderStyle* style, Element*) const
 #if ENABLE(DATALIST_ELEMENT)
 IntSize RenderThemeEfl::sliderTickSize() const
 {
-    // FIXME: We need to set this to the size of one tick mark.
-    return IntSize(0, 0);
+    return IntSize(1, 6);
 }
 
 int RenderThemeEfl::sliderTickOffsetFromTrackCenter() const
 {
-    // FIXME: We need to set this to the position of the tick marks.
-    return 0;
+    static const int sliderTickOffset = -12;
+
+    return sliderTickOffset;
 }
 #endif
+
+bool RenderThemeEfl::supportsDataListUI(const AtomicString& type) const
+{
+#if ENABLE(DATALIST_ELEMENT)
+    // FIXME: We need to support other types.
+    return type == InputTypeNames::range();
+#else
+    return false;
+#endif
+}
 
 bool RenderThemeEfl::paintSliderThumb(RenderObject* object, const PaintInfo& info, const IntRect& rect)
 {
