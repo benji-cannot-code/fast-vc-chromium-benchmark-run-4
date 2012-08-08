0e1022b0bad7b46f0111df65ab315a779c2a06f6
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 17:57:58 +0000

    Video element image loader must persist after element detach. https://bugs.webkit.org/show_bug.cgi?id=90801
    Reviewed by Eric Carlson.
    
    We rely on the OwnPtr in the element to cleanup the loader.
    
    Source/WebCore:
    
    Test: fast/dom/beforeload/remove-video-poster-in-beforeload-listener.html
    
    * html/HTMLVideoElement.cpp:
    (WebCore):
    * html/HTMLVideoElement.h:
    (HTMLVideoElement):
    
    LayoutTests:
    
    * fast/dom/beforeload/remove-video-poster-in-beforeload-listener-expected.txt: Added.
    * fast/dom/beforeload/remove-video-poster-in-beforeload-listener.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125052 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLVideoElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLVideoElement.cpp
@@ -88,14 +88,6 @@ void HTMLVideoElement::attach()
 #endif
 }
 
-void HTMLVideoElement::detach()
-{
-    HTMLMediaElement::detach();
-    
-    if (!shouldDisplayPosterImage() && m_imageLoader)
-        m_imageLoader.clear();
-}
-
 void HTMLVideoElement::collectStyleForAttribute(const Attribute& attribute, StylePropertySet* style)
 {
     if (attribute.name() == widthAttr)
