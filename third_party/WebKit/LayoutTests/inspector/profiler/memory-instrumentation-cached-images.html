benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit d703b1c77a3025cc751b4e345a5ead9bfadee03b

Web Inspector: native memory instrumentation: cover MemoryCache with MemoryInstrumentation https://bugs.webkit.org/show_bug.cgi?id=91734
Reviewed by Yury Semikhatsky.

Old version of native memory instrumentation doesn't mark the memory cache objects as visited.
As a result MI could count the same objects multiple times.
The new version just traverses through MemoryCache the same way as through DOM.
Also not all the cached resource types were counted.

* dom/Document.cpp:
(WebCore::Document::reportMemoryUsage):
* dom/MemoryInstrumentation.h:
* inspector/InspectorMemoryAgent.cpp:
(MemoryBlockName):
(WebCore):
(WebCore::collectDomTreeInfo):
(WebCore::InspectorMemoryAgent::getProcessMemoryDistribution):
* loader/cache/CachedCSSStyleSheet.cpp:
(WebCore::CachedCSSStyleSheet::reportMemoryUsage):
(WebCore):
* loader/cache/CachedCSSStyleSheet.h:
(WebCore):
(CachedCSSStyleSheet):
* loader/cache/CachedFont.cpp:
(WebCore::CachedFont::reportMemoryUsage):
(WebCore):
* loader/cache/CachedFont.h:
(CachedFont):
* loader/cache/CachedImage.cpp:
(WebCore::CachedImage::reportMemoryUsage):
(WebCore):
* loader/cache/CachedImage.h:
(CachedImage):
* loader/cache/CachedRawResource.cpp:
(WebCore::CachedRawResource::reportMemoryUsage):
(WebCore):
* loader/cache/CachedRawResource.h:
(WebCore):
(CachedRawResource):
* loader/cache/CachedResource.cpp:
(WebCore::CachedResource::reportMemoryUsage):
(WebCore):
* loader/cache/CachedResource.h:
(WebCore):
(CachedResource):
* loader/cache/CachedResourceHandle.cpp:
(WebCore::CachedResourceHandleBase::reportMemoryUsage):
(WebCore):
* loader/cache/CachedResourceHandle.h:
(WebCore):
(CachedResourceHandleBase):
* loader/cache/CachedResourceLoader.cpp:
(WebCore):
(WebCore::CachedResourceLoader::reportMemoryUsage):
* loader/cache/CachedResourceLoader.h:
(WebCore):
(CachedResourceLoader):
* loader/cache/CachedSVGDocument.cpp:
(WebCore::CachedSVGDocument::reportMemoryUsage):
(WebCore):
* loader/cache/CachedSVGDocument.h:
(CachedSVGDocument):

third_party/WebKit/Source/WebCore/loader/cache/CachedImage.cpp
@@ -468,4 +468,14 @@ void CachedImage::changedInRect(const Image* image, const IntRect& rect)
     notifyObservers(&rect);
 }
 
+void CachedImage::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
+{
+    MemoryClassInfo<CachedImage> info(memoryObjectInfo, this, MemoryInstrumentation::CachedResourceImage);
+    CachedResource::reportMemoryUsage(memoryObjectInfo);
+    info.addMember(m_image);
+#if ENABLE(SVG)
+    info.addMember(m_svgImageCache);
+#endif
+}
+
 } // namespace WebCore

third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -6096,6 +6096,8 @@ void Document::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
     info.addVector(m_customFonts);
     info.addMember(m_documentURI);
     info.addMember(m_baseTarget);
+    info.addInstrumentedMember(m_frame);
+    info.addInstrumentedMember(m_cachedResourceLoader);
     info.addInstrumentedMember(m_elemSheet);
     info.addInstrumentedMember(m_pageUserSheet);
     if (m_pageGroupUserSheets)

third_party/WebKit/Source/WebCore/dom/MemoryInstrumentation.h
@@ -1,4 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+
 /*
  * Copyright (C) 2012 Google Inc. All rights reserved.
  *
@@ -53,6 +54,15 @@ public:
         CSS,
         Binding,
         Loader,
+        MemoryCacheStructures,
+        CachedResource,
+        CachedResourceCSS,
+        CachedResourceFont,
+        CachedResourceImage,
+        CachedResourceScript,
+        CachedResourceSVG,
+        CachedResourceShader,
+        CachedResourceXSLT,
         LastTypeEntry
     };
 

third_party/WebKit/Source/WebCore/inspector/InspectorMemoryAgent.cpp
@@ -82,14 +82,20 @@ static const char jsExternalStrings[] = "JSExternalStrings";
 static const char inspectorData[] = "InspectorData";
 static const char inspectorDOMData[] = "InspectorDOMData";
 static const char inspectorJSHeapData[] = "InspectorJSHeapData";
-static const char memoryCache[] = "MemoryCache";
 static const char processPrivateMemory[] = "ProcessPrivateMemory";
 
-static const char cachedImages[] = "CachedImages";
-static const char cachedCssStyleSheets[] = "CachedCssStyleSheets";
-static const char cachedScripts[] = "CachedScripts";
-static const char cachedXslStyleSheets[] = "CachedXslStyleSheets";
-static const char cachedFonts[] = "CachedFonts";
+static const char memoryCache[] = "MemoryCache";
+static const char memoryCacheStructures[] = "MemoryCacheStructures";
+static const char cachedResource[] = "CachedResource";
+static const char cachedResourceImage[] = "CachedImages";
+static const char cachedResourceCSS[] = "CachedCssStyleSheets";
+static const char cachedResourceScript[] = "CachedScripts";
+static const char cachedResourceXSL[] = "CachedXslStyleSheets";
+static const char cachedResourceFont[] = "CachedFonts";
+static const char cachedResourceSVG[] = "CachedSVGGraphics";
+static const char cachedResourceShader[] = "CachedShaders";
+static const char cachedResourceXSLT[] = "CachedShadersXSLT";
+
 static const char renderTreeUsed[] = "RenderTreeUsed";
 static const char renderTreeAllocated[] = "RenderTreeAllocated";
 
@@ -470,12 +476,40 @@ public:
         ScriptProfiler::collectBindingMemoryInfo(&m_domMemoryUsage);
     }
 
-    PassRefPtr<InspectorMemoryBlock> dumpStatistics(InspectorDataCounter* inspectorData)
+    void visitMemoryCache()
     {
-        inspectorData->addComponent(MemoryBlockName::inspectorDOMData, m_domMemoryUsage.selfSize());
+        m_domMemoryUsage.addRootObject(memoryCache());
+    }
 
+    PassRefPtr<InspectorMemoryBlock> buildObjectForMemoryCache() const
+    {
         size_t totalSize = 0;
-        for (int i = MemoryInstrumentation::Other; i < MemoryInstrumentation::LastTypeEntry; ++i)
+
+        COMPILE_ASSERT(MemoryInstrumentation::LastTypeEntry == MemoryInstrumentation::CachedResourceXSLT + 1, object_type_enum_was_changed_please_fix_the_implementation);
+        for (int i = MemoryInstrumentation::MemoryCacheStructures; i < MemoryInstrumentation::LastTypeEntry; ++i)
+            totalSize += m_domMemoryUsage.totalSize(static_cast<MemoryInstrumentation::ObjectType>(i));
+
+        RefPtr<TypeBuilder::Array<InspectorMemoryBlock> > children = TypeBuilder::Array<InspectorMemoryBlock>::create();
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::MemoryCacheStructures), MemoryBlockName::memoryCacheStructures);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResource), MemoryBlockName::cachedResource);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceCSS), MemoryBlockName::cachedResourceCSS);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceFont), MemoryBlockName::cachedResourceFont);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceImage), MemoryBlockName::cachedResourceImage);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceScript), MemoryBlockName::cachedResourceScript);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceSVG), MemoryBlockName::cachedResourceSVG);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceShader), MemoryBlockName::cachedResourceShader);
+        addMemoryBlockFor(children.get(), m_domMemoryUsage.totalSize(MemoryInstrumentation::CachedResourceXSLT), MemoryBlockName::cachedResourceXSLT);
+
+        RefPtr<InspectorMemoryBlock> block = InspectorMemoryBlock::create().setName(MemoryBlockName::memoryCache);
+        block->setSize(totalSize);
+        block->setChildren(children.release());
+        return block.release();
+    }
+
+    PassRefPtr<InspectorMemoryBlock> buildObjectForDOM() const
+    {
+        size_t totalSize = 0;
+        for (int i = MemoryInstrumentation::Other; i < MemoryInstrumentation::MemoryCacheStructures; ++i)
             totalSize += m_domMemoryUsage.totalSize(static_cast<MemoryInstrumentation::ObjectType>(i));
 
         RefPtr<TypeBuilder::Array<InspectorMemoryBlock> > domChildren = TypeBuilder::Array<InspectorMemoryBlock>::create();
@@ -491,6 +525,13 @@ public:
         return dom.release();
     }
 
+    void dumpStatistics(TypeBuilder::Array<InspectorMemoryBlock>* children, InspectorDataCounter* inspectorData)
+    {
+        children->addItem(buildObjectForMemoryCache());
+        children->addItem(buildObjectForDOM());
+        inspectorData->addComponent(MemoryBlockName::inspectorDOMData, m_domMemoryUsage.selfSize());
+    }
+
 private:
     Page* m_page;
     MemoryInstrumentationImpl m_domMemoryUsage;
@@ -498,9 +539,10 @@ private:
 
 }
 
-static PassRefPtr<InspectorMemoryBlock> domTreeInfo(Page* page, VisitedObjects& visitedObjects, InspectorDataCounter* inspectorData)
+static void collectDomTreeInfo(Page* page, VisitedObjects& visitedObjects, TypeBuilder::Array<InspectorMemoryBlock>* children, InspectorDataCounter* inspectorData)
 {
     DOMTreesIterator domTreesIterator(page, visitedObjects);
+
     ScriptProfiler::visitNodeWrappers(&domTreesIterator);
 
     // Make sure all documents reachable from the main frame are accounted.
@@ -512,8 +554,9 @@ static PassRefPtr<InspectorMemoryBlock> domTreeInfo(Page* page, VisitedObjects& 
     }
 
     domTreesIterator.visitBindings();
+    domTreesIterator.visitMemoryCache();
 
-    return domTreesIterator.dumpStatistics(inspectorData);
+    domTreesIterator.dumpStatistics(children, inspectorData);
 }
 
 static void addPlatformComponentsInfo(PassRefPtr<TypeBuilder::Array<InspectorMemoryBlock> > children)
@@ -525,27 +568,6 @@ static void addPlatformComponentsInfo(PassRefPtr<TypeBuilder::Array<InspectorMem
         block->setSize(it->m_sizeInBytes);
         children->addItem(block);
     }
-}
-
-static PassRefPtr<InspectorMemoryBlock> memoryCacheInfo()
-{
-    MemoryCache::Statistics stats = memoryCache()->getStatistics();
-    int totalSize = stats.images.size +
-                    stats.cssStyleSheets.size +
-                    stats.scripts.size +
-                    stats.xslStyleSheets.size +
-                    stats.fonts.size;
-    RefPtr<InspectorMemoryBlock> memoryCacheStats = InspectorMemoryBlock::create().setName(MemoryBlockName::memoryCache);
-    memoryCacheStats->setSize(totalSize);
-
-    RefPtr<TypeBuilder::Array<InspectorMemoryBlock> > children = TypeBuilder::Array<InspectorMemoryBlock>::create();
-    addMemoryBlockFor(children.get(), stats.images.size, MemoryBlockName::cachedImages);
-    addMemoryBlockFor(children.get(), stats.cssStyleSheets.size, MemoryBlockName::cachedCssStyleSheets);
-    addMemoryBlockFor(children.get(), stats.scripts.size, MemoryBlockName::cachedScripts);
-    addMemoryBlockFor(children.get(), stats.xslStyleSheets.size, MemoryBlockName::cachedXslStyleSheets);
-    addMemoryBlockFor(children.get(), stats.fonts.size, MemoryBlockName::cachedFonts);
-    memoryCacheStats->setChildren(children.get());
-    return memoryCacheStats.release();
 }
 
 static PassRefPtr<InspectorMemoryBlock> jsExternalResourcesInfo(VisitedObjects& visitedObjects)
@@ -586,10 +608,9 @@ void InspectorMemoryAgent::getProcessMemoryDistribution(ErrorString*, RefPtr<Ins
     VisitedObjects visitedObjects;
     RefPtr<TypeBuilder::Array<InspectorMemoryBlock> > children = TypeBuilder::Array<InspectorMemoryBlock>::create();
     children->addItem(jsHeapInfo());
-    children->addItem(memoryCacheInfo());
     children->addItem(renderTreeInfo(m_page)); // FIXME: collect for all pages?
-    children->addItem(domTreeInfo(m_page, visitedObjects, &inspectorData)); // FIXME: collect for all pages?
     children->addItem(jsExternalResourcesInfo(visitedObjects));
+    collectDomTreeInfo(m_page, visitedObjects, children.get(), &inspectorData); // FIXME: collect for all pages?
     children->addItem(inspectorData.dumpStatistics());
     children->addItem(dumpDOMStorageCache(m_domStorageAgent->memoryBytesUsedByStorageCache()));
     addPlatformComponentsInfo(children);

third_party/WebKit/Source/WebCore/loader/cache/CachedCSSStyleSheet.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CachedStyleSheetClient.h"
 #include "HTTPParsers.h"
 #include "MemoryCache.h"
+#include "MemoryInstrumentation.h"
 #include "SharedBuffer.h"
 #include "StyleSheetContents.h"
 #include "TextResourceDecoder.h"
@@ -197,6 +198,15 @@ void CachedCSSStyleSheet::saveParsedStyleSheet(PassRefPtr<StyleSheetContents> sh
     m_parsedStyleSheetCache->addedToMemoryCache();
 
     setDecodedSize(m_parsedStyleSheetCache->estimatedSizeInBytes());
+}
+
+void CachedCSSStyleSheet::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
+{
+    MemoryClassInfo<CachedCSSStyleSheet> info(memoryObjectInfo, this, MemoryInstrumentation::CachedResourceCSS);
+    CachedResource::reportMemoryUsage(memoryObjectInfo);
+    info.addMember(m_decoder);
+    info.addInstrumentedMember(m_parsedStyleSheetCache);
+    info.addMember(m_decodedSheetText);
 }
 
 }

third_party/WebKit/Source/WebCore/loader/cache/CachedCSSStyleSheet.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
     class CachedResourceClient;
+    class MemoryObjectInfo;
     class SharedBuffer;
     class StyleSheetContents;
     class TextResourceDecoder;
@@ -59,6 +60,8 @@ namespace WebCore {
         PassRefPtr<StyleSheetContents> restoreParsedStyleSheet(const CSSParserContext&);
         void saveParsedStyleSheet(PassRefPtr<StyleSheetContents>);
     
+        virtual void reportMemoryUsage(MemoryObjectInfo*) const OVERRIDE;
+
     private:
         bool canUseSheet(bool enforceMIMEType, bool* hasValidMIMEType) const;
         virtual PurgePriority purgePriority() const { return PurgeLast; }

third_party/WebKit/Source/WebCore/loader/cache/CachedFont.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CachedResourceLoader.h"
 #include "FontPlatformData.h"
 #include "MemoryCache.h"
+#include "MemoryInstrumentation.h"
 #include "SharedBuffer.h"
 #include "TextResourceDecoder.h"
 #include <wtf/Vector.h>
@@ -205,6 +206,18 @@ void CachedFont::error(CachedResource::Status status)
     ASSERT(errorOccurred());
     setLoading(false);
     checkNotify();
+}
+
+void CachedFont::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
+{
+    MemoryClassInfo<CachedFont> info(memoryObjectInfo, this, MemoryInstrumentation::CachedResourceFont);
+    CachedResource::reportMemoryUsage(memoryObjectInfo);
+#if ENABLE(SVG_FONTS)
+    info.addInstrumentedMember(m_externalSVGDocument);
+#endif
+#ifdef STORE_FONT_CUSTOM_PLATFORM_DATA
+    info.addMember(m_fontData);
+#endif
 }
 
 }

third_party/WebKit/Source/WebCore/loader/cache/CachedFont.h
@@ -71,6 +71,8 @@ public:
     SVGFontElement* getSVGFontById(const String&) const;
 #endif
 
+    virtual void reportMemoryUsage(MemoryObjectInfo*) const OVERRIDE;
+
 private:
     FontCustomPlatformData* m_fontData;
     bool m_loadInitiated;

commit 0ab61d159805cdc754cf58c11887ef69c9406bae

Web Inspector: cached images memory instrumentation regression after r124744 https://bugs.webkit.org/show_bug.cgi?id=93366
Reviewed by Vsevolod Vlasov.

Source/WebCore:

CachedImage memory usage data no includes size of both encoded and
decoded data.

Test: inspector/profiler/memory-instrumentation-cached-images.html

* loader/cache/CachedImage.cpp:
(WebCore::CachedImage::reportMemoryUsage):

LayoutTests:

Added a sanity test checking that memory usage data includes decoded image size.

* inspector/profiler/memory-instrumentation-cached-images-expected.txt: Added.
* inspector/profiler/memory-instrumentation-cached-images.html: Added.


git-svn-id: svn://svn.chromium.org/blink/trunk@125011 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/loader/cache/CachedImage.cpp
@@ -472,7 +472,11 @@ void CachedImage::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
 {
     MemoryClassInfo info(memoryObjectInfo, this, MemoryInstrumentation::CachedResourceImage);
     CachedResource::reportMemoryUsage(memoryObjectInfo);
-    info.addMember(m_image);
+    if (m_image) {
+        if (m_image->data())
+            info.addRawBuffer(m_image->data(), m_image->data()->size());
+        info.addRawBuffer(m_image.get(), decodedSize());
+    }
 #if ENABLE(SVG)
     info.addMember(m_svgImageCache);
 #endif

third_party/WebKit/LayoutTests/inspector/profiler/memory-instrumentation-cached-images-expected.txt
@@ -1,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This test checks that CachedImages size reported by the memory agent includes images decoded size. Bug 93366
+
+
+didReceiveMemorySnapshot
+PASS: CachedImages size is OK.
+

third_party/WebKit/LayoutTests/inspector/profiler/memory-instrumentation-cached-images.html
@@ -1,0 +1,74 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+<head>
+<script src="../../http/tests/inspector/inspector-test.js"></script>
+<script>
+
+function displayAndRunTest()
+{
+    if (window.testRunner)
+        testRunner.display();
+    runTest();
+}
+
+
+function test()
+{
+    function memoryBlockSize(path, root)
+    {
+        var pathPos = 0;
+        var children = [root];
+
+        while (true) {
+            var name = path[pathPos++];
+            var child = null;
+            for (var i = 0; i < children.length; i++) {
+                if (children[i].name === name) {
+                    if (pathPos === path.length)
+                        return children[i].size;
+                    else {
+                        child = children[i];
+                        break;
+                    }
+                }
+            }
+            if (child) {
+                children = child.children;
+                if (!children) {
+                    InspectorTest.addResult(name + " has no children");
+                    return -1;
+                }
+            } else {
+                InspectorTest.addResult(name + " not found");
+                return -1;
+            }
+        }
+        return -1;
+    }
+
+    function didReceiveMemorySnapshot(error, memoryBlock)
+    {
+        InspectorTest.addResult("didReceiveMemorySnapshot");
+        var imagesSize = memoryBlockSize(["ProcessPrivateMemory", "MemoryCache", "CachedImages"], memoryBlock);
+        var jpegDecodedSizeExpected = 20000000;
+        if (imagesSize > jpegDecodedSizeExpected)
+            InspectorTest.addResult("PASS: CachedImages size is OK.");
+        else
+            InspectorTest.addResult("FAIL: CachedImages size is too small.");
+        InspectorTest.completeTest();
+    }
+
+    MemoryAgent.getProcessMemoryDistribution(didReceiveMemorySnapshot.bind(this));
+}
+
+</script>
+</head>
+
+<body>
+<p>
+This test checks that CachedImages size reported by the memory agent includes images decoded size.
+<a href="https://bugs.webkit.org/show_bug.cgi?id=93366">Bug 93366</a>
+</p>
+<img src="resources/image.jpeg" width="100" height="60" onload="displayAndRunTest()"/>
+</body>
+</html>