d3086e10fc0a97c35396416501de10dde3fcfe9a
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 05:06:32 +0000

    [V8] Replace throwError(ExceptionCode, Isolate*) with setDOMException(ExceptionCode, Isolate*) in v8/* and v8/custom/* https://bugs.webkit.org/show_bug.cgi?id=93226
    Reviewed by Eric Seidel.
    
    Now throwError(ExceptionCode, Isolate*) is equivalent to
    setDOMException(ExceptionCode, Isolate*). We can replace the former with
    the latter. After this replacement, the rule becomes simple and sane:
    "Use throwError() for throwing JavaScript errors, use setDOMException()
    for throwing DOM exceptions".
    
    No tests. No change in behavior.
    
    * bindings/v8/SerializedScriptValue.cpp:
    (WebCore::SerializedScriptValue::transferArrayBuffers):
    (WebCore::SerializedScriptValue::SerializedScriptValue):
    * bindings/v8/custom/V8AudioContextCustom.cpp:
    (WebCore::V8AudioContext::constructorCallback):
    * bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp:
    (WebCore::V8CSSStyleDeclaration::namedPropertySetter):
    * bindings/v8/custom/V8DOMStringMapCustom.cpp:
    (WebCore::V8DOMStringMap::namedPropertySetter):
    * bindings/v8/custom/V8DOMWindowCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8DedicatedWorkerContextCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8DirectoryEntryCustom.cpp:
    (WebCore::V8DirectoryEntry::getDirectoryCallback):
    (WebCore::V8DirectoryEntry::getFileCallback):
    * bindings/v8/custom/V8DocumentCustom.cpp:
    (WebCore::V8Document::evaluateCallback):
    * bindings/v8/custom/V8HistoryCustom.cpp:
    (WebCore::V8History::pushStateCallback):
    (WebCore::V8History::replaceStateCallback):
    * bindings/v8/custom/V8IntentConstructor.cpp:
    (WebCore::V8Intent::constructorCallback):
    * bindings/v8/custom/V8MessagePortCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8MutationObserverCustom.cpp:
    (WebCore::V8MutationObserver::constructorCallback):
    * bindings/v8/custom/V8NotificationCenterCustom.cpp:
    (WebCore::V8NotificationCenter::createHTMLNotificationCallback):
    (WebCore::V8NotificationCenter::createNotificationCallback):
    (WebCore::V8NotificationCenter::requestPermissionCallback):
    * bindings/v8/custom/V8SQLTransactionCustom.cpp:
    (WebCore::V8SQLTransaction::executeSqlCallback):
    * bindings/v8/custom/V8SQLTransactionSyncCustom.cpp:
    (WebCore::V8SQLTransactionSync::executeSqlCallback):
    * bindings/v8/custom/V8StorageCustom.cpp:
    (WebCore::storageSetter):
    * bindings/v8/custom/V8WebSocketCustom.cpp:
    (WebCore::V8WebSocket::constructorCallback):
    (WebCore::V8WebSocket::sendCallback):
    * bindings/v8/custom/V8WorkerContextCustom.cpp:
    (WebCore::V8WorkerContext::importScriptsCallback):
    * bindings/v8/custom/V8WorkerCustom.cpp:
    (WebCore::handlePostMessageCallback):
    * bindings/v8/custom/V8XMLHttpRequestCustom.cpp:
    (WebCore::V8XMLHttpRequest::responseTextAccessorGetter):
    (WebCore::V8XMLHttpRequest::openCallback):
    (WebCore::V8XMLHttpRequest::sendCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@124985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8AudioContextCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8AudioContextCustom.cpp
@@ -66,7 +66,7 @@ v8::Handle<v8::Value> V8AudioContext::constructorCallback(const v8::Arguments& a
         ExceptionCode ec = 0;
         audioContext = AudioContext::create(document, ec);
         if (ec)
-            return throwError(ec, args.GetIsolate());
+            return V8Proxy::setDOMException(ec, args.GetIsolate());
         if (!audioContext.get())
             return V8Proxy::throwError(V8Proxy::SyntaxError, "audio resources unavailable for AudioContext construction", args.GetIsolate());
     } else {
@@ -92,7 +92,7 @@ v8::Handle<v8::Value> V8AudioContext::constructorCallback(const v8::Arguments& a
         ExceptionCode ec = 0;
         audioContext = AudioContext::createOfflineContext(document, numberOfChannels, numberOfFrames, sampleRate, ec);
         if (ec)
-            return throwError(ec, args.GetIsolate());
+            return V8Proxy::setDOMException(ec, args.GetIsolate());
     }
 
     if (!audioContext.get())
--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8XMLHttpRequestCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8XMLHttpRequestCustom.cpp
@@ -58,7 +58,7 @@ v8::Handle<v8::Value> V8XMLHttpRequest::responseTextAccessorGetter(v8::Local<v8:
     ExceptionCode ec = 0;
     const String& text = xmlHttpRequest->responseText(ec);
     if (ec)
-        return throwError(ec, info.GetIsolate());
+        return V8Proxy::setDOMException(ec, info.GetIsolate());
     return v8String(text, info.GetIsolate());
 }
 
@@ -144,7 +144,7 @@ v8::Handle<v8::Value> V8XMLHttpRequest::openCallback(const v8::Arguments& args)
         xmlHttpRequest->open(method, url, ec);
 
     if (ec)
-        return throwError(ec, args.GetIsolate());
+        return V8Proxy::setDOMException(ec, args.GetIsolate());
 
     return v8::Undefined();
 }
@@ -201,7 +201,7 @@ v8::Handle<v8::Value> V8XMLHttpRequest::sendCallback(const v8::Arguments& args)
     }
 
     if (ec)
-        return throwError(ec, args.GetIsolate());
+        return V8Proxy::setDOMException(ec, args.GetIsolate());
 
     return v8::Undefined();
 }
