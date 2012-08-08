8bda9b9be2ac87508c2719d7d7c28e715d1e38aa
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-08 18:36:34 +0000

    Unreviewed build fix for Mac port after http://trac.webkit.org/changeset/125051
    * platform/mac/ClipboardMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125057 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/ClipboardMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/ClipboardMac.mm
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "config.h"
 #import "ClipboardMac.h"
 
+#import "CachedImageClient.h"
 #import "DOMElementInternal.h"
 #import "DragClient.h"
 #import "DragController.h"
