b5d9f246e352a5ff9e3070aa26d02d0575fa0236
hans@chromium.org <hans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 15:13:33 +0000

    AccelerometerMac::GetOrientation(): Fix return statement.
    We need to call release() on the scoped_refptr, otherwise it will
    destroy the underlying object.
    
    BUG=141041
    TEST=used macbook to navigate to page using device orientation and didn't crash
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825245
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150556 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/device_orientation/accelerometer_mac.cc
+++ content/browser/device_orientation/accelerometer_mac.cc
@@ -94,7 +94,7 @@ const Orientation* AccelerometerMac::GetOrientation() {
   DCHECK_GE(orientation->gamma(), -90.0);
   DCHECK_LT(orientation->gamma(),  90.0);
 
-  return orientation;
+  return orientation.release();
 }
 
 bool AccelerometerMac::Init() {
