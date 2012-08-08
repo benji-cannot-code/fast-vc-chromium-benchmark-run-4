aadfa03896b9299baee4383d0d67a3c6c98dc949
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-08 00:10:16 +0000

    sync: Attempt to recover from directory corruption
    Sync when sqlite fails to maintain databsae integrity, most likely due
    to unexpected shutdowns or flaky storage devices. They can also be
    corrupted by logic errors in the sync code, though we hope that's a rare
    occurence.
    
    This patch attempts to improve a user's experience in this scenario by
    transparently deleting the old directory and re-downloading all their
    data. This will be much better than the old behaviours, which included
    silently signing a user out of sync, triggering an unrecoverable error,
    or crashing.
    
    To help catch sync logic bugs, debug builds are set to crash when they
    detect directory corruption.
    
    Also included in this patch is a new test. The
    DirectoryBackingStore.MinorCorruption test exercises the code invoked
    when the directory is successfully re-created.
    
    BUG=109668, 129825
    
    
    Review URL: https://chromiumcodereview.appspot.com/10821121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150465 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/syncable/directory_backing_store.cc
+++ sync/syncable/directory_backing_store.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 
 #include "base/base64.h"
+#include "base/debug/trace_event.h"
 #include "base/file_util.h"
 #include "base/hash_tables.h"
 #include "base/logging.h"
@@ -1082,6 +1083,37 @@ bool DirectoryBackingStore::CreateShareInfoTableVersion71(
       "next_id INT default -2, "
       "cache_guid TEXT )");
   return db_->Execute(query.c_str());
+}
+
+// This function checks to see if the given list of Metahandles has any nodes
+// whose PREV_ID, PARENT_ID or NEXT_ID values refer to ID values that do not
+// actually exist.  Returns true on success.
+bool DirectoryBackingStore::VerifyReferenceIntegrity(
+    const syncable::MetahandlesIndex &index) {
+  TRACE_EVENT0("sync", "SyncDatabaseIntegrityCheck");
+  using namespace syncable;
+  typedef base::hash_set<std::string> IdsSet;
+
+  IdsSet ids_set;
+  bool is_ok = true;
+
+  for (MetahandlesIndex::const_iterator it = index.begin();
+       it != index.end(); ++it) {
+    EntryKernel* entry = *it;
+    bool is_duplicate_id = !(ids_set.insert(entry->ref(ID).value()).second);
+    is_ok = is_ok && !is_duplicate_id;
+  }
+
+  IdsSet::iterator end = ids_set.end();
+  for (MetahandlesIndex::const_iterator it = index.begin();
+       it != index.end(); ++it) {
+    EntryKernel* entry = *it;
+    bool prev_exists = (ids_set.find(entry->ref(PREV_ID).value()) != end);
+    bool parent_exists = (ids_set.find(entry->ref(PARENT_ID).value()) != end);
+    bool next_exists = (ids_set.find(entry->ref(NEXT_ID).value()) != end);
+    is_ok = is_ok && prev_exists && parent_exists && next_exists;
+  }
+  return is_ok;
 }
 
 }  // namespace syncable
--- sync/test/test_directory_backing_store.cc
+++ sync/test/test_directory_backing_store.cc
@@ -36,6 +36,8 @@ DirOpenResult TestDirectoryBackingStore::Load(
     return FAILED_DATABASE_CORRUPT;
   if (!LoadInfo(kernel_load_info))
     return FAILED_DATABASE_CORRUPT;
+  if (!VerifyReferenceIntegrity(*entry_bucket))
+    return FAILED_DATABASE_CORRUPT;
 
   return OPENED;
 }
