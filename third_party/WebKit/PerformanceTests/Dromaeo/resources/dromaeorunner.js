787f53fa4a528dec2943d1461cd1bccb7074029c
zoltan@webkit.org <zoltan@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-09 16:22:05 +0000

    Add memory measurement to balls.html and dromaeorunner.js https://bugs.webkit.org/show_bug.cgi?id=93617
    Reviewed by Adam Barth.
    
    Since we added memory measurement support to performance tests we need to modify these tests.
    
    * Animation/balls.html:
    * Dromaeo/resources/dromaeorunner.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PerformanceTests/Dromaeo/resources/dromaeorunner.js
+++ third_party/WebKit/PerformanceTests/Dromaeo/resources/dromaeorunner.js
@@ -63,7 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
          teardown: function(data) {
              var scores = DRT.computeScores(data.result);
-             PerfTestRunner.printStatistics(scores);
+             PerfTestRunner.printStatistics(scores, "Time:");
+             PerfTestRunner.getAndPrintMemoryStatistics();
              window.setTimeout(function() {
                  if (window.testRunner)
                      testRunner.notifyDone();
