99674f65ea293462ce90c9204d1b5326406bf621
kseo@webkit.org <kseo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-09 02:35:43 +0000

    Extract the definition of NativeImagePtr from ImageSource.h into NativeImagePtr.h. https://bugs.webkit.org/show_bug.cgi?id=93466
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-08-08
    Reviewed by Adam Barth.
    
    Image.h includes ImageSource.h in order to know the definition of NativeImagePtr.
    But Image does not need to know the definition of ImageSource. So, this patch
    extracts the definition of NativeImagePtr from ImageSource.h into NativeImagePtr.h.
    
    This is a preparation for asynchronous image decoding. Async decoding
    will need a callback function, which sends a NativeImagePtr to clients, and the
    callback must know the definition of NativeImagePtr, not ImageSource.
    
    No new tests, behavior is unchanged.
    
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * platform/graphics/BitmapImage.h:
    * platform/graphics/Image.h:
    * platform/graphics/ImageSource.h:
    (WebCore):
    * platform/graphics/NativeImagePtr.h: Added.
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/Image.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/Image.h
@@ -31,8 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Color.h"
 #include "ColorSpace.h"
 #include "GraphicsTypes.h"
-#include "ImageSource.h"
+#include "ImageOrientation.h"
 #include "IntRect.h"
+#include "NativeImagePtr.h"
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
--- third_party/WebKit/Source/WebCore/platform/graphics/cairo/GraphicsContext3DCairo.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cairo/GraphicsContext3DCairo.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Extensions3DOpenGL.h"
 #include "GraphicsContext3DPrivate.h"
 #include "Image.h"
+#include "ImageSource.h"
 #include "NotImplemented.h"
 #include "OpenGLShims.h"
 #include "PlatformContextCairo.h"
