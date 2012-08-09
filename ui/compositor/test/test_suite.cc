05b7164917d3cb882064f5ff3b51500c1a6efef8
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 05:35:04 +0000

    Use WebKit Platform API header locations instead of client API
    This cuts down on the possible include paths for compositor.
    
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825259
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150747 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/test/test_suite.cc
+++ ui/compositor/test/test_suite.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/test/test_suite.h"
 
 #include "base/message_loop.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/compositor/compositor.h"
 #include "ui/compositor/test/compositor_test_support.h"
--- ui/compositor/test_web_graphics_context_3d.h
+++ ui/compositor/test_web_graphics_context_3d.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebGraphicsContext3D.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebGraphicsContext3D.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
 #include "ui/compositor/compositor_export.h"
 
 namespace gfx {
