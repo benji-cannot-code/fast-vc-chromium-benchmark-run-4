5e2a92aa25399227878ae865fb9b4c2373a03902
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-09 21:35:55 +0000

    nrwt: should log progress like ninja does https://bugs.webkit.org/show_bug.cgi?id=93563
    Reviewed by Ojan Vafai.
    
    This change modifies the output of nrwt in the
    non--debug-rwt-logging modes.
    
    In non-verbose mode, the output changes from:
      "Testing (50%): 96 ran as expected, 4 didn't, 100 left"
    to:
      "[100/200] foo.html (+4)"
    or:
      "[100/200] foo.html failed  unexpectedly (text diff)"
    
    which indicates the earliest test currently running (optionally plus the
    count of other currently-executing tests if we're running tests in parallel)
    or the result of said test.
    
    If the test produces unexpected results, that line is flushed
    rather than erased (so we no longer need the %d didn't running
    tally).
    
    In --verbose mode, the output is similar, but every test gets a
    line (and tests are logged in order of them being started, not
    order of them being completed) (and in --details mode, the
    first line of each test matches --verbose but the other lines
    are unchanged).
    
    The overall result should be easier to read and is simpler to
    implement as well (apart from managing the number of tests that
    have started but not completed, which is slightly annoying).
    
    This also matches the output style for test-webkitpy, more-or-less.
    
    * Scripts/webkitpy/layout_tests/controllers/layout_test_runner.py:
    (LayoutTestRunner.run_tests):
    (LayoutTestRunner._update_summary_with_result):
    (LayoutTestRunner._handle_started_test):
    * Scripts/webkitpy/layout_tests/controllers/layout_test_runner_unittest.py:
    (FakePrinter):
    (FakePrinter.print_started_test):
    (FakePrinter.print_finished_test):
    * Scripts/webkitpy/layout_tests/views/printing.py:
    (Printer.__init__):
    (Printer.print_started_test):
    (Printer.print_finished_test):
    (Printer._print_test_trace):
    (Printer._print_progress):
    * Scripts/webkitpy/layout_tests/views/printing_unittest.py:
    (test_details):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125204 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/layout_test_runner.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/layout_test_runner.py
@@ -102,6 +102,8 @@ class LayoutTestRunner(object):
         self._needs_websockets = needs_websockets
         self._retrying = retrying
         self._test_files_list = [test_input.test_name for test_input in test_inputs]
+        self._printer.num_tests = len(self._test_files_list)
+        self._printer.num_completed = 0
 
         self._all_results = []
         self._group_stats = {}
@@ -203,8 +205,7 @@ class LayoutTestRunner(object):
 
         result_summary.add(result, expected, self._test_is_slow(result.test_name))
 
-        # FIXME: there's too many arguments to this function.
-        self._printer.print_finished_test(result, expected, exp_str, got_str, result_summary, self._retrying, self._test_files_list)
+        self._printer.print_finished_test(result, expected, exp_str, got_str)
 
         self._interrupt_if_at_failure_limits(result_summary)
 
@@ -238,8 +239,7 @@ class LayoutTestRunner(object):
         raise AssertionError('unknown message %s received from %s, args=%s' % (name, source, repr(args)))
 
     def _handle_started_test(self, worker_name, test_input, test_timeout_sec):
-        # FIXME: log that we've started another test.
-        pass
+        self._printer.print_started_test(test_input.test_name)
 
     def _handle_finished_test_list(self, worker_name, list_name, num_tests, elapsed_time):
         self._group_stats[list_name] = (num_tests, elapsed_time)
