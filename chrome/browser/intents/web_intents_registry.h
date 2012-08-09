440397e117c48c058fb6c4301344770d7ad14e29
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 01:06:29 +0000

    Make GetIntentServicesForExtensionFilter work synchronously
    Since it does not require any IO tasks, it should not involved posting a task. 
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10854019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intents/web_intents_registry.h
+++ chrome/browser/intents/web_intents_registry.h
@@ -85,7 +85,7 @@ class WebIntentsRegistry : public ProfileKeyedService {
   void GetIntentServicesForExtensionFilter(const string16& action,
                                            const string16& type,
                                            const std::string& extension_id,
-                                           const QueryCallback& callback);
+                                           IntentServiceList* services);
 
   // Record the given default service entry.
   virtual void RegisterDefaultIntentService(
@@ -143,12 +143,6 @@ class WebIntentsRegistry : public ProfileKeyedService {
       const QueryParams& params,
       const DefaultQueryCallback& callback,
       const WDTypedResult* result);
-
-  // Implementation of GetIntentServicesForExtensionFilter.
-  void DoGetIntentServicesForExtensionFilter(
-      const QueryParams& params,
-      const std::string& extension_id,
-      const QueryCallback& callback);
 
   const extensions::Extension* ExtensionForURL(const std::string& url);
 
