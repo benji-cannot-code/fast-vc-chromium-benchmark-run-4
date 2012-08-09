001886484d823858fcb7553c76a38acf0552976b
shouqun.liu@intel.com <shouqun.liu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 11:52:30 +0000

    Add shortcut to create new window for GTK content shell.
    Use Control+N to create new content shell window.
    
    BUG=90445,120153
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10855070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150781 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_gtk.cc
+++ content/shell/shell_gtk.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
 #include "content/public/common/renderer_preferences.h"
+#include "content/shell/shell_browser_context.h"
+#include "content/shell/shell_content_browser_client.h"
 #include "content/shell/shell_switches.h"
 #include "third_party/skia/include/core/SkColor.h"
 
@@ -82,6 +84,12 @@ void Shell::PlatformCreateWindow(int width, int height) {
       GTK_ACCEL_VISIBLE,
       g_cclosure_new(G_CALLBACK(OnCloseWindowKeyPressedThunk),
                      this, NULL));
+
+  gtk_accel_group_connect(
+      accel_group, GDK_n, GDK_CONTROL_MASK,
+      GTK_ACCEL_VISIBLE,
+      g_cclosure_new(G_CALLBACK(OnNewWindowKeyPressedThunk),
+                    this, NULL));
 
   GtkWidget* toolbar = gtk_toolbar_new();
   // Turn off the labels on the toolbar buttons.
@@ -222,6 +230,21 @@ gboolean Shell::OnCloseWindowKeyPressed(GtkAccelGroup* accel_group,
                                         guint keyval,
                                         GdkModifierType modifier) {
   gtk_widget_destroy(GTK_WIDGET(window_));
+  return TRUE;
+}
+
+gboolean Shell::OnNewWindowKeyPressed(GtkAccelGroup* accel_group,
+                                      GObject* acceleratable,
+                                      guint keyval,
+                                      GdkModifierType modifier) {
+  ShellBrowserContext* browser_context =
+      static_cast<ShellContentBrowserClient*>(
+        GetContentClient()->browser())->browser_context();
+  Shell::CreateNewWindow(browser_context,
+                         GURL(),
+                         NULL,
+                         MSG_ROUTING_NONE,
+                         NULL);
   return TRUE;
 }
 
