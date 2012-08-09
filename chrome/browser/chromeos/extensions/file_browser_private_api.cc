fa887ed33e345428ff78dccdb9e0f0001a5f8442
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 16:13:38 +0000

    FileBrowser: Hide DriveApps on Open-with menu when the selected file is not on drive.
    BUG=139699
    
    Review URL: https://chromiumcodereview.appspot.com/10832185
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150806 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_private_api.cc
+++ chrome/browser/chromeos/extensions/file_browser_private_api.cc
@@ -341,6 +341,7 @@ void CreateDriveTasks(
     if (!best_icon.is_empty()) {
       task->SetString("iconUrl", best_icon.spec());
     }
+    task->SetBoolean("driveApp", true);
     result_list->Append(task);
   }
 }
@@ -697,6 +698,7 @@ bool GetFileTasksFileBrowserFunction::RunImpl() {
                                         ExtensionIconSet::MATCH_BIGGER,
                                         false, NULL);     // grayscale
     task->SetString("iconUrl", icon.spec());
+    task->SetBoolean("driveApp", false);
     result_list->Append(task);
   }
 
