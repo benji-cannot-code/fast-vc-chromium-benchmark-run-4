1318e9a4b8ffabe1d05a0e4c12032d76755ada26
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 02:57:14 +0000

    Linux: update base/third_party/xdg_mime from upstream.
    BUG=none
    TEST=none
    Review URL: https://chromiumcodereview.appspot.com/10824186
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150696 0039d316-1c4b-4281-b951-d872f2087c98

--- base/third_party/xdg_mime/xdgmimealias.c
+++ base/third_party/xdg_mime/xdgmimealias.c
@@ -27,7 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #ifdef HAVE_CONFIG_H
-#include "config.h"
+#include <config.h>
 #endif
 
 #include "xdgmimealias.h"
