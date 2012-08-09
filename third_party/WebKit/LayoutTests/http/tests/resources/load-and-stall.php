1d2b776c6f9c45a66d463177d16dc3781c395d98
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-09 06:23:42 +0000

    Source/WebCore: Remove image decoding in some BitmapImage metadata functions https://bugs.webkit.org/show_bug.cgi?id=93171
    Reviewed by Simon Fraser.
    
    These two metadata functions were decoding an entire frame:
    - frameOrientationAtIndex
    - frameHasAlphaAtIndex
    
    This change removes image decoding from these two methods. This is for
    preparation of having asynchronous image decoding, intending to reduce
    code location that trigger image decoding.
    
    frameOrientationAtIndex() doesn't require decoding a frame. This method
    is only implemented in CG port in ImageSourceCG.cpp which doesn't do
    image decoding.
    
    frameHasAlphaAtIndex() is used to optimize certain drawing operations
    and accelerated compositing. This change uses a heuristic for non-CG
    port to determine if an image has alpha. If an image is not yet
    decoded the function answers having alpha. Only if a frame is decoded
    and cached that the alpha state of the frame is returned. This is an
    admissible heuristic that postpone answering the question until a frame
    is decoded.
    
    Tested this change with a fully loaded image and partially loaded image
    with background color.
    
    Test: http/tests/images/jpg-img-partial-load.html
    
    * platform/graphics/BitmapImage.cpp:
    (WebCore::BitmapImage::frameHasAlphaAtIndex):
    (WebCore::BitmapImage::frameOrientationAtIndex):
    * platform/graphics/ImageSource.cpp:
    (WebCore::ImageSource::frameHasAlphaAtIndex):
    * platform/image-decoders/ImageDecoder.cpp:
    (WebCore::ImageDecoder::frameHasAlphaAtIndex):
    (WebCore):
    * platform/image-decoders/ImageDecoder.h:
    (ImageDecoder):
    
    LayoutTests: Test rendering JPEG <img> with CSS background
    https://bugs.webkit.org/show_bug.cgi?id=93171
    
    Reviewed by Simon Fraser.
    
    Tests defer answering the alpha state for BitmapImage not yet
    decoded doesn't affect rendering results.
    
    Test: http/tests/images/jpg-img-partial-load.html
    
    Tests the rendering results of a partially loaded JPEG image.
    The image should be green in the top region and blue in the
    lower region. The blue is the background color revealed by
    full alpha of the undecoded region.
    
    * fast/images/resources/green-256x256.jpg: Added.
    * http/tests/images/jpg-img-partial-load-expected.png: Added.
    * http/tests/images/jpg-img-partial-load-expected.txt: Added.
    * http/tests/images/jpg-img-partial-load.html: Added.
    * http/tests/resources/load-and-stall.php: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/resources/load-and-stall.php
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+$name = $_GET['name'];
+$stallAt = $_GET['stallAt'];
+$stallFor = $_GET['stallFor'];
+$mimeType = $_GET['mimeType'];
+
+$file = fopen($name, "rb");
+if (!$file)
+    die("Cannot open file.");
+
+header("Content-Type: " . $mimeType);
+header("Content-Length: " . filesize($name));
+
+if (isset($stallAt) && isset($stallFor)) {
+    $stallAt = (int)$stallAt;
+    $stallFor = (int)$stallFor;
+    if ($stallAt > filesize($name))
+        die("Incorrect value for stallAt.");
+
+    while ($written < $stallAt) {
+        $write = 1024;
+        if ($write > $stallAt - $written)
+            $write = $stallAt - $written;
+
+        echo(fread($file, $write));
+        $written += $write;
+        flush();
+        ob_flush();
+    }
+    sleep($stallFor);
+    echo(fread($file, filesize($name) - $stallAt));
+} else {
+    echo(fread($file, filesize($name)));
+}
+
+fclose($file);
+?>
