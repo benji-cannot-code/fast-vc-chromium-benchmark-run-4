1ddddcf6778e437b7b7c02402f83473cad5c4b1b
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 07:24:46 +0000

    Remove SyncManager::Init() return value
    The return value was almost entirely unusued.  Success or failure is
    reported through a parameter in the asynchronous callback function.
    
    BUG=140180
    
    
    Review URL: https://chromiumcodereview.appspot.com/10826193
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/sync_backend_host.cc
+++ chrome/browser/sync/glue/sync_backend_host.cc
@@ -1047,8 +1047,9 @@ void SyncBackendHost::Core::DoInitialize(const DoInitializeOptions& options) {
 
   // Make sure that the directory exists before initializing the backend.
   // If it already exists, this will do no harm.
-  bool success = file_util::CreateDirectory(sync_data_folder_path_);
-  DCHECK(success);
+  if (!file_util::CreateDirectory(sync_data_folder_path_)) {
+    DLOG(FATAL) << "Sync Data directory creation failed.";
+  }
 
   DCHECK(!registrar_);
   registrar_ = options.registrar;
@@ -1060,7 +1061,7 @@ void SyncBackendHost::Core::DoInitialize(const DoInitializeOptions& options) {
 
   sync_manager_ = options.sync_manager_factory->CreateSyncManager(name_);
   sync_manager_->AddObserver(this);
-  success = sync_manager_->Init(
+  sync_manager_->Init(
       sync_data_folder_path_,
       options.event_handler,
       options.service_url.host() + options.service_url.path(),
@@ -1084,7 +1085,6 @@ void SyncBackendHost::Core::DoInitialize(const DoInitializeOptions& options) {
       &encryptor_,
       options.unrecoverable_error_handler,
       options.report_unrecoverable_error_function);
-  LOG_IF(ERROR, !success) << "Sync manager initialization failed!";
 
   // Now check the command line to see if we need to simulate an
   // unrecoverable error for testing purpose. Note the error is thrown
--- sync/internal_api/public/sync_manager.h
+++ sync/internal_api/public/sync_manager.h
@@ -364,7 +364,7 @@ class SyncManager {
   //
   // TODO(akalin): Replace the |post_factory| parameter with a
   // URLFetcher parameter.
-  virtual bool Init(
+  virtual void Init(
       const FilePath& database_location,
       const WeakHandle<JsEventHandler>& event_handler,
       const std::string& sync_server_and_path,
