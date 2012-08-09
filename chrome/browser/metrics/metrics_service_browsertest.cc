369061a50f3ff699db7aeba336e0308ec65d9204
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 23:07:37 +0000

    Inform GetEntropySource of whether or not metrics reporting will be enabled.
    This resolves an issue where we were calling GetEntropySource too early before MetricsService::Start was called. Instead of checking the reporting_active member in MetricsService, we instead just check if the criteria for metrics reporting is allowed or not, ahead of time.
    
    BUG=141628
    
    
    Review URL: https://chromiumcodereview.appspot.com/10830241
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150934 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service_browsertest.cc
+++ chrome/browser/metrics/metrics_service_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/path_service.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
@@ -54,6 +55,14 @@ class MetricsServiceTest : public InProcessBrowserTest {
         net::FilePathToFileURL(page2_path),
         NEW_FOREGROUND_TAB,
         kBrowserTestFlags);
+  }
+};
+
+class MetricsServiceReportingTest : public InProcessBrowserTest {
+ public:
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    // Enable the metrics service for testing (in the full mode).
+    command_line->AppendSwitch(switches::kEnableMetricsReportingForTesting);
   }
 };
 
@@ -104,4 +113,22 @@ IN_PROC_BROWSER_TEST_F(MetricsServiceTest, MAYBE_CrashRenderers) {
   // TODO(isherman): We should also verify that prefs::kStabilityExitedCleanly
   // is set to true, but this preference isn't set until the browser
   // exits... it's not clear to me how to test that.
+}
+
+IN_PROC_BROWSER_TEST_F(MetricsServiceTest, CheckLowEntropySourceUsed) {
+  // Since MetricsService is only in recording mode, and is not reporting,
+  // check that the low entropy source is returned at some point.
+  ASSERT_TRUE(g_browser_process->metrics_service());
+  EXPECT_EQ(MetricsService::LAST_ENTROPY_LOW,
+            g_browser_process->metrics_service()->entropy_source_returned());
+}
+
+IN_PROC_BROWSER_TEST_F(MetricsServiceReportingTest,
+                       CheckHighEntropySourceUsed) {
+  // Since the full metrics service runs in this test, we expect that
+  // MetricsService returns the full entropy source at some point during
+  // BrowserMain startup.
+  ASSERT_TRUE(g_browser_process->metrics_service());
+  EXPECT_EQ(MetricsService::LAST_ENTROPY_HIGH,
+            g_browser_process->metrics_service()->entropy_source_returned());
 }
