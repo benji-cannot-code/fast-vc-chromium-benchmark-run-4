2efcc75dd4011778d5f01b6e7d825f3916be7b0b
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 16:01:34 +0000

    reland http://codereview.chromium.org/10830063/
    refactor EnumerateDevices to make it a persistent request.
    After the requester calls EnumerateDevices, it should expect new device list as initial response and whenever devices are changed.
    A new method StopEnumerateDevices is also added to allow the request to stop device enumeration.
    
    It also includes corresponding change in Pepper since EnumerateDevices contract is changed (http://codereview.chromium.org/10837064/).
    This patch keeps current Pepper API, while using the new API of EnumerateDevices from MediaStreamDispatcher. 
    Pepper calls StopEnumerateDevices when it receives the first enumerated result. 
    
    A new Pepper API will be created in a separate patch to make EnumerateDevices persistent. 
    
    BUG=137799
    Review URL: https://chromiumcodereview.appspot.com/10828230
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150803 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/media_stream_dispatcher_host.cc
+++ content/browser/renderer_host/media/media_stream_dispatcher_host.cc
@@ -101,10 +101,9 @@ void MediaStreamDispatcherHost::DevicesEnumerated(
   StreamMap::iterator it = streams_.find(label);
   DCHECK(it != streams_.end());
   StreamRequest request = it->second;
-  streams_.erase(it);
 
   Send(new MediaStreamMsg_DevicesEnumerated(
-      request.render_view_id, request.page_request_id, devices));
+      request.render_view_id, request.page_request_id, label, devices));
 }
 
 void MediaStreamDispatcherHost::DeviceOpened(
@@ -197,7 +196,6 @@ void MediaStreamDispatcherHost::OnStopGeneratedStream(
            << ", {label = " << label <<  "})";
 
   StreamMap::iterator it = streams_.find(label);
-  DCHECK(it != streams_.end());
   GetManager()->StopGeneratedStream(label);
   streams_.erase(it);
 }
