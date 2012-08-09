9a8521a6195f1fdae83c0f1533e8bbff9bbdf4cc
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 20:50:18 +0000

    gdata: Make callback parameters mandatory for TransferFile functions
    The public functions TransferFileFromRemoteToLocal() and
    TransferFileFromLocalToRemote() are called from file_browser_private_api.cc
    with callbacks, hence it's safe to make the callback parameter mandatory.
    
    BUG=126634, 139446
    TEST=out/Release/unit_tests --gtest_filter=GData*
    
    Review URL: https://chromiumcodereview.appspot.com/10824240
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150896 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system.h
+++ chrome/browser/chromeos/gdata/gdata_file_system.h
@@ -202,6 +202,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
   // TransferRegularFile.
   //
   // Must be called from *UI* thread. |callback| is run on the calling thread.
+  // |callback| must not be null.
   void TransferFileForResourceId(const FilePath& local_file_path,
                                  const FilePath& remote_dest_file_path,
                                  const FileOperationCallback& callback,
@@ -322,6 +323,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
   // |local_dest_file_path|.
   //
   // Can be called from UI thread. |callback| is run on the calling thread.
+  // |callback| must not be null.
   void OnGetFileCompleteForTransferFile(const FilePath& local_dest_file_path,
                                         const FileOperationCallback& callback,
                                         GDataFileError error,
@@ -347,6 +349,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
   // and names the copied document as |new_name|.
   //
   // Can be called from UI thread. |callback| is run on the calling thread.
+  // |callback| must not be null.
   void CopyDocumentToDirectory(const FilePath& dir_path,
                                const std::string& resource_id,
                                const FilePath::StringType& new_name,
@@ -414,6 +417,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
       const FilePath& cache_file_path);
 
   // Callback for handling document copy attempt.
+  // |callback| must not be null.
   void OnCopyDocumentCompleted(const FilePath& dir_path,
                                const FileOperationCallback& callback,
                                GDataErrorCode status,
@@ -743,7 +747,7 @@ class GDataFileSystem : public GDataFileSystemInterface,
                                  const base::Closure& callback);
 
   // Part of CopyOnUIThread(). Called after GetEntryInfoPairByPaths() is
-  // complete.
+  // complete. |callback| must not be null.
   void CopyOnUIThreadAfterGetEntryInfoPair(
     const FilePath& dest_file_path,
     const FileOperationCallback& callback,
