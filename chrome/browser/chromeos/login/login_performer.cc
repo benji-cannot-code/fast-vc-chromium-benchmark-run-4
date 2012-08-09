83552a9a21ece7760e735758043b97e8dca9951b
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-09 00:48:44 +0000

    Remove metrics collection from Kiosk Mode. Metrics collection for KioskMode has been replaced by data pulled from Analytics. Remove the current metrics reporting since it's broken and bloat.
    Reviews requested:
    satorux@ - Main reivew + OWNER's review for ChromeOS
    sky@ - OWNER's review for everything else :)
    
    R=satorux@chromium.org,sky@chromium.org
    BUG=126878
    TEST=Kiosk mode works as before.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825257
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150657 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_performer.cc
+++ chrome/browser/chromeos/login/login_performer.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
-#include "chrome/browser/chromeos/kiosk_mode/kiosk_mode_metrics.h"
 #include "chrome/browser/chromeos/login/login_utils.h"
 #include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
@@ -124,7 +123,6 @@ void LoginPerformer::OnLoginFailure(const LoginFailure& failure) {
 void LoginPerformer::OnDemoUserLoginSuccess() {
   content::RecordAction(
       UserMetricsAction("Login_DemoUserLoginSuccess"));
-  KioskModeMetrics::Get()->SessionStarted();
 
   LoginStatusConsumer::OnDemoUserLoginSuccess();
 }
