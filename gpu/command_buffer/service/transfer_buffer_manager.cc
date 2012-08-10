a614dc45fbcee720e584a6ecf240809be7c529ac
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 03:47:22 +0000

    Fix transfer buffer accounting
    BUG=141550
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825288
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150987 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/transfer_buffer_manager.cc
+++ gpu/command_buffer/service/transfer_buffer_manager.cc
@@ -26,11 +26,12 @@ TransferBufferManager::TransferBufferManager()
 TransferBufferManager::~TransferBufferManager() {
   for (size_t i = 0; i < registered_objects_.size(); ++i) {
     if (registered_objects_[i].shared_memory) {
+      DCHECK(shared_memory_bytes_allocated_ >= registered_objects_[i].size);
       shared_memory_bytes_allocated_ -= registered_objects_[i].size;
       delete registered_objects_[i].shared_memory;
     }
   }
-  // TODO(gman): Should we report 0 bytes to TRACE here?
+  DCHECK(!shared_memory_bytes_allocated_);
 }
 
 bool TransferBufferManager::Initialize() {
@@ -42,10 +43,6 @@ int32 TransferBufferManager::CreateTransferBuffer(
   SharedMemory buffer;
   if (!buffer.CreateAnonymous(size))
     return -1;
-
-  shared_memory_bytes_allocated_ += size;
-  TRACE_COUNTER_ID1(
-      "CommandBuffer", "SharedMemory", this, shared_memory_bytes_allocated_);
 
   return RegisterTransferBuffer(&buffer, size, id_request);
 }
@@ -81,6 +78,10 @@ int32 TransferBufferManager::RegisterTransferBuffer(
   buffer.ptr = duped_shared_memory->memory();
   buffer.size = size;
   buffer.shared_memory = duped_shared_memory.release();
+
+  shared_memory_bytes_allocated_ += size;
+  TRACE_COUNTER_ID1(
+      "CommandBuffer", "SharedMemory", this, shared_memory_bytes_allocated_);
 
   // If caller requested specific id, first try to use id_request.
   if (id_request != -1) {
@@ -121,6 +122,7 @@ void TransferBufferManager::DestroyTransferBuffer(int32 handle) {
   if (static_cast<size_t>(handle) >= registered_objects_.size())
     return;
 
+  DCHECK(shared_memory_bytes_allocated_ >= registered_objects_[handle].size);
   shared_memory_bytes_allocated_ -= registered_objects_[handle].size;
   TRACE_COUNTER_ID1(
       "CommandBuffer", "SharedMemory", this, shared_memory_bytes_allocated_);
