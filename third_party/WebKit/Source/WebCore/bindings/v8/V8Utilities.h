615c570743202e5ec5768892ba6841d3af4efeda
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-10 02:13:55 +0000

    [V8] V8Utilities::throwTypeMismatchException() should use setDOMException() https://bugs.webkit.org/show_bug.cgi?id=93610
    Reviewed by Adam Barth.
    
    Given that V8Utilities::throwTypeMismatchException() throws a DOM exception,
    we should use setDOMException() instead of throwError().
    
    This patch also renames throwTypeMismatchException() to setTypeMismatchException().
    
    No tests. I couldn't figure out how to trigger the call path.
    Either way this is just a minor change of error message.
    
    * bindings/v8/V8Utilities.cpp:
    (WebCore::setTypeMismatchException):
    * bindings/v8/V8Utilities.h:
    (WebCore::createFunctionOnlyCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Utilities.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Utilities.h
@@ -61,7 +61,7 @@ namespace WebCore {
 
     ScriptExecutionContext* getScriptExecutionContext();
 
-    void throwTypeMismatchException(v8::Isolate*);
+    void setTypeMismatchException(v8::Isolate*);
 
     enum CallbackAllowedValueFlag {
         CallbackAllowUndefined = 1,
@@ -103,7 +103,7 @@ namespace WebCore {
 
         if (!value->IsFunction()) {
             succeeded = false;
-            throwTypeMismatchException(isolate);
+            setTypeMismatchException(isolate);
             return 0;
         }
 
