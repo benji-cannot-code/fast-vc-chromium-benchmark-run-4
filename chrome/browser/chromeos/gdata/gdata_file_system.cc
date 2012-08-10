ca20212dc1e6be373fbf2ed52e508ae7931958b9
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 08:34:44 +0000

    Remove parent* arg from GDataEntry ctor.
    * Remove static FromDocumentEntry from GDataEntry, GDataFile, GDataDirectory. Replace with InitFromDocumentEntry.
    * Move common code from GDataFile::InitFromDocumentEntry and GDataDirectory::InitFromDocumentEntry to GDataEntry::InitFromDocumentEntry.
    * Add GDataDirectoryService::FromDocumentEntry and use this everywhere.
    * Make ctors of GDataFile, GDataDirectory private, so these must be created by GDataDirectoryService's CreateGDataFile and
    CreateGDataDirectory. Make GDataEntry ctor protected.
    
    BUG=141494
    TEST=unit tests.
    Review URL: https://chromiumcodereview.appspot.com/10854083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151008 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_file_system.cc
+++ chrome/browser/chromeos/gdata/gdata_file_system.cc
@@ -1746,9 +1746,7 @@ void GDataFileSystem::OnGetDocumentEntry(const FilePath& cache_file_path,
   if (error == GDATA_FILE_OK) {
     scoped_ptr<DocumentEntry> doc_entry(DocumentEntry::ExtractAndParse(*data));
     if (doc_entry.get()) {
-      fresh_entry.reset(
-          GDataEntry::FromDocumentEntry(NULL, doc_entry.get(),
-                                        directory_service_.get()));
+      fresh_entry.reset(directory_service_->FromDocumentEntry(doc_entry.get()));
     }
     if (!fresh_entry.get() || !fresh_entry->AsGDataFile()) {
       LOG(ERROR) << "Got invalid entry from server for " << params.resource_id;
@@ -2341,8 +2339,7 @@ void GDataFileSystem::OnSearch(const SearchCallback& callback,
   // result directory.
   for (size_t i = 0; i < feed->entries().size(); ++i) {
     DocumentEntry* doc = const_cast<DocumentEntry*>(feed->entries()[i]);
-    scoped_ptr<GDataEntry> entry(
-        GDataEntry::FromDocumentEntry(NULL, doc, directory_service_.get()));
+    scoped_ptr<GDataEntry> entry(directory_service_->FromDocumentEntry(doc));
 
     if (!entry.get())
       continue;
@@ -2481,8 +2478,7 @@ void GDataFileSystem::OnCopyDocumentCompleted(
     return;
   }
 
-  GDataEntry* entry = GDataEntry::FromDocumentEntry(
-      NULL, doc_entry.get(), directory_service_.get());
+  GDataEntry* entry = directory_service_->FromDocumentEntry(doc_entry.get());
   if (!entry) {
     callback.Run(GDATA_FILE_ERROR_FAILED);
     return;
@@ -2828,8 +2824,8 @@ GDataFileError GDataFileSystem::AddNewDirectory(
   if (!parent_dir)
     return GDATA_FILE_ERROR_FAILED;
 
-  GDataEntry* new_entry = GDataEntry::FromDocumentEntry(
-      NULL, doc_entry.get(), directory_service_.get());
+  GDataEntry* new_entry =
+      directory_service_->FromDocumentEntry(doc_entry.get());
   if (!new_entry)
     return GDATA_FILE_ERROR_FAILED;
 
@@ -2951,8 +2947,7 @@ void GDataFileSystem::AddUploadedFileOnUIThread(
     return;
 
   scoped_ptr<GDataEntry> new_entry(
-      GDataEntry::FromDocumentEntry(
-          NULL, entry.get(), directory_service_.get()));
+      directory_service_->FromDocumentEntry(entry.get()));
   if (!new_entry.get())
     return;
 
