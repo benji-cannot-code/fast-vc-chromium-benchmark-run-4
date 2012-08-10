cd0ac1bfb7f5663941b7beba189d5b59cd72ac21
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 16:04:57 +0000

    run_test_cases.py: Enable use of RUN_TEST_CASES_RESULT_FILE as environment variable.
    Much more strict on what is allowable for GTEST_* environment variables.
    Enable support for GTEST_FILTER.
    Workaround non utf-8 output.
    
    fix_test_cases.py:
    Work better with run_test_cases.py inside isolate.py.
    Runs all the test cases simultaneously, so it is much faster.
    
    R=cmp@chromium.org
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151043 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/fix_test_cases.py
+++ tools/isolate/fix_test_cases.py
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 """Scripts to run a test, grab the failures and trace them."""
 
+import json
 import os
 import subprocess
 import sys
+import tempfile
 
 import run_test_cases
 
@@ -49,6 +51,29 @@ def trace_and_merge(result, test):
       [sys.executable, 'isolate.py', 'merge', '-r', result])
 
 
+def run_all(result):
+  """Runs all the tests. Returns the tests that failed or None on failure.
+
+  Assumes run_test_cases.py is implicitly called.
+  """
+  handle, result_file = tempfile.mkstemp(prefix='run_test_cases')
+  os.close(handle)
+  env = os.environ.copy()
+  env['RUN_TEST_CASES_RESULT_FILE'] = result_file
+  subprocess.call(
+      [sys.executable, 'isolate.py', 'run', '-r', result], env=env)
+  if not os.path.isfile(result_file):
+    print >> sys.stderr, 'Failed to find %s' % result_file
+    return None
+  with open(result_file) as f:
+    data = json.load(f)
+  os.remove(result_file)
+  return [
+    test for test, runs in data.iteritems()
+    if not any(not run['returncode'] for run in runs)
+  ]
+
+
 def run(result, test):
   """Runs a single test case in an isolated environment.
 
@@ -56,7 +81,7 @@ def run(result, test):
   """
   return not subprocess.call([
     sys.executable, 'isolate.py', 'run', '-r', result,
-    '--', '--gtest_filter=' + test
+    '--', '--gtest_filter=' + test,
   ])
 
 
@@ -76,14 +101,28 @@ def trace_and_verify(result, test):
   return run(result, test)
 
 
-def run_all(result, executable):
+def fix_all(result):
   """Runs all the test cases in a gtest executable and trace the failing tests.
 
-  Then make sure the test passes afterward.
+  Returns True on success.
+
+  Makes sure the test passes afterward.
   """
-  test_cases = run_test_cases.list_test_cases(
-      executable, 0, 0, False, False, False)
-  print 'Found %d test cases.' % len(test_cases)
+  # These could have adverse side-effects.
+  # TODO(maruel): Be more intelligent about it, for now be safe.
+  for i in run_test_cases.KNOWN_GTEST_ENV_VARS:
+    if i in os.environ:
+      print >> 'Please unset %s' % i
+      return False
+
+  test_cases = run_all(result)
+  if test_cases is None:
+    return False
+
+  print '\nFound %d broken test cases.' % len(test_cases)
+  if not test_cases:
+    return True
+
   failures = []
   fixed_tests = []
   try:
@@ -136,7 +175,7 @@ def main():
         '%s doesn\'t exist, please build %s_run' % (result, basename))
     return 1
 
-  return not run_all(result, executable)
+  return not fix_all(result)
 
 
 if __name__ == '__main__':
