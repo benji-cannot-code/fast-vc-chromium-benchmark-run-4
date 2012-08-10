717155e2edf9d9ff80c580b5bfb644777611fc45
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 18:15:56 +0000

    Ensure that the correct open gl context is made current before attempting to write to the open gl textures in the DXVA decoder on windows.
    This fixes the problem with flashing in HTML5 videos with gpu compositing turned on.
    
    BUG=141233
    R=fischman
    Review URL: https://chromiumcodereview.appspot.com/10826238
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151089 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/dxva_video_decode_accelerator.h
+++ content/common/gpu/media/dxva_video_decode_accelerator.h
@@ -41,7 +41,8 @@ class CONTENT_EXPORT DXVAVideoDecodeAccelerator
 
   // Does not take ownership of |client| which must outlive |*this|.
   explicit DXVAVideoDecodeAccelerator(
-      media::VideoDecodeAccelerator::Client* client);
+      media::VideoDecodeAccelerator::Client* client,
+      const base::Callback<bool(void)>& make_context_current);
   virtual ~DXVAVideoDecodeAccelerator();
 
   // media::VideoDecodeAccelerator implementation.
@@ -201,6 +202,9 @@ class CONTENT_EXPORT DXVAVideoDecodeAccelerator
   // 1. All required decoder dlls were successfully loaded.
   // 2. The device manager initialization completed.
   static bool pre_sandbox_init_done_;
+
+  // Callback to set the correct gl context.
+  base::Callback<bool(void)> make_context_current_;
 };
 
 #endif  // CONTENT_COMMON_GPU_MEDIA_DXVA_VIDEO_DECODE_ACCELERATOR_H_
