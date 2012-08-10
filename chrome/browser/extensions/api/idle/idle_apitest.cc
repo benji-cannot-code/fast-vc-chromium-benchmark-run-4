0d491caa480eaeeab7a05ad1d57d9de7532a1e45
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-10 06:05:31 +0000

    Disable ExtensionApiTest.Idle on all platforms, since it's flaky everywhere.
    BUG=130138
    TBR=brg@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828248
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@150998 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/idle/idle_apitest.cc
+++ chrome/browser/extensions/api/idle/idle_apitest.cc
@@ -7,11 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "net/base/mock_host_resolver.h"
 
-// Sometimes this test fails on Linux: crbug.com/130138
-#if defined(OS_LINUX) || defined(OS_CHROMEOS)
+// This test is flaky: crbug.com/130138
 IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_Idle) {
-#else
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, Idle) {
-#endif
   ASSERT_TRUE(RunExtensionTest("idle")) << message_;
 }
