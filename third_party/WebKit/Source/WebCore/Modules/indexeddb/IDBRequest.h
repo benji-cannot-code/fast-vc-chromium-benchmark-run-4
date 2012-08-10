a5a999c6b34d84218ca9c5785ff4fd62700101ab
dgrogan@chromium.org <dgrogan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-10 01:32:05 +0000

    IndexedDB: intversion-long-queue.html fails an assert https://bugs.webkit.org/show_bug.cgi?id=92783
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    onBlocked needs the same shouldEnqueueEvent check that the other
    event generators have.
    
    Tests: intversion-long-queue is no longer skipped.
    
    * Modules/indexeddb/IDBRequest.h:
    (IDBRequest):
    * Modules/indexeddb/IDBVersionChangeRequest.cpp:
    (WebCore::IDBVersionChangeRequest::onBlocked):
    
    LayoutTests:
    
    * platform/chromium/TestExpectations:
    * storage/indexeddb/intversion-long-queue-expected.txt:
    Add some FAIL lines that will be fixed when patch from 92897 lands.
    This patch doesn't actually change any behavior,
    intversion-long-queue-expected.txt was generated with 92897 patched in
    and the test was skipped so it didn't matter.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125231 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBRequest.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBRequest.h
@@ -109,14 +109,14 @@ public:
 protected:
     IDBRequest(ScriptExecutionContext*, PassRefPtr<IDBAny> source, IDBTransaction*);
     void enqueueEvent(PassRefPtr<Event>);
+    bool shouldEnqueueEvent() const;
+
     RefPtr<IDBAny> m_result;
     unsigned short m_errorCode;
     String m_errorMessage;
     RefPtr<DOMError> m_error;
 
 private:
-    bool shouldEnqueueEvent() const;
-
     // EventTarget
     virtual void refEventTarget() { ref(); }
     virtual void derefEventTarget() { deref(); }
--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBVersionChangeRequest.cpp
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBVersionChangeRequest.cpp
@@ -59,6 +59,8 @@ const AtomicString& IDBVersionChangeRequest::interfaceName() const
 
 void IDBVersionChangeRequest::onBlocked()
 {
+    if (!shouldEnqueueEvent())
+        return;
     ASSERT(!m_errorCode && m_errorMessage.isNull() && !m_result);
     enqueueEvent(IDBVersionChangeEvent::create(m_version, eventNames().blockedEvent));
 }
