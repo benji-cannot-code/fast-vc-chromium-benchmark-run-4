da8d4df755a4964eea4efad1d531eddc65a4ab74
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-11 00:02:26 +0000

    Revert 151153 - Partial revert of 146621
    The original patch introduced a crash (GetCompositor() can return NULL), but
    reverting the whole thing is not possible without a lot of other reverts, and
    the proper fix is fairly involve (likely not reasonable to patch to the branch).
    
    This only reverts the problematic part, and a follow up will do a proper fix.
    
    BUG=141756
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828262
    
    TBR=piman@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10824270
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151159 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/layer.h
+++ ui/compositor/layer.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebContentLayerClient.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebExternalTextureLayerClient.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebLayer.h"
 #include "third_party/skia/include/core/SkColor.h"
 #include "third_party/skia/include/core/SkRegion.h"
@@ -44,7 +45,8 @@ class Texture;
 // NULL, but the children are not deleted.
 class COMPOSITOR_EXPORT Layer
     : public LayerAnimationDelegate,
-      NON_EXPORTED_BASE(public WebKit::WebContentLayerClient) {
+      NON_EXPORTED_BASE(public WebKit::WebContentLayerClient),
+      NON_EXPORTED_BASE(public WebKit::WebExternalTextureLayerClient) {
  public:
   Layer();
   explicit Layer(LayerType type);
@@ -260,6 +262,11 @@ class COMPOSITOR_EXPORT Layer
 #endif
 
   WebKit::WebLayer web_layer() { return web_layer_; }
+
+  // WebExternalTextureLayerClient
+  virtual unsigned prepareTexture(
+      WebKit::WebTextureUpdater& /* updater */) OVERRIDE;
+  virtual WebKit::WebGraphicsContext3D* context() OVERRIDE;
 
   float device_scale_factor() const { return device_scale_factor_; }
 
