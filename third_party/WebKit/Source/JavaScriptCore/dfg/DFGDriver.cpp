8db6096e3e9d424b40adf54f385aceb4ca336d32
allan.jensen@nokia.com <allan.jensen@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-12 11:47:34 +0000

    Doesn't build with ENABLE_JIT=0 https://bugs.webkit.org/show_bug.cgi?id=85042
    Reviewed by Eric Seidel.
    
    Include headers without which CallFrame.h does not build, and
    fix gcc warning about comparing unsigned int with 0.
    
    * dfg/DFGDriver.cpp:
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::isOpcode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGDriver.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGDriver.cpp
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "DFGDriver.h"
 
+#include "JSObject.h"
+#include "JSString.h"
+#include "ScopeChain.h"
+
 #if ENABLE(DFG_JIT)
 
 #include "DFGArgumentsSimplificationPhase.h"
