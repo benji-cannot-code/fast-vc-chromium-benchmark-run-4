fea5b519ca739a0f1c52909b3f380a52aba7495e
ibraaaa@google.com <ibraaaa@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 15:07:38 +0000

    Preprocessing version number for plugins to ignore leading zeros during comparisons. This eliminates wrong version number comparisons for versions like 1.7.0_05 vs. 1.7.0_5
    BUGR=137388
    
    Review URL: https://chromiumcodereview.appspot.com/10828279
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151265 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_group.cc
+++ webkit/plugins/npapi/plugin_group.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/plugins/npapi/plugin_group.h"
 
 #include "base/memory/linked_ptr.h"
+#include "base/string_split.h"
 #include "base/string_util.h"
 #include "base/sys_string_conversions.h"
 #include "base/utf_string_conversions.h"
@@ -101,6 +102,27 @@ bool PluginGroup::Match(const WebPluginInfo& plugin) const {
 }
 
 /* static */
+std::string PluginGroup::RemoveLeadingZerosFromVersionComponents(
+    const std::string& version) {
+  std::string no_leading_zeros_version;
+  std::vector<std::string> numbers;
+  base::SplitString(version, '.', &numbers);
+  for (size_t i = 0; i < numbers.size(); ++i) {
+    size_t n = numbers[i].size();
+    size_t j = 0;
+    while (j < n && numbers[i][j] == '0') {
+      ++j;
+    }
+    no_leading_zeros_version += (j < n) ? numbers[i].substr(j) : "0";
+    if (i != numbers.size() - 1) {
+      no_leading_zeros_version += ".";
+    }
+  }
+
+  return no_leading_zeros_version;
+}
+
+/* static */
 void PluginGroup::CreateVersionFromString(const string16& version_string,
                                           Version* parsed_version) {
   // Remove spaces and ')' from the version string,
@@ -112,6 +134,9 @@ void PluginGroup::CreateVersionFromString(const string16& version_string,
   std::replace(version.begin(), version.end(), ',', '.');
   std::replace(version.begin(), version.end(), '(', '.');
   std::replace(version.begin(), version.end(), '_', '.');
+
+  // Remove leading zeros from each of the version components.
+  version = RemoveLeadingZerosFromVersionComponents(version);
 
   *parsed_version = Version(version);
 }
