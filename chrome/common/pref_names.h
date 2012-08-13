ca439c383561eb372dcb994b2a439e7ceccba5a9
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 21:17:54 +0000

    Add Chromoting curtain-mode policy.
    BUG=110111
    
    
    Review URL: https://chromiumcodereview.appspot.com/10827265
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151350 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -613,6 +613,7 @@ extern const char kRemoteAccessHostFirewallTraversal[];
 extern const char kRemoteAccessHostRequireTwoFactor[];
 extern const char kRemoteAccessHostDomain[];
 extern const char kRemoteAccessHostTalkGadgetPrefix[];
+extern const char kRemoteAccessHostRequireCurtain[];
 
 extern const char kPrintPreviewStickySettings[];
 extern const char kCloudPrintServiceURL[];
--- remoting/host/policy_hack/policy_watcher.cc
+++ remoting/host/policy_hack/policy_watcher.cc
@@ -98,6 +98,9 @@ scoped_ptr<base::DictionaryValue> AddDefaultValuesWhenNecessary(
   CopyStringOrDefault(to.get(), from,
                       PolicyWatcher::kHostTalkGadgetPrefixPolicyName,
                       kDefaultTalkGadgetPrefix, kDefaultTalkGadgetPrefix);
+  CopyBooleanOrDefault(to.get(), from,
+                       PolicyWatcher::kHostRequireCurtainPolicyName,
+                       false, false);
 
   return to.Pass();
 }
@@ -115,6 +118,9 @@ const char PolicyWatcher::kHostDomainPolicyName[] =
 
 const char PolicyWatcher::kHostTalkGadgetPrefixPolicyName[] =
     "RemoteAccessHostTalkGadgetPrefix";
+
+const char PolicyWatcher::kHostRequireCurtainPolicyName[] =
+    "RemoteAccessHostRequireCurtain";
 
 const char* const PolicyWatcher::kBooleanPolicyNames[] =
     { PolicyWatcher::kNatPolicyName,
