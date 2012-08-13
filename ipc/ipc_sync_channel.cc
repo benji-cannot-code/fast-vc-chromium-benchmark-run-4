eb55f3506977fb4d2adc594ef6b0f2dc336e602b
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 09:41:19 +0000

    Add moar logging for "Couldn't load plug-in" errors.
    BUG=141055
    
    Review URL: https://chromiumcodereview.appspot.com/10854092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151246 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_sync_channel.cc
+++ ipc/ipc_sync_channel.cc
@@ -302,9 +302,15 @@ bool SyncChannel::SyncContext::TryToUnblockListener(const Message* msg) {
     return false;
   }
 
+  // TODO(bauerb): Remove logging once investigation of http://crbug.com/141055
+  // has finished.
   if (!msg->is_reply_error()) {
-    deserializers_.back().send_result = deserializers_.back().deserializer->
+    bool send_result = deserializers_.back().deserializer->
         SerializeOutputParameters(*msg);
+    deserializers_.back().send_result = send_result;
+    LOG_IF(ERROR, !send_result) << "Couldn't deserialize reply message";
+  } else {
+    LOG(ERROR) << "Received error reply";
   }
   deserializers_.back().done_event->Signal();
 
@@ -358,6 +364,7 @@ void SyncChannel::SyncContext::OnChannelClosed() {
 void SyncChannel::SyncContext::OnSendTimeout(int message_id) {
   base::AutoLock auto_lock(deserializers_lock_);
   PendingSyncMessageQueue::iterator iter;
+  LOG(ERROR) << "Send timeout";
   for (iter = deserializers_.begin(); iter != deserializers_.end(); iter++) {
     if (iter->id == message_id) {
       iter->done_event->Signal();
@@ -369,6 +376,7 @@ void SyncChannel::SyncContext::OnSendTimeout(int message_id) {
 void SyncChannel::SyncContext::CancelPendingSends() {
   base::AutoLock auto_lock(deserializers_lock_);
   PendingSyncMessageQueue::iterator iter;
+  LOG(ERROR) << "Canceling pending sends";
   for (iter = deserializers_.begin(); iter != deserializers_.end(); iter++)
     iter->done_event->Signal();
 }
@@ -428,6 +436,7 @@ bool SyncChannel::SendWithTimeout(Message* message, int timeout_ms) {
   // *this* might get deleted in WaitForReply.
   scoped_refptr<SyncContext> context(sync_context());
   if (context->shutdown_event()->IsSignaled()) {
+    LOG(ERROR) << "shutdown event is signaled";
     delete message;
     return false;
   }
