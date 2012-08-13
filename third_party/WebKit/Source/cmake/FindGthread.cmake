81f8ec3ae94e37327224f8708be247532d6bc4b8
rakuco@webkit.org <rakuco@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-13 20:38:06 +0000

    [CMake] Remove glib-related Find modules and write single new one instead. https://bugs.webkit.org/show_bug.cgi?id=93786
    Reviewed by Rob Buis.
    
    .:
    
    As part of the ongoing effort to write proper Find modules that use
    absolute include and library paths instead of the short ones from
    pkg-config, replace Find{GIO,Glib,Gthread}.cmake with a single
    FindGLIB.cmake that optionally detects components such as GIO and
    GObject.
    
    Library paths are now defined as "/full/path/to/libfoo.so" instead of
    simply "foo", so that the linker receives a full path and we can then
    avoid accidentally picking up a libfoo.so installed into /usr/lib
    instead of a local one built with jhbuild.
    
    * Source/cmake/FindGIO.cmake: Removed.
    * Source/cmake/FindGLIB.cmake: Added.
    * Source/cmake/FindGlib.cmake: Removed.
    * Source/cmake/FindGthread.cmake: Removed.
    * Source/cmake/OptionsEfl.cmake: Look for GLIB with the GIO component.
    
    Source/JavaScriptCore:
    
    * shell/PlatformEfl.cmake: Use GLIB_* instead of Glib_*.
    
    Source/WebCore:
    
    No new tests, build system plumbing.
    
    * PlatformEfl.cmake: Use GLIB_* instead of Glib_*, and link
    directly against GIO, as code in the soup network backend uses
    that.
    
    Source/WebKit:
    
    * PlatformEfl.cmake: Use GLIB_* instead of Glib_*.
    
    Source/WebKit2:
    
    * PlatformEfl.cmake: Use GLIB_* instead of Glib_*.
    
    Source/WTF:
    
    * wtf/PlatformEfl.cmake: Use GLIB_* instead of Glib_*, and link directly to GIO, as wtf/gobject uses functions from it.
    
    Tools:
    
    * DumpRenderTree/efl/CMakeLists.txt: Use GLIB_* instead of Glib_*.
    * EWebLauncher/CMakeLists.txt: Ditto.
    * MiniBrowser/efl/CMakeLists.txt: Ditto.
    * WebKitTestRunner/PlatformEfl.cmake: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmake/FindGthread.cmake
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Find include and libraries for Gthread library
-
-INCLUDE(FindPkgConfig)
-PKG_CHECK_MODULES (Gthread REQUIRED gthread-2.0>=2.20.0)
