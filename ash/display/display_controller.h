b475c91e2b635b7b2d0d7fef20f49864d1342150
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 23:09:26 +0000

    Add a unit test which verifies if the resizer sets/resets DisplayController::dont_warp_mouse_ as expected.
    BUG=None
    TEST=try
    
    
    Review URL: https://chromiumcodereview.appspot.com/10826240
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151381 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_controller.h
+++ ash/display/display_controller.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/ash_export.h"
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "ui/aura/display_observer.h"
 #include "ui/aura/display_manager.h"
 
@@ -91,6 +92,8 @@ class ASH_EXPORT DisplayController : public aura::DisplayObserver {
   static bool IsExtendedDesktopEnabled();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(WorkspaceWindowResizerTest, WarpMousePointer);
+
   // Creates a root window for |display| and stores it in the |root_windows_|
   // map.
   aura::RootWindow* AddRootWindowForDisplay(const gfx::Display& display);
