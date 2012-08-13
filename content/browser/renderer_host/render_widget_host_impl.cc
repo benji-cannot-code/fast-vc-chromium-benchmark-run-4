3be055b7be19b3e15f2f09ba79af6b069cbffa71
husky@chromium.org <husky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 17:56:16 +0000

    Throttle SelectRange IPC
    This code is used by the Android port. We add a SelectRange_ACK to
    throttle the rate at which the selection changes. Without throttling,
    the selection handle updates can fall behind the user's touch input.
    
    BUG=138939
    
    
    Review URL: https://chromiumcodereview.appspot.com/10823077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151304 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -131,6 +131,7 @@ RenderWidgetHostImpl::RenderWidgetHostImpl(RenderWidgetHostDelegate* delegate,
       should_auto_resize_(false),
       mouse_move_pending_(false),
       mouse_wheel_pending_(false),
+      select_range_pending_(false),
       needs_repainting_on_restore_(false),
       is_unresponsive_(false),
       in_flight_event_count_(0),
@@ -286,6 +287,7 @@ bool RenderWidgetHostImpl::OnMessageReceived(const IPC::Message &msg) {
     IPC_MESSAGE_HANDLER(ViewHostMsg_UpdateIsDelayed, OnMsgUpdateIsDelayed)
     IPC_MESSAGE_HANDLER(ViewHostMsg_HandleInputEvent_ACK, OnMsgInputEventAck)
     IPC_MESSAGE_HANDLER(ViewHostMsg_BeginSmoothScroll, OnMsgBeginSmoothScroll)
+    IPC_MESSAGE_HANDLER(ViewHostMsg_SelectRange_ACK, OnMsgSelectRangeAck)
     IPC_MESSAGE_HANDLER(ViewHostMsg_Focus, OnMsgFocus)
     IPC_MESSAGE_HANDLER(ViewHostMsg_Blur, OnMsgBlur)
     IPC_MESSAGE_HANDLER(ViewHostMsg_HasTouchEventHandlers,
@@ -1084,6 +1086,10 @@ void RenderWidgetHostImpl::RendererExited(base::TerminationStatus status,
   mouse_wheel_pending_ = false;
   coalesced_mouse_wheel_events_.clear();
 
+  // Must reset these to ensure that SelectRange works with a new renderer.
+  select_range_pending_ = false;
+  next_selection_range_.reset();
+
   // Must reset these to ensure that gesture events work with a new renderer.
   gesture_event_filter_->Reset();
 
@@ -1587,6 +1593,14 @@ void RenderWidgetHostImpl::TickActiveSmoothScrollGesture() {
   }
 }
 
+void RenderWidgetHostImpl::OnMsgSelectRangeAck() {
+  select_range_pending_ = false;
+  if (next_selection_range_.get()) {
+    scoped_ptr<SelectionRange> next(next_selection_range_.Pass());
+    SelectRange(next->start, next->end);
+  }
+}
+
 void RenderWidgetHostImpl::ProcessWheelAck(bool processed) {
   mouse_wheel_pending_ = false;
 
@@ -1879,6 +1893,16 @@ void RenderWidgetHostImpl::ScrollFocusedEditableNodeIntoRect(
 
 void RenderWidgetHostImpl::SelectRange(const gfx::Point& start,
                                        const gfx::Point& end) {
+  if (select_range_pending_) {
+    if (!next_selection_range_.get()) {
+      next_selection_range_.reset(new SelectionRange());
+    }
+    next_selection_range_->start = start;
+    next_selection_range_->end = end;
+    return;
+  }
+
+  select_range_pending_ = true;
   Send(new ViewMsg_SelectRange(GetRoutingID(), start, end));
 }
 
