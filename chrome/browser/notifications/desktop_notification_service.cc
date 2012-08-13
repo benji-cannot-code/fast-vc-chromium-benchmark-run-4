4de20be768bf4bf5005da0ee60f3aad2ab317561
stevenjb@google.com <stevenjb@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 17:56:45 +0000

    Correctly remove file browser notifications from Ash notification tray.
    Also added replace_id to DesktopNotificationService::AddNotification so that notifications are correctly replaced/updated.
    
    BUG=135286
    
    For minor changes to status_icons/ background/ and printing/:
    TBR=atwilson@chromium.org,thestig@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10824265
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151305 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notification_service.cc
+++ chrome/browser/notifications/desktop_notification_service.cc
@@ -239,6 +239,7 @@ std::string DesktopNotificationService::AddNotification(
     const string16& title,
     const string16& message,
     const GURL& icon_url,
+    const string16& replace_id,
     NotificationDelegate* delegate,
     Profile* profile) {
 #if defined(USE_ASH)
@@ -246,7 +247,7 @@ std::string DesktopNotificationService::AddNotification(
     // For Ash create a non-HTML notification with |icon_url|.
     Notification notification(GURL(), icon_url, title, message,
                               WebKit::WebTextDirectionDefault,
-                              string16(), string16(), delegate);
+                              string16(), replace_id, delegate);
     g_browser_process->notification_ui_manager()->Add(notification, profile);
     return notification.notification_id();
   }
@@ -255,7 +256,7 @@ std::string DesktopNotificationService::AddNotification(
   GURL content_url(CreateDataUrl(
       icon_url, title, message, WebKit::WebTextDirectionDefault));
   Notification notification(
-      GURL(), content_url, string16(), string16(), delegate);
+      GURL(), content_url, string16(), replace_id, delegate);
   g_browser_process->notification_ui_manager()->Add(notification, profile);
   return notification.notification_id();
 }
@@ -266,6 +267,7 @@ std::string DesktopNotificationService::AddIconNotification(
     const string16& title,
     const string16& message,
     const gfx::ImageSkia& icon,
+    const string16& replace_id,
     NotificationDelegate* delegate,
     Profile* profile) {
 #if defined(USE_ASH)
@@ -273,7 +275,7 @@ std::string DesktopNotificationService::AddIconNotification(
     // For Ash create a non-HTML notification with |icon|.
     Notification notification(GURL(), icon, title, message,
                               WebKit::WebTextDirectionDefault,
-                              string16(), string16(), delegate);
+                              string16(), replace_id, delegate);
     g_browser_process->notification_ui_manager()->Add(notification, profile);
     return notification.notification_id();
   }
@@ -282,7 +284,13 @@ std::string DesktopNotificationService::AddIconNotification(
   if (!icon.empty())
     icon_url = GURL(web_ui_util::GetImageDataUrl(icon));
   return AddNotification(
-      origin_url, title, message, icon_url, delegate, profile);
+      origin_url, title, message, icon_url, replace_id, delegate, profile);
+}
+
+// static
+void DesktopNotificationService::RemoveNotification(
+    const std::string& notification_id) {
+    g_browser_process->notification_ui_manager()->CancelById(notification_id);
 }
 
 DesktopNotificationService::DesktopNotificationService(Profile* profile,
