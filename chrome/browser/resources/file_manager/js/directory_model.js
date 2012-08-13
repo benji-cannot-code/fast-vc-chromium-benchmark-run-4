892c488419297cf356223f7ad2b60b96a8ac5c7c
oleg@chromium.org <oleg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 15:04:07 +0000

    In FileManager send 'on-completed' event only when a scan is completed. Send 'on-cancelled' event when a scan is cancelled. Make search & usual spinner work with these events.
    BUG=141381
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824277
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151263 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_model.js
+++ chrome/browser/resources/file_manager/js/directory_model.js
@@ -172,6 +172,13 @@ DirectoryModel.prototype.isReadOnly = function() {
 };
 
 /**
+ * @return {boolean} True if the a scan is active.
+ */
+DirectoryModel.prototype.isScanning = function() {
+  return this.currentDirContents_.isScanning();
+};
+
+/**
  * @return {boolean} True if search is in progress.
  */
 DirectoryModel.prototype.isSearching = function() {
@@ -461,6 +468,7 @@ DirectoryModel.prototype.scan_ = function(dirContents, successCallback) {
 
   dirContents.addEventListener('scan-completed', onSuccess);
   dirContents.addEventListener('scan-failed', onFailure);
+  dirContents.addEventListener('scan-cancelled', this.dispatchEvent.bind(this));
   dirContents.scan();
 };
 
