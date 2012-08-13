9d218c7252d44f877490716091988f09c3528f0e
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-13 20:17:57 +0000

    Media(Stream|Element)AudioSourceNode should be guarded by the WEB_AUDIO condition as well https://bugs.webkit.org/show_bug.cgi?id=93864
    Reviewed by Eric Carlson.
    
    Add the WEB_AUDIO to the Conditional attribute in MediaElementAudioSourceNode
    IDL file and the ENABLE(WEB_AUDIO) compilation guard to the header files for both
    MediaStreamAudioSourceNode and MediaElementAudioSourceNode.
    
    No new tests - no new functionality.
    
    * Modules/webaudio/MediaElementAudioSourceNode.h:
    * Modules/webaudio/MediaStreamAudioSourceNode.h:
    * Modules/webaudio/MediaStreamAudioSourceNode.idl:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/MediaElementAudioSourceNode.h
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/MediaElementAudioSourceNode.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MediaElementAudioSourceNode_h
 #define MediaElementAudioSourceNode_h
 
-#if ENABLE(VIDEO)
+#if ENABLE(WEB_AUDIO) && ENABLE(VIDEO)
 
 #include "AudioSourceNode.h"
 #include "AudioSourceProviderClient.h"
@@ -75,6 +75,6 @@ private:
 
 } // namespace WebCore
 
-#endif // ENABLE(VIDEO)
+#endif // ENABLE(WEB_AUDIO) && ENABLE(VIDEO)
 
 #endif // MediaElementAudioSourceNode_h
