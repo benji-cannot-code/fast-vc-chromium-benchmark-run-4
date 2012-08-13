87937efe5944ad1dcb973a0b36635a8cdecef1f5
oleg@chromium.org <oleg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 16:22:19 +0000

    Fix the right margin for search breadcrumbs.
    BUG=142332
    
    
    Review URL: https://chromiumcodereview.appspot.com/10837220
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2208,8 +2208,8 @@ FileManager.prototype = {
 
     // Now this.selection is complete. Update buttons.
     this.updateCommonActionButtons_();
-    this.updateSearchBreadcrumbs_();
     this.updatePreviewPanelVisibility_();
+    this.updateSearchBreadcrumbs_();
     forcedShowTimeout = setTimeout(showThumbnails,
         FileManager.THUMBNAIL_SHOW_DELAY);
     onThumbnailLoaded();
