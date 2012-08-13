e9bec25ffcd0cc674fb6d11b809d751ac82435d2
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 21:04:08 +0000

    Revert "TLS channel id field trial."
    This reverts commit 145978.
    
    BUG=136462,142388
    TBR=rch@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10826284
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151347 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -109,7 +109,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/platform_locale_settings.h"
 #include "net/base/net_module.h"
 #include "net/base/sdch_manager.h"
-#include "net/base/ssl_config_service.h"
 #include "net/cookies/cookie_monster.h"
 #include "net/http/http_stream_factory.h"
 #include "net/spdy/spdy_session.h"
