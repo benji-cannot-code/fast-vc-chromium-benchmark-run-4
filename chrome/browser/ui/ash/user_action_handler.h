d54354aa6cbcee43438823aab1d79fb190d3c42d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-13 00:45:55 +0000

    ash: Fix include paths and header guards.
    BUG=125846
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10823280
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/user_action_handler.h
+++ chrome/browser/ui/ash/user_action_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_VIEWS_ASH_USER_ACTION_HANDLER_H_
-#define CHROME_BROWSER_UI_VIEWS_ASH_USER_ACTION_HANDLER_H_
+#ifndef CHROME_BROWSER_UI_ASH_USER_ACTION_HANDLER_H_
+#define CHROME_BROWSER_UI_ASH_USER_ACTION_HANDLER_H_
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
@@ -23,4 +23,4 @@ class UserActionHandler : public aura::client::UserActionClient {
   DISALLOW_COPY_AND_ASSIGN(UserActionHandler);
 };
 
-#endif  // CHROME_BROWSER_UI_VIEWS_ASH_USER_ACTION_HANDLER_H_
+#endif  // CHROME_BROWSER_UI_ASH_USER_ACTION_HANDLER_H_
