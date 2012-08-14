a567b2c25b642260224ce355c0c3a2b2f7c5b53c
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 23:32:29 +0000

    Revert 151586 - [net/dns] Resolve AF_UNSPEC on dual-stacked systems. Sort addresses according to RFC3484.
    BUG=113993
    TEST=./net_unittests --gtest_filter=AddressSorter*:HostResolverImplDnsTest.DnsTaskUnspec
    
    Review URL: https://chromiumcodereview.appspot.com/10442098
    
    TBR=szym@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10855163
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151603 0039d316-1c4b-4281-b951-d872f2087c98

--- net/dns/dns_response_unittest.cc
+++ net/dns/dns_response_unittest.cc
@@ -300,7 +300,7 @@ TEST(DnsResponseTest, ParseToAddressList) {
     DnsResponse response(t.response_data, t.response_size, t.query_size);
     AddressList addr_list;
     base::TimeDelta ttl;
-    EXPECT_EQ(DnsResponse::DNS_PARSE_OK,
+    EXPECT_EQ(DnsResponse::DNS_SUCCESS,
               response.ParseToAddressList(&addr_list, &ttl));
     std::vector<const char*> expected_addresses(
         t.expected_addresses,
@@ -430,9 +430,8 @@ TEST(DnsResponseTest, ParseToAddressListFail) {
       DnsResponse::DNS_CNAME_AFTER_ADDRESS },
     { kResponseTTLMismatch, arraysize(kResponseTTLMismatch),
       DnsResponse::DNS_ADDRESS_TTL_MISMATCH },
-    // Not actually a failure, just an empty result.
     { kResponseNoAddresses, arraysize(kResponseNoAddresses),
-      DnsResponse::DNS_PARSE_OK },
+      DnsResponse::DNS_NO_ADDRESSES },
   };
 
   const size_t kQuerySize = 12 + 7;
--- net/dns/dns_test_util.h
+++ net/dns/dns_test_util.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NET_DNS_DNS_TEST_UTIL_H_
 #define NET_DNS_DNS_TEST_UTIL_H_
 
-#include <string>
-#include <vector>
-
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "net/dns/dns_config_service.h"
@@ -170,25 +167,8 @@ static const int kT3TTL = 0x00000015;
 static const unsigned kT3RecordCount = arraysize(kT3IpAddresses) + 2;
 
 class DnsClient;
-
-struct MockDnsClientRule {
-  enum Result {
-    FAIL_SYNC,   // Fail synchronously with ERR_NAME_NOT_RESOLVED.
-    FAIL_ASYNC,  // Fail asynchronously with ERR_NAME_NOT_RESOLVED.
-    EMPTY,       // Return an empty response.
-    OK,          // Return a response with loopback address.
-  };
-
-  std::string prefix;
-  uint16 qtype;
-  Result result;
-};
-
-typedef std::vector<MockDnsClientRule> MockDnsClientRuleList;
-
 // Creates mock DnsClient for testing HostResolverImpl.
-scoped_ptr<DnsClient> CreateMockDnsClient(const DnsConfig& config,
-                                          const MockDnsClientRuleList& rules);
+scoped_ptr<DnsClient> CreateMockDnsClient(const DnsConfig& config);
 
 class MockDnsConfigService : public DnsConfigService {
  public:
