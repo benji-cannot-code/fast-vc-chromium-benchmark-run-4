66b4061fc67198c85a12b24e227d2400a0959fc9
sergio@webkit.org <sergio@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-14 09:28:20 +0000

    [GTK] REGRESSION (r122428) WebKit2APITests/TestWebKitFindController fails "next" test https://bugs.webkit.org/show_bug.cgi?id=91083
    Reviewed by Carlos Garcia Campos.
    
    Source/WebKit2:
    
    After r122428 search_next()/search_prev() should return just 1 match
    if the text is found.
    
    * UIProcess/API/gtk/tests/TestWebKitFindController.cpp:
    (testFindControllerNext):
    (testFindControllerPrevious):
    
    Tools:
    
    Unskipped a couple of API tests that should work fine from now on.
    
    * Scripts/run-gtk-tests:
    (TestRunner):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestWebKitFindController.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/tests/TestWebKitFindController.cpp
@@ -166,14 +166,14 @@ static void testFindControllerNext(FindControllerTest* test, gconstpointer)
     test->waitUntilFindFinished();
 
     g_assert(test->m_textFound);
-    g_assert(test->m_matchCount == 2);
+    g_assert(test->m_matchCount == 1);
     g_assert(!(webkit_find_controller_get_options(test->m_findController.get()) & WEBKIT_FIND_OPTIONS_BACKWARDS));
 
     webkit_find_controller_search_next(test->m_findController.get());
     test->waitUntilFindFinished();
 
     g_assert(!test->m_textFound);
-    g_assert(test->m_matchCount == 2);
+    g_assert(test->m_matchCount == 1);
     g_assert(!(webkit_find_controller_get_options(test->m_findController.get()) & WEBKIT_FIND_OPTIONS_BACKWARDS));
 }
 
@@ -192,14 +192,14 @@ static void testFindControllerPrevious(FindControllerTest* test, gconstpointer)
     test->waitUntilFindFinished();
 
     g_assert(test->m_textFound);
-    g_assert(test->m_matchCount == 2);
+    g_assert(test->m_matchCount == 1);
     g_assert(!(webkit_find_controller_get_options(test->m_findController.get()) & WEBKIT_FIND_OPTIONS_BACKWARDS));
 
     webkit_find_controller_search_previous(test->m_findController.get());
     test->waitUntilFindFinished();
 
     g_assert(test->m_textFound);
-    g_assert(test->m_matchCount == 2);
+    g_assert(test->m_matchCount == 1);
     g_assert(webkit_find_controller_get_options(test->m_findController.get()) & WEBKIT_FIND_OPTIONS_BACKWARDS);
 }
 
