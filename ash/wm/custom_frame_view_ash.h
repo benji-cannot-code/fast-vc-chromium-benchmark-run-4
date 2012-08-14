9cd107d4baf311c71ed8566ee50e88675ee1693a
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 23:33:43 +0000

    Adding unit tests for maximize menu
    BUG=141635
    TEST=Unittest
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825327
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151604 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/custom_frame_view_ash.h
+++ ash/wm/custom_frame_view_ash.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 class FramePainter;
+class FrameMaximizeButton;
 }
 namespace gfx {
 class Font;
@@ -41,7 +42,7 @@ class ASH_EXPORT CustomFrameViewAsh : public views::NonClientFrameView,
      explicit TestApi(CustomFrameViewAsh* frame) : frame_(frame) {
      }
 
-     views::ImageButton* maximize_button() const {
+     ash::FrameMaximizeButton* maximize_button() const {
        return frame_->maximize_button_;
      }
 
@@ -82,7 +83,7 @@ class ASH_EXPORT CustomFrameViewAsh : public views::NonClientFrameView,
   // Not owned.
   views::Widget* frame_;
 
-  views::ImageButton* maximize_button_;
+  ash::FrameMaximizeButton* maximize_button_;
   views::ImageButton* close_button_;
   views::ImageButton* window_icon_;
 
