c0ee8f663ad79ee6d0aebaa472dd3433ad7193ca
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 21:21:48 +0000

    MediaStreamCaptureIndicator: fix crash due to destruction on wrong thread
    BUG=141831
    
    Review URL: https://chromiumcodereview.appspot.com/10825313
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151565 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/media_stream_capture_indicator.h
+++ chrome/browser/media/media_stream_capture_indicator.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/memory/ref_counted.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/extensions/image_loading_tracker.h"
 #include "content/public/common/media_stream_request.h"
 #include "ui/base/models/simple_menu_model.h"
@@ -128,6 +129,9 @@ class MediaStreamCaptureIndicator
   // UpdateStatusTrayIconContextMenu().
   void UpdateStatusTrayIconDisplay(bool audio, bool video);
 
+  // Initializes image loading state.
+  void EnsureImageLoadingTracker();
+
   // Reference to our status icon - owned by the StatusTray. If null,
   // the platform doesn't support status icons.
   StatusIcon* status_icon_;
@@ -142,7 +146,7 @@ class MediaStreamCaptureIndicator
   CaptureDeviceTabs tabs_;
 
   // Tracks the load of extension icons.
-  ImageLoadingTracker tracker_;
+  scoped_ptr<ImageLoadingTracker> tracker_;
   // The messages to display when extension images are loaded. The index
   // corresponds to the index of the associated LoadImage request.
   std::map<int, string16> pending_messages_;
