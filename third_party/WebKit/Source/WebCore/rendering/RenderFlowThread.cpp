93ed4c08695c30c3629c99f5e39e9cc40b9eac48
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-14 20:53:09 +0000

    [CSSRegions]regionOverset is computed as "overset" even though the region is not the last in the chain https://bugs.webkit.org/show_bug.cgi?id=93102
    Patch by Andrei Onea <onea@adobe.com> on 2012-08-14
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Making Element.webkitRegionOverset return "overset" only for the last region in a region chain,
    if the content cannot fit inside it, to reflect current spec. http://www.w3.org/TR/css3-regions/#dom-region-regionoverset
    
    * rendering/RenderFlowThread.cpp:
    (WebCore::RenderFlowThread::computeOverflowStateForRegions):
    
    LayoutTests:
    
    Modified existing tests to reflect spec, where Element.webkitRegionOverset can be
    "overset" only for the last region in a region chain, if the content cannot fit inside it.
    
    * fast/regions/element-region-overset-state-expected.txt:
    * fast/regions/element-region-overset-state-vertical-rl-expected.txt:
    * fast/regions/element-region-overset-state-vertical-rl.html:
    * fast/regions/element-region-overset-state.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFlowThread.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFlowThread.cpp
@@ -682,6 +682,7 @@ void RenderFlowThread::computeOverflowStateForRegions(LayoutUnit oldClientAfterE
     if (hasRenderOverflow())
         height = isHorizontalWritingMode() ? visualOverflowRect().maxY() : visualOverflowRect().maxX();
 
+    RenderRegion* lastReg = lastRegion();
     for (RenderRegionList::iterator iter = m_regionList.begin(); iter != m_regionList.end(); ++iter) {
         RenderRegion* region = *iter;
         if (!region->isValid()) {
@@ -694,7 +695,7 @@ void RenderFlowThread::computeOverflowStateForRegions(LayoutUnit oldClientAfterE
         RenderRegion::RegionState state = RenderRegion::RegionFit;
         if (flowMin <= 0)
             state = RenderRegion::RegionEmpty;
-        if (flowMax > 0)
+        if (flowMax > 0 && region == lastReg)
             state = RenderRegion::RegionOverset;
         region->setRegionState(state);
         // determine whether the NamedFlow object should dispatch a regionLayoutUpdate event
@@ -708,7 +709,6 @@ void RenderFlowThread::computeOverflowStateForRegions(LayoutUnit oldClientAfterE
 
     // With the regions overflow state computed we can also set the overset flag for the named flow.
     // If there are no valid regions in the chain, overset is true
-    RenderRegion* lastReg = lastRegion();
     m_overset = lastReg ? lastReg->regionState() == RenderRegion::RegionOverset : true;
 }
 
