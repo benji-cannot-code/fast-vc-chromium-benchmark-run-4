34afd995191ef3a287eb239bd2e765a7e7ae258d
yutak@chromium.org <yutak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 09:44:38 +0000

    Revert 151351 - Replace the DownloadFileManager with direct ownership of DownloadFileImpl by DownloadItemImpl.
    BUG=123998
    R=benjhayden@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10799005
    
    TBR=rdsmith@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10827314
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151443 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_number_conversions.h"
 #include "base/threading/thread_restrictions.h"
 #include "content/browser/browser_thread_impl.h"
+#include "content/browser/download/download_file_manager.h"
 #include "content/browser/download/save_file_manager.h"
 #include "content/browser/gamepad/gamepad_service.h"
 #include "content/browser/gpu/browser_gpu_channel_host_factory.h"
@@ -553,8 +554,10 @@ void BrowserMainLoop::ShutdownThreadsAndCleanUp() {
 
         // Clean up state that lives on or uses the file_thread_ before
         // it goes away.
-        if (resource_dispatcher_host_.get())
+        if (resource_dispatcher_host_.get()) {
+          resource_dispatcher_host_.get()->download_file_manager()->Shutdown();
           resource_dispatcher_host_.get()->save_file_manager()->Shutdown();
+        }
         break;
       case BrowserThread::PROCESS_LAUNCHER:
         thread_to_stop = &process_launcher_thread_;
