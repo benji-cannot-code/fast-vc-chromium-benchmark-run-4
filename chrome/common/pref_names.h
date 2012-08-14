12a03aa8c97d2850107d052689c2c92b95ca8751
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 09:46:47 +0000

    Allow offset for secondary display position in chrome://settings/display.
    - Adding 'offset' value in the preference and set to display controller
    - Allow offset in Options UI too
    - Redesign the Options UI a bit: now it has "apply" button rather than just applying on mouse-up, and remove 'ok' button.  See crbug.com/130385#c26 for the mock.
    
    R=ben@chromium.org
    BUG=137516,130385
    
    Review URL: https://chromiumcodereview.appspot.com/10836046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -267,6 +267,7 @@ extern const char kOAuth1Token[];
 extern const char kOAuth1Secret[];
 extern const char kEnableCrosDRM[];
 extern const char kSecondaryDisplayLayout[];
+extern const char kSecondaryDisplayOffset[];
 #endif  // defined(OS_CHROMEOS)
 extern const char kIpcDisabledMessages[];
 extern const char kShowHomeButton[];
