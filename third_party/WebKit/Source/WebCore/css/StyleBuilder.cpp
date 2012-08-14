4f99ad99d98d3a013f6f3f4ffce8fc1cd5be0d99
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-14 00:22:42 +0000

    [CSS Exclusions] Remove unused CSSWrapShapes header includes https://bugs.webkit.org/show_bug.cgi?id=93395
    Patch by Bear Travis <betravis@adobe.com> on 2012-08-13
    Reviewed by Kentaro Hara.
    
    Removing the #include "CSSWrapShapes.h" lines from StyleBuilder.cpp and
    RenderStyle.cpp. Both files now use the length-based WrapShapes.h classes.
    
    * css/StyleBuilder.cpp:
    * rendering/style/RenderStyle.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125473 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleBuilder.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleBuilder.cpp
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/UnusedParam.h>
 
 #if ENABLE(CSS_EXCLUSIONS)
-#include "CSSWrapShapes.h"
 #include "WrapShapeFunctions.h"
 #include "WrapShapes.h"
 #endif
--- third_party/WebKit/Source/WebCore/rendering/style/RenderStyle.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/RenderStyle.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContentData.h"
 #include "CursorList.h"
 #include "CSSPropertyNames.h"
-#include "CSSWrapShapes.h"
 #include "FontSelector.h"
 #include "MemoryInstrumentation.h"
 #include "QuotesData.h"
