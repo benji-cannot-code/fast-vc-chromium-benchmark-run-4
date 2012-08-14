1855cbe55f501472e749bea68c0f60d053f9f9b7
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 20:05:56 +0000

    Reland "Fix white flash when user signs out and fix wallpaper animation regression"
    This CL reland CL http://codereview.chromium.org/10827284/. Previous CL was failed on some
    interactive ui tests.
    
    " When user signs out and transit to the login screen, the wallpaper fade in from a white screen.
      Described in 141563, this Cl fixed it.
      Also, after user login, or switch users at login screen. when wallpaper change, the new wallpaper
      should fade in nicely. There is a regression in recent builds. This fix fixed the regression as well. "
    
    BUG=141563, 141676, 142042
    TBR=nkostylev, sky
    
    Review URL: https://chromiumcodereview.appspot.com/10834308
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151548 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/desktop_background_view.cc
+++ ash/desktop_background/desktop_background_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_export.h"
 #include "ash/desktop_background/desktop_background_controller.h"
+#include "ash/desktop_background/desktop_background_widget_controller.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/wm/window_animations.h"
@@ -44,8 +45,16 @@ class ShowWallpaperAnimationObserver : public ui::ImplicitAnimationObserver {
  private:
   // Overridden from ui::ImplicitAnimationObserver:
   virtual void OnImplicitAnimationsCompleted() OVERRIDE {
-    ash::Shell::GetInstance()->
-        user_wallpaper_delegate()->OnWallpaperAnimationFinished();
+    ash::Shell* shell = ash::Shell::GetInstance();
+    shell->user_wallpaper_delegate()->OnWallpaperAnimationFinished();
+    // Only removes old component when wallpaper animation finished. If we
+    // remove the old one too early, there will be a white flash during
+    // animation.
+    if (root_window_->GetProperty(kComponentWrapper)) {
+      internal::DesktopBackgroundWidgetController* component =
+          root_window_->GetProperty(kComponentWrapper)->component();
+      root_window_->SetProperty(kWindowDesktopComponent, component);
+    }
 
     MessageLoop::current()->DeleteSoon(FROM_HERE, this);
   }
@@ -154,8 +163,17 @@ views::Widget* CreateDesktopBackground(aura::RootWindow* root_window,
       ash::Shell::GetInstance()->user_wallpaper_delegate()->GetAnimationType();
   ash::SetWindowVisibilityAnimationType(desktop_widget->GetNativeView(),
                                         animation_type);
-  ash::SetWindowVisibilityAnimationTransition(desktop_widget->GetNativeView(),
-                                              ash::ANIMATE_SHOW);
+  // Disable animation when creating the first widget. Otherwise, wallpaper
+  // will animate from a white screen. Note that boot animation is different.
+  // It animates from a white background.
+  if (animation_type == ash::WINDOW_VISIBILITY_ANIMATION_TYPE_FADE &&
+      NULL == root_window->GetProperty(internal::kComponentWrapper)) {
+    ash::SetWindowVisibilityAnimationTransition(desktop_widget->GetNativeView(),
+                                                ash::ANIMATE_NONE);
+  } else {
+    ash::SetWindowVisibilityAnimationTransition(desktop_widget->GetNativeView(),
+                                                ash::ANIMATE_SHOW);
+  }
   desktop_widget->SetBounds(params.parent->bounds());
   ui::ScopedLayerAnimationSettings settings(
       desktop_widget->GetNativeView()->layer()->GetAnimator());
--- ash/desktop_background/desktop_background_widget_controller.cc
+++ ash/desktop_background/desktop_background_widget_controller.cc
@@ -9,12 +9,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 
 DECLARE_WINDOW_PROPERTY_TYPE(ash::internal::DesktopBackgroundWidgetController*);
+DECLARE_WINDOW_PROPERTY_TYPE(ash::internal::ComponentWrapper*);
 
 namespace ash {
 namespace internal {
 
 DEFINE_OWNED_WINDOW_PROPERTY_KEY(DesktopBackgroundWidgetController,
                                  kWindowDesktopComponent, NULL);
+DEFINE_OWNED_WINDOW_PROPERTY_KEY(ComponentWrapper, kComponentWrapper, NULL);
 
 DesktopBackgroundWidgetController::DesktopBackgroundWidgetController(
     views::Widget* widget) : widget_(widget) {
@@ -40,7 +42,6 @@ void DesktopBackgroundWidgetController::SetBounds(gfx::Rect bounds) {
     layer_->SetBounds(bounds);
 }
 
-
 void DesktopBackgroundWidgetController::Reparent(aura::RootWindow* root_window,
                                                  int src_container,
                                                  int dest_container) {
@@ -52,6 +53,10 @@ void DesktopBackgroundWidgetController::Reparent(aura::RootWindow* root_window,
     root_window->GetChildById(src_container)->layer()->Remove(layer);
     root_window->GetChildById(dest_container)->layer()->Add(layer);
   }
+}
+
+ComponentWrapper::ComponentWrapper(
+    DesktopBackgroundWidgetController* component) : component_(component) {
 }
 
 }  // namespace internal
