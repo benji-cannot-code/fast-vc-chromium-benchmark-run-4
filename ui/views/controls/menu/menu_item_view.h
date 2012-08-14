e8cc2282cde6619d6469ed809507796700fbdb20
saintlou@chromium.org <saintlou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 23:06:36 +0000

    Update drop down menu look & feel
    BUG=122908
    TBR=oshima@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10829176
    Patch from Harry McCleave <harrym@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151596 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_item_view.h
+++ ui/views/controls/menu/menu_item_view.h
@@ -101,8 +101,7 @@ class VIEWS_EXPORT MenuItemView : public View {
   enum MenuPosition {
     POSITION_BEST_FIT,
     POSITION_ABOVE_BOUNDS,
-    POSITION_BELOW_BOUNDS,
-    POSITION_OVER_BOUNDS
+    POSITION_BELOW_BOUNDS
   };
 
   // Constructor for use with the top level menu item. This menu is never
@@ -218,11 +217,6 @@ class VIEWS_EXPORT MenuItemView : public View {
   // Returns the type of this menu.
   const Type& GetType() const { return type_; }
 
-  // Returns the requested menu position.
-  const MenuPosition& GetRequestedMenuPosition() {
-    return requested_menu_position_;
-  }
-
   // Sets whether this item is selected. This is invoked as the user moves
   // the mouse around the menu while open.
   void SetSelected(bool selected);
@@ -300,12 +294,6 @@ class VIEWS_EXPORT MenuItemView : public View {
   // Set top and bottom margins in pixels.  If no margin is set or a
   // negative margin is specified then MenuConfig values are used.
   void SetMargins(int top_margin, int bottom_margin);
-
-  // Set the position of the menu with respect to the bounds (top
-  // level only).
-  void set_menu_position(MenuPosition menu_position) {
-    requested_menu_position_ = menu_position;
-  }
 
   // Suppress the right margin if this is set to false.
   void set_use_right_margin(bool use_right_margin) {
