05a3f242ba37574d6fdb7242912792065fa42942
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-14 19:17:39 +0000

    Remove unused yyparse method in CSSParser https://bugs.webkit.org/show_bug.cgi?id=93986
    Patch by Olivier Blin <olivier.blin@softathome.com> on 2012-08-14
    Reviewed by Darin Adler.
    
    This method is unused and not even declared.
    
    No new tests, it just removes unused code.
    
    * css/CSSParser.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125589 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.h
+++ third_party/WebKit/Source/WebCore/css/CSSParser.h
@@ -248,8 +248,6 @@ public:
 
     bool parseFontVariantLigatures(bool important);
 
-    int yyparse();
-
     CSSParserSelector* createFloatingSelector();
     PassOwnPtr<CSSParserSelector> sinkFloatingSelector(CSSParserSelector*);
 
