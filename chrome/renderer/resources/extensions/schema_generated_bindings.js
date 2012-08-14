419e8e20db49bc7e673c19154d0048df2efad414
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 03:52:36 +0000

    Revert 151420 - Add documentation for filtered events.
    BUG=132950
    
    Review URL: https://chromiumcodereview.appspot.com/10855015
    
    TBR=koz@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10826295
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/schema_generated_bindings.js
+++ chrome/renderer/resources/extensions/schema_generated_bindings.js
@@ -343,20 +343,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
           var eventName = apiDef.namespace + "." + eventDef.name;
           var customEvent = customEvents[apiDef.namespace];
-          var options = eventDef.options || {};
-
-          if (eventDef.filters && eventDef.filters.length > 0)
-            options.supportsFilters = true;
-
           if (customEvent) {
             mod[eventDef.name] = new customEvent(
                 eventName, eventDef.parameters, eventDef.extraParameters,
-                options);
+                eventDef.options);
           } else if (eventDef.anonymous) {
             mod[eventDef.name] = new chrome.Event();
           } else {
             mod[eventDef.name] = new chrome.Event(
-                eventName, eventDef.parameters, options);
+                eventName, eventDef.parameters, eventDef.options);
           }
         });
       }
