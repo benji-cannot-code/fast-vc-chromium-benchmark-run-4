c89d1f7cf2cdfa3ae0a22e1b0287bfad21c169ec
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 16:10:13 +0000

    We can leak the system tray UI when closing down the tab due to several possibilities: # An error happens on the video_capture_device and we erase the request without notifying the UI. # When the tab is shutting down, it goes away before we we delete its entry in tabs_ in media_stream_capture_indicator # When canceling a request in media_stream_device_settings, we search the next request and bring it up the UI before we delete the old request, in this case we might bring the the old request back to UI again.
    And this patch also fixed a problem when hitting a DCHECK in  media_stream_device_settings.cc(196)] Check failed: request->devices_full.count(stream_type) == static_cast<size_t>(0) (1 vs. 0)
    This happened because calling devices_full[type].empty() will create an empty entry to the map.
    
    BUG=140441
    Test=content_unittests,chrome/test/functional/webrtc_brutality_test.py
    
    Review URL: https://chromiumcodereview.appspot.com/10829190
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151486 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/media_stream_capture_indicator.cc
+++ chrome/browser/media/media_stream_capture_indicator.cc
@@ -215,7 +215,6 @@ void MediaStreamCaptureIndicator::DoDevicesClosedOnUIThread(
   if (!status_icon_)
     return;
 
-  DCHECK(!tabs_.empty());
   RemoveCaptureDeviceTab(render_process_id, render_view_id, devices);
 }
 
@@ -308,6 +307,7 @@ void MediaStreamCaptureIndicator::OnImageLoaded(
 
 void MediaStreamCaptureIndicator::Hide() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  DCHECK(tabs_.empty());
   if (!status_icon_)
     return;
 
@@ -333,9 +333,11 @@ void MediaStreamCaptureIndicator::UpdateStatusTrayIconContextMenu() {
        iter != tabs_.end();  ++iter) {
     string16 tab_title = GetTitle(iter->render_process_id,
                                   iter->render_view_id);
-    // The tab has gone away.
-    if (tab_title.empty())
+    if (tab_title.empty()) {
+      // Delete the entry since the tab has gone away.
+      tabs_.erase(iter);
       continue;
+    }
 
     // Check if any audio and video devices have been used.
     audio = audio || iter->audio_ref_count > 0;
@@ -352,9 +354,10 @@ void MediaStreamCaptureIndicator::UpdateStatusTrayIconContextMenu() {
     ++command_id;
   }
 
-  // All the tabs have gone away.
-  if (!audio && !video)
+  if (!audio && !video) {
+    Hide();
     return;
+  }
 
   // The icon will take the ownership of the passed context menu.
   status_icon_->SetContextMenu(menu.release());
@@ -440,9 +443,6 @@ void MediaStreamCaptureIndicator::RemoveCaptureDeviceTab(
   if (iter->audio_ref_count == 0 && iter->video_ref_count == 0)
     tabs_.erase(iter);
 
-  if (tabs_.empty())
-    Hide();
-  else
-    UpdateStatusTrayIconContextMenu();
+  UpdateStatusTrayIconContextMenu();
 }
 
