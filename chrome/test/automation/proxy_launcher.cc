1d32187d7698e8821234270a8a893c76350627c7
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 17:15:30 +0000

    Get rid of debug_flags.h includes from ui tests base class. Developers don't debug the few remaining tests so this isn't needed, and if they really want to pass these flags, they can use --extra-chrome-flags.
    BUG=98716
    Review URL: https://chromiumcodereview.appspot.com/10855149
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/automation/proxy_launcher.cc
+++ chrome/test/automation/proxy_launcher.cc
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/test_launcher_utils.h"
 #include "chrome/test/base/test_switches.h"
 #include "chrome/test/ui/ui_test.h"
-#include "content/common/debug_flags.h"
 #include "content/public/common/process_type.h"
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_descriptors.h"
@@ -437,8 +436,6 @@ bool ProxyLauncher::LaunchBrowserHelper(const LaunchState& state,
 
   // Add command line arguments that should be applied to all UI tests.
   PrepareTestCommandline(&command_line, state.include_testing_id);
-  DebugFlags::ProcessDebugFlags(
-      &command_line, content::PROCESS_TYPE_UNKNOWN, false);
 
   // Sometimes one needs to run the browser under a special environment
   // (e.g. valgrind) without also running the test harness (e.g. python)
--- chrome/test/ui/ui_test.cc
+++ chrome/test/ui/ui_test.cc
@@ -49,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/test_launcher_utils.h"
 #include "chrome/test/base/test_switches.h"
-#include "content/common/debug_flags.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_util.h"
 #include "ui/gl/gl_implementation.h"
