d0a67426c85307140d2f2b895795383103b60ae9
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 14:22:58 +0000

    Sort includes in cookie_monster.h.
    This is part of Chrome for Android upstreaming.
    
    This was causing an unnecessary diff with downstream.
    
    BUG=136750
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828302
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151474 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cookies/cookie_monster.h
+++ net/cookies/cookie_monster.h
@@ -23,9 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/synchronization/lock.h"
 #include "base/time.h"
+#include "net/base/net_export.h"
 #include "net/cookies/canonical_cookie.h"
 #include "net/cookies/cookie_store.h"
-#include "net/base/net_export.h"
 
 class GURL;
 
