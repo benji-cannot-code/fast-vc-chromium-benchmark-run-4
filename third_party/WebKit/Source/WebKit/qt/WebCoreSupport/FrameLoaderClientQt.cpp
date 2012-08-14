70cccc292b553238b0eec0d75e7a57d82792e039
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-14 02:37:54 +0000

    [chromium] release FrameLoaderClientImpl::m_pluginWidget refptr upon Plugin Document detach. https://bugs.webkit.org/show_bug.cgi?id=93283
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Chromium has a refptr that needs to be NULLed at this point.
    
    The approach is to change the client redirectDataToPlugin method(s) to expect
    the possibility of a NULL argument, and the use this to clear the refptr on
    the chromium platform.  Other platforms can merely ignore the NULL case, thereby
    maintaining the existing behaviour.
    
    Formal testing is nearly impossible without some chrome-specific plugins.
    
    * html/PluginDocument.cpp:
    (WebCore::PluginDocument::detach):
    
    Source/WebKit/blackberry:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::redirectDataToPlugin):
    
    Source/WebKit/chromium:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, NULLing Chromium's refptr that needs to be NULLed.
    
    * src/FrameLoaderClientImpl.cpp:
    (WebKit::FrameLoaderClientImpl::redirectDataToPlugin):
    
    Source/WebKit/efl:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebCoreSupport/FrameLoaderClientEfl.cpp:
    (WebCore::FrameLoaderClientEfl::redirectDataToPlugin):
    
    Source/WebKit/gtk:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebCoreSupport/FrameLoaderClientGtk.cpp:
    (WebKit::FrameLoaderClient::redirectDataToPlugin):
    
    Source/WebKit/qt:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebCoreSupport/FrameLoaderClientQt.cpp:
    (WebCore::FrameLoaderClientQt::redirectDataToPlugin):
    
    Source/WebKit/win:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebCoreSupport/WebFrameLoaderClient.cpp:
    (WebFrameLoaderClient::redirectDataToPlugin):
    
    Source/WebKit/wince:
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebCoreSupport/FrameLoaderClientWinCE.cpp:
    (WebKit::FrameLoaderClientWinCE::redirectDataToPlugin):
    
    Source/WebKit/wx:
    
    Chromium has a refptr that needs to be NULLed at this point.
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebKitSupport/FrameLoaderClientWx.cpp:
    (WebCore::FrameLoaderClientWx::redirectDataToPlugin):
    
    Source/WebKit2:
    
    Chromium has a refptr that needs to be NULLed at this point.
    
    Change the client redirectDataToPlugin method(s) to expect the possibility of
    a NULL argument, keeping existing behaviour otherwise.
    
    * WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp:
    (WebKit::WebFrameLoaderClient::redirectDataToPlugin):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.cpp
@@ -1639,9 +1639,9 @@ PassRefPtr<Widget> FrameLoaderClientQt::createPlugin(const IntSize& pluginSize, 
 
 void FrameLoaderClientQt::redirectDataToPlugin(Widget* pluginWidget)
 {
-    ASSERT(!m_pluginView);
     m_pluginView = static_cast<PluginView*>(pluginWidget);
-    m_hasSentResponseToPlugin = false;
+    if (pluginWidget)
+        m_hasSentResponseToPlugin = false;
 }
 
 PassRefPtr<Widget> FrameLoaderClientQt::createJavaAppletWidget(const IntSize& pluginSize, HTMLAppletElement* element, const KURL& url,
--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -1357,9 +1357,6 @@ PassRefPtr<Widget> WebFrameLoaderClient::createPlugin(const IntSize&, HTMLPlugIn
 
 void WebFrameLoaderClient::redirectDataToPlugin(Widget* pluginWidget)
 {
-    ASSERT(!m_pluginView);
-    ASSERT(pluginWidget);
-    
     m_pluginView = static_cast<PluginView*>(pluginWidget);
 }
 
