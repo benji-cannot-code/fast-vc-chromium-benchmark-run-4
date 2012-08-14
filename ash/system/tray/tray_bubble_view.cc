661c9c2fd2eac241de83863134ffca165a3525bc
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 18:28:11 +0000

    Show extension settings for extension notifications
    This also:
    * Sends notification removal requests instead of directly removing notifications for removal by source
    * Fixes a bug where clicking on a menu item outside the bubble would close the buble and not trigger the menu event
    
    BUG=139697
    For chrome/browser/ui/views/ash/balloon_collection_impl_ash.cc:
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10855113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151518 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_bubble_view.cc
+++ ash/system/tray/tray_bubble_view.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/tray/tray_bubble_view.h"
 
+#include "ash/root_window_controller.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/system/tray/tray_constants.h"
+#include "ash/wm/property_util.h"
 #include "ash/wm/shelf_layout_manager.h"
 #include "ash/wm/window_animations.h"
 #include "grit/ash_strings.h"
@@ -464,7 +466,7 @@ bool TrayBubbleView::Host::PreHandleKeyEvent(aura::Window* target,
 bool TrayBubbleView::Host::PreHandleMouseEvent(aura::Window* target,
                                                ui::MouseEvent* event) {
   if (event->type() == ui::ET_MOUSE_PRESSED)
-    ProcessLocatedEvent(*event);
+    ProcessLocatedEvent(target, *event);
   return false;
 }
 
@@ -472,7 +474,7 @@ ui::TouchStatus TrayBubbleView::Host::PreHandleTouchEvent(
     aura::Window* target,
     ui::TouchEvent* event) {
   if (event->type() == ui::ET_TOUCH_PRESSED)
-    ProcessLocatedEvent(*event);
+    ProcessLocatedEvent(target, *event);
   return ui::TOUCH_STATUS_UNKNOWN;
 }
 
@@ -483,7 +485,16 @@ ui::GestureStatus TrayBubbleView::Host::PreHandleGestureEvent(
 }
 
 void TrayBubbleView::Host::ProcessLocatedEvent(
-    const ui::LocatedEvent& event) {
+    aura::Window* target, const ui::LocatedEvent& event) {
+  if (target) {
+    // Don't process events that occurred inside an embedded menu.
+    RootWindowController* root_controller =
+        GetRootWindowController(target->GetRootWindow());
+    if (root_controller && root_controller->GetContainer(
+            ash::internal::kShellWindowId_MenuContainer)->Contains(target)) {
+      return;
+    }
+  }
   if (!widget_)
     return;
   gfx::Rect bounds = widget_->GetNativeWindow()->GetBoundsInRootWindow();
--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -163,7 +163,8 @@ class WebNotificationList {
     notifications_.clear();
   }
 
-  void RemoveNotificationsBySource(const std::string& id) {
+  void SendRemoveNotificationsBySource(WebNotificationTray* tray,
+                                       const std::string& id) {
     Notifications::iterator source_iter = GetNotification(id);
     if (source_iter == notifications_.end())
       return;
@@ -172,11 +173,12 @@ class WebNotificationList {
          loopiter != notifications_.end(); ) {
       Notifications::iterator curiter = loopiter++;
       if (curiter->display_source == display_source)
-        EraseNotification(curiter);
+        tray->SendRemoveNotification(curiter->id);
     }
   }
 
-  void RemoveNotificationsByExtension(const std::string& id) {
+  void SendRemoveNotificationsByExtension(WebNotificationTray* tray,
+                                          const std::string& id) {
     Notifications::iterator source_iter = GetNotification(id);
     if (source_iter == notifications_.end())
       return;
@@ -185,7 +187,7 @@ class WebNotificationList {
          loopiter != notifications_.end(); ) {
       Notifications::iterator curiter = loopiter++;
       if (curiter->extension_id == extension_id)
-        EraseNotification(curiter);
+        tray->SendRemoveNotification(curiter->id);
     }
   }
 
@@ -996,17 +998,17 @@ void WebNotificationTray::SendRemoveAllNotifications() {
 // When we disable notifications, we remove any existing matching
 // notifications to avoid adding complicated UI to re-enable the source.
 void WebNotificationTray::DisableByExtension(const std::string& id) {
-  // Will call SendRemoveNotification for each matching notification.
-  notification_list_->RemoveNotificationsByExtension(id);
   if (delegate_)
     delegate_->DisableExtension(id);
+  // Will call SendRemoveNotification for each matching notification.
+  notification_list_->SendRemoveNotificationsByExtension(this, id);
 }
 
 void WebNotificationTray::DisableByUrl(const std::string& id) {
-  // Will call SendRemoveNotification for each matching notification.
-  notification_list_->RemoveNotificationsBySource(id);
   if (delegate_)
     delegate_->DisableNotificationsFromSource(id);
+  // Will call SendRemoveNotification for each matching notification.
+  notification_list_->SendRemoveNotificationsBySource(this, id);
 }
 
 bool WebNotificationTray::PerformAction(const ui::Event& event) {
