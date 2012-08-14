63ae745f18e0059f1bf58ae44a4d1d8faf497f41
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-14 03:55:43 +0000

    Make extension browser tests always run as though they're on dev channel. Tests that explicitly want to test other channels (e.g. test feature X behaves differently on stable) then they can still enable that explicitly.
    Review URL: https://chromiumcodereview.appspot.com/10829312
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_browsertest.h
+++ chrome/browser/extensions/extension_browsertest.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_temp_dir.h"
 #include "chrome/browser/extensions/extension_host.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/features/feature.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_observer.h"
@@ -242,6 +243,9 @@ class ExtensionBrowserTest : virtual public InProcessBrowserTest,
   // When waiting for visible page action count to change, we wait until it
   // reaches this value.
   int target_visible_page_action_count_;
+
+  // Make the current channel "dev" for the duration of the test.
+  extensions::Feature::ScopedCurrentChannel current_channel_;
 };
 
 #endif  // CHROME_BROWSER_EXTENSIONS_EXTENSION_BROWSERTEST_H_
