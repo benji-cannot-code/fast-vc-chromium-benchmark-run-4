2e80c1d747564011aa97654672e45147d9014286
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 00:10:36 +0000

    Clear pattern to prevent timing problem between cancelVibration and vibrate https://bugs.webkit.org/show_bug.cgi?id=93957
    Patch by Kihong Kwon <kihong.kwon@samsung.com> on 2012-08-14
    Reviewed by Kentaro Hara.
    
    There is a timing issue in the cancelVibration.
    Since vibrate works based on timer, cancelVibration might be called
    eariler than vibrate when cancelVibration is called just after vibrate call.
    It can be prevented from clearing m_pattern in the cancelVibration.
    
    * Modules/vibration/Vibration.cpp:
    (WebCore::Vibration::cancelVibration):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/vibration/Vibration.cpp
+++ third_party/WebKit/Source/WebCore/Modules/vibration/Vibration.cpp
@@ -74,6 +74,7 @@ void Vibration::vibrate(const VibrationPattern& pattern)
 
 void Vibration::cancelVibration()
 {
+    m_pattern.clear();
     if (m_isVibrating) {
         m_vibrationClient->cancelVibration();
         m_isVibrating = false;
