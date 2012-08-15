9410c5175a3d1aef2b707bbde881e1f3f045c898
gdk@chromium.org <gdk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 23:04:18 +0000

    Adding tests for USB extension API
    Includes test coverage for the recently-fixed zero-length transfer bug.
    
    BUG=140390
    TEST=self
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824298
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/usb/usb_api.h
+++ chrome/browser/extensions/api/usb/usb_api.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/api_resource_manager.h"
 #include "chrome/common/extensions/api/experimental_usb.h"
 
+class UsbDevice;
+
 namespace extensions {
 
 class ApiResourceEventNotifier;
@@ -34,6 +36,8 @@ class UsbFindDeviceFunction : public UsbAsyncApiFunction {
   DECLARE_EXTENSION_FUNCTION_NAME("experimental.usb.findDevice");
 
   UsbFindDeviceFunction();
+
+  static void SetDeviceForTest(UsbDevice* device);
 
  protected:
   virtual ~UsbFindDeviceFunction();
--- chrome/browser/extensions/api/usb/usb_device_resource.cc
+++ chrome/browser/extensions/api/usb/usb_device_resource.cc
@@ -158,7 +158,13 @@ UsbDeviceResource::UsbDeviceResource(ApiResourceEventNotifier* notifier,
                                      UsbDevice* device)
     : ApiResource(notifier), device_(device) {}
 
-UsbDeviceResource::~UsbDeviceResource() {}
+UsbDeviceResource::~UsbDeviceResource() {
+  Close();
+}
+
+void UsbDeviceResource::Close() {
+  device_->Close();
+}
 
 void UsbDeviceResource::ControlTransfer(const ControlTransferInfo& transfer) {
   UsbDevice::TransferDirection direction;
