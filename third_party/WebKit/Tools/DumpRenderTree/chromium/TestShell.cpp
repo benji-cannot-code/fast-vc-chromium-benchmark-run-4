6581f8d276b2681f565d4d1a73f8bfae7d256c11
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 06:28:00 +0000

    Build fix after r125516. Revert the erroneous rename since we need to keep exposing layoutTestController in Chromium port as explained r124785.
    * DumpRenderTree/chromium/TestShell.cpp:
    (TestShell::bindJSObjectsToWindow):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125651 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.cpp
@@ -727,7 +727,7 @@ void TestShell::bindJSObjectsToWindow(WebFrame* frame)
     WebTestingSupport::injectInternalsObject(frame);
     m_testInterfaces->bindTo(frame);
     m_testRunner->bindToJavascript(frame, WebString::fromUTF8("testRunner"));
-    m_testRunner->bindToJavascript(frame, WebString::fromUTF8("testRunner"));
+    m_testRunner->bindToJavascript(frame, WebString::fromUTF8("layoutTestController"));
 }
 
 WebViewHost* TestShell::createNewWindow(const WebKit::WebURL& url)
