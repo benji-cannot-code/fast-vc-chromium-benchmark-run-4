ab1e42e082450e3b8935cacc91eea074d0304035
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 04:00:39 +0000

    FileWriter fails with assertion when trying to write empty Blob https://bugs.webkit.org/show_bug.cgi?id=93694
    Reviewed by Kent Tamura.
    
    if the given data size is zero (i.e. m_bytesToWrite is zero) the assertion 'bytes + m_bytesWritten > 0' should not be tested.
    
    Source/WebCore:
    
    Tests: fast/filesystem/file-writer-empty-blob.html
           fast/filesystem/workers/file-writer-empty-blob.html
    
    * Modules/filesystem/FileWriter.cpp:
    (WebCore::FileWriter::didWrite):
    
    LayoutTests:
    
    * fast/filesystem/file-writer-empty-blob-expected.txt: Added.
    * fast/filesystem/file-writer-empty-blob.html: Added.
    * fast/filesystem/resources/file-writer-empty-blob.js: Added.
    * fast/filesystem/workers/file-writer-empty-blob-expected.txt: Added.
    * fast/filesystem/workers/file-writer-empty-blob.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/filesystem/resources/file-writer-empty-blob.js
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (this.importScripts) {
+    importScripts('fs-worker-common.js');
+    importScripts('../../js/resources/js-test-pre.js');
+    importScripts('file-writer-utils.js');
+}
+
+description("Test that FileWriter works without crash when trying to write an empty blob.");
+
+var fileEntry;
+
+function onTestSuccess() {
+    testPassed("Successfully wrote blob.");
+    cleanUp();
+}
+
+function startWrite(writer) {
+    var blob = new Blob([""]);
+    writer.onerror = onError;
+    writer.onwriteend = onTestSuccess;
+    writer.write(blob);
+}
+
+function runTest(unusedFileEntry, fileWriter) {
+    startWrite(fileWriter);
+    gc();
+}
+var jsTestIsAsync = true;
+setupAndRunTest(2 * 1024 * 1024, 'file-writer-empty-blob', runTest);
+
