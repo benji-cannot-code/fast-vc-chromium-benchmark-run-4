48da9b45d03b84a47326cbd30cf551deeab4e547
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 22:07:46 +0000

    [BlackBerry] Upstream FrameLoaderClientBlackBerry::securityOriginForNewDocument() https://bugs.webkit.org/show_bug.cgi?id=94139
    Patch by Yong Li <yoli@rim.com> on 2012-08-15
    Reviewed by Antonio Gomes.
    
    Keep same security origin when a file URL automatically redirects to its child folder.
    This patch will be reverted when we finish the better solution.
    PR# 172333 and PR# 180590.
    
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::FrameLoaderClientBlackBerry):
    (WebCore::FrameLoaderClientBlackBerry::dispatchDidStartProvisionalLoad):
    (WebCore::FrameLoaderClientBlackBerry::securityOriginForNewDocument):
    (WebCore):
    * WebCoreSupport/FrameLoaderClientBlackBerry.h:
    (FrameLoaderClientBlackBerry):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125714 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/FrameLoaderClientBlackBerry.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/FrameLoaderClientBlackBerry.h
@@ -171,6 +171,8 @@ public:
 
     virtual PassRefPtr<FrameNetworkingContext> createNetworkingContext();
 
+    virtual PassRefPtr<SecurityOrigin> securityOriginForNewDocument(const KURL&);
+
     void readyToRender(bool pageIsVisuallyNonEmpty);
 
     void doPendingFragmentScroll();
@@ -216,6 +218,8 @@ private:
 
     // Used to stop media files from loading because we don't need to have the entire file loaded by WebKit.
     bool m_cancelLoadOnNextData;
+
+    bool m_wasProvisionalLoadTriggeredByUserGesture;
 };
 
 } // WebCore
