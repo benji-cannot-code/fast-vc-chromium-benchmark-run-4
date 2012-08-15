2780341aeb9841f57da32cd37b5b4b6feb04efa8
disher@chromium.org <disher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 15:41:46 +0000

    Refresh output cache when setting display mode
    Historically, all the entry-points into the OutputConfigurator needed to refresh
    their view of the cached output data.  This newly-added entry-point doesn't do
    this which can lead to the cached data becoming stale (since it technically only
    applies within the context where the corresponding XRRScreenResources are
    valid).
    Ideally, no such information would be cached and every attempt to evaluate or
    change the state of the video outputs would start by requesting this structure.
    That change is of larger scope and risk, though, so this is the tactical
    solution to the immediate problem suitable for porting to stable branches.
    
    BUG=chromium:140807
    TEST=Manually tested that ctrl-F4 and "manage displays" (in chrome://settings)
    work on a Lumpy the same way with and without this change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825306
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151696 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/display/output_configurator.cc
+++ chromeos/display/output_configurator.cc
@@ -397,6 +397,9 @@ bool OutputConfigurator::SetDisplayMode(OutputState new_state) {
   XRRScreenResources* screen = XRRGetScreenResources(display, window);
   CHECK(screen != NULL);
 
+  // We are about to act on the cached output data but it could be stale so
+  // force the outputs to be recached.
+  ForceRecacheOutputs(display, screen);
   UpdateCacheAndXrandrToState(display,
                               screen,
                               window,
@@ -466,9 +469,15 @@ bool OutputConfigurator::TryRecacheOutputs(Display* display,
     }
   }
 
-  if (!outputs_did_change)
-    return false;
-  // We now know that we need to recache so free and re-alloc the buffer.
+  if (outputs_did_change) {
+    // We now know that we need to recache so free and re-alloc the buffer.
+    ForceRecacheOutputs(display, screen);
+  }
+  return outputs_did_change;
+}
+
+void OutputConfigurator::ForceRecacheOutputs(Display* display,
+                                             XRRScreenResources* screen) {
   output_count_ = screen->noutput;
   if (output_count_ == 0) {
     output_cache_.reset(NULL);
@@ -573,7 +582,6 @@ bool OutputConfigurator::TryRecacheOutputs(Display* display,
             << " primary " << primary_mode
             << " secondary " << second_mode;
   }
-  return outputs_did_change;
 }
 
 void OutputConfigurator::UpdateCacheAndXrandrToState(
