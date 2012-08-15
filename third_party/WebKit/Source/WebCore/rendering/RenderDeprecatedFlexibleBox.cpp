bd35546669ffca1a0a6ef4a2c4b22e8fcb63aefb
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 02:14:23 +0000

    Fix access to m_markupBox in WebCore::EllipsisBox::paint https://bugs.webkit.org/show_bug.cgi?id=91138
    Reviewed by Abhishek Arya.
    
    Source/WebCore:
    
    EllipsisBox would hold on to m_markupBox, which would then get destroyed during
    the followup layoutIfNeeded in layoutVerticalBox. Instead, have EllipsisBox
    dynamically grab to pointer to the markup box during paint since there's no
    straightforward way to notify the EllipsisBox that the markupBox has been destroyed
    and/or point it at the new markupBox.
    
    Test: fast/overflow/line-clamp-and-columns.html
    
    * rendering/EllipsisBox.cpp:
    (WebCore::EllipsisBox::paint):
    (WebCore):
    (WebCore::EllipsisBox::paintMarkupBox):
    * rendering/EllipsisBox.h:
    (WebCore::EllipsisBox::EllipsisBox):
    Just store a boolean that we have a markup box that needs painting.
    * rendering/RenderDeprecatedFlexibleBox.cpp:
    (WebCore::RenderDeprecatedFlexibleBox::applyLineClamp):
    Clearing the override size right after setting it was incorrect because
    there are cases where we'll do a followup layout in layoutVerticalBox, at which
    point we'll still need the override size.
    (WebCore::RenderDeprecatedFlexibleBox::clearLineClamp):
    Clear the override size here to handle cases where line clamp is removed since
    we don't call applyLineClamp in those cases.
    
    LayoutTests:
    
    * fast/overflow/line-clamp-and-columns-expected.html: Added.
    * fast/overflow/line-clamp-and-columns.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderDeprecatedFlexibleBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderDeprecatedFlexibleBox.cpp
@@ -884,6 +884,7 @@ void RenderDeprecatedFlexibleBox::applyLineClamp(FlexBoxIterator& iterator, bool
         if (childDoesNotAffectWidthOrFlexing(child))
             continue;
 
+        child->clearOverrideSize();
         if (relayoutChildren || (child->isReplaced() && (child->style()->width().isPercent() || child->style()->height().isPercent()))
             || (child->style()->height().isAuto() && child->isBlockFlow())) {
             child->setChildNeedsLayout(true, MarkOnlyThis);
@@ -922,7 +923,6 @@ void RenderDeprecatedFlexibleBox::applyLineClamp(FlexBoxIterator& iterator, bool
         child->setChildNeedsLayout(true, MarkOnlyThis);
         child->setOverrideLogicalContentHeight(newHeight - child->borderAndPaddingHeight());
         child->layoutIfNeeded();
-        child->clearOverrideSize();
 
         // FIXME: For now don't support RTL.
         if (style()->direction() != LTR)
@@ -985,6 +985,7 @@ void RenderDeprecatedFlexibleBox::clearLineClamp()
         if (childDoesNotAffectWidthOrFlexing(child))
             continue;
 
+        child->clearOverrideSize();
         if ((child->isReplaced() && (child->style()->width().isPercent() || child->style()->height().isPercent()))
             || (child->style()->height().isAuto() && child->isBlockFlow())) {
             child->setChildNeedsLayout(true);
