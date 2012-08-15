aaa68c1a31f3d81663e25728dff6256300ae605c
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 20:44:25 +0000

    Add a chrome://nacl page to show if NaCl is enabled and if PNaCl is installed.
    How it looks with pnacl enabled:
    http://www.corp.google.com/~jvoung/with-pnacl.png
    
    and without pnacl enabled:
    http://www.corp.google.com/~jvoung/without-pnacl.png
    
    BUG= 114444
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2813
    
    Review URL: https://chromiumcodereview.appspot.com/10823214
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151739 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -39,6 +39,7 @@ const char* const kChromePaths[] = {
   chrome::kChromeUIInspectHost,
   chrome::kChromeUIMediaInternalsHost,
   chrome::kChromeUIMemoryHost,
+  chrome::kChromeUINaClHost,
   chrome::kChromeUINetInternalsHost,
   chrome::kChromeUINetworkViewCacheHost,
   chrome::kChromeUINewTabHost,
