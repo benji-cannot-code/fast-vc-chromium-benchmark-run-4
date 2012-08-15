46d3d70ba3ff2295f6843326226ab377297e335c
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 20:54:05 +0000

    Address post commit nits for r151132
    BUG=141533
    TBR=scherkus@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10830342
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151740 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base_unittest.cc
+++ media/filters/video_renderer_base_unittest.cc
@@ -108,7 +108,7 @@ class VideoRendererBaseTest : public ::testing::Test {
     InSequence s;
 
     EXPECT_CALL(*decoder_, Initialize(_, _, _))
-      .WillOnce(RunPipelineStatusCB1(PIPELINE_OK));
+        .WillOnce(RunPipelineStatusCB1(PIPELINE_OK));
 
     // Set playback rate before anything else happens.
     renderer_->SetPlaybackRate(1.0f);
@@ -697,7 +697,6 @@ TEST_F(VideoRendererBaseTest, VideoDecoder_InitFailure) {
   EXPECT_CALL(*decoder_, Initialize(_, _, _))
       .WillOnce(RunPipelineStatusCB1(PIPELINE_ERROR_DECODE));
   InitializeRenderer(PIPELINE_ERROR_DECODE);
-
- }
+}
 
 }  // namespace media
