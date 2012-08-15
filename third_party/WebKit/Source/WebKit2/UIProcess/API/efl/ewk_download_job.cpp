c4aadd6c577a7797f1fdc23839eb137976dbb6cc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 13:50:14 +0000

    [EFL][WK2] Refactoring: start using WKEinaSharedString in ewk_ classes https://bugs.webkit.org/show_bug.cgi?id=93964
    Patch by Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com> on 2012-08-15
    Reviewed by Kenneth Rohde Christiansen.
    
    Started using WKEinaSharedString where it is advisable.
    
    * UIProcess/API/cpp/efl/WKEinaSharedString.cpp:
    (WKEinaSharedString::operator==):
    * UIProcess/API/cpp/efl/WKEinaSharedString.h:
    * UIProcess/API/efl/ewk_download_job.cpp:
    (_Ewk_Download_Job):
    (_Ewk_Download_Job::_Ewk_Download_Job):
    (_Ewk_Download_Job::~_Ewk_Download_Job):
    (ewk_download_job_destination_set):
    (ewk_download_job_suggested_filename_set):
    * UIProcess/API/efl/ewk_intent.cpp:
    (_Ewk_Intent):
    (_Ewk_Intent::_Ewk_Intent):
    (_Ewk_Intent::~_Ewk_Intent):
    (ewk_intent_action_get):
    (ewk_intent_type_get):
    (ewk_intent_service_get):
    * UIProcess/API/efl/ewk_intent_service.cpp:
    (_Ewk_Intent_Service):
    (_Ewk_Intent_Service::_Ewk_Intent_Service):
    (_Ewk_Intent_Service::~_Ewk_Intent_Service):
    (ewk_intent_service_action_get):
    (ewk_intent_service_type_get):
    (ewk_intent_service_href_get):
    (ewk_intent_service_title_get):
    (ewk_intent_service_disposition_get):
    * UIProcess/API/efl/ewk_navigation_policy_decision.cpp:
    (_Ewk_Navigation_Policy_Decision):
    (_Ewk_Navigation_Policy_Decision::_Ewk_Navigation_Policy_Decision):
    (_Ewk_Navigation_Policy_Decision::~_Ewk_Navigation_Policy_Decision):
    * UIProcess/API/efl/ewk_url_request.cpp:
    (_Ewk_Url_Request):
    (_Ewk_Url_Request::_Ewk_Url_Request):
    (_Ewk_Url_Request::~_Ewk_Url_Request):
    (ewk_url_request_url_get):
    (ewk_request_cookies_first_party_get):
    (ewk_url_request_http_method_get):
    * UIProcess/API/efl/ewk_url_response.cpp:
    (_Ewk_Url_Response):
    (_Ewk_Url_Response::_Ewk_Url_Response):
    (_Ewk_Url_Response::~_Ewk_Url_Response):
    (ewk_url_response_url_get):
    (ewk_url_response_mime_type_get):
    * UIProcess/API/efl/ewk_view.cpp:
    (_Ewk_View_Private_Data):
    (_Ewk_View_Private_Data::_Ewk_View_Private_Data):
    (_Ewk_View_Private_Data::~_Ewk_View_Private_Data):
    (ewk_view_uri_update):
    (ewk_view_title_get):
    (ewk_view_theme_set):
    (ewk_view_setting_encoding_custom_get):
    (ewk_view_setting_encoding_custom_set):
    * UIProcess/API/efl/ewk_web_error.cpp:
    (_Ewk_Web_Error):
    (_Ewk_Web_Error::_Ewk_Web_Error):
    (_Ewk_Web_Error::~_Ewk_Web_Error):
    (ewk_web_error_url_get):
    (ewk_web_error_description_get):
    * UIProcess/API/efl/ewk_web_resource.cpp:
    (_Ewk_Web_Resource):
    (_Ewk_Web_Resource::_Ewk_Web_Resource):
    (_Ewk_Web_Resource::~_Ewk_Web_Resource):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125671 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_download_job.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_download_job.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "DownloadProxy.h"
 #include "WKAPICast.h"
+#include "WKEinaSharedString.h"
 #include "WKRetainPtr.h"
 #include "WebURLRequest.h"
 #include "ewk_download_job_private.h"
@@ -51,8 +52,8 @@ struct _Ewk_Download_Job {
     double startTime;
     double endTime;
     uint64_t downloaded; /**< length already downloaded */
-    const char* destination;
-    const char* suggestedFilename;
+    WKEinaSharedString destination;
+    WKEinaSharedString suggestedFilename;
 
     _Ewk_Download_Job(DownloadProxy* download, Evas_Object* ewkView)
         : __ref(1)
@@ -64,8 +65,6 @@ struct _Ewk_Download_Job {
         , startTime(-1)
         , endTime(-1)
         , downloaded(0)
-        , destination(0)
-        , suggestedFilename(0)
     { }
 
     ~_Ewk_Download_Job()
@@ -75,8 +74,6 @@ struct _Ewk_Download_Job {
             ewk_url_request_unref(request);
         if (response)
             ewk_url_response_unref(response);
-        eina_stringshare_del(destination);
-        eina_stringshare_del(suggestedFilename);
     }
 };
 
@@ -160,7 +157,7 @@ Eina_Bool ewk_download_job_destination_set(Ewk_Download_Job* download, const cha
     EINA_SAFETY_ON_NULL_RETURN_VAL(download, false);
     EINA_SAFETY_ON_NULL_RETURN_VAL(destination, false);
 
-    eina_stringshare_replace(&download->destination, destination);
+    download->destination = destination;
 
     return true;
 }
@@ -237,7 +234,7 @@ void ewk_download_job_suggested_filename_set(Ewk_Download_Job* download, const c
 {
     EINA_SAFETY_ON_NULL_RETURN(download);
 
-    eina_stringshare_replace(&download->suggestedFilename, suggestedFilename);
+    download->suggestedFilename = suggestedFilename;
 }
 
 /**
