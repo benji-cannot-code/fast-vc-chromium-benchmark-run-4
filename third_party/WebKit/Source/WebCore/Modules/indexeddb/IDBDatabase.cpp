e2cb09432455792282d62193eff2f050aeaa00a0
dgrogan@chromium.org <dgrogan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-15 21:54:50 +0000

    IndexedDB: Add some ASSERTs https://bugs.webkit.org/show_bug.cgi?id=94055
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    These are just some additional ASSERTS and some ASSERT_WITH_MESSAGE in
    place of ASSERT. They are from the patch in
    https://bugs.webkit.org/show_bug.cgi?id=92897
    
    No new tests - we should never hit this behavior.
    
    * Modules/indexeddb/IDBDatabase.cpp:
    (WebCore::IDBDatabase::setVersion):
    (WebCore::IDBDatabase::registerFrontendCallbacks):
    * Modules/indexeddb/IDBRequest.cpp:
    (WebCore::IDBRequest::dispatchEvent):
    (WebCore::IDBRequest::enqueueEvent):
    * Modules/indexeddb/IDBTransaction.cpp:
    (WebCore::IDBTransaction::setActive):
    
    Source/WebKit/chromium:
    
    * src/IDBDatabaseBackendProxy.cpp:
    (WebKit::IDBDatabaseBackendProxy::setVersion):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125713 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBDatabase.cpp
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBDatabase.cpp
@@ -209,6 +209,7 @@ PassRefPtr<IDBVersionChangeRequest> IDBDatabase::setVersion(ScriptExecutionConte
     }
 
     RefPtr<IDBVersionChangeRequest> request = IDBVersionChangeRequest::create(context, IDBAny::create(this), version);
+    ASSERT(m_backend);
     m_backend->setVersion(version, request, m_databaseCallbacks, ec);
     return request;
 }
@@ -315,6 +316,7 @@ void IDBDatabase::onVersionChange(const String& version)
 
 void IDBDatabase::registerFrontendCallbacks()
 {
+    ASSERT(m_backend);
     m_backend->registerFrontendCallbacks(m_databaseCallbacks);
 }
 
