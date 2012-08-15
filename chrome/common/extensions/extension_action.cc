c9a9470151cea966b0e5194f7fd13ab5df65bf3b
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-15 23:02:17 +0000

    Fix crash in browserAction.setIcon
    Crash occurs when icon we set is empty.
    
    BUG=142908
    TEST=manual
    
    NOTE: I'll add test for this later.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824314
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_action.cc
+++ chrome/common/extensions/extension_action.cc
@@ -300,7 +300,7 @@ void ExtensionAction::CacheIcon(const std::string& path,
 }
 
 void ExtensionAction::SetIcon(int tab_id, const gfx::Image& image) {
-  SetValue(&icon_, tab_id, *image.ToImageSkia());
+  SetValue(&icon_, tab_id, image.AsImageSkia());
 }
 
 gfx::Image ExtensionAction::GetIcon(int tab_id) const {
