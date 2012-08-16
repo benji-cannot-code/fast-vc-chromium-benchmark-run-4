07f956df74ad7e905ca240bb38afe69699ba598f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-16 17:22:44 +0000

    Web Inspector: added Paint events for Images to TimelineAgent https://bugs.webkit.org/show_bug.cgi?id=90277
    Patch by Sergey Rogulenko <rogulenko@google.com> on 2012-08-16
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    - add DecodeImage & ResizeImage timeline events;
    - implement {will,did}DecodeImage and {will,did}ResizeImage methods of Timeline agent;
    
    Test: inspector/timeline/timeline-decode-resize.html
    
    * English.lproj/localizedStrings.js:
    * inspector/InspectorTimelineAgent.cpp:
    (TimelineRecordType):
    (WebCore::InspectorTimelineAgent::willDecodeImage):
    (WebCore::InspectorTimelineAgent::didDecodeImage):
    (WebCore::InspectorTimelineAgent::willResizeImage):
    (WebCore::InspectorTimelineAgent::didResizeImage):
    * inspector/TimelineRecordFactory.cpp:
    (WebCore::TimelineRecordFactory::createDecodeImageData):
    (WebCore):
    (WebCore::TimelineRecordFactory::createResizeImageData):
    * inspector/TimelineRecordFactory.h:
    (TimelineRecordFactory):
    * inspector/front-end/TimelineModel.js:
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel.initRecordStyles_):
    (WebInspector.TimelinePresentationModel.Record.prototype._getRecordDetails):
    
    LayoutTests:
    
    Added a test and fixed enum-stabitily because of a new Timeline Event.
    
    * inspector/timeline/resources/big.png: Added.
    * inspector/timeline/resources/test.bmp: Added.
    * inspector/timeline/resources/test.gif: Added.
    * inspector/timeline/resources/test.ico: Added.
    * inspector/timeline/resources/test.jpg: Added.
    * inspector/timeline/resources/test.png: Added.
    * inspector/timeline/resources/test.webp: Added.
    * inspector/timeline/timeline-decode-resize-expected.txt: Added.
    * inspector/timeline/timeline-decode-resize.html: Added.
    * inspector/timeline/timeline-enum-stability-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelineModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelineModel.js
@@ -53,6 +53,8 @@ WebInspector.TimelineModel.RecordType = {
     Layout: "Layout",
     RecalculateStyles: "RecalculateStyles",
     Paint: "Paint",
+    DecodeImage: "DecodeImage",
+    ResizeImage: "ResizeImage",
     CompositeLayers: "CompositeLayers",
 
     ParseHTML: "ParseHTML",
