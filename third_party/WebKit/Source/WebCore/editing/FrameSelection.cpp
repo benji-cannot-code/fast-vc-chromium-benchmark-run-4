76a8bfc7f6a4c684059a3cee37ab44bec8e6587c
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-16 07:06:14 +0000

    Unreviewed, rolling out r119705. http://trac.webkit.org/changeset/119705 https://bugs.webkit.org/show_bug.cgi?id=94192
    Causing crash on ClusterFuzz due to incorrect layout ordering
    change (Requested by inferno-sec on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-08-16
    
    * dom/CharacterData.cpp:
    (WebCore::CharacterData::setDataAndUpdate):
    * editing/FrameSelection.cpp:
    (WebCore::updatePositionAfterAdoptingTextReplacement):
    (WebCore::FrameSelection::textWillBeReplaced):
    * editing/FrameSelection.h:
    (FrameSelection):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
@@ -423,8 +423,6 @@ static void updatePositionAfterAdoptingTextReplacement(Position& position, Chara
     // (positionOffset > offset + oldLength) to avoid having a stale offset.
     if (positionOffset > offset + oldLength)
         position.moveToOffset(positionOffset - oldLength + newLength);
-
-    ASSERT(static_cast<unsigned>(position.offsetInContainerNode()) <= node->length());
 }
 
 static inline bool nodeIsDetachedFromDocument(Node* node)
@@ -434,7 +432,7 @@ static inline bool nodeIsDetachedFromDocument(Node* node)
     return highest->nodeType() == Node::DOCUMENT_FRAGMENT_NODE && !highest->isShadowRoot();
 }
 
-void FrameSelection::textWasReplaced(CharacterData* node, unsigned offset, unsigned oldLength, unsigned newLength)
+void FrameSelection::textWillBeReplaced(CharacterData* node, unsigned offset, unsigned oldLength, unsigned newLength)
 {
     // The fragment check is a performance optimization. See http://trac.webkit.org/changeset/30062.
     if (isNone() || !node || nodeIsDetachedFromDocument(node))
