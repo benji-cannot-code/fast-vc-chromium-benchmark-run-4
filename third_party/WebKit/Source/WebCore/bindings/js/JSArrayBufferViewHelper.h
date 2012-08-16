d2d96b675a60fcb8d2dc4cffb2a773201a3113ba
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-16 23:53:24 +0000

    Update TypedArrays to throw RangeError or similar instead of INDEX_SIZE_ERR https://bugs.webkit.org/show_bug.cgi?id=45118
    Patch by Arnaud Renevier <a.renevier@sisa.samsung.com> on 2012-08-16
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    Update TypedArrays to raise JavaScript RangeError instead of dom
    INDEX_SIZE_ERR exceptions. Also, update TypedArrays to raise TypeError
    instead of JavaScript SyntaxError or dom exceptions SYNTAX_ERR when
    calling set method with invalid arguments.
    
    Specification does not define the type of exceptions to raise, but
    other browsers raise JavaScript errors, so those changes will improve
    compatibility.
    
    New Test: fast/canvas/webgl/array-set-invalid-arguments.html
    
    Updated Tests expectations:
        fast/canvas/webgl/array-set-out-of-bounds.html
        fast/canvas/webgl/data-view-crash.html
        fast/canvas/webgl/data-view-test.html
    
    * bindings/js/JSArrayBufferViewHelper.h:
    (WebCore):
    (WebCore::setWebGLArrayWithTypedArrayArgument):
    (WebCore::setWebGLArrayHelper):
    (WebCore::constructArrayBufferViewWithTypedArrayArgument):
    (WebCore::constructArrayBufferViewWithArrayBufferArgument):
    (WebCore::constructArrayBufferView):
    * bindings/v8/custom/V8ArrayBufferViewCustom.h:
    (WebCore):
    (WebCore::constructWebGLArrayWithArrayBufferArgument):
    (WebCore::setWebGLArrayHelper):
    
    LayoutTests:
    
    New test to test invalid arguments to TypedArray set method
    
    Update array-set-out-of-bounds.html, data-view-crash.html and
    data-view-test.html test expectations no match new exceptions thrown.
    
    * fast/canvas/webgl/array-set-invalid-arguments-expected.txt: Added.
    * fast/canvas/webgl/array-set-invalid-arguments.html: Added.
    * fast/canvas/webgl/array-set-out-of-bounds-expected.txt:
    * fast/canvas/webgl/data-view-crash-expected.txt:
    * fast/canvas/webgl/data-view-test-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSArrayBufferViewHelper.h
+++ third_party/WebKit/Source/WebCore/bindings/js/JSArrayBufferViewHelper.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JSArrayBufferViewHelper_h
 #define JSArrayBufferViewHelper_h
 
-#include "ExceptionCode.h"
 #include "JSArrayBuffer.h"
 #include "JSArrayBufferView.h"
 #include "JSDOMBinding.h"
@@ -41,6 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/TypedArrayBase.h>
 
 namespace WebCore {
+
+static const char* tooLargeSize = "Size is too large (or is negative).";
 
 template<class C, typename T>
 bool copyTypedArrayBuffer(C* target, ArrayBufferView* source, unsigned sourceLength, unsigned offset)
@@ -116,7 +117,7 @@ bool setWebGLArrayWithTypedArrayArgument(JSC::ExecState* exec, C* impl)
     uint32_t length = asObject(exec->argument(0))->get(exec, JSC::Identifier(exec, "length")).toUInt32(exec);
 
     if (!(copyTypedArrayBuffer<C, T>(impl, array.get(), length, offset)))
-        setDOMException(exec, INDEX_SIZE_ERR);
+        throwError(exec, createRangeError(exec, "Index is out of range."));
 
     return true;
 }
@@ -125,7 +126,7 @@ template<class C, typename T>
 JSC::JSValue setWebGLArrayHelper(JSC::ExecState* exec, C* impl)
 {
     if (exec->argumentCount() < 1)
-        return JSC::throwSyntaxError(exec);
+        return JSC::throwError(exec, createNotEnoughArgumentsError(exec));
 
     if (setWebGLArrayWithTypedArrayArgument<C, T>(exec, impl))
         // void set(in WebGL<>Array array, [Optional] in unsigned long offset);
@@ -139,7 +140,7 @@ JSC::JSValue setWebGLArrayHelper(JSC::ExecState* exec, C* impl)
             offset = exec->argument(1).toInt32(exec);
         uint32_t length = array->get(exec, JSC::Identifier(exec, "length")).toInt32(exec);
         if (!impl->checkInboundData(offset, length))
-            setDOMException(exec, INDEX_SIZE_ERR);
+            throwError(exec, createRangeError(exec, "Index is out of range."));
         else {
             for (uint32_t i = 0; i < length; i++) {
                 JSC::JSValue v = array->get(exec, i);
@@ -152,7 +153,7 @@ JSC::JSValue setWebGLArrayHelper(JSC::ExecState* exec, C* impl)
         return JSC::jsUndefined();
     }
 
-    return JSC::throwSyntaxError(exec);
+    return JSC::throwTypeError(exec, "Invalid argument");
 }
 
 // Template function used by XXXArrayConstructors.
@@ -171,12 +172,12 @@ PassRefPtr<C> constructArrayBufferViewWithTypedArrayArgument(JSC::ExecState* exe
     uint32_t length = asObject(exec->argument(0))->get(exec, JSC::Identifier(exec, "length")).toUInt32(exec);
     RefPtr<C> array = C::createUninitialized(length);
     if (!array) {
-        setDOMException(exec, INDEX_SIZE_ERR);
+        throwError(exec, createRangeError(exec, tooLargeSize));
         return array;
     }
 
     if (!(copyTypedArrayBuffer<C, T>(array.get(), source.get(), length, 0))) {
-        setDOMException(exec, INDEX_SIZE_ERR);
+        throwError(exec, createRangeError(exec, tooLargeSize));
         return array;
     }
 
@@ -205,7 +206,7 @@ PassRefPtr<C> constructArrayBufferViewWithArrayBufferArgument(JSC::ExecState* ex
     }
     RefPtr<C> array = C::create(buffer, offset, length);
     if (!array)
-        setDOMException(exec, INDEX_SIZE_ERR);
+        throwError(exec, createRangeError(exec, tooLargeSize));
     return array;
 }
 
@@ -246,7 +247,7 @@ PassRefPtr<C> constructArrayBufferView(JSC::ExecState* exec)
         uint32_t length = srcArray->get(exec, JSC::Identifier(exec, "length")).toUInt32(exec);
         RefPtr<C> array = C::createUninitialized(length);
         if (!array) {
-            setDOMException(exec, INDEX_SIZE_ERR);
+            throwError(exec, createRangeError(exec, tooLargeSize));
             return array;
         }
 
@@ -262,7 +263,7 @@ PassRefPtr<C> constructArrayBufferView(JSC::ExecState* exec)
     if (length >= 0)
         result = C::create(static_cast<unsigned>(length));
     if (!result)
-        throwError(exec, createRangeError(exec, "ArrayBufferView size is not a small enough positive integer."));
+        throwError(exec, createRangeError(exec, tooLargeSize));
     return result;
 }
 
