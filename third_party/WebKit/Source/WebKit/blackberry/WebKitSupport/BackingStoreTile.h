0cf8a425136b459d8ab72d24003ba5604651059b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-16 18:07:14 +0000

    [BlackBerry] SurfacePool::waitForBuffer() sometimes waits for deleted EGLSyncKHR object https://bugs.webkit.org/show_bug.cgi?id=94208
    Patch by Arvid Nilsson <anilsson@rim.com> on 2012-08-16
    Reviewed by Rob Buis.
    
    SurfacePool::notifyBuffersComposited() adds a tile's previous
    sync object to the garbage list before replacing it with a new one.
    However, it failed to thoroughly clear all tiles that were referencing
    the old sync object.
    
    Thus it could happen that if a set of tiles A was composited, then
    another set of tiles B was composited, only the intersection of A and B
    was cleared of the soon-to-be-deleted sync object, and the subtraction
    A - B would reference an invalid sync object in case the BackingStore
    decided to render to one of the tiles in A - B before they were
    composited again.
    
    Fixed by storing each individual sync object in only one place so we
    don't have to rummage through all tiles and remove stale references to
    sync objects that are about to be destroyed.
    
    A new reference counted Fence class is added for this purpose, to store
    a sync object. Tiles refer to Fence instances instead of holding a sync
    object directly. Since Fence is reference counted, several tiles can
    refer to the same Fence instance, and clearing its sync object will
    instantly remove it from the grasp of all tiles that depend on that
    Fence.
    
    Since there's no point in waiting for the same Fence twice, the only
    operation provided on Fence is takePlatformSync() which returns the
    sync object and clears the Fence of its sync object.
    
    Reviewed internally by Filip Spacek.
    
    PR 193610
    
    * WebKitSupport/BackingStoreTile.cpp:
    (BlackBerry::WebKit::TileBuffer::TileBuffer):
    * WebKitSupport/BackingStoreTile.h:
    (BlackBerry):
    (Fence):
    (BlackBerry::Fence::create):
    (BlackBerry::Fence::takePlatformSync):
    (BlackBerry::Fence::Fence):
    (BlackBerry::WebKit::TileBuffer::fence):
    (BlackBerry::WebKit::TileBuffer::setFence):
    (TileBuffer):
    * WebKitSupport/SurfacePool.cpp:
    (BlackBerry::WebKit::SurfacePool::waitForBuffer):
    (BlackBerry::WebKit::SurfacePool::notifyBuffersComposited):
    * WebKitSupport/SurfacePool.h:
    (SurfacePool):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/BackingStoreTile.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/BackingStoreTile.h
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "BlackBerryPlatformIntRectRegion.h"
 #include "BlackBerryPlatformPrimitives.h"
+#include <wtf/PassRefPtr.h>
+#include <wtf/RefCounted.h>
+#include <wtf/RefPtr.h>
 
 namespace BlackBerry {
 namespace Platform {
@@ -29,6 +32,37 @@ namespace Graphics {
 struct Buffer;
 }
 }
+
+// Represents a fence that has been inserted into the command stream. You can wait on the corresponding platform sync
+// object to make sure that previous commands have been completed.
+// There is no reason to wait twice on the same platform sync object, so the only mechanism provided to access the sync
+// object will also clear it to prevent anyone from waiting again.
+// Fence is only refcounted on the compositing thread, so RefCounted will suffice, we don't need ThreadSafeRefCounted.
+class Fence : public RefCounted<Fence> {
+public:
+    static PassRefPtr<Fence> create(void* platformSync = 0)
+    {
+        return adoptRef(new Fence(platformSync));
+    }
+
+    // Returns 0 if this fence is stale (someone already waited on it)
+    // Otherwise returns the platform sync object and clears the sync
+    // object so no-one waits again.
+    void* takePlatformSync()
+    {
+        void* tmp = m_platformSync;
+        m_platformSync = 0;
+        return tmp;
+    }
+
+private:
+    Fence(void* platformSync)
+        : m_platformSync(platformSync)
+    {
+    }
+
+    void* m_platformSync;
+};
 
 namespace WebKit {
 class TileBuffer {
@@ -48,13 +82,13 @@ class TileBuffer {
 
         Platform::Graphics::Buffer* nativeBuffer() const;
 
-        void* syncObject() const { return m_syncObject; }
-        void setSyncObject(void* syncObject) { m_syncObject = syncObject; }
+        Fence* fence() const { return m_fence.get(); }
+        void setFence(PassRefPtr<Fence> fence) { m_fence = fence; }
 
     private:
         Platform::IntSize m_size;
         Platform::IntRectRegion m_renderedRegion;
-        void* m_syncObject;
+        RefPtr<Fence> m_fence;
         mutable Platform::Graphics::Buffer* m_buffer;
 };
 
