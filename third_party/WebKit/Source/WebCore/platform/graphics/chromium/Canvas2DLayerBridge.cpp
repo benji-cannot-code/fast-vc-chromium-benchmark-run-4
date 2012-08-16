7869194f96a23fc3cdb976d366df871accc43494
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-16 20:11:48 +0000

    [Chromium] Changing Canvas2DLayerBridge to use SkDeferredCanvas's notification client API https://bugs.webkit.org/show_bug.cgi?id=94234
    Patch by Justin Novosad <junov@chromium.org> on 2012-08-16
    Reviewed by James Robinson.
    
    The existing DeviceContext API in skia's SkDeferredCanvas is being
    deprecated in favor of the new NotificationClient interface, which is
    designed to allow WebKit to control deferred canvas global memory
    consumption.
    
    No new tests: this patch does not fix any bug and does not add new
    functionality. It is just a transition to a new skia interface.
    Coverage is assured by existing tests.
    
    * platform/graphics/chromium/Canvas2DLayerBridge.cpp:
    (WebCore):
    Removed the AcceleratedDeviceContext class completely and mixed it
    into Canvas2DLayerBridge by inheriting
    SkDeferredCanvas::NotificationClient
    (WebCore::Canvas2DLayerBridge::~Canvas2DLayerBridge):
    (WebCore::Canvas2DLayerBridge::deferredCanvas):
    (WebCore::Canvas2DLayerBridge::prepareForDraw):
    (WebCore::Canvas2DLayerBridge::skCanvas):
    * platform/graphics/chromium/Canvas2DLayerBridge.h:
    (Canvas2DLayerBridge):
    * platform/graphics/skia/ImageBufferSkia.cpp:
    Cleaning up unnecessary include of SkDeferredCanvas.h
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125804 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/Canvas2DLayerBridge.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/Canvas2DLayerBridge.cpp
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext3D.h"
 #include "GraphicsContext3DPrivate.h"
 #include "LayerRendererChromium.h" // For GLC() macro.
-#include "SkCanvas.h"
-#include "SkDeferredCanvas.h"
 #include "TraceEvent.h"
 #include <public/WebCompositor.h>
 #include <public/WebGraphicsContext3D.h>
@@ -43,31 +41,6 @@ using WebKit::WebGraphicsContext3D;
 using WebKit::WebTextureUpdater;
 
 namespace WebCore {
-
-class AcceleratedDeviceContext : public SkDeferredCanvas::DeviceContext {
-public:
-    AcceleratedDeviceContext(WebGraphicsContext3D* context, WebExternalTextureLayer layer, bool useDoubleBuffering)
-        : m_layer(layer)
-        , m_context()
-        , m_useDoubleBuffering(useDoubleBuffering)
-    {
-        ASSERT(context);
-        ASSERT(!layer.isNull());
-        m_context = context;
-    }
-
-    virtual void prepareForDraw()
-    {
-        if (!m_useDoubleBuffering)
-            m_layer.willModifyTexture();
-        m_context->makeContextCurrent();
-    }
-
-private:
-    WebExternalTextureLayer m_layer;
-    WebGraphicsContext3D* m_context;
-    bool m_useDoubleBuffering;
-};
 
 Canvas2DLayerBridge::Canvas2DLayerBridge(PassRefPtr<GraphicsContext3D> context, const IntSize& size, DeferralMode deferralMode, unsigned textureId)
     : m_deferralMode(deferralMode)
@@ -102,9 +75,10 @@ Canvas2DLayerBridge::Canvas2DLayerBridge(PassRefPtr<GraphicsContext3D> context, 
     m_layer.setRateLimitContext(!WebKit::WebCompositor::threadingEnabled() || m_useDoubleBuffering);
 }
 
-
 Canvas2DLayerBridge::~Canvas2DLayerBridge()
 {
+    if (SkDeferredCanvas* deferred = deferredCanvas())
+        deferred->setNotificationClient(0);
     m_layer.setTextureId(0);
     if (m_useDoubleBuffering) {
         m_context->makeContextCurrent();
@@ -114,11 +88,28 @@ Canvas2DLayerBridge::~Canvas2DLayerBridge()
     m_layer.clearClient();
 }
 
+SkDeferredCanvas* Canvas2DLayerBridge::deferredCanvas()
+{
+    if (m_deferralMode == Deferred)
+        return static_cast<SkDeferredCanvas*>(m_canvas);
+    return 0;
+}
+
+void Canvas2DLayerBridge::prepareForDraw()
+{
+    ASSERT(deferredCanvas());
+    if (!m_useDoubleBuffering)
+        m_layer.willModifyTexture();
+    m_context->makeContextCurrent();
+}
+
 SkCanvas* Canvas2DLayerBridge::skCanvas(SkDevice* device)
 {
+    ASSERT(!m_canvas);
     if (m_deferralMode == Deferred) {
-        SkAutoTUnref<AcceleratedDeviceContext> deviceContext(new AcceleratedDeviceContext(context(), m_layer, m_useDoubleBuffering));
-        m_canvas = new SkDeferredCanvas(device, deviceContext.get());
+        SkDeferredCanvas* deferred = new SkDeferredCanvas(device);
+        deferred->setNotificationClient(this);
+        m_canvas = deferred;
     } else
         m_canvas = new SkCanvas(device);
 
