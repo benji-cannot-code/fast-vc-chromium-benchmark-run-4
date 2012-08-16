d4ca085f2bf1c493351f89be06c7aa0c4ecda105
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-16 02:02:54 +0000

    Attempt at fixing crash. Because BrowserActionView lazily creates its button it is entirely possible for Layout() to be invoked before the button_ has been created.
    BUG=143001
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10857024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/action_box_menu.cc
+++ chrome/browser/ui/views/action_box_menu.cc
@@ -58,7 +58,7 @@ void ActionBoxMenu::RunMenu(views::MenuButton* menu_button) {
 }
 
 void ActionBoxMenu::ExecuteCommand(int id) {
-};
+}
 
 views::Border* ActionBoxMenu::CreateMenuBorder() {
   // TODO(yefim): Use correct theme color on non-Windows.
