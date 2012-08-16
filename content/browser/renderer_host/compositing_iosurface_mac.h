fa77c1fff9d574a8d4cb2d599fde44bb00a41cb6
jbates@chromium.org <jbates@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-16 19:00:14 +0000

    Hook up Mac OSX VSync parameters to compositor thread
    Also fixes precision error on Windows.
    
    BUG=137794
    
    Review URL: https://chromiumcodereview.appspot.com/10830347
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@151932 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositing_iosurface_mac.h
+++ content/browser/renderer_host/compositing_iosurface_mac.h
@@ -75,6 +75,8 @@ class CompositingIOSurfaceMac {
   bool is_vsync_disabled() const { return is_vsync_disabled_; }
 
   // Get vsync scheduling parameters.
+  // |interval_numerator/interval_denominator| equates to fractional number of
+  // seconds between vsyncs.
   void GetVSyncParameters(base::TimeTicks* timebase,
                           uint32* interval_numerator,
                           uint32* interval_denominator);
@@ -159,7 +161,7 @@ class CompositingIOSurfaceMac {
 
   // Called on display-link thread.
   void DisplayLinkTick(CVDisplayLinkRef display_link,
-                       const CVTimeStamp* output_time);
+                       const CVTimeStamp* time);
 
   void CalculateVsyncParametersLockHeld(const CVTimeStamp* time);
 
