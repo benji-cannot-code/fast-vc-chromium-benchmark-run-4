831d69fd1af223e387c708333ed2fa3711bfa6b2
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 13:08:56 +0000

    Send NOTIFICATION_WALLPAPER_ANIMATION_FINISHED for solid background
    BUG=143346
    TEST=set index of wallpaper image to -2 in Local State and run chrome with --login_manager
    
    Review URL: https://chromiumcodereview.appspot.com/10832340
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152079 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/desktop_background/desktop_background_controller.cc
+++ ash/desktop_background/desktop_background_controller.cc
@@ -261,6 +261,11 @@ void DesktopBackgroundController::OnWallpaperLoadCompleted(
   wallpaper_op_ = NULL;
 }
 
+void DesktopBackgroundController::NotifyAnimationFinished() {
+  Shell* shell = Shell::GetInstance();
+  shell->user_wallpaper_delegate()->OnWallpaperAnimationFinished();
+}
+
 ui::Layer* DesktopBackgroundController::SetColorLayerForContainer(
     SkColor color,
     aura::RootWindow* root_window,
@@ -270,6 +275,11 @@ ui::Layer* DesktopBackgroundController::SetColorLayerForContainer(
 
   Shell::GetContainer(root_window,container_id)->
       layer()->Add(background_layer);
+
+  MessageLoop::current()->PostTask(FROM_HERE,
+      base::Bind(&DesktopBackgroundController::NotifyAnimationFinished,
+                 weak_ptr_factory_.GetWeakPtr()));
+
   return background_layer;
 }
 
