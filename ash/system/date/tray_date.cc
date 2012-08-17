2ccaa0a80987edaffd0ae3c4ade4c5e516b2d7e1
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 23:44:23 +0000

    Fix the date text truncation issue.
    BUG=142716
    
    Review URL: https://chromiumcodereview.appspot.com/10827388
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152189 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/date/tray_date.cc
+++ ash/system/date/tray_date.cc
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const int kPaddingVertical = 10;
+const int kPaddingVertical = 6;
 
 class DateDefaultView : public views::View,
                         public views::ButtonListener {
