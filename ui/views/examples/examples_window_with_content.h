bf42a0c17160d4ef69fe6e8aa74624db54e65bc9
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 02:11:41 +0000

    views/examples: Fix include paths and header guards.
    BUG=141187
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10855193
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152015 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_window_with_content.h
+++ ui/views/examples/examples_window_with_content.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
-#define UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
+#ifndef UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_WITH_CONTENT_H_
+#define UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_WITH_CONTENT_H_
 
 namespace content {
 class BrowserContext;
@@ -25,4 +25,4 @@ void ShowExamplesWindow(Operation operation,
 }  // namespace examples
 }  // namespace views
 
-#endif  // UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_H_
+#endif  // UI_VIEWS_EXAMPLES_EXAMPLES_WINDOW_WITH_CONTENT_H_
