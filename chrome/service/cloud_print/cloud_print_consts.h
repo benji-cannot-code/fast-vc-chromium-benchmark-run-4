08b78a0b2af9ddf927fb45aed534a9f4f0e66199
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 02:22:32 +0000

    Add additional logging. Add additional logging and message to server for dealing with printers that a connector loses track of.
    BUG=142337
    
    
    Review URL: https://chromiumcodereview.appspot.com/10836278
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152021 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/cloud_print/cloud_print_consts.h
+++ chrome/service/cloud_print/cloud_print_consts.h
@@ -46,6 +46,7 @@ extern const char kJobFetchReasonQueryMore[];
 extern const char kPrintSystemFailedMessageId[];
 extern const char kGetPrinterCapsFailedMessageId[];
 extern const char kEnumPrintersFailedMessageId[];
+extern const char kZombiePrinterMessageId[];
 extern const char kDefaultCloudPrintOAuthClientId[];
 extern const char kDefaultCloudPrintOAuthClientSecret[];
 
