f2fe98d4a161629cbd36a52dbcdccbb89a695e6d
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 20:08:11 +0000

    Revert 151975 - Enable verbose logging during PluginMsg_CreateInstance.
    This should get rid of the spurious ERROR messages when closing tabs, and log backtraces when we need them.
    
    BUG=141055
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834355
    
    TBR=bauerb@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10854207
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152141 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/np_channel_base.cc
+++ content/common/np_channel_base.cc
@@ -142,7 +142,7 @@ bool NPChannelBase::Init(base::MessageLoopProxy* ipc_message_loop,
 
 bool NPChannelBase::Send(IPC::Message* message) {
   if (!channel_.get()) {
-    VLOG(1) << "Channel is NULL; dropping message";
+    LOG(ERROR) << "Channel is NULL; dropping message";
     delete message;
     return false;
   }
