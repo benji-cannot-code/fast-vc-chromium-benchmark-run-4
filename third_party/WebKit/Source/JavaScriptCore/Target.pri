498765a68269180070f2912a9caf084c0aff5dd2
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-17 09:42:31 +0000

    [Qt] Replace use of internal Weak smart pointer with JSWeakObjectMap https://bugs.webkit.org/show_bug.cgi?id=93872
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/JavaScriptCore:
    
    * Target.pri: Add missing JSWeakObjectMap file to build.
    
    Source/WebCore:
    
    The intention of this patch series is to replace use of internal JSC
    API with use of the stable and public C API.
    
    The JSC::Weak template is internal API and the only part of the C API
    that exposes similar functionality is the JSWeakObjectMap. It is
    special in the sense that its life-time is tied to the life-time of the
    JS global object, which in turn is subject to garbage collection. In
    order to maximize re-use of the same map across different JSContextRef
    instances, we use one JSWeakObjectMap per context group and store the
    map in a separate context.
    
    * bridge/qt/qt_instance.cpp:
    (JSC::Bindings::unusedWeakObjectMapCallback):
    (Bindings):
    (JSC::Bindings::WeakMapImpl::WeakMapImpl):
    (JSC::Bindings::WeakMapImpl::~WeakMapImpl):
    (JSC::Bindings::WeakMap::~WeakMap):
    (JSC::Bindings::WeakMap::set):
    (JSC::Bindings::WeakMap::get):
    (JSC::Bindings::WeakMap::remove):
    * bridge/qt/qt_instance.h:
    (WeakMapImpl):
    (Bindings):
    (WeakMap):
    (QtInstance):
    * bridge/qt/qt_runtime.cpp:
    (JSC::Bindings::QtRuntimeMethod::~QtRuntimeMethod):
    (JSC::Bindings::QtRuntimeMethod::jsObjectRef):
    * bridge/qt/qt_runtime.h:
    (QtRuntimeMethod):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125873 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/Target.pri
+++ third_party/WebKit/Source/JavaScriptCore/Target.pri
@@ -41,6 +41,7 @@ SOURCES += \
     API/JSObjectRef.cpp \
     API/JSStringRef.cpp \
     API/JSValueRef.cpp \
+    API/JSWeakObjectMapRefPrivate.cpp \
     API/OpaqueJSString.cpp \
     assembler/ARMAssembler.cpp \
     assembler/ARMv7Assembler.cpp \
--- third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.h
+++ third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define qt_instance_h
 
 #include "BridgeJSC.h"
+#include "JSWeakObjectMapRefPrivate.h"
 #include <QPointer>
 #include "Weak.h"
 #include "runtime_root.h"
@@ -35,6 +36,27 @@ namespace Bindings {
 class QtClass;
 class QtField;
 class QtRuntimeMethod;
+
+class WeakMapImpl : public RefCounted<WeakMapImpl> {
+public:
+    WeakMapImpl(JSContextGroupRef);
+    ~WeakMapImpl();
+
+    JSGlobalContextRef m_context;
+    JSWeakObjectMapRef m_map;
+};
+
+class WeakMap {
+public:
+    ~WeakMap();
+
+    void set(JSContextRef, void* key, JSObjectRef);
+    JSObjectRef get(void* key);
+    void remove(void* key);
+
+private:
+    RefPtr<WeakMapImpl> m_impl;
+};
 
 class QtInstance : public Instance {
 public:
@@ -82,11 +104,13 @@ private:
 
     friend class QtClass;
     friend class QtField;
+    friend class QtRuntimeMethod;
     QtInstance(QObject*, PassRefPtr<RootObject>, ValueOwnership); // Factory produced only..
     mutable QtClass* m_class;
     QPointer<QObject> m_object;
     QObject* m_hashkey;
     mutable QHash<QByteArray, QtRuntimeMethod*> m_methods;
+    WeakMap m_cachedMethods;
     mutable QHash<QString, QtField*> m_fields;
     ValueOwnership m_ownership;
 };
