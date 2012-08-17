67f7ce5e3c7938f454e693cc21db4058a8cff774
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 21:00:25 +0000

    ResourceId and unit test cleanup.
    * GDataEntry must always be constructed with a valid directory_service ptr. Add a DCHECK for this, and remove runtime tests for null directory_service_ ptr in all methods.
    * RemoveEntryFromResourceMap takes resource_id instead of GDataEntry*.
    * RemoveEntryFromResourceMap ensures resource_id is non-empty and that an actual deletion occurs every time.
    * AddEntryToResourceMap has a DCHECK to guard against multiple additions, or addition of empty resource_id.
    * Remove accessors child_files() and child_directories() and make typedefs for GDataFileCollection and GDataDirectoryCollection private.
    * Move GetChildDirectoryPaths from gdata_wapi_feed_processor.cc anon namespace to GDataDirectory.
    * Fixes for unit tests that added entries with null resource ids, and duplicate resource ids.
    
    BUG=137725
    TEST=unit tests.
    Review URL: https://chromiumcodereview.appspot.com/10854199
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152151 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_files.cc
+++ chrome/browser/chromeos/gdata/gdata_files.cc
@@ -34,6 +34,7 @@ GDataEntry::GDataEntry(GDataDirectoryService* directory_service)
     : parent_(NULL),
       directory_service_(directory_service),
       deleted_(false) {
+  DCHECK(directory_service);
 }
 
 GDataEntry::~GDataEntry() {
@@ -237,8 +238,7 @@ void GDataDirectory::AddEntry(GDataEntry* entry) {
            << ", resource = " + entry->resource_id();
 
   // Add entry to resource map.
-  if (directory_service_)
-    directory_service_->AddEntryToResourceMap(entry);
+  directory_service_->AddEntryToResourceMap(entry);
 
   // Setup child and parent links.
   AddChild(entry);
@@ -246,9 +246,10 @@ void GDataDirectory::AddEntry(GDataEntry* entry) {
 }
 
 bool GDataDirectory::TakeOverEntries(GDataDirectory* dir) {
-  for (GDataFileCollection::iterator iter = dir->child_files_.begin();
+  for (GDataFileCollection::const_iterator iter = dir->child_files_.begin();
        iter != dir->child_files_.end(); ++iter) {
     GDataEntry* entry = iter->second;
+    directory_service_->RemoveEntryFromResourceMap(entry->resource_id());
     entry->SetParent(NULL);
     AddEntry(entry);
   }
@@ -258,6 +259,7 @@ bool GDataDirectory::TakeOverEntries(GDataDirectory* dir) {
       dir->child_directories_.begin();
        iter != dir->child_directories_.end(); ++iter) {
     GDataEntry* entry = iter->second;
+    directory_service_->RemoveEntryFromResourceMap(entry->resource_id());
     entry->SetParent(NULL);
     AddEntry(entry);
   }
@@ -274,9 +276,9 @@ void GDataDirectory::RemoveEntry(GDataEntry* entry) {
 
 GDataEntry* GDataDirectory::FindChild(
     const FilePath::StringType& file_name) const {
-  GDataFileCollection::const_iterator it = child_files_.find(file_name);
-  if (it != child_files_.end())
-    return it->second;
+  GDataFileCollection::const_iterator iter = child_files_.find(file_name);
+  if (iter != child_files_.end())
+    return iter->second;
 
   GDataDirectoryCollection::const_iterator itd =
       child_directories_.find(file_name);
@@ -289,13 +291,13 @@ GDataEntry* GDataDirectory::FindChild(
 void GDataDirectory::AddChild(GDataEntry* entry) {
   DCHECK(entry);
 
-  GDataFile* file = entry->AsGDataFile();
-  if (file)
-    child_files_.insert(std::make_pair(entry->base_name(), file));
+  if (entry->AsGDataFile())
+    child_files_.insert(std::make_pair(entry->base_name(),
+                                       entry->AsGDataFile()));
 
-  GDataDirectory* directory = entry->AsGDataDirectory();
-  if (directory)
-    child_directories_.insert(std::make_pair(entry->base_name(), directory));
+  if (entry->AsGDataDirectory())
+    child_directories_.insert(std::make_pair(entry->base_name(),
+                                             entry->AsGDataDirectory()));
 }
 
 void GDataDirectory::RemoveChild(GDataEntry* entry) {
@@ -305,8 +307,7 @@ void GDataDirectory::RemoveChild(GDataEntry* entry) {
   // entry must be present in this directory.
   DCHECK_EQ(entry, FindChild(base_name));
   // Remove entry from resource map first.
-  if (directory_service_)
-    directory_service_->RemoveEntryFromResourceMap(entry);
+  directory_service_->RemoveEntryFromResourceMap(entry->resource_id());
 
   // Then delete it from tree.
   child_files_.erase(base_name);
@@ -321,10 +322,10 @@ void GDataDirectory::RemoveChildren() {
 }
 
 void GDataDirectory::RemoveChildFiles() {
+  DVLOG(1) << "RemoveChildFiles " << resource_id();
   for (GDataFileCollection::const_iterator iter = child_files_.begin();
        iter != child_files_.end(); ++iter) {
-    if (directory_service_)
-      directory_service_->RemoveEntryFromResourceMap(iter->second);
+    directory_service_->RemoveEntryFromResourceMap(iter->second->resource_id());
   }
   STLDeleteValues(&child_files_);
   child_files_.clear();
@@ -336,11 +337,19 @@ void GDataDirectory::RemoveChildDirectories() {
     GDataDirectory* dir = iter->second;
     // Remove directories recursively.
     dir->RemoveChildren();
-    if (directory_service_)
-      directory_service_->RemoveEntryFromResourceMap(dir);
+    directory_service_->RemoveEntryFromResourceMap(dir->resource_id());
   }
   STLDeleteValues(&child_directories_);
   child_directories_.clear();
+}
+
+void GDataDirectory::GetChildDirectoryPaths(std::set<FilePath>* child_dirs) {
+  for (GDataDirectoryCollection::const_iterator it = child_directories_.begin();
+       it != child_directories_.end(); ++it) {
+    GDataDirectory* child_dir = it->second;
+    child_dirs->insert(child_dir->GetFilePath());
+    child_dir->GetChildDirectoryPaths(child_dirs);
+  }
 }
 
 // Convert to/from proto.
@@ -499,15 +508,15 @@ void GDataDirectory::ToProto(GDataDirectoryProto* proto) const {
 
 scoped_ptr<GDataEntryProtoVector> GDataDirectory::ToProtoVector() const {
   scoped_ptr<GDataEntryProtoVector> entries(new GDataEntryProtoVector);
-  for (GDataFileCollection::const_iterator iter = child_files().begin();
-       iter != child_files().end(); ++iter) {
+  for (GDataFileCollection::const_iterator iter = child_files_.begin();
+       iter != child_files_.end(); ++iter) {
     GDataEntryProto proto;
     iter->second->ToProto(&proto);
     entries->push_back(proto);
   }
   for (GDataDirectoryCollection::const_iterator iter =
-           child_directories().begin();
-       iter != child_directories().end(); ++iter) {
+           child_directories_.begin();
+       iter != child_directories_.end(); ++iter) {
     GDataEntryProto proto;
     // Convert to GDataEntry, as we don't want to include children in |proto|.
     static_cast<const GDataEntry*>(iter->second)->ToProtoFull(&proto);
--- chrome/browser/chromeos/gdata/gdata_files.h
+++ chrome/browser/chromeos/gdata/gdata_files.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_CHROMEOS_GDATA_GDATA_FILES_H_
 
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -64,6 +65,7 @@ class GDataEntry {
   // Converts to/from proto. Only handles the common part (i.e. does not
   // touch |file_specific_info|).
   bool FromProto(const GDataEntryProto& proto) WARN_UNUSED_RESULT;
+  // TODO(achuith,satorux): Should this be virtual?
   void ToProto(GDataEntryProto* proto) const;
 
   // Similar to ToProto() but this fills in |file_specific_info| and
@@ -168,10 +170,6 @@ class GDataEntry {
   DISALLOW_COPY_AND_ASSIGN(GDataEntry);
 };
 
-typedef std::map<FilePath::StringType, GDataFile*> GDataFileCollection;
-typedef std::map<FilePath::StringType, GDataDirectory*>
-    GDataDirectoryCollection;
-
 // Represents "file" in in a GData virtual file system. On gdata feed side,
 // this could be either a regular file or a server side document.
 class GDataFile : public GDataEntry {
@@ -230,12 +228,6 @@ class GDataDirectory : public GDataEntry {
   // Converts the children as a vector of GDataEntryProto.
   scoped_ptr<GDataEntryProtoVector> ToProtoVector() const;
 
-  // Collection of children files/directories.
-  const GDataFileCollection& child_files() const { return child_files_; }
-  const GDataDirectoryCollection& child_directories() const {
-    return child_directories_;
-  }
-
  private:
   // TODO(satorux): Remove the friend statements. crbug.com/139649
   friend class GDataDirectoryService;
@@ -279,7 +271,14 @@ class GDataDirectory : public GDataEntry {
   void RemoveChildFiles();
   void RemoveChildDirectories();
 
-  // Collection of children GDataEntry items.
+  // Recursively extracts the paths set of all sub-directories.
+  void GetChildDirectoryPaths(std::set<FilePath>* child_dirs);
+
+  // Maps between base_name and resource_id of files and directories.
+  typedef std::map<FilePath::StringType, GDataFile*> GDataFileCollection;
+  typedef std::map<FilePath::StringType, GDataDirectory*>
+      GDataDirectoryCollection;
+  // Collection of children.
   GDataFileCollection child_files_;
   GDataDirectoryCollection child_directories_;
 
