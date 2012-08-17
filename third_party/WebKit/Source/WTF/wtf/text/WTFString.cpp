fa8d0923da7cb6d09872e78a79c623dbace38392
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-17 05:24:34 +0000

    Build fix for Chromium-mac.
    * wtf/text/WTFString.cpp: Remove unnecessary includes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125855 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/text/WTFString.cpp
+++ third_party/WebKit/Source/WTF/wtf/text/WTFString.cpp
@@ -33,10 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/dtoa.h>
 #include <wtf/unicode/UTF8.h>
 #include <wtf/unicode/Unicode.h>
-#if OS(DARWIN)
-#include <emmintrin.h>
-#include <tmmintrin.h>
-#endif
 
 using namespace std;
 
