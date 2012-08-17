238d90a6f35126e4582fcc1c23f8522704b16dff
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-17 15:38:39 +0000

    Unreviewed, rolling out r125892. http://trac.webkit.org/changeset/125892 https://bugs.webkit.org/show_bug.cgi?id=94350
    Broke windows build (Requested by vollick on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-08-17
    
    Source/WebCore:
    
    * platform/graphics/chromium/cc/CCActiveAnimation.cpp:
    (WebCore::CCActiveAnimation::CCActiveAnimation):
    (WebCore::CCActiveAnimation::~CCActiveAnimation):
    (WebCore::CCActiveAnimation::setRunState):
    (WebCore::CCActiveAnimation::cloneForImplThread):
    * platform/graphics/chromium/cc/CCActiveAnimation.h:
    (CCActiveAnimation):
    * platform/graphics/chromium/cc/CCLayerAnimationController.cpp:
    (WebCore::CCLayerAnimationController::pushNewAnimationsToImplThread):
    (WebCore):
    (WebCore::CCLayerAnimationController::replaceImplThreadAnimations):
    (WebCore::CCLayerAnimationController::tickAnimations):
    
    Source/WebKit/chromium:
    
    * src/WebAnimationImpl.cpp:
    (WebKit::WebAnimationImpl::cloneToCCAnimation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCActiveAnimation.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCActiveAnimation.h
@@ -57,16 +57,12 @@ public:
         Running,
         Paused,
         Finished,
-        Aborted,
-        // This sentinel must be last.
-        RunStateEnumSize
+        Aborted
     };
 
     enum TargetProperty {
         Transform = 0,
-        Opacity,
-        // This sentinel must be last.
-        TargetPropertyEnumSize
+        Opacity
     };
 
     static PassOwnPtr<CCActiveAnimation> create(PassOwnPtr<CCAnimationCurve>, int animationId, int groupId, TargetProperty);
@@ -117,14 +113,7 @@ public:
     // of iterations, returns the relative time in the current iteration.
     double trimTimeToCurrentIteration(double monotonicTime) const;
 
-    enum InstanceType {
-        ControllingInstance = 0,
-        NonControllingInstance
-    };
-
-    PassOwnPtr<CCActiveAnimation> clone(InstanceType) const;
-    PassOwnPtr<CCActiveAnimation> cloneAndInitialize(InstanceType, RunState initialRunState, double startTime) const;
-    bool isControllingInstance() const { return m_isControllingInstance; }
+    PassOwnPtr<CCActiveAnimation> cloneForImplThread() const;
 
     void pushPropertiesTo(CCActiveAnimation*) const;
 
@@ -166,15 +155,6 @@ private:
     // about these values.
     double m_pauseTime;
     double m_totalPausedTime;
-
-    // Animations lead dual lives. An active animation will be conceptually owned by
-    // two controllers, one on the impl thread and one on the main. In reality, there
-    // will be two separate CCActiveAnimation instances for the same animation. They
-    // will have the same group id and the same target property (these two values
-    // uniquely identify an animation). The instance on the impl thread is the instance
-    // that ultimately controls the values of the animating layer and so we will refer
-    // to it as the 'controlling instance'.
-    bool m_isControllingInstance;
 };
 
 } // namespace WebCore
