287b13083d39c07dd9961f3ed2ad2fc39a4ba16d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-17 00:29:35 +0000

    Rename DOMWindow::clear to something more descriptive https://bugs.webkit.org/show_bug.cgi?id=93993
    Reviewed by Eric Seidel.
    
    As requested by Eric Seidel. In WebKit, we typically use "clear" to
    mean zeroing out a pointer (e.g., OwnPtr::clear). For DOMWindow, it's
    more like we're resetting the DOMWindow to a known state (i.e., its
    initial state), modulo a wrinkle w.r.t. the PageCache. The new name
    attempts to clarify the situation.
    
    * dom/Document.cpp:
    (WebCore::Document::~Document):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::clear):
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::~DOMWindow):
    (WebCore::DOMWindow::frameDestroyed):
    (WebCore::DOMWindow::resetUnlessSuspendedForPageCache):
    (WebCore::DOMWindow::resetDOMWindowProperties):
    * page/DOMWindow.h:
    (DOMWindow):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/DOMWindow.cpp
+++ third_party/WebKit/Source/WebCore/page/DOMWindow.cpp
@@ -438,9 +438,9 @@ DOMWindow::~DOMWindow()
     else
         willDestroyDocumentInFrame();
 
-    // As the ASSERTs above indicate, this clear should only be necesary if this DOMWindow is suspended for the page cache.
+    // As the ASSERTs above indicate, this reset should only be necessary if this DOMWindow is suspended for the page cache.
     // But we don't want to risk any of these objects hanging around after we've been destroyed.
-    clearDOMWindowProperties();
+    resetDOMWindowProperties();
 
     removeAllUnloadEventListeners(this);
     removeAllBeforeUnloadEventListeners(this);
@@ -475,7 +475,7 @@ void DOMWindow::frameDestroyed()
 {
     willDestroyDocumentInFrame();
     FrameDestructionObserver::frameDestroyed();
-    clearDOMWindowProperties();
+    resetDOMWindowProperties();
 }
 
 void DOMWindow::willDetachPage()
@@ -523,16 +523,12 @@ void DOMWindow::unregisterProperty(DOMWindowProperty* property)
     m_properties.remove(property);
 }
 
-void DOMWindow::clear()
+void DOMWindow::resetUnlessSuspendedForPageCache()
 {
-    // The main frame will always try to clear its DOMWindow when a new load is committed, even if that
-    // DOMWindow is suspended in the page cache.
-    // In those cases we need to make sure we don't actually clear it.
     if (m_suspendedForPageCache)
         return;
-    
     willDestroyDocumentInFrame();
-    clearDOMWindowProperties();
+    resetDOMWindowProperties();
 }
 
 void DOMWindow::suspendForPageCache()
@@ -568,7 +564,7 @@ void DOMWindow::reconnectDOMWindowProperties()
         properties[i]->reconnectFrameFromPageCache(m_frame);
 }
 
-void DOMWindow::clearDOMWindowProperties()
+void DOMWindow::resetDOMWindowProperties()
 {
     m_properties.clear();
 
