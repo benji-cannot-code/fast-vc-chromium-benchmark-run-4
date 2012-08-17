d3364675c7c3b8b997f65c936368dc68e0fc6548
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 18:41:59 +0000

    [Chromoting] Avoid potential conflicts leading to pending reboots while upgrading the me2me host: - Install the host binaries to versioned folders. - Drop references to the COM objects quickly. - Auto-generate CLSID of the daemon controller at GYP time. This makes sure that a freshly installed version of the controller will be launched after upgrade even if there is already running instance cached by RPCSS.
    This CL restores https://chromiumcodereview.appspot.com/10831037 (r148671) and https://chromiumcodereview.appspot.com/10837087 (r149866) previously reverted by r150494. Unlike the previous two CLs this one does not require GYP changes (that is changes to GYP itself, not .gyp files).
    
    BUG=137755
    
    
    Review URL: https://chromiumcodereview.appspot.com/10828352
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152120 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/tools/uuidgen.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/env python
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""
+uuidgen.py -- UUID generation utility.
+"""
+
+import sys
+import uuid
+
+def main():
+  print uuid.uuid4()
+  return 0
+
+if __name__ == '__main__':
+  sys.exit(main())
