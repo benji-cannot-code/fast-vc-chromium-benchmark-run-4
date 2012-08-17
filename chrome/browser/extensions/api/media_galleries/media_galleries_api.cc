b9a03d0d6b26560f91614461ef56ef278cfe3204
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 19:17:13 +0000

    Cocoa: Implement media gallery dialog
    This is the cocoa version of the media galleries permission dialog. The dialog is implemented as an NSAlert displayed as a constrained window. The gallery checkboxes are added inside a accessory view.
    
    The views and GTK versions are here:
    http://codereview.chromium.org/10828166/
    https://chromiumcodereview.appspot.com/10826129
    
    Screenshots: http://i.imgur.com/fedm0.png
    
    BUG=134929
    TBR=mihaip@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10834242
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152128 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/media_galleries/media_galleries_api.cc
+++ chrome/browser/extensions/api/media_galleries/media_galleries_api.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/platform_file.h"
 #include "base/values.h"
 #include "chrome/browser/media_gallery/media_file_system_registry.h"
+#include "chrome/browser/media_gallery/media_galleries_dialog_controller.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/extensions/api/experimental_media_galleries.h"
 #include "content/public/browser/child_process_security_policy.h"
@@ -22,10 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/sys_string_conversions.h"
-#endif
-
-#if !defined(OS_MACOSX)
-#include "chrome/browser/media_gallery/media_galleries_dialog_controller.h"
 #endif
 
 using content::WebContents;
@@ -113,16 +110,11 @@ void MediaGalleriesGetMediaFileSystemsFunction::ShowDialog() {
     return;
   }
 
-#if !defined(OS_MACOSX)
   // Controller will delete itself.
   new chrome::MediaGalleriesDialogController(
       tab_contents, *GetExtension(),
       base::Bind(&MediaGalleriesGetMediaFileSystemsFunction::ReturnGalleries,
                  this));
-#else
-  // TODO(estade): implement dialog on Views and Cocoa.
-  ReturnGalleries();
-#endif
 }
 
 // MediaGalleriesAssembleMediaFileFunction -------------------------------------
