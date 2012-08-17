4aa3be0381fe64cd7506af96253d015ee5e37721
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-17 00:02:23 +0000

    ProgressTracker never completes if iframe detached during parsing https://bugs.webkit.org/show_bug.cgi?id=92272
    Reviewed by Adam Barth.
    
    Add a simple helper class to FrameLoader to ensure progressStarted/progressCompleted calls are matched,
    and balance the calls when the Frame is detached.
    
    No new tests, as this behavior has only been producing reliably by setting a breakpoint in a specific place.
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::init):
    (WebCore::FrameLoader::FrameProgressTracker::progressStarted):
    (WebCore::FrameLoader::FrameProgressTracker::progressCompleted):
    (WebCore::FrameLoader::FrameProgressTracker::~FrameProgressTracker):
    (WebCore::FrameLoader::prepareForLoadStart):
    (WebCore::FrameLoader::clearProvisionalLoad):
    (WebCore::FrameLoader::checkLoadCompleteForThisFrame):
    (WebCore::FrameLoader::detachFromParent):
    * loader/FrameLoader.h:
    (FrameProgressTracker):
    (WebCore::FrameLoader::FrameProgressTracker::create):
    (WebCore::FrameLoader::FrameProgressTracker::FrameProgressTracker):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -166,6 +166,42 @@ static bool isDocumentSandboxed(Frame* frame, SandboxFlags mask)
     return frame->document() && frame->document()->isSandboxed(mask);
 }
 
+class FrameLoader::FrameProgressTracker {
+public:
+    static PassOwnPtr<FrameProgressTracker> create(Frame* frame) { return adoptPtr(new FrameProgressTracker(frame)); }
+    ~FrameProgressTracker()
+    {
+        ASSERT(!m_inProgressCount || m_frame->page());
+        for (; m_inProgressCount; m_inProgressCount--)
+            m_frame->page()->progress()->progressCompleted(m_frame);
+    }
+
+    void progressStarted()
+    {
+        ASSERT(m_frame->page());
+        m_inProgressCount++;
+        m_frame->page()->progress()->progressStarted(m_frame);
+    }
+
+    void progressCompleted()
+    {
+        ASSERT(m_inProgressCount > 0);
+        ASSERT(m_frame->page());
+        m_inProgressCount--;
+        m_frame->page()->progress()->progressCompleted(m_frame);
+    }
+
+private:
+    FrameProgressTracker(Frame* frame)
+        : m_frame(frame)
+        , m_inProgressCount(0)
+    {
+    }
+
+    Frame* m_frame;
+    int m_inProgressCount;
+};
+
 FrameLoader::FrameLoader(Frame* frame, FrameLoaderClient* client)
     : m_frame(frame)
     , m_client(client)
@@ -229,6 +265,7 @@ void FrameLoader::init()
     m_didCallImplicitClose = true;
 
     m_networkingContext = m_client->createNetworkingContext();
+    m_progressTracker = FrameProgressTracker::create(m_frame);
 }
 
 void FrameLoader::setDefersLoading(bool defers)
@@ -1088,9 +1125,8 @@ void FrameLoader::prepareForHistoryNavigation()
 
 void FrameLoader::prepareForLoadStart()
 {
-    if (Page* page = m_frame->page())
-        page->progress()->progressStarted(m_frame);
     m_client->dispatchDidStartProvisionalLoad();
+    m_progressTracker->progressStarted();
 
     // Notify accessibility.
     if (AXObjectCache::accessibilityEnabled()) {
@@ -1647,8 +1683,7 @@ void FrameLoader::setState(FrameState newState)
 void FrameLoader::clearProvisionalLoad()
 {
     setProvisionalDocumentLoader(0);
-    if (Page* page = m_frame->page())
-        page->progress()->progressCompleted(m_frame);
+    m_progressTracker->progressCompleted();
     setState(FrameStateComplete);
 }
 
@@ -2124,9 +2159,8 @@ void FrameLoader::checkLoadCompleteForThisFrame()
                 return;
 
             if (!settings->needsDidFinishLoadOrderQuirk()) {
+                m_progressTracker->progressCompleted();
                 if (Page* page = m_frame->page()) {
-                    page->progress()->progressCompleted(m_frame);
-
                     if (m_frame == page->mainFrame())
                         page->resetRelevantPaintedObjectCounter();
                 }
@@ -2144,9 +2178,8 @@ void FrameLoader::checkLoadCompleteForThisFrame()
             }
 
             if (settings->needsDidFinishLoadOrderQuirk()) {
+                m_progressTracker->progressCompleted();
                 if (Page* page = m_frame->page()) {
-                    page->progress()->progressCompleted(m_frame);
-
                     if (m_frame == page->mainFrame())
                         page->resetRelevantPaintedObjectCounter();
                 }
@@ -2369,6 +2402,8 @@ void FrameLoader::detachFromParent()
     InspectorInstrumentation::frameDetachedFromParent(m_frame);
 
     detachViewsAndDocumentLoader();
+
+    m_progressTracker.clear();
 
     if (Frame* parent = m_frame->tree()->parent()) {
         parent->loader()->closeAndRemoveChild(m_frame);
