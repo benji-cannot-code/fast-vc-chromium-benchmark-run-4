941c06c5f0ca51af21d7a163f6985d396259c960
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-17 20:08:57 +0000

    [content shell] add --no-timeout option
    BUG=111316
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825419
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152142 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/layout_test_controller_host.cc
+++ content/shell/layout_test_controller_host.cc
@@ -5,9 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/layout_test_controller_host.h"
 
+#include "base/command_line.h"
 #include "base/message_loop.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/shell/shell_messages.h"
+#include "content/shell/shell_switches.h"
 #include "webkit/support/webkit_support_gfx.h"
 
 namespace content {
@@ -185,12 +187,14 @@ void LayoutTestControllerHost::OnDumpChildFramesAsText() {
 void LayoutTestControllerHost::OnWaitUntilDone() {
   if (wait_until_done_)
     return;
-  watchdog_.Reset(base::Bind(&LayoutTestControllerHost::TimeoutHandler,
-                             base::Unretained(this)));
-  MessageLoop::current()->PostDelayedTask(
-      FROM_HERE,
-      watchdog_.callback(),
-      base::TimeDelta::FromMilliseconds(kTestTimeoutMilliseconds));
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kNoTimeout)) {
+    watchdog_.Reset(base::Bind(&LayoutTestControllerHost::TimeoutHandler,
+                               base::Unretained(this)));
+    MessageLoop::current()->PostDelayedTask(
+        FROM_HERE,
+        watchdog_.callback(),
+        base::TimeDelta::FromMilliseconds(kTestTimeoutMilliseconds));
+  }
   wait_until_done_ = true;
 }
 
--- content/shell/shell_switches.h
+++ content/shell/shell_switches.h
@@ -13,6 +13,7 @@ namespace switches {
 extern const char kCheckLayoutTestSysDeps[];
 extern const char kContentBrowserTest[];
 extern const char kDumpRenderTree[];
+extern const char kNoTimeout[];
 
 }  // namespace switches
 
