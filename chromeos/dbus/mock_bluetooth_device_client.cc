acef6d68903c4d8841fde01d3e284cc305e3ae49
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-18 03:19:06 +0000

    bluetooth: Improve testability
    Make a bunch of changes to the bluetooth code to improve its testability:
    
     - BluetoothAdapter and BluetoothDevice base observer and delegate classes
       made public, allows a test to static cast the pointer and call those
       methods as if it were the dbus client.
    
     - Add MockBluetoothAdapter::Observer and MockBluetoothDevice::Observer
       classes; these can be used by tests to check BluetoothAdapter and
       BluetoothDevice call observer methods at the correct time, with the
       correct arguments.
    
     - Add MockBluetooth*Client::Properties classes; these mock out the
       Get(), GetAll() and Set() methods so a test can verify whether or
       not they're called.
    
       Tests can also use MockBluetooth*Client::GetProperties() to return
       one of these mock classes constructed statically in the test, using
       ReplaceValue() on each property to set values.
    
    BUG=none
    TEST=can write a unit test
    
    Change-Id: I9eaeb6412de891f9b1b3cdaae66d32dfecbac8c9
    
    
    Review URL: https://chromiumcodereview.appspot.com/10831360
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152234 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/mock_bluetooth_device_client.cc
+++ chromeos/dbus/mock_bluetooth_device_client.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+MockBluetoothDeviceClient::Properties::Properties()
+    : BluetoothDeviceClient::Properties::Properties(
+        NULL, PropertyChangedCallback()) {}
+
+MockBluetoothDeviceClient::Properties::~Properties() {}
+
 MockBluetoothDeviceClient::MockBluetoothDeviceClient() {}
 
 MockBluetoothDeviceClient::~MockBluetoothDeviceClient() {}
--- chromeos/dbus/mock_bluetooth_node_client.cc
+++ chromeos/dbus/mock_bluetooth_node_client.cc
@@ -7,6 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
+MockBluetoothNodeClient::Properties::Properties()
+    : BluetoothNodeClient::Properties::Properties(
+        NULL, PropertyChangedCallback()) {}
+
+MockBluetoothNodeClient::Properties::~Properties() {}
+
 MockBluetoothNodeClient::MockBluetoothNodeClient() {}
 
 MockBluetoothNodeClient::~MockBluetoothNodeClient() {}
