e36e15c06719b5064c6e98985bc5d23890268a3e
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-18 03:11:25 +0000

    Throw exceptions when attempts are made to use localStorage in packaged apps.
    BUG=130224
    
    
    Review URL: https://chromiumcodereview.appspot.com/10832352
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/storage/test.js
+++ chrome/test/data/extensions/platform_apps/storage/test.js
@@ -3,6 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+function assertContains(string, substring, error) {
+  chrome.test.assertTrue(string.indexOf(substring) != -1, error);
+}
+
 chrome.test.runTests([
   function testOpenDatabase() {
     chrome.test.assertTrue(!window.openDatabase);
@@ -15,7 +19,15 @@ chrome.test.runTests([
   },
 
   function testLocalStorage() {
-    chrome.test.assertTrue(!window.localStorage);
-    chrome.test.succeed();
+    try {
+      window.localStorage;
+      chrome.test.fail('error not thrown');
+    } catch (e) {
+      var message = e.message || e;
+      var expected = 'is not available in packaged apps. ' +
+                     'Use chrome.storage.local instead.';
+      assertContains(message, expected, 'Unexpected message ' + message);
+      chrome.test.succeed();
+    }
   }
 ]);
