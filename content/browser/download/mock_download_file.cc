4aeb26b1e5ff0ab9b83175a1ab339ad3c3f19391
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-19 14:53:00 +0000

    Revert 152213 - Replace the DownloadFileManager with direct ownership of DownloadFile.
    This CL is a re-land of http://codereview.chromium.org/10799005, which 
    was reverted due to test flakiness.  
    
    BUG=123998
    R=benjhayden@chromium.org
    TBR=jam@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10836293
    
    BUG=143600
    TBR=rdsmith@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10861002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152282 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/mock_download_file.cc
+++ content/browser/download/mock_download_file.cc
@@ -8,19 +8,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using ::testing::_;
 using ::testing::Return;
 
-namespace {
-
-void SuccessRun(content::DownloadFile::InitializeCallback callback) {
-  callback.Run(content::DOWNLOAD_INTERRUPT_REASON_NONE);
-}
-
-}  // namespace
-
 MockDownloadFile::MockDownloadFile() {
   // This is here because |Initialize()| is normally called right after
   // construction.
-  ON_CALL(*this, Initialize(_))
-      .WillByDefault(::testing::Invoke(SuccessRun));
+  ON_CALL(*this, Initialize())
+      .WillByDefault(Return(content::DOWNLOAD_INTERRUPT_REASON_NONE));
 }
 
 MockDownloadFile::~MockDownloadFile() {
--- content/browser/download/save_package.cc
+++ content/browser/download/save_package.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/sys_string_conversions.h"
 #include "base/threading/thread.h"
 #include "base/utf_string_conversions.h"
+#include "content/browser/download/download_file_manager.h"
 #include "content/browser/download/download_item_impl.h"
 #include "content/browser/download/download_manager_impl.h"
 #include "content/browser/download/download_stats.h"
@@ -338,8 +339,7 @@ void SavePackage::OnMHTMLGenerated(const FilePath& path, int64 size) {
   // Must call OnAllDataSaved here in order for
   // GDataDownloadObserver::ShouldUpload() to return true.
   // ShouldCompleteDownload() may depend on the gdata uploader to finish.
-  download_->UpdateProgress(size, CurrentSpeed(), "");
-  download_->OnAllDataSaved(DownloadItem::kEmptyFileHash);
+  download_->OnAllDataSaved(size, DownloadItem::kEmptyFileHash);
   if (!download_manager_->GetDelegate() ||
       download_manager_->GetDelegate()->ShouldCompleteDownload(
           download_, base::Bind(&SavePackage::Finish, this))) {
@@ -742,10 +742,9 @@ void SavePackage::Finish() {
                  save_ids));
 
   if (download_) {
-    if (save_type_ != content::SAVE_PAGE_TYPE_AS_MHTML) {
-      download_->UpdateProgress(all_save_items_count_, CurrentSpeed(), "");
-      download_->OnAllDataSaved(DownloadItem::kEmptyFileHash);
-    }
+    if (save_type_ != content::SAVE_PAGE_TYPE_AS_MHTML)
+      download_->OnAllDataSaved(all_save_items_count_,
+                                DownloadItem::kEmptyFileHash);
     download_->MarkAsComplete();
     FinalizeDownloadEntry();
   }
