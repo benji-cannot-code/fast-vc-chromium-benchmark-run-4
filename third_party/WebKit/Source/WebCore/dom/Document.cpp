0f6e828d741ad34651af08f7036993ad92e5a903
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 22:24:35 +0000

    Remove redundant TOUCH_LISTENER event type https://bugs.webkit.org/show_bug.cgi?id=94524
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Code that needs to determine whether there are touch listeners
    can instead call Document::touchEventHandlerCount(), added in r107832.
    TOUCH_LISTENER didn't fit very well into the hasListenerType() model
    anyway, as there's not a 1:1 correspondance between the enum value and
    an event.
    
    * dom/Document.cpp:
    (WebCore::Document::addListenerTypeIfNeeded): Remove two bits of code:
    the bookkeeping for TOUCH_LISTENER, and the notification into
    ChromeClient (which is handled by calls to didAddTouchEventHandler in
    all the places that call addListenerTypeIfNeeded).
    (WebCore::Document::didRemoveTouchEventHandler): Remove bookkeeping for TOUCH_LISTENER.
    * dom/Document.h:
    * history/CachedFrame.cpp:
    (WebCore::CachedFrameBase::restore): Call touchEventHandlerCount instead of hasListenerType.
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::updateType): ditto
    * page/EventHandler.cpp:
    (WebCore::EventHandler::handleTouchEvent): ditto
    * page/Frame.cpp:
    (WebCore::Frame::setDocument): ditto
    * testing/Internals.cpp: Remove hasTouchEventListener method since its
    data source no longer exists.
    * testing/Internals.h: ditto
    (Internals):
    * testing/Internals.idl: ditto
    
    Source/WebKit/chromium:
    
    * src/WebPluginContainerImpl.cpp:
    (WebKit::WebPluginContainerImpl::setIsAcceptingTouchEvents): Remove
    bookkeeping for TOUCH_LISTENER.
    
    LayoutTests:
    
    Removed tests for hasTouchEventListener as they're redundant
    with tests for touchEventHandlerCount.
    
    * fast/events/touch/touch-handler-count-expected.txt:
    * fast/events/touch/touch-handler-count.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126080 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -4150,16 +4150,6 @@ void Document::addListenerTypeIfNeeded(const AtomicString& eventType)
         addListenerType(TRANSITIONEND_LISTENER);
     else if (eventType == eventNames().beforeloadEvent)
         addListenerType(BEFORELOAD_LISTENER);
-#if ENABLE(TOUCH_EVENTS)
-    else if (eventType == eventNames().touchstartEvent
-             || eventType == eventNames().touchmoveEvent
-             || eventType == eventNames().touchendEvent
-             || eventType == eventNames().touchcancelEvent) {
-        addListenerType(TOUCH_LISTENER);
-        if (Page* page = this->page())
-            page->chrome()->client()->needTouchEvents(true);
-    }
-#endif
     else if (eventType == eventNames().scrollEvent)
         addListenerType(SCROLL_LISTENER);
 }
@@ -5987,7 +5977,6 @@ void Document::didRemoveTouchEventHandler()
     if (m_touchEventHandlerCount)
         return;
 
-    m_listenerTypes &= ~TOUCH_LISTENER;
     Page* page = this->page();
     if (!page)
         return;
