b79143173e96c739f3e8c7e957570259af5b9ec4
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-20 16:59:02 +0000

    Disable TabposeWindowTest.TestModelObserver on 10.7. It crashes flakily.
    BUG=127845
    TBR=thakis@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10829430
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabpose_window_unittest.mm
+++ chrome/browser/ui/cocoa/tabpose_window_unittest.mm
@@ -64,6 +64,12 @@ TEST_F(TabposeWindowTest, TestShow) {
 }
 
 TEST_F(TabposeWindowTest, TestModelObserver) {
+  // Skip this test on 10.7
+  // http://code.google.com/p/chromium/issues/detail?id=127845
+  if (base::mac::IsOSLionOrLater()) {
+    return;
+  }
+
   NSWindow* parent = browser()->window()->GetNativeWindow();
   [parent orderFront:nil];
 
