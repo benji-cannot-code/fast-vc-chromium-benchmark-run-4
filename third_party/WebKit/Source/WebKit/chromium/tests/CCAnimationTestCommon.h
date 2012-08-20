0a62ce212e82c043c0d83c113e519e876b29da9a
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 18:21:45 +0000

    [chromium] Add tracing for active composited animations https://bugs.webkit.org/show_bug.cgi?id=84210
    Reviewed by James Robinson.
    
    This patch issues the trace events from the animations. Animations will
    report when they start and finish on the main and impl threads (via
    TRACE_EVENT_ASYNC*), and also issues instant trace events whenever they
    change state.
    
    Source/WebCore:
    
    No new tests, only changes tracing behavior.
    
    * platform/graphics/chromium/cc/CCActiveAnimation.cpp:
    (WebCore::CCActiveAnimation::CCActiveAnimation):
    (WebCore::CCActiveAnimation::~CCActiveAnimation):
    (WebCore::CCActiveAnimation::setRunState):
    (WebCore::CCActiveAnimation::clone):
    (WebCore):
    (WebCore::CCActiveAnimation::cloneAndInitialize):
    * platform/graphics/chromium/cc/CCActiveAnimation.h:
    (WebCore::CCActiveAnimation::isControllingInstance):
    (CCActiveAnimation):
    * platform/graphics/chromium/cc/CCLayerAnimationController.cpp:
    (WebCore::CCLayerAnimationController::pushNewAnimationsToImplThread):
    (WebCore::CCLayerAnimationController::replaceImplThreadAnimations):
    (WebCore::CCLayerAnimationController::tickAnimations):
    
    Source/WebKit/chromium:
    
    * src/WebAnimationImpl.cpp:
    (WebKit::WebAnimationImpl::cloneToCCAnimation):
    * tests/CCAnimationTestCommon.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126045 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCAnimationTestCommon.h
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCAnimationTestCommon.h
@@ -26,9 +26,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CCAnimationTestCommon_h
 #define CCAnimationTestCommon_h
 
+#include "CCActiveAnimation.h"
 #include "CCAnimationCurve.h"
 #include "CCLayerAnimationController.h"
 #include "IntSize.h"
+
 #include <wtf/OwnPtr.h>
 
 namespace WebCore {
