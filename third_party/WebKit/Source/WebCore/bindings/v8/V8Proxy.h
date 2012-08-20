a37bd034ab22711e6013bb5b592b700d1909b3d1
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 07:53:33 +0000

    [V8] Move contextDebugId() and setContextDebugId() from V8Proxy to ScriptController https://bugs.webkit.org/show_bug.cgi?id=94446
    Reviewed by Adam Barth.
    
    To kill V8Proxy, we can move contextDebugId() and setContextDebugId()
    from V8Proxy to ScriptController.
    
    No tests. No change in behavior.
    
    Source/WebCore:
    
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::setContextDebugId):
    (WebCore):
    (WebCore::ScriptController::contextDebugId):
    * bindings/v8/ScriptController.h:
    (ScriptController):
    * bindings/v8/V8IsolatedContext.cpp:
    (WebCore::V8IsolatedContext::V8IsolatedContext):
    * bindings/v8/V8Proxy.cpp:
    * bindings/v8/V8Proxy.h:
    (V8Proxy):
    
    Source/WebKit/chromium:
    
    * src/WebDevToolsAgentImpl.cpp:
    (WebKit::WebDevToolsAgentImpl::didCreateScriptContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.h
@@ -137,9 +137,6 @@ namespace WebCore {
         // will be moved to ScriptController.
         V8DOMWindowShell* windowShell() const;
 
-        bool setContextDebugId(int id);
-        static int contextDebugId(v8::Handle<v8::Context>);
-
         static void reportUnsafeAccessTo(Document* targetDocument);
 
         // FIXME: Move m_isolatedWorlds to ScriptController and remove this getter.
