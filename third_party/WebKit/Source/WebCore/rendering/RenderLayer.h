36db2460e5648a437422a11ff6378e85f5b4807e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 03:33:41 +0000

    Source/WebCore: parse CSS attribute -webkit-blend-mode https://bugs.webkit.org/show_bug.cgi?id=94024
    Patch by Rik Cabanier <cabanier@adobe.com> on 2012-08-19
    Reviewed by Dirk Schulze.
    
    Added parsing and general CSS handling of -webkit-blend-mode per http://www.w3.org/TR/2012/WD-compositing-20120816/
    
    Tests: css3/compositing/blend-mode-property-parsing-invalid.html
           css3/compositing/blend-mode-property-parsing.html
           css3/compositing/blend-mode-property.html
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore):
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    * css/CSSParser.cpp:
    (WebCore::isValidKeywordPropertyAndValue):
    (WebCore::isKeywordPropertyID):
    (WebCore::CSSParser::parseValue):
    * css/CSSProperty.cpp:
    (WebCore::CSSProperty::isInheritedProperty):
    * css/CSSPropertyNames.in:
    * css/CSSValueKeywords.in:
    * css/StyleBuilder.cpp:
    (WebCore::StyleBuilder::StyleBuilder):
    * rendering/RenderLayer.h:
    (RenderLayer):
    * rendering/RenderLayerBacking.cpp:
    (WebCore::RenderLayerBacking::createPrimaryGraphicsLayer):
    (WebCore):
    (WebCore::RenderLayerBacking::updateLayerBlendMode):
    (WebCore::RenderLayerBacking::updateGraphicsLayerGeometry):
    (WebCore::RenderLayerBacking::setBlendMode):
    * rendering/RenderLayerBacking.h:
    (RenderLayerBacking):
    * rendering/style/RenderStyle.cpp:
    (WebCore::RenderStyle::diff):
    * rendering/style/RenderStyle.h:
    * rendering/style/StyleRareNonInheritedData.cpp:
    (WebCore::StyleRareNonInheritedData::StyleRareNonInheritedData):
    (WebCore::StyleRareNonInheritedData::operator==):
    * rendering/style/StyleRareNonInheritedData.h:
    (StyleRareNonInheritedData):
    
    LayoutTests: parse CSS attribute -webkit-blend-mode
    https://bugs.webkit.org/show_bug.cgi?id=94024
    
    Patch by Rik Cabanier <cabanier@adobe.com> on 2012-08-19
    Reviewed by Dirk Schulze.
    
    Added parsing and general CSS handling of -webkit-blend-mode per http://www.w3.org/TR/2012/WD-compositing-20120816/
    
    * css3/compositing: Added.
    * css3/compositing/blend-mode-property-expected.txt: Added.
    * css3/compositing/blend-mode-property-parsing-expected.txt: Added.
    * css3/compositing/blend-mode-property-parsing-invalid-expected.txt: Added.
    * css3/compositing/blend-mode-property-parsing-invalid.html: Added.
    * css3/compositing/blend-mode-property-parsing.html: Added.
    * css3/compositing/blend-mode-property.html: Added.
    * css3/compositing/script-tests: Added.
    * css3/compositing/script-tests/blend-mode-property-parsing-invalid.js: Added.
    (testInvalidFilterRule):
    * css3/compositing/script-tests/blend-mode-property-parsing.js: Added.
    (jsWrapperClass):
    (shouldBeType):
    (testFilterRule):
    * css3/compositing/script-tests/blend-mode-property.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@125989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.h
@@ -398,6 +398,10 @@ public:
     bool isPaginated() const { return m_isPaginated; }
 
     void updateTransform();
+    
+#if ENABLE(CSS_COMPOSITING)
+    void updateBlendMode();
+#endif
 
     const LayoutSize& relativePositionOffset() const { return m_relativeOffset; }
 
@@ -930,6 +934,10 @@ protected:
 
 #if ENABLE(CSS_FILTERS)
     bool m_hasFilterInfo : 1;
+#endif
+
+#if ENABLE(CSS_COMPOSITING)
+    BlendMode m_blendMode;
 #endif
 
     RenderBoxModelObject* m_renderer;
--- third_party/WebKit/Source/WebCore/rendering/style/StyleRareNonInheritedData.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleRareNonInheritedData.cpp
@@ -79,6 +79,9 @@ StyleRareNonInheritedData::StyleRareNonInheritedData()
     , m_runningAcceleratedAnimation(false)
 #endif
     , m_hasAspectRatio(false)
+#if ENABLE(CSS_COMPOSITING)
+    , m_effectiveBlendMode(RenderStyle::initialBlendMode())
+#endif
 {
     m_maskBoxImage.setMaskDefaults();
 }
@@ -150,6 +153,9 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(const StyleRareNonInherited
     , m_runningAcceleratedAnimation(o.m_runningAcceleratedAnimation)
 #endif
     , m_hasAspectRatio(o.m_hasAspectRatio)
+#if ENABLE(CSS_COMPOSITING)
+    , m_effectiveBlendMode(RenderStyle::initialBlendMode())
+#endif
 {
 }
 
@@ -225,6 +231,9 @@ bool StyleRareNonInheritedData::operator==(const StyleRareNonInheritedData& o) c
         && m_wrapThrough == o.m_wrapThrough
 #if USE(ACCELERATED_COMPOSITING)
         && !m_runningAcceleratedAnimation && !o.m_runningAcceleratedAnimation
+#endif
+#if ENABLE(CSS_COMPOSITING)
+        && m_effectiveBlendMode == o.m_effectiveBlendMode
 #endif
         && m_hasAspectRatio == o.m_hasAspectRatio;
 }
