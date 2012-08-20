7f3e238a35f36ccb02ba53da835177f34922e5be
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 19:53:44 +0000

    Respect runtime flags for Device Orientation and Device Motion https://bugs.webkit.org/show_bug.cgi?id=94479
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    There are flags that allows disabling of device orientation and device
    motion at runtime. These flags determine the availability of the
    corresponding event constructors in DOMWindow.
    
    However, the flags should also control the ability to add event
    listeners for these events, otherwise the feature can be used even if
    it is disabled.
    
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::addEventListener):
    
    Source/WebKit/chromium:
    
    WebRuntimeFeatures::enableDeviceMotion should not be hard-coded to
    false.
    
    * src/WebViewImpl.cpp:
    (WebKit::WebView::create):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126057 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.cpp
@@ -120,6 +120,7 @@ TestShell::TestShell()
     , m_isDisplayingModalDialog(false)
 {
     WebRuntimeFeatures::enableDataTransferItems(true);
+    WebRuntimeFeatures::enableDeviceMotion(false);
     WebRuntimeFeatures::enableGeolocation(true);
     WebRuntimeFeatures::enablePointerLock(true);
     WebRuntimeFeatures::enableIndexedDatabase(true);
