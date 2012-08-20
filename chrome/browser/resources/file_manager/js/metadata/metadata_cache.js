087f4b6986b506c01cc9a65031151559e3113935
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-20 13:58:45 +0000

    Avoid log spam in Files app
    BUG=142779
    
    Review URL: https://chromiumcodereview.appspot.com/10827410
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152316 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/metadata/metadata_cache.js
+++ chrome/browser/resources/file_manager/js/metadata/metadata_cache.js
@@ -844,7 +844,7 @@ ContentProvider.prototype.onMessage_ = function(event) {
       'on' + data.verb.substr(0, 1).toUpperCase() + data.verb.substr(1) + '_';
 
   if (!(methodName in this)) {
-    console.log('Unknown message from metadata reader: ' + data.verb, data);
+    console.error('Unknown message from metadata reader: ' + data.verb, data);
     return;
   }
 
@@ -929,7 +929,8 @@ ContentProvider.prototype.onResult_ = function(url, metadata) {
  * @private
  */
 ContentProvider.prototype.onError_ = function(url, step, error, metadata) {
-  console.warn('metadata: ' + url + ': ' + step + ': ' + error);
+  if (localStorage.logMetadata)  // Avoid log spam by default.
+    console.warn('metadata: ' + url + ': ' + step + ': ' + error);
   metadata = metadata || {};
   // Prevent asking for thumbnail again.
   metadata.thumbnailURL = '';
@@ -942,5 +943,6 @@ ContentProvider.prototype.onError_ = function(url, step, error, metadata) {
  * @private
  */
 ContentProvider.prototype.onLog_ = function(arglist) {
-  console.log.apply(console, ['metadata:'].concat(arglist));
+  if (localStorage.logMetadata)  // Avoid log spam by default.
+    console.log.apply(console, ['metadata:'].concat(arglist));
 };
