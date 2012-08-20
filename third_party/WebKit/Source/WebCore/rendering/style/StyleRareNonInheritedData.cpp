37fdde7210bcae16121c9da4be3d502738210404
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 19:36:03 +0000

    [css3-text] Add parsing support for -webkit-text-decoration-style https://bugs.webkit.org/show_bug.cgi?id=94093
    Patch by Bruno de Oliveira Abinader <bruno.abinader@basyskom.com> on 2012-08-20
    Reviewed by Julien Chaffraix.
    
    This patch implements the "text-decoration-style" property parsing as specified
    in CSS3 working draft, with "-webkit-" prefix. The specification can be found
    here: http://dev.w3.org/csswg/css3-text/#text-decoration-style
    
    Additionally, Mozilla implementation details can be found here:
    https://developer.mozilla.org/en-US/docs/CSS/text-decoration-style
    
    This is an individual task for bug 90958. Rendering support will be handled on a
    different bug.
    
    Source/WebCore:
    
    Test: fast/css3-text-decoration/getComputedStyle/getComputedStyle-text-decoration-style.html
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::renderTextDecorationStyleFlagsToCSSValue):
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseValue):
    * css/CSSPrimitiveValueMappings.h:
    (WebCore::CSSPrimitiveValue::operator TextDecorationStyle):
    * css/CSSProperty.cpp:
    (WebCore::CSSProperty::isInheritedProperty):
    * css/CSSPropertyNames.in:
    * css/CSSValueKeywords.in:
    * css/StyleBuilder.cpp:
    (WebCore::StyleBuilder::StyleBuilder):
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::collectMatchingRulesForList):
    * rendering/style/RenderStyle.cpp:
    (WebCore::RenderStyle::diff):
    * rendering/style/RenderStyle.h:
    * rendering/style/RenderStyleConstants.h: Added non-bitwise TextDecorationStyle enum.
    * rendering/style/StyleRareNonInheritedData.cpp:
    (WebCore::StyleRareNonInheritedData::StyleRareNonInheritedData): Added m_textDecorationStyle to default and copy constructors.
    (WebCore::StyleRareNonInheritedData::operator==): Include m_textDecorationStyle in comparison.
    * rendering/style/StyleRareNonInheritedData.h:
    (StyleRareNonInheritedData):
    Added m_textDecorationStyle here as it won't be used regularly.
    
    LayoutTests:
    
    * fast/css3-text-decoration/getComputedStyle/getComputedStyle-text-decoration-style-expected.txt: Added.
    * fast/css3-text-decoration/getComputedStyle/getComputedStyle-text-decoration-style.html: Added.
    * fast/css3-text-decoration/getComputedStyle/script-tests/getComputedStyle-text-decoration-style.js: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/style/StyleRareNonInheritedData.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleRareNonInheritedData.cpp
@@ -73,6 +73,9 @@ StyleRareNonInheritedData::StyleRareNonInheritedData()
     , m_appearance(RenderStyle::initialAppearance())
     , m_borderFit(RenderStyle::initialBorderFit())
     , m_textCombine(RenderStyle::initialTextCombine())
+#if ENABLE(CSS3_TEXT_DECORATION)
+    , m_textDecorationStyle(RenderStyle::initialTextDecorationStyle())
+#endif // CSS3_TEXT_DECORATION
     , m_wrapFlow(RenderStyle::initialWrapFlow())
     , m_wrapThrough(RenderStyle::initialWrapThrough())
 #if USE(ACCELERATED_COMPOSITING)
@@ -144,6 +147,9 @@ StyleRareNonInheritedData::StyleRareNonInheritedData(const StyleRareNonInherited
     , m_appearance(o.m_appearance)
     , m_borderFit(o.m_borderFit)
     , m_textCombine(o.m_textCombine)
+#if ENABLE(CSS3_TEXT_DECORATION)
+    , m_textDecorationStyle(o.m_textDecorationStyle)
+#endif // CSS3_TEXT_DECORATION
     , m_wrapFlow(o.m_wrapFlow)
     , m_wrapThrough(o.m_wrapThrough)
 #if USE(ACCELERATED_COMPOSITING)
@@ -221,6 +227,9 @@ bool StyleRareNonInheritedData::operator==(const StyleRareNonInheritedData& o) c
         && m_appearance == o.m_appearance
         && m_borderFit == o.m_borderFit
         && m_textCombine == o.m_textCombine
+#if ENABLE(CSS3_TEXT_DECORATION)
+        && m_textDecorationStyle == o.m_textDecorationStyle
+#endif // CSS3_TEXT_DECORATION
         && m_wrapFlow == o.m_wrapFlow
         && m_wrapThrough == o.m_wrapThrough
 #if USE(ACCELERATED_COMPOSITING)
