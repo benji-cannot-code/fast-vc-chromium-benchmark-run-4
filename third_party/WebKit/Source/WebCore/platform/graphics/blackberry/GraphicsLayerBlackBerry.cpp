574da92b4bb6f0caba1e64ef5415458961a0f309
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-20 21:15:36 +0000

    [BlackBerry] Enabling DEBUG_LAYER_ANIMATION results in build break & warnings https://bugs.webkit.org/show_bug.cgi?id=94514
    Patch by Andrew Lo <anlo@rim.com> on 2012-08-20
    Reviewed by Rob Buis.
    
    Add wtf::CString definition to fix build break when enabling DEBUG_LAYER_ANIMATION
    debug prints. Fix build warnings from prints.
    
    No new tests, non-functional change.
    
    * platform/graphics/blackberry/GraphicsLayerBlackBerry.cpp:
    (WebCore::GraphicsLayerBlackBerry::addAnimation):
    (WebCore::GraphicsLayerBlackBerry::pauseAnimation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/blackberry/GraphicsLayerBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/blackberry/GraphicsLayerBlackBerry.cpp
@@ -54,6 +54,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerWebKitThread.h"
 #include "NotImplemented.h"
 
+#if DEBUG_LAYER_ANIMATION
+#include <wtf/text/CString.h>
+#endif
+
 namespace WebCore {
 
 static void setLayerBorderColor(LayerWebKitThread& layer, const Color& color)
@@ -447,7 +451,7 @@ bool GraphicsLayerBlackBerry::addAnimation(const KeyframeValueList& values, cons
     RefPtr<LayerAnimation> layerAnimation = LayerAnimation::create(values, boxSize, animation, animationName, timeOffset);
 
 #if DEBUG_LAYER_ANIMATION
-    fprintf(stderr, "LayerAnimation 0x%08x: Adding animation %s for property %d\n", layerAnimation.get(), animationName.latin1().data(), values.property());
+    fprintf(stderr, "LayerAnimation 0x%08x: Adding animation %s for property %d\n", (int)layerAnimation.get(), animationName.latin1().data(), values.property());
 #endif
 
     m_runningAnimations.append(layerAnimation);
@@ -463,7 +467,7 @@ void GraphicsLayerBlackBerry::pauseAnimation(const String& animationName, double
 
     while (RefPtr<LayerAnimation> animation = removeAnimationByName(animationName, m_runningAnimations)) {
 #if DEBUG_LAYER_ANIMATION
-        fprintf(stderr, "LayerAnimation 0x%08x: Pausing animation %s\n", animation.get(), animation->name().latin1().data());
+        fprintf(stderr, "LayerAnimation 0x%08x: Pausing animation %s\n", (int)animation.get(), animation->name().latin1().data());
 #endif
 
         // LayerAnimation is readonly. Create a new animation with the same data except for timeOffset.
@@ -476,7 +480,7 @@ void GraphicsLayerBlackBerry::pauseAnimation(const String& animationName, double
         m_suspendedAnimations.append(animation);
 
 #if DEBUG_LAYER_ANIMATION
-        fprintf(stderr, "LayerAnimation 0x%08x: Paused animation %s\n", animation.get(), animation->name().latin1().data());
+        fprintf(stderr, "LayerAnimation 0x%08x: Paused animation %s\n", (int)animation.get(), animation->name().latin1().data());
 #endif
     };
 
