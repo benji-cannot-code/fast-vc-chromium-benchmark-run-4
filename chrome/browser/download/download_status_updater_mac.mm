6fb29615e4de5edffd2e03821b30dffd41e7c18e
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 22:18:12 +0000

    Fix file download icons in the Dock in 10.8.
    Match what Safari does by not registering the final rename.
    
    BUG=143415
    TEST=as in bug
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834433
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152648 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_status_updater_mac.mm
+++ chrome/browser/download/download_status_updater_mac.mm
@@ -161,7 +161,9 @@ class CrNSProgressUserData : public base::SupportsUserData::Data {
       : target_(target) {
     progress_.reset(progress);
   }
-  virtual ~CrNSProgressUserData() {}
+  virtual ~CrNSProgressUserData() {
+    [progress_.get() unpublish];
+  }
 
   NSProgress* progress() const { return progress_.get(); }
   FilePath target() const { return target_; }
@@ -248,9 +250,6 @@ void UpdateNSProgress(content::DownloadItem* download,
 
 void DestroyNSProgress(content::DownloadItem* download,
                        CrNSProgressUserData* progress_data) {
-  NSProgress* progress = progress_data->progress();
-  [progress unpublish];
-
   download->RemoveUserData(&kCrNSProgressUserDataKey);
 }
 
@@ -273,11 +272,10 @@ void DownloadStatusUpdater::UpdateAppIconDownloadProgress(
         download->GetUserData(&kCrNSProgressUserDataKey));
     if (!progress_data)
       CreateNSProgress(download);
+    else if (download->GetState() != content::DownloadItem::IN_PROGRESS)
+      DestroyNSProgress(download, progress_data);
     else
       UpdateNSProgress(download, progress_data);
-
-    if (download->GetState() != content::DownloadItem::IN_PROGRESS)
-      DestroyNSProgress(download, progress_data);
   }
 
   // Handle downloads that ended.
