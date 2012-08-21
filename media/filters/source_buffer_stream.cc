b673643a4c77ad804b94cedf6267478cbf0c7171
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 20:27:12 +0000

    Add support for config changes during playback to FFmpegVideoDecoder. Also fixed a minor config change related bug in SourceBufferStream.
    BUG=122913
    TEST=PipelineIntegrationTest.MediaSource_MP4ConfigChange, SourceBufferStreamTest.ConfigChange_Basic, PipelineIntegrationTest.MediaSource_Seek
    
    Review URL: https://chromiumcodereview.appspot.com/10836304
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152622 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -271,8 +271,6 @@ namespace media {
 SourceBufferStream::SourceBufferStream(const AudioDecoderConfig& audio_config)
     : current_config_index_(0),
       append_config_index_(0),
-      audio_configs_(1),
-      video_configs_(0),
       stream_start_time_(kNoTimestamp()),
       seek_pending_(false),
       seek_buffer_timestamp_(kNoTimestamp()),
@@ -283,15 +281,14 @@ SourceBufferStream::SourceBufferStream(const AudioDecoderConfig& audio_config)
       last_buffer_timestamp_(kNoTimestamp()),
       max_interbuffer_distance_(kNoTimestamp()),
       memory_limit_(kDefaultAudioMemoryLimit) {
-  audio_configs_[0] = new AudioDecoderConfig();
-  audio_configs_[0]->CopyFrom(audio_config);
+  DCHECK(audio_config.IsValidConfig());
+  audio_configs_.push_back(new AudioDecoderConfig());
+  audio_configs_.back()->CopyFrom(audio_config);
 }
 
 SourceBufferStream::SourceBufferStream(const VideoDecoderConfig& video_config)
     : current_config_index_(0),
       append_config_index_(0),
-      audio_configs_(0),
-      video_configs_(1),
       stream_start_time_(kNoTimestamp()),
       seek_pending_(false),
       seek_buffer_timestamp_(kNoTimestamp()),
@@ -302,8 +299,9 @@ SourceBufferStream::SourceBufferStream(const VideoDecoderConfig& video_config)
       last_buffer_timestamp_(kNoTimestamp()),
       max_interbuffer_distance_(kNoTimestamp()),
       memory_limit_(kDefaultVideoMemoryLimit) {
-  video_configs_[0] = new VideoDecoderConfig();
-  video_configs_[0]->CopyFrom(video_config);
+  DCHECK(video_config.IsValidConfig());
+  video_configs_.push_back(new VideoDecoderConfig());
+  video_configs_.back()->CopyFrom(video_config);
 }
 
 SourceBufferStream::~SourceBufferStream() {
@@ -737,6 +735,7 @@ void SourceBufferStream::Seek(base::TimeDelta timestamp) {
   DCHECK(timestamp >= stream_start_time_);
   SetSelectedRange(NULL);
   track_buffer_.clear();
+  config_change_pending_ = false;
 
   if (ShouldSeekToStartOfBuffered(timestamp)) {
     SetSelectedRange(ranges_.front());
@@ -768,9 +767,13 @@ bool SourceBufferStream::IsSeekPending() const {
 
 SourceBufferStream::Status SourceBufferStream::GetNextBuffer(
     scoped_refptr<StreamParserBuffer>* out_buffer) {
+  CHECK(!config_change_pending_);
+
   if (!track_buffer_.empty()) {
-    if (track_buffer_.front()->GetConfigId() != current_config_index_)
+    if (track_buffer_.front()->GetConfigId() != current_config_index_) {
+      config_change_pending_ = true;
       return kConfigChange;
+    }
 
     *out_buffer = track_buffer_.front();
     track_buffer_.pop_front();
@@ -780,8 +783,10 @@ SourceBufferStream::Status SourceBufferStream::GetNextBuffer(
   if (!selected_range_ || !selected_range_->HasNextBuffer())
     return kNeedBuffer;
 
-  if (selected_range_->GetNextConfigId() != current_config_index_)
+  if (selected_range_->GetNextConfigId() != current_config_index_) {
+    config_change_pending_ = true;
     return kConfigChange;
+  }
 
   CHECK(selected_range_->GetNextBuffer(out_buffer));
   return kSuccess;
@@ -853,12 +858,14 @@ bool SourceBufferStream::IsEndSelected() const {
 }
 
 const AudioDecoderConfig& SourceBufferStream::GetCurrentAudioDecoderConfig() {
-  CompleteConfigChange();
+  if (config_change_pending_)
+    CompleteConfigChange();
   return *audio_configs_[current_config_index_];
 }
 
 const VideoDecoderConfig& SourceBufferStream::GetCurrentVideoDecoderConfig() {
-  CompleteConfigChange();
+  if (config_change_pending_)
+    CompleteConfigChange();
   return *video_configs_[current_config_index_];
 }
 
@@ -934,15 +941,15 @@ bool SourceBufferStream::UpdateVideoConfig(const VideoDecoderConfig& config) {
 }
 
 void SourceBufferStream::CompleteConfigChange() {
+  config_change_pending_ = false;
+
   if (!track_buffer_.empty()) {
     current_config_index_ = track_buffer_.front()->GetConfigId();
     return;
   }
 
-  if (!selected_range_ || !selected_range_->HasNextBuffer())
-    return;
-
-  current_config_index_ = selected_range_->GetNextConfigId();
+  if (selected_range_ && selected_range_->HasNextBuffer())
+    current_config_index_ = selected_range_->GetNextConfigId();
 }
 
 SourceBufferRange::SourceBufferRange(
