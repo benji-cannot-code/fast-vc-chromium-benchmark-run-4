d0ccffaf120b5f73cbaa7ff9ecf6d19a6d1574f5
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 14:21:44 +0000

    [content shell] disable the auth prompt during layout tests
    BUG=111316
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/10860066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152556 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_resource_dispatcher_host_delegate.cc
+++ content/shell/shell_resource_dispatcher_host_delegate.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/shell_resource_dispatcher_host_delegate.h"
 
+#include "base/command_line.h"
 #include "content/shell/shell_login_dialog.h"
+#include "content/shell/shell_switches.h"
 
 namespace content {
 
@@ -18,8 +20,9 @@ ShellResourceDispatcherHostDelegate::~ShellResourceDispatcherHostDelegate() {
 bool ShellResourceDispatcherHostDelegate::AcceptAuthRequest(
     net::URLRequest* request,
     net::AuthChallengeInfo* auth_info) {
-  // Why not give it a try?
-  return true;
+  bool accept_auth_request =
+      !CommandLine::ForCurrentProcess()->HasSwitch(switches::kDumpRenderTree);
+  return accept_auth_request;
 }
 
 ResourceDispatcherHostLoginDelegate*
