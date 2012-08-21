c45da040aaedaabdf842bb885fd0a85a4d7ddb56
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-21 00:45:17 +0000

    Never notify of insertedIntoTree during document destruction. https://bugs.webkit.org/show_bug.cgi?id=94535
    Patch by Elliott Sprehn <esprehn@chromium.org> on 2012-08-20
    Reviewed by Eric Seidel.
    
    Never notify of insertedIntoTree during document destruction. Previously since we
    avoid notifying of willBeRemovedFromTree it's possible we could have gotten several
    insertedIntoTree notifications without ever being told we were removed.
    
    No tests needed since this just closes holes related to future code.
    
    * rendering/RenderObjectChildList.cpp:
    (WebCore::RenderObjectChildList::appendChildNode): Never call insertedIntoTree during document destruction.
    (WebCore::RenderObjectChildList::insertChildNode): Same.
    * rendering/RenderRegion.cpp:
    (WebCore::RenderRegion::attachRegion): Removed unneeded document destruction check.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126107 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObjectChildList.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObjectChildList.cpp
@@ -139,7 +139,7 @@ void RenderObjectChildList::appendChildNode(RenderObject* owner, RenderObject* n
 
     setLastChild(newChild);
     
-    if (notifyRenderer)
+    if (!owner->documentBeingDestroyed() && notifyRenderer)
         newChild->insertedIntoTree();
 
     if (!owner->documentBeingDestroyed()) {
@@ -179,7 +179,7 @@ void RenderObjectChildList::insertChildNode(RenderObject* owner, RenderObject* c
 
     child->setParent(owner);
     
-    if (notifyRenderer)
+    if (!owner->documentBeingDestroyed() && notifyRenderer)
         child->insertedIntoTree();
 
     if (!owner->documentBeingDestroyed()) {
