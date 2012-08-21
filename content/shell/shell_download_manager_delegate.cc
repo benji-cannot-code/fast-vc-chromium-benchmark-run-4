56e089011e9aa359f0fd3b47f7091e6691a0aba6
shouqun.liu@intel.com <shouqun.liu@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 23:41:40 +0000

    Fix the DownloadManager not initialized error in content shell.
    - Fix DownloadManager is not set for ShellDownloadManagerDelegate.
    - Also add the save file chooser for file download [GTK].
    
    BUG=
    TEST=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825478
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152668 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_download_manager_delegate.cc
+++ content/shell/shell_download_manager_delegate.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/shell_download_manager_delegate.h"
 
+#if defined(TOOLKIT_GTK)
+#include <gtk/gtk.h>
+#endif
+
 #if defined(OS_WIN)
 #include <windows.h>
 #include <commdlg.h>
@@ -19,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/download_manager.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_view.h"
 #include "net/base/net_util.h"
 
 namespace content {
@@ -119,6 +124,29 @@ void ShellDownloadManagerDelegate::ChooseDownloadPath(
 
   if (GetSaveFileName(&save_as))
     result = FilePath(std::wstring(save_as.lpstrFile));
+#elif defined(TOOLKIT_GTK)
+  GtkWidget *dialog;
+  gfx::NativeWindow parent_window;
+  std::string base_name = FilePath(suggested_path).BaseName().value();
+
+  parent_window = item->GetWebContents()->GetView()->GetTopLevelNativeWindow();
+  dialog = gtk_file_chooser_dialog_new("Save File",
+                                       parent_window,
+                                       GTK_FILE_CHOOSER_ACTION_SAVE,
+                                       GTK_STOCK_CANCEL, GTK_RESPONSE_CANCEL,
+                                       GTK_STOCK_SAVE, GTK_RESPONSE_ACCEPT,
+                                       NULL);
+  gtk_file_chooser_set_do_overwrite_confirmation(GTK_FILE_CHOOSER(dialog),
+                                                 TRUE);
+  gtk_file_chooser_set_current_name(GTK_FILE_CHOOSER(dialog),
+                                    base_name.c_str());
+
+  if (gtk_dialog_run(GTK_DIALOG(dialog)) == GTK_RESPONSE_ACCEPT) {
+    char *filename;
+    filename = gtk_file_chooser_get_filename(GTK_FILE_CHOOSER(dialog));
+    result = FilePath(filename);
+  }
+  gtk_widget_destroy(dialog);
 #else
   NOTIMPLEMENTED();
 #endif
