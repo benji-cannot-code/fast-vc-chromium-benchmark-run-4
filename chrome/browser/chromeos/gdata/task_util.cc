e5835c9d84a65ba23cc17f7ba23e9e49f756af76
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 00:02:11 +0000

    gdata: Move task related functions like RunTaskOnUIThread from gdata_file_system.cc to task_util.cc
    BUG=143564
    TEST=unit_tests --gtest_filter="GData*"
    
    TBR=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10825459
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152438 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/gdata/task_util.cc
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/gdata/task_util.h"
+
+#include "base/location.h"
+#include "content/public/browser/browser_thread.h"
+
+using content::BrowserThread;
+
+namespace gdata {
+
+void RunTaskOnThread(scoped_refptr<base::MessageLoopProxy> relay_proxy,
+                     const base::Closure& task) {
+  if (relay_proxy->BelongsToCurrentThread()) {
+    task.Run();
+  } else {
+    const bool posted = relay_proxy->PostTask(FROM_HERE, task);
+    DCHECK(posted);
+  }
+}
+
+void RunTaskOnUIThread(const base::Closure& task) {
+  RunTaskOnThread(
+      BrowserThread::GetMessageLoopProxyForThread(BrowserThread::UI), task);
+}
+
+}  // namespace gdata
