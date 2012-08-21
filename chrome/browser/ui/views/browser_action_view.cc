5e7a01cc5c5ecc73c120bcc966bb6ad90729700a
yefim@chromium.org <yefim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 04:43:08 +0000

    Fixed a couple places that may crash browser.
    BUG=143455
    
    Review URL: https://chromiumcodereview.appspot.com/10823422
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152507 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/browser_action_view.cc
+++ chrome/browser/ui/views/browser_action_view.cc
@@ -47,6 +47,11 @@ BrowserActionView::BrowserActionView(const Extension* extension,
       delegate_(delegate),
       button_(NULL),
       extension_(extension) {
+  button_ = new BrowserActionButton(extension_, browser_, delegate_);
+  button_->set_drag_controller(delegate_);
+  button_->set_owned_by_client();
+  AddChildView(button_);
+  button_->UpdateState();
 }
 
 BrowserActionView::~BrowserActionView() {
@@ -65,10 +70,6 @@ gfx::ImageSkia BrowserActionView::GetIconWithBadge() {
 }
 
 void BrowserActionView::Layout() {
-  // |button_| is NULL if Layout() is invoked before we have a valid widget.
-  if (!button_)
-    return;
-
   // We can't rely on button_->GetPreferredSize() here because that's not set
   // correctly until the first call to
   // BrowserActionsContainer::RefreshBrowserActionViews(), whereas this can be
@@ -79,18 +80,6 @@ void BrowserActionView::Layout() {
   gfx::Point offset = delegate_->GetViewContentOffset();
   button_->SetBounds(offset.x(), offset.y(), width() - offset.x(),
                      BrowserActionsContainer::IconHeight());
-}
-
-void BrowserActionView::ViewHierarchyChanged(bool is_add,
-                                             View* parent,
-                                             View* child) {
-  if (is_add && (child == this) && (GetWidget() != NULL) && (button_ == NULL)) {
-    button_ = new BrowserActionButton(extension_, browser_, delegate_);
-    button_->set_drag_controller(delegate_);
-    button_->set_owned_by_client();
-    AddChildView(button_);
-    button_->UpdateState();
-  }
 }
 
 void BrowserActionView::GetAccessibleState(ui::AccessibleViewState* state) {
@@ -125,7 +114,8 @@ BrowserActionButton::BrowserActionButton(const Extension* extension,
       extension_(extension),
       ALLOW_THIS_IN_INITIALIZER_LIST(tracker_(this)),
       delegate_(delegate),
-      context_menu_(NULL) {
+      context_menu_(NULL),
+      called_registered_extension_command_(false) {
   set_border(NULL);
   set_alignment(TextButton::ALIGN_CENTER);
   set_context_menu_controller(this);
@@ -170,8 +160,11 @@ void BrowserActionButton::ViewHierarchyChanged(
                                    Extension::kBrowserActionIconMaxSize),
                          ImageLoadingTracker::DONT_CACHE);
     }
+  }
 
+  if (is_add && !called_registered_extension_command_ && GetFocusManager()) {
     MaybeRegisterExtensionCommand();
+    called_registered_extension_command_ = true;
   }
 
   MenuButton::ViewHierarchyChanged(is_add, parent, child);
