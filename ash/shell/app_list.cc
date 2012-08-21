32918e11600035598209f55487d200e49e06e00f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 01:08:31 +0000

    views/examples: Switch to use examples_window_with_content.*
    In a follow up CL we will be able to clean up examples_window.* and get rid
    the dependency on content/ package from it.
    
    BUG=141187
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10857048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152462 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/app_list.cc
+++ ash/shell/app_list.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/font.h"
 #include "ui/gfx/rect.h"
-#include "ui/views/examples/examples_window.h"
+#include "ui/views/examples/examples_window_with_content.h"
 
 namespace ash {
 namespace shell {
--- ash/shell/window_type_launcher.cc
+++ ash/shell/window_type_launcher.cc
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/button/text_button.h"
 #include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/menu_runner.h"
-#include "ui/views/examples/examples_window.h"
+#include "ui/views/examples/examples_window_with_content.h"
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
 
