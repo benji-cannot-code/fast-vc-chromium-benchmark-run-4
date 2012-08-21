1bc0da54e929f91511ff415289d01ba02c8b7256
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 04:01:33 +0000

    Revert 152495 - Add SetScale to PPB_WidgetDev, use scale for painting
    In order to allow for HiDPI-aware scrollbars, this patch allows the plugin to
    specify a scale factor used during widget paint operations.
    
    The widgets (of which Scrollbar is the only current instance) will continue to
    use the DIP coordinate system for events and sizing, which is consistent with
    their WebKit implementation.
    
    This change should maintain backward compat. with existing users of interface
    'PPB_Wdiget(Dev);0.3'
    
    BUG=140833
    TEST=Run pre-built (with 0.3 interfaces) PDF plugin and verify scrollbars
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824386
    
    TBR=jhorwich@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10861026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152499 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/ppb_widget_dev.h
+++ ppapi/c/dev/ppb_widget_dev.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From dev/ppb_widget_dev.idl modified Mon Aug 20 10:21:06 2012. */
+/* From dev/ppb_widget_dev.idl modified Wed Oct  5 14:06:02 2011. */
 
 #ifndef PPAPI_C_DEV_PPB_WIDGET_DEV_H_
 #define PPAPI_C_DEV_PPB_WIDGET_DEV_H_
@@ -18,8 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_stdint.h"
 
 #define PPB_WIDGET_DEV_INTERFACE_0_3 "PPB_Widget(Dev);0.3"
-#define PPB_WIDGET_DEV_INTERFACE_0_4 "PPB_Widget(Dev);0.4"
-#define PPB_WIDGET_DEV_INTERFACE PPB_WIDGET_DEV_INTERFACE_0_4
+#define PPB_WIDGET_DEV_INTERFACE PPB_WIDGET_DEV_INTERFACE_0_3
 
 /**
  * @file
@@ -34,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * The interface for reusing browser widgets.
  */
-struct PPB_Widget_Dev_0_4 {
+struct PPB_Widget_Dev_0_3 {
   /**
    * Returns PP_TRUE if the given resource is a Widget. Returns PP_FALSE if the
    * resource is invalid or some type other than an Widget.
@@ -60,23 +59,9 @@ struct PPB_Widget_Dev_0_4 {
    * Set the location of the widget.
    */
   void (*SetLocation)(PP_Resource widget, const struct PP_Rect* location);
-  /**
-   * Set scale used during paint operations.
-   */
-  void (*SetScale)(PP_Resource widget, float scale);
 };
 
-typedef struct PPB_Widget_Dev_0_4 PPB_Widget_Dev;
-
-struct PPB_Widget_Dev_0_3 {
-  PP_Bool (*IsWidget)(PP_Resource resource);
-  PP_Bool (*Paint)(PP_Resource widget,
-                   const struct PP_Rect* rect,
-                   PP_Resource image);
-  PP_Bool (*HandleEvent)(PP_Resource widget, PP_Resource input_event);
-  PP_Bool (*GetLocation)(PP_Resource widget, struct PP_Rect* location);
-  void (*SetLocation)(PP_Resource widget, const struct PP_Rect* location);
-};
+typedef struct PPB_Widget_Dev_0_3 PPB_Widget_Dev;
 /**
  * @}
  */
--- ppapi/cpp/dev/widget_dev.h
+++ ppapi/cpp/dev/widget_dev.h
@@ -30,7 +30,6 @@ class Widget_Dev : public Resource {
   bool HandleEvent(const InputEvent& event);
   bool GetLocation(Rect* location);
   void SetLocation(const Rect& location);
-  void SetScale(float scale);
 };
 
 }  // namespace pp
