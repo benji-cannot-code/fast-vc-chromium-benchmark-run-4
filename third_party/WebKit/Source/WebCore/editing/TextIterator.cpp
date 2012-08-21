94511905d8551ee8fccdf786d5c10592e3706aa5
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-21 16:43:56 +0000

    <rdar://problem/12104508> TextIterator takes O(n^2) to iterate over n empty blocks https://bugs.webkit.org/show_bug.cgi?id=94429
    Reviewed by Sam Weinig.
    
    No new tests, because behavior is unchanged.
    
    * editing/TextIterator.cpp:
    (WebCore::TextIterator::shouldRepresentNodeOffsetZero): Enhanced the check for nodes that
    cannot contain VisiblePosition to also check for zero-height blocks.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126164 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/TextIterator.cpp
+++ third_party/WebKit/Source/WebCore/editing/TextIterator.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2012 Apple Inc. All rights reserved.
  * Copyright (C) 2005 Alexey Proskuryakov.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -894,9 +894,10 @@ bool TextIterator::shouldRepresentNodeOffsetZero()
     // If this node is unrendered or invisible the VisiblePosition checks below won't have much meaning.
     // Additionally, if the range we are iterating over contains huge sections of unrendered content, 
     // we would create VisiblePositions on every call to this function without this check.
-    if (!m_node->renderer() || m_node->renderer()->style()->visibility() != VISIBLE)
+    if (!m_node->renderer() || m_node->renderer()->style()->visibility() != VISIBLE
+        || (m_node->renderer()->isBlockFlow() && !toRenderBlock(m_node->renderer())->height() && !m_node->hasTagName(bodyTag)))
         return false;
-    
+
     // The startPos.isNotNull() check is needed because the start could be before the body,
     // and in that case we'll get null. We don't want to put in newlines at the start in that case.
     // The currPos.isNotNull() check is needed because positions in non-HTML content
