ab76ed3b8a739ab963050aea1ea921c5854d5f20
marcheu@chromium.org <marcheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 04:45:04 +0000

    VAVDA: Properly size the picture vector before using it
    Otherwise this causes memory corruption, which eventually makes the GPU
    process crash, usually inside tcmalloc functions.
    
    BUG=chromium:142452
    TEST=by hand, attach gdb to the gpu process, navigate to
    TEST=http://www.apple.com/html5/showcase/video/ and press reload multiple
    TEST=times
    
    Change-Id: Ia6040e73eed7307c79f06bfff1514eef8bb4f2ed
    
    
    Review URL: https://chromiumcodereview.appspot.com/10834411
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152509 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/media/vaapi_h264_decoder.cc
+++ content/common/gpu/media/vaapi_h264_decoder.cc
@@ -1356,15 +1356,20 @@ int VaapiH264Decoder::LongTermPicNumF(H264Picture *pic) {
 
 // Shift elements on the |v| starting from |from| to |to|, inclusive,
 // one position to the right and insert pic at |from|.
-static void ShiftRightAndInsert(H264Picture::PtrVector& v,
+static void ShiftRightAndInsert(H264Picture::PtrVector *v,
                                 int from,
                                 int to,
                                 H264Picture* pic) {
   DCHECK(pic);
-  for (int i = to + 1; i > from; --i)
-    v[i] = v[i - 1];
+  DCHECK((to + 1 == static_cast<int>(v->size())) ||
+         (to + 2 == static_cast<int>(v->size())));
 
-  v[from] = pic;
+  v->resize(to + 2);
+
+  for (int i = to + 1; i > from; --i)
+    (*v)[i] = (*v)[i - 1];
+
+  (*v)[from] = pic;
 }
 
 bool VaapiH264Decoder::ModifyReferencePicList(H264SliceHeader *slice_hdr,
@@ -1440,7 +1445,7 @@ bool VaapiH264Decoder::ModifyReferencePicList(H264SliceHeader *slice_hdr,
           DVLOG(1) << "Malformed stream, no pic num " << pic_num_lx;
           return false;
         }
-        ShiftRightAndInsert(*ref_pic_listx, ref_idx_lx,
+        ShiftRightAndInsert(ref_pic_listx, ref_idx_lx,
                             num_ref_idx_lX_active_minus1, pic);
         ref_idx_lx++;
 
@@ -1460,7 +1465,7 @@ bool VaapiH264Decoder::ModifyReferencePicList(H264SliceHeader *slice_hdr,
           DVLOG(1) << "Malformed stream, no pic num " << pic_num_lx;
           return false;
         }
-        ShiftRightAndInsert(*ref_pic_listx, ref_idx_lx,
+        ShiftRightAndInsert(ref_pic_listx, ref_idx_lx,
                             num_ref_idx_lX_active_minus1, pic);
         ref_idx_lx++;
 
