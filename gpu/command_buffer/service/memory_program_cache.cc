2e027b86672b1cef6a25c8feed350b2e6173aa85
dmurph@chromium.org <dmurph@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 01:55:17 +0000

    Fixed in-memory gpu program cache size flag interpretation
    Review URL: https://chromiumcodereview.appspot.com/10860055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152474 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/memory_program_cache.cc
+++ gpu/command_buffer/service/memory_program_cache.cc
@@ -14,14 +14,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_bindings.h"
 
 namespace {
-size_t GetCacheSize() {
+size_t GetCacheSizeBytes() {
   size_t size;
   const CommandLine* command_line = CommandLine::ForCurrentProcess();
   if (command_line->HasSwitch(switches::kGpuProgramCacheSizeKb) &&
       base::StringToSizeT(command_line->GetSwitchValueNative(
           switches::kGpuProgramCacheSizeKb),
           &size)) {
-      return size;
+      return size * 1024;
   }
   return gpu::gles2::MemoryProgramCache::kDefaultMaxProgramCacheMemoryBytes;
 }
@@ -31,7 +31,7 @@ namespace gpu {
 namespace gles2 {
 
 MemoryProgramCache::MemoryProgramCache()
-    : max_size_bytes_(GetCacheSize()),
+    : max_size_bytes_(GetCacheSizeBytes()),
       curr_size_bytes_(0) { }
 
 MemoryProgramCache::MemoryProgramCache(const size_t max_cache_size_bytes)
