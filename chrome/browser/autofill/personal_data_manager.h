f6d068961ba4293cea34347b3b6ddebc8b0f3c36
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 15:19:20 +0000

    Convert the autofill pyauto tests to browser tests, and remove all the supporting automation hooks that are no longer necessary.
    Tests I left:
    -AutofillCrowdsourcing: this is a manual, non-automated test against the autofill server. I've been told it's not used, but I left it anyways. It was composed of two parts: testing chrome autofill works and sending the resulting form to the server. The former is tested in many tests already, so I took that out.
    -autofill edit address dialog tests: these are using webdriver.
    
    Also remove the autofill tests in sync.py since they used the same automation hooks. testAutofillProfileSync is redundant because many of the tests in two_client_autofill_sync_test.cc do this. For testNoCreditCardSync, I added a test case in two_client_autofill_sync_test.cc.
    
    BUG=143637
    Review URL: https://chromiumcodereview.appspot.com/10855253
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152560 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/personal_data_manager.h
+++ chrome/browser/autofill/personal_data_manager.h
@@ -30,6 +30,7 @@ class Profile;
 
 namespace autofill_helper {
 void SetProfiles(int, std::vector<AutofillProfile>*);
+void SetCreditCards(int, std::vector<CreditCard>*);
 }
 
 // Handles loading and saving Autofill profile information to the web database.
@@ -149,12 +150,14 @@ class PersonalDataManager
   FRIEND_TEST_ALL_PREFIXES(AutofillMetricsTest, AutofillIsEnabledAtStartup);
   FRIEND_TEST_ALL_PREFIXES(PersonalDataManagerTest,
                            AggregateExistingAuxiliaryProfile);
+  friend class AutofillTest;
   friend class PersonalDataManagerFactory;
   friend class PersonalDataManagerTest;
   friend class scoped_ptr<PersonalDataManager>;
   friend class ProfileSyncServiceAutofillTest;
   friend class TestingAutomationProvider;
   friend void autofill_helper::SetProfiles(int, std::vector<AutofillProfile>*);
+  friend void autofill_helper::SetCreditCards(int, std::vector<CreditCard>*);
 
   PersonalDataManager();
   virtual ~PersonalDataManager();
