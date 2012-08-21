e534796d62990c224c19d423979a99957f69bf88
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-21 23:00:12 +0000

    [chromium] Add software bitmap resources to CCResourceProvider https://bugs.webkit.org/show_bug.cgi?id=93677
    Patch by Alexandre Elias <aelias@google.com> on 2012-08-21
    Reviewed by Adrienne Walker.
    
    This adds the ability to CCResourceProvider to use software bitmaps.
    They are allocated as plain-old-memory, and exposed as Skia objects.
    
    We want the ResourceProvider to be able to handle different resource
    types at the same time.  In practice, a default resource type is
    desired for most uses within a single compositor instance, which is
    specified by the default resource type.  Default resource types are
    expected to be mostly 1-to-1 with CCRenderer types.
    
    New tests added by parametrizing existing CCResourceProvider tests.
    
    Source/WebCore:
    
    * platform/graphics/chromium/FrameBufferSkPictureCanvasLayerTextureUpdater.cpp:
    (WebCore::FrameBufferSkPictureCanvasLayerTextureUpdater::updateTextureRect):
    * platform/graphics/chromium/LayerRendererChromium.cpp:
    (WebCore::applyFilters):
    (WebCore::LayerRendererChromium::drawRenderPassQuad):
    (WebCore::LayerRendererChromium::drawTileQuad):
    (WebCore::LayerRendererChromium::drawYUVVideoQuad):
    (WebCore::LayerRendererChromium::drawTextureQuad):
    (WebCore::LayerRendererChromium::getFramebufferTexture):
    (WebCore::LayerRendererChromium::bindFramebufferToTexture):
    * platform/graphics/chromium/LayerRendererChromium.h:
    (DrawingFrame):
    * platform/graphics/chromium/cc/CCResourceProvider.cpp:
    (WebCore::CCResourceProvider::createResource):
    (WebCore):
    (WebCore::CCResourceProvider::createGLTexture):
    (WebCore::CCResourceProvider::createBitmap):
    (WebCore::CCResourceProvider::createResourceFromExternalTexture):
    (WebCore::CCResourceProvider::deleteResource):
    (WebCore::CCResourceProvider::upload):
    (WebCore::CCResourceProvider::flush):
    (WebCore::CCResourceProvider::shallowFlushIfSupported):
    (WebCore::CCResourceProvider::lockForRead):
    (WebCore::CCResourceProvider::unlockForRead):
    (WebCore::CCResourceProvider::lockForWrite):
    (WebCore::CCResourceProvider::unlockForWrite):
    (WebCore::CCResourceProvider::ScopedReadLockGL::ScopedReadLockGL):
    (WebCore::CCResourceProvider::ScopedReadLockGL::~ScopedReadLockGL):
    (WebCore::CCResourceProvider::ScopedWriteLockGL::ScopedWriteLockGL):
    (WebCore::CCResourceProvider::ScopedWriteLockGL::~ScopedWriteLockGL):
    (WebCore::CCResourceProvider::populateSkBitmapWithResource):
    (WebCore::CCResourceProvider::ScopedReadLockSoftware::ScopedReadLockSoftware):
    (WebCore::CCResourceProvider::ScopedReadLockSoftware::~ScopedReadLockSoftware):
    (WebCore::CCResourceProvider::ScopedWriteLockSoftware::ScopedWriteLockSoftware):
    (WebCore::CCResourceProvider::ScopedWriteLockSoftware::~ScopedWriteLockSoftware):
    (WebCore::CCResourceProvider::CCResourceProvider):
    * platform/graphics/chromium/cc/CCResourceProvider.h:
    (WebCore):
    (WebCore::CCResourceProvider::setCreationPolicy):
    (WebCore::CCResourceProvider::creationPolicy):
    (CCResourceProvider):
    (ScopedReadLockGL):
    (WebCore::CCResourceProvider::ScopedReadLockGL::textureId):
    (ScopedWriteLockGL):
    (WebCore::CCResourceProvider::ScopedWriteLockGL::textureId):
    (ScopedReadLockSoftware):
    (WebCore::CCResourceProvider::ScopedReadLockSoftware::skBitmap):
    (ScopedWriteLockSoftware):
    (WebCore::CCResourceProvider::ScopedWriteLockSoftware::skCanvas):
    (Resource):
    
    Source/WebKit/chromium:
    
    * tests/CCResourceProviderTest.cpp:
    (WebKit::CCResourceProviderTest::getResourcePixels):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCResourceProvider.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCResourceProvider.h
@@ -31,6 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CCGraphicsContext.h"
 #include "GraphicsContext3D.h"
 #include "IntSize.h"
+#include "SkBitmap.h"
+#include "SkCanvas.h"
 #include <wtf/Deque.h>
 #include <wtf/HashMap.h>
 #include <wtf/OwnPtr.h>
@@ -45,8 +47,6 @@ class WebGraphicsContext3D;
 
 namespace WebCore {
 
-class CCScopedLockResourceForRead;
-class CCScopedLockResourceForWrite;
 class IntRect;
 class LayerTextureSubImage;
 
@@ -59,6 +59,10 @@ public:
     typedef Vector<ResourceId> ResourceIdArray;
     typedef HashMap<ResourceId, ResourceId> ResourceIdMap;
     enum TextureUsageHint { TextureUsageAny, TextureUsageFramebuffer };
+    enum ResourceType {
+        GLTexture = 1,
+        Bitmap,
+    };
     struct Mailbox {
         GC3Dbyte name[64];
     };
@@ -88,11 +92,19 @@ public:
 
     // Producer interface.
 
-    // Creates a resource of the given size/format, into the given pool.
+    void setDefaultResourceType(ResourceType type) { m_defaultResourceType = type; }
+    ResourceType defaultResourceType() const { return m_defaultResourceType; }
+    ResourceType resourceType(ResourceId);
+
+    // Creates a resource of the default resource type.
     ResourceId createResource(int pool, const IntSize&, GC3Denum format, TextureUsageHint);
 
-    // Wraps an external texture into a resource.
+    // You can also explicitly create a specific resource type.
+    ResourceId createGLTexture(int pool, const IntSize&, GC3Denum format, TextureUsageHint);
+    ResourceId createBitmap(int pool, const IntSize&);
+    // Wraps an external texture into a GL resource.
     ResourceId createResourceFromExternalTexture(unsigned textureId);
+
     void deleteResource(ResourceId);
 
     // Deletes all resources owned by a given pool.
@@ -148,13 +160,72 @@ public:
     // Only for testing
     size_t mailboxCount() const { return m_mailboxes.size(); }
 
-private:
-    friend class CCScopedLockResourceForRead;
-    friend class CCScopedLockResourceForWrite;
+    // The following lock classes are part of the CCResourceProvider API and are
+    // needed to read and write the resource contents. The user must ensure
+    // that they only use GL locks on GL resources, etc, and this is enforced
+    // by assertions.
+    class ScopedReadLockGL {
+        WTF_MAKE_NONCOPYABLE(ScopedReadLockGL);
+    public:
+        ScopedReadLockGL(CCResourceProvider*, CCResourceProvider::ResourceId);
+        ~ScopedReadLockGL();
 
+        unsigned textureId() const { return m_textureId; }
+
+    private:
+        CCResourceProvider* m_resourceProvider;
+        CCResourceProvider::ResourceId m_resourceId;
+        unsigned m_textureId;
+    };
+
+    class ScopedWriteLockGL {
+        WTF_MAKE_NONCOPYABLE(ScopedWriteLockGL);
+    public:
+        ScopedWriteLockGL(CCResourceProvider*, CCResourceProvider::ResourceId);
+        ~ScopedWriteLockGL();
+
+        unsigned textureId() const { return m_textureId; }
+
+    private:
+        CCResourceProvider* m_resourceProvider;
+        CCResourceProvider::ResourceId m_resourceId;
+        unsigned m_textureId;
+    };
+
+    class ScopedReadLockSoftware {
+        WTF_MAKE_NONCOPYABLE(ScopedReadLockSoftware);
+    public:
+        ScopedReadLockSoftware(CCResourceProvider*, CCResourceProvider::ResourceId);
+        ~ScopedReadLockSoftware();
+
+        const SkBitmap* skBitmap() const { return &m_skBitmap; }
+
+    private:
+        CCResourceProvider* m_resourceProvider;
+        CCResourceProvider::ResourceId m_resourceId;
+        SkBitmap m_skBitmap;
+    };
+
+    class ScopedWriteLockSoftware {
+        WTF_MAKE_NONCOPYABLE(ScopedWriteLockSoftware);
+    public:
+        ScopedWriteLockSoftware(CCResourceProvider*, CCResourceProvider::ResourceId);
+        ~ScopedWriteLockSoftware();
+
+        SkCanvas* skCanvas() { return &m_skCanvas; }
+
+    private:
+        CCResourceProvider* m_resourceProvider;
+        CCResourceProvider::ResourceId m_resourceId;
+        SkBitmap m_skBitmap;
+        SkCanvas m_skCanvas;
+    };
+
+private:
     struct Resource {
         Resource()
             : glId(0)
+            , pixels(0)
             , pool(0)
             , lockForReadCount(0)
             , lockedForWrite(false)
@@ -162,9 +233,11 @@ private:
             , exported(false)
             , size()
             , format(0)
+            , type(static_cast<ResourceType>(0))
         { }
         Resource(unsigned textureId, int pool, const IntSize& size, GC3Denum format)
             : glId(textureId)
+            , pixels(0)
             , pool(pool)
             , lockForReadCount(0)
             , lockedForWrite(false)
@@ -172,8 +245,22 @@ private:
             , exported(false)
             , size(size)
             , format(format)
+            , type(GLTexture)
+        { }
+        Resource(uint8_t* pixels, int pool, const IntSize& size, GC3Denum format)
+            : glId(0)
+            , pixels(pixels)
+            , pool(pool)
+            , lockForReadCount(0)
+            , lockedForWrite(false)
+            , external(false)
+            , exported(false)
+            , size(size)
+            , format(format)
+            , type(Bitmap)
         { }
         unsigned glId;
+        uint8_t* pixels;
         int pool;
         int lockForReadCount;
         bool lockedForWrite;
@@ -181,6 +268,7 @@ private:
         bool exported;
         IntSize size;
         GC3Denum format;
+        ResourceType type;
     };
     typedef HashMap<ResourceId, Resource> ResourceMap;
     struct Child {
@@ -193,13 +281,11 @@ private:
     explicit CCResourceProvider(CCGraphicsContext*);
     bool initialize();
 
-    // Gets a GL texture id representing the resource, that can be rendered into.
-    unsigned lockForWrite(ResourceId);
-    void unlockForWrite(ResourceId);
-
-    // Gets a GL texture id representing the resource, that can be rendered with.
-    unsigned lockForRead(ResourceId);
+    const Resource* lockForRead(ResourceId);
     void unlockForRead(ResourceId);
+    const Resource* lockForWrite(ResourceId);
+    void unlockForWrite(ResourceId);
+    static void populateSkBitmapWithResource(SkBitmap*, const Resource*);
 
     bool transferResource(WebKit::WebGraphicsContext3D*, ResourceId, TransferableResource*);
     void trimMailboxDeque();
@@ -212,56 +298,12 @@ private:
 
     Deque<Mailbox> m_mailboxes;
 
+    ResourceType m_defaultResourceType;
     bool m_useTextureStorageExt;
     bool m_useTextureUsageHint;
     bool m_useShallowFlush;
     OwnPtr<LayerTextureSubImage> m_texSubImage;
     int m_maxTextureSize;
-};
-
-class CCScopedLockResourceForRead {
-    WTF_MAKE_NONCOPYABLE(CCScopedLockResourceForRead);
-public:
-    CCScopedLockResourceForRead(CCResourceProvider* resourceProvider, CCResourceProvider::ResourceId resourceId)
-        : m_resourceProvider(resourceProvider)
-        , m_resourceId(resourceId)
-        , m_textureId(resourceProvider->lockForRead(resourceId))
-    {
-        ASSERT(m_textureId);
-    }
-
-    ~CCScopedLockResourceForRead()
-    {
-        m_resourceProvider->unlockForRead(m_resourceId);
-    }
-
-    unsigned textureId() const { return m_textureId; }
-
-private:
-    CCResourceProvider* m_resourceProvider;
-    CCResourceProvider::ResourceId m_resourceId;
-    unsigned m_textureId;
-};
-
-class CCScopedLockResourceForWrite {
-    WTF_MAKE_NONCOPYABLE(CCScopedLockResourceForWrite);
-public:
-    CCScopedLockResourceForWrite(CCResourceProvider* resourceProvider, CCResourceProvider::ResourceId resourceId)
-        : m_resourceProvider(resourceProvider)
-        , m_resourceId(resourceId)
-        , m_textureId(resourceProvider->lockForWrite(resourceId)) { }
-
-    ~CCScopedLockResourceForWrite()
-    {
-        m_resourceProvider->unlockForWrite(m_resourceId);
-    }
-
-    unsigned textureId() const { return m_textureId; }
-
-private:
-    CCResourceProvider* m_resourceProvider;
-    CCResourceProvider::ResourceId m_resourceId;
-    unsigned m_textureId;
 };
 
 }
