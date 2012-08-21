5708d3ebc067396e10b976842532453d287eed9c
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-21 23:12:59 +0000

    clear QTCaptureDecompressedVideoOutput's delegate when CaptureSession is released. When CaptureSession is released, it's needed to remove it as QTCaptureDecompressedVideoOutput's delegate.
    BUG=138326
    Review URL: https://chromiumcodereview.appspot.com/10827458
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152665 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/mac/video_capture_device_qtkit_mac.mm
+++ media/video/capture/mac/video_capture_device_qtkit_mac.mm
@@ -92,6 +92,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       // The device is still running.
       [self stopCapture];
     }
+    [[[captureSession_ outputs] objectAtIndex:0] setDelegate:nil];
+    [captureSession_ removeOutput:[[captureSession_ outputs] objectAtIndex:0]];
     [captureSession_ release];
     captureSession_ = nil;
     [captureDeviceInput_ release];
