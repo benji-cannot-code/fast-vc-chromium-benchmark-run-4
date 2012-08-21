fd00a283d2c1f39cd05cda2b8e6e00fec99e1d12
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-21 06:50:42 +0000

    [V8] Move handleOutOfMemory() from V8Proxy to V8Binding https://bugs.webkit.org/show_bug.cgi?id=94563
    Reviewed by Adam Barth.
    
    To kill V8Proxy, this patch moves handleOutOfMemory() from V8Proxy to V8Binding.
    
    No tests. No change in behavior.
    
    * bindings/v8/V8AbstractEventListener.cpp:
    (WebCore::V8AbstractEventListener::invokeEventHandler):
    * bindings/v8/V8Binding.cpp:
    (WebCore::handleOutOfMemory):
    (WebCore):
    * bindings/v8/V8Binding.h:
    (WebCore):
    * bindings/v8/V8Proxy.cpp:
    * bindings/v8/V8Proxy.h:
    (V8Proxy):
    * bindings/v8/WorkerContextExecutionProxy.cpp:
    (WebCore::WorkerContextExecutionProxy::runScript):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126137 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Binding.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryInstrumentation.h"
 #include "PlatformString.h"
 #include "QualifiedName.h"
+#include "Settings.h"
 #include "V8DOMStringList.h"
 #include "V8DOMWindow.h"
 #include "V8Element.h"
@@ -409,6 +410,31 @@ V8PerContextData* perContextDataForCurrentWorld(Frame* frame)
     if (UNLIKELY(!!(isolatedContext = V8IsolatedContext::getEntered())))
         return isolatedContext->perContextData();
     return frame->script()->windowShell()->perContextData();
+}
+
+bool handleOutOfMemory()
+{
+    v8::Local<v8::Context> context = v8::Context::GetCurrent();
+
+    if (!context->HasOutOfMemoryException())
+        return false;
+
+    // Warning, error, disable JS for this frame?
+    Frame* frame = toFrameIfNotDetached(context);
+    if (!frame)
+        return true;
+
+    frame->script()->clearForClose();
+    frame->script()->windowShell()->destroyGlobal();
+
+#if PLATFORM(CHROMIUM)
+    PlatformSupport::notifyJSOutOfMemory(frame);
+#endif
+
+    if (Settings* settings = frame->settings())
+        settings->setScriptEnabled(false);
+
+    return true;
 }
 
 void crashIfV8IsDead()
