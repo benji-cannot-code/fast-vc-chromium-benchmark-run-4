4e4dbfd384a36ef73ba23691160cd352c1c90609
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-22 22:01:55 +0000

    win64 compile error fix for BinaryPropertyList.cpp https://bugs.webkit.org/show_bug.cgi?id=94122
    Patch by Alex Christensen <alex.christensen@flexsim.com> on 2012-08-22
    Reviewed by Eric Seidel.
    
    Fixed compile errors for MSVC x64 without changing performance
    
    * platform/cf/BinaryPropertyList.cpp:
    (WebCore::BinaryPropertyListSerializer::appendStringObject):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126357 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/cf/BinaryPropertyList.cpp
+++ third_party/WebKit/Source/WebCore/platform/cf/BinaryPropertyList.cpp
@@ -698,7 +698,7 @@ void BinaryPropertyListSerializer::appendStringObject(const String& string)
     unsigned length = string.length();
     if (charactersAreAllASCII(characters, length)) {
         if (length <= maxLengthInMarkerByte)
-            appendByte(asciiStringMarkerByte | length);
+            appendByte(static_cast<unsigned char>(asciiStringMarkerByte | length));
         else {
             appendByte(asciiStringWithSeparateLengthMarkerByte);
             appendInteger(length);
@@ -707,7 +707,7 @@ void BinaryPropertyListSerializer::appendStringObject(const String& string)
             appendByte(characters[i]);
     } else {
         if (length <= maxLengthInMarkerByte)
-            appendByte(unicodeStringMarkerByte | length);
+            appendByte(static_cast<unsigned char>(unicodeStringMarkerByte | length));
         else {
             appendByte(unicodeStringWithSeparateLengthMarkerByte);
             appendInteger(length);
@@ -724,7 +724,7 @@ void BinaryPropertyListSerializer::appendStringObject(const char* string)
     startObject();
     unsigned length = strlen(string);
     if (length <= maxLengthInMarkerByte)
-        appendByte(asciiStringMarkerByte | length);
+        appendByte(static_cast<unsigned char>(asciiStringMarkerByte | length));
     else {
         appendByte(asciiStringWithSeparateLengthMarkerByte);
         appendInteger(length);
