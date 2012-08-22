bbd3ea162515b8051286d1521ac68c90f1ae5f30
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 04:10:25 +0000

    Revert "Fixing crash Crash Report - Stack Signature: content::RenderWidgetHostImpl::ForwardKeybo"
    ben asked to revert this in the main trunk. The real fix will be in once Issue
    142422 gets fixed.
    
    This reverts commit 54bac78404d9b2d3ddb475526da8211414ae36b2.
    
    R=skuhne@chromium.org
    BUG=134465
    
    
    Review URL: https://chromiumcodereview.appspot.com/10866009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152729 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_tabstrip.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/chrome_pages.h"
-#include "chrome/browser/ui/fullscreen/fullscreen_controller.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/webui/sync_promo/sync_promo_ui.h"
@@ -43,10 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/win/metro.h"
-#endif
-
-#if defined(USE_ASH)
-#include "ash/wm/window_util.h"
 #endif
 
 using content::WebContents;
@@ -288,31 +283,13 @@ void BrowserCommandController::ExecuteCommandWithDisposition(
       NewIncognitoWindow(browser_);
       break;
     case IDC_CLOSE_WINDOW:
-      // Destroying a tab / browser window while it has opened a full screen
-      // window will destroy it's content class - which will destroy the
-      // delegate - which is also used by the opened full screen window's
-      // event handler. That will cause then a crash. To avoid that we supress
-      // closing of windows via key stroke while a full screen window is open.
-      // http://crbug.com/134465,  http://crbug.com/131436
-#if defined(OS_CHROMEOS)
-      if (!IsFullScreenWindowOpen())
-#endif
-        CloseWindow(browser_);
+      CloseWindow(browser_);
       break;
     case IDC_NEW_TAB:
       NewTab(browser_);
       break;
     case IDC_CLOSE_TAB:
-      // Destroying a tab / browser window while it has opened a full screen
-      // window will destroy it's content class - which will destroy the
-      // delegate - which is also used by the opened full screen window's
-      // event handler. That will cause then a crash. To avoid that we supress
-      // closing of windows via key stroke while a full screen window is open.
-      // http://crbug.com/134465,  http://crbug.com/131436
-#if defined(OS_CHROMEOS)
-      if (!IsFullScreenWindowOpen())
-#endif
-        CloseTab(browser_);
+      CloseTab(browser_);
       break;
     case IDC_SELECT_NEXT_TAB:
       SelectNextTab(browser_);
@@ -1100,15 +1077,6 @@ BrowserWindow* BrowserCommandController::window() {
 
 Profile* BrowserCommandController::profile() {
   return browser_->profile();
-}
-
-bool BrowserCommandController::IsFullScreenWindowOpen() {
-#if defined(USE_ASH)
-  aura::Window* window = ash::wm::GetActiveWindow();
-  return (window && ash::wm::IsWindowFullscreen(window));
-#else
-  return false;
-#endif
 }
 
 }  // namespace chrome
