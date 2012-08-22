e3eacf6df7b9f8c612c9ef58a824067ae2f83828
yefim@chromium.org <yefim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 00:34:15 +0000

    Fixed Issue 143925: Chrome: Crash Report - Stack Signature: views::MenuItemView::SetMargins(int,int)-56...
    During MAC port ActionBoxMenuModel was changed but windows code was not updated to reflect that change.
    So:
     - removed bookmark item (and separator) creation from action_box_menu (now hey are in model)
     - moved stared_ to model
     - fixed extensions related code in action box menu and model (they used to rely on extensions only model).
    
    BUG=143925
    
    
    Review URL: https://chromiumcodereview.appspot.com/10824407
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152693 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/action_box_menu.h
+++ chrome/browser/ui/views/action_box_menu.h
@@ -29,9 +29,7 @@ class ActionBoxMenu : public views::MenuDelegate,
                       public BrowserActionView::Delegate,
                       public content::NotificationObserver {
  public:
-  ActionBoxMenu(Browser* browser,
-                ActionBoxMenuModel* model,
-                bool starred);
+  ActionBoxMenu(Browser* browser, ActionBoxMenuModel* model);
   virtual ~ActionBoxMenu();
 
   void Init();
@@ -66,10 +64,6 @@ class ActionBoxMenu : public views::MenuDelegate,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
-  // Adds a new bookmark menu item to the menu, |next_id| is incremented.
-  views::MenuItemView* AddBookmarkMenuItem(views::MenuItemView* parent,
-                                           int* next_id);
-
   // Populates |root_| with all the child menu items from the |model_|.
   void PopulateMenu();
 
@@ -82,12 +76,6 @@ class ActionBoxMenu : public views::MenuDelegate,
 
   // The model that tracks the order of the toolbar icons.
   ActionBoxMenuModel* model_;
-
-  // Set when the current page is bookmarked and thus the star icon should be
-  // drawn in the "starred" rather than "unstarred" state.
-  bool starred_;
-
-  ScopedVector<BrowserActionView> browser_action_views_;
 
   DISALLOW_COPY_AND_ASSIGN(ActionBoxMenu);
 };
