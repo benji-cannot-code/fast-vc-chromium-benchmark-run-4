0884e38b6ecfd6a1018e7c6ed6949127fb070abb
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-22 20:14:08 +0000

    [WebGL] Mac/ATI/AMD systems need to translate built-in GLSL functions https://bugs.webkit.org/show_bug.cgi?id=94030
    Reviewed by Tim Horton.
    
    ATI/AMD GPUs on Apple platforms do not give correct values for some of
    the built-in GLSL functions. Add a compile flag that is passed to ANGLE
    so that, with this configuration, it will rewrite the shader to emulate
    the function in code.
    
    This is exposing some design weaknesses in the way we call ANGLE. We'll
    soon need to add more compiler flags; Future bugs will likely clean
    this code up. But this approach is satisfactory for the moment.
    
    This change is tested by the Khronos WebGL conformance test suite, in particular:
    conformance/glsl/functions/glsl-function-distance.html
    conformance/glsl/functions/glsl-function-dot.html
    conformance/glsl/functions/glsl-function-length.html
    
    * platform/graphics/ANGLEWebKitBridge.cpp:
    (WebCore::ANGLEWebKitBridge::validateShaderSource): Test for ATI cards
    on the Mac platform, and pass in an extra flag to the translation step.
    * platform/graphics/ANGLEWebKitBridge.h:
    (ANGLEWebKitBridge): Add a new parameter to getTranslatedShaderSourceANGLE
    that accepts some extra options to pass to ANGLE.
    * platform/graphics/opengl/Extensions3DOpenGLCommon.cpp:
    (WebCore::Extensions3DOpenGLCommon::getTranslatedShaderSourceANGLE): Pass
    the extra options into ANGLE's compile function.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126342 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ANGLEWebKitBridge.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ANGLEWebKitBridge.h
@@ -54,7 +54,7 @@ public:
     ShBuiltInResources getResources() { return m_resources; }
     void setResources(ShBuiltInResources);
     
-    bool validateShaderSource(const char* shaderSource, ANGLEShaderType shaderType, String& translatedShaderSource, String& shaderValidationLog);
+    bool validateShaderSource(const char* shaderSource, ANGLEShaderType shaderType, String& translatedShaderSource, String& shaderValidationLog, int extraCompileOptions);
 
 private:
 
