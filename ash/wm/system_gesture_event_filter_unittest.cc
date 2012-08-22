55747032fb4e74f78c4bd0bfcac8eb28f005af8a
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 14:57:01 +0000

    Long press affordance UI polish: 1. Add shrink animation after long press. 2. Simplify glow (shadow) drawing so that one gradient is drawn instead of 4. 3. Currently, the life of the affordance was tied to the target on which tap down happens. Instead, it should be tied to the touch id. This makes sense, for example, when the target is destroyed while the affordance is on. 4. Update various color values.
    BUG=139650
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10832429
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152769 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/system_gesture_event_filter_unittest.cc
+++ ash/wm/system_gesture_event_filter_unittest.cc
@@ -138,8 +138,8 @@ class SystemGestureEventFilterTest : public AshTestBase {
     return &GetLongPressAffordance()->timer_;
   }
 
-  aura::Window* GetLongPressAffordanceTarget() {
-    return GetLongPressAffordance()->tap_down_target_;
+  int GetLongPressAffordanceTouchId() {
+    return GetLongPressAffordance()->tap_down_touch_id_;
   }
 
   views::View* GetLongPressAffordanceView() {
@@ -435,7 +435,7 @@ TEST_F(SystemGestureEventFilterTest, LongPressAffordanceStateOnCaptureLoss) {
   base::OneShotTimer<internal::LongPressAffordanceAnimation>* timer =
       GetLongPressAffordanceTimer();
   EXPECT_TRUE(timer->IsRunning());
-  EXPECT_EQ(window1.get(), GetLongPressAffordanceTarget());
+  EXPECT_EQ(kTouchId, GetLongPressAffordanceTouchId());
 
   // Force timeout so that the affordance animation can start.
   timer->user_task().Run();
@@ -447,13 +447,19 @@ TEST_F(SystemGestureEventFilterTest, LongPressAffordanceStateOnCaptureLoss) {
   EXPECT_TRUE(window2->HasCapture());
 
   EXPECT_TRUE(GetLongPressAffordance()->is_animating());
-  EXPECT_EQ(window1.get(), GetLongPressAffordanceTarget());
+  EXPECT_EQ(kTouchId, GetLongPressAffordanceTouchId());
 
   // Animate to completion.
-  GetLongPressAffordance()->End();
+  GetLongPressAffordance()->End();  // end grow animation.
+  // Force timeout to start shrink animation.
+  EXPECT_TRUE(timer->IsRunning());
+  timer->user_task().Run();
+  timer->Stop();
+  EXPECT_TRUE(GetLongPressAffordance()->is_animating());
+  GetLongPressAffordance()->End();  // end shrink animation.
 
   // Check if state has reset.
-  EXPECT_EQ(NULL, GetLongPressAffordanceTarget());
+  EXPECT_EQ(-1, GetLongPressAffordanceTouchId());
   EXPECT_EQ(NULL, GetLongPressAffordanceView());
 }
 
