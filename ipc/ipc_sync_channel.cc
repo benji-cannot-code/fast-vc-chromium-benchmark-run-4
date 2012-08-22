bd824aca1298a92af26613394d8210433cd2c605
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 22:39:57 +0000

    Add traces for SyncChannel::SendWithTimeout
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10831415
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152841 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_sync_channel.cc
+++ ipc/ipc_sync_channel.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_sync_channel.h"
 
 #include "base/bind.h"
+#include "base/debug/trace_event.h"
 #include "base/lazy_instance.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -13,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event_watcher.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/threading/thread_local.h"
+#include "ipc/ipc_logging.h"
+#include "ipc/ipc_message_macros.h"
 #include "ipc/ipc_sync_message.h"
 
 using base::TimeDelta;
@@ -429,6 +432,17 @@ bool SyncChannel::Send(Message* message) {
 }
 
 bool SyncChannel::SendWithTimeout(Message* message, int timeout_ms) {
+#ifdef IPC_MESSAGE_LOG_ENABLED
+  Logging* logger = Logging::GetInstance();
+  std::string name;
+  logger->GetMessageText(message->type(), &name, message, NULL);
+  TRACE_EVENT1("task", "SyncChannel::SendWithTimeout",
+               "name", name);
+#else
+  TRACE_EVENT2("task", "SyncChannel::SendWithTimeout",
+               "class", IPC_MESSAGE_ID_CLASS(message->type()),
+               "line", IPC_MESSAGE_ID_LINE(message->type()));
+#endif
   if (!message->is_sync()) {
     ChannelProxy::Send(message);
     return true;
