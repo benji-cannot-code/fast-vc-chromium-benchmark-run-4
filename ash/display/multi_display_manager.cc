0b2acf354bcb3e021a5d49eda47bc6d4f71a5ec6
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 17:09:06 +0000

    Clean-up: use int64 for display ID, instead of int.
    The root cause of the bug seems FindDisplayForRootWindow in multi_display_manager.cc,
    but here I fixed all wrong "int" declarations.
    Verified working on both lumpy/link too.
    
    BUG=144068
    
    Review URL: https://chromiumcodereview.appspot.com/10878005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152783 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/multi_display_manager.cc
+++ ash/display/multi_display_manager.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_util.h"
 #endif
 
-DECLARE_WINDOW_PROPERTY_TYPE(int);
+DECLARE_WINDOW_PROPERTY_TYPE(int64);
 
 namespace ash {
 namespace internal {
@@ -43,7 +43,8 @@ using aura::Window;
 using std::string;
 using std::vector;
 
-DEFINE_WINDOW_PROPERTY_KEY(int64, kDisplayIdKey, -1);
+DEFINE_WINDOW_PROPERTY_KEY(int64, kDisplayIdKey,
+                           gfx::Display::kInvalidDisplayID);
 
 MultiDisplayManager::MultiDisplayManager() {
   Init();
@@ -197,7 +198,7 @@ std::string MultiDisplayManager::GetDisplayNameAt(size_t index) {
 #if defined(USE_X11)
   gfx::Display* display = GetDisplayAt(index);
   std::vector<XID> outputs;
-  if (display && display->id() != -1 &&
+  if (display && display->id() != gfx::Display::kInvalidDisplayID &&
       ui::GetOutputDeviceHandles(&outputs)) {
     for (size_t i = 0; i < outputs.size(); ++i) {
       uint16 manufacturer_id = 0;
@@ -286,7 +287,7 @@ void MultiDisplayManager::ScaleDisplayImpl() {
 
 gfx::Display& MultiDisplayManager::FindDisplayForRootWindow(
     const aura::RootWindow* root_window) {
-  int id = root_window->GetProperty(kDisplayIdKey);
+  int64 id = root_window->GetProperty(kDisplayIdKey);
   for (Displays::iterator iter = displays_.begin();
        iter != displays_.end(); ++iter) {
     if ((*iter).id() == id)
