720cd56ffd4914694997467d55988c275e157f76
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-22 21:55:10 +0000

    HTMLTreeBuilder::furthestBlockForFormattingElement should belong to HTMLElementStack https://bugs.webkit.org/show_bug.cgi?id=93857
    Patch by Kwang Yul Seo <skyul@company100.net> on 2012-08-22
    Reviewed by Eric Seidel.
    
    HTMLTreeBuilder::furthestBlockForFormattingElement should belong to
    HTMLElementStack because it traverses the element stack and finds the
    furthest block for the given formatting element.
    
    Currently, it belongs to HTMLTreeBuilder just because
    isSpecialNode(const HTMLStackItem*) function used by
    furthestBlockForFormattingElement is internal to HTMLTreeBuilder.
    
    Moved isSpecialNode to HTMLStackItem and changed
    furthestBlockForFormattingElement to be a method of HTMLElementStack.
    
    No behavior change. Just a refactoring.
    
    * html/parser/HTMLElementStack.cpp:
    (WebCore):
    (WebCore::HTMLElementStack::popUntilNumberedHeaderElementPopped):
    (WebCore::HTMLElementStack::hasNumberedHeaderElementInScope):
    (WebCore::HTMLElementStack::furthestBlockForFormattingElement):
    * html/parser/HTMLElementStack.h:
    (HTMLElementStack):
    * html/parser/HTMLStackItem.h:
    (WebCore::HTMLStackItem::isInHTMLNamespace):
    (HTMLStackItem):
    (WebCore::HTMLStackItem::isNumberedHeaderElement):
    (WebCore::HTMLStackItem::isTableBodyContextElement):
    (WebCore::HTMLStackItem::isSpecialNode):
    * html/parser/HTMLTreeBuilder.cpp:
    (WebCore::HTMLTreeBuilder::constructTreeFromAtomicToken):
    (WebCore::HTMLTreeBuilder::processCloseWhenNestedTag):
    (WebCore::HTMLTreeBuilder::processStartTagForInBody):
    (WebCore::HTMLTreeBuilder::processAnyOtherEndTagForInBody):
    (WebCore::HTMLTreeBuilder::callTheAdoptionAgency):
    (WebCore::HTMLTreeBuilder::shouldProcessTokenInForeignContent):
    (WebCore::HTMLTreeBuilder::processTokenInForeignContent):
    * html/parser/HTMLTreeBuilder.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/HTMLElementStack.h
+++ third_party/WebKit/Source/WebCore/html/parser/HTMLElementStack.h
@@ -102,6 +102,7 @@ public:
     HTMLStackItem* oneBelowTop() const;
     ElementRecord* topRecord() const;
     ElementRecord* find(Element*) const;
+    ElementRecord* furthestBlockForFormattingElement(Element*) const;
     ElementRecord* topmost(const AtomicString& tagName) const;
 
     void insertAbove(PassRefPtr<HTMLStackItem>, ElementRecord*);
@@ -181,15 +182,6 @@ private:
     unsigned m_stackDepth;
 };
     
-inline bool isInHTMLNamespace(const HTMLStackItem* item)
-{
-    // A DocumentFragment takes the place of the document element when parsing
-    // fragments and should be considered in the HTML namespace.
-    return item->namespaceURI() == HTMLNames::xhtmlNamespaceURI
-        || item->isDocumentFragmentNode(); // FIXME: Does this also apply to ShadowRoot?
-}
-
-
 } // namespace WebCore
 
 #endif // HTMLElementStack_h
--- third_party/WebKit/Source/WebCore/html/parser/HTMLTreeBuilder.h
+++ third_party/WebKit/Source/WebCore/html/parser/HTMLTreeBuilder.h
@@ -175,7 +175,6 @@ private:
 
     Vector<Attribute> attributesForIsindexInput(AtomicHTMLToken*);
 
-    HTMLElementStack::ElementRecord* furthestBlockForFormattingElement(Element*);
     void callTheAdoptionAgency(AtomicHTMLToken*);
 
     void closeTheCell();
