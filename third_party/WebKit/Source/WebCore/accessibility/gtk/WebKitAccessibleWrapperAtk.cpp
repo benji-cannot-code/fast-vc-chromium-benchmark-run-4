524af9d8a1d17f2456471c09abe8e8e4a2405c13
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-22 01:37:04 +0000

    [Gtk] No accessible caret-moved events found in certain content https://bugs.webkit.org/show_bug.cgi?id=72811
    Patch by Joanmarie Diggs <jdiggs@igalia.com> on 2012-08-21
    Reviewed by Chris Fleizach.
    
    Part of the bug is due to objects which should claim to implement AtkText
    failed to do so as a result of containing a mixture of inline and block
    spans.
    
    An updated unit test was provided.
    
    Source/WebCore:
    
    * accessibility/gtk/WebKitAccessibleWrapperAtk.cpp:
    (roleIsTextType): New method to check if a role is one expected to have
    implemented the AtkText interface. Currently that is defined as:
     - ParagraphRole
     - HeadingRole
     - DivRole
     - CellRole
    (getInterfaceMaskFromObject): If a role is one of the text type roles,
    automatically add WAI_TEXT to the accessible object's interface mask.
    
    Source/WebKit/gtk:
    
    * tests/testatk.c:
    (testWebkitAtkCaretOffsets): Added instances of objects containing a
    mixture of inline and block spans and tested that they implement AtkText
    and contain the right textual contents.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126243 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/gtk/WebKitAccessibleWrapperAtk.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/gtk/WebKitAccessibleWrapperAtk.cpp
@@ -816,6 +816,11 @@ static GType GetAtkInterfaceTypeFromWAIType(WAIType type)
     return G_TYPE_INVALID;
 }
 
+static bool roleIsTextType(AccessibilityRole role)
+{
+    return role == ParagraphRole || role == HeadingRole || role == DivRole || role == CellRole;
+}
+
 static guint16 getInterfaceMaskFromObject(AccessibilityObject* coreObject)
 {
     guint16 interfaceMask = 0;
@@ -857,7 +862,7 @@ static guint16 getInterfaceMaskFromObject(AccessibilityObject* coreObject)
         } else {
             if (role != TableRole) {
                 interfaceMask |= 1 << WAI_HYPERTEXT;
-                if (renderer && renderer->childrenInline())
+                if ((renderer && renderer->childrenInline()) || roleIsTextType(role))
                     interfaceMask |= 1 << WAI_TEXT;
             }
 
