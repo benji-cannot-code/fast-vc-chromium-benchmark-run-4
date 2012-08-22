50b3f0d919be24ce0b2bf1944acb670f8c64a129
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-22 13:34:13 +0000

    [GTK] Preferred languages and spellchecker APIs are not consistent in WebKit2 https://bugs.webkit.org/show_bug.cgi?id=94683
    Reviewed by Alejandro G. Castro.
    
    Source/WebCore:
    
    Use a Vector<String> instead of a comma-separated string to
    get/set languages.
    
    * platform/text/gtk/TextCheckerEnchant.cpp:
    (TextCheckerEnchant::updateSpellCheckingLanguages):
    (TextCheckerEnchant::getSpellCheckingLanguages):
    * platform/text/gtk/TextCheckerEnchant.h:
    (TextCheckerEnchant):
    
    Source/WebKit/gtk:
    
    * webkit/webkitspellcheckerenchant.cpp:
    (updateSpellCheckingLanguages): Split the languages string to pass a
    Vector to updateSpellCheckingLanguages().
    
    Source/WebKit2:
    
    Change spell-checker and preferred languages API to use a GStrv
    instead of a comma-separated string and GList. This makes the API
    more consistent and convenient to use.
    
    * UIProcess/API/gtk/WebKitTextChecker.cpp:
    (WebKitTextChecker::getSpellCheckingLanguages): Return a
    Vector<String> instead of a String.
    (WebKitTextChecker::setSpellCheckingLanguages): Receive a
    Vector<String> instead of a String.
    * UIProcess/API/gtk/WebKitTextChecker.h:
    (WebKitTextChecker): Use a GPtrArray to cache languages.
    * UIProcess/API/gtk/WebKitWebContext.cpp:
    (webkit_web_context_get_spell_checking_languages):
    (webkit_web_context_set_spell_checking_languages):
    (webkit_web_context_set_preferred_languages):
    * UIProcess/API/gtk/WebKitWebContext.h:
    * UIProcess/API/gtk/tests/TestWebKitWebContext.cpp:
    (testWebContextSpellChecker):
    (testWebContextLanguages):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitTextChecker.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitTextChecker.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/FastAllocBase.h>
 #include <wtf/PassOwnPtr.h>
 #include <wtf/Vector.h>
+#include <wtf/gobject/GRefPtr.h>
 #include <wtf/text/CString.h>
 
 class WebKitTextChecker {
@@ -45,14 +46,14 @@ public:
     void ignoreWord(const String& word);
 
     // To be called from WebKitWebContext only.
-    const CString& getSpellCheckingLanguages();
-    void setSpellCheckingLanguages(const CString& spellCheckingLanguages);
+    const char* const* getSpellCheckingLanguages();
+    void setSpellCheckingLanguages(const char* const* spellCheckingLanguages);
 
 private:
     WebKitTextChecker();
 
     OwnPtr<WebCore::TextCheckerEnchant> m_textChecker;
-    CString m_spellCheckingLanguages;
+    GRefPtr<GPtrArray> m_spellCheckingLanguages;
     bool m_spellCheckingEnabled;
 };
 
