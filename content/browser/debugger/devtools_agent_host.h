b4fdb209649cab9feef6dca94dcb219f447a2feb
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 04:15:13 +0000

    Fix a bug where the inspector would not keep event pages alive.
    The notification that an inspector window was opened was not always being sent.
    
    BUG=143284
    TBR=creis@chromium.org,pkasting@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10837296
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152733 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/debugger/devtools_agent_host.h
+++ content/browser/debugger/devtools_agent_host.h
@@ -36,9 +36,6 @@ class CONTENT_EXPORT DevToolsAgentHost {
   void AddMessageToConsole(ConsoleMessageLevel level,
                            const std::string& message);
 
-  // TODO(yurys): get rid of this method
-  virtual void NotifyClientClosing() = 0;
-
   virtual int GetRenderProcessId() = 0;
 
   void set_close_listener(CloseListener* listener) {
@@ -50,6 +47,9 @@ class CONTENT_EXPORT DevToolsAgentHost {
   virtual ~DevToolsAgentHost() {}
 
   virtual void SendMessageToAgent(IPC::Message* msg) = 0;
+  virtual void NotifyClientAttaching() = 0;
+  virtual void NotifyClientDetaching() = 0;
+
   void NotifyCloseListener();
 
   CloseListener* close_listener_;
--- content/browser/debugger/render_view_devtools_agent_host.h
+++ content/browser/debugger/render_view_devtools_agent_host.h
@@ -29,7 +29,8 @@ class CONTENT_EXPORT RenderViewDevToolsAgentHost
 
   // DevToolsAgentHost implementation.
   virtual void SendMessageToAgent(IPC::Message* msg) OVERRIDE;
-  virtual void NotifyClientClosing() OVERRIDE;
+  virtual void NotifyClientAttaching() OVERRIDE;
+  virtual void NotifyClientDetaching() OVERRIDE;
   virtual int GetRenderProcessId() OVERRIDE;
 
   // content::RenderViewHostObserver overrides.
