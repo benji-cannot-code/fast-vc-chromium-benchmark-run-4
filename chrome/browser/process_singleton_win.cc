9aabe094e4f65874f200abdd85770b04f963cc2e
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-22 22:37:47 +0000

    Fix the tree.
    Forgot windows include
    
    BUG=none
    TBR=scottmg
    Review URL: https://chromiumcodereview.appspot.com/10872021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton_win.cc
+++ chrome/browser/process_singleton_win.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/process_singleton.h"
 
+#include <shellapi.h>
+
 #include "base/base_paths.h"
 #include "base/command_line.h"
 #include "base/file_path.h"
