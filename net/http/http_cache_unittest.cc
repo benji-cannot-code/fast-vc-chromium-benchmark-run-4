2857a9b42365d4c7a17642f4148de7ffbe9ad585
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 18:19:03 +0000

    Revert 153025 - Automatically retry failed network requests.
    This is a temporary experiment to see if this behavior is
    worth implementing in a cleaner fashion.
    
    BUG=143425
    
    Review URL: https://chromiumcodereview.appspot.com/10854204
    
    TBR=mmenke@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10883011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153031 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_cache_unittest.cc
+++ net/http/http_cache_unittest.cc
@@ -237,7 +237,6 @@ class FastTransactionServer {
 bool FastTransactionServer::no_store;
 
 const MockTransaction kFastNoStoreGET_Transaction = {
-  net::OK,
   "http://www.google.com/nostore",
   "GET",
   base::Time(),
@@ -375,7 +374,6 @@ void RangeTransactionServer::RangeHandler(const net::HttpRequestInfo* request,
 }
 
 const MockTransaction kRangeGET_TransactionOK = {
-  net::OK,
   "http://www.google.com/range",
   "GET",
   base::Time(),
@@ -1794,14 +1792,14 @@ static void ConditionalizedRequestUpdatesCacheHelper(
 
   // We will control the network layer's responses for |kUrl| using
   // |mock_network_response|.
-  MockTransaction mock_network_response = { net::OK, 0 };
+  MockTransaction mock_network_response = { 0 };
   mock_network_response.url = kUrl;
   AddMockTransaction(&mock_network_response);
 
   // Request |kUrl| for the first time. It should hit the network and
   // receive |kNetResponse1|, which it saves into the HTTP cache.
 
-  MockTransaction request = { net::OK, 0 };
+  MockTransaction request = { 0 };
   request.url = kUrl;
   request.method = "GET";
   request.request_headers = "";
@@ -1985,11 +1983,11 @@ TEST(HttpCache, ConditionalizedRequestUpdatesCache4) {
 
   // We will control the network layer's responses for |kUrl| using
   // |mock_network_response|.
-  MockTransaction mock_network_response = { net::OK, 0 };
+  MockTransaction mock_network_response = { 0 };
   mock_network_response.url = kUrl;
   AddMockTransaction(&mock_network_response);
 
-  MockTransaction request = { net::OK, 0 };
+  MockTransaction request = { 0 };
   request.url = kUrl;
   request.method = "GET";
   request.request_headers = kExtraRequestHeaders;
@@ -2029,11 +2027,11 @@ TEST(HttpCache, ConditionalizedRequestUpdatesCache5) {
 
   // We will control the network layer's responses for |kUrl| using
   // |mock_network_response|.
-  MockTransaction mock_network_response = { net::OK, 0 };
+  MockTransaction mock_network_response = { 0 };
   mock_network_response.url = kUrl;
   AddMockTransaction(&mock_network_response);
 
-  MockTransaction request = { net::OK, 0 };
+  MockTransaction request = { 0 };
   request.url = kUrl;
   request.method = "GET";
   request.request_headers = kExtraRequestHeaders;
@@ -4661,14 +4659,14 @@ TEST(HttpCache, UpdatesRequestResponseTimeOn304) {
   const char* kUrl = "http://foobar";
   const char* kData = "body";
 
-  MockTransaction mock_network_response = { net::OK, 0 };
+  MockTransaction mock_network_response = { 0 };
   mock_network_response.url = kUrl;
 
   AddMockTransaction(&mock_network_response);
 
   // Request |kUrl|, causing |kNetResponse1| to be written to the cache.
 
-  MockTransaction request = { net::OK, 0 };
+  MockTransaction request = { 0 };
   request.url = kUrl;
   request.method = "GET";
   request.request_headers = "";
