4770d1e85b075d0d2015773c1631375f7b81a52a
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 09:29:36 +0000

    Omnibox placeholder text garbled
    This fixes bugs introduced with http://src.chromium.org/viewvc/chrome?view=rev&revision=152436.
    That change made the background color transparent which doesn't work with anti-aliased text.
    Also, that change erroneously changed the background color of --enable-instant-extended-api to non-white.
    
    BUG=137351
    TEST=Manual, launch Chrome on ChromeOS build with --enable-instant-extended-api flag set, verify Omnibox text is not garbled.
    R=varunjain@chromium.org, sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10873009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_bar_container.h
+++ chrome/browser/ui/views/location_bar/location_bar_container.h
@@ -70,7 +70,7 @@ class LocationBarContainer : public views::View,
   void PlatformInit();
 
   // Returns the background color.
-  static SkColor GetBackgroundColor();
+  SkColor GetBackgroundColor();
 
   // Returns animation duration in milliseconds.
   static int GetAnimationDuration();
@@ -86,6 +86,8 @@ class LocationBarContainer : public views::View,
   views::NativeViewHost* native_view_host_;
 
   bool in_toolbar_;
+
+  bool instant_extended_api_enabled_;
 
   DISALLOW_COPY_AND_ASSIGN(LocationBarContainer);
 };
