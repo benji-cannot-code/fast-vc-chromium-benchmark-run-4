3208c0464f8cdbd3c3733df88e2829060fa7ef6e
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-23 10:46:56 +0000

    WebInspector: unreviewed single line test fix.
    The test has to open profiles panel explicitly because it is creating lazily.
    
    * inspector/heap-snapshot-performance-test.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PerformanceTests/inspector/heap-snapshot-performance-test.js
+++ third_party/WebKit/PerformanceTests/inspector/heap-snapshot-performance-test.js
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function test()
 {
+    WebInspector.showPanel("profiles");
     InspectorTest.measureFunction(WebInspector.HeapSnapshot.prototype, "_buildEdgeIndexes");
     InspectorTest.measureFunction(WebInspector.HeapSnapshot.prototype, "_buildRetainers");
     InspectorTest.measureFunction(WebInspector.HeapSnapshot.prototype, "_buildDominatedNodes");
