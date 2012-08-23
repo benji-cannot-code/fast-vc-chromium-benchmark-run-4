a530aa67b7ad49b44a91615624bfb47caf50a948
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 22:23:33 +0000

    Extensions Docs Server: Set Cache-Control header
    Cache-Control: max-age is now set to 5 mins.
    
    BUG=144480
    
    Review URL: https://chromiumcodereview.appspot.com/10879052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/server_instance.py
+++ chrome/common/extensions/docs/server2/server_instance.py
@@ -55,6 +55,7 @@ class ServerInstance(object):
       content = templates.Render(path)
 
     if content:
+      response.headers['cache-control'] = 'max-age=300'
       response.out.write(content)
     else:
       response.set_status(404);
