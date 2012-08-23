c2408e4e0d985fb9ec82fc757572302e0a1dd738
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 18:39:44 +0000

    app_list: Refocus app when clicked from search result.
    BUG=137316
    TEST=Verify clicking app from app list search result refocus running app instead of opening a new instance.
    
    R=benwells@chromeium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10871028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153037 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/app_list/app_list_controller.h
+++ chrome/browser/ui/ash/app_list/app_list_controller.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Profile;
 
-namespace extensions {
-class Extension;
-}
-
 // Interface to allow the view delegate to call out to whatever is controlling
 // the app list. This will have different implementations for different
 // platforms.
@@ -34,10 +30,6 @@ class AppListController {
   virtual void ActivateApp(Profile* profile,
                            const std::string& extension_id,
                            int event_flags) = 0;
-  // Open the app (e.g. from search results).
-  virtual void OpenApp(Profile* profile,
-                       const std::string& extension_id,
-                       int event_flags) = 0;
 };
 
 #endif  // CHROME_BROWSER_UI_ASH_APP_LIST_APP_LIST_CONTROLLER_H_
