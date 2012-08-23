38cac49868102203655711f9f38a98b0136ec375
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-23 11:30:53 +0000

    Build fix for WinCE after r126359.
    * rendering/RenderThemeWinCE.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeWinCE.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeWinCE.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSStyleSheet.h"
 #include "CSSValueKeywords.h"
 #include "Document.h"
+#include "FontMetrics.h"
 #include "GraphicsContext.h"
 #if ENABLE(VIDEO)
 #include "HTMLMediaElement.h"
