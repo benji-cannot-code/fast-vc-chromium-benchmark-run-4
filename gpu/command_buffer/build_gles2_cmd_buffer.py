2e21694ac1297c0b7c620b9ece98f8f68e7b5bc0
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 01:00:50 +0000

    Remove overly agressive ASSERTs on output GL parameters.
    BUG=144022
    
    Review URL: https://chromiumcodereview.appspot.com/10860082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152900 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -4633,6 +4633,10 @@ class Argument(object):
 
   def __init__(self, name, type):
     self.name = name
+    self.optional = type.endswith("Optional*")
+    if self.optional:
+      type = type[:-9] + "*"
+    print name, type
     self.type = type
 
     if type in self.cmd_type_map_:
@@ -4713,8 +4717,9 @@ class Argument(object):
     if len(parts) > 1:
       return
     if parts[0] in self.need_validation_:
-      file.Write("  GPU_CLIENT_VALIDATE_DESTINATION_INITALIZATION(%s, %s);\n" %
-          (self.type[:-1], self.name))
+      file.Write(
+          "  GPU_CLIENT_VALIDATE_DESTINATION_%sINITALIZATION(%s, %s);\n" %
+          ("OPTIONAL_" if self.optional else "", self.type[:-1], self.name))
 
 
   def WriteGetAddress(self, file):
--- gpu/command_buffer/client/gles2_implementation.h
+++ gpu/command_buffer/client/gles2_implementation.h
@@ -77,6 +77,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     GPU_CLIENT_VALIDATE_DESTINATION_INITALIZATION_ASSERT(ptr && \
         (ptr[0] == static_cast<type>(0) || ptr[0] == static_cast<type>(-1)));
 
+#define GPU_CLIENT_VALIDATE_DESTINATION_OPTIONAL_INITALIZATION(type, ptr) \
+    GPU_CLIENT_VALIDATE_DESTINATION_INITALIZATION_ASSERT(!ptr || \
+        (ptr[0] == static_cast<type>(0) || ptr[0] == static_cast<type>(-1)));
+
 struct GLUniformDefinitionCHROMIUM;
 
 namespace gpu {
