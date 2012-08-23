55924d9777ace0e55c365f2a1f00ed29b6bf3795
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 01:05:16 +0000

    Expose EXT_debug_marker to webkit
    BUG=141700
    R=apatrick@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10828328
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152903 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
+++ content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
@@ -583,6 +583,10 @@ class WebGraphicsContext3DCommandBufferImpl
   virtual void consumeTextureCHROMIUM(WGC3Denum target,
                                       const WGC3Dbyte* mailbox);
 
+  virtual void insertEventMarkerEXT(const WGC3Dchar* marker);
+  virtual void pushGroupMarkerEXT(const WGC3Dchar* marker);
+  virtual void popGroupMarkerEXT();
+
  protected:
   virtual GrGLInterface* onCreateGrGLInterface();
 
