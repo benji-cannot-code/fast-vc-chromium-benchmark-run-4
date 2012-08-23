6feb57888612c4047eddf5961f6f9d396cf885e8
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 21:35:00 +0000

    disable media galleries if local file access is blocked by policy.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10832330
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153077 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/media_galleries/media_galleries_api.cc
+++ chrome/browser/extensions/api/media_galleries/media_galleries_api.cc
@@ -15,9 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/shell_window_registry.h"
 #include "chrome/browser/media_gallery/media_file_system_registry.h"
 #include "chrome/browser/media_gallery/media_galleries_dialog_controller.h"
+#include "chrome/browser/ui/chrome_select_file_policy.h"
 #include "chrome/browser/ui/extensions/shell_window.h"
 #include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/extensions/api/experimental_media_galleries.h"
+#include "chrome/common/pref_names.h"
 #include "content/public/browser/child_process_security_policy.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
@@ -33,7 +35,21 @@ namespace extensions {
 
 namespace {
 
+const char kDisallowedByPolicy[] =
+    "Media Galleries API is disallowed by policy: ";
 const char kInvalidInteractive[] = "Unknown value for interactive.";
+
+// Checks whether the MediaGalleries API is currently accessible (it may be
+// disallowed even if an extension has the requisite permission).
+bool ApiIsAccessible(std::string* error) {
+  if (!ChromeSelectFilePolicy::FileSelectDialogsAllowed()) {
+    *error = std::string(kDisallowedByPolicy) +
+        prefs::kAllowFileSelectionDialogs;
+    return false;
+  }
+
+  return true;
+}
 
 }  // namespace
 
@@ -47,6 +63,9 @@ MediaGalleriesGetMediaFileSystemsFunction::
     ~MediaGalleriesGetMediaFileSystemsFunction() {}
 
 bool MediaGalleriesGetMediaFileSystemsFunction::RunImpl() {
+  if (!ApiIsAccessible(&error_))
+    return false;
+
   scoped_ptr<GetMediaFileSystems::Params> params(
       GetMediaFileSystems::Params::Create(*args_));
   EXTENSION_FUNCTION_VALIDATE(params.get());
@@ -139,6 +158,9 @@ MediaGalleriesAssembleMediaFileFunction::
     ~MediaGalleriesAssembleMediaFileFunction() {}
 
 bool MediaGalleriesAssembleMediaFileFunction::RunImpl() {
+  if (!ApiIsAccessible(&error_))
+    return false;
+
   // TODO(vandebo) Update the metadata and return the new file.
   SetResult(Value::CreateNullValue());
   return true;
