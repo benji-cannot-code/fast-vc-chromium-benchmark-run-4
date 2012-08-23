7034327f8ea96bab17ab9488f0486bd2f7822ff4
jeremycho@google.com <jeremycho@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 02:06:34 +0000

    NTP5: Implement the Recently Closed page.
    Implement a page for showing Recently Closed tabs and create an ID for it to log when it's been selected.  Remove bare-minimum functionality to show all available dots.  Factor handleCardSelected/Deselected into MostVisitedPage - it's not needed for general selection logging.
    
    Also delete showApps and isSuggestionsEnabled calls - they've been deprecated but didn't get removed from ntp_search and throw JS errors. 
    
    Original review thread (prior to merge with 10829131):
    http://codereview.chromium.org/10827185/
    
    BUG=142669
    
    Review URL: https://chromiumcodereview.appspot.com/10834399
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@152920 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_search/mock/mock_data_autogen.js
+++ chrome/browser/resources/ntp_search/mock/mock_data_autogen.js
@@ -40,8 +40,7 @@ chrome.mock && chrome.mock({
           'version': '0.1'
         }
       ],
-      'showLauncher': false,
-      'showPromo': false
+      'showLauncher': false
     }
   ],
   'getRecentlyClosedTabs': [
@@ -180,6 +179,7 @@ chrome.mock && chrome.mock({
     'otherSessionsLearnMoreUrl': 'http://support.google.com/chrome/bin/' +
         'answer.py?answer=185277&hl=en',
     'recentlyclosed': 'Recently closed',
+    'recently_closed_page_id': 5120,
     'removethumbnailtooltip': 'Don\'t show on this page',
     'restoreSessionMenuItemText': 'Open all',
     'restoreThumbnailsShort': 'Restore all',
