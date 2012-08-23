0bf960f171fdabd024dcf3edee900f9f7d6aab41
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-23 00:38:08 +0000

    [V8] Move matchesCurrentContext() from V8Proxy to ScriptController https://bugs.webkit.org/show_bug.cgi?id=94596
    Reviewed by Adam Barth.
    
    To kill V8Proxy:
    
    - We can move matchesCurrentContext() from V8Proxy to ScriptController.
    - We can remove V8Proxy::isolatedWorldContext() since it is not used by anybody.
    - We can remove V8Proxy::finishedWithEvent() since it is empty.
    
    No tests. No change in behavior.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateToV8Converters):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.cpp:
    (WebCore::V8TestActiveDOMObject::wrapSlow):
    * bindings/scripts/test/V8/V8TestNode.cpp:
    (WebCore::V8TestNode::wrapSlow):
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::finishedWithEvent):
    (WebCore::ScriptController::matchesCurrentContext):
    (WebCore):
    * bindings/v8/ScriptController.h:
    (ScriptController):
    * bindings/v8/V8Proxy.cpp:
    * bindings/v8/V8Proxy.h:
    (V8Proxy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126370 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8Proxy.h
@@ -88,8 +88,6 @@ namespace WebCore {
 
         Frame* frame() const { return m_frame; }
 
-        void finishedWithEvent(Event*) { }
-
         // Evaluate a script file in the current execution environment.
         // The caller must hold an execution context.
         // If cannot evalute the script, it returns an error.
@@ -97,9 +95,6 @@ namespace WebCore {
 
         // Run an already compiled script.
         v8::Local<v8::Value> runScript(v8::Handle<v8::Script>);
-
-        v8::Local<v8::Context> isolatedWorldContext(int worldId);
-        bool matchesCurrentContext();
 
         // FIXME: This should eventually take DOMWrapperWorld argument!
         // FIXME: This method will be soon removed, as all methods that access windowShell()
