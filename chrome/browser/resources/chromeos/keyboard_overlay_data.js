433bef7716b62ad30d10d551c3d29d8cad6b0d84
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 20:51:47 +0000

    Add keyboard overlay strings for Alt+F6 and Alt+F7.
    BUG=143848
    TEST=manually checked with Link
    
    Review URL: https://chromiumcodereview.appspot.com/10861024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153063 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -15802,6 +15802,8 @@ var keyboardOverlayData = {
     'backspace<>CTRL': 'keyboardOverlayDeleteWord',
     'backspace<>CTRL<>SHIFT': 'keyboardOverlayClearBrowsingDataDialog',
     'backspace<>SHIFT': 'keyboardOverlayGoForward',
+    'bright down<>ALT': 'keyboardOverlayKeyboardBrightDown',
+    'bright up<>ALT': 'keyboardOverlayKeyboardBrightUp',
     'c<>CTRL': 'keyboardOverlayCopy',
     'c<>CTRL<>SHIFT': 'keyboardOverlayDomInspector',
     'd<>ALT': 'keyboardOverlayFocusAddressBar',
