76faab408a5de1e71e68a1a3d854d04461a7df9a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 22:33:17 +0000

    aura: Make sure event copying works properly for X11.
    This includes two changes:
     * In X11, copying event is tricky, especially for XInput2 events. So avoid
       doing that when possible.
     * Mouse-wheel events used to piggy-back on MouseEvents during dispatch, and
       the offset used to be extracted from the native-event. Fix this to make sure
       the wheel-events are more like scroll-events so that the offset is carried
       with the ui::Event during dispatch to avoid dependency on the native-event.
    
    BUG=144211
    
    Review URL: https://chromiumcodereview.appspot.com/10875018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153094 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/web_input_event_aura.cc
+++ content/browser/renderer_host/web_input_event_aura.cc
@@ -24,7 +24,7 @@ WebKit::WebTouchPoint* UpdateWebTouchEventFromNativeEvent(
 #else
 WebKit::WebMouseEvent MakeWebMouseEventFromAuraEvent(ui::MouseEvent* event);
 WebKit::WebMouseWheelEvent MakeWebMouseWheelEventFromAuraEvent(
-    ui::MouseEvent* event);
+    ui::MouseWheelEvent* event);
 WebKit::WebMouseWheelEvent MakeWebMouseWheelEventFromAuraEvent(
     ui::ScrollEvent* event);
 WebKit::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
@@ -81,7 +81,7 @@ WebKit::WebMouseEvent MakeWebMouseEvent(ui::MouseEvent* event) {
   return webkit_event;
 }
 
-WebKit::WebMouseWheelEvent MakeWebMouseWheelEvent(ui::MouseEvent* event) {
+WebKit::WebMouseWheelEvent MakeWebMouseWheelEvent(ui::MouseWheelEvent* event) {
 #if defined(OS_WIN)
   // Construct an untranslated event from the platform event data.
   WebKit::WebMouseWheelEvent webkit_event =
