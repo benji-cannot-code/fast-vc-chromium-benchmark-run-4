833b79567059e5d8c613aa8b37b34802262e4199
rjkroege@chromium.org <rjkroege@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-23 22:36:36 +0000

    Make the fling acceleration constant adjustable from chrome://gesture
    The GestureRecognizer applies an acceleration to touchscreen fling velocities
    to make them align more nicely with the touchpad fling deacceleration curves.
    Make this configurable from the interactive configurator.
    
    BUG=141648
    
    
    Review URL: https://chromiumcodereview.appspot.com/10861033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153097 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gesture_config.js
+++ chrome/browser/resources/gesture_config.js
@@ -125,6 +125,12 @@ var gesture_config = (function() {
       label: 'Rail-Start Proportion',
       units: '%',
       default: 2
+    },
+    {
+      key: 'touchscreen_fling_acceleration_adjustment',
+      label: 'Touchscreen Fling Acceleration Adjustment',
+      units: 'pixels/sec.',
+      default: 1 / 900
     }
   ];
 
--- chrome/browser/ui/gesture_prefs_observer_factory_aura.cc
+++ chrome/browser/ui/gesture_prefs_observer_factory_aura.cc
@@ -65,6 +65,7 @@ const char* kPrefsToObserve[] = {
   prefs::kRailBreakProportion,
   prefs::kRailStartProportion,
   prefs::kSemiLongPressTimeInSeconds,
+  prefs::kTouchScreenFlingAccelerationAdjustment,
 };
 
 GesturePrefsObserver::GesturePrefsObserver(PrefService* prefs)
@@ -143,6 +144,9 @@ void GesturePrefsObserver::Update() {
   GestureConfiguration::set_rail_start_proportion(
       prefs_->GetDouble(
           prefs::kRailStartProportion));
+  GestureConfiguration::set_touchscreen_fling_acceleration_adjustment(
+      prefs_->GetDouble(
+          prefs::kTouchScreenFlingAccelerationAdjustment));
 }
 
 }  // namespace
@@ -236,6 +240,10 @@ void GesturePrefsObserverFactoryAura::RegisterUserPrefs(PrefService* prefs) {
   prefs->RegisterDoublePref(
       prefs::kRailStartProportion,
       GestureConfiguration::rail_start_proportion(),
+      PrefService::UNSYNCABLE_PREF);
+  prefs->RegisterDoublePref(
+      prefs::kTouchScreenFlingAccelerationAdjustment,
+      GestureConfiguration::touchscreen_fling_acceleration_adjustment(),
       PrefService::UNSYNCABLE_PREF);
 }
 
