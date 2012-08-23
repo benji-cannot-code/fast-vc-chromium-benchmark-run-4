28cf1e003920afd83150db348c95fea9b41a0e39
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-23 22:52:30 +0000

    CSSParser: Move enumeration to a common place (StylePropertyShorthand) https://bugs.webkit.org/show_bug.cgi?id=93210
    Patch by Adenilson Cavalcanti <cavalcantii@gmail.com> on 2012-08-23
    Reviewed by Dean Jackson.
    
    CSSParser::parseAnimationShorthand() uses an enumeration with the same
    elements as another enumeration present in StylePropertyShorthand, but
    with different ordering of values. This patch puts both enums in the same place.
    
    No new tests. No change in behavior.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseAnimationShorthand):
    * css/StylePropertyShorthand.cpp:
    (WebCore::webkitAnimationShorthandForParsing):
    (WebCore):
    * css/StylePropertyShorthand.h:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StylePropertyShorthand.h
+++ third_party/WebKit/Source/WebCore/css/StylePropertyShorthand.h
@@ -81,6 +81,7 @@ const StylePropertyShorthand& outlineShorthand();
 const StylePropertyShorthand& overflowShorthand();
 const StylePropertyShorthand& paddingShorthand();
 const StylePropertyShorthand& webkitAnimationShorthand();
+const StylePropertyShorthand& webkitAnimationShorthandForParsing();
 const StylePropertyShorthand& webkitBorderAfterShorthand();
 const StylePropertyShorthand& webkitBorderBeforeShorthand();
 const StylePropertyShorthand& webkitBorderEndShorthand();
