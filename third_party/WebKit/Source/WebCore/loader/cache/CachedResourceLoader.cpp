e4dc7330cf72a9fc9e233d8fb2a7940051c1c67f
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-23 01:07:22 +0000

    Unreviewed, rolling out r126325. http://trac.webkit.org/changeset/126325 https://bugs.webkit.org/show_bug.cgi?id=94644
    Caused subtle but reproducible failure to call onload handler
    properly in an SVG layout test in Chromium Mac Debug builds,
    indicating potentially larger problem
    
    * css/CSSFontSelector.cpp:
    (WebCore::CSSFontSelector::beginLoadTimerFired):
    * loader/cache/CachedResourceLoader.cpp:
    (WebCore::CachedResourceLoader::decrementRequestCount):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResourceLoader.cpp
@@ -39,7 +39,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContentSecurityPolicy.h"
 #include "DOMWindow.h"
 #include "Document.h"
-#include "DocumentLoader.h"
 #include "Frame.h"
 #include "FrameLoader.h"
 #include "FrameLoaderClient.h"
@@ -727,12 +726,6 @@ void CachedResourceLoader::decrementRequestCount(const CachedResource* res)
 
     --m_requestCount;
     ASSERT(m_requestCount > -1);
-
-    // New resource loads (e.g. font loads) may be triggered by layout after the document load is
-    // complete but before we have dispatched didFinishLoading for the frame. Make sure the delegate
-    // is always dispatched by checking explicitly once we are done loading all resources.
-    if (!m_requestCount && m_document && m_document->loader() && m_document->loader()->frameLoader())
-        m_document->loader()->frameLoader()->checkLoadComplete();
 }
     
 void CachedResourceLoader::preload(CachedResource::Type type, ResourceRequest& request, const String& charset, bool referencedFromBody)
