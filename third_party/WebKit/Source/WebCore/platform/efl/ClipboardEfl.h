1fa6315308bd0d9086ea7799b762a3e34f9db8c9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-23 01:56:54 +0000

    Consider replacing return type of Clipboard::types() from HashSet<String> to Vector<String>. https://bugs.webkit.org/show_bug.cgi?id=82888
    Patch by Vineet Chaudhary <rgf748@motorola.com> on 2012-08-22
    Reviewed by Kentaro Hara.
    
    As part of removing custom bindings of types Array Clipboard::types() needs to return
    Vector<String> than HashSet<String>
    
    No new tests. Exixting test should pass with this change as no behavoural changes.
    
    * bindings/js/JSClipboardCustom.cpp: Replace data type from HashSet<> to Vector<>.
    (WebCore::JSClipboard::types):
    * bindings/v8/custom/V8ClipboardCustom.cpp: Ditto.
    (WebCore::V8Clipboard::typesAccessorGetter):
    * dom/Clipboard.h: Ditto.
    (Clipboard):
    * platform/blackberry/ClipboardBlackBerry.cpp: Ditto.
    (WebCore::ClipboardBlackBerry::types):
    * platform/blackberry/ClipboardBlackBerry.h: Ditto.
    (ClipboardBlackBerry):
    * platform/chromium/ChromiumDataObject.cpp: Ditto.
    (WebCore::ChromiumDataObject::types):
    * platform/chromium/ChromiumDataObject.h: Ditto.
    (ChromiumDataObject):
    * platform/chromium/ClipboardChromium.cpp: Ditto.
    (WebCore::ClipboardChromium::types):
    * platform/chromium/ClipboardChromium.h: Ditto.
    (ClipboardChromium):
    * platform/efl/ClipboardEfl.cpp: Ditto.
    (WebCore::ClipboardEfl::types):
    * platform/efl/ClipboardEfl.h: Ditto.
    (ClipboardEfl):
    * platform/gtk/ClipboardGtk.cpp: Ditto.
    (WebCore::ClipboardGtk::types):
    * platform/gtk/ClipboardGtk.h: Ditto.
    (ClipboardGtk):
    * platform/mac/ClipboardMac.h: Ditto.
    (ClipboardMac):
    * platform/mac/ClipboardMac.mm: Ditto.
    (WebCore::addHTMLClipboardTypesForCocoaType):
    (WebCore::ClipboardMac::types):
    * platform/qt/ClipboardQt.cpp: Ditto.
    (WebCore::ClipboardQt::types):
    * platform/qt/ClipboardQt.h: Ditto.
    (ClipboardQt):
    * platform/win/ClipboardWin.cpp: Ditto.
    (WebCore::addMimeTypesForFormat):
    (WebCore::ClipboardWin::types):
    * platform/win/ClipboardWin.h: Ditto.
    (ClipboardWin):
    * platform/wx/ClipboardWx.cpp: Ditto.
    (WebCore::ClipboardWx::types):
    * platform/wx/ClipboardWx.h: Ditto.
    (ClipboardWx):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/ClipboardEfl.h
+++ third_party/WebKit/Source/WebCore/platform/efl/ClipboardEfl.h
@@ -40,7 +40,7 @@ public:
     String getData(const String&) const;
     bool setData(const String&, const String&);
 
-    HashSet<String> types() const;
+    Vector<String> types() const;
     virtual PassRefPtr<FileList> files() const;
 
     IntPoint dragLocation() const;
