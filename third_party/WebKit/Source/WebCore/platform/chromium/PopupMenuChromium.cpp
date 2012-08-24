25a40ab499adeedf782c257c95d1d4392eadb07d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 23:02:52 +0000

    [chromium] Reduce padding on popup menu entries when used on a touch screen. https://bugs.webkit.org/show_bug.cgi?id=94953
    Patch by Kevin Ellis <kevers@chromium.org> on 2012-08-24
    Reviewed by Adam Barth.
    
    Prior to the patch, the height of entries in a popup menu was 44px,
    which is excessive.  Reduced the padding to make the menu entries
    30px tall to match the wrench menu.
    
    No new tests required.
    
    * platform/chromium/PopupMenuChromium.cpp:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126643 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/PopupMenuChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/PopupMenuChromium.cpp
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 int PopupMenuChromium::s_minimumRowHeight = 0;
-int PopupMenuChromium::s_optionPaddingForTouch = 30;
+int PopupMenuChromium::s_optionPaddingForTouch = 16;
 
 // The settings used for the drop down menu.
 // This is the delegate used if none is provided.
