bba3b489f5b075cd3e768e64b69e5cf9ddd5d19e
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 19:37:14 +0000

    Ash: Remove notifications when clicked on
    BUG=144546
    TEST=Ensure that clicking on a notification still invokes any associated action. Notification should be removed regardless of whether it is a popup or in the tray.
    
    
    
    Review URL: https://chromiumcodereview.appspot.com/10870051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/balloon_collection_impl_ash.cc
+++ chrome/browser/ui/views/ash/balloon_collection_impl_ash.cc
@@ -98,6 +98,7 @@ void BalloonCollectionImplAsh::OnClicked(const std::string& notifcation_id) {
   if (!balloon)
     return;
   balloon->OnClick();
+  RemoveById(notifcation_id);
 }
 
 bool BalloonCollectionImplAsh::AddWebUIMessageCallback(
