66b1eb0d732f9b40b6e2089401fc3d239f981b59
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 20:43:50 +0000

    Gets animations working for new workspace code. Some tuning of times needs be done, but the basics are there.
    BUG=137342
    TEST=none
    R=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10868077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153272 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_manager2.h
+++ ash/wm/workspace/workspace_manager2.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/workspace/base_workspace_manager.h"
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/timer.h"
 #include "ui/base/ui_base_types.h"
 
 namespace aura {
@@ -23,6 +24,10 @@ class Window;
 namespace gfx {
 class Point;
 class Rect;
+}
+
+namespace ui {
+class Layer;
 }
 
 namespace ash {
@@ -47,6 +52,7 @@ class ASH_EXPORT WorkspaceManager2 : public BaseWorkspaceManager {
   // Returns true if |window| is considered maximized and should exist in its
   // own workspace.
   static bool IsMaximized(aura::Window* window);
+  static bool IsMaximizedState(ui::WindowShowState state);
 
   // Returns true if |window| is minimized and will restore to a maximized
   // window.
@@ -69,6 +75,18 @@ class ASH_EXPORT WorkspaceManager2 : public BaseWorkspaceManager {
 
   typedef std::vector<Workspace2*> Workspaces;
 
+  // Describes which, if any, of the workspaces should animate. NEW is the
+  // workspace that is becoming active, and OLD the current workspace.
+  enum AnimateType {
+    ANIMATE_NONE,
+
+    // Animates only the new workspace in.
+    ANIMATE_NEW,
+
+    // Animates both the old and new workspaces.
+    ANIMATE_OLD_AND_NEW,
+  };
+
   // Updates the visibility and whether any windows overlap the shelf.
   void UpdateShelfVisibility();
 
@@ -76,7 +94,7 @@ class ASH_EXPORT WorkspaceManager2 : public BaseWorkspaceManager {
   Workspace2* FindBy(aura::Window* window) const;
 
   // Sets the active workspace.
-  void SetActiveWorkspace(Workspace2* workspace);
+  void SetActiveWorkspace(Workspace2* workspace, AnimateType animate_type);
 
   // Returns the bounds of the work area.
   gfx::Rect GetWorkAreaBounds() const;
@@ -96,10 +114,19 @@ class ASH_EXPORT WorkspaceManager2 : public BaseWorkspaceManager {
   // if |workspace| contains no children it is deleted, otherwise it it moved to
   // |pending_workspaces_|.
   void MoveWorkspaceToPendingOrDelete(Workspace2* workspace,
-                                      aura::Window* stack_beneath);
+                                      aura::Window* stack_beneath,
+                                      AnimateType animate_type);
 
   // Selects the next workspace.
-  void SelectNextWorkspace();
+  void SelectNextWorkspace(AnimateType animate_type);
+
+  // Schedules |workspace| for deletion when it no longer contains any layers.
+  // See comments above |to_delete_| as to why we do this.
+  void ScheduleDelete(Workspace2* workspace);
+
+  // Deletes any workspaces scheduled via ScheduleDelete() that don't contain
+  // any layers.
+  void ProcessDeletion();
 
   // These methods are forwarded from the LayoutManager installed on the
   // Workspace's window.
@@ -113,7 +140,8 @@ class ASH_EXPORT WorkspaceManager2 : public BaseWorkspaceManager {
                                            aura::Window* child);
   void OnWorkspaceWindowShowStateChanged(Workspace2* workspace,
                                          aura::Window* child,
-                                         ui::WindowShowState last_show_state);
+                                         ui::WindowShowState last_show_state,
+                                         ui::Layer* old_layer);
 
   aura::Window* contents_view_;
 
@@ -143,6 +171,13 @@ class ASH_EXPORT WorkspaceManager2 : public BaseWorkspaceManager {
   // MoveWorkspaceToPendingOrDelete() we use this to avoid doing anything if
   // already in MoveWorkspaceToPendingOrDelete().
   bool in_move_;
+
+  // Ideally we would delete workspaces when not needed. Unfortunately doing so
+  // would effectively cancel animations. Instead when a workspace is no longer
+  // needed we add it here and start a timer. When the timer fires any windows
+  // no longer contain layers are deleted.
+  std::set<Workspace2*> to_delete_;
+  base::OneShotTimer<WorkspaceManager2> delete_timer_;
 
   DISALLOW_COPY_AND_ASSIGN(WorkspaceManager2);
 };
