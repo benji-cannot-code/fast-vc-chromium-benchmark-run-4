1cc29629b6e14f57e3b78a5fda743e9d3e163b2e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 09:02:58 +0000

    Source/WebCore: Check argument count in the dispatch function for overloaded functions https://bugs.webkit.org/show_bug.cgi?id=94790
    Patch by Xiaobo Wang <xbwang@torchmobile.com.cn> on 2012-08-24
    Reviewed by Kentaro Hara.
    
    Throw NotEnoughArguments exception for overloaded functions if actual argument
    count is less than the least mandatory argument count among all overloaded.
    
    Reset JS and V8 binding tests result for the change.
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateFunctionParametersCheck):
    (GenerateOverloadedFunction):
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateFunctionParametersCheck):
    (GenerateOverloadedFunctionCallback):
    * bindings/scripts/test/JS/JSTestObj.cpp:
    (WebCore::jsTestObjPrototypeFunctionOverloadedMethod):
    (WebCore::jsTestObjConstructorFunctionOverloadedMethod1):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::overloadedMethodCallback):
    (WebCore::TestObjV8Internal::overloadedMethod1Callback):
    
    LayoutTests: Remove platform specific expected files after the change
    https://bugs.webkit.org/show_bug.cgi?id=94790
    
    Patch by Xiaobo Wang <xbwang@torchmobile.com.cn> on 2012-08-24
    Reviewed by Kentaro Hara.
    
    1. Use the cross-platform expected files for the 2 tests in question.
    2. Update other tests for overloaded functions and their expected files
    affected by the change.
    
    * fast/canvas/canvas-overloads-drawImage-expected.txt:
    * fast/canvas/canvas-overloads-setFillColor-expected.txt:
    * fast/canvas/canvas-overloads-setShadow-expected.txt:
    * fast/canvas/canvas-overloads-setStrokeColor-expected.txt:
    * fast/canvas/script-tests/canvas-overloads-drawImage.js:
    * fast/canvas/script-tests/canvas-overloads-setFillColor.js:
    * fast/canvas/script-tests/canvas-overloads-setShadow.js:
    * fast/canvas/script-tests/canvas-overloads-setStrokeColor.js:
    * fast/canvas/webgl/texImageTest-expected.txt:
    * platform/chromium/fast/files/create-blob-url-crash-expected.txt: Removed.
    * platform/chromium/fast/files/url-required-arguments-expected.txt: Removed.
    * platform/gtk/fast/files/create-blob-url-crash-expected.txt: Removed.
    * platform/gtk/fast/files/url-required-arguments-expected.txt: Removed.
    * storage/indexeddb/index-get-key-argument-required-expected.txt:
    * storage/indexeddb/objectStore-required-arguments-expected.txt:
    * storage/indexeddb/transaction-and-objectstore-calls-expected.txt:
    * storage/indexeddb/transaction-storeNames-required-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1411,14 +1411,16 @@ sub GenerateFunctionParametersCheck
 
     my @orExpression = ();
     my $numParameters = 0;
+    my $numMandatoryParams = @{$function->parameters};
     foreach my $parameter (@{$function->parameters}) {
         if ($parameter->extendedAttributes->{"Optional"}) {
             push(@orExpression, GenerateParametersCheckExpression($numParameters, $function));
+            $numMandatoryParams--;
         }
         $numParameters++;
     }
     push(@orExpression, GenerateParametersCheckExpression($numParameters, $function));
-    return join(" || ", @orExpression);
+    return ($numMandatoryParams, join(" || ", @orExpression));
 }
 
 sub GenerateOverloadedFunctionCallback
@@ -1435,6 +1437,7 @@ sub GenerateOverloadedFunctionCallback
 
     my $name = $function->signature->name;
     my $conditionalString = $codeGenerator->GenerateConditionalString($function->signature);
+    my $leastNumMandatoryParams = 255;
     push(@implContentDecls, "#if ${conditionalString}\n\n") if $conditionalString;
     push(@implContentDecls, <<END);
 static v8::Handle<v8::Value> ${name}Callback(const v8::Arguments& args)
@@ -1443,9 +1446,14 @@ static v8::Handle<v8::Value> ${name}Callback(const v8::Arguments& args)
 END
 
     foreach my $overload (@{$function->{overloads}}) {
-        my $parametersCheck = GenerateFunctionParametersCheck($overload);
+        my ($numMandatoryParams, $parametersCheck) = GenerateFunctionParametersCheck($overload);
+        $leastNumMandatoryParams = $numMandatoryParams if ($numMandatoryParams < $leastNumMandatoryParams);
         push(@implContentDecls, "    if ($parametersCheck)\n");
         push(@implContentDecls, "        return ${name}$overload->{overloadIndex}Callback(args);\n");
+    }
+    if ($leastNumMandatoryParams >= 1) {
+        push(@implContentDecls, "    if (args.Length() < $leastNumMandatoryParams)\n");
+        push(@implContentDecls, "        return throwNotEnoughArgumentsError(args.GetIsolate());\n");
     }
     push(@implContentDecls, <<END);
     return throwTypeError(0, args.GetIsolate());
--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestObj.cpp
@@ -1677,6 +1677,8 @@ static v8::Handle<v8::Value> overloadedMethodCallback(const v8::Arguments& args)
         return overloadedMethod9Callback(args);
     if ((args.Length() == 1 && (args[0]->IsArray())))
         return overloadedMethod10Callback(args);
+    if (args.Length() < 1)
+        return throwNotEnoughArgumentsError(args.GetIsolate());
     return throwTypeError(0, args.GetIsolate());
 }
 
@@ -1734,6 +1736,8 @@ static v8::Handle<v8::Value> overloadedMethod1Callback(const v8::Arguments& args
         return overloadedMethod11Callback(args);
     if ((args.Length() == 1 && (args[0]->IsNull() || args[0]->IsUndefined() || args[0]->IsString() || args[0]->IsObject())))
         return overloadedMethod12Callback(args);
+    if (args.Length() < 1)
+        return throwNotEnoughArgumentsError(args.GetIsolate());
     return throwTypeError(0, args.GetIsolate());
 }
 
