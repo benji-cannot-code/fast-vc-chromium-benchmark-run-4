250cdd1e2f47bc72534d1a9d75699243bc47033b
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 10:08:15 +0000

    Quota code cleanup
    * Replace foo_usage_tracker_ with GetUsageTracker(type) in quota_manager.cc
    * Killing unused template types in quota_types.h
    * Adding parameter names in Callback typedefs in quota_types.h
    
    BUG=none
    TEST=green bots
    
    
    Review URL: https://chromiumcodereview.appspot.com/10882009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153182 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_manager.cc
+++ webkit/quota/quota_manager.cc
@@ -127,6 +127,8 @@ class QuotaManager::UsageAndQuotaDispatcherTask : public QuotaTask {
   void DidGetHostUsage(const std::string& host, StorageType type, int64 usage) {
     DCHECK_EQ(this->host(), host);
     DCHECK_EQ(this->type(), type);
+    if (quota_status_ == kQuotaStatusUnknown)
+      quota_status_ = kQuotaStatusOk;
     host_usage_ = usage;
     CheckCompleted();
   }
@@ -369,9 +371,9 @@ class QuotaManager::UsageAndQuotaDispatcherTaskForTemporary
 
  protected:
   virtual void RunBody() OVERRIDE {
-    manager()->temporary_usage_tracker_->GetGlobalUsage(
+    manager()->GetUsageTracker(type())->GetGlobalUsage(
         NewWaitableGlobalUsageCallback());
-    manager()->temporary_usage_tracker_->GetHostUsage(
+    manager()->GetUsageTracker(type())->GetHostUsage(
         host(), NewWaitableHostUsageCallback());
     manager()->GetAvailableSpace(NewWaitableAvailableSpaceCallback());
   }
@@ -403,7 +405,7 @@ class QuotaManager::UsageAndQuotaDispatcherTaskForPersistent
 
  protected:
   virtual void RunBody() OVERRIDE {
-    manager()->persistent_usage_tracker_->GetHostUsage(
+    manager()->GetUsageTracker(type())->GetHostUsage(
         host(), NewWaitableHostUsageCallback());
     manager()->GetPersistentHostQuota(
         host(), NewWaitableHostQuotaCallback());
@@ -425,7 +427,7 @@ class QuotaManager::UsageAndQuotaDispatcherTaskForTemporaryGlobal
 
  protected:
   virtual void RunBody() OVERRIDE {
-    manager()->temporary_usage_tracker_->GetGlobalUsage(
+    manager()->GetUsageTracker(type())->GetGlobalUsage(
         NewWaitableGlobalUsageCallback());
     manager()->GetAvailableSpace(NewWaitableAvailableSpaceCallback());
   }
@@ -1433,17 +1435,16 @@ void QuotaManager::DeleteHostData(const std::string& host,
 }
 
 bool QuotaManager::ResetUsageTracker(StorageType type) {
+  DCHECK(GetUsageTracker(type));
+  if (GetUsageTracker(type)->IsWorking())
+    return false;
   switch (type) {
     case kStorageTypeTemporary:
-      if (temporary_usage_tracker_->IsWorking())
-        return false;
       temporary_usage_tracker_.reset(
           new UsageTracker(clients_, kStorageTypeTemporary,
                            special_storage_policy_));
       return true;
     case kStorageTypePersistent:
-      if (persistent_usage_tracker_->IsWorking())
-        return false;
       persistent_usage_tracker_.reset(
           new UsageTracker(clients_, kStorageTypePersistent,
                            special_storage_policy_));
@@ -1470,18 +1471,8 @@ void QuotaManager::GetCachedOrigins(
     StorageType type, std::set<GURL>* origins) {
   DCHECK(origins);
   LazyInitialize();
-  switch (type) {
-    case kStorageTypeTemporary:
-      DCHECK(temporary_usage_tracker_.get());
-      temporary_usage_tracker_->GetCachedOrigins(origins);
-      return;
-    case kStorageTypePersistent:
-      DCHECK(persistent_usage_tracker_.get());
-      persistent_usage_tracker_->GetCachedOrigins(origins);
-      return;
-    default:
-      NOTREACHED();
-  }
+  DCHECK(GetUsageTracker(type));
+  GetUsageTracker(type)->GetCachedOrigins(origins);
 }
 
 void QuotaManager::NotifyStorageAccessedInternal(
