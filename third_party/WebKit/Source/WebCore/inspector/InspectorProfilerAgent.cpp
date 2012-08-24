ade8b0340f4608fd1f43965a62202a96d54e390b
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 13:30:11 +0000

    Web Inspector: do not send profile headers to the front-end until explicitly requested. https://bugs.webkit.org/show_bug.cgi?id=94928
    Reviewed by Vsevolod Vlasov.
    
    Simple flag is added.
    
    * inspector/InspectorProfilerAgent.cpp:
    (WebCore::InspectorProfilerAgent::InspectorProfilerAgent):
    (WebCore::InspectorProfilerAgent::addProfile):
    (WebCore::InspectorProfilerAgent::disable):
    (WebCore::InspectorProfilerAgent::getProfileHeaders):
    (WebCore):
    (WebCore::InspectorProfilerAgent::resetFrontendProfiles):
    * inspector/InspectorProfilerAgent.h:
    (InspectorProfilerAgent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorProfilerAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorProfilerAgent.cpp
@@ -124,6 +124,7 @@ InspectorProfilerAgent::InspectorProfilerAgent(InstrumentingAgents* instrumentin
     , m_frontend(0)
     , m_enabled(false)
     , m_recordingUserInitiatedProfile(false)
+    , m_headersRequested(false)
     , m_currentUserInitiatedProfileNumber(-1)
     , m_nextUserInitiatedProfileNumber(1)
     , m_nextUserInitiatedHeapSnapshotNumber(1)
@@ -140,7 +141,7 @@ void InspectorProfilerAgent::addProfile(PassRefPtr<ScriptProfile> prpProfile, un
 {
     RefPtr<ScriptProfile> profile = prpProfile;
     m_profiles.add(profile->uid(), profile);
-    if (m_frontend)
+    if (m_frontend && m_headersRequested)
         m_frontend->addProfileHeader(createProfileHeader(*profile));
     addProfileFinishedMessageToConsole(profile, lineNumber, sourceURL);
 }
@@ -221,6 +222,7 @@ void InspectorProfilerAgent::disable()
     if (!m_enabled)
         return;
     m_enabled = false;
+    m_headersRequested = false;
     PageScriptDebugServer::shared().recompileAllJSFunctionsSoon();
 }
 
@@ -243,6 +245,7 @@ String InspectorProfilerAgent::getCurrentUserInitiatedProfileName(bool increment
 
 void InspectorProfilerAgent::getProfileHeaders(ErrorString*, RefPtr<TypeBuilder::Array<TypeBuilder::Profiler::ProfileHeader> >& headers)
 {
+    m_headersRequested = true;
     headers = TypeBuilder::Array<TypeBuilder::Profiler::ProfileHeader>::create();
 
     ProfilesMap::iterator profilesEnd = m_profiles.end();
@@ -322,7 +325,7 @@ void InspectorProfilerAgent::resetState()
 
 void InspectorProfilerAgent::resetFrontendProfiles()
 {
-    if (m_frontend && enabled()
+    if (m_headersRequested && m_frontend
         && m_profiles.begin() == m_profiles.end()
         && m_snapshots.begin() == m_snapshots.end())
         m_frontend->resetProfiles();
