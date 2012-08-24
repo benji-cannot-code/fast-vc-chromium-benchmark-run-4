036f94abbaea8b3eb9674517e6553c8ab9139390
penghuang@chromium.org <penghuang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 12:58:18 +0000

    Fix build errors `undeclared functions read() write() and usleep()' in Fedora 17.
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10883010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153193 0039d316-1c4b-4281-b951-d872f2087c98

--- net/tools/flip_server/flip_config.cc
+++ net/tools/flip_server/flip_config.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/tools/flip_server/flip_config.h"
 
+#include <unistd.h>
+
 namespace net {
 
 FlipAcceptor::FlipAcceptor(enum FlipHandlerType flip_handler_type,
--- net/tools/flip_server/mem_cache.cc
+++ net/tools/flip_server/mem_cache.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdio.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <unistd.h>
 
 #include <deque>
 
