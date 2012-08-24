ba6e4fac3ad59aa4604d2824d2aadc601894f520
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 11:38:19 +0000

    Rename GDataOperationInterface to AuthenticatedOperationInterface
    BUG=137151
    TEST=pass all existing tests.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10879061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/auth_service.cc
+++ chrome/browser/chromeos/gdata/auth_service.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/message_loop_proxy.h"
-#include "chrome/browser/chromeos/gdata/gdata_operations.h"
+#include "chrome/browser/chromeos/gdata/operations_base.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/signin/token_service.h"
 #include "chrome/browser/signin/token_service_factory.h"
--- chrome/browser/chromeos/gdata/operation_runner.cc
+++ chrome/browser/chromeos/gdata/operation_runner.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/gdata/operation_runner.h"
 
 #include "base/bind.h"
-#include "chrome/browser/chromeos/gdata/gdata_operations.h"
+#include "chrome/browser/chromeos/gdata/operations_base.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -44,7 +44,7 @@ void OperationRunner::Authenticate(const AuthStatusCallback& callback) {
 }
 
 void OperationRunner::StartOperationWithRetry(
-    GDataOperationInterface* operation) {
+    AuthenticatedOperationInterface* operation) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   // The re-authenticatation callback will run on UI thread.
@@ -54,7 +54,8 @@ void OperationRunner::StartOperationWithRetry(
   StartOperation(operation);
 }
 
-void OperationRunner::StartOperation(GDataOperationInterface* operation) {
+void OperationRunner::StartOperation(
+    AuthenticatedOperationInterface* operation) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   if (!auth_service_->HasAccessToken()) {
@@ -71,7 +72,7 @@ void OperationRunner::StartOperation(GDataOperationInterface* operation) {
 }
 
 void OperationRunner::OnOperationAuthRefresh(
-    GDataOperationInterface* operation,
+    AuthenticatedOperationInterface* operation,
     GDataErrorCode code,
     const std::string& auth_token) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
@@ -84,7 +85,8 @@ void OperationRunner::OnOperationAuthRefresh(
   }
 }
 
-void OperationRunner::RetryOperation(GDataOperationInterface* operation) {
+void OperationRunner::RetryOperation(
+    AuthenticatedOperationInterface* operation) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   auth_service_->ClearAccessToken();
