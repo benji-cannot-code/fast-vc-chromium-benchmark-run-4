e8422c8c3af11f89ac659968a21f3ec742ff5979
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 06:43:36 +0000

    Unreviewed, rolling out r126542. http://trac.webkit.org/changeset/126542 https://bugs.webkit.org/show_bug.cgi?id=94907
    Broke WK2 build on OS X (Requested by tronical_ on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-08-23
    
    Source/WebCore:
    
    * inspector/CodeGeneratorInspector.py:
    (EnumConstants.get_enum_constant_code):
    (TypeBindings.create_type_declaration_.EnumBinding.get_code_generator.CodeGenerator.generate_type_builder):
    (Generator.go):
    (Generator.process_event):
    (Generator.process_command):
    
    Source/WebKit2:
    
    * Scripts/webkit2/messages.py:
    * Scripts/webkit2/parser.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126548 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Scripts/webkit2/messages.py
+++ third_party/WebKit/Source/WebKit2/Scripts/webkit2/messages.py
@@ -23,7 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import collections
 import re
-from webkit2 import parser
+
+import parser
+
 
 DELAYED_ATTRIBUTE = 'Delayed'
 DISPATCH_ON_CONNECTION_QUEUE_ATTRIBUTE = 'DispatchOnConnectionQueue'
