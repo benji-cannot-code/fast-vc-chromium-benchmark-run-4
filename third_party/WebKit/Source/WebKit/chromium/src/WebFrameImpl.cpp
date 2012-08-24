db3af0df2394e23240e9326793a9c25c40b26515
cdn@chromium.org <cdn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 19:01:18 +0000

    Source/WebCore: Add support for consumable user gestures https://bugs.webkit.org/show_bug.cgi?id=94867
    Reviewed by Adam Barth.
    
    Adds a static counter of the number of consumable gestures. This is decremented either when
    a gestures falls out of scope or is consumed via a call to consumeUserGesture().
    
    Planned usage in Chromium is to prevent the piggybacking of multiple popup windows on a
    single user gesture.
    
    No new tests as this should not change behavior. The platform must implement consumable
    gestures for this to do anything.
    
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::consumeUserGesture):
    (WebCore):
    * bindings/v8/ScriptController.h:
    (ScriptController):
    * dom/UserGestureIndicator.cpp:
    (WebCore):
    (WebCore::UserGestureIndicator::UserGestureIndicator):
    (WebCore::UserGestureIndicator::~UserGestureIndicator):
    (WebCore::UserGestureIndicator::consumeUserGesture):
    * dom/UserGestureIndicator.h:
    (WebCore::UserGestureIndicator::processingUserGesture):
    (UserGestureIndicator):
    
    Source/WebKit/chromium: Plumb consumeUserGesture() to the chromium platform layer.
    https://bugs.webkit.org/show_bug.cgi?id=9475294867
    
    Reviewed by Adam Barth.
    
    * public/WebFrame.h:
    (WebFrame):
    * src/WebFrameImpl.cpp:
    (WebKit::WebFrameImpl::consumeUserGesture):
    (WebKit):
    * src/WebFrameImpl.h:
    (WebFrameImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126609 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -1191,6 +1191,11 @@ bool WebFrameImpl::isProcessingUserGesture() const
     return ScriptController::processingUserGesture();
 }
 
+bool WebFrameImpl::consumeUserGesture() const
+{
+    return UserGestureIndicator::consumeUserGesture();
+}
+
 bool WebFrameImpl::willSuppressOpenerInNewFrame() const
 {
     return frame()->loader()->suppressOpenerInNewFrame();
