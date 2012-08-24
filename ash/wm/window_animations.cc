da91b5f32b73d50bc27436d68fb139346007b852
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 22:09:35 +0000

    Revert 153291 - Fixes crash introduced @ 153047 (you can hit crash by maximizing a window). The cross fade code deletes the layer when the animation finishes. The newly added code was accessing members after the animation finished and the animator was deleted, resulting in the crash. Since I'm sure this will come up more in the future I've restructured the code to allow for deletion when calling out like this.
    The cross fade test exercises this code path now, but I'll see about a
    more focused tests shortly.
    
    BUG=129033
    TEST=covered by tests.
    R=vollick@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10874064
    
    TBR=sky@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10882043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153312 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_animations.cc
+++ ash/wm/window_animations.cc
@@ -51,6 +51,7 @@ namespace internal {
 namespace {
 const float kWindowAnimation_Vertical_TranslateY = 15.f;
 
+bool delayed_old_layer_deletion_in_cross_fade_for_test_ = false;
 }
 
 DEFINE_WINDOW_PROPERTY_KEY(WindowVisibilityAnimationType,
@@ -664,7 +665,13 @@ class CrossFadeObserver : public ui::CompositorObserver,
 
   // ui::ImplicitAnimationObserver overrides:
   virtual void OnImplicitAnimationsCompleted() OVERRIDE {
-    delete this;
+    // ImplicitAnimationObserver's base class uses the object after
+    // calling this function, so we cannot delete |this|. The |layer_|
+    // may be gone by the next message loop run when shutting down, so
+    // clean them up now.
+    if (!delayed_old_layer_deletion_in_cross_fade_for_test_)
+      Cleanup();
+    MessageLoop::current()->DeleteSoon(FROM_HERE, this);
   }
 
  private:
@@ -974,6 +981,10 @@ bool AnimateOnChildWindowVisibilityChanged(aura::Window* window, bool visible) {
     return window->layer()->GetTargetOpacity() != 0.0f &&
         AnimateHideWindow(window);
   }
+}
+
+void SetDelayedOldLayerDeletionInCrossFadeForTest(bool value) {
+  delayed_old_layer_deletion_in_cross_fade_for_test_ = value;
 }
 
 }  // namespace internal
