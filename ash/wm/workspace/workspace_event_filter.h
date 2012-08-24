6db1aad985fa51a7c10bcf6c691fd769ecd40a00
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-24 22:44:06 +0000

    ash: Double-tap on the window title should maximize/restore.
    BUG=144705
    
    Review URL: https://chromiumcodereview.appspot.com/10873066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153329 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_event_filter.h
+++ ash/wm/workspace/workspace_event_filter.h
@@ -26,6 +26,9 @@ class WorkspaceEventFilter : public ToplevelWindowEventFilter {
   // Overridden from ToplevelWindowEventFilter:
   virtual bool PreHandleMouseEvent(aura::Window* target,
                                    ui::MouseEvent* event) OVERRIDE;
+  virtual ui::GestureStatus PreHandleGestureEvent(
+      aura::Window* target,
+      ui::GestureEvent* event) OVERRIDE;
 
  protected:
   // Overridden from ToplevelWindowEventFilter:
