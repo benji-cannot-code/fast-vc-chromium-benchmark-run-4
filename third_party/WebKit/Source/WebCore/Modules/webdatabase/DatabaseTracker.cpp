17f26ba634e51d0133d6be6772ea5ebebef40204
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 23:18:55 +0000

    Populate DatabaseTracker's origins lazily on demand https://bugs.webkit.org/show_bug.cgi?id=94649
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-08-24
    Reviewed by Brady Eidson.
    
    Previously, we would populate m_quotaMap on startup when initialializing DatabaseTracker.
    This takes a significant amount of time and memory and it is not needed in many cases.
    
    This patch change DatabaseTracker to invoke populateOrigins() only when needed.
    This saves:
    -7-15ms on startup.
    -96Kbytes of SQLite page cache
    -the size of m_quotaMap on that particular system.
    
    * Modules/webdatabase/DatabaseTracker.cpp:
    (WebCore::DatabaseTracker::DatabaseTracker):
    (WebCore::DatabaseTracker::hasEntryForOriginNoLock):
    (WebCore::DatabaseTracker::populateOriginsIfNeeded):
    (WebCore::DatabaseTracker::origins):
    (WebCore::DatabaseTracker::quotaForOriginNoLock):
    (WebCore::DatabaseTracker::addDatabase):
    (WebCore::DatabaseTracker::deleteOrigin):
    * Modules/webdatabase/DatabaseTracker.h:
    (DatabaseTracker):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126648 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webdatabase/DatabaseTracker.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webdatabase/DatabaseTracker.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2007, 2008 Apple Inc. All rights reserved.
+ * Copyright (C) 2007, 2008, 2012 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -85,9 +85,6 @@ DatabaseTracker::DatabaseTracker(const String& databasePath)
     setDatabaseDirectoryPath(databasePath);
     
     SQLiteFileSystem::registerSQLiteVFS();
-
-    MutexLocker lockDatabase(m_databaseGuard);
-    populateOrigins();
 }
 
 void DatabaseTracker::setDatabaseDirectoryPath(const String& path)
@@ -195,6 +192,7 @@ bool DatabaseTracker::canEstablishDatabase(ScriptExecutionContext* context, cons
 bool DatabaseTracker::hasEntryForOriginNoLock(SecurityOrigin* origin)
 {
     ASSERT(!m_databaseGuard.tryLock());
+    populateOriginsIfNeeded();
     ASSERT(m_quotaMap);
     return m_quotaMap->contains(origin);
 }
@@ -333,7 +331,7 @@ String DatabaseTracker::fullPathForDatabase(SecurityOrigin* origin, const String
     return fullPathForDatabaseNoLock(origin, name, createIfNotExists).isolatedCopy();
 }
 
-void DatabaseTracker::populateOrigins()
+void DatabaseTracker::populateOriginsIfNeeded()
 {
     ASSERT(!m_databaseGuard.tryLock());
     if (m_quotaMap)
@@ -365,6 +363,7 @@ void DatabaseTracker::populateOrigins()
 void DatabaseTracker::origins(Vector<RefPtr<SecurityOrigin> >& result)
 {
     MutexLocker lockDatabase(m_databaseGuard);
+    populateOriginsIfNeeded();
     ASSERT(m_quotaMap);
     copyKeysToVector(*m_quotaMap, result);
 }
@@ -646,6 +645,7 @@ unsigned long long DatabaseTracker::usageForOrigin(SecurityOrigin* origin)
 unsigned long long DatabaseTracker::quotaForOriginNoLock(SecurityOrigin* origin)
 {
     ASSERT(!m_databaseGuard.tryLock());
+    populateOriginsIfNeeded();
     ASSERT(m_quotaMap);
     return m_quotaMap->get(origin);
 }
@@ -706,10 +706,11 @@ void DatabaseTracker::setQuota(SecurityOrigin* origin, unsigned long long quota)
 bool DatabaseTracker::addDatabase(SecurityOrigin* origin, const String& name, const String& path)
 {
     ASSERT(!m_databaseGuard.tryLock());
-    ASSERT(m_quotaMap);
     openTrackerDatabase(true);
     if (!m_database.isOpen())
         return false;
+    populateOriginsIfNeeded();
+    ASSERT(m_quotaMap);
 
     // New database should never be added until the origin has been established
     ASSERT(hasEntryForOriginNoLock(origin));
@@ -806,6 +807,7 @@ bool DatabaseTracker::deleteOrigin(SecurityOrigin* origin)
 
         SQLiteFileSystem::deleteEmptyDatabaseDirectory(originPath(origin));
 
+        populateOriginsIfNeeded();
         RefPtr<SecurityOrigin> originPossiblyLastReference = origin;
         m_quotaMap->remove(origin);
 
--- third_party/WebKit/Source/WebCore/Modules/webdatabase/DatabaseTracker.h
+++ third_party/WebKit/Source/WebCore/Modules/webdatabase/DatabaseTracker.h
@@ -126,7 +126,7 @@ private:
     bool hasEntryForDatabase(SecurityOrigin*, const String& databaseIdentifier);
 
     bool addDatabase(SecurityOrigin*, const String& name, const String& path);
-    void populateOrigins();
+    void populateOriginsIfNeeded();
 
     bool deleteDatabaseFile(SecurityOrigin*, const String& name);
 
