dcc6279d53b59f1e890e84fb0eeb4f95cf2b7cc4
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-24 04:23:10 +0000

    [Forms] Step mismatched value should be editable in multiple field time UI. https://bugs.webkit.org/show_bug.cgi?id=94664
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    This patch changes layout method of multiple field time input UI to
    check value of the "input" element in addition to step and range to
    allow users to change step mismatched value from UI.
    
    Tests: fast/forms/time-multiple-fields/time-multiple-fields-change-layout-by-value.html: for changing layout by value change.
    This patch also adds new test case into fast/forms/time-multiple-fields/time-multiple-fields-appearance-basic.html.
    
    * html/TimeInputType.cpp:
    (WebCore::TimeInputType::createShadowSubtree): Changed for DateTimeEditElement::create signature change.
    (WebCore::TimeInputType::minOrMaxAttributeChanged): Changed to call updateInnerTextValue instead of removed updateEditElementLayout()
    (WebCore::TimeInputType::stepAttributeChanged): ditto.
    (WebCore::TimeInputType::updateInnerTextValue): Changed for DateTimeEditElement::setValueAsDate() and setEmptyValue() signature change.
    * html/TimeInputType.h:
    (TimeInputType): Removed updateEditElementLayout() declaration.
    * html/shadow/DateTimeEditElement.cpp:
    (DateTimeEditBuilder):
    (WebCore::DateTimeEditBuilder::DateTimeEditBuilder): Added a new parameter date value to constructor to use it for layout decision.
    (WebCore::DateTimeEditBuilder::needMillisecondField): Changed to check date value too.
    (WebCore::DateTimeEditBuilder::needMinuteField): ditto
    (WebCore::DateTimeEditBuilder::needSecondField): ditto
    (WebCore::DateTimeEditBuilder::shouldMillisecondFieldReadOnly): ditto
    (WebCore::DateTimeEditBuilder::shouldMinuteFieldReadOnly): ditto
    (WebCore::DateTimeEditBuilder::shouldSecondFieldReadOnly): ditto
    (WebCore::DateTimeEditElement::create): Changed for removing stepRange parameter.
    (WebCore::DateTimeEditElement::layout): Changed to keep focus field for new layout.
    (WebCore::DateTimeEditElement::setValueAsDate): Changed to call layout() with StepRange and date value.
    (WebCore::DateTimeEditElement::setEmptyValue): ditto
    * html/shadow/DateTimeEditElement.h:
    (DateTimeEditElement):
    
    LayoutTests:
    
    This patch adds new test case for step mismatched value case and
    updates test cases to avoid step mismatched value, and a test file
    for dynamic layout change casued by input element's value change.
    
    Note: We need to do rebaseline for Chromium-Mac and Chromium-Win.
    
    * fast/forms/time-multiple-fields/time-multiple-fields-appearance-basic-expected.html: Updated for new test case and behavior change.
    * fast/forms/time-multiple-fields/time-multiple-fields-appearance-basic.html: ditto
    * fast/forms/time-multiple-fields/time-multiple-fields-appearance-disabled-readonly.html: Updated for behavior change.
    * fast/forms/time-multiple-fields/time-multiple-fields-appearance-pseudo-elements.html: ditto
    * fast/forms/time-multiple-fields/time-multiple-fields-appearance-style.html: ditto
    * fast/forms/time-multiple-fields/time-multiple-fields-change-layout-by-value-expected.txt: Added.
    * fast/forms/time-multiple-fields/time-multiple-fields-change-layout-by-value.html: Added for dynamic layout change from value change.
    * platform/chromium-linux/fast/forms/time-multiple-fields/time-multiple-fields-appearance-disabled-readonly-expected.png: ditto
    * platform/chromium-linux/fast/forms/time-multiple-fields/time-multiple-fields-appearance-disabled-readonly-expected.txt: ditto
    * platform/chromium-linux/fast/forms/time-multiple-fields/time-multiple-fields-appearance-pseudo-classes-expected.png: ditto
    * platform/chromium-linux/fast/forms/time-multiple-fields/time-multiple-fields-appearance-pseudo-classes-expected.txt: ditto
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126534 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/TimeInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/TimeInputType.cpp
@@ -183,8 +183,7 @@ void TimeInputType::createShadowSubtree()
 {
     ASSERT(element()->shadow());
 
-    const StepRange stepRange(createStepRange(AnyIsDefaultStep));
-    RefPtr<DateTimeEditElement> dateTimeEditElement(DateTimeEditElement::create(element()->document(), m_dateTimeEditControlOwnerImpl, stepRange));
+    RefPtr<DateTimeEditElement> dateTimeEditElement(DateTimeEditElement::create(element()->document(), m_dateTimeEditControlOwnerImpl));
     m_dateTimeEditElement = dateTimeEditElement.get();
     element()->userAgentShadowRoot()->appendChild(m_dateTimeEditElement);
     updateInnerTextValue();
@@ -239,7 +238,7 @@ bool TimeInputType::isMouseFocusable() const
 
 void TimeInputType::minOrMaxAttributeChanged()
 {
-    updateEditElementLayout();
+    updateInnerTextValue();
 }
 
 void TimeInputType::readonlyAttributeChanged()
@@ -267,13 +266,7 @@ bool TimeInputType::shouldUseInputMethod() const
 
 void TimeInputType::stepAttributeChanged()
 {
-    updateEditElementLayout();
-}
-
-void TimeInputType::updateEditElementLayout()
-{
-    if (m_dateTimeEditElement)
-        m_dateTimeEditElement->layout(createStepRange(AnyIsDefaultStep));
+    updateInnerTextValue();
 }
 
 void TimeInputType::updateInnerTextValue()
@@ -281,13 +274,13 @@ void TimeInputType::updateInnerTextValue()
     if (!m_dateTimeEditElement)
         return;
 
+    const StepRange stepRange(createStepRange(AnyIsDefaultStep));
     DateComponents date;
     if (parseToDateComponents(element()->value(), &date))
-        m_dateTimeEditElement->setValueAsDate(date);
+        m_dateTimeEditElement->setValueAsDate(stepRange, date);
     else {
-        const StepRange stepRange(createStepRange(AnyIsDefaultStep));
         setMillisecondToDateComponents(stepRange.minimum().toDouble(), &date);
-        m_dateTimeEditElement->setEmptyValue(date);
+        m_dateTimeEditElement->setEmptyValue(stepRange, date);
     }
 }
 #else
