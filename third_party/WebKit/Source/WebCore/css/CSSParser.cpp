6a385892b34b2edc60d76e886cbe9cf97d663df1
michelangelo@webkit.org <michelangelo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-25 09:23:44 +0000

    [Crash] Null pointer in CSSParser::parseMixFunction() https://bugs.webkit.org/show_bug.cgi?id=94998
    Reviewed by Benjamin Poulain.
    
    Source/WebCore:
    
    parseMixFunction() may try to access invalid memory when the arguments of the
    mix() function are comma-terminated.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseMixFunction):
    
    LayoutTests:
    
    New test cases added to check invalid comma-terminated values within mix().
    
    * css3/filters/custom/custom-filter-property-parsing-invalid-expected.txt:
    * css3/filters/script-tests/custom-filter-property-parsing-invalid.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -7446,6 +7446,9 @@ PassRefPtr<WebKitCSSMixFunctionValue> CSSParser::parseMixFunction(CSSParserValue
         return 0;
 
     CSSParserValueList* argsList = value->function->args.get();
+    if (!argsList)
+        return 0;
+
     unsigned numArgs = argsList->size();
     if (numArgs < 1 || numArgs > 3)
         return 0;
