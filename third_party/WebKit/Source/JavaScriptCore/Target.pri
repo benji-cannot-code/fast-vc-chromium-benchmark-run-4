bd33455775a72927a7f1aef04a34e3b7d4261310
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-25 22:34:43 +0000

    The redundant phi elimination phase is not used and should be removed https://bugs.webkit.org/show_bug.cgi?id=95006
    Reviewed by Dan Bernstein.
    
    Just removing dead code.
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * Target.pri:
    * dfg/DFGDriver.cpp:
    * dfg/DFGRedundantPhiEliminationPhase.cpp: Removed.
    * dfg/DFGRedundantPhiEliminationPhase.h: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126689 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/Target.pri
+++ third_party/WebKit/Source/JavaScriptCore/Target.pri
@@ -122,7 +122,6 @@ SOURCES += \
     dfg/DFGOSRExitCompiler32_64.cpp \
     dfg/DFGPhase.cpp \
     dfg/DFGPredictionPropagationPhase.cpp \
-    dfg/DFGRedundantPhiEliminationPhase.cpp \
     dfg/DFGRepatch.cpp \
     dfg/DFGSpeculativeJIT.cpp \
     dfg/DFGSpeculativeJIT32_64.cpp \
--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGDriver.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGDriver.cpp
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DFGFixupPhase.h"
 #include "DFGJITCompiler.h"
 #include "DFGPredictionPropagationPhase.h"
-#include "DFGRedundantPhiEliminationPhase.h"
 #include "DFGStructureCheckHoistingPhase.h"
 #include "DFGValidate.h"
 #include "DFGVirtualRegisterAllocationPhase.h"
