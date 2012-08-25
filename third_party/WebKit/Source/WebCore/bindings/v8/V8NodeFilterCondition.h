76be0af58b2053f378832060d8fd4776611ba64f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-25 23:06:24 +0000

    [V8] V8NodeFilterCondition should use ScopedPersistent https://bugs.webkit.org/show_bug.cgi?id=95010
    Reviewed by Eric Seidel.
    
    V8NodeFilterCondition just re-implements ScopedPersistent by hand.
    
    * bindings/v8/V8NodeFilterCondition.cpp:
    (WebCore::V8NodeFilterCondition::V8NodeFilterCondition):
    (WebCore::V8NodeFilterCondition::~V8NodeFilterCondition):
    (WebCore::V8NodeFilterCondition::acceptNode):
    * bindings/v8/V8NodeFilterCondition.h:
    (WebCore):
    (V8NodeFilterCondition):
    (WebCore::V8NodeFilterCondition::create):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8NodeFilterCondition.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8NodeFilterCondition.h
@@ -33,32 +33,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define V8NodeFilterCondition_h
 
 #include "NodeFilterCondition.h"
+#include "ScopedPersistent.h"
 #include <v8.h>
 #include <wtf/PassRefPtr.h>
 
-// NodeFilter is a JavaScript function that takes a Node as parameter and returns a short (ACCEPT, SKIP, REJECT) as the result.
 namespace WebCore {
 
-    class Node;
-    class ScriptState;
+class Node;
+class ScriptState;
 
-    // NodeFilterCondition is a wrapper around a NodeFilter JS function.
-    class V8NodeFilterCondition : public NodeFilterCondition {
-    public:
-        static PassRefPtr<V8NodeFilterCondition> create(v8::Handle<v8::Value> filter)
-        {
-            return adoptRef(new V8NodeFilterCondition(filter));
-        }
+class V8NodeFilterCondition : public NodeFilterCondition {
+public:
+    static PassRefPtr<V8NodeFilterCondition> create(v8::Handle<v8::Value> filter)
+    {
+        return adoptRef(new V8NodeFilterCondition(filter));
+    }
 
-        virtual ~V8NodeFilterCondition();
+    virtual ~V8NodeFilterCondition();
 
-        virtual short acceptNode(ScriptState*, Node*) const;
+    virtual short acceptNode(ScriptState*, Node*) const;
 
-    private:
-        explicit V8NodeFilterCondition(v8::Handle<v8::Value> filter);
+private:
+    explicit V8NodeFilterCondition(v8::Handle<v8::Value> filter);
 
-        mutable v8::Persistent<v8::Value> m_filter;
-    };
+    ScopedPersistent<v8::Value> m_filter;
+};
 
 } // namespace WebCore
 
