eb3973a813d4254e86312d0d3be2a7af60fe6274
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-25 01:51:13 +0000

    Make glGetQueryObjectuivEXT return true for GL_QUERY_RESULT_AVAILABLE_EXT
    This is so a client waiting for the result to be available
    does not wait forever if the context is lost.
    
    BUG=144731
    
    
    Review URL: https://chromiumcodereview.appspot.com/10883036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153376 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/cmd_buffer_helper_test.cc
+++ gpu/command_buffer/client/cmd_buffer_helper_test.cc
@@ -353,4 +353,10 @@ TEST_F(CommandBufferHelperTest, Noop) {
   }
 }
 
+TEST_F(CommandBufferHelperTest, IsContextLost) {
+  EXPECT_FALSE(helper_->IsContextLost());
+  command_buffer_->SetParseError(error::kGenericError);
+  EXPECT_TRUE(helper_->IsContextLost());
+}
+
 }  // namespace gpu
