3f50128f37729911354f215fd68a348c0c29ed41
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-25 00:13:19 +0000

    Unify Number to StringImpl conversion https://bugs.webkit.org/show_bug.cgi?id=94879
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-08-24
    Reviewed by Geoffrey Garen.
    
    Source/JavaScriptCore: 
    
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.def:
    * runtime/UString.cpp:
    * runtime/UString.h:
    (JSC::UString::number):
    Update UString to directly use the common NumberToString implementation.
    
    Source/WebKit2: 
    
    * win/WebKit2.def: Update the exported symbols.
    
    Source/WTF: 
    
    Previously, UString::number() and String::number() used different implementations.
    
    WTF::String::number() was simply forwarding to String::format().
    UString::number() had an optimized version of the conversion.
    
    This patch replace both implementation by a new version, faster than the two previous versions.
    
    The new functions numberToStringImpl improvements are:
    -about 3 times faster than String::number().
    -14% faster than UString::number() on signed numbers.
    -9% faster than UString::number() on unsigned numbers.
    
    * GNUmakefile.list.am:
    * WTF.gypi:
    * WTF.pro:
    * WTF.vcproj/WTF.vcproj:
    * WTF.xcodeproj/project.pbxproj:
    * wtf/CMakeLists.txt:
    * wtf/text/IntegerToStringConversion.cpp: Added.
    (WTF::numberToStringImplSigned):
    (WTF::numberToStringImpl):
    (WTF::numberToStringImplUnsigned):
    * wtf/text/IntegerToStringConversion.h: Added.
    * wtf/text/WTFString.cpp:
    (WTF::String::format):
    * wtf/text/WTFString.h:
    (WTF::String::number):
    
    Tools: 
    
    Add testing for the new IntegerToStringConversion.
    
    * TestWebKitAPI/CMakeLists.txt:
    * TestWebKitAPI/GNUmakefile.am:
    * TestWebKitAPI/TestWebKitAPI.xcodeproj/project.pbxproj:
    * TestWebKitAPI/Tests/WTF/IntegerToStringConversion.cpp: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/UString.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/UString.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 1999-2000 Harri Porten (porten@kde.org)
- * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2004, 2005, 2006, 2007, 2008, 2009, 2012 Apple Inc. All rights reserved.
  * Copyright (C) 2009 Google Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UString_h
 #define UString_h
 
+#include <wtf/text/IntegerToStringConversion.h>
 #include <wtf/text/StringImpl.h>
 
 namespace JSC {
@@ -113,10 +114,10 @@ public:
         return m_impl->characters16()[index];
     }
 
-    JS_EXPORT_PRIVATE static UString number(int);
-    JS_EXPORT_PRIVATE static UString number(unsigned);
-    JS_EXPORT_PRIVATE static UString number(long);
-    static UString number(long long);
+    static UString number(int i) { return WTF::numberToStringImpl(i); }
+    static UString number(unsigned u) { return WTF::numberToStringImpl(u); }
+    static UString number(long i) { return WTF::numberToStringImpl(i); }
+    static UString number(long long i) { return WTF::numberToStringImpl(i); }
     JS_EXPORT_PRIVATE static UString number(double);
 
     // Find a single character or string, also with match function & latin1 forms.
