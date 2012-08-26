eb0dbf7fbb1e327934a142fb06d668e815c7f9a6
anilsson@rim.com <anilsson@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-26 13:48:14 +0000

    [BlackBerry] ASSERT failed in WebPagePrivate::commitRootLayerIfNeeded() https://bugs.webkit.org/show_bug.cgi?id=94921
    PR 104676
    As visibility changed due to WebPage::setVisible(true),
    resumeRootLayerCommit() would call commitRootLayerIfNeeded(), without
    layouting or rendering. If a one shot drawing sync was pending at this
    time, it would cause the condition
    ASSERT(!needsOneShotDrawingSynchronization()) to fail.
    
    The comment immediately before the assert states that "In case of one
    shot drawing synchronization, you should first layoutIfNeeded, render,
    then commit and draw the layers". However, resumeRootLayerCommit() did
    no such thing.
    
    Fixed by removing the call to commitRootLayerIfNeeded() from
    resumeRootLayerCommit(), because the latter is only called in a code
    path where we're going to layout, render and then commit anyway. These
    operations will be performed from resumeBackingStore() which is called
    at the end of setVisible().
    
    Reviewed by George Staikos.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::resumeRootLayerCommit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -5896,9 +5896,6 @@ void WebPagePrivate::resumeRootLayerCommit()
 
     m_suspendRootLayerCommit = false;
     m_needsCommit = true;
-
-    // Recreate layer resources if needed.
-    commitRootLayerIfNeeded();
 }
 
 bool WebPagePrivate::needsOneShotDrawingSynchronization()
