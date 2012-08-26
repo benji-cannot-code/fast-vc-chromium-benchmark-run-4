c81d062582262f5b08070ded84883ac273b04200
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-26 18:23:54 +0000

    Don't allocate space for arguments and call frame if arguments aren't captured https://bugs.webkit.org/show_bug.cgi?id=95024
    Reviewed by Geoffrey Garen.
    
    Test coverage for the case where an argument was legitimately captured.
    
    * fast/js/jsc-test-list:
    * fast/js/legitimately-captured-argument-expected.txt: Added.
    * fast/js/legitimately-captured-argument.html: Added.
    * fast/js/script-tests/legitimately-captured-argument.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/legitimately-captured-argument.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Tests that using an argument as a captured variable, in the legitimate sense rather than the function.arguments sense, works as expected."
+);
+
+function makeCounter(x) {
+    return function() {
+        return ++x;
+    };
+}
+
+for (var i = 0; i < 100; ++i) {
+    var counter = makeCounter(i);
+    for (var j = 0; j < 10; ++j)
+        shouldBe("counter()", "" + (i + j + 1));
+}
