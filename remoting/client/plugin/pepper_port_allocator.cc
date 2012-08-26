00609df4ebd3dae649d605ec6cbaf6afbddf139e
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-26 12:17:31 +0000

    Updating chrome to libjingle r175
    Review URL: https://chromiumcodereview.appspot.com/10882037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153413 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/pepper_port_allocator.cc
+++ remoting/client/plugin/pepper_port_allocator.cc
@@ -129,7 +129,8 @@ void PepperPortAllocatorSession::ConfigReady(
 void PepperPortAllocatorSession::GetPortConfigurations() {
   // Add an empty configuration synchronously, so a local connection
   // can be started immediately.
-  ConfigReady(new cricket::PortConfiguration(talk_base::SocketAddress()));
+  ConfigReady(new cricket::PortConfiguration(
+      talk_base::SocketAddress(), "", ""));
 
   ResolveStunServerAddress();
   TryCreateRelaySession();
@@ -192,7 +193,7 @@ void PepperPortAllocatorSession::OnStunAddressResolved(int32_t result) {
     ReceiveSessionResponse(std::string(relay_response_body_.begin(),
                                        relay_response_body_.end()));
   } else {
-    ConfigReady(new cricket::PortConfiguration(stun_address_));
+    ConfigReady(new cricket::PortConfiguration(stun_address_, "", ""));
   }
 }
 
