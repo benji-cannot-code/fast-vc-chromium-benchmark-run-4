290d28da25aa199e0653824ef7d5069680cc50b3
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-26 11:54:38 +0000

    Removes two properties that are defined else where. With these we get link warnings.
    BUG=144155
    TEST=none
    R=oshima@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10871076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153398 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_constants.cc
+++ ash/ash_constants.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/aura/window_property.h"
 
-DECLARE_EXPORTED_WINDOW_PROPERTY_TYPE(ASH_EXPORT, bool)
-
 namespace ash {
 
 DEFINE_WINDOW_PROPERTY_KEY(bool, kConstrainedWindowKey, false);
--- ui/views/widget/desktop_native_widget_helper_aura.cc
+++ ui/views/widget/desktop_native_widget_helper_aura.cc
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/x11_window_event_filter.h"
 #endif
 
-DECLARE_WINDOW_PROPERTY_TYPE(aura::Window*);
-
 namespace views {
 
 DEFINE_WINDOW_PROPERTY_KEY(
