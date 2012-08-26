9ebefbde4143c410089224846e08303d82fdfcd7
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-26 03:58:21 +0000

    2012-08-25  Geoffrey Garen  <ggaren@apple.com>
            Try a little harder to fix the Linux build.
    
            * runtime/JSActivation.cpp:
            * runtime/JSActivation.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSActivation.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSActivation.h
@@ -31,9 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define JSActivation_h
 
 #include "CodeBlock.h"
+#include "CopiedSpaceInlineMethods.h"
 #include "JSVariableObject.h"
-#include "SymbolTable.h"
 #include "Nodes.h"
+#include "SymbolTable.h"
 
 namespace JSC {
 
