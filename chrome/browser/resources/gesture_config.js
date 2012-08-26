96e31b99335973f450ea9f86fbdac914134077b0
rjkroege@chromium.org <rjkroege@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-26 11:54:13 +0000

    Slow touchscreen acceleration model and adjust default multiplier.
    Touchscreen fling acceleration is excessive. Reduce and make eaiser to
    control.
    
    BUG=144674
    
    
    Review URL: https://chromiumcodereview.appspot.com/10879070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153396 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gesture_config.js
+++ chrome/browser/resources/gesture_config.js
@@ -130,7 +130,7 @@ var gesture_config = (function() {
       key: 'touchscreen_fling_acceleration_adjustment',
       label: 'Touchscreen Fling Acceleration Adjustment',
       units: 'pixels/sec.',
-      default: 1 / 900
+      default: 1
     }
   ];
 
