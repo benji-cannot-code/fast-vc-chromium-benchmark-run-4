bebaf3e88503995f0748a0d6d504de650ab3b9e5
enne@google.com <enne@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-27 21:20:43 +0000

    [chromium] Unreviewed, remove OVERRIDE from non-virtual function. https://bugs.webkit.org/show_bug.cgi?id=94859
    Unreviewed build fix.
    
    * platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.h:
    (CCScrollbarGeometryFixedThumb):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126794 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.h
@@ -41,7 +41,7 @@ public:
     virtual ~CCScrollbarGeometryFixedThumb();
 
     // Update thumb length from scrollbar
-    void update(WebKit::WebScrollbar*) OVERRIDE;
+    void update(WebKit::WebScrollbar*);
 
     // WebScrollbarThemeGeometry interface
     virtual WebKit::WebScrollbarThemeGeometry* clone() const OVERRIDE;
