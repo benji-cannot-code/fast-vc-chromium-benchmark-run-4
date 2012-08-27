2f489b0408586f2467269a8c001c08100484aa80
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 23:16:25 +0000

    Remove MediaDeviceMapService references from |DeviceAttachedIntentSource::OnMediaDetached| function.
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10868110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intents/device_attached_intent_source.cc
+++ chrome/browser/intents/device_attached_intent_source.cc
@@ -24,13 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/isolated_context.h"
 #include "webkit/glue/web_intent_data.h"
 #include "webkit/glue/web_intent_service_data.h"
-#include "webkit/fileapi/media/media_file_system_config.h"
-
-#if defined(SUPPORT_MEDIA_FILESYSTEM)
-#include "webkit/fileapi/media/media_device_map_service.h"
-
-using fileapi::MediaDeviceMapService;
-#endif
 
 using base::SystemMonitor;
 using chrome::MediaStorageUtil;
@@ -140,6 +133,8 @@ void DeviceAttachedIntentSource::DispatchIntentsForService(
   const FilePath device_path(device_info.location);
 
   // TODO(kinuko, kmadhusu): Use a different file system type for MTP.
+  // TODO(kmadhusu): To manage the registered file systems efficiently, register
+  // the attached device media file system using MediaFileSystemRegistry.
   const std::string fs_id = fileapi::IsolatedContext::GetInstance()->
       RegisterFileSystemForPath(fileapi::kFileSystemTypeNativeMedia,
                                 device_path, &device_name);
@@ -158,19 +153,7 @@ void DeviceAttachedIntentSource::OnRemovableStorageDetached(
   if (it == device_id_map_.end())
     return;
 
-  // TODO(kmadhusu) This should be something like
-  // RevokeFileSystemByDevice(std::string)
   FilePath path(it->second.location);
   fileapi::IsolatedContext::GetInstance()->RevokeFileSystemByPath(path);
-
-#if defined(SUPPORT_MEDIA_FILESYSTEM)
-  // TODO(kmadhusu, vandebo): Clean up this code. http://crbug.com/140340.
-  MediaStorageUtil::Type type;
-  MediaStorageUtil::CrackDeviceId(it->second.device_id, &type, NULL);
-  if (type == MediaStorageUtil::USB_MTP) {
-    MediaDeviceMapService::GetInstance()->RemoveMediaDevice(
-        it->second.location);
-  }
-#endif
   device_id_map_.erase(it);
 }
