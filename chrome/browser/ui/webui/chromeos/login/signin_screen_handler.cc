9741ef980df12aaff5a11e082f5da76a61ad5c61
glotov@chromium.org <glotov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 18:02:34 +0000

    Add button tooltip is different for enterprise owned device.
    BUG=122770
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10876085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153506 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/webui_login_display.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/io_thread.h"
+#include "chrome/browser/policy/browser_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
@@ -362,7 +363,10 @@ void SigninScreenHandler::GetLocalizedStrings(
   localized_strings->SetString("removeUser",
       l10n_util::GetStringUTF16(IDS_LOGIN_REMOVE));
   localized_strings->SetString("disabledAddUserTooltip",
-      l10n_util::GetStringUTF16(IDS_DISABLED_ADD_USER_TOOLTIP));
+      l10n_util::GetStringUTF16(
+          g_browser_process->browser_policy_connector()->IsEnterpriseManaged() ?
+            IDS_DISABLED_ADD_USER_TOOLTIP_ENTERPRISE :
+            IDS_DISABLED_ADD_USER_TOOLTIP));
 
   if (chromeos::KioskModeSettings::Get()->IsKioskModeEnabled()) {
     localized_strings->SetString("demoLoginMessage",
@@ -847,6 +851,7 @@ void SigninScreenHandler::HandleRemoveUser(const base::ListValue* args) {
   }
 
   delegate_->RemoveUser(email);
+  UpdateAddButtonStatus();
 }
 
 void SigninScreenHandler::HandleShowAddUser(const base::ListValue* args) {
