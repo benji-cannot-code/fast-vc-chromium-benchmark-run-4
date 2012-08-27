52e2314d214ea200dd11abfe960065b212b9e1ce
annacc@chromium.org <annacc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-27 20:12:39 +0000

    TextTrack modes are strings https://bugs.webkit.org/show_bug.cgi?id=85050
    Reviewed by Eric Carlson.
    
    The spec recently changed away from enumeration to string for the type
    of TextTrack mode.
    http://dev.w3.org/html5/spec/media-elements.html#texttrackmode
    
    Source/WebCore:
    
    No new tests. Updates to existing tests.
    
    * html/HTMLMediaElement.cpp:
    
    Use the new string mode rather than the old enum.  Also, TextTrack::setMode()
    no longer requires an ExceptionCode:
    (WebCore::HTMLMediaElement::loadInternal):
    (WebCore::HTMLMediaElement::textTrackModeChanged):
    (WebCore::HTMLMediaElement::textTrackKindChanged):
    (WebCore::HTMLMediaElement::addTextTrack):
    (WebCore::HTMLMediaElement::showingTrackWithSameKind):
    (WebCore::HTMLMediaElement::configureTextTrackGroup):
    (WebCore::HTMLMediaElement::configureNewTextTracks):
    (WebCore::HTMLMediaElement::configureTextTrackDisplay):
    
    * html/track/TextTrack.cpp:
    
    Update these methods to use ConstructFromLiteral for efficiency:
    (WebCore::TextTrack::subtitlesKeyword):
    (WebCore::TextTrack::captionsKeyword):
    (WebCore::TextTrack::descriptionsKeyword):
    (WebCore::TextTrack::chaptersKeyword):
    (WebCore::TextTrack::metadataKeyword):
    
    New methods to use for the mode string (replacing the enum):
    (WebCore::TextTrack::disabledKeyword):
    (WebCore::TextTrack::hiddenKeyword):
    (WebCore::TextTrack::showingKeyword):
    
    (WebCore::TextTrack::TextTrack): Initialize mode to disabled.
    (WebCore::TextTrack::setMode): Remove ExceptionCode and no longer throw an
    INVALID_ACCESS_ERR.
    (WebCore::TextTrack::mode): Return a keyword string instead of an enum.
    
    Use the new string mode rather than the old enum:
    (WebCore::TextTrack::cues):
    (WebCore::TextTrack::activeCues):
    (WebCore::TextTrack::isRendered):
    
    * html/track/TextTrack.h: Remove old enum, add new string keyword methods,
    and update mode() and setMode() accordingly.
    
    * html/track/TextTrack.idl: Remove old enum and make mode attribute be a string.
    
    Use the new string mode rather than the old enum:
    * html/track/TextTrackCue.cpp:
    (WebCore::TextTrackCue::dispatchEvent):
    (WebCore::TextTrackCue::isActive):
    
    LayoutTests:
    
    Remove check for mode constants as we are no longer using an enum:
    * media/track/track-constants-expected.txt:
    * media/track/track-constants.html:
    
    Update tests to use the new strings rather than the enums:
    * media/track/track-delete-during-setup-expected.txt:
    * media/track/track-delete-during-setup.html:
    * media/track/track-element-dom-change-crash-expected.txt:
    * media/track/track-element-dom-change-crash.html:
    * media/track/track-load-from-src-readyState.html:
    * media/track/track-mode-expected.txt:
    * media/track/track-mode-not-changed-by-new-track-expected.txt:
    * media/track/track-mode-not-changed-by-new-track.html:
    * media/track/track-mode-triggers-loading-expected.txt:
    * media/track/track-mode-triggers-loading.html:
    * media/track/track-mode.html:
    * media/track/track-texttracks-expected.txt:
    * media/track/track-texttracks.html:
    * media/video-test.js:
    (enableAllTextTracks):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
@@ -778,7 +778,7 @@ void HTMLMediaElement::loadInternal()
         if (m_textTracks) {
             for (unsigned i = 0; i < m_textTracks->length(); ++i) {
                 TextTrack* track = m_textTracks->item(i);
-                if (track->mode() != TextTrack::DISABLED)
+                if (track->mode() != TextTrack::disabledKeyword())
                     m_textTracksWhenResourceSelectionBegan.append(track);
             }
         }
@@ -1296,7 +1296,7 @@ void HTMLMediaElement::textTrackModeChanged(TextTrack* track)
             
             // Mark this track as "configured" so configureNewTextTracks won't change the mode again.
             trackElement->setHasBeenConfigured(true);
-            if (track->mode() != TextTrack::DISABLED) {
+            if (track->mode() != TextTrack::disabledKeyword()) {
                 if (trackElement->readyState() == HTMLTrackElement::LOADED)
                     textTrackAddCues(track, track->cues());
                 else if (trackElement->readyState() == HTMLTrackElement::NONE)
@@ -1315,8 +1315,8 @@ void HTMLMediaElement::textTrackModeChanged(TextTrack* track)
 
 void HTMLMediaElement::textTrackKindChanged(TextTrack* track)
 {
-    if (track->kind() != TextTrack::captionsKeyword() && track->kind() != TextTrack::subtitlesKeyword() && track->mode() == TextTrack::SHOWING)
-        track->setMode(TextTrack::HIDDEN, ASSERT_NO_EXCEPTION);
+    if (track->kind() != TextTrack::captionsKeyword() && track->kind() != TextTrack::subtitlesKeyword() && track->mode() == TextTrack::showingKeyword())
+        track->setMode(TextTrack::hiddenKeyword());
 }
 
 void HTMLMediaElement::textTrackAddCues(TextTrack*, const TextTrackCueList* cues) 
@@ -2689,7 +2689,7 @@ PassRefPtr<TextTrack> HTMLMediaElement::addTextTrack(const String& kind, const S
     textTrack->setReadinessState(TextTrack::Loaded);
 
     // ... its text track mode to the text track hidden mode, and its text track list of cues to an empty list ...
-    textTrack->setMode(TextTrack::HIDDEN, ec);
+    textTrack->setMode(TextTrack::hiddenKeyword());
 
     return textTrack.release();
 }
@@ -2714,7 +2714,7 @@ HTMLTrackElement* HTMLMediaElement::showingTrackWithSameKind(HTMLTrackElement* t
             continue;
 
         HTMLTrackElement* showingTrack = static_cast<HTMLTrackElement*>(node);
-        if (showingTrack->kind() == trackElement->kind() && showingTrack->track()->mode() == TextTrack::SHOWING)
+        if (showingTrack->kind() == trackElement->kind() && showingTrack->track()->mode() == TextTrack::showingKeyword())
             return showingTrack;
     }
     
@@ -2865,10 +2865,9 @@ void HTMLMediaElement::configureTextTrackGroup(const TrackGroup& group) const
     for (size_t i = 0; i < group.tracks.size(); ++i) {
         HTMLTrackElement* trackElement = group.tracks[i];
         RefPtr<TextTrack> textTrack = trackElement->track();
-        ExceptionCode unusedException;
         
         if (trackElementToEnable == trackElement) {
-            textTrack->setMode(TextTrack::SHOWING, unusedException);
+            textTrack->setMode(TextTrack::showingKeyword());
             if (defaultTrack == trackElement)
                 textTrack->setShowingByDefault(true);
         } else {
@@ -2877,18 +2876,17 @@ void HTMLMediaElement::configureTextTrackGroup(const TrackGroup& group) const
                 // mode is showing by default, the user agent must furthermore change that text track's
                 // text track mode to hidden.
                 textTrack->setShowingByDefault(false);
-                textTrack->setMode(TextTrack::HIDDEN, unusedException);
+                textTrack->setMode(TextTrack::hiddenKeyword());
             } else
-                textTrack->setMode(TextTrack::DISABLED, unusedException);
+                textTrack->setMode(TextTrack::disabledKeyword());
         }
     }
 
     if (trackElementToEnable && group.defaultTrack && group.defaultTrack != trackElementToEnable) {
         RefPtr<TextTrack> textTrack = group.defaultTrack->track();
         if (textTrack && textTrack->showingByDefault()) {
-            ExceptionCode unusedException;
             textTrack->setShowingByDefault(false);
-            textTrack->setMode(TextTrack::HIDDEN, unusedException);
+            textTrack->setMode(TextTrack::hiddenKeyword());
         }
     }
 }
@@ -2923,7 +2921,7 @@ void HTMLMediaElement::configureNewTextTracks()
         else
             currentGroup = &otherTracks;
 
-        if (!currentGroup->visibleTrack && textTrack->mode() == TextTrack::SHOWING)
+        if (!currentGroup->visibleTrack && textTrack->mode() == TextTrack::showingKeyword())
             currentGroup->visibleTrack = trackElement;
         if (!currentGroup->defaultTrack && trackElement->isDefault())
             currentGroup->defaultTrack = trackElement;
@@ -4094,7 +4092,7 @@ void HTMLMediaElement::configureTextTrackDisplay()
 
     bool haveVisibleTextTrack = false;
     for (unsigned i = 0; i < m_textTracks->length(); ++i) {
-        if (m_textTracks->item(i)->mode() == TextTrack::SHOWING) {
+        if (m_textTracks->item(i)->mode() == TextTrack::showingKeyword()) {
             haveVisibleTextTrack = true;
             break;
         }
