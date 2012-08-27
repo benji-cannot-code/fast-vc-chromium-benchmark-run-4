61371960a4cb54312feb6798f45d92b0e71ac6f1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-27 02:49:36 +0000

    [chromium] Fix a bug where CCThreadProxy::canDraw() gets stuck at false on tab switch https://bugs.webkit.org/show_bug.cgi?id=94903
    Patch by Christopher Cameron <ccameron@chromium.org> on 2012-08-26
    Reviewed by James Robinson.
    
    Suppose the impl thread deletes all textures via
    releaseContentsTextures().  The impl thread will not be able to draw
    again until resetContentsTexturesPurged() is called in
    scheduledActionCommit().  When deleting the textures, the function
    releaseContentsTextures() calls setNeedsCommitOnImplThread() to ensure
    that a commit will come along to allow drawing again.  If this commit
    is aborted, then the page will not draw until a commit is scheduled,
    which may be never.
    
    Make beginFrameAborted() call setNeedsCommit(), so that the requested
    commit will eventually occur.
    
    No new tests, update to CCSchedulerStateMachineTest's
    TestGoesInvisibleBeforeBeginFrameCompletes to require new
    functionality (new test fails with old behavior).
    
    * platform/graphics/chromium/cc/CCSchedulerStateMachine.cpp:
    (WebCore::CCSchedulerStateMachine::beginFrameAborted):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCSchedulerStateMachine.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCSchedulerStateMachine.cpp
@@ -304,6 +304,7 @@ void CCSchedulerStateMachine::beginFrameAborted()
 {
     ASSERT(m_commitState == COMMIT_STATE_FRAME_IN_PROGRESS);
     m_commitState = COMMIT_STATE_IDLE;
+    setNeedsCommit();
 }
 
 void CCSchedulerStateMachine::beginUpdateMoreResourcesComplete(bool morePending)
