bf0282931e9026c9df04f1a18796f20dd7bd9e1c
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 21:51:23 +0000

    Move more code from NWW to HWNDMessageHandler.
    . Show() variants
    . SetInitialFocus()
    . SetWindowTitle()
    . Accessibility setters.
    . WindowImpl overrides
    . Activate/Focus restoration
    . Close()
    . Modality
    
    http://crbug.com/142962
    TBR=sky@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10870110
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153551 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/win/hwnd_message_handler.h
+++ ui/views/win/hwnd_message_handler.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "base/string16.h"
 #include "ui/base/accessibility/accessibility_types.h"
 #include "ui/base/ui_base_types.h"
 #include "ui/gfx/rect.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace gfx {
 class Canvas;
+class ImageSkia;
 class Insets;
 }
 
@@ -50,6 +52,7 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
   void Init(const gfx::Rect& bounds);
   void InitModalType(ui::ModalType modal_type);
 
+  void Close();
   void CloseNow();
 
   gfx::Rect GetWindowBoundsInScreen() const;
@@ -68,6 +71,10 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
   void StackAbove(HWND other_hwnd);
   void StackAtTop();
 
+  void Show();
+  void ShowWindowWithState(ui::WindowShowState show_state);
+  // TODO(beng): distinguish from ShowWindowWithState().
+  void Show(int show_state);
   void ShowMaximizedWithBounds(const gfx::Rect& bounds);
   void Hide();
 
@@ -86,6 +93,7 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
   bool IsMaximized() const;
 
   bool RunMoveLoop(const gfx::Point& drag_offset);
+  void EndMoveLoop();
 
   // Tells the HWND its client area has changed.
   void SendFrameChanged();
@@ -105,6 +113,11 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
 
   InputMethod* CreateInputMethod();
 
+  void SetTitle(const string16& title);
+
+  void SetAccessibleName(const string16& name);
+  void SetAccessibleRole(ui::AccessibilityTypes::Role role);
+  void SetAccessibleState(ui::AccessibilityTypes::State state);
   void SendNativeAccessibilityEvent(int id,
                                     ui::AccessibilityTypes::Event event_type);
 
@@ -118,6 +131,9 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
   }
   void SchedulePaintInRect(const gfx::Rect& rect);
   void SetOpacity(BYTE opacity);
+
+  void SetWindowIcons(const gfx::ImageSkia& window_icon,
+                      const gfx::ImageSkia& app_icon);
 
   // Message Handlers.
   void OnActivate(UINT action, BOOL minimized, HWND window);
@@ -198,8 +214,24 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
   // TODO(beng): remove once this is the WindowImpl.
   friend class NativeWidgetWin;
 
-  // Overridden from internal::InputMethodDelegate
+  // Overridden from internal::InputMethodDelegate:
   virtual void DispatchKeyEventPostIME(const ui::KeyEvent& key) OVERRIDE;
+
+  // Overridden from WindowImpl:
+  virtual HICON GetDefaultWindowIcon() const;
+  virtual LRESULT OnWndProc(UINT message, WPARAM w_param, LPARAM l_param);
+
+  // Can be called after the delegate has had the opportunity to set focus and
+  // did not do so.
+  void SetInitialFocus();
+
+  // Called after the WM_ACTIVATE message has been processed by the default
+  // windows procedure.
+  void PostProcessActivateMessage(int activation_state);
+
+  // Enables disabled owner windows that may have been disabled due to this
+  // window's modality.
+  void RestoreEnabledIfNecessary();
 
   // Executes the specified SC_command.
   void ExecuteSystemMenuCommand(int command);
@@ -249,7 +281,19 @@ class VIEWS_EXPORT HWNDMessageHandler : public internal::InputMethodDelegate {
 
   scoped_ptr<FullscreenHandler> fullscreen_handler_;
 
+  // The following factory is used for calls to close the NativeWidgetWin
+  // instance.
+  base::WeakPtrFactory<HWNDMessageHandler> close_widget_factory_;
+
   bool remove_standard_frame_;
+
+  // Whether the focus should be restored next time we get enabled.  Needed to
+  // restore focus correctly when Windows modal dialogs are displayed.
+  bool restore_focus_when_enabled_;
+
+  // Whether all ancestors have been enabled. This is only used if is_modal_ is
+  // true.
+  bool restored_enabled_;
 
   // The last cursor that was active before the current one was selected. Saved
   // so that we can restore it.
