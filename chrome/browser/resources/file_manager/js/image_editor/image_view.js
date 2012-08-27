c37bab96e40c160d82eba43cd5126fd09e260b56
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 15:35:47 +0000

    First cut of the updated Photo Editor UI
    Limitations:
    - "mosaic" is a stub based on standard cr.ui.Grid (same as in the Files app thumbnail mode).
    - The new delete button does not really delete the files.
    - Standard (browser-provided) tooltips on the buttons look awful on Aura, need to change the black background.
    
    BUG=142542
    
    Review URL: https://chromiumcodereview.appspot.com/10829421
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153488 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/image_editor/image_view.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_view.js
@@ -496,6 +496,7 @@ ImageView.prototype.changeUrl = function(newUrl) {
  */
 ImageView.prototype.unload = function() {
   this.container_.textContent = '';
+  this.contentCanvas_ = null;
   this.screenImage_ = null;
   this.videoElement_ = null;
 };
