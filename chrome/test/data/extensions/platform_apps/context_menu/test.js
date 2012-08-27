dfe23652f17ee4c8cbb34ab2fc3d0bf6190736fb
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-27 23:48:05 +0000

    Platform app context menu items should not be grouped into a submenu.
    BUG=141898
    
    
    Review URL: https://chromiumcodereview.appspot.com/10832349
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153585 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/context_menu/test.js
+++ chrome/test/data/extensions/platform_apps/context_menu/test.js
@@ -4,11 +4,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.contextMenus.create({
-        id: 'id1',
-        title: 'Extension Item 1',
-      },
-      function() {
-        chrome.app.window.create('main.html', {}, function() {});
-      });
+  chrome.contextMenus.create({title: 'Extension Item 1',
+                             id: 'id1'}, function() {
+    chrome.contextMenus.create({title: 'Extension Item 2',
+                               id: 'id2'}, function() {
+      chrome.app.window.create('main.html', {}, function() {});
+    });
+  });
 });
