a453cc1e963e84b75ea6227272eda774cda896cb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-28 02:00:14 +0000

    [Chromium] Stop texture updates when context is lost. https://bugs.webkit.org/show_bug.cgi?id=94983
    Patch by David Reveman <reveman@chromium.org> on 2012-08-27
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Free m_currentTextureUpdateController when
    CCThreadProxy::didLoseContextOnImplThread() is called.
    
    Unit test: CCLayerTreeHostTestLostContextWhileUpdatingResources.runMultiThread
    
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    (WebCore::CCThreadProxy::didLoseContextOnImplThread):
    (WebCore::CCThreadProxy::scheduledActionCommit):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostTest.cpp:
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::create):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::setContextLostCallback):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::isContextLost):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::beginQueryEXT):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::endQueryEXT):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::getQueryObjectuivEXT):
    (CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext::CompositorFakeWebGraphicsContext3DWithEndQueryCausingLostContext):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources::CCLayerTreeHostTestLostContextWhileUpdatingResources):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources::createOutputSurface):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources::beginTest):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources::commitCompleteOnCCThread):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources::layout):
    (CCLayerTreeHostTestLostContextWhileUpdatingResources::afterTest):
    (TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCThreadProxy.cpp
@@ -315,6 +315,7 @@ void CCThreadProxy::didLoseContextOnImplThread()
 {
     ASSERT(isImplThread());
     TRACE_EVENT0("cc", "CCThreadProxy::didLoseContextOnImplThread");
+    m_currentTextureUpdateControllerOnImplThread.clear();
     m_schedulerOnImplThread->didLoseContext();
 }
 
@@ -620,8 +621,7 @@ void CCThreadProxy::scheduledActionCommit()
 {
     TRACE_EVENT0("cc", "CCThreadProxy::scheduledActionCommit");
     ASSERT(isImplThread());
-    ASSERT(m_currentTextureUpdateControllerOnImplThread);
-    ASSERT(!m_currentTextureUpdateControllerOnImplThread->hasMoreUpdates());
+    ASSERT(!hasMoreResourceUpdates());
     ASSERT(m_commitCompletionEventOnImplThread);
 
     m_currentTextureUpdateControllerOnImplThread.clear();
