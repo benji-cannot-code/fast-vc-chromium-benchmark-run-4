4c6fb817569868f3ecd1fa321060cf52ecd038ec
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 06:17:05 +0000

    Change default values of some gesture related constants
    BUG=139650,145059
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10878085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153634 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/gestures/gesture_recognizer_unittest.cc
+++ ui/aura/gestures/gesture_recognizer_unittest.cc
@@ -533,7 +533,7 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
      EXPECT_FALSE(delegate->long_press());
 
      delegate->Reset();
-     ui::TouchEvent release(ui::ET_TOUCH_RELEASED, gfx::Point(377, 291),
+     ui::TouchEvent release(ui::ET_TOUCH_RELEASED, gfx::Point(367, 291),
                                 kTouchId, press.time_stamp() +
                                 base::TimeDelta::FromMilliseconds(50));
      release.set_radius_x(20);
@@ -551,9 +551,9 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
 
      EXPECT_EQ(1, delegate->tap_count());
      gfx::Point actual_point(delegate->tap_location());
-     EXPECT_EQ(46, delegate->bounding_box().width());
+     EXPECT_EQ(40, delegate->bounding_box().width());
      EXPECT_EQ(40, delegate->bounding_box().height());
-     EXPECT_EQ(373, actual_point.x());
+     EXPECT_EQ(366, actual_point.x());
      EXPECT_EQ(290, actual_point.y());
   }
 
@@ -575,7 +575,7 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
      EXPECT_FALSE(delegate->long_press());
 
      delegate->Reset();
-     ui::TouchEvent move(ui::ET_TOUCH_MOVED, gfx::Point(52, 200),
+     ui::TouchEvent move(ui::ET_TOUCH_MOVED, gfx::Point(49, 204),
                              kTouchId, press.time_stamp() +
                              base::TimeDelta::FromMilliseconds(50));
      move.set_radius_x(8);
@@ -591,7 +591,7 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
      EXPECT_FALSE(delegate->long_press());
 
      delegate->Reset();
-     ui::TouchEvent release(ui::ET_TOUCH_RELEASED, gfx::Point(50, 195),
+     ui::TouchEvent release(ui::ET_TOUCH_RELEASED, gfx::Point(49, 204),
                                 kTouchId, press.time_stamp() +
                                 base::TimeDelta::FromMilliseconds(50));
      release.set_radius_x(4);
@@ -609,10 +609,10 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
 
      EXPECT_EQ(1, delegate->tap_count());
      gfx::Point actual_point(delegate->tap_location());
-     EXPECT_EQ(28, delegate->bounding_box().width());
-     EXPECT_EQ(28, delegate->bounding_box().height());
-     EXPECT_EQ(49, actual_point.x());
-     EXPECT_EQ(200, actual_point.y());
+     EXPECT_EQ(25, delegate->bounding_box().width());
+     EXPECT_EQ(24, delegate->bounding_box().height());
+     EXPECT_EQ(48, actual_point.x());
+     EXPECT_EQ(203, actual_point.y());
   }
 
   // Test with a few ET_TOUCH_MOVED events.
@@ -633,7 +633,7 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
      EXPECT_FALSE(delegate->long_press());
 
      delegate->Reset();
-     ui::TouchEvent move(ui::ET_TOUCH_MOVED, gfx::Point(397, 155),
+     ui::TouchEvent move(ui::ET_TOUCH_MOVED, gfx::Point(397, 151),
                              kTouchId, press.time_stamp() +
                              base::TimeDelta::FromMilliseconds(50));
      move.set_radius_x(13);
@@ -649,7 +649,7 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
      EXPECT_FALSE(delegate->long_press());
 
      delegate->Reset();
-     ui::TouchEvent move1(ui::ET_TOUCH_MOVED, gfx::Point(395, 148),
+     ui::TouchEvent move1(ui::ET_TOUCH_MOVED, gfx::Point(397, 149),
                               kTouchId, move.time_stamp() +
                               base::TimeDelta::FromMilliseconds(50));
      move1.set_radius_x(16);
@@ -699,10 +699,10 @@ TEST_F(GestureRecognizerTest, GestureEventTapRegion) {
 
      EXPECT_EQ(1, delegate->tap_count());
      gfx::Point actual_point(delegate->tap_location());
-     EXPECT_EQ(35, delegate->bounding_box().width());
-     EXPECT_EQ(36, delegate->bounding_box().height());
-     EXPECT_EQ(396, actual_point.x());
-     EXPECT_EQ(149, actual_point.y());
+     EXPECT_EQ(33, delegate->bounding_box().width());
+     EXPECT_EQ(32, delegate->bounding_box().height());
+     EXPECT_EQ(397, actual_point.x());
+     EXPECT_EQ(148, actual_point.y());
   }
 }
 
