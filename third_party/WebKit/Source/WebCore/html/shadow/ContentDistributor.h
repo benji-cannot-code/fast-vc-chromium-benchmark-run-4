aabfeefec7f2efc60fbf7a7d533c0a29104fd66e
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-28 00:14:37 +0000

    Rename ContentDistributor::distributeShadowChildrenTo to distributeNodeChildrenTo. https://bugs.webkit.org/show_bug.cgi?id=95150
    Reviewed by Hajime Morita.
    
    Also, made the function take ContainerNode, which decouples it from the callsite's context.
    
    No new tests, simple rename.
    
    * html/shadow/ContentDistributor.cpp:
    (WebCore::ContentDistributor::distribute): Changed the callsite.
    (WebCore::ContentDistributor::distributeNodeChildrenTo): Renamed from distributeShadowChildrenTo.
    * html/shadow/ContentDistributor.h:
    (WebCore): Added forward decl for ContainerNode.
    (ContentDistributor): Changed the decl name.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/ContentDistributor.h
+++ third_party/WebKit/Source/WebCore/html/shadow/ContentDistributor.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class ContainerNode;
 class Element;
 class InsertionPoint;
 class Node;
@@ -70,7 +71,7 @@ public:
     bool needsInvalidation() const { return m_validity != Invalidated; }
 
     void distributeSelectionsTo(InsertionPoint*, ContentDistribution& pool);
-    void distributeShadowChildrenTo(InsertionPoint*, ShadowRoot*);
+    void distributeNodeChildrenTo(InsertionPoint*, ContainerNode*);
     void invalidateDistributionIn(ContentDistribution*);
 
 private:
