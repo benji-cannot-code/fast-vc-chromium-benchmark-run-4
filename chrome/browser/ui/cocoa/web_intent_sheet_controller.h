d4e54d817ad4869789c9b4d6bcd407f1f26e3fc9
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 21:19:16 +0000

    Unify suggestions/service list.
    R=thakis@chromium.org
    BUG=139026
    
    
    Review URL: https://chromiumcodereview.appspot.com/10882008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/web_intent_sheet_controller.h
+++ chrome/browser/ui/cocoa/web_intent_sheet_controller.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class WebIntentPickerCocoa;
 class WebIntentPickerModel;
-@class SuggestionView;
+@class IntentView;
 
 // Controller for intent picker constrained dialog. This dialog pops up
 // whenever a web page invokes ActivateIntent and lets the user choose which
@@ -31,9 +31,8 @@ class WebIntentPickerModel;
   WebIntentPickerModel* model_;
 
   scoped_nsobject<NSTextField> actionTextField_;
-  scoped_nsobject<SuggestionView> suggestionView_;
+  scoped_nsobject<IntentView> intentView_;
   scoped_nsobject<NSButton> closeButton_;
-  scoped_nsobject<NSMutableArray> intentButtons_;
   scoped_nsobject<NSView> flipView_;
   scoped_nsobject<NSTextField> inlineDispositionTitleField_;
 }
