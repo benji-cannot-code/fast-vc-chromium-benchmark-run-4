5d340e386bb30c5a3ee48ee8f8fe4b12434eedfd
droger@chromium.org <droger@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 10:15:17 +0000

    Add MainHook to net unittests.
    MainHooks are in particular used on iOS to prevent the system from killing the
    test application at startup.
    See https://chromiumcodereview.appspot.com/10690161/.
    
    BUG=b/6785946
    
    Review URL: https://chromiumcodereview.appspot.com/10880069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153650 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/run_all_unittests.cc
+++ net/base/run_all_unittests.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/metrics/statistics_recorder.h"
+#include "base/test/main_hook.h"
 #include "build/build_config.h"
 #include "crypto/nss_util.h"
 #include "net/base/net_test_suite.h"
@@ -20,6 +21,8 @@ using net::internal::ClientSocketPoolBaseHelper;
 using net::SpdySession;
 
 int main(int argc, char** argv) {
+  MainHook hook(main, argc, argv);
+
   // Record histograms, so we can get histograms data in tests.
   base::StatisticsRecorder::Initialize();
 
