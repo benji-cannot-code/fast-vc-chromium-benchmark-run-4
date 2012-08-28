d6ca518d2a9af7ed2a8a9a1008f428a23a13807f
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 03:13:27 +0000

    Basic framework for devtools-based scrolling tests.
    BUG=144483
    
    
    Review URL: https://chromiumcodereview.appspot.com/10875044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153616 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/chrome_remote_control/chrome_remote_control/websocket.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from __future__ import absolute_import
+import os
+import sys
+
+def __init__():
+  ws_path = os.path.join(os.path.dirname(__file__),
+                         "../third_party/websocket-client")
+  assert os.path.exists(os.path.join(ws_path, "websocket.py"))
+  sys.path.append(ws_path)
+
+__init__()
+from websocket import *
