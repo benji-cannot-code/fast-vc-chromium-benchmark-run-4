c9202e5d1d4a8748b4d3118b47fe0901e4ad5b51
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 17:56:19 +0000

    net: disable SPDY compression.
    This change is being landed temporarily on trunk for testing. It'll be
    reverted once the change has be shown to be safe for merging.
    
    BUG=139744
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153697 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_framer_test.cc
+++ net/spdy/spdy_framer_test.cc
@@ -1670,7 +1670,9 @@ TEST_P(SpdyFramerTest, CreateSynStreamUncompressed) {
   }
 }
 
-TEST_P(SpdyFramerTest, CreateSynStreamCompressed) {
+// TODO(agl): reenable this test once disabling SPDY compression has been
+// tested on Canary. See crbug.com/139744.
+TEST_P(SpdyFramerTest, DISABLED_CreateSynStreamCompressed) {
   SpdyFramer framer(spdy_version_);
   framer.set_enable_compression(true);
 
@@ -1858,7 +1860,9 @@ TEST_P(SpdyFramerTest, CreateSynReplyUncompressed) {
   }
 }
 
-TEST_P(SpdyFramerTest, CreateSynReplyCompressed) {
+// TODO(agl): reenable this test once disabling SPDY compression has been
+// tested on Canary. See crbug.com/139744.
+TEST_P(SpdyFramerTest, DISABLED_CreateSynReplyCompressed) {
   SpdyFramer framer(spdy_version_);
   framer.set_enable_compression(true);
 
@@ -2250,7 +2254,9 @@ TEST_P(SpdyFramerTest, CreateHeadersUncompressed) {
   }
 }
 
-TEST_P(SpdyFramerTest, CreateHeadersCompressed) {
+// TODO(agl): reenable this test once disabling SPDY compression has been
+// tested on Canary. See crbug.com/139744.
+TEST_P(SpdyFramerTest, DISABLED_CreateHeadersCompressed) {
   SpdyFramer framer(spdy_version_);
   framer.set_enable_compression(true);
 
