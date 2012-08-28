9c1620363f6b2482f4cf8e24bcac4511c57775eb
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-28 01:02:11 +0000

    [chromium] Add some #includes that were implicitly picked up to CCScrollbarGeometry* https://bugs.webkit.org/show_bug.cgi?id=95158
    Reviewed by Adrienne Walker.
    
    These includes are picked up implicitly, better to #include them explicitly to be more robust to refactors.
    
    * platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.cpp:
    * platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.h:
    * platform/graphics/chromium/cc/CCScrollbarGeometryStub.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126830 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CCScrollbarGeometryFixedThumb.h"
 
+#include <cmath>
 #include <public/WebRect.h>
 #include <public/WebScrollbar.h>
 
--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCScrollbarGeometryFixedThumb.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CCScrollbarGeometryFixedThumb_h
 
 #include "CCScrollbarGeometryStub.h"
+#include "IntSize.h"
 
 namespace WebCore {
 
