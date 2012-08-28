c0edf7781d2cd429ea770489067ed0232d780e93
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 23:31:01 +0000

    First version of "Waiting" dialog for views.
    BUG=137281
    R=sky@chromium.org, gbillock@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10870092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153787 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_controller.cc
+++ chrome/browser/ui/intents/web_intent_picker_controller.cc
@@ -413,7 +413,7 @@ void WebIntentPickerController::OnChooseAnotherService() {
 }
 
 void WebIntentPickerController::OnClosing() {
-  picker_shown_ = false;
+  SetDialogState(kPickerHidden);
   picker_ = NULL;
 }
 
@@ -881,8 +881,7 @@ void WebIntentPickerController::SetDialogState(WebIntentPickerState state) {
 
   switch (state) {
     case kPickerSetup:
-      DCHECK(dialog_state_ == kPickerHidden);
-
+      DCHECK_EQ(dialog_state_, kPickerHidden);
       // Post timer CWS pending
       MessageLoop::current()->PostDelayedTask(FROM_HERE,
           base::Bind(&WebIntentPickerController::OnPickerEvent,
@@ -892,7 +891,7 @@ void WebIntentPickerController::SetDialogState(WebIntentPickerState state) {
       break;
 
     case kPickerWaiting:
-      DCHECK(dialog_state_ == kPickerSetup);
+      DCHECK_EQ(dialog_state_, kPickerSetup);
       // Waiting dialog can be dismissed after minimum wait time.
       MessageLoop::current()->PostDelayedTask(FROM_HERE,
           base::Bind(&WebIntentPickerController::OnPickerEvent,
@@ -902,7 +901,7 @@ void WebIntentPickerController::SetDialogState(WebIntentPickerState state) {
       break;
 
     case kPickerWaitLong:
-      DCHECK(dialog_state_ == kPickerWaiting);
+      DCHECK_EQ(dialog_state_, kPickerWaiting);
       break;
 
     case kPickerMain:
