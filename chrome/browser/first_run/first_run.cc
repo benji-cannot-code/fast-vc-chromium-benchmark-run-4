0b26c1514ceeddaeef522a907d36c637314c7507
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 23:14:36 +0000

    Don't record browser window display UMA if first run dialog is shown
    Missed this in http://codereview.chromium.org/10662046/ . Don't record the Startup.BrowserWindowDisplay UMA if the first run UI is displayed.
    
    This is the only other place I could find in the code that was displaying a custom dialog on startup on Win/Mac.  The previous CL added this call to the ShowMessageBox() call so all other dialogs should be covered.
    
    BUG=None
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10887006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153770 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run.cc
+++ chrome/browser/first_run/first_run.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/startup_metric_utils.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/installer/util/master_preferences.h"
 #include "chrome/installer/util/master_preferences_constants.h"
@@ -376,6 +377,7 @@ void AutoImportPlatformCommon(
     // The home page string may be set in the preferences, but the user should
     // initially use Chrome with the NTP as home page in organic builds.
     profile->GetPrefs()->SetBoolean(prefs::kHomePageIsNewTabPage, true);
+    startup_metric_utils::SetNonBrowserUIDisplayed();
     ShowFirstRunDialog(profile);
   }
 
