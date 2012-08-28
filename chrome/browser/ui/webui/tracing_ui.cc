5a4753ce36e2527077962c685fd677a651bc83ec
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 09:31:05 +0000

    Escape control characters when sending trace data to JavaScript
    BUG=145115
    
    
    Review URL: https://chromiumcodereview.appspot.com/10882074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153649 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/tracing_ui.cc
+++ chrome/browser/ui/webui/tracing_ui.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/string_number_conversions.h"
+#include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/gpu_blacklist.h"
 #include "chrome/browser/gpu_util.h"
@@ -291,7 +292,8 @@ void ReadTraceFileCallback(TaskProxy* proxy, const FilePath& path) {
 
   // We need to escape the file contents, because it will go into a javascript
   // quoted string in TracingMessageHandler::LoadTraceFileComplete. We need to
-  // escape \ and ' (the only special characters in a ''-quoted string).
+  // escape control characters (to have well-formed javascript statements), as
+  // well as \ and ' (the only special characters in a ''-quoted string).
   // Do the escaping on this thread, it may take a little while for big files
   // and we don't want to block the UI during that time. Also do the UTF-16
   // conversion here.
@@ -304,6 +306,10 @@ void ReadTraceFileCallback(TaskProxy* proxy, const FilePath& path) {
   escaped_contents.reserve(size);
   for (size_t i = 0; i < size; ++i) {
     char c = file_contents[i];
+    if (c < ' ') {
+      escaped_contents += base::StringPrintf("\\u%04x", c);
+      continue;
+    }
     if (c == '\\' || c == '\'')
       escaped_contents.push_back('\\');
     escaped_contents.push_back(c);
