cf483988392b44a0e2090f570196f4d257087857
aestes@apple.com <aestes@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-28 18:25:19 +0000

    Add header guards to WebTileCacheLayer.h. https://bugs.webkit.org/show_bug.cgi?id=95171
    Reviewed by Simon Hausmann.
    
    Headers love include guards.
    
    * platform/graphics/ca/mac/WebTileCacheLayer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/WebTileCacheLayer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/WebTileCacheLayer.h
@@ -23,6 +23,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
+ 
+#ifndef WebTileCacheLayer_h
+#define WebTileCacheLayer_h
 
 #import <QuartzCore/QuartzCore.h>
 #import <wtf/OwnPtr.h>
@@ -42,3 +45,5 @@ namespace WebCore {
 - (void)invalidate;
 
 @end
+
+#endif // WebTileCacheLayer_h
