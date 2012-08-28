e6cf2740646e3830a9316a9058d71b94bf9800da
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-28 04:26:48 +0000

    https://bugs.webkit.org/show_bug.cgi?id=94848 When paged-x/y is specified on the root, columnGap is ignored, and garbage pixels are likely
    Source/WebCore: 
    
    Reviewed by Dan Bernstein.
    
    We used to call setPagination() from applyOverflowToViewport(), but that is too 
    late. We want to setPagination() before we actually lay anything out so that all 
    of the styles (including columnGap!) will be properly set on the RenderView. 
    
    No longer handle pagination here since we take care of it in 
    applyPaginationToViewport()        
    * page/FrameView.cpp:
    (WebCore::FrameView::applyOverflowToViewport):
    
    New function to call setPagination(). This function gets the appropriate renderer 
    and uses its RenderStyle to determine if pagination should be set.
    (WebCore::FrameView::applyPaginationToViewport):
    (WebCore):
    
    Call applyPaginationToViewport() before the call to updateStyleIfNeeded().
    (WebCore::FrameView::layout):
    
    This code was meant to prevent garbage pixels in column gaps for pagination on the 
    RenderView, but paintContents() does not always get called when we paint into 
    tiles, so I moved this code to RenderView.
    (WebCore::FrameView::paintContents):
    * page/FrameView.h:
    (FrameView):
    
    The code from RenderView::paintContents() to prevent garbage pixels is moved here.
    * rendering/RenderView.cpp:
    (WebCore::RenderView::paint):
    
    LayoutTests: 
    
    Reviewed by Dan Bernsetin.
    
    New tests.
    * fast/overflow/paged-x-div-with-column-gap.html: Added.
    * fast/overflow/paged-x-with-column-gap.html: Added.
    * platform/mac/fast/overflow/paged-x-div-with-column-gap-expected.png: Added.
    * platform/mac/fast/overflow/paged-x-div-with-column-gap-expected.txt: Added.
    * platform/mac/fast/overflow/paged-x-with-column-gap-expected.png: Added.
    * platform/mac/fast/overflow/paged-x-with-column-gap-expected.txt: Added.
    
    Updated results needed for Chromium.
    * platform/chromium/TestExpectations:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.h
+++ third_party/WebKit/Source/WebCore/page/FrameView.h
@@ -383,6 +383,7 @@ private:
     bool contentsInCompositedLayer() const;
 
     void applyOverflowToViewport(RenderObject*, ScrollbarMode& hMode, ScrollbarMode& vMode);
+    void applyPaginationToViewport();
 
     void updateOverflowStatus(bool horizontalOverflow, bool verticalOverflow);
 
