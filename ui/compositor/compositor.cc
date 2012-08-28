f916f45cc9a9c07cca2f48f2e31033f6f5089bca
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-28 01:00:07 +0000

    Remove transitional WEBLAYERTREEVIEW_IS_PURE_VIRTUAL guarded code
    Depends on http://trac.webkit.org/changeset/126652
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10873073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153598 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -155,14 +155,9 @@ Compositor::Compositor(CompositorDelegate* delegate,
       test_compositor_enabled ? kTestRefreshRate : kDefaultRefreshRate;
 
   root_web_layer_->setAnchorPoint(WebKit::WebFloatPoint(0.f, 0.f));
-#if defined(WEBLAYERTREEVIEW_IS_PURE_VIRTUAL)
   host_.reset(WebKit::WebLayerTreeView::create(this, *root_web_layer_,
                                                settings));
   host_->setSurfaceReady();
-#else
-  host_.initialize(this, *root_web_layer_, settings);
-  host_.setSurfaceReady();
-#endif
 }
 
 Compositor::~Compositor() {
@@ -204,11 +199,7 @@ void Compositor::ScheduleDraw() {
     // TODO(nduca): Temporary while compositor calls
     // compositeImmediately() directly.
     layout();
-#if defined(WEBLAYERTREEVIEW_IS_PURE_VIRTUAL)
     host_->composite();
-#else
-    host_.composite();
-#endif
   } else if (delegate_) {
     delegate_->ScheduleDraw();
   }
@@ -240,21 +231,13 @@ void Compositor::Draw(bool force_clear) {
   // TODO(nduca): Temporary while compositor calls
   // compositeImmediately() directly.
   layout();
-#if defined(WEBLAYERTREEVIEW_IS_PURE_VIRTUAL)
   host_->composite();
-#else
-  host_.composite();
-#endif
   if (!g_compositor_thread && !swap_posted_)
     NotifyEnd();
 }
 
 void Compositor::ScheduleFullDraw() {
-#if defined(WEBLAYERTREEVIEW_IS_PURE_VIRTUAL)
   host_->setNeedsRedraw();
-#else
-  host_.setNeedsRedraw();
-#endif
 }
 
 bool Compositor::ReadPixels(SkBitmap* bitmap,
@@ -272,11 +255,7 @@ bool Compositor::ReadPixels(SkBitmap* bitmap,
   bitmap->allocPixels();
   SkAutoLockPixels lock_image(*bitmap);
   unsigned char* pixels = static_cast<unsigned char*>(bitmap->getPixels());
-#if defined(WEBLAYERTREEVIEW_IS_PURE_VIRTUAL)
   if (host_->compositeAndReadback(
-#else
-  if (host_.compositeAndReadback(
-#endif
           pixels, gfx::Rect(new_origin, bounds_in_pixel.size()))) {
     SwizzleRGBAToBGRAAndFlip(pixels, bounds_in_pixel.size());
     return true;
@@ -289,11 +268,7 @@ void Compositor::SetScaleAndSize(float scale, const gfx::Size& size_in_pixel) {
   if (size_in_pixel.IsEmpty() || scale <= 0)
     return;
   size_ = size_in_pixel;
-#if defined(WEBLAYERTREEVIEW_IS_PURE_VIRTUAL)
   host_->setViewportSize(size_in_pixel);
-#else
-  host_.setViewportSize(size_in_pixel);
-#endif
   root_web_layer_->setBounds(size_in_pixel);
 
   if (device_scale_factor_ != scale) {
