aa9fa8b3fee5642bc585e5927f3b094933dc46b3
sschmitz@chromium.org <sschmitz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 17:11:38 +0000

    crbug.com/144000 "1 minutes left" on battery status
    Fixed: Using the "Super L10N-izer" TimeFormat::TimeRemaining(const base::TimeDelta& delta) from src/chrome/common/time_format.h. Using shell delegate mechanism to access it.
    
    BUG=144000
    TEST=Observe battery notification when only 1 minute left.
    (On desktop chrome cycles through simulated battery times.
    Observe notification when < 10% down to 1%)
    
    Review URL: https://chromiumcodereview.appspot.com/10891008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153905 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/shell_delegate_impl.cc
+++ ash/shell/shell_delegate_impl.cc
@@ -148,5 +148,10 @@ void ShellDelegateImpl::HandleMediaPlayPause() {
 void ShellDelegateImpl::HandleMediaPrevTrack() {
 }
 
+string16 ShellDelegateImpl::GetTimeRemainingString(
+    const base::TimeDelta& delta) {
+  return string16();
+}
+
 }  // namespace shell
 }  // namespace ash
