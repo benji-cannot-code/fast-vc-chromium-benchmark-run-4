09113b002f7a2d432ef55b38880d943b352b13e2
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 18:01:09 +0000

    Cleanup references to /data/local/tmp.
    We want to be able to change the TEST_DATA_DIR easily (to /sdcard).
    This CL does not change the actual location.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/10897017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153917 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/flag_changer.py
+++ build/android/pylib/flag_changer.py
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
+import constants
 import traceback
 import warnings
 
 
 # Location where chrome reads command line flags from
-CHROME_COMMAND_FILE = '/data/local/tmp/chrome-command-line'
+CHROME_COMMAND_FILE = constants.TEST_EXECUTABLE_DIR + '/chrome-command-line'
 
 class FlagChanger(object):
   """Changes the flags Chrome runs with.
