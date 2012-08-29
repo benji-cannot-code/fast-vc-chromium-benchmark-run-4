c73fb4e8934cec073665bc3b9de45d0985cf85e7
wjmaclean@chromium.org <wjmaclean@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-29 18:14:01 +0000

    [chromium] Link highlight should clear on page navigation. https://bugs.webkit.org/show_bug.cgi?id=95129
    Reviewed by James Robinson.
    
    Modified WebViewImpl to clear link highlight when navigating to new url.
    
    Source/WebKit/chromium:
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::didCommitLoad):
    
    LayoutTests:
    
    * platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-navigate.html: Added.
    * platform/chromium-linux/compositing/gestures/resources/gesture-tapHighlight-simple-navigate-destination.html: Added.
    * platform/chromium-linux/platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-navigate-expected.png: Added.
    * platform/chromium-linux/platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-navigate-expected.txt: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -3444,6 +3444,8 @@ void WebViewImpl::didCommitLoad(bool* isNewNavigation, bool isNavigationWithinPa
     if (*isNewNavigation && !isNavigationWithinPage)
         m_pageScaleFactorIsSet = false;
 
+    // Make sure link highlight from previous page is cleared.
+    m_linkHighlight.clear();
     m_gestureAnimation.clear();
     resetSavedScrollAndScaleState();
 }
