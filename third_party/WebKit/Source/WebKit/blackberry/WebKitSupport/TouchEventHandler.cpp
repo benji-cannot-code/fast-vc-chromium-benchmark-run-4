6963ef9a93e843fa1ee7df2825e55103b558ccd1
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-29 21:52:04 +0000

    [BlackBerry] Replace blackberry specific HitTestRequest::FingerUp by TouchEvent + Release https://bugs.webkit.org/show_bug.cgi?id=95383
    Reviewed by Rob Buis.
    Patch by Antonio Gomes <agomes@rim.com>
    
    * WebKitSupport/TouchEventHandler.cpp:
    (BlackBerry::WebKit::TouchEventHandler::touchEventCancel):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/TouchEventHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/TouchEventHandler.cpp
@@ -145,7 +145,7 @@ void TouchEventHandler::touchEventCancel()
             && !elementUnderFatFinger->renderer()->style()->affectedByActiveRules())
             break;
 
-        HitTestRequest request(HitTestRequest::FingerUp);
+        HitTestRequest request(HitTestRequest::TouchEvent | HitTestRequest::Release);
         // The HitTestResult point is not actually needed.
         HitTestResult result(IntPoint::zero());
         result.setInnerNode(elementUnderFatFinger);
