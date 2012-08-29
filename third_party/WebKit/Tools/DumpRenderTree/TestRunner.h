1b9046e0168a2770df07cd0fe8ae2ac3cb5913d9
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-29 17:42:16 +0000

    Update TestRunner API for web notifications https://bugs.webkit.org/show_bug.cgi?id=95093 <rdar://problem/12179649>
    Reviewed by Alexey Proskuryakov.
    
    As it turns out the patch for this accidentally got squashed into the commit for
    bug 95099 (r126909). This amendment patch adds some comments about the renaming
    of the older TestRunner API for web notifications.
    
    * DumpRenderTree/TestRunner.h:
    (TestRunner):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127018 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/TestRunner.h
+++ third_party/WebKit/Tools/DumpRenderTree/TestRunner.h
@@ -137,7 +137,9 @@ public:
     JSRetainPtr<JSStringRef> platformName() const;
 #endif
 
+    // Legacy here refers to the old TestRunner API for handling web notifications, not the legacy web notification API.
     void ignoreLegacyWebNotificationPermissionRequests();
+    // Legacy here refers to the old TestRunner API for handling web notifications, not the legacy web notification API.
     void simulateLegacyWebNotificationClick(JSStringRef title);
     void grantWebNotificationPermission(JSStringRef origin);
     void denyWebNotificationPermission(JSStringRef origin);
@@ -419,6 +421,7 @@ private:
     bool m_useDeferredFrameLoading;
     bool m_shouldPaintBrokenImage;
     bool m_shouldStayOnPageAfterHandlingBeforeUnload;
+    // FIXME 81697: This variable most likely will be removed once we have migrated the tests from fast/notifications to http/tests/notifications.
     bool m_areLegacyWebNotificationPermissionRequestsIgnored;
     bool m_customFullScreenBehavior;
 
