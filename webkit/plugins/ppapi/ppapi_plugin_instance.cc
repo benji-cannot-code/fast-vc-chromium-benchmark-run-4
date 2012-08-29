afacf0ab836b500e7fd06bf207231e43c6e4400f
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 15:48:28 +0000

    Add UMA histogram to determine how often Stage3D could be used.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/10875077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153887 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppapi_plugin_instance.cc
+++ webkit/plugins/ppapi/ppapi_plugin_instance.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/ppp_mouse_lock.h"
 #include "ppapi/c/private/pp_content_decryptor.h"
 #include "ppapi/c/private/ppp_instance_private.h"
+#include "ppapi/shared_impl/ppapi_preferences.h"
 #include "ppapi/shared_impl/ppb_input_event_shared.h"
 #include "ppapi/shared_impl/ppb_url_util_shared.h"
 #include "ppapi/shared_impl/ppb_view_shared.h"
@@ -100,6 +101,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/metrics/histogram.h"
+#include "base/win/windows_version.h"
 #include "skia/ext/platform_canvas.h"
 #include "ui/gfx/codec/jpeg_codec.h"
 #include "ui/gfx/gdi_util.h"
@@ -594,6 +596,37 @@ void PluginInstance::InstanceCrashed() {
   delegate()->PluginCrashed(this);
 }
 
+static void SetGPUHistogram(const ::ppapi::Preferences& prefs,
+                            const std::vector<std::string>& arg_names,
+                            const std::vector<std::string>& arg_values) {
+  // Calculate a histogram to let us determine how likely people are to try to
+  // run Stage3D content on machines that have it blacklisted.
+#if defined(OS_WIN)
+  bool needs_gpu = false;
+  bool is_xp = base::win::GetVersion() <= base::win::VERSION_XP;
+
+  for (size_t i = 0; i < arg_names.size(); i++) {
+    if (arg_names[i] == "wmode") {
+      // In theory content other than Flash could have a "wmode" argument,
+      // but that's pretty unlikely.
+      if (arg_values[i] == "direct" || arg_values[i] == "gpu")
+        needs_gpu = true;
+      break;
+    }
+  }
+  // 0 : No 3D content and GPU is blacklisted
+  // 1 : No 3D content and GPU is not blacklisted
+  // 2 : 3D content but GPU is blacklisted
+  // 3 : 3D content and GPU is not blacklisted
+  // 4 : No 3D content and GPU is blacklisted on XP
+  // 5 : No 3D content and GPU is not blacklisted on XP
+  // 6 : 3D content but GPU is blacklisted on XP
+  // 7 : 3D content and GPU is not blacklisted on XP
+  UMA_HISTOGRAM_ENUMERATION("Flash.UsesGPU",
+      is_xp * 4 + needs_gpu * 2 + prefs.is_webgl_supported, 8);
+#endif
+}
+
 bool PluginInstance::Initialize(WebPluginContainer* container,
                                 const std::vector<std::string>& arg_names,
                                 const std::vector<std::string>& arg_values,
@@ -604,6 +637,8 @@ bool PluginInstance::Initialize(WebPluginContainer* container,
   full_frame_ = full_frame;
 
   container_->setIsAcceptingTouchEvents(IsAcceptingTouchEvents());
+
+  SetGPUHistogram(delegate_->GetPreferences(), arg_names, arg_values);
 
   argn_ = arg_names;
   argv_ = arg_values;
