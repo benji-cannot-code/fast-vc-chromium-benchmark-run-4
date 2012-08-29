17aa60d15f129dddd77a947d2117a66d8336709c
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-29 10:15:58 +0000

    [Qt] REGRESSION(r125428): fast/profiler/nested-start-and-stop-profiler.html fails https://bugs.webkit.org/show_bug.cgi?id=93897
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    Before r125428 run-time methods (wrapped signals, slots or invokable
    functions) were subclasses of JSInternalFunction and therefore real
    function objects in the JavaScript sense. r125428 changed them to be
    just callable objects, but they did not have Function.prototype as
    prototype anymore for example nor was their name correct (resulting in
    a layout test failure).
    
    This patch changes run-time methods back to being real function objects
    that have a correct name and have Function.prototype in their prototype
    change
    
    The objects returned by JSObjectMakeFunctionWithCallbackInjected are
    light-weight internal function objects that do not support
    JSObject{Set/Get}Private. Therefore we inject our own prototype right
    before the Function.prototype prototype, which uses private data to
    store a pointer to our C++ QtRuntimeMethod object.  This complicates
    the retrieval of the pointer to that instance slightly, which is why
    this patch introduces the toRuntimeMethod convenience function that
    looks up our prototype first and does a check for type-safety.
    
    At the same time the patch removes the length properties from the
    run-time method itself as well as connect/disconnect.  The length
    property on a function signifies the number of arguments, but in all
    three cases that number is actually variable, because of overloading.
    That is why we choose not to expose it in the first place.
    
    In QtInstance we cache the JS wrapper objects for QtRuntimeMethod in a
    JSWeakObjectMap. JSWeakObjectMap requires the stored objects to be
    either the result of JSObjectMake or the global object of a context ref
    (AFAICS), which is ensured using an ASSERT. Objects created via
    JSObjectMakeFunctionWithCalllback do not fall into the required
    category, cause a failing assertion and can therefore not be stored in
    the weak object map.
    
    Consequently this patch removes the use of JSWeakObjectMap again and
    goes back to the old way of using the internal Weak<> API, for the time
    being. In a future patch the storage will be simplified to not require
    the use of a weak object map cache for the run-time methods anymore.
    
    * bridge/qt/qt_instance.cpp: Remove unused WeakMap code.
    * bridge/qt/qt_instance.h: Remove method cache.
    (QtInstance):
    * bridge/qt/qt_runtime.cpp:
    (JSC::Bindings::prototypeForSignalsAndSlots):
    (JSC::Bindings::QtRuntimeMethod::call):
    (JSC::Bindings::QtRuntimeMethod::jsObjectRef):
    (JSC::Bindings::QtRuntimeMethod::toRuntimeMethod):
    (Bindings):
    (JSC::Bindings::QtRuntimeMethod::connectOrDisconnect):
    * bridge/qt/qt_runtime.h:
    (QtRuntimeMethod): Remove unused member variables.
    
    Source/WebKit/qt:
    
    Fixed some test expectations.
    
    * tests/qobjectbridge/tst_qobjectbridge.cpp:
    (tst_QObjectBridge::objectDeleted): Since runtime methods are real function objects again, we
    can go back to testing Function.prototype.call, as it was done before r125428.
    (tst_QObjectBridge::introspectQtMethods_data): Removed tests for the length property.
    (tst_QObjectBridge::introspectQtMethods): Changed test expectation of the properties of
    run-time methods back to being non-configurable, as before r125428.
    
    LayoutTests:
    
    * platform/qt/Skipped: Unskip test that is now passing.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@126976 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.h
+++ third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.h
@@ -110,7 +110,6 @@ private:
     QPointer<QObject> m_object;
     QObject* m_hashkey;
     mutable QHash<QByteArray, QtRuntimeMethod*> m_methods;
-    WeakMap m_cachedMethods;
     mutable QHash<QString, QtField*> m_fields;
     ValueOwnership m_ownership;
 };
