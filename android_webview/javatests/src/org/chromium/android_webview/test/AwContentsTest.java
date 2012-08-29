c70cde8258b421a4f0ecc5815e8f6ff7930463c2
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 19:39:20 +0000

    [Android] Remove ContentView usage from android_webview.
    This changes the android_webview Java test code to use
    the new AwContentsView test class instead of ContentView.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/10873049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153953 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsTest.java
@@ -9,12 +9,8 @@ import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.test.suitebuilder.annotation.SmallTest;
-import android.view.KeyEvent;
-import android.view.View;
-import android.webkit.ConsoleMessage;
 import android.widget.GridLayout;
 
-import org.chromium.android_webview.AndroidWebViewUtil;
 import org.chromium.android_webview.AwContents;
 import org.chromium.android_webview.AwContentsClient;
 import org.chromium.base.test.Feature;
@@ -32,6 +28,9 @@ public class AwContentsTest extends AndroidWebViewTestBase {
     private AwContents createContents(Context context) {
         GridLayout viewGroup = new GridLayout(context);
 
+        // TODO: Required ContentViewCore changes are not upstreamed yet.
+        // ContentViewCore contentViewCore = new ContentViewCore(
+        //        context, ContentViewCore.PERSONALITY_VIEW);
         ContentViewCore contentViewCore = new ContentViewCore(
                 context, viewGroup, null, 0, ContentViewCore.PERSONALITY_VIEW);
         AwContents awContents = new AwContents(viewGroup, null, contentViewCore,
@@ -40,6 +39,7 @@ public class AwContentsTest extends AndroidWebViewTestBase {
     }
 
     @SmallTest
+    @Feature({"Android-WebView"})
     public void testCreateDestroy() throws Throwable {
         final Throwable[] error = new Throwable[1];
         final Semaphore s = new Semaphore(0);
