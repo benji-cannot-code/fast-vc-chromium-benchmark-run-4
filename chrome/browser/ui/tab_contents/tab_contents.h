da5ebece966deb1cb63d42e3b4284e7c0d21b509
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 02:16:51 +0000

    [Panels refactor] Support window.open for browserless panels.
    BUG=127323
    TEST=Added WindowOpenPanelTest.WindowOpenFromPanel
    
    
    Review URL: https://chromiumcodereview.appspot.com/10897011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents.h
+++ chrome/browser/ui/tab_contents/tab_contents.h
@@ -45,6 +45,7 @@ class NavigationMetricsRecorder;
 class OffscreenTabContentsCreator;
 class OldBasePanelBrowserTest;
 class OmniboxSearchHint;
+class PanelHost;
 class PasswordManager;
 class PasswordManagerDelegate;
 class PDFTabObserver;
@@ -166,6 +167,7 @@ class TabContents : public content::WebContentsObserver {
     friend class InstantLoader;
     friend class OldBasePanelBrowserTest;
     friend class OffscreenTabContentsCreator;
+    friend class PanelHost;
     friend class prerender::PrerenderContents;
     friend class ShellWindow;
     friend class TabContentsTestHarness;
