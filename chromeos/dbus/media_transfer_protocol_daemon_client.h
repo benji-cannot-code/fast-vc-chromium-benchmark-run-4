75c8eca5c410a84e2afc073997acaeee11a1adb4
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-29 02:17:05 +0000

    Add default constructor for MediaTransferProtocolClient FileEntry and StorageInfo classes.
    BUG=144527
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10876094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@153825 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/media_transfer_protocol_daemon_client.h
+++ chromeos/dbus/media_transfer_protocol_daemon_client.h
@@ -55,6 +55,7 @@ enum FileType {
 // A class to represent information about a storage sent from mtpd.
 class CHROMEOS_EXPORT StorageInfo {
  public:
+  StorageInfo();
   StorageInfo(const std::string& storage_name, dbus::Response* response);
   ~StorageInfo();
 
@@ -127,6 +128,7 @@ class CHROMEOS_EXPORT StorageInfo {
 // A class to represent information about a file entry sent from mtpd.
 class CHROMEOS_EXPORT FileEntry {
  public:
+  FileEntry();
   explicit FileEntry(dbus::Response* response);
   ~FileEntry();
 
