d9ca0026cda1c380ec3bc2a27f87039d49bff12b
yoli@rim.com <yoli@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 20:52:09 +0000

    Vector::shrinkToFit should use realloc when suitable. https://bugs.webkit.org/show_bug.cgi?id=94810
    Reviewed by Benjamin Poulain.
    
    Only tested on BlackBerry. So it is wrapped with PLATFORM(BLACKBERRY) in the mean time.
    Use realloc to shrink buffer when inline buffer isn't involved and and canMoveWithMemcpy is true.
    
    When running the test code attached to the bug, it gives 30-45% performance boost for the large blocks
    (Every test cycle includes an extra pair of malloc/free, so the boost on shrinkToFit() is even bigger)
    Performance impact on small blocks is not noticeable. (Tested on BlackBerry)
    
    * wtf/Vector.h:
    (WTF::VectorBufferBase::shouldReallocateBuffer):
    (VectorBufferBase):
    (WTF::VectorBufferBase::reallocateBuffer):
    (VectorBuffer):
    (WTF::VectorBuffer::shouldReallocateBuffer):
    (WTF::VectorBuffer::reallocateBuffer):
    (WTF::VectorBuffer::inlineBuffer):
    (WTF::::shrinkCapacity):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127186 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Vector.h
+++ third_party/WebKit/Source/WTF/wtf/Vector.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Noncopyable.h>
 #include <wtf/NotFound.h>
 #include <wtf/StdLibExtras.h>
+#include <wtf/UnusedParam.h>
 #include <wtf/ValueCheck.h>
 #include <wtf/VectorTraits.h>
 #include <limits>
@@ -280,6 +281,27 @@ namespace WTF {
             return false;
         }
 
+        bool shouldReallocateBuffer(size_t newCapacity) const
+        {
+#if PLATFORM(BLACKBERRY)
+            // Tested on BlackBerry.
+            return VectorTraits<T>::canMoveWithMemcpy && m_capacity && newCapacity;
+#else
+            // FIXME: Return true on the platforms where realloc() gives better performance.
+            UNUSED_PARAM(newCapacity);
+            return false;
+#endif
+        }
+
+        void reallocateBuffer(size_t newCapacity)
+        {
+            ASSERT(shouldReallocateBuffer(newCapacity));
+            m_capacity = newCapacity;
+            if (newCapacity > std::numeric_limits<size_t>::max() / sizeof(T))
+                CRASH();
+            m_buffer = static_cast<T*>(fastRealloc(m_buffer, newCapacity * sizeof(T)));
+        }
+
         void deallocateBuffer(T* bufferToDeallocate)
         {
             if (!bufferToDeallocate)
@@ -363,6 +385,8 @@ namespace WTF {
 
         using Base::allocateBuffer;
         using Base::tryAllocateBuffer;
+        using Base::shouldReallocateBuffer;
+        using Base::reallocateBuffer;
         using Base::deallocateBuffer;
 
         using Base::buffer;
@@ -424,7 +448,19 @@ namespace WTF {
                 return;
             Base::deallocateBuffer(bufferToDeallocate);
         }
-        
+
+        bool shouldReallocateBuffer(size_t newCapacity) const
+        {
+            // We cannot reallocate the inline buffer.
+            return Base::shouldReallocateBuffer(newCapacity) && std::min(m_capacity, newCapacity) > inlineCapacity;
+        }
+
+        void reallocateBuffer(size_t newCapacity)
+        {
+            ASSERT(shouldReallocateBuffer(newCapacity));
+            Base::reallocateBuffer(newCapacity);
+        }
+
         void swap(VectorBuffer<T, inlineCapacity>& other)
         {
             if (buffer() == inlineBuffer() && other.buffer() == other.inlineBuffer()) {
@@ -471,6 +507,7 @@ namespace WTF {
 
         static const size_t m_inlineBufferSize = inlineCapacity * sizeof(T);
         T* inlineBuffer() { return reinterpret_cast_ptr<T*>(m_inlineBuffer.buffer); }
+        const T* inlineBuffer() const { return reinterpret_cast_ptr<const T*>(m_inlineBuffer.buffer); }
 
         AlignedBuffer<m_inlineBufferSize, WTF_ALIGN_OF(T)> m_inlineBuffer;
     };
@@ -965,6 +1002,11 @@ namespace WTF {
 
         T* oldBuffer = begin();
         if (newCapacity > 0) {
+            if (m_buffer.shouldReallocateBuffer(newCapacity)) {
+                m_buffer.reallocateBuffer(newCapacity);
+                return;
+            }
+
             T* oldEnd = end();
             m_buffer.allocateBuffer(newCapacity);
             if (begin() != oldBuffer)
