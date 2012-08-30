f698b9715091b483747d114116ea119d0e428603
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 05:45:37 +0000

    [EFL][WK2] Replace PageClientImpl with ewk view in constructor of EflViewportHandler. https://bugs.webkit.org/show_bug.cgi?id=95408
    Patch by Kangil Han <kangil.han@samsung.com> on 2012-08-29
    Reviewed by Gyuyoung Kim.
    
    To keep consistency of implementation, derived classes(from ewk view) should have view reference.
    From this, derived classes would have less interference from changes that would happen in port specific classes, i.e. PageClientImpl.
    
    * UIProcess/API/efl/EflViewportHandler.cpp:
    (WebKit::EflViewportHandler::EflViewportHandler):
    (WebKit::EflViewportHandler::drawingArea):
    (WebKit):
    (WebKit::EflViewportHandler::updateViewportSize):
    * UIProcess/API/efl/EflViewportHandler.h:
    (WebKit::EflViewportHandler::create):
    (EflViewportHandler):
    * UIProcess/API/efl/ewk_view.cpp:
    (_ewk_view_initialize):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EflViewportHandler.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EflViewportHandler.cpp
@@ -32,20 +32,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerTreeCoordinatorProxy.h"
 #include "LayerTreeRenderer.h"
 #include "TransformationMatrix.h"
+#include "ewk_view_private.h"
 
 using namespace WebCore;
 
 namespace WebKit {
 
-EflViewportHandler::EflViewportHandler(PageClientImpl* pageClientImpl)
-    : m_pageClientImpl(pageClientImpl)
+EflViewportHandler::EflViewportHandler(Evas_Object* viewWidget)
+    : m_viewWidget(viewWidget)
     , m_scaleFactor(1)
 {
-    ASSERT(m_pageClientImpl);
+    ASSERT(m_viewWidget);
 }
 
 EflViewportHandler::~EflViewportHandler()
 {
+}
+
+DrawingAreaProxy* EflViewportHandler::drawingArea() const
+{
+    return ewk_view_page_get(m_viewWidget)->drawingArea();
 }
 
 void EflViewportHandler::display(const IntRect& rect)
@@ -62,7 +68,7 @@ void EflViewportHandler::display(const IntRect& rect)
 void EflViewportHandler::updateViewportSize(const IntSize& viewportSize)
 {
     m_viewportSize = viewportSize;
-    m_pageClientImpl->page()->setViewportSize(viewportSize);
+    ewk_view_page_get(m_viewWidget)->setViewportSize(viewportSize);
     setVisibleContentsRect(m_visibleContentRect.location(), m_scaleFactor, FloatPoint());
 }
 
