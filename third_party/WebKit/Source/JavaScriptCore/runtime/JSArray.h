50a3c94e3ee2265231209e39ec6480200745dbe4
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 23:33:05 +0000

    Undo Qt build fix guess, since it breaks other builds.
    * runtime/JSArray.h:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127210 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSArray.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSArray.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef JSArray_h
 #define JSArray_h
 
-#include "JSScope.h"
+#include "JSObject.h"
 
 #define CHECK_ARRAY_CONSISTENCY 0
 
