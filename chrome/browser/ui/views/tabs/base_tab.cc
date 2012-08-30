d6fe9f090d18f4e934aee21e03c26ead3205fb31
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 19:28:04 +0000

    Removes unnecessary overrides that are just calling through to the superclass. I should have removed these in my last patch.
    BUG=none
    TEST=none
    R=sadrul@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10917010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/base_tab.cc
+++ chrome/browser/ui/views/tabs/base_tab.cc
@@ -79,13 +79,6 @@ class BaseTab::TabCloseButton : public views::ImageButton {
     return event.IsOnlyMiddleMouseButton() ? false : handled;
   }
 
-  // We need to let the parent know about mouse state so that it
-  // can highlight itself appropriately. Note that Exit events
-  // fire before Enter events, so this works.
-  virtual void OnMouseEntered(const ui::MouseEvent& event) OVERRIDE {
-    CustomButton::OnMouseEntered(event);
-  }
-
   virtual void OnMouseMoved(const ui::MouseEvent& event) OVERRIDE {
     if (tab_->controller())
       tab_->controller()->OnMouseEventInTab(this, event);
@@ -96,10 +89,6 @@ class BaseTab::TabCloseButton : public views::ImageButton {
     if (tab_->controller())
       tab_->controller()->OnMouseEventInTab(this, event);
     CustomButton::OnMouseReleased(event);
-  }
-
-  virtual void OnMouseExited(const ui::MouseEvent& event) OVERRIDE {
-    CustomButton::OnMouseExited(event);
   }
 
   virtual ui::GestureStatus OnGestureEvent(
