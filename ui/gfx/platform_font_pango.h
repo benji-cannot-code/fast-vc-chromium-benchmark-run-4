453044abffb2962ec7e6bca35b29af0a3f54a46f
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 21:08:15 +0000

    Add DISALLOW_COPY_AND_ASSIGN() to PlatformFont* classes.
    BUG=none
    TEST=compiles
    
    Review URL: https://chromiumcodereview.appspot.com/10915009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154267 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/platform_font_pango.h
+++ ui/gfx/platform_font_pango.h
@@ -33,7 +33,7 @@ class UI_EXPORT PlatformFontPango : public PlatformFont {
 
   // Position as an offset from the height of the drawn text, used to draw
   // an underline. This is a negative number, so the underline would be
-  // drawn at y + height + underline_position;
+  // drawn at y + height + underline_position.
   double underline_position() const;
   // The thickness to draw the underline.
   double underline_thickness() const;
@@ -70,7 +70,7 @@ class UI_EXPORT PlatformFontPango : public PlatformFont {
   // Potentially slow call to get pango metrics (average width, underline info).
   void InitPangoMetrics();
 
-  // Setup a Skia context to use the current typeface
+  // Setup a Skia context to use the current typeface.
   void PaintSetup(SkPaint* paint) const;
 
   // Make |this| a copy of |other|.
@@ -91,7 +91,7 @@ class UI_EXPORT PlatformFontPango : public PlatformFont {
   int font_size_pixels_;
   int style_;
 
-  // Cached metrics, generated at construction
+  // Cached metrics, generated at construction.
   int height_pixels_;
   int ascent_pixels_;
 
@@ -104,6 +104,8 @@ class UI_EXPORT PlatformFontPango : public PlatformFont {
 
   // The default font, used for the default constructor.
   static Font* default_font_;
+
+  DISALLOW_COPY_AND_ASSIGN(PlatformFontPango);
 };
 
 }  // namespace gfx
