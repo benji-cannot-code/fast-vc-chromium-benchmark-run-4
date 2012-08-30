e278dd74e5f86d6c2aeb0d946a7dc7923c541d9c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 03:30:51 +0000

    [Qt][WK2] ApplicationCache LayoutTests failed https://bugs.webkit.org/show_bug.cgi?id=69541
    Patch by Luciano Wolf <luciano.wolf@openbossa.org> on 2012-08-29
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebKit2:
    
    Returns defaultDiskCacheDirectory when no cache directory was provided.
    It's used by setOfflineWebApplicationCacheEnabled method that won't work
    with an invalid cache directory.
    
    * UIProcess/qt/WebContextQt.cpp:
    (WebKit::WebContext::applicationCacheDirectory):
    
    LayoutTests:
    
    Unskip http/appcache tests for qt-5.0-wk2.
    
    * platform/qt-5.0-wk2/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/WebContextQt.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/WebContextQt.cpp
@@ -59,7 +59,12 @@ static String defaultDiskCacheDirectory()
 
 String WebContext::applicationCacheDirectory()
 {
-    return WebCore::cacheStorage().cacheDirectory();
+    const String cacheDirectory = WebCore::cacheStorage().cacheDirectory();
+
+    if (cacheDirectory.isEmpty())
+        return defaultDiskCacheDirectory();
+
+    return cacheDirectory;
 }
 
 void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& parameters)
