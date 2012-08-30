ac2a062d72ff7ae5ce31ab136c41ee4db0233200
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 20:00:27 +0000

    Drive: Fix the build
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10915018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/drive_file_system.cc
+++ chrome/browser/chromeos/gdata/drive_file_system.cc
@@ -252,7 +252,7 @@ DriveFileSystem::CreateDirectoryParams::CreateDirectoryParams(
       is_exclusive(is_exclusive),
       is_recursive(is_recursive),
       callback(callback) {
-  DCHECK(!callback.is_null())
+  DCHECK(!callback.is_null());
 }
 
 DriveFileSystem::CreateDirectoryParams::~CreateDirectoryParams() {
