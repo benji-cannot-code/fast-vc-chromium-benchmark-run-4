f348636871a38cdc90f3e9c8d6c4c379e0df7085
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 22:01:56 +0000

    Remove GetLastActive from NetworkProfileBubble.
    BUG=129187
    TEST=NONE
    
    
    Review URL: https://chromiumcodereview.appspot.com/10887021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/network_profile_bubble.cc
+++ chrome/browser/ui/network_profile_bubble.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "chrome/common/chrome_switches.h"
@@ -173,8 +174,14 @@ void NetworkProfileBubble::RecordUmaEvent(MetricNetworkedProfileCheck event) {
 
 // static
 void NetworkProfileBubble::NotifyNetworkProfileDetected() {
-  if (BrowserList::GetLastActive())
-    ShowNotification(BrowserList::GetLastActive());
+  // TODO(robertshield): Eventually, we will need to figure out the correct
+  //                     desktop type for this for platforms that can have
+  //                     multiple desktop types (win8/metro).
+  Browser* browser = browser::FindLastActiveWithHostDesktopType(
+      chrome::HOST_DESKTOP_TYPE_NATIVE);
+
+  if (browser)
+    ShowNotification(browser);
   else
     BrowserList::AddObserver(new BrowserListObserver());
 }
