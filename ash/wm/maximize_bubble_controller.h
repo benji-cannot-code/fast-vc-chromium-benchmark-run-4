b1d30c27a5cc4d1cc9e8e1ec29714525a7e61a1a
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 23:24:12 +0000

    Revert 154295 - Added restore functionality for maximize full/left/right
    BUG=141725
    TEST=unit test
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=154226
    
    Review URL: https://chromiumcodereview.appspot.com/10883069
    
    TBR=skuhne@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10919022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154306 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/maximize_bubble_controller.h
+++ ash/wm/maximize_bubble_controller.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ASH_WM_MAXIMIZE_BUBBLE_CONTROLLER_H_
 
 #include "ash/ash_export.h"
-#include "ash/wm/workspace/maximize_bubble_frame_state.h"
 #include "ash/wm/workspace/snap_types.h"
 #include "base/memory/scoped_ptr.h"
 
@@ -33,7 +32,7 @@ class ASH_EXPORT MaximizeBubbleController {
   class Bubble;
 
   MaximizeBubbleController(FrameMaximizeButton* frame_maximize_button,
-                           MaximizeBubbleFrameState maximize_type,
+                           bool is_maximized,
                            int appearance_delay_ms);
   // Called from the outside to destroy the interface to the UI visuals.
   // The visuals will then delete when possible (maybe asynchronously).
@@ -63,7 +62,7 @@ class ASH_EXPORT MaximizeBubbleController {
   }
 
   // The status of the associated window: Maximized or normal.
-  MaximizeBubbleFrameState maximize_type() const { return maximize_type_; }
+  bool is_maximized() const { return is_maximized_; }
 
   // A unit test function to return buttons of the sub menu. |state| can be
   // either SNAP_LEFT, SNAP_RIGHT or SNAP_MINIMIZE.
@@ -85,8 +84,8 @@ class ASH_EXPORT MaximizeBubbleController {
   // The bubble menu.
   Bubble* bubble_;
 
-  // The current maximize state of the owning window.
-  const MaximizeBubbleFrameState maximize_type_;
+  // If true the owning window is maximized.
+  const bool is_maximized_;
 
   // The timer for the delayed creation of the menu.
   scoped_ptr<base::Timer> timer_;
