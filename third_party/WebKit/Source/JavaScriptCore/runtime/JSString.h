17ce9d343fe04a2562b2e6f99122953f5296ec06
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 23:41:29 +0000

    Ambiguous operator[]  after r127191 on some compiler https://bugs.webkit.org/show_bug.cgi?id=95509
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-08-30
    Reviewed by Simon Fraser.
    
    Source/JavaScriptCore: 
    
    On some compilers, the operator[] conflicts with the Obj-C++ operators. This attempts to solve
    the issue.
    
    * runtime/JSString.h:
    (JSC::jsSingleCharacterSubstring):
    (JSC::jsString):
    (JSC::jsSubstring8):
    (JSC::jsSubstring):
    (JSC::jsOwnedString):
    
    Source/WTF: 
    
    * wtf/text/WTFString.h:
    (WTF::String::characterAt): At this as a synonym to operator[] to attempt a build fix.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127212 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSString.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSString.h
@@ -348,7 +348,7 @@ namespace JSC {
     {
         JSGlobalData* globalData = &exec->globalData();
         ASSERT(offset < static_cast<unsigned>(s.length()));
-        UChar c = s[offset];
+        UChar c = s.characterAt(offset);
         if (c <= maxSingleCharacterString)
             return globalData->smallStrings.singleCharacterString(globalData, c);
         return JSString::create(*globalData, StringImpl::create(s.impl(), offset, 1));
@@ -397,7 +397,7 @@ namespace JSC {
         if (!size)
             return globalData->smallStrings.emptyString(globalData);
         if (size == 1) {
-            UChar c = s[0];
+            UChar c = s.characterAt(0);
             if (c <= maxSingleCharacterString)
                 return globalData->smallStrings.singleCharacterString(globalData, c);
         }
@@ -423,7 +423,7 @@ namespace JSC {
         if (!length)
             return globalData->smallStrings.emptyString(globalData);
         if (length == 1) {
-            UChar c = s[offset];
+            UChar c = s.characterAt(offset);
             if (c <= maxSingleCharacterString)
                 return globalData->smallStrings.singleCharacterString(globalData, c);
         }
@@ -438,7 +438,7 @@ namespace JSC {
         if (!length)
             return globalData->smallStrings.emptyString(globalData);
         if (length == 1) {
-            UChar c = s[offset];
+            UChar c = s.characterAt(offset);
             if (c <= maxSingleCharacterString)
                 return globalData->smallStrings.singleCharacterString(globalData, c);
         }
@@ -451,7 +451,7 @@ namespace JSC {
         if (!size)
             return globalData->smallStrings.emptyString(globalData);
         if (size == 1) {
-            UChar c = s[0];
+            UChar c = s.characterAt(0);
             if (c <= maxSingleCharacterString)
                 return globalData->smallStrings.singleCharacterString(globalData, c);
         }
