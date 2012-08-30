e90ad92c74ca63b372bcbb358cfae2d3ac24020f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 22:13:25 +0000

    Unreviewed, rolling out r127171. http://trac.webkit.org/changeset/127171 https://bugs.webkit.org/show_bug.cgi?id=95505
    testRunner does not need dumpWebNotificationCallbacks().
    (Requested by jonlee on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-08-30
    
    Source/WebKit/mac:
    
    * WebKit.exp:
    
    Tools:
    
    * DumpRenderTree/TestRunner.cpp:
    (TestRunner::TestRunner):
    (TestRunner::staticFunctions):
    * DumpRenderTree/TestRunner.h:
    (TestRunner):
    * DumpRenderTree/mac/MockWebNotificationProvider.mm:
    (-[MockWebNotificationProvider webView:didShowNotification:]):
    (-[MockWebNotificationProvider webView:didClickNotification:]):
    (-[MockWebNotificationProvider webView:didCloseNotifications:]):
    * DumpRenderTree/mac/UIDelegate.mm:
    (-[UIDelegate webView:decidePolicyForNotificationRequestFromOrigin:listener:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/mac/MockWebNotificationProvider.mm
+++ third_party/WebKit/Tools/DumpRenderTree/mac/MockWebNotificationProvider.mm
@@ -27,24 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#import "config.h"
 #import "MockWebNotificationProvider.h"
-
-#import "DumpRenderTree.h"
-#import "TestRunner.h"
-#import <WebKit/WebNotification.h>
 #import <WebKit/WebSecurityOriginPrivate.h>
-
-@interface WebNotification (DRTExtras)
-- (NSString *)_drt_descriptionSuitableForTestResult;
-@end
-
-@implementation WebNotification (DRTExtras)
-- (NSString *)_drt_descriptionSuitableForTestResult
-{
-    return [NSString stringWithFormat:@"{title: \"%@\", body: \"%@\", tag: \"%@\", iconURL: %@}", [self title], [self body], [self tag], [self iconURL]];
-}
-@end
 
 @implementation MockWebNotificationProvider
 
@@ -110,22 +94,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)webView:(WebView *)webView didShowNotification:(uint64_t)notificationID
 {
-    WebNotification *notification = _notifications.get(notificationID).get();
-    ASSERT(notification);
-
-    if (!done && gTestRunner->dumpWebNotificationCallbacks())
-        printf("SHOWED WEB NOTIFICATION %s\n", [[notification _drt_descriptionSuitableForTestResult] UTF8String]);
-    [notification dispatchShowEvent];
+    [_notifications.get(notificationID).get() dispatchShowEvent];
 }
 
 - (void)webView:(WebView *)webView didClickNotification:(uint64_t)notificationID
 {
-    WebNotification *notification = _notifications.get(notificationID).get();
-    ASSERT(notification);
-
-    if (!done && gTestRunner->dumpWebNotificationCallbacks())
-        printf("CLICKED WEB NOTIFICATION %s\n", [[notification _drt_descriptionSuitableForTestResult] UTF8String]);
-    [notification dispatchClickEvent];
+    [_notifications.get(notificationID).get() dispatchClickEvent];
 }
 
 - (void)webView:(WebView *)webView didCloseNotifications:(NSArray *)notificationIDs
@@ -134,12 +108,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         uint64_t id = [notificationID unsignedLongLongValue];
         NotificationIDMap::iterator it = _notifications.find(id);
         ASSERT(it != _notifications.end());
-
-        WebNotification *notification = it->second.get();
-        if (!done && gTestRunner->dumpWebNotificationCallbacks())
-            printf("CLOSED WEB NOTIFICATION %s\n", [[notification _drt_descriptionSuitableForTestResult] UTF8String]);
-
-        [notification dispatchCloseEvent];
+        [it->second.get() dispatchCloseEvent];
         _notifications.remove(it);
         _notificationViewMap.remove(id);
     }
