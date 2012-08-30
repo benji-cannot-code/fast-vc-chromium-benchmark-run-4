d7c8e68b4f30ad68e67d149f1e813729e9300767
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 02:05:15 +0000

    use after free in WebCore::FileReader::doAbort https://bugs.webkit.org/show_bug.cgi?id=91004
    Reviewed by Jian Li.
    
    Source/WebCore: 
    
    Added check in FileReader::abort to not process the abort if we aren't in the LOADING
    state.  This is per the FileAPI spec section 8.5.6 step #1.
    
    Tests: fast/files/file-reader-immediate-abort.html
           fast/files/file-reader-done-reading-abort.html
    
    * fileapi/FileReader.cpp:
    (WebCore::FileReader::abort):
    
    LayoutTests: 
    
    New tests to check that FileReader::abort doesn't crash or create events before
    or after reading.
    
    * fast/files/file-reader-done-reading-abort-expected.txt: Added.
    * fast/files/file-reader-done-reading-abort.html: Added.
    * fast/files/file-reader-immediate-abort-expected.txt: Added.
    * fast/files/file-reader-immediate-abort.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/fileapi/FileReader.cpp
+++ third_party/WebKit/Source/WebCore/fileapi/FileReader.cpp
@@ -162,7 +162,7 @@ void FileReader::abort()
 {
     LOG(FileAPI, "FileReader: aborting\n");
 
-    if (m_aborting)
+    if (m_aborting || m_state != LOADING)
         return;
     m_aborting = true;
 
