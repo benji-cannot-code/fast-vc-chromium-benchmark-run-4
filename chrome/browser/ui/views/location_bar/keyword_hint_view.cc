be0cc1b7fd1710c9d85354e9014126ad9c6512fc
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 14:45:57 +0000

    Switch to new assets and fix corresponding constants related to ev bubble, tab to search UI elements and the content settings bubble. Assets checked in in http://codereview.chromium.org/10896009/
    BUG=137351,137717
    TEST=manual: To test EV bubble, invoke it in the omnibox by going to paypal.com. The bubble should be completely inside the omnibox with one pixel padding on top and bottom. Same checks for tab-to-search bubble and content setting bubble.
    
    Review URL: https://chromiumcodereview.appspot.com/10899009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/keyword_hint_view.cc
+++ chrome/browser/ui/views/location_bar/keyword_hint_view.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 
 // Amount of space to offset the tab image from the top of the view by.
-static const int kTabImageYOffset = 4;
+static const int kTabImageYOffset = 1;
 
 // The tab key image.
 static const gfx::ImageSkia* kTabButtonImage = NULL;
