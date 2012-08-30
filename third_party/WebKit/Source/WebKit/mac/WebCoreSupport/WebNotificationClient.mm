b0d5c71d577b96a8c2111e01428e6b0a36e86006
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 17:59:47 +0000

    [Mac] Registering web views for notifications is unbalanced https://bugs.webkit.org/show_bug.cgi?id=95465 <rdar://problem/12206765>
    Reviewed by Darin Adler.
    
    Setting the notification provider for a WK1 WebView registers that web view to the
    provider. When the WebView is closed, WebCore is essentially responsible for
    unregistering the web view. This leads to an imbalance on Lion since notifications are
    not supported, and the NotificationController responsible for making the unregistration
    call doesn't exist.
    
    Instead, the unregisterWebView method should be called when the web view is closed.
    
    * WebView/WebViewPrivate.h: Remove _notificationControllerDestroyed, since it is
    unneeded.
    
    * WebCoreSupport/WebNotificationClient.mm:
    (WebNotificationClient::notificationControllerDestroyed):
    * WebView/WebView.mm:
    (-[WebView _close]): Unregister the web view from the assigned provider here.
    (-[WebView _setNotificationProvider:]): Only allow setting the provider once, to avoid
    registering the web view to multiple providers.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebNotificationClient.mm
+++ third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebNotificationClient.mm
@@ -161,9 +161,6 @@ void WebNotificationClient::notificationObjectDestroyed(Notification* notificati
 
 void WebNotificationClient::notificationControllerDestroyed()
 {
-#if ENABLE(NOTIFICATIONS) || ENABLE(LEGACY_NOTIFICATIONS)
-    [m_webView _notificationControllerDestroyed];
-#endif
     delete this;
 }
 
--- third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
+++ third_party/WebKit/Source/WebKit/mac/WebView/WebView.mm
@@ -1141,6 +1141,8 @@ static bool fastDocumentTeardownEnabled()
     [self _clearGlibLoopObserver];
 #endif
 
+    [[self _notificationProvider] unregisterWebView:self];
+
     [[NSDistributedNotificationCenter defaultCenter] removeObserver:self];
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 
@@ -6518,15 +6520,10 @@ static void glibContextIterationCallback(CFRunLoopObserverRef, CFRunLoopActivity
 @implementation WebView (WebViewNotification)
 - (void)_setNotificationProvider:(id<WebNotificationProvider>)notificationProvider
 {
-    if (_private) {
+    if (_private && !_private->_notificationProvider) {
         _private->_notificationProvider = notificationProvider;
         [_private->_notificationProvider registerWebView:self];
     }
-}
-
-- (void)_notificationControllerDestroyed
-{
-    [[self _notificationProvider] unregisterWebView:self];
 }
 
 - (id<WebNotificationProvider>)_notificationProvider
