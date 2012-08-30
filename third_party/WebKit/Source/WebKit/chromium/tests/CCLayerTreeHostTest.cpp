c7b13802f30475adf6849185e127ea323b72b469
gavinp@chromium.org <gavinp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-30 17:23:16 +0000

    [chromium] Disable CCLayerTreeHostTestScrollMultipleRedraw.runMultiThread https://bugs.webkit.org/show_bug.cgi?id=95472
    Unreviewed gardening.
    
    When landing r127079, this test was re-enabled, and has not passed since. I'm disabling it, and created bug 95473 to track fixing this issue.
    
    * tests/CCLayerTreeHostTest.cpp:
    (WebKitTests::TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127156 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCLayerTreeHostTest.cpp
@@ -1049,7 +1049,7 @@ private:
     int m_scrolls;
 };
 
-TEST_F(CCLayerTreeHostTestScrollMultipleRedraw, runMultiThread)
+TEST_F(CCLayerTreeHostTestScrollMultipleRedraw, DISABLED_runMultiThread)
 {
     runTest(true);
 }
