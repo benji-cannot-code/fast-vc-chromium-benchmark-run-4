ab74a97b9db689e81cc71f2be3174cdf46af7e52
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 00:19:02 +0000

    Revert 154002 - RefreshFile cleanup.
    BUG=142048
    TEST=unit tests.
    
    * RefreshFile now takes a scoped_ptr<DocumentEntry> instead of scoped_ptr<DriveFile>. It also has a GetEntryInfoWithFilePathCallback so the file path and entry_proto of the refreshed file may be provided to the caller.
    * Rename PostError to PostFileMoveCallbackError. Introduce PostGetEntryInfoWithFilePathCallbackError in anonymous scope.
    * Get rid of RefreshFile unit test. Various DriveFileSystemTests provide adequate coverage. Need to write a more targeted RefreshFile unit test esp for error scenarios, but for now, this test is not salvageable.
    * DocumentFeed now has a new method ReleaseEntries, necessary for DriveFileSystem::OnSearch.
    * AddEntryToSearchResults is now a private method DriveFileSystem::AddToSearchResults. This is necessary to get rid of entry_skipped_callback, which is unnecessary - CheckForUpdates should be directly called instead. Also, this takes the AddToSearchResultsCallback, to get around the 7 arg limit for base::Bind. should_run_callback check is replaced by the null callback check.
    * DriveFileSystem::OnSearch is considerably simplified.
    * OnGetDocumentEntry is continued with CheckForSpaceBeforeDownload after RefreshFile (previously errors were ignored).
    * Add cache_file_path to GetFileFromCacheParams, so these params can be reused for CheckForSpaceBeforeDownload.
    * UpdateEntryDataOnUIThread is continued with UpdateCacheEntryOnUIThread after RefreshFile (previously errors were ignored). UpdateEntryParams is introduced so number of args of UpdateCacheEntryOnUIThread is under 7.
    * Remove some unnecessary struct dtors.
    Review URL: https://chromiumcodereview.appspot.com/10898013
    
    TBR=achuith@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10887040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154012 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_wapi_parser.cc
+++ chrome/browser/chromeos/gdata/gdata_wapi_parser.cc
@@ -986,10 +986,6 @@ bool DocumentFeed::GetNextFeedURL(GURL* url) {
   return false;
 }
 
-void DocumentFeed::ReleaseEntries(std::vector<DocumentEntry*>* entries) {
-  entries_.release(entries);
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 // InstalledApp implementation
 
