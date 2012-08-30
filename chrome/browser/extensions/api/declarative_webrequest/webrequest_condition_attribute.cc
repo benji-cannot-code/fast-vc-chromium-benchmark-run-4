b9bab51f6bc3f1636a87222d6b738f3098a55889
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-30 15:52:28 +0000

    Correcting indenting and naming
    This is a post-cleanup for CL https://chromiumcodereview.appspot.com/10874029/.
    
    No functional change made.
    
    
    BUG=143662, 112155
    TEST=N/A (no change in the way the code works)
    TBR=battre@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10893022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative_webrequest/webrequest_condition_attribute.cc
+++ chrome/browser/extensions/api/declarative_webrequest/webrequest_condition_attribute.cc
@@ -294,7 +294,8 @@ WebRequestConditionAttributeResponseHeaders::Create(
       return scoped_ptr<WebRequestConditionAttribute>(NULL);
     }
 
-    scoped_ptr<const HeaderMatchTest> header_test(CreateTests(tests, error));
+    scoped_ptr<const HeaderMatchTest> header_test(
+        CreateHeaderMatchTest(tests, error));
     if (header_test.get() == NULL)
       return scoped_ptr<WebRequestConditionAttribute>(NULL);
     header_tests.push_back(header_test.release());
@@ -344,7 +345,7 @@ bool WebRequestConditionAttributeResponseHeaders::IsFulfilled(
 
 WebRequestConditionAttribute::Type
 WebRequestConditionAttributeResponseHeaders::GetType() const {
-  return CONDITION_REQUEST_HEADERS;
+  return CONDITION_RESPONSE_HEADERS;
 }
 
 bool WebRequestConditionAttributeResponseHeaders::StringMatchTest::Matches(
@@ -383,7 +384,7 @@ bool WebRequestConditionAttributeResponseHeaders::HeaderMatchTest::Matches(
 
 // static
 scoped_ptr<const WebRequestConditionAttributeResponseHeaders::HeaderMatchTest>
-WebRequestConditionAttributeResponseHeaders::CreateTests(
+WebRequestConditionAttributeResponseHeaders::CreateHeaderMatchTest(
     const DictionaryValue* tests,
     std::string* error) {
   ScopedVector<const StringMatchTest> name;
@@ -430,14 +431,15 @@ WebRequestConditionAttributeResponseHeaders::CreateTests(
         for (ListValue::const_iterator it = list->begin();
              it != list->end(); ++it) {
           ScopedVector<const StringMatchTest>* tests = is_name ? &name : &value;
-          tests->push_back(CreateMatchTest(*it, is_name, match_type).release());
+          tests->push_back(
+              CreateStringMatchTest(*it, is_name, match_type).release());
         }
         break;
       }
       case Value::TYPE_STRING: {
         ScopedVector<const StringMatchTest>* tests = is_name ? &name : &value;
         tests->push_back(
-            CreateMatchTest(content, is_name, match_type).release());
+            CreateStringMatchTest(content, is_name, match_type).release());
         break;
       }
       default: {
@@ -453,8 +455,10 @@ WebRequestConditionAttributeResponseHeaders::CreateTests(
 
 // static
 scoped_ptr<const WebRequestConditionAttributeResponseHeaders::StringMatchTest>
-WebRequestConditionAttributeResponseHeaders::CreateMatchTest(
-    const Value* content, bool is_name_test, MatchType match_type) {
+WebRequestConditionAttributeResponseHeaders::CreateStringMatchTest(
+    const Value* content,
+    bool is_name_test,
+    MatchType match_type) {
   std::string str;
 
   CHECK(content->GetAsString(&str));
