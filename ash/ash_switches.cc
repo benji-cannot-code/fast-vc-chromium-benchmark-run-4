0bed580ff45fdcebe885b4db83888eb5d90bc442
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-31 04:27:21 +0000

    Disable pointer barrier on desktop by default. Added flag to enable it.
    BUG=145836
    TEST=run on precise desktop. you can move mouse out of the ash window.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10911023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154373 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.cc
+++ ash/ash_switches.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 namespace switches {
 
+// Constrains the pointer movement within a root window on desktop.
+const char kAshConstrainPointerToRoot[] = "ash-constrain-pointer-to-root";
+
 // Enable keyboard shortcuts useful for debugging.
 const char kAshDebugShortcuts[] = "ash-debug-shortcuts";
 
--- ash/display/display_controller.cc
+++ ash/display/display_controller.cc
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/display.h"
 #include "ui/gfx/screen.h"
 
+#if defined(OS_CHROMEOS)
+#include "base/chromeos/chromeos_version.h"
+#endif
+
 namespace ash {
 namespace internal {
 namespace {
@@ -221,10 +225,18 @@ void DisplayController::OnDisplayRemoved(const gfx::Display& display) {
 
 aura::RootWindow* DisplayController::AddRootWindowForDisplay(
     const gfx::Display& display) {
+  static bool force_constrain_pointer_to_root =
+      CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kAshConstrainPointerToRoot);
+
   aura::RootWindow* root = aura::Env::GetInstance()->display_manager()->
       CreateRootWindowForDisplay(display);
   root_windows_[display.id()] = root;
-  root->ConfineCursorToWindow();
+
+#if defined(OS_CHROMEOS)
+  if (base::chromeos::IsRunningOnChromeOS() || force_constrain_pointer_to_root)
+    root->ConfineCursorToWindow();
+#endif
   return root;
 }
 
