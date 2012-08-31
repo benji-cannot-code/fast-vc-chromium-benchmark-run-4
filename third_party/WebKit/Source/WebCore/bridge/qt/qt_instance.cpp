8d61cce770bfa201066fe2194db1b77b2926faae
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-31 08:07:42 +0000

    [Qt] Port convertValueToQVariant to use the JSC C API https://bugs.webkit.org/show_bug.cgi?id=94695
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    Most of the code is portable to the C API, with a few exceptions for internal
    types such as byte arrays, dates or when unboxing primitive types.
    
    * bridge/qt/qt_instance.cpp:
    (JSC::Bindings::QtField::setValueToInstance):
    * bridge/qt/qt_runtime.cpp:
    (JSC::Bindings::operator<<):
    (JSC::Bindings::isJSUint8ClampedArray):
    (JSC::Bindings::isJSArray):
    (Bindings):
    (JSC::Bindings::isJSDate):
    (JSC::Bindings::isQtObject):
    (JSC::Bindings::valueRealType):
    (JSC::Bindings::toString):
    (JSC::Bindings::unwrapBoxedPrimitive):
    (JSC::Bindings::convertValueToQVariantMap):
    (JSC::Bindings::convertToList):
    (JSC::Bindings::toQString):
    (JSC::Bindings::getGregorianDateTimeUTC):
    (JSC::Bindings::toQDateTimeUTC):
    (JSC::Bindings::convertValueToQVariant):
    (JSC::Bindings::findMethodIndex):
    (JSC::Bindings::QtConnectionObject::execute):
    * bridge/qt/qt_runtime.h:
    (Bindings):
    
    Source/WebKit/qt:
    
    Minor adaptations are needed here to accomodate the changed signature of
    convertValueToQVariant.
    
    * Api/qwebelement.cpp:
    (QWebElement::evaluateJavaScript):
    (convertJSValueToWebElementVariant):
    * Api/qwebframe.cpp:
    (QWebFrame::evaluateJavaScript):
    * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
    (convertJSValueToNodeVariant):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127238 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.cpp
+++ third_party/WebKit/Source/WebCore/bridge/qt/qt_instance.cpp
@@ -304,9 +304,6 @@ JSValue QtInstance::valueOf(ExecState* exec) const
     return stringValue(exec);
 }
 
-// In qt_runtime.cpp
-QVariant convertValueToQVariant(ExecState*, JSValue, QMetaType::Type hint, int *distance);
-
 QByteArray QtField::name() const
 {
     if (m_type == MetaProperty)
@@ -361,7 +358,12 @@ void QtField::setValueToInstance(ExecState* exec, const Instance* inst, JSValue 
             argtype = (QMetaType::Type) m_property.userType();
 
         // dynamic properties just get any QVariant
-        QVariant val = convertValueToQVariant(exec, aValue, argtype, 0);
+        JSValueRef exception = 0;
+        QVariant val = convertValueToQVariant(toRef(exec), toRef(exec, aValue), argtype, 0, &exception);
+        if (exception) {
+            throwError(exec, toJS(exec, exception));
+            return;
+        }
         if (m_type == MetaProperty) {
             if (m_property.isWritable())
                 m_property.write(obj, val);
