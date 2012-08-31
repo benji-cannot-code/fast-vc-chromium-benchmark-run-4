54775e31c52c0b3cd777431644776a30af529806
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-31 07:38:10 +0000

    [content shell] report renderer crashes and hangs, as well as plugin crashes
    BUG=111316
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10913006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154403 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_runner_host.h
+++ content/shell/webkit_test_runner_host.h
@@ -68,6 +68,8 @@ class WebKitTestController : public base::NonThreadSafe,
   // True if the controller was reset successfully.
   bool ResetAfterLayoutTest();
 
+  void RendererUnresponsive();
+
   WebKitTestResultPrinter& printer() { return printer_; }
 
   // Interface for WebKitTestRunnerHost.
@@ -93,9 +95,9 @@ class WebKitTestController : public base::NonThreadSafe,
   bool is_printing() const { return is_printing_; }
   void set_is_printing(bool is_printing) { is_printing_ = is_printing; }
 
-
   // WebContentsObserver implementation.
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
+  virtual void PluginCrashed(const FilePath& plugin_path) OVERRIDE;
   virtual void RenderViewGone(base::TerminationStatus status) OVERRIDE;
   virtual void WebContentsDestroyed(WebContents* web_contents) OVERRIDE;
 
@@ -115,6 +117,7 @@ class WebKitTestController : public base::NonThreadSafe,
   Shell* main_window_;
 
   bool pumping_messages_;
+  bool renderer_crashed_;
   bool enable_pixel_dumping_;
   std::string expected_pixel_hash_;
 
