99613bc78a914491569833c028ccc893c0ed29fe
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-31 12:28:14 +0000

    Build fix for WinCE after r127191.
    * bytecode/JumpTable.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/JumpTable.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/JumpTable.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MacroAssembler.h"
 #include <wtf/HashMap.h>
 #include <wtf/Vector.h>
+#include <wtf/text/StringImpl.h>
 
 namespace JSC {
 
