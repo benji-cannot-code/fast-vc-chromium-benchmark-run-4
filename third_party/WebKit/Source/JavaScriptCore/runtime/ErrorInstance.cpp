6f50de5b94ade29100c8d714029d0e24abb394cd
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-31 00:00:46 +0000

    Try to fix the Qt build: add some #includes that, for some reason, only the Qt linker requires.
    * runtime/BooleanObject.cpp:
    * runtime/ErrorInstance.cpp:
    * runtime/NameInstance.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/ErrorInstance.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/ErrorInstance.cpp
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ErrorInstance.h"
 
+#include "JSScope.h"
+
 namespace JSC {
 
 ASSERT_HAS_TRIVIAL_DESTRUCTOR(ErrorInstance);
--- third_party/WebKit/Source/JavaScriptCore/runtime/NameInstance.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/NameInstance.cpp
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "NameInstance.h"
 
+#include "JSScope.h"
+
 namespace JSC {
 
 const ClassInfo NameInstance::s_info = { "Name", &Base::s_info, 0, 0, CREATE_METHOD_TABLE(NameInstance) };
