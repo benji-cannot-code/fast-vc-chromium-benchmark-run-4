3ea2f4add7860242658960e9e111c11f26ffd686
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-31 16:54:07 +0000

    [Qt] LayerTreeRenderer::callOnMainTread method name is misleading https://bugs.webkit.org/show_bug.cgi?id=95558
    Reviewed by Kenneth Rohde Christiansen.
    
    Fix the typo in the function name so callers will really call this
    and not WTF::callOnMainThread. Renamed to dispatchOnMainThread to
    make it clear that these things are not the same.
    
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp:
    (WebKit::LayerTreeRenderer::dispatchOnMainThread):
    (WebKit::LayerTreeRenderer::flushLayerChanges):
    (WebKit::LayerTreeRenderer::purgeGLResources):
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.h:
    (LayerTreeRenderer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127273 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp
@@ -67,7 +67,7 @@ private:
     }
 };
 
-void LayerTreeRenderer::callOnMainTread(const Function<void()>& function)
+void LayerTreeRenderer::dispatchOnMainThread(const Function<void()>& function)
 {
     if (isMainThread())
         function();
@@ -398,7 +398,7 @@ void LayerTreeRenderer::flushLayerChanges()
     commitTileOperations();
 
     // The pending tiles state is on its way for the screen, tell the web process to render the next one.
-    callOnMainThread(bind(&LayerTreeRenderer::renderNextFrame, this));
+    dispatchOnMainThread(bind(&LayerTreeRenderer::renderNextFrame, this));
 }
 
 void LayerTreeRenderer::renderNextFrame()
@@ -459,7 +459,7 @@ void LayerTreeRenderer::purgeGLResources()
 
     setActive(false);
 
-    callOnMainThread(bind(&LayerTreeRenderer::purgeBackingStores, this));
+    dispatchOnMainThread(bind(&LayerTreeRenderer::purgeBackingStores, this));
 }
 
 void LayerTreeRenderer::setAnimatedOpacity(uint32_t id, float opacity)
