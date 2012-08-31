fccac360d3f7e445e84dbd45c24e2ed27e678e7c
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-31 22:09:54 +0000

    [BlackBerry] Eliminate WebGL IT policy https://bugs.webkit.org/show_bug.cgi?id=95604 Internal RIM PR #198764
    Patch by Joshua Netterfield <jnetterfield@rim.com> on 2012-08-31
    Reviewed by Rob Buis.
    
    This is no longer a requirement for the BlackBerry port.
    
    Source/WebCore:
    
    No new tests, because this introduces no new functionality.
    
    * html/canvas/WebGLRenderingContext.cpp:
    (WebCore):
    (WebCore::WebGLRenderingContext::create):
    
    Source/WebKit/blackberry:
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::init):
    (BlackBerry::WebKit::WebPage::setWebGLEnabled):
    * WebKitSupport/AboutData.cpp:
    (BlackBerry::WebKit::configPage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -154,7 +154,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <BlackBerryPlatformDeviceInfo.h>
 #include <BlackBerryPlatformExecutableMessage.h>
-#include <BlackBerryPlatformITPolicy.h>
 #include <BlackBerryPlatformKeyboardEvent.h>
 #include <BlackBerryPlatformMessageClient.h>
 #include <BlackBerryPlatformMouseEvent.h>
@@ -570,8 +569,7 @@ void WebPagePrivate::init(const WebString& pageGroupName)
     m_inRegionScroller = adoptPtr(new InRegionScroller(this));
 
 #if ENABLE(WEBGL)
-    Platform::Settings* settings = Platform::Settings::instance();
-    m_page->settings()->setWebGLEnabled(settings && settings->isWebGLSupported());
+    m_page->settings()->setWebGLEnabled(true);
 #endif
 #if ENABLE(ACCELERATED_2D_CANVAS)
     m_page->settings()->setCanvasUsesAcceleratedDrawing(true);
@@ -6212,10 +6210,6 @@ void WebPagePrivate::scheduleCompositingRun()
 
 void WebPage::setWebGLEnabled(bool enabled)
 {
-    if (!Platform::ITPolicy::isWebGLEnabled()) {
-        d->m_page->settings()->setWebGLEnabled(false);
-        return;
-    }
     d->m_page->settings()->setWebGLEnabled(enabled);
 }
 
