7877d4d83c4b64803a6f4ff626ce5bd628942611
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-08-31 00:32:50 +0000

    Turning off the bezel device control functionality
    BUG=144708
    
    
    Review URL: https://chromiumcodereview.appspot.com/10911015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154329 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ui_base_switches.h
+++ ui/base/ui_base_switches.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace switches {
 
-UI_EXPORT extern const char kDisableBezelTouch[];
+UI_EXPORT extern const char kEnableBezelTouch[];
 UI_EXPORT extern const char kDisableScalingInImageSkiaOperations[];
 UI_EXPORT extern const char kEnableTextSubpixelPositioning[];
 UI_EXPORT extern const char kEnableTouchCalibration[];
