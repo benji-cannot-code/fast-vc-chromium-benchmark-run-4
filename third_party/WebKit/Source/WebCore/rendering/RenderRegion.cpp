7f55c27690507bc8b922981ab8f415375d684e7a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-08-31 15:50:05 +0000

    [CSS Regions] It should be possible to specify ::BEFORE/AFTER as regions https://bugs.webkit.org/show_bug.cgi?id=90079
    Patch by Andrei Bucur <abucur@adobe.com> on 2012-08-31
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    The calls for RenderRegion::node() have been replaced with RenderRegion::generatingNode() to allow pseudo-elements to have RenderRegion renderers.
    The compare function that establishes the region order was adapted to take into account pseudo-regions.
    A renderer for the pseudo-element is now generated if it's a region even though there's no content property specified.
    
    Test: fast/regions/content-flowed-into-pseudo-regions.html
    
    * rendering/RenderFlowThread.cpp:
    (WebCore::RenderFlowThread::computeOverflowStateForRegions):
    * rendering/RenderNamedFlowThread.cpp:
    (WebCore::compareRenderRegions):
    * rendering/RenderObjectChildList.cpp:
    (WebCore::ensureBeforeAfterContainer):
    (WebCore):
    (WebCore::RenderObjectChildList::updateBeforeAfterContent):
    * rendering/RenderRegion.cpp:
    (WebCore::RenderRegion::nodeAtPoint):
    (WebCore::RenderRegion::checkRegionStyle):
    (WebCore::RenderRegion::computeStyleInRegion):
    * rendering/RenderTreeAsText.cpp:
    (WebCore::writeRenderNamedFlowThreads):
    
    LayoutTests:
    
    The new ref-test has a block with both a :before and :after pseudo-elements marked as regions.
    In the block there's another region.
    
    The test verifies the content is correctly flowed (the region order is respected) in the region chain:
    :before region -> child region -> :after region
    
    As a region a pseudo-element should also be displayed if there's no content property specified in the style sheet.
    
    * fast/regions/content-flowed-into-pseudo-regions-expected.html: Added.
    * fast/regions/content-flowed-into-pseudo-regions.html: Added.
    * fast/regions/resources/region-style.css:
    (.regionArticleAfter::after, .regionArticleBefore::before):
    (.borderBoxNoFloat):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderRegion.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderRegion.cpp
@@ -151,7 +151,7 @@ bool RenderRegion::nodeAtPoint(const HitTestRequest& request, HitTestResult& res
         if (m_flowThread && m_flowThread->hitTestFlowThreadPortionInRegion(this, flowThreadPortionRect(), flowThreadPortionOverflowRect(), request, result, locationInContainer, LayoutPoint(adjustedLocation.x() + borderLeft() + paddingLeft(), adjustedLocation.y() + borderTop() + paddingTop())))
             return true;
         updateHitTestResult(result, locationInContainer.point() - toLayoutSize(adjustedLocation));
-        if (!result.addNodeToRectBasedTestResult(node(), locationInContainer, boundsRect))
+        if (!result.addNodeToRectBasedTestResult(generatingNode(), locationInContainer, boundsRect))
             return true;
     }
 
@@ -162,6 +162,8 @@ void RenderRegion::checkRegionStyle()
 {
     ASSERT(m_flowThread);
     bool customRegionStyle = false;
+
+    // FIXME: Region styling doesn't work for pseudo elements.
     if (node()) {
         Element* regionElement = static_cast<Element*>(node());
         customRegionStyle = view()->document()->styleResolver()->checkRegionStyle(regionElement);
@@ -400,6 +402,7 @@ PassRefPtr<RenderStyle> RenderRegion::computeStyleInRegion(const RenderObject* o
     ASSERT(!object->isAnonymous());
     ASSERT(object->node() && object->node()->isElementNode());
 
+    // FIXME: Region styling fails for pseudo-elements because the renderers don't have a node.
     Element* element = toElement(object->node());
     RefPtr<RenderStyle> renderObjectRegionStyle = object->view()->document()->styleResolver()->styleForElement(element, 0, DisallowStyleSharing, MatchAllRules, this);
 
