c8187a3be6d60eb4edf8958bc6f52a59e7af64ce
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-01 08:20:01 +0000

    Remove all-but-one use of WTF::String::operator+= from WebCore https://bugs.webkit.org/show_bug.cgi?id=95508
    Reviewed by Benjamin Poulain.
    
    This patch removes all the uses of WTF::String::operator+= from
    WebCore, except those in WorkerScriptLoader (which need a more delicate
    patch). There were actually a handful of legitimate use cases for += in
    this group. I've replaced them with calls to String::append rather than
    += so that we can remove += and encourage most contributors to use
    more efficient string idioms.
    
    (There are likely some more uses in WebCore hiding in port-specific
    code---this patch covers only those call sites found when compiling the
    chromium-mac port.)
    
    * inspector/InspectorStyleTextEditor.cpp:
    (WebCore::InspectorStyleTextEditor::insertProperty):
        - This complicated function looks really inefficient, but I didn't
          have the heart to rewrite it.
    * inspector/NetworkResourcesData.cpp:
    (WebCore::NetworkResourcesData::ResourceData::decodeDataToContent):
    * loader/cache/CachedCSSStyleSheet.cpp:
    (WebCore::CachedCSSStyleSheet::sheetText):
    (WebCore::CachedCSSStyleSheet::data):
    * loader/cache/CachedFont.cpp:
    (WebCore::CachedFont::ensureSVGFontData):
    * loader/cache/CachedScript.cpp:
    (WebCore::CachedScript::script):
    * loader/cache/CachedXSLStyleSheet.cpp:
    (WebCore::CachedXSLStyleSheet::data):
        - This decoder/flush pattern can probably be improved by combining
          the decode and flush operations, but I didn't do that in this
          patch.
    * page/FrameTree.cpp:
    (WebCore::FrameTree::uniqueChildName):
        - I found this code very amusing. It's worried enough about
          efficiency to give a big speech about why snprintf is safe, but
          then it implicitly performs a large number of mallocs and memcpy
          operations.
    * page/Page.cpp:
    (WebCore::Page::userStyleSheet):
    * platform/chromium/support/WebHTTPLoadInfo.cpp:
    (WebKit::addHeader):
    * platform/chromium/support/WebURLResponse.cpp:
    (WebKit::WebURLResponse::addHTTPHeaderField):
        - This header-appending idiom looks like a reasonable use case for
          String::append.
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::send):
    (WebCore::XMLHttpRequest::setRequestHeaderInternal):
    * xml/XPathFunctions.cpp:
    (WebCore::XPath::FunTranslate::evaluate):
        - Fixes 6 year old FIXME.
    * xml/parser/XMLDocumentParser.cpp:
    (WebCore::XMLDocumentParser::append):
    * xml/parser/XMLDocumentParser.h:
    (XMLDocumentParser):
    * xml/parser/XMLDocumentParserLibxml2.cpp:
    (WebCore::XMLDocumentParser::doEnd):
    * xml/parser/XMLDocumentParserQt.cpp:
    (WebCore::XMLDocumentParser::doEnd):
        - Changed m_originalSourceForTransform to a SegmentedString so that
          we don't need to malloc and copy the entire document every time
          we get more data from the network.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameTree.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameTree.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/StringExtras.h>
 #include <wtf/Vector.h>
 #include <wtf/text/CString.h>
+#include <wtf/text/StringBuilder.h>
 
 using std::swap;
 
@@ -151,29 +152,23 @@ AtomicString FrameTree::uniqueChildName(const AtomicString& requestedName) const
             break;
         chain.append(frame);
     }
-    String name;
-    name += framePathPrefix;
-    if (frame)
-        name += frame->tree()->uniqueName().string().substring(framePathPrefixLength,
-            frame->tree()->uniqueName().length() - framePathPrefixLength - framePathSuffixLength);
+    StringBuilder name;
+    name.append(framePathPrefix);
+    if (frame) {
+        name.append(frame->tree()->uniqueName().string().substring(framePathPrefixLength,
+            frame->tree()->uniqueName().length() - framePathPrefixLength - framePathSuffixLength));
+    }
     for (int i = chain.size() - 1; i >= 0; --i) {
         frame = chain[i];
-        name += "/";
-        name += frame->tree()->uniqueName();
+        name.append('/');
+        name.append(frame->tree()->uniqueName());
     }
 
-    // Suffix buffer has more than enough space for:
-    //     10 characters before the number
-    //     a number (20 digits for the largest 64-bit integer)
-    //     6 characters after the number
-    //     trailing null byte
-    // But we still use snprintf just to be extra-safe.
-    char suffix[40];
-    snprintf(suffix, sizeof(suffix), "/<!--frame%u-->-->", childCount());
+    name.appendLiteral("/<!--frame");
+    name.append(String::number(childCount()));
+    name.appendLiteral("-->-->");
 
-    name += suffix;
-
-    return AtomicString(name);
+    return name.toAtomicString();
 }
 
 inline Frame* FrameTree::scopedChild(unsigned index, TreeScope* scope) const
