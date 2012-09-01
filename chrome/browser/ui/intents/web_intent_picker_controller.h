93ad7541e24fabb7eb24554be93f51835bcc9b1a
smckay@chromium.org <smckay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-01 02:24:27 +0000

    Add UMA reporting by Action+Type histograms across a variety of web intent handling events.
    BUG=143998
    
    
    Review URL: https://chromiumcodereview.appspot.com/10891040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154574 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_controller.h
+++ chrome/browser/ui/intents/web_intent_picker_controller.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/favicon/favicon_service.h"
 #include "chrome/browser/intents/cws_intents_registry.h"
 #include "chrome/browser/intents/web_intents_registry.h"
+#include "chrome/browser/intents/web_intents_reporting.h"
 #include "chrome/browser/ui/intents/web_intent_picker_delegate.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -276,6 +277,12 @@ class WebIntentPickerController
 
   // Timer factory for minimum display time of "waiting" dialog.
   base::WeakPtrFactory<WebIntentPickerController> timer_factory_;
+
+  // Bucket identifier for UMA reporting. Saved off in a field
+  // to avoid repeated calculation of the bucket across
+  // multiple UMA calls. Should be recalculated each time
+  // |intents_dispatcher_| is set.
+  web_intents::UMABucket uma_bucket_;
 
   DISALLOW_COPY_AND_ASSIGN(WebIntentPickerController);
 };
