f341bce288299fc8c7a78f83ae1634208c152b14
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-01 06:12:24 +0000

    Disable IME for newly created tabs.
    Enable/disable input methods according to |text_input_type_| when a view
    is created, so that input methods don't eat keyboard shortcuts unexpectedly.
    
    Some time ago a similar CL caused regression in NPAPI Flash IME handling
    (http://code.google.com/p/chromium/issues/detail?id=75556#c62),
    but thanks to another CL r126347 later came in to save us, the current CL
    won't cause the regression anymore; Chrome now properly tracks plugin IME
    state so that it won't be disturbed by host-side handling.
    
    BUG=145196, 80772
    
    Review URL: https://chromiumcodereview.appspot.com/10896057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154604 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_win.cc
+++ content/browser/renderer_host/render_widget_host_view_win.cc
@@ -965,14 +965,9 @@ void RenderWidgetHostViewWin::TextInputStateChanged(
   // as true. We need to support "can_compose_inline=false" for PPAPI plugins
   // that may want to avoid drawing composition-text by themselves and pass
   // the responsibility to the browser.
-  bool is_enabled = (params.type != ui::TEXT_INPUT_TYPE_NONE &&
-      params.type != ui::TEXT_INPUT_TYPE_PASSWORD);
   if (text_input_type_ != params.type) {
     text_input_type_ = params.type;
-    if (is_enabled)
-      ime_input_.EnableIME(m_hWnd);
-    else
-      ime_input_.DisableIME(m_hWnd);
+    UpdateIMEState();
   }
 }
 
@@ -1266,6 +1261,7 @@ LRESULT RenderWidgetHostViewWin::OnCreate(CREATESTRUCT* create_struct) {
     SetToTouchMode();
   else
     SetToGestureMode();
+  UpdateIMEState();
 
   return 0;
 }
@@ -3053,6 +3049,15 @@ LRESULT RenderWidgetHostViewWin::OnQueryCharPosition(
   position->cLineHeight = target_rect.bottom - target_rect.top;
   position->rcDocument = document_rect;
   return 1;
+}
+
+void RenderWidgetHostViewWin::UpdateIMEState() {
+  if (text_input_type_ != ui::TEXT_INPUT_TYPE_NONE &&
+      text_input_type_ != ui::TEXT_INPUT_TYPE_PASSWORD) {
+    ime_input_.EnableIME(m_hWnd);
+  } else {
+    ime_input_.DisableIME(m_hWnd);
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
