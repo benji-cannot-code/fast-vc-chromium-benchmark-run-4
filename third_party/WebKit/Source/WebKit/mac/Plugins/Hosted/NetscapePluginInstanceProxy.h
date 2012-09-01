cde480837e719350ed0905a54c3177aa6f0cc111
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-01 16:14:12 +0000

    Added #include <JavaScriptCore/Identifier.h>. https://bugs.webkit.org/show_bug.cgi?id=95641.
    Patch by Mark Lam <mark.lam@apple.com> on 2012-09-01
    Reviewed by Dan Bernstein.
    
    The code references JSC::Indentifier but was previously depending on
    someone else to #include it. This results in build problems when
    building for the llint C loop configuration.
    
    * Plugins/Hosted/NetscapePluginInstanceProxy.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/Plugins/Hosted/NetscapePluginInstanceProxy.h
+++ third_party/WebKit/Source/WebKit/mac/Plugins/Hosted/NetscapePluginInstanceProxy.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef NetscapePluginInstanceProxy_h
 #define NetscapePluginInstanceProxy_h
 
+#include <JavaScriptCore/Identifier.h>
 #include <JavaScriptCore/JSGlobalData.h>
 #include <JavaScriptCore/Strong.h>
 #include <WebCore/Timer.h>
