0ae6d909f45d16f30dfa3685181b59311ca7e5fc
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-02 23:16:37 +0000

    Get rid of autofill/api directory.  We're not building an API for each component yet, and this vs. the chrome/browser/api directory was confusing several people.
    TBR=erikwright@chromium.org
    BUG=140037
    
    
    Review URL: https://chromiumcodereview.appspot.com/10920054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154647 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_manager_delegate.h
+++ chrome/browser/autofill/autofill_manager_delegate.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_AUTOFILL_API_AUTOFILL_MANAGER_DELEGATE_H_
-#define CHROME_BROWSER_AUTOFILL_API_AUTOFILL_MANAGER_DELEGATE_H_
+#ifndef CHROME_BROWSER_AUTOFILL_AUTOFILL_MANAGER_DELEGATE_H_
+#define CHROME_BROWSER_AUTOFILL_AUTOFILL_MANAGER_DELEGATE_H_
 
 namespace autofill {
 class PasswordGenerator;
@@ -60,4 +60,4 @@ class AutofillManagerDelegate {
 
 }  // namespace autofill
 
-#endif  // CHROME_BROWSER_AUTOFILL_API_AUTOFILL_MANAGER_DELEGATE_H_
+#endif  // CHROME_BROWSER_AUTOFILL_AUTOFILL_MANAGER_DELEGATE_H_
