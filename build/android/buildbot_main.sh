ecd625fe020d4879e78743e40e81d8986982850f
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-03 22:22:16 +0000

    Add android main annotated scripts
    - Also adding zip step to buildbot_main, buildbot_webkit_main
    
    BUG=138463
    
    Review URL: https://chromiumcodereview.appspot.com/10905055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154719 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot_main.sh
+++ build/android/buildbot_main.sh
@@ -16,4 +16,5 @@ BB_SRC_ROOT="$(cd "$(dirname $0)/../.."; pwd)"
 
 bb_baseline_setup "$BB_SRC_ROOT" "$@"
 bb_compile
+bb_zip_build
 
--- build/android/buildbot_webkit_tester.sh
+++ build/android/buildbot_webkit_tester.sh
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 #
-# Buildbot annotator script for the WebKit builder on the Canary waterfall.
+# Buildbot annotator script for the WebKit tester on the Canary waterfall.
 
 BB_SRC_ROOT="$(cd "$(dirname $0)/../.."; pwd)"
 . "${BB_SRC_ROOT}/build/android/buildbot_functions.sh"
@@ -15,5 +15,6 @@ BB_SRC_ROOT="$(cd "$(dirname $0)/../.."; pwd)"
 ## bb_force_bot_green_and_exit
 
 bb_baseline_setup "$BB_SRC_ROOT" "$@"
-bb_compile
-bb_zip_build
+bb_extract_build
+bb_reboot_phones
+bb_run_unit_tests
