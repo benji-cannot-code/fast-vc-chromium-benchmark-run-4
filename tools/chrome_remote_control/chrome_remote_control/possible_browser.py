5ab82708fb2ccf0cfa70d8f793ce622bc3ab87fd
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-03 04:50:36 +0000

    Add android support to chrome_remote_control
    BUG=144483
    
    
    Review URL: https://chromiumcodereview.appspot.com/10914032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154656 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/chrome_remote_control/chrome_remote_control/possible_browser.py
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+class PossibleBrowser(object):
+  """A browser that can be controlled.
+
+  Call Create() to launch the browser and begin manipulating it..
+  """
+
+  def __init__(self, type, options):
+    self.type = type
+    self._options = options
+
+  def __repr__(self):
+    return "PossibleBrowser(type=%s)" % self.type
+
+  def Create(self):
+    raise Exception("Must be implemented in subclass.")
--- tools/chrome_remote_control/examples/rendering_microbenchmark_test.py
+++ tools/chrome_remote_control/examples/rendering_microbenchmark_test.py
@@ -6,18 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import re
 import sys
-import time
-import optparse
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 import chrome_remote_control
 
 def Main(args):
-  parser = chrome_remote_control.BrowserOptions.CreateParser(
+  options = chrome_remote_control.BrowserOptions()
+  parser = options.CreateParser(
       "rendering_microbenchmark_test.py <sitelist>")
   # TODO(nduca): Add test specific options here, if any.
-  options, args = parser.parse_args()
+  options, args = parser.parse_args(args)
   if len(args) != 1:
     parser.print_usage()
     return 255
@@ -74,4 +73,4 @@ def Main(args):
   return 0
 
 if __name__ == "__main__":
-  sys.exit(Main(sys.argv))
+  sys.exit(Main(sys.argv[1:]))
