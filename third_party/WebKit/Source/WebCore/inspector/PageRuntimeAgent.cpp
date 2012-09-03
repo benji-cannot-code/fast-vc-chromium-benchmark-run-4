1957870307cc1439f0474cbd72d236230fdae752
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-03 13:20:02 +0000

    Web Inspector: rename JavaScriptContextManager to RuntimeModel for consistency. https://bugs.webkit.org/show_bug.cgi?id=95677
    Reviewed by Yury Semikhatsky.
    
    - renamed the file and the usages
    - renamed isolatedContextCreated to executionContextCreated for consistency.
    
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * inspector/Inspector.json:
    * inspector/PageRuntimeAgent.cpp:
    (WebCore::PageRuntimeAgent::notifyContextCreated):
    * inspector/compile-front-end.py:
    * inspector/front-end/RuntimeModel.js: Renamed from Source/WebCore/inspector/front-end/JavaScriptContextManager.js.
    (WebInspector.RuntimeModel):
    (WebInspector.RuntimeModel.prototype._frameAdded):
    (WebInspector.RuntimeModel.prototype._frameDetached):
    (WebInspector.RuntimeModel.prototype._executionContextCreated):
    (WebInspector.RuntimeDispatcher):
    (WebInspector.RuntimeDispatcher.prototype.executionContextCreated):
    * inspector/front-end/WebKit.qrc:
    * inspector/front-end/inspector.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127417 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/PageRuntimeAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/PageRuntimeAgent.cpp
@@ -161,7 +161,7 @@ void PageRuntimeAgent::notifyContextCreated(const String& frameId, ScriptState* 
     ASSERT(securityOrigin || isPageContext);
     int executionContextId = injectedScriptManager()->injectedScriptIdFor(scriptState);
     String name = securityOrigin ? securityOrigin->toString() : "";
-    m_frontend->isolatedContextCreated(ExecutionContextDescription::create()
+    m_frontend->executionContextCreated(ExecutionContextDescription::create()
         .setId(executionContextId)
         .setIsPageContext(isPageContext)
         .setName(name)
