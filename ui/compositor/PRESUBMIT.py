73dcef07a226239ead1981361d782d058839f51c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-03 03:34:48 +0000

    ui: Fix typos in PRESUBMIT.py scripts.
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10918041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154655 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/PRESUBMIT.py
+++ ui/compositor/PRESUBMIT.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Chromium presubmit script for src/chrome/browser/externsions.
+"""Chromium presubmit script for src/ui/compositor/.
 
 See http://dev.chromium.org/developers/how-tos/depottools/presubmit-scripts
 for more details on the presubmit API built into gcl.
