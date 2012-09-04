f5ab85ab0befc2ef2bb4080430dfc8e9df21a2c9
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 21:09:26 +0000

    Cleanup buildbot step names
    - unify unittest step names with chromium in preparation for
      CQ / gatekeeper
    - Simplify instrumentation test step names for clarity
      and because stats is broken on testers due to symbols in 
      the step names.
     [http://build.chromium.org/p/chromium.fyi/stats/Chromium%20Linux%20Android%20Tester]
    
    BUG=138463
    
    Review URL: https://chromiumcodereview.appspot.com/10910056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154810 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot_functions.sh
+++ build/android/buildbot_functions.sh
@@ -43,7 +43,6 @@ function bb_force_bot_green_and_exit {
 }
 
 function bb_run_gclient_hooks {
-  echo "@@@BUILD_STEP runhooks android@@@"
   gclient runhooks
 }
 
@@ -248,7 +247,6 @@ function bb_run_unit_tests {
   rm -rf "$LOGCAT_DUMP_DIR"
   python build/android/adb_logcat_monitor.py "$LOGCAT_DUMP_DIR" &
 
-  echo "@@@BUILD_STEP Run Tests on actual hardware@@@"
   build/android/run_tests.py --xvfb --verbose
 
   echo "@@@BUILD_STEP Logcat dump@@@"
@@ -262,9 +260,6 @@ function bb_run_unit_tests {
 function bb_run_instrumentation_test {
   local TEST_APK=${1}
   local EXTRA_FLAGS=${2}
-  local APK_NAME=$(basename ${TEST_APK})
-  echo "@@@BUILD_STEP Android Instrumentation ${APK_NAME} ${EXTRA_FLAGS} "\
-       "on actual hardware@@@"
   local INSTRUMENTATION_FLAGS="-vvv"
   INSTRUMENTATION_FLAGS+=" --test-apk ${TEST_APK}"
   INSTRUMENTATION_FLAGS+=" ${EXTRA_FLAGS}"
