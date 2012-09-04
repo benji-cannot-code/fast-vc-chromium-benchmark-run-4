14a5a295649553fa531f3a64bd0296e09636a6be
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 22:16:10 +0000

    ash: Allow the shelf to be dragged in from the bottom edge of the screen.
    BUG=145406
    
    Review URL: https://chromiumcodereview.appspot.com/10911075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154823 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/gestures/bezel_gesture_handler.h
+++ ash/wm/gestures/bezel_gesture_handler.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 
+#include "ash/wm/gestures/shelf_gesture_handler.h"
+
 namespace aura {
 class Window;
 }
@@ -114,6 +116,8 @@ class BezelGestureHandler {
 
   // True if device control bezel operations (brightness, volume) are enabled.
   bool enable_bezel_device_control_;
+
+  ShelfGestureHandler shelf_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(BezelGestureHandler);
 };
