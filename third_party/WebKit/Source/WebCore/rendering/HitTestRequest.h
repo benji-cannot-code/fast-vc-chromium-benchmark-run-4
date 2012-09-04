e4379028b7481643ce05e113be9fd8dcaff9af62
allan.jensen@nokia.com <allan.jensen@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-04 11:56:46 +0000

    Allow child-frame content in hit-tests. https://bugs.webkit.org/show_bug.cgi?id=95204
    .:
    
    Reviewed by Antonio Gomes.
    
    Update exported symbols.
    
    * Source/autotools/symbols.filter:
    
    Source/WebCore:
    
    Reviewed by Antonio Gomes.
    
    Refactors how EventHandler::hitTestResultAtPoint handles child-frame content,
    it is now handled by the hit test itself controlled by the AllowChildFrameContent
    flag in HitTestRequest.
    
    Tests: fast/dom/nodesFromRect/nodesFromRect-child-frame-content.html
           touchadjustment/iframe-boundary.html
    
    * WebCore.exp.in:
    * WebCore.order:
    * dom/Document.cpp:
    (WebCore::Document::nodesFromRect):
    * dom/Document.h:
    (Document):
    * page/EventHandler.cpp:
    (WebCore::EventHandler::hitTestResultAtPoint):
    * rendering/HitTestRequest.h:
    (WebCore::HitTestRequest::allowChildFrameContent):
    (WebCore::HitTestRequest::childFrameHitTest):
    (WebCore::HitTestRequest::testChildFrameScrollBars):
    * rendering/RenderFrameBase.cpp:
    (WebCore::RenderFrameBase::nodeAtPoint):
    (WebCore):
    * rendering/RenderFrameBase.h:
    (RenderFrameBase):
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::hitTest):
    * testing/Internals.cpp:
    (WebCore::Internals::nodesFromRect):
    * testing/Internals.h:
    (Internals):
    * testing/Internals.idl:
    
    Source/WebKit2:
    
    Reviewed by Antonio Gomes.
    
    Update symbols for the Windows build-system.
    
    * win/WebKit2.def:
    * win/WebKit2CFLite.def:
    
    LayoutTests:
    
    Reviewed by Antonio Gomes.
    Touch-Adjustment test by Kevin Ellis
    
    Two new tests for hit-testing child frame content. One for testing it as an option to
    nodesFromRect, and one testing its improvement on touch adjustment on iframe boundaries.
    
    * fast/dom/nodesFromRect/nodesFromRect-child-frame-content-expected.txt: Added.
    * fast/dom/nodesFromRect/nodesFromRect-child-frame-content.html: Added.
    * fast/dom/nodesFromRect/resources/child-frame.html: Added.
    * fast/dom/nodesFromRect/resources/nodesFromRect.js:
    (check):
    (checkShadowContent):
    (checkRect):
    (nodesFromRectAsString):
    * touchadjustment/iframe-boundary-expected.txt: Added.
    * touchadjustment/iframe-boundary.html: Added.
    * touchadjustment/resources/inner-content-page.html: Added.
    * touchadjustment/resources/inner-navigation-frame.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127457 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/HitTestRequest.h
+++ third_party/WebKit/Source/WebCore/rendering/HitTestRequest.h
@@ -36,7 +36,10 @@ public:
         IgnoreClipping = 1 << 5,
         SVGClipContent = 1 << 6,
         TouchEvent = 1 << 7,
-        AllowShadowContent = 1 << 8
+        AllowShadowContent = 1 << 8,
+        AllowChildFrameContent = 1 << 9,
+        ChildFrameHitTest = 1 << 10,
+        TestChildFrameScrollBars = 1 << 11
     };
 
     typedef unsigned HitTestRequestType;
@@ -55,6 +58,9 @@ public:
     bool touchEvent() const { return m_requestType & TouchEvent; }
     bool mouseEvent() const { return !touchEvent(); }
     bool allowsShadowContent() const { return m_requestType & AllowShadowContent; }
+    bool allowsChildFrameContent() const { return m_requestType & AllowChildFrameContent; }
+    bool isChildFrameHitTest() const { return m_requestType & ChildFrameHitTest; }
+    bool shouldTestChildFrameScrollBars() const { return m_requestType & TestChildFrameScrollBars; }
 
     // Convenience functions
     bool touchMove() const { return move() && touchEvent(); }
