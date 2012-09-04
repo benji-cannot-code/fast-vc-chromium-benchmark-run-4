17fc0ae0b0e3e18afb54e87f4ee7a6c4a1be3221
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 18:50:48 +0000

    Android: Use external storage for test data.
    Also increasing priority for logging test markers.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10896050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154786 0039d316-1c4b-4281-b951-d872f2087c98

--- base/test/test_support_android.cc
+++ base/test/test_support_android.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdarg.h>
 #include <string.h>
 
+#include "base/android/path_utils.h"
 #include "base/file_path.h"
 #include "base/logging.h"
 #include "base/memory/singleton.h"
@@ -15,10 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 
 namespace {
-
-// The test implementation of AndroidOS stores everything in the following
-// directory.
-const char* kAndroidTestTempDirectory = "/data/local/tmp";
 
 struct RunState {
   RunState(base::MessagePump::Delegate* delegate, int run_depth)
@@ -142,15 +139,16 @@ base::MessagePump* CreateMessagePumpForUIStub() {
   return new MessagePumpForUIStub();
 };
 
-// Provides the test path for DIR_MODULE, DIR_CACHE and DIR_ANDROID_APP_DATA.
+// Provides the test path for DIR_MODULE and DIR_ANDROID_APP_DATA.
 bool GetTestProviderPath(int key, FilePath* result) {
   switch (key) {
     case base::DIR_MODULE: {
-      *result = FilePath(kAndroidTestTempDirectory);
+      *result = FilePath(base::android::GetExternalStorageDirectory());
       return true;
     }
     case base::DIR_ANDROID_APP_DATA: {
-      *result = FilePath(kAndroidTestTempDirectory);
+      // For tests, app data is put in external storage.
+      *result = FilePath(base::android::GetExternalStorageDirectory());
       return true;
     }
     default:
@@ -184,7 +182,6 @@ void InitAndroidTestLogging() {
 
 void InitAndroidTestPaths() {
   InitPathProvider(DIR_MODULE);
-  InitPathProvider(DIR_CACHE);
   InitPathProvider(DIR_ANDROID_APP_DATA);
 }
 
