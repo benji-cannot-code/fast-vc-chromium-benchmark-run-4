0769e193e82402cc1d8dd1f1617b2e0a8904dd8d
zvorygin@chromium.org <zvorygin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 16:17:44 +0000

    Added context to commands.
    Command should have context, to simplify command handling and distinguish elements, on which command was invoked.
    
    BUG=141096
    
    Review URL: https://chromiumcodereview.appspot.com/10854024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154759 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/menu.js
+++ chrome/browser/resources/shared/js/cr/ui/menu.js
@@ -8,7 +8,9 @@ cr.define('cr.ui', function() {
   /** @const */ var MenuItem = cr.ui.MenuItem;
 
   /**
-   * Creates a new menu element.
+   * Creates a new menu element. Menu dispatches all commands on the element it
+   * was shown for.
+   *
    * @param {Object=} opt_propertyBag Optional properties.
    * @constructor
    * @extends {HTMLMenuElement}
@@ -19,6 +21,11 @@ cr.define('cr.ui', function() {
     __proto__: HTMLMenuElement.prototype,
 
     selectedIndex_: -1,
+
+    /**
+     * Element for which menu is being shown.
+     */
+    contextElement: null,
 
     /**
      * Initializes the menu element.
@@ -183,6 +190,17 @@ cr.define('cr.ui', function() {
       }
 
       return false;
+    },
+
+    /**
+     * Updates menu items command according to context.
+     * @param {Node=} node Node for which to actuate commands state.
+     */
+    updateCommands: function(node) {
+      var children = this.children;
+
+      for (var i = 0, child; child = children[i]; i++)
+        child.updateCommand(node);
     }
   };
 
@@ -194,6 +212,7 @@ cr.define('cr.ui', function() {
     if (item)
       item.selected = true;
   }
+
   /**
    * The selected menu item.
    * @type {number}
