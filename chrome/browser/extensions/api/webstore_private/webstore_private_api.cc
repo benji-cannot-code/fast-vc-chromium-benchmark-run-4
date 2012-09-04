735e2b17067841b0236d83f8193c68680b7e0091
tbreisacher@chromium.org <tbreisacher@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 17:52:11 +0000

    Fix memory leak in GetWebGLStatusFunction
    BUG=145742
    TEST=valgrind is happy
    
    Review URL: https://chromiumcodereview.appspot.com/10918013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154772 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/webstore_private/webstore_private_api.h"
 
+#include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/lazy_instance.h"
 #include "base/memory/scoped_vector.h"
@@ -478,7 +479,8 @@ bool SetStoreLoginFunction::RunImpl() {
 GetWebGLStatusFunction::GetWebGLStatusFunction() {
   feature_checker_ = new GPUFeatureChecker(
       content::GPU_FEATURE_TYPE_WEBGL,
-      base::Bind(&GetWebGLStatusFunction::OnFeatureCheck, this));
+      base::Bind(&GetWebGLStatusFunction::OnFeatureCheck,
+          base::Unretained(this)));
 }
 
 GetWebGLStatusFunction::~GetWebGLStatusFunction() {}
