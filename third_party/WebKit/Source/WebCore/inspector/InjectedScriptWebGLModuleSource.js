f2c26e29755761516716ad181b3585f4d0b0f7e5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-04 09:37:41 +0000

    Web Inspector: [WebGL] Save gl.getError() status before taking a WebGL state snapshot and restore it afterwards https://bugs.webkit.org/show_bug.cgi?id=95443
    Patch by Andrey Adaikin <aandrey@chromium.org> on 2012-09-04
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    Save gl.getError() status before taking the GL snapshot and restore it afterwards.
    
    * inspector/InjectedScriptWebGLModuleSource.js:
    (.):
    
    LayoutTests:
    
    Adds a test to check that we properly save the WebGL getError() status while doing the instrumentation.
    
    * inspector/profiler/webgl/webgl-profiler-get-error-expected.txt: Added.
    * inspector/profiler/webgl/webgl-profiler-get-error.html: Added.
    * inspector/profiler/webgl/webgl-profiler-test.js: Added.
    (initialize_WebGLProfilerTest.InspectorTest.enableWebGLAgent):
    (initialize_WebGLProfilerTest):
    (createWebGLContext):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127452 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InjectedScriptWebGLModuleSource.js
+++ third_party/WebKit/Source/WebCore/inspector/InjectedScriptWebGLModuleSource.js
@@ -340,6 +340,14 @@ Call.prototype = {
         this._stackTrace = stackTrace;
     },
 
+    /**
+     * @param {*} result
+     */
+    setResult: function(result)
+    {
+        this._result = result;
+    },
+
     freeze: function()
     {
         if (this._freezed)
@@ -896,6 +904,8 @@ WebGLProgramResource.prototype = {
         var gl = glResource.wrappedObject();
         var program = this.wrappedObject();
 
+        var originalErrors = glResource.getAllErrors();
+
         var uniforms = [];
         var uniformsCount = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
         for (var i = 0; i < uniformsCount; ++i) {
@@ -913,6 +923,8 @@ WebGLProgramResource.prototype = {
             });
         }
         data.uniforms = uniforms;
+
+        glResource.restoreErrors(originalErrors);
     },
 
     /**
@@ -1216,6 +1228,79 @@ WebGLRenderingContextResource.prototype = {
     },
 
     /**
+     * @return {Array.<number>}
+     */
+    getAllErrors: function()
+    {
+        var errors = [];
+        var gl = this.wrappedObject();
+        if (gl) {
+            while (true) {
+                var error = gl.getError();
+                if (error === gl.NO_ERROR)
+                    break;
+                this.clearError(error);
+                errors.push(error);
+            }
+        }
+        if (this._customErrors) {
+            for (var key in this._customErrors) {
+                var error = Number(key);
+                errors.push(error);
+            }
+            delete this._customErrors;
+        }
+        return errors;
+    },
+
+    /**
+     * @param {Array.<number>} errors
+     */
+    restoreErrors: function(errors)
+    {
+        var gl = this.wrappedObject();
+        if (gl) {
+            var wasError = false;
+            while (gl.getError() !== gl.NO_ERROR)
+                wasError = true;
+            console.assert(!wasError, "Error(s) while capturing current WebGL state.");
+        }
+        if (!errors.length)
+            delete this._customErrors;
+        else {
+            this._customErrors = {};
+            for (var i = 0, n = errors.length; i < n; ++i)
+                this._customErrors[errors[i]] = true;
+        }
+    },
+
+    /**
+     * @param {number} error
+     */
+    clearError: function(error)
+    {
+        if (this._customErrors)
+            delete this._customErrors[error];
+    },
+
+    /**
+     * @return {number}
+     */
+    nextError: function()
+    {
+        if (this._customErrors) {
+            for (var key in this._customErrors) {
+                var error = Number(key);
+                delete this._customErrors[error];
+                return error;
+            }
+        }
+        delete this._customErrors;
+        var gl = this.wrappedObject();
+        return gl ? gl.NO_ERROR : 0;
+    },
+
+    /**
      * @override
      * @param {Object} data
      * @param {Cache} cache
@@ -1225,7 +1310,7 @@ WebGLRenderingContextResource.prototype = {
         var gl = this.wrappedObject();
         data.replayContextCallback = this._replayContextCallback;
 
-        // FIXME: Save the getError() status and restore it after taking the GL state snapshot.
+        var originalErrors = this.getAllErrors();
 
         // Take a full GL state snapshot.
         var glState = {};
@@ -1234,7 +1319,6 @@ WebGLRenderingContextResource.prototype = {
         });
         WebGLRenderingContextResource.StateParameters.forEach(function(parameter) {
             glState[parameter] = Resource.toReplayable(gl.getParameter(gl[parameter]), cache);
-            // FIXME: Call while(gl.getError() != gl.NO_ERROR) {...} to check if a particular parameter is supported.
         });
 
         // VERTEX_ATTRIB_ARRAYS
@@ -1267,6 +1351,8 @@ WebGLRenderingContextResource.prototype = {
         gl.activeTexture(currentTextureBinding);
 
         data.glState = glState;
+
+        this.restoreErrors(originalErrors);
     },
 
     /**
@@ -1547,6 +1633,16 @@ WebGLRenderingContextResource.WrapFunction.prototype = {
         if (!this._call)
             this._call = new Call(this._glResource, this._functionName, this._args, this.result());
         return this._call;
+    },
+
+    /**
+     * @param {*} result
+     */
+    _overrideResult: function(result)
+    {
+        var call = this.call();
+        call.setResult(result);
+        this._result = result;
     }
 }
 
@@ -1627,6 +1723,20 @@ WebGLRenderingContextResource.wrapFunctions = function()
         customWrapFunction("framebufferRenderbuffer");
         customWrapFunction("framebufferTexture2D");
         customWrapFunction("renderbufferStorage");
+
+        /** @this WebGLRenderingContextResource.WrapFunction */
+        wrapFunctions["getError"] = function()
+        {
+            var gl = this._originalObject;
+            var error = this.result();
+            if (error !== gl.NO_ERROR)
+                this._glResource.clearError(error);
+            else {
+                error = this._glResource.nextError();
+                if (error !== gl.NO_ERROR)
+                    this._overrideResult(error);
+            }
+        }
 
         WebGLRenderingContextResource._wrapFunctions = wrapFunctions;
     }
