3176e96657091d8b18aacceffeca650f408b232d
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 19:42:25 +0000

    [Mac] Nil out the NSMenu delegates in BookmarkBarFolderController in |-dealloc|.
    BUG=129833
    TEST=No repro. Look at crash server.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10910066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154797 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_folder_controller.mm
@@ -279,6 +279,8 @@ struct LayoutMetrics {
     [button setTarget:nil];
     [button setAction:nil];
   }
+  [buttonMenu_ setDelegate:nil];
+  [folderMenu_ setDelegate:nil];
 
   // Note: we don't need to
   //   [NSObject cancelPreviousPerformRequestsWithTarget:self];
