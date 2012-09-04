0b7add4bd8eca22196858a4abfe7b1901b41a1b7
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-04 18:04:23 +0000

    Use correct JS getter name to get old attribute value in setter.
    BUG=146342
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/10919072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154776 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr.js
+++ chrome/browser/resources/shared/js/cr.js
@@ -134,7 +134,7 @@ this.cr = (function() {
       case PropertyKind.JS:
         var privateName = name + '_';
         return function(value) {
-          var oldValue = this[privateName];
+          var oldValue = this[name];
           if (value !== oldValue) {
             this[privateName] = value;
             if (opt_setHook)
@@ -146,7 +146,7 @@ this.cr = (function() {
       case PropertyKind.ATTR:
         var attributeName = getAttributeName(name);
         return function(value) {
-          var oldValue = this[attributeName];
+          var oldValue = this[name];
           if (value !== oldValue) {
             if (value == undefined)
               this.removeAttribute(attributeName);
@@ -161,7 +161,7 @@ this.cr = (function() {
       case PropertyKind.BOOL_ATTR:
         var attributeName = getAttributeName(name);
         return function(value) {
-          var oldValue = this[attributeName];
+          var oldValue = this[name];
           if (value !== oldValue) {
             if (value)
               this.setAttribute(attributeName, name);
