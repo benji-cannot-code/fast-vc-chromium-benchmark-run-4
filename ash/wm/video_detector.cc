bfe34025619494cd7753d34fb879709d56be45d6
rharrison@chromium.org <rharrison@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 07:16:23 +0000

    Add is_fullscreen to video updates
    This adds a boolean to the video update message sent to powerd that indicates whether or not the video playing window is fullscreen'd. This is information used in powerd for controlling when to enable/disable keyboard backlights. Specifically if video is playing and fullscreen we assume that the user is doing something like watching a movie so will disable backlight.
        
    An additional change that I have made is to convert the message being sent from an int64 to a protobuffer that contains a int64 and a boolean. Currently, since the receiver of this message does not live in the same repo it means that every time one wants to change the message format there is a small window where bad builds can be generated or one has to land two changes to the other repo, one to add handling of the new message format and then a second one to remove the old format. Converting to a protobuffer means that for common cases this issues are removed.
        
    This CL depends on CLs for power_manager (https://gerrit.chromium.org/gerrit/32092/), one for system_api (https://gerrit.chromium.org/gerrit/31916), and a DEPS roll(http://codereview.chromium.org/10915032/). There will be future CLs for power_manager that will use this information and test this code.
        
    BUG=chrome-os-partner:9203
    TEST=Run updated unittests.
         Confirm there is no messages in the cros logs about this method being mishandled.
    
    Review URL: https://chromiumcodereview.appspot.com/10905026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154905 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/video_detector.cc
+++ ash/wm/video_detector.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/video_detector.h"
 
 #include "ash/shell.h"
+#include "ash/wm/window_util.h"
 #include "ui/aura/env.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
@@ -104,7 +105,10 @@ void VideoDetector::MaybeNotifyObservers(aura::Window* window,
   if (!window->GetBoundsInRootWindow().Intersects(root_bounds))
     return;
 
-  FOR_EACH_OBSERVER(VideoDetectorObserver, observers_, OnVideoDetected());
+  bool is_fullscreen = wm::IsWindowFullscreen(window);
+  FOR_EACH_OBSERVER(VideoDetectorObserver,
+                    observers_,
+                    OnVideoDetected(is_fullscreen));
   last_observer_notification_time_ = now;
 }
 
