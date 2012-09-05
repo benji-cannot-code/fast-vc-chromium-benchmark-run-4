e0831516817f04da67578cd36adceac20275e030
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 04:24:52 +0000

    Make IndexedDB layout browser tests save incorrect results to a file
    Like it says in the bug, this won't help poring over a log on a bot, you'd still have to copy/paste and then unescape those results manually. But outputting to a file is so helpful when running a small amount of tests locally in a develop-compile-test cycle.
    
    The expected and actual results of the test that inspired this are each >3k, of which about 100 bytes differed.
    
    BUG=144786
    
    Review URL: https://chromiumcodereview.appspot.com/10869061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154896 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_switches.h
+++ content/shell/shell_switches.h
@@ -15,6 +15,7 @@ extern const char kContentBrowserTest[];
 extern const char kContentShellDataPath[];
 extern const char kDumpRenderTree[];
 extern const char kNoTimeout[];
+extern const char kOutputLayoutTestDifferences[];
 
 }  // namespace switches
 
--- content/test/layout_browsertest.cc
+++ content/test/layout_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/test/layout_browsertest.h"
 
+#include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_paths.h"
 #include "content/public/test/browser_test_utils.h"
 #include "content/shell/shell.h"
+#include "content/shell/shell_switches.h"
 #include "content/test/content_browser_test_utils.h"
 #include "content/test/layout_test_http_server.h"
 #include "net/base/net_util.h"
@@ -216,7 +218,13 @@ void InProcessBrowserLayoutTest::RunLayoutTestInternal(
   ReplaceSubstringsAfterOffset(&expected_text, 0, "\r", "");
   TrimString(expected_text, "\n", &expected_text);
 
-  EXPECT_EQ(expected_text, actual_text);
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kOutputLayoutTestDifferences)) {
+    EXPECT_EQ(expected_text, actual_text) <<
+        SaveResults(expected_text, actual_text);
+  } else {
+    EXPECT_EQ(expected_text, actual_text);
+  }
 }
 
 void InProcessBrowserLayoutTest::AddResourceForLayoutTest(
@@ -256,4 +264,21 @@ void InProcessBrowserLayoutTest::WriteModifiedFile(
   ASSERT_TRUE(file_util::WriteFile(*test_path,
                                    &test_html.at(0),
                                    static_cast<int>(test_html.size())));
+}
+
+std::string InProcessBrowserLayoutTest::SaveResults(const std::string& expected,
+                                                    const std::string& actual) {
+  FilePath cwd;
+  EXPECT_TRUE(file_util::CreateNewTempDirectory(FILE_PATH_LITERAL(""), &cwd));
+  FilePath expected_filename = cwd.Append(FILE_PATH_LITERAL("expected.txt"));
+  FilePath actual_filename = cwd.Append(FILE_PATH_LITERAL("actual.txt"));
+  EXPECT_NE(-1, file_util::WriteFile(expected_filename,
+                                     expected.c_str(),
+                                     expected.size()));
+  EXPECT_NE(-1, file_util::WriteFile(actual_filename,
+                                     actual.c_str(),
+                                     actual.size()));
+  return StringPrintf("Wrote %"PRFilePath" %"PRFilePath,
+                      expected_filename.value().c_str(),
+                      actual_filename.value().c_str());
 }
