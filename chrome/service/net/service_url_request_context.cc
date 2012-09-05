92524683d6d2b7057eef819add9ae8606179a9f2
scottbyer@chromium.org <scottbyer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 23:35:24 +0000

    Bring back service process throttler.
    Somewhere along the way, the service process lost it's throttler. Under certain
    conditions (network going away) that meant that the service process could spin
    trying to access something, eating up CPU.
    
    BUG=140019
    TEST=(Mac) Enable cloud print, disable network. Before this change, after 30 minutes, the Chromium Helper process would be >100% CPU in Activity Monitor. After this change, should be near nothing.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10919102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155050 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/net/service_url_request_context.cc
+++ chrome/service/net/service_url_request_context.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_server_properties_impl.h"
 #include "net/proxy/proxy_config_service.h"
 #include "net/proxy/proxy_service.h"
+#include "net/url_request/url_request_throttler_manager.h"
 
 namespace {
 // Copied from webkit/glue/user_agent.cc. We don't want to pull in a dependency
@@ -124,6 +125,7 @@ ServiceURLRequestContext::ServiceURLRequestContext(
       net::HttpAuthHandlerFactory::CreateDefault(host_resolver()));
   storage_.set_http_server_properties(new net::HttpServerPropertiesImpl);
   storage_.set_transport_security_state(new net::TransportSecurityState);
+  storage_.set_throttler_manager(new net::URLRequestThrottlerManager);
 
   net::HttpNetworkSession::Params session_params;
   session_params.host_resolver = host_resolver();
