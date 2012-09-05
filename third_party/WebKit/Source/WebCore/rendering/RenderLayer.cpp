4cebf75f79230eafae1de7aaad25ac765bf9d43c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-05 18:53:29 +0000

    Enable/disable composited scrolling based on overflow https://bugs.webkit.org/show_bug.cgi?id=95323
    Patch by Sami Kyostila <skyostil@google.com> on 2012-09-05
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    When an overflow:{auto,overlay} and -webkit-overflow-scrolling:touch
    element gains or loses overflow, we should correspondingly enable and
    disable composited scrolling depending on whether the element can be
    scrolled or not.
    
    The previous logic in RenderLayer::usesCompositedScrolling() already
    checked for actual overflow, but we also need to recompute the
    compositing requirements when the amount of overflow changes during
    layout. Additionally, layers using composited scrolling are marked as
    self-painting to ensure they are always promoted to composited layers
    when needed.
    
    Test: compositing/overflow/overflow-auto-with-touch-toggle.html
    
    Note that the behavior for maintaining a stacking context even without
    the presense of overflow is covered by platform/chromium/compositing/overflow/overflow-scrolling-touch-stacking-context.html.
    
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::updateScrollInfoAfterLayout):
    (WebCore::RenderLayer::shouldBeSelfPaintingLayer):
    
    LayoutTests:
    
    Added a new test for making sure composited scrolling is enabled when overflow
    is introduced.
    
    Note that this test will pass only if OVERFLOW_SCROLLING is enabled.
    
    * compositing/overflow/overflow-auto-with-touch-toggle-expected.txt: Added.
    * compositing/overflow/overflow-auto-with-touch-toggle.html: Added.
    * platform/chromium/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -2656,6 +2656,12 @@ void RenderLayer::updateScrollInfoAfterLayout()
 
     if (originalScrollOffset != scrollOffset())
         scrollToOffsetWithoutAnimation(toPoint(scrollOffset()));
+
+#if USE(ACCELERATED_COMPOSITING)
+    // Composited scrolling may need to be enabled or disabled if the amount of overflow changed.
+    if (renderer()->view() && compositor()->updateLayerCompositingState(this))
+        compositor()->setCompositingLayersNeedRebuild();
+#endif
 }
 
 void RenderLayer::paintOverflowControls(GraphicsContext* context, const IntPoint& paintOffset, const IntRect& damageRect, bool paintingOverlayControls)
@@ -4848,6 +4854,7 @@ bool RenderLayer::shouldBeSelfPaintingLayer() const
 {
     return !isNormalFlowOnly()
         || hasOverlayScrollbars()
+        || usesCompositedScrolling()
         || renderer()->hasReflection()
         || renderer()->hasMask()
         || renderer()->isTableRow()
