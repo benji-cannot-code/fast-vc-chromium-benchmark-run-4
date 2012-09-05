f13f3648d8a497a4801fa321fb1693f2904ae6a0
barraclough@apple.com <barraclough@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-05 02:36:45 +0000

    inc/dec behave incorrectly operating on a resolved const https://bugs.webkit.org/show_bug.cgi?id=95815
    Reviewed by Geoff Garen.
    
    Source/JavaScriptCore: 
    
    There are two bugs here.
    
    (1) When the value being incremented is const, and the result is ignored, we assume this cannot be observed, and emit no code.
        However if the value being incremented is not a primitive & has a valueOf conversion, then this should be being called.
    
    (2) In the case of a pre-increment of a const value where the result is not ignored, we'll move +/-1 to the destination, then
        add the resolved const value being incremented to this. This is problematic if the destination is a local, and the const
        value being incremented has a valueOf conversion that throws - the destination will be modified erroneously. Instead, we
        need to use a temporary location.
    
    * bytecompiler/NodesCodegen.cpp:
    (JSC::PostfixResolveNode::emitBytecode):
    (JSC::PrefixResolveNode::emitBytecode):
        - always at least perform a toNumber conversion, use tempDestination when reducing inc/dec to an add +/-1.
    
    LayoutTests: 
    
    Added test cases.
    
    * fast/js/inc-const-valueOf-expected.txt: Added.
    * fast/js/inc-const-valueOf.html: Added.
    * fast/js/script-tests/inc-const-valueOf.js: Added.
    (testPostIncConstVarWithIgnoredResult.const.a.valueOf):
    (testPostIncConstVarWithIgnoredResult):
        test that 'a++' results in a valueOf call, where 'a' is const.
    (testPreIncConstVarWithIgnoredResult.const.a.valueOf):
    (testPreIncConstVarWithIgnoredResult):
        test that '++a' results in a valueOf call, where 'a' is const.
    (testPreIncConstVarWithAssign.const.a.valueOf):
    (testPreIncConstVarWithAssign):
        test that 'b = ++a' does not erroneously update 'b', where 'a' is const.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127544 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/inc-const-valueOf.js
@@ -0,0 +1,57 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+'Test for regression against <a href="https://bugs.webkit.org/show_bug.cgi?id=95815">'
+);
+
+function testPostIncConstVarWithIgnoredResult()
+{
+    var okay = false;
+    const a = {
+        valueOf: (function(){
+            okay = true;
+        })
+    };
+
+    a++;
+
+    return okay;
+}
+
+function testPreIncConstVarWithIgnoredResult()
+{
+    var okay = false;
+    const a = {
+        valueOf: (function(){
+            okay = true;
+        })
+    };
+
+    ++a;
+
+    return okay;
+}
+
+function testPreIncConstVarWithAssign()
+{
+    var okay = false;
+    var x = 42;
+    const a = {
+        valueOf: (function(){
+            throw x == 42;
+        })
+    };
+
+    try {
+        x = ++a;
+    } catch (e) {
+        okay = e
+    };
+
+    return okay;
+}
+
+shouldBeTrue('testPostIncConstVarWithIgnoredResult()');
+shouldBeTrue('testPreIncConstVarWithIgnoredResult()');
+shouldBeTrue('testPreIncConstVarWithAssign()');
+
+successfullyParsed = true;
