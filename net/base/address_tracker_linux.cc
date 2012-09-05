86f0cea8ff45488149e994c3eea43cca57b636a2
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 14:28:26 +0000

    [net (linux)] Don't send OnIPAddressChanged when the AddressMap is first populated.
    The only observer that cares about the initial signal is AddressSorterPosix.
    
    This CL:
    - Adds ReadMessages to AddressTrackerLinux::Init so that the initial enumeration of
      interfaces does not trigger the notification.
    - Moves AddressTrackerLinux::Init before DnsConfigService::WatchConfig so that
      HostResolverImpl won't create a DnsClient until AddressTrackerLinux assembles the
      AddressMap.
    
    BUG=146383
    
    Review URL: https://chromiumcodereview.appspot.com/10910076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154953 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/address_tracker_linux.cc
+++ net/base/address_tracker_linux.cc
@@ -142,6 +142,9 @@ void AddressTrackerLinux::Init() {
   }
 
   netlink_fd_ = sock;
+
+  // Consume any pending messages to populate the AddressMap, but don't notify.
+  ReadMessages();
 }
 
 AddressTrackerLinux::AddressMap AddressTrackerLinux::GetAddressMap() const {
