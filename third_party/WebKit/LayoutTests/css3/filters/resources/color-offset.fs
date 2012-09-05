6f640f809b9e8acbf7023b94f5cea3af024be436
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-05 18:26:57 +0000

    [CSS Shaders] Update custom filter tests to use the new security model https://bugs.webkit.org/show_bug.cgi?id=95760
    Patch by Max Vujovic <mvujovic@adobe.com> on 2012-09-05
    Reviewed by Dirk Schulze.
    
    This patch updates the custom filter tests to not sample u_texture and not write
    to gl_FragColor. Now, the tests use the new security model by writing to
    css_MixColor and indirectly blending with the DOM element texture.
    
    This patch prepares us to remove direct DOM texture access via "u_texture":
    https://bugs.webkit.org/show_bug.cgi?id=93871
    
    * css3/filters/custom/custom-filter-css-keyword-as-parameter-name-expected.png:
    * css3/filters/custom/custom-filter-shader-cache-expected.png:
    * css3/filters/custom/custom-filter-shader-cache.html:
        Use the CSS mix function with the updated color-offset.fs.
    * css3/filters/custom/effect-color-check-expected.png:
    * css3/filters/custom/effect-color-check.html:
        Use the CSS mix function with the updated color-fill.fs.
    * css3/filters/custom/effect-custom-combined-missing-expected.png:
    * css3/filters/custom/effect-custom-combined-missing.html:
        Use the CSS mix function with the updated color-offset.fs.
    * css3/filters/custom/effect-custom-expected.png:
    * css3/filters/custom/effect-custom-parameters-expected.png:
    * css3/filters/custom/effect-custom-parameters.html:
        Use the mix-color.fs shader instead of color-offset-parameters.fs.
    * css3/filters/custom/effect-custom.html:
        Use the CSS mix function with the updated color-offset.fs.
    * css3/filters/custom/filter-repaint-custom-clipped-expected.png:
    * css3/filters/custom/filter-repaint-custom-clipped.html:
        Use the mix-color.fs shader instead of color-add.fs.
    * css3/filters/custom/filter-repaint-custom-expected.png:
    * css3/filters/custom/filter-repaint-custom-rotated-expected.png:
    * css3/filters/custom/filter-repaint-custom-rotated.html:
        Use the mix-color.fs shader instead of color-add.fs. Fix a small typo in a comment.
    * css3/filters/custom/filter-repaint-custom.html:
        Use the mix-color.fs shader instead of color-add.fs.
    * css3/filters/custom/invalid-custom-filter-shader.html:
        Use the CSS mix function with the updated color-offset.fs.
    * css3/filters/custom/missing-custom-filter-shader-expected.png:
    * css3/filters/custom/missing-custom-filter-shader.html:
        Use the CSS mix function with the updated color-offset.fs.
    * css3/filters/resources/color-add.fs:
        Removed. The mix-color.fs shader can perform this shader's function.
    * css3/filters/resources/color-fill.fs:
        Rewrote this shader to write to css_MixColor instead of sampling u_texture and writing
        to gl_FragColor.
    * css3/filters/resources/color-offset-parameters.fs:
        Removed. The mix-color.fs shader can perform this shader's function.
    * css3/filters/resources/color-offset.fs:
        Rewrote this shader to write to css_MixColor instead of sampling u_texture and writing
        to gl_FragColor.
    * platform/chromium-mac/css3/filters/custom/custom-filter-shader-cache-expected.png:
    * platform/chromium-mac/css3/filters/custom/effect-color-check-expected.png:
    * platform/chromium-mac/css3/filters/custom/effect-custom-combined-missing-expected.png:
    * platform/chromium-mac/css3/filters/custom/effect-custom-expected.png:
    * platform/chromium-mac/css3/filters/custom/effect-custom-parameters-expected.png:
    * platform/chromium-mac/css3/filters/custom/filter-repaint-custom-clipped-expected.png:
    * platform/chromium-mac/css3/filters/custom/filter-repaint-custom-expected.png:
    * platform/chromium-mac/css3/filters/custom/filter-repaint-custom-rotated-expected.png:
    * platform/chromium/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/css3/filters/resources/color-offset.fs
+++ third_party/WebKit/LayoutTests/css3/filters/resources/color-offset.fs
@@ -1,9 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 precision mediump float;
-varying vec2 v_texCoord;
-uniform sampler2D u_texture;
 void main()
 {
-    // Offset the color value with 0.3 on each color channel.
-    gl_FragColor = texture2D(u_texture, v_texCoord) + 0.3;
+    // Mix some white with the DOM element texture.
+    css_MixColor = vec4(1.0, 1.0, 1.0, 0.5);
 }
