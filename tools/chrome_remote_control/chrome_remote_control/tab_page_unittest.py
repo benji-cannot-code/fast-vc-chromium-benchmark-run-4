a434fec9513e2b14ac60e610cfd35179a371edab
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-05 00:58:53 +0000

    Polish to chrome_remote_control to reduce flake and improve modularity.
    - Make android work
    - Overhaul tools/gpu to be more component-y
    - Fix error where pexpect missing breaks all browser discovery
    - Use page.navigate and get rid of the strange timeout logic in InspectorBackend
    - Make browser selection explicit. You have to select browsers explicitly.
    - Add --repeat-count to run_tests
    - Move sleep-after-each-load to gpu_tools.url_test
    - Make timeouts explicit
    - Fix handling of optparse defaults and counters
    - Add is_content_shell property on backends
    - Set up content-shell command line on android
    - Override user-data-dir only for non-content-shell backends
    - If browser fails to die on sigterm, send it a sigkill
    - Move gpu tools browser creation to toplevel test
    - Make non-jsonizable evaluate test independent of window for perf reasons
    - Add runtime.execute to avoid hacks of runtime.evaluate
    
    TBR=dtu@chromium.org
    BUG=144483
    
    Review URL: https://chromiumcodereview.appspot.com/10909067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@154874 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/chrome_remote_control/chrome_remote_control/tab_page_unittest.py
+++ tools/chrome_remote_control/chrome_remote_control/tab_page_unittest.py
@@ -5,10 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import browser
 import browser_finder
 import browser_options
-import tab
+import tab_page
 import unittest
 
-class TabTest(unittest.TestCase):
+class TabPageTest(unittest.TestCase):
   def setUp(self):
     self._browser = None
     self._tab = None
@@ -29,10 +29,16 @@ class TabTest(unittest.TestCase):
     if self._browser:
       self._browser.Close()
 
-  def testNavigateAndWaitToForCompleteState(self):
-    self._tab.page.Navigate("http://www.google.com")
+  def testPageNavigateToNormalUrl(self):
+    res = self._tab.page.Navigate("http://www.google.com")
     self._tab.WaitForDocumentReadyStateToBeComplete()
 
-  def testNavigateAndWaitToForInteractiveState(self):
-    self._tab.page.Navigate("http://www.google.com")
-    self._tab.WaitForDocumentReadyStateToBeInteractiveOrBetter()
+  def testPageNavigateToUrlChanger(self):
+    # The Url that we actually load is http://www.youtube.com/.
+    res = self._tab.page.Navigate("http://youtube.com/")
+
+    self._tab.WaitForDocumentReadyStateToBeComplete()
+
+  def testPageNavigateToImpossibleURL(self):
+    res = self._tab.page.Navigate("http://23f09f0f9fsdflajsfaldfkj2f3f.com")
+    self._tab.WaitForDocumentReadyStateToBeComplete()
