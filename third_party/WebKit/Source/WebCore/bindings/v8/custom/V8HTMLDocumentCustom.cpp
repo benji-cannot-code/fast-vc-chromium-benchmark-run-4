896e2eb6ff42d1bfbfe90c0d7c8a3737687bfcfa
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-06 07:16:52 +0000

    [V8] V8HTMLDocument has two out-of-style function names https://bugs.webkit.org/show_bug.cgi?id=95893
    Patch by Adam Barth <abarth@chromium.org> on 2012-09-06
    Reviewed by Kentaro Hara.
    
    Just a simple style cleanup.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeader):
    * bindings/v8/V8DOMWindowShell.cpp:
    (WebCore::getter):
    * bindings/v8/V8DOMWrapper.cpp:
    (WebCore::V8DOMWrapper::instantiateV8Object):
    * bindings/v8/custom/V8HTMLDocumentCustom.cpp:
    (WebCore::V8HTMLDocument::wrapInShadowObject):
    (WebCore::V8HTMLDocument::getNamedProperty):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLDocumentCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8HTMLDocumentCustom.cpp
@@ -53,7 +53,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Local<v8::Object> V8HTMLDocument::WrapInShadowObject(v8::Local<v8::Object> wrapper, Node* impl)
+v8::Local<v8::Object> V8HTMLDocument::wrapInShadowObject(v8::Local<v8::Object> wrapper, Node* impl)
 {
     DEFINE_STATIC_LOCAL(v8::Persistent<v8::FunctionTemplate>, shadowTemplate, ());
     if (shadowTemplate.IsEmpty()) {
@@ -81,7 +81,7 @@ v8::Local<v8::Object> V8HTMLDocument::WrapInShadowObject(v8::Local<v8::Object> w
     return shadow;
 }
 
-v8::Handle<v8::Value> V8HTMLDocument::GetNamedProperty(HTMLDocument* htmlDocument, const AtomicString& key, v8::Isolate* isolate)
+v8::Handle<v8::Value> V8HTMLDocument::getNamedProperty(HTMLDocument* htmlDocument, const AtomicString& key, v8::Isolate* isolate)
 {
     if (!htmlDocument->hasNamedItem(key.impl()) && !htmlDocument->hasExtraNamedItem(key.impl()))
         return v8Undefined();
