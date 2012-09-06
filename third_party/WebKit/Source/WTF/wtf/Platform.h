92463b648029d90f856ce4a153c4d9a78a7ab94e
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-06 07:21:03 +0000

    Remove WTF::String::operator+= https://bugs.webkit.org/show_bug.cgi?id=95797
    Patch by Adam Barth <abarth@chromium.org> on 2012-09-06
    Reviewed by Sam Weinig.
    
    This patch encloses WTF::String::operator+= in an ifdef so that we can
    gradually remove it from the various port-specific files. The ifdef
    lets us whitelist use of operator+= in individual files during the
    transition.
    
    * wtf/Platform.h
    * wtf/text/WTFString.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -1186,6 +1186,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_USE_ZLIB 1
 #endif
 
+#if PLATFORM(QT) || PLATFORM(GTK) || PLATFORM(WIN)
+#define WTF_DEPRECATED_STRING_OPERATORS
+#endif
+
 #if PLATFORM(QT)
 #include <qglobal.h>
 #if defined(QT_OPENGL_ES_2) && !defined(WTF_USE_OPENGL_ES_2)
