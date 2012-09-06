dfae37f051fc460ad2a970224571478f4a1608d2
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 20:58:56 +0000

    Revert 155122 - Code to collect issue 97285 debugging info for crash reports. [TPM is suspicious of Windows renderer crash rate.  Seems impossible, but who knows?]
    It is useful to record breakpad keys for debugging, which is not
    applicable to multi-process bugs.  This adds infrastructure to allow
    collection of information across processes.
    
    Also, fix so that GetPluginChannelHost() can successfully fail without
    crashing.
    
    BUG=97285,141055
    
    
    Review URL: https://chromiumcodereview.appspot.com/10908078
    
    TBR=shess@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10908130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155240 0039d316-1c4b-4281-b951-d872f2087c98

--- content/plugin/plugin_thread.cc
+++ content/plugin/plugin_thread.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/child_process.h"
 #include "content/common/npobject_util.h"
 #include "content/common/plugin_messages.h"
-#include "content/public/common/content_debug_logging.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/plugin/content_plugin_client.h"
 #include "ipc/ipc_channel_handle.h"
@@ -70,19 +69,6 @@ class EnsureTerminateMessageFilter : public IPC::ChannelProxy::MessageFilter {
   }
 };
 
-void RecordMsg(int bug_id, const std::string& msg) {
-  PluginThread::current()->Send(
-      new PluginProcessHostMsg_ContentDebugRecordMsg(bug_id, msg));
-}
-
-bool GetMessages(int bug_id, std::vector<std::string>* msgs) {
-  bool handled = false;
-  PluginThread::current()->Send(
-      new PluginProcessHostMsg_ContentDebugGetMessages(
-          bug_id, &handled, msgs));
-  return handled;
-}
-
 }  // namespace
 
 static base::LazyInstance<base::ThreadLocalPointer<PluginThread> > lazy_tls =
@@ -121,8 +107,6 @@ PluginThread::PluginThread()
 
   ui::SetDefaultX11ErrorHandlers();
 #endif
-
-  content::debug::RegisterMessageHandlers(RecordMsg, GetMessages);
 
   PatchNPNFunctions();
 
@@ -194,11 +178,6 @@ void PluginThread::OnCreateChannel(int renderer_id,
     channel->set_incognito(incognito);
   }
 
-#if defined(OS_MACOSX)
-  content::debug::RecordMsg(97285, base::StringPrintf(
-      "OnCreateChannel({%s, %d})",
-      channel_handle.name.c_str(), channel_handle.socket.fd));
-#endif
   Send(new PluginProcessHostMsg_ChannelCreated(channel_handle));
 }
 
