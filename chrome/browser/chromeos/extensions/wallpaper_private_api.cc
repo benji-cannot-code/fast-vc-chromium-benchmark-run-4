c1908fe44d1e82d35d13431b0079c82e0936fdec
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 22:16:11 +0000

    Saves full url path to local state if user choose an online wallpaper from new picker ui
    Saves the full url path so we can support wallpaper sync later.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/10916102
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/wallpaper_private_api.cc
+++ chrome/browser/chromeos/extensions/wallpaper_private_api.cc
@@ -130,11 +130,8 @@ bool WallpaperSetWallpaperFunction::RunImpl() {
     return false;
   }
   layout_ = ash::GetLayoutEnum(layout_string);
-  std::string url;
-  if (!args_->GetString(2, &url) || url.empty()) {
+  if (!args_->GetString(2, &url_) || url_.empty())
     return false;
-  }
-  file_name_ = GURL(url).ExtractFileName();
 
   // Gets email address while at UI thread.
   email_ = chromeos::UserManager::Get()->GetLoggedInUser().email();
@@ -174,7 +171,8 @@ void WallpaperSetWallpaperFunction::SaveToFile() {
                    this));
     return;
   }
-  FilePath file_path = wallpaper_dir.Append(file_name_);
+  std::string file_name = GURL(url_).ExtractFileName();
+  FilePath file_path = wallpaper_dir.Append(file_name);
   if (file_util::PathExists(file_path) ||
       file_util::WriteFile(file_path, image_data_.c_str(),
                            image_data_.size()) != -1 ) {
@@ -197,7 +195,7 @@ void WallpaperSetWallpaperFunction::SetDecodedWallpaper() {
   bool is_persistent =
       !chromeos::UserManager::Get()->IsCurrentUserEphemeral();
   chromeos::WallpaperInfo info = {
-      file_name_,
+      url_,
       layout_,
       chromeos::User::ONLINE,
       base::Time::Now().LocalMidnight()
