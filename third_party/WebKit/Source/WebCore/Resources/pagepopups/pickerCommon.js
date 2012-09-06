e31656ce213d518c13559bc618a0180e6be19e8a
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-06 16:41:23 +0000

    Use native function bind in page popups https://bugs.webkit.org/show_bug.cgi?id=95976
    Reviewed by Kent Tamura.
    
    We should be using Function.prototype.bind instead of our own bind implementation.
    
    No new tests. No behavior change.
    
    * Resources/pagepopups/calendarPicker.js:
    (CalendarPicker):
    (CalendarPicker.prototype._layoutButtons):
    (YearMonthController.prototype.attachTo):
    (YearMonthController.prototype._attachLeftButtonsTo):
    (YearMonthController.prototype._attachRightButtonsTo):
    (DaysTable.prototype.attachTo):
    * Resources/pagepopups/colorSuggestionPicker.js:
    (ColorPicker):
    (ColorPicker.prototype._layout):
    * Resources/pagepopups/pickerCommon.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Resources/pagepopups/pickerCommon.js
+++ third_party/WebKit/Source/WebCore/Resources/pagepopups/pickerCommon.js
@@ -30,12 +30,6 @@ function $(id) {
     return document.getElementById(id);
 }
 
-function bind(func, context) {
-    return function() {
-        return func.apply(context, arguments);
-    };
-}
-
 /**
  * @param {!string} tagName
  * @param {string=} opt_class
