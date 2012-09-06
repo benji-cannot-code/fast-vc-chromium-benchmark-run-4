33cc3dd564b8ed42872248ed5e8fb50a3f37da0e
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-06 03:58:27 +0000

    [EFL][WK2] Implement missing feature to support <select> tag https://bugs.webkit.org/show_bug.cgi?id=95708
    Reviewed by Gyuyoung Kim.
    
    Added missing APIs for select tag.
    
    This patch provides all functionality of current WebPopupItem to applications.
    
    * UIProcess/API/efl/ewk_popup_menu_item.cpp:
    (_Ewk_Popup_Menu_Item):
    (_Ewk_Popup_Menu_Item::_Ewk_Popup_Menu_Item):
    (ewk_popup_menu_item_new):
    (ewk_popup_menu_item_text_direction_get):
    (ewk_popup_menu_item_text_direction_override_get):
    (ewk_popup_menu_item_tooltip_get):
    (ewk_popup_menu_item_accessibility_text_get):
    (ewk_popup_menu_item_enabled_get):
    (ewk_popup_menu_item_is_label_get):
    (ewk_popup_menu_item_selected_get):
    * UIProcess/API/efl/ewk_popup_menu_item.h:
    * UIProcess/API/efl/ewk_popup_menu_item_private.h:
    * UIProcess/API/efl/ewk_view.cpp:
    (ewk_view_popup_menu_request):
    * UIProcess/API/efl/tests/test_ewk2_view.cpp:
    (checkBasicPopupMenuItem):
    (showPopupMenu): Updated test case to cover new APIs.
    (TEST_F):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_popup_menu_item_private.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_popup_menu_item_private.h
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WebPopupItem.h"
 
-Ewk_Popup_Menu_Item* ewk_popup_menu_item_new(WebKit::WebPopupItem::Type type, const char* text);
+Ewk_Popup_Menu_Item* ewk_popup_menu_item_new(const WebKit::WebPopupItem& item);
 void ewk_popup_menu_item_free(Ewk_Popup_Menu_Item* item);
 
 #endif // ewk_popup_menu_item_private_h
