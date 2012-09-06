656dbdb61348446171529e8ad63b12713d7c817f
yefim@chromium.org <yefim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 08:42:20 +0000

    Change enable-action-box flag to disable-action-box, so action box is enabled by default.
    BUG=125307
    
    Review URL: https://chromiumcodereview.appspot.com/10911077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_to_mobile_service.cc
+++ chrome/browser/chrome_to_mobile_service.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/cloud_print/cloud_print_helpers.h"
+#include "chrome/common/extensions/extension_switch_utils.h"
 #include "chrome/common/net/gaia/gaia_constants.h"
 #include "chrome/common/net/gaia/gaia_urls.h"
 #include "chrome/common/net/gaia/oauth2_access_token_fetcher.h"
@@ -164,8 +165,7 @@ ChromeToMobileService::JobData::~JobData() {}
 // static
 bool ChromeToMobileService::IsChromeToMobileEnabled() {
   // Chrome To Mobile is currently gated on the Action Box UI.
-  return CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableActionBox);
+  return extensions::switch_utils::IsActionBoxEnabled();
 }
 
 // static
