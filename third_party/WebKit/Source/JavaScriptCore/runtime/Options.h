56a7fd147358887075030866a5b145dc606770ea
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-06 10:13:14 +0000

    Renamed useYarrJIT() option to useRegExpJIT(). Also fixed regression in which inadvertantly allows the ASM llint to use the baseline JIT when useRegExpJIT() is true. https://bugs.webkit.org/show_bug.cgi?id=95918.
    Patch by Mark Lam <mark.lam@apple.com> on 2012-09-06
    Reviewed by Geoffrey Garen.
    
    * runtime/JSGlobalData.cpp:
    (JSC::enableAssembler):
    (JSC::JSGlobalData::JSGlobalData):
    * runtime/JSGlobalData.h:
    (JSC::JSGlobalData::canUseJIT):
    (JSC::JSGlobalData::canUseRegExpJIT):
    (JSGlobalData):
    * runtime/Options.cpp:
    (JSC::Options::initialize):
    * runtime/Options.h:
    (JSC):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127719 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Options.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Options.h
@@ -66,7 +66,7 @@ namespace JSC {
 #define JSC_OPTIONS(v) \
     v(bool, useJIT,    true) \
     v(bool, useDFGJIT, true) \
-    v(bool, useYarrJIT, true) \
+    v(bool, useRegExpJIT, true) \
     \
     /* showDisassembly implies showDFGDisassembly. */ \
     v(bool, showDisassembly, false) \
