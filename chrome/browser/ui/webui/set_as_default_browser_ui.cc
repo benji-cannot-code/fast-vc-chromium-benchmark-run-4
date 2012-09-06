5c45f36523664a855dbaea6a23924adc83d9275f
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 00:44:52 +0000

    Make sure the SetAsDefault dialog is highly visible on Windows.
    BUG=145470
    TEST=MAke sure the welcome to chrome dialog has it's own taskbar item and that it gets highlited when not in the foreground.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10911034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155076 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/set_as_default_browser_ui.cc
+++ chrome/browser/ui/webui/set_as_default_browser_ui.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_font_util.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/gfx/font.h"
+#include "ui/views/widget/widget.h"
 #include "ui/web_dialogs/web_dialog_delegate.h"
 
 using content::BrowserThread;
@@ -246,8 +247,15 @@ SetAsDefaultBrowserDialogImpl::~SetAsDefaultBrowserDialogImpl() {
 }
 
 void SetAsDefaultBrowserDialogImpl::ShowDialog() {
-  chrome::ShowWebDialog(browser_->window()->GetNativeWindow(),
-                        browser_->profile(), this);
+  // Use a NULL parent window to make sure that the dialog will have an item
+  // in the Windows task bar. The code below will make it highlight if the
+  // dialog is not in the foreground.
+  gfx::NativeWindow native_window = chrome::ShowWebDialog(NULL,
+                                                          browser_->profile(),
+                                                          this);
+  views::Widget* widget = views::Widget::GetWidgetForNativeWindow(
+      native_window);
+  widget->FlashFrame(true);
 }
 
 ui::ModalType SetAsDefaultBrowserDialogImpl::GetDialogModalType() const {
