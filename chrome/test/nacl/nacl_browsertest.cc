3187b847c0cbc61fbbeb3103c037133ab4597425
ncbray@google.com <ncbray@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 17:48:08 +0000

    Enable NaCl browser tests on Linux ASAN bots.
    The CL http://codereview.chromium.org/10830009/ should have fixed the breakage.
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2959
    
    Review URL: https://chromiumcodereview.appspot.com/10918034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155213 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl/nacl_browsertest.cc
+++ chrome/test/nacl/nacl_browsertest.cc
@@ -7,15 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Disable tests under Linux ASAN.
-// Linux ASAN doesn't work with NaCl.  See: http://crbug.com/104832.
-// TODO(ncbray) enable after http://codereview.chromium.org/10830009/ lands.
-#if !(defined(ADDRESS_SANITIZER) && defined(OS_LINUX))
-
 NACL_BROWSER_TEST_F(NaClBrowserTest, SimpleLoad, {
   RunLoadTest(FILE_PATH_LITERAL("nacl_load_test.html"));
 })
-
-#endif  // !(defined(ADDRESS_SANITIZER) && defined(OS_LINUX))
 
 }  // namespace anonymous
