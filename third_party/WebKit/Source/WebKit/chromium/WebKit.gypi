d05c634ee4f939af57ad9a16d7443cff77eb397c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-06 09:19:18 +0000

    Added modifiers to distinguish between left/right Shift/Ctrl/Alt in WebInputEvents https://bugs.webkit.org/show_bug.cgi?id=94142
    Patch by Raymes Khoury <raymes@chromium.org> on 2012-09-06
    Reviewed by Tony Chang.
    
    Previously we distinguished between left/right Shift/Ctrl/Alt in
    WebInputEvents by using the location-specific virtual keycodes (e.g.
    VK_LSHIFT instead of VK_SHIFT. However, the Windows API for key events
    always returns the non-locational version (e.g. VK_SHIFT).
    Furthermore, KeyboardEvent also returns
    non-locational keycodes and separates the location information which
    matches the behavior described in the DOM3
    spec http://www.w3.org/TR/DOM-Level-3-Events/#events-keyboardevents. We
    should be consistent with this in our representation of key events.
    This patch changes WebInputEvents so they always store the
    non-locational keycode and location information is stored in
    modifiers. Since this only changes internal representation, no new
    tests are added.
    
    This patch also ensures that when KeyboardEvents are converted back to
    WebInputEvents location information is preserved (which was not
    happening before). This is important so that key location information
    is passed on to plugins. A unittest is added for this.
    
    * WebKit.gypi:
    * public/WebInputEvent.h:
    (WebKeyboardEvent):
    * src/WebInputEvent.cpp:
    (WebKit):
    (WebKit::WebKeyboardEvent::windowsKeyCodeWithoutLocation):
    (WebKit::WebKeyboardEvent::locationModifiersFromWindowsKeyCode):
    * src/WebInputEventConversion.cpp:
    (WebKit::PlatformKeyboardEventBuilder::PlatformKeyboardEventBuilder):
    (WebKit::WebKeyboardEventBuilder::WebKeyboardEventBuilder):
    * src/android/WebInputEventFactory.cpp:
    (WebKit::WebInputEventFactory::keyboardEvent):
    * src/gtk/WebInputEventFactory.cpp:
    (WebKit::WebInputEventFactory::keyboardEvent):
    * src/mac/WebInputEventFactory.mm:
    (WebKit::WebInputEventFactory::keyboardEvent):
    * src/win/WebInputEventFactory.cpp:
    (WebKit::getLocationModifier):
    (WebKit::WebInputEventFactory::keyboardEvent):
    * tests/WebInputEventConversionTest.cpp: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/WebKit.gypi
+++ third_party/WebKit/Source/WebKit/chromium/WebKit.gypi
@@ -176,6 +176,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'tests/TreeSynchronizerTest.cpp',
             'tests/WebAnimationTest.cpp',
             'tests/WebFloatAnimationCurveTest.cpp',
+            'tests/WebInputEventConversionTest.cpp',
             'tests/WebLayerTest.cpp',
             'tests/WebLayerTreeViewTest.cpp',
             'tests/WebLayerTreeViewTestCommon.h',
--- third_party/WebKit/Source/WebKit/chromium/src/mac/WebInputEventFactory.mm
+++ third_party/WebKit/Source/WebKit/chromium/src/mac/WebInputEventFactory.mm
@@ -607,7 +607,9 @@ WebKeyboardEvent WebInputEventFactory::keyboardEvent(NSEvent* event)
     if (([event type] != NSFlagsChanged) && [event isARepeat])
         result.modifiers |= WebInputEvent::IsAutoRepeat;
 
-    result.windowsKeyCode = windowsKeyCodeForKeyEvent(event);
+    int windowsKeyCode = windowsKeyCodeForKeyEvent(event);
+    result.windowsKeyCode = WebKeyboardEvent::windowsKeyCodeWithoutLocation(windowsKeyCode);
+    result.modifiers |= WebKeyboardEvent::locationModifiersFromKeyCode(windowsKeyCode);
     result.nativeKeyCode = [event keyCode];
 
     NSString* textStr = textFromEvent(event);
