c7243a8eb577cb51f428fef41555f0f11ee2d8a1
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-06 00:00:22 +0000

    Disallow app windows in windows 8 metro mode. I will take a look at a clean way to remove/disable the new window menu option which is done via JS in a subsequent CL.
    Fixes bug http://code.google.com/p/chromium/issues/detail?id=146368
    
    BUG=146368
    R=aa
    Review URL: https://chromiumcodereview.appspot.com/10905088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155062 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_prefs.cc
+++ chrome/browser/extensions/extension_prefs.cc
@@ -27,6 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if defined(OS_WIN)
+#include "base/win/metro.h"
+#endif  // OS_WIN
+
 namespace extensions {
 
 namespace {
@@ -1161,6 +1165,12 @@ ExtensionPrefs::LaunchType ExtensionPrefs::GetLaunchType(
     if (result == LAUNCH_WINDOW)
       result = LAUNCH_REGULAR;
   #endif
+
+#if defined(OS_WIN)
+    // We don't support app windows in windows 8 metro mode.
+    if (base::win::IsMetroProcess() && result == LAUNCH_WINDOW)
+      result = LAUNCH_REGULAR;
+#endif  // OS_WIN
 
   return result;
 }
