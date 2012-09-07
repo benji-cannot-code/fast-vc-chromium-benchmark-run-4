f8b66d09c08967342b53989179e7ee1388416274
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 18:24:52 +0000

    Integrate the Windows 8 code into the Chromium tree.
    BUG=127799
    TEST=A Chromium build can run as the immersive browser on Windows 8.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10875008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155429 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ win8/metro_driver/secondary_tile.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#ifndef CHROME_BROWSER_UI_METRO_DRIVER_SECONDARY_TILE_H_
+#define CHROME_BROWSER_UI_METRO_DRIVER_SECONDARY_TILE_H_
+
+#include "base/string16.h"
+
+extern "C" __declspec(dllexport)
+BOOL MetroIsPinnedToStartScreen(const string16& url);
+
+extern "C" __declspec(dllexport)
+void MetroTogglePinnedToStartScreen(const string16& title, const string16& url);
+
+#endif  // CHROME_BROWSER_UI_METRO_DRIVER_SECONDARY_TILE_H_
+
