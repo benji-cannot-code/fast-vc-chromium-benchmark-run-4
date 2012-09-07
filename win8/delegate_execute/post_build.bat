cc9a92e2f02a3da6d0845cd700d69ac5eabd27bd
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 20:44:51 +0000

    Integrate the Windows 8 code into the Chromium tree (take 3).
    This time, also:
    fix invalid path in metro_driver.gyp.
    set executable bit on check_sdk_patch.py and post_build.bat.
    add back a switch that was removed recently
    
    This reverts "Revert 155429 - Integrate the Windows 8 code into the Chromium tree."
    
    Original CL: https://chromiumcodereview.appspot.com/10875008
    
    BUG=127799
    TEST=A Chromium build can run as the immersive browser on Windows 8.
    TBR=ben
    
    Review URL: https://chromiumcodereview.appspot.com/10919158
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155474 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ win8/delegate_execute/post_build.bat
@@ -0,0 +1,4 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+REM invoke as:  post_build.bat OUTPUT_PATH_TO_EXE PATH_TO_CHROME_ROOT CONFIGURATION
+mkdir %2\build\%3
+copy %1 %2\build\%3\
