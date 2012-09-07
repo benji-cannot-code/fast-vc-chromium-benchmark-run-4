13037661f6f0760134243f120f7762ecd973f253
zysxqn@chromium.org <zysxqn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 22:19:26 +0000

    Update Windows UI for the Password Generation.
    Patched issue 10829373.
    
    
    BUG=143330
    
    
    Review URL: https://chromiumcodereview.appspot.com/10919111
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155505 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/password_generation_bubble_view.h
+++ chrome/browser/ui/views/password_generation_bubble_view.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect.h"
 #include "ui/views/bubble/bubble_delegate.h"
 #include "ui/views/controls/button/button.h"
-#include "ui/views/controls/link_listener.h"
 #include "ui/views/view.h"
 #include "webkit/forms/password_form.h"
 
@@ -24,6 +23,8 @@ class RenderViewHost;
 }
 
 namespace views {
+class ImageButton;
+class Label;
 class TextButton;
 class Textfield;
 }
@@ -35,17 +36,21 @@ class PasswordManager;
 // If the generated password is accepted by the user, the renderer associated
 // with |render_view_host| and the |password_manager| are informed.
 class PasswordGenerationBubbleView : public views::BubbleDelegateView,
-                                     public views::ButtonListener,
-                                     public views::LinkListener {
+                                     public views::ButtonListener {
  public:
-  PasswordGenerationBubbleView(const gfx::Rect& anchor_rect,
-                               const webkit::forms::PasswordForm& form,
+  PasswordGenerationBubbleView(const webkit::forms::PasswordForm& form,
+                               const gfx::Rect& anchor_rect,
                                views::View* anchor_view,
                                content::RenderViewHost* render_view_host,
+                               PasswordManager* password_manager,
                                autofill::PasswordGenerator* password_generator,
                                content::PageNavigator* navigator,
-                               PasswordManager* password_manager);
+                               ui::ThemeProvider* theme_provider);
   virtual ~PasswordGenerationBubbleView();
+
+  // views::View
+  virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual void Layout() OVERRIDE;
 
  private:
   // views::BubbleDelegateView
@@ -56,24 +61,27 @@ class PasswordGenerationBubbleView : public views::BubbleDelegateView,
   virtual void ButtonPressed(views::Button* sender,
                              const ui::Event& event) OVERRIDE;
 
-  // views::LinkListener
-  virtual void LinkClicked(views::Link* source, int event_flags) OVERRIDE;
-
   // views::WidgetDelegate
   virtual views::View* GetInitiallyFocusedView() OVERRIDE;
 
   // Subviews
+  views::Label* title_label_;
   views::TextButton* accept_button_;
-  views::Textfield* text_field_;
-
-  // Location that the bubble points to
-  gfx::Rect anchor_rect_;
+  views::Textfield* textfield_;
+  views::ImageButton* regenerate_button_;
+  views::View* textfield_wrapper_;
 
   // The form associated with the password field(s) that we are generated.
   webkit::forms::PasswordForm form_;
 
+  // Location that the bubble points to
+  gfx::Rect anchor_rect_;
+
   // RenderViewHost associated with the button that spawned this bubble.
   content::RenderViewHost* render_view_host_;
+
+  // PasswordManager associated with this tab.
+  PasswordManager* password_manager_;
 
   // Object to generate passwords. The class won't take the ownership of it.
   autofill::PasswordGenerator* password_generator_;
@@ -82,8 +90,8 @@ class PasswordGenerationBubbleView : public views::BubbleDelegateView,
   // within this UI.
   content::PageNavigator* navigator_;
 
-  // PasswordManager associated with this tab.
-  PasswordManager* password_manager_;
+  // Theme provider used to draw the regenerate button.
+  ui::ThemeProvider* theme_provider_;
 
   DISALLOW_COPY_AND_ASSIGN(PasswordGenerationBubbleView);
 };
