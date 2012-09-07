44e693818fe9338a0007d6c6c9b6153c2d90dbdb
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 22:02:29 +0000

    Revert 155351 - Make DownloadManager::GetAllDownloads return all downloads regardless of state, persistence, temporariness, or save page.
    Also change some callers from SearchDownloads() over to GetAllDownloads().
    
    This is an experiment in preparation for DownloadHistory-that-is-an-Observer, which removes the persisted concept from content. http://codereview.chromium.org/10665049/
    
    I tried to inspect each caller in this CL to check that it would be ok with this change, but I might have missed something.
    
    
    Reviewers breakdown:
    Done:
    rdsmith: *
    jam: chrome/browser/automation
    michaeln: chrome/browser/browsing_data
    jcivelli: content/public/test
    
    Review URL: https://chromiumcodereview.appspot.com/10913015
    
    TBR=benjhayden@chromium.org
    BUG=147359
    Review URL: https://chromiumcodereview.appspot.com/10914158
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/downloads/downloads_api_unittest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_unittest.cc
@@ -370,7 +370,7 @@ class DownloadExtensionTest : public ExtensionApiTest {
       entries.push_back(entry);
     }
     GetOnRecordManager()->OnPersistentStoreQueryComplete(&entries);
-    GetOnRecordManager()->GetAllDownloads(items);
+    GetOnRecordManager()->GetAllDownloads(FilePath(), items);
     EXPECT_EQ(count, items->size());
     if (count != items->size())
       return false;
@@ -402,7 +402,7 @@ class DownloadExtensionTest : public ExtensionApiTest {
       EXPECT_EQ(
           1u, observer->NumDownloadsSeenInState(DownloadItem::IN_PROGRESS));
     }
-    GetCurrentManager()->GetAllDownloads(items);
+    GetCurrentManager()->GetAllDownloads(FilePath(), items);
     ASSERT_EQ(count, items->size());
   }
 
@@ -424,7 +424,7 @@ class DownloadExtensionTest : public ExtensionApiTest {
     EXPECT_EQ(1u, observer->NumDownloadsSeenInState(DownloadItem::IN_PROGRESS));
 
     DownloadManager::DownloadVector items;
-    manager->GetAllDownloads(&items);
+    manager->GetAllDownloads(FilePath(), &items);
 
     DownloadItem* new_item = NULL;
     for (DownloadManager::DownloadVector::iterator iter = items.begin();
