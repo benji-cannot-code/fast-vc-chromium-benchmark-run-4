23c70fb77f81f6eece75a2f96432e1c235d26304
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 14:22:37 +0000

    Add README.py to document the isolate tools automatically.
    This file contains the "package-specific" documentation and generates
    documentation for each tool with their module docstring.
    
    Rename shard.py to shard_test_cases.py to stay consistent with the
    naming convention.
    
    R=csharp@chromium.org
    NOTRY=true
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10909118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155384 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/merge_isolate.py
+++ tools/isolate/merge_isolate.py
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Merges multiple OS-specific gyp dependency lists into one that works on all
 of them.
 
-
 The logic is relatively simple. Takes the current conditions, add more
 condition, find the strict subset. Done.
 """
