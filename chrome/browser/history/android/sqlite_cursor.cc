b2831ba73739e58db686d7d8715f8925500e027b
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 07:11:19 +0000

    Fix FaviconHandler::GetRawFaviconForID
    The Android API needs to be able to insert and retrieve arbitrary favicons from the history backend.
    http://codereview.chromium.org/10891007/ changed the API such that it returns favicons resized to 16x16 which broke the API.
    This changes GetRawFaviconForID to return unresized favicons and renames the method to GetLargestRawFaviconForID.
    
    Bug=145799
    Test=SQLiteCursorTest passes
    
    Review URL: https://chromiumcodereview.appspot.com/10916144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/android/sqlite_cursor.cc
+++ chrome/browser/history/android/sqlite_cursor.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/android/android_history_types.h"
 #include "content/public/browser/browser_thread.h"
 #include "jni/SQLiteCursor_jni.h"
-#include "ui/gfx/favicon_size.h"
 #include "sql/statement.h"
 
 using base::android::ConvertUTF8ToJavaString;
@@ -242,8 +241,7 @@ void SQLiteCursor::GetFaviconForIDInUIThread(
     CancelableRequestConsumerBase* consumer,
     const FaviconService::FaviconRawCallback& callback) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  favicon_service_->GetRawFaviconForID(id, gfx::kFaviconSize,
-      ui::SCALE_FACTOR_100P, consumer, callback);
+  favicon_service_->GetLargestRawFaviconForID(id, consumer, callback);
 }
 
 
