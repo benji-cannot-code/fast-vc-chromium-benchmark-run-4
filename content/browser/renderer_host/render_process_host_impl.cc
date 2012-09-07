f7f0f94d38c643fa69b161d0d265b6332e0158bf
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 18:56:11 +0000

    Enable Media Source API by default.
    BUG=144669
    
    
    Review URL: https://chromiumcodereview.appspot.com/10915117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155438 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -772,7 +772,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableGPUClientLogging,
     switches::kEnableGpuBenchmarking,
     switches::kEnableLogging,
-    switches::kEnableMediaSource,
+    switches::kDisableMediaSource,
     switches::kEnablePartialSwap,
     switches::kEnablePerTilePainting,
     switches::kEnableRendererSideMixing,
