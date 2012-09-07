4d37b6f79d46d07cc13140c77ae087f384173521
dmazzoni@google.com <dmazzoni@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-07 17:07:25 +0000

    Source/WebCore: AX: ARIA spin button should support range value attributes https://bugs.webkit.org/show_bug.cgi?id=96076
    Reviewed by Chris Fleizach.
    
    Make an ARIA spin button support ARIA range attributes like
    aria-valuenow, aria-valuemin, etc. - just like slider, progressbar,
    and scrollbar.
    
    Test: accessibility/spinbutton-value.html
    
    * accessibility/AccessibilityObject.h:
    (WebCore::AccessibilityObject::isSpinButton):
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::valueDescription):
    (WebCore):
    (WebCore::AccessibilityRenderObject::isAriaRange):
    (WebCore::AccessibilityRenderObject::valueForRange):
    (WebCore::AccessibilityRenderObject::maxValueForRange):
    (WebCore::AccessibilityRenderObject::minValueForRange):
    (WebCore::AccessibilityRenderObject::stringValue):
    (WebCore::AccessibilityRenderObject::title):
    (WebCore::AccessibilityRenderObject::isGenericFocusableElement):
    (WebCore::AccessibilityRenderObject::ariaRoleHasPresentationalChildren):
    * accessibility/AccessibilityRenderObject.h:
    (AccessibilityRenderObject):
    * accessibility/mac/WebAccessibilityObjectWrapper.mm:
    (-[WebAccessibilityObjectWrapper accessibilityAttributeValue:]):
    
    Source/WebKit/chromium: AX: ARIA spin button should support range value attributes
    https://bugs.webkit.org/show_bug.cgi?id=96076
    
    Reviewed by Chris Fleizach.
    
    Expose supportsRangeValue to simplify Chromium logic for when to
    extract a value from a range.
    
    * public/WebAccessibilityObject.h:
    (WebAccessibilityObject):
    * src/WebAccessibilityObject.cpp:
    (WebKit::WebAccessibilityObject::supportsRangeValue):
    (WebKit):
    
    Tools: New time input needs accessibility
    https://bugs.webkit.org/show_bug.cgi?id=96032
    
    Reviewed by Chris Fleizach.
    
    Add support for valueDescription for testing.
    
    * DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.cpp:
    (getValueDescription):
    (AccessibilityUIElement::AccessibilityUIElement):
    (AccessibilityUIElement::valueDescriptionGetterCallback):
    * DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.h:
    (AccessibilityUIElement):
    
    LayoutTests: AX: ARIA spin button should support range value attributes
    https://bugs.webkit.org/show_bug.cgi?id=96076
    
    Reviewed by Chris Fleizach.
    
    Test that an ARIA spin button exposes ARIA range attributes like
    aria-valuenow, aria-valuemin, etc.
    
    * accessibility/spinbutton-value-expected.txt: Added.
    * accessibility/spinbutton-value.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127882 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.cpp
@@ -1557,6 +1557,14 @@ AccessibilitySortDirection AccessibilityObject::sortDirection() const
     
     return SortDirectionNone;
 }
+
+bool AccessibilityObject::supportsRangeValue() const
+{
+    return isProgressIndicator()
+        || isSlider()
+        || isScrollbar()
+        || isSpinButton();
+}
     
 bool AccessibilityObject::supportsARIAExpanded() const
 {
