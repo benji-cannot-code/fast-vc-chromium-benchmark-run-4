e2cc1e9ba4304fb6620afccd6d32d36689ac2ca7
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-07 12:32:51 +0000

    Web Inspector: Migrate Database to async protocol commands https://bugs.webkit.org/show_bug.cgi?id=95983
    Reviewed by Alexander Pavlov.
    
    * English.lproj/localizedStrings.js:
    * inspector/Inspector.json:
    * inspector/InspectorDatabaseAgent.cpp:
    (WebCore):
    (WebCore::InspectorDatabaseAgent::didOpenDatabase):
    (WebCore::InspectorDatabaseAgent::setFrontend):
    (WebCore::InspectorDatabaseAgent::clearFrontend):
    (WebCore::InspectorDatabaseAgent::enable):
    (WebCore::InspectorDatabaseAgent::executeSQL):
    * inspector/InspectorDatabaseAgent.h:
    (InspectorDatabaseAgent):
    * inspector/front-end/Database.js:
    (WebInspector.Database.prototype.executeSql):
    (WebInspector.DatabaseDispatcher.prototype.addDatabase):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127861 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorDatabaseAgent.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorDatabaseAgent.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(INSPECTOR) && ENABLE(SQL_DATABASE)
 
 #include "InspectorBaseAgent.h"
+#include "InspectorFrontend.h"
 #include <wtf/HashMap.h>
 #include <wtf/PassOwnPtr.h>
 #include <wtf/text/WTFString.h>
@@ -50,8 +51,6 @@ typedef String ErrorString;
 
 class InspectorDatabaseAgent : public InspectorBaseAgent<InspectorDatabaseAgent>, public InspectorBackendDispatcher::DatabaseCommandHandler {
 public:
-    class FrontendProvider;
-
     static PassOwnPtr<InspectorDatabaseAgent> create(InstrumentingAgents* instrumentingAgents, InspectorState* state)
     {
         return adoptPtr(new InspectorDatabaseAgent(instrumentingAgents, state));
@@ -68,7 +67,7 @@ public:
     virtual void enable(ErrorString*);
     virtual void disable(ErrorString*);
     virtual void getDatabaseTableNames(ErrorString*, const String& databaseId, RefPtr<TypeBuilder::Array<String> >& names);
-    virtual void executeSQL(ErrorString*, const String& databaseId, const String& query, bool* success, int* transactionId);
+    virtual void executeSQL(ErrorString*, const String& databaseId, const String& query, PassRefPtr<ExecuteSQLCallback>);
 
     // Called from the injected script.
     String databaseId(Database*);
@@ -80,9 +79,9 @@ private:
     Database* databaseForId(const String& databaseId);
     InspectorDatabaseResource* findByFileName(const String& fileName);
 
+    InspectorFrontend::Database* m_frontend;
     typedef HashMap<String, RefPtr<InspectorDatabaseResource> > DatabaseResourcesMap;
     DatabaseResourcesMap m_resources;
-    RefPtr<FrontendProvider> m_frontendProvider;
     bool m_enabled;
 };
 
