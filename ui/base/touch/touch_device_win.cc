2861c481a2b9fc3f3d1e3793b11c662208f5a48d
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 21:22:01 +0000

    CSS Media Query now reports touch-support accurately for Windows.
    BUG=143792
    
    
    Review URL: https://chromiumcodereview.appspot.com/10836349
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155484 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/base/touch/touch_device_win.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/touch/touch_device_win.h"
+#include "base/win/windows_version.h"
+#include <windows.h>
+
+namespace ui {
+namespace base {
+
+bool IsTouchDevicePresent() {
+  // Docs: http://msdn.microsoft.com/en-us/library/dd371581(VS.85).aspx
+  return (::base::win::GetVersion() >= ::base::win::VERSION_WIN7) &&
+      (::GetSystemMetrics(SM_DIGITIZER) > 0);
+}
+
+}  // namespace base
+}  // namespace ui
