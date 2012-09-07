51432a08b72d03b1aac54ef0bf463f3ab78a1c59
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 19:52:18 +0000

    Fix invalid thread check.
    BUG=None
    TEST=Manually done
    
    
    Review URL: https://chromiumcodereview.appspot.com/10918116
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155459 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/win/tsf_bridge.cc
+++ ui/base/win/tsf_bridge.cc
@@ -317,7 +317,7 @@ TsfBridge::~TsfBridge() {
 
 // static
 bool TsfBridge::Initialize() {
-  if (MessageLoop::current()->type() == MessageLoop::TYPE_UI) {
+  if (MessageLoop::current()->type() != MessageLoop::TYPE_UI) {
     VLOG(1) << "Do not use TsfBridge without UI thread.";
     return false;
   }
@@ -329,7 +329,7 @@ bool TsfBridge::Initialize() {
 
 // static
 TsfBridge* TsfBridge::GetInstance() {
-  if (MessageLoop::current()->type() == MessageLoop::TYPE_UI) {
+  if (MessageLoop::current()->type() != MessageLoop::TYPE_UI) {
     VLOG(1) << "Do not use TsfBridge without UI thread.";
     return NULL;
   }
