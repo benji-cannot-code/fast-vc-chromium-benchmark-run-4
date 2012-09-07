6b28e2ee3d0f47e487c9b48ae78851bcacb5fb57
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 12:52:51 +0000

    webkit/quota: Fix clang warnings about missing virtual and OVERRIDE annotations.
    BUG=115047
    TBR=kinuko@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10915137
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155367 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_manager.cc
+++ webkit/quota/quota_manager.cc
@@ -539,7 +539,7 @@ class QuotaManager::UsageAndQuotaDispatcherTaskForTemporaryGlobal
                           available_space());
   }
 
-  virtual StorageType type() const { return kStorageTypeTemporary; }
+  virtual StorageType type() const OVERRIDE { return kStorageTypeTemporary; }
 };
 
 // static
