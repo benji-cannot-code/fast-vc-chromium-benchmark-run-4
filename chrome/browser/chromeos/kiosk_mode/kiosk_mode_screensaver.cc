b99217c981c7baf569007391090ea1d98036aeed
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 23:15:35 +0000

    Fix crash in screensaver shutdown. We remove the user activity detector's observer when shutting down the kiosk mode screensaver. This apparently can cause a crash since the ash shell or the user activity detector instance's could be null by this point. Check before we do a remove.
    Crash report is in the bug.
    
    R=derat@chromium.org
    BUG=147340
    TEST=None.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10909129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155526 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/kiosk_mode/kiosk_mode_screensaver.cc
+++ chrome/browser/chromeos/kiosk_mode/kiosk_mode_screensaver.cc
@@ -128,7 +128,9 @@ KioskModeScreensaver::~KioskModeScreensaver() {
 
   // In case we're shutting down without ever triggering the active
   // notification and/or logging in.
-  ash::Shell::GetInstance()->user_activity_detector()->RemoveObserver(this);
+  if (ash::Shell::GetInstance() &&
+      ash::Shell::GetInstance()->user_activity_detector())
+    ash::Shell::GetInstance()->user_activity_detector()->RemoveObserver(this);
   registrar_.RemoveAll();
 }
 
