49544f266d5f8e8224e3658ef1609f7175b171c8
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-07 13:24:43 +0000

    Web Inspector: Implement SCSS source highlighter https://bugs.webkit.org/show_bug.cgi?id=95585
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    This change modifies the existing CSS highlighter to support SCSS as well. The "text/x-scss" mime-type is assigned to SASSSource.
    
    * inspector/front-end/CSSCompletions.js:
    (WebInspector.CSSCompletions.cssPropertiesMetainfoKeySet):
    * inspector/front-end/ContentProviders.js:
    (WebInspector.StaticContentProvider): Enabled clients to supply the content mime-type.
    (WebInspector.StaticContentProvider.prototype.mimeType):
    (WebInspector.StaticContentProvider.prototype.requestContent): Use client-supplied content mime-type, if any.
    * inspector/front-end/SASSSourceMapping.js:
    (WebInspector.SASSSource): Construct StaticContentProvider with the "text/x-scss" mime-type.
    * inspector/front-end/SourceCSSTokenizer.js:
    (WebInspector.SourceCSSTokenizer):
    (WebInspector.SourceCSSTokenizer.prototype.nextToken):
    * inspector/front-end/SourceCSSTokenizer.re2js: Extended with the SCSS syntax.
    * inspector/front-end/SourceTokenizer.js:
    (WebInspector.SourceTokenizer.Registry):
    * inspector/front-end/inspectorSyntaxHighlight.css:
    (.webkit-css-bang-keyword):
    
    LayoutTests:
    
    Test modified to cover the SCSS syntax as well.
    
    * inspector/syntax-highlight-css-expected.txt:
    * inspector/syntax-highlight-css.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ContentProviders.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ContentProviders.js
@@ -242,12 +242,14 @@ WebInspector.CompilerSourceMappingContentProvider.prototype.__proto__ = WebInspe
  * @constructor
  * @implements {WebInspector.ContentProvider}
  * @param {WebInspector.ResourceType} contentType 
- * @param {string} content 
+ * @param {string} content
+ * @param {string=} mimeType
  */
-WebInspector.StaticContentProvider = function(contentType, content)
+WebInspector.StaticContentProvider = function(contentType, content, mimeType)
 {
     this._content = content;
     this._contentType = contentType;
+    this._mimeType = mimeType;
 }
 
 WebInspector.StaticContentProvider.prototype = {
@@ -272,7 +274,7 @@ WebInspector.StaticContentProvider.prototype = {
      */
     requestContent: function(callback)
     {
-        callback(this._content, false, this._contentType.canonicalMimeType());
+        callback(this._content, false, this._mimeType || this._contentType.canonicalMimeType());
     },
 
     /**
--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceTokenizer.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceTokenizer.js
@@ -84,7 +84,8 @@ WebInspector.SourceTokenizer.Registry = function() {
     this._tokenizerConstructors = {
         "text/css": "SourceCSSTokenizer",
         "text/html": "SourceHTMLTokenizer",
-        "text/javascript": "SourceJavaScriptTokenizer"
+        "text/javascript": "SourceJavaScriptTokenizer",
+        "text/x-scss": "SourceCSSTokenizer"
     };
 }
 
