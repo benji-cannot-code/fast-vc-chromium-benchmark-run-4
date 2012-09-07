02a8de1562b66b2650b94e4511f6f6c02ac68f9f
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-07 23:00:02 +0000

    drive: Get rid of DocumentEntry::GetAllEntryKinds()
    DocumentEntry::GetAllEntryKinds() was necessary as EntryKind enum
    was using complex bitmasks, rather than consecutive numbers.
    This was fragile as we needed to keep EntryKind and EntryKindMap
    in sync manually without help from the compiler.
    
    In this patch, EntryKind is changed to consecutive numbers,
    ClassifyEntryKind() is introduced, and GetAllEntryKinds() is
    removed.
    
    BUG=135162
    TEST=unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/10912160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155519 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_wapi_feed_processor.cc
+++ chrome/browser/chromeos/gdata/gdata_wapi_feed_processor.cc
@@ -72,7 +72,6 @@ void GDataWapiFeedProcessor::UpdateFileCountUmaHistograms(
   UMA_HISTOGRAM_COUNTS("GData.NumberOfHostedDocuments",
                        uma_stats.num_hosted_documents);
   UMA_HISTOGRAM_COUNTS("GData.NumberOfTotalFiles", num_total_files);
-  const std::vector<int> all_entry_kinds = DocumentEntry::GetAllEntryKinds();
   for (FeedToFileResourceMapUmaStats::EntryKindToCountMap::const_iterator iter =
            uma_stats.num_files_with_entry_kind.begin();
        iter != uma_stats.num_files_with_entry_kind.end();
@@ -80,8 +79,8 @@ void GDataWapiFeedProcessor::UpdateFileCountUmaHistograms(
     const DocumentEntry::EntryKind kind = iter->first;
     const int count = iter->second;
     for (int i = 0; i < count; ++i) {
-      UMA_HISTOGRAM_CUSTOM_ENUMERATION(
-          "GData.EntryKind", kind, all_entry_kinds);
+      UMA_HISTOGRAM_ENUMERATION(
+          "Drive.EntryKind", kind, DocumentEntry::NUM_ENTRY_KINDS);
     }
   }
 }
