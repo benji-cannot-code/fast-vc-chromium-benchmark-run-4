83076f9bd923f9f82c0f45f2d72fe12bef73fb9e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-08 00:25:24 +0000

    [Chromium] Multiple commits without invalidation allowed per redraw. https://bugs.webkit.org/show_bug.cgi?id=95661
    Patch by David Reveman <reveman@chromium.org> on 2012-09-07
    Reviewed by James Robinson.
    
    Source/WebCore:
    
    Always enter COMMIT_STATE_WAITING_FOR_FIRST_DRAW after commit. This
    ensures we draw before beginning a new frame.
    
    Unit tests: CCLayerTreeHostTestContinuousCommit.runMultiThread
                CCLayerTreeHostTestContinuousInvalidate.runMultiThread
                CCLayerTreeHostTestContinuousAnimate.runMultiThread
    
    * platform/graphics/chromium/cc/CCSchedulerStateMachine.cpp:
    (WebCore::CCSchedulerStateMachine::updateState):
    
    Source/WebKit/chromium:
    
    * tests/CCLayerTreeHostTest.cpp:
    (CCLayerTreeHostTestContinuousCommit):
    (WebKitTests::CCLayerTreeHostTestContinuousCommit::CCLayerTreeHostTestContinuousCommit):
    (WebKitTests):
    (WebKitTests::TEST_F):
    (CCLayerTreeHostTestContinuousInvalidate):
    (WebKitTests::CCLayerTreeHostTestContinuousInvalidate::CCLayerTreeHostTestContinuousInvalidate):
    (CCLayerTreeHostTestContinuousAnimate):
    (WebKitTests::CCLayerTreeHostTestContinuousAnimate::CCLayerTreeHostTestContinuousAnimate):
    * tests/CCSchedulerStateMachineTest.cpp:
    (WebCore::TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127940 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/CCSchedulerStateMachineTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/CCSchedulerStateMachineTest.cpp
@@ -767,7 +767,7 @@ TEST(CCSchedulerStateMachineTest, TestFullCycle)
 
     // Commit.
     state.updateState(CCSchedulerStateMachine::ACTION_COMMIT);
-    EXPECT_EQ(CCSchedulerStateMachine::COMMIT_STATE_IDLE, state.commitState());
+    EXPECT_EQ(CCSchedulerStateMachine::COMMIT_STATE_WAITING_FOR_FIRST_DRAW, state.commitState());
     EXPECT_TRUE(state.needsRedraw());
 
     // Expect to do nothing until vsync.
@@ -980,6 +980,11 @@ TEST(CCSchedulerStateMachineTest, TestContextLostWhileCommitInProgress)
     EXPECT_EQ(CCSchedulerStateMachine::ACTION_COMMIT, state.nextAction());
     state.updateState(state.nextAction());
 
+    EXPECT_EQ(CCSchedulerStateMachine::COMMIT_STATE_WAITING_FOR_FIRST_DRAW, state.commitState());
+
+    EXPECT_EQ(CCSchedulerStateMachine::ACTION_DRAW_IF_POSSIBLE, state.nextAction());
+    state.updateState(state.nextAction());
+
     // Expect to be told to begin context recreation, independent of vsync state
     state.didEnterVSync();
     EXPECT_EQ(CCSchedulerStateMachine::ACTION_BEGIN_CONTEXT_RECREATION, state.nextAction());
@@ -1010,9 +1015,6 @@ TEST(CCSchedulerStateMachineTest, TestContextLostWhileCommitInProgressAndAnother
     state.didLoseContext();
 
     // Ask for another draw and also set needs commit. Expect nothing happens.
-    // Setting another commit will put us into
-    // COMMIT_STATE_WAITING_FOR_FIRST_DRAW after we finish the frame on the main
-    // thread.
     state.setNeedsRedraw(true);
     state.setNeedsCommit(true);
     EXPECT_EQ(CCSchedulerStateMachine::ACTION_NONE, state.nextAction());
@@ -1105,7 +1107,7 @@ TEST(CCSchedulerStateMachineTest, TestBeginFrameWhenCommitInProgress)
     EXPECT_EQ(CCSchedulerStateMachine::ACTION_COMMIT, state.nextAction());
     state.updateState(state.nextAction());
 
-    EXPECT_EQ(CCSchedulerStateMachine::COMMIT_STATE_IDLE, state.commitState());
+    EXPECT_EQ(CCSchedulerStateMachine::COMMIT_STATE_WAITING_FOR_FIRST_DRAW, state.commitState());
 
     EXPECT_EQ(CCSchedulerStateMachine::ACTION_BEGIN_FRAME, state.nextAction());
 }
