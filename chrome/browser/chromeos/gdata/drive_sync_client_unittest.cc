eaaf68d1de2e76f03db4ae1a0dc5dd60c27e90a5
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-08 17:41:40 +0000

    Rename gdata_test_util.{h,cc} to drive_test_util.{h,cc}
    BUG=137151
    TEST=pass all existing tests.
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10905143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155585 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/drive_sync_client_unittest.cc
+++ chrome/browser/chromeos/gdata/drive_sync_client_unittest.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/cros/mock_network_library.h"
 #include "chrome/browser/chromeos/gdata/drive.pb.h"
 #include "chrome/browser/chromeos/gdata/drive_file_system_util.h"
-#include "chrome/browser/chromeos/gdata/gdata_test_util.h"
+#include "chrome/browser/chromeos/gdata/drive_test_util.h"
 #include "chrome/browser/chromeos/gdata/mock_drive_file_system.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/common/chrome_paths.h"
--- chrome/browser/chromeos/gdata/file_write_helper_unittest.cc
+++ chrome/browser/chromeos/gdata/file_write_helper_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/message_loop.h"
 #include "base/threading/thread_restrictions.h"
-#include "chrome/browser/chromeos/gdata/gdata_test_util.h"
+#include "chrome/browser/chromeos/gdata/drive_test_util.h"
 #include "chrome/browser/chromeos/gdata/mock_drive_file_system.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_browser_thread.h"
