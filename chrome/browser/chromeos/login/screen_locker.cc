476594ee986aa8ab54a21abd0c1d777f28c34b87
khorimoto@chromium.org <khorimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-08 00:24:04 +0000

    Revert 155538 - Replace calls to PowerManagerClient's Notify* methods with SessionManagerClient's methods.
    Use SessionManagerClient's NotifyLockScreen{Shown,Dismissed} to have the "locked" status more in-sync.
    session_manager broadcasts ScreenIs{Locked,Unlocked} signals and power_manager listens to them and take appropriate action.
    See the related changes attached to the bug.
    
    Another following cl will remove powerd's handlers in power_manager.
    
    BUG=chromium-os:32457
    TEST=unittests and manually verify you can lock/unlock the screen.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10905112
    
    TBR=haruki@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10918132
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/power_manager_client.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_service.h"
@@ -92,6 +93,9 @@ class ScreenLockObserver : public chromeos::SessionManagerClient::Observer,
     VLOG(1) << "In: ScreenLockObserver::LockScreen";
     if (session_started_) {
       chromeos::ScreenLocker::Show();
+      chromeos::SessionManagerClient* session_manager =
+          chromeos::DBusThreadManager::Get()->GetSessionManagerClient();
+      session_manager->NotifyLockScreenShown();
     } else {
       // If the user has not completed the sign in we will log them out. This
       // avoids complications with displaying the lock screen over the login
@@ -295,8 +299,8 @@ void ScreenLocker::Show() {
     locker->Init();
   } else {
     DVLOG(1) << "Show: locker already exists. Just sending completion event.";
-    DBusThreadManager::Get()->GetSessionManagerClient()->
-        NotifyLockScreenShown();
+    DBusThreadManager::Get()->GetPowerManagerClient()->
+        NotifyScreenLockCompleted();
   }
 }
 
@@ -335,8 +339,8 @@ ScreenLocker::~ScreenLocker() {
       chrome::NOTIFICATION_SCREEN_LOCK_STATE_CHANGED,
       content::Source<ScreenLocker>(this),
       content::Details<bool>(&state));
-  DBusThreadManager::Get()->GetSessionManagerClient()->
-      NotifyLockScreenDismissed();
+  DBusThreadManager::Get()->GetPowerManagerClient()->
+      NotifyScreenUnlockCompleted();
 }
 
 void ScreenLocker::SetAuthenticator(Authenticator* authenticator) {
@@ -344,7 +348,7 @@ void ScreenLocker::SetAuthenticator(Authenticator* authenticator) {
 }
 
 void ScreenLocker::ScreenLockReady() {
-  VLOG(1) << "ScreenLockReady: sending completed signal to session manager.";
+  VLOG(1) << "ScreenLockReady: sending completed signal to power manager.";
   locked_ = true;
   base::TimeDelta delta = base::Time::Now() - start_time_;
   VLOG(1) << "Screen lock time: " << delta.InSecondsF();
@@ -355,7 +359,8 @@ void ScreenLocker::ScreenLockReady() {
       chrome::NOTIFICATION_SCREEN_LOCK_STATE_CHANGED,
       content::Source<ScreenLocker>(this),
       content::Details<bool>(&state));
-  DBusThreadManager::Get()->GetSessionManagerClient()->NotifyLockScreenShown();
+  DBusThreadManager::Get()->GetPowerManagerClient()->
+      NotifyScreenLockCompleted();
 }
 
 }  // namespace chromeos
