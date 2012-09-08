909936043cba19d43785134b3ea810aeed09e386
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-08 06:16:08 +0000

    Unreviewed prospective build fix.
    * tests/WebCompositorInputHandlerImplTest.cpp: Looks like someone is missing an #include.
    Remove the using directive from r127959.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@127960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebCompositorInputHandlerImplTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebCompositorInputHandlerImplTest.cpp
@@ -39,8 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <public/WebInputHandlerClient.h>
 #include <public/WebPoint.h>
 #include <wtf/OwnPtr.h>
+#include <wtf/PassOwnPtr.h>
 
-using namespace WTF;
 using namespace WebKit;
 
 namespace {
