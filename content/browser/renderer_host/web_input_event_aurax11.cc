8de701dee89865e88c8c2b2af028ebb8ca514cf6
rjkroege@chromium.org <rjkroege@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-09 02:00:12 +0000

    Mark touchscreen-sourced GestureFlingStart events as such.
    Touchpad and touchscreen flings need to use different curves. GestureFlingStart
    type WebGestureEvents can be marked as being touchscreen or touchpad sourced.
    Mark touchscreen-sourced as such so that renderer-side logic can choose the
    correct curve.
    
    BUG=116538
    
    
    Review URL: https://chromiumcodereview.appspot.com/10912125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155608 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/web_input_event_aurax11.cc
+++ content/browser/renderer_host/web_input_event_aurax11.cc
@@ -257,12 +257,18 @@ WebKit::WebMouseWheelEvent MakeWebMouseWheelEventFromAuraEvent(
   return webkit_event;
 }
 
+// NOTE: ui::ScrollEvent instances come from the touchpad.
 WebKit::WebGestureEvent MakeWebGestureEventFromAuraEvent(
     ui::ScrollEvent* event) {
   WebKit::WebGestureEvent webkit_event;
 
   switch (event->type()) {
     case ui::ET_SCROLL:
+      // TODO(sadrul || rjkroege): This will do touchscreen style scrolling in
+      // response to touchpad events. Currently, touchscreen and touchpad
+      // scrolls are the same. However, if the planned changes to touchscreen
+      // scrolling take place, this will no longer be so. If so, this needs to
+      // be adjusted.
       webkit_event.type = WebKit::WebInputEvent::GestureScrollUpdate;
       webkit_event.data.scrollUpdate.deltaX = event->x_offset();
       webkit_event.data.scrollUpdate.deltaY = event->y_offset();
@@ -271,6 +277,8 @@ WebKit::WebGestureEvent MakeWebGestureEventFromAuraEvent(
       webkit_event.type = WebKit::WebInputEvent::GestureFlingStart;
       webkit_event.data.flingStart.velocityX = event->x_offset();
       webkit_event.data.flingStart.velocityY = event->y_offset();
+      webkit_event.data.flingStart.sourceDevice =
+          WebKit::WebGestureEvent::Touchpad;
       break;
     case ui::ET_SCROLL_FLING_CANCEL:
       webkit_event.type = WebKit::WebInputEvent::GestureFlingCancel;
@@ -337,6 +345,7 @@ WebKit::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
   return webkit_event;
 }
 
+// NOTE: ui::GestureEvent instances come from the touchscreen.
 WebKit::WebGestureEvent MakeWebGestureEventFromAuraEvent(
     ui::GestureEvent* event) {
   WebKit::WebGestureEvent gesture_event;
@@ -385,6 +394,8 @@ WebKit::WebGestureEvent MakeWebGestureEventFromAuraEvent(
       gesture_event.type = WebKit::WebInputEvent::GestureFlingStart;
       gesture_event.data.flingStart.velocityX = event->details().velocity_x();
       gesture_event.data.flingStart.velocityY = event->details().velocity_y();
+      gesture_event.data.flingStart.sourceDevice =
+          WebKit::WebGestureEvent::Touchscreen;
       gesture_event.deltaX = event->details().velocity_x();
       gesture_event.deltaY = event->details().velocity_y();
       break;
