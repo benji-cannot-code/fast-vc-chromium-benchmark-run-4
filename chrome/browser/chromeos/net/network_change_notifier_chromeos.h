de9982f2cd1cbf22738de7660feb97633f1c94e4
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-09 02:00:26 +0000

    chromeos: Fix a bug where connection type change sometimes unreported
    BUG=146676
    TEST=http://codereview.chromium.org/10915077/ needs to be landed to test this patch.  Then verifying that 141485 is fixed also verifies this change.
    
    Review URL: https://chromiumcodereview.appspot.com/10913103
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155609 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/net/network_change_notifier_chromeos.h
+++ chrome/browser/chromeos/net/network_change_notifier_chromeos.h
@@ -54,7 +54,7 @@ class NetworkChangeNotifierChromeos
                                 const chromeos::Network* network) OVERRIDE;
 
   // Initiate online status change reporting.
-  void ReportConnectionChange(bool is_online);
+  void ReportConnectionChange();
   void ReportConnectionChangeOnUIThread();
   // Callback from online_notification_task_ when online state notification
   // is actually scheduled.
@@ -85,8 +85,6 @@ class NetworkChangeNotifierChromeos
   // Current active network's IP address.
   std::string ip_address_;
 
-  // The last reported online state.
-  bool is_online_;
   base::WeakPtrFactory<NetworkChangeNotifierChromeos> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(NetworkChangeNotifierChromeos);
