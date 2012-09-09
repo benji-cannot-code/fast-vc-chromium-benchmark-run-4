6f22da36377812b2fdc674ff4f9b462784855057
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-09 23:59:41 +0000

    [chromium] Do not delete texture backing structures on the main thread https://bugs.webkit.org/show_bug.cgi?id=96018
    Patch by Christopher Cameron <ccameron@chromium.org> on 2012-09-09
    Reviewed by James Robinson.
    
    Do not delete CCPrioritizedTexture::Backing structures on the main
    thread.  Instead, unlink them from their owning CCPrioritizedTexture
    in the main thread, and have the impl thread then delete all unlinked
    textures.
    
    This is towards having the main thread not access the m_backings set,
    which will allow the impl thread to traverse that set when deleting
    resources in response to GPU memory management events.
    
    Tested by existing eviction tests (CCLayerTreeHostTest's
    TestEvictTextures, LostContextAfterEvictTextures)
    
    Fix a place where single thread proxy wasn't setting the main thread
    blocked state correctly.
    
    * platform/graphics/chromium/cc/CCLayerTreeHost.cpp:
    (WebCore::CCLayerTreeHost::unlinkAllContentTextures):
    (WebCore):
    (WebCore::CCLayerTreeHost::deleteUnlinkedTextures):
    * platform/graphics/chromium/cc/CCLayerTreeHost.h:
    (CCLayerTreeHost):
    * platform/graphics/chromium/cc/CCPrioritizedTextureManager.cpp:
    (WebCore::CCPrioritizedTextureManager::clearAllMemory):
    (WebCore::CCPrioritizedTextureManager::unlinkAllBackings):
    (WebCore):
    (WebCore::CCPrioritizedTextureManager::deleteAllUnlinkedBackings):
    * platform/graphics/chromium/cc/CCPrioritizedTextureManager.h:
    (CCPrioritizedTextureManager):
    * platform/graphics/chromium/cc/CCSingleThreadProxy.cpp:
    (WebCore::CCSingleThreadProxy::commitAndComposite):
    * platform/graphics/chromium/cc/CCThreadProxy.cpp:
    (WebCore::CCThreadProxy::beginFrame):
    (WebCore::CCThreadProxy::beginFrameCompleteOnImplThread):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCSingleThreadProxy.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/cc/CCSingleThreadProxy.cpp
@@ -155,6 +155,7 @@ bool CCSingleThreadProxy::recreateContext()
 
     bool initialized;
     {
+        DebugScopedSetMainThreadBlocked mainThreadBlocked;
         DebugScopedSetImplThread impl;
         if (!m_layerTreeHostImpl->contentsTexturesPurged())
             m_layerTreeHost->deleteContentsTexturesOnImplThread(m_layerTreeHostImpl->resourceProvider());
@@ -297,12 +298,14 @@ bool CCSingleThreadProxy::commitAndComposite()
 {
     ASSERT(CCProxy::isMainThread());
 
-
     if (!m_layerTreeHost->initializeRendererIfNeeded())
         return false;
 
-    if (m_layerTreeHostImpl->contentsTexturesPurged())
-        m_layerTreeHost->evictAllContentTextures();
+    if (m_layerTreeHostImpl->contentsTexturesPurged()) {
+        m_layerTreeHost->unlinkAllContentTextures();
+        DebugScopedSetImplThreadAndMainThreadBlocked implAndMainBlocked;
+        m_layerTreeHost->deleteUnlinkedTextures();
+    }
 
     CCTextureUpdateQueue queue;
     m_layerTreeHost->updateLayers(queue, m_layerTreeHostImpl->memoryAllocationLimitBytes());
