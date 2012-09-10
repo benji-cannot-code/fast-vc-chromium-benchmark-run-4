8ccc1d62847b898a9fe518f7e9893990bf996488
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-10 22:12:52 +0000

    Source/WebCore: Update notifications to latest spec https://bugs.webkit.org/show_bug.cgi?id=91726 <rdar://problem/11910451>
    Reviewed by Alexey Proskuryakov.
    
    Among the changes:
    
    Addition of lang attribute
    http://lists.w3.org/Archives/Public/public-web-notification/2012Jul/0025.html
    
    Specification of icon URL is icon instead of iconURL
    http://lists.w3.org/Archives/Public/public-web-notification/2012Jul/0012.html
    
    Tests are not possible since at best they only verify exposure of the attributes, which isn't
    terribly useful, and use of the attributes depend on the platform.
    
    * Modules/notifications/Notification.cpp:
    (WebCore::Notification::create): Add support for reading lang, dir, and icon from option dictionary.
    * Modules/notifications/Notification.h:
    (Notification): Make url() and setURL(), which are used for HTML notifications, only available with
    ENABLE(LEGACY_NOTIFICATIONS).
    (WebCore::Notification::setIconURL): Added so that it can be set from the option dictionary.
    (WebCore::Notification::lang):
    (WebCore::Notification::setLang):
    * Modules/notifications/Notification.idl: According to specification, dir is not exposed.
    
    Source/WebKit/mac: Update notifications to latest spec
    https://bugs.webkit.org/show_bug.cgi?id=91726
    <rdar://problem/11910451>
    
    Reviewed by Alexey Proskuryakov.
    
    Expose lang and dir.
    
    * WebView/WebNotification.h:
    * WebView/WebNotification.mm:
    (-[WebNotification lang]):
    (-[WebNotification dir]):
    
    Source/WebKit2: Update notifications to latest spec
    https://bugs.webkit.org/show_bug.cgi?id=91726
    <rdar://problem/11910451>
    
    Reviewed by Alexey Proskuryakov.
    
    Expose lang and dir.
    
    * UIProcess/API/C/WKNotification.cpp:
    (WKNotificationCopyLang):
    (WKNotificationCopyDir):
    * UIProcess/API/C/WKNotification.h:
    * UIProcess/Notifications/WebNotification.cpp:
    (WebKit::WebNotification::WebNotification):
    * UIProcess/Notifications/WebNotification.h:
    (WebKit::WebNotification::create):
    (WebKit::WebNotification::lang):
    (WebKit::WebNotification::dir):
    (WebNotification):
    * UIProcess/Notifications/WebNotificationManagerProxy.cpp:
    (WebKit::WebNotificationManagerProxy::show):
    * UIProcess/Notifications/WebNotificationManagerProxy.h:
    (WebNotificationManagerProxy):
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::showNotification):
    * UIProcess/WebPageProxy.h:
    (WebPageProxy):
    * UIProcess/WebPageProxy.messages.in:
    * WebProcess/Notifications/WebNotificationManager.cpp:
    (WebKit::WebNotificationManager::show):
    
    LayoutTests: The options dictionary in Notification constructor should not accept event listeners
    https://bugs.webkit.org/show_bug.cgi?id=95538
    <rdar://problem/12213901>
    
    Reviewed by Alexey Proskuryakov.
    
    Make sure the onshow handler added in the options dictionary does not get called.
    
    * http/tests/notifications/events-in-dictionary-expected.txt: Added.
    * http/tests/notifications/events-in-dictionary.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Notifications/WebNotification.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/Notifications/WebNotification.cpp
@@ -34,11 +34,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-WebNotification::WebNotification(const String& title, const String& body, const String& iconURL, const String& tag, const String& originString, uint64_t notificationID)
+WebNotification::WebNotification(const String& title, const String& body, const String& iconURL, const String& tag, const String& lang, const String& dir, const String& originString, uint64_t notificationID)
     : m_title(title)
     , m_body(body)
     , m_iconURL(iconURL)
     , m_tag(tag)
+    , m_lang(lang)
+    , m_dir(dir)
     , m_origin(WebSecurityOrigin::createFromString(originString))
     , m_notificationID(notificationID)
 {
