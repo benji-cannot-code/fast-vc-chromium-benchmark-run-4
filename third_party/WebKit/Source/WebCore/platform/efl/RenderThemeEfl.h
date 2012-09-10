8b0900265f42e27484e149a4acf7d3d256c6807c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-10 12:57:03 +0000

    [EFL] Create a toggle button for closed captions https://bugs.webkit.org/show_bug.cgi?id=96259
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-09-10
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    Add toggle button for closed captions to EFL's
    RenderTheme.
    
    Test: media/video-controls-captions.html
    
    * platform/efl/RenderThemeEfl.cpp:
    (WebCore::RenderThemeEfl::edjeGroupFromFormType):
    (WebCore::RenderThemeEfl::emitMediaButtonSignal):
    (WebCore):
    (WebCore::RenderThemeEfl::supportsClosedCaptioning):
    (WebCore::RenderThemeEfl::paintMediaToggleClosedCaptionsButton):
    * platform/efl/RenderThemeEfl.h:
    (RenderThemeEfl):
    
    Source/WebKit/efl:
    
    Add closed caption toggling button to default WebKitEFL
    theme.
    
    * DefaultTheme/default.edc:
    * DefaultTheme/widget/mediacontrol/togglecaptionsbutton/closedcaption.png: Added.
    * DefaultTheme/widget/mediacontrol/togglecaptionsbutton/closedcaption_disabled.png: Added.
    * DefaultTheme/widget/mediacontrol/togglecaptionsbutton/toggle_captions_button.edc: Added.
    
    LayoutTests:
    
    Unskip media/video-controls-captions.html test case
    now that EFL has a toggle button for closed captions.
    
    Rebaseline a few media track test cases that now
    display the new closed caption toggling button.
    
    * platform/efl/TestExpectations:
    * platform/efl/media/track/track-cue-rendering-horizontal-expected.png:
    * platform/efl/media/track/track-cue-rendering-horizontal-expected.txt:
    * platform/efl/media/track/track-cue-rendering-vertical-expected.png:
    * platform/efl/media/track/track-cue-rendering-vertical-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.h
+++ third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.h
@@ -68,6 +68,9 @@ enum FormType { // KEEP IN SYNC WITH edjeGroupFromFormType()
     SeekBackwardButton,
     FullScreenButton,
 #endif
+#if ENABLE(VIDEO_TRACK)
+    ToggleCaptionsButton,
+#endif
     Spinner,
     FormTypeLast
 };
@@ -200,6 +203,10 @@ public:
     virtual bool paintMediaVolumeSliderTrack(RenderObject*, const PaintInfo&, const IntRect&);
     virtual bool paintMediaVolumeSliderThumb(RenderObject*, const PaintInfo&, const IntRect&);
     virtual bool paintMediaCurrentTime(RenderObject*, const PaintInfo&, const IntRect&);
+#endif
+#if ENABLE(VIDEO_TRACK)
+    virtual bool supportsClosedCaptioning() const OVERRIDE;
+    virtual bool paintMediaToggleClosedCaptionsButton(RenderObject*, const PaintInfo&, const IntRect&) OVERRIDE;
 #endif
     virtual bool shouldShowPlaceholderWhenFocused() const OVERRIDE { return true; }
 
