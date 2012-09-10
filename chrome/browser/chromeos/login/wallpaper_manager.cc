b74e5cb85c5be32021e17e4e2de8645ceb8acaea
sschmitz@chromium.org <sschmitz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 17:14:00 +0000

    UMA wallpaper stats
    Records wallpaper type and default index at user login.
    Ash.Wallpaper.Type
    Ash.wallpaper.DefaultIndex
    
    (see also the now closed: https://chromiumcodereview.appspot.com/10910090)
    
    BUG=137360
    TEST=change wallpaper; log out & back in; see chrome://histograms
    
    Review URL: https://chromiumcodereview.appspot.com/10913128
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155752 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wallpaper_manager.cc
+++ chrome/browser/chromeos/login/wallpaper_manager.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/file_util.h"
 #include "base/memory/ref_counted_memory.h"
+#include "base/metrics/histogram.h"
 #include "base/path_service.h"
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
@@ -148,6 +149,8 @@ void WallpaperManager::EnsureLoggedInUserWallpaperLoaded() {
   WallpaperInfo info;
   if (!new_wallpaper_ui_disabled &&
       GetLoggedInUserWallpaperInfo(&info)) {
+    // TODO(sschmitz): We need an index for default wallpapers for the new UI.
+    RecordUma(info.type, -1);
     if (info == current_user_wallpaper_info_)
       return;
   } else {
@@ -155,7 +158,7 @@ void WallpaperManager::EnsureLoggedInUserWallpaperLoaded() {
     int index;
     base::Time date;
     GetLoggedInUserWallpaperProperties(&type, &index, &date);
-
+    RecordUma(type, index);
     if (type == current_user_wallpaper_type_ &&
         index == current_user_wallpaper_index_) {
       return;
@@ -1057,6 +1060,18 @@ void WallpaperManager::ResizeAndSaveCustomWallpaper(const UserImage& wallpaper,
       base::Bind(&WallpaperManager::OnWallpaperEncoded,
                  weak_factory_.GetWeakPtr(),
                  path));
+}
+
+void WallpaperManager::RecordUma(User::WallpaperType type, int index) {
+  UMA_HISTOGRAM_ENUMERATION("Ash.Wallpaper.Type", type,
+                            User::WALLPAPER_TYPE_COUNT);
+  if (type == User::DEFAULT) {
+    if (index >= 0) {
+      // TODO(sschmitz): Remove "if" when the index for new UI is available.
+      UMA_HISTOGRAM_ENUMERATION("Ash.Wallpaper.DefaultIndex", index,
+                                ash::GetWallpaperCount());
+    }
+  }
 }
 
 void WallpaperManager::SaveWallpaperInternal(const FilePath& path,
