ec5024da0c2344692636c47894df2965afa4d26b
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 05:02:24 +0000

    events: Add some test.
    Add a test to verify that the event-handlers are called in
    the right order.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10912168
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155663 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/events/event_dispatcher.h
+++ ui/base/events/event_dispatcher.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_BASE_EVENTS_EVENT_CONSTANTS_EVENTS_EVENT_DISPATCHER_H_
-#define UI_BASE_EVENTS_EVENT_CONSTANTS_EVENTS_EVENT_DISPATCHER_H_
+#ifndef UI_BASE_EVENTS_EVENT_DISPATCHER_H_
+#define UI_BASE_EVENTS_EVENT_DISPATCHER_H_
 
 #include "ui/base/events/event.h"
 #include "ui/base/events/event_constants.h"
@@ -112,4 +112,4 @@ class UI_EXPORT EventDispatcher {
 
 }  // namespace ui
 
-#endif  // UI_BASE_EVENTS_EVENT_CONSTANTS_EVENTS_EVENT_DISPATCHER_H_
+#endif  // UI_BASE_EVENTS_EVENT_DISPATCHER_H_
--- ui/base/events/event_handler.h
+++ ui/base/events/event_handler.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_BASE_EVENTS_EVENT_CONSTANTS_EVENTS_EVENT_HANDLER_H_
-#define UI_BASE_EVENTS_EVENT_CONSTANTS_EVENTS_EVENT_HANDLER_H_
+#ifndef UI_BASE_EVENTS_EVENT_HANDLER_H_
+#define UI_BASE_EVENTS_EVENT_HANDLER_H_
 
 #include <vector>
 
@@ -42,4 +42,4 @@ typedef std::vector<EventHandler*> EventHandlerList;
 
 }  // namespace ui
 
-#endif  // UI_BASE_EVENTS_EVENT_CONSTANTS_EVENTS_EVENT_HANDLER_H_
+#endif  // UI_BASE_EVENTS_EVENT_HANDLER_H_
