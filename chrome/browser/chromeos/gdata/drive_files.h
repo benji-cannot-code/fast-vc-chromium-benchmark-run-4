b7f113c037f6be1e12eb114814d87f49035f316c
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 23:45:37 +0000

    drive: Move EntryKind to drive.proto as DriveEntryKind
    Also add DriveEntryProto::kind of type DriveEntryKind.
    This member is unused now, but will be used as part of
    crbug.com/141511 as we'll need to be able to process
    feeds as DriveEntryProto and count the entry kinds for
    UMA stats.
    
    BUG=141511
    TEST=unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/10907150
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155871 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/drive_files.h
+++ chrome/browser/chromeos/gdata/drive_files.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/platform_file.h"
+#include "chrome/browser/chromeos/gdata/drive.pb.h"
 #include "chrome/browser/chromeos/gdata/gdata_wapi_parser.h"
 
 namespace gdata {
@@ -175,7 +176,7 @@ class DriveFile : public DriveEntry {
   void FromProto(const DriveEntryProto& proto);
   void ToProto(DriveEntryProto* proto) const;
 
-  DocumentEntry::EntryKind kind() const { return kind_; }
+  DriveEntryKind kind() const { return kind_; }
   const GURL& thumbnail_url() const { return thumbnail_url_; }
   const GURL& alternate_url() const { return alternate_url_; }
   const std::string& content_mime_type() const { return content_mime_type_; }
@@ -199,7 +200,7 @@ class DriveFile : public DriveEntry {
 
   virtual DriveFile* AsDriveFile() OVERRIDE;
 
-  DocumentEntry::EntryKind kind_;  // Not saved in proto.
+  DriveEntryKind kind_;  // Not saved in proto.
   GURL thumbnail_url_;
   GURL alternate_url_;
   std::string content_mime_type_;
