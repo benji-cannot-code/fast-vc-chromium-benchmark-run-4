ee1f26faa7c9613f6d3227faefa29efe415df1ac
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-10 21:43:41 +0000

    [WTR] Generated source files should include config.h https://bugs.webkit.org/show_bug.cgi?id=96254
    Reviewed by Benjamin Poulain.
    
    Current coding style has encouraged to include config.h at the first position,
    But derived sources, generated by CodeGeneratorTestRunner.pm, don't follow
    this rule.
    Therefore, this patch added it.
    
    * WebKitTestRunner/InjectedBundle/Bindings/CodeGeneratorTestRunner.pm:
    (_generateImplementationFile):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/Bindings/CodeGeneratorTestRunner.pm
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/Bindings/CodeGeneratorTestRunner.pm
@@ -329,6 +329,7 @@ EOF
 EOF
 
     unshift(@contents, map { "#include \"$_\"\n" } sort keys(%contentsIncludes));
+    unshift(@contents, "#include \"config.h\"\n");
     unshift(@contents, @contentsPrefix);
 
     return { name => $filename, contents => \@contents };
