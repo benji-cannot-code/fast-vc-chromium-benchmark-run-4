168fc88483b30da6496c94e7e0f0f00d3429ae42
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 22:19:03 +0000

    Revert "Moving v8 string conversion functions to content/public/renderer so that they can be shared."
    As per discussions with Jam, this CL extendes the content/public api without
    providing significant gains.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10913170
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/searchbox/searchbox_extension.cc
+++ chrome/renderer/searchbox/searchbox_extension.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/renderer/searchbox/searchbox.h"
 #include "content/public/renderer/render_view.h"
-#include "content/public/renderer/v8_string_conversions.h"
 #include "grit/renderer_resources.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebScriptSource.h"
@@ -34,6 +33,17 @@ void SplitLeadingNumberToken(std::string* number, std::string* suffix) {
     ++i;
   suffix->assign(*number, i, number->size() - i);
   number->resize(i);
+}
+
+// Converts a V8 value to a string16.
+string16 V8ValueToUTF16(v8::Handle<v8::Value> v) {
+  v8::String::Value s(v);
+  return string16(reinterpret_cast<const char16*>(*s), s.length());
+}
+
+// Converts string16 to V8 String.
+v8::Handle<v8::String> UTF16ToV8String(const string16& s) {
+  return v8::String::New(reinterpret_cast<const uint16_t*>(s.data()), s.size());
 }
 
 }  // namespace
@@ -244,7 +254,7 @@ v8::Handle<v8::Value> SearchBoxExtensionWrapper::GetQuery(
     const v8::Arguments& args) {
   content::RenderView* render_view = GetRenderView();
   if (!render_view) return v8::Undefined();
-  return content::UTF16ToV8String(SearchBox::Get(render_view)->query());
+  return UTF16ToV8String(SearchBox::Get(render_view)->query());
 }
 
 // static
@@ -315,9 +325,9 @@ v8::Handle<v8::Value> SearchBoxExtensionWrapper::GetAutocompleteResults(
   for (size_t i = 0; i < results.size(); ++i) {
     v8::Handle<v8::Object> result = v8::Object::New();
     result->Set(v8::String::New("provider"),
-                content::UTF16ToV8String(results[i].provider));
+                UTF16ToV8String(results[i].provider));
     result->Set(v8::String::New("contents"),
-                content::UTF16ToV8String(results[i].contents));
+                UTF16ToV8String(results[i].contents));
     result->Set(v8::String::New("destination_url"),
                 v8::String::New(results[i].destination_url.spec().c_str()));
     result->Set(v8::String::New("rid"), v8::Uint32::New(results_base + i));
@@ -381,7 +391,7 @@ v8::Handle<v8::Value> SearchBoxExtensionWrapper::SetSuggestions(
         v8::Handle<v8::Value> suggestion_object_value =
             suggestion_object->Get(v8::String::New("value"));
         if (!suggestion_object_value->IsString()) continue;
-        string16 text = content::V8ValueToUTF16(suggestion_object_value);
+        string16 text = V8ValueToUTF16(suggestion_object_value);
 
         suggestions.push_back(InstantSuggestion(text, behavior, type));
       }
@@ -397,7 +407,7 @@ v8::Handle<v8::Value> SearchBoxExtensionWrapper::SetSuggestions(
 v8::Handle<v8::Value> SearchBoxExtensionWrapper::SetQuerySuggestion(
     const v8::Arguments& args) {
   if (1 <= args.Length() && args.Length() <= 2 && args[0]->IsString()) {
-    string16 text = content::V8ValueToUTF16(args[0]);
+    string16 text = V8ValueToUTF16(args[0]);
     InstantCompleteBehavior behavior = INSTANT_COMPLETE_NOW;
     InstantSuggestionType type = INSTANT_SUGGESTION_URL;
 
@@ -459,7 +469,7 @@ v8::Handle<v8::Value> SearchBoxExtensionWrapper::SetQuery(
     const v8::Arguments& args) {
   // TODO(sreeram): Make the second argument (type) mandatory.
   if (1 <= args.Length() && args.Length() <= 2 && args[0]->IsString()) {
-    string16 text = content::V8ValueToUTF16(args[0]);
+    string16 text = V8ValueToUTF16(args[0]);
     InstantCompleteBehavior behavior = INSTANT_COMPLETE_REPLACE;
     InstantSuggestionType type = INSTANT_SUGGESTION_SEARCH;
 
