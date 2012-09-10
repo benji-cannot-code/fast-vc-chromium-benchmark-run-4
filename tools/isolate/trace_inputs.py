3955b8766536f4a5da1443371fe1ec0b7821b12b
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 13:50:13 +0000

    Update trace_input s.py to work with Strace 4.7
    NOTRY=true
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10905157
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155716 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/isolate/trace_inputs.py
+++ tools/isolate/trace_inputs.py
@@ -1049,6 +1049,8 @@ class Strace(ApiBase):
       # because the process was forcibly killed so it won't open any new file.
       RE_KILLED = re.compile(
           r'^.*\+\+\+ killed by ([A-Z]+)( \(core dumped\))? \+\+\+$')
+      # The process has exited.
+      RE_PROCESS_EXITED = re.compile(r'^\+\+\+ exited with (\d+) \+\+\+')
       # A call was canceled. Ignore any prefix.
       RE_UNAVAILABLE = re.compile(r'^.*\)\s*= \? <unavailable>$')
       # Happens when strace fails to even get the function name.
@@ -1158,6 +1160,12 @@ class Strace(ApiBase):
           match = self.RE_KILLED.match(line)
           if match:
             # Converts a '+++ killed by Foo +++' trace into an exit_group().
+            self.handle_exit_group(match.group(1), None)
+            return
+
+          match = self.RE_PROCESS_EXITED.match(line)
+          if match:
+            # Converts a '+++ exited with 1 +++' trace into an exit_group()
             self.handle_exit_group(match.group(1), None)
             return
 
