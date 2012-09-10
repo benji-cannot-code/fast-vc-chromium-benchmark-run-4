cbd692ad0cbe10c390e79be7f8feb90117f4fccd
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 21:48:40 +0000

    Fix CrOS build.
    TBR=khorimoto
    Review URL: https://chromiumcodereview.appspot.com/10905194
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155842 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/cryptohome/async_method_caller.h
+++ chromeos/cryptohome/async_method_caller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/basictypes.h"
 #include "base/callback_forward.h"
 #include "chromeos/chromeos_export.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
