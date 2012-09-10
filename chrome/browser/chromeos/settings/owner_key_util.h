37f5d37da1582fe38d14faf46514de2ff10fb62c
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 13:26:33 +0000

    Make OwnerKeyUtil thread safe as originally intended.
    There are actually crash reports that indicate a crash when
    chromeos::SessionManagerOperation::LoadOwnerKey() calls into
    util->IsPublicKeyPresent(). The available data points at the vtable
    contents being invalid, which can happen due to improperly using
    base::RefCounted instead of base::RefCountedThreadSafe.
    
    BUG=chromium:147334
    TEST=Less crashes. Manual or automated test not practical, search the crash database for SessionManagerOperation::LoadOwnerKey() and check that there are no crashes after the fix.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10905179
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings/owner_key_util.h
+++ chrome/browser/chromeos/settings/owner_key_util.h
@@ -25,7 +25,7 @@ namespace chromeos {
 
 class OwnerKeyUtilTest;
 
-class OwnerKeyUtil : public base::RefCounted<OwnerKeyUtil> {
+class OwnerKeyUtil : public base::RefCountedThreadSafe<OwnerKeyUtil> {
  public:
   // Creates an OwnerKeyUtil instance.
   static OwnerKeyUtil* Create();
@@ -48,7 +48,7 @@ class OwnerKeyUtil : public base::RefCounted<OwnerKeyUtil> {
   virtual ~OwnerKeyUtil();
 
  private:
-  friend class base::RefCounted<OwnerKeyUtil>;
+  friend class base::RefCountedThreadSafe<OwnerKeyUtil>;
 
   FRIEND_TEST_ALL_PREFIXES(OwnerKeyUtilTest, ExportImportPublicKey);
 };
