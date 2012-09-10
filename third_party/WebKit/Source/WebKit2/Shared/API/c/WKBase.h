564f583b371d2357ff12a1a75e61e66cf25579d1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-10 09:09:14 +0000

    [WK2] Make WK2 C API headers compile in the Qt build on Windows https://bugs.webkit.org/show_bug.cgi?id=96246
    Patch by Simon Hausmann <simon.hausmann@nokia.com> on 2012-09-10
    Reviewed by Kenneth Rohde Christiansen.
    
    WKBase.h and WKAPICast.h include files that are specific to the Safari/Windows
    port if WIN32 or _WIN32 is defined. When building WebKit2 on Windows with Qt, we
    do not want to include these files, as they define an API that is different to ours
    (the view aspect differs). Therefore guard the inclusion with !defined(BUILDING_QT__)
    
    * Shared/API/c/WKBase.h:
    * UIProcess/API/C/WKAPICast.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128033 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/API/c/WKBase.h
+++ third_party/WebKit/Source/WebKit2/Shared/API/c/WKBase.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdint.h>
 
-#if defined(WIN32) || defined(_WIN32)
+#if (defined(WIN32) || defined(_WIN32)) && !defined(BUILDING_QT__)
 #include <WebKit2/WKBaseWin.h>
 #endif
 
--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKAPICast.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKAPICast.h
@@ -424,7 +424,7 @@ inline ProxyingRefPtr<WebGrammarDetail> toAPI(const WebCore::GrammarDetail& gram
 
 } // namespace WebKit
 
-#if defined(WIN32) || defined(_WIN32)
+#if (defined(WIN32) || defined(_WIN32)) && !defined(BUILDING_QT__)
 #include "WKAPICastWin.h"
 #endif
 
