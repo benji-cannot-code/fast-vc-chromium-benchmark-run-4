3f701ceb6d95d17d379a1d6ab89120f8cad8da67
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 20:47:10 +0000

    Fix leaks in ash_unittests also suppressed leaks in non chrome component plus a couple of cleanups (eliminated unused code/variables)
    BUG=144990,146947
    TEST=valgrind reports no leak on ash_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10907101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155819 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/test/test_shell_delegate.h
+++ ash/test/test_shell_delegate.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/shell_delegate.h"
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 
 namespace ash {
 namespace test {
@@ -54,6 +55,7 @@ class TestShellDelegate : public ShellDelegate {
  private:
   bool locked_;
   bool spoken_feedback_enabled_;
+  scoped_ptr<content::BrowserContext> current_browser_context_;
 
   DISALLOW_COPY_AND_ASSIGN(TestShellDelegate);
 };
