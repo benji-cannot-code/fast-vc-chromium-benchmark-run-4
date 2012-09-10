2d8eb0a60f79ba14e1f42c93e02091fe7535ba11
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 22:32:40 +0000

    Re-enable most of the tests disabled for bug 136310, the underlying WebKit bug has now been fixed. MultipartResponseTest.SingleVisit is still disabled, since it's still failing for some reason.
    BUG=136310
    
    Review URL: https://chromiumcodereview.appspot.com/10911187
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155852 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/errorpage_browsertest.cc
+++ chrome/browser/errorpage_browsertest.cc
@@ -141,8 +141,7 @@ IN_PROC_BROWSER_TEST_F(ErrorPageTest, DNSError_GoBack1) {
 #endif
 // Test that a DNS error occuring in the main frame does not result in an
 // additional session history entry.
-// Disabled:  http://crbug.com/136310
-IN_PROC_BROWSER_TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2) {
+IN_PROC_BROWSER_TEST_F(ErrorPageTest, DNSError_GoBack2) {
   NavigateToFileURL(FILE_PATH_LITERAL("title2.html"));
 
   NavigateToURLAndWaitForTitle(GetDnsErrorURL(), "Mock Link Doctor", 2);
@@ -160,8 +159,7 @@ IN_PROC_BROWSER_TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2) {
 #endif
 // Test that a DNS error occuring in the main frame does not result in an
 // additional session history entry.
-// Disabled:  http://crbug.com/136310
-IN_PROC_BROWSER_TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2AndForward) {
+IN_PROC_BROWSER_TEST_F(ErrorPageTest, DNSError_GoBack2AndForward) {
   NavigateToFileURL(FILE_PATH_LITERAL("title2.html"));
 
   NavigateToURLAndWaitForTitle(GetDnsErrorURL(), "Mock Link Doctor", 2);
@@ -181,8 +179,7 @@ IN_PROC_BROWSER_TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2AndForward) {
 #endif
 // Test that a DNS error occuring in the main frame does not result in an
 // additional session history entry.
-// Disabled:  http://crbug.com/136310
-IN_PROC_BROWSER_TEST_F(ErrorPageTest, DISABLED_DNSError_GoBack2Forward2) {
+IN_PROC_BROWSER_TEST_F(ErrorPageTest, DNSError_GoBack2Forward2) {
   NavigateToFileURL(FILE_PATH_LITERAL("title3.html"));
 
   NavigateToURLAndWaitForTitle(GetDnsErrorURL(), "Mock Link Doctor", 2);
