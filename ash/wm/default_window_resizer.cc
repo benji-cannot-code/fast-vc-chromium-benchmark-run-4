bbfc79acdd9ab4b6ec7dcb51ad1b2a9d0da3c6b0
khorimoto@chromium.org <khorimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 22:34:13 +0000

    Revert 155840 - Adding proper dragging behavior for L/R maximized windows.
    BUG=141750
    TEST=unit tests & visual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10918077
    
    TBR=skuhne@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10919198
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155854 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/default_window_resizer.cc
+++ ash/wm/default_window_resizer.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell.h"
 #include "ash/wm/coordinate_conversion.h"
 #include "ash/wm/cursor_manager.h"
-#include "ash/wm/property_util.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/env.h"
 #include "ui/aura/root_window.h"
@@ -44,8 +43,6 @@ void DefaultWindowResizer::Drag(const gfx::Point& location, int event_flags) {
 
   gfx::Rect bounds(CalculateBoundsForDrag(details_, location));
   if (bounds != details_.window->bounds()) {
-    if (!did_move_or_resize_ && !details_.restore_bounds.IsEmpty())
-      ClearRestoreBounds(details_.window);
     did_move_or_resize_ = true;
     details_.window->SetBounds(bounds);
   }
@@ -59,9 +56,6 @@ void DefaultWindowResizer::RevertDrag() {
     return;
 
   details_.window->SetBounds(details_.initial_bounds);
-
-  if (!details_.restore_bounds.IsEmpty())
-    SetRestoreBoundsInScreen(details_.window, details_.restore_bounds);
 }
 
 aura::Window* DefaultWindowResizer::GetTarget() {
