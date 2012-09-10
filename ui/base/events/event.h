1e7efbea1a5833e43065414783e244e9e56b587c
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-10 05:10:52 +0000

    Revert 155663 because it broke builds on cros/mac. --- events: Add some test.
    Add a test to verify that the event-handlers are called in
    the right order.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10912168
    
    TBR=sadrul@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/10919187
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155666 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/events/event.h
+++ ui/base/events/event.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef UI_BASE_EVENTS_EVENT_H_
-#define UI_BASE_EVENTS_EVENT_H_
+#ifndef UI_BASE_EVENTS_EVENT_CONSTANTS_EVENT_H_
+#define UI_BASE_EVENTS_EVENT_CONSTANTS_EVENT_H_
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
@@ -621,4 +621,4 @@ class UI_EXPORT GestureEvent : public LocatedEvent {
 
 }  // namespace ui
 
-#endif  // UI_BASE_EVENTS_EVENT_H_
+#endif  // UI_BASE_EVENTS_EVENT_CONSTANTS_EVENT_H_
--- ui/ui_unittests.gypi
+++ ui/ui_unittests.gypi
@@ -75,7 +75,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'base/cocoa/events_mac_unittest.mm',
         'base/cocoa/focus_tracker_unittest.mm',
         'base/dialogs/select_file_dialog_win_unittest.cc',
-        'base/events/event_dispatcher_unittest.cc',
         'base/events/event_unittest.cc',
         'base/gtk/gtk_expanded_container_unittest.cc',
         'base/gtk/gtk_im_context_util_unittest.cc',
