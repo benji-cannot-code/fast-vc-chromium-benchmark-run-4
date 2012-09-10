dd0168130c7173191c6f2546437cd9f87171bdce
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-10 18:50:54 +0000

    WKTR often reports an unresponsive WebProcess on Mac bots https://bugs.webkit.org/show_bug.cgi?id=95906
    Reviewed by Dan Bernstein.
    
    Attempt to work around resource contention issues by increasing the NRWT
    timeout when running against WKTR, as well as WKTR's own WebProcess timeout.
    
    * Scripts/webkitpy/layout_tests/port/base.py:
    (Port.default_timeout_ms): Default timeout when using WKTR -> 80 seconds.
    * WebKitTestRunner/TestController.cpp:
    (WTR): LongTimeout (used when waiting for WebProcess to finish) -> 60 seconds.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128086 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/TestController.cpp
@@ -56,10 +56,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTR {
 
-// defaultLongTimeout + defaultShortTimeout should be less than 50,
+// defaultLongTimeout + defaultShortTimeout should be less than 80,
 // the default timeout value of the test harness so we can detect an
 // unresponsive web process.
-static const double defaultLongTimeout = 30;
+static const double defaultLongTimeout = 60;
 static const double defaultShortTimeout = 15;
 static const double defaultNoTimeout = -1;
 
