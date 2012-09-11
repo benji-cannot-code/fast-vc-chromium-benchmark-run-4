6f064e75b1d4c75a565770d851264a8c7a459dd2
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 19:16:16 +0000

    CSS Media Query now reports touch-support accurately for Windows (retry) This patch was LGTM'ed as https://chromiumcodereview.appspot.com/10836349/ and submitted through the CQ, but was later reverted.  Re-patching.
    BUG=143792
    
    
    Review URL: https://chromiumcodereview.appspot.com/10915175
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156091 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/base/touch/touch_device_win.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_BASE_TOUCH_TOUCH_DEVICE_H_
+#define UI_BASE_TOUCH_TOUCH_DEVICE_H_
+
+#include "ui/base/ui_export.h"
+
+namespace ui {
+
+// Returns true if a touch device is available.
+UI_EXPORT bool IsTouchDevicePresent();
+
+}  // namespace ui
+
+#endif  // UI_BASE_TOUCH_TOUCH_DEVICE_H_
