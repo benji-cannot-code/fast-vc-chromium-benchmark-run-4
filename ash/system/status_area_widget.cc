d6e0a1b59e05e930fd25adfea515b8f8a12d8cea
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 22:54:00 +0000

    Add "Display" item in the uber tray.
    Previously I submitted a CL for this, but it was reverted due to
    build break of win_aura. To prevent the error again, I splitted
    it into two, and this is the second CL to add the actual UI.
    
    BUG=141394
    TBR=sky@chromium.org, oshima@chromium.org, jennyz@chromium.org, sadrul@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10918180
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156156 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/status_area_widget.cc
+++ ash/system/status_area_widget.cc
@@ -117,6 +117,9 @@ class DummySystemTrayDelegate : public SystemTrayDelegate {
   virtual void ShowBluetoothSettings() OVERRIDE {
   }
 
+  virtual void ShowDisplaySettings() OVERRIDE {
+  }
+
   virtual void ShowDriveSettings() OVERRIDE {
   }
 
