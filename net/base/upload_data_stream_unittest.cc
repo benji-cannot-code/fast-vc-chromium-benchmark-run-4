5c564aa49d2d5cae9a1f677f7593fafc4af8c8c4
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 20:27:21 +0000

    net: Move data reading functionalities from UploadElement to UploadElementReader
    Add a new interface net::UploadElementReader and its implementations for bytes and file.
    Rewrite UploadDataStream using UploadElementReader.
    UploadElement::SetContentLength is replaced with UploadFileElementReader::ScopedOverridingContentLengthForTests.
    HttpNetworkTransactionSpdy(2|3)Test.UnreadableUploadFileAfterAuthRestart is bit modified since a new FileStream is created every time when UploadDataStream is initialized.
    
    BUG=145329
    TEST=net_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10868064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156113 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/upload_data_stream_unittest.cc
+++ net/base/upload_data_stream_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 #include "net/base/net_errors.h"
 #include "net/base/upload_data.h"
+#include "net/base/upload_file_element_reader.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
@@ -117,10 +118,12 @@ TEST_F(UploadDataStreamTest, FileSmallerThanLength) {
             file_util::WriteFile(temp_file_path, kTestData, kTestDataSize));
   const uint64 kFakeSize = kTestDataSize*2;
 
+  UploadFileElementReader::ScopedOverridingContentLengthForTests
+      overriding_content_length(kFakeSize);
+
   std::vector<UploadElement> elements;
   UploadElement element;
   element.SetToFilePath(temp_file_path);
-  element.SetContentLength(kFakeSize);
   elements.push_back(element);
   upload_data_->SetElements(elements);
 
