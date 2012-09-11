5823345e65890b4f4088bcc0ee6a1c8137aa275a
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-11 19:17:20 +0000

    IndexedDB: IDBObjectStore.count() is slow https://bugs.webkit.org/show_bug.cgi?id=96033
    Reviewed by Tony Chang.
    
    Avoid decoding record values when performing count() iteration, and factor out common code
    used for initializing object store and index cursors. Also, remove unused indexDataId()
    methods (which date back to the SQLite backing store), and rename some p's and q's.
    
    No new tests - no functional changes. Covered by existing test:
        LayoutTests/storage/indexeddb/objectstore-count.html
    
    * Modules/indexeddb/IDBBackingStore.h: Expose new (internal) ObjectStoreKeyCursor
    (IDBBackingStore):
    * Modules/indexeddb/IDBLevelDBBackingStore.cpp:
    (WebCore): Factor out common CursorOptions initialization code. The objectStoreCursorOptions()
    function is needed for this patch. The indexCursorOptions() function is not, but eliminates
    duplicated code and is a nearly identical refactor.
    (WebCore::IDBLevelDBBackingStore::openObjectStoreCursor): Refactor.
    (WebCore::IDBLevelDBBackingStore::openObjectStoreKeyCursor): Implement.
    (WebCore::IDBLevelDBBackingStore::openIndexKeyCursor): Refactor.
    (WebCore::IDBLevelDBBackingStore::openIndexCursor): Ditto.
    * Modules/indexeddb/IDBLevelDBBackingStore.h:
    (IDBLevelDBBackingStore):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.cpp:
    (WebCore::IDBObjectStoreBackendImpl::countInternal): Use ObjectStoreKeyCursor for iteration.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBLevelDBBackingStore.cpp
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBLevelDBBackingStore.cpp
@@ -1140,7 +1140,6 @@ public:
     virtual PassRefPtr<IDBKey> primaryKey() { return m_currentKey; }
     virtual String value() = 0;
     virtual PassRefPtr<IDBBackingStore::ObjectStoreRecordIdentifier> objectStoreRecordIdentifier() = 0;
-    virtual int64_t indexDataId() = 0;
     virtual void close() { }
 
     virtual bool loadCurrentRow() = 0;
@@ -1318,6 +1317,65 @@ bool CursorImplCommon::isPastBounds() const
     return compareIndexKeys(m_iterator->key(), m_cursorOptions.lowKey) < 0;
 }
 
+class ObjectStoreKeyCursorImpl : public CursorImplCommon {
+public:
+    static PassRefPtr<ObjectStoreKeyCursorImpl> create(LevelDBTransaction* transaction, const CursorOptions& cursorOptions)
+    {
+        return adoptRef(new ObjectStoreKeyCursorImpl(transaction, cursorOptions));
+    }
+
+    virtual PassRefPtr<IDBBackingStore::Cursor> clone()
+    {
+        return adoptRef(new ObjectStoreKeyCursorImpl(this));
+    }
+
+    // CursorImplCommon
+    virtual String value() { ASSERT_NOT_REACHED(); return String(); }
+    virtual PassRefPtr<IDBBackingStore::ObjectStoreRecordIdentifier> objectStoreRecordIdentifier() OVERRIDE
+    {
+        return m_identifier;
+    }
+    virtual bool loadCurrentRow();
+
+private:
+    ObjectStoreKeyCursorImpl(LevelDBTransaction* transaction, const CursorOptions& cursorOptions)
+        : CursorImplCommon(transaction, cursorOptions)
+    {
+    }
+
+    ObjectStoreKeyCursorImpl(const ObjectStoreKeyCursorImpl* other)
+        : CursorImplCommon(other)
+    {
+    }
+
+    RefPtr<LevelDBRecordIdentifier> m_identifier;
+};
+
+bool ObjectStoreKeyCursorImpl::loadCurrentRow()
+{
+    const char* keyPosition = m_iterator->key().begin();
+    const char* keyLimit = m_iterator->key().end();
+
+    ObjectStoreDataKey objectStoreDataKey;
+    keyPosition = ObjectStoreDataKey::decode(keyPosition, keyLimit, &objectStoreDataKey);
+    ASSERT(keyPosition);
+    if (!keyPosition)
+        return false;
+
+    m_currentKey = objectStoreDataKey.userKey();
+
+    int64_t version;
+    const char* valuePosition = decodeVarInt(m_iterator->value().begin(), m_iterator->value().end(), version);
+    ASSERT(valuePosition);
+    if (!valuePosition)
+        return false;
+
+    // FIXME: This re-encodes what was just decoded; try and optimize.
+    m_identifier = LevelDBRecordIdentifier::create(encodeIDBKey(*m_currentKey), version);
+
+    return true;
+}
+
 class ObjectStoreCursorImpl : public CursorImplCommon {
 public:
     static PassRefPtr<ObjectStoreCursorImpl> create(LevelDBTransaction* transaction, const CursorOptions& cursorOptions)
@@ -1336,7 +1394,6 @@ public:
     {
         return m_identifier;
     }
-    virtual int64_t indexDataId() { ASSERT_NOT_REACHED(); return 0; }
     virtual bool loadCurrentRow();
 
 private:
@@ -1357,27 +1414,27 @@ private:
 
 bool ObjectStoreCursorImpl::loadCurrentRow()
 {
-    const char* p = m_iterator->key().begin();
+    const char* keyPosition = m_iterator->key().begin();
     const char* keyLimit = m_iterator->key().end();
 
     ObjectStoreDataKey objectStoreDataKey;
-    p = ObjectStoreDataKey::decode(p, keyLimit, &objectStoreDataKey);
-    ASSERT(p);
-    if (!p)
+    keyPosition = ObjectStoreDataKey::decode(keyPosition, keyLimit, &objectStoreDataKey);
+    ASSERT(keyPosition);
+    if (!keyPosition)
         return false;
 
     m_currentKey = objectStoreDataKey.userKey();
 
     int64_t version;
-    const char* q = decodeVarInt(m_iterator->value().begin(), m_iterator->value().end(), version);
-    ASSERT(q);
-    if (!q)
+    const char* valuePosition = decodeVarInt(m_iterator->value().begin(), m_iterator->value().end(), version);
+    ASSERT(valuePosition);
+    if (!valuePosition)
         return false;
 
     // FIXME: This re-encodes what was just decoded; try and optimize.
     m_identifier = LevelDBRecordIdentifier::create(encodeIDBKey(*m_currentKey), version);
 
-    m_currentValue = decodeString(q, m_iterator->value().end());
+    m_currentValue = decodeString(valuePosition, m_iterator->value().end());
 
     return true;
 }
@@ -1398,7 +1455,6 @@ public:
     virtual String value() { ASSERT_NOT_REACHED(); return String(); }
     virtual PassRefPtr<IDBKey> primaryKey() { return m_primaryKey; }
     virtual PassRefPtr<IDBBackingStore::ObjectStoreRecordIdentifier> objectStoreRecordIdentifier() { ASSERT_NOT_REACHED(); return 0; }
-    virtual int64_t indexDataId() { ASSERT_NOT_REACHED(); return 0; }
     virtual bool loadCurrentRow();
 
 private:
@@ -1418,22 +1474,23 @@ private:
 
 bool IndexKeyCursorImpl::loadCurrentRow()
 {
-    const char* p = m_iterator->key().begin();
+    const char* keyPosition = m_iterator->key().begin();
     const char* keyLimit = m_iterator->key().end();
+
     IndexDataKey indexDataKey;
-    p = IndexDataKey::decode(p, keyLimit, &indexDataKey);
+    keyPosition = IndexDataKey::decode(keyPosition, keyLimit, &indexDataKey);
 
     m_currentKey = indexDataKey.userKey();
 
     int64_t indexDataVersion;
-    const char* q = decodeVarInt(m_iterator->value().begin(), m_iterator->value().end(), indexDataVersion);
-    ASSERT(q);
-    if (!q)
+    const char* valuePosition = decodeVarInt(m_iterator->value().begin(), m_iterator->value().end(), indexDataVersion);
+    ASSERT(valuePosition);
+    if (!valuePosition)
         return false;
 
-    q = decodeIDBKey(q, m_iterator->value().end(), m_primaryKey);
-    ASSERT(q);
-    if (!q)
+    valuePosition = decodeIDBKey(valuePosition, m_iterator->value().end(), m_primaryKey);
+    ASSERT(valuePosition);
+    if (!valuePosition)
         return false;
 
     Vector<char> primaryLevelDBKey = ObjectStoreDataKey::encode(indexDataKey.databaseId(), indexDataKey.objectStoreId(), *m_primaryKey);
@@ -1474,7 +1531,6 @@ public:
     virtual String value() { return m_value; }
     virtual PassRefPtr<IDBKey> primaryKey() { return m_primaryKey; }
     virtual PassRefPtr<IDBBackingStore::ObjectStoreRecordIdentifier> objectStoreRecordIdentifier() { ASSERT_NOT_REACHED(); return 0; }
-    virtual int64_t indexDataId() { ASSERT_NOT_REACHED(); return 0; }
     bool loadCurrentRow();
 
 private:
@@ -1498,25 +1554,25 @@ private:
 
 bool IndexCursorImpl::loadCurrentRow()
 {
-    const char* p = m_iterator->key().begin();
-    const char* limit = m_iterator->key().end();
+    const char* keyPosition = m_iterator->key().begin();
+    const char* keyLimit = m_iterator->key().end();
 
     IndexDataKey indexDataKey;
-    p = IndexDataKey::decode(p, limit, &indexDataKey);
+    keyPosition = IndexDataKey::decode(keyPosition, keyLimit, &indexDataKey);
 
     m_currentKey = indexDataKey.userKey();
 
-    const char* q = m_iterator->value().begin();
+    const char* valuePosition = m_iterator->value().begin();
     const char* valueLimit = m_iterator->value().end();
 
     int64_t indexDataVersion;
-    q = decodeVarInt(q, valueLimit, indexDataVersion);
-    ASSERT(q);
-    if (!q)
+    valuePosition = decodeVarInt(valuePosition, valueLimit, indexDataVersion);
+    ASSERT(valuePosition);
+    if (!valuePosition)
         return false;
-    q = decodeIDBKey(q, valueLimit, m_primaryKey);
-    ASSERT(q);
-    if (!q)
+    valuePosition = decodeIDBKey(valuePosition, valueLimit, m_primaryKey);
+    ASSERT(valuePosition);
+    if (!valuePosition)
         return false;
 
     m_primaryLevelDBKey = ObjectStoreDataKey::encode(indexDataKey.databaseId(), indexDataKey.objectStoreId(), *m_primaryKey);
@@ -1542,14 +1598,8 @@ bool IndexCursorImpl::loadCurrentRow()
     return true;
 }
 
-}
-
-PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openObjectStoreCursor(int64_t databaseId, int64_t objectStoreId, const IDBKeyRange* range, IDBCursor::Direction direction)
+bool objectStoreCursorOptions(LevelDBTransaction* transaction, int64_t databaseId, int64_t objectStoreId, const IDBKeyRange* range, IDBCursor::Direction direction, CursorOptions& cursorOptions)
 {
-    IDB_TRACE("IDBLevelDBBackingStore::openObjectStoreCursor");
-    ASSERT(m_currentTransaction);
-    CursorOptions cursorOptions;
-
     bool lowerBound = range && range->lower();
     bool upperBound = range && range->upper();
     cursorOptions.forward = (direction == IDBCursor::NEXT_NO_DUPLICATE || direction == IDBCursor::NEXT);
@@ -1570,8 +1620,8 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openObjectStoreCurso
             cursorOptions.highOpen = true; // Not included.
         else {
             // We need a key that exists.
-            if (!findGreatestKeyLessThanOrEqual(m_currentTransaction.get(), cursorOptions.highKey, cursorOptions.highKey))
-                return 0;
+            if (!findGreatestKeyLessThanOrEqual(transaction, cursorOptions.highKey, cursorOptions.highKey))
+                return false;
             cursorOptions.highOpen = false;
         }
     } else {
@@ -1581,8 +1631,8 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openObjectStoreCurso
         if (!cursorOptions.forward) {
             // For reverse cursors, we need a key that exists.
             Vector<char> foundHighKey;
-            if (!findGreatestKeyLessThanOrEqual(m_currentTransaction.get(), cursorOptions.highKey, foundHighKey))
-                return 0;
+            if (!findGreatestKeyLessThanOrEqual(transaction, cursorOptions.highKey, foundHighKey))
+                return false;
 
             // If the target key should not be included, but we end up with a smaller key, we should include that.
             if (cursorOptions.highOpen && compareIndexKeys(foundHighKey, cursorOptions.highKey) < 0)
@@ -1592,18 +1642,12 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openObjectStoreCurso
         }
     }
 
-    RefPtr<ObjectStoreCursorImpl> cursor = ObjectStoreCursorImpl::create(m_currentTransaction.get(), cursorOptions);
-    if (!cursor->firstSeek())
-        return 0;
-
-    return cursor.release();
+    return true;
 }
 
-PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openIndexKeyCursor(int64_t databaseId, int64_t objectStoreId, int64_t indexId, const IDBKeyRange* range, IDBCursor::Direction direction)
+bool indexCursorOptions(LevelDBTransaction* transaction, int64_t databaseId, int64_t objectStoreId, int64_t indexId, const IDBKeyRange* range, IDBCursor::Direction direction, CursorOptions& cursorOptions)
 {
-    IDB_TRACE("IDBLevelDBBackingStore::openIndexKeyCursor");
-    ASSERT(m_currentTransaction);
-    CursorOptions cursorOptions;
+    ASSERT(transaction);
     bool lowerBound = range && range->lower();
     bool upperBound = range && range->upper();
     cursorOptions.forward = (direction == IDBCursor::NEXT_NO_DUPLICATE || direction == IDBCursor::NEXT);
@@ -1622,8 +1666,8 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openIndexKeyCursor(i
         cursorOptions.highOpen = false; // Included.
 
         if (!cursorOptions.forward) { // We need a key that exists.
-            if (!findGreatestKeyLessThanOrEqual(m_currentTransaction.get(), cursorOptions.highKey, cursorOptions.highKey))
-                return 0;
+            if (!findGreatestKeyLessThanOrEqual(transaction, cursorOptions.highKey, cursorOptions.highKey))
+                return false;
             cursorOptions.highOpen = false;
         }
     } else {
@@ -1631,8 +1675,8 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openIndexKeyCursor(i
         cursorOptions.highOpen = range->upperOpen();
 
         Vector<char> foundHighKey;
-        if (!findGreatestKeyLessThanOrEqual(m_currentTransaction.get(), cursorOptions.highKey, foundHighKey)) // Seek to the *last* key in the set of non-unique keys.
-            return 0;
+        if (!findGreatestKeyLessThanOrEqual(transaction, cursorOptions.highKey, foundHighKey)) // Seek to the *last* key in the set of non-unique keys.
+            return false;
 
         // If the target key should not be included, but we end up with a smaller key, we should include that.
         if (cursorOptions.highOpen && compareIndexKeys(foundHighKey, cursorOptions.highKey) < 0)
@@ -1641,6 +1685,46 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openIndexKeyCursor(i
         cursorOptions.highKey = foundHighKey;
     }
 
+    return true;
+}
+
+}
+
+PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openObjectStoreCursor(int64_t databaseId, int64_t objectStoreId, const IDBKeyRange* range, IDBCursor::Direction direction)
+{
+    IDB_TRACE("IDBLevelDBBackingStore::openObjectStoreCursor");
+    ASSERT(m_currentTransaction);
+    CursorOptions cursorOptions;
+    if (!objectStoreCursorOptions(m_currentTransaction.get(), databaseId, objectStoreId, range, direction, cursorOptions))
+        return 0;
+    RefPtr<ObjectStoreCursorImpl> cursor = ObjectStoreCursorImpl::create(m_currentTransaction.get(), cursorOptions);
+    if (!cursor->firstSeek())
+        return 0;
+
+    return cursor.release();
+}
+
+PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openObjectStoreKeyCursor(int64_t databaseId, int64_t objectStoreId, const IDBKeyRange* range, IDBCursor::Direction direction)
+{
+    IDB_TRACE("IDBLevelDBBackingStore::openObjectStoreKeyCursor");
+    ASSERT(m_currentTransaction);
+    CursorOptions cursorOptions;
+    if (!objectStoreCursorOptions(m_currentTransaction.get(), databaseId, objectStoreId, range, direction, cursorOptions))
+        return 0;
+    RefPtr<ObjectStoreKeyCursorImpl> cursor = ObjectStoreKeyCursorImpl::create(m_currentTransaction.get(), cursorOptions);
+    if (!cursor->firstSeek())
+        return 0;
+
+    return cursor.release();
+}
+
+PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openIndexKeyCursor(int64_t databaseId, int64_t objectStoreId, int64_t indexId, const IDBKeyRange* range, IDBCursor::Direction direction)
+{
+    IDB_TRACE("IDBLevelDBBackingStore::openIndexKeyCursor");
+    ASSERT(m_currentTransaction);
+    CursorOptions cursorOptions;
+    if (!indexCursorOptions(m_currentTransaction.get(), databaseId, objectStoreId, indexId, range, direction, cursorOptions))
+        return 0;
     RefPtr<IndexKeyCursorImpl> cursor = IndexKeyCursorImpl::create(m_currentTransaction.get(), cursorOptions);
     if (!cursor->firstSeek())
         return 0;
@@ -1653,43 +1737,8 @@ PassRefPtr<IDBBackingStore::Cursor> IDBLevelDBBackingStore::openIndexCursor(int6
     IDB_TRACE("IDBLevelDBBackingStore::openIndexCursor");
     ASSERT(m_currentTransaction);
     CursorOptions cursorOptions;
-    bool lowerBound = range && range->lower();
-    bool upperBound = range && range->upper();
-    cursorOptions.forward = (direction == IDBCursor::NEXT_NO_DUPLICATE || direction == IDBCursor::NEXT);
-    cursorOptions.unique = (direction == IDBCursor::NEXT_NO_DUPLICATE || direction == IDBCursor::PREV_NO_DUPLICATE);
-
-    if (!lowerBound) {
-        cursorOptions.lowKey = IndexDataKey::encodeMinKey(databaseId, objectStoreId, indexId);
-        cursorOptions.lowOpen = false; // Included.
-    } else {
-        cursorOptions.lowKey = IndexDataKey::encode(databaseId, objectStoreId, indexId, *range->lower());
-        cursorOptions.lowOpen = range->lowerOpen();
-    }
-
-    if (!upperBound) {
-        cursorOptions.highKey = IndexDataKey::encodeMaxKey(databaseId, objectStoreId, indexId);
-        cursorOptions.highOpen = false; // Included.
-
-        if (!cursorOptions.forward) { // We need a key that exists.
-            if (!findGreatestKeyLessThanOrEqual(m_currentTransaction.get(), cursorOptions.highKey, cursorOptions.highKey))
-                return 0;
-            cursorOptions.highOpen = false;
-        }
-    } else {
-        cursorOptions.highKey = IndexDataKey::encode(databaseId, objectStoreId, indexId, *range->upper());
-        cursorOptions.highOpen = range->upperOpen();
-
-        Vector<char> foundHighKey;
-        if (!findGreatestKeyLessThanOrEqual(m_currentTransaction.get(), cursorOptions.highKey, foundHighKey)) // Seek to the *last* key in the set of non-unique keys.
-            return 0;
-
-        // If the target key should not be included, but we end up with a smaller key, we should include that.
-        if (cursorOptions.highOpen && compareIndexKeys(foundHighKey, cursorOptions.highKey) < 0)
-            cursorOptions.highOpen = false;
-
-        cursorOptions.highKey = foundHighKey;
-    }
-
+    if (!indexCursorOptions(m_currentTransaction.get(), databaseId, objectStoreId, indexId, range, direction, cursorOptions))
+        return 0;
     RefPtr<IndexCursorImpl> cursor = IndexCursorImpl::create(m_currentTransaction.get(), cursorOptions);
     if (!cursor->firstSeek())
         return 0;
