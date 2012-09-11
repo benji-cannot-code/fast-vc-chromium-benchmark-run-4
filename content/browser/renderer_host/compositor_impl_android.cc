b55de425f5171426701ef2c6d8a3a596835d5414
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 19:22:45 +0000

    Use WebCompositorSupport instead of static functions in compositor_impl_android.cc
    The static functions will go away real soon now in favor of an injectable
    WebCompositorSupport on WebKitPlatformSupport. This prepares for the change.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10917181
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156095 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -12,7 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/client/gpu_channel_host.h"
 #include "content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h"
 #include "content/common/gpu/gpu_process_launch_causes.h"
-#include "third_party/WebKit/Source/Platform/chromium/public/WebCompositor.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/Platform.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebCompositorSupport.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebCompositorOutputSurface.h"
 
 namespace {
@@ -70,11 +71,12 @@ Compositor* Compositor::Create() {
 
 // static
 void Compositor::Initialize() {
-  WebKit::WebCompositor::initialize(NULL);
+  WebKit::Platform::current()->compositorSupport()->initialize(NULL);
 }
 
-CompositorImpl::CompositorImpl()
-    : root_layer_(WebKit::WebLayer::create()) {
+CompositorImpl::CompositorImpl() {
+  root_layer_.reset(
+      WebKit::Platform::current()->compositorSupport()->createLayer());
 }
 
 CompositorImpl::~CompositorImpl() {
@@ -97,7 +99,10 @@ void CompositorImpl::SetWindowSurface(ANativeWindow* window) {
     context_.reset(GraphicsContext::CreateForUI(window));
     WebKit::WebLayerTreeView::Settings settings;
     settings.refreshRate = 60.0;
-    host_.reset(WebKit::WebLayerTreeView::create(this, *root_layer_, settings));
+    WebKit::WebCompositorSupport* compositor_support =
+        WebKit::Platform::current()->compositorSupport();
+    host_.reset(
+        compositor_support->createLayerTreeView(this, *root_layer_, settings));
     host_->setVisible(true);
     host_->setSurfaceReady();
   } else {
