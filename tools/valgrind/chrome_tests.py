9e58d04dcbc857652b8f13413edc4f2f3fbfe96c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 21:48:52 +0000

    Suppress leaks/uninitialized found in compositor_unittests Add compositor_unittests to chrome_test.sh
    BUG=148477,148475,124658
    TEST=valgrind/heapchecker reports no errors
    
    Review URL: https://chromiumcodereview.appspot.com/10911224
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156131 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -234,6 +234,9 @@ class ChromeTests:
   def TestBase(self):
     return self.SimpleTest("base", "base_unittests")
 
+  def TestCompositor(self):
+    return self.SimpleTest("compositor", "compositor_unittests")
+
   def TestContent(self):
     return self.SimpleTest("content", "content_unittests")
 
@@ -476,6 +479,7 @@ class ChromeTests:
     "automated_ui" : TestAutomatedUI,
     "base": TestBase,            "base_unittests": TestBase,
     "browser": TestBrowser,      "browser_tests": TestBrowser,
+    "compositor": TestCompositor,"compositor_unittests": TestCompositor,
     "content": TestContent,      "content_unittests": TestContent,
     "content_browsertests": TestContentBrowser,
     "courgette": TestCourgette,  "courgette_unittests": TestCourgette,
