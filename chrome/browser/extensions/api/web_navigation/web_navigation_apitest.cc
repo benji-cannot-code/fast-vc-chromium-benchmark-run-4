e171a42aca2906ebd7c4eb9fe0ab5889d7230c5f
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 12:09:19 +0000

    Include the route id in the ResourceRequestInfo object
    It's not enough to look at the child id to tell RenderViewHosts apart
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/10910170
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@155992 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_apitest.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/browser_thread.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/resource_controller.h"
 #include "content/public/browser/resource_dispatcher_host.h"
@@ -396,6 +397,36 @@ IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ServerRedirect) {
   ASSERT_TRUE(
       RunExtensionSubtest("webnavigation", "test_serverRedirect.html"))
           << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ServerRedirectSingleProcess) {
+  // Set max renderers to 1 to force running out of processes.
+  content::RenderProcessHost::SetMaxRendererProcessCount(1);
+
+  // Wait for the extension to set itself up and return control to us.
+  ASSERT_TRUE(RunExtensionSubtest(
+      "webnavigation", "test_serverRedirectSingleProcess.html"))
+          << message_;
+
+  WebContents* tab = chrome::GetActiveWebContents(browser());
+  content::WaitForLoadStop(tab);
+
+  ResultCatcher catcher;
+  GURL url(base::StringPrintf(
+      "http://www.a.com:%d/"
+          "files/extensions/api_test/webnavigation/serverRedirect/a.html",
+      test_server()->host_port_pair().port()));
+
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  url = GURL(base::StringPrintf(
+      "http://www.b.com:%d/server-redirect?http://www.b.com:%d/",
+      test_server()->host_port_pair().port(),
+      test_server()->host_port_pair().port()));
+
+  ui_test_utils::NavigateToURL(browser(), url);
+
+  ASSERT_TRUE(catcher.GetNextResult()) << catcher.message();
 }
 
 IN_PROC_BROWSER_TEST_F(WebNavigationApiTest, ForwardBack) {
