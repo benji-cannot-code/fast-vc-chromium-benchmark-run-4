ca3f61dbb9f478df3e3ecb61a0249424047b30ad
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 19:22:55 +0000

    Get mouse events to work with win aura, and make the combobox function in the views examples app.
    http://crbug.com/146077
    R=sky@chromium.org
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=155832
    Review URL: https://chromiumcodereview.appspot.com/10910144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156096 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_capture_client.h
+++ ui/views/widget/desktop_capture_client.h
@@ -9,10 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "ui/aura/client/capture_client.h"
+#include "ui/views/views_export.h"
 
 namespace views {
 
-class DesktopCaptureClient : public aura::client::CaptureClient {
+class VIEWS_EXPORT DesktopCaptureClient : public aura::client::CaptureClient {
  public:
   DesktopCaptureClient();
   virtual ~DesktopCaptureClient();
