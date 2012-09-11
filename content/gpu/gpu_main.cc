a923ce73767b4eeba5d3f0b7156221552204b624
skaslev@google.com <skaslev@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 22:36:00 +0000

    Show gpu process sandbox status in the about:gpu page.
    BUG=144663
    R=nduca@chromium.org,jln@chromium.org,piman@chromium.org,estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10915189
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156146 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -159,7 +159,7 @@ int GpuMain(const content::MainFunctionParams& parameters) {
 #endif
 
     if (do_init_sandbox) {
-      content::InitializeSandbox();
+      gpu_info.sandboxed = content::InitializeSandbox();
     }
   }
 #endif
@@ -172,8 +172,10 @@ int GpuMain(const content::MainFunctionParams& parameters) {
     // content.
     sandbox::TargetServices* target_services =
         parameters.sandbox_info->target_services;
-    if (target_services)
+    if (target_services) {
       target_services->LowerToken();
+      gpu_info.sandboxed = true;
+    }
   }
 #endif
 
