03588605cb61640829a61b94a819ded700ce42f6
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-11 17:50:34 +0000

    Improve the tests for GpuDataManagerImpl.
    Before we test with calling a bunch of private APIs.  Now we test with calling the public APIs, and the tests are as close to production code path as we can get.
    
    Also, move some logic to the production only code path, so it won't interfere with testing.
    
    BUG=
    TEST=content_unittests
    Review URL: https://chromiumcodereview.appspot.com/10911192
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156051 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl.h
+++ content/browser/gpu/gpu_data_manager_impl.h
@@ -92,6 +92,7 @@ class CONTENT_EXPORT GpuDataManagerImpl
   friend struct DefaultSingletonTraits<GpuDataManagerImpl>;
 
   FRIEND_TEST_ALL_PREFIXES(GpuDataManagerImplTest, GpuSideBlacklisting);
+  FRIEND_TEST_ALL_PREFIXES(GpuDataManagerImplTest, GpuSideExceptions);
   FRIEND_TEST_ALL_PREFIXES(GpuDataManagerImplTest, BlacklistCard);
   FRIEND_TEST_ALL_PREFIXES(GpuDataManagerImplTest, GpuInfoUpdate);
   FRIEND_TEST_ALL_PREFIXES(GpuDataManagerImplTest,
@@ -137,6 +138,10 @@ class CONTENT_EXPORT GpuDataManagerImpl
   // Current card force-blacklisted due to GPU crashes, or disabled through
   // the --disable-gpu commandline switch.
   bool card_blacklisted_;
+
+  // We disable histogram stuff in testing, especially in unit tests because
+  // they cause random failures.
+  bool update_histograms_;
 
   DISALLOW_COPY_AND_ASSIGN(GpuDataManagerImpl);
 };
