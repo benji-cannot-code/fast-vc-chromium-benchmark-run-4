712630aea0e70cc1633e676948fdc20e64fcc521
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 18:54:14 +0000

    Fix the getMessage example.
    The Spanish translation was in Latin-1 instead of UTF-8, so it didn't
    load, and the init() function was never called, so the popup stayed
    blank forever.
    
    Review URL: https://chromiumcodereview.appspot.com/10918185
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/examples/api/i18n/getMessage/popup.js
+++ chrome/common/extensions/docs/examples/api/i18n/getMessage/popup.js
@@ -22,4 +22,5 @@ function getAcceptLanguages() {
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#accept_lang').addEventListener(
       'click', getAcceptLanguages);
+  init();
 });
