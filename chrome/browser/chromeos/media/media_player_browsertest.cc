3ed61c5d1f4c694ad31e8d714b9fc68546ce4928
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 11:57:46 +0000

    Fixed MediaPlayerBrowserTest
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/10915229
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/media/media_player_browsertest.cc
+++ chrome/browser/chromeos/media/media_player_browsertest.cc
@@ -34,7 +34,7 @@ class MediaPlayerBrowserTest : public InProcessBrowserTest {
   }
 
   bool IsPlayerVisible() {
-    return IsBrowserVisible(MediaPlayer::GetInstance()->mediaplayer_browser_);
+    return IsBrowserVisible(MediaPlayer::GetInstance()->GetBrowser());
   }
 };
 
