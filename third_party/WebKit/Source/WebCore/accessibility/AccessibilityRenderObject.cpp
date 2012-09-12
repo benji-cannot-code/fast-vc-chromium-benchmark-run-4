0755aa7a8a9bb3262c078642291a4d6b4699d214
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 22:56:47 +0000

    AX: svg:image not accessible https://bugs.webkit.org/show_bug.cgi?id=96341
    Reviewed by Adele Peterson.
    
    Source/WebCore: 
    
    Test: accessibility/svg-image.html
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::determineAccessibilityRole):
    
    LayoutTests: 
    
    * accessibility/svg-image.html: Added.
    * platform/mac/accessibility/svg-image-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityRenderObject.cpp
@@ -2357,11 +2357,12 @@ AccessibilityRole AccessibilityRenderObject::determineAccessibilityRole()
         return LegendRole;
     if (m_renderer->isText())
         return StaticTextRole;
-    if (cssBox && cssBox->isImage()) {
+    if ((cssBox && cssBox->isImage()) || m_renderer->isSVGImage()) {
         if (node && node->hasTagName(inputTag))
             return ariaHasPopup() ? PopUpButtonRole : ButtonRole;
         return ImageRole;
     }
+    
     if (node && node->hasTagName(canvasTag))
         return CanvasRole;
 
