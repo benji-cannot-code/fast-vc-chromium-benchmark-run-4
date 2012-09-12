e4f83856045aceda8913e5658d6944b715a19361
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 22:20:34 +0000

    Refactor Opcodes to distinguish between core and extension opcodes. https://bugs.webkit.org/show_bug.cgi?id=96466.
    Patch by Mark Lam <mark.lam@apple.com> on 2012-09-12
    Reviewed by Filip Pizlo.
    
    * bytecode/Opcode.h:
    (JSC): Added FOR_EACH_CORE_OPCODE_ID() macro.
    * llint/LowLevelInterpreter.h:
    (JSC): Auto-generate llint opcode aliases using the
        FOR_EACH_CORE_OPCODE_ID() macro.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/Opcode.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/Opcode.h
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-    #define FOR_EACH_OPCODE_ID(macro) \
+    #define FOR_EACH_CORE_OPCODE_ID_WITH_EXTENSION(macro, extension__) \
         macro(op_enter, 1) \
         macro(op_create_activation, 2) \
         macro(op_init_lazy_reg, 2) \
@@ -201,9 +201,19 @@ namespace JSC {
         macro(op_profile_will_call, 2) \
         macro(op_profile_did_call, 2) \
         \
-        FOR_EACH_LLINT_OPCODE_EXTENSION(macro) \
+        extension__ \
         \
         macro(op_end, 2) // end must be the last opcode in the list
+
+    #define FOR_EACH_CORE_OPCODE_ID(macro) \
+        FOR_EACH_CORE_OPCODE_ID_WITH_EXTENSION(macro, /* No extension */ )
+
+    #define FOR_EACH_OPCODE_ID(macro) \
+        FOR_EACH_CORE_OPCODE_ID_WITH_EXTENSION( \
+            macro, \
+            FOR_EACH_LLINT_OPCODE_EXTENSION(macro) \
+        )
+
 
     #define OPCODE_ID_ENUM(opcode, length) opcode,
         typedef enum { FOR_EACH_OPCODE_ID(OPCODE_ID_ENUM) } OpcodeID;
