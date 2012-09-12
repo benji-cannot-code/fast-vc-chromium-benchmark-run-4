36776dab20aebf5f0a4e4770f60cf17655d78553
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 10:31:51 +0000

    Add support for new accessibility roles from WebKit, with tests.
    These roles worked before, but the code is now cleaner because
    they directly correspond to a WebKit role, rather than requiring
    matching on the tag name:
    div, form, hr, label, p, presentational, spinbuttonpart
    
    These roles bring new support for roles that were not previously
    accessible:
    canvas, spinbutton, togglebutton.
    
    All of the interesting cases have new tests (content_browsertest target).
    
    BUG=145346,143689
    
    
    Review URL: https://chromiumcodereview.appspot.com/10919157
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156272 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_win.cc
+++ content/browser/accessibility/browser_accessibility_win.cc
@@ -3147,6 +3147,13 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       if (is_mixed)
         ia_state_ |= STATE_SYSTEM_MIXED;
       break;
+    case AccessibilityNodeData::ROLE_CANVAS:
+      ia_role_ = ROLE_SYSTEM_GRAPHIC;
+      break;
+    case AccessibilityNodeData::ROLE_CANVAS_WITH_FALLBACK_CONTENT:
+      role_name_ = L"canvas";
+      ia2_role_ = IA2_ROLE_CANVAS;
+      break;
     case AccessibilityNodeData::ROLE_CELL:
       ia_role_ = ROLE_SYSTEM_CELL;
       break;
@@ -3167,6 +3174,10 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       break;
     case AccessibilityNodeData::ROLE_COMBO_BOX:
       ia_role_ = ROLE_SYSTEM_COMBOBOX;
+      break;
+    case AccessibilityNodeData::ROLE_DIV:
+      role_name_ = L"div";
+      ia2_role_ = IA2_ROLE_SECTION;
       break;
     case AccessibilityNodeData::ROLE_DEFINITION_LIST_DEFINITION:
       role_name_ = html_tag;
@@ -3197,6 +3208,10 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       ia2_state_ |= IA2_STATE_SINGLE_LINE;
       ia2_state_ |= IA2_STATE_EDITABLE;
       break;
+    case AccessibilityNodeData::ROLE_FORM:
+      role_name_ = L"form";
+      ia2_role_ = IA2_ROLE_FORM;
+      break;
     case AccessibilityNodeData::ROLE_FOOTER:
       ia_role_ = IA2_ROLE_FOOTER;
       ia_state_ |= STATE_SYSTEM_READONLY;
@@ -3212,12 +3227,6 @@ void BrowserAccessibilityWin::InitRoleAndState() {
         ia_role_ = ROLE_SYSTEM_GROUPING;
       } else if (html_tag == L"li") {
         ia_role_ = ROLE_SYSTEM_LISTITEM;
-      } else if (html_tag == L"form") {
-        role_name_ = html_tag;
-        ia2_role_ = IA2_ROLE_FORM;
-      } else if (html_tag == L"p") {
-        role_name_ = html_tag;
-        ia2_role_ = IA2_ROLE_PARAGRAPH;
       } else {
         if (html_tag.empty())
           role_name_ = L"div";
@@ -3237,6 +3246,9 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       ia2_role_ = IA2_ROLE_HEADING;
       ia_state_ |= STATE_SYSTEM_READONLY;
       break;
+    case AccessibilityNodeData::ROLE_HORIZONTAL_RULE:
+      ia_role_ = ROLE_SYSTEM_SEPARATOR;
+      break;
     case AccessibilityNodeData::ROLE_IMAGE:
       ia_role_ = ROLE_SYSTEM_GRAPHIC;
       ia_state_ |= STATE_SYSTEM_READONLY;
@@ -3250,6 +3262,10 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       ia_role_ = ROLE_SYSTEM_LINK;
       ia_state_ |= STATE_SYSTEM_LINKED;
       ia_state_ |= STATE_SYSTEM_READONLY;
+      break;
+    case AccessibilityNodeData::ROLE_LABEL:
+      ia_role_ = ROLE_SYSTEM_TEXT;
+      ia2_role_ = IA2_ROLE_LABEL;
       break;
     case AccessibilityNodeData::ROLE_LANDMARK_APPLICATION:
     case AccessibilityNodeData::ROLE_LANDMARK_BANNER:
@@ -3324,6 +3340,10 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       ia_role_ = ROLE_SYSTEM_OUTLINE;
       ia_state_ |= STATE_SYSTEM_READONLY;
       break;
+    case AccessibilityNodeData::ROLE_PARAGRAPH:
+      role_name_ = L"P";
+      ia2_role_ = IA2_ROLE_PARAGRAPH;
+      break;
     case AccessibilityNodeData::ROLE_POPUP_BUTTON:
       if (html_tag == L"select") {
         ia_role_ = ROLE_SYSTEM_COMBOBOX;
@@ -3371,6 +3391,12 @@ void BrowserAccessibilityWin::InitRoleAndState() {
     case AccessibilityNodeData::ROLE_SLIDER:
       ia_role_ = ROLE_SYSTEM_SLIDER;
       break;
+    case AccessibilityNodeData::ROLE_SPIN_BUTTON:
+      ia_role_ = ROLE_SYSTEM_SPINBUTTON;
+      break;
+    case AccessibilityNodeData::ROLE_SPIN_BUTTON_PART:
+      ia_role_ = ROLE_SYSTEM_PUSHBUTTON;
+      break;
     case AccessibilityNodeData::ROLE_SPLIT_GROUP:
       ia_role_ = ROLE_SYSTEM_CLIENT;
       ia2_role_ = IA2_ROLE_SPLIT_PANE;
@@ -3409,6 +3435,10 @@ void BrowserAccessibilityWin::InitRoleAndState() {
       break;
     case AccessibilityNodeData::ROLE_TAB_PANEL:
       ia_role_ = ROLE_SYSTEM_PROPERTYPAGE;
+      break;
+    case AccessibilityNodeData::ROLE_TOGGLE_BUTTON:
+      ia_role_ = ROLE_SYSTEM_PUSHBUTTON;
+      ia2_role_ = IA2_ROLE_TOGGLE_BUTTON;
       break;
     case AccessibilityNodeData::ROLE_TEXTAREA:
       ia_role_ = ROLE_SYSTEM_TEXT;
@@ -3460,6 +3490,7 @@ void BrowserAccessibilityWin::InitRoleAndState() {
     case AccessibilityNodeData::ROLE_LOG:
     case AccessibilityNodeData::ROLE_MARQUEE:
     case AccessibilityNodeData::ROLE_MATTE:
+    case AccessibilityNodeData::ROLE_PRESENTATIONAL:
     case AccessibilityNodeData::ROLE_RULER_MARKER:
     case AccessibilityNodeData::ROLE_SHEET:
     case AccessibilityNodeData::ROLE_SLIDER_THUMB:
