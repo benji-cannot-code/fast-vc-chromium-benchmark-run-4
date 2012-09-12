e9c87ceb0930aa4771172640340b1f24c3119c90
smckay@chromium.org <smckay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 03:35:01 +0000

    Fix bug where one of the "close" methods on picker wasn't being called in ChromeOS.
    BUG=146945
    
    
    Review URL: https://chromiumcodereview.appspot.com/10911163
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156225 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/web_intent_picker_views.cc
+++ chrome/browser/ui/views/web_intent_picker_views.cc
@@ -909,6 +909,7 @@ void WebIntentPickerViews::ButtonPressed(views::Button* sender,
 
 void WebIntentPickerViews::WindowClosing() {
   delegate_->OnClosing();
+  delegate_->OnPickerClosed();
 }
 
 void WebIntentPickerViews::DeleteDelegate() {
