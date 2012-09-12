3bde66893b9017cb7e26f149bcff3710e76a48e7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 16:30:04 +0000

    [CSSRegions]Use RefPtr's instead of weak references on DOMNamedFlowCollection https://bugs.webkit.org/show_bug.cgi?id=95311
    Patch by Andrei Onea <onea@adobe.com> on 2012-09-12
    Reviewed by Andreas Kling.
    
    Source/WebCore:
    
    Currently, DOMNamedFlowCollection holds a collection of raw pointers to WebKitNamedFlow.
    This causes a crash when there is a JS instance of a NamedFlowCollection snapshot taken
    before the NamedFlow is deleted, since the memory previously occupied by the NamedFlow
    can be accessed. Because of this, we need to use RefPtr's for the snapshot, so that such
    dangling references extend the lifetime of the NamedFlow objects.
    
    Test: fast/regions/webkit-named-flow-collection-crash.html
    
    * dom/DOMNamedFlowCollection.cpp:
    (WebCore::DOMNamedFlowCollection::DOMNamedFlowCollection):
    (WebCore::DOMNamedFlowCollection::item):
    (WebCore::DOMNamedFlowCollection::namedItem):
    (WebCore):
    (WebCore::DOMNamedFlowCollection::DOMNamedFlowHashFunctions::hash):
    (WebCore::DOMNamedFlowCollection::DOMNamedFlowHashFunctions::equal):
    (DOMNamedFlowCollection::DOMNamedFlowHashFunctions):
    (WebCore::DOMNamedFlowCollection::DOMNamedFlowHashTranslator::hash):
    (WebCore::DOMNamedFlowCollection::DOMNamedFlowHashTranslator::equal):
    Create new internal ListHashSet for RefPtr<WebKitNamedFlow>.
    * dom/DOMNamedFlowCollection.h:
    (WebCore::DOMNamedFlowCollection::create):
    (DOMNamedFlowCollection):
    * dom/NamedFlowCollection.cpp:
    (WebCore::NamedFlowCollection::createCSSOMSnapshot):
    (WebCore):
    (WebCore::NamedFlowCollection::NamedFlowHashFunctions::hash):
    (WebCore::NamedFlowCollection::NamedFlowHashFunctions::equal):
    (NamedFlowCollection::NamedFlowHashFunctions):
    (WebCore::NamedFlowCollection::NamedFlowHashTranslator::hash):
    (WebCore::NamedFlowCollection::NamedFlowHashTranslator::equal):
    Move back the definitions for NamedFlowHashFunctions and NamedFlowHashTranslator
    to the .cpp file.
    * dom/NamedFlowCollection.h:
    (NamedFlowCollection):
    
    LayoutTests:
    
    Added test for crash which occurs when there is a raw pointer to a NamedFlow
    (in a NamedFlowCollection) which has been freed.
    
    * fast/regions/webkit-named-flow-collection-crash-expected.txt: Added.
    * fast/regions/webkit-named-flow-collection-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DOMNamedFlowCollection.h
+++ third_party/WebKit/Source/WebCore/dom/DOMNamedFlowCollection.h
@@ -31,8 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DOMNamedFlowCollection_h
 
 #include "NamedFlowCollection.h"
+#include <wtf/ListHashSet.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
+#include <wtf/Vector.h>
 
 namespace WebCore {
 
@@ -41,9 +43,9 @@ class WebKitNamedFlow;
 
 class DOMNamedFlowCollection : public RefCounted<DOMNamedFlowCollection> {
 public:
-    static PassRefPtr<DOMNamedFlowCollection> create(NamedFlowCollection::NamedFlowSet& set)
+    static PassRefPtr<DOMNamedFlowCollection> create(const Vector<WebKitNamedFlow*>& namedFlows)
     {
-        return adoptRef(new DOMNamedFlowCollection(set));
+        return adoptRef(new DOMNamedFlowCollection(namedFlows));
     }
 
     unsigned long length() const;
@@ -53,8 +55,12 @@ public:
     bool hasNamedItem(const AtomicString& name) const;
 
 private:
-    explicit DOMNamedFlowCollection(NamedFlowCollection::NamedFlowSet&);
-    NamedFlowCollection::NamedFlowSet m_namedFlows;
+    struct DOMNamedFlowHashFunctions;
+    struct DOMNamedFlowHashTranslator;
+
+    typedef ListHashSet<RefPtr<WebKitNamedFlow>, 1, DOMNamedFlowHashFunctions> DOMNamedFlowSet;
+    explicit DOMNamedFlowCollection(const Vector<WebKitNamedFlow*>&);
+    DOMNamedFlowSet m_namedFlows;
 };
 
 } // namespace WebCore
