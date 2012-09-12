7cc3e1654afe3fcc9c63337014fa402ce023ad2c
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 14:54:15 +0000

    Add UMA reporting for audio hardware parameters.
    Records bits per channel, channel layout, and sample rate.
    
    BUG=147572
    TEST=build/run
    
    Review URL: https://chromiumcodereview.appspot.com/10914203
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156296 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/sample_rates.h
+++ media/audio/sample_rates.h
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 // Enumeration used for histogramming sample rates into distinct buckets.
+// Logged to UMA, so never reuse a value, always add new/greater ones!
 enum AudioSampleRate {
-  // Do not change the order of these values.
   k8000Hz = 0,
   k16000Hz = 1,
   k32000Hz = 2,
@@ -21,6 +21,9 @@ enum AudioSampleRate {
   k11025Hz = 5,
   k22050Hz = 6,
   k44100Hz = 7,
+  k88200Hz = 8,
+  k176400Hz = 9,
+  k192000Hz = 10,
   kUnexpectedAudioSampleRate  // Must always be last!
 };
 
