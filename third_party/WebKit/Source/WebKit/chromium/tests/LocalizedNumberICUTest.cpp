5b6044ef4f9fd6b1612f1aa50dc404394bd2316b
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 02:24:57 +0000

    Create Localizer factory method for LocaleICU https://bugs.webkit.org/show_bug.cgi?id=96363
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    Preparing to use Localizer instead of LocaleMac/Win/ICU
    
    No new tests. Covered in unit test tests/LocalizedNumberICUTest.cpp
    
    * platform/text/LocaleICU.cpp:
    (WebCore::Localizer::create):
    (WebCore):
    
    Source/WebKit/chromium:
    
    * tests/LocalizedNumberICUTest.cpp:
    (testNumberIsReversible):
    (testDecimalSeparator):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128257 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/LocalizedNumberICUTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/LocalizedNumberICUTest.cpp
@@ -37,13 +37,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using namespace WebCore;
 
-void testNumberIsReversible(const char* localeString, const char* original, const char* shouldHave = 0)
+void testNumberIsReversible(const AtomicString& locale, const char* original, const char* shouldHave = 0)
 {
-    OwnPtr<LocaleICU> locale = LocaleICU::create(localeString);
-    String localized = locale->convertToLocalizedNumber(original);
+    OwnPtr<Localizer> localizer = Localizer::create(locale);
+    String localized = localizer->convertToLocalizedNumber(original);
     if (shouldHave)
         EXPECT_TRUE(localized.contains(shouldHave));
-    String converted = locale->convertFromLocalizedNumber(localized);
+    String converted = localizer->convertFromLocalizedNumber(localized);
     EXPECT_EQ(original, converted);
 }
 
@@ -83,10 +83,10 @@ TEST(LocalizedNumberICUTest, Reversible)
 
 #if ENABLE(INPUT_TYPE_TIME_MULTIPLE_FIELDS)
 
-static String testDecimalSeparator(const char* localeString)
+static String testDecimalSeparator(const AtomicString& locale)
 {
-    OwnPtr<LocaleICU> locale = LocaleICU::create(localeString);
-    return locale->localizedDecimalSeparator();
+    OwnPtr<Localizer> localizer = Localizer::create(locale);
+    return localizer->localizedDecimalSeparator();
 }
 
 TEST(LocalizedNumberICUTest, localizedDecimalSeparator)
