b91c856e63e7855afedccfb57a79dae06a6c725f
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 18:45:56 +0000

    Implemented print-to-skpicture using chrome-remote-control. Review URL: https://chromiumcodereview.appspot.com/10915017
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156340 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gpu/gpu_tools/multi_page_benchmark.py
+++ tools/gpu/gpu_tools/multi_page_benchmark.py
@@ -215,6 +215,10 @@ class MultiPageBenchmarkUnitTest(unittest.TestCase):
     ps.pages.append(page)
     return ps
 
+  def CustomizeOptionsForTest(self, options):
+    """Override to customize default options."""
+    pass
+
   def RunBenchmark(self, benchmark, ps):
     """Runs a benchmark against a pageset, returning the rows its outputs."""
     rows = []
@@ -234,6 +238,7 @@ class MultiPageBenchmarkUnitTest(unittest.TestCase):
       setattr(options, k, v)
 
     benchmark.CustomizeBrowserOptions(options)
+    self.CustomizeOptionsForTest(options)
     possible_browser = chrome_remote_control.FindBrowser(options)
     with possible_browser.Create() as browser:
       with browser.CreateTemporaryHTTPServer(self.unittest_data_dir) as server:
