bc8bf4ad02575638416186ee7e74313212709c3c
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 07:23:08 +0000

    Use the new favicon resources below common/, delete the old assets
    Consolidate to the new favicon_foo naming scheme preferred by ux.
    
    BUG=none
    TBR=oshima
    
    Review URL: https://chromiumcodereview.appspot.com/10909187
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156252 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager_worker_resource_provider.cc
+++ chrome/browser/task_manager/task_manager_worker_resource_provider.cc
@@ -111,7 +111,7 @@ string16 TaskManagerSharedWorkerResource::GetProfileName() const {
 gfx::ImageSkia TaskManagerSharedWorkerResource::GetIcon() const {
   if (!default_icon_) {
     ResourceBundle& rb = ResourceBundle::GetSharedInstance();
-    default_icon_ = rb.GetImageSkiaNamed(IDR_PLUGIN);
+    default_icon_ = rb.GetImageSkiaNamed(IDR_PLUGINS_FAVICON);
     // TODO(jabdelmalek): use different icon for web workers.
   }
   return *default_icon_;
