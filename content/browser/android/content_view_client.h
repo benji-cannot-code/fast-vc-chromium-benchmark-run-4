fcb1ff0cbd198b9ed341c2f8e142e129c16de1f7
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 20:18:44 +0000

    Remove ContentViewClient::OnInternalPageLoadRequest
    Method is empty both up and downstream.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10928118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156363 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_view_client.h
+++ content/browser/android/content_view_client.h
@@ -73,8 +73,6 @@ class ContentViewClient {
                                                           jobject obj);
 
   // Called by ContentView:
-  void OnInternalPageLoadRequest(WebContents* source,
-                                 const GURL& url);
   void OnPageStarted(const GURL& url);
   void OnPageFinished(const GURL& url);
   void OnLoadStarted();
