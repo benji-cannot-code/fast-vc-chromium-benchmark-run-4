642e9420ecf69275294822eafb2252cc91a94e21
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 16:16:14 +0000

    [Shadow DOM] Unpolished elements should reject author shadows https://bugs.webkit.org/show_bug.cgi?id=96404
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Gave areAuthorShadowsAllowed() overrides for these replaced elements
    which aren't author shadow ready.
    
    No new tests. Covered by existing tests.
    
    * html/HTMLCanvasElement.h: Did areAuthorShadowsAllowed() overrride.
    * html/HTMLFieldSetElement.h: Did areAuthorShadowsAllowed() overrride.
    * html/HTMLFrameElementBase.h: Did areAuthorShadowsAllowed() overrride.
    * html/HTMLMediaElement.h: Did areAuthorShadowsAllowed() overrride.
    * html/HTMLPlugInElement.h: Did areAuthorShadowsAllowed() overrride.
    * html/HTMLSelectElement.h: Did areAuthorShadowsAllowed() overrride.
    
    LayoutTests:
    
    - Added setAuthorShadowDOMForAnyElementEnabled() calls to make tests works.
    - UPdated shadow-disable.html to capture the disabled elements.
    
    * fast/dom/shadow/shadow-disable-expected.txt:
    * fast/dom/shadow/shadow-disable.html:
    * fast/dom/shadow/shadowdom-for-fieldset-without-shadow.html:
    * fast/dom/shadow/shadowdom-for-form-associated-element-useragent.html:
    * fast/dom/shadow/shadowdom-for-media.html:
    * platform/chromium/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLPlugInElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLPlugInElement.h
@@ -63,6 +63,7 @@ protected:
     virtual void detach();
     virtual bool isPresentationAttribute(const QualifiedName&) const OVERRIDE;
     virtual void collectStyleForAttribute(const Attribute&, StylePropertySet*) OVERRIDE;
+    virtual bool areAuthorShadowsAllowed() const OVERRIDE { return false; }
 
     bool m_inBeforeLoadEventHandler;
     // Subclasses should use guardedDispatchBeforeLoadEvent instead of calling dispatchBeforeLoadEvent directly.
