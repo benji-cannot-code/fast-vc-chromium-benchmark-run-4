7b652cd230e8243e1eb0a13fef9263366bdbba3a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 16:38:16 +0000

    [WK2][WKTR] TestRunner needs to implement dumpApplicationCacheDelegateCallbacks https://bugs.webkit.org/show_bug.cgi?id=96374
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-09-12
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebKit2:
    
    Add Bundle C API to reset the application cache quota
    for a given origin.
    
    Add new reachedApplicationCacheOriginQuota callback
    to WKBundlePageUIClient which is called from
    WebChromeClient::reachedApplicationCacheOriginQuota().
    
    Those are needed by WebKitTestRunner to dump
    information about the application cache callbacks
    if instructed to.
    
    * Shared/APIClientTraits.cpp:
    (WebKit):
    * Shared/APIClientTraits.h:
    * WebProcess/InjectedBundle/API/c/WKBundle.cpp:
    (WKBundleSetApplicationCacheOriginQuota):
    (WKBundleResetApplicationCacheOriginQuota):
    * WebProcess/InjectedBundle/API/c/WKBundlePage.h:
    * WebProcess/InjectedBundle/API/c/WKBundlePrivate.h:
    * WebProcess/InjectedBundle/InjectedBundle.cpp:
    (WebKit::InjectedBundle::resetApplicationCacheOriginQuota):
    (WebKit):
    * WebProcess/InjectedBundle/InjectedBundle.h:
    (InjectedBundle):
    * WebProcess/InjectedBundle/InjectedBundlePageUIClient.cpp:
    (WebKit::InjectedBundlePageUIClient::didReachApplicationCacheOriginQuota):
    (WebKit):
    * WebProcess/InjectedBundle/InjectedBundlePageUIClient.h:
    (WebKit):
    (InjectedBundlePageUIClient):
    * WebProcess/WebCoreSupport/WebChromeClient.cpp:
    (WebKit::WebChromeClient::reachedApplicationCacheOriginQuota):
    * win/WebKit2.def:
    * win/WebKit2CFLite.def:
    
    Tools:
    
    Implement support for dumpApplicationCacheDelegateCallbacks
    and disallowIncreaseForApplicationCacheQuota in
    WebKitTestRunner and properly dump the information
    expected by the tests.
    
    If the application cache quota is reached for a given
    security origin, WebKitTestRunner will reset the quota
    to its default value, unless intructed not to via
    disallowIncreaseForApplicationCacheQuota().
    
    * WebKitTestRunner/InjectedBundle/Bindings/TestRunner.idl:
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    (WTR::InjectedBundlePage::InjectedBundlePage):
    (WTR::InjectedBundlePage::didReachApplicationCacheOriginQuota):
    (WTR):
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.h:
    (InjectedBundlePage):
    * WebKitTestRunner/InjectedBundle/TestRunner.cpp:
    (WTR::TestRunner::TestRunner):
    (WTR::TestRunner::disallowIncreaseForApplicationCacheQuota):
    (WTR):
    * WebKitTestRunner/InjectedBundle/TestRunner.h:
    (WTR::TestRunner::dumpApplicationCacheDelegateCallbacks):
    (TestRunner):
    (WTR::TestRunner::shouldDisallowIncreaseForApplicationCacheQuota):
    (WTR::TestRunner::shouldDumpApplicationCacheDelegateCallbacks):
    
    LayoutTests:
    
    Unskip test cases that are passing now that WebKitTestRunner
    implements dumpApplicationCacheDelegateCallbacks and
    disallowIncreaseForApplicationCacheQuota.
    
    * platform/efl-wk1/TestExpectations:
    * platform/efl/Skipped:
    * platform/wk2/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/InjectedBundle.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/InjectedBundle.h
@@ -151,6 +151,7 @@ public:
     void setAppCacheMaximumSize(uint64_t);
     uint64_t appCacheUsageForOrigin(const String& origin);
     void setApplicationCacheOriginQuota(const String& origin, uint64_t);
+    void resetApplicationCacheOriginQuota(const String& origin);
 
     // Garbage collection API
     void garbageCollectJavaScriptObjects();
