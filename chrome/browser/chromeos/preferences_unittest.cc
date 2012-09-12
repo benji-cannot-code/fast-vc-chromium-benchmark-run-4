c87fc556f2a6a82f277e642f8c220805e2b177cd
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 02:07:03 +0000

    UMA stats to track drive as default download destination.
    BUG=142051
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10880061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences_unittest.cc
+++ chrome/browser/chromeos/preferences_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/api/prefs/pref_member.h"
 #include "chrome/browser/chromeos/input_method/mock_input_method_manager.h"
+#include "chrome/browser/download/download_prefs.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_pref_service.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -56,6 +57,7 @@ class MyMockInputMethodManager : public input_method::MockInputMethodManager {
 TEST(PreferencesTest, TestUpdatePrefOnBrowserScreenDetails) {
   TestingPrefService prefs;
   Preferences::RegisterUserPrefs(&prefs);
+  DownloadPrefs::RegisterUserPrefs(&prefs);
 
   StringPrefMember previous;
   previous.Init(prefs::kLanguagePreviousInputMethod, &prefs, NULL);
