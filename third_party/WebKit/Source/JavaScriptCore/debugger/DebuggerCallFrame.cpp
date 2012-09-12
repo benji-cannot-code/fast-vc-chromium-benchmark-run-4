bccc115154f159d9cd4abf54ff07f3bd215901d5
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 06:14:56 +0000

    Don't allocate a backing store just for a function's name https://bugs.webkit.org/show_bug.cgi?id=96468
    Reviewed by Oliver Hunt.
    
    Treat function.name like function.length etc., and use a custom getter.
    This saves space in closures.
    
    * debugger/DebuggerCallFrame.cpp:
    (JSC::DebuggerCallFrame::functionName):
    * debugger/DebuggerCallFrame.h:
    (DebuggerCallFrame): Updated for interface change.
    
    * runtime/Executable.h:
    (JSC::JSFunction::JSFunction): Do a little inlining.
    
    * runtime/JSFunction.cpp:
    (JSC::JSFunction::finishCreation): Gone now. That's the point of the patch.
    
    (JSC::JSFunction::name):
    (JSC::JSFunction::displayName):
    (JSC::JSFunction::nameGetter):
    (JSC::JSFunction::getOwnPropertySlot):
    (JSC::JSFunction::getOwnPropertyDescriptor):
    (JSC::JSFunction::getOwnPropertyNames):
    (JSC::JSFunction::put):
    (JSC::JSFunction::deleteProperty):
    (JSC::JSFunction::defineOwnProperty): Added custom accessors for .name
    just like .length and others.
    
    * runtime/JSFunction.h:
    (JSC::JSFunction::create):
    (JSFunction): Updated for interface changes.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/debugger/DebuggerCallFrame.cpp
+++ third_party/WebKit/Source/JavaScriptCore/debugger/DebuggerCallFrame.cpp
@@ -37,18 +37,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace JSC {
 
-const String* DebuggerCallFrame::functionName() const
+String DebuggerCallFrame::functionName() const
 {
     if (!m_callFrame->codeBlock())
-        return 0;
+        return String();
 
     if (!m_callFrame->callee())
-        return 0;
+        return String();
 
     JSObject* function = m_callFrame->callee();
     if (!function || !function->inherits(&JSFunction::s_info))
-        return 0;
-    return &jsCast<JSFunction*>(function)->name(m_callFrame);
+        return String();
+    return jsCast<JSFunction*>(function)->name(m_callFrame);
 }
     
 String DebuggerCallFrame::calculatedFunctionName() const
--- third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HandlerInfo.h"
 #include "JSFunction.h"
 #include "Interpreter.h"
+#include "JSGlobalObject.h"
 #include "LLIntCLoop.h"
 #include "Nodes.h"
 #include "SamplingTool.h"
@@ -754,6 +755,13 @@ namespace JSC {
         WriteBarrier<JSString> m_nameValue;
         WriteBarrier<SharedSymbolTable> m_symbolTable;
     };
+
+    inline JSFunction::JSFunction(JSGlobalData& globalData, FunctionExecutable* executable, JSScope* scope)
+        : Base(globalData, scope->globalObject()->functionStructure())
+        , m_executable(globalData, this, executable)
+        , m_scope(globalData, this, scope)
+    {
+    }
 
     inline FunctionExecutable* JSFunction::jsExecutable() const
     {
