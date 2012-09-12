349e15033e0dfba882a397b1b91486fbaaadf6be
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 01:36:23 +0000

    win aura: fixes for non-chrome binaries
    1. Force compositing and threaded-compositing always-on for aura
    2. Avoid some bad swap-chain interaction with hackage in
    HWNDMessageHandler::GetClientAreaInsets() which otherwise causes the
    accelerated surface never to present.
    
    BUG=
    TBR=jam@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10933031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156203 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/compositor_util.cc
+++ content/common/compositor_util.cc
@@ -13,6 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace content {
 
 bool IsThreadedCompositingEnabled() {
+#if defined(OS_WIN) && defined(USE_AURA)
+  // We always want compositing on Aura Windows.
+  return true;
+#endif
+
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(switches::kEnableThreadedCompositing) &&
       !command_line.HasSwitch(switches::kDisableThreadedCompositing))
@@ -26,6 +31,11 @@ bool IsThreadedCompositingEnabled() {
 }
 
 bool IsForceCompositingModeEnabled() {
+#if defined(OS_WIN) && defined(USE_AURA)
+  // We always want compositing on Aura Windows.
+  return true;
+#endif
+
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   if (command_line.HasSwitch(switches::kForceCompositingMode) &&
       !command_line.HasSwitch(switches::kDisableForceCompositingMode))
--- ui/surface/accelerated_surface_win.cc
+++ ui/surface/accelerated_surface_win.cc
@@ -708,6 +708,7 @@ void AcceleratedPresenter::DoPresentAndAcknowledge(
     return;
   }
 
+#if !defined(USE_AURA)
   // If the window is a different size than the swap chain that is being
   // presented then drop the frame.
   RECT window_rect;
@@ -721,6 +722,7 @@ void AcceleratedPresenter::DoPresentAndAcknowledge(
                  "windowheight", window_rect.bottom);
     return;
   }
+#endif
 
   // Round up size so the swap chain is not continuously resized with the
   // surface, which could lead to memory fragmentation.
