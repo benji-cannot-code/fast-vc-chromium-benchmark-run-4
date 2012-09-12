0485fa8ef1dff313be23378a7a267c5acc1721f1
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-12 22:28:39 +0000

    [Chromoting] Refactoring DesktopEnvironment and moving screen/audio recorders to ClientSession.
    This CL changes the way screen/audio recorders and event executors are managed. New DesktopEnvironmentFactory class is now used by ChromotingHost's owner to specify the kind of desktop environment (or virtual terminal) to be used by the host. Screen/audio recorders and event executors now owned by the ClientSession instance, so there is a separate set of recorders and stubs exists for each authenticated client session. Clients sessions can now be torn dowsn in parallel with the host shuttting down.
    
    This is the 4th attempt to land this change. This version includes:
    - |ClientSession| objects are torn down asynchronously now.
    - |ClientSession| objects are ref-counted to facilitate the asynchronous shutdown. They still have to be used and destroyed on the network thread.
    - |ChromotingHost| now waits until all connections are torn down before deleting the session manager.
    - The unit tests were fixed to run message loops until all asynchronous object have been destroyed.
    - |ClientSessionTest.ClampMouseEvents| makes sure that the expectations are destroyed when leaving TEST_F scope so that ASAN is not getting confused.
    
    BUG=134694
    TEST=remoting_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/10916263
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156398 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/win/session_desktop_environment_factory.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_WIN_SESSION_DESKTOP_ENVIRONMENT_FACTORY_H_
+#define REMOTING_HOST_WIN_SESSION_DESKTOP_ENVIRONMENT_FACTORY_H_
+
+#include "remoting/host/desktop_environment_factory.h"
+
+namespace remoting {
+
+class SessionDesktopEnvironmentFactory : public DesktopEnvironmentFactory {
+ public:
+  SessionDesktopEnvironmentFactory();
+  virtual ~SessionDesktopEnvironmentFactory();
+
+  virtual scoped_ptr<DesktopEnvironment> Create(
+      ChromotingHostContext* context) OVERRIDE;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SessionDesktopEnvironmentFactory);
+};
+
+}  // namespace remoting
+
+#endif  // REMOTING_HOST_WIN_SESSION_DESKTOP_ENVIRONMENT_FACTORY_H_
