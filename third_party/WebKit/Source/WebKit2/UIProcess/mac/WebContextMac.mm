e6c5e321229fa9a5d965376d34691cb566247bfb
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-12 20:05:36 +0000

    <rdar://problem/12275537> REGRESSION(r127384): Non-existent directories are no longer created for sandbox paths         https://bugs.webkit.org/show_bug.cgi?id=96442
            Reviewed by Darin Adler.
    
            * Shared/SandboxExtension.h:
            (WebKit::SandboxExtension::createHandleForReadWriteDirectory):
            * Shared/mac/SandboxExtensionMac.mm:
            (WebKit::SandboxExtension::createHandleForReadWriteDirectory):
            Added a function for read-write configuration directories. It matches
            appendReadwriteSandboxDirectory() function behavior from WebProcessMac.mm.
    
            * UIProcess/WebContext.cpp:
            (WebKit::WebContext::createNewWebProcess):
            * UIProcess/mac/WebContextMac.mm:
            (WebKit::WebContext::platformInitializeWebProcess):
            Use the new function for directories that need to be created if they don't exist.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128352 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/mac/WebContextMac.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/mac/WebContextMac.mm
@@ -91,7 +91,7 @@ void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& para
         cachePath = reinterpret_cast<CFStringRef>(NSHomeDirectory());
 
     parameters.nsURLCachePath = [(NSString *)cachePath.get() stringByStandardizingPath];
-    SandboxExtension::createHandle(parameters.nsURLCachePath, SandboxExtension::ReadWrite, parameters.nsURLCachePathExtensionHandle);
+    SandboxExtension::createHandleForReadWriteDirectory(parameters.nsURLCachePath, parameters.nsURLCachePathExtensionHandle);
     ASSERT(!parameters.nsURLCachePath.isEmpty());
 
     NSURLCache *urlCache = [NSURLCache sharedURLCache];
