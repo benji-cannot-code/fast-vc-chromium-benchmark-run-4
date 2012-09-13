9014eb45f7672923c1d6cb61a263df9d348b16fc
jeremycho@chromium.org <jeremycho@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 21:42:26 +0000

    NTP5: Fix page blacklisting and remove recently closed tabs when they're clicked.  Fix the styling of the blacklist notification and pull it out of the tile page.
    BUG=142669
    
    
    Review URL: https://chromiumcodereview.appspot.com/10907065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156641 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_search/tile_page.js
+++ chrome/browser/resources/ntp_search/tile_page.js
@@ -306,25 +306,6 @@ cr.define('ntp', function() {
     },
 
     /**
-     * The notification content of this tile (if any, otherwise null).
-     * @type {!HTMLElement}
-     */
-    get notification() {
-      return this.content_.firstChild.id == 'notification-container' ?
-          this.content_.firstChild : null;
-    },
-    /**
-     * The notification content of this tile (if any, otherwise null).
-     * @type {!HTMLElement}
-     */
-    set notification(node) {
-      assert(node instanceof HTMLElement, '|node| isn\'t an HTMLElement!');
-      // NOTE: Implicitly removes from DOM if |node| is inside it.
-      this.content_.insertBefore(node, this.content_.firstChild);
-      this.positionNotification_();
-    },
-
-    /**
      * Removes the tilePage from the DOM and cleans up event handlers.
      */
     remove: function() {
@@ -420,13 +401,6 @@ cr.define('ntp', function() {
      * @private
      */
     handleCardDeselection_: function(e) {
-    },
-
-    /**
-     * Position the notification if there's one showing.
-     * TODO(pedrosimonetti): Fix the position of the notification.
-     */
-    positionNotification_: function() {
     },
 
     // #########################################################################
