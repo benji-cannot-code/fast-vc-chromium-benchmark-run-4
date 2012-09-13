5661fe9a02fd5a4ceb57cd2329ae03329bd3293b
mhahnenberg@apple.com <mhahnenberg@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-13 20:08:53 +0000

    Rename forEachCell to forEachLiveCell https://bugs.webkit.org/show_bug.cgi?id=96685
    Reviewed by Oliver Hunt.
    
    forEachCell actually only iterates over live cells. We should rename it to 
    reflect what it actually does. This is also helpful because we want to add a new 
    forEachCell that actually does iterate each and every cell in a MarkedBlock 
    regardless of whether or not it is live.
    
    * debugger/Debugger.cpp:
    (JSC::Debugger::recompileAllJSFunctions):
    * heap/Heap.cpp:
    (JSC::Heap::globalObjectCount):
    (JSC::Heap::objectTypeCounts):
    * heap/MarkedBlock.h:
    (MarkedBlock):
    (JSC::MarkedBlock::forEachLiveCell):
    * heap/MarkedSpace.h:
    (MarkedSpace):
    (JSC::MarkedSpace::forEachLiveCell):
    * runtime/JSGlobalData.cpp:
    (JSC::JSGlobalData::releaseExecutableMemory):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
+++ third_party/WebKit/Source/JavaScriptCore/heap/Heap.cpp
@@ -619,7 +619,7 @@ size_t Heap::protectedGlobalObjectCount()
 
 size_t Heap::globalObjectCount()
 {
-    return m_objectSpace.forEachCell<CountIfGlobalObject>();
+    return m_objectSpace.forEachLiveCell<CountIfGlobalObject>();
 }
 
 size_t Heap::protectedObjectCount()
@@ -634,7 +634,7 @@ PassOwnPtr<TypeCountSet> Heap::protectedObjectTypeCounts()
 
 PassOwnPtr<TypeCountSet> Heap::objectTypeCounts()
 {
-    return m_objectSpace.forEachCell<RecordType>();
+    return m_objectSpace.forEachLiveCell<RecordType>();
 }
 
 void Heap::deleteAllCompiledCode()
