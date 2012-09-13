b5ead26327be82a08a5975a57a3a8636bf4111f5
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-13 10:33:36 +0000

    [Qt] Fix build against namespaced Qt
    Patch by Oswald Buddenhagen <oswald.buddenhagen@nokia.com> on 2012-09-13
    Reviewed by Tor Arne Vestbø.
    
    * UIProcess/qt/PageViewportControllerClientQt.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/qt/PageViewportControllerClientQt.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/PageViewportControllerClientQt.h
@@ -32,11 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QVariantAnimation>
 
 QT_BEGIN_NAMESPACE
-class QQuickWebPage;
-class QQuickWebView;
 class QRectF;
 QT_END_NAMESPACE
 
+class QQuickWebPage;
+class QQuickWebView;
 class QWebKitTest;
 
 namespace WebKit {
