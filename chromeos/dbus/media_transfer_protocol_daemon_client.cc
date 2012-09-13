87291adb86015fc8c5d42e7ff6cfabe8f07cf94d
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 23:11:51 +0000

    Modified MediaTransferProtocolDaemonClient response handlers to handle PopArrayOfBytesAsProto failures gracefully.
    Also added checks in power_manager_client.cc.
    
    BUG=144527
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10911286
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156666 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/media_transfer_protocol_daemon_client.cc
+++ chromeos/dbus/media_transfer_protocol_daemon_client.cc
@@ -258,7 +258,11 @@ class MediaTransferProtocolDaemonClientImpl
 
     dbus::MessageReader reader(response);
     MtpStorageInfo protobuf;
-    reader.PopArrayOfBytesAsProto(&protobuf);
+    if (!reader.PopArrayOfBytesAsProto(&protobuf)) {
+      LOG(ERROR) << "Invalid response: " << response->ToString();
+      error_callback.Run();
+      return;
+    }
     callback.Run(protobuf);
   }
 
@@ -305,7 +309,12 @@ class MediaTransferProtocolDaemonClientImpl
     std::vector<MtpFileEntry> file_entries;
     dbus::MessageReader reader(response);
     MtpFileEntries entries_protobuf;
-    reader.PopArrayOfBytesAsProto(&entries_protobuf);
+    if (!reader.PopArrayOfBytesAsProto(&entries_protobuf)) {
+      LOG(ERROR) << "Invalid response: " << response->ToString();
+      error_callback.Run();
+      return;
+    }
+
     for (int i = 0; i < entries_protobuf.file_entries_size(); ++i)
       file_entries.push_back(entries_protobuf.file_entries(i));
     callback.Run(file_entries);
@@ -344,7 +353,11 @@ class MediaTransferProtocolDaemonClientImpl
 
     dbus::MessageReader reader(response);
     MtpFileEntry protobuf;
-    reader.PopArrayOfBytesAsProto(&protobuf);
+    if (!reader.PopArrayOfBytesAsProto(&protobuf)) {
+      LOG(ERROR) << "Invalid response: " << response->ToString();
+      error_callback.Run();
+      return;
+    }
     callback.Run(protobuf);
   }
 
--- chromeos/dbus/power_manager_client.cc
+++ chromeos/dbus/power_manager_client.cc
@@ -238,7 +238,11 @@ class PowerManagerClientImpl : public PowerManagerClient {
     protobuf.set_last_activity_time(last_activity_time.ToInternalValue());
     protobuf.set_is_fullscreen(is_fullscreen);
 
-    writer.AppendProtoAsArrayOfBytes(protobuf);
+    if (!writer.AppendProtoAsArrayOfBytes(protobuf)) {
+      LOG(ERROR) << "Error calling "
+                 << power_manager::kHandleVideoActivityMethod;
+      return;
+    }
     power_manager_proxy_->CallMethod(
         &method_call,
         dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
@@ -262,7 +266,11 @@ class PowerManagerClientImpl : public PowerManagerClient {
     protobuf.set_disable_idle_suspend(overrides & DISABLE_IDLE_SUSPEND);
     protobuf.set_disable_lid_suspend(overrides & DISABLE_IDLE_LID_SUSPEND);
 
-    writer.AppendProtoAsArrayOfBytes(protobuf);
+    if (!writer.AppendProtoAsArrayOfBytes(protobuf)) {
+      LOG(ERROR) << "Error calling "
+                 << power_manager::kStateOverrideRequest;
+      return;
+    }
     power_manager_proxy_->CallMethod(
         &method_call,
         dbus::ObjectProxy::TIMEOUT_USE_DEFAULT,
@@ -384,7 +392,12 @@ class PowerManagerClientImpl : public PowerManagerClient {
 
     dbus::MessageReader reader(response);
     PowerSupplyProperties protobuf;
-    reader.PopArrayOfBytesAsProto(&protobuf);
+    if (!reader.PopArrayOfBytesAsProto(&protobuf)) {
+      LOG(ERROR) << "Error calling "
+                 << power_manager::kGetPowerSupplyPropertiesMethod
+                 << response->ToString();
+      return;
+    }
 
     PowerSupplyStatus status;
     status.line_power_on = protobuf.line_power_on();
