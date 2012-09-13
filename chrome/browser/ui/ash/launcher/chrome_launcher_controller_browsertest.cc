2a9fd8937e04a1c0cdb09561c0f6ce17bb5735e5
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 22:16:52 +0000

    Moved app list back to right
    BUG=148846
    TEST=Visual
    
    Review URL: https://codereview.chromium.org/10919274
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156652 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_browsertest.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_browsertest.cc
@@ -106,7 +106,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, LaunchUnpinned) {
   ShellWindow* window = CreateShellWindow(extension);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item.status);
   CloseShellWindow(window);
@@ -160,7 +161,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, PinRunning) {
   ShellWindow* window = CreateShellWindow(extension);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID id = item.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item.status);
@@ -255,7 +257,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, MultipleWindows) {
   ShellWindow* window1 = CreateShellWindow(extension);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID item_id = item.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item.status);
@@ -290,7 +293,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, MultipleApps) {
   ShellWindow* window1 = CreateShellWindow(extension1);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item1 = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item1 =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID item_id1 = item1.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item1.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item1.status);
@@ -300,7 +304,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, MultipleApps) {
   ShellWindow* window2 = CreateShellWindow(extension2);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item2 = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item2 =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID item_id2 = item2.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item2.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item2.status);
@@ -333,7 +338,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, WindowActivation) {
   ShellWindow* window1 = CreateShellWindow(extension1);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item1 = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item1 =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID item_id1 = item1.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item1.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item1.status);
@@ -343,7 +349,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, WindowActivation) {
   ShellWindow* window2 = CreateShellWindow(extension2);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item2 = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item2 =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID item_id2 = item2.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item2.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item2.status);
@@ -415,7 +422,8 @@ IN_PROC_BROWSER_TEST_F(LauncherPlatformAppBrowserTest, BrowserActivation) {
   CreateShellWindow(extension1);
   ++item_count;
   ASSERT_EQ(item_count, launcher->model()->item_count());
-  ash::LauncherItem item1 = launcher->model()->items()[item_count - 1];
+  ash::LauncherItem item1 =
+      launcher->model()->items()[launcher->model()->item_count() - 2];
   ash::LauncherID item_id1 = item1.id;
   EXPECT_EQ(ash::TYPE_PLATFORM_APP, item1.type);
   EXPECT_EQ(ash::STATUS_ACTIVE, item1.status);
--- chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller_unittest.cc
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using extensions::Extension;
 
 namespace {
-const int kExpectedAppIndex = 2;
+const int kExpectedAppIndex = 1;
 }
 
 class ChromeLauncherControllerTest : public testing::Test {
