a7a5ef6367cb589f7548a88997c47c74c99aac8a
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-13 15:03:24 +0000

    [Qt] Fix build of Qt WTR/MiniBrowser with MSVC and nmake https://bugs.webkit.org/show_bug.cgi?id=96633
    Reviewed by Csaba Osztrogonác.
    
    WTR and MiniBrowser both have DRT in their include path (for the shared font stuff). Batch
    builds with MSVC and nmake result in nmake picking up main.cpp from the wrong directory.
    It's the usual problem and the usual solution is to rename one of the conflicting files.
    This patch chooses to rename DRT instead of WTR and MBR.
    
    Fixed include order at the same time to match style guide.
    
    * DumpRenderTree/qt/DumpRenderTree.pro:
    * DumpRenderTree/qt/DumpRenderTreeMain.cpp: Renamed from Tools/DumpRenderTree/qt/main.cpp.
    (messageHandler):
    (isOption):
    (takeOptionValue):
    (printUsage):
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/DumpRenderTreeMain.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/qt/DumpRenderTreeMain.cpp
@@ -32,30 +32,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "QtInitializeTestFonts.h"
 
-#include <wtf/AlwaysInline.h>
-
-#include <qstringlist.h>
 #include <qapplication.h>
-#include <qurl.h>
-#include <qdir.h>
 #include <qdebug.h>
+#include <qdir.h>
 #include <qfont.h>
-#include <qwebdatabase.h>
+#include <qstringlist.h>
 #include <qtimer.h>
+#include <qurl.h>
+#include <qwebdatabase.h>
 #include <qwindowsstyle.h>
+
+#include <wtf/AlwaysInline.h>
+#include <wtf/ExportMacros.h>
 
 #ifdef Q_WS_X11
 #include <qx11info_x11.h>
 #endif
 
 #ifdef Q_OS_WIN
-#include <io.h>
 #include <fcntl.h>
+#include <io.h>
 #endif
 
 #include <limits.h>
 
-#include <wtf/ExportMacros.h>
 #include <wtf/Assertions.h>
 
 void messageHandler(QtMsgType type, const char *message)
