05a5cb436829bc4ae6360f54f0a3899263b9e003
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 23:17:06 +0000

    Fix bug 148565: Panels [WIN]: When creating a focused panel, the specified input item not focused
    BUG=148565
    TEST=Manual test by creating panel with focus on Windows
    
    
    Review URL: https://chromiumcodereview.appspot.com/10915235
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156670 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_view.cc
+++ chrome/browser/ui/panels/panel_view.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/panels/panel_manager.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host_view.h"
+#include "content/public/browser/web_contents_view.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/path.h"
 #include "ui/gfx/screen.h"
@@ -225,6 +226,12 @@ PanelView::~PanelView() {
 void PanelView::ShowPanel() {
   ShowPanelInactive();
   ActivatePanel();
+
+  // Give web contents view a chance to set focus to the appropriate element
+  // when it is created for the first time.
+  content::WebContents* web_contents = panel_->GetWebContents();
+  if (web_contents)
+    web_contents->GetView()->RestoreFocus();
 }
 
 void PanelView::ShowPanelInactive() {
@@ -600,8 +607,7 @@ void PanelView::OnWidgetActivationChanged(views::Widget* widget, bool active) {
 #if defined(OS_WIN) && !defined(USE_AURA)
   // The panel window is in focus (actually accepting keystrokes) if it is
   // active and belongs to a foreground application.
-  bool focused = active &&
-      GetFrameView()->GetWidget()->GetNativeView() == ::GetForegroundWindow();
+  bool focused = active && widget->GetNativeWindow() == ::GetForegroundWindow();
 #else
   NOTIMPLEMENTED();
   bool focused = active;
