3b96917083b9964311d760c7485cd61e18aced16
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-13 16:37:42 +0000

    Another prospective Qt/Windows build fix: Add missing CString.h include.
    * platform/qt/UserAgentQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128476 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/UserAgentQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/qt/UserAgentQt.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QCoreApplication>
 
+#include <wtf/text/CString.h>
 #include <wtf/text/WTFString.h>
 #if defined Q_OS_WIN32
 #include <SystemInfo.h>
