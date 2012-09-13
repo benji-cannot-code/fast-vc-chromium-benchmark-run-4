6b40828b2d443644ded1b240ac6a21be5f14e76b
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 03:20:26 +0000

    [Android] Upstream ChromeBrowserProvider, the Android port ContentProvider implementation.
    BUG=138755
    TEST=3 new java tests coming as soon as we're ready to build them
    
    
    Review URL: https://chromiumcodereview.appspot.com/10912172
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156488 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/android/provider/blocking_ui_thread_async_request.cc
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/android/provider/blocking_ui_thread_async_request.h"
+
+BlockingUIThreadAsyncRequest::BlockingUIThreadAsyncRequest()
+    : request_completed_(false, false) {
+}
+
+void BlockingUIThreadAsyncRequest::RequestCompleted() {
+  // Currently all our use cases receive their request response in the UI
+  // thread (the same thread that made the request). However this is not
+  // a design constraint and can be changed if ever needed.
+  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
+  request_completed_.Signal();
+}
