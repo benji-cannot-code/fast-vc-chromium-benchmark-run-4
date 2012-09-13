1df1634a567731f02556684ad3cfd1d91d569290
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 23:37:27 +0000

    alternate ntp: clip middle omnibox when resizing browser window
    - when resizing browser window, middle omnibox wasn't clipped; it would float in the root window if the browser window is too small to contain it
    - fix to make LocationBarContainer clip its child LocationBarView
    - LocationBarContainer's bounds is resized to within its parent's, while LocationBarView's width fills it but height stays the same, allowing the former to clip the latter without resizing the latter
    
    BUG=145744
    TEST=verify per bug rpt.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10907038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156675 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_bar_container.cc
+++ chrome/browser/ui/views/location_bar/location_bar_container.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/instant_ui.h"
 #include "ui/base/events/event.h"
 #include "ui/views/background.h"
-#include "ui/views/layout/fill_layout.h"
 
 namespace {
 
@@ -33,7 +32,6 @@ LocationBarContainer::LocationBarContainer(views::View* parent,
     view_parent_->set_background(
         views::Background::CreateSolidBackground(GetBackgroundColor()));
   }
-  SetLayoutManager(new views::FillLayout);
 }
 
 LocationBarContainer::~LocationBarContainer() {
@@ -66,6 +64,14 @@ std::string LocationBarContainer::GetClassName() const {
 
 gfx::Size LocationBarContainer::GetPreferredSize() {
   return location_bar_view_->GetPreferredSize();
+}
+
+void LocationBarContainer::Layout() {
+  // |location_bar_view_| fills the entire width of the container, but retains
+  // its preferred height, so that it's not resized which, if shrunk, will
+  // squish the placeholder text within a smaller vertical space.
+  location_bar_view_->SetBounds(0, 0, bounds().width(),
+      location_bar_view_->GetPreferredSize().height());
 }
 
 bool LocationBarContainer::SkipDefaultKeyEventProcessing(
