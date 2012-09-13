889b6a6c9e3b17ba823521a182cb9f7ebdb715b1
yoli@rim.com <yoli@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-13 20:02:39 +0000

    [BlackBerry] ResourceRequest string optimizations https://bugs.webkit.org/show_bug.cgi?id=96661
    Reviewed by Rob Buis.
    
    RIM PR# 207965.
    Call PlatformRequest's method to add the Accept-Language header.
    
    No new tests as no functional change.
    
    * platform/network/blackberry/ResourceRequestBlackBerry.cpp:
    (WebCore::ResourceRequest::initializePlatformRequest):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/blackberry/ResourceRequestBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/blackberry/ResourceRequestBlackBerry.cpp
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BlobRegistryImpl.h"
 #include "CookieManager.h"
 #include "ReadOnlyLatin1String.h"
-#include <LocaleHandler.h>
 #include <network/NetworkRequest.h>
 #include <wtf/HashMap.h>
 #include <wtf/text/CString.h>
@@ -227,17 +226,8 @@ void ResourceRequest::initializePlatformRequest(NetworkRequest& platformRequest,
                 platformRequest.addHeader("Cookie", cookiePairs.containsOnlyLatin1() ? cookiePairs.latin1().data() : cookiePairs.utf8().data());
         }
 
-        if (!httpHeaderFields().contains("Accept-Language")) {
-            // Locale has the form "en-US". Construct accept language like "en-US, en;q=0.8".
-            std::string locale = BlackBerry::Platform::LocaleHandler::instance()->language();
-            // POSIX locale has '_' instead of '-'.
-            // Replace to conform to HTTP spec.
-            size_t underscore = locale.find('_');
-            if (underscore != std::string::npos)
-                locale.replace(underscore, 1, "-");
-            std::string acceptLanguage = locale + ", " + locale.substr(0, 2) + ";q=0.8";
-            platformRequest.addHeader("Accept-Language", acceptLanguage.c_str());
-        }
+        if (!httpHeaderFields().contains("Accept-Language"))
+            platformRequest.addAcceptLanguageHeader();
     }
 }
 
