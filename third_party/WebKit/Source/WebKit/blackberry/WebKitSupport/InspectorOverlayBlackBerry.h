eb5132c315cf6350037357ad36d1fc78995eb8c9
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-13 20:43:55 +0000

    [BlackBerry] Rename InspectorOverlay in blackberry/WebKitSupport https://bugs.webkit.org/show_bug.cgi?id=96689
    Reviewed by Antonio Gomes.
    
    Rename to InspectorClientBlackBerry to avoid include problems.
    
    * WebCoreSupport/InspectorClientBlackBerry.h:
    * WebKitSupport/InspectorOverlayBlackBerry.cpp: Renamed from Source/WebKit/blackberry/WebKitSupport/InspectorOverlay.cpp.
    (WebKit):
    (BlackBerry::WebKit::InspectorOverlay::create):
    (BlackBerry::WebKit::InspectorOverlay::InspectorOverlay):
    (BlackBerry::WebKit::InspectorOverlay::notifySyncRequired):
    (BlackBerry::WebKit::InspectorOverlay::paintContents):
    (BlackBerry::WebKit::InspectorOverlay::showDebugBorders):
    (BlackBerry::WebKit::InspectorOverlay::showRepaintCounter):
    (BlackBerry::WebKit::InspectorOverlay::~InspectorOverlay):
    (BlackBerry::WebKit::InspectorOverlay::clear):
    (BlackBerry::WebKit::InspectorOverlay::update):
    * WebKitSupport/InspectorOverlayBlackBerry.h: Renamed from Source/WebKit/blackberry/WebKitSupport/InspectorOverlay.h.
    (WebCore):
    (WebKit):
    (InspectorOverlay):
    (InspectorOverlayClient):
    (BlackBerry::WebKit::InspectorOverlay::setClient):
    (BlackBerry::WebKit::InspectorOverlay::notifyAnimationStarted):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128502 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InspectorOverlayBlackBerry.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InspectorOverlayBlackBerry.h
@@ -17,8 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-#ifndef InspectorOverlay_h
-#define InspectorOverlay_h
+#ifndef InspectorOverlayBlackBerry_h
+#define InspectorOverlayBlackBerry_h
 
 #include "WebOverlay.h"
 #include <GraphicsLayerClient.h>
@@ -71,4 +71,4 @@ private:
 } // namespace WebKit
 } // namespace BlackBerry
 
-#endif /* InspectorOverlay_h */
+#endif /* InspectorOverlayBlackBerry_h */
