26e69f3335072ed1e6b4e3f21cc86981f6ba2fd2
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 00:17:04 +0000

    Ensure that windows belonging to a different profile show up when the corresponding user avatar is selected.
    BUG=141712
    R=sky
    Review URL: https://chromiumcodereview.appspot.com/10916274
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156434 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_frame_win.h
+++ chrome/browser/ui/views/frame/browser_frame_win.h
@@ -64,6 +64,7 @@ class BrowserFrameWin : public views::NativeWidgetWin,
   virtual void Close() OVERRIDE;
   virtual void FrameTypeChanged() OVERRIDE;
   virtual void SetFullscreen(bool fullscreen) OVERRIDE;
+  virtual void Activate() OVERRIDE;
 
   // Overridden from NativeBrowserFrame:
   virtual views::NativeWidget* AsNativeWidget() OVERRIDE;
