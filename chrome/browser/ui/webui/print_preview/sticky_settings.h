8816636ff7f3d78bd4a9c4df9eee0eaf6cd1911b
rltoscano@google.com <rltoscano@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-13 00:06:21 +0000

    Simplifies sticky settings code. Now only file name (for save as PDF) and "AppState" are persisted. Persistence happens immediately when values change. AppState is a string generated by the print preview JS that contains data to persist. Also removes the print settings reset experiment flag. Makes use of native layer changes in javascript. Adds AppState object in javascript layer that manages persistance of print preview. Fixes small bug where UMA stats for print preview destination search widget weren't getting recorded.
    BUG=146476,136926,138625
    
    Review URL: https://chromiumcodereview.appspot.com/10909124
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/print_preview/sticky_settings.h
+++ chrome/browser/ui/webui/print_preview/sticky_settings.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 
 class FilePath;
@@ -22,44 +21,20 @@ class DictionaryValue;
 }
 
 namespace printing {
-struct PageSizeMargins;
-}
-
-FORWARD_DECLARE_TEST(PrintPreviewHandlerTest, StickyMarginsCustom);
-FORWARD_DECLARE_TEST(PrintPreviewHandlerTest, StickyMarginsDefault);
-FORWARD_DECLARE_TEST(PrintPreviewHandlerTest, StickyMarginsCustomThenDefault);
-FORWARD_DECLARE_TEST(PrintPreviewHandlerTest, GetLastUsedMarginSettingsCustom);
-FORWARD_DECLARE_TEST(PrintPreviewHandlerTest, GetLastUsedMarginSettingsDefault);
-
-namespace printing {
 
 // Holds all the settings that should be remembered (sticky) in print preview.
 // A sticky setting will be restored next time the user launches print preview.
 // Only one instance of this class is instantiated.
-// TODO(abodenha) Simplify this class.  See http://crbug.com/136926.
 class StickySettings {
  public:
   StickySettings();
   ~StickySettings();
 
-  // Populates |last_used_custom_margins| according to the last used margin
-  // settings.
-  void GetLastUsedMarginSettings(
-      base::DictionaryValue* last_used_custom_margins) const;
-
-  std::string* printer_name();
-  std::string* printer_cloud_print_data();
-  printing::ColorModels color_model() const;
-  bool headers_footers() const;
   FilePath* save_path();
-  printing::DuplexMode duplex_mode() const;
+  std::string* printer_app_state();
 
-  // Stores color model, duplex mode, margins and headers footers.
-  void Store(const base::DictionaryValue& settings);
-  // Stores last used printer name.
-  void StorePrinterName(const std::string& printer_name);
-  // Stores last used printer cloud print data.
-  void StoreCloudPrintData(const std::string& cloud_print_data);
+  // Stores app state for the last used printer.
+  void StoreAppState(const std::string& app_state);
   // Stores the last path the user used to save to pdf.
   void StoreSavePath(const FilePath& path);
 
@@ -67,29 +42,9 @@ class StickySettings {
   void RestoreFromPrefs(PrefService* profile);
   static void RegisterUserPrefs(PrefService* prefs);
  private:
-  void StoreColorModel(const base::DictionaryValue& settings);
-  void StoreMarginSettings(const base::DictionaryValue& settings);
-  void StoreHeadersFooters(const base::DictionaryValue& settings);
-  void StoreDuplexMode(const base::DictionaryValue& settings);
 
   scoped_ptr<FilePath> save_path_;
-  scoped_ptr<std::string> printer_name_;
-  scoped_ptr<std::string> printer_cloud_print_data_;
-  printing::ColorModels color_model_;
-  printing::MarginType margins_type_;
-  scoped_ptr<printing::PageSizeMargins> page_size_margins_;
-  bool headers_footers_;
-  printing::DuplexMode duplex_mode_;
-
-  friend class ::PrintPreviewHandlerTest;
-  FRIEND_TEST_ALL_PREFIXES(::PrintPreviewHandlerTest, StickyMarginsCustom);
-  FRIEND_TEST_ALL_PREFIXES(::PrintPreviewHandlerTest, StickyMarginsDefault);
-  FRIEND_TEST_ALL_PREFIXES(::PrintPreviewHandlerTest,
-                           StickyMarginsCustomThenDefault);
-  FRIEND_TEST_ALL_PREFIXES(::PrintPreviewHandlerTest,
-                           GetLastUsedMarginSettingsCustom);
-  FRIEND_TEST_ALL_PREFIXES(::PrintPreviewHandlerTest,
-                           GetLastUsedMarginSettingsDefault);
+  scoped_ptr<std::string> printer_app_state_;
 };
 
 } // namespace printing
