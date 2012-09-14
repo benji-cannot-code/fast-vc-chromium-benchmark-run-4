4559483af7ceb1c5c59aef7ecb06a4650a7fb6c5
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 22:05:36 +0000

    net: don't run InfiniteCache.DeleteBetween on Android.
    BUG=147383
    TEST=net_unittests
    TBR=gavinp@chromium.org
    Review URL: https://codereview.chromium.org/10914299
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156894 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/infinite_cache_unittest.cc
+++ net/http/infinite_cache_unittest.cc
@@ -181,6 +181,7 @@ TEST(InfiniteCache, Delete) {
 }
 
 TEST(InfiniteCache, DeleteBetween) {
+#if !defined(OS_ANDROID)
   ScopedTempDir dir;
   ASSERT_TRUE(dir.CreateUniqueTempDir());
   FilePath path = dir.path().Append(FILE_PATH_LITERAL("infinite"));
@@ -232,4 +233,5 @@ TEST(InfiniteCache, DeleteBetween) {
   EXPECT_EQ(1, cb.GetResult(cache->QueryItemsForTest(cb.callback())));
   ProcessRequest(kTypicalGET_Transaction, cache.get());
   EXPECT_EQ(1, cb.GetResult(cache->QueryItemsForTest(cb.callback())));
+#endif  // OS_ANDROID
 }
