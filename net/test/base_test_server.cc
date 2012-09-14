df05e07bb415006d3c7cae49613d554ec2281edf
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 04:22:46 +0000

    Launch pywebsocket via net::TestServer
    BUG=137639
    
    
    Review URL: https://chromiumcodereview.appspot.com/10879029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156742 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/base_test_server.cc
+++ net/test/base_test_server.cc
@@ -30,8 +30,7 @@ namespace {
 
 std::string GetHostname(BaseTestServer::Type type,
                         const BaseTestServer::SSLOptions& options) {
-  if ((type == BaseTestServer::TYPE_HTTPS ||
-       type == BaseTestServer::TYPE_WSS) &&
+  if (BaseTestServer::UsingSSL(type) &&
       options.server_certificate ==
           BaseTestServer::SSLOptions::CERT_MISMATCHED_NAME) {
     // Return a different hostname string that resolves to the same hostname.
@@ -128,7 +127,7 @@ BaseTestServer::BaseTestServer(Type type, const SSLOptions& ssl_options)
       type_(type),
       started_(false),
       log_to_console_(false) {
-  DCHECK(type == TYPE_HTTPS || type == TYPE_WSS);
+  DCHECK(UsingSSL(type));
   Init(GetHostname(type, ssl_options));
 }
 
@@ -310,7 +309,7 @@ bool BaseTestServer::LoadTestRootCert() const {
 bool BaseTestServer::SetupWhenServerStarted() {
   DCHECK(host_port_pair_.port());
 
-  if ((type_ == TYPE_HTTPS || type_ == TYPE_WSS) && !LoadTestRootCert())
+  if (UsingSSL(type_) && !LoadTestRootCert())
       return false;
 
   started_ = true;
