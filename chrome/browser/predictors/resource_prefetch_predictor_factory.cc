67e71038180d3a461c4253821bfa3debc2099ec2
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 23:29:04 +0000

    Adds speculative prefetching of resources.
    The learning component was already checked in.
    
    BUG=None
    TEST=To be done.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10817004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156926 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/predictors/resource_prefetch_predictor_factory.cc
+++ chrome/browser/predictors/resource_prefetch_predictor_factory.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/predictors/predictor_database_factory.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor.h"
+#include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 
@@ -38,10 +39,11 @@ ResourcePrefetchPredictorFactory::~ResourcePrefetchPredictorFactory() {}
 ProfileKeyedService*
     ResourcePrefetchPredictorFactory::BuildServiceInstanceFor(
         Profile* profile) const {
-  return ResourcePrefetchPredictor::IsEnabled(profile) ?
-          new ResourcePrefetchPredictor(ResourcePrefetchPredictor::Config(),
-                                        profile) :
-          NULL;
+  return (prerender::IsSpeculativeResourcePrefetchingEnabled(profile) ||
+          prerender::IsSpeculativeResourcePrefetchingLearningEnabled(profile)) ?
+      new ResourcePrefetchPredictor(ResourcePrefetchPredictorConfig(),
+                                    profile) :
+      NULL;
 }
 
 }  // namespace predictors
