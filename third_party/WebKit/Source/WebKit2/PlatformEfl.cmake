f26b6e1715032ee83e67b7b4d6ac0f0ebc3290fe
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-14 01:15:39 +0000

    [EFL][WK2] Implement missing initializeLogChannel function https://bugs.webkit.org/show_bug.cgi?id=96478
    Patch by KwangYong Choi <ky0.choi@samsung.com> on 2012-09-13
    Reviewed by Gyuyoung Kim.
    
    Implemented log channel initialization function for EFL platform in WebKit2.
    
    * Platform/Logging.cpp:
    (WebKit):
    * Platform/Logging.h:
    (WebKit):
    * Platform/efl/LoggingEfl.cpp: Added.
    (WebKit):
    (WebKit::initializeLogChannel): Channel is initialized if its name is found in WEBKIT_DEBUG.
    * PlatformEfl.cmake:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128537 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
+++ third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 LIST(APPEND WebKit2_SOURCES
+    Platform/efl/LoggingEfl.cpp
     Platform/efl/ModuleEfl.cpp
     Platform/efl/WorkQueueEfl.cpp
     Platform/unix/SharedMemoryUnix.cpp
