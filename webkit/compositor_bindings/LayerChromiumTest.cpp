95a724bc22bb270e6ad5cd09fa0c24151fe7537c
vollick@google.com <vollick@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 19:57:44 +0000

    WebKit revision 120360 changed chromium so that we don't need to have a layerTreeHost to add an animation successfully. For now we are going back to old semantics because it causes mapsGL pages to have some un-accelerated animations that hang as a result.
    The tradeoff of doing this patch is that we gain correctness for mapsGL page,
    but we regress the fix in WebKit 120360 that fixed janky animations.
    
    BUG=http://code.google.com/p/chromium/issues/detail?id=138213
    
    Review URL: https://codereview.chromium.org/10911302
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156864 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/compositor_bindings/LayerChromiumTest.cpp
+++ webkit/compositor_bindings/LayerChromiumTest.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerChromium.h"
 
 #include "CCGeometryTestUtils.h"
+#include "CCKeyframedAnimationCurve.h"
 #include "CCLayerImpl.h"
 #include "CCLayerTreeHost.h"
 #include "CCSingleThreadProxy.h"
@@ -792,6 +793,40 @@ TEST(LayerChromiumLayerTreeHostTest, destroyHostWithNonNullRootLayer)
     OwnPtr<FakeCCLayerTreeHost> layerTreeHost(FakeCCLayerTreeHost::create());
     layerTreeHost->setRootLayer(root);
     layerTreeHost.clear();
+}
+
+static bool addTestAnimation(LayerChromium* layer)
+{
+    OwnPtr<CCKeyframedFloatAnimationCurve> curve(CCKeyframedFloatAnimationCurve::create());
+    curve->addKeyframe(CCFloatKeyframe::create(0, 0.3, nullptr));
+    curve->addKeyframe(CCFloatKeyframe::create(1, 0.7, nullptr));
+    OwnPtr<CCActiveAnimation> animation(CCActiveAnimation::create(curve.release(), 0, 0, CCActiveAnimation::Opacity));
+
+    return layer->addAnimation(animation.release());
+}
+
+TEST(LayerChromiumLayerTreeHostTest, shouldNotAddAnimationWithoutLayerTreeHost)
+{
+    // Currently, WebCore assumes that animations will be started immediately / very soon
+    // if a composited layer's addAnimation() returns true. However, without a layerTreeHost,
+    // layers cannot actually animate yet. So, to prevent violating this WebCore assumption,
+    // the animation should not be accepted if the layer doesn't already have a layerTreeHost.
+
+    WebKit::Platform::current()->compositorSupport()->setAcceleratedAnimationEnabled(true);
+
+    WebCompositorInitializer compositorInitializer(0);
+    RefPtr<LayerChromium> layer = LayerChromium::create();
+
+    // Case 1: without a layerTreeHost, the animation should not be accepted.
+    EXPECT_FALSE(addTestAnimation(layer.get()));
+
+    OwnPtr<FakeCCLayerTreeHost> layerTreeHost(FakeCCLayerTreeHost::create());
+    layerTreeHost->setRootLayer(layer.get());
+    layer->setLayerTreeHost(layerTreeHost.get());
+    assertLayerTreeHostMatchesForSubtree(layer.get(), layerTreeHost.get());
+
+    // Case 2: with a layerTreeHost, the animation should be accepted.
+    EXPECT_TRUE(addTestAnimation(layer.get()));
 }
 
 class MockLayerChromium : public LayerChromium {
