e9b6f68a349b83520572f2c204dca27b029571e0
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 12:01:44 +0000

    Add --disable-audio-fallback flag for debugging.
    We may need to help users triage audio issues, this flag will allow
    us to determine which audio path they're one.
    
    BUG=148418
    TEST=media_unittests w/ flag results in failed fallback tests.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10915276
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156783 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_output_resampler.cc
+++ media/audio/audio_output_resampler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/message_loop.h"
 #include "base/metrics/histogram.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/sample_rates.h"
 #include "media/base/audio_pull_fifo.h"
 #include "media/base/limits.h"
+#include "media/base/media_switches.h"
 #include "media/base/multi_channel_resampler.h"
 
 namespace media {
@@ -150,6 +152,13 @@ bool AudioOutputResampler::OpenStream() {
   // nothing more to be done.
   if (output_params_.format() == AudioParameters::AUDIO_PCM_LINEAR)
     return false;
+
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kDisableAudioFallback)) {
+    LOG(ERROR) << "Open failed and automatic fallback to high latency audio "
+               << "path is disabled, aborting.";
+    return false;
+  }
 
   DLOG(ERROR) << "Unable to open audio device in low latency mode.  Falling "
               << "back to high latency audio output.";
