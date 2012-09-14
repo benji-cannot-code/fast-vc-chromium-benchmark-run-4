0178c732f1f7842035d8942f7f381e78159894cd
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 16:44:20 +0000

    Hide cursor when key typed, moving to chrome
    BUG=136057
    Test=None
    
    Review URL: https://codereview.chromium.org/10914275
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156814 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/shared/compound_event_filter.cc
+++ ui/aura/shared/compound_event_filter.cc
@@ -150,7 +150,7 @@ ui::TouchStatus CompoundEventFilter::FilterTouchEvent(
 }
 
 void CompoundEventFilter::SetCursorVisibilityOnEvent(aura::Window* target,
-                                                     ui::LocatedEvent* event,
+                                                     ui::Event* event,
                                                      bool show) {
   if (update_cursor_visibility_ && !(event->flags() & ui::EF_IS_SYNTHESIZED)) {
     client::CursorClient* client =
@@ -180,6 +180,8 @@ ui::TouchStatus CompoundEventFilter::PreHandleTouchEvent(
 // CompoundEventFilter, ui::EventHandler implementation:
 
 ui::EventResult CompoundEventFilter::OnKeyEvent(ui::KeyEvent* event) {
+  SetCursorVisibilityOnEvent(
+      static_cast<Window*>(event->target()), event, false);
   return FilterKeyEvent(event);
 }
 
