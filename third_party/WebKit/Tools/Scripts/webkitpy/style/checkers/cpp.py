33c2c1da77d4e11012de9a05811655ce517faad9
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-14 10:58:24 +0000

    The runtime/unsigned category should be valid in the cpp style-checker. https://bugs.webkit.org/show_bug.cgi?id=96748
    Reviewed by Csaba Osztrogonác.
    
    This is causing the webkitpy tests to fail because it's not listed in the
    category array. Furthermore, change an "int" to "long" because it's
    throwing a double error in one of the earlier asserts.
    
    * Scripts/webkitpy/style/checkers/cpp.py:
    (CppChecker):
    * Scripts/webkitpy/style/checkers/cpp_unittest.py:
    (WebKitStyleTest.test_names):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/cpp.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/cpp.py
@@ -3601,6 +3601,7 @@ class CppChecker(object):
         'runtime/sizeof',
         'runtime/string',
         'runtime/threadsafe_fn',
+        'runtime/unsigned',
         'runtime/virtual',
         'whitespace/blank_line',
         'whitespace/braces',
