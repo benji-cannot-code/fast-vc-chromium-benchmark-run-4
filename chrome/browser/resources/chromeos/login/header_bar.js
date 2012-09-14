b45a46c4f7d5e652ad57636b83763928a68ae777
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 23:28:49 +0000

    GAIA frame should not trigger network error screen
    Fixes:
    * Delay offline message for the first time notification;
    * Do not show offline message if we're in connecting state;
    * Adjust Gaia reload timeouts;
    
    BUG=141557
    TEST=Manual testing on Alex
    
    
    Review URL: https://chromiumcodereview.appspot.com/10914005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/header_bar.js
+++ chrome/browser/resources/chromeos/login/header_bar.js
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 cr.define('login', function() {
   // Network state constants.
-  const NET_STATE = {
+  /** @const */ var NET_STATE = {
     OFFLINE: 0,
     ONLINE: 1,
     PORTAL: 2
@@ -83,17 +83,17 @@ cr.define('login', function() {
   /**
    * Continues add user button click handling after network state has
    * been recieved.
-   * @param {Integer} state Current state of the network (see NET_STATE).
+   * @param {number} state Current state of the network (see NET_STATE).
    * @param {string} network Name of the network.
    * @param {string} reason Reason the callback was called.
-   * @param {int} last Last active network type.
+   * @param {number} last Last active network type.
    */
   HeaderBar.handleAddUser = function(state, network, reason, last) {
     if (state != NET_STATE.OFFLINE) {
       Oobe.showSigninUI();
     } else {
-      const BUBBLE_OFFSET = 8;
-      const BUBBLE_PADDING = 5;
+      /** @const */ var BUBBLE_OFFSET = 8;
+      /** @const */ var BUBBLE_PADDING = 5;
       $('bubble').showTextForElement(
           $('add-user-button'),
           localStrings.getString('addUserErrorMessage'),
@@ -105,10 +105,10 @@ cr.define('login', function() {
 
   /**
    * Observes network state, and close the bubble when network becomes online.
-   * @param {Integer} state Current state of the network (see NET_STATE).
+   * @param {number} state Current state of the network (see NET_STATE).
    * @param {string} network Name of the network.
    * @param {string} reason Reason the callback was called.
-   * @param {int} last Last active network type.
+   * @param {number} last Last active network type.
    */
   HeaderBar.bubbleWatchdog = function(state, network, reason, last) {
     if (state != NET_STATE.OFFLINE) {
