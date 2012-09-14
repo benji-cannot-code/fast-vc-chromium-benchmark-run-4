9ea9ad0688ddf4f7d320c67845bd9fc5bbd76dea
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 15:19:26 +0000

    Split Drive test utilities to drive_test_util and gdata_test_util
    BUG=127855
    TEST=unit_tests, browser_tests
    TBR=thestig@chromium.org
      for .gypi change
    
    Review URL: https://codereview.chromium.org/10911306
    Patch from Hiroki Nakagawa <nhiroki@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/gdata/gdata_wapi_parser_unittest.cc
+++ chrome/browser/chromeos/gdata/gdata_wapi_parser_unittest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
-#include "chrome/browser/chromeos/gdata/drive_test_util.h"
+#include "chrome/browser/chromeos/gdata/gdata_test_util.h"
 #include "chrome/browser/chromeos/gdata/gdata_util.h"
 #include "chrome/common/chrome_paths.h"
 #include "testing/gtest/include/gtest/gtest.h"
