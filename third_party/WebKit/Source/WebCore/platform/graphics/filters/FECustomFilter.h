4f4bf1014e0082265ec15a639d3044e85a2d9d81
achicu@adobe.com <achicu@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-14 20:08:09 +0000

    [CSS Shaders] Enable anti-aliasing on the FECustomFilter https://bugs.webkit.org/show_bug.cgi?id=96668
    Reviewed by Dean Jackson.
    
    Added multisample frame buffer for the FECustomFilter. It will try to allocate a multisample framebuffer
    with a maximum sample count of 4. At the end it will blit the samples back to the original frame buffer,
    so that we can read the pixels back.
    
    This patch uses extensions GL_ANGLE_framebuffer_multisample, GL_ANGLE_framebuffer_blit
    and GL_OES_rgb8_rgba8.
    
    No new tests, multisampling is not working in Chromium DRT and has not pixel effect on WebKit Mac.
    
    * platform/graphics/filters/FECustomFilter.cpp:
    (WebCore::FECustomFilter::FECustomFilter):
    (WebCore::FECustomFilter::deleteRenderBuffers):
    (WebCore):
    (WebCore::FECustomFilter::deleteMultisampleRenderBuffers):
    (WebCore::FECustomFilter::drawFilterMesh):
    (WebCore::FECustomFilter::createMultisampleBuffer):
    (WebCore::FECustomFilter::resolveMultisampleBuffer):
    (WebCore::FECustomFilter::canUseMultisampleBuffers):
    (WebCore::FECustomFilter::resizeMultisampleBuffers):
    (WebCore::FECustomFilter::resizeContext):
    * platform/graphics/filters/FECustomFilter.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128646 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/filters/FECustomFilter.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/filters/FECustomFilter.h
@@ -57,7 +57,6 @@ class CustomFilterValidatedProgram;
 class DrawingBuffer;
 class GraphicsContext3D;
 class IntSize;
-class Texture;
 
 class FECustomFilter : public FilterEffect {
 public:
@@ -93,6 +92,12 @@ private:
     bool resizeContextIfNeeded(const IntSize&);
     bool resizeContext(const IntSize&);
 
+    bool canUseMultisampleBuffers() const;
+    bool createMultisampleBuffer();
+    bool resizeMultisampleBuffers(const IntSize&);
+    void resolveMultisampleBuffer();
+    void deleteMultisampleRenderBuffers();
+
     bool ensureFrameBuffer();
     void deleteRenderBuffers();
 
@@ -118,6 +123,11 @@ private:
     Platform3DObject m_frameBuffer;
     Platform3DObject m_depthBuffer;
     Platform3DObject m_destTexture;
+
+    bool m_triedMultisampleBuffer;
+    Platform3DObject m_multisampleFrameBuffer;
+    Platform3DObject m_multisampleRenderBuffer;
+    Platform3DObject m_multisampleDepthBuffer;
 
     RefPtr<CustomFilterProgram> m_program;
     CustomFilterParameterList m_parameters;
