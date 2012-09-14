e938f77be5e227f3ca77b2de05897166dfb8705a
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-14 06:38:02 +0000

    Refresh Guest session UI
    Removed guest icons and add guest label in tray
    
    BUG=145546
    TEST=manual
    
    Review URL: https://codereview.chromium.org/10918215
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156757 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/user/tray_user.h
+++ ash/system/user/tray_user.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace views {
 class ImageView;
+class Label;
 }
 
 namespace ash {
@@ -44,6 +45,7 @@ class TrayUser : public SystemTrayItem,
 
   tray::UserView* user_;
   tray::RoundedImageView* avatar_;
+  views::Label* label_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayUser);
 };
--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -469,7 +469,7 @@ bool BrowserView::IsGuestSession() const {
 bool BrowserView::ShouldShowAvatar() const {
   if (!IsBrowserTypeNormal())
     return false;
-  if (IsOffTheRecord())
+  if (IsOffTheRecord() && !IsGuestSession())
     return true;
   if (ManagedMode::IsInManagedMode())
     return true;
