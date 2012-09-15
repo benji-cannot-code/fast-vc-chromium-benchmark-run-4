e9800f3acd556ccce116e36b3a4e6aed97fcc01c
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-15 23:07:27 +0000

    Make V8ValueConverter.FromV8Value be more consistent with JSON.stringify: don't serialize undefined as null, don't serialize functions as objects, omit values from objects when they don't serialize, and insert null into arrays when they don't serialize.
    It is now possible for FromV8Value to return NULL; previously it would return
    Value::CreateNullValue on failure.
    
    This is needed for the Storage API, where we promise that the values that are
    passed in are serialized as JSON, yet the value conversion doesn't work in a
    way that allows it.
    
    However, the null-stripping behavior needs to be configurable so that existing
    extension APIs (which only expect null/undefined to appear for optional values)
    still work.
    
    BUG=145081
    
    Review URL: https://codereview.chromium.org/10890002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157025 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/schema_generated_bindings.js
+++ chrome/renderer/resources/extensions/schema_generated_bindings.js
@@ -312,9 +312,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             if (this.handleRequest) {
               retval = this.handleRequest.apply(this, args);
             } else {
+              var optArgs = {
+                customCallback: this.customCallback,
+                allowFunctionsInObjects: functionDef.allow_functions_in_objects
+              };
               retval = sendRequest(this.name, args,
                                    this.definition.parameters,
-                                   {customCallback: this.customCallback});
+                                   optArgs);
             }
 
             // Validate return value if defined - only in debug.
