0d3235bdb089facc1b68f7e36c49033c96293dcc
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-15 20:37:24 +0000

    Rename "chrome/browser/policy/policy_constants.h" so that it doesn't clash with the generated "policy/policy_constants.h" when it is included from chrome/browser.
    Review URL: https://chromiumcodereview.appspot.com/10917266
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157016 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/cloud_policy_client.h
+++ chrome/browser/policy/cloud_policy_client.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/time.h"
 #include "chrome/browser/policy/cloud_policy_constants.h"
-#include "chrome/browser/policy/policy_constants.h"
+#include "chrome/browser/policy/policy_types.h"
 
 namespace enterprise_management {
 class DeviceManagementResponse;
--- chrome/browser/ui/webui/options/preferences_browsertest.h
+++ chrome/browser/ui/webui/options/preferences_browsertest.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "chrome/browser/api/prefs/pref_change_registrar.h"
 #include "chrome/browser/policy/mock_configuration_policy_provider.h"
-#include "chrome/browser/policy/policy_constants.h"
+#include "chrome/browser/policy/policy_types.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/notification_observer.h"
