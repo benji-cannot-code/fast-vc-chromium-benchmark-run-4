34b4841461109b0107d673b102419971fd8217e0
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-15 01:40:14 +0000

    Revert 156926 - Adds speculative prefetching of resources.
    Massive heapcheck unit tests failures at http://build.chromium.org/p/chromium.memory.fyi/builders/Chromium%20OS%20Heapcheck/builds/12317
    The first one looks like: Suppression (error hash=#06E3805142FE1B35#): { <insert_a_suppression_name_here> Heapcheck:Leak fun:std::basic_string::_Rep::_S_create fun:bool ::InitCanonical fun:GURL::GURL fun:predictors::ResourcePrefetcherTest_TestPrefetcherStopped_Test::TestBody }
    
    The learning component was already checked in.
    
    BUG=None
    TEST=To be done.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10817004
    
    TBR=shishir@chromium.org
    Review URL: https://codereview.chromium.org/10908276
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156954 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/predictors/predictor_database.cc
+++ chrome/browser/predictors/predictor_database.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/predictors/autocomplete_action_predictor_table.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor_tables.h"
-#include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/browser_thread.h"
 #include "sql/connection.h"
@@ -68,8 +67,7 @@ PredictorDatabaseInternal::PredictorDatabaseInternal(Profile* profile)
       autocomplete_table_(new AutocompleteActionPredictorTable()),
       resource_prefetch_tables_(new ResourcePrefetchPredictorTables()) {
   is_resource_prefetch_predictor_enabled_ =
-      prerender::IsSpeculativeResourcePrefetchingLearningEnabled(profile) ||
-      prerender::IsSpeculativeResourcePrefetchingEnabled(profile);
+      ResourcePrefetchPredictor::IsEnabled(profile);
 }
 
 PredictorDatabaseInternal::~PredictorDatabaseInternal() {
