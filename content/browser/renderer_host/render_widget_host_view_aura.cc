ad6150aa2de82539e16336b451b26d4e039e2f40
horo@chromium.org <horo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-15 05:12:50 +0000

    Add ExtendSelectionAndDelete() method to ui::TextInputClient.
    This cl only implements RenderWidgetHostViewWin::ExtendSelectionAndDelete() which is needed to support HANJA key of Korean IME in Metro.
    
    BUG=148355
    TEST=trybots
    
    Review URL: https://chromiumcodereview.appspot.com/10928199
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@156992 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -1171,6 +1171,13 @@ bool RenderWidgetHostViewAura::ChangeTextDirectionAndLayoutAlignment(
   return true;
 }
 
+void RenderWidgetHostViewAura::ExtendSelectionAndDelete(
+    size_t before, size_t after) {
+  // TODO(horo): implement this method if it is required.
+  // http://crbug.com/149155
+  NOTIMPLEMENTED();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // RenderWidgetHostViewAura, aura::WindowDelegate implementation:
 
