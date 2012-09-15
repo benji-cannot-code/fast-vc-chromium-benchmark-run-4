525dc3c42f150c4aaeefb61b39e10e0323e97cb6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-15 01:43:41 +0000

    Unreviewed, rolling out r128673. http://trac.webkit.org/changeset/128673 https://bugs.webkit.org/show_bug.cgi?id=96847
    Makes plugins/document-open.html crash (Requested by jamesr_
    on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-09-14
    
    * dom/ContainerNode.cpp:
    (WebCore):
    (WebCore::ContainerNode::insertBeforeCommon):
    (WebCore::ContainerNode::replaceChild):
    (WebCore::ContainerNode::removeBetween):
    (WebCore::ContainerNode::removeChildren):
    (WebCore::ContainerNode::appendChild):
    (WebCore::ContainerNode::parserAddChild):
    (WebCore::dispatchChildInsertionEvents):
    (WebCore::dispatchChildRemovalEvents):
    * dom/ContainerNode.h:
    * dom/ContainerNodeAlgorithms.h:
    (WebCore::ChildNodeInsertionNotifier::notifyNodeInsertedIntoTree):
    (WebCore::ChildNodeInsertionNotifier::notify):
    (WebCore::ChildNodeRemovalNotifier::notifyNodeRemovedFromTree):
    * dom/Document.cpp:
    (WebCore::Document::dispatchWindowEvent):
    (WebCore::Document::dispatchWindowLoadEvent):
    * dom/EventDispatcher.cpp:
    (WebCore::EventDispatcher::dispatchEvent):
    * dom/EventTarget.cpp:
    (WebCore):
    (WebCore::forbidEventDispatch):
    (WebCore::allowEventDispatch):
    (WebCore::eventDispatchForbidden):
    (WebCore::EventTarget::fireEventListeners):
    * dom/EventTarget.h:
    (WebCore):
    (WebCore::forbidEventDispatch):
    (WebCore::allowEventDispatch):
    * dom/Node.cpp:
    (WebCore::Node::dispatchSubtreeModifiedEvent):
    (WebCore::Node::dispatchFocusInEvent):
    (WebCore::Node::dispatchFocusOutEvent):
    (WebCore::Node::dispatchDOMActivateEvent):
    * dom/WebKitNamedFlow.cpp:
    (WebCore::WebKitNamedFlow::dispatchRegionLayoutUpdateEvent):
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::loadInternal):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -4103,7 +4103,7 @@ EventListener* Document::getWindowAttributeEventListener(const AtomicString& eve
 
 void Document::dispatchWindowEvent(PassRefPtr<Event> event,  PassRefPtr<EventTarget> target)
 {
-    ASSERT(!AssertNoEventDispatch::isEventDispatchForbidden());
+    ASSERT(!eventDispatchForbidden());
     DOMWindow* domWindow = this->domWindow();
     if (!domWindow)
         return;
@@ -4112,7 +4112,7 @@ void Document::dispatchWindowEvent(PassRefPtr<Event> event,  PassRefPtr<EventTar
 
 void Document::dispatchWindowLoadEvent()
 {
-    ASSERT(!AssertNoEventDispatch::isEventDispatchForbidden());
+    ASSERT(!eventDispatchForbidden());
     DOMWindow* domWindow = this->domWindow();
     if (!domWindow)
         return;
