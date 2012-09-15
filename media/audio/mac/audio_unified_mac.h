25ee8feb4a594606232ad702a222bb48a12f4aeb
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-15 23:07:02 +0000

    mac: Unbreak building with the 10.7 SDK
    BUG=none
    TEST=No compile errors when building with the 10.7 SDK.
    NOTRY=true
    TBR=crogers
    
    Review URL: https://chromiumcodereview.appspot.com/10910300
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157024 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_unified_mac.h
+++ media/audio/mac/audio_unified_mac.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_AUDIO_MAC_AUDIO_UNIFIED_MAC_H_
 #define MEDIA_AUDIO_MAC_AUDIO_UNIFIED_MAC_H_
 
-#include <AudioUnit/AudioUnit.h>
+#include <CoreAudio/CoreAudio.h>
 
 #include "base/memory/scoped_ptr.h"
 #include "media/audio/audio_io.h"
