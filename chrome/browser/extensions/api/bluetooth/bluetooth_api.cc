c5c2aaf9724787aa2be32ddafa1ef619c4f7375b
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-16 00:35:16 +0000

    Revert 156348 (caused http://crbug.com/149828) - Change getDevices to use a DeviceCallback.
    This modifies the getDevices method of the bluetooth Extensions API to
    accept a DeviceCallback, which will be called for every matching device.
    
    This will allow more thorough, long-running searches (involving SDP
    queries) to be performed without slowing down access to matching devices
    that can be returned quickly.
    
    TEST=api test updated
    BUG=134545
    
    
    Review URL: https://chromiumcodereview.appspot.com/10915148
    
    TBR=bryeung@chromium.org
    Review URL: https://codereview.chromium.org/10916331
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
+++ chrome/browser/extensions/api/bluetooth/bluetooth_api.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "chrome/browser/extensions/api/bluetooth/bluetooth_api_utils.h"
-#include "chrome/browser/extensions/event_names.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/experimental_bluetooth.h"
@@ -101,23 +100,14 @@ bool BluetoothGetNameFunction::RunImpl() {
 BluetoothGetDevicesFunction::BluetoothGetDevicesFunction()
     : callbacks_pending_(0) {}
 
-void BluetoothGetDevicesFunction::DispatchDeviceSearchResult(
-    const chromeos::BluetoothDevice& device) {
-  experimental_bluetooth::Device extension_device;
-  experimental_bluetooth::BluetoothDeviceToApiDevice(device, &extension_device);
-  GetEventRouter(profile())->DispatchDeviceEvent(
-      extensions::event_names::kBluetoothOnDeviceSearchResult,
-      extension_device);
-}
-
-void BluetoothGetDevicesFunction::ProvidesServiceCallback(
+void BluetoothGetDevicesFunction::AddDeviceIfTrueCallback(
+    ListValue* list,
     const chromeos::BluetoothDevice* device,
-    bool providesService) {
+    bool shouldAdd) {
   DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 
-  CHECK(device);
-  if (providesService)
-    DispatchDeviceSearchResult(*device);
+  if (shouldAdd)
+    list->Append(experimental_bluetooth::BluetoothDeviceToValue(*device));
 
   callbacks_pending_--;
   if (callbacks_pending_ == -1)
@@ -140,6 +130,9 @@ bool BluetoothGetDevicesFunction::RunImpl() {
     }
   }
 
+  ListValue* matches = new ListValue;
+  SetResult(matches);
+
   CHECK_EQ(0, callbacks_pending_);
 
   chromeos::BluetoothAdapter::DeviceList devices =
@@ -147,21 +140,21 @@ bool BluetoothGetDevicesFunction::RunImpl() {
   for (chromeos::BluetoothAdapter::DeviceList::iterator i = devices.begin();
       i != devices.end(); ++i) {
     chromeos::BluetoothDevice* device = *i;
-    CHECK(device);
 
     if (!uuid.empty() && !(device->ProvidesServiceWithUUID(uuid)))
       continue;
 
     if (options.name.get() == NULL) {
-      DispatchDeviceSearchResult(*device);
+      matches->Append(experimental_bluetooth::BluetoothDeviceToValue(*device));
       continue;
     }
 
     callbacks_pending_++;
     device->ProvidesServiceWithName(
         *(options.name),
-        base::Bind(&BluetoothGetDevicesFunction::ProvidesServiceCallback,
+        base::Bind(&BluetoothGetDevicesFunction::AddDeviceIfTrueCallback,
                    this,
+                   matches,
                    device));
   }
   callbacks_pending_--;
--- chrome/browser/extensions/event_names.cc
+++ chrome/browser/extensions/event_names.cc
@@ -63,8 +63,6 @@ const char kBluetoothOnAvailabilityChanged[] =
     "experimental.bluetooth.onAvailabilityChanged";
 const char kBluetoothOnDeviceDiscovered[] =
     "experimental.bluetooth.onDeviceDiscovered";
-const char kBluetoothOnDeviceSearchResult[] =
-    "experimental.bluetooth.onDeviceSearchResult";
 const char kBluetoothOnDiscoveringChanged[] =
     "experimental.bluetooth.onDiscoveringChanged";
 const char kBluetoothOnPowerChanged[] =
