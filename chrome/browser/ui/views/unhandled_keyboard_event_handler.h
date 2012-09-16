9406a6d1f60c6022cd420f62c0cc1bba65e9bbf1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-16 01:38:51 +0000

    views: Forward declare NativeWebKeyboardEvent instead of including its header file.
    BUG=125846
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/10918262
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157033 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/unhandled_keyboard_event_handler.h
+++ chrome/browser/ui/views/unhandled_keyboard_event_handler.h
@@ -6,12 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_UNHANDLED_KEYBOARD_EVENT_HANDLER_H_
 #define CHROME_BROWSER_UI_VIEWS_UNHANDLED_KEYBOARD_EVENT_HANDLER_H_
 
-#include "content/public/browser/native_web_keyboard_event.h"
-#include "ui/views/view.h"
+#include "base/basictypes.h"
+
+namespace content {
+struct NativeWebKeyboardEvent;
+}
 
 namespace views {
 class FocusManager;
-}  // namespace views
+}
 
 // This class handles unhandled keyboard messages coming back from the renderer
 // process.
