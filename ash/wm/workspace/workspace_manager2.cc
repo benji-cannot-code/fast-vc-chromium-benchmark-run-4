e9bc9e809ab68b854cb96492e2b7f505609f1b00
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-16 21:31:14 +0000

    Makes it so we don't fade out the background of the desktop when the app is exiting. Otherwise when chrome exits the desktop appears at the initial frame of animating in, which is scaled down. This happens because we end up drawing a frame of the animation, chrome exits and until chrome restarts you see the scaled down chrome.
    BUG=149060
    TEST=see bug
    R=ben@chromium.org,nkostylev@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10910302
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157060 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_manager2.cc
+++ ash/wm/workspace/workspace_manager2.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/workspace/workspace_layout_manager2.h"
 #include "ash/wm/workspace/workspace2.h"
 #include "base/auto_reset.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
 #include "base/stringprintf.h"
@@ -99,7 +100,8 @@ WorkspaceManager2::WorkspaceManager2(Window* contents_view)
       shelf_(NULL),
       in_move_(false),
       ALLOW_THIS_IN_INITIALIZER_LIST(
-          clear_unminimizing_workspace_factory_(this)) {
+          clear_unminimizing_workspace_factory_(this)),
+      app_terminating_(false) {
   // Clobber any existing event filter.
   contents_view->SetEventFilter(NULL);
   // |contents_view| takes ownership of WorkspaceManagerLayoutManager2.
@@ -108,9 +110,11 @@ WorkspaceManager2::WorkspaceManager2(Window* contents_view)
   active_workspace_ = CreateWorkspace(false);
   workspaces_.push_back(active_workspace_);
   active_workspace_->window()->Show();
+  Shell::GetInstance()->AddShellObserver(this);
 }
 
 WorkspaceManager2::~WorkspaceManager2() {
+  Shell::GetInstance()->RemoveShellObserver(this);
   // Release the windows, they'll be destroyed when |contents_view_| is
   // destroyed.
   std::for_each(workspaces_.begin(), workspaces_.end(),
@@ -224,6 +228,10 @@ Window* WorkspaceManager2::GetParentForNewWindow(Window* window) {
   return desktop_workspace()->window();
 }
 
+void WorkspaceManager2::OnAppTerminating() {
+  app_terminating_ = true;
+}
+
 void WorkspaceManager2::UpdateShelfVisibility() {
   if (shelf_)
     shelf_->UpdateVisibilityState();
@@ -285,7 +293,7 @@ void WorkspaceManager2::SetActiveWorkspace(Workspace2* workspace,
     base::TimeDelta delay(GetSystemBackgroundDestroyDuration());
     if (ui::LayerAnimator::slow_animation_mode())
       delay *= ui::LayerAnimator::slow_animation_scale_factor();
-    // Delay an extra 100ms to make sure everything settlts down before
+    // Delay an extra 100ms to make sure everything settles down before
     // destroying the background.
     delay += base::TimeDelta::FromMilliseconds(100);
     destroy_background_timer_.Start(
@@ -315,7 +323,7 @@ void WorkspaceManager2::SetActiveWorkspace(Workspace2* workspace,
     if (last_active == desktop_workspace()) {
       AnimateWorkspaceOut(background, WORKSPACE_ANIMATE_DOWN,
                           WORKSPACE_DESKTOP);
-    } else if (active_workspace_ == desktop_workspace()) {
+    } else if (active_workspace_ == desktop_workspace() && !app_terminating_) {
       AnimateWorkspaceIn(background, WORKSPACE_ANIMATE_UP);
     }
   }
