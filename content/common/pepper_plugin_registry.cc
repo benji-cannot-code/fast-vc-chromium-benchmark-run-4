8510e1740d694f8d47405a3b642bf432bb851259
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-16 04:02:12 +0000

    Add some logging to debug PPAPI permissions.
    Somehow these aren't getting set properly for the bundled Flash case. The code path we take to get them is pretty long, so this patch adds some temporary logging to try to see wher they're lost.
    
    BUG=147507
    
    Review URL: https://codereview.chromium.org/10917160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157040 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/pepper_plugin_registry.cc
+++ content/common/pepper_plugin_registry.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/file_util.h"
+#include "base/logging.h"
 #include "base/native_library.h"
 #include "base/string_split.h"
 #include "base/string_util.h"
@@ -99,6 +100,11 @@ webkit::WebPluginInfo content::PepperPluginInfo::ToWebPluginInfo() const {
   info.mime_types = mime_types;
   info.pepper_permissions = permissions;
 
+  // TODO(brettw) bug 147507: remove this logging.
+  LOG(INFO) << "PepperPluginInfo::ToWebPluginInfo \""
+            << UTF16ToUTF8(info.path.LossyDisplayName()) << "\" "
+            << "permissions = " << permissions;
+
   return info;
 }
 
@@ -117,6 +123,10 @@ bool MakePepperPluginInfo(const webkit::WebPluginInfo& webplugin_info,
   pepper_info->version = UTF16ToASCII(webplugin_info.version);
   pepper_info->mime_types = webplugin_info.mime_types;
   pepper_info->permissions = webplugin_info.pepper_permissions;
+
+  LOG(INFO) << "PepperPluginInfo::ToWebPluginInfo \""
+            << UTF16ToUTF8(pepper_info->path.LossyDisplayName()) << "\" "
+            << "permissions = " << pepper_info->permissions;
 
   return true;
 }
@@ -225,6 +235,11 @@ PepperPluginRegistry::PepperPluginRegistry() {
     const content::PepperPluginInfo& current = plugin_list_[i];
     if (current.is_out_of_process)
       continue;  // Out of process plugins need no special pre-initialization.
+
+    // TODO(brettw) bug 147507: Remove this logging.
+    LOG(INFO) << "PepperPluginRegistry::PepperPluginRegistry \""
+              << UTF16ToUTF8(current.path.LossyDisplayName()) << "\" "
+              << " permissions =" << current.permissions;
 
     scoped_refptr<webkit::ppapi::PluginModule> module =
         new webkit::ppapi::PluginModule(current.name, current.path, this,
