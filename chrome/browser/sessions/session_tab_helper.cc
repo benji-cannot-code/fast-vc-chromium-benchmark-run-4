69637ea5ba60d94397544b390f095a165a4966a5
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 17:08:12 +0000

    Switch NOTIFICATION_TAB_PARENTED to use WebContents.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10917198
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157134 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_tab_helper.cc
+++ chrome/browser/sessions/session_tab_helper.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_service.h"
 #include "chrome/browser/sessions/session_service_factory.h"
-#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/extensions/extension_messages.h"
 #include "content/public/browser/notification_service.h"
@@ -27,14 +26,10 @@ SessionTabHelper::~SessionTabHelper() {
 void SessionTabHelper::SetWindowID(const SessionID& id) {
   window_id_ = id;
 
-  // TODO(mpcomplete): Maybe this notification should send out a WebContents.
-  TabContents* tab = TabContents::FromWebContents(web_contents());
-  if (tab) {
-    content::NotificationService::current()->Notify(
-        chrome::NOTIFICATION_TAB_PARENTED,
-        content::Source<TabContents>(tab),
-        content::NotificationService::NoDetails());
-  }
+  content::NotificationService::current()->Notify(
+      chrome::NOTIFICATION_TAB_PARENTED,
+      content::Source<content::WebContents>(web_contents()),
+      content::NotificationService::NoDetails());
 
   // Extension code in the renderer holds the ID of the window that hosts it.
   // Notify it that the window ID changed.
