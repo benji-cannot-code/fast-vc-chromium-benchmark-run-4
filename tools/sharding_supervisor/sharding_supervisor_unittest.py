485c6210f23070a1091b75aa84fe7459e80dbdf2
hinoka@chromium.org <hinoka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 20:41:49 +0000

    Sharding supervisor changes * Fixed tests in windows * Split up stdout and stderr so they print separately
    Its not clear whether or not this'll fix http://code.google.com/p/chromium/issues/detail?id=145308&can=3&colspec=ID%20Pri%20Mstone%20ReleaseBlock%20OS%20Area%20Feature%20Status%20Owner%20Summary
    But the goal is to quarantine stderr from mixing in with the stdout pipe, so this code uses two extra threads per shard to collect stdout and stderr byte by byte, and emit them line by line.
    
    Review URL: https://chromiumcodereview.appspot.com/10919228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157184 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/sharding_supervisor/sharding_supervisor_unittest.py
+++ tools/sharding_supervisor/sharding_supervisor_unittest.py
@@ -28,8 +28,10 @@ def generate_expected_output(start, end, num_shards):
   stdout = ''
   stderr = ''
   for i in range(start, end):
-    stdout += 'Running shard %d of %d\n' % (i, num_shards)
-  stdout += '\nALL SHARDS PASSED!\nALL TESTS PASSED!\n'
+    stdout += 'Running shard %d of %d%s' % (i, num_shards, os.linesep)
+  stdout += '%sALL SHARDS PASSED!%sALL TESTS PASSED!%s' % (os.linesep,
+                                                           os.linesep,
+                                                           os.linesep)
 
   return (stdout, stderr)
 
@@ -40,8 +42,9 @@ class ShardingSupervisorUnittest(unittest.TestCase):
     expected_shards = NUM_CORES * SHARDS_PER_CORE
     (expected_out, expected_err) = generate_expected_output(
         0, expected_shards, expected_shards)
-    p = subprocess.Popen([SHARDING_SUPERVISOR, '--no-color', DUMMY_TEST],
-                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p = subprocess.Popen([sys.executable, SHARDING_SUPERVISOR, '--no-color',
+                          DUMMY_TEST], stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE)
 
     (out, err) = p.communicate()
     self.assertEqual(expected_out, out)
@@ -53,7 +56,7 @@ class ShardingSupervisorUnittest(unittest.TestCase):
     expected_shards = NUM_CORES * 25
     (expected_out, expected_err) = generate_expected_output(
         0, expected_shards, expected_shards)
-    p = subprocess.Popen([SHARDING_SUPERVISOR, '--no-color',
+    p = subprocess.Popen([sys.executable, SHARDING_SUPERVISOR, '--no-color',
                           '--shards_per_core', '25', DUMMY_TEST],
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
@@ -73,7 +76,7 @@ class ShardingSupervisorUnittest(unittest.TestCase):
       end = begin + NUM_CORES * SHARDS_PER_CORE
       (expected_out, expected_err) = generate_expected_output(
           begin, end, expected_shards)
-      p = subprocess.Popen([SHARDING_SUPERVISOR, '--no-color',
+      p = subprocess.Popen([sys.executable, SHARDING_SUPERVISOR, '--no-color',
                             '--total-slaves', str(total_shards),
                             '--slave-index', str(index),
                             DUMMY_TEST],
