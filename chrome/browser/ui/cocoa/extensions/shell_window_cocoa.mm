742a15d02dad62ccb857842b9c1dcee0b9b56abc
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 22:16:55 +0000

    Re-enable native UI for {frame:'chrome'} in app windows.
    The HTML titlebar is still available as {frame:'experimental-html'} when the --enable-experimental-extensions-apis command line switch is present.
    
    R=mihaip@chromium.org
    BUG=149689
    
    
    Review URL: https://chromiumcodereview.appspot.com/10917274
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/shell_window_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/shell_window_cocoa.mm
@@ -87,16 +87,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @end
 
-@interface ShellNSWindow : ChromeEventProcessingWindow
-
-- (void)drawCustomFrameRect:(NSRect)rect forView:(NSView*)view;
-
-@end
-
 // This is really a method on NSGrayFrame, so it should only be called on the
 // view passed into -[NSWindow drawCustomFrameRect:forView:].
 @interface NSView (PrivateMethods)
 - (CGFloat)roundedCornerRadius;
+@end
+
+@interface ShellNSWindow : ChromeEventProcessingWindow
+
+- (void)drawCustomFrameRect:(NSRect)rect forView:(NSView*)view;
+
 @end
 
 @implementation ShellNSWindow
@@ -116,6 +116,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [[NSColor whiteColor] set];
   NSRectFill(rect);
 }
+
+@end
+
+@interface ShellFramelessNSWindow : ShellNSWindow
+
+@end
+
+@implementation ShellFramelessNSWindow
 
 + (NSRect)frameRectForContentRect:(NSRect)contentRect
                         styleMask:(NSUInteger)mask {
@@ -176,11 +184,20 @@ ShellWindowCocoa::ShellWindowCocoa(ShellWindow* shell_window,
   NSUInteger style_mask = NSTitledWindowMask | NSClosableWindowMask |
                           NSMiniaturizableWindowMask | NSResizableWindowMask |
                           NSTexturedBackgroundWindowMask;
-  scoped_nsobject<NSWindow> window([[ShellNSWindow alloc]
-      initWithContentRect:cocoa_bounds
-                styleMask:style_mask
-                  backing:NSBackingStoreBuffered
-                    defer:NO]);
+  scoped_nsobject<NSWindow> window;
+  if (has_frame_) {
+    window.reset([[ShellNSWindow alloc]
+        initWithContentRect:cocoa_bounds
+                  styleMask:style_mask
+                    backing:NSBackingStoreBuffered
+                      defer:NO]);
+  } else {
+    window.reset([[ShellFramelessNSWindow alloc]
+        initWithContentRect:cocoa_bounds
+                  styleMask:style_mask
+                    backing:NSBackingStoreBuffered
+                      defer:NO]);
+  }
   [window setTitle:base::SysUTF8ToNSString(extension()->name())];
   gfx::Size min_size = params.minimum_size;
   if (min_size.width() || min_size.height()) {
