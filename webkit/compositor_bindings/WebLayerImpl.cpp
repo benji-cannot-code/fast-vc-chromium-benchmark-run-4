efc8874c60a2a9f57a91db887815ca5678600031
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 11:43:05 +0000

    Add scrolling related getters to WebLayerImpl
    This patch adds a number of scrolling related getters to WebLayerImpl
    and LayerChromium for testing purposes. They will be used by the
    ScrollingCoordinatorChromium test (http://webkit.org/b/96657).
    
    Note that the WebLayerImpl getters aren't marked with OVERRIDE because
    the corresponding changes to WebLayer aren't in place yet. This will be
    corrected later.
    
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10909233
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157101 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/compositor_bindings/WebLayerImpl.cpp
+++ webkit/compositor_bindings/WebLayerImpl.cpp
@@ -349,9 +349,19 @@ void WebLayerImpl::setMaxScrollPosition(WebSize maxScrollPosition)
     m_layer->setMaxScrollPosition(convert(maxScrollPosition));
 }
 
+WebSize WebLayerImpl::maxScrollPosition() const
+{
+    return convert(m_layer->maxScrollPosition());
+}
+
 void WebLayerImpl::setScrollable(bool scrollable)
 {
     m_layer->setScrollable(scrollable);
+}
+
+bool WebLayerImpl::scrollable() const
+{
+    return m_layer->scrollable();
 }
 
 void WebLayerImpl::setHaveWheelEventHandlers(bool haveWheelEventHandlers)
@@ -359,9 +369,19 @@ void WebLayerImpl::setHaveWheelEventHandlers(bool haveWheelEventHandlers)
     m_layer->setHaveWheelEventHandlers(haveWheelEventHandlers);
 }
 
+bool WebLayerImpl::haveWheelEventHandlers() const
+{
+    return m_layer->haveWheelEventHandlers();
+}
+
 void WebLayerImpl::setShouldScrollOnMainThread(bool shouldScrollOnMainThread)
 {
     m_layer->setShouldScrollOnMainThread(shouldScrollOnMainThread);
+}
+
+bool WebLayerImpl::shouldScrollOnMainThread() const
+{
+    return m_layer->shouldScrollOnMainThread();
 }
 
 void WebLayerImpl::setNonFastScrollableRegion(const WebVector<WebRect>& rects)
@@ -375,14 +395,33 @@ void WebLayerImpl::setNonFastScrollableRegion(const WebVector<WebRect>& rects)
 
 }
 
+WebVector<WebRect> WebLayerImpl::nonFastScrollableRegion() const
+{
+    Vector<WebCore::IntRect> regionRects = m_layer->nonFastScrollableRegion().rects();
+    WebVector<WebRect> result(regionRects.size());
+    for (size_t i = 0; i < regionRects.size(); ++i)
+        result[i] = convert(regionRects[i]);
+    return result;
+}
+
 void WebLayerImpl::setIsContainerForFixedPositionLayers(bool enable)
 {
     m_layer->setIsContainerForFixedPositionLayers(enable);
 }
 
+bool WebLayerImpl::isContainerForFixedPositionLayers() const
+{
+    return m_layer->isContainerForFixedPositionLayers();
+}
+
 void WebLayerImpl::setFixedToContainerLayer(bool enable)
 {
     m_layer->setFixedToContainerLayer(enable);
+}
+
+bool WebLayerImpl::fixedToContainerLayer() const
+{
+    return m_layer->fixedToContainerLayer();
 }
 
 void WebLayerImpl::setScrollClient(WebLayerScrollClient* scrollClient)
