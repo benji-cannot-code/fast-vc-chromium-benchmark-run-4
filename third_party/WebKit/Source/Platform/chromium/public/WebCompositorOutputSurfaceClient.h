32d25fdce1099e3f2c7cd2f5abe1c732e9ba0eb3
piman@chromium.org <piman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-17 21:31:14 +0000

    [chromium] Add onSendFrameToParentCompositorAck to WebCompositorOutputSurfaceClient https://bugs.webkit.org/show_bug.cgi?id=96850
    Reviewed by James Robinson.
    
    Hook for the WebCompositorOutputSurface::sendFrameToParent ack.
    Also changes WebCompositorFrame from a class to a struct.
    
    * chromium/public/WebCompositorOutputSurface.h:
    (WebKit):
    * chromium/public/WebCompositorOutputSurfaceClient.h:
    (WebKit):
    (WebCompositorOutputSurfaceClient):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebCompositorOutputSurfaceClient.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebCompositorOutputSurfaceClient.h
@@ -29,9 +29,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
+struct WebCompositorFrameAck;
+
 class WebCompositorOutputSurfaceClient {
 public:
     virtual void onVSyncParametersChanged(double monotonicTimebase, double intervalInSeconds) = 0;
+    virtual void onSendFrameToParentCompositorAck(const WebCompositorFrameAck&) { }
 
 protected:
     ~WebCompositorOutputSurfaceClient() { }
