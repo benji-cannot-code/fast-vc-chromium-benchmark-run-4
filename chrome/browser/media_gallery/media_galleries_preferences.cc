9673af70360698d380a0c3390f04b82cdc25e85b
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 11:27:35 +0000

    make sure to init POD in MediaGalleriesPreferences
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10907216
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157099 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_gallery/media_galleries_preferences.cc
+++ chrome/browser/media_gallery/media_galleries_preferences.cc
@@ -123,7 +123,8 @@ DictionaryValue* CreateGalleryPrefInfoDictionary(
 }  // namespace
 
 MediaGalleryPrefInfo::MediaGalleryPrefInfo()
-    : pref_id(kInvalidMediaGalleryPrefId) {
+    : pref_id(kInvalidMediaGalleryPrefId),
+      type(kInvalidType) {
 }
 MediaGalleryPrefInfo::~MediaGalleryPrefInfo() {}
 
@@ -199,7 +200,7 @@ bool MediaGalleriesPreferences::LookUpGalleryByPath(
       gallery_info->display_name = string16();
       gallery_info->device_id = std::string();
       gallery_info->path = FilePath();
-      gallery_info->type = MediaGalleryPrefInfo::kBlackListed;
+      gallery_info->type = MediaGalleryPrefInfo::kInvalidType;
     }
     return false;
   }
