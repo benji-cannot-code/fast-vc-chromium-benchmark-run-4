fe4bf1b395956936b8eb926053675d7f885aa918
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-17 21:43:53 +0000

    [WIN] Use BString in favour of BSTR to improve memory management https://bugs.webkit.org/show_bug.cgi?id=93128
    Reviewed by Anders Carlsson.
    
    BString automatically calls SysFreeString() in its destructor which helps
    avoiding memory leaks. So it should be used instead of BSTR directly.
    Add operator& to BString to allow its usage for out parameters too (like COMPtr).
    This fixes already a few memory leaks in the existing code.
    
    * platform/win/BString.cpp:
    (WebCore::BString::~BString):
    (WebCore::BString::adoptBSTR):
    (WebCore::BString::clear):
    (WebCore):
    * platform/win/BString.h:
    (BString):
    (WebCore::BString::operator&):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128809 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/win/DefaultPolicyDelegate.cpp
+++ third_party/WebKit/Source/WebKit/win/DefaultPolicyDelegate.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitDLL.h"
 #include "DefaultPolicyDelegate.h"
 
+#include <WebCore/BString.h>
 #include <WebCore/COMPtr.h>
 #include <wtf/text/WTFString.h>
 
@@ -117,7 +118,7 @@ HRESULT STDMETHODCALLTYPE DefaultPolicyDelegate::decidePolicyForNavigationAction
         else if (navType == WebNavigationTypePlugInRequest)
             listener->use();
         else {
-            BSTR url;
+            BString url;
             // A file URL shouldn't fall through to here, but if it did,
             // it would be a security risk to open it.
             if (SUCCEEDED(request->URL(&url)) && !String(url, SysStringLen(url)).startsWith("file:")) {
@@ -125,7 +126,6 @@ HRESULT STDMETHODCALLTYPE DefaultPolicyDelegate::decidePolicyForNavigationAction
                 ;
             }
             listener->ignore();
-            SysFreeString(url);
         }
     }
     return S_OK;
@@ -153,7 +153,7 @@ HRESULT STDMETHODCALLTYPE DefaultPolicyDelegate::decidePolicyForMIMEType(
     if (FAILED(webView->canShowMIMEType(type, &canShowMIMEType)))
         canShowMIMEType = FALSE;
 
-    BSTR url;
+    BString url;
     request->URL(&url);
 
     if (String(url, SysStringLen(url)).startsWith("file:")) {
@@ -172,7 +172,6 @@ HRESULT STDMETHODCALLTYPE DefaultPolicyDelegate::decidePolicyForMIMEType(
         listener->use();
     else
         listener->ignore();
-    SysFreeString(url);
     return S_OK;
 }
 
@@ -181,15 +180,13 @@ HRESULT STDMETHODCALLTYPE DefaultPolicyDelegate::unableToImplementPolicyWithErro
     /*[in]*/ IWebError* error, 
     /*[in]*/ IWebFrame* frame)
 {
-    BSTR errorStr;
+    BString errorStr;
     error->localizedDescription(&errorStr);
 
-    BSTR frameName;
+    BString frameName;
     frame->name(&frameName);
 
     LOG_ERROR("called unableToImplementPolicyWithError:%S inFrame:%S", errorStr ? errorStr : TEXT(""), frameName ? frameName : TEXT(""));
-    SysFreeString(errorStr);
-    SysFreeString(frameName);
 
     return S_OK;
 }
--- third_party/WebKit/Source/WebKit/win/WebNotificationCenter.cpp
+++ third_party/WebKit/Source/WebKit/win/WebNotificationCenter.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebNotificationCenter.h"
 
 #include "WebNotification.h"
+#include <WebCore/BString.h>
 #include <WebCore/COMPtr.h>
 #include <utility>
 #include <wchar.h>
@@ -161,7 +162,7 @@ HRESULT STDMETHODCALLTYPE WebNotificationCenter::addObserver(
 HRESULT STDMETHODCALLTYPE WebNotificationCenter::postNotification( 
     /* [in] */ IWebNotification* notification)
 {
-    BSTR name;
+    BString name;
     HRESULT hr = notification->name(&name);
     if (FAILED(hr))
         return hr;
@@ -172,7 +173,6 @@ HRESULT STDMETHODCALLTYPE WebNotificationCenter::postNotification(
         return hr;
 
     postNotificationInternal(notification, name, obj.get());
-    SysFreeString(name);
 
     return hr;
 }
