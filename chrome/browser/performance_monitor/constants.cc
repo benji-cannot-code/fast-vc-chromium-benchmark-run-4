19b683c751fa53769fadc475edbaf978ea08190b
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 19:34:43 +0000

    Add guards to metric values; erase bad events/metrics from db
    Improves the database by not inserting blatantly erroneous data, and by deleting
    any events or metrics which cannot be properly retrieved from the database.
    Also make DB take a metric for AddMetric() - this makes it so that we have a
    more accurate timestamp since we record the time before the thread-jumping.
    
    BUG=130212
    
    
    Review URL: https://chromiumcodereview.appspot.com/10907121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157168 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/performance_monitor/constants.cc
+++ chrome/browser/performance_monitor/constants.cc
@@ -5,9 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/performance_monitor/constants.h"
 
-namespace performance_monitor {
+#include "base/time.h"
 
-// TODO(chebert): i18n on all constants.
+namespace performance_monitor {
 
 // The error message displayed when a metric's details are not found.
 const char kMetricNotFoundError[] = "Metric details not found.";
@@ -28,5 +28,17 @@ const char kStateChromeVersion[] = "chrome_version";
 // The prefix to the state of a profile's name, to prevent any possible naming
 // collisions in the database.
 const char kStateProfilePrefix[] = "profile";
+
+// Unit values
+// Memory measurements
+const int64 kBytesPerKilobyte = 1 << 10;
+const int64 kBytesPerMegabyte = kBytesPerKilobyte * (1 << 10);
+const int64 kBytesPerGigabyte = kBytesPerMegabyte * (1 << 10);
+const int64 kBytesPerTerabyte = kBytesPerGigabyte * (1 << 10);
+
+// Time measurements
+const int64 kMicrosecondsPerMonth = base::Time::kMicrosecondsPerDay * 30;
+const int64 kMicrosecondsPerYear = base::Time::kMicrosecondsPerDay * 365;
+
 
 }  // namespace performance_monitor
