f27a90f5c1638899f6a10d784ab0397e79777f95
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 22:36:57 +0000

    Makes the cross fade duration depend upon the window sizes. This effectively gives the old logic for determining time.
    BUG=none
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/10916340
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157227 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_manager2.h
+++ ash/wm/workspace/workspace_manager2.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/weak_ptr.h"
+#include "base/time.h"
 #include "base/timer.h"
 #include "ui/base/ui_base_types.h"
 
@@ -208,6 +209,9 @@ class ASH_EXPORT WorkspaceManager2
   // exits (as the last frame we draw before exiting is a frame from the
   // animation).
   bool app_terminating_;
+
+  // If non-empty this is the amount of time animating the desktop in/out.
+  base::TimeDelta switch_duration_;
 
   DISALLOW_COPY_AND_ASSIGN(WorkspaceManager2);
 };
