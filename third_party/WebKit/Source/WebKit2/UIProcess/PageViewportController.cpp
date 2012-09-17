b47e2fd0e5ae990ab9389d4e8e58f4505069a12e
vestbo@webkit.org <vestbo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-17 09:55:29 +0000

    [Qt] Remove 'using namespace WebCore' from header file
    Broke the build on Mac OS X by causing clashes between Fixed from
    /usr/include/MacTypes.h and Source/WebCore/platform/Length.h.
    
    Reviewed by Simon Hausmann.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128735 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.cpp
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/FloatSize.h>
 #include <wtf/MathExtras.h>
 
+using namespace WebCore;
+
 namespace WebKit {
 
 static inline float bound(float min, float value, float max)
--- third_party/WebKit/Source/WebKit2/UIProcess/qt/PageViewportControllerClientQt.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/qt/PageViewportControllerClientQt.cpp
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/FloatRect.h>
 #include <WebCore/FloatSize.h>
 
+using namespace WebCore;
+
 namespace WebKit {
 
 static const int kScaleAnimationDurationMillis = 250;
