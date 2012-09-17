84513b0089c1db0a23e2e0da232916d2c1c29c38
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 18:15:13 +0000

    Replace talk_base::scoped_refptr with Chromes scoped_refptr where possible.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10933123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157150 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_extra_data.h
+++ content/renderer/media/media_stream_extra_data.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_MEDIA_MEDIA_STREAM_EXTRA_DATA_H_
 
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
 #include "content/common/content_export.h"
-#include "third_party/libjingle/source/talk/base/scoped_ref_ptr.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebMediaStreamDescriptor.h"
 
 namespace webrtc {
@@ -30,8 +30,8 @@ class CONTENT_EXPORT MediaStreamExtraData
   }
 
  private:
-  talk_base::scoped_refptr<webrtc::MediaStreamInterface> remote_stream_;
-  talk_base::scoped_refptr<webrtc::LocalMediaStreamInterface> local_stream_;
+  scoped_refptr<webrtc::MediaStreamInterface> remote_stream_;
+  scoped_refptr<webrtc::LocalMediaStreamInterface> local_stream_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaStreamExtraData);
 };
--- content/renderer/media/media_stream_impl.h
+++ content/renderer/media/media_stream_impl.h
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_view_observer.h"
 #include "content/renderer/media/media_stream_dispatcher_eventhandler.h"
 #include "third_party/libjingle/source/talk/app/webrtc/mediastream.h"
-#include "third_party/libjingle/source/talk/base/scoped_ref_ptr.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebUserMediaClient.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebUserMediaRequest.h"
 #include "webkit/media/media_stream_client.h"
@@ -125,8 +124,7 @@ class CONTENT_EXPORT MediaStreamImpl
   // We keep a list of the label and WebFrame of generated local media streams,
   // so that we can stop them when needed.
   typedef std::map<std::string, WebKit::WebFrame*> LocalNativeStreamMap;
-  typedef talk_base::scoped_refptr<webrtc::LocalMediaStreamInterface>
-      LocalNativeStreamPtr;
+  typedef scoped_refptr<webrtc::LocalMediaStreamInterface> LocalNativeStreamPtr;
 
   scoped_refptr<media::VideoDecoder> CreateLocalVideoDecoder(
       webrtc::MediaStreamInterface* stream,
