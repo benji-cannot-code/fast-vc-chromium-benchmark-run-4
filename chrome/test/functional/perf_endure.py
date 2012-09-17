37bce863b18d71d4696b000b57ce93d08cdc6cbe
dennisjeffrey@chromium.org <dennisjeffrey@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 22:52:38 +0000

    Fix Chrome Endure Gmail test testGmailExpandCollapseConversation to account for UI change.
    A change in the "style" attribute of two DOM nodes in Gmail caused the Chrome
    Endure Gmail test testGmailExpandCollapseConversation to fail by not finding
    the DOM nodes.  This CL fixes the problem so the test continues to work again.
    
    BUG=None
    TEST=Verified the test fails immediately without this change, but then passes
    (at least when the test is run for a short time) with the change.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/10916351
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157233 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/perf_endure.py
+++ chrome/test/functional/perf_endure.py
@@ -1048,7 +1048,7 @@ class ChromeEndureGmailTest(ChromeEndureBaseTest):
       self._ClickElementAndRecordLatency(expand, test_description, 'ExpandAll')
       self.WaitForDomNode(
           '//img[@alt="Expand all"]/parent::*/parent::*/parent::*'
-          '[@style="display: none; "]',
+          '[@style="display: none;"]',
           frame_xpath=self._FRAME_XPATH)
       time.sleep(1)
 
@@ -1061,7 +1061,7 @@ class ChromeEndureGmailTest(ChromeEndureBaseTest):
       collapse.click()
       self.WaitForDomNode(
           '//img[@alt="Collapse all"]/parent::*/parent::*/parent::*'
-          '[@style="display: none; "]',
+          '[@style="display: none;"]',
           frame_xpath=self._FRAME_XPATH)
       time.sleep(1)
 
