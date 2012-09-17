4c5531db08902f40fcae7018c41355c25d9be56b
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-17 17:13:44 +0000

    [Chromium] IndexedDB: Remove legacy two-phase open() API members https://bugs.webkit.org/show_bug.cgi?id=96802
    Reviewed by Tony Chang.
    
    Following http://webkit.org/b/90411 and subsequent cleanup on the Chromium side,
    these entry points are no longer needed.
    
    * public/WebIDBDatabase.h: Delete old second-phase open(db-callbacks)
    * public/WebIDBFactory.h: Delete old first-phase open() w/o db-callbacks
    * src/WebIDBDatabaseImpl.cpp: No longer need to account for a close between phases.
    (WebKit::WebIDBDatabaseImpl::WebIDBDatabaseImpl):
    (WebKit::WebIDBDatabaseImpl::close):
    (WebKit::WebIDBDatabaseImpl::forceClose):
    * src/WebIDBDatabaseImpl.h:
    (WebIDBDatabaseImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128773 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebIDBDatabaseImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebIDBDatabaseImpl.cpp
@@ -49,7 +49,6 @@ namespace WebKit {
 WebIDBDatabaseImpl::WebIDBDatabaseImpl(PassRefPtr<IDBDatabaseBackendInterface> databaseBackend, WTF::PassRefPtr<IDBDatabaseCallbacksProxy> databaseCallbacks)
     : m_databaseBackend(databaseBackend)
     , m_databaseCallbacks(databaseCallbacks)
-    , m_closePending(false)
 {
 }
 
@@ -97,19 +96,15 @@ void WebIDBDatabaseImpl::close()
 {
     // Use the callbacks passed in to the constructor so that the backend in
     // multi-process chromium knows which database connection is closing.
-    if (!m_databaseCallbacks) {
-        m_closePending = true;
+    if (!m_databaseCallbacks)
         return;
-    }
     m_databaseBackend->close(m_databaseCallbacks.release());
 }
 
 void WebIDBDatabaseImpl::forceClose()
 {
-    if (!m_databaseCallbacks) {
-        m_closePending = true;
+    if (!m_databaseCallbacks)
         return;
-    }
     RefPtr<IDBDatabaseCallbacksProxy> callbacks = m_databaseCallbacks.release();
     m_databaseBackend->close(callbacks);
     callbacks->onForcedClose();
