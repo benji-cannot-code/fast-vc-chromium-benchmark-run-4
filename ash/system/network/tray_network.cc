123a1496efe876f5e5edba480d1b40c84b432272
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-17 16:46:25 +0000

    Fix the crash issue caused by toggling wifi button.
    BUG=149179
    
    Review URL: https://codereview.chromium.org/10910295
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157128 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/network/tray_network.cc
+++ ash/system/network/tray_network.cc
@@ -360,6 +360,9 @@ class NetworkListDetailedView : public NetworkDetailedView,
     bool needs_relayout = false;
     views::View* highlighted_view = NULL;
 
+    if (service_path_map_.empty())
+      scroll_content()->RemoveAllChildViews(true);
+
     for (size_t i = 0; i < network_list_.size(); ++i) {
       std::map<std::string, HoverHighlightView*>::const_iterator it =
           service_path_map_.find(network_list_[i].service_path);
@@ -427,6 +430,8 @@ class NetworkListDetailedView : public NetworkDetailedView,
   }
 
   void RefreshNetworkScrollWithEmptyNetworkList() {
+    service_path_map_.clear();
+    network_map_.clear();
     scroll_content()->RemoveAllChildViews(true);
     HoverHighlightView* container = new HoverHighlightView(this);
     container->set_fixed_height(kTrayPopupItemHeight);
@@ -445,7 +450,6 @@ class NetworkListDetailedView : public NetworkDetailedView,
       container->AddLabel(ui::ResourceBundle::GetSharedInstance().
           GetLocalizedString(IDS_ASH_STATUS_TRAY_NETWORK_WIFI_DISABLED),
           gfx::Font::NORMAL);
-      AddChildView(container);
     }
 
     scroll_content()->AddChildViewAt(container, 0);
