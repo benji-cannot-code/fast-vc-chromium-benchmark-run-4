7b64553a8a8b5257eea209be121cfdc806360512
anilsson@rim.com <anilsson@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-18 08:17:21 +0000

    [BlackBerry] Prevent scroll adjustment of input fields when region of interest mechanism active https://bugs.webkit.org/show_bug.cgi?id=96750
    Reviewed by Antonio Gomes.
    
    The region of interest mechanism replaces the scrolling/zooming
    functionality in InputHandler::ensureFocusTextElementVisible().
    
    We introduce a new fine-grained setting for the various adjustment
    modes. The WebKit embedder can disable all scroll types in favor of the
    region of interest mechanism by using the new setting.
    
    PR #208387
    
    Reviewed internally by Mike Fenton.
    
    * WebKitSupport/AboutData.cpp:
    (BlackBerry::WebKit::configPage):
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::ensureFocusTextElementVisible):
    * WebKitSupport/InputHandler.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
@@ -1052,7 +1052,7 @@ void InputHandler::ensureFocusTextElementVisible(CaretScrollType scrollType)
     if (!isActiveTextEdit() || !isInputModeEnabled() || !m_currentFocusElement->document())
         return;
 
-    if (!Platform::Settings::instance()->allowCenterScrollAdjustmentForInputFields() && scrollType != EdgeIfNeeded)
+    if (!(Platform::Settings::instance()->allowedScrollAdjustmentForInputFields() & scrollType))
         return;
 
     Frame* elementFrame = m_currentFocusElement->document()->frame();
