bb7232af718328576dc88488007427fde85d112f
pedrosimonetti@chromium.org <pedrosimonetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-18 00:23:32 +0000

    NTP5: Adding other devices to the mock tool.
    BUG=142669
    
    Review URL: https://chromiumcodereview.appspot.com/10913204
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_search/mock/mock.js
+++ chrome/browser/resources/ntp_search/mock/mock.js
@@ -3,8 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(pedrosimonetti): document how to generate the data pseudo-automatically.
-!/^chrome:\/\/./.test(location.href) && (function() {
+// If you set this variable to true, then compile and run Chrome, all messages
+// sent with chrome.send() will be intercepted and their callback data will be
+// recorded. You can later see the recorded data by executing chrome.mock()
+// on Web Developer Tools' console.
+var recordMockData = false;
+
+(recordMockData || !/^chrome:\/\/./.test(location.href)) && (function() {
 
   var __chrome__ = chrome;
   var shouldRegisterData = !!window.chrome && !!window.chrome.send;
@@ -13,11 +18,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Only messages registered in the callback map will be intercepted.
   var callbackMap = {
-    'metricsHandler:logEventTime': NO_CALLBACK,
-    'metricsHandler:recordInHistogram': NO_CALLBACK,
     'getApps': 'ntp.getAppsCallback',
+    'getForeignSessions': 'ntp.setForeignSessions',
+    'getMostVisited': 'ntp.setMostVisitedPages',
     'getRecentlyClosedTabs': 'ntp.setRecentlyClosedTabs',
-    'getMostVisited': 'ntp.setMostVisitedPages'
+    'metricsHandler:logEventTime': NO_CALLBACK,
+    'metricsHandler:recordInHistogram': NO_CALLBACK
   };
 
   // TODO(pedrosimonetti): include automatically in the recorded data
@@ -177,7 +183,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           __chrome__.send(message);
         }
       }
-    },
+    }
   };
 
   //----------------------------------------------------------------------------
