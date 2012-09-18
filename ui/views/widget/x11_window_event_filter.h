aae072d4501f24ccaedc0dddb66cdd6c395db2d0
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-18 17:40:45 +0000

    linux_aura: Start implementing DesktopRootWindowHostLinux
    This starts copying code from RootWindowHostLinux and the various x11 helper
    classes into the new central class on Ben's new architecture.
    
    Things that work:
    - Opening the inital views_examples window
    - Clicking on things
    
    Things that dont work:
    - Opening other windows
    - Closing windows
    
    BUG=146077
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10916349
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157377 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/x11_window_event_filter.h
+++ ui/views/widget/x11_window_event_filter.h
@@ -30,8 +30,7 @@ class VIEWS_EXPORT X11WindowEventFilter : public aura::EventFilter {
  public:
   explicit X11WindowEventFilter(
       aura::RootWindow* root_window,
-      aura::DesktopActivationClient* activation_client,
-      NativeWidgetAura* widget);
+      aura::DesktopActivationClient* activation_client);
   virtual ~X11WindowEventFilter();
 
   // Changes whether borders are shown on this |root_window|.
@@ -55,7 +54,6 @@ class VIEWS_EXPORT X11WindowEventFilter : public aura::EventFilter {
   bool DispatchHostWindowDragMovement(int hittest,
                                       const gfx::Point& screen_location);
 
-  NativeWidgetAura* widget_;
   aura::DesktopActivationClient* activation_client_;
 
   // The display and the native X window hosting the root window.
