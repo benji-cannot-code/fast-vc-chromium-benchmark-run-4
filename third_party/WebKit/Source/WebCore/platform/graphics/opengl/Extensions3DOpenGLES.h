aa969939e2c666594e8e32bfa7b7dade361303db
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-18 12:21:44 +0000

    Fix compilation with Qt 5 on MeeGo 1.2 Harmattan https://bugs.webkit.org/show_bug.cgi?id=96937
    Patch by Simon Hausmann <simon.hausmann@digia.com> on 2012-09-18
    Reviewed by Jocelyn Turcotte.
    
    The gl2ext.h header file on the platform is outdated. Instead use the newer copy from Qt
    through implicit inclusion of qopengl.h. Since Qt's declarations are based on newer Khronos
    headers, the multi sampling extensions do have the PROC suffix, we need the same workaround
    as QNX.
    
    * platform/graphics/opengl/Extensions3DOpenGLES.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/opengl/Extensions3DOpenGLES.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/opengl/Extensions3DOpenGLES.h
@@ -30,11 +30,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Extensions3DOpenGLCommon.h"
 
+#if PLATFORM(QT)
+// Takes care of declaring the GLES extensions.
+#include <qopengl.h>
+#else
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
+#endif
 
-#if OS(QNX)
+#if OS(QNX) || PLATFORM(QT)
 // See https://bugs.webkit.org/show_bug.cgi?id=91030.
+// Newer Khorons headers do define these with a PROC suffix, but older headers don't.
 #define PFNGLFRAMEBUFFERTEXTURE2DMULTISAMPLEIMG PFNGLFRAMEBUFFERTEXTURE2DMULTISAMPLEIMGPROC
 #define PFNGLRENDERBUFFERSTORAGEMULTISAMPLEIMG PFNGLRENDERBUFFERSTORAGEMULTISAMPLEIMGPROC
 #endif
