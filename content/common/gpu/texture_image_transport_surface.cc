a901b1d5d29f7986e781fd91e73111b6ea9f0053
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-18 20:57:08 +0000

    Use offscreen surface to make TextureImageTransportSurface current.
    This is a first step to eliminate dependencies on the parent context.
    Instead of using the UI context's surface handle, use a 1x1 offscreen surface
    to make the renderer's compositor context current. (The actual rendering
    goes to FBOs.)
    
    
    Review URL: https://chromiumcodereview.appspot.com/10916355
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157415 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/texture_image_transport_surface.cc
+++ content/common/gpu/texture_image_transport_surface.cc
@@ -89,6 +89,10 @@ bool TextureImageTransportSurface::Initialize() {
         texture.info, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   }
 
+  surface_ = gfx::GLSurface::CreateOffscreenGLSurface(false, gfx::Size(1, 1));
+  if (!surface_.get())
+    return false;
+
   if (!helper_->Initialize())
     return false;
 
@@ -105,6 +109,11 @@ void TextureImageTransportSurface::Destroy() {
     ReleaseParentStub();
   }
 
+  if (surface_.get()) {
+    surface_->Destroy();
+    surface_ = NULL;
+  }
+
   helper_->Destroy();
 }
 
@@ -113,7 +122,7 @@ bool TextureImageTransportSurface::Resize(const gfx::Size&) {
 }
 
 bool TextureImageTransportSurface::IsOffscreen() {
-  return parent_stub_ ? parent_stub_->surface()->IsOffscreen() : true;
+  return true;
 }
 
 bool TextureImageTransportSurface::OnMakeCurrent(gfx::GLContext* context) {
@@ -194,11 +203,11 @@ void* TextureImageTransportSurface::GetShareHandle() {
 }
 
 void* TextureImageTransportSurface::GetDisplay() {
-  return parent_stub_ ? parent_stub_->surface()->GetDisplay() : NULL;
+  return surface_.get() ? surface_->GetDisplay() : NULL;
 }
 
 void* TextureImageTransportSurface::GetConfig() {
-  return parent_stub_ ? parent_stub_->surface()->GetConfig() : NULL;
+  return surface_.get() ? surface_->GetConfig() : NULL;
 }
 
 void TextureImageTransportSurface::OnResize(gfx::Size size) {
@@ -336,11 +345,11 @@ gfx::Size TextureImageTransportSurface::GetSize() {
 }
 
 void* TextureImageTransportSurface::GetHandle() {
-  return parent_stub_ ? parent_stub_->surface()->GetHandle() : NULL;
+  return surface_.get() ? surface_->GetHandle() : NULL;
 }
 
 unsigned TextureImageTransportSurface::GetFormat() {
-  return parent_stub_ ? parent_stub_->surface()->GetFormat() : 0;
+  return surface_ ? surface_->GetFormat() : 0;
 }
 
 void TextureImageTransportSurface::OnSetFrontSurfaceIsProtected(
