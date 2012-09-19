c9dea6e02c80df0c2c3e2a87d4b4f78b18d6a145
yukishiino@google.com <yukishiino@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 00:19:27 +0000

    Supports stacked graphs for Endure.
    BUG=122119
    
    
    Review URL: https://chromiumcodereview.appspot.com/10914139
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157459 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/functional/perf/endure_graphs/js/dom_utils.js
@@ -0,0 +1,44 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+/*
+  Copyright (c) 2012 The Chromium Authors. All rights reserved.
+  Use of this source code is governed by a BSD-style license that can be
+  found in the LICENSE file.
+*/
+
+/**
+ * @fileoverview Collection of functions which operate on DOM.
+ */
+
+var domUtils = window['domUtils'] || {};
+
+/**
+ * Returns pageX and pageY of the given element.
+ *
+ * @param {Element} element An element of which the top-left position is to be
+ *     returned in the coordinate system of the document page.
+ * @return {Object} A point object which has {@code x} and {@code y} fields.
+ */
+domUtils.pageXY = function(element) {
+  var x = 0, y = 0;
+  for (; element; element = element.offsetParent) {
+    x += element.offsetLeft;
+    y += element.offsetTop;
+  }
+  return {'x': x, 'y': y};
+};
+
+/**
+ * Returns pageX and pageY of the given event.
+ *
+ * @param {Event} event An event of which the position is to be returned in
+ *     the coordinate system of the document page.
+ * @return {Object} A point object which has {@code x} and {@code y} fields.
+ */
+domUtils.pageXYOfEvent = function(event) {
+  return (event.pageX != null && event.pageY != null) ?
+      {'x': event.pageX, 'y': event.pageY} :
+      {'x': event.clientX + document.body.scrollLeft +
+            document.documentElement.scrollLeft,
+       'y': event.clientY + document.body.scrollTop +
+            document.documentElement.scrollTop};
+};
