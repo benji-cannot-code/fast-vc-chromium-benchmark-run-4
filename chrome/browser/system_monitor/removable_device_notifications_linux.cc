04e91d1d9827e6901cd8a552e3eeeebd29d1b2ce
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 22:52:19 +0000

    Clean up: Omit default constructor in RemoveableDeviceNotificationsLinux::MountPointInfo struct.
    Since there is no other constructors, this default constructor is not required.
    
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10939024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157639 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/system_monitor/removable_device_notifications_linux.cc
+++ chrome/browser/system_monitor/removable_device_notifications_linux.cc
@@ -232,9 +232,6 @@ void GetDeviceInfo(const FilePath& device_path, std::string* unique_id,
 
 }  // namespace
 
-RemovableDeviceNotificationsLinux::MountPointInfo::MountPointInfo() {
-}
-
 RemovableDeviceNotificationsLinux::RemovableDeviceNotificationsLinux(
     const FilePath& path)
     : initialized_(false),
