496467eeb678ef562f413bb12dbf5f342ceb5605
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 23:05:16 +0000

    Not performing default action when user taps a file (exceps file name).
    BUG=145054
    TEST=Check that tap on a file selects it, not performs the default action. Check tap on the file name performs the default action. Check double tap outside the file name performs the default action.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10938023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157644 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -983,12 +983,9 @@ FileManager.prototype = {
     // TODO(bshe): should override cr.ui.List's activateItemAtIndex function
     // rather than listen explicitly for double click or tap events.
     this.grid_.addEventListener(
-        'dblclick', this.onDetailDoubleClickOrTap_.bind(this));
+        'dblclick', this.onDetailDoubleClick_.bind(this));
     this.grid_.addEventListener(
         'click', this.onDetailClick_.bind(this));
-    this.grid_.addEventListener(
-        cr.ui.TouchHandler.EventType.TAP,
-        this.onDetailDoubleClickOrTap_.bind(this));
   };
 
   /**
@@ -1046,12 +1043,9 @@ FileManager.prototype = {
     // rather than listen explicitly for double click or tap events.
     // Don't pay attention to double clicks on the table header.
     this.table_.list.addEventListener(
-        'dblclick', this.onDetailDoubleClickOrTap_.bind(this));
+        'dblclick', this.onDetailDoubleClick_.bind(this));
     this.table_.list.addEventListener(
         'click', this.onDetailClick_.bind(this));
-    this.table_.list.addEventListener(
-        cr.ui.TouchHandler.EventType.TAP,
-        this.onDetailDoubleClickOrTap_.bind(this));
   };
 
   FileManager.prototype.onCopyProgress_ = function(event) {
@@ -2707,7 +2701,7 @@ FileManager.prototype = {
    *
    * @param {Event} event The click event.
    */
-  FileManager.prototype.onDetailDoubleClickOrTap_ = function(event) {
+  FileManager.prototype.onDetailDoubleClick_ = function(event) {
     if (this.isRenamingInProgress()) {
       // Don't pay attention to double clicks during a rename.
       return;
@@ -2742,7 +2736,7 @@ FileManager.prototype = {
 
     if (event.target.parentElement.classList.contains('filename-label') ||
         event.target.classList.contains('detail-icon')) {
-      this.onDetailDoubleClickOrTap_(event);
+      this.onDetailDoubleClick_(event);
       event.stopPropagation();
       event.preventDefault();
     }
