07e1237d832055b549b71025e006042d5b90f723
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-19 05:41:20 +0000

    [WK2][WTR] CodeGeneratorTestRunner could keep original copyright. https://bugs.webkit.org/show_bug.cgi?id=96181
    Patch by Kangil Han <kangil.han@samsung.com> on 2012-09-18
    Reviewed by Daniel Bates.
    
    This patch enabled derived files, in DerivedSources/InjectedBundle, to keep original copyright.
    
    * WebKitTestRunner/InjectedBundle/Bindings/CodeGeneratorTestRunner.pm:
    (new):
    (_parseLicenseBlock):
    (_parseLicenseBlockFromFile):
    (_defaultLicenseBlock):
    (_licenseBlock):
    (_generateHeaderFile):
    (_generateImplementationFile):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/Bindings/CodeGeneratorTestRunner.pm
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/Bindings/CodeGeneratorTestRunner.pm
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (C) 2010 Apple Inc. All rights reserved.
+# Copyright (C) 2012 Samsung Electronics
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -30,11 +31,12 @@ package CodeGeneratorTestRunner;
 
 sub new
 {
-    my ($class, $codeGenerator, $outputDir) = @_;
+    my ($class, $codeGenerator, $outputDir, $outputHeaderDir, $layerOnTop, $preprocessor, $writeDependencies, $verbose, $idlFilePath) = @_;
 
     my $reference = {
         codeGenerator => $codeGenerator,
         outputDir => $outputDir,
+        idlFilePath => $idlFilePath,
     };
 
     bless($reference, $class);
@@ -73,12 +75,37 @@ sub _classRefGetter
     return $$self{codeGenerator}->WK_lcfirst(_implementationClassName($idlType)) . "Class";
 }
 
-sub _fileHeaderString
+sub _parseLicenseBlock
 {
-    my ($filename) = @_;
+    my ($fileHandle) = @_;
 
-    # FIXME: We should pull header out of the IDL file to get the copyright
-    # year(s) right.
+    my ($copyright, $readCount, $buffer, $currentCharacter, $previousCharacter);
+    my $startSentinel = "/*";
+    my $lengthOfStartSentinel = length($startSentinel);
+    $readCount = read($fileHandle, $buffer, $lengthOfStartSentinel);
+    return "" if ($readCount < $lengthOfStartSentinel || $buffer ne $startSentinel);
+    $copyright = $buffer;
+
+    while ($readCount = read($fileHandle, $currentCharacter, 1)) {
+        $copyright .= $currentCharacter;
+        return $copyright if $currentCharacter eq "/" && $previousCharacter eq "*";
+        $previousCharacter = $currentCharacter;
+    }
+
+    return "";
+}
+
+sub _parseLicenseBlockFromFile
+{
+    my ($path) = @_;
+    open my $fileHandle, "<", $path or die "Failed to open $path for reading: $!";
+    my $licenseBlock = _parseLicenseBlock($fileHandle);
+    close($fileHandle);
+    return $licenseBlock;
+}
+
+sub _defaultLicenseBlock
+{
     return <<EOF;
 /*
  * Copyright (C) 2010 Apple Inc. All rights reserved.
@@ -107,6 +134,16 @@ sub _fileHeaderString
 EOF
 }
 
+sub _licenseBlock
+{
+    my ($self) = @_;
+    return $self->{licenseBlock} if $self->{licenseBlock};
+
+    my $licenseBlock = _parseLicenseBlockFromFile($self->{idlFilePath}) || _defaultLicenseBlock();
+    $self->{licenseBlock} = $licenseBlock;
+    return $licenseBlock;
+}
+
 sub _generateHeaderFile
 {
     my ($self, $interface) = @_;
@@ -118,7 +155,7 @@ sub _generateHeaderFile
     my $implementationClassName = _implementationClassName($idlType);
     my $filename = $className . ".h";
 
-    push(@contents, _fileHeaderString($filename));
+    push(@contents, $self->_licenseBlock());
 
     my $parentClassName = _parentClassName($interface);
 
@@ -185,7 +222,7 @@ sub _generateImplementationFile
     my $implementationClassName = _implementationClassName($idlType);
     my $filename = $className . ".cpp";
 
-    push(@contentsPrefix, _fileHeaderString($filename));
+    push(@contentsPrefix, $self->_licenseBlock());
 
     my $classRefGetter = $self->_classRefGetter($idlType);
     my $parentClassName = _parentClassName($interface);
