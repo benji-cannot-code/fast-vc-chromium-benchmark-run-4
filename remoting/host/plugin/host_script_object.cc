fc0a0693c63ffc5ed8b932ae55ea165e2c378971
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 22:08:53 +0000

    Show a helpful error message if a host domain policy blocked an attempt to use IT2Me to share a computer.
    BUG=140386
    
    
    Review URL: https://chromiumcodereview.appspot.com/10951020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157620 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/host_script_object.cc
+++ remoting/host/plugin/host_script_object.cc
@@ -73,6 +73,7 @@ const char* kAttrNameReceivedAccessCode = "RECEIVED_ACCESS_CODE";
 const char* kAttrNameConnected = "CONNECTED";
 const char* kAttrNameDisconnecting = "DISCONNECTING";
 const char* kAttrNameError = "ERROR";
+const char* kAttrNameInvalidDomainError = "INVALID_DOMAIN_ERROR";
 
 const int kMaxLoginAttempts = 5;
 
@@ -316,6 +317,9 @@ bool HostNPScriptObject::GetProperty(const std::string& property_name,
   } else if (property_name == kAttrNameError) {
     INT32_TO_NPVARIANT(kError, *result);
     return true;
+  } else if (property_name == kAttrNameInvalidDomainError) {
+    INT32_TO_NPVARIANT(kInvalidDomainError, *result);
+    return true;
   } else {
     SetException("GetProperty: unsupported property " + property_name);
     return false;
@@ -546,7 +550,7 @@ void HostNPScriptObject::FinishConnect(
   // Check the host domain policy.
   if (!required_host_domain_.empty() &&
       !EndsWith(uid, std::string("@") + required_host_domain_, false)) {
-    SetState(kError);
+    SetState(kInvalidDomainError);
     return;
   }
 
@@ -1036,7 +1040,8 @@ void HostNPScriptObject::SetState(State state) {
     case kStarting:
       DCHECK(state == kRequestedAccessCode ||
              state == kDisconnecting ||
-             state == kError) << state;
+             state == kError ||
+             state == kInvalidDomainError) << state;
       break;
     case kRequestedAccessCode:
       DCHECK(state == kReceivedAccessCode ||
@@ -1057,6 +1062,9 @@ void HostNPScriptObject::SetState(State state) {
       DCHECK(state == kDisconnected) << state;
       break;
     case kError:
+      DCHECK(state == kDisconnecting) << state;
+      break;
+    case kInvalidDomainError:
       DCHECK(state == kDisconnecting) << state;
       break;
   };
