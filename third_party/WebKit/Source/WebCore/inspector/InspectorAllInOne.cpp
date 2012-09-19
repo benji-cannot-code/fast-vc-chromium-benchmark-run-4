c668805d68544195935ef1ec92ad5924e079274e
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-19 13:50:11 +0000

    Unreviewed follow up Apple Win build fix for r128992.
    * inspector/InspectorAllInOne.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorAllInOne.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorApplicationCacheAgent.cpp"
 #include "InspectorBaseAgent.cpp"
 #include "InspectorCSSAgent.cpp"
+#include "InspectorCanvasAgent.cpp"
 #include "InspectorClient.cpp"
 #include "InspectorConsoleAgent.cpp"
 #include "InspectorController.cpp"
