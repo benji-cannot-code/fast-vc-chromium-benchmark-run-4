a4060aa26025dc7d15b92d3fe832fc0e566c7136
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-19 15:30:43 +0000

    [CSSRegions][CSSOM] Make sure all Regions APIs are not visible if CSS_REGIONS is not defined https://bugs.webkit.org/show_bug.cgi?id=96300
    Patch by Raul Hudea <rhudea@adobe.com> on 2012-09-19
    Reviewed by Yury Semikhatsky.
    
    All CSS Regions APIs should be exposed only if CSS_REGIONS is enabled
    
    No new tests because of no behavior changes.
    
    * css/StyleRule.cpp:
    (WebCore::StyleRuleBase::reportMemoryUsage): Fix compile without CSS_REGIONS enabled and added an ASSERT_NOT_REACHED as in other functions
    * dom/Element.cpp: Compile webkitRegionOverset only if CSS_REGIONS is enabled
    (WebCore):
    * dom/Element.h:
    * dom/Element.idl: Expose webkitRegionOverset only if CSS_REGIONS is enabled
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.h
+++ third_party/WebKit/Source/WebCore/dom/Element.h
@@ -427,8 +427,8 @@ public:
     PassRefPtr<RenderStyle> styleForRenderer();
 
     RenderRegion* renderRegion() const;
-    const AtomicString& webkitRegionOverset() const;
 #if ENABLE(CSS_REGIONS)
+    const AtomicString& webkitRegionOverset() const;
     Vector<RefPtr<Range> > webkitGetRegionFlowRanges() const;
 #endif
 
