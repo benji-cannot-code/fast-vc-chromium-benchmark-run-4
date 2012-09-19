fed46b94e1872a1d7dc41c688630b29f70a98b8d
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 17:04:47 +0000

    Fix crash in Extension::GetBrowserImages()
    BUG=150763
    TEST=successfully installed browser action extension without default icon defined in manifest
    
    Review URL: https://codereview.chromium.org/10945026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157560 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.cc
+++ chrome/common/extensions/extension.cc
@@ -3244,7 +3244,7 @@ std::set<FilePath> Extension::GetBrowserImages() const {
     }
   }
 
-  if (page_action()) {
+  if (page_action() && page_action()->default_icon()) {
     for (ExtensionIconSet::IconMap::const_iterator iter =
              page_action()->default_icon()->map().begin();
          iter != page_action()->default_icon()->map().end();
@@ -3253,7 +3253,7 @@ std::set<FilePath> Extension::GetBrowserImages() const {
     }
   }
 
-  if (browser_action()) {
+  if (browser_action() && browser_action()->default_icon()) {
     for (ExtensionIconSet::IconMap::const_iterator iter =
              browser_action()->default_icon()->map().begin();
          iter != browser_action()->default_icon()->map().end();
