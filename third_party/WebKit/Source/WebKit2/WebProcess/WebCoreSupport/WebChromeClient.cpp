ccf1548674e1f9deac76701f5876519fd97426ca
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-19 17:52:14 +0000

    [EFL][WK2] fast/forms/select-writing-direction-natural.html is failing https://bugs.webkit.org/show_bug.cgi?id=97082
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-09-19
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebKit2:
    
    WebChromeClient::selectItemWritingDirectionIsNatural() now returns
    true for EFL-WK2, consistently with EFL-WK1, so that the style
    is properly adjusted in RenderMenuList::adjustInnerStyle().
    
    * WebProcess/WebCoreSupport/WebChromeClient.cpp:
    (WebKit::WebChromeClient::selectItemWritingDirectionIsNatural):
    
    LayoutTests:
    
    Rebaseline fast/forms/select-writing-direction-natural.html and
    unskip it for efl-wk2.
    
    * platform/efl-wk2/TestExpectations:
    * platform/efl/fast/forms/select-writing-direction-natural-expected.png:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129019 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
@@ -659,7 +659,7 @@ void WebChromeClient::formStateDidChange(const Node*)
 
 bool WebChromeClient::selectItemWritingDirectionIsNatural()
 {
-#if PLATFORM(WIN)
+#if PLATFORM(WIN) || PLATFORM(EFL)
     return true;
 #else
     return false;
