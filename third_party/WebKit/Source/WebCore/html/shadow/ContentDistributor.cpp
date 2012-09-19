45ae76787525242f0d8e9eb2fc0a57f798392aaa
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-19 01:04:59 +0000

    [Refactoring] ContentDistributor::distributeSelectionsTo should not change ContentDistribution pool. https://bugs.webkit.org/show_bug.cgi?id=96993
    Reviewed by Dimitri Glazkov.
    
    Since we would like to reuse ContentDistribution pool, it should not be updated in
    ContentDistributor::distributeSelectionsTo. Instead, we should have Vector<bool> to indicate an element is
    distributed or not.
    
    No new tests, simple refactoring.
    
    * html/shadow/ContentDistributor.cpp:
    (WebCore::ContentDistributor::distribute):
    (WebCore::ContentDistributor::distributeSelectionsTo):
    * html/shadow/ContentDistributor.h:
    (ContentDistributor):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@128956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/ContentDistributor.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/ContentDistributor.cpp
@@ -60,6 +60,8 @@ void ContentDistributor::distribute(Element* host)
     ContentDistribution pool;
     for (Node* node = host->firstChild(); node; node = node->nextSibling())
         pool.append(node);
+    Vector<bool> distributed(pool.size());
+    distributed.fill(false);
 
     for (ShadowRoot* root = host->youngestShadowRoot(); root; root = root->olderShadowRoot()) {
         for (Node* node = root; node; node = node->traverseNextNode(root)) {
@@ -70,7 +72,7 @@ void ContentDistributor::distribute(Element* host)
                 continue;
             ShadowRoot* older = root->olderShadowRoot();
             if (point->doesSelectFromHostChildren())
-                distributeSelectionsTo(point, pool);
+                distributeSelectionsTo(point, pool, distributed);
             else if (older && !older->assignedTo()) {
                 distributeNodeChildrenTo(point, older);
                 older->setAssignedTo(point);
@@ -107,21 +109,22 @@ void ContentDistributor::finishInivalidation()
     m_validity = Invalidated;
 }
 
-void ContentDistributor::distributeSelectionsTo(InsertionPoint* insertionPoint, ContentDistribution& pool)
+void ContentDistributor::distributeSelectionsTo(InsertionPoint* insertionPoint, const ContentDistribution& pool, Vector<bool>& distributed)
 {
     ContentDistribution distribution;
     ContentSelectorQuery query(insertionPoint);
 
     for (size_t i = 0; i < pool.size(); ++i) {
-        Node* child = pool[i].get();
-        if (!child)
+        if (distributed[i])
             continue;
+
+        Node* child = pool[i].get();
         if (!query.matches(child))
             continue;
 
         distribution.append(child);
         m_nodeToInsertionPoint.add(child, insertionPoint);
-        pool[i] = 0;
+        distributed[i] = true;
     }
 
     insertionPoint->setDistribution(distribution);
