aac9a7e3d40b86bc90999f39733cf24da2a49b7f
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-19 18:56:31 +0000

    [Chromium] Remove unused PlatformSupport reference in V8DOMWindowShell https://bugs.webkit.org/show_bug.cgi?id=97117
    Reviewed by Ryosuke Niwa.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * bindings/v8/V8DOMWindowShell.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWindowShell.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWindowShell.cpp
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryUsageSupport.h"
 #include "Page.h"
 #include "PageGroup.h"
-#include "PlatformSupport.h"
 #include "RuntimeEnabledFeatures.h"
 #include "ScriptCallStack.h"
 #include "ScriptCallStackFactory.h"
