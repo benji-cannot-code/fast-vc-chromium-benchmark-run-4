afe3e65f00e43d61f0c1f903bc210b762f852fe3
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-19 22:45:35 +0000

    Plumb ClearNativeFocus to DesktopRootWindowHostWin.
    Get rid of FocusNativeView since it's only called in unittests now. Also removed from Widget.
    
    Also, don't send non client mouse events to the RootWindowHostDelegate from DesktopRootWindowHostWin (RootWindowHostWin already does this). This fixes an assert when the message eventually gets to WebInputEventFactory, which doesn't handle non client messages.
    
    BUG=146077
    Review URL: https://codereview.chromium.org/10951033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157638 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/win/hwnd_message_handler.h
+++ ui/views/win/hwnd_message_handler.h
@@ -116,7 +116,6 @@ class VIEWS_EXPORT HWNDMessageHandler : public ui::WindowImpl,
   void FlashFrame(bool flash);
 
   void ClearNativeFocus();
-  void FocusHWND(HWND hwnd);
 
   void SetCapture();
   void ReleaseCapture();
