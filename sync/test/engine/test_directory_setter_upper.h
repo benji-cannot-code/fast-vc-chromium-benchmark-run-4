8e18c871c8fedf0682b3b08f44a7bf6f8b17037c
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 22:34:12 +0000

    Remove ScopedTempDir from sync test utility class
    BUG=150907
    
    
    Review URL: https://chromiumcodereview.appspot.com/10946042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157855 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/test/engine/test_directory_setter_upper.h
+++ sync/test/engine/test_directory_setter_upper.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/scoped_temp_dir.h"
 #include "sync/test/fake_sync_encryption_handler.h"
 #include "sync/test/null_directory_change_delegate.h"
 #include "sync/util/test_unrecoverable_error_handler.h"
@@ -70,7 +69,6 @@ class TestDirectorySetterUpper {
 
   void RunInvariantCheck();
 
-  ScopedTempDir temp_dir_;
   FakeSyncEncryptionHandler encryption_handler_;
   scoped_ptr<syncable::Directory> directory_;
   std::string name_;
