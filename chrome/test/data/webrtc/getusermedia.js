d239b2594d14ed6cdc8678673a4e74ca8d59714e
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 13:32:10 +0000

    Implemented things requested from previous review of the WebRTC manual tests:
    * Added clear log button
    * Made toggling nicer
    * Made it possible to remove the local stream
    * Removed 00 test (but left the automatic test in for now)
    
    The previous patch also requested a close() button, but that is precisely what hangup does.
    
    
    BUG=
    NOTRY=True
    
    
    Review URL: https://chromiumcodereview.appspot.com/10937032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webrtc/getusermedia.js
+++ chrome/test/data/webrtc/getusermedia.js
@@ -46,7 +46,7 @@ function getUserMedia(requestVideo, requestAudio, mediaHints) {
     returnToTest('Browser does not support WebRTC.');
     return;
   }
-  gMediaHints = eval(mediaHints);
+  eval('gMediaHints = ' + mediaHints);
 
   debug('Requesting: video ' + requestVideo + ', audio ' + requestAudio);
   navigator.webkitGetUserMedia({video: requestVideo, audio: requestAudio},
@@ -100,6 +100,22 @@ function addLocalStreamToPeerConnection(peerConnection) {
         + exception);
   }
   debug('Added local stream.');
+}
+
+/**
+ * Removes the local stream from the peer connection.
+ * @param peerConnection
+ */
+function removeLocalStreamFromPeerConnection(peerConnection) {
+  if (gLocalStream == null)
+    failTest('Tried to remove local stream from peer connection,'
+        + ' but there is no stream yet.');
+  try {
+    peerConnection.removeStream(gLocalStream);
+  } catch (exception) {
+    failTest('Could not remove stream: ' + exception);
+  }
+  debug('Removed local stream.');
 }
 
 // Internals.
