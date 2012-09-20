e9c35be6b9edb98778b2592712485a4166788154
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 10:30:34 +0000

    [Mac] Website settings: Draw focus ring manually to match the custom tabs.
    BUG=149138
    
    Review URL: https://codereview.chromium.org/10944030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/website_settings_bubble_controller.mm
+++ chrome/browser/ui/cocoa/website_settings_bubble_controller.mm
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <cmath>
 
+#import <AppKit/AppKit.h>
+
 #include "base/string_number_conversions.h"
 #include "base/sys_string_conversions.h"
 #import "chrome/browser/certificate_viewer.h"
@@ -29,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/GTM/AppKit/GTMUILocalizerAndLayoutTweaker.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/gfx/scoped_ns_graphics_context_save_gstate_mac.h"
 
 namespace {
 
@@ -129,6 +132,10 @@ NSColor* IdentityVerifiedTextColor() {
   scoped_nsobject<NSImage> tabCenterImage_;
   scoped_nsobject<NSImage> tabLeftImage_;
   scoped_nsobject<NSImage> tabRightImage_;
+
+  // Key track of the index of segment which has keyboard focus. This is not
+  // the same as the currently selected segment.
+  NSInteger keySegment_;
 }
 @end
 
@@ -146,6 +153,24 @@ NSColor* IdentityVerifiedTextColor() {
         rb.GetNativeImageNamed(IDR_WEBSITE_SETTINGS_TAB_RIGHT).CopyNSImage());
   }
   return self;
+}
+
+// Called when keyboard focus in the segmented control is moved forward.
+- (void)makeNextSegmentKey {
+  [super makeNextSegmentKey];
+  keySegment_ = (keySegment_ + 1) % [self segmentCount];
+}
+
+// Called when keyboard focus in the segmented control is moved backwards.
+- (void)makePreviousSegmentKey {
+  [super makePreviousSegmentKey];
+  if (--keySegment_ < 0)
+    keySegment_ += [self segmentCount];
+}
+
+- (void)setSelectedSegment:(NSInteger)selectedSegment {
+  keySegment_ = selectedSegment;
+  [super setSelectedSegment:selectedSegment];
 }
 
 - (void)drawWithFrame:(NSRect)cellFrame inView:(NSView*)controlView {
@@ -192,6 +217,15 @@ NSColor* IdentityVerifiedTextColor() {
 
   // Call the superclass method to trigger drawing of the tab labels.
   [self drawInteriorWithFrame:cellFrame inView:controlView];
+  if ([[controlView window] firstResponder] == controlView)
+    [self drawFocusRect];
+}
+
+- (void)drawFocusRect {
+  gfx::ScopedNSGraphicsContextSaveGState scoped_state;
+  NSSetFocusRingStyle(NSFocusRingOnly);
+  [[NSColor keyboardFocusIndicatorColor] set];
+  NSFrameRect([self hitRectForSegment:keySegment_]);
 }
 
 // Return the hit rect (i.e., the visual bounds of the tab) for
