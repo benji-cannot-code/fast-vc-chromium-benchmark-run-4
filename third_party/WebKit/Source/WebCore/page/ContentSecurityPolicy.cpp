78f586e2071d1b496262e17a34b5701b2e10f1f9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-20 18:11:19 +0000

    Support paths in Content Security Policy directives. https://bugs.webkit.org/show_bug.cgi?id=89750
    Patch by Mike West <mkwst@chromium.org> on 2012-09-20
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    In CSP 1.0, paths are simply ignored: 'script-src
    http://example.com/path/to/a/file' would allow script to be loaded from
    http://example.com/path/to/a/file/javascript.js, but also from
    http://example.com/javascript.js.
    
    This patch is an experimental implementation of more granular path
    support in CSP source lists as proposed in the current editor's draft of
    CSP 1.1. Paths are treated as specifying directories in which resources
    can be found, and are implicitly terminated with a '/': in other words,
    'script-src http://a.com/path' is the same as
    'script-src http://a.com/path/'. Moreover, paths cannot contain either
    '?' or '#' characters.
    
    This is implemented outside the CSP_NEXT flag. All ports will be
    effected.
    
    Spec: https://dvcs.w3.org/hg/content-security-policy/raw-file/tip/csp-specification.dev.html#matching
    
    Tests: http/tests/security/contentSecurityPolicy/source-list-parsing-paths-01.html
           http/tests/security/contentSecurityPolicy/source-list-parsing-paths-02.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::CSPSource::CSPSource):
        Store a path along with each CSP source.
    (WebCore::CSPSource::matches):
        Check the path when comparing a URL to the source.
    (WebCore::CSPSource::pathMatches):
        Compare the URL-decoded version of the resource to validate against
        the source's stored path. If the resource's path begins with the
        stored path, then it matches! If not, it doesn't.
    (CSPSource):
        Store a path along with each CSP source.
    (WebCore::CSPSourceList::parse):
        Pass a 'path' in when creating CSPSource objects.
    (WebCore::CSPSourceList::parsePath):
        Actually parse the path, flagging errors if '?' or '#' are present,
        URL-decoding the result, and ensuring that a terminal '/' is
        added if necessary.
    (WebCore::CSPSourceList::addSourceSelf):
        Ensure that 'self' sources have an empty path.
    * page/ContentSecurityPolicy.h:
        Dropping the "ignored path component" console warning.
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/source-list-parsing-05-expected.txt:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-05.html:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-06-expected.txt:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-06.html:
        The behavior of these tests changes based on the new functionality.
    * http/tests/security/contentSecurityPolicy/source-list-parsing-paths-01-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/source-list-parsing-paths-01.html: Added.
    * http/tests/security/contentSecurityPolicy/source-list-parsing-paths-02-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/source-list-parsing-paths-02.html: Added.
        New tests for various path cases.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
@@ -72,6 +72,11 @@ bool isSourceCharacter(UChar c)
     return !isASCIISpace(c);
 }
 
+bool isPathComponentCharacter(UChar c)
+{
+    return c != '?' && c != '#';
+}
+
 bool isHostCharacter(UChar c)
 {
     return isASCIIAlphanumeric(c) || c == '-';
@@ -133,10 +138,11 @@ static void skipWhile(const UChar*& position, const UChar* end)
 
 class CSPSource {
 public:
-    CSPSource(const String& scheme, const String& host, int port, bool hostHasWildcard, bool portHasWildcard)
+    CSPSource(const String& scheme, const String& host, int port, const String& path, bool hostHasWildcard, bool portHasWildcard)
         : m_scheme(scheme)
         , m_host(host)
         , m_port(port)
+        , m_path(path)
         , m_hostHasWildcard(hostHasWildcard)
         , m_portHasWildcard(portHasWildcard)
     {
@@ -148,7 +154,7 @@ public:
             return false;
         if (isSchemeOnly())
             return true;
-        return hostMatches(url) && portMatches(url);
+        return hostMatches(url) && portMatches(url) && pathMatches(url);
     }
 
 private:
@@ -164,6 +170,16 @@ private:
             return true;
         return m_hostHasWildcard && host.endsWith("." + m_host, false);
 
+    }
+
+    bool pathMatches(const KURL& url) const
+    {
+        if (m_path.isEmpty())
+            return true;
+
+        String path = decodeURLEscapeSequences(url.path());
+
+        return path.startsWith(m_path, false);
     }
 
     bool portMatches(const KURL& url) const
@@ -190,6 +206,7 @@ private:
     String m_scheme;
     String m_host;
     int m_port;
+    String m_path;
 
     bool m_hostHasWildcard;
     bool m_portHasWildcard;
@@ -288,9 +305,7 @@ void CSPSourceList::parse(const UChar* begin, const UChar* end)
                 continue;
             if (scheme.isEmpty())
                 scheme = m_policy->securityOrigin()->protocol();
-            if (!path.isEmpty())
-                m_policy->reportIgnoredPathComponent(m_directiveName, String(beginSource, position - beginSource), path);
-            m_list.append(CSPSource(scheme, host, port, hostHasWildcard, portHasWildcard));
+            m_list.append(CSPSource(scheme, host, port, path, hostHasWildcard, portHasWildcard));
         } else
             m_policy->reportInvalidSourceExpression(m_directiveName, String(beginSource, position - beginSource));
 
@@ -475,16 +490,23 @@ bool CSPSourceList::parseHost(const UChar* begin, const UChar* end, String& host
     return true;
 }
 
-// FIXME: Deal with an actual path. This just sucks up everything to the end of the string.
 bool CSPSourceList::parsePath(const UChar* begin, const UChar* end, String& path)
 {
     ASSERT(begin <= end);
     ASSERT(path.isEmpty());
 
-    if (begin == end)
+    const UChar* position = begin;
+    skipWhile<isPathComponentCharacter>(position, end);
+    // path/to/file.js?query=string || path/to/file.js#anchor
+    //                ^                               ^
+    if (position < end)
         return false;
 
-    path = String(begin, end - begin);
+    path = decodeURLEscapeSequences(String(begin, end - begin));
+    if (!path.endsWith('/'))
+        path = path + '/';
+
+    ASSERT(position == end && path.endsWith('/'));
     return true;
 }
 
@@ -521,7 +543,7 @@ bool CSPSourceList::parsePort(const UChar* begin, const UChar* end, int& port, b
 
 void CSPSourceList::addSourceSelf()
 {
-    m_list.append(CSPSource(m_policy->securityOrigin()->protocol(), m_policy->securityOrigin()->host(), m_policy->securityOrigin()->port(), false, false));
+    m_list.append(CSPSource(m_policy->securityOrigin()->protocol(), m_policy->securityOrigin()->host(), m_policy->securityOrigin()->port(), String(), false, false));
 }
 
 void CSPSourceList::addSourceStar()
@@ -1548,12 +1570,6 @@ void ContentSecurityPolicy::reportInvalidDirectiveValueCharacter(const String& d
 void ContentSecurityPolicy::reportInvalidNonce(const String& nonce) const
 {
     String message = makeString("Ignoring invalid Content Security Policy script nonce: '", nonce, "'.\n");
-    logToConsole(message);
-}
-
-void ContentSecurityPolicy::reportIgnoredPathComponent(const String& directiveName, const String& completeSource, const String& path) const
-{
-    String message = makeString("The source list for Content Security Policy directive '", directiveName, "' contains the source '", completeSource, "'. Content Security Policy 1.0 supports only schemes, hosts, and ports. Paths might be supported in the future, but for now, '", path, "' is being ignored. Be careful.");
     logToConsole(message);
 }
 
