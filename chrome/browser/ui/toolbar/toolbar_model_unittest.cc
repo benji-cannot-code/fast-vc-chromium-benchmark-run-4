d6fe4a95e0ff006b2faf94872f00b55105026736
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 03:25:30 +0000

    Disable ToolbarModelTest.ShouldDisplayURLInstantExtendedAPIEnabled on branch
    Disables ToolbarModelTest.ShouldDisplayURLInstantExtendedAPIEnabled on branded
    branch.  These tests don't work there due to channel status with branded builds.
    
    BUG=none
    TEST=ToolbarModelTest.ShouldDisplayURLInstantExtendedAPIEnabled
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10949031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/toolbar_model_unittest.cc
+++ chrome/browser/ui/toolbar/toolbar_model_unittest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
+#include "chrome/browser/ui/search/search.h"
 #include "chrome/browser/ui/toolbar/toolbar_model.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
@@ -188,6 +189,10 @@ TEST_F(ToolbarModelTest, ShouldDisplayURLInstantExtendedAPIDisabled) {
 TEST_F(ToolbarModelTest, ShouldDisplayURLInstantExtendedAPIEnabled) {
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableInstantExtendedAPI);
+
+  // Avoid tests on branded Chrome where channel is set to CHANNEL_STABLE.
+  if (!chrome::search::IsInstantExtendedAPIEnabled(profile()))
+    return;
 
   ResetDefaultTemplateURL();
   AddTab(browser(), GURL(chrome::kAboutBlankURL));
