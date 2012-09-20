0c52d72d728d52fb55f9c572ecd363934e041dfa
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-20 02:34:43 +0000

    [BlackBerry] Basic authentication challenge credentials for stored credentials again after restarting browser https://bugs.webkit.org/show_bug.cgi?id=96362
    Patch by Sean Wang <Xuewen.Wang@torchmobile.com.cn> on 2012-09-19
    Reviewed by Rob Buis.
    
    Source/WebCore:
    
    This patch enable reading credentials from the persistent credential storage
    when it is not private browsing mode and there is not a credential in the RAM
    for the requested resource.
    
    Since we don't load persistent stored credentials into RAM at the starting time,
    even we have saved the credentials at the last browsing, after restarting the browser,
    it will still challenge for credentials for the requesting resources.
    
    No new tests, it uses the original authentication tests. There is no way to
    clear all credentials or restarting browsers to test this feature.
    
    * platform/network/blackberry/CredentialBackingStore.cpp:
    (WebCore::CredentialBackingStore::getProtectionSpace):
    (WebCore):
    * platform/network/blackberry/CredentialBackingStore.h:
    (CredentialBackingStore):
    * platform/network/blackberry/NetworkManager.cpp:
    (WebCore::NetworkManager::startJob):
    
    Source/WebKit/blackberry:
    
    Make the FrameLoaderClient use credential storage according to the macro
    BLACKBERRY_CREDENTIAL_PERSIST
    
    * WebCoreSupport/FrameLoaderClientBlackBerry.cpp:
    (WebCore::FrameLoaderClientBlackBerry::shouldUseCredentialStorage):
    (WebCore):
    * WebCoreSupport/FrameLoaderClientBlackBerry.h:
    (FrameLoaderClientBlackBerry):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129087 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/FrameLoaderClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/FrameLoaderClientBlackBerry.cpp
@@ -58,6 +58,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ProgressTracker.h"
 #include "ProtectionSpace.h"
 #include "ScopePointer.h"
+#if ENABLE(BLACKBERRY_CREDENTIAL_PERSIST)
+#include "Settings.h"
+#endif
 #include "SharedBuffer.h"
 #include "SkData.h"
 #include "SkImageEncoder.h"
@@ -1011,6 +1014,17 @@ void FrameLoaderClientBlackBerry::dispatchWillSendRequest(DocumentLoader* docLoa
         m_historyNavigationSourceURLs.add(docLoader->url());
         m_historyNavigationSourceURLs.add(docLoader->originalURL());
     }
+}
+
+bool FrameLoaderClientBlackBerry::shouldUseCredentialStorage(DocumentLoader* loader, long unsigned identifier)
+{
+#if ENABLE(BLACKBERRY_CREDENTIAL_PERSIST)
+    if (m_frame->page()->settings()->privateBrowsingEnabled())
+        return false;
+    return true;
+#else
+    return false;
+#endif
 }
 
 void FrameLoaderClientBlackBerry::loadIconExternally(const String& originalPageUrl, const String& finalPageUrl, const String& iconUrl)
