22c32726f11094ac167f3e5d2777a7d7467b3eb8
ukai@chromium.org <ukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 21:31:41 +0000

    Fix case of import path
    R=wez@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10961010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157844 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/clipboard_mac.mm
+++ remoting/host/clipboard_mac.mm
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/clipboard.h"
 
-#import <cocoa/cocoa.h>
+#import <Cocoa/Cocoa.h>
 
 #include "base/basictypes.h"
 #include "base/logging.h"
