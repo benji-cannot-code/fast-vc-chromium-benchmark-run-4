8bf424264d757ef9046cfafde95362f4e88cb902
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-20 13:40:39 +0000

    DevTools: provide the debugger detach reason in chrome.debugger.onDetach extension API.
    BUG=129539
    
    
    Review URL: https://chromiumcodereview.appspot.com/10947037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157754 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_window.cc
+++ chrome/browser/debugger/devtools_window.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/debugger/devtools_window.h"
+#include "chrome/browser/extensions/api/debugger/debugger_api.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/file_select_helper.h"
 #include "chrome/browser/prefs/pref_service.h"
@@ -629,6 +630,7 @@ DevToolsWindow* DevToolsWindow::ToggleDevToolsWindow(
   DevToolsWindow* window = AsDevToolsWindow(host);
   if (host && !window) {
     // Break remote debugging / extension debugging session.
+    DebuggerApi::MarkDevToolsClientHostAsReplaced(host);
     manager->UnregisterDevToolsClientHostFor(agent);
   }
 
