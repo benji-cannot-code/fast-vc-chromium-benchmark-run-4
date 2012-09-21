08c06886f91ad2fd366b24ba2975c74d551f077c
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-21 23:40:35 +0000

    Unreviewed, fix ARM build.
    * assembler/MacroAssemblerARMv7.h:
    (JSC::MacroAssemblerARMv7::store8):
    (MacroAssemblerARMv7):
    * offlineasm/armv7.rb:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129274 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/offlineasm/armv7.rb
+++ third_party/WebKit/Source/JavaScriptCore/offlineasm/armv7.rb
@@ -354,7 +354,7 @@ def armV7LowerMisplacedImmediates(list)
         | node |
         if node.is_a? Instruction
             case node.opcode
-            when "storei", "storep"
+            when "storeb", "storei", "storep"
                 operands = node.operands
                 newOperands = []
                 operands.each {
