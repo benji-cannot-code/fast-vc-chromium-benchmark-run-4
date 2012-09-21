61054ffde3b0d5b3b056ab08375872391063fbf4
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 19:35:25 +0000

    Aura: --disable-ui-vsync is redundant; use --disable-gpu-vsync instead
    TEST=by hand
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10970013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158047 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_context.h"
 #include "ui/gl/gl_implementation.h"
 #include "ui/gl/gl_surface.h"
+#include "ui/gl/gl_switches.h"
 #include "webkit/glue/webthread_impl.h"
 #include "webkit/gpu/webgraphicscontext3d_in_process_impl.h"
 
@@ -118,7 +119,7 @@ WebKit::WebGraphicsContext3D* DefaultContextFactory::CreateContextCommon(
   if (!offscreen) {
     context->makeContextCurrent();
     gfx::GLContext* gl_context = gfx::GLContext::GetCurrent();
-    bool vsync = !command_line->HasSwitch(switches::kDisableUIVsync);
+    bool vsync = !command_line->HasSwitch(switches::kDisableGpuVsync);
     gl_context->SetSwapInterval(vsync ? 1 : 0);
     gl_context->ReleaseCurrent(NULL);
   }
