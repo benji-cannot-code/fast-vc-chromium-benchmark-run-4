dca5e2bbb235d3cc6f8ae2b2a95ef5dedb481487
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 04:21:11 +0000

    Fix cursor getting invisible after moving a window.
    The platform cursor value was not correctly set when cursor was unlocked.
    This CL fixes it and adds unittests.
    
    BUG=150904
    
    
    Review URL: https://chromiumcodereview.appspot.com/10965020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157924 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/cursor_manager.h
+++ ash/wm/cursor_manager.h
@@ -6,21 +6,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_CURSOR_MANAGER_H_
 #define ASH_WM_CURSOR_MANAGER_H_
 
+#include "ash/ash_export.h"
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
-#include "ui/aura/aura_export.h"
 #include "ui/aura/client/cursor_client.h"
 #include "ui/gfx/native_widget_types.h"
 
 namespace ash {
+
+namespace test {
+class CursorManagerTestApi;
+}
+
 class CursorDelegate;
 class ImageCursors;
 
 // This class controls the visibility and the type of the cursor.
 // The cursor type can be locked so that the type stays the same
 // until it's unlocked.
-class CursorManager : public aura::client::CursorClient {
+class ASH_EXPORT CursorManager : public aura::client::CursorClient {
  public:
   CursorManager();
   virtual ~CursorManager();
@@ -43,6 +48,8 @@ class CursorManager : public aura::client::CursorClient {
   virtual void SetDeviceScaleFactor(float device_scale_factor) OVERRIDE;
 
  private:
+  friend class test::CursorManagerTestApi;
+
   void SetCursorInternal(gfx::NativeCursor cursor);
 
   CursorDelegate* delegate_;
@@ -69,6 +76,6 @@ class CursorManager : public aura::client::CursorClient {
   DISALLOW_COPY_AND_ASSIGN(CursorManager);
 };
 
-}  // namespace aura
+}  // namespace ash
 
 #endif  // UI_AURA_CURSOR_MANAGER_H_
