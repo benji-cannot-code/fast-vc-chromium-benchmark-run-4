b2fad7f0651ca5c5ec0ec34b18cf1228041765ed
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 03:51:16 +0000

    [filemanager] Show "view" action for PDF before the PDF plugin is loaded.
    GetPluginInfoByPath works only for already loaded plugins. This patch changes
    to use GetRegisteredPpapiPluginInfo, to that category the built-in PDF plugin
    belongs, and is available before plugin loading.
    
    BUG=147530
    TEST=Login to ChromeOS, Ctrl-M, and check the context menu of PDF has "view".
    
    Review URL: https://chromiumcodereview.appspot.com/10956007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_manager_util.cc
+++ chrome/browser/chromeos/extensions/file_manager_util.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/storage_partition.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/pepper_plugin_info.h"
 #include "grit/generated_resources.h"
 #include "net/base/escape.h"
 #include "net/base/net_util.h"
@@ -784,15 +785,16 @@ bool ShouldBeOpenedWithPdfPlugin(Profile* profile, const char* file_extension) {
   FilePath pdf_path;
   PathService::Get(chrome::FILE_PDF_PLUGIN, &pdf_path);
 
-  webkit::WebPluginInfo plugin;
-  if (!PluginService::GetInstance()->GetPluginInfoByPath(pdf_path, &plugin))
+  content::PepperPluginInfo* pepper_info =
+      PluginService::GetInstance()->GetRegisteredPpapiPluginInfo(pdf_path);
+  if (!pepper_info)
     return false;
 
   PluginPrefs* plugin_prefs = PluginPrefs::GetForProfile(profile);
   if (!plugin_prefs)
     return false;
 
-  return plugin_prefs->IsPluginEnabled(plugin);
+  return plugin_prefs->IsPluginEnabled(pepper_info->ToWebPluginInfo());
 }
 
 ListValue* ProgressStatusVectorToListValue(
