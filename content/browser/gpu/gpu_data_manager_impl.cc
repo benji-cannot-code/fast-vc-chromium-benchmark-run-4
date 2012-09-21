208f59397db41828bf2f7bdf99464e32f8ba738a
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 23:26:33 +0000

    Some extra testing for GpuDataManager.
    BUG=
    TEST=content_unittests
    Review URL: https://codereview.chromium.org/10968042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158119 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl.cc
+++ content/browser/gpu/gpu_data_manager_impl.cc
@@ -482,12 +482,10 @@ void GpuDataManagerImpl::RegisterSwiftShaderPath(const FilePath& path) {
 void GpuDataManagerImpl::EnableSoftwareRenderingIfNecessary() {
   if (!GpuAccessAllowed() ||
       (gpu_feature_type_ & content::GPU_FEATURE_TYPE_WEBGL)) {
-#if defined(ENABLE_SWIFTSHADER)
     if (!swiftshader_path_.empty() &&
         !CommandLine::ForCurrentProcess()->HasSwitch(
              switches::kDisableSoftwareRasterizer))
       software_rendering_ = true;
-#endif
   }
 }
 
@@ -503,4 +501,3 @@ void GpuDataManagerImpl::BlacklistCard() {
   EnableSoftwareRenderingIfNecessary();
   NotifyGpuInfoUpdate();
 }
-
