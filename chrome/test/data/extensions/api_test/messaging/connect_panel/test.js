1c2f90566f1029bfddc3675270bcb9120ababd8e
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 19:57:38 +0000

    [Panels] Provide a tab id for web contents in Panels.
    TBR=mihaip
    BUG=150830
    TEST=ExtensionApiTest.MessagingPanel
    
    
    Review URL: https://chromiumcodereview.appspot.com/10965023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158052 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/messaging/connect_panel/test.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var panelWindowId = 0;
+
+chrome.test.runTests([
+  function openPanelThatConnectsToExtension() {
+    chrome.test.listenOnce(chrome.extension.onConnect, function(port) {
+      chrome.test.assertEq(panelWindowId, port.sender.tab.windowId);
+      chrome.test.assertTrue(port.sender.tab.id > 0);
+    });
+    chrome.windows.create(
+        { 'url': chrome.extension.getURL('panel.html'), 'type': 'panel' },
+        chrome.test.callbackPass(function(win) {
+          chrome.test.assertEq('panel', win.type);
+          panelWindowId = win.id;
+        }));
+  }
+]);
