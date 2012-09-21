003a5ba22fc54a5a75bb543446b984dcd16d90ec
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 03:35:53 +0000

    Add field trial for enabling Stage3D in XP
    Adds an experiment that we can set to enable Stage3D on XP (where not completely blacklisted). It will be disabled by default.
    
    
    BUG=136447
    
    
    Review URL: https://chromiumcodereview.appspot.com/10944037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157910 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl.cc
+++ content/browser/gpu/gpu_data_manager_impl.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
 #include "base/file_util.h"
+#include "base/metrics/field_trial.h"
 #include "base/string_piece.h"
 #include "base/stringprintf.h"
 #include "base/sys_info.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/gpu_data_manager_observer.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/content_constants.h"
 #include "content/public/common/content_switches.h"
 #include "grit/content_resources.h"
 #include "ui/base/ui_base_switches.h"
@@ -422,6 +424,12 @@ void GpuDataManagerImpl::UpdateVideoMemoryUsageStats(
                          video_memory_usage_stats);
 }
 
+// Experiment to determine whether Stage3D should be blacklisted on XP.
+bool Stage3DBlacklisted() {
+  return base::FieldTrialList::FindFullName(content::kStage3DFieldTrialName) ==
+      content::kStage3DFieldTrialBlacklistedName;
+}
+
 void GpuDataManagerImpl::UpdateBlacklistedFeatures(
     GpuFeatureType features) {
   CommandLine* command_line = CommandLine::ForCurrentProcess();
@@ -436,6 +444,9 @@ void GpuDataManagerImpl::UpdateBlacklistedFeatures(
   if (card_blacklisted_ ||
       command_line->HasSwitch(switches::kBlacklistWebGL)) {
     flags |= content::GPU_FEATURE_TYPE_WEBGL;
+  }
+  if (Stage3DBlacklisted()) {
+    flags |= content::GPU_FEATURE_TYPE_FLASH_STAGE3D;
   }
   gpu_feature_type_ = static_cast<GpuFeatureType>(flags);
 
