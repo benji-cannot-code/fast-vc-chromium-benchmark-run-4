7d704b86b01d7849b92c6aae88554c81e6f81181
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-21 13:16:17 +0000

    [WK2][WKTR] EventSender needs to implement scheduleAsynchronousClick https://bugs.webkit.org/show_bug.cgi?id=97326
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-09-21
    Reviewed by Kenneth Rohde Christiansen.
    
    Tools:
    
    Implement scheduleAsynchronousClick() in WebKitTestRunner's
    EventSender by sending a "MouseDown" and a "MouseUp" message
    asynchronously to the WebProcess.
    
    * WebKitTestRunner/InjectedBundle/Bindings/EventSendingController.idl:
    * WebKitTestRunner/InjectedBundle/EventSendingController.cpp:
    (WTR::createMouseMessageBody):
    (WTR):
    (WTR::EventSendingController::mouseDown):
    (WTR::EventSendingController::mouseUp):
    (WTR::EventSendingController::scheduleAsynchronousClick):
    * WebKitTestRunner/InjectedBundle/EventSendingController.h:
    (EventSendingController):
    * WebKitTestRunner/TestController.cpp:
    (WTR::TestController::didReceiveMessageFromInjectedBundle):
    
    LayoutTests:
    
    Unskip fast/events/popup-blocking-click-in-iframe.html for
    WebKit2 now that WKTR's EventSender implements
    scheduleAsynchronousClick.
    
    * platform/efl-wk2/TestExpectations:
    * platform/gtk-wk2/TestExpectations:
    * platform/mac-wk2/Skipped:
    * platform/qt-5.0-wk2/Skipped:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129220 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/EventSendingController.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/EventSendingController.h
@@ -54,6 +54,7 @@ public:
     void mouseScrollBy(int x, int y);
     void continuousMouseScrollBy(int x, int y, bool paged);
     void leapForward(int milliseconds);
+    void scheduleAsynchronousClick();
 
     void keyDown(JSStringRef key, JSValueRef modifierArray, int location);
 
