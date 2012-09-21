658f46c486ca283dbdfe95483c9af9ed924e0c66
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 03:46:45 +0000

    cros: Fix crash in AppNonClientFrameViewAura::ControlView::ButtonPressed
    Restoring the window via a keyboard shortcut will result in the
    AppNonClientFrameViewAura being deleted but the widget with the
    restore/close buttons still visible. The widget's view will have
    an owner_ pointer to deleted memory, then dereference it if the
    buttons are clicked.
    
    Crash was introduced by the fix to crbug.com/147635 at crrev.com/156152
    
    BUG=149938
    TEST=added AppNonClientFrameViewAuraTest.SnapLeftClosesControls
    
    Review URL: https://chromiumcodereview.appspot.com/10961027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@157918 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/app_non_client_frame_view_aura_browsertest.cc
+++ chrome/browser/ui/views/frame/app_non_client_frame_view_aura_browsertest.cc
@@ -17,6 +17,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/event_generator.h"
 #include "ui/aura/window.h"
 
+using aura::Window;
+
+namespace {
+
+bool HasChildWindowNamed(Window* window, const char* name) {
+  for (size_t i = 0; i < window->children().size(); ++i) {
+    Window* child = window->children()[i];
+    if (child->name() == name)
+      return true;
+  }
+  return false;
+}
+
+}  // namespace
+
 class AppNonClientFrameViewAuraTest : public InProcessBrowserTest {
  public:
   AppNonClientFrameViewAuraTest() : InProcessBrowserTest(), app_browser_(NULL) {
@@ -94,4 +109,24 @@ IN_PROC_BROWSER_TEST_F(AppNonClientFrameViewAuraTest, KeyboardClose) {
 
   // App browser is closed.
   EXPECT_EQ(1u, browser::GetBrowserCount(browser()->profile()));
+}
+
+// Ensure that snapping left with Alt-[ closes the control window.
+IN_PROC_BROWSER_TEST_F(AppNonClientFrameViewAuraTest, SnapLeftClosesControls) {
+  aura::RootWindow* root_window = GetRootWindow();
+  aura::test::EventGenerator eg(root_window);
+  aura::Window* native_window = app_browser()->window()->GetNativeWindow();
+
+  // Control window exists.
+  EXPECT_TRUE(HasChildWindowNamed(
+      native_window, AppNonClientFrameViewAura::kControlWindowName));
+
+  // Send Alt-[
+  eg.PressKey(ui::VKEY_OEM_4, ui::EF_ALT_DOWN);
+  eg.ReleaseKey(ui::VKEY_OEM_4, ui::EF_ALT_DOWN);
+  content::RunAllPendingInMessageLoop();
+
+  // Control window is gone.
+  EXPECT_FALSE(HasChildWindowNamed(
+      native_window, AppNonClientFrameViewAura::kControlWindowName));
 }
