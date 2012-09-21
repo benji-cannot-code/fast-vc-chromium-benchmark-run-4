c24d8177f14477fd2e55265446b173bd695ff0cd
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-21 21:29:08 +0000

    Simplified Launcher Auto-Hide behavior, now has two behavior modes (always hide, never hide) defaulting to have the launcher always visible.
    BUG=146773
    
    Review URL: https://chromiumcodereview.appspot.com/10961006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/chrome_launcher_prefs.cc
+++ chrome/browser/ui/ash/chrome_launcher_prefs.cc
@@ -36,7 +36,6 @@ namespace ash {
 const char kPinnedAppsPrefAppIDPath[] = "id";
 
 const char kShelfAutoHideBehaviorAlways[] = "Always";
-const char kShelfAutoHideBehaviorDefault[] = "Default";
 const char kShelfAutoHideBehaviorNever[] = "Never";
 
 extern const char kShelfAlignmentBottom[] = "Bottom";
@@ -53,7 +52,7 @@ void RegisterChromeLauncherUserPrefs(PrefService* user_prefs) {
                                CreateDefaultPinnedAppsList(),
                                PrefService::SYNCABLE_PREF);
   user_prefs->RegisterStringPref(prefs::kShelfAutoHideBehavior,
-                                 kShelfAutoHideBehaviorDefault,
+                                 kShelfAutoHideBehaviorNever,
                                  PrefService::SYNCABLE_PREF);
   user_prefs->RegisterStringPref(prefs::kShelfAlignment,
                                  kShelfAlignmentBottom,
