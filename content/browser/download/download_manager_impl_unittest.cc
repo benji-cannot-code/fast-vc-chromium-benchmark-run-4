bfd3cc4eda6fd0aa5b714392ff9ed913aed0562d
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-22 01:03:47 +0000

    Avoid accessing uninitialized memory during DownloadManagerTest.
    BUG=150705
    
    
    Review URL: https://chromiumcodereview.appspot.com/10961030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158144 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/download_manager_impl_unittest.cc
+++ content/browser/download/download_manager_impl_unittest.cc
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using ::testing::AllOf;
 using ::testing::DoAll;
 using ::testing::Eq;
-using ::testing::Field;
-using ::testing::Pointee;
 using ::testing::Ref;
 using ::testing::Return;
 using ::testing::ReturnRef;
@@ -59,6 +57,13 @@ class ByteStreamReader;
 }
 
 namespace {
+
+// Matches a DownloadCreateInfo* that points to the same object as |info| and
+// has a |default_download_directory| that matches |download_directory|.
+MATCHER_P2(DownloadCreateInfoWithDefaultPath, info, download_directory, "") {
+  return arg == info &&
+      arg->default_download_directory == download_directory;
+}
 
 class MockDownloadItemImpl : public DownloadItemImpl {
  public:
@@ -591,12 +596,11 @@ TEST_F(DownloadManagerTest, StartDownload) {
       .WillOnce(Return(true));
   EXPECT_CALL(GetMockDownloadManagerDelegate(), GetSaveDir(_,_,_,_))
       .WillOnce(SetArgPointee<2>(download_path));
+
   // The CreateDownloadFile call should specify a DownloadCreateInfo that
   // includes the result of the GetSaveDir() call.
   EXPECT_CALL(GetMockDownloadFileManager(), MockCreateDownloadFile(
-      AllOf(Eq(info.get()),
-            Pointee(Field(&DownloadCreateInfo::default_download_directory,
-                          download_path))),
+      DownloadCreateInfoWithDefaultPath(info.get(), download_path),
       static_cast<content::ByteStreamReader*>(NULL),
       download_manager_.get(), true, _, _));
 
