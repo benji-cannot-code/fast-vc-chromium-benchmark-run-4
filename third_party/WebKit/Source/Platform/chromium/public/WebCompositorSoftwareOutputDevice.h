7424c91d0935d4f9f43acc9c8262ca8a8df6e681
aelias@chromium.org <aelias@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-22 00:53:53 +0000

    [chromium] Forward-declare WebSize as a struct https://bugs.webkit.org/show_bug.cgi?id=97381
    Reviewed by James Robinson.
    
    The mismatched "class" forward-declaration for WebSize in this file
    will cause a Clang error when it's included in Chromium.
    
    * chromium/public/WebCompositorSoftwareOutputDevice.h:
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebCompositorSoftwareOutputDevice.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebCompositorSoftwareOutputDevice.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebImage;
-class WebSize;
+struct WebSize;
 
 // This is a "tear-off" class providing software drawing support to
 // WebCompositorOutputSurface, such as to a platform-provided window
