e6569f108185543ec62501544f5586e961a0b573
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-22 06:17:02 +0000

    app_list: Fix app_list_unittests crash.
    - Fix crash when enabling layers;
    - Fix crash when AppsGridView is tested without host widget;
    
    BUG=none.
    TEST=app_list_unittests should pass without crash.
    
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/10963040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158175 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/test/app_list_test_suite.cc
+++ ui/app_list/test/app_list_test_suite.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ui_base_paths.h"
 #include "ui/base/resource/resource_bundle.h"
+#include "ui/compositor/compositor_setup.h"
+#include "ui/compositor/test/compositor_test_support.h"
 
 namespace app_list {
 namespace test {
@@ -23,9 +25,13 @@ void AppListTestSuite::Initialize() {
   // Force unittests to run using en-US so if we test against string
   // output, it'll pass regardless of the system language.
   ui::ResourceBundle::InitSharedInstanceWithLocale("en-US", NULL);
+
+  ui::CompositorTestSupport::Initialize();
+  ui::SetupTestCompositor();
 }
 
 void AppListTestSuite::Shutdown() {
+  ui::CompositorTestSupport::Terminate();
   ui::ResourceBundle::CleanupSharedInstance();
 
   base::TestSuite::Shutdown();
