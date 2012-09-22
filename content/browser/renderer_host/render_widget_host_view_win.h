a2ab996363e178bb360ed32c54445b30bfaa1875
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-22 06:16:33 +0000

    Disable force focus call from pointer events in Chrome/Win/Desktop
    This code was added in an attempt to make the on-screen-keyboard function in desktop chrome. By coincidence, it messes up popup menus/drop-down boxes. Removing the code.
    
    Issue 105637 tracks the OSK issue. We'll need to revisit/re-enable this code when we work on that.
    
    BUG=139420
    
    Review URL: https://chromiumcodereview.appspot.com/10825233
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158173 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_win.h
+++ content/browser/renderer_host/render_widget_host_view_win.h
@@ -142,8 +142,6 @@ class RenderWidgetHostViewWin
     MESSAGE_HANDLER(WM_MOUSEACTIVATE, OnMouseActivate)
     MESSAGE_HANDLER(WM_GETOBJECT, OnGetObject)
     MESSAGE_HANDLER(WM_PARENTNOTIFY, OnParentNotify)
-    MESSAGE_HANDLER(WM_POINTERDOWN, OnPointerMessage)
-    MESSAGE_HANDLER(WM_POINTERUP, OnPointerMessage)
     MESSAGE_HANDLER(WM_GESTURE, OnGestureEvent)
   END_MSG_MAP()
 
@@ -328,9 +326,6 @@ class RenderWidgetHostViewWin
   LRESULT OnParentNotify(UINT message, WPARAM wparam, LPARAM lparam,
                          BOOL& handled);
 
-  // Handle the new pointer messages
-  LRESULT OnPointerMessage(UINT message, WPARAM wparam, LPARAM lparam,
-                           BOOL& handled);
   // Handle high-level touch events.
   LRESULT OnGestureEvent(UINT message, WPARAM wparam, LPARAM lparam,
                          BOOL& handled);
