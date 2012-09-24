1aa789a5f1b0588d75c61a08789748877fa71c0c
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 21:46:31 +0000

    Revert 158366 - Cleanups in Chromoting Host.
    1. Removed constants.[h|cc]
    2. Removed platform if-defs in remoting_me2me_host.cc where they are not necessary.
    
    Review URL: https://codereview.chromium.org/10913260
    
    TBR=sergeyu@chromium.org
    Review URL: https://codereview.chromium.org/10988004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158400 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/signaling_connector.cc
+++ remoting/host/signaling_connector.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "net/url_request/url_fetcher.h"
 #include "remoting/host/chromoting_host_context.h"
+#include "remoting/host/constants.h"
 #include "remoting/host/dns_blackhole_checker.h"
 #include "remoting/host/url_request_context.h"
 
