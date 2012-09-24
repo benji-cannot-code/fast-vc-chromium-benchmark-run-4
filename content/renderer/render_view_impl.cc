bc7895c9684bb496b5347c55f3c27e7fb3271f35
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 18:59:55 +0000

    Adding the implementation of the StreamTextureFactoryImpl for android
    This should allow embedded video to work properly
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/10960039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158337 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2512,8 +2512,17 @@ WebMediaPlayer* RenderViewImpl::createMediaPlayer(
 
   const CommandLine* cmd_line = CommandLine::ForCurrentProcess();
 #if defined(OS_ANDROID)
-  // TODO(qinmin): upstream the implementation of getting WebGraphicsContext3D
-  // and GpuChannelHost here to replace the NULL params.
+  WebGraphicsContext3D* resource_context =
+      GetWebView()->sharedGraphicsContext3D();
+
+  GpuChannelHost* gpu_channel_host =
+      RenderThreadImpl::current()->EstablishGpuChannelSync(
+          content::CAUSE_FOR_GPU_LAUNCH_VIDEODECODEACCELERATOR_INITIALIZE);
+  if (!gpu_channel_host) {
+    LOG(ERROR) << "Failed to establish GPU channel for media player";
+    return NULL;
+  }
+
   if (cmd_line->HasSwitch(switches::kMediaPlayerInRenderProcess)) {
     if (!media_bridge_manager_.get()) {
       media_bridge_manager_.reset(
@@ -2526,7 +2535,7 @@ WebMediaPlayer* RenderViewImpl::createMediaPlayer(
         media_player_manager_.get(),
         media_bridge_manager_.get(),
         new content::StreamTextureFactoryImpl(
-            NULL, NULL, routing_id_),
+            resource_context, gpu_channel_host, routing_id_),
         cmd_line->HasSwitch(switches::kDisableMediaHistoryLogging));
   }
   if (!media_player_proxy_) {
@@ -2539,7 +2548,7 @@ WebMediaPlayer* RenderViewImpl::createMediaPlayer(
       media_player_manager_.get(),
       media_player_proxy_,
       new content::StreamTextureFactoryImpl(
-          NULL, NULL, routing_id_));
+          resource_context, gpu_channel_host, routing_id_));
 #endif
 
   media::MessageLoopFactory* message_loop_factory =
