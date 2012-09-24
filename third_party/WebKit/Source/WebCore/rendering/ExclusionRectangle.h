416d000834f9732e49138847bc3bc55dcd02995f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-24 21:18:54 +0000

    [CSS Exclusions] ExclusionShape API should use logical coordinates for input/output https://bugs.webkit.org/show_bug.cgi?id=96156
    Patch by Hans Muller <hmuller@adobe.com> on 2012-09-24
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    Correct how ExclusionShapes deal with logical coordinates and enable
    shape-inside exclusion layout for vertical writing-modes.
    
    BasicShape's are defined in physical cooordinates, incoming line and box
    dimensions are logical coordinates, and the ExclusionShape internals assume
    that lines are aligned with the Y axis. The createExclusionShape() method
    is responsible for converting the BasicShape to the internal coordinate
    system when the writing-mode is vertical. Similarly, the getInclude,ExcludedIntervals()
    methods are responsible for converting their logical line parameters to Y
    values in the internal coordinate system. The min,maxYForLogicalLine()
    methods do the conversion, based on the WritingMode the ExclusionShape
    was created with. The getInclude,ExcludedIntervals() methods return the
    logical left and right edges of line segments. No transformation is needed for this.
    
    The ExclusionShape's internal coordinate system is essentially the
    "logical" one, except that top/bottom always map to Y, no matter what
    the writing-mode is.  This is just to simplify writing geometrical shape
    algorithms, notably the complex ones for polygons. The bug report includes a
    pair of diagrams that clarify how internal coordinates are related to logical
    and physical coordinates.
    
    Test: fast/exclusions/shape-inside/shape-inside-vertical-text.html
    
    * rendering/ExclusionRectangle.cpp:
    (WebCore::ExclusionRectangle::getExcludedIntervals): rename more accurately reflects return value
    (WebCore::ExclusionRectangle::getIncludedIntervals): rename for consistency with "excluded" version
    * rendering/ExclusionRectangle.h:
    * rendering/ExclusionShape.cpp:
    (WebCore):
    (WebCore::ExclusionShape::createExclusionShape):
    * rendering/ExclusionShape.h:
    (LineSegment):
    (WebCore::LineSegment::LineSegment): relocated from WrapShapeInfo.h
    (WebCore):
    (ExclusionShape):
    (WebCore::ExclusionShape::minYForLogicalLine):
    (WebCore::ExclusionShape::maxYForLogicalLine):
    (WebCore::ExclusionShape::internalToLogicalBoundingBox):
    * rendering/WrapShapeInfo.cpp:
    (WebCore::WrapShapeInfo::isWrapShapeInfoEnabledForRenderBlock):
    (WebCore::WrapShapeInfo::computeShapeSize): pass writingMode to createExclusionShape()
    (WebCore::WrapShapeInfo::computeSegmentsForLine): removed short-circuit for vertical writing-modes
    * rendering/WrapShapeInfo.h:
    (WebCore):
    
    LayoutTests:
    
    Verify that shape-inside respects the writing-mode, notably the vertical writing-modes.
    
    * fast/exclusions/shape-inside/shape-inside-vertical-text-expected.html: Added.
    * fast/exclusions/shape-inside/shape-inside-vertical-text.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129411 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/ExclusionRectangle.h
+++ third_party/WebKit/Source/WebCore/rendering/ExclusionRectangle.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ExclusionRectangle_h
 
 #include "ExclusionShape.h"
+#include "FloatSize.h"
 #include <wtf/Assertions.h>
 #include <wtf/Vector.h>
 
@@ -39,20 +40,20 @@ namespace WebCore {
 
 class ExclusionRectangle : public ExclusionShape {
 public:
-    ExclusionRectangle(float x, float y, float width, float height, float rx = 0, float ry = 0)
+    ExclusionRectangle(const FloatRect& bounds, const FloatSize& radii)
         : ExclusionShape()
-        , m_x(x)
-        , m_y(y)
-        , m_width(width)
-        , m_height(height)
-        , m_rx(rx)
-        , m_ry(ry)
+        , m_x(bounds.x())
+        , m_y(bounds.y())
+        , m_width(bounds.width())
+        , m_height(bounds.height())
+        , m_rx(radii.width())
+        , m_ry(radii.height())
     {
     }
 
-    virtual FloatRect shapeLogicalBoundingBox() const OVERRIDE { return FloatRect(m_x, m_y, m_width, m_height); }
-    virtual void getOutsideIntervals(float y1, float y2, Vector<ExclusionInterval>&) const OVERRIDE;
-    virtual void getInsideIntervals(float y1, float y2, Vector<ExclusionInterval>&) const OVERRIDE;
+    virtual FloatRect shapeLogicalBoundingBox() const OVERRIDE { return internalToLogicalBoundingBox(FloatRect(m_x, m_y, m_width, m_height)); }
+    virtual void getExcludedIntervals(float logicalTop, float logicalBottom, SegmentList&) const OVERRIDE;
+    virtual void getIncludedIntervals(float logicalTop, float logicalBottom, SegmentList&) const OVERRIDE;
 
 private:
     float m_x;
