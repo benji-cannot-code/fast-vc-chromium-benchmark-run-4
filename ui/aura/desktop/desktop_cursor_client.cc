fdd0713f8f7716b2652e593bcaecdaddee0141b7
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 18:36:55 +0000

    linux-aura: Fix setting the default X cursors.
    For ChromeOS, most of the default cursors use custom images, and these are
    set from ash. For linux aura, these image cursors are not used, so use the
    default X11 cursors instead.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/10967062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158328 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/desktop/desktop_cursor_client.cc
+++ ui/aura/desktop/desktop_cursor_client.cc
@@ -6,17 +6,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/desktop/desktop_cursor_client.h"
 
 #include "ui/aura/root_window.h"
+#include "ui/base/cursor/cursor_loader.h"
 
 namespace aura {
 
 DesktopCursorClient::DesktopCursorClient(aura::RootWindow* window)
-    : root_window_(window) {
+    : root_window_(window),
+      cursor_loader_(ui::CursorLoader::Create()) {
 }
 
 DesktopCursorClient::~DesktopCursorClient() {
 }
 
 void DesktopCursorClient::SetCursor(gfx::NativeCursor cursor) {
+  cursor_loader_->SetPlatformCursor(&cursor);
   root_window_->SetCursor(cursor);
 }
 
@@ -29,7 +32,8 @@ bool DesktopCursorClient::IsCursorVisible() const {
 }
 
 void DesktopCursorClient::SetDeviceScaleFactor(float device_scale_factor) {
-  // TODO(ben|erg): Use the device scale factor set here for the cursor.
+  cursor_loader_->UnloadAll();
+  cursor_loader_->set_device_scale_factor(device_scale_factor);
 }
 
 }  // namespace aura
--- ui/aura/desktop/desktop_cursor_client.h
+++ ui/aura/desktop/desktop_cursor_client.h
@@ -7,9 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_AURA_DESKTOP_DESKTOP_CURSOR_CLIENT_H_
 
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 #include "ui/aura/aura_export.h"
-
 #include "ui/aura/client/cursor_client.h"
+
+namespace ui {
+class CursorLoader;
+}
 
 namespace aura {
 class RootWindow;
@@ -29,6 +33,7 @@ class AURA_EXPORT DesktopCursorClient : public client::CursorClient {
 
  private:
   aura::RootWindow* root_window_;
+  scoped_ptr<ui::CursorLoader> cursor_loader_;
 
   DISALLOW_COPY_AND_ASSIGN(DesktopCursorClient);
 };
