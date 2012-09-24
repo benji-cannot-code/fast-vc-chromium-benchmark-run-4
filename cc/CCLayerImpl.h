a980703069cbdd94e79fee7f1e85c69e4fcc34fc
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 21:14:50 +0000

    Revert 158362 - Support high DPI scrollbar on top level web frame.
    BUG=141389
    TEST=Run chrome with --force-device-scale-factor=2 --force-compositing-mode and verify that the top-level scrollbar is high dpi.
    
    Reverting: CCLayerTreeHostTestAtomicCommit.runMultiThread broke, this change was the only CC change in the blamelist.
    
    Review URL: https://chromiumcodereview.appspot.com/10909255
    
    TBR=flackr@chromium.org
    Review URL: https://codereview.chromium.org/10990004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158386 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/CCLayerImpl.h
+++ cc/CCLayerImpl.h
@@ -263,8 +263,6 @@ protected:
 
     void appendDebugBorderQuad(CCQuadSink&, const CCSharedQuadState*, CCAppendQuadsData&) const;
 
-    IntRect layerRectToContentRect(const WebKit::WebRect& layerRect);
-
     virtual void dumpLayerProperties(std::string*, int indent) const;
     static std::string indentString(int indent);
 
