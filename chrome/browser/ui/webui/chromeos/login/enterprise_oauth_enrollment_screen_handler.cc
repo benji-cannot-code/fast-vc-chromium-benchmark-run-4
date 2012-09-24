20c262bf7313192105b0fad9567395b7b2deac1f
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 19:20:17 +0000

    Log ChromeToMobile search and auth errors to UMA and INFO.
    Add more UMA and VLOG(0) to OnGetTokenFailure and HandleSearchResponse.
    (add supporting GoogleServiceAuthError::NUM_STATES enum value)
    (add supporting CTMService SEARCH_[SUCCESS|ERROR] enum values)
    (change existing LOG_IF(INFO, ...) to VLOG_IF(0, ...) in HandleSubmitResponse)
    
    ChromeToMobile.AuthError UMA histogram added here:
    https://chromereviews.googleplex.com/4964015
    
    BUG=102709,120941
    TEST=Additional UMA and INFO logging of any auth token generation errors.
    R=joi@chromium.org,sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/10970055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/enterprise_oauth_enrollment_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/enterprise_oauth_enrollment_screen_handler.cc
@@ -183,6 +183,8 @@ void EnterpriseOAuthEnrollmentScreenHandler::ShowAuthError(
       LOG(WARNING) << "Network error " << error.state();
       ShowEnrollmentError(NETWORK_ERROR);
       return;
+    case GoogleServiceAuthError::NUM_STATES:
+      break;
   }
   UMAFailure(policy::kMetricEnrollmentOtherFailed);
   NOTREACHED();
