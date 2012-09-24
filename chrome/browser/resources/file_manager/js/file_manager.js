2fbb7670d063d301d327eabfb042df3e5a626114
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 11:19:00 +0000

    [filemanager] Fix initial files sorting on Drive.
    If you load file manager on Drive, call to |setupCurrentDirectory_| displays empty list
    (until Drive data loads), and saves the current default sort order before using the stored one.
    
    Now we load sort order before setting the directory.
    
    BUG=131450
    TEST=See bug.
    Review URL: https://codereview.chromium.org/10960032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158253 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -425,6 +425,13 @@ FileManager.prototype = {
 
     this.summarizeSelection_();
 
+    var sortField =
+        window.localStorage['sort-field-' + this.dialogType_] ||
+        'modificationTime';
+    var sortDirection =
+        window.localStorage['sort-direction-' + this.dialogType_] || 'desc';
+    this.directoryModel_.sortFileList(sortField, sortDirection);
+
     this.setupCurrentDirectory_(true /* page loading */);
 
     var stateChangeHandler =
@@ -434,13 +441,6 @@ FileManager.prototype = {
     chrome.fileBrowserPrivate.onNetworkConnectionChanged.addListener(
         stateChangeHandler);
     stateChangeHandler();
-
-    var sortField =
-        window.localStorage['sort-field-' + this.dialogType_] ||
-        'modificationTime';
-    var sortDirection =
-        window.localStorage['sort-direction-' + this.dialogType_] || 'desc';
-    this.directoryModel_.sortFileList(sortField, sortDirection);
 
     this.refocus();
 
