75d23b47d89bc1de422957b1c4264d3460176613
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-24 21:35:13 +0000

    flex-grow should be 1 when omitted from flex shorthand https://bugs.webkit.org/show_bug.cgi?id=97480
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    We were using 0, based on an outdated version of the spec.
    
    Tests: css3/flexbox/flex-property-parsing.html
           css3/flexbox/flex-algorithm.html: New test case.
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseFlex):
    
    LayoutTests:
    
    Update tests due to default CSS value changes.
    
    * css3/flexbox/flex-algorithm-expected.txt:
    * css3/flexbox/flex-algorithm.html: Add test case using -webkit-flex: auto.
    * css3/flexbox/flex-property-parsing-expected.txt:
    * css3/flexbox/flex-property-parsing.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129414 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -5995,7 +5995,7 @@ bool CSSParser::parseFlex(CSSParserValueList* args, bool important)
     }
 
     if (flexGrow == unsetValue)
-        flexGrow = 0;
+        flexGrow = 1;
     if (flexShrink == unsetValue)
         flexShrink = 1;
     if (!flexBasis)
