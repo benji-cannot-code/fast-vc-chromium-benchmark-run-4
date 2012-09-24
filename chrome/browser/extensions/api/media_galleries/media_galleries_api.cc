e58fbeba8d8038f506f377f65b64cca0f75646c8
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-24 01:41:45 +0000

    Fix drive-by memory leak in media_galleries_api.cc.
    Review URL: https://chromiumcodereview.appspot.com/10970026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158223 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/media_galleries/media_galleries_api.cc
+++ chrome/browser/extensions/api/media_galleries/media_galleries_api.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/memory/scoped_ptr.h"
 #include "base/platform_file.h"
 #include "base/stl_util.h"
 #include "base/values.h"
@@ -115,7 +116,8 @@ void MediaGalleriesGetMediaFileSystemsFunction::ReturnGalleries(
   std::set<std::string> file_system_names;
   base::ListValue* list = new base::ListValue();
   for (size_t i = 0; i < filesystems.size(); i++) {
-    base::DictionaryValue* file_system_dict_value = new base::DictionaryValue();
+    scoped_ptr<base::DictionaryValue> file_system_dict_value(
+        new base::DictionaryValue());
 
     // Send the file system id so the renderer can create a valid FileSystem
     // object.
@@ -131,7 +133,7 @@ void MediaGalleriesGetMediaFileSystemsFunction::ReturnGalleries(
     file_system_dict_value->SetWithoutPathExpansion(
         "name", Value::CreateStringValue(filesystems[i].name));
 
-    list->Append(file_system_dict_value);
+    list->Append(file_system_dict_value.release());
 
     if (!filesystems[i].path.empty() &&
         MediaGalleriesPermission::HasReadAccess(*GetExtension())) {
