e41918e3a9f4cb76a34f1d60ab0f1d3d8a40e5dc
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-24 15:38:03 +0000

    Remove remaining WTF_DEPRECATED_STRING_OPERATORS from cpp files https://bugs.webkit.org/show_bug.cgi?id=97412
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    * css/StylePropertySet.cpp: Removed useless define.
    
    Source/WebKit/chromium:
    
    * src/WebPageSerializerImpl.cpp:
    (WebKit::WebPageSerializerImpl::openTagToString): Replaced operator+= with StringBuilder::append().
    (WebKit::WebPageSerializerImpl::endTagToString): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129371 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StylePropertySet.cpp
+++ third_party/WebKit/Source/WebCore/css/StylePropertySet.cpp
@@ -20,8 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02110-1301, USA.
  */
 
-#define WTF_DEPRECATED_STRING_OPERATORS
-
 #include "config.h"
 #include "StylePropertySet.h"
 
