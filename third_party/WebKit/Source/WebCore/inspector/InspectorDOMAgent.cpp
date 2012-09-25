72ebf7ae2b6824f36c73d428a466ab054d85a4bb
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-25 09:01:54 +0000

    Web Inspector: Relative URL Link Tooltips do not respect <base> https://bugs.webkit.org/show_bug.cgi?id=43434
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    New "baseURL" field added into the DOM.Node payload for document nodes and is made use of while resolving and completing URLs.
    
    * inspector/Inspector.json:
    * inspector/InspectorDOMAgent.cpp:
    (WebCore::documentBaseURLString):
    (WebCore):
    (WebCore::InspectorDOMAgent::buildObjectForNode):
    * inspector/front-end/AuditRules.js:
    (WebInspector.AuditRules.ImageDimensionsRule.prototype.doRun):
    (WebInspector.AuditRules.CssInHeadRule.prototype.doRun.externalStylesheetsReceived):
    (WebInspector.AuditRules.StylesScriptsOrderRule.prototype.doRun.cssBeforeInlineReceived):
    * inspector/front-end/DOMAgent.js:
    (WebInspector.DOMNode.prototype.resolveURL):
    (WebInspector.DOMDocument):
    
    LayoutTests:
    
    Added an iframe containing the <base> tag to test DOMNode.prototype.resolveURL() with a non-trivial baseURL.
    
    * inspector/elements/elements-panel-rewrite-href.html:
    * inspector/elements/elements-panel-rewrite-href-expected.txt:
    * inspector/elements/resources/elements-panel-rewrite-href-iframe.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.cpp
@@ -1177,6 +1177,11 @@ String InspectorDOMAgent::documentURLString(Document* document)
     return document->url().string();
 }
 
+static String documentBaseURLString(Document* document)
+{
+    return document->completeURL("").string();
+}
+
 PassRefPtr<TypeBuilder::DOM::Node> InspectorDOMAgent::buildObjectForNode(Node* node, int depth, NodeToIdMap* nodesMap)
 {
     int id = bind(node, nodesMap);
@@ -1241,6 +1246,7 @@ PassRefPtr<TypeBuilder::DOM::Node> InspectorDOMAgent::buildObjectForNode(Node* n
     } else if (node->isDocumentNode()) {
         Document* document = static_cast<Document*>(node);
         value->setDocumentURL(documentURLString(document));
+        value->setBaseURL(documentBaseURLString(document));
         value->setXmlVersion(document->xmlVersion());
     } else if (node->nodeType() == Node::DOCUMENT_TYPE_NODE) {
         DocumentType* docType = static_cast<DocumentType*>(node);
