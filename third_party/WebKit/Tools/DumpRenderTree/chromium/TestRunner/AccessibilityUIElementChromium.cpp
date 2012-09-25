3825d1640ebd5ae83ba53b7d5d61f20f484b9d1f
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-25 23:00:56 +0000

    AX: AccessibilityUIElementChromium.cpp won't build on Windows without wtf/StringExtras.h https://bugs.webkit.org/show_bug.cgi?id=97614
    Unreviewed, fixing windows breakage.
    
    Added wtf/StringExtras.h to the includes to fix the Chromium/Windows build.
    
    Patch by Dominic Mazzoni <dmazzoni@google.com> on 2012-09-25
    
    * DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/AccessibilityUIElementChromium.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "platform/WebRect.h"
 #include "platform/WebString.h"
 #include <wtf/Assertions.h>
+#include <wtf/StringExtras.h>
 
 using namespace WebKit;
 using namespace std;
