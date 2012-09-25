107d027f83c9a9072fcf6f18143dde2a86f3a39c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 21:37:58 +0000

    bookmarks: Remove browser_list.h include from bookmark_extension_api.cc
    BUG=144783
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/10990035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158662 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_extension_api.cc
+++ chrome/browser/bookmarks/bookmark_extension_api.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/importer/importer_host.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_paths.h"
