9abee55228a2eac9f662bfff785b4bd063e79b4a
jocelyn.turcotte@digia.com <jocelyn.turcotte@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-25 17:28:24 +0000

    [Qt] PageViewportController: Consolidate positionRangeForViewportAtScale and boundPosition https://bugs.webkit.org/show_bug.cgi?id=97222
    Reviewed by Kenneth Rohde Christiansen.
    
    Those methods are used together everywhere, merge them into a single
    clampViewportToContents method.
    
    * UIProcess/PageViewportController.cpp:
    (WebKit::PageViewportController::innerBoundedViewportScale):
    (WebKit::PageViewportController::outerBoundedViewportScale):
    (WebKit::PageViewportController::clampViewportToContents):
    (WebKit):
    (WebKit::PageViewportController::pageDidRequestScroll):
    (WebKit::PageViewportController::syncVisibleContents):
    * UIProcess/PageViewportController.h:
    (PageViewportController):
    (WebKit):
    * UIProcess/qt/PageViewportControllerClientQt.cpp:
    (WebKit::PageViewportControllerClientQt::focusEditableArea):
    (WebKit::PageViewportControllerClientQt::zoomToAreaGestureEnded):
    (WebKit::PageViewportControllerClientQt::nearestValidVisibleContentsRect):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.h
@@ -75,10 +75,9 @@ public:
     void suspendContent();
     void resumeContent();
 
-    WebCore::FloatRect positionRangeForViewportAtScale(float viewportScale) const;
-
     float innerBoundedViewportScale(float) const;
     float outerBoundedViewportScale(float) const;
+    WebCore::FloatPoint clampViewportToContents(const WebCore::FloatPoint& viewportPos, float viewportScale);
 
     bool hasSuspendedContent() const { return m_hasSuspendedContent; }
     bool hadUserInteraction() const { return m_hadUserInteraction; }
@@ -128,7 +127,6 @@ private:
 };
 
 bool fuzzyCompare(float, float, float epsilon);
-WebCore::FloatPoint boundPosition(const WebCore::FloatPoint minPosition, const WebCore::FloatPoint& position, const WebCore::FloatPoint& maxPosition);
 
 } // namespace WebKit
 
