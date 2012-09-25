5fda9b37ff84625220b498e25a2a6f2bbfb53339
gdk@chromium.org <gdk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 18:16:33 +0000

    Changing USB API buffer ownership.
    This change changes the ownership of the buffer used in the final
    transfer callback to the UsbDevice itself instead of having the USB API
    keep a reference to its own buffer.
    
    BUG=151987
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/10972018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158608 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/usb/usb_device_resource.h
+++ chrome/browser/extensions/api/usb/usb_device_resource.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/linked_ptr.h"
+#include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
 #include "chrome/browser/extensions/api/api_resource.h"
 #include "chrome/browser/usb/usb_device.h"
@@ -51,8 +52,9 @@ class UsbDeviceResource : public ApiResource {
  private:
   // Invoked by the underlying device's transfer callbacks. Indicates transfer
   // completion to the ApiResource's event notifier.
-  void TransferComplete(net::IOBuffer* buffer, const size_t length,
-                        UsbTransferStatus status);
+  void TransferComplete(UsbTransferStatus status,
+                        scoped_refptr<net::IOBuffer> buffer,
+                        size_t length);
 
   scoped_refptr<UsbDevice> device_;
 
