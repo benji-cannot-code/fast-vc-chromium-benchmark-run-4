3f06832d796a128b29717cf2ed4f053567fd9d4f
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 00:52:15 +0000

    Remove .dmg from the list of dangerous file extensions for Mac OS X.
    While here, add some that are dangerous.
    
    BUG=29287,19096
    TEST=When downloading .bash, .command, .csh, .ksh, .sh, .tcsh files,
    expect the "The file may harm your computer" warning.
    Review URL: https://codereview.chromium.org/10933082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_extensions.cc
+++ chrome/browser/download/download_extensions.cc
@@ -177,9 +177,14 @@ static const struct Executables {
   { "wsh", AllowOnUserGesture },
   { "xbap", Dangerous },
 #elif defined(OS_MACOSX)
-  // TODO(thakis): Figure out what makes sense here -- crbug.com/19096
-  { "app", AllowOnUserGesture },
-  { "dmg", AllowOnUserGesture },
+  { "bash", AllowOnUserGesture },
+  { "command", AllowOnUserGesture },
+  { "csh", AllowOnUserGesture },
+  { "ksh", AllowOnUserGesture },
+  { "pkg", AllowOnUserGesture },
+  { "sh", AllowOnUserGesture },
+  { "shar", AllowOnUserGesture },
+  { "tcsh", AllowOnUserGesture },
 #elif defined(OS_POSIX)
   // TODO(estade): lengthen this list.
   { "bash", AllowOnUserGesture },
@@ -189,6 +194,7 @@ static const struct Executables {
   { "ksh", AllowOnUserGesture },
   { "rpm", AllowOnUserGesture },
   { "sh", AllowOnUserGesture },
+  { "shar", AllowOnUserGesture },
   { "tcsh", AllowOnUserGesture },
 #endif
 };
