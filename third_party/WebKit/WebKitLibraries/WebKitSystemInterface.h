4d40d89cf0d9cd15a3db9da9bf499a26b37227ee
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-25 22:54:19 +0000

    <rdar://problem/12351906> Have DumpRenderTree and WebKitTestRunner crash logs show which test crashed
    Reviewed by Tim Horton.
    
    New WKSI libraries and header.
    
    * WebKitSystemInterface.h:
    * libWebKitSystemInterfaceLion.a:
    * libWebKitSystemInterfaceMountainLion.a:
    * libWebKitSystemInterfaceSnowLeopard.a:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129565 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
+++ third_party/WebKit/WebKitLibraries/WebKitSystemInterface.h
@@ -462,6 +462,8 @@ void WKHideWordDefinitionWindow(void);
 
 CFStringRef WKCopyDefaultSearchProviderDisplayName(void);
 
+void WKSetCrashReportApplicationSpecificInformation(CFStringRef);
+
 NSURL* WKAVAssetResolvedURL(AVAsset*);
 
 NSCursor *WKCursor(const char *name);
