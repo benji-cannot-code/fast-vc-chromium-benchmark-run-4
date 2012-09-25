369d382c49dbb8d42e950d86ddfb8c82800a5803
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-25 21:43:32 +0000

    drive: Nullify --enable-drive-v2-api temporarily
    The feature under the flag is not yet ready to be used.
    Will remove the flag from about:flags in a separate patch.
    
    BUG=152230,151957,149009,151963
    TEST=Drive on Files.app works as before (the flag was disabled by default)
    
    Review URL: https://codereview.chromium.org/10986031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158663 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/gdata_util.cc
+++ chrome/browser/google_apis/gdata_util.cc
@@ -90,9 +90,8 @@ bool IsGDataAvailable(Profile* profile) {
 }
 
 bool IsDriveV2ApiEnabled() {
-  static bool enabled = CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableDriveV2Api);
-  return enabled;
+  // TODO(kochi): Re-enable this. crbug.com/152230
+  return false;
 }
 
 base::PlatformFileError DriveFileErrorToPlatformError(
