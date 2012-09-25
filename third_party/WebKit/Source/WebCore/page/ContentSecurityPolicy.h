f70c707fb4b81227815a9671ae019ccbbfd4ba3f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-25 17:24:18 +0000

    CSP paths: Ignore invalid path components, rather than dropping the source completely. https://bugs.webkit.org/show_bug.cgi?id=97538
    Patch by Mike West <mkwst@google.com> on 2012-09-25
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Content Security Policy directives that accept source lists currently
    handle soure URI paths containing '?' or '#' in a draconian fashion by
    dropping the entire source expression. We should be slightly friendlier,
    and ignore simply the portion of the source expression that we know to
    be invalid, just as we previously ignored everything after and including
    a '/' in source expressions before we supported paths.
    
    The new behavior is covered by existing tests, and two test-cases added
    to http/tests/security/contentSecurityPolicy/source-list-parsing-paths-02.html.
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::CSPSourceList::parsePath):
        Report an error to the console, rather than ignoring a path that
        contains an invalid fragment or query.
    (WebCore::ContentSecurityPolicy::reportInvalidPathCharacter):
    (WebCore):
    * page/ContentSecurityPolicy.h:
        Add a new warning type to cover this case.
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/source-list-parsing-05-expected.txt:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-06-expected.txt:
        Updating to the new error message.
    * http/tests/security/contentSecurityPolicy/source-list-parsing-paths-02-expected.txt:
    * http/tests/security/contentSecurityPolicy/source-list-parsing-paths-02.html:
        Adding new tests to check that valid paths followed by '?' or '#'
        drop the invalid component, but remain valid sources.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
@@ -102,6 +102,7 @@ public:
 
     void reportDuplicateDirective(const String&) const;
     void reportInvalidDirectiveValueCharacter(const String& directiveName, const String& value) const;
+    void reportInvalidPathCharacter(const String& directiveName, const String& value, const char) const;
     void reportInvalidNonce(const String&) const;
     void reportInvalidPluginTypes(const String&) const;
     void reportInvalidSourceExpression(const String& directiveName, const String& source) const;
