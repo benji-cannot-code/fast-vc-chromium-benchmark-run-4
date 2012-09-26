ab77ba5036157d1d501eb0787aac2004d6556962
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-26 20:54:26 +0000

    [WK2] Update WebPageProxy::reattachToWebProcess() for multi-web process mode         https://bugs.webkit.org/show_bug.cgi?id=97726
            Reviewed by Anders Carlsson.
    
            * UIProcess/WebContext.cpp:
            (WebKit::WebContext::ensureSharedWebProcess): createNewWebProcess() now takes care
            of adding the new process to m_processes.
            (WebKit::WebContext::createNewWebProcess): Every time we call this function, we need
            to add the result to m_processes. Better do it internally to avoid mistakes.
            (WebKit::WebContext::warmInitialProcess): Updated for the above.
            (WebKit::WebContext::createWebPage): Ditto.
            (WebKit::WebContext::relaunchProcessIfNecessary): This function makes no sense in
            multi-web process mode. Assert that we didnt get here accidentally.
    
            * UIProcess/WebContext.h: (WebKit::WebContext::sendToAllProcessesRelaunchingThemIfNecessary):
            Only call relaunchProcessIfNecessary in single process mode. The functionality that
            needs this behavior will be greatly refactored for multi-web process.
    
            * UIProcess/WebPageProxy.cpp: (WebKit::WebPageProxy::reattachToWebProcess):
            We already know that we need a new process, "relaunch if necessary" is not the
            right logic. Also added a few more assertions checking that process state is
            as expected.
            
            * UIProcess/WebResourceCacheManagerProxy.cpp:
            (WebKit::WebResourceCacheManagerProxy::getCacheOrigins): Removed relaunchProcessIfNecessary().
            It will be called inside sendToAllProcessesRelaunchingThemIfNecessary().
    
            * UIProcess/Plugins/WebPluginSiteDataManager.cpp:
            (WebKit::WebPluginSiteDataManager::getSitesWithData): Ditto.
            (WebKit::WebPluginSiteDataManager::clearSiteData): Ditto.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
@@ -305,7 +305,7 @@ void WebContext::textCheckerStateChanged()
 void WebContext::ensureSharedWebProcess()
 {
     if (m_processes.isEmpty())
-        m_processes.append(createNewWebProcess());
+        createNewWebProcess();
 }
 
 PassRefPtr<WebProcessProxy> WebContext::createNewWebProcess()
@@ -367,6 +367,8 @@ PassRefPtr<WebProcessProxy> WebContext::createNewWebProcess()
         injectedBundleInitializationUserData = m_injectedBundleInitializationUserData;
     process->send(Messages::WebProcess::InitializeWebProcess(parameters, WebContextUserMessageEncoder(injectedBundleInitializationUserData.get())), 0);
 
+    m_processes.append(process);
+
     return process.release();
 }
 
@@ -377,7 +379,7 @@ void WebContext::warmInitialProcess()
         return;
     }
 
-    m_processes.append(createNewWebProcess());
+    createNewWebProcess();
     m_haveInitialEmptyProcess = true;
 }
 
@@ -515,7 +517,6 @@ PassRefPtr<WebPageProxy> WebContext::createWebPage(PageClient* pageClient, WebPa
         } else {
             // FIXME (Multi-WebProcess): <rdar://problem/12239661> Consider limiting the number of web processes in per-tab process model.
             process = createNewWebProcess();
-            m_processes.append(process);
         }
     }
 
@@ -530,10 +531,10 @@ WebProcessProxy* WebContext::relaunchProcessIfNecessary()
     if (m_processModel == ProcessModelSharedSecondaryProcess) {
         ensureSharedWebProcess();
         return m_processes[0].get();
-    } else {
-        // FIXME (Multi-WebProcess): What should this do in this model?
-        return 0;
     }
+
+    ASSERT_NOT_REACHED();
+    return 0;
 }
 
 DownloadProxy* WebContext::download(WebPageProxy* initiatingPage, const ResourceRequest& request)
--- third_party/WebKit/Source/WebKit2/UIProcess/WebResourceCacheManagerProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebResourceCacheManagerProxy.cpp
@@ -65,7 +65,6 @@ bool WebResourceCacheManagerProxy::shouldTerminate(WebProcessProxy*) const
 void WebResourceCacheManagerProxy::getCacheOrigins(PassRefPtr<ArrayCallback> prpCallback)
 {
     RefPtr<ArrayCallback> callback = prpCallback;
-    m_webContext->relaunchProcessIfNecessary();
     uint64_t callbackID = callback->callbackID();
     m_arrayCallbacks.set(callbackID, callback.release());
 
