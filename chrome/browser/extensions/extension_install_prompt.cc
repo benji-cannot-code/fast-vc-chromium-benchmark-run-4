e21ebb6b5ce491557e92a79b1ab7b6875870c7e9
jhawkins@chromium.org <jhawkins@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 17:32:25 +0000

    Revert 158590 - Extensions: Fix a crash installing an app w/ oauth2 scopes in incognito.
    Get the parent profile when fetching oauth2 issue advice.
    
    BUG=152192
    TEST=none
    R=aa
    
    Review URL: https://codereview.chromium.org/10981022
    
    TBR=jhawkins@chromium.org
    Review URL: https://codereview.chromium.org/10983042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_install_prompt.cc
+++ chrome/browser/extensions/extension_install_prompt.cc
@@ -495,7 +495,7 @@ void ExtensionInstallPrompt::FetchOAuthIssueAdviceIfNeeded() {
     return;
   }
 
-  Profile* profile = install_ui_->profile()->GetOriginalProfile();
+  Profile* profile = install_ui_->profile();
   TokenService* token_service = TokenServiceFactory::GetForProfile(profile);
 
   token_flow_.reset(new OAuth2MintTokenFlow(
