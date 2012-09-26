d2003c6212923fb1cf20ad897981df7ae6b812f1
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 20:44:56 +0000

    Switch ExternalProtocolObserver to use WebContentsUserData.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/10961053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158878 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_protocol/external_protocol_observer.cc
+++ chrome/browser/external_protocol/external_protocol_observer.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::WebContents;
 
+int ExternalProtocolObserver::kUserDataKey;
+
 ExternalProtocolObserver::ExternalProtocolObserver(WebContents* web_contents)
     : content::WebContentsObserver(web_contents) {
 }
