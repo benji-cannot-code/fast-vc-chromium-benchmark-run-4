01010027c54753ecfb9d4c25ce7deeecebd0afb8
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 21:30:02 +0000

    Disable TLS channel ID by default. (for merging to branch)
    BUG=129174
    TEST=browse to https://www.google.com, check chrome://histograms for DomainBoundCerts.Support. Then try with --enable-tls-channel-id flag.
    
    Review URL: https://codereview.chromium.org/10979040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/command_line_pref_store.cc
+++ chrome/browser/prefs/command_line_pref_store.cc
@@ -50,7 +50,7 @@ const CommandLinePrefStore::BooleanSwitchToPreferenceMapEntry
         prefs::kWebKitAllowDisplayingInsecureContent, false },
       { switches::kAllowCrossOriginAuthPrompt,
         prefs::kAllowCrossOriginAuthPrompt, true },
-      { switches::kDisableTLSChannelID, prefs::kEnableOriginBoundCerts, false },
+      { switches::kEnableTLSChannelID, prefs::kEnableOriginBoundCerts, true },
       { switches::kDisableSSLFalseStart, prefs::kDisableSSLRecordSplitting,
           true },
       { switches::kEnableMemoryInfo, prefs::kEnableMemoryInfo, true },
