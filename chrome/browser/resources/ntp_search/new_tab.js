69ceab4f693f845041d609ff867d6335f4b28a67
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 22:28:51 +0000

    1. other_sessions_page.js has been renamed to other_sessions_page.js 2. The code in this file was rewritten to reflect the fact that “other devices” is not a menu anymore, but a “sub-page” on NTP that will show rectangular tiles. 3. There is no logic for showing tiles or reacting to a mouse click; these changes will follow.
    For reference, this is the record of the code review with pedrosimonetti and jeremycho (CR 10919248).
    
    BUG=148770
    
    Review URL: https://chromiumcodereview.appspot.com/10986037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158911 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_search/new_tab.js
+++ chrome/browser/resources/ntp_search/new_tab.js
@@ -137,6 +137,12 @@ cr.define('ntp', function() {
     recentlyClosedPage: undefined,
 
     /**
+     * The Devices page.
+     * @type {!Element|undefined}
+     */
+    otherDevicesPage: undefined,
+
+    /**
      * The 'dots-list' element.
      * @type {!Element|undefined}
      */
@@ -263,6 +269,12 @@ cr.define('ntp', function() {
       if (typeof ntp.RecentlyClosedPage != 'undefined' &&
           page instanceof ntp.RecentlyClosedPage) {
         this.recentlyClosedPage = page;
+      }
+
+      // Remember special OtherDevicesPage.
+      if (typeof ntp.OtherDevicesPage != 'undefined' &&
+          page instanceof ntp.OtherDevicesPage) {
+        this.otherDevicesPage = page;
       }
 
       // Make a deep copy of the dot template to add a new one.
@@ -506,6 +518,10 @@ cr.define('ntp', function() {
           if (this.recentlyClosedPage)
             this.cardSlider.selectCardByValue(this.recentlyClosedPage);
           break;
+        case loadTimeData.getInteger('other_devices_page_id'):
+          if (this.otherDevicesPage)
+            this.cardSlider.selectCardByValue(this.otherDevicesPage);
+          break;
       }
     },
 
@@ -539,6 +555,9 @@ cr.define('ntp', function() {
         } else if (page.classList.contains('recently-closed-page')) {
           this.setShownPage_(
               loadTimeData.getInteger('recently_closed_page_id'), 0);
+        } else if (page.classList.contains('other-devices-page')) {
+          this.setShownPage_(
+              loadTimeData.getInteger('other_devices_page_id'), 0);
         } else {
           console.error('unknown page selected');
         }
@@ -690,6 +709,10 @@ cr.define('ntp', function() {
     newTabView.appendTilePage(recentlyClosed,
                               loadTimeData.getString('recentlyclosed'));
     chrome.send('getRecentlyClosedTabs');
+
+    var devices = new ntp.OtherDevicesPage();
+    newTabView.appendTilePage(devices, loadTimeData.getString('otherSessions'));
+    chrome.send('getForeignSessions');
 
     if (loadTimeData.getString('login_status_message')) {
       loginBubble = new cr.ui.Bubble;
@@ -1000,8 +1023,7 @@ cr.define('ntp', function() {
   }
 
   function setForeignSessions(sessionList, isTabSyncEnabled) {
-    // TODO(jeremycho): Support this once the Devices page is implemented.
-    console.warn('setForeignSessions not implemented.');
+    return newTabView.otherDevicesPage.setData(sessionList, isTabSyncEnabled);
   }
 
   function getAppsCallback() {
