d295d2052afd5e421d6baa7ce2176af68dcd0a30
dominik.rottsches@intel.com <dominik.rottsches@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-26 14:08:26 +0000

    [soup] Obey setTimeoutInterval in soup backend https://bugs.webkit.org/show_bug.cgi?id=94796
    Reviewed by Gustavo Noronha Silva.
    
    Implementing ResourceHandle's setTimeoutInterval support for the soup backend.
    This is preparatory work for bug 74802, timeout support for XHR2. The patch
    has been successfully tested in combination with my work-in-progress
    for that bug.
    
    No new tests yet, tests will be added with the patch in bug 74802.
    
    * platform/network/ResourceHandleInternal.h:
    (WebCore::ResourceHandleInternal::ResourceHandleInternal): Adding a timeout source.
    (ResourceHandleInternal):
    * platform/network/soup/ResourceHandleSoup.cpp:
    (WebCore):
    (WebCore::cleanupSoupRequestOperation): Resetting the timer.
    (WebCore::sendRequestCallback): Stopping the timer.
    (WebCore::startHTTPRequest): Starting a timeout timer before the async request.
    (WebCore::ResourceHandle::platformSetDefersLoading): Starting a timeout timer before the async request.
    (WebCore::requestTimeoutCallback): Preparing a corresponding resource error and notifying clients of failure.
    (WebCore::startNonHTTPRequest): Starting a timeout timer before the async request.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129636 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Copyright (C) 2009 Christian Dywan <christian@imendio.com>
  * Copyright (C) 2009, 2010, 2011 Igalia S.L.
  * Copyright (C) 2009 John Kjellberg <john.kjellberg@power.alstom.com>
+ * Copyright (C) 2012 Intel Corporation
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -71,6 +72,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 #define READ_BUFFER_SIZE 8192
+
+// Use the same value as in NSURLError.h
+static const int gTimeoutError = -1001;
 
 static bool loadingSynchronousRequest = false;
 
@@ -202,6 +206,7 @@ static void cleanupSoupRequestOperation(ResourceHandle*, bool isDestroying);
 static void sendRequestCallback(GObject*, GAsyncResult*, gpointer);
 static void readCallback(GObject*, GAsyncResult*, gpointer);
 static void closeCallback(GObject*, GAsyncResult*, gpointer);
+static gboolean requestTimeoutCallback(void*);
 static bool startNonHTTPRequest(ResourceHandle*, KURL);
 #if ENABLE(WEB_TIMING)
 static int  milisecondsSinceRequest(double requestTime);
@@ -378,6 +383,11 @@ static void cleanupSoupRequestOperation(ResourceHandle* handle, bool isDestroyin
     if (d->m_buffer) {
         g_slice_free1(READ_BUFFER_SIZE, d->m_buffer);
         d->m_buffer = 0;
+    }
+
+    if (d->m_timeoutSource) {
+        g_source_destroy(d->m_timeoutSource.get());
+        d->m_timeoutSource.clear();
     }
 
     if (!isDestroying)
@@ -755,6 +765,10 @@ static bool startHTTPRequest(ResourceHandle* handle)
 #if ENABLE(WEB_TIMING)
         d->m_response.resourceLoadTiming()->requestTime = monotonicallyIncreasingTime();
 #endif
+        if (d->m_firstRequest.timeoutInterval() > 0) {
+            // soup_add_timeout returns a GSource* whose only reference is owned by the context. We need to have our own reference to it, hence not using adoptRef.
+            d->m_timeoutSource = soup_add_timeout(g_main_context_get_thread_default(), d->m_firstRequest.timeoutInterval() * 1000, requestTimeoutCallback, handle);
+        }
         d->m_cancellable = adoptGRef(g_cancellable_new());
         soup_request_send_async(d->m_soupRequest.get(), d->m_cancellable.get(), sendRequestCallback, handle);
     }
@@ -840,9 +854,12 @@ void ResourceHandle::platformSetDefersLoading(bool defersLoading)
     if (d->m_cancelled)
         return;
 
-    // We only need to take action here to UN-defer loading.
-    if (defersLoading)
+    // Except when canceling a possible timeout timer, we only need to take action here to UN-defer loading.
+    if (defersLoading) {
+        g_source_destroy(d->m_timeoutSource.get());
+        d->m_timeoutSource.clear();
         return;
+    }
 
     // We need to check for d->m_soupRequest because the request may
     // have raised a failure (for example invalid URLs). We cannot
@@ -854,6 +871,10 @@ void ResourceHandle::platformSetDefersLoading(bool defersLoading)
             d->m_response.resourceLoadTiming()->requestTime = monotonicallyIncreasingTime();
 #endif
         d->m_cancellable = adoptGRef(g_cancellable_new());
+        if (d->m_firstRequest.timeoutInterval() > 0) {
+            // soup_add_timeout returns a GSource* whose only reference is owned by the context. We need to have our own reference to it, hence not using adoptRef.
+            d->m_timeoutSource = soup_add_timeout(g_main_context_get_thread_default(), d->m_firstRequest.timeoutInterval() * 1000, requestTimeoutCallback, this);
+        }
         soup_request_send_async(d->m_soupRequest.get(), d->m_cancellable.get(), sendRequestCallback, this);
         return;
     }
@@ -974,6 +995,20 @@ static void readCallback(GObject*, GAsyncResult* asyncResult, gpointer data)
                               d->m_cancellable.get(), readCallback, handle.get());
 }
 
+static gboolean requestTimeoutCallback(gpointer data)
+{
+    RefPtr<ResourceHandle> handle = static_cast<ResourceHandle*>(data);
+    ResourceHandleInternal* d = handle->getInternal();
+    ResourceHandleClient* client = handle->client();
+
+    ResourceError timeoutError("WebKitNetworkError", gTimeoutError, d->m_firstRequest.url().string(), "Request timed out");
+    timeoutError.setIsTimeout(true);
+    client->didFail(handle.get(), timeoutError);
+    cleanupSoupRequestOperation(handle.get());
+
+    return FALSE;
+}
+
 static bool startNonHTTPRequest(ResourceHandle* handle, KURL url)
 {
     ASSERT(handle);
@@ -1002,6 +1037,10 @@ static bool startNonHTTPRequest(ResourceHandle* handle, KURL url)
     // Send the request only if it's not been explicitly deferred.
     if (!d->m_defersLoading) {
         d->m_cancellable = adoptGRef(g_cancellable_new());
+        if (d->m_firstRequest.timeoutInterval() > 0) {
+            // soup_add_timeout returns a GSource* whose only reference is owned by the context. We need to have our own reference to it, hence not using adoptRef.
+            d->m_timeoutSource = soup_add_timeout(g_main_context_get_thread_default(), d->m_firstRequest.timeoutInterval() * 1000, requestTimeoutCallback, handle);
+        }
         soup_request_send_async(d->m_soupRequest.get(), d->m_cancellable.get(), sendRequestCallback, handle);
     }
 
