524b2a40db0b412ec1a9675ea374787f1151c2cc
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-26 18:59:41 +0000

    [cros] Move desktop background to LockScreenBackground container during OOBE.
    Also: OOBE widget isn't closed when another (not initiated by OOBE itself) browser is opened, e.g. a chat window. In combination with moving desktop background to LockScreenBackground container this allows extensions to open windows while user session hasn't started yet, e.g. on user image screen.
    
    
    BUG=145533
    
    
    Review URL: https://chromiumcodereview.appspot.com/10978021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158836 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/base_login_display_host.cc
+++ chrome/browser/chromeos/login/base_login_display_host.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/base_login_display_host.h"
 
+#include "ash/desktop_background/desktop_background_controller.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/wm/workspace_controller.h"  // temporary until w2 is the default.
@@ -135,7 +136,8 @@ BaseLoginDisplayHost::BaseLoginDisplayHost(const gfx::Rect& background_bounds)
     : background_bounds_(background_bounds),
       ALLOW_THIS_IN_INITIALIZER_LIST(pointer_factory_(this)),
       shutting_down_(false),
-      oobe_progress_bar_visible_(false) {
+      oobe_progress_bar_visible_(false),
+      session_starting_(false) {
   // We need to listen to CLOSE_ALL_BROWSERS_REQUEST but not APP_TERMINATIN
   // because/ APP_TERMINATING will never be fired as long as this keeps
   // ref-count. CLOSE_ALL_BROWSERS_REQUEST is safe here because there will be no
@@ -171,8 +173,14 @@ BaseLoginDisplayHost::~BaseLoginDisplayHost() {
 ////////////////////////////////////////////////////////////////////////////////
 // BaseLoginDisplayHost, LoginDisplayHost implementation:
 
+void BaseLoginDisplayHost::BeforeSessionStart() {
+  session_starting_ = true;
+}
+
 void BaseLoginDisplayHost::OnSessionStart() {
   DVLOG(1) << "Session starting";
+  ash::Shell::GetInstance()->
+      desktop_background_controller()->MoveDesktopToUnlockedContainer();
   if (wizard_controller_.get())
     wizard_controller_->OnSessionStart();
   // Display host is deleted once animation is completed
@@ -198,6 +206,9 @@ void BaseLoginDisplayHost::StartWizard(
   // is done before new controller creation.
   wizard_controller_.reset();
   wizard_controller_.reset(CreateWizardController());
+
+  ash::Shell::GetInstance()->
+      desktop_background_controller()->MoveDesktopToLockedContainer();
 
   oobe_progress_bar_visible_ = !WizardController::IsDeviceRegistered();
   SetOobeProgressBarVisible(oobe_progress_bar_visible_);
@@ -279,7 +290,9 @@ void BaseLoginDisplayHost::Observe(
     const content::NotificationDetails& details) {
   if (type == chrome::NOTIFICATION_CLOSE_ALL_BROWSERS_REQUEST) {
     ShutdownDisplayHost(true);
-  } else if (type == chrome::NOTIFICATION_BROWSER_OPENED) {
+  } else if (type == chrome::NOTIFICATION_BROWSER_OPENED && session_starting_) {
+    // Browsers created before session start (windows opened by extensions, for
+    // example) are ignored.
     OnBrowserCreated();
     registrar_.Remove(this,
                       chrome::NOTIFICATION_CLOSE_ALL_BROWSERS_REQUEST,
--- chrome/browser/chromeos/login/base_login_display_host.h
+++ chrome/browser/chromeos/login/base_login_display_host.h
@@ -43,6 +43,7 @@ class BaseLoginDisplayHost : public LoginDisplayHost,
   }
 
   // LoginDisplayHost implementation:
+  virtual void BeforeSessionStart() OVERRIDE;
   virtual void OnSessionStart() OVERRIDE;
   virtual void OnCompleteLogin() OVERRIDE;
   virtual void StartWizard(
@@ -111,6 +112,9 @@ class BaseLoginDisplayHost : public LoginDisplayHost,
 
   // Whether progress bar is shown on the OOBE page.
   bool oobe_progress_bar_visible_;
+
+  // True if session start is in progress.
+  bool session_starting_;
 
   DISALLOW_COPY_AND_ASSIGN(BaseLoginDisplayHost);
 };
