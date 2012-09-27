e2719ddfed7e7e9f1d603fd42800a98d69b8fe8b
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-27 17:01:58 +0000

    Page overlays are not updated when WebView is resized https://bugs.webkit.org/show_bug.cgi?id=97789
    Reviewed by Vsevolod Vlasov.
    
    Invalidate page overlays when view is resized.
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::sendResizeEventAndRepaint):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129772 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -3162,6 +3162,8 @@ void WebViewImpl::sendResizeEventAndRepaint()
             m_client->didInvalidateRect(damagedRect);
         }
     }
+    if (m_pageOverlays)
+        m_pageOverlays->update();
 }
 
 void WebViewImpl::configureAutoResizeMode()
