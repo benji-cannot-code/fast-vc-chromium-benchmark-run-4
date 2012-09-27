f613266c72ad6c2a3fd5beddb46b8198c580105d
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-27 21:54:20 +0000

    Fix 32-bit build.
    * Plugins/WebNetscapePluginPackage.mm:
    (-[WebNetscapePluginPackage _initWithPath:]):
    Add back the architecture check that i accidentally.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
+++ third_party/WebKit/Source/WebKit/mac/Plugins/WebNetscapePluginPackage.mm
@@ -231,6 +231,10 @@ static TransitionVector tVectorForFunctionPointer(FunctionPointer);
     NSFileHandle *executableFile = [NSFileHandle fileHandleForReadingAtPath:[(NSURL *)executableURL.get() path]];
     NSData *data = [executableFile readDataOfLength:512];
     [executableFile closeFile];
+
+     if (![self isNativeLibraryData:data])
+         return NO;
+
 #endif
 
     if (![self getPluginInfoFromPLists] && ![self getPluginInfoFromResources])
