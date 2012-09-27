ed2e6171533212fcb517421d81ffeca1f2719289
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 23:16:34 +0000

    Add guards to metric values; erase bad events/metrics from db
    Improves the database by not inserting blatantly erroneous data, and by deleting
    any events or metrics which cannot be properly retrieved from the database.
    Also make DB take a metric for AddMetric() - this makes it so that we have a
    more accurate timestamp since we record the time before the thread-jumping.
    
    This is a re-land of r157168 (previously broke sizes).
    
    BUG=130212
    
    Review URL: https://chromiumcodereview.appspot.com/10956028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/performance_monitor/constants.cc
+++ chrome/browser/performance_monitor/constants.cc
@@ -7,19 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace performance_monitor {
 
-// TODO(chebert): i18n on all constants.
-
 // The error message displayed when a metric's details are not found.
 const char kMetricNotFoundError[] = "Metric details not found.";
 
 // Any metric that is not associated with a specific activity will use this as
 // its activity.
 const char kProcessChromeAggregate[] = "chrome_aggregate";
-
-// The default interval at which PerformanceMonitor performs its timed
-// collections; this can be overridden by using the kPerformanceMonitorGathering
-// switch with an associated (positive integer) value.
-const int kDefaultGatherIntervalInSeconds = 120;
 
 // Tokens to retrieve state values from the database.
 
--- chrome/browser/ui/webui/performance_monitor/performance_monitor_handler.cc
+++ chrome/browser/ui/webui/performance_monitor/performance_monitor_handler.cc
@@ -326,7 +326,8 @@ void DoGetMetric(DictionaryValue* results,
       results,
       metric_type,
       db->GetMaxStatsForActivityAndMetric(metric_type),
-      AggregateMetric(metric_vector.get(),
+      AggregateMetric(metric_type,
+                      metric_vector.get(),
                       start,
                       resolution).get());
 }
