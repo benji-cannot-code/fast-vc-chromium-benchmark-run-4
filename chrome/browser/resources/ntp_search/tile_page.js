c41557e92f1a2b1574feb19ee592397fa4d7a2e5
pedrosimonetti@chromium.org <pedrosimonetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 01:14:05 +0000

    NTP5: Addressing the TBR dynamic padding CL: http://codereview.chromium.org/10963052/
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/10985030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_search/tile_page.js
+++ chrome/browser/resources/ntp_search/tile_page.js
@@ -704,8 +704,7 @@ cr.define('ntp', function() {
             Math.round((windowHeight - HEIGHT_FOR_TWO_ROWS) / 2);
       }
 
-      var cardSliderFrame = $('card-slider-frame');
-      cardSliderFrame.style.bottom = paddingBottom + 'px';
+      $('card-slider-frame').style.bottom = paddingBottom + 'px';
 
       // Width logic
 
