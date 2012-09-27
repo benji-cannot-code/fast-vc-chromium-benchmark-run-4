38510b3fe2f53be639c3b8b0c454657332c69676
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 00:20:44 +0000

    Move code in the right places in chrome/browser/profiles/profile_shortcut_manager*
    Note: No code/comment was added, all new lines were copied from some other place where they didn't belong.
    
    BUG=148539
    TEST=unit_tests.exe --gtest_filter=ProfileShortcut*
    
    Review URL: https://chromiumcodereview.appspot.com/10996031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_shortcut_manager_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/path_service.h"
+#include "base/string16.h"
 #include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/app_icon_win.h"
@@ -173,6 +174,7 @@ class ProfileShortcutManagerWin : public ProfileShortcutManager,
 
   virtual void CreateProfileShortcut(const FilePath& profile_path) OVERRIDE;
 
+  // ProfileInfoCacheObserver:
   virtual void OnProfileAdded(const FilePath& profile_path) OVERRIDE;
   virtual void OnProfileWillBeRemoved(const FilePath& profile_path) OVERRIDE;
   virtual void OnProfileWasRemoved(const FilePath& profile_path,
@@ -189,7 +191,10 @@ class ProfileShortcutManagerWin : public ProfileShortcutManager,
   FilePath GetOtherProfilePath(const FilePath& profile_path);
   void UpdateShortcutForProfileAtPath(const FilePath& profile_path,
                                       bool create_always);
+
   ProfileManager* profile_manager_;
+
+  DISALLOW_COPY_AND_ASSIGN(ProfileShortcutManagerWin);
 };
 
 // static
@@ -305,7 +310,6 @@ void ProfileShortcutManagerWin::StartProfileShortcutNameChange(
   }
 }
 
-// Gets the other profile path in the cache when there are two profiles.
 FilePath ProfileShortcutManagerWin::GetOtherProfilePath(
     const FilePath& profile_path) {
   DCHECK_EQ(2U, profile_manager_->GetProfileInfoCache().GetNumberOfProfiles());
@@ -369,4 +373,3 @@ void ProfileShortcutManagerWin::UpdateShortcutForProfileAtPath(
   profile_manager_->GetProfileInfoCache().SetShortcutNameOfProfileAtIndex(
       profile_index, new_shortcut_appended_name);
 }
-
