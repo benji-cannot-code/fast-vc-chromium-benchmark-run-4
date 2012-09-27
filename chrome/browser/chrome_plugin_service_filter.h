e8464e750b657202eff1d8e73d7fe64c01895430
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 12:51:52 +0000

    Override plug-ins by path instead of name.
    BUG=152270
    
    
    Review URL: https://chromiumcodereview.appspot.com/10993016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159008 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_plugin_service_filter.h
+++ chrome/browser/chrome_plugin_service_filter.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/plugin_service_filter.h"
 #include "googleurl/src/gurl.h"
+#include "webkit/plugins/webplugininfo.h"
 
 class PluginPrefs;
 class Profile;
@@ -39,7 +40,7 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
   void OverridePluginForTab(int render_process_id,
                             int render_view_id,
                             const GURL& url,
-                            const string16& plugin_name);
+                            const webkit::WebPluginInfo& plugin);
 
   // Restricts the given plugin to the given profile and origin of the given
   // URL.
@@ -66,7 +67,7 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
     int render_process_id;
     int render_view_id;
     GURL url;  // If empty, the override applies to all urls in render_view.
-    string16 plugin_name;
+    webkit::WebPluginInfo plugin;
   };
 
   ChromePluginServiceFilter();
