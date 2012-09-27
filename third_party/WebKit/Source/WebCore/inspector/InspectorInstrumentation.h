30ad0911b386f45c217e13475963be6dbf011688
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-27 16:58:22 +0000

    Dropping JSC references from InspectorInstrumentation.h by including 'ScriptState.h' https://bugs.webkit.org/show_bug.cgi?id=97759
    Reviewed by Adam Barth.
    
    InspectorInstrumentation.h includes two '#if USE(JSC)' blocks, which
    I've been reliably informed should not appear in WebCore[1]. This patch
    drops both blocks, and includes 'ScriptState.h' instead, which should
    have the same practical effect.
    
    There's no functional change, so this should be covered by existing tests.
    
    [1]: https://bugs.webkit.org/show_bug.cgi?id=94433#c55
    
    * inspector/InspectorInstrumentation.h:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129771 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorInstrumentation.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorInstrumentation.h
@@ -38,13 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 #include "Page.h"
 #include "ScriptExecutionContext.h"
+#include "ScriptState.h"
 #include "StorageArea.h"
-
-#if USE(JSC)
-namespace JSC {
-class ExecState;
-}
-#endif
 
 namespace WebCore {
 
@@ -82,12 +77,6 @@ class ThreadableLoaderClient;
 class WorkerContext;
 class WorkerContextProxy;
 class XMLHttpRequest;
-
-#if USE(JSC)
-typedef JSC::ExecState ScriptState;
-#else
-class ScriptState;
-#endif
 
 #if ENABLE(WEB_SOCKETS)
 struct WebSocketFrame;
