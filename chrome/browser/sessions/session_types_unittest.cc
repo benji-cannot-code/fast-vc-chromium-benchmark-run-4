e61daa54818322c8b741e9bade5be981f63def12
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 23:44:40 +0000

    Add a timestamp field to NavigationEntry and propagate it to history and sync
    Set the active NavigationEntry's timestamp in NavigationControllerImpl::RendererDidNavigate.
    
    Null out the timestamp when restoring navigations from sync data.
    
    Avoid has_*() checks when restoring navigations.
    
    Pickle/unpickle the timestamp.
    
    Remove now-unneeded timestamp reconciliation logic in SessionModelAssociator.
    
    Add timestamp assertions to session restore and tab restore
    browser tests.
    
    Add sync integration test to make sure that the tab sync timestamps match the history timestamps.
    
    BUG=128449
    
    
    Review URL: https://chromiumcodereview.appspot.com/11000014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_types_unittest.cc
+++ chrome/browser/sessions/session_types_unittest.cc
@@ -60,6 +60,7 @@ scoped_ptr<content::NavigationEntry> MakeNavigationEntryForTest() {
   navigation_entry->SetPostID(kPostID);
   navigation_entry->SetOriginalRequestURL(kOriginalRequestURL);
   navigation_entry->SetIsOverridingUserAgent(kIsOverridingUserAgent);
+  navigation_entry->SetTimestamp(kTimestamp);
   return navigation_entry.Pass();
 }
 
@@ -97,7 +98,7 @@ TEST(TabNavigationTest, DefaultInitializer) {
   EXPECT_EQ(-1, SessionTypesTestHelper::GetPostID(navigation));
   EXPECT_EQ(GURL(), SessionTypesTestHelper::GetOriginalRequestURL(navigation));
   EXPECT_FALSE(SessionTypesTestHelper::GetIsOverridingUserAgent(navigation));
-  EXPECT_EQ(base::Time(), navigation.timestamp());
+  EXPECT_TRUE(navigation.timestamp().is_null());
 }
 
 // Create a TabNavigation from a NavigationEntry.  All its fields
@@ -107,8 +108,7 @@ TEST(TabNavigationTest, FromNavigationEntry) {
       MakeNavigationEntryForTest());
 
   const TabNavigation& navigation =
-      TabNavigation::FromNavigationEntry(
-          kIndex, *navigation_entry, kTimestamp);
+      TabNavigation::FromNavigationEntry(kIndex, *navigation_entry);
 
   EXPECT_EQ(kIndex, navigation.index());
 
@@ -155,7 +155,7 @@ TEST(TabNavigationTest, FromSyncData) {
   EXPECT_EQ(-1, SessionTypesTestHelper::GetPostID(navigation));
   EXPECT_EQ(GURL(), SessionTypesTestHelper::GetOriginalRequestURL(navigation));
   EXPECT_FALSE(SessionTypesTestHelper::GetIsOverridingUserAgent(navigation));
-  EXPECT_EQ(kTimestamp, navigation.timestamp());
+  EXPECT_TRUE(navigation.timestamp().is_null());
 }
 
 // Create a TabNavigation, pickle it, then create another one by
@@ -163,8 +163,7 @@ TEST(TabNavigationTest, FromSyncData) {
 // that aren't pickled, which should be set to default values.
 TEST(TabNavigationTest, Pickle) {
   const TabNavigation& old_navigation =
-      TabNavigation::FromNavigationEntry(
-          kIndex, *MakeNavigationEntryForTest(), kTimestamp);
+      TabNavigation::FromNavigationEntry(kIndex, *MakeNavigationEntryForTest());
 
   Pickle pickle;
   old_navigation.WriteToPickle(&pickle);
@@ -192,7 +191,7 @@ TEST(TabNavigationTest, Pickle) {
             SessionTypesTestHelper::GetOriginalRequestURL(new_navigation));
   EXPECT_EQ(kIsOverridingUserAgent,
             SessionTypesTestHelper::GetIsOverridingUserAgent(new_navigation));
-  EXPECT_EQ(base::Time(), new_navigation.timestamp());
+  EXPECT_EQ(kTimestamp, new_navigation.timestamp());
 }
 
 // Create a NavigationEntry, then create another one by converting to
@@ -204,8 +203,7 @@ TEST(TabNavigationTest, ToNavigationEntry) {
       MakeNavigationEntryForTest());
 
   const TabNavigation& navigation =
-      TabNavigation::FromNavigationEntry(
-          kIndex, *old_navigation_entry, kTimestamp);
+      TabNavigation::FromNavigationEntry(kIndex, *old_navigation_entry);
 
   const scoped_ptr<content::NavigationEntry> new_navigation_entry(
       navigation.ToNavigationEntry(kPageID, NULL));
@@ -234,8 +232,7 @@ TEST(TabNavigationTest, ToSyncData) {
       MakeNavigationEntryForTest());
 
   const TabNavigation& navigation =
-      TabNavigation::FromNavigationEntry(
-          kIndex, *navigation_entry, kTimestamp);
+      TabNavigation::FromNavigationEntry(kIndex, *navigation_entry);
 
   const sync_pb::TabNavigation sync_data = navigation.ToSyncData();
 
@@ -270,8 +267,7 @@ TEST(TabNavigationTest, TransitionTypes) {
 
       navigation_entry->SetTransitionType(transition);
       const TabNavigation& navigation =
-          TabNavigation::FromNavigationEntry(
-              kIndex, *navigation_entry, kTimestamp);
+          TabNavigation::FromNavigationEntry(kIndex, *navigation_entry);
       const sync_pb::TabNavigation& sync_data = navigation.ToSyncData();
       const TabNavigation& constructed_nav =
           TabNavigation::FromSyncData(kIndex, sync_data);
