19fecc7cf3006420026cb41ca24526787037d110
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-27 00:28:39 +0000

    Add some CHECKs to learn more about a crash bug.
    TBR=simonjam@chromium.org
    BUG=151040
    Review URL: https://codereview.chromium.org/10991049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@158946 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/resource_dispatcher.cc
+++ content/common/resource_dispatcher.cc
@@ -335,11 +335,15 @@ void ResourceDispatcher::OnSetDataBuffer(const IPC::Message& message,
     return;
 
   bool shm_valid = base::SharedMemory::IsHandleValid(shm_handle);
-  DCHECK((shm_valid && shm_size > 0) || (!shm_valid && !shm_size));
+  CHECK((shm_valid && shm_size > 0) || (!shm_valid && !shm_size));
 
   request_info->buffer.reset(
       new base::SharedMemory(shm_handle, true));  // read only
-  request_info->buffer->Map(shm_size);
+
+  bool ok = request_info->buffer->Map(shm_size);
+  CHECK(ok);
+
+  request_info->buffer_size = shm_size;
 }
 
 void ResourceDispatcher::OnReceivedData(const IPC::Message& message,
@@ -349,6 +353,9 @@ void ResourceDispatcher::OnReceivedData(const IPC::Message& message,
                                         int encoded_data_length) {
   PendingRequestInfo* request_info = GetPendingRequestInfo(request_id);
   if (request_info && data_length > 0) {
+    CHECK(base::SharedMemory::IsHandleValid(request_info->buffer->handle()));
+    CHECK_GE(request_info->buffer_size, data_offset + data_length);
+
     base::TimeTicks time_start = base::TimeTicks::Now();
 
     request_info->peer->OnReceivedData(
@@ -433,6 +440,7 @@ void ResourceDispatcher::OnRequestComplete(
     return;
   request_info->completion_time = base::TimeTicks::Now();
   request_info->buffer.reset();
+  request_info->buffer_size = 0;
 
   ResourceLoaderBridge::Peer* peer = request_info->peer;
 
@@ -515,7 +523,8 @@ void ResourceDispatcher::SetDefersLoading(int request_id, bool value) {
 ResourceDispatcher::PendingRequestInfo::PendingRequestInfo()
     : peer(NULL),
       resource_type(ResourceType::SUB_RESOURCE),
-      is_deferred(false) {
+      is_deferred(false),
+      buffer_size(0) {
 }
 
 ResourceDispatcher::PendingRequestInfo::PendingRequestInfo(
