b49f17e899ca6f2f869932a537be3b35a1f22ffc
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 10:27:10 +0000

    Linux: fix build with system ICU.
    This is upstreaming Gentoo Linux patch,
    fixing breakage from https://chromiumcodereview.appspot.com/10941025/
    
    TBR=vitalybuka,abodenha
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10986080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159235 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/print_backend.cc
+++ printing/backend/print_backend.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "third_party/icu/public/common/unicode/uchar.h"
 #include "ui/base/text/text_elider.h"
+#include "unicode/uchar.h"
 
 namespace {
 
