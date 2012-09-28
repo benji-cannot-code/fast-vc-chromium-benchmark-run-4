9def51fe99586f0766be2810cc7162dc4945776b
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 02:59:36 +0000

    [Chromium][Android] Remove unused PlatformSupport reference in ScrollbarThemeChromiumAndroid https://bugs.webkit.org/show_bug.cgi?id=97846
    Reviewed by Kentaro Hara.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * platform/chromium/ScrollbarThemeChromiumAndroid.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/ScrollbarThemeChromiumAndroid.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/ScrollbarThemeChromiumAndroid.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "PlatformContextSkia.h"
 #include "PlatformMouseEvent.h"
-#include "PlatformSupport.h"
 #include "Scrollbar.h"
 #include "TransformationMatrix.h"
 
