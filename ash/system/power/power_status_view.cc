8e3461a2f5b3000ef9ae5ac958ea19e6422edfd6
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 17:50:50 +0000

    Display minimum battery percentage as 1% instead of 0%.
    BUG=152983
    
    Review URL: https://codereview.chromium.org/10982090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159283 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/power/power_status_view.cc
+++ ash/system/power/power_status_view.cc
@@ -31,6 +31,8 @@ const int kPaddingVertical = 10;
 const int kLabelMinWidth = 120;
 // Padding between battery status text and battery icon on default view.
 const int kPaddingBetweenBatteryStatusAndIcon = 3;
+// Minimum battery percentage rendered in UI.
+const int kMinBatteryPercent = 1;
 }  // namespace
 
 PowerStatusView::PowerStatusView(ViewType view_type,
@@ -116,8 +118,7 @@ void PowerStatusView::UpdateTextForDefaultView() {
   } else {
     string16 battery_percentage = l10n_util::GetStringFUTF16(
         IDS_ASH_STATUS_TRAY_BATTERY_PERCENT_ONLY,
-        base::IntToString16(
-            static_cast<int>(supply_status_.battery_percentage)));
+        base::IntToString16(GetRoundedBatteryPercentage()));
     string16 battery_time = string16();
     int hour = 0;
     int min = 0;
@@ -167,8 +168,7 @@ void PowerStatusView::UpdateTextForNotificationView() {
       status_label_->SetText(
           l10n_util::GetStringFUTF16(
               IDS_ASH_STATUS_TRAY_BATTERY_PERCENT,
-              base::IntToString16(
-                  static_cast<int>(supply_status_.battery_percentage))));
+              base::IntToString16(GetRoundedBatteryPercentage())));
     }
   }
 
@@ -198,6 +198,12 @@ void PowerStatusView::UpdateTextForNotificationView() {
   } else {
     time_label_->SetText(string16());
   }
+}
+
+int PowerStatusView::GetRoundedBatteryPercentage() const {
+  DCHECK(supply_status_.battery_percentage >= 0.0f);
+  return std::max(kMinBatteryPercent,
+      static_cast<int>(supply_status_.battery_percentage));
 }
 
 void PowerStatusView::UpdateIcon() {
