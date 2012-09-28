95d758316ff7d16f5c72e995387739d33be9063e
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 03:03:29 +0000

    [Chromium] Clean up includes in ColorChooserUIController https://bugs.webkit.org/show_bug.cgi?id=97849
    Reviewed by Kentaro Hara.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * src/ColorChooserUIController.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/ColorChooserUIController.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/ColorChooserUIController.cpp
@@ -37,8 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LocalizedStrings.h"
 #include "PickerCommon.h"
 #include "WebColorChooser.h"
-#include "platform/WebColor.h"
-#include "platform/WebKitPlatformSupport.h"
+#include <public/Platform.h>
+#include <public/WebColor.h>
 #include <public/WebLocalizedString.h>
 
 namespace WebKit {
