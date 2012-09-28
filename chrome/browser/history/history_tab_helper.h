04f3e2b4f0ed97be1397b155284918ea00c23b60
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 09:52:03 +0000

    Use NavigationEntry timestamp in history code
    Rework arguments to CreateHistoryAddPageArgs so that
    ExternalTabContainerWin can pass in a timestamp explicitly.
    (This should fix the chrome frame test failures.)
    
    This was split off from 11000014 (to narrow down what's causing
    the chrome frame test failures).
    
    BUG=128449
    TBR=sky@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=159218
    
    Review URL: https://codereview.chromium.org/10987089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_tab_helper.h
+++ chrome/browser/history/history_tab_helper.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_HISTORY_HISTORY_TAB_HELPER_H_
 
 #include "base/memory/ref_counted.h"
+#include "base/time.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
@@ -38,7 +39,8 @@ class HistoryTabHelper : public content::WebContentsObserver,
   // history.
   history::HistoryAddPageArgs CreateHistoryAddPageArgs(
       const GURL& virtual_url,
-      const content::LoadCommittedDetails& details,
+      base::Time timestamp,
+      bool did_replace_entry,
       const content::FrameNavigateParams& params);
 
  private:
--- chrome/browser/ui/views/external_tab_container_win.cc
+++ chrome/browser/ui/views/external_tab_container_win.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/string16.h"
+#include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/win_util.h"
 #include "chrome/app/chrome_command_ids.h"
@@ -453,12 +454,11 @@ WebContents* ExternalTabContainerWin::OpenURLFromTab(
         nav_params.page_id = -1;
         nav_params.transition = content::PAGE_TRANSITION_LINK;
 
-        content::LoadCommittedDetails details;
-        details.did_replace_entry = false;
-
         const history::HistoryAddPageArgs& add_page_args =
             tab_contents_->history_tab_helper()->
-                CreateHistoryAddPageArgs(params.url, details, nav_params);
+                CreateHistoryAddPageArgs(
+                    params.url, base::Time::Now(),
+                    false /* did_replace_entry */, nav_params);
         tab_contents_->history_tab_helper()->
             UpdateHistoryForNavigation(add_page_args);
 
