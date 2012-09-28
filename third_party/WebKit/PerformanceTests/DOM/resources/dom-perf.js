de60c69ffc3708117e7e4a389f8d7c1ea261dc41
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 04:18:04 +0000

    Build fix after r129824.
    * DOM/resources/dom-perf.js:
    (runBenchmarkSuite):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PerformanceTests/DOM/resources/dom-perf.js
+++ third_party/WebKit/PerformanceTests/DOM/resources/dom-perf.js
@@ -352,5 +352,5 @@ function runBenchmarkSuite(suite, runCount) {
         var container = document.getElementById('container');
         if (container.firstChild)
             container.removeChild(container.firstChild);
-    });
+    }});
 }
