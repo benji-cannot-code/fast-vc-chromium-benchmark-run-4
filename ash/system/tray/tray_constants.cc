8258635ed8ec23122772088f20d57c76f37371ba
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 16:52:25 +0000

    Move down the entire status tray down by 2 pixels for better alignment on the login screen.
    BUG=152520
    TBR=sadrul
    
    Review URL: https://codereview.chromium.org/10989061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159275 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_constants.cc
+++ ash/system/tray/tray_constants.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 const int kPaddingFromRightEdgeOfScreenBottomAlignment = 15;
-const int kPaddingFromBottomOfScreenBottomAlignment = 9;
+const int kPaddingFromBottomOfScreenBottomAlignment = 7;
 const int kPaddingFromOuterEdgeOfLauncherVerticalAlignment = 8;
 const int kPaddingFromInnerEdgeOfLauncherVerticalAlignment = 9;
 const int kPaddingFromBottomOfScreenVerticalAlignment = 10;
