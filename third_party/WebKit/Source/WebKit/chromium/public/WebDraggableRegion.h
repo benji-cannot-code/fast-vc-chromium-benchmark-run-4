3794960a0ae103d781c3f8dfacac53e962f1042b
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 23:38:48 +0000

    Add draggable property to WebDraggableRegion. https://bugs.webkit.org/show_bug.cgi?id=97950
    Reviewed by Adam Barth.
    
    This needs to be done before we can land the patch to use new syntax for
    draggable regions. This is because Chromium side code cannot enable
    the changes to support new syntax side by side when this property is
    not present.
    
    * public/WebDraggableRegion.h:
    (WebDraggableRegion):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129960 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebDraggableRegion.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebDraggableRegion.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 struct WebDraggableRegion {
+    bool draggable;
     WebString label;
     WebRect bounds;
     WebRect clip;
