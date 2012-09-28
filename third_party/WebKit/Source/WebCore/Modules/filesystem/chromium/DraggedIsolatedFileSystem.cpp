eb9433f8319ae97239de0636c1c4bf4a26b39618
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 03:41:50 +0000

    [Chromium] Remove unused PlatformSupport reference in DraggedIsolatedFileSystem https://bugs.webkit.org/show_bug.cgi?id=97851
    Reviewed by Kentaro Hara.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * Modules/filesystem/chromium/DraggedIsolatedFileSystem.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/filesystem/chromium/DraggedIsolatedFileSystem.cpp
+++ third_party/WebKit/Source/WebCore/Modules/filesystem/chromium/DraggedIsolatedFileSystem.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(FILE_SYSTEM)
 
 #include "DOMFileSystem.h"
-#include "PlatformSupport.h"
 #include "ScriptExecutionContext.h"
 #include "SecurityOrigin.h"
 #include "Supplementable.h"
