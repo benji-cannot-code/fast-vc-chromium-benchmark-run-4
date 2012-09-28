00699590e86665f4e5de8122674aa2af855b845f
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 23:02:13 +0000

    Content setting bubble was not showing up when no text animation is used. This was because no layout manager was set which would make the view not paint the child icon properly.
    BUG=152771,152833
    
    
    Review URL: https://chromiumcodereview.appspot.com/10984091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159358 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_bar_decoration_view.h
+++ chrome/browser/ui/views/location_bar/location_bar_decoration_view.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/animation/animation_delegate.h"
 #include "ui/base/events/event.h"
 #include "ui/gfx/font.h"
-#include "ui/views/controls/image_view.h"
 #include "ui/views/painter.h"
 #include "ui/views/view.h"
 
