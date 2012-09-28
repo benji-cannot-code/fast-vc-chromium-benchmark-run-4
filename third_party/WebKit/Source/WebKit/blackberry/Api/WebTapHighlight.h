299ba1b7a77704257d8ce90b1c82eee33dc28b95
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 15:45:20 +0000

    [BLACKBERRY] Add isVisible method to WebTapHighlight https://bugs.webkit.org/show_bug.cgi?id=97915
    Patch by Genevieve Mak <gmak@rim.com> on 2012-09-28
    Reviewed by Rob Buis.
    
    Reviewed Internally By Andrew Lo
    The isVisible() method lets us avoid dispatching to the webkit thread
    if the tap highlight isn't visible.
    Always clear tap highlight on Touch up when converting Touch Events
    to Mouse Events.
    
    * Api/WebTapHighlight.h:
    * WebKitSupport/DefaultTapHighlight.h:
    (BlackBerry::WebKit::DefaultTapHighlight::isVisible):
    * WebKitSupport/TouchEventHandler.cpp:
    (BlackBerry::WebKit::TouchEventHandler::handleTouchPoint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129901 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebTapHighlight.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebTapHighlight.h
@@ -34,6 +34,7 @@ public:
     virtual void draw(const Platform::IntRectRegion&, int red, int green, int blue, int alpha, bool hideAfterScroll) = 0;
     virtual void hide() = 0;
 
+    virtual bool isVisible() const = 0;
     virtual bool shouldHideAfterScroll() const = 0;
 };
 
