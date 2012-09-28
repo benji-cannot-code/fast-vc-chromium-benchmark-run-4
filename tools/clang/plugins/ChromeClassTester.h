9a50eb6af1b95564fc5f0fa638be4d0563269478
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 22:08:35 +0000

    Remove check_cc_directory flag from Clang plugin
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10961005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159349 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/plugins/ChromeClassTester.h
+++ tools/clang/plugins/ChromeClassTester.h
@@ -17,8 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // headers to subclasses which implement CheckChromeClass().
 class ChromeClassTester : public clang::ASTConsumer {
  public:
-  explicit ChromeClassTester(clang::CompilerInstance& instance,
-                             bool check_cc_directory);
+  explicit ChromeClassTester(clang::CompilerInstance& instance);
   virtual ~ChromeClassTester();
 
   // clang::ASTConsumer:
@@ -81,9 +80,6 @@ class ChromeClassTester : public clang::ASTConsumer {
 
   // List of decls to check once the current top-level decl is parsed.
   std::vector<clang::TagDecl*> pending_class_decls_;
-
-  // TODO(jamesr): Remove once cc/ directory compiles without warnings.
-  bool check_cc_directory_;
 };
 
 #endif  // TOOLS_CLANG_PLUGINS_CHROMECLASSTESTER_H_
--- tools/clang/plugins/FindBadConstructs.cpp
+++ tools/clang/plugins/FindBadConstructs.cpp
@@ -51,9 +51,8 @@ class FindBadConstructsConsumer : public ChromeClassTester {
  public:
   FindBadConstructsConsumer(CompilerInstance& instance,
                             bool check_refcounted_dtors,
-                            bool check_virtuals_in_implementations,
-                            bool check_cc_directory)
-      : ChromeClassTester(instance, check_cc_directory),
+                            bool check_virtuals_in_implementations)
+      : ChromeClassTester(instance),
         check_refcounted_dtors_(check_refcounted_dtors),
         check_virtuals_in_implementations_(check_virtuals_in_implementations) {
   }
@@ -397,8 +396,7 @@ class FindBadConstructsAction : public PluginASTAction {
  public:
   FindBadConstructsAction()
       : check_refcounted_dtors_(true),
-        check_virtuals_in_implementations_(true),
-        check_cc_directory_(false) {
+        check_virtuals_in_implementations_(true) {
   }
 
  protected:
@@ -406,8 +404,7 @@ class FindBadConstructsAction : public PluginASTAction {
   virtual ASTConsumer* CreateASTConsumer(CompilerInstance& instance,
                                          llvm::StringRef ref) {
     return new FindBadConstructsConsumer(
-        instance, check_refcounted_dtors_, check_virtuals_in_implementations_,
-        check_cc_directory_);
+        instance, check_refcounted_dtors_, check_virtuals_in_implementations_);
   }
 
   virtual bool ParseArgs(const CompilerInstance& instance,
@@ -419,8 +416,6 @@ class FindBadConstructsAction : public PluginASTAction {
         check_refcounted_dtors_ = false;
       } else if (args[i] == "skip-virtuals-in-implementations") {
         check_virtuals_in_implementations_ = false;
-      } else if (args[i] == "check-cc-directory") {
-        check_cc_directory_ = true;
       } else {
         parsed = false;
         llvm::errs() << "Unknown argument: " << args[i] << "\n";
@@ -433,7 +428,6 @@ class FindBadConstructsAction : public PluginASTAction {
  private:
   bool check_refcounted_dtors_;
   bool check_virtuals_in_implementations_;
-  bool check_cc_directory_;
 };
 
 }  // namespace
