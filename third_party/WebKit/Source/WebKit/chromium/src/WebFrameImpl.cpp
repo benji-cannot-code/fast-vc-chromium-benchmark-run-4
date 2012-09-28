29293f1f6439c4147645912406773959c172dd9a
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 02:37:49 +0000

    [Chromium] Remove unused PlatformSupport reference in WebFrameImpl https://bugs.webkit.org/show_bug.cgi?id=97842
    Reviewed by Kentaro Hara.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * src/WebFrameImpl.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129833 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -117,7 +117,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PageOverlay.h"
 #include "Performance.h"
 #include "PlatformMessagePortChannel.h"
-#include "PlatformSupport.h"
 #include "PluginDocument.h"
 #include "PrintContext.h"
 #include "RenderBox.h"
