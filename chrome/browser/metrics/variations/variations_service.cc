81963b92397b36e1017a72ab7cdc1e818ccf70b1
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-28 19:35:37 +0000

    Log the VariationsService fetch response code.
    TEST=Start Chrome and navigate to about:histograms. Ensure that Variations.SeedFetchResponseCode appears with a value logged (usually 200 or 304).
    BUG=152958
    
    
    Review URL: https://chromiumcodereview.appspot.com/10996059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159314 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/variations/variations_service.cc
+++ chrome/browser/metrics/variations/variations_service.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/field_trial.h"
+#include "base/metrics/histogram.h"
 #include "base/version.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/metrics/proto/trials_seed.pb.h"
@@ -25,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/load_flags.h"
 #include "net/base/network_change_notifier.h"
 #include "net/http/http_response_headers.h"
+#include "net/http/http_util.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
 
@@ -204,6 +206,11 @@ void VariationsService::OnURLFetchComplete(const net::URLFetcher* source) {
     DVLOG(1) << "Variations server request failed.";
     return;
   }
+
+  // Log the response code.
+  UMA_HISTOGRAM_CUSTOM_ENUMERATION("Variations.SeedFetchResponseCode",
+      net::HttpUtil::MapStatusCodeForHistogram(request->GetResponseCode()),
+      net::HttpUtil::GetStatusCodesForHistogram());
 
   if (request->GetResponseCode() != 200) {
     DVLOG(1) << "Variations server request returned non-200 response code: "
