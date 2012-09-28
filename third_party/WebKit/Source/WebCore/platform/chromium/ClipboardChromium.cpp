5ff191a4af1f0b7b71470e8bb7a7163456bd31fc
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 02:40:27 +0000

    [Chromium] Remove unused PlatformSupport reference in ClipboardChromium https://bugs.webkit.org/show_bug.cgi?id=97840
    Reviewed by Kentaro Hara.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * platform/chromium/ClipboardChromium.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129834 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/chromium/ClipboardChromium.cpp
+++ third_party/WebKit/Source/WebCore/platform/chromium/ClipboardChromium.cpp
@@ -46,7 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Image.h"
 #include "MIMETypeRegistry.h"
 #include "NamedNodeMap.h"
-#include "PlatformSupport.h"
 #include "Range.h"
 #include "RenderImage.h"
 #include "StringCallback.h"
