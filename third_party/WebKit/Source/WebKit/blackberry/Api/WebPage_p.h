273f82251a09e99abd3ac1e4392ce2298ebf0687
tonikitoo@webkit.org <tonikitoo@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 16:34:07 +0000

    [BlackBerry] Exiting fullscreen does not set the correct scroll position https://bugs.webkit.org/show_bug.cgi?id=97917 PR #212920
    Reviewed by Yong Li.
    Patch by Antonio Gomes <agomes@rim.com>
    Internally reviewed by Jacky Jiang.
    
    Restore the zoom level and scroll position at the time when know
    the fullscreen exit routine has ended (i.e. in ::setViewportSize
    instead of ::exitFullScreenForElement).
    
    Also patch caches now the scroll position instead of only the
    "x scroll position.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::WebPagePrivate):
    (BlackBerry::WebKit::WebPagePrivate::setViewportSize):
    (BlackBerry::WebKit::WebPagePrivate::enterFullScreenForElement):
    (BlackBerry::WebKit::WebPagePrivate::exitFullScreenForElement):
    * Api/WebPage_p.h:
    (WebPagePrivate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage_p.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage_p.h
@@ -524,7 +524,7 @@ public:
 #if ENABLE(FULLSCREEN_API)
 #if ENABLE(VIDEO)
     double m_scaleBeforeFullScreen;
-    int m_xScrollOffsetBeforeFullScreen;
+    WebCore::IntPoint m_scrollOffsetBeforeFullScreen;
 #endif
     bool m_isTogglingFullScreenState;
 #endif
