f5d60ac7ee14688a89439353df760afb717fffe0
mark.lam@apple.com <mark.lam@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-09-28 01:43:52 +0000

    Put initializeHostCallReturnValue() behind #if ENABLE(JIT). Fixes non JIT builds. https://bugs.webkit.org/show_bug.cgi?id=97838.
    Reviewed by John Sullivan.
    
    * runtime/JSGlobalData.cpp:
    (JSC::JSGlobalData::JSGlobalData):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@129830 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.cpp
@@ -232,7 +232,9 @@ JSGlobalData::JSGlobalData(GlobalDataType globalDataType, ThreadStackType thread
     
     interpreter->initialize(this->canUseJIT());
     
+#if ENABLE(JIT)
     initializeHostCallReturnValue(); // This is needed to convince the linker not to drop host call return support.
+#endif
 
     heap.notifyIsSafeToCollect();
     
