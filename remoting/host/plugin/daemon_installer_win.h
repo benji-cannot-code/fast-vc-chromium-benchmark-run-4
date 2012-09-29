350f1b1fae2ee8455b9ab937b99bca42b1182ec1
michaeln@google.com <michaeln@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-29 00:07:12 +0000

    Revert 159354 - Move DaemonController to remoting/host/setup
    DaemonController will be used outside of the plugin, so it doesn't make
    sense to keep it in the plugin directory.
    
    Review URL: https://codereview.chromium.org/10985086
    
    Busted build on windows, missing include file.
    TBR=sergeyu@chromium.org
    Review URL: https://codereview.chromium.org/11014004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159368 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/plugin/daemon_installer_win.h
+++ remoting/host/plugin/daemon_installer_win.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
+#include "remoting/host/plugin/daemon_controller.h"
 
 namespace remoting {
 
