d7634f6e444bb1f2aa120b25a81bb5732955cd0e
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-09-30 11:25:18 +0000

    views: Forward declare ToolbarModel in zoom_view.h
    TBR=pkasting@chromium.org
    
    Review URL: https://codereview.chromium.org/10977045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159438 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/zoom_view.cc
+++ chrome/browser/ui/views/location_bar/zoom_view.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/location_bar/zoom_view.h"
 
+#include "chrome/browser/ui/toolbar/toolbar_model.h"
 #include "chrome/browser/ui/view_ids.h"
-#include "chrome/browser/ui/views/browser_dialogs.h"
 #include "chrome/browser/ui/views/location_bar/zoom_bubble_view.h"
 #include "chrome/browser/ui/zoom/zoom_controller.h"
 #include "grit/generated_resources.h"
