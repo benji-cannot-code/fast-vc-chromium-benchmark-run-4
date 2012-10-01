2588526e3507f44e39aa3d72530c6664ea8f29df
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-01 16:05:45 +0000

    DevTools: rollback r136953, make server socket blocking again.
    BUG=125191
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159499 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_devtools_delegate_android.cc
+++ content/shell/shell_devtools_delegate_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/shell_devtools_delegate.h"
 
+#include "base/bind.h"
 #include "base/stringprintf.h"
 #include "content/public/browser/android/devtools_auth.h"
 #include "content/public/browser/devtools_http_handler.h"
