02c0b739e746aca2437ebcac8c54b33af047b54a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-01 19:23:49 +0000

    [BlackBerry] Add a early return in buildCMakeProjectOrExit() https://bugs.webkit.org/show_bug.cgi?id=98052
    Patch by Ming Xie <mxie@rim.com> on 2012-10-01
    Reviewed by Rob Buis.
    
    This option allows our build to only run the CMake build configuration
    step when environment variable GENERATE_CMAKE_PROJECT_ONLY is specified.
    
    * Scripts/webkitdirs.pm:
    (buildCMakeProjectOrExit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130066 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -2171,6 +2171,9 @@ sub buildCMakeProjectOrExit($$$$@)
 
     $returnCode = exitStatus(generateBuildSystemFromCMakeProject($port, $prefixPath, @cmakeArgs));
     exit($returnCode) if $returnCode;
+    if (isBlackBerry()) {
+        return 0 if (defined($ENV{"GENERATE_CMAKE_PROJECT_ONLY"}) eq '1');
+    }
     $returnCode = exitStatus(buildCMakeGeneratedProject($makeArgs));
     exit($returnCode) if $returnCode;
     return 0;
