f8faee3b38f26c453a7031f5618ed5e9eb5537a6
jocelyn.turcotte@digia.com <jocelyn.turcotte@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-01 11:19:18 +0000

    Make sure that the history position is applied correctly when using delegatesScrolling https://bugs.webkit.org/show_bug.cgi?id=97778
    Reviewed by Kenneth Rohde Christiansen.
    
    The position is applied asynchronously and the UI process is the one holding the current
    state. For this reason we can't rely in WebCore on ScrollView::scrollPosition holding
    the current position in that case.
    
    * page/Page.cpp:
    (WebCore::Page::setPageScaleFactor):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130032 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Page.cpp
+++ third_party/WebKit/Source/WebCore/page/Page.cpp
@@ -652,7 +652,7 @@ void Page::setPageScaleFactor(float scale, const IntPoint& origin)
     FrameView* view = document->view();
 
     if (scale == m_pageScaleFactor) {
-        if (view && view->scrollPosition() != origin) {
+        if (view && (view->scrollPosition() != origin || view->delegatesScrolling())) {
             document->updateLayoutIgnorePendingStylesheets();
             view->setScrollPosition(origin);
         }
