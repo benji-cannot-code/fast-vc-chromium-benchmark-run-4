da2fd2356e9553cd2185643dc2e1cd06bc8ca524
hartmanng@chromium.org <hartmanng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-01 23:33:01 +0000

    Fix gmail scroll on-device, along with bug 152469.
    If there's a better way to do this than treat gmail as a special case, please tell me, but it already seems to be setup this way in scroll.js.
    
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/10981037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159586 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gpu/gpu_tools/scrolling_benchmark.py
+++ tools/gpu/gpu_tools/scrolling_benchmark.py
@@ -40,7 +40,7 @@ class ScrollingBenchmark(multi_page_benchmark.MultiPageBenchmark):
                       help='Reports all data collected, not just FPS')
 
   @staticmethod
-  def ScrollPageFully(tab):
+  def ScrollPageFully(page, tab):
     scroll_js_path = os.path.join(os.path.dirname(__file__), 'scroll.js')
     scroll_js = open(scroll_js_path, 'r').read()
 
@@ -50,8 +50,8 @@ class ScrollingBenchmark(multi_page_benchmark.MultiPageBenchmark):
       window.__renderingStatsDeltas = null;
       new __ScrollTest(function(rendering_stats_deltas) {
         window.__renderingStatsDeltas = rendering_stats_deltas;
-      });
-    """)
+      }, %s);
+    """ % str(page.is_gmail).lower())
 
     # Poll for scroll benchmark completion.
     util.WaitFor(lambda: tab.runtime.Evaluate(
@@ -68,8 +68,8 @@ class ScrollingBenchmark(multi_page_benchmark.MultiPageBenchmark):
     if not options.no_gpu_benchmarking_extension:
       options.extra_browser_args.append('--enable-gpu-benchmarking')
 
-  def MeasurePage(self, _, tab):
-    rendering_stats_deltas = self.ScrollPageFully(tab)
+  def MeasurePage(self, page, tab):
+    rendering_stats_deltas = self.ScrollPageFully(page, tab)
     scroll_results = CalcScrollResults(rendering_stats_deltas)
     if self.options.report_all_results:
       all_results = {}
