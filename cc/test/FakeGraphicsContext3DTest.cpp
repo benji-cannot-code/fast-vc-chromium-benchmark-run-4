1ff53a29a6c854f340fb1f6756d80dae2ba552b1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-01 19:49:39 +0000

    cc: Remove unused include to wtf/Vector.h
    BUG=144577
    TEST=cc_unittests
    TBR=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11027007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159537 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/FakeGraphicsContext3DTest.cpp
+++ cc/test/FakeGraphicsContext3DTest.cpp
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
-using namespace cc;
 using namespace WebCore;
 using namespace WebKit;
 
