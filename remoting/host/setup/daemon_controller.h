c9995b1cb76f2354700127be2b3f63d4539a193b
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-01 18:11:34 +0000

    Move DaemonController to remoting/host/setup
    DaemonController will be used outside of the plugin, so it doesn't make
    sense to keep it in the plugin directory.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10985086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159525 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/setup/daemon_controller.h
+++ remoting/host/setup/daemon_controller.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_HOST_DAEMON_CONTROLLER_H_
-#define REMOTING_HOST_DAEMON_CONTROLLER_H_
+#ifndef REMOTING_HOST_SETUP_DAEMON_CONTROLLER_H_
+#define REMOTING_HOST_SETUP_DAEMON_CONTROLLER_H_
 
 #include <string>
 
@@ -149,4 +149,4 @@ class DaemonController {
 
 }  // namespace remoting
 
-#endif  // REMOTING_HOST_DAEMON_CONTROLLER_H_
+#endif  // REMOTING_HOST_SETUP_DAEMON_CONTROLLER_H_
