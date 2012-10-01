3116687fad676cbe5fc7ffaa463b46b0ae5ae59b
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-01 21:25:55 +0000

    Uses "SoloWindow" transparent header for each display.
    BUG=139234
    TEST=run on Goobuntu with --aura-host-window-size='800+0-1000x600,800+700-1000x400' flag.  ash_unittests passed.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10991042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159560 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_properties.cc
+++ ash/wm/window_properties.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/root_window_controller.h"
 #include "ash/wm/always_on_top_controller.h"
+#include "ash/wm/frame_painter.h"
 #include "ash/wm/shadow_types.h"
 #include "ui/aura/window_property.h"
 #include "ui/ui_controls/ui_controls_aura.h"
@@ -15,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // defined in aura.
 DECLARE_WINDOW_PROPERTY_TYPE(ash::internal::AlwaysOnTopController*);
 DECLARE_WINDOW_PROPERTY_TYPE(ash::internal::ShadowType);
+DECLARE_WINDOW_PROPERTY_TYPE(ash::FramePainter*);
 DECLARE_WINDOW_PROPERTY_TYPE(ash::WindowPersistsAcrossAllWorkspacesType)
 DECLARE_WINDOW_PROPERTY_TYPE(ui_controls::UIControlsAura*)
 DECLARE_WINDOW_PROPERTY_TYPE(ash::internal::RootWindowController*);
@@ -32,6 +34,8 @@ DEFINE_WINDOW_PROPERTY_KEY(
 DEFINE_WINDOW_PROPERTY_KEY(RootWindowController*,
                            kRootWindowControllerKey, NULL);
 DEFINE_WINDOW_PROPERTY_KEY(ShadowType, kShadowTypeKey, SHADOW_TYPE_NONE);
+DEFINE_WINDOW_PROPERTY_KEY(
+    ash::FramePainter*, kSoloWindowFramePainterKey, NULL);
 DEFINE_WINDOW_PROPERTY_KEY(bool, kStayInSameRootWindowKey, false);
 DEFINE_OWNED_WINDOW_PROPERTY_KEY(ui_controls::UIControlsAura,
                                  kUIControlsKey,
