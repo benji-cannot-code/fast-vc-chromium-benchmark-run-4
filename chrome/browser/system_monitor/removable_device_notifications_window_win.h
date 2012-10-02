335d327fc52865cdfac7a2c15437b1c624cc40b2
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-02 20:32:23 +0000

    [Win, MediaGallery] Display device friendly name instead of device path in media galleries permissions dialog.
    With the current code, media galleries permissions dialog display device path(e.g.: G:\) as the device name. Device path is not persistent and it may confuse the user. Instead, construct a display name using friendly device name and device path details (e.g.: "Usb disk device (G:)"). Whenever the device is mounted at a different volume path, the display name will be updated correspondingly.
    
    Added code to
    (1) Extract device friendly name and description using SetupDiGetRegistryProperty() function. 
    (2) Update the device name in the preferences dictionary if required.
    
    BUG=152808
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/10991030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159752 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/system_monitor/removable_device_notifications_window_win.h
+++ chrome/browser/system_monitor/removable_device_notifications_window_win.h
@@ -21,12 +21,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome {
 
 // Gets device information given a |device_path|. On success, returns true and
-// fills in |unique_id|, |name|, and |removable|.
-typedef bool (*GetDeviceInfoFunc)(const FilePath& device,
+// fills in |location|, |unique_id| and |removable|.
+typedef bool (*GetDeviceInfoFunc)(const FilePath& device_path,
                                   string16* location,
                                   std::string* unique_id,
-                                  string16* name,
                                   bool* removable);
+
+// Gets the friendly name of the device given a |device_location|.
+// |device_location| is the device mount point. E.g.: "G:\" or "C:\foo".
+typedef string16 (*GetDeviceNameFunc)(const FilePath& device_location);
 
 // Returns a vector of all the removable devices that are connected.
 typedef std::vector<FilePath> (*GetAttachedDevicesFunc)();
@@ -56,6 +59,7 @@ class RemovableDeviceNotificationsWindowWin
  protected:
   // Only for use in unit tests.
   void InitForTest(GetDeviceInfoFunc getDeviceInfo,
+                   GetDeviceNameFunc getDeviceName,
                    GetAttachedDevicesFunc getAttachedDevices);
 
   void OnDeviceChange(UINT event_type, LPARAM data);
@@ -83,12 +87,11 @@ class RemovableDeviceNotificationsWindowWin
       GetAttachedDevicesFunc get_attached_devices_func);
 
   void CheckDeviceTypeOnFileThread(const std::string& unique_id,
-                                   const FilePath::StringType& device_name,
                                    const FilePath& device);
 
   void ProcessDeviceAttachedOnUIThread(
       const std::string& device_id,
-      const FilePath::StringType& device_name,
+      const string16& device_name,
       const FilePath& device);
 
   // The window class of |window_|.
@@ -98,6 +101,7 @@ class RemovableDeviceNotificationsWindowWin
   HWND window_;
 
   GetDeviceInfoFunc get_device_info_func_;
+  GetDeviceNameFunc get_device_name_func_;
 
   // A map from device mount point to device id. Only accessed on the UI Thread.
   MountPointDeviceIdMap device_ids_;
