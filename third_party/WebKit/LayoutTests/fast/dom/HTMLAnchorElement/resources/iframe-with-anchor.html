benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit a1cfc43e3303dfc1cfdf0b4bb4abf7e8f15528e3

Source/WebCore: Patch by Swapna P <spottabathini@innominds.com> on 2012-02-04 Reviewed by Antonio Gomes.
Bug: iframe with scrolling=no incorrectly autoscrollable
https://bugs.webkit.org/show_bug.cgi?id=61558

Added check for frame scrolling mode just before applying scroll on frame content in function RenderLayer::scrollRect

Testcase: LayoutTests/fast/events/autoscroll-with-non-scrollable-parent.html

* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::scrollRectToVisible):

LayoutTests: Patch by Swapna P <spottabathini@innominds.com> on 2012-02-04
Reviewed by Antonio Gomes.

Bug: iframe with scrolling=no incorrectly autoscrollable
https://bugs.webkit.org/show_bug.cgi?id=61558

Did change inorder to print correct log as per the description provided in test case.

* fast/events/autoscroll-with-non-scrollable-parent.html:

git-svn-id: svn://svn.chromium.org/blink/trunk@106730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -67,6 +67,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameView.h"
 #include "Gradient.h"
 #include "GraphicsContext.h"
+#include "HTMLFrameElement.h"
 #include "HTMLFrameOwnerElement.h"
 #include "HTMLNames.h"
 #include "HitTestingTransformState.h"
@@ -1543,20 +1544,31 @@ void RenderLayer::scrollRectToVisible(const LayoutRect& rect, const ScrollAlignm
         }
     } else if (!parentLayer && renderer()->isBox() && renderBox()->canBeProgramaticallyScrolled()) {
         if (frameView) {
-            if (renderer()->document() && renderer()->document()->ownerElement() && renderer()->document()->ownerElement()->renderer()) {
-                LayoutRect viewRect = frameView->visibleContentRect();
-                LayoutRect r = getRectToExpose(viewRect, rect, alignX, alignY);
-                
-                LayoutUnit xOffset = r.x();
-                LayoutUnit yOffset = r.y();
-                // Adjust offsets if they're outside of the allowable range.
-                xOffset = max<LayoutUnit>(0, min(frameView->contentsWidth(), xOffset));
-                yOffset = max<LayoutUnit>(0, min(frameView->contentsHeight(), yOffset));
+            Element* ownerElement = 0;
+            if (renderer()->document())
+                ownerElement = renderer()->document()->ownerElement();
 
-                frameView->setScrollPosition(IntPoint(xOffset, yOffset));
-                parentLayer = renderer()->document()->ownerElement()->renderer()->enclosingLayer();
-                newRect.setX(rect.x() - frameView->scrollX() + frameView->x());
-                newRect.setY(rect.y() - frameView->scrollY() + frameView->y());
+            if (ownerElement && ownerElement->renderer()) {
+                HTMLFrameElement* frameElement = 0;
+
+                if (ownerElement->hasTagName(frameTag) || ownerElement->hasTagName(iframeTag))
+                    frameElement = static_cast<HTMLFrameElement*>(ownerElement);
+
+                if (frameElement && frameElement->scrollingMode() != ScrollbarAlwaysOff) {
+                    LayoutRect viewRect = frameView->visibleContentRect();
+                    LayoutRect exposeRect = getRectToExpose(viewRect, rect, alignX, alignY);
+
+                    LayoutUnit xOffset = exposeRect.x();
+                    LayoutUnit yOffset = exposeRect.y();
+                    // Adjust offsets if they're outside of the allowable range.
+                    xOffset = max<LayoutUnit>(0, min(frameView->contentsWidth(), xOffset));
+                    yOffset = max<LayoutUnit>(0, min(frameView->contentsHeight(), yOffset));
+
+                    frameView->setScrollPosition(IntPoint(xOffset, yOffset));
+                    parentLayer = ownerElement->renderer()->enclosingLayer();
+                    newRect.setX(rect.x() - frameView->scrollX() + frameView->x());
+                    newRect.setY(rect.y() - frameView->scrollY() + frameView->y());
+                }
             } else {
                 LayoutRect viewRect = frameView->visibleContentRect();
                 LayoutRect r = getRectToExpose(viewRect, rect, alignX, alignY);

third_party/WebKit/LayoutTests/fast/events/autoscroll-with-non-scrollable-parent.html
@@ -40,7 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
            
            var iframe = document.getElementById('NoScrolliFrame');
            var iframeDocument = iframe.contentDocument;
-           if (iframeDocument.body.scrollLeft != 0)
+           if (iframeDocument.body.scrollLeft == 0)
                log("PASSED");
            else
                log("FAILED : " + iframeDocument.body.scrollLeft + " pixels have been scrolled");

commit 05f5af280870c150b9ec087e7a78019d1b2e83f2

Source/WebCore: iframes with scrolling=no can't scroll to anchors https://bugs.webkit.org/show_bug.cgi?id=96539
Reviewed by Antonio Gomes.

This appears to have regressed in r106730. This patch matches Firefox's behavior.

Test: fast/dom/HTMLAnchorElement/anchor-in-noscroll-iframe.html

* page/EventHandler.h:
* rendering/RenderLayer.cpp:
(WebCore::RenderLayer::scrollRectToVisible): Currently scrolls are always forbidden
   if scrollbars are explicitly disabled. This adds an exception for scrolls that
   are not user-initiated and are not autoscrolls.

LayoutTests: Test for https://bugs.webkit.org/show_bug.cgi?id=96539.

Reviewed by Antonio Gomes.

* fast/dom/HTMLAnchorElement/anchor-in-noscroll-iframe-expected.txt: Added.
* fast/dom/HTMLAnchorElement/anchor-in-noscroll-iframe.html: Added.
* fast/dom/HTMLAnchorElement/resources: Added.
* fast/dom/HTMLAnchorElement/resources/iframe-with-anchor.html: Added.


git-svn-id: svn://svn.chromium.org/blink/trunk@130226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/WebCore/page/EventHandler.h
@@ -112,6 +112,7 @@ public:
     void stopAutoscrollTimer(bool rendererIsBeingDestroyed = false);
     RenderObject* autoscrollRenderer() const;
     void updateAutoscrollRenderer();
+    bool autoscrollInProgress() const { return m_autoscrollInProgress; }
 
     void dispatchFakeMouseMoveEventSoon();
     void dispatchFakeMouseMoveEventSoonInQuad(const FloatQuad&);

third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -1807,7 +1807,8 @@ void RenderLayer::scrollRectToVisible(const LayoutRect& rect, const ScrollAlignm
                 if (ownerElement->hasTagName(frameTag) || ownerElement->hasTagName(iframeTag))
                     frameElement = static_cast<HTMLFrameElement*>(ownerElement);
 
-                if (frameElement && frameElement->scrollingMode() != ScrollbarAlwaysOff) {
+                if ((frameElement && frameElement->scrollingMode() != ScrollbarAlwaysOff)
+                    || (!frameView->frame()->eventHandler()->autoscrollInProgress() && !frameView->wasScrolledByUser())) {
                     LayoutRect viewRect = frameView->visibleContentRect();
                     LayoutRect exposeRect = getRectToExpose(viewRect, rect, alignX, alignY);
 

third_party/WebKit/LayoutTests/fast/dom/HTMLAnchorElement/anchor-in-noscroll-iframe-expected.txt
@@ -1,0 +1,3 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+This tests whether clicking on an anchor in an iframe with scrolling="no" will scroll to anchor. If clicking on the link below triggers a scroll, the test passes.
+PASS

third_party/WebKit/LayoutTests/fast/dom/HTMLAnchorElement/anchor-in-noscroll-iframe.html
@@ -1,0 +1,7 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+<body>
+This tests whether clicking on an anchor in an iframe with scrolling="no" will scroll to anchor. If clicking on the link below triggers a scroll, the test passes.<br>
+<iframe src="resources/iframe-with-anchor.html" width="100%" height="2000" scrolling="no"></iframe>
+</body>
+</html>

third_party/WebKit/LayoutTests/fast/dom/HTMLAnchorElement/resources/iframe-with-anchor.html
@@ -1,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+<head>
+<script>
+
+if (window.testRunner) {
+    testRunner.waitUntilDone();
+    testRunner.dumpAsText();
+}
+
+function run() {
+if (window.eventSender) {
+    var anchor = document.getElementById("start");
+    var x = window.frameElement.offsetLeft + anchor.offsetLeft + 2;
+    var y = window.frameElement.offsetTop + anchor.offsetTop + 2;
+    eventSender.mouseMoveTo(x, y);
+    eventSender.mouseDown();
+    eventSender.mouseUp();
+    setTimeout(finish, 1000);
+}
+
+function finish() {
+    var result;
+    if (window.top.scrollY == 0)
+        result = "FAIL: navigating to the anchor should trigger a scroll";
+    else
+        result = "PASS";
+
+    window.top.document.body.appendChild(document.createTextNode(result));
+    if (window.testRunner)
+        testRunner.notifyDone();
+}
+
+}
+</script>
+</head>
+<body onload="run();">
+
+<a id="start" href="#anchor">Go to anchor</a>
+<div style="height: 1000px"></div>
+<a name="anchor">Anchor</a>
+</body>
+</html>