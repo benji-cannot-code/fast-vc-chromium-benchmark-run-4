ebe8bf8885becdf18adfbff53ba41107d76b2608
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-02 10:47:19 +0000

    Return a sensible default locale for renderer tests
    BUG=none
    
    Review URL: https://codereview.chromium.org/11029010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159670 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/mock_render_thread.cc
+++ content/public/test/mock_render_thread.cc
@@ -66,7 +66,7 @@ IPC::SyncChannel* MockRenderThread::GetChannel() {
 }
 
 std::string MockRenderThread::GetLocale() {
-  return std::string();
+  return "en-US";
 }
 
 IPC::SyncMessageFilter* MockRenderThread::GetSyncMessageFilter() {
