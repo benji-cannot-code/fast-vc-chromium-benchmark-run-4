0c8010babdd1802e9b0f05abd24802b06113aeb4
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-02 09:29:14 +0000

    Remove anonymous namespace from StyleBuilder.cpp for better debugging experience https://bugs.webkit.org/show_bug.cgi?id=98124
    Reviewed by Alexander Pavlov.
    
    * css/StyleBuilder.cpp: removed anonymous namespace.
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleBuilder.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleBuilder.cpp
@@ -52,8 +52,6 @@ using namespace std;
 
 namespace WebCore {
 
-namespace {
-
 enum ExpandValueBehavior {SuppressValue = 0, ExpandValue};
 template <ExpandValueBehavior expandValue, CSSPropertyID one = CSSPropertyInvalid, CSSPropertyID two = CSSPropertyInvalid, CSSPropertyID three = CSSPropertyInvalid, CSSPropertyID four = CSSPropertyInvalid, CSSPropertyID five = CSSPropertyInvalid>
 class ApplyPropertyExpanding {
@@ -1783,8 +1781,6 @@ public:
     }
 };
 #endif
-
-} // namespace
 
 const StyleBuilder& StyleBuilder::sharedStyleBuilder()
 {
