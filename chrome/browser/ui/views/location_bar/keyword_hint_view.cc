2e4f148e52e1b83892ef7eac136ef387bef6d338
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-02 00:33:33 +0000

    alternate ntp: fix omnibox border in toolbar
    1) remove white outline around omnibox border by painting the background with
       the gray ntp color before painting the standard border
    2) draw blue focus border whenever omnibox in toolbar has focus
    3) fix bug where bottom edge of border is pushed lower by 1 pixel
    4) after implementing 1 and 2, grayish or bluish patches appear on the inside of
       the left and right of location bar; fixed by combination of the following:
       a) children of LocationBarView that query LoctionBarView::GetColor for
          background color were using a transparent white color whereas edit field
          is using a translucent (close to opaque) white color.  this results in the
          grayish or bluish patches being seen through on the left and right of edit
          field but not the edit field.
          change signature of LocationBarView::GetColor and all callers to pass in
          the InstantExtendedAPI flag so as to use the same translucent white
          background color as edit field;
          cleanup code by moving public LocationBarView::kOmniboxBackgroundColor to
          anonymous namespace so that no other callers will mistakenly use the wrong
          background color again
       b) change drawing order in LocationBarView: draw blue focus border before
          painting background inside omnibox, because the focus border is actually
          drawn as a filled masked blue rect.
       c) paint a solid white background inside omnibox before painting the actual
          translucent background color, so that the last-painted translucent color
          would simply see through to a white background rather than the gray or
          blue background painted by 1 and 2
    
    BUG=152857
    TEST=verify per bug rpt
    
    
    Review URL: https://chromiumcodereview.appspot.com/10986076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159603 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/keyword_hint_view.cc
+++ chrome/browser/ui/views/location_bar/keyword_hint_view.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search_engines/template_url_service.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/ui/search/search.h"
 #include "chrome/browser/ui/views/location_bar/location_bar_view.h"
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
@@ -132,9 +133,13 @@ void KeywordHintView::Layout() {
 
 views::Label* KeywordHintView::CreateLabel() {
   views::Label* label = new views::Label();
-  label->SetBackgroundColor(LocationBarView::GetColor(ToolbarModel::NONE,
+  bool instant_extended_api_enabled =
+      chrome::search::IsInstantExtendedAPIEnabled(profile_);
+  label->SetBackgroundColor(LocationBarView::GetColor(
+      instant_extended_api_enabled, ToolbarModel::NONE,
       LocationBarView::BACKGROUND));
-  label->SetEnabledColor(LocationBarView::GetColor(ToolbarModel::NONE,
+  label->SetEnabledColor(LocationBarView::GetColor(
+      instant_extended_api_enabled, ToolbarModel::NONE,
       LocationBarView::DEEMPHASIZED_TEXT));
   AddChildView(label);
   return label;
