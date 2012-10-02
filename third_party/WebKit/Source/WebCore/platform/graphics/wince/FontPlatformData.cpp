54f54c10a45a60cc7940d65290b9cfb8907dfd1e
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-02 13:36:02 +0000

    [WINCE] Remove FontPlatformData::averageCharWidth() https://bugs.webkit.org/show_bug.cgi?id=97883
    Reviewed by Andreas Kling.
    
    Use SimpleFontData::avgCharWidth() instead to remove duplicated code.
    
    * platform/graphics/wince/FontPlatformData.cpp:
    (WebCore::FixedSizeFontData::create):
    * platform/graphics/wince/FontPlatformData.h:
    (FontPlatformData):
    * platform/graphics/wince/GraphicsContextWinCE.cpp:
    (WebCore::GraphicsContext::drawText):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/wince/FontPlatformData.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/wince/FontPlatformData.cpp
@@ -464,11 +464,6 @@ const LOGFONT& FontPlatformData::logFont() const
     return m_private->m_rootFontData->m_font;
 }
 
-int FontPlatformData::averageCharWidth() const
-{
-    return (m_private->m_rootFontData->m_metrics.tmAveCharWidth * size() + 36) / 72;
-}
-
 bool FontPlatformData::isDisabled() const
 {
     return !isValid() || m_private->m_disabled;
--- third_party/WebKit/Source/WebCore/platform/graphics/wince/GraphicsContextWinCE.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/wince/GraphicsContextWinCE.cpp
@@ -1599,7 +1599,7 @@ void GraphicsContext::drawText(const SimpleFontData* fontData, const GlyphBuffer
     double scaleY = m_data->m_transform.d();
 
     int height = fontData->platformData().size() * scaleY;
-    int width = fontData->platformData().averageCharWidth() * scaleX;
+    int width = fontData->avgCharWidth() * scaleX;
 
     if (!height || !width)
         return;
