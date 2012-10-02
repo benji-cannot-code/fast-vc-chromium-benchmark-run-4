3e035b8f1e4b15d6913a23f6ec881dbbdd8f447c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-02 04:42:14 +0000

    The static function 'deleteFileSystem' in the LocalFileSystem.cpp should have another name. https://bugs.webkit.org/show_bug.cgi?id=98106
    Patch by Dongwoo Joshua Im <dw.im@samsung.com> on 2012-10-01
    Reviewed by Yuta Kitamura.
    
    A build error occurs because there are two functions which have same name in the LocalFileSystem.cpp file.
    One is member function of the class, and the other is a static function which is called by the member function.
    So, I've tried to change the name of the static function from 'deleteFileSystem' to 'performDeleteFileSystem'.
    
    No new functionality, no new test.
    
    * Modules/filesystem/LocalFileSystem.cpp:
    (WebCore::performDeleteFileSystem): The name of function is changed from 'deleteFileSystem'.
    (WebCore::LocalFileSystem::requestFileSystem):
    (WebCore::LocalFileSystem::deleteFileSystem):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/filesystem/LocalFileSystem.cpp
+++ third_party/WebKit/Source/WebCore/Modules/filesystem/LocalFileSystem.cpp
@@ -83,7 +83,7 @@ static void openFileSystem(ScriptExecutionContext*, const String& basePath, cons
     AsyncFileSystem::openFileSystem(basePath, identifier, type, create, callbacks);
 }
 
-static void deleteFileSystem(ScriptExecutionContext*, const String& basePath, const String& identifier, FileSystemType type, PassOwnPtr<AsyncFileSystemCallbacks> callbacks)
+static void performDeleteFileSystem(ScriptExecutionContext*, const String& basePath, const String& identifier, FileSystemType type, PassOwnPtr<AsyncFileSystemCallbacks> callbacks)
 {
     AsyncFileSystem::deleteFileSystem(basePath, identifier, type, callbacks);
 }
@@ -96,7 +96,6 @@ void LocalFileSystem::readFileSystem(ScriptExecutionContext* context, FileSystem
 
 void LocalFileSystem::requestFileSystem(ScriptExecutionContext* context, FileSystemType type, long long, PassOwnPtr<AsyncFileSystemCallbacks> callbacks, FileSystemSynchronousType)
 {
-    // FIXME: Pass FileSystemType parameter.
     // AsyncFileSystem::openFileSystem calls callbacks synchronously, so the method needs to be called asynchronously.
     context->postTask(createCallbackTask(&openFileSystem, fileSystemBasePath(), context->securityOrigin()->databaseIdentifier(), type, true, callbacks));
 }
@@ -104,7 +103,7 @@ void LocalFileSystem::requestFileSystem(ScriptExecutionContext* context, FileSys
 void LocalFileSystem::deleteFileSystem(ScriptExecutionContext* context, FileSystemType type, PassOwnPtr<AsyncFileSystemCallbacks> callbacks)
 {
     // AsyncFileSystem::deleteFileSystem calls callbacks synchronously, so the method needs to be called asynchronously.
-    context->postTask(createCallbackTask(&deleteFileSystem, filesystemBasePath(), context->securityOrigin()->databaseIdentifier(), type, callbacks);
+    context->postTask(createCallbackTask(&performDeleteFileSystem, fileSystemBasePath(), context->securityOrigin()->databaseIdentifier(), type, callbacks));
 }
 
 } // namespace
