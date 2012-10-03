eb48f604dc3ce7dcfb741b9fe24570f9461a34d2
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 00:31:07 +0000

    Support GL_OES_compressed_ETC1_RGB8_texture where available.
    Review URL: https://chromiumcodereview.appspot.com/10981036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159813 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -7081,6 +7081,9 @@ const int kS3TCBlockWidth = 4;
 const int kS3TCBlockHeight = 4;
 const int kS3TCDXT1BlockSize = 8;
 const int kS3TCDXT3AndDXT5BlockSize = 16;
+const int kETC1BlockWidth = 4;
+const int kETC1BlockHeight = 4;
+const int kETC1BlockSize = 8;
 
 bool IsValidDXTSize(GLint level, GLsizei size) {
   return (size == 1) ||
@@ -7115,6 +7118,15 @@ bool GLES2DecoderImpl::ValidateCompressedTexFuncData(
         bytes_required = num_blocks * kS3TCDXT3AndDXT5BlockSize;
         break;
       }
+    case GL_ETC1_RGB8_OES: {
+        int num_blocks_across =
+            (width + kETC1BlockWidth - 1) / kETC1BlockWidth;
+        int num_blocks_down =
+            (height + kETC1BlockHeight - 1) / kETC1BlockHeight;
+        int num_blocks = num_blocks_across * num_blocks_down;
+        bytes_required = num_blocks * kETC1BlockSize;
+        break;
+      }
     default:
       SetGLErrorInvalidEnum(function_name, format, "format");
       return false;
@@ -7145,6 +7157,14 @@ bool GLES2DecoderImpl::ValidateCompressedTexDimensions(
       }
       return true;
     }
+    case GL_ETC1_RGB8_OES:
+      if (width <= 0 || height <= 0) {
+        SetGLError(
+            GL_INVALID_OPERATION, function_name,
+            "width or height invalid for level");
+        return false;
+      }
+      return true;
     default:
       return false;
   }
@@ -7185,6 +7205,12 @@ bool GLES2DecoderImpl::ValidateCompressedTexSubDimensions(
       }
       return ValidateCompressedTexDimensions(
           function_name, level, width, height, format);
+    }
+    case GL_ETC1_RGB8_OES: {
+      SetGLError(
+          GL_INVALID_OPERATION, function_name,
+          "TexsubImage2d not supported for ECT1_RGB8_OES textures");
+      return false;
     }
     default:
       return false;
@@ -7764,7 +7790,8 @@ void GLES2DecoderImpl::DoCopyTexSubImage2D(
   uint32 channels_exist = GLES2Util::GetChannelsForFormat(read_format);
   uint32 channels_needed = GLES2Util::GetChannelsForFormat(format);
 
-  if ((channels_needed & channels_exist) != channels_needed) {
+  if (!channels_needed ||
+      (channels_needed & channels_exist) != channels_needed) {
     SetGLError(
         GL_INVALID_OPERATION, "glCopyTexSubImage2D", "incompatible format");
     return;
