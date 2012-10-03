8dd84f7c9293a48751000eaf9684f9e300c90c96
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-03 00:34:46 +0000

    Try to fix the Snow Leopard build.
    * platform/graphics/opengl/Extensions3DOpenGLCommon.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130239 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/opengl/Extensions3DOpenGLCommon.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/opengl/Extensions3DOpenGLCommon.cpp
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "OpenGLShims.h"
 #endif
 
+#include <wtf/MainThread.h>
 #include <wtf/Vector.h>
 
 namespace WebCore {
