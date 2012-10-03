100ea2ef80afd4eba5752dd66e4223fc6fb52c12
hans@chromium.org <hans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-03 18:31:25 +0000

    Speech JavaScript API: Add SpeechRecognition.interimResults attribute https://bugs.webkit.org/show_bug.cgi?id=98279
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Add the interimResults attribute and pass it to the embedder. It was
    added to the spec draft in
    http://dvcs.w3.org/hg/speech-api/rev/d25fea0d029c
    
    Tested in fast/speech/scripted/basics.html
    
    * Modules/speech/SpeechRecognition.cpp:
    (WebCore::SpeechRecognition::start):
    (WebCore::SpeechRecognition::SpeechRecognition):
    * Modules/speech/SpeechRecognition.h:
    (WebCore::SpeechRecognition::interimResults):
    (WebCore::SpeechRecognition::setInterimResults):
    * Modules/speech/SpeechRecognition.idl:
    * Modules/speech/SpeechRecognitionClient.h:
    (SpeechRecognitionClient):
    * Modules/speech/SpeechRecognitionController.h:
    (WebCore::SpeechRecognitionController::start):
    
    Source/WebKit/chromium:
    
    Plumb interimResults through to Chromium.
    
    * public/WebSpeechRecognitionParams.h:
    (WebKit::WebSpeechRecognitionParams::WebSpeechRecognitionParams):
    (WebKit::WebSpeechRecognitionParams::interimResults):
    (WebSpeechRecognitionParams):
    * src/SpeechRecognitionClientProxy.cpp:
    (WebKit::SpeechRecognitionClientProxy::start):
    * src/SpeechRecognitionClientProxy.h:
    (SpeechRecognitionClientProxy):
    
    LayoutTests:
    
    Update test to check for the interimResults attribute.
    
    * fast/speech/scripted/basics-expected.txt:
    * fast/speech/scripted/basics.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130308 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/speech/SpeechRecognition.cpp
+++ third_party/WebKit/Source/WebCore/Modules/speech/SpeechRecognition.cpp
@@ -55,7 +55,7 @@ void SpeechRecognition::start(ExceptionCode& ec)
     }
 
     setPendingActivity(this);
-    m_controller->start(this, m_grammars.get(), m_lang, m_continuous, m_maxAlternatives);
+    m_controller->start(this, m_grammars.get(), m_lang, m_continuous, m_interimResults, m_maxAlternatives);
     m_started = true;
 }
 
@@ -151,6 +151,7 @@ SpeechRecognition::SpeechRecognition(ScriptExecutionContext* context)
     : ActiveDOMObject(context, this)
     , m_grammars(SpeechGrammarList::create()) // FIXME: The spec is not clear on the default value for the grammars attribute.
     , m_continuous(false)
+    , m_interimResults(false)
     , m_maxAlternatives(1)
     , m_controller(0)
     , m_stoppedByActiveDOMObject(false)
--- third_party/WebKit/Source/WebKit/chromium/public/WebSpeechRecognitionParams.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebSpeechRecognitionParams.h
@@ -38,10 +38,11 @@ class WebSpeechGrammar;
 
 class WebSpeechRecognitionParams {
 public:
-    WebSpeechRecognitionParams(const WebVector<WebSpeechGrammar>& grammars, const WebString& language, bool continuous, unsigned long maxAlternatives, const WebSecurityOrigin& origin)
+    WebSpeechRecognitionParams(const WebVector<WebSpeechGrammar>& grammars, const WebString& language, bool continuous, bool interimResults, unsigned long maxAlternatives, const WebSecurityOrigin& origin)
         : m_grammars(grammars)
         , m_language(language)
         , m_continuous(continuous)
+        , m_interimResults(interimResults)
         , m_maxAlternatives(maxAlternatives)
         , m_origin(origin)
     {
@@ -50,6 +51,7 @@ public:
     const WebVector<WebSpeechGrammar>& grammars() const { return m_grammars; }
     const WebString& language() const { return m_language; }
     bool continuous() const { return m_continuous; }
+    bool interimResults() const { return m_interimResults; }
     unsigned long maxAlternatives() const { return m_maxAlternatives; }
     const WebSecurityOrigin& origin() const { return m_origin; }
 
@@ -57,6 +59,7 @@ private:
     WebVector<WebSpeechGrammar> m_grammars;
     WebString m_language;
     bool m_continuous;
+    bool m_interimResults;
     unsigned long m_maxAlternatives;
     WebSecurityOrigin m_origin;
 };
