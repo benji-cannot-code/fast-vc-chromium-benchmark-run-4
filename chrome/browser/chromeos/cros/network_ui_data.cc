2531b894936fc7cd4f02e71997b921a95e3032e0
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 00:14:59 +0000

    This fixes some really silly memory leaks, and eliminates an unused constructor.
    TBR=nkostylev@chromium.org
    BUG=none
    TEST=compile
    
    Review URL: https://chromiumcodereview.appspot.com/11000061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159807 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/network_ui_data.cc
+++ chrome/browser/chromeos/cros/network_ui_data.cc
@@ -110,12 +110,6 @@ NetworkPropertyUIData::NetworkPropertyUIData()
 NetworkPropertyUIData::~NetworkPropertyUIData() {
 }
 
-NetworkPropertyUIData::NetworkPropertyUIData(Controller controller,
-                                             base::Value* default_value)
-    : controller_(controller),
-      default_value_(default_value) {
-}
-
 NetworkPropertyUIData::NetworkPropertyUIData(
     const NetworkUIData& ui_data) {
   Reset(ui_data);
