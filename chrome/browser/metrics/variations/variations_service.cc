bea7e1175c3d9566e3f2f7ce0f1bf2fc0f9c2dd4
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 22:11:55 +0000

    Measure and log the latency of VariationsService requests.
    TEST=Start Chrome, navigate to about:histograms, and ensure that there is an entry for Variations.RequestSuccessLatency or Variations.RequestNonSuccessLatency.
    BUG=152958
    
    
    Review URL: https://chromiumcodereview.appspot.com/10996067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159989 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/variations/variations_service.cc
+++ chrome/browser/metrics/variations/variations_service.cc
@@ -184,6 +184,8 @@ void VariationsService::DoActualFetch() {
                                                  variations_serial_number_);
   }
   pending_seed_request_->Start();
+
+  last_request_started_time_ = base::TimeTicks::Now();
 }
 
 void VariationsService::FetchVariationsSeed() {
@@ -212,11 +214,19 @@ void VariationsService::OnURLFetchComplete(const net::URLFetcher* source) {
       net::HttpUtil::MapStatusCodeForHistogram(request->GetResponseCode()),
       net::HttpUtil::GetStatusCodesForHistogram());
 
+  const base::TimeDelta latency =
+      base::TimeTicks::Now() - last_request_started_time_;
+
   if (request->GetResponseCode() != 200) {
     DVLOG(1) << "Variations server request returned non-200 response code: "
              << request->GetResponseCode();
+    if (request->GetResponseCode() == 304)
+      UMA_HISTOGRAM_MEDIUM_TIMES("Variations.FetchNotModifiedLatency", latency);
+    else
+      UMA_HISTOGRAM_MEDIUM_TIMES("Variations.FetchOtherLatency", latency);
     return;
   }
+  UMA_HISTOGRAM_MEDIUM_TIMES("Variations.FetchSuccessLatency", latency);
 
   std::string seed_data;
   bool success = request->GetResponseAsString(&seed_data);
