6e980979291a4daab3462d09e1b8146ca246a285
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 20:57:23 +0000

    Fix copied shared quad state in CCDelegatedRendererLayerImpl::appendRenderPassQuads
    Previous code was modifying the shared state multiple times.
    Also, the clip rect was not correctly transformed by the layer's transform, so fixed that.
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11049020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159974 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/CCDelegatedRendererLayerImpl.cpp
+++ cc/CCDelegatedRendererLayerImpl.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CCAppendQuadsData.h"
 #include "CCQuadSink.h"
+#include "CCMathUtil.h"
 #include "CCRenderPassDrawQuad.h"
 #include "CCRenderPassSink.h"
 
@@ -147,19 +148,19 @@ void CCDelegatedRendererLayerImpl::appendRenderPassQuads(CCQuadSink& quadSink, C
         if (quad->sharedQuadState() != currentSharedQuadState) {
             currentSharedQuadState = quad->sharedQuadState();
             copiedSharedQuadState = quadSink.useSharedQuadState(currentSharedQuadState->copy());
+            bool targetIsFromDelegatedRendererLayer = appendQuadsData.renderPassId.layerId == id();
+            if (!targetIsFromDelegatedRendererLayer) {
+              // Should be the root render pass.
+              ASSERT(delegatedRenderPass == m_renderPassesInDrawOrder.last());
+              // This layer must be drawing to a renderTarget other than itself.
+              ASSERT(renderTarget() != this);
+
+              copiedSharedQuadState->clippedRectInTarget = CCMathUtil::mapClippedRect(drawTransform(), copiedSharedQuadState->clippedRectInTarget);
+              copiedSharedQuadState->quadTransform = copiedSharedQuadState->quadTransform * drawTransform();
+              copiedSharedQuadState->opacity *= drawOpacity();
+            }
         }
         ASSERT(copiedSharedQuadState);
-
-        bool targetIsFromDelegatedRendererLayer = appendQuadsData.renderPassId.layerId == id();
-        if (!targetIsFromDelegatedRendererLayer) {
-            // Should be the root render pass.
-            ASSERT(delegatedRenderPass == m_renderPassesInDrawOrder.last());
-            // This layer must be drawing to a renderTarget other than itself.
-            ASSERT(renderTarget() != this);
-
-            copiedSharedQuadState->quadTransform = copiedSharedQuadState->quadTransform * drawTransform();
-            copiedSharedQuadState->opacity *= drawOpacity();
-        }
 
         scoped_ptr<CCDrawQuad> copyQuad;
         if (quad->material() != CCDrawQuad::RenderPass)
