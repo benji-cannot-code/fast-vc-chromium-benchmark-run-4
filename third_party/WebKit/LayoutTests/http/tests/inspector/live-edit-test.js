b4586e7269c439eff3072a25f0fc14e3c4973e31
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-03 14:37:38 +0000

    Web Inspector: UISourceCode commitWorkingCopy should not fail when original script had syntax error. https://bugs.webkit.org/show_bug.cgi?id=97272
    Reviewed by Pavel Feldman.
    
    This patch is based on patch by John J. Barton.
    
    Source/WebCore:
    
    * inspector/front-end/JavaScriptSource.js:
    (WebInspector.JavaScriptSource.prototype.workingCopyCommitted): added rawLocation null check.
    
    LayoutTests:
    
    * http/tests/inspector/live-edit-test.js:
    (initialize_LiveEditTest.InspectorTest.replaceInSource):
    (initialize_LiveEditTest):
    * inspector/debugger/live-edit-expected.txt:
    * inspector/debugger/live-edit.html:
    * inspector/debugger/resources/edit-me-syntax-error.js: Added.
    (syntaxError):
    * platform/chromium/TestExpectations: re-enabled the test.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130287 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/live-edit-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/live-edit-test.js
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var initialize_LiveEditTest = function() {
 
-InspectorTest.replaceInSource = function(sourceFrame, string, replacement, callback)
+InspectorTest.replaceInSource = function(sourceFrame, string, replacement)
 {
     sourceFrame._textEditor._mainPanel.setReadOnly(false);
     var oldRange, newRange;
