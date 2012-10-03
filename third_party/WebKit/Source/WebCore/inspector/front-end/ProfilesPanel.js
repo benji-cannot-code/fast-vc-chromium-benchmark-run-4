ba1828b97855b412b9b030b8ae8b638d5c33ef0f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-03 09:50:06 +0000

    Web Inspector: Profiles: taking heap snapshot causes error message in console. https://bugs.webkit.org/show_bug.cgi?id=97890
    Patch by Eugene Klyuchnikov <eustas.bug@gmail.com> on 2012-10-03
    Reviewed by Yury Semikhatsky.
    
    Actual problem is that proxy loader is closed twice.
    
    "_snapshotReceived" should never try to close receiver,
    because it is a callback fired by close.
    
    Also minor glitches fixed:
     - taking snapshot shows "Loading..." status first,
    and then "Saving xxx%";
     - after all chunks are sent "Parsing" status is set and
    then is replaced with "Saving 100%";
     - removed dead code in proxy;
     - proxy was ignoring callback parameter;
     - "Loading %d\%" is not localized.
    
    * English.lproj/localizedStrings.js: Added missing "Loading %d%" string.
    * inspector/front-end/HeapSnapshotProxy.js: Removed dead code.
    (WebInspector.HeapSnapshotLoaderProxy):
    (WebInspector.HeapSnapshotLoaderProxy.prototype.write): Make this method
    interface-conformant.
    * inspector/front-end/HeapSnapshotView.js:
    (WebInspector.HeapProfileHeader): Fixed update-status and
    finish-transfer logic.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ProfilesPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ProfilesPanel.js
@@ -675,7 +675,7 @@ WebInspector.ProfilesPanel.prototype = {
         var profile = this._profilesIdMap[this._makeKey(uid, WebInspector.HeapSnapshotProfileType.TypeId)];
         if (!profile)
             return;
-        profile.finishHeapSnapshot(false);
+        profile.finishHeapSnapshot();
     },
 
     /**
