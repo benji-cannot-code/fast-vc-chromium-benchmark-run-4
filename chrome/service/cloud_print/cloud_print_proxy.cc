fcf4955b6eb005a5c0e3a064b3fff28df9ef34f1
gene@chromium.org <gene@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 00:33:22 +0000

    Disabling CloudPrint for experiement on staging. This should prevent CloudPrint from running, but keep all user configuration intact.
    BUG=none
    
    Review URL: https://codereview.chromium.org/11016019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/cloud_print/cloud_print_proxy.cc
+++ chrome/service/cloud_print/cloud_print_proxy.cc
@@ -137,6 +137,10 @@ void CloudPrintProxy::EnableForUserWithRobot(
 
 bool CloudPrintProxy::CreateBackend() {
   DCHECK(CalledOnValidThread());
+  // Temporary fix to disable CloudPrint for Chrome canary build.
+  return false;
+
+  /*
   if (backend_.get())
     return false;
 
@@ -156,6 +160,7 @@ bool CloudPrintProxy::CreateBackend() {
   backend_.reset(new CloudPrintProxyBackend(this, settings_, oauth_client_info,
                                             enable_job_poll));
   return true;
+  */
 }
 
 void CloudPrintProxy::UnregisterPrintersAndDisableForUser() {
