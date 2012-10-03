67b5eae7e22b625539c07fcd423cbfdc238f4cea
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 01:44:46 +0000

    Implement part of Android WebView.clearCache
    For velocity, not refactoring BrowsingDataRemover code into content/.
    This method is similar to BrowsingDataRemover::REMOVE_CACHE mask. Still need to implement clearing http disk cache.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11051003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159827 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/android_webview.gyp
+++ android_webview/android_webview.gyp
@@ -54,6 +54,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'lib/main/aw_main_delegate.h',
         'lib/main/webview_entry_point.cc',
         'lib/main/webview_stubs.cc',
+        'renderer/aw_render_process_observer.cc',
+        'renderer/aw_render_process_observer.h',
         'renderer/aw_render_view_ext.cc',
         'renderer/aw_render_view_ext.h',
       ],
--- android_webview/native/aw_contents.cc
+++ android_webview/native/aw_contents.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/native/aw_contents.h"
 
 #include "android_webview/browser/renderer_host/aw_render_view_host_ext.h"
+#include "android_webview/common/render_view_messages.h"
 #include "android_webview/native/aw_browser_dependency_factory.h"
 #include "android_webview/native/aw_contents_container.h"
 #include "android_webview/native/aw_web_contents_delegate.h"
@@ -242,6 +243,14 @@ void AwContents::FindNext(JNIEnv* env, jobject obj, jboolean forward) {
 
 void AwContents::ClearMatches(JNIEnv* env, jobject obj) {
   GetFindHelper()->ClearMatches();
+}
+
+void AwContents::ClearCache(
+    JNIEnv* env,
+    jobject obj,
+    jboolean include_disk_files) {
+  render_view_host_ext_->Send(new AwViewMsg_ClearCache);
+  // TODO(boliu): Implement clear network disk cache.
 }
 
 FindHelper* AwContents::GetFindHelper() {
