3582db657d70248760a7de9e38a725af52660c4a
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 20:10:44 +0000

    Add Android fingerprint to metrics logs
    (Upstreaming)
    
    Adds information about the Android build fingerprint to information that we
    send back in our metrics.
    
    BUG=153694
    
    Review URL: https://chromiumcodereview.appspot.com/11014006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159957 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_log.cc
+++ chrome/browser/metrics/metrics_log.cc
@@ -46,6 +46,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/screen.h"
 #include "webkit/plugins/webplugininfo.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/build_info.h"
+#endif
+
 #define OPEN_ELEMENT_FOR_SCOPE(name) ScopedElement scoped_element(this, name)
 
 #if defined(OS_WIN)
@@ -779,6 +783,10 @@ void MetricsLog::RecordEnvironmentProto(
 #endif
   os->set_name(os_name);
   os->set_version(base::SysInfo::OperatingSystemVersion());
+#if defined(OS_ANDROID)
+  os->set_fingerprint(
+      base::android::BuildInfo::GetInstance()->android_build_fp());
+#endif
 
   const content::GPUInfo& gpu_info =
       GpuDataManager::GetInstance()->GetGPUInfo();
