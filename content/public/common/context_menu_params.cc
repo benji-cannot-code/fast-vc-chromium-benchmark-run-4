fc4742d339b8e188a06e024ac04e89831f054d3f
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 21:01:07 +0000

    Adding link_text copy functionality.
    Adding link_text copy functionality that will be used for Android.
    
    BUG=152196
    
    
    Review URL: https://chromiumcodereview.appspot.com/11046003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159979 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/context_menu_params.cc
+++ content/public/common/context_menu_params.cc
@@ -5,8 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/common/context_menu_params.h"
 
+#include "base/logging.h"
 #include "content/common/ssl_status_serialization.h"
 #include "webkit/glue/glue_serialize.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebNode.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebElement.h"
 
 namespace content {
 
@@ -71,6 +74,25 @@ ContextMenuParams::ContextMenuParams(const WebKit::WebContextMenuData& data)
   if (!data.frameHistoryItem.isNull()) {
     frame_content_state =
         webkit_glue::HistoryItemToString(data.frameHistoryItem);
+  }
+
+  if (!link_url.is_empty()) {
+    WebKit::WebNode selectedNode = data.node;
+
+    // If there are other embedded tags (like <a ..>Some <b>text</b></a>)
+    // we need to extract the parent <a/> node.
+    while (!selectedNode.isLink() && !selectedNode.parentNode().isNull()) {
+      selectedNode = selectedNode.parentNode();
+    }
+
+    WebKit::WebElement selectedElement = selectedNode.to<WebKit::WebElement>();
+    if (selectedNode.isLink() && !selectedElement.isNull()) {
+      link_text = selectedElement.innerText();
+    } else {
+      LOG(ERROR) << "Creating a ContextMenuParams for a node that has a link"
+                 << "url but is not an ElementNode or does not have an"
+                 << "ancestor that is a link.";
+    }
   }
 
   // Deserialize the SSL info.
