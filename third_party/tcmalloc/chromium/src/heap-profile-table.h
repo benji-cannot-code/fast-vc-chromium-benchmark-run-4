7c22e3a881eb03ef7c77cbc4a87853806ba8f495
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 19:45:00 +0000

    Dump type statistics of malloc'ed objects.
    This change adds type statistics in heap-profiler.  It dumps
    "prefix.<pid>.<num>.type" along with heap-profiles (.heap),
    which contains type information.
    
    Type statistics is for example "    13:      520 @ N3WTF5MutexE",
    which means that 13 objects of WTF::Mutex occupy 520 bytes.
    
    It depends on a modified version of the LLVM/Clang compiler 
    at deps/third_party/llvm-allocated-type. 
    
    
    BUG=123758
    
    Review URL: https://chromiumcodereview.appspot.com/10830113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159952 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/heap-profile-table.h
+++ third_party/tcmalloc/chromium/src/heap-profile-table.h
@@ -40,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/logging.h"   // for RawFD
 
+#if defined(TYPE_PROFILING)
+#include <gperftools/type_profiler_map.h>
+#endif  // defined(TYPE_PROFILING)
+
 // Table to maintain a heap profile data inside,
 // i.e. the set of currently active heap memory allocations.
 // thread-unsafe and non-reentrant code:
@@ -239,6 +243,10 @@ class HeapProfileTable {
   // used for leak checking (using HeapLeakChecker).
   void DumpMarkedObjects(AllocationMark mark, const char* file_name);
 
+#if defined(TYPE_PROFILING)
+  void DumpTypeStatistics(const char* file_name) const;
+#endif  // defined(TYPE_PROFILING)
+
  private:
   friend class DeepHeapProfile;
 
@@ -322,6 +330,18 @@ class HeapProfileTable {
     MarkArgs(AllocationMark m, bool a) : mark(m), mark_all(a) { }
   };
 
+#if defined(TYPE_PROFILING)
+  struct TypeCount {
+    size_t bytes;
+    unsigned int objects;
+
+    TypeCount(size_t bytes_arg, unsigned int objects_arg)
+        : bytes(bytes_arg),
+          objects(objects_arg) {
+    }
+  };
+#endif  // defined(TYPE_PROFILING)
+
   // helpers ----------------------------
 
   // Unparse bucket b and print its portion of profile dump into buf.
@@ -383,6 +403,16 @@ class HeapProfileTable {
   // Helper for filling size variables in buckets by zero.
   inline static void ZeroBucketCountsIterator(
       const void* ptr, AllocValue* v, HeapProfileTable* heap_profile);
+
+#if defined(TYPE_PROFILING)
+  inline static void TallyTypesItererator(const void* ptr,
+                                          AllocValue* value,
+                                          AddressMap<TypeCount>* type_size_map);
+
+  inline static void DumpTypesIterator(const void* ptr,
+                                       TypeCount* size,
+                                       const DumpArgs& args);
+#endif  // defined(TYPE_PROFILING)
 
   // Helper for IterateOrderedAllocContexts and FillOrderedProfile.
   // Creates a sorted list of Buckets whose length is num_alloc_buckets_ +
