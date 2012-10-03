1ccb54e044aa28ca0e1ba7475348d9f90e5735de
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-03 19:17:18 +0000

    Element::computeInheritedLanguage: evaluate the while() condition after fetching the string https://bugs.webkit.org/show_bug.cgi?id=98220
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-10-03
    Reviewed by Andreas Kling.
    
    * dom/Element.cpp:
    (WebCore::Element::computeInheritedLanguage):
    The condition is never false on the first execution. Move the condition to the
    end of the loop for fun and profit.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130311 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -1757,7 +1757,7 @@ AtomicString Element::computeInheritedLanguage() const
     const Node* n = this;
     AtomicString value;
     // The language property is inherited, so we iterate over the parents to find the first language.
-    while (n && value.isNull()) {
+    do {
         if (n->isElementNode()) {
             // Spec: xml:lang takes precedence -- http://www.w3.org/TR/xhtml1/#C_7
             value = static_cast<const Element*>(n)->fastGetAttribute(XMLNames::langAttr);
@@ -1769,7 +1769,7 @@ AtomicString Element::computeInheritedLanguage() const
         }
 
         n = n->parentNode();
-    }
+    } while (n && value.isNull());
 
     return value;
 }
