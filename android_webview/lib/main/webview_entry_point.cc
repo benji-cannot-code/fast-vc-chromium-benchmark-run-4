bb4666108ee41ae38d1a7f8e400b2d75de6cfb42
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-03 15:44:33 +0000

    [Android] Set a default locale for WebView.
    Recent WebKit rolls have introduced an assertion for the default language that is causing
    some failures in WebView. The reason is that the default locale is not properly iset in
    single process mode. However, since this will probably require more elaborate solution
    we'll be setting a value manually for now to unblock tests and prevent further issues.
    
    BUG=153758
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11054012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@159899 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/lib/main/webview_entry_point.cc
+++ android_webview/lib/main/webview_entry_point.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/app/content_main.h"
 #include "content/public/browser/android/compositor.h"
 #include "content/public/common/content_switches.h"
+#include "ui/base/ui_base_switches.h"
 
 // This is called by the VM when the shared library is first loaded.
 // Most of the initialization is done in LibraryLoadedOnMainThread(), not here.
@@ -27,6 +28,12 @@ JNI_EXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
   CommandLine::Init(0, NULL);
   CommandLine::ForCurrentProcess()->AppendSwitch(
       switches::kEnableWebViewSynchronousAPIs);
+
+  // TODO: The default locale needs to be set in order to prevent assertion
+  // failures in WebKit. However, this is really a single process mode issue
+  // and should be properly fixed. See bug 153758.
+  CommandLine::ForCurrentProcess()->AppendSwitchASCII(
+      switches::kLang, "en-US");
 
   // TODO: The next two lines are temporarily required for the renderer
   // initialization to not crash.
