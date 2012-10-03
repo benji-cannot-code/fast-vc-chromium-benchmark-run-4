a3beb14cfe5cda519f38b9fd0bca13a3e68c60f5
arko@motorola.com <arko@motorola.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-03 06:28:44 +0000

    Microdata: itemprop names must not override builtin properties. https://bugs.webkit.org/show_bug.cgi?id=98025
    Reviewed by Kentaro Hara.
    
    Source/WebCore: 
    
    We should look in the prototype for functions before assuming it as
    an item's name. Return false if the prototype of the object has a
    property (function) with propertyName.
    Named properties Spec: http://dev.w3.org/2006/webapi/WebIDL/#idl-named-properties
    Named property visibility algorithm:
    http://dev.w3.org/2006/webapi/WebIDL/#indexed-and-named-properties
    ...
    7. If the result of calling the [[HasProperty]] internal method on
    prototype with property name P is true, then return false.
    ...
    Also [OverrideBuiltins] is not declared for any of the properties,
    hence no overriding is allowed in this case.
    
    Test: fast/dom/MicroData/itemprop-names-override-builtin-properties.html
    
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateGetOwnPropertySlotBody):
    (GenerateGetOwnPropertyDescriptorBody):
    
    LayoutTests: 
    
    Added test case to verify that itemprop names must not override builtin properties.
    
    * fast/dom/MicroData/itemprop-names-override-builtin-properties-expected.txt: Added.
    * fast/dom/MicroData/itemprop-names-override-builtin-properties.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorJS.pm
@@ -392,7 +392,7 @@ sub GenerateGetOwnPropertySlotBody
 
     my @getOwnPropertySlotImpl = ();
 
-    if ($interfaceName eq "NamedNodeMap" or $interfaceName eq "HTMLCollection" or $interfaceName eq "HTMLAllCollection") {
+    if ($interfaceName eq "NamedNodeMap" or $interfaceName eq "HTMLCollection" or $interfaceName eq "HTMLAllCollection" or $interfaceName eq "HTMLPropertiesCollection") {
         push(@getOwnPropertySlotImpl, "    ${namespaceMaybe}JSValue proto = thisObject->prototype();\n");
         push(@getOwnPropertySlotImpl, "    if (proto.isObject() && jsCast<${namespaceMaybe}JSObject*>(asObject(proto))->hasProperty(exec, propertyName))\n");
         push(@getOwnPropertySlotImpl, "        return false;\n\n");
@@ -484,7 +484,7 @@ sub GenerateGetOwnPropertyDescriptorBody
         push(@implContent, "        return false;\n");
     }
     
-    if ($interfaceName eq "NamedNodeMap" or $interfaceName eq "HTMLCollection" or $interfaceName eq "HTMLAllCollection") {
+    if ($interfaceName eq "NamedNodeMap" or $interfaceName eq "HTMLCollection" or $interfaceName eq "HTMLAllCollection" or $interfaceName eq "HTMLPropertiesCollection") {
         push(@getOwnPropertyDescriptorImpl, "    ${namespaceMaybe}JSValue proto = thisObject->prototype();\n");
         push(@getOwnPropertyDescriptorImpl, "    if (proto.isObject() && jsCast<${namespaceMaybe}JSObject*>(asObject(proto))->hasProperty(exec, propertyName))\n");
         push(@getOwnPropertyDescriptorImpl, "        return false;\n\n");
