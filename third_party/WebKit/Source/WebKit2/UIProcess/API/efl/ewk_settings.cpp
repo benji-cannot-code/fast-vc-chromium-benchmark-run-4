317c52f2e5d32e846ca245b85d3c8bdd2070adb9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-04 15:52:29 +0000

    [EFL][WK2] Add setting to allow file access from file:// URLs https://bugs.webkit.org/show_bug.cgi?id=98121
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-10-04
    Reviewed by Laszlo Gombos.
    
    Source/WebKit2:
    
    Add Ewk setting to set / query permission to access
    files from file:// URLs.
    
    * UIProcess/API/efl/ewk_settings.cpp:
    (ewk_settings_file_access_from_file_urls_allowed_set):
    (ewk_settings_file_access_from_file_urls_allowed_get):
    * UIProcess/API/efl/ewk_settings.h:
    * UIProcess/API/efl/tests/resources/local_file_access.html: Added.
    * UIProcess/API/efl/tests/test_ewk2_settings.cpp:
    (TEST_F): Add unit tests for new ewk setting.
    
    Tools:
    
    Allow file access from file:// URLs by default in Minibrowser
    to facilitate testing.
    
    * MiniBrowser/efl/main.c:
    (browserCreate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_settings.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_settings.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ewk_settings_private.h"
 #include <WebKit2/WKPreferences.h>
+#include <WebKit2/WKPreferencesPrivate.h>
 
 using namespace WebKit;
 
@@ -99,4 +100,20 @@ Eina_Bool ewk_settings_developer_extras_enabled_get(const Ewk_Settings* settings
     EINA_SAFETY_ON_NULL_RETURN_VAL(settings, false);
 
     return WKPreferencesGetDeveloperExtrasEnabled(settings->preferences.get());
+}
+
+Eina_Bool ewk_settings_file_access_from_file_urls_allowed_set(Ewk_Settings* settings, Eina_Bool enable)
+{
+    EINA_SAFETY_ON_NULL_RETURN_VAL(settings, false);
+
+    WKPreferencesSetFileAccessFromFileURLsAllowed(settings->preferences.get(), enable);
+
+    return true;
+}
+
+Eina_Bool ewk_settings_file_access_from_file_urls_allowed_get(const Ewk_Settings* settings)
+{
+    EINA_SAFETY_ON_NULL_RETURN_VAL(settings, false);
+
+    return WKPreferencesGetFileAccessFromFileURLsAllowed(settings->preferences.get());
 }
