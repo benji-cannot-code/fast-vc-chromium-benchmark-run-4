acf760dcb10544616b6d3ca6e5d69c9b51470504
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 03:57:20 +0000

    Update context menu for v2 packaged apps in App List / Launcher
    The menu for packaged apps will now be:
      Launch
      Create shortcut (win) / Pin to Launcher (cros)
      Uninstall
    
    Also, Remove from Chrome has been changed to Uninstall for all app types.
    
    BUG=153333
    
    
    Review URL: https://chromiumcodereview.appspot.com/11051013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160076 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/app_list/app_list_controller_ash.cc
+++ chrome/browser/ui/ash/app_list/app_list_controller_ash.cc
@@ -34,6 +34,14 @@ bool AppListControllerAsh::CanPin() {
   return ChromeLauncherController::instance()->CanPin();
 }
 
+bool AppListControllerAsh::CanShowCreateShortcutsDialog() {
+  return false;
+}
+
+void AppListControllerAsh::ShowCreateShortcutsDialog(
+    Profile* profile,
+    const std::string& extension_id) {}
+
 void AppListControllerAsh::ActivateApp(Profile* profile,
                                        const std::string& extension_id,
                                        int event_flags) {
--- chrome/browser/ui/views/app_list/app_list_controller_win.cc
+++ chrome/browser/ui/views/app_list/app_list_controller_win.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_controller.h"
 #include "chrome/browser/ui/app_list/app_list_view_delegate.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
+#include "chrome/browser/ui/views/browser_dialogs.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_switches.h"
 #include "grit/generated_resources.h"
@@ -50,6 +51,10 @@ class AppListControllerWin : public AppListController {
   virtual void PinApp(const std::string& extension_id) OVERRIDE;
   virtual void UnpinApp(const std::string& extension_id) OVERRIDE;
   virtual bool CanPin() OVERRIDE;
+  virtual bool CanShowCreateShortcutsDialog() OVERRIDE;
+  virtual void ShowCreateShortcutsDialog(
+      Profile* profile,
+      const std::string& extension_id) OVERRIDE;
   virtual void ActivateApp(Profile* profile,
                            const std::string& extension_id,
                            int event_flags) OVERRIDE;
@@ -77,6 +82,21 @@ void AppListControllerWin::UnpinApp(const std::string& extension_id) {}
 
 bool AppListControllerWin::CanPin() {
   return false;
+}
+
+bool AppListControllerWin::CanShowCreateShortcutsDialog() {
+  return true;
+}
+
+void AppListControllerWin::ShowCreateShortcutsDialog(
+    Profile* profile,
+    const std::string& extension_id) {
+  ExtensionService* service = profile->GetExtensionService();
+  DCHECK(service);
+  const extensions::Extension* extension = service->GetInstalledExtension(
+      extension_id);
+  DCHECK(extension);
+  chrome::ShowCreateChromeAppShortcutsDialog(NULL, profile, extension);
 }
 
 void AppListControllerWin::ActivateApp(Profile* profile,
