6f784cd0ab290975ed0347d90afc7b226ea758ca
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-04 02:17:34 +0000

    Chromium needs support for border radius clipping https://bugs.webkit.org/show_bug.cgi?id=69866
    Patch by Douglas Stockwell <dstockwell@chromium.org> on 2012-10-03
    Reviewed by Stephen White.
    
    Source/WebCore:
    
    Changes to make this suitably efficient have already landed upstream in skia:
    http://code.google.com/p/skia/source/detail?r=2924
    
    Covered by existing tests.
    
    * rendering/RenderLayer.cpp:
    
    LayoutTests:
    
    * platform/chromium-linux/fast/clip/overflow-border-radius-combinations-expected.png:
    * platform/chromium-linux/fast/clip/overflow-border-radius-fixed-position-expected.png:
    * platform/chromium-linux/fast/clip/overflow-border-radius-transformed-expected.png:
    * platform/chromium-linux/http/tests/media/video-buffered-range-contains-currentTime-expected.png:
    * platform/chromium-linux/media/audio-controls-rendering-expected.png:
    * platform/chromium-linux/media/audio-repaint-expected.png:
    * platform/chromium-linux/media/controls-after-reload-expected.png:
    * platform/chromium-linux/media/controls-strict-expected.png:
    * platform/chromium-linux/media/controls-styling-expected.png:
    * platform/chromium-linux/media/controls-styling-strict-expected.png:
    * platform/chromium-linux/media/controls-without-preload-expected.png:
    * platform/chromium-linux/media/track/track-cue-rendering-horizontal-expected.png:
    * platform/chromium-linux/media/track/track-cue-rendering-vertical-expected.png:
    * platform/chromium-linux/media/video-controls-rendering-expected.png:
    * platform/chromium-linux/media/video-display-toggle-expected.png:
    * platform/chromium-linux/media/video-no-audio-expected.png:
    * platform/chromium-linux/media/video-playing-and-pause-expected.png:
    * platform/chromium-linux/media/video-zoom-controls-expected.png:
    * platform/chromium/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130355 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -110,9 +110,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SVGNames.h"
 #endif
 
-#if PLATFORM(CHROMIUM) || PLATFORM(BLACKBERRY)
-// FIXME: border radius clipping triggers too-slow path on Chromium
-// https://bugs.webkit.org/show_bug.cgi?id=69866
+#if PLATFORM(BLACKBERRY)
 #define DISABLE_ROUNDED_CORNER_CLIPPING
 #endif
 
