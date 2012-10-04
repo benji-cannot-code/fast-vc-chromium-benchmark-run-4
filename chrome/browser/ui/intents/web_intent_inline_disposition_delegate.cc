94b4a14d0d585f243efbeea7a34119efd8f6d1e3
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 21:25:53 +0000

    gtk: improve web intent picker inline intent display
    BUG=153998
    
    
    Review URL: https://chromiumcodereview.appspot.com/11033022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_inline_disposition_delegate.cc
+++ chrome/browser/ui/intents/web_intent_inline_disposition_delegate.cc
@@ -90,6 +90,7 @@ bool WebIntentInlineDispositionDelegate::OnMessageReceived(
   IPC_END_MESSAGE_MAP()
   return handled;
 }
+
 void WebIntentInlineDispositionDelegate::RenderViewCreated(
     content::RenderViewHost* render_view_host) {
   DCHECK(render_view_host);
