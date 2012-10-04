c72b5a84e3d87e61653763645921a64b9dcdb0e2
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 21:07:27 +0000

    Fix zoom icon/bubble for virtual URLs (i.e. chrome://settings).
    R=khorimoto@chromium.org,sky@chromium.org
    BUG=153950
    
    
    Review URL: https://chromiumcodereview.appspot.com/11040031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/zoom/zoom_controller.cc
+++ chrome/browser/ui/zoom/zoom_controller.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/host_zoom_map.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_types.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_service.h"
@@ -81,8 +82,15 @@ void ZoomController::Observe(int type,
 }
 
 void ZoomController::UpdateState(const std::string& host) {
-  if (!host.empty() &&
-      host != net::GetHostOrSpecFromURL(web_contents()->GetURL())) {
+  if (host.empty())
+    return;
+
+  // Use the active navigation entry's URL instead of the WebContents' so
+  // virtual URLs work (e.g. chrome://settings). http://crbug.com/153950
+  content::NavigationEntry* active_entry =
+      web_contents()->GetController().GetActiveEntry();
+  if (!active_entry ||
+      host != net::GetHostOrSpecFromURL(active_entry->GetURL())) {
     return;
   }
 
