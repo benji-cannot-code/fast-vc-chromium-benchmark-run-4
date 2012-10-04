46e0afd1fcf7b3940c9b1f8ada69fc398f9e182c
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 22:48:57 +0000

    Disable AudioLayoutTest.AudioConstructor and AudioLayoutTest.AudioConstructorSrc on Linux Aura since they fail there.
    BUG=154157
    Review URL: https://codereview.chromium.org/11066031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160253 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/audio_browsertest.cc
+++ content/browser/audio_browsertest.cc
@@ -16,6 +16,8 @@ IN_PROC_BROWSER_TEST_F(AudioLayoutTest, AudioConstructorPreload) {
   RunLayoutTest("audio-constructor-preload.html");
 }
 
+#if defined(OS_LINUX) && defined(USE_AURA)
+// http://crbug.com/154157
 IN_PROC_BROWSER_TEST_F(AudioLayoutTest, AudioConstructor) {
   RunLayoutTest("audio-constructor.html");
 }
@@ -23,6 +25,7 @@ IN_PROC_BROWSER_TEST_F(AudioLayoutTest, AudioConstructor) {
 IN_PROC_BROWSER_TEST_F(AudioLayoutTest, AudioConstructorSrc) {
   RunLayoutTest("audio-constructor-src.html");
 }
+#endif  // defined(OS_LINUX) && defined(USE_AURA)
 
 IN_PROC_BROWSER_TEST_F(AudioLayoutTest, AudioDataUrl) {
   RunLayoutTest("audio-data-url.html");
