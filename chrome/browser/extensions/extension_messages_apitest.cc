885f25d5ad31d2d236174da929f48f29af569fef
jennb@chromium.org <jennb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 20:07:14 +0000

    Ensure panels are enabled for tests that create panels.
    BUG=152025
    TEST=PanelMessagingTest.MessagingPanel
    
    
    Review URL: https://chromiumcodereview.appspot.com/11045012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160209 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_messages_apitest.cc
+++ chrome/browser/extensions/extension_messages_apitest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/chrome_switches.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_service.h"
 #include "googleurl/src/gurl.h"
@@ -93,6 +94,13 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MessagingEventURL) {
 }
 
 // Tests connecting from a panel to its extension.
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MessagingPanel) {
+class PanelMessagingTest : public ExtensionApiTest {
+  virtual void SetUpCommandLine(CommandLine* command_line) {
+    ExtensionApiTest::SetUpCommandLine(command_line);
+    command_line->AppendSwitch(switches::kEnablePanels);
+  }
+};
+
+IN_PROC_BROWSER_TEST_F(PanelMessagingTest, MessagingPanel) {
   ASSERT_TRUE(RunExtensionTest("messaging/connect_panel")) << message_;
 }
