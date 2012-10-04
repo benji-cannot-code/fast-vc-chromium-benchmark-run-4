9277ba8288a8cb0fee21d419a73fdfc3d50a9103
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 12:32:18 +0000

    [Android] Fix AwSettingsTest.testUserAgent... tests.
    Upstream missing code.
    
    This fixes the following tests:
      AwSettingsTest.testUserAgentStringOverrideForHistory
      AwSettingsTest.testUserAgentStringNormal
      AwSettingsTest.testUserAgentStringIncognito
      AwSettingsTest.testUserAgentStringBoth
    
    R=mkosiba@chromium.org,bulach@chromium.org
    BUG=153516
    
    Review URL: https://codereview.chromium.org/11013022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160120 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1150,12 +1150,8 @@ public class AwSettingsTest extends AndroidWebViewTestBase {
 
     // Verify that the current UA override setting has a priority over UA
     // overrides in navigation history entries.
-    /**
-     * @SmallTest
-     * @Feature({"Android-WebView", "Preferences"})
-     * BUG=153516
-     */
-    @FlakyTest
+    @SmallTest
+    @Feature({"Android-WebView", "Preferences"})
     public void testUserAgentStringOverrideForHistory() throws Throwable {
         final TestAwContentsClient contentClient = new TestAwContentsClient();
         final ContentViewCore contentView =
@@ -1192,12 +1188,8 @@ public class AwSettingsTest extends AndroidWebViewTestBase {
         assertEquals(page2Title + defaultUserAgentString, getTitleOnUiThread(contentView));
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"Android-WebView", "Preferences"})
-     * BUG=153516
-     */
-    @FlakyTest
+    @SmallTest
+    @Feature({"Android-WebView", "Preferences"})
     public void testUserAgentStringNormal() throws Throwable {
         ViewPair views = createViews(NORMAL_VIEW, NORMAL_VIEW);
         runPerViewSettingsTest(
@@ -1205,12 +1197,8 @@ public class AwSettingsTest extends AndroidWebViewTestBase {
             new AwSettingsUserAgentStringTestHelper(views.getView1(), views.getClient1()));
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"Android-WebView", "Preferences"})
-     * BUG=153516
-     */
-    @FlakyTest
+    @SmallTest
+    @Feature({"Android-WebView", "Preferences"})
     public void testUserAgentStringIncognito() throws Throwable {
         ViewPair views = createViews(INCOGNITO_VIEW, INCOGNITO_VIEW);
         runPerViewSettingsTest(
@@ -1218,12 +1206,8 @@ public class AwSettingsTest extends AndroidWebViewTestBase {
             new AwSettingsUserAgentStringTestHelper(views.getView1(), views.getClient1()));
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"Android-WebView", "Preferences"})
-     * BUG=153516
-     */
-    @FlakyTest
+    @SmallTest
+    @Feature({"Android-WebView", "Preferences"})
     public void testUserAgentStringBoth() throws Throwable {
         ViewPair views = createViews(NORMAL_VIEW, INCOGNITO_VIEW);
         runPerViewSettingsTest(
