3ed84ba9a6e3f5d730e744cd952548d76b7747f3
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 13:33:09 +0000

    find_bar: Fix header include path to views file.
    BUG=125846
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/11017005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160129 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
+++ chrome/browser/ui/find_bar/find_bar_host_browsertest.cc
@@ -39,7 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/keycodes/keyboard_codes.h"
 
 #if defined(TOOLKIT_VIEWS)
-#include "chrome/browser/ui/views/find_bar_host.h"
+#include "chrome/browser/ui/views/dropdown_bar_host.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/widget/widget.h"
 #elif defined(TOOLKIT_GTK)
