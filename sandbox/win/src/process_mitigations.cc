fd22e7064a0e6b7b96c0dd7a6e6c10766a2cd48d
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 22:44:58 +0000

    Make mitigation failures CHECK
    This is a temporary change to narrow down failures enabling mitigations on Win8. I'll revert once the cause is determined.
    
    BUG=153399
    Review URL: https://codereview.chromium.org/11040046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160252 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/win/src/process_mitigations.cc
+++ sandbox/win/src/process_mitigations.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "sandbox/win/src/process_mitigations.h"
 
+#include "base/logging.h"
 #include "base/win/windows_version.h"
 #include "sandbox/win/src/nt_internals.h"
 #include "sandbox/win/src/sandbox_utils.h"
@@ -46,9 +47,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
 
     // Check for SetDefaultDllDirectories since it requires KB2533623.
     if (set_default_dll_directories) {
-      if (!set_default_dll_directories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS) &&
-          ERROR_ACCESS_DENIED != ::GetLastError()) {
-        return false;
+      if (!set_default_dll_directories(LOAD_LIBRARY_SEARCH_DEFAULT_DIRS)) {
+        CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
       }
     }
   }
@@ -57,9 +57,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
   if (version >= base::win::VERSION_VISTA &&
       (flags & MITIGATION_HEAP_TERMINATE)) {
     if (!::HeapSetInformation(NULL, HeapEnableTerminationOnCorruption,
-                              NULL, 0) &&
-        ERROR_ACCESS_DENIED != ::GetLastError()) {
-      return false;
+                              NULL, 0)) {
+      CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
     }
   }
 
@@ -76,9 +75,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
         reinterpret_cast<SetProcessDEPPolicyFunction>(
             ::GetProcAddress(module, "SetProcessDEPPolicy"));
     if (set_process_dep_policy) {
-      if (!set_process_dep_policy(dep_flags) &&
-          ERROR_ACCESS_DENIED != ::GetLastError() && return_on_fail) {
-        return false;
+      if (!set_process_dep_policy(dep_flags) && return_on_fail) {
+        CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
       }
     } else {
       // We're on XP sp2, so use the less standard approach.
@@ -99,8 +97,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
       if (!SUCCEEDED(set_information_process(GetCurrentProcess(),
                                              ProcessExecuteFlags,
                                              &dep, sizeof(dep))) &&
-          ERROR_ACCESS_DENIED != ::GetLastError() && return_on_fail) {
-        return false;
+          return_on_fail) {
+        CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
       }
     }
   }
@@ -125,9 +123,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
         MITIGATION_RELOCATE_IMAGE_REQUIRED;
 
     if (!set_process_mitigation_policy(ProcessASLRPolicy, &policy,
-                                       sizeof(policy)) &&
-        ERROR_ACCESS_DENIED != ::GetLastError()) {
-      return false;
+                                       sizeof(policy))) {
+      CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
     }
   }
 
@@ -138,9 +135,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
         policy.RaiseExceptionOnInvalidHandleReference = true;
 
     if (!set_process_mitigation_policy(ProcessStrictHandleCheckPolicy, &policy,
-                                       sizeof(policy)) &&
-        ERROR_ACCESS_DENIED != ::GetLastError()) {
-      return false;
+                                       sizeof(policy))) {
+      CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
     }
   }
 
@@ -150,9 +146,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
     policy.DisallowWin32kSystemCalls = true;
 
     if (!set_process_mitigation_policy(ProcessSystemCallDisablePolicy, &policy,
-                                       sizeof(policy)) &&
-        ERROR_ACCESS_DENIED != ::GetLastError()) {
-      return false;
+                                       sizeof(policy))) {
+      CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
     }
   }
 
@@ -162,9 +157,8 @@ bool ApplyProcessMitigationsToCurrentProcess(MitigationFlags flags) {
     policy.DisableExtensionPoints = true;
 
     if (!set_process_mitigation_policy(ProcessExtensionPointDisablePolicy,
-                                       &policy, sizeof(policy)) &&
-        ERROR_ACCESS_DENIED != ::GetLastError()) {
-      return false;
+                                       &policy, sizeof(policy))) {
+      CHECK_EQ(ERROR_ACCESS_DENIED, static_cast<long>(::GetLastError()));
     }
   }
 
