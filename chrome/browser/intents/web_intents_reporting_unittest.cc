195ac2bdc2e0b9c9020dbb9ffa8b3cdadc9970d8
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-04 05:33:14 +0000

    Using custom enum histograms for Web Intents reporting to reduce histogram size.
    BUG=152970
    
    R=smckay@chromium.org
    R=jar@chromium.org
    R=gbillock@chromium.org
    R=sky@chromium.org
    
    +jar@chromium.org because he requested this change.
    
    +sky@chromium.org to verify the change to chrome_browser.gypi file.
    
    +gbillock@chromium.org
    +smckay@chromium.org
        - to verify the changes to Web Intents reporting implementation.
    
    Review URL: https://chromiumcodereview.appspot.com/10979069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160084 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intents/web_intents_reporting_unittest.cc
+++ chrome/browser/intents/web_intents_reporting_unittest.cc
@@ -45,9 +45,5 @@ TEST(WebIntentsReportingTest, CustomActionAndType) {
   EXPECT_EQ(BUCKET_CUSTOM_CUSTOM, ToBucket(kCustomAction, kCustomType));
 }
 
-TEST(WebIntentsUtilTest, MaxValueConstMatchesLastKnownMaxValue) {
-  EXPECT_EQ(kMaxActionTypeHistogramValue, BUCKET_VIEW_VIDEO);
-}
-
 }  // namespace reporting
 }  // namespace web_intents
