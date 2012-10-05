cd12c6405cfe68b22807e5e8635758c4ae2d5da8
klundberg@chromium.org <klundberg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 21:04:22 +0000

    Upstream Chrome for Android's KeyUtils, TouchUtils and UiUtils.
    BUG=154141
    
    
    Review URL: https://chromiumcodereview.appspot.com/11038048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160478 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/public/test/android/javatests/src/org/chromium/content/browser/test/util/KeyUtils.java
@@ -0,0 +1,66 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.browser.test.util;
+
+import android.app.Instrumentation;
+import android.view.KeyEvent;
+
+/**
+ * Collection of keyboard utilities.
+ */
+public class KeyUtils {
+    /**
+     * Press "Enter".
+     */
+    public static void pressEnter(Instrumentation instrumentation) {
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_ENTER));
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_UP,
+                        KeyEvent.KEYCODE_ENTER));
+        instrumentation.waitForIdleSync();
+    }
+
+    /**
+     * Press "Tab".
+     */
+    public static void pressTab(Instrumentation instrumentation) {
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_TAB));
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_UP,
+                        KeyEvent.KEYCODE_TAB));
+        instrumentation.waitForIdleSync();
+    }
+
+    /**
+     * Press "Backspace".
+     */
+    public static void pressBackspace(Instrumentation instrumentation) {
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_DEL));
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_UP,
+                        KeyEvent.KEYCODE_DEL));
+        instrumentation.waitForIdleSync();
+    }
+
+    /**
+     * Press "Back".
+     */
+    public static void pressBack(Instrumentation instrumentation) {
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_DOWN,
+                        KeyEvent.KEYCODE_BACK));
+        instrumentation.sendKeySync(new KeyEvent(KeyEvent.ACTION_UP,
+                        KeyEvent.KEYCODE_BACK));
+        instrumentation.waitForIdleSync();
+    }
+
+    /**
+     * Input a String.
+     */
+    public static void inputString(Instrumentation instrumentation, String text) {
+        instrumentation.sendStringSync(text);
+        instrumentation.waitForIdleSync();
+    }
+}
