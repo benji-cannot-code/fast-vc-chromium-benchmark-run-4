166794cf307bdca7e6b0e9e8f5799bd915f7830d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 16:55:15 +0000

    Remove gpu test_switches.* and move the constant from content to chrome.
    It's not used by content at all, so it can live in chrome instead.
    
    This is the same as http://src.chromium.org/viewvc/chrome?view=rev&revision=160362,
    that had to be reverted.
    
    TBR=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/11035058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ppapi/ppapi_test.cc
+++ chrome/test/ppapi/ppapi_test.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_paths.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
-#include "content/test/gpu/test_switches.h"
 #include "media/audio/audio_manager.h"
 #include "net/base/net_util.h"
 #include "net/test/test_server.h"
