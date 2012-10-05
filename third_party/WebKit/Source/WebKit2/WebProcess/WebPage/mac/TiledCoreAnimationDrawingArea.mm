957fe4671a51397bcaff9200d332570f788f254c
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-05 00:54:12 +0000

    TiledCoreAnimationDrawingArea.mm should not include RenderLayerCompositor.h https://bugs.webkit.org/show_bug.cgi?id=98471
    Reviewed by Dean Jackson.
    
    Remove #import of <WebCore/RenderLayerCompositor.h> from this file.
    It is not required.
    
    * WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <WebCore/GraphicsContext.h>
 #import <WebCore/GraphicsLayerCA.h>
 #import <WebCore/Page.h>
-#import <WebCore/RenderLayerCompositor.h>
 #import <WebCore/RenderView.h>
 #import <WebCore/ScrollingCoordinator.h>
 #import <WebCore/ScrollingThread.h>
