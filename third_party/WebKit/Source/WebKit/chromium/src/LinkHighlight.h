0c29aa46501383d459c83cf1108cf9c18dff3724
wjmaclean@chromium.org <wjmaclean@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-05 19:05:44 +0000

    [chromium] Modify gesture highlight behaviour. Cancel on GestureLongPress and animate on GestureTapCancel. https://bugs.webkit.org/show_bug.cgi?id=97355
    Reviewed by Adrienne Walker.
    
    This patch revises the behaviour of GestureTapDown highlighting so that the highlight should disappear if
    interrupted by GestureTap, and animate away if interrupted by GestureTapCancel.
    
    Source/WebKit/chromium:
    
    * src/LinkHighlight.cpp:
    (WebKit::LinkHighlight::LinkHighlight):
    (WebKit::LinkHighlight::startHighlightAnimationIfNeeded):
    * src/LinkHighlight.h:
    (LinkHighlight):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::handleGestureEvent):
    (WebKit::WebViewImpl::enableTouchHighlight):
    
    LayoutTests:
    
    * platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-cancel.html: removed extra testRunner.display().
    * platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-cancel2.html: Added.
    * platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-longPress.html: Added.
    * platform/chromium-linux/platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-cancel2-expected.png: Added.
    * platform/chromium-linux/platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-cancel2-expected.txt: Added.
    * platform/chromium-linux/platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-longPress-expected.png: Added.
    * platform/chromium-linux/platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-longPress-expected.txt: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130539 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/LinkHighlight.h
+++ third_party/WebKit/Source/WebKit/chromium/src/LinkHighlight.h
@@ -55,7 +55,7 @@ public:
 
     WebContentLayer* contentLayer();
     WebLayer* clipLayer();
-    void startHighlightAnimation();
+    void startHighlightAnimationIfNeeded();
     void updateGeometry();
 
     // WebContentLayerClient implementation.
@@ -91,6 +91,7 @@ private:
     WebCore::GraphicsLayerChromium* m_currentGraphicsLayer;
 
     bool m_geometryNeedsUpdate;
+    bool m_isAnimating;
     WebCore::FloatPoint m_graphicsLayerOffset;
 };
 
