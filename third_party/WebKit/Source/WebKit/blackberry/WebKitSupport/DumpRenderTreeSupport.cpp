0e292de3dc4b344c700bce105f6e1d0703d2beb8
rwlbuis@webkit.org <rwlbuis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-05 19:01:47 +0000

    [BlackBerry] Implement TestRunner.setMockDeviceOrientation https://bugs.webkit.org/show_bug.cgi?id=98542
    Reviewed by Antonio Gomes.
    
    PR 120681
    
    Source/WebKit/blackberry:
    
    Provide framework for mock device motion.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::init):
    * WebKitSupport/DumpRenderTreeSupport.cpp:
    (toDeviceOrientationClientMock):
    (DumpRenderTreeSupport::setMockDeviceOrientation):
    * WebKitSupport/DumpRenderTreeSupport.h:
    (DumpRenderTreeSupport):
    
    Tools:
    
    This fixes tests in fast/dom/DeviceOrientation.
    
    * DumpRenderTree/blackberry/TestRunnerBlackBerry.cpp:
    (TestRunner::setMockDeviceOrientation):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DumpRenderTreeSupport.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DumpRenderTreeSupport.cpp
@@ -22,6 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DumpRenderTreeSupport.h"
 
 #include "CSSComputedStyleDeclaration.h"
+#include "DeviceOrientationClientMock.h"
+#include "DeviceOrientationController.h"
+#include "DeviceOrientationData.h"
 #include "Frame.h"
 #include "GeolocationClientMock.h"
 #include "GeolocationController.h"
@@ -135,3 +138,18 @@ JSValueRef DumpRenderTreeSupport::computedStyleIncludingVisitedInfo(JSContextRef
     return toRef(exec, toJS(exec, jsElement->globalObject(), style.get()));
 }
 
+#if ENABLE(DEVICE_ORIENTATION)
+DeviceOrientationClientMock* toDeviceOrientationClientMock(DeviceOrientationClient* client)
+{
+    return static_cast<DeviceOrientationClientMock*>(client);
+}
+#endif
+
+void DumpRenderTreeSupport::setMockDeviceOrientation(BlackBerry::WebKit::WebPage* webPage, bool canProvideAlpha, double alpha, bool canProvideBeta, double beta, bool canProvideGamma, double gamma)
+{
+#if ENABLE(DEVICE_ORIENTATION)
+    Page* page = corePage(webPage);
+    DeviceOrientationClientMock* mockClient = toDeviceOrientationClientMock(DeviceOrientationController::from(page)->client());
+    mockClient->setOrientation(DeviceOrientationData::create(canProvideAlpha, alpha, canProvideBeta, beta, canProvideGamma, gamma));
+#endif
+}
