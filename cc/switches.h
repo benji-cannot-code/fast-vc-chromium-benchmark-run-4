fe47fc3869434b72179ac0f3e402c3e284cccf41
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 22:11:19 +0000

    cc: Remove unnecessary includes of build_config.h from switches.*
    R=enne@chromium.org
    
    Review URL: https://codereview.chromium.org/11038065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160496 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/switches.h
+++ cc/switches.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_SWITCHES_H_
 #define CC_SWITCHES_H_
 
-#include "build/build_config.h"
 #include "webkit/glue/webkit_glue_export.h"
 
 // Since cc is used from the render process, anything that goes here also needs
