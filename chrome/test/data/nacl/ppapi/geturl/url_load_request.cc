fb95a328c699f704e4dd93492ad7c858a347e41f
ncbray@chromium.org <ncbray@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 19:46:05 +0000

    Port three nacl_integration tests to browser_tests.
    * ppapi_geturl
    * ppapi_ppb_dev
    * ppapi_ppp_instance
    
    BUG= http://code.google.com/p/nativeclient/issues/detail?id=2959
    
    
    Review URL: https://chromiumcodereview.appspot.com/11035048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160454 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/nacl/ppapi/geturl/url_load_request.cc
+++ chrome/test/data/nacl/ppapi/geturl/url_load_request.cc
@@ -3,16 +3,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "native_client/tests/ppapi_geturl/url_load_request.h"
+#include "chrome/test/data/nacl/ppapi/geturl/url_load_request.h"
 
 #include <stdio.h>
 #include <string>
 #include <sstream>
 
+#include "chrome/test/data/nacl/ppapi/geturl/module.h"
 #include "native_client/src/include/portability.h"
 #include "native_client/src/include/nacl_macros.h"
 #include "native_client/src/shared/platform/nacl_check.h"
-#include "native_client/tests/ppapi_geturl/module.h"
 #include "ppapi/c/pp_bool.h"
 #include "ppapi/c/pp_completion_callback.h"
 #include "ppapi/c/pp_errors.h"
--- chrome/test/data/nacl/ppapi/geturl/url_load_request.h
+++ chrome/test/data/nacl/ppapi/geturl/url_load_request.h
@@ -2,8 +2,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-#ifndef TESTS_PPAPI_GETURL_URL_LOAD_REQUEST_H_
-#define TESTS_PPAPI_GETURL_URL_LOAD_REQUEST_H_
+#ifndef CHROME_TEST_DATA_NACL_PPAPI_GETURL_URL_LOAD_REQUEST_H_
+#define CHROME_TEST_DATA_NACL_PPAPI_GETURL_URL_LOAD_REQUEST_H_
 
 #include <string>
 #include <vector>
@@ -77,4 +77,4 @@ class UrlLoadRequest {
   int32_t read_offset_;
 };
 
-#endif  // TESTS_PPAPI_GETURL_URL_LOAD_REQUEST_H_
+#endif  // CHROME_TEST_DATA_NACL_PPAPI_GETURL_URL_LOAD_REQUEST_H_
