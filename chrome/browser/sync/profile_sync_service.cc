b1e4a50e19a88c75aa368128d87102431ce8bbd5
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 18:03:14 +0000

    [Invalidations] Require there to be no registered handlers on Invalidator destruction
    Add CHECK to catch sloppy clients.
    
    Make ProfileSyncService destroy its invalidator registrar on shut down (also to catch
    sloppy clients).  Comment on expected usage of Initialize() and Shutdown(),
    and add DCHECKs for them.
    
    Fix Invalidator test template to unregister handlers properly.  Also fix some ProfileSyncService tests.
    
    BUG=137086
    TBR=tim@chromium.org,arv@chromium.org
    
    Review URL: https://codereview.chromium.org/11046008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160420 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -68,6 +68,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sync/internal_api/public/util/sync_string_conversions.h"
 #include "sync/js/js_arg_list.h"
 #include "sync/js/js_event_details.h"
+#include "sync/notifier/invalidator_registrar.h"
 #include "sync/util/cryptographer.h"
 #include "ui/base/l10n/l10n_util.h"
 
@@ -190,6 +191,9 @@ bool ProfileSyncService::IsSyncTokenAvailable() {
 }
 
 void ProfileSyncService::Initialize() {
+  DCHECK(!invalidator_registrar_.get());
+  invalidator_registrar_.reset(new syncer::InvalidatorRegistrar());
+
   InitSettings();
 
   // We clear this here (vs Shutdown) because we want to remember that an error
@@ -436,7 +440,7 @@ void ProfileSyncService::StartUp() {
   // http://crbug.com/140354).
   if (backend_.get()) {
     backend_->UpdateRegisteredInvalidationIds(
-        invalidator_registrar_.GetAllRegisteredIds());
+        invalidator_registrar_->GetAllRegisteredIds());
   }
 
   if (!sync_global_error_.get()) {
@@ -451,29 +455,29 @@ void ProfileSyncService::StartUp() {
 
 void ProfileSyncService::RegisterInvalidationHandler(
     syncer::InvalidationHandler* handler) {
-  invalidator_registrar_.RegisterHandler(handler);
+  invalidator_registrar_->RegisterHandler(handler);
 }
 
 void ProfileSyncService::UpdateRegisteredInvalidationIds(
     syncer::InvalidationHandler* handler,
     const syncer::ObjectIdSet& ids) {
-  invalidator_registrar_.UpdateRegisteredIds(handler, ids);
+  invalidator_registrar_->UpdateRegisteredIds(handler, ids);
 
   // If |backend_| is NULL, its registered IDs will be updated when
   // it's created and initialized.
   if (backend_.get()) {
     backend_->UpdateRegisteredInvalidationIds(
-        invalidator_registrar_.GetAllRegisteredIds());
+        invalidator_registrar_->GetAllRegisteredIds());
   }
 }
 
 void ProfileSyncService::UnregisterInvalidationHandler(
     syncer::InvalidationHandler* handler) {
-  invalidator_registrar_.UnregisterHandler(handler);
+  invalidator_registrar_->UnregisterHandler(handler);
 }
 
 syncer::InvalidatorState ProfileSyncService::GetInvalidatorState() const {
-  return invalidator_registrar_.GetInvalidatorState();
+  return invalidator_registrar_->GetInvalidatorState();
 }
 
 void ProfileSyncService::EmitInvalidationForTest(
@@ -488,6 +492,7 @@ void ProfileSyncService::EmitInvalidationForTest(
 }
 
 void ProfileSyncService::Shutdown() {
+  DCHECK(invalidator_registrar_.get());
   // TODO(akalin): Remove this once http://crbug.com/153827 is fixed.
   ExtensionService* const extension_service =
       extensions::ExtensionSystem::Get(profile_)->extension_service();
@@ -495,6 +500,10 @@ void ProfileSyncService::Shutdown() {
   // (e.g., extension sync wasn't enabled in tests).
   if (extension_service)
     extension_service->OnProfileSyncServiceShutdown();
+
+  // Reset |invalidator_registrar_| first so that ShutdownImpl cannot
+  // use it.
+  invalidator_registrar_.reset();
 
   ShutdownImpl(false);
 }
@@ -689,14 +698,14 @@ void ProfileSyncService::DisableBrokenDatatype(
 
 void ProfileSyncService::OnInvalidatorStateChange(
     syncer::InvalidatorState state) {
-  invalidator_registrar_.UpdateInvalidatorState(state);
+  invalidator_registrar_->UpdateInvalidatorState(state);
 }
 
 void ProfileSyncService::OnIncomingInvalidation(
     const syncer::ObjectIdInvalidationMap& invalidation_map,
     syncer::IncomingInvalidationSource source) {
-  invalidator_registrar_.DispatchInvalidationsToHandlers(invalidation_map,
-                                                         source);
+  invalidator_registrar_->DispatchInvalidationsToHandlers(invalidation_map,
+                                                          source);
 }
 
 void ProfileSyncService::OnBackendInitialized(
--- chrome/browser/sync/profile_sync_service.h
+++ chrome/browser/sync/profile_sync_service.h
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sync/internal_api/public/util/experiments.h"
 #include "sync/internal_api/public/util/unrecoverable_error_handler.h"
 #include "sync/js/sync_js_controller.h"
-#include "sync/notifier/invalidator_registrar.h"
 
 class Profile;
 class ProfileSyncComponentsFactory;
@@ -61,6 +60,7 @@ namespace sessions { class SyncSessionSnapshot; }
 
 namespace syncer {
 class BaseTransaction;
+class InvalidatorRegistrar;
 struct SyncCredentials;
 struct UserShare;
 }
@@ -219,8 +219,8 @@ class ProfileSyncService : public ProfileSyncServiceBase,
                      StartBehavior start_behavior);
   virtual ~ProfileSyncService();
 
-  // Initializes the object. This should be called every time an object of this
-  // class is constructed.
+  // Initializes the object. This must be called at most once, and
+  // immediately after an object of this class is constructed.
   void Initialize();
 
   virtual void SetSyncSetupCompleted();
@@ -553,7 +553,8 @@ class ProfileSyncService : public ProfileSyncServiceBase,
   // been cleared yet. Virtual for testing purposes.
   virtual bool waiting_for_auth() const;
 
-  // InvalidationFrontend implementation.
+  // InvalidationFrontend implementation.  It is an error to have
+  // registered handlers when Shutdown() is called.
   virtual void RegisterInvalidationHandler(
       syncer::InvalidationHandler* handler) OVERRIDE;
   virtual void UpdateRegisteredInvalidationIds(
@@ -563,7 +564,8 @@ class ProfileSyncService : public ProfileSyncServiceBase,
       syncer::InvalidationHandler* handler) OVERRIDE;
   virtual syncer::InvalidatorState GetInvalidatorState() const OVERRIDE;
 
-  // ProfileKeyedService implementation.
+  // ProfileKeyedService implementation.  This must be called exactly
+  // once (before this object is destroyed).
   virtual void Shutdown() OVERRIDE;
 
   // Simulate an incoming notification for the given id and payload.
@@ -842,8 +844,9 @@ class ProfileSyncService : public ProfileSyncServiceBase,
   // Factory the backend will use to build the SyncManager.
   syncer::SyncManagerFactory sync_manager_factory_;
 
-  // Dispatches invalidations to handlers.
-  syncer::InvalidatorRegistrar invalidator_registrar_;
+  // Dispatches invalidations to handlers.  Set in Initialize() and
+  // unset in Shutdown().
+  scoped_ptr<syncer::InvalidatorRegistrar> invalidator_registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(ProfileSyncService);
 };
