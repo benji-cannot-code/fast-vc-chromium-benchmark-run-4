fc50b99b9316bf1e656ccb1d98c70c7e960b82ad
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-05 01:49:02 +0000

    Remove ChromotingHostContext from SignalingConnector.
    BUG=145856
    
    
    Review URL: https://chromiumcodereview.appspot.com/11027021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160295 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/signaling_connector.cc
+++ remoting/host/signaling_connector.cc
@@ -8,9 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/callback.h"
 #include "net/url_request/url_fetcher.h"
-#include "remoting/host/chromoting_host_context.h"
+#include "net/url_request/url_request_context_getter.h"
 #include "remoting/host/dns_blackhole_checker.h"
-#include "remoting/host/url_request_context.h"
 
 namespace remoting {
 
@@ -36,11 +35,11 @@ SignalingConnector::OAuthCredentials::OAuthCredentials(
 
 SignalingConnector::SignalingConnector(
     XmppSignalStrategy* signal_strategy,
-    ChromotingHostContext* context,
+    scoped_refptr<net::URLRequestContextGetter> url_request_context_getter,
     scoped_ptr<DnsBlackholeChecker> dns_blackhole_checker,
     const base::Closure& auth_failed_callback)
     : signal_strategy_(signal_strategy),
-      context_(context),
+      url_request_context_getter_(url_request_context_getter),
       auth_failed_callback_(auth_failed_callback),
       dns_blackhole_checker_(dns_blackhole_checker.Pass()),
       reconnect_attempts_(0),
@@ -63,7 +62,7 @@ void SignalingConnector::EnableOAuth(
     scoped_ptr<OAuthCredentials> oauth_credentials) {
   oauth_credentials_ = oauth_credentials.Pass();
   gaia_oauth_client_.reset(new GaiaOAuthClient(
-      OAuthProviderInfo::GetDefault(), context_->url_request_context_getter()));
+      OAuthProviderInfo::GetDefault(), url_request_context_getter_));
 }
 
 void SignalingConnector::OnSignalStrategyStateChange(
