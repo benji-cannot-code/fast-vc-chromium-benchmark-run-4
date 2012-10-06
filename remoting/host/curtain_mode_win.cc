bfb6aaf1c64a348c2707ba22e34c56d5cbc63bff
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-06 02:06:34 +0000

    Curtain policy cross-platform plumbing and fixes
    This moves everything into a cross-platform plumbing, makes the curtain policy lifetime the same as the other HostStatusObservers (to avoid issues with host restarts), and changes the OnPolicyUpdated handler to restart the host at most once.
    
    BUG=153590
    
    Review URL: https://chromiumcodereview.appspot.com/10914298
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160546 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/curtain_mode_win.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "remoting/host/curtain_mode.h"
+
+#include "base/logging.h"
+
+namespace remoting {
+
+class CurtainModeWin : public CurtainMode {
+ public:
+  CurtainModeWin() {}
+  // Overriden from CurtainMode.
+  virtual void SetActivated(bool activated) OVERRIDE {
+    NOTIMPLEMENTED();
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(CurtainModeWin);
+};
+
+// static
+scoped_ptr<CurtainMode> CurtainMode::Create(
+    const base::Closure& on_session_activate,
+    const base::Closure& on_error) {
+  return scoped_ptr<CurtainMode>(
+      new CurtainModeWin());
+}
+
+}  // namespace remoting
