44e7e9e52e58de69b3714e2829a630958d6cf123
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-07 22:27:11 +0000

    Add clang builder top level script
    - Removes the zip_build and check_licenses currently run.
    
    TBR=yaron
    BUG=
    
    Review URL: https://codereview.chromium.org/11078010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160612 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/buildbot/bb_clang_builder.sh
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/bash -ex
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+#
+# Buildbot annotator script for clang.
+
+BB_DIR="$(dirname $0)"
+BB_SRC_ROOT="$(cd  "$BB_DIR/../../.."; pwd)"
+. "$BB_DIR/buildbot_functions.sh"
+
+bb_baseline_setup "$BB_SRC_ROOT" "$@"
+bb_compile
