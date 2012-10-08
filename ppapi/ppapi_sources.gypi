45fe83fbac92e9551c18d49c1a177baf0a1e233d
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 16:51:30 +0000

    Remove unused PPP_CursorControl_Dev
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11032053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160665 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/ppapi_sources.gypi
+++ ppapi/ppapi_sources.gypi
@@ -80,7 +80,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'c/dev/ppb_video_decoder_dev.h',
       'c/dev/ppb_widget_dev.h',
       'c/dev/ppb_zoom_dev.h',
-      'c/dev/ppp_cursor_control_dev.h',
       'c/dev/ppp_find_dev.h',
       'c/dev/ppp_network_state_dev.h',
       'c/dev/ppp_scrollbar_dev.h',
--- ppapi/tests/all_c_includes.h
+++ ppapi/tests/all_c_includes.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/dev/ppb_widget_dev.h"
 #include "ppapi/c/dev/ppb_zoom_dev.h"
 #include "ppapi/c/dev/ppp_class_deprecated.h"
-#include "ppapi/c/dev/ppp_cursor_control_dev.h"
 #include "ppapi/c/dev/ppp_find_dev.h"
 #include "ppapi/c/dev/ppp_printing_dev.h"
 #include "ppapi/c/dev/ppp_scrollbar_dev.h"
