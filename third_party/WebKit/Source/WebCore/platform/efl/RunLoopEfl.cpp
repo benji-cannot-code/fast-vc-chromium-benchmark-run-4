c8355326672b738b1fc0c609e4be3eb6460a6fed
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-08 07:20:17 +0000

    [EFL] Use ecore_main_loop_thread_safe_call_async() to wakeup main loop. https://bugs.webkit.org/show_bug.cgi?id=98505
    Patch by Byungwoo Lee <bw80.lee@samsung.com> on 2012-10-08
    Reviewed by Kenneth Rohde Christiansen.
    
    Instead of ecore_pipe_write(),
    use ecore_main_loop_thread_safe_call_async() to wakeup ecore main loop.
    
    According to the EFL API document, this function is designed to dispatch
    a function on ecore main loop by avoiding dead lock or race condition.
    With this function, webkit doesn't need to maintain ecore pipe also.
    
    Source/WebCore:
    
    No new tests. The function to wakeup main loop is changed.
    
    * platform/RunLoop.h:
    (RunLoop):
    * platform/efl/RunLoopEfl.cpp:
    (WebCore::RunLoop::RunLoop):
    (WebCore::RunLoop::wakeUpEvent):
    (WebCore::RunLoop::wakeUp):
    
    Source/WTF:
    
    * wtf/efl/MainThreadEfl.cpp:
    (WTF::monitorDispatchFunctions):
    (WTF::initializeMainThreadPlatform):
    (WTF::scheduleDispatchFunctionsOnMainThread):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/RunLoopEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/efl/RunLoopEfl.cpp
@@ -35,9 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
-static const int ecorePipeMessageSize = 1;
-static const char wakupEcorePipeMessage[] = "W";
-
 namespace WebCore {
 
 RunLoop::RunLoop()
@@ -63,7 +60,6 @@ RunLoop::RunLoop()
         goto errorEdje;
     }
 
-    m_pipe = adoptPtr(ecore_pipe_add(wakeUpEvent, this));
     m_initEfl = true;
 
     return;
@@ -96,14 +92,14 @@ void RunLoop::stop()
     ecore_main_loop_quit();
 }
 
-void RunLoop::wakeUpEvent(void* data, void*, unsigned int)
+void RunLoop::wakeUpEvent(void* data)
 {
     static_cast<RunLoop*>(data)->performWork();
 }
 
 void RunLoop::wakeUp()
 {
-    ecore_pipe_write(m_pipe.get(), wakupEcorePipeMessage, ecorePipeMessageSize);
+    ecore_main_loop_thread_safe_call_async(wakeUpEvent, this);
 }
 
 RunLoop::TimerBase::TimerBase(RunLoop*)
