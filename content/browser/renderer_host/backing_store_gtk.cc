bd98e38feb6ce8132be31604ed947b23f7d7d242
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 10:32:00 +0000

    Reland 160434 - Keep track of which transport DIBs are currently in the X servers queue
    Since X11 is asynchronous, there might be operations on the shared memory pending. If we unmap the shared memory immediately, the queued operations will make the renderer crash.
    
    This can happen if a renderer is brought up, renderers a single frame, and is immediately destroyed again.
    
    BUG=143580
    TEST=running layout tests using content shell doesn't crash constantly
    TBR=piman,backer
    
    Original Review URL: https://chromiumcodereview.appspot.com/11014018
    
    
    
    Review URL: https://chromiumcodereview.appspot.com/11070011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160640 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/backing_store_gtk.cc
+++ content/browser/renderer_host/backing_store_gtk.cc
@@ -84,7 +84,10 @@ class XSyncHandler {
     return loaded_extension_;
   }
 
-  void PushPaintCounter(Display* display, Picture picture, Pixmap pixmap,
+  void PushPaintCounter(TransportDIB* dib,
+                        Display* display,
+                        Picture picture,
+                        Pixmap pixmap,
                         const base::Closure& completion_callback);
 
  private:
@@ -93,13 +96,17 @@ class XSyncHandler {
   // A struct that has cleanup and callback tasks that were queued into the
   // future and are run on |g_backing_store_sync_alarm| firing.
   struct BackingStoreEvents {
-    BackingStoreEvents(Display* d, Picture pic, Pixmap pix,
+    BackingStoreEvents(TransportDIB* dib, Display* d, Picture pic, Pixmap pix,
                        const base::Closure& c)
-        : display(d),
+        : dib(dib),
+          display(d),
           picture(pic),
           pixmap(pix),
           closure(c) {
+      dib->IncreaseInFlightCounter();
     }
+
+    TransportDIB* dib;
 
     // The display we're running on.
     Display* display;
@@ -134,12 +141,13 @@ class XSyncHandler {
   std::queue<BackingStoreEvents*> backing_store_events_;
 };
 
-void XSyncHandler::PushPaintCounter(Display* display,
+void XSyncHandler::PushPaintCounter(TransportDIB* dib,
+                                    Display* display,
                                     Picture picture,
                                     Pixmap pixmap,
                                     const base::Closure& completion_callback) {
-  backing_store_events_.push(
-      new BackingStoreEvents(display, picture, pixmap, completion_callback));
+  backing_store_events_.push(new BackingStoreEvents(
+        dib, display, picture, pixmap, completion_callback));
 
   // Push a change counter event into the X11 event queue that will trigger our
   // alarm when it is processed.
@@ -183,6 +191,7 @@ XSyncHandler::~XSyncHandler() {
   if (loaded_extension_)
     gdk_window_remove_filter(NULL, &OnEventThunk, this);
 
+  XSync(ui::GetXDisplay(), False);
   while (!backing_store_events_.empty()) {
     // We delete the X11 resources we're holding onto. We don't run the
     // callbacks because we are shutting down.
@@ -190,6 +199,7 @@ XSyncHandler::~XSyncHandler() {
     backing_store_events_.pop();
     XRenderFreePicture(data->display, data->picture);
     XFreePixmap(data->display, data->pixmap);
+    data->dib->DecreaseInFlightCounter();
     delete data;
   }
 }
@@ -220,6 +230,8 @@ GdkFilterReturn XSyncHandler::OnEvent(GdkXEvent* gdkxevent,
 
       // Dispatch the closure we were given.
       data->closure.Run();
+
+      data->dib->DecreaseInFlightCounter();
       delete data;
 
       return GDK_FILTER_REMOVE;
@@ -473,7 +485,8 @@ void BackingStoreGtk::PaintToBackingStore(
     XSyncHandler* handler = XSyncHandler::GetInstance();
     if (handler->Enabled()) {
       *scheduled_completion_callback = true;
-      handler->PushPaintCounter(display_, picture, pixmap, completion_callback);
+      handler->PushPaintCounter(
+          dib, display_, picture, pixmap, completion_callback);
     } else {
       XSync(display_, False);
     }
