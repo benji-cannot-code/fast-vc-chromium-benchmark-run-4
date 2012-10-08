a0838395c97002b3398fd7a142312964ad8d909f
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 22:58:33 +0000

    add default constructor to PendingTask; compilation fix for VS2012
    std::queue in TaskQueue wants a default ctor for its contained value in 2012.
    
    R=willchan@chromium.org
    BUG=143646
    
    
    Review URL: https://chromiumcodereview.appspot.com/11095007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160736 0039d316-1c4b-4281-b951-d872f2087c98

--- base/pending_task.cc
+++ base/pending_task.cc
@@ -9,6 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
+PendingTask::PendingTask() {
+}
+
 PendingTask::PendingTask(const tracked_objects::Location& posted_from,
                          const base::Closure& task)
     : base::TrackingInfo(posted_from, TimeTicks()),
