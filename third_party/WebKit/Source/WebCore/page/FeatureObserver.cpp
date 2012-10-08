c4e04e7c87062a1d8baf460eddf745bc10e4ac53
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-08 18:15:01 +0000

    Null-check for DOMWindow before feeding it to FeatureObserver. https://bugs.webkit.org/show_bug.cgi?id=98624
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    We shouldn't call out to FeatureObserver in
    ContentSecurityPolicy::didReceiveHeader if the policy's document doesn't
    have a DOMWindow.
    
    Test: http/tests/security/contentSecurityPolicy/xmlhttprequest-protected-resource-does-not-crash.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::ContentSecurityPolicy::didReceiveHeader):
        Null check 'document->domWindow' before passing it on.
    * page/FeatureObserver.cpp:
    (WebCore::FeatureObserver::observe):
        ASSERT 'domWindow'.
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/xmlhttprequest-protected-resource-does-not-crash-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/xmlhttprequest-protected-resource-does-not-crash.html: Added.
        This test shouldn't crash.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130657 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FeatureObserver.cpp
+++ third_party/WebKit/Source/WebCore/page/FeatureObserver.cpp
@@ -55,6 +55,8 @@ FeatureObserver::~FeatureObserver()
 
 void FeatureObserver::observe(DOMWindow* domWindow, Feature feature)
 {
+    ASSERT(domWindow);
+
     Document* document = domWindow->document();
     if (!document)
         return;
