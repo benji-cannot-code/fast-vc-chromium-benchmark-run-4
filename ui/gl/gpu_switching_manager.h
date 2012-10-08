3423388a85d498baca1b2d57de660ded01f23fa5
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 22:33:14 +0000

    gl: Move GpuSwitchingManager into ui namespace.
    BUG=127983
    TBR=apatrick@chromium.org
    
    Review URL: https://codereview.chromium.org/11013008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160728 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gpu_switching_manager.h
+++ ui/gl/gpu_switching_manager.h
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <OpenGL/OpenGL.h>
 #endif  // OS_MACOSX
 
-namespace gfx {
+namespace ui {
 
 class GL_EXPORT GpuSwitchingManager {
  public:
@@ -31,7 +31,7 @@ class GL_EXPORT GpuSwitchingManager {
 
   // If no GPU is forced, return the original GpuPreference; otherwise, return
   // the forced GPU.
-  GpuPreference AdjustGpuPreference(GpuPreference gpu_preference);
+  gfx::GpuPreference AdjustGpuPreference(gfx::GpuPreference gpu_preference);
 
   // In the browser process, the value for this flag is computed the first time
   // this function is called.
@@ -51,7 +51,7 @@ class GL_EXPORT GpuSwitchingManager {
   CGLPixelFormatObj discrete_pixel_format_;
 #endif  // OS_MACOSX
 
-  GpuPreference gpu_switching_option_;
+  gfx::GpuPreference gpu_switching_option_;
   bool gpu_switching_option_set_;
 
   bool supports_dual_gpus_;
@@ -60,7 +60,6 @@ class GL_EXPORT GpuSwitchingManager {
   DISALLOW_COPY_AND_ASSIGN(GpuSwitchingManager);
 };
 
-}  // namespace gfx
+}  // namespace ui
 
 #endif  // UI_GL_GPU_SWITCHING_MANAGER_H_
-
