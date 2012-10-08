de9ee46924450b7d5e9b01529e2fb17450cf4683
aberent@chromium.org <aberent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-08 14:54:42 +0000

    Upstreaming latest build/android changes
    Includes:
    Fix to reboot so that it works on all devices
    Handling EOF in logcat
    Clear caches as root - required for recent android versions
    Print standard deviation for performance tests
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11026040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160651 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/perf_tests_helper.py
+++ build/android/pylib/perf_tests_helper.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import re
 
 import android_commands
+import math
 
 # Valid values of result type.
 RESULT_TYPES = {'unimportant': 'RESULT ',
@@ -47,10 +48,14 @@ def PrintPerfResult(measurement, trace, values, units, result_type='default',
   assert len(values)
   assert '/' not in measurement
   avg = None
+  sd = None
   if len(values) > 1:
     try:
       value = '[%s]' % ','.join([str(v) for v in values])
       avg = sum([float(v) for v in values]) / len(values)
+      sqdiffs = [(float(v) - avg) ** 2 for v in values]
+      variance = sum(sqdiffs) / (len(values) - 1)
+      sd = math.sqrt(variance)
     except ValueError:
       value = ", ".join(values)
   else:
@@ -68,6 +73,8 @@ def PrintPerfResult(measurement, trace, values, units, result_type='default',
     units)
   if avg:
     output += '\nAvg %s: %f%s' % (measurement, avg, units)
+  if sd:
+    output += '\nSd  %s: %f%s' % (measurement, sd, units)
   if print_to_stdout:
     print output
   return output
@@ -88,6 +95,9 @@ class PerfTestSetup(object):
 
   def DropRamCaches(self):
     """Drops the filesystem ram caches for performance testing."""
+    if not self._adb.IsRootEnabled():
+      self._adb.EnableAdbRoot()
+    self._adb.RunShellCommand('sync')
     self._adb.RunShellCommand('echo 3 > ' + PerfTestSetup._DROP_CACHES)
 
   def SetUp(self):
