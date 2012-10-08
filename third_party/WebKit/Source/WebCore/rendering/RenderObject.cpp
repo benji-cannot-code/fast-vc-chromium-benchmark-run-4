851fb58fe40c4474c00bc5c01fd1c3149fa59ea6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-08 22:30:22 +0000

    Rename RenderObject::firstLineStyleSlowCase() to a more appropriate cachedFirstLineStyle() https://bugs.webkit.org/show_bug.cgi?id=98631
    Patch by Arpita Bahuguna <arpitabahuguna@gmail.com> on 2012-10-08
    Reviewed by Eric Seidel.
    
    Renaming firstLineStyleSlowCase() to cachedFirstLineStyle().
    
    The current name is confusing since the function actually gets the
    the cached first-line styles.
    It also makes the name more descriptive of its functionality
    and in accordance with RenderObject::uncachedFirstLineStyle().
    
    No new tests required for this change since no change in behavior is expected.
    
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::cachedFirstLineStyle):
    * rendering/RenderObject.h:
    (WebCore::RenderObject::firstLineStyle):
    (RenderObject):
    Renamed RenderObject::firstLineStyleSlowCase() to cachedFirstLineStyle()
    and made the associated changes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130694 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -2644,7 +2644,7 @@ PassRefPtr<RenderStyle> RenderObject::uncachedFirstLineStyle(RenderStyle* style)
     return firstLineStyleForCachedUncachedType(Uncached, this, style);
 }
 
-RenderStyle* RenderObject::firstLineStyleSlowCase() const
+RenderStyle* RenderObject::cachedFirstLineStyle() const
 {
     ASSERT(document()->styleSheetCollection()->usesFirstLineRules());
 
