b85fb5eb59c3772816c3125c344eb4a975676a62
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-09 21:35:11 +0000

    Only measure text once instead of twice when performing line layout. https://bugs.webkit.org/show_bug.cgi?id=98317 <rdar://problem/12080821>
    Reviewed by Dan Bernstein.
    
    Since we are measuring each word to find out where the line break should occur,
    we should cache that information to avoid measuring the run again when
    creating the line box. The bulk of the change is in nextLineBreak, where
    the measurements are collected and placed in a vector so that they can
    be consumed in setLogicalWidthForTextRun where we used to measure the
    text one more time.
    Each entry in the vector is a WordMeasurement object that contains information
    about the start and end offset in the run, the renderer, the measured width
    and, possibly, a list of fallback fonts.
    When we need to compute the width of the run to create the line box, we add
    all the measurements for the given renderer in the run to get the total width.
    
    * platform/graphics/Font.cpp:
    (WebCore::Font::width): Added fallback fonts parameter.
    * platform/graphics/Font.h:
    (Font): Added fallback fonts parameter to the width static member function.
    This method is called when we compute the width using TextLayout.
    * platform/graphics/mac/ComplexTextController.cpp:
    (WebCore::TextLayout::width): Added fallback fonts parameter.
    (WebCore::Font::width):
    (WebCore::ComplexTextController::advance):
    * platform/graphics/mac/ComplexTextController.h:
    (ComplexTextController): Added fallback fonts parameter to advance method.
    * rendering/RenderBlock.h: Added WordMeasures parameter to few methods.
    * rendering/RenderBlockLineLayout.cpp:
    (WordMeasurement): Added new class to hold measurement information.
    (WebCore::setLogicalWidthForTextRun): This is where we compute the run width using the
    cached information.
    (WebCore::RenderBlock::computeInlineDirectionPositionsForLine): Added wordMeasures parameter.
    (WebCore::RenderBlock::createLineBoxesFromBidiRuns): Added wordMeasures parameter.
    (WebCore::RenderBlock::layoutRunsAndFloatsInRange): Added declaration of the WordMeasures
    vector and its use.
    (WebCore::textWidth): Added fallbackFonts parameter, since now we only measure once.
    (WebCore::RenderBlock::LineBreaker::nextLineBreak): This method has been modified to collect
    the measurements of the individual words and add them to the vector.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
@@ -54,6 +54,7 @@ struct PaintInfo;
 class LineInfo;
 class RenderRubyRun;
 class TextLayout;
+class WordMeasurement;
 
 template <class Iterator, class Run> class BidiResolver;
 template <class Run> class BidiRunList;
@@ -63,6 +64,7 @@ typedef MidpointState<InlineIterator> LineMidpointState;
 typedef WTF::ListHashSet<RenderBox*, 16> TrackedRendererListHashSet;
 typedef WTF::HashMap<const RenderBlock*, TrackedRendererListHashSet*> TrackedDescendantsMap;
 typedef WTF::HashMap<const RenderBox*, HashSet<RenderBlock*>*> TrackedContainerMap;
+typedef Vector<WordMeasurement, 64> WordMeasurements;
 
 enum CaretType { CursorCaret, DragCaret };
 
@@ -730,7 +732,7 @@ private:
             reset();
         }
 
-        InlineIterator nextLineBreak(InlineBidiResolver&, LineInfo&, RenderTextInfo&, FloatingObject* lastFloatFromPreviousLine, unsigned consecutiveHyphenatedLines);
+        InlineIterator nextLineBreak(InlineBidiResolver&, LineInfo&, RenderTextInfo&, FloatingObject* lastFloatFromPreviousLine, unsigned consecutiveHyphenatedLines, WordMeasurements&);
 
         bool lineWasHyphenated() { return m_hyphenated; }
         const Vector<RenderBox*>& positionedObjects() { return m_positionedObjects; }
@@ -758,7 +760,7 @@ private:
 
     void setMarginsForRubyRun(BidiRun*, RenderRubyRun*, RenderObject*, const LineInfo&);
 
-    void computeInlineDirectionPositionsForLine(RootInlineBox*, const LineInfo&, BidiRun* firstRun, BidiRun* trailingSpaceRun, bool reachedEnd, GlyphOverflowAndFallbackFontsMap&, VerticalPositionCache&);
+    void computeInlineDirectionPositionsForLine(RootInlineBox*, const LineInfo&, BidiRun* firstRun, BidiRun* trailingSpaceRun, bool reachedEnd, GlyphOverflowAndFallbackFontsMap&, VerticalPositionCache&, WordMeasurements&);
     void computeBlockDirectionPositionsForLine(RootInlineBox*, BidiRun*, GlyphOverflowAndFallbackFontsMap&, VerticalPositionCache&);
     void deleteEllipsisLineBoxes();
     void checkLinesForTextOverflow();
@@ -964,7 +966,7 @@ private:
     // End helper functions and structs used by layoutBlockChildren.
 
     // Helper function for layoutInlineChildren()
-    RootInlineBox* createLineBoxesFromBidiRuns(BidiRunList<BidiRun>&, const InlineIterator& end, LineInfo&, VerticalPositionCache&, BidiRun* trailingSpaceRun);
+    RootInlineBox* createLineBoxesFromBidiRuns(BidiRunList<BidiRun>&, const InlineIterator& end, LineInfo&, VerticalPositionCache&, BidiRun* trailingSpaceRun, WordMeasurements&);
     void layoutRunsAndFloats(LineLayoutState&, bool hasInlineChild);
     void layoutRunsAndFloatsInRange(LineLayoutState&, InlineBidiResolver&, const InlineIterator& cleanLineStart, const BidiStatus& cleanLineBidiStatus, unsigned consecutiveHyphenatedLines);
     void linkToEndLineIfNeeded(LineLayoutState&);
