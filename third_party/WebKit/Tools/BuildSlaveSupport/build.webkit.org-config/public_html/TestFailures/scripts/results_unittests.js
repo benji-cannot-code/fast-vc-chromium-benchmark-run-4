50ecf49f1a3eca72380aad7935f772ff6437b4d6
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-09 01:36:26 +0000

    results.html and garden-o-matic are ignoring IMAGE failures when expected to FAIL https://bugs.webkit.org/show_bug.cgi?id=98706
    Reviewed by Ojan Vafai.
    
    FAIL is supposed to map onto Failure which is supposed to map
    onto the old [ TEXT, IMAGE_PLUS_TEXT, AUDIO ] mapping.
    results.html was including IMAGE in this and garden-o-matic was
    including CRASH and TIMEOUT as well :(.
    
    Tools:
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results_unittests.js:
    
    LayoutTests:
    
    * fast/harness/resources/results-test.js:
    * fast/harness/results.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results_unittests.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results_unittests.js
@@ -85,7 +85,7 @@ unittest.kExampleResultsJSON = {
     "revision": "90430"
 };
 
-test("ResultAnalyzer", 35, function() {
+test("ResultAnalyzer", 55, function() {
     var analyzer;
 
     analyzer = new results.ResultAnalyzer({expected: 'PASS', actual: 'TEXT'});
@@ -131,6 +131,34 @@ test("ResultAnalyzer", 35, function() {
     ok(analyzer.flaky());
 
     analyzer = new results.ResultAnalyzer({expected: 'FAIL', actual: 'TIMEOUT'});
+    ok(!analyzer.expectedToSucceed());
+    ok(analyzer.hasUnexpectedFailures());
+    deepEqual(analyzer.unexpectedResults(), ['TIMEOUT']);
+    ok(!analyzer.succeeded());
+    ok(!analyzer.flaky());
+
+    analyzer = new results.ResultAnalyzer({expected: 'FAIL', actual: 'IMAGE'});
+    ok(!analyzer.expectedToSucceed());
+    ok(analyzer.hasUnexpectedFailures());
+    deepEqual(analyzer.unexpectedResults(), ['IMAGE']);
+    ok(!analyzer.succeeded());
+    ok(!analyzer.flaky());
+
+    analyzer = new results.ResultAnalyzer({expected: 'FAIL', actual: 'AUDIO'});
+    ok(!analyzer.expectedToSucceed());
+    ok(!analyzer.hasUnexpectedFailures());
+    deepEqual(analyzer.unexpectedResults(), []);
+    ok(!analyzer.succeeded());
+    ok(!analyzer.flaky());
+
+    analyzer = new results.ResultAnalyzer({expected: 'FAIL', actual: 'TEXT'});
+    ok(!analyzer.expectedToSucceed());
+    ok(!analyzer.hasUnexpectedFailures());
+    deepEqual(analyzer.unexpectedResults(), []);
+    ok(!analyzer.succeeded());
+    ok(!analyzer.flaky());
+
+    analyzer = new results.ResultAnalyzer({expected: 'FAIL', actual: 'IMAGE+TEXT'});
     ok(!analyzer.expectedToSucceed());
     ok(!analyzer.hasUnexpectedFailures());
     deepEqual(analyzer.unexpectedResults(), []);
