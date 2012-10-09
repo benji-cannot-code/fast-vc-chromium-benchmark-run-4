8671c2eadc85e428f1977f3fa3a783faf7fe57f8
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-09 21:49:06 +0000

    Build fix for Qt Windows.
    * html/HTMLPlugInElement.h: Include Image.h.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130814 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLPlugInElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLPlugInElement.h
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HTMLPlugInElement_h
 
 #include "HTMLFrameOwnerElement.h"
+#include "Image.h"
 #include "ImageLoaderClient.h"
 #include "ScriptInstance.h"
 
@@ -34,7 +35,6 @@ struct NPObject;
 
 namespace WebCore {
 
-class Image;
 class RenderEmbeddedObject;
 class RenderWidget;
 class Widget;
