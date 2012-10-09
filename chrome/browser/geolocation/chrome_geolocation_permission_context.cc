c414f970086ce0ab0928d28157a2703ae0f1c7fb
jknotten@chromium.org <jknotten@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-09 18:30:37 +0000

    Don't retain reference to ChromeGeolocationPermissionContext
    ChromeGeolocationPermissionContext owns
    GeolocationInfoBarQueueController, so make sure that the callback
    passed to GeolocationInfoBarQueueController doesn't increase the
    reference count on ChromeGeolocationPermissionContext (which 
    https://chromiumcodereview.appspot.com/11072012 accidentally does).
    
    TBR=bulach
    
    BUG=152921
    TEST=unittest:chrome_geolocation_permission_context on memory.fyi bot
    
    Review URL: https://chromiumcodereview.appspot.com/11087030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@160881 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/chrome_geolocation_permission_context.cc
+++ chrome/browser/geolocation/chrome_geolocation_permission_context.cc
@@ -41,7 +41,7 @@ ChromeGeolocationPermissionContext::ChromeGeolocationPermissionContext(
          new GeolocationInfoBarQueueController(
              base::Bind(
                  &ChromeGeolocationPermissionContext::NotifyPermissionSet,
-                 this),
+                 base::Unretained(this)),
              profile))) {
 }
 
