6dab77c02b8913279bc332b2941143a75893f4f2
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 12:32:36 +0000

    Replace legacy SyncableFileSystem code to use new utility functions now available in SyncableFileSystemUtil.
    BUG=153965
    
    
    Review URL: https://chromiumcodereview.appspot.com/11094042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161094 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_context.cc
+++ webkit/fileapi/file_system_context.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/fileapi/isolated_mount_point_provider.h"
 #include "webkit/fileapi/sandbox_mount_point_provider.h"
 #include "webkit/fileapi/syncable/local_file_change_tracker.h"
+#include "webkit/fileapi/syncable/syncable_file_system_util.h"
 #include "webkit/fileapi/test_mount_point_provider.h"
 #include "webkit/quota/quota_manager.h"
 #include "webkit/quota/special_storage_policy.h"
@@ -205,16 +206,9 @@ void FileSystemContext::OpenSyncableFileSystem(
   DCHECK(!callback.is_null());
 
   DCHECK(type == kFileSystemTypeSyncable);
-  IsolatedContext::GetInstance()->RegisterExternalFileSystem(
-      mount_name,
-      kFileSystemTypeSyncable,
-      FilePath());
+  RegisterSyncableFileSystem(mount_name);
 
-  std::string root = GetFileSystemRootURI(
-      origin_url, kFileSystemTypeExternal).spec();
-  root.append(mount_name);
-  root.append("/");
-  GURL root_url = GURL(root);
+  GURL root_url = GetSyncableFileSystemRootURI(origin_url, mount_name);
   std::string name = GetFileSystemName(origin_url, kFileSystemTypeSyncable);
 
   FileSystemMountPointProvider* mount_point_provider =
