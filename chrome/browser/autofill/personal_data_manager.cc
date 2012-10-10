d1ef4a03767a968535133e063662c8d47c76cfc7
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 03:56:30 +0000

    add some missing includes of <functional> to fix compilation on VS2012
    BUG=143646
    
    
    Review URL: https://chromiumcodereview.appspot.com/11094029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161031 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/personal_data_manager.cc
+++ chrome/browser/autofill/personal_data_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/personal_data_manager.h"
 
 #include <algorithm>
+#include <functional>
 #include <iterator>
 
 #include "base/logging.h"
--- webkit/appcache/appcache_service.cc
+++ webkit/appcache/appcache_service.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/appcache/appcache_service.h"
 
+#include <functional>
+
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/logging.h"
