8a3070aa47cea0ab00c423fa380afbcbe85609e8
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 04:05:53 +0000

    Fix a crash on Google Drive operations cancelled during reauthenticaiton.
    This patch introduces a weak pointer to safely notify to the authenticater
    that an operations is terminated by the user.
    
    The operations are terminated for three reasons: (1) user cancel, (2)
    succesful or failure) completion, and (3) an authentification failure,
    later separated from (2).
     - (1) immediately shuts down (2) by URLFetcher::Cancel().
     - (2) and (3) immediately unrolls the operation so (1) won't happen.
     - The steps (2) and (3) are never simultaneously run, so no problem.
    So, the only problematic case was the event (1) followed by (3), in
    that case a deleted operation object was used.
    
    BUG=153003
    
    
    Review URL: https://chromiumcodereview.appspot.com/11032031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161036 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/operation_runner.cc
+++ chrome/browser/google_apis/operation_runner.cc
@@ -64,7 +64,7 @@ void OperationRunner::StartOperation(
         operation_registry_.get(),
         base::Bind(&OperationRunner::OnOperationAuthRefresh,
                    weak_ptr_factory_.GetWeakPtr(),
-                   operation));
+                   operation->GetWeakPtr()));
     return;
   }
 
@@ -72,10 +72,14 @@ void OperationRunner::StartOperation(
 }
 
 void OperationRunner::OnOperationAuthRefresh(
-    AuthenticatedOperationInterface* operation,
+    const base::WeakPtr<AuthenticatedOperationInterface>& operation,
     GDataErrorCode code,
     const std::string& auth_token) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+  // Do nothing if the operation is canceled during authentication.
+  if (!operation)
+    return;
 
   if (code == HTTP_SUCCESS) {
     DCHECK(auth_service_->HasRefreshToken());
