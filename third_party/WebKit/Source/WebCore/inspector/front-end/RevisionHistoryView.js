02e9841bee9fc5ceb95f8a13affb0c5f6ea0e10b
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-10 16:20:16 +0000

    Web Inspector: Exception when committing java script editing with revision history view open. https://bugs.webkit.org/show_bug.cgi?id=98908
    Reviewed by Alexander Pavlov.
    
    Added null check.
    
    * inspector/front-end/RevisionHistoryView.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130913 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/RevisionHistoryView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/RevisionHistoryView.js
@@ -167,6 +167,8 @@ WebInspector.RevisionHistoryView.prototype = {
     _removeUISourceCode: function(uiSourceCode)
     {
         var uiSourceCodeItem = this._uiSourceCodeItems.get(uiSourceCode);
+        if (!uiSourceCodeItem)
+            return;
         this._treeOutline.removeChild(uiSourceCodeItem);
         this._uiSourceCodeItems.remove(uiSourceCode);
     },
