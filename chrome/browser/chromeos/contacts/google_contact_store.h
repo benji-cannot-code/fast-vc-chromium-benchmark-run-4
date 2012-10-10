0d5b0f27b53469195c1dea0bc5748e5a40b49df2
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 19:48:01 +0000

    contacts: Don't save deleted contacts to disk.
    GoogleContactStore was previously passing all contacts to
    ContactDatabase regardless of their "deleted" flag.  This
    change makes it instead not save already-deleted contacts,
    drop contacts when they become deleted, and track the
    most-recent last-updated time from a deleted contact (which
    is needed for incremental updates).
    
    BUG=128805
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/10933127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/contacts/google_contact_store.h
+++ chrome/browser/chromeos/contacts/google_contact_store.h
@@ -46,7 +46,9 @@ class GoogleContactStore
     ~TestAPI();
 
     bool update_scheduled() { return store_->update_timer_.IsRunning(); }
-
+    base::Time last_contact_update_time() const {
+      return store_->last_contact_update_time_;
+    }
     void set_current_time(const base::Time& time) {
       store_->current_time_for_testing_ = time;
     }
@@ -63,6 +65,10 @@ class GoogleContactStore
 
     // Notifies the store that the system has gone online or offline.
     void NotifyAboutNetworkStateChange(bool online);
+
+    // Returns pointers to all of the contacts in the store's |contacts_|
+    // member.
+    scoped_ptr<ContactPointers> GetLoadedContacts();
 
    private:
     GoogleContactStore* store_;  // not owned
