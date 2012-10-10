5e7209fb707fedbda30c8f29611dede757e7dd24
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 02:49:56 +0000

    Add crash key for Mac video capture
    This CL calls SetCrashKeyValue() if a video capture device is opened. This can
    be used to determine if the crash involves video capture or if simply
    enumerating video capture device is enough to cause a crash.
    
    BUG=139164
    
    
    Review URL: https://chromiumcodereview.appspot.com/11096030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161025 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/mac/video_capture_device_qtkit_mac.mm
+++ media/video/capture/mac/video_capture_device_qtkit_mac.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <QTKit/QTKit.h>
 
 #include "base/logging.h"
+#include "base/mac/crash_logging.h"
 #include "media/video/capture/mac/video_capture_device_mac.h"
 #include "media/video/capture/video_capture_device.h"
 #include "media/video/capture/video_capture_types.h"
@@ -96,6 +97,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                   << [[error localizedDescription] UTF8String];
       return NO;
     }
+
+    // This key can be used to check if video capture code was related to a
+    // particular crash.
+    base::mac::SetCrashKeyValue(@"VideoCaptureDeviceQTKit", @"OpenedDevice");
+
     return YES;
   } else {
     // Remove the previously set capture device.
