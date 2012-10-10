dbcd9af5267455b70ec1a1d09e0c93af4d7807a1
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 18:49:33 +0000

    Add gfx::ToRoundedInt safe conversion method from float to int.
    BUG=147395
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11093031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161137 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/safe_integer_conversions.cc
+++ ui/gfx/safe_integer_conversions.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ui/gfx/safe_integer_conversions.h"
+
 #include <cmath>
 #include <limits>
 
@@ -11,9 +13,9 @@ namespace gfx {
 int ClampToInt(float value) {
   if (value != value)
      return 0; // no int NaN.
-  if (value > std::numeric_limits<int>::max())
+  if (value >= std::numeric_limits<int>::max())
     return std::numeric_limits<int>::max();
-  if (value < std::numeric_limits<int>::min())
+  if (value <= std::numeric_limits<int>::min())
     return std::numeric_limits<int>::min();
   return static_cast<int>(value);
 }
@@ -26,5 +28,13 @@ int ToCeiledInt(float value) {
   return ClampToInt(std::ceil(value));
 }
 
-}  // namespace gfx
+int ToRoundedInt(float value) {
+  float rounded;
+  if (value >= 0.0f)
+    rounded = std::floor(value + 0.5f);
+  else
+    rounded = std::ceil(value - 0.5f);
+  return ClampToInt(rounded);
+}
 
+}  // namespace gfx
