2b899a3a8e3d078ace414d2c9762e827b6b2663a
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 04:19:15 +0000

    beginSmoothScroll should send wheel ticks at constant velocity.
    There are two fixes here:
    1. Compute the wheel tick amount based on time elapsed rather than our post
       message rate.
    2. Try to trigger a new forward when needed on the inputAck, instead of using
       postTask. This helps when the main thread is busy doing swaps, which happens
       on OSX.
    
    BUG=149408
    
    
    Review URL: https://chromiumcodereview.appspot.com/11013043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161038 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/process_util.h"
 #include "base/string16.h"
+#include "base/time.h"
 #include "base/timer.h"
 #include "build/build_config.h"
 #include "content/common/view_message_enums.h"
@@ -826,6 +827,8 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   typedef std::map<int, scoped_refptr<SmoothScrollGesture> >
       SmoothScrollGestureMap;
   SmoothScrollGestureMap active_smooth_scroll_gestures_;
+  base::TimeTicks last_smooth_scroll_gestures_tick_time_;
+  bool tick_active_smooth_scroll_gestures_task_posted_;
 
   scoped_ptr<GestureEventFilter> gesture_event_filter_;
 
