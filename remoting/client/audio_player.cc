de8f6a7409420226d1bd0272c669f6f972ebb71e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 03:38:40 +0000

    Fix remoting static initializer errors from r161017.
    TBR=garykac
    Review URL: https://codereview.chromium.org/11098038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161029 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/audio_player.cc
+++ remoting/client/audio_player.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/logging.h"
 #include "base/stl_util.h"
 
 // The number of channels in the audio stream (only supporting stereo audio
--- remoting/client/audio_player.h
+++ remoting/client/audio_player.h
@@ -8,13 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <list>
 
-#include "base/bind.h"
-#include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
 #include "remoting/proto/audio.pb.h"
-#include "testing/gtest/include/gtest/gtest.h"
 
 namespace remoting {
 
