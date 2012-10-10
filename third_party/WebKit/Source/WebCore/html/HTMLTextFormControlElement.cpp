526b1a65beebbf98593944e4b928479932f98ccb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-10 13:56:10 +0000

    HTMLTextFormControlElement calls setInlineStyleProperty with the wrong parameters. https://bugs.webkit.org/show_bug.cgi?id=98892
    Patch by Mike West <mkwst@google.com> on 2012-10-10
    Reviewed by Jochen Eisinger.
    
    HTMLTextFormControlElement::updatePlaceholderVisibility calls
    HTMLElement::setInlineStyleProperty with an ExceptionCode as the last
    parameter. This only succeeds because it's automagically cast to a bool,
    which is what the method actually expects.
    
    Implicitly passing in 'false' by dropping the parameter shouldn't change
    any functionality; so no new tests are required.
    
    * html/HTMLTextFormControlElement.cpp:
    (WebCore::HTMLTextFormControlElement::updatePlaceholderVisibility):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLTextFormControlElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLTextFormControlElement.cpp
@@ -160,9 +160,7 @@ void HTMLTextFormControlElement::updatePlaceholderVisibility(bool placeholderVal
     HTMLElement* placeholder = placeholderElement();
     if (!placeholder)
         return;
-    ExceptionCode ec = 0;
-    placeholder->setInlineStyleProperty(CSSPropertyVisibility, placeholderShouldBeVisible() ? "visible" : "hidden", ec);
-    ASSERT(!ec);
+    placeholder->setInlineStyleProperty(CSSPropertyVisibility, placeholderShouldBeVisible() ? "visible" : "hidden");
 }
 
 void HTMLTextFormControlElement::fixPlaceholderRenderer(HTMLElement* placeholder, HTMLElement* siblingElement)
