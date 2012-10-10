091b080147fe6fd61f4eb9577db7bde8ed26673a
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-10 15:20:38 +0000

    Activate appropriate window from a session restore.
    BUG=151979
    
    
    Review URL: https://chromiumcodereview.appspot.com/10982098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161116 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_service_test_helper.h
+++ chrome/browser/sessions/session_service_test_helper.h
@@ -11,10 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "chrome/browser/sessions/session_id.h"
 
 class SessionBackend;
 class SessionCommand;
-class SessionID;
 class SessionService;
 struct SessionTab;
 struct SessionWindow;
@@ -27,9 +27,6 @@ class SessionServiceTestHelper {
   explicit SessionServiceTestHelper();
   explicit SessionServiceTestHelper(SessionService* service);
   ~SessionServiceTestHelper();
-
-  void RestoreSessionFromCommands(const std::vector<SessionCommand*>& commands,
-                                  std::vector<SessionWindow*>* valid_windows);
 
   void PrepareTabInWindow(const SessionID& window_id,
                           const SessionID& tab_id,
@@ -48,7 +45,8 @@ class SessionServiceTestHelper {
       bool force_browser_not_alive_with_no_windows);
 
   // Reads the contents of the last session.
-  void ReadWindows(std::vector<SessionWindow*>* windows);
+  void ReadWindows(std::vector<SessionWindow*>* windows,
+                   SessionID::id_type* active_window_id);
 
   void AssertTabEquals(SessionID& window_id,
                        SessionID& tab_id,
