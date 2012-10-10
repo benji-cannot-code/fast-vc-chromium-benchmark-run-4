355bb973dd2e16a500f4db028150706f4cd48816
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-10 17:30:55 +0000

    NRWT crashes on too many timeouts https://bugs.webkit.org/show_bug.cgi?id=97047
    Reviewed by Tony Chang.
    
    Apparently if you kill a subprocess directly python doesn't
    auto-close the file descriptors.
    
    * Scripts/webkitpy/layout_tests/port/server_process.py:
    (ServerProcess._reset):
    * Scripts/webkitpy/layout_tests/port/server_process_unittest.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@130922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/server_process_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/server_process_unittest.py
@@ -58,6 +58,7 @@ class TrivialMockPort(object):
 class MockFile(object):
     def __init__(self, server_process):
         self._server_process = server_process
+        self.closed = False
 
     def fileno(self):
         return 1
@@ -67,7 +68,7 @@ class MockFile(object):
         raise IOError
 
     def close(self):
-        pass
+        self.closed = True
 
 
 class MockProc(object):
@@ -88,6 +89,8 @@ class FakeServerProcess(server_process.ServerProcess):
     def _start(self):
         self._proc = MockProc(self)
         self.stdin = self._proc.stdin
+        self.stdout = self._proc.stdout
+        self.stderr = self._proc.stderr
         self._pid = self._proc.pid
         self.broken_pipes = []
 
@@ -121,6 +124,15 @@ class TestServerProcess(unittest.TestCase):
             self.assertEquals(line.strip(), "stderr")
 
         proc.stop(0)
+
+    def test_cleanup(self):
+        port_obj = TrivialMockPort()
+        server_process = FakeServerProcess(port_obj=port_obj, name="test", cmd=["test"])
+        server_process._start()
+        server_process.stop()
+        self.assertTrue(server_process.stdin.closed)
+        self.assertTrue(server_process.stdout.closed)
+        self.assertTrue(server_process.stderr.closed)
 
     def test_broken_pipe(self):
         port_obj = TrivialMockPort()
