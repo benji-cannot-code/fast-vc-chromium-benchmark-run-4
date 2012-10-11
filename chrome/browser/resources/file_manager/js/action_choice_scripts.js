ece7e14bba6648c0b0af950f413e546ff76ad48a
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 12:24:05 +0000

    [filemanager] Polish of action choice dialog.
    BUG=151822,151824,151834,151825
    TEST=See bugs.
    Review URL: https://codereview.chromium.org/11093049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/action_choice_scripts.js
+++ chrome/browser/resources/file_manager/js/action_choice_scripts.js
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 //<include src="util.js"/>
 //<include src="file_type.js"/>
+//<include src="path_util.js"/>
+//<include src="volume_manager.js"/>
 //<include src="metadata/metadata_cache.js"/>
 //<include src="metrics.js"/>
 //<include src="image_editor/image_util.js"/>
