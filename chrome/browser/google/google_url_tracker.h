100f2d79fc5fb34ee995c3191c9a9953ee10b8fb
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 22:21:54 +0000

    Remove NOTIFICATION_TAB_CONTENTS_DESTROYED from GoogleURLTracker.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11094083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_url_tracker.h
+++ chrome/browser/google/google_url_tracker.h
@@ -101,12 +101,12 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
     MapEntry();  // Required by STL.
     MapEntry(GoogleURLTrackerInfoBarDelegate* infobar,
              const content::NotificationSource& navigation_controller_source,
-             const content::NotificationSource& tab_contents_source);
+             const content::NotificationSource& web_contents_source);
     ~MapEntry();
 
     GoogleURLTrackerInfoBarDelegate* infobar;
     content::NotificationSource navigation_controller_source;
-    content::NotificationSource tab_contents_source;
+    content::NotificationSource web_contents_source;
   };
 
   typedef std::map<const InfoBarTabHelper*, MapEntry> InfoBarMap;
@@ -154,7 +154,7 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
 
   // Called by Observe() after SearchCommitted() registers notification
   // listeners, to indicate that we've received the "load now pending"
-  // notification.  |navigation_controller_source| and |tab_contents_source| are
+  // notification.  |navigation_controller_source| and |web_contents_source| are
   // NotificationSources pointing to the associated NavigationController and
   // TabContents, respectively, for this load; |infobar_helper| is the
   // InfoBarTabHelper of the associated tab; and |pending_id| is the unique ID
@@ -164,7 +164,7 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
   // (still-invisible) InfoBarDelegate for the associated tab.
   void OnNavigationPending(
       const content::NotificationSource& navigation_controller_source,
-      const content::NotificationSource& tab_contents_source,
+      const content::NotificationSource& web_contents_source,
       InfoBarTabHelper* infobar_helper,
       int pending_id);
 
@@ -181,7 +181,7 @@ class GoogleURLTracker : public net::URLFetcherDelegate,
   // listening for, may then also call OnNavigationCommittedOrTabClosed().
   void OnInstantCommitted(
     const content::NotificationSource& navigation_controller_source,
-    const content::NotificationSource& tab_contents_source,
+    const content::NotificationSource& web_contents_source,
     InfoBarTabHelper* infobar_helper,
     const GURL& search_url);
 
--- chrome/browser/google/google_url_tracker_unittest.cc
+++ chrome/browser/google/google_url_tracker_unittest.cc
@@ -22,8 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/url_request/url_fetcher.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-class TabContents;
-
 // TestNotificationObserver ---------------------------------------------------
 
 namespace {
@@ -250,7 +248,8 @@ void GoogleURLTrackerTest::SetNavigationPending(int unique_id, bool is_search) {
     google_url_tracker_->OnNavigationPending(
         content::Source<content::NavigationController>(
             reinterpret_cast<content::NavigationController*>(unique_id)),
-        content::Source<TabContents>(reinterpret_cast<TabContents*>(unique_id)),
+        content::Source<content::WebContents>(
+            reinterpret_cast<content::WebContents*>(unique_id)),
         reinterpret_cast<InfoBarTabHelper*>(unique_id), unique_id);
   }
 }
@@ -299,7 +298,8 @@ void GoogleURLTrackerTest::DoInstantNavigation(int unique_id,
     google_url_tracker_->OnInstantCommitted(
         content::Source<content::NavigationController>(
             reinterpret_cast<content::NavigationController*>(unique_id)),
-        content::Source<TabContents>(reinterpret_cast<TabContents*>(unique_id)),
+        content::Source<content::WebContents>(
+            reinterpret_cast<content::WebContents*>(unique_id)),
         reinterpret_cast<InfoBarTabHelper*>(unique_id), search_url);
   }
 }
@@ -308,9 +308,11 @@ void GoogleURLTrackerTest::CloseTab(int unique_id) {
   unique_ids_seen_.erase(unique_id);
   InfoBarTabHelper* infobar_helper =
       reinterpret_cast<InfoBarTabHelper*>(unique_id);
-  if (google_url_tracker_->registrar_.IsRegistered(google_url_tracker_.get(),
-      chrome::NOTIFICATION_TAB_CONTENTS_DESTROYED, content::Source<TabContents>(
-          reinterpret_cast<TabContents*>(unique_id)))) {
+  if (google_url_tracker_->registrar_.IsRegistered(
+          google_url_tracker_.get(),
+          content::NOTIFICATION_WEB_CONTENTS_DESTROYED,
+          content::Source<content::WebContents>(
+              reinterpret_cast<content::WebContents*>(unique_id)))) {
     google_url_tracker_->OnNavigationCommittedOrTabClosed(infobar_helper,
                                                           GURL());
   } else {
