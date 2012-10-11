f3fc181099e5641f6261a696559ad88862ebd265
simonjam@chromium.org <simonjam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-11 23:08:13 +0000

    unprefix window.performance.webkitNow() https://bugs.webkit.org/show_bug.cgi?id=88278
    Reviewed by Tony Gentilcore.
    
    PerformanceTests:
    
    * resources/runner.js: Bind to proper object to avoid invalid invocation errors.
    
    Source/WebCore:
    
    Test: fast/dom/Window/window-properties-performance.html
          fast/performance/performance-now-timestamps.html
    
    * page/Performance.cpp:
    (WebCore::Performance::now):
    * page/Performance.h:
    (Performance):
    * page/Performance.idl:
    
    LayoutTests:
    
    * fast/dom/Window/window-properties-performance-expected.txt:
    * fast/performance/performance-now-timestamps-expected.txt:
    * fast/performance/script-tests/performance-now-timestamps.js:
    * platform/chromium/TestExpectations: Remove skipped expectation.
    * platform/efl/TestExpectations:
    * platform/efl/fast/dom/Window/window-properties-performance-expected.txt:
    * platform/gtk/TestExpectations:
    * platform/qt/TestExpectations:
    * platform/qt/fast/dom/Window/window-properties-performance-expected.txt:
    * resources/magnitude-perf.js:
    (Magnitude._runIteration):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131106 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/performance/script-tests/performance-now-timestamps.js
+++ third_party/WebKit/LayoutTests/fast/performance/script-tests/performance-now-timestamps.js
@@ -1,12 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description("Tests the timestamps from performance.webkitNow increase and are in milliseconds");
+description("Tests the timestamps from performance.now increase and are in milliseconds");
 
 function busyWait(millis) {
     var start = Date.now();
     while (Date.now() - start < millis) {}
 }
 
-var firstTimestamp = window.performance.webkitNow();
+var firstTimestamp = window.performance.now();
 shouldBeDefined("firstTimestamp");
 shouldBeGreaterThanOrEqual("firstTimestamp", "0");
 
@@ -18,7 +18,7 @@ shouldBeTrue("firstTimestamp < 5000");
 var waitTime = 10;
 busyWait(waitTime);
 
-var secondTimestamp = window.performance.webkitNow();
+var secondTimestamp = window.performance.now();
 shouldBeDefined("secondTimestamp");
 shouldBeGreaterThanOrEqual("secondTimestamp", "firstTimestamp + (waitTime / 2)");
 
