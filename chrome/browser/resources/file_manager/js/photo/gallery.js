94f4953d32afaf6f5e51c210af6d65b984b3f234
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 14:46:55 +0000

    Add controls to slideshow
    BUG=151817
    
    Review URL: https://codereview.chromium.org/11087005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161343 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/photo/gallery.js
+++ chrome/browser/resources/file_manager/js/photo/gallery.js
@@ -369,7 +369,9 @@ Gallery.prototype.checkActivity_ = function() {
 */
 Gallery.prototype.onUserAction_ = function() {
   this.closeShareMenu_();
+  // Show the toolbar and hide it after the default timeout.
   this.inactivityWatcher_.startActivity();
+  this.inactivityWatcher_.stopActivity();
 };
 
 /**
@@ -589,8 +591,7 @@ Gallery.prototype.onKeyDown_ = function(event) {
 
 
     case 'U+0056':  // 'v'
-      this.slideMode_.toggleSlideshow(
-          SlideMode.SLIDESHOW_INTERVAL_FIRST, event);
+      this.slideMode_.startSlideshow(SlideMode.SLIDESHOW_INTERVAL_FIRST, event);
       return;
 
     case 'U+007F':  // Delete
