adfa5a05840d19e3735db97827b2e80846c8570c
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-11 21:53:23 +0000

    UInt32ToNumber and OSR exit should be aware of copy propagation and correctly recover both versions of a variable that was subject to a UInt32ToNumber cast https://bugs.webkit.org/show_bug.cgi?id=99100 <rdar://problem/12480955>
    Reviewed by Michael Saboff and Mark Hahnenberg.
    
    Source/JavaScriptCore: 
    
    Fixed by forcing UInt32ToNumber to use a different register. This "undoes" the copy propagation that we
    would have been doing, since it has no performance effect in this case and has the benefit of making the
    OSR exit compiler a lot simpler.
    
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compileUInt32ToNumber):
    
    LayoutTests: 
    
    * fast/js/dfg-uint32-to-number-in-middle-of-copy-propagation-expected.txt: Added.
    * fast/js/dfg-uint32-to-number-in-middle-of-copy-propagation.html: Added.
    * fast/js/jsc-test-list:
    * fast/js/script-tests/dfg-uint32-to-number-in-middle-of-copy-propagation.js: Added.
    (foo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131097 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/dfg-uint32-to-number-in-middle-of-copy-propagation.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Tests that UInt32ToNumber and OSR exit are aware of copy propagation and correctly recover both versions of a variable that was subject to a UInt32ToNumber cast."
+);
+
+function foo(b) {
+    var a = b | 0;
+    var x, y;
+    x = a;
+    y = a >>> 0;
+    return [x, y];
+}
+
+for (var i = 0; i < 100; ++i)
+    shouldBe("foo(-1)", "[-1, 4294967295]");
+
