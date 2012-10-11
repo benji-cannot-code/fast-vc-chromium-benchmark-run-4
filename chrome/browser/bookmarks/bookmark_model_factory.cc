b2f97523016439c82c19f7171a359f3852dd16db
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 14:59:15 +0000

    bookmarks: Remove unused includes of history_service_factory.h from some files.
    BUG=144783
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/11085051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model_factory.cc
+++ chrome/browser/bookmarks/bookmark_model_factory.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/singleton.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
-#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 #include "chrome/common/pref_names.h"
--- chrome/browser/bookmarks/bookmark_storage.cc
+++ chrome/browser/bookmarks/bookmark_storage.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "chrome/browser/bookmarks/bookmark_codec.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
-#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_constants.h"
 #include "content/public/browser/browser_thread.h"
