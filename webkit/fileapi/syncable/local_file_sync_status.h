2e5913f13e193fd2553c8aae73ce65bcefbec3d3
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 15:27:38 +0000

    Make LocalFileSyncStatus non thread safe
    * Removing locks
    * Making it inherit from NonThreadSafe
    
    BUG=154234
    TEST=existing tests
    TBR=tzik@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11098068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161348 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/syncable/local_file_sync_status.h
+++ webkit/fileapi/syncable/local_file_sync_status.h
@@ -11,14 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "base/threading/thread.h"
+#include "base/threading/non_thread_safe.h"
 #include "webkit/fileapi/file_system_url.h"
 
 namespace fileapi {
 
 // Represents local file sync status.
-// This class is thread-safe and fields of this class are protected by a lock.
-// TODO(kinuko): Limit access to this class only on IO thread.
+// This class is supposed to run only on IO thread.
 //
 // This class manages two important synchronization flags: writing (counter)
 // and syncing (flag).  Writing counter keeps track of which URL is in
@@ -27,7 +26,7 @@ namespace fileapi {
 // An invariant of this class is: no FileSystem objects should be both
 // in syncing_ and writing_ status, i.e. trying to increment writing
 // while the target url is in syncing must fail and vice versa.
-class FILEAPI_EXPORT LocalFileSyncStatus {
+class FILEAPI_EXPORT LocalFileSyncStatus : public base::NonThreadSafe {
  public:
   LocalFileSyncStatus();
   ~LocalFileSyncStatus();
@@ -59,8 +58,6 @@ class FILEAPI_EXPORT LocalFileSyncStatus {
   // These private methods must be called with the lock_ held.
   bool IsChildOrParentWriting(const FileSystemURL& url) const;
   bool IsChildOrParentSyncing(const FileSystemURL& url) const;
-
-  mutable base::Lock lock_;
 
   // If this count is non-zero positive there're ongoing write operations.
   URLCountMap writing_;
