319eb7ce4bf7f1fd6b4df99d7e514084d44e02e2
bashi@chromium.org <bashi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-11 23:23:47 +0000

    [Chromium] Improve vertical text rendering of HarfBuzzShaper https://bugs.webkit.org/show_bug.cgi?id=98979
    Reviewed by Tony Chang.
    
    - Specify 'vert' and 'vrt2' features when we render vertical text.
    - Set appropriate script so that harfbuzz-ng can use the features.
    
    No new tests. fast/text/international/text-spliced-font.html should cover this change.
    This change will affect after switching to harfbuzz-ng.
    
    * platform/graphics/harfbuzz/ng/HarfBuzzNGFace.cpp:
    (WebCore::HarfBuzzNGFace::HarfBuzzNGFace):
    (WebCore::findScriptForVerticalGlyphSubstitution): Added.
    (WebCore):
    (WebCore::HarfBuzzNGFace::setScriptForVerticalGlyphSubstitution): Added.
    * platform/graphics/harfbuzz/ng/HarfBuzzNGFace.h:
    (HarfBuzzNGFace): Added m_scriptForVerticalText.
    * platform/graphics/harfbuzz/ng/HarfBuzzShaper.cpp:
    (WebCore::HarfBuzzShaper::setFontFeatures):
    Set 'vert' and 'vrt2' features when orientation() == Vertical.
    (WebCore::HarfBuzzShaper::shapeHarfBuzzRuns):
    Call setScriptForVerticalGlyphSubstitution() when orientation() == Vertical.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131109 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzNGFace.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/harfbuzz/ng/HarfBuzzNGFace.h
@@ -45,6 +45,9 @@ class FontPlatformData;
 
 class HarfBuzzNGFace : public RefCounted<HarfBuzzNGFace> {
 public:
+    static const hb_tag_t vertTag;
+    static const hb_tag_t vrt2Tag;
+
     static PassRefPtr<HarfBuzzNGFace> create(FontPlatformData* platformData, uint64_t uniqueID)
     {
         return adoptRef(new HarfBuzzNGFace(platformData, uniqueID));
@@ -52,6 +55,8 @@ public:
     ~HarfBuzzNGFace();
 
     hb_font_t* createFont();
+
+    void setScriptForVerticalGlyphSubstitution(hb_buffer_t*);
 
 private:
     HarfBuzzNGFace(FontPlatformData*, uint64_t);
@@ -62,6 +67,8 @@ private:
     uint64_t m_uniqueID;
     hb_face_t* m_face;
     WTF::HashMap<uint32_t, uint16_t>* m_glyphCacheForFaceCacheEntry;
+
+    hb_script_t m_scriptForVerticalText;
 };
 
 }
