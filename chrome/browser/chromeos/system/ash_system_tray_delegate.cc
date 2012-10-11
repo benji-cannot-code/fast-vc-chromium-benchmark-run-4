e1834998da8328a7ce3d6ab3bbb86f065ea22fc4
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 04:29:46 +0000

    drive: Move IsDriveEnabled() and DisableDrive() to DriveSystemService
    These functions are part of Drive on Chrome OS, hence should be moved
    to chromeos/gdata.
    
    BUG=153962
    TEST=none
    
    Review URL: https://codereview.chromium.org/11094061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/ash_system_tray_delegate.cc
+++ chrome/browser/chromeos/system/ash_system_tray_delegate.cc
@@ -54,7 +54,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/status/network_menu_icon.h"
 #include "chrome/browser/chromeos/system/timezone_settings.h"
 #include "chrome/browser/chromeos/system_key_event_listener.h"
-#include "chrome/browser/google_apis/gdata_util.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile_manager.h"
@@ -1145,7 +1144,7 @@ class SystemTrayDelegate : public ash::SystemTrayDelegate,
 
   DriveSystemService* FindDriveSystemService() {
     Profile* profile = ProfileManager::GetDefaultProfile();
-    if (!gdata::util::IsDriveEnabled(profile))
+    if (!gdata::DriveSystemService::IsDriveEnabled(profile))
       return NULL;
     return DriveSystemServiceFactory::FindForProfile(profile);
   }
