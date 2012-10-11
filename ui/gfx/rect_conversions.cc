b819d5602ac9d845add944a59632727b0051ec16
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 08:44:32 +0000

    Add gfx::ToRoundedInt safe conversion method from float to int.
    This fixes the problems with the windows build that caused revert.
    
    BUG=147395
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11094051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161317 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/rect_conversions.cc
+++ ui/gfx/rect_conversions.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/rect_conversions.h"
 
-#include "ui/gfx/safe_floor_ceil.h"
+#include "ui/gfx/safe_integer_conversions.h"
 
 namespace gfx {
 
