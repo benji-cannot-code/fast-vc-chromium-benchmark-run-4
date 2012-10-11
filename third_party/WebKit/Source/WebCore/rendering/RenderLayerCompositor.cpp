e26f237b15aea042708b6c9fe204a0dff76b4bc8
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-11 04:07:22 +0000

    compositing/tiling/crash-reparent-tiled-layer.html is flakey https://bugs.webkit.org/show_bug.cgi?id=82546
    Reviewed by Beth Dakin.
    
    Source/WebCore:
    
    When calling layerTreeAsText() inside the document load event,
    GraphicsLayerCA may have not flushed layers yet, so layer tree
    properties that are affected by flushing (tiled layer, visible
    rects) appeared flakey in tests.
    
    Fix by forcing a flush before dumping the layer tree.
    
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::layerTreeAsText):
    
    LayoutTests:
    
    Unskip a previously-flakey tiled layer test that should not be flakey after this change.
    
    * platform/mac/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -1114,6 +1114,8 @@ String RenderLayerCompositor::layerTreeAsText(LayerTreeFlags flags)
     if (!m_rootContentLayer)
         return String();
 
+    flushPendingLayerChanges(true);
+
     LayerTreeAsTextBehavior layerTreeBehavior = LayerTreeAsTextBehaviorNormal;
     if (flags & LayerTreeFlagsIncludeDebugInfo)
         layerTreeBehavior |= LayerTreeAsTextDebug;
