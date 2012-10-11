172bd68883a909f277db847d198000b2f852a1ae
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-11 07:01:21 +0000

    Revert 161304 - WebSocket test server migration on ExtensionApiTest.WebSocket
    WebSocket test server migration from content::TestWebSocketServer to
    net::TestServer.
    
    This is the first change to use net::TestServer as a WebSocket test server.
    Other changes will follow to obsoelte content::TestWebSocketServer.
    
    BUG=137639
    TEST=browser_test --gtest_filter='ExtensionApiTest.WebSocket'
    
    
    Review URL: https://chromiumcodereview.appspot.com/11087027
    
    TBR=toyoshim@chromium.org
    Review URL: https://codereview.chromium.org/11091059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161308 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/websocket/test.js
+++ chrome/test/data/extensions/api_test/websocket/test.js
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 function echoTest(port) {
-  var url = "ws://localhost:" + port + "/echo";
+  var url =
+      "ws://localhost:" + port + "/websocket/tests/hybi/workers/resources/echo";
   var ws = new WebSocket(url);
   var MESSAGE_A = "message a";
   var MESSAGE_B = "message b";
@@ -34,11 +35,8 @@ function echoTest(port) {
   };
 }
 
-chrome.test.getConfig(function(config) {
-  chrome.test.runTests([
-    function runEchoTest() {
-      echoTest(config.testWebSocketPort);
-    }
-  ]);
-});
-
+chrome.test.runTests([
+  chrome.test.getConfig(function(config) {
+    echoTest(config.testWebSocketPort);
+  })
+]);
