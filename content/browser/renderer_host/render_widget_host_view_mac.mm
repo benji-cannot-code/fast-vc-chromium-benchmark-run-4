cf5b8308ae6ed53e6fddf355608b26a7b3067810
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 00:20:56 +0000

    touch: Cleanup touch-event mode switching on windows.
    On windows, chrome used to switch between raw-touch and gesture modes depending on
    whether the touch-start event was processed by the renderer or not. But now, the
    switch happens iff touch-events is enabled (using command-line flag) on appropriate
    version of windows. So cleanup the relevant code.
    
    BUG=138153
    TBR=avi@chromium.org (content/port/)
    
    Review URL: https://codereview.chromium.org/11028144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161458 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -1319,8 +1319,7 @@ void RenderWidgetHostViewMac::ForceTextureReload() {
   plugin_container_manager_.ForceTextureReload();
 }
 
-void RenderWidgetHostViewMac::ProcessTouchAck(
-    WebKit::WebInputEvent::Type type, bool processed) {
+void RenderWidgetHostViewMac::ProcessTouchAck(bool processed) {
 }
 
 void RenderWidgetHostViewMac::SetHasHorizontalScrollbar(
