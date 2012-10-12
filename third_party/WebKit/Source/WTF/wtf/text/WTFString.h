a7f8eeece3f2487568cfd1c1c57811775b04498c
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-12 21:25:57 +0000

    Move QDataStream functions into HistoryItemQt.cpp https://bugs.webkit.org/show_bug.cgi?id=99203
    Reviewed by Andreas Kling.
    
    It seems like the QDataStream stream operators are only used from HistoryItemQt.cpp
    inside WebCore, so move them there. If in the future they are required elsewhere, they should
    be moved into a separate header instead of polluting headers unnecessarily.
    
    Source/WebCore:
    
    * history/qt/HistoryItemQt.cpp:
    (operator<<):
    (operator>>):
    * platform/FractionalLayoutUnit.h:
    * platform/graphics/IntPoint.h:
    
    Source/WTF:
    
    * wtf/Vector.h:
    * wtf/qt/StringQt.cpp:
    * wtf/text/WTFString.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/text/WTFString.h
+++ third_party/WebKit/Source/WTF/wtf/text/WTFString.h
@@ -41,7 +41,6 @@ typedef const struct __CFString * CFStringRef;
 QT_BEGIN_NAMESPACE
 class QString;
 QT_END_NAMESPACE
-#include <QDataStream>
 #endif
 
 #if PLATFORM(WX)
@@ -465,11 +464,6 @@ private:
 
     RefPtr<StringImpl> m_impl;
 };
-
-#if PLATFORM(QT)
-QDataStream& operator<<(QDataStream& stream, const String& str);
-QDataStream& operator>>(QDataStream& stream, String& str);
-#endif
 
 inline bool operator==(const String& a, const String& b) { return equal(a.impl(), b.impl()); }
 inline bool operator==(const String& a, const LChar* b) { return equal(a.impl(), b); }
