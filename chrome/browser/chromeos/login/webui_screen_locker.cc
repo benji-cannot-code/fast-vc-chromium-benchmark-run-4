157ef40aef84f2e236fe1888828b3ef7574d2143
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 02:00:16 +0000

    chromeos: Add VLOGs to screen-locking code
    This cleans up some of the existing logging in the
    screen-locking code and converts DVLOGs to VLOGs.
    
    BUG=155403
    
    
    Review URL: https://chromiumcodereview.appspot.com/11110002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161475 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/webui_screen_locker.cc
+++ chrome/browser/chromeos/login/webui_screen_locker.cc
@@ -135,6 +135,8 @@ void WebUIScreenLocker::Observe(
       break;
     }
     case chrome::NOTIFICATION_LOCK_WEBUI_READY: {
+      VLOG(1) << "WebUI ready; lock window is "
+              << (lock_ready_ ? "too" : "not");
       webui_ready_ = true;
       if (lock_ready_)
         ScreenLockReady();
@@ -203,6 +205,7 @@ void WebUIScreenLocker::OnStartDeviceReset() {
 // LockWindow::Observer implementation:
 
 void WebUIScreenLocker::OnLockWindowReady() {
+  VLOG(1) << "Lock window ready; WebUI is " << (webui_ready_ ? "too" : "not");
   lock_ready_ = true;
   if (webui_ready_)
     ScreenLockReady();
