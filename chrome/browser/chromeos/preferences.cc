c45fda150199ec21368ea3b5b19823f11ff76b78
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 00:18:54 +0000

    Use a command line flag instead of board to force natural scroll on.
    BUG=152795
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11099074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161456 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -38,17 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "unicode/timezone.h"
 
 namespace chromeos {
-namespace {
-
-// TODO(achuith): Get rid of this.
-bool IsLink() {
-  std::string board;
-  system::StatisticsProvider::GetInstance()->GetMachineStatistic(
-      "CHROMEOS_RELEASE_BOARD", &board);
-  return StartsWithASCII(board, "link", false);
-}
-
-}  // namespace
 
 static const char kFallbackInputMethodLocale[] = "en-US";
 
@@ -425,8 +414,10 @@ void Preferences::NotifyPrefChanged(const std::string* pref_name) {
       UMA_HISTOGRAM_BOOLEAN("Touchpad.ThreeFingerClick.Started", enabled);
   }
   if (!pref_name || *pref_name == prefs::kNaturalScroll) {
-    // If user is on link, force natural scroll to on.
-    if (IsLink() &&
+    // Force natural scroll to on if kNaturalScrollDefault is specified on the
+    // cmd line.
+    if (CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kNaturalScrollDefault) &&
         !pref_name &&
         !prefs_->GetUserPrefValue(prefs::kNaturalScroll)) {
       natural_scroll_.SetValue(true);
