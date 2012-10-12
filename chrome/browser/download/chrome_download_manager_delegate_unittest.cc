4ebf2135568d3b22bc55a87752ffed706d746cb6
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 23:40:05 +0000

    Don't use web intents for rss feeds.
    R=jhawkins@chromium.org
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11048039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161675 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/chrome_download_manager_delegate_unittest.cc
+++ chrome/browser/download/chrome_download_manager_delegate_unittest.cc
@@ -912,6 +912,7 @@ TEST_F(ChromeDownloadManagerDelegateTest, StartDownload_LastSavePath) {
   RunTestCases(kLastSavePathTestCases, 1);
 }
 
+#if defined(OS_CHROMEOS)
 TEST_F(ChromeDownloadManagerDelegateTest, StartDownload_WebIntents) {
   const DownloadTestCase kWebIntentsTestCases[] = {
     {
@@ -920,7 +921,7 @@ TEST_F(ChromeDownloadManagerDelegateTest, StartDownload_WebIntents) {
       // .webintents extension).
       AUTOMATIC,
       content::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS,
-      "http://example.com/feed.exe", "application/rss+xml",
+      "http://example.com/feed.exe", "application/msword",
       FILE_PATH_LITERAL(""),
 
       FILE_PATH_LITERAL("feed.exe.webintents"),
@@ -934,7 +935,7 @@ TEST_F(ChromeDownloadManagerDelegateTest, StartDownload_WebIntents) {
       // 2: A download with a forced path won't be handled by web intents.
       FORCED,
       content::DOWNLOAD_DANGER_TYPE_NOT_DANGEROUS,
-      "http://example.com/feed.exe", "application/rss+xml",
+      "http://example.com/feed.exe", "application/msword",
       FILE_PATH_LITERAL("forced.feed.exe"),
 
       FILE_PATH_LITERAL("forced.feed.exe"),
@@ -947,6 +948,7 @@ TEST_F(ChromeDownloadManagerDelegateTest, StartDownload_WebIntents) {
 
   RunTestCases(kWebIntentsTestCases, arraysize(kWebIntentsTestCases));
 }
+#endif
 
 // TODO(asanka): Add more tests.
 // * Default download path is not writable.
