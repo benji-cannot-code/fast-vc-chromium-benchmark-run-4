ddaeab85cfbe1deaf997881303966ef4e8a7220e
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-12 09:38:43 +0000

    Web Inspector: Extract domain specific editing handling logic from UISourceCode descendants (step 1). https://bugs.webkit.org/show_bug.cgi?id=98912
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    This is the first step that introduces StyleFile and ScriptFile and extracts domain specific editing handling logic.
    StyleFile and ScriptFile should listen for the UISourceCode events and process them, this will be done in the next patch.
    
    * inspector/front-end/BreakpointManager.js:
    (WebInspector.BreakpointManager.Breakpoint.prototype._updateBreakpoint):
    * inspector/front-end/JavaScriptSource.js:
    * inspector/front-end/JavaScriptSourceFrame.js:
    (WebInspector.JavaScriptSourceFrame):
    (WebInspector.JavaScriptSourceFrame.prototype.onTextChanged):
    * inspector/front-end/ResourceScriptMapping.js:
    (WebInspector.ResourceScriptMapping.prototype.rawLocationToUILocation):
    (WebInspector.ResourceScriptMapping.prototype._hasDivergedFromVMChanged):
    (WebInspector.ResourceScriptMapping.prototype._bindUISourceCodeToScripts):
    (WebInspector.ScriptFile):
    (WebInspector.ScriptFile.prototype.hasDivergedFromVM):
    (WebInspector.ScriptFile.prototype.isDivergingFromVM):
    (WebInspector.ScriptFile.prototype.addEventListener):
    (WebInspector.ScriptFile.prototype.removeEventListener):
    (WebInspector.ResourceScriptFile):
    (WebInspector.ResourceScriptFile.prototype.workingCopyCommitted):
    (WebInspector.ResourceScriptFile.prototype.workingCopyChanged):
    (WebInspector.ResourceScriptFile.prototype.fireHasDivergedFromVMChanged):
    (WebInspector.ResourceScriptFile.prototype.hasDivergedFromVM):
    (WebInspector.ResourceScriptFile.prototype.isDivergingFromVM):
    * inspector/front-end/ScriptSnippetModel.js:
    (WebInspector.ScriptSnippetModel.prototype._addScriptSnippet):
    (WebInspector.SnippetScriptFile):
    (WebInspector.SnippetScriptFile.prototype.hasDivergedFromVM):
    (WebInspector.SnippetScriptFile.prototype.setHasDivergedFromVM):
    (WebInspector.SnippetScriptFile.prototype.isDivergingFromVM):
    (WebInspector.SnippetScriptFile.prototype.setIsDivergingFromVM):
    (WebInspector.SnippetScriptFile.prototype.workingCopyCommitted):
    (WebInspector.SnippetScriptFile.prototype.workingCopyChanged):
    * inspector/front-end/ScriptsPanel.js:
    (WebInspector.ScriptsPanel.prototype._revealExecutionLine):
    * inspector/front-end/StyleSource.js:
    * inspector/front-end/StylesSourceMapping.js:
    (WebInspector.StylesSourceMapping.prototype._uiSourceCodeAddedToWorkspace):
    (WebInspector.StylesSourceMapping.prototype._addUISourceCode):
    (WebInspector.StyleFile):
    (WebInspector.StyleFile.prototype.workingCopyCommitted):
    (WebInspector.StyleFile.prototype.workingCopyChanged):
    (WebInspector.StyleFile.prototype._callOrSetTimeout):
    (WebInspector.StyleFile.prototype._commitIncrementalEdit):
    (WebInspector.StyleFile.prototype._clearIncrementalUpdateTimer):
    * inspector/front-end/UISourceCode.js:
    (WebInspector.UISourceCode.prototype.scriptFile):
    (WebInspector.UISourceCode.prototype.setScriptFile):
    (WebInspector.UISourceCode.prototype.styleFile):
    (WebInspector.UISourceCode.prototype.setStyleFile):
    (WebInspector.UISourceCode.prototype.setWorkingCopy):
    (WebInspector.UISourceCode.prototype.commitWorkingCopy):
    
    LayoutTests:
    
    * inspector/debugger/live-edit.html:
    * inspector/extensions/extensions-resources.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131165 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/UISourceCode.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/UISourceCode.js
@@ -119,6 +119,38 @@ WebInspector.UISourceCode.prototype = {
     },
 
     /**
+     * @return {WebInspector.ScriptFile}
+     */
+    scriptFile: function()
+    {
+        return this._scriptFile;
+    },
+
+    /**
+     * @param {WebInspector.ScriptFile} scriptFile
+     */
+    setScriptFile: function(scriptFile)
+    {
+        this._scriptFile = scriptFile;
+    },
+
+    /**
+     * @return {WebInspector.StyleFile}
+     */
+    styleFile: function()
+    {
+        return this._styleFile;
+    },
+
+    /**
+     * @param {WebInspector.StyleFile} styleFile
+     */
+    setStyleFile: function(styleFile)
+    {
+        this._styleFile = styleFile;
+    },
+
+    /**
      * @param {function(?string,boolean,string)} callback
      */
     requestContent: function(callback)
@@ -279,13 +311,11 @@ WebInspector.UISourceCode.prototype = {
             delete this._workingCopy;
         else
             this._workingCopy = newWorkingCopy;
-        this.workingCopyChanged();
+        if (this.scriptFile())
+            this.scriptFile().workingCopyChanged();
+        else if (this.styleFile())
+            this.styleFile().workingCopyChanged();
         this.dispatchEventToListeners(WebInspector.UISourceCode.Events.WorkingCopyChanged, {oldWorkingCopy: oldWorkingCopy, workingCopy: newWorkingCopy});
-    },
-
-    workingCopyChanged: function()
-    {  
-        // Overridden.
     },
 
     /**
@@ -299,16 +329,11 @@ WebInspector.UISourceCode.prototype = {
         }
 
         var newContent = this._workingCopy;
-        this.workingCopyCommitted(callback);
+        if (this.scriptFile())
+            this.scriptFile().workingCopyCommitted(callback);
+        else if (this.styleFile())
+            this.styleFile().workingCopyCommitted(callback);
         this.addRevision(newContent);
-    },
-
-    /**
-     * @param {function(?string)} callback
-     */
-    workingCopyCommitted: function(callback)
-    {  
-        // Overridden.
     },
 
     /**
