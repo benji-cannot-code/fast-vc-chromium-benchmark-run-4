575404f2e721b5d07c4554aec4d5a593a7dcf456
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 01:12:19 +0000

    Take out ResourceBundle initialization from ContentTestSuite. This isn't needed right now, and it's causing issues on ChromeOS heapcheck bot. Review URL: https://codereview.chromium.org/11092089
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161467 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/content_test_suite.cc
+++ content/test/content_test_suite.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_content_client_initializer.h"
 #include "content/test/test_content_client.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "ui/base/resource/resource_bundle.h"
 
 #if defined(USE_AURA)
 #include "ui/aura/test/test_aura_initializer.h"
@@ -61,10 +60,6 @@ void ContentTestSuite::Initialize() {
 #endif
 
   ContentTestSuiteBase::Initialize();
-
-  // Force unittests to run using en-US so if we test against string
-  // output, it'll pass regardless of the system language.
-  ResourceBundle::InitSharedInstanceWithLocale("en-US", NULL);
 
   testing::TestEventListeners& listeners =
       testing::UnitTest::GetInstance()->listeners();
