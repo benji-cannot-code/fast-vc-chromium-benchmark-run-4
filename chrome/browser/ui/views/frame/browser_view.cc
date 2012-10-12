f6c8433a5830c655aa9c85285132eac67f4dc18a
cem.kocagil@gmail.com <cem.kocagil@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 19:12:05 +0000

    Override WebContentsFocused in DevToolsWindow
    BUG=142730
    TEST=Open a page, click somewhere in the page, hit F12 repeatedly. Devtools window should correctly open/close every time you hit F12.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10993063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -1219,7 +1219,10 @@ int BrowserView::GetExtraRenderViewHeight() const {
 }
 
 void BrowserView::WebContentsFocused(WebContents* contents) {
-  contents_container_->OnWebContentsFocused(contents);
+  if (contents_container_->GetWebContents() == contents)
+    contents_container_->OnWebContentsFocused(contents);
+  else
+    devtools_container_->OnWebContentsFocused(contents);
 }
 
 void BrowserView::ShowPageInfo(content::WebContents* web_contents,
