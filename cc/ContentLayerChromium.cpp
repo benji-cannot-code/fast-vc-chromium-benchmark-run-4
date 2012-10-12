f863b0f8d002bc58f0cd84ec0b166325c0a87148
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 01:44:13 +0000

    cc: Remove currentTime() use from ContentLayerChromium.
    Use base/time.h API instead.
    
    BUG=154451
    TEST=cc_unittests
    R=enne@chromium.org,jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11098061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161473 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/ContentLayerChromium.cpp
+++ cc/ContentLayerChromium.cpp
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ContentLayerChromium.h"
 
+#include "base/time.h"
 #include "BitmapCanvasLayerTextureUpdater.h"
 #include "BitmapSkPictureCanvasLayerTextureUpdater.h"
 #include "CCLayerTreeHost.h"
@@ -17,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FrameBufferSkPictureCanvasLayerTextureUpdater.h"
 #include "LayerPainterChromium.h"
 #include <public/Platform.h>
-#include <wtf/CurrentTime.h>
 
 namespace cc {
 
@@ -33,11 +33,11 @@ PassOwnPtr<ContentLayerPainter> ContentLayerPainter::create(ContentLayerChromium
 
 void ContentLayerPainter::paint(SkCanvas* canvas, const IntRect& contentRect, FloatRect& opaque)
 {
-    double paintStart = currentTime();
+    base::TimeTicks paintStart = base::TimeTicks::HighResNow();
     m_client->paintContents(canvas, contentRect, opaque);
-    double paintEnd = currentTime();
-    double pixelsPerSec = (contentRect.width() * contentRect.height()) / (paintEnd - paintStart);
-    WebKit::Platform::current()->histogramCustomCounts("Renderer4.AccelContentPaintDurationMS", (paintEnd - paintStart) * 1000, 0, 120, 30);
+    base::TimeTicks paintEnd = base::TimeTicks::HighResNow();
+    double pixelsPerSec = (contentRect.width() * contentRect.height()) / (paintEnd - paintStart).InSecondsF();
+    WebKit::Platform::current()->histogramCustomCounts("Renderer4.AccelContentPaintDurationMS", (paintEnd - paintStart).InMillisecondsF(), 0, 120, 30);
     WebKit::Platform::current()->histogramCustomCounts("Renderer4.AccelContentPaintMegapixPerSecond", pixelsPerSec / 1000000, 10, 210, 30);
 }
 
