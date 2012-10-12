f0df4c9920156c5bcd739f01aa5fae68d4b1c9a2
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 00:08:16 +0000

    Enable CursorManager::LockCursor to lock cursor visibility.
    This CL changes mainly three parts:
    - Add LockCursor and UnlockCursor to CursorClient.
    - Move the code to change cursor visibility from aura::RootWindow and 
       aura::RootWindowHostLinux to ash::CursorManager.
    - Remove CursorDelegate, which was too much simple delegation and unnecessary.
    
    BUG=153702
    TEST=CursorManagerTest.ShowCursor
    
    
    Review URL: https://chromiumcodereview.appspot.com/11035050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161454 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -212,8 +212,6 @@ Shell::~Shell() {
   if (active_root_window_)
     active_root_window_->GetFocusManager()->SetFocusedWindow(NULL, NULL);
 
-  cursor_manager_.set_delegate(NULL);
-
   // Please keep in same order as in Init() because it's easy to miss one.
   RemoveEnvEventFilter(user_activity_detector_.get());
   RemoveEnvEventFilter(event_rewriter_filter_.get());
@@ -370,8 +368,6 @@ void Shell::Init() {
 
   env_filter_.reset(new aura::shared::CompoundEventFilter);
   AddEnvEventFilter(env_filter_.get());
-
-  cursor_manager_.set_delegate(this);
 
   focus_manager_.reset(new aura::FocusManager);
   activation_controller_.reset(
@@ -801,20 +797,6 @@ void Shell::InitLayoutManagersForPrimaryDisplay(
       new internal::PanelWindowEventFilter(
           panel_container, panel_layout_manager_));
   panel_container->SetLayoutManager(panel_layout_manager_);
-}
-
-void Shell::SetCursor(gfx::NativeCursor cursor) {
-  RootWindowList root_windows = GetAllRootWindows();
-  for (RootWindowList::iterator iter = root_windows.begin();
-       iter != root_windows.end(); ++iter)
-    (*iter)->SetCursor(cursor);
-}
-
-void Shell::ShowCursor(bool visible) {
-  RootWindowList root_windows = GetAllRootWindows();
-  for (RootWindowList::iterator iter = root_windows.begin();
-       iter != root_windows.end(); ++iter)
-    (*iter)->ShowCursor(visible);
 }
 
 bool Shell::CanWindowReceiveEvents(aura::Window* window) {
