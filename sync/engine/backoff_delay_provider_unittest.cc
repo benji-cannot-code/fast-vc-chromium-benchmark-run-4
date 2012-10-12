25a54e34092cac35797c9f8045c92db266983036
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 02:31:58 +0000

    sync: return short initial backoff delay for NETWORK_CONNECTION_UNAVAILABLE
    BUG=chromium-os:35073
    
    
    Review URL: https://chromiumcodereview.appspot.com/11098084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161481 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/backoff_delay_provider_unittest.cc
+++ sync/engine/backoff_delay_provider_unittest.cc
@@ -46,6 +46,10 @@ TEST_F(BackoffDelayProviderTest, GetInitialDelay) {
   EXPECT_EQ(kInitialBackoffShortRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 
+  state.last_download_updates_result = NETWORK_CONNECTION_UNAVAILABLE;
+  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+            delay->GetInitialDelay(state).InSeconds());
+
   state.last_download_updates_result = SERVER_RETURN_TRANSIENT_ERROR;
   EXPECT_EQ(kInitialBackoffRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
@@ -63,6 +67,10 @@ TEST_F(BackoffDelayProviderTest, GetInitialDelay) {
             delay->GetInitialDelay(state).InSeconds());
 
   state.commit_result = SERVER_RETURN_MIGRATION_DONE;
+  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+            delay->GetInitialDelay(state).InSeconds());
+
+  state.commit_result = NETWORK_CONNECTION_UNAVAILABLE;
   EXPECT_EQ(kInitialBackoffShortRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 }
