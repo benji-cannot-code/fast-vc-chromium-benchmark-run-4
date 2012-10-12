90c67d7a1af9cac5ad18c896f503def5b0a81ec7
morrita@chromium.org <morrita@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 07:38:14 +0000

    Turned on Shadow DOM and style scoped flags by default.
    BUG=145868
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/10917002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161531 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -703,8 +703,7 @@ void RenderThreadImpl::EnsureWebKitInitialized() {
 
   WebRuntimeFeatures::enableQuota(true);
 
-  WebRuntimeFeatures::enableShadowDOM(
-      command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures));
+  WebRuntimeFeatures::enableShadowDOM(true);
 
   WebRuntimeFeatures::enableStyleScoped(
       command_line.HasSwitch(switches::kEnableExperimentalWebKitFeatures));
