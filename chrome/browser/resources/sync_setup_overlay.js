1927be5d36d573767efa2dc56ba8640c1ec6e70d
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 04:01:51 +0000

    Remove spinner icon in sync setup wait dialog, per request from UI leads.
    BUG=148704
    TEST=Remove sync data from https://www.google.com/settings/chrome/sync then disconnect any network and try "Set up Sync" from chrome://settings
    
    
    Review URL: https://chromiumcodereview.appspot.com/10966028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161497 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -418,7 +418,6 @@ cr.define('options', function() {
     showSpinner_: function() {
       this.resetPage_('sync-setup-spinner');
       $('sync-setup-spinner').hidden = false;
-      this.setThrobbersVisible_(true);
     },
 
     showTimeoutPage_: function() {
