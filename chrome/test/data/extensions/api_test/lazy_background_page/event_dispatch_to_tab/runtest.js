1d6d4ec9b8dac1d9a6c9d0ce1f3b48e5b9936ad0
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 15:56:50 +0000

    Add a test for lazy extension event dispatch.
    Test that events will be delivered to both a regular page and an event
    page even when the event page is not loaded.
    
    TEST=this
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11066117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161583 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/lazy_background_page/event_dispatch_to_tab/runtest.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var called = false;
+function testReceivedEvent() {
+  chrome.test.assertTrue(called);
+
+  chrome.runtime.getBackgroundPage(
+      function(background_page) {
+        chrome.test.assertTrue(background_page.called);
+        chrome.test.succeed();
+      });
+}
+
+chrome.bookmarks.onCreated.addListener(
+    function() {
+      called = true;
+    });
+
+chrome.test.sendMessage('ready',
+    function(message) {
+      chrome.test.runTests([testReceivedEvent]);
+    });
