a865a49bdc08e7da382bf4c77345bed30db1d014
torne@chromium.org <torne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 13:01:44 +0000

    Android: Make webview builds more convenient.
    Allow the top level Android.mk to include GypAndroid.mk if the relevant
    environment variable is set, to avoid having to edit this file to build
    in the Android tree without breaking other Android builds. Also, provide
    a gyp wrapper for webview which passes the correct options.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/11090069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161566 0039d316-1c4b-4281-b951-d872f2087c98

--- Android.mk
+++ Android.mk
@@ -3,5 +3,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-# This is an empty Android makefile to prevent the Android build system from
-# erroneously picking up other Android.mk files in the Chromium tree.
+# This Android makefile is used to build WebView in the Android build system.
+# gyp autogenerates the real makefiles, which we just include here if we are
+# doing a WebView build. For other builds, this makefile does nothing, which
+# prevents the Android build system from mistakenly loading any other
+# Android.mk that may exist in the Chromium tree.
+
+ifdef CHROME_ANDROID_BUILD_WEBVIEW
+include $(call my-dir)/GypAndroid.mk
+endif
