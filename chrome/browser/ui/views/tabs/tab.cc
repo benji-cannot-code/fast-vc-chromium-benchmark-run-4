7dde116bf81e93b54632d319eca34de0ed863dfa
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-12 18:41:50 +0000

    Couple of vertical positioning tweaks for tabs and the location bar when touch is enabled.
    BUG=150873
    TEST=Chrome no longer looks wonky when the touch UI is enabled.
    
    Review URL: https://codereview.chromium.org/11036046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161619 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab.cc
+++ chrome/browser/ui/views/tabs/tab.cc
@@ -65,7 +65,7 @@ int top_padding() {
         value = 7;
         break;
       case ui::LAYOUT_TOUCH:
-        value = 12;
+        value = 10;
         break;
       default:
         NOTREACHED();
