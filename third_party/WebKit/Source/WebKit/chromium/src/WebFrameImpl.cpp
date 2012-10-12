46c5c5e37e60aa62e3aa20958c290761ada8e10d
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-12 21:01:31 +0000

    [Chromium] Remove used frameForEnteredContext API https://bugs.webkit.org/show_bug.cgi?id=99201
    Reviewed by Eric Seidel.
    
    No one in Chromium-land understands the difference between
    frameForEnteredContext and frameForCurrentContext. Rather than give
    folks a confusing choice, let's just expose the one we most often want
    to use.
    
    I've already removed all callers of frameForEnteredContext downstream.
    
    * public/WebFrame.h:
    * src/WebFrameImpl.cpp:
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131223 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -556,14 +556,6 @@ int WebFrame::instanceCount()
     return frameCount;
 }
 
-WebFrame* WebFrame::frameForEnteredContext()
-{
-    v8::Handle<v8::Context> context = v8::Context::GetEntered();
-    if (context.IsEmpty())
-        return 0;
-    return frameForContext(context);
-}
-
 WebFrame* WebFrame::frameForCurrentContext()
 {
     v8::Handle<v8::Context> context = v8::Context::GetCurrent();
