70a74b8adba8a3303c2f7e600835670e00543360
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-13 10:24:44 +0000

    Include sstream instead of iostream to avoid the additional IO static initializer.
    BUG=155472
    TEST=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11114010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161756 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/system_info_cpu/cpu_info_provider_linux.cc
+++ chrome/browser/extensions/api/system_info_cpu/cpu_info_provider_linux.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/system_info_cpu/cpu_info_provider.h"
 
 #include <cstdio>
-#include <iostream>
+#include <sstream>
 
 #include "base/file_util.h"
 #include "base/format_macros.h"
