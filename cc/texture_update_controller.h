39792d1f86b5862360d3774f5ea16087615cf821
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-13 17:14:59 +0000

    cc: Add missing include to scoped_ptr.
    This should fix the buildbot breakage.
    
    BUG=154451
    TBR=enne@chromium.org
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/11149007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161767 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/texture_update_controller.h
+++ cc/texture_update_controller.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CCTextureUpdateController_h
 
 #include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/time.h"
 #include "CCTextureUpdateQueue.h"
 #include "CCTimer.h"
