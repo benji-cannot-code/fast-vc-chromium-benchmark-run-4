dee00618b29ae3c328079d7e04a26fc75bc7fe7a
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-13 21:15:15 +0000

    Fix the build after r161775 part 2.
    TBR=satorux
    Review URL: https://codereview.chromium.org/11149008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161778 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/system_monitor/media_transfer_protocol_device_observer_linux.cc
+++ chrome/browser/system_monitor/media_transfer_protocol_device_observer_linux.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_split.h"
 #include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
-#include "chrome/browser/media_gallery/mtp_storage_info.pb.h"
+#include "chrome/browser/media_transfer_protocol/mtp_storage_info.pb.h"
 #include "chrome/browser/system_monitor/media_storage_util.h"
 #include "chrome/browser/system_monitor/removable_device_constants.h"
 
