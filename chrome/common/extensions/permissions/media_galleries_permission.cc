605bece67d4f5031fc65a69ad6249cb5cfc00f7e
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-14 16:17:35 +0000

    Clean up permission names.
    This gets rid of the fileSystem: read permission (which didn't serve any purpose), and
    change the mediaGalleries all-auto-detected permission to be camelCased like all other
    permissions.
    
    BUG=155427
    
    
    Review URL: https://chromiumcodereview.appspot.com/11106017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161808 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/permissions/media_galleries_permission.cc
+++ chrome/common/extensions/permissions/media_galleries_permission.cc
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kAllAutoDetectedString[] = "all-auto-detected";
+const char kAllAutoDetectedString[] = "allAutoDetected";
 const char kReadString[] = "read";
 const char kInvalidString[] = "invalid";
 
