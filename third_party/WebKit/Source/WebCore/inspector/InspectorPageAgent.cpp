71654639dbd9bef3a4c82a40bbfbecafc25d1547
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-14 14:19:52 +0000

    InspectorPageAgent should expose a method to control the compositing debug settings. https://bugs.webkit.org/show_bug.cgi?id=99253
    Patch by Antoine Quint <graouts@apple.com> on 2012-10-14
    Reviewed by Timothy Hatcher.
    
    * inspector/Inspector.json:
    * inspector/InspectorPageAgent.cpp:
    (WebCore::InspectorPageAgent::setCompositingBordersVisible):
    * inspector/InspectorPageAgent.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131265 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorPageAgent.cpp
@@ -1109,6 +1109,16 @@ void InspectorPageAgent::setTouchEmulationEnabled(ErrorString* error, bool enabl
 #endif
 }
 
+void InspectorPageAgent::setCompositingBordersVisible(ErrorString*, bool visible)
+{
+    Settings* settings = m_page->settings();
+    if (!settings)
+        return;
+
+    settings->setShowDebugBorders(visible);
+    settings->setShowRepaintCounter(visible);
+}
+
 } // namespace WebCore
 
 #endif // ENABLE(INSPECTOR)
