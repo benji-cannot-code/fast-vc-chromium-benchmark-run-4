e4c6d66f01f4649cdf98e8abde104e8f3740db45
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-14 16:22:49 +0000

    Build fix for WinCE after r131089.
    WinCE does not support getenv().
    
    * runtime/Options.cpp:
    (JSC::overrideOptionWithHeuristic):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131267 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Options.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Options.cpp
@@ -76,6 +76,7 @@ static bool parse(const char* string, double& value)
 template<typename T>
 void overrideOptionWithHeuristic(T& variable, const char* name)
 {
+#if !OS(WINCE)
     const char* stringValue = getenv(name);
     if (!stringValue)
         return;
@@ -84,6 +85,7 @@ void overrideOptionWithHeuristic(T& variable, const char* name)
         return;
     
     fprintf(stderr, "WARNING: failed to parse %s=%s\n", name, stringValue);
+#endif
 }
 
 static unsigned computeNumberOfGCMarkers(int maxNumberOfGCMarkers)
