bfac423dd99f4e1b294f62b2e962462f96ecbc6c
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 20:37:21 +0000

    Parse regexes and return errors on invalid ones when creating URLMatcherConditions.
    BUG=135609
    
    
    Review URL: https://chromiumcodereview.appspot.com/10911273
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/matcher/url_matcher_factory_unittest.cc
+++ chrome/common/extensions/matcher/url_matcher_factory_unittest.cc
@@ -14,7 +14,7 @@ namespace extensions {
 
 namespace keys = url_matcher_constants;
 
-TEST(URLMatcherFactory, CreateFromURLFilterDictionary) {
+TEST(URLMatcherFactoryTest, CreateFromURLFilterDictionary) {
   URLMatcher matcher;
 
   std::string error;
@@ -27,6 +27,10 @@ TEST(URLMatcherFactory, CreateFromURLFilterDictionary) {
   // Invalid value type: {"hostSuffix": []}
   DictionaryValue invalid_condition2;
   invalid_condition2.Set(keys::kHostSuffixKey, new ListValue);
+
+  // Invalid regex value: {"urlMatches": "*"}
+  DictionaryValue invalid_condition3;
+  invalid_condition3.SetString(keys::kURLMatchesKey, "*");
 
   // Valid values:
   // {
@@ -67,10 +71,17 @@ TEST(URLMatcherFactory, CreateFromURLFilterDictionary) {
   EXPECT_FALSE(error.empty());
   EXPECT_FALSE(result.get());
 
+  // Test invalid regex in urlMatches.
+  error.clear();
+  result = URLMatcherFactory::CreateFromURLFilterDictionary(
+      matcher.condition_factory(), &invalid_condition3, 3, &error);
+  EXPECT_FALSE(error.empty());
+  EXPECT_FALSE(result.get());
+
   // Test success.
   error.clear();
   result = URLMatcherFactory::CreateFromURLFilterDictionary(
-      matcher.condition_factory(), &valid_condition, 3, &error);
+      matcher.condition_factory(), &valid_condition, 100, &error);
   EXPECT_EQ("", error);
   ASSERT_TRUE(result.get());
 
