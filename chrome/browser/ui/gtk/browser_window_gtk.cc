c324e451f9912745b14a657ca86c1bfb8b87af6a
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 20:05:59 +0000

    DevTools: [Linux] "dock to right" is broken after turning a tab into a window.
    BUG=122772
    
    
    Review URL: https://chromiumcodereview.appspot.com/11147020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161932 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/browser_window_gtk.cc
+++ chrome/browser/ui/gtk/browser_window_gtk.cc
@@ -230,6 +230,7 @@ GQuark GetBrowserWindowQuarkKey() {
 
 BrowserWindowGtk::BrowserWindowGtk(Browser* browser)
     :  window_(NULL),
+       window_has_shown_(false),
        window_container_(NULL),
        window_vbox_(NULL),
        render_area_vbox_(NULL),
@@ -604,6 +605,8 @@ void BrowserWindowGtk::Show() {
   gtk_widget_set_size_request(contents_container_->widget(), -1, -1);
 
   browser()->OnWindowDidShow();
+  window_has_shown_ = true;
+  UpdateDevTools();
 }
 
 void BrowserWindowGtk::ShowInactive() {
@@ -675,6 +678,7 @@ void BrowserWindowGtk::Close() {
   // To help catch bugs in any event handlers that might get fired during the
   // destruction, set window_ to NULL before any handlers will run.
   window_ = NULL;
+  window_has_shown_ = false;
   titlebar_->set_window(NULL);
 
   // We don't want GlobalMenuBar handling any notifications or commands after
@@ -1350,6 +1354,8 @@ void BrowserWindowGtk::MaybeShowBookmarkBar(bool animate) {
 }
 
 void BrowserWindowGtk::UpdateDevToolsForContents(WebContents* contents) {
+  if (!window_has_shown_)
+    return;
   TRACE_EVENT0("ui::gtk", "BrowserWindowGtk::UpdateDevToolsForContents");
   TabContents* old_devtools = devtools_container_->tab();
   TabContents* devtools_contents = contents ?
