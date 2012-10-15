1821fd2de22e0d07c072578dfc4b508629708a0b
mark.lam@apple.com <mark.lam@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-15 21:40:33 +0000

    Fix build broken by r131348. https://bugs.webkit.org/show_bug.cgi?id=97533.
    Not reviewed.
    
    * rendering/RenderView.cpp:
    (WebCore::RenderView::layoutContent):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131363 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderView.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderView.cpp
@@ -135,6 +135,7 @@ bool RenderView::isChildAllowed(RenderObject* child, RenderStyle*) const
 
 void RenderView::layoutContent(const LayoutState& state)
 {
+    UNUSED_PARAM(state);
     ASSERT(needsLayout());
 
     RenderBlock::layout();
