679f9791f07ac429064687f714357aca22e2a806
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 23:08:56 +0000

    cc: Switch CCLayerTreeHostTestFinishAllRendering to use base::AutoLock.
    BUG=154451
    TEST=cc_unittests
    R=enne@chromium.org,jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11137026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161990 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_unittest.cc
+++ cc/layer_tree_host_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "CCLayerTreeHost.h"
 
+#include "base/synchronization/lock.h"
 #include "CCGeometryTestUtils.h"
 #include "CCGraphicsContext.h"
 #include "CCLayerTreeHostImpl.h"
@@ -2124,7 +2125,6 @@ class CCLayerTreeHostTestFinishAllRendering : public CCLayerTreeHostTest {
 public:
     CCLayerTreeHostTestFinishAllRendering()
         : m_once(false)
-        , m_mutex()
         , m_drawCount(0)
     {
     }
@@ -2142,12 +2142,12 @@ public:
         m_layerTreeHost->setNeedsRedraw();
         m_layerTreeHost->acquireLayerTextures();
         {
-            Locker<Mutex> lock(m_mutex);
+            base::AutoLock lock(m_lock);
             m_drawCount = 0;
         }
         m_layerTreeHost->finishAllRendering();
         {
-            Locker<Mutex> lock(m_mutex);
+            base::AutoLock lock(m_lock);
             EXPECT_EQ(0, m_drawCount);
         }
         endTest();
@@ -2155,7 +2155,7 @@ public:
 
     virtual void drawLayersOnCCThread(CCLayerTreeHostImpl* impl) OVERRIDE
     {
-        Locker<Mutex> lock(m_mutex);
+        base::AutoLock lock(m_lock);
         ++m_drawCount;
     }
 
@@ -2165,7 +2165,7 @@ public:
 private:
 
     bool m_once;
-    Mutex m_mutex;
+    base::Lock m_lock;
     int m_drawCount;
 };
 
