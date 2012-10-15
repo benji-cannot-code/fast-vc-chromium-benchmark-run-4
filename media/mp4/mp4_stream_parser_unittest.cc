38f91216fa8d3641411374014176ef429ca7a698
ajwong@chromium.org <ajwong@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 20:36:05 +0000

    Fix move.h's to use a concrete RValue carrier object rather than hacking a RValue&.
    For move semantics, we need to create a private "RValue" type that is used to
    create move constructors and move operators.  Previously, we emulated Boost's
    idea of making the RValue type a subclass of the move-only type that doesn't add
    any new member fields. We then just reinterpret_cast "this" into a RValue&
    depending on the fact that RValue is just a type pun for the move-only type.
    This ends up being undefined behavior though (C++98 5.2.10.7).
    
    This change makes use a concrete RValue class that contains a pointer to the
    move-only type.  With -O2 on clang version 3.2 (trunk 163674), this yields
    identical assembly code to the previous implementation. With -O0, we generate
    2 more instructions to allocate and initialize the temporary RValue struct's
    object field when calling Pass().
    
    This should be acceptable.  The snowman says so ☃.
    
    BUG=155436
    
    Review URL: https://chromiumcodereview.appspot.com/11078014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161945 0039d316-1c4b-4281-b951-d872f2087c98

--- media/mp4/mp4_stream_parser_unittest.cc
+++ media/mp4/mp4_stream_parser_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
-#include "base/move.h"
 #include "base/time.h"
 #include "media/base/audio_decoder_config.h"
 #include "media/base/decoder_buffer.h"
