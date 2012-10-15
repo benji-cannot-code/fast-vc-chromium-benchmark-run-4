bcde6396f16e662c0decf50064a07147e3873814
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 16:09:13 +0000

    Implement FileSystemURL.DebugString()
    BUG=154766
    TEST=FileSystemURLTest.DebugString
    
    
    Review URL: https://chromiumcodereview.appspot.com/11150017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161867 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/file_system_url.cc
+++ webkit/fileapi/file_system_url.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/fileapi/file_system_url.h"
 
+#include <sstream>
+
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "net/base/escape.h"
@@ -101,11 +103,20 @@ FileSystemURL::FileSystemURL(
 
 FileSystemURL::~FileSystemURL() {}
 
-std::string FileSystemURL::spec() const {
+std::string FileSystemURL::DebugString() const {
   if (!is_valid_)
-    return std::string();
-  return GetFileSystemRootURI(origin_, type_).spec() + "/" +
-      path_.AsUTF8Unsafe();
+    return "invalid filesystem: URL";
+  std::ostringstream ss;
+  ss << GetFileSystemRootURI(origin_, mount_type_);
+  if (!virtual_path_.empty())
+    ss << virtual_path_.value();
+  if (type_ != mount_type_ || path_ != virtual_path_) {
+    ss << " (";
+    ss << GetFileSystemTypeString(type_) << "@" << filesystem_id_ << ":";
+    ss << path_.value();
+    ss << ")";
+  }
+  return ss.str();
 }
 
 FileSystemURL FileSystemURL::WithPath(const FilePath& path) const {
