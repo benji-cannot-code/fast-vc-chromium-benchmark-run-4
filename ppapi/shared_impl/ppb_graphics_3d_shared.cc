061fc98f0f2b1799256424d2034ec289ac6f45dd
nfullagar@chromium.org <nfullagar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 20:58:17 +0000

    Put locks around PPB_Graphics3d.
    This CL was inherited from dmichael's CL:
    
    PPAPI: Lock around GLES2 entry in to the proxy
    
    This might not cover all the ways 3D could go wrong in a multithreaded situation (I don't understand it well enough to know). It also probably locks a bit more than is necessary. But so far it works, and makes Var reference counting (and pretty much everything else) usable on background threads. Probably good enough for M23 experimentation.
    
    http://codereview.chromium.org/10960046/
    
    BUG=116317, 92909
    TEST=build with enable_pepper_threading=1. PPAPI 3D example works. Zombie Track Meat, Tumbler, and other 3d stuff works in the NaCl IPC proxy.
    
    Review URL: https://chromiumcodereview.appspot.com/11054024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161956 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/shared_impl/ppb_graphics_3d_shared.cc
+++ ppapi/shared_impl/ppb_graphics_3d_shared.cc
@@ -51,6 +51,7 @@ int32_t PPB_Graphics3D_Shared::ResizeBuffers(int32_t width, int32_t height) {
   if ((width < 0) || (height < 0))
     return PP_ERROR_BADARGUMENT;
 
+  ScopedNoLocking already_locked(this);
   gles2_impl()->ResizeCHROMIUM(width, height);
   // TODO(alokp): Check if resize succeeded and return appropriate error code.
   return PP_OK;
@@ -58,6 +59,7 @@ int32_t PPB_Graphics3D_Shared::ResizeBuffers(int32_t width, int32_t height) {
 
 int32_t PPB_Graphics3D_Shared::SwapBuffers(
     scoped_refptr<TrackedCallback> callback) {
+  ScopedNoLocking already_locked(this);
   if (HasPendingSwap()) {
     // Already a pending SwapBuffers that hasn't returned yet.
     return PP_ERROR_INPROGRESS;
@@ -76,11 +78,13 @@ void* PPB_Graphics3D_Shared::MapTexSubImage2DCHROMIUM(GLenum target,
                                                       GLenum format,
                                                       GLenum type,
                                                       GLenum access) {
+  ScopedNoLocking already_locked(this);
   return gles2_impl_->MapTexSubImage2DCHROMIUM(
       target, level, xoffset, yoffset, width, height, format, type, access);
 }
 
 void PPB_Graphics3D_Shared::UnmapTexSubImage2DCHROMIUM(const void* mem) {
+  ScopedNoLocking already_locked(this);
   gles2_impl_->UnmapTexSubImage2DCHROMIUM(mem);
 }
 
@@ -97,6 +101,7 @@ bool PPB_Graphics3D_Shared::CreateGLES2Impl(
     int32 command_buffer_size,
     int32 transfer_buffer_size,
     gpu::gles2::GLES2Implementation* share_gles2) {
+  ScopedNoLocking already_locked(this);
   gpu::CommandBuffer* command_buffer = GetCommandBuffer();
   DCHECK(command_buffer);
 
@@ -132,9 +137,18 @@ bool PPB_Graphics3D_Shared::CreateGLES2Impl(
 }
 
 void PPB_Graphics3D_Shared::DestroyGLES2Impl() {
+  ScopedNoLocking already_locked(this);
   gles2_impl_.reset();
   transfer_buffer_.reset();
   gles2_helper_.reset();
+}
+
+void PPB_Graphics3D_Shared::PushAlreadyLocked() {
+  // Do nothing. This should be overridden in the plugin side.
+}
+
+void PPB_Graphics3D_Shared::PopAlreadyLocked() {
+  // Do nothing. This should be overridden in the plugin side.
 }
 
 }  // namespace ppapi
