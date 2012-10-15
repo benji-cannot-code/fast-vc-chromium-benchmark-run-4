7355844d8e7e0b628989eacf55e39fb0d6b9c153
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-15 19:55:23 +0000

    Remove obsolete TODO about browser-based panels.
    They were deleted by r155930.
    
    R=jennb@chromium.org
    
    Review URL: https://codereview.chromium.org/11154015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@161930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/browser_extension_window_controller.cc
+++ chrome/browser/extensions/browser_extension_window_controller.cc
@@ -83,6 +83,5 @@ Browser* BrowserExtensionWindowController::GetBrowser() const {
 bool BrowserExtensionWindowController::IsVisibleToExtension(
     const extensions::Extension* extension) const {
   // Platform apps can only see their own windows.
-  // TODO(mihaip): what about non-Aura panels?
   return !extension->is_platform_app();
 }
