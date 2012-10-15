5d6a626fa285ff5f3c954bc8b5ab75d729009b3b
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-15 21:50:53 +0000

    Source/WebCore: WebCore part of <rdar://problem/12470680> Font’s fast code path doesn’t support kerning and ligatures https://bugs.webkit.org/show_bug.cgi?id=99113
    Reviewed by Tim Horton.
    
    * WebCore.exp.in: Exported wkCTFontTransformGlyphs.
    * platform/graphics/Font.cpp:
    (WebCore::Font::width): Added a local GlyphBuffer to pass to floatWidthForSimpleText().
    (WebCore::Font::codePath): Rather than always use the complex code path when any typesetting
    features are enabled, changed to do so only if WidthIterator doesn’t support this Font’s
    typesetting features.
    * platform/graphics/FontFastPath.cpp:
    (WebCore::Font::getGlyphsAndAdvancesForSimpleText): Added a local GlyphBuffer to pass to
    WidthIterator::advance() when advancing to or from the range of interest. Added a FIXME
    about how this is not entirely correct when kerning or ligatures are enabled.
    (WebCore::Font::selectionRectForSimpleText): Added a local GlyphBuffer to pass to
    WidthIterator::advance() when advancing to or from the range of interest.
    (WebCore::Font::offsetForPositionForSimpleText): Updated for the change to
    WidthIterator::advanceOneCharacter().
    * platform/graphics/SimpleFontData.h:
    (WebCore::SimpleFontData::applyTransforms): Added. Calls wkCTFontTransformGlyphs() where
    available.
    * platform/graphics/WidthIterator.cpp:
    (WebCore::WidthIterator::WidthIterator): Added initializer for the new m_typesettingFeatures
    data member.
    (OriginalAdvancesForCharacterTreatedAsSpace): Added this data type, used to track spaces and
    characters treated as spaces.
    (WebCore::applyFontTransforms): Added. Applies shaping and positioning transforms, as
    required by the typesetting features, to the glyphs recently added to a GlyphBuffer, while
    maintaining the advances of characters that are treated as spaces and the characters
    preceding them, if necessary.
    (WebCore::WidthIterator::advanceInternal): Added calls to applyFontTransforms() at the end
    of each contiguous run of glyphs from the same font. Also added code to maintain a vector
    of spaces and characters treated as space.
    (WebCore::WidthIterator::advanceOneCharacter): Changed the parameter type from a pointer to
    a reference.
    * platform/graphics/WidthIterator.h:
    (WebCore::WidthIterator::supportsTypesettingFeatures): Added. Returns whether WidthIterator
    instances support the typesetting features of the given font. Returns true if the font is
    not a screen font and its typesetting features are kerning, ligatures or both.
    (WebCore::WidthIterator::shouldApplyFontTransforms): Added. Returns true if the typesetting
    features include kerning or ligatures.
    * platform/mac/WebCoreSystemInterface.h: Defined wkCTFontTransformOptions and declared
    wkCTFontTransformGlyphs.
    * platform/mac/WebCoreSystemInterface.mm: Defined wkCTFontTransformGlyphs.
    * rendering/svg/SVGTextMetricsBuilder.cpp:
    (WebCore::SVGTextMetricsBuilder::advanceSimpleText): Added a local GlyphBuffer to pass to
    WidthIterator::advance().
    * rendering/svg/SVGTextRunRenderingContext.cpp:
    (WebCore::SVGTextRunRenderingContext::floatWidthUsingSVGFont): Ditto.
    
    Source/WebKit/mac: WebKit/mac part of <rdar://problem/12470680> Font’s fast code path doesn’t support kerning and ligatures
    https://bugs.webkit.org/show_bug.cgi?id=99113
    
    Reviewed by Tim Horton.
    
    * WebCoreSupport/WebSystemInterface.mm:
    (InitWebCoreSystemInterface): Added wkCTFontTransformGlyphs.
    
    Source/WebKit2: WebKit2 part of <rdar://problem/12470680> Font’s fast code path doesn’t support kerning and ligatures
    https://bugs.webkit.org/show_bug.cgi?id=99113
    
    Reviewed by Tim Horton.
    
    * WebProcess/WebCoreSupport/mac/WebSystemInterface.mm:
    (InitWebCoreSystemInterface): Added wkCTFontTransformGlyphs.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/FontFastPath.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/FontFastPath.cpp
@@ -329,7 +329,10 @@ float Font::getGlyphsAndAdvancesForSimpleText(const TextRun& run, int from, int 
     float initialAdvance;
 
     WidthIterator it(this, run, 0, false, forTextEmphasis);
-    it.advance(from);
+    // FIXME: Using separate glyph buffers for the prefix and the suffix is incorrect when kerning or
+    // ligatures are enabled.
+    GlyphBuffer localGlyphBuffer;
+    it.advance(from, &localGlyphBuffer);
     float beforeWidth = it.m_runWidthSoFar;
     it.advance(to, &glyphBuffer);
 
@@ -340,7 +343,7 @@ float Font::getGlyphsAndAdvancesForSimpleText(const TextRun& run, int from, int 
 
     if (run.rtl()) {
         float finalRoundingWidth = it.m_finalRoundingWidth;
-        it.advance(run.length());
+        it.advance(run.length(), &localGlyphBuffer);
         initialAdvance = finalRoundingWidth + it.m_runWidthSoFar - afterWidth;
     } else
         initialAdvance = beforeWidth;
@@ -484,15 +487,16 @@ float Font::floatWidthForSimpleText(const TextRun& run, GlyphBuffer* glyphBuffer
 
 FloatRect Font::selectionRectForSimpleText(const TextRun& run, const FloatPoint& point, int h, int from, int to) const
 {
+    GlyphBuffer glyphBuffer;
     WidthIterator it(this, run);
-    it.advance(from);
+    it.advance(from, &glyphBuffer);
     float beforeWidth = it.m_runWidthSoFar;
-    it.advance(to);
+    it.advance(to, &glyphBuffer);
     float afterWidth = it.m_runWidthSoFar;
 
     // Using roundf() rather than ceilf() for the right edge as a compromise to ensure correct caret positioning.
     if (run.rtl()) {
-        it.advance(run.length());
+        it.advance(run.length(), &glyphBuffer);
         float totalWidth = it.m_runWidthSoFar;
         return FloatRect(floorf(point.x() + totalWidth - afterWidth), point.y(), roundf(point.x() + totalWidth - beforeWidth) - floorf(point.x() + totalWidth - afterWidth), h);
     }
@@ -512,7 +516,7 @@ int Font::offsetForPositionForSimpleText(const TextRun& run, float x, bool inclu
         while (1) {
             offset = it.m_currentCharacter;
             float w;
-            if (!it.advanceOneCharacter(w, &localGlyphBuffer))
+            if (!it.advanceOneCharacter(w, localGlyphBuffer))
                 break;
             delta += w;
             if (includePartialGlyphs) {
@@ -527,7 +531,7 @@ int Font::offsetForPositionForSimpleText(const TextRun& run, float x, bool inclu
         while (1) {
             offset = it.m_currentCharacter;
             float w;
-            if (!it.advanceOneCharacter(w, &localGlyphBuffer))
+            if (!it.advanceOneCharacter(w, localGlyphBuffer))
                 break;
             delta -= w;
             if (includePartialGlyphs) {
--- third_party/WebKit/Source/WebCore/platform/graphics/SimpleFontData.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/SimpleFontData.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FontMetrics.h"
 #include "FontPlatformData.h"
 #include "FloatRect.h"
+#include "GlyphBuffer.h"
 #include "GlyphMetricsMap.h"
 #include "GlyphPageTreeNode.h"
 #if ENABLE(OPENTYPE_VERTICAL)
@@ -38,7 +39,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TypesettingFeatures.h"
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
+#include <wtf/UnusedParam.h>
 #include <wtf/text/StringHash.h>
+
+#if PLATFORM(MAC)
+#include "WebCoreSystemInterface.h"
+#endif
 
 #if PLATFORM(MAC) || (PLATFORM(CHROMIUM) && OS(DARWIN)) || (PLATFORM(WX) && OS(DARWIN))
 #include <wtf/RetainPtr.h>
@@ -190,6 +196,21 @@ public:
 #if PLATFORM(MAC) || (PLATFORM(CHROMIUM) && OS(DARWIN)) || (PLATFORM(WX) && OS(DARWIN)) || USE(HARFBUZZ_NG)
     bool canRenderCombiningCharacterSequence(const UChar*, size_t) const;
 #endif
+
+    bool applyTransforms(GlyphBufferGlyph* glyphs, GlyphBufferAdvance* advances, size_t glyphCount, TypesettingFeatures typesettingFeatures) const
+    {
+#if !PLATFORM(MAC) || __MAC_OS_X_VERSION_MIN_REQUIRED <= 1080
+        UNUSED_PARAM(glyphs);
+        UNUSED_PARAM(advances);
+        UNUSED_PARAM(glyphCount);
+        UNUSED_PARAM(typesettingFeatures);
+        ASSERT_NOT_REACHED();
+        return false;
+#else
+    wkCTFontTransformOptions options = (typesettingFeatures & Kerning ? wkCTFontTransformApplyPositioning : 0) | (typesettingFeatures & Ligatures ? wkCTFontTransformApplyShaping : 0);
+    return wkCTFontTransformGlyphs(m_platformData.ctFont(), glyphs, reinterpret_cast<CGSize*>(advances), glyphCount, options);
+#endif
+    }
 
 #if PLATFORM(QT)
     QRawFont getQtRawFont() const { return m_platformData.rawFont(); }
