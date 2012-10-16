13724f34583db028864bc43077f860e37bf3ce94
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 16:22:56 +0000

    [cros] OOBE: move desktop background to ScreenLock layer only after user login.
    BUG=145533
    
    
    Review URL: https://chromiumcodereview.appspot.com/11170004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user_manager_impl.cc
+++ chrome/browser/chromeos/login/user_manager_impl.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/user_manager_impl.h"
 
-#include "ash/desktop_background/desktop_background_controller.h"
 #include "ash/shell.h"
 #include "base/bind.h"
 #include "base/chromeos/chromeos_version.h"
