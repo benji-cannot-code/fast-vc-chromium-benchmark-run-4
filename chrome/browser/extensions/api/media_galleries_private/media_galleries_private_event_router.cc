0575e952bc479d733a1a4dd8bc61afa91a1a9aa8
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 02:13:48 +0000

    Make mediaGalleriesPrivate.onDevice{Attached,Detached} work properly by always running the internal device tracking code.
    BUG=155652
    Review URL: https://codereview.chromium.org/11151010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162046 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/media_galleries_private/media_galleries_private_event_router.cc
+++ chrome/browser/extensions/api/media_galleries_private/media_galleries_private_event_router.cc
@@ -103,13 +103,14 @@ void MediaGalleriesPrivateEventRouter::OnRemovableStorageAttached(
     const std::string& id,
     const string16& name,
     const FilePath::StringType& location) {
+  TransientDeviceIds* device_ids = TransientDeviceIds::GetInstance();
+  if (!device_ids->DeviceAttached(id))
+    return;
+
   EventRouter* router = profile_->GetExtensionEventRouter();
   if (!router->HasEventListener(kOnAttachEventName))
     return;
 
-  TransientDeviceIds* device_ids = TransientDeviceIds::GetInstance();
-  if (!device_ids->DeviceAttached(id))
-    return;
   std::string transient_id = device_ids->GetTransientIdForUniqueId(id);
   CHECK(!transient_id.empty());
 
@@ -124,10 +125,6 @@ void MediaGalleriesPrivateEventRouter::OnRemovableStorageAttached(
 
 void MediaGalleriesPrivateEventRouter::OnRemovableStorageDetached(
     const std::string& id) {
-  EventRouter* router = profile_->GetExtensionEventRouter();
-  if (!router->HasEventListener(kOnDetachEventName))
-    return;
-
   TransientDeviceIds* device_ids = TransientDeviceIds::GetInstance();
   std::string transient_id = device_ids->GetTransientIdForUniqueId(id);
   if (transient_id.empty()) {
@@ -135,6 +132,10 @@ void MediaGalleriesPrivateEventRouter::OnRemovableStorageDetached(
     return;
   }
   CHECK(device_ids->DeviceDetached(id));
+
+  EventRouter* router = profile_->GetExtensionEventRouter();
+  if (!router->HasEventListener(kOnDetachEventName))
+    return;
 
   DeviceDetachmentDetails details;
   details.device_id = transient_id;
