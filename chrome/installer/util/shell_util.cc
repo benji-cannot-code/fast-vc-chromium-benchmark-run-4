50049d0ad4ab262167c99ad9d5ad18daa6b526ea
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 13:58:08 +0000

    Only register XPStyle registrations on Win8 if the user chooses Chrome when prompted with the greg dialog.
    (http://codereview.chromium.org/11127002/ must go in before this)
    
    R=grt@chromium.org
    BUG=154806
    
    Review URL: https://codereview.chromium.org/11121009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162133 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/util/shell_util.cc
+++ chrome/installer/util/shell_util.cc
@@ -1601,7 +1601,8 @@ bool ShellUtil::ShowMakeChromeDefaultSystemUI(BrowserDistribution* dist,
   // Return true only when the user took an action and there was no error.
   const bool ret = LaunchSelectDefaultProtocolHandlerDialog(L"http");
 
-  // TODO(gab): RegisterChromeAsDefaultForXP if Chrome was chosen.
+  if (ret && IsChromeDefault())
+    RegisterChromeAsDefaultForXP(dist, CURRENT_USER, chrome_exe);
 
   return ret;
 }
@@ -1671,7 +1672,8 @@ bool ShellUtil::ShowMakeChromeDefaultProtocolClientSystemUI(
   // Return true only when the user took an action and there was no error.
   const bool ret = LaunchSelectDefaultProtocolHandlerDialog(protocol.c_str());
 
-  // TODO(gab): RegisterChromeAsDefaultProtocolClientForXP if Chrome was chosen.
+  if (ret && IsChromeDefaultProtocolClient(protocol))
+    RegisterChromeAsDefaultProtocolClientForXP(dist, chrome_exe, protocol);
 
   return ret;
 }
