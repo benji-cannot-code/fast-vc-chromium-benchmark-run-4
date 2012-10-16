fe87019fff167d8ec262b0b22129fe47f1cbdd40
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 01:58:21 +0000

    Cleanup: Rename to base::SysInfo::CPUArchitecture() to OperatingSystemArchitecture().
    Review URL: https://chromiumcodereview.appspot.com/11140015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162041 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_info_posix.cc
+++ base/sys_info_posix.cc
@@ -76,7 +76,7 @@ std::string SysInfo::OperatingSystemVersion() {
 #endif
 
 // static
-std::string SysInfo::CPUArchitecture() {
+std::string SysInfo::OperatingSystemArchitecture() {
   struct utsname info;
   if (uname(&info) < 0) {
     NOTREACHED();
