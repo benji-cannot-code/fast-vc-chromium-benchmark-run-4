37f9f2ec15ff60043ec3e5eada088c2fcbc041b3
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-16 12:53:21 +0000

    Fix build-webkit bailing out of !isSVN() and !isGit()
    Reviewed by Tor Arne Vestbø.
    
    Added missing else case with early return.
    
    * Scripts/VCSUtils.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131446 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/VCSUtils.pm
+++ third_party/WebKit/Tools/Scripts/VCSUtils.pm
@@ -2121,6 +2121,8 @@ sub listOfChangedFilesBetweenRevisions
         $command = "git diff --name-status $firstCommit..$lastCommit";
     } elsif (isSVN()) {
         $command = "svn diff --summarize -r $firstRevision:$lastRevision";
+    } else {
+        return ();
     }
     my $diffOutput = `$command`;
     $diffOutput =~ s/^[A-Z]\s+//gm;
