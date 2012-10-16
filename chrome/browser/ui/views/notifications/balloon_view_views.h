6aa9306259f07b269076a242bf96475295e05a81
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 16:46:01 +0000

    Attempt at fixing crash in balloon code. The current code does a Widget::Close() and posts a delayed task to cleanup (delete this) after that. That means there is a window of time between when Close() completes and the task is run where the widget has been destroyed. I'm moving cleanup to DeleteDelegate() so there is no window.
    This change would also cover the case of the NativeWindow being
    deleted out from under the balloon.
    
    BUG=154173
    TEST=none
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/11143031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/notifications/balloon_view_views.h
+++ chrome/browser/ui/views/notifications/balloon_view_views.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/notifications/balloon.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -37,7 +36,7 @@ class MenuButton;
 class MenuRunner;
 }
 
-// A balloon view is the UI component for a desktop notification toasts.
+// A balloon view is the UI component for a desktop notification toast.
 // It draws a border, and within the border an HTML renderer.
 class BalloonViewImpl : public BalloonView,
                         public views::MenuButtonListener,
@@ -72,6 +71,7 @@ class BalloonViewImpl : public BalloonView,
   // views::WidgetDelegate interface.
   virtual void OnDisplayChanged() OVERRIDE;
   virtual void OnWorkAreaChanged() OVERRIDE;
+  virtual void DeleteDelegate() OVERRIDE;
 
   // views::ButtonListener interface.
   virtual void ButtonPressed(views::Button* sender,
@@ -96,9 +96,6 @@ class BalloonViewImpl : public BalloonView,
 
   // Adjust the contents window size to be appropriate for the frame.
   void SizeContentsWindow();
-
-  // Do the delayed close work.
-  void DelayedClose(bool by_user);
 
   // The height of the balloon's shelf.
   // The shelf is where is close button is located.
@@ -138,9 +135,6 @@ class BalloonViewImpl : public BalloonView,
   // The renderer of the HTML contents.
   scoped_ptr<BalloonViewHost> html_contents_;
 
-  // The following factory is used to call methods at a later time.
-  base::WeakPtrFactory<BalloonViewImpl> method_factory_;
-
   // Pointer to sub-view is owned by the View sub-class.
   views::ImageButton* close_button_;
 
@@ -161,6 +155,9 @@ class BalloonViewImpl : public BalloonView,
 
   // Set to true if this is browser generate web UI.
   bool enable_web_ui_;
+
+  // Most recent value passed to Close().
+  bool closed_by_user_;
 
   DISALLOW_COPY_AND_ASSIGN(BalloonViewImpl);
 };
