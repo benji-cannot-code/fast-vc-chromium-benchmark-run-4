39e5a8057e61315c251f064dc4db72893884f9fb
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 23:36:23 +0000

    cc: Remove wtf Locker include from unittest.
    BUG=154451
    TEST=cc_unittests
    R=enne@chromium.org,jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11191018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162281 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/threaded_unittest.cc
+++ cc/threaded_unittest.cc
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <public/WebFilterOperation.h>
 #include <public/WebFilterOperations.h>
 #include <public/WebThread.h>
-#include <wtf/Locker.h>
 
 using namespace cc;
 using namespace WebKit;
