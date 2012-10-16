ced45e30961659547e0f88a8af97a9bc68538ad5
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-16 05:25:34 +0000

    Warn when CSP headers don't separate directives with ';'. https://bugs.webkit.org/show_bug.cgi?id=99274
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    A recent bug report exhibited confusion as to what role, exactly,
    semicolons play in Content Security Policy definitions. This patch
    adjusts the parser to warn web authors if a directive name is
    encountered while parsing a source list, which would almost certainly
    point to a missing semicolon. For example, something like:
    
        script-src 'self' object-src 'self' style-src *
    
    would throw two warnings, noting that 'object-src' and 'style-src' are
    probably meant as directives, and not as source expressions in the
    'script-src' source list.
    
    Test: http/tests/security/contentSecurityPolicy/source-list-parsing-no-semicolon.html
    
    * page/ContentSecurityPolicy.cpp:
    (WebCore::CSPSourceList::parse):
        Throw the new warning onto the console if the hostname of a source
        expression matches a CSP directive name.
    (WebCore::CSPDirectiveList::addDirective):
        Use the exciting new static variables so that the names of various
        directives are available outside of CSPDirectiveList::addDirective.
        These were previously defined as local static ASCIILiterals. Now
        they're static const char[] in an anonymous namespace.
    (WebCore::ContentSecurityPolicy::isDirectiveName):
        Static method that compares a string to all the known directive
        names, returning true if there's a (case-insensitive) match.
    (WebCore):
    (WebCore::ContentSecurityPolicy::reportDirectiveAsSourceExpression):
        A new warning message which should be printed when a directive name
        is encountered as a source expression. Matching the directive name
        is done via a new 'isDirectiveName' function which lives in an
        anonymous namespace along with the other CSP helper functions.
    * page/ContentSecurityPolicy.h:
        Added 'reportDirectiveAsSourceExpression' signature.
    
    LayoutTests:
    
    * http/tests/security/contentSecurityPolicy/source-list-parsing-no-semicolon-expected.txt: Added.
    * http/tests/security/contentSecurityPolicy/source-list-parsing-no-semicolon.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.cpp
@@ -105,6 +105,44 @@ bool isMediaTypeCharacter(UChar c)
     return !isASCIISpace(c) && c != '/';
 }
 
+static const char connectSrc[] = "connect-src";
+static const char defaultSrc[] = "default-src";
+static const char fontSrc[] = "font-src";
+static const char frameSrc[] = "frame-src";
+static const char imgSrc[] = "img-src";
+static const char mediaSrc[] = "media-src";
+static const char objectSrc[] = "object-src";
+static const char reportURI[] = "report-uri";
+static const char sandbox[] = "sandbox";
+static const char scriptSrc[] = "script-src";
+static const char styleSrc[] = "style-src";
+#if ENABLE(CSP_NEXT)
+static const char formAction[] = "form-action";
+static const char pluginTypes[] = "plugin-types";
+static const char scriptNonce[] = "script-nonce";
+#endif
+
+bool isDirectiveName(const String& name)
+{
+    return (equalIgnoringCase(name, connectSrc)
+        || equalIgnoringCase(name, defaultSrc)
+        || equalIgnoringCase(name, fontSrc)
+        || equalIgnoringCase(name, frameSrc)
+        || equalIgnoringCase(name, imgSrc)
+        || equalIgnoringCase(name, mediaSrc)
+        || equalIgnoringCase(name, objectSrc)
+        || equalIgnoringCase(name, reportURI)
+        || equalIgnoringCase(name, sandbox)
+        || equalIgnoringCase(name, scriptSrc)
+        || equalIgnoringCase(name, styleSrc)
+#if ENABLE(CSP_NEXT)
+        || equalIgnoringCase(name, formAction)
+        || equalIgnoringCase(name, pluginTypes)
+        || equalIgnoringCase(name, scriptNonce)
+#endif
+    );
+}
+
 } // namespace
 
 static bool skipExactly(const UChar*& position, const UChar* end, UChar delimiter)
@@ -311,6 +349,8 @@ void CSPSourceList::parse(const UChar* begin, const UChar* end)
                 continue;
             if (scheme.isEmpty())
                 scheme = m_policy->securityOrigin()->protocol();
+            if (isDirectiveName(host))
+                m_policy->reportDirectiveAsSourceExpression(m_directiveName, host);
             m_list.append(CSPSource(scheme, host, port, path, hostHasWildcard, portHasWildcard));
         } else
             m_policy->reportInvalidSourceExpression(m_directiveName, String(beginSource, position - beginSource));
@@ -1233,23 +1273,6 @@ void CSPDirectiveList::applySandboxPolicy(const String& name, const String& sand
 
 void CSPDirectiveList::addDirective(const String& name, const String& value)
 {
-    DEFINE_STATIC_LOCAL(String, defaultSrc, (ASCIILiteral("default-src")));
-    DEFINE_STATIC_LOCAL(String, scriptSrc, (ASCIILiteral("script-src")));
-    DEFINE_STATIC_LOCAL(String, objectSrc, (ASCIILiteral("object-src")));
-    DEFINE_STATIC_LOCAL(String, frameSrc, (ASCIILiteral("frame-src")));
-    DEFINE_STATIC_LOCAL(String, imgSrc, (ASCIILiteral("img-src")));
-    DEFINE_STATIC_LOCAL(String, styleSrc, (ASCIILiteral("style-src")));
-    DEFINE_STATIC_LOCAL(String, fontSrc, (ASCIILiteral("font-src")));
-    DEFINE_STATIC_LOCAL(String, mediaSrc, (ASCIILiteral("media-src")));
-    DEFINE_STATIC_LOCAL(String, connectSrc, (ASCIILiteral("connect-src")));
-    DEFINE_STATIC_LOCAL(String, sandbox, (ASCIILiteral("sandbox")));
-    DEFINE_STATIC_LOCAL(String, reportURI, (ASCIILiteral("report-uri")));
-#if ENABLE(CSP_NEXT)
-    DEFINE_STATIC_LOCAL(String, formAction, (ASCIILiteral("form-action")));
-    DEFINE_STATIC_LOCAL(String, pluginTypes, (ASCIILiteral("plugin-types")));
-    DEFINE_STATIC_LOCAL(String, scriptNonce, (ASCIILiteral("script-nonce")));
-#endif
-
     ASSERT(!name.isEmpty());
 
     if (equalIgnoringCase(name, defaultSrc))
@@ -1578,6 +1601,12 @@ void ContentSecurityPolicy::reportViolation(const String& directiveText, const S
 void ContentSecurityPolicy::reportUnrecognizedDirective(const String& name) const
 {
     String message = makeString("Unrecognized Content-Security-Policy directive '", name, "'.\n");
+    logToConsole(message);
+}
+
+void ContentSecurityPolicy::reportDirectiveAsSourceExpression(const String& directiveName, const String& sourceExpression) const
+{
+    String message = "The Content Security Policy directive '" + directiveName + "' contains '" + sourceExpression + "' as a source expression. Did you mean '" + directiveName + " ...; " + sourceExpression + "...' (note the semicolon)?";
     logToConsole(message);
 }
 
