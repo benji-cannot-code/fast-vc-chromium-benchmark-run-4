650315ca524ac3f59fa83c6e246010c4b41a94f4
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 21:22:10 +0000

    Make Web Intents picker in Views conform to latest mocks
    BUG=148615
    
    
    Review URL: https://chromiumcodereview.appspot.com/11044020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162252 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_views.h
+++ chrome/browser/ui/views/constrained_window_views.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "chrome/browser/ui/constrained_window.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gfx/rect.h"
@@ -60,11 +62,19 @@ class NativeConstrainedWindow {
 class ConstrainedWindowViews : public views::Widget,
                                public ConstrainedWindow,
                                public NativeConstrainedWindowDelegate,
-                               public content::WebContentsObserver {
+                               public content::WebContentsObserver,
+                               public content::NotificationObserver {
  public:
+  // Types of insets to use with chrome style frame.
+  enum ChromeStyleClientInsets {
+    DEFAULT_INSETS,
+    NO_INSETS,
+  };
+
   ConstrainedWindowViews(content::WebContents* web_contents,
                          views::WidgetDelegate* widget_delegate,
-                         bool enable_chrome_style);
+                         bool enable_chrome_style,
+                         ChromeStyleClientInsets chrome_style_client_insets);
   virtual ~ConstrainedWindowViews();
 
   // Returns the WebContents that constrains this Constrained Window.
@@ -79,6 +89,9 @@ class ConstrainedWindowViews : public views::Widget,
   // Overridden from views::Widget:
   void CenterWindow(const gfx::Size& size);
 
+  // Default insets for the dialog:
+  static gfx::Insets GetDefaultInsets();
+
  private:
   void NotifyTabHelperWillClose();
 
@@ -92,20 +105,29 @@ class ConstrainedWindowViews : public views::Widget,
       AsNativeWidgetDelegate() OVERRIDE;
   virtual int GetNonClientComponent(const gfx::Point& point) OVERRIDE;
 
-  // Set the top of the window to overlap the browser chrome.
-  void PositionChromeStyleWindow();
-
   // Overridden from content::WebContentsObserver:
   virtual void WebContentsDestroyed(content::WebContents* web_contents)
       OVERRIDE;
 
-  content::WebContents* web_contents_;
+  // Overridden from content::NotificationObserver:
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
 
-  NativeConstrainedWindow* native_constrained_window_;
+  // Set the top of the window to overlap the browser chrome.
+  void PositionChromeStyleWindow(const gfx::Size& size);
+
+  content::NotificationRegistrar registrar_;
+  content::WebContents* web_contents_;
 
   // TODO(wittman): remove once all constrained window dialogs are moved
   // over to Chrome style.
   const bool enable_chrome_style_;
+
+  // Client insets to use when |enable_chrome_style_| is true.
+  ChromeStyleClientInsets chrome_style_client_insets_;
+
+  NativeConstrainedWindow* native_constrained_window_;
 
   DISALLOW_COPY_AND_ASSIGN(ConstrainedWindowViews);
 };
