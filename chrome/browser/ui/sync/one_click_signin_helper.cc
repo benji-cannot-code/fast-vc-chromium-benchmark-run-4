49d4c1f7850798bd3fefc578cf73e711dc983bf7
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-16 17:47:25 +0000

    Do not display one-click if setup is in progress.
    Mirrors a previous change that was inadvertently reverted (https://chromiumcodereview.appspot.com/10829029)
    
    BUG=138343
    TEST=unit_tests: OneClickSigninHelper.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/11150032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162166 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_helper.cc
+++ chrome/browser/ui/sync/one_click_signin_helper.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/signin_manager.h"
 #include "chrome/browser/signin/signin_manager_factory.h"
 #include "chrome/browser/sync/profile_sync_service.h"
+#include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
@@ -317,6 +318,21 @@ bool OneClickSigninHelper::CanOffer(content::WebContents* web_contents,
           return false;
       }
     }
+
+    // If we're about to show a one-click infobar but the user has started
+    // a concurrent signin flow (perhaps via the promo), we may not have yet
+    // established an authenticated username but we still shouldn't move
+    // forward with two simultaneous signin processes.  This is a bit
+    // contentious as the one-click flow is a much smoother flow from the user
+    // perspective, but it's much more difficult to hijack the other flow from
+    // here as it is to bail.
+    ProfileSyncService* service =
+        ProfileSyncServiceFactory::GetForProfile(profile);
+    if (!service)
+      return false;
+
+    if (service->FirstSetupInProgress())
+      return false;
   }
 
   return true;
