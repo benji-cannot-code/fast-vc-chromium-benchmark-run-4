83dd9e2d8546c4fbab8377ee5700d5e4c6863e05
posciak@chromium.org <posciak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 18:06:59 +0000

    Only GVD::DestroyTextures() on CrOS for now.
    BUG=156251
    TEST=
    TBR=fischman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11196021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162461 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/gpu_video_decoder.cc
+++ media/filters/gpu_video_decoder.cc
@@ -216,7 +216,10 @@ void GpuVideoDecoder::DestroyVDA() {
       base::Bind(&VideoDecodeAccelerator::Destroy, weak_vda_),
       base::Bind(&GpuVideoDecoder::Release, this));
 
+// TODO(posciak): enable for all.
+#ifdef OS_CHROMEOS
   DestroyTextures();
+#endif
 }
 
 void GpuVideoDecoder::Read(const ReadCB& read_cb) {
@@ -541,7 +544,10 @@ GpuVideoDecoder::~GpuVideoDecoder() {
   }
   bitstream_buffers_in_decoder_.clear();
 
+// TODO(posciak): enable for all.
+#ifdef OS_CHROMEOS
   DestroyTextures();
+#endif
 }
 
 void GpuVideoDecoder::EnsureDemuxOrDecode() {
