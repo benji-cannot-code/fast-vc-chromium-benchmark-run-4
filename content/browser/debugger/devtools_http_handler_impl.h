7b72e58cbf59bf517f828f587a8e012f57abf9be
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 12:11:03 +0000

    DevTools: [remote debugging] terminate web socket connection upon renderer disconnect.
    BUG=154253
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=162123
    
    Review URL: https://chromiumcodereview.appspot.com/11141024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162355 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/debugger/devtools_http_handler_impl.h
+++ content/browser/debugger/devtools_http_handler_impl.h
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/browser/devtools_http_handler.h"
 #include "content/public/browser/devtools_http_handler_delegate.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "net/server/http_server.h"
 
 namespace base {
@@ -36,6 +38,7 @@ class RenderViewHost;
 
 class DevToolsHttpHandlerImpl
     : public DevToolsHttpHandler,
+      public NotificationObserver,
       public base::RefCountedThreadSafe<DevToolsHttpHandlerImpl>,
       public net::HttpServer::Delegate {
  private:
@@ -58,6 +61,11 @@ class DevToolsHttpHandlerImpl
   virtual void SetRenderViewHostBinding(
       RenderViewHostBinding* binding) OVERRIDE;
   virtual GURL GetFrontendURL(RenderViewHost* render_view_host) OVERRIDE;
+
+  // NotificationObserver implementation.
+  virtual void Observe(int type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details) OVERRIDE;
 
   // net::HttpServer::Delegate implementation.
   virtual void OnHttpRequest(int connection_id,
@@ -122,6 +130,7 @@ class DevToolsHttpHandlerImpl
   scoped_ptr<DevToolsHttpHandlerDelegate> delegate_;
   RenderViewHostBinding* binding_;
   scoped_ptr<RenderViewHostBinding> default_binding_;
+  NotificationRegistrar registrar_;
   DISALLOW_COPY_AND_ASSIGN(DevToolsHttpHandlerImpl);
 };
 
