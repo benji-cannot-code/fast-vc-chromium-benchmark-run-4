570e285e6733dfaf61e023eca21715d2eab845fb
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 16:55:06 +0000

    Disable automatic flushes on compositor contexts.
    The compositor contexts handle flushes themselves, so they don't need the automatic flushes, which can hurt performance.
    
    BUG=152393
    
    
    Review URL: https://chromiumcodereview.appspot.com/11099029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162434 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -1915,6 +1915,7 @@ WebKit::WebCompositorOutputSurface* RenderViewImpl::createOutputSurface() {
   WebKit::WebGraphicsContext3D::Attributes attributes;
   attributes.antialias = false;
   attributes.shareResources = true;
+  attributes.noAutomaticFlushes = true;
   WebGraphicsContext3D* context = CreateGraphicsContext3D(attributes);
   if (!context)
     return NULL;
