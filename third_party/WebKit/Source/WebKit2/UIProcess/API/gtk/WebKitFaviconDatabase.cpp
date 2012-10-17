5c5c03dd25dded3f7746d183f3baaff2e766211c
mario@webkit.org <mario@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 09:27:41 +0000

    [WK2][GTK] Unneeded GError field in internal structure in WebKitFaviconDatabase.cpp https://bugs.webkit.org/show_bug.cgi?id=99564
    Reviewed by Carlos Garcia Campos.
    
    Removed field 'error' from GetFaviconSurfaceAsyncData and updated
    the code accordingly, by using g_simple_async_result_take_error().
    
    * UIProcess/API/gtk/WebKitFaviconDatabase.cpp:
    (GetFaviconSurfaceAsyncData): Removed unneeded field.
    (processPendingIconsForPageURL): Avoid using g_propagate_error and
    use g_simple_async_result_take_error directly instead.
    (webkit_favicon_database_get_favicon_finish): Remove unneeded code.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitFaviconDatabase.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitFaviconDatabase.cpp
@@ -111,7 +111,6 @@ struct GetFaviconSurfaceAsyncData {
     GRefPtr<WebKitFaviconDatabase> faviconDatabase;
     String pageURL;
     RefPtr<cairo_surface_t> icon;
-    GOwnPtr<GError> error;
     GRefPtr<GCancellable> cancellable;
 };
 WEBKIT_DEFINE_ASYNC_DATA_STRUCT(GetFaviconSurfaceAsyncData)
@@ -161,7 +160,7 @@ static void processPendingIconsForPageURL(WebKitFaviconDatabase* database, const
         GetFaviconSurfaceAsyncData* data = static_cast<GetFaviconSurfaceAsyncData*>(g_simple_async_result_get_op_res_gpointer(result));
         if (!g_cancellable_is_cancelled(data->cancellable.get())) {
             if (error)
-                g_propagate_error(&data->error.outPtr(), error.release());
+                g_simple_async_result_take_error(result, error.release());
             else
                 data->icon = icon;
         }
@@ -346,11 +345,6 @@ cairo_surface_t* webkit_favicon_database_get_favicon_finish(WebKitFaviconDatabas
 
     GetFaviconSurfaceAsyncData* data = static_cast<GetFaviconSurfaceAsyncData*>(g_simple_async_result_get_op_res_gpointer(simpleResult));
     ASSERT(data);
-    if (data->error) {
-        g_propagate_error(error, data->error.release());
-        return 0;
-    }
-
     return cairo_surface_reference(data->icon.get());
 }
 
