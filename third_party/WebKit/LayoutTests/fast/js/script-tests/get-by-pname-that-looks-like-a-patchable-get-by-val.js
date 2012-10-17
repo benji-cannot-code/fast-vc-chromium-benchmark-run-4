a2f9951b32d599f5875e764859641cd4262a6b27
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 21:12:52 +0000

    JIT op_get_by_pname should call cti_get_by_val_generic and not cti_get_by_val https://bugs.webkit.org/show_bug.cgi?id=99631 <rdar://problem/12483221>
    Reviewed by Mark Hahnenberg.
    
    Source/JavaScriptCore: 
    
    cti_get_by_val assumes that the return address has patching metadata associated with it, which won't
    be true for op_get_by_pname. cti_get_by_val_generic makes no such assumptions.
    
    * jit/JITPropertyAccess.cpp:
    (JSC::JIT::emitSlow_op_get_by_pname):
    * jit/JITPropertyAccess32_64.cpp:
    (JSC::JIT::emitSlow_op_get_by_pname):
    
    LayoutTests: 
    
    * fast/js/get-by-pname-that-looks-like-a-patchable-get-by-val-expected.txt: Added.
    * fast/js/get-by-pname-that-looks-like-a-patchable-get-by-val.html: Added.
    * fast/js/jsc-test-list:
    * fast/js/script-tests/get-by-pname-that-looks-like-a-patchable-get-by-val.js: Added.
    (foo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131642 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/get-by-pname-that-looks-like-a-patchable-get-by-val.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Tests that using get_by_pname in a way that appears like a get_by_val that can be patched does not cause the patching machinery to crash."
+);
+
+function foo() {
+    var o = [1, 2, 3];
+    var result = 0;
+    
+    for (var i = 0; i < 100; ++i) {
+        for (var s in o) {
+            s = 0;
+            result += o[s];
+        }
+    }
+    
+    return result;
+}
+
+shouldBe("foo()", "300");
+
+
