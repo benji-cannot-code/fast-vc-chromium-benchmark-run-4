6272a4062a4898af7457c6963c53aa7f03b96687
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 12:35:20 +0000

    Remove TabContents from AutomationTabHelperTest.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11191008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162362 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_tab_helper_unittest.cc
+++ chrome/browser/automation/automation_tab_helper_unittest.cc
@@ -8,8 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "chrome/browser/automation/automation_tab_helper.h"
 #include "chrome/browser/automation/mock_tab_event_observer.h"
-#include "chrome/browser/ui/tab_contents/tab_contents.h"
-#include "chrome/browser/ui/tab_contents/test_tab_contents.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "content/public/test/test_browser_thread.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -17,14 +16,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::BrowserThread;
 using testing::_;
 
-class AutomationTabHelperTest : public TabContentsTestHarness {
+class AutomationTabHelperTest : public ChromeRenderViewHostTestHarness {
  public:
   AutomationTabHelperTest()
-      : TabContentsTestHarness(),
+      : ChromeRenderViewHostTestHarness(),
         browser_thread_(BrowserThread::UI, &message_loop_) {}
 
   virtual void SetUp() {
-    TabContentsTestHarness::SetUp();
+    ChromeRenderViewHostTestHarness::SetUp();
+    AutomationTabHelper::CreateForWebContents(web_contents());
     mock_observer_.StartObserving(tab_helper());
   }
 
@@ -39,7 +39,7 @@ class AutomationTabHelperTest : public TabContentsTestHarness {
     tab_helper()->DidStopLoading(NULL);
   }
 
-  void TabContentsDestroyed() {
+  void WebContentsDestroyed() {
     tab_helper()->WebContentsDestroyed(web_contents());
   }
 
@@ -209,6 +209,6 @@ TEST_F(AutomationTabHelperTest, DestroyedTabStopsLoading) {
 
   StartLoading();
   WillPerformClientRedirect(1);
-  TabContentsDestroyed();
+  WebContentsDestroyed();
   EXPECT_FALSE(tab_helper()->has_pending_loads());
 }
