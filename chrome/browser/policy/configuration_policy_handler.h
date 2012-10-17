d5477f75cebdd5026a35e58dbc3ccf1996823e5e
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 12:15:44 +0000

    Don't reject extension list policies with invalid entries.
    Even if there's an entry that doesn't have a valid extension ID, we
    should allow the remaining entries to be processed.
    
    BUG=chromium:131065
    TEST=unit test
    
    Review URL: https://chromiumcodereview.appspot.com/11183029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162358 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_handler.h
+++ chrome/browser/policy/configuration_policy_handler.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/values.h"
 #include "chrome/browser/prefs/incognito_mode_prefs.h"
 #include "chrome/common/content_settings.h"
@@ -120,7 +121,7 @@ class ExtensionListPolicyHandler : public TypeCheckingPolicyHandler {
   // Runs sanity checks on the policy value and returns it in |extension_ids|.
   bool CheckAndGetList(const PolicyMap& policies,
                        PolicyErrorMap* errors,
-                       const base::ListValue** extension_ids);
+                       scoped_ptr<base::ListValue>* extension_ids);
 
  private:
   const char* pref_path_;
