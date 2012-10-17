05baa828bb70371e8ee0c211be172462f3f0da11
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 08:49:14 +0000

    Web Inspector: [Regression] SASS sources are not saved to disk. https://bugs.webkit.org/show_bug.cgi?id=99551
    Reviewed by Yury Semikhatsky.
    
    Added resource null checks.
    
    * inspector/front-end/StylesSourceMapping.js:
    (WebInspector.StylesSourceMapping.prototype._uiSourceCodeAddedToWorkspace):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/StylesSourceMapping.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/StylesSourceMapping.js
@@ -73,7 +73,9 @@ WebInspector.StylesSourceMapping.prototype = {
             return;
         if (uiSourceCode.contentType() !== WebInspector.resourceTypes.Stylesheet)
             return;
-            
+        if (!WebInspector.resourceForURL(uiSourceCode.url))
+            return;
+
         this._addUISourceCode(uiSourceCode);
     },
 
@@ -188,6 +190,11 @@ WebInspector.StyleContentBinding.prototype = {
     setStyleContent: function(uiSourceCode, content, majorChange, userCallback)
     {
         var resource = WebInspector.resourceForURL(uiSourceCode.url);
+        if (!resource) {
+            userCallback("No resource found: " + uiSourceCode.url);
+            return;
+        }
+            
         this._cssModel.resourceBinding().requestStyleSheetIdForResource(resource, callback.bind(this));
 
         /**
