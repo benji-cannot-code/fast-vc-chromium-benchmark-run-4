04ca2815d9a32cdfa5f8a578d9ed2889f009e05a
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 15:31:45 +0000

    [GTK] Don't use the C API internally in WebKitWebInspector https://bugs.webkit.org/show_bug.cgi?id=96786
    Reviewed by Gustavo Noronha Silva.
    
    Using the C++ classes directly instead of the C API wrappers we
    avoid a lot of toImpl/toAPI casts, string conversions and
    allocations. The code is also a lot simpler and easier to read.
    
    * UIProcess/API/gtk/WebKitWebInspector.cpp:
    (_WebKitWebInspectorPrivate):
    (webkitWebInspectorFinalize):
    (webkitWebInspectorCreate):
    (webkit_web_inspector_get_web_view):
    (webkit_web_inspector_is_attached):
    (webkit_web_inspector_attach):
    (webkit_web_inspector_detach):
    (webkit_web_inspector_show):
    (webkit_web_inspector_close):
    (webkit_web_inspector_get_attached_height):
    * UIProcess/API/gtk/WebKitWebInspectorPrivate.h:
    * UIProcess/API/gtk/WebKitWebView.cpp:
    (webkit_web_view_get_inspector):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitWebInspectorPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitWebInspectorPrivate.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitPrivate.h"
 #include "WebKitWebInspector.h"
 
-WebKitWebInspector* webkitWebInspectorCreate(WKInspectorRef);
+using namespace WebKit;
+
+WebKitWebInspector* webkitWebInspectorCreate(WebInspectorProxy*);
 
 #endif // WebKitWebInspectorPrivate_h
