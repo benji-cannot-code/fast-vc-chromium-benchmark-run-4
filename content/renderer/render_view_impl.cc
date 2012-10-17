4148d2b6b4566f2f85e23a102ecdd80199eb09b6
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 21:11:46 +0000

    Cancel pending custom context menu when destroying a ContextMenuClient.
    BUG=156289
    
    
    Review URL: https://chromiumcodereview.appspot.com/11189033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162528 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -4435,6 +4435,11 @@ int RenderViewImpl::ShowContextMenu(content::ContextMenuClient* client,
   return our_params.custom_context.request_id;
 }
 
+void RenderViewImpl::CancelContextMenu(int request_id) {
+  DCHECK(pending_context_menus_.Lookup(request_id));
+  pending_context_menus_.Remove(request_id);
+}
+
 WebKit::WebPageVisibilityState RenderViewImpl::GetVisibilityState() const {
   return visibilityState();
 }
@@ -5167,7 +5172,8 @@ void RenderViewImpl::OnCustomContextMenuAction(
     // External context menu request, look in our map.
     content::ContextMenuClient* client =
         pending_context_menus_.Lookup(custom_context.request_id);
-    client->OnMenuAction(custom_context.request_id, action);
+    if (client)
+      client->OnMenuAction(custom_context.request_id, action);
   } else {
     // Internal request, forward to WebKit.
     webview()->performCustomContextMenuAction(action);
@@ -6309,8 +6315,10 @@ void RenderViewImpl::OnContextMenuClosed(
     // External request, should be in our map.
     content::ContextMenuClient* client =
         pending_context_menus_.Lookup(custom_context.request_id);
-    client->OnMenuClosed(custom_context.request_id);
-    pending_context_menus_.Remove(custom_context.request_id);
+    if (client) {
+      client->OnMenuClosed(custom_context.request_id);
+      pending_context_menus_.Remove(custom_context.request_id);
+    }
   } else {
     // Internal request, forward to WebKit.
     context_menu_node_.reset();
