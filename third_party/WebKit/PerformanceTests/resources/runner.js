b25f36b98813cb4a472ce84a3338a3ef8620cb8b
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 20:57:46 +0000

    Performance test should support asynchronous tests https://bugs.webkit.org/show_bug.cgi?id=95668
    Reviewed by Dirk Pranke.
    
    Added PerfTestRunner.prepareToMeasureValuesAsync and PerfTestRunner.measureValueAsync,
    and deployed it in Animation/balls.
    
    * Animation/balls.html:
    * resources/runner.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/PerformanceTests/resources/runner.js
+++ third_party/WebKit/PerformanceTests/resources/runner.js
@@ -171,7 +171,8 @@ if (window.testRunner) {
         runCount = test.runCount || 20;
         logLines = window.testRunner ? [] : null;
         PerfTestRunner.log("Running " + runCount + " times");
-        scheduleNextRun(runner);
+        if (runner)
+            scheduleNextRun(runner);
     }
 
     function scheduleNextRun(runner) {
@@ -233,6 +234,25 @@ if (window.testRunner) {
 
         if (window.testRunner)
             testRunner.notifyDone();
+    }
+
+    PerfTestRunner.prepareToMeasureValuesAsync = function (test) {
+        PerfTestRunner.unit = test.unit;
+        start(test);
+    }
+
+    PerfTestRunner.measureValueAync = function (measuredValue) {
+        completedRuns++;
+
+        try {
+            ignoreWarmUpAndLog(measuredValue);
+        } catch (exception) {
+            logFatalError("Got an exception while logging the result with name=" + exception.name + ", message=" + exception.message);
+            return;
+        }
+
+        if (completedRuns >= runCount)
+            finish();
     }
 
     PerfTestRunner.measureTime = function (test) {
