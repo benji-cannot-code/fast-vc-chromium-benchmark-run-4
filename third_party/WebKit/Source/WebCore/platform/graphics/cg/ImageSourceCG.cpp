aa9a17f6376a52138316039437e8d5bfaaf59ff0
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 14:36:35 +0000

    Unreviewed fix for compilation on CG bots.
    * platform/graphics/cg/ImageSourceCG.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131594 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cg/ImageSourceCG.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cg/ImageSourceCG.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntPoint.h"
 #include "IntSize.h"
 #include "MIMETypeRegistry.h"
+#include "PlatformMemoryInstrumentation.h"
 #include "SharedBuffer.h"
 #include <ApplicationServices/ApplicationServices.h>
 #include <wtf/UnusedParam.h>
