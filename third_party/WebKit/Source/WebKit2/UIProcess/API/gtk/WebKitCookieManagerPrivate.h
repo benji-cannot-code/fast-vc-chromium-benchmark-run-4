a784728b25c0f6322fad4389a8274dae9cc97211
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 15:56:29 +0000

    [GTK] Don't use the C API internally in WebKitCookieManager https://bugs.webkit.org/show_bug.cgi?id=96772
    Reviewed by Gustavo Noronha Silva.
    
    Using the C++ classes directly instead of the C API wrappers we
    avoid a lot of toImpl/toAPI casts, string conversions and
    allocations. The code is also a lot simpler and easier to read.
    
    * UIProcess/API/gtk/WebKitCookieManager.cpp:
    (_WebKitCookieManagerPrivate):
    (webkitCookieManagerFinalize):
    (webkitCookieManagerCreate):
    (webkit_cookie_manager_set_persistent_storage):
    (webkit_cookie_manager_set_accept_policy):
    (webkit_cookie_manager_get_accept_policy):
    (webkitCookieManagerGetDomainsWithCookiesCallback):
    (webkit_cookie_manager_get_domains_with_cookies):
    (webkit_cookie_manager_delete_cookies_for_domain):
    (webkit_cookie_manager_delete_all_cookies):
    * UIProcess/API/gtk/WebKitCookieManagerPrivate.h:
    * UIProcess/API/gtk/WebKitWebContext.cpp:
    (webkit_web_context_get_cookie_manager):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131607 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitCookieManagerPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitCookieManagerPrivate.h
@@ -24,6 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitCookieManager.h"
 #include "WebKitPrivate.h"
 
-WebKitCookieManager* webkitCookieManagerCreate(WKCookieManagerRef);
+using namespace WebKit;
+
+WebKitCookieManager* webkitCookieManagerCreate(WebCookieManagerProxy*);
 
 #endif // WebKitCookieManagerPrivate_h
