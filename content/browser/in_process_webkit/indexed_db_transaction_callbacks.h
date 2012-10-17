af7f5d693c319da6a932f20dfcf8082212404d7d
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 13:26:12 +0000

    IndexedDB: Delete old IPC plumbing following WK99097
    R=dgrogan@chromium.org
    BUG=113118
    
    
    Review URL: https://chromiumcodereview.appspot.com/11193003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162385 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_transaction_callbacks.h
+++ content/browser/in_process_webkit/indexed_db_transaction_callbacks.h
@@ -21,8 +21,6 @@ class IndexedDBTransactionCallbacks
 
   virtual ~IndexedDBTransactionCallbacks();
 
-  // TODO(jsbell): Remove once WK99097 has landed.
-  virtual void onAbort();
   virtual void onAbort(const WebKit::WebIDBDatabaseError& error);
   virtual void onComplete();
 
