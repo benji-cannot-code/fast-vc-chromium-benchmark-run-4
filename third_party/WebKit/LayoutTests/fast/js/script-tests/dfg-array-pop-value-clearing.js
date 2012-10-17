792ab05e8cefa9fc151c6fa7e729bf524a12784b
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 23:09:36 +0000

    REGRESSION (r130826 or r130828): Twitter top bar is dysfunctional https://bugs.webkit.org/show_bug.cgi?id=99577 <rdar://problem/12518883>
    Reviewed by Mark Hahnenberg.
    
    Source/JavaScriptCore: 
    
    It turns out that it's a good idea to maintain the invariants of your object model, such as that
    elements past publicLength should have the hole value.
    
    * dfg/DFGGraph.cpp:
    (JSC::DFG::Graph::dump):
    * dfg/DFGSpeculativeJIT32_64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    * dfg/DFGSpeculativeJIT64.cpp:
    (JSC::DFG::SpeculativeJIT::compile):
    
    LayoutTests: 
    
    * fast/js/dfg-array-pop-value-clearing-expected.txt: Added.
    * fast/js/dfg-array-pop-value-clearing.html: Added.
    * fast/js/jsc-test-list:
    * fast/js/script-tests/dfg-array-pop-value-clearing.js: Added.
    (foo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131662 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/dfg-array-pop-value-clearing.js
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Tests that array popping properly cleans up the popped element."
+);
+
+function foo(a) {
+    var x = a.pop();
+    a[a.length + 1] = 42;
+    return [x, a.pop(), a.pop()];
+}
+
+for (var i = 0; i < 1000; ++i)
+    shouldBe("foo([1, 2])", "[2,42,,]");
