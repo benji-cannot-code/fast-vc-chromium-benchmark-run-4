b81fe9e8179ef2b2519966bfb22c7db67e8ddb15
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-17 22:48:44 +0000

    IndexedDB: Enforce unsigned long/unsigned long long ranges https://bugs.webkit.org/show_bug.cgi?id=99637
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    The IndexedDB spec has [EnforceRange] specified on unsigned long and unsigned long long
    arguments, which requires the implementation to throw TypeError for negative values or
    values that exceed 2^53-1 (maximum JS number that behaves like an integer) - and 0 is
    specifically forbidden by the APIs as well.
    
    A more correct fix in the binding layer is in webkit.org/b/96798 but we can temporarily
    address this in the implementation.
    
    Also refactor to prevent IDBFactory.open(name, -1) from triggering an internal code path.
    
    Tests: storage/indexeddb/cursor-advance.html
           storage/indexeddb/intversion-bad-parameters.html
           storage/indexeddb/intversion-encoding.html
    
    * Modules/indexeddb/IDBCursor.cpp:
    (WebCore::IDBCursor::advance): Validate argument range.
    * Modules/indexeddb/IDBCursor.h:
    (IDBCursor):
    * Modules/indexeddb/IDBCursor.idl: Drop "unsigned" qualifier as the binding code is
    not yet doing the correct validation.
    * Modules/indexeddb/IDBFactory.cpp: Refactor to prevent open(name, -1)
    (WebCore):
    (WebCore::IDBFactory::open): Validate the int version here, then pass to...
    (WebCore::IDBFactory::openInternal): ... this method.
    * Modules/indexeddb/IDBFactory.h:
    (IDBFactory):
    * Modules/indexeddb/IDBFactory.idl: Drop "unsigned" qualifier; meaningless to binding
    code right now, can be re-added once webkit.org/b/96798 lands.
    
    LayoutTests:
    
    Additional edge case tests and updated expectations.
    
    * storage/indexeddb/cursor-advance-expected.txt:
    * storage/indexeddb/intversion-bad-parameters-expected.txt:
    * storage/indexeddb/intversion-encoding-expected.txt:
    * storage/indexeddb/resources/cursor-advance.js:
    (testBadAdvance.advanceBadly):
    (testBadAdvance):
    * storage/indexeddb/resources/intversion-bad-parameters.js:
    (deleteSuccess):
    * storage/indexeddb/resources/intversion-encoding.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBFactory.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBFactory.h
@@ -68,6 +68,8 @@ public:
 private:
     IDBFactory(IDBFactoryBackendInterface*);
 
+    PassRefPtr<IDBOpenDBRequest> openInternal(ScriptExecutionContext*, const String& name, int64_t version, ExceptionCode&);
+
     RefPtr<IDBFactoryBackendInterface> m_backend;
 };
 
