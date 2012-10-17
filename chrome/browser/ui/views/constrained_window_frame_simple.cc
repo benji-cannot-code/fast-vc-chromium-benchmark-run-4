d834bdd9d84683716b603753dbd900c747f61923
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-17 21:45:51 +0000

    Do not draw the non-client frame controls behind the client view in ConstrainedWindowFrameSimple
    BUG=156160
    
    
    Review URL: https://chromiumcodereview.appspot.com/11186028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_frame_simple.cc
+++ chrome/browser/ui/views/constrained_window_frame_simple.cc
@@ -32,11 +32,14 @@ ConstrainedWindowFrameSimple::ConstrainedWindowFrameSimple(
     ConstrainedWindowViews* container,
     ConstrainedWindowViews::ChromeStyleClientInsets client_insets)
     : container_(container),
-      title_label_(
-          new views::Label(container->widget_delegate()->GetWindowTitle())),
-      ALLOW_THIS_IN_INITIALIZER_LIST(close_button_(
-          new views::ImageButton(this))) {
+      title_label_(NULL),
+      close_button_(NULL) {
   container_->set_frame_type(views::Widget::FRAME_TYPE_FORCE_CUSTOM);
+
+  // The NO_INSETS consumers draw atop the frame and do not get the close button
+  // and title label below.
+  if (client_insets == ConstrainedWindowViews::NO_INSETS)
+    return;
 
   views::GridLayout* layout = new views::GridLayout(this);
   const int kHeaderTopPadding = std::min(
@@ -57,6 +60,8 @@ ConstrainedWindowFrameSimple::ConstrainedWindowFrameSimple(
   layout->StartRow(0, 0);
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  title_label_ = new views::Label(
+      container->widget_delegate()->GetWindowTitle());
   title_label_->SetFont(rb.GetFont(
       ConstrainedWindowConstants::kTitleFontStyle));
   title_label_->SetHorizontalAlignment(views::Label::ALIGN_LEFT);
@@ -66,6 +71,7 @@ ConstrainedWindowFrameSimple::ConstrainedWindowFrameSimple(
       0, 0, 0));
   layout->AddView(title_label_);
 
+  close_button_ = new views::ImageButton(this);
   close_button_->SetImage(views::CustomButton::BS_NORMAL,
                           rb.GetImageSkiaNamed(IDR_SHARED_IMAGES_X));
   close_button_->SetImage(views::CustomButton::BS_HOT,
@@ -85,19 +91,15 @@ ConstrainedWindowFrameSimple::ConstrainedWindowFrameSimple(
   //   view.
   // - The header insets are the distance from the window border to the header
   //   elements.
-  //
-  // The NO_ISNETS consumers draw atop the views above.
-  if (client_insets == ConstrainedWindowViews::DEFAULT_INSETS) {
-    const int kTitleBuiltinBottomPadding = 4;
-    set_border(views::Border::CreateEmptyBorder(
-        ConstrainedWindowConstants::kClientTopPadding + kHeaderTopPadding +
-            std::max(close_button_->GetPreferredSize().height(),
-                     title_label_->GetPreferredSize().height()) -
-            kTitleBuiltinBottomPadding,
-        ConstrainedWindowConstants::kHorizontalPadding,
-        ConstrainedWindowConstants::kClientBottomPadding,
-        ConstrainedWindowConstants::kHorizontalPadding));
-  }
+  const int kTitleBuiltinBottomPadding = 4;
+  set_border(views::Border::CreateEmptyBorder(
+      ConstrainedWindowConstants::kClientTopPadding + kHeaderTopPadding +
+          std::max(close_button_->GetPreferredSize().height(),
+                   title_label_->GetPreferredSize().height()) -
+          kTitleBuiltinBottomPadding,
+      ConstrainedWindowConstants::kHorizontalPadding,
+      ConstrainedWindowConstants::kClientBottomPadding,
+      ConstrainedWindowConstants::kHorizontalPadding));
 }
 
 ConstrainedWindowFrameSimple::~ConstrainedWindowFrameSimple() {
@@ -115,8 +117,8 @@ gfx::Rect ConstrainedWindowFrameSimple::GetWindowBoundsForClientBounds(
        bounds.width(),
        ConstrainedWindowConstants::kHorizontalPadding +
            2 * ConstrainedWindowConstants::kCloseButtonPadding +
-           title_label_->GetPreferredSize().width() +
-           close_button_->GetPreferredSize().width()));
+           (title_label_ ? title_label_->GetPreferredSize().width() : 0) +
+           (close_button_ ? close_button_->GetPreferredSize().width() : 0)));
   return bounds;
 }
 
@@ -152,7 +154,8 @@ void ConstrainedWindowFrameSimple::UpdateWindowIcon() {
 }
 
 void ConstrainedWindowFrameSimple::UpdateWindowTitle() {
-  title_label_->SetText(container_->widget_delegate()->GetWindowTitle());
+  if (title_label_)
+    title_label_->SetText(container_->widget_delegate()->GetWindowTitle());
 }
 
 gfx::Size ConstrainedWindowFrameSimple::GetPreferredSize() {
@@ -162,6 +165,7 @@ gfx::Size ConstrainedWindowFrameSimple::GetPreferredSize() {
 
 void ConstrainedWindowFrameSimple::ButtonPressed(views::Button* sender,
                                                  const ui::Event& event) {
+  DCHECK(close_button_);
   if (sender == close_button_)
     sender->GetWidget()->Close();
 }
