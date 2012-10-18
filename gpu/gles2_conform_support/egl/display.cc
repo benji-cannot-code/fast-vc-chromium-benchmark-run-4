090ed18b3deaf8838bf903c4484adbdc988b6a3c
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 05:15:50 +0000

    gpu: Add support for GLX_EXT_texture_from_pixmap extension.
    Implement CHROMIUM_texture_from_image. This extension behaves just like
    EXT_texture_from_pixmap but uses chromium specific image identifiers rather
    than platform specific pixmap IDs.
    
    Add IPC message for creating an image identifier using a
    gfx::PluginWindowHandle. Each GPU channel maintains a different set of
    images and deleting an image will cause the internal image representation
    to be freed once it's no longer bound to a texture.
    
    BUG=132342
    TEST=gpu_unittests --gtest_filter=TextureInfoTest.GetLevelImage:GLES2DecoderTest.BindTexImage2DCHROMIUM:GLES2DecoderTest.ReleaseTexImage2DCHROMIUM and manual
    
    Review URL: https://chromiumcodereview.appspot.com/10543125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162654 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/gles2_conform_support/egl/display.cc
+++ gpu/gles2_conform_support/egl/display.cc
@@ -114,6 +114,7 @@ EGLSurface Display::CreateWindowSurface(EGLConfig config,
 
   gpu::gles2::ContextGroup::Ref group(new gpu::gles2::ContextGroup(NULL,
                                                                    NULL,
+                                                                   NULL,
                                                                    true));
 
   decoder_.reset(gpu::gles2::GLES2Decoder::Create(group.get()));
