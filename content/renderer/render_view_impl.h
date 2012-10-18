fca898521591beae59907618858287a3bdd88e97
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 16:31:59 +0000

    Notify v8 engine of timezone changes on ChromeOS.
    BUG=152962
    
    
    Review URL: https://chromiumcodereview.appspot.com/11185027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162718 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -1033,6 +1033,7 @@ class RenderViewImpl : public RenderWidget,
   void OnThemeChanged();
   void OnUndo();
   void OnUpdateTargetURLAck();
+  void OnUpdateTimezone();
   CONTENT_EXPORT void OnUpdateWebPreferences(
       const webkit_glue::WebPreferences& prefs);
   CONTENT_EXPORT void OnUnselect();
