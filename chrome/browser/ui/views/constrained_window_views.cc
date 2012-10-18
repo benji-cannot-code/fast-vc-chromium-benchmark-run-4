5433fecaa92e5532051d6bb3cd94064dece460db
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 01:41:24 +0000

    Do not automatically create the ConstrainedWindowTabHelper.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11186033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_views.cc
+++ chrome/browser/ui/views/constrained_window_views.cc
@@ -576,7 +576,6 @@ ConstrainedWindowViews::ConstrainedWindowViews(
   params.delegate = widget_delegate;
   params.native_widget = native_constrained_window_->AsNativeWidget();
   params.child = true;
-  ConstrainedWindowTabHelper::CreateForWebContents(web_contents);
 
   if (enable_chrome_style_) {
     params.parent_widget = Widget::GetTopLevelWidgetForNativeView(
