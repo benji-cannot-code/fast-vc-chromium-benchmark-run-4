8c64e13f9b5a9752f44adaf7ac3e3c22e8c76b0f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-18 13:38:19 +0000

    [WK2] Add CustomFilterOperation serialization in ArgumentCoder. https://bugs.webkit.org/show_bug.cgi?id=98733
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-10-18
    Reviewed by Noam Rosenthal.
    
    Source/WebCore:
    
    Change CustomFilterOperation::parameters() to const function, because
    this method is a getter and a const instance needs to call this method.
    
    No new tests because there is no change in behavior.
    
    * platform/graphics/filters/CustomFilterOperation.h:
    (WebCore::CustomFilterOperation::parameters):
    
    Source/WebKit2:
    
    Add ArgumentCoders for CustomFilterOperation and all dependent classes.
    Coordinated Graphics will use this css shaders serialization when it enables CSS
    Shaders.
    
    No new tests, code path is not activated yet.
    
    * CMakeLists.txt:
    * Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.cpp: Added.
    (CoreIPC):
    (CoreIPC::::encode):
    (CoreIPC::::decode):
    * Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.h: Added.
    (WebCore):
    (CoreIPC):
    * Shared/CoordinatedGraphics/WebCustomFilterProgram.h: Added.
    (WebKit):
    (WebCustomFilterProgram):
        WebCustomFilterProgram is made to serialize the data of a
        StyleCustomFilterProgram to the UI process.
    (WebKit::WebCustomFilterProgram::create):
    (WebKit::WebCustomFilterProgram::WebCustomFilterProgram):
    * Shared/ShareableSurface.cpp:
    * Shared/WebCoreArgumentCoders.cpp:
    * Shared/WebCoreArgumentCoders.h:
    * Target.pri:
    * UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.h:
    * WebKit2.pri:
    * WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(COORDINATED_GRAPHICS)
 
 #include "BackingStore.h"
+#include "CoordinatedGraphicsArgumentCoders.h"
 #include "DrawingAreaProxy.h"
 #include "Region.h"
 #include "SurfaceUpdateInfo.h"
--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(COORDINATED_GRAPHICS)
 #include "LayerTreeCoordinator.h"
 
+#include "CoordinatedGraphicsArgumentCoders.h"
 #include "CoordinatedGraphicsLayer.h"
 #include "DrawingAreaImpl.h"
 #include "GraphicsContext.h"
