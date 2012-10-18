bdcd3f0e1c87e3c689e00f564e0a169ac9da8f2c
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 19:20:04 +0000

    Upstreaming ContentViewCore APIs for webview
    This should make upstream and downstream API compatible for webview glue layer.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11184034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162757 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -988,7 +988,7 @@ public class ContentViewCore implements MotionEventDelegate {
      * @see View#onAttachedToWindow()
      */
     @SuppressWarnings("javadoc")
-    protected void onAttachedToWindow() {
+    public void onAttachedToWindow() {
         setAccessibilityState(true);
     }
 
@@ -996,7 +996,7 @@ public class ContentViewCore implements MotionEventDelegate {
      * @see View#onDetachedFromWindow()
      */
     @SuppressWarnings("javadoc")
-    protected void onDetachedFromWindow() {
+    public void onDetachedFromWindow() {
         setAccessibilityState(false);
     }
 
@@ -1578,6 +1578,10 @@ public class ContentViewCore implements MotionEventDelegate {
         if (mNativeContentViewCore != 0) {
             nativeSetUseDesktopUserAgent(mNativeContentViewCore, override, reloadOnChange);
         }
+    }
+
+    public void clearSslPreferences() {
+        nativeClearSslPreferences(mNativeContentViewCore);
     }
 
     /**
@@ -2183,6 +2187,8 @@ public class ContentViewCore implements MotionEventDelegate {
     private native void nativeSetUseDesktopUserAgent(int nativeContentViewCoreImpl,
             boolean enabled, boolean reloadOnChange);
     private native boolean nativeGetUseDesktopUserAgent(int nativeContentViewCoreImpl);
+
+    private native void nativeClearSslPreferences(int nativeContentViewCoreImpl);
 
     private native void nativeAddJavascriptInterface(int nativeContentViewCoreImpl, Object object,
                                                      String name, boolean requireAnnotation);
