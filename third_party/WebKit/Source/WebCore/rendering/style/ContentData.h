eb852d5427e5c03a9ebea738ad591a8edf50cf62
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-18 00:59:39 +0000

    Remove StyleContentType since it's not used anymore https://bugs.webkit.org/show_bug.cgi?id=99659
    Patch by Elliott Sprehn <esprehn@chromium.org> on 2012-10-17
    Reviewed by Eric Seidel.
    
    Remove StyleContentType enum since it's not used anymore and
    make the is*() type checking methods on ContentData public.
    
    No new tests needed, this is just deleting dead code.
    
    * rendering/style/ContentData.h:
    (ContentData):
    * rendering/style/RenderStyleConstants.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131684 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/style/ContentData.h
+++ third_party/WebKit/Source/WebCore/rendering/style/ContentData.h
@@ -54,7 +54,6 @@ public:
 
     virtual RenderObject* createRenderer(Document*, RenderStyle*) const = 0;
 
-    virtual StyleContentType type() const = 0;
     virtual PassOwnPtr<ContentData> clone() const;
 
     ContentData* next() const { return m_next.get(); }
@@ -75,6 +74,7 @@ public:
     StyleImage* image() { return m_image.get(); }
     void setImage(PassRefPtr<StyleImage> image) { m_image = image; }
 
+    virtual bool isImage() const OVERRIDE { return true; }
     virtual RenderObject* createRenderer(Document*, RenderStyle*) const OVERRIDE;
 
     virtual bool equals(const ContentData& data) const OVERRIDE
@@ -90,8 +90,6 @@ private:
     {
     }
 
-    virtual StyleContentType type() const { return CONTENT_OBJECT; }
-    virtual bool isImage() const { return true; }
     virtual PassOwnPtr<ContentData> cloneInternal() const
     {
         RefPtr<StyleImage> image = const_cast<StyleImage*>(this->image());
@@ -107,6 +105,7 @@ public:
     const String& text() const { return m_text; }
     void setText(const String& text) { m_text = text; }
 
+    virtual bool isText() const OVERRIDE { return true; }
     virtual RenderObject* createRenderer(Document*, RenderStyle*) const OVERRIDE;
 
     virtual bool equals(const ContentData& data) const OVERRIDE
@@ -122,8 +121,6 @@ private:
     {
     }
 
-    virtual StyleContentType type() const { return CONTENT_TEXT; }
-    virtual bool isText() const { return true; }
     virtual PassOwnPtr<ContentData> cloneInternal() const { return create(text()); }
 
     String m_text;
@@ -135,6 +132,7 @@ public:
     const CounterContent* counter() const { return m_counter.get(); }
     void setCounter(PassOwnPtr<CounterContent> counter) { m_counter = counter; }
 
+    virtual bool isCounter() const OVERRIDE { return true; }
     virtual RenderObject* createRenderer(Document*, RenderStyle*) const OVERRIDE;
 
 private:
@@ -143,8 +141,6 @@ private:
     {
     }
 
-    virtual StyleContentType type() const { return CONTENT_COUNTER; }
-    virtual bool isCounter() const { return true; }
     virtual PassOwnPtr<ContentData> cloneInternal() const
     {
         OwnPtr<CounterContent> counterData = adoptPtr(new CounterContent(*counter()));
@@ -167,6 +163,7 @@ public:
     QuoteType quote() const { return m_quote; }
     void setQuote(QuoteType quote) { m_quote = quote; }
 
+    virtual bool isQuote() const OVERRIDE { return true; }
     virtual RenderObject* createRenderer(Document*, RenderStyle*) const OVERRIDE;
 
     virtual bool equals(const ContentData& data) const OVERRIDE
@@ -182,8 +179,6 @@ private:
     {
     }
 
-    virtual StyleContentType type() const { return CONTENT_QUOTE; }
-    virtual bool isQuote() const { return true; }
     virtual PassOwnPtr<ContentData> cloneInternal() const { return create(quote()); }
 
     QuoteType m_quote;
