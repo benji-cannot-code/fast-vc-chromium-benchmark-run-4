95f4d2f5eee627dfab6ce686449a2225c9982cc4
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 21:39:38 +0000

    [Media Gallery] When a NOTIFICATION_APP_TERMINATING notification is received, signal the |media_task_runner_| about the shutdown sequence.
    BUG=154758
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/11154014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_gallery/mtp_device_delegate_impl_linux.h
+++ chrome/browser/media_gallery/mtp_device_delegate_impl_linux.h
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "base/platform_file.h"
+#include "base/synchronization/waitable_event.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "webkit/fileapi/file_system_file_util.h"
 #include "webkit/fileapi/media/mtp_device_delegate.h"
 
@@ -23,7 +26,8 @@ namespace chrome {
 // operations. This class contains platform specific code to communicate with
 // the attached MTP storage. Instantiate this class per MTP storage.
 // This class is ref-counted, because MtpDeviceDelegate is ref-counted.
-class MtpDeviceDelegateImplLinux : public fileapi::MtpDeviceDelegate {
+class MtpDeviceDelegateImplLinux : public fileapi::MtpDeviceDelegate,
+                                   public content::NotificationObserver {
  public:
   // Constructed on UI thread. Defer the device initializations until the first
   // file operation request. Do all the initializations in LazyInit() function.
@@ -50,6 +54,11 @@ class MtpDeviceDelegateImplLinux : public fileapi::MtpDeviceDelegate {
   // Private because this class is ref-counted.
   virtual ~MtpDeviceDelegateImplLinux();
 
+  // content::NotificationObserver implementation.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
+
   // Opens the device for communication. This function is called on
   // |media_task_runner_|. Returns true if the device is ready for
   // communication, else false.
@@ -66,6 +75,18 @@ class MtpDeviceDelegateImplLinux : public fileapi::MtpDeviceDelegate {
   // Stores a reference to worker pool thread. All requests and response of file
   // operations are posted on |media_task_runner_|.
   scoped_refptr<base::SequencedTaskRunner> media_task_runner_;
+
+  // |media_task_runner_| can wait on this event until the requested task is
+  // complete.
+  base::WaitableEvent on_task_completed_event_;
+
+  // Used to notify |media_task_runner_| pending tasks about the shutdown
+  // sequence.
+  base::WaitableEvent on_shutdown_event_;
+
+  // Handles registering notifications with the NotificationService.
+  // Used to listen for application termination message.
+  content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(MtpDeviceDelegateImplLinux);
 };
