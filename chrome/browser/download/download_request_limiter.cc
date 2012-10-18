908232b28cb8c9050066a67a0a1a86ea6bff352b
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 18:12:45 +0000

    Remove TabContents from DownloadRequestLimiterTest.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11193016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_request_limiter.cc
+++ chrome/browser/download/download_request_limiter.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "chrome/browser/ui/blocked_content/blocked_content_tab_helper.h"
 #include "chrome/browser/ui/blocked_content/blocked_content_tab_helper_delegate.h"
-#include "chrome/browser/ui/tab_contents/tab_contents.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_entry.h"
@@ -62,17 +61,18 @@ DownloadRequestLimiter::TabDownloadState::~TabDownloadState() {
 
 void DownloadRequestLimiter::TabDownloadState::DidGetUserGesture() {
   if (is_showing_prompt()) {
-    // Don't change the state if the user clicks on the page some where.
+    // Don't change the state if the user clicks on the page somewhere.
     return;
   }
 
-  TabContents* tab_contents = TabContents::FromWebContents(web_contents());
-  // See PromptUserForDownload(): if there's no TabContents, then
+  InfoBarTabHelper* infobar_helper =
+      InfoBarTabHelper::FromWebContents(web_contents());
+  // See PromptUserForDownload(): if there's no InfoBarTabHelper, then
   // DOWNLOADS_NOT_ALLOWED is functionally equivalent to PROMPT_BEFORE_DOWNLOAD.
-  if ((tab_contents &&
+  if ((infobar_helper &&
        status_ != DownloadRequestLimiter::ALLOW_ALL_DOWNLOADS &&
        status_ != DownloadRequestLimiter::DOWNLOADS_NOT_ALLOWED) ||
-      (!tab_contents &&
+      (!infobar_helper &&
        status_ != DownloadRequestLimiter::ALLOW_ALL_DOWNLOADS)) {
     // Revert to default status.
     host_->Remove(this);
--- chrome/browser/download/download_request_limiter_unittest.cc
+++ chrome/browser/download/download_request_limiter_unittest.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "chrome/browser/download/download_request_limiter.h"
-#include "chrome/browser/ui/tab_contents/tab_contents.h"
-#include "chrome/browser/ui/tab_contents/test_tab_contents.h"
+#include "chrome/browser/infobars/infobar_tab_helper.h"
+#include "chrome/browser/ui/blocked_content/blocked_content_tab_helper.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/web_contents.h"
@@ -16,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::BrowserThread;
 using content::WebContents;
 
-class DownloadRequestLimiterTest : public TabContentsTestHarness {
+class DownloadRequestLimiterTest : public ChromeRenderViewHostTestHarness {
  public:
   DownloadRequestLimiterTest()
       : ui_thread_(BrowserThread::UI, &message_loop_),
@@ -26,7 +27,9 @@ class DownloadRequestLimiterTest : public TabContentsTestHarness {
   }
 
   virtual void SetUp() {
-    TabContentsTestHarness::SetUp();
+    ChromeRenderViewHostTestHarness::SetUp();
+    BlockedContentTabHelper::CreateForWebContents(web_contents());
+    InfoBarTabHelper::CreateForWebContents(web_contents());
 
     allow_download_ = true;
     ask_allow_count_ = cancel_count_ = continue_count_ = 0;
@@ -38,7 +41,7 @@ class DownloadRequestLimiterTest : public TabContentsTestHarness {
 
   virtual void TearDown() {
     UnsetDelegate();
-    TabContentsTestHarness::TearDown();
+    ChromeRenderViewHostTestHarness::TearDown();
   }
 
   virtual void UnsetDelegate() {
@@ -235,15 +238,10 @@ TEST_F(DownloadRequestLimiterTest,
 
 TEST_F(DownloadRequestLimiterTest,
        DownloadRequestLimiter_RawWebContents) {
-  // By-pass TabContentsTestHarness and use
-  // RenderViewHostTestHarness::CreateTestWebContents() directly so that there
-  // will be no TabContents for web_contents.
   scoped_ptr<WebContents> web_contents(CreateTestWebContents());
-  TabContents* tab_contents = TabContents::FromWebContents(web_contents.get());
-  ASSERT_TRUE(tab_contents == NULL);
   // DownloadRequestLimiter won't try to make an infobar if it doesn't have a
-  // TabContents, and we want to test that it will Cancel() instead of prompting
-  // when it doesn't have a TabContents, so unset the delegate.
+  // InfoBarTabHelper, and we want to test that it will Cancel() instead of
+  // prompting when it doesn't have a InfoBarTabHelper, so unset the delegate.
   UnsetDelegate();
   EXPECT_EQ(0, continue_count_);
   EXPECT_EQ(0, cancel_count_);
