fe8f4ddea4560060be878fade6222c27efcc6a98
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 22:38:04 +0000

    gfx: Transform is using its own custom function to round a float to an integer.
    Use ToRoundedInt() from safe_integer_conversions.h which also has unittests,
    so it's safer.
    
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/11183034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162816 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/transform.cc
+++ ui/gfx/transform.cc
@@ -7,18 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/point3.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/safe_integer_conversions.h"
 #include "ui/gfx/skia_util.h"
-
-namespace {
-
-static int SymmetricRound(float x) {
-  return static_cast<int>(
-    x > 0
-      ? std::floor(x + 0.5f)
-      : std::ceil(x - 0.5f));
-}
-
-} // namespace
 
 namespace gfx {
 
@@ -193,16 +183,12 @@ void Transform::TransformPointInternal(const SkMatrix44& xform,
 
 void Transform::TransformPointInternal(const SkMatrix44& xform,
                                        Point& point) const {
-  SkScalar p[4] = {
-    SkIntToScalar(point.x()),
-    SkIntToScalar(point.y()),
-    0,
-    1 };
+  SkScalar p[4] = { SkIntToScalar(point.x()),  SkIntToScalar(point.y()),
+                    0, 1 };
 
   xform.map(p);
 
-  point.SetPoint(SymmetricRound(p[0]),
-                 SymmetricRound(p[1]));
+  point.SetPoint(ToRoundedInt(p[0]), ToRoundedInt(p[1]));
 }
 
 }  // namespace gfx
