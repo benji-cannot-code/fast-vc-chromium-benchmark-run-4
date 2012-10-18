79fe8f92a9d41172804bd0e83c26516418650124
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 03:31:30 +0000

    Implement size and position support for launchWebAuthFlow.
    Also fix a bug where we would still try to close WebContents in a WebAuthFlow
    where the contents had already been destroyed.
    
    Also remove some indentity API test files, the test that used them was removed
    with r148017.
    
    BUG=132143
    R=munjal@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11186036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162639 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/identity/web_auth_flow.h
+++ chrome/browser/extensions/api/identity/web_auth_flow.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "googleurl/src/gurl.h"
+#include "ui/gfx/rect.h"
 
 class Profile;
 class TabContents;
@@ -64,7 +65,8 @@ class WebAuthFlow : public content::NotificationObserver {
               Profile* profile,
               const std::string& extension_id,
               const GURL& provider_url,
-              Mode mode);
+              Mode mode,
+              const gfx::Rect& initial_bounds);
   virtual ~WebAuthFlow();
 
   // Starts the flow.
@@ -97,6 +99,7 @@ class WebAuthFlow : public content::NotificationObserver {
   Profile* profile_;
   GURL provider_url_;
   Mode mode_;
+  gfx::Rect initial_bounds_;
   // List of valid redirect URL prefixes.
   std::vector<std::string> valid_prefixes_;
 
