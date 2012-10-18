2d565cd7d06f7db245a12c4a2a7ec065f79c06ea
mitz@apple.com <mitz@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-18 05:01:00 +0000

    REGRESSION (r95391): ComplexTextController is unnecessarily slow with characters with combining marks when the base character is not covered by any font https://bugs.webkit.org/show_bug.cgi?id=99654
    Reviewed by Adele Peterson.
    
    When the base character of a combining character sequence is not covered by any one of the
    available fonts, there is no point looking for a font that covers the entire sequence, nor
    to try to use a combination of fallback fonts for the entire sequence.
    
    * platform/graphics/SimpleFontData.h:
    (WebCore::SimpleFontData::systemFallback): Moved ComplexTextController::systemFallbackFontData
    here and renamed it.
    * platform/graphics/mac/ComplexTextController.cpp:
    (WebCore::ComplexTextController::collectComplexTextRuns): This function used to use
    systemFallbackFontData() whenever Font::fontDataForCombiningCharacterSequence returned 0
    for a base character with combining marks, regardless of whether the base character was
    covered by any font. Changed it to preserve the return value of
    fontDataForCombiningCharacterSequence, which is now 0 only if the base charcater is not in
    any font, while systemFallbackFontData() is used to indicate that no single font in the
    fallback list covers all characters in the sequence, but the base character is in some font.
    * platform/graphics/mac/ComplexTextController.h:
    (ComplexTextController): Moved systemFallbackFontData from here to SimpleFontData.
    * platform/graphics/mac/ComplexTextControllerCoreText.mm:
    (WebCore::ComplexTextController::collectComplexTextRunsForCharacters): Updated for the above
    move.
    * platform/graphics/mac/FontComplexTextMac.cpp:
    (WebCore::Font::fontDataForCombiningCharacterSequence): Changed to return
    systemFallbackFontData() if no single font in the fallback list covers all characters in the
    sequence, while using 0 to signal the base character does not exist in any font.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131703 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/mac/FontComplexTextMac.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/mac/FontComplexTextMac.cpp
@@ -177,8 +177,11 @@ const SimpleFontData* Font::fontDataForCombiningCharacterSequence(const UChar* c
 
     GlyphData baseCharacterGlyphData = glyphDataForCharacter(baseCharacter, false, variant);
 
+    if (!baseCharacterGlyphData.glyph)
+        return 0;
+
     if (length == baseCharacterLength)
-        return baseCharacterGlyphData.glyph ? baseCharacterGlyphData.fontData : 0;
+        return baseCharacterGlyphData.fontData;
 
     bool triedBaseCharacterFontData = false;
 
@@ -217,7 +220,7 @@ const SimpleFontData* Font::fontDataForCombiningCharacterSequence(const UChar* c
     if (!triedBaseCharacterFontData && baseCharacterGlyphData.fontData && baseCharacterGlyphData.fontData->canRenderCombiningCharacterSequence(characters, length))
         return baseCharacterGlyphData.fontData;
 
-    return 0;
+    return SimpleFontData::systemFallback();
 }
 
 } // namespace WebCore
