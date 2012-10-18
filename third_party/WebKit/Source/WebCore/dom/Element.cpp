bf500c7faa9a550f2f62737eaff900ffae86ed67
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-18 13:22:36 +0000

    treeScopeOfParent doesn't return the TreeScope of the parent https://bugs.webkit.org/show_bug.cgi?id=98207
    Reviewed by Hajime Morita.
    
    A minor clean up. Remove treeScopeOfParent() in Element.cpp.
    
    No tests needed, this is just a clean up.
    
    * dom/Element.cpp:
    (WebCore::Element::removedFrom):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -963,14 +963,6 @@ Node::InsertionNotificationRequest Element::insertedInto(ContainerNode* insertio
     return InsertionDone;
 }
 
-static inline TreeScope* treeScopeOfParent(Node* node, ContainerNode* insertionPoint)
-{
-    if (Node* parent = node->parentNode())
-        parent->treeScope();
-    return insertionPoint->treeScope();
-}
-
-
 void Element::removedFrom(ContainerNode* insertionPoint)
 {
 #if ENABLE(FULLSCREEN_API)
@@ -987,7 +979,7 @@ void Element::removedFrom(ContainerNode* insertionPoint)
     if (insertionPoint->inDocument()) {
         const AtomicString& idValue = getIdAttribute();
         if (!idValue.isNull() && inDocument())
-            updateId(treeScopeOfParent(this, insertionPoint), idValue, nullAtom);
+            updateId(insertionPoint->treeScope(), idValue, nullAtom);
 
         const AtomicString& nameValue = getNameAttribute();
         if (!nameValue.isNull())
