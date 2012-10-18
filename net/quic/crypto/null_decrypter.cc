b3678cd0ea09fef4cdbf2107bdce99f57b3d6772
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 21:53:49 +0000

    Trying yet again to add QuicFramer and friends.
    Includes a second attempt to fix the constants.  This time, I was able
    to set up a 32bit chroot to test the fix on since none of the bots
    seemed to do the trick.
    
    162618 - Reverted 162606
    162606 - Relanding with second fix
    162468 - Reverted 162462
    162462 - Relanding w/ fix
    162263 - Reverted 162259
    162259 - Add QuicFramer and friends.
    
    Review URL: https://codereview.chromium.org/11193036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162805 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/quic/crypto/null_decrypter.cc
@@ -0,0 +1,36 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/quic/crypto/null_decrypter.h"
+#include "net/quic/quic_utils.h"
+#include "net/quic/quic_data_reader.h"
+
+using base::StringPiece;
+using std::string;
+
+namespace net {
+
+QuicData* NullDecrypter::Decrypt(StringPiece associated_data,
+                                 StringPiece ciphertext) {
+  QuicDataReader reader(ciphertext.data(), ciphertext.length());
+
+  uint128 hash;
+  if (!reader.ReadUInt128(&hash)) {
+    return NULL;
+  }
+
+  StringPiece plaintext = reader.ReadRemainingPayload();
+
+  // TODO(rch): avoid buffer copy here
+  string buffer = associated_data.as_string();
+  plaintext.AppendToString(&buffer);
+
+  if (hash != QuicUtils::FNV1a_128_Hash(buffer.data(), buffer.length())) {
+    return NULL;
+  }
+  return new QuicData(plaintext.data(), plaintext.length());
+}
+
+}  // namespace net
