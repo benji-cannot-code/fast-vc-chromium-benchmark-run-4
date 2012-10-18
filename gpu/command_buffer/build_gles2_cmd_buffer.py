eba9102ed6fa1e784f941af0eeaf44730ff3b033
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 22:39:01 +0000

    Flush after GenXXX commands
    BUG=156146
    
    
    Review URL: https://chromiumcodereview.appspot.com/11224002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162817 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -2969,6 +2969,7 @@ class GENnHandler(TypeHandler):
   GetIdHandler(id_namespaces::k%(resource_types)s)->
       MakeIds(this, 0, %(args)s);
   helper_->%(name)sImmediate(%(args)s);
+  helper_->CommandBufferHelper::Flush();
 %(log_code)s
 }
 
--- gpu/command_buffer/client/gles2_implementation_impl_autogen.h
+++ gpu/command_buffer/client/gles2_implementation_impl_autogen.h
@@ -361,6 +361,7 @@ void GLES2Implementation::GenBuffers(GLsizei n, GLuint* buffers) {
   GetIdHandler(id_namespaces::kBuffers)->
       MakeIds(this, 0, n, buffers);
   helper_->GenBuffersImmediate(n, buffers);
+  helper_->CommandBufferHelper::Flush();
   GPU_CLIENT_LOG_CODE_BLOCK({
     for (GLsizei i = 0; i < n; ++i) {
       GPU_CLIENT_LOG("  " << i << ": " << buffers[i]);
@@ -384,6 +385,7 @@ void GLES2Implementation::GenFramebuffers(GLsizei n, GLuint* framebuffers) {
   GetIdHandler(id_namespaces::kFramebuffers)->
       MakeIds(this, 0, n, framebuffers);
   helper_->GenFramebuffersImmediate(n, framebuffers);
+  helper_->CommandBufferHelper::Flush();
   GPU_CLIENT_LOG_CODE_BLOCK({
     for (GLsizei i = 0; i < n; ++i) {
       GPU_CLIENT_LOG("  " << i << ": " << framebuffers[i]);
@@ -401,6 +403,7 @@ void GLES2Implementation::GenRenderbuffers(GLsizei n, GLuint* renderbuffers) {
   GetIdHandler(id_namespaces::kRenderbuffers)->
       MakeIds(this, 0, n, renderbuffers);
   helper_->GenRenderbuffersImmediate(n, renderbuffers);
+  helper_->CommandBufferHelper::Flush();
   GPU_CLIENT_LOG_CODE_BLOCK({
     for (GLsizei i = 0; i < n; ++i) {
       GPU_CLIENT_LOG("  " << i << ": " << renderbuffers[i]);
@@ -418,6 +421,7 @@ void GLES2Implementation::GenTextures(GLsizei n, GLuint* textures) {
   GetIdHandler(id_namespaces::kTextures)->
       MakeIds(this, 0, n, textures);
   helper_->GenTexturesImmediate(n, textures);
+  helper_->CommandBufferHelper::Flush();
   GPU_CLIENT_LOG_CODE_BLOCK({
     for (GLsizei i = 0; i < n; ++i) {
       GPU_CLIENT_LOG("  " << i << ": " << textures[i]);
@@ -1333,6 +1337,7 @@ void GLES2Implementation::GenQueriesEXT(GLsizei n, GLuint* queries) {
   GetIdHandler(id_namespaces::kQueries)->
       MakeIds(this, 0, n, queries);
   helper_->GenQueriesEXTImmediate(n, queries);
+  helper_->CommandBufferHelper::Flush();
   GPU_CLIENT_LOG_CODE_BLOCK({
     for (GLsizei i = 0; i < n; ++i) {
       GPU_CLIENT_LOG("  " << i << ": " << queries[i]);
@@ -1370,6 +1375,7 @@ void GLES2Implementation::GenVertexArraysOES(GLsizei n, GLuint* arrays) {
   GetIdHandler(id_namespaces::kVertexArrays)->
       MakeIds(this, 0, n, arrays);
   helper_->GenVertexArraysOESImmediate(n, arrays);
+  helper_->CommandBufferHelper::Flush();
   GPU_CLIENT_LOG_CODE_BLOCK({
     for (GLsizei i = 0; i < n; ++i) {
       GPU_CLIENT_LOG("  " << i << ": " << arrays[i]);
