7d21f068fbcb5e21f57fb7824cd67aa8197688da
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 03:27:05 +0000

    Use LANCZOS3 resize algorithm to generate missing image reps for extension action icons.
    BUG=155329
    TEST=manual (
    install gmail notification extension,
    verifiy that extension icon does not look too pixelated)
    
    Review URL: https://chromiumcodereview.appspot.com/11017046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/extension_action/extension_actions_api.cc
+++ chrome/browser/extensions/api/extension_action/extension_actions_api.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/render_messages.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_service.h"
+#include "ui/gfx/image/image_skia_operations.h"
 
 namespace {
 
@@ -131,7 +132,13 @@ void SetDefaultsFromValue(const base::DictionaryValue* dict,
         icon.AddRepresentation(gfx::ImageSkiaRep(bitmap, kIconSizes[i].scale));
       }
     }
-    action->SetIcon(kTabId, gfx::Image(icon));
+
+    if (!icon.isNull()) {
+      gfx::ImageSkia final_icon =
+          gfx::ImageSkiaOperations::CreateImageWithCustomResizeMethod(
+              icon, skia::ImageOperations::RESIZE_LANCZOS3);
+      action->SetIcon(kTabId, gfx::Image(final_icon));
+    }
   }
 }
 
@@ -156,11 +163,9 @@ scoped_ptr<base::DictionaryValue> DefaultsToValue(ExtensionAction* action) {
   if (!icon.isNull()) {
     base::DictionaryValue* icon_value = new base::DictionaryValue();
     for (size_t i = 0; i < arraysize(kIconSizes); i++) {
-      if (icon.HasRepresentation(kIconSizes[i].scale)) {
-        icon_value->SetString(
-            kIconSizes[i].size_string,
-            RepresentationToString(icon, kIconSizes[i].scale));
-      }
+      icon_value->SetString(
+          kIconSizes[i].size_string,
+          RepresentationToString(icon, kIconSizes[i].scale));
     }
     dict->Set(kIconStorageKey, icon_value);
   }
@@ -470,7 +475,10 @@ bool ExtensionActionSetIconFunction::RunExtensionAction() {
       }
     }
 
-    extension_action_->SetIcon(tab_id_, gfx::Image(icon));
+    gfx::ImageSkia final_icon =
+        gfx::ImageSkiaOperations::CreateImageWithCustomResizeMethod(
+            icon, skia::ImageOperations::RESIZE_LANCZOS3);
+    extension_action_->SetIcon(tab_id_, gfx::Image(final_icon));
   } else if (details_->GetInteger("iconIndex", &icon_index)) {
     // Obsolete argument: ignore it.
     return true;
