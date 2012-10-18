952abc60d998687b3903d9e39ca188ecfe14959f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 17:26:55 +0000

    ui: Build fix for gcc 4.6.3, default version of ubuntu 12.04.
    EXPECT_EQ(true, ...) or EXPECT_EQ(false, ...) cause errors like:
    
    $ converting 'true' to pointer type for argument 1 of '...'.
    
    Switch us to use EXPECT_TRUE/EXPECT_FALSE where appropriate.
    
    This came from https://codereview.chromium.org/11141010/ (r161762).
    
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/11139009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162727 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/point_unittest.cc
+++ ui/gfx/point_unittest.cc
@@ -11,22 +11,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-static int test_pointf(const gfx::PointF& p) {
+namespace {
+
+int TestPointF(const gfx::PointF& p) {
   return p.x();
 }
+
+}  // namespace
 
 TEST(PointTest, ToPointF) {
   // Check that implicit conversion from integer to float compiles.
   gfx::Point a(10, 20);
-  float x = test_pointf(a);
+  float x = TestPointF(a);
   EXPECT_EQ(x, a.x());
 
   gfx::PointF b(10, 20);
-  bool equals = a == b;
-  EXPECT_EQ(true, equals);
-
-  equals = b == a;
-  EXPECT_EQ(true, equals);
+  EXPECT_EQ(a, b);
+  EXPECT_EQ(b, a);
 }
 
 TEST(PointTest, IsOrigin) {
