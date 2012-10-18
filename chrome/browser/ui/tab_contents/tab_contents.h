c640fcd2724f5aaa600772c23c61ac9f192b9777
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 21:18:29 +0000

    Remove TabContents from WebUITest.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11183064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162791 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents.h
+++ chrome/browser/ui/tab_contents/tab_contents.h
@@ -35,7 +35,6 @@ class WebDialogWindowControllerTabContentsCreator;
 class WebIntentInlineDispositionBrowserTest;
 class WebIntentPickerCocoa;
 class WebIntentPickerGtk;
-class WebUITestContentsCreator;
 
 namespace chromeos {
 class SimpleWebViewDialog;
@@ -101,7 +100,6 @@ class TabContents : public content::WebContentsObserver {
     friend class WebIntentInlineDispositionBrowserTest;
     friend class WebIntentPickerCocoa;
     friend class WebIntentPickerGtk;
-    friend class WebUITestContentsCreator;
     FRIEND_TEST_ALL_PREFIXES(SessionRestoreTest, SessionStorageAfterTabReplace);
 
     static TabContents* CreateTabContents(content::WebContents* contents);
