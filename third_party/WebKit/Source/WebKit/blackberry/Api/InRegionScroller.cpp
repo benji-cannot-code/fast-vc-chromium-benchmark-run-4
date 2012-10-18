f0de03777c17635d14651b6b9e16ed6535c2f5ff
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-18 19:53:38 +0000

    Crash in InRegionScrollableArea constructor while loading page https://bugs.webkit.org/show_bug.cgi?id=99733
    Patch by John Griggs <jgriggs@rim.com> on 2012-10-18
    Reviewed by Antonio Gomes.
    
    Check scrollLayer and avoid constructing InRegionScrollableArea when it is NULL.
    
    * Api/InRegionScroller.cpp:
    (BlackBerry::WebKit::InRegionScrollerPrivate::calculateInRegionScrollableAreasForPoint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131793 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/InRegionScroller.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/InRegionScroller.cpp
@@ -220,6 +220,9 @@ void InRegionScrollerPrivate::calculateInRegionScrollableAreasForPoint(const Web
                     return;
                 }
 
+                if (!renderView->compositor()->scrollLayer())
+                    continue;
+
                 if (canScrollInnerFrame(view->frame())) {
                     pushBackInRegionScrollable(new InRegionScrollableArea(m_webPage, layer));
                     continue;
