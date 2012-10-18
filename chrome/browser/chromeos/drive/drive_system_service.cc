e82b707c0b8fb9bd422082f63f8f8718028f9620
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 18:34:32 +0000

    Revert 162725 - drive: Don't create DriveSystemService if Drive is disabled
    There is no need to create DriveSystemService if Drive is disabled.
    Note that DriveSystemServiceFactory::GetForProfile() returns NULL
    for incognito window and guest login, so the behavior of returning
    NULL is consistent with these cases.
    
    BUG=154144
    TEST=Drive works as before. Drive is disabled if --disable-gdata is specified.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11198059
    
    TBR=satorux@chromium.org
    BUG=156653
    Review URL: https://codereview.chromium.org/11189065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162744 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_system_service.cc
+++ chrome/browser/chromeos/drive/drive_system_service.cc
@@ -43,22 +43,6 @@ namespace {
 DriveServiceInterface* g_test_drive_service = NULL;
 const std::string* g_test_cache_root = NULL;
 
-// Returns true if Drive is enabled for the given Profile.
-bool IsDriveEnabledForProfile(Profile* profile) {
-  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-
-  if (!gdata::AuthService::CanAuthenticate(profile))
-    return false;
-
-  // Disable Drive if preference is set.  This can happen with commandline flag
-  // --disable-gdata or enterprise policy, or probably with user settings too
-  // in the future.
-  if (profile->GetPrefs()->GetBoolean(prefs::kDisableGData))
-    return false;
-
-  return true;
-}
-
 }  // namespace
 
 DriveSystemService::DriveSystemService(Profile* profile)
@@ -131,7 +115,13 @@ void DriveSystemService::Shutdown() {
 bool DriveSystemService::IsDriveEnabled() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
-  if (!IsDriveEnabledForProfile(profile_))
+  if (!gdata::AuthService::CanAuthenticate(profile_))
+    return false;
+
+  // Disable Drive if preference is set.  This can happen with commandline flag
+  // --disable-gdata or enterprise policy, or probably with user settings too
+  // in the future.
+  if (profile_->GetPrefs()->GetBoolean(prefs::kDisableGData))
     return false;
 
   // Drive may be disabled for cache initialization failure, etc.
@@ -168,6 +158,9 @@ void DriveSystemService::AddBackDriveMountPoint(
 
 void DriveSystemService::AddDriveMountPoint() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+  if (!IsDriveEnabled())
+    return;
 
   const FilePath mount_point = util::GetDriveMountPointPath();
   fileapi::ExternalFileSystemMountPointProvider* provider =
@@ -238,23 +231,15 @@ void DriveSystemService::DisableDrive() {
 // static
 DriveSystemService* DriveSystemServiceFactory::GetForProfile(
     Profile* profile) {
-  DriveSystemService* service = static_cast<DriveSystemService*>(
+  return static_cast<DriveSystemService*>(
       GetInstance()->GetServiceForProfile(profile, true));
-  if (service && !service->IsDriveEnabled())
-    return NULL;
-
-  return service;
 }
 
 // static
 DriveSystemService* DriveSystemServiceFactory::FindForProfile(
     Profile* profile) {
-  DriveSystemService* service = static_cast<DriveSystemService*>(
+  return static_cast<DriveSystemService*>(
       GetInstance()->GetServiceForProfile(profile, false));
-  if (service && !service->IsDriveEnabled())
-    return NULL;
-
-  return service;
 }
 
 // static
@@ -289,9 +274,6 @@ void DriveSystemServiceFactory::set_cache_root_for_test(
 
 ProfileKeyedService* DriveSystemServiceFactory::BuildServiceInstanceFor(
     Profile* profile) const {
-  if (!IsDriveEnabledForProfile(profile))
-    return NULL;
-
   DriveSystemService* service = new DriveSystemService(profile);
 
   DriveServiceInterface* drive_service = g_test_drive_service;
--- chrome/browser/chromeos/extensions/file_browser_private_api.cc
+++ chrome/browser/chromeos/extensions/file_browser_private_api.cc
@@ -524,7 +524,7 @@ void RequestLocalFileSystemFunction::RespondSuccessOnUIThread(
   // proper authentication.
   drive::DriveSystemService* system_service =
       drive::DriveSystemServiceFactory::GetForProfile(profile_);
-  if (system_service)
+  if (system_service && system_service->IsDriveEnabled())
     AddDriveMountPoint(profile_, extension_id(), render_view_host());
   DictionaryValue* dict = new DictionaryValue();
   SetResult(dict);
@@ -2029,7 +2029,8 @@ bool FileDialogStringsFunction::RunImpl() {
 
   drive::DriveSystemService* system_service =
       drive::DriveSystemServiceFactory::GetForProfile(profile_);
-  dict->SetBoolean("ENABLE_GDATA", system_service != NULL);
+  dict->SetBoolean("ENABLE_GDATA",
+                   system_service && system_service->IsDriveEnabled());
 
 #if defined(USE_ASH)
   dict->SetBoolean("ASH", true);
@@ -2603,7 +2604,7 @@ bool GetPreferencesFunction::RunImpl() {
 
   drive::DriveSystemService* system_service =
       drive::DriveSystemServiceFactory::GetForProfile(profile_);
-  bool drive_enabled = (system_service != NULL);
+  bool drive_enabled = (system_service && system_service->IsDriveEnabled());
 
   if (drive_enabled)
     AddDriveMountPoint(profile_, extension_id(), render_view_host());
