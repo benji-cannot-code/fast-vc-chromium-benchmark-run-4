5e3ce32f2ee2d98c6fa000b212580b14bb8cc9e6
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-18 12:16:46 +0000

    [GTK] Don't use the C API internally in WebKitHitTestResult https://bugs.webkit.org/show_bug.cgi?id=96779
    Reviewed by Xan Lopez.
    
    Using the C++ classes directly instead of the C API wrappers we
    avoid a lot of toImpl/toAPI casts, string conversions and
    allocations. The code is also a lot simpler and easier to read.
    
    * UIProcess/API/gtk/WebKitHitTestResult.cpp:
    (webkitHitTestResultCreate):
    (webkitHitTestResultCompare):
    * UIProcess/API/gtk/WebKitHitTestResultPrivate.h:
    * UIProcess/API/gtk/WebKitWebView.cpp:
    (webkitWebViewMouseTargetChanged):
    (webkitWebViewPopulateContextMenu):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitHitTestResultPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitHitTestResultPrivate.h
@@ -24,7 +24,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitHitTestResult.h"
 #include "WebKitPrivate.h"
 
-WebKitHitTestResult* webkitHitTestResultCreate(WKHitTestResultRef);
-bool webkitHitTestResultCompare(WebKitHitTestResult*, WKHitTestResultRef);
+using namespace WebKit;
+
+WebKitHitTestResult* webkitHitTestResultCreate(WebHitTestResult*);
+bool webkitHitTestResultCompare(WebKitHitTestResult*, WebHitTestResult*);
 
 #endif // WebKitHitTestResultPrivate_h
