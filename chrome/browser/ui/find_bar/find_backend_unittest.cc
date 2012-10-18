2af1a438368136f733f9fac9915a207c4560d7ec
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 19:21:12 +0000

    Remove TabContents from FindBackendTest.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11198062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/find_bar/find_backend_unittest.cc
+++ chrome/browser/ui/find_bar/find_backend_unittest.cc
@@ -9,9 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/find_bar/find_bar_state.h"
 #include "chrome/browser/ui/find_bar/find_bar_state_factory.h"
 #include "chrome/browser/ui/find_bar/find_tab_helper.h"
-#include "chrome/browser/ui/tab_contents/tab_contents.h"
-#include "chrome/browser/ui/tab_contents/test_tab_contents.h"
 #include "chrome/common/url_constants.h"
+#include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/test/test_browser_thread.h"
@@ -21,13 +20,18 @@ using content::BrowserThread;
 using content::WebContents;
 using content::WebContentsTester;
 
-class FindBackendTest : public TabContentsTestHarness {
+class FindBackendTest : public ChromeRenderViewHostTestHarness {
  public:
   FindBackendTest()
-      : TabContentsTestHarness(),
+      : ChromeRenderViewHostTestHarness(),
         browser_thread_(BrowserThread::UI, &message_loop_) {}
 
  private:
+  virtual void SetUp() OVERRIDE {
+    ChromeRenderViewHostTestHarness::SetUp();
+    FindTabHelper::CreateForWebContents(web_contents());
+  }
+
   content::TestBrowserThread browser_thread_;
 };
 
@@ -44,7 +48,7 @@ string16 FindPrepopulateText(WebContents* contents) {
 // tests the internal state for find_text and find_prepopulate_text.
 TEST_F(FindBackendTest, InternalState) {
   FindTabHelper* find_tab_helper =
-      FindTabHelper::FromWebContents(tab_contents()->web_contents());
+      FindTabHelper::FromWebContents(web_contents());
   // Initial state for the WebContents is blank strings.
   EXPECT_EQ(string16(), FindPrepopulateText(contents()));
   EXPECT_EQ(string16(), find_tab_helper->find_text());
