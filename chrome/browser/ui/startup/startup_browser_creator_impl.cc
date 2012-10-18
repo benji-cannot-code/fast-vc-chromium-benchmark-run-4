62e820965d8d07d4f5dc18746282e0f49fc310de
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 04:49:18 +0000

    Revert "Fix for out-of-sync scale factor issue"
    Reverting to create another CL that has #ifdef USE_ASH guard to merge to 23.
    
    TBR=oshima@chromium.org
    BUG=155201, 152728
    TEST=none
    
    Review URL: https://codereview.chromium.org/11191034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -98,8 +98,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_ASH)
 #include "ash/launcher/launcher_types.h"
-#include "ash/shell.h"
-#include "ui/aura/window.h"
 #endif
 
 #if defined(OS_MACOSX)
@@ -811,20 +809,6 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(Browser* browser,
     gtk_util::SetWMLastUserActionTime(browser->window()->GetNativeWindow());
 #endif
   }
-
-#if defined(USE_ASH)
-  if (ash::Shell::HasInstance()) {
-    // Set the browser's root window to be an active root window now so
-    // that that web contents can determine correct scale factor for the
-    // renderer. This is a short term fix for crbug.com/155201.  Without
-    // this, the renderer may use wrong scale factor first, then
-    // switched to the correct scale factor, which can cause race
-    // condition and lead to the results rendered at wrong scale factor.
-    // Long term fix is tracked in crbug.com/15543.
-    ash::Shell::GetInstance()->set_active_root_window(
-        browser->window()->GetNativeWindow()->GetRootWindow());
-  }
-#endif
 
   // In kiosk mode, we want to always be fullscreen, so switch to that now.
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kKioskMode))
