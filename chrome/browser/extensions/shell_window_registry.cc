6ffa3bcbf4ad5af2438d63a9222d7a4ded692d86
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-18 20:19:31 +0000

    Create a ShellWindowRegistry in incognito mode.
    This registry is specific to the incognito profile in use. Without this apps cannot be used in incognito mode.
    
    BUG=155130
    
    
    Review URL: https://chromiumcodereview.appspot.com/11171003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/shell_window_registry.cc
+++ chrome/browser/extensions/shell_window_registry.cc
@@ -113,6 +113,10 @@ ProfileKeyedService* ShellWindowRegistry::Factory::BuildServiceInstanceFor(
   return new ShellWindowRegistry();
 }
 
+bool ShellWindowRegistry::Factory::ServiceHasOwnInstanceInIncognito() const {
+  return true;
+}
+
 bool ShellWindowRegistry::Factory::ServiceIsCreatedWithProfile() const {
   return true;
 }
