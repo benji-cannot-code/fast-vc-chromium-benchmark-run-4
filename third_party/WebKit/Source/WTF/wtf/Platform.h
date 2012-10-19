da20b24cb34e9205231debac8a6ff3152a907301
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-19 14:06:51 +0000

    Unreviewed buildfix, use C-style comment instead of C++
    * wtf/Platform.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -908,8 +908,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_LLINT 0
 #endif
 
-// REGRESSION(r131822): It made 500+ tests crash on 32 bit platforms
-// https://bugs.webkit.org/show_bug.cgi?id=99814
+/* REGRESSION(r131822): It made 500+ tests crash on 32 bit platforms
+ * https://bugs.webkit.org/show_bug.cgi?id=99814 */
 #if (PLATFORM(QT) && CPU(X86))
 #define ENABLE_LLINT 0
 #endif
