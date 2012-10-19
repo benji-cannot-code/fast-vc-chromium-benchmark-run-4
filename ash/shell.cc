6660165d6b6095377a8610496c55abc271965bb0
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 03:22:22 +0000

    Ash: fix a crash with MetricsLogs accessing a deleted AshScreen pointer.
    Make the AshScreen* screen_ a leaky object and annotate it as such to
    suppress valgrind warnings.
    
    The rationale is that it is a small structure and it is only leaked at
    shutdown anyway.
    
    BUG=156466
    TEST=CQ
    
    
    Review URL: https://chromiumcodereview.appspot.com/11193044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162904 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell.cc
+++ ash/shell.cc
@@ -63,6 +63,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/workspace_controller.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/leak_annotations.h"
 #include "ui/aura/client/aura_constants.h"
 #include "ui/aura/client/user_action_client.h"
 #include "ui/aura/display_manager.h"
@@ -189,8 +190,9 @@ Shell::Shell(ShellDelegate* delegate)
 #endif  // defined(OS_CHROMEOS)
       browser_context_(NULL),
       simulate_modal_window_open_for_testing_(false) {
-  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, screen_.get());
-  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_ALTERNATE, screen_.get());
+  ANNOTATE_LEAKING_OBJECT_PTR(screen_);  // see crbug.com/156466
+  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_NATIVE, screen_);
+  gfx::Screen::SetScreenInstance(gfx::SCREEN_TYPE_ALTERNATE, screen_);
   ui_controls::InstallUIControlsAura(internal::CreateUIControls());
 #if defined(OS_CHROMEOS)
   content::GpuFeatureType blacklisted_features =
