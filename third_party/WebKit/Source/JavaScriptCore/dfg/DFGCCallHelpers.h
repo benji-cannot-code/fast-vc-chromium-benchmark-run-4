d9633b242c9b3429b0f9ce1008650b186bfb3949
yuqiang.xian@intel.com <yuqiang.xian@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-19 06:23:09 +0000

    [Qt] REGRESSION(r131858): It broke the ARM build https://bugs.webkit.org/show_bug.cgi?id=99809
    Reviewed by Csaba Osztrogonác.
    
    * dfg/DFGCCallHelpers.h:
    (CCallHelpers):
    (JSC::DFG::CCallHelpers::setupArgumentsWithExecState):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131860 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGCCallHelpers.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGCCallHelpers.h
@@ -552,7 +552,7 @@ public:
         move(arg2, GPRInfo::argumentGPR2);
         move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
     }
-    
+#if CPU(X86_64)    
     ALWAYS_INLINE void setupArgumentsWithExecState(GPRReg arg1, TrustedImm64 arg2)
     {
         move(arg1, GPRInfo::argumentGPR1);
@@ -560,6 +560,13 @@ public:
         move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
     }
     
+    ALWAYS_INLINE void setupArgumentsWithExecState(TrustedImm64 arg1, GPRReg arg2)
+    {
+        move(arg2, GPRInfo::argumentGPR2); // Move this first, so setting arg1 does not trample!
+        move(arg1, GPRInfo::argumentGPR1);
+        move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
+    }
+#endif
     ALWAYS_INLINE void setupArgumentsWithExecState(GPRReg arg1, TrustedImm32 arg2)
     {
         move(arg1, GPRInfo::argumentGPR1);
@@ -581,13 +588,6 @@ public:
         move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
     }
     
-    ALWAYS_INLINE void setupArgumentsWithExecState(TrustedImm64 arg1, GPRReg arg2)
-    {
-        move(arg2, GPRInfo::argumentGPR2); // Move this first, so setting arg1 does not trample!
-        move(arg1, GPRInfo::argumentGPR1);
-        move(GPRInfo::callFrameRegister, GPRInfo::argumentGPR0);
-    }
-
     ALWAYS_INLINE void setupArgumentsWithExecState(TrustedImm32 arg1, GPRReg arg2)
     {
         move(arg2, GPRInfo::argumentGPR2); // Move this first, so setting arg1 does not trample!
