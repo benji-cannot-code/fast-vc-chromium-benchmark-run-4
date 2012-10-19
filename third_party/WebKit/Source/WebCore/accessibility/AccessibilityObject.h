8d54bb0c3f9dfa826ef260fdb4b2f157ec50d9f2
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-19 17:16:19 +0000

    AX: aria-hidden=false does not work as expected https://bugs.webkit.org/show_bug.cgi?id=98787
    Reviewed by Beth Dakin.
    
    Source/WebCore: 
    
    ARIA requires that aria-hidden=false override an element's native visibility and include that
    node in the AX hierarchy.
     
    To accomplish this we have to allow invisible items to be included, as well as items that
    have no renderers associated with them.
    
    Test: accessibility/aria-hidden-negates-no-visibility.html
    
    * accessibility/AXObjectCache.cpp:
    (WebCore::AXObjectCache::getOrCreate):
    * accessibility/AccessibilityARIAGrid.cpp:
    (WebCore::AccessibilityARIAGrid::addTableCellChild):
    (WebCore::AccessibilityARIAGrid::addChildren):
    * accessibility/AccessibilityARIAGrid.h:
    (AccessibilityARIAGrid):
    * accessibility/AccessibilityNodeObject.cpp:
    (WebCore):
    (WebCore::AccessibilityNodeObject::boundingBoxRect):
    (WebCore::AccessibilityNodeObject::insertChild):
    (WebCore::AccessibilityNodeObject::addChild):
    (WebCore::AccessibilityNodeObject::addChildren):
    (WebCore::AccessibilityNodeObject::textUnderElement):
    * accessibility/AccessibilityNodeObject.h:
    (AccessibilityNodeObject):
    * accessibility/AccessibilityObject.cpp:
    (WebCore::AccessibilityObject::textIteratorBehaviorForTextRange):
    (WebCore):
    * accessibility/AccessibilityObject.h:
    (AccessibilityObject):
    (WebCore::AccessibilityObject::addChild):
    (WebCore::AccessibilityObject::insertChild):
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore):
    (WebCore::AccessibilityRenderObject::accessibilityIsIgnoredBase):
    (WebCore::AccessibilityRenderObject::addHiddenChildren):
    (WebCore::AccessibilityRenderObject::addChildren):
    * accessibility/AccessibilityRenderObject.h:
    (AccessibilityRenderObject):
    
    LayoutTests: 
    
    * accessibility/aria-hidden-negates-no-visibility.html: Added.
    * platform/mac/accessibility/aria-hidden-negates-no-visibility-expected.txt: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131915 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.h
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FloatQuad.h"
 #include "FractionalLayoutRect.h"
 #include "LayoutTypes.h"
+#include "TextIterator.h"
 #include "VisiblePosition.h"
 #include "VisibleSelection.h"
 #include <wtf/Forward.h>
@@ -574,6 +575,7 @@ public:
     virtual IntPoint clickPoint();
     static IntRect boundingBoxForQuads(RenderObject*, const Vector<FloatQuad>&);
     
+    TextIteratorBehavior textIteratorBehaviorForTextRange() const;
     virtual PlainTextRange selectedTextRange() const { return PlainTextRange(); }
     unsigned selectionStart() const { return selectedTextRange().start; }
     unsigned selectionEnd() const { return selectedTextRange().length; }
@@ -613,6 +615,9 @@ public:
     virtual void updateAccessibilityRole() { }
     const AccessibilityChildrenVector& children();
     virtual void addChildren() { }
+    virtual void addChild(AccessibilityObject*) { }
+    virtual void insertChild(AccessibilityObject*, unsigned) { }
+    
     virtual bool canHaveChildren() const { return true; }
     virtual bool hasChildren() const { return m_haveChildren; }
     virtual void updateChildrenIfNecessary();
