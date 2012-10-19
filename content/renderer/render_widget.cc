8d2c349713bf76aa61bfbc673418869df1dfc7ea
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 01:07:02 +0000

    Revert 162559 - [content shell] implement testRunner.display
    For layout tests, we only generate frames at defined points (when the test finishes, and when e.g. testRunner.display() is invoked). For each frame, we must only paint the union of all invalidated rects since the last frame.
    
    To achieve this, we forward paint related callbacks from the WebWidgetClient interface to the RenderViewObserver.
    
    With that WebKitTestRunner can mimick the exact behavior of DRT.
    
    BUG=111316,156762
    TEST=repaint tests pass
    
    Review URL: https://codereview.chromium.org/11187020
    
    TBR=jochen@chromium.org
    Review URL: https://codereview.chromium.org/11194074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162878 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -1060,8 +1060,6 @@ void RenderWidget::DoDeferredUpdate() {
 // WebWidgetClient
 
 void RenderWidget::didInvalidateRect(const WebRect& rect) {
-  DidInvalidateRect(rect);
-
   // The invalidated rect might be outside the bounds of the view.
   gfx::Rect view_rect(size_);
   gfx::Rect damaged_rect = view_rect.Intersect(rect);
@@ -1097,8 +1095,6 @@ void RenderWidget::didInvalidateRect(const WebRect& rect) {
 }
 
 void RenderWidget::didScrollRect(int dx, int dy, const WebRect& clip_rect) {
-  DidScrollRect(dx, dy, clip_rect);
-
   // Drop scrolls on the floor when we are in compositing mode.
   // TODO(nduca): stop WebViewImpl from sending scrolls in the first place.
   if (is_accelerated_compositing_active_)
@@ -1237,8 +1233,6 @@ void RenderWidget::didCompleteSwapBuffers() {
 }
 
 void RenderWidget::scheduleComposite() {
-  DidRequestScheduleComposite();
-
   if (WebWidgetHandlesCompositorScheduling()) {
     webwidget_->composite(false);
   } else {
@@ -1253,8 +1247,6 @@ void RenderWidget::scheduleComposite() {
 }
 
 void RenderWidget::scheduleAnimation() {
-  DidRequestScheduleAnimation();
-
   if (animation_update_pending_)
     return;
 
