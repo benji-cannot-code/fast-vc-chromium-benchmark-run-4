7fab46062c903d6b73a3587e6519d589b73425f0
trchen@chromium.org <trchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 20:37:51 +0000

    Disable disambiguation popup handler for non-Android targets
    The handler can cause gesture handling regressions for targets that haven't
    implemented browser-side handling yet. Currently the only platform supporting
    it is Android.
    
    BUG=156702
    
    
    Review URL: https://chromiumcodereview.appspot.com/11189079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163049 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -6392,6 +6392,7 @@ void RenderViewImpl::OnUpdatedFrameTree(
   updating_frame_tree_ = false;
 }
 
+#if defined(OS_ANDROID)
 bool RenderViewImpl::didTapMultipleTargets(
     const WebKit::WebGestureEvent& event,
     const WebVector<WebRect>& target_rects) {
@@ -6428,6 +6429,7 @@ bool RenderViewImpl::didTapMultipleTargets(
 
   return true;
 }
+#endif
 
 void RenderViewImpl::OnReleaseDisambiguationPopupDIB(
     TransportDIB::Handle dib_handle) {
