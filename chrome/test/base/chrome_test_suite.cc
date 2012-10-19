7844fbcfcf26a43ac2459d9d6cf9580ba76bf7d9
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 00:39:18 +0000

    Changes toward ability to run Chrome unit tests for iOS.
    Make parts of the chrome/ test infrastructure work on iOS.
    
    Review URL: https://chromiumcodereview.appspot.com/11098044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162863 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/chrome_test_suite.cc
+++ chrome/test/base/chrome_test_suite.cc
@@ -39,9 +39,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_MACOSX)
 #include "base/mac/bundle_locations.h"
-#include "base/mac/mac_util.h"
 #include "base/mac/scoped_nsautorelease_pool.h"
+#if !defined(OS_IOS)
+#include "base/mac/mac_util.h"
 #include "chrome/browser/chrome_browser_application_mac.h"
+#endif  // !defined(OS_IOS)
 #endif
 
 #if defined(OS_POSIX)
@@ -185,7 +187,9 @@ ChromeTestSuite::~ChromeTestSuite() {
 void ChromeTestSuite::Initialize() {
 #if defined(OS_MACOSX)
   base::mac::ScopedNSAutoreleasePool autorelease_pool;
+#if !defined(OS_IOS)
   chrome_browser_application_mac::RegisterBrowserCrApp();
+#endif  // !defined(OS_IOS)
 #endif
 
 #if defined(OS_ANDROID)
@@ -209,7 +213,7 @@ void ChromeTestSuite::Initialize() {
   // values for DIR_EXE and DIR_MODULE.
   content::ContentTestSuiteBase::Initialize();
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) && !defined(OS_IOS)
   // Look in the framework bundle for resources.
   FilePath path;
   PathService::Get(base::DIR_EXE, &path);
@@ -245,7 +249,7 @@ content::ContentClient* ChromeTestSuite::CreateClientForInitialization() {
 void ChromeTestSuite::Shutdown() {
   ResourceBundle::CleanupSharedInstance();
 
-#if defined(OS_MACOSX)
+#if defined(OS_MACOSX) && !defined(OS_IOS)
   base::mac::SetOverrideFrameworkBundle(NULL);
 #endif
 
--- chrome/test/base/run_all_unittests.cc
+++ chrome/test/base/run_all_unittests.cc
@@ -3,9 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/test/main_hook.h"
 #include "chrome/test/base/chrome_test_suite.h"
 #include "content/public/test/unittest_test_suite.h"
 
 int main(int argc, char **argv) {
+  MainHook hook(main, argc, argv);
   return content::UnitTestTestSuite(new ChromeTestSuite(argc, argv)).Run();
 }
