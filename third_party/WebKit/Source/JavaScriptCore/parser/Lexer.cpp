b205068dffc62937a7aa9732b0bcda53b59862d5
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-19 23:23:34 +0000

    Lexer should create 8 bit Identifiers for RegularExpressions and ASCII identifiers https://bugs.webkit.org/show_bug.cgi?id=99855
    Reviewed by Filip Pizlo.
    
    Added makeIdentifier helpers that will always make an 8 bit Identifier or make an
    Identifier that is the same size as the template parameter.  Used the first in the fast
    path when looking for a JS identifier and the second when scanning regular expressions.
    
    * parser/Lexer.cpp:
    (JSC::::scanRegExp):
    * parser/Lexer.h:
    (Lexer):
    (JSC::::makeIdentifierSameType):
    (JSC::::makeLCharIdentifier):
    (JSC::::lexExpectIdentifier):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/parser/Lexer.cpp
+++ third_party/WebKit/Source/JavaScriptCore/parser/Lexer.cpp
@@ -1618,7 +1618,7 @@ bool Lexer<T>::scanRegExp(const Identifier*& pattern, const Identifier*& flags, 
         }
     }
 
-    pattern = makeIdentifier(m_buffer16.data(), m_buffer16.size());
+    pattern = makeIdentifierSameType(m_buffer16.data(), m_buffer16.size());
     m_buffer16.resize(0);
 
     while (isIdentPart(m_current)) {
@@ -1626,7 +1626,7 @@ bool Lexer<T>::scanRegExp(const Identifier*& pattern, const Identifier*& flags, 
         shift();
     }
 
-    flags = makeIdentifier(m_buffer16.data(), m_buffer16.size());
+    flags = makeIdentifierSameType(m_buffer16.data(), m_buffer16.size());
     m_buffer16.resize(0);
 
     return true;
