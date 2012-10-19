e1a6e54b755ac73bf076e2d7fa84fb7dff407f2c
mihaip@chromium.org <mihaip@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 05:35:27 +0000

    Rename the window onClose event to be onClosed, which is more consistent with other event names.
    R=miket@chromium.org
    
    Review URL: https://codereview.chromium.org/11186053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/app_window_custom_bindings.js
+++ chrome/renderer/resources/extensions/app_window_custom_bindings.js
@@ -50,7 +50,7 @@ chromeHidden.registerCustomHook('app.window', function(bindingsAPI) {
   chromeHidden.OnAppWindowClosed = function() {
     if (!chromeHidden.currentAppWindow)
       return;
-    chromeHidden.currentAppWindow.onClose.dispatch();
+    chromeHidden.currentAppWindow.onClosed.dispatch();
   }
 
   // This is an internal function, but needs to be bound with setHandleRequest
@@ -64,7 +64,7 @@ chromeHidden.registerCustomHook('app.window', function(bindingsAPI) {
     AppWindow.prototype.moveTo = window.moveTo.bind(window);
     AppWindow.prototype.resizeTo = window.resizeTo.bind(window);
     AppWindow.prototype.contentWindow = window;
-    AppWindow.prototype.onClose = new chrome.Event;
+    AppWindow.prototype.onClosed = new chrome.Event;
     AppWindow.prototype.close = function() {
       this.contentWindow.close();
     };
