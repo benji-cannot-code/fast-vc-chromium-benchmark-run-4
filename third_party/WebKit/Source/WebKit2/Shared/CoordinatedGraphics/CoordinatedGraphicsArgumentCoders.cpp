f8ae588f1a22050387dea25442ef8a793e7db22f
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-19 09:43:24 +0000

    Build fix after r131741 for !ENABLE(CSS_SHADERS) platforms. https://bugs.webkit.org/show_bug.cgi?id=99775
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-10-19
    Reviewed by Csaba Osztrogonác.
    
    A class in WebKit namespace is only used when ENABLE(CSS_SHADERS), so we
    must guard "using namespace WebKit" by ENABLE(CSS_SHADERS). Otherwise,
    it brokes the Qt --minimal build.
    
    * Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.cpp:
    (CoreIPC::::decode):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131878 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.cpp
@@ -64,7 +64,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 using namespace WebCore;
+#if ENABLE(CSS_SHADERS)
 using namespace WebKit;
+#endif
 
 namespace CoreIPC {
 
