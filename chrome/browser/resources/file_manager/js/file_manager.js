dcb3ac7b6c5a54e5dca224a4c610572c51ff6361
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 05:04:57 +0000

    File Manager: make "date modified" column depending on the 12/24 hour setting.
    BUG=154730
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/11137032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@162937 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -382,13 +382,7 @@ FileManager.prototype = {
        return this.metadataCache_.isInitialized();
     }.bind(this));
 
-    this.dateFormatter_ = v8Intl.DateTimeFormat(
-        [] /* default locale */,
-        {year: 'numeric', month: 'short', day: 'numeric',
-         hour: 'numeric', minute: 'numeric', hour12: true});
-    this.timeFormatter_ = v8Intl.DateTimeFormat(
-        [] /* default locale */,
-        {hour: 'numeric', minute: 'numeric', hour12: true});
+    this.initDateTimeFormatters_();
 
     this.collator_ = v8Intl.Collator([], {numeric: true, sensitivity: 'base'});
 
@@ -467,6 +461,19 @@ FileManager.prototype = {
 
     metrics.recordInterval('Load.DOM');
     metrics.recordInterval('Load.Total');
+  };
+
+  FileManager.prototype.initDateTimeFormatters_ = function() {
+    var use12hourClock = !this.preferences_['use24hourClock'];
+    this.dateFormatter_ = v8Intl.DateTimeFormat(
+        [] /* default locale */,
+        {year: 'numeric', month: 'short', day: 'numeric',
+         hour: 'numeric', minute: 'numeric',
+         hour12: use12hourClock});
+    this.timeFormatter_ = v8Intl.DateTimeFormat(
+        [] /* default locale */,
+        {hour: 'numeric', minute: 'numeric',
+         hour12: use12hourClock});
   };
 
   FileManager.prototype.initDataTransferOperations_ = function() {
@@ -2474,6 +2481,9 @@ FileManager.prototype = {
     this.updateNetworkStateAndPreferences_(function() {
       var gdata = self.preferences_;
       var network = self.networkState_;
+
+      self.initDateTimeFormatters_();
+      self.refreshCurrentDirectoryMetadata_();
 
       self.directoryModel_.setGDataEnabled(self.isGDataEnabled());
       self.directoryModel_.setOffline(!network.online);
