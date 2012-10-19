90b826221a0f2af60d761c238e86568a051acf3f
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-19 22:49:42 +0000

    Add checks to resource update creation and image upload.
    Add instrumentation to capture input in mini-dumps, in case the crashes observed don't correspond to violations of the added checks.
    
    BUG=156107
    
    
    Review URL: https://chromiumcodereview.appspot.com/11193054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163085 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resource_update.cc
+++ cc/resource_update.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/resource_update.h"
 
+#include "base/logging.h"
+
 namespace cc {
 
 ResourceUpdate ResourceUpdate::Create(CCPrioritizedTexture* texture,
@@ -14,6 +16,7 @@ ResourceUpdate ResourceUpdate::Create(CCPrioritizedTexture* texture,
                                       IntRect content_rect,
                                       IntRect source_rect,
                                       IntSize dest_offset) {
+    CHECK(content_rect.contains(source_rect));
     ResourceUpdate update;
     update.texture = texture;
     update.bitmap = bitmap;
@@ -28,6 +31,7 @@ ResourceUpdate ResourceUpdate::CreateFromPicture(CCPrioritizedTexture* texture,
                                                  IntRect content_rect,
                                                  IntRect source_rect,
                                                  IntSize dest_offset) {
+    CHECK(content_rect.contains(source_rect));
     ResourceUpdate update;
     update.texture = texture;
     update.picture = picture;
