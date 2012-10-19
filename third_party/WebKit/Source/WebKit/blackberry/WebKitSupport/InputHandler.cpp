aa580d390929715d3d5f4e7ed549c637f6be240a
mifenton@rim.com <mifenton@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-19 22:25:14 +0000

    [BlackBerry] Convert input style to int64_t https://bugs.webkit.org/show_bug.cgi?id=99848
    Reviewed by Yong Li.
    
    PR 227381.
    
    Switch input style mask to int64_t to match the the updated definition.
    
    Reviewed Internally by Nima Ghanavatian.
    
    * Api/WebPageClient.h:
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::inputStyle):
    * WebKitSupport/InputHandler.h:
    (InputHandler):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
@@ -189,7 +189,7 @@ static BlackBerryInputType convertInputType(const HTMLInputElement* inputElement
     return InputTypeText;
 }
 
-static int inputStyle(BlackBerryInputType type, const Element* element)
+static int64_t inputStyle(BlackBerryInputType type, const Element* element)
 {
     switch (type) {
     case InputTypeEmail:
