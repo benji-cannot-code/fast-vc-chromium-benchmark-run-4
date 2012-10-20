724c2dd413b55b12ce8fff0b39dbcee2083da2b7
hartmanng@chromium.org <hartmanng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-20 04:36:19 +0000

    The current code does a lot of hooking in and out of javascript (after a timeout) to check if we're done scrolling yet. Instead, this patch will determine how many pixels to scroll right away, and let the C++ handle it from there. This should be simpler code, and should also reduce artificial jank introduced by the test harness.
    BUG=155139
    
    
    Review URL: https://chromiumcodereview.appspot.com/11226004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163154 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/scroll.js
+++ tools/perf/perf_tools/scroll.js
@@ -64,31 +64,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     return r;
   };
 
-  function rectsDoIntersect(r1, r2) {
-    return (r1.right >= r2.left &&
-            r1.left <= r2.right &&
-            r1.bottom >= r2.top &&
-            r1.top <= r2.bottom);
-  };
-
   SmoothScrollDownGesture.prototype.start = function(callback) {
     this.callback_ = callback;
     if (chrome &&
         chrome.gpuBenchmarking &&
-        chrome.gpuBenchmarking.beginSmoothScrollDown) {
+        chrome.gpuBenchmarking.smoothScrollBy) {
       rect = getBoundingVisibleRect(this.element_);
-      if (chrome.gpuBenchmarking.beginSmoothScrollSupportsPositioning ||
-         rectsDoIntersect(rect, {left: 0,
-                                 top: 0,
-                                 right: 1,
-                                 bottom: 1,
-                                 height: 1,
-                                 width: 1})) {
-        chrome.gpuBenchmarking.beginSmoothScrollDown(true, function() {
-          callback();
-        }, rect.left + rect.width / 2, rect.top + rect.height / 2);
-        return;
-      }
+      chrome.gpuBenchmarking.smoothScrollBy(
+          this.element_.scrollHeight, function() {
+        callback();
+      }, rect.left + rect.width / 2, rect.top + rect.height / 2);
+      return;
     }
 
     var SCROLL_DELTA = 100;
