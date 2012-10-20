c8ff7ecda977db5467bc4fd99038ac42b8942e1c
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-20 06:39:27 +0000

    Avoid crashing if the AddressBook is broken.
    BUG=129022
    TEST=watch crash server
    
    
    Review URL: https://chromiumcodereview.appspot.com/11175020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163162 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/personal_data_manager_mac.mm
+++ chrome/browser/autofill/personal_data_manager_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/guid.h"
 #include "base/logging.h"
+#import "base/mac/scoped_nsexception_enabler.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
 #include "base/stringprintf.h"
@@ -69,7 +70,13 @@ class AuxiliaryProfilesImpl {
 void AuxiliaryProfilesImpl::GetAddressBookMeCard() {
   profiles_.clear();
 
-  ABAddressBook* addressBook = [ABAddressBook sharedAddressBook];
+  // +[ABAddressBook sharedAddressBook] throws an exception internally in
+  // circumstances that aren't clear. The exceptions are only observed in crash
+  // reports, so it is unknown whether they would be caught by AppKit and nil
+  // returned, or if they would take down the app. In either case, avoid
+  // crashing. http://crbug.com/129022
+  ABAddressBook* addressBook = base::mac::PerformSelectorIgnoringExceptions(
+      NSClassFromString(@"ABAddressBook"), @selector(sharedAddressBook));
   ABPerson* me = [addressBook me];
   if (!me)
     return;
