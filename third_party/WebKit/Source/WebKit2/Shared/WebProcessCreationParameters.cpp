1671a209a1ca6269410dc6849b8a44d263cefa26
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-20 00:00:19 +0000

    WebProcess should only connect to the NetworkProcess if NetworkProcess is enabled https://bugs.webkit.org/show_bug.cgi?id=99885
    Reviewed by Alexey Proskuryakov.
    
    WebContext should tell each WebProcess it creates whether NetworkProcess use is enabled.
    
    Add a WebProcess initialization flag for whether or not NetworkProcess use is enabled:
    * Shared/WebProcessCreationParameters.cpp:
    (WebKit::WebProcessCreationParameters::WebProcessCreationParameters):
    (WebKit::WebProcessCreationParameters::encode):
    (WebKit::WebProcessCreationParameters::decode):
    * Shared/WebProcessCreationParameters.h:
    (WebProcessCreationParameters):
    
    Include that info in the initialization parameters:
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::createNewWebProcess):
    
    Store whether or not NetworkProcess use is enabled, and only connect to the NetworkProcess if appropriate:
    * WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::WebProcess):
    (WebKit::WebProcess::initializeWebProcess):
    (WebKit::WebProcess::ensureNetworkProcessConnection):
    * WebProcess/WebProcess.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@131963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebProcessCreationParameters.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/WebProcessCreationParameters.cpp
@@ -47,6 +47,9 @@ WebProcessCreationParameters::WebProcessCreationParameters()
 #elif PLATFORM(WIN)
     , shouldPaintNativeControls(false)
 #endif
+#if ENABLE(NETWORK_PROCESS)
+    , usesNetworkProcess(false)
+#endif
 {
 }
 
@@ -109,6 +112,10 @@ void WebProcessCreationParameters::encode(CoreIPC::ArgumentEncoder* encoder) con
 
 #if ENABLE(NOTIFICATIONS) || ENABLE(LEGACY_NOTIFICATIONS)
     encoder->encode(notificationPermissions);
+#endif
+
+#if ENABLE(NETWORK_PROCESS)
+    encoder->encode(usesNetworkProcess);
 #endif
 }
 
@@ -216,6 +223,11 @@ bool WebProcessCreationParameters::decode(CoreIPC::ArgumentDecoder* decoder, Web
 
 #if ENABLE(NOTIFICATIONS) || ENABLE(LEGACY_NOTIFICATIONS)
     if (!decoder->decode(parameters.notificationPermissions))
+        return false;
+#endif
+
+#if ENABLE(NETWORK_PROCESS)
+    if (!decoder->decode(parameters.usesNetworkProcess))
         return false;
 #endif
 
--- third_party/WebKit/Source/WebKit2/WebProcess/WebProcess.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebProcess.cpp
@@ -163,6 +163,9 @@ WebProcess::WebProcess()
     , m_notificationManager(this)
 #endif
     , m_iconDatabaseProxy(this)
+#if ENABLE(NETWORK_PROCESS)
+    , m_usesNetworkProcess(false)
+#endif
 #if USE(SOUP)
     , m_soupRequestManager(this)
 #endif
@@ -262,6 +265,7 @@ void WebProcess::initializeWebProcess(const WebProcessCreationParameters& parame
 #endif
 
 #if ENABLE(NETWORK_PROCESS)
+    m_usesNetworkProcess = parameters.usesNetworkProcess;
     ensureNetworkProcessConnection();
 #endif
     setTerminationTimeout(parameters.terminationTimeout);
@@ -270,6 +274,9 @@ void WebProcess::initializeWebProcess(const WebProcessCreationParameters& parame
 #if ENABLE(NETWORK_PROCESS)
 void WebProcess::ensureNetworkProcessConnection()
 {
+    if (!m_usesNetworkProcess)
+        return;
+
     if (m_networkProcessConnection)
         return;
 
