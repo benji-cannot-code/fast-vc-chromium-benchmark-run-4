3db6281c58bd229851f063c2779b91a27b922920
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-21 13:56:13 +0000

    Change CryptoFramer to return a QuicData* instead of returning a bool and setting an out-param to the new QuicData*.  This simplifies the API for callers.
    Merge internal change 35500299
    
    
    Review URL: https://chromiumcodereview.appspot.com/11184057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163203 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/crypto/crypto_framer_test.cc
+++ net/quic/crypto/crypto_framer_test.cc
@@ -101,10 +101,8 @@ TEST(CryptoFramerTest, ConstructHandshakeMessage) {
   };
 
   CryptoFramer framer;
-  QuicData* data;
-  EXPECT_TRUE(framer.ConstructHandshakeMessage(message, &data));
-  scoped_ptr<QuicData> scoped_data(data);
-
+  scoped_ptr<QuicData> data(framer.ConstructHandshakeMessage(message));
+  ASSERT_TRUE(data.get() != NULL);
   test::CompareCharArraysWithHexError("constructed packet",
                                       data->data(), data->length(),
                                       AsChars(packet), arraysize(packet));
@@ -138,9 +136,8 @@ TEST(CryptoFramerTest, ConstructHandshakeMessageWithTwoKeys) {
   };
 
   CryptoFramer framer;
-  QuicData* data;
-  EXPECT_TRUE(framer.ConstructHandshakeMessage(message, &data));
-  scoped_ptr<QuicData> scoped_data(data);
+  scoped_ptr<QuicData> data(framer.ConstructHandshakeMessage(message));
+  ASSERT_TRUE(data.get() != NULL);
 
   test::CompareCharArraysWithHexError("constructed packet",
                                       data->data(), data->length(),
@@ -155,10 +152,8 @@ TEST(CryptoFramerTest, ConstructHandshakeMessageTooManyEntries) {
   }
 
   CryptoFramer framer;
-
-  QuicData* dummy = NULL;
-  EXPECT_FALSE(framer.ConstructHandshakeMessage(message, &dummy));
-  scoped_ptr<QuicData> scoped_data(dummy);
+  scoped_ptr<QuicData> data(framer.ConstructHandshakeMessage(message));
+  EXPECT_TRUE(data.get() == NULL);
 }
 
 
@@ -168,9 +163,8 @@ TEST(CryptoFramerTest, ConstructHandshakeMessageInvalidLength) {
   message.tag_value_map[0x12345678] = "";
 
   CryptoFramer framer;
-  QuicData* dummy = NULL;
-  EXPECT_FALSE(framer.ConstructHandshakeMessage(message, &dummy));
-  scoped_ptr<QuicData> scoped_data(dummy);
+  scoped_ptr<QuicData> data(framer.ConstructHandshakeMessage(message));
+  EXPECT_TRUE(data.get() == NULL);
 }
 
 TEST(CryptoFramerTest, ProcessInput) {
