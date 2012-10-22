c6cc219b5f2089aad0f8502038e4e92a14594823
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-22 16:44:28 +0000

    Gstreamer 1.0 not working https://bugs.webkit.org/show_bug.cgi?id=99852
    Patch by Nicolas Dufresne <nicolas.dufresne@collabora.com> on 2012-10-22
    Reviewed by Philippe Normand.
    
    There was a series of trivial issue, g_object_is_floating() was called
    on type GstCaps (which is not a GObject), webkitGstGetPadCaps() was
    returning non-fixed caps and GST_MESSAGE_DURATION has been renamed
    to GST_MESSAGE_DURATION_CHANGED.
    
    Most failing test passes now.
    
    * platform/graphics/gstreamer/GRefPtrGStreamer.cpp:
    (WTF::adoptGRef):
    * platform/graphics/gstreamer/GStreamerVersioning.cpp:
    (webkitGstGetPadCaps):
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::handleMessage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/GRefPtrGStreamer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/GRefPtrGStreamer.cpp
@@ -89,7 +89,6 @@ template <> void derefGPtr<GstPadTemplate>(GstPadTemplate* ptr)
 
 template <> GRefPtr<GstCaps> adoptGRef(GstCaps* ptr)
 {
-    ASSERT(!ptr || !gstObjectIsFloating(GST_OBJECT(ptr)));
     return GRefPtr<GstCaps>(ptr, GRefPtrAdopt);
 }
 
