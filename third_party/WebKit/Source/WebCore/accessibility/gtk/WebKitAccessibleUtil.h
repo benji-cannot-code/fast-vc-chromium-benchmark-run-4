56dff83b8798678e15affac76def7f3996c309ea
mario@webkit.org <mario@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-22 17:32:52 +0000

    [GTK] Don't use deprecated AccessibilityObject methods after r99502 https://bugs.webkit.org/show_bug.cgi?id=99985
    Reviewed by Chris Fleizach.
    
    Source/WebCore:
    
    Update callers for AccessibilityObject's title() and
    accessibilityDescription() so they now use AccessibilityText.
    
    * accessibility/gtk/WebKitAccessibleUtil.cpp:
    (titleTagShouldBeUsedInDescriptionField): Internal helper function.
    (accessibilityTitle): New helper function, returns an String with
    the title for a AccessibilityObject, using AccessibleText.
    (accessibilityDescription): New helper function, returns an String with
    the description for a AccessibilityObject, using AccessibleText.
    * accessibility/gtk/WebKitAccessibleUtil.h: Added public
    declarations for accessibilityTitle and accessibilityDescription.
    
    * accessibility/gtk/WebKitAccessibleWrapperAtk.cpp:
    (webkitAccessibleGetName): Use new helpers for retrieving the title.
    (webkitAccessibleGetDescription): Use new helpers for retrieving
    the title and description.
    
    * accessibility/gtk/WebKitAccessibleInterfaceImage.cpp:
    (webkitAccessibleImageGetImageDescription): Use new helpers for
    retrieving the title.
    
    LayoutTests:
    
    Update test and expectations to reflect the changes in the
    accessible title (now it's the localized one) being returned.
    
    * platform/gtk/accessibility/media-controls-panel-title-expected.txt: Updated.
    * platform/gtk/accessibility/media-controls-panel-title.html: Updated.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132091 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/gtk/WebKitAccessibleUtil.h
+++ third_party/WebKit/Source/WebCore/accessibility/gtk/WebKitAccessibleUtil.h
@@ -38,6 +38,10 @@ void contentsRelativeToAtkCoordinateType(WebCore::AccessibilityObject*, AtkCoord
 
 const char* returnString(const String&);
 
+String accessibilityTitle(WebCore::AccessibilityObject*);
+
+String accessibilityDescription(WebCore::AccessibilityObject*);
+
 bool selectionBelongsToObject(WebCore::AccessibilityObject*, WebCore::VisibleSelection&);
 
 #endif // WebKitAccessibleUtil_h
--- third_party/WebKit/Source/WebCore/accessibility/gtk/WebKitAccessibleWrapperAtk.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/gtk/WebKitAccessibleWrapperAtk.cpp
@@ -142,7 +142,7 @@ static const gchar* webkitAccessibleGetName(AtkObject* object)
 
     // Nothing worked so far, try with the AccessibilityObject's
     // title() before going ahead with stringValue().
-    String axTitle = coreObject->title();
+    String axTitle = accessibilityTitle(coreObject);
     if (!axTitle.isEmpty())
         return returnString(axTitle);
 
@@ -156,7 +156,7 @@ static const gchar* webkitAccessibleGetDescription(AtkObject* object)
     if (coreObject->isAccessibilityRenderObject())
         node = coreObject->node();
     if (!node || !node->isHTMLElement() || coreObject->ariaRoleAttribute() != UnknownRole)
-        return returnString(coreObject->accessibilityDescription());
+        return returnString(accessibilityDescription(coreObject));
 
     // atk_table_get_summary returns an AtkObject. We have no summary object, so expose summary here.
     if (coreObject->roleValue() == TableRole) {
@@ -171,7 +171,7 @@ static const gchar* webkitAccessibleGetDescription(AtkObject* object)
     if (!title.isEmpty())
         return returnString(title);
 
-    return returnString(coreObject->accessibilityDescription());
+    return returnString(accessibilityDescription(coreObject));
 }
 
 static void setAtkRelationSetFromCoreObject(AccessibilityObject* coreObject, AtkRelationSet* relationSet)
