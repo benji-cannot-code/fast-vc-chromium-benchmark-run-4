94f5d8d5c8e63ec1174eb056014eb852cd60ca54
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 17:16:02 +0000

    Don't autorestart if incognito windows are open.
    BUG=155489
    TEST=Same procedure as when testing the critical notification bubble, except open an incognito window and the bubble should not appear until you close the incognito window.
    Review URL: https://codereview.chromium.org/11235028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163315 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/upgrade_detector.cc
+++ chrome/browser/upgrade_detector.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
+#include "chrome/browser/ui/browser_otr_state.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
@@ -104,6 +105,11 @@ void UpgradeDetector::CheckIdle() {
 }
 
 void UpgradeDetector::IdleCallback(IdleState state) {
+  // Don't proceed while an incognito window is open. The timer will still
+  // keep firing, so this function will get a chance to re-evaluate this.
+  if (chrome::IsOffTheRecordSessionActive())
+    return;
+
   switch (state) {
     case IDLE_STATE_LOCKED:
       // Computer is locked, auto-restart.
