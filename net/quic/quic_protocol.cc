c36f6840d0ecb281efe242a5bc1c9cce65e957f6
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 18:17:02 +0000

    Minor style cleanup of QuicProtocol
    Merge internal change 35543340
    
    
    Review URL: https://chromiumcodereview.appspot.com/11188072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163321 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_protocol.cc
+++ net/quic/quic_protocol.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/quic_protocol.h"
 
 using base::StringPiece;
+using base::hash_set;
+using std::ostream;
 
 namespace net {
 
@@ -29,7 +31,42 @@ SentPacketInfo::SentPacketInfo() {}
 
 SentPacketInfo::~SentPacketInfo() {}
 
+ostream& operator<<(ostream& os, const QuicAckFrame& s) {
+  os << "largest_received: " << s.received_info.largest_received
+     << " time: " << s.received_info.time_received
+     << " missing: ";
+  for (hash_set<QuicPacketSequenceNumber>::const_iterator it =
+           s.received_info.missing_packets.begin();
+       it != s.received_info.missing_packets.end(); ++it) {
+    os << *it << " ";
+  }
+
+  os << " least_waiting: " << s.sent_info.least_unacked
+     << " no_retransmit: ";
+  for (hash_set<QuicPacketSequenceNumber>::const_iterator it =
+           s.sent_info.non_retransmiting.begin();
+       it != s.sent_info.non_retransmiting.end(); ++it) {
+    os << *it << " ";
+  }
+  os << "\n";
+  return os;
+}
+
 QuicFecData::QuicFecData() {}
+
+bool QuicFecData::operator==(const QuicFecData& other) const {
+  if (fec_group != other.fec_group) {
+    return false;
+  }
+  if (first_protected_packet_sequence_number !=
+      other.first_protected_packet_sequence_number) {
+    return false;
+  }
+  if (redundancy != other.redundancy) {
+    return false;
+  }
+  return true;
+}
 
 QuicData::~QuicData() {
   if (owns_buffer_) {
