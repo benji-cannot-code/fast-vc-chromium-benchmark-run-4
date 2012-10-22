e939b45bfa3eaee017f34dddd3935ea39ba4ad63
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 08:19:13 +0000

    Files app: fixed infinite Google Drive loading spinner
    BUG=156804
    
    Review URL: https://codereview.chromium.org/11226002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163240 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_model.js
+++ chrome/browser/resources/file_manager/js/directory_model.js
@@ -77,6 +77,8 @@ DirectoryModel.prototype.__proto__ = cr.EventTarget.prototype;
 DirectoryModel.prototype.start = function() {
   var volumesChangeHandler = this.onMountChanged_.bind(this);
   this.volumeManager_.addEventListener('change', volumesChangeHandler);
+  this.volumeManager_.addEventListener('gdata-status-changed',
+      this.onGDataStatusChanged_.bind(this));
   this.updateRoots_();
 };
 
@@ -1084,7 +1086,7 @@ DirectoryModel.prototype.updateRootsListSelection_ = function() {
 };
 
 /**
- * @return {boolean} True if GDATA if fully mounted.
+ * @return {boolean} True if GDATA is fully mounted.
  * @private
  */
 DirectoryModel.prototype.isGDataMounted_ = function() {
@@ -1093,7 +1095,7 @@ DirectoryModel.prototype.isGDataMounted_ = function() {
 };
 
 /**
- * Handler for the VolumeManager's event.
+ * Handler for the VolumeManager's 'change' event.
  * @private
  */
 DirectoryModel.prototype.onMountChanged_ = function() {
@@ -1105,9 +1107,15 @@ DirectoryModel.prototype.onMountChanged_ = function() {
       !this.volumeManager_.isMounted(this.getCurrentRootPath())) {
     this.changeDirectory(this.getDefaultDirectory());
   }
+};
 
-  if (rootType != RootType.GDATA)
-    return;
+/**
+ * Handler for the VolumeManager's 'gdata-status-changed' event.
+ * @private
+ */
+DirectoryModel.prototype.onGDataStatusChanged_ = function() {
+  if (this.getCurrentRootType() != RootType.GDATA)
+     return;
 
   var mounted = this.isGDataMounted_();
   if (this.getCurrentDirEntry() == DirectoryModel.fakeGDataEntry_) {
