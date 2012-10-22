0114c3c1d51fa1a081de5dbf00080304f6d2604c
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 18:55:50 +0000

    Photo Editor: Update stale thumbnails from Drive
    BUG=157131
    
    Review URL: https://codereview.chromium.org/11232039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163330 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/photo/gallery.js
+++ chrome/browser/resources/file_manager/js/photo/gallery.js
@@ -166,6 +166,16 @@ Gallery.prototype.initListeners_ = function() {
           Gallery.FADE_TIMEOUT);
   this.document_.body.addEventListener('touchend', initiateFading);
   this.document_.body.addEventListener('touchcancel', initiateFading);
+
+  var thumbnailObserverId = this.metadataCache_.addObserver(
+      this.context_.curDirEntry,
+      MetadataCache.CHILDREN,
+      'thumbnail',
+      this.updateThumbnails_.bind(this));
+
+  this.document_.defaultView.addEventListener('unload',
+      this.metadataCache_.removeObserver.bind(
+          this.metadataCache_, thumbnailObserverId));
 };
 
 /**
@@ -203,6 +213,8 @@ Gallery.prototype.initDom_ = function() {
   this.prompt_ = new ImageEditor.Prompt(
       this.container_, this.displayStringFunction_);
 
+  var onThumbnailError = this.context_.onThumbnailError || function() {};
+
   if (this.context_.allowMosaic) {
     this.modeButton_ = util.createChild(this.toolbar_, 'button mode');
     this.modeButton_.addEventListener('click',
@@ -210,13 +222,14 @@ Gallery.prototype.initDom_ = function() {
 
     this.mosaicMode_ = new MosaicMode(content,
         this.dataModel_, this.selectionModel_, this.metadataCache_,
-        this.toggleMode_.bind(this, null));
+        this.toggleMode_.bind(this, null), onThumbnailError);
   }
 
   this.slideMode_ = new SlideMode(this.container_, content,
       this.toolbar_, this.prompt_,
-      this.dataModel_, this.selectionModel_,
-      this.context_, this.toggleMode_.bind(this), this.displayStringFunction_);
+      this.dataModel_, this.selectionModel_, this.context_,
+      this.toggleMode_.bind(this), onThumbnailError,
+      this.displayStringFunction_);
 
   var deleteButton = this.document_.createElement('div');
   deleteButton.className = 'button delete';
@@ -787,4 +800,16 @@ Gallery.prototype.updateShareMenu_ = function() {
     ImageUtil.setAttribute(this.shareButton_, 'disabled', empty);
     this.shareMenu_.hidden = wasHidden || empty;
   }.bind(this));
+};
+
+/**
+ * Update thumbnails.
+ * @private
+ */
+Gallery.prototype.updateThumbnails_ = function() {
+  if (this.currentMode_ == this.slideMode_)
+    this.slideMode_.updateThumbnails();
+
+  if (this.mosaicMode_)
+    this.mosaicMode_.getMosaic().reload();
 };
