9b34e617d27d49f6de2ea59bf6621a82108a74e9
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 06:39:51 +0000

    Add extension API to get quota and usage of 'Syncable' storage for drive-backed File API
    BUG=153501
    
    Review URL: https://chromiumcodereview.appspot.com/11146034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163234 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/sync_file_system/sync_file_system_apitest.cc
+++ chrome/browser/extensions/api/sync_file_system/sync_file_system_apitest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/extensions/features/feature.h"
+#include "webkit/quota/quota_manager.h"
 
 namespace chrome {
 
@@ -18,13 +19,31 @@ class SyncFileSystemApiTest : public ExtensionApiTest {
   // Override the current channel to "trunk" as syncFileSystem is currently
   // available only on trunk channel.
   SyncFileSystemApiTest()
-      : current_channel_(VersionInfo::CHANNEL_UNKNOWN) {}
+      : current_channel_(VersionInfo::CHANNEL_UNKNOWN) {
+  }
+
+  void SetUp() {
+    // TODO(calvinlo): Update test code after default quota is made const
+    // (http://crbug.com/155488).
+    real_default_quota_ = quota::QuotaManager::kSyncableStorageDefaultHostQuota;
+    quota::QuotaManager::kSyncableStorageDefaultHostQuota = 123456789;
+  }
+
+  void TearDown() {
+    quota::QuotaManager::kSyncableStorageDefaultHostQuota = real_default_quota_;
+  }
 
  private:
   extensions::Feature::ScopedCurrentChannel current_channel_;
+  int64 real_default_quota_;
 };
 
 }  // namespace
+
+IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, GetUsageAndQuota) {
+  ASSERT_TRUE(RunExtensionTest("sync_file_system/get_usage_and_quota"))
+      << message_;
+}
 
 IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, RequestFileSystem) {
   ASSERT_TRUE(RunExtensionTest("sync_file_system/request_file_system"))
---
+++ chrome/test/data/extensions/api_test/sync_file_system/get_usage_and_quota/test.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// TODO(calvinlo): Update test code after default quota is made const
+// (http://crbug.com/155488).
+chrome.test.runTests([
+  function getUsageAndQuota() {
+    chrome.syncFileSystem.getUsageAndQuota(
+        'drive',
+        chrome.test.callbackPass(function(info) {
+            chrome.test.assertEq(0, info.usage);
+            chrome.test.assertEq(123456789, info.quota);
+        }));
+  }
+]);
