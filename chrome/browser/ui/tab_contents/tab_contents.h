f76871330e45f1c19740d7dc7c1999f40ddefe50
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 22:50:43 +0000

    Remove TabContents from WebIntentInlineDispositionBrowserTest.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11236031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163421 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents.h
+++ chrome/browser/ui/tab_contents/tab_contents.h
@@ -31,7 +31,6 @@ class TabStripModelContentsCreator;
 class TranslationInfoBarTestContentsCreator;
 class WebDialogGtk;
 class WebDialogWindowControllerTabContentsCreator;
-class WebIntentInlineDispositionBrowserTest;
 class WebIntentPickerCocoa;
 class WebIntentPickerGtk;
 
@@ -95,7 +94,6 @@ class TabContents : public content::WebContentsObserver {
     friend class TranslationInfoBarTestContentsCreator;
     friend class WebDialogGtk;
     friend class WebDialogWindowControllerTabContentsCreator;
-    friend class WebIntentInlineDispositionBrowserTest;
     friend class WebIntentPickerCocoa;
     friend class WebIntentPickerGtk;
     FRIEND_TEST_ALL_PREFIXES(SessionRestoreTest, SessionStorageAfterTabReplace);
