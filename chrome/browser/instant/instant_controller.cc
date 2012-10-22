de36b85431bf15490041537bdbcd4ebbb332e3b4
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-22 19:12:37 +0000

    Remove useless call to SetFavicons in instant_controller
    BUG=None
    Test=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11234022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.cc
+++ chrome/browser/instant/instant_controller.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/autocomplete/autocomplete_provider.h"
-#include "chrome/browser/favicon/favicon_service_factory.h"
 #include "chrome/browser/google/google_util.h"
 #include "chrome/browser/history/history.h"
 #include "chrome/browser/history/history_service_factory.h"
@@ -389,15 +388,6 @@ void InstantController::CommitCurrentPreview(InstantCommitType type) {
 
     // Update the page title.
     history_tab_helper->UpdateHistoryPageTitle(*entry);
-
-    // Update the favicon.
-    FaviconService* favicon_service = FaviconServiceFactory::GetForProfile(
-        preview->profile(), Profile::EXPLICIT_ACCESS);
-    if (favicon_service && entry->GetFavicon().valid &&
-        entry->GetFavicon().image.IsEmpty()) {
-      favicon_service->SetFavicons(entry->GetURL(), entry->GetFavicon().url,
-          history::FAVICON, entry->GetFavicon().image);
-    }
   }
 
   // Add a fake history entry with a non-Instant search URL, so that search
