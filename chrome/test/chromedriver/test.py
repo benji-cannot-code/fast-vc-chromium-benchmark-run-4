bb538433af038201025b877791218c141799f239
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 19:56:46 +0000

    Initial check-in for new ChromeDriver based on DevTools.
    Introduces 3 new targets, chromedriver2_lib, chromedriver2_unittests, and chromedriver2. The latter is a shared library which can be loaded into a test/automation process and used to control chrome. The shared library exposes a function ExecuteCommand, which takes WebDriver JSON commands and returns a WebDriver JSON response.
    
    In the future, when necessary, we may create a new chromedriver_server target which uses the chromedriver2_lib and exposes it remotely via sockets/http/whatever.
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11099075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163651 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/chromedriver/test.py
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""End to end tests for ChromeDriver."""
+
+import ctypes
+import os
+import sys
+import unittest
+
+import chromedriver
+
+
+class ChromeDriverTest(unittest.TestCase):
+  """End to end tests for ChromeDriver."""
+
+  def testStartStop(self):
+    driver = chromedriver.ChromeDriver(_CHROMEDRIVER_LIB)
+    driver.Quit()
+
+if __name__ == '__main__':
+  if len(sys.argv) != 2:
+    print 'Usage: %s <path_to_chromedriver_so>' % __file__
+    sys.exit(1)
+  global _CHROMEDRIVER_LIB
+  _CHROMEDRIVER_LIB = os.path.abspath(sys.argv[1])
+  all_tests_suite = unittest.defaultTestLoader.loadTestsFromModule(
+      sys.modules[__name__])
+  unittest.TextTestRunner().run(all_tests_suite)
