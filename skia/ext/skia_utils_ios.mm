5ce68b98fa66faa846a11092acd96dab68f02819
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 23:40:06 +0000

    Sets the proper device scale factor on iOS.
    iOS sets a single supported scale factor at runtime, either 100P or 200P
    depending on whether the device is hidpi.  This CL also corrects some conversion
    methods that did not properly support 200P scale factors.
    
    BUG=None
    TEST=Image unittests all pass on hidpi devices.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11233040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163725 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/skia_utils_ios.mm
+++ skia/ext/skia_utils_ios.mm
@@ -68,6 +68,7 @@ SkBitmap UIImageToSkBitmap(UIImage* image, CGSize size, bool is_opaque) {
 }
 
 UIImage* SkBitmapToUIImageWithColorSpace(const SkBitmap& skia_bitmap,
+                                         CGFloat scale,
                                          CGColorSpaceRef color_space) {
   if (skia_bitmap.isNull())
     return nil;
@@ -77,8 +78,9 @@ UIImage* SkBitmapToUIImageWithColorSpace(const SkBitmap& skia_bitmap,
       SkCreateCGImageRefWithColorspace(skia_bitmap, color_space));
 
   // Now convert to UIImage.
-  // TODO(rohitrao): Gotta incorporate the scale factor somewhere!
-  return [UIImage imageWithCGImage:cg_image.get()];
+  return [UIImage imageWithCGImage:cg_image.get()
+                             scale:scale
+                       orientation:UIImageOrientationUp];
 }
 
 }  // namespace gfx
