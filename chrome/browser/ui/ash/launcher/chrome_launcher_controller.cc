f88640dc23d6ba10955b8930117b079112e48972
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 20:26:28 +0000

    Reland of "work on separate browser contexts"
    A couple tests fixed up from http://codereview.chromium.org/11146023
    
    Always start up in --desktop-aura mode now, and open ash from tools menu.
    
    - Implement host_desktop.cc and plumb a few places that need context to determine which style of desktop it belongs to.
    - Remove --desktop-aura, --open-ash, --enable-ash
    
    TBR=ben@chromium.org, jam@chromium.org
    BUG=133312
    
    Review URL: https://chromiumcodereview.appspot.com/11227057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163664 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
+++ chrome/browser/ui/ash/launcher/chrome_launcher_controller.cc
@@ -529,7 +529,8 @@ bool ChromeLauncherController::IsLoggedInAsGuest() {
 }
 
 void ChromeLauncherController::CreateNewWindow() {
-  chrome::NewEmptyWindow(GetProfileForNewWindows());
+  chrome::NewEmptyWindow(
+      GetProfileForNewWindows(), chrome::HOST_DESKTOP_TYPE_ASH);
 }
 
 void ChromeLauncherController::CreateNewIncognitoWindow() {
--- content/shell/shell_aura.cc
+++ content/shell/shell_aura.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/view.h"
 #include "ui/views/test/test_views_delegate.h"
-#include "ui/views/views_switches.h"
 #include "ui/views/widget/desktop_native_widget_aura.h"
 #include "ui/views/widget/desktop_native_widget_helper_aura.h"
 #include "ui/views/widget/widget.h"
@@ -293,8 +292,6 @@ void Shell::PlatformInitialize() {
   gfx::Screen::SetScreenInstance(
       gfx::SCREEN_TYPE_NATIVE, aura::CreateDesktopScreen());
   views_delegate_ = new ShellViewsDelegateAura();
-
-  CommandLine::ForCurrentProcess()->AppendSwitch(views::switches::kDesktopAura);
 }
 
 void Shell::PlatformExit() {
