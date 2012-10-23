e233adc24a4908c198c5a5243ccdef8fc3e1670f
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 22:20:24 +0000

    WebIntentPickerGtk dropped TabContents with r163622.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11189147
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163700 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents.h
+++ chrome/browser/ui/tab_contents/tab_contents.h
@@ -30,7 +30,6 @@ class TabStripModelContentsCreator;
 class WebDialogGtk;
 class WebDialogWindowControllerTabContentsCreator;
 class WebIntentPickerCocoa;
-class WebIntentPickerGtk;
 
 namespace chromeos {
 class SimpleWebViewDialog;
@@ -91,7 +90,6 @@ class TabContents : public content::WebContentsObserver {
     friend class WebDialogGtk;
     friend class WebDialogWindowControllerTabContentsCreator;
     friend class WebIntentPickerCocoa;
-    friend class WebIntentPickerGtk;
     FRIEND_TEST_ALL_PREFIXES(SessionRestoreTest, SessionStorageAfterTabReplace);
 
     static TabContents* CreateTabContents(content::WebContents* contents);
