125b24f490b350a3e5142ff1987e027dc59a03c2
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 19:20:36 +0000

    Revert 163584 - Add OnSyncEnabled/OnWriteEnabled notification handling to operation runner
    * Slightly changing ownership relationship: now LocalFileSyncContext owns both SyncableFileOperationRunner and LocalFileSyncStatus (previously SyncContext used to own OperationRunner that owns SyncStatus)
    * Adding OnSyncEnabled, OnWriteEnabled notifications to the LocalFileSyncStatus
    * Let SyncableFileOperation listen to OnWriteEnabled notification so that it can automatically start write operations (as far as the # of operations is less than the given max)
    
    BUG=148897, 154234
    TEST=SyncableFileOperationRunnerTest.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/11238054
    
    TBR=kinuko@chromium.org
    Review URL: https://codereview.chromium.org/11236076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163637 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/syncable/local_file_sync_context.cc
+++ webkit/fileapi/syncable/local_file_sync_context.cc
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace fileapi {
 
-namespace {
-const int kMaxConcurrentSyncableOperation = 3;
-}  // namespace
-
 LocalFileSyncContext::LocalFileSyncContext(
     base::SingleThreadTaskRunner* ui_task_runner,
     base::SingleThreadTaskRunner* io_task_runner)
@@ -73,18 +69,12 @@ LocalFileSyncContext::operation_runner() const {
   return base::WeakPtr<SyncableFileOperationRunner>();
 }
 
-LocalFileSyncStatus* LocalFileSyncContext::sync_status() const {
-  DCHECK(io_task_runner_->RunsTasksOnCurrentThread());
-  return sync_status_.get();
-}
-
 LocalFileSyncContext::~LocalFileSyncContext() {
 }
 
 void LocalFileSyncContext::ShutdownOnIOThread() {
   DCHECK(io_task_runner_->RunsTasksOnCurrentThread());
   operation_runner_.reset();
-  sync_status_.reset();
 }
 
 void LocalFileSyncContext::InitializeFileSystemContextOnIOThread(
@@ -109,13 +99,8 @@ void LocalFileSyncContext::InitializeFileSystemContextOnIOThread(
                    make_scoped_refptr(file_system_context)));
     return;
   }
-  if (!operation_runner_.get()) {
-    DCHECK(!sync_status_.get());
-    sync_status_.reset(new LocalFileSyncStatus);
-    operation_runner_.reset(new SyncableFileOperationRunner(
-            kMaxConcurrentSyncableOperation,
-            sync_status_.get()));
-  }
+  if (!operation_runner_.get())
+    operation_runner_.reset(new SyncableFileOperationRunner);
   file_system_context->set_sync_context(this);
   DidInitialize(source_url, file_system_context, SYNC_STATUS_OK);
 }
