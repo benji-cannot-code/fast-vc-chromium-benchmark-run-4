d250a421234281d77d98aab64d1615cb1318c78d
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 23:29:37 +0000

    Add QuicPacketCreator and QuicFecGroup.
    Review URL: https://chromiumcodereview.appspot.com/11238013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163724 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_utils.cc
+++ net/quic/quic_utils.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace net {
 
 // static
-int QuicUtils::StreamFramePacketOverhead(int num_frames) {
+size_t QuicUtils::StreamFramePacketOverhead(int num_frames) {
   // TODO(jar): Use sizeof(some name).
   return kPacketHeaderSize +
          1 +   // frame count
