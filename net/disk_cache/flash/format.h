055aea22f76d2025c71572586a3dd7083e33960e
agayev@chromium.org <agayev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 16:14:40 +0000

    Cache backend for devices with flash storage
    BUG=157187
    TEST=net_unittest --gtest_filter="SegmentTest.*;FlashCacheTest.*"
    
    
    Review URL: https://chromiumcodereview.appspot.com/11095004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163568 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/disk_cache/flash/format.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef NET_DISK_CACHE_FLASH_FORMAT_H
+#define NET_DISK_CACHE_FLASH_FORMAT_H
+
+namespace disk_cache {
+
+// Storage constants.
+const int32 kFlashPageSize = 8 * 1024;
+const int32 kFlashBlockSize = 512 * kFlashPageSize;
+
+// Segment constants.
+const int32 kFlashSegmentSize = 4 * 1024 * 1024;
+const int32 kFlashSmallEntrySize = 4 * 1024;
+const size_t kFlashMaxEntryCount = kFlashSegmentSize / kFlashSmallEntrySize - 1;
+
+// Segment summary consists of a fixed region at the end of the segment
+// containing a counter specifying the number of saved offsets followed by the
+// offsets.
+const int32 kFlashSummarySize = (1 + kFlashMaxEntryCount) * sizeof(int32);
+
+}  // namespace disk_cache
+
+#endif  // NET_DISK_CACHE_FLASH_FORMAT_H
