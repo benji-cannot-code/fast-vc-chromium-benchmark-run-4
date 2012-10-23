a63cc4ac8c964989e39111c65bfefa789cbfdf0b
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 01:49:02 +0000

    content: Remove nested 'internal' namespace.
    BUG=none
    
    Review URL: https://codereview.chromium.org/11189129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163473 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/browser_plugin/browser_plugin_bindings.h
+++ content/renderer/browser_plugin/browser_plugin_bindings.h
@@ -17,11 +17,8 @@ class WebSerializedScriptValue;
 
 namespace content {
 
-namespace internal {
-class BrowserPluginMethodBinding;
-}
-
 class BrowserPlugin;
+class BrowserPluginMethodBinding;
 
 class BrowserPluginBindings {
  public:
@@ -54,7 +51,7 @@ class BrowserPluginBindings {
   // The NPObject we use to expose postMessage to JavaScript.
   BrowserPluginNPObject* np_object_;
 
-  typedef ScopedVector<internal::BrowserPluginMethodBinding> BindingList;
+  typedef ScopedVector<BrowserPluginMethodBinding> BindingList;
   BindingList method_bindings_;
 
   // This is used to ensure pending tasks will not fire after this object is
