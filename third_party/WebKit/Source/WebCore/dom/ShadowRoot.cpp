95aff0e62a74ddd77604739d49f04673eb152cb1
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-23 02:05:07 +0000

    [Chromium] Needs to track ShadowRoot usage https://bugs.webkit.org/show_bug.cgi?id=99955
    Reviewed by Dimitri Glazkov.
    
    Added an UMA instrumentation.
    
    * dom/ShadowRoot.cpp:
    (WebCore::determineUsageType):
    (WebCore):
    (WebCore::ShadowRoot::create):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132171 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLInputElement.h"
 #include "HTMLNames.h"
 #include "HTMLTextAreaElement.h"
+#include "HistogramSupport.h"
 #include "InsertionPoint.h"
 #include "NodeRareData.h"
 #include "RuntimeEnabledFeatures.h"
@@ -87,8 +88,28 @@ static bool allowsAuthorShadowRoot(Element* element)
     return element->areAuthorShadowsAllowed();
 }
 
+enum ShadowRootUsageOriginType {
+    ShadowRootUsageOriginWeb = 0,
+    ShadowRootUsageOriginNotWeb,
+    ShadowRootUsageOriginTypes
+};
+
+static inline ShadowRootUsageOriginType determineUsageType(Element* host)
+{
+    // Enables only on CHROMIUM since this cost won't worth paying for platforms which don't collect this metrics.
+#if PLATFORM(CHROMIUM)
+    if (!host)
+        return ShadowRootUsageOriginWeb;
+    return host->document()->url().string().startsWith("http") ? ShadowRootUsageOriginWeb : ShadowRootUsageOriginNotWeb;
+#else
+    UNUSED_PARAM(host);
+    return ShadowRootUsageOriginWeb;
+#endif
+}
+
 PassRefPtr<ShadowRoot> ShadowRoot::create(Element* element, ExceptionCode& ec)
 {
+    HistogramSupport::histogramEnumeration("WebCore.ShadowRoot.constructor", determineUsageType(element), ShadowRootUsageOriginTypes);
     return create(element, AuthorShadowRoot, ec);
 }
 
