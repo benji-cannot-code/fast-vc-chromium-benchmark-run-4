df59be2132f5fc31aa9b1fd3bd248ede28cbbcda
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-23 08:25:46 +0000

    Unreviewed build fix with newer Qt 5.
    QPlastiqueStyle has been removed, but as pointed out by Jens it's also not
    maintained anymore and hence safe to remove. We can do tweaks with the new
    fusion style if required.
    
    * WebCoreSupport/QStyleFacadeImp.cpp:
    (WebKit::QStyleFacadeImp::paintInnerSpinButton):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QStyleFacadeImp.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QStyleFacadeImp.cpp
@@ -31,9 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QMacStyle>
 #include <QPainter>
 #include <QPushButton>
-#ifndef QT_NO_STYLE_PLASTIQUE
-#include <QPlastiqueStyle>
-#endif
 #include <QStyleFactory>
 #include <QStyleOption>
 
@@ -372,13 +369,6 @@ void QStyleFacadeImp::paintInnerSpinButton(QPainter* painter, const QStyleFacade
         // Render mini aqua spin buttons for QMacStyle to fit nicely into
         // the editor area, like Chromium.
         option.state |= QStyle::State_Mini;
-    }
-#endif
-#if !defined(QT_NO_STYLE_PLASTIQUE)
-    // QPlastiqueStyle looks best when the spin buttons are flush with the frame's edge.
-    if (qobject_cast<QPlastiqueStyle*>(style())) {
-        inflateX = 0;
-        inflateY = 0;
     }
 #endif
 
