f2b3212f1e9ca3ce3edac0183212c2fa9009b48f
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 17:44:05 +0000

    Add support for list-of-strings property type.  This only supports reading the property, not updating it.  This change is broken out of CL http://codereview.chromium.org/11188002/ to keep the changes more focused and easier to review.
    BUG=137118
    TEST=No used in any code yet.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11235015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163589 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/api/prefs/pref_member.h
+++ chrome/browser/api/prefs/pref_member.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_API_PREFS_PREF_MEMBER_H_
 
 #include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/file_path.h"
@@ -132,6 +133,11 @@ class PrefMemberBase : public content::NotificationObserver {
  protected:
   bool setting_value_;
 };
+
+// This function implements StringListPrefMember::UpdateValue().
+// It is exposed here for testing purposes.
+bool PrefMemberVectorStringUpdate(const Value& value,
+                                  std::vector<std::string>* string_vector);
 
 }  // namespace subtle
 
@@ -254,5 +260,7 @@ typedef PrefMember<int> IntegerPrefMember;
 typedef PrefMember<double> DoublePrefMember;
 typedef PrefMember<std::string> StringPrefMember;
 typedef PrefMember<FilePath> FilePathPrefMember;
+// This preference member is expensive for large string arrays.
+typedef PrefMember<std::vector<std::string> > StringListPrefMember;
 
 #endif  // CHROME_BROWSER_API_PREFS_PREF_MEMBER_H_
