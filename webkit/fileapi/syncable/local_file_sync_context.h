9a0c219c4920584ceedb6b4daf6e65c8e384cba1
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 17:10:53 +0000

    Add OnSyncEnabled/OnWriteEnabled notification handling to operation runner
    * Slightly changing ownership relationship: now LocalFileSyncContext owns both SyncableFileOperationRunner and LocalFileSyncStatus (previously SyncContext used to own OperationRunner that owns SyncStatus)
    * Adding OnSyncEnabled, OnWriteEnabled notifications to the LocalFileSyncStatus
    * Let SyncableFileOperation listen to OnWriteEnabled notification so that it can automatically start write operations (as far as the # of operations is less than the given max)
    
    BUG=148897, 154234
    TEST=SyncableFileOperationRunnerTest.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/11238054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163584 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/syncable/local_file_sync_context.h
+++ webkit/fileapi/syncable/local_file_sync_context.h
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/fileapi/syncable/file_change.h"
+#include "webkit/fileapi/syncable/local_file_sync_status.h"
 #include "webkit/fileapi/syncable/sync_status_code.h"
 #include "webkit/storage/webkit_storage_export.h"
 
@@ -28,6 +29,7 @@ class SingleThreadTaskRunner;
 
 namespace fileapi {
 
+class FileChange;
 class FileSystemContext;
 class LocalFileChangeTracker;
 class SyncableFileOperationRunner;
@@ -60,12 +62,15 @@ class WEBKIT_STORAGE_EXPORT LocalFileSyncContext
   // OperationRunner is accessible only on IO thread.
   base::WeakPtr<SyncableFileOperationRunner> operation_runner() const;
 
+  // SyncContext is accessible only on IO thread.
+  LocalFileSyncStatus* sync_status() const;
+
  private:
   typedef std::deque<StatusCallback> StatusCallbackQueue;
   friend class base::RefCountedThreadSafe<LocalFileSyncContext>;
   friend class CannedSyncableFileSystem;
 
-  ~LocalFileSyncContext();
+  virtual ~LocalFileSyncContext();
 
   void ShutdownOnIOThread();
 
@@ -92,16 +97,22 @@ class WEBKIT_STORAGE_EXPORT LocalFileSyncContext
   // OperationRunner. This must be accessed only on IO thread.
   scoped_ptr<SyncableFileOperationRunner> operation_runner_;
 
+  // Keeps track of writing/syncing status.
+  // This must be accessed only on IO thread.
+  scoped_ptr<LocalFileSyncStatus> sync_status_;
+
   // Pointers to file system contexts that have been initialized for
   // synchronization (i.e. that own this instance).
   // This must be accessed only on UI thread.
   std::set<FileSystemContext*> file_system_contexts_;
 
+  // Accessed only on UI thread.
   std::map<FileSystemContext*, StatusCallbackQueue>
       pending_initialize_callbacks_;
 
   // Origin to context map. (Assuming that as far as we're in the same
   // profile single origin wouldn't belong to multiple FileSystemContexts.)
+  // Accessed only on UI thread.
   std::map<GURL, FileSystemContext*> origin_to_contexts_;
 
   DISALLOW_COPY_AND_ASSIGN(LocalFileSyncContext);
