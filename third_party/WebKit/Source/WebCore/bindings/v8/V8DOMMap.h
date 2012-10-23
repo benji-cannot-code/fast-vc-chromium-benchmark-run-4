ccf117dc377a510a139bf61010babdcd5244ffba
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-23 23:41:39 +0000

    [V8] Remove unused function from DOM wrapper visitor https://bugs.webkit.org/show_bug.cgi?id=100163
    Reviewed by Eric Seidel.
    
    No one overrides these virtual functions. They were added in
    http://trac.webkit.org/changeset/73491, but we don't use them in the GC
    controller anymore.
    
    * bindings/v8/V8DOMMap.h:
    (WebCore::WeakReferenceMap::visit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132281 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMMap.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMMap.h
@@ -50,8 +50,6 @@ namespace WebCore {
 
         class Visitor {
         public:
-            virtual void startMap() { }
-            virtual void endMap() { }
             virtual void visitDOMWrapper(DOMDataStore* store, KeyType* key, v8::Persistent<ValueType> object) = 0;
         protected:
             virtual ~Visitor() { }
@@ -129,11 +127,9 @@ namespace WebCore {
 
         virtual void visit(DOMDataStore* store, typename Parent::Visitor* visitor)
         {
-            visitor->startMap();
             typename HashMap<KeyType*, ValueType*>::iterator it = m_map.begin();
             for (; it != m_map.end(); ++it)
                 visitor->visitDOMWrapper(store, it->key, v8::Persistent<ValueType>(it->value));
-            visitor->endMap();
         }
 
         virtual void reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const OVERRIDE
