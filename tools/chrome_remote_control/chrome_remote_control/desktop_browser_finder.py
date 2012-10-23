6201658ae3bc29d1063a6de3e3a72a3352c15898
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-23 00:21:29 +0000

    [chrome-remote-control] Fix build search path on mac.
    Mac may be built with ninja or xcode so we need to search both out and
    xcodebuild. Now it seems cleaner to just search all the build paths regardless
    of platform.
    
    This fixes the test on the mac buildbots which use xcode. Example:
    http://build.chromium.org/p/chromium.perf/builders/Mac10.7%20GPU%20%28Intel%29%20Perf/builds/6056/steps/scrolling_benchmark/logs/stdio
    
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/11229046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163453 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/chrome_remote_control/chrome_remote_control/desktop_browser_finder.py
+++ tools/chrome_remote_control/chrome_remote_control/desktop_browser_finder.py
@@ -72,11 +72,10 @@ def FindAllAvailableBrowsers(options):
   else:
     raise Exception('Platform not recognized')
 
-  build_dirs = ['sconsbuild']
-  if sys.platform.startswith('win'):
-    build_dirs.append('build')
-  else:
-    build_dirs.append('out')
+  build_dirs = ['build',
+                'out',
+                'sconsbuild',
+                'xcodebuild']
 
   # Add local builds
   def AddIfFound(browser_type, type_dir, app_name, content_shell):
