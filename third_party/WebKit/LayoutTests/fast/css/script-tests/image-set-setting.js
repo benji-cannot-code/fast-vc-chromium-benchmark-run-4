5a3f00d2c7b3a87b90e7b294cdf931f1aa24f449
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 20:18:36 +0000

    image-set doesn't round-trip properly with cssText https://bugs.webkit.org/show_bug.cgi?id=99725
    Patch by Rick Byers <rbyers@chromium.org> on 2012-10-24
    Reviewed by Beth Dakin.
    
    Source/WebCore:
    
    Fix serialization of -webkit-image-set rules to use the same format as
    is used for parsing.
    
    Test: fast/css/image-set-setting.html
    
    * css/CSSImageSetValue.cpp:
    (WebCore::CSSImageSetValue::customCssText):
    * css/CSSValueList.h:
    (WebCore::CSSValueList::item): Add const overload
    
    LayoutTests:
    
    Update image-set-setting test to expect the css text to round-trip properly.
    
    * fast/css/image-set-setting-expected.txt:
    * fast/css/script-tests/image-set-setting.js:
    (testComputedStyle):
    (testImageSetRule):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/css/script-tests/image-set-setting.js
+++ third_party/WebKit/LayoutTests/fast/css/script-tests/image-set-setting.js
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Test the setting of the -webkit-image-set function.");
 
-function testComputedStyle(property, rule) 
+function testComputedStyle(property, fullRule)
 {
     var div = document.createElement("div");
     document.body.appendChild(div);
-    div.setAttribute("style", property + ": -webkit-image-set(" + rule + ")");
+    div.setAttribute("style", property + ": " + fullRule);
     var computedValue = div.style.background;
     document.body.removeChild(div);
     return computedValue;
@@ -16,22 +16,31 @@ function testImageSetRule(description, property, rule, expected)
     debug("");
     debug(description + " : " + rule);
 
-    shouldBeEqualToString('testComputedStyle("' + property + '", "' + rule + '")', expected);
+    var fullRule = "-webkit-image-set(" + rule + ")";
+    shouldBeEqualToString('testComputedStyle("' + property + '", "' + fullRule + '")', fullRule);
 }
 
 testImageSetRule("Single value for background-image",
                 "background-image",
-                "url('http://www.webkit.org/a') 1x", 
-                "-webkit-image-set(url(http://www.webkit.org/a), 1)");
+                "url(http://www.webkit.org/a) 1x");
 
 testImageSetRule("Multiple values for background-image",
                 "background-image",
-                "url('http://www.webkit.org/a') 1x, url('http://www.webkit.org/b') 2x", 
-                "-webkit-image-set(url(http://www.webkit.org/a), 1, url(http://www.webkit.org/b), 2)");
+                "url(http://www.webkit.org/a) 1x, url(http://www.webkit.org/b) 2x");
 
 testImageSetRule("Multiple values for background-image, out of order",
                 "background-image",
-                "url('http://www.webkit.org/c') 3x, url('http://www.webkit.org/b') 2x, url('http://www.webkit.org/a') 1x", 
-                "-webkit-image-set(url(http://www.webkit.org/c), 3, url(http://www.webkit.org/b), 2, url(http://www.webkit.org/a), 1)");
+                "url(http://www.webkit.org/c) 3x, url(http://www.webkit.org/b) 2x, url(http://www.webkit.org/a) 1x");
+
+testImageSetRule("Duplicate values for background-image",
+                "background-image",
+                "url(http://www.webkit.org/c) 1x, url(http://www.webkit.org/b) 2x, url(http://www.webkit.org/a) 1x");
+
+testImageSetRule("Fractional values for background-image",
+                "background-image",
+                "url(http://www.webkit.org/c) 0.2x, url(http://www.webkit.org/b) 2.3x, url(http://www.webkit.org/a) 12.3456x");
+
+// FIXME: We should also be testing the behavior of negative values somewhere, but it's currently
+// broken.  http://wkb.ug/100132
 
 successfullyParsed = true;
