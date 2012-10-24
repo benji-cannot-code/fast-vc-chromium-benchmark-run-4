158ccba211049b06cb99abe3467952336b3fd217
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 04:33:48 +0000

    REGRESSION(r130643): ASSERTION FAILED: result.iterator != end() below PluginDatabase::add <http://webkit.org/b/100065>
    Reviewed by Anders Carlsson.
    
    Restore the pre-r130643 hash table size for PluginDatabase to prevent these easily reproducible
    collisions. This will need a proper fix at some point.
    
    * plugins/PluginDatabase.h:
    * plugins/PluginPackage.h:
    (PluginPackageHashTraits):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132302 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/PluginDatabase.h
+++ third_party/WebKit/Source/WebCore/plugins/PluginDatabase.h
@@ -43,7 +43,7 @@ namespace WebCore {
     class PluginDatabaseClient;
     class PluginPackage;
 
-    typedef HashSet<RefPtr<PluginPackage>, PluginPackageHash> PluginSet;
+    typedef HashSet<RefPtr<PluginPackage>, PluginPackageHash, PluginPackageHashTraits> PluginSet;
 
     class PluginDatabase {
         WTF_MAKE_NONCOPYABLE(PluginDatabase); WTF_MAKE_FAST_ALLOCATED;
