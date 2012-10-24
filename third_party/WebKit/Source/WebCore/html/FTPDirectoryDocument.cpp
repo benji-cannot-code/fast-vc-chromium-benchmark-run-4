33167f68cdefbc28500094f01c08a6dfd35c7cdb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 17:29:50 +0000

    Incorrect conditional use of LogFTP https://bugs.webkit.org/show_bug.cgi?id=100260
    Patch by Cosmin Truta <ctruta@rim.com> on 2012-10-24
    Reviewed by Alexey Proskuryakov.
    
    Use LogFTP if !LOG_DISABLED, to allow toggling ASSERTIONS_DISABLED_DEFAULT
    without breaking the build.
    
    * html/FTPDirectoryDocument.cpp:
    (WebCore::FTPDirectoryDocument::FTPDirectoryDocument):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132368 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/FTPDirectoryDocument.cpp
+++ third_party/WebKit/Source/WebCore/html/FTPDirectoryDocument.cpp
@@ -432,7 +432,7 @@ void FTPDirectoryDocumentParser::finish()
 FTPDirectoryDocument::FTPDirectoryDocument(Frame* frame, const KURL& url)
     : HTMLDocument(frame, url)
 {
-#ifndef NDEBUG
+#if !LOG_DISABLED
     LogFTP.state = WTFLogChannelOn;
 #endif
 }
