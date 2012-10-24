df81f8c3909d412d43fa7e739be1dbb44900b47e
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 22:25:58 +0000

    cc: Make --jank-instead-of-checkerboard jank more often
    Prior to this patch, "force drawn" frames would also get drawn.  As frames
    get forced after commit if three frames in a row fail to draw, this happens
    fairly often.
    
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11294003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163928 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler_state_machine.cc
+++ cc/scheduler_state_machine.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/stringprintf.h"
+#include "cc/settings.h"
 
 namespace cc {
 
@@ -264,7 +265,7 @@ void SchedulerStateMachine::didDrawIfPossibleCompleted(bool success)
         m_needsRedraw = true;
         m_needsCommit = true;
         m_consecutiveFailedDraws++;
-        if (m_consecutiveFailedDraws >= m_maximumNumberOfFailedDrawsBeforeDrawIsForced) {
+        if (!Settings::jankInsteadOfCheckerboard() && m_consecutiveFailedDraws >= m_maximumNumberOfFailedDrawsBeforeDrawIsForced) {
             m_consecutiveFailedDraws = 0;
             // We need to force a draw, but it doesn't make sense to do this until
             // we've committed and have new textures.
