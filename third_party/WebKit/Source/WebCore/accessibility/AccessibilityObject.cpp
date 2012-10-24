cde1011b29e0315a081438f62256e538c7dddee8
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 19:02:57 +0000

    AX: WebKit exposes abstract ARIA role range as AXSlider; should be generic AXGroup https://bugs.webkit.org/show_bug.cgi?id=100204
    Reviewed by Beth Dakin.
    
    Source/WebCore: 
    
    Remove the "range" role as a valid ARIA role.
    
    Test: platform/mac/accessibility/aria-slider-value.html
    
    * accessibility/AccessibilityObject.cpp:
    (WebCore::createARIARoleMap):
    
    LayoutTests: 
    
    Remove the aria-range.html test because the same functionality is covered in aria-slider.html
    Rename aria-range-value to aria-slider-value and use the non-abstract role slider instead (and 
    move that to the general accessibility platform).
    
    * accessibility/aria-slider-value-expected.txt: Copied from LayoutTests/platform/mac/accessibility/aria-range-value-expected.txt.
    * accessibility/aria-slider-value.html: Copied from LayoutTests/platform/mac/accessibility/aria-range-value.html.
    * platform/mac/accessibility/aria-range-expected.txt: Removed.
    * platform/mac/accessibility/aria-range-value-expected.txt: Removed.
    * platform/mac/accessibility/aria-range-value.html: Removed.
    * platform/mac/accessibility/aria-range.html: Removed.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132381 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityObject.cpp
@@ -1418,7 +1418,6 @@ static ARIARoleMap* createARIARoleMap()
         { "radiogroup", RadioGroupRole },
         { "region", DocumentRegionRole },
         { "row", RowRole },
-        { "range", SliderRole },
         { "scrollbar", ScrollBarRole },
         { "search", LandmarkSearchRole },
         { "separator", SplitterRole },
