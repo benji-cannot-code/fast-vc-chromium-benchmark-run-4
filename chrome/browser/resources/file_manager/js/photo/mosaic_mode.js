ab8a2ae717b0f623d2dc378c127f2443b234c3c9
dgozman@chromium.org <dgozman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 14:09:15 +0000

    [filemanager] Properly invoke file handlers at page reload.
    BUG=157331
    TEST=See bug.
    
    Review URL: https://chromiumcodereview.appspot.com/11229059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/photo/mosaic_mode.js
+++ chrome/browser/resources/file_manager/js/photo/mosaic_mode.js
@@ -139,6 +139,13 @@ Mosaic.prototype.init = function() {
 };
 
 /**
+ * @return {boolean} Whether mosaic is initialized.
+ */
+Mosaic.prototype.isInitialized = function() {
+  return !!this.tiles_;
+};
+
+/**
  * Start listening to events.
  *
  * We keep listening to events even when the mosaic is hidden in order to
