49e1fcd5f50775498986dd6ddd33ff7e94e07cbb
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 15:12:43 +0000

    This keeps a sorted history of the texture upload rate and increases the history size to improve consistency/accuracy and overhead.
    Prior to this patch, calling ThrottledTextureUploader::estimatedTexturesPerSecond
    ran a sort operation, which will be slow for larger history sizes.
    
    Instead of keeping a FIFO history, it keeps sorted history and discards
    the min and max values.
        
    BUG=155172
    
    Review URL: https://chromiumcodereview.appspot.com/11028129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163828 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/texture_uploader.h
+++ cc/texture_uploader.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "cc/scoped_ptr_deque.h"
-#include <deque>
+#include <set>
 #include "third_party/khronos/GLES2/gl2.h"
 
 namespace WebKit {
@@ -88,7 +88,7 @@ private:
     WebKit::WebGraphicsContext3D* m_context;
     ScopedPtrDeque<Query> m_pendingQueries;
     ScopedPtrDeque<Query> m_availableQueries;
-    std::deque<double> m_texturesPerSecondHistory;
+    std::multiset<double> m_texturesPerSecondHistory;
     size_t m_numBlockingTextureUploads;
 
     bool m_useMapTexSubImage;
