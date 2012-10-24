cea8cf91eb536ce1144878db044d6f17ac2fe92a
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 11:22:17 +0000

    2nd try: Add OnSyncEnabled/OnWriteEnabled notification handling to operation runner
    Original review: https://chromiumcodereview.appspot.com/11238054
    1st submit (http://crrev.com/163584) got reverted because it broke win_aura/win7 bots.
    
    * Slightly changing ownership relationship: now LocalFileSyncContext owns both SyncableFileOperationRunner and LocalFileSyncStatus (previously SyncContext used to own OperationRunner that owns SyncStatus)
    * Adding OnSyncEnabled, OnWriteEnabled notifications to the LocalFileSyncStatus
    * Let SyncableFileOperation listen to OnWriteEnabled notification so that it can automatically start write operations (as far as the # of operations is less than the given max)
    
    BUG=148897, 154234
    TEST=SyncableFileOperationRunnerTest.*
    
    Review URL: https://codereview.chromium.org/11271003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163812 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/syncable/syncable_file_operation_runner_unittest.cc
+++ webkit/fileapi/syncable/syncable_file_operation_runner_unittest.cc
@@ -76,16 +76,8 @@ class SyncableFileOperationRunnerTest : public testing::Test {
     return file_system_.URL(path);
   }
 
-  LocalFileSyncContext* sync_context() {
-    return file_system_.file_system_context()->sync_context();
-  }
-
-  SyncableFileOperationRunner* operation_runner() {
-    return sync_context()->operation_runner().get();
-  }
-
   LocalFileSyncStatus* sync_status() {
-    return operation_runner()->sync_status();
+    return file_system_.file_system_context()->sync_context()->sync_status();
   }
 
   void ResetCallbackStatus() {
@@ -165,7 +157,6 @@ TEST_F(SyncableFileOperationRunnerTest, SimpleQueue) {
   ASSERT_TRUE(sync_status()->IsWritable(URL(kFile)));
 
   ResetCallbackStatus();
-  operation_runner()->RunNextRunnableTask();
   MessageLoop::current()->RunAllPending();
   EXPECT_EQ(2, callback_count_);
 
@@ -186,13 +177,10 @@ TEST_F(SyncableFileOperationRunnerTest, WriteToParentAndChild) {
   sync_status()->StartSyncing(URL(kDir));
   ASSERT_FALSE(sync_status()->IsWritable(URL(kDir)));
 
-  // Writes to kParent, kDir and kChild should be all queued up.
+  // Writes to kParent and kChild should be all queued up.
   ResetCallbackStatus();
   file_system_.NewOperation()->Truncate(
       URL(kChild), 1, ExpectStatus(FROM_HERE, base::PLATFORM_FILE_OK));
-  file_system_.NewOperation()->Remove(
-      URL(kDir), true /* recursive */,
-      ExpectStatus(FROM_HERE, base::PLATFORM_FILE_OK));
   file_system_.NewOperation()->Remove(
       URL(kParent), true /* recursive */,
       ExpectStatus(FROM_HERE, base::PLATFORM_FILE_OK));
@@ -218,9 +206,8 @@ TEST_F(SyncableFileOperationRunnerTest, WriteToParentAndChild) {
   ASSERT_TRUE(sync_status()->IsWritable(URL(kDir)));
 
   ResetCallbackStatus();
-  operation_runner()->RunNextRunnableTask();
   MessageLoop::current()->RunAllPending();
-  EXPECT_EQ(3, callback_count_);
+  EXPECT_EQ(2, callback_count_);
 }
 
 TEST_F(SyncableFileOperationRunnerTest, CopyAndMove) {
@@ -263,7 +250,6 @@ TEST_F(SyncableFileOperationRunnerTest, CopyAndMove) {
   // Finish syncing the "dest-copy2" directory to unlock Copy.
   sync_status()->EndSyncing(URL("dest-copy2"));
   ResetCallbackStatus();
-  operation_runner()->RunNextRunnableTask();
   MessageLoop::current()->RunAllPending();
   EXPECT_EQ(1, callback_count_);
 
@@ -274,7 +260,6 @@ TEST_F(SyncableFileOperationRunnerTest, CopyAndMove) {
   // Finish syncing the kParent to unlock Move.
   sync_status()->EndSyncing(URL(kParent));
   ResetCallbackStatus();
-  operation_runner()->RunNextRunnableTask();
   MessageLoop::current()->RunAllPending();
   EXPECT_EQ(1, callback_count_);
 
@@ -300,7 +285,6 @@ TEST_F(SyncableFileOperationRunnerTest, Write) {
 
   sync_status()->EndSyncing(URL(kFile));
   ResetCallbackStatus();
-  operation_runner()->RunNextRunnableTask();
 
   while (!write_complete_)
     MessageLoop::current()->RunAllPending();
