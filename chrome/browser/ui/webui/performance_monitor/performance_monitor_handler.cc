763bbc907947e10aaf7dcd87af6e8323ad391b75
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 01:29:14 +0000

    New features for CPM UI pt II
    Localized strings for HTML constants; automatically select events and metrics so
    that something is seen immediately on startup; warn the user if the gathering
    switch is not enabled; add the flag to the about:flags.
    
    BUG=130212
    
    
    Review URL: https://chromiumcodereview.appspot.com/11053010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163736 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/performance_monitor/performance_monitor_handler.cc
+++ chrome/browser/ui/webui/performance_monitor/performance_monitor_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/command_line.h"
 #include "base/time.h"
 #include "base/values.h"
 #include "chrome/browser/performance_monitor/database.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/performance_monitor/performance_monitor_l10n.h"
 #include "chrome/browser/ui/webui/performance_monitor/performance_monitor_ui_constants.h"
 #include "chrome/browser/ui/webui/performance_monitor/performance_monitor_ui_util.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/value_builder.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/web_ui.h"
@@ -366,6 +368,10 @@ void PerformanceMonitorHandler::RegisterMessages() {
       base::Bind(&PerformanceMonitorHandler::HandleGetActiveIntervals,
                  AsWeakPtr()));
   web_ui()->RegisterMessageCallback(
+      "getFlagEnabled",
+      base::Bind(&PerformanceMonitorHandler::HandleGetFlagEnabled,
+                 AsWeakPtr()));
+  web_ui()->RegisterMessageCallback(
       "getAggregationTypes",
       base::Bind(&PerformanceMonitorHandler::HandleGetAggregationTypes,
                  AsWeakPtr()));
@@ -410,6 +416,15 @@ void PerformanceMonitorHandler::HandleGetActiveIntervals(
       base::Bind(&PerformanceMonitorHandler::ReturnResults, AsWeakPtr(),
                  "PerformanceMonitor.getActiveIntervalsCallback",
                  base::Owned(results)));
+}
+
+void PerformanceMonitorHandler::HandleGetFlagEnabled(const ListValue* args) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+  CHECK_EQ(0u, args->GetSize());
+  scoped_ptr<Value> value(Value::CreateBooleanValue(
+      CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kPerformanceMonitorGathering)));
+  ReturnResults("PerformanceMonitor.getFlagEnabledCallback", value.get());
 }
 
 void PerformanceMonitorHandler::HandleGetAggregationTypes(
