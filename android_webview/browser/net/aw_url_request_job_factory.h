df3757882f09addabbdb1f664ca25f953005003a
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-24 11:20:52 +0000

    [android_webview] Fix clang build.
    Added missing destructor.
    
    BUG=
    TBR=benm@chromium.org
    
    Review URL: https://codereview.chromium.org/11264007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163811 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/net/aw_url_request_job_factory.h
+++ android_webview/browser/net/aw_url_request_job_factory.h
@@ -20,6 +20,7 @@ namespace android_webview {
 class AwURLRequestJobFactory : public net::URLRequestJobFactory {
  public:
   AwURLRequestJobFactory();
+  virtual ~AwURLRequestJobFactory();
 
   virtual bool SetProtocolHandler(const std::string& scheme,
                           ProtocolHandler* protocol_handler) OVERRIDE;
