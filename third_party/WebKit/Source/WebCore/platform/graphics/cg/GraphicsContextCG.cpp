0ce15d035cbda7db5139f9269880aaa042850de6
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 19:30:45 +0000

    Honor image orientation in GraphicsContextSkia https://bugs.webkit.org/show_bug.cgi?id=100179
    Reviewed by Stephen White.
    
    Also fix a bug in ImageOrientation.h: Some of the values were
    switched. They now match the description in the exif spec at
    http://www.exif.org/Exif2-2.PDF page 18, and the notes at
    http://www.sno.phy.queensu.ca/~phil/exiftool/TagNames/EXIF.html and
    the picture at http://www.80sidea.com/archives/2316.
    
    They showed up correctly in Safari because the orientation transform
    was done after flipping Y. Let the flipping happen later. I verified
    that flipping transformation order and fixing the exif enum cancelled
    each other out, so this has no effect on the apple port.
    
    Not hooked up yet, so no observable difference.
    
    * platform/graphics/BitmapImage.h:
    * platform/graphics/ImageOrientation.h:
    (WebCore::ImageOrientation::usesWidthAsHeight):
    (WebCore::ImageOrientation::fromEXIFValue):
    * platform/graphics/cg/GraphicsContextCG.cpp:
    (WebCore::GraphicsContext::drawNativeImage):
    * platform/graphics/skia/ImageSkia.cpp:
    (WebCore::paintSkBitmap):
    (WebCore::BitmapImage::draw):
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/cg/GraphicsContextCG.cpp
@@ -327,11 +327,6 @@ void GraphicsContext::drawNativeImage(NativeImagePtr imagePtr, const FloatSize& 
 
     setPlatformCompositeOperation(op);
 
-    // Flip the coords.
-    CGContextTranslateCTM(context, adjustedDestRect.x(), adjustedDestRect.maxY());
-    CGContextScaleCTM(context, 1, -1);
-    adjustedDestRect.setLocation(FloatPoint());
-
     if (orientation != DefaultImageOrientation) {
         CGContextConcatCTM(context, orientation.transformFromDefault(adjustedDestRect.size()));
         if (orientation.usesWidthAsHeight()) {
@@ -341,6 +336,11 @@ void GraphicsContext::drawNativeImage(NativeImagePtr imagePtr, const FloatSize& 
         }
     }
     
+    // Flip the coords.
+    CGContextTranslateCTM(context, adjustedDestRect.x(), adjustedDestRect.maxY());
+    CGContextScaleCTM(context, 1, -1);
+    adjustedDestRect.setLocation(FloatPoint());
+
     // Adjust the color space.
     image = Image::imageWithColorSpace(image.get(), styleColorSpace);
 
