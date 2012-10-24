0d936478b92257f88dbd060ed66a79e3602ee15b
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 17:36:32 +0000

    Show an error when there are no completed builds in the past 20 runs https://bugs.webkit.org/show_bug.cgi?id=100266
    Reviewed by Dimitri Glazkov.
    
    Right now we get a cryptic JS error in the console when loading the gtk port. This at least gives an error in
    the notification stream. We should probably tweak this UI to make it so we can make
    errors in the notification stream stick out (e.g. make the text red).
    
    We should also consider making the number of runs to load dynamic (e.g. if 20 runs don't work,
    try 40).
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/garden-o-matic.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results_unittests.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132369 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/results.js
@@ -627,6 +627,11 @@ results.fetchResultsForMostRecentCompletedBuildOnBuilder = function(builderName,
         var resultsCallback = function(buildResults) {
             if ($.isEmptyObject(buildResults)) {
                 ++currentIndex;
+                if (currentIndex >= buildLocations.length) {
+                    callback(null);
+                    return;
+                }
+
                 net.jsonp(buildLocations[currentIndex].url, resultsCallback);
                 return;
             }
@@ -659,7 +664,8 @@ results.fetchResultsByBuilder = function(builderNameList, callback)
                     // FIXME: use RequestTracker
                     ++requestsInFlight;
                     results.fetchResultsForMostRecentCompletedBuildOnBuilder(builderName, function(resultsTree) {
-                        resultsByBuilder[builderName] = resultsTree;
+                        if (resultsTree)
+                            resultsByBuilder[builderName] = resultsTree;
                         --requestsInFlight;
                         if (!requestsInFlight)
                             callback(resultsByBuilder);
