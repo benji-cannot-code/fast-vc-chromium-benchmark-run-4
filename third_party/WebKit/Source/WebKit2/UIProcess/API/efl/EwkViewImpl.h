4507d01fc9ea084e2faceec8e7cfc282cb1028ff
dominik.rottsches@intel.com <dominik.rottsches@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-24 14:41:19 +0000

    [EFL][AC] Fix build break when AC is enabled https://bugs.webkit.org/show_bug.cgi?id=100245
    Patch by Sudarsana Nagineni <sudarsana.nagineni@intel.com> on 2012-10-24
    Reviewed by Kenneth Rohde Christiansen.
    
    Fix build break after r132337.
    
    * UIProcess/API/efl/EwkViewImpl.h:
    * UIProcess/API/efl/ewk_view.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
@@ -31,6 +31,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/OwnPtr.h>
 #include <wtf/RefPtr.h>
 
+#if USE(ACCELERATED_COMPOSITING)
+#include <Evas_GL.h>
+#endif
+
 #define EWK_VIEW_PRIV_GET(smartData, priv)                                     \
     EwkViewImpl* priv = smartData->priv
 
--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
@@ -76,10 +76,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebInspectorProxy.h"
 #endif
 
-#if USE(ACCELERATED_COMPOSITING)
-#include <Evas_GL.h>
-#endif
-
 #if USE(TILED_BACKING_STORE)
 #include "PageViewportController.h"
 #include "PageViewportControllerClientEfl.h"
