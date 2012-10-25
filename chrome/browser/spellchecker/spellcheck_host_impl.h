dfc430de9c29cedeb8c87f6a4eeb8c4ee3e6e82f
jhorwich@chromium.org <jhorwich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 19:57:28 +0000

    Revert 164142 - Pulling custom dictionary out of spellcheck_profile as prep to combine profile and host.
    (check_perms flagged the added files)
    
    BUG=154921
    
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11175041
    
    TBR=rlp@chromium.org
    Review URL: https://codereview.chromium.org/11269057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164152 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_host_impl.h
+++ chrome/browser/spellchecker/spellcheck_host_impl.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/sequenced_task_runner_helpers.h"
-#include "chrome/browser/spellchecker/spellcheck_custom_dictionary.h"
 #include "chrome/browser/spellchecker/spellcheck_host.h"
 #include "chrome/browser/spellchecker/spellcheck_profile_provider.h"
 #include "content/public/browser/notification_observer.h"
@@ -67,7 +66,7 @@ class SpellCheckHostImpl : public SpellCheckHost,
   virtual bool IsUsingPlatformChecker() const OVERRIDE;
 
  private:
-  typedef chrome::spellcheck_common::WordList CustomWordList;
+  typedef SpellCheckProfileProvider::CustomWordList CustomWordList;
 
   // These two classes can destruct us.
   friend class content::BrowserThread;
--- chrome/browser/spellchecker/spellcheck_profile.h
+++ chrome/browser/spellchecker/spellcheck_profile.h
@@ -17,8 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/public/pref_change_registrar.h"
 #include "chrome/browser/profiles/profile_keyed_service.h"
 #include "chrome/browser/spellchecker/spellcheck_profile_provider.h"
-#include "chrome/browser/spellchecker/spellcheck_custom_dictionary.h"
-#include "chrome/common/spellcheck_common.h"
 #include "content/public/browser/notification_observer.h"
 
 class Profile;
@@ -60,13 +58,10 @@ class SpellCheckProfile : public SpellCheckProfileProvider,
   void StartRecordingMetrics(bool spellcheck_enabled);
 
   // SpellCheckProfileProvider implementation.
-  virtual void SpellCheckHostInitialized(
-      chrome::spellcheck_common::WordList* custom_words) OVERRIDE;
-  virtual const chrome::spellcheck_common::WordList&
-      GetCustomWords() const OVERRIDE;
+  virtual void SpellCheckHostInitialized(CustomWordList* custom_words) OVERRIDE;
+  virtual const CustomWordList& GetCustomWords() const OVERRIDE;
   virtual void CustomWordAddedLocally(const std::string& word) OVERRIDE;
-  virtual void LoadCustomDictionary(
-      chrome::spellcheck_common::WordList* custom_words) OVERRIDE;
+  virtual void LoadCustomDictionary(CustomWordList* custom_words) OVERRIDE;
   virtual void WriteWordToCustomDictionary(const std::string& word) OVERRIDE;
 
   // ProfileKeyedService implementation.
@@ -125,6 +120,8 @@ class SpellCheckProfile : public SpellCheckProfileProvider,
       const std::string& language,
       net::URLRequestContextGetter* request_context);
 
+  const FilePath& GetCustomDictionaryPath();
+
   PrefChangeRegistrar pref_change_registrar_;
 
   Profile* profile_;
@@ -136,10 +133,14 @@ class SpellCheckProfile : public SpellCheckProfileProvider,
   // finished.
   bool host_ready_;
 
+  // In-memory cache of the custom words file.
+  scoped_ptr<CustomWordList> custom_words_;
+
   // A directory path of profile.
   FilePath profile_dir_;
 
-  scoped_ptr<SpellcheckCustomDictionary> custom_dictionary_;
+  // A path for custom dictionary per profile.
+  scoped_ptr<FilePath> custom_dictionary_path_;
 
   DISALLOW_COPY_AND_ASSIGN(SpellCheckProfile);
 };
