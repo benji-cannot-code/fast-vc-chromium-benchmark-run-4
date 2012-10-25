4caa85008e5050b6d127a67fae92e5c5b7c4b563
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 11:21:15 +0000

    Make GL calls go through subclassable class.
    This is a step to virtualizing GL
    
    BUG=155557
    
    
    Review URL: https://chromiumcodereview.appspot.com/11266023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164047 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_implementation_android.cc
+++ ui/gl/gl_implementation_android.cc
@@ -10,7 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/native_library.h"
 #include "base/path_service.h"
 #include "ui/gl/gl_bindings.h"
+#include "ui/gl/gl_egl_api_implementation.h"
+#include "ui/gl/gl_gl_api_implementation.h"
 #include "ui/gl/gl_implementation.h"
+#include "ui/gl/gl_osmesa_api_implementation.h"
 
 namespace gfx {
 
@@ -84,8 +87,8 @@ bool InitializeGLBindings(GLImplementation implementation) {
 
       // These two functions take single precision float rather than double
       // precision float parameters in GLES.
-      ::gfx::g_glClearDepth = MarshalClearDepthToClearDepthf;
-      ::gfx::g_glDepthRange = MarshalDepthRangeToDepthRangef;
+      ::gfx::g_driver_gl.fn.glClearDepthFn = MarshalClearDepthToClearDepthf;
+      ::gfx::g_driver_gl.fn.glDepthRangeFn = MarshalDepthRangeToDepthRangef;
       break;
     }
     case kGLImplementationMockGL: {
