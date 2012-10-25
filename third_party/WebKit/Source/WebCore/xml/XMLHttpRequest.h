88594000229a3de40a7515342ef6df5a9257d283
dominik.rottsches@intel.com <dominik.rottsches@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-25 19:26:47 +0000

    .: Adding feature for XHR_TIMEOUT
    Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding feature for XHR_TIMEOUT to disable it on ports that don't have
    network backend support for setTimeoutInterval.
    
    * Source/cmake/OptionsEfl.cmake: Default ON on EFL.
    * Source/cmake/WebKitFeatures.cmake: Default OFF for any CMAKE based port.
    * Source/cmakeconfig.h.cmake: Adding define.
    * configure.ac: Make autogen.sh support the --enable/--disable-xhr-timeout parameter.
    
    Source/JavaScriptCore: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding XHR_TIMEOUT feature to conditionalize this on ports without network backend support.
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebCore: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding feature for XHR_TIMEOUT to disable it on ports that don't have
    network backend support for setTimeoutInterval.
    
    No new tests, covered by http/tests/xmlhttprequest/timeout/*
    
    * Configurations/FeatureDefines.xcconfig: Default ON on mac.
    * GNUmakefile.am:
    * GNUmakefile.features.am:
      Autotools support for the flag.
    * xml/XMLHttpRequest.cpp: #if ENABLE(XHR_TIMEOUT) wrapping
    (WebCore::XMLHttpRequest::XMLHttpRequest):
    (WebCore):
    (WebCore::XMLHttpRequest::open):
    (WebCore::XMLHttpRequest::createRequest):
    (WebCore::XMLHttpRequest::didFail):
    * xml/XMLHttpRequest.h: #if ENABLE(XHR_TIMEOUT) wrapping
    (XMLHttpRequest):
    * xml/XMLHttpRequest.idl: Conditional properties timeout and ontimeout (event handler).
    
    Source/WebKit/chromium: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding XHR_TIMEOUT feature to conditionalize this on ports without network backend support.
    Defaults to off on chromium until webkit.org/b/98397 is solved.
    
    * features.gypi:
    
    Source/WebKit/mac: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding XHR_TIMEOUT feature to conditionalize this on ports without network backend support.
    Defaults to ON on Mac since the Mac NSUrlConnection based backend has setTimeoutInterval support.
    
    * Configurations/FeatureDefines.xcconfig:
    
    Source/WebKit2: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding XHR_TIMEOUT feature to conditionalize this on ports without network backend support.
    Defaults to ON on Mac since the Mac NSUrlConnection based backend has setTimeoutInterval support.
    
    * Configurations/FeatureDefines.xcconfig:
    
    Tools: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding XHR_TIMEOUT feature in perl and qmake files.
    
    * Scripts/webkitperl/FeatureList.pm:
    * qmake/mkspecs/features/features.pri:
    
    WebKitLibraries: Conditionalize XHR timeout support
    https://bugs.webkit.org/show_bug.cgi?id=100356
    
    Reviewed by Adam Barth.
    
    Adding ENABLE_XHR_TIMEOUT feature, which is default off for Apple Win.
    See also https://bugs.webkit.org/show_bug.cgi?id=100349 for an attempt
    to fix timeout support for CF Network.
    
    * win/tools/vsprops/FeatureDefines.vsprops:
    * win/tools/vsprops/FeatureDefinesCairo.vsprops:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/xml/XMLHttpRequest.h
+++ third_party/WebKit/Source/WebCore/xml/XMLHttpRequest.h
@@ -71,7 +71,9 @@ public:
     };
 
     virtual void contextDestroyed();
+#if ENABLE(XHR_TIMEOUT)
     virtual void didTimeout();
+#endif
     virtual bool canSuspend() const;
     virtual void suspend(ReasonForSuspension);
     virtual void resume();
@@ -107,8 +109,10 @@ public:
     Document* optionalResponseXML() const { return m_responseDocument.get(); }
     Blob* responseBlob(ExceptionCode&);
     Blob* optionalResponseBlob() const { return m_responseBlob.get(); }
+#if ENABLE(XHR_TIMEOUT)
     unsigned long timeout() const { return m_timeoutMilliseconds; }
     void setTimeout(unsigned long timeout, ExceptionCode&);
+#endif
 
     void sendFromInspector(PassRefPtr<FormData>, ExceptionCode&);
 
@@ -138,7 +142,9 @@ public:
     DEFINE_ATTRIBUTE_EVENT_LISTENER(loadend);
     DEFINE_ATTRIBUTE_EVENT_LISTENER(loadstart);
     DEFINE_ATTRIBUTE_EVENT_LISTENER(progress);
+#if ENABLE(XHR_TIMEOUT)
     DEFINE_ATTRIBUTE_EVENT_LISTENER(timeout);
+#endif
 
     using RefCounted<XMLHttpRequest>::ref;
     using RefCounted<XMLHttpRequest>::deref;
@@ -197,7 +203,9 @@ private:
     String m_mimeTypeOverride;
     bool m_async;
     bool m_includeCredentials;
+#if ENABLE(XHR_TIMEOUT)
     unsigned long m_timeoutMilliseconds;
+#endif
     RefPtr<Blob> m_responseBlob;
 
     RefPtr<ThreadableLoader> m_loader;
--- third_party/WebKit/Tools/Scripts/webkitperl/FeatureList.pm
+++ third_party/WebKit/Tools/Scripts/webkitperl/FeatureList.pm
@@ -140,6 +140,7 @@ my (
     $webTimingSupport,
     $workersSupport,
     $xhrResponseBlobSupport,
+    $xhrTimeoutSupport,
     $xsltSupport,
 );
 
@@ -431,6 +432,9 @@ my @features = (
 
     { option => "xhr-response-blob", desc => "Toggle XHR Response BLOB support",
       define => "ENABLE_XHR_RESPONSE_BLOB", default => isBlackBerry(), value => \$xhrResponseBlobSupport },
+
+    { option => "xhr-timeout", desc => "Toggle XHR Timeout support",
+      define => "ENABLE_XHR_TIMEOUT", default => (isEfl() || isGtk() || isAppleMacWebKit()), value => \$xhrTimeoutSupport },
 
     { option => "xslt", desc => "Toggle XSLT support",
       define => "ENABLE_XSLT", default => 1, value => \$xsltSupport },
