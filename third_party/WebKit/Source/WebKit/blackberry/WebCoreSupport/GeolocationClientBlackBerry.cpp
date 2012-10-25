1c7480a0b4036398b41499fee89ff1ec6b10735a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-25 02:01:16 +0000

    [BlackBerry] Geolocation is segfaulting https://bugs.webkit.org/show_bug.cgi?id=100305
    Patch by Otto Derek Cheung <otcheung@rim.com> on 2012-10-24
    Reviewed by Rob Buis.
    
    We weren't wrapping the Geolocation object in a RefPtr when we call setIsAllowed in
    GeolocationClientBlackBerry::onPermission.
    
    This patch also removes all references of the Geolocation object in WebPageClientImpl.
    One RefPtr<Geolocation> is used to store the reference of the WebCore object on our port.
    
    We don't need a mapping between origin -> context of Geolocation because in our port,
    one WebPage.cpp contains one GeolocationController only.
    
    * Api/WebPageClient.h:
    * WebCoreSupport/GeolocationClientBlackBerry.cpp:
    (frameOrigin):
    (GeolocationClientBlackBerry::requestPermission):
    (GeolocationClientBlackBerry::cancelPermissionRequest):
    (GeolocationClientBlackBerry::onPermission):
    * WebCoreSupport/GeolocationClientBlackBerry.h:
    (GeolocationClientBlackBerry):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/GeolocationClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/GeolocationClientBlackBerry.cpp
@@ -27,14 +27,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Page.h"
 #include "WebPage_p.h"
 
+#include <BlackBerryPlatformString.h>
+
 using namespace WebCore;
 
-static CString frameOrigin(Frame* frame)
+static const BlackBerry::Platform::String frameOrigin(Frame* frame)
 {
     DOMWindow* window = frame->document()->domWindow();
     SecurityOrigin* origin = window->document()->securityOrigin();
-    CString latinOrigin = origin->toString().latin1();
-    return latinOrigin;
+    return BlackBerry::Platform::String(origin->toString().utf8().data());
 }
 
 GeolocationClientBlackBerry::GeolocationClientBlackBerry(BlackBerry::WebKit::WebPagePrivate* webPagePrivate)
@@ -75,15 +76,22 @@ void GeolocationClientBlackBerry::requestPermission(Geolocation* location)
     Frame* frame = location->frame();
     if (!frame)
         return;
-    m_webPagePrivate->m_page->chrome()->client()->requestGeolocationPermissionForFrame(frame, location);
+    if (!m_webPagePrivate->m_webSettings->isGeolocationEnabled()) {
+        location->setIsAllowed(false);
+        return;
+    }
+    DOMWindow* window = frame->document()->domWindow();
+    if (!window)
+        return;
+
+    const BlackBerry::Platform::String origin = frameOrigin(frame);
+    m_pendingPermissionGeolocation = location;
+    m_webPagePrivate->m_client->requestGeolocationPermission(this, origin);
 }
 
 void GeolocationClientBlackBerry::cancelPermissionRequest(Geolocation* location)
 {
-    Frame* frame = location->frame();
-    if (!frame)
-        return;
-    m_webPagePrivate->m_page->chrome()->client()->cancelGeolocationPermissionRequestForFrame(frame, location);
+    m_webPagePrivate->m_client->cancelGeolocationPermission();
 }
 
 void GeolocationClientBlackBerry::onLocationUpdate(double timestamp, double latitude, double longitude, double accuracy, double altitude, bool altitudeValid,
@@ -100,10 +108,10 @@ void GeolocationClientBlackBerry::onLocationError(const char* errorStr)
     GeolocationController::from(m_webPagePrivate->m_page)->errorOccurred(error.get());
 }
 
-void GeolocationClientBlackBerry::onPermission(void* context, bool isAllowed)
+void GeolocationClientBlackBerry::onPermission(bool isAllowed)
 {
-    Geolocation* position = static_cast<Geolocation*>(context);
-    position->setIsAllowed(isAllowed);
+    if (m_pendingPermissionGeolocation)
+        m_pendingPermissionGeolocation->setIsAllowed(isAllowed);
 }
 
 void GeolocationClientBlackBerry::setEnableHighAccuracy(bool newAccuracy)
