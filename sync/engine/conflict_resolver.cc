d31f0f08e2b1155dbfa3e0d92fc7b1e7e39df2aa
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 02:01:12 +0000

    sync: wrap conflict_utils in a namespace
    This makes it easier to hunt down function definitions and matches file hierarchy as suggested by the style guide.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11238043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@163992 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/conflict_resolver.cc
+++ sync/engine/conflict_resolver.cc
@@ -194,7 +194,7 @@ ConflictResolver::ProcessSimpleConflict(WriteTransaction* trans,
         !needs_reinsertion) {
       DVLOG(1) << "Resolving simple conflict, everything matches, ignoring "
                << "changes for: " << entry;
-      IgnoreConflict(&entry);
+      conflict_util::IgnoreConflict(&entry);
       UMA_HISTOGRAM_ENUMERATION("Sync.ResolveSimpleConflict",
                                 CHANGES_MATCH,
                                 CONFLICT_RESOLUTION_SIZE);
@@ -202,12 +202,12 @@ ConflictResolver::ProcessSimpleConflict(WriteTransaction* trans,
       DVLOG(1) << "Resolving simple conflict, ignoring server encryption "
                << " changes for: " << entry;
       status->increment_num_server_overwrites();
-      OverwriteServerChanges(&entry);
+      conflict_util::OverwriteServerChanges(&entry);
       UMA_HISTOGRAM_ENUMERATION("Sync.ResolveSimpleConflict",
                                 IGNORE_ENCRYPTION,
                                 CONFLICT_RESOLUTION_SIZE);
     } else if (entry_deleted || !name_matches || !parent_matches) {
-      OverwriteServerChanges(&entry);
+      conflict_util::OverwriteServerChanges(&entry);
       status->increment_num_server_overwrites();
       DVLOG(1) << "Resolving simple conflict, overwriting server changes "
                << "for: " << entry;
@@ -217,7 +217,7 @@ ConflictResolver::ProcessSimpleConflict(WriteTransaction* trans,
     } else {
       DVLOG(1) << "Resolving simple conflict, ignoring local changes for: "
                << entry;
-      IgnoreLocalChanges(&entry);
+      conflict_util::IgnoreLocalChanges(&entry);
       status->increment_num_local_overwrites();
       UMA_HISTOGRAM_ENUMERATION("Sync.ResolveSimpleConflict",
                                 OVERWRITE_LOCAL,
@@ -247,7 +247,7 @@ ConflictResolver::ProcessSimpleConflict(WriteTransaction* trans,
     // The entry is deleted on the server but still exists locally.
     // We undelete it by overwriting the server's tombstone with the local
     // data.
-    OverwriteServerChanges(&entry);
+    conflict_util::OverwriteServerChanges(&entry);
     status->increment_num_server_overwrites();
     DVLOG(1) << "Resolving simple conflict, undeleting server entry: "
              << entry;
