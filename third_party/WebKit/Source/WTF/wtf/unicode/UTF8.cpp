5428e5373f39fe7154ab2ed66a0e2f46b8c5a619
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-25 18:40:53 +0000

    REGRESSION (r131836): failures in list styles tests on EFL, GTK https://bugs.webkit.org/show_bug.cgi?id=99824
    Reviewed by Oliver Hunt.
    
    Source/JavaScriptCore:
    
    Saved start of string since it is modified by call convertUTF8ToUTF16().
    
    * API/JSStringRef.cpp:
    (JSStringCreateWithUTF8CString):
    
    Source/WTF:
    
    Fixed ASCII check.
    Added placeholder argument for new argument added to convertUTF8ToUTF16() in http://trac.webkit.org/changeset/131836.
    
    * wtf/unicode/UTF8.cpp:
    (WTF::Unicode::convertUTF8ToUTF16):
    * wtf/unicode/glib/UnicodeGLib.cpp:
    (WTF::Unicode::convertCase):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132507 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/unicode/UTF8.cpp
+++ third_party/WebKit/Source/WTF/wtf/unicode/UTF8.cpp
@@ -364,7 +364,7 @@ ConversionResult convertUTF8ToUTF16(
     *targetStart = target;
 
     if (sourceAllASCII)
-        *sourceAllASCII = !(orAllData & 0x7f);
+        *sourceAllASCII = !(orAllData & ~0x7f);
 
     return result;
 }
