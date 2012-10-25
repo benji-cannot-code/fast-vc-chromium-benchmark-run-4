43704bdf8f32aedad0236eba3e7ebce597c72c64
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-25 16:34:48 +0000

    Assertion failure trying to dispatch notification messages https://bugs.webkit.org/show_bug.cgi?id=100387 <rdar://problem/12573195>
    Reviewed by Jessie Berlin.
    
    Pass a zero destination ID when sending WebNotificationManagerProxy messages; they are not
    used by the UI process and since the WebNotificationManagerProxy message receiver is global
    passing a non-zero destination ID would cause the aforementioned assertion failure.
    
    * WebProcess/Notifications/WebNotificationManager.cpp:
    (WebKit::WebNotificationManager::cancel):
    (WebKit::WebNotificationManager::clearNotifications):
    (WebKit::WebNotificationManager::didDestroyNotification):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Notifications/WebNotificationManager.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Notifications/WebNotificationManager.cpp
@@ -175,7 +175,7 @@ void WebNotificationManager::cancel(Notification* notification, WebPage* page)
     if (!notificationID)
         return;
     
-    m_process->connection()->send(Messages::WebNotificationManagerProxy::Cancel(notificationID), page->pageID());
+    m_process->connection()->send(Messages::WebNotificationManagerProxy::Cancel(notificationID), 0);
 #else
     UNUSED_PARAM(notification);
     UNUSED_PARAM(page);
@@ -190,7 +190,7 @@ void WebNotificationManager::clearNotifications(WebCore::ScriptExecutionContext*
         return;
     
     Vector<uint64_t>& notificationIDs = it->value;
-    m_process->connection()->send(Messages::WebNotificationManagerProxy::ClearNotifications(notificationIDs), page->pageID());
+    m_process->connection()->send(Messages::WebNotificationManagerProxy::ClearNotifications(notificationIDs), 0);
     size_t count = notificationIDs.size();
     for (size_t i = 0; i < count; ++i) {
         RefPtr<Notification> notification = m_notificationIDMap.take(notificationIDs[i]);
@@ -216,7 +216,7 @@ void WebNotificationManager::didDestroyNotification(Notification* notification, 
 
     m_notificationIDMap.remove(notificationID);
     removeNotificationFromContextMap(notificationID, notification);
-    m_process->connection()->send(Messages::WebNotificationManagerProxy::DidDestroyNotification(notificationID), page->pageID());
+    m_process->connection()->send(Messages::WebNotificationManagerProxy::DidDestroyNotification(notificationID), 0);
 #else
     UNUSED_PARAM(notification);
     UNUSED_PARAM(page);
