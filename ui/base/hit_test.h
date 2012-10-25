b85f4c51893edde9cddadf46cbe7f2410176a6f6
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 17:19:42 +0000

    ash: Remove some inconsistency from two-finger window moving.
    Allow two-finger window moving only if both fingers are on the same edge of the window.
    
    BUG=151153
    
    Review URL: https://codereview.chromium.org/11269031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164094 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/hit_test.h
+++ ui/base/hit_test.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef VIEWS_WINDOW_HIT_TEST_H_
-#define VIEWS_WINDOW_HIT_TEST_H_
+#ifndef UI_BASE_HIT_TEST_H_
+#define UI_BASE_HIT_TEST_H_
 
 #if !defined(OS_WIN)
 
@@ -43,4 +43,4 @@ enum HitTestCompat {
 
 #endif  // !defined(OS_WIN)
 
-#endif  // VIEWS_WINDOW_HIT_TEST_H_
+#endif  // UI_BASE_HIT_TEST_H_
