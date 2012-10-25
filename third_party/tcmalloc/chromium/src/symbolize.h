85dec48c8bb0278b4d274b9574f71f0042650bed
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-25 13:07:31 +0000

    Remove unnecessary #include and fix style.
    BUG=157121
    
    
    Review URL: https://chromiumcodereview.appspot.com/11270007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164058 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/symbolize.h
+++ third_party/tcmalloc/chromium/src/symbolize.h
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #ifdef HAVE_STDINT_H
-#include <stddef.h>
 #include <stdint.h>  // for uintptr_t
 #endif
 #include <stddef.h>  // for NULL
