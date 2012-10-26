8b83d65d68ef0cf8af9a53b12b3cc91e9f75e83d
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 15:54:05 +0000

    Fix extension event dispatch for split mode extensions.
    Previously extensions with loaded background pages that were also split
    mode (i.e. with an unloaded incognito mode background page) would not
    receive events.
    
    BUG=156484
    TEST=on ChromeOS, the calculator app can be closed and immediately reopened
    
    
    Review URL: https://chromiumcodereview.appspot.com/11260041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164339 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/event_router.h
+++ chrome/browser/extensions/event_router.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <set>
 #include <string>
+#include <utility>
 
 #include "base/compiler_specific.h"
 #include "base/memory/linked_ptr.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/extensions/event_listener_map.h"
 #include "chrome/common/extensions/event_filtering_info.h"
+#include "content/public/browser/browser_context.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "ipc/ipc_sender.h"
@@ -170,6 +172,11 @@ class EventRouter : public content::NotificationObserver,
   // to that event.
   typedef std::map<std::string, std::set<ListenerProcess> > ListenerMap;
 
+  // An identifier for an event dispatch that is used to prevent double dispatch
+  // due to race conditions between the direct and lazy dispatch paths.
+  typedef std::pair<const content::BrowserContext*, std::string>
+      EventDispatchIdentifier;
+
   // TODO(gdk): Document this.
   static void DispatchExtensionMessage(
       IPC::Sender* ipc_sender,
@@ -199,10 +206,11 @@ class EventRouter : public content::NotificationObserver,
 
   // Ensures that all lazy background pages that are interested in the given
   // event are loaded, and queues the event if the page is not ready yet.
-  // Returns true if the event was queued for subsequent dispatch, false
-  // otherwise.
-  bool DispatchLazyEvent(const std::string& extension_id,
-                         const linked_ptr<Event>& event);
+  // Inserts an EventDispatchIdentifier into |already_dispatched| for each lazy
+  // event dispatch that is queued.
+  void DispatchLazyEvent(const std::string& extension_id,
+                         const linked_ptr<Event>& event,
+                         std::set<EventDispatchIdentifier>* already_dispatched);
 
   // Dispatches the event to the specified extension running in |process|.
   void DispatchEventToProcess(const std::string& extension_id,
