5d3621cc6f84820bc0e2609e4d60cfb55641498b
pdr@google.com <pdr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-26 22:29:27 +0000

    Prevent NaN offset values in ElementTimeControl. https://bugs.webkit.org/show_bug.cgi?id=100322
    Reviewed by Abhishek Arya.
    
    Source/WebCore:
    
    NaN values can cause ElementTimeControl to go back in time!
    If a value of NaN is passed to ElementTimeControl::beginElementAt(offset),
    subsequent sorting will cause an assert in SVGSMILElement::findInstanceTime
    because NaN values are not properly sorted. NaN SMILTime values
    should not be allowed at all, so this patch adds a check for them in
    ElementTimeControl's setters.
    
    This patch also adds preventative asserts to catch if SMILTime is ever
    initialized with NaN, or if addEndTime/addBeginTime are ever called
    with NaN values.
    
    Test: svg/custom/elementTimeControl-nan-crash.html
    
    * svg/SVGAnimationElement.cpp:
    (WebCore::SVGAnimationElement::beginElementAt):
    (WebCore::SVGAnimationElement::endElementAt):
    * svg/animation/SMILTime.h:
    (WebCore::SMILTime::SMILTime):
    * svg/animation/SVGSMILElement.cpp:
    (WebCore::SVGSMILElement::addBeginTime):
    (WebCore::SVGSMILElement::addEndTime):
    
    LayoutTests:
    
    * svg/custom/elementTimeControl-nan-crash-expected.txt: Added.
    * svg/custom/elementTimeControl-nan-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimationElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimationElement.cpp
@@ -241,6 +241,8 @@ void SVGAnimationElement::beginElement()
 
 void SVGAnimationElement::beginElementAt(float offset)
 {
+    if (isnan(offset))
+        return;
     SMILTime elapsed = this->elapsed();
     addBeginTime(elapsed, elapsed + offset, SMILTimeWithOrigin::ScriptOrigin);
 }
@@ -252,6 +254,8 @@ void SVGAnimationElement::endElement()
 
 void SVGAnimationElement::endElementAt(float offset)
 {
+    if (isnan(offset))
+        return;
     SMILTime elapsed = this->elapsed();
     addEndTime(elapsed, elapsed + offset, SMILTimeWithOrigin::ScriptOrigin);
 }
