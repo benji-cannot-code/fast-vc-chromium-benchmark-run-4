eb8febb2100f326a718248cfb49fcf8472e2a3b9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-26 05:41:30 +0000

    A mistake in WebCore::JavaScriptCallFrame::evaluate which will cause assert failed https://bugs.webkit.org/show_bug.cgi?id=100347
    Patch by Peter Wang <peter.wang@torchmobile.com.cn> on 2012-10-25
    Reviewed by Mark Lam and Filip Pizlo.
    
    In worker context, calling "JSDOMWindowBase::commonJSGlobalData" will cause assert,
    since there is "ASSERT(isMainThread())" in "JSDOMWindowBase::commonJSGlobalData".
    
    No new test case, since no behaviour changed.
    
    * bindings/js/JavaScriptCallFrame.cpp:
    (WebCore::JavaScriptCallFrame::evaluate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JavaScriptCallFrame.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JavaScriptCallFrame.cpp
@@ -116,7 +116,7 @@ JSValue JavaScriptCallFrame::evaluate(const String& script, JSValue& exception) 
     if (!m_isValid)
         return jsNull();
 
-    JSLockHolder lock(JSDOMWindowBase::commonJSGlobalData());
+    JSLockHolder lock(m_debuggerCallFrame.callFrame());
     return m_debuggerCallFrame.evaluate(script, exception);
 }
 
