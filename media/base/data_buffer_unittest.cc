11ca1b2eb9289bae996fd13f1d76cd1e60470c89
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 08:15:05 +0000

    Make media::DataBuffer::DataBuffer(data, size) public.
    TBR=viettrungluu@chromium.org
    BUG=none
    TEST=media_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/11274066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164289 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/data_buffer_unittest.cc
+++ media/base/data_buffer_unittest.cc
@@ -10,14 +10,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace media {
 
 TEST(DataBufferTest, Constructors) {
-  const int kTestSize = 10;
+  const uint8 kTestData[] = { 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77 };
+  const int kTestDataSize = arraysize(kTestData);
 
   scoped_refptr<DataBuffer> buffer(new DataBuffer(0));
   EXPECT_FALSE(buffer->GetData());
 
-  scoped_refptr<DataBuffer> buffer2(new DataBuffer(kTestSize));
+  scoped_refptr<DataBuffer> buffer2(new DataBuffer(kTestDataSize));
   EXPECT_EQ(0, buffer2->GetDataSize());
-  EXPECT_EQ(kTestSize, buffer2->GetBufferSize());
+  EXPECT_EQ(kTestDataSize, buffer2->GetBufferSize());
+
+  scoped_refptr<DataBuffer> buffer3(new DataBuffer(kTestData, kTestDataSize));
+  EXPECT_EQ(kTestDataSize, buffer3->GetDataSize());
+  EXPECT_EQ(kTestDataSize, buffer3->GetBufferSize());
+  ASSERT_EQ(0, memcmp(buffer3->GetData(), kTestData, kTestDataSize));
+  // Ensure we are copying the data, not just pointing to the original data.
+  buffer3->GetWritableData()[0] = 0xFF;
+  ASSERT_NE(0, memcmp(buffer3->GetData(), kTestData, kTestDataSize));
 }
 
 TEST(DataBufferTest, ReadingWriting) {
