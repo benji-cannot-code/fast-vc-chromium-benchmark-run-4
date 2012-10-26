a3349e4acc2a121c777e3bed74db6fb851b565db
scheib@chromium.org <scheib@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-26 17:49:23 +0000

    Unreviewed, rolling out r132644. http://trac.webkit.org/changeset/132644 https://bugs.webkit.org/show_bug.cgi?id=100497
    Causes webkit_unit_tests
    MemoryInstrumentationTest.ImageObserver to fail.
    
    * platform/network/ResourceRequestBase.cpp:
    (WebCore::ResourceRequestBase::reportMemoryUsage):
    * platform/network/ResourceRequestBase.h:
    (ResourceRequestBase):
    * platform/network/chromium/ResourceRequest.cpp:
    * platform/network/chromium/ResourceRequest.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceRequest.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceRequest.cpp
@@ -27,8 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "ResourceRequest.h"
 
-#include "PlatformMemoryInstrumentation.h"
-
 namespace WebCore {
 
 // This is used by the loader to control the number of issued parallel load requests. 
@@ -59,13 +57,6 @@ void ResourceRequest::doPlatformAdopt(PassOwnPtr<CrossThreadResourceRequestData>
     m_hasUserGesture = data->m_hasUserGesture;
     m_downloadToFile = data->m_downloadToFile;
     m_targetType = data->m_targetType;
-}
-
-void ResourceRequest::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
-{
-    MemoryClassInfo info(memoryObjectInfo, this, PlatformMemoryTypes::Loader);
-    ResourceRequestBase::reportMemoryUsageBase(memoryObjectInfo);
-    info.addMember(m_extraData);
 }
 
 } // namespace WebCore
