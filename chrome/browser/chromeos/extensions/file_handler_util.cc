9f20b186f8779645894da62d57507cf95fffdd16
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 05:43:35 +0000

    The goal of the patch is avoid calling FindOrCreateTabbedBrowser. Since FindTabbedBrowser is deprecated this patch avoids looking for the active browser altogether and passes the tab id from where it is readily available.
    BUG=155674
    TEST=out/Release/browser_tests --gtest_filter=FileSystemExtensionApiTest.FileBrowserTest*
    
    Review URL: https://chromiumcodereview.appspot.com/11275041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_handler_util.cc
+++ chrome/browser/chromeos/extensions/file_handler_util.cc
@@ -477,6 +477,7 @@ class ExtensionTaskExecutor : public FileTaskExecutor {
 
   ExtensionTaskExecutor(Profile* profile,
                         const GURL source_url,
+                        int32 tab_id,
                         const std::string& extension_id,
                         const std::string& action_id);
   virtual ~ExtensionTaskExecutor();
@@ -531,6 +532,7 @@ class ExtensionTaskExecutor : public FileTaskExecutor {
   void SetupHandlerHostFileAccessPermissions(int handler_pid);
 
   const GURL source_url_;
+  int32 tab_id_;
   const std::string action_id_;
   FileTaskFinishedCallback done_;
 
@@ -564,12 +566,14 @@ class WebIntentTaskExecutor : public FileTaskExecutor {
 // static
 FileTaskExecutor* FileTaskExecutor::Create(Profile* profile,
                                            const GURL source_url,
+                                           int32 tab_id,
                                            const std::string& extension_id,
                                            const std::string& task_type,
                                            const std::string& action_id) {
   if (task_type == kTaskFile)
     return new ExtensionTaskExecutor(profile,
                                      source_url,
+                                     tab_id,
                                      extension_id,
                                      action_id);
 
@@ -602,6 +606,8 @@ bool FileTaskExecutor::Execute(const std::vector<GURL>& file_urls) {
   return ExecuteAndNotify(file_urls, FileTaskFinishedCallback());
 }
 
+// TODO(kaznacheev): Remove this method and inline its implementation at the
+// only place where it is used (DriveTaskExecutor::OnAppAuthorized)
 Browser* FileTaskExecutor::GetBrowser() const {
   return browser::FindOrCreateTabbedBrowser(
       profile_ ? profile_ : ProfileManager::GetDefaultProfileOrOffTheRecord(),
@@ -783,10 +789,12 @@ class ExtensionTaskExecutor::ExecuteTasksFileSystemCallbackDispatcher {
 ExtensionTaskExecutor::ExtensionTaskExecutor(
     Profile* profile,
     const GURL source_url,
+    int tab_id,
     const std::string& extension_id,
     const std::string& action_id)
     : FileTaskExecutor(profile, extension_id),
       source_url_(source_url),
+      tab_id_(tab_id),
       action_id_(action_id) {
 }
 
@@ -953,13 +961,7 @@ void ExtensionTaskExecutor::SetupPermissionsAndDispatchEvent(
     file_def->SetBoolean("fileIsDirectory", iter->is_directory);
   }
 
-  // Get tab id.
-  Browser* current_browser = GetBrowser();
-  if (current_browser) {
-    WebContents* contents = chrome::GetActiveWebContents(current_browser);
-    if (contents)
-      details->SetInteger("tab_id", ExtensionTabUtil::GetTabId(contents));
-  }
+  details->SetInteger("tab_id", tab_id_);
 
   event_router->DispatchEventToExtension(
       extension_id(), std::string("fileBrowserHandler.onExecute"),
