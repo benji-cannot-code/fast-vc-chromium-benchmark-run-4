1f90aaa6fe5f562548479e8192332ab8eb98f3ed
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 19:37:04 +0000

    Move AsyncDns field trial to server-side control.
    BUG=143454
    
    Review URL: https://codereview.chromium.org/11262018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/async_dns_field_trial.h
+++ chrome/browser/net/async_dns_field_trial.h
@@ -6,17 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_NET_ASYNC_DNS_FIELD_TRIAL_H_
 #define CHROME_BROWSER_NET_ASYNC_DNS_FIELD_TRIAL_H_
 
-#include <string>
-
-class CommandLine;
-class Profile;
-
 namespace chrome_browser_net {
 
-// Configures the AsyncDns field trial as follows.
-// group A (async disabled): use CreateSystemHostResolver
-// group B (async enabled):  use CreateAsyncHostResolver
-// Returns true if should use CreateAsyncHostResolver.
+// Returns true when the async resolver should be used.
 bool ConfigureAsyncDnsFieldTrial();
 
 }  // namespace chrome_browser_net
