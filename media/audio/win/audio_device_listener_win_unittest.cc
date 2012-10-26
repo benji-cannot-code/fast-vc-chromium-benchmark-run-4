d48c32b03483534b0db5e073eefb378a2f5b82fd
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 02:25:53 +0000

    Disable AudioDeviceListenerWinTest on XP.
    BUG=none
    TEST=compiles
    TBR=scherkus
    
    Review URL: https://codereview.chromium.org/11260054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164250 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_device_listener_win_unittest.cc
+++ media/audio/win/audio_device_listener_win_unittest.cc
@@ -31,6 +31,9 @@ class AudioDeviceListenerWinTest : public testing::Test {
   }
 
   virtual void SetUp() {
+    if (!media::IsWASAPISupported())
+      return;
+
     output_device_listener_.reset(new AudioDeviceListenerWin(base::Bind(
         &AudioDeviceListenerWinTest::OnDeviceChange, base::Unretained(this))));
   }
