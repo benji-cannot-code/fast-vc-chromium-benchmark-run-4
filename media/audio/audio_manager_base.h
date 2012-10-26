65bddd01004b23ec81304a5b857bd4467b1efd10
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 01:06:19 +0000

    Handle audio device changes on Windows.
    Uses the new AudioDeviceListener framework to notify of device
    changes.  Handles only default device changes at the moment,
    e.g., not manually changing the sample rate, etc on a current
    default device.
    
    This all works well enough that I can connect / disconnect remote
    desktop sessions with and without audio and everything continues
    to play seamlessly and in sync!
    
    BUG=153056
    TEST=Unplug... Plug... Unplug! Plug!
    
    Review URL: https://codereview.chromium.org/11233023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164236 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_manager_base.h
+++ media/audio/audio_manager_base.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
+#include <utility>
 
 #include "base/atomic_ref_count.h"
 #include "base/compiler_specific.h"
@@ -105,8 +106,8 @@ class MEDIA_EXPORT AudioManagerBase : public AudioManager {
   // TODO(dalecurtis): This must change to map both input and output parameters
   // to a single dispatcher, otherwise on a device state change we'll just get
   // the exact same invalid dispatcher.
-  typedef std::map<AudioParameters, scoped_refptr<AudioOutputDispatcher>,
-                   AudioParameters::Compare>
+  typedef std::map<std::pair<AudioParameters, AudioParameters>,
+                   scoped_refptr<AudioOutputDispatcher> >
       AudioOutputDispatchersMap;
 
   // Shuts down the audio thread and releases all the audio output dispatchers
--- media/audio/win/audio_manager_win.h
+++ media/audio/win/audio_manager_win.h
@@ -6,15 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MEDIA_AUDIO_WIN_AUDIO_MANAGER_WIN_H_
 #define MEDIA_AUDIO_WIN_AUDIO_MANAGER_WIN_H_
 
-#include <windows.h>
 #include <string>
 
-#include "base/basictypes.h"
-#include "base/compiler_specific.h"
-#include "base/gtest_prod_util.h"
 #include "media/audio/audio_manager_base.h"
 
 namespace media {
+
+class AudioDeviceListenerWin;
 
 // Windows implementation of the AudioManager singleton. This class is internal
 // to the audio output and only internal users can call methods not exposed by
@@ -22,6 +20,7 @@ namespace media {
 class MEDIA_EXPORT AudioManagerWin : public AudioManagerBase {
  public:
   AudioManagerWin();
+
   // Implementation of AudioManager.
   virtual bool HasAudioOutputDevices() OVERRIDE;
   virtual bool HasAudioInputDevices() OVERRIDE;
@@ -45,6 +44,9 @@ class MEDIA_EXPORT AudioManagerWin : public AudioManagerBase {
 
  protected:
   virtual ~AudioManagerWin();
+
+  // Implementation of AudioManager.
+  virtual void InitializeOnAudioThread() OVERRIDE;
 
  private:
   enum EnumerationType {
@@ -70,6 +72,13 @@ class MEDIA_EXPORT AudioManagerWin : public AudioManagerBase {
   AudioInputStream* CreatePCMWaveInAudioInputStream(
       const AudioParameters& params,
       const std::string& device_id);
+
+  // |output_device_listener_| must be destructed on the same COM thread it was
+  // initialized on.
+  void DestructOnAudioThread();
+
+  // Listen for output device changes.
+  scoped_ptr<AudioDeviceListenerWin> output_device_listener_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioManagerWin);
 };
