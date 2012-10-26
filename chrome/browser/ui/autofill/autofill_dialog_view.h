0d9b00e7f64c3c3242623bc8ddb4b4f2605b77fb
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 21:05:18 +0000

    [views] "Hello, world" autofill imperative API dialog.
    not hooked up to anything as the API doesn't exist yet.
    
    BUG=157270,157273
    
    Review URL: https://codereview.chromium.org/11231063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164403 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/autofill/autofill_dialog_view.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_DIALOG_VIEW_H_
+#define CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_DIALOG_VIEW_H_
+
+class AutofillDialogController;
+
+// An interface for the dialog that appears when a site initiates an Autofill
+// action via the imperative autocomplete API.
+class AutofillDialogView {
+ public:
+  virtual ~AutofillDialogView();
+
+  // Shows the dialog.
+  virtual void Show() = 0;
+
+  // Factory function to create the dialog (implemented once per view
+  // implementation). |controller| will own the created dialog.
+  static AutofillDialogView* Create(AutofillDialogController* controller);
+};
+
+#endif  // CHROME_BROWSER_UI_AUTOFILL_AUTOFILL_DIALOG_VIEW_H_
