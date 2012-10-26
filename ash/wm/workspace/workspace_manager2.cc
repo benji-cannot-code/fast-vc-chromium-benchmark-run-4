b8086fc1d874c36e82db103d2d2931e6ffc4107d
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 18:29:32 +0000

    Re-vamped the entire window auto positioning logic
    As long as the user did not move / resize the window:
    
    A single visible window (browser / app) on the screen will always be centered.
    When a second visible (browser / app) window joins in, both will be pushed into opposite corners.
    When more windows get piled up, they go every time into the opposite side of the currently active window.
    
    When the user has resized / moved a window, it will be treated as a visible (browser / app) window, but it will not get moved - the others will however.
    
    
    Addressing bug: Moving last remaining tabbed window back into screen center
    
    
    BUG=153431, 150879, 153302
    TEST=unit-tests & visual
    
    Review URL: https://chromiumcodereview.appspot.com/11085053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164352 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_manager2.cc
+++ ash/wm/workspace/workspace_manager2.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/wm/window_animations.h"
 #include "ash/wm/window_properties.h"
 #include "ash/wm/window_util.h"
+#include "ash/wm/workspace/auto_window_management.h"
 #include "ash/wm/workspace/desktop_background_fade_controller.h"
 #include "ash/wm/workspace/workspace_animations.h"
 #include "ash/wm/workspace/workspace_layout_manager2.h"
@@ -594,10 +595,14 @@ void WorkspaceManager2::OnWindowAddedToWorkspace(Workspace2* workspace,
   // to the workspace.
   if (workspace == active_workspace_)
     UpdateShelfVisibility();
+
+  RearrangeVisibleWindowOnShow(child);
 }
 
 void WorkspaceManager2::OnWillRemoveWindowFromWorkspace(Workspace2* workspace,
                                                         Window* child) {
+  if (child->TargetVisibility())
+    RearrangeVisibleWindowOnHideOrRemove(child);
   child->ClearProperty(kWorkspaceKey);
 }
 
@@ -610,10 +615,16 @@ void WorkspaceManager2::OnWindowRemovedFromWorkspace(Workspace2* workspace,
 void WorkspaceManager2::OnWorkspaceChildWindowVisibilityChanged(
     Workspace2* workspace,
     Window* child) {
-  if (workspace->ShouldMoveToPending())
+  if (workspace->ShouldMoveToPending()) {
     MoveWorkspaceToPendingOrDelete(workspace, NULL, SWITCH_VISIBILITY_CHANGED);
-  else if (workspace == active_workspace_)
-    UpdateShelfVisibility();
+  } else {
+    if (child->TargetVisibility())
+      RearrangeVisibleWindowOnShow(child);
+    else
+      RearrangeVisibleWindowOnHideOrRemove(child);
+    if (workspace == active_workspace_)
+      UpdateShelfVisibility();
+  }
 }
 
 void WorkspaceManager2::OnWorkspaceWindowChildBoundsChanged(
