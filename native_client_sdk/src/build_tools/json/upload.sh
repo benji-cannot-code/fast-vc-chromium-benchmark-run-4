d48a3effbceac4881ea38b8db5a6482dc0ef8c3c
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 07:18:00 +0000

    Stop excluding native_client_sdk from PRESUBMIT
    Modify code in native_client_sdk to pass common PRESUBMIT
    tests.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11201003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164279 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/json/upload.sh
+++
@@ -1,5 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/bin/sh
-echo DEPRECATED gsutil -h "Cache-Control: no-cache" cp -a public-read naclsdk_manifest.json gs://nativeclient-mirror/nacl/nacl_sdk/naclsdk_manifest.json
-gsutil -h "Cache-Control: no-cache" cp -a public-read naclsdk_manifest0.json gs://nativeclient-mirror/nacl/nacl_sdk/naclsdk_manifest0.json
-gsutil -h "Cache-Control: no-cache" cp -a public-read naclsdk_manifest2.json gs://nativeclient-mirror/nacl/nacl_sdk/naclsdk_manifest2.json
