e705ff3e2ccc243ef4cd4998d713ef43d00a8cfb
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 02:07:27 +0000

    Fixing threading issues in remoting::DesktopProcess. The UI thread is now owned by the caller of remoting::DesktopThread so it can be properly shared between the caller and the class.
    BUG=157808,157886
    
    Review URL: https://codereview.chromium.org/11272036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164243 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/desktop_process.h
+++ remoting/host/desktop_process.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "ipc/ipc_listener.h"
 
@@ -19,11 +20,13 @@ class ChannelProxy;
 
 namespace remoting {
 
+class AutoThreadTaskRunner;
 class DesktopSessionAgent;
 
 class DesktopProcess : public IPC::Listener {
  public:
-  explicit DesktopProcess(const std::string& daemon_channel_name);
+  DesktopProcess(scoped_refptr<AutoThreadTaskRunner> caller_task_runner,
+                 const std::string& daemon_channel_name);
   virtual ~DesktopProcess();
 
   // IPC::Listener implementation.
@@ -31,8 +34,9 @@ class DesktopProcess : public IPC::Listener {
   virtual void OnChannelConnected(int32 peer_pid) OVERRIDE;
   virtual void OnChannelError() OVERRIDE;
 
-  // Runs the desktop process.
-  int Run();
+  // Creates the desktop agent and required threads and IPC channels. Returns
+  // true on success.
+  bool Start();
 
  private:
   // Crashes the process in response to a daemon's request. The daemon passes
@@ -41,6 +45,9 @@ class DesktopProcess : public IPC::Listener {
   void OnCrash(const std::string& function_name,
                const std::string& file_name,
                const int& line_number);
+
+  // Task runner on which public methods of this class should be called.
+  scoped_refptr<AutoThreadTaskRunner> caller_task_runner_;
 
   // Name of the IPC channel connecting the desktop process with the daemon
   // process.
