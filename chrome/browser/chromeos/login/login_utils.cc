9b7543c8cafb518e4fb139c796a137187f8e49b0
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 19:48:58 +0000

    CrOS: whitelist --ui-enable-threaded-compositing
    BUG=135607
    
    
    Review URL: https://chromiumcodereview.appspot.com/11300004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164384 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -799,6 +799,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       cc::switches::kEnablePartialSwap,
       cc::switches::kEnablePinchInCompositor,
       ::switches::kUIEnablePartialSwap,
+      ::switches::kUIEnableThreadedCompositing,
       ::switches::kUIPrioritizeInGpuProcess,
 #if defined(USE_CRAS)
       ::switches::kUseCras,
