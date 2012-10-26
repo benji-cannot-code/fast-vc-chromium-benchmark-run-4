665f7febbd57381932043debbee52513e2d8d973
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-26 08:10:52 +0000

    Unreviewed. This ASSERT is no longer correct (and fires on many tests.)
    * bindings/v8/V8DOMWrapper.h:
    (WebCore::V8DOMWrapper::setJSWrapperForDOMObject):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWrapper.h
@@ -132,7 +132,6 @@ namespace WebCore {
     {
         v8::Persistent<v8::Object> wrapperHandle = v8::Persistent<v8::Object>::New(wrapper);
         ASSERT(maybeDOMWrapper(wrapperHandle));
-        ASSERT(!domWrapperType(wrapperHandle)->toActiveDOMObjectFunction);
         getDOMObjectMap(isolate).set(object.leakRef(), wrapperHandle);
         return wrapperHandle;
     }
