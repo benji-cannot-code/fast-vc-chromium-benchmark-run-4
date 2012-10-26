5f06bfad84f343085cb43b5c8a0e5ffd8d9c1755
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-26 00:01:51 +0000

    cc: Remove wtf includes from delay_based_time_source.cc
    cmath seems to be enough for our needs here.
    
    BUG=154451
    R=enne@chromium.org,jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11265018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164219 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/delay_based_time_source.cc
+++ cc/delay_based_time_source.cc
@@ -8,11 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/delay_based_time_source.h"
 
 #include <algorithm>
+#include <cmath>
 
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
-#include <wtf/CurrentTime.h>
-#include <wtf/MathExtras.h>
 
 namespace cc {
 
