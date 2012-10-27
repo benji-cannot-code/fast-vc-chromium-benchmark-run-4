fffd1a40811463115b5254f4cbd00c1acc13c0a5
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-27 18:24:26 +0000

    Add upload identifier to URLRequest::Start net log event.
    BUG=145070
    
    
    Review URL: https://chromiumcodereview.appspot.com/11276050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164550 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_netlog_params.cc
+++ net/url_request/url_request_netlog_params.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/url_request/url_request_netlog_params.h"
 
+#include "base/string_number_conversions.h"
 #include "base/values.h"
 #include "googleurl/src/gurl.h"
 
@@ -14,12 +15,15 @@ Value* NetLogURLRequestStartCallback(const GURL* url,
                                      const std::string* method,
                                      int load_flags,
                                      RequestPriority priority,
+                                     int64 upload_id,
                                      NetLog::LogLevel /* log_level */) {
   DictionaryValue* dict = new DictionaryValue();
   dict->SetString("url", url->possibly_invalid_spec());
   dict->SetString("method", *method);
   dict->SetInteger("load_flags", load_flags);
   dict->SetInteger("priority", static_cast<int>(priority));
+  if (upload_id > -1)
+    dict->SetString("upload_id", base::Int64ToString(upload_id));
   return dict;
 }
 
