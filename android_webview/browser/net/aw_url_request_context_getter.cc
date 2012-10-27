benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 9e0089998562d9fd303eab5bef73a961cc6a3ab1

Fix leaking request factory in android_webview
Have the context getter own the factory since it already owns the
context itself.

BUG=
Run through android_fyi_dbg trybot
NOTRY=true


Review URL: https://chromiumcodereview.appspot.com/11320004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164387 0039d316-1c4b-4281-b951-d872f2087c98

android_webview/browser/net/aw_url_request_context_getter.cc
@@ -197,17 +197,16 @@ void AwURLRequestContextGetter::Init() {
   storage->set_http_transaction_factory(new net::HttpCache(
       network_session_params, http_cache_backend.release()));
 
-  scoped_ptr<AwURLRequestJobFactory> job_factory(
-      new AwURLRequestJobFactory);
-  bool set_protocol = job_factory->SetProtocolHandler(
+  job_factory_.reset(new AwURLRequestJobFactory);
+  bool set_protocol = job_factory_->SetProtocolHandler(
       chrome::kFileScheme, new net::FileProtocolHandler());
   DCHECK(set_protocol);
-  set_protocol = job_factory->SetProtocolHandler(
+  set_protocol = job_factory_->SetProtocolHandler(
       chrome::kDataScheme, new net::DataProtocolHandler());
   DCHECK(set_protocol);
-  RegisterAndroidProtocolsOnIOThread(job_factory.get());
-  job_factory->AddInterceptor(new AwRequestInterceptor());
-  storage->set_job_factory(job_factory.release());
+  RegisterAndroidProtocolsOnIOThread(job_factory_.get());
+  job_factory_->AddInterceptor(new AwRequestInterceptor());
+  storage->set_job_factory(job_factory_.get());
 }
 
 content::ResourceContext* AwURLRequestContextGetter::GetResourceContext() {

android_webview/browser/net/aw_url_request_context_getter.h
@@ -18,6 +18,7 @@ class ResourceContext;
 namespace net {
 class ProxyConfigService;
 class URLRequestContext;
+class URLRequestJobFactory;
 }
 
 namespace android_webview {
@@ -50,6 +51,7 @@ class AwURLRequestContextGetter : public net::URLRequestContextGetter,
   scoped_ptr<net::URLRequestContext> url_request_context_;
   scoped_ptr<net::ProxyConfigService> proxy_config_service_;
   scoped_ptr<content::ResourceContext> resource_context_;
+  scoped_ptr<net::URLRequestJobFactory> job_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(AwURLRequestContextGetter);
 };

commit 00d9ace00df7a1eeef2b84fcecb38a1f49e59b44

Improve URLRequestContextBuilder for use in android_webview
Implemented [Enable|Disable]HttpCache methods.
Added OS_ANDROID for set_proxy_config_service guard.
Added set_network_delegate method.

Also fix a double owning issue introduced by me in r164387.

BUG=

Review URL: https://chromiumcodereview.appspot.com/11266049

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164495 0039d316-1c4b-4281-b951-d872f2087c98

android_webview/browser/net/aw_url_request_context_getter.cc
@@ -15,22 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/resource_context.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/url_constants.h"
-#include "net/base/cert_verifier.h"
-#include "net/base/host_resolver.h"
-#include "net/base/ssl_config_service_defaults.h"
-#include "net/base/transport_security_state.h"
-#include "net/base/transport_security_state.h"
-#include "net/cookies/cookie_monster.h"
-#include "net/ftp/ftp_network_layer.h"
-#include "net/http/http_auth_handler_factory.h"
-#include "net/http/http_cache.h"
-#include "net/http/http_network_session.h"
-#include "net/http/http_server_properties_impl.h"
 #include "net/proxy/proxy_service.h"
 #include "net/url_request/data_protocol_handler.h"
 #include "net/url_request/file_protocol_handler.h"
+#include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_context.h"
-#include "net/url_request/url_request_context_storage.h"
 
 using content::BrowserThread;
 
@@ -68,39 +57,6 @@ net::URLRequestContext* AwResourceContext::GetRequestContext() {
   return context_;
 }
 
-// Inspired by BasicURLRequestContext.
-// TODO(boliu): Remove this class and use URLRequestContextBuilder when it
-// has all the necessary features for Android WebView.
-class AwURLRequestContext : public net::URLRequestContext {
- public:
-  AwURLRequestContext();
-  virtual ~AwURLRequestContext();
-
-  virtual const std::string& GetUserAgent(const GURL& url) const OVERRIDE;
-
-  net::URLRequestContextStorage* storage();
-
- private:
-  net::URLRequestContextStorage storage_;
-
-  DISALLOW_COPY_AND_ASSIGN(AwURLRequestContext);
-};
-
-AwURLRequestContext::AwURLRequestContext()
-    : ALLOW_THIS_IN_INITIALIZER_LIST(storage_(this)) {
-}
-
-AwURLRequestContext::~AwURLRequestContext() {
-}
-
-const std::string& AwURLRequestContext::GetUserAgent(const GURL& url) const {
-  return content::GetUserAgent(url);
-}
-
-net::URLRequestContextStorage* AwURLRequestContext::storage() {
-  return &storage_;
-}
-
 }  // namespace
 
 AwURLRequestContextGetter::AwURLRequestContextGetter(
@@ -124,7 +80,20 @@ AwURLRequestContextGetter::~AwURLRequestContextGetter() {
 void AwURLRequestContextGetter::Init() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
 
-  url_request_context_.reset(new AwURLRequestContext);
+  net::URLRequestContextBuilder builder;
+  builder.set_user_agent(content::GetUserAgent(GURL()));
+  builder.set_network_delegate(new AwNetworkDelegate());
+  builder.set_ftp_enabled(false);  // Android WebView does not support ftp yet.
+  builder.set_proxy_config_service(proxy_config_service_.release());
+
+  net::URLRequestContextBuilder::HttpCacheParams cache_params;
+  cache_params.type = net::URLRequestContextBuilder::HttpCacheParams::DISK;
+  cache_params.max_size = 10 * 1024 * 1024;  // 10M
+  cache_params.path =
+      browser_context_->GetPath().Append(FILE_PATH_LITERAL("Cache")),
+  builder.EnableHttpCache(cache_params);
+
+  url_request_context_.reset(builder.Build());
 
   url_request_context_->set_accept_language(
       net::HttpUtil::GenerateAcceptLanguageHeader(
@@ -135,68 +104,6 @@ void AwURLRequestContextGetter::Init() {
   url_request_context_->set_accept_charset(
       net::HttpUtil::GenerateAcceptCharsetHeader("ISO-8859-1"));
 
-  net::URLRequestContextStorage* storage =
-      static_cast<AwURLRequestContext*>(url_request_context_.get())->storage();
-
-  storage->set_network_delegate(new AwNetworkDelegate());
-
-  storage->set_host_resolver(net::HostResolver::CreateDefaultResolver(
-      content::GetContentClient()->browser()->GetNetLog()));
-
-  // Note we do not call set_ftp_transaction_factory here since we do not
-  // support ftp yet in Android WebView.
-
-  storage->set_proxy_service(
-      net::ProxyService::CreateUsingSystemProxyResolver(
-          proxy_config_service_.release(),
-          0,  // Use default number of threads.
-          url_request_context_->net_log()));
-
-  storage->set_ssl_config_service(new net::SSLConfigServiceDefaults);
-  storage->set_http_auth_handler_factory(
-      net::HttpAuthHandlerRegistryFactory::CreateDefault(
-          url_request_context_->host_resolver()));
-  storage->set_cookie_store(new net::CookieMonster(NULL, NULL));
-  storage->set_transport_security_state(new net::TransportSecurityState());
-  storage->set_http_server_properties(new net::HttpServerPropertiesImpl);
-  storage->set_cert_verifier(net::CertVerifier::CreateDefault());
-
-  net::HttpNetworkSession::Params network_session_params;
-  network_session_params.host_resolver =
-      url_request_context_->host_resolver();
-  network_session_params.cert_verifier =
-      url_request_context_->cert_verifier();
-  network_session_params.transport_security_state =
-      url_request_context_->transport_security_state();
-  network_session_params.proxy_service =
-      url_request_context_->proxy_service();
-  network_session_params.ssl_config_service =
-      url_request_context_->ssl_config_service();
-  network_session_params.http_auth_handler_factory =
-      url_request_context_->http_auth_handler_factory();
-  network_session_params.network_delegate =
-      url_request_context_->network_delegate();
-  network_session_params.http_server_properties =
-      url_request_context_->http_server_properties();
-  network_session_params.net_log = url_request_context_->net_log();
-  network_session_params.server_bound_cert_service =
-      url_request_context_->server_bound_cert_service();
-  network_session_params.ignore_certificate_errors = false;
-  network_session_params.http_pipelining_enabled = false;
-  // TODO(boliu): Other settings are set by commandline switches in chrome/
-  // and seem to be only used in testing. Check if we need to set them here.
-
-  scoped_ptr<net::HttpCache::BackendFactory> http_cache_backend(
-        new net::HttpCache::DefaultBackend(
-            net::DISK_CACHE,
-            browser_context_->GetPath().Append(FILE_PATH_LITERAL("Cache")),
-            10*1024*1024,  // Max cache size 10M
-            BrowserThread::GetMessageLoopProxyForThread(
-                BrowserThread::CACHE)));
-
-  storage->set_http_transaction_factory(new net::HttpCache(
-      network_session_params, http_cache_backend.release()));
-
   job_factory_.reset(new AwURLRequestJobFactory);
   bool set_protocol = job_factory_->SetProtocolHandler(
       chrome::kFileScheme, new net::FileProtocolHandler());
@@ -206,7 +113,7 @@ void AwURLRequestContextGetter::Init() {
   DCHECK(set_protocol);
   RegisterAndroidProtocolsOnIOThread(job_factory_.get());
   job_factory_->AddInterceptor(new AwRequestInterceptor());
-  storage->set_job_factory(job_factory_.get());
+  url_request_context_->set_job_factory(job_factory_.get());
 }
 
 content::ResourceContext* AwURLRequestContextGetter::GetResourceContext() {

net/url_request/url_request_context_builder.cc
@@ -193,12 +193,12 @@ URLRequestContextBuilder::URLRequestContextBuilder()
       http_cache_enabled_(true) {}
 URLRequestContextBuilder::~URLRequestContextBuilder() {}
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
 void URLRequestContextBuilder::set_proxy_config_service(
     ProxyConfigService* proxy_config_service) {
   proxy_config_service_.reset(proxy_config_service);
 }
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_ANDROID)
 
 URLRequestContext* URLRequestContextBuilder::Build() {
   BasicURLRequestContext* context = new BasicURLRequestContext;
@@ -206,8 +206,9 @@ URLRequestContext* URLRequestContextBuilder::Build() {
 
   context->set_user_agent(user_agent_);
 
-  BasicNetworkDelegate* network_delegate = new BasicNetworkDelegate;
-  storage->set_network_delegate(network_delegate);
+  if (!network_delegate_)
+    network_delegate_.reset(new BasicNetworkDelegate);
+  storage->set_network_delegate(network_delegate_.release());
 
   storage->set_host_resolver(net::HostResolver::CreateDefaultResolver(NULL));
 
@@ -220,14 +221,14 @@ URLRequestContext* URLRequestContextBuilder::Build() {
 
   // TODO(willchan): Switch to using this code when
   // ProxyService::CreateSystemProxyConfigService()'s signature doesn't suck.
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
   ProxyConfigService* proxy_config_service = proxy_config_service_.release();
 #else
   ProxyConfigService* proxy_config_service =
       ProxyService::CreateSystemProxyConfigService(
           base::ThreadTaskRunnerHandle::Get(),
           context->file_message_loop());
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_ANDROID)
   storage->set_proxy_service(
       ProxyService::CreateUsingSystemProxyResolver(
           proxy_config_service,

net/url_request/url_request_context_builder.h
@@ -29,6 +29,7 @@ namespace net {
 class HostMappingRules;
 class ProxyConfigService;
 class URLRequestContext;
+class NetworkDelegate;
 
 class NET_EXPORT URLRequestContextBuilder {
  public:
@@ -68,9 +69,9 @@ class NET_EXPORT URLRequestContextBuilder {
   URLRequestContextBuilder();
   ~URLRequestContextBuilder();
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
   void set_proxy_config_service(ProxyConfigService* proxy_config_service);
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_ANDROID)
 
   // Call this function to specify a hard-coded User-Agent for all requests that
   // don't have a User-Agent already set.
@@ -83,9 +84,21 @@ class NET_EXPORT URLRequestContextBuilder {
     ftp_enabled_ = enable;
   }
 
+  // Uses BasicNetworkDelegate by default. Note that calling Build will unset
+  // any custom delegate in builder, so this must be called each time before
+  // Build is called.
+  void set_network_delegate(NetworkDelegate* delegate) {
+    network_delegate_.reset(delegate);
+  }
+
   // By default HttpCache is enabled with a default constructed HttpCacheParams.
-  void EnableHttpCache(const HttpCacheParams& params);
-  void DisableHttpCache();
+  void EnableHttpCache(const HttpCacheParams& params) {
+    http_cache_params_ = params;
+  }
+
+  void DisableHttpCache() {
+    http_cache_params_ = HttpCacheParams();
+  }
 
   // Override default net::HttpNetworkSession::Params settings.
   void set_http_network_session_params(
@@ -101,9 +114,10 @@ class NET_EXPORT URLRequestContextBuilder {
   bool http_cache_enabled_;
   HttpCacheParams http_cache_params_;
   HttpNetworkSessionParams http_network_session_params_;
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
   scoped_ptr<ProxyConfigService> proxy_config_service_;
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_ANDROID)
+  scoped_ptr<NetworkDelegate> network_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(URLRequestContextBuilder);
 };

net/url_request/url_request_context_builder_unittest.cc
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "testing/platform_test.h"
 
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
 #include "net/proxy/proxy_config.h"
 #include "net/proxy/proxy_config_service_fixed.h"
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_ANDROID)
 
 namespace net {
 
@@ -41,10 +41,10 @@ class URLRequestContextBuilderTest : public PlatformTest {
   URLRequestContextBuilderTest()
       : test_server_(
           FilePath(FILE_PATH_LITERAL("net/data/url_request_unittest"))) {
-#if defined(OS_LINUX)
+#if defined(OS_LINUX) || defined(OS_ANDROID)
     builder_.set_proxy_config_service(
         new ProxyConfigServiceFixed(ProxyConfig::CreateDirect()));
-#endif  // defined(OS_LINUX)
+#endif  // defined(OS_LINUX) || defined(OS_ANDROID)
   }
 
   LocalHttpTestServer test_server_;