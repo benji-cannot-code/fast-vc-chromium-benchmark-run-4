c8f6c8d391dbcf23d2380ddf4bff0d18c2615dd9
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-27 00:46:34 +0000

    Http cache: Measure back navigations on the infinite cache simulation.
    Also count entries that are not cached.
    
    BUG=147383
    TEST=none
    Review URL: https://codereview.chromium.org/11267028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164465 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/infinite_cache.cc
+++ net/http/infinite_cache.cc
@@ -276,6 +276,12 @@ void InfiniteCacheTransaction::OnRequestStart(const HttpRequestInfo* request) {
   CryptoHash(cache_key, &resource_data_->key);
 }
 
+void InfiniteCacheTransaction::OnBackForwardNavigation() {
+  if (!cache_)
+    return;
+  UMA_HISTOGRAM_BOOLEAN("InfiniteCache.BackForwardNavigation", true);
+}
+
 void InfiniteCacheTransaction::OnResponseReceived(
     const HttpResponseInfo* response) {
   if (!cache_)
@@ -523,10 +529,14 @@ void InfiniteCache::Worker::Process(
     return;
 
   UMA_HISTOGRAM_BOOLEAN("InfiniteCache.TotalRequests", true);
+  if (data->details.flags & NO_STORE)
+    UMA_HISTOGRAM_BOOLEAN("InfiniteCache.NoStore", true);
+
   header_->num_requests++;
   KeyMap::iterator i = map_.find(data->key);
   if (i != map_.end()) {
     if (data->details.flags & DOOM_METHOD) {
+      UMA_HISTOGRAM_BOOLEAN("InfiniteCache.DoomMethodHit", true);
       Remove(i->second);
       map_.erase(i);
       return;
@@ -562,8 +572,10 @@ void InfiniteCache::Worker::Process(
   if (data->details.flags & NO_STORE)
     return;
 
-  if (data->details.flags & DOOM_METHOD)
+  if (data->details.flags & DOOM_METHOD) {
+    UMA_HISTOGRAM_BOOLEAN("InfiniteCache.DoomMethodHit", false);
     return;
+  }
 
   map_[data->key] = data->details;
   Add(data->details);
@@ -571,7 +583,7 @@ void InfiniteCache::Worker::Process(
 
 void InfiniteCache::Worker::LoadData() {
   if (path_.empty())
-    return InitializeData();;
+    return InitializeData();
 
   PlatformFile file = base::CreatePlatformFile(
       path_, base::PLATFORM_FILE_OPEN | base::PLATFORM_FILE_READ, NULL, NULL);
