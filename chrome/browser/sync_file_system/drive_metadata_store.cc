08a2d347a262190a213c6e5522c1d07efd112cc8
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-27 02:07:17 +0000

    Add tests for sync origins in DriveMetadataStore
    BUG=157838
    TEST=unit_tests --gtest_filter=\*DriveMetadataStoreTest\*
    
    Review URL: https://chromiumcodereview.appspot.com/11273062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164483 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_metadata_store.cc
+++ chrome/browser/sync_file_system/drive_metadata_store.cc
@@ -210,7 +210,6 @@ void DriveMetadataStore::AddBatchSyncOrigin(const GURL& origin) {
   DCHECK(!IsIncrementalSyncOrigin(origin));
 
   // TODO(tzik): Store |origin| to DB. crbug.com/157821
-  // TODO(tzik): Add test.
   batch_sync_origins_.insert(origin);
 }
 
@@ -220,7 +219,6 @@ void DriveMetadataStore::MoveBatchSyncOriginToIncremental(const GURL& origin) {
   DCHECK(!IsIncrementalSyncOrigin(origin));
 
   // TODO(tzik): Store |origin| to DB. crbug.com/157821
-  // TODO(tzik): Add test.
   batch_sync_origins_.erase(origin);
   incremental_sync_origins_.insert(origin);
 }
