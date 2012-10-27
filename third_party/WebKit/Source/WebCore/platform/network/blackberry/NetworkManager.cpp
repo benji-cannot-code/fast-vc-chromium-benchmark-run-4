451f718cac9884d09a7fe22d56b739e2e5ca4562
charles.wei@torchmobile.com.cn <charles.wei@torchmobile.com.cn@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-27 06:00:43 +0000

    [BlackBerry] Browser prematurely sends wrong credentials https://bugs.webkit.org/show_bug.cgi?id=100585
    Reviewed by Yong Li.
    
    Manually revert the patch for bug 96362, which causes regressions and the right patch has been
    submitted with patch for bug 100448. Since the auto-revert fails, we use this patch to manually
    revert.
    
    No new tests. The test is coverted by patch for 100448.
    
    * platform/network/blackberry/CredentialBackingStore.cpp:
    * platform/network/blackberry/CredentialBackingStore.h:
    (CredentialBackingStore):
    * platform/network/blackberry/NetworkManager.cpp:
    (WebCore::NetworkManager::startJob):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132722 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/blackberry/NetworkManager.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/blackberry/NetworkManager.cpp
@@ -21,17 +21,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "NetworkManager.h"
 
 #include "Chrome.h"
-#if ENABLE(BLACKBERRY_CREDENTIAL_PERSIST)
-#include "CredentialBackingStore.h"
-#endif
 #include "CredentialStorage.h"
 #include "Frame.h"
 #include "FrameLoaderClientBlackBerry.h"
 #include "NetworkJob.h"
 #include "Page.h"
-#if ENABLE(BLACKBERRY_CREDENTIAL_PERSIST)
-#include "ResourceHandleClient.h"
-#endif
 #include "ResourceHandleInternal.h"
 #include "ResourceRequest.h"
 #include "SecurityOrigin.h"
@@ -125,14 +119,6 @@ bool NetworkManager::startJob(int playerId, const String& pageGroupName, PassRef
         // For URLs that match the paths of those previously challenged for HTTP Basic authentication,
         // try and reuse the credential preemptively, as allowed by RFC 2617.
         Credential credential = CredentialStorage::get(url);
-#if ENABLE(BLACKBERRY_CREDENTIAL_PERSIST)
-        // FIXME: needs to refactor the credentialBackingStore to get credential and protection space at one time.
-        if (credential.isEmpty() && guardJob->client()->shouldUseCredentialStorage(guardJob.get())) {
-            credential = credentialBackingStore().getLogin(url);
-            if (!credential.isEmpty())
-                CredentialStorage::set(credential, credentialBackingStore().getProtectionSpace(url), url);
-        }
-#endif
         if (!credential.isEmpty())
             platformRequest.setCredentials(credential.user().utf8().data(), credential.password().utf8().data(), BlackBerry::Platform::NetworkRequest::AuthHTTPBasic);
     }
