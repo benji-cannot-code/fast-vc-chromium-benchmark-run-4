5552bea934d75356a64230800e3707a3be8d4715
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-27 01:53:38 +0000

    [Chromium] Convert Chromium template string to an LDML date format https://bugs.webkit.org/show_bug.cgi?id=100128
    Reviewed by Tony Chang.
    
    * src/LocalizedStrings.cpp:
    (WebCore): Include WebCore/platform/text/DateTimeFormat.h.
    (WebCore::weekFormatInLDML):
    Replace $1 with yyyy, $2 with ww, and quote other parts.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132718 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/LocalizedStrings.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/LocalizedStrings.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "LocalizedStrings.h"
 
+#include "DateTimeFormat.h"
 #include "IntSize.h"
 #include "NotImplemented.h"
 
@@ -271,7 +272,23 @@ String placeholderForYearField()
 
 String weekFormatInLDML()
 {
-    return query(WebLocalizedString::WeekFormatTemplate);
+    String templ = query(WebLocalizedString::WeekFormatTemplate);
+    // Converts a string like "Week $2, $1" to an LDML date format pattern like
+    // "'Week 'ww', 'yyyy".
+    StringBuilder builder;
+    unsigned literalStart = 0;
+    unsigned length = templ.length();
+    for (unsigned i = 0; i + 1 < length; ++i) {
+        if (templ[i] == '$' && (templ[i + 1] == '1' || templ[i + 1] == '2')) {
+            if (literalStart < i)
+                DateTimeFormat::quoteAndAppendLiteral(templ.substring(literalStart, i - literalStart), builder);
+            builder.append(templ[++i] == '1' ? "yyyy" : "ww");
+            literalStart = i + 1;
+        }
+    }
+    if (literalStart < length)
+        DateTimeFormat::quoteAndAppendLiteral(templ.substring(literalStart, length - literalStart), builder);
+    return builder.toString();
 }
 
 #endif
