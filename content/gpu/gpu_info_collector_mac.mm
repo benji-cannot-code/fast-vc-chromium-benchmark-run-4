17d4f4f045d290f2728bd33c8df1e45195ab154b
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-28 19:46:58 +0000

    Collect machine model info in gpu_info_collector and expose it to about:gpu.
    BUG=158147
    TEST=content_unittests, about:gpu page on Mac(manual)
    
    Review URL: https://chromiumcodereview.appspot.com/11311021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164571 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_info_collector_mac.mm
+++ content/gpu/gpu_info_collector_mac.mm
@@ -9,9 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
+#include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/string_number_conversions.h"
 #include "base/string_piece.h"
+#include "base/string_util.h"
 #include "base/sys_string_conversions.h"
 #include "ui/gl/gl_bindings.h"
 #include "ui/gl/gl_context.h"
@@ -169,6 +172,14 @@ bool CollectGraphicsInfo(content::GPUInfo* gpu_info) {
 
 bool CollectPreliminaryGraphicsInfo(content::GPUInfo* gpu_info) {
   DCHECK(gpu_info);
+
+  std::string model_name;
+  int32 model_major = 0, model_minor = 0;
+  base::mac::ParseModelIdentifier(base::mac::GetModelIdentifier(),
+                                  &model_name, &model_major, &model_minor);
+  ReplaceChars(model_name, " ", "_", &gpu_info->machine_model);
+  gpu_info->machine_model += " " + base::IntToString(model_major) +
+                             "." + base::IntToString(model_minor);
 
   return CollectPCIVideoCardInfo(gpu_info);
 }
