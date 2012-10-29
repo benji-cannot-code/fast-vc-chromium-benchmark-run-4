0233bba79efd493efe5931f74b8d2fcc3e32b8ad
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 23:33:45 +0000

    Move MockRenderWidgetHostDelegate into an anonymous namespace. There is another class with that name in content_unittests that was previously also in the content namespace. When this one moved into it, the linker got confused and mixed the destructor with the wrong object. Review URL: https://codereview.chromium.org/11347028
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164781 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
+++ content/browser/renderer_host/render_widget_host_view_aura_unittest.cc
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_types.h"
 
 namespace content {
-
+namespace {
 class MockRenderWidgetHostDelegate : public RenderWidgetHostDelegate {
  public:
   MockRenderWidgetHostDelegate() {}
@@ -105,6 +105,8 @@ class RenderWidgetHostViewAuraTest : public testing::Test {
  private:
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostViewAuraTest);
 };
+
+}  // namespace
 
 // Checks that a fullscreen view has the correct show-state and receives the
 // focus.
