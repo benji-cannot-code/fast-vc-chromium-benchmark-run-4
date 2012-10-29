3e78406ce76463586cc52d023cd8a1464717bd7e
cdn@chromium.org <cdn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 20:29:27 +0000

    Adding mof files to the list of dangerous files for Windows. See http://blogs.iss.net/archive/papers/ibm-xforce-an-inside-look-at-stuxnet.pdf for example of attack.
    BUG=N/A
    TEST=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/11307010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_extensions.cc
+++ chrome/browser/download/download_extensions.cc
@@ -141,6 +141,7 @@ static const struct Executables {
   { "mht", AllowOnUserGesture },
   { "mhtml", AllowOnUserGesture },
   { "mmc", AllowOnUserGesture },
+  { "mof", Dangerous },
   { "msc", AllowOnUserGesture },
   { "msh", AllowOnUserGesture },
   { "mshxml", AllowOnUserGesture },
