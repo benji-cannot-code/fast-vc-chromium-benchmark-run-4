d9c10871fdc33595a77943612bacb4712b435aa0
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-29 22:50:02 +0000

    Non-special escape character sequences cause JSC::Lexer::parseString to create 16 bit strings https://bugs.webkit.org/show_bug.cgi?id=100576
    Reviewed by Darin Adler.
    
    Source/JavaScriptCore: 
    
    Changed singleEscape() processing to be based on a lookup of a static table.  The table
    covers ASCII characters SPACE through DEL.  If a character can be a single character escape,
    then the table provides the non-zero result of that escape.  Updated the result of
    singleEscape to be an LChar to make the table as small as possible.
    Added a new test fast/js/normal-character-escapes-in-string-literals.html to validated
    the behavior.
    
    * parser/Lexer.cpp:
    (JSC::singleEscape):
    (JSC::Lexer::parseString):
    (JSC::Lexer::parseStringSlowCase):
    
    LayoutTests: 
    
    Added a new test to validated the behavior of the corresponding changes to string processing
    in the Lexer.
    
    * fast/js/normal-character-escapes-in-string-literals-expected.txt: Added.
    * fast/js/normal-character-escapes-in-string-literals.html: Added.
    * fast/js/script-tests/normal-character-escapes-in-string-literals.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132853 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/normal-character-escapes-in-string-literals.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Test non-escaping characters in string literals - added with https://bugs.webkit.org/show_bug.cgi?id=100580"
+);
+
+function test(character)
+{
+    shouldBeEqualToString('eval(\'"\\' + character + '"\')', character);
+}
+
+charactersToTest = " !#$%&\'()*+,-./:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[]^_`acdeghijklmopqswyz{|}~";
+
+for (i = 0; i < charactersToTest.length; i++)
+    test(charactersToTest.charAt(i));
+
+function testOther(character)
+{
+    shouldEvaluateTo('eval(\'"\\' + character + '"\').charCodeAt(0)', character.charCodeAt(0));
+}
+
+// Test some characters outside the printable ASCII range
+otherCharactersToTest = "\x01\x07\x08\x1f\xa0\xa3\xab\xb4";
+
+for (i = 0; i < otherCharactersToTest.length; i++)
+    testOther(otherCharactersToTest.charAt(i));
