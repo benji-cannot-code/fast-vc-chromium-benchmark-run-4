e364a48a794414de625b359c6a72a6faad993ade
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-29 07:38:01 +0000

    Make rendering tables with <colgroups> twice as fast by avoiding walking the DOM for colgroups 4 times for each cell https://bugs.webkit.org/show_bug.cgi?id=100630
    Reviewed by Ojan Vafai.
    
    This is not a complete fix.  Our rendering of this large tables still takes 7.8 seconds
    on my retina MBP (down from 14.3s before this change).
    It's very expensive to walk the DOM each time we call RenderTable::colElement
    so this caches the RenderTableCol* in a vector for easier walking.
    We invalidate the cache any time a RenderTableCol is added or removed from the
    rendering sub-tree to avoid holding a bad pointer.
    
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::RenderTable):
    (WebCore::RenderTable::invalidateCachedColumns):
    (WebCore):
    (WebCore::RenderTable::addColumn):
    (WebCore::RenderTable::removeColumn):
    (WebCore::RenderTable::updateColumnCache):
    (WebCore::RenderTable::slowColElement):
    * rendering/RenderTable.h:
    (RenderTable):
    * rendering/RenderTableCol.cpp:
    (WebCore::RenderTableCol::insertedIntoTree):
    (WebCore):
    (WebCore::RenderTableCol::willBeRemovedFromTree):
    * rendering/RenderTableCol.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132764 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTable.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderTable.h
@@ -46,8 +46,6 @@ public:
     explicit RenderTable(Node*);
     virtual ~RenderTable();
 
-    int getColumnPos(unsigned col) const { return m_columnPos[col]; }
-
     // Per CSS 3 writing-mode: "The first and second values of the 'border-spacing' property represent spacing between columns
     // and rows respectively, not necessarily the horizontal and vertical spacing respectively".
     int hBorderSpacing() const { return m_hSpacing; }
@@ -259,6 +257,8 @@ public:
 
     void addCaption(const RenderTableCaption*);
     void removeCaption(const RenderTableCaption*);
+    void addColumn(const RenderTableCol*);
+    void removeColumn(const RenderTableCol*);
 
 protected:
     virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle);
@@ -284,6 +284,9 @@ private:
 
     RenderTableCol* slowColElement(unsigned col, bool* startEdge, bool* endEdge) const;
 
+    void updateColumnCache() const;
+    void invalidateCachedColumns();
+
     virtual RenderBlock* firstLineBlock() const;
     virtual void updateFirstLetter();
     
@@ -306,6 +309,7 @@ private:
     mutable Vector<int> m_columnPos;
     mutable Vector<ColumnStruct> m_columns;
     mutable Vector<RenderTableCaption*> m_captions;
+    mutable Vector<RenderTableCol*> m_columnRenderers;
 
     mutable RenderTableSection* m_head;
     mutable RenderTableSection* m_foot;
@@ -316,10 +320,12 @@ private:
     CollapsedBorderValues m_collapsedBorders;
     const CollapsedBorderValue* m_currentBorder;
     bool m_collapsedBordersValid : 1;
-    
+
     mutable bool m_hasColElements : 1;
     mutable bool m_needsSectionRecalc : 1;
+
     bool m_columnLogicalWidthChanged : 1;
+    mutable bool m_columnRenderersValid: 1;
 
     short m_hSpacing;
     short m_vSpacing;
