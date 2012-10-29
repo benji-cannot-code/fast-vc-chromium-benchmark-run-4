1ae14268e7859b1e85cdd3028221698aae0fadb4
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 16:53:53 +0000

    Read and apply the audio mute setting preserved when creating a new AudioHandler. Do not undo the mute setting if the volume percent > 0.
    BUG=157668
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=164341
    
    Review URL: https://codereview.chromium.org/11263015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/volume_controller_browsertest_chromeos.cc
+++ chrome/browser/ui/ash/volume_controller_browsertest_chromeos.cc
@@ -13,10 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// Default volume as a percentage in the range [0.0, 100.0].
+const double kDefaultVolumePercent = 75.0;
+
 class MockAudioMixer : public chromeos::AudioMixer {
  public:
   MockAudioMixer()
-      : volume_(0.0),
+      : volume_(kDefaultVolumePercent),
         is_muted_(false) {
   }
 
