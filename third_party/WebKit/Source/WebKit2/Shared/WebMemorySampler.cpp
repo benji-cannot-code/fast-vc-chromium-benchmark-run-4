9b8a21e3d6287b6ad59d06557fa7423da766f4ff
rakuco@webkit.org <rakuco@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-29 09:17:34 +0000

    Unreviewed build fix after r132709.
    * Shared/WebMemorySampler.cpp: Include unistd.h for getpid(3).
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebMemorySampler.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/WebMemorySampler.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(MEMORY_SAMPLER)
 
 #include <stdio.h>
+#include <unistd.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/StringBuilder.h>
 
