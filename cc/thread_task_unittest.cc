8942d4e2f24b9e557284c9c06c49f54b744ae85c
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 04:23:36 +0000

    cc: Remove 'using' namespace directive of WTF from thread_task_unittest.cc
    BUG=154451
    TEST=cc_unittests
    TBR=enne@chromium.org,jamesr@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11348002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164592 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/thread_task_unittest.cc
+++ cc/thread_task_unittest.cc
@@ -10,10 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using namespace cc;
-using namespace WTF;
-
-namespace {
+namespace cc {
 
 class Mock {
 public:
@@ -43,4 +40,4 @@ TEST(ThreadTaskTest, runnableMethods)
     createThreadTask(&mock, &Mock::method5, 9, 8, 7, 6, 5)->performTask();
 }
 
-} // namespace
+}  // namespace cc
