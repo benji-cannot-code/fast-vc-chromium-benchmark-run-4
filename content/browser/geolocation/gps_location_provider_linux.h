1315373597a259e8601de39fc18c5f7a8e6f36b6
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 03:28:25 +0000

    fix cros build error
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164587 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/gps_location_provider_linux.h
+++ content/browser/geolocation/gps_location_provider_linux.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 #include "content/public/common/geoposition.h"
 
+struct gps_data_t;
+
 namespace content {
 
 // Defines a wrapper around the C libgps API (gps.h). Similar to the libgpsmm.h
