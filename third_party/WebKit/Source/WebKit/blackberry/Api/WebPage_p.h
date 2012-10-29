f1e4ab7e86a7f31a2cc909d9d9859af7a907a5e0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-29 15:51:50 +0000

    [BlackBerry] Typo in WebPagePrivate::postponeDocumentRecalcStyle() https://bugs.webkit.org/show_bug.cgi?id=100377
    Patch by Mike Lattanzio <mlattanzio@rim.com> on 2012-10-29
    Reviewed by Adam Treat.
    
    Should be void, not bool.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::postponeDocumentStyleRecalc):
    * Api/WebPage_p.h:
    (WebPagePrivate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage_p.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage_p.h
@@ -462,7 +462,7 @@ public:
     void applySizeOverride(int overrideWidth, int overrideHeight);
     void setTextZoomFactor(float);
 
-    bool postponeDocumentStyleRecalc();
+    void postponeDocumentStyleRecalc();
     void resumeDocumentStyleRecalc();
 
     const WebCore::HitTestResult& hitTestResult(const WebCore::IntPoint& contentPos);
