2af891ed1de7bc0e2c84e93c727dee4b1f2824ad
asanka@chromium.org <asanka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 13:24:06 +0000

    Handle the case where IAttachmentExecute::Save() deletes a downloaded file.
    As a first step, mark the download as interrupted due to the file being blocked or due to the file being infected by a virus. The interrupt reason will be shown to the user in the downloads shelf and the downloads page.
    
    BUG=155957
    
    
    Review URL: https://chromiumcodereview.appspot.com/11150027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164632 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/base_file_mac.cc
+++ content/browser/download/base_file_mac.cc
@@ -10,12 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-void BaseFile::AnnotateWithSourceInformation() {
+DownloadInterruptReason BaseFile::AnnotateWithSourceInformation() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::FILE));
   DCHECK(!detached_);
 
   AddQuarantineMetadataToFile(full_path_, source_url_, referrer_url_);
   AddOriginMetadataToFile(full_path_, source_url_, referrer_url_);
+  return DOWNLOAD_INTERRUPT_REASON_NONE;
 }
 
 }  // namespace content
