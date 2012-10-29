b53b5977267c545be60d522da4cdff088035aaef
tomfinegan@chromium.org <tomfinegan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 22:50:23 +0000

    Add type argument to pepper content decryptor method GenerateKeyRequest().
    The type argument is used to carry a MIME type that identifies the type of initialization data.
        
    TEST=encrypted ExternalClearKey tests pass
    
    
    Review URL: https://chromiumcodereview.appspot.com/11270057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164757 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/ppp_content_decryptor_private_proxy.cc
+++ ppapi/proxy/ppp_content_decryptor_private_proxy.cc
@@ -132,6 +132,7 @@ bool InitializePppDecryptorBuffer(PP_Instance instance,
 
 void GenerateKeyRequest(PP_Instance instance,
                         PP_Var key_system,
+                        PP_Var type,
                         PP_Var init_data) {
   HostDispatcher* dispatcher = HostDispatcher::GetForInstance(instance);
   if (!dispatcher) {
@@ -144,6 +145,7 @@ void GenerateKeyRequest(PP_Instance instance,
           API_ID_PPP_CONTENT_DECRYPTOR_PRIVATE,
           instance,
           SerializedVarSendInput(dispatcher, key_system),
+          SerializedVarSendInput(dispatcher, type),
           SerializedVarSendInput(dispatcher, init_data)));
 }
 
@@ -412,11 +414,13 @@ bool PPP_ContentDecryptor_Private_Proxy::OnMessageReceived(
 void PPP_ContentDecryptor_Private_Proxy::OnMsgGenerateKeyRequest(
     PP_Instance instance,
     SerializedVarReceiveInput key_system,
+    SerializedVarReceiveInput type,
     SerializedVarReceiveInput init_data) {
   if (ppp_decryptor_impl_) {
     CallWhileUnlocked(ppp_decryptor_impl_->GenerateKeyRequest,
                       instance,
                       ExtractReceivedVarAndAddRef(dispatcher(), &key_system),
+                      ExtractReceivedVarAndAddRef(dispatcher(), &type),
                       ExtractReceivedVarAndAddRef(dispatcher(), &init_data));
   }
 }
