f02880a04ddbeda4624204a8f0a23b6a45214060
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-29 17:47:25 +0000

    [Qt] Flaky security tests https://bugs.webkit.org/show_bug.cgi?id=100388
    Reviewed by Jocelyn Turcotte.
    
    Tools:
    
    Disable text dumping and load a blank URL to ensure the documentLoader is cleared
    before we start dumping text for the new page. Otherwise messages from the old page
    may end up in the text output of the new test.
    
    This matches what WebKitTestRunner does, and reuses the Qt code for waiting for flag.
    
    * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
    (WebCore::DumpRenderTree::DumpRenderTree):
    (WebCore::runUntil):
    (WebCore::DumpRenderTree::resetToConsistentStateBeforeTesting):
    (WebCore::DumpRenderTree::finishedResetting):
    (WebCore::DumpRenderTree::dump):
    * DumpRenderTree/qt/DumpRenderTreeQt.h:
    (DumpRenderTree):
    * DumpRenderTree/qt/TestRunnerQt.cpp:
    (TestRunner::resetDumping):
    (TestRunner::reset):
    * DumpRenderTree/qt/TestRunnerQt.h:
    (TestRunner):
    
    LayoutTests:
    
    Unskip no longer flaky tests.
    
    * platform/qt/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/TestRunnerQt.h
+++ third_party/WebKit/Tools/DumpRenderTree/qt/TestRunnerQt.h
@@ -79,6 +79,7 @@ public:
     bool isPrinting() { return m_isPrinting; }
 
     void reset();
+    void resetDumping();
 
     static const unsigned int maxViewWidth;
     static const unsigned int maxViewHeight;
