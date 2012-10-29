55ac221bcbdfa191fcd2a1c008a36c8774b3c9fb
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-29 17:45:04 +0000

    app_list: Fix the remaining cpplint warn.
    This makes app_list cpplint clean, i.e, ZERO warnings!
    
    $ cpplint.py ui/app_list/* 2>&1
    Total errors found: 0
    
    R=xiyuan@chromium.org
    
    Review URL: https://codereview.chromium.org/11264042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164674 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/search_result_view.h
+++ ui/app_list/search_result_view.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_APP_LIST_SEARCH_RESULT_VIEW_H_
 
 #include <string>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
