7a7385336341c5beb2ad59d862f3727513c48652
scottmg@google.com <scottmg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 19:12:38 +0000

    Merge branch 'master' of http://git.chromium.org/chromium/src into reverty
    Revert "Automate Kraken benchmark with Chrome Remote Control."
    
    This reverts commit cf4d52c90d21f94e122910353d8b15ac9b17e87c.
    
    TBR=tonyg@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164969 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/third_party/kraken/tests/kraken-1.1/stanford-crypto-aes.js
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+new sjcl.test.TestCase("AES official known-answer tests", function (cb) {
+  if (!sjcl.cipher.aes) {
+    this.unimplemented();
+    cb && cb();
+    return;
+  }
+  
+  var i, kat = sjcl.test.vector.aes, tv, len, aes;
+
+  //XXX add more vectors instead of looping
+  for (var index = 0; index < 8; index++) {
+      for (i=0; i<kat.length; i++) {
+        tv = kat[i];
+        len = 32 * tv.key.length;
+        aes = new sjcl.cipher.aes(tv.key);
+        this.require(sjcl.bitArray.equal(aes.encrypt(tv.pt), tv.ct), "encrypt "+len+" #"+i);
+        this.require(sjcl.bitArray.equal(aes.decrypt(tv.ct), tv.pt), "decrypt "+len+" #"+i);
+      }
+  }
+  cb && cb();
+});
+
+sjcl.test.run();
