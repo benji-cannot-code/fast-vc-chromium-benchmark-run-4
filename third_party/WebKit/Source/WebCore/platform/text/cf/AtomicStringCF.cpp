c55119520b1c94f333e215b9c120d0da4274a051
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 11:04:54 +0000

    [Qt][Mac] Unreviewed build fix. https://bugs.webkit.org/show_bug.cgi?id=100727.
    Speculative build fix after 132858. Include missing CoreFoundation/CoreFoundation.h header.
    
    Patch by Ádám Kallai <kadam@inf.u-szeged.hu> on 2012-10-30
    
    * platform/text/cf/AtomicStringCF.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132891 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/cf/AtomicStringCF.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/cf/AtomicStringCF.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(CF)
 
+#include <CoreFoundation/CoreFoundation.h>
 #include <wtf/text/CString.h>
 
 namespace WTF {
