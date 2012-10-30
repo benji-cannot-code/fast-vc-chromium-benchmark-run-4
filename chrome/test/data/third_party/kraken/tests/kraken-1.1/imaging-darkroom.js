e5bb0af1101c7dceafcdbea260aaa1135c53b155
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 18:37:39 +0000

    Automate Kraken benchmark with Chrome Remote Control.
    This uses web page replay to record the benchmark itself so it deletes the
    unused kraken data checked into chrome/test/data.
    
    BUG=126516
    TEST=Manual on linux
    
    Review URL: https://codereview.chromium.org/11348021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/third_party/kraken/tests/kraken-1.1/imaging-darkroom.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-var width = 400, height = 267;
-for (var index = 0; index < paramArray.length; index++) {
-   var data = squidImageData;
-   data = ProcessImageData(data, width, height, paramArray[index]);
-}
