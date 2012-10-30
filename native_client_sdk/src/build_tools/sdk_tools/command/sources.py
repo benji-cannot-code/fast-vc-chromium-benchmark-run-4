60d113b4a60f3fdc8c3c74512ef5f8d3ef5afee9
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 20:38:30 +0000

    [NaCl SDK] Refactor sdk_update*.
    Still need to add tests and documentation, but this CL is getting large already.
    
    BUG=156766
    R=sbc@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/11228013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164998 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ native_client_sdk/src/build_tools/sdk_tools/command/sources.py
@@ -0,0 +1,30 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import download
+from sdk_update_common import Error
+
+def AddSource(config, url):
+  try:
+    download.UrlOpen(url)
+  except Exception as e:
+    raise Error('Not adding %s, unable to load URL.\n  %s' % (url, e))
+  config.AddSource(url)
+
+
+def RemoveSource(config, url):
+  if url == 'all':
+    config.RemoveAllSources()
+  else:
+    config.RemoveSource(url)
+
+
+def ListSources(config):
+  if config.sources:
+    print 'Installed sources:'
+    for s in config.sources:
+      print '  ' + s
+  else:
+    print 'No external sources installed.'
