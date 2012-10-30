1ec053a35b6fe9c952eb511a7e3e1e058646da8d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 00:30:09 +0000

    content/browser: Move notification_service_impl into content namespace.
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/11340018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164793 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/test_content_client_initializer.h
+++ content/public/test/test_content_client_initializer.h
@@ -9,12 +9,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 
-class NotificationServiceImpl;
-
 namespace content {
 
 class ContentClient;
 class MockRenderProcessHostFactory;
+class NotificationServiceImpl;
 class TestContentBrowserClient;
 class TestRenderViewHostFactory;
 
