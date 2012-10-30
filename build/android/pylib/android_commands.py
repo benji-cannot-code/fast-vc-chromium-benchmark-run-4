d4d23d88f147309c4f3558210ce95f5030cf7056
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 21:51:15 +0000

    Restart app before running monkey test.
    Also remove dependency on env variables during import of apk_info.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/11273099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165022 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/android_commands.py
+++ build/android/pylib/android_commands.py
@@ -509,7 +509,8 @@ class AndroidCommands(object):
   def StartActivity(self, package, activity, wait_for_completion=False,
                     action='android.intent.action.VIEW',
                     category=None, data=None,
-                    extras=None, trace_file_name=None):
+                    extras=None, trace_file_name=None,
+                    force_stop=False):
     """Starts |package|'s activity on the device.
 
     Args:
@@ -522,8 +523,12 @@ class AndroidCommands(object):
       data: Data string to pass to activity (e.g. 'http://www.example.com/').
       extras: Dict of extras to pass to activity. Values are significant.
       trace_file_name: If used, turns on and saves the trace to this file name.
+      force_stop: force stop the target app before starting the activity (-S
+        flag).
     """
     cmd = 'am start -a %s' % action
+    if force_stop:
+      cmd += ' -S'
     if wait_for_completion:
       cmd += ' -W'
     if category:
@@ -1081,4 +1086,3 @@ class NewLineNormalizer(object):
 
   def flush(self):
     self._output.flush()
-
