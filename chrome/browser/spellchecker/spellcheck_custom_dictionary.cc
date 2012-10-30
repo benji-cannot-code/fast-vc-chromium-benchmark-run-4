4f7bcf6821f5f7f598b728b6b4330238442e41e6
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 03:20:03 +0000

    fix compile on vs2012
    From http://build.chromium.org/p/chromium.fyi/waterfall?show=Chromium%20Win%20VS2012 :
    
    FAILED: ninja -t msvc -r . -o obj\chrome\browser\spellchecker\browser.spellcheck_custom_dictionary.obj -e environment.x86 -- cl.exe /nologo /showIncludes /FC @obj\chrome\browser\spellchecker\browser.spellcheck_custom_dictionary.obj.rsp  /c ..\..\chrome\browser\spellchecker\spellcheck_custom_dictionary.cc /Foobj\chrome\browser\spellchecker\browser.spellcheck_custom_dictionary.obj  /Fdbrowser.pdb 
    e:\b\build\slave\chromium_win_vs2012\build\src\chrome\browser\spellchecker\spellcheck_custom_dictionary.cc(41) :error C3861: 'mem_fun_ref': identifier not found
    
    R=rlp@chromium.org
    BUG=143646
    
    Review URL: https://chromiumcodereview.appspot.com/11338018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
+++ chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "content/public/browser/browser_thread.h"
 
+#include <functional>
+
 using content::BrowserThread;
 using chrome::spellcheck_common::WordList;
 
