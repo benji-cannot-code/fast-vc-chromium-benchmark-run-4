ea4537804432378bff5b6476171177101d4b2bc3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 12:01:48 +0000

    Web Inspector: Timeline: promote "cpu activity" out of experiment https://bugs.webkit.org/show_bug.cgi?id=100726
    Patch by Eugene Klyuchnikov <eustas.bug@gmail.com> on 2012-10-30
    Reviewed by Pavel Feldman.
    
    1. CPU actibity bars redesigned
    2. Experiment setting removed
    3. Feature setting added
    
    * English.lproj/localizedStrings.js: Updated setting label.
    * inspector/front-end/Settings.js: Replaced experiment with setting.
    * inspector/front-end/SettingsScreen.js: Added new setting.
    * inspector/front-end/TimelinePanel.js: Update cpu bars appearance.
    * inspector/front-end/timelinePanel.css: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132894 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/Settings.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Settings.js
@@ -56,7 +56,7 @@ var Capabilities = {
     heapProfilerPresent: false,
     canOverrideDeviceMetrics: false,
     timelineSupportsFrameInstrumentation: false,
-    canMonitorMainThread: false,
+    timelineCanMonitorMainThread: false,
     canOverrideGeolocation: false,
     canOverrideDeviceOrientation: false,
 }
@@ -109,6 +109,8 @@ WebInspector.Settings = function()
     this.lastDockState = this.createSetting("lastDockState", "");
     this.cssReloadEnabled = this.createSetting("cssReloadEnabled", false);
     this.cssReloadTimeout = this.createSetting("cssReloadTimeout", 1000);
+    this.showCpuOnTimelineRuler = this.createSetting("showCpuOnTimelineRuler", false);
+
 
     // If there are too many breakpoints in a storage, it is likely due to a recent bug that caused
     // periodical breakpoints duplication leading to inspector slowness.
@@ -197,7 +199,6 @@ WebInspector.ExperimentsSettings = function()
     this.liveNativeMemoryChart = this._createExperiment("liveNativeMemoryChart", "Live native memory chart");
     this.fileSystemInspection = this._createExperiment("fileSystemInspection", "FileSystem inspection");
     this.canvasInspection = this._createExperiment("canvasInspection ", "Canvas inspection");
-    this.mainThreadMonitoring = this._createExperiment("mainThreadMonitoring", "Show CPU activity in Timeline");
     this.sass = this._createExperiment("sass", "Support for SASS");
     this.codemirror = this._createExperiment("codemirror", "Use CodeMirror editor");
     this.cssRegions = this._createExperiment("cssRegions", "CSS Regions Support");
