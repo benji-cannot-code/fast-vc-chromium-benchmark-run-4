ed5bc2c2fe985abb4e480f73dc9624b01652083c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 03:49:55 +0000

    [chromium] Null-check WebViewImpl::m_client before calling invalidateRect() on it https://bugs.webkit.org/show_bug.cgi?id=100716
    Patch by James Robinson <jamesr@chromium.org> on 2012-10-29
    Reviewed by Dirk Pranke.
    
    This check was accidentally removed in r132862 and the value is null in at least some unit tests.
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::invalidateRect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -3850,7 +3850,7 @@ void WebViewImpl::invalidateRect(const IntRect& rect)
         IntRect dirtyRect = view->windowToContents(rect);
         updateLayerTreeViewport();
         m_nonCompositedContentHost->invalidateRect(dirtyRect);
-    } else
+    } else if (m_client)
         m_client->didInvalidateRect(rect);
 }
 
