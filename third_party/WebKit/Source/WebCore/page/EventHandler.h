ae1a143611adbd97252096971e00989189f0cc1e
wjmaclean@chromium.org <wjmaclean@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 21:29:32 +0000

    [chromium] Restrict link highlights to targets that display a hand-cursor https://bugs.webkit.org/show_bug.cgi?id=99862
    Reviewed by James Robinson.
    
    To avoid applying highlights to inappropriate targets, such as large divs with
    click handlers installed, test candidate targets and only highlight them if they
    would normally display a hand cursor on mouse over. This is a heuristic, but has
    been tested and found effective in practice.
    
    Source/WebCore:
    
    This CL refactors EventHandler::selectCursor() to make the logic for determining
    if CURSOR_AUTO should resolve to CURSOR_POINTER available outside EventHandler.
    
    Relies on existing cursor tests.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::useHandCursor):
    (WebCore):
    (WebCore::EventHandler::selectCursor):
    * page/EventHandler.h:
    (EventHandler):
    
    Source/WebKit/chromium:
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::handleGestureEvent):
    (WebKit::invokesHandCursor):
    (WebKit::WebViewImpl::bestTouchLinkNode):
    (WebKit::WebViewImpl::enableTouchHighlight):
    * src/WebViewImpl.h:
    (WebViewImpl):
    * tests/LinkHighlightTest.cpp:
    (WebCore::TEST):
    * tests/data/test_touch_link_highlight.html:
    
    LayoutTests:
    
    * platform/chromium-linux/compositing/gestures/gesture-tapHighlight-simple-rotated-div.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.h
+++ third_party/WebKit/Source/WebCore/page/EventHandler.h
@@ -235,6 +235,8 @@ public:
     bool handleTouchEvent(const PlatformTouchEvent&);
 #endif
 
+    bool useHandCursor(Node*, bool isOverLink, bool shiftKey);
+
 private:
 #if ENABLE(DRAG_SUPPORT)
     static DragState& dragState();
