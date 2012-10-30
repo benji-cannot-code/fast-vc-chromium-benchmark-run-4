ae14972698d85acc757081395d178eaca59ccc72
simonmorris@chromium.org <simonmorris@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 23:31:40 +0000

    [Chromoting] Make the AuthCodeGetter component of the Windows host configurer use ScopedComPtr correctly.
    BUG=158611
    
    
    Review URL: https://chromiumcodereview.appspot.com/11343050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165061 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/setup/win/auth_code_getter.cc
+++ remoting/host/setup/win/auth_code_getter.cc
@@ -19,7 +19,6 @@ namespace remoting {
 
 AuthCodeGetter::AuthCodeGetter() :
     browser_(NULL),
-    browser_running_(false),
     timer_interval_(base::TimeDelta::FromMilliseconds(kUrlPollIntervalMs)) {
 }
 
@@ -29,7 +28,7 @@ AuthCodeGetter::~AuthCodeGetter() {
 
 void AuthCodeGetter::GetAuthCode(
     base::Callback<void(const std::string&)> on_auth_code) {
-  if (browser_running_) {
+  if (browser_) {
     on_auth_code.Run("");
     return;
   }
@@ -40,7 +39,6 @@ void AuthCodeGetter::GetAuthCode(
     on_auth_code_.Run("");
     return;
   }
-  browser_running_ = true;
   base::win::ScopedBstr url(UTF8ToWide(
       GetOauthStartUrl(GetDefaultOauthRedirectUrl())).c_str());
   base::win::ScopedVariant empty_variant;
@@ -70,7 +68,7 @@ void AuthCodeGetter::OnTimer() {
 
 bool AuthCodeGetter::TestBrowserUrl(std::string* auth_code) {
   *auth_code = "";
-  if (!browser_running_) {
+  if (!browser_) {
     return true;
   }
   base::win::ScopedBstr url;
@@ -89,9 +87,9 @@ bool AuthCodeGetter::TestBrowserUrl(std::string* auth_code) {
 }
 
 void AuthCodeGetter::KillBrowser() {
-  if (browser_running_) {
+  if (browser_) {
     browser_->Quit();
-    browser_running_ = false;
+    browser_.Release();
   }
 }
 
