00058785b1982e02e52b71d80170d2a217fc6054
achicu@adobe.com <achicu@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 17:08:16 +0000

    [CSS Shaders] Add CustomFilterRenderer to reuse this class by Accelerated Compositing. https://bugs.webkit.org/show_bug.cgi?id=98989
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-10-30
    Reviewed by Dean Jackson.
    
    Extract CustomFilterRenderer class from the rendering part of FECustomFilter.
    FECustomFilter now plays a role in extending FilterEffect and delegates
    rendering CSS Shaders to CustomFilterRenderer.
    
    CustomFilterRenderer does not know Filter and FilterEffect. We can
    create a CustomFilterRenderer instance with only GraphicsContext3D and
    CustomFilterValidatedProgram. It means that Accelerated Compositing can
    create the CustomFilterRenderer instance if Accelerated Compositing has
    GraphicsContext3D and CustomFilterOperation, and it is already possible.
    
    This patch prepares to enable CSS Shaders on Accelerated Compositing.
    
    No new tests. Covered by css3/filters/custom
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * platform/graphics/filters/CustomFilterRenderer.cpp: Added.
    (WebCore):
    (WebCore::orthogonalProjectionMatrix):
    (WebCore::CustomFilterRenderer::create):
    (WebCore::CustomFilterRenderer::CustomFilterRenderer):
    (WebCore::CustomFilterRenderer::~CustomFilterRenderer):
    (WebCore::CustomFilterRenderer::premultipliedAlpha):
    (WebCore::CustomFilterRenderer::programNeedsInputTexture):
    (WebCore::CustomFilterRenderer::draw):
    (WebCore::CustomFilterRenderer::prepareForDrawing):
    (WebCore::CustomFilterRenderer::initializeCompiledProgramIfNeeded):
    (WebCore::CustomFilterRenderer::initializeMeshIfNeeded):
    (WebCore::CustomFilterRenderer::bindVertexAttribute):
    (WebCore::CustomFilterRenderer::unbindVertexAttribute):
    (WebCore::CustomFilterRenderer::bindProgramArrayParameters):
    (WebCore::CustomFilterRenderer::bindProgramNumberParameters):
    (WebCore::CustomFilterRenderer::bindProgramTransformParameter):
    (WebCore::CustomFilterRenderer::bindProgramParameters):
    (WebCore::CustomFilterRenderer::bindProgramAndBuffers):
    (WebCore::CustomFilterRenderer::unbindVertexAttributes):
    * platform/graphics/filters/CustomFilterRenderer.h: Copied from Source/WebCore/platform/graphics/filters/FECustomFilter.h.
    (WebCore):
    (CustomFilterRenderer):
      CustomFilterRenderer renders custom filters in GPU using a
      GraphicsContext3D.
    * platform/graphics/filters/FECustomFilter.cpp:
    (WebCore::FECustomFilter::FECustomFilter):
    (WebCore::FECustomFilter::create):
    (WebCore::FECustomFilter::deleteRenderBuffers):
    (WebCore::FECustomFilter::drawFilterMesh):
    (WebCore::FECustomFilter::prepareForDrawing):
    (WebCore::FECustomFilter::applyShader):
    (WebCore::FECustomFilter::resolveMultisampleBuffer):
    (WebCore::FECustomFilter::resizeMultisampleBuffers):
    (WebCore::FECustomFilter::resizeContext):
    * platform/graphics/filters/FECustomFilter.h:
    (WebCore):
    (FECustomFilter):
    * rendering/FilterEffectRenderer.cpp:
    (WebCore::createCustomFilterEffect):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132920 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/WebCore.gypi
+++ third_party/WebKit/Source/WebCore/WebCore.gypi
@@ -4986,6 +4986,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'platform/graphics/filters/CustomFilterProgram.h',
             'platform/graphics/filters/CustomFilterProgramInfo.cpp',
             'platform/graphics/filters/CustomFilterProgramInfo.h',
+            'platform/graphics/filters/CustomFilterRenderer.cpp',
+            'platform/graphics/filters/CustomFilterRenderer.h',
             'platform/graphics/filters/CustomFilterCompiledProgram.cpp',
             'platform/graphics/filters/CustomFilterCompiledProgram.h',
             'platform/graphics/filters/CustomFilterValidatedProgram.cpp',
--- third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
@@ -45,8 +45,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(CSS_SHADERS) && USE(3D_GRAPHICS)
 #include "CustomFilterGlobalContext.h"
-#include "CustomFilterProgram.h"
 #include "CustomFilterOperation.h"
+#include "CustomFilterProgram.h"
 #include "CustomFilterValidatedProgram.h"
 #include "FECustomFilter.h"
 #include "RenderView.h"
@@ -98,20 +98,22 @@ static PassRefPtr<FECustomFilter> createCustomFilterEffect(Filter* filter, Docum
 {
     if (!isCSSCustomFilterEnabled(document))
         return 0;
-    
+
     RefPtr<CustomFilterProgram> program = operation->program();
     if (!program->isLoaded())
         return 0;
 
     CustomFilterGlobalContext* globalContext = document->renderView()->customFilterGlobalContext();
     globalContext->prepareContextIfNeeded(document->view()->hostWindow());
+    if (!globalContext->context())
+        return 0;
+
     RefPtr<CustomFilterValidatedProgram> validatedProgram = globalContext->getValidatedProgram(program->programInfo());
     if (!validatedProgram->isInitialized())
         return 0;
 
-    return FECustomFilter::create(filter, globalContext, validatedProgram, operation->parameters(),
-                                  operation->meshRows(), operation->meshColumns(),
-                                  operation->meshBoxType(), operation->meshType());
+    return FECustomFilter::create(filter, globalContext->context(), validatedProgram.release(), operation->parameters(),
+        operation->meshRows(), operation->meshColumns(), operation->meshBoxType(), operation->meshType());
 }
 #endif
 
