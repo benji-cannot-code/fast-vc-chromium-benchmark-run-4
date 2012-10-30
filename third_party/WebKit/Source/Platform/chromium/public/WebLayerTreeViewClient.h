cf2e9a3a820c688c1e3b9d68037624da75a628a9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 19:24:08 +0000

    [chromium] Forward declare WebGraphicsContext3D in WebLayerTreeViewClient.h https://bugs.webkit.org/show_bug.cgi?id=100717
    Patch by James Robinson <jamesr@chromium.org> on 2012-10-30
    Reviewed by Adrienne Walker.
    
    Currently it isn't possible to include WebLayerTreeViewClient by itself.
    
    * chromium/public/WebLayerTreeViewClient.h:
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132938 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebLayerTreeViewClient.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebLayerTreeViewClient.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 class WebCompositorOutputSurface;
+class WebGraphicsContext3D;
 class WebInputHandler;
 class WebThread;
 struct WebSize;
