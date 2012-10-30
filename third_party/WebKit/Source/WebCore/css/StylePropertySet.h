9056d0c3943843f1ce7b03bd9359fb8199d10738
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 22:55:23 +0000

    StylePropertySet: Convert more logic to use PropertyReference. <http://webkit.org/b/100779>
    Reviewed by Antti Koivisto.
    
    Rewrite more of the StylePropertySet logic to use PropertyReference instead of CSSProperty.
    This is cleanup before changing the internal storage layout of immutable StylePropertySet.
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::CSSComputedStyleDeclaration::cssPropertyMatches):
    * css/CSSComputedStyleDeclaration.h:
    * css/CSSStyleDeclaration.h:
    * css/PropertySetCSSStyleDeclaration.cpp:
    (WebCore::PropertySetCSSStyleDeclaration::cssPropertyMatches):
    * css/PropertySetCSSStyleDeclaration.h:
    * css/StylePropertySet.cpp:
    (WebCore::StylePropertySet::appendFontLonghandValueIfExplicit):
    (WebCore::StylePropertySet::fontValue):
    (WebCore::StylePropertySet::get4Values):
    (WebCore::StylePropertySet::getPropertyCSSValue):
    (WebCore::StylePropertySet::removeProperty):
    (WebCore::StylePropertySet::propertyIsImportant):
    (WebCore::StylePropertySet::getPropertyShorthand):
    (WebCore::StylePropertySet::isPropertyImplicit):
    (WebCore::StylePropertySet::setProperty):
    (WebCore::StylePropertySet::mergeAndOverrideOnConflict):
    (WebCore::StylePropertySet::findPropertyIndex):
    (WebCore::StylePropertySet::findMutableCSSPropertyWithID):
    (WebCore::StylePropertySet::propertyMatches):
    (WebCore::StylePropertySet::removeEquivalentProperties):
    * css/StylePropertySet.h:
    (WebCore::StylePropertySet::PropertyReference::shorthandID):
    (WebCore::StylePropertySet::PropertyReference::isImplicit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StylePropertySet.h
+++ third_party/WebKit/Source/WebCore/css/StylePropertySet.h
@@ -63,7 +63,9 @@ public:
         { }
 
         CSSPropertyID id() const { return propertyInternal().id(); }
+        CSSPropertyID shorthandID() const { return propertyInternal().shorthandID(); }
         bool isImportant() const { return propertyInternal().isImportant(); }
+        bool isImplicit() const { return propertyInternal().isImplicit(); }
         bool isInherited() const { return propertyInternal().isInherited(); }
 
         String cssName() const { return propertyInternal().cssName(); }
@@ -180,12 +182,10 @@ private:
     bool appendFontLonghandValueIfExplicit(CSSPropertyID, StringBuilder& result) const;
 
     bool removeShorthandProperty(CSSPropertyID);
-    bool propertyMatches(const CSSProperty*) const;
+    bool propertyMatches(const PropertyReference&) const;
 
-    const CSSProperty* findPropertyWithId(CSSPropertyID) const;
-    CSSProperty* findPropertyWithId(CSSPropertyID);
-
-    void append(const CSSProperty&);
+    int findPropertyIndex(CSSPropertyID) const;
+    CSSProperty* findMutableCSSPropertyWithID(CSSPropertyID);
 
     CSSProperty& propertyAtInternal(unsigned index);
     const CSSProperty& propertyAtInternal(unsigned index) const;
