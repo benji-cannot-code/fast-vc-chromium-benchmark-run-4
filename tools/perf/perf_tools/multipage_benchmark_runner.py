3dcd587ff1bdc43bd05b4e16f50891fdc83b89b7
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 21:20:22 +0000

    Automate Kraken benchmark with Chrome Remote Control.
    BUG=126516
    TEST=Manual on linux
    
    Review URL: https://codereview.chromium.org/11346041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165012 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/multipage_benchmark_runner.py
+++ tools/perf/perf_tools/multipage_benchmark_runner.py
@@ -15,6 +15,7 @@ from chrome_remote_control import page_runner
 from chrome_remote_control import page_set
 
 import perf_tools.first_paint_time_benchmark
+import perf_tools.kraken
 import perf_tools.scrolling_benchmark
 import perf_tools.skpicture_printer
 import perf_tools.texture_upload_benchmark
@@ -23,6 +24,8 @@ import perf_tools.texture_upload_benchmark
 _BENCHMARKS = {
   'first_paint_time_benchmark':
       perf_tools.first_paint_time_benchmark.FirstPaintTimeBenchmark,
+  'kraken':
+      perf_tools.kraken.Kraken,
   'scrolling_benchmark':
       perf_tools.scrolling_benchmark.ScrollingBenchmark,
   'skpicture_printer':
