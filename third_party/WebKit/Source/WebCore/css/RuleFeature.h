3d3da1d6b0a9c4a1d018baf2372e014c421b12a2
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 20:25:52 +0000

    Avoid unnecessary style recalcs on class attribute mutation https://bugs.webkit.org/show_bug.cgi?id=100776
    Reviewed by Andreas Kling.
    
    There is no need to invalidate element style on class attribute change if neither the added
    or removed classes featured in any active stylesheet.
    
    * css/RuleFeature.cpp:
    (WebCore::RuleFeatureSet::add):
    (WebCore::RuleFeatureSet::clear):
    (WebCore::RuleFeatureSet::reportMemoryUsage):
    * css/RuleFeature.h:
    (RuleFeatureSet):
    * css/RuleSet.cpp:
    (WebCore::collectFeaturesFromSelector):
            
        Collect classes mentioned in CSS selectors the same way ids and attribute names are
        already collected.
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::hasSelectorForClass):
            
        Add a method to test if a given class name is mentioned anywhere in stylehseets.
    
    (WebCore):
    * css/StyleResolver.h:
    * css/StyleScopeResolver.h:
    (WebCore):
    * dom/Element.cpp:
    (WebCore::collectAddedAndRemovedClasses):
    (WebCore):
    (WebCore::Element::classAttributeChanged):
            
        Figure out which classes were added and removed. Test if they are present in any style
        rule and invalidate the style only if they are.
    
    * dom/SpaceSplitString.cpp:
    (WebCore::SpaceSplitStringData::add):
    (WebCore::SpaceSplitStringData::remove):
    (WebCore::SpaceSplitString::add):
    (WebCore::SpaceSplitString::remove):
            
        Added bool return value to indicate if anything was actually removed. Reorganized 
        the code a bit to avoid unnecessary uniquing when nothing changes.
    
    * dom/SpaceSplitString.h:
    (SpaceSplitStringData):
    (SpaceSplitString):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/RuleFeature.h
+++ third_party/WebKit/Source/WebCore/css/RuleFeature.h
@@ -32,8 +32,7 @@ namespace WebCore {
 
 class StyleRule;
 
-class RuleFeature {
-public:
+struct RuleFeature {
     RuleFeature(StyleRule* rule, unsigned selectorIndex, bool hasDocumentSecurityOrigin)
         : rule(rule)
         , selectorIndex(selectorIndex)
@@ -45,8 +44,7 @@ public:
     bool hasDocumentSecurityOrigin;
 };
 
-class RuleFeatureSet {
-public:
+struct RuleFeatureSet {
     RuleFeatureSet()
         : usesFirstLineRules(false)
         , usesBeforeAfterRules(false)
@@ -56,6 +54,7 @@ public:
     void clear();
     void reportMemoryUsage(MemoryObjectInfo*) const;
     HashSet<AtomicStringImpl*> idsInRules;
+    HashSet<AtomicStringImpl*> classesInRules;
     HashSet<AtomicStringImpl*> attrsInRules;
     Vector<RuleFeature> siblingRules;
     Vector<RuleFeature> uncommonAttributeRules;
