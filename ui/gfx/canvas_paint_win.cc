395d8832a7c982095ca035c3fb1d0fd2feb82470
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 13:46:12 +0000

    reapply 164660 w/ fix to add UI_EXPORT to new classes, so we can link in component build.
    https://codereview.chromium.org/11193037/
    Review URL: https://codereview.chromium.org/11338013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164905 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/canvas_paint_win.cc
+++ ui/gfx/canvas_paint_win.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "ui/gfx/canvas.h"
-#include "ui/gfx/canvas_paint.h"
 #include "ui/gfx/canvas_skia_paint.h"
 #include "ui/gfx/rect.h"
 
