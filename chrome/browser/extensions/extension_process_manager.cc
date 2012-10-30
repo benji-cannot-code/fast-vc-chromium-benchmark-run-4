9452f35a13bead8818b4244e8e917ac769cb05d8
MHX348@motorola.com <MHX348@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 22:14:31 +0000

    Incognito-allowed package apps produce duplicate items in context menu
    Clean-up all context menu items created for 'incognito' 'split' mode
    while destroying incognito profile.
    
    Patch from Joe Thomas <mhx348@motorola.com>.
    
    BUG=138487
    TEST=As mentioned in the bug report
    
    
    Review URL: https://chromiumcodereview.appspot.com/11226037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.cc
+++ chrome/browser/extensions/extension_process_manager.cc
@@ -67,7 +67,7 @@ std::string GetExtensionID(RenderViewHost* render_view_host) {
 class IncognitoExtensionProcessManager : public ExtensionProcessManager {
  public:
   explicit IncognitoExtensionProcessManager(Profile* profile);
-  virtual ~IncognitoExtensionProcessManager() {}
+  virtual ~IncognitoExtensionProcessManager();
   virtual ExtensionHost* CreateViewHost(
       const Extension* extension,
       const GURL& url,
@@ -187,6 +187,17 @@ ExtensionProcessManager::ExtensionProcessManager(Profile* profile)
 ExtensionProcessManager::~ExtensionProcessManager() {
   CloseBackgroundHosts();
   DCHECK(background_hosts_.empty());
+}
+
+const ExtensionProcessManager::ViewSet
+ExtensionProcessManager::GetAllViews() const {
+  ViewSet result;
+  for (ExtensionRenderViews::const_iterator iter =
+           all_extension_views_.begin();
+       iter != all_extension_views_.end(); ++iter) {
+    result.insert(iter->first);
+  }
+  return result;
 }
 
 void ExtensionProcessManager::EnsureBrowserWhenRequired(
@@ -735,15 +746,13 @@ IncognitoExtensionProcessManager::IncognitoExtensionProcessManager(
                  content::NotificationService::AllSources());
 }
 
-const ExtensionProcessManager::ViewSet
-ExtensionProcessManager::GetAllViews() const {
-  ViewSet result;
-  for (ExtensionRenderViews::const_iterator iter =
-           all_extension_views_.begin();
-       iter != all_extension_views_.end(); ++iter) {
-    result.insert(iter->first);
-  }
-  return result;
+IncognitoExtensionProcessManager::~IncognitoExtensionProcessManager() {
+  // TODO(yoz): This cleanup code belongs in the MenuManager.
+  // Remove "incognito" "split" mode context menu items.
+  ExtensionService* service =
+      extensions::ExtensionSystem::Get(GetProfile())->extension_service();
+  if (service)
+    service->menu_manager()->RemoveAllIncognitoContextItems();
 }
 
 ExtensionHost* IncognitoExtensionProcessManager::CreateViewHost(
