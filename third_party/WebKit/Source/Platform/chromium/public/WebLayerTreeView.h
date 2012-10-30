d7ff84a92b3b7d7ec71fb43642317b0c239974a3
wjmaclean@chromium.org <wjmaclean@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 21:38:00 +0000

    [chromium] Transform mouse/gesture event coordinates to account for pinch-zoom in compositor. https://bugs.webkit.org/show_bug.cgi?id=100542
    Reviewed by James Robinson.
    
    Pinch-zoom in the compositor applies a transformation that WebCore does not know about.
    We need to adjust mouse and gesture event coordinates to account for this in order that
    hit testing work properly for these types while zoomed.
    
    Source/Platform:
    
    * chromium/public/WebLayerTreeView.h:
    (WebLayerTreeView):
    (WebKit::WebLayerTreeView::adjustPointForZoom): Adjusts point coordinated to account for zoom in compositor.
    
    Source/WebKit/chromium:
    
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::handleInputEvent): Modified to adjoint event coordinates to account for zoom.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132946 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebLayerTreeView.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebLayerTreeView.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "SkBitmap.h"
 #include "WebColor.h"
 #include "WebCommon.h"
+#include "WebFloatPoint.h"
 #include "WebNonCopyable.h"
 #include "WebPrivateOwnPtr.h"
 #include "WebSize.h"
@@ -95,6 +96,9 @@ public:
     // from the above if there exists page scale, device scale or fixed layout
     // mode).
     virtual WebSize deviceViewportSize() const = 0;
+
+    // FIXME: Once cc::LayerTreeHost::adjustEventPointForPinchZoom lands, make this pure virtual.
+    virtual WebFloatPoint adjustEventPointForPinchZoom(const WebFloatPoint& point) const { return point; }
 
     virtual void setDeviceScaleFactor(float) = 0;
     virtual float deviceScaleFactor() const = 0;
