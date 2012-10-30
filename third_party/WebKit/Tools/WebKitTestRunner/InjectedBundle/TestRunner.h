2d50c1196423cf49edc92832391e304c7ff6bc44
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-30 15:50:52 +0000

    [WK2][WTR] WebKitTestRunner needs testRunner.queueLoadHTMLString https://bugs.webkit.org/show_bug.cgi?id=100747
    Patch by Mikhail Pozdnyakov <mikhail.pozdnyakov@intel.com> on 2012-10-30
    Reviewed by Kenneth Rohde Christiansen.
    
    Tools:
    
    Added testRunner.queueLoadHTMLString implementation.
    
    * WebKitTestRunner/InjectedBundle/Bindings/TestRunner.idl:
    * WebKitTestRunner/InjectedBundle/InjectedBundle.cpp:
    (WTR::InjectedBundle::queueLoadHTMLString):
    (WTR):
    * WebKitTestRunner/InjectedBundle/InjectedBundle.h:
    (InjectedBundle):
    * WebKitTestRunner/InjectedBundle/TestRunner.cpp:
    (WTR::TestRunner::queueLoadHTMLString):
    (WTR):
    * WebKitTestRunner/InjectedBundle/TestRunner.h:
    (TestRunner):
    * WebKitTestRunner/TestInvocation.cpp:
    (WTR::TestInvocation::didReceiveMessageFromInjectedBundle):
    * WebKitTestRunner/WorkQueueManager.cpp:
    (WTR::WorkQueueManager::queueLoadHTMLString):
    (WTR):
    * WebKitTestRunner/WorkQueueManager.h:
    (WorkQueueManager):
    
    LayoutTests:
    
    Unskipped http/tests/navigation/go-back-to-error-page.html for WK2 ports.
    
    * platform/wk2/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/TestRunner.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/TestRunner.h
@@ -266,6 +266,7 @@ public:
     void queueBackNavigation(unsigned howFarBackward);
     void queueForwardNavigation(unsigned howFarForward);
     void queueLoad(JSStringRef url, JSStringRef target);
+    void queueLoadHTMLString(JSStringRef content, JSStringRef baseURL, JSStringRef unreachableURL);
     void queueReload();
     void queueLoadingScript(JSStringRef script);
     void queueNonLoadingScript(JSStringRef script);
