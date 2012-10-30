benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 149c36273e285ae73b9c6ff72cfec6a912766353

Replace shadow assets
BUG=154959
R=xiyuan@chromium.org


Review URL: https://chromiumcodereview.appspot.com/11185068

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164119 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/notifications/balloon_view_views.cc
@@ -110,7 +110,7 @@ BalloonViewImpl::BalloonViewImpl(BalloonCollection* collection)
 
   views::BubbleBorder* bubble_border =
       new views::BubbleBorder(views::BubbleBorder::FLOAT,
-                              views::BubbleBorder::NO_SHADOW);
+                              views::BubbleBorder::SMALL_SHADOW);
   set_border(bubble_border);
 }
 

ash/resources/ash_resources.grd
@@ -91,14 +91,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_INACTIVE_TOP" file="common/window_shadow_inactive_top.png" />
       <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_INACTIVE_TOP_LEFT" file="common/window_shadow_inactive_top_left.png" />
       <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_INACTIVE_TOP_RIGHT" file="common/window_shadow_inactive_top_right.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_BOTTOM" file="common/small_shadow_bottom.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_BOTTOM_LEFT" file="common/small_shadow_bottom_left.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_BOTTOM_RIGHT" file="common/small_shadow_bottom_right.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_LEFT" file="common/small_shadow_left.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_RIGHT" file="common/small_shadow_right.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_TOP" file="common/small_shadow_top.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_TOP_LEFT" file="common/small_shadow_top_left.png" />
-      <structure type="chrome_scaled_image" name="IDR_AURA_SHADOW_SMALL_TOP_RIGHT" file="common/small_shadow_top_right.png" />
       <structure type="chrome_scaled_image" name="IDR_AURA_UBER_TRAY_ACCESSIBILITY" file="common/status_accessibility_mode.png" />
       <structure type="chrome_scaled_image" name="IDR_AURA_UBER_TRAY_ACCESSIBILITY_DARK" file="common/status_accessibility_dark.png" />
       <structure type="chrome_scaled_image" name="IDR_AURA_UBER_TRAY_BLUETOOTH" file="common/status_bluetooth.png" />

commit 3804e3d168b497d8fef408cd8b36b16a8ce06746

Fix notification balloons rendering on Windows. Regression from r164119. BUG=158132
Review URL: https://codereview.chromium.org/11339023

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164809 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/notifications/balloon_view_views.cc
@@ -110,7 +110,7 @@ BalloonViewImpl::BalloonViewImpl(BalloonCollection* collection)
 
   views::BubbleBorder* bubble_border =
       new views::BubbleBorder(views::BubbleBorder::FLOAT,
-                              views::BubbleBorder::SMALL_SHADOW);
+                              views::BubbleBorder::NO_SHADOW);
   set_border(bubble_border);
 }
 