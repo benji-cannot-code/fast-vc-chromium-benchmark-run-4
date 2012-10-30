01684a3fc58f7a27960a83c680790ba2039df5da
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 04:56:01 +0000

    ui-events: Prevent a crash when the dispatcher is destroyed in the middle of dispatching an event.
    BUG=158351
    
    Review URL: https://codereview.chromium.org/11342018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164846 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/events/event_dispatcher.cc
+++ ui/base/events/event_dispatcher.cc
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-EventDispatcher::EventDispatcher() {
+EventDispatcher::EventDispatcher()
+    : set_on_destroy_(NULL) {
 }
 
 EventDispatcher::~EventDispatcher() {
+  if (set_on_destroy_)
+    *set_on_destroy_ = true;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
