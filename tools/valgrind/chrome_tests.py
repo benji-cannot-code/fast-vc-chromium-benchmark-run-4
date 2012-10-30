cdbe588bb691e7ef06f11bb1c0662301dc2c55f7
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 20:37:02 +0000

    Add device_unittests to valgrind/heapchecker scripts.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11345039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164997 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/chrome_tests.py
+++ tools/valgrind/chrome_tests.py
@@ -272,6 +272,9 @@ class ChromeTests:
   def TestCrypto(self):
     return self.SimpleTest("crypto", "crypto_unittests")
 
+  def TestDevice(self):
+    return self.SimpleTest("device", "device_unittests")
+
   def TestFFmpeg(self):
     return self.SimpleTest("chrome", "ffmpeg_unittests")
 
@@ -508,6 +511,7 @@ class ChromeTests:
     "content_browsertests": TestContentBrowser,
     "courgette": TestCourgette,  "courgette_unittests": TestCourgette,
     "crypto": TestCrypto,        "crypto_unittests": TestCrypto,
+    "device": TestDevice,        "device_unittests": TestDevice,
     "ffmpeg": TestFFmpeg,        "ffmpeg_unittests": TestFFmpeg,
     "ffmpeg_regression_tests": TestFFmpegRegressions,
     "googleurl": TestGURL,       "googleurl_unittests": TestGURL,
