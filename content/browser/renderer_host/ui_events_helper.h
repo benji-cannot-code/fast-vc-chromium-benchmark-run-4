4305fbb4e475a814737d63f0d1f3c4b2a36855b2
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-30 18:53:55 +0000

    gesture: Share some code across non-aura Windows and CrOS.
    Both Windows and CrOS had their own code to create a WebGestureEvent out of a
    ui::GestureEvent. This change moves the code to a common place so that both can
    use the same implementation.
    
    BUG=143246
    
    Review URL: https://codereview.chromium.org/11344034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@164964 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/ui_events_helper.h
+++ content/browser/renderer_host/ui_events_helper.h
@@ -10,11 +10,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/content_export.h"
 
 namespace WebKit {
+class WebGestureEvent;
 class WebTouchEvent;
 class WebTouchPoint;
 }
 
 namespace ui {
+class GestureEvent;
 class TouchEvent;
 }
 
@@ -29,13 +31,19 @@ CONTENT_EXPORT bool MakeUITouchEventsFromWebTouchEvents(
     const WebKit::WebTouchEvent& touch,
     ScopedVector<ui::TouchEvent>* list);
 
+// Creates a WebGestureEvent from a ui::GestureEvent. Note that it does not
+// populate the event coordinates (i.e. |x|, |y|, |globalX|, and |globalY|). So
+// the caller must populate these fields.
+WebKit::WebGestureEvent MakeWebGestureEventFromUIEvent(
+    const ui::GestureEvent& event);
+
 int EventFlagsToWebEventModifiers(int flags);
 
 // Updates the WebTouchEvent based on the TouchEvent. It returns the updated
 // WebTouchPoint contained in the WebTouchEvent, or NULL if no point was
 // updated.
 WebKit::WebTouchPoint* UpdateWebTouchEventFromUIEvent(
-    ui::TouchEvent* event,
+    const ui::TouchEvent& event,
     WebKit::WebTouchEvent* web_event);
 }
 
--- content/browser/renderer_host/web_input_event_aura.cc
+++ content/browser/renderer_host/web_input_event_aura.cc
@@ -28,8 +28,6 @@ WebKit::WebMouseWheelEvent MakeWebMouseWheelEventFromAuraEvent(
 WebKit::WebKeyboardEvent MakeWebKeyboardEventFromAuraEvent(
     ui::KeyEvent* event);
 WebKit::WebGestureEvent MakeWebGestureEventFromAuraEvent(
-    ui::GestureEvent* event);
-WebKit::WebGestureEvent MakeWebGestureEventFromAuraEvent(
     ui::ScrollEvent* event);
 #endif
 
@@ -146,7 +144,7 @@ WebKit::WebGestureEvent MakeWebGestureEvent(ui::GestureEvent* event) {
 #if defined(OS_WIN)
   gesture_event = MakeWebGestureEventFromNativeEvent(event->native_event());
 #else
-  gesture_event = MakeWebGestureEventFromAuraEvent(event);
+  gesture_event = MakeWebGestureEventFromUIEvent(*event);
 #endif
 
   gesture_event.x = event->x();
