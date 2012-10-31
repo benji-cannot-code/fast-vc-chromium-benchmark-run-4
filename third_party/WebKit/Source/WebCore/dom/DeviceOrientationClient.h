1d5c014fbc1d6fa72f5d9369ca1b4a7e75818f99
kihong.kwon@samsung.com <kihong.kwon@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-31 12:55:54 +0000

    Add DeviceController base-class to remove duplication of DeviceXXXControler https://bugs.webkit.org/show_bug.cgi?id=96894
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    Add DeviceController which is extracted from DeviceOrientationController to remove duplication.
    And soon-to-be-added DeviceMotionController and ProximityController.
    
    Covered by existing tests.
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * dom/DeviceOrientationClient.h:
    * dom/DeviceOrientationController.cpp:
    Remove member functions to move to DeviceController.
    - addListener(), removeListener(), removeAllListeners(), isActive()
    (WebCore::DeviceOrientationController::DeviceOrientationController):
    (WebCore::DeviceOrientationController::didChangeDeviceOrientation):
    (WebCore::DeviceOrientationController::client):
    (WebCore::DeviceOrientationController::hasLastData):
    (WebCore::DeviceOrientationController::getLastEvent):
    (WebCore::DeviceOrientationController::from):
    (WebCore):
    * dom/DeviceOrientationController.h:
    (WebCore):
    (WebCore::DeviceOrientationController::~DeviceOrientationController):
    (DeviceOrientationController):
    * dom/Document.cpp:
    Remove suspendEventsForAllListeners() and resumeEventsForAllListeners() function calls.
    These calls can be made by checking activeDOMObjectsAreSuspended() and activeDOMObjectsAreStopped() before dispatchEvent.
    (WebCore::Document::suspendActiveDOMObjects):
    (WebCore::Document::resumeActiveDOMObjects):
    * loader/EmptyClients.h:
    (EmptyDeviceClient):
    (WebCore::EmptyDeviceClient::startUpdating):
    (WebCore::EmptyDeviceClient::stopUpdating):
    (WebCore):
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::addEventListener):
    (WebCore::DOMWindow::removeEventListener):
    (WebCore::DOMWindow::removeAllEventListeners):
    * page/DeviceClient.h: Added.
    (WebCore):
    (DeviceClient):
    (WebCore::DeviceClient::~DeviceClient):
    * page/DeviceController.cpp: Added.
    DeviceController has extracted functions from DeviceOrientationController and DeviceMotionController.
    - addDeviceEventListener(), removeDeviceEventlistener(), removeAllDeviceEventListeners(), dispatchDeviceEvent()
    All kind of device event controller which has DeviceClient can be inherited from DeviceController.
    (WebCore):
    (WebCore::DeviceController::DeviceController):
    (WebCore::DeviceController::addDeviceEventListener):
    (WebCore::DeviceController::removeDeviceEventListener):
    (WebCore::DeviceController::removeAllDeviceEventListeners):
    (WebCore::DeviceController::dispatchDeviceEvent):
    (WebCore::DeviceController::fireDeviceEvent):
    * page/DeviceController.h: Added.
    (WebCore):
    (DeviceController):
    (WebCore::DeviceController::~DeviceController):
    (WebCore::DeviceController::isActive):
    (WebCore::DeviceController::client):
    (WebCore::DeviceController::hasLastData):
    (WebCore::DeviceController::getLastEvent):
    
    Source/WebKit/qt:
    
    Change client() to deviceOrientationClient() to get DeviceOrientationClient*.
    
    * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
    (DumpRenderTreeSupportQt::setMockDeviceOrientation):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133016 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DeviceOrientationClient.h
+++ third_party/WebKit/Source/WebCore/dom/DeviceOrientationClient.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright 2010, The Android Open Source Project
+ * Copyright (C) 2012 Samsung Electronics. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,18 +28,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DeviceOrientationClient_h
 #define DeviceOrientationClient_h
 
+#include "DeviceClient.h"
+
 namespace WebCore {
 
 class DeviceOrientationController;
 class DeviceOrientationData;
 class Page;
 
-class DeviceOrientationClient {
+class DeviceOrientationClient : public DeviceClient {
 public:
     virtual ~DeviceOrientationClient() {}
     virtual void setController(DeviceOrientationController*) = 0;
-    virtual void startUpdating() = 0;
-    virtual void stopUpdating() = 0;
     virtual DeviceOrientationData* lastOrientation() const = 0;
     virtual void deviceOrientationControllerDestroyed() = 0;
 };
