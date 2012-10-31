c529e54a6fbe648b891d45813cae098d54608390
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-31 19:34:11 +0000

    garden-o-matic should support gardening a single port and specifying how to deal with overwritten baselines https://bugs.webkit.org/show_bug.cgi?id=100563
    Reviewed by Ojan Vafai.
    
    This patch starts the process of making garden-o-matic more useful for
    bringing up a single new port in two ways. The goal is to fully
    obsolete webkit-patch rebaseline-server.
    
    First, it adds support for the --platform arguments to garden-o-matic
    (so that the UI will default to the right cluster of bots), and if the platform arg
    specifies a single bot, filters the results down to just that bot.
    
    Second, it adds support for --move-overwritten-baselines and changes
    the implementation of the move_overwritten_baselines setting in builders.py
    so that it is only used if this flag is provided.
    
    Future patches will make it possible to use local copies of the build
    results (rather than going to the bots) and other optimizations to make
    it faster to review lots of changes on a single bot.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/garden-o-matic.html:
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/config.js:
    (.):
    * Scripts/webkitpy/layout_tests/port/builders.py:
    * Scripts/webkitpy/tool/commands/gardenomatic.py:
    (GardenOMatic):
    (GardenOMatic.execute):
    * Scripts/webkitpy/tool/commands/rebaseline.py:
    (AbstractRebaseliningCommand):
    (AbstractRebaseliningCommand.__init__):
    (RebaselineTest):
    (OptimizeBaselines):
    (AnalyzeBaselines):
    (AbstractParallelRebaselineCommand):
    (AbstractParallelRebaselineCommand._rebaseline_commands):
    (AbstractParallelRebaselineCommand._rebaseline):
    (Rebaseline.__init__):
    * Scripts/webkitpy/tool/servers/gardeningserver.py:
    (GardeningHTTPServer.url):
    (GardeningHTTPRequestHandler.rebaselineall):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/model.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/model.js
@@ -159,7 +159,7 @@ model.latestRevisionWithNoBuildersInFlight = function()
 
 model.updateResultsByBuilder = function(callback)
 {
-    var platformBuilders = config.kPlatforms[config.currentPlatform].builders;
+    var platformBuilders = config.currentBuilders();
     results.fetchResultsByBuilder(Object.keys(platformBuilders), function(resultsByBuilder) {
         model.state.resultsByBuilder = resultsByBuilder;
         callback();
--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/gardeningserver.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/gardeningserver.py
@@ -28,6 +28,7 @@ import logging
 import json
 import os
 import sys
+import urllib
 
 from webkitpy.common.memoized import memoized
 from webkitpy.tool.servers.reflectionhandler import ReflectionHandler
@@ -59,8 +60,10 @@ class GardeningHTTPServer(BaseHTTPServer.HTTPServer):
         self.options = config['options']
         BaseHTTPServer.HTTPServer.__init__(self, (server_name, httpd_port), GardeningHTTPRequestHandler)
 
-    def url(self):
-        return 'file://' + os.path.join(GardeningHTTPRequestHandler.STATIC_FILE_DIRECTORY, 'garden-o-matic.html')
+    def url(self, args=None):
+        # We can't use urllib.encode() here because that encodes spaces as plus signs and the buildbots don't decode those properly.
+        arg_string = ('?' + '&'.join("%s=%s" % (key, urllib.quote(value)) for (key, value) in args.items())) if args else ''
+        return 'file://' + os.path.join(GardeningHTTPRequestHandler.STATIC_FILE_DIRECTORY, 'garden-o-matic.html' + arg_string)
 
 
 class GardeningHTTPRequestHandler(ReflectionHandler):
@@ -107,6 +110,10 @@ class GardeningHTTPRequestHandler(ReflectionHandler):
 
     def rebaselineall(self):
         command = ['rebaseline-json']
+        if self.server.options.move_overwritten_baselines:
+            command.append('--move-overwritten-baselines')
+        if not self.server.options.optimize:
+            command.append('--no-optimize')
         if self.server.options.verbose:
             command.append('--verbose')
         json_input = self.read_entity_body()
