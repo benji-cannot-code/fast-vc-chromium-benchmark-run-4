3189547192409acc92dfd9486eb51b5560a05f62
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 21:54:02 +0000

    Reenable the BookmarkBarViewTest8.DNDBackToOriginatingMenu test on AURA.
    While instantiating the Shell class for the duration of the tests, we pass in a ShellDelegate
    which is implemented by the TestShellDelegate class. This should ensure that the tests run without
    assuming that the screen is locked. 
    
    The code to remove registered EventClients has been removed.
    
    BUG=158564
    R=ben
    Review URL: https://codereview.chromium.org/11347051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165239 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/view_event_test_base.cc
+++ chrome/test/base/view_event_test_base.cc
@@ -19,7 +19,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_ASH)
 #include "ash/shell.h"
+#include "ash/test/test_shell_delegate.h"
 #endif
+
 #if defined(USE_AURA)
 #include "ui/aura/client/event_client.h"
 #include "ui/aura/env.h"
@@ -87,11 +89,7 @@ void ViewEventTestBase::SetUp() {
   ui::TextInputTestSupport::Initialize();
   ui::CompositorTestSupport::Initialize();
 #if defined(USE_ASH)
-  ash::Shell::CreateInstance(NULL);
-  // The shell runs with a locked screen in tests, so we must clear the event
-  // client so it doesn't interfere with event propagation.
-  aura::client::SetEventClient(ash::Shell::GetPrimaryRootWindow(),
-                               NULL);
+  ash::Shell::CreateInstance(new ash::test::TestShellDelegate());
 #endif
   window_ = views::Widget::CreateWindow(this);
 }
