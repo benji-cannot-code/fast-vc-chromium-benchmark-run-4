7edd61acf6ab0c58f995114a4952f0bc9e27203e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-31 08:58:58 +0000

    [EFL][DRT] EventSender requires contextClick implementation https://bugs.webkit.org/show_bug.cgi?id=86091
    Patch by Michał Pakuła vel Rutka <m.pakula@samsung.com> on 2012-10-31
    Reviewed by Gyuyoung Kim.
    
    Source/WebKit:
    
    Add new unit test file, needed due to ewk API changes.
    
    * PlatformEfl.cmake:
    
    Source/WebKit/efl:
    
    Add a pointer to parent menu in Ewk_Context_Menu_Item and a getter
    function to allow selecting a context menu item without having
    parent menu pointer in DumpRenderTree EventSender.
    Three unit tests added due ewk API changes.
    
    * ewk/ewk_contextmenu.cpp:
    (_Ewk_Context_Menu_Item):
    (ewk_context_menu_item_new):
    (ewk_context_menu_item_parent_get):
    (ewk_context_menu_item_append):
    * ewk/ewk_contextmenu.h:
    * ewk/ewk_view.cpp:
    (ewk_view_context_menu_get):
    * ewk/ewk_view.h:
    * tests/test_ewk_contextmenu.cpp: Added.
    (TEST_F):
    * tests/test_ewk_view.cpp:
    (TEST_F):
    
    Tools:
    
    Add contextClick for DumpRenderTreeEventSender basing on WebKit GTK
    implementation.
    
    * DumpRenderTree/efl/EventSender.cpp:
    (getMenuItemTitleCallback):
    (setMenuItemTitleCallback):
    (menuItemClickCallback):
    (contextClickCallback):
    
    LayoutTests:
    
    Move contextClick dependent tests to WebKit2 TestExpectations and
    add new expected results.
    
    * platform/efl-wk2/TestExpectations:
    * platform/efl/TestExpectations:
    * platform/efl/editing/selection/5354455-2-expected.txt: Added.
    * platform/efl/fast/events/context-no-deselect-expected.txt: Added.
    * platform/efl/fast/events/context-onmousedown-event-expected.png: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.h
@@ -115,6 +115,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ewk_view_h
 #define ewk_view_h
 
+#include "ewk_contextmenu.h"
 #include "ewk_file_chooser.h"
 #include "ewk_frame.h"
 #include "ewk_history.h"
@@ -2775,6 +2776,14 @@ EAPI Eina_Bool ewk_view_setting_enable_fullscreen_set(Evas_Object *o, Eina_Bool 
  *         @c EINA_FALSE if not or on failure
  */
 EAPI Eina_Bool ewk_view_setting_enable_fullscreen_get(const Evas_Object *o);
+
+/**
+ * Gets the context menu object.
+ * @param o The view that contains context menu.
+ *
+ * @return context menu structure on success or @c NULL on failure
+ */
+EAPI Ewk_Context_Menu *ewk_view_context_menu_get(const Evas_Object *o);
 
 #ifdef __cplusplus
 }
