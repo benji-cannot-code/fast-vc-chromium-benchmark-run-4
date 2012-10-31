dd69a53e20c8c6ba352e497724f221cd65e209df
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-31 00:03:27 +0000

    cssText for cursor property doesn't include hotspot https://bugs.webkit.org/show_bug.cgi?id=99530
    Patch by Rick Byers <rbyers@chromium.org> on 2012-10-30
    Reviewed by Darin Adler.
    
    Source/WebCore:
    
    Implement customCssText in CSSCursorImageValue to include the hotspot when present.
    Also explicitly track whether or not a hotspot was supplied in the parser,
    while still using the existing convention of (-1,-1) to denote no hotspot
    in the rest of the system.
    
    Test: fast/css/cursor-parsing.html
    
    * css/CSSCursorImageValue.cpp:
    (WebCore::CSSCursorImageValue::customCssText):
    * css/CSSCursorImageValue.h:
    (CSSCursorImageValue):
    
    LayoutTests:
    
    Add simple test for parsing of CSS cursor property.
    
    * fast/css/cursor-parsing-expected.txt: Added.
    * fast/css/cursor-parsing.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@132966 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSCursorImageValue.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSCursorImageValue.cpp
@@ -57,8 +57,9 @@ static inline SVGCursorElement* resourceReferencedByCursorElement(const String& 
 }
 #endif
 
-CSSCursorImageValue::CSSCursorImageValue(const String& url, const IntPoint& hotSpot)
+CSSCursorImageValue::CSSCursorImageValue(const String& url, bool hasHotSpot, const IntPoint& hotSpot)
     : CSSImageValue(CursorImageClass, url)
+    , m_hasHotSpot(hasHotSpot)
     , m_hotSpot(hotSpot)
 {
 }
@@ -81,6 +82,19 @@ CSSCursorImageValue::~CSSCursorImageValue()
 #endif
 }
 
+String CSSCursorImageValue::customCssText() const
+{
+    StringBuilder result;
+    result.append(CSSImageValue::customCssText());
+    if (m_hasHotSpot) {
+        result.append(' ');
+        result.appendNumber(m_hotSpot.x());
+        result.append(' ');
+        result.appendNumber(m_hotSpot.y());
+    }
+    return result.toString();
+}
+
 bool CSSCursorImageValue::updateIfSVGCursorIsUsed(Element* element)
 {
 #if !ENABLE(SVG)
@@ -95,6 +109,7 @@ bool CSSCursorImageValue::updateIfSVGCursorIsUsed(Element* element)
     if (SVGCursorElement* cursorElement = resourceReferencedByCursorElement(url(), element->document())) {
         // FIXME: This will override hot spot specified in CSS, which is probably incorrect.
         SVGLengthContext lengthContext(0);
+        m_hasHotSpot = true;
         float x = roundf(cursorElement->x().value(lengthContext));
         m_hotSpot.setX(static_cast<int>(x));
 
