4d2ea2d73f78e6e01f3873ec7a85e07e5778eb95
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 05:24:19 +0000

    [Mac] Suppress and debug "invalid drawable" messages.
    Per the bug, this message happens when -[NSOpenGLContext setView:] is
    called with a view that is either not in a window, or who's window
    does not have backing store.  This checks for those cases before
    continuing, and in those cases records breakpad keys which will
    hopefully point to a repro case.
    
    BUG=148882
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11275064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165110 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
+#include "base/mac/crash_logging.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
 #import "base/mac/scoped_nsautorelease_pool.h"
@@ -1007,6 +1008,29 @@ bool RenderWidgetHostViewMac::CompositorSwapBuffers(uint64 surface_handle,
                                                     const gfx::Size& size) {
   if (is_hidden_)
     return true;
+
+  // TODO(shess) If the view does not have a window, or the window
+  // does not have backing, the IOSurface will log "invalid drawable"
+  // in -setView:.  It is not clear how this code is reached with such
+  // a case, so record some info into breakpad (some subset of
+  // browsers are likely to crash later for unrelated reasons).
+  // http://crbug.com/148882
+  NSWindow* window = [cocoa_view_ window];
+  if ([window windowNumber] <= 0) {
+    NSString* const kCrashKey = @"rwhvm_window";
+    if (!window) {
+      base::mac::SetCrashKeyValue(kCrashKey, @"Missing window");
+    } else {
+      NSString* value =
+          [NSString stringWithFormat:@"window %s delegate %s controller %s",
+                    object_getClassName(window),
+                    object_getClassName([window delegate]),
+                    object_getClassName([window windowController])];
+      base::mac::SetCrashKeyValue(kCrashKey, value);
+    }
+
+    return true;
+  }
 
   if (!compositing_iosurface_.get())
     compositing_iosurface_.reset(CompositingIOSurfaceMac::Create());
