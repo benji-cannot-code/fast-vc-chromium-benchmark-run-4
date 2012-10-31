ec22ff46dca294cde6eb6d709598b714607ee58f
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-31 13:33:12 +0000

    X-Frame-Options console message should be associated with a request. https://bugs.webkit.org/show_bug.cgi?id=100735
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    In 99941, we added the possibility to tie console messages to requests,
    which enables automatic generation of stack traces, line numbers, etc.
    making the error simpler to diagnose for web developers. This patch
    uses the piping laid in that patch to improve the console message
    generated when a document is blocked by X-Frame-Options.
    
    No new tests; the functionality is covered by changes to existing tests.
    
    * dom/Document.cpp:
    (WebCore::Document::processHttpEquiv):
        Grab the request identifier from the currently active DocumentLoader
        and pass it into the console message.
    * loader/MainResourceLoader.cpp:
    (WebCore::MainResourceLoader::didReceiveResponse):
        Grab the request identifier from the MainResourceLoader, and pass it
        into the console message.
    
    LayoutTests:
    
    * http/tests/inspector/network/x-frame-options-deny-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-deny-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-parent-same-origin-deny-expected.txt:
    * http/tests/security/xssAuditor/meta-tag-http-refresh-x-frame-options-expected.txt:
        Update tests with new console message.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133019 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -109,6 +109,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InspectorInstrumentation.h"
 #include "Language.h"
 #include "Logging.h"
+#include "MainResourceLoader.h"
 #include "MediaCanStartListener.h"
 #include "MediaQueryList.h"
 #include "MediaQueryMatcher.h"
@@ -2951,11 +2952,14 @@ void Document::processHttpEquiv(const String& equiv, const String& content)
         if (frame) {
             FrameLoader* frameLoader = frame->loader();
             if (frameLoader->shouldInterruptLoadForXFrameOptions(content, url())) {
+                unsigned long requestIdentifier = 0;
+                if (frameLoader->activeDocumentLoader() && frameLoader->activeDocumentLoader()->mainResourceLoader())
+                    requestIdentifier = frameLoader->activeDocumentLoader()->mainResourceLoader()->identifier();
+                String message = "Refused to display '" + url().string() + "' in a frame because it set 'X-Frame-Options' to '" + content + "'.";
+
                 frameLoader->stopAllLoaders();
                 frame->navigationScheduler()->scheduleLocationChange(securityOrigin(), blankURL(), String());
-
-                DEFINE_STATIC_LOCAL(String, consoleMessage, (ASCIILiteral("Refused to display document because display forbidden by X-Frame-Options.\n")));
-                addConsoleMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, consoleMessage);
+                addConsoleMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, message, url().string(), 0, 0, requestIdentifier);
             }
         }
     } else if (equalIgnoringCase(equiv, "x-webkit-csp"))
