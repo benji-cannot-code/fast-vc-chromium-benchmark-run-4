3642fc517f4132803987cb04d042c48e95c0e469
davemoore@chromium.org <davemoore@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 22:16:18 +0000

    Create ash memory monitor
    BUG=None
    TEST=Enable via about:flags/enable-memory-monitor
    
    Review URL: https://codereview.chromium.org/11345017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165244 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray.cc
+++ ash/system/tray/system_tray.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/system/tray/system_tray.h"
 
+#include "ash/ash_switches.h"
 #include "ash/shell.h"
 #include "ash/shell/panel_window.h"
 #include "ash/shell_window_ids.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/drive/tray_drive.h"
 #include "ash/system/ime/tray_ime.h"
 #include "ash/system/locale/tray_locale.h"
+#include "ash/system/monitor/tray_monitor.h"
 #include "ash/system/power/power_status_observer.h"
 #include "ash/system/power/power_supply_status.h"
 #include "ash/system/power/tray_power.h"
@@ -31,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/user/login_status.h"
 #include "ash/system/user/tray_user.h"
 #include "ash/wm/shelf_layout_manager.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/timer.h"
 #include "base/utf_string_conversions.h"
@@ -197,6 +200,12 @@ void SystemTray::CreateItems() {
   AddTrayItem(tray_caps_lock);
   AddTrayItem(tray_settings);
   AddTrayItem(tray_date);
+
+  // Add memory monitor if enabled.
+  CommandLine* cmd = CommandLine::ForCurrentProcess();
+  if (cmd->HasSwitch(ash::switches::kAshEnableMemoryMonitor))
+    AddTrayItem(new internal::TrayMonitor);
+
   SetVisible(ash::Shell::GetInstance()->tray_delegate()->
       GetTrayVisibilityOnStartup());
 }
