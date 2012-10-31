5e27b9acffcf47fd5d95c8eacec58c3186e83827
noam.rosenthal@nokia.com <noam.rosenthal@nokia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-10-31 18:06:08 +0000

    [Texmap] Enable filter animations in GraphicsLayerAnimation https://bugs.webkit.org/show_bug.cgi?id=100318
    Reviewed by Kenneth Rohde Christiansen.
    
    Source/WebCore:
    
    Use the same method of animating filters in WebCore to animate filters for TextureMapper.
    Added the appropriate methods to GraphicsLayerAnimation and TextureMapperLayer.
    
    Tested by LayoutTests/css3/filters/filter-animation-hw.html and other tests.
    
    * platform/graphics/GraphicsLayerAnimation.cpp:
    (WebCore):
    (WebCore::blendFunc):
    (WebCore::applyFilterAnimation):
    (WebCore::GraphicsLayerAnimation::applyInternal):
    * platform/graphics/GraphicsLayerAnimation.h:
    (Client):
    * platform/graphics/texmap/TextureMapperLayer.cpp:
    (WebCore::TextureMapperLayer::intermediateSurfaceRect):
    (WebCore::TextureMapperLayer::shouldPaintToIntermediateSurface):
    (WebCore::TextureMapperLayer::paintRecursive):
    (WebCore::TextureMapperLayer::syncAnimations):
    * platform/graphics/texmap/TextureMapperLayer.h:
    (TextureMapperLayer):
    (WebCore::TextureMapperLayer::setFilters):
    (WebCore::TextureMapperLayer::setAnimatedFilters):
    
    Source/WebKit2:
    
    Encode/decode KeyframeValues of type FilterOperations.
    
    * Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.cpp:
    (CoreIPC::::encode):
    (CoreIPC::::decode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperLayer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperLayer.cpp
@@ -224,12 +224,12 @@ IntRect TextureMapperLayer::intermediateSurfaceRect(const TransformationMatrix& 
     }
 
 #if ENABLE(CSS_FILTERS)
-    if (m_state.filters.hasOutsets()) {
+    if (m_filters.hasOutsets()) {
         int leftOutset;
         int topOutset;
         int bottomOutset;
         int rightOutset;
-        m_state.filters.getOutsets(topOutset, rightOutset, bottomOutset, leftOutset);
+        m_filters.getOutsets(topOutset, rightOutset, bottomOutset, leftOutset);
         IntRect unfilteredTargetRect(rect);
         rect.move(std::max(0, -leftOutset), std::max(0, -topOutset));
         rect.expand(leftOutset + rightOutset, topOutset + bottomOutset);
@@ -263,7 +263,7 @@ TextureMapperLayer::ContentsLayerCount TextureMapperLayer::countPotentialLayersW
 bool TextureMapperLayer::shouldPaintToIntermediateSurface() const
 {
 #if ENABLE(CSS_FILTERS)
-    if (m_state.filters.size())
+    if (m_filters.size())
         return true;
 #endif
     bool hasOpacity = m_opacity < 0.99;
@@ -382,7 +382,7 @@ void TextureMapperLayer::paintRecursive(const TextureMapperPaintOptions& options
         maskTexture = 0;
 
 #if ENABLE(CSS_FILTERS)
-    surface = applyFilters(m_state.filters, options.textureMapper, surface.get(), surfaceRect);
+    surface = applyFilters(m_filters, options.textureMapper, surface.get(), surfaceRect);
 #endif
 
     options.textureMapper->bindSurface(options.surface.get());
@@ -525,6 +525,10 @@ void TextureMapperLayer::syncAnimations()
         setTransform(m_state.transform);
     if (!m_animations.hasActiveAnimationsOfType(AnimatedPropertyOpacity))
         setOpacity(m_state.opacity);
+#if ENABLE(CSS_FILTERS)
+    if (!m_animations.hasActiveAnimationsOfType(AnimatedPropertyWebkitFilter))
+        setFilters(m_state.filters);
+#endif
 }
 
 void TextureMapperLayer::flushCompositingState(GraphicsLayerTextureMapper* graphicsLayer, TextureMapper* textureMapper, int options)
