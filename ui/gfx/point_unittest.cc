e259b83c9960ab2e2c7e3e997361256427f83bfd
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 23:44:19 +0000

    ui: Add gfx::ToRoundedPoint() method.
    This acts like ToRoundedSize, it returns a Point from a PointF, where each
    component is rounded to the nearest integer.
    
    Tests:
    ui_unittests:PointTest.ToRoundedPoint
    
    BUG=147395
    R=sky
    
    Review URL: https://codereview.chromium.org/11369013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165261 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/point_unittest.cc
+++ ui/gfx/point_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/point.h"
+#include "ui/gfx/point_conversions.h"
 #include "ui/gfx/point_f.h"
 
 namespace ui {
@@ -79,6 +80,41 @@ TEST(PointTest, OffsetFromPoint) {
             b.OffsetFrom(a).ToString());
   EXPECT_EQ(gfx::Vector2d(-20 - 1, 8 - 5).ToString(),
             (b - a).ToString());
+}
+
+TEST(PointTest, ToRoundedPoint) {
+  EXPECT_EQ(gfx::Point(0, 0),
+            gfx::ToRoundedPoint(gfx::PointF(0, 0)));
+  EXPECT_EQ(gfx::Point(0, 0),
+            gfx::ToRoundedPoint(gfx::PointF(0.0001f, 0.0001f)));
+  EXPECT_EQ(gfx::Point(0, 0),
+            gfx::ToRoundedPoint(gfx::PointF(0.4999f, 0.4999f)));
+  EXPECT_EQ(gfx::Point(1, 1),
+            gfx::ToRoundedPoint(gfx::PointF(0.5f, 0.5f)));
+  EXPECT_EQ(gfx::Point(1, 1),
+            gfx::ToRoundedPoint(gfx::PointF(0.9999f, 0.9999f)));
+
+  EXPECT_EQ(gfx::Point(10, 10),
+            gfx::ToRoundedPoint(gfx::PointF(10, 10)));
+  EXPECT_EQ(gfx::Point(10, 10),
+            gfx::ToRoundedPoint(gfx::PointF(10.0001f, 10.0001f)));
+  EXPECT_EQ(gfx::Point(10, 10),
+            gfx::ToRoundedPoint(gfx::PointF(10.4999f, 10.4999f)));
+  EXPECT_EQ(gfx::Point(11, 11),
+            gfx::ToRoundedPoint(gfx::PointF(10.5f, 10.5f)));
+  EXPECT_EQ(gfx::Point(11, 11),
+            gfx::ToRoundedPoint(gfx::PointF(10.9999f, 10.9999f)));
+
+  EXPECT_EQ(gfx::Point(-10, -10),
+            gfx::ToRoundedPoint(gfx::PointF(-10, -10)));
+  EXPECT_EQ(gfx::Point(-10, -10),
+            gfx::ToRoundedPoint(gfx::PointF(-10.0001f, -10.0001f)));
+  EXPECT_EQ(gfx::Point(-10, -10),
+            gfx::ToRoundedPoint(gfx::PointF(-10.4999f, -10.4999f)));
+  EXPECT_EQ(gfx::Point(-11, -11),
+            gfx::ToRoundedPoint(gfx::PointF(-10.5f, -10.5f)));
+  EXPECT_EQ(gfx::Point(-11, -11),
+            gfx::ToRoundedPoint(gfx::PointF(-10.9999f, -10.9999f)));
 }
 
 }  // namespace ui
