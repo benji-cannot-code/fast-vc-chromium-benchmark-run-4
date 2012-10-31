b807a64d5f1041f53fed8a193c8acc895d1db112
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-10-31 20:48:09 +0000

    Animation changed to fade out on Launcher related bubbles.
    BUG=137285
    
    
    Review URL: https://chromiumcodereview.appspot.com/11341034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165221 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/overflow_bubble.cc
+++ ash/launcher/overflow_bubble.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/launcher/launcher_types.h"
 #include "ash/launcher/launcher_view.h"
+#include "ash/system/tray/system_tray.h"
 #include "ash/shell.h"
 #include "ui/gfx/insets.h"
 #include "ui/gfx/screen.h"
@@ -268,6 +269,8 @@ void OverflowBubble::Show(LauncherDelegate* delegate,
                                   overflow_start_index);
 
   bubble_ = bubble_view;
+  ash::Shell::GetInstance()->system_tray()->InitializeBubbleAnimations(
+      bubble_->GetWidget());
   bubble_->GetWidget()->AddObserver(this);
   bubble_->GetWidget()->Show();
 }
