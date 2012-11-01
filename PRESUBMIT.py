39459a49be89ff51114da473b16542bd15ba8254
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 20:46:29 +0000

    Add presumit check to stop any more usages of MessageLoop::RunAllPending() function
    While we work through the existing list.
    
    BUG=131220
    R=maruel@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11293045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165489 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -178,6 +178,14 @@ _BANNED_CPP_FUNCTIONS = (
       ),
       True,
     ),
+    (
+      'RunAllPending()',
+      (
+       'This function is deprecated and we\'re working on removing it. Rename',
+       'to RunUntilIdle',
+      ),
+      True,
+    ),
 )
 
 
