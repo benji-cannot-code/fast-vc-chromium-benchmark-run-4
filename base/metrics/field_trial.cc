03201514fae56e244b60a1c842e91efdb01dd065
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 18:59:47 +0000

    Notify FieldTrial observers only once group() is called for the first time.
    This fixes the problem where observers would not see the correct value
    of the trial when Disable() is called.
    
    Also cleans up some style issues and DCHECKs that would never get hit.
    
    BUG=156202, 158801
    TEST=New unit test FieldTrialTest.ObserveDisabled.
    
    Review URL: https://chromiumcodereview.appspot.com/11267044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165454 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -65,7 +65,8 @@ FieldTrial::FieldTrial(const std::string& name,
       next_group_number_(kDefaultGroupNumber + 1),
       group_(kNotFinalized),
       enable_field_trial_(true),
-      forced_(false) {
+      forced_(false),
+      group_reported_(false) {
   DCHECK_GT(total_probability, 0);
   DCHECK(!name_.empty());
   DCHECK(!default_group_name_.empty());
@@ -93,6 +94,7 @@ void FieldTrial::UseOneTimeRandomization() {
 }
 
 void FieldTrial::Disable() {
+  DCHECK(!group_reported_);
   enable_field_trial_ = false;
 
   // In case we are disabled after initialization, we need to switch
@@ -133,25 +135,22 @@ int FieldTrial::AppendGroup(const std::string& name,
   if (group_ == kNotFinalized && accumulated_group_probability_ > random_) {
     // This is the group that crossed the random line, so we do the assignment.
     SetGroupChoice(name, next_group_number_);
-    FieldTrialList::NotifyFieldTrialGroupSelection(name_, group_name_);
   }
   return next_group_number_++;
 }
 
 int FieldTrial::group() {
-  if (group_ == kNotFinalized) {
-    accumulated_group_probability_ = divisor_;
-    // Here it's OK to use kDefaultGroupNumber
-    // since we can't be forced and not finalized.
-    DCHECK(!forced_);
-    SetGroupChoice(default_group_name_, kDefaultGroupNumber);
+  FinalizeGroupChoice();
+  if (!group_reported_) {
     FieldTrialList::NotifyFieldTrialGroupSelection(name_, group_name_);
+    group_reported_ = true;
   }
   return group_;
 }
 
 std::string FieldTrial::group_name() {
-  group();  // call group() to make sure group assignment was done.
+  // Call |group()| to ensure group gets assigned and observers are notified.
+  group();
   DCHECK(!group_name_.empty());
   return group_name_;
 }
@@ -189,7 +188,7 @@ void FieldTrial::SetForced() {
   DCHECK_GT(next_group_number_, kDefaultGroupNumber + 1);
 
   // And we must finalize the group choice before we mark ourselves as forced.
-  group();
+  FinalizeGroupChoice();
   forced_ = true;
 }
 
@@ -202,6 +201,16 @@ void FieldTrial::SetGroupChoice(const std::string& name, int number) {
   else
     group_name_ = name;
   DVLOG(1) << "Field trial: " << name_ << " Group choice:" << group_name_;
+}
+
+void FieldTrial::FinalizeGroupChoice() {
+  if (group_ != kNotFinalized)
+    return;
+  accumulated_group_probability_ = divisor_;
+  // Here it's OK to use |kDefaultGroupNumber| since we can't be forced and not
+  // finalized.
+  DCHECK(!forced_);
+  SetGroupChoice(default_group_name_, kDefaultGroupNumber);
 }
 
 //------------------------------------------------------------------------------
@@ -401,7 +410,6 @@ FieldTrial* FieldTrialList::CreateFieldTrial(
 void FieldTrialList::AddObserver(Observer* observer) {
   if (!global_)
     return;
-  DCHECK(global_);
   global_->observer_list_->AddObserver(observer);
 }
 
@@ -409,7 +417,6 @@ void FieldTrialList::AddObserver(Observer* observer) {
 void FieldTrialList::RemoveObserver(Observer* observer) {
   if (!global_)
     return;
-  DCHECK(global_);
   global_->observer_list_->RemoveObserver(observer);
 }
 
@@ -419,7 +426,6 @@ void FieldTrialList::NotifyFieldTrialGroupSelection(
     const std::string& group_name) {
   if (!global_)
     return;
-  DCHECK(global_);
   global_->observer_list_->Notify(
       &FieldTrialList::Observer::OnFieldTrialGroupFinalized,
       name,
