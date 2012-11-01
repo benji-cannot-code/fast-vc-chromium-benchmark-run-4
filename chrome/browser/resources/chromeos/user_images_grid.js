f73cf240af4346385ecb0eb53a1ec93cf28ab2c5
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 18:55:04 +0000

    [cros] Camera stream stops when leaving camera view.
    BUG=157387
    
    
    Review URL: https://chromiumcodereview.appspot.com/11368027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165448 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/user_images_grid.js
+++ chrome/browser/resources/chromeos/user_images_grid.js
@@ -261,6 +261,8 @@ cr.define('options', function() {
       this.cameraOnline = false;
       if (this.cameraVideo_)
         this.cameraVideo_.src = '';
+      if (this.cameraStream_)
+        this.cameraStream_.stop();
       // Cancel any pending getUserMedia() checks.
       this.cameraCheckInProgress_ = false;
     },
@@ -274,8 +276,12 @@ cr.define('options', function() {
      */
     handleCameraAvailable_: function(onAvailable, stream) {
       this.cameraPresent = true;
-      if (this.cameraCheckInProgress_ && onAvailable())
+      if (this.cameraCheckInProgress_ && onAvailable()) {
         this.cameraVideo_.src = window.webkitURL.createObjectURL(stream);
+        this.cameraStream_ = stream;
+      } else {
+        stream.stop();
+      }
       this.cameraCheckInProgress_ = false;
     },
 
