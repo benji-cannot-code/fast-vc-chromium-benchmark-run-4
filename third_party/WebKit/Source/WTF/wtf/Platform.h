35aa894ba810d761f5b1da353b56dec39c281a54
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 22:26:55 +0000

    Fix HAVE(NETWORK_CFDATA_ARRAY_CALLBACK) build         https://bugs.webkit.org/show_bug.cgi?id=100979
            Reviewed by Joseph Pecoraro.
    
            * loader/ResourceBuffer.cpp: (WebCore::ResourceBuffer::append):
            * loader/ResourceBuffer.h:
            Added a version of append() that takes CFDataRef, passing it directly to SharedBuffer.
    
            * loader/mac/ResourceLoaderMac.mm: (WebCore::ResourceLoader::didReceiveDataArray):
            InspectorInstrumentation now takes a length. Unfortunately, we don't have encodedLength
            here, meaning that the results will likely be incorrect.
    
            * platform/network/cf/ResourceHandleCFNet.cpp:
            (WebCore::willCacheResponse): Removed a version that needlessly used deprecated API.
            (WebCore::ResourceHandle::createCFURLConnection): This code assumed that
            HAVE(NETWORK_CFDATA_ARRAY_CALLBACK) and USE(PROTECTION_SPACE_AUTH_CALLBACK) always
            come together. Changed to check for these separately.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -593,7 +593,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ENABLE_ORIENTATION_EVENTS 1
 #define ENABLE_REPAINT_THROTTLING 1
 #define ENABLE_WEB_ARCHIVE 1
-#define HAVE_NETWORK_CFDATA_ARRAY_CALLBACK 1
+#define HAVE_NETWORK_CFDATA_ARRAY_CALLBACK 1 // FIXME: All CFNetwork based platforms have this callback now, so it should be USE, not HAVE.
 #define HAVE_PTHREAD_RWLOCK 1
 #define HAVE_READLINE 1
 #define WTF_USE_CF 1
