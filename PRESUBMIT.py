79bb1df23bc31281f62863019de8a6bb570a29df
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 19:56:02 +0000

    Rename the few remaining instances of the context-free variant of FindTabbedBrowser. Update FindTabbedBrowser PRESUBMIT check.
    BUG=129187
    
    Review URL: https://codereview.chromium.org/11340045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165479 0039d316-1c4b-4281-b951-d872f2087c98

--- PRESUBMIT.py
+++ PRESUBMIT.py
@@ -147,7 +147,7 @@ _BANNED_CPP_FUNCTIONS = (
       (
        'This function is deprecated and we\'re working on removing it. Pass',
        'more context to get a Browser*, like a WebContents, window, or session',
-       'id. Talk to ben@ or jam@ for more information.',
+       'id. Talk to robertshield@ for more information.',
       ),
       True,
     ),
@@ -156,7 +156,7 @@ _BANNED_CPP_FUNCTIONS = (
       (
        'This function is deprecated and we\'re working on removing it. Pass',
        'more context to get a Browser*, like a WebContents, window, or session',
-       'id. Talk to ben@ or jam@ for more information.',
+       'id. Talk to robertshield@ for more information.',
       ),
       True,
     ),
@@ -165,16 +165,16 @@ _BANNED_CPP_FUNCTIONS = (
       (
        'This function is deprecated and we\'re working on removing it. Pass',
        'more context to get a Browser*, like a WebContents, window, or session',
-       'id. Talk to ben@ or jam@ for more information.',
+       'id. Talk to robertshield@ for more information.',
       ),
       True,
     ),
     (
-      'browser::FindTabbedBrowser',
+      'browser::FindTabbedBrowserDeprecated',
       (
        'This function is deprecated and we\'re working on removing it. Pass',
        'more context to get a Browser*, like a WebContents, window, or session',
-       'id. Talk to ben@ or jam@ for more information.',
+       'id. Talk to robertshield@ for more information.',
       ),
       True,
     ),
