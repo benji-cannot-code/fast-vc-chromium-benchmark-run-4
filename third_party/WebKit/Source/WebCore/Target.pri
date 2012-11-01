311d1bd85717db02a07d8c0acb20ec810301df54
kihong.kwon@samsung.com <kihong.kwon@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 10:38:04 +0000

    Add DeviceController base-class to remove duplication of DeviceXXXControler https://bugs.webkit.org/show_bug.cgi?id=96894
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    Add DeviceController which is extracted from DeviceOrientationController to remove duplication.
    And soon-to-be-added DeviceMotionController and ProximityController.
    
    Covered by existing tests.
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * dom/DeviceOrientationClient.h:
    * dom/DeviceOrientationController.cpp:
    Remove member functions to move to DeviceController.
    - addListener(), removeListener(), removeAllListeners(), isActive()
    (WebCore::DeviceOrientationController::DeviceOrientationController):
    (WebCore::DeviceOrientationController::didChangeDeviceOrientation):
    (WebCore::DeviceOrientationController::client):
    (WebCore::DeviceOrientationController::hasLastData):
    (WebCore::DeviceOrientationController::getLastEvent):
    (WebCore::DeviceOrientationController::from):
    (WebCore):
    * dom/DeviceOrientationController.h:
    (WebCore):
    (WebCore::DeviceOrientationController::~DeviceOrientationController):
    (DeviceOrientationController):
    * dom/Document.cpp:
    Remove suspendEventsForAllListeners() and resumeEventsForAllListeners() function calls.
    These calls can be made by checking activeDOMObjectsAreSuspended() and activeDOMObjectsAreStopped() before dispatchEvent.
    (WebCore::Document::suspendActiveDOMObjects):
    (WebCore::Document::resumeActiveDOMObjects):
    * loader/EmptyClients.h:
    (EmptyDeviceClient):
    (WebCore::EmptyDeviceClient::startUpdating):
    (WebCore::EmptyDeviceClient::stopUpdating):
    (WebCore):
    * page/DOMWindow.cpp:
    (WebCore::DOMWindow::addEventListener):
    (WebCore::DOMWindow::removeEventListener):
    (WebCore::DOMWindow::removeAllEventListeners):
    * page/DeviceClient.h: Added.
    (WebCore):
    (DeviceClient):
    (WebCore::DeviceClient::~DeviceClient):
    * page/DeviceController.cpp: Added.
    DeviceController has extracted functions from DeviceOrientationController and DeviceMotionController.
    - addDeviceEventListener(), removeDeviceEventlistener(), removeAllDeviceEventListeners(), dispatchDeviceEvent()
    All kind of device event controller which has DeviceClient can be inherited from DeviceController.
    (WebCore):
    (WebCore::DeviceController::DeviceController):
    (WebCore::DeviceController::addDeviceEventListener):
    (WebCore::DeviceController::removeDeviceEventListener):
    (WebCore::DeviceController::removeAllDeviceEventListeners):
    (WebCore::DeviceController::dispatchDeviceEvent):
    (WebCore::DeviceController::fireDeviceEvent):
    * page/DeviceController.h: Added.
    (WebCore):
    (DeviceController):
    (WebCore::DeviceController::~DeviceController):
    (WebCore::DeviceController::isActive):
    (WebCore::DeviceController::client):
    (WebCore::DeviceController::hasLastData):
    (WebCore::DeviceController::getLastEvent):
    
    Source/WebKit/qt:
    
    Change client() to deviceOrientationClient() to get DeviceOrientationClient*.
    
    * WebCoreSupport/DumpRenderTreeSupportQt.cpp:
    (DumpRenderTreeSupportQt::setMockDeviceOrientation):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133143 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Target.pri
+++ third_party/WebKit/Source/WebCore/Target.pri
@@ -873,6 +873,7 @@ SOURCES += \
     page/ContentSecurityPolicy.cpp \
     page/ContextMenuController.cpp \
     page/Crypto.cpp \
+    page/DeviceController.cpp \
     page/DiagnosticLoggingKeys.cpp \
     page/DOMSelection.cpp \
     page/DOMTimer.cpp \
@@ -2000,6 +2001,8 @@ HEADERS += \
     page/ContextMenuController.h \
     page/ContextMenuProvider.h \
     page/Coordinates.h \
+    page/DeviceClient.h \
+    page/DeviceController.h \
     page/DiagnosticLoggingKeys.h \
     page/DOMSelection.h \
     page/DOMTimer.h \
--- third_party/WebKit/Source/WebCore/page/DOMWindow.cpp
+++ third_party/WebKit/Source/WebCore/page/DOMWindow.cpp
@@ -1602,7 +1602,7 @@ bool DOMWindow::addEventListener(const AtomicString& eventType, PassRefPtr<Event
             controller->addListener(this);
     } else if (eventType == eventNames().deviceorientationEvent && RuntimeEnabledFeatures::deviceOrientationEnabled()) {
         if (DeviceOrientationController* controller = DeviceOrientationController::from(page()))
-            controller->addListener(this);
+            controller->addDeviceEventListener(this);
     }
 #endif
 
@@ -1631,7 +1631,7 @@ bool DOMWindow::removeEventListener(const AtomicString& eventType, EventListener
             controller->removeListener(this);
     } else if (eventType == eventNames().deviceorientationEvent) {
         if (DeviceOrientationController* controller = DeviceOrientationController::from(page()))
-            controller->removeListener(this);
+            controller->removeDeviceEventListener(this);
     }
 #endif
 
@@ -1688,7 +1688,7 @@ void DOMWindow::removeAllEventListeners()
     if (DeviceMotionController* controller = DeviceMotionController::from(page()))
         controller->removeAllListeners(this);
     if (DeviceOrientationController* controller = DeviceOrientationController::from(page()))
-        controller->removeAllListeners(this);
+        controller->removeAllDeviceEventListeners(this);
 #endif
 
     removeAllUnloadEventListeners(this);
