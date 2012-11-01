ab13d2acab26e37dadb418f3e0f11515a946f79f
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 14:14:06 +0000

    [EFL] Correct our use of the coordinated graphics https://bugs.webkit.org/show_bug.cgi?id=100947
    Reviewed by Alexis Menard.
    
    Replace the display(...) call with a regular update() on the view.
    In the coordinated graphics case this updates our view using
    paintToCurrentGLContext with our viewmodel matrix and clips it to
    the viewport. This avoid the need to manually joining update regions.
    
    Unneeded update calls have also been removed and the resizing
    logic has been improved.
    
    * UIProcess/API/efl/EwkViewImpl.cpp:
    (EwkViewImpl::displayTimerFired):
    (EwkViewImpl::update):
    * UIProcess/API/efl/EwkViewImpl.h:
    (WebCore):
    (EwkViewImpl):
    (EwkViewImpl::clearEvasGLSurface):
    * UIProcess/API/efl/ewk_view.cpp:
    (_ewk_view_smart_calculate):
    * UIProcess/efl/PageClientImpl.cpp:
    (WebKit::PageClientImpl::setViewNeedsDisplay):
    * UIProcess/efl/PageViewportControllerClientEfl.cpp:
    (WebKit::PageViewportControllerClientEfl::setVisibleContentsRect):
    (WebKit::PageViewportControllerClientEfl::didChangeContentsSize):
    (WebKit::PageViewportControllerClientEfl::didChangeVisibleContents):
    * UIProcess/efl/PageViewportControllerClientEfl.h:
    (PageViewportControllerClientEfl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKEinaSharedString.h"
 #include "WKRetainPtr.h"
 #include <Evas.h>
+#include <WebCore/IntRect.h>
 #include <WebCore/TextDirection.h>
 #include <WebCore/Timer.h>
 #include <WebKit2/WKBase.h>
@@ -64,9 +65,7 @@ class WebPopupMenuProxyEfl;
 namespace WebCore {
 class Color;
 class Cursor;
-class IntRect;
 class IntSize;
-class Region;
 }
 
 class Ewk_Back_Forward_List;
@@ -127,8 +126,9 @@ public:
 #endif
 
     void setCursor(const WebCore::Cursor& cursor);
-    void redrawRegion(const WebCore::IntRect& rect);
     void setImageData(void* imageData, const WebCore::IntSize& size);
+
+    void update(const WebCore::IntRect& rect = WebCore::IntRect());
 
     static void addToPageViewMap(EwkViewImpl* viewImpl);
     static void removeFromPageViewMap(EwkViewImpl* viewImpl);
@@ -182,7 +182,7 @@ public:
     Evas_GL* evasGL() { return m_evasGL.get(); }
     Evas_GL_Context* evasGLContext() { return m_evasGLContext ? m_evasGLContext->context() : 0; }
     Evas_GL_Surface* evasGLSurface() { return m_evasGLSurface ? m_evasGLSurface->surface() : 0; }
-    void resetEvasGLSurface() { m_evasGLSurface.clear(); }
+    void clearEvasGLSurface() { m_evasGLSurface.clear(); }
 #endif
 
     // FIXME: needs refactoring (split callback invoke)
@@ -238,7 +238,6 @@ private:
     bool m_touchEventsEnabled;
 #endif
     WebCore::Timer<EwkViewImpl> m_displayTimer;
-    OwnPtr<WebCore::Region> m_dirtyRegion;
     OwnPtr<Ewk_Popup_Menu> m_popupMenu;
     OwnPtr<WebKit::InputMethodContextEfl> m_inputMethodContext;
     OwnPtr<Ewk_Color_Picker> m_colorPicker;
