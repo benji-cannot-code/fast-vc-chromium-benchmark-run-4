e4cd8a8a7df2e6e5979cdd55cb05fe70d27df03b
staikos@webkit.org <staikos@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 00:59:13 +0000

    [BlackBerry] Crash on html popup form control when element is deleted https://bugs.webkit.org/show_bug.cgi?id=100869
    Reviewed by Yong Li.
    
    Make the element pointers RefPtr so they don't go away before the popup
    is closed.
    
    * WebCoreSupport/ColorPickerClient.h:
    (ColorPickerClient):
    * WebCoreSupport/DatePickerClient.h:
    (DatePickerClient):
    * WebCoreSupport/SelectPopupClient.h:
    (SelectPopupClient):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/ColorPickerClient.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/ColorPickerClient.h
@@ -54,7 +54,7 @@ private:
 
     String m_source;
     BlackBerry::WebKit::WebPagePrivate* m_webPage;
-    HTMLInputElement* m_element;
+    RefPtr<HTMLInputElement> m_element;
 };
 } // namespace WebCore
 #endif
