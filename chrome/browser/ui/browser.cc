4d534ecd1e1cacbb3809f16079a4ed19e66817fd
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 21:03:52 +0000

    Cleanup: Remove a browser GRD entry that does not require translation. Review URL: https://codereview.chromium.org/11362021
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include <windows.h>
 #include <shellapi.h>
-#endif  // OS_WIN
+#endif  // defined(OS_WIN)
 
 #include <algorithm>
 #include <string>
@@ -570,11 +570,10 @@ string16 Browser::GetWindowTitleForCurrentTab() const {
   // the application name.
   return title;
 #else
-  int string_id = IDS_BROWSER_WINDOW_TITLE_FORMAT;
   // Don't append the app name to window titles on app frames and app popups
-  if (is_app())
-    string_id = IDS_BROWSER_WINDOW_TITLE_FORMAT_NO_LOGO;
-  return l10n_util::GetStringFUTF16(string_id, title);
+  return is_app() ?
+      title :
+      l10n_util::GetStringFUTF16(IDS_BROWSER_WINDOW_TITLE_FORMAT, title);
 #endif
 }
 
@@ -1256,7 +1255,7 @@ void Browser::OnWindowDidShow() {
           base::Time::Now() - *process_creation_time);
     }
   }
-#endif // OS_MACOSX || OS_WIN
+#endif  // defined(OS_MACOSX) || defined(OS_WIN)
 
   // Nothing to do for non-tabbed windows.
   if (!is_type_tabbed())
