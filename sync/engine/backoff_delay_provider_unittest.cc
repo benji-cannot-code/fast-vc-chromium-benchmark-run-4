d817f8934c6ae9b8cfcf0129bb6b83c1ef7337e2
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 08:16:42 +0000

    sync: add kInitialBackoffImmediateRetry
    The primary impact this has is speeding up some integration tests by
    ~ 50%. In particular it lets us re-enable MigrationHell, which is
    currently disabled as it takes too long for sharding_supervisor. 
    
    Rationale is discussed in the bug, I believe this is safe for the class
    of errors we target with the immediate delay, because we don't need backoff.
    
    (Note that when used with GetDelay, this still results in a 1s minimum
    backoff.)
    
    kInitialBackoffShortRetry is still used if the command line flag is passed
    (and in integration tests) to change the default initial backoff from 5 minutes
    to 1s (as it used to be).
    
    BUG=143641
    
    Review URL: https://chromiumcodereview.appspot.com/11183073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165312 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/backoff_delay_provider_unittest.cc
+++ sync/engine/backoff_delay_provider_unittest.cc
@@ -20,7 +20,7 @@ class BackoffDelayProviderTest : public testing::Test {};
 
 TEST_F(BackoffDelayProviderTest, GetRecommendedDelay) {
   scoped_ptr<BackoffDelayProvider> delay(BackoffDelayProvider::FromDefaults());
-  EXPECT_LE(TimeDelta::FromSeconds(0),
+  EXPECT_EQ(TimeDelta::FromSeconds(1),
             delay->GetDelay(TimeDelta::FromSeconds(0)));
   EXPECT_LE(TimeDelta::FromSeconds(1),
             delay->GetDelay(TimeDelta::FromSeconds(1)));
@@ -43,11 +43,11 @@ TEST_F(BackoffDelayProviderTest, GetInitialDelay) {
 
   state.last_get_key_result = UNSET;
   state.last_download_updates_result = SERVER_RETURN_MIGRATION_DONE;
-  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+  EXPECT_EQ(kInitialBackoffImmediateRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 
   state.last_download_updates_result = NETWORK_CONNECTION_UNAVAILABLE;
-  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+  EXPECT_EQ(kInitialBackoffImmediateRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 
   state.last_download_updates_result = SERVER_RETURN_TRANSIENT_ERROR;
@@ -67,11 +67,11 @@ TEST_F(BackoffDelayProviderTest, GetInitialDelay) {
             delay->GetInitialDelay(state).InSeconds());
 
   state.commit_result = SERVER_RETURN_MIGRATION_DONE;
-  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+  EXPECT_EQ(kInitialBackoffImmediateRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 
   state.commit_result = NETWORK_CONNECTION_UNAVAILABLE;
-  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+  EXPECT_EQ(kInitialBackoffImmediateRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 }
 
@@ -85,7 +85,7 @@ TEST_F(BackoffDelayProviderTest, GetInitialDelayWithOverride) {
 
   state.last_get_key_result = UNSET;
   state.last_download_updates_result = SERVER_RETURN_MIGRATION_DONE;
-  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+  EXPECT_EQ(kInitialBackoffImmediateRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 
   state.last_download_updates_result = SERVER_RETURN_TRANSIENT_ERROR;
@@ -105,7 +105,7 @@ TEST_F(BackoffDelayProviderTest, GetInitialDelayWithOverride) {
             delay->GetInitialDelay(state).InSeconds());
 
   state.commit_result = SERVER_RETURN_MIGRATION_DONE;
-  EXPECT_EQ(kInitialBackoffShortRetrySeconds,
+  EXPECT_EQ(kInitialBackoffImmediateRetrySeconds,
             delay->GetInitialDelay(state).InSeconds());
 }
 
