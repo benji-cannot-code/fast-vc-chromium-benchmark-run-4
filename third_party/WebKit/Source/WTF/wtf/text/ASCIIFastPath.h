59afbdcb54f56ecc2942521e82a96a524715b302
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 00:31:14 +0000

    Add an optimized version of copyLCharsFromUCharSource for ARM https://bugs.webkit.org/show_bug.cgi?id=94886
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2012-10-31
    Reviewed by Gavin Barraclough.
    
    Michael Saboff added a SIMD version of copyLCharsFromUCharSource() in r125846.
    
    This patch a similar optimization for ARMv7 by using the interleaved load/store available
    in the NEON extension.
    
    The performance gains:
    -10000 characters: ~3.5 times faster.
    -20 characters (2 vectors): ~55% faster.
    -15 characters (1 vector): ~21% faster.
    -3 characters (no vector, pure overhead): ~10% slower.
    
    * wtf/text/ASCIIFastPath.h:
    (WTF::copyLCharsFromUCharSource):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133100 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/text/ASCIIFastPath.h
+++ third_party/WebKit/Source/WTF/wtf/text/ASCIIFastPath.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2011 Apple Inc. All rights reserved.
+ * Copyright (C) 2011, 2012 Apple Inc. All rights reserved.
  * Copyright (C) 2011 Nokia Corporation and/or its subsidiary(-ies).
  *
  * This library is free software; you can redistribute it and/or
@@ -132,6 +132,31 @@ inline void copyLCharsFromUCharSource(LChar* destination, const UChar* source, s
         ASSERT(!(source[i] & 0xff00));
         destination[i] = static_cast<LChar>(source[i]);
     }
+#elif COMPILER(GCC) && CPU(ARM_NEON) && !(PLATFORM(BIG_ENDIAN) || PLATFORM(MIDDLE_ENDIAN)) && defined(NDEBUG)
+    const LChar* const end = destination + length;
+    const uintptr_t memoryAccessSize = 8;
+
+    if (length >= (2 * memoryAccessSize) - 1) {
+        // Prefix: align dst on 64 bits.
+        const uintptr_t memoryAccessMask = memoryAccessSize - 1;
+        do {
+            *destination++ = static_cast<LChar>(*source++);
+        } while (!isAlignedTo<memoryAccessMask>(destination));
+
+        // Vector interleaved unpack, we only store the lower 8 bits.
+        const uintptr_t lengthLeft = end - destination;
+        const LChar* const simdEnd = end - (lengthLeft % memoryAccessSize);
+        do {
+            asm("vld2.8   { d0-d1 }, [%[SOURCE]] !\n\t"
+                "vst1.8   { d0 }, [%[DESTINATION],:64] !\n\t"
+                : [SOURCE]"+r" (source), [DESTINATION]"+r" (destination)
+                :
+                : "memory", "d0", "d1");
+        } while (destination != simdEnd);
+    }
+
+    while (destination != end)
+        *destination++ = static_cast<LChar>(*source++);
 #else
     for (size_t i = 0; i < length; ++i) {
         ASSERT(!(source[i] & 0xff00));
