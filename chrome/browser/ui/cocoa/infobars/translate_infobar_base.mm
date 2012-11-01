2a71fc80318f93a504b0ad019e17049fc90cfaf8
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 17:06:30 +0000

    Implement missing Translate UMAs for Linux and Windows
    Move UMA handling code from Cocoa specific implementation
    to TranslateInfoBarDelegate to work on Linux and Windows.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/11360005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165399 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/infobars/translate_infobar_base.mm
+++ chrome/browser/ui/cocoa/infobars/translate_infobar_base.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/infobars/translate_infobar_base.h"
 
 #include "base/logging.h"
-#include "base/metrics/histogram.h"
 #include "base/sys_string_conversions.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/translate/translate_infobar_delegate.h"
@@ -396,7 +395,6 @@ InfoBar* TranslateInfoBarDelegate::CreateInfoBar(InfoBarService* owner) {
   DCHECK(state == TranslateInfoBarDelegate::BEFORE_TRANSLATE ||
          state == TranslateInfoBarDelegate::TRANSLATION_ERROR);
   delegate->Translate();
-  UMA_HISTOGRAM_COUNTS("Translate.Translate", 1);
 }
 
 // Called when someone clicks on the "Nope" button.
@@ -406,7 +404,6 @@ InfoBar* TranslateInfoBarDelegate::CreateInfoBar(InfoBarService* owner) {
   TranslateInfoBarDelegate* delegate = [self delegate];
   DCHECK(delegate->type() == TranslateInfoBarDelegate::BEFORE_TRANSLATE);
   delegate->TranslationDeclined();
-  UMA_HISTOGRAM_COUNTS("Translate.DeclineTranslate", 1);
   [super removeSelf];
 }
 
