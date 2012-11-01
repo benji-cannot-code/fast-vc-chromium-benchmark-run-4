2bdfc84e3a053f900418c2834d906505e218cc8a
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 10:40:58 +0000

    It should be CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_RENDERER_H_ instead of CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_CAPTURER_H_
    TBR=xians@chromium.org
    
    Review URL: https://codereview.chromium.org/11312037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165332 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_renderer.h
+++ content/renderer/media/webrtc_audio_renderer.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_CAPTURER_H_
-#define CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_CAPTURER_H_
+#ifndef CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_RENDERER_H_
+#define CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_RENDERER_H_
 
 #include "base/memory/ref_counted.h"
 #include "base/synchronization/lock.h"
@@ -75,4 +75,4 @@ class CONTENT_EXPORT WebRtcAudioRenderer
 
 }  // namespace content
 
-#endif  // CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_CAPTURER_H_
+#endif  // CONTENT_RENDERER_MEDIA_WEBRTC_AUDIO_RENDERER_H_
