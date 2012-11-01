c44a3e8620586f678d648aabcfa5ae4d4cffb98a
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 17:55:04 +0000

    Use correct thread for audio capturing and encoding.
    Previously video capture thread was passed to AudioScheduler instead of the dedicated audio thread. Fixed ChromotingHost so that the correct thread is given to AudioScheduler.
    
    BUG=157992
    
    Review URL: https://codereview.chromium.org/11260053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165426 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/chromoting_host.h
+++ remoting/host/chromoting_host.h
@@ -73,6 +73,7 @@ class ChromotingHost : public base::RefCountedThreadSafe<ChromotingHost>,
       SignalStrategy* signal_strategy,
       DesktopEnvironmentFactory* desktop_environment_factory,
       scoped_ptr<protocol::SessionManager> session_manager,
+      scoped_refptr<base::SingleThreadTaskRunner> audio_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> capture_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> encode_task_runner,
       scoped_refptr<base::SingleThreadTaskRunner> network_task_runner);
@@ -184,6 +185,7 @@ class ChromotingHost : public base::RefCountedThreadSafe<ChromotingHost>,
   // Parameters specified when the host was created.
   DesktopEnvironmentFactory* desktop_environment_factory_;
   scoped_ptr<protocol::SessionManager> session_manager_;
+  scoped_refptr<base::SingleThreadTaskRunner> audio_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> capture_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> encode_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> network_task_runner_;
--- remoting/host/client_session.h
+++ remoting/host/client_session.h
@@ -90,6 +90,7 @@ class ClientSession
   // |event_handler| must outlive |this|. |desktop_environment_factory| is only
   // used by the constructor to create an instance of DesktopEnvironment.
   ClientSession(EventHandler* event_handler,
+                scoped_refptr<base::SingleThreadTaskRunner> audio_task_runner,
                 scoped_refptr<base::SingleThreadTaskRunner> capture_task_runner,
                 scoped_refptr<base::SingleThreadTaskRunner> encode_task_runner,
                 scoped_refptr<base::SingleThreadTaskRunner> network_task_runner,
@@ -217,6 +218,7 @@ class ClientSession
   // is reached.
   base::OneShotTimer<ClientSession> max_duration_timer_;
 
+  scoped_refptr<base::SingleThreadTaskRunner> audio_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> capture_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> encode_task_runner_;
   scoped_refptr<base::SingleThreadTaskRunner> network_task_runner_;
