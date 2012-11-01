9491dde8b7691f06c195abed845976e98ac0b6f2
kbr@google.com <kbr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 18:53:40 +0000

    Add RGB to supported destination formats of ImageBuffer::copyToPlatformTexture https://bugs.webkit.org/show_bug.cgi?id=100971
    Reviewed by Stephen White.
    
    The ImageBuffer::copyToPlatformTexture restriction added in
    r132965 was too restrictive. Allow RGB textures to prevent
    performance regressions.
    
    * html/canvas/WebGLRenderingContext.cpp:
    (WebCore):
    (WebCore::WebGLRenderingContext::texImage2D):
    * platform/graphics/ImageBuffer.h:
    (ImageBuffer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133202 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ImageBuffer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ImageBuffer.h
@@ -121,7 +121,7 @@ namespace WebCore {
 #endif
 
         // FIXME: current implementations of this method have the restriction that they only work
-        // with textures that are RGBA format and UNSIGNED_BYTE type.
+        // with textures that are RGB or RGBA format, and UNSIGNED_BYTE type.
         bool copyToPlatformTexture(GraphicsContext3D&, Platform3DObject, GC3Denum, bool, bool);
 
         void reportMemoryUsage(MemoryObjectInfo*) const;
