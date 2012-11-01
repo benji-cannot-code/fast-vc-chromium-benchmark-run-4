57a93b2e5ac2fe5145df84a13be5d1d8ddc9099e
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 19:27:42 +0000

    Adding histogram for a LBS request latency. See bug.
    BUG=158787
    
    Review URL: https://chromiumcodereview.appspot.com/11364009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165471 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/network_location_request.cc
+++ content/browser/geolocation/network_location_request.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/metrics/histogram.h"
 #include "base/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
@@ -101,6 +102,7 @@ bool NetworkLocationRequest::MakeRequest(const string16& access_token,
       net::LOAD_DO_NOT_SAVE_COOKIES | net::LOAD_DO_NOT_SEND_COOKIES |
       net::LOAD_DO_NOT_SEND_AUTH_DATA);
 
+  start_time_ = base::TimeTicks::Now();
   url_fetcher_->Start();
   return true;
 }
@@ -126,6 +128,17 @@ void NetworkLocationRequest::OnURLFetchComplete(
   const bool server_error =
       !status.is_success() || (response_code >= 500 && response_code < 600);
   url_fetcher_.reset();
+
+  if (!server_error) {
+    const base::TimeDelta request_time = base::TimeTicks::Now() - start_time_;
+
+    UMA_HISTOGRAM_CUSTOM_TIMES(
+        "Net.Wifi.LbsLatency",
+        request_time,
+        base::TimeDelta::FromMilliseconds(1),
+        base::TimeDelta::FromSeconds(10),
+        100);
+  }
 
   DCHECK(listener_);
   DVLOG(1) << "NetworkLocationRequest::Run() : Calling listener with position.";
