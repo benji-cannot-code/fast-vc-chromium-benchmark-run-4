e4d39dec3c6bc55bfbad61f0dd4447eecb0cdc51
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 22:23:12 +0000

    Revert 165505 - Introduce RemoveChromeTaskbarShortcuts() to delete all pinned-to-taskbar shortcuts owned by the uninstalled Chrome.
    TBR=brettw@chromium.org
    BUG=158632
    TEST=No user-level shortcut Chrome left behind in taskbar post user-level self-destruct.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11361015
    
    TBR=gab@chromium.org
    Review URL: https://codereview.chromium.org/11366047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165524 0039d316-1c4b-4281-b951-d872f2087c98

--- base/base_paths_win.cc
+++ base/base_paths_win.cc
@@ -193,12 +193,6 @@ bool PathProviderWin(int key, FilePath* result) {
       if (!GetQuickLaunchPath(true, &cur))
         return false;
       break;
-    case base::DIR_TASKBAR_PINS:
-      if (!PathService::Get(base::DIR_USER_QUICK_LAUNCH, &cur))
-        return false;
-      cur = cur.AppendASCII("User Pinned");
-      cur = cur.AppendASCII("TaskBar");
-      break;
     default:
       return false;
   }
