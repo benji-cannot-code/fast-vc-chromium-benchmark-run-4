03a827b7f035946e90cadcd56bad988f80c3cbe7
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 22:38:17 +0000

    Remove a bit of unneeded code
    BUG=none
    TBR=thakis@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11301049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165531 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/utility/chrome_content_utility_client.cc
+++ chrome/utility/chrome_content_utility_client.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/message_loop_proxy.h"
 #include "base/threading/thread.h"
-#include "chrome/browser/importer/external_process_importer_bridge.h"
 #include "chrome/browser/importer/importer.h"
 #include "chrome/browser/importer/profile_import_process_messages.h"
 #include "chrome/common/child_process_logging.h"
--- chrome/utility/chrome_content_utility_client.h
+++ chrome/utility/chrome_content_utility_client.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/utility/content_utility_client.h"
 #include "printing/pdf_render_settings.h"
 
-class ExternalProcessImporterBridge;
 class FilePath;
 class Importer;
 
