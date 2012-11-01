fe58edeb282674e33248212fe4431b9cc21985fc
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 15:33:29 +0000

    Unreviewed. Build fix.
    * bindings/v8/V8GCController.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133178 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8GCController.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8GCController.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8CSSStyleDeclaration.h"
 #include "V8DOMImplementation.h"
 #include "V8MessagePort.h"
+#include "V8Node.h"
 #include "V8RecursionScope.h"
 #include "V8StyleSheet.h"
 #include "V8StyleSheetList.h"
