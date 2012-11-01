95b76ba88dc94ef01d59ae023803523c230cf5f2
yusukes@chromium.org <yusukes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 21:26:36 +0000

    Allow Caps Lock to be remapped [part 2 of 2].
    * Add a new Chrome command line flag: --has-chromeos-keyboard
    * Show the new UI for remapping Caps Lock if --has-chromeos-keyboard is not specified.
    
    This CL depends on http://codereview.chromium.org/11314020/ and https://gerrit.chromium.org/gerrit/#/c/36884/.
    
    BUG=146204
    TEST=manually confirmed that 1) the new UI is shown when /etc/session_manager_use_flags.txt does not have "chromeos_keyboard", is not shown when the file has the flag, 2) the UI for guest mode is the same as non-guest mode, 3) the UI correctly remaps the Caps Lock key.
    
    Review URL: https://chromiumcodereview.appspot.com/11346028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -773,6 +773,7 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ::switches::kDisableThreadedCompositing,
       ::switches::kForceCompositingMode,
       ::switches::kGpuStartupDialog,
+      ::switches::kHasChromeOSKeyboard,
       ::switches::kLoginProfile,
       ::switches::kScrollPixels,
       ::switches::kNaturalScrollDefault,
