2826a296e60c15a895b78598049111c5b53625f1
nfullagar@chromium.org <nfullagar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 23:54:24 +0000

    Add more tests to ppapi/tests/test_image_data.cc, to facilitate removal of corresponding NaCl PPAPI test. Add IsImageDataFormatSupported() static method to PPAPI cpp interface. Add comments to indicate which formats are always supported.
    TEST=test_image_data
    BUG=http://code.google.com/p/chromium/issues/detail?id=154400
    
    Review URL: https://chromiumcodereview.appspot.com/11348022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165557 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_image_data.h
+++ ppapi/tests/test_image_data.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PPAPI_TESTS_TEST_IMAGE_DATA_H_
 #define PPAPI_TESTS_TEST_IMAGE_DATA_H_
 
+#include <string>
+
 #include "ppapi/c/ppb_image_data.h"
 #include "ppapi/tests/test_case.h"
 
@@ -19,10 +21,19 @@ class TestImageData : public TestCase {
 
  private:
   std::string TestInvalidFormat();
+  std::string TestGetNativeFormat();
+  std::string TestFormatSupported();
   std::string TestInvalidSize();
   std::string TestHugeSize();
   std::string TestInitToZero();
   std::string TestIsImageData();
+
+  // Subtests used by tests above; pass in a pixel format.
+  std::string SubTestFormatSupported(PP_ImageDataFormat format);
+  std::string SubTestInvalidSize(PP_ImageDataFormat format);
+  std::string SubTestHugeSize(PP_ImageDataFormat format);
+  std::string SubTestInitToZero(PP_ImageDataFormat format);
+  std::string SubTestIsImageData(PP_ImageDataFormat format);
 
   // Used by the tests that access the C API directly.
   const PPB_ImageData* image_data_interface_;
