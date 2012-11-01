1049fb4dda8c1b1f05637defd785034d72022678
aroben@webkit.org <aroben@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-01 15:40:31 +0000

    Crash beneath WKRelease after failed load in MiniBrowser https://bugs.webkit.org/show_bug.cgi?id=100954
    Reviewed by Sam Weinig.
    
    There were just some simple retain/release bugs.
    
    * MiniBrowser/mac/WK2BrowserWindowController.m:
    (-[WK2BrowserWindowController updateTextFieldFromURL:]):
    (-[WK2BrowserWindowController updateProvisionalURLForFrame:]):
    Don't try to release null URLs.
    
    (-[WK2BrowserWindowController updateCommittedURLForFrame:]): Don't
    release a URL for which we don't have ownership.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133179 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/mac/WK2BrowserWindowController.m
+++ third_party/WebKit/Tools/MiniBrowser/mac/WK2BrowserWindowController.m
@@ -734,10 +734,8 @@ static void runOpenPanel(WKPageRef page, WKFrameRef frame, WKOpenPanelParameters
     if (!emptyURL)
         emptyURL = WKURLCreateWithUTF8CString("");
 
-    if (WKURLIsEqual(URLRef, emptyURL)) {
-        WKRelease(URLRef);
+    if (WKURLIsEqual(URLRef, emptyURL))
         return;
-    }
 
     CFURLRef cfSourceURL = WKURLCopyCFURL(0, URLRef);
     [urlText setStringValue:(NSString*)CFURLGetString(cfSourceURL)];
@@ -747,6 +745,8 @@ static void runOpenPanel(WKPageRef page, WKFrameRef frame, WKOpenPanelParameters
 - (void)updateProvisionalURLForFrame:(WKFrameRef)frame
 {
     WKURLRef url = WKFrameCopyProvisionalURL(frame);
+    if (!url)
+        return;
     [self updateTextFieldFromURL:url];
     WKRelease(url);
 }
@@ -754,6 +754,8 @@ static void runOpenPanel(WKPageRef page, WKFrameRef frame, WKOpenPanelParameters
 - (void)updateCommittedURLForFrame:(WKFrameRef)frame
 {
     WKURLRef url = WKFrameCopyURL(frame);
+    if (!url)
+        return;
     [self updateTextFieldFromURL:url];
     WKRelease(url);
 }
