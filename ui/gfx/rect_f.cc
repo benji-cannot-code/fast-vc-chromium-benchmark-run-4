4e322dbc4e27a80aebfd3a7746c71f81fbd72276
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-01 15:13:55 +0000

    Include <algorithm> for std::min/max, not <cmath>.
    No new tests, fixing includes.
    
    BUG=147395
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11359018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165382 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/rect_f.cc
+++ ui/gfx/rect_f.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/rect_f.h"
 
-#include <cmath>
+#include <algorithm>
 
 #include "base/logging.h"
 #include "base/stringprintf.h"
