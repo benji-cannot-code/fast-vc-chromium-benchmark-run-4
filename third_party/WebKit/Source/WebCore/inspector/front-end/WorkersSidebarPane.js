56274f3532c62b9941815664715a44a7c446911a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-02 07:54:27 +0000

    Web Inspector: a small defect in "WorkersSidebarPanel.js" https://bugs.webkit.org/show_bug.cgi?id=101022
    Patch by Peter Wang <peter.wang@torchmobile.com.cn> on 2012-11-02
    Reviewed by Yury Semikhatsky.
    
    In "autoattachToWorkersClicked", the "event.target.checked" doesn't alwyas valid, since the
    "event.target" might be a label.
    
    The problem just causes an error report, doesn't effect function, so no new test case.
    
    * inspector/front-end/WorkersSidebarPane.js:
    (WebInspector.WorkersSidebarPane.prototype._autoattachToWorkersClicked):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133271 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/WorkersSidebarPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/WorkersSidebarPane.js
@@ -118,7 +118,7 @@ WebInspector.WorkersSidebarPane.prototype = {
 
     _autoattachToWorkersClicked: function(event)
     {
-        WorkerAgent.setAutoconnectToWorkers(event.target.checked);
+        WorkerAgent.setAutoconnectToWorkers(this._enableWorkersCheckbox.checked);
     },
 
     __proto__: WebInspector.SidebarPane.prototype
