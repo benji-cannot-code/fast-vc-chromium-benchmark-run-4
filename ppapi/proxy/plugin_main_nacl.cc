3c7de9a9e4f860d3f34333c150b4cb64005725d0
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 02:58:20 +0000

    Don't assert if PluginInstance can't get PPP_Combined. Some tests exercise this path. BUG=116317 TEST=nacl_integration
    Review URL: https://chromiumcodereview.appspot.com/11367043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165608 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/plugin_main_nacl.cc
+++ ppapi/proxy/plugin_main_nacl.cc
@@ -234,7 +234,6 @@ int IrtInit() {
 }
 
 int PpapiPluginMain() {
-  base::AtExitManager exit_manager;
   MessageLoop loop;
   IPC::Logging::set_log_function_map(&g_log_function_mapping);
   ppapi::proxy::PluginGlobals plugin_globals;
