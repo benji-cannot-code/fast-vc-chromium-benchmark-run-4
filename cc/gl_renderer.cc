9b586551618e2e005da1fe3b4f2b5af80fb947b1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 15:09:06 +0000

    cc: Get rid of NotImplemented stub file.
    BUG=147395
    R=enne@chromium.org,jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11361053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165679 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/gl_renderer.cc
+++ cc/gl_renderer.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/gl_renderer.h"
 
 #include "FloatQuad.h"
-#include "NotImplemented.h"
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/string_split.h"
