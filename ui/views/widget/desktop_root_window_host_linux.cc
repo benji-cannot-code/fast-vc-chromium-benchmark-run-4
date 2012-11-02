2a730f8163bfa2eb1cf20c6d0d23baa9af5e1251
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 01:57:12 +0000

    Fix a typo. This change replaces 'DesktopRootWindowLinux' with 'DesktopRootWindowHost' to fix the build break on the "Linux Aura" bot.
    TBR=sky,ben
    BUG=none
    TEST=fix build on the "Linux Aura" bot.
    Review URL: https://codereview.chromium.org/11362055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165588 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_root_window_host_linux.cc
+++ ui/views/widget/desktop_root_window_host_linux.cc
@@ -90,7 +90,7 @@ DesktopRootWindowHostLinux::~DesktopRootWindowHostLinux() {
 }
 
 // static
-ui::NativeTheme* DesktopRootWindowLinux::GetNativeTheme(aura::Window* window) {
+ui::NativeTheme* DesktopRootWindowHost::GetNativeTheme(aura::Window* window) {
   return NativeTheme::instance();
 }
 
