bbf3dd9dae436d5efbc125f7782000401aaa9b2e
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 02:40:48 +0000

    <webview> no longer behind flag and instead marked as dev.
    BUG=159004
    
    
    Review URL: https://chromiumcodereview.appspot.com/11369032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -925,7 +925,6 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kEnablePasswordGeneration,
       switches::kEnablePnacl,
       switches::kEnableWatchdog,
-      switches::kEnableWebView,
       switches::kMemoryProfiling,
       switches::kMessageLoopHistogrammer,
       switches::kNoJsRandomness,
