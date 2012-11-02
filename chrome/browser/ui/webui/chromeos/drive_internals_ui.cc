2bf9ee394f546c76fbf0641c77cb3589c88c09c1
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 05:03:59 +0000

    drive: Split update of "Account Metadata" into remote and local parts.
    Otherwise, we cannot see the local metadata information until
    we get online and connected to the remote server.
    
    BUG=158010
    TEST=Open chrome:drive-internals and verify local changestamp is shown
    earlier than remote timestamp.
    
    Review URL: https://chromiumcodereview.appspot.com/11275095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165620 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
+++ chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
@@ -193,6 +193,8 @@ class DriveInternalsWebUIHandler : public content::WebUIMessageHandler {
       google_apis::DriveServiceInterface* drive_service);
   void UpdateAccountMetadataSection(
       google_apis::DriveServiceInterface* drive_service);
+  void UpdateLocalMetadataSection(
+      google_apis::DriveServiceInterface* drive_service);
   void UpdateDeltaUpdateStatusSection();
   void UpdateInFlightOperationsSection(
       google_apis::DriveServiceInterface* drive_service);
@@ -285,13 +287,6 @@ void DriveInternalsWebUIHandler::OnGetAccountMetadata(
     account_metadata.Set("installed-apps", installed_apps);
   }
 
-  // Add the local largest chargestamp.
-  const drive::DriveFileSystemMetadata metadata =
-      GetSystemService()->file_system()->GetMetadata();
-  account_metadata.SetDouble("account-largest-changestamp-local",
-                             metadata.largest_changestamp);
-  account_metadata.SetString("account-metadata-origin", metadata.origin);
-
   web_ui()->CallJavascriptFunction("updateAccountMetadata", account_metadata);
 }
 
@@ -329,6 +324,7 @@ void DriveInternalsWebUIHandler::OnPageLoaded(const base::ListValue* args) {
   UpdateDriveRelatedPreferencesSection();
   UpdateAuthStatusSection(drive_service);
   UpdateAccountMetadataSection(drive_service);
+  UpdateLocalMetadataSection(drive_service);
   UpdateDeltaUpdateStatusSection();
   UpdateInFlightOperationsSection(drive_service);
   UpdateGCacheContentsSection();
@@ -406,6 +402,19 @@ void DriveInternalsWebUIHandler::UpdateAccountMetadataSection(
                  weak_ptr_factory_.GetWeakPtr()));
 }
 
+void DriveInternalsWebUIHandler::UpdateLocalMetadataSection(
+    google_apis::DriveServiceInterface* drive_service) {
+  DCHECK(drive_service);
+
+  base::DictionaryValue local_metadata;
+  const drive::DriveFileSystemMetadata metadata =
+      GetSystemService()->file_system()->GetMetadata();
+  local_metadata.SetDouble("account-largest-changestamp-local",
+                           metadata.largest_changestamp);
+  local_metadata.SetString("account-metadata-origin", metadata.origin);
+  web_ui()->CallJavascriptFunction("updateLocalMetadata", local_metadata);
+}
+
 void DriveInternalsWebUIHandler::UpdateDeltaUpdateStatusSection() {
   const drive::DriveFileSystemMetadata metadata =
       GetSystemService()->file_system()->GetMetadata();
@@ -425,7 +434,6 @@ void DriveInternalsWebUIHandler::UpdateDeltaUpdateStatusSection() {
 
   web_ui()->CallJavascriptFunction("updateDeltaUpdateStatus",
                                    delta_update_status);
-
 }
 
 void DriveInternalsWebUIHandler::UpdateInFlightOperationsSection(
