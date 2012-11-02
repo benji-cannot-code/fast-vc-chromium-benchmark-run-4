bab8ee42aedea23ddfbf120f0a23af2c6f0b8883
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-02 06:35:27 +0000

    [Shadow] Element should have getter and setter of attribute 'pseudo' https://bugs.webkit.org/show_bug.cgi?id=100831
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    We expose 'pseudo' attribute in Element. When nothing is assigned to 'pseudo', it should return null.
    
    Test: fast/dom/shadow/pseudo-attribute.html
    
    * dom/Element.cpp:
    (WebCore::Element::pseudo):
    (WebCore):
    (WebCore::Element::setPseudo):
    * dom/Element.h:
    (Element):
    * dom/Element.idl:
    * html/HTMLAttributeNames.in:
    
    LayoutTests:
    
    * fast/dom/shadow/pseudo-attribute-expected.txt: Added.
    * fast/dom/shadow/pseudo-attribute.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -1775,6 +1775,16 @@ String Element::title() const
     return String();
 }
 
+const AtomicString& Element::pseudo() const
+{
+    return getAttribute(pseudoAttr);
+}
+
+void Element::setPseudo(const AtomicString& value)
+{
+    setAttribute(pseudoAttr, value);
+}
+
 LayoutSize Element::minimumSizeForResizing() const
 {
     return hasRareData() ? elementRareData()->m_minimumSizeForResizing : defaultMinimumSizeForResizing();
