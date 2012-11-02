60ce415bdf9460b3894cd26204c4d5943cf92a8f
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 22:43:00 +0000

    Adding histogram for the number of Wifi adapters in Windows.
    BUG=158787
    
    Review URL: https://chromiumcodereview.appspot.com/11358056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165783 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/wifi_data_provider_win.cc
+++ content/browser/geolocation/wifi_data_provider_win.cc
@@ -104,6 +104,9 @@ class WindowsWlanApi : public WifiDataProviderCommon::WlanApiInterface {
                            const GUID& interface_id,
                            WifiData::AccessPointDataSet* data);
 
+  // Logs number of detected wlan interfaces.
+  static void LogWlanInterfaceCount(int count);
+
   // Handle to the wlanapi.dll library.
   HINSTANCE library_;
 
@@ -236,6 +239,15 @@ void WindowsWlanApi::GetWLANFunctions(HINSTANCE wlan_library) {
          WlanCloseHandle_function_);
 }
 
+void WindowsWlanApi::LogWlanInterfaceCount(int count) {
+  UMA_HISTOGRAM_CUSTOM_COUNTS(
+      "Net.Wifi.InterfaceCount",
+      count,
+      1,
+      5,
+      5);
+}
+
 bool WindowsWlanApi::GetAccessPointData(
     WifiData::AccessPointDataSet* data) {
   DCHECK(data);
@@ -251,6 +263,7 @@ bool WindowsWlanApi::GetAccessPointData(
                                   NULL,
                                   &negotiated_version,
                                   &wlan_handle) != ERROR_SUCCESS) {
+    LogWlanInterfaceCount(0);
     return false;
   }
   DCHECK(wlan_handle);
@@ -259,9 +272,12 @@ bool WindowsWlanApi::GetAccessPointData(
   WLAN_INTERFACE_INFO_LIST* interface_list = NULL;
   if ((*WlanEnumInterfaces_function_)(wlan_handle, NULL, &interface_list) !=
       ERROR_SUCCESS) {
+    LogWlanInterfaceCount(0);
     return false;
   }
   DCHECK(interface_list);
+
+  LogWlanInterfaceCount(interface_list->dwNumberOfItems);
 
   // Go through the list of interfaces and get the data for each.
   for (int i = 0; i < static_cast<int>(interface_list->dwNumberOfItems); ++i) {
