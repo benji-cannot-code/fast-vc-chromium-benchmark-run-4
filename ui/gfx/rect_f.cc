1aad42d001d34a9026e7a875d285fe1d402a5912
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-02 19:12:36 +0000

    ui: Add IsExpressibleAsRect() method to RectF
    This method reports if the RectF can be safely converted to an
    integer Rect. When it is false, some dimension of the RectF is
    outside the bounds of what an integer can represent, and
    converting it to a Rect will require clamping.
    
    Tests:
    ui_unittests:RectTest.IsExpressibleAsRect
    
    R=sky
    BUG=147395
    
    Review URL: https://codereview.chromium.org/11364054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165725 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/rect_f.cc
+++ ui/gfx/rect_f.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "ui/gfx/insets_f.h"
 #include "ui/gfx/rect_base_impl.h"
+#include "ui/gfx/safe_integer_conversions.h"
 
 namespace gfx {
 
@@ -39,6 +40,12 @@ RectF::RectF(const gfx::PointF& origin, const gfx::SizeF& size)
 }
 
 RectF::~RectF() {}
+
+bool RectF::IsExpressibleAsRect() const {
+  return IsExpressibleAsInt(x()) && IsExpressibleAsInt(y()) &&
+      IsExpressibleAsInt(width()) && IsExpressibleAsInt(height()) &&
+      IsExpressibleAsInt(right()) && IsExpressibleAsInt(bottom());
+}
 
 std::string RectF::ToString() const {
   return base::StringPrintf("%s %s",
