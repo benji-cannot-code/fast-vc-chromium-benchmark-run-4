7b28007c5e848c24e3aa4ca264b9afc7711e4527
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-02 09:16:16 +0000

    Web Inspector: Middle click closes an editor and pastes selection into current editor on Linux https://bugs.webkit.org/show_bug.cgi?id=100743
    Reviewed by Alexander Pavlov.
    
    Editor now consumes all middle mouse button mouse ups except for the ones in the main panel.
    TabbedPane now consumes all middle mouse button mouse ups as well.
    Navigator view does not pass focus anymore if element was selected with middle mouse button.
    
    * inspector/front-end/DefaultTextEditor.js:
    (.preventDefaultOnMouseUp):
    (.consumeMouseUp):
    * inspector/front-end/NavigatorView.js:
    (WebInspector.NavigatorSourceTreeElement.prototype.ondblclick):
    * inspector/front-end/TabbedPane.js:
    (WebInspector.TabbedPane):
    (WebInspector.TabbedPane.prototype.onMouseUp):
    (WebInspector.TabbedPane.prototype.onMouseClick):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TabbedPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TabbedPane.js
@@ -46,6 +46,7 @@ WebInspector.TabbedPane = function()
     this._tabsHistory = [];
     this._tabsById = {};
     this.element.addEventListener("click", this.focus.bind(this), false);
+    this.element.addEventListener("mouseup", this.onMouseUp.bind(this), false);
 
     this._dropDownButton = this._createDropDownButton();
 }
@@ -91,6 +92,16 @@ WebInspector.TabbedPane.prototype = {
     defaultFocusedElement: function()
     {
         return this.visibleView ? this.visibleView.defaultFocusedElement() : null;
+    },
+
+    /**
+     * @param {Event} event
+     */
+    onMouseUp: function(event)
+    {
+        // This is needed to prevent middle-click pasting on linux when tabs are clicked.
+        if (event.button === 1)
+            event.consume(true);
     },
 
     /**
