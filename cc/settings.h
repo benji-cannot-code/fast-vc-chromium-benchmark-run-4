8a1a81f1ae17ea2edc003ae7e18dd6d4590fbbfd
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-03 00:08:31 +0000

    cc: Remove other entries from DEPS list.
    They were also fixed in past revisions, now that cc component landed and the
    TillingData landed to, we can remove them.
    
    BUG=147395,154052
    TEST=checkdeps.py succeeds.
    R=enne@chromium.org,jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/11360069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165805 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/settings.h
+++ cc/settings.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CCSettings_h
 #define CCSettings_h
 
-#include "webkit/glue/webkit_glue_export.h"
 #include "cc/cc_export.h"
 
 namespace cc {
