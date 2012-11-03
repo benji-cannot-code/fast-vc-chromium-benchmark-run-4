f71281e05f918978bae47c1c6cd217424b11481f
kbalazs@webkit.org <kbalazs@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-03 21:42:48 +0000

    Unreviewed, rolling out r133397. http://trac.webkit.org/changeset/133397 https://bugs.webkit.org/show_bug.cgi?id=101155
    "Broke Qt. No way to force the damn bots to do a clean build.
    I'm giving up." (Requested by kbalazs on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-11-03
    
    Source/WebCore:
    
    * Target.pri:
    * WebCore.pri:
    * platform/qt/QtTestSupport.h: Removed.
    
    Tools:
    
    * DumpRenderTree/qt/DumpRenderTree.pro:
    * DumpRenderTree/qt/DumpRenderTreeMain.cpp:
    (main):
    * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
    (WebCore::DumpRenderTree::open):
    * DumpRenderTree/qt/QtInitializeTestFonts.cpp: Renamed from Source/WebCore/platform/qt/QtTestSupport.cpp.
    (WebKit):
    (WebKit::initializeTestFonts):
    * DumpRenderTree/qt/QtInitializeTestFonts.h: Copied from Tools/WebKitTestRunner/InjectedBundle/qt/ActivateFontsQt.cpp.
    (WebKit):
    * QtTestBrowser/QtTestBrowser.pro:
    * QtTestBrowser/qttestbrowser.cpp:
    (LauncherApplication::handleUserOptions):
    * WebKitTestRunner/InjectedBundle/Target.pri:
    * WebKitTestRunner/InjectedBundle/qt/ActivateFontsQt.cpp:
    (WTR::activateFonts):
    * WebKitTestRunner/InjectedBundle/qt/QtInitializeTestFonts.cpp: Copied from Tools/WebKitTestRunner/InjectedBundle/qt/ActivateFontsQt.cpp.
    * WebKitTestRunner/InjectedBundle/qt/QtInitializeTestFonts.h: Copied from Tools/WebKitTestRunner/InjectedBundle/qt/ActivateFontsQt.cpp.
    * WebKitTestRunner/InjectedBundle/qt/TestRunnerQt.cpp:
    (WTR::TestRunner::platformInitialize):
    * WebKitTestRunner/Target.pri:
    * WebKitTestRunner/qt/main.cpp:
    (main):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/qt/QtInitializeTestFonts.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/qt/QtInitializeTestFonts.cpp
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
- * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
+ * Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -29,17 +28,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "ActivateFonts.h"
-#include "DumpRenderTreeSupportQt.h"
-#include "QtInitializeTestFonts.h"
-#include <QCoreApplication>
+#include "../../../DumpRenderTree/qt/QtInitializeTestFonts.cpp"
 
-namespace WTR {
-
-void activateFonts()
-{
-    WebKit::initializeTestFonts();
-    QCoreApplication::setAttribute(Qt::AA_Use96Dpi, true);
-}
-
-}
--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/qt/QtInitializeTestFonts.h
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/qt/QtInitializeTestFonts.h
@@ -1,7 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2005, 2006 Apple Computer, Inc.  All rights reserved.
- * Copyright (C) 2008 Nokia Corporation and/or its subsidiary(-ies)
+ * Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies)
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -28,18 +27,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include "config.h"
-#include "ActivateFonts.h"
-#include "DumpRenderTreeSupportQt.h"
-#include "QtInitializeTestFonts.h"
-#include <QCoreApplication>
+#include "../../../DumpRenderTree/qt/QtInitializeTestFonts.h"
 
-namespace WTR {
-
-void activateFonts()
-{
-    WebKit::initializeTestFonts();
-    QCoreApplication::setAttribute(Qt::AA_Use96Dpi, true);
-}
-
-}
