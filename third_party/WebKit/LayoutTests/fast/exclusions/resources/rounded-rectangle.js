39d5675c93dc9efd1ecb4aa62fb21476dd196210
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-03 01:21:53 +0000

    [CSS Exclusions] Clean up shape-inside tests' subpixel snapping code https://bugs.webkit.org/show_bug.cgi?id=100704
    Patch by Bear Travis <betravis@adobe.com> on 2012-11-02
    Reviewed by Dirk Schulze.
    
    Factor out the pixel snapping behavior from polygon tests to be shared with
    the rounded rectangle tests. In sub-pixel layout, the rounding behavior may be
    slightly different. This adds a new file, subpixel-utils.js, which tests for the
    feature availability and provides functions for rounding at line left & line right.
    I've also taken the opportunity to factor the window.internal.settings code into
    rounded-rectangle.js and simple-polygon.js.
    
    * fast/exclusions/resources/rounded-rectangle.js:
    (simulateShape): Use the new subpixel rounding code.
    * fast/exclusions/resources/simple-polygon.js:
    (polygonXIntercepts): Ditto.
    * fast/exclusions/resources/subpixel-utils.js: Added.
    (SubPixelLayout): Adding the SubPixelLayout object which has helper methods
    for determining how line edges should round, and whether subpixel layout is enabled.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-002-expected.html:
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-002.html:
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-003-expected.html:
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-003.html:
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-004-expected.html:
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-004.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-001-expected.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-001.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-002-expected.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-002.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-003-expected.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-003.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-004-expected.html:
    * fast/exclusions/shape-inside/shape-inside-simple-polygon-004.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/exclusions/resources/rounded-rectangle.js
+++ third_party/WebKit/LayoutTests/fast/exclusions/resources/rounded-rectangle.js
@@ -1,4 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (window.internals)
+    window.internals.settings.setCSSExclusionsEnabled(true);
+
 function xFromEllipseCenter(yFromEllipseCenter, radiusX, radiusY) {
     return radiusX * Math.sqrt(1 - Math.pow(yFromEllipseCenter / radiusY, 2));
 }
@@ -53,12 +56,12 @@ function simulateShape(elementId, stylesheet, dimensions, lineHeight) {
 
         var paddingLeft = document.createElement("div");
         paddingLeft.setAttribute("class", "float left");
-        paddingLeft.style.width = dimensions.shapeX + inset + "px";
+        paddingLeft.style.width = SubPixelLayout.roundLineLeft(dimensions.shapeX + inset) + "px";
         element.appendChild(paddingLeft);
 
         var paddingRight = document.createElement("div");
         paddingRight.setAttribute("class", "float right");
-        paddingRight.style.width = Math.floor((dimensions.width - dimensions.shapeWidth - dimensions.shapeX) + inset) + "px";
+        paddingRight.style.width = SubPixelLayout.roundLineRight((dimensions.width - dimensions.shapeWidth - dimensions.shapeX) + inset) + "px";
         element.appendChild(paddingRight);
     }
 }
