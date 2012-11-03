b932f5db128bc9af5264766798173c91c16b8052
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-03 02:42:51 +0000

    Handle FFmpeg initialization in a lazy instance.
    Sadly function level static initialization is not thread safe on
    MSVC (though it is on clang/gcc), so we need to switch back to
    using a lazy instance to manage ffmpeg initialization :(
    
    See "magic statics":
    http://msdn.microsoft.com/en-us/library/vstudio/hh567368.aspx
    
    BUG=159139
    TEST=tests pass.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11368064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165835 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_glue.cc
+++ media/filters/ffmpeg_glue.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/filters/ffmpeg_glue.h"
 
+#include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/synchronization/lock.h"
 #include "media/ffmpeg/ffmpeg_common.h"
@@ -84,23 +85,42 @@ static int LockManagerOperation(void** lock, enum AVLockOp op) {
   return 1;
 }
 
-static bool InitializeFFmpegInternal() {
-  // Before doing anything disable logging as it interferes with layout tests.
-  av_log_set_level(AV_LOG_QUIET);
+// FFmpeg must only be initialized once, so use a LazyInstance to ensure this.
+class FFmpegInitializer {
+ public:
+  bool initialized() { return initialized_; }
 
-  // Register our protocol glue code with FFmpeg.
-  if (av_lockmgr_register(&LockManagerOperation) != 0)
-    return false;
+ private:
+  friend struct base::DefaultLazyInstanceTraits<FFmpegInitializer>;
 
-  // Now register the rest of FFmpeg.
-  av_register_all();
-  return true;
-}
+  FFmpegInitializer()
+      : initialized_(false) {
+    // Before doing anything disable logging as it interferes with layout tests.
+    av_log_set_level(AV_LOG_QUIET);
+
+    // Register our protocol glue code with FFmpeg.
+    if (av_lockmgr_register(&LockManagerOperation) != 0)
+      return;
+
+    // Now register the rest of FFmpeg.
+    av_register_all();
+
+    initialized_ = true;
+  }
+
+  ~FFmpegInitializer() {
+    NOTREACHED() << "FFmpegInitializer should be leaky!";
+  }
+
+  bool initialized_;
+
+  DISALLOW_COPY_AND_ASSIGN(FFmpegInitializer);
+};
 
 void FFmpegGlue::InitializeFFmpeg() {
-  // FFmpeg only needs to be initialized once.
-  static const bool kStatus = InitializeFFmpegInternal();
-  CHECK(kStatus);
+  static base::LazyInstance<FFmpegInitializer>::Leaky li =
+      LAZY_INSTANCE_INITIALIZER;
+  CHECK(li.Get().initialized());
 }
 
 FFmpegGlue::FFmpegGlue(FFmpegURLProtocol* protocol)
