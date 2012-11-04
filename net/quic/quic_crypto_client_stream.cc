84868719ac5bc9a8900c77015ce9ca0183de041e
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-04 05:14:40 +0000

    Adding QuicClientSession and friends
    Review URL: https://chromiumcodereview.appspot.com/11363021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165878 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/quic/quic_crypto_client_stream.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/quic/quic_crypto_client_stream.h"
+
+#include "net/quic/quic_session.h"
+
+namespace net {
+
+QuicCryptoClientStream::QuicCryptoClientStream(QuicSession* session)
+    : QuicCryptoStream(session) {
+}
+
+
+void QuicCryptoClientStream::OnHandshakeMessage(
+    const CryptoHandshakeMessage& message) {
+  // Do not process handshake messages after the handshake is complete.
+  if (handshake_complete()) {
+    CloseConnection(QUIC_CRYPTO_MESSAGE_AFTER_HANDSHAKE_COMPLETE);
+    return;
+  }
+
+  if (message.tag != kSHLO) {
+    CloseConnection(QUIC_INVALID_CRYPTO_MESSAGE_TYPE);
+    return;
+  }
+
+  // TODO(rch): correctly validate the message
+  set_handshake_complete(true);
+  return;
+}
+
+}  // namespace net
