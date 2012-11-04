eb67599b997654a513ee37aeda6c6aa4e11c50b5
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-04 23:16:13 +0000

    BaseChooserOnlyDateAndTimeInputType should have BaseClickableWithKeyInputType behavior https://bugs.webkit.org/show_bug.cgi?id=101039
    Reviewed by Hajime Morita.
    
    Add BaseClickableWithKeyInputType behavior to
    BaseChooserOnlyDateAndTimeInputType. It means date/time input types
    without ENABLE_INPUT_MULTIPLE_FIELDS_UI receive DOMActivate events by
    pressing space or enter key. They're going to open a date/time chooser
    by DOMActive event.
    
    Because BaseChooserOnlyDateAndTimeInputType inherits from
    BaseDateAndTimeInputType, it can't inherit
    BaseClickableWithKeyInputType. So, this patch adds static helper
    functions to BaseClickableWithKeyInputType, and
    BaseChooserOnlyDateAndTimeInputType uses them.
    
    This patch doesn't change behavior yet because Chromimum-Android port
    intercepts user events and doesn't deliver them to date/time input
    elements.
    
    * html/BaseChooserOnlyDateAndTimeInputType.cpp:
    (WebCore::BaseChooserOnlyDateAndTimeInputType::handleDOMActivateEvent):
    Add an empty implementation with a FIXME comment.
    (WebCore::BaseChooserOnlyDateAndTimeInputType::handleKeydownEvent):
    Add BaseClickableWithKeyInputType behavior by a helper function.
    (WebCore::BaseChooserOnlyDateAndTimeInputType::handleKeypressEvent): Ditto.
    (WebCore::BaseChooserOnlyDateAndTimeInputType::handleKeyupEvent): Ditto.
    (WebCore::BaseChooserOnlyDateAndTimeInputType::accessKeyAction): Ditto.
    * html/BaseChooserOnlyDateAndTimeInputType.h:
    (BaseChooserOnlyDateAndTimeInputType): Add function declarations.
    * html/BaseClickableWithKeyInputType.cpp:
    Add static helper functions, and existing member functions use them.
    * html/BaseClickableWithKeyInputType.h:
    (BaseClickableWithKeyInputType): Add declarations of the helper functions.
    * html/InputType.h:
    (InputType): Make dispatchSimulatedClickIfActive public because it is
    called from a helper function.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133427 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/BaseChooserOnlyDateAndTimeInputType.h
+++ third_party/WebKit/Source/WebCore/html/BaseChooserOnlyDateAndTimeInputType.h
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BaseChooserOnlyDateAndTimeInputType_h
 
 #if ENABLE(DATE_AND_TIME_INPUT_TYPES) && !ENABLE(INPUT_MULTIPLE_FIELDS_UI)
+#include "BaseClickableWithKeyInputType.h"
 #include "BaseDateAndTimeInputType.h"
 
 namespace WebCore {
@@ -38,7 +39,13 @@ protected:
     BaseChooserOnlyDateAndTimeInputType(HTMLInputElement* element) : BaseDateAndTimeInputType(element) { }
     virtual ~BaseChooserOnlyDateAndTimeInputType();
 
-    // FIXME: Composite BaseClickableWithKeyInputType behavior. webkit.org/b/101039.
+private:
+    // InputType functions:
+    virtual void handleDOMActivateEvent(Event*) OVERRIDE;
+    virtual void handleKeydownEvent(KeyboardEvent*) OVERRIDE;
+    virtual void handleKeypressEvent(KeyboardEvent*) OVERRIDE;
+    virtual void handleKeyupEvent(KeyboardEvent*) OVERRIDE;
+    virtual void accessKeyAction(bool sendMouseEvents) OVERRIDE;
 };
 
 }
