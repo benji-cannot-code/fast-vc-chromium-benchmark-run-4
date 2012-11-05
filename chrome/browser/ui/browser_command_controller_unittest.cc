bdd7e548f20147f691ee77e78ee12e4bb21ff469
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 18:20:48 +0000

    win-aura: Fix build from r165951.
    TBR=ben@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/11366092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165960 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_command_controller_unittest.cc
+++ chrome/browser/ui/browser_command_controller_unittest.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_window_state.h"
 #include "chrome/test/base/browser_with_test_window_test.h"
 #include "content/public/browser/native_web_keyboard_event.h"
+#include "ui/base/keycodes/keyboard_codes.h"
 
 TEST_F(BrowserWithTestWindowTest, IsReservedCommandOrKey) {
 #if defined(OS_CHROMEOS)
