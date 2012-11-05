987040fc952bdffc57c4e08d0b42a21af2b0f70a
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-05 19:09:55 +0000

    [BLACKBERRY] Update touch code to reflect touch refactor https://bugs.webkit.org/show_bug.cgi?id=101227
    Patch by Genevieve Mak <gmak@rim.com> on 2012-11-05
    Reviewed by Rob Buis.
    
    PR #2706785
    Reviewed Internally by: Mike Lattanzio
    
    Source/WebCore:
    
    * platform/blackberry/PlatformTouchEventBlackBerry.cpp:
    (WebCore::touchEventType):
    (WebCore::PlatformTouchEvent::PlatformTouchEvent):
    
    Source/WebKit/blackberry:
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::touchEvent):
    (BlackBerry::WebKit::WebPagePrivate::dispatchTouchEventToFullScreenPlugin):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133502 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -4034,7 +4034,7 @@ bool WebPage::touchEvent(const Platform::TouchEvent& event)
         tEvent.m_points[i].m_screenPos = d->mapFromTransformed(tEvent.m_points[i].m_screenPos);
     }
 
-    if (event.hasGesture(Platform::Gesture::SingleTap))
+    if (event.isSingleTap())
         d->m_pluginMayOpenNewTab = true;
     else if (tEvent.m_type == Platform::TouchEvent::TouchStart || tEvent.m_type == Platform::TouchEvent::TouchCancel)
         d->m_pluginMayOpenNewTab = false;
@@ -4044,7 +4044,7 @@ bool WebPage::touchEvent(const Platform::TouchEvent& event)
 
     bool handled = false;
 
-    if (d->m_needTouchEvents && !event.hasGesture(Platform::Gesture::Injected))
+    if (d->m_needTouchEvents && !event.m_type != Platform::TouchEvent::TouchInjected)
         handled = d->m_mainFrame->eventHandler()->handleTouchEvent(PlatformTouchEvent(&tEvent));
 
     // Unpress mouse if touch end is consumed by a JavaScript touch handler, otherwise the mouse state will remain pressed
@@ -4064,7 +4064,7 @@ bool WebPage::touchEvent(const Platform::TouchEvent& event)
         return true;
     }
 
-    if (event.hasGesture(Platform::Gesture::TouchHold))
+    if (event.isTouchHold())
         d->m_touchEventHandler->touchHoldEvent();
 #endif
 
@@ -4095,9 +4095,9 @@ bool WebPagePrivate::dispatchTouchEventToFullScreenPlugin(PluginView* plugin, co
 {
     NPTouchEvent npTouchEvent;
 
-    if (event.hasGesture(Platform::Gesture::DoubleTap))
+    if (event.isDoubleTap())
         npTouchEvent.type = TOUCH_EVENT_DOUBLETAP;
-    else if (event.hasGesture(Platform::Gesture::TouchHold))
+    else if (event.isTouchHold())
         npTouchEvent.type = TOUCH_EVENT_TOUCHHOLD;
     else {
         switch (event.m_type) {
