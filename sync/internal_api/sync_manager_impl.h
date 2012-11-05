c4f45db90d8897a14c6662acb230c8719b81dd5b
nyquist@chromium.org <nyquist@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 19:29:41 +0000

    [sync] Upstream Android connection observer for sync.
    Android has framework support for online state changes. This pings adds
    support in sync to receive such notifications.
    
    BUG=139259, 106034
    TEST=Verify that online state change detection works on Android builds.
    
    
    Review URL: https://chromiumcodereview.appspot.com/10829050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165992 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/sync_manager_impl.h
+++ sync/internal_api/sync_manager_impl.h
@@ -47,14 +47,16 @@ class SyncSessionContext;
 //
 // Unless stated otherwise, all methods of SyncManager should be called on the
 // same thread.
-class SyncManagerImpl : public SyncManager,
-                        public net::NetworkChangeNotifier::IPAddressObserver,
-                        public InvalidationHandler,
-                        public JsBackend,
-                        public SyncEngineEventListener,
-                        public ServerConnectionEventListener,
-                        public syncable::DirectoryChangeDelegate,
-                        public SyncEncryptionHandler::Observer {
+class SyncManagerImpl :
+    public SyncManager,
+    public net::NetworkChangeNotifier::IPAddressObserver,
+    public net::NetworkChangeNotifier::ConnectionTypeObserver,
+    public InvalidationHandler,
+    public JsBackend,
+    public SyncEngineEventListener,
+    public ServerConnectionEventListener,
+    public syncable::DirectoryChangeDelegate,
+    public SyncEncryptionHandler::Observer {
  public:
   // Create an uninitialized SyncManager.  Callers must Init() before using.
   explicit SyncManagerImpl(const std::string& name);
@@ -175,8 +177,12 @@ class SyncManagerImpl : public SyncManager,
       const ObjectIdInvalidationMap& invalidation_map,
       IncomingInvalidationSource source) OVERRIDE;
 
-  // Called only by our NetworkChangeNotifier.
+  // These OnYYYChanged() methods are only called by our NetworkChangeNotifier.
+  // Called when IP address of primary interface changes.
   virtual void OnIPAddressChanged() OVERRIDE;
+  // Called when the connection type of the system has changed.
+  virtual void OnConnectionTypeChanged(
+      net::NetworkChangeNotifier::ConnectionType) OVERRIDE;
 
   const SyncScheduler* scheduler() const;
 
@@ -253,7 +259,7 @@ class SyncManagerImpl : public SyncManager,
       const ModelTypeInvalidationMap& invalidation_map);
 
   // Checks for server reachabilty and requests a nudge.
-  void OnIPAddressChangedImpl();
+  void OnNetworkConnectivityChangedImpl();
 
   // Helper function used only by the constructor.
   void BindJsMessageHandler(
@@ -343,7 +349,7 @@ class SyncManagerImpl : public SyncManager,
   // Set to true once Init has been called.
   bool initialized_;
 
-  bool observing_ip_address_changes_;
+  bool observing_network_connectivity_changes_;
 
   InvalidatorState invalidator_state_;
 
