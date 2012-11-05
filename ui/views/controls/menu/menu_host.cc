80f587e5c249f9e6167b25ccc920ad96a7923038
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 05:42:33 +0000

    Removes some dead code.
    R=ben@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11365071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165902 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_host.cc
+++ ui/views/controls/menu/menu_host.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_controller.h"
 #include "ui/views/controls/menu/menu_host_root_view.h"
 #include "ui/views/controls/menu/menu_item_view.h"
-#include "ui/views/controls/menu/native_menu_host.h"
 #include "ui/views/controls/menu/submenu_view.h"
 #include "ui/views/widget/native_widget_private.h"
 #include "ui/views/widget/widget.h"
--- ui/views/views.gyp
+++ ui/views/views.gyp
@@ -148,8 +148,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'controls/menu/menu_win.cc',
         'controls/menu/menu_win.h',
         'controls/menu/menu_wrapper.h',
-        'controls/menu/native_menu_host.h',
-        'controls/menu/native_menu_host_delegate.h',
         'controls/menu/native_menu_win.cc',
         'controls/menu/native_menu_win.h',
         'controls/menu/menu_image_util.cc',
