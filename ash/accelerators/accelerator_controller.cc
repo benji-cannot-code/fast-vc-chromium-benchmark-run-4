e2b47001612e94912be84bc647acd93aaad52a93
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 22:59:27 +0000

    Check status_area before accessing system_tray where appropriate
    This needs a bit of a re-factor when we add support for multiple status areas (or decide not to), but will at least be safer for now.
    
    BUG=chromium-os:36058
    For shell.h:
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11365093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166062 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/accelerators/accelerator_controller.cc
+++ ash/accelerators/accelerator_controller.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_window_ids.h"
 #include "ash/system/brightness/brightness_control_delegate.h"
 #include "ash/system/keyboard_brightness/keyboard_brightness_control_delegate.h"
+#include "ash/system/status_area_widget.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "ash/volume_control_delegate.h"
@@ -620,11 +621,15 @@ bool AcceleratorController::PerformAction(int action,
       return HandleRotatePaneFocus(Shell::FORWARD);
     case FOCUS_PREVIOUS_PANE:
       return HandleRotatePaneFocus(Shell::BACKWARD);
-    case FOCUS_SYSTEM_TRAY:
-      if (shell->system_tray())
+    case FOCUS_SYSTEM_TRAY: {
+      internal::StatusAreaWidget* status_area =
+          ash::Shell::GetInstance()->status_area_widget();
+      if (status_area) {
         return shell->focus_cycler()->FocusWidget(
-            shell->system_tray()->GetWidget());
+            status_area->system_tray()->GetWidget());
+      }
       break;
+    }
     case SHOW_KEYBOARD_OVERLAY:
       ash::Shell::GetInstance()->delegate()->ShowKeyboardOverlay();
       return true;
--- ash/system/tray_update.cc
+++ ash/system/tray_update.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/root_window_controller.h"
 #include "ash/shell.h"
+#include "ash/system/status_area_widget.h"
 #include "ash/system/tray/system_tray.h"
 #include "ash/system/tray/system_tray_delegate.h"
 #include "ash/system/tray/tray_constants.h"
@@ -107,8 +108,12 @@ class UpdateNagger : public ui::LayerAnimationObserver {
   }
 
   virtual ~UpdateNagger() {
-    Shell::GetInstance()->system_tray()->GetWidget()->GetNativeView()->layer()->
-        GetAnimator()->RemoveObserver(this);
+    internal::StatusAreaWidget* status_area =
+        ash::Shell::GetInstance()->status_area_widget();
+    if (status_area) {
+      status_area->system_tray()->GetWidget()->GetNativeView()->layer()->
+          GetAnimator()->RemoveObserver(this);
+    }
   }
 
   void RestartTimer() {
