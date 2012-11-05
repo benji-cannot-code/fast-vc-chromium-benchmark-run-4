401c70587ee16b2de1fd8bc4b62187b2969ab50b
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 21:31:56 +0000

    Make PrerenderManager depend on HistoryService
    This is needed since PrerenderLocalPredictor calls into HistoryService
    to add itself as a visit DB observer.
    
    This is a speculative fix for the bug below.
    
    BUG=159331
    
    
    Review URL: https://chromiumcodereview.appspot.com/11367090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166035 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager_factory.cc
+++ chrome/browser/prerender/prerender_manager_factory.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_system_factory.h"
+#include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
@@ -36,6 +37,8 @@ PrerenderManagerFactory::PrerenderManagerFactory()
     : ProfileKeyedServiceFactory("PrerenderManager",
                                  ProfileDependencyManager::GetInstance()) {
   DependsOn(extensions::ExtensionSystemFactory::GetInstance());
+  // PrerenderLocalPredictor observers the history visit DB.
+  DependsOn(HistoryServiceFactory::GetInstance());
 }
 
 PrerenderManagerFactory::~PrerenderManagerFactory() {
