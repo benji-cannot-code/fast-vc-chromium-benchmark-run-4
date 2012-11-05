4c10d4b80480706352f2007328ce7fe776775373
egraether@chromium.org <egraether@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 21:48:15 +0000

    this change improves the following points of the FPS counter: -cleaner design: less colors, text arranged above the graph, deviation number right aligned -added transparency: FPS counter does not completely cover the webpage -less frequent number updates: makes the numbers easier to read -indicator line at 60fps
    comparison images: https://docs.google.com/folder/d/0B8Y78t3tjy1XZk1xdWx6VjN5aFE/edit
    
    Please download the patch and provide a screenshot from your system to test the layout.
    
    Review URL: https://chromiumcodereview.appspot.com/11272042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166041 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/heads_up_display_layer.cc
+++ cc/heads_up_display_layer.cc
@@ -22,7 +22,7 @@ HeadsUpDisplayLayer::HeadsUpDisplayLayer()
     : Layer()
 {
 
-    setBounds(gfx::Size(512, 128));
+    setBounds(gfx::Size(256, 128));
 }
 
 HeadsUpDisplayLayer::~HeadsUpDisplayLayer()
@@ -40,7 +40,7 @@ void HeadsUpDisplayLayer::update(ResourceUpdateQueue&, const OcclusionTracker*, 
         int height = std::min(maxTextureSize, layerTreeHost()->deviceViewportSize().height());
         bounds = gfx::Size(width, height);
     } else {
-        bounds = gfx::Size(512, 128);
+        bounds = gfx::Size(256, 128);
     }
 
     setBounds(bounds);
--- cc/heads_up_display_layer_impl.h
+++ cc/heads_up_display_layer_impl.h
@@ -7,12 +7,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_HEADS_UP_DISPLAY_LAYER_IMPL_H_
 
 #include "base/memory/scoped_ptr.h"
+#include "base/time.h"
 #include "cc/cc_export.h"
 #include "cc/font_atlas.h"
 #include "cc/layer_impl.h"
 #include "cc/scoped_texture.h"
 
 class SkCanvas;
+class SkPaint;
+struct SkRect;
 
 namespace cc {
 
@@ -45,13 +48,19 @@ private:
     virtual const char* layerTypeAsString() const OVERRIDE;
 
     void drawHudContents(SkCanvas*);
-    void drawFPSCounter(SkCanvas*, FrameRateCounter*, int top, int height);
-    void drawFPSCounterText(SkCanvas*, FrameRateCounter*, int top, int width, int height);
+    int drawFPSCounter(SkCanvas*, FrameRateCounter*);
+    void drawFPSCounterText(SkCanvas*, SkPaint&, FrameRateCounter*, SkRect);
+    void drawFPSCounterGraph(SkCanvas*, SkPaint&, FrameRateCounter*, SkRect);
     void drawDebugRects(SkCanvas*, DebugRectHistory*);
 
     scoped_ptr<FontAtlas> m_fontAtlas;
     scoped_ptr<ScopedTexture> m_hudTexture;
     scoped_ptr<SkCanvas> m_hudCanvas;
+
+    double m_averageFPS;
+    double m_stdDeviation;
+
+    base::TimeTicks textUpdateTime;
 };
 
 }  // namespace cc
