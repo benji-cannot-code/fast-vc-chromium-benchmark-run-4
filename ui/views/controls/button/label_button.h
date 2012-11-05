301fbd600e843bf1ad6c23b0721cf67c7a8d19ff
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 18:20:50 +0000

    Make font used by views::LabelButton modifiable
    This CL adds a getter/setter pair for the font used by a LabelButton,
    implementing functionality that a TextButton has but a LabelButton was
    missing.
    
    BUG=155363
    
    
    Review URL: https://chromiumcodereview.appspot.com/11345029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165961 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/button/label_button.h
+++ ui/views/controls/button/label_button.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "third_party/skia/include/core/SkColor.h"
+#include "ui/gfx/font.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/views/controls/button/custom_button.h"
 #include "ui/views/controls/image_view.h"
@@ -39,6 +40,10 @@ class VIEWS_EXPORT LabelButton : public CustomButton,
   bool GetTextMultiLine() const;
   void SetTextMultiLine(bool text_multi_line);
 
+  // Get or set the font used by this button.
+  const gfx::Font& GetFont() const;
+  void SetFont(const gfx::Font& font);
+
   // Get or set the horizontal alignment used for the button.
   // The optional image will lead the text, unless the button is right-aligned.
   Label::Alignment GetHorizontalAlignment() const;
@@ -61,6 +66,7 @@ class VIEWS_EXPORT LabelButton : public CustomButton,
   FRIEND_TEST_ALL_PREFIXES(LabelButtonTest, Label);
   FRIEND_TEST_ALL_PREFIXES(LabelButtonTest, Image);
   FRIEND_TEST_ALL_PREFIXES(LabelButtonTest, LabelAndImage);
+  FRIEND_TEST_ALL_PREFIXES(LabelButtonTest, Font);
 
   // Overridden from CustomButton:
   virtual void StateChanged() OVERRIDE;
