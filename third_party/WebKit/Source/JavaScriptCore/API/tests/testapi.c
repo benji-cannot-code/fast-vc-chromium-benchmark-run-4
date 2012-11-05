21d071238a9f674fc1b2ddfad176d355122bc6a1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-05 23:10:07 +0000

    Back out controversial changes from Bug 98665. https://bugs.webkit.org/show_bug.cgi?id=101244
    Patch by Dima Gorbik <dgorbik@apple.com> on 2012-11-05
    Reviewed by David Kilzer.
    
    Backing out changes from Bug 98665 until further discussions take place on rules for including Platform.h in Assertions.h.
    
    Source/JavaScriptCore:
    
    * API/tests/minidom.c:
    * API/tests/testapi.c:
    
    Source/WTF:
    
    * wtf/Assertions.h:
    
    Tools:
    
    * DumpRenderTree/mac/MockGeolocationProvider.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/API/tests/testapi.c
+++ third_party/WebKit/Source/JavaScriptCore/API/tests/testapi.c
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSObjectRefPrivate.h"
 #include <math.h>
 #define ASSERT_DISABLED 0
-#include <wtf/Platform.h>
 #include <wtf/Assertions.h>
 #include <wtf/UnusedParam.h>
 
