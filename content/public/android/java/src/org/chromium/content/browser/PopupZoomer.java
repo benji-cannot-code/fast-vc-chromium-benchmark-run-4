0a1ceaf1e42c83f8c1d41b74458f751e37b84fef
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-05 14:21:40 +0000

    [Android] Fallback to a default drawable for the PopupZoomer.
    At the moment, tests must invoke PopupZoomer.injectOverlayDrawable to
    ensure there is a default drawable resource set. WebView also needs
    to do something similar, so rather than using a testing specific
    function allow the popupzoomer to gracefully fallback to a default
    drawable for all users.
    
    Android only change, android bots are happy
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11360060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@165935 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/PopupZoomer.java
+++ content/public/android/java/src/org/chromium/content/browser/PopupZoomer.java
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.content.browser;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
@@ -18,8 +19,10 @@ import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
 import android.graphics.RectF;
 import android.graphics.Region.Op;
+import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
@@ -33,6 +36,8 @@ import org.chromium.content.app.AppResource;
  * canvas and touch events to display the on-demand zoom magnifier.
  */
 class PopupZoomer extends View {
+    private static String LOGTAG = "PopupZoomer";
+
     // The padding between the edges of the view and the popup. Note that there is a mirror
     // constant in content/renderer/render_view_impl.cc which should be kept in sync if
     // this is changed.
@@ -121,23 +126,17 @@ class PopupZoomer extends View {
      */
     private static Drawable getOverlayDrawable(Context context) {
         if (sOverlayDrawable == null) {
-            sOverlayDrawable = context.getResources().getDrawable(
-                    AppResource.DRAWABLE_LINK_PREVIEW_POPUP_OVERLAY);
+            try {
+                sOverlayDrawable = context.getResources().getDrawable(
+                        AppResource.DRAWABLE_LINK_PREVIEW_POPUP_OVERLAY);
+            } catch (Resources.NotFoundException e) {
+                Log.w(LOGTAG, "No drawable resource for PopupZoomer overlay found.");
+                sOverlayDrawable = new ColorDrawable();
+            }
             sOverlayPadding = new Rect();
             sOverlayDrawable.getPadding(sOverlayPadding);
         }
         return sOverlayDrawable;
-    }
-
-    /**
-     * Injects the overlay drawable for tests. Needs to be called before any instance
-     * of this class tries to draw.
-     *
-     * @VisibleForTesting
-     */
-    static void injectOverlayDrawable(Drawable drawable) {
-        sOverlayDrawable = drawable;
-        sOverlayPadding = new Rect();
     }
 
     private static float constrain(float amount, float low, float high) {
