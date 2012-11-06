4844ee758b379479888dbc91c1d3464df3135b2b
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 05:52:42 +0000

    drive: Return scoped_ptr<> instead of a raw pointer from DocumentEntry
    To make the ownershp of the return object clear.
    
    BUG=148705
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11369045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/gdata_wapi_parser_unittest.cc
+++ chrome/browser/google_apis/gdata_wapi_parser_unittest.cc
@@ -47,7 +47,7 @@ class GDataWAPIParserTest : public testing::Test {
     scoped_ptr<DocumentEntry> entry;
     while (reader.Read()) {
       if (reader.NodeName() == "entry") {
-        entry.reset(DocumentEntry::CreateFromXml(&reader));
+        entry = DocumentEntry::CreateFromXml(&reader);
         break;
       }
     }
