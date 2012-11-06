41553d094ae7148df103d3b7e7794559d15eb37b
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 04:39:59 +0000

    Initialize renderer color preferences to reasonable defaults.
    BUG=158422
    TEST=as in bug
    
    
    Review URL: https://chromiumcodereview.appspot.com/11365096
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166146 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_gtk.cc
+++ content/shell/shell_gtk.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/renderer_preferences.h"
 #include "content/shell/shell_browser_context.h"
 #include "content/shell/shell_content_browser_client.h"
-#include "third_party/skia/include/core/SkColor.h"
 
 namespace content {
 
@@ -191,20 +190,6 @@ void Shell::PlatformCreateWindow(int width, int height) {
 void Shell::PlatformSetContents() {
   WebContentsView* content_view = web_contents_->GetView();
   gtk_container_add(GTK_CONTAINER(vbox_), content_view->GetNativeView());
-
-  // As an additional requirement on Linux, we must set the colors for the
-  // render widgets in webkit.
-  content::RendererPreferences* prefs =
-      web_contents_->GetMutableRendererPrefs();
-  prefs->focus_ring_color = SkColorSetARGB(255, 229, 151, 0);
-  prefs->thumb_active_color = SkColorSetRGB(244, 244, 244);
-  prefs->thumb_inactive_color = SkColorSetRGB(234, 234, 234);
-  prefs->track_color = SkColorSetRGB(211, 211, 211);
-
-  prefs->active_selection_bg_color = SkColorSetRGB(30, 144, 255);
-  prefs->active_selection_fg_color = SK_ColorWHITE;
-  prefs->inactive_selection_bg_color = SkColorSetRGB(200, 200, 200);
-  prefs->inactive_selection_fg_color = SkColorSetRGB(50, 50, 50);
 }
 
 void Shell::SizeTo(int width, int height) {
