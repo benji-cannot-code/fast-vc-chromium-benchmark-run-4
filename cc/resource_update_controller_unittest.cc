1b8e8c3b7978281031d50eb7552a1e2b5cc433d3
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 19:29:53 +0000

    cc: Remove WebThread includes.
    BUG=154451
    TEST=cc_unittests
    R=enne@chromium.org,jamesr@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166244 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resource_update_controller_unittest.cc
+++ cc/resource_update_controller_unittest.cc
@@ -13,13 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/test/scheduler_test_common.h"
 #include "cc/test/tiled_layer_test_common.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include <public/WebThread.h>
 
 using namespace cc;
 using namespace WebKit;
 using namespace WebKitTests;
 using testing::Test;
-
 
 namespace {
 
@@ -186,7 +184,6 @@ protected:
     scoped_ptr<ResourceProvider> m_resourceProvider;
     scoped_ptr<ResourceUpdateQueue> m_queue;
     scoped_ptr<PrioritizedTexture> m_textures[4];
-    scoped_ptr<WebThread> m_thread;
     scoped_ptr<PrioritizedTextureManager> m_textureManager;
     SkBitmap m_bitmap;
 
