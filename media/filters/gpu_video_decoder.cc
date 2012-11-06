76f8b07db19ccc6cbe13fc7b9adb48ca5491f30a
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 19:43:18 +0000

    Make media targets work with the shared library build for Android.
    It looks like gpu_video_decoder has been refactored to not depend on
    ffmpeg and is required for the shared library build.
    
    BUG=158821
    
    
    Review URL: https://chromiumcodereview.appspot.com/11362107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166249 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/gpu_video_decoder.cc
+++ media/filters/gpu_video_decoder.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/pipeline.h"
 #include "media/base/pipeline_status.h"
 #include "media/base/video_decoder_config.h"
-#include "media/ffmpeg/ffmpeg_common.h"
 
 namespace media {
 
