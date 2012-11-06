3e2d6300ca1a56fbb9d00904d58a0d23fab751bb
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 04:44:10 +0000

    make sure AddGalleryByPath flips blacklisted galleries back to autodetected
    BUG=158142
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_gallery/media_galleries_preferences.cc
+++ chrome/browser/media_gallery/media_galleries_preferences.cc
@@ -244,7 +244,6 @@ MediaGalleryPrefIdSet MediaGalleriesPreferences::LookUpGalleriesByDeviceId(
   return found->second;
 }
 
-
 MediaGalleryPrefId MediaGalleriesPreferences::AddGallery(
     const std::string& device_id, const string16& display_name,
     const FilePath& relative_path, bool user_added) {
@@ -260,7 +259,7 @@ MediaGalleryPrefId MediaGalleriesPreferences::AddGallery(
 
     const MediaGalleryPrefInfo& existing = known_galleries_[*it];
     bool update_gallery_type =
-      existing.type == MediaGalleryPrefInfo::kBlackListed;
+        existing.type == MediaGalleryPrefInfo::kBlackListed;
     bool update_gallery_name = existing.display_name != display_name;
     if (update_gallery_name || update_gallery_type) {
       PrefService* prefs = profile_->GetPrefs();
@@ -312,9 +311,10 @@ MediaGalleryPrefId MediaGalleriesPreferences::AddGallery(
 MediaGalleryPrefId MediaGalleriesPreferences::AddGalleryByPath(
     const FilePath& path) {
   MediaGalleryPrefInfo gallery_info;
-  if (LookUpGalleryByPath(path, &gallery_info))
+  if (LookUpGalleryByPath(path, &gallery_info) &&
+      gallery_info.type != MediaGalleryPrefInfo::kBlackListed) {
     return gallery_info.pref_id;
-  DCHECK_EQ(kInvalidMediaGalleryPrefId, gallery_info.pref_id);
+  }
   return AddGallery(gallery_info.device_id, gallery_info.display_name,
                     gallery_info.path, true /*user added*/);
 }
