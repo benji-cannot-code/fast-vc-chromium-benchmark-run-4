4f55fa2d4f2f3d85cdb541b486acac7f3bc2d656
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-06 22:05:49 +0000

    Reduce direct m_symbolTable usage in CodeBlock https://bugs.webkit.org/show_bug.cgi?id=101391
    Reviewed by Sam Weinig.
    
    Simple refactoring.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::dump):
    (JSC::CodeBlock::dumpStatistics):
    (JSC::CodeBlock::nameForRegister):
    * bytecode/CodeBlock.h:
    (JSC::CodeBlock::isCaptured):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
@@ -590,8 +590,8 @@ namespace JSC {
             if (usesArguments() && operand == unmodifiedArgumentsRegister(argumentsRegister()))
                 return true;
 
-            return operand >= m_symbolTable->captureStart() 
-                && operand < m_symbolTable->captureEnd();
+            return operand >= symbolTable()->captureStart()
+                && operand < symbolTable()->captureEnd();
         }
 
         CodeType codeType() const { return m_codeType; }
@@ -965,7 +965,7 @@ namespace JSC {
         StringJumpTable& stringSwitchJumpTable(int tableIndex) { ASSERT(m_rareData); return m_rareData->m_stringSwitchJumpTables[tableIndex]; }
 
 
-        SharedSymbolTable* symbolTable() { return m_symbolTable.get(); }
+        SharedSymbolTable* symbolTable() const { return m_symbolTable.get(); }
 
         EvalCodeCache& evalCodeCache() { createRareDataIfNecessary(); return m_rareData->m_evalCodeCache; }
 
