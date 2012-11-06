ebe995cef3cdd2e6d334c9d4474b9b03477d018b
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 03:27:54 +0000

    drive: Unify the use of ResourceMetadata term "origin" or "initialized" to "loaded".
    BUG=159073
    
    Review URL: https://codereview.chromium.org/11293057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166118 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_feed_loader.cc
+++ chrome/browser/chromeos/drive/drive_feed_loader.cc
@@ -246,7 +246,7 @@ void DriveFeedLoader::ReloadFromServerIfNeeded(
   DCHECK(!callback.is_null());
   DVLOG(1) << "ReloadFromServerIfNeeded local_changestamp="
            << resource_metadata_->largest_changestamp()
-           << ", initialized=" << resource_metadata_->initialized();
+           << ", loaded=" << resource_metadata_->loaded();
 
   // Sets the refreshing flag, so that the caller does not send refresh requests
   // in parallel (see DriveFileSystem::CheckForUpdates).
@@ -758,7 +758,7 @@ void DriveFeedLoader::OnNotifyDocumentFeedFetched(
 void DriveFeedLoader::LoadFromCache(const FileOperationCallback& callback) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(!callback.is_null());
-  DCHECK(!resource_metadata_->initialized());
+  DCHECK(!resource_metadata_->loaded());
 
   // Sets the refreshing flag, so that the caller does not send refresh requests
   // in parallel (see DriveFileSystem::LoadFeedIfNeeded).
