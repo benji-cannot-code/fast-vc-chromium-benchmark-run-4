932503b443bb1e57481497bd3f4f4979ea07d549
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-06 03:46:16 +0000

    [EFL][WK2] Fix wrong coding style in the boilerplates https://bugs.webkit.org/show_bug.cgi?id=101283
    Patch by Jinwoo Song <jinwoo7.song@samsung.com> on 2012-11-05
    Reviewed by Gyuyoung Kim.
    
    Fix boilerplate which have wrong coding style.
    
    * UIProcess/efl/InputMethodContextEfl.cpp:
    * UIProcess/efl/InputMethodContextEfl.h:
    * UIProcess/efl/PageClientDefaultImpl.cpp:
    * UIProcess/efl/PageClientDefaultImpl.h:
    * UIProcess/efl/PageClientLegacyImpl.cpp:
    * UIProcess/efl/PageClientLegacyImpl.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133554 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/efl/PageClientDefaultImpl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/efl/PageClientDefaultImpl.cpp
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Samsung Electronics
-   Copyright (C) 2012 Intel Corporation. All rights reserved.
+ * Copyright (C) 2012 Intel Corporation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
