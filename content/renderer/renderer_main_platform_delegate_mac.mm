9ab47a9b4a655590fc04d9bbaf4d49392d0a295b
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 21:50:31 +0000

    Add two crash keys for debugging to record Text Input Services information in the renderer.
    BUG=152566
    
    
    Review URL: https://chromiumcodereview.appspot.com/11363106
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166275 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_main_platform_delegate_mac.mm
+++ content/renderer/renderer_main_platform_delegate_mac.mm
@@ -5,11 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/renderer/renderer_main_platform_delegate.h"
 
+#include <Carbon/Carbon.h>
 #import <Cocoa/Cocoa.h>
 #include <objc/runtime.h>
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#import "base/mac/foundation_util.h"
+#import "base/mac/crash_logging.h"
+#include "base/mac/scoped_cftyperef.h"
 #include "base/sys_string_conversions.h"
 #include "content/common/sandbox_mac.h"
 #include "content/public/common/content_switches.h"
@@ -40,6 +44,19 @@ void RendererMainPlatformDelegate::PlatformInitialize() {
                              toTarget:string
                            withObject:nil];
   }
+
+  // Debugging for http://crbug.com/152566
+  base::mac::ScopedCFTypeRef<TISInputSourceRef> input_source(
+      TISCopyCurrentKeyboardInputSource());
+  base::mac::ScopedCFTypeRef<CFStringRef> description(
+      CFCopyDescription(input_source));
+  base::mac::SetCrashKeyValue(@"tis_input_source",
+                              base::mac::CFToNSCast(description));
+
+  input_source.reset(TISCopyCurrentKeyboardLayoutInputSource());
+  description.reset(CFCopyDescription(input_source));
+  base::mac::SetCrashKeyValue(@"tis_layout_source",
+                              base::mac::CFToNSCast(description));
 }
 
 void RendererMainPlatformDelegate::PlatformUninitialize() {
