7ce36c28352de3b1e2b7b98cb083fa5a0b717c5f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-06 06:41:29 +0000

    Ensure acquireLineBreakIterator signature consistency on non-ICU ports. https://bugs.webkit.org/show_bug.cgi?id=101281
    Patch by Glenn Adams <glenn@skynav.com> on 2012-11-05
    Reviewed by Ryosuke Niwa.
    
    Fix (newly) inconsistent signature for acquireLineBreakIterator for non-ICU ports
    caused by http://trac.webkit.org/changeset/133529.
    
    See new tests added from https://bugs.webkit.org/show_bug.cgi?id=89235.
    
    * platform/text/TextBreakIterator.cpp:
    (WebCore::acquireLineBreakIterator):
    * platform/text/gtk/TextBreakIteratorGtk.cpp:
    (WebCore::setUpIterator):
    (WebCore::acquireLineBreakIterator):
    * platform/text/wince/TextBreakIteratorWinCE.cpp:
    (WebCore::acquireLineBreakIterator):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133562 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/text/wince/TextBreakIteratorWinCE.cpp
+++ third_party/WebKit/Source/WebCore/platform/text/wince/TextBreakIteratorWinCE.cpp
@@ -257,7 +257,7 @@ NonSharedCharacterBreakIterator::~NonSharedCharacterBreakIterator()
 
 static TextBreakIterator* staticLineBreakIterator;
 
-TextBreakIterator* acquireLineBreakIterator(const UChar* string, int length, const AtomicString&)
+TextBreakIterator* acquireLineBreakIterator(const UChar* string, int length, const AtomicString&, LineBreakIteratorMode, bool)
 {
     TextBreakIterator* lineBreakIterator = 0;
     if (staticLineBreakIterator) {
