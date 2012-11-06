4526e026beb18d6d4c8347099c65bd09eb272948
zmo@google.com <zmo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 20:48:54 +0000

    No further GPUInfo update once we fallback to SwiftShader.
    Otherwise because we have no further access to D3D devices, we will fail to collect driver date, etc., and then blacklisting will be based on incomplete GPUInfo.
    
    This causes about:gpu to mis-behave when --blacklist-webgl is passed in and SwiftShader is installed.
    
    BUG=158377
    TEST=content_unittests, about:gpu
    Review URL: https://codereview.chromium.org/11364060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166260 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl.cc
+++ content/browser/gpu/gpu_data_manager_impl.cc
@@ -186,6 +186,10 @@ bool GpuDataManagerImpl::IsCompleteGpuInfoAvailable() const {
 }
 
 void GpuDataManagerImpl::UpdateGpuInfo(const GPUInfo& gpu_info) {
+  // No further update of gpu_info if falling back to software renderer.
+  if (software_rendering_)
+    return;
+
   GetContentClient()->SetGpuInfo(gpu_info);
 
   if (gpu_blacklist_.get()) {
