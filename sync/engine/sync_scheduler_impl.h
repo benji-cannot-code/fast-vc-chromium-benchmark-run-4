7c4a0487623ab8a63087d51c2e2481957751af09
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 16:31:54 +0000

    sync: fix for bug 158313, which is a top crash on canary.
    If a nudge is scheduled after a connection change canary, an abandoned job may wind up
    in DoCanaryJob.  Since the job knows by design if it's in this state, this patch adds a defensive
    check in DoCanaryJob to check (similar to the one in DoSyncSessionJob).  
    
    We should be able to remove the CreateSyncSession code that actually tries to use the job before
    calling DoSyncSessionJob, though.
    
    BUG=158313
    
    
    Review URL: https://chromiumcodereview.appspot.com/11366079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166215 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/sync_scheduler_impl.h
+++ sync/engine/sync_scheduler_impl.h
@@ -114,6 +114,8 @@ class SyncSchedulerImpl : public SyncScheduler {
   FRIEND_TEST_ALL_PREFIXES(SyncSchedulerTest, GetInitialBackoffDelay);
   FRIEND_TEST_ALL_PREFIXES(SyncSchedulerTest,
                            ServerConnectionChangeDuringBackoff);
+  FRIEND_TEST_ALL_PREFIXES(SyncSchedulerTest,
+                           ConnectionChangeCanaryPreemptedByNudge);
 
   struct WaitInterval {
     enum Mode {
