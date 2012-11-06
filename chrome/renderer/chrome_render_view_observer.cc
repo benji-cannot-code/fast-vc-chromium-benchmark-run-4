c804a534a102dd937113ec4b2c66ced65c1f394f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 13:57:03 +0000

    chrome: Change --enable-frameless-constrained-dialogs flag to --enable-chrome-style-dialogs.
    The dialogs referenced by this flag are not frameless, and the constrained part
    will soon be going away also -- see issue 157161.  Update the flag to reflect
    its proper purpose.
    
    Raised (in part) by ben@ in https://codereview.chromium.org/11275065.
    
    BUG=158772
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/11367083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166205 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_render_view_observer.cc
+++ chrome/renderer/chrome_render_view_observer.cc
@@ -368,7 +368,7 @@ void ChromeRenderViewObserver::OnSetVisuallyDeemphasized(bool deemphasized) {
 #endif
 
   if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kEnableFramelessConstrainedDialogs)) {
+      switches::kEnableChromeStyleDialogs)) {
     bool already_deemphasized = !!dimmed_color_overlay_.get();
     if (already_deemphasized == deemphasized)
       return;
