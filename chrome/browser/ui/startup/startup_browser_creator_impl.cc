189951daecfd011dd98d010d86241db2b5c39fe1
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 04:35:52 +0000

    Add a flag to control whether there is a shortcut for the app list / launcher in the Windows taskbar.
    When the flag is on, a shortcut will be created and pinned if it doesn't exist. When it is off, the shortcut will be deleted if it exists.
    
    BUG=158715
    
    
    Review URL: https://chromiumcodereview.appspot.com/11367002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -363,6 +363,7 @@ bool StartupBrowserCreatorImpl::Launch(Profile* profile,
     }
   }
 
+  app_list_controller::CheckAppListTaskbarShortcut();
   if (command_line_.HasSwitch(switches::kShowAppList)) {
     app_list_controller::ShowAppList();
     return true;
