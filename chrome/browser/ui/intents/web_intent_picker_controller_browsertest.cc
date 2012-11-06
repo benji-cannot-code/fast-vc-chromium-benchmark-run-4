8675b7303b95b85a3fc69f8721630f142fc82941
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-06 03:10:47 +0000

    This is the original patch from issue 11142033 (which had ASAN issues) plus a patch that ensured WorkerPools are flushed for the test
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11191073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166110 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_controller_browsertest.cc
+++ chrome/browser/ui/intents/web_intent_picker_controller_browsertest.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/string_util.h"
 #include "base/stringprintf.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/favicon/favicon_service.h"
 #include "chrome/browser/favicon/favicon_service_factory.h"
@@ -419,6 +420,10 @@ IN_PROC_BROWSER_TEST_F(WebIntentPickerControllerBrowserTest,
 
   controller_->ShowDialog(kAction1, kType1);
   picker_.Wait();
+  // Flush all pending worker tasks for PNG decoding.
+  content::BrowserThread::GetBlockingPool()->FlushForTesting();
+  // Flush all tasks posted from the worker tasks.
+  MessageLoop::current()->RunUntilIdle();
   EXPECT_EQ(2, picker_.num_installed_services_);
   EXPECT_EQ(0, picker_.num_icons_changed_);
   EXPECT_EQ(1, picker_.num_extension_icons_changed_);
