a25cd12c76b8cad13426c85a57c32fc269ef95a9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 06:46:00 +0000

    Move DrawingAreaImpl methods graphicsLayerFactory, setRootCompositingLayer & scheduleCompositingLayerSync under ACCELERATED_COMPOSITING https://bugs.webkit.org/show_bug.cgi?id=101427
    Patch by Vivek Galatage <vivekgalatage@gmail.com> on 2012-11-06
    Reviewed by Anders Carlsson.
    
    Moving these methods under the ACCELERATED_COMPOSITING flag.
    This fixes broken wincairo due to use of override specifier.
    
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    (WebKit):
    * WebProcess/WebPage/DrawingAreaImpl.h:
    (DrawingAreaImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -308,6 +308,7 @@ void DrawingAreaImpl::layerHostDidFlushLayers()
 #endif
 }
 
+#if USE(ACCELERATED_COMPOSITING)
 GraphicsLayerFactory* DrawingAreaImpl::graphicsLayerFactory()
 {
     if (m_layerTreeHost)
@@ -362,6 +363,7 @@ void DrawingAreaImpl::scheduleCompositingLayerFlush()
         return;
     m_layerTreeHost->scheduleLayerFlush();
 }
+#endif
 
 void DrawingAreaImpl::updateBackingStoreState(uint64_t stateID, bool respondImmediately, float deviceScaleFactor, const WebCore::IntSize& size, const WebCore::IntSize& scrollOffset)
 {
