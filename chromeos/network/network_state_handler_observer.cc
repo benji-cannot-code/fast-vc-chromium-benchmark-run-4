def1b5844a9a5de77873d74f69c2b67d20542d97
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 10:14:59 +0000

    Add chromeos::NetworkStateManager to src/chromeos/network.
    The intention for this class is to provide up-to-date state information about the network for the majority of the network related UI. It should also serve as a foundation for classes monitoring the state of the active network.
    
    This code is not designed to provide detailed information about specific network service or device properties, not should it provide the ability to set (configure) those properties.
    
    BUG=154072
    
    Review URL: https://chromiumcodereview.appspot.com/11192024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166375 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chromeos/network/network_state_handler_observer.cc
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chromeos/network/network_state_handler_observer.h"
+
+namespace chromeos {
+
+NetworkStateHandlerObserver::NetworkStateHandlerObserver() {
+}
+
+NetworkStateHandlerObserver::~NetworkStateHandlerObserver() {
+}
+
+void NetworkStateHandlerObserver::NetworkManagerChanged() {
+}
+
+void NetworkStateHandlerObserver::NetworkListChanged(
+    const NetworkStateList& networks) {
+}
+
+void NetworkStateHandlerObserver::DeviceListChanged() {
+}
+
+void NetworkStateHandlerObserver::ActiveNetworkChanged(
+    const NetworkState* network) {
+}
+
+void NetworkStateHandlerObserver::ActiveNetworkStateChanged(
+    const NetworkState* network) {
+}
+
+void NetworkStateHandlerObserver::NetworkServiceChanged(
+    const NetworkState* network) {
+}
+
+}  // namespace chromeos
