27552e4c73ab80066c940fb41e888bd3d52a5c54
roger_fong@apple.com <roger_fong@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 21:58:00 +0000

    Unreviewed. Style fix following r133262.
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::clearMediaPlayer):
    * html/HTMLMediaElement.h:
    (HTMLMediaElement):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.h
@@ -468,7 +468,7 @@ private:
     void scheduleNextSourceChild();
     void loadNextSourceChild();
     void userCancelledLoad();
-    void clearMediaPlayer(signed flags);
+    void clearMediaPlayer(int flags);
     bool havePotentialSourceChild();
     void noneSupported();
     void mediaEngineError(PassRefPtr<MediaError> err);
