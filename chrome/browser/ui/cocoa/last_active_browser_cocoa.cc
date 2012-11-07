4ceb52aa65ac412e23d8ff5a606ec04b93dc1080
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 12:19:04 +0000

    Remove BrowserList::GetLastActive usage in cocoa-land.
    BUG=129187
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11369111
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166398 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/last_active_browser_cocoa.cc
+++ chrome/browser/ui/cocoa/last_active_browser_cocoa.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/cocoa/last_active_browser_cocoa.h"
 
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_finder.h"
 
 namespace chrome {
 
 Browser* GetLastActiveBrowser() {
-  return BrowserList::GetLastActive();
+  return browser::FindLastActiveWithHostDesktopType(HOST_DESKTOP_TYPE_NATIVE);
 }
 
 }  // namespace chrome
