2bf2ff5ff8c40849c656170b2bd9a72068862c73
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 12:27:33 +0000

    Make the "deny" button on the Pepper broker infobar sticky.
    TBR=brettw@chromium.org
    BUG=159198
    
    
    Review URL: https://chromiumcodereview.appspot.com/11367071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/pepper_broker_observer.cc
+++ chrome/browser/pepper_broker_observer.cc
@@ -67,6 +67,8 @@ class PepperBrokerInfoBarDelegate : public ConfirmInfoBarDelegate {
   virtual gfx::Image* GetIcon() const OVERRIDE;
 
  private:
+  void DispatchCallback(bool result);
+
   const GURL url_;
   const FilePath plugin_path_;
   const std::string languages_;
@@ -128,23 +130,12 @@ string16 PepperBrokerInfoBarDelegate::GetButtonLabel(
 }
 
 bool PepperBrokerInfoBarDelegate::Accept() {
-  content::RecordAction(
-      content::UserMetricsAction("PPAPI.BrokerInfobarClickedAllow"));
-  callback_.Run(true);
-  callback_ = base::Callback<void(bool)>();
-  content_settings_->SetContentSetting(
-      ContentSettingsPattern::FromURLNoWildcard(url_),
-      ContentSettingsPattern::Wildcard(),
-      CONTENT_SETTINGS_TYPE_PPAPI_BROKER,
-      std::string(), CONTENT_SETTING_ALLOW);
+  DispatchCallback(true);
   return true;
 }
 
 bool PepperBrokerInfoBarDelegate::Cancel() {
-  content::RecordAction(
-      content::UserMetricsAction("PPAPI.BrokerInfobarClickedDeny"));
-  callback_.Run(false);
-  callback_ = base::Callback<void(bool)>();
+  DispatchCallback(false);
   return true;
 }
 
@@ -166,6 +157,19 @@ bool PepperBrokerInfoBarDelegate::LinkClicked(
 gfx::Image* PepperBrokerInfoBarDelegate::GetIcon() const {
   return &ResourceBundle::GetSharedInstance().GetNativeImageNamed(
       IDR_INFOBAR_PLUGIN_INSTALL);
+}
+
+void PepperBrokerInfoBarDelegate::DispatchCallback(bool result) {
+  content::RecordAction(result ?
+      content::UserMetricsAction("PPAPI.BrokerInfobarClickedAllow") :
+      content::UserMetricsAction("PPAPI.BrokerInfobarClickedDeny"));
+  callback_.Run(result);
+  callback_ = base::Callback<void(bool)>();
+  content_settings_->SetContentSetting(
+      ContentSettingsPattern::FromURLNoWildcard(url_),
+      ContentSettingsPattern::Wildcard(),
+      CONTENT_SETTINGS_TYPE_PPAPI_BROKER,
+      std::string(), result ? CONTENT_SETTING_ALLOW : CONTENT_SETTING_BLOCK);
 }
 
 }  // namespace
