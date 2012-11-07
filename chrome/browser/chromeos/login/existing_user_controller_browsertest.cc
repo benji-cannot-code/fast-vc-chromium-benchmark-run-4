b2dc0721d8cac60fb7d64a6036d2f674cbec7951
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 16:27:46 +0000

    Don't disable keyboard input at the OOBE UI if auto-enrollment is going to be performed.
    BUG=159472
    
    
    Review URL: https://chromiumcodereview.appspot.com/11312121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166444 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/existing_user_controller_browsertest.cc
+++ chrome/browser/chromeos/login/existing_user_controller_browsertest.cc
@@ -40,6 +40,7 @@ using ::testing::Invoke;
 using ::testing::InvokeWithoutArgs;
 using ::testing::Return;
 using ::testing::ReturnNull;
+using ::testing::Sequence;
 using ::testing::WithArg;
 
 namespace chromeos {
@@ -283,6 +284,10 @@ IN_PROC_BROWSER_TEST_F(ExistingUserControllerTest, AutoEnrollAfterSignIn) {
   EXPECT_CALL(*mock_user_manager_.user_manager(), IsCurrentUserNew())
       .Times(AnyNumber())
       .WillRepeatedly(Return(false));
+  // The UI should not be disabled if the enrollment screen is shown after
+  // the user signs in.
+  EXPECT_CALL(*mock_login_display_, SetUIEnabled(_))
+      .Times(0);
   existing_user_controller()->DoAutoEnrollment();
   existing_user_controller()->CompleteLogin(kUsername, kPassword);
   content::RunAllPendingInMessageLoop();
@@ -317,6 +322,17 @@ IN_PROC_BROWSER_TEST_F(ExistingUserControllerTest,
   EXPECT_CALL(*mock_user_manager_.user_manager(), IsCurrentUserNew())
       .Times(AnyNumber())
       .WillRepeatedly(Return(true));
+
+  // The order of these expected calls matters: the UI if first disabled
+  // during the login sequence, and is enabled again after login completion.
+  Sequence uiEnabledSequence;
+  EXPECT_CALL(*mock_login_display_, SetUIEnabled(false))
+      .Times(1)
+      .InSequence(uiEnabledSequence);
+  EXPECT_CALL(*mock_login_display_, SetUIEnabled(true))
+      .Times(1)
+      .InSequence(uiEnabledSequence);
+
   existing_user_controller()->CompleteLogin(kNewUsername, kPassword);
   content::RunAllPendingInMessageLoop();
 }
