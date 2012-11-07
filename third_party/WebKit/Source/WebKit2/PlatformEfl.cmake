00ade06db5904f1aa9fa8d613ff5e94361b468d7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 00:32:54 +0000

    [EFL][WK2] Add ewk_database_manager APIs https://bugs.webkit.org/show_bug.cgi?id=101193
    Patch by Jihye Kang <jye.kang@samsung.com> on 2012-11-06
    Reviewed by Gyuyoung Kim.
    
    Add ewk_database_manager to manage web database and
    ewk_database_manaager_origins_get to get origins list of web storage.
    Add unit tests for ewk_database_manager APIs.
    
    * PlatformEfl.cmake:
    * UIProcess/API/efl/ewk_context.cpp:
    (Ewk_Context::Ewk_Context):
    (Ewk_Context::databaseManager):
    (ewk_context_database_manager_get):
    * UIProcess/API/efl/ewk_context.h:
    * UIProcess/API/efl/ewk_context_private.h:
    (Ewk_Context):
    * UIProcess/API/efl/ewk_database_manager.cpp: Added.
    (Ewk_Database_Manager::Ewk_Database_Manager):
    (Ewk_Database_Manager::getDatabaseOrigins):
    (Ewk_Database_Manager::createOriginList):
    (Ewk_Database_Origins_Async_Get_Context):
    (Ewk_Database_Origins_Async_Get_Context::Ewk_Database_Origins_Async_Get_Context):
    (getDatabaseOriginsCallback):
    (ewk_database_manager_origins_get):
    * UIProcess/API/efl/ewk_database_manager.h: Added.
    * UIProcess/API/efl/ewk_database_manager_private.h: Added.
    (Ewk_Database_Manager):
    (Ewk_Database_Manager::create):
    * UIProcess/API/efl/tests/test_ewk2_context.cpp:
    (TEST_F):
    * UIProcess/API/efl/tests/test_ewk2_database_manager.cpp: Added.
    (OriginData):
    (OriginData::OriginData):
    (getDatabaseOriginsCallback):
    (timerCallback):
    (TEST_F):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133692 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
+++ third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
@@ -51,6 +51,7 @@ LIST(APPEND WebKit2_SOURCES
     UIProcess/API/efl/ewk_color_picker.cpp
     UIProcess/API/efl/ewk_context.cpp
     UIProcess/API/efl/ewk_cookie_manager.cpp
+    UIProcess/API/efl/ewk_database_manager.cpp
     UIProcess/API/efl/ewk_download_job.cpp
     UIProcess/API/efl/ewk_error.cpp
     UIProcess/API/efl/ewk_favicon_database.cpp
@@ -255,6 +256,7 @@ SET (EWebKit2_HEADERS
     "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_color_picker.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_context.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_cookie_manager.h"
+    "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_database_manager.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_download_job.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_error.h"
     "${CMAKE_CURRENT_SOURCE_DIR}/UIProcess/API/efl/ewk_favicon_database.h"
@@ -362,6 +364,7 @@ SET(EWK2UnitTests_BINARIES
     test_ewk2_context
     test_ewk2_context_history_callbacks
     test_ewk2_cookie_manager
+    test_ewk2_database_manager
     test_ewk2_download_job
     test_ewk2_eina_shared_string
     test_ewk2_favicon_database
--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EWebKit2.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EWebKit2.h
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ewk_color_picker.h"
 #include "ewk_context.h"
 #include "ewk_cookie_manager.h"
+#include "ewk_database_manager.h"
 #include "ewk_download_job.h"
 #include "ewk_error.h"
 #include "ewk_favicon_database.h"
