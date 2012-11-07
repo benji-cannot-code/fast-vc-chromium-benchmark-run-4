2a3848cf6bd34d7f4b8ce3153fd4e662f74f4eb6
yosin@chromium.org <yosin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 12:05:30 +0000

    CanShowBookmarkPrompt() is called during construction of Browser object before initializing window_ member variable by calling CreateBrowserWindow().
    CreateBrowserWindow() calls Windows API CreateWindowEx() and it dispatches WM_NCACTIVATE.
    
    It seems some tools, such as RocketDoc(http://rocketdock.com/) and Matrox PowrDesk(http://www.matrox.com/graphics/en/products/multi_display_software/), are involved this symptom.
    
    BUG=159146
    
    
    Review URL: https://chromiumcodereview.appspot.com/11361091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166385 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/bookmarks/bookmark_prompt_controller.cc
+++ chrome/browser/ui/bookmarks/bookmark_prompt_controller.cc
@@ -49,11 +49,6 @@ enum PromptDisplayReason {
 };
 
 bool CanShowBookmarkPrompt(Browser* browser) {
-  if (!browser || browser->type() != Browser::TYPE_TABBED ||
-      browser->profile()->IsOffTheRecord() ||
-      !browser->SupportsWindowFeature(Browser::FEATURE_LOCATIONBAR) ||
-      !browser->window()->GetLocationBar())
-    return false;
   BookmarkPromptPrefs prefs(browser->profile()->GetPrefs());
   if (!prefs.IsBookmarkPromptEnabled())
     return false;
@@ -221,7 +216,13 @@ void BookmarkPromptController::OnBrowserRemoved(Browser* browser) {
 }
 
 void BookmarkPromptController::OnBrowserSetLastActive(Browser* browser) {
-  SetBrowser(CanShowBookmarkPrompt(browser) ? browser : NULL);
+  if (browser && browser->type() == Browser::TYPE_TABBED &&
+      !browser->profile()->IsOffTheRecord() &&
+      browser->CanSupportWindowFeature(Browser::FEATURE_LOCATIONBAR) &&
+      CanShowBookmarkPrompt(browser))
+    SetBrowser(browser);
+  else
+    SetBrowser(NULL);
 }
 
 void BookmarkPromptController::OnDidQueryURL(
@@ -242,7 +243,8 @@ void BookmarkPromptController::OnDidQueryURL(
   UMA_HISTOGRAM_TIMES("BookmarkPrompt.QueryURLDuration",
                       base::Time::Now() - query_url_start_time_);
 
-  if (!CanShowBookmarkPrompt(browser_) ||
+  if (!browser_->SupportsWindowFeature(Browser::FEATURE_LOCATIONBAR) ||
+      !CanShowBookmarkPrompt(browser_) ||
       !IsActiveWebContents(browser_, web_contents_) ||
       IsBookmarked(browser_, url))
     return;
