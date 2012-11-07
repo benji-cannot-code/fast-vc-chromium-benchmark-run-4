e575981ddf3bb077b277b4a154f53517bcccd5a4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 13:53:57 +0000

    Web Inspector: [Canvas] Save CanvasGradient objects state for a replay https://bugs.webkit.org/show_bug.cgi?id=101432
    Patch by Andrey Adaikin <aandrey@chromium.org> on 2012-11-07
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Save CanvasGradient resource state for a subsequent replay. The gradient's state is modified by the gradient.addColorStop() call.
    
    Test: inspector/profiler/canvas2d/canvas2d-gradient-capturing.html
    
    * inspector/InjectedScriptCanvasModuleSource.js:
    (.):
    
    LayoutTests:
    
    Alerts about API changes to CanvasGradient and CanvasPattern classes.
    Checks capturing for CanvasGradient objects' state.
    
    * inspector/profiler/canvas2d/canvas2d-api-changes-expected.txt:
    * inspector/profiler/canvas2d/canvas2d-api-changes.html:
    * inspector/profiler/canvas2d/canvas2d-gradient-capturing-expected.txt: Added.
    * inspector/profiler/canvas2d/canvas2d-gradient-capturing.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InjectedScriptCanvasModuleSource.js
+++ third_party/WebKit/Source/WebCore/inspector/InjectedScriptCanvasModuleSource.js
@@ -880,11 +880,11 @@ Resource.prototype = {
     },
 
     /**
-     * @return {Object.<string, Function>}
+     * @return {!Object.<string, Function>}
      */
     _customWrapFunctions: function()
     {
-        return {}; // May be overridden by subclasses.
+        return Object.create(null); // May be overridden by subclasses.
     }
 }
 
@@ -993,6 +993,39 @@ ReplayableResource.prototype = {
 ReplayableResource.replay = function(obj, cache)
 {
     return (obj instanceof ReplayableResource) ? obj.replay(cache).wrappedObject() : obj;
+}
+
+/**
+ * @constructor
+ * @extends {Resource}
+ */
+function LogEverythingResource(wrappedObject)
+{
+    Resource.call(this, wrappedObject);
+}
+
+LogEverythingResource.prototype = {
+    /**
+     * @override
+     * @return {!Object.<string, Function>}
+     */
+    _customWrapFunctions: function()
+    {
+        var wrapFunctions = Object.create(null);
+        var wrappedObject = this.wrappedObject();
+        if (wrappedObject) {
+            for (var property in wrappedObject) {
+                /** @this Resource.WrapFunction */
+                wrapFunctions[property] = function()
+                {
+                    this._resource.pushCall(this.call());
+                }
+            }
+        }
+        return wrapFunctions;
+    },
+
+    __proto__: Resource.prototype
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1172,12 +1205,11 @@ WebGLTextureResource.prototype = {
         this.pushCall(call);
     },
 
-    pushCall_texParameteri: WebGLTextureResource.prototype.pushCall_texParameterf,
-
-    pushCall_copyTexSubImage2D: WebGLTextureResource.prototype.pushCall_copyTexImage2D,
-
     __proto__: WebGLBoundResource.prototype
 }
+
+WebGLTextureResource.prototype.pushCall_texParameteri = WebGLTextureResource.prototype.pushCall_texParameterf;
+WebGLTextureResource.prototype.pushCall_copyTexSubImage2D = WebGLTextureResource.prototype.pushCall_copyTexImage2D;
 
 /**
  * @constructor
@@ -1788,7 +1820,7 @@ WebGLRenderingContextResource.prototype = {
 
     /**
      * @override
-     * @return {Object.<string, Function>}
+     * @return {!Object.<string, Function>}
      */
     _customWrapFunctions: function()
     {
@@ -2192,7 +2224,7 @@ CanvasRenderingContext2DResource.prototype = {
 
     /**
      * @override
-     * @return {Object.<string, Function>}
+     * @return {!Object.<string, Function>}
      */
     _customWrapFunctions: function()
     {
@@ -2200,10 +2232,9 @@ CanvasRenderingContext2DResource.prototype = {
         if (!wrapFunctions) {
             wrapFunctions = Object.create(null);
 
-            // FIXME: Save state of the CanvasGradient objects.
-            wrapFunctions["createLinearGradient"] = Resource.WrapFunction.resourceFactoryMethod(Resource);
-            wrapFunctions["createRadialGradient"] = Resource.WrapFunction.resourceFactoryMethod(Resource);
-            wrapFunctions["createPattern"] = Resource.WrapFunction.resourceFactoryMethod(Resource);
+            wrapFunctions["createLinearGradient"] = Resource.WrapFunction.resourceFactoryMethod(LogEverythingResource);
+            wrapFunctions["createRadialGradient"] = Resource.WrapFunction.resourceFactoryMethod(LogEverythingResource);
+            wrapFunctions["createPattern"] = Resource.WrapFunction.resourceFactoryMethod(LogEverythingResource);
 
             /**
              * @param {string} methodName
