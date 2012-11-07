b2b173d29a0c28ee306bff13f22cfc7ff327e47a
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 16:12:53 +0000

    [Qt] Open link in this window action https://bugs.webkit.org/show_bug.cgi?id=101226
    Reviewed by Simon Hausmann.
    
    Source/WebCore:
    
    Adds the action to the WebCore context-menu controller.
    
    * page/ContextMenuController.cpp:
    (WebCore::ContextMenuController::contextMenuItemSelected):
    (WebCore::ContextMenuController::checkOrEnableIfNeeded):
    * platform/ContextMenuItem.h:
    * platform/LocalizedStrings.h:
    * platform/qt/LocalizedStringsQt.cpp:
    (WebCore::contextMenuItemTagOpenLinkInThisWindow):
    
    Source/WebKit/qt:
    
    Adds the action to QtWebKit.
    
    * Api/qwebpage.cpp:
    (webActionForContextMenuAction):
    (QWebPage::triggerAction):
    (QWebPage::action):
    * Api/qwebpage.h:
    
    Tools:
    
    Populates the context-menu with the new action when the default action is to open in a new window.
    
    * QtTestBrowser/webview.cpp:
    (createContextMenu):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/LocalizedStrings.h
+++ third_party/WebKit/Source/WebCore/platform/LocalizedStrings.h
@@ -50,6 +50,9 @@ namespace WebCore {
 
 #if ENABLE(CONTEXT_MENUS)
     String contextMenuItemTagOpenLinkInNewWindow();
+#if PLATFORM(QT)
+    String contextMenuItemTagOpenLinkInThisWindow();
+#endif
     String contextMenuItemTagDownloadLinkToDisk();
     String contextMenuItemTagCopyLinkToClipboard();
     String contextMenuItemTagOpenImageInNewWindow();
