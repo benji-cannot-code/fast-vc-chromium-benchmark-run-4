53495d6dea2d40c956c35cf964b6072e35bc88c3
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 18:26:18 +0000

    IndexedDB: Assertion failure with open() within upgradeneeded https://bugs.webkit.org/show_bug.cgi?id=96947
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Postpone creation of the "pending second half open" until the version change
    transaction has started.
    
    Test: storage/indexeddb/unblocked-version-changes.html
    
    * Modules/indexeddb/IDBDatabaseBackendImpl.cpp:
    (WebCore::IDBDatabaseBackendImpl::setIntVersionInternal):
    (WebCore::IDBDatabaseBackendImpl::runIntVersionChangeTransaction):
    * Modules/indexeddb/IDBDatabaseBackendImpl.h:
    (IDBDatabaseBackendImpl):
    * Modules/indexeddb/IDBDatabaseCallbacks.h:
    
    LayoutTests:
    
    Exercise the code path leading to the assert by having a second version change transaction
    unblocked by the first's connection closing. Includes a known failure due to metadata
    snapshot timing.
    
    * storage/indexeddb/resources/unblocked-version-changes.js: Added.
    (test):
    (openConnection):
    (onUpgradeNeeded):
    (onError):
    (onUpgradeNeeded2):
    (onSuccess):
    * storage/indexeddb/unblocked-version-changes-expected.txt: Added.
    * storage/indexeddb/unblocked-version-changes.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133776 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBDatabaseCallbacks.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBDatabaseCallbacks.h
@@ -35,7 +35,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class IDBDatabaseCallbacks : public RefCounted<IDBDatabaseCallbacks> {
+// FIXME: Uses ThreadSafeRefCounted for storage in ScriptExecutionContext::Task but
+// it is never actually used on multiple threads. http://webkit.org/b/101483
+class IDBDatabaseCallbacks : public ThreadSafeRefCounted<IDBDatabaseCallbacks> {
 public:
     virtual ~IDBDatabaseCallbacks() { }
 
