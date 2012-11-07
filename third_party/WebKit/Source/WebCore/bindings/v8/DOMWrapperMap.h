a9a550e0ba5cb30be021562367f6a7e4f6f99cce
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 21:45:32 +0000

    [V8] DOMWrapperMap doesn't need to use virtual functions https://bugs.webkit.org/show_bug.cgi?id=101494
    Reviewed by Kentaro Hara.
    
    DOMWrapperMap has only one subclass. There's no reason to use virtual functions.
    
    * bindings/v8/DOMDataStore.cpp:
    (WebCore::DOMDataStore::DOMDataStore):
    * bindings/v8/DOMWrapperMap.h:
    (WebCore::DOMWrapperMap::DOMWrapperMap):
    (WebCore::DOMWrapperMap::get):
    (WebCore::DOMWrapperMap::set):
    (WebCore::DOMWrapperMap::clear):
    (WebCore::DOMWrapperMap::reportMemoryUsage):
    (WebCore::DOMWrapperMap::remove):
    (WebCore::DOMWrapperMap::defaultWeakCallback):
    * bindings/v8/V8NPObject.cpp:
    (WebCore::staticNPObjectMap):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/DOMWrapperMap.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/DOMWrapperMap.h
@@ -40,36 +40,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class DOMDataStore;
-
 template<class KeyType>
 class DOMWrapperMap {
 public:
-    virtual ~DOMWrapperMap() { }
-
-    virtual v8::Persistent<v8::Object> get(KeyType*) = 0;
-    virtual void set(KeyType*, v8::Persistent<v8::Object>) = 0;
-    virtual void clear() = 0;
-
-    virtual void reportMemoryUsage(MemoryObjectInfo*) const = 0;
-};
-
-template<class KeyType>
-class DOMWrapperHashMap : public DOMWrapperMap<KeyType> {
-public:
     typedef HashMap<KeyType*, v8::Persistent<v8::Object> > MapType;
 
-    explicit DOMWrapperHashMap(v8::WeakReferenceCallback callback = &defaultWeakCallback)
+    explicit DOMWrapperMap(v8::WeakReferenceCallback callback = &defaultWeakCallback)
         : m_callback(callback)
     {
     }
 
-    virtual v8::Persistent<v8::Object> get(KeyType* key) OVERRIDE
+    v8::Persistent<v8::Object> get(KeyType* key)
     {
         return m_map.get(key);
     }
 
-    virtual void set(KeyType* key, v8::Persistent<v8::Object> wrapper) OVERRIDE
+    void set(KeyType* key, v8::Persistent<v8::Object> wrapper)
     {
         ASSERT(!m_map.contains(key));
         ASSERT(static_cast<KeyType*>(toNative(wrapper)) == key);
@@ -77,7 +63,7 @@ public:
         m_map.set(key, wrapper);
     }
 
-    virtual void clear() OVERRIDE
+    void clear()
     {
         for (typename MapType::iterator it = m_map.begin(); it != m_map.end(); ++it) {
             v8::Persistent<v8::Object> wrapper = it->value;
@@ -88,13 +74,13 @@ public:
         m_map.clear();
     }
 
-    virtual void reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const OVERRIDE
+    void reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) const
     {
         MemoryClassInfo info(memoryObjectInfo, this, WebCoreMemoryTypes::Binding);
         info.addMember(m_map);
     }
 
-    virtual void remove(KeyType* key, v8::Persistent<v8::Object> wrapper)
+    void remove(KeyType* key, v8::Persistent<v8::Object> wrapper)
     {
         typename MapType::iterator it = m_map.find(key);
         ASSERT(it != m_map.end());
@@ -105,7 +91,7 @@ public:
 private:
     static void defaultWeakCallback(v8::Persistent<v8::Value> value, void* context)
     {
-        DOMWrapperHashMap<KeyType>* map = static_cast<DOMWrapperHashMap<KeyType>*>(context);
+        DOMWrapperMap<KeyType>* map = static_cast<DOMWrapperMap<KeyType>*>(context);
         ASSERT(value->IsObject());
         v8::Persistent<v8::Object> wrapper = v8::Persistent<v8::Object>::Cast(value);
         WrapperTypeInfo* type = toWrapperTypeInfo(wrapper);
