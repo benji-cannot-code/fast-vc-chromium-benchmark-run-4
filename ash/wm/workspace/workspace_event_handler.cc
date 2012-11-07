e64932b03b9cfef038d49b0a88e6b8f74a621aab
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-07 16:02:20 +0000

    ui: Remove event_functions.* component.
    Instead move the only function from there to event_utils.h
    
    R=ben@chromium.org,sadrul@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11360118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166443 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_event_handler.cc
+++ ash/wm/workspace/workspace_event_handler.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/window.h"
 #include "ui/aura/window_delegate.h"
 #include "ui/base/events/event.h"
-#include "ui/base/events/event_functions.h"
+#include "ui/base/events/event_utils.h"
 #include "ui/base/hit_test.h"
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/screen.h"
