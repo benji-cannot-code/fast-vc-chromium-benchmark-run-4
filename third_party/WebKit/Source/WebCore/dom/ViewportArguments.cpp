cae14a59267049b23d84300e09a27e526a229248
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-07 08:54:46 +0000

    Remove support for "desktop-width" in the viewport meta tag https://bugs.webkit.org/show_bug.cgi?id=101217
    Reviewed by Gyuyoung Kim.
    
    Source/WebCore:
    
    This has been removed from the CSS Device Adaptation spec, and
    should be safe to remove as it is not documented anywhere (neither
    in blog posts).
    
    The viewport meta should only support the legacy features.
    
    * dom/ViewportArguments.cpp:
    (WebCore::computeViewportAttributes):
    (WebCore::findSizeValue):
    (WebCore::findScaleValue):
    (WebCore::findUserScalableValue):
    * dom/ViewportArguments.h:
    
    Source/WebKit/blackberry:
    
    Sync WebViewportArguments enum with the one in
    WebCore::ViewportArguments
    
    * Api/WebViewportArguments.h:
    
    Source/WebKit/chromium:
    
    Remove some code trying to use the set desktop width (say 980) for
    sites not using a viewport meta element.
    
    Removing this code should have no effect as it ends up being set
    to desktop width in that case anyway, by the ViewportArguments
    algorithm.
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::dispatchViewportPropertiesDidChange):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ViewportArguments.cpp
+++ third_party/WebKit/Source/WebCore/dom/ViewportArguments.cpp
@@ -62,9 +62,6 @@ ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktop
     }
 
     switch (int(args.width)) {
-    case ViewportArguments::ValueDesktopWidth:
-        args.width = desktopWidth;
-        break;
     case ViewportArguments::ValueDeviceWidth:
         args.width = deviceWidth;
         break;
@@ -74,9 +71,6 @@ ViewportAttributes computeViewportAttributes(ViewportArguments args, int desktop
     }
 
     switch (int(args.height)) {
-    case ViewportArguments::ValueDesktopWidth:
-        args.height = desktopWidth;
-        break;
     case ViewportArguments::ValueDeviceWidth:
         args.height = deviceWidth;
         break;
@@ -219,8 +213,6 @@ static float findSizeValue(const String& keyString, const String& valueString, D
     // 3) device-width and device-height are used as keywords.
     // 4) Other keywords and unknown values translate to 0.0.
 
-    if (equalIgnoringCase(valueString, "desktop-width"))
-        return ViewportArguments::ValueDesktopWidth;
     if (equalIgnoringCase(valueString, "device-width"))
         return ViewportArguments::ValueDeviceWidth;
     if (equalIgnoringCase(valueString, "device-height"))
@@ -246,8 +238,6 @@ static float findScaleValue(const String& keyString, const String& valueString, 
         return 1;
     if (equalIgnoringCase(valueString, "no"))
         return 0;
-    if (equalIgnoringCase(valueString, "desktop-width"))
-        return 10;
     if (equalIgnoringCase(valueString, "device-width"))
         return 10;
     if (equalIgnoringCase(valueString, "device-height"))
@@ -274,8 +264,6 @@ static float findUserScalableValue(const String& keyString, const String& valueS
         return 1;
     if (equalIgnoringCase(valueString, "no"))
         return 0;
-    if (equalIgnoringCase(valueString, "desktop-width"))
-        return 1;
     if (equalIgnoringCase(valueString, "device-width"))
         return 1;
     if (equalIgnoringCase(valueString, "device-height"))
