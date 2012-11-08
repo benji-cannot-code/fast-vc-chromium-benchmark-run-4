420b04d155c1f6eeaabcd7056eabe6bee3abb550
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 13:06:19 +0000

    Add Closure-based API to PrefChangeObserver and PrefMember, deprecate PrefObserver-based API.
    This switches the API with minimal implementation changes; PrefNotifierImpl still uses PrefObserver to accept registrations, and still filters on pref names (which PrefChangeObserver looks up again when it receives callbacks via its PrefObserver implementation).
    
    This approach is chosen for now to establish the new API so that usages can be switched, because:
    
    a) We need a way for PrefNotifierImpl to dispatch to both PrefChangeObserver and PrefMember, and it's unclear to me whether we want to switch that interface to base::Callback as well (if so, how to unregister?), or whether we want PrefMember to have a PrefChangeObserver instance (probably inefficient?) or something else.
    
    b) There are plans to do performance measurements of a few different implementation approaches in how PrefNotifierImpl and PrefChangeObserver interact; that interaction can be changed "under the hood" while the new API stays unchanged, and this lets us start switching users to the new API and removing the now-deprecated PrefObserver-based API.
    
    TBR=ben@chromium.org,finnur@chromium.org
    BUG=155525
    
    Review URL: https://chromiumcodereview.appspot.com/11368098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166670 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/api/prefs/pref_member.h
+++ chrome/browser/api/prefs/pref_member.h
@@ -29,6 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/bind.h"
+#include "base/callback_forward.h"
 #include "base/file_path.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
@@ -92,7 +94,8 @@ class PrefMemberBase : public PrefObserver {
 
   // See PrefMember<> for description.
   void Init(const char* pref_name, PrefServiceBase* prefs,
-            PrefObserver* observer);
+            const base::Closure& observer);
+  void Init(const char* pref_name, PrefServiceBase* prefs);
 
   virtual void CreateInternal() const = 0;
 
@@ -127,7 +130,7 @@ class PrefMemberBase : public PrefObserver {
  private:
   // Ordered the members to compact the class instance.
   std::string pref_name_;
-  PrefObserver* observer_;
+  base::Closure observer_;  // Initially bound to base::DoNothing.
   PrefServiceBase* prefs_;
 
  protected:
@@ -149,12 +152,28 @@ class PrefMember : public subtle::PrefMemberBase {
   PrefMember() {}
   virtual ~PrefMember() {}
 
-  // Do the actual initialization of the class.  |observer| may be null if you
-  // don't want any notifications of changes.
-  // This method should only be called on the UI thread.
+  // Do the actual initialization of the class.  Use the two-parameter
+  // version if you don't want any notifications of changes.  This
+  // method should only be called on the UI thread.
+  void Init(const char* pref_name, PrefServiceBase* prefs,
+            const base::Closure& observer) {
+    subtle::PrefMemberBase::Init(pref_name, prefs, observer);
+  }
+
+  void Init(const char* pref_name, PrefServiceBase* prefs) {
+    subtle::PrefMemberBase::Init(pref_name, prefs);
+  }
+
+  // Deprecated version of Init.
   void Init(const char* pref_name, PrefServiceBase* prefs,
             PrefObserver* observer) {
-    subtle::PrefMemberBase::Init(pref_name, prefs, observer);
+    if (observer) {
+      Init(pref_name, prefs, base::Bind(&PrefObserver::OnPreferenceChanged,
+                                        base::Unretained(observer),
+                                        prefs, std::string(pref_name)));
+    } else {
+      Init(pref_name, prefs);
+    }
   }
 
   // Unsubscribes the PrefMember from the PrefService. After calling this
