15be319b15ff2f5c96c6b5280dfca133f3901fba
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 08:09:58 +0000

    Drive: Re-initialize DriveOprtations after ResourceMetadata is reset()'d.
    This fixes the potential invalid dereference.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/11347009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_file_system.cc
+++ chrome/browser/chromeos/drive/drive_file_system.cc
@@ -325,7 +325,7 @@ DriveFileSystem::DriveFileSystem(
 }
 
 void DriveFileSystem::Reload() {
-  InitializeResourceMetadataAndFeedLoader();
+  ResetResourceMetadata();
 
   feed_loader_->ReloadFromServerIfNeeded(
       base::Bind(&DriveFileSystem::NotifyInitialLoadFinishedAndRun,
@@ -339,16 +339,7 @@ void DriveFileSystem::Initialize() {
 
   drive_service_->Initialize(profile_);
 
-  InitializeResourceMetadataAndFeedLoader();
-
-  // Allocate the drive operation handlers.
-  drive_operations_.Init(drive_service_,
-                         this,  // DriveFileSystemInterface
-                         cache_,
-                         resource_metadata_.get(),
-                         uploader_,
-                         blocking_task_runner_,
-                         this);  // OperationObserver
+  ResetResourceMetadata();
 
   PrefService* pref_service = profile_->GetPrefs();
   hide_hosted_docs_ = pref_service->GetBoolean(prefs::kDisableDriveHostedFiles);
@@ -358,7 +349,7 @@ void DriveFileSystem::Initialize() {
   InitializePreferenceObserver();
 }
 
-void DriveFileSystem::InitializeResourceMetadataAndFeedLoader() {
+void DriveFileSystem::ResetResourceMetadata() {
   resource_metadata_.reset(new DriveResourceMetadata);
   feed_loader_.reset(new DriveFeedLoader(resource_metadata_.get(),
                                          drive_service_,
@@ -366,6 +357,15 @@ void DriveFileSystem::InitializeResourceMetadataAndFeedLoader() {
                                          cache_,
                                          blocking_task_runner_));
   feed_loader_->AddObserver(this);
+
+  // Allocate the drive operation handlers.
+  drive_operations_.Init(drive_service_,
+                         this,  // DriveFileSystemInterface
+                         cache_,
+                         resource_metadata_.get(),
+                         uploader_,
+                         blocking_task_runner_,
+                         this);  // OperationObserver
 }
 
 void DriveFileSystem::CheckForUpdates() {
