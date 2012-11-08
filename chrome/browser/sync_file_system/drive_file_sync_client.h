465496eb6e4063afbf6564a2ce1e9181522a4e7a
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 10:09:02 +0000

    Fix title query escape in DriveFileSyncClient.
    The parameter of 'title:' query on Document List API needs escape as follows:
    - ' to \'
    - \ to \\
    
    BUG=157815
    
    Review URL: https://chromiumcodereview.appspot.com/11366145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166648 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_file_sync_client.h
+++ chrome/browser/sync_file_system/drive_file_sync_client.h
@@ -90,6 +90,8 @@ class DriveFileSyncClient : public base::NonThreadSafe,
                        const DocumentFeedCallback& callback);
 
  private:
+  friend class DriveFileSyncClientTest;
+
   // Constructor for test use.
   DriveFileSyncClient(
       Profile* profile,
@@ -123,6 +125,8 @@ class DriveFileSyncClient : public base::NonThreadSafe,
   void DidGetDocumentFeedData(const DocumentFeedCallback& callback,
                               google_apis::GDataErrorCode error,
                               scoped_ptr<base::Value> data);
+
+  static std::string FormatTitleQuery(const std::string& title);
 
   scoped_ptr<google_apis::DriveServiceInterface> drive_service_;
   scoped_ptr<google_apis::DriveUploaderInterface> drive_uploader_;
