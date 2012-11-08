44da6710f01866a961b38f99457364c920edbbe3
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 19:53:40 +0000

    Chrome remote control multipage tests: Add interactions.
    The page set .json description defines what kind of interactions (e.g., link
    clicks) are possible for a page. The corresponding Web Page Replay .wpr file
    must contain replies to the requests sent when the interaction is executed.
    
    This will be used in memory tests (see
    http://codereview.chromium.org/11273081/ ).
    
    The top_25_usage.json is a proof of concept page set description adding a couple
    of interactions. When it's more complete, the existing tests will be migrated to
    use it.
    
    Parts of the code are stolen from nduca@chromium.org (see
    http://codereview.chromium.org/11114020/ ).
    
    BUG=158323
    
    Review URL: https://codereview.chromium.org/11369075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166734 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/compound_interaction.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+from telemetry import page_interaction
+
+class CompoundInteraction(page_interaction.PageInteraction):
+  def __init__(self, attributes=None):
+    super(CompoundInteraction, self).__init__(attributes)
+
+  def PerformInteraction(self, page, tab):
+    for interaction_data in self.actions:
+      interaction = page_interaction.FindClassWithName(
+          interaction_data['action'])(interaction_data)
+      interaction.PerformInteraction(page, tab)
+
+
+page_interaction.RegisterClass('compound', CompoundInteraction)
