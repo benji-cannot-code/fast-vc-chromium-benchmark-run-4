962cb2594512b1505f7fa3e754d68ffc3e576812
scottmg@google.com <scottmg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 01:34:31 +0000

    Revert 166570 - [views] continue hacking away at autofill dialog (#4).
    Failed to compile on cros:
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20%28Clang%20dbg%29/builds/23814
    
    
    in this CL, accept inputs (which fields a page asks for) and collect outputs (values for those fields).
    
    The input data structure is a vector of AutofillFieldType (order is not important). The output structure is a mapping from these field types to string values supplied by the user (or filled in by suggestion). This input and output format will need to be supplied and accepted by the autofill agent.
    
    BUG=157270, 157273
    
    Review URL: https://codereview.chromium.org/11369087
    
    TBR=estade@chromium.org
    Review URL: https://codereview.chromium.org/11361162
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166576 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/autofill_dialog_views.h
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_VIEWS_AUTOFILL_AUTOFILL_DIALOG_VIEWS_H_
 #define CHROME_BROWSER_UI_VIEWS_AUTOFILL_AUTOFILL_DIALOG_VIEWS_H_
 
-#include "chrome/browser/ui/autofill/autofill_dialog_controller.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_view.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/combobox/combobox_listener.h"
@@ -16,7 +15,6 @@ class ConstrainedWindowViews;
 
 namespace views {
 class Checkbox;
-class Textfield;
 }
 
 namespace ui {
@@ -25,6 +23,7 @@ class ComboboxModel;
 
 namespace autofill {
 
+class AutofillDialogController;
 struct DetailInput;
 
 // Views toolkit implementation of the Autofill dialog that handles the
@@ -39,10 +38,6 @@ class AutofillDialogViews : public AutofillDialogView,
 
   // AutofillDialogView implementation:
   virtual void Show() OVERRIDE;
-  virtual int GetSuggestionSelection(DialogSection section) OVERRIDE;
-  virtual void GetUserInput(DialogSection section,
-                            DetailOutputMap* output) OVERRIDE;
-  virtual bool UseBillingForShipping() OVERRIDE;
 
   // views::DialogDelegate implementation:
   virtual string16 GetWindowTitle() const OVERRIDE;
@@ -64,8 +59,6 @@ class AutofillDialogViews : public AutofillDialogView,
   virtual void OnSelectedIndexChanged(views::Combobox* combobox) OVERRIDE;
 
  private:
-  typedef std::map<AutofillFieldType, views::Textfield*> TextfieldMap;
-
   // A convenience struct for holding pointers to views within each detail
   // section. None of the member pointers are owned.
   struct DetailsGroup {
@@ -77,8 +70,6 @@ class AutofillDialogViews : public AutofillDialogView,
     views::Combobox* suggested_input;
     // The view that allows manual input.
     views::View* manual_input;
-    // The textfields in |manual_input|, tracked by their AutofillFieldType.
-    TextfieldMap textfields;
   };
 
   void InitChildViews();
@@ -87,34 +78,25 @@ class AutofillDialogViews : public AutofillDialogView,
   views::View* CreateDetailsContainer();
 
   // Creates a detail section (Shipping, Billing, etc.) with the given label,
-  // inputs View, and suggestion model. Relevant pointers are stored in |group|.
-  void CreateDetailsSection(const string16& label,
-                            views::View* inputs,
-                            ui::ComboboxModel* model,
-                            DetailsGroup* group);
+  // inputs View, and suggestion model.
+  DetailsGroup CreateDetailsSection(const string16& label,
+                                    views::View* inputs,
+                                    ui::ComboboxModel* model);
 
   // These functions create the views that hold inputs for the section.
   views::View* CreateEmailInputs();
   views::View* CreateBillingInputs();
   views::View* CreateShippingInputs();
 
-  // Creates a grid of textfield views for the given section, and stores them
-  // in the appropriate DetailsGroup. The top level View in the hierarchy is
-  // returned.
-  views::View* InitInputsView(DialogSection section);
+  // Reads a DetailInput array and creates inputs in a grid.
+  views::View* InitInputsFromTemplate(const DetailInput* inputs,
+                                      size_t inputs_len);
 
   // Updates the visual state of the given group as per the model.
   void UpdateDetailsGroupState(const DetailsGroup& group);
 
-  // Gets a pointer to the DetailsGroup that's associated with the given section
-  // of the dialog.
-  DetailsGroup* GroupForSection(DialogSection section);
-
   // The controller that drives this view. Weak pointer, always non-NULL.
   AutofillDialogController* const controller_;
-
-  // True if the termination action was a submit.
-  bool did_submit_;
 
   // The window that displays |contents_|. Weak pointer; may be NULL when the
   // dialog is closing.
