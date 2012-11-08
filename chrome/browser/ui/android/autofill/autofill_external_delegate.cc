bb1c856a2387008ebac5fd635fe0870c8c025bd1
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 01:06:05 +0000

    Temporarily fix NOTREACHED in autofill code for Android
    BUG=159809
    
    
    Review URL: https://chromiumcodereview.appspot.com/11362141
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/android/autofill/autofill_external_delegate.cc
+++ chrome/browser/ui/android/autofill/autofill_external_delegate.cc
@@ -22,6 +22,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using base::android::MethodID;
 using content::BrowserThread;
 
+void AutofillExternalDelegateAndroid::ClearPreviewedForm() {
+  // Intentionally blank as Autofill preview is unused on Android
+  // TODO(aurimas): Clean this up (crbug.com/159809)
+}
+
 void AutofillExternalDelegate::CreateForWebContentsAndManager(
     content::WebContents* web_contents,
     AutofillManager* autofill_manager) {
