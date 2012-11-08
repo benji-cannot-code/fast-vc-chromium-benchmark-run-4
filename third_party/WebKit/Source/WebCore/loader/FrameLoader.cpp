c8439b410adc46dbba38e29a61e664681bde976e
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-08 09:43:17 +0000

    Warn when parsing an invalid X-Frame-Options header. https://bugs.webkit.org/show_bug.cgi?id=101447
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    An 'X-Frame-Options' header that contains an invalid option (that is,
    neither 'DENY' nor 'SAMEORIGIN') is ignored. This patch adds a console
    warning to notify developers that they've made a mistake.
    
    Test: http/tests/security/XFrameOptions/x-frame-options-invalid.html
    
    * dom/Document.cpp:
    (WebCore::Document::processHttpEquiv):
        Move the request identifier generation out of the failure block in
        order to pass it into 'shouldInterruptLoadForXFrameOptions'. This
        ensures that the console message is properly tied to a request.
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::shouldInterruptLoadForXFrameOptions):
    * loader/FrameLoader.h:
    (FrameLoader):
        'shouldInterruptLoadForXFrameOptions' now accepts a request
        identifier as a parameter, and generates a console message if the
        load is blocked.
    * loader/MainResourceLoader.cpp:
    (WebCore::MainResourceLoader::didReceiveResponse):
        Pass the request identifier into 'shouldInterruptLoadForXFrameOptions'.
    
    LayoutTests:
    
    * http/tests/security/XFrameOptions/resources/x-frame-options-invalid.cgi: Added.
    * http/tests/security/XFrameOptions/x-frame-options-invalid-expected.txt: Added.
    * http/tests/security/XFrameOptions/x-frame-options-invalid.html: Added.
        New test with an invalid frame option value.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -2901,7 +2901,7 @@ void FrameLoader::applyUserAgent(ResourceRequest& request)
     request.setHTTPUserAgent(userAgent);
 }
 
-bool FrameLoader::shouldInterruptLoadForXFrameOptions(const String& content, const KURL& url)
+bool FrameLoader::shouldInterruptLoadForXFrameOptions(const String& content, const KURL& url, unsigned long requestIdentifier)
 {
     Frame* topFrame = m_frame->tree()->top();
     if (m_frame == topFrame)
@@ -2909,11 +2909,13 @@ bool FrameLoader::shouldInterruptLoadForXFrameOptions(const String& content, con
 
     if (equalIgnoringCase(content, "deny"))
         return true;
-
-    if (equalIgnoringCase(content, "sameorigin")) {
+    else if (equalIgnoringCase(content, "sameorigin")) {
         RefPtr<SecurityOrigin> origin = SecurityOrigin::create(url);
         if (!origin->isSameSchemeHostPort(topFrame->document()->securityOrigin()))
             return true;
+    } else {
+        String message = "Invalid 'X-Frame-Options' header encountered when loading '" + url.string() + "': '" + content + "' is not a recognized directive. The header will be ignored.";
+        m_frame->document()->addConsoleMessage(JSMessageSource, LogMessageType, ErrorMessageLevel, message, url.string(), 0, 0, requestIdentifier);
     }
 
     return false;
