74d81c3c8b68a81cf4d8372e3651abe3188c153e
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 22:13:28 +0000

    Make 0 call sequence numbers be invalid.
    This was part of a previous change I abandoned but this part still seems like
    a good idea, since if you store sequence numbers for some reason, it's
    impossible to store one indicating you haven't made a request.
    
    I also worked to define the wraparound behavior since from a recent
    Chromium-dev thread, I learned that signed overflow is not defined in C++. My
    previous patch did this by using unsigned sequence numbers, but this is
    discouraged by the style guide and is kind of difficult to change at this point
    anyway.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/11375003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166776 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/plugin_resource.cc
+++ ppapi/proxy/plugin_resource.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/proxy/plugin_resource.h"
 
+#include <limits>
+
 #include "ppapi/proxy/ppapi_messages.h"
 
 namespace ppapi {
@@ -13,7 +15,7 @@ namespace proxy {
 PluginResource::PluginResource(Connection connection, PP_Instance instance)
     : Resource(OBJECT_IS_PROXY, instance),
       connection_(connection),
-      next_sequence_number_(0),
+      next_sequence_number_(1),
       sent_create_to_browser_(false),
       sent_create_to_renderer_(false) {
 }
@@ -75,13 +77,13 @@ void PluginResource::SendCreate(Destination dest, const IPC::Message& msg) {
     DCHECK(!sent_create_to_browser_);
     sent_create_to_browser_ = true;
   }
-  ResourceMessageCallParams params(pp_resource(), next_sequence_number_++);
+  ResourceMessageCallParams params(pp_resource(), GetNextSequence());
   GetSender(dest)->Send(
       new PpapiHostMsg_ResourceCreated(params, pp_instance(), msg));
 }
 
 void PluginResource::Post(Destination dest, const IPC::Message& msg) {
-  ResourceMessageCallParams params(pp_resource(), next_sequence_number_++);
+  ResourceMessageCallParams params(pp_resource(), GetNextSequence());
   SendResourceCall(dest, params, msg);
 }
 
@@ -96,7 +98,7 @@ bool PluginResource::SendResourceCall(
 int32_t PluginResource::GenericSyncCall(Destination dest,
                                         const IPC::Message& msg,
                                         IPC::Message* reply) {
-  ResourceMessageCallParams params(pp_resource(), next_sequence_number_++);
+  ResourceMessageCallParams params(pp_resource(), GetNextSequence());
   params.set_has_callback();
   ResourceMessageReplyParams reply_params;
   bool success = GetSender(dest)->Send(new PpapiHostMsg_ResourceSyncCall(
@@ -104,6 +106,18 @@ int32_t PluginResource::GenericSyncCall(Destination dest,
   if (success)
     return reply_params.result();
   return PP_ERROR_FAILED;
+}
+
+int32_t PluginResource::GetNextSequence() {
+  // Return the value with wraparound, making sure we don't make a sequence
+  // number with a 0 ID. Note that signed wraparound is undefined in C++ so we
+  // manually check.
+  int32_t ret = next_sequence_number_;
+  if (next_sequence_number_ == std::numeric_limits<int32_t>::max())
+    next_sequence_number_ = 1;  // Skip 0 which is invalid.
+  else
+    next_sequence_number_++;
+  return ret;
 }
 
 }  // namespace proxy
