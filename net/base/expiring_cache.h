e231afaa8241948e7050b1a4bccb5246e9cdda1d
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 22:26:48 +0000

    [net] Measure DNS.CacheEvicted and DNS.CacheExpired.
    On every eviction from HostCache record the time left to expiration
    (DNS.CacheEvicted) or time since expiration (DNS.CacheExpired).
    A distinction is made for evictions on Lookup (DNS.CacheExpiredOnGet).
    
    
    Review URL: https://chromiumcodereview.appspot.com/11359053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166781 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/expiring_cache.h
+++ net/base/expiring_cache.h
@@ -15,6 +15,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+template <typename KeyType,
+          typename ValueType,
+          typename ExpirationType>
+class NoopEvictionHandler {
+ public:
+  void Handle(const KeyType& key,
+              const ValueType& value,
+              const ExpirationType& expiration,
+              const ExpirationType& now,
+              bool onGet) const {
+  }
+};
+
 // Cache implementation where all entries have an explicit expiration policy. As
 // new items are added, expired items will be removed first.
 // The template types have the following requirements:
@@ -55,7 +68,10 @@ namespace net {
 template <typename KeyType,
           typename ValueType,
           typename ExpirationType,
-          typename ExpirationCompare>
+          typename ExpirationCompare,
+          typename EvictionHandler = NoopEvictionHandler<KeyType,
+                                                         ValueType,
+                                                         ExpirationType> >
 class ExpiringCache {
  private:
   // Intentionally violate the C++ Style Guide so that EntryMap is known to be
@@ -114,7 +130,7 @@ class ExpiringCache {
 
     // Immediately remove expired entries.
     if (!expiration_comp_(now, it->second.second)) {
-      entries_.erase(it);
+      Evict(it, now, true);
       return NULL;
     }
 
@@ -164,7 +180,7 @@ class ExpiringCache {
     typename EntryMap::iterator it;
     for (it = entries_.begin(); it != entries_.end(); ) {
       if (!expiration_comp_(now, it->second.second)) {
-        entries_.erase(it++);
+        Evict(it++, now, false);
       } else {
         ++it;
       }
@@ -176,8 +192,16 @@ class ExpiringCache {
     // If the cache is still too full, start deleting items 'randomly'.
     for (it = entries_.begin();
          it != entries_.end() && entries_.size() >= max_entries_;) {
-      entries_.erase(it++);
+      Evict(it++, now, false);
     }
+  }
+
+  void Evict(typename EntryMap::iterator it,
+             const ExpirationType& now,
+             bool on_get) {
+    eviction_handler_.Handle(it->first, it->second.first, it->second.second,
+                             now, on_get);
+    entries_.erase(it);
   }
 
   // Bound on total size of the cache.
@@ -185,6 +209,7 @@ class ExpiringCache {
 
   EntryMap entries_;
   ExpirationCompare expiration_comp_;
+  EvictionHandler eviction_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(ExpiringCache);
 };
