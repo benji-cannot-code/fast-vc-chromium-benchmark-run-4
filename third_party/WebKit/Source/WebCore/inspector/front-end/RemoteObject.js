53dc97e1e437b74b6ed622125fd0b245e49ef336
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-08 07:38:45 +0000

    Web Inspector: console error after inspecting IndexedDB https://bugs.webkit.org/show_bug.cgi?id=101481
    Reviewed by Pavel Feldman.
    
    Added objectId nullity check to RemoteOBject::release.
    Drive-by added release for entry key and primaryKey.
    
    * inspector/front-end/IndexedDBViews.js:
    (WebInspector.IDBDataView.prototype.clear):
    * inspector/front-end/RemoteObject.js:
    (WebInspector.RemoteObject.prototype.release):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133857 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/RemoteObject.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/RemoteObject.js
@@ -325,6 +325,8 @@ WebInspector.RemoteObject.prototype = {
 
     release: function()
     {
+        if (!this._objectId)
+            return;
         RuntimeAgent.releaseObject(this._objectId);
     },
 
