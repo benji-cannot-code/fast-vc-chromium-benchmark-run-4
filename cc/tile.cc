ea582d72730f1aca05df92a8d92449d9d7e8116e
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 22:42:34 +0000

    [cc] Tiles and tile manager for impl side painting
    BUG=155209
    R=enne@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11369130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166787 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ cc/tile.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "cc/tile.h"
+
+#include "cc/tile_manager.h"
+
+namespace cc {
+
+Tile::Tile(TileManager* tile_manager,
+           gfx::Size tile_size,
+           gfx::Rect rect_inside_picture,
+           TileQuality quality)
+  : tile_manager_(tile_manager),
+    tile_size_(tile_size),
+    rect_inside_picture_(rect_inside_picture),
+    quality_(quality) {
+  tile_manager_->RegisterTile(this);
+}
+
+Tile::~Tile() {
+  tile_manager_->UnregisterTile(this);
+}
+
+}  // namespace cc
