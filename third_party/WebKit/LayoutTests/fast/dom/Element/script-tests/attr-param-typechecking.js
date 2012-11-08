b28dc39fb60ded5890a859331c20ced65f10b7a9
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-08 21:52:15 +0000

    setAttributeNode and friends should not have optional argument https://bugs.webkit.org/show_bug.cgi?id=101631
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    http://www.w3.org/TR/DOM-Level-3-Core/core.html#ID-887236154
    
    Fix getAttributeNode, getAttributeNodeNS and removeAttributeNode to make the Attr
    argument mandatory.
    
    These used to throw DOMExceptions when an invalid type was passed instead of
    TypeError which is also a spec violation.
    
    Updated existing tests.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    * dom/Element.cpp:
    * dom/Element.idl:
    
    LayoutTests:
    
    Fix getAttributeNode, getAttributeNodeNS and removeAttributeNode to make the Attr
    argument mandatory.
    
    These used to throw DOMExceptions when an invalid type was passed instead of
    TypeError which is also a spec violation.
    
    * fast/dom/Element/attr-param-typechecking-expected.txt:
    * fast/dom/Element/script-tests/attr-param-typechecking.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/Element/script-tests/attr-param-typechecking.js
+++ third_party/WebKit/LayoutTests/fast/dom/Element/script-tests/attr-param-typechecking.js
@@ -5,6 +5,10 @@ description(
 
 var element = document.createElement("input");
 
+shouldThrow('element.setAttributeNode();');
+shouldThrow('element.setAttributeNodeNS();');
+shouldThrow('element.removeAttributeNode();');
+
 shouldThrow('element.setAttributeNode("style");');
 shouldThrow('element.setAttributeNode(null);');
 shouldThrow('element.setAttributeNode(undefined);');
--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1584,7 +1584,7 @@ END
     my $raisesExceptions = @{$function->raisesExceptions};
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
-            if ((!$parameter->extendedAttributes->{"Callback"} and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
+            if ($parameter->extendedAttributes->{"IsIndex"}) {
                 $raisesExceptions = 1;
             }
         }
@@ -1798,13 +1798,6 @@ sub GenerateParametersCheck
             }
             $parameterCheckString .= "    if (${parameterName}DidThrow)\n";
             $parameterCheckString .= "        return v8Undefined();\n";
-        } elsif (TypeCanFailConversion($parameter)) {
-            $parameterCheckString .= "    $nativeType $parameterName = " .
-                 JSValueToNative($parameter, "args[$paramIndex]", "args.GetIsolate()") . ";\n";
-            $parameterCheckString .= "    if (UNLIKELY(!$parameterName)) {\n";
-            $parameterCheckString .= "        ec = TYPE_MISMATCH_ERR;\n";
-            $parameterCheckString .= "        goto fail;\n";
-            $parameterCheckString .= "    }\n";
         } elsif ($parameter->isVariadic) {
             my $nativeElementType = GetNativeType($parameter->type);
             if ($nativeElementType =~ />$/) {
@@ -1872,7 +1865,7 @@ sub GenerateConstructorCallback
     }
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
-            if ((!$parameter->extendedAttributes->{"Callback"} and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
+            if ($parameter->extendedAttributes->{"IsIndex"}) {
                 $raisesExceptions = 1;
             }
         }
@@ -2050,7 +2043,7 @@ sub GenerateNamedConstructorCallback
     }
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
-            if ((!$parameter->extendedAttributes->{"Callback"} and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
+            if ($parameter->extendedAttributes->{"IsIndex"}) {
                 $raisesExceptions = 1;
             }
         }
@@ -3730,17 +3723,6 @@ sub TranslateParameter
     if ($signature->type eq "TimeoutHandler") {
       $signature->type("DOMString");
     }
-}
-
-sub TypeCanFailConversion
-{
-    my $signature = shift;
-
-    my $type = GetTypeFromSignature($signature);
-
-    AddToImplIncludes("ExceptionCode.h") if $type eq "Attr";
-    return 1 if $type eq "Attr";
-    return 0;
 }
 
 sub JSValueToNative
