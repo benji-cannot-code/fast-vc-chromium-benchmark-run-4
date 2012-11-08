a6b96e77651ad8fd73b003d02efa8c01319d5dc9
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-08 10:49:38 +0000

    GraphicsSurfaceGLX needs to query the drawable for YInversion. https://bugs.webkit.org/show_bug.cgi?id=101472
    After r133049 the WebGL content is now displayed upside down
    for the Qt port. We cannot just apply a generic rule, that
    the content needs to be flipped upside down.
    Instead we have to query GLX to tell us, if the drawable's
    framebuffer is y-inverted.
    
    Reviewed by Kenneth Rohde Christiansen.
    
    * platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp:
    (WebCore::GraphicsSurfacePrivate::textureIsYInverted):
    (GraphicsSurfacePrivate):
    (WebCore::GraphicsSurface::platformPaintToTextureMapper):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133874 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp
@@ -244,6 +244,11 @@ struct GraphicsSurfacePrivate {
         XFree(configs);
     }
 
+    bool textureIsYInverted()
+    {
+        return m_textureIsYInverted;
+    }
+
     void makeCurrent()
     {
         m_detachedContext = glXGetCurrentContext();
@@ -383,7 +388,8 @@ void GraphicsSurface::platformPaintToTextureMapper(TextureMapper* textureMapper,
     TextureMapperGL* texMapGL = static_cast<TextureMapperGL*>(textureMapper);
     TransformationMatrix adjustedTransform = transform;
     adjustedTransform.multiply(TransformationMatrix::rectToRect(FloatRect(FloatPoint::zero(), m_size), targetRect));
-    texMapGL->drawTexture(platformGetTextureID(), TextureMapperGL::ShouldFlipTexture, m_size, targetRect, adjustedTransform, opacity, mask);
+    TextureMapperGL::Flags flags = m_private->textureIsYInverted() ? TextureMapperGL::ShouldFlipTexture : 0;
+    texMapGL->drawTexture(platformGetTextureID(), flags, m_size, targetRect, adjustedTransform, opacity, mask);
 }
 
 uint32_t GraphicsSurface::platformFrontBuffer() const
