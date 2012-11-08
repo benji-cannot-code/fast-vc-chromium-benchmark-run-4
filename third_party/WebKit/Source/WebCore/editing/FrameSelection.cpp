1f966a17079aee91a2013aaf8047f5d871337947
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-08 03:50:25 +0000

    Crash in WebCore::RenderLayer::normalFlowList https://bugs.webkit.org/show_bug.cgi?id=101528
    Reviewed by Simon Fraser.
    
    Make sure the layout is up to date before re-computing the caret rect.
    Avoid doing the layout when the selection is cleared since we can always stop
    the blink timer in that case.
    
    Unfortunately, we haven't found any reproduction of this crash yet.
    
    * editing/FrameSelection.cpp:
    (WebCore::isNonOrphanedCaret):
    (WebCore::FrameSelection::localCaretRect):
    (WebCore::FrameSelection::updateAppearance):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
@@ -1264,10 +1264,15 @@ RenderObject* DragCaretController::caretRenderer() const
     return CaretBase::caretRenderer(m_position.deepEquivalent().deprecatedNode());
 }
 
+static bool isNonOrphanedCaret(const VisibleSelection& selection)
+{
+    return selection.isCaret() && !selection.start().isOrphan() && !selection.end().isOrphan();
+}
+
 LayoutRect FrameSelection::localCaretRect()
 {
     if (shouldUpdateCaretRect()) {
-        if (!isCaret() || m_selection.start().isOrphan() || m_selection.end().isOrphan())
+        if (!isNonOrphanedCaret(m_selection))
             clearCaretRect();
         else if (updateCaretRect(m_frame->document(), VisiblePosition(m_selection.start(), m_selection.affinity())))
             m_absCaretBoundsDirty = true;
@@ -1321,6 +1326,7 @@ bool FrameSelection::recomputeCaretRect()
     FrameView* v = m_frame->document()->view();
     if (!v)
         return false;
+    ASSERT(!v->needsLayout());
 
     LayoutRect oldRect = localCaretRectWithoutUpdate();
     LayoutRect newRect = localCaretRect();
@@ -1722,14 +1728,19 @@ inline static bool shouldStopBlinkingDueToTypingCommand(Frame* frame)
 void FrameSelection::updateAppearance()
 {
 #if ENABLE(TEXT_CARET)
-    bool caretRectChanged = recomputeCaretRect();
+    bool caretRectChangedOrCleared = false;
+    if (isNonOrphanedCaret(m_selection)) {
+        m_frame->document()->updateLayout();
+        caretRectChangedOrCleared = recomputeCaretRect();
+    } else
+        caretRectChangedOrCleared = true;
 
     bool caretBrowsing = m_frame->settings() && m_frame->settings()->caretBrowsingEnabled();
     bool shouldBlink = caretIsVisible() && isCaret() && (isContentEditable() || caretBrowsing);
 
     // If the caret moved, stop the blink timer so we can restart with a
     // black caret in the new location.
-    if (caretRectChanged || !shouldBlink || shouldStopBlinkingDueToTypingCommand(m_frame))
+    if (caretRectChangedOrCleared || !shouldBlink || shouldStopBlinkingDueToTypingCommand(m_frame))
         m_caretBlinkTimer.stop();
 
     // Start blinking with a black caret. Be sure not to restart if we're
