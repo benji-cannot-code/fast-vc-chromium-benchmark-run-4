7e2dd95d830d68b7ea2b48a6b96a7c68de46a0a8
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 00:33:34 +0000

    Add touchEventHandlerRegion to Layer and LayerImpl
    These will be used to keep track of JavaScript touch event handler on the compositor thread
    and will be updated accordingly when there is a change involving adding/removing or changing
    the layout of the nodes that contain the touch event handlers.
    
    BUG=135818
    
    
    Review URL: https://chromiumcodereview.appspot.com/11377013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166561 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/compositor_bindings/web_layer_impl.h
+++ webkit/compositor_bindings/web_layer_impl.h
@@ -89,6 +89,8 @@ public:
     virtual bool shouldScrollOnMainThread() const;
     virtual void setNonFastScrollableRegion(const WebVector<WebRect>&) OVERRIDE;
     virtual WebVector<WebRect> nonFastScrollableRegion() const;
+    virtual void setTouchEventHandlerRegion(const WebVector<WebRect>&);
+    virtual WebVector<WebRect> touchEventHandlerRegion() const;
     virtual void setIsContainerForFixedPositionLayers(bool) OVERRIDE;
     virtual bool isContainerForFixedPositionLayers() const;
     virtual void setFixedToContainerLayer(bool) OVERRIDE;
