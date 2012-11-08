3d140eb22e6090634c48e087e54ee04fd857a08f
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-08 18:29:40 +0000

    Add conflict handling interface for SyncFileSystem
    No implementation yet.
    
    BUG=155505
    TEST=n/a
    
    
    Review URL: https://chromiumcodereview.appspot.com/11275192
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166709 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/fileapi/syncable/sync_file_metadata.cc
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "webkit/fileapi/syncable/sync_file_metadata.h"
+
+namespace fileapi {
+
+SyncFileMetadata::SyncFileMetadata()
+    : file_type(SYNC_FILE_TYPE_UNKNOWN),
+      size(-1) {
+}
+
+SyncFileMetadata::~SyncFileMetadata() {}
+
+ConflictFileInfo::ConflictFileInfo() {}
+ConflictFileInfo::~ConflictFileInfo() {}
+
+}  // namespace fileapi
