90db2e305afa646741734217a77740bc78f56529
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-09 23:27:22 +0000

    CustomEvent: Allow taking in a serialized value during initialization. https://bugs.webkit.org/show_bug.cgi?id=101348
    Patch by Sadrul Habib Chowdhury <sadrul@chromium.org> on 2012-11-09
    Reviewed by Adam Barth.
    
    If a CustomEvent is initialized using a serialized value, then for each access
    to |detail|, the value is deserialized first. This way, each world gets a different
    deserialization.
    
    * UseV8.cmake:
    * WebCore.gypi:
    * bindings/v8/V8HiddenPropertyName.h:
    (WebCore):
    * bindings/v8/custom/V8CustomEventCustom.cpp: Copied from Source/WebCore/bindings/v8/V8HiddenPropertyName.h.
    (WebCore):
    (WebCore::V8CustomEvent::detailAccessorGetter):
    * dom/CustomEvent.cpp:
    (WebCore::CustomEvent::initCustomEvent):
    (WebCore):
    * dom/CustomEvent.h:
    (CustomEvent):
    (WebCore::CustomEvent::serializedScriptValue):
    * dom/CustomEvent.idl:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134120 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/CustomEvent.cpp
+++ third_party/WebKit/Source/WebCore/dom/CustomEvent.cpp
@@ -35,7 +35,6 @@ CustomEventInit::CustomEventInit()
 {
 }
 
-
 CustomEvent::CustomEvent()
 {
 }
@@ -52,6 +51,7 @@ CustomEvent::~CustomEvent()
 
 void CustomEvent::initCustomEvent(const AtomicString& type, bool canBubble, bool cancelable, const ScriptValue& detail)
 {
+    ASSERT(!m_serializedScriptValue.get());
     if (dispatched())
         return;
 
@@ -59,6 +59,19 @@ void CustomEvent::initCustomEvent(const AtomicString& type, bool canBubble, bool
 
     m_detail = detail;
 }
+
+#if USE(V8)
+void CustomEvent::initCustomEvent(const AtomicString& type, bool canBubble, bool cancelable, PassRefPtr<SerializedScriptValue> serializedScriptValue)
+{
+    ASSERT(m_detail.hasNoValue());
+    if (dispatched())
+        return;
+
+    initEvent(type, canBubble, cancelable);
+
+    m_serializedScriptValue = serializedScriptValue;
+}
+#endif // USE(V8)
 
 const AtomicString& CustomEvent::interfaceName() const
 {
