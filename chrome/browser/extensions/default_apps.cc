ea06751a0ea7783b685c59ab2f9d0eeac5f21cfc
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 21:40:59 +0000

    Don't install external extensions in tests
    BUG=chromium-os:36058
    TEST=existing tests passes
    
    
    Review URL: https://chromiumcodereview.appspot.com/11359105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166978 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/default_apps.cc
+++ chrome/browser/extensions/default_apps.cc
@@ -115,11 +115,6 @@ bool Provider::ShouldInstallInProfile() {
   if (install_apps && !IsLocaleSupported())
     install_apps = false;
 
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableDefaultApps)) {
-    install_apps = false;
-  }
-
   // Default apps are only installed on profile creation or a new chrome
   // download.
   if (state == kUnknown) {
