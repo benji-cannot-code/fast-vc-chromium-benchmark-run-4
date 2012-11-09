edb0998b646c8b316fe0a25df8758af74a9a8df3
vabr@chromium.org <vabr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 14:13:44 +0000

    Split ExtensionWebRequestApiTest.PostData into two
    On some debug trybots the original PostData test takes too long and hits the
    45s time limit. This splits it into two tests.
    
    BUG=152859
    
    
    Review URL: https://chromiumcodereview.appspot.com/11358159
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166907 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/webrequest/test_post2.js
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+runTests([
+  // Navigates to a page with a form and submits it, generating a POST request.
+  // Multipart-encoded form.
+  sendPost('multipart.html', true /*parseableForm*/),
+  // An unparseable form, thus only raw POST data are extracted.
+  sendPost('plaintext.html', false /*parseableForm*/),
+]);
