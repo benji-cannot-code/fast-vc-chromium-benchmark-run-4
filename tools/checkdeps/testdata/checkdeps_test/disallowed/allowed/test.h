e3e5f0833385a67207402bf17c1b6170d954a4a8
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 23:54:36 +0000

    Commit these files in advance of tests so that they pass on their CQ run.
    It appears that, while checkdeps_test is run on the CQ, the new files are not actually source-controlled. Since the checkdeps tests expect them to be, they fail.
    
    Commit the data files first, and then later commit the tests that run against them.
    
    TBR=joi@chromium.org
    BUG=155596
    
    
    Review URL: https://chromiumcodereview.appspot.com/11363174
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167010 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/checkdeps/testdata/checkdeps_test/disallowed/allowed/test.h
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "allowed/good.h"
+// Always allowed to include self and parents.
+#include "disallowed/good.h"
+#include "disallowed/allowed/good.h"
+#include "third_party/explicitly_disallowed/bad.h"
+#include "third_party/allowed_may_use/bad.h"
+#include "third_party/no_rule/bad.h"
---
+++ tools/checkdeps/testdata/checkdeps_test/disallowed/test.h
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "allowed/good.h"
+// Always allowed to include self.
+#include "disallowed/good.h"
+#include "disallowed/allowed/good.h"
+#include "third_party/explicitly_disallowed/bad.h"
+// Only allowed for code under allowed/.
+#include "third_party/allowed_may_use/bad.h"
+#include "third_party/no_rule/bad.h"
