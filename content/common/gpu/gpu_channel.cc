8af399055328141a5869ba00e3009a02d212333b
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 19:50:36 +0000

    GPUChannel pools mailbox names.
    It asynchronously refills the pool to a target size when it falls below half that size.
    
    GenerateMailboxNames is only synchronous if the pool is depleted. Common case is a few microseconds per name.
    
    Review URL: https://chromiumcodereview.appspot.com/11362171
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166962 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_channel.cc
+++ content/common/gpu/gpu_channel.cc
@@ -173,6 +173,8 @@ class MailboxMessageFilter : public IPC::ChannelProxy::MessageFilter {
     IPC_BEGIN_MESSAGE_MAP(MailboxMessageFilter, message)
       IPC_MESSAGE_HANDLER(GpuChannelMsg_GenerateMailboxNames,
                           OnGenerateMailboxNames)
+      IPC_MESSAGE_HANDLER(GpuChannelMsg_GenerateMailboxNamesAsync,
+                          OnGenerateMailboxNamesAsync)
       IPC_MESSAGE_UNHANDLED(handled = false)
     IPC_END_MESSAGE_MAP()
 
@@ -205,6 +207,12 @@ class MailboxMessageFilter : public IPC::ChannelProxy::MessageFilter {
 
       (*result)[i].assign(name, sizeof(name));
     }
+  }
+
+  void OnGenerateMailboxNamesAsync(unsigned num) {
+    std::vector<std::string> names;
+    OnGenerateMailboxNames(num, &names);
+    Send(new GpuChannelMsg_GenerateMailboxNamesReply(names));
   }
 
   IPC::Channel* channel_;
