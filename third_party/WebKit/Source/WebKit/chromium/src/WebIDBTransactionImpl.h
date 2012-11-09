dd0805789f933a4a4b60aa2bbce7e713bae5f6d3
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-09 01:36:04 +0000

    IndexedDB: switch frontend to use int64_t-based references https://bugs.webkit.org/show_bug.cgi?id=100426
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    Remove String-based objectStore/index references, obsoleted by
    https://bugs.webkit.org/show_bug.cgi?id=100425.
    
    No new tests as this is the second half of a refactor.
    
    * Modules/indexeddb/IDBCallbacks.h:
    * Modules/indexeddb/IDBDatabase.cpp:
    (WebCore::IDBDatabase::deleteObjectStore):
    (WebCore::IDBDatabase::transaction):
    * Modules/indexeddb/IDBDatabaseBackendImpl.cpp:
    (WebCore::IDBDatabaseBackendImpl::setVersion):
    (WebCore::IDBDatabaseBackendImpl::transaction):
    (WebCore::IDBDatabaseBackendImpl::runIntVersionChangeTransaction):
    * Modules/indexeddb/IDBDatabaseBackendImpl.h:
    (IDBDatabaseBackendImpl):
    * Modules/indexeddb/IDBDatabaseBackendInterface.h:
    (IDBDatabaseBackendInterface):
    * Modules/indexeddb/IDBMetadata.h:
    (WebCore::IDBObjectStoreMetadata::findIndex):
    (IDBObjectStoreMetadata):
    (WebCore::IDBObjectStoreMetadata::containsIndex):
    * Modules/indexeddb/IDBObjectStore.cpp:
    (WebCore::IDBObjectStore::put):
    (WebCore):
    (WebCore::IDBObjectStore::index):
    (WebCore::IDBObjectStore::deleteIndex):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.cpp:
    (WebCore::IDBObjectStoreBackendImpl::put):
    (WebCore):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.h:
    (IDBObjectStoreBackendImpl):
    * Modules/indexeddb/IDBObjectStoreBackendInterface.h:
    * inspector/Inspector-1.0.json:
    * inspector/Inspector.json:
    * inspector/InspectorIndexedDBAgent.cpp:
    (WebCore):
    (WebCore::InspectorIndexedDBAgent::requestData):
    * inspector/InspectorIndexedDBAgent.h:
    (InspectorIndexedDBAgent):
    
    Source/WebKit/chromium:
    
    Remove implementation stubs from chromium API for
    methods obsoleted by https://bugs.webkit.org/show_bug.cgi?id=100425
    
    * src/IDBObjectStoreBackendProxy.cpp:
    * src/IDBObjectStoreBackendProxy.h:
    (IDBObjectStoreBackendProxy):
    * src/WebIDBDatabaseImpl.cpp:
    * src/WebIDBDatabaseImpl.h:
    (WebIDBDatabaseImpl):
    * src/WebIDBObjectStoreImpl.cpp:
    * src/WebIDBObjectStoreImpl.h:
    (WebIDBObjectStoreImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133984 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebIDBTransactionImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebIDBTransactionImpl.h
@@ -42,7 +42,6 @@ public:
     WebIDBTransactionImpl(WTF::PassRefPtr<WebCore::IDBTransactionBackendInterface>);
     virtual ~WebIDBTransactionImpl();
 
-    virtual WebIDBObjectStore* objectStore(const WebString& name, WebExceptionCode&);
     virtual WebIDBObjectStore* objectStore(long long indexId, WebExceptionCode&);
     virtual void commit();
     virtual void abort();
