6546f5ae9479b5a3ab081c49e4f3f0a007e1b480
milanb@chromium.org <milanb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 13:22:53 +0000

    Enable iOS background music.
    Setting an audio flag that enables playing a background music during browsing.
    This flag does not affect either speech input or media playback, they
    still work as before.
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293176
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166902 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/ios/audio_manager_ios.mm
+++ media/audio/ios/audio_manager_ios.mm
@@ -26,6 +26,9 @@ static bool InitAudioSessionInternal() {
   BOOL result = [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
                                     error:nil];
   DCHECK(result);
+  UInt32 allowMixing = true;
+  AudioSessionSetProperty(kAudioSessionProperty_OverrideCategoryMixWithOthers,
+                          sizeof(allowMixing), &allowMixing);
   return error == kAudioSessionNoError;
 }
 
