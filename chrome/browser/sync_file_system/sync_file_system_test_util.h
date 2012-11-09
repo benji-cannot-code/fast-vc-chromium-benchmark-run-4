73cf46d457f0896ce0dbdf8693d981702f1d343e
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 05:50:29 +0000

    Move sync_file_system_test_util template functions to the .cc file.
    Review URL: https://chromiumcodereview.appspot.com/11293190
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166859 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_test_util.h
+++ chrome/browser/sync_file_system/sync_file_system_test_util.h
@@ -6,13 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SYNC_FILE_SYSTEM_SYNC_FILE_SYSTEM_TEST_UTIL_H_
 #define CHROME_BROWSER_SYNC_FILE_SYSTEM_SYNC_FILE_SYSTEM_TEST_UTIL_H_
 
-#include "base/bind.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
-#include "base/run_loop.h"
 
 namespace base {
+class RunLoop;
 class SingleThreadTaskRunner;
 class Thread;
 }
@@ -24,17 +23,10 @@ class TestBrowserThread;
 namespace sync_file_system {
 
 template <typename R>
-void AssignAndQuit(base::RunLoop* run_loop, R* result_out, R result) {
-  DCHECK(result_out);
-  DCHECK(run_loop);
-  *result_out = result;
-  run_loop->Quit();
-}
+void AssignAndQuit(base::RunLoop* run_loop, R* result_out, R result);
 
 template <typename R> base::Callback<void(R)>
-AssignAndQuitCallback(base::RunLoop* run_loop, R* result) {
-  return base::Bind(&AssignAndQuit<R>, run_loop, base::Unretained(result));
-}
+AssignAndQuitCallback(base::RunLoop* run_loop, R* result);
 
 // This sets up FILE, IO and UI browser threads for testing.
 // (UI thread is set to the current thread.)
