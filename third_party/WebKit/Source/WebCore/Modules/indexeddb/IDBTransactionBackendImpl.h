24d597103a6c3d6c02496f110f3f9dd617e982ef
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-09 07:41:19 +0000

    Unreviewed, rolling out r134010. http://trac.webkit.org/changeset/134010 https://bugs.webkit.org/show_bug.cgi?id=101716
    Broke the chromium windows build. (Requested by noel_ on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-11-08
    
    Source/WebCore:
    
    * Modules/indexeddb/IDBAny.cpp:
    * Modules/indexeddb/IDBCallbacks.h:
    * Modules/indexeddb/IDBDatabase.cpp:
    (WebCore::IDBDatabase::createObjectStore):
    (WebCore::IDBDatabase::deleteObjectStore):
    (WebCore::IDBDatabase::transaction):
    * Modules/indexeddb/IDBDatabase.h:
    (IDBDatabase):
    * Modules/indexeddb/IDBDatabaseBackendImpl.cpp:
    (WebCore::IDBDatabaseBackendImpl::getObjectStoreId):
    (WebCore):
    (WebCore::IDBDatabaseBackendImpl::setVersion):
    (WebCore::IDBDatabaseBackendImpl::transaction):
    (WebCore::IDBDatabaseBackendImpl::runIntVersionChangeTransaction):
    * Modules/indexeddb/IDBDatabaseBackendImpl.h:
    (WebCore::IDBDatabaseBackendImpl::deleteObjectStore):
    (IDBDatabaseBackendImpl):
    * Modules/indexeddb/IDBDatabaseBackendInterface.h:
    (WebCore):
    (IDBDatabaseBackendInterface):
    * Modules/indexeddb/IDBMetadata.h:
    (WebCore::IDBObjectStoreMetadata::containsIndex):
    (WebCore::IDBDatabaseMetadata::findObjectStore):
    (IDBDatabaseMetadata):
    (WebCore::IDBDatabaseMetadata::containsObjectStore):
    * Modules/indexeddb/IDBObjectStore.cpp:
    (WebCore::IDBObjectStore::put):
    (WebCore):
    (WebCore::IDBObjectStore::createIndex):
    (WebCore::IDBObjectStore::index):
    (WebCore::IDBObjectStore::deleteIndex):
    * Modules/indexeddb/IDBObjectStore.h:
    (WebCore::IDBObjectStore::openCursor):
    (IDBObjectStore):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.cpp:
    (WebCore::IDBObjectStoreBackendImpl::put):
    (WebCore::IDBObjectStoreBackendImpl::putWithIndexKeys):
    (WebCore):
    (WebCore::IDBObjectStoreBackendImpl::setIndexKeys):
    (WebCore::IDBObjectStoreBackendImpl::setIndexesReady):
    (WebCore::IDBObjectStoreBackendImpl::index):
    (WebCore::IDBObjectStoreBackendImpl::getIndexId):
    (WebCore::IDBObjectStoreBackendImpl::getIndexIds):
    (WebCore::IDBObjectStoreBackendImpl::deleteIndex):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.h:
    (IDBObjectStoreBackendImpl):
    * Modules/indexeddb/IDBObjectStoreBackendInterface.h:
    (WebCore):
    * Modules/indexeddb/IDBTransaction.cpp:
    (WebCore::IDBTransaction::objectStore):
    * Modules/indexeddb/IDBTransactionBackendImpl.cpp:
    (WebCore::IDBTransactionBackendImpl::objectStore):
    (WebCore):
    * Modules/indexeddb/IDBTransactionBackendImpl.h:
    (IDBTransactionBackendImpl):
    * Modules/indexeddb/IDBTransactionBackendInterface.h:
    
    Source/WebKit/chromium:
    
    * src/IDBCallbacksProxy.cpp:
    * src/IDBDatabaseBackendProxy.cpp:
    (WebKit::IDBDatabaseBackendProxy::transaction):
    (WebKit):
    * src/IDBDatabaseBackendProxy.h:
    (IDBDatabaseBackendProxy):
    * src/IDBObjectStoreBackendProxy.cpp:
    (WebKit::IDBObjectStoreBackendProxy::putWithIndexKeys):
    (WebKit):
    (WebKit::IDBObjectStoreBackendProxy::setIndexKeys):
    (WebKit::IDBObjectStoreBackendProxy::createIndex):
    (WebKit::IDBObjectStoreBackendProxy::setIndexesReady):
    (WebKit::IDBObjectStoreBackendProxy::index):
    (WebKit::IDBObjectStoreBackendProxy::deleteIndex):
    * src/IDBObjectStoreBackendProxy.h:
    (IDBObjectStoreBackendProxy):
    * src/WebIDBDatabaseImpl.cpp:
    (WebKit::WebIDBDatabaseImpl::deleteObjectStore):
    (WebKit):
    (WebKit::WebIDBDatabaseImpl::transaction):
    * src/WebIDBDatabaseImpl.h:
    (WebIDBDatabaseImpl):
    * src/WebIDBObjectStoreImpl.cpp:
    (WebKit::WebIDBObjectStoreImpl::putWithIndexKeys):
    (WebKit):
    (WebKit::WebIDBObjectStoreImpl::setIndexKeys):
    (WebKit::WebIDBObjectStoreImpl::setIndexesReady):
    (WebKit::WebIDBObjectStoreImpl::index):
    (WebKit::WebIDBObjectStoreImpl::deleteIndex):
    * src/WebIDBObjectStoreImpl.h:
    (WebIDBObjectStoreImpl):
    * src/WebIDBTransactionImpl.cpp:
    (WebKit::WebIDBTransactionImpl::objectStore):
    (WebKit):
    * src/WebIDBTransactionImpl.h:
    * tests/IDBRequestTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134022 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBTransactionBackendImpl.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBTransactionBackendImpl.h
@@ -53,6 +53,7 @@ public:
     virtual ~IDBTransactionBackendImpl();
 
     // IDBTransactionBackendInterface
+    virtual PassRefPtr<IDBObjectStoreBackendInterface> objectStore(const String& name, ExceptionCode&);
     virtual PassRefPtr<IDBObjectStoreBackendInterface> objectStore(int64_t, ExceptionCode&);
     virtual void didCompleteTaskEvents();
     virtual void abort();
