14a66bd6b02a6985cad47f61913f52e3816f0fb2
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-09 00:12:07 +0000

    DocumentLoader: Shrink-to-fit the ResourceResponse vector after loading completes. <http://webkit.org/b/101657>
    Reviewed by Anders Carlsson.
    
    Shrink DocumentLoader::m_responses to exact size when we stop adding responses to it,
    as we know it won't grow after that.
    
    520kB progression on Membuster3.
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::stopRecordingResponses):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/DocumentLoader.cpp
@@ -715,6 +715,7 @@ void DocumentLoader::addResponse(const ResourceResponse& r)
 void DocumentLoader::stopRecordingResponses()
 {
     m_stopRecordingResponses = true;
+    m_responses.shrinkToFit();
 }
 
 void DocumentLoader::setTitle(const StringWithDirection& title)
