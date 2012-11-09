99ed90a05a9c39f81a5d4448e2b98967d80412c5
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-09 01:01:26 +0000

    https://bugs.webkit.org/show_bug.cgi?id=101644 Fixed header on Facebook news feed becomes detached from top of viewport after rubber band scrolling -and corresponding- <rdar://problem/12651944>
    Reviewed by Simon Fraser.
    
    Source/WebCore: 
    
    There is code to handle this for non-threaded scrolling on FrameView. 
    This patch moves most of that code into a convenience function on 
    ScrollingCoordinator.
    
    Have FrameView::scrollOffsetForFixedPosition() call 
    WebCore::scrollOffsetForFixedPosition() with all the right 
    parameters.
    * page/FrameView.cpp:
    (WebCore::FrameView::scrollOffsetForFixedPosition):
    
    Here's where all the math happens.
    * page/scrolling/ScrollingCoordinator.cpp:
    (WebCore::fixedPositionScrollOffset):
    (WebCore::scrollOffsetForFixedPosition):
    
    The viewportRect in these three places needs to have the 
    adjusted-for-fixed offset.
    (WebCore::ScrollingCoordinator::updateMainFrameScrollPosition):
    * page/scrolling/mac/ScrollingTreeScrollingNodeMac.mm:
    (WebCore::ScrollingTreeScrollingNodeMac::setScrollLayerPosition):
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::computeFixedViewportConstraints):
    
    LayoutTests: 
    
    New test ensures that negative offsets will not make their way into 
    the FixedPositionViewportConstraints.
    * platform/mac/tiled-drawing/fixed/negative-scroll-offset-expected.txt: Added.
    * platform/mac/tiled-drawing/fixed/negative-scroll-offset.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@133974 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -2591,7 +2591,9 @@ const FixedPositionViewportConstraints RenderLayerCompositor::computeFixedViewpo
     ASSERT(layer->isComposited());
 
     FrameView* frameView = m_renderView->frameView();
+
     LayoutRect viewportRect = frameView->visibleContentRect();
+    viewportRect.setLocation(toPoint(frameView->scrollOffsetForFixedPosition()));
 
     FixedPositionViewportConstraints constraints = FixedPositionViewportConstraints();
 
