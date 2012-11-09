c48696b11b7e19a71fcf091e2f38177537041242
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-09 03:51:04 +0000

    Split Extension::ShouldDisplayInLauncher into ShouldDisplayIn{AppLauncher,NewTabPage}.
    Adds manifest property show_in_new_tab_page (default value=show_in_launcher)
    to give component apps a way to indicate that they should only appear in the
    app launcher, and not the browser new tab page.
    
    BUG=158880
    TEST=unit_tests --gtest_filter=ExtensionTest.DisplayInXManifestProperties, and (on Windows)
    settings app icon does appear on app launcher: ./chrome --user-data-dir=empty --enable-settings-app --show-app-list 
    settings app icon does not appear on NTP: ./chrome --user-data-dir=empty --enable-settings-app
    
    
    Review URL: https://chromiumcodereview.appspot.com/11362081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@166849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_prefs.cc
+++ chrome/browser/extensions/extension_prefs.cc
@@ -1498,7 +1498,7 @@ void ExtensionPrefs::OnExtensionInstalled(
   // Clear state that may be registered from a previous install.
   extension_dict->Remove(kRegisteredEvents, NULL);
 
-  if (extension->ShouldDisplayInLauncher())
+  if (extension->RequiresSortOrdinal())
     extension_sorting_->EnsureValidOrdinals(extension->id(), page_ordinal);
 
   extension_pref_value_map_->RegisterExtension(
--- chrome/common/extensions/extension_manifest_constants.h
+++ chrome/common/extensions/extension_manifest_constants.h
@@ -35,6 +35,7 @@ namespace extension_manifest_keys {
   extern const char kDescription[];
   extern const char kDevToolsPage[];
   extern const char kDisplayInLauncher[];
+  extern const char kDisplayInNewTabPage[];
   extern const char kEventName[];
   extern const char kExcludeGlobs[];
   extern const char kExcludeMatches[];
@@ -204,6 +205,7 @@ namespace extension_manifest_errors {
   extern const char kInvalidDescription[];
   extern const char kInvalidDevToolsPage[];
   extern const char kInvalidDisplayInLauncher[];
+  extern const char kInvalidDisplayInNewTabPage[];
   extern const char kInvalidExcludeMatch[];
   extern const char kInvalidExcludeMatches[];
   extern const char kInvalidFileAccessList[];
