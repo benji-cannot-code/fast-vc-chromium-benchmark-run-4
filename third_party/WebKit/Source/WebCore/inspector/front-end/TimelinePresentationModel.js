3b6767a3aa2d327f78011e67ee6537369930af53
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-09 07:45:52 +0000

    Web Inspector: Timeline: "Send Request" events are shown out of order. https://bugs.webkit.org/show_bug.cgi?id=101544
    Patch by Eugene Klyuchnikov <eustas.bug@gmail.com> on 2012-11-08
    Reviewed by Yury Semikhatsky.
    
    Solution: replace obsolete out-of-order record pushing with frontend
    record reparenting.
    
    * inspector/InspectorTimelineAgent.cpp:
    (WebCore::InspectorTimelineAgent::willSendResourceRequest): Replaced
    direct record pushing with standard appendRecord invokation.
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel.prototype._findParentRecord):
    Made "Send Request" records top-level if gluing is on.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePresentationModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePresentationModel.js
@@ -369,6 +369,9 @@ WebInspector.TimelinePresentationModel.prototype = {
         case recordTypes.ResourceReceivedData:
             return this._sendRequestRecords[record.data["requestId"]];
 
+        case recordTypes.ResourceSendRequest:
+            return this._rootRecord;
+
         case recordTypes.TimerFire:
             return this._timerRecords[record.data["timerId"]];
 
