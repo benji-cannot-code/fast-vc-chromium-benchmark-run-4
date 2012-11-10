0f1476487a0285b4fa14bcf7e89ba17445700bc7
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-10 21:19:43 +0000

    [Qt][WK2] Use QLibraryInfo to search for executables. https://bugs.webkit.org/show_bug.cgi?id=101774
    Reviewed by Simon Hausmann.
    
    This allows finding the WebProcess or the PluginProcess
    in it's installed location. This is necessary when
    deploying QtWebKit with an installer.
    
    * Shared/qt/ProcessExecutablePathQt.cpp:
    (WebKit::executablePath):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/qt/ProcessExecutablePathQt.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/qt/ProcessExecutablePathQt.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QCoreApplication>
 #include <QDir>
 #include <QFile>
+#include <QLibraryInfo>
 
 namespace WebKit {
 
@@ -39,6 +40,11 @@ static String executablePath(QString baseName)
     QString expectedPath = QCoreApplication::applicationDirPath() + QDir::separator() + baseName;
     if (QFile::exists(expectedPath))
         return String(expectedPath);
+
+    expectedPath = QLibraryInfo::location(QLibraryInfo::BinariesPath) + QDir::separator() + baseName;
+    if (QFile::exists(expectedPath))
+        return String(expectedPath);
+
     return String(QString(baseName));
 }
 
