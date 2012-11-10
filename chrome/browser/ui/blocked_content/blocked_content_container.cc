b6c2f7d70cee73e32e9ce2163e8fb11f35e24520
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-10 05:47:00 +0000

    Fix blocked content leak when TabContents is not created.
    Chrome on Android doesn't create TabContents for blocked WebContents because
    the TabContents is not needed until it is actually displayed.
    When a blocked WebContents is to be destroyed, the WebContents should be
    deleted directly if there is no TabContents, otherwise the WebContents as
    well as the renderer process will be leaked.
    
    The final solution should be fixing crbug.com/38458 so that no WebContents will
    be created for blocked contents.
    
    BUG=158894
    
    
    Review URL: https://chromiumcodereview.appspot.com/11377008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167064 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/blocked_content/blocked_content_container.cc
+++ chrome/browser/ui/blocked_content/blocked_content_container.cc
@@ -14,6 +14,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::OpenURLParams;
 using content::WebContents;
 
+namespace {
+
+void DestroyBlockedContents(WebContents* web_contents) {
+  TabContents* tab_contents = TabContents::FromWebContents(web_contents);
+  if (tab_contents)
+    delete tab_contents;
+  else
+    delete web_contents;
+}
+
+}  // namespace
+
 // static
 const size_t BlockedContentContainer::kImpossibleNumberOfPopups = 30;
 
@@ -110,7 +122,7 @@ void BlockedContentContainer::Clear() {
     WebContents* web_contents = i->web_contents;
     web_contents->SetDelegate(NULL);
     BlockedContentTabHelper::FromWebContents(web_contents)->set_delegate(NULL);
-    delete TabContents::FromWebContents(web_contents);
+    DestroyBlockedContents(web_contents);
   }
   blocked_contents_.clear();
 }
@@ -145,7 +157,7 @@ void BlockedContentContainer::CloseContents(WebContents* source) {
       BlockedContentTabHelper::FromWebContents(web_contents)->
           set_delegate(NULL);
       blocked_contents_.erase(i);
-      delete TabContents::FromWebContents(web_contents);
+      DestroyBlockedContents(web_contents);
       break;
     }
   }
