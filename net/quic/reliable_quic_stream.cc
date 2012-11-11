c06dc14551f0af4edff627d5dee340a591711837
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-11 19:22:19 +0000

    Several cleanups of QuicSession, et al. as per the Chrome code review.
    Merge internal change: 38012917
    
    
    Review URL: https://chromiumcodereview.appspot.com/11365153
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167128 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/reliable_quic_stream.cc
+++ net/quic/reliable_quic_stream.cc
@@ -59,7 +59,7 @@ bool ReliableQuicStream::OnStreamFrame(const QuicStreamFrame& frame) {
 void ReliableQuicStream::OnStreamReset(QuicErrorCode error,
                                        QuicStreamOffset offset) {
   error_ = error;
-  sequencer_.CloseStreamAtOffset(offset, false);  // Full close
+  sequencer_.CloseStreamAtOffset(offset, false);  // Full close.
 }
 
 void ReliableQuicStream::ConnectionClose(QuicErrorCode error, bool from_peer) {
@@ -84,11 +84,11 @@ void ReliableQuicStream::Close(QuicErrorCode error) {
   session()->SendRstStream(id(), error, offset_);
 }
 
-bool ReliableQuicStream::IsHalfClosed() {
+bool ReliableQuicStream::IsHalfClosed() const {
   return sequencer_.IsHalfClosed();
 }
 
-bool ReliableQuicStream::HasBytesToRead() {
+bool ReliableQuicStream::HasBytesToRead() const {
   return sequencer_.HasBytesToRead();
 }
 
