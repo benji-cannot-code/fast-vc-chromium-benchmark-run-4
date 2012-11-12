49110c1e1f88127e483b8a4452d82e7f12d293d4
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-12 05:53:56 +0000

    Extract InfolistWindowView from candidate_window.cc
    This patch includes:
    1. rename candidate_window.cc to candidate_window_view.cc
    2. Clean Up candidate_window_view.cc inclusion headers.
    3. Extract common constants from candidate_window.cc
    4. Split infolist specific unit tests from candidate_window_view_unittests.cc
    
    BUG=None
    TEST=ran unit_tests and manually done on lumpy
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11359099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167148 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/candidate_window_controller.cc
+++ chrome/browser/chromeos/input_method/candidate_window_controller.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "chrome/browser/chromeos/input_method/candidate_window_view.h"
+#include "chrome/browser/chromeos/input_method/infolist_window_view.h"
 #include "ui/views/widget/widget.h"
 
 #if defined(USE_ASH)
