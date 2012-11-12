a22bcfa900a8bfec11c5d430842c752e30402770
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-12 01:32:20 +0000

    browser: Move FindLastActiveWithProfile() into chrome namespace.
    BUG=133088
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/11363188
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167131 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_finder.cc
+++ chrome/browser/ui/browser_finder.cc
@@ -195,6 +195,10 @@ Browser* FindBrowserWithWebContents(const WebContents* web_contents) {
   return NULL;
 }
 
+}  // namespace browser
+
+namespace chrome {
+
 Browser* FindLastActiveWithProfile(Profile* profile) {
   // We are only interested in last active browsers, so we don't fall back to
   // all browsers like FindBrowserWith* do.
@@ -202,10 +206,6 @@ Browser* FindLastActiveWithProfile(Profile* profile) {
       BrowserList::begin_last_active(), BrowserList::end_last_active(), profile,
       Browser::FEATURE_NONE, kMatchAny);
 }
-
-}  // namespace browser
-
-namespace chrome {
 
 Browser* FindLastActiveWithHostDesktopType(HostDesktopType type) {
   BrowserListImpl* browser_list_impl = BrowserListImpl::GetInstance(type);
