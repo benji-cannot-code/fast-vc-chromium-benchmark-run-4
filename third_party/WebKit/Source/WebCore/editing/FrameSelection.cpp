9a5ad7955e9a4784052a76a7b6bcf2a5f7200d6e
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-12 09:30:17 +0000

    Build fix after r134191. Turns out that FrameView::performPostLayoutTasks calls FrameSelection::updateAppearance in the middle of a layout. So we can't have assertions in recomputeCaretRect and updateAppearance.
    Furthermore, we can't update layout in updateAppearance. So do that in its call sites.
    
    * editing/FrameSelection.cpp:
    (WebCore::FrameSelection::setSelection):
    (WebCore::FrameSelection::recomputeCaretRect):
    (WebCore::FrameSelection::updateAppearance):
    (WebCore::FrameSelection::setCaretVisibility):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134197 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
+++ third_party/WebKit/Source/WebCore/editing/FrameSelection.cpp
@@ -296,8 +296,14 @@ void FrameSelection::setSelection(const VisibleSelection& newSelection, SetSelec
     if (!s.isNone() && !(options & DoNotSetFocus))
         setFocusedNodeIfNeeded();
 
-    if (!(options & DoNotUpdateAppearance))
+    if (!(options & DoNotUpdateAppearance)) {
+#if ENABLE(TEXT_CARET)
+        m_frame->document()->updateLayoutIgnorePendingStylesheets();
+#else
+        m_frame->document()->updateStyleIfNeeded();
+#endif
         updateAppearance();
+    }
 
     // Always clear the x position used for vertical arrow navigation.
     // It will be restored by the vertical arrow navigation code if necessary.
@@ -1342,7 +1348,6 @@ bool FrameSelection::recomputeCaretRect()
     FrameView* v = m_frame->document()->view();
     if (!v)
         return false;
-    ASSERT(!v->needsLayout());
 
     LayoutRect oldRect = localCaretRectWithoutUpdate();
     LayoutRect newRect = localCaretRect();
@@ -1744,7 +1749,6 @@ inline static bool shouldStopBlinkingDueToTypingCommand(Frame* frame)
 void FrameSelection::updateAppearance()
 {
 #if ENABLE(TEXT_CARET)
-    m_frame->document()->updateLayout();
     bool caretRectChangedOrCleared = recomputeCaretRect();
 
     bool caretBrowsing = m_frame->settings() && m_frame->settings()->caretBrowsingEnabled();
@@ -1766,9 +1770,6 @@ void FrameSelection::updateAppearance()
             invalidateCaretRect();
         }
     }
-#else
-    // We need to update style in case the node containing the selection is made display:none.
-    m_frame->document()->updateStyleIfNeeded();
 #endif
 
     RenderView* view = m_frame->contentRenderer();
@@ -1818,6 +1819,8 @@ void FrameSelection::setCaretVisibility(CaretVisibility visibility)
         invalidateCaretRect();
     }
     CaretBase::setCaretVisibility(visibility);
+#else
+    m_frame->document()->updateStyleIfNeeded();
 #endif
 
     updateAppearance();
