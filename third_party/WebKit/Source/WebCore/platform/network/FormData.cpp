c17a5677f37bdba3015cf1b61e4d094f258a1ff2
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-12 19:13:08 +0000

    Move resolving blob references to FormData. https://bugs.webkit.org/show_bug.cgi?id=101754
    Reviewed by Simon Hausmann.
    
    Resolving Blob-references to a set of just File and Data is done similar by several platforms.
    This patch adds a generic implementation in FormData and uses that from CFNetwork, Qt and
    BlackBerry network implementation.
    
    * platform/network/FormData.cpp:
    (WebCore::appendBlobResolved):
    (WebCore::FormData::resolveBlobReferences):
    * platform/network/FormData.h:
    * platform/network/blackberry/ResourceRequestBlackBerry.cpp:
    (WebCore::ResourceRequest::initializePlatformRequest):
    * platform/network/cf/FormDataStreamCFNet.cpp:
    (WebCore::setHTTPBody):
    * platform/network/qt/QNetworkReplyHandler.cpp:
    (WebCore::FormDataIODevice::prepareFormElements):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/FormData.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/FormData.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2004, 2006, 2008, 2011 Apple Inc. All rights reserved.
  * Copyright (C) 2009 Google Inc. All rights reserved.
+ * Copyright (C) 2012 Digia Plc. and/or its subsidiary(-ies)
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FormData.h"
 
 #include "BlobData.h"
+#include "BlobRegistryImpl.h"
 #include "BlobURL.h"
 #include "Chrome.h"
 #include "ChromeClient.h"
@@ -317,6 +319,64 @@ String FormData::flattenToString() const
     flatten(bytes);
     return Latin1Encoding().decode(reinterpret_cast<const char*>(bytes.data()), bytes.size());
 }
+
+#if ENABLE(BLOB)
+static void appendBlobResolved(FormData* formData, const KURL& url)
+{
+    RefPtr<BlobStorageData> blobData = static_cast<BlobRegistryImpl&>(blobRegistry()).getBlobDataFromURL(KURL(ParsedURLString, url));
+    if (!blobData)
+        return;
+
+    BlobDataItemList::const_iterator it = blobData->items().begin();
+    const BlobDataItemList::const_iterator itend = blobData->items().end();
+    for (; it != itend; ++it) {
+        const BlobDataItem& blobItem = *it;
+        if (blobItem.type == BlobDataItem::Data)
+            formData->appendData(blobItem.data->data() + static_cast<int>(blobItem.offset), static_cast<int>(blobItem.length));
+        else if (blobItem.type == BlobDataItem::File)
+            formData->appendFileRange(blobItem.path, blobItem.offset, blobItem.length, blobItem.expectedModificationTime);
+        else if (blobItem.type == BlobDataItem::Blob)
+            appendBlobResolved(formData, blobItem.url);
+        else
+            ASSERT_NOT_REACHED();
+    }
+}
+
+PassRefPtr<FormData> FormData::resolveBlobReferences()
+{
+    // First check if any blobs needs to be resolved, or we can take the fast path.
+    bool hasBlob = false;
+    Vector<FormDataElement>::const_iterator it = elements().begin();
+    const Vector<FormDataElement>::const_iterator itend = elements().end();
+    for (; it != itend; ++it) {
+        if (it->m_type == FormDataElement::encodedBlob) {
+            hasBlob = true;
+            break;
+        }
+    }
+
+    if (!hasBlob)
+        return this;
+
+    // Create a copy to append the result into.
+    RefPtr<FormData> newFormData = FormData::create();
+    newFormData->setAlwaysStream(alwaysStream());
+    newFormData->setIdentifier(identifier());
+    it = elements().begin();
+    for (; it != itend; ++it) {
+        const FormDataElement& element = *it;
+        if (element.m_type == FormDataElement::data)
+            newFormData->appendData(element.m_data.data(), element.m_data.size());
+        else if (element.m_type == FormDataElement::encodedFile)
+            newFormData->appendFileRange(element.m_filename, element.m_fileStart, element.m_fileLength, element.m_expectedFileModificationTime, element.m_shouldGenerateFile);
+        else if (element.m_type == FormDataElement::encodedBlob)
+            appendBlobResolved(newFormData.get(), element.m_url);
+        else
+            ASSERT_NOT_REACHED();
+    }
+    return newFormData.release();
+}
+#endif
 
 void FormData::generateFiles(Document* document)
 {
