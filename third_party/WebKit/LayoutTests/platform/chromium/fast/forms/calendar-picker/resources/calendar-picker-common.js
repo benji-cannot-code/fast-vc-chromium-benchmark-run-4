45b88a6ffe283757524eea618f9fcf3f80b20b81
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-12 12:31:58 +0000

    Add tests for month picker https://bugs.webkit.org/show_bug.cgi?id=101556
    Reviewed by Kent Tamura.
    
    Adding tests for month picker.
    
    * platform/chromium-mac/platform/chromium/fast/forms/calendar-picker/month-picker-appearance-expected.png: Added.
    * platform/chromium-mac/platform/chromium/fast/forms/calendar-picker/month-picker-appearance-step-expected.png: Added.
    * platform/chromium-win/platform/chromium/fast/forms/calendar-picker/month-picker-key-operations-expected.txt: Added.
    * platform/chromium/TestExpectations: Marking appearance tests as needs rebaseline.
    * platform/chromium/fast/forms/calendar-picker/month-picker-appearance-expected.txt: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-appearance-step-expected.txt: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-appearance-step.html: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-appearance.html: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-key-operations-expected.txt: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-key-operations.html: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-mouse-operations-expected.txt: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-mouse-operations.html: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-with-step-expected.txt: Added.
    * platform/chromium/fast/forms/calendar-picker/month-picker-with-step.html: Added.
    * platform/chromium/fast/forms/calendar-picker/resources/calendar-picker-common.js:
    (selectedMonth): Returns ISO string for selected month.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/platform/chromium/fast/forms/calendar-picker/resources/calendar-picker-common.js
+++ third_party/WebKit/LayoutTests/platform/chromium/fast/forms/calendar-picker/resources/calendar-picker-common.js
@@ -24,3 +24,10 @@ function selectedDates() {
         return element.dataset.submitValue;
     }).sort();
 }
+
+function selectedMonth() {
+    var selected = popupWindow.document.querySelectorAll(".day.day-selected");
+    if (selected.length === 0)
+        return null;
+    return selected[0].dataset.monthValue;
+}
