0a8bfe7043cd07c1116333b78a6ec1541aaf8fbe
scottmg@google.com <scottmg@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-12 22:05:18 +0000

    fix vs2012 after https://chromiumcodereview.appspot.com/11092029
    http://build.chromium.org/p/chromium.fyi/waterfall?show=Chromium%20Win%20VS2012
    
    FAILED: ninja -t msvc -r . -o obj\chrome\browser\ui\sync\browser_ui.one_click_signin_helper.obj -e environment.x86 -- cl.exe /nologo /showIncludes /FC @obj\chrome\browser\ui\sync\browser_ui.one_click_signin_helper.obj.rsp  /c ..\..\chrome\browser\ui\sync\one_click_signin_helper.cc /Foobj\chrome\browser\ui\sync\browser_ui.one_click_signin_helper.obj  /Fdbrowser_ui.pdb
    e:\b\build\slave\chromium_win_vs2012\build\src\chrome\browser\ui\sync\one_click_signin_helper.cc(483) :error C2039: 'bind2nd' : is not a member of 'std'
    
    e:\b\build\slave\chromium_win_vs2012\build\src\chrome\browser\ui\sync\one_click_signin_helper.cc(483) :error C3861: 'bind2nd': identifier not found
    
    TBR=rogerta@chromium.org
    
    Review URL: https://codereview.chromium.org/11361228
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167248 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_helper.cc
+++ chrome/browser/ui/sync/one_click_signin_helper.cc
@@ -57,6 +57,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
 
+#include <functional>
+
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(OneClickSigninHelper)
 
 namespace {
