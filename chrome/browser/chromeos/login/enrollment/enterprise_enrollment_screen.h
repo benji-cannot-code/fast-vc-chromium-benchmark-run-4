64f2721064fbd7a5161692e3377dc751b13189aa
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-12 16:20:14 +0000

    Wire up enrollment with re-written enterprise enrollment code.
    If --enable-cloud-policy-service is specified, have enrollment go
    through DeviceCloudPolicyManagerChromeOS.
    
    BUG=chromium:108928
    TEST=Specify --enable-cloud-policy-service flag, check that enrollment still working.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11233062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/enrollment/enterprise_enrollment_screen.h
+++ chrome/browser/chromeos/login/enrollment/enterprise_enrollment_screen.h
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/wizard_screen.h"
 #include "chrome/browser/policy/cloud_policy_constants.h"
 #include "chrome/browser/policy/cloud_policy_subsystem.h"
-#include "chrome/browser/policy/device_cloud_policy_manager_chromeos.h"
 
 namespace chromeos {
 
