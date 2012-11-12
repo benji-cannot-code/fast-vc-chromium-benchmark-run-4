053523a4809228923b5d945fb64fe29286167271
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-12 18:50:02 +0000

    Reverting this to see if this fixes browser tests redness on linux chromium os (2). Please reland with clean try runs.
    Revert 167195 - Eliminate use of window.localStorage in drive_banners.js
    
    BUG=160002
    TEST=Check banner showing loginc.
    
    Review URL: https://codereview.chromium.org/11377099
    
    TBR=serya@google.com
    Review URL: https://codereview.chromium.org/11369191
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167216 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_model.js
+++ chrome/browser/resources/file_manager/js/directory_model.js
@@ -659,7 +659,7 @@ DirectoryModel.prototype.changeDirectory = function(path) {
 DirectoryModel.prototype.resolveDirectory = function(path, successCallback,
                                                      errorCallback) {
   if (PathUtil.getRootType(path) == RootType.GDATA) {
-    if (!this.isDriveMounted()) {
+    if (!this.isGDataMounted_()) {
       if (path == DirectoryModel.fakeGDataEntry_.fullPath)
         successCallback(DirectoryModel.fakeGDataEntry_);
       else  // Subdirectory.
@@ -1020,7 +1020,7 @@ DirectoryModel.prototype.resolveRoots_ = function(callback) {
 
   if (this.gDataEnabled_) {
     var fake = [DirectoryModel.fakeGDataEntry_];
-    if (this.isDriveMounted()) {
+    if (this.isGDataMounted_()) {
       readSingle(RootDirectory.GDATA.substring(1), 'gdata', fake);
     } else {
       groups.gdata = fake;
@@ -1070,8 +1070,9 @@ DirectoryModel.prototype.updateRootsListSelection_ = function() {
 
 /**
  * @return {boolean} True if GDATA is fully mounted.
+ * @private
  */
-DirectoryModel.prototype.isDriveMounted = function() {
+DirectoryModel.prototype.isGDataMounted_ = function() {
   return this.volumeManager_.getGDataStatus() ==
       VolumeManager.GDataStatus.MOUNTED;
 };
@@ -1099,7 +1100,7 @@ DirectoryModel.prototype.onGDataStatusChanged_ = function() {
   if (this.getCurrentRootType() != RootType.GDATA)
      return;
 
-  var mounted = this.isDriveMounted();
+  var mounted = this.isGDataMounted_();
   if (this.getCurrentDirEntry() == DirectoryModel.fakeGDataEntry_) {
     if (mounted) {
       // Change fake entry to real one and rescan.
