48e841fb64aeccacc8e9ee5f377e6f630df35a7f
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-12 21:20:32 +0000

    Close the task manager dialog on shutdown for aura. This is done by implementing views::Widget::CloseAllSecondaryWidgets()" on win aura. Review URL: https://codereview.chromium.org/11359154
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/lifetime/application_lifetime_aura.cc
+++ chrome/browser/lifetime/application_lifetime_aura.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
+#include "ui/views/widget/widget.h"
 
 #if defined(USE_ASH)
 #include "ash/shell.h"
@@ -34,8 +35,7 @@ void HandleAppExitingForPlatform() {
   g_browser_process->notification_ui_manager()->CancelAll();
 #endif
 
-  // TODO(oshima): Close all non browser windows here while
-  // the message loop is still alive.
+  views::Widget::CloseAllSecondaryWidgets();
 
 #if defined(OS_CHROMEOS)
   if (!CommandLine::ForCurrentProcess()->HasSwitch(
