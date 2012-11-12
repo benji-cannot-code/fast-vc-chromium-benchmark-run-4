18f9f32ed9300795c78bbcb79cc38664fd23c6d2
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-12 22:36:01 +0000

    webkitpy: integrate pylint into check-webkit-style, part I https://bugs.webkit.org/show_bug.cgi?id=101285
    Reviewed by Ojan Vafai.
    
    This patch re-works lint-webkitpy so that the logic is pushed
    into check-webkit-style (mostly); we don't yet control which
    messages are displayed using the rules in webkitpy/style/checker.py
    (we're still using the pylintrc to suppress messages instead),
    but otherwise things work. For now we will only report pylint
    "errors", not warnings.
    
    * Scripts/lint-webkitpy:
    * Scripts/webkitpy/style/checker.py:
    * Scripts/webkitpy/style/checkers/python.py:
    (PythonChecker):
    (PythonChecker.check):
    (PythonChecker._check_pep8):
    (PythonChecker._check_pylint):
    (Pylinter):
    (Pylinter.__init__):
    (Pylinter.run):
    (_FilteredStringIO):
    (_FilteredStringIO.__init__):
    (_FilteredStringIO.write):
    (_FilteredStringIO._filter):
    * Scripts/webkitpy/style/checkers/python_unittest.py:
    (PythonCheckerTest.test_check):
    * Scripts/webkitpy/style/checkers/python_unittest_input.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134309 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/python_unittest_input.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/style/checkers/python_unittest_input.py
@@ -1,3 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# This file is sample input for python_unittest.py and includes a single
-# error which is an extra space at the end of this line. 
+# This file is sample input for python_unittest.py and includes two
+# problems, one that will generate a PEP-8 warning for trailing whitespace
+# and one that will generate a pylint error for an undefined variable.
+print error() 
