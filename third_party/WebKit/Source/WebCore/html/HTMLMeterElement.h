a492587695d03df4f87046bcc85113a1d17ec079
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-12 09:15:13 +0000

    Don't update style when attaching in HTMLMeterElement https://bugs.webkit.org/show_bug.cgi?id=101714
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    HTMLMeterElement was updating style when it's being attached. However, updating style when attaching
    can cause style-update prevention. The similar thing has already happened in Bug 100507.
    
    Since we have already set the default value in creating ShadowDOM subtree, we don't need to update style actually.
    
    Test: fast/dom/HTMLMeterElement/meter-bar-set-value.html
    
    * html/HTMLMeterElement.cpp:
    * html/HTMLMeterElement.h:
    (HTMLMeterElement): Removed attach(). We don't need it.
    
    LayoutTests:
    
    * fast/dom/HTMLMeterElement/meter-bar-set-value-expected.html: Added.
    * fast/dom/HTMLMeterElement/meter-bar-set-value.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMeterElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLMeterElement.h
@@ -82,8 +82,6 @@ private:
     virtual bool childShouldCreateRenderer(const NodeRenderingContext&) const OVERRIDE;
     virtual void parseAttribute(const Attribute&) OVERRIDE;
 
-    virtual void attach() OVERRIDE;
-
     void didElementStateChange();
     void createShadowSubtree();
 
