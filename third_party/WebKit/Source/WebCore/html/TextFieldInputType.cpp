aa7ca1e2309eea685c832a96d8af8653e90d6775
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 23:53:50 +0000

    Missing NodeRenderStyle.h include in WebCore/html/TextFieldInputType.cpp https://bugs.webkit.org/show_bug.cgi?id=102108
    Reviewed by Adam Barth.
    
    No new tests. Fix clang build break.
    
    * html/TextFieldInputType.cpp: Added #include "NodeRenderStyle.h"
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134500 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/TextFieldInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/TextFieldInputType.cpp
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLInputElement.h"
 #include "HTMLNames.h"
 #include "KeyboardEvent.h"
+#include "NodeRenderStyle.h"
 #include "Page.h"
 #include "RenderLayer.h"
 #include "RenderTextControlSingleLine.h"
