0e5a304cc86473d9a4c31d7a1350a807e877544a
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 17:47:00 +0000

    Unreviewed, rolling out r134418. http://trac.webkit.org/changeset/134418 https://bugs.webkit.org/show_bug.cgi?id=101903
    The newly added test is crashing on cr-win.
    
    Source/WebCore:
    
    * dom/ElementShadow.cpp:
    * dom/ElementShadow.h:
    * html/HTMLInputElement.cpp:
    (WebCore::HTMLInputElement::updateType):
    (WebCore::HTMLInputElement::setIndeterminate):
    * html/HTMLProgressElement.cpp:
    (WebCore::HTMLProgressElement::didElementStateChange):
    * html/shadow/SelectRuleFeatureSet.cpp:
    (WebCore::SelectRuleFeatureSet::add):
    (WebCore::SelectRuleFeatureSet::clear):
    (WebCore::SelectRuleFeatureSet::collectFeaturesFromSelector):
    * html/shadow/SelectRuleFeatureSet.h:
    (SelectRuleFeatureSet):
    
    LayoutTests:
    
    * fast/dom/shadow/pseudoclass-update-indeterminate-input-expected.html: Removed.
    * fast/dom/shadow/pseudoclass-update-indeterminate-input.html: Removed.
    * fast/dom/shadow/pseudoclass-update-indeterminate-progress-expected.html: Removed.
    * fast/dom/shadow/pseudoclass-update-indeterminate-progress.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134432 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLInputElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLInputElement.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CSSValueKeywords.h"
 #include "DateTimeChooser.h"
 #include "Document.h"
-#include "ElementShadow.h"
 #include "EventNames.h"
 #include "ExceptionCode.h"
 #include "FileInputType.h"
@@ -52,7 +51,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLParserIdioms.h"
 #include "IdTargetObserver.h"
 #include "InputType.h"
-#include "InsertionPoint.h"
 #include "KeyboardEvent.h"
 #include "LocalizedStrings.h"
 #include "MouseEvent.h"
@@ -534,9 +532,6 @@ void HTMLInputElement::updateType()
             updateFocusAppearance(true);
     }
 
-    if (ElementShadow* elementShadow = shadowOfParentForDistribution(this))
-        elementShadow->invalidateDistribution();
-
     setChangedSinceLastFormControlChangeEvent(false);
 
     addToRadioButtonGroup();
@@ -920,7 +915,6 @@ void HTMLInputElement::setIndeterminate(bool newValue)
     m_isIndeterminate = newValue;
 
     setNeedsStyleRecalc();
-    invalidateParentDistributionIfNecessary(this, CSSSelector::PseudoIndeterminate);
 
     if (renderer() && renderer()->style()->hasAppearance())
         renderer()->theme()->stateChanged(renderer(), CheckedState);
--- third_party/WebKit/Source/WebCore/html/HTMLProgressElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLProgressElement.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLParserIdioms.h"
 #include "ProgressShadowElement.h"
 #include "RenderProgress.h"
-#include "SelectRuleFeatureSet.h"
 #include "ShadowRoot.h"
 #include <wtf/StdLibExtras.h>
 
@@ -160,10 +159,8 @@ void HTMLProgressElement::didElementStateChange()
     if (RenderProgress* render = renderProgress()) {
         bool wasDeterminate = render->isDeterminate();
         render->updateFromElement();
-        if (wasDeterminate != isDeterminate()) {
+        if (wasDeterminate != isDeterminate())
             setNeedsStyleRecalc();
-            invalidateParentDistributionIfNecessary(this, CSSSelector::PseudoIndeterminate);
-        }
     }
 }
 
