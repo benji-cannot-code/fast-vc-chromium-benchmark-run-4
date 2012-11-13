070939031de0f3a96c7599d1274e6a5310fd4d43
kling@webkit.org <kling@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 14:21:28 +0000

    Exploit ElementAttributeData sharing in Node.cloneNode. <http://webkit.org/b/101374>
    Reviewed by Anders Carlsson.
    
    Instead of blindly creating a new ElementAttributeData for Node.cloneNode, let's be clever!
    If the source data is immutable, simply ref it from the new node at virtually no cost.
    
    If the source data is mutable, convert it to immutable data so it can be shared between both nodes.
    Since the typical use-case for Node.cloneNode is to create-once/clone-many, this saves both time
    and memory in the long run.
    
    ~8% speed-up on PerformanceTests/DOM/CloneNodes on my MBP.
    
    * dom/Element.cpp:
    (WebCore::Element::cloneAttributesFromElement):
    
        Move attribute data cloning logic from ElementAttributeData to Element.
    
    * dom/ElementAttributeData.cpp:
    (WebCore::ImmutableElementAttributeData::~ImmutableElementAttributeData):
    (WebCore::ImmutableElementAttributeData::ImmutableElementAttributeData):
    (WebCore::ElementAttributeData::ElementAttributeData):
    (WebCore::MutableElementAttributeData::MutableElementAttributeData):
    (WebCore::ElementAttributeData::makeMutableCopy):
    (WebCore::ElementAttributeData::makeImmutableCopy):
    * dom/ElementAttributeData.h:
    (ElementAttributeData):
    (ImmutableElementAttributeData):
    (MutableElementAttributeData):
    
        Add facilities for converting a mutable ElementAttributeData to an immutable one.
        Share some code in the common base class constructor.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134408 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ElementAttributeData.h
+++ third_party/WebKit/Source/WebCore/dom/ElementAttributeData.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *           (C) 1999 Antti Koivisto (koivisto@kde.org)
  *           (C) 2001 Peter Kelly (pmk@post.com)
  *           (C) 2001 Dirk Mueller (mueller@kde.org)
- * Copyright (C) 2003, 2004, 2005, 2006, 2008, 2010 Apple Inc. All rights reserved.
+ * Copyright (C) 2003, 2004, 2005, 2006, 2008, 2010, 2012 Apple Inc. All rights reserved.
  * Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies).
  *
  * This library is free software; you can redistribute it and/or
@@ -102,6 +102,8 @@ protected:
         , m_arraySize(arraySize)
     { }
 
+    ElementAttributeData(const ElementAttributeData&, bool isMutable);
+
     unsigned m_isMutable : 1;
     unsigned m_arraySize : 31;
 
@@ -119,10 +121,9 @@ private:
     Attribute* getAttributeItem(const AtomicString& name, bool shouldIgnoreAttributeCase);
     const Attribute* getAttributeItem(const AtomicString& name, bool shouldIgnoreAttributeCase) const;
     size_t getAttributeItemIndexSlowCase(const AtomicString& name, bool shouldIgnoreAttributeCase) const;
-    void cloneDataFrom(const ElementAttributeData& sourceData, const Element& sourceElement, Element& targetElement);
-    void clearAttributes();
 
     PassRefPtr<ElementAttributeData> makeMutableCopy() const;
+    PassRefPtr<ElementAttributeData> makeImmutableCopy() const;
 
     Vector<Attribute, 4>& mutableAttributeVector();
     const Vector<Attribute, 4>& mutableAttributeVector() const;
@@ -131,6 +132,7 @@ private:
 class ImmutableElementAttributeData : public ElementAttributeData {
 public:
     ImmutableElementAttributeData(const Vector<Attribute>&);
+    ImmutableElementAttributeData(const MutableElementAttributeData&);
     ~ImmutableElementAttributeData();
 
     void* m_attributeArray;
@@ -140,6 +142,7 @@ class MutableElementAttributeData : public ElementAttributeData {
 public:
     MutableElementAttributeData() { }
     MutableElementAttributeData(const ImmutableElementAttributeData&);
+    MutableElementAttributeData(const MutableElementAttributeData&);
 
     Vector<Attribute, 4> m_attributeVector;
 };
