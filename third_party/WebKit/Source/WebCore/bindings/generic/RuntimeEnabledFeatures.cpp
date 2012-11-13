4db336bbe02a4f4a60b4dd30d323552befa30138
keishi@webkit.org <keishi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 16:06:41 +0000

    [Chromium] Enable input type month/week https://bugs.webkit.org/show_bug.cgi?id=102042
    Reviewed by Kent Tamura.
    
    Enable input type month/week for Chromium.
    
    No new tests. Tests will be added later in Bug 102045 and Bug 102046.
    
    * bindings/generic/RuntimeEnabledFeatures.cpp:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134421 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.cpp
+++ third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.cpp
@@ -218,11 +218,7 @@ bool RuntimeEnabledFeatures::isInputTypeDateTimeLocalEnabled = true;
 #endif
 
 #if ENABLE(INPUT_TYPE_MONTH)
-#if PLATFORM(CHROMIUM) && !OS(ANDROID)
-bool RuntimeEnabledFeatures::isInputTypeMonthEnabled = false;
-#else
 bool RuntimeEnabledFeatures::isInputTypeMonthEnabled = true;
-#endif
 #endif
 
 #if ENABLE(INPUT_TYPE_TIME)
@@ -230,11 +226,7 @@ bool RuntimeEnabledFeatures::isInputTypeTimeEnabled = true;
 #endif
 
 #if ENABLE(INPUT_TYPE_WEEK)
-#if PLATFORM(CHROMIUM) && !OS(ANDROID)
-bool RuntimeEnabledFeatures::isInputTypeWeekEnabled = false;
-#else
 bool RuntimeEnabledFeatures::isInputTypeWeekEnabled = true;
-#endif
 #endif
 
 #if ENABLE(DIALOG_ELEMENT)
