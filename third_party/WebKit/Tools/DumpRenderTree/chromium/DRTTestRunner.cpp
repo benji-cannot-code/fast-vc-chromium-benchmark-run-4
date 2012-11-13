484e5339d10ce2aed830ace8fa73cb6193892771
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 10:53:50 +0000

    [chromium] move tracking of damaged regions from WebViewHost to WebTestProxy https://bugs.webkit.org/show_bug.cgi?id=101927
    Reviewed by Adam Barth.
    
    This will allow for sharing the code with content_shell
    
    * DumpRenderTree/chromium/DRTTestRunner.cpp:
    (DRTTestRunner::display):
    * DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h:
    (WebKit):
    (WebTestProxyBase):
    (WebTestRunner::WebTestProxy::didInvalidateRect):
    (WebTestRunner::WebTestProxy::didScrollRect):
    (WebTestRunner::WebTestProxy::scheduleComposite):
    (WebTestRunner::WebTestProxy::scheduleAnimation):
    (WebTestRunner::WebTestProxy::setWindowRect):
    (WebTestRunner::WebTestProxy::show):
    (WebTestRunner::WebTestProxy::didAutoResize):
    (WebTestRunner::WebTestProxy::postAccessibilityNotification):
    * DumpRenderTree/chromium/TestRunner/src/WebTestProxy.cpp:
    (WebTestRunner::WebTestProxyBase::setPaintRect):
    (WebTestRunner):
    (WebTestRunner::WebTestProxyBase::paintRect):
    (WebTestRunner::WebTestProxyBase::didInvalidateRect):
    (WebTestRunner::WebTestProxyBase::didScrollRect):
    (WebTestRunner::WebTestProxyBase::scheduleComposite):
    (WebTestRunner::WebTestProxyBase::scheduleAnimation):
    (WebTestRunner::WebTestProxyBase::show):
    (WebTestRunner::WebTestProxyBase::setWindowRect):
    (WebTestRunner::WebTestProxyBase::didAutoResize):
    (WebTestRunner::WebTestProxyBase::postAccessibilityNotification):
    * DumpRenderTree/chromium/TestShell.cpp:
    (TestShell::createNewWindow):
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::scheduleAnimation):
    (WebViewHost::show):
    (WebViewHost::setWindowRect):
    (WebViewHost::WebViewHost):
    (WebViewHost::proxy):
    (WebViewHost::setProxy):
    (WebViewHost::reset):
    (WebViewHost::paintInvalidatedRegion):
    * DumpRenderTree/chromium/WebViewHost.h:
    (WebViewHost):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/DRTTestRunner.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/DRTTestRunner.cpp
@@ -1267,7 +1267,7 @@ void DRTTestRunner::display(const CppArgumentList& arguments, CppVariant* result
     WebViewHost* host = m_shell->webViewHost();
     const WebKit::WebSize& size = m_shell->webView()->size();
     WebRect rect(0, 0, size.width, size.height);
-    host->updatePaintRect(rect);
+    host->proxy()->setPaintRect(rect);
     host->paintInvalidatedRegion();
     host->displayRepaintMask();
     result->setNull();
--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.h
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebSpellCheckClient.h"
 #include "WebTask.h"
 #include "WebTestDelegate.h"
+#include "WebTestProxy.h"
 #include "WebViewClient.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
@@ -87,6 +88,8 @@ class WebViewHost : public WebKit::WebViewClient, public WebKit::WebFrameClient,
     void setWebWidget(WebKit::WebWidget*);
     WebKit::WebView* webView() const;
     WebKit::WebWidget* webWidget() const;
+    WebTestRunner::WebTestProxyBase* proxy() const;
+    void setProxy(WebTestRunner::WebTestProxyBase*);
     void reset();
     void setSelectTrailingWhitespaceEnabled(bool);
     void setSmartInsertDeleteEnabled(bool);
@@ -100,7 +103,6 @@ class WebViewHost : public WebKit::WebViewClient, public WebKit::WebFrameClient,
     void setDeviceScaleFactor(float);
 
     void paintRect(const WebKit::WebRect&);
-    void updatePaintRect(const WebKit::WebRect&);
     void paintInvalidatedRegion();
     void paintPagesWithBoundaries();
     SkCanvas* canvas();
@@ -206,14 +208,8 @@ class WebViewHost : public WebKit::WebViewClient, public WebKit::WebFrameClient,
     virtual void printPage(WebKit::WebFrame*);
 
     // WebKit::WebWidgetClient
-    virtual void didInvalidateRect(const WebKit::WebRect&);
-    virtual void didScrollRect(int dx, int dy, const WebKit::WebRect&);
     virtual void didAutoResize(const WebKit::WebSize& newSize);
-    virtual void scheduleComposite();
-#if ENABLE(REQUEST_ANIMATION_FRAME)
-    virtual void serviceAnimation();
     virtual void scheduleAnimation();
-#endif
     virtual void didFocus();
     virtual void didBlur();
     virtual void didChangeCursor(const WebKit::WebCursorInfo&);
@@ -369,6 +365,9 @@ private:
 
     // Non-owning pointer. The WebViewHost instance is owned by this TestShell instance.
     TestShell* m_shell;
+
+    // Non-owning pointer. This class needs to be wrapped in a WebTestProxy. This is the pointer to the WebTestProxyBase.
+    WebTestRunner::WebTestProxyBase* m_proxy;
 
     // This delegate works for the following widget.
     WebKit::WebWidget* m_webWidget;
