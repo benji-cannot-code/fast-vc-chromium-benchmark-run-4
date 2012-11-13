7d292352ad19dacf99438282885576e85034bf9b
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 05:03:22 +0000

    Make ash hack CHROMEOS specific to fix ash on Windows where the hack is not needed.
    This hack on Windows otherwise causes a hang when opening a Desktop window when ash is also running in Metro.
    
    (this hang is not reproducable right now as it's hidden by an earlier DCHECK fixed in upcoming CL https://codereview.chromium.org/11363124/)
    
    TEST=Able to open Desktop Aura windows when ash is running in Metro (requires https://codereview.chromium.org/11363124/).
    
    Review URL: https://chromiumcodereview.appspot.com/11359115
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -810,7 +810,7 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(Browser* browser,
 #endif
   }
 
-#if defined(USE_ASH)
+#if defined(OS_CHROMEOS)
   if (ash::Shell::HasInstance()) {
     // Set the browser's root window to be an active root window now so
     // that that web contents can determine correct scale factor for the
@@ -818,7 +818,7 @@ Browser* StartupBrowserCreatorImpl::OpenTabsInBrowser(Browser* browser,
     // this, the renderer may use wrong scale factor first, then
     // switched to the correct scale factor, which can cause race
     // condition and lead to the results rendered at wrong scale factor.
-    // Long term fix is tracked in crbug.com/15543.
+    // Long term fix is tracked in crbug.com/155443.
     ash::Shell::GetInstance()->set_active_root_window(
         browser->window()->GetNativeWindow()->GetRootWindow());
   }
