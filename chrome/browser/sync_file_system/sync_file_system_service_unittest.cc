eebeb4527b3fe4002321a86faf4b36d6a5000ace
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 09:18:58 +0000

    Makes SharedCallbackRunner thread safe
    Also makes GetConflictFiles non-flaky
    (so that we can be notified when failure happens again)
    
    BUG=160694
    TEST=SyncFileSystemServiceTest.GetConflictFiles
    
    Review URL: https://codereview.chromium.org/11362222
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167336 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service_unittest.cc
+++ chrome/browser/sync_file_system/sync_file_system_service_unittest.cc
@@ -154,7 +154,7 @@ TEST_F(SyncFileSystemServiceTest, GetConflictFilesWithoutInitialize) {
   }
 }
 
-TEST_F(SyncFileSystemServiceTest, FLAKY_GetConflictFiles) {
+TEST_F(SyncFileSystemServiceTest, GetConflictFiles) {
   InitializeApp();
 
   // 1. Sets up (conflicting) files.
