63d84f6b863eb26614df6198d4f516e75337f890
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 11:30:56 +0000

    Removing a vestigial reference to spellcheck_host.h which will be deleted soon.
    BUG=154923
    
    TBR=brettw@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11358209
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167373 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.cc
+++ chrome/browser/tab_contents/render_view_context_menu.cc
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/speech/chrome_speech_recognition_preferences.h"
-#include "chrome/browser/spellchecker/spellcheck_host.h"
+#include "chrome/browser/spellchecker/spellcheck_service.h"
 #include "chrome/browser/spellchecker/spellcheck_host_metrics.h"
 #include "chrome/browser/tab_contents/retargeting_details.h"
 #include "chrome/browser/tab_contents/spellchecker_submenu_observer.h"
