f3d12c67cb2e6230c953c8a2f5ff58ebc43fc362
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 22:43:28 +0000

    Remove Instant histograms.
    These histograms are not being used (I was the only person who ever
    looked at them, and I stopped doing so a long time ago). Besides, they
    are also wrong in some scenarios.
    
    BUG=none
    R=dhollowa@chromium.org
    TEST=none; no change in functionality.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11364220
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167499 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.cc
+++ chrome/browser/instant/instant_controller.cc
@@ -43,15 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-enum PreviewUsageType {
-  PREVIEW_CREATED = 0,
-  PREVIEW_DELETED,
-  PREVIEW_LOADED,
-  PREVIEW_SHOWED,
-  PREVIEW_COMMITTED,
-  PREVIEW_NUM_TYPES,
-};
-
 // An artificial delay (in milliseconds) we introduce before telling the Instant
 // page about the new omnibox bounds, in cases where the bounds shrink. This is
 // to avoid the page jumping up/down very fast in response to bounds changes.
@@ -74,15 +65,6 @@ std::string ModeToString(InstantController::Mode mode) {
 
   NOTREACHED();
   return std::string();
-}
-
-void AddPreviewUsageForHistogram(InstantController::Mode mode,
-                                 PreviewUsageType usage) {
-  DCHECK(0 <= usage && usage < PREVIEW_NUM_TYPES) << usage;
-  base::Histogram* histogram = base::LinearHistogram::FactoryGet(
-      "Instant.Previews" + ModeToString(mode), 1, PREVIEW_NUM_TYPES,
-      PREVIEW_NUM_TYPES + 1, base::Histogram::kUmaTargetedHistogramFlag);
-  histogram->Add(usage);
 }
 
 void AddSessionStorageHistogram(InstantController::Mode mode,
@@ -157,8 +139,6 @@ InstantModel::PreviewState GetNewPreviewState(InstantShownReason reason) {
 }  // namespace
 
 InstantController::~InstantController() {
-  if (GetPreviewContents())
-    AddPreviewUsageForHistogram(mode_, PREVIEW_DELETED);
 }
 
 // static
@@ -415,7 +395,6 @@ void InstantController::CommitCurrentPreview(InstantCommitType type) {
                      history::SOURCE_BROWSED, false);
   }
 
-  AddPreviewUsageForHistogram(mode_, PREVIEW_COMMITTED);
   DeleteLoader();
 
   preview->web_contents()->GetController().PruneAllButActive();
@@ -629,7 +608,6 @@ void InstantController::ShowInstantPreview(InstantLoader* loader,
 }
 
 void InstantController::InstantLoaderPreviewLoaded(InstantLoader* loader) {
-  AddPreviewUsageForHistogram(mode_, PREVIEW_LOADED);
 }
 
 void InstantController::InstantSupportDetermined(InstantLoader* loader,
@@ -690,8 +668,6 @@ void InstantController::ResetLoader(const std::string& instant_url,
       loader_->OnAutocompleteLostFocus();
     loader_->OnActiveTabModeChanged(active_tab_is_ntp_);
 
-    AddPreviewUsageForHistogram(mode_, PREVIEW_CREATED);
-
     // Reset the loader timer.
     stale_loader_timer_.Stop();
     stale_loader_timer_.Start(
@@ -743,10 +719,9 @@ void InstantController::DeleteLoader() {
   last_match_was_search_ = false;
   last_omnibox_bounds_ = gfx::Rect();
   url_for_history_ = GURL();
-  if (GetPreviewContents()) {
-    AddPreviewUsageForHistogram(mode_, PREVIEW_DELETED);
+  if (GetPreviewContents())
     model_.SetPreviewState(InstantModel::NOT_READY, 0, INSTANT_SIZE_PERCENT);
-  }
+
   // Schedule the deletion for later, since we may have gotten here from a call
   // within a |loader_| method (i.e., it's still on the stack). If we deleted
   // the loader immediately, things would still be fine so long as the caller
@@ -766,9 +741,6 @@ void InstantController::Show(InstantShownReason reason,
       model_.preview_state() == InstantModel::NOT_READY)
     return;
 
-  if (model_.preview_state() == InstantModel::NOT_READY ||
-      model_.preview_state() == InstantModel::AWAITING_SUGGESTIONS)
-    AddPreviewUsageForHistogram(mode_, PREVIEW_SHOWED);
   model_.SetPreviewState(GetNewPreviewState(reason), height, units);
 }
 
