8d76b888442eceb4b31184d09dcda654ff810f95
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 05:44:19 +0000

    Revert 167305 - Implement DriveMetadataStore::RemoveOrigin
    BUG=156041
    TEST=DriveMetadataStoreTest.RemoveOrigin
    
    Review URL: https://codereview.chromium.org/11365194
    
    TBR=tzik@chromium.org
    Review URL: https://codereview.chromium.org/11365218
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167308 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_metadata_store.h
+++ chrome/browser/sync_file_system/drive_metadata_store.h
@@ -82,9 +82,6 @@ class DriveMetadataStore
   // |origin| must be a batch sync origin.
   void MoveBatchSyncOriginToIncremental(const GURL& origin);
 
-  void RemoveOrigin(const GURL& origin,
-                    const fileapi::SyncStatusCallback& callback);
-
   // Sets the directory identified by |resource_id| as the sync data directory.
   // All data for the Sync FileSystem should be store into the directory.
   // It is invalid to overwrite the directory.
@@ -115,8 +112,6 @@ class DriveMetadataStore
   void DidInitialize(const InitializationCallback& callback,
                      DriveMetadataDBContents* contents,
                      fileapi::SyncStatusCode error);
-  void DidRemoveOrigin(const fileapi::SyncStatusCallback& callback,
-                       fileapi::SyncStatusCode status);
 
   // These are only for testing.
   void RestoreSyncRootDirectory(const fileapi::SyncStatusCallback& callback);
