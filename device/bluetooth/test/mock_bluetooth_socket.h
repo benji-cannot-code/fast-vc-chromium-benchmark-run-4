00ce9a3e24060fe8bd0e7057725e0c7c7da5d21c
youngki@chromium.org <youngki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 21:56:45 +0000

    Added MockBluetoothSocket.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11358220
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167481 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ device/bluetooth/test/mock_bluetooth_socket.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef DEVICE_BLUETOOTH_TEST_MOCK_BLUETOOTH_SOCKET_H_
+#define DEVICE_BLUETOOTH_TEST_MOCK_BLUETOOTH_SOCKET_H_
+
+#include "device/bluetooth/bluetooth_socket.h"
+#include "testing/gmock/include/gmock/gmock.h"
+
+namespace device {
+
+class MockBluetoothSocket : public BluetoothSocket {
+ public:
+  MockBluetoothSocket();
+  MOCK_CONST_METHOD0(fd, int());
+
+ protected:
+  virtual ~MockBluetoothSocket();
+};
+
+}  // namespace device
+
+#endif  // DEVICE_BLUETOOTH_TEST_MOCK_BLUETOOTH_SOCKET_H_
--- device/device.gyp
+++ device/device.gyp
@@ -64,6 +64,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'bluetooth/test/mock_bluetooth_adapter.h',
         'bluetooth/test/mock_bluetooth_device.cc',
         'bluetooth/test/mock_bluetooth_device.h',
+        'bluetooth/test/mock_bluetooth_socket.cc',
+        'bluetooth/test/mock_bluetooth_socket.h',
       ],
       'include_dirs': [
         '..',
