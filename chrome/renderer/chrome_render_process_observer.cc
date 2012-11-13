c7645637951b93fbcc2877c082296e40a7152c14
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 06:31:09 +0000

    Ensure that field trials synced to the renderer process are considered "active".
    http://crrev.com/166168 made trials on which group() hasn't been called not get
    reported. This resulted in renderer crash reports not being tagged with field
    trials because group() wasn't called on the renderer side after they are
    synchronized from the browser process.
    
    This change makes CreateTrialsFromString() mark trials as used as well as
    making trials synchronized to the renderer later via OnSetFieldTrialGroup()
    notification also get marked as used.
    
    Also fixes a couple of lint warnings in chrome_render_process_observer.cc and
    changes field_trial_unittest.cc to not use a deprecated function.
    
    BUG=158801
    TEST=New FieldTrialTest.CreateFieldTrialIsActive test.
    
    Review URL: https://chromiumcodereview.appspot.com/11376002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167313 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_render_process_observer.cc
+++ chrome/renderer/chrome_render_process_observer.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/chrome_render_process_observer.h"
 
+#include <limits>
+#include <vector>
+
 #include "base/allocator/allocator_extension.h"
 #include "base/bind.h"
 #include "base/command_line.h"
@@ -270,7 +273,11 @@ void ChromeRenderProcessObserver::OnGetCacheResourceStats() {
 void ChromeRenderProcessObserver::OnSetFieldTrialGroup(
     const std::string& field_trial_name,
     const std::string& group_name) {
-  base::FieldTrialList::CreateFieldTrial(field_trial_name, group_name);
+  base::FieldTrial* trial =
+      base::FieldTrialList::CreateFieldTrial(field_trial_name, group_name);
+  // Ensure the trial is marked as "used" by calling group() on it. This is
+  // needed to ensure the trial is properly reported in renderer crash reports.
+  trial->group();
   chrome_variations::SetChildProcessLoggingVariationList();
 }
 
