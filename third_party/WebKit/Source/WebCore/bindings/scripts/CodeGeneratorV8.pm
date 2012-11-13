57470f0d932fd3e9371042c1320aeedc5b577422
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 06:04:36 +0000

    [V8] V8DOMWrapper::instantiateV8Object shouldn't use deprecatedDocument https://bugs.webkit.org/show_bug.cgi?id=102015
    Reviewed by Kentaro Hara.
    
    Previously, Nodes had a special path through
    V8DOMWrapper::instantiateV8Object using deprecatedDocument so that we
    could find the V8PerContextData quickly. Now that we can get the
    V8PerContextData from the v8::Context quickly, we can move Nodes to the
    general case. The net consequence is that creation of all DOM objects
    should be as fast as Nodes are.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateToV8Converters):
    * bindings/scripts/test/V8/V8Float64Array.cpp:
    (WebCore::V8Float64Array::wrapSlow):
    * bindings/scripts/test/V8/V8TestActiveDOMObject.cpp:
    (WebCore::V8TestActiveDOMObject::wrapSlow):
    * bindings/scripts/test/V8/V8TestCustomNamedGetter.cpp:
    (WebCore::V8TestCustomNamedGetter::wrapSlow):
    * bindings/scripts/test/V8/V8TestEventConstructor.cpp:
    (WebCore::V8TestEventConstructor::wrapSlow):
    * bindings/scripts/test/V8/V8TestEventTarget.cpp:
    (WebCore::V8TestEventTarget::wrapSlow):
    * bindings/scripts/test/V8/V8TestException.cpp:
    (WebCore::V8TestException::wrapSlow):
    * bindings/scripts/test/V8/V8TestInterface.cpp:
    (WebCore::V8TestInterface::wrapSlow):
    * bindings/scripts/test/V8/V8TestMediaQueryListListener.cpp:
    (WebCore::V8TestMediaQueryListListener::wrapSlow):
    * bindings/scripts/test/V8/V8TestNamedConstructor.cpp:
    (WebCore::V8TestNamedConstructor::wrapSlow):
    * bindings/scripts/test/V8/V8TestNode.cpp:
    (WebCore::V8TestNode::wrapSlow):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::V8TestObj::wrapSlow):
    * bindings/scripts/test/V8/V8TestOverloadedConstructors.cpp:
    (WebCore::V8TestOverloadedConstructors::wrapSlow):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore::V8TestSerializedScriptValueInterface::wrapSlow):
    * bindings/v8/V8Binding.cpp:
    * bindings/v8/V8Binding.h:
    (WebCore):
    * bindings/v8/V8DOMWrapper.cpp:
    (WebCore::V8DOMWrapper::instantiateV8Object):
    * bindings/v8/V8DOMWrapper.h:
    (V8DOMWrapper):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -3486,18 +3486,6 @@ END
     }
 
     push(@implContent, <<END);
-    // Please don't add any more uses of this variable.
-    Document* deprecatedDocument = 0;
-    UNUSED_PARAM(deprecatedDocument);
-END
-
-    if (IsNodeSubType($dataNode)) {
-        push(@implContent, <<END);
-    deprecatedDocument = impl->document(); 
-END
-    }
-
-    push(@implContent, <<END);
 
     v8::Handle<v8::Context> context;
     if (!creationContext.IsEmpty() && creationContext->CreationContext() != v8::Context::GetCurrent()) {
@@ -3508,7 +3496,7 @@ END
         context->Enter();
     }
 
-    wrapper = V8DOMWrapper::instantiateV8Object(deprecatedDocument, &info, impl.get());
+    wrapper = V8DOMWrapper::instantiateV8Object(&info, impl.get());
 
     if (!context.IsEmpty())
         context->Exit();
--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestNode.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestNode.cpp
@@ -113,10 +113,6 @@ v8::Handle<v8::Object> V8TestNode::wrapSlow(PassRefPtr<TestNode> impl, v8::Handl
 {
     v8::Handle<v8::Object> wrapper;
     ASSERT(static_cast<void*>(static_cast<Node*>(impl.get())) == static_cast<void*>(impl.get()));
-    // Please don't add any more uses of this variable.
-    Document* deprecatedDocument = 0;
-    UNUSED_PARAM(deprecatedDocument);
-    deprecatedDocument = impl->document(); 
 
     v8::Handle<v8::Context> context;
     if (!creationContext.IsEmpty() && creationContext->CreationContext() != v8::Context::GetCurrent()) {
@@ -127,7 +123,7 @@ v8::Handle<v8::Object> V8TestNode::wrapSlow(PassRefPtr<TestNode> impl, v8::Handl
         context->Enter();
     }
 
-    wrapper = V8DOMWrapper::instantiateV8Object(deprecatedDocument, &info, impl.get());
+    wrapper = V8DOMWrapper::instantiateV8Object(&info, impl.get());
 
     if (!context.IsEmpty())
         context->Exit();
