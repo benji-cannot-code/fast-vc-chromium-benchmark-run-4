ea289f3ef406d467b3e4b2be645937a347607697
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 22:19:57 +0000

    CSSParser::setStyleSheet() should be inline https://bugs.webkit.org/show_bug.cgi?id=101829
    Reviewed by Andreas Kling.
    
    * css/CSSParser.cpp:
    * css/CSSParser.h:
    (WebCore::CSSParser::setStyleSheet):
    CSSParser::setStyleSheet() just assign a pointer, it does not even
    change a ref-count. The function should probably be in the header.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134483 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -1453,11 +1453,6 @@ void CSSParser::clearProperties()
     m_hasFontFaceOnlyValues = false;
 }
 
-void CSSParser::setStyleSheet(StyleSheetContents* styleSheet)
-{
-    m_styleSheet = styleSheet;
-}
-
 KURL CSSParser::completeURL(const CSSParserContext& context, const String& url)
 {
     if (url.isNull())
