2f9c6a0b0f2d5a3f429597617f93d4153011798c
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 06:04:51 +0000

    The act of getting the callee during 'this' construction should be explicit in bytecode https://bugs.webkit.org/show_bug.cgi?id=102016
    Reviewed by Michael Saboff.
    
    This is mostly a rollout of http://trac.webkit.org/changeset/116673, but also includes
    changes to have create_this use the result of get_callee.
            
    No performance or behavioral impact. This is just meant to allow us to profile
    get_callee in the future.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::dump):
    * bytecode/Opcode.h:
    (JSC):
    (JSC::padOpcodeName):
    * bytecompiler/BytecodeGenerator.cpp:
    (JSC::BytecodeGenerator::BytecodeGenerator):
    * dfg/DFGByteCodeParser.cpp:
    (JSC::DFG::ByteCodeParser::parseBlock):
    * dfg/DFGCapabilities.h:
    (JSC::DFG::canCompileOpcode):
    * jit/JIT.cpp:
    (JSC::JIT::privateCompileMainPass):
    * jit/JIT.h:
    (JIT):
    * jit/JITOpcodes.cpp:
    (JSC::JIT::emit_op_get_callee):
    (JSC):
    (JSC::JIT::emit_op_create_this):
    * jit/JITOpcodes32_64.cpp:
    (JSC::JIT::emit_op_get_callee):
    (JSC):
    (JSC::JIT::emit_op_create_this):
    * llint/LLIntSlowPaths.cpp:
    (JSC::LLInt::LLINT_SLOW_PATH_DECL):
    * llint/LowLevelInterpreter32_64.asm:
    * llint/LowLevelInterpreter64.asm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134361 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGByteCodeParser.cpp
@@ -219,7 +219,7 @@ private:
 
         if (operand == JSStack::Callee)
             return getCallee();
-
+        
         // Is this an argument?
         if (operandIsArgument(operand))
             return getArgument(operand);
@@ -2125,7 +2125,8 @@ bool ByteCodeParser::parseBlock(unsigned limit)
         }
 
         case op_create_this: {
-            set(currentInstruction[1].u.operand, addToGraph(CreateThis, get(JSStack::Callee)));
+            int calleeOperand = currentInstruction[2].u.operand;
+            set(currentInstruction[1].u.operand, addToGraph(CreateThis, get(calleeOperand)));
             NEXT_OPCODE(op_create_this);
         }
             
@@ -2178,6 +2179,11 @@ bool ByteCodeParser::parseBlock(unsigned limit)
             NEXT_OPCODE(op_new_regexp);
         }
             
+        case op_get_callee: {
+            set(currentInstruction[1].u.operand, get(JSStack::Callee));
+            NEXT_OPCODE(op_get_callee);
+        }
+
         // === Bitwise operations ===
 
         case op_bitand: {
