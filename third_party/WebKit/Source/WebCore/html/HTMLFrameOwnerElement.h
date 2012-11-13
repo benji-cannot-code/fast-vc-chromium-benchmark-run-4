16522e3923742c9375691cdc3bd090b6f96798a6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 04:44:20 +0000

    Make Frames and HTMLFrameOwnerElement less friendly https://bugs.webkit.org/show_bug.cgi?id=102003
    Patch by Elliott Sprehn <esprehn@chromium.org> on 2012-11-12
    Reviewed by Ojan Vafai.
    
    Frame used to assign HTMLFrameOwnerElement's m_contentFrame directly
    this patch makes it go through a method to allow future hooks when frames
    are associated with owners.
    
    No new tests, this is just a refactor.
    
    * html/HTMLFrameOwnerElement.cpp:
    (WebCore::HTMLFrameOwnerElement::setContentFrame):
    * html/HTMLFrameOwnerElement.h:
    (HTMLFrameOwnerElement):
    (WebCore::HTMLFrameOwnerElement::clearContentFrame):
    * page/Frame.cpp:
    (WebCore::Frame::Frame): Use new methods.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFrameOwnerElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLFrameOwnerElement.h
@@ -43,6 +43,11 @@ public:
     DOMWindow* contentWindow() const;
     Document* contentDocument() const;
 
+    void setContentFrame(Frame*);
+    void clearContentFrame() { m_contentFrame = 0; }
+
+    void disconnectContentFrame();
+
     // Most subclasses use RenderPart (either RenderEmbeddedObject or RenderIFrame)
     // except for HTMLObjectElement and HTMLEmbedElement which may return any
     // RenderObject when using fallback content.
@@ -55,15 +60,12 @@ public:
     virtual ScrollbarMode scrollingMode() const { return ScrollbarAuto; }
 
     SandboxFlags sandboxFlags() const { return m_sandboxFlags; }
-    void disconnectContentFrame();
 
 protected:
     HTMLFrameOwnerElement(const QualifiedName& tagName, Document*);
     void setSandboxFlags(SandboxFlags);
 
 private:
-    friend class Frame;
-
     virtual bool isKeyboardFocusable(KeyboardEvent*) const;
     virtual bool isFrameOwnerElement() const OVERRIDE { return true; }
 
