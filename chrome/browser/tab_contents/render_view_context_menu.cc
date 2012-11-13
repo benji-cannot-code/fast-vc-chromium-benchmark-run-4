8a1580c67a27048f250ad594a1f58569fa5a1d93
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 23:08:29 +0000

    Reverting this to fix the compile failures on linux clang. please reland after fixing.
    Revert 167486 - Triggering InspectElement from context menu over a guest will open guest's devtools with correct element highlighted.
    
    BUG=140329
    
    Review URL: https://chromiumcodereview.appspot.com/11362016
    
    TBR=lazyboy@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/11275300
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167506 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.cc
+++ chrome/browser/tab_contents/render_view_context_menu.cc
@@ -225,10 +225,6 @@ bool ShouldShowTranslateItem(const GURL& page_url) {
 
   return true;
 }
-
-void DevToolsInspectElementAt(RenderViewHost* rvh, int x, int y) {
-  DevToolsWindow::InspectElement(rvh, x, y);
-}
 }  // namespace
 
 // static
@@ -1842,8 +1838,8 @@ void RenderViewContextMenu::CopyImageAt(int x, int y) {
 
 void RenderViewContextMenu::Inspect(int x, int y) {
   content::RecordAction(UserMetricsAction("DevTools_InspectElement"));
-  source_web_contents_->GetRenderViewHostAtPosition(
-      x, y, base::Bind(&DevToolsInspectElementAt));
+  DevToolsWindow::InspectElement(
+      source_web_contents_->GetRenderViewHost(), x, y);
 }
 
 void RenderViewContextMenu::WriteURLToClipboard(const GURL& url) {
--- content/browser/browser_plugin/browser_plugin_embedder.h
+++ content/browser/browser_plugin/browser_plugin_embedder.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebDragStatus.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebDragOperation.h"
@@ -132,17 +131,6 @@ class CONTENT_EXPORT BrowserPluginEmbedder : public WebContentsObserver,
     factory_ = factory;
   }
 
-  // Returns the RenderViewHost at a point (|x|, |y|) asynchronously via
-  // |callback|. We need a roundtrip to renderer process to get this
-  // information.
-  void GetRenderViewHostAtPosition(
-      int x,
-      int y,
-      const WebContents::GetRenderViewHostCallback& callback);
-  void PluginAtPositionResponse(int instance_id,
-                                int request_id,
-                                const gfx::Point& position);
-
  private:
   friend class TestBrowserPluginEmbedder;
 
@@ -155,7 +143,7 @@ class CONTENT_EXPORT BrowserPluginEmbedder : public WebContentsObserver,
   // Adds a new guest web_contents to the embedder (overridable in test).
   virtual void AddGuest(int instance_id, WebContents* guest_web_contents);
   void DestroyGuestByInstanceID(int instance_id);
-  void CleanUp();
+  void DestroyGuests();
 
   // Called when visiblity of web_contents changes, so the embedder will
   // show/hide its guest.
@@ -172,14 +160,6 @@ class CONTENT_EXPORT BrowserPluginEmbedder : public WebContentsObserver,
   RenderViewHost* render_view_host_;
   // Tracks the visibility state of the embedder.
   bool visible_;
-  // Map that contains outstanding queries to |GetBrowserPluginAt|.
-  // We need a roundtrip to renderer process to know the answer, therefore
-  // storing these callbacks is required.
-  typedef std::map<int, WebContents::GetRenderViewHostCallback>
-      GetRenderViewHostCallbackMap;
-  GetRenderViewHostCallbackMap pending_get_render_view_callbacks_;
-  // Next request id for BrowserPluginMsg_PluginAtPositionRequest query.
-  int next_get_render_view_request_id_;
 
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginEmbedder);
 };
