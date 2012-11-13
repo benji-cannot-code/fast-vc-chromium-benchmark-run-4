109be9f447a7009b7ef749ddd788019be2321c20
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 09:56:31 +0000

    Revert 167341 - Use the WebTestProxy for layout tests in content_shell
    - Add a hook to the content renderer client to allow embedders to override the
      creation of RenderViewImpls. To simplify this, I consolidate the parameters
      passed to RVI's constructor into a struct.
    
    - Add a content_layouttest_support library that provides a method for creating
      a modified RenderViewImpl suitable for running layout tests.
    
    - Use above library from the content_shell.
    
    BUG=111316
    TEST=almost all accessibility tests pass.
    
    Review URL: https://codereview.chromium.org/11362161
    
    TBR=jochen@chromium.org
    Review URL: https://codereview.chromium.org/11275276
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167345 0039d316-1c4b-4281-b951-d872f2087c98

--- content/content_shell.gypi
+++ content/content_shell.gypi
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'content_shell_resources',
         'content_utility',
         'content_worker',
-        'test_support_content',
         'content_resources.gyp:content_resources',
         '../base/base.gyp:base',
         '../base/third_party/dynamic_annotations/dynamic_annotations.gyp:dynamic_annotations',
--- content/shell/shell_content_renderer_client.cc
+++ content/shell/shell_content_renderer_client.cc
@@ -5,30 +5,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/shell_content_renderer_client.h"
 
-#include "base/callback.h"
 #include "base/command_line.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/common/content_switches.h"
-#include "content/public/test/layouttest_support.h"
 #include "content/shell/shell_render_process_observer.h"
 #include "content/shell/shell_switches.h"
 #include "content/shell/webkit_test_runner.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPluginParams.h"
-#include "third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h"
 #include "v8/include/v8.h"
-
-using WebTestRunner::WebTestProxyBase;
 
 namespace content {
 
-ShellContentRendererClient::ShellContentRendererClient()
-    : latest_test_runner_(NULL) {
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kDumpRenderTree)) {
-    EnableWebTestProxyCreation(
-        base::Bind(&ShellContentRendererClient::WebTestProxyCreated,
-                   base::Unretained(this)));
-  }
+ShellContentRendererClient::ShellContentRendererClient() {
 }
 
 ShellContentRendererClient::~ShellContentRendererClient() {
@@ -42,8 +31,7 @@ void ShellContentRendererClient::RenderViewCreated(RenderView* render_view) {
   if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kDumpRenderTree))
     return;
 
-  CHECK(!latest_test_runner_);
-  latest_test_runner_ = new WebKitTestRunner(render_view);
+  new WebKitTestRunner(render_view);
 }
 
 bool ShellContentRendererClient::OverrideCreatePlugin(
@@ -59,14 +47,6 @@ bool ShellContentRendererClient::OverrideCreatePlugin(
         switches::kEnableBrowserPluginForAllViewTypes);
   }
   return false;
-}
-
-void ShellContentRendererClient::WebTestProxyCreated(WebTestProxyBase* proxy) {
-  CHECK(latest_test_runner_);
-  proxy->setDelegate(latest_test_runner_);
-  latest_test_runner_ = NULL;
-  proxy->setInterfaces(
-      ShellRenderProcessObserver::GetInstance()->test_interfaces());
 }
 
 }  // namespace content
