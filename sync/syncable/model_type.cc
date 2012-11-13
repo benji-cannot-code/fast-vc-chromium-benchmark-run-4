a579bdab05dfc9ef120071be77d91d64c87c56a2
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 02:02:52 +0000

    Revert 167280 - [Sync] Add application logic for non-nigori control types.
    We introduce logic to apply non-nigori control types. First, we iterate over
    any new top level datatype entities, applying those. Then we go through the
    rest of the unapplied control datatype updates, applying those. Any conflict
    should be just a simple conflict, which we handle by ignoring the local
    changes.
    
    Also updates chromiumsync.py to support the new control types (and fixes
    the parent folder pattern that was in use).
    
    BUG=122825
    
    
    Review URL: https://chromiumcodereview.appspot.com/11271009
    
    TBR=zea@chromium.org
    Review URL: https://codereview.chromium.org/11359175
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167282 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/syncable/model_type.cc
+++ sync/syncable/model_type.cc
@@ -282,6 +282,12 @@ ModelTypeSet ControlTypes() {
     set.Put(ModelTypeFromInt(i));
   }
 
+  // TODO(rlarocque): Re-enable this when the server supports it.
+  set.Remove(DEVICE_INFO);
+
+  // TODO(rlarocque): Re-enable this when the server supports it.
+  set.Remove(EXPERIMENTS);
+
   return set;
 }
 
