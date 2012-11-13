fc6e116d87ac96825f84a971fd524757975c64d8
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-13 08:53:09 +0000

    Fix the ARM traditional build after r134332 https://bugs.webkit.org/show_bug.cgi?id=102044
    Patch by Peter Gal <galpeter@inf.u-szeged.hu> on 2012-11-13
    Reviewed by Zoltan Herczeg.
    
    Added missing methods for the MacroAssemblerARM, based on the MacroAssemblerARMv7.
    
    * assembler/MacroAssemblerARM.h:
    (JSC::MacroAssemblerARM::canJumpReplacePatchableBranchPtrWithPatch):
    (MacroAssemblerARM):
    (JSC::MacroAssemblerARM::startOfPatchableBranchPtrWithPatch):
    (JSC::MacroAssemblerARM::revertJumpReplacementToPatchableBranchPtrWithPatch):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARM.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARM.h
@@ -1267,6 +1267,18 @@ public:
         return 0;
     }
 
+    static bool canJumpReplacePatchableBranchPtrWithPatch() { return false; }
+
+    static CodeLocationLabel startOfPatchableBranchPtrWithPatch(CodeLocationDataLabelPtr label)
+    {
+        UNREACHABLE_FOR_PLATFORM();
+    }
+
+    static void revertJumpReplacementToPatchableBranchPtrWithPatch(CodeLocationLabel instructionStart, Address, void* initialValue)
+    {
+        UNREACHABLE_FOR_PLATFORM();
+    }
+
 protected:
     ARMAssembler::Condition ARMCondition(RelationalCondition cond)
     {
