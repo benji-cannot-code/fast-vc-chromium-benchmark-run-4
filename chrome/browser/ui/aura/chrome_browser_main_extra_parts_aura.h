cf16404324e2169867ea70bcee59828fac7f48b8
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 19:47:52 +0000

    Revert 167434 - build fix
    TBR=scottmg@chromium.org
    Review URL: https://chromiumcodereview.appspot.com/11366227
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h
+++ chrome/browser/ui/aura/chrome_browser_main_extra_parts_aura.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/chrome_browser_main_extra_parts.h"
 
-namespace aura {
+namespace views {
 class DesktopStackingClient;
 }
 
