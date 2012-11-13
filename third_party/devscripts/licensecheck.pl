6f51027d1079ea4474a934a3b2ab1dbc9c5f489f
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-13 21:19:44 +0000

    licensecheck: recognize MPL 2.0
    TBR=wtc,thestig@chromium.org
    
    BUG=159669
    
    Review URL: https://codereview.chromium.org/11360236
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167471 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/devscripts/licensecheck.pl
+++ third_party/devscripts/licensecheck.pl
@@ -482,8 +482,8 @@ sub parselicense($) {
         $license = "BSD-like $license";
     }
 
-    if ($licensetext =~ /Mozilla Public License Version ([^ ]+)/) {
-	$license = "MPL (v$1) $license";
+    if ($licensetext =~ /Mozilla Public License( Version|, v.) ([^ ]+[^. ])/) {
+	$license = "MPL (v$2) $license";
     }
 
     if ($licensetext =~ /Released under the terms of the Artistic License ([^ ]+)/) {
