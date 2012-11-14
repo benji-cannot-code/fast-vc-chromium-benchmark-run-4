061ab2d7d4d3a60c685446dab5148ee512771a65
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 01:33:20 +0000

    [Spellcheck] Removing cruft.
    BUG=154918
    
    Review URL: https://codereview.chromium.org/11377045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167563 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/chrome_render_view_test.cc
+++ chrome/test/base/chrome_render_view_test.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/extensions/event_bindings.h"
 #include "chrome/renderer/extensions/extension_custom_bindings.h"
 #include "chrome/renderer/extensions/miscellaneous_bindings.h"
+#include "chrome/renderer/spellchecker/spellcheck.h"
 #include "content/public/browser/native_web_keyboard_event.h"
 #include "content/public/common/renderer_preferences.h"
 #include "content/public/renderer/render_view.h"
@@ -56,6 +57,7 @@ void ChromeRenderViewTest::SetUp() {
       &chrome_content_renderer_client_);
   extension_dispatcher_ = new extensions::Dispatcher();
   chrome_content_renderer_client_.SetExtensionDispatcher(extension_dispatcher_);
+  chrome_content_renderer_client_.SetSpellcheck(new SpellCheck());
 
   chrome_render_thread_ = new ChromeMockRenderThread();
   render_thread_.reset(chrome_render_thread_);
