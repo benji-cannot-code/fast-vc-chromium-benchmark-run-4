246fc67cd8ca8849de553e3f03f4e1f18be72bf7
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 04:26:26 +0000

    Add an OVERRIDE keyword This change adds an OVERRIDE keyword to the BaseFormatView::ButtonPressed function to fix a build break on the "Linux ChromiumOS (Clang dbg)" bot.
    TBR=miket
    BUG=none
    TEST=fix builds on the "Linux ChromiumOS (Clang dbg)" bot and the "Linux Chromium OS ASAN Builder" bot.
    Review URL: https://codereview.chromium.org/11410077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167593 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/base_format_view.h
+++ ui/message_center/base_format_view.h
@@ -26,7 +26,8 @@ class BaseFormatView : public MessageView {
   virtual void SetUpView() OVERRIDE;
 
   // views::ButtonListener
-  virtual void ButtonPressed(views::Button* sender, const ui::Event& event);
+  virtual void ButtonPressed(views::Button* sender,
+                             const ui::Event& event) OVERRIDE;
 
  protected:
   BaseFormatView();
