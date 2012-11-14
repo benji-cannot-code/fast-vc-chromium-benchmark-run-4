b418dcb7e7141b74c42448d5b2c3f38a5d3f10ae
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 11:05:55 +0000

    [Qt] QStyleFacadeImp should use a QPointer to store the application style https://bugs.webkit.org/show_bug.cgi?id=102084
    Reviewed by Simon Hausmann.
    
    QStyleFacadeImp implicitly creates a default style during the first
    call to QApplication::style() in QStyleFacadeImp::style() but this
    style is deleted when a custom style is set, thus the stored m_style
    raw pointer in QStyleFacadeImp becomes dangling.
    Use a QPointer to cache the style instead of a raw pointer.
    
    This fixes the start-up crash in the anomaly example.
    (https://bugreports.qt-project.org/browse/QTBUG-27924)
    
    * WebCoreSupport/QStyleFacadeImp.h:
    (QStyleFacadeImp):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134587 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QStyleFacadeImp.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QStyleFacadeImp.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef QStyleFacadeImp_h
 #define QStyleFacadeImp_h
 
+#include <QPointer>
 #include <QStyleFacade.h>
 
 QT_BEGIN_NAMESPACE
@@ -88,7 +89,7 @@ private:
     QStyle* style() const;
 
     WebCore::Page* m_page;
-    mutable QStyle* m_style;
+    mutable QPointer<QStyle> m_style;
     QStyle* m_fallbackStyle;
     bool m_ownFallbackStyle;
     mutable QScopedPointer<QLineEdit> m_lineEdit;
