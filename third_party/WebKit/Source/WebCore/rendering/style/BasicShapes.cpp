ed9c3a73a3f2651ab6cdd90c8bdd8dac340eb7a1
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 23:27:11 +0000

    Cleanup BasicShape blending check https://bugs.webkit.org/show_bug.cgi?id=102289
    Reviewed by Daniel Bates.
    
    CSSPropertyAnimation had the same code for varifying that two BasicShape objects can be blended
    twice. Refactor the code and add a canBlend method in BasicShape that combines both checks. This
    is a preparation for follow-up patches.
    
    Pure refactoring without behavior change, no new tests.
    
    * page/animation/CSSPropertyAnimation.cpp:
    (WebCore::blendFunc): Call new canBlend method for blending verification.
    * rendering/style/BasicShapes.cpp:
    (WebCore::BasicShape::canBlend): Check if two BasicShape objects can be blended.
    (WebCore):
    * rendering/style/BasicShapes.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134679 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/style/BasicShapes.cpp
+++ third_party/WebKit/Source/WebCore/rendering/style/BasicShapes.cpp
@@ -38,6 +38,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+bool BasicShape::canBlend(const BasicShape* other) const
+{
+    // FIXME: Support animations between different shapes in the future.
+    if (type() != other->type())
+        return false;
+
+    // FIXME: Support animations between polygons in the future.
+    if (type() == BasicShape::BASIC_SHAPE_POLYGON)
+        return false;
+
+    return true;
+}
+
 void BasicShapeRectangle::path(Path& path, const FloatRect& boundingBox)
 {
     ASSERT(path.isEmpty());
