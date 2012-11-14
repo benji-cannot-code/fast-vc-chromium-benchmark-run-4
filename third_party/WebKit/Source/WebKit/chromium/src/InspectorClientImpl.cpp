3a7b9240a81cc70cf6a6953ea7b8b6bb19bd368b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 05:52:55 +0000

    checkbox to toggle FPS counter in the inspector's settings https://bugs.webkit.org/show_bug.cgi?id=99660
    Patch by Eberhard Graether <egraether@google.com> on 2012-11-13
    Reviewed by Pavel Feldman.
    
    Added a checkbox to the inspector's settings to toggle a FPS counter. The checkbox appears when InspectorClient::canShowFPSCounter() returns true.
    
    Source/Platform:
    
    * chromium/public/WebLayerTreeView.h:
    (WebLayerTreeView):
    (WebKit::WebLayerTreeView::setShowFPSCounter):
    
    Source/WebCore:
    
    No new tests.
    
    * English.lproj/localizedStrings.js:
    * inspector/Inspector.json:
    * inspector/InspectorClient.h:
    (WebCore::InspectorClient::canShowFPSCounter):
    (WebCore::InspectorClient::setShowFPSCounter):
    (InspectorClient):
    * inspector/InspectorPageAgent.cpp:
    (PageAgentState):
    (WebCore::InspectorPageAgent::enable):
    (WebCore::InspectorPageAgent::disable):
    (WebCore::InspectorPageAgent::canShowFPSCounter):
    (WebCore):
    (WebCore::InspectorPageAgent::setShowFPSCounter):
    * inspector/InspectorPageAgent.h:
    * inspector/front-end/Settings.js:
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.GenericSettingsTab):
    (WebInspector.GenericSettingsTab.prototype.get _showFPSCounterChanged):
    * inspector/front-end/inspector.js:
    (WebInspector.doLoadedDone):
    
    Source/WebKit/chromium:
    
    * public/WebView.h:
    (WebView):
    * src/InspectorClientImpl.cpp:
    (WebKit::InspectorClientImpl::canShowFPSCounter):
    (WebKit):
    (WebKit::InspectorClientImpl::setShowFPSCounter):
    * src/InspectorClientImpl.h:
    (InspectorClientImpl):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::WebViewImpl):
    (WebKit::WebViewImpl::setShowFPSCounter):
    (WebKit):
    (WebKit::WebViewImpl::setIsAcceleratedCompositingActive):
    (WebKit::WebViewImpl::loadFontAtlasIfNecessary):
    * src/WebViewImpl.h:
    (WebViewImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/InspectorClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/InspectorClientImpl.cpp
@@ -164,6 +164,16 @@ void InspectorClientImpl::autoZoomPageToFitWidth()
         agent->autoZoomPageToFitWidth();
 }
 
+bool InspectorClientImpl::canShowFPSCounter()
+{
+    return true;
+}
+
+void InspectorClientImpl::setShowFPSCounter(bool show)
+{
+    m_inspectedWebView->setShowFPSCounter(show);
+}
+
 bool InspectorClientImpl::supportsFrameInstrumentation()
 {
     return true;
--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.h
@@ -310,6 +310,7 @@ public:
 #endif
     virtual void transferActiveWheelFlingAnimation(const WebActiveWheelFlingParameters&);
     virtual WebViewBenchmarkSupport* benchmarkSupport();
+    virtual void setShowFPSCounter(bool);
 
     // WebLayerTreeViewClient
     virtual void willBeginFrame();
@@ -651,6 +652,7 @@ private:
 
 #if USE(ACCELERATED_COMPOSITING)
     void setIsAcceleratedCompositingActive(bool);
+    void loadFontAtlasIfNecessary();
     void doComposite();
     void doPixelReadbackToCanvas(WebCanvas*, const WebCore::IntRect&);
     void reallocateRenderer();
@@ -847,6 +849,7 @@ private:
     bool m_compositorSurfaceReady;
     float m_deviceScaleInCompositor;
     int m_inputHandlerIdentifier;
+    bool m_isFontAtlasLoaded;
 #endif
     static const WebInputEvent* m_currentInputEvent;
 
