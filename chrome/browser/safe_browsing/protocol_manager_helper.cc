5b838116c632648c37323a105c963d06f5061419
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 13:04:33 +0000

    Added unit tests for SafeBrowsingProtocolManager.
    BUG=158246
    
    
    Review URL: https://chromiumcodereview.appspot.com/11369155
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167661 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/protocol_manager_helper.cc
+++ chrome/browser/safe_browsing/protocol_manager_helper.cc
@@ -19,6 +19,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/google_api_keys.h"
 #include "net/base/escape.h"
 
+SafeBrowsingProtocolConfig::SafeBrowsingProtocolConfig()
+    : disable_auto_update(false) {
+}
+
+SafeBrowsingProtocolConfig::~SafeBrowsingProtocolConfig() {
+}
+
 // static
 std::string SafeBrowsingProtocolManagerHelper::Version() {
   chrome::VersionInfo version_info;
--- chrome/browser/safe_browsing/protocol_manager_helper.h
+++ chrome/browser/safe_browsing/protocol_manager_helper.h
@@ -17,9 +17,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 
 struct SafeBrowsingProtocolConfig {
-  SafeBrowsingProtocolConfig() : disable_auto_update(false) {}
+  SafeBrowsingProtocolConfig();
+  ~SafeBrowsingProtocolConfig();
   std::string client_name;
   std::string url_prefix;
+  std::string version;
   bool disable_auto_update;
 };
 
