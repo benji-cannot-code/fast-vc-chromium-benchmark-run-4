777f3339f90cc360bc208a8f281d61910e2c4482
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 12:18:51 +0000

    Disable storage/indexeddb/database-basics.html as it's failing after wkrev.com/134533
    BUG=160961
    TBR=dgrogan@chromium.org
    
    Review URL: https://codereview.chromium.org/11312233
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167658 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
+++ content/browser/in_process_webkit/indexed_db_layout_browsertest.cc
@@ -26,7 +26,8 @@ static const char* kBasicTests[] = {
   "basics-shared-workers.html",
   // Failing on Precise bot (crbug.com/145592).
   // "basics-workers.html",
-  "database-basics.html",
+  // Failing on all platforms (crbug.com/160961).
+  // "database-basics.html",
   "factory-basics.html",
   "index-basics.html",
   "objectstore-basics.html",
