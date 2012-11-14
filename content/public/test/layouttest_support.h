17d38b05fa84e48d0e01ebef63381c798eb24c22
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 20:32:19 +0000

    Reland r167341 - Use the WebTestProxy for layout tests in content_shell
    - Add a hook to the content renderer client to allow embedders to override the
      creation of RenderViewImpls. To simplify this, I consolidate the parameters
      passed to RVI's constructor into a struct.
    
    - Add a content_layouttest_support library that provides a method for creating
      a modified RenderViewImpl suitable for running layout tests.
    
    - Use above library from the content_shell.
    
    BUG=111316
    TEST=almost all accessibility tests pass.
    
    Original review: https://chromiumcodereview.appspot.com/11293237
    
    Review URL: https://codereview.chromium.org/11275277
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167731 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/public/test/layouttest_support.h
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CONTENT_PUBLIC_TEST_LAYOUTTEST_SUPPORT_H_
+#define CONTENT_PUBLIC_TEST_LAYOUTTEST_SUPPORT_H_
+
+#include "base/callback_forward.h"
+
+namespace WebTestRunner {
+class WebTestProxyBase;
+}
+
+namespace content {
+
+// Enable injecting of a WebTestProxy between WebViews and RenderViews.
+// |callback| is invoked with a pointer to WebTestProxyBase for each created
+// WebTestProxy.
+void EnableWebTestProxyCreation(
+    const base::Callback<void(WebTestRunner::WebTestProxyBase*)>& callback);
+
+}  // namespace content
+
+#endif  // CONTENT_PUBLIC_TEST_LAYOUTTEST_SUPPORT_H_
