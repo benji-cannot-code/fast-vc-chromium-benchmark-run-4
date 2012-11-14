ffa75b27dd9903a460f7225bdf835e8f7228322c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 17:37:48 +0000

    Call to enclosingFilterLayer() in RenderObject::containerForRepaint() is expensive https://bugs.webkit.org/show_bug.cgi?id=101846
    Patch by Max Vujovic <mvujovic@adobe.com> on 2012-11-14
    Reviewed by Simon Fraser.
    
    If software-rendered CSS Filters have not been used in the document, avoid doing the second
    tree walk in RenderObject::containerForRepaint, which determines the RenderObject's
    enclosing filter layer.
    
    No new tests. We now avoid a filters related code path for a performance improvement when
    we're not using filters.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::FrameView):
    * page/FrameView.h:
    (FrameView):
    (WebCore::FrameView::setHasSoftwareFilters):
    (WebCore::FrameView::hasSoftwareFilters):
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::updateOrRemoveFilterEffectRenderer):
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::containerForRepaint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134619 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderObject.cpp
@@ -1281,10 +1281,12 @@ RenderLayerModelObject* RenderObject::containerForRepaint() const
 #endif
     
 #if ENABLE(CSS_FILTERS)
-    if (RenderLayer* parentLayer = enclosingLayer()) {
-        RenderLayer* enclosingFilterLayer = parentLayer->enclosingFilterLayer();
-        if (enclosingFilterLayer)
-            return enclosingFilterLayer->renderer();
+    if (document()->view()->hasSoftwareFilters()) {
+        if (RenderLayer* parentLayer = enclosingLayer()) {
+            RenderLayer* enclosingFilterLayer = parentLayer->enclosingFilterLayer();
+            if (enclosingFilterLayer)
+                return enclosingFilterLayer->renderer();
+        }
     }
 #endif
 
