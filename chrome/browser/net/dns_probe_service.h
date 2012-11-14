190e71c5dec6b88b3ffe8e15e73f3378929cd549
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 00:37:38 +0000

    Put DnsProbeService on IOThread.
    Instantiate the DnsProbeService in IOThread::Globals.  We'll access it from
    the NetErrorTabHelper using a couple of stubs that are run on the IO thread.
    
    BUG=156415
    TEST=doesn't change how the service works
    
    
    Review URL: https://chromiumcodereview.appspot.com/11395002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/dns_probe_service.h
+++ chrome/browser/net/dns_probe_service.h
@@ -29,7 +29,7 @@ class DnsProbeService {
   typedef base::Callback<void(Result result)> CallbackType;
 
   DnsProbeService();
-  ~DnsProbeService();
+  virtual ~DnsProbeService();
 
   void ProbeDns(const CallbackType& callback);
 
