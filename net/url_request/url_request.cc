125349b821a1cc08738d4c0cb1ddaae03e7ad885
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 05:04:30 +0000

    Set blocked_on_delegate_ when a URLRequest is blocked by its NetworkDelegate during a redirect.
    R=eroman@chromium.org
    BUG=159197
    
    Review URL: https://chromiumcodereview.appspot.com/11364057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167600 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request.cc
+++ net/url_request/url_request.cc
@@ -327,7 +327,8 @@ void URLRequest::SetExtraRequestHeaders(
 }
 
 LoadStateWithParam URLRequest::GetLoadState() const {
-  if (blocked_on_delegate_) {
+  // Only return LOAD_STATE_WAITING_FOR_DELEGATE if there's a load state param.
+  if (blocked_on_delegate_ && !load_state_param_.empty()) {
     return LoadStateWithParam(LOAD_STATE_WAITING_FOR_DELEGATE,
                               load_state_param_);
   }
@@ -652,6 +653,7 @@ void URLRequest::NotifyReceivedRedirect(const GURL& location,
     RestartWithJob(job);
   } else if (delegate_) {
     delegate_->OnReceivedRedirect(this, location, defer_redirect);
+    // |this| may be have been destroyed here.
   }
 }
 
@@ -965,7 +967,12 @@ void URLRequest::NotifyRequestCompleted() {
 
 void URLRequest::SetBlockedOnDelegate() {
   blocked_on_delegate_ = true;
-  net_log_.BeginEvent(NetLog::TYPE_URL_REQUEST_BLOCKED_ON_DELEGATE);
+  if (!load_state_param_.empty()) {
+    net_log_.BeginEvent(NetLog::TYPE_URL_REQUEST_BLOCKED_ON_DELEGATE,
+                        NetLog::StringCallback("delegate", &load_state_param_));
+  } else {
+    net_log_.BeginEvent(NetLog::TYPE_URL_REQUEST_BLOCKED_ON_DELEGATE);
+  }
 }
 
 void URLRequest::SetUnblockedOnDelegate() {
