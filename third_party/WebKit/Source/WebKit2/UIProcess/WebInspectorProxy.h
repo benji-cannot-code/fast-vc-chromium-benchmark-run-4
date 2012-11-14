a72b7da18d63ff3e64d494f68fad31a71580b8ad
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 01:18:39 +0000

    Make InspectorFrontendHost.moveWindowBy work in WebKit2 for Mac.
    https://bugs.webkit.org/show_bug.cgi?id=102154
    
    Reviewed by Joseph Pecoraro.
    
    * UIProcess/WebInspectorProxy.h:
    * UIProcess/mac/WebInspectorProxyMac.mm:
    (WebKit::getWindowFrame): Added. Call setInspectorWindowFrame.
    (WebKit::setWindowFrame): Added. Call inspectorWindowFrame.
    (WebKit::WebInspectorProxy::setInspectorWindowFrame): Added.
    (WebKit::WebInspectorProxy::inspectorWindowFrame): Added.
    (WebKit::WebInspectorProxy::platformCreateInspectorPage): Create and set the WKPageUIClient
    with getWindowFrame and setWindowFrame.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebInspectorProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebInspectorProxy.h
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/text/WTFString.h>
 
 #if PLATFORM(MAC)
+#include "WKGeometry.h"
 #include <wtf/RetainPtr.h>
 
 OBJC_CLASS NSButton;
@@ -100,6 +101,9 @@ public:
     void createInspectorWindow();
     void updateInspectorWindowTitle() const;
     void inspectedViewFrameDidChange();
+
+    void setInspectorWindowFrame(WKRect&);
+    WKRect inspectorWindowFrame();
 #endif
 
 #if PLATFORM(GTK)
