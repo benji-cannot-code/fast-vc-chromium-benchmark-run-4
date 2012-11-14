45745cd8e2d3a5c70da09e1487e53d174d3af82f
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 20:35:21 +0000

    Move empty loading to DocumentLoader, simplify FrameLoader::init() https://bugs.webkit.org/show_bug.cgi?id=101512
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    No new tests, though several outputs changed because we no longer send resource
        load callbacks for empty loads.
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::startLoadingMainResource): Handle empty main resource
        loads directly here.
    * loader/DocumentLoader.h:
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::FrameLoader): Initialize some variables whose values
        were previously being reset in init(). Given that the FrameLoader is in
        an inconsistent state before init() is called anyway, there doesn't seem
        to be a disadvantage to just initializing them to their post-init() values.
    (WebCore::FrameLoader::init): Just call startLoadingMainResource(), instead of
        doing a bunch of direct calls to functions FrameLoader shouldn't know about.
    * loader/FrameLoaderStateMachine.cpp:
    * loader/FrameLoaderStateMachine.h:
    * loader/MainResourceLoader.cpp: Throughout, remove the concept of an empty load.
    (WebCore::MainResourceLoader::loadNow): This only returned true when an empty
        load got deferred, which won't happen now. Return void and always treat
        as returning false.
    * loader/MainResourceLoader.h:
    
    Tools:
    
    Chromium DRT has some code that doesn't play nicely with empty urls and incorrectly
    interprets them as a non-empty load. This ensures
    http/tests/navigation/new-window-redirect-history.html continues to pass
    in chromium.
    
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::updateForCommittedLoad):
    
    LayoutTests:
    
    Don't expect resource load callbacks for empty loads.
    
    * http/tests/inspector/network/network-iframe-load-and-delete.html:
    * http/tests/loading/redirect-methods-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-in-body-expected.txt:
    * http/tests/security/XFrameOptions/x-frame-options-deny-meta-tag-parent-same-origin-deny-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/MainResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/MainResourceLoader.cpp
@@ -68,11 +68,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-static bool shouldLoadAsEmptyDocument(const KURL& url)
-{
-    return url.isEmpty() || SchemeRegistry::shouldLoadURLSchemeAsEmptyDocument(url.protocol());
-}
-
 MainResourceLoader::MainResourceLoader(Frame* frame)
     : ResourceLoader(frame, ResourceLoaderOptions(SendCallbacks, SniffContent, BufferData, AllowStoredCredentials, AskClientForCrossOriginCredentials, SkipSecurityCheck))
     , m_dataLoadTimer(this, &MainResourceLoader::handleSubstituteDataLoadNow)
@@ -339,13 +334,10 @@ void MainResourceLoader::continueAfterContentPolicy(PolicyAction contentPolicy, 
     if (!reachedTerminalState())
         ResourceLoader::didReceiveResponse(r);
 
-    if (frameLoader() && !frameLoader()->activeDocumentLoader()->isStopping()) {
-        if (m_substituteData.isValid()) {
-            if (m_substituteData.content()->size())
-                didReceiveData(m_substituteData.content()->data(), m_substituteData.content()->size(), m_substituteData.content()->size(), true);
-            if (frameLoader() && !frameLoader()->activeDocumentLoader()->isStopping()) 
-                didFinishLoading(0);
-        } else if (shouldLoadAsEmptyDocument(url) || frameLoader()->client()->representationExistsForURLScheme(url.protocol()))
+    if (frameLoader() && !frameLoader()->activeDocumentLoader()->isStopping() && m_substituteData.isValid()) {
+        if (m_substituteData.content()->size())
+            didReceiveData(m_substituteData.content()->data(), m_substituteData.content()->size(), m_substituteData.content()->size(), true);
+        if (frameLoader() && !frameLoader()->activeDocumentLoader()->isStopping()) 
             didFinishLoading(0);
     }
 }
@@ -386,7 +378,7 @@ void MainResourceLoader::didReceiveResponse(const ResourceResponse& r)
     // There is a bug in CFNetwork where callbacks can be dispatched even when loads are deferred.
     // See <rdar://problem/6304600> for more details.
 #if !USE(CF)
-    ASSERT(shouldLoadAsEmptyDocument(r.url()) || !defersLoading());
+    ASSERT(!defersLoading());
 #endif
 
     if (m_loadingMultipartContent) {
@@ -501,7 +493,7 @@ void MainResourceLoader::didFinishLoading(double finishTime)
     // There is a bug in CFNetwork where callbacks can be dispatched even when loads are deferred.
     // See <rdar://problem/6304600> for more details.
 #if !USE(CF)
-    ASSERT(shouldLoadAsEmptyDocument(frameLoader()->activeDocumentLoader()->url()) || !defersLoading() || InspectorInstrumentation::isDebuggerPaused(m_frame.get()));
+    ASSERT(!defersLoading() || InspectorInstrumentation::isDebuggerPaused(m_frame.get()));
 #endif
 
     // The additional processing can do anything including possibly removing the last
@@ -562,18 +554,6 @@ void MainResourceLoader::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) c
     info.addMember(m_dataLoadTimer);
 }
 
-void MainResourceLoader::handleEmptyLoad(const KURL& url, bool forURLScheme)
-{
-    String mimeType;
-    if (forURLScheme)
-        mimeType = frameLoader()->client()->generatedMIMETypeForURLScheme(url.protocol());
-    else
-        mimeType = "text/html";
-    
-    ResourceResponse response(url, mimeType, 0, String(), String());
-    didReceiveResponse(response);
-}
-
 void MainResourceLoader::handleSubstituteDataLoadNow(MainResourceLoaderTimer*)
 {
     RefPtr<MainResourceLoader> protect(this);
@@ -610,29 +590,10 @@ void MainResourceLoader::handleSubstituteDataLoadSoon(const ResourceRequest& r)
         handleSubstituteDataLoadNow(0);
 }
 
-bool MainResourceLoader::loadNow(ResourceRequest& r)
+void MainResourceLoader::loadNow(ResourceRequest& r)
 {
-    bool shouldLoadEmptyBeforeRedirect = shouldLoadAsEmptyDocument(r.url());
-
     ASSERT(!m_handle);
-    ASSERT(shouldLoadEmptyBeforeRedirect || !defersLoading());
-
-    // Send this synthetic delegate callback since clients expect it, and
-    // we no longer send the callback from within NSURLConnection for
-    // initial requests.
-    willSendRequest(r, ResourceResponse());
-    ASSERT(!deletionHasBegun());
-
-    // <rdar://problem/4801066>
-    // willSendRequest() is liable to make the call to frameLoader() return NULL, so we need to check that here
-    if (!frameLoader())
-        return false;
-    
-    const KURL& url = r.url();
-    bool shouldLoadEmpty = shouldLoadAsEmptyDocument(url) && !m_substituteData.isValid();
-
-    if (shouldLoadEmptyBeforeRedirect && !shouldLoadEmpty && defersLoading())
-        return true;
+    ASSERT(!defersLoading());
 
 #if USE(PLATFORM_STRATEGIES)
     platformStrategies()->loaderStrategy()->resourceLoadScheduler()->addMainResourceLoad(this);
@@ -642,12 +603,10 @@ bool MainResourceLoader::loadNow(ResourceRequest& r)
 
     if (m_substituteData.isValid())
         handleSubstituteDataLoadSoon(r);
-    else if (shouldLoadEmpty || frameLoader()->client()->representationExistsForURLScheme(url.protocol()))
-        handleEmptyLoad(url, !shouldLoadEmpty);
     else
         m_handle = ResourceHandle::create(m_frame->loader()->networkingContext(), r, this, false, true);
 
-    return false;
+    return;
 }
 
 void MainResourceLoader::load(const ResourceRequest& r, const SubstituteData& substituteData)
@@ -665,23 +624,25 @@ void MainResourceLoader::load(const ResourceRequest& r, const SubstituteData& su
     documentLoader()->timing()->markFetchStart();
     ResourceRequest request(r);
 
+    // Send this synthetic delegate callback since clients expect it, and
+    // we no longer send the callback from within NSURLConnection for
+    // initial requests.
+    willSendRequest(request, ResourceResponse());
+    ASSERT(!deletionHasBegun());
+
+    // <rdar://problem/4801066>
+    // willSendRequest() is liable to make the call to frameLoader() return null, so we need to check that here
+    if (!frameLoader() || request.isNull()) {
+        releaseResources();
+        return;
+    }
+
     documentLoader()->applicationCacheHost()->maybeLoadMainResource(request, m_substituteData);
 
-    bool defer = defersLoading();
-    if (defer) {
-        bool shouldLoadEmpty = shouldLoadAsEmptyDocument(request.url());
-        if (shouldLoadEmpty)
-            defer = false;
-    }
-    if (!defer) {
-        if (loadNow(request)) {
-            // Started as an empty document, but was redirected to something non-empty.
-            ASSERT(defersLoading());
-            defer = true;
-        }
-    }
-    if (defer)
+    if (defersLoading())
         m_initialRequest = request;
+    else
+        loadNow(request);
 }
 
 void MainResourceLoader::setDefersLoading(bool defers)
