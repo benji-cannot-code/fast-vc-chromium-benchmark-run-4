ea40d89256c2eaa463c410192e6ea91d2cba1c97
youngki@chromium.org <youngki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 00:47:48 +0000

    Implemented BluetoothAdapterWin::IsPresent().
    BUG=135470
    
    
    Review URL: https://chromiumcodereview.appspot.com/11345037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167539 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth_adapter_win.h
+++ device/bluetooth/bluetooth_adapter_win.h
@@ -14,9 +14,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace device {
 
 class BluetoothAdapterFactory;
+class BluetoothAdapterWinTest;
 class BluetoothDevice;
 
 class BluetoothAdapterWin : public BluetoothAdapter {
+ public:
   // BluetoothAdapter override
   virtual void AddObserver(BluetoothAdapter::Observer* observer) OVERRIDE;
   virtual void RemoveObserver(BluetoothAdapter::Observer* observer) OVERRIDE;
@@ -39,11 +41,23 @@ class BluetoothAdapterWin : public BluetoothAdapter {
       const BluetoothOutOfBandPairingDataCallback& callback,
       const ErrorCallback& error_callback) OVERRIDE;
 
- private:
-  friend class device::BluetoothAdapterFactory;
-
+ protected:
   BluetoothAdapterWin();
   virtual ~BluetoothAdapterWin();
+
+  virtual void UpdateAdapterState();
+
+ private:
+  friend class BluetoothAdapterFactory;
+  friend class BluetoothAdapterWinTest;
+
+  // Obtains the default adapter info (the first bluetooth radio info found on
+  // the system) and tracks future changes to it.
+  void TrackDefaultAdapter();
+
+  void PollAdapterState();
+
+  static const int kPollIntervalMs;
 
   // NOTE: This should remain the last member so it'll be destroyed and
   // invalidate its weak pointers before any other members are destroyed.
--- device/device.gyp
+++ device/device.gyp
@@ -85,6 +85,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'sources': [
         'bluetooth/bluetooth_adapter_chromeos_unittest.cc',
         'bluetooth/bluetooth_adapter_devices_chromeos_unittest.cc',
+        'bluetooth/bluetooth_adapter_win_unittest.cc',
         'bluetooth/bluetooth_service_record_unittest.cc',
         'bluetooth/bluetooth_utils_unittest.cc',
         'test/device_test_suite.cc',
