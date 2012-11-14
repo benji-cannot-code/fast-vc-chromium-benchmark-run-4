a9c4a9b7f897ba33a4453c49baba1dce0b67be8c
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-14 04:22:39 +0000

    [V8] Add missing ENABLE(SVG) test in header https://bugs.webkit.org/show_bug.cgi?id=102143
    Reviewed by Kentaro Hara.
    
    Need to wrap the #include of a header that's only conditionally generated.
    
    Fixes build error if compiling e.g. w/ GYP_DEFINES="enable_svg=0"
    
    * bindings/v8/custom/V8ElementCustom.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134527 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8ElementCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8ElementCustom.cpp
@@ -34,7 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "V8Element.h"
 #include "V8HTMLElement.h"
+
+#if ENABLE(SVG)
 #include "V8SVGElement.h"
+#endif
 
 namespace WebCore {
 
