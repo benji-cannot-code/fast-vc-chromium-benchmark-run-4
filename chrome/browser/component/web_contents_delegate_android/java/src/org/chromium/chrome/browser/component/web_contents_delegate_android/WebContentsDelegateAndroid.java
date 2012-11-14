5c0bf9547131f9e25ab9803cf7f74d5bda51f09f
jinsukkim@google.com <jinsukkim@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 01:29:12 +0000

    Start hooking up "element.requestFullscreen()" in chrome for android.
    Plumbs through the required mechanism in web_contents_delegate.cc. This
    doesn't make any functional/visible changes but paves the way to support
    fullscreen on Android platform.
    
    BUG=113041
    
    
    Review URL: https://chromiumcodereview.appspot.com/11360077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167562 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component/web_contents_delegate_android/java/src/org/chromium/chrome/browser/component/web_contents_delegate_android/WebContentsDelegateAndroid.java
+++ chrome/browser/component/web_contents_delegate_android/java/src/org/chromium/chrome/browser/component/web_contents_delegate_android/WebContentsDelegateAndroid.java
@@ -116,4 +116,13 @@ public class WebContentsDelegateAndroid {
     @CalledByNative
     public void showRepostFormWarningDialog(ContentViewCore contentViewCore) {
     }
+
+    @CalledByNative
+    public void toggleFullscreenModeForTab(boolean enterFullscreen) {
+    }
+
+    @CalledByNative
+    public boolean isFullscreenForTabOrPending() {
+        return false;
+    }
 }
