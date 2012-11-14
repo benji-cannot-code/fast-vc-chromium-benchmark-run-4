d57524456d29139ee6b1f943dd1cc497d8d5e74e
miket@chromium.org <miket@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-14 04:10:29 +0000

    Continuing TODO items from a previous CL, remove the "extra_field" and "second_extra_field" keys from the API and replace with real keys from the Base Format View spec. Implement a rough layout that resembles the expanded mode of the view.
    Outstanding items:
    
      - As stated above, the layout is still ugly.
      - We don't gracefully handle empty buttons. We don't crash, either, but it looks even uglier when the buttons are missing.
      - The buttons don't do anything when the user clicks on them.
      - The second icon is just a redraw of the first.
      - The timestamp is hardcoded to "10:43 AM" for layout purposes. I need to do more research on the appropriate date-formatting facilities available to us, and from that back out the timestamp field format in the API itself.
    
    Review URL: https://chromiumcodereview.appspot.com/11358144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167589 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_view.cc
+++ ui/message_center/message_view.cc
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/ui_strings.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/models/simple_menu_model.h"
+#include "ui/base/resource/resource_bundle.h"
+#include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/views/controls/button/image_button.h"
 #include "ui/views/controls/menu/menu_model_adapter.h"
 #include "ui/views/controls/menu/menu_runner.h"
@@ -108,9 +110,14 @@ MessageView::MessageView(
     const NotificationList::Notification& notification)
     : list_delegate_(list_delegate),
       notification_(notification),
-      icon_(NULL),
       close_button_(NULL),
       scroller_(NULL) {
+  close_button_ = new views::ImageButton(this);
+  close_button_->SetImage(
+      views::CustomButton::BS_NORMAL,
+      ResourceBundle::GetSharedInstance().GetImageSkiaNamed(IDR_MESSAGE_CLOSE));
+  close_button_->SetImageAlignment(views::ImageButton::ALIGN_CENTER,
+                                   views::ImageButton::ALIGN_MIDDLE);
 }
 
 MessageView::MessageView() {
@@ -153,7 +160,7 @@ ui::EventResult MessageView::OnGestureEvent(ui::GestureEvent* event) {
 }
 
 void MessageView::ButtonPressed(views::Button* sender,
-                                        const ui::Event& event) {
+                                const ui::Event& event) {
   if (sender == close_button_)
     list_delegate_->SendRemoveNotification(notification_.id);
 }
