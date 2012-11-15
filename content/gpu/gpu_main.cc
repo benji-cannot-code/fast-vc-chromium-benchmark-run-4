def88f951f1f1f155e81cfc97b901a6ec0582e60
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 19:55:20 +0000

    Stop watchdog thread before sandbox initialization.
    Then Start it again after sandbox is up.
    
    BUG=132893
    TEST=no sandbox error message
    Review URL: https://codereview.chromium.org/11275337
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167995 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_main.cc
+++ content/gpu/gpu_main.cc
@@ -233,7 +233,11 @@ int GpuMain(const MainFunctionParams& parameters) {
 #endif
 
     if (do_init_sandbox) {
+      if (watchdog_thread.get())
+        watchdog_thread->Stop();
       gpu_info.sandboxed = InitializeSandbox();
+      if (watchdog_thread.get())
+        watchdog_thread->Start();
     }
   }
 #endif
