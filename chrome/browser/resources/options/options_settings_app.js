104ed112805af61ecd9e35ac710e9415407e796a
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 08:21:01 +0000

    Settings App WebUI description string substitution, "re"localization. This change introduces the mechanism for subtituting strings in the Settings App WebUI to allow for the different wording when the settings webui elements are used for the settings app, and sample strings.
    This introduces OptionsPageUIHandler::RegisterConstStrings, a helper
    function to remove some of the boilerplate around substituting product
    names into loadTimeData.
    
    The approach for substituting strings for the settings app is by populating
    a dictionary within loadTimeData which is used as a source of replacements
    in JS (by adding a JS function LoadTimeData.overrideValues). This allows
    the same strings.js to be used, and cached, on the browser side.
    
    BUG=130210
    TEST=chrome://settings unchanged, chrome://settings-frame/options_settings_app.html
    should show new wording for, e.g., Users (on an empty profile): "You are currently
    the only App Launcher user."
    
    Review URL: https://chromiumcodereview.appspot.com/11339035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167886 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options_settings_app.js
+++ chrome/browser/resources/options/options_settings_app.js
@@ -11,4 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Override the offset in the options page.
   OptionsPage.setHorizontalOffset(30);
+
+  loadTimeData.overrideValues(loadTimeData.getValue('settingsApp'));
 }());
