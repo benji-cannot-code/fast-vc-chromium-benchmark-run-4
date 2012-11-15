1ee427e94c65a19d78e8e95406ae36268aea64d3
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-15 11:01:22 +0000

    Unreviewed. Fix build break when using --no-tiled-backing-store option.
    * ewk/ewk_frame_private.h:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_frame_private.h
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_frame_private.h
@@ -27,12 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Evas.h>
 #include <wtf/PassRefPtr.h>
 #include <wtf/Vector.h>
+#include <wtf/text/WTFString.h>
 
 namespace WebCore {
 class HistoryItem;
 class HTMLPlugInElement;
 class KURL;
 class Frame;
+class IntSize;
 class Widget;
 }
 
