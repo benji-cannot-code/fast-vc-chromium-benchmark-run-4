684a6d67c3ee797a8b9ce1eee916d678c9ac6bb8
irobert@chromium.org <irobert@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 23:29:18 +0000

    Fix Cross-Process Redirect Navigation (Second attempt, after committing in http://crrev.com/167800.)
    BUG=104493
    TEST=Go back when clicking an SSL search result to a hosted app.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11359098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168072 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/page_navigator.cc
+++ content/public/browser/page_navigator.cc
@@ -18,7 +18,8 @@ OpenURLParams::OpenURLParams(
       source_frame_id(-1),
       disposition(disposition),
       transition(transition),
-      is_renderer_initiated(is_renderer_initiated) {
+      is_renderer_initiated(is_renderer_initiated),
+      is_cross_site_redirect(false) {
 }
 
 OpenURLParams::OpenURLParams(
@@ -33,14 +34,16 @@ OpenURLParams::OpenURLParams(
       source_frame_id(source_frame_id),
       disposition(disposition),
       transition(transition),
-      is_renderer_initiated(is_renderer_initiated) {
+      is_renderer_initiated(is_renderer_initiated),
+      is_cross_site_redirect(false) {
 }
 
 OpenURLParams::OpenURLParams()
     : source_frame_id(-1),
       disposition(UNKNOWN),
       transition(PageTransitionFromInt(0)),
-      is_renderer_initiated(false) {
+      is_renderer_initiated(false),
+      is_cross_site_redirect(false) {
 }
 
 OpenURLParams::~OpenURLParams() {
