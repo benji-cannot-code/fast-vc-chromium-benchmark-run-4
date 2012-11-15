1cbaed91200153ff0f4eedbe11e043d847c1a69f
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-15 20:01:20 +0000

    [V8] We shouldn't call deprecated V8 APIs https://bugs.webkit.org/show_bug.cgi?id=102407
    Reviewed by Eric Seidel.
    
    This patch was written by Sven Panne on the V8 team. He would prefer
    that we not call deprecated V8 APIs. This patch updates these call
    sites to more modern idioms.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateConstructorGetter):
    (GenerateImplementation):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::TestObjConstructorGetter):
    * bindings/v8/PageScriptDebugServer.cpp:
    (WebCore::PageScriptDebugServer::addListener):
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::setContextDebugId):
    (WebCore::ScriptController::contextDebugId):
    * bindings/v8/ScriptProfiler.cpp:
    (WebCore::ScriptProfiler::objectByHeapObjectId):
        - This code doesn't seem to do anything anymore.
    * bindings/v8/V8DOMConfiguration.h:
    (WebCore::V8DOMConfiguration::configureAttribute):
    * bindings/v8/V8DOMWindowShell.cpp:
    (WebCore::setInjectedScriptContextDebugId):
    * bindings/v8/V8DOMWrapper.cpp:
    (WebCore::V8DOMWrapper::maybeDOMWrapper):
    (WebCore::V8DOMWrapper::isWrapperOfType):
    * bindings/v8/V8EventListenerList.h:
    (WebCore::V8EventListenerList::doFindWrapper):
    (WebCore::V8EventListenerList::findOrCreateWrapper):
    * bindings/v8/WorkerContextExecutionProxy.cpp:
    (WebCore::WorkerContextExecutionProxy::initializeIfNeeded):
    * bindings/v8/WrapperTypeInfo.h:
    (WebCore::WrapperTypeInfo::unwrap):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/ScriptController.cpp
@@ -692,7 +692,7 @@ bool ScriptController::setContextDebugId(int debugId)
         return false;
     v8::HandleScope scope;
     v8::Handle<v8::Context> context = m_windowShell->context();
-    if (!context->GetData()->IsUndefined())
+    if (!context->GetEmbedderData(0)->IsUndefined())
         return false;
 
     v8::Context::Scope contextScope(context);
@@ -700,7 +700,7 @@ bool ScriptController::setContextDebugId(int debugId)
     char buffer[32];
     snprintf(buffer, sizeof(buffer), "page,%d", debugId);
     buffer[31] = 0;
-    context->SetData(v8::String::New(buffer));
+    context->SetEmbedderData(0, v8::String::New(buffer));
 
     return true;
 }
@@ -708,9 +708,9 @@ bool ScriptController::setContextDebugId(int debugId)
 int ScriptController::contextDebugId(v8::Handle<v8::Context> context)
 {
     v8::HandleScope scope;
-    if (!context->GetData()->IsString())
+    if (!context->GetEmbedderData(0)->IsString())
         return -1;
-    v8::String::AsciiValue ascii(context->GetData());
+    v8::String::AsciiValue ascii(context->GetEmbedderData(0));
     char* comma = strnstr(*ascii, ",", ascii.length());
     if (!comma)
         return -1;
