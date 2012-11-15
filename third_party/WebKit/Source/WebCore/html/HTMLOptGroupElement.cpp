df40426c8fdab177fb54c278238b9af3a45f8352
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-15 07:41:33 +0000

    Changing pseudoClass (:disabled) should cause distribution. https://bugs.webkit.org/show_bug.cgi?id=101901
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    We have to invalidate distribution when pseudo-class (:disabled) is changed.
    
    According to the HTML5 spec, :disabled will match button, input, select, textarea, optgroup,
    option, command, li, and fieldset. However, command is not implemented yet, we skip li and command. li might
    have 'disabled' state, but we need a command element to make it 'disabled' state.
    
    Tests: fast/dom/shadow/pseudoclass-update-disabled-button.html
           fast/dom/shadow/pseudoclass-update-disabled-fieldset.html
           fast/dom/shadow/pseudoclass-update-disabled-input.html
           fast/dom/shadow/pseudoclass-update-disabled-optgroup.html
           fast/dom/shadow/pseudoclass-update-disabled-option.html
           fast/dom/shadow/pseudoclass-update-disabled-select.html
           fast/dom/shadow/pseudoclass-update-disabled-textarea.html
    
    * html/HTMLFormControlElement.cpp:
    (WebCore::HTMLFormControlElement::disabledAttributeChanged):
    * html/HTMLOptGroupElement.cpp:
    (WebCore::HTMLOptGroupElement::parseAttribute):
    * html/HTMLOptionElement.cpp:
    (WebCore::HTMLOptionElement::parseAttribute):
    
    LayoutTests:
    
    * fast/dom/shadow/pseudoclass-update-disabled-button-expected.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-button.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-fieldset-expected.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-fieldset.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-input-expected.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-input.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-optgroup-expected.txt: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-optgroup.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-option-expected.txt: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-option.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-select-expected.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-select.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-textarea-expected.html: Added.
    * fast/dom/shadow/pseudoclass-update-disabled-textarea.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134748 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLOptGroupElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLOptGroupElement.cpp
@@ -83,6 +83,9 @@ void HTMLOptGroupElement::parseAttribute(const Attribute& attribute)
 {
     HTMLElement::parseAttribute(attribute);
     recalcSelectOptions();
+
+    if (attribute.name() == disabledAttr)
+        invalidateParentDistributionIfNecessary(this, SelectRuleFeatureSet::RuleFeatureDisabled);
 }
 
 void HTMLOptGroupElement::recalcSelectOptions()
