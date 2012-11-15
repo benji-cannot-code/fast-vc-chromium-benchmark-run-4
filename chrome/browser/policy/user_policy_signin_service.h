58cbbef3ae5156950f2beb3e4f58fd3378de23a9
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 18:05:57 +0000

    Updated CloudPolicyService to notify Observers when initialization is complete. This allows observers to determine when we do not have a cached DMToken for the CloudPolicyClient.
    Observers can now avoid fetching auth tokens if the policy code already has a cached DMToken from a previous policy fetch.
    
    This CL does not change the ChromeOS functionality, as the logic there is more complex due to the need to support both new and legacy policy frameworks. I've added a TODO to address the cros code as part of removing the legacy policy framework.
    
    Adding nkostylev as TBR for comment-only change.
    
    BUG=143187
    TBR=nkostylev
    
    
    Review URL: https://chromiumcodereview.appspot.com/11052020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167956 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/user_policy_signin_service.h
+++ chrome/browser/policy/user_policy_signin_service.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "chrome/browser/policy/cloud_policy_service.h"
 #include "chrome/browser/profiles/profile_keyed_service.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -31,6 +32,7 @@ class UserCloudPolicyManager;
 class UserPolicySigninService
     : public ProfileKeyedService,
       public OAuth2AccessTokenConsumer,
+      public CloudPolicyService::Observer,
       public content::NotificationObserver {
  public:
   // Creates a UserPolicySigninService associated with the passed |profile|.
@@ -41,6 +43,9 @@ class UserPolicySigninService
   virtual void Observe(int type,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
+
+  // CloudPolicyService::Observer implementation.
+  virtual void OnInitializationCompleted(CloudPolicyService* service) OVERRIDE;
 
   // OAuth2AccessTokenConsumer implementation.
   virtual void OnGetTokenSuccess(const std::string& access_token,
@@ -55,6 +60,9 @@ class UserPolicySigninService
   // Fetches an OAuth token to allow the cloud policy service to register with
   // the cloud policy server.
   void RegisterCloudPolicyService();
+
+  // Helper routine to unregister for CloudPolicyService notifications.
+  void StopObserving();
 
   // Weak pointer to the profile this service is associated with.
   Profile* profile_;
