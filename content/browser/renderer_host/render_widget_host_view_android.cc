87332d08b48bb4ddfb274c8dec68d7ad814d3993
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 01:47:14 +0000

    Expose the RWHVA's WebLayer.
    When we have a prerenderer RWHV the delegate is not set so we never get the
    AttachLayer call, this means we need to expose a method to query the current
    layer when we swap out our TabContents and therefore our RWHVA instances.
    
    BUG=http://crbug.com/159763
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293260
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167814 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -55,6 +55,7 @@ RenderWidgetHostViewAndroid::RenderWidgetHostViewAndroid(
       is_hidden_(!content_view_core),
       content_view_core_(content_view_core),
       ime_adapter_android_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+      cached_background_color_(SK_ColorWHITE),
       texture_layer_(WebKit::WebExternalTextureLayer::create()),
       texture_id_in_layer_(0) {
   host_->SetView(this);
@@ -176,6 +177,10 @@ bool RenderWidgetHostViewAndroid::PopulateBitmapWithContents(jobject jbitmap) {
   }
 
   return true;
+}
+
+WebKit::WebLayer* RenderWidgetHostViewAndroid::GetWebLayer() {
+  return texture_layer_->layer();
 }
 
 gfx::NativeView RenderWidgetHostViewAndroid::GetNativeView() const {
--- content/public/browser/android/content_view_core.h
+++ content/public/browser/android/content_view_core.h
@@ -19,6 +19,10 @@ namespace ui {
 class WindowAndroid;
 }
 
+namespace WebKit {
+class WebLayer;
+}
+
 namespace content {
 class WebContents;
 
@@ -41,7 +45,7 @@ class ContentViewCore {
   virtual jint GetCurrentRenderProcessId(JNIEnv* env, jobject obj) = 0;
   virtual void ShowPastePopup(int x, int y) = 0;
   virtual unsigned int GetScaledContentTexture(const gfx::Size& size) = 0;
-
+  virtual WebKit::WebLayer* GetWebLayer() = 0;
  protected:
   virtual ~ContentViewCore() {};
 };
