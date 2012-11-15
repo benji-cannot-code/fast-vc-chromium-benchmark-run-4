10d6cbf5331a54ace06c435c896e937863d1cdc0
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 17:08:08 +0000

    aura: Refactor the code used to update an event for the device-scale-factor.
    BUG=none
    
    Review URL: https://codereview.chromium.org/11410103
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167941 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/root_window.cc
+++ ui/aura/root_window.cc
@@ -587,6 +587,13 @@ gfx::Point RootWindow::QueryMouseLocationForTest() const {
 ////////////////////////////////////////////////////////////////////////////////
 // RootWindow, private:
 
+void RootWindow::TransformEventForDeviceScaleFactor(ui::LocatedEvent* event) {
+  float scale = ui::GetDeviceScaleFactor(layer());
+  gfx::Transform transform = layer()->transform();
+  transform.ConcatScale(scale, scale);
+  event->UpdateForRootTransform(transform);
+}
+
 void RootWindow::HandleMouseMoved(const ui::MouseEvent& event, Window* target) {
   if (target == mouse_moved_handler_)
     return;
@@ -816,11 +823,8 @@ bool RootWindow::OnHostMouseEvent(ui::MouseEvent* event) {
 
 bool RootWindow::OnHostScrollEvent(ui::ScrollEvent* event) {
   DispatchHeldMouseMove();
-  float scale = ui::GetDeviceScaleFactor(layer());
-  gfx::Transform transform = layer()->transform();
-  transform.ConcatScale(scale, scale);
-  event->UpdateForRootTransform(transform);
 
+  TransformEventForDeviceScaleFactor(event);
   SetLastMouseLocation(this, event->location());
   synthesize_mouse_move_ = false;
 
@@ -860,10 +864,7 @@ bool RootWindow::OnHostTouchEvent(ui::TouchEvent* event) {
     default:
       break;
   }
-  float scale = ui::GetDeviceScaleFactor(layer());
-  gfx::Transform transform = layer()->transform();
-  transform.ConcatScale(scale, scale);
-  event->UpdateForRootTransform(transform);
+  TransformEventForDeviceScaleFactor(event);
   bool handled = false;
   ui::EventResult result = ui::ER_UNHANDLED;
   Window* target = client::GetCaptureWindow(this);
@@ -953,10 +954,7 @@ RootWindow* RootWindow::AsRootWindow() {
 // RootWindow, private:
 
 bool RootWindow::DispatchMouseEventImpl(ui::MouseEvent* event) {
-  float scale = ui::GetDeviceScaleFactor(layer());
-  gfx::Transform transform = layer()->transform();
-  transform.ConcatScale(scale, scale);
-  event->UpdateForRootTransform(transform);
+  TransformEventForDeviceScaleFactor(event);
   Window* target = mouse_pressed_handler_ ?
       mouse_pressed_handler_ : client::GetCaptureWindow(this);
   if (!target)
