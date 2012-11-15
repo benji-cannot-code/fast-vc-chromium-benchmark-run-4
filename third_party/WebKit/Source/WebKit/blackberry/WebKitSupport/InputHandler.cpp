218e298e0d1e2be3160ab59ad7289217d2cd1399
mifenton@rim.com <mifenton@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-15 18:40:35 +0000

    [BlackBerry] Don't restore zoom and scroll when leaving an input field. https://bugs.webkit.org/show_bug.cgi?id=102376
    Reviewed by Yong Li.
    
    Restoring the zoom causes undesirable behavior when switching between fields.
    
    Reviewed Internally by Andrew Lo.
    
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::InputHandler):
    (BlackBerry::WebKit::InputHandler::ensureFocusTextElementVisible):
    * WebKitSupport/InputHandler.h:
    (InputHandler):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134795 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
@@ -140,7 +140,6 @@ InputHandler::InputHandler(WebPagePrivate* page)
     , m_delayKeyboardVisibilityChange(false)
     , m_request(0)
     , m_processingTransactionId(-1)
-    , m_focusZoomScale(0.0)
     , m_receivedBackspaceKeyDown(false)
     , m_expectedKeyUpChar(0)
 {
@@ -1117,17 +1116,6 @@ void InputHandler::ensureFocusTextElementVisible(CaretScrollType scrollType)
         break;
     }
     case VisibleSelection::NoSelection:
-        if (m_focusZoomScale) {
-            m_webPage->zoomAboutPoint(m_focusZoomScale, selectionFocusRect.location());
-            InputLog(LogLevelInfo, "InputHandler::ensureFocusTextElementVisible resetting zoom to %f", m_focusZoomScale);
-            m_focusZoomScale = 0.0;
-        }
-
-        if (m_focusScrollLocation != WebCore::IntPoint(-1, -1)) {
-            mainFrameView->setScrollPosition(m_focusScrollLocation);
-            InputLog(LogLevelInfo, "InputHandler::ensureFocusTextElementVisible resetting scroll to %d, %d", m_focusScrollLocation.x(), m_focusScrollLocation.y());
-            m_focusScrollLocation = WebCore::IntPoint(-1, -1);
-        }
         return;
     }
 
@@ -1135,17 +1123,11 @@ void InputHandler::ensureFocusTextElementVisible(CaretScrollType scrollType)
 
     m_webPage->suspendBackingStore();
 
-    WebCore::IntPoint sourceScrollPosition = mainFrameView->scrollPosition();
-
     // If the text is too small, zoom in to make it a minimum size.
     // The minimum size being defined as 3 mm is a good value based on my observations.
     static const int s_minimumTextHeightInPixels = Graphics::Screen::primaryScreen()->heightInMMToPixels(3);
 
     if (m_webPage->isUserScalable() && fontHeight && fontHeight * m_webPage->currentScale() < s_minimumTextHeightInPixels && !isRunningDrt()) {
-        if (!m_focusZoomScale) {
-            m_focusZoomScale = m_webPage->currentScale();
-            m_focusScrollLocation = sourceScrollPosition;
-        }
         double zoomScaleRequired = static_cast<double>(s_minimumTextHeightInPixels) / fontHeight;
         m_webPage->zoomAboutPoint(zoomScaleRequired, selectionFocusRect.location());
         InputLog(LogLevelInfo, "InputHandler::ensureFocusTextElementVisible zooming in to %f at point %d, %d", zoomScaleRequired, selectionFocusRect.location().x(), selectionFocusRect.location().y());
@@ -1217,8 +1199,6 @@ void InputHandler::ensureFocusTextElementVisible(CaretScrollType scrollType)
             WebCore::IntPoint maximumScrollPosition = WebCore::IntPoint(mainFrameView->contentsWidth() - actualScreenRect.width(), mainFrameView->contentsHeight() - actualScreenRect.height());
             scrollLocation = scrollLocation.shrunkTo(maximumScrollPosition);
             if (scrollLocation != mainFrameView->scrollPosition()) {
-                if (m_focusScrollLocation == WebCore::IntPoint(-1, -1))
-                    m_focusScrollLocation = sourceScrollPosition;
                 mainFrameView->setScrollPosition(scrollLocation);
                 mainFrameView->setConstrainsScrollingToContentEdge(true);
                 InputLog(LogLevelInfo, "InputHandler::ensureFocusTextElementVisible scrolling to point %d, %d", scrollLocation.x(), scrollLocation.y());
