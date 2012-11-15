2d5415e406f2970c6c5c5c88d78afd2c4223e78c
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 19:57:03 +0000

    cc: Add PictureLayerTiling for impl-side painting
    This adds an interface for generating/iterating/creating tiles and tilings.
    PictureLayerImpl now knows a lot less about the internals of the tiling (yay).
    
    TBR=nduca@chromium.org
    BUG=155209
    
    
    Review URL: https://chromiumcodereview.appspot.com/11377176
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167997 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/cc.gyp
+++ cc/cc.gyp
@@ -118,6 +118,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'picture_layer.h',
       'picture_layer_impl.cc',
       'picture_layer_impl.h',
+      'picture_layer_tiling.cc',
+      'picture_layer_tiling.h',
       'picture_pile.cc',
       'picture_pile.h',
       'platform_color.h',
--- cc/picture_layer_impl.h
+++ cc/picture_layer_impl.h
@@ -7,14 +7,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_PICTURE_LAYER_IMPL_H_
 
 #include "cc/layer_impl.h"
+#include "cc/picture_layer_tiling.h"
 #include "cc/picture_pile.h"
+#include "cc/scoped_ptr_vector.h"
 
 namespace cc {
 
 struct AppendQuadsData;
 class QuadSink;
 
-class CC_EXPORT PictureLayerImpl : public LayerImpl {
+class CC_EXPORT PictureLayerImpl : public LayerImpl,
+                                   public PictureLayerTilingClient {
 public:
   static scoped_ptr<PictureLayerImpl> create(int id)
   {
@@ -27,9 +30,20 @@ public:
   virtual void appendQuads(QuadSink&, AppendQuadsData&) OVERRIDE;
   virtual void dumpLayerProperties(std::string*, int indent) const OVERRIDE;
 
+  // PictureLayerTilingClient overrides.
+  virtual scoped_refptr<Tile> CreateTile(PictureLayerTiling*,
+                                         gfx::Rect) OVERRIDE;
+
+  // PushPropertiesTo active tree => pending tree
+  void SyncFromActiveLayer(const PictureLayerImpl* other);
+
+  // Called post-calcDraw.
+  void Update();
+
 protected:
   PictureLayerImpl(int id);
 
+  ScopedPtrVector<PictureLayerTiling> tilings_;
   PicturePile pile_;
 
   friend class PictureLayer;
