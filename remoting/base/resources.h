827df2bc6a20a93d47583a255bfb354a8e4231f6
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 01:28:29 +0000

    Revert 167794 - Native remoting host configurer for linux.
    This is initial version of the remoting host configurer for linux. It doesn't look pretty yet,
    but it works.
    
    This requires a new dependency and a PSA was not sent in advance to chromium-dev.  It breaks
    out bots because someone needs to manually run install-build-deps on each of them.
    
    Please wait a couple days before landing.  yfriedman has sent the required PSA.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11260004
    
    TBR=sergeyu@chromium.org
    Review URL: https://codereview.chromium.org/11362266
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167811 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/base/resources.h
+++ remoting/base/resources.h
@@ -10,10 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace remoting {
 
-// Loads chromoting resources. Returns false in case of a failure. |pref_locale|
-// is passed to l10n_util::GetApplicationLocale(), so the default system locale
-// is used if |pref_locale| is empty.
-bool LoadResources(const std::string& pref_locale);
+// Loads chromoting resources. Returns false in case of a failure.
+bool LoadResources(const std::string& locale);
 
 }  // namespace remoting
 
