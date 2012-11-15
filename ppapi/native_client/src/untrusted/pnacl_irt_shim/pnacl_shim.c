0aaa20c3a6c170cabc4605cecace17305370db19
jvoung@google.com <jvoung@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-15 18:15:41 +0000

    Get rid of pnacl_shim time stamps.
    They cause superfluous patch clashes.
    
    semi-related to:
    BUG=160550
    
    Review URL: https://codereview.chromium.org/11312261
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@167960 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/untrusted/pnacl_irt_shim/pnacl_shim.c
+++ ppapi/native_client/src/untrusted/pnacl_irt_shim/pnacl_shim.c
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* Last generated from IDL: Mon Nov 12 11:22:17 2012. */
+/* NOTE: this is auto-generated from IDL */
 #include "ppapi/generators/pnacl_shim.h"
 
 #include "ppapi/c/ppb.h"
