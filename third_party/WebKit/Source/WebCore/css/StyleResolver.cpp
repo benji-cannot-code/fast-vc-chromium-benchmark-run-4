edd64acdaf219a07ae93d5890a10cc00f8307373
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 20:23:14 +0000

    StyleResolver: Only input elements need equal "readonly" attribute for style sharing. <http://webkit.org/b/102536>
    Reviewed by Antti Koivisto.
    
    Move the comparison of the "readonly" attribute into canShareStyleWithControl() since it's only
    relevant for sharing style between <input> elements.
    
    Also skip attribute comparisons for form control elements that share the same ElementAttributeData.
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::canShareStyleWithControl):
    (WebCore::haveIdenticalStyleAffectingAttributes):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134984 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
@@ -1084,8 +1084,12 @@ bool StyleResolver::canShareStyleWithControl(StyledElement* element) const
     if (!thisInputElement || !otherInputElement)
         return false;
 
-    if (thisInputElement->fastGetAttribute(typeAttr) != otherInputElement->fastGetAttribute(typeAttr))
-        return false;
+    if (thisInputElement->attributeData() != otherInputElement->attributeData()) {
+        if (thisInputElement->fastGetAttribute(typeAttr) != otherInputElement->fastGetAttribute(typeAttr))
+            return false;
+        if (thisInputElement->fastGetAttribute(readonlyAttr) != otherInputElement->fastGetAttribute(readonlyAttr))
+            return false;
+    }
 
     if (thisInputElement->isAutofilled() != otherInputElement->isAutofilled())
         return false;
@@ -1160,8 +1164,6 @@ static inline bool haveIdenticalStyleAffectingAttributes(StyledElement* a, Style
     if (a->fastGetAttribute(XMLNames::langAttr) != b->fastGetAttribute(XMLNames::langAttr))
         return false;
     if (a->fastGetAttribute(langAttr) != b->fastGetAttribute(langAttr))
-        return false;
-    if (a->fastGetAttribute(readonlyAttr) != b->fastGetAttribute(readonlyAttr))
         return false;
     // FIXME: This is probably not necessary.
     if (a->fastGetAttribute(cellpaddingAttr) != b->fastGetAttribute(cellpaddingAttr))
