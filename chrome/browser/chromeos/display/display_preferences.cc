fa9c35fae5a8ddeb0ed2a5b89f71b2d9b35d2e31
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 04:38:55 +0000

    Cancel the existing overscan settings when start calibration, for the visibility.
    BUG=161079
    TEST=manually on device
    
    
    Review URL: https://chromiumcodereview.appspot.com/11275330
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168136 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/display/display_preferences.cc
+++ chrome/browser/chromeos/display/display_preferences.cc
@@ -116,7 +116,8 @@ void NotifyDisplayLayoutChanged(PrefService* pref_service) {
 
 void NotifyDisplayOverscans() {
   PrefService* local_state = g_browser_process->local_state();
-  ash::internal::DisplayManager* display_manager = GetDisplayManager();
+  ash::DisplayController* display_controller =
+      ash::Shell::GetInstance()->display_controller();
 
   const base::DictionaryValue* overscans = local_state->GetDictionary(
       prefs::kDisplayOverscans);
@@ -140,9 +141,7 @@ void NotifyDisplayOverscans() {
       continue;
     }
 
-    // TODO(oshima|mukai): DisplayManager is internal class.
-    // Move |SetOverscanInsets| to display controller.
-    display_manager->SetOverscanInsets(display_id, insets);
+    display_controller->SetOverscanInsets(display_id, insets);
   }
 }
 
