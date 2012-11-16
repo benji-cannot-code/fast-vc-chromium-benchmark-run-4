0eb6c1045d182c1b172ef692ce7818051e809c9c
dglazkov@chromium.org <dglazkov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 20:31:09 +0000

    WebKit Roll 134928:134966.
    TBR=haraken
    BUG=
    TEST=
    
    Review URL: https://codereview.chromium.org/11413038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168279 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/restrictions/main.js
+++ chrome/test/data/extensions/platform_apps/restrictions/main.js
@@ -123,7 +123,7 @@ chrome.test.runTests([
     var xhr = new XMLHttpRequest();
     assertThrowsError(function() {
       xhr.open('GET', 'data:should not load', false);
-    }, 'INVALID_ACCESS_ERR: DOM Exception 15');
+    }, 'InvalidAccessError: DOM Exception 15');
     succeed();
   },
 
