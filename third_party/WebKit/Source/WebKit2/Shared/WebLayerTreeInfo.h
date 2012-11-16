58b23895f021c61fb65ed8b08f0b5e366e5b55a3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 14:58:37 +0000

    [Qt][Mac] Fix the build after r124873 https://bugs.webkit.org/show_bug.cgi?id=102475
    Patch by Peter Gal <galpeter@inf.u-szeged.hu> on 2012-11-16
    Reviewed by Noam Rosenthal.
    
    Change the CoordinatedImageBackingID typedef from uintptr_t to uint64_t.
    
    * Shared/WebLayerTreeInfo.h:
    * UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.messages.in:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134946 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/WebLayerTreeInfo.h
+++ third_party/WebKit/Source/WebKit2/Shared/WebLayerTreeInfo.h
@@ -34,7 +34,7 @@ namespace WebKit {
 typedef uint32_t WebLayerID;
 enum { InvalidWebLayerID = 0 };
 
-typedef uintptr_t CoordinatedImageBackingID;
+typedef uint64_t CoordinatedImageBackingID;
 enum { InvalidCoordinatedImageBackingID = 0 };
 
 // NOTE: WebLayerInfo should only use POD types, as to make serialization faster.
