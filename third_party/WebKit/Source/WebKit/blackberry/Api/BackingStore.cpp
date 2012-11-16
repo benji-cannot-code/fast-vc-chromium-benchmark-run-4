a8985744f59f36e231ad5006ef62b717d16a9890
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 19:48:03 +0000

    [BlackBerry] Fix regression causing checkerboard flicker after app wake-up https://bugs.webkit.org/show_bug.cgi?id=102526
    Patch by Adam Treat <atreat@rim.com> on 2012-11-16
    Reviewed by George Staikos.
    PR 245027
    
    The webkit patch for 219976 introduced an undesirable behavior change
    resulting in a regression where there was checkerboard flickering on
    resuming from an inactive application state.  This patch restores the
    previous behavior and fixes the regression.
    
    * Api/BackingStore.cpp:
    (BlackBerry::WebKit::BackingStore::createBackingStoreMemory):
    (BlackBerry::WebKit::BackingStore::releaseBackingStoreMemory):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134978 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -2700,11 +2700,13 @@ void BackingStore::createBackingStoreMemory()
     if (BackingStorePrivate::s_currentBackingStoreOwner == d->m_webPage)
         SurfacePool::globalSurfacePool()->createBuffers();
     resumeBackingStoreUpdates();
+    resumeScreenUpdates(BackingStore::RenderAndBlit);
 }
 
 void BackingStore::releaseBackingStoreMemory()
 {
     suspendBackingStoreUpdates();
+    suspendScreenUpdates();
     if (BackingStorePrivate::s_currentBackingStoreOwner == d->m_webPage)
         SurfacePool::globalSurfacePool()->releaseBuffers();
 }
