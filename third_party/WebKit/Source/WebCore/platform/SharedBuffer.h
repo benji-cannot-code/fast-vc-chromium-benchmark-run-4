94998fe2594bd77ad155b4d0b165ab587f3f9dec
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 20:46:59 +0000

    For single element arrays use the pointer into the CFDataRef instead of copying data https://bugs.webkit.org/show_bug.cgi?id=102306 <rdar://problem/12267471>
    Reviewed by Alexey Proskuryakov.
    
    We generally copy the data received from CFNetwork into our own buffers. But if the
    CFArrayRef has exactly one CFDataRef inside it, then we can just hold on to the CFDataRef
    and access its memory directly and avoid making a copy.
    
    This also moves the creation of PurgeableBuffer from CachedResource to SharedBuffer.
    SharedBuffer::createPurgeableBuffer() will avoid creating PurgeableBuffer when the
    SharedBuffer is backed by a NSData/CFDataRef and when we want to optimize and directly use
    the memory in the data array.
    
    No new tests because no change in functionality.
    
    * loader/ResourceBuffer.cpp:
    (WebCore::ResourceBuffer::createPurgeableBuffer): Added.
    * loader/ResourceBuffer.h:
    * loader/cache/CachedResource.cpp:
    (WebCore::CachedResource::makePurgeable):
    * platform/SharedBuffer.cpp:
    (WebCore::SharedBuffer::createPurgeableBuffer): Added.
    (WebCore::SharedBuffer::data):
    * platform/SharedBuffer.h:
    * platform/cf/SharedBufferCF.cpp:
    (WebCore::SharedBuffer::platformData): Use reinterpret_cast instead of C-style cast.
    (WebCore::SharedBuffer::maybeTransferPlatformData): Use reinterpret_cast instead of C-style cast.
    (WebCore::SharedBuffer::singleDataArrayBuffer): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/SharedBuffer.h
+++ third_party/WebKit/Source/WebCore/platform/SharedBuffer.h
@@ -117,6 +117,8 @@ public:
 
     void reportMemoryUsage(MemoryObjectInfo*) const;
 
+    void createPurgeableBuffer() const;
+
 private:
     SharedBuffer();
     explicit SharedBuffer(size_t);
@@ -137,11 +139,12 @@ private:
     unsigned m_size;
     mutable Vector<char> m_buffer;
     mutable Vector<char*> m_segments;
-    OwnPtr<PurgeableBuffer> m_purgeableBuffer;
+    mutable OwnPtr<PurgeableBuffer> m_purgeableBuffer;
 #if USE(NETWORK_CFDATA_ARRAY_CALLBACK)
     mutable Vector<RetainPtr<CFDataRef> > m_dataArray;
     void copyDataArrayAndClear(char *destination, unsigned bytesToCopy) const;
     unsigned copySomeDataFromDataArray(const char*& someData, unsigned position) const;
+    const char *singleDataArrayBuffer() const;
 #endif
 #if USE(CF)
     explicit SharedBuffer(CFDataRef);
