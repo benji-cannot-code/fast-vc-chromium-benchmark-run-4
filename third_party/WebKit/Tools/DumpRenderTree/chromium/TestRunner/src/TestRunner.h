3b212a6873ef2d7a4582d7223519f432a0d30910
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 23:05:33 +0000

    [chromium] move stubs from DRTTestRunner to TestRunner https://bugs.webkit.org/show_bug.cgi?id=102489
    Reviewed by Adam Barth.
    
    This is part of moving all methods from DRTTestRunner to TestRunner.
    Instead of keeping one stub per method, I'm using a shared stub to
    clean up the code.
    
    * DumpRenderTree/chromium/DRTTestRunner.cpp:
    (DRTTestRunner::DRTTestRunner):
    * DumpRenderTree/chromium/DRTTestRunner.h:
    (DRTTestRunner):
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp:
    (WebTestRunner::TestRunner::TestRunner):
    (WebTestRunner):
    (WebTestRunner::TestRunner::fallbackMethod):
    (WebTestRunner::TestRunner::notImplemented):
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.h:
    (WebTestRunner):
    (TestRunner):
    (WebTestRunner::TestRunner::setDelegate):
    * DumpRenderTree/chromium/TestShell.cpp:
    (TestShell::createMainWindow):
    (TestShell::~TestShell):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135008 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.h
@@ -37,9 +37,28 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebTestRunner {
 
+class WebTestDelegate;
+
 class TestRunner : public CppBoundClass {
 public:
     TestRunner();
+
+    // FIXME: once DRTTestRunner is moved entirely to this class, change this
+    // method to take a TestDelegate* instead.
+    void setDelegate(WebTestDelegate* delegate) { m_delegate = delegate; }
+
+private:
+    // The fallback method is called when a nonexistent method is called on
+    // the layout test controller object.
+    // It is usefull to catch typos in the JavaScript code (a few layout tests
+    // do have typos in them) and it allows the script to continue running in
+    // that case (as the Mac does).
+    void fallbackMethod(const CppArgumentList&, CppVariant*);
+
+    // Stub for not implemented methods.
+    void notImplemented(const CppArgumentList&, CppVariant*);
+
+    WebTestDelegate* m_delegate;
 };
 
 }
