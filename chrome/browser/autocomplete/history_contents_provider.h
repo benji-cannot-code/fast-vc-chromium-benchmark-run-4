71d6f482e05084c4bf7ade339b8dd1a7b71182c9
mrossetti@chromium.org <mrossetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 00:16:56 +0000

    Remove old bookmark searching from HistoryContentProvider.
    BUG=None
    TEST=Updated
    Review URL: https://codereview.chromium.org/11293288
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168092 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_contents_provider.h
+++ chrome/browser/autocomplete/history_contents_provider.h
@@ -12,10 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/history_provider.h"
 #include "chrome/browser/history/history.h"
 
-namespace bookmark_utils {
-struct TitleMatch;
-}
-
 // HistoryContentsProvider is an AutocompleteProvider that provides results from
 // the contents (body and/or title) of previously visited pages.
 // HistoryContentsProvider gets results from two sources:
@@ -72,14 +68,6 @@ class HistoryContentsProvider : public HistoryProvider {
   // Calculates and returns the relevance of a particular result. See the
   // chart in autocomplete.h for the list of values this returns.
   int CalculateRelevance(const history::URLResult& result);
-
-  // Queries the bookmarks for any bookmarks whose title matches input. All
-  // matches are added directly to results_.
-  void QueryBookmarks(const AutocompleteInput& input);
-
-  // Converts a BookmarkModel::TitleMatch to a QueryResult and adds it to
-  // results_.
-  void AddBookmarkTitleMatchToResults(const bookmark_utils::TitleMatch& match);
 
   // Return true if the search term can be found in the title of |result|.
   bool MatchInTitle(const history::URLResult& result);
