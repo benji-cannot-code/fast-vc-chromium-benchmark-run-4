9dd2f4642e6fd099435d1cefb69d17f3f997d622
skaslev@chromium.org <skaslev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 07:36:20 +0000

    Switched cc::Resource and cc::ScopedResource to Chrome coding style.
    BUG=144576,144577
    
    Review URL: https://chromiumcodereview.appspot.com/11412022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168152 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scoped_resource_unittest.cc
+++ cc/scoped_resource_unittest.cc
@@ -35,7 +35,7 @@ TEST(ScopedResourceTest, CreateScopedResource)
     scoped_ptr<GraphicsContext> context(createFakeGraphicsContext());
     scoped_ptr<ResourceProvider> resourceProvider(ResourceProvider::create(context.get()));
     scoped_ptr<ScopedResource> texture = ScopedResource::create(resourceProvider.get());
-    texture->allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
+    texture->Allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
 
     // The texture has an allocated byte-size now.
     size_t expectedBytes = 30 * 30 * 4;
@@ -55,7 +55,7 @@ TEST(ScopedResourceTest, ScopedResourceIsDeleted)
         scoped_ptr<ScopedResource> texture = ScopedResource::create(resourceProvider.get());
 
         EXPECT_EQ(0u, resourceProvider->numResources());
-        texture->allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
+        texture->Allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
         EXPECT_LT(0u, texture->id());
         EXPECT_EQ(1u, resourceProvider->numResources());
     }
@@ -65,10 +65,10 @@ TEST(ScopedResourceTest, ScopedResourceIsDeleted)
     {
         scoped_ptr<ScopedResource> texture = ScopedResource::create(resourceProvider.get());
         EXPECT_EQ(0u, resourceProvider->numResources());
-        texture->allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
+        texture->Allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
         EXPECT_LT(0u, texture->id());
         EXPECT_EQ(1u, resourceProvider->numResources());
-        texture->free();
+        texture->Free();
         EXPECT_EQ(0u, resourceProvider->numResources());
     }
 }
@@ -82,15 +82,15 @@ TEST(ScopedResourceTest, LeakScopedResource)
         scoped_ptr<ScopedResource> texture = ScopedResource::create(resourceProvider.get());
 
         EXPECT_EQ(0u, resourceProvider->numResources());
-        texture->allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
+        texture->Allocate(Renderer::ImplPool, gfx::Size(30, 30), GL_RGBA, ResourceProvider::TextureUsageAny);
         EXPECT_LT(0u, texture->id());
         EXPECT_EQ(1u, resourceProvider->numResources());
 
-        texture->leak();
+        texture->Leak();
         EXPECT_EQ(0u, texture->id());
         EXPECT_EQ(1u, resourceProvider->numResources());
 
-        texture->free();
+        texture->Free();
         EXPECT_EQ(0u, texture->id());
         EXPECT_EQ(1u, resourceProvider->numResources());
     }
