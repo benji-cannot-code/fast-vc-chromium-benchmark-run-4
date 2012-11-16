ea780b3eaa6d09c96556d505ce49e68a855f21ad
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 01:58:58 +0000

    content: Add stub implementation for overscroll gesture.
    The way this is going to work is as follows:
     * The OverscrollController receives events as they are sent to the
       renderer and, the ACKs for the interesting events (i.e. gesture, wheel).
     * It decides whether to initiate an overscroll gesture, and notifies the
       OverscrollControllerDelegate appropriately for each overscroll state update.
     * The delegate takes care of updating the display during this overscroll
       gesture (and resetting the display when it is cancelled).
     * The controller recognizes whether the overscroll gesture was complete or not
       based on some heuristic on the amount of scroll. The delegate decides what
       to do with the overscroll gesture.
    
    This CL implementes these steps (with relevant tests). But it creates the controller
    only for the tests. Subsequent CLs are going to:
     * Turn RenderWidgetHostViewAura into a delegate for overscroll-controller so
       that it receives the updates for overscroll gesture.
     * Add a flag that can be used to turn on the overscroll controller.
     * Update display from RenderWidgetHostViewAura (or move the display to another
       layer)
    
    BUG=160668
    
    Review URL: https://codereview.chromium.org/11293238
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168102 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.h
+++ content/browser/renderer_host/render_widget_host_impl.h
@@ -60,6 +60,7 @@ namespace content {
 class BackingStore;
 class GestureEventFilter;
 class MockRenderWidgetHost;
+class OverscrollController;
 class RenderWidgetHostDelegate;
 class RenderWidgetHostViewPort;
 class SmoothScrollGesture;
@@ -445,6 +446,10 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   // Update the renderer's cache of the screen rect of the view and window.
   void SendScreenRects();
 
+  GestureEventFilter* gesture_event_filter() {
+    return gesture_event_filter_.get();
+  }
+
  protected:
   virtual RenderWidgetHostImpl* AsRenderWidgetHostImpl() OVERRIDE;
 
@@ -508,6 +513,12 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
   // RenderViewHostImpl can account for in-flight beforeunload/unload events.
   int increment_in_flight_event_count() { return ++in_flight_event_count_; }
   int decrement_in_flight_event_count() { return --in_flight_event_count_; }
+
+  // Creates and initializes the overscroll controller.
+  void InitializeOverscrollController();
+
+  // Returns whether an overscroll gesture is in progress.
+  bool IsInOverscrollGesture() const;
 
   void GetWebScreenInfo(WebKit::WebScreenInfo* result);
 
@@ -861,6 +872,7 @@ class CONTENT_EXPORT RenderWidgetHostImpl : virtual public RenderWidgetHost,
 
   scoped_ptr<TouchEventQueue> touch_event_queue_;
   scoped_ptr<GestureEventFilter> gesture_event_filter_;
+  scoped_ptr<OverscrollController> overscroll_controller_;
 
 #if defined(OS_WIN)
   std::list<HWND> dummy_windows_for_activation_;
