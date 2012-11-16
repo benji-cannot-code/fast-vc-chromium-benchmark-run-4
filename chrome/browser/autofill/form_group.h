996602258fb09e19c62ce40d54efe0aa1f6d666d
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 19:20:39 +0000

    Revert 168247 - more autofill refactoring
    makes AutofillProfile and CreditCard better resemble one another. Much code deleted.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/11312259
    
    TBR=estade@chromium.org
    
    Failed some Autofill related sync tests:
    http://build.chromium.org/p/chromium.mac/builders/Mac10.6%20Sync/builds/24913
    Review URL: https://codereview.chromium.org/11413043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168259 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/form_group.h
+++ chrome/browser/autofill/form_group.h
@@ -6,14 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_AUTOFILL_FORM_GROUP_H_
 #define CHROME_BROWSER_AUTOFILL_FORM_GROUP_H_
 
-#include <string>
 #include <vector>
 
 #include "base/string16.h"
 #include "base/string_util.h"
 #include "chrome/browser/autofill/field_types.h"
 
-class AutofillField;
 struct FormFieldData;
 
 // This class is an interface for collections of form fields, grouped by type.
@@ -22,10 +20,6 @@ struct FormFieldData;
 class FormGroup {
  public:
   virtual ~FormGroup() {}
-
-  // Returns a globally unique ID for this object. It is an error to call the
-  // default implementation.
-  virtual std::string GetGUID() const;
 
   // Used to determine the type of a field based on the text that a user enters
   // into the field.  The field types can then be reported back to the server.
@@ -55,12 +49,6 @@ class FormGroup {
   // prior to storing, if appropriate.
   virtual bool SetCanonicalizedInfo(AutofillFieldType type,
                                     const string16& value);
-
-  // Set |field_data|'s value based on |field| and contents of |this| (using
-  // data variant |variant|). It is an error to call the default implementation.
-  virtual void FillFormField(const AutofillField& field,
-                             size_t variant,
-                             FormFieldData* field_data) const;
 
   // Fills in select control with data matching |type| from |this|.
   // Public for testing purposes.
