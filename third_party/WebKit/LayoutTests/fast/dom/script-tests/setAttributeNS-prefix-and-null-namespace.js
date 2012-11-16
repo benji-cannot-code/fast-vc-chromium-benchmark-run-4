28a64cdede3c1005b4f2f7e741242e79d0a4afd5
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 03:29:10 +0000

    Update DOMException name: NamespaceError https://bugs.webkit.org/show_bug.cgi?id=102395
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    Patch 14 of 25 to update DOMException name to match the spec and Firefox.
    
    Updated existing tests.
    
    * dom/DOMCoreException.cpp:
    
    LayoutTests:
    
    Updated tests and expectations.
    
    * fast/dom/EntityReference/readonly-exceptions-expected.txt:
    * fast/dom/SelectorAPI/not-supported-namespace-in-selector-expected.txt:
    * fast/dom/node-prefix-setter-namespace-exception-expected.txt:
    * fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js:
    * fast/dom/setAttributeNS-prefix-and-null-namespace-expected.txt:
    * fast/xpath/attr-namespace-expected.txt:
    * fast/xpath/py-dom-xpath/nodetests-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134879 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js
+++ third_party/WebKit/LayoutTests/fast/dom/script-tests/setAttributeNS-prefix-and-null-namespace.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("Test that calling setAttributeNS() with a prefixed qualifiedName and null NS throws NAMESPACE_ERR.");
 
-shouldThrow("document.createElement('test').setAttributeNS(null, 'foo:bar', 'baz')", "'Error: NAMESPACE_ERR: DOM Exception 14'");
+shouldThrow("document.createElement('test').setAttributeNS(null, 'foo:bar', 'baz')", "'Error: NamespaceError: DOM Exception 14'");
