1cfb1d78febeb222cb26a43f77bea9cd53f84c94
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 07:30:01 +0000

    Memory instrumentation: add code for reporting stack traces of unknown instrumented objects https://bugs.webkit.org/show_bug.cgi?id=102384
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    * inspector/InspectorMemoryAgent.cpp:
    (WebCore::MemoryInstrumentationClientImpl::checkCountedObject): return false
    if the check fails.
    * inspector/MemoryInstrumentationImpl.h:
    (MemoryInstrumentationClientImpl):
    
    Source/WTF:
    
    Added an option to collect stack traces for instrumented pointers so that they
    can be printed in case the check failed for the pointer. This code is hidden
    behind a define.
    
    * wtf/MemoryInstrumentation.h:
    (MemoryInstrumentationClient):
    (WTF::MemoryInstrumentation::checkCountedObject): the method now returns false
    in case the check has failed.
    (InstrumentedPointer):
    (WTF::::InstrumentedPointer):
    (WTF):
    (WTF::::process):
    
    Tools:
    
    Updated return type in accord with the changes in MemoryInstrumentationClient.
    
    * TestWebKitAPI/Tests/WTF/MemoryInstrumentationTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134906 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/MemoryInstrumentationImpl.h
+++ third_party/WebKit/Source/WebCore/inspector/MemoryInstrumentationImpl.h
@@ -81,7 +81,7 @@ public:
 
     virtual void countObjectSize(const void*, MemoryObjectType, size_t) OVERRIDE;
     virtual bool visited(const void*) OVERRIDE;
-    virtual void checkCountedObject(const void*) OVERRIDE;
+    virtual bool checkCountedObject(const void*) OVERRIDE;
 
     void reportMemoryUsage(MemoryObjectInfo*) const;
 
