fcf31342d64b22fba48ef79d3bf776f76fc6365b
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 02:02:31 +0000

    Reduce high latency input buffers to 1.
    Multiple buffers lead to trampling input data which is in the process
    of being transmitted across the shared memory.
    
    Since this API was never intended for low latency usage, clients should
    instead use a larger buffer size. At least 2048 @ 44.1kHz (Flash) and
    1600 at 16kHz (speech recognizer).
    
    vtl will land a fix to flash along side this change to move it off of the
    RecommendSampleCount() method to a hard coded 2048 frame buffer.
    
    NOTRY=true
    BUG=157613
    TEST=input is clear and crisp w/ flash @ 2048 buffer.
    
    Review URL: https://codereview.chromium.org/11418017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168106 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_input_mac.h
+++ media/audio/mac/audio_input_mac.h
@@ -63,7 +63,7 @@ class PCMQueueInAudioInputStream : public AudioInputStream {
                          UInt32 num_packets,
                          const AudioStreamPacketDescription* packet_desc);
 
-  static const int kNumberBuffers = 3;
+  static const int kNumberBuffers = 1;
 
   // Manager that owns this stream, used for closing down.
   AudioManagerBase* manager_;
