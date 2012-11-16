aed665aa556d1f5d109cbc78800e0a8019db0808
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 07:56:19 +0000

    Revert "Revert 167613 - Cache more GL state both service and client side."
    This reverts commit 14efc6d257c73271ae0b70d2e71571b335827570.
    
    The issue appears to be that glBlitFramebufferANGLE fails if GL_SCISSOR_TEST
    is enabled and the caching code ends up enabling it places it didn't use to
    be. That doesn't explain issues on non-Windows machines though. It disable
    before glBlitFramebufferANGLE and glBlitFramebufferEXT on all machines
    at the moment.
    
    I tried to get the code to fail on Linux NVidia both debug and release
    and Mac Retina both debug and release and using integrated and discrete
    but no luck.
    
    AFAICT there's no try bots to test Linux Intel or Mac Intel so if
    this still causes tests to fail feel free to revert it
    
    TBR=apatrick@chromium.org,brettw@chromium.org
    R=shawnsingh@chromium.org
    BUG=160370
    
    
    Review URL: https://chromiumcodereview.appspot.com/11415003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168165 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_implementation_unittest_autogen.h
+++ gpu/command_buffer/client/gles2_implementation_unittest_autogen.h
@@ -34,6 +34,9 @@ TEST_F(GLES2ImplementationTest, BindBuffer) {
 
   gl_->BindBuffer(GL_ARRAY_BUFFER, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
+  ClearCommands();
+  gl_->BindBuffer(GL_ARRAY_BUFFER, 2);
+  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BindFramebuffer) {
@@ -45,6 +48,9 @@ TEST_F(GLES2ImplementationTest, BindFramebuffer) {
 
   gl_->BindFramebuffer(GL_FRAMEBUFFER, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
+  ClearCommands();
+  gl_->BindFramebuffer(GL_FRAMEBUFFER, 2);
+  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BindRenderbuffer) {
@@ -56,6 +62,9 @@ TEST_F(GLES2ImplementationTest, BindRenderbuffer) {
 
   gl_->BindRenderbuffer(GL_RENDERBUFFER, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
+  ClearCommands();
+  gl_->BindRenderbuffer(GL_RENDERBUFFER, 2);
+  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BindTexture) {
@@ -67,6 +76,9 @@ TEST_F(GLES2ImplementationTest, BindTexture) {
 
   gl_->BindTexture(GL_TEXTURE_2D, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
+  ClearCommands();
+  gl_->BindTexture(GL_TEXTURE_2D, 2);
+  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BlendColor) {
@@ -85,9 +97,9 @@ TEST_F(GLES2ImplementationTest, BlendEquation) {
     BlendEquation cmd;
   };
   Cmds expected;
-  expected.cmd.Init(GL_FUNC_ADD);
+  expected.cmd.Init(GL_FUNC_SUBTRACT);
 
-  gl_->BlendEquation(GL_FUNC_ADD);
+  gl_->BlendEquation(GL_FUNC_SUBTRACT);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
 }
 
@@ -96,9 +108,9 @@ TEST_F(GLES2ImplementationTest, BlendEquationSeparate) {
     BlendEquationSeparate cmd;
   };
   Cmds expected;
-  expected.cmd.Init(GL_FUNC_ADD, GL_FUNC_ADD);
+  expected.cmd.Init(GL_FUNC_SUBTRACT, GL_FUNC_ADD);
 
-  gl_->BlendEquationSeparate(GL_FUNC_ADD, GL_FUNC_ADD);
+  gl_->BlendEquationSeparate(GL_FUNC_SUBTRACT, GL_FUNC_ADD);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
 }
 
@@ -367,17 +379,6 @@ TEST_F(GLES2ImplementationTest, DetachShader) {
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
 }
 
-TEST_F(GLES2ImplementationTest, Disable) {
-  struct Cmds {
-    Disable cmd;
-  };
-  Cmds expected;
-  expected.cmd.Init(GL_BLEND);
-
-  gl_->Disable(GL_BLEND);
-  EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-}
-
 TEST_F(GLES2ImplementationTest, DisableVertexAttribArray) {
   struct Cmds {
     DisableVertexAttribArray cmd;
@@ -397,17 +398,6 @@ TEST_F(GLES2ImplementationTest, DrawArrays) {
   expected.cmd.Init(GL_POINTS, 2, 3);
 
   gl_->DrawArrays(GL_POINTS, 2, 3);
-  EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-}
-
-TEST_F(GLES2ImplementationTest, Enable) {
-  struct Cmds {
-    Enable cmd;
-  };
-  Cmds expected;
-  expected.cmd.Init(GL_BLEND);
-
-  gl_->Enable(GL_BLEND);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
 }
 
@@ -1051,9 +1041,9 @@ TEST_F(GLES2ImplementationTest, StencilOp) {
     StencilOp cmd;
   };
   Cmds expected;
-  expected.cmd.Init(GL_KEEP, GL_KEEP, GL_KEEP);
+  expected.cmd.Init(GL_KEEP, GL_INCR, GL_KEEP);
 
-  gl_->StencilOp(GL_KEEP, GL_KEEP, GL_KEEP);
+  gl_->StencilOp(GL_KEEP, GL_INCR, GL_KEEP);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
 }
 
@@ -1062,9 +1052,9 @@ TEST_F(GLES2ImplementationTest, StencilOpSeparate) {
     StencilOpSeparate cmd;
   };
   Cmds expected;
-  expected.cmd.Init(GL_FRONT, GL_KEEP, GL_KEEP, GL_KEEP);
+  expected.cmd.Init(GL_FRONT, GL_INCR, GL_KEEP, GL_KEEP);
 
-  gl_->StencilOpSeparate(GL_FRONT, GL_KEEP, GL_KEEP, GL_KEEP);
+  gl_->StencilOpSeparate(GL_FRONT, GL_INCR, GL_KEEP, GL_KEEP);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
 }
 
@@ -1668,6 +1658,9 @@ TEST_F(GLES2ImplementationTest, BindVertexArrayOES) {
 
   gl_->BindVertexArrayOES(1);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
+  ClearCommands();
+  gl_->BindVertexArrayOES(1);
+  EXPECT_TRUE(NoCommandsWritten());
 }
 // TODO: Implement unit test for GenSharedIdsCHROMIUM
 // TODO: Implement unit test for DeleteSharedIdsCHROMIUM
