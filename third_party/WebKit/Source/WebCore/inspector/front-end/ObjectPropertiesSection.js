17b0dcd6de99bba8473b6e833b2717e0cda0e1de
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 09:06:31 +0000

    Web Inspector: show internal properties in inspector frontend https://bugs.webkit.org/show_bug.cgi?id=100021
    Patch by Peter Rybin <prybin@chromium.org> on 2012-11-16
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    New field 'internalProperties' is parsed and passed via all callbacks to Object Properties section.
    
    Test: inspector/debugger/properties-special.html
    
    * inspector/front-end/ObjectPropertiesSection.js:
    (WebInspector.ObjectPropertiesSection.prototype.update.callback):
    (WebInspector.ObjectPropertiesSection.prototype.update):
    (.callback):
    (WebInspector.ObjectPropertyTreeElement.populate):
    (.processProperties):
    (WebInspector.ArrayGroupingTreeElement._populateAsFragment):
    (WebInspector.ArrayGroupingTreeElement._populateNonIndexProperties):
    * inspector/front-end/RemoteObject.js:
    (WebInspector.RemoteObject.prototype.set else):
    
    LayoutTests:
    
    A new test is added. InspectorTest.dumpObjectPropertySectionDeep function is added.
    
    * http/tests/inspector/elements-test.js:
    (initialize_ElementTest.InspectorTest.dumpObjectPropertySectionDeep):
    (initialize_ElementTest.InspectorTest.dumpObjectPropertySectionDeep.dumpTreeElement):
    * inspector/debugger/properties-special-expected.txt: Added.
    * inspector/debugger/properties-special.html: Added.
    * platform/chromium/inspector/debugger/properties-special-expected.txt: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ObjectPropertiesSection.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ObjectPropertiesSection.js
@@ -77,7 +77,7 @@ WebInspector.ObjectPropertiesSection.prototype = {
             return;
         }
 
-        function callback(properties)
+        function callback(properties, internalProperties)
         {
             if (!properties)
                 return;
@@ -429,7 +429,11 @@ WebInspector.ObjectPropertyTreeElement.populate = function(treeElement, value) {
         return;
     }
 
-    function callback(properties)
+    /**
+     * @param {Array.<WebInspector.RemoteObjectProperty>} properties
+     * @param {Array.<WebInspector.RemoteObjectProperty>=} internalProperties
+     */
+    function callback(properties, internalProperties)
     {
         treeElement.removeChildren();
         if (!properties)
@@ -442,8 +446,29 @@ WebInspector.ObjectPropertyTreeElement.populate = function(treeElement, value) {
             properties[i].parentObject = value;
             treeElement.appendChild(new treeElement.treeOutline.section.treeElementConstructor(properties[i]));
         }
-        if (value.type === "function")
-            treeElement.appendChild(new WebInspector.FunctionScopeMainTreeElement(value));
+        if (value.type === "function") {
+            // Whether function has TargetFunction internal property.
+            // This is a simple way to tell that the function is actually a bound function (we are not told).
+            // Bound function never has inner scope and doesn't need corresponding UI node.   
+            var hasTargetFunction = false;
+
+            if (internalProperties) {
+                for (var i = 0; i < internalProperties.length; i++) {
+                    if (internalProperties[i].name == "[[TargetFunction]]") {
+                        hasTargetFunction = true;
+                        break;
+                    }
+                }
+            }
+            if (!hasTargetFunction)
+                treeElement.appendChild(new WebInspector.FunctionScopeMainTreeElement(value));
+        }
+        if (internalProperties) {
+            for (var i = 0; i < internalProperties.length; i++) {
+                internalProperties[i].parentObject = value;
+                treeElement.appendChild(new treeElement.treeOutline.section.treeElementConstructor(internalProperties[i]));
+            } 
+        }
     }
 
     value.getOwnProperties(callback);
@@ -689,7 +714,7 @@ WebInspector.ArrayGroupingTreeElement._populateAsFragment = function(treeElement
     }
 
     /** @this {WebInspector.ArrayGroupingTreeElement} */
-    function processProperties(properties)
+    function processProperties(properties, internalProperties)
     {
         if (!properties)
             return;
@@ -734,7 +759,7 @@ WebInspector.ArrayGroupingTreeElement._populateNonIndexProperties = function(tre
     }
 
     /** @this {WebInspector.ArrayGroupingTreeElement} */
-    function processProperties(properties)
+    function processProperties(properties, internalProperties)
     {
         if (!properties)
             return;
--- third_party/WebKit/Source/WebCore/inspector/front-end/RemoteObject.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/RemoteObject.js
@@ -100,7 +100,7 @@ WebInspector.RemoteObject.resolveNode = function(node, objectGroup, callback)
 }
 
 /**
- * @param {RuntimeAgent.RemoteObject} payload
+ * @param {RuntimeAgent.RemoteObject=} payload
  * @return {WebInspector.RemoteObject}
  */
 WebInspector.RemoteObject.fromPayload = function(payload)
@@ -164,7 +164,7 @@ WebInspector.RemoteObject.prototype = {
     },
 
     /**
-     * @param {function(Array.<WebInspector.RemoteObjectProperty>)} callback
+     * @param {function(Array.<WebInspector.RemoteObjectProperty>, Array.<WebInspector.RemoteObjectProperty>=)} callback
      */
     getOwnProperties: function(callback)
     {
@@ -172,7 +172,7 @@ WebInspector.RemoteObject.prototype = {
     },
 
     /**
-     * @param {function(Array.<WebInspector.RemoteObjectProperty>)} callback
+     * @param {function(Array.<WebInspector.RemoteObjectProperty>, Array.<WebInspector.RemoteObjectProperty>=)} callback
      */
     getAllProperties: function(callback)
     {
@@ -181,7 +181,7 @@ WebInspector.RemoteObject.prototype = {
 
     /**
      * @param {boolean} ownProperties
-     * @param {function(Array.<RuntimeAgent.RemoteObject>)} callback
+     * @param {function(Array.<WebInspector.RemoteObjectProperty>, Array.<WebInspector.RemoteObjectProperty>=)} callback
      */
     _getProperties: function(ownProperties, callback)
     {
@@ -192,9 +192,10 @@ WebInspector.RemoteObject.prototype = {
 
         /**
          * @param {?Protocol.Error} error
-         * @param {Array.<WebInspector.RemoteObjectProperty>} properties
+         * @param {Array.<RuntimeAgent.PropertyDescriptor>} properties
+         * @param {Array.<RuntimeAgent.InternalPropertyDescriptor>=} internalProperties
          */
-        function remoteObjectBinder(error, properties)
+        function remoteObjectBinder(error, properties, internalProperties)
         {
             if (error) {
                 callback(null);
@@ -211,7 +212,15 @@ WebInspector.RemoteObject.prototype = {
                 } else
                     result.push(new WebInspector.RemoteObjectProperty(property.name, WebInspector.RemoteObject.fromPayload(property.value), property));
             }
-            callback(result);
+            var internalPropertiesResult;
+            if (internalProperties) {
+                internalPropertiesResult = [];
+                for (var i = 0; i < internalProperties.length; i++) {
+                    var property = internalProperties[i];
+                    internalPropertiesResult.push(new WebInspector.RemoteObjectProperty(property.name, WebInspector.RemoteObject.fromPayload(property.value)));
+                }
+            }
+            callback(result, internalPropertiesResult);
         }
         RuntimeAgent.getProperties(this._objectId, ownProperties, remoteObjectBinder);
     },
