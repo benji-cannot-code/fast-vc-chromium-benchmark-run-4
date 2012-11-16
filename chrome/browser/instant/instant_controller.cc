3efaed9fa44526f70fff63e16f1964f6c5cdd0d8
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-16 04:17:08 +0000

    Remove unused focus/blur API from searchbox (Instant).
    BUG=80118
    R=jered@chromium.org,jschuh@chromium.org
    TEST=none; no change in functionality.
    
    Review URL: https://codereview.chromium.org/11415013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168131 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.cc
+++ chrome/browser/instant/instant_controller.cc
@@ -463,8 +463,6 @@ void InstantController::OnAutocompleteLostFocus(
   if (!GetPreviewContents())
     return;
 
-  loader_->OnAutocompleteLostFocus();
-
   // It's bizarre if custom NTP content disappears when the user focuses
   // outside the omnibox.
   if (model_.preview_state() == InstantModel::CUSTOM_NTP_CONTENT)
@@ -547,8 +545,6 @@ void InstantController::OnAutocompleteLostFocus(
 
 void InstantController::OnAutocompleteGotFocus() {
   is_omnibox_focused_ = true;
-  if (GetPreviewContents())
-    loader_->OnAutocompleteGotFocus();
   CreateDefaultLoader();
 }
 
@@ -697,11 +693,7 @@ void InstantController::ResetLoader(const std::string& instant_url,
     loader_.reset(new InstantLoader(this, instant_url, active_tab));
     loader_->Init();
 
-    // Ensure the searchbox API has the correct focus state and context.
-    if (is_omnibox_focused_)
-      loader_->OnAutocompleteGotFocus();
-    else
-      loader_->OnAutocompleteLostFocus();
+    // Ensure the searchbox API has the correct context.
     loader_->OnActiveTabModeChanged(active_tab_mode_.is_ntp());
 
     // Reset the loader timer.
--- chrome/renderer/resources/extensions/searchbox/searchbox_api.js
+++ chrome/renderer/resources/extensions/searchbox/searchbox_api.js
@@ -30,7 +30,6 @@ if (!chrome.searchBox) {
     native function GetWidth();
     native function GetHeight();
     native function GetAutocompleteResults();
-    native function GetIsFocused();
     native function GetContext();
     native function NavigateContentWindow();
     native function SetSuggestions();
@@ -176,7 +175,6 @@ if (!chrome.searchBox) {
     this.__defineGetter__('width', GetWidth);
     this.__defineGetter__('height', GetHeight);
     this.__defineGetter__('nativeSuggestions', GetAutocompleteResultsWrapper);
-    this.__defineGetter__('isFocused', GetIsFocused);
     this.__defineGetter__('context', GetContext);
     this.setSuggestions = function(text) {
       SetSuggestions(text);
@@ -208,8 +206,6 @@ if (!chrome.searchBox) {
     this.onresize = null;
     this.onautocompleteresults = null;
     this.onkeypress = null;
-    this.onfocus = null;
-    this.onblur = null;
     this.oncontextchange = null;
   };
 }
