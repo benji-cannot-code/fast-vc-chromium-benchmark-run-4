120a2823e5c1f79ce16e61a414f81cdaf1073fc5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 20:56:30 +0000

    IndexedDB: Propagate DOMRequestState to IndexedDB binding utility functions https://bugs.webkit.org/show_bug.cgi?id=102430
    Patch by Michael Pruett <michael@68k.org> on 2012-11-16
    Reviewed by Adam Barth.
    
    DOMRequestState is currently propagated to some but not all of
    the IndexedDB binding utility functions. In order to implement
    these functions for JSC, this state must be propagated to all
    of the utility functions.
    
    Source/WebCore:
    
    Tests: storage/indexeddb/*
    
    * Modules/indexeddb/IDBCursor.cpp:
    (WebCore::IDBCursor::update):
    (WebCore::IDBCursor::setValueReady):
    * Modules/indexeddb/IDBObjectStore.cpp:
    (WebCore::generateIndexKeysForValue):
    (WebCore::IDBObjectStore::put):
    (WebCore):
    * Modules/indexeddb/IDBRequest.cpp:
    (WebCore::IDBRequest::onSuccess):
    (WebCore::IDBRequest::dispatchEvent):
    * Modules/indexeddb/IDBRequest.h:
    (WebCore::IDBRequest::requestState):
    (IDBRequest):
    * bindings/v8/IDBBindingUtilities.cpp:
    (WebCore::createIDBKeyFromScriptValueAndKeyPath):
    (WebCore::serializeIDBValue):
    (WebCore::injectIDBKeyIntoScriptValue):
    * bindings/v8/IDBBindingUtilities.h:
    (WebCore):
    
    Source/WebKit/chromium:
    
    * tests/IDBBindingUtilitiesTest.cpp:
    (WebKit::checkKeyFromValueAndKeyPathInternal):
    (WebKit::injectKey):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134989 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBRequest.cpp
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBRequest.cpp
@@ -287,7 +287,7 @@ void IDBRequest::onSuccess(PassRefPtr<IDBCursorBackendInterface> backend, PassRe
         return;
 
     DOMRequestState::Scope scope(m_requestState);
-    ScriptValue value = deserializeIDBValue(&m_requestState, serializedValue);
+    ScriptValue value = deserializeIDBValue(requestState(), serializedValue);
     ASSERT(m_cursorType != IDBCursorBackendInterface::InvalidCursorType);
     RefPtr<IDBCursor> cursor;
     if (m_cursorType == IDBCursorBackendInterface::IndexKeyCursor)
@@ -344,7 +344,7 @@ void IDBRequest::onSuccess(PassRefPtr<SerializedScriptValue> serializedScriptVal
         return;
 
     DOMRequestState::Scope scope(m_requestState);
-    ScriptValue value = deserializeIDBValue(&m_requestState, serializedScriptValue);
+    ScriptValue value = deserializeIDBValue(requestState(), serializedScriptValue);
     onSuccessInternal(value);
 }
 
@@ -371,14 +371,14 @@ void IDBRequest::onSuccess(PassRefPtr<SerializedScriptValue> prpSerializedScript
     ASSERT(keyPath == effectiveObjectStore(m_source)->keyPath());
 #endif
     DOMRequestState::Scope scope(m_requestState);
-    ScriptValue value = deserializeIDBValue(&m_requestState, prpSerializedScriptValue);
+    ScriptValue value = deserializeIDBValue(requestState(), prpSerializedScriptValue);
 
     RefPtr<IDBKey> primaryKey = prpPrimaryKey;
 #ifndef NDEBUG
-    RefPtr<IDBKey> expectedKey = createIDBKeyFromScriptValueAndKeyPath(value, keyPath);
+    RefPtr<IDBKey> expectedKey = createIDBKeyFromScriptValueAndKeyPath(requestState(), value, keyPath);
     ASSERT(!expectedKey || expectedKey->isEqual(primaryKey.get()));
 #endif
-    bool injected = injectIDBKeyIntoScriptValue(primaryKey, value, keyPath);
+    bool injected = injectIDBKeyIntoScriptValue(requestState(), primaryKey, value, keyPath);
     ASSERT_UNUSED(injected, injected);
     onSuccessInternal(value);
 }
@@ -410,7 +410,7 @@ void IDBRequest::onSuccess(PassRefPtr<IDBKey> key, PassRefPtr<IDBKey> primaryKey
         return;
 
     DOMRequestState::Scope scope(m_requestState);
-    ScriptValue value = deserializeIDBValue(&m_requestState, serializedValue);
+    ScriptValue value = deserializeIDBValue(requestState(), serializedValue);
     ASSERT(m_pendingCursor);
     setResultCursor(m_pendingCursor.release(), key, primaryKey, value);
     enqueueEvent(createSuccessEvent());
@@ -483,7 +483,7 @@ bool IDBRequest::dispatchEvent(PassRefPtr<Event> event)
     if (event->type() == eventNames().successEvent) {
         cursorToNotify = getResultCursor();
         if (cursorToNotify) {
-            cursorToNotify->setValueReady(&m_requestState, m_cursorKey.release(), m_cursorPrimaryKey.release(), m_cursorValue);
+            cursorToNotify->setValueReady(requestState(), m_cursorKey.release(), m_cursorPrimaryKey.release(), m_cursorValue);
             m_cursorValue.clear();
         }
     }
