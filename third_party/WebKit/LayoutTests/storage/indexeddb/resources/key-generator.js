848846b825af7e5859c4a3e6cde3f2ac78fc09ad
dgrogan@chromium.org <dgrogan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-16 01:14:12 +0000

    IndexedDB: setVersion batch 8 https://bugs.webkit.org/show_bug.cgi?id=102434
    Reviewed by Tony Chang.
    
    2 lines were added to intversion-bad-parameters to capture the tests
    that were in the now-deleted setVersion-{null,undefined}.
    
    * storage/indexeddb/intversion-bad-parameters-expected.txt:
    * storage/indexeddb/key-generator-expected.txt:
    * storage/indexeddb/open-during-transaction-expected.txt:
    * storage/indexeddb/readonly-properties-expected.txt:
    * storage/indexeddb/resources/intversion-bad-parameters.js:
    (deleteSuccess):
    * storage/indexeddb/resources/key-generator.js:
      Again awkward to call indexedDBTest all over the place. The prefix
      line's gotta go, and we could also pull a resetDatabase method out of
      indexedDBTest that would just do a delete and open.
    
    (runTests.nextTest.prepareDatabase):
    (runTests.nextTest.onSuccess.finishTest):
    (runTests.nextTest.onSuccess):
    (runTests.nextTest):
    (testAcrossConnections.prepareDatabase):
    * storage/indexeddb/resources/open-during-transaction.js:
    (prepareDatabase.event.target.transaction.oncomplete):
    (prepareDatabase):
    (tryOpens):
    * storage/indexeddb/resources/readonly-properties.js:
    (prepareDatabase):
    * storage/indexeddb/resources/setVersion-null.js: Removed.
    * storage/indexeddb/resources/setVersion-undefined.js: Removed.
    * storage/indexeddb/resources/transaction-after-close.js:
    (prepareDatabase):
    (firstTransactionComplete):
    (onFinalPutSuccess):
    * storage/indexeddb/resources/versionchangerequest-activedomobject.js:
    (testSetVersion):
    (upgradeNeededCallback):
    (successCallback):
    * storage/indexeddb/setVersion-null-expected.txt: Removed.
    * storage/indexeddb/setVersion-null.html: Removed.
    * storage/indexeddb/setVersion-undefined-expected.txt: Removed.
    * storage/indexeddb/setVersion-undefined.html: Removed.
    * storage/indexeddb/transaction-after-close-expected.txt:
    * storage/indexeddb/versionchangerequest-activedomobject-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@134866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/storage/indexeddb/resources/key-generator.js
+++ third_party/WebKit/LayoutTests/storage/indexeddb/resources/key-generator.js
@@ -36,34 +36,27 @@ function runTests() {
         debug("");
         debug(test.description);
 
-        evalAndLog("request = indexedDB.deleteDatabase('key-generator')");
-        request.onerror = unexpectedErrorCallback;
-        request.onsuccess = function () {
-            evalAndLog("request = indexedDB.open('key-generator')");
-            request.onerror = unexpectedErrorCallback;
-            request.onsuccess = function () {
-                evalAndLog("db = request.result");
-                evalAndLog("request = db.setVersion('1')");
-                request.onerror = unexpectedErrorCallback;
-                request.onsuccess = function () {
-                    evalAndLog("trans = request.result");
-                    trans.onabort = unexpectedAbortCallback;
-                    test.verchange(db, trans);
-                    trans.oncomplete = function () {
+        indexedDBTest(prepareDatabase, onSuccess);
+        function prepareDatabase()
+        {
+            db = event.target.result;
+            trans = event.target.transaction;
+            trans.onabort = unexpectedAbortCallback;
+            test.verchange(db, trans);
+        }
+        function onSuccess() {
+            db = event.target.result;
 
-                        function finishTest() {
-                            evalAndLog("db.close()");
-                            nextTest();
-                        }
+            function finishTest() {
+                evalAndLog("db.close()");
+                nextTest();
+            }
 
-                        if (test.optional) {
-                            test.optional(db, finishTest);
-                        } else {
-                            finishTest();
-                        }
-                    };
-                };
-            };
+            if (test.optional) {
+                test.optional(db, finishTest);
+            } else {
+                finishTest();
+            }
         };
     }
 
@@ -234,22 +227,11 @@ function testAcrossConnections()
 {
     debug("");
     debug("Ensure key generator state is maintained across connections:");
-    request = evalAndLog("indexedDB.deleteDatabase('key-generator')");
-    request.onerror = unexpectedErrorCallback;
-    request.onsuccess = function () {
-        evalAndLog("request = indexedDB.open('key-generator')");
-        request.onerror = unexpectedErrorCallback;
-        request.onsuccess = function () {
-            evalAndLog("db = request.result");
-            request = evalAndLog("db.setVersion('1')");
-            request.onerror = unexpectedErrorCallback;
-            request.onsuccess = function () {
-                trans = request.result;
-                trans.onabort = unexpectedAbortCallback;
-                evalAndLog("db.createObjectStore('store', {autoIncrement: true})");
-                trans.oncomplete = doFirstWrite;
-            };
-        };
+    indexedDBTest(prepareDatabase, doFirstWrite);
+    function prepareDatabase()
+    {
+        db = event.target.result;
+        evalAndLog("db.createObjectStore('store', {autoIncrement: true})");
     };
 
     function doFirstWrite() {
@@ -268,7 +250,7 @@ function testAcrossConnections()
     function closeAndReopen() {
         evalAndLog("db.close()");
         debug("");
-        evalAndLog("request = indexedDB.open('key-generator')");
+        evalAndLog("request = indexedDB.open(dbname)");
         request.onsuccess = function () {
             evalAndLog("db = request.result");
             doSecondWrite();
