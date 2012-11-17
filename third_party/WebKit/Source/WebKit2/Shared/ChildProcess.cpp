696d3d14dadd0aee3625b04a1ec3d4ec7daaa887
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-17 01:34:20 +0000

    Set task priority of WebKit2 processes to zero on Mac https://bugs.webkit.org/show_bug.cgi?id=102567
    Patch by Kiran Muppala <cmuppala@apple.com> on 2012-11-16
    Reviewed by Mark Rowe.
    
    Explicitly set task priority of WebKit2 processes to zero, to
    override automatic priority determination by the system.
    
    * Shared/ChildProcess.cpp:
    (WebKit::ChildProcess::ChildProcess): Add call to platformInitialize() and
    move MAC specific call to disableProcessSuppression to platformInitialize.
    (WebKit::ChildProcess::platformInitialize): Dummy for non MAC platforms.
    * Shared/ChildProcess.h:
    * Shared/mac/ChildProcessMac.mm:
    (WebKit::ChildProcess::platformInitialize): Add call to setpriority().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/ChildProcess.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/ChildProcess.cpp
@@ -67,9 +67,8 @@ ChildProcess::ChildProcess()
 {
     // FIXME: The termination timer should not be scheduled on the main run loop.
     // It won't work with the threaded mode, but it's not really useful anyway as is.
-#if PLATFORM(MAC)
-    disableProcessSuppression(processSuppressionVisibleApplicationReason);
-#endif
+    
+    platformInitialize();
 }
 
 ChildProcess::~ChildProcess()
@@ -105,5 +104,11 @@ void ChildProcess::didCloseOnConnectionWorkQueue(WorkQueue& workQueue, CoreIPC::
 
     workQueue.dispatchAfterDelay(bind(static_cast<void(*)()>(watchdogCallback)), watchdogDelay);
 }
-    
+
+#if !PLATFORM(MAC)
+void ChildProcess::platformInitialize()
+{
+}
+#endif
+
 } // namespace WebKit
