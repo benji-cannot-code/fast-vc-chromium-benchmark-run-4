c68521d62b603105f3c6d7143249305365014a64
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-17 14:36:34 +0000

    Disable find if constrained window is shown
    This CL disables the "find in place" command if a constrained window is shown.
    
    BUG=156969
    
    
    Review URL: https://chromiumcodereview.appspot.com/11293259
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168440 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -1790,8 +1790,6 @@ void Browser::SetTabContentBlocked(content::WebContents* web_contents,
     return;
   }
   tab_strip_model_->SetTabBlocked(index, blocked);
-  command_controller_->PrintingStateChanged();
-  command_controller_->FullscreenStateChanged();
   if (!blocked && tab_strip_model_->GetActiveWebContents() == web_contents)
     web_contents->Focus();
 }
--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -731,6 +731,14 @@ void BrowserCommandController::TabReplacedAt(TabStripModel* tab_strip_model,
   AddInterstitialObservers(new_contents);
 }
 
+void BrowserCommandController::TabBlockedStateChanged(
+    content::WebContents* contents,
+    int index) {
+  PrintingStateChanged();
+  FullscreenStateChanged();
+  UpdateCommandsForFind();
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // BrowserCommandController, TabRestoreServiceObserver implementation:
 
@@ -890,13 +898,6 @@ void BrowserCommandController::InitCommandState() {
 #endif
   command_updater_.UpdateCommandEnabled(IDC_TABPOSE, normal_window);
 
-  // Find-in-page
-  command_updater_.UpdateCommandEnabled(IDC_FIND, !browser_->is_devtools());
-  command_updater_.UpdateCommandEnabled(IDC_FIND_NEXT,
-                                        !browser_->is_devtools());
-  command_updater_.UpdateCommandEnabled(IDC_FIND_PREVIOUS,
-                                        !browser_->is_devtools());
-
   // Show various bits of UI
   command_updater_.UpdateCommandEnabled(IDC_CLEAR_BROWSING_DATA, normal_window);
 
@@ -1005,6 +1006,7 @@ void BrowserCommandController::UpdateCommandsForTabState() {
 
   UpdateCommandsForContentRestrictionState();
   UpdateCommandsForBookmarkEditing();
+  UpdateCommandsForFind();
 }
 
 void BrowserCommandController::UpdateCommandsForContentRestrictionState() {
@@ -1108,8 +1110,8 @@ void BrowserCommandController::UpdateCommandsForFullscreenMode(
 #if defined(OS_MACOSX)
   // The Mac implementation doesn't support switching to fullscreen while
   // a tab modal dialog is displayed.
-  int tabIndex = chrome::IndexOfFirstBlockedTab(browser_->tab_strip_model());
-  bool has_blocked_tab = tabIndex != browser_->tab_strip_model()->count();
+  int tab_index = chrome::IndexOfFirstBlockedTab(browser_->tab_strip_model());
+  bool has_blocked_tab = tab_index != browser_->tab_strip_model()->count();
   fullscreen_enabled &= !has_blocked_tab;
 #endif
 
@@ -1166,6 +1168,16 @@ void BrowserCommandController::UpdateReloadStopState(bool is_loading,
                                                      bool force) {
   window()->UpdateReloadStopState(is_loading, force);
   command_updater_.UpdateCommandEnabled(IDC_STOP, is_loading);
+}
+
+void BrowserCommandController::UpdateCommandsForFind() {
+  TabStripModel* model = browser_->tab_strip_model();
+  bool enabled = !model->IsTabBlocked(model->active_index()) &&
+                 !browser_->is_devtools();
+
+  command_updater_.UpdateCommandEnabled(IDC_FIND, enabled);
+  command_updater_.UpdateCommandEnabled(IDC_FIND_NEXT, enabled);
+  command_updater_.UpdateCommandEnabled(IDC_FIND_PREVIOUS, enabled);
 }
 
 void BrowserCommandController::AddInterstitialObservers(WebContents* contents) {
