536d108611971996b424274bee61574e63eb47dd
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-17 14:11:48 +0000

    Prevent redownloading of already downloaded crx files.
    Now that extension updates might not be installed immediately after downloading
    and unpacking it was possible that ExtensionUpdater/Downloader would download
    the same update again in a new update check. This attempts to fix that by taking
    the version of a pending update into account when determining what updates to
    download.
    
    BUG=161353
    
    
    Review URL: https://chromiumcodereview.appspot.com/11316029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168424 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/updater/extension_downloader_delegate.cc
+++ chrome/browser/extensions/updater/extension_downloader_delegate.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/updater/extension_downloader_delegate.h"
 
+#include "base/logging.h"
+#include "base/version.h"
+
 namespace extensions {
 
 ExtensionDownloaderDelegate::PingResult::PingResult() : did_ping(false) {}
