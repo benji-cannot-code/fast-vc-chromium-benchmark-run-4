ea628291a9217eda10799f9d30261c16538973a2
jorgelo@chromium.org <jorgelo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-17 03:57:56 +0000

    Initialize NSS in the PPAPI process for ClearKey CDM.
    BUG=156864
    TEST=content_browsertests passes on linux_rel_precise.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11411013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168372 0039d316-1c4b-4281-b951-d872f2087c98

--- content/ppapi_plugin/ppapi_plugin_main.cc
+++ content/ppapi_plugin/ppapi_plugin_main.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
 #include "content/public/plugin/content_plugin_client.h"
+#include "crypto/nss_util.h"
 #include "ppapi/proxy/proxy_module.h"
 #include "ui/base/ui_base_switches.h"
 
@@ -80,6 +81,12 @@ int PpapiPluginMain(const MainFunctionParams& parameters) {
 
   MessageLoop main_message_loop;
   base::PlatformThread::SetName("CrPPAPIMain");
+
+#if defined(OS_LINUX) && defined(USE_NSS)
+  // Some out-of-process PPAPI plugins use NSS.
+  // NSS must be initialized before enabling the sandbox below.
+  crypto::WarmUpNSSSafely();
+#endif
 
   // Allow the embedder to perform any necessary per-process initialization
   // before the sandbox is initialized.
