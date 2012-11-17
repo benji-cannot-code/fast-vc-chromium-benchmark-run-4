9f3f98df811a2f2979bdcac2abb64f51cb32aee4
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-17 01:09:57 +0000

    overscroll: Make sure the overscroll gesture interacts properly with touch events.
    Touch events can arrive during an overscroll gesture initiated by
    touch-scrolling. These events should not reset the overscroll state, should
    not be sent to the renderer, and should not be in the touch-event queue awaiting
    an ACK.
    
    BUG=160668
    
    Review URL: https://codereview.chromium.org/11316056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168345 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/overscroll_controller.cc
+++ content/browser/renderer_host/overscroll_controller.cc
@@ -115,7 +115,9 @@ bool OverscrollController::DispatchEventResetsState(
       return false;
 
     default:
-      return true;
+      // Touch events can arrive during an overscroll gesture initiated by
+      // touch-scrolling. These events should not reset the overscroll state.
+      return !WebKit::WebInputEvent::isTouchEventType(event.type);
   }
 }
 
@@ -161,7 +163,9 @@ void OverscrollController::ProcessEventForOverscroll(
     }
 
     default:
-      DCHECK(WebKit::WebInputEvent::isGestureEventType(event.type));
+      DCHECK(WebKit::WebInputEvent::isGestureEventType(event.type) ||
+             WebKit::WebInputEvent::isTouchEventType(event.type))
+          << "Received unexpected event: " << event.type;
   }
 }
 
