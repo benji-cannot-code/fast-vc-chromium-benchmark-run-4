999811de5b3545019edbae012620b20e4982226f
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-18 23:38:26 +0000

    Ensure AudioPropertyListener callbacks happen on the browser thread.
    Per discussion with shess, there may be issues when issuing calls
    which modify OSX's HAL state while it's in the middle of calling us
    back on another thread.
    
    Adding some print statements shows that the property callbacks are
    driven by the NSApplication pump on the base thread (in this case
    that's BrowserMainLoop).  Creating the listeners on the audio thread
    did not cause callbacks to occur on the audio thread.
    
    As such this change simply ensures that our device notification won't
    run until after the NSApplication pump completes its current cycle.
    
    BUG=158170
    TEST=device changes continue to work.
    
    Review URL: https://codereview.chromium.org/11348081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168480 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_manager_mac.h
+++ media/audio/mac/audio_manager_mac.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MEDIA_AUDIO_MAC_AUDIO_MANAGER_MAC_H_
 
 #include "base/basictypes.h"
-#include "base/callback.h"
 #include "base/compiler_specific.h"
+#include "base/message_loop_proxy.h"
 #include "media/audio/audio_manager_base.h"
 
 namespace media {
@@ -36,12 +36,16 @@ class MEDIA_EXPORT AudioManagerMac : public AudioManagerBase {
   virtual AudioInputStream* MakeLowLatencyInputStream(
       const AudioParameters& params, const std::string& device_id) OVERRIDE;
 
+  // Called by an internal device change listener.  Must be called on
+  // |creating_message_loop_|.
+  void OnDeviceChange();
+
  protected:
   virtual ~AudioManagerMac();
 
  private:
-  // Listens for output device changes.
-  base::Closure listener_cb_;
+  bool listener_registered_;
+  scoped_refptr<base::MessageLoopProxy> creating_message_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioManagerMac);
 };
