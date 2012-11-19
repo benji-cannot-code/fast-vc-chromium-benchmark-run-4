f84db55935854578241b29eed46267b493461598
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-19 01:49:52 +0000

    Unreviewed, rolling out r135047. http://trac.webkit.org/changeset/135047 https://bugs.webkit.org/show_bug.cgi?id=102339
    it broke Chromium Windows/Mac build
    
    * WebKit.gyp:
    * WebKit.gypi:
    * public/WebNode.h:
    (WebKit):
    * public/WebPluginContainer.h:
    (WebKit):
    (WebPluginContainer):
    * src/WebFrameImpl.cpp:
    (WebKit::pluginContainerFromNode):
    (WebKit):
    (WebKit::WebFrameImpl::executeCommand):
    (WebKit::WebFrameImpl::printBegin):
    (WebKit::WebFrameImpl::isPrintScalingDisabledForPlugin):
    * src/WebNode.cpp:
    * src/WebPluginContainerImpl.cpp:
    * src/WebPluginContainerImpl.h:
    (WebPluginContainerImpl):
    * tests/FakeWebPlugin.cpp: Removed.
    * tests/FakeWebPlugin.h: Removed.
    * tests/WebPluginContainerTest.cpp: Removed.
    * tests/data/plugin_container.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebFrameImpl.cpp
@@ -131,6 +131,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderObject.h"
 #include "RenderTreeAsText.h"
 #include "RenderView.h"
+#include "RenderWidget.h"
 #include "ResourceHandle.h"
 #include "ResourceRequest.h"
 #include "SchemeRegistry.h"
@@ -302,6 +303,23 @@ static long long generateFrameIdentifier()
 {
     static long long next = 0;
     return ++next;
+}
+
+static WebPluginContainerImpl* pluginContainerFromNode(const WebNode& node)
+{
+    if (node.isNull())
+        return 0;
+
+    const Node* coreNode = node.constUnwrap<Node>();
+    if (coreNode->hasTagName(HTMLNames::objectTag) || coreNode->hasTagName(HTMLNames::embedTag)) {
+        RenderObject* object = coreNode->renderer();
+        if (object && object->isWidget()) {
+            Widget* widget = toRenderWidget(object)->widget();
+            if (widget && widget->isPluginContainer())
+                return static_cast<WebPluginContainerImpl*>(widget);
+        }
+    }
+    return 0;
 }
 
 WebPluginContainerImpl* WebFrameImpl::pluginContainerFromFrame(Frame* frame)
@@ -1215,7 +1233,7 @@ bool WebFrameImpl::executeCommand(const WebString& name, const WebNode& node)
     if (command == "Copy") {
         WebPluginContainerImpl* pluginContainer = pluginContainerFromFrame(frame());
         if (!pluginContainer)
-            pluginContainer = static_cast<WebPluginContainerImpl*>(node.pluginContainer());
+            pluginContainer = pluginContainerFromNode(node);
         if (pluginContainer) {
             pluginContainer->copy();
             return true;
@@ -1411,7 +1429,7 @@ int WebFrameImpl::printBegin(const WebPrintParams& printParams, const WebNode& c
         pluginContainer = pluginContainerFromFrame(frame());
     } else {
         // We only support printing plugin nodes for now.
-        pluginContainer = static_cast<WebPluginContainerImpl*>(constrainToNode.pluginContainer());
+        pluginContainer = pluginContainerFromNode(constrainToNode);
     }
 
     if (pluginContainer && pluginContainer->supportsPaginatedPrint())
@@ -1460,7 +1478,7 @@ void WebFrameImpl::printEnd()
 
 bool WebFrameImpl::isPrintScalingDisabledForPlugin(const WebNode& node)
 {
-    WebPluginContainerImpl* pluginContainer =  node.isNull() ? pluginContainerFromFrame(frame()) : static_cast<WebPluginContainerImpl*>(node.pluginContainer());
+    WebPluginContainerImpl* pluginContainer =  node.isNull() ? pluginContainerFromFrame(frame()) : pluginContainerFromNode(node);
 
     if (!pluginContainer || !pluginContainer->supportsPaginatedPrint())
         return false;
