55e3471d6cbb61487f11e0808bec507d2f0d781e
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-19 18:01:31 +0000

    Let shift globally trigger slow animation mode.
    A long time ago, we'd used an accelerator to link shift with slow animation
    mode, but this affected real key combos so a more piecemeal approach was taken,
    disabling/enabling slow animation modes for common ui operations. The problem
    with this approach is that it didn't catch many important UI animations. The
    fading in and fading out of context menus for example. This CL hooks into the
    keyboard event stream at a very high level to enable/disable slow animation
    mode. This has its downsides, too -- mainly that we end up with a specific bit
    of business littering some very general code.
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11415050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168552 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/accelerators/accelerator_filter.cc
+++ ash/accelerators/accelerator_filter.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/accelerators/accelerator_manager.h"
 #include "ui/base/events/event.h"
+#include "ui/base/keycodes/keyboard_codes_posix.h"
 
 namespace ash {
 namespace {
@@ -70,6 +71,8 @@ AcceleratorFilter::~AcceleratorFilter() {
 
 ui::EventResult AcceleratorFilter::OnKeyEvent(ui::KeyEvent* event) {
   const ui::EventType type = event->type();
+  if (event->key_code() == ui::VKEY_SHIFT)
+    ui::LayerAnimator::set_slow_animation_mode(type == ui::ET_KEY_PRESSED);
   if (type != ui::ET_KEY_PRESSED && type != ui::ET_KEY_RELEASED)
     return ui::ER_UNHANDLED;
   if (event->is_char())
--- ash/launcher/launcher_view.cc
+++ ash/launcher/launcher_view.cc
@@ -1015,8 +1015,6 @@ void LauncherView::ButtonPressed(views::Button* sender,
   if (view_index == -1)
     return;
 
-  if (event.IsShiftDown())
-    ui::LayerAnimator::set_slow_animation_mode(true);
   tooltip_->Close();
   switch (model_->items()[view_index].type) {
     case TYPE_TABBED:
@@ -1045,8 +1043,6 @@ void LauncherView::ButtonPressed(views::Button* sender,
       delegate_->OnBrowserShortcutClicked(event.flags());
       break;
   }
-  if (event.IsShiftDown())
-    ui::LayerAnimator::set_slow_animation_mode(false);
 }
 
 void LauncherView::ShowContextMenuForView(views::View* source,
