45f522c2021e222a2bd984588be94667280992cc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-19 11:43:33 +0000

    google_apis: Break the dependency in chrome/common/chrome_switches.h.
    BUG=146989
    R=satorux@chromium.org
    TBR=ben@chromium.org # for gyp changes
    
    Review URL: https://codereview.chromium.org/11316067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168507 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/drive_api_util.cc
+++ chrome/browser/google_apis/drive_api_util.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/drive_api_util.h"
 
 #include "base/command_line.h"
-#include "chrome/common/chrome_switches.h"
+#include "chrome/browser/google_apis/drive_switches.h"
 
 namespace google_apis {
 namespace util {
---
+++ chrome/browser/google_apis/drive_switches.h
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_GOOGLE_APIS_DRIVE_SWITCHES_H_
+#define CHROME_BROWSER_GOOGLE_APIS_DRIVE_SWITCHES_H_
+
+namespace google_apis {
+namespace switches {
+
+extern const char kEnableDriveV2Api[];
+
+}  // namespace switches
+}  // namespace google_apis
+
+#endif  // CHROME_BROWSER_GOOGLE_APIS_DRIVE_SWITCHES_H_
