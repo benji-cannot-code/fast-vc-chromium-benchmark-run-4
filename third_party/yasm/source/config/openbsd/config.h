509261d015e3c23d011add449b6f18762ad29b40
hbono@chromium.org <hbono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-19 06:11:14 +0000

    Update Update Yasm to 1.2.0 (Chromium Part) This change updates our copy of Yasm to 1.2.0. (This change updates the version numbers used for compiling yasm.)
    BUG=156030
    TEST=build Chromium
    Review URL: https://codereview.chromium.org/11367057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168491 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/yasm/source/config/openbsd/config.h
+++ third_party/yasm/source/config/openbsd/config.h
@@ -108,17 +108,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /* Define to the address where bug reports for this package should be sent. */
 #define PACKAGE_BUGREPORT "bug-yasm@tortall.net"
 
-/* Define to build version of this package. */
-#define PACKAGE_BUILD "2352"
-
-/* Define to internal version of this package. */
-#define PACKAGE_INTVER "1.1.0"
-
 /* Define to the full name of this package. */
 #define PACKAGE_NAME "yasm"
 
 /* Define to the full name and version of this package. */
-#define PACKAGE_STRING "yasm 1.1.0"
+#define PACKAGE_STRING "yasm 1.2.0"
 
 /* Define to the one symbol short name of this package. */
 #define PACKAGE_TARNAME "yasm"
@@ -127,7 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define PACKAGE_URL ""
 
 /* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1.0"
+#define PACKAGE_VERSION "1.2.0"
 
 /* Define to 1 if the C compiler supports function prototypes. */
 #define PROTOTYPES 1
@@ -151,7 +145,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define STDC_HEADERS 1
 
 /* Version number of package */
-#define VERSION "1.1.0"
+#define VERSION "1.2.0"
 
 /* Define if using the dmalloc debugging malloc package */
 /* #undef WITH_DMALLOC */
