e2acb41a79fb211418a0178dffcf0d34455e2945
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-19 10:49:42 +0000

    Add a UMA metric for whenever an extension is wiped out and when it gets re-enabled.
    TBR=sky (for OWNERS)
    BUG=154624
    TEST=None, the UMA experiment dashboard needs to be checked to see the new boolean histogram.
    Review URL: https://codereview.chromium.org/11416033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -927,6 +927,10 @@ void ExtensionService::EnableExtension(const std::string& extension_id) {
     AcknowledgeExternalExtension(extension->id());
   }
 
+  if (extension_prefs_->GetDisableReasons(extension->id()) &
+      Extension::DISABLE_SIDELOAD_WIPEOUT)
+    UMA_HISTOGRAM_BOOLEAN("DisabledExtension.ExtensionWipedStatus", false);
+
   // Move it over to the enabled list.
   extensions_.Insert(make_scoped_refptr(extension));
   disabled_extensions_.Remove(extension->id());
@@ -2255,6 +2259,7 @@ void ExtensionService::MaybeWipeout(
           extension->id(),
           static_cast<Extension::DisableReason>(
           Extension::DISABLE_SIDELOAD_WIPEOUT));
+      UMA_HISTOGRAM_BOOLEAN("DisabledExtension.ExtensionWipedStatus", true);
     }
   }
 }
