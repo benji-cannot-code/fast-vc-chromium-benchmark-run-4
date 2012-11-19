4783f5f579fd96e410267039ebe172c20c95aa6a
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-19 06:22:01 +0000

    Modify DriveFileSyncService::pending_changes_ to be unique for URL.
    BUG=156041
    
    Review URL: https://codereview.chromium.org/11316016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168493 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_file_sync_service.h
+++ chrome/browser/sync_file_system/drive_file_sync_service.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <deque>
 #include <map>
-#include <queue>
+#include <set>
 #include <string>
 #include <vector>
 
@@ -86,17 +86,33 @@ class DriveFileSyncService
   friend class DriveFileSyncServiceTest;
   class TaskToken;
 
+  struct ChangeQueueItem {
+    int64 changestamp;
+    fileapi::FileSystemURL url;
+
+    ChangeQueueItem();
+    ChangeQueueItem(int64 changestamp, const fileapi::FileSystemURL& url);
+  };
+
+  struct ChangeQueueComparator {
+    bool operator()(const ChangeQueueItem& left, const ChangeQueueItem& right);
+  };
+
+  typedef std::set<ChangeQueueItem, ChangeQueueComparator> PendingChangeQueue;
+
   struct RemoteChange {
     int64 changestamp;
     std::string resource_id;
     fileapi::FileSystemURL url;
     fileapi::FileChange change;
+    PendingChangeQueue::iterator position_in_queue;
 
     RemoteChange();
     RemoteChange(int64 changestamp,
                  const std::string& resource_id,
                  const fileapi::FileSystemURL& url,
-                 const fileapi::FileChange& change);
+                 const fileapi::FileChange& change,
+                 PendingChangeQueue::iterator position_in_queue);
     ~RemoteChange();
   };
 
@@ -104,10 +120,10 @@ class DriveFileSyncService
     bool operator()(const RemoteChange& left, const RemoteChange& right);
   };
 
-  typedef std::priority_queue<RemoteChange, std::vector<RemoteChange>,
-                              RemoteChangeComparator> ChangeQueue;
-  typedef std::map<FilePath, int64> PathToChangeStamp;
-  typedef std::map<GURL, PathToChangeStamp> ChangeStampMap;
+  // TODO(tzik): Consider using std::pair<FilePath, FileType> as the key below
+  // to support directories and custom conflict handling.
+  typedef std::map<FilePath, RemoteChange> PathToChange;
+  typedef std::map<GURL, PathToChange> URLToChange;
 
   // Task types; used for task token handling.
   enum TaskType {
@@ -175,8 +191,8 @@ class DriveFileSyncService
   std::deque<base::Closure> pending_tasks_;
 
   int64 largest_changestamp_;
-  ChangeQueue pending_changes_;
-  ChangeStampMap changestamp_map_;
+  PendingChangeQueue pending_changes_;
+  URLToChange url_to_change_;
 
   // Absence of |token_| implies a task is running. Incoming tasks should
   // wait for the task to finish in |pending_tasks_| if |token_| is null.
