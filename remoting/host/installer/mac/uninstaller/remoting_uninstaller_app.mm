285bcebcddb449ebab0c78051605b0b62f5038f1
skonig@chromium.org <skonig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 22:40:01 +0000

    Adds an icon to the final confirmation dialog after uninstall succeeds (or fails) by calling setAlertStyle. Also adds an icon into the initial confirmation dialog before uninstall starts.
    BUG=160303
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/11419083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168896 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/installer/mac/uninstaller/remoting_uninstaller_app.mm
+++ remoting/host/installer/mac/uninstaller/remoting_uninstaller_app.mm
@@ -26,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                                  alternateButton:nil
                                      otherButton:nil
                        informativeTextWithFormat:@"%@", message];
+  [alert setAlertStyle:
+       (success ? NSInformationalAlertStyle : NSCriticalAlertStyle)];
   [alert runModal];
 }
 
