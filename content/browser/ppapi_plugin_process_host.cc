4318bcc2448283a8ec5fb787a13be7add2f6ea97
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 03:40:57 +0000

    Put pepper threading support behind a command line flag.
    Also, fix deadlock for sync browser messages by unlocking.
    
    BUG=161429
    
    
    Review URL: https://chromiumcodereview.appspot.com/11412053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168714 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/ppapi_plugin_process_host.cc
+++ content/browser/ppapi_plugin_process_host.cc
@@ -238,6 +238,7 @@ bool PpapiPluginProcessHost::Init(const PepperPluginInfo& info) {
     // going to explode.
     static const char* kPluginForwardSwitches[] = {
       switches::kDisableSeccompFilterSandbox,
+      switches::kEnablePepperThreading,
 #if defined(OS_MACOSX)
       switches::kEnableSandboxLogging,
 #endif
--- ppapi/proxy/plugin_globals.cc
+++ ppapi/proxy/plugin_globals.cc
@@ -21,6 +21,12 @@ PluginGlobals::PluginGlobals()
       callback_tracker_(new CallbackTracker),
       loop_for_main_thread_(
           new MessageLoopResource(MessageLoopResource::ForMainThread())) {
+#if defined(ENABLE_PEPPER_THREADING)
+  enable_threading_ = true;
+#else
+  enable_threading_ = false;
+#endif
+
   DCHECK(!plugin_globals_);
   plugin_globals_ = this;
 }
@@ -29,6 +35,11 @@ PluginGlobals::PluginGlobals(ForTest for_test)
     : ppapi::PpapiGlobals(for_test),
       plugin_proxy_delegate_(NULL),
       callback_tracker_(new CallbackTracker) {
+#if defined(ENABLE_PEPPER_THREADING)
+  enable_threading_ = true;
+#else
+  enable_threading_ = false;
+#endif
   DCHECK(!plugin_globals_);
 }
 
@@ -81,11 +92,9 @@ void PluginGlobals::PreCacheFontForFlash(const void* logfontw) {
 }
 
 base::Lock* PluginGlobals::GetProxyLock() {
-#ifdef ENABLE_PEPPER_THREADING
-  return &proxy_lock_;
-#else
+  if (enable_threading_)
+    return &proxy_lock_;
   return NULL;
-#endif
 }
 
 void PluginGlobals::LogWithSource(PP_Instance instance,
