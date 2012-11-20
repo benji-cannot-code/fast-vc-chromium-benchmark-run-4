ec42eaed8fa9d54e65ea52d8ca7798cec415fac1
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 04:17:54 +0000

    cc: Make UV coords in TileDrawQuad a full RectF
    Previously, TileDrawQuad assumed that texels were 1:1 with geometry, so it only
    needed an offset into the texture to calculate the UVs.  For impl-side painting
    where multiple contents scales might be in play, a given texture rect isn't going
    to be 1:1 with geometry rects and also will not be necessarily integer aligned.
    
    Also, some things (like edge antialiasing) still need to know the texture size
    so that it can do half-texel offsets, so this new texture rect needs to stay in
    "texel space" rather than 0-1 "uv space".
    
    R=danakj@chromium.org
    BUG=155209
    
    
    Review URL: https://chromiumcodereview.appspot.com/11348109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168723 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/tile_draw_quad.cc
+++ cc/tile_draw_quad.cc
@@ -10,14 +10,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-scoped_ptr<TileDrawQuad> TileDrawQuad::create(const SharedQuadState* sharedQuadState, const gfx::Rect& quadRect, const gfx::Rect& opaqueRect, unsigned resourceId, const gfx::Vector2d& textureOffset, const gfx::Size& textureSize, bool swizzleContents, bool leftEdgeAA, bool topEdgeAA, bool rightEdgeAA, bool bottomEdgeAA)
+scoped_ptr<TileDrawQuad> TileDrawQuad::create(const SharedQuadState* sharedQuadState, const gfx::Rect& quadRect, const gfx::Rect& opaqueRect, unsigned resourceId, const gfx::RectF& texCoordRect, const gfx::Size& textureSize, bool swizzleContents, bool leftEdgeAA, bool topEdgeAA, bool rightEdgeAA, bool bottomEdgeAA)
 {
-    return make_scoped_ptr(new TileDrawQuad(sharedQuadState, quadRect, opaqueRect, resourceId, textureOffset, textureSize, swizzleContents, leftEdgeAA, topEdgeAA, rightEdgeAA, bottomEdgeAA));
+    return make_scoped_ptr(new TileDrawQuad(sharedQuadState, quadRect, opaqueRect, resourceId, texCoordRect, textureSize, swizzleContents, leftEdgeAA, topEdgeAA, rightEdgeAA, bottomEdgeAA));
 }
 
-TileDrawQuad::TileDrawQuad(const SharedQuadState* sharedQuadState, const gfx::Rect& quadRect, const gfx::Rect& opaqueRect, unsigned resourceId, const gfx::Vector2d& textureOffset, const gfx::Size& textureSize, bool swizzleContents, bool leftEdgeAA, bool topEdgeAA, bool rightEdgeAA, bool bottomEdgeAA)
+TileDrawQuad::TileDrawQuad(const SharedQuadState* sharedQuadState, const gfx::Rect& quadRect, const gfx::Rect& opaqueRect, unsigned resourceId, const gfx::RectF& texCoordRect, const gfx::Size& textureSize, bool swizzleContents, bool leftEdgeAA, bool topEdgeAA, bool rightEdgeAA, bool bottomEdgeAA)
     : m_resourceId(resourceId)
-    , m_textureOffset(textureOffset)
+    , m_texCoordRect(texCoordRect)
     , m_textureSize(textureSize)
     , m_swizzleContents(swizzleContents)
     , m_leftEdgeAA(leftEdgeAA)
