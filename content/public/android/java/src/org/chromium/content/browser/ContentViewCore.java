0c22c50b4af2ed98e49b0d9c219cf339432ba8aa
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 22:42:15 +0000

    Call afterSizeCHanged "after" size is changed
    During upstreaming bits of this function it got out of order and we were
    calling afterSizeChanged before we update the size.
    
    BUG=158667
    
    
    Review URL: https://chromiumcodereview.appspot.com/11417091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168897 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -1191,8 +1191,6 @@ public class ContentViewCore implements MotionEventDelegate {
     public void onSizeChanged(int w, int h, int ow, int oh) {
         mPopupZoomer.hide(false);
 
-        updateAfterSizeChanged();
-
         // Update the content size to make sure it is at least the View size
         if (mContentWidth < w) mContentWidth = w;
         if (mContentHeight < h) mContentHeight = h;
@@ -1204,6 +1202,8 @@ public class ContentViewCore implements MotionEventDelegate {
                 nativeSetSize(mNativeContentViewCore, mViewportWidth, mViewportHeight);
             }
         }
+
+        updateAfterSizeChanged();
     }
 
     public void updateAfterSizeChanged() {
