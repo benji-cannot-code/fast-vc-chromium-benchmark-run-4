f762545a51c2671aebd7c7f8d988f32dfa3e9ab0
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 22:04:04 +0000

    Video enumeration API is using the QTCaptureDevice, and if we use IOService for device monitoring, the notification comes before the initialization of the device, so the device list won't be updated.
    After doing some tests, QTCaptureDevice notification comes after the AudioObjectAddPropertyListener notification, so it is also safe the use QTCaptureDevice for audio devices.
    
    BUG=160872 
    TEST= goto content setting media, plug in or unplug audio/video devices, the device selection UI should be updated.
    
    Review URL: https://chromiumcodereview.appspot.com/11415059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168880 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/device_monitor_mac.h
+++ content/browser/device_monitor_mac.h
@@ -6,11 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_BROWSER_DEVICE_MONITOR_MAC_H_
 #define CONTENT_BROWSER_DEVICE_MONITOR_MAC_H_
 
-#include <CoreAudio/AudioHardware.h>
-#include <IOKit/IOKitLib.h>
-
-#include <vector>
-
 #include "base/basictypes.h"
 #include "base/system_monitor/system_monitor.h"
 
@@ -22,26 +17,11 @@ class DeviceMonitorMac {
   ~DeviceMonitorMac();
 
  private:
-  void RegisterAudioServices();
-  void RegisterVideoServices();
-
-  static OSStatus AudioDeviceCallback(
-      AudioObjectID object, UInt32 size,
-      const AudioObjectPropertyAddress addresses[],
-      void* context);
-
-  static void VideoDeviceCallback(void* context, io_iterator_t iterator);
-
   // Forward the notifications to system monitor.
   void NotifyDeviceChanged(base::SystemMonitor::DeviceType type);
 
-  // Helper.
-  void RegisterServices(CFMutableDictionaryRef dictionary,
-                        IOServiceMatchingCallback callback);
-
-  IONotificationPortRef notification_port_;
-  std::vector<io_iterator_t*> notification_iterators_;
-
+  class QTMonitorImpl;
+  scoped_ptr<DeviceMonitorMac::QTMonitorImpl> qt_monitor_;
   DISALLOW_COPY_AND_ASSIGN(DeviceMonitorMac);
 };
 
