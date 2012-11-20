c232f23745d5acb6034cb4f6b70e19cac9234dd8
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 01:10:22 +0000

    Fix Linux AudioPipeReader creation & teardown.
    This CL adds AudioPipeReader::Create(), which takes a reference to the new AudioPipeReader before posting the startup task to the audio thread, avoiding a race condition that could result in the object being destroyed before being returned to the caller.
    
    It also fixes the AudioPipeReader to destruct itself on the audio thread, which is required to properly tear down internal timer structures.
    
    BUG=161653
    
    Review URL: https://chromiumcodereview.appspot.com/11420053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168678 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/audio_capturer_linux.cc
+++ remoting/host/audio_capturer_linux.cc
@@ -27,7 +27,7 @@ void AudioCapturerLinux::InitializePipeReader(
     scoped_refptr<base::SingleThreadTaskRunner> task_runner,
     const FilePath& pipe_name) {
   g_pulseaudio_pipe_sink_reader.Get() =
-      new AudioPipeReader(task_runner, pipe_name);
+      AudioPipeReader::Create(task_runner, pipe_name);
 }
 
 AudioCapturerLinux::AudioCapturerLinux(
