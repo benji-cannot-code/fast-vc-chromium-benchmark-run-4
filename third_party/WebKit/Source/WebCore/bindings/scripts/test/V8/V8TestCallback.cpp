1c2ad534a316cdfa6079d6a9dd07633a5f730a9c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 19:59:14 +0000

    Store MutationObserver callback in a hidden property for V8 https://bugs.webkit.org/show_bug.cgi?id=102555
    Patch by Elliott Sprehn <esprehn@chromium.org> on 2012-11-20
    Reviewed by Adam Barth.
    
    .:
    
    Test for reference cycle leaks with mutation observers. There doesn't seem
    to be a way to check this for v8, but if you manually run you can see if it
    leaks observers.
    
    * ManualTests/leak-cycle-observer-wrapper.html: Added.
    
    Source/WebCore:
    
    To prevent circular reference leaks we should store the MutationObserver
    callback in a hidden property on the wrapper of the observer.
    
    This is done by extending the code generator to support a new owner
    argument to ::create() that lets you set the owner of the callback where
    the hidden property should be stored.
    
    Test: ManualTests/leak-cycle-observer-wrapper.html
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateCallbackHeader):
    (GenerateCallbackImplementation):
    * bindings/scripts/test/V8/V8TestCallback.cpp: rebaselined.
    * bindings/scripts/test/V8/V8TestCallback.h: rebaselined.
    * bindings/v8/V8HiddenPropertyName.h:
    * bindings/v8/custom/V8MutationObserverCustom.cpp:
    (WebCore::V8MutationObserver::constructorCallback):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135305 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestCallback.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestCallback.cpp
@@ -40,16 +40,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-V8TestCallback::V8TestCallback(v8::Local<v8::Object> callback, ScriptExecutionContext* context)
+V8TestCallback::V8TestCallback(v8::Handle<v8::Object> callback, ScriptExecutionContext* context, v8::Handle<v8::Object> owner)
     : ActiveDOMCallback(context)
     , m_callback(v8::Persistent<v8::Object>::New(callback))
     , m_worldContext(UseCurrentWorld)
 {
+    if (!owner.IsEmpty()) {
+        owner->SetHiddenValue(V8HiddenPropertyName::callback(), callback);
+        m_callback.MakeWeak(this, &V8TestCallback::weakCallback);
+    }
 }
 
 V8TestCallback::~V8TestCallback()
 {
-    m_callback.Dispose();
+    if (!m_callback.IsEmpty())
+        m_callback.Dispose();
 }
 
 // Functions
--- third_party/WebKit/Source/WebCore/bindings/v8/V8HiddenPropertyName.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8HiddenPropertyName.h
@@ -38,6 +38,7 @@ namespace WebCore {
 
 #define V8_HIDDEN_PROPERTIES(V) \
     V(attributeListener) \
+    V(callback) \
     V(detail) \
     V(document) \
     V(domStringMap) \
