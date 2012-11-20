cc06def533eb44d1eb9e0fdd5e4034f4fe01ad95
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 23:45:31 +0000

    Added in NetworkType to network notifications, and updated network connection failed message to display a different icon for failed cellular network connections, temporarily using IDR_AURA_UBER_TRAY_CELLULAR_DISABLED_HOVER awaiting larger dedicated version of that icon for this specific message.
    BUG=148993
    
    
    Review URL: https://chromiumcodereview.appspot.com/11420089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168911 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray_notifier.h
+++ ash/system/tray/system_tray_notifier.h
@@ -81,6 +81,7 @@ public:
   void NotifyRefreshNetwork(const NetworkIconInfo &info);
   void NotifySetNetworkMessage(NetworkTrayDelegate* delegate,
                                NetworkObserver::MessageType message_type,
+                               NetworkObserver::NetworkType network_type,
                                const string16& title,
                                const string16& message,
                                const std::vector<string16>& links);
