36d853b7d3b621629dbb290115de48f3b767be02
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 22:28:36 +0000

    Adding functional include back to spellcheck_custom_dictionary.cc
    This include is necessary to compile on VS2012.
    
    BUG=162054
    
    TBR=rpetterson
    
    Review URL: https://chromiumcodereview.appspot.com/11308127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168888 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
+++ chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/spellchecker/spellcheck_custom_dictionary.h"
 
+#include <functional>
+
 #include "base/file_util.h"
 #include "base/string_split.h"
 #include "chrome/browser/profiles/profile.h"
