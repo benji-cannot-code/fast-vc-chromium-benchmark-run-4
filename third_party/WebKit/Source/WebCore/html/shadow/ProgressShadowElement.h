850b5fa071f7496f14a3cc3702c2fb4254ad9f63
shinyak@chromium.org <shinyak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 05:49:45 +0000

    [Refactoring] Remove shadowPseudoId() and use setPseudo() in <progress> ElementShadow. https://bugs.webkit.org/show_bug.cgi?id=101703
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    We're migrating shadowPseudoId() to pseudo(). We remove shadowPseudoId() from HTMLKeygenElement and use
    setPseudo()/pseudo() instead.
    
    No new tests, simple refactoring.
    
    * html/shadow/ProgressShadowElement.cpp:
    (WebCore::ProgressInnerElement::ProgressInnerElement):
    * html/shadow/ProgressShadowElement.h:
    (ProgressInnerElement):
    (WebCore::ProgressBarElement::ProgressBarElement):
    (ProgressBarElement):
    (WebCore::ProgressValueElement::ProgressValueElement):
    
    LayoutTests:
    
    * editing/style/apply-style-atomic-expected.txt:
    * fast/dom/HTMLProgressElement/progress-element-markup-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/ProgressShadowElement.h
+++ third_party/WebKit/Source/WebCore/html/shadow/ProgressShadowElement.h
@@ -57,7 +57,6 @@ public:
     static PassRefPtr<ProgressInnerElement> create(Document*);
 private:
     virtual RenderObject* createRenderer(RenderArena*, RenderStyle*) OVERRIDE;
-    virtual const AtomicString& shadowPseudoId() const;
     virtual bool rendererIsNeeded(const NodeRenderingContext&);
 };
 
@@ -66,10 +65,11 @@ public:
     ProgressBarElement(Document* document) 
         : ProgressShadowElement(document)
     {
+        DEFINE_STATIC_LOCAL(AtomicString, pseudoId, ("-webkit-progress-bar", AtomicString::ConstructFromLiteral));
+        setPseudo(pseudoId);
     }
 
     static PassRefPtr<ProgressBarElement> create(Document*);
-    virtual const AtomicString& shadowPseudoId() const;
 };
 
 inline PassRefPtr<ProgressBarElement> ProgressBarElement::create(Document* document)
@@ -82,9 +82,10 @@ public:
     ProgressValueElement(Document* document) 
         : ProgressShadowElement(document)
     {
+        DEFINE_STATIC_LOCAL(AtomicString, pseudoId, ("-webkit-progress-value", AtomicString::ConstructFromLiteral));
+        setPseudo(pseudoId);
     }
 
-    virtual const AtomicString& shadowPseudoId() const;
     static PassRefPtr<ProgressValueElement> create(Document*);
     void setWidthPercentage(double);
 };
