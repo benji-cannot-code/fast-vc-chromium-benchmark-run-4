1f18dd43084530c844f5eab6805df59d72a76619
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 20:33:20 +0000

    NaCl: Make some C files compilable with -Wstrict-prototypes
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3114
    TEST=build
    
    Review URL: https://codereview.chromium.org/11416105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168865 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/tests/nacl_browser/inbrowser_test_runner/test_runner_ppapi.c
+++ ppapi/native_client/tests/nacl_browser/inbrowser_test_runner/test_runner_ppapi.c
@@ -50,7 +50,7 @@ int RunTests(int (*test_func)(void)) {
   }
 }
 
-int TestRunningInBrowser() {
+int TestRunningInBrowser(void) {
   return 1;
 }
 
@@ -145,7 +145,7 @@ int32_t PPP_InitializeModule(PP_Module module,
   return PP_OK;
 }
 
-void PPP_ShutdownModule() {
+void PPP_ShutdownModule(void) {
 }
 
 const void *PPP_GetInterface(const char *interface_name) {
