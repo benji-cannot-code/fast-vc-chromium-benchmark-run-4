727bf43189a0a652ae3674f91ed83432041d2c60
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 21:01:08 +0000

    chromeos: Remove old chromeos_wm_ipc_enums.h includes.
    These have been obsolete since we switched to Aura.
    
    BUG=none
    TBR=davemoore@chromium.org
    
    Review URL: https://codereview.chromium.org/11417073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168878 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/base_login_display_host.cc
+++ chrome/browser/chromeos/login/base_login_display_host.cc
@@ -43,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 #include "googleurl/src/gurl.h"
-#include "third_party/cros_system_api/window_manager/chromeos_wm_ipc_enums.h"
 #include "ui/aura/window.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/compositor/layer.h"
--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -45,7 +45,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/user_metrics.h"
 #include "googleurl/src/gurl.h"
 #include "grit/generated_resources.h"
-#include "third_party/cros_system_api/window_manager/chromeos_wm_ipc_enums.h"
 #include "ui/base/l10n/l10n_util.h"
 
 using content::BrowserThread;
