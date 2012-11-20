6004f939e5248089ecbac451950ed8efb6cacbe4
skaslev@chromium.org <skaslev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 04:29:29 +0000

    Removed unnecessary member out of WebGraphicsContext3DCommandBufferImpl.
    Review URL: https://chromiumcodereview.appspot.com/11414065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168728 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.cc
+++ content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.cc
@@ -130,7 +130,6 @@ WebGraphicsContext3DCommandBufferImpl::WebGraphicsContext3DCommandBufferImpl(
       surface_id_(surface_id),
       active_url_(active_url),
       swap_client_(swap_client),
-      memory_allocation_changed_callback_(0),
       context_lost_callback_(0),
       context_lost_reason_(GL_NO_ERROR),
       error_message_callback_(0),
@@ -707,15 +706,14 @@ void WebGraphicsContext3DCommandBufferImpl::sendManagedMemoryStatsCHROMIUM(
 void WebGraphicsContext3DCommandBufferImpl::
     setMemoryAllocationChangedCallbackCHROMIUM(
         WebGraphicsMemoryAllocationChangedCallbackCHROMIUM* callback) {
-  memory_allocation_changed_callback_ = callback;
-
   if (!command_buffer_)
     return;
 
   if (callback)
     command_buffer_->SetMemoryAllocationChangedCallback(base::Bind(
         &WebGraphicsContext3DCommandBufferImpl::OnMemoryAllocationChanged,
-        weak_ptr_factory_.GetWeakPtr()));
+        weak_ptr_factory_.GetWeakPtr(),
+        callback));
   else
     command_buffer_->SetMemoryAllocationChangedCallback(
         base::Callback<void(const GpuMemoryAllocationForRenderer&)>());
@@ -1449,6 +1447,7 @@ WebGraphicsMemoryAllocation::PriorityCutoff
 }
 
 void WebGraphicsContext3DCommandBufferImpl::OnMemoryAllocationChanged(
+    WebGraphicsMemoryAllocationChangedCallbackCHROMIUM* callback,
     const GpuMemoryAllocationForRenderer& allocation) {
 
   // Convert the gpu structure to the WebKit structure.
@@ -1473,9 +1472,8 @@ void WebGraphicsContext3DCommandBufferImpl::OnMemoryAllocationChanged(
   web_allocation.suggestHaveBackbuffer =
       allocation.have_backbuffer_when_not_visible;
 
-  if (memory_allocation_changed_callback_)
-    memory_allocation_changed_callback_->onMemoryAllocationChanged(
-        web_allocation);
+  if (callback)
+    callback->onMemoryAllocationChanged(web_allocation);
 
   // We may have allocated transfer buffers in order to free GL resources in a
   // backgrounded tab. Re-free the transfer buffers.
