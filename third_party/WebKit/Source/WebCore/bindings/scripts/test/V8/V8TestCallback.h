2e5d342bfb2726c777b745012e848efa622d3164
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 23:38:44 +0000

    [v8] Avoid unnecessary call to ToObject() in Callback constructors https://bugs.webkit.org/show_bug.cgi?id=102831
    Reviewed by Adam Barth.
    
    The code already asserted that the argument was an object, so calling
    ToObject() is unnecessary: a simple Cast() suffices.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateCallbackHeader):
    * bindings/scripts/test/V8/V8TestCallback.h:
    (WebCore::V8TestCallback::create):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestCallback.h
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/V8/V8TestCallback.h
@@ -40,7 +40,7 @@ public:
     {
         ASSERT(value->IsObject());
         ASSERT(context);
-        return adoptRef(new V8TestCallback(value->ToObject(), context, owner));
+        return adoptRef(new V8TestCallback(v8::Handle<v8::Object>::Cast(value), context, owner));
     }
 
     virtual ~V8TestCallback();
