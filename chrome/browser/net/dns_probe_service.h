a58de4f9f1fe68cf5c2390823ce05d3d74f4ab2d
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 00:18:05 +0000

    Glue browser-side DNS probing together.  We should detect DNS errors on main frame loads, run probes, and get the results, but they won't show error pages yet.
    BUG=156415
    TEST=extended NetErrorTabHelper test
    
    Review URL: https://chromiumcodereview.appspot.com/11363157
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168650 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/dns_probe_service.h
+++ chrome/browser/net/dns_probe_service.h
@@ -49,6 +49,7 @@ class DnsProbeService {
   void CallCallbacks();
 
   void OnProbeJobComplete(DnsProbeJob* job, DnsProbeJob::Result result);
+  Result EvaluateResults();
 
   // These are expected to be overridden by tests to return mock jobs.
   virtual scoped_ptr<DnsProbeJob> CreateSystemProbeJob(
@@ -61,6 +62,7 @@ class DnsProbeService {
       const DnsProbeJob::CallbackType& job_callback);
   void GetSystemDnsConfig(net::DnsConfig* config);
   void GetPublicDnsConfig(net::DnsConfig* config);
+  bool ResultsExpired();
 
   scoped_ptr<DnsProbeJob> system_job_;
   scoped_ptr<DnsProbeJob> public_job_;
@@ -69,6 +71,7 @@ class DnsProbeService {
   std::vector<CallbackType> callbacks_;
   State state_;
   Result result_;
+  base::Time last_probe_time_;
 
   DISALLOW_COPY_AND_ASSIGN(DnsProbeService);
 };
--- chrome/browser/net/net_error_tab_helper_unittest.cc
+++ chrome/browser/net/net_error_tab_helper_unittest.cc
@@ -5,10 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/net/net_error_tab_helper.h"
 
+#include "base/message_loop.h"
+#include "base/run_loop.h"
 #include "chrome/browser/net/dns_probe_service.h"
+#include "content/public/browser/browser_thread.h"
+#include "content/public/test/test_browser_thread.h"
 #include "googleurl/src/gurl.h"
 #include "net/base/net_errors.h"
 #include "testing/gtest/include/gtest/gtest.h"
+
+using content::BrowserThread;
+using content::TestBrowserThread;
 
 namespace chrome_browser_net {
 
@@ -19,13 +26,14 @@ class TestNetErrorTabHelper : public NetErrorTabHelper {
   TestNetErrorTabHelper()
       : NetErrorTabHelper(NULL), probe_start_count_(0) { }
 
-  virtual void StartDnsProbe() {
+  virtual void PostStartDnsProbeTask() OVERRIDE {
     ++probe_start_count_;
-    NetErrorTabHelper::StartDnsProbe();
   }
 
+  virtual bool DnsProbesAllowedByPref() const OVERRIDE { return true; }
+
   void SimulateProbeResult(DnsProbeService::Result result) {
-    set_dns_probe_running(false);
+    OnDnsProbeFinished(result);
   }
 
   bool dns_probe_running() { return NetErrorTabHelper::dns_probe_running(); }
@@ -37,7 +45,8 @@ class TestNetErrorTabHelper : public NetErrorTabHelper {
 
 class NetErrorTabHelperTest : public testing::Test {
  public:
-  NetErrorTabHelperTest() { }
+  NetErrorTabHelperTest()
+      : ui_thread_(BrowserThread::UI, &message_loop_) { }
 
  protected:
   void SimulateFailure(bool is_main_frame, int error_code) {
@@ -52,6 +61,9 @@ class NetErrorTabHelperTest : public testing::Test {
         error_description,
         NULL /* render_view_host */);
   }
+
+  MessageLoop message_loop_;
+  TestBrowserThread ui_thread_;
 
   TestNetErrorTabHelper tab_helper_;
 };
