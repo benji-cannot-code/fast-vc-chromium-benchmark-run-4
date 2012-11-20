97b609614741baa7c165dac2b1d827c24e156b1b
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 15:35:38 +0000

    Remove historical enums from ExceptionCode.h https://bugs.webkit.org/show_bug.cgi?id=102095
    Reviewed by Ojan Vafai.
    
    Remove unused enums from ExceptionCode.
    
    No new tests. No tests affected.
    
    * Modules/intents/NavigatorIntents.cpp:
    (WebCore::NavigatorIntents::webkitStartActivity): The error type for this is unspecified.
    * dom/DOMCoreException.cpp:
    * dom/ExceptionCode.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/intents/NavigatorIntents.cpp
+++ third_party/WebKit/Source/WebCore/Modules/intents/NavigatorIntents.cpp
@@ -62,7 +62,7 @@ void NavigatorIntents::webkitStartActivity(Navigator* navigator,
     }
 
     if (intent->action().isEmpty() || intent->type().isEmpty()) {
-        ec = VALIDATION_ERR;
+        ec = TYPE_MISMATCH_ERR;
         return;
     }
 
