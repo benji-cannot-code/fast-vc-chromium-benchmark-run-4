4d785664c860918d687c1fec1de65a6cb652f333
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 04:41:05 +0000

    Add systemInfo.display API implementation skeleton
    BUG=143750
    TEST=browser_test --gtest_filter=SystemInfoDisplayApiTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/11194054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168732 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/api/system_info_display/display_info_provider.h
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_API_SYSTEM_INFO_DISPLAY_DISPLAY_INFO_PROVIDER_H_
+#define CHROME_BROWSER_EXTENSIONS_API_SYSTEM_INFO_DISPLAY_DISPLAY_INFO_PROVIDER_H_
+
+#include "chrome/browser/extensions/system_info_provider.h"
+#include "chrome/common/extensions/api/experimental_system_info_display.h"
+
+namespace extensions {
+
+typedef std::vector<linked_ptr<
+    api::experimental_system_info_display::DisplayUnitInfo> > DisplayInfo;
+
+class DisplayInfoProvider : public SystemInfoProvider<DisplayInfo> {
+ public:
+  static DisplayInfoProvider* Get();
+
+  // Overriden from SystemInfoProvider<DisplayInfo>.
+  virtual bool QueryInfo(DisplayInfo* info) OVERRIDE;
+
+ protected:
+  friend class SystemInfoProvider<DisplayInfo>;
+
+  DisplayInfoProvider() {}
+  virtual ~DisplayInfoProvider() {}
+
+  DISALLOW_COPY_AND_ASSIGN(DisplayInfoProvider);
+};
+
+}  // namespace extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_API_SYSTEM_INFO_DISPLAY_DISPLAY_INFO_PROVIDER_H_
