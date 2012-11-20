470d2a25e1965a246b6d11c3884f4c66d65bd855
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 17:25:32 +0000

    overscroll-aura: Fix a couple of crashes related to the overscroll window.
     * Fix a crash when a tab is closed while in the middle of an overscroll
       animation.
     * Fix a crash when an overscroll gesture starts while the reset animation
       from the previous overscroll gesture is still in progress.
    
    Added a test to cover both these cases.
    
    BUG=160668
    
    Review URL: https://codereview.chromium.org/11299093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168822 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_view_aura.cc
+++ content/browser/web_contents/web_contents_view_aura.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/web_contents/web_contents_view_aura.h"
 
+#include "base/auto_reset.h"
 #include "base/utf_string_conversions.h"
 #include "content/browser/renderer_host/dip_util.h"
 #include "content/browser/renderer_host/overscroll_controller.h"
@@ -238,7 +239,7 @@ int ConvertAuraEventFlagsToWebInputEventModifiers(int aura_event_flags) {
 // Given the scrolled amount (|scroll|) and the threshold (|threshold|), returns
 // the amount the window should be translated.
 int GetResistedScrollAmount(int scroll, int threshold) {
-  CHECK_GE(scroll, 0);
+  DCHECK_GE(scroll, 0);
   if (scroll <= threshold)
     return scroll / 2;
 
@@ -366,6 +367,17 @@ void WebContentsViewAura::EndDrag(WebKit::WebDragOperationsMask ops) {
 }
 
 void WebContentsViewAura::PrepareOverscrollWindow() {
+  // If there is an existing |overscroll_window_| which is in the middle of an
+  // animation, then destroying the window here causes the animation to be
+  // completed immidiately, which triggers |OnImplicitAnimationsCompleted()|
+  // callback, and that tries to reset |overscroll_window_| again, causing a
+  // double-free. So use a temporary variable here.
+  if (overscroll_window_.get()) {
+    AutoReset<OverscrollMode> reset_state(&current_overscroll_gesture_,
+                                          current_overscroll_gesture_);
+    scoped_ptr<aura::Window> reset_window(overscroll_window_.release());
+  }
+
   overscroll_window_.reset(new aura::Window(NULL));
   overscroll_window_->SetType(aura::client::WINDOW_TYPE_CONTROL);
   overscroll_window_->SetTransparent(false);
@@ -837,6 +849,12 @@ void WebContentsViewAura::OnDeviceScaleFactorChanged(
 }
 
 void WebContentsViewAura::OnWindowDestroying() {
+  // This means the destructor is going to be called soon. If there is an
+  // overscroll gesture in progress (i.e. |overscroll_window_| is not NULL),
+  // then destroying it in the WebContentsViewAura destructor can trigger other
+  // virtual functions to be called (e.g. OnImplicitAnimationsCompleted()). So
+  // destroy the overscroll window here.
+  overscroll_window_.reset();
 }
 
 void WebContentsViewAura::OnWindowDestroyed() {
