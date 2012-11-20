a06999996311fbf09e055789a29cc0e73f5be226
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 18:26:32 +0000

    Remove V8DOMWindowShell::getEntered https://bugs.webkit.org/show_bug.cgi?id=96637
    Patch by Dan Carney <dcarney@google.com> on 2012-11-20
    Reviewed by Adam Barth.
    
    V8DOMWindowShell::getEntered was refactored so that the window shell
    no longer has to be kept alive by a v8 context. Instead, only
    the DOMWrapperWorld will be kept alive.
    
    No new tests. No change in functionality.
    
    * bindings/v8/DOMDataStore.cpp:
    (WebCore::DOMDataStore::current):
    * bindings/v8/DOMWrapperWorld.cpp:
    (WebCore::isolatedWorldWeakCallback):
    (WebCore):
    (WebCore::DOMWrapperWorld::makeContextWeak):
    (WebCore::DOMWrapperWorld::setIsolatedWorldField):
    * bindings/v8/DOMWrapperWorld.h:
    (DOMWrapperWorld):
    (WebCore::DOMWrapperWorld::isolated):
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::existingWindowShell):
    (WebCore::ScriptController::windowShell):
    (WebCore::ScriptController::evaluateInIsolatedWorld):
    (WebCore::ScriptController::currentWorldContext):
    (WebCore::ScriptController::collectIsolatedContexts):
    * bindings/v8/ScriptController.h:
    (ScriptController):
    * bindings/v8/V8Binding.h:
    (WebCore::worldForEnteredContextIfIsolated):
    * bindings/v8/V8DOMWindowShell.cpp:
    (WebCore::V8DOMWindowShell::destroyIsolatedShell):
    (WebCore::V8DOMWindowShell::disposeContext):
    (WebCore::V8DOMWindowShell::initializeIfNeeded):
    * bindings/v8/V8DOMWindowShell.h:
    (V8DOMWindowShell):
    * bindings/v8/V8PerContextData.h:
    * bindings/v8/WorldContextHandle.cpp:
    (WebCore::WorldContextHandle::WorldContextHandle):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135293 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWindowShell.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMWindowShell.h
@@ -81,11 +81,6 @@ public:
 
     void destroyGlobal();
 
-    static V8DOMWindowShell* isolated(v8::Handle<v8::Context> context)
-    {
-        return static_cast<V8DOMWindowShell*>(context->GetAlignedPointerFromEmbedderData(v8ContextIsolatedWindowShell));
-    }
-
     V8PerContextData* perContextData() { return m_perContextData.get(); }
     DOMWrapperWorld* world() { return m_world.get(); }
 
@@ -94,7 +89,7 @@ public:
 private:
     V8DOMWindowShell(Frame*, PassRefPtr<DOMWrapperWorld>);
 
-    void disposeContext(bool weak = false);
+    void disposeContext();
 
     void setSecurityToken();
 
--- third_party/WebKit/Source/WebCore/bindings/v8/WorldContextHandle.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/WorldContextHandle.cpp
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptController.h"
 #include "V8Binding.h"
 #include "V8DOMWindow.h"
-#include "V8DOMWindowShell.h"
 
 namespace WebCore {
 
@@ -53,7 +52,7 @@ WorldContextHandle::WorldContextHandle(WorldToUse worldToUse)
             return;
         }
 #endif
-        if (V8DOMWindowShell::isolated(context)) {
+        if (DOMWrapperWorld::isolated(context)) {
             m_context = SharedPersistent<v8::Context>::create(context);
             return;
         }
