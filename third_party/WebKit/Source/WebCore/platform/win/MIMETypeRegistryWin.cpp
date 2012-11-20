205bd66dfb8c485008b4d94cd078ed3622be0c4e
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 01:35:57 +0000

    [WIN] Add WebCore::getRegistryValue() https://bugs.webkit.org/show_bug.cgi?id=97828
    Reviewed by Brent Fulgham.
    
    The new function adds an abstraction to SHGetValue(), which isn't available on WinCE.
    Changing the existing files allows us to share more code between WinCE and WinNT in a next step.
    
    * platform/win/MIMETypeRegistryWin.cpp:
    (WebCore::mimeTypeForExtension):
    (WebCore):
    (WebCore::MIMETypeRegistry::getPreferredExtensionForMIMEType):
    * platform/win/WindowsExtras.h:
    (WebCore::getRegistryValue):
    (WebCore):
    * plugins/win/PluginDatabaseWin.cpp:
    (WebCore::addPluginPathsFromRegistry):
    (WebCore::addWindowsMediaPlayerPluginDirectory):
    (WebCore::addQuickTimePluginDirectory):
    (WebCore::addAdobeAcrobatPluginDirectory):
    (WebCore::addJavaPluginDirectory):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/MIMETypeRegistryWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/win/MIMETypeRegistryWin.cpp
@@ -27,13 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "MIMETypeRegistry.h"
 
-#include <shlwapi.h>
+#include "WindowsExtras.h"
 #include <wtf/Assertions.h>
 #include <wtf/HashMap.h>
 #include <wtf/MainThread.h>
 
-namespace WebCore 
-{
+namespace WebCore {
 
 static String mimeTypeForExtension(const String& extension)
 {
@@ -42,14 +41,14 @@ static String mimeTypeForExtension(const String& extension)
     DWORD contentTypeStrLen = sizeof(contentTypeStr);
     DWORD keyType;
 
-    HRESULT result = SHGetValue(HKEY_CLASSES_ROOT, ext.charactersWithNullTermination(), L"Content Type", &keyType, (LPVOID)contentTypeStr, &contentTypeStrLen);
+    HRESULT result = getRegistryValue(HKEY_CLASSES_ROOT, ext.charactersWithNullTermination(), L"Content Type", &keyType, contentTypeStr, &contentTypeStrLen);
 
-    if (result == ERROR_SUCCESS && keyType == REG_SZ) 
+    if (result == ERROR_SUCCESS && keyType == REG_SZ)
         return String(contentTypeStr, contentTypeStrLen / sizeof(contentTypeStr[0]) - 1);
 
     return String();
 }
-   
+
 String MIMETypeRegistry::getPreferredExtensionForMIMEType(const String& type)
 {
     String path = "MIME\\Database\\Content Type\\" + type;
@@ -57,9 +56,9 @@ String MIMETypeRegistry::getPreferredExtensionForMIMEType(const String& type)
     DWORD extStrLen = sizeof(extStr);
     DWORD keyType;
 
-    HRESULT result = SHGetValueW(HKEY_CLASSES_ROOT, path.charactersWithNullTermination(), L"Extension", &keyType, (LPVOID)extStr, &extStrLen);
+    HRESULT result = getRegistryValue(HKEY_CLASSES_ROOT, path.charactersWithNullTermination(), L"Extension", &keyType, extStr, &extStrLen);
 
-    if (result == ERROR_SUCCESS && keyType == REG_SZ) 
+    if (result == ERROR_SUCCESS && keyType == REG_SZ)
         return String(extStr + 1, extStrLen / sizeof(extStr[0]) - 2);
 
     return String();
