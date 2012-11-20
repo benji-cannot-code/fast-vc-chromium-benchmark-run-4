1a7b2ded82a5fe9f7690b86aee94198f8da54cd1
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 00:30:06 +0000

    Improve NaCl plugin error reporting when using IPC proxy. While we're at it, create the IDL for PPB_NaCl_Private. BUG=160076 TEST=manual Review URL: https://codereview.chromium.org/11418072
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/pepper/ppb_nacl_private_impl.h
+++ chrome/renderer/pepper/ppb_nacl_private_impl.h
@@ -9,8 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "build/build_config.h"
 
 #ifndef DISABLE_NACL
-
-struct PPB_NaCl_Private;
+#include "ppapi/c/private/ppb_nacl_private.h"
 
 class PPB_NaCl_Private_Impl {
  public:
