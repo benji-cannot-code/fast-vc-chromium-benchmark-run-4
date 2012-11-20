cfdf56eff0dcbda43ca040beac9d605624937f40
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 23:50:59 +0000

    [V8] Pass ScriptState::current() to functions marked with CallWith=ScriptState https://bugs.webkit.org/show_bug.cgi?id=102739
    Patch by Michael Pruett <michael@68k.org> on 2012-11-20
    Reviewed by Kentaro Hara.
    
    Previously EmptyScriptState rather than ScriptState::current()
    was passed to functions marked with [CallWith=ScriptState].
    Since the EmptyScriptState has a null v8::Context, any functions
    which depended upon a valid v8::Context would fail.
    
    No new tests. Covered by existing tests.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateNormalAttrGetter):
    (GenerateNormalAttrSetter):
    (GenerateCallWith):
    (GenerateFunctionCallString):
    * bindings/v8/ScriptState.h:
    (WebCore::ScriptState::clearException): Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135325 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/ScriptState.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/ScriptState.h
@@ -57,6 +57,7 @@ public:
         m_exception = exception;
     }
     v8::Local<v8::Value> exception() { return m_exception; }
+    void clearException() { m_exception.Clear(); }
 
     v8::Local<v8::Context> context() const
     {
