b746215b04ebd8aa7208aed574bdc02b5f58dd66
yoli@rim.com <yoli@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 16:32:32 +0000

    [ARMv7] Neither linkCall() nor linkPointer() should flush code. https://bugs.webkit.org/show_bug.cgi?id=99213
    Reviewed by George Staikos.
    
    LinkBuffer doesn't need to flush code during linking. It will
    eventually flush the whole executable. Fixing this gives >%5
    sunspider boost (on QNX).
    
    Also make replaceWithLoad() and replaceWithAddressComputation() flush
    only when necessary.
    
    * assembler/ARMv7Assembler.h:
    (JSC::ARMv7Assembler::linkCall):
    (JSC::ARMv7Assembler::linkPointer):
    (JSC::ARMv7Assembler::relinkCall):
    (JSC::ARMv7Assembler::repatchInt32):
    (JSC::ARMv7Assembler::repatchPointer):
    (JSC::ARMv7Assembler::replaceWithLoad): Flush only after it did write.
    (JSC::ARMv7Assembler::replaceWithAddressComputation): Flush only after it did write.
    (JSC::ARMv7Assembler::setInt32):
    (JSC::ARMv7Assembler::setPointer):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135286 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/ARMv7Assembler.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/ARMv7Assembler.h
@@ -2051,12 +2051,12 @@ public:
         ASSERT(from.isSet());
         ASSERT(reinterpret_cast<intptr_t>(to) & 1);
 
-        setPointer(reinterpret_cast<uint16_t*>(reinterpret_cast<intptr_t>(code) + from.m_offset) - 1, to);
+        setPointer(reinterpret_cast<uint16_t*>(reinterpret_cast<intptr_t>(code) + from.m_offset) - 1, to, false);
     }
 
     static void linkPointer(void* code, AssemblerLabel where, void* value)
     {
-        setPointer(reinterpret_cast<char*>(code) + where.m_offset, value);
+        setPointer(reinterpret_cast<char*>(code) + where.m_offset, value, false);
     }
 
     static void relinkJump(void* from, void* to)
@@ -2074,7 +2074,7 @@ public:
         ASSERT(!(reinterpret_cast<intptr_t>(from) & 1));
         ASSERT(reinterpret_cast<intptr_t>(to) & 1);
 
-        setPointer(reinterpret_cast<uint16_t*>(from) - 1, to);
+        setPointer(reinterpret_cast<uint16_t*>(from) - 1, to, true);
     }
     
     static void* readCallTarget(void* from)
@@ -2086,7 +2086,7 @@ public:
     {
         ASSERT(!(reinterpret_cast<intptr_t>(where) & 1));
         
-        setInt32(where, value);
+        setInt32(where, value, true);
     }
     
     static void repatchCompact(void* where, int32_t offset)
@@ -2113,7 +2113,7 @@ public:
     {
         ASSERT(!(reinterpret_cast<intptr_t>(where) & 1));
         
-        setPointer(where, value);
+        setPointer(where, value, true);
     }
 
     static void* readPointer(void* where)
@@ -2149,11 +2149,11 @@ public:
             ptr[0] |= OP_LDR_imm_T3;
             ptr[1] |= (ptr[1] & 0x0F00) << 4;
             ptr[1] &= 0xF0FF;
+            cacheFlush(ptr, sizeof(uint16_t) * 2);
             break;
         default:
             ASSERT_NOT_REACHED();
         }
-        cacheFlush(ptr, sizeof(uint16_t) * 2);
     }
 
     static void replaceWithAddressComputation(void* instructionStart)
@@ -2167,13 +2167,13 @@ public:
             ptr[0] |= OP_ADD_imm_T3;
             ptr[1] |= (ptr[1] & 0xF000) >> 4;
             ptr[1] &= 0x0FFF;
+            cacheFlush(ptr, sizeof(uint16_t) * 2);
             break;
         case OP_ADD_imm_T3:
             break;
         default:
             ASSERT_NOT_REACHED();
         }
-        cacheFlush(ptr, sizeof(uint16_t) * 2);
     }
 
     unsigned debugOffset() { return m_formatter.debugOffset(); }
@@ -2276,7 +2276,7 @@ private:
         return VFPOperand(op);
     }
 
-    static void setInt32(void* code, uint32_t value)
+    static void setInt32(void* code, uint32_t value, bool flush)
     {
         uint16_t* location = reinterpret_cast<uint16_t*>(code);
         ASSERT(isMOV_imm_T3(location - 4) && isMOVT(location - 2));
@@ -2288,7 +2288,8 @@ private:
         location[-2] = twoWordOp5i6Imm4Reg4EncodedImmFirst(OP_MOVT, hi16);
         location[-1] = twoWordOp5i6Imm4Reg4EncodedImmSecond((location[-1] >> 8) & 0xf, hi16);
 
-        cacheFlush(location - 4, 4 * sizeof(uint16_t));
+        if (flush)
+            cacheFlush(location - 4, 4 * sizeof(uint16_t));
     }
     
     static int32_t readInt32(void* code)
@@ -2319,9 +2320,9 @@ private:
         cacheFlush(location, sizeof(uint16_t));
     }
 
-    static void setPointer(void* code, void* value)
+    static void setPointer(void* code, void* value, bool flush)
     {
-        setInt32(code, reinterpret_cast<uint32_t>(value));
+        setInt32(code, reinterpret_cast<uint32_t>(value), flush);
     }
 
     static bool isB(void* address)
