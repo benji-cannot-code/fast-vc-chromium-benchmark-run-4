6a4c58ee9963d106382377384776cb06ff92cb0c
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 17:53:26 +0000

    Fix the issue of cursor being shown at startup.
    Cursor was shown by MagnificationController when it's enabled in chrome::OpenAsh.
    This CL makes MagnificationController not to show cursor if unnecessary.
    Also makes cursor initially hidden when the host window is not using full screen
    for testing ChromeOS build on desktop.
    
    BUG=157918
    TEST=Manually on device and desktop
    
    Review URL: https://chromiumcodereview.appspot.com/11364003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168830 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/magnifier/magnification_controller.cc
+++ ash/magnifier/magnification_controller.cc
@@ -362,6 +362,8 @@ void MagnificationControllerImpl::ValidateScale(float* scale) {
 }
 
 void MagnificationControllerImpl::OnImplicitAnimationsCompleted() {
+  if (!is_on_zooming_)
+    return;
   aura::client::CursorClient* cursor_client =
       aura::client::GetCursorClient(root_window_);
   if (cursor_client)
