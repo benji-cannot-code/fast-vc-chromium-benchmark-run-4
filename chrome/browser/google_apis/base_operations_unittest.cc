42fbc6abb373c42a245a41b98032913861f47832
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 04:36:12 +0000

    google_apis: Rename gdata_wapi_operations_unittest.cc
    to base_operations_unittest.cc.
    
    The test does not depend on code in gdata_wapi_operations.cc
    
    BUG=161722
    TEST=none; just a rename
    
    Review URL: https://codereview.chromium.org/11418076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168730 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/base_operations_unittest.cc
+++ chrome/browser/google_apis/base_operations_unittest.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "base/values.h"
-#include "chrome/browser/google_apis/gdata_wapi_operations.h"
+#include "chrome/browser/google_apis/base_operations.h"
 #include "chrome/browser/google_apis/operation_runner.h"
 #include "chrome/browser/google_apis/test_util.h"
 #include "chrome/test/base/testing_profile.h"
