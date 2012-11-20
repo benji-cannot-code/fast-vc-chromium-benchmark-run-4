94b46f0c20148402546927d1336ce69081c9c2ae
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 23:44:53 +0000

    Remove DCHECK causing TabCapture API test to fail due to no microphone.
    BUG=160787
    
    
    Review URL: https://chromiumcodereview.appspot.com/11412087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168909 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_low_latency_input_mac.cc
+++ media/audio/mac/audio_low_latency_input_mac.cc
@@ -535,8 +535,7 @@ int AUAudioInputStream::HardwareSampleRate() {
                                                0,
                                                &info_size,
                                                &device_id);
-  OSSTATUS_DCHECK(result == noErr, result);
-  if (result)
+  if (result != noErr)
     return 0.0;
 
   Float64 nominal_sample_rate;
@@ -553,8 +552,7 @@ int AUAudioInputStream::HardwareSampleRate() {
                                       0,
                                       &info_size,
                                       &nominal_sample_rate);
-  DCHECK_EQ(result, 0);
-  if (result)
+  if (result != noErr)
     return 0.0;
 
   return static_cast<int>(nominal_sample_rate);
