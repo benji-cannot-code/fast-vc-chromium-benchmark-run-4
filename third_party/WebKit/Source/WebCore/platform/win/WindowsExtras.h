b2a97e3581504f85a1cad67174968a7e2047ab1d
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-20 22:25:33 +0000

    [Qt] Build fix after r135217. https://bugs.webkit.org/show_bug.cgi?id=102787
    * platform/win/WindowsExtras.h: Add #undef of NOSHLWAPI to
    allow include of <ObjBase.h> needed by Qt build.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/WindowsExtras.h
+++ third_party/WebKit/Source/WebCore/platform/win/WindowsExtras.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WindowsExtras_h
 
 #include <windows.h>
+#undef NOSHLWAPI
 #include <shlwapi.h>
 
 namespace WebCore {
