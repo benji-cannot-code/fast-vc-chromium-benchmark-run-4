07c7db068f9a3836adb7ad8aff932be9985329bd
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 22:38:15 +0000

    Browser-tests for editing the dictionary on chrome://settings/editDictionary
    BUG=161361
    
    
    Review URL: https://chromiumcodereview.appspot.com/11308076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168894 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/language_dictionary_overlay.js
+++ chrome/browser/resources/options/language_dictionary_overlay.js
@@ -8,6 +8,10 @@ cr.define('options', function() {
   var ArrayDataModel = cr.ui.ArrayDataModel;
   var DictionaryWordsList = options.dictionary_words.DictionaryWordsList;
 
+  /**
+   * Adding and removing words in custom spelling dictionary.
+   * @constructor
+   */
   function EditDictionaryOverlay() {
     OptionsPage.call(this, 'editDictionary',
                      loadTimeData.getString('languageDictionaryOverlayPage'),
