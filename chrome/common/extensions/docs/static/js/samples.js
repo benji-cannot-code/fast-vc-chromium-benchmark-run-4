368216b97f44abf06e50fb4ee9d73a12498fab48
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 00:14:24 +0000

    Extensions Docs Server: Samples filter was not ignoring "chrome"
    When a filter was clicked on the samples page, nothing would show up, because
    none of the API call tags started with "chrome".
    
    Now all the API call tags start with "chrome.".
    
    BUG=161611
    
    Review URL: https://codereview.chromium.org/11416080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168645 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/static/js/samples.js
+++ chrome/common/extensions/docs/static/js/samples.js
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   var api_filter_items = document.getElementById('api_filter_items');
   api_filter_items.addEventListener('click', function(event) {
     if (event.target instanceof HTMLAnchorElement) {
-      search_box.value = 'chrome.' + event.target.innerText;
+      search_box.value = event.target.innerText;
       filterSamples();
     }
   });
