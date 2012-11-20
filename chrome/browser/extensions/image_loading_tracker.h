4a0642c6c110773580b32fc3aa6a1477b98e648b
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-20 16:40:13 +0000

    Deprecate ImageLoadingTracker after r168155 added a replacement.
    This makes the constructor private to prevent new code from using this class
    and adds all current users as friends.
    
    BUG=141673
    
    
    Review URL: https://chromiumcodereview.appspot.com/11348141
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168810 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/image_loading_tracker.h
+++ chrome/browser/extensions/image_loading_tracker.h
@@ -24,7 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class SkBitmap;
 
 namespace extensions {
+class AppShortcutManager;
 class Extension;
+class IconImage;
+class TabHelper;
 }
 
 namespace gfx {
@@ -46,6 +49,8 @@ class Image;
 // Observer is notified immediately from the call to LoadImage. In other words,
 // by the time LoadImage returns the observer has been notified.
 //
+// NOTE: This class is deprecated! New code should be using
+// extensions::ImageLoader or extensions::IconImage instead.
 class ImageLoadingTracker : public content::NotificationObserver {
  public:
   enum CacheParam {
@@ -97,7 +102,6 @@ class ImageLoadingTracker : public content::NotificationObserver {
     ui::ScaleFactor scale_factor;
   };
 
-  explicit ImageLoadingTracker(Observer* observer);
   virtual ~ImageLoadingTracker();
 
   // Specify image resource to load. If the loaded image is larger than
@@ -124,6 +128,28 @@ class ImageLoadingTracker : public content::NotificationObserver {
   int next_id() const { return next_id_; }
 
  private:
+  // This class is deprecated. This just lists all currently remaining
+  // usage of this class, so once this list is empty this class can and
+  // should be removed.
+  friend class CreateChromeApplicationShortcutsDialogGtk;
+  friend class CreateChromeApplicationShortcutView;
+  friend class ExtensionIconManager;
+  friend class ExtensionInfoBar;
+  friend class ExtensionInfoBarGtk;
+  friend class ExtensionUninstallDialog;
+  friend class InfobarBridge;
+  friend class Panel;
+  friend class ShellWindow;
+  friend class extensions::AppShortcutManager;
+  friend class extensions::IconImage;
+  friend class extensions::TabHelper;
+  FRIEND_TEST_ALL_PREFIXES(ImageLoadingTrackerTest, Cache);
+  FRIEND_TEST_ALL_PREFIXES(ImageLoadingTrackerTest,
+                           DeleteExtensionWhileWaitingForCache);
+  FRIEND_TEST_ALL_PREFIXES(ImageLoadingTrackerTest, MultipleImages);
+
+  explicit ImageLoadingTracker(Observer* observer);
+
   // Information for pending resource load operation for one or more image
   // representations.
   struct PendingLoadInfo {
