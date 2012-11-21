75209b5b3c30fe409cb115eee5b9235797d9eebd
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 20:11:38 +0000

    Check pref before running DNS probes
    Right now, the NetErrorTabHelper assumes it's never allowed to run probes.
    This change checks the "Use web service to resolve navigation errors" pref
    and allows probes to run if it's checked.
    
    BUG=156415
    
    
    Review URL: https://chromiumcodereview.appspot.com/11418074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google/google_util.h"
 #include "chrome/browser/io_thread.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
+#include "chrome/browser/net/net_error_tab_helper.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
@@ -151,6 +152,8 @@ void InProcessBrowserTest::SetUp() {
   captive_portal::CaptivePortalService::set_state_for_testing(
       captive_portal::CaptivePortalService::DISABLED_FOR_TESTING);
 #endif
+
+  chrome_browser_net::NetErrorTabHelper::set_enabled_for_testing(false);
 
   google_util::SetMockLinkDoctorBaseURLForTesting();
 
