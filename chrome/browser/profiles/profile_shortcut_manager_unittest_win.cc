16e7a8d3fd1be381d8f83fad3459fc563fc3ef47
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 00:17:33 +0000

    Fix ProfileShortcutManagerTests when the feature is enabled.
    There were several problems. The shell util code expected that
    COM was initialized, which was not happening in the test code
    path, so a call to CoInitialize() was necessary. There was also
    a mismatch in the code using GetShortcutNameForProfile(), where
    some code was expecting it to return a name that includes the
    extension and other code expected the extension to not be present.
    
    This CL fixes both problems and makes the tests pass when the
    feature code is enabled.
    
    BUG=161013
    TEST=Make ProfileShortcutManager::IsFeatureEnabled() return true
    and ensure the ProfileShortcutManagerTest.* tests pass.
    
    Review URL: https://chromiumcodereview.appspot.com/11418068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_shortcut_manager_unittest_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_unittest_win.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <objbase.h>  // For CoInitialize().
+
 #include "base/file_util.h"
 #include "base/message_loop.h"
 #include "base/path_service.h"
@@ -81,6 +83,7 @@ class ProfileShortcutManagerTest : public testing::Test {
   }
 
   virtual void SetUp() OVERRIDE {
+    CoInitialize(NULL);
     dist_ = BrowserDistribution::GetDistribution();
     ASSERT_TRUE(dist_ != NULL);
 
