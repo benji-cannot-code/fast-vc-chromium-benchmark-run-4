e392916f00f1173f884b37cd84f9160b5e8dff3e
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-21 15:30:56 +0000

    Unreviewed Qt/Windows build fix. https://bugs.webkit.org/show_bug.cgi?id=102787
    Remove inclusion of QtQml, which drags in QtNetwork, which drags in qnetworksession.h, which
    defines a member function called "interface". Since windows headers also define a pre-processor
    macro called "interface", it has to do that. However we have now a dependency between combaseapi.h,
    which #defines interface and is included earlier (before qnetworksession.h) and shlwapi.h, which _needs_
    the interface macro and is included afterwards. Since this module-wide QtQml inclusion is unnecessary
    and just slows down the build, this patch removes it.
    
    * UIProcess/API/qt/qwebkittest_p.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwebkittest_p.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/qwebkittest_p.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QtCore/QPointer>
 #include <QtCore/QSize>
 #include <QtCore/QVariant>
-#include <QtQml/QtQml>
 #include <QtQuick/qquickitem.h>
 
 class QQuickWebViewPrivate;
