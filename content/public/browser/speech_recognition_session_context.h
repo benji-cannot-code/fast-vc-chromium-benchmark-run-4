c1ea467d6e779777b89ed04fae59dc294c7eb9a7
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 17:26:08 +0000

    This patch does: # use MakeMediaRequest to query the permission for the devices. # Notify the UI on device opened and closed.
    BUG=146498,132965
    TEST=run chrome with --enable-scripted-speech, go to http://www.corp.google.com/~hwennborg/speechreco.html, test the behavior manually.
    
    Review URL: https://chromiumcodereview.appspot.com/11415097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169068 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/speech_recognition_session_context.h
+++ content/public/browser/speech_recognition_session_context.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "content/common/content_export.h"
+#include "content/public/common/media_stream_request.h"
 #include "ui/gfx/rect.h"
 
 namespace content {
@@ -39,6 +40,12 @@ struct CONTENT_EXPORT SpeechRecognitionSessionContext {
   // A texual description of the context (website, extension name) that is
   // requesting recognition, for prompting security notifications to the user.
   std::string context_name;
+
+  // The label for the permission request, it is used for request abortion.
+  std::string label;
+
+  // A list of devices being used by the recognition session.
+  MediaStreamDevices devices;
 };
 
 }  // namespace content
