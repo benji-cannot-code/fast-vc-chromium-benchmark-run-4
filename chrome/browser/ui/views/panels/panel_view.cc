1cc1a1760b2aa153271fb74a48df83368f535b9c
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 02:28:36 +0000

    Panel window size workaround code for win aura
    RootWindow can't be resized below a certain minimum size when
    it's an OVERLAPPED window, so change to a POPUP window when the
    size is below the threshold. This is what's done on native windows,
    this CL does it on aura windows too.
    
    R=ben@chromium.org
    TEST=browser_tests, PanelViewTest.CheckMinimizedHeight
    
    
    Review URL: https://chromiumcodereview.appspot.com/11417100
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168955 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/panels/panel_view.cc
+++ chrome/browser/ui/views/panels/panel_view.cc
@@ -32,6 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/icon_util.h"
 #endif
 
+#if defined(OS_WIN) && defined(USE_AURA)
+#include "ui/aura/root_window.h"
+#include "ui/aura/window.h"
+#endif
+
 namespace {
 
 // Supported accelerators.
@@ -311,7 +316,7 @@ void PanelView::AnimationProgressed(const ui::Animation* animation) {
 }
 
 void PanelView::SetWidgetBounds(const gfx::Rect& new_bounds) {
-#if defined(OS_WIN) && !defined(USE_ASH) && !defined(USE_AURA)
+#if defined(OS_WIN)
   // An overlapped window is a top-level window that has a titlebar, border,
   // and client area. The Windows system will automatically put the shadow
   // around the whole window. Also the system will enforce the minimum height
@@ -852,12 +857,18 @@ bool PanelView::IsWithinResizingArea(const gfx::Point& mouse_location) const {
          mouse_location.y() >= bounds.bottom() - kResizeInsideBoundsSize;
 }
 
-#if defined(OS_WIN) && !defined(USE_ASH) && !defined(USE_AURA)
+#if defined(OS_WIN)
 void PanelView::UpdateWindowAttribute(int attribute_index,
                                       int attribute_value_to_set,
                                       int attribute_value_to_reset,
                                       bool update_frame) {
-  gfx::NativeWindow native_window = window_->GetNativeWindow();
+  HWND native_window;
+#if defined(USE_AURA)
+  native_window =
+      window_->GetNativeWindow()->GetRootWindow()->GetAcceleratedWidget();
+#else
+  native_window = window_->GetNativeWindow();
+#endif
   int value = ::GetWindowLong(native_window, attribute_index);
   int expected_value = value;
   if (attribute_value_to_set)
