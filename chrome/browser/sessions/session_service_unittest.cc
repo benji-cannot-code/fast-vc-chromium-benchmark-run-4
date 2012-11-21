4e3d26c8a458df8d5095c5f641e5b9739479ab9c
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 06:59:48 +0000

    Attempt 2 at: Tweak session restore ifdef to be chromeos specific. I needed to ifdef a test too.
    BUG=none
    TEST=none
    R=davemoore@chromium.org
    TBR=davemoore@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11299130
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168997 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_service_unittest.cc
+++ chrome/browser/sessions/session_service_unittest.cc
@@ -526,8 +526,8 @@ TEST_F(SessionServiceTest, RestorePopup) {
   helper_.AssertNavigationEquals(nav2, tab->navigations[0]);
 }
 
-#if defined (USE_AURA)
-// Makes sure we track apps. Only applicable on aura.
+#if defined (OS_CHROMEOS)
+// Makes sure we track apps. Only applicable on chromeos.
 TEST_F(SessionServiceTest, RestoreApp) {
   SessionID window2_id;
   SessionID tab_id;
@@ -577,7 +577,7 @@ TEST_F(SessionServiceTest, RestoreApp) {
   helper_.AssertTabEquals(window2_id, tab2_id, 0, 0, 1, *tab);
   helper_.AssertNavigationEquals(nav2, tab->navigations[0]);
 }
-#endif  // defined (USE_AURA)
+#endif  // defined (OS_CHROMEOS)
 
 // Tests pruning from the front.
 TEST_F(SessionServiceTest, PruneFromFront) {
