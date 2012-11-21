1f153f67867d15a110b89350566a0aeae75b1d33
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 17:52:03 +0000

    Pass extensions' Content Security Policy to WebKit when using isolated worlds.
    http://webkit.org/b/97398 added the ability for script in an isolated world to
    bypass the main world's Content Security Policy by setting a new policy on the
    isolated world. This new policy is not yet enforced, but that's the end goal.
    
    This CL uses the new API methods that landed in that WebKit patch to associate
    a policy with the isolated world created for extensions' content scripts. This
    should allow extensions free reign on a website, regardless of it's defined
    CSP. A future patch on the WebKit side will allow the isolated world to enforce
    the CSP that's passed in, which will give extensions a clearly understandable
    policy that's the same everywhere.
    
    BUG=158716
    
    
    Review URL: https://chromiumcodereview.appspot.com/11366003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169071 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/content_scripts/bypass_page_csp/a.js
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// Inject an image.
+var img = document.createElement('img');
+img.onload = function () {
+  chrome.extension.connect().postMessage(true);
+};
+img.onerror = function () {
+  chrome.extension.connect().postMessage(false);
+};
+img.src = 'icon3.png';
+document.body.appendChild(img);
