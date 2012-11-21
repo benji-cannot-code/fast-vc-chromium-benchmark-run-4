28c0f7bbf7cfb8fb2141e7884ca1d5545a8fa0e4
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 18:55:58 +0000

    f
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169081 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/render_surface_impl.cc
+++ cc/render_surface_impl.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/render_pass_draw_quad.h"
 #include "cc/render_pass_sink.h"
 #include "cc/shared_quad_state.h"
+#include "third_party/skia/include/core/SkImageFilter.h"
 #include "ui/gfx/rect_conversions.h"
 #include <public/WebTransformationMatrix.h>
 
