0ec8e5e687e54ef43df3ccd4e4be59b1b453d278
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-21 23:21:41 +0000

    Coordinated Graphics: refactor changing children code in CoordinatedGraphicsLayer. https://bugs.webkit.org/show_bug.cgi?id=102896
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-11-21
    Reviewed by Noam Rosenthal.
    
    When changing children, we don't need to sync a layer state because
    there is no more need to send the parent ID every time the layer tree
    hierarchy changes. So this patch removes didChangeLayerState() in
    methods which change children.
    
    In addition, purgeBackingStores() does not need to call didChangeChildren()
    because purgeBackingStores() does not change children.
    
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::setChildren):
    (WebCore::CoordinatedGraphicsLayer::addChild):
    (WebCore::CoordinatedGraphicsLayer::addChildAtIndex):
    (WebCore::CoordinatedGraphicsLayer::addChildAbove):
    (WebCore::CoordinatedGraphicsLayer::addChildBelow):
    (WebCore::CoordinatedGraphicsLayer::replaceChild):
    (WebCore::CoordinatedGraphicsLayer::removeFromParent):
    (WebCore::CoordinatedGraphicsLayer::purgeBackingStores):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135443 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp
@@ -147,10 +147,6 @@ bool CoordinatedGraphicsLayer::setChildren(const Vector<GraphicsLayer*>& childre
     bool ok = GraphicsLayer::setChildren(children);
     if (!ok)
         return false;
-    for (unsigned i = 0; i < children.size(); ++i) {
-        CoordinatedGraphicsLayer* child = toCoordinatedGraphicsLayer(children[i]);
-        child->didChangeLayerState();
-    }
     didChangeChildren();
     return true;
 }
@@ -158,28 +154,24 @@ bool CoordinatedGraphicsLayer::setChildren(const Vector<GraphicsLayer*>& childre
 void CoordinatedGraphicsLayer::addChild(GraphicsLayer* layer)
 {
     GraphicsLayer::addChild(layer);
-    toCoordinatedGraphicsLayer(layer)->didChangeLayerState();
     didChangeChildren();
 }
 
 void CoordinatedGraphicsLayer::addChildAtIndex(GraphicsLayer* layer, int index)
 {
     GraphicsLayer::addChildAtIndex(layer, index);
-    toCoordinatedGraphicsLayer(layer)->didChangeLayerState();
     didChangeChildren();
 }
 
 void CoordinatedGraphicsLayer::addChildAbove(GraphicsLayer* layer, GraphicsLayer* sibling)
 {
     GraphicsLayer::addChildAbove(layer, sibling);
-    toCoordinatedGraphicsLayer(layer)->didChangeLayerState();
     didChangeChildren();
 }
 
 void CoordinatedGraphicsLayer::addChildBelow(GraphicsLayer* layer, GraphicsLayer* sibling)
 {
     GraphicsLayer::addChildBelow(layer, sibling);
-    toCoordinatedGraphicsLayer(layer)->didChangeLayerState();
     didChangeChildren();
 }
 
@@ -189,8 +181,6 @@ bool CoordinatedGraphicsLayer::replaceChild(GraphicsLayer* oldChild, GraphicsLay
     if (!ok)
         return false;
     didChangeChildren();
-    toCoordinatedGraphicsLayer(oldChild)->didChangeLayerState();
-    toCoordinatedGraphicsLayer(newChild)->didChangeLayerState();
     return true;
 }
 
@@ -199,8 +189,6 @@ void CoordinatedGraphicsLayer::removeFromParent()
     if (CoordinatedGraphicsLayer* parentLayer = toCoordinatedGraphicsLayer(parent()))
         parentLayer->didChangeChildren();
     GraphicsLayer::removeFromParent();
-
-    didChangeLayerState();
 }
 
 void CoordinatedGraphicsLayer::setPosition(const FloatPoint& p)
@@ -790,7 +778,6 @@ void CoordinatedGraphicsLayer::purgeBackingStores()
     releaseImageBackingIfNeeded();
 
     didChangeLayerState();
-    didChangeChildren();
 }
 
 void CoordinatedGraphicsLayer::setCoordinator(WebKit::CoordinatedGraphicsLayerClient* coordinator)
