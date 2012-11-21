c6218eff4b7bdde20675b768f23c78e1979aee63
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-21 20:29:21 +0000

    Remove $outputDir and $outputHeadersDir from global variables of code generators https://bugs.webkit.org/show_bug.cgi?id=102492
    Reviewed by Adam Barth.
    
    $outputDir and $outputHeadersDir are used in WriteData() only.
    We don't need to use global variables for them.
    
    No tests. No change in behavior.
    
    * bindings/scripts/CodeGenerator.pm:
    (ProcessDocument):
    (FileNamePrefix):
    * bindings/scripts/CodeGeneratorCPP.pm:
    (new):
    (GenerateInterface):
    (WriteData):
    * bindings/scripts/CodeGeneratorGObject.pm:
    (new):
    (WriteData):
    (GenerateInterface):
    * bindings/scripts/CodeGeneratorJS.pm:
    (new):
    (GenerateInterface):
    (WriteData):
    * bindings/scripts/CodeGeneratorObjC.pm:
    (new):
    (GenerateInterface):
    (WriteData):
    * bindings/scripts/CodeGeneratorV8.pm:
    (new):
    (GenerateInterface):
    (WriteData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135426 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorGObject.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorGObject.pm
@@ -46,8 +46,6 @@ sub new {
     my $reference = { };
 
     $codeGenerator = shift;
-    $outputDir = shift;
-    mkdir $outputDir;
 
     bless($reference, $object);
 }
@@ -1317,10 +1315,11 @@ EOF
     $object->GenerateEndHeader();
 }
 
-# Internal helper
 sub WriteData {
     my $object = shift;
     my $interface = shift;
+    my $outputDir = shift;
+    mkdir $outputDir;
 
     # Write a private header.
     my $interfaceName = $interface->name;
@@ -1434,7 +1433,6 @@ sub GenerateInterface {
     $className = GetClassName($interface->name);
 
     $object->Generate($interface);
-    $object->WriteData($interface);
 }
 
 1;
