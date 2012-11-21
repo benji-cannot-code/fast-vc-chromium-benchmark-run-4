90b2cdafd3bd4353a1feaea63c296dd414d0d8a7
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 15:59:57 +0000

    JS hooks for ChromeOS video player and audio player
    BUG=138666
    
    
    Review URL: https://chromiumcodereview.appspot.com/11411089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169052 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
+++ chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
@@ -13,4 +13,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="../file_type.js"/>
 //<include src="media_controls.js"/>
 //<include src="audio_player.js"/>
+//<include src="player_testapi.js"/>
 //<include src="../metadata/metadata_cache.js"/>
