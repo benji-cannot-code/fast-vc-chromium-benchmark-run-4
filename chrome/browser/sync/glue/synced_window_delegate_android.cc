307085fcb33873275d707f157ff17645e82405cc
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 00:30:04 +0000

    Refactor TabModel to not hold a profile.
    If we close all of the tabs in the incognito profile, the profile goes
    away. We need to not cache the profile as we have a bad pointer.
    
    BUG=
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11417089
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@168930 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/synced_window_delegate_android.cc
+++ chrome/browser/sync/glue/synced_window_delegate_android.cc
@@ -45,7 +45,7 @@ SyncedWindowDelegateAndroid::SyncedWindowDelegateAndroid(
 SyncedWindowDelegateAndroid::~SyncedWindowDelegateAndroid() {}
 
 bool SyncedWindowDelegateAndroid::HasWindow() const {
-  return !tab_model_->GetProfile()->IsOffTheRecord();
+  return !tab_model_->IsOffTheRecord();
 }
 
 SessionID::id_type SyncedWindowDelegateAndroid::GetSessionId() const {
