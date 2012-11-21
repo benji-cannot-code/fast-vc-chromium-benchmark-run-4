df952d6a1bcaf198cc1c0ed993cba0b5f40d2b7c
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 07:54:40 +0000

    Move UpdateFileByResourceId into its own drive operation class
    TBR=sky@chromium.org
    BUG=161966
    TEST=Run unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/11308119
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169002 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system/drive_operations.cc
+++ chrome/browser/chromeos/drive/file_system/drive_operations.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/file_system/copy_operation.h"
 #include "chrome/browser/chromeos/drive/file_system/move_operation.h"
 #include "chrome/browser/chromeos/drive/file_system/remove_operation.h"
+#include "chrome/browser/chromeos/drive/file_system/update_operation.h"
 
 namespace drive {
 namespace file_system {
@@ -39,14 +40,21 @@ void DriveOperations::Init(
                                                            cache,
                                                            metadata,
                                                            observer));
+  update_operation_.reset(new file_system::UpdateOperation(cache,
+                                                           metadata,
+                                                           uploader,
+                                                           blocking_task_runner,
+                                                           observer));
 }
 
 void DriveOperations::InitForTesting(CopyOperation* copy_operation,
                                      MoveOperation* move_operation,
-                                     RemoveOperation* remove_operation) {
+                                     RemoveOperation* remove_operation,
+                                     UpdateOperation* update_operation) {
   copy_operation_.reset(copy_operation);
   move_operation_.reset(move_operation);
   remove_operation_.reset(remove_operation);
+  update_operation_.reset(update_operation);
 }
 
 void DriveOperations::Copy(const FilePath& src_file_path,
@@ -93,5 +101,12 @@ void DriveOperations::Remove(const FilePath& file_path,
                              const FileOperationCallback& callback) {
   remove_operation_->Remove(file_path, is_recursive, callback);
 }
+
+void DriveOperations::UpdateFileByResourceId(
+    const std::string& resource_id,
+    const FileOperationCallback& callback) {
+  update_operation_->UpdateFileByResourceId(resource_id, callback);
+}
+
 }  // namespace file_system
 }  // namespace drive
--- chrome/browser/chromeos/drive/file_system/drive_operations.h
+++ chrome/browser/chromeos/drive/file_system/drive_operations.h
@@ -28,6 +28,7 @@ class CopyOperation;
 class MoveOperation;
 class OperationObserver;
 class RemoveOperation;
+class UpdateOperation;
 
 // Passes notifications from Drive operations back to the file system.
 class DriveOperations {
@@ -47,7 +48,8 @@ class DriveOperations {
   // Initializes the operation pointers.  For testing only.
   void InitForTesting(CopyOperation* copy_operation,
                       MoveOperation* move_operation,
-                      RemoveOperation* remove_operation);
+                      RemoveOperation* remove_operation,
+                      UpdateOperation* update_operation);
 
   // Wrapper function for copy_operation_.
   void Copy(const FilePath& src_file_path,
@@ -79,10 +81,15 @@ class DriveOperations {
               bool is_recursive,
               const FileOperationCallback& callback);
 
+  // Wrapper function for update_operation_.
+  void UpdateFileByResourceId(const std::string& resource_id,
+                              const FileOperationCallback& callback);
+
  private:
   scoped_ptr<CopyOperation> copy_operation_;
   scoped_ptr<MoveOperation> move_operation_;
   scoped_ptr<RemoveOperation> remove_operation_;
+  scoped_ptr<UpdateOperation> update_operation_;
 };
 
 }  // namespace file_system
