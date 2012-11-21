56df3d7109bba32b5d55c942beaf6e9bbeb2a55e
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-21 18:05:01 +0000

    Fix regression in button padding.
    BUG=161983
    TEST=see bug
    R=wittman@chromium.org
    TBR=wittman@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11299135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169074 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/button/text_button.cc
+++ ui/views/controls/button/text_button.cc
@@ -116,13 +116,13 @@ TextButtonDefaultBorder::TextButtonDefaultBorder()
     : vertical_padding_(kPreferredPaddingVertical) {
   set_hot_set(BorderImages(BorderImages::kHot));
   set_pushed_set(BorderImages(BorderImages::kPushed));
-}
-
-TextButtonDefaultBorder::~TextButtonDefaultBorder() {
   SetInsets(gfx::Insets(vertical_padding_,
                         kPreferredPaddingHorizontal,
                         vertical_padding_,
                         kPreferredPaddingHorizontal));
+}
+
+TextButtonDefaultBorder::~TextButtonDefaultBorder() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
