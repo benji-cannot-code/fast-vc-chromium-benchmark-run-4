96ae7727aebfb331271f561581f7b57ee0ac30e8
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-22 14:30:41 +0000

    overscroll: Allow turning off overscroll in some places.
    Ask the WebContentsDelegate before initiating overscroll. This allows turning
    overscroll off in login/lock screen etc.
    
    BUG=160668
    
    Review URL: https://codereview.chromium.org/11280124
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169263 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/web_contents_delegate.cc
+++ content/public/browser/web_contents_delegate.cc
@@ -32,6 +32,8 @@ bool WebContentsDelegate::IsPopupOrPanel(const WebContents* source) const {
 
 bool WebContentsDelegate::CanLoadDataURLsInWebUI() const { return false; }
 
+bool WebContentsDelegate::CanOverscrollContent() const { return false; }
+
 gfx::Rect WebContentsDelegate::GetRootWindowResizerRect() const {
   return gfx::Rect();
 }
