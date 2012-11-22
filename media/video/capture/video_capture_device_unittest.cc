51c83662084bcaf3588ccd4ab96d0148e5f1fde9
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-22 11:51:37 +0000

    Get the VideoCaptureDeviceTest tests running on windows (again?).
    TEST=run "media_unittests --gtest_filter=VideoCaptureDeviceTest*" on Windows.
        Before all of the tests would succeed, but actually not test anything
        since COM wasn't initialized.
    BUG=162221
    TBR=dalecurtis
    
    Review URL: https://chromiumcodereview.appspot.com/11348178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169255 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/video_capture_device_unittest.cc
+++ media/video/capture/video_capture_device_unittest.cc
@@ -14,12 +14,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
+#if defined(OS_WIN)
+#include "base/win/scoped_com_initializer.h"
+#endif
+
 #if defined(OS_MACOSX)
 // Mac/QTKit will always give you the size you ask for and this case will fail.
 #define MAYBE_AllocateBadSize DISABLED_AllocateBadSize
 // We will always get ARGB from the Mac/QTKit implementation.
 #define MAYBE_MJPEG DISABLED_CaptureMjpeg
-#elif defined(OS_WINDOWS)
+#elif defined(OS_WIN)
 #define MAYBE_AllocateBadSize AllocateBadSizei
 // Windows currently uses DirectShow to convert from MJPEG and a raw format is
 // always delivered.
@@ -81,6 +85,9 @@ class VideoCaptureDeviceTest : public testing::Test {
   virtual void TearDown() {
   }
 
+#if defined(OS_WIN)
+  base::win::ScopedCOMInitializer initialize_com_;
+#endif
   base::WaitableEvent wait_event_;
   scoped_ptr<MockFrameObserver> frame_observer_;
   VideoCaptureDevice::Names names_;
