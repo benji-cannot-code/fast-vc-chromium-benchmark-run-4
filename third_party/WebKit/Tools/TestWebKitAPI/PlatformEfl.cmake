ce987b55c0088b69ba96c2da9778352eb2014954
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-22 08:30:17 +0000

    [EFL][WK2] Enable WKPreferences and AboutBlankLoad API tests https://bugs.webkit.org/show_bug.cgi?id=102926
    Patch by Sudarsana Nagineni <sudarsana.nagineni@intel.com> on 2012-11-22
    Reviewed by Kenneth Rohde Christiansen.
    
    Add missing bits needed by WKPreferences and AboutBlankLoad API
    tests in the test harness and enable these tests on EFL port.
    
    * TestWebKitAPI/PlatformEfl.cmake:
    * TestWebKitAPI/Tests/WebKit2/WKPreferences.cpp:
    (TestWebKitAPI::TEST):
    * TestWebKitAPI/efl/PlatformUtilities.cpp:
    (TestWebKitAPI::Util::MIMETypeForWKURLResponse):
    (Util):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135489 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/PlatformEfl.cmake
+++ third_party/WebKit/Tools/TestWebKitAPI/PlatformEfl.cmake
@@ -45,6 +45,7 @@ SET(webkit2_api_harness_SOURCES
 # Release builds before adding it to test_webkit2_api_BINARIES.
 
 SET(test_webkit2_api_BINARIES
+    AboutBlankLoad
     CookieManager
     DOMWindowExtensionNoCache
     DocumentStartUserScriptAlertCrash
@@ -67,6 +68,7 @@ SET(test_webkit2_api_BINARIES
     PreventEmptyUserAgent
     PrivateBrowsingPushStateNoHistoryCallback
     WKConnection
+    WKPreferences
     WKString
     WKStringJSString
     WKURL
@@ -88,12 +90,10 @@ ADD_DEFINITIONS(-DTHEME_DIR="${THEME_BINARY_DIR}")
 
 # Tests disabled because of missing features on the test harness:
 #
-#   AboutBlankLoad
 #   HitTestResultNodeHandle
 #   MouseMoveAfterCrash
 #   ResponsivenessTimerDoesntFireEarly
 #   SpacebarScrolling
-#   WKPreferences
 #
 # Flaky test, fails on Release but passes on Debug:
 #
