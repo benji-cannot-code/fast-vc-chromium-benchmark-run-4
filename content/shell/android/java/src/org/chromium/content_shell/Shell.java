848f421dd6263901940c1a127a2deec76ecda26b
jinsukkim@google.com <jinsukkim@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-22 07:44:17 +0000

    Start hooking up element.requestFullscreen() for content shell
    Plumbs through the required mechanism in web_contents_delegate.cc.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11410078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169231 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/android/java/src/org/chromium/content_shell/Shell.java
+++ content/shell/android/java/src/org/chromium/content_shell/Shell.java
@@ -192,6 +192,15 @@ public class Shell extends LinearLayout {
         if (progress == 1.0) postDelayed(mClearProgressRunnable, COMPLETED_PROGRESS_TIMEOUT_MS);
     }
 
+    @CalledByNative
+    private void toggleFullscreenModeForTab(boolean enterFullscreen) {
+    }
+
+    @CalledByNative
+    private boolean isFullscreenForTabOrPending() {
+        return false;
+    }
+
     @SuppressWarnings("unused")
     @CalledByNative
     private void setIsLoading(boolean loading) {
