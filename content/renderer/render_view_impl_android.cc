e0ffd2785b6e7b0000cd127eaadd94c0e02637e1
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-22 03:15:45 +0000

    Add page scale min/max to UpdateFrameInfo.
    This information wasn't getting propagated to Android's ContentViewCore,
    causing a bug with double-tap zooming.
    
    BUG=161934
    
    
    Review URL: https://chromiumcodereview.appspot.com/11299144
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169201 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl_android.cc
+++ content/renderer/render_view_impl_android.cc
@@ -30,6 +30,8 @@ void RenderViewImpl::SendUpdateFrameInfo() {
       routing_id_,
       GetScrollOffset(),
       webview()->pageScaleFactor(),
+      webview()->minimumPageScaleFactor(),
+      webview()->maximumPageScaleFactor(),
       gfx::Size(webview()->mainFrame()->contentsSize())));
 }
 
