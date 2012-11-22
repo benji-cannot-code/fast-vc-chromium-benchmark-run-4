6e3639ffe1c8945ba9b547734e6141abd729b843
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-22 07:35:09 +0000

    [V8] WebCoreStringResourceBase should be located in V8StringResource.h https://bugs.webkit.org/show_bug.cgi?id=103002
    Reviewed by Adam Barth.
    
    Currently WebCoreStringResourceBase is mis-located in V8ValueCache.h.
    It should be in V8StringResource.h.
    
    No tests. No change in behavior.
    
    * bindings/v8/V8StringResource.cpp:
    (WebCore::WebCoreStringResourceBase::toWebCoreStringResourceBase):
    (WebCore):
    (WebCore::WebCoreStringResourceBase::visitStrings):
    * bindings/v8/V8StringResource.h:
    (WebCore):
    (WebCoreStringResourceBase):
    (WebCore::WebCoreStringResourceBase::WebCoreStringResourceBase):
    (WebCore::WebCoreStringResourceBase::~WebCoreStringResourceBase):
    (WebCore::WebCoreStringResourceBase::webcoreString):
    (WebCore::WebCoreStringResourceBase::atomicString):
    (WebCore::WebCoreStringResourceBase::memoryConsumption):
    (WebCoreStringResource16):
    (WebCore::WebCoreStringResource16::WebCoreStringResource16):
    (WebCoreStringResource8):
    (WebCore::WebCoreStringResource8::WebCoreStringResource8):
    * bindings/v8/V8ValueCache.cpp:
    * bindings/v8/V8ValueCache.h:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8ValueCache.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8ValueCache.cpp
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8ValueCache.h"
 
-#include "BindingVisitors.h"
 #include "V8Binding.h"
 
 namespace WebCore {
@@ -100,24 +99,6 @@ v8::Local<v8::String> StringCache::v8ExternalStringSlow(StringImpl* stringImpl, 
     m_lastV8String = wrapper;
 
     return newString;
-}
-
-WebCoreStringResourceBase* WebCoreStringResourceBase::toWebCoreStringResourceBase(v8::Handle<v8::String> string)
-{
-    v8::String::Encoding encoding;
-    v8::String::ExternalStringResourceBase* resource = string->GetExternalStringResourceBase(&encoding);
-    if (!resource)
-        return 0;
-    if (encoding == v8::String::ASCII_ENCODING)
-        return static_cast<WebCoreStringResource8*>(resource);
-    return static_cast<WebCoreStringResource16*>(resource);
-}
-
-void WebCoreStringResourceBase::visitStrings(ExternalStringVisitor* visitor)
-{
-    visitor->visitJSExternalString(m_plainString.impl());
-    if (m_plainString.impl() != m_atomicString.impl() && !m_atomicString.isNull())
-        visitor->visitJSExternalString(m_atomicString.impl());
 }
 
 void IntegerCache::createSmallIntegers(v8::Isolate* isolate)
