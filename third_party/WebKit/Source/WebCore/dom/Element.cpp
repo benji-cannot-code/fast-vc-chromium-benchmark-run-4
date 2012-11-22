619bdcf893eb1c40ab9ac5788f735d780a3b4a21
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-22 03:15:14 +0000

    [Shadow DOM] Implement Element::createShadowRoot() https://bugs.webkit.org/show_bug.cgi?id=102911
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    Added an API implementation and exposed it.
    
    This is basically an alias of the ShadowRoot constructor, which
    will be removed as bug 102913.
    
    Test: fast/dom/shadow/shadow-aware-create-shdow-root.html
    
    * dom/Element.cpp:
    (WebCore::Element::createShadowRoot):
    (WebCore):
    * dom/Element.h:
    (Element):
    * dom/Element.idl:
    
    LayoutTests:
    
    The coverage might not seem comprehensive at a glance. However,
    this is just an alias of ShadowRoot constructor and there are
    bunch of test cases which cover it.
    
    As bug 102913 will convert such callsites to use createShadowRoot(),
    the API will get be covered well then.
    
    * fast/dom/shadow/shadow-aware-create-shdow-root-expected.txt: Added.
    * fast/dom/shadow/shadow-aware-create-shdow-root.html: Added.
      Further ShadowAware API will come here.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135464 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -1430,6 +1430,11 @@ ElementShadow* Element::ensureShadow()
     return data->m_shadow.get();
 }
 
+PassRefPtr<ShadowRoot> Element::createShadowRoot(ExceptionCode& ec)
+{
+    return ShadowRoot::create(this, ec);
+}
+
 ShadowRoot* Element::userAgentShadowRoot() const
 {
     if (ElementShadow* elementShadow = shadow()) {
