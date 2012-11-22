2cdabccc20a6d6203d4a2698678b8f73f71e7fa9
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-22 13:45:59 +0000

    INPUT_MULTIPLE_FIELDS_UI: Refactoring: Do not call updateInnerTextValue if only read-only sub-fields have values https://bugs.webkit.org/show_bug.cgi?id=103033
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    input[type=time] can have read-only sub fields. If only read-only fields
    have values, we don't need to call updateInnerTextValue when
    HTMLInputElement::value is set to "".
    
    Tests: this patch doesn't change any visible behavior, but we add test cases to
    fast/forms/time-multiple-fields/time-multiple-fields-value-set-empty.html
    just in case.
    
    * html/shadow/DateTimeEditElement.h:
    (DateTimeEditElement): Declare anyEditableFieldsHaveValues.
    * html/shadow/DateTimeEditElement.cpp:
    (WebCore::DateTimeEditElement::anyEditableFieldsHaveValues):
    Added. This function checks value existence against only editable sub-fields.
    * html/BaseMultipleFieldsDateAndTimeInputType.cpp:
    (WebCore::BaseMultipleFieldsDateAndTimeInputType::setValue):
    Call DateTimeEditElement::anyEditableFieldsHaveValues() instead of
    DateTimeFieldsState::hasAnyValue().
    * html/DateTimeFieldsState.h: Remove hasAnyValue.
    
    LayoutTests:
    
    * fast/forms/time-multiple-fields/time-multiple-fields-value-set-empty-expected.txt:
    * fast/forms/time-multiple-fields/time-multiple-fields-value-set-empty.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/DateTimeEditElement.h
+++ third_party/WebKit/Source/WebCore/html/shadow/DateTimeEditElement.h
@@ -87,6 +87,7 @@ public:
 
     virtual ~DateTimeEditElement();
     void addField(PassRefPtr<DateTimeFieldElement>);
+    bool anyEditableFieldsHaveValues() const;
     void blurByOwner();
     virtual void defaultEventHandler(Event*) OVERRIDE;
     void disabledStateChanged();
