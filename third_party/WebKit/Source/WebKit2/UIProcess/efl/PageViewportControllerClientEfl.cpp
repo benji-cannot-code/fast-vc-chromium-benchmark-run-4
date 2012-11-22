16862c19555c5c9a5c516efca62e71fd59ee60ee
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-22 00:20:42 +0000

    [EFL][WK2] Remove #include "ewk_view_private.h" in PageViewportControllerClientEfl.cpp https://bugs.webkit.org/show_bug.cgi?id=102773
    Patch by Jaehun Lim <ljaehun.lim@samsung.com> on 2012-11-21
    Reviewed by Gyuyoung Kim.
    
    PageViewportControllerClientEfl.cpp doesn't need to include ewk_view_private.h.
    
    * UIProcess/efl/PageViewportControllerClientEfl.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135453 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/efl/PageViewportControllerClientEfl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/efl/PageViewportControllerClientEfl.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayerTreeRenderer.h"
 #include "PageViewportController.h"
 #include "TransformationMatrix.h"
-#include "ewk_view_private.h"
 
 using namespace WebCore;
 
