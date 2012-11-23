b86c469a639d876b16461c5ea3ad8d9b5ed74c3e
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-23 13:51:32 +0000

    Add policy to show a logout button in the ash system tray
    This CL adds a policy that, when enabled, adds a big, red logout button
    to the ash system tray.
    
    BUG=152929
    
    
    Review URL: https://chromiumcodereview.appspot.com/11411143
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169337 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_handler_list.cc
+++ chrome/browser/policy/configuration_policy_handler_list.cc
@@ -330,6 +330,9 @@ const PolicyToPreferenceMapEntry kSimplePolicyMap[] = {
   { key::kAudioCaptureAllowed,
     prefs::kAudioCaptureAllowed,
     Value::TYPE_BOOLEAN },
+  { key::kShowLogoutButtonInTray,
+    prefs::kShowLogoutButtonInTray,
+    Value::TYPE_BOOLEAN },
 #endif  // defined(OS_CHROMEOS)
 
 #if !defined(OS_MACOSX) && !defined(OS_CHROMEOS)
