a14e1c5d1caddca711ab5896fe99cd02eb78c792
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-23 11:15:59 +0000

    [chromium] Remove WebScreenInfo.{horizontal,vertical}DPI https://bugs.webkit.org/show_bug.cgi?id=101772
    Patch by Sami Kyostila <skyostil@chromium.org> on 2012-11-23
    Reviewed by Adam Barth.
    
    Now that nothing is using WebScreenInfo.{horizontal,vertical}DPI we can simply
    remove them.
    
    Source/Platform:
    
    * chromium/public/WebScreenInfo.h:
    (WebKit::WebScreenInfo::WebScreenInfo):
    
    Source/WebKit/chromium:
    
    * src/mac/WebScreenInfoFactory.mm:
    (WebKit::WebScreenInfoFactory::screenInfo):
    * src/win/WebScreenInfoFactory.cpp:
    (WebKit::WebScreenInfoFactory::screenInfo):
    * src/x11/WebScreenInfoFactory.cpp:
    (WebKit::WebScreenInfoFactory::screenInfo):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135580 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebScreenInfo.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebScreenInfo.h
@@ -37,12 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 struct WebScreenInfo {
-    // The horizontal screen dpi (deprecated).
-    int horizontalDPI;
-
-    // The vertical screen dpi (deprecated).
-    int verticalDPI;
-
     // Device scale factor. Specifies the ratio between physical and logical
     // pixels.
     float deviceScaleFactor;
@@ -75,9 +69,7 @@ struct WebScreenInfo {
     WebRect availableRect;
 
     WebScreenInfo()
-        : horizontalDPI(0)
-        , verticalDPI(0)
-        , deviceScaleFactor(1)
+        : deviceScaleFactor(1)
         , depth(0)
         , depthPerComponent(0)
         , isMonochrome(false) { }
