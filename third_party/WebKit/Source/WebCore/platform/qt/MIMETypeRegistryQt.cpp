dab0c9507b491ead6b4ca710b9974a8bf090e542
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-23 10:30:23 +0000

    [Qt] REGRESSION(r135507): It made 13 tests fail https://bugs.webkit.org/show_bug.cgi?id=103069
    Reviewed by Simon Hausmann.
    
    Source/WebCore:
    
    Fix regression in recognizing MHTML on some platforms.
    
    * platform/qt/MIMETypeRegistryQt.cpp:
    (ExtensionMap):
    (WebCore::MIMETypeRegistry::getMIMETypeForExtension):
    (WebCore::MIMETypeRegistry::getMIMETypeForPath):
    
    LayoutTests:
    
    Unskip fixed test and update one benign change.
    
    * platform/qt/TestExpectations:
    * platform/qt/fast/preloader/script-expected.txt: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135577 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/qt/MIMETypeRegistryQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/qt/MIMETypeRegistryQt.cpp
@@ -36,14 +36,37 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+struct ExtensionMap {
+    const char* extension;
+    const char* dotExtension;
+    const char* mimeType;
+};
+
+// This is a short list of extensions not recognized by the freedesktop shared mimetype database 1.0.
+static const ExtensionMap extensionMap[] = {
+    { "mht", ".mht", "application/x-mimearchive" },
+    { "mhtml", ".mhtml", "application/x-mimearchive" },
+    { "text", ".text", "text/plain" },
+    { "wmlc", ".wmlc", "application/vnd.wap.wmlc" },
+    { 0, 0, 0 }
+};
+
 String MIMETypeRegistry::getMIMETypeForExtension(const String &ext)
 {
     // QMimeDatabase lacks the ability to query by extension alone, so we create a fake filename to lookup.
-    const QString filename = QStringLiteral("filename.") + QString(ext.lower());
+    String suffix = ext.lower();
+    const QString filename = QStringLiteral("filename.") + QString(suffix);
 
     QMimeType mimeType = QMimeDatabase().mimeTypeForFile(filename, QMimeDatabase::MatchExtension);
     if (mimeType.isValid() && !mimeType.isDefault())
         return mimeType.name();
+
+    const ExtensionMap *e = extensionMap;
+    while (e->extension) {
+        if (suffix == e->extension)
+            return e->mimeType;
+        ++e;
+    }
 
     return String();
 }
@@ -51,8 +74,15 @@ String MIMETypeRegistry::getMIMETypeForExtension(const String &ext)
 String MIMETypeRegistry::getMIMETypeForPath(const String& path)
 {
     QMimeType type = QMimeDatabase().mimeTypeForFile(path, QMimeDatabase::MatchExtension);
-    if (type.isValid())
+    if (type.isValid() && !type.isDefault())
         return type.name();
+
+    const ExtensionMap *e = extensionMap;
+    while (e->extension) {
+        if (path.endsWith(e->dotExtension))
+            return e->mimeType;
+        ++e;
+    }
 
     return defaultMIMEType();
 }
