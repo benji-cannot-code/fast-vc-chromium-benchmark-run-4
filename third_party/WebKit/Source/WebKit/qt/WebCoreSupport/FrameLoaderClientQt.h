dec82c049f60c10352586f4d33881ac9a0a23d99
hausmann@webkit.org <hausmann@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-23 09:12:44 +0000

    Unreviewed trivial Qt/Win build fix:
    Include FormState.h/HTMLFormElement.h in the FrameLoaderClient
    declaration because MSVC needs it. Also fix Q_DECL_EXPORT declarations
    for functions that are supposed to be exported.
    
    * Api/qwebelement.cpp:
    * WebCoreSupport/FrameLoaderClientQt.h:
    * WebCoreSupport/InitWebCoreQt.h:
    (WebKit):
    * WebCoreSupport/InitWebKitQt.h:
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/FrameLoaderClientQt.h
@@ -31,9 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef FrameLoaderClientQt_h
 #define FrameLoaderClientQt_h
 
+#include "FormState.h"
 #include "Frame.h"
 #include "FrameLoader.h"
 #include "FrameLoaderClient.h"
+#include "HTMLFormElement.h"
 #include "KURL.h"
 #include "ResourceError.h"
 #include "ResourceResponse.h"
--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/InitWebKitQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/InitWebKitQt.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-void initializeWebKitWidgets();
+Q_DECL_EXPORT void initializeWebKitWidgets();
 
 }
 #endif
