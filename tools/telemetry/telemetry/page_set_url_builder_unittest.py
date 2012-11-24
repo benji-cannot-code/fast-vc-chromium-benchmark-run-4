49eb69fcc86290f995b3c7a2c395600249ef2fad
hartmanng@chromium.org <hartmanng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-24 03:27:26 +0000

    [telemetry] Updating scrolling page_sets to new interaction-based format.
    These are all the scrolling pagesets I know of, am I missing any?
    
    (Also a tiny bug in page_runner.py when dealing with 'file://' pages, not sure how long that's been around)
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11412152
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169377 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/page_set_url_builder_unittest.py
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+import unittest
+import urlparse
+
+from telemetry import page_set_url_builder
+
+class TestPageSetUrlBuilder(unittest.TestCase):
+  def testGetUrlBaseDirAndFileForAbsolutePath(self):
+    url = 'file:///somedir/otherdir/file.html'
+    parsed_url = urlparse.urlparse(url)
+    base_dir = 'basedir'
+    dirname, filename = page_set_url_builder.GetUrlBaseDirAndFile(base_dir,
+                                                                  parsed_url)
+    self.assertEqual(dirname, 'basedir/somedir/otherdir')
+    self.assertEqual(filename, 'file.html')
+
+  def testGetUrlBaseDirAndFileForRelativePath(self):
+    url = 'file:///../../otherdir/file.html'
+    parsed_url = urlparse.urlparse(url)
+    base_dir = 'basedir'
+    dirname, filename = page_set_url_builder.GetUrlBaseDirAndFile(base_dir,
+                                                                  parsed_url)
+    self.assertEqual(dirname, 'basedir/../../otherdir')
+    self.assertEqual(filename, 'file.html')
