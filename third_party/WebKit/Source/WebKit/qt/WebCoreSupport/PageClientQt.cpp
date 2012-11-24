1908348c59bcc102e330541a4a59782c7d055a9c
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-24 15:11:27 +0000

    Unreviewed Qt build fix:
    Make sure QGLWidget definition is available.
    
    * WebCoreSupport/PageClientQt.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PageClientQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/PageClientQt.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <QX11Info>
 #endif
 
-#ifdef QT_OPENGL_LIB
+#if USE(TEXTURE_MAPPER_GL)
 #include <QGLWidget>
 #endif
 
