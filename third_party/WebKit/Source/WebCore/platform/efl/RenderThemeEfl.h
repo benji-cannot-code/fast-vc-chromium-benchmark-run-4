2faf3aa38c3a19f9984c385cbf395656505b2aec
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-24 22:25:11 +0000

    [EFL] Refactor RenderThemeEfl::getThemePartFromCache() https://bugs.webkit.org/show_bug.cgi?id=103186
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2012-11-24
    Reviewed by Kenneth Rohde Christiansen.
    
    Refactor getThemePartFromCache() so that a list is used
    for the cache instead of a vector. The function moves
    items to the head of the container which is much more
    efficient with a list than a vector.
    
    The list ordering is also slightly altered so that the
    cached parts that were used last come first. Previously,
    they were sorted by creation time which may cause an
    item to be removed from the cache because it is older
    even if is was used recently.
    
    This patch also gets rid of some code duplication at the
    end of the function.
    
    No new tests, no behavior change.
    
    * platform/efl/RenderThemeEfl.cpp:
    (WebCore::RenderThemeEfl::ThemePartCacheEntry::create): Return
    a smart pointer instead of a raw pointer.
    (WebCore::RenderThemeEfl::getThemePartFromCache):
    (WebCore::RenderThemeEfl::clearThemePartCache): Rename
    flushThemePartCache to clearThemePartCache for clarity.
    (WebCore::RenderThemeEfl::loadTheme):
    (WebCore::RenderThemeEfl::RenderThemeEfl):
    (WebCore::RenderThemeEfl::~RenderThemeEfl):
    * platform/efl/RenderThemeEfl.h:
    (ThemePartCacheEntry):
    (RenderThemeEfl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.h
+++ third_party/WebKit/Source/WebCore/platform/efl/RenderThemeEfl.h
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #include "RenderTheme.h"
 
+#include <Eina.h>
 #include <cairo.h>
 #include <wtf/efl/RefPtrEfl.h>
 
@@ -264,7 +265,7 @@ private:
     void applyPartDescription(Evas_Object*, struct ThemePartDesc*);
 
     struct ThemePartCacheEntry {
-        static ThemePartCacheEntry* create(const String& themePath, FormType, const IntSize&);
+        static PassOwnPtr<RenderThemeEfl::ThemePartCacheEntry> create(const String& themePath, FormType, const IntSize&);
         void reuse(const String& themePath, FormType, const IntSize& = IntSize());
 
         ALWAYS_INLINE Ecore_Evas* canvas() { return m_canvas.get(); }
@@ -283,12 +284,13 @@ private:
 
     struct ThemePartDesc m_partDescs[FormTypeLast];
 
-    // this should be small and not so frequently used,
-    // so use a vector and do linear searches
-    Vector<ThemePartCacheEntry*> m_partCache;
+    // List of ThemePartCacheEntry* sorted so that the most recently
+    // used entries come first. We use a list for efficient moving
+    // of items within the container.
+    Eina_List* m_partCache;
 
     ThemePartCacheEntry* getThemePartFromCache(FormType, const IntSize&);
-    void flushThemePartCache();
+    void clearThemePartCache();
 };
 }
 
