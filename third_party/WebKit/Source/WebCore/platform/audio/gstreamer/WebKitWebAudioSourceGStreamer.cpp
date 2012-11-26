9f1d65b4087706b479d726d1053711d8a02d68dd
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 12:10:28 +0000

    [GStreamer] Floating reference handling fix https://bugs.webkit.org/show_bug.cgi?id=101349
    Patch by Thiago Santos <thiago.sousa.santos@collabora.com> on 2012-11-26
    Reviewed by Philippe Normand.
    
    GStreamer 0.10 and 1.0 differ when creating GstGhostPad from pad
    templates, the 1.0 doesn't take ownership on the passed
    GstPadTemplate, while 0.10 does. So this patch adds a
    GStreamerVersioning function to handle this different approach
    transparently in Webkit gstreamer elements.
    
    Existing media tests cover this change.
    
    * platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp:
    (webkit_web_audio_src_init):
    * platform/graphics/gstreamer/GStreamerVersioning.cpp:
    (webkitGstGhostPadFromStaticTemplate):
    * platform/graphics/gstreamer/GStreamerVersioning.h:
    * platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (webkit_web_src_init):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp
+++ third_party/WebKit/Source/WebCore/platform/audio/gstreamer/WebKitWebAudioSourceGStreamer.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "AudioIOCallback.h"
 #include <wtf/gobject/GOwnPtr.h>
 #include "GRefPtrGStreamer.h"
+#include "GStreamerVersioning.h"
 #include <gst/audio/multichannel.h>
 #include <gst/pbutils/pbutils.h>
 
@@ -179,8 +180,7 @@ static void webkit_web_audio_src_init(WebKitWebAudioSrc* src)
     src->priv = priv;
     new (priv) WebKitWebAudioSourcePrivate();
 
-    GRefPtr<GstPadTemplate> padTemplate = adoptGRef(gst_static_pad_template_get(&srcTemplate));
-    priv->sourcePad = gst_ghost_pad_new_no_target_from_template("src", padTemplate.get());
+    priv->sourcePad = webkitGstGhostPadFromStaticTemplate(&srcTemplate, "src", 0);
     gst_element_add_pad(GST_ELEMENT(src), priv->sourcePad);
 
     priv->provider = 0;
