c77da019606c5c50de769b77cf0943bace9d6292
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-26 18:20:19 +0000

    https://codereview.chromium.org/11364048/ increased the startup time on Mac by 40% since the enumeration API is done on the main thread.
    Revert this first and will reland it once the problem is resolved.
    
    TBR=thakis@chromium.org
    BUG=161583
    
    Review URL: https://codereview.chromium.org/11416184
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169449 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/media_internals.h
+++ chrome/browser/media/media_internals.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/media_observer.h"
 #include "content/public/common/media_stream_request.h"
 
-class MediaCaptureDevicesDispatcher;
 class MediaInternalsObserver;
 class MediaStreamCaptureIndicator;
 
@@ -51,10 +50,6 @@ class MediaInternals : public content::MediaObserver {
       int render_process_id,
       int render_view_id,
       const content::MediaStreamDevices& devices) OVERRIDE;
-  virtual void OnAudioCaptureDevicesChanged(
-      const content::MediaStreamDevices& devices) OVERRIDE;
-  virtual void OnVideoCaptureDevicesChanged(
-      const content::MediaStreamDevices& devices) OVERRIDE;
   virtual void OnMediaRequestStateChanged(
       int render_process_id,
       int render_view_id,
@@ -69,8 +64,6 @@ class MediaInternals : public content::MediaObserver {
   void SendEverything();
 
   scoped_refptr<MediaStreamCaptureIndicator> GetMediaStreamCaptureIndicator();
-  scoped_refptr<MediaCaptureDevicesDispatcher>
-      GetMediaCaptureDevicesDispatcher();
 
  private:
   friend class MediaInternalsTest;
@@ -98,7 +91,6 @@ class MediaInternals : public content::MediaObserver {
   DictionaryValue data_;
   ObserverList<MediaInternalsObserver> observers_;
   scoped_refptr<MediaStreamCaptureIndicator> media_stream_capture_indicator_;
-  scoped_refptr<MediaCaptureDevicesDispatcher> media_devices_dispatcher_;
 
   DISALLOW_COPY_AND_ASSIGN(MediaInternals);
 };
