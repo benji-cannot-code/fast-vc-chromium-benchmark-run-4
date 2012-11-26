95227c04131383bb81907bf281f707b6f66738fe
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-26 20:11:17 +0000

    Add a header file for ProfileShortcutManagerWin and various clean up.
    - Rename GetShortcutNameForProfile() to GetShortcutFilenameForProfile()
      and make it standalone in a namespace that is exposed for testing.
    - Remove various unused headers.
    - Various other clean ups
    
    BUG=none
    TEST=compiles and existing unit tests
    R=gab,sail
    TBR=sky
    Review URL: https://codereview.chromium.org/11414135
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_shortcut_manager_stub.cc
+++ chrome/browser/profiles/profile_shortcut_manager_stub.cc
@@ -15,10 +15,3 @@ ProfileShortcutManager* ProfileShortcutManager::Create(
     ProfileManager* manager) {
   return NULL;
 }
-
-// static
-string16 ProfileShortcutManager::GetShortcutNameForProfile(
-    const string16& profile_name) {
-  NOTREACHED();
-  return string16();
-}
