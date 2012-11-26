4035061eba8ca7283996e6df609b5d7386a8ac29
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 06:35:57 +0000

    Web Inspector: use native Element.prototype.normalize instead of custom defined Element.prototype.pruneEmptyTextNodes https://bugs.webkit.org/show_bug.cgi?id=103205
    Patch by Nikita Vasilyev <me@elv1s.ru> on 2012-11-25
    Reviewed by Pavel Feldman.
    
    * inspector/front-end/DOMExtension.js:
    * inspector/front-end/TextPrompt.js:
    (WebInspector.TextPrompt.prototype.clearAutoComplete):
    (WebInspector.TextPrompt.prototype._completionsReady):
    (WebInspector.TextPrompt.prototype.applySuggestion):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135683 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TextPrompt.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TextPrompt.js
@@ -328,7 +328,7 @@ WebInspector.TextPrompt.prototype = {
             return;
 
         this._userEnteredRange.deleteContents();
-        this._element.pruneEmptyTextNodes();
+        this._element.normalize();
 
         var userTextNode = document.createTextNode(this._userEnteredText);
         this._userEnteredRange.insertNode(userTextNode);
@@ -501,7 +501,7 @@ WebInspector.TextPrompt.prototype = {
         if (auto) {
             if (this.isCaretAtEndOfPrompt()) {
                 this._userEnteredRange.deleteContents();
-                this._element.pruneEmptyTextNodes();
+                this._element.normalize();
                 var finalSelectionRange = document.createRange();
                 var prefixText = completionText.substring(0, wordPrefixLength);
                 var suffixText = completionText.substring(wordPrefixLength);
@@ -550,7 +550,7 @@ WebInspector.TextPrompt.prototype = {
             wordPrefixLength = this._userEnteredText ? this._userEnteredText.length : 0;
 
         this._userEnteredRange.deleteContents();
-        this._element.pruneEmptyTextNodes();
+        this._element.normalize();
         var finalSelectionRange = document.createRange();
         var completionTextNode = document.createTextNode(completionText);
         this._userEnteredRange.insertNode(completionTextNode);
