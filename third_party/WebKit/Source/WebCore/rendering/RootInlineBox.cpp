0234f1cbff050005e20f96cf3a52693ff38790ce
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 20:00:49 +0000

    [CSS Regions] Add Region info for RootLineBoxes and pack the pagination data https://bugs.webkit.org/show_bug.cgi?id=101332
    Patch by Andrei Bucur <abucur@adobe.com> on 2012-11-26
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    Currently the pagination information for lines is spread between the RootInlineBox and InlineFlowBox classes, consuming memory even though
    the boxes were not the result of an pagination layout. To overcome this, a new struct (LineFragmentationData) is created that wraps all the data,
    including two new members, the containing Region for the line and a boolean that states if the line was laid out in a Region or not.
    The flag is necessary because the sanitize function on LineFragmentationData resets the containing Region to 0 if the Region was removed from
    chain (so a value of 0 for the containing Region means two things). The sanitize function should prevent access to an invalid address.
    The containing Region is used to detect if a line changed the Region where it resides. This will be helpful especially when implementing region
    styling for layout properties (e.g. the font-size property https://bugs.webkit.org/show_bug.cgi?id=95559 ).
    A line can change the region when it is shifted inside the containing block or if the entire block moves. This means it's better to delegate
    the task of updating the containing Region to the block.
    
    Tests: fast/regions/line-containing-region-crash.html
    
    * rendering/InlineFlowBox.cpp:
    (SameSizeAsInlineFlowBox):
    * rendering/InlineFlowBox.h:
    (WebCore::InlineFlowBox::InlineFlowBox):
    (InlineFlowBox):
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::lineWidthForPaginatedLineChanged):
    * rendering/RenderBlockLineLayout.cpp:
    (WebCore::RenderBlock::layoutRunsAndFloatsInRange):
    (WebCore::RenderBlock::linkToEndLineIfNeeded):
    (WebCore::RenderBlock::determineStartPosition):
    * rendering/RootInlineBox.cpp:
    (WebCore::RootInlineBox::RootInlineBox):
    (WebCore::RootInlineBox::setContainingRegion):
    (WebCore):
    (WebCore::RootInlineBox::LineFragmentationData::sanitize): This is an O(1) function that checks if the containig Region is still valid pointer.
    * rendering/RootInlineBox.h:
    (WebCore):
    (WebCore::RootInlineBox::paginationStrut):
    (WebCore::RootInlineBox::setPaginationStrut):
    (WebCore::RootInlineBox::isFirstAfterPageBreak):
    (WebCore::RootInlineBox::setIsFirstAfterPageBreak):
    (WebCore::RootInlineBox::paginatedLineWidth):
    (WebCore::RootInlineBox::setPaginatedLineWidth):
    (RootInlineBox):
    (WebCore::RootInlineBox::containingRegion):
    (WebCore::RootInlineBox::hasContainingRegion): Use this to determine if the line has a region or not.
    (WebCore::RootInlineBox::ensureLineFragmentationData):
    (LineFragmentationData):
    (WebCore::RootInlineBox::LineFragmentationData::LineFragmentationData):
    
    LayoutTests:
    
    The test checks if there is a crash when doing a line layout if:
    - the flow has no region
    - the flow has a region but the lines have no containing region
    - the flow has no region but the lines have a containing region
    
    * fast/regions/line-containing-region-crash-expected.txt: Added.
    * fast/regions/line-containing-region-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RootInlineBox.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RootInlineBox.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PaintInfo.h"
 #include "RenderArena.h"
 #include "RenderBlock.h"
+#include "RenderFlowThread.h"
 #include "RenderView.h"
 #include "VerticalPositionCache.h"
 #include <wtf/unicode/Unicode.h>
@@ -53,8 +54,6 @@ RootInlineBox::RootInlineBox(RenderBlock* block)
     , m_lineBottom(0)
     , m_lineTopWithLeading(0)
     , m_lineBottomWithLeading(0)
-    , m_paginationStrut(0)
-    , m_paginatedLineWidth(0)
 {
     setIsHorizontal(block->isHorizontalWritingMode());
 }
@@ -250,6 +249,30 @@ void RootInlineBox::childRemoved(InlineBox* box)
         prev->setLineBreakInfo(0, 0, BidiStatus());
         prev->markDirty();
     }
+}
+
+void RootInlineBox::setContainingRegion(RenderRegion* region)
+{
+    ASSERT(!isDirty());
+    ASSERT(block()->inRenderFlowThread());
+    LineFragmentationData* fragmentationData  = ensureLineFragmentationData();
+    fragmentationData->m_containingRegion = region;
+    fragmentationData->m_hasContainingRegion = !!region;
+}
+
+RootInlineBox::LineFragmentationData* RootInlineBox::LineFragmentationData::sanitize(const RenderBlock* block)
+{
+    ASSERT(block->inRenderFlowThread());
+    if (!m_containingRegion)
+        return this;
+
+    RenderFlowThread* flowThread = block->enclosingRenderFlowThread();
+    const RenderRegionList& regionList = flowThread->renderRegionList();
+    // For pointer types the hash function is |safeToCompareToEmptyOrDeleted|. There shouldn't be any problems if m_containingRegion was deleted.
+    if (!regionList.contains(m_containingRegion))
+        m_containingRegion = 0;
+
+    return this;
 }
 
 LayoutUnit RootInlineBox::alignBoxesInBlockDirection(LayoutUnit heightOfBlock, GlyphOverflowAndFallbackFontsMap& textBoxDataMap, VerticalPositionCache& verticalPositionCache)
