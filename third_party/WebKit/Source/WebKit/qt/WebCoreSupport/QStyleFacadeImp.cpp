a52b6d54056123138575f3b3279fd90950b2c1d2
michael.bruning@digia.com <michael.bruning@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 10:35:28 +0000

    [Qt] QStyleFacadeImp build break with latest Qt 5 https://bugs.webkit.org/show_bug.cgi?id=103198
    Reviewed by Simon Hausmann.
    
    Original patch by J-P Nurmi <jpnurmi@digia.com>.
    
    Fixes QtWebKit build by replacing qobject_cast to
    QMacStyle with calls to QObject::inherits. Also
    replaces Q_WS_MAC preprocesser directives with Q_OS_MAC
    for Qt 5 compatibility.
    
    * WebCoreSupport/QStyleFacadeImp.cpp:
    (WebKit::QStyleFacadeImp::getButtonMetrics):
    (WebKit::QStyleFacadeImp::paintComboBox):
    (WebKit::QStyleFacadeImp::paintInnerSpinButton):
    (WebKit::QStyleFacadeImp::paintScrollBar):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135696 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QStyleFacadeImp.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QStyleFacadeImp.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <QApplication>
 #include <QLineEdit>
-#include <QMacStyle>
 #include <QPainter>
 #include <QPushButton>
 #include <QStyleFactory>
@@ -222,7 +221,7 @@ void QStyleFacadeImp::getButtonMetrics(QString *buttonFontFamily, int *buttonFon
     QFont defaultButtonFont = QApplication::font(&button);
     *buttonFontFamily = defaultButtonFont.family();
     *buttonFontPixelSize = 0;
-#ifdef Q_WS_MAC
+#ifdef Q_OS_MAC
     button.setAttribute(Qt::WA_MacSmallSize);
     QFontInfo fontInfo(defaultButtonFont);
     *buttonFontPixelSize = fontInfo.pixelSize();
@@ -276,11 +275,11 @@ void QStyleFacadeImp::paintComboBox(QPainter *painter, const QStyleFacadeOption 
 
     IntRect rect = opt.rect;
 
-#if defined(Q_WS_MAC) && !defined(QT_NO_STYLE_MAC)
+#if defined(Q_OS_MAC) && !defined(QT_NO_STYLE_MAC)
     // QMacStyle makes the combo boxes a little bit smaller to leave space for the focus rect.
     // Because of it, the combo button is drawn at a point to the left of where it was expect to be and may end up
     // overlapped with the text. This will force QMacStyle to draw the combo box with the expected width.
-    if (qobject_cast<QMacStyle*>(m_style))
+    if (m_style->inherits("QMacStyle"))
         rect.inflateX(3);
 #endif
 
@@ -360,11 +359,11 @@ void QStyleFacadeImp::paintInnerSpinButton(QPainter* painter, const QStyleFacade
     // Default to moving the buttons a little bit within the editor frame.
     int inflateX = -2;
     int inflateY = -2;
-#if defined(Q_WS_MAC) && !defined(QT_NO_STYLE_MAC)
+#if defined(Q_OS_MAC) && !defined(QT_NO_STYLE_MAC)
     // QMacStyle will position the aqua buttons flush to the right.
     // This will move them more within the control for better style, a la
     // Chromium look & feel.
-    if (qobject_cast<QMacStyle*>(m_style)) {
+    if (m_style->inherits("QMacStyle")) {
         inflateX = -4;
         // Render mini aqua spin buttons for QMacStyle to fit nicely into
         // the editor area, like Chromium.
@@ -452,10 +451,10 @@ void QStyleFacadeImp::paintScrollBar(QPainter *painter, const QStyleFacadeOption
 
     MappedStyleOption<QStyleOptionSlider> opt(widget, proxyOption);
 
-#ifdef Q_WS_MAC
+#ifdef Q_OS_MAC
     // FIXME: We also need to check the widget style but today ScrollbarTheme is not aware of the page so we
     // can't get the widget.
-    if (qobject_cast<QMacStyle*>(m_style))
+    if (m_style->inherits("QMacStyle"))
         m_style->drawComplexControl(QStyle::CC_ScrollBar, &opt, painter, widget);
     else
 #endif
