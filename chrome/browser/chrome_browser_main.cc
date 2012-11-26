3f47dcd601e9a3dd13b3040c11a8c2d24851c486
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-26 19:22:11 +0000

    Disable VariationsService for non-GOOGLE_CHROME_BUILD builds.
    BUG=162231
    TEST=Run non-Official Chromium build and ensure no server-side
    only trials are reflected in chrome://version/
    
    Review URL: https://chromiumcodereview.appspot.com/11419142
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169459 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -584,7 +584,8 @@ void ChromeBrowserMainParts::SetupMetricsAndFieldTrials() {
 
   chrome_variations::VariationsService* variations_service =
       browser_process_->variations_service();
-  variations_service->CreateTrialsFromSeed(browser_process_->local_state());
+  if (variations_service)
+    variations_service->CreateTrialsFromSeed(browser_process_->local_state());
 
   const int64 install_date = local_state_->GetInt64(
       prefs::kUninstallMetricsInstallDate);
@@ -1485,8 +1486,11 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
     // StartRepeatedVariationsSeedFetch.
     if (parameters().ui_task == NULL) {
       // Request new variations seed information from server.
-      browser_process_->variations_service()->
-          StartRepeatedVariationsSeedFetch();
+      chrome_variations::VariationsService* variations_service =
+          browser_process_->variations_service();
+      if (variations_service)
+        variations_service->StartRepeatedVariationsSeedFetch();
+
 #if !defined(OS_CHROMEOS)
       // TODO(mad): Move this call in a proper place on CrOS.
       // http://crosbug.com/17687
