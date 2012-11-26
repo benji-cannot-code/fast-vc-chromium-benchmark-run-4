13631c407b290e727cc7358af53167fcc36cf314
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 10:41:25 +0000

    REGRESSION(134142): ASSERT(!m_size.isZero()) hits in CoordinatedBackingStore::paintToTextureMapper(). https://bugs.webkit.org/show_bug.cgi?id=103217
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-11-26
    Reviewed by Noam Rosenthal.
    
    It is possible for CoordinatedBackingStore of directed composited image to not
    have tiles, because CoordinatedImageBacking does not create tiles when the image
    is invisible.
    
    * UIProcess/CoordinatedGraphics/CoordinatedBackingStore.cpp:
    (WebKit::CoordinatedBackingStore::paintToTextureMapper):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/CoordinatedBackingStore.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/CoordinatedBackingStore.cpp
@@ -141,6 +141,10 @@ void CoordinatedBackingStore::paintTilesToTextureMapper(Vector<TextureMapperTile
 
 void CoordinatedBackingStore::paintToTextureMapper(TextureMapper* textureMapper, const FloatRect& targetRect, const TransformationMatrix& transform, float opacity, BitmapTexture* mask)
 {
+    if (m_tiles.isEmpty())
+        return;
+    ASSERT(!m_size.isZero());
+
     Vector<TextureMapperTile*> tilesToPaint;
     Vector<TextureMapperTile*> previousTilesToPaint;
 
@@ -166,7 +170,6 @@ void CoordinatedBackingStore::paintToTextureMapper(TextureMapper* textureMapper,
         previousTilesToPaint.append(&tile);
     }
 
-    ASSERT(!m_size.isZero());
     FloatRect rectOnContents(FloatPoint::zero(), m_size);
     TransformationMatrix adjustedTransform = transform;
     // targetRect is on the contents coordinate system, so we must compare two rects on the contents coordinate system.
