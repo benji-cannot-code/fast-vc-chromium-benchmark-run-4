d4415fb9242b716b68f4cd977ede55438412c36e
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-26 11:59:47 +0000

    Revert 169413 - Wiring up sync methods in SyncFileSystemService
    - Added MaybeStartSync() method which makes an attempt to start local and remote sync if possible.  The sync process is started if:
     * No other sync process is running (remote and local sync can run simultaneously but we never start two remote syncs and so on)
     * Remote sync service state is OK
     * The number of pending changes is greater than 0
    - Now we also retry RegisterOrigin when the remote service state changes to OK (and we have pending origins)
    
    Remaining major tasks:
    - We need conflict handling (P1)
    - We should fire OnFileSynced notifications (P2)
    - It might be better to also run a repeated timer function to make sure we run remote/local sync at least once in a while (P2)
    
    BUG=161436
    TEST=SyncFileSystemServiceTest.Simple{Local,Remote}SyncFlow
    
    Review URL: https://codereview.chromium.org/11418085
    
    TBR=kinuko@chromium.org
    Review URL: https://codereview.chromium.org/11413158
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169417 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_file_sync_service.h
+++ chrome/browser/sync_file_system/drive_file_sync_service.h
@@ -73,7 +73,6 @@ class DriveFileSyncService
   virtual void GetRemoteFileMetadata(
       const fileapi::FileSystemURL& url,
       const fileapi::SyncFileMetadataCallback& callback) OVERRIDE;
-  virtual RemoteServiceState GetCurrentState() const OVERRIDE;
 
   // LocalChangeProcessor overrides.
   virtual void ApplyLocalChange(
