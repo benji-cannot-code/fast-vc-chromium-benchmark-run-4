de0282e61c801bed3e8721221b7457c95feb42da
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-26 20:56:27 +0000

    Revert 169453 - Enable IPC logging for component build
    Since I refactored the registering of IPC loggers to go through a proper API,
    there's no dependency from ipc to chrome anymore, and we can enable it in
    debug builds
    
    BUG=none
    TEST=compiles in debug build
    
    ------------------------------------------------------
    
    Caused "unresolved external" link errors on Windows Debug builds:
    http://build.chromium.org/p/chromium.win/builders/Win%20Builder%20%28dbg%29/builds/5494/steps/compile/logs/stdio
    
    Possibly the cause of various "malloc" related failures seen elsewhere in
    the tree:
    http://build.chromium.org/p/chromium.mac/buildstatus?builder=Mac10.6%20Sync&number=25248
    
    
    Review URL: https://chromiumcodereview.appspot.com/11415122
    
    TBR=jochen@chromium.org
    Review URL: https://codereview.chromium.org/11428013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169486 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message.h
+++ ipc/ipc_message.h
@@ -17,7 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // from here.
 #include "ipc/ipc_sender.h"
 
-#if !defined(NDEBUG)
+// Ipc logging adds a dependency from the 'chrome' target on all ipc message
+// classes. In a component build, this would require exporting all message
+// classes, so don't support ipc logging in the components build.
+#if !defined(NDEBUG) && !defined(COMPONENT_BUILD)
 #define IPC_MESSAGE_LOG_ENABLED
 #endif
 
