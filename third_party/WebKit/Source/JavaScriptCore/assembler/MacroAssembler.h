4f28226778e1be5cc05e41eaba4f085aa040983e
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 21:20:37 +0000

    Don't blind all the things. https://bugs.webkit.org/show_bug.cgi?id=102572
    Reviewed by Gavin Barraclough.
    
    No longer blind all the constants in the instruction stream.  We use a
    simple non-deterministic filter to avoid blinding everything.  Also modified
    the basic integer blinding logic to avoid blinding small negative values.
    
    * assembler/MacroAssembler.h:
    (MacroAssembler):
    (JSC::MacroAssembler::shouldConsiderBlinding):
    (JSC::MacroAssembler::shouldBlind):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssembler.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssembler.h
@@ -840,26 +840,30 @@ public:
     using MacroAssemblerBase::and64;
     using MacroAssemblerBase::convertInt32ToDouble;
     using MacroAssemblerBase::store64;
-    
+    static const unsigned BlindingModulus = 64;
+    bool shouldConsiderBlinding()
+    {
+        return !(random() & (BlindingModulus - 1));
+    }
     bool shouldBlindDouble(double value)
     {
         // Don't trust NaN or +/-Infinity
         if (!isfinite(value))
-            return true;
+            return shouldConsiderBlinding();
 
         // Try to force normalisation, and check that there's no change
         // in the bit pattern
         if (bitwise_cast<uint64_t>(value * 1.0) != bitwise_cast<uint64_t>(value))
-            return true;
+            return shouldConsiderBlinding();
 
         value = abs(value);
         // Only allow a limited set of fractional components
         double scaledValue = value * 8;
         if (scaledValue / 8 != value)
-            return true;
+            return shouldConsiderBlinding();
         double frac = scaledValue - floor(scaledValue);
         if (frac != 0.0)
-            return true;
+            return shouldConsiderBlinding();
 
         return value > 0xff;
     }
@@ -888,8 +892,14 @@ public:
         default: {
             if (value <= 0xff)
                 return false;
+            if (~value <= 0xff)
+                return false;
         }
         }
+
+        if (!shouldConsiderBlinding())
+            return false;
+
         return shouldBlindForSpecificArch(value);
     }
     
@@ -941,6 +951,9 @@ public:
         default: {
             if (value <= 0xff)
                 return false;
+            if (~value <= 0xff)
+                return false;
+
             JSValue jsValue = JSValue::decode(value);
             if (jsValue.isInt32())
                 return shouldBlind(Imm32(jsValue.asInt32()));
@@ -951,6 +964,10 @@ public:
                 return false;
         }
         }
+
+        if (!shouldConsiderBlinding())
+            return false;
+
         return shouldBlindForSpecificArch(value);
     }
     
@@ -1069,7 +1086,13 @@ public:
         default:
             if (value <= 0xff)
                 return false;
+            if (~value <= 0xff)
+                return false;
         }
+
+        if (!shouldConsiderBlinding())
+            return false;
+
         return shouldBlindForSpecificArch(value);
 #endif
     }
