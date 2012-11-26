2fba1b5ec614c65233579f0909d95b2c5df310c9
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 20:49:06 +0000

    Add ResourceBuffer::append(CFDataRef) to get code to compile with USE(NETWORK_CFDATA_ARRAY_CALLBACK) https://bugs.webkit.org/show_bug.cgi?id=102706
    Reviewed by Brent Fulgham.
    
    No new tests because the flag isn't enabled. Also the functionality should be covered by
    existing tests.
    
    * loader/ResourceBuffer.cpp:
    (WebCore):
    (WebCore::ResourceBuffer::append):
    * loader/ResourceBuffer.h:
    (ResourceBuffer):
    * loader/mac/ResourceLoaderMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135755 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/mac/ResourceLoaderMac.mm
+++ third_party/WebKit/Source/WebCore/loader/mac/ResourceLoaderMac.mm
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(NETWORK_CFDATA_ARRAY_CALLBACK)
 #include "InspectorInstrumentation.h"
+#include "ResourceBuffer.h"
 #endif
 
 #if USE(CFNETWORK)
