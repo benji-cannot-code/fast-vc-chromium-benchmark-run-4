6c8436da2f0121e9477c81083bf88f8932f5efc9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-26 11:16:09 +0000

    Text Autosizing: Add Text Autosizing APIs for WK2 https://bugs.webkit.org/show_bug.cgi?id=100633
    Patch by Jaehun Lim <ljaehun.lim@samsung.com> on 2012-11-26
    Reviewed by Sam Weinig.
    
    Source/WebKit2:
    
    Implement basic Text Autosizing APIs for WK2.
    Text Autosizing is a useful feature for mobile browsers. It adjusts the font size
    of text in wide columns, and makes text more legible.
    This patch adds setting APIs for Text Autosizing in WK2.
    
    * Shared/WebPreferencesStore.h:
    (WebKit):
    * UIProcess/API/C/WKPreferences.cpp:
    (WKPreferencesSetTextAutosizingEnabled):
    (WKPreferencesGetTextAutosizingEnabled):
    * UIProcess/API/C/WKPreferences.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::updatePreferences):
    
    Tools:
    
    Add test cases for Text Autosizing in WKPreferences.
    
    * TestWebKitAPI/Tests/WebKit2/WKPreferences.cpp:
    (TestWebKitAPI::TEST):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKPreferences.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/C/WKPreferences.cpp
@@ -954,3 +954,14 @@ bool WKPreferencesGetEncodingDetectorEnabled(WKPreferencesRef preferencesRef)
 {
     return toImpl(preferencesRef)->usesEncodingDetector();
 }
+
+void WKPreferencesSetTextAutosizingEnabled(WKPreferencesRef preferencesRef, bool textAutosizingEnabled)
+{
+    toImpl(preferencesRef)->setTextAutosizingEnabled(textAutosizingEnabled);
+}
+
+bool WKPreferencesGetTextAutosizingEnabled(WKPreferencesRef preferencesRef)
+{
+    return toImpl(preferencesRef)->textAutosizingEnabled();
+}
+
--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -2294,6 +2294,10 @@ void WebPage::updatePreferences(const WebPreferencesStore& store)
     settings->setPlugInSnapshottingEnabled(store.getBoolValueForKey(WebPreferencesKey::plugInSnapshottingEnabledKey()));
     settings->setUsesEncodingDetector(store.getBoolValueForKey(WebPreferencesKey::usesEncodingDetectorKey()));
 
+#if ENABLE(TEXT_AUTOSIZING)
+    settings->setTextAutosizingEnabled(store.getBoolValueForKey(WebPreferencesKey::textAutosizingEnabledKey()));
+#endif
+
     platformPreferencesDidChange(store);
 
     if (m_drawingArea)
