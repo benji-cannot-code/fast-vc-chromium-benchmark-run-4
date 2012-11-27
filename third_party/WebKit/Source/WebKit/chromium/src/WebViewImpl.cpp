395e3bfc48760510acd1454ec7b794964726de0b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-27 00:39:53 +0000

    Add hasTouchEventhandlersAt to WebView API https://bugs.webkit.org/show_bug.cgi?id=102541
    Patch by Yusuf Ozuysal <yusufo@google.com> on 2012-11-26
    Reviewed by James Robinson.
    
    Adds hasTouchEventHandlersAt to WebWidget API to check for touch event handlers at a
    given point. This will be used to distinguish between events not processed by
    touch event handlers and event not hitting any touch event handlers. Both are
    returning the same ACK message currently. Default implementation returns true to
    continue the same behavior as we currently have.
    
    * public/WebWidget.h:
    (WebWidget):
    (WebKit::WebWidget::hasTouchEventHandlersAt):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::hasTouchEventHandlersAt):
    (WebKit):
    * src/WebViewImpl.h:
    (WebViewImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135787 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -1275,6 +1275,11 @@ void WebViewImpl::hasTouchEventHandlers(bool hasTouchHandlers)
         m_client->hasTouchEventHandlers(hasTouchHandlers);
 }
 
+bool WebViewImpl::hasTouchEventHandlersAt(const WebPoint& point)
+{
+    return true;
+}
+
 #if !OS(DARWIN)
 // Mac has no way to open a context menu based on a keyboard event.
 bool WebViewImpl::sendContextMenuEvent(const WebKeyboardEvent& event)
