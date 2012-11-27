d1e66512b6940c6109ad12f2dadcc665e42a604c
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 12:32:24 +0000

    Revert 169641 - Cache resource bindings on client
    This is the rest of a CL that was already approved plus the fix for one ChromeOS instance.
    https://chromiumcodereview.appspot.com/11415003
    
    BUG=160370
    TBR=apatrick@chromium.org,brettw@chromium.org
    
    Reverting due to test failing on all configs: GLES2ImplementationTest.BindTexture
    
    Review URL: https://codereview.chromium.org/11421070
    
    TBR=gman@chromium.org
    Review URL: https://codereview.chromium.org/11299199
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169646 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_implementation_unittest_autogen.h
+++ gpu/command_buffer/client/gles2_implementation_unittest_autogen.h
@@ -34,9 +34,6 @@ TEST_F(GLES2ImplementationTest, BindBuffer) {
 
   gl_->BindBuffer(GL_ARRAY_BUFFER, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-  ClearCommands();
-  gl_->BindBuffer(GL_ARRAY_BUFFER, 2);
-  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BindFramebuffer) {
@@ -48,9 +45,6 @@ TEST_F(GLES2ImplementationTest, BindFramebuffer) {
 
   gl_->BindFramebuffer(GL_FRAMEBUFFER, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-  ClearCommands();
-  gl_->BindFramebuffer(GL_FRAMEBUFFER, 2);
-  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BindRenderbuffer) {
@@ -62,9 +56,6 @@ TEST_F(GLES2ImplementationTest, BindRenderbuffer) {
 
   gl_->BindRenderbuffer(GL_RENDERBUFFER, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-  ClearCommands();
-  gl_->BindRenderbuffer(GL_RENDERBUFFER, 2);
-  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BindTexture) {
@@ -76,9 +67,6 @@ TEST_F(GLES2ImplementationTest, BindTexture) {
 
   gl_->BindTexture(GL_TEXTURE_2D, 2);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-  ClearCommands();
-  gl_->BindTexture(GL_TEXTURE_2D, 2);
-  EXPECT_TRUE(NoCommandsWritten());
 }
 
 TEST_F(GLES2ImplementationTest, BlendColor) {
@@ -1658,9 +1646,6 @@ TEST_F(GLES2ImplementationTest, BindVertexArrayOES) {
 
   gl_->BindVertexArrayOES(1);
   EXPECT_EQ(0, memcmp(&expected, commands_, sizeof(expected)));
-  ClearCommands();
-  gl_->BindVertexArrayOES(1);
-  EXPECT_TRUE(NoCommandsWritten());
 }
 // TODO: Implement unit test for GenSharedIdsCHROMIUM
 // TODO: Implement unit test for DeleteSharedIdsCHROMIUM
