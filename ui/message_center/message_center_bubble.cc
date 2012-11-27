c6f4d41ec7e5596901698fbb05399a1c9b31577b
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 17:57:45 +0000

    Changes the message item appearance in message center.
    BUG=161052
    
    Review URL: https://codereview.chromium.org/11422002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169688 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center_bubble.cc
+++ ui/message_center/message_center_bubble.cc
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/size.h"
 #include "ui/message_center/message_view.h"
 #include "ui/message_center/message_view_factory.h"
+#include "ui/views/background.h"
+#include "ui/views/border.h"
 #include "ui/views/controls/button/text_button.h"
 #include "ui/views/controls/label.h"
 #include "ui/views/controls/scroll_view.h"
@@ -26,7 +28,11 @@ namespace {
 
 const int kMessageBubbleBaseMinHeight = 80;
 const int kMessageBubbleBaseMaxHeight = 400;
+const int kMarginBetweenItems = 10;
+const SkColor kMessageCenterBackgroundColor = SkColorSetRGB(0xd3, 0xd3, 0xd3);
 const SkColor kBorderDarkColor = SkColorSetRGB(0xaa, 0xaa, 0xaa);
+const SkColor kMessageItemBorderWidth = 1;
+const SkColor kMessageItemBorderColor = SkColorSetRGB(0xaa, 0xaa, 0xaa);
 
 // The view for the buttons at the bottom of the web notification tray.
 class WebNotificationButtonView : public views::View,
@@ -90,6 +96,8 @@ class FixedSizedScrollView : public views::ScrollView {
   FixedSizedScrollView() {
     set_focusable(true);
     set_notify_enter_exit_on_child(true);
+    set_background(views::Background::CreateSolidBackground(
+        kMessageCenterBackgroundColor));
   }
 
   virtual ~FixedSizedScrollView() {}
@@ -141,10 +149,13 @@ class FixedSizedScrollView : public views::ScrollView {
 class ScrollContentView : public views::View {
  public:
   ScrollContentView() {
-    views::BoxLayout* layout =
-        new views::BoxLayout(views::BoxLayout::kVertical, 0, 0, 1);
-    layout->set_spread_blank_space(true);
-    SetLayoutManager(layout);
+    SetLayoutManager(
+        new views::BoxLayout(views::BoxLayout::kVertical,
+                             kMarginBetweenItems,
+                             kMarginBetweenItems,
+                             kMarginBetweenItems));
+    set_background(views::Background::CreateSolidBackground(
+        kMessageCenterBackgroundColor));
   }
 
   virtual ~ScrollContentView() {
@@ -199,6 +210,8 @@ class MessageCenterContentsView : public views::View {
       MessageView* view =
           MessageViewFactory::ViewForNotification(*iter, list_delegate_);
       view->set_scroller(scroller_);
+      view->set_border(views::Border::CreateSolidBorder(
+          kMessageItemBorderWidth, kMessageItemBorderColor));
       view->SetUpView();
       scroll_content_->AddChildView(view);
       if (++num_children >=
@@ -265,6 +278,7 @@ views::TrayBubbleView::InitParams MessageCenterBubble::GetInitParams(
   views::TrayBubbleView::InitParams init_params =
       GetDefaultInitParams(anchor_alignment);
   init_params.max_height = kMessageBubbleBaseMaxHeight;
+  init_params.bubble_width += kMarginBetweenItems * 2;
   init_params.can_activate = true;
   return init_params;
 }
