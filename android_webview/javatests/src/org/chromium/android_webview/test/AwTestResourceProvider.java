87e505c95dfd3018201c60e9e5242cc931b0e552
newt@chromium.org <newt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 10:53:31 +0000

    Remove AppResource and unneeded resources.
    Now that content and chrome have their own resources and are accessing them via
    the org.chromium.[content|chrome].R classes, we can remove the duplicates of
    these resources that live in content shell and test shell as well as any
    remaining references to AppResource.java.
    
    This is the third of three CLs to replace AppResource with R:
    1. http://codereview.chromium.org/11363150 - Support Java resources within content
    2. http://codereview.chromium.org/11360207 - Add Java resources to content and chrome
    3. http://codereview.chromium.org/11377117 - Remove AppResource and unneeded resources
    
    BUG=136704
    
    Review URL: https://chromiumcodereview.appspot.com/11377117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169633 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwTestResourceProvider.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwTestResourceProvider.java
@@ -8,7 +8,6 @@ package org.chromium.android_webview.test;
 import android.content.Context;
 
 import org.chromium.android_webview.AwResource;
-import org.chromium.content.app.AppResource;
 
 public class AwTestResourceProvider {
     private static boolean sInitialized;
@@ -17,12 +16,6 @@ public class AwTestResourceProvider {
         if (sInitialized) {
             return;
         }
-
-        AppResource.DRAWABLE_ICON_ACTION_BAR_SHARE = R.drawable.resource_icon;
-        AppResource.DRAWABLE_ICON_ACTION_BAR_WEB_SEARCH = R.drawable.resource_icon;
-
-        AppResource.STRING_ACTION_BAR_SHARE = R.string.test_string;
-        AppResource.STRING_ACTION_BAR_WEB_SEARCH = R.string.test_string;
 
         AwResource.setResources(context.getResources());
 
