f616638ecd310428405a8a315463fb764dcf9a2b
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 18:58:27 +0000

    This fix is to make the applauncher window will not be closed after uninstall dialog is showed.
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/11413161
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169697 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/extension_app_item.cc
+++ chrome/browser/ui/app_list/extension_app_item.cc
@@ -57,9 +57,11 @@ enum CommandId {
 class ExtensionUninstaller : public ExtensionUninstallDialog::Delegate {
  public:
   ExtensionUninstaller(Profile* profile,
-                       const std::string& extension_id)
+                       const std::string& extension_id,
+                       AppListControllerDelegate* controller)
       : profile_(profile),
-        extension_id_(extension_id) {
+        extension_id_(extension_id),
+        controller_(controller) {
   }
 
   void Run() {
@@ -69,7 +71,7 @@ class ExtensionUninstaller : public ExtensionUninstallDialog::Delegate {
       CleanUp();
       return;
     }
-
+    controller_->AboutToUninstallApp();
     ExtensionUninstallDialog* dialog =
         ExtensionUninstallDialog::Create(NULL, this);
     dialog->ConfirmUninstall(extension);
@@ -85,11 +87,12 @@ class ExtensionUninstaller : public ExtensionUninstallDialog::Delegate {
                                   false, /* external_uninstall*/
                                   NULL);
     }
-
+    controller_->UninstallAppCompleted();
     CleanUp();
   }
 
   virtual void ExtensionUninstallCanceled() OVERRIDE {
+    controller_->UninstallAppCompleted();
     CleanUp();
   }
 
@@ -99,6 +102,7 @@ class ExtensionUninstaller : public ExtensionUninstallDialog::Delegate {
 
   Profile* profile_;
   std::string extension_id_;
+  AppListControllerDelegate* controller_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionUninstaller);
 };
@@ -237,7 +241,8 @@ void ExtensionAppItem::ShowExtensionDetails() {
 void ExtensionAppItem::StartExtensionUninstall() {
   // ExtensionUninstall deletes itself when done or aborted.
   ExtensionUninstaller* uninstaller = new ExtensionUninstaller(profile_,
-                                                               extension_id_);
+                                                               extension_id_,
+                                                               controller_);
   uninstaller->Run();
 }
 
--- chrome/browser/ui/views/app_list/app_list_controller_win.cc
+++ chrome/browser/ui/views/app_list/app_list_controller_win.cc
@@ -96,6 +96,8 @@ class AppListControllerDelegateWin : public AppListControllerDelegate {
   virtual void ViewClosing() OVERRIDE;
   virtual void ViewActivationChanged(bool active) OVERRIDE;
   virtual bool CanPin() OVERRIDE;
+  virtual void AboutToUninstallApp() OVERRIDE;
+  virtual void UninstallAppCompleted() OVERRIDE;
   virtual bool CanShowCreateShortcutsDialog() OVERRIDE;
   virtual void ShowCreateShortcutsDialog(
       Profile* profile,
@@ -114,9 +116,11 @@ class AppListControllerDelegateWin : public AppListControllerDelegate {
 // list to operate, and controls when the app list is opened and closed.
 class AppListController {
  public:
-  AppListController() : current_view_(NULL) {}
+  AppListController() : current_view_(NULL), can_close_app_list_(true) {}
   ~AppListController() {}
 
+  void set_can_close(bool can_close) { can_close_app_list_ = can_close; }
+  bool can_close() { return can_close_app_list_; }
   void ShowAppList();
   void CloseAppList();
   void AppListClosing();
@@ -149,6 +153,9 @@ class AppListController {
 
   app_list::PaginationModel pagination_model_;
 
+  // True if the controller can close the app list.
+  bool can_close_app_list_;
+
   DISALLOW_COPY_AND_ASSIGN(AppListController);
 };
 
@@ -177,6 +184,14 @@ void AppListControllerDelegateWin::ViewClosing() {
 
 bool AppListControllerDelegateWin::CanPin() {
   return false;
+}
+
+void AppListControllerDelegateWin::AboutToUninstallApp() {
+  g_app_list_controller.Get().set_can_close(false);
+}
+
+void AppListControllerDelegateWin::UninstallAppCompleted() {
+  g_app_list_controller.Get().set_can_close(true);
 }
 
 bool AppListControllerDelegateWin::CanShowCreateShortcutsDialog() {
@@ -257,7 +272,7 @@ void AppListController::ShowAppList() {
 }
 
 void AppListController::CloseAppList() {
-  if (current_view_)
+  if (current_view_ && can_close_app_list_)
     current_view_->GetWidget()->Close();
 }
 
