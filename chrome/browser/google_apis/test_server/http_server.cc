3c3bb75a3f25918788fc7d2072998f7b7ef8db4a
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 04:59:15 +0000

    google_apis: Remove a hard-coded URL hack from HttpRequestParser
    "http://localhost/" was hard-coded in HttpRequestParser, but this was wrong
    as the server runs at a different host name and a port.
    
    Along the way, add a test for HttpServer::RegisterDefaultResponse, which was
    missing. Also add the request URL to warning messages for 404 requests.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/11280156
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169576 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/test_server/http_server.cc
+++ chrome/browser/google_apis/test_server/http_server.cc
@@ -32,7 +32,8 @@ const int kRetries = 10;
 scoped_ptr<HttpResponse> HandleDefaultRequest(const GURL& url,
                                               const HttpResponse& response,
                                               const HttpRequest& request) {
-  if (url.path() != request.url.path())
+  const GURL request_url = url.Resolve(request.relative_url);
+  if (url.path() != request_url.path())
     return scoped_ptr<HttpResponse>(NULL);
   return scoped_ptr<HttpResponse>(new HttpResponse(response));
 }
@@ -137,7 +138,8 @@ void HttpServer::HandleRequest(HttpConnection* connection,
     }
   }
 
-  LOG(WARNING) << "Request not handled. Returning 404.";
+  LOG(WARNING) << "Request not handled. Returning 404: "
+               << request->relative_url;
   scoped_ptr<HttpResponse> not_found_response(new HttpResponse());
   not_found_response->set_code(NOT_FOUND);
   connection->SendResponse(not_found_response.Pass());
@@ -170,10 +172,9 @@ void HttpServer::RegisterDefaultResponse(
   DCHECK(StartsWithASCII(relative_path, "/", true /* case_sensitive */))
       << relative_path;
 
-  GURL request_url = base_url_.Resolve(relative_path);
   const HandleRequestCallback callback =
       base::Bind(&HandleDefaultRequest,
-                 request_url,
+                 GetURL(relative_path),
                  default_response);
   request_handlers_.push_back(callback);
 }
