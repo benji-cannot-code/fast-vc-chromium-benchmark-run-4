154ac8030b8ed50d86a72a1d699bd4218c22dd6c
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 22:16:53 +0000

    [Sync] Refactor control types initialization
    We now have all the control type initialization happen on the sync 
    thread, avoiding the ConfigureDataTypes method which would stuff 
    the nigori node into any download. This allows us to ensure only those 
    control types without progress markers are downloaded as part of backend 
    initialization. 
    
    To accomplish this, HandleInitializationCompletedOnFrontendLoop no longer 
    operates as a state machine. Instead, we only call it when initialization has 
    completely finished (either as a success or a failure). 
    
    Note that theoretically HandleSyncManagerInitializationOnFrontendLoop could 
    also be removed, in favor of having all initialization happen on the sync thread, 
    but it's useful to keep it around for the purpose of doing pre-initialization 
    completion work in tests. 
    
    BUG=161824
    
    
    Review URL: https://chromiumcodereview.appspot.com/11419171
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/test_profile_sync_service.h
+++ chrome/browser/sync/test_profile_sync_service.h
@@ -60,7 +60,6 @@ class SyncBackendHostForProfileSyncTest : public SyncBackendHost {
       const syncer::WeakHandle<syncer::JsBackend>& js_backend,
       const syncer::WeakHandle<syncer::DataTypeDebugInfoListener>&
           debug_info_listener,
-      bool success,
       syncer::ModelTypeSet restored_types) OVERRIDE;
 
   static void SetHistoryServiceExpectations(ProfileMock* profile);
