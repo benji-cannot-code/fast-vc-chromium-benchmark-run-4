d8943cc79c3fa9f279ec4eb96a9d54a431c90aae
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 09:45:53 +0000

    Magnifier: Prevent from sometimes not showing back the cursor after animation.
    Major changes:
    - Rename: |is_on_zoopming_|-> |is_on_animation_|
    - Set |is_on_animation_| in Redraw() instead of the callees.
    
    BUG=none
    TEST=manual
    
    TBR=sky@chromium.org
    # TBRing for simple changes inside magnifier.
    
    Review URL: https://chromiumcodereview.appspot.com/11428018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169616 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/magnifier/magnification_controller.cc
+++ ash/magnifier/magnification_controller.cc
@@ -133,9 +133,9 @@ class MagnificationControllerImpl : virtual public MagnificationController,
 
   aura::RootWindow* root_window_;
 
-  // True if the magnified window is in motion of zooming or un-zooming effect.
-  // Otherwise, false.
-  bool is_on_zooming_;
+  // True if the magnified window is currently animating a change. Otherwise,
+  // false.
+  bool is_on_animation_;
 
   bool is_enabled_;
 
@@ -157,7 +157,7 @@ class MagnificationControllerImpl : virtual public MagnificationController,
 
 MagnificationControllerImpl::MagnificationControllerImpl()
     : root_window_(ash::Shell::GetPrimaryRootWindow()),
-      is_on_zooming_(false),
+      is_on_animation_(false),
       is_enabled_(false),
       move_cursor_after_animation_(false),
       scale_(std::numeric_limits<double>::min()) {
@@ -244,6 +244,9 @@ bool MagnificationControllerImpl::RedrawDIP(const gfx::PointF& position_in_dip,
       base::TimeDelta::FromMilliseconds(animate ? 100 : 0));
 
   root_window_->layer()->SetTransform(transform);
+
+  if (animate)
+    is_on_animation_ = true;
 
   return true;
 }
@@ -332,12 +335,10 @@ void MagnificationControllerImpl::OnMouseMove(const gfx::Point& location) {
     start_zoom = true;
   }
 
-  if (start_zoom && !is_on_zooming_) {
+  if (start_zoom && !is_on_animation_) {
     bool ret = RedrawDIP(gfx::Point(x, y), scale_, true);
 
     if (ret) {
-      is_on_zooming_ = true;
-
       int x_diff = origin_.x() - window_rect.x();
       int y_diff = origin_.y() - window_rect.y();
       // If the magnified region is moved, hides the mouse cursor and moves it.
@@ -391,7 +392,7 @@ void MagnificationControllerImpl::ValidateScale(float* scale) {
 }
 
 void MagnificationControllerImpl::OnImplicitAnimationsCompleted() {
-  if (!is_on_zooming_)
+  if (!is_on_animation_)
     return;
 
   if (move_cursor_after_animation_) {
@@ -403,7 +404,8 @@ void MagnificationControllerImpl::OnImplicitAnimationsCompleted() {
       aura::client::GetCursorClient(root_window_);
   if (cursor_client)
     cursor_client->ShowCursor(true);
-  is_on_zooming_ = false;
+
+  is_on_animation_ = false;
 }
 
 void MagnificationControllerImpl::SwitchTargetRootWindow(
