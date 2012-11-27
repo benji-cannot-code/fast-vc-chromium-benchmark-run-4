56b07a58086d65701b13bcbad049e5d0845d3f94
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 19:09:32 +0000

    Revert "Revert 169641 - Cache resource bindings on client"
    This reverts commit 7ced098111694ae800d0a54fc889ef9f6e81a3e3.
    
    BUG=160370
    TBR=apatrick@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11316191
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169702 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/gles2_implementation_impl_autogen.h
+++ gpu/command_buffer/client/gles2_implementation_impl_autogen.h
@@ -18,6 +18,18 @@ void GLES2Implementation::AttachShader(GLuint program, GLuint shader) {
   helper_->AttachShader(program, shader);
 }
 
+void GLES2Implementation::BindBuffer(GLenum target, GLuint buffer) {
+  GPU_CLIENT_SINGLE_THREAD_CHECK();
+  GPU_CLIENT_LOG("[" << GetLogPrefix() << "] glBindBuffer(" << GLES2Util::GetStringBufferTarget(target) << ", " << buffer << ")");  // NOLINT
+  if (IsBufferReservedId(buffer)) {
+    SetGLError(GL_INVALID_OPERATION, "BindBuffer", "buffer reserved id");
+    return;
+  }
+  if (BindBufferHelper(target, buffer)) {
+    helper_->BindBuffer(target, buffer);
+  }
+}
+
 void GLES2Implementation::BindFramebuffer(GLenum target, GLuint framebuffer) {
   GPU_CLIENT_SINGLE_THREAD_CHECK();
   GPU_CLIENT_LOG("[" << GetLogPrefix() << "] glBindFramebuffer(" << GLES2Util::GetStringFrameBufferTarget(target) << ", " << framebuffer << ")");  // NOLINT
@@ -26,8 +38,9 @@ void GLES2Implementation::BindFramebuffer(GLenum target, GLuint framebuffer) {
         GL_INVALID_OPERATION, "BindFramebuffer", "framebuffer reserved id");
     return;
   }
-  BindFramebufferHelper(target, framebuffer);
-  helper_->BindFramebuffer(target, framebuffer);
+  if (BindFramebufferHelper(target, framebuffer)) {
+    helper_->BindFramebuffer(target, framebuffer);
+  }
 }
 
 void GLES2Implementation::BindRenderbuffer(
@@ -39,8 +52,9 @@ void GLES2Implementation::BindRenderbuffer(
         GL_INVALID_OPERATION, "BindRenderbuffer", "renderbuffer reserved id");
     return;
   }
-  BindRenderbufferHelper(target, renderbuffer);
-  helper_->BindRenderbuffer(target, renderbuffer);
+  if (BindRenderbufferHelper(target, renderbuffer)) {
+    helper_->BindRenderbuffer(target, renderbuffer);
+  }
 }
 
 void GLES2Implementation::BindTexture(GLenum target, GLuint texture) {
@@ -50,8 +64,9 @@ void GLES2Implementation::BindTexture(GLenum target, GLuint texture) {
     SetGLError(GL_INVALID_OPERATION, "BindTexture", "texture reserved id");
     return;
   }
-  BindTextureHelper(target, texture);
-  helper_->BindTexture(target, texture);
+  if (BindTextureHelper(target, texture)) {
+    helper_->BindTexture(target, texture);
+  }
 }
 
 void GLES2Implementation::BlendColor(
@@ -1182,12 +1197,6 @@ void GLES2Implementation::UniformMatrix4fv(
   helper_->UniformMatrix4fvImmediate(location, count, transpose, value);
 }
 
-void GLES2Implementation::UseProgram(GLuint program) {
-  GPU_CLIENT_SINGLE_THREAD_CHECK();
-  GPU_CLIENT_LOG("[" << GetLogPrefix() << "] glUseProgram(" << program << ")");
-  helper_->UseProgram(program);
-}
-
 void GLES2Implementation::ValidateProgram(GLuint program) {
   GPU_CLIENT_SINGLE_THREAD_CHECK();
   GPU_CLIENT_LOG("[" << GetLogPrefix() << "] glValidateProgram(" << program << ")");  // NOLINT
@@ -1416,8 +1425,9 @@ void GLES2Implementation::BindVertexArrayOES(GLuint array) {
         GL_INVALID_OPERATION, "BindVertexArrayOES", "array reserved id");
     return;
   }
-  BindVertexArrayHelper(array);
-  helper_->BindVertexArrayOES(array);
+  if (BindVertexArrayHelper(array)) {
+    helper_->BindVertexArrayOES(array);
+  }
 }
 
 void GLES2Implementation::GetTranslatedShaderSourceANGLE(
