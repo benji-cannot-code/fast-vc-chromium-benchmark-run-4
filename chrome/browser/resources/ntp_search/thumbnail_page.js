7923adb07b7648d72e40e7a782cc7d27bf045251
pedrosimonetti@chromium.org <pedrosimonetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 07:10:58 +0000

    Revert 169194 - NTP5: Implementing new specification for favicons of Most Visited Pages.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416091
    
    TBR=pedrosimonetti@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11308215
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169599 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp_search/thumbnail_page.js
+++ chrome/browser/resources/ntp_search/thumbnail_page.js
@@ -86,12 +86,9 @@ cr.define('ntp', function() {
       if (banner)
         thumbnailImage.removeChild(banner);
 
-      var favicon = this.querySelector('.thumbnail-favicon') ||
-                    this.ownerDocument.createElement('div');
-      favicon.className = 'thumbnail-favicon';
-      favicon.style.backgroundImage =
-          url('chrome://favicon/size/16/' + dataUrl);
-      this.appendChild(favicon);
+      var favicon = thumbnailImage.querySelector('.thumbnail-favicon');
+      if (favicon)
+        thumbnailImage.removeChild(favicon);
 
       var self = this;
       var image = new Image();
@@ -107,6 +104,13 @@ cr.define('ntp', function() {
         // TODO(jeremycho): Consult with UX on URL truncation.
         banner.textContent = dataUrl.replace(/^(http:\/\/)?(www\.)?|\/$/gi, '');
         thumbnailImage.appendChild(banner);
+
+        favicon = thumbnailImage.querySelector('.thumbnail-favicon') ||
+            self.ownerDocument.createElement('div');
+        favicon.className = 'thumbnail-favicon';
+        favicon.style.backgroundImage =
+            url('chrome://favicon/size/16/' + dataUrl);
+        thumbnailImage.appendChild(favicon);
       };
 
       var thumbnailUrl = ntp.getThumbnailUrl(dataUrl);
