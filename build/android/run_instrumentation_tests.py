21b51a5fd23424c83d056fa78539085c8c4287eb
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 01:58:09 +0000

    Android tests: Always run PrintStepResultIfNeeded
    - DispatchInstrumentationTests was throwing an exception
      so python was not printing step failure.
    
    R=Yaron
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11421087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169555 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/run_instrumentation_tests.py
+++ build/android/run_instrumentation_tests.py
@@ -97,8 +97,11 @@ def main(argv):
   buildbot_report.PrintNamedStep(
       'Instrumentation tests: %s - %s' % (', '.join(options.annotation),
                                           options.test_apk))
-  ret = DispatchInstrumentationTests(options)
-  buildbot_report.PrintStepResultIfNeeded(options, ret)
+  ret = 1
+  try:
+    ret = DispatchInstrumentationTests(options)
+  finally:
+    buildbot_report.PrintStepResultIfNeeded(options, ret)
   return ret
 
 
