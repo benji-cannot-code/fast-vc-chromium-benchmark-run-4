d0be18d877f816541ae70a1ffb43b2854ea1194b
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 20:23:23 +0000

    Add more 'Dev' interfaces to the NaCl IPC proxy. BUG=116317 TEST=none
    Review URL: https://chromiumcodereview.appspot.com/11412184
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169721 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/resource_creation_proxy.h
+++ ppapi/proxy/resource_creation_proxy.h
@@ -126,6 +126,7 @@ class ResourceCreationProxy : public InterfaceProxy,
       PP_Instance instance,
       PPB_NetworkMonitor_Callback callback,
       void* user_data) OVERRIDE;
+  virtual PP_Resource CreatePrinting(PP_Instance) OVERRIDE;
   virtual PP_Resource CreateTCPServerSocketPrivate(
       PP_Instance instance) OVERRIDE;
   virtual PP_Resource CreateTCPSocketPrivate(PP_Instance instance) OVERRIDE;
@@ -150,7 +151,6 @@ class ResourceCreationProxy : public InterfaceProxy,
   virtual PP_Resource CreateFlashMenu(PP_Instance instance,
                                       const PP_Flash_Menu* menu_data) OVERRIDE;
   virtual PP_Resource CreateFlashMessageLoop(PP_Instance instance) OVERRIDE;
-  virtual PP_Resource CreatePrinting(PP_Instance) OVERRIDE;
   virtual PP_Resource CreateScrollbar(PP_Instance instance,
                                       PP_Bool vertical) OVERRIDE;
   virtual PP_Resource CreateTalk(PP_Instance instance) OVERRIDE;
