3b6aa7d2fd067bb6a8cb3066ebb4a5dfdb104d69
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 17:01:39 +0000

    Add tap gesture support for tree views.
    BUG=162509
    
    
    Review URL: https://chromiumcodereview.appspot.com/11411178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169673 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/tree/tree_view_views.h
+++ ui/views/controls/tree/tree_view_views.h
@@ -109,6 +109,7 @@ class VIEWS_EXPORT TreeView : public View,
   virtual void Layout() OVERRIDE;
   virtual gfx::Size GetPreferredSize() OVERRIDE;
   virtual bool OnMousePressed(const ui::MouseEvent& event) OVERRIDE;
+  virtual ui::EventResult OnGestureEvent(ui::GestureEvent* event) OVERRIDE;
   virtual void ShowContextMenu(const gfx::Point& p,
                                bool is_mouse_gesture) OVERRIDE;
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
@@ -147,6 +148,10 @@ class VIEWS_EXPORT TreeView : public View,
 
  private:
   friend class TreeViewViewsTest;
+
+  // Selects, expands or collapses nodes in the tree.  Consistent behavior for
+  // tap gesture and click events.
+  bool OnClickOrTap(const ui::LocatedEvent& event);
 
   // InternalNode is used to track information about the set of nodes displayed
   // by TreeViewViews.
