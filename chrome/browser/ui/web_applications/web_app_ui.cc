c3445166815fa39ed80548a2a30da4239a978f82
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 04:55:38 +0000

    [win] Don't use app_host.exe for v1 apps.
    The app_host won't be installed for v1 apps, so may not be on the system. It should not be used for shortcuts.
    
    BUG=160479
    TEST=Check on Windows that the Create Shortcuts... command works for all types of apps.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11413134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169573 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/web_applications/web_app_ui.cc
+++ chrome/browser/ui/web_applications/web_app_ui.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/extensions/extension.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_registrar.h"
@@ -352,6 +353,18 @@ void UpdateShortcutForTabContents(WebContents* web_contents) {
   UpdateShortcutWorker* worker = new UpdateShortcutWorker(web_contents);
   worker->Run();
 #endif  // defined(OS_WIN)
+}
+
+void UpdateShortcutInfoForApp(const extensions::Extension& app,
+                              Profile* profile,
+                              ShellIntegration::ShortcutInfo* shortcut_info) {
+  shortcut_info->extension_id = app.id();
+  shortcut_info->is_platform_app = app.is_platform_app();
+  shortcut_info->url = GURL(app.launch_web_url());
+  shortcut_info->title = UTF8ToUTF16(app.name());
+  shortcut_info->description = UTF8ToUTF16(app.description());
+  shortcut_info->extension_path = app.path();
+  shortcut_info->profile_path = profile->GetPath();
 }
 
 }  // namespace web_app
