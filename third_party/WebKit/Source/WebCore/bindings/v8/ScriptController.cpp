4dddbc9a9fdc83035aa73dee56ad101a86060883
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-27 11:52:49 +0000

    [V8] add contextForWorld helper function to ScriptController https://bugs.webkit.org/show_bug.cgi?id=103376
    Patch by Dan Carney <dcarney@google.com> on 2012-11-27
    Reviewed by Kentaro Hara.
    
    Reducing some commonly occuring code with an inline function.
    
    No new tests. No change in functionality.
    
    * bindings/v8/ScriptController.cpp:
    (WebCore::ScriptController::contextForWorld):
    (WebCore):
    (WebCore::ScriptController::currentWorldContext):
    (WebCore::ScriptController::mainWorldContext):
    * bindings/v8/ScriptController.h:
    (ScriptController):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/ScriptController.cpp
@@ -435,15 +435,20 @@ void ScriptController::finishedWithEvent(Event* event)
 {
 }
 
+static inline v8::Local<v8::Context> contextForWorld(ScriptController* scriptController, DOMWrapperWorld* world)
+{
+    return v8::Local<v8::Context>::New(scriptController->windowShell(world)->context());
+}
+
 v8::Local<v8::Context> ScriptController::currentWorldContext()
 {
     if (!v8::Context::InContext())
-        return v8::Local<v8::Context>::New(windowShell(mainThreadNormalWorld())->context());
+        return contextForWorld(this, mainThreadNormalWorld());
 
     v8::Handle<v8::Context> context = v8::Context::GetEntered();
     DOMWrapperWorld* isolatedWorld = DOMWrapperWorld::isolated(context);
     if (!isolatedWorld)
-        return v8::Local<v8::Context>::New(windowShell(mainThreadNormalWorld())->context());
+        return contextForWorld(this, mainThreadNormalWorld());
 
     Frame* frame = toFrameIfNotDetached(context);
     if (!m_frame)
@@ -456,12 +461,12 @@ v8::Local<v8::Context> ScriptController::currentWorldContext()
     if (isolatedWorld->createdFromUnitializedWorld())
         return v8::Local<v8::Context>();
 
-    return v8::Local<v8::Context>::New(windowShell(isolatedWorld)->context());
+    return contextForWorld(this, isolatedWorld);
 }
 
 v8::Local<v8::Context> ScriptController::mainWorldContext()
 {
-    return v8::Local<v8::Context>::New(windowShell(mainThreadNormalWorld())->context());
+    return contextForWorld(this, mainThreadNormalWorld());
 }
 
 v8::Local<v8::Context> ScriptController::mainWorldContext(Frame* frame)
@@ -469,7 +474,7 @@ v8::Local<v8::Context> ScriptController::mainWorldContext(Frame* frame)
     if (!frame)
         return v8::Local<v8::Context>();
 
-    return frame->script()->mainWorldContext();
+    return contextForWorld(frame->script(), mainThreadNormalWorld());
 }
 
 // Create a V8 object with an interceptor of NPObjectPropertyGetter.
