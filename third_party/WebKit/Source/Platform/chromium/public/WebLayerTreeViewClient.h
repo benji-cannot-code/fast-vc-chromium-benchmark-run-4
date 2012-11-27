d7744f710a9416fb1dec6356e834e62a1f46bb4c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-27 02:25:46 +0000

    [chromium] Remove deprecated and unused WebGraphicsContext3D compositor binding calls https://bugs.webkit.org/show_bug.cgi?id=103322
    Patch by James Robinson <jamesr@chromium.org> on 2012-11-26
    Reviewed by Adam Barth.
    
    Source/Platform:
    
    These have been uncalled for a while.
    
    * chromium/public/WebLayerTreeViewClient.h:
    (WebLayerTreeViewClient):
    
    Source/WebKit/chromium:
    
    * src/WebViewImpl.cpp:
    * src/WebViewImpl.h:
    (WebViewImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebLayerTreeViewClient.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebLayerTreeViewClient.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 class WebCompositorOutputSurface;
-class WebGraphicsContext3D;
 class WebInputHandler;
 class WebThread;
 struct WebSize;
@@ -57,15 +56,6 @@ public:
     // for mobile-device pinch zooming). This is triggered by events sent to the
     // compositor thread through the WebCompositor interface.
     virtual void applyScrollAndScale(const WebSize& scrollDelta, float scaleFactor) = 0;
-
-    // DEPRECATED: Creates a 3D context suitable for the compositing. This may be called
-    // more than once if the context gets lost. This will be removed once
-    // downstream dependencies have been removed.
-    virtual WebGraphicsContext3D* createContext3D() { return 0; }
-
-    // DEPRECATED: Signals a successful rebinding of the 3D context (e.g. after a lost
-    // context event).
-    virtual void didRebindGraphicsContext(bool) { return; }
 
     // Creates the output surface. This may be called more than once
     // if the context gets lost.
