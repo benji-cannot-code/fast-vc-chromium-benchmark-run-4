f0fc18105399a3cb16c4bab82b0c62e88a4726bb
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-27 13:54:12 +0000

    FindLastActiveWithProfile now requires a HostDesktopType.
    BUG=129187
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/11365128
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/cloud_print/cloud_print_setup_flow.cc
+++ chrome/browser/printing/cloud_print/cloud_print_setup_flow.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/host_desktop.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/service_messages.h"
@@ -63,7 +64,8 @@ CloudPrintSetupFlow* CloudPrintSetupFlow::OpenDialog(
     const base::WeakPtr<Delegate>& delegate,
     gfx::NativeWindow parent_window) {
   DCHECK(profile);
-  Browser* browser = chrome::FindLastActiveWithProfile(profile);
+  Browser* browser = chrome::FindLastActiveWithProfile(profile,
+      chrome::GetActiveDesktop());
   // Set the arguments for showing the gaia login page.
   DictionaryValue args;
   args.SetString("user", "");
