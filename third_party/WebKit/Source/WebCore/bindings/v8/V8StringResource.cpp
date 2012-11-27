3690866de4881b549c8685d5d68ccc5a44e0ef16
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-27 15:50:07 +0000

    [V8] Remove V8StringResource::m_string https://bugs.webkit.org/show_bug.cgi?id=103367
    Reviewed by Adam Barth.
    
    This patch cleans up V8StringResource and removes V8StringResource::m_string.
    This patch won't change performance. (This patch just adds one redundant IsString()
    check and one IsInt32() check.) I confirmed no performance regression in micro benchmarks
    (https://bugs.webkit.org/attachment.cgi?id=176178).
    
    No tests. No change in behavior.
    
    * bindings/v8/V8Binding.cpp:
    (WebCore::v8NonStringValueToWebCoreString):
    * bindings/v8/V8StringResource.cpp:
    (WebCore::int32ToWebCoreStringFast):
    (WebCore::String):
    (WebCore::AtomicString):
    (WebCore):
    * bindings/v8/V8StringResource.h:
    (WebCore::V8StringResource::V8StringResource):
    (WebCore::V8StringResource::prepareBase):
    (WebCore::V8StringResource::toString):
    (V8StringResource):
    (WebCore::::prepare):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8StringResource.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8StringResource.cpp
@@ -191,6 +191,8 @@ String int32ToWebCoreStringFast(int value)
 
     // Most numbers used are <= 100. Even if they aren't used there's very little cost in using the space.
     const int kLowNumbers = 100;
+
+    // FIXME: Store lowNumbers in V8PerIsolateData so that workers can also use them.
     DEFINE_STATIC_LOCAL(Vector<AtomicString>, lowNumbers, (kLowNumbers + 1));
     String webCoreString;
     if (0 <= value && value <= kLowNumbers) {
@@ -205,12 +207,17 @@ String int32ToWebCoreStringFast(int value)
     return webCoreString;
 }
 
-String int32ToWebCoreString(int value)
+template<> String int32ToWebCoreString<String>(int value)
 {
     // If we are on the main thread (this should always true for non-workers), call the faster one.
     if (isMainThread())
         return int32ToWebCoreStringFast(value);
     return String::number(value);
+}
+
+template<> AtomicString int32ToWebCoreString<AtomicString>(int value)
+{
+    return AtomicString(int32ToWebCoreString<String>(value));
 }
 
 } // namespace WebCore
