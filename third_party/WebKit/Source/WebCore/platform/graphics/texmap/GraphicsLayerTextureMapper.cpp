f51cf7f47e3b15929b4650607a63ef8f6543c4a8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-27 07:22:35 +0000

    Unreviewed, rolling out r135822. http://trac.webkit.org/changeset/135822 https://bugs.webkit.org/show_bug.cgi?id=103369
    Undo the revert of r135818, since that change does not affect
    code used by Chromium (Requested by apavlov on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-11-26
    
    Source/WebCore:
    
    * platform/graphics/texmap/GraphicsLayerTextureMapper.cpp:
    (WebCore::GraphicsLayerTextureMapper::setMaskLayer):
    (WebCore::GraphicsLayerTextureMapper::setContentsVisible):
    
    Source/WebKit2:
    
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::setContentsVisible):
    (WebCore::CoordinatedGraphicsLayer::setMaskLayer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/GraphicsLayerTextureMapper.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/GraphicsLayerTextureMapper.cpp
@@ -196,6 +196,7 @@ void GraphicsLayerTextureMapper::setMaskLayer(GraphicsLayer* value)
     if (!value)
         return;
     value->setSize(size());
+    value->setContentsVisible(contentsAreVisible());
 }
 
 
@@ -301,6 +302,8 @@ void GraphicsLayerTextureMapper::setContentsVisible(bool value)
         return;
     notifyChange(TextureMapperLayer::ContentsVisibleChange);
     GraphicsLayer::setContentsVisible(value);
+    if (maskLayer())
+        maskLayer()->setContentsVisible(value);
 }
 
 /* \reimp (GraphicsLayer.h)
