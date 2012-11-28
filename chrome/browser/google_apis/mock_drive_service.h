6e93a2246badd5bb7401a41040cc6ba5dfb99133
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 06:00:28 +0000

    google_apis: Rename AuthorizeAppsOperation to AuthorizeAppOperation
    This operation only handles one app... Fix the class comment along the way.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/11348272
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169860 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/mock_drive_service.h
+++ chrome/browser/google_apis/mock_drive_service.h
@@ -94,7 +94,7 @@ class MockDriveService : public DriveServiceInterface {
                     const ResumeUploadCallback& callback));
   MOCK_METHOD3(AuthorizeApp,
                void(const GURL& resource_url,
-                    const std::string& app_ids,
+                    const std::string& app_id,
                     const GetDataCallback& callback));
   MOCK_CONST_METHOD0(HasAccessToken, bool());
   MOCK_CONST_METHOD0(HasRefreshToken, bool());
