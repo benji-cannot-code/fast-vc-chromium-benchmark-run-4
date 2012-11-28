40aababc5f6d7e967756a2930458676bdc11c6a6
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-28 01:15:05 +0000

    Compile time size assertions for TreeScope, ShadowRoot and ElementAttributeData.
    Rubber-stamped by Anders Carlsson.
    
    * dom/ShadowRoot.cpp:
    (SameSizeAsShadowRoot):
    * dom/TreeScope.cpp:
    (SameSizeAsTreeScope):
    * dom/ElementAttributeData.cpp: Derp.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135955 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/TreeScope.cpp
+++ third_party/WebKit/Source/WebCore/dom/TreeScope.cpp
@@ -53,6 +53,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class SameSizeAsTreeScope {
+    virtual ~SameSizeAsTreeScope();
+    void* pointers[7];
+};
+
+COMPILE_ASSERT(sizeof(TreeScope) == sizeof(SameSizeAsTreeScope), treescope_should_stay_small);
+
 using namespace HTMLNames;
 
 TreeScope::TreeScope(ContainerNode* rootNode)
