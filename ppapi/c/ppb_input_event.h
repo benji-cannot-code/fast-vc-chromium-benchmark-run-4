260343255bbb23b86a33dfdc4568dcfcbd140337
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 23:47:27 +0000

    ppapi: Opt-in to receive synthetic mouse events for touch-events.
    If a pepper plugin did not opt-in to receive touch-events, then opt-in to receive
    synthetic mouse-events instead.
    
    BUG=138362
    
    Review URL: https://codereview.chromium.org/11418134
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170097 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/ppb_input_event.h
+++ ppapi/c/ppb_input_event.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * found in the LICENSE file.
  */
 
-/* From ppb_input_event.idl modified Tue Aug 14 09:41:28 2012. */
+/* From ppb_input_event.idl modified Mon Nov 26 19:51:21 2012. */
 
 #ifndef PPAPI_C_PPB_INPUT_EVENT_H_
 #define PPAPI_C_PPB_INPUT_EVENT_H_
@@ -275,7 +275,12 @@ typedef enum {
    *
    * Request touch events only if you intend to handle them. If the browser
    * knows you do not need to handle touch events, it can handle them at a
-   * higher level and achieve higher performance.
+   * higher level and achieve higher performance. If the plugin does not
+   * register for touch-events, then it will receive synthetic mouse events that
+   * are generated from the touch events (e.g. mouse-down for touch-start,
+   * mouse-move for touch-move (with left-button down), and mouse-up for
+   * touch-end. If the plugin does register for touch events, then the synthetic
+   * mouse events are not created.
    */
   PP_INPUTEVENT_CLASS_TOUCH = 1 << 3,
   /**
@@ -321,6 +326,9 @@ struct PPB_InputEvent_1_0 {
    * that message. Requesting that such messages be delivered, even if they are
    * processed very quickly, may have a noticeable effect on the performance of
    * the page.
+   *
+   * Note that synthetic mouse events will be generated from touch events if
+   * (and only if) the you do not request touch events.
    *
    * When requesting input events through this function, the events will be
    * delivered and <i>not</i> bubbled to the page. This means that even if you
