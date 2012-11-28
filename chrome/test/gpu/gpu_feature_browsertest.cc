3a5ed4e30b04bf86b34448e02f11ef08c50da807
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 03:35:57 +0000

    Disable GpuFeatureTest.ThreadedCompositingTextureSharing on Win Aura.
    Not sure why it's failing. It also fails with --disable-test-compositor.
    
    BUG=154081,163046
    TBR=jamesr
    Review URL: https://codereview.chromium.org/11415169
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169829 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/gpu/gpu_feature_browsertest.cc
+++ chrome/test/gpu/gpu_feature_browsertest.cc
@@ -235,6 +235,8 @@ IN_PROC_BROWSER_TEST_F(AcceleratedCompositingBlockedTest,
   RunEventTest(url, kSwapBuffersEvent, false);
 }
 
+#if !(defined(OS_WIN) && defined(USE_AURA))  // http://crbug.com/163046
+
 IN_PROC_BROWSER_TEST_F(GpuFeatureTest, ThreadedCompositingTextureSharing) {
   const std::string json_blacklist =
       "{\n"
@@ -258,6 +260,8 @@ IN_PROC_BROWSER_TEST_F(GpuFeatureTest, ThreadedCompositingTextureSharing) {
   const FilePath url(FILE_PATH_LITERAL("feature_compositing.html"));
   RunEventTest(url, kThreadedCompositingEvent, false);
 }
+
+#endif
 
 class AcceleratedCompositingTest : public GpuFeatureTest {
  public:
