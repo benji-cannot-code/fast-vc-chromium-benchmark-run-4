55ea3de8290acfbb6978163cac2624a39f14b185
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 03:54:39 +0000

    google_apis: Add a test for DeleteDocumentOperation
    Along the way, update the existing tests to check if the method is GET,
    whereas the DeleteDocumentOperation uses DELETE.
    
    BUG=162348
    TEST=none
    
    Review URL: https://codereview.chromium.org/11308240
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169836 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/test_server/http_request.h
+++ chrome/browser/google_apis/test_server/http_request.h
@@ -28,7 +28,8 @@ enum HttpMethod {
 };
 
 // Represents a HTTP request. Since it can be big, use scoped_ptr to pass it
-// instead of copying.
+// instead of copying. However, the struct is copyable so tests can save and
+// examine a HTTP request.
 struct HttpRequest {
   HttpRequest();
   ~HttpRequest();
@@ -38,9 +39,6 @@ struct HttpRequest {
   std::map<std::string, std::string> headers;
   std::string content;
   bool has_content;
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(HttpRequest);
 };
 
 // Parses the input data and produces a valid HttpRequest object. If there is
