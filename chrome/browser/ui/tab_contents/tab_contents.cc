b3d8f758d9550ab9ffd570e468e91091f0179eef
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 01:18:10 +0000

    Kill TabContents::Clone.
    BUG=107201
    TEST=no visible change
    
    Review URL: https://codereview.chromium.org/11428029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169810 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents.cc
+++ chrome/browser/ui/tab_contents/tab_contents.cc
@@ -97,10 +97,6 @@ TabContents* TabContents::Factory::CreateTabContents(WebContents* contents) {
 }
 
 // static
-TabContents* TabContents::Factory::CloneTabContents(TabContents* contents) {
-  return contents->Clone();
-}
-
 TabContents::TabContents(WebContents* contents)
     : content::WebContentsObserver(contents),
       in_destructor_(false),
@@ -204,11 +200,6 @@ TabContents::TabContents(WebContents* contents)
 
 TabContents::~TabContents() {
   in_destructor_ = true;
-}
-
-TabContents* TabContents::Clone() {
-  WebContents* new_web_contents = web_contents()->Clone();
-  return new TabContents(new_web_contents);
 }
 
 // static
