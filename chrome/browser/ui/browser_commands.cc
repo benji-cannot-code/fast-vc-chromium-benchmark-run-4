df90ed6d4555c2149be4db454078df2f418f3f67
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 05:20:22 +0000

    Remove TabContents from browser_commands API.
    BUG=107201
    TEST=no visible change
    
    
    Committed: http://src.chromium.org/viewvc/chrome?view=rev&revision=169772
    
    Reverted: http://src.chromium.org/viewvc/chrome?view=rev&revision=169774
    
    Review URL: https://chromiumcodereview.appspot.com/11416204
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169852 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_commands.cc
+++ chrome/browser/ui/browser_commands.cc
@@ -568,7 +568,7 @@ bool CanDuplicateTab(const Browser* browser) {
   return contents && contents->GetController().GetLastCommittedEntry();
 }
 
-TabContents* DuplicateTabAt(Browser* browser, int index) {
+WebContents* DuplicateTabAt(Browser* browser, int index) {
   WebContents* contents = browser->tab_strip_model()->GetWebContentsAt(index);
   CHECK(contents);
   TabContents* contents_dupe =
@@ -620,7 +620,7 @@ TabContents* DuplicateTabAt(Browser* browser, int index) {
       SessionServiceFactory::GetForProfileIfExisting(browser->profile());
   if (session_service)
     session_service->TabRestored(contents_dupe->web_contents(), pinned);
-  return contents_dupe;
+  return contents_dupe->web_contents();
 }
 
 bool CanDuplicateTabAt(Browser* browser, int index) {
@@ -977,13 +977,12 @@ bool IsDebuggerAttachedToCurrentTab(Browser* browser) {
       content::DevToolsAgentHostRegistry::IsDebuggerAttached(contents) : false;
 }
 
-void ViewSource(Browser* browser, TabContents* contents) {
+void ViewSource(Browser* browser, WebContents* contents) {
   DCHECK(contents);
 
   // Use the last committed entry, since the pending entry hasn't loaded yet and
   // won't be copied into the cloned tab.
-  NavigationEntry* entry =
-    contents->web_contents()->GetController().GetLastCommittedEntry();
+  NavigationEntry* entry = contents->GetController().GetLastCommittedEntry();
   if (!entry)
     return;
 
@@ -991,7 +990,7 @@ void ViewSource(Browser* browser, TabContents* contents) {
 }
 
 void ViewSource(Browser* browser,
-                TabContents* contents,
+                WebContents* contents,
                 const GURL& url,
                 const std::string& content_state) {
   content::RecordAction(UserMetricsAction("ViewSource"));
@@ -1000,8 +999,7 @@ void ViewSource(Browser* browser,
   // Note that Clone does not copy the pending or transient entries, so the
   // active entry in view_source_contents will be the last committed entry.
   TabContents* view_source_contents =
-      BrowserCommandsTabContentsCreator::CreateTabContents(
-          contents->web_contents()->Clone());
+      BrowserCommandsTabContentsCreator::CreateTabContents(contents->Clone());
   view_source_contents->web_contents()->GetController().PruneAllButActive();
   NavigationEntry* active_entry =
       view_source_contents->web_contents()->GetController().GetActiveEntry();
@@ -1023,7 +1021,7 @@ void ViewSource(Browser* browser,
   if (browser->CanSupportWindowFeature(Browser::FEATURE_TABSTRIP)) {
     // If this is a tabbed browser, just create a duplicate tab inside the same
     // window next to the tab being duplicated.
-    int index = browser->tab_strip_model()->GetIndexOfTabContents(contents);
+    int index = browser->tab_strip_model()->GetIndexOfWebContents(contents);
     int add_types = TabStripModel::ADD_ACTIVE |
         TabStripModel::ADD_INHERIT_GROUP;
     browser->tab_strip_model()->InsertWebContentsAt(
@@ -1057,11 +1055,12 @@ void ViewSource(Browser* browser,
 }
 
 void ViewSelectedSource(Browser* browser) {
-  ViewSource(browser, browser->tab_strip_model()->GetActiveTabContents());
+  ViewSource(browser, browser->tab_strip_model()->GetActiveWebContents());
 }
 
 bool CanViewSource(const Browser* browser) {
-  return GetActiveWebContents(browser)->GetController().CanViewSource();
+  return browser->tab_strip_model()->GetActiveWebContents()->
+      GetController().CanViewSource();
 }
 
 void CreateApplicationShortcuts(Browser* browser) {
