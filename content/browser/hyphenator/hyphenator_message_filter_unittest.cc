983c2c8eb35e9cb6edb913c4156feabdfcb75c2e
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 14:26:10 +0000

    Fix bug in HyphenatorMessageFilterTest.OpenDictionary where a file descriptor was being closed twice. This caused this exception to be thrown while debugging content_unittests: 'Unhandled exception at 0x76E812F7 in content_unittests.exe: 0xC0000008: An invalid handle was specified'
    BUG=none
    TEST=Fixes content_unittests --gtest_filter=HyphenatorMessageFilterTest.OpenDictionary
    TBR=hbono@chromium.org
    Review URL: https://codereview.chromium.org/11299227
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169931 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/hyphenator/hyphenator_message_filter_unittest.cc
+++ content/browser/hyphenator/hyphenator_message_filter_unittest.cc
@@ -132,6 +132,7 @@ TEST_F(HyphenatorMessageFilterTest, OpenDictionary) {
       NULL, NULL);
   EXPECT_NE(base::kInvalidPlatformFileValue, file);
   filter_->SetDictionary(file);
+  file = base::kInvalidPlatformFileValue;  // Ownership has been transferred.
 
   // Send a HyphenatorHostMsg_OpenDictionary message with an empty locale and
   // verify it sends a HyphenatorMsg_SetDictionary message with a valid file.
@@ -149,8 +150,6 @@ TEST_F(HyphenatorMessageFilterTest, OpenDictionary) {
 
   // Delete all resources used by this test.
   filter_->Reset();
-  if (file != base::kInvalidPlatformFileValue)
-    base::ClosePlatformFile(file);
 }
 
 }  // namespace content
