15f04d75a93dcbad29ae1447072067e45a4f195c
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 15:26:26 +0000

    This fix is to avoid leaking the ExtensionUninstallDialog instance via scoped_ptr usage.
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11419160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/extension_app_item.cc
+++ chrome/browser/ui/app_list/extension_app_item.cc
@@ -72,9 +72,8 @@ class ExtensionUninstaller : public ExtensionUninstallDialog::Delegate {
       return;
     }
     controller_->AboutToUninstallApp();
-    ExtensionUninstallDialog* dialog =
-        ExtensionUninstallDialog::Create(NULL, this);
-    dialog->ConfirmUninstall(extension);
+    dialog_.reset(ExtensionUninstallDialog::Create(NULL, this));
+    dialog_->ConfirmUninstall(extension);
   }
 
  private:
@@ -103,6 +102,7 @@ class ExtensionUninstaller : public ExtensionUninstallDialog::Delegate {
   Profile* profile_;
   std::string extension_id_;
   AppListControllerDelegate* controller_;
+  scoped_ptr<ExtensionUninstallDialog> dialog_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionUninstaller);
 };
