0abc08d4961366a99f86553d1d941a8623f87541
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-28 12:07:27 +0000

    GraphicsSurface: Implement usage of GraphicsSurface for WebGL in WK1. https://bugs.webkit.org/show_bug.cgi?id=103497
    Using GraphicsSurface for WebGL in WK1 allows us to share
    the code with the WK2 implementation.
    
    Reviewed by Simon Hausmann.
    
    * platform/graphics/qt/GraphicsContext3DQt.cpp:
    (WebCore::GraphicsContext3DPrivate::GraphicsContext3DPrivate):
        Remove obsolete code.
    (WebCore::GraphicsContext3DPrivate::paintToTextureMapper):
        After blitting the multisample framebuffer,
        we can copy the contents onto the GraphicsSurface.
        Right after that we can use the GraphicsSurface
        as a texture origin to paint to the TextureMapper.
        Flags related to flipping and blending are being resolved
        by GraphicsSurface, and do not need to be handled here anymore.
    (WebCore::GraphicsContext3DPrivate::copyToGraphicsSurface):
        Remove a redundant makeCurrentIfNeeded call.
        The original context is being restored by
        blitMultisampleFramebufferAndRestoreContext anyway.
        Instead of explicitly calling swapBuffers here,
        we call swapBuffers on the GraphicsSurface implicitly
        after in GraphicsSurfacePrivate::copyFromTexture().
    * platform/graphics/surfaces/mac/GraphicsSurfaceMac.cpp:
        Add members to hold the reading end texture IDs.
        The texture on the reading side are in a different
        context than the texture on the writing side.
        By using GraphicsSurface in WK1 we have to distinguish
        between these textures, as a single instance serves
        for reading and writing.
    (WebCore::GraphicsSurfacePrivate::GraphicsSurfacePrivate):
    (WebCore::GraphicsSurfacePrivate::~GraphicsSurfacePrivate):
    (WebCore::GraphicsSurfacePrivate::swapBuffers):
    (WebCore::GraphicsSurfacePrivate::copyFromTexture):
        Add an implicit call to swapBuffers().
    (WebCore::GraphicsSurfacePrivate::frontBufferTextureID):
    (GraphicsSurfacePrivate):
    * platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp:
    (WebCore::GraphicsSurfacePrivate::GraphicsSurfacePrivate):
        Add a flag to distinguish between sending and receiving/reading
        side of the surface. This flag is being initialized in the constructor
        which is the only place where we know if an instance will be used for
        writing or reading.
        The purpose of this flag is to avoid erroneously swapping the buffers
        on the reading side. We can no longer rely on m_xPixmap for this purpose.
    (WebCore::GraphicsSurfacePrivate::swapBuffers):
    (WebCore::GraphicsSurfacePrivate::copyFromTexture):
    (WebCore::GraphicsSurfacePrivate::glxPixmap):
        Lazily create the GLX pixmap on demand.
    (GraphicsSurfacePrivate):
    (WebCore::GraphicsSurface::platformImport):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp
@@ -139,7 +139,7 @@ static const int glxAttributes[] = {
 
 struct GraphicsSurfacePrivate {
     GraphicsSurfacePrivate(const PlatformGraphicsContext3D shareContext = 0)
-        : m_display(0)
+        : m_display(m_offScreenWindow.display())
         , m_xPixmap(0)
         , m_glxPixmap(0)
         , m_surface(0)
@@ -150,9 +150,9 @@ struct GraphicsSurfacePrivate {
         , m_fbConfig(0)
         , m_textureIsYInverted(false)
         , m_hasAlpha(false)
+        , m_isReceiver(false)
     {
         GLXContext shareContextObject = 0;
-        m_display = m_offScreenWindow.display();
 
 #if PLATFORM(QT)
         if (shareContext) {
@@ -212,6 +212,7 @@ struct GraphicsSurfacePrivate {
         , m_fbConfig(0)
         , m_textureIsYInverted(false)
         , m_hasAlpha(false)
+        , m_isReceiver(true)
     { }
 
     ~GraphicsSurfacePrivate()
@@ -300,10 +301,9 @@ struct GraphicsSurfacePrivate {
 
     void swapBuffers()
     {
-        // If there is a xpixmap, we are on the reading side and do not want to swap any buffers.
         // The buffers are being switched on the writing side, the reading side just reads
         // whatever texture the XWindow contains.
-        if (m_xPixmap)
+        if (m_isReceiver)
             return;
 
         GLXContext glContext = glXGetCurrentContext();
@@ -344,12 +344,19 @@ struct GraphicsSurfacePrivate {
         pGlDeleteFramebuffers(1, &originFBO);
 
         glPopAttrib();
+
+        swapBuffers();
         doneCurrent();
     }
 
     Display* display() const { return m_display; }
 
-    GLXPixmap glxPixmap() const { return m_glxPixmap; }
+    GLXPixmap glxPixmap() const
+    {
+        if (!m_glxPixmap && m_surface)
+            const_cast<GraphicsSurfacePrivate*>(this)->createPixmap(m_surface);
+        return m_glxPixmap;
+    }
 
     IntSize size() const
     {
@@ -375,6 +382,7 @@ private:
     GLXFBConfig m_fbConfig;
     bool m_textureIsYInverted;
     bool m_hasAlpha;
+    bool m_isReceiver;
 };
 
 static bool resolveGLMethods(GraphicsSurfacePrivate*)
@@ -485,8 +493,6 @@ PassRefPtr<GraphicsSurface> GraphicsSurface::platformImport(const IntSize& size,
     surface->m_private = new GraphicsSurfacePrivate(surface->m_platformSurface);
     if (!resolveGLMethods(surface->m_private))
         return PassRefPtr<GraphicsSurface>();
-
-    surface->m_private->createPixmap(surface->m_platformSurface);
 
     return surface;
 }
