b6c83b96657b8ef5f61638b99ffe9d141fb3269f
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 16:45:30 +0000

    [Spellcheck] Removed reference to metrics service.
    UMA takes care of handling enabled/disabled reporting by itself, so no need to
    check that when creating a spellcheck service.
    
    R=mpearson@chromium.org, rlp@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11299186
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169965 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_factory.cc
+++ chrome/browser/spellchecker/spellcheck_factory.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/spellchecker/spellcheck_factory.h"
 
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/metrics/metrics_service.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
@@ -41,10 +40,8 @@ ProfileKeyedService* SpellcheckServiceFactory::BuildServiceInstanceFor(
   SpellcheckService* spellcheck = new SpellcheckService(profile);
 
   // Instantiates Metrics object for spellchecking for use.
-  if (g_browser_process->metrics_service() &&
-      g_browser_process->metrics_service()->recording_active())
-    spellcheck->StartRecordingMetrics(
-        profile->GetPrefs()->GetBoolean(prefs::kEnableSpellCheck));
+  spellcheck->StartRecordingMetrics(
+      profile->GetPrefs()->GetBoolean(prefs::kEnableSpellCheck));
 
   return spellcheck;
 }
