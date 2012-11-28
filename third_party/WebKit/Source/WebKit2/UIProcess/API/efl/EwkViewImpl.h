4fd483c6e79be8e7344ad328c0936d7cab629817
kenneth@webkit.org <kenneth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-28 13:09:34 +0000

    [EFL][WK2] MiniBrowser rendering should not get blurry when scrolled down with different scale values https://bugs.webkit.org/show_bug.cgi?id=103079
    Reviewed by Simon Hausmann.
    
    The problem was caused because we moved the page inside our viewport
    in non-discrete units.
    
    Now instead of changing the content visible in our viewport by
    applying scale, and then transforming the CSS offset, we now first
    discretely position the page in UI coordinates.
    
    This patch also removes some dead code.
    
    * UIProcess/API/efl/EwkViewImpl.cpp:
    (EwkViewImpl::transformFromScene):
    (EwkViewImpl::displayTimerFired):
    
       Transform according to the page position.
    
    * UIProcess/API/efl/EwkViewImpl.h:
    (EwkViewImpl::setPagePosition): Now stores a float.
    (EwkViewImpl::discretePagePosition): Returns the discrete page position.
    (EwkViewImpl):
    * UIProcess/efl/PageClientLegacyImpl.cpp:
    (WebKit::PageClientLegacyImpl::updateViewportSize):
    (WebKit::PageClientLegacyImpl::pageDidRequestScroll):
    * UIProcess/efl/PageViewportControllerClientEfl.cpp:
    (WebKit::PageViewportControllerClientEfl::PageViewportControllerClientEfl):
    (WebKit::PageViewportControllerClientEfl::setViewportPosition): Convert to page position.
    (WebKit::PageViewportControllerClientEfl::setContentsScale):
    (WebKit::PageViewportControllerClientEfl::didResumeContent):
    * UIProcess/efl/PageViewportControllerClientEfl.h:
    (WebKit::PageViewportControllerClientEfl::scaleFactor): Use the value
    from the view directly.
    (WebKit::PageViewportControllerClientEfl::contentPosition):
    (PageViewportControllerClientEfl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136000 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKGeometry.h"
 #include "WKRetainPtr.h"
 #include <Evas.h>
+#include <WebCore/FloatPoint.h>
 #include <WebCore/IntRect.h>
 #include <WebCore/TextDirection.h>
 #include <WebCore/Timer.h>
@@ -201,8 +202,8 @@ public:
     void setScaleFactor(float scaleFactor) { m_scaleFactor = scaleFactor; }
     float scaleFactor() const { return m_scaleFactor; }
 
-    void setScrollPosition(WebCore::IntPoint position) { m_scrollPosition = position; }
-    const WebCore::IntPoint scrollPosition() const { return m_scrollPosition; }
+    void setPagePosition(const WebCore::FloatPoint& position) { m_pagePosition = position; }
+    const WebCore::IntPoint discretePagePosition() const { return roundedIntPoint(m_pagePosition); }
 #endif
 
     // FIXME: needs refactoring (split callback invoke)
@@ -262,7 +263,7 @@ private:
     OwnPtr<EwkBackForwardList> m_backForwardList;
 #if USE(TILED_BACKING_STORE)
     float m_scaleFactor;
-    WebCore::IntPoint m_scrollPosition;
+    WebCore::FloatPoint m_pagePosition;
 #endif
     OwnPtr<EwkSettings> m_settings;
     RefPtr<EwkWindowFeatures> m_windowFeatures;
