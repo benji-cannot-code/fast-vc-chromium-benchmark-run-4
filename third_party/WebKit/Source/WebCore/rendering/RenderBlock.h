6f408cbf19e5c9428add656eee9da4219b09f5f2
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-28 22:46:59 +0000

    Source/WebCore: Heap-use-after-free in WebCore::RenderLayerModelObject::hasSelfPaintingL
    LayoutTests: Heap-use-after-free in WebCore::RenderLayerModelObject::hasSelfPaintingLayer
    https://bugs.webkit.org/show_bug.cgi?id=101970
    
    Reviewed by David Hyatt.
    
    * fast/block/float/float-not-removed-from-pre-block-expected.txt: Added.
    * fast/block/float/float-not-removed-from-pre-block.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
@@ -118,6 +118,8 @@ public:
     static void removePositionedObject(RenderBox*);
     void removePositionedObjects(RenderBlock*, ContainingBlockState = SameContainingBlock);
 
+    void removeFloatingObjects();
+
     TrackedRendererListHashSet* positionedObjects() const;
     bool hasPositionedObjects() const
     {
