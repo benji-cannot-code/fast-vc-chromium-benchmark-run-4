308103d4f2e1e2f16d11010f975adc47253a6bc7
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 06:09:02 +0000

    app_list: Non-triggerable mouse events should not start a drag.
    Right button down is not a triggerable event for CustomButton and should not
    initiate a drag as well.
    
    BUG=162727
    TEST=Verify the white flash is gone when opening context menu on app and unable
    to drag an app via right button (or two finger down).
    
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416213
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169863 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/app_list_item_view.cc
+++ ui/app_list/app_list_item_view.cc
@@ -272,6 +272,10 @@ bool AppListItemView::ShouldEnterPushedState(const ui::Event& event) {
 
 bool AppListItemView::OnMousePressed(const ui::MouseEvent& event) {
   CustomButton::OnMousePressed(event);
+
+  if (!ShouldEnterPushedState(event))
+    return true;
+
   apps_grid_view_->InitiateDrag(this, AppsGridView::MOUSE, event);
 
   if (apps_grid_view_->IsDraggedView(this)) {
