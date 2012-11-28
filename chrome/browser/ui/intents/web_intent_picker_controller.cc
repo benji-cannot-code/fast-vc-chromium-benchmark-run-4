89dc1ddfb9529e6aad21b96a315d34c5232d3348
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 19:19:01 +0000

    intents: Switch to non-deprecated SendReply() method.
    R=groby@chromium.org,gbillock@chromium.org
    TBR=jam@chromium.org # for content trivia change.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416242
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170015 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/intents/web_intent_picker_controller.cc
+++ chrome/browser/ui/intents/web_intent_picker_controller.cc
@@ -203,17 +203,17 @@ void WebIntentPickerController::ShowDialog(DefaultsUsage suppress_defaults) {
   // in-flight dispatches since we don't create the picker
   // in this method, but only after calling the registry.
   if (picker_shown_) {
-    intents_dispatcher_->SendReplyMessage(
+    intents_dispatcher_->SendReply(webkit_glue::WebIntentReply(
         webkit_glue::WEB_INTENT_REPLY_FAILURE,
-        ASCIIToUTF16("Simultaneous intent invocation."));
+        ASCIIToUTF16("Simultaneous intent invocation.")));
     return;
   }
 
   // TODO(binji): Figure out what to do when intents are invoked from incognito
   // mode.
   if (profile_->IsOffTheRecord()) {
-    intents_dispatcher_->SendReplyMessage(
-        webkit_glue::WEB_INTENT_REPLY_FAILURE, string16());
+    intents_dispatcher_->SendReply(webkit_glue::WebIntentReply(
+        webkit_glue::WEB_INTENT_REPLY_FAILURE, string16()));
     return;
   }
 
@@ -448,8 +448,8 @@ void WebIntentPickerController::OnUserCancelledPickerDialog() {
   if (!intents_dispatcher_)
     return;
 
-  intents_dispatcher_->SendReplyMessage(
-      webkit_glue::WEB_INTENT_PICKER_CANCELLED, string16());
+  intents_dispatcher_->SendReply(webkit_glue::WebIntentReply(
+      webkit_glue::WEB_INTENT_PICKER_CANCELLED, string16()));
   web_intents::RecordPickerCancel(uma_bucket_);
 
   ClosePicker();
@@ -611,9 +611,9 @@ void WebIntentPickerController::OnWebIntentServicesAvailableForExplicitIntent(
   }
 
   // No acceptable extension. The intent cannot be dispatched.
-  intents_dispatcher_->SendReplyMessage(
+  intents_dispatcher_->SendReply(webkit_glue::WebIntentReply(
       webkit_glue::WEB_INTENT_REPLY_FAILURE,  ASCIIToUTF16(
-          "Explicit extension URL is not available."));
+          "Explicit extension URL is not available.")));
 
   AsyncOperationFinished();
 }
@@ -850,9 +850,9 @@ void WebIntentPickerController::AsyncOperationFinished() {
 void WebIntentPickerController::InvokeServiceWithSelection(
     const webkit_glue::WebIntentServiceData& service) {
   if (picker_shown_) {
-    intents_dispatcher_->SendReplyMessage(
+    intents_dispatcher_->SendReply(webkit_glue::WebIntentReply(
         webkit_glue::WEB_INTENT_REPLY_FAILURE,
-        ASCIIToUTF16("Simultaneous intent invocation."));
+        ASCIIToUTF16("Simultaneous intent invocation.")));
     return;
   }
 
