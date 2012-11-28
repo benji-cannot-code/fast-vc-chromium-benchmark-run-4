ad775a9f38a17c129ba9d2ec7d042fad6228962e
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 22:49:46 +0000

    Remove compositor_test_support
    It is not needed anymore after https://codereview.chromium.org/11415089/
    
    BUG=159278
    
    
    Review URL: https://chromiumcodereview.appspot.com/11419166
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170082 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/bench/bench_main.cc
+++ ui/aura/bench/bench_main.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/compositor_observer.h"
 #include "ui/compositor/debug_utils.h"
 #include "ui/compositor/layer.h"
-#include "ui/compositor/test/compositor_test_support.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/rect.h"
 #include "ui/gfx/skia_util.h"
@@ -298,7 +297,6 @@ int main(int argc, char** argv) {
   ResourceBundle::InitSharedInstanceWithLocale("en-US", NULL);
 
   MessageLoop message_loop(MessageLoop::TYPE_UI);
-  ui::CompositorTestSupport::Initialize();
   aura::Env::GetInstance();
   aura::SetUseFullscreenHostWindow(true);
   aura::TestScreen test_screen;
@@ -356,8 +354,6 @@ int main(int argc, char** argv) {
   MessageLoopForUI::current()->Run();
   focus_client.reset();
   root_window.reset();
-
-  ui::CompositorTestSupport::Terminate();
 
   return 0;
 }
