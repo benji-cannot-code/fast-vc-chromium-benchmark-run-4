e78bc7d07be8dd8e11c240ebbdda7cdff536f726
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-28 02:03:17 +0000

    Fix build errors by r135955.
    * dom/ShadowRoot.cpp: Use struct instead of class.
    * dom/TreeScope.cpp: Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
+++ third_party/WebKit/Source/WebCore/dom/ShadowRoot.cpp
@@ -56,7 +56,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class SameSizeAsShadowRoot : public DocumentFragment, public TreeScope, public DoublyLinkedListNode<ShadowRoot> {
+struct SameSizeAsShadowRoot : public DocumentFragment, public TreeScope, public DoublyLinkedListNode<ShadowRoot> {
     void* pointers[3];
     unsigned countersAndFlags[4];
 };
