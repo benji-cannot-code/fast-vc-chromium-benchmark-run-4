bac636bb45411253336bf780230c22ec6667879a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 23:06:28 +0000

    touch: Add a new metric.
    BUG=162511
    
    Review URL: https://codereview.chromium.org/11416188
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170086 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/touch/touch_uma.cc
+++ ash/touch/touch_uma.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_delegate.h"
 #include "base/metrics/histogram.h"
 #include "base/stringprintf.h"
+#include "ui/aura/env.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
 #include "ui/aura/window_property.h"
@@ -248,7 +249,9 @@ UMAEventType UMAEventTypeFromEvent(const ui::Event& event) {
 namespace ash {
 namespace internal {
 
-TouchUMA::TouchUMA() {
+TouchUMA::TouchUMA()
+    : touch_in_progress_(false),
+      burst_length_(0) {
 }
 
 TouchUMA::~TouchUMA() {
@@ -284,6 +287,8 @@ void TouchUMA::RecordTouchEvent(aura::Window* target,
   UMA_HISTOGRAM_CUSTOM_COUNTS("Ash.TouchRadius",
       static_cast<int>(std::max(event.radius_x(), event.radius_y())),
       1, 500, 100);
+
+  UpdateBurstData(event);
 
   WindowTouchDetails* details = target->GetProperty(kWindowTouchDetails);
   if (!details) {
@@ -386,6 +391,28 @@ void TouchUMA::RecordTouchEvent(aura::Window* target,
 
     details->last_move_time_[event.touch_id()] = event.time_stamp();
     details->last_touch_position_[event.touch_id()] = event.location();
+  }
+}
+
+void TouchUMA::UpdateBurstData(const ui::TouchEvent& event) {
+  if (event.type() == ui::ET_TOUCH_PRESSED) {
+    if (!touch_in_progress_) {
+      base::TimeDelta difference = event.time_stamp() - last_touch_down_time_;
+      if (difference > base::TimeDelta::FromMilliseconds(250)) {
+        if (burst_length_) {
+          UMA_HISTOGRAM_COUNTS_100("Ash.TouchStartBurst",
+                                   std::min(burst_length_, 100));
+        }
+        burst_length_ = 1;
+      } else {
+        ++burst_length_;
+      }
+    }
+    touch_in_progress_ = true;
+    last_touch_down_time_ = event.time_stamp();
+  } else if (event.type() == ui::ET_TOUCH_RELEASED) {
+    if (!aura::Env::GetInstance()->is_touch_down())
+      touch_in_progress_ = false;
   }
 }
 
