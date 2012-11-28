7fd995ff991a485547b41a32e77eb02a6dc7a57a
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-28 01:56:33 +0000

    White space change.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169814 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/audio_manager_win.cc
+++ media/audio/win/audio_manager_win.cc
@@ -38,8 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Libraries required for the SetupAPI and Wbem APIs used here.
 #pragma comment(lib, "setupapi.lib")
 
-// The following are defined in various DDK headers, and we (re)define them
-// here to avoid adding the DDK as a chrome dependency.
+// The following are defined in various DDK headers, and we (re)define them here
+// to avoid adding the DDK as a chrome dependency.
 #define DRV_QUERYDEVICEINTERFACE 0x80c
 #define DRVM_MAPPER_PREFERRED_GET 0x2015
 #define DRV_QUERYDEVICEINTERFACESIZE 0x80d
