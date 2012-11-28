6be21a4949de7c8633bf4e2db7b05852eef3ae4f
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-28 00:34:19 +0000

    Shrink ShadowRoot and TreeScope. <http://webkit.org/b/103459>
    Reviewed by Antti Koivisto.
    
    We're creating ~1500 ShadowRoots on Apple's PLT3 content (from <input> and <textarea> elements)
    and a lot of this is wasted space.
    
    Shrink ShadowRoot and TreeScope a bit by making TreeScope's DocumentOrderedMaps heap-allocated
    and packing the counters on ShadowRoot harder together with the bits.
    
    202kB progression on Membuster3.
    
    * dom/ShadowRoot.cpp:
    (WebCore::ShadowRoot::ShadowRoot):
    * dom/ShadowRoot.h:
    (WebCore::ShadowRoot::countElementShadow):
    (ShadowRoot):
    * dom/TreeScope.cpp:
    (WebCore::TreeScope::TreeScope):
    (WebCore::TreeScope::getElementById):
    (WebCore::TreeScope::addElementById):
    (WebCore::TreeScope::removeElementById):
    (WebCore::TreeScope::addImageMap):
    (WebCore::TreeScope::removeImageMap):
    (WebCore::TreeScope::getImageMap):
    (WebCore::TreeScope::addLabel):
    (WebCore::TreeScope::removeLabel):
    (WebCore::TreeScope::labelElementForId):
    * dom/TreeScope.h:
    (WebCore::TreeScope::shouldCacheLabelsByForAttribute):
    (TreeScope):
    (WebCore::TreeScope::hasElementWithId):
    (WebCore::TreeScope::containsMultipleElementsWithId):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@135939 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/TreeScope.h
+++ third_party/WebKit/Source/WebCore/dom/TreeScope.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Google Inc. All Rights Reserved.
+ * Copyright (C) 2012 Apple Inc. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -65,7 +66,7 @@ public:
     HTMLMapElement* getImageMap(const String& url) const;
 
     // For accessibility.
-    bool shouldCacheLabelsByForAttribute() const { return m_shouldCacheLabelsByForAttribute; }
+    bool shouldCacheLabelsByForAttribute() const { return m_labelsByForAttribute; }
     void addLabel(const AtomicString& forAttributeValue, HTMLLabelElement*);
     void removeLabel(const AtomicString& forAttributeValue, HTMLLabelElement*);
     HTMLLabelElement* labelElementForId(const AtomicString& forAttributeValue);
@@ -101,11 +102,9 @@ private:
     ContainerNode* m_rootNode;
     TreeScope* m_parentTreeScope;
 
-    DocumentOrderedMap m_elementsById;
-    DocumentOrderedMap m_imageMapsByName;
-
-    DocumentOrderedMap m_labelsByForAttribute;
-    bool m_shouldCacheLabelsByForAttribute;
+    OwnPtr<DocumentOrderedMap> m_elementsById;
+    OwnPtr<DocumentOrderedMap> m_imageMapsByName;
+    OwnPtr<DocumentOrderedMap> m_labelsByForAttribute;
 
     OwnPtr<IdTargetObserverRegistry> m_idTargetObserverRegistry;
 
@@ -115,12 +114,12 @@ private:
 inline bool TreeScope::hasElementWithId(AtomicStringImpl* id) const
 {
     ASSERT(id);
-    return m_elementsById.contains(id);
+    return m_elementsById && m_elementsById->contains(id);
 }
 
 inline bool TreeScope::containsMultipleElementsWithId(const AtomicString& id) const
 {
-    return m_elementsById.containsMultiple(id.impl());
+    return m_elementsById && m_elementsById->containsMultiple(id.impl());
 }
 
 TreeScope* commonTreeScope(Node*, Node*);
