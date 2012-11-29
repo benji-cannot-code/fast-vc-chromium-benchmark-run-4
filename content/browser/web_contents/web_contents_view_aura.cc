611004147e03ed942a164d187fcbaad475e15783
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 07:36:47 +0000

    overscroll: Introduce some prefs that can be configured in chrome://gesture
    BUG=160668
    
    Review URL: https://codereview.chromium.org/11413098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170145 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_view_aura.cc
+++ content/browser/web_contents/web_contents_view_aura.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/notification_types.h"
+#include "content/public/browser/overscroll_configuration.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/render_widget_host_view.h"
@@ -469,9 +470,10 @@ gfx::Vector2d WebContentsViewAura::GetTranslationForOverscroll(int delta_x,
   if (current_overscroll_gesture_ == OVERSCROLL_NORTH ||
       current_overscroll_gesture_ == OVERSCROLL_SOUTH) {
     // For vertical overscroll, always do a resisted drag.
-    const int kVerticalOverscrollAmount = 30;
+    const float threshold = GetOverscrollConfig(
+        OVERSCROLL_CONFIG_VERT_RESIST_AFTER);
     int scroll = GetResistedScrollAmount(abs(delta_y),
-                                         kVerticalOverscrollAmount);
+                                         static_cast<int>(threshold));
     return gfx::Vector2d(0, delta_y < 0 ? -scroll : scroll);
   }
 
@@ -486,9 +488,10 @@ gfx::Vector2d WebContentsViewAura::GetTranslationForOverscroll(int delta_x,
       return gfx::Vector2d(delta_x, 0);
   }
 
-  const int kHorizontalOverscrollAmount = 30;
+  const float threshold = GetOverscrollConfig(
+      OVERSCROLL_CONFIG_HORIZ_RESIST_AFTER);
   int scroll = GetResistedScrollAmount(abs(delta_x),
-                                       kHorizontalOverscrollAmount);
+                                       static_cast<int>(threshold));
   return gfx::Vector2d(delta_x < 0 ? -scroll : scroll, 0);
 }
 
