79d6ccbe786f968c6c502e8dd7a6817c5ff59a47
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 22:37:02 +0000

    Fix unallowed I/O in URLRequestMockLinkDoctorJob
    This just follows the established pattern for similar mocks.
    
    BUG=121750
    
    Review URL: https://codereview.chromium.org/11412045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170258 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/url_request_mock_link_doctor_job.h
+++ chrome/browser/net/url_request_mock_link_doctor_job.h
@@ -18,7 +18,7 @@ class URLRequestMockLinkDoctorJob : public content::URLRequestMockHTTPJob {
   static net::URLRequest::ProtocolFactory Factory;
 
   // Adds the testing URLs to the net::URLRequestFilter.
-  static void AddUrlHandler();
+  static void AddUrlHandler(const FilePath& base_path);
 
  private:
   virtual ~URLRequestMockLinkDoctorJob() {}
