546571cd0004ad24fba76435785815a603c835e7
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-29 15:00:49 +0000

    Possible to resize out of bounds https://bugs.webkit.org/show_bug.cgi?id=103521
    Reviewed by Jocelyn Turcotte.
    
    Enforce the viewportBounds after resize, and ensure a user fitted page remains
    fit on viewport resize, but not on content growth.
    
    * UIProcess/API/qt/tests/qmltests/WebView/tst_resize.qml: Added.
    * UIProcess/PageViewportController.cpp:
    (WebKit::PageViewportController::didChangeContentsSize):
    (WebKit::PageViewportController::didChangeViewportAttributes):
    (WebKit::PageViewportController::updateMinimumScaleToFit):
    * UIProcess/PageViewportController.h:
    (PageViewportController):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.cpp
@@ -112,7 +112,7 @@ void PageViewportController::didChangeContentsSize(const IntSize& newSize)
 {
     m_contentsSize = newSize;
 
-    bool minimumScaleUpdated = updateMinimumScaleToFit();
+    bool minimumScaleUpdated = updateMinimumScaleToFit(false);
 
     if (m_initiallyFitToViewport) {
         // Restrict scale factors to m_minimumScaleToFit.
@@ -233,7 +233,7 @@ void PageViewportController::didChangeViewportAttributes(const WebCore::Viewport
     if (!m_initiallyFitToViewport)
         WebCore::restrictScaleFactorToInitialScaleIfNotUserScalable(m_rawAttributes);
 
-    if (updateMinimumScaleToFit())
+    if (updateMinimumScaleToFit(true))
         m_client->didChangeViewportAttributes();
 
     syncVisibleContents();
@@ -278,10 +278,12 @@ void PageViewportController::applyPositionAfterRenderingContents(const FloatPoin
     syncVisibleContents();
 }
 
-bool PageViewportController::updateMinimumScaleToFit()
+bool PageViewportController::updateMinimumScaleToFit(bool userInitiatedUpdate)
 {
     if (m_viewportSize.isEmpty() || m_contentsSize.isEmpty())
         return false;
+
+    bool currentlyScaledToFit = fuzzyCompare(m_effectiveScale, toViewportScale(m_minimumScaleToFit), 0.001);
 
     float minimumScale = WebCore::computeMinimumScaleFactorForContentContained(m_rawAttributes, WebCore::roundedIntSize(m_viewportSize), WebCore::roundedIntSize(m_contentsSize), devicePixelRatio());
 
@@ -291,8 +293,16 @@ bool PageViewportController::updateMinimumScaleToFit()
     if (!fuzzyCompare(minimumScale, m_minimumScaleToFit, 0.001)) {
         m_minimumScaleToFit = minimumScale;
 
-        if (!m_hadUserInteraction && !hasSuspendedContent())
-            applyScaleAfterRenderingContents(toViewportScale(minimumScale));
+        if (!hasSuspendedContent()) {
+            if (!m_hadUserInteraction || (userInitiatedUpdate && currentlyScaledToFit))
+                applyScaleAfterRenderingContents(toViewportScale(m_minimumScaleToFit));
+            else {
+                // Ensure the effective scale stays within bounds.
+                float boundedScale = innerBoundedViewportScale(m_effectiveScale);
+                if (!fuzzyCompare(boundedScale, m_effectiveScale, 0.001))
+                    applyScaleAfterRenderingContents(boundedScale);
+            }
+        }
 
         return true;
     }
--- third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/PageViewportController.h
@@ -85,7 +85,7 @@ private:
     void syncVisibleContents(const WebCore::FloatPoint &trajectoryVector = WebCore::FloatPoint::zero());
     void applyScaleAfterRenderingContents(float scale);
     void applyPositionAfterRenderingContents(const WebCore::FloatPoint& pos);
-    bool updateMinimumScaleToFit();
+    bool updateMinimumScaleToFit(bool userInitiatedUpdate);
     WebCore::FloatSize viewportSizeInContentsCoordinates() const;
 
     WebPageProxy* const m_webPageProxy;
