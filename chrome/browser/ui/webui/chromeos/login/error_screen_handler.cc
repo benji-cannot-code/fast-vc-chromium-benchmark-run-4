a46dd0b24eba80167f2d431394f25cf8f9d670f8
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 10:00:40 +0000

    VLOG(1) -> LOG({INFO|WARNING})
    BUG=123579
    TEST=Linux ChromeOS manual testing
    
    
    Review URL: https://chromiumcodereview.appspot.com/11418176
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170154 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/error_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/error_screen_handler.cc
@@ -86,9 +86,9 @@ void ErrorScreenHandler::UpdateState(NetworkStateInformer::State state,
                                      const std::string& network_name,
                                      const std::string& reason,
                                      ConnectionType last_network_type) {
-  VLOG(1) << "ErrorScreenHandler::UpdateState(): state=" << state << ", "
-          << "network_name=" << network_name << ", reason=" << reason << ", "
-          << "last_network_type=" << last_network_type;
+  LOG(INFO) << "ErrorScreenHandler::UpdateState(): state=" << state << ", "
+            << "network_name=" << network_name << ", reason=" << reason << ", "
+            << "last_network_type=" << last_network_type;
   UpdateStateInternal(state, network_name, reason, last_network_type, false);
 }
 
@@ -155,7 +155,7 @@ void ErrorScreenHandler::UpdateStateInternal(NetworkStateInformer::State state,
     // Schedules a immediate retry.
     ReloadGaiaScreen();
     is_gaia_reloaded = true;
-    VLOG(1) << "Retry page load since proxy settings has been changed";
+    LOG(WARNING) << "Retry page load since proxy settings has been changed";
   }
 
   // Fake portal state for loading timeout.
@@ -173,9 +173,10 @@ void ErrorScreenHandler::UpdateStateInternal(NetworkStateInformer::State state,
   }
 
   if (!is_online && should_overlay) {
-    VLOG(1) << "Show offline message: state=" << state << ", "
-            << "network_name=" << network_name << ", reason=" << reason << ", "
-            << "is_under_captive_portal=" << is_under_captive_portal;
+    LOG(WARNING) << "Show offline message: state=" << state << ", "
+                 << "network_name=" << network_name << ", "
+                 << "reason=" << reason << ", "
+                 << "is_under_captive_portal=" << is_under_captive_portal;
     ClearOobeErrors();
     OnBeforeShow(last_network_type);
 
@@ -211,8 +212,8 @@ void ErrorScreenHandler::UpdateStateInternal(NetworkStateInformer::State state,
     HideCaptivePortal();
 
     if (is_shown()) {
-      VLOG(1) << "Hide offline message. state=" << state << ", "
-              << "network_name=" << network_name << ", reason=" << reason;
+      LOG(WARNING) << "Hide offline message. state=" << state << ", "
+                   << "network_name=" << network_name << ", reason=" << reason;
       OnBeforeHide();
       Hide();
 
