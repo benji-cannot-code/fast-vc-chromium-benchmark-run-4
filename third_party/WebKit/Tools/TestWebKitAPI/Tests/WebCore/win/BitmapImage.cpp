148c17b5c3a7f057714ae8b8c543919f9e486f94
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-29 23:40:23 +0000

    [Windows, WinCairo] Unreviewed build correction. Correct debug build case on build machine.
    * TestWebKitAPI/Tests/WebCore/win/BitmapImage.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136177 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/Tests/WebCore/win/BitmapImage.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/Tests/WebCore/win/BitmapImage.cpp
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
+#ifndef ASSERT_DISABLED
 #define ASSERT_DISABLED
+#endif
 #include <WebCore/BitmapImage.h>
 
 using namespace WebCore;
