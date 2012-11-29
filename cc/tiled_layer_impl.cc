29dcbee1210a7db2d0bae688f4f4c38fe5ef4180
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 17:53:30 +0000

    cc: Add debug borders / checkerboards for impl-side painting
    BUG=155209
    
    
    Review URL: https://chromiumcodereview.appspot.com/11428050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170220 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/tiled_layer_impl.cc
+++ cc/tiled_layer_impl.cc
@@ -24,9 +24,6 @@ using namespace std;
 
 namespace cc {
 
-// Non-debug checkerboards are grey.
-const SkColor kTileCheckerboardColor = SkColorSetRGB(241, 241, 241);
-
 class DrawableTile : public LayerTilingData::Tile {
 public:
     static scoped_ptr<DrawableTile> create() { return make_scoped_ptr(new DrawableTile()); }
@@ -160,7 +157,7 @@ void TiledLayerImpl::appendQuads(QuadSink& quadSink, AppendQuadsData& appendQuad
                     if (showDebugBorders())
                         checkerColor = tile ? DebugColors::InvalidatedTileCheckerboardColor() : DebugColors::EvictedTileCheckerboardColor();
                     else
-                        checkerColor = kTileCheckerboardColor;
+                        checkerColor = DebugColors::DefaultCheckerboardColor();
 
                     scoped_ptr<CheckerboardDrawQuad> checkerboardQuad = CheckerboardDrawQuad::Create();
                     checkerboardQuad->SetNew(sharedQuadState, tileRect, checkerColor);
