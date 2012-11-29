6994f60f81f1adc6e854ed3ee77643ff174901c9
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 12:51:48 +0000

    Show a blocking page/interstitial when blocking requests in managed mode.
    TBR=ben@chromium.org,willchan@chromium.org
    BUG=162375
    
    
    Review URL: https://chromiumcodereview.appspot.com/11419136
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170169 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
@@ -53,6 +53,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_ANDROID)
 #include "content/components/navigation_interception/intercept_navigation_delegate.h"
+#else
+#include "chrome/browser/managed_mode/managed_mode_resource_throttle.h"
 #endif
 
 // TODO(oshima): Enable this for other platforms.
@@ -293,6 +295,11 @@ void ChromeResourceDispatcherHostDelegate::AppendStandardResourceThrottles(
     throttles->push_back(SafeBrowsingResourceThrottleFactory::Create(
         request, child_id, route_id, is_subresource_request, safe_browsing_));
   }
+#endif
+
+#if !defined(OS_ANDROID)
+  throttles->push_back(new ManagedModeResourceThrottle(
+        request, child_id, route_id, !is_subresource_request));
 #endif
 
   content::ResourceThrottle* throttle =
---
+++ chrome/browser/resources/managed_mode_block_interstitial.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function sendCommand(cmd) {
+  window.domAutomationController.setAutomationId(1);
+  window.domAutomationController.send(cmd);
+}
+
+function initialize() {
+  $('bypass-block-button').onclick = function(event) {
+    sendCommand('preview');
+  }
+  $('back-button').onclick = function(event) {
+    sendCommand('back');
+  };
+  $('content-packs-section-button').onclick = function(event) {
+    sendCommand('ntp');
+  };
+}
+
+document.addEventListener('DOMContentLoaded', initialize);
