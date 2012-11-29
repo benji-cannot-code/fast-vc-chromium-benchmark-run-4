95e8616daf69a3b8660a032378ee4d08b8858e3e
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-29 06:05:36 +0000

    Perform install tasks for newly installed or upgraded component apps/extensions. This ensures, e.g., that background pages for component extensions are run and that their event listeners are registered appropriately. The change also saves manifest information into profile preferences for component applications so that the version can be tracked, which was not previously done.
    BUG=157717
    TEST=browser_test PlatformAppBrowserTest.ComponentAppBackgroundPages
    
    Review URL: https://chromiumcodereview.appspot.com/11275069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_url_rewrite_browsertest.cc
+++ chrome/browser/extensions/extension_url_rewrite_browsertest.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process_util.h"
 #include "base/sys_string_conversions.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/browser/extensions/component_loader.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
@@ -25,6 +26,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::NavigationEntry;
 
 class ExtensionURLRewriteBrowserTest : public ExtensionBrowserTest {
+ public:
+  virtual void SetUp() OVERRIDE {
+    extensions::ComponentLoader::EnableBackgroundExtensionsForTesting();
+    ExtensionBrowserTest::SetUp();
+  }
+
  protected:
   std::string GetLocationBarText() const {
     return UTF16ToUTF8(
