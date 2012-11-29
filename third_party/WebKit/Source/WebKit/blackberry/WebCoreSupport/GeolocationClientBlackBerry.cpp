710a65edd6c11969a59694789bbd6800c3339aa8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-29 22:13:29 +0000

    [BlackBerry] Modifying GeoClientBlackBerry to switch between high and low accuracy https://bugs.webkit.org/show_bug.cgi?id=103666
    Patch by Otto Derek Cheung <otcheung@rim.com> on 2012-11-29
    Reviewed by Yong Li.
    
    Internally reviewed by Yong Li.
    
    PR 243420
    
    Updating GeolocationClientBlackBerry to switch accuracy when GeolocationController
    calls setEnableHighAccuracy.
    
    * WebCoreSupport/GeolocationClientBlackBerry.cpp:
    (GeolocationClientBlackBerry::startUpdating):
    (GeolocationClientBlackBerry::setEnableHighAccuracy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/GeolocationClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/GeolocationClientBlackBerry.cpp
@@ -53,7 +53,7 @@ void GeolocationClientBlackBerry::geolocationDestroyed()
 void GeolocationClientBlackBerry::startUpdating()
 {
     if (!m_isActive)
-        BlackBerry::Platform::GeolocationHandler::instance()->addListener(this, false);
+        BlackBerry::Platform::GeolocationHandler::instance()->addListener(this);
     m_isActive = true;
 }
 
@@ -130,7 +130,9 @@ void GeolocationClientBlackBerry::onPermission(bool isAllowed)
 
 void GeolocationClientBlackBerry::setEnableHighAccuracy(bool newAccuracy)
 {
-    // FIXME: we have to implement high accuracy on our side too
-    m_accuracy = newAccuracy;
+    if (m_accuracy != newAccuracy) {
+        m_accuracy = newAccuracy;
+        BlackBerry::Platform::GeolocationHandler::instance()->switchAccuracy(this);
+    }
 }
 
