948bc4271b7b8318dd18cba9c5e7fbfec319e355
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-29 17:07:34 +0000

    GraphicsSurfaceGLX: remove redundant parameter from resolveGLMethods. https://bugs.webkit.org/show_bug.cgi?id=103636
    Reviewed by Kenneth Rohde Christiansen.
    
    * platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp:
    (WebCore::resolveGLMethods):
    (WebCore::GraphicsSurface::platformCreate):
    (WebCore::GraphicsSurface::platformImport):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136135 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/surfaces/qt/GraphicsSurfaceGLX.cpp
@@ -385,7 +385,7 @@ private:
     bool m_isReceiver;
 };
 
-static bool resolveGLMethods(GraphicsSurfacePrivate*)
+static bool resolveGLMethods()
 {
     static bool resolved = false;
     if (resolved)
@@ -471,7 +471,7 @@ PassRefPtr<GraphicsSurface> GraphicsSurface::platformCreate(const IntSize& size,
     RefPtr<GraphicsSurface> surface = adoptRef(new GraphicsSurface(size, flags));
 
     surface->m_private = new GraphicsSurfacePrivate(shareContext);
-    if (!resolveGLMethods(surface->m_private))
+    if (!resolveGLMethods())
         return PassRefPtr<GraphicsSurface>();
 
     surface->m_platformSurface = surface->m_private->createSurface(size);
@@ -491,7 +491,7 @@ PassRefPtr<GraphicsSurface> GraphicsSurface::platformImport(const IntSize& size,
     surface->m_platformSurface = token.frontBufferHandle;
 
     surface->m_private = new GraphicsSurfacePrivate(surface->m_platformSurface);
-    if (!resolveGLMethods(surface->m_private))
+    if (!resolveGLMethods())
         return PassRefPtr<GraphicsSurface>();
 
     return surface;
