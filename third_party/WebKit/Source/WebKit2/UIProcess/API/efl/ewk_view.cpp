da7a0ea573732ae8ffee31d7fa13e03901749dd3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-29 07:00:22 +0000

    Unreviewed, rolling out r135973. http://trac.webkit.org/changeset/135973 https://bugs.webkit.org/show_bug.cgi?id=103604
    It made API test crash on EFL bots (Requested by gyuyoung on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-11-28
    
    * UIProcess/API/efl/ewk_view.cpp:
    (createEwkView):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136102 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_view.cpp
@@ -509,7 +509,8 @@ static inline Evas_Object* createEwkView(Evas* canvas, Evas_Smart* smart, PassRe
     }
 
     ASSERT(!smartData->priv);
-    smartData->priv = new EwkViewImpl(ewkView, context, toImpl(pageGroupRef), behavior);
+    RefPtr<WebPageGroup> pageGroup = pageGroupRef ? toImpl(pageGroupRef) : WebPageGroup::create();
+    smartData->priv = new EwkViewImpl(ewkView, context, pageGroup, behavior);
     return ewkView;
 }
 
