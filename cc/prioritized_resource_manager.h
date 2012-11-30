4cc93fc5f6115ae9aa52c685fe8a853bb03ef16c
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 05:21:14 +0000

    Use a mutex to deal with concurrent access to the m_evictedBackings list, instead of passing the list of pointers around.
    In ThreadProxy::beginFrame, we unlink all evicted textures, so that
    the call to updateLayers will know to re-paint the evicted layers.
    
    In ThreadProxy::scheduleActionCommit, we determine if we can draw now
    by checking to see if there exist any linked evicted textures. If there
    are none, we can draw what we are in the process of committing.
    
    This assumes that once we get to m_layerTreeHost->willBeginFrame()
    in ThreadProxy::beginFrame, the next frame to be commited by
    ThreadProxy::scheduleActionCommit be at or after the frame that is
    being produced by that call to ThreadProxy::beginFrame.
    
    BUG=158747
    
    
    Review URL: https://chromiumcodereview.appspot.com/11411251
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170403 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/prioritized_resource_manager.h
+++ cc/prioritized_resource_manager.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/hash_tables.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/synchronization/lock.h"
 #include "cc/cc_export.h"
 #include "cc/proxy.h"
 #include "cc/prioritized_resource.h"
@@ -83,16 +84,17 @@ public:
     // be called on the impl thread while the main thread is running. Returns true if resources are
     // indeed evicted as a result of this call.
     bool reduceMemoryOnImplThread(size_t limitBytes, int priorityCutoff, ResourceProvider*);
+
     // Returns true if there exist any textures that are linked to backings that have had their
     // resources evicted. Only when we commit a tree that has no textures linked to evicted backings
-    // may we allow drawing.
+    // may we allow drawing. After an eviction, this will not become true until
+    // unlinkAndClearEvictedBackings is called.
     bool linkedEvictedBackingsExist() const;
-    // Retrieve the list of all contents textures' backings that have been evicted, to pass to the
-    // main thread to unlink them from their owning textures.
-    void getEvictedBackings(BackingList& evictedBackings);
-    // Unlink the list of contents textures' backings from their owning textures on the main thread
-    // before updating layers.
-    void unlinkEvictedBackings(const BackingList& evictedBackings);
+
+    // Unlink the list of contents textures' backings from their owning textures and delete the evicted
+    // backings' structures. This is called just before updating layers, and is only ever called on the
+    // main thread.
+    void unlinkAndClearEvictedBackings();
 
     bool requestLate(PrioritizedResource*);
 
@@ -160,7 +162,6 @@ private:
                                      ResourceProvider*);
     PrioritizedResource::Backing* createBacking(gfx::Size, GLenum format, ResourceProvider*);
     void evictFirstBackingResource(ResourceProvider*);
-    void deleteAllEvictedBackings();
     void sortBackings();
 
     void assertInvariants();
@@ -189,6 +190,11 @@ private:
     // are not sorted by priority.
     BackingList m_backings;
     bool m_backingsTailNotSorted;
+
+    // The list of backings that have been evicted, but may still be linked
+    // to textures. This can be accessed concurrently by the main and impl
+    // threads, and may only be accessed while holding m_evictedBackingsLock.
+    mutable base::Lock m_evictedBackingsLock;
     BackingList m_evictedBackings;
 
     TextureVector m_tempTextureVector;
