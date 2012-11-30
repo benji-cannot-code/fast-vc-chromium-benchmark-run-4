fd7acc4f523fe4b2d79e428de77f44edf6d821ed
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 21:32:24 +0000

    Track merged nudge sources
    There's a lot of valuable information in coalesced nudges.  Currently, one
    nudge source can overwrite another, which leaves us in the dark as to why the
    client behaved a certain way.  In fact, today we can't even determine whether
    or not any coalescing was done.  
    
    By logging all the coalesced sources and their payloads, we can learn a lot
    more about client behaviour.  I'm hoping to use this to improve our
    notification effectiveness metrics.
    
    BUG=138613
    
    Review URL: https://chromiumcodereview.appspot.com/11416126
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170549 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/public/sessions/sync_session_snapshot.h
+++ sync/internal_api/public/sessions/sync_session_snapshot.h
@@ -40,6 +40,7 @@ class SyncSessionSnapshot {
       int num_hierarchy_conflicts,
       int num_server_conflicts,
       const SyncSourceInfo& source,
+      const std::vector<SyncSourceInfo>& debug_info_sources_list,
       bool notifications_enabled,
       size_t num_entries,
       base::Time sync_start_time,
@@ -64,6 +65,7 @@ class SyncSessionSnapshot {
   int num_hierarchy_conflicts() const;
   int num_server_conflicts() const;
   SyncSourceInfo source() const;
+  const std::vector<SyncSourceInfo>& debug_info_sources_list() const;
   bool notifications_enabled() const;
   size_t num_entries() const;
   base::Time sync_start_time() const;
@@ -83,6 +85,7 @@ class SyncSessionSnapshot {
   int num_hierarchy_conflicts_;
   int num_server_conflicts_;
   SyncSourceInfo source_;
+  std::vector<SyncSourceInfo> debug_info_sources_list_;
   bool notifications_enabled_;
   size_t num_entries_;
   base::Time sync_start_time_;
