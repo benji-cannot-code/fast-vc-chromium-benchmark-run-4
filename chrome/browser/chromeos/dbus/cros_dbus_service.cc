74bad63bc6de910a049430ed64b9e7aefa958567
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 01:34:43 +0000

    chromeos: Add LivenessServiceProvider.
    This exports a CheckLiveness D-Bus method that the session
    manager will call to confirm that Chrome's UI thread is
    responding to D-Bus messages.  This is intended to replace
    the LivenessRequested signal.
    
    BUG=chromium-os:35729,chromium-os:36686
    TEST=run dbus-send command from header
    TBR=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416244
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170329 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/dbus/cros_dbus_service.cc
+++ chrome/browser/chromeos/dbus/cros_dbus_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/chromeos/chromeos_version.h"
 #include "base/stl_util.h"
 #include "base/threading/platform_thread.h"
+#include "chrome/browser/chromeos/dbus/liveness_service_provider.h"
 #include "chrome/browser/chromeos/dbus/proxy_resolution_service_provider.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "dbus/bus.h"
@@ -110,6 +111,7 @@ void CrosDBusService::Initialize() {
   if (base::chromeos::IsRunningOnChromeOS() && bus) {
     CrosDBusServiceImpl* service = new CrosDBusServiceImpl(bus);
     service->RegisterServiceProvider(ProxyResolutionServiceProvider::Create());
+    service->RegisterServiceProvider(new LivenessServiceProvider);
     g_cros_dbus_service = service;
     service->Start();
   } else {
--- chrome/chrome_browser_chromeos.gypi
+++ chrome/chrome_browser_chromeos.gypi
@@ -192,6 +192,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/display/primary_display_switch_observer.h',
         'browser/chromeos/dbus/cros_dbus_service.cc',
         'browser/chromeos/dbus/cros_dbus_service.h',
+        'browser/chromeos/dbus/liveness_service_provider.cc',
+        'browser/chromeos/dbus/liveness_service_provider.h',
         'browser/chromeos/dbus/proxy_resolution_service_provider.cc',
         'browser/chromeos/dbus/proxy_resolution_service_provider.h',
         'browser/chromeos/device_hierarchy_observer.h',
