dadd1c00a471f2160bb0d29ad5bcd8286b42dd10
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 05:15:57 +0000

    google_apis: Move AuthOperation to auth_service.cc
    AuthOperation was only used in auth_service, hence there is no need to have
    this class in base_operations.h
    
    Along the way, remove an unused function Authenticate() from OperationRunner,
    and make some functions private there.
    
    Remove AuthenticateStub from MockDriveService as well.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/11316262
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170399 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/operation_runner.h
+++ chrome/browser/google_apis/operation_runner.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
+#include "chrome/browser/google_apis/auth_service.h"
 #include "chrome/browser/google_apis/base_operations.h"
 #include "chrome/browser/google_apis/gdata_errorcode.h"
 
@@ -47,16 +48,12 @@ class OperationRunner {
   // Cancels all in-flight operations.
   void CancelAll();
 
-  // Authenticates the user by fetching the auth token as needed. |callback|
-  // will be run with the error code and the auth token, on the thread this
-  // function is run.
-  void Authenticate(const AuthStatusCallback& callback);
-
   // Starts an operation implementing the AuthenticatedOperationInterface
   // interface, and makes the operation retry upon authentication failures by
   // calling back to RetryOperation.
   void StartOperationWithRetry(AuthenticatedOperationInterface* operation);
 
+ private:
   // Starts an operation implementing the AuthenticatedOperationInterface
   // interface.
   void StartOperation(AuthenticatedOperationInterface* operation);
@@ -71,7 +68,6 @@ class OperationRunner {
   // forces an authentication token refresh.
   void RetryOperation(AuthenticatedOperationInterface* operation);
 
- private:
   Profile* profile_;  // not owned
 
   scoped_ptr<AuthService> auth_service_;
