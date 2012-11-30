fdc781404d1025e7c16e276d95edae63792922c2
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 18:46:19 +0000

    Clean Up: remove unused variable selection_item_id
    There is no usage of selection_item_id and radio grouping logic is in engine.
    So it is safe to remove selection_item_id from Chrome.
    
    I checked property behaviors and there is no regression on Japanese/Simplified Chinese/Traditional Chinese/Hangul.
    (including radio handling, property order, activation handling etc...)
    
    I also checked any properties in all m17n engines, but there is no property.
    
    BUG=None
    TEST=Manually done on lumpy
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416261
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_manager_impl_unittest.cc
+++ chrome/browser/chromeos/input_method/input_method_manager_impl_unittest.cc
@@ -560,8 +560,7 @@ TEST_F(InputMethodManagerImplTest, TestGetCurrentInputMethodProperties) {
   current_property_list.push_back(InputMethodProperty("key",
                                                       "label",
                                                       false,
-                                                      false,
-                                                      -1));
+                                                      false));
   controller_->SetCurrentPropertiesForTesting(current_property_list);
   controller_->NotifyPropertyChangedForTesting();
 
@@ -593,8 +592,7 @@ TEST_F(InputMethodManagerImplTest, TestGetCurrentInputMethodPropertiesTwoImes) {
   current_property_list.push_back(InputMethodProperty("key-mozc",
                                                       "label",
                                                       false,
-                                                      false,
-                                                      -1));
+                                                      false));
   controller_->SetCurrentPropertiesForTesting(current_property_list);
   controller_->NotifyPropertyChangedForTesting();
 
@@ -610,8 +608,7 @@ TEST_F(InputMethodManagerImplTest, TestGetCurrentInputMethodPropertiesTwoImes) {
   current_property_list.push_back(InputMethodProperty("key-chewing",
                                                       "label",
                                                       false,
-                                                      false,
-                                                      -1));
+                                                      false));
   controller_->SetCurrentPropertiesForTesting(current_property_list);
   controller_->NotifyPropertyChangedForTesting();
   ASSERT_EQ(1U, manager_->GetCurrentInputMethodProperties().size());
