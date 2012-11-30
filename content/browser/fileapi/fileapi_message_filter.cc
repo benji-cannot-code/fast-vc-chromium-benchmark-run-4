a8572995577252b393859f5772d518ffc803348f
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 09:47:02 +0000

    Quota calculation is wrong in cross-filesystem operation
    We fire update/change notifications to the observers attached to the FileSystemOperationContext, but the current code creates a new FileSystemOperation (and thus FileSystemOperationContext) for the source URL. This results in no update/change modifications in the destination filesystem in cross-filesystem operation cases.
    
    BUG=163540
    TEST=manual
    
    Review URL: https://codereview.chromium.org/11316265
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170428 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/fileapi/fileapi_message_filter.cc
+++ content/browser/fileapi/fileapi_message_filter.cc
@@ -237,7 +237,7 @@ void FileAPIMessageFilter::OnMove(
     return;
   }
 
-  FileSystemOperation* operation = GetNewOperation(src_url, request_id);
+  FileSystemOperation* operation = GetNewOperation(dest_url, request_id);
   if (!operation)
     return;
   operation->Move(
@@ -257,7 +257,7 @@ void FileAPIMessageFilter::OnCopy(
     return;
   }
 
-  FileSystemOperation* operation = GetNewOperation(src_url, request_id);
+  FileSystemOperation* operation = GetNewOperation(dest_url, request_id);
   if (!operation)
     return;
   operation->Copy(
