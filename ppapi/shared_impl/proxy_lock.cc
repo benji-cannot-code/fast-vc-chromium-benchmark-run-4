d08c30f17b7109d3f618b62c56a87848b30f53fb
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 19:44:52 +0000

    [ppapi] Simple deadlock detector for proxy lock.
    base::lock provides a simple detector for single-thread deadlock in
    debug mode.  Implement a similar approach for the proxy lock in debug
    or release.
    
    BUG=161286
    
    
    Review URL: https://chromiumcodereview.appspot.com/11280041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170513 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/shared_impl/proxy_lock.cc
+++ ppapi/shared_impl/proxy_lock.cc
@@ -5,30 +5,54 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/shared_impl/proxy_lock.h"
 
+#include "base/lazy_instance.h"
 #include "base/synchronization/lock.h"
+#include "base/threading/thread_local.h"
 #include "ppapi/shared_impl/ppapi_globals.h"
 
 namespace ppapi {
 
+// Simple single-thread deadlock detector for the proxy lock.
+// |true| when the current thread has the lock.
+base::LazyInstance<base::ThreadLocalBoolean>::Leaky
+    g_proxy_locked_on_thread = LAZY_INSTANCE_INITIALIZER;
+
 // static
 void ProxyLock::Acquire() {
   base::Lock* lock(PpapiGlobals::Get()->GetProxyLock());
-  if (lock)
+  if (lock) {
+    // This thread does not already hold the lock.
+    const bool deadlock = g_proxy_locked_on_thread.Get().Get();
+    CHECK(!deadlock);
+
     lock->Acquire();
+    g_proxy_locked_on_thread.Get().Set(true);
+  }
 }
 
 // static
 void ProxyLock::Release() {
   base::Lock* lock(PpapiGlobals::Get()->GetProxyLock());
-  if (lock)
+  if (lock) {
+    // This thread currently holds the lock.
+    const bool locked = g_proxy_locked_on_thread.Get().Get();
+    CHECK(locked);
+
+    g_proxy_locked_on_thread.Get().Set(false);
     lock->Release();
+  }
 }
 
 // static
 void ProxyLock::AssertAcquired() {
   base::Lock* lock(PpapiGlobals::Get()->GetProxyLock());
-  if (lock)
+  if (lock) {
+    // This thread currently holds the lock.
+    const bool locked = g_proxy_locked_on_thread.Get().Get();
+    CHECK(locked);
+
     lock->AssertAcquired();
+  }
 }
 
 void CallWhileUnlocked(const base::Closure& closure) {
