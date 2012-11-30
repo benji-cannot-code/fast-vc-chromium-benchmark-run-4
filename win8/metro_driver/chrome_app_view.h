3387ab5e45e8ad99e40f396e6120282988eff327
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 21:42:32 +0000

    Fix Aura Release compile break in metro_driver.
    The breakpad export has been unused for the last five months, so I suppose it's safe to remove it entirely.
    
    BUG=162405
    TEST=Chromium Win (aura) bot should be able to compile metro_driver.dll.
    
    Review URL: https://chromiumcodereview.appspot.com/11412265
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170555 0039d316-1c4b-4281-b951-d872f2087c98

--- win8/metro_driver/chrome_app_view.h
+++ win8/metro_driver/chrome_app_view.h
@@ -140,9 +140,6 @@ class ChromeAppView
   MetroDialogBox dialog_box_;
 };
 
-// This function is exported by chrome.exe.
-typedef int (__cdecl *BreakpadExceptionHandler)(EXCEPTION_POINTERS* info);
-
 // Global information used across the metro driver.
 struct Globals {
   LPTHREAD_START_ROUTINE host_main;
@@ -165,7 +162,6 @@ struct Globals {
   // to be initiated from that thread, notably spawning file pickers.
   base::MessageLoopProxy* appview_msg_loop;
   winapp::Core::ICoreApplicationExit* app_exit;
-  BreakpadExceptionHandler breakpad_exception_handler;
   string16 metro_command_line_switches;
 };
 
