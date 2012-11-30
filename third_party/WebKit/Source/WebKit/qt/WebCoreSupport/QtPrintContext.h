81c8d848524517eb324321e38f6dcd7e2ab3dcb9
pierre.rossi@gmail.com <pierre.rossi@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-30 17:57:18 +0000

    [Qt] Unreviewed build fix.
    In the same vein as r136245. Include PlatformExportMacros proper.
    
    * WebCoreSupport/QtPrintContext.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QtPrintContext.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/QtPrintContext.h
@@ -21,10 +21,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef QtPrintContext_h
 #define QtPrintContext_h
 
+#include <PlatformExportMacros.h>
 #include <QPainter>
 #include <QRect>
 #include <qwebkitglobal.h>
-#include <wtf/ExportMacros.h>
 
 namespace WebCore {
 class PrintContext;
