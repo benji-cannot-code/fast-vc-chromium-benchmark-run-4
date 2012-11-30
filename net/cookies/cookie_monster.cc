0d0112bbebbbb5e9140a8b79aaaa10ca531f0e82
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 12:56:25 +0000

    Remove unused parameter from the method IsDomainMatch of the class CanonicalCookie.
    TEST=CanonicalCookie*
    BUG=163349
    
    Review URL: https://chromiumcodereview.appspot.com/11308270
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170442 0039d316-1c4b-4281-b951-d872f2087c98

--- net/cookies/cookie_monster.cc
+++ net/cookies/cookie_monster.cc
@@ -1134,7 +1134,6 @@ int CookieMonster::DeleteAllForHost(const GURL& url) {
   if (!HasCookieableScheme(url))
     return 0;
 
-  const std::string scheme(url.scheme());
   const std::string host(url.host());
 
   // We store host cookies in the store by their canonical host name;
@@ -1149,7 +1148,7 @@ int CookieMonster::DeleteAllForHost(const GURL& url) {
     const CanonicalCookie* const cc = curit->second;
 
     // Delete only on a match as a host cookie.
-    if (cc->IsHostCookie() && cc->IsDomainMatch(scheme, host)) {
+    if (cc->IsHostCookie() && cc->IsDomainMatch(host)) {
       num_deleted++;
 
       InternalDeleteCookie(curit, true, DELETE_COOKIE_EXPLICIT);
@@ -1604,7 +1603,6 @@ void CookieMonster::FindCookiesForKey(
     std::vector<CanonicalCookie*>* cookies) {
   lock_.AssertAcquired();
 
-  const std::string scheme(url.scheme());
   const std::string host(url.host());
   bool secure = url.SchemeIsSecure();
 
@@ -1629,7 +1627,7 @@ void CookieMonster::FindCookiesForKey(
       continue;
 
     // Filter out cookies that don't apply to this domain.
-    if (!cc->IsDomainMatch(scheme, host))
+    if (!cc->IsDomainMatch(host))
       continue;
 
     if (!cc->IsOnPath(url.path()))
