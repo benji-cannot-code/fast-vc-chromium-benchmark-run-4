f142fdcdfe3925e4d4a7cf69c5854b06fb835cd5
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 23:49:41 +0000

    Fix compile warnings for 64-bit constants.
    Review URL: https://chromiumcodereview.appspot.com/11428117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170601 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/hash_pair_unittest.cc
+++ cc/hash_pair_unittest.cc
@@ -4,6 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "cc/hash_pair.h"
+
+#include "base/basictypes.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
@@ -29,7 +31,8 @@ TEST_F(HashPairTest, IntegerPairs) {
   INSERT_PAIR_TEST(ShortShortPair, 4, 6);
   INSERT_PAIR_TEST(ShortIntPair, 7, (1 << 30) + 4342);
   INSERT_PAIR_TEST(ShortInt32Pair, 9, (1 << 29) + 378128932);
-  INSERT_PAIR_TEST(ShortInt64Pair, 10, (1LL << 60) + 78931732321);
+  INSERT_PAIR_TEST(ShortInt64Pair, 10,
+                   (GG_INT64_C(1) << 60) + GG_INT64_C(78931732321));
 
   typedef std::pair<int, short> IntShortPair;
   typedef std::pair<int, int> IntIntPair;
@@ -39,7 +42,8 @@ TEST_F(HashPairTest, IntegerPairs) {
   INSERT_PAIR_TEST(IntShortPair, 4, 6);
   INSERT_PAIR_TEST(IntIntPair, 7, (1 << 30) + 4342);
   INSERT_PAIR_TEST(IntInt32Pair, 9, (1 << 29) + 378128932);
-  INSERT_PAIR_TEST(IntInt64Pair, 10, (1LL << 60) + 78931732321);
+  INSERT_PAIR_TEST(IntInt64Pair, 10,
+                   (GG_INT64_C(1) << 60) + GG_INT64_C(78931732321));
 
   typedef std::pair<int32, short> Int32ShortPair;
   typedef std::pair<int32, int> Int32IntPair;
@@ -49,7 +53,8 @@ TEST_F(HashPairTest, IntegerPairs) {
   INSERT_PAIR_TEST(Int32ShortPair, 4, 6);
   INSERT_PAIR_TEST(Int32IntPair, 7, (1 << 30) + 4342);
   INSERT_PAIR_TEST(Int32Int32Pair, 9, (1 << 29) + 378128932);
-  INSERT_PAIR_TEST(Int32Int64Pair, 10, (1LL << 60) + 78931732321);
+  INSERT_PAIR_TEST(Int32Int64Pair, 10,
+                   (GG_INT64_C(1) << 60) + GG_INT64_C(78931732321));
 
   typedef std::pair<int64, short> Int64ShortPair;
   typedef std::pair<int64, int> Int64IntPair;
@@ -59,7 +64,8 @@ TEST_F(HashPairTest, IntegerPairs) {
   INSERT_PAIR_TEST(Int64ShortPair, 4, 6);
   INSERT_PAIR_TEST(Int64IntPair, 7, (1 << 30) + 4342);
   INSERT_PAIR_TEST(Int64Int32Pair, 9, (1 << 29) + 378128932);
-  INSERT_PAIR_TEST(Int64Int64Pair, 10, (1LL << 60) + 78931732321);
+  INSERT_PAIR_TEST(Int64Int64Pair, 10,
+                   (GG_INT64_C(1) << 60) + GG_INT64_C(78931732321));
 }
 
 }  // anonymous namespace
