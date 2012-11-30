f667e57b611a64334fdda69af6584a2e4e20c38f
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 21:30:12 +0000

    Http cache: fix a bug that prevented the infinite cache simulation from saving large data sets.
    Also, go back to a limit of 200k entries, to monitor things, and increase the
    version number to have clean stats after this change.
    
    BUG=147383
    TEST=none
    Review URL: https://codereview.chromium.org/11415222
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170548 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/infinite_cache.cc
+++ net/http/infinite_cache.cc
@@ -83,10 +83,10 @@ const size_t kRecordSize = sizeof(Key) + sizeof(Details);
 
 // Some constants related to the database file.
 uint32 kMagicSignature = 0x1f00cace;
-uint32 kCurrentVersion = 0x10001;
+uint32 kCurrentVersion = 0x10002;
 
 // Basic limits for the experiment.
-int kMaxNumEntries = 500 * 1000;
+int kMaxNumEntries = 200 * 1000;
 int kMaxTrackingSize = 40 * 1024 * 1024;
 
 // Settings that control how we generate histograms.
@@ -605,7 +605,7 @@ void InfiniteCache::Worker::LoadData() {
   base::ClosePlatformFile(file);
   if (header_->disabled) {
     UMA_HISTOGRAM_BOOLEAN("InfiniteCache.Full", true);
-    map_.clear();
+    InitializeData();
   }
 }
 
@@ -640,6 +640,7 @@ void InfiniteCache::Worker::InitializeData() {
   header_->magic = kMagicSignature;
   header_->version = kCurrentVersion;
   header_->creation_time = Time::Now().ToInternalValue();
+  map_.clear();
 
   UMA_HISTOGRAM_BOOLEAN("InfiniteCache.Initialize", true);
   init_ = true;
@@ -654,7 +655,7 @@ bool InfiniteCache::Worker::ReadData(PlatformFile file) {
   uint32 hash = adler32(0, Z_NULL, 0);
 
   for (int remaining_records = header_->num_entries; remaining_records;) {
-    int num_records = std::min(header_->num_entries,
+    int num_records = std::min(remaining_records,
                                static_cast<int>(kMaxRecordsToRead));
     size_t num_bytes = num_records * kRecordSize;
     remaining_records -= num_records;
@@ -704,7 +705,7 @@ bool InfiniteCache::Worker::WriteData(PlatformFile file) {
   DCHECK_EQ(header_->num_entries, static_cast<int32>(map_.size()));
   KeyMap::iterator iterator = map_.begin();
   for (int remaining_records = header_->num_entries; remaining_records;) {
-    int num_records = std::min(header_->num_entries,
+    int num_records = std::min(remaining_records,
                                static_cast<int>(kMaxRecordsToRead));
     size_t num_bytes = num_records * kRecordSize;
     remaining_records -= num_records;
@@ -805,6 +806,7 @@ void InfiniteCache::Worker::Add(const Details& details) {
     int entry_size = static_cast<int>(header_->total_size / kMaxNumEntries);
     UMA_HISTOGRAM_COUNTS("InfiniteCache.FinalAvgEntrySize", entry_size);
     header_->disabled = 1;
+    header_->num_entries = 0;
     map_.clear();
   }
 }
