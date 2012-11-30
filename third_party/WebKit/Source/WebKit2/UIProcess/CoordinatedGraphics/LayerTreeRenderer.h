3b57c05bd07fc40b1daddc97736a2e88f22f48d8
christophe.dumez@intel.com <christophe.dumez@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-30 00:29:13 +0000

    [CoordinatedGraphics] Use OwnPtr for LayerMap's layers in LayerTreeRenderer https://bugs.webkit.org/show_bug.cgi?id=103650
    Reviewed by Noam Rosenthal.
    
    We currently store raw pointers to WebCore::GraphicsLayer in the LayerMap
    meaning that we need to delete them manually. This patch leverages smart
    pointers and stores layers as OwnPtr in the LayerMap so that the layers
    are owned by the HashMap and we don't have to handle memory manually.
    
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp:
    (WebKit::LayerTreeRenderer::adjustPositionForFixedLayers):
    (WebKit::LayerTreeRenderer::setLayerChildren):
    (WebKit::LayerTreeRenderer::deleteLayer):
    (WebKit::LayerTreeRenderer::ensureLayer):
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.h
@@ -152,7 +152,6 @@ private:
     void removeBackingStoreIfNeeded(WebCore::GraphicsLayer*);
     void resetBackingStoreSizeToLayerSize(WebCore::GraphicsLayer*);
 
-    typedef HashMap<WebLayerID, WebCore::GraphicsLayer*> LayerMap;
     WebCore::FloatSize m_contentsSize;
     WebCore::FloatRect m_visibleContentsRect;
 
@@ -182,8 +181,10 @@ private:
 
     OwnPtr<WebCore::GraphicsLayer> m_rootLayer;
 
+    typedef HashMap<WebLayerID, OwnPtr<WebCore::GraphicsLayer> > LayerMap;
     LayerMap m_layers;
-    LayerMap m_fixedLayers;
+    typedef HashMap<WebLayerID, WebCore::GraphicsLayer*> LayerRawPtrMap;
+    LayerRawPtrMap m_fixedLayers;
     WebLayerID m_rootLayerID;
     WebCore::IntPoint m_renderedContentsScrollPosition;
     WebCore::IntPoint m_pendingRenderedContentsScrollPosition;
