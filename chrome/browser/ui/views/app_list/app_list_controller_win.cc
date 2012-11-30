028b22cb2c9bb3d0c0f187db0df84cba81c1b0c1
hongbo.min@intel.com <hongbo.min@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 03:45:00 +0000

    When a create shortcuts dialog is popup for v2 packaged app, the app launcher window will be disappeared due to focus losing. This fix is to make the app launcher window as the parent of create shorts dialog.
    BUG=162596
    
    Review URL: https://chromiumcodereview.appspot.com/11421069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170386 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/app_list/app_list_controller_win.cc
+++ chrome/browser/ui/views/app_list/app_list_controller_win.cc
@@ -125,6 +125,7 @@ class AppListController {
   void CloseAppList();
   void AppListClosing();
   void AppListActivationChanged(bool active);
+  app_list::AppListView* GetView() { return current_view_; }
 
  private:
   // Utility methods for showing the app list.
@@ -206,7 +207,14 @@ void AppListControllerDelegateWin::ShowCreateShortcutsDialog(
   const extensions::Extension* extension = service->GetInstalledExtension(
       extension_id);
   DCHECK(extension);
-  chrome::ShowCreateChromeAppShortcutsDialog(NULL, profile, extension);
+
+  app_list::AppListView* view = g_app_list_controller.Get().GetView();
+  if (!view)
+    return;
+
+  gfx::NativeWindow parent_hwnd =
+      view->GetWidget()->GetTopLevelWidget()->GetNativeWindow();
+  chrome::ShowCreateChromeAppShortcutsDialog(parent_hwnd, profile, extension);
 }
 
 void AppListControllerDelegateWin::ActivateApp(Profile* profile,
