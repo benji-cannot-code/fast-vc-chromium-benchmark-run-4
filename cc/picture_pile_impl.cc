c325d8ac146066d218f3740b30df0f1422cd5928
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-11-30 05:55:02 +0000

    [cc] Track the rendering & painting cost of impl-side painting.
    We record how long the rendering thread takes to render each PicturePile,
    accumulate in TileManager, and report through LayerTreeHostImpl; telemetry
    queries this information and dumps the output.
    
    BUG=163324
    R=reveman
    TBR=enne
    
    Review URL: https://codereview.chromium.org/11434033
    
    Review URL: https://codereview.chromium.org/11418250
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170413 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_pile_impl.cc
+++ cc/picture_pile_impl.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/trace_event.h"
 #include "cc/picture_pile_impl.h"
+#include "cc/rendering_stats.h"
 #include "third_party/skia/include/core/SkCanvas.h"
+#include "third_party/skia/include/core/SkSize.h"
 
 namespace cc {
 
@@ -29,7 +31,10 @@ scoped_refptr<PicturePileImpl> PicturePileImpl::CloneForDrawing() const {
   return clone;
 }
 
-void PicturePileImpl::Raster(SkCanvas* canvas, gfx::Rect rect) {
+void PicturePileImpl::Raster(SkCanvas* canvas, gfx::Rect rect,
+                             RenderingStats* stats) {
+  base::TimeTicks rasterizeBeginTime = base::TimeTicks::Now();
+
   // TODO(enne): do this more efficiently, i.e. top down with Skia clips
   canvas->save();
   canvas->translate(-rect.x(), -rect.y());
@@ -40,8 +45,14 @@ void PicturePileImpl::Raster(SkCanvas* canvas, gfx::Rect rect) {
     if (!pile_[i]->LayerRect().Intersects(rect))
       continue;
     pile_[i]->Raster(canvas);
+
+    SkISize deviceSize = canvas->getDeviceSize();
+    stats->totalPixelsRasterized += deviceSize.width() * deviceSize.height();
   }
   canvas->restore();
+
+  stats->totalRasterizeTimeInSeconds += (base::TimeTicks::Now() -
+                                         rasterizeBeginTime).InSecondsF();
 }
 
 }  // namespace cc
--- cc/tile_manager.h
+++ cc/tile_manager.h
@@ -21,6 +21,7 @@ namespace cc {
 
 class Tile;
 class TileVersion;
+struct RenderingStats;
 class ResourceProvider;
 
 class CC_EXPORT TileManagerClient {
@@ -76,6 +77,8 @@ class CC_EXPORT TileManager {
 
   void ManageTiles();
 
+  void renderingStats(RenderingStats* stats);
+
  protected:
   // Methods called by Tile
   friend class Tile;
@@ -92,7 +95,8 @@ class CC_EXPORT TileManager {
   void OnRasterTaskCompleted(
       scoped_refptr<Tile>,
       ResourceProvider::ResourceId,
-      scoped_refptr<PicturePileImpl>);
+      scoped_refptr<PicturePileImpl>,
+      RenderingStats*);
   void DidFinishTileInitialization(Tile*, ResourceProvider::ResourceId);
 
   TileManagerClient* client_;
@@ -106,6 +110,8 @@ class CC_EXPORT TileManager {
   typedef std::vector<Tile*> TileVector;
   TileVector tiles_;
   TileVector tiles_that_need_to_be_rasterized_;
+
+  RenderingStats rendering_stats_;
 };
 
 }  // namespace cc
