d282c82d88a165696f057c4e44fd831ca02cbfdb
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-30 01:06:47 +0000

    Web Inspector: optimize repaint regions upon text editing https://bugs.webkit.org/show_bug.cgi?id=103674
    Reviewed by Vsevolod Vlasov.
    
    There is a lot happening that makes repaint area larger than it should be,
    namely unnecessary tabbed pane and navigator updates.
    
    * inspector/front-end/NavigatorView.js:
    (WebInspector.NavigatorView.prototype._updateScriptTitle):
    * inspector/front-end/TabbedPane.js:
    (WebInspector.TabbedPaneTab.prototype.set title):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136187 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NavigatorView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NavigatorView.js
@@ -480,6 +480,8 @@ WebInspector.BaseNavigatorTreeElement.prototype = {
 
     set titleText(titleText)
     {
+        if (this._titleText === titleText)
+            return;
         this._titleText = titleText || "";
         if (this.titleElement)
             this.titleElement.textContent = this._titleText;
