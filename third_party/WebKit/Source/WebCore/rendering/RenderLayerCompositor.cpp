53bb4d14a43435169e1b66074b0afae0916d9c94
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-30 22:30:59 +0000

    Late-loading stylesheets can cause composited layers to be blank https://bugs.webkit.org/show_bug.cgi?id=103773
    Reviewed by Tim Horton.
    
    Early painting can be short-circuited in RenderBlock::paintContents() if we know a stylesheet
    is pending, which is done to avoid a flash of unstyled content (FOUC). When the stylesheet
    finally loaded, Document::styleResolverChanged() would try to repaint everything by calling
    repaint() on the RenderView(). In a composited world, however, this repaint() doesn't repaint
    composited layers.
    
    This was particularly prevalent on this specific URL because it failed to load
    a CSS file from typekit.com, so Document::styleResolverChanged()
    just did the repaint and returned (rather than doing a recalc style as would
    happen for correctly loaded stylesheets).
    
    Fix by making a way to repaint all compositing layers, and calling it
    from Document::styleResolverChanged().
    
    No tests because this is timing-dependant.
    
    * dom/Document.cpp:
    (WebCore::Document::styleResolverChanged): Call repaintViewAndCompositedLayers().
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::paintContents): Fix the comment.
    * rendering/RenderLayerCompositor.cpp: Convert repaintCompositedLayersAbsoluteRect()
    and associated recursiveRepaintLayerRect() to allow the rect to be null, which indicates
    that we should just repaint the entire layer, and improve their names.
    (WebCore::RenderLayerCompositor::repaintCompositedLayers):
    (WebCore::RenderLayerCompositor::recursiveRepaintLayer):
    * rendering/RenderLayerCompositor.h:
    * rendering/RenderView.cpp:
    (WebCore::RenderView::repaintRectangleInViewAndCompositedLayers): repaintCompositedLayersAbsoluteRect()
    was renamed to repaintCompositedLayers().
    (WebCore::RenderView::repaintViewAndCompositedLayers):
    * rendering/RenderView.h:
    (RenderView): repaintViewRectangle() and repaintRectangleInViewAndCompositedLayers() should not
    be virtual. Add repaintViewAndCompositedLayers().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136277 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -1341,16 +1341,20 @@ void RenderLayerCompositor::updateCompositingDescendantGeometry(RenderLayer* com
 }
 
 
-void RenderLayerCompositor::repaintCompositedLayersAbsoluteRect(const IntRect& absRect)
+void RenderLayerCompositor::repaintCompositedLayers(const IntRect* absRect)
 {
-    recursiveRepaintLayerRect(rootRenderLayer(), absRect);
+    recursiveRepaintLayer(rootRenderLayer(), absRect);
 }
 
-void RenderLayerCompositor::recursiveRepaintLayerRect(RenderLayer* layer, const IntRect& rect)
+void RenderLayerCompositor::recursiveRepaintLayer(RenderLayer* layer, const IntRect* rect)
 {
     // FIXME: This method does not work correctly with transforms.
-    if (layer->isComposited() && !layer->backing()->paintsIntoCompositedAncestor())
-        layer->setBackingNeedsRepaintInRect(rect);
+    if (layer->isComposited() && !layer->backing()->paintsIntoCompositedAncestor()) {
+        if (rect)
+            layer->setBackingNeedsRepaintInRect(*rect);
+        else
+            layer->setBackingNeedsRepaint();
+    }
 
 #if !ASSERT_DISABLED
     LayerListMutationDetector mutationChecker(layer);
@@ -1361,9 +1365,12 @@ void RenderLayerCompositor::recursiveRepaintLayerRect(RenderLayer* layer, const 
             size_t listSize = negZOrderList->size();
             for (size_t i = 0; i < listSize; ++i) {
                 RenderLayer* curLayer = negZOrderList->at(i);
-                IntRect childRect(rect);
-                curLayer->convertToPixelSnappedLayerCoords(layer, childRect);
-                recursiveRepaintLayerRect(curLayer, childRect);
+                if (rect) {
+                    IntRect childRect(*rect);
+                    curLayer->convertToPixelSnappedLayerCoords(layer, childRect);
+                    recursiveRepaintLayer(curLayer, &childRect);
+                } else
+                    recursiveRepaintLayer(curLayer);
             }
         }
 
@@ -1371,9 +1378,12 @@ void RenderLayerCompositor::recursiveRepaintLayerRect(RenderLayer* layer, const 
             size_t listSize = posZOrderList->size();
             for (size_t i = 0; i < listSize; ++i) {
                 RenderLayer* curLayer = posZOrderList->at(i);
-                IntRect childRect(rect);
-                curLayer->convertToPixelSnappedLayerCoords(layer, childRect);
-                recursiveRepaintLayerRect(curLayer, childRect);
+                if (rect) {
+                    IntRect childRect(*rect);
+                    curLayer->convertToPixelSnappedLayerCoords(layer, childRect);
+                    recursiveRepaintLayer(curLayer, &childRect);
+                } else
+                    recursiveRepaintLayer(curLayer);
             }
         }
     }
@@ -1381,9 +1391,12 @@ void RenderLayerCompositor::recursiveRepaintLayerRect(RenderLayer* layer, const 
         size_t listSize = normalFlowList->size();
         for (size_t i = 0; i < listSize; ++i) {
             RenderLayer* curLayer = normalFlowList->at(i);
-            IntRect childRect(rect);
-            curLayer->convertToPixelSnappedLayerCoords(layer, childRect);
-            recursiveRepaintLayerRect(curLayer, childRect);
+            if (rect) {
+                IntRect childRect(*rect);
+                curLayer->convertToPixelSnappedLayerCoords(layer, childRect);
+                recursiveRepaintLayer(curLayer, &childRect);
+            } else
+                recursiveRepaintLayer(curLayer);
         }
     }
 }
