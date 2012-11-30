169cc73991d745133076514d67829d7f969e2fea
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-11-30 02:28:37 +0000

    Unreviewed, rolling out r136167. http://trac.webkit.org/changeset/136167 https://bugs.webkit.org/show_bug.cgi?id=103701
    Win debug build failure (Requested by zhenyao on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2012-11-29
    
    * platform/graphics/chromium/LazyDecodingPixelRef.cpp:
    (WebCore::LazyDecodingPixelRef::LazyDecodingPixelRef):
    * platform/graphics/chromium/LazyDecodingPixelRef.h:
    (LazyDecodingPixelRef):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/chromium/LazyDecodingPixelRef.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/chromium/LazyDecodingPixelRef.cpp
@@ -35,7 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 LazyDecodingPixelRef::LazyDecodingPixelRef(PassRefPtr<ImageFrameGenerator> frameGenerator, const SkISize& scaledSize, const SkIRect& scaledSubset)
-    : m_frameGenerator(frameGenerator)
+    : SkPixelRef(0)
+    , m_frameGenerator(frameGenerator)
     , m_scaledSize(scaledSize)
     , m_scaledSubset(scaledSubset)
     , m_lockedCachedImage(0)
@@ -89,20 +90,5 @@ bool LazyDecodingPixelRef::onLockPixelsAreWritable() const
 {
     return false;
 }
-
-bool LazyDecodingPixelRef::PrepareToDecode(const LazyPixelRef::PrepareParams& params)
-{
-    MutexLocker lock(m_mutex);
-    // TODO: check if only a particular rect is available in image cache.
-    UNUSED_PARAM(params);
-    return ImageDecodingStore::instance()->lockCompleteCache(m_frameGenerator.get(), m_scaledSize);
-}
-
-void LazyDecodingPixelRef::Decode()
-{
-    lockPixels();
-    unlockPixels();
-}
-
 
 } // namespace WebKit
