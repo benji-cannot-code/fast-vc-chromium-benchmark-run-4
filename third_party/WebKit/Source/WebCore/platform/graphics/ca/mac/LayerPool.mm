365baafc75ac01d2c0a3f309ab23750c1c4f55f6
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-01 00:53:01 +0000

    https://bugs.webkit.org/show_bug.cgi?id=103790 The LayerPool m_pruneTimer should have a longer delay
    Reviewed by Simon Fraser.
    
    A 1 second delay is more appropriate here.
    * platform/graphics/ca/mac/LayerPool.mm:
    (WebCore::LayerPool::schedulePrune):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/LayerPool.mm
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/LayerPool.mm
@@ -105,7 +105,7 @@ void LayerPool::schedulePrune()
 {
     if (m_pruneTimer.isActive())
         return;
-    m_pruneTimer.startOneShot(0);
+    m_pruneTimer.startOneShot(1);
 }
 
 void LayerPool::pruneTimerFired(Timer<LayerPool>*)
