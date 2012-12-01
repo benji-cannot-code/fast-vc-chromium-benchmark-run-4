2e627d33a165afa8f723878790f2c2b8ebb9abef
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-01 02:17:55 +0000

    Revert "Use WS_POPUP for ash_unittests"
    Use WS_POPUP for ash_unittests
    - Layout test windows starting from (1,1) and move
    mouse to (0,0) so that they don't overlap unless
    test moves the cursor.
    - Enabled disabled tests.
    - RootWindowHostWin::SetBounds should move the window
    and call OnHostResized when the scale factor has changed.
    
    This also seems to fix the desktop size limitation issue (148691)
    probably because a popup window can be larger than the desktop size.
    
    TBR=oshima@chromium.org
    BUG=150986, 157817, 141577, 148691
    TEST=none
    
    Review URL: https://codereview.chromium.org/11419271
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170632 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/test/ash_test_base.cc
+++ ash/test/ash_test_base.cc
@@ -27,10 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/display.h"
 #include "ui/gfx/screen.h"
 
-#if defined(OS_WIN)
-#include "ui/aura/root_window_host_win.h"
-#endif
-
 namespace ash {
 namespace test {
 
@@ -48,13 +44,8 @@ AshTestBase::~AshTestBase() {
 }
 
 void AshTestBase::SetUp() {
-  // Use the origin (1,1) so that it doesn't over
-  // lap with the native mouse cursor.
   CommandLine::ForCurrentProcess()->AppendSwitchASCII(
-      switches::kAuraHostWindowSize, "1+1-800x600");
-#if defined(OS_WIN)
-  aura::test::SetUsePopupAsRootWindowForTest(true);
-#endif
+      switches::kAuraHostWindowSize, "0+0-800x600");
   // Disable animations during tests.
   ui::LayerAnimator::set_disable_animations_for_test(true);
   ui::TextInputTestSupport::Initialize();
@@ -63,9 +54,9 @@ void AshTestBase::SetUp() {
   ash::Shell::CreateInstance(test_shell_delegate_);
   Shell::GetPrimaryRootWindow()->Show();
   Shell::GetPrimaryRootWindow()->ShowRootWindow();
-  // Move the mouse cursor to (0,0) so that native events doesn't
+  // Move the mouse cursor to far away so that native events doesn't
   // interfere test expectations.
-  Shell::GetPrimaryRootWindow()->MoveCursorTo(gfx::Point(0, 0));
+  Shell::GetPrimaryRootWindow()->MoveCursorTo(gfx::Point(-1000, -1000));
   Shell::GetInstance()->cursor_manager()->ShowCursor(true);
 }
 
@@ -77,9 +68,6 @@ void AshTestBase::TearDown() {
   Shell::DeleteInstance();
   aura::Env::DeleteInstance();
   ui::TextInputTestSupport::Shutdown();
-#if defined(OS_WIN)
-  aura::test::SetUsePopupAsRootWindowForTest(false);
-#endif
 }
 
 void AshTestBase::ChangeDisplayConfig(float scale,
