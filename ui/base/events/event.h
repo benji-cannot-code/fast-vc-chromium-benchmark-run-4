fa5e14e366fa5b7866dbe140c341dcbd2add4863
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-01 03:10:56 +0000

    events: Dispatch all events through EventHandler::OnEvent from the dispatcher.
    The EventDispatcher dispatches all events to EventHandler::OnEvent. The default
    implementation routes various events to more appropriate handlers (e.g. OnKeyEvent
    for KeyEvent etc.)
    
    BUG=163606
    
    Review URL: https://codereview.chromium.org/11299285
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170641 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/events/event.h
+++ ui/base/events/event.h
@@ -98,7 +98,8 @@ class UI_EXPORT Event {
            type_ == ET_MOUSE_MOVED ||
            type_ == ET_MOUSE_ENTERED ||
            type_ == ET_MOUSE_EXITED ||
-           type_ == ET_MOUSEWHEEL;
+           type_ == ET_MOUSEWHEEL ||
+           type_ == ET_MOUSE_CAPTURE_CHANGED;
   }
 
   bool IsTouchEvent() const {
@@ -115,6 +116,7 @@ class UI_EXPORT Event {
       case ET_GESTURE_SCROLL_END:
       case ET_GESTURE_SCROLL_UPDATE:
       case ET_GESTURE_TAP:
+      case ET_GESTURE_TAP_CANCEL:
       case ET_GESTURE_TAP_DOWN:
       case ET_GESTURE_BEGIN:
       case ET_GESTURE_END:
