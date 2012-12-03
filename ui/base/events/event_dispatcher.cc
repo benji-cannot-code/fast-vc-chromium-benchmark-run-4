2c3f7eac58a5cc20d4863c6934685a37af19e944
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-03 20:29:46 +0000

    events: Start changing EventHandler interface to not return a value.
    The EventHandler should explicitly stop the propagation of an event
    (Event::StopPropagation()), or mark it has having been handled
    (Event::PreventDefault()), by calling a function on the event
    object, instead of returning a value from the event-handler functions.
    
    This patch takes the first step by converting EventHandler::OnEvent. Subsequent
    patches will conver the event-specific callbacks (OnKeyEvent etc.)
    
    BUG=163618
    
    Review URL: https://codereview.chromium.org/11308322
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170801 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/events/event_dispatcher.cc
+++ ui/base/events/event_dispatcher.cc
@@ -20,9 +20,9 @@ EventDispatcher::~EventDispatcher() {
 ////////////////////////////////////////////////////////////////////////////////
 // EventDispatcher, private:
 
-EventResult EventDispatcher::DispatchEventToSingleHandler(EventHandler* handler,
+void EventDispatcher::DispatchEventToSingleHandler(EventHandler* handler,
                                                           Event* event) {
-  return handler->OnEvent(event);
+  handler->OnEvent(event);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -30,6 +30,7 @@ EventResult EventDispatcher::DispatchEventToSingleHandler(EventHandler* handler,
 
 EventDispatcher::ScopedDispatchHelper::ScopedDispatchHelper(Event* event)
     : Event::DispatcherApi(event) {
+  set_result(ui::ER_UNHANDLED);
 }
 
 EventDispatcher::ScopedDispatchHelper::~ScopedDispatchHelper() {
