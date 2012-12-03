588ab6d6d227d7ac5356c16534e0f3efc0d4405d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-03 18:18:22 +0000

    Optimization in image decoding https://bugs.webkit.org/show_bug.cgi?id=88424
    Patch by Viatcheslav Ostapenko <v.ostapenko@samsung.com> on 2012-12-03
    Reviewed by Brent Fulgham.
    
    Replace int with J_COLOR_SPACE in parameters of templates setPixel and
    outputScanlines to avoid casting from int to enum. It required move
    inclusion of jpeglib.h and related #if to JPEGImageDecoder.h .
    
    * platform/image-decoders/jpeg/JPEGImageDecoder.cpp:
    (WebCore):
    (WebCore::setPixel):
    (WebCore::JPEGImageDecoder::outputScanlines):
    * platform/image-decoders/jpeg/JPEGImageDecoder.h:
    (JPEGImageDecoder):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136410 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.cpp
@@ -41,20 +41,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "JPEGImageDecoder.h"
 #include "PlatformInstrumentation.h"
-#include <stdio.h>  // Needed by jpeglib.h for FILE.
 #include <wtf/PassOwnPtr.h>
 
-#if OS(WINCE)
-// Remove warning: 'FAR' macro redefinition
-#undef FAR
-
-// jmorecfg.h in libjpeg checks for XMD_H with the comment: "X11/xmd.h correctly defines INT32"
-// fix INT32 redefinition error by pretending we are X11/xmd.h
-#define XMD_H
-#endif
-
 extern "C" {
-#include "jpeglib.h"
 #if USE(ICCJPEG)
 #include "iccjpeg.h"
 #endif
@@ -647,12 +636,12 @@ bool JPEGImageDecoder::setFailed()
     return ImageDecoder::setFailed();
 }
 
-template <int colorSpace>
+template <J_COLOR_SPACE colorSpace>
 void setPixel(ImageFrame& buffer, ImageFrame::PixelData* currentAddress, JSAMPARRAY samples, int column)
 {
-    JSAMPLE* jsample = *samples + column * (static_cast<J_COLOR_SPACE>(colorSpace) == JCS_RGB ? 3 : 4);
+    JSAMPLE* jsample = *samples + column * (colorSpace == JCS_RGB ? 3 : 4);
 
-    switch (static_cast<J_COLOR_SPACE>(colorSpace)) {
+    switch (colorSpace) {
 #if defined(TURBO_JPEG_RGB_SWIZZLE)
     case JCS_EXT_BGRA:
         buffer.setRGBA(currentAddress, jsample[2], jsample[1], jsample[0], 0xFF);
@@ -678,7 +667,7 @@ void setPixel(ImageFrame& buffer, ImageFrame::PixelData* currentAddress, JSAMPAR
     }
 }
 
-template <int colorSpace, bool isScaled>
+template <J_COLOR_SPACE colorSpace, bool isScaled>
 bool JPEGImageDecoder::outputScanlines(ImageFrame& buffer)
 {
     JSAMPARRAY samples = m_reader->samples();
@@ -713,7 +702,7 @@ bool JPEGImageDecoder::outputScanlines(ImageFrame& buffer)
     return true;
 }
 
-template <int colorSpace>
+template <J_COLOR_SPACE colorSpace>
 bool JPEGImageDecoder::outputScanlines(ImageFrame& buffer)
 {
     return m_scaled ? outputScanlines<colorSpace, true>(buffer) : outputScanlines<colorSpace, false>(buffer);
--- third_party/WebKit/Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/jpeg/JPEGImageDecoder.h
@@ -29,7 +29,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define JPEGImageDecoder_h
 
 #include "ImageDecoder.h"
+#include <stdio.h> // Needed by jpeglib.h for FILE.
 #include <wtf/OwnPtr.h>
+
+#if OS(WINCE)
+// Remove warning: 'FAR' macro redefinition
+#undef FAR
+
+// jmorecfg.h in libjpeg checks for XMD_H with the comment: "X11/xmd.h correctly defines INT32"
+// fix INT32 redefinition error by pretending we are X11/xmd.h
+#define XMD_H
+#endif
+
+extern "C" {
+#include "jpeglib.h"
+}
 
 namespace WebCore {
 
@@ -63,10 +77,10 @@ namespace WebCore {
         // data coming, sets the "decode failure" flag.
         void decode(bool onlySize);
 
-        template <int colorSpace>
+        template <J_COLOR_SPACE colorSpace>
         bool outputScanlines(ImageFrame& buffer);
 
-        template <int colorSpace, bool isScaled>
+        template <J_COLOR_SPACE colorSpace, bool isScaled>
         bool outputScanlines(ImageFrame& buffer);
 
         OwnPtr<JPEGImageReader> m_reader;
