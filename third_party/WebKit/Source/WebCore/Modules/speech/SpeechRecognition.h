56d78e19af73c919d4cc34f1a53a918661998aaf
tommyw@google.com <tommyw@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-03 14:27:24 +0000

    Speech Recognition API: Update SpeechRecognitionEvent to match the specification https://bugs.webkit.org/show_bug.cgi?id=103407
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    Specification for the Speech Recognition API:
    http://dvcs.w3.org/hg/speech-api/raw-file/tip/speechapi.html
    
    This patch does the following:
    o Adds results, and removing result, on SpeechRecognitionEvent
    o Renaming SpeechRecognitionResult::final to isFinal
    o Removing emma from SpeechRecognitionResult
    
    The emma attribute is supposed to move from SpeechRecognitionResult to the SpeechRecognitionEvent,
    but that caused GC issues for unrelated code so it will be added later when properly fixed.
    
    Existing tests updated to test the new API.
    
    * Modules/speech/SpeechRecognition.cpp:
    (WebCore::SpeechRecognition::start):
    (WebCore::SpeechRecognition::didReceiveResults):
    (WebCore):
    * Modules/speech/SpeechRecognition.h:
    (SpeechRecognition):
    * Modules/speech/SpeechRecognitionEvent.cpp:
    (WebCore::SpeechRecognitionEvent::createResult):
    (WebCore):
    (WebCore::SpeechRecognitionEvent::SpeechRecognitionEvent):
    (WebCore::SpeechRecognitionEvent::~SpeechRecognitionEvent):
    * Modules/speech/SpeechRecognitionEvent.h:
    (SpeechRecognitionEventInit):
    (SpeechRecognitionEvent):
    (WebCore::SpeechRecognitionEvent::resultIndex):
    (WebCore::SpeechRecognitionEvent::results):
    * Modules/speech/SpeechRecognitionEvent.idl:
    * Modules/speech/SpeechRecognitionResult.cpp:
    * Modules/speech/SpeechRecognitionResult.h:
    (WebCore::SpeechRecognitionResult::isFinal):
    (SpeechRecognitionResult):
    * Modules/speech/SpeechRecognitionResult.idl:
    * WebCore.gypi:
    * bindings/js/JSDictionary.cpp:
    (WebCore::JSDictionary::convertValue):
    (WebCore):
    * bindings/js/JSDictionary.h:
    * bindings/v8/Dictionary.cpp:
    (WebCore::Dictionary::get):
    (WebCore):
    * bindings/v8/Dictionary.h:
    (Dictionary):
    * bindings/v8/custom/V8SpeechRecognitionResultCustom.cpp: Removed.
    
    Source/WebKit/chromium:
    
    Adding the new didReceiveResults method.
    
    * public/WebSpeechRecognizerClient.h:
    * src/SpeechRecognitionClientProxy.cpp:
    (WebKit::SpeechRecognitionClientProxy::didReceiveResults):
    (WebKit):
    * src/SpeechRecognitionClientProxy.h:
    (SpeechRecognitionClientProxy):
    
    Tools:
    
    Changing the mock to use the new results delivery API.
    
    * DumpRenderTree/chromium/MockWebSpeechRecognizer.cpp:
    
    LayoutTests:
    
    Updating basic tests and removing emma tests temporarily.
    
    * fast/speech/scripted/emma-expected.txt: Removed.
    * fast/speech/scripted/emma.html: Removed.
    * fast/speech/scripted/speechrecognition-basics-expected.txt:
    * fast/speech/scripted/speechrecognition-basics.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136392 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/speech/SpeechRecognition.h
+++ third_party/WebKit/Source/WebCore/Modules/speech/SpeechRecognition.h
@@ -74,6 +74,8 @@ public:
     void didEndSpeech();
     void didEndSound();
     void didEndAudio();
+    void didReceiveResults(const Vector<RefPtr<SpeechRecognitionResult> >& newFinalResults, const Vector<RefPtr<SpeechRecognitionResult> >& currentInterimResults);
+    // FIXME: Remove this once we start using didReceiveResults.
     void didReceiveResult(PassRefPtr<SpeechRecognitionResult>, unsigned long resultIndex, PassRefPtr<SpeechRecognitionResultList> resultHistory);
     void didReceiveNoMatch(PassRefPtr<SpeechRecognitionResult>);
     void didReceiveError(PassRefPtr<SpeechRecognitionError>);
@@ -125,6 +127,7 @@ private:
     SpeechRecognitionController* m_controller;
     bool m_stoppedByActiveDOMObject;
     bool m_started;
+    Vector<RefPtr<SpeechRecognitionResult> > m_finalResults;
 };
 
 } // namespace WebCore
--- third_party/WebKit/Source/WebCore/Modules/speech/SpeechRecognitionResult.h
+++ third_party/WebKit/Source/WebCore/Modules/speech/SpeechRecognitionResult.h
@@ -35,8 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class Document;
-
 class SpeechRecognitionResult : public RefCounted<SpeechRecognitionResult> {
 public:
     ~SpeechRecognitionResult();
@@ -44,15 +42,13 @@ public:
 
     unsigned long length() { return m_alternatives.size(); }
     SpeechRecognitionAlternative* item(unsigned long index);
-    bool final() { return m_final; }
-    Document* emma();
+    bool isFinal() { return m_final; }
 
 private:
     SpeechRecognitionResult(const Vector<RefPtr<SpeechRecognitionAlternative> >&, bool final);
 
     Vector<RefPtr<SpeechRecognitionAlternative> > m_alternatives;
     bool m_final;
-    RefPtr<Document> m_emma;
 };
 
 } // namespace WebCore
