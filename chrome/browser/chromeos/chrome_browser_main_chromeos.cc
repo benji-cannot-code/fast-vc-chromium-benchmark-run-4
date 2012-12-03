01eb49fd199d07207b04db6d194d8ab9a3883f0f
dmazzoni@google.com <dmazzoni@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-03 03:37:31 +0000

    Revert 170590
    It looks like the call to GetMagnifierType() on accessibility_util:124 is
    introducing some crashes.
    
    chrome!chromeos::accessibility::GetMagnifierType()
    [accessibility_util.cc : 283 + 0xb
    
    > Add Chrome OS accessibility histograms.
    > 
    > BUG=99504
    > TBR=joi
    > 
    > Review URL: https://chromiumcodereview.appspot.com/11414232
    
    TBR=dmazzoni@chromium.org
    Review URL: https://codereview.chromium.org/11348342
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170698 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_number_conversions.h"
 #include "base/string_split.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chromeos/accessibility/accessibility_util.h"
 #include "chrome/browser/chromeos/accessibility/magnification_manager.h"
 #include "chrome/browser/chromeos/audio/audio_handler.h"
 #include "chrome/browser/chromeos/boot_times_loader.h"
@@ -530,7 +529,6 @@ void ChromeBrowserMainPartsChromeos::PostProfileInit() {
   }
   magnification_manager_.reset(
       chromeos::MagnificationManager::CreateInstance());
-  chromeos::accessibility::Initialize();
 
   primary_display_switch_observer_.reset(
       new PrimaryDisplaySwitchObserver());
--- content/public/browser/browser_accessibility_state.h
+++ content/public/browser/browser_accessibility_state.h
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_PUBLIC_BROWSER_BROWSER_ACCESSIBILITY_STATE_H_
 #define CONTENT_PUBLIC_BROWSER_BROWSER_ACCESSIBILITY_STATE_H_
 
-#include "base/callback_forward.h"
 #include "content/common/content_export.h"
 
 namespace content {
@@ -29,12 +28,6 @@ class CONTENT_EXPORT BrowserAccessibilityState {
 
   // Returns true if the browser should be customized for accessibility.
   virtual bool IsAccessibleBrowser() = 0;
-
-  // Add a callback method that will be called once, a small while after the
-  // browser starts up, when accessibility state histograms are updated.
-  // Use this to register a method to update additional accessibility
-  // histograms.
-  virtual void AddHistogramCallback(base::Closure callback) = 0;
 };
 
 }  // namespace content
