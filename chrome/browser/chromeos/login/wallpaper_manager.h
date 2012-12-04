f742c8a20539e39a6d8cfa66518aac651a8137cb
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 20:46:03 +0000

    Remove wallpaper thumbnail related c++ code. Thumbnail is now generated by javascript.
    BUG=159714
    
    Review URL: https://codereview.chromium.org/11316305
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171040 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/wallpaper_manager.h
+++ chrome/browser/chromeos/login/wallpaper_manager.h
@@ -92,9 +92,6 @@ class WallpaperManager: public system::TimezoneSettings::Observer,
   FilePath GetWallpaperPathForUser(const std::string& username,
                                    bool is_small);
 
-  // Gets the thumbnail of custom wallpaper from cache.
-  gfx::ImageSkia GetCustomWallpaperThumbnail(const std::string& email);
-
   // Gets wallpaper information of logged in user.
   bool GetLoggedInUserWallpaperInfo(WallpaperInfo* info);
 
@@ -187,10 +184,6 @@ class WallpaperManager: public system::TimezoneSettings::Observer,
   // Caches |email|'s wallpaper to memory.
   void CacheUserWallpaper(const std::string& email);
 
-  // Generates a 128x80 thumbnail and caches it.
-  void CacheThumbnail(const std::string& email,
-                      scoped_ptr<gfx::ImageSkia> wallpaper);
-
   // Clears all obsolete wallpaper prefs from old version wallpaper pickers.
   void ClearObsoleteWallpaperPrefs();
 
@@ -205,11 +198,6 @@ class WallpaperManager: public system::TimezoneSettings::Observer,
   void LoadWallpaper(const std::string& email,
                      const WallpaperInfo& info,
                      bool update_wallpaper);
-
-  // Generates a 128x80 thumbnail.
-  void GenerateUserWallpaperThumbnail(const std::string& email,
-                                      User::WallpaperType type,
-                                      const gfx::ImageSkia& wallpaper);
 
   // Gets |email|'s custom wallpaper at |wallpaper_path|. Falls back on original
   // custom wallpaper. When |update_wallpaper| is true, sets wallpaper to the
@@ -301,8 +289,6 @@ class WallpaperManager: public system::TimezoneSettings::Observer,
 
   // Caches wallpapers of users. Accessed only on UI thread.
   CustomWallpaperMap wallpaper_cache_;
-
-  CustomWallpaperMap custom_wallpaper_thumbnail_cache_;
 
   // The last selected user on user pod row.
   std::string last_selected_user_;
