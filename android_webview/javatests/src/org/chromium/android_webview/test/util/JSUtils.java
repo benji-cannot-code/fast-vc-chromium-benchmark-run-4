a253b195c2c615d5b9451fcaad833cdbf9f8fcc2
aruslan@chromium.org <aruslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 11:47:15 +0000

    Add waitUntilHasValue() to simplify usage.
    BUG=161941
    
    
    Review URL: https://chromiumcodereview.appspot.com/11280250
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170927 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/util/JSUtils.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/util/JSUtils.java
@@ -62,7 +62,6 @@ public class JSUtils {
             final AwContents awContents,
             final OnEvaluateJavaScriptResultHelper onEvaluateJavaScriptResultHelper,
             final String code) throws Throwable {
-        int currentCallCount = onEvaluateJavaScriptResultHelper.getCallCount();
         testCase.getInstrumentation().runOnMainSync(new Runnable() {
             @Override
             public void run() {
@@ -70,7 +69,7 @@ public class JSUtils {
                         awContents.getContentViewCore(), code);
             }
         });
-        onEvaluateJavaScriptResultHelper.waitForCallback(currentCallCount);
+        onEvaluateJavaScriptResultHelper.waitUntilHasValue();
         Assert.assertTrue("Failed to retrieve JavaScript evaluation results.",
                 onEvaluateJavaScriptResultHelper.hasValue());
         return onEvaluateJavaScriptResultHelper.getJsonResultAndClear();
