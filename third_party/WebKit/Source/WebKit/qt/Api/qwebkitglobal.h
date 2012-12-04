aa43cece4de36266f84e0b8eeb8a2acddfa38b10
pierre.rossi@gmail.com <pierre.rossi@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-04 10:33:56 +0000

    [Qt] Yet another unreviewed speculative windows build fix.
    WebKit2 relies on the QWEBKIT_EXPORT macro from WebKit1 as well.
    
    * Api/qwebkitglobal.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/Api/qwebkitglobal.h
+++ third_party/WebKit/Source/WebKit/qt/Api/qwebkitglobal.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define QTWEBKIT_VERSION_CHECK(major, minor, patch) ((major<<16)|(minor<<8)|(patch))
 
 #ifndef QT_STATIC
-#  if defined(QT_BUILD_WEBKIT_LIB)
+#  if !defined(QT_BUILD_WEBKITWIDGETS_LIB) && defined(BUILDING_WEBKIT)
 #      define QWEBKIT_EXPORT Q_DECL_EXPORT
 #  else
 #      define QWEBKIT_EXPORT Q_DECL_IMPORT
