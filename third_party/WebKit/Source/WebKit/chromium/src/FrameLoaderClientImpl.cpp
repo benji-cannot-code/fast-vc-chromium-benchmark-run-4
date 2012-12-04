e4281d5aa048b92172361aeaf9e708f8150b18ea
creis@chromium.org <creis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-04 17:39:26 +0000

    Add FrameLoaderClient::didDisownOpener https://bugs.webkit.org/show_bug.cgi?id=103789
    Reviewed by Darin Fisher.
    
    Source/WebCore:
    
    Adds a didDisownOwner notification when a frame's opener is cleared,
    so that FrameLoaderClients can react.
    
    No new tests.
    
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::setOpener): Calls didDisownOpener if opener is cleared.
    * loader/FrameLoaderClient.h:
    (FrameLoaderClient):
    (WebCore::FrameLoaderClient::didDisownOpener):
    
    Source/WebKit/chromium:
    
    Forwards the didDisownOpener notification to the WebFrameClient.
    
    * public/WebFrameClient.h:
    (WebFrameClient):
    (WebKit::WebFrameClient::didDisownOpener):
    * src/FrameLoaderClientImpl.cpp:
    (WebKit::FrameLoaderClientImpl::didDisownOpener):
    (WebKit):
    * src/FrameLoaderClientImpl.h:
    (FrameLoaderClientImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136516 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -1217,6 +1217,12 @@ bool FrameLoaderClientImpl::shouldStopLoadingForHistoryItem(HistoryItem* targetI
     return !url.protocolIs(backForwardNavigationScheme);
 }
 
+void FrameLoaderClientImpl::didDisownOpener()
+{
+    if (m_webFrame->client())
+        m_webFrame->client()->didDisownOpener(m_webFrame);
+}
+
 void FrameLoaderClientImpl::didDisplayInsecureContent()
 {
     if (m_webFrame->client())
