60eac209505c188cdb278d4064eed3195377b83d
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 17:02:05 +0000

    [Spellcheck] Cleanup. Resubmit, again
    Completely unmodified resubmit of https://codereview.chromium.org/11361265/
    
    Actual bug causing previous submission to fail is fixed in https://codereview.chromium.org/11413054/
    
    TBR=rlp@chromium.org,sky@chromium.org
    BUG=154918
    
    Review URL: https://codereview.chromium.org/11316090
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170983 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/spellchecker/cocoa_spelling_engine_mac.h
+++ chrome/renderer/spellchecker/cocoa_spelling_engine_mac.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class CocoaSpellingEngine : public SpellingEngine {
  public:
+  virtual void Init(base::PlatformFile bdict_file,
+                    const std::vector<std::string>& custom_words) OVERRIDE;
   virtual bool InitializeIfNeeded() OVERRIDE;
   virtual bool IsEnabled() OVERRIDE;
   virtual bool CheckSpelling(const string16& word_to_check, int tag) OVERRIDE;
