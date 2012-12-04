f96c98d8a63ec561c624b263e6847eb82b65de7e
simonjam@chromium.org <simonjam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-04 22:00:00 +0000

    [User Timing] Fix negative durations https://bugs.webkit.org/show_bug.cgi?id=104038
    Reviewed by Tony Gentilcore.
    
    At some point, we went from passing in a duration to passing in the end time. User Timing
    wasn't updated.
    
    Tests: http/tests/w3c/webperf/submission/Intel/user-timing
    
    * page/PerformanceMark.h:
    (WebCore::PerformanceMark::PerformanceMark):
    * page/PerformanceUserTiming.cpp:
    (WebCore::UserTiming::measure):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/PerformanceMark.h
+++ third_party/WebKit/Source/WebCore/page/PerformanceMark.h
@@ -40,7 +40,7 @@ public:
     static PassRefPtr<PerformanceMark> create(const String& name, double startTime) { return adoptRef(new PerformanceMark(name, startTime)); }
 
 private:
-    PerformanceMark(const String& name, double startTime) : PerformanceEntry(name, "mark", startTime, 0.0) { }
+    PerformanceMark(const String& name, double startTime) : PerformanceEntry(name, "mark", startTime, startTime) { }
     ~PerformanceMark() { }
 };
 
