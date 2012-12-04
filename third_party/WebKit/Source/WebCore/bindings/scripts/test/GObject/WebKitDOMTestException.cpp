09358448cf4c49a8f03b64e5d9deca73d8d7082c
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-04 09:42:07 +0000

    [GTK] Simplify GObject DOM bindings code for handling properties https://bugs.webkit.org/show_bug.cgi?id=101072
    Reviewed by Xan Lopez.
    
    Do not override GObjectClass::set_property if the wrapped object
    doesn't have writable properties, and don't override
    GObjectClass::get_property if the wrapped object doesn't have
    properties at all. It reduces some GObject boilerplate in
    generated code for some objects.
    
    * bindings/scripts/CodeGeneratorGObject.pm:
    (GenerateProperties):
    * bindings/scripts/test/GObject/WebKitDOMFloat64Array.cpp:
    (webkit_dom_float64array_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestActiveDOMObject.cpp:
    (webkit_dom_test_active_dom_object_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestCallback.cpp:
    (webkit_dom_test_callback_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestCustomNamedGetter.cpp:
    (webkit_dom_test_custom_named_getter_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestEventConstructor.cpp:
    (webkit_dom_test_event_constructor_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestEventTarget.cpp:
    (webkit_dom_test_event_target_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestException.cpp:
    (webkit_dom_test_exception_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestInterface.cpp:
    (webkit_dom_test_interface_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestMediaQueryListListener.cpp:
    (webkit_dom_test_media_query_list_listener_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestNamedConstructor.cpp:
    (webkit_dom_test_named_constructor_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestNode.cpp:
    (webkit_dom_test_node_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestObj.cpp:
    (webkit_dom_test_obj_class_init):
    * bindings/scripts/test/GObject/WebKitDOMTestSerializedScriptValueInterface.cpp:
    (webkit_dom_test_serialized_script_value_interface_class_init):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/GObject/WebKitDOMTestException.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/GObject/WebKitDOMTestException.cpp
@@ -92,17 +92,6 @@ static void webkit_dom_test_exception_finalize(GObject* object)
     G_OBJECT_CLASS(webkit_dom_test_exception_parent_class)->finalize(object);
 }
 
-static void webkit_dom_test_exception_set_property(GObject* object, guint propertyId, const GValue* value, GParamSpec* pspec)
-{
-    WebCore::JSMainThreadNullState state;
-    switch (propertyId) {
-    default:
-        G_OBJECT_WARN_INVALID_PROPERTY_ID(object, propertyId, pspec);
-        break;
-    }
-}
-
-
 static void webkit_dom_test_exception_get_property(GObject* object, guint propertyId, GValue* value, GParamSpec* pspec)
 {
     WebCore::JSMainThreadNullState state;
@@ -121,21 +110,11 @@ static void webkit_dom_test_exception_get_property(GObject* object, guint proper
     }
 }
 
-
-static void webkit_dom_test_exception_constructed(GObject* object)
-{
-
-    if (G_OBJECT_CLASS(webkit_dom_test_exception_parent_class)->constructed)
-        G_OBJECT_CLASS(webkit_dom_test_exception_parent_class)->constructed(object);
-}
-
 static void webkit_dom_test_exception_class_init(WebKitDOMTestExceptionClass* requestClass)
 {
     GObjectClass* gobjectClass = G_OBJECT_CLASS(requestClass);
     gobjectClass->finalize = webkit_dom_test_exception_finalize;
-    gobjectClass->set_property = webkit_dom_test_exception_set_property;
     gobjectClass->get_property = webkit_dom_test_exception_get_property;
-    gobjectClass->constructed = webkit_dom_test_exception_constructed;
 
     g_object_class_install_property(gobjectClass,
                                     PROP_NAME,
@@ -144,8 +123,6 @@ static void webkit_dom_test_exception_class_init(WebKitDOMTestExceptionClass* re
                                                            "read-only  gchar* TestException.name", /* longer - could do with some extra doc stuff here */
                                                            "", /* default */
                                                            WEBKIT_PARAM_READABLE));
-
-
 }
 
 static void webkit_dom_test_exception_init(WebKitDOMTestException* request)
