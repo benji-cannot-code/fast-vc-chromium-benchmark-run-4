9bc6a2061cb38d242e98190a9f7fda2c459e04b9
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 03:01:28 +0000

    New resources for launcher rename files to match IDR names.
    BUG=152929
    TEST=none
    
    Review URL: https://codereview.chromium.org/11348366
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170872 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher.cc
+++ ash/launcher/launcher.cc
@@ -109,7 +109,7 @@ class DimmerView : public views::WidgetDelegateView,
     if (!launcher_background) {
       ResourceBundle& rb = ResourceBundle::GetSharedInstance();
       launcher_background =
-          rb.GetImageNamed(IDR_AURA_LAUNCHER_DIMMING).ToImageSkia();
+          rb.GetImageNamed(IDR_AURA_LAUNCHER_DIMMING_BOTTOM).ToImageSkia();
     }
     paint.setAlpha(kDimAlpha);
     canvas->DrawImageInt(
@@ -172,7 +172,7 @@ void Launcher::DelegateView::OnPaintBackground(gfx::Canvas* canvas) {
     if (!launcher_background) {
       ResourceBundle& rb = ResourceBundle::GetSharedInstance();
       launcher_background =
-          rb.GetImageNamed(IDR_AURA_LAUNCHER_BACKGROUND).ToImageSkia();
+          rb.GetImageNamed(IDR_AURA_LAUNCHER_BACKGROUND_BOTTOM).ToImageSkia();
     }
     paint.setAlpha(alpha_);
     canvas->DrawImageInt(
