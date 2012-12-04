fe80b3a4ec465e36cb0636239fa77355f126c6c2
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 09:40:27 +0000

    Pass calls to GetApplicationInfo through the scheduler
    BUG=160904
    
    
    Review URL: https://chromiumcodereview.appspot.com/11280140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170913 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_scheduler.cc
+++ chrome/browser/chromeos/drive/drive_scheduler.cc
@@ -77,6 +77,19 @@ void DriveScheduler::Initialize() {
   initialized_ = true;
 }
 
+void DriveScheduler::GetApplicationInfo(
+    const google_apis::GetDataCallback& callback) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
+
+  scoped_ptr<QueueEntry> new_job(
+      new QueueEntry(TYPE_GET_APPLICATION_INFO, FilePath()));
+  new_job->get_data_callback = callback;
+
+  QueueJob(new_job.Pass());
+
+  StartJobLoop();
+}
+
 void DriveScheduler::Copy(const FilePath& src_file_path,
                           const FilePath& dest_file_path,
                           const FileOperationCallback& callback) {
@@ -234,6 +247,14 @@ void DriveScheduler::DoJobLoop() {
   const QueueEntry* queue_entry = job_iter->second.get();
 
   switch (job_info.job_type) {
+    case TYPE_GET_APPLICATION_INFO: {
+      drive_service_->GetApplicationInfo(
+          base::Bind(&DriveScheduler::OnGetDataJobDone,
+                     weak_ptr_factory_.GetWeakPtr(),
+                     job_id));
+    }
+    break;
+
     case TYPE_COPY: {
       drive_operations_->Copy(
           job_info.file_path,
