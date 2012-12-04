7e8aaf979ac627992591c29bc551419aaeab3b60
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 17:49:34 +0000

    FTP: misc fixes resulting from my testing.
    - "total ..." lines can have values bigger than 32-bit
    - some servers use "l" as a flag in permission listing
    - some servers prepend garbage to the month column
    - NetWare directory listings can have spaces in file names
    - one additional variant of VMS error message
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/11418296
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170996 0039d316-1c4b-4281-b951-d872f2087c98

--- net/ftp/ftp_directory_listing_parser_netware.cc
+++ net/ftp/ftp_directory_listing_parser_netware.cc
@@ -50,7 +50,7 @@ bool ParseFtpDirectoryListingNetware(
     std::vector<string16> columns;
     base::SplitString(CollapseWhitespace(lines[i], false), ' ', &columns);
 
-    if (columns.size() != 8)
+    if (columns.size() < 8)
       return false;
 
     FtpDirectoryListingEntry entry;
@@ -84,7 +84,7 @@ bool ParseFtpDirectoryListingNetware(
       return false;
     }
 
-    entry.name = columns[7];
+    entry.name = FtpUtil::GetStringPartAfterColumns(lines[i], 7);
 
     entries->push_back(entry);
   }
--- net/ftp/ftp_directory_listing_parser_vms.cc
+++ net/ftp/ftp_directory_listing_parser_vms.cc
@@ -125,6 +125,7 @@ bool LooksLikeVmsUserIdentificationCode(const string16& input) {
 bool LooksLikePermissionDeniedError(const string16& text) {
   static const char* kPermissionDeniedMessages[] = {
     "%RMS-E-PRV",
+    "%SYSTEM-F-NOPRIV",
     "privilege",
   };
 
