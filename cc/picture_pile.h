2f5b2f0565ce630f68b088ecdd81f14c0ed66841
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 09:51:20 +0000

    Make PicturePile pile.
    This follows a similar policy as Android browser:
    
    - If an invalidate only intersects the base plus at most one non-base SkPicture, then it creates a new SkPicture of that size.
    - If an invalidate intersects two or more non-base SkPictures, then a new SkPicture is created at the top of the pile, sized to the union of the invalidate plus the bounds of all the intersecting pictures.
    - Whenever new picture's area fully contains an existing picture, that old picture is destroyed.
    - If an SkPicture's area would be >70% of the base, the pile is
      destroyed and the base SkPicture is recreated.
    
    During the invalidate pass, invalidated pictures are represented as
    Pictures with a size but no recording.  Then all the blank pictures
    are filled in at the end of the Update().
    
    BUG=163429
    
    Review URL: https://chromiumcodereview.appspot.com/11299324
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170917 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_pile.h
+++ cc/picture_pile.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_PICTURE_PILE_H_
 #define CC_PICTURE_PILE_H_
 
+#include <list>
+
 #include "base/basictypes.h"
 #include "cc/cc_export.h"
 #include "cc/picture.h"
@@ -39,7 +41,13 @@ public:
   void PushPropertiesTo(PicturePileImpl* other);
 
 private:
-  std::vector<scoped_refptr<Picture> > pile_;
+  friend class PicturePileImpl;
+
+  void InvalidateRect(gfx::Rect invalidation);
+  void ResetPile(ContentLayerClient* painter, RenderingStats& stats);
+
+  typedef std::list<scoped_refptr<Picture> > Pile;
+  Pile pile_;
   gfx::Size size_;
 
   DISALLOW_COPY_AND_ASSIGN(PicturePile);
--- cc/picture_pile_impl.h
+++ cc/picture_pile_impl.h
@@ -10,11 +10,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "cc/cc_export.h"
 #include "cc/picture.h"
+#include "cc/picture_pile.h"
 #include "cc/scoped_ptr_vector.h"
 #include "ui/gfx/rect.h"
 
 namespace cc {
-class PicturePile;
 struct RenderingStats;
 
 class CC_EXPORT PicturePileImpl : public base::RefCounted<PicturePileImpl> {
@@ -35,7 +35,7 @@ private:
   PicturePileImpl();
   ~PicturePileImpl();
 
-  std::vector<scoped_refptr<Picture> > pile_;
+  PicturePile::Pile pile_;
 
   friend class base::RefCounted<PicturePileImpl>;
   DISALLOW_COPY_AND_ASSIGN(PicturePileImpl);
