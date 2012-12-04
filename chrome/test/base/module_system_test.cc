d73cbea6ad49212ca9792c965bb4a3b82bb85bd8
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 07:23:23 +0000

    Add ExceptionHandler to ModuleSystem, remove heap allocated v8::TryCatch.
    BUG=152389
    
    
    Review URL: https://chromiumcodereview.appspot.com/11312157
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170898 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/module_system_test.cc
+++ chrome/test/base/module_system_test.cc
@@ -76,6 +76,13 @@ class StringSourceMap : public ModuleSystem::SourceMap {
   std::map<std::string, std::string> source_map_;
 };
 
+class FailsOnException : public ModuleSystem::ExceptionHandler {
+ public:
+  virtual void HandleUncaughtException() {
+    FAIL();
+  }
+};
+
 ModuleSystemTest::ModuleSystemTest()
     : context_(v8::Context::New()),
       source_map_(new StringSourceMap()),
@@ -85,7 +92,8 @@ ModuleSystemTest::ModuleSystemTest()
   module_system_.reset(new ModuleSystem(context_, source_map_.get()));
   module_system_->RegisterNativeHandler("assert", scoped_ptr<NativeHandler>(
       assert_natives_));
-  try_catch_.SetCaptureMessage(true);
+  module_system_->set_exception_handler(
+      scoped_ptr<ModuleSystem::ExceptionHandler>(new FailsOnException));
 }
 
 ModuleSystemTest::~ModuleSystemTest() {
@@ -113,9 +121,6 @@ void ModuleSystemTest::OverrideNativeHandler(const std::string& name,
 }
 
 void ModuleSystemTest::TearDown() {
-  if (try_catch_.HasCaught())
-    ModuleSystem::DumpException(try_catch_);
-  EXPECT_FALSE(try_catch_.HasCaught());
   // All tests must assert at least once unless otherwise specified.
   EXPECT_EQ(should_assertions_be_made_,
             assert_natives_->assertion_made());
