d0f62245ab7d1a5cf84806020de7cd3ccbf45a2f
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 02:39:17 +0000

    Revert 170866 > Browser Plugin: Update DOM Node attributes when properties are updated > > BUG=163611 > Test=BrowserPluginHostTest.*, BrowserPluginTest.*, WebViewTest.* > > Review URL: https://codereview.chromium.org/11418261
    TBR=fsamuel@chromium.org
    Review URL: https://codereview.chromium.org/11415279
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@170867 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/browser_plugin/browser_plugin_bindings.cc
+++ content/renderer/browser_plugin/browser_plugin_bindings.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/message_loop.h"
 #include "base/string16.h"
-#include "base/string_number_conversions.h"
 #include "base/string_split.h"
 #include "base/utf_string_conversions.h"
 #include "content/renderer/browser_plugin/browser_plugin.h"
@@ -394,7 +393,6 @@ class BrowserPluginPropertyBinding {
   explicit BrowserPluginPropertyBinding(const char name[]) : name_(name) {
   }
   virtual ~BrowserPluginPropertyBinding() {}
-  const std::string& name() const { return name_; }
   bool MatchesName(NPIdentifier name) const {
     return WebBindings::getStringIdentifier(name_.c_str()) == name;
   }
@@ -403,12 +401,6 @@ class BrowserPluginPropertyBinding {
   virtual bool SetProperty(BrowserPluginBindings* bindings,
                            NPObject* np_obj,
                            const NPVariant* variant) = 0;
-  virtual std::string GetDOMAttributeValue(BrowserPlugin* browser_plugin) = 0;
-  // Updates the DOM Attribute value with the current property value.
-  void UpdateDOMAttribute(BrowserPluginBindings* bindings) {
-    bindings->instance()->UpdateDOMAttribute(name(),
-        GetDOMAttributeValue(bindings->instance()));
-  }
  private:
   std::string name_;
 
@@ -423,20 +415,16 @@ class BrowserPluginPropertyBindingAutoSize
   }
   virtual bool GetProperty(BrowserPluginBindings* bindings,
                            NPVariant* result) OVERRIDE {
-    bool auto_size = bindings->instance()->auto_size_attribute();
-    BOOLEAN_TO_NPVARIANT(auto_size, *result);
+    bool autosize = bindings->instance()->auto_size_attribute();
+    BOOLEAN_TO_NPVARIANT(autosize, *result);
     return true;
   }
   virtual bool SetProperty(BrowserPluginBindings* bindings,
                            NPObject* np_obj,
                            const NPVariant* variant) OVERRIDE {
-    bool auto_size = NPVARIANT_TO_BOOLEAN(*variant);
-    bindings->instance()->SetAutoSizeAttribute(auto_size);
+    bool autosize = NPVARIANT_TO_BOOLEAN(*variant);
+    bindings->instance()->SetAutoSizeAttribute(autosize);
     return true;
-  }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return browser_plugin->auto_size_attribute() ? "true" : "false";
   }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingAutoSize);
@@ -462,9 +450,6 @@ class BrowserPluginPropertyBindingContentWindow
                            const NPVariant* variant) OVERRIDE {
     return false;
   }
-  virtual std::string GetDOMAttributeValue(BrowserPlugin* browser_plugin) {
-    return std::string();
-  }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingContentWindow);
 };
@@ -487,10 +472,6 @@ class BrowserPluginPropertyBindingMaxHeight
     int max_height = Int32FromNPVariant(*variant);
     bindings->instance()->SetMaxHeightAttribute(max_height);
     return true;
-  }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return base::IntToString(browser_plugin->max_height_attribute());
   }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingMaxHeight);
@@ -515,10 +496,6 @@ class BrowserPluginPropertyBindingMaxWidth
     bindings->instance()->SetMaxWidthAttribute(max_width);
     return true;
   }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return base::IntToString(browser_plugin->max_width_attribute());
-  }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingMaxWidth);
 };
@@ -542,10 +519,6 @@ class BrowserPluginPropertyBindingMinHeight
     bindings->instance()->SetMinHeightAttribute(min_height);
     return true;
   }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return base::IntToString(browser_plugin->min_height_attribute());
-  }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingMinHeight);
 };
@@ -568,10 +541,6 @@ class BrowserPluginPropertyBindingMinWidth
     int min_width = Int32FromNPVariant(*variant);
     bindings->instance()->SetMinWidthAttribute(min_width);
     return true;
-  }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return base::IntToString(browser_plugin->min_width_attribute());
   }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingMinWidth);
@@ -601,10 +570,6 @@ class BrowserPluginPropertyBindingPartition
     }
     return true;
   }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return browser_plugin->GetPartitionAttribute();
-  }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingPartition);
 };
@@ -630,10 +595,6 @@ class BrowserPluginPropertyBindingSrc : public BrowserPluginPropertyBinding {
       return false;
     }
     return true;
-  }
-  virtual std::string GetDOMAttributeValue(
-      BrowserPlugin* browser_plugin) OVERRIDE {
-    return browser_plugin->src_attribute();
   }
  private:
   DISALLOW_COPY_AND_ASSIGN(BrowserPluginPropertyBindingSrc);
@@ -720,13 +681,8 @@ bool BrowserPluginBindings::SetProperty(NPObject* np_obj,
   for (PropertyBindingList::iterator iter = property_bindings_.begin();
        iter != property_bindings_.end();
        ++iter) {
-    if ((*iter)->MatchesName(name)) {
-      if ((*iter)->SetProperty(this, np_obj, variant)) {
-        (*iter)->UpdateDOMAttribute(this);
-        return true;
-      }
-      break;
-    }
+    if ((*iter)->MatchesName(name))
+      return (*iter)->SetProperty(this, np_obj, variant);
   }
   return false;
 }
