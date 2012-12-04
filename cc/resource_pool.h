5cb5f4e0a2bec97fb67558a216cfc572eb771549
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-04 21:17:54 +0000

    cc: Add basic texture recycling to ResourcePool.
    Add enough recycling logic to ResourcePool to eliminate unnecessary
    command buffer flushes.
    
    BUG=163430
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/11418299
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171046 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resource_pool.h
+++ cc/resource_pool.h
@@ -6,14 +6,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_RESOURCE_POOL_H_
 #define CC_RESOURCE_POOL_H_
 
+#include <list>
+
 #include "base/memory/scoped_ptr.h"
 #include "cc/renderer.h"
-#include "cc/resource_provider.h"
+#include "cc/resource.h"
 
 namespace cc {
+class ResourceProvider;
 
 class ResourcePool {
  public:
+  class Resource : public cc::Resource {
+   public:
+    Resource(ResourceProvider* resource_provider,
+             Renderer::ResourcePool pool_id,
+             const gfx::Size& size,
+             GLenum format);
+    ~Resource();
+
+   private:
+    ResourceProvider* resource_provider_;
+
+    DISALLOW_COPY_AND_ASSIGN(Resource);
+  };
+
   static scoped_ptr<ResourcePool> Create(ResourceProvider* resource_provider,
                                          Renderer::ResourcePool pool_id) {
     return make_scoped_ptr(new ResourcePool(resource_provider, pool_id));
@@ -23,9 +40,11 @@ class ResourcePool {
 
   ResourceProvider* resource_provider() { return resource_provider_; };
 
-  ResourceProvider::ResourceId AcquireResource(
+  scoped_ptr<ResourcePool::Resource> AcquireResource(
       const gfx::Size&, GLenum format);
-  void ReleaseResource(ResourceProvider::ResourceId);
+  void ReleaseResource(scoped_ptr<ResourcePool::Resource>);
+
+  void SetMaxMemoryUsageBytes(size_t max_memory_usage_bytes);
 
  protected:
   ResourcePool(ResourceProvider* resource_provider,
@@ -34,6 +53,11 @@ class ResourcePool {
  private:
   ResourceProvider* resource_provider_;
   Renderer::ResourcePool pool_id_;
+  size_t max_memory_usage_bytes_;
+  size_t memory_usage_bytes_;
+
+  typedef std::list<Resource*> ResourceList;
+  ResourceList resources_;
 
   DISALLOW_COPY_AND_ASSIGN(ResourcePool);
 };
