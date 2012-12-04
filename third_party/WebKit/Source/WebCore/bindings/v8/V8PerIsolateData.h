44db2e1f5920a732207624e8b146dbaef5b95200
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-04 18:28:32 +0000

    [V8] Make v8::Null(isolate) faster by caching the value in V8 bindings https://bugs.webkit.org/show_bug.cgi?id=103862
    Reviewed by Adam Barth.
    
    Performance comparison between V8 and JSC revealed that DOM attributes
    that return v8::Null(isolate) are much slower than JSC.
    (https://docs.google.com/a/chromium.org/spreadsheet/ccc?key=0AlobCOyvTnPKdDg5S0dMdGRGRTRSaW53V1ppVzh6eXc#gid=2)
    
    We can optimize v8::Null(isolate) by caching the value to V8PerIsolateData.
    (Although I don't like the optimization, it looks difficult to optimize
    v8::Null(isolate) more.)
    
    [Bindings/undefined-first-child.html]
    Before: 969 runs/sec, 970 runs/sec, 962 runs/sec
    After:  1086 runs/sec, 1085 runs/sec, 1086 runs/sec
    
    No tests. No change in behavior.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateHeader):
    (GenerateNormalAttrGetter):
    (GenerateFunctionCallback):
    (NativeToJSValue):
    * bindings/scripts/test/V8/V8TestNode.h:
    (WebCore::toV8Fast):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::contentDocumentAttrGetter):
    (WebCore::TestObjV8Internal::getSVGDocumentCallback):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore::TestSerializedScriptValueInterfaceV8Internal::valueAttrGetter):
    (WebCore::TestSerializedScriptValueInterfaceV8Internal::readonlyValueAttrGetter):
    (WebCore::TestSerializedScriptValueInterfaceV8Internal::cachedValueAttrGetter):
    (WebCore::TestSerializedScriptValueInterfaceV8Internal::cachedReadonlyValueAttrGetter):
    * bindings/v8/V8Binding.h:
    (WebCore):
    (WebCore::v8Null):
    (WebCore::v8NullWithCheck):
    * bindings/v8/V8PerIsolateData.cpp:
    (WebCore::V8PerIsolateData::V8PerIsolateData):
    * bindings/v8/V8PerIsolateData.h:
    (WebCore::V8PerIsolateData::v8Null):
    (V8PerIsolateData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8PerIsolateData.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8PerIsolateData.h
@@ -79,6 +79,8 @@ public:
     StringCache* stringCache() { return m_stringCache.get(); }
     IntegerCache* integerCache() { return m_integerCache.get(); }
 
+    v8::Handle<v8::Value> v8Null() { return m_v8Null.get(); }
+
     v8::Persistent<v8::Value> ensureLiveRoot();
 
 #if ENABLE(INSPECTOR)
@@ -139,6 +141,7 @@ private:
     v8::Persistent<v8::FunctionTemplate> m_lazyEventListenerToStringTemplate;
     OwnPtr<StringCache> m_stringCache;
     OwnPtr<IntegerCache> m_integerCache;
+    ScopedPersistent<v8::Value> m_v8Null;
 
     Vector<DOMDataStore*> m_domDataList;
     DOMDataStore* m_domDataStore;
