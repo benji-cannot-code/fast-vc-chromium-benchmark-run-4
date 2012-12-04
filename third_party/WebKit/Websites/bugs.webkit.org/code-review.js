6161455f913bf0830ce05e11a6978e1de1858156
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-04 21:11:01 +0000

    Can't add followup comment to a previous comment https://bugs.webkit.org/show_bug.cgi?id=104025
    Reviewed by Adam Barth.
    
    If we side-by-sidify a shared diff line, and then apply
    a previous comment, we would incorrectly put the comment
    on the Line instead of the LineContainer.
    
    Also, get rid of global next_line_id to simplify testing.
    
    * code-review-test.html:
    * code-review.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136556 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/code-review.js
+++ third_party/WebKit/Websites/bugs.webkit.org/code-review.js
@@ -67,7 +67,6 @@ var CODE_REVIEW_UNITTEST;
   var minLeftSideRatio = 10;
   var maxLeftSideRatio = 90;
   var file_diff_being_resized = null;
-  var next_line_id = 0;
   var files = {};
   var original_file_contents = {};
   var patched_file_contents = {};
@@ -90,18 +89,15 @@ var CODE_REVIEW_UNITTEST;
     return 'line' + number;
   }
 
-  function nextLineID() {
-    return idForLine(next_line_id++);
-  }
-
   function forEachLine(callback) {
-    for (var i = 0; i < next_line_id; ++i) {
-      callback($('#' + idForLine(i)));
-    }
-  }
-
-  function idify() {
-    this.id = nextLineID();
+    var i = 0;
+    var line;
+    do {
+      line = $('#' + idForLine(i));
+      if (line[0])
+        callback(line);
+      i++;
+    } while (line[0]);
   }
 
   function hoverify() {
@@ -424,7 +420,7 @@ var CODE_REVIEW_UNITTEST;
       $(file).find(query).each(function() {
         if ($(this).text() != line_number)
           return;
-        var line = $(this).parent();
+        var line = lineContainerFromDescendant($(this));
         addPreviousComment(line, author, comment_text);
       });
     }
@@ -602,6 +598,11 @@ var CODE_REVIEW_UNITTEST;
   }
 
   function crawlDiff() {
+    var next_line_id = 0;
+    var idify = function() {
+      this.id = idForLine(next_line_id++);
+    }
+
     $('.Line').each(idify).each(hoverify);
     $('.FileDiff').each(function() {
       var header = $(this).children('h1');
@@ -1929,6 +1930,10 @@ var CODE_REVIEW_UNITTEST;
     return descendant.hasClass('Line') ? descendant : descendant.parents('.Line');
   }
 
+  function lineContainerFromDescendant(descendant) {
+    return descendant.hasClass('LineContainer') ? descendant : descendant.parents('.LineContainer');
+  }
+
   function lineFromLineContainer(lineContainer) {
     var line = $(lineContainer);
     if (!line.hasClass('Line'))
@@ -2038,6 +2043,8 @@ var CODE_REVIEW_UNITTEST;
     window.addPreviousComment = addPreviousComment;
     window.tracLinks = tracLinks;
     window.crawlDiff = crawlDiff;
+    window.convertAllFileDiffs = convertAllFileDiffs;
+    window.displayPreviousComments = displayPreviousComments;
     window.discardComment = discardComment;
     window.addCommentField = addCommentField;
     window.acceptComment = acceptComment;
