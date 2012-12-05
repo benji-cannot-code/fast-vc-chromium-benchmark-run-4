5f7aa9804d2615342d9388ab668f0867bf7e13cc
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 20:38:50 +0000

    app_list: Improve initial icon loading.
    - Creates and populates AppListModel and starts icon loading for
      current page apps before creating views;
    - Shows UI when all icons are loaded or a maximum wait time has
      passed (currently 50ms);
    
    BUG=161025
    TEST=Bring up app launcher and the app icons should not flash.
    
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11418312
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171299 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/apps_grid_view.cc
+++ ui/app_list/apps_grid_view.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
-#include "ui/app_list/app_list_item_model.h"
 #include "ui/app_list/app_list_item_view.h"
 #include "ui/app_list/apps_grid_view_delegate.h"
 #include "ui/app_list/page_switcher.h"
@@ -324,7 +323,8 @@ void AppsGridView::ViewHierarchyChanged(bool is_add,
 }
 
 void AppsGridView::Update() {
-  selected_view_ = NULL;
+  DCHECK(!selected_view_ && !drag_view_);
+
   view_model_.Clear();
   if (model_ && model_->apps()->item_count())
     ListItemsAdded(0, model_->apps()->item_count());
