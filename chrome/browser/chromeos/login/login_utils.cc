65525808ced02a2bf67ca6253cd52aa3e498c126
wjmaclean@chromium.org <wjmaclean@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 21:06:06 +0000

    Simplify pinch flags.
    At present, we have two flags to enable two different pinch models:
    
    enable-pinch => enables Android style zoom
    enable-pinch + enable-pinch-in-compositor => enables chrome linux/win accelerated zoom
    
    This CL rationalizes the flags so that
    
    enable-css-transform-pinch => enables Android style zoom
    enable-pinch => enables chrome linux/win accelerated zoom
    
    and the latter can be controlled via about:flags.
    
    BUG=
    NOTRY=TRUE
    
    
    Review URL: https://chromiumcodereview.appspot.com/11348353
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171309 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_utils.cc
+++ chrome/browser/chromeos/login/login_utils.cc
@@ -816,7 +816,6 @@ std::string LoginUtilsImpl::GetOffTheRecordCommandLine(
       ash::switches::kAshDisablePanelFitting,
       cc::switches::kDisableThreadedAnimation,
       cc::switches::kEnablePartialSwap,
-      cc::switches::kEnablePinchInCompositor,
       chromeos::switches::kDbusStub,
       chromeos::switches::kEnableNewNetworkHandlers,
       gfx::switches::kEnableBrowserTextSubpixelPositioning,
--- content/public/common/content_switches.h
+++ content/public/common/content_switches.h
@@ -109,6 +109,7 @@ CONTENT_EXPORT extern const char kUseFakeDeviceForMediaStream[];
 extern const char kEnableMonitorProfile[];
 extern const char kEnableUIReleaseFrontSurface[];
 extern const char kEnablePinch[];
+extern const char kEnableCssTransformPinch[];
 extern const char kEnablePreparsedJsCaching[];
 CONTENT_EXPORT extern const char kEnablePrivilegedWebGLExtensions[];
 extern const char kEnablePruneGpuCommandBuffers[];
