39252f6bbff7d718d3005ba3e0796bb9a14e2495
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 06:18:58 +0000

    Pass calls to GetAccountMetadata through the scheduler
    BUG=160904
    
    
    Review URL: https://chromiumcodereview.appspot.com/11299153
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171184 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_feed_loader.cc
+++ chrome/browser/chromeos/drive/drive_feed_loader.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/drive_webapps_registry.h"
 #include "chrome/browser/google_apis/drive_api_parser.h"
 #include "chrome/browser/google_apis/drive_api_util.h"
-#include "chrome/browser/google_apis/drive_service_interface.h"
 #include "chrome/common/chrome_switches.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -255,13 +254,11 @@ struct DriveFeedLoader::GetDocumentsUiState {
 
 DriveFeedLoader::DriveFeedLoader(
     DriveResourceMetadata* resource_metadata,
-    google_apis::DriveServiceInterface* drive_service,
     DriveScheduler* scheduler,
     DriveWebAppsRegistryInterface* webapps_registry,
     DriveCache* cache,
     scoped_refptr<base::SequencedTaskRunner> blocking_task_runner)
     : resource_metadata_(resource_metadata),
-      drive_service_(drive_service),
       scheduler_(scheduler),
       webapps_registry_(webapps_registry),
       cache_(cache),
@@ -308,7 +305,7 @@ void DriveFeedLoader::ReloadFromServerIfNeeded(
 
   // First fetch the latest changestamp to see if there were any new changes
   // there at all.
-  drive_service_->GetAccountMetadata(
+  scheduler_->GetAccountMetadata(
       base::Bind(&DriveFeedLoader::OnGetAccountMetadata,
                  weak_ptr_factory_.GetWeakPtr(),
                  callback));
