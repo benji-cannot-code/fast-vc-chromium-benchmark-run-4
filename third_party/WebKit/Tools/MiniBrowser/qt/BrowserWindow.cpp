629def2938545a57113b7bfca162feb99c37a4ca
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-05 17:48:15 +0000

    Fix compilation for Qt5.0.0 stable branch. https://bugs.webkit.org/show_bug.cgi?id=103870
    Source/WebCore:
    
    Patch by Andras Becsi <andras.becsi@digia.com> on 2012-12-05
    Reviewed by Simon Hausmann.
    
    This is a squashed patch consisting of patches by multiple authors.
    
    Fixed use of to-be-removed compatibility functions in QWindow
    Patch by Samuel Rødal <samuel.rodal@digia.com>
    
    Build fix. No new tests needed.
    
    * platform/graphics/gstreamer/PlatformVideoWindowQt.cpp:
    (FullScreenVideoWindow::FullScreenVideoWindow):
    (PlatformVideoWindow::PlatformVideoWindow):
    
    Source/WebKit2:
    
    Patch by Michael Brüning <michael.bruning@digia.com> on 2012-12-05
    Reviewed by Simon Hausmann.
    
    QWindow::pos() and QQuickItem::pos() have been renamed to position().
    Patch by Lars Knoll <lars.knoll@digia.com>
    
    * UIProcess/qt/PageViewportControllerClientQt.cpp:
    (WebKit::PageViewportControllerClientQt::setContentRectVisiblePositionAtScale):
    (WebKit::PageViewportControllerClientQt::setViewportPosition):
    
    Tools:
    
    Patch by Andras Becsi <andras.becsi@digia.com> on 2012-12-05
    Reviewed by Simon Hausmann.
    
    Fix the use of renamed QWindow functions.
    Patch by Andras Becsi <andras.becsi@digia.com>
    
    * MiniBrowser/qt/BrowserWindow.cpp:
    (BrowserWindow::BrowserWindow):
    (BrowserWindow::onTitleChanged):
    * MiniBrowser/qt/raw/View.cpp:
    (View::View):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136707 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/qt/BrowserWindow.cpp
+++ third_party/WebKit/Tools/MiniBrowser/qt/BrowserWindow.cpp
@@ -40,8 +40,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BrowserWindow::BrowserWindow(WindowOptions* options)
     : m_windowOptions(options)
 {
-    setWindowTitle("MiniBrowser");
-    setWindowFlags(Qt::Window | Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint);
+    setTitle("MiniBrowser");
+    setFlags(Qt::Window | Qt::WindowTitleHint | Qt::WindowMinMaxButtonsHint | Qt::WindowCloseButtonHint);
     setResizeMode(QQuickView::SizeRootObjectToView);
 
     // This allows starting MiniBrowser from the build directory without previously defining QML_IMPORT_PATH.
@@ -148,7 +148,7 @@ void BrowserWindow::onTitleChanged(QString title)
 {
     if (title.isEmpty())
         title = QLatin1String("MiniBrowser");
-    setWindowTitle(title);
+    setTitle(title);
 }
 
 void BrowserWindow::zoomIn()
