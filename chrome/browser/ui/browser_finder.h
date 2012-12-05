85942f804f500e5557aaddb57db63e86fb38d96d
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 05:55:57 +0000

    Add HostDesktopType parameter to browser::FindAnyBrowser().
    BUG=129187
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416349
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_finder.h
+++ chrome/browser/ui/browser_finder.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_BROWSER_FINDER_H_
 
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/host_desktop.h"
 #include "ui/gfx/native_widget_types.h"
 
 class Profile;
@@ -39,9 +40,11 @@ Browser* FindOrCreateTabbedBrowser(Profile* profile);
 Browser* FindOrCreateTabbedBrowser(Profile* profile,
                                    chrome::HostDesktopType type);
 
-// Find an existing browser window with any type. See comment above for
-// additional information.
-Browser* FindAnyBrowser(Profile* profile, bool match_original_profiles);
+// Finds an existing browser window of any kind.
+// |type| refers to the host desktop the returned browser should belong to.
+Browser* FindAnyBrowser(Profile* profile,
+                        bool match_original_profiles,
+                        chrome::HostDesktopType type);
 
 // Find an existing browser window with the provided profile and hosted in the
 // given desktop. Searches in the order of last activation. Only browsers that
