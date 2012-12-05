3f468b472583fd1351fc4674f215035a0c7b2fff
sheckylin@chromium.org <sheckylin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 15:00:14 +0000

    Handling cursor visibility changing events for ChromeOS
    The CL does 5 things:
    1. A handler in RootWindowHost that will be called whenever cursor
    visibility is changed.
    2. An utility function in RootWindowHostLinux that can be used to
    set XInput device properties.
    3. Use 1) and 2) to temporarily pause trackpad tap-to-click feature
    when the cursor is hidden.
    4. Add a cache class that caches X input device lists to minimize
    round-trip time to X server for device list queries.
    5. Refactor around the places where XListInputDevices/XIQueryDevice
    queries are used.
    
    Contributed by sheckylin@chromium.org
    
    BUG=chromium:156697,chrome-os-partner:14085
    TEST=Tested on link. "Tap paused" was successfully set when visibility
    changed.
    
    Change-Id: Ia5c62f821c80efc973e3b1ce54a467b674b466a4
    
    
    Review URL: https://chromiumcodereview.appspot.com/11316074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171223 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_win.cc
@@ -432,6 +432,9 @@ bool DesktopRootWindowHostWin::ConfineCursorToRootWindow() {
 void DesktopRootWindowHostWin::UnConfineCursor() {
 }
 
+void DesktopRootWindowHostWin::OnCursorVisibilityChanged(bool show) {
+}
+
 void DesktopRootWindowHostWin::MoveCursorTo(const gfx::Point& location) {
 }
 
