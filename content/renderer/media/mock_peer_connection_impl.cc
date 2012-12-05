51e963b2f99efdd2f975e49001132156c610acde
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 21:57:05 +0000

    Rolling libjingle r245:r248
    Review URL: https://chromiumcodereview.appspot.com/11438011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171327 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/mock_peer_connection_impl.cc
+++ content/renderer/media/mock_peer_connection_impl.cc
@@ -110,6 +110,13 @@ bool MockPeerConnectionImpl::SendDtmf(
   return false;
 }
 
+bool MockPeerConnectionImpl::GetStats(
+    webrtc::StatsObserver* observer,
+    webrtc::MediaStreamTrackInterface* track) {
+  NOTIMPLEMENTED();
+  return false;
+}
+
 void MockPeerConnectionImpl::RemoveStream(
     MediaStreamInterface* local_stream) {
   DCHECK_EQ(stream_label_, local_stream->label());
