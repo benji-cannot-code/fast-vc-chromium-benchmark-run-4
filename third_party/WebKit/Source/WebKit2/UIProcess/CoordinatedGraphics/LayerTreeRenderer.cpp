3dc091fcc1c3e6ee5f354a856c3bf00e9d86a24a
christophe.dumez@intel.com <christophe.dumez@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-05 10:11:40 +0000

    [CoordinatedGraphics] Use unsigned integers for CoordinatedTile IDs https://bugs.webkit.org/show_bug.cgi?id=103816
    Reviewed by Jocelyn Turcotte.
    
    Use unsigned integer for CoordinatedTile identifier type.
    CoordinatedTile was previously using signed integers for
    its identifier which is unsafe because the generated ID
    will overflow at some point and the C and C++ language
    standards say that overflow of a signed value is undefined
    behaviour.
    
    * UIProcess/CoordinatedGraphics/CoordinatedBackingStore.cpp:
    (WebKit::CoordinatedBackingStore::createTile):
    (WebKit::CoordinatedBackingStore::removeTile):
    (WebKit::CoordinatedBackingStore::removeAllTiles):
    (WebKit::CoordinatedBackingStore::updateTile):
    (WebKit::CoordinatedBackingStore::texture):
    (WebKit::CoordinatedBackingStore::paintToTextureMapper):
    (WebKit::CoordinatedBackingStore::commitTileOperations):
    * UIProcess/CoordinatedGraphics/CoordinatedBackingStore.h:
    (CoordinatedBackingStore):
    * UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.cpp:
    (WebKit::LayerTreeCoordinatorProxy::createTileForLayer):
    (WebKit::LayerTreeCoordinatorProxy::updateTileForLayer):
    (WebKit::LayerTreeCoordinatorProxy::removeTileForLayer):
    * UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.h:
    (LayerTreeCoordinatorProxy):
    * UIProcess/CoordinatedGraphics/LayerTreeCoordinatorProxy.messages.in:
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp:
    (WebKit::LayerTreeRenderer::createTile):
    (WebKit::LayerTreeRenderer::removeTile):
    (WebKit::LayerTreeRenderer::updateTile):
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.h:
    (LayerTreeRenderer):
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::createTile):
    (WebCore::CoordinatedGraphicsLayer::updateTile):
    (WebCore::CoordinatedGraphicsLayer::removeTile):
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.h:
    (CoordinatedGraphicsLayerClient):
    (CoordinatedGraphicsLayer):
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedTile.cpp:
    (WebKit):
    (WebKit::CoordinatedTile::CoordinatedTile):
    (WebKit::CoordinatedTile::~CoordinatedTile):
    (WebKit::CoordinatedTile::updateBackBuffer):
    (WebKit::CoordinatedTile::isReadyToPaint):
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedTile.h:
    (CoordinatedTile):
    (CoordinatedTileClient):
    * WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.cpp:
    (WebKit::LayerTreeCoordinator::createTile):
    (WebKit::LayerTreeCoordinator::updateTile):
    (WebKit::LayerTreeCoordinator::removeTile):
    * WebProcess/WebPage/CoordinatedGraphics/LayerTreeCoordinator.h:
    (LayerTreeCoordinator):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp
@@ -485,7 +485,7 @@ void LayerTreeRenderer::resetBackingStoreSizeToLayerSize(GraphicsLayer* graphics
     backingStore->setSize(graphicsLayer->size());
 }
 
-void LayerTreeRenderer::createTile(WebLayerID layerID, int tileID, float scale)
+void LayerTreeRenderer::createTile(WebLayerID layerID, uint32_t tileID, float scale)
 {
     GraphicsLayer* layer = layerByID(layerID);
     ASSERT(layer);
@@ -495,7 +495,7 @@ void LayerTreeRenderer::createTile(WebLayerID layerID, int tileID, float scale)
     resetBackingStoreSizeToLayerSize(layer);
 }
 
-void LayerTreeRenderer::removeTile(WebLayerID layerID, int tileID)
+void LayerTreeRenderer::removeTile(WebLayerID layerID, uint32_t tileID)
 {
     GraphicsLayer* layer = layerByID(layerID);
     ASSERT(layer);
@@ -508,7 +508,7 @@ void LayerTreeRenderer::removeTile(WebLayerID layerID, int tileID)
     m_backingStoresWithPendingBuffers.add(backingStore);
 }
 
-void LayerTreeRenderer::updateTile(WebLayerID layerID, int tileID, const TileUpdate& update)
+void LayerTreeRenderer::updateTile(WebLayerID layerID, uint32_t tileID, const TileUpdate& update)
 {
     GraphicsLayer* layer = layerByID(layerID);
     ASSERT(layer);
