478076560c4729a01d33c1a54ef4610c347b0300
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-05 05:18:55 +0000

    Web Inspector: Resources: domain cookies are not shown for sub-sub domain pages. https://bugs.webkit.org/show_bug.cgi?id=104016
    Patch by Eugene Klyuchnikov <eustas@chromium.org> on 2012-12-04
    Reviewed by Pavel Feldman.
    
    Fixed regexp to allow zero or more subdomain prefix (was zero or one).
    
    * inspector/front-end/CookieParser.js:
    (WebInspector.Cookies.cookieDomainMatchesResourceDomain): Fixed regexp.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136636 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/CookieParser.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/CookieParser.js
@@ -423,5 +423,5 @@ WebInspector.Cookies.cookieDomainMatchesResourceDomain = function(cookieDomain, 
 {
     if (cookieDomain.charAt(0) !== '.')
         return resourceDomain === cookieDomain;
-    return !!resourceDomain.match(new RegExp("^([^\\.]+\\.)?" + cookieDomain.substring(1).escapeForRegExp() + "$", "i"));
+    return !!resourceDomain.match(new RegExp("^([^\\.]+\\.)*" + cookieDomain.substring(1).escapeForRegExp() + "$", "i"));
 }
