cbc80f5354b246757f794f63186128897c558dd2
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 02:21:28 +0000

    [Spellcheck] Added UMA metrics for WebKit API usage
    Part of the new metrics suggested for Spellcheck.
    
    R=rlp@chromium.org
    BUG=none.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11308339
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/spellchecker/spellcheck_provider.cc
+++ chrome/renderer/spellchecker/spellcheck_provider.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/spellchecker/spellcheck_provider.h"
 
 #include "base/command_line.h"
+#include "base/metrics/histogram.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/spellcheck_messages.h"
 #include "chrome/common/spellcheck_result.h"
@@ -145,9 +146,10 @@ void SpellCheckProvider::spellCheck(
   spellcheck_->SpellCheckWord(
       word.c_str(), word.size(), routing_id(),
       &offset, &length, optional_suggestions ? & suggestions : NULL);
-  if (optional_suggestions)
-    *optional_suggestions = suggestions;
-  if (!optional_suggestions) {
+  if (optional_suggestions) {
+    UMA_HISTOGRAM_COUNTS("SpellCheck.api.check.suggestions", word.size());
+  } else {
+    UMA_HISTOGRAM_COUNTS("SpellCheck.api.check", word.size());
     // If optional_suggestions is not requested, the API is called
     // for marking.  So we use this for counting markable words.
     Send(new SpellCheckHostMsg_NotifyChecked(routing_id(), word, 0 < length));
@@ -168,6 +170,7 @@ void SpellCheckProvider::checkTextOfParagraph(
     return;
 
   spellcheck_->SpellCheckParagraph(string16(text), results);
+  UMA_HISTOGRAM_COUNTS("SpellCheck.api.paragraph", text.length());
 #endif
 }
 
@@ -175,17 +178,21 @@ void SpellCheckProvider::requestCheckingOfText(
     const WebString& text,
     WebTextCheckingCompletion* completion) {
   RequestTextChecking(text, completion);
+  UMA_HISTOGRAM_COUNTS("SpellCheck.api.async", text.length());
 }
 
 WebString SpellCheckProvider::autoCorrectWord(const WebString& word) {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
-  if (command_line.HasSwitch(switches::kEnableSpellingAutoCorrect))
+  if (command_line.HasSwitch(switches::kEnableSpellingAutoCorrect)) {
+    UMA_HISTOGRAM_COUNTS("SpellCheck.api.autocorrect", word.length());
     return spellcheck_->GetAutoCorrectionWord(word, routing_id());
+  }
   return string16();
 }
 
 void SpellCheckProvider::showSpellingUI(bool show) {
 #if defined(OS_MACOSX)
+  UMA_HISTOGRAM_BOOLEAN("SpellCheck.api.showUI", show);
   Send(new SpellCheckHostMsg_ShowSpellingPanel(routing_id(), show));
 #endif
 }
