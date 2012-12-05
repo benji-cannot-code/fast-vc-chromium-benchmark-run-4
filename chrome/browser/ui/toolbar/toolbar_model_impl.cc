e4bb92958a1e2d93dd8c67467e488e169ef40b79
dcblack@chromium.org <dcblack@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 12:11:39 +0000

    Always show a magnifying glass next to queries in the omnibox when doing search term replacement.
    BUG=161589
    
    Review URL: https://chromiumcodereview.appspot.com/11416051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171211 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/toolbar_model_impl.cc
+++ chrome/browser/ui/toolbar/toolbar_model_impl.cc
@@ -50,10 +50,8 @@ ToolbarModelImpl::~ToolbarModelImpl() {
 // ToolbarModelImpl Implementation.
 string16 ToolbarModelImpl::GetText(
     bool display_search_urls_as_search_terms) const {
-  GURL url(GetURL());
-
   if (display_search_urls_as_search_terms) {
-    string16 search_terms = TryToExtractSearchTermsFromURL(url);
+    string16 search_terms = TryToExtractSearchTermsFromURL();
     if (!search_terms.empty())
       return search_terms;
   }
@@ -62,6 +60,7 @@ string16 ToolbarModelImpl::GetText(
   if (profile)
     languages = profile->GetPrefs()->GetString(prefs::kAcceptLanguages);
 
+  GURL url(GetURL());
   if (url.spec().length() > content::kMaxURLDisplayChars)
     url = url.IsStandard() ? url.GetOrigin() : GURL(url.scheme() + ":");
   // Note that we can't unescape spaces here, because if the user copies this
@@ -84,7 +83,7 @@ GURL ToolbarModelImpl::GetURL() const {
 }
 
 bool ToolbarModelImpl::WouldReplaceSearchURLWithSearchTerms() const {
-  return !TryToExtractSearchTermsFromURL(GetURL()).empty();
+  return !TryToExtractSearchTermsFromURL().empty();
 }
 
 bool ToolbarModelImpl::ShouldDisplayURL() const {
@@ -159,6 +158,8 @@ ToolbarModelImpl::SecurityLevel ToolbarModelImpl::GetSecurityLevel() const {
 }
 
 int ToolbarModelImpl::GetIcon() const {
+  if (WouldReplaceSearchURLWithSearchTerms())
+    return IDR_OMNIBOX_SEARCH;
   static int icon_ids[NUM_SECURITY_LEVELS] = {
     IDR_LOCATION_BAR_HTTP,
     IDR_OMNIBOX_HTTPS_VALID,
@@ -211,8 +212,8 @@ NavigationController* ToolbarModelImpl::GetNavigationController() const {
   return current_tab ? &current_tab->GetController() : NULL;
 }
 
-string16 ToolbarModelImpl::TryToExtractSearchTermsFromURL(
-    const GURL& url) const {
+string16 ToolbarModelImpl::TryToExtractSearchTermsFromURL() const {
+  const GURL& url = GetURL();
   Profile* profile = GetProfile();
 
   // Ensure instant extended API is enabled and query URL is HTTPS.
