2cce35c0379aad59ede1d261fa18748ee48d3df1
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 00:09:58 +0000

    Fixes theme includes for test_webkit_platform_support. I don't believe the linux one is needed at all. Layout tests that try to set a default theme when USE_DEFAULT_RENDER_THEME is set won't work. I'll figure that out when we get further along.
    BUG=none
    TEST=none
    R=tony@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11416342
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171090 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/test_webkit_platform_support.cc
+++ webkit/support/test_webkit_platform_support.cc
@@ -62,8 +62,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/tools/test_shell/test_shell_webthemeengine.h"
 #elif defined(OS_MACOSX)
 #include "base/mac/mac_util.h"
-#elif defined(OS_POSIX) && !defined(OS_ANDROID)
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/linux/WebThemeEngine.h"
 #endif
 
 using WebKit::WebScriptController;
