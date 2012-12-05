602fc3b7d1af9f64def84b2a7f09e80096dc2214
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-05 20:49:21 +0000

    [Chromium] Move WorkerContextProxy::create out of PlatformSupport https://bugs.webkit.org/show_bug.cgi?id=104151
    Reviewed by Adam Barth.
    
    Doesn't really solve the layering violation as much as kick it
    down the road a bit. Part of a refactoring series; see tracking
    bug 82948.
    
    * WebKit.gyp:
    * src/PlatformSupport.cpp:
    (WebCore):
    * src/WorkerContextProxy.cpp: Added.
    (WebCore):
    (WebCore::WebCore::WorkerContextProxy::create):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WorkerContextProxy.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WorkerContextProxy.cpp
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2009 Google Inc. All rights reserved.
+ * Copyright (C) 2012 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -30,66 +30,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "PlatformSupport.h"
-
-#include "Chrome.h"
-#include "ChromeClientImpl.h"
-#include "FileMetadata.h"
-#include "Page.h"
-#include "WebFileInfo.h"
-#include "WebFileUtilities.h"
-#include "WebFrameClient.h"
-#include "WebFrameImpl.h"
-#include "WebKit.h"
-#include "WebPluginContainerImpl.h"
-#include "WebPluginListBuilderImpl.h"
-#include "WebSandboxSupport.h"
-#include "WebScreenInfo.h"
-#include "WebViewClient.h"
-#include "WebViewImpl.h"
-#include "platform/WebAudioBus.h"
-#include "platform/WebData.h"
-#include "platform/WebDragData.h"
-#include "platform/WebImage.h"
-#include "platform/WebKitPlatformSupport.h"
-#include "platform/WebSerializedScriptValue.h"
-#include "platform/WebString.h"
-#include "platform/WebURL.h"
-
-#include "FrameView.h"
-#include "IDBFactoryBackendProxy.h"
-#include "KURL.h"
-#include "NotImplemented.h"
-#include "PluginData.h"
-#include "SharedBuffer.h"
-
-#include "Worker.h"
 #include "WorkerContextProxy.h"
-#include <public/WebMimeRegistry.h>
-#include <public/WebVector.h>
-#include <wtf/Assertions.h>
+
+#include "WebWorkerClientImpl.h"
 
 // We are part of the WebKit implementation.
 using namespace WebKit;
 
 namespace WebCore {
 
-// Indexed Database -----------------------------------------------------------
-
-PassRefPtr<IDBFactoryBackendInterface> PlatformSupport::idbFactory()
+#if ENABLE(WORKERS)
+WebCore::WorkerContextProxy* WebCore::WorkerContextProxy::create(WebCore::Worker* worker)
 {
-    // There's no reason why we need to allocate a new proxy each time, but
-    // there's also no strong reason not to.
-    return IDBFactoryBackendProxy::create();
-}
-
-// Plugin ---------------------------------------------------------------------
-
-bool PlatformSupport::plugins(bool refresh, Vector<PluginInfo>* results)
-{
-    WebPluginListBuilderImpl builder(results);
-    webKitPlatformSupport()->getPluginList(refresh, &builder);
-    return true;  // FIXME: There is no need for this function to return a value.
+    return WebWorkerClientImpl::createWorkerContextProxy(worker);
 }
 
 } // namespace WebCore
+
+#endif
