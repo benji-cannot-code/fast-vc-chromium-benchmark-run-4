e6b5cd35448d930c4832db914d3d4747a8b306b0
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 19:47:24 +0000

    Encrypted Media: Support pp::Buffer_Dev reuse in PpbBufferAllocator.
    This CL reuses shared memory buffers at the plugin side. The renderer side shared memory reusing is done in r168632 and r169100.
    
    In CdmWrapper, we use pp::Buffer_Dev to pass large chunks of data back to the renderer via shared memory buffers. pp::Buffer_Dev wraps shared memory which is expensive to allocate. Allocating shared memory in the plugin process involves a sync IPC to the renderer which will then request the shared memory from the browser.
    
    To reduce the latency caused by this, in this CL we reuse pp::Buffer_Dev in PpbBufferAllocator. Each pp::Buffer_Dev is assigned a buffer ID. This buffer ID is passed in PPB calls to the renderer. When the renderer side finished using the buffer, it sends the buffer ID(s) back to the plugin in PPP calls so that PpbBufferAllocator can recycle it(them).
    
    Tests on Linux (Ubuntu 10.04, with Debug build) show that this change reduces the average time to allocate a PpbBuffer using PpbBufferAllocator::Allocate() from 1.467ms to 0.032 ms. The difference can be larger on slower platforms.
    
    BUG=162193
    TEST=content_browsertests passes; demo page works.
    
    Review URL: https://chromiumcodereview.appspot.com/11417124
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171285 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/content_decryptor_delegate.h
+++ webkit/plugins/ppapi/content_decryptor_delegate.h
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_PLUGINS_PPAPI_CONTENT_DECRYPTOR_DELEGATE_H_
 #define WEBKIT_PLUGINS_PPAPI_CONTENT_DECRYPTOR_DELEGATE_H_
 
+#include <queue>
 #include <string>
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
 #include "media/base/decryptor.h"
 #include "ppapi/c/private/pp_content_decryptor.h"
 #include "ppapi/c/private/ppp_content_decryptor_private.h"
@@ -112,6 +114,10 @@ class WEBKIT_PLUGINS_EXPORT ContentDecryptorDelegate {
                                const uint8* data, int size,
                                ::ppapi::ScopedPPResource* resource);
 
+  void FreeBuffer(uint32_t buffer_id);
+
+  void SetBufferToFreeInTrackingInfo(PP_DecryptTrackingInfo* tracking_info);
+
   const PP_Instance pp_instance_;
   const PPP_ContentDecryptor_Private* const plugin_decryption_interface_;
 
@@ -149,6 +155,11 @@ class WEBKIT_PLUGINS_EXPORT ContentDecryptorDelegate {
   int audio_input_resource_size_;
   ::ppapi::ScopedPPResource video_input_resource_;
   int video_input_resource_size_;
+
+  std::queue<uint32_t> free_buffers_;
+
+  base::WeakPtrFactory<ContentDecryptorDelegate> weak_ptr_factory_;
+  base::WeakPtr<ContentDecryptorDelegate> weak_this_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentDecryptorDelegate);
 };
