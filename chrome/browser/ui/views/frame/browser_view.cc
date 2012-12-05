benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 984a43975a711e60d65d976cbaf92d2bda4e8b87

Remove ui_controls usage from the BrowserView. This is in preparation for moving it to test code. Review URL: https://codereview.chromium.org/11348237
git-svn-id: svn://svn.chromium.org/chrome/trunk/src@169554 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/frame/browser_view.cc
@@ -100,6 +100,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/locale_settings.h"
 #include "grit/theme_resources.h"
 #include "grit/ui_resources.h"
+#include "grit/ui_strings.h"
 #include "grit/webkit_resources.h"
 #include "ui/base/accelerators/accelerator.h"
 #include "ui/base/accessibility/accessible_view_state.h"
@@ -111,7 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/safe_integer_conversions.h"
 #include "ui/gfx/sys_color_change_listener.h"
-#include "ui/ui_controls/ui_controls.h"
 #include "ui/views/controls/single_split_view.h"
 #include "ui/views/controls/webview/webview.h"
 #include "ui/views/focus/external_focus_tracker.h"
@@ -1475,24 +1475,63 @@ void BrowserView::Cut() {
   // Omnibox is focused, send a Ctrl+x key event to Chrome. Using RWH interface
   // rather than the fake key event for a WebContent is important since the fake
   // event might be consumed by the web content (crbug.com/137908).
-  if (!DoCutCopyPaste(&content::RenderWidgetHost::Cut)) {
-    ui_controls::SendKeyPress(GetNativeWindow(), ui::VKEY_X,
-                              true, false, false, false);
+  if (DoCutCopyPaste(&content::RenderWidgetHost::Cut))
+    return;
+
+  OmniboxView* omnibox_view = GetLocationBarView()->GetLocationEntry();
+  if (!omnibox_view->model()->has_focus())
+    return;
+
+#if defined(OS_WIN) && !defined(USE_AURA)
+  OmniboxViewWin* omnibox_win = GetOmniboxViewWin(omnibox_view);
+  if (omnibox_win) {
+    ::SendMessage(omnibox_win->GetNativeView(), WM_CUT, 0, 0);
+    return;
   }
+#endif
+
+  OmniboxViewViews* omnibox_views = GetOmniboxViewViews(omnibox_view);
+  omnibox_views->ExecuteCommandOnTextField(IDS_APP_CUT);
 }
 
 void BrowserView::Copy() {
-  if (!DoCutCopyPaste(&content::RenderWidgetHost::Copy)) {
-    ui_controls::SendKeyPress(GetNativeWindow(), ui::VKEY_C,
-                              true, false, false, false);
+  if (DoCutCopyPaste(&content::RenderWidgetHost::Copy))
+    return;
+
+  OmniboxView* omnibox_view = GetLocationBarView()->GetLocationEntry();
+  if (!omnibox_view->model()->has_focus())
+    return;
+
+#if defined(OS_WIN) && !defined(USE_AURA)
+  OmniboxViewWin* omnibox_win = GetOmniboxViewWin(omnibox_view);
+  if (omnibox_win) {
+    ::SendMessage(omnibox_win->GetNativeView(), WM_COPY, 0, 0);
+    return;
   }
+#endif
+
+  OmniboxViewViews* omnibox_views = GetOmniboxViewViews(omnibox_view);
+  omnibox_views->ExecuteCommandOnTextField(IDS_APP_COPY);
 }
 
 void BrowserView::Paste() {
-  if (!DoCutCopyPaste(&content::RenderWidgetHost::Paste)) {
-    ui_controls::SendKeyPress(GetNativeWindow(), ui::VKEY_V,
-                              true, false, false, false);
+  if (DoCutCopyPaste(&content::RenderWidgetHost::Paste))
+    return;
+
+  OmniboxView* omnibox_view = GetLocationBarView()->GetLocationEntry();
+  if (!omnibox_view->model()->has_focus())
+    return;
+
+#if defined(OS_WIN) && !defined(USE_AURA)
+  OmniboxViewWin* omnibox_win = GetOmniboxViewWin(omnibox_view);
+  if (omnibox_win) {
+    ::SendMessage(omnibox_win->GetNativeView(), WM_PASTE, 0, 0);
+    return;
   }
+#endif
+
+  OmniboxViewViews* omnibox_views = GetOmniboxViewViews(omnibox_view);
+  omnibox_views->ExecuteCommandOnTextField(IDS_APP_PASTE);
 }
 
 gfx::Rect BrowserView::GetInstantBounds() {

chrome/browser/ui/views/omnibox/omnibox_view_views.cc
@@ -418,6 +418,10 @@ bool OmniboxViewViews::IsLocationEntryFocusableInRootView() const {
   return textfield_->IsFocusable();
 }
 
+void OmniboxViewViews::ExecuteCommandOnTextField(int command_id) {
+  return textfield_->ExecuteCommand(command_id);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // OmniboxViewViews, views::View implementation:
 void OmniboxViewViews::Layout() {

chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -86,6 +86,9 @@ class OmniboxViewViews
   // the root view.
   bool IsLocationEntryFocusableInRootView() const;
 
+  // Executes the given command on the text field.
+  void ExecuteCommandOnTextField(int command_id);
+
   // Implements views::View
   virtual void Layout() OVERRIDE;
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;

ui/views/controls/textfield/native_textfield_views.cc
@@ -596,6 +596,10 @@ int NativeTextfieldViews::GetFontHeight() {
   return GetRenderText()->GetFont().GetHeight();
 }
 
+void NativeTextfieldViews::ExecuteTextCommand(int command_id) {
+  ExecuteCommand(command_id);
+}
+
 /////////////////////////////////////////////////////////////////
 // NativeTextfieldViews, ui::SimpleMenuModel::Delegate overrides:
 

ui/views/controls/textfield/native_textfield_views.h
@@ -128,6 +128,7 @@ class VIEWS_EXPORT NativeTextfieldViews : public TouchSelectionClientView,
   virtual void ApplyDefaultStyle() OVERRIDE;
   virtual void ClearEditHistory() OVERRIDE;
   virtual int GetFontHeight() OVERRIDE;
+  virtual void ExecuteTextCommand(int command_id) OVERRIDE;
 
   // ui::SimpleMenuModel::Delegate overrides
   virtual bool IsCommandIdChecked(int command_id) const OVERRIDE;

ui/views/controls/textfield/native_textfield_win.cc
@@ -422,6 +422,10 @@ int NativeTextfieldWin::GetFontHeight() {
   return textfield_->font().GetHeight();
 }
 
+void NativeTextfieldWin::ExecuteTextCommand(int command_id) {
+  ExecuteCommand(command_id);
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // NativeTextfieldWin, ui::SimpleMenuModel::Delegate implementation:
 

ui/views/controls/textfield/native_textfield_win.h
@@ -104,6 +104,7 @@ class NativeTextfieldWin
   virtual void ApplyDefaultStyle() OVERRIDE;
   virtual void ClearEditHistory() OVERRIDE;
   virtual int GetFontHeight() OVERRIDE;
+  virtual void ExecuteTextCommand(int command_id) OVERRIDE;
 
   // ui::SimpleMenuModel::Delegate:
   virtual bool IsCommandIdChecked(int command_id) const OVERRIDE;

ui/views/controls/textfield/native_textfield_wrapper.h
@@ -158,6 +158,10 @@ class VIEWS_EXPORT NativeTextfieldWrapper {
   // Get the height in pixels of the first font used in this textfield.
   virtual int GetFontHeight() = 0;
 
+  // Performs the action associated with the specified command id. Not called
+  // ExecuteCommand to avoid name clash.
+  virtual void ExecuteTextCommand(int command_id) = 0;
+
   // Creates an appropriate NativeTextfieldWrapper for the platform.
   static NativeTextfieldWrapper* CreateWrapper(Textfield* field);
 };

commit 09f68bd1895fc09f1134bf7fb2fa305b2d3e8574

Fix paste through the wrench menu not working in content area. This regressed in r169554.
BUG=164235
Review URL: https://codereview.chromium.org/11414316

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171155 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/views/frame/browser_view.cc
@@ -2755,16 +2755,21 @@ void BrowserView::ShowPasswordGenerationBubble(
 }
 
 bool BrowserView::DoCutCopyPaste(void (content::RenderWidgetHost::*method)()) {
-#if defined(USE_AURA)
   WebContents* contents = chrome::GetActiveWebContents(browser_.get());
-  if (contents && contents->GetContentNativeView() &&
-      contents->GetContentNativeView()->HasFocus()) {
+  if (!contents)
+    return false;
+  gfx::NativeView native_view = contents->GetContentNativeView();
+  if (!native_view)
+    return false;
+#if defined(USE_AURA)
+  if (native_view->HasFocus()) {
+#elif defined(OS_WIN)
+  if (native_view == ::GetFocus()) {
+#endif
     (contents->GetRenderViewHost()->*method)();
     return true;
   }
-#elif defined(OS_WIN)
-  // TODO(yusukes): Support non-Aura Windows.
-#endif
+
   return false;
 }
 