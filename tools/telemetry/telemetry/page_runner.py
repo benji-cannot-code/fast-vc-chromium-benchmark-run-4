1242e3768320189195909f92ecf64165bbac938d
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-05 00:06:16 +0000

    [Telemetry] Fix a possible source of flakiness on Android.
    With this, we survive killing the adb process which produces the same stack as:
    http://build.chromium.org/p/chromium.perf/builders/Android%20GN/builds/764/steps/jsgamebench/logs/stdio
    
    BUG=163661
    TEST='killall -9 adb' while running a benchmark
    
    Review URL: https://codereview.chromium.org/11348370
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171088 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page_runner.py
+++ tools/telemetry/telemetry/page_runner.py
@@ -9,6 +9,7 @@ import traceback
 import urlparse
 import random
 
+from telemetry import browser_gone_exception
 from telemetry import page_set_url_builder
 from telemetry import page_test
 from telemetry import tab_crash_exception
@@ -112,23 +113,34 @@ http://goto/read-src-internal, or create a new archive using --record.
     state = _RunState()
     try:
       for page in pages:
-        if not state.browser:
-          self._SetupBrowser(state, test, possible_browser, credentials_path,
-                             archive_path)
-        if not state.tab:
-          state.tab = state.browser.ConnectToNthTab(0)
-        if options.trace_dir:
-          self._SetupTracingTab(state)
+        tries = 3
+        while tries:
+          try:
+            if not state.browser:
+              self._SetupBrowser(state, test, possible_browser,
+                                 credentials_path, archive_path)
+            if not state.tab:
+              state.tab = state.browser.ConnectToNthTab(0)
+            if options.trace_dir:
+              self._SetupTracingTab(state)
 
-        try:
-          self._RunPage(options, page, state.tab, test, results)
-        except tab_crash_exception.TabCrashException:
-          # If we don't support tab control, just restart the browser.
-          # TODO(dtu): Create a new tab: crbug.com/155077, crbug.com/159852
-          state.Close()
+            try:
+              self._RunPage(options, page, state.tab, test, results)
+            except tab_crash_exception.TabCrashException:
+              # If we don't support tab control, just restart the browser.
+              # TODO(dtu): Create a new tab: crbug.com/155077, crbug.com/159852
+              state.Close()
 
-        if options.trace_dir and state.trace_tab:
-          self._EndTracing(state, options, page)
+            if options.trace_dir and state.trace_tab:
+              self._EndTracing(state, options, page)
+            break
+          except browser_gone_exception.BrowserGoneException:
+            logging.warning('Lost connection to browser. Retrying.')
+            state.Close()
+            tries -= 1
+            if not tries:
+              logging.error('Lost connection to browser 3 times. Failing.')
+              raise
     finally:
       state.Close()
 
@@ -151,6 +163,8 @@ http://goto/read-src-internal, or create a new archive using --record.
       logging.warning('Tab crashed: %s', page.url)
       results.AddFailure(page, ex, traceback.format_exc())
       raise
+    except browser_gone_exception.BrowserGoneException:
+      raise browser_gone_exception.BrowserGoneException()
     except Exception, ex:
       logging.error('Unexpected failure while running %s: %s',
                     page.url, traceback.format_exc())
@@ -175,6 +189,8 @@ http://goto/read-src-internal, or create a new archive using --record.
       logging.warning('Tab crashed: %s', page.url)
       results.AddFailure(page, ex, traceback.format_exc())
       raise
+    except browser_gone_exception.BrowserGoneException:
+      raise browser_gone_exception.BrowserGoneException()
     except Exception, ex:
       logging.error('Unexpected failure while running %s: %s',
                     page.url, traceback.format_exc())
@@ -295,5 +311,8 @@ http://goto/read-src-internal, or create a new archive using --record.
   def _CleanUpPage(self, page, tab, page_state): # pylint: disable=R0201
     if page.credentials and page_state.did_login:
       tab.browser.credentials.LoginNoLongerNeeded(tab, page.credentials)
-    tab.runtime.Evaluate("""window.chrome && chrome.benchmarking &&
-                            chrome.benchmarking.closeConnections()""")
+    try:
+      tab.runtime.Evaluate("""window.chrome && chrome.benchmarking &&
+                              chrome.benchmarking.closeConnections()""")
+    except Exception:
+      pass
