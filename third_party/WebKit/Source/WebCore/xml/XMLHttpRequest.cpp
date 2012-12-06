359ba6a156f286cec2a895afadeabde0f947455b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-06 22:32:46 +0000

    XMLHttpRequest Content-Type should be taken from Blob type https://bugs.webkit.org/show_bug.cgi?id=99983
    Patch by Alexander Shalamov <alexander.shalamov@intel.com> on 2012-12-06
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    Fix XMLHttpRequest::send(Blob*) method, so that the Content-Type is set according to W3C specification.
    http://www.w3.org/TR/XMLHttpRequest/#the-send-method
    
    Added test that check if content type is set correctly when blob object is sent.
    
    Test: http/tests/xmlhttprequest/post-blob-content-type.html
    
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::send):
        Set correct MIME type for Blob objects.
    * WebCore.vcproj/WebCore.vcproj:
        Added ParsedContentType to project file.
    
    LayoutTests:
    
    Added tests that check if content type is set correctly when blob object is sent.
    
    * http/tests/xmlhttprequest/post-blob-content-type-expected.txt: Added.
    * http/tests/xmlhttprequest/post-blob-content-type.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136893 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/WebCore/xml/XMLHttpRequest.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HistogramSupport.h"
 #include "InspectorInstrumentation.h"
 #include "MemoryCache.h"
+#include "ParsedContentType.h"
 #include "ResourceError.h"
 #include "ResourceRequest.h"
 #include "ScriptCallStack.h"
@@ -644,7 +645,17 @@ void XMLHttpRequest::send(Blob* body, ExceptionCode& ec)
         return;
 
     if (m_method != "GET" && m_method != "HEAD" && m_url.protocolIsInHTTPFamily()) {
-        // FIXME: Should we set a Content-Type if one is not set.
+        const String& contentType = getRequestHeader("Content-Type");
+        if (contentType.isEmpty()) {
+            const String& blobType = body->type();
+            if (!blobType.isEmpty() && isValidContentType(blobType))
+                setRequestHeaderInternal("Content-Type", blobType);
+            else {
+                // From FileAPI spec, whenever media type cannot be determined, empty string must be returned.
+                setRequestHeaderInternal("Content-Type", "");
+            }
+        }
+
         // FIXME: add support for uploading bundles.
         m_requestEntityBody = FormData::create();
         if (body->isFile())
