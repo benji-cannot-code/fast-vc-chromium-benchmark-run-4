ade2d6408ac128656e6a89efaf8d6924eb1dd644
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 06:23:06 +0000

    cros: Account picker UI for public account.
    BUG=152918
    TEST=Verify public accounts show up in account picker UI.
    
    Review URL: https://chromiumcodereview.appspot.com/11308081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h
@@ -211,6 +211,7 @@ class SigninScreenHandler
   void HandleAuthenticateUser(const base::ListValue* args);
   void HandleLaunchDemoUser(const base::ListValue* args);
   void HandleLaunchIncognito(const base::ListValue* args);
+  void HandleLaunchPublicAccount(const base::ListValue* args);
   void HandleOfflineLogin(const base::ListValue* args);
   void HandleShutdownSystem(const base::ListValue* args);
   void HandleLoadWallpaper(const base::ListValue* args);
