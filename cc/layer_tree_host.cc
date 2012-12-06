2c70403f0b318dbae7fabfea6938f571416a2610
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 06:19:22 +0000

    cc: Initialize boolean with false, not 0.
    TBR=jamesr
    
    Review URL: https://codereview.chromium.org/11453029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171416 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host.cc
+++ cc/layer_tree_host.cc
@@ -147,7 +147,7 @@ LayerTreeSettings::~LayerTreeSettings()
 
 RendererCapabilities::RendererCapabilities()
     : bestTextureFormat(0)
-    , usingPartialSwap(0)
+    , usingPartialSwap(false)
     , usingAcceleratedPainting(false)
     , usingSetVisibility(false)
     , usingSwapCompleteCallback(false)
