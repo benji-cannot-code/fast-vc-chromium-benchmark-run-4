bcd2021671da1e6cab4d58573646b57c58763ffb
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 23:04:03 +0000

    Change www.crbug.com (fails) links to crbug.com links (works).
    BUG=none
    TEST=working links
    TBR=ben@chromium.org
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11437043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171625 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/client_socket_pool_base.cc
+++ net/socket/client_socket_pool_base.cc
@@ -575,7 +575,7 @@ LoadState ClientSocketPoolBaseHelper::GetLoadState(
         return max_state;
       } else {
         // TODO(wtc): Add a state for being on the wait list.
-        // See http://www.crbug.com/5077.
+        // See http://crbug.com/5077.
         return LOAD_STATE_IDLE;
       }
     }
