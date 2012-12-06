033f7e2f851b52e11da1814e350009d1ebb806d5
bfulgham@webkit.org <bfulgham@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-06 03:44:08 +0000

    [Windows, WinCairo] Regenerate .def files on changes https://bugs.webkit.org/show_bug.cgi?id=104136
    Reviewed by Tim Horton.
    
    The new .def file generator is not regenerating the link
    export definitions once it creates the definition file
    the first time. You must clean the build directory for
    new symbols to be added (or removed).
    
    * win/WebKit2.vcproj: Change the WebKit2.def file reference to
      point to the generated version of the file.
    * win/WebKit2ExportGenerator.vcproj: Add WebKit2.def.in as a source
      file that participates in the build so that Visual Studio knows
      to build the project when the file changes.
    * win/WebKit2ExportGeneratorBuildCmd.cmd: Added. Refactor build
      command into its own shell script.
    * win/WebKit2ExportGeneratorPostBuild.cmd: Call new BuildCmd script.
    * win/WebKit2ExportGeneratorPreBuild.cmd: Delete the old generator
      and definition files to ensure a clean build.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136792 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/win/WebKit2ExportGeneratorPostBuild.cmd
+++ third_party/WebKit/Source/WebKit2/win/WebKit2ExportGeneratorPostBuild.cmd
@@ -1,5 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set GeneratorDirectory=%CONFIGURATIONBUILDDIR%/obj/WebKit2ExportGenerator
-if exist "%OUTDIR%\..\bin\WebKit2ExportGenerator.exe" "%OUTDIR%\..\bin\WebKit2ExportGenerator.exe" > "%GeneratorDirectory%\WebKit2.def"
+echo Exporting link definition output (%GeneratorDirectory%\WebKit2.def)
+if exist "%OUTDIR%\WebKit2ExportGenerator.exe" "%OUTDIR%\WebKit2ExportGenerator.exe" > "%GeneratorDirectory%\WebKit2.def"
 
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
