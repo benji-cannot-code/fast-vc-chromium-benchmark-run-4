7eabb0d68bf075c4641f326bfb5d25c8aca860af
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 23:05:25 +0000

    [Telemetry] Fix testCalcResultsRealRenderStats and testCalcResultsFromRAFRenderStats.
    WillMeasurePage takes a Page not a bool.
    
    BUG=164688
    TEST=tools/perf/run_tests
    
    Review URL: https://codereview.chromium.org/11478007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171626 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/smoothness_benchmark_unittest.py
+++ tools/perf/perf_tools/smoothness_benchmark_unittest.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 from telemetry import multi_page_benchmark
 from telemetry import multi_page_benchmark_unittest_base
+from telemetry import page
 from perf_tools import smoothness_benchmark
 
 from telemetry import browser_finder
@@ -49,7 +50,7 @@ class SmoothnessBenchmarkUnitTest(
                        'numAnimationFrames': 10,
                        'numFramesSentToScreen': 10}
     res = multi_page_benchmark.BenchmarkResults()
-    res.WillMeasurePage(True)
+    res.WillMeasurePage(page.Page('http://foo.com/'))
     smoothness_benchmark.CalcScrollResults(rendering_stats, res)
     res.DidMeasurePage()
     self.assertEquals(50, res.page_results[0]['dropped_percent'])
@@ -69,7 +70,7 @@ class SmoothnessBenchmarkUnitTest(
                        'totalRasterizeTimeInSeconds': 0,
                        'totalTimeInSeconds': 1.0}
     res = multi_page_benchmark.BenchmarkResults()
-    res.WillMeasurePage(True)
+    res.WillMeasurePage(page.Page('http://foo.com/'))
     smoothness_benchmark.CalcScrollResults(rendering_stats, res)
     res.DidMeasurePage()
     self.assertEquals(0, res.page_results[0]['dropped_percent'])
