6ab22ce6d451b6d58bb6d8be7ebc95085a7b2120
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-06 20:11:42 +0000

    IndexedDB: Remove IDBDatabaseException.idl https://bugs.webkit.org/show_bug.cgi?id=102961
    Reviewed by Adam Barth.
    
    Delete the IDL and references to it. No longer needed as a enum member
    in the autogenerated ExceptionCodeDescription.h so removed from the ".in"
    file; only direct references are retained in the autogenerated cpp file.
    
    Ideally the code generator would handle these new-style DOMExceptions,
    but we don't have any other examples yet to know what pattern to follow.
    
    No new tests - just removing dead code.
    
    * CMakeLists.txt:
    * DerivedSources.cpp:
    * DerivedSources.make:
    * DerivedSources.pri:
    * GNUmakefile.list.am:
    * Modules/indexeddb/IDBDatabaseException.idl: Removed.
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.xcodeproj/project.pbxproj:
    * dom/DOMExceptions.in:
    * dom/make_dom_exceptions.pl:
    (generateImplementation):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/DerivedSources.cpp
+++ third_party/WebKit/Source/WebCore/DerivedSources.cpp
@@ -220,7 +220,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSIDBAny.cpp"
 #include "JSIDBCursor.cpp"
 #include "JSIDBDatabase.cpp"
-#include "JSIDBDatabaseException.cpp"
 #include "JSIDBFactory.cpp"
 #include "JSIDBIndex.cpp"
 #include "JSIDBKey.cpp"
