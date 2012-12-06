c74d8be5f52bdc6cd2313e2ec07741d574ede63c
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-06 06:16:37 +0000

    Source/WebCore: INPUT_MULTIPLE_FIELDS_UI: The hour, am/pm, millisecond fields should support read-only state correctly https://bugs.webkit.org/show_bug.cgi?id=104210
    Reviewed by Kentaro Hara.
    
    1. The hour, am/pm, and millisecond fields should have CSS rules for
       [readonly]. They should be gray when they are read-only.
    2. The am/pm field should reject to set an empty value like other
       numeric fields do.
    
    Tests: Add test cases to fast/forms/time/time-appearance-basic.html.
    
    * css/html.css: merge existing readonly rules, and add ampm, hour, and
    millisecond pseudo classes.
    * html/shadow/DateTimeSymbolicFieldElement.cpp:
    (WebCore::DateTimeSymbolicFieldElement::setEmptyValue):
    Reject to set empty value if it is read-only.
    
    LayoutTests: INPUT_MULTIPLE_FIELDS_UI: The hour field and the am/pm field should support read-only state correctly
    https://bugs.webkit.org/show_bug.cgi?id=104210
    
    Reviewed by Kentaro Hara.
    
    * fast/forms/time/time-appearance-basic.html:
    Add test cases for step=86400 (24 hours)
    * platform/chromium-mac/fast/forms/time/time-appearance-basic-expected.png:
    * platform/chromium/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/DateTimeSymbolicFieldElement.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/DateTimeSymbolicFieldElement.cpp
@@ -108,6 +108,8 @@ int DateTimeSymbolicFieldElement::minimum() const
 
 void DateTimeSymbolicFieldElement::setEmptyValue(EventBehavior eventBehavior)
 {
+    if (isReadOnly())
+        return;
     m_selectedIndex = invalidIndex;
     updateVisibleValue(eventBehavior);
 }
