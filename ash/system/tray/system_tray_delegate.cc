9a8121957746b1a7b94a4e876b32148e8a1dfb9b
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 06:24:22 +0000

    chromeos: Use Widget::NotifyAccessibilityEvent to speak network change
    Stop using accessibility::Speak() in favor of Widget::NotifyAccessibilityEvent() which gives ChromeVox control of speech timing.
    Network speech code is moved from ash_system_tray_delegate.cc to tray_network.cc because NotifyAccessibilityEvent requires a pointer to views::Widget.
    ash::NetworkIconInfo::highlight is replaced with connecting/connected.
    
    BUG=151495
    TEST=Press Ctrl+Alt+Z to enable spoken feedback, connect/disconnect to ethernet/wifi, ensure it speaks "connected to <network name>"
    
    Review URL: https://chromiumcodereview.appspot.com/11414289
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171421 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray_delegate.cc
+++ ash/system/tray/system_tray_delegate.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ash {
 
 NetworkIconInfo::NetworkIconInfo()
-    : highlight(false),
+    : connecting(false),
+      connected(false),
       tray_icon_visible(true) {
 }
 
