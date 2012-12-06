8b7ed197008ce9b11caea0aabb01754d905c8314
nfullagar@chromium.org <nfullagar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-06 06:56:02 +0000

    Explicitly support FileIO interface 1.0, to restore functioning SRPC proxy for Mini-Ninjas and other clients. This CL isn't enough to restore PPAPI tests on the bots for the SRPC proxy, for that we'll need to either implement missing new interfaces for SRPC proxy or disable the tests that exercise the new interfaces that are currently not proxied in SRPC.
    BUG=http://code.google.com/p/chromium/issues/detail?id=164009
    TEST=Mini-Ninjas, From Dust
    
    Review URL: https://chromiumcodereview.appspot.com/11437031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171436 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/native_client/src/shared/ppapi_proxy/plugin_ppb_file_io.cc
+++ ppapi/native_client/src/shared/ppapi_proxy/plugin_ppb_file_io.cc
@@ -285,8 +285,8 @@ void Close(PP_Resource file_io) {
 
 }  // namespace
 
-const PPB_FileIO* PluginFileIO::GetInterface() {
-  static const PPB_FileIO file_io_interface = {
+const PPB_FileIO_1_0* PluginFileIO::GetInterface1_0() {
+  static const PPB_FileIO_1_0 file_io_interface = {
     Create,
     IsFileIO,
     Open,
@@ -296,8 +296,7 @@ const PPB_FileIO* PluginFileIO::GetInterface() {
     Write,
     SetLength,
     Flush,
-    Close,
-    NULL  // TODO: support ReadToArray
+    Close
   };
   return &file_io_interface;
 }
