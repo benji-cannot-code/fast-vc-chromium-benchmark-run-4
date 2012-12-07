7344ea0ac32e27c510f5ac7734b13efcd8111230
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 20:57:24 +0000

    Move gpu related stuff from chrome/browser to chrome/browser/gpu
    OWNERS file is copied from content/browser/gpu minus jbates
    
    More gpu related stuff will be added to chrome/browser.  Having a folder makes future reviews much easier.
    
    BUG=
    TEST=bots
    Review URL: https://codereview.chromium.org/11474028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171834 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/extensions/webstore_installer.h"
+#include "chrome/browser/gpu/gpu_feature_checker.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/signin/token_service.h"
--- chrome/browser/extensions/api/webstore_private/webstore_private_api.h
+++ chrome/browser/extensions/api/webstore_private/webstore_private_api.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_install_prompt.h"
 #include "chrome/browser/extensions/webstore_install_helper.h"
 #include "chrome/browser/extensions/webstore_installer.h"
-#include "chrome/browser/gpu_feature_checker.h"
 #include "content/public/browser/gpu_data_manager_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -24,6 +23,8 @@ class ProfileSyncService;
 namespace content {
 class GpuDataManager;
 }
+
+class GPUFeatureChecker;
 
 namespace extensions {
 
