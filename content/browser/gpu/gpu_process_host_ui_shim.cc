20472f9f5413447a06ec5b09405eb02b86e1d9b6
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 01:52:44 +0000

    Exit the unsandboxed GPU process after GPUInfo collected.
    BUG=164712
    TEST=on windows, open about:gpu, see the second gpu process in task manager, then once the full info is displayed, the the second gpu process disappear.
    Review URL: https://codereview.chromium.org/11466012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171686 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host_ui_shim.cc
+++ content/browser/gpu/gpu_process_host_ui_shim.cc
@@ -179,7 +179,7 @@ GpuProcessHostUIShim::~GpuProcessHostUIShim() {
 
   GpuDataManagerImpl::GetInstance()->AddLogMessage(
       logging::LOG_ERROR, "GpuProcessHostUIShim",
-      "GPU Process Crashed.");
+      "GPU process crashed or exited.");
 }
 
 bool GpuProcessHostUIShim::OnControlMessageReceived(
