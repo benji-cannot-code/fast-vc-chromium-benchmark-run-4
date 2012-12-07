28ddc44cc59f773b92fb64df724cb217fb0c057c
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 00:06:04 +0000

    Make BrowserFont take an InstanceHandle.
    We missed this when we converted Instance* -> InstanceHandle. Nobody uses this yet.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11442034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171646 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/trusted/browser_font_trusted.cc
+++ ppapi/cpp/trusted/browser_font_trusted.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/c/dev/ppb_font_dev.h"
 #include "ppapi/cpp/image_data.h"
-#include "ppapi/cpp/instance.h"
+#include "ppapi/cpp/instance_handle.h"
 #include "ppapi/cpp/point.h"
 #include "ppapi/cpp/rect.h"
 #include "ppapi/cpp/module_impl.h"
@@ -132,15 +132,15 @@ BrowserFont_Trusted::BrowserFont_Trusted(PP_Resource resource)
 }
 
 BrowserFont_Trusted::BrowserFont_Trusted(
-    Instance* instance,
+    const InstanceHandle& instance,
     const BrowserFontDescription& description) {
   if (has_interface<PPB_BrowserFont_Trusted_1_0>()) {
     PassRefFromConstructor(get_interface<PPB_BrowserFont_Trusted_1_0>()->Create(
-        instance->pp_instance(),
+        instance.pp_instance(),
         &description.pp_font_description()));
   } else if (!has_interface<PPB_Font_Dev_0_6>()) {
     PassRefFromConstructor(get_interface<PPB_Font_Dev_0_6>()->Create(
-        instance->pp_instance(),
+        instance.pp_instance(),
         BrowserFontDescToFontDesc(&description.pp_font_description())));
   }
 }
@@ -156,12 +156,12 @@ BrowserFont_Trusted& BrowserFont_Trusted::operator=(
 }
 
 // static
-Var BrowserFont_Trusted::GetFontFamilies(Instance* instance) {
+Var BrowserFont_Trusted::GetFontFamilies(const InstanceHandle& instance) {
   if (!has_interface<PPB_Font_Dev_0_6>())
     return Var();
   return Var(PASS_REF,
              get_interface<PPB_Font_Dev_0_6>()->GetFontFamilies(
-                 instance->pp_instance()));
+                 instance.pp_instance()));
 }
 
 bool BrowserFont_Trusted::Describe(
