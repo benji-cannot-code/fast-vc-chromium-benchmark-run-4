e2eaa98a7788c25fc23e5b6d6395dbd996f34292
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 23:33:43 +0000

    Remove PPB_Flash_File_ModuleLocal_2_0.
    PPB_Flash_File_ModuleLocal_2_0 was replaced in Flash by version 3_0 in M21 and can be removed.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/11472036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171879 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_flash_file.cc
+++ ppapi/tests/test_flash_file.cc
@@ -95,8 +95,6 @@ void TestFlashFile::RunTests(const std::string& filter) {
 }
 
 std::string TestFlashFile::TestCreateTemporaryFile() {
-  ASSERT_TRUE(FileModuleLocal::IsCreateTemporaryFileAvailable());
-
   // Make sure that the root directory exists.
   FileModuleLocal::CreateDir(instance_, std::string());
 
