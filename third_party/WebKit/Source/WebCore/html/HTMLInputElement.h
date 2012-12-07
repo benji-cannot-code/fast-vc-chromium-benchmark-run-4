5810db17798a484810e6fbff2d38c0f741987d57
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-07 07:46:58 +0000

    Refactoring: Clean up placeholder attribute usage https://bugs.webkit.org/show_bug.cgi?id=104337
    Reviewed by Kentaro Hara.
    
    Source/WebCore:
    
    No new tests. This doesn't change any behavior.
    
    * html/HTMLInputElement.cpp: Remove placeholder and setPlacehodler.
    * html/HTMLInputElement.h: Ditto.
    * html/HTMLTextFormControlElement.cpp:
    (WebCore::HTMLTextFormControlElement::strippedPlaceholder):
    We can use fastGetAttribute for placeholder.
    (WebCore::HTMLTextFormControlElement::isPlaceholderEmpty):
    Ditto.
    
    Source/WebKit/chromium:
    
    * src/WebInputElement.cpp:
    (WebKit::WebInputElement::setPlaceholder):
    Use setAttribute.
    (WebKit::WebInputElement::placeholder):
    Use fastGetAttribute.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLInputElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLInputElement.h
@@ -177,9 +177,6 @@ public:
     double valueAsNumber() const;
     void setValueAsNumber(double, ExceptionCode&, TextFieldEventBehavior = DispatchNoEvent);
 
-    virtual String placeholder() const;
-    virtual void setPlaceholder(const String&);
-
     String valueWithDefault() const;
 
     void setValueFromRenderer(const String&);
