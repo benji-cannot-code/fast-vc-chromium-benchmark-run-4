d9dc48d01c3ca3c149eeb76474df74c22d7ca0e7
perkj@chromium.org <perkj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 14:57:02 +0000

    Actually Disable ExtensionBrowserTest.WebKitPrefsBackgroundPage to get Win7 to buildbots to pass. (Was originally submitted with a stupid mistake in https://codereview.chromium.org/11471029/)
    The reason is that it fails on a Windows 7 build bot and cause LKGR to not update. 
    http://build.chromium.org/p/chromium.win/builders/Win7%20Tests%20%28dbg%29%282%29?numbuilds=25 
    
    
    The failure started to happen around Dec 06 08:16 am (bot time) but I have not been able to locate a candidate cl that caused this. 
    
    
    BUG=164835
    TBR=yoz@chromium.org 
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=171770
    Review URL: https://codereview.chromium.org/11476023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171777 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/gpu_browsertest.cc
+++ chrome/browser/extensions/gpu_browsertest.cc
@@ -23,7 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #else
 #define MAYBE_WebKitPrefsBackgroundPage WebKitPrefsBackgroundPage
 #endif
-IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, WebKitPrefsBackgroundPage) {
+IN_PROC_BROWSER_TEST_F(ExtensionBrowserTest, MAYBE_WebKitPrefsBackgroundPage) {
   ASSERT_TRUE(LoadExtension(
       test_data_dir_.AppendASCII("good").AppendASCII("Extensions")
                     .AppendASCII("behllobkkfkfnphdnhnkndlbkcpglgmj")
