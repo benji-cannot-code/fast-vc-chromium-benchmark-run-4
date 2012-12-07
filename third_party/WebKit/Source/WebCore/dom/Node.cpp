11b7217856d03d5e07f31656d1d915673960ca76
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-07 03:03:26 +0000

    Event's relatedTarget re-targeting does not occur for manually fired mouse events created by event.initMouseEvent(). https://bugs.webkit.org/show_bug.cgi?id=102681
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Make sure that event's relatedTarget re-targeting occurs for mouse
    events created by event.initMouseEvent().  Since user-generated
    mouse events can have a relatedTarget which is same to the target
    node, the algorithm which calculates event's ancestors is also
    updated so that ancestors are not shrunk wrongly.
    
    Test: fast/events/dispatch-synthetic-mouseevent.html
          fast/dom/shadow/shadow-dom-event-dispatching.html
    
    * dom/EventDispatcher.cpp:
    (WebCore::EventRelatedTargetAdjuster::adjust):
    * dom/MouseEvent.cpp:
    (WebCore::MouseEventDispatchMediator::create):
    (WebCore::MouseEventDispatchMediator::MouseEventDispatchMediator):
    (WebCore::MouseEventDispatchMediator::dispatchEvent):
    * dom/MouseEvent.h:
    (WebCore::MouseEventDispatchMediator::isSyntheticMouseEvent):
    (MouseEventDispatchMediator):
    * dom/Node.cpp:
    (WebCore::Node::dispatchEvent):
    
    LayoutTests:
    
    * fast/dom/shadow/shadow-dom-event-dispatching-expected.txt:
    * fast/dom/shadow/shadow-dom-event-dispatching.html:
    * fast/events/dispatch-synthetic-mouseevent-expected.txt: Added.
    * fast/events/dispatch-synthetic-mouseevent.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@136918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.cpp
+++ third_party/WebKit/Source/WebCore/dom/Node.cpp
@@ -2482,6 +2482,8 @@ void Node::dispatchScopedEventDispatchMediator(PassRefPtr<EventDispatchMediator>
 
 bool Node::dispatchEvent(PassRefPtr<Event> event)
 {
+    if (event->isMouseEvent())
+        return EventDispatcher::dispatchEvent(this, MouseEventDispatchMediator::create(adoptRef(toMouseEvent(event.leakRef())), MouseEventDispatchMediator::SyntheticMouseEvent));
     return EventDispatcher::dispatchEvent(this, EventDispatchMediator::create(event));
 }
 
