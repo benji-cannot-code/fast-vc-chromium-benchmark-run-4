93471b57728dd87f245b24232fd71ce96eb3e5c2
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 07:40:31 +0000

    Implement DriveFileSyncService::ResolveToLocalChange
    BUG=164335
    TEST=n/a
    
    Review URL: https://codereview.chromium.org/11441033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171716 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_file_sync_service_unittest.cc
+++ chrome/browser/sync_file_system/drive_file_sync_service_unittest.cc
@@ -289,6 +289,10 @@ class DriveFileSyncServiceTest : public testing::Test {
   DISALLOW_COPY_AND_ASSIGN(DriveFileSyncServiceTest);
 };
 
+ACTION(InvokeCompletionCallback) {
+  base::MessageLoopProxy::current()->PostTask(FROM_HERE, arg1);
+}
+
 // Invokes |arg0| as a GetDataCallback.
 ACTION_P2(InvokeGetDataCallback0, error, result) {
   scoped_ptr<base::Value> value(result.Pass());
@@ -760,6 +764,9 @@ TEST_F(DriveFileSyncServiceTest, RemoteChange_Busy) {
   EXPECT_CALL(*mock_remote_processor(),
               PrepareForProcessRemoteChange(CreateURL(kOrigin, kFileName), _))
       .WillOnce(PrepareForRemoteChange_Busy());
+  EXPECT_CALL(*mock_remote_processor(),
+              ClearLocalChanges(CreateURL(kOrigin, kFileName), _))
+      .WillOnce(InvokeCompletionCallback());
 
   SetUpDriveSyncService();
 
@@ -792,6 +799,9 @@ TEST_F(DriveFileSyncServiceTest, RemoteChange_NewFile) {
   EXPECT_CALL(*mock_remote_processor(),
               PrepareForProcessRemoteChange(CreateURL(kOrigin, kFileName), _))
       .WillOnce(PrepareForRemoteChange_NotFound());
+  EXPECT_CALL(*mock_remote_processor(),
+              ClearLocalChanges(CreateURL(kOrigin, kFileName), _))
+      .WillOnce(InvokeCompletionCallback());
 
   scoped_ptr<Value> file_entry(LoadJSONFile("gdata/file_entry.json").Pass());
   EXPECT_CALL(*mock_drive_service(),
@@ -838,6 +848,9 @@ TEST_F(DriveFileSyncServiceTest, RemoteChange_UpdateFile) {
   EXPECT_CALL(*mock_remote_processor(),
               PrepareForProcessRemoteChange(CreateURL(kOrigin, kFileName), _))
       .WillOnce(PrepareForRemoteChange_NotModified());
+  EXPECT_CALL(*mock_remote_processor(),
+              ClearLocalChanges(CreateURL(kOrigin, kFileName), _))
+      .WillOnce(InvokeCompletionCallback());
 
   scoped_ptr<Value> file_entry(LoadJSONFile("gdata/file_entry.json").Pass());
   EXPECT_CALL(*mock_drive_service(),
