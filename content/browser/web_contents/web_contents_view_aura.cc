fe03092a3d765e7ebd6304ea6f51d6f60020527a
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 06:03:20 +0000

    This is a revert of 171414 with a different workaround to get the view in WebContentsViewAura::WindowObserver::SendScreenRects. I'll make a larger change to get rid of RenderviewHostView caching in WebContentsView implementations later.
    BUG=162981
    Review URL: https://codereview.chromium.org/11476010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171699 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_view_aura.cc
+++ content/browser/web_contents/web_contents_view_aura.cc
@@ -308,9 +308,7 @@ class WebContentsViewAura::WindowObserver
 
  private:
   void SendScreenRects() {
-    if (!view_->view_)
-      return;
-    RenderWidgetHostImpl::From(view_->view_->GetRenderWidgetHost())->
+    RenderWidgetHostImpl::From(view_->web_contents_->GetRenderViewHost())->
         SendScreenRects();
   }
 
@@ -515,9 +513,7 @@ void WebContentsViewAura::CreateView(const gfx::Size& initial_size) {
   window_->layer()->SetMasksToBounds(true);
   window_->SetName("WebContentsViewAura");
 
-#if !defined(OS_CHROMEOS)  // http://crbug.com/162981
   window_observer_.reset(new WindowObserver(this));
-#endif
 
   // delegate_->GetDragDestDelegate() creates a new delegate on every call.
   // Hence, we save a reference to it locally. Similar model is used on other
