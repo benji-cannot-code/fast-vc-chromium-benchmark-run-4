76d44c5c31149f3bf02c4d84442afc86becf88fc
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 20:24:33 +0000

    Put local accounts device setting behind --enable-local-accounts
    BUG=chromium:152917
    TEST=Public accounts ignored if --enable-local-accounts is not present
    
    
    Review URL: https://chromiumcodereview.appspot.com/11443037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings/device_settings_provider.cc
+++ chrome/browser/chromeos/settings/device_settings_provider.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/logging.h"
 #include "base/string_util.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud_policy_constants.h"
 #include "chrome/browser/policy/proto/device_management_backend.pb.h"
 #include "chrome/browser/ui/options/options_util.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/installer/util/google_update_settings.h"
 
 using google::protobuf::RepeatedPtrField;
@@ -381,12 +383,15 @@ void DeviceSettingsProvider::DecodeLoginPolicies(
   new_values_cache->SetValue(kAccountsPrefUsers, list);
 
   base::ListValue* account_list = new base::ListValue();
-  const RepeatedPtrField<em::DeviceLocalAccountInfoProto>& accounts =
-      policy.device_local_accounts().account();
-  RepeatedPtrField<em::DeviceLocalAccountInfoProto>::const_iterator entry;
-  for (entry = accounts.begin(); entry != accounts.end(); ++entry) {
-    if (entry->has_id())
-      account_list->AppendString(entry->id());
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kEnableLocalAccounts)) {
+    const RepeatedPtrField<em::DeviceLocalAccountInfoProto>& accounts =
+        policy.device_local_accounts().account();
+    RepeatedPtrField<em::DeviceLocalAccountInfoProto>::const_iterator entry;
+    for (entry = accounts.begin(); entry != accounts.end(); ++entry) {
+      if (entry->has_id())
+        account_list->AppendString(entry->id());
+    }
   }
   new_values_cache->SetValue(kAccountsPrefDeviceLocalAccounts, account_list);
 }
