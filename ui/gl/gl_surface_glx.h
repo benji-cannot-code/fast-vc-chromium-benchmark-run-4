f8cce940027e8b4d148abc5a921bdecadc87ec2f
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 01:36:45 +0000

    Explicitly create the GLX window for onscreen surfaces.
    Always use glXCreateContextAttribsARB to create GLX contexts.
    
    This will allow us to explicitly destroy hibernated GLX windows, which will hopefully plug some GPU memory leaks that were causing instability.
    
    BUG=145600
    
    
    Review URL: https://chromiumcodereview.appspot.com/11467008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171679 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_surface_glx.h
+++ ui/gl/gl_surface_glx.h
@@ -77,6 +77,7 @@ class GL_EXPORT NativeViewGLSurfaceGLX : public GLSurfaceGLX {
   virtual ~NativeViewGLSurfaceGLX();
 
   gfx::AcceleratedWidget window_;
+  XID glx_window_;
 
  private:
   void* config_;
