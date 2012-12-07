6886b4b707adce0460098d0fbea29440578028ea
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 01:53:51 +0000

    Fix potential compilation error in metro_viewer_process_host
    One trybot is complaning about std::string not being defined
    
    BUG=151718
    TEST=none
    TBR=scottmg
    Review URL: https://codereview.chromium.org/11468017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171687 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metro_viewer/metro_viewer_process_host_win.h
+++ chrome/browser/metro_viewer/metro_viewer_process_host_win.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_METRO_VIEWER_METRO_VIEWER_PROCESS_HOST_WIN_H_
 #define CHROME_BROWSER_METRO_VIEWER_METRO_VIEWER_PROCESS_HOST_WIN_H_
 
+#include <string>
+
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/non_thread_safe.h"
 #include "ipc/ipc_listener.h"
