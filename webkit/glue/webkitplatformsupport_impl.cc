f15fb7929e81dcadbd8f7dad63e2efbb822fd87c
alexeif@chromium.org <alexeif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 12:19:51 +0000

    Report memory usage retained by TCMalloc freelists.
    Review URL: https://chromiumcodereview.appspot.com/10823205
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171767 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webkitplatformsupport_impl.cc
+++ webkit/glue/webkitplatformsupport_impl.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/allocator/allocator_extension.h"
 #include "base/bind.h"
 #include "base/debug/trace_event.h"
 #include "base/memory/scoped_ptr.h"
@@ -830,6 +831,10 @@ bool WebKitPlatformSupportImpl::processMemorySizesInBytes(
     size_t* private_bytes,
     size_t* shared_bytes) {
   return CurrentProcessMetrics()->GetMemoryBytes(private_bytes, shared_bytes);
+}
+
+bool WebKitPlatformSupportImpl::memoryAllocatorWasteInBytes(size_t* size) {
+  return base::allocator::GetAllocatorWasteSize(size);
 }
 
 void WebKitPlatformSupportImpl::SuspendSharedTimer() {
