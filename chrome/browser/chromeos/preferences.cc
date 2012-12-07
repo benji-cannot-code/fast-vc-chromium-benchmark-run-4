d99fdb214928866937d2290306a9064ca7acd0cb
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 01:31:34 +0000

    Fixes display preference around swap display.
    The layout of displays (position to the primary) has been stored to the user's preference,
    but it should be in Local State. Otherwise, the display layout can be wrongly on the login
    screen.
    Also, the PrimaryDisplaySwitchObserver doesn't store the new inverted layout after the swap.
    
    BUG=163812
    TEST=login with external display, Alt-F4 to swap the primary, then logout, and make sure how displays are connected.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11411348
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171674 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences.cc
+++ chrome/browser/chromeos/preferences.cc
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/api/prefs/pref_member.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chromeos/display/display_preferences.h"
 #include "chrome/browser/chromeos/drive/drive_file_system_util.h"
 #include "chrome/browser/chromeos/input_method/input_method_manager.h"
 #include "chrome/browser/chromeos/input_method/input_method_util.h"
@@ -292,8 +291,6 @@ void Preferences::RegisterUserPrefs(PrefService* prefs) {
   prefs->RegisterBooleanPref(prefs::kExternalStorageDisabled,
                              false,
                              PrefService::UNSYNCABLE_PREF);
-
-  RegisterDisplayPrefs(prefs);
 }
 
 void Preferences::InitUserPrefs(PrefService* prefs) {
@@ -397,8 +394,6 @@ void Preferences::Init(PrefService* prefs) {
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kGuestSession)) {
     LoginUtils::Get()->SetFirstLoginPrefs(prefs);
   }
-
-  NotifyDisplayPrefChanged(prefs);
 }
 
 void Preferences::InitUserPrefsForTesting(PrefService* prefs) {
--- chrome/browser/ui/webui/options/chromeos/display_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/display_options_handler.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/chromeos/display/display_preferences.h"
 #include "chrome/browser/chromeos/display/overscan_calibrator.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "chromeos/display/output_configurator.h"
 #include "content/public/browser/web_ui.h"
@@ -194,10 +193,9 @@ void DisplayOptionsHandler::OnFadeOutForMirroringFinished(bool is_mirroring) {
 
 void DisplayOptionsHandler::OnFadeOutForDisplayLayoutFinished(
     int layout, int offset) {
-  PrefService* pref_service = Profile::FromWebUI(web_ui())->GetPrefs();
   const gfx::Display& secondary_display = ash::ScreenAsh::GetSecondaryDisplay();
   if (secondary_display.is_valid())
-    SetDisplayLayoutPref(pref_service, secondary_display, layout, offset);
+    SetDisplayLayoutPref(secondary_display, layout, offset);
 
   SendDisplayInfo();
   ash::Shell::GetInstance()->output_configurator_animation()->
