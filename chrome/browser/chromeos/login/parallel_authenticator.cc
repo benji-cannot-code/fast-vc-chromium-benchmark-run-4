ffdcd5fa4371bdbc71baaefdb019b2dd07e69588
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 06:24:43 +0000

    Add flag for requesting an ephemeral mount
    This CL adds a flag that allows Chrome to request an ephemeral mount for
    a user. If the flag is set, a cryptohome backed by tmpfs will always be
    mounted, even if a regular vault exists for the user.
    
    This functionality is required for public accounts that look like regular
    accounts to cryptohomed otherwise but whose cryptohomes must always be
    ephemeral.
    
    The CL is a companion to https://gerrit.chromium.org/gerrit/#/c/38995/ on
    the cryptohomed side.
    
    BUG=chromium-os:36892
    
    
    Review URL: https://chromiumcodereview.appspot.com/11444006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171702 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/parallel_authenticator.cc
+++ chrome/browser/chromeos/login/parallel_authenticator.cc
@@ -67,14 +67,14 @@ void TriggerResolveWithLoginTimeMarker(
 // Calls cryptohome's mount method.
 void Mount(AuthAttemptState* attempt,
            scoped_refptr<ParallelAuthenticator> resolver,
-           bool create_if_missing) {
+           int flags) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   chromeos::BootTimesLoader::Get()->AddLoginTimeMarker(
       "CryptohomeMount-Start", false);
   cryptohome::AsyncMethodCaller::GetInstance()->AsyncMount(
       attempt->username,
       attempt->ascii_hash,
-      create_if_missing,
+      flags,
       base::Bind(&TriggerResolveWithLoginTimeMarker,
                  "CryptohomeMount-End",
                  attempt,
@@ -211,13 +211,12 @@ void ParallelAuthenticator::AuthenticateToLogin(
   // Reset the verified flag.
   owner_is_verified_ = false;
 
-  const bool create_if_missing = false;
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::Bind(&Mount,
                  current_state_.get(),
                  scoped_refptr<ParallelAuthenticator>(this),
-                 create_if_missing));
+                 cryptohome::MOUNT_FLAGS_NONE));
   // ClientLogin authentication check should happen immediately here.
   // We should not try OAuthLogin check until the profile loads.
   if (!using_oauth_) {
@@ -244,13 +243,12 @@ void ParallelAuthenticator::CompleteLogin(Profile* profile,
   // Reset the verified flag.
   owner_is_verified_ = false;
 
-  const bool create_if_missing = false;
   BrowserThread::PostTask(
       BrowserThread::UI, FROM_HERE,
       base::Bind(&Mount,
                  current_state_.get(),
                  scoped_refptr<ParallelAuthenticator>(this),
-                 create_if_missing));
+                 cryptohome::MOUNT_FLAGS_NONE));
 
   if (!using_oauth_) {
     // Test automation needs to disable oauth, but that leads to other
@@ -458,7 +456,7 @@ void ParallelAuthenticator::RetryAuth(Profile* profile,
 void ParallelAuthenticator::Resolve() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   bool request_pending = false;
-  bool create = false;
+  int mount_flags = cryptohome::MOUNT_FLAGS_NONE;
   ParallelAuthenticator::AuthState state = ResolveState();
   VLOG(1) << "Resolved state to: " << state;
   switch (state) {
@@ -502,7 +500,7 @@ void ParallelAuthenticator::Resolve() {
                      LoginFailure(LoginFailure::TPM_ERROR)));
       break;
     case CREATE_NEW:
-      create = true;
+      mount_flags |= cryptohome::CREATE_IF_MISSING;
     case RECOVER_MOUNT:
       current_state_->ResetCryptohomeStatus();
       BrowserThread::PostTask(
@@ -510,7 +508,7 @@ void ParallelAuthenticator::Resolve() {
           base::Bind(&Mount,
                      current_state_.get(),
                      scoped_refptr<ParallelAuthenticator>(this),
-                     create));
+                     mount_flags));
       break;
     case NEED_OLD_PW:
       BrowserThread::PostTask(
