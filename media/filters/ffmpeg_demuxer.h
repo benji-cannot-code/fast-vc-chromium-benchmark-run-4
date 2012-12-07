43d69e1b2bfdc3f9f178b2e0398d83dbe2d5be15
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-07 23:55:19 +0000

    Introduce time-based caching of encoded data to FFmpegDemuxer (round 2).
    The original commit r169004 introduce seek regressions on constrained network connections. For now we'll limit the number of asynchronous pending reads to one. While it limits throughput it shouldn't be a big factor during normal playback.
    
    BUG=160640
    
    Review URL: https://codereview.chromium.org/11308145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171883 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/ffmpeg_demuxer.h
+++ media/filters/ffmpeg_demuxer.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "media/base/audio_decoder_config.h"
 #include "media/base/decoder_buffer.h"
+#include "media/base/decoder_buffer_queue.h"
 #include "media/base/demuxer.h"
 #include "media/base/pipeline.h"
 #include "media/base/video_decoder_config.h"
@@ -56,14 +57,15 @@ class FFmpegDemuxerStream : public DemuxerStream {
   // inside |stream|.  Both parameters must outlive |this|.
   FFmpegDemuxerStream(FFmpegDemuxer* demuxer, AVStream* stream);
 
-  // Returns true is this stream has pending reads, false otherwise.
-  bool HasPendingReads();
-
-  // Enqueues the given AVPacket.  If |packet| is NULL an end of stream packet
-  // is enqueued.
+  // Enqueues the given AVPacket. It is invalid to queue a |packet| after
+  // SetEndOfStream() has been called.
   void EnqueuePacket(ScopedAVPacket packet);
 
-  // Signals to empty the buffer queue and mark next packet as discontinuous.
+  // Enters the end of stream state. After delivering remaining queued buffers
+  // only end of stream buffers will be delivered.
+  void SetEndOfStream();
+
+  // Drops queued buffers and clears end of stream state.
   void FlushBuffers();
 
   // Empties the queues and ignores any additional calls to Read().
@@ -85,6 +87,9 @@ class FFmpegDemuxerStream : public DemuxerStream {
   // Returns elapsed time based on the already queued packets.
   // Used to determine stream duration when it's not known ahead of time.
   base::TimeDelta GetElapsedTime() const;
+
+  // Returns true if this stream has capacity for additional data.
+  bool HasAvailableCapacity();
 
  protected:
   virtual ~FFmpegDemuxerStream();
@@ -108,11 +113,11 @@ class FFmpegDemuxerStream : public DemuxerStream {
   Type type_;
   base::TimeDelta duration_;
   bool stopped_;
+  bool end_of_stream_;
   base::TimeDelta last_packet_timestamp_;
   Ranges<base::TimeDelta> buffered_ranges_;
 
-  typedef std::deque<scoped_refptr<DecoderBuffer> > BufferQueue;
-  BufferQueue buffer_queue_;
+  DecoderBufferQueue buffer_queue_;
 
   typedef std::deque<ReadCB> ReadQueue;
   ReadQueue read_queue_;
@@ -139,9 +144,9 @@ class MEDIA_EXPORT FFmpegDemuxer : public Demuxer {
       DemuxerStream::Type type) OVERRIDE;
   virtual base::TimeDelta GetStartTime() const OVERRIDE;
 
-  // Allow FFmpegDemuxerStream to notify us when it requires more data or has
-  // updated information about what buffered data is available.
-  void NotifyHasPendingRead();
+  // Allow FFmpegDemuxerStream to notify us when there is updated information
+  // about capacity and what buffered data is available.
+  void NotifyCapacityAvailable();
   void NotifyBufferingChanged();
 
  private:
@@ -170,16 +175,11 @@ class MEDIA_EXPORT FFmpegDemuxer : public Demuxer {
   // Carries out disabling the audio stream on the demuxer thread.
   void DisableAudioStreamTask();
 
-  // Returns true if any of the streams have pending reads.  Since we lazily
-  // post a DemuxTask() for every read, we use this method to quickly terminate
-  // the tasks if there is no work to do.
-  //
-  // Must be called on the demuxer thread.
-  bool StreamsHavePendingReads();
+  // Returns true iff any stream has additional capacity. Note that streams can
+  // go over capacity depending on how the file is muxed.
+  bool StreamsHaveAvailableCapacity();
 
-  // Signal all FFmpegDemuxerStream that the stream has ended.
-  //
-  // Must be called on the demuxer thread.
+  // Signal all FFmpegDemuxerStreams that the stream has ended.
   void StreamHasEnded();
 
   // Called by |url_protocol_| whenever |data_source_| returns a read error.
@@ -196,6 +196,16 @@ class MEDIA_EXPORT FFmpegDemuxer : public Demuxer {
 
   // Thread on which all blocking FFmpeg operations are executed.
   base::Thread blocking_thread_;
+
+  // Tracks if there's an outstanding av_read_frame() operation.
+  //
+  // TODO(scherkus): Allow more than one read in flight for higher read
+  // throughput using demuxer_bench to verify improvements.
+  bool pending_read_;
+
+  // Tracks if there's an outstanding av_seek_frame() operation. Used to discard
+  // results of pre-seek av_read_frame() operations.
+  bool pending_seek_;
 
   // |streams_| mirrors the AVStream array in |format_context_|. It contains
   // FFmpegDemuxerStreams encapsluating AVStream objects at the same index.
