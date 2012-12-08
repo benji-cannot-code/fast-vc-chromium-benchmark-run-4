32064bacef5a45f73c5f8fe4ccd213b0bbdf0382
serya@chromium.org <serya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-08 16:55:14 +0000

    Refactoring table ang rgid rendering code.
    BUG=
    TEST=Manual test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11453033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171978 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -58,8 +58,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="directory_model.js"/>
 //<include src="file_copy_manager_wrapper.js"/>
 //<include src="drive_banners.js" />
+//<include src="file_grid.js"/>
 //<include src="file_manager.js"/>
 //<include src="file_manager_pyauto.js"/>
+//<include src="file_table.js"/>
 //<include src="file_tasks.js"/>
 //<include src="file_transfer_controller.js"/>
 //<include src="file_type.js"/>
