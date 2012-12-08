39eee259ceece67a45468f27c34f4d741b41d350
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-08 03:54:54 +0000

    Add missing const qualifier to JSC::CodeBlock::getJITType() https://bugs.webkit.org/show_bug.cgi?id=104424
    Patch by Jonathan Liu <net147@gmail.com> on 2012-12-07
    Reviewed by Laszlo Gombos.
    
    JSC::CodeBlock::getJITType() has the const qualifier when JIT is
    enabled but is missing the const qualifier when JIT is disabled.
    
    * bytecode/CodeBlock.h:
    (JSC::CodeBlock::getJITType):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137010 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.h
@@ -529,7 +529,7 @@ namespace JSC {
             return result;
         }
 #else
-        JITCode::JITType getJITType() { return JITCode::BaselineJIT; }
+        JITCode::JITType getJITType() const { return JITCode::BaselineJIT; }
 #endif
 
         ScriptExecutable* ownerExecutable() const { return m_ownerExecutable.get(); }
