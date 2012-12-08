fddb1021d277597e405f4ff283d28b2f4eac889a
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-08 16:45:25 +0000

    Replace av_malloc with AlignedAlloc for memory allocation in VideoFrame.
    See also r157836.
    
    BUG=150920
    TEST=media_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/11308310
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171976 0039d316-1c4b-4281-b951-d872f2087c98

--- media/ffmpeg/ffmpeg_common.cc
+++ media/ffmpeg/ffmpeg_common.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/logging.h"
 #include "media/base/decoder_buffer.h"
+#include "media/base/video_frame.h"
 #include "media/base/video_util.h"
 
 namespace media {
@@ -18,18 +19,31 @@ namespace media {
 COMPILE_ASSERT(DecoderBuffer::kPaddingSize >= FF_INPUT_BUFFER_PADDING_SIZE,
                decoder_buffer_padding_size_does_not_fit_ffmpeg_requirement);
 
-// Alignment requirement by FFmpeg for input buffers. This need to be updated
-// to match FFmpeg when it changes.
+// Alignment requirement by FFmpeg for input and output buffers. This need to
+// be updated to match FFmpeg when it changes.
 #if defined(ARCH_CPU_ARM_FAMILY)
-static const int kFFmpegInputBufferAlignmentSize = 16;
+static const int kFFmpegBufferAddressAlignment = 16;
 #else
-static const int kFFmpegInputBufferAlignmentSize = 32;
+static const int kFFmpegBufferAddressAlignment = 32;
 #endif
+
 // Check here to ensure FFmpeg only receives data aligned to its specifications.
 COMPILE_ASSERT(
-    DecoderBuffer::kAlignmentSize >= kFFmpegInputBufferAlignmentSize &&
-    DecoderBuffer::kAlignmentSize % kFFmpegInputBufferAlignmentSize == 0,
+    DecoderBuffer::kAlignmentSize >= kFFmpegBufferAddressAlignment &&
+    DecoderBuffer::kAlignmentSize % kFFmpegBufferAddressAlignment == 0,
     decoder_buffer_alignment_size_does_not_fit_ffmpeg_requirement);
+
+// Allows faster SIMD YUV convert. Also, FFmpeg overreads/-writes occasionally.
+// See video_get_buffer() in libavcodec/utils.c.
+static const int kFFmpegOutputBufferPaddingSize = 16;
+
+COMPILE_ASSERT(VideoFrame::kFrameSizePadding >= kFFmpegOutputBufferPaddingSize,
+               video_frame_padding_size_does_not_fit_ffmpeg_requirement);
+
+COMPILE_ASSERT(
+    VideoFrame::kFrameAddressAlignment >= kFFmpegBufferAddressAlignment &&
+    VideoFrame::kFrameAddressAlignment % kFFmpegBufferAddressAlignment == 0,
+    video_frame_address_alignment_does_not_fit_ffmpeg_requirement);
 
 static const AVRational kMicrosBase = { 1, base::Time::kMicrosecondsPerSecond };
 
