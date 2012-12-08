043b677bf14316606655ead15d6190be6d79fec9
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-08 04:13:35 +0000

    Start GPU process earlier (when force-compositing mode is enabled).
    This allows the renderer to not block on the GPU process on startup. Don't prelaunch GPU process if force-compositing-mode is disabled, because it probably won't be used in that case.
    
    BUG=163215,164609
    
    
    Review URL: https://chromiumcodereview.appspot.com/11469022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171932 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/trace_controller_impl.h"
 #include "content/public/browser/browser_main_parts.h"
 #include "content/public/browser/browser_shutdown.h"
+#include "content/public/browser/compositor_util.h"
 #include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/common/content_switches.h"
@@ -466,23 +467,6 @@ void BrowserMainLoop::CreateThreads() {
   if (parts_.get())
     parts_->PreMainMessageLoopRun();
 
-#if !defined(OS_IOS)
-  // When running the GPU thread in-process, avoid optimistically starting it
-  // since creating the GPU thread races against creation of the one-and-only
-  // ChildProcess instance which is created by the renderer thread.
-  if (GpuDataManagerImpl::GetInstance()->GpuAccessAllowed() &&
-      !parsed_command_line_.HasSwitch(switches::kDisableGpuProcessPrelaunch) &&
-      !parsed_command_line_.HasSwitch(switches::kSingleProcess) &&
-      !parsed_command_line_.HasSwitch(switches::kInProcessGPU)) {
-    TRACE_EVENT_INSTANT0("gpu", "Post task to launch GPU process");
-    BrowserThread::PostTask(
-        BrowserThread::IO, FROM_HERE, base::Bind(
-            base::IgnoreResult(&GpuProcessHost::Get),
-            GpuProcessHost::GPU_PROCESS_KIND_SANDBOXED,
-            CAUSE_FOR_GPU_LAUNCH_BROWSER_STARTUP));
-  }
-#endif  // !defined(OS_IOS)
-
   // If the UI thread blocks, the whole UI is unresponsive.
   // Do not allow disk IO from the UI thread.
   base::ThreadRestrictions::SetIOAllowed(false);
@@ -696,6 +680,22 @@ void BrowserMainLoop::BrowserThreadsStarted() {
   allowed_clipboard_threads.push_back(io_thread_->thread_id());
 #endif
   ui::Clipboard::SetAllowedThreads(allowed_clipboard_threads);
+
+  // When running the GPU thread in-process, avoid optimistically starting it
+  // since creating the GPU thread races against creation of the one-and-only
+  // ChildProcess instance which is created by the renderer thread.
+  if (GpuDataManagerImpl::GetInstance()->GpuAccessAllowed() &&
+      content::IsForceCompositingModeEnabled() &&
+      !parsed_command_line_.HasSwitch(switches::kDisableGpuProcessPrelaunch) &&
+      !parsed_command_line_.HasSwitch(switches::kSingleProcess) &&
+      !parsed_command_line_.HasSwitch(switches::kInProcessGPU)) {
+    TRACE_EVENT_INSTANT0("gpu", "Post task to launch GPU process");
+    BrowserThread::PostTask(
+        BrowserThread::IO, FROM_HERE, base::Bind(
+            base::IgnoreResult(&GpuProcessHost::Get),
+            GpuProcessHost::GPU_PROCESS_KIND_SANDBOXED,
+            CAUSE_FOR_GPU_LAUNCH_BROWSER_STARTUP));
+  }
 #endif  // !defined(OS_IOS)
 }
 
