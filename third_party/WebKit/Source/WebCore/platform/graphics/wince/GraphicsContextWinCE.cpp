7ec1ac8cf1c1ec4b9a8df02a2c36826ea0be4103
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-08 21:47:56 +0000

    Build fix for WinCE after r137011.
    * platform/graphics/GraphicsContext.h:
    * platform/graphics/wince/GraphicsContextWinCE.cpp:
    (WebCore::GraphicsContext::setPlatformCompositeOperation):
    (WebCore::GraphicsContext::drawBitmap):
    * platform/graphics/wince/ImageBufferWinCE.cpp:
    (WebCore::BufferedImage::draw):
    (WebCore::ImageBuffer::draw):
    * platform/graphics/wince/ImageWinCE.cpp:
    (WebCore::BitmapImage::getHBITMAPOfSize):
    (WebCore::BitmapImage::drawFrameMatchingSourceSize):
    (WebCore::BitmapImage::draw):
    * platform/graphics/wince/SharedBitmap.cpp:
    (WebCore::SharedBitmap::draw):
    * platform/graphics/wince/SharedBitmap.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/wince/GraphicsContextWinCE.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/wince/GraphicsContextWinCE.cpp
@@ -1194,7 +1194,7 @@ void GraphicsContext::setAlpha(float alpha)
     m_data->m_opacity = alpha;
 }
 
-void GraphicsContext::setPlatformCompositeOperation(CompositeOperator op)
+void GraphicsContext::setPlatformCompositeOperation(CompositeOperator op, BlendMode blendMode)
 {
     notImplemented();
 }
@@ -1830,7 +1830,7 @@ void GraphicsContext::paintTextField(const IntRect& rect, unsigned state)
     FillRect(dc, &rectWin, reinterpret_cast<HBRUSH>(((state & DFCS_INACTIVE) ? COLOR_BTNFACE : COLOR_WINDOW) + 1));
 }
 
-void GraphicsContext::drawBitmap(SharedBitmap* bmp, const IntRect& dstRectIn, const IntRect& srcRect, ColorSpace styleColorSpace, CompositeOperator compositeOp)
+void GraphicsContext::drawBitmap(SharedBitmap* bmp, const IntRect& dstRectIn, const IntRect& srcRect, ColorSpace styleColorSpace, CompositeOperator compositeOp, BlendMode blendMode)
 {
     if (!m_data->m_opacity)
         return;
@@ -1846,7 +1846,7 @@ void GraphicsContext::drawBitmap(SharedBitmap* bmp, const IntRect& dstRectIn, co
         return;
     dstRect.move(transparentDC.toShift());
 
-    bmp->draw(dc, dstRect, srcRect, compositeOp);
+    bmp->draw(dc, dstRect, srcRect, compositeOp, blendMode);
 
     if (bmp->is16bit())
         transparentDC.fillAlphaChannel();
