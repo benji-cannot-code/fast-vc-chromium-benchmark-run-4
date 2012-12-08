dac2ec2d1f3c7c5e4602506f9163f3daa2b14230
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-08 02:42:31 +0000

    Remove WebSocket server and HostStatusService from remoting host.
    We are not planning to use WebSockets for host configuration. Removing
    websocket-related code from remoting host.
    
    Review URL: https://codereview.chromium.org/11475037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171915 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/host_config.h"
 #include "remoting/host/host_event_logger.h"
 #include "remoting/host/host_exit_codes.h"
-#include "remoting/host/host_status_service.h"
 #include "remoting/host/host_user_interface.h"
 #include "remoting/host/ipc_constants.h"
 #include "remoting/host/ipc_desktop_environment_factory.h"
@@ -105,9 +104,6 @@ const char kVersionSwitchName[] = "version";
 // The command line switch used to pass name of the pipe to capture audio on
 // linux.
 const char kAudioPipeSwitchName[] = "audio-pipe-name";
-
-// The command line switch used to enable host status service.
-const char kEnableStatusServiceSwitchName[] = "enable-status-service";
 
 void QuitMessageLoop(MessageLoop* message_loop) {
   message_loop->PostTask(FROM_HERE, MessageLoop::QuitClosure());
@@ -277,7 +273,6 @@ class HostProcess
   HostState state_;
 
   scoped_ptr<ConfigFileWatcher> config_watcher_;
-  scoped_ptr<HostStatusService> status_service_;
 
   std::string host_id_;
   protocol::SharedSecretHash host_secret_hash_;
@@ -462,11 +457,6 @@ void HostProcess::OnConfigWatcherError() {
 
 void HostProcess::StartOnNetworkThread() {
   DCHECK(context_->network_task_runner()->BelongsToCurrentThread());
-
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          kEnableStatusServiceSwitchName)) {
-    status_service_.reset(new HostStatusService());
-  }
 
 #if !defined(REMOTING_MULTI_PROCESS)
   // Start watching the host configuration file.
@@ -939,9 +929,6 @@ void HostProcess::StartHost() {
         host_, base::Bind(&HostProcess::OnDisconnectRequested, this));
   }
 
-  if (status_service_)
-    status_service_->SetHostIsUp(host_id_);
-
   host_->Start(xmpp_login_);
 
   CreateAuthenticatorFactory();
@@ -998,8 +985,6 @@ void HostProcess::ShutdownHost(int exit_code) {
 
     case HOST_STARTED:
       state_ = HOST_STOPPING;
-      if (status_service_)
-        status_service_->SetHostIsDown();
       host_->Shutdown(base::Bind(&HostProcess::ShutdownOnNetworkThread, this));
       break;
 
@@ -1039,7 +1024,6 @@ void HostProcess::ShutdownOnNetworkThread() {
     }
 
     config_watcher_.reset();
-    status_service_.reset();
 
     // Complete the rest of shutdown on the main thread.
     context_->ui_task_runner()->PostTask(
