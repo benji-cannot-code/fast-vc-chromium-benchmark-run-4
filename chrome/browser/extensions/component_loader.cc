1f3be8c07557bb8389ad74c95b25072cf539c06c
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-08 08:49:30 +0000

    Added RemoveAll() method to ComponentLoader.
    BUG=164903
    TEST=ComponentLoaderTest.RemoveAll
    
    
    Review URL: https://chromiumcodereview.appspot.com/11468026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171967 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -196,6 +196,14 @@ const Extension* ComponentLoader::Load(const ComponentExtensionInfo& info) {
   return extension;
 }
 
+void ComponentLoader::RemoveAll() {
+  RegisteredComponentExtensions::iterator it = component_extensions_.begin();
+  for (; it != component_extensions_.end(); ++it)
+    UnloadComponent(&(*it));
+
+  component_extensions_.clear();
+}
+
 void ComponentLoader::Remove(const FilePath& root_directory) {
   // Find the ComponentExtensionInfo for the extension.
   RegisteredComponentExtensions::iterator it = component_extensions_.begin();
@@ -211,11 +219,8 @@ void ComponentLoader::Remove(const std::string& id) {
   RegisteredComponentExtensions::iterator it = component_extensions_.begin();
   for (; it != component_extensions_.end(); ++it) {
     if (it->extension_id == id) {
-      delete it->manifest;
+      UnloadComponent(&(*it));
       it = component_extensions_.erase(it);
-      if (extension_service_->is_ready())
-        extension_service_->
-            UnloadExtension(id, extension_misc::UNLOAD_REASON_DISABLE);
       break;
     }
   }
@@ -411,6 +416,15 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
   }
 #endif
 
+}
+
+void ComponentLoader::UnloadComponent(ComponentExtensionInfo* component) {
+  delete component->manifest;
+  if (extension_service_->is_ready()) {
+    extension_service_->
+        UnloadExtension(component->extension_id,
+                        extension_misc::UNLOAD_REASON_DISABLE);
+  }
 }
 
 // static
