63bd0b7a6f6a5aa4f10036d34763920cba7e4523
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-08 04:08:28 +0000

    Add the QuicRandom interface with a default implementation that is both cryptographically secure and thread-safe. The default QuicRandom is a singleton.
    crypto/random.h can include the smaller <stddef.h> for size_t.
    
    Add missing header files to net/net.gyp.
    
    R=rch@chromium.org
    BUG=none
    TEST=new unit tests
    
    Review URL: https://chromiumcodereview.appspot.com/11476031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@171931 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/random.h
+++ crypto/random.h
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CRYPTO_RANDOM_H_
 #define CRYPTO_RANDOM_H_
 
-#include <stdlib.h>
+#include <stddef.h>
 
 #include "crypto/crypto_export.h"
 
