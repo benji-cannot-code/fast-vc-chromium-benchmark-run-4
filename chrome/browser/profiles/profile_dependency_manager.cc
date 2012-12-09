50851319b43f655ecd0b99c60d71a0af71d14c53
rdevlin.cronin@chromium.org <rdevlin.cronin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-09 09:25:21 +0000

    Make FontSettingsEventRouter a PKS in order to decouple it from ExtensionService.
    TBR=ben@chromium.org (for gypi)
    BUG=159265
    
    Review URL: https://chromiumcodereview.appspot.com/11476014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172009 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_dependency_manager.cc
+++ chrome/browser/profiles/profile_dependency_manager.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/commands/command_service_factory.h"
 #include "chrome/browser/extensions/api/cookies/cookies_api_factory.h"
 #include "chrome/browser/extensions/api/discovery/suggested_links_registry_factory.h"
+#include "chrome/browser/extensions/api/font_settings/font_settings_api_factory.h"
 #include "chrome/browser/extensions/api/idle/idle_manager_factory.h"
 #include "chrome/browser/extensions/api/managed_mode/managed_mode_api_factory.h"
 #include "chrome/browser/extensions/api/management/management_api_factory.h"
@@ -229,6 +230,7 @@ void ProfileDependencyManager::AssertFactoriesBuilt() {
   extensions::CommandServiceFactory::GetInstance();
   extensions::CookiesAPIFactory::GetInstance();
   extensions::ExtensionSystemFactory::GetInstance();
+  extensions::FontSettingsAPIFactory::GetInstance();
   extensions::IdleManagerFactory::GetInstance();
   extensions::ManagedModeAPIFactory::GetInstance();
   extensions::ProcessesAPIFactory::GetInstance();
