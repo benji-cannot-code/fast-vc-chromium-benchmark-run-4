e9069fec73173af049f9748b68f2670ef0d9665c
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-09 09:19:20 +0000

    Have ExtensionService notify ThemeService directly for theme installations
    This is a reland of r170880 and r171368, now that unrelated regressions
    have been addressed.
    
    > Revert changes to have ExtensionService notify ThemeService directly for themes
    > 
    > Changes r170880 introduced a regression and r171368 only partially fixed it.
    > 
    > Reverting both of them to get back to a clean state.
    > 
    > BUG=164400
    > TBR=asargent@chromium.org,pkotwicz@chromium.org,erg@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/11445038
    
    TBR=akalin@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172006 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/themes/theme_service.h
+++ chrome/browser/themes/theme_service.h
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/non_thread_safe.h"
 #include "chrome/browser/profiles/profile_keyed_service.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "ui/base/theme_provider.h"
 
 class BrowserThemePack;
@@ -50,7 +48,6 @@ extern "C" NSString* const kBrowserThemeDidChangeNotification;
 #endif  // __OBJC__
 
 class ThemeService : public base::NonThreadSafe,
-                     public content::NotificationObserver,
                      public ProfileKeyedService,
                      public ui::ThemeProvider {
  public:
@@ -184,6 +181,8 @@ class ThemeService : public base::NonThreadSafe,
 #endif
 
   // Set the current theme to the theme defined in |extension|.
+  // |extension| must already be added to this profile's
+  // ExtensionService.
   virtual void SetTheme(const extensions::Extension* extension);
 
   // Reset the theme to default.
@@ -275,11 +274,6 @@ class ThemeService : public base::NonThreadSafe,
 
   Profile* profile() { return profile_; }
 
-  // content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
-
  private:
   friend class ThemeServiceTest;
 
@@ -320,8 +314,6 @@ class ThemeService : public base::NonThreadSafe,
 
   // The number of infobars currently displayed.
   int number_of_infobars_;
-
-  content::NotificationRegistrar registrar_;
 
   scoped_ptr<ThemeSyncableService> theme_syncable_service_;
 
