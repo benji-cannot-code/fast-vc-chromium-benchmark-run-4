fed3c4cda93384ad5985165b5c97875345b27404
ericu@chromium.org <ericu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-10 23:31:01 +0000

    Remove the old-sandbox-to-new-sandbox migration code.  The only users of the old sandbox are those who used the filesystem in apps 1.5 years ago, and haven't loaded those apps since.
    Review URL: https://chromiumcodereview.appspot.com/11419193
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172182 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/sandbox_mount_point_provider.h
+++ webkit/fileapi/sandbox_mount_point_provider.h
@@ -61,13 +61,8 @@ class WEBKIT_STORAGE_EXPORT SandboxMountPointProvider
     virtual bool HasFileSystemType(FileSystemType type) const = 0;
   };
 
-  // The legacy [pre-obfuscation] FileSystem directory name, kept around for
-  // migration and migration testing.
-  static const FilePath::CharType kOldFileSystemDirectory[];
   // The FileSystem directory name.
-  static const FilePath::CharType kNewFileSystemDirectory[];
-  // Where we move the old filesystem directory if migration fails.
-  static const FilePath::CharType kRenamedOldFileSystemDirectory[];
+  static const FilePath::CharType kFileSystemDirectory[];
 
   static bool CanHandleType(FileSystemType type);
 
@@ -115,10 +110,6 @@ class WEBKIT_STORAGE_EXPORT SandboxMountPointProvider
       FileSystemType type,
       FileSystemContext* context,
       const DeleteFileSystemCallback& callback) OVERRIDE;
-
-  FilePath old_base_path() const;
-  FilePath new_base_path() const;
-  FilePath renamed_old_base_path() const;
 
   // Returns an origin enumerator of this provider.
   // This method can only be called on the file thread.
@@ -196,9 +187,6 @@ class WEBKIT_STORAGE_EXPORT SandboxMountPointProvider
       const GURL& origin_url,
       FileSystemType type,
       base::PlatformFileError* error_out);
-
-  FilePath OldCreateFileSystemRootPath(
-      const GURL& origin_url, FileSystemType type);
 
   // Returns true if the given |url|'s scheme is allowed to access
   // filesystem.
