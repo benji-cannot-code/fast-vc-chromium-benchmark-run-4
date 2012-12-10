1c117c112701ffdae88d7d68c7d8ffe6fce06fef
bcwhite@chromium.org <bcwhite@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-10 23:04:53 +0000

    Don't show Sync promo if we have zero network connectivity.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11475011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172172 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_promo/sync_promo_ui.cc
+++ chrome/browser/ui/webui/sync_promo/sync_promo_ui.cc
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "grit/theme_resources.h"
 #include "net/base/escape.h"
+#include "net/base/network_change_notifier.h"
 #include "ui/base/l10n/l10n_util.h"
 
 using content::WebContents;
@@ -125,6 +126,10 @@ bool SyncPromoUI::ShouldShowSyncPromo(Profile* profile) {
   // into sync already.
   return false;
 #endif
+
+  // Don't bother if we don't have any kind of network connection.
+  if (net::NetworkChangeNotifier::IsOffline())
+    return false;
 
   // Honor the sync policies.
   if (!profile->GetOriginalProfile()->IsSyncAccessible())
