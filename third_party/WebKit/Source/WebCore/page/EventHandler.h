a8795d064382d8ba36ec5a6f3541d041125f9642
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-10 23:34:32 +0000

    Unreviewed, rolling out r134150. http://trac.webkit.org/changeset/134150 https://bugs.webkit.org/show_bug.cgi?id=99940
    Caused JS-based tooltips to remain during scroll on Facebook
    and Gmail. Rollout until I have time to experiment with less
    aggresive approaches.
    
    Source/WebCore:
    
    * page/EventHandler.cpp:
    (WebCore):
    (WebCore::MaximumDurationTracker::MaximumDurationTracker):
    (WebCore::MaximumDurationTracker::~MaximumDurationTracker):
    (MaximumDurationTracker):
    (WebCore::EventHandler::EventHandler):
    (WebCore::EventHandler::clear):
    (WebCore::EventHandler::mouseMoved):
    (WebCore::EventHandler::dispatchFakeMouseMoveEventSoon):
    (WebCore::EventHandler::fakeMouseMoveEventTimerFired):
    * page/EventHandler.h:
    (EventHandler):
    * platform/Timer.h:
    
    LayoutTests:
    
    * fast/scrolling/fake-mouse-event-throttling-expected.txt: Removed.
    * fast/scrolling/fake-mouse-event-throttling.html: Removed.
    * platform/mac/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137214 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.h
+++ third_party/WebKit/Source/WebCore/page/EventHandler.h
@@ -288,7 +288,7 @@ private:
     static bool isKeyboardOptionTab(KeyboardEvent*);
     static bool eventInvertsTabsToLinksClientCallResult(KeyboardEvent*);
 
-    void fakeMouseMoveEventTimerFired(DeferrableOneShotTimer<EventHandler>*);
+    void fakeMouseMoveEventTimerFired(Timer<EventHandler>*);
     void cancelFakeMouseMoveEvent();
 
     bool isInsideScrollbar(const IntPoint&) const;
@@ -413,7 +413,7 @@ private:
     bool m_mouseDownMayStartAutoscroll;
     bool m_mouseDownWasInSubframe;
 
-    DeferrableOneShotTimer<EventHandler> m_fakeMouseMoveEventTimer;
+    Timer<EventHandler> m_fakeMouseMoveEventTimer;
 
 #if ENABLE(SVG)
     bool m_svgPan;
@@ -471,7 +471,7 @@ private:
     RefPtr<Scrollbar> m_scrollbarHandlingScrollGesture;
 #endif
 
-    double m_mouseMovedDurationRunningAverage;
+    double m_maxMouseMovedDuration;
     PlatformEvent::Type m_baseEventType;
     bool m_didStartDrag;
     bool m_didLongPressInvokeContextMenu;
