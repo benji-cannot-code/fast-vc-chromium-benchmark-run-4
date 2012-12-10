0c3d31c2d6db07709bab53a5d5315d6517856966
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-10 19:31:23 +0000

    Caused crashes in v8.  Rolling out until abarth can fix.  See cr-issue 165136 for more info https://bugs.webkit.org/show_bug.cgi?id=104575
    Unreviewed, just rolling out r136559.
    
    
    * bindings/v8/V8GCController.cpp:
    * bindings/v8/V8NodeFilterCondition.cpp:
    (WebCore::V8NodeFilterCondition::V8NodeFilterCondition):
    (WebCore::V8NodeFilterCondition::acceptNode):
    * bindings/v8/V8NodeFilterCondition.h:
    (WebCore::V8NodeFilterCondition::create):
    (V8NodeFilterCondition):
    * dom/NodeFilter.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8GCController.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8GCController.cpp
@@ -41,8 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8MessagePort.h"
 #include "V8MutationObserver.h"
 #include "V8Node.h"
-#include "V8NodeFilter.h"
-#include "V8NodeFilterCondition.h"
 #include "V8RecursionScope.h"
 #include "WrapperTypeInfo.h"
 #include <algorithm>
@@ -150,7 +148,6 @@ public:
         WrapperTypeInfo* type = toWrapperTypeInfo(wrapper);
         void* object = toNative(wrapper);
 
-        // FIXME: Abstract this if cascade into a WrapperTypeInfo function.
         if (V8MessagePort::info.equals(type)) {
             // Mark each port as in-use if it's entangled. For simplicity's sake,
             // we assume all ports are remotely entangled, since the Chromium port
@@ -166,9 +163,6 @@ public:
             for (HashSet<Node*>::iterator it = observedNodes.begin(); it != observedNodes.end(); ++it)
                 m_grouper.addToGroup(V8GCController::opaqueRootForGC(*it), wrapper);
 #endif // ENABLE(MUTATION_OBSERVERS)
-        } else if (V8NodeFilter::info.equals(type)) {
-            NodeFilter* filter = static_cast<NodeFilter*>(object);
-            m_grouper.addToGroup(type->opaqueRootForGC(object, wrapper), static_cast<V8NodeFilterCondition*>(filter->condition())->callback());
         } else {
             ActiveDOMObject* activeDOMObject = type->toActiveDOMObject(wrapper);
             if (activeDOMObject && activeDOMObject->hasPendingActivity())
