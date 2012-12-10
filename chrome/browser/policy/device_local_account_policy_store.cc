39fcf7b35ca2d7922464982f01563a2268145d1b
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-10 19:16:04 +0000

    Public accounts: Always show logout button and never hide shelf
    This CL ensures that the ShowLogoutButtonInTray and ShelfAutoHideBehavior
    policies are always set for public accounts to show a big, red logout
    button in the launcher's system tray and prevent the launcher from
    auto-hiding.
    
    BUG=152920,152929
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/11487002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172097 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/device_local_account_policy_store.cc
+++ chrome/browser/policy/device_local_account_policy_store.cc
@@ -6,10 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/device_local_account_policy_store.h"
 
 #include "base/bind.h"
+#include "base/values.h"
 #include "chrome/browser/policy/device_management_service.h"
+#include "chrome/browser/policy/policy_types.h"
 #include "chrome/browser/policy/proto/cloud_policy.pb.h"
 #include "chrome/browser/policy/proto/device_management_backend.pb.h"
 #include "chromeos/dbus/session_manager_client.h"
+#include "policy/policy_constants.h"
 
 namespace em = enterprise_management;
 
@@ -72,6 +75,19 @@ void DeviceLocalAccountPolicyStore::UpdatePolicy(
   }
 
   InstallPolicy(validator->policy_data().Pass(), validator->payload().Pass());
+  // Force the |ShelfAutoHideBehavior| policy to |Never|, ensuring that the ash
+  // shelf does not auto-hide.
+  policy_map_.Set(key::kShelfAutoHideBehavior,
+                  POLICY_LEVEL_MANDATORY,
+                  POLICY_SCOPE_USER,
+                  Value::CreateStringValue("Never"));
+  // Force the |ShowLogoutButtonInTray| policy to |true|, ensuring that a big,
+  // red logout button is shown in the ash system tray.
+  policy_map_.Set(key::kShowLogoutButtonInTray,
+                  POLICY_LEVEL_MANDATORY,
+                  POLICY_SCOPE_USER,
+                  Value::CreateBooleanValue(true));
+
   status_ = STATUS_OK;
   NotifyStoreLoaded();
 }
