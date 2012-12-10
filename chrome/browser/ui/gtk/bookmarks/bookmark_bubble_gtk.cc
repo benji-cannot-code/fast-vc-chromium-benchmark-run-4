ede00a847c88eaf7d22672bc722d3959be8e6012
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-10 22:45:37 +0000

    gtk: No need to send a notification that nobody listens to.
    Nobody seems to be listening to chrome::NOTIFICATION_BOOKMARK_BUBBLE_HIDDEN
    notification, at least on gtk side, so let's not send it at all.
    
    R=erg@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11478030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/bookmarks/bookmark_bubble_gtk.cc
+++ chrome/browser/ui/gtk/bookmarks/bookmark_bubble_gtk.cc
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/gtk/gtk_theme_service.h"
 #include "chrome/browser/ui/gtk/gtk_util.h"
 #include "chrome/common/chrome_notification_types.h"
-#include "content/public/browser/notification_service.h"
+#include "content/public/browser/notification_source.h"
 #include "content/public/browser/user_metrics.h"
 #include "grit/generated_resources.h"
 #include "ui/base/gtk/gtk_hig_constants.h"
@@ -59,11 +59,6 @@ void BookmarkBubbleGtk::BubbleClosing(BubbleGtk* bubble,
     remove_bookmark_ = newly_bookmarked_;
     apply_edits_ = false;
   }
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_BOOKMARK_BUBBLE_HIDDEN,
-      content::Source<Profile>(profile_->GetOriginalProfile()),
-      content::NotificationService::NoDetails());
 }
 
 void BookmarkBubbleGtk::Observe(int type,
