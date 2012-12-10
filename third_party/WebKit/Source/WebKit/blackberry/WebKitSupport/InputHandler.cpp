992df6c29639974b43dc4ef7a3943c3666e5ec8d
nghanavatian@rim.com <nghanavatian@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-10 17:01:52 +0000

    [BlackBerry] Fix warning for comparison between signed and unsigned https://bugs.webkit.org/show_bug.cgi?id=104563
    Reviewed by Rob Buis.
    
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::isCaretAtEndOfText):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137163 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/InputHandler.cpp
@@ -1867,7 +1867,7 @@ bool InputHandler::setBatchEditingActive(bool active)
 
 bool InputHandler::isCaretAtEndOfText()
 {
-    return caretPosition() == elementText().length();
+    return caretPosition() == static_cast<int>(elementText().length());
 }
 
 int InputHandler::caretPosition() const
