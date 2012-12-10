0a086c262865d78fa8e153059246af36acf99b1d
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-10 08:52:51 +0000

    Changed misleading translations for toggling spoken feedback.
    Before, the ctrl-alt-z was described in the shortcuts layer as 'Toggle accessibility features', which was wrong since it toggles spoken feedback only. Also a notification text has been changed from 'Accessibility features are enabled' to 'Spoken feedback is enabled'.
    
    BUG=164806
    
    
    Review URL: https://chromiumcodereview.appspot.com/11474021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172026 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -15876,7 +15876,7 @@ var keyboardOverlayData = {
     'w<>CTRL': 'keyboardOverlayCloseTab',
     'w<>CTRL<>SHIFT': 'keyboardOverlayCloseWindow',
     'x<>CTRL': 'keyboardOverlayCut',
-    'z<>ALT<>CTRL': 'keyboardOverlayToggleAccessibilityFeatures',
+    'z<>ALT<>CTRL': 'keyboardOverlayToggleSpokenFeedback',
     'z<>CTRL': 'keyboardOverlayUndo'
   }
 };
