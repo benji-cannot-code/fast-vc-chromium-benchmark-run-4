175a223428958917175b254c6c710187a43ec3e7
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-11 13:53:14 +0000

    [Coverity] [GTK] Remove some redundant null checks in DumpRenderTree https://bugs.webkit.org/show_bug.cgi?id=104570
    Reviewed by Daniel Bates.
    
    Source/WebKit/gtk:
    
    * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
    (DumpRenderTreeSupportGtk::elementDoesAutoCompleteForElementWithId): Remove a redundant null check.
    
    Tools:
    
    * DumpRenderTree/gtk/DumpRenderTree.cpp:
    (descriptionSuitableForTestResult): Remove a redundant null check.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.cpp
+++ third_party/WebKit/Source/WebKit/gtk/WebCoreSupport/DumpRenderTreeSupportGtk.cpp
@@ -739,9 +739,6 @@ bool DumpRenderTreeSupportGtk::elementDoesAutoCompleteForElementWithId(WebKitWeb
         return false;
 
     HTMLInputElement* inputElement = static_cast<HTMLInputElement*>(coreNode);
-    if (!inputElement)
-        return false;
-
     return inputElement->isTextField() && !inputElement->isPasswordField() && inputElement->shouldAutocomplete();
 }
 
