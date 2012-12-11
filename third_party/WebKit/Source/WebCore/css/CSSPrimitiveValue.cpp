9fd53d787cbc35319b1264c776b86dab5ada6e1f
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-11 22:56:54 +0000

    Clamp out-of-range numbers in CSS https://bugs.webkit.org/show_bug.cgi?id=102735
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Currently when a large number (outside of the supported range)
    is applied from a style rule or set from javascript using
    Element.style we check if it is within the supported range and
    set it to zero if it is not. This is incorrect and confusing.
    
    Change it to clamp values to the supported range. This matches
    the behavior in Gecko.
    
    Test: fast/css/large-numbers.html
    
    * css/CSSPrimitiveValue.cpp:
    (WebCore::CSSPrimitiveValue::computeLength):
    Change to clamp out-of-range values instead of interpreting them
    as zero.
    
    LayoutTests:
    
    Add test verifying that large numbers are handled correctly
    (based on fast/sub-pixel/large-sizes.html).
    Update css/large-number-round-trip and css3/flexbox/flex-algorithm.html
    
    * css3/flexbox/flex-algorithm.html:
    * fast/css/large-numbers-expected.txt: Added.
    * fast/css/large-numbers.html: Copied from LayoutTests/fast/sub-pixel/large-sizes.html.
    * fast/sub-pixel/large-sizes-expected.txt: Removed.
    * fast/sub-pixel/large-sizes.html: Removed.
    * platform/chromium/fast/css/large-number-round-trip-expected.txt:
    * platform/mac/fast/css/large-number-round-trip-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSPrimitiveValue.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSPrimitiveValue.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Counter.h"
 #include "ExceptionCode.h"
 #include "Font.h"
+#include "LayoutUnit.h"
 #include "Node.h"
 #include "Pair.h"
 #include "RGBColor.h"
@@ -53,6 +54,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using namespace WTF;
 
 namespace WebCore {
+    
+// Max/min values for CSS, needs to slightly smaller/larger than the true max/min values to allow for rounding without overflowing.
+// Subtract two (rather than one) to allow for values to be converted to float and back without exceeding the LayoutUnit::max.
+const int maxValueForCssLength = INT_MAX / kFixedPointDenominator - 2;
+const int minValueForCssLength = INT_MIN / kFixedPointDenominator + 2;
 
 static inline bool isValidCSSUnitTypeForDoubleConversion(CSSPrimitiveValue::UnitTypes unitType)
 {
@@ -480,10 +486,9 @@ template<> unsigned CSSPrimitiveValue::computeLength(RenderStyle* style, RenderS
 template<> Length CSSPrimitiveValue::computeLength(RenderStyle* style, RenderStyle* rootStyle, float multiplier, bool computingFontSize)
 {
 #if ENABLE(SUBPIXEL_LAYOUT)
-    double value = computeLengthDouble(style, rootStyle, multiplier, computingFontSize);
-    return Length(static_cast<float>(value > intMaxForLayoutUnit || value < intMinForLayoutUnit ? 0.0 : value), Fixed);
+    return Length(clampTo<float>(computeLengthDouble(style, rootStyle, multiplier, computingFontSize), minValueForCssLength, maxValueForCssLength), Fixed);
 #else
-    return Length(roundForImpreciseConversion<float>(computeLengthDouble(style, rootStyle, multiplier, computingFontSize)), Fixed);
+    return Length(clampTo<float>(roundForImpreciseConversion<float>(computeLengthDouble(style, rootStyle, multiplier, computingFontSize)), minValueForCssLength, maxValueForCssLength), Fixed);
 #endif
 }
 
