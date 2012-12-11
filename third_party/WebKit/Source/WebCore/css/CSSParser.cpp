b5a53d34e7919b44ea98ed444d4a1659a1869c88
alexis@webkit.org <alexis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-11 20:55:52 +0000

    Cleanup: Use consistent naming in CSSParser when dealing with the forward slash operator. https://bugs.webkit.org/show_bug.cgi?id=104699
    Reviewed by Daniel Bates.
    
    http://trac.webkit.org/changeset/136525 added an helper function to
    check if a CSSParserValue is a forward slash operator.
    BorderImageParseContext is also dealing with the forward slash operator
    but its naming is inconsistent with isForwardSlashOperator(). This is
    fixed with this patch where BorderImageParseContext::{allowSlash,
    commitSlash}() is now replaced by BorderImageParseContext::{allowForwardSlashOperator,
    commitForwardSlashOperator}().
    
    No new tests : just a renaming, covered by existing tests..
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::parseFillPosition): A little fixup on the way, use
    isForwardSlashOperator() instead of the current code here.
    (WebCore::BorderImageParseContext::BorderImageParseContext):
    (WebCore::BorderImageParseContext::allowForwardSlashOperator):
    (WebCore::BorderImageParseContext::commitImage):
    (WebCore::BorderImageParseContext::commitImageSlice):
    (WebCore::BorderImageParseContext::commitForwardSlashOperator):
    (WebCore::BorderImageParseContext::commitBorderWidth):
    (WebCore::BorderImageParseContext::commitBorderOutset):
    (WebCore::BorderImageParseContext::commitRepeat):
    (BorderImageParseContext):
    (WebCore::CSSParser::parseBorderImage):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -3885,7 +3885,7 @@ void CSSParser::parseFillPosition(CSSParserValueList* valueList, RefPtr<CSSValue
     unsigned numberOfValues = 0;
     for (unsigned i = valueList->currentIndex(); i < valueList->size(); ++i, ++numberOfValues) {
         CSSParserValue* current = valueList->valueAt(i);
-        if (isComma(current) || !current || (current->unit == CSSParserValue::Operator && current->iValue == '/') || !isPotentialPositionValue(current))
+        if (isComma(current) || !current || isForwardSlashOperator(current) || !isPotentialPositionValue(current))
             break;
     }
 
@@ -6285,7 +6285,7 @@ struct BorderImageParseContext {
     , m_allowImage(true)
     , m_allowImageSlice(true)
     , m_allowRepeat(true)
-    , m_allowSlash(false)
+    , m_allowForwardSlashOperator(false)
     , m_requireWidth(false)
     , m_requireOutset(false)
     {}
@@ -6297,7 +6297,7 @@ struct BorderImageParseContext {
     bool allowImage() const { return m_allowImage; }
     bool allowImageSlice() const { return m_allowImageSlice; }
     bool allowRepeat() const { return m_allowRepeat; }
-    bool allowSlash() const { return m_allowSlash; }
+    bool allowForwardSlashOperator() const { return m_allowForwardSlashOperator; }
 
     bool requireWidth() const { return m_requireWidth; }
     bool requireOutset() const { return m_requireOutset; }
@@ -6307,7 +6307,7 @@ struct BorderImageParseContext {
         m_image = image;
         m_canAdvance = true;
         m_allowCommit = true;
-        m_allowImage = m_allowSlash = m_requireWidth = m_requireOutset = false;
+        m_allowImage = m_allowForwardSlashOperator = m_requireWidth = m_requireOutset = false;
         m_allowImageSlice = !m_imageSlice;
         m_allowRepeat = !m_repeat;
     }
@@ -6315,15 +6315,15 @@ struct BorderImageParseContext {
     {
         m_imageSlice = slice;
         m_canAdvance = true;
-        m_allowCommit = m_allowSlash = true;
+        m_allowCommit = m_allowForwardSlashOperator = true;
         m_allowImageSlice = m_requireWidth = m_requireOutset = false;
         m_allowImage = !m_image;
         m_allowRepeat = !m_repeat;
     }
-    void commitSlash()
+    void commitForwardSlashOperator()
     {
         m_canAdvance = true;
-        m_allowCommit = m_allowImage = m_allowImageSlice = m_allowRepeat = m_allowSlash = false;
+        m_allowCommit = m_allowImage = m_allowImageSlice = m_allowRepeat = m_allowForwardSlashOperator = false;
         if (!m_borderSlice) {
             m_requireWidth = true;
             m_requireOutset = false;
@@ -6336,7 +6336,7 @@ struct BorderImageParseContext {
     {
         m_borderSlice = slice;
         m_canAdvance = true;
-        m_allowCommit = m_allowSlash = true;
+        m_allowCommit = m_allowForwardSlashOperator = true;
         m_allowImageSlice = m_requireWidth = m_requireOutset = false;
         m_allowImage = !m_image;
         m_allowRepeat = !m_repeat;
@@ -6346,7 +6346,7 @@ struct BorderImageParseContext {
         m_outset = outset;
         m_canAdvance = true;
         m_allowCommit = true;
-        m_allowImageSlice = m_allowSlash = m_requireWidth = m_requireOutset = false;
+        m_allowImageSlice = m_allowForwardSlashOperator = m_requireWidth = m_requireOutset = false;
         m_allowImage = !m_image;
         m_allowRepeat = !m_repeat;
     }
@@ -6355,7 +6355,7 @@ struct BorderImageParseContext {
         m_repeat = repeat;
         m_canAdvance = true;
         m_allowCommit = true;
-        m_allowRepeat = m_allowSlash = m_requireWidth = m_requireOutset = false;
+        m_allowRepeat = m_allowForwardSlashOperator = m_requireWidth = m_requireOutset = false;
         m_allowImageSlice = !m_imageSlice;
         m_allowImage = !m_image;
     }
@@ -6388,7 +6388,7 @@ struct BorderImageParseContext {
     bool m_allowImage;
     bool m_allowImageSlice;
     bool m_allowRepeat;
-    bool m_allowSlash;
+    bool m_allowForwardSlashOperator;
 
     bool m_requireWidth;
     bool m_requireOutset;
@@ -6408,8 +6408,8 @@ bool CSSParser::parseBorderImage(CSSPropertyID propId, RefPtr<CSSValue>& result,
     while (CSSParserValue* val = m_valueList->current()) {
         context.setCanAdvance(false);
 
-        if (!context.canAdvance() && context.allowSlash() && isForwardSlashOperator(val))
-            context.commitSlash();
+        if (!context.canAdvance() && context.allowForwardSlashOperator() && isForwardSlashOperator(val))
+            context.commitForwardSlashOperator();
 
         if (!context.canAdvance() && context.allowImage()) {
             if (val->unit == CSSPrimitiveValue::CSS_URI)
