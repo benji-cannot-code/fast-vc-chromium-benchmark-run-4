aeabe0d991279ea26e5b02f66e97e11acc0831cf
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-11 09:05:54 +0000

    [GTK] Remove the Pango backend https://bugs.webkit.org/show_bug.cgi?id=104569
    Reviewed by Daniel Bates.
    
    .:
    
    Always look for Freetype and Harfbuzz. We still depend on Pango for a few
    things, so we cannot yet eliminate our dependency on Pango.
    
    * configure.ac: Always look for FreeType/Harfbuzz.
    
    Source/WebCore:
    
    Remove the Pango backend entirely. The EFL build has no support for it and it's
    not really maintained enough for the GTK+ port, now that HarfbuzzNG shapes complex
    text when Freetype is the active text rendering backend.
    
    No new tests. This does not change behavior.
    
    * GNUmakefile.am: Remove Pango backend options and move Freetype options to the common list.
    * GNUmakefile.list.am: Ditto.
    * platform/graphics/FontPlatformData.h:
    * platform/graphics/pango/FontCachePango.cpp: Removed.
    * platform/graphics/pango/FontCustomPlatformDataPango.cpp: Removed.
    * platform/graphics/pango/FontPango.cpp: Removed.
    * platform/graphics/pango/FontPlatformData.h: Removed.
    * platform/graphics/pango/FontPlatformDataPango.cpp: Removed.
    * platform/graphics/pango/GlyphPageTreeNodePango.cpp: Removed.
    * platform/graphics/pango/PangoUtilities.cpp: Removed.
    * platform/graphics/pango/PangoUtilities.h: Removed.
    * platform/graphics/pango/SimpleFontDataPango.cpp: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137263 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/FontPlatformData.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/FontPlatformData.h
@@ -32,10 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wince/FontPlatformData.h"
 #elif PLATFORM(WX)
 #include "wx/FontPlatformData.h"
-#elif (PLATFORM(EFL) || PLATFORM(GTK)) && USE(FREETYPE)
+#elif PLATFORM(EFL) || PLATFORM(GTK)
 #include "freetype/FontPlatformData.h"
-#elif (PLATFORM(EFL) || PLATFORM(GTK)) && USE(PANGO)
-#include "pango/FontPlatformData.h"
 #else
 
 #ifndef FontPlatformData_h
