d3dbff38d08a9180054f4cab324c30e87ec56efd
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 18:47:49 +0000

    Ensure that the Inspect Element operation when performed on Windows 8 Metro ASH opens a docked dev tools window.
    Currently if this operation is performed on Windows 8 ASH we end up opening the Dev tools window in desktop mode which
    is incorrect. This happens because we attempt to find the inspected WebContents instance in the desktop browser list.
    
    Fix is to find the inspected webcontents in the desktop and the ASH browser lists as they can both be active concurrently
    for Windows 8.
    
    BUG=151718
    R=sky
    Review URL: https://codereview.chromium.org/11519012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172360 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/debugger/devtools_window.h
+++ chrome/browser/debugger/devtools_window.h
@@ -29,6 +29,10 @@ namespace base {
 class Value;
 }
 
+namespace chrome {
+class BrowserListImpl;
+}
+
 namespace content {
 class DevToolsAgentHost;
 class DevToolsClientHost;
@@ -120,6 +124,10 @@ class DevToolsWindow : private content::NotificationObserver,
 
   void CreateDevToolsBrowser();
   bool FindInspectedBrowserAndTabIndex(Browser**, int* tab);
+  bool FindInspectedBrowserAndTabIndexFromBrowserList(
+    chrome::BrowserListImpl* browser_list,
+    Browser** browser,
+    int* tab);
   BrowserWindow* GetInspectedBrowserWindow();
   bool IsInspectedBrowserPopupOrPanel();
   void UpdateFrontendDockSide();
