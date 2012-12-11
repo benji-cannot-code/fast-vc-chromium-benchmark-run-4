6ea707f2f62923ab01f07cf092708b2bf88aedfd
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 11:05:34 +0000

    This CL is part of the code in https://codereview.chromium.org/11316275/ to refactor the media UI. This patch does # split CONTENT_SETTINGS_TYPE_MEDIASTREAM into CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC and CONTENT_SETTINGS_TYPE_MEDIASTREAM_CAMERA # use ContentSetting as value for CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC/CAMERA, deprecate CONTENT_SETTINGS_TYPE_MEDIASTREAM compound values. # CONTENT_SETTINGS_TYPE_MEDIASTREAM will be used only for default setting. # On the content setting media exceptions, show two columns, one for audio and one for video.
    BUG=163940
    TEST=browser_tests, manual tests like
    go to content setting media, use webrtc demo like https://webrtc-demos.appspot.com/html/pc1.html to make an exception, verify the exception show correctly in content settting.
    
    Review URL: https://chromiumcodereview.appspot.com/11316306
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172302 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/host_content_settings_map.cc
+++ chrome/browser/content_settings/host_content_settings_map.cc
@@ -349,6 +349,12 @@ bool HostContentSettingsMap::IsSettingAllowedForType(
     return false;
   }
 
+  // We don't support ALLOW for media default setting.
+  if (content_type == CONTENT_SETTINGS_TYPE_MEDIASTREAM &&
+      setting == CONTENT_SETTING_ALLOW) {
+    return false;
+  }
+
   // DEFAULT, ALLOW and BLOCK are always allowed.
   if (setting == CONTENT_SETTING_DEFAULT ||
       setting == CONTENT_SETTING_ALLOW ||
@@ -364,6 +370,8 @@ bool HostContentSettingsMap::IsSettingAllowedForType(
     case CONTENT_SETTINGS_TYPE_INTENTS:
     case CONTENT_SETTINGS_TYPE_MOUSELOCK:
     case CONTENT_SETTINGS_TYPE_MEDIASTREAM:
+    case CONTENT_SETTINGS_TYPE_MEDIASTREAM_MIC:
+    case CONTENT_SETTINGS_TYPE_MEDIASTREAM_CAMERA:
     case CONTENT_SETTINGS_TYPE_PPAPI_BROKER:
       return setting == CONTENT_SETTING_ASK;
     default:
@@ -374,9 +382,9 @@ bool HostContentSettingsMap::IsSettingAllowedForType(
 // static
 bool HostContentSettingsMap::ContentTypeHasCompoundValue(
     ContentSettingsType type) {
-  // Values for content type CONTENT_SETTINGS_TYPE_AUTO_SELECT_CERTIFICATE are
-  // of type dictionary/map. Compound types like dictionaries can't be mapped to
-  // the type |ContentSetting|.
+  // Values for content type CONTENT_SETTINGS_TYPE_AUTO_SELECT_CERTIFICATE and
+  // CONTENT_SETTINGS_TYPE_MEDIASTREAM are of type dictionary/map. Compound
+  // types like dictionaries can't be mapped to the type |ContentSetting|.
   return (type == CONTENT_SETTINGS_TYPE_AUTO_SELECT_CERTIFICATE ||
           type == CONTENT_SETTINGS_TYPE_MEDIASTREAM);
 }
@@ -461,7 +469,6 @@ void HostContentSettingsMap::MigrateObsoleteClearOnExitPref() {
 
   prefs_->SetBoolean(prefs::kContentSettingsClearOnExitMigrated, true);
 }
-
 
 void HostContentSettingsMap::AddSettingsForOneType(
     const content_settings::ProviderInterface* provider,
