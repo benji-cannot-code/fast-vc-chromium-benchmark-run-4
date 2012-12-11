269fe5aea8df452ae66c1d30d2cd200837364d8f
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 20:18:03 +0000

    Avoid drag cursor flicker in Chrome OS Files app
    BUG=160800
    
    
    Review URL: https://chromiumcodereview.appspot.com/11458010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172388 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_transfer_controller.js
+++ chrome/browser/resources/file_manager/js/file_transfer_controller.js
@@ -255,6 +255,7 @@ FileTransferController.prototype = {
   },
 
   onDragEnterList_: function(list, event) {
+    event.preventDefault();  // Required to prevent the cursor flicker.
     this.lastEnteredTarget_ = event.target;
     var item = list.getListItemAncestor(event.target);
     item = item && list.isItem(item) ? item : null;
@@ -271,6 +272,7 @@ FileTransferController.prototype = {
   },
 
   onDragEnterBreadcrumbs_: function(breadcrumbsContainer, event) {
+    event.preventDefault();  // Required to prevent the cursor flicker.
     this.lastEnteredTarget_ = event.target;
     var path = breadcrumbsContainer.getTargetPath(event);
     if (!path)
