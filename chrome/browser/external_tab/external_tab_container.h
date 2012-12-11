d3c826eeb72b7ae5c1037d83df40eb1752cdebe3
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 00:32:10 +0000

    Remove TabContents from Chrome Frame.
    BUG=107201
    TEST=no visible change
    
    
    Review URL: https://chromiumcodereview.appspot.com/11474051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172207 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/external_tab/external_tab_container.h
+++ chrome/browser/external_tab/external_tab_container.h
@@ -15,7 +15,6 @@ class AutomationProvider;
 class AutomationResourceMessageFilter;
 class GURL;
 class Profile;
-class TabContents;
 
 namespace content {
 class WebContents;
--- chrome/browser/ui/views/external_tab_container_win.h
+++ chrome/browser/ui/views/external_tab_container_win.h
@@ -32,7 +32,6 @@ class AutomationProvider;
 class Browser;
 class Profile;
 class TabContentsContainer;
-class TabContents;
 class RenderViewContextMenuViews;
 struct NavigationInfo;
 
@@ -270,7 +269,7 @@ class ExternalTabContainerWin : public ExternalTabContainer,
   // ExternalTabContainerWin.
   void SetupExternalTabView();
 
-  scoped_ptr<TabContents> tab_contents_;
+  scoped_ptr<content::WebContents> web_contents_;
   scoped_refptr<AutomationProvider> automation_;
 
   content::NotificationRegistrar registrar_;
