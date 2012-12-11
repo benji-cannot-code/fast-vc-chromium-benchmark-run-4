876667b4453a335016ffaf14d65b23f8fb00b1cf
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-11 03:23:10 +0000

    google_apis: Rename DocumentFeed to ResourceList
    --
    This is a reland of crrev.com/172038, which was reverted due to clang
    build failures. I thought CQ would prevent build failures like this,
    but apparently I was wrong...
    --
    
    DocumentFeed was a misnomer, as this class represented a resource list.
    
    Along the way, add more useful comments in gdata_wapi_parser.h, and
    fix wrong variable names like 'doc'.
    
    BUG=164090
    TEST=none
    
    Review URL: https://codereview.chromium.org/11506018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172249 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_browser_event_router.cc
+++ chrome/browser/chromeos/extensions/file_browser_event_router.cc
@@ -434,7 +434,7 @@ void FileBrowserEventRouter::OnDirectoryChanged(
   HandleFileWatchNotification(directory_path, false);
 }
 
-void FileBrowserEventRouter::OnDocumentFeedFetched(
+void FileBrowserEventRouter::OnResourceListFetched(
     int num_accumulated_entries) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
