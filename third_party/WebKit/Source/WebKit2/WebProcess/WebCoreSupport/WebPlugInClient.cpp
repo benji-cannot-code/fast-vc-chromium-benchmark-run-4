086c361119596832f0f86280cd1072a06e56917e
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-11 03:34:03 +0000

    Build fix.
    * WebProcess/WebCoreSupport/WebPlugInClient.cpp:
    (WebKit::WebPlugInClient::WebPlugInClient): Remove unneeded m_page variable.
    * WebProcess/WebCoreSupport/WebPlugInClient.h:
    (WebPlugInClient):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137241 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebPlugInClient.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebPlugInClient.cpp
@@ -34,8 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-WebPlugInClient::WebPlugInClient(WebPage* page)
-    : m_page(page)
+WebPlugInClient::WebPlugInClient(WebPage*)
 {
 }
 
