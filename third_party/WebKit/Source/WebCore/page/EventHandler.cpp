a28ff16f624439c3a20eadd84a49adf84c669985
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-11 13:00:53 +0000

    Web Inspector: Duplicate emulated touch events in iframe https://bugs.webkit.org/show_bug.cgi?id=104653
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    Synthetic touch events should be dispatched only on the frame containing its target, not on its ancestor frames,
    up to the top frame.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::dispatchSyntheticTouchEventIfEnabled):
      Do not dispatch a synthetic touch event on the current frame if its target is in a subframe.
    
    LayoutTests:
    
    The test incorrectly expected two synthetic touchend events from an iframe,
    while only one touchstart and one touchend should be dispatched.
    
    * fast/events/touch/emulated-touch-iframe-expected.txt:
    * fast/events/touch/emulated-touch-iframe.html:
    * fast/events/touch/resources/emulated-touch-iframe2.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -4059,6 +4059,12 @@ bool EventHandler::dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent
     if (eventType == PlatformEvent::MouseMoved && !m_touchPressed)
         return false;
 
+    HitTestRequest request(HitTestRequest::Active);
+    MouseEventWithHitTestResults mev = prepareMouseEvent(request, event);
+
+    if (mev.scrollbar() || subframeForHitTestResult(mev))
+        return false;
+
     SyntheticSingleTouchEvent touchEvent(event);
     return handleTouchEvent(touchEvent);
 }
