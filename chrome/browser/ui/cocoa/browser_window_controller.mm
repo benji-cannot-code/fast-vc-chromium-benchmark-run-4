7136f144affd423d35f5777c7a0fd300a28b3329
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 17:48:25 +0000

    Switch to -magnification for Mac pinch-to-zoom
    -deltaZ was deprecated in 10.6, and replaced with -magnification (which
    has steps that are 500x smaller).
    
    BUG=144893
    TEST=Pinch to zoom should still work as before on Mac
    
    Review URL: https://chromiumcodereview.appspot.com/11543021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1678,8 +1678,7 @@ enum {
   [[[[self window] contentView] superview] addSubview:view];
 }
 
-// Documented in 10.6+, but present starting in 10.5. Called when we get a
-// three-finger swipe.
+// Called when we get a three-finger swipe.
 - (void)swipeWithEvent:(NSEvent*)event {
   CGFloat deltaX = [event deltaX];
   CGFloat deltaY = [event deltaY];
@@ -1707,12 +1706,11 @@ enum {
   }
 }
 
-// Documented in 10.6+, but present starting in 10.5. Called repeatedly during
-// a pinch gesture, with incremental change values.
+// Called repeatedly during a pinch gesture, with incremental change values.
 - (void)magnifyWithEvent:(NSEvent*)event {
   // The deltaZ difference necessary to trigger a zoom action. Derived from
   // experimentation to find a value that feels reasonable.
-  const float kZoomStepValue = 300;
+  const float kZoomStepValue = 0.6;
 
   // Find the (absolute) thresholds on either side of the current zoom factor,
   // then convert those to actual numbers to trigger a zoom in or out.
@@ -1727,7 +1725,7 @@ enum {
   float zoomOutThreshold = (currentZoomStepDelta_ <= 0) ? -nextStep : backStep;
 
   unsigned int command = 0;
-  totalMagnifyGestureAmount_ += [event deltaZ];
+  totalMagnifyGestureAmount_ += [event magnification];
   if (totalMagnifyGestureAmount_ > zoomInThreshold) {
     command = IDC_ZOOM_PLUS;
   } else if (totalMagnifyGestureAmount_ < zoomOutThreshold) {
