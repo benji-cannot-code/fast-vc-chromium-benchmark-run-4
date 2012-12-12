4b73e3aea00c040589e767473666f43577853dbd
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 06:39:49 +0000

    Specify an id for settings app, so that it only launches once.
    And, if already launched, focus the window. http://crrev.com/171617
    gives us exactly this feature, enabled by specifying a window id for
    the settings app.
    
    BUG=163774
    TEST=In the App Launcher, try launching two copies of the settings app.
         It should only launch once, and focus the window on subsequent
         attempts.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11526002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172544 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/settings_app/settings_app.js
+++ chrome/browser/resources/settings_app/settings_app.js
@@ -6,5 +6,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 chrome.app.runtime.onLaunched.addListener(function() {
   chrome.app.window.create(
       'chrome://settings-frame/options_settings_app.html',
-      {'height': 550, 'width': 660});
+      {'id': 'settings_app', 'height': 550, 'width': 660});
 });
