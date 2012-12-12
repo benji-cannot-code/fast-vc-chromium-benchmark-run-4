a9b3965b5a94adb26511a05f13a574d7efa77f6c
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-12 19:32:12 +0000

    [Chromium] Remove all references to sharedWorkerRepository() https://bugs.webkit.org/show_bug.cgi?id=104704
    Reviewed by James Robinson.
    
    Now that Chromium calls setSharedWorkerRepository upon
    initializing WebKit (
    https://chromiumcodereview.appspot.com/10990121/ ) and no longer
    tries to override sharedWorkerRepository, it is safe to remove
    all references to this function.
    
    * public/platform/WebKitPlatformSupport.h:
    (WebKit):
    (WebKit::WebKitPlatformSupport::idbFactory):
    * src/SharedWorkerRepository.cpp:
    (WebKit::sharedWorkerRepository):
    (WebCore::SharedWorkerRepository::isAvailable):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137490 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h
+++ third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebIDBFactory; // FIXME: Does this belong in platform?
-class WebSharedWorkerRepository; // FIXME: Does this belong in platform?
 
 // FIXME: Eventually all these API will need to move to WebKit::Platform.
 class WebKitPlatformSupport : public Platform {
@@ -45,11 +44,6 @@ public:
     // Indexed Database ----------------------------------------------------
 
     virtual WebIDBFactory* idbFactory() { return 0; }
-
-
-    // Shared Workers ------------------------------------------------------
-
-    virtual WebSharedWorkerRepository* sharedWorkerRepository() { return 0; }
 
 protected:
     ~WebKitPlatformSupport() { }
