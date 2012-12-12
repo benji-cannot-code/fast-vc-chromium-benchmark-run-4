6d7baa589305e06f7786e970d96d6b828ffebdd3
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 01:45:51 +0000

    Clearing a local reference to the I/O thread's task runner to makre sure the daemon can be stopped. This CL also switches the daemon to use base::RunLoop instead of using the message loop directly.
    Review URL: https://chromiumcodereview.appspot.com/11512008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172492 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/win/host_service.h
+++ remoting/host/win/host_service.h
@@ -15,7 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/win/wts_console_monitor.h"
 
 class CommandLine;
-class MessageLoop;
+
+namespace base {
+class SingleThreadTaskRunner;
+}  // namespace base
 
 namespace remoting {
 
@@ -48,11 +51,7 @@ class HostService : public WtsConsoleMonitor {
   void OnSessionChange();
 
   // Creates the process launcher.
-  void CreateLauncher(scoped_refptr<AutoThreadTaskRunner> io_task_runner);
-
-  // This is a common entry point to the main service loop called by both
-  // RunAsService() and RunInConsole().
-  void RunMessageLoop(MessageLoop* message_loop);
+  void CreateLauncher(scoped_refptr<AutoThreadTaskRunner> task_runner);
 
   // Runs the binary specified by the command line, elevated.
   int Elevate();
@@ -96,7 +95,7 @@ class HostService : public WtsConsoleMonitor {
   scoped_ptr<Stoppable> child_;
 
   // Service message loop.
-  scoped_refptr<AutoThreadTaskRunner> main_task_runner_;
+  scoped_refptr<base::SingleThreadTaskRunner> main_task_runner_;
 
   // The action routine to be executed.
   int (HostService::*run_routine_)();
