4471352bd7a7d852b96f98ff47faa9bd69651e63
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-12 01:15:00 +0000

    -webkit-svg-shadow radius changes don't cause children's boundaries to be recomputed https://bugs.webkit.org/show_bug.cgi?id=104722 <rdar://problem/12821080>
    Reviewed by Simon Fraser.
    
    Changes to -webkit-svg-shadow currently cause a relayout of the directly affected renderer
    and its parents, but not its children. However, children have the shadow radius
    baked into their cached boundaries, so these need to be invalidated.
    
    Test: svg/repaint/repaint-webkit-svg-shadow-container.html
    
    * rendering/RenderObject.h: Expose needsBoundariesUpdate().
    * rendering/svg/RenderSVGContainer.h: Expose needsBoundariesUpdate().
    * rendering/svg/RenderSVGImage.h: Expose needsBoundariesUpdate().
    * rendering/svg/RenderSVGRoot.h: Expose needsBoundariesUpdate().
    * rendering/svg/RenderSVGShape.h: Expose needsBoundariesUpdate().
    * rendering/svg/SVGRenderSupport.cpp:
    (WebCore::SVGRenderSupport::layoutChildren): If the renderer has a shadow and
    is in need of a boundaries update, mark children as needing boundaries updates too.
    
    Add a text repaint test that ensures that a child (a path, in this case)
    recomputes its bounds after its parent's shadow is expanded.
    
    * svg/repaint/repaint-webkit-svg-shadow-container-expected.txt: Added.
    * svg/repaint/repaint-webkit-svg-shadow-container.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGShape.h
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGShape.h
@@ -73,6 +73,7 @@ public:
 
     void setNeedsShapeUpdate() { m_needsShapeUpdate = true; }
     virtual void setNeedsBoundariesUpdate() { m_needsBoundariesUpdate = true; }
+    virtual bool needsBoundariesUpdate() OVERRIDE { return m_needsBoundariesUpdate; }
     virtual void setNeedsTransformUpdate() { m_needsTransformUpdate = true; }
     virtual void fillShape(GraphicsContext*) const;
     virtual void strokeShape(GraphicsContext*) const;
