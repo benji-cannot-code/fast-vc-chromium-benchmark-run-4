1a395ddd1f5921baf96f9d5b17e7525a77a5652a
nghanavatian@rim.com <nghanavatian@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-12 01:30:47 +0000

    [BlackBerry] Redraw spell check dialog after rotation. https://bugs.webkit.org/show_bug.cgi?id=104737
    Reviewed by Rob Buis.
    
    After rotating the device, all dialogs are dismissed. We should
    restore the spell check dialog as it is still valid after
    rotation and the user will expect it there.
    
    Internally reviewed by Mike Fenton and Gen Mak.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::applyPendingOrientationIfNeeded):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -3605,6 +3605,9 @@ void WebPage::applyPendingOrientationIfNeeded()
 {
     if (d->m_pendingOrientation != -1)
         d->setScreenOrientation(d->m_pendingOrientation);
+
+    // After rotation, we should redraw the dialog box instead of just moving it since rotation dismisses all dialogs.
+    d->m_inputHandler->redrawSpellCheckDialogIfRequired(false /* shouldMoveDialog */);
 }
 
 void WebPagePrivate::setViewportSize(const IntSize& transformedActualVisibleSize, bool ensureFocusElementVisible)
