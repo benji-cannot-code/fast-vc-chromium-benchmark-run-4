9819916d3469ff25a6a4540a5ae9c554e8b3bfd0
abecsi@webkit.org <abecsi@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-12 19:44:24 +0000

    [Qt][WK2] Fix the build on Mac
    Unreviewed build fix.
    
    Add missing virtual destructor with empty
    definition to fix missing vtable error
    in test util's LoadStartedCatcher.
    
    * UIProcess/API/qt/tests/util.h:
    (LoadStartedCatcher::~LoadStartedCatcher):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/tests/util.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/tests/util.h
@@ -43,6 +43,7 @@ class LoadStartedCatcher : public QObject {
     Q_OBJECT
 public:
     LoadStartedCatcher(QQuickWebView* webView);
+    virtual ~LoadStartedCatcher() { }
 public Q_SLOTS:
     void onLoadingChanged(QWebLoadRequest* loadRequest);
 Q_SIGNALS:
