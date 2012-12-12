4927b78045119e505f79ce9360554440944bb697
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 00:36:10 +0000

    Add #include <vector> in two places
    Required to build with libc++.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/11553009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172472 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/intents/cws_intents_registry.h
+++ chrome/browser/intents/cws_intents_registry.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_INTENTS_CWS_INTENTS_REGISTRY_H_
 #define CHROME_BROWSER_INTENTS_CWS_INTENTS_REGISTRY_H_
 
+#include <vector>
+
 #include "base/callback_forward.h"
 #include "base/gtest_prod_util.h"
 #include "base/hash_tables.h"
