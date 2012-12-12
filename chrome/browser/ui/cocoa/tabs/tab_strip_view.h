8cdf848632a85672fe8217353905cd59cb433f61
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-12 02:06:15 +0000

    Revert 172478
    Broke a few Mac testers:
    
    [ RUN      ] BrowserWindowFullScreenControllerTest.TestFullscreen
    2012-12-11 17:32:02.376 unit_tests[1102:903] *** Assertion failure in -[NSWindow setTitle:], /SourceCache/AppKit/AppKit-1038.36/AppKit.subproj/NSWindow.m:2036
    2012-12-11 17:32:02.377 unit_tests[1102:903] An uncaught exception was raised
    2012-12-11 17:32:02.378 unit_tests[1102:903] Invalid parameter not satisfying: aString != nil
    2012-12-11 17:32:02.399 unit_tests[1102:903] *** Terminating app due to uncaught exception 'NSInternalInconsistencyException', reason: 'Invalid parameter not satisfying: aString != nil'
    *** Call stack at first throw:
    (
      0   CoreFoundation                      0x998886ca __raiseError + 410
      1   libobjc.A.dylib                     0x95bd95a9 objc_exception_throw + 56
      2   CoreFoundation                      0x998883f8 +[NSException raise:format:arguments:] + 136
      3   Foundation                          0x9a8bb6e7 -[NSAssertionHandler handleFailureInMethod:object:file:lineNumber:description:] + 116
      4   AppKit                              0x9149f441 -[NSWindow setTitle:] + 156
      5   unit_tests                          0x19cf662a 0x0 + 433022506
      6   unit_tests                          0x19cf6af2 0x0 + 433023730
      7   unit_tests                          0x19cee921 0x0 + 432990497
      8   unit_tests                          0x19cee967 0x0 + 432990567
      9   unit_tests                          0x18cf3bee 0x0 + 416234478
      10  unit_tests                          0x1ab408f2 0x0 + 448006386
      11  unit_tests                          0x1ab39277 0x0 + 447976055
      12  unit_tests                          0x1ab397f4 0x0 + 447977460
      13  unit_tests                          0x1ab39c0b 0x0 + 447978507
      14  unit_tests                          0x1ab3f09b 0x0 + 448000155
      15  unit_tests                          0x1ab40e62 0x0 + 448007778
      16  unit_tests                          0x1ab3ecbf 0x0 + 447999167
      17  unit_tests                          0x1a8080d0 0x0 + 444629200
      18  unit_tests                          0x1b5df673 0x0 + 459142771
      19  unit_tests                          0x1ac667b7 0x0 + 449210295
      20  unit_tests                          0x1700aaf5 0x0 + 385919733
      21  ???                                 0x00000003 0x0 + 3
    )
    
    > mac: Remove BrowserWindow.xib
    > 
    > It doesn't do any interesting layout, so this isn't worth a xib.
    > 
    > BUG=none
    > TEST=Browser window looks the same.
    > Pop out and back in a gmail chat mole to check popup windows.
    > Make sure http://crbug.com/81446 still isn't happening.
    > 
    > Review URL: https://codereview.chromium.org/11506017
    
    TBR=thakis@chromium.org
    Review URL: https://codereview.chromium.org/11555008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172497 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_strip_view.h
+++ chrome/browser/ui/cocoa/tabs/tab_strip_view.h
@@ -26,7 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // Handles being a drag-and-drop target.
   scoped_nsobject<URLDropTargetHandler> dropHandler_;
 
-  scoped_nsobject<NewTabButton> newTabButton_;
+  // Weak; the following come from the nib.
+  NewTabButton* newTabButton_;
 
   // Whether the drop-indicator arrow is shown, and if it is, the coordinate of
   // its tip.
