d14e15baac29ebeca417f4ad6884e9e1622e273a
yoichio@chromium.org <yoichio@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 09:09:19 +0000

    Delay for submenu in bookmarks showing follows system value
    BUG=78179
    
    
    Review URL: https://chromiumcodereview.appspot.com/11529005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172840 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_config.h
+++ ui/views/controls/menu/menu_config.h
@@ -125,6 +125,10 @@ struct VIEWS_EXPORT MenuConfig {
 
   const ui::NativeTheme* native_theme;
 
+  // Delay, in ms, between when menus are selected or moused over and the menu
+  // appears.
+  int show_delay;
+
  private:
   // Configures a MenuConfig as appropriate for the current platform.
   void Init(const ui::NativeTheme* theme);
