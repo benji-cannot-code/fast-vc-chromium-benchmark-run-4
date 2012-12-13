7302839c610f62c399e8920fedc03b9883b464fa
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 18:05:56 +0000

    Fixed UI move from the error screen to the signin screen.
    BUG=164832
    TEST=manual tests on Lumpy and Linux ChromeOS
    
    
    Review URL: https://chromiumcodereview.appspot.com/11573007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172898 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_error_message.js
+++ chrome/browser/resources/chromeos/login/screen_error_message.js
@@ -184,9 +184,8 @@ cr.define('login', function() {
     /**
      * Shows/hides offline message.
      * @param {boolean} visible True to show offline message.
-     * @private
      */
-    showOfflineMessage_: function(visible) {
+    showOfflineMessage: function(visible) {
       var offlineMessage = this;
       var currentScreen = Oobe.getInstance().currentScreen;
 
@@ -252,7 +251,7 @@ cr.define('login', function() {
    * @param {boolean} visible True to show offline message.
    */
   ErrorMessageScreen.showOfflineMessage = function(visible) {
-    $('error-message').showOfflineMessage_(visible);
+    $('error-message').showOfflineMessage(visible);
   };
 
   ErrorMessageScreen.onBeforeShow = function(lastNetworkType) {
