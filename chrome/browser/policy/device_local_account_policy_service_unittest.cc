a36308f126c06e9ef57ec59c0f6fc466fc98a9ae
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 15:43:05 +0000

    Restrict extensions to hosted_apps for public accounts.
    BUG=chromium:165662
    TEST=Try installing an extension or packaged app in a public account, observe failure.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11555020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172881 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/device_local_account_policy_service_unittest.cc
+++ chrome/browser/policy/device_local_account_policy_service_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/chromeos/settings/device_settings_test_helper.h"
 #include "chrome/browser/policy/cloud_policy_client.h"
 #include "chrome/browser/policy/cloud_policy_constants.h"
@@ -54,6 +55,13 @@ class DeviceLocalAccountPolicyServiceTest
                              POLICY_LEVEL_MANDATORY,
                              POLICY_SCOPE_USER,
                              Value::CreateBooleanValue(true));
+    scoped_ptr<base::ListValue> allowed_extension_types(new base::ListValue());
+    allowed_extension_types->AppendString("hosted_app");
+    expected_policy_map_.Set(key::kExtensionAllowedTypes,
+                             POLICY_LEVEL_MANDATORY,
+                             POLICY_SCOPE_USER,
+                             allowed_extension_types.release());
+
     // Explicitly set value.
     expected_policy_map_.Set(key::kDisableSpdy,
                              POLICY_LEVEL_MANDATORY,
@@ -422,13 +430,15 @@ TEST_F(DeviceLocalAccountPolicyProviderTest, Policy) {
       Value::CreateBooleanValue(false));
   EXPECT_TRUE(expected_policy_bundle.Equals(provider_.policies()));
 
-  // Any values set for the |ShelfAutoHideBehavior| and |ShowLogoutButtonInTray|
-  // policies should be overridden.
+  // Any values set for the |ShelfAutoHideBehavior|, |ShowLogoutButtonInTray|
+  // and |ExtensionAllowedTypes| policies should be overridden.
   EXPECT_CALL(provider_observer_, OnUpdatePolicy(&provider_)).Times(AtLeast(1));
   device_local_account_policy_.payload().mutable_shelfautohidebehavior()->
       set_value("Always");
   device_local_account_policy_.payload().mutable_showlogoutbuttonintray()->
       set_value(false);
+  device_local_account_policy_.payload().mutable_extensionallowedtypes()->
+      mutable_value()->mutable_entries()->Clear();
   device_local_account_policy_.Build();
   device_settings_test_helper_.set_device_local_account_policy_blob(
       PolicyBuilder::kFakeUsername, device_local_account_policy_.GetBlob());
@@ -478,6 +488,7 @@ TEST_F(DeviceLocalAccountPolicyProviderTest, RefreshPolicies) {
   EXPECT_CALL(mock_device_management_service_, CreateJob(_))
       .WillRepeatedly(
           mock_device_management_service_.FailJob(DM_STATUS_REQUEST_FAILED));
+  EXPECT_CALL(mock_device_management_service_, StartJob(_, _, _, _, _, _, _));
   service_.Connect(&mock_device_management_service_);
   FlushDeviceSettings();
   Mock::VerifyAndClearExpectations(&mock_device_management_service_);
