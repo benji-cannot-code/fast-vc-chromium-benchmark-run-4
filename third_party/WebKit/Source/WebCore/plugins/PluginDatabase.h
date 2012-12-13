dccc2ba22a7ee16ddf8ac860657badc04dbe513e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 19:23:23 +0000

    Allow plugins to be disabled by shared library filename https://bugs.webkit.org/show_bug.cgi?id=101274
    Patch by Parth Patel  <parpatel@rim.com>, Max Feil <mfeil@rim.com> on 2012-12-13
    Reviewed by Antonio Gomes.
    
    Source/WebCore:
    
    This fix makes PluginDatabase changes general so that other ports
    can use this mechanism if they wish.
    
    Layout test not included with this patch because there
    is currently no framework within LayoutTests to allow calls
    from an external application via platform API code (such as the
    WebView API).
    
    * plugins/PluginDatabase.cpp:
    (WebCore::PluginDatabase::fileExistsAndIsNotDisabled):
    (WebCore):
    (WebCore::PluginDatabase::getDeletedPlugins):
    (WebCore::PluginDatabase::removeDisabledPluginFile):
    (WebCore::PluginDatabase::addDisabledPluginFile):
    (WebCore::PluginDatabase::getPluginPathsInDirectories):
    * plugins/PluginDatabase.h:
    (PluginDatabase):
    
    Source/WebKit/blackberry:
    
    Mechanism allows plugins to be disabled by specifying their shared library filenames.
    Once disabled, a plugin's shared library should never be loaded.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPage::setExtraPluginDirectory):
    (WebKit):
    (BlackBerry::WebKit::WebPage::updateDisabledPluginFiles):
    * Api/WebPage.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137627 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/PluginDatabase.h
+++ third_party/WebKit/Source/WebCore/plugins/PluginDatabase.h
@@ -76,6 +76,8 @@ namespace WebCore {
             m_pluginDirectories = directories;
         }
 
+        bool removeDisabledPluginFile(const String& fileName);
+        bool addDisabledPluginFile(const String& fileName);
         static Vector<String> defaultPluginDirectories();
         Vector<String> pluginDirectories() const { return m_pluginDirectories; }
 
@@ -90,6 +92,7 @@ namespace WebCore {
     private:
         void getPluginPathsInDirectories(HashSet<String>&) const;
         void getDeletedPlugins(PluginSet&) const;
+        bool fileExistsAndIsNotDisabled(const String&) const;
 
         // Returns whether the plugin was actually added or not (it won't be added if it's a duplicate of an existing plugin).
         bool add(PassRefPtr<PluginPackage>);
@@ -99,6 +102,7 @@ namespace WebCore {
         void updatePersistentMetadataCache();
 #endif
 
+        HashSet<String> m_disabledPluginFiles;
         Vector<String> m_pluginDirectories;
         HashSet<String> m_registeredMIMETypes;
         PluginSet m_plugins;
