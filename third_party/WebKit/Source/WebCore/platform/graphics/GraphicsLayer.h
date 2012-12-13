46864b47c6266cf5beced34ce106a9320d874464
roger_fong@apple.com <roger_fong@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 02:33:52 +0000

    Include Apple Windows in GraphicsLayer::supportsBackgroundColorContent(). https://bugs.webkit.org/show_bug.cgi?id=104852.
    Reviewed by Simon Fraser.
    
    * platform/graphics/GraphicsLayer.h:
    (WebCore::GraphicsLayer::supportsBackgroundColorContent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GraphicsLayer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/GraphicsLayer.h
@@ -428,7 +428,7 @@ public:
 
     static bool supportsBackgroundColorContent()
     {
-#if PLATFORM(MAC) || USE(TEXTURE_MAPPER)
+#if USE(CA) || USE(TEXTURE_MAPPER)
         return true;
 #else
         return false;
