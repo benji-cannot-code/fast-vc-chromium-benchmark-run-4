22694ca02f32b0d66663139df9b3e84fd29b2a21
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 14:04:37 +0000

    Remove support for gdata from the Python test server and net/test.
    Drive related classes' tests do not use the python http test server anymore. Therefore, support for it is removed in this patch.
    
    BUG=164071
    
    
    Review URL: https://chromiumcodereview.appspot.com/11554018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172874 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/base_test_server.cc
+++ net/test/base_test_server.cc
@@ -113,7 +113,6 @@ std::string BaseTestServer::SSLOptions::GetOCSPArgument() const {
 }
 
 const char BaseTestServer::kLocalhost[] = "127.0.0.1";
-const char BaseTestServer::kGDataAuthToken[] = "testtoken";
 
 BaseTestServer::BaseTestServer(Type type, const std::string& host)
     : type_(type),
@@ -148,7 +147,6 @@ std::string BaseTestServer::GetScheme() const {
   switch (type_) {
     case TYPE_FTP:
       return "ftp";
-    case TYPE_GDATA:
     case TYPE_HTTP:
     case TYPE_SYNC:
       return "http";
