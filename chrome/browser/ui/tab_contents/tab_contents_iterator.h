8adce1068a82de5db45af355febfae8845c15913
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 00:34:03 +0000

    Ensure that the TabContentsIterator class iterates over the desktop and ASH browser lists.
    Currently the TabContentsIterator iterates over the default browser list which is the active browser list
    in desktop mode. On Windows 8 chrome ASH we can have multiple browser lists active, the desktop browser list
    and the ASH browser list, which basically means that the TabContentsIterator functionality is broken for Windows 8
    Chrome ASH.
    
    Fix is to add support in the TabContentsIterator class to iterate over both browser lists, i.e. the desktop and
    ASH ones.
    
    Added a helper function to the browser_finder helper to retrieve the host desktop type for a WebContents passed in.
    
    BUG=151718
    R=cpu,sky
    Review URL: https://codereview.chromium.org/11316340
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172724 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tab_contents/tab_contents_iterator.h
+++ chrome/browser/ui/tab_contents/tab_contents_iterator.h
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <set>
 
 #include "base/basictypes.h"
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_list_impl.h"
+#include "chrome/browser/ui/host_desktop.h"
 
 namespace content {
 class WebContents;
@@ -36,11 +37,7 @@ class TabContentsIterator {
 
   // Returns the Browser instance associated with the current
   // WebContents. Valid as long as !done()
-  Browser* browser() const {
-    if (browser_iterator_ != BrowserList::end())
-      return *browser_iterator_;
-    return NULL;
-  }
+  Browser* browser() const;
 
   // Returns the current WebContents, valid as long as !Done()
   content::WebContents* operator->() const {
@@ -67,8 +64,11 @@ class TabContentsIterator {
   // web_view_index_ is -1, it will fill the first host.
   void Advance();
 
-  // Iterator over all the Browser objects.
-  BrowserList::const_iterator browser_iterator_;
+  // Helper function to iterate the BrowserList passed in.
+  // Returns true if the iterator was successfully advanced.
+  bool AdvanceBrowserIterator(
+      chrome::BrowserListImpl::const_iterator* list_iterator,
+      chrome::BrowserListImpl* browser_list);
 
   // Tab index into the current Browser of the current web view.
   int web_view_index_;
@@ -77,6 +77,18 @@ class TabContentsIterator {
   // can be extracted given the browser iterator and index, but it's nice to
   // cache this since the caller may access the current host many times.
   content::WebContents* cur_;
+
+  // The Desktop Browser list.
+  chrome::BrowserListImpl* desktop_browser_list_;
+
+  // The Ash Browser list.
+  chrome::BrowserListImpl* ash_browser_list_;
+
+  // Iterator over all the Browser objects in desktop mode.
+  chrome::BrowserListImpl::const_iterator desktop_browser_iterator_;
+
+  // Iterator over all the Browser objects in Ash mode.
+  chrome::BrowserListImpl::const_iterator ash_browser_iterator_;
 
   DISALLOW_COPY_AND_ASSIGN(TabContentsIterator);
 };
