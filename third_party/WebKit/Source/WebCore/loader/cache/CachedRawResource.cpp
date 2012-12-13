f21d4d147531805254b4d2d35508514e609f8f5d
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 18:27:51 +0000

    Route main resource loads through the memory cache. https://bugs.webkit.org/show_bug.cgi?id=49246
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    Note that this patch doesn't actually enable caching of main resources. That will be done in a later patch.
    The MainResourceLoader actually has an underlying SubresourceLoader (with the cache layer between them).
    In several places, the MainResourceLoader's SubresourceLoader is treated as special.
    
    No new tests, as this is primarily a refactor. A couple of expected results changed slightly.
    
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::setRequest):
    (WebCore::DocumentLoader::subresource):
    (WebCore::DocumentLoader::addSubresourceLoader): Because the SubresourceLoader underlying the main resource
        needs special handling in certain cases, track it separately from the real SubresourceLoaders.
    (WebCore::DocumentLoader::removeSubresourceLoader):
    (WebCore::DocumentLoader::startLoadingMainResource):
    * loader/MainResourceLoader.cpp:
    (WebCore::MainResourceLoader::MainResourceLoader):
    (WebCore::MainResourceLoader::receivedError):
    (WebCore::MainResourceLoader::cancel):
    (WebCore::MainResourceLoader::clearResource):
    (WebCore):
    (WebCore::MainResourceLoader::frameLoader):
    (WebCore::MainResourceLoader::request):
    (WebCore::MainResourceLoader::continueAfterNavigationPolicy):
    (WebCore::MainResourceLoader::resourceData):
    (WebCore::MainResourceLoader::redirectReceived):
    (WebCore::MainResourceLoader::willSendRequest):
    (WebCore::MainResourceLoader::continueAfterContentPolicy):
    (WebCore::MainResourceLoader::responseReceived):
    (WebCore::MainResourceLoader::dataReceived):
    (WebCore::MainResourceLoader::didFinishLoading):
    (WebCore::MainResourceLoader::notifyFinished):
    (WebCore::MainResourceLoader::reportMemoryUsage):
    (WebCore::MainResourceLoader::handleSubstituteDataLoadNow):
    (WebCore::MainResourceLoader::load):
    (WebCore::MainResourceLoader::setDefersLoading):
    (WebCore::MainResourceLoader::defersLoading):
    (WebCore::MainResourceLoader::setShouldBufferData):
    (WebCore::MainResourceLoader::loader):
    (WebCore::MainResourceLoader::identifier):
    * loader/MainResourceLoader.h:
    (MainResourceLoader):
    (WebCore::MainResourceLoader::documentLoader):
    * loader/ResourceLoader.cpp:
    (WebCore::ResourceLoader::willSendRequest):
    * loader/ResourceLoader.h:
    (WebCore::ResourceLoader::defersLoading):
    (WebCore::ResourceLoader::cancelled):
    * loader/appcache/ApplicationCacheHost.cpp:
    (WebCore::ApplicationCacheHost::maybeLoadFallbackForMainResponse):
    (WebCore::ApplicationCacheHost::maybeLoadFallbackForMainError):
    * loader/mac/DocumentLoaderMac.cpp:
    (WebCore::DocumentLoader::schedule):
    (WebCore::DocumentLoader::unschedule):
    
    LayoutTests:
    
    * http/tests/misc/will-send-request-returns-null-on-redirect-expected.txt: The error code for loads cancelled
        by an embedder's willSendRequest() will show as standard load cancellations rather than as a content policy
        failure. This was an odd quirk of how MainResourceLoader::willSendRequest() was implemented (namely, doing a
        content policy check on ResourceRequests with empty urls).
    * platform/chromium-linux/fast/replaced/border-radius-clip-content-edge-expected.png:
    * platform/chromium/http/tests/misc/will-send-request-returns-null-on-redirect-expected.txt: The error code for loads cancelled
        by an embedder's willSendRequest() will show as standard load cancellations rather than as a content policy
        failure. This was an odd quirk of how MainResourceLoader::willSendRequest() was implemented (namely, doing a
        content policy check on ResourceRequests with empty urls).
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137607 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedRawResource.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedRawResource.cpp
@@ -99,6 +99,7 @@ void CachedRawResource::allClientsRemoved()
 
 void CachedRawResource::willSendRequest(ResourceRequest& request, const ResourceResponse& response)
 {
+    CachedResourceHandle<CachedRawResource> protect(this);
     if (!response.isNull()) {
         CachedResourceClientWalker<CachedRawResourceClient> w(m_clients);
         while (CachedRawResourceClient* c = w.next())
