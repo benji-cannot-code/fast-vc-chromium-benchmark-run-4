894d22e4e5844d9df10eb897052adfb4dcebb897
thiago.santos@intel.com <thiago.santos@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 16:26:38 +0000

    [EFL] Update the way we set the cookies policy https://bugs.webkit.org/show_bug.cgi?id=104915
    Reviewed by Kenneth Rohde Christiansen.
    
    We are mistakenly assigning the cookie policy to the persistent storage
    type which is not correct. This was caused by r137432 and is making
    the unit tests fail.
    
    * UIProcess/efl/WebContextEfl.cpp:
    (WebKit::WebContext::platformInitializeWebProcess):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/efl/WebContextEfl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/efl/WebContextEfl.cpp
@@ -90,7 +90,8 @@ void WebContext::platformInitializeWebProcess(WebProcessCreationParameters& para
     initializeInspectorServer();
 
     parameters.urlSchemesRegistered = m_soupRequestManagerProxy->registeredURISchemes();
-    m_cookieManagerProxy->getCookiePersistentStorage(parameters.cookiePersistentStoragePath, parameters.cookieAcceptPolicy);
+    m_cookieManagerProxy->getCookiePersistentStorage(parameters.cookiePersistentStoragePath, parameters.cookiePersistentStorageType);
+    parameters.cookieAcceptPolicy = m_initialHTTPCookieAcceptPolicy;
 }
 
 void WebContext::platformInvalidateContext()
