3ef27251b6f8359e94833b4136bb5702e2b557a0
szym@chromium.org <szym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 01:46:44 +0000

    [net] Make IPEndPoint::GetFamily() return AddressFamily and add GetSockAddrFamily() to be used when creating sockets.
    BUG=126002
    TEST=browse anywhere
    
    
    Review URL: https://chromiumcodereview.appspot.com/11528012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172768 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/ip_endpoint.h
+++ net/base/ip_endpoint.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "net/base/address_family.h"
 #include "net/base/net_export.h"
 #include "net/base/net_util.h"
 
@@ -30,8 +31,11 @@ class NET_EXPORT IPEndPoint {
   const IPAddressNumber& address() const { return address_; }
   int port() const { return port_; }
 
-  // Returns AF_INET or AF_INET6 depending on the type of the address.
-  int GetFamily() const;
+  // Returns AddressFamily of the address.
+  AddressFamily GetFamily() const;
+
+  // Returns the sockaddr family of the address, AF_INET or AF_INET6.
+  int GetSockAddrFamily() const;
 
   // Convert to a provided sockaddr struct.
   // |address| is the sockaddr to copy into.  Should be at least
--- net/dns/address_sorter_win.cc
+++ net/dns/address_sorter_win.cc
@@ -59,7 +59,7 @@ class AddressSorterWin : public AddressSorter {
       for (size_t i = 0; i < list.size(); ++i) {
         IPEndPoint ipe = list[i];
         // Addresses must be sockaddr_in6.
-        if (ipe.GetFamily() == AF_INET) {
+        if (ipe.GetFamily() == ADDRESS_FAMILY_IPV4) {
           ipe = IPEndPoint(ConvertIPv4NumberToIPv6Number(ipe.address()),
                            ipe.port());
         }
@@ -166,7 +166,7 @@ class AddressSorterWinXP : public AddressSorter {
     AddressList list_ipv6;
     for (size_t i = 0; i < list.size(); ++i) {
       const IPEndPoint& ipe = list[i];
-      if (ipe.GetFamily() == AF_INET) {
+      if (ipe.GetFamily() == ADDRESS_FAMILY_IPV4) {
         list_ipv4.push_back(ipe);
       } else {
         list_ipv6.push_back(ipe);
