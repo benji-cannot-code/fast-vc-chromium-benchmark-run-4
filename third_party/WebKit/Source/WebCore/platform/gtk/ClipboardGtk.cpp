714e32f23641c5f35b6777b6d24f957484b66301
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 18:47:13 +0000

    [GTK] Add missing breaks in switch-case statements https://bugs.webkit.org/show_bug.cgi?id=104929
    Patch by Claudio Saavedra <csaavedra@igalia.com> on 2012-12-13
    Reviewed by Martin Robinson.
    
    * platform/gtk/ClipboardGtk.cpp:
    (WebCore::ClipboardGtk::clearData): Add missing breaks.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/ClipboardGtk.cpp
+++ third_party/WebKit/Source/WebCore/platform/gtk/ClipboardGtk.cpp
@@ -121,8 +121,10 @@ void ClipboardGtk::clearData(const String& typeString)
         break;
     case ClipboardDataTypeImage:
         m_dataObject->clearImage();
+        break;
     case ClipboardDataTypeUnknown:
         m_dataObject->clearAll();
+        break;
     }
 
     if (m_clipboard)
