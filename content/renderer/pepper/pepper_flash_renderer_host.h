c3c780d0d952da3acfb5bd02dcff06c033885922
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 03:35:35 +0000

    Refactor 4 PPB_Flash functions to the new PPAPI resource model.
    The functions being refactored are:
    -SetInstanceAlwaysOnTop
    -DrawGlyphs
    -GetProxyForURL
    -Navigate
    
    Each of these functions was manually tested with a Flash movie that uses it.
    
    Review URL: https://chromiumcodereview.appspot.com/11510008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172806 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_flash_renderer_host.h
+++ content/renderer/pepper/pepper_flash_renderer_host.h
@@ -6,9 +6,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_RENDERER_PEPPER_PEPPER_FLASH_RENDERER_HOST_H_
 #define CONTENT_RENDERER_PEPPER_PEPPER_FLASH_RENDERER_HOST_H_
 
+#include <string>
+
 #include "base/basictypes.h"
 #include "ppapi/host/host_message_context.h"
 #include "ppapi/host/resource_host.h"
+
+struct PP_Rect;
+
+namespace ppapi {
+struct URLRequestInfoData;
+}
+
+namespace ppapi {
+namespace proxy {
+struct PPBFlash_DrawGlyphs_Params;
+}
+}
 
 namespace content {
 
@@ -29,6 +43,19 @@ class PepperFlashRendererHost : public ppapi::host::ResourceHost {
  private:
   int32_t OnMsgGetProxyForURL(ppapi::host::HostMessageContext* host_context,
                               const std::string& url);
+  int32_t OnMsgSetInstanceAlwaysOnTop(
+      ppapi::host::HostMessageContext* host_context,
+      bool on_top);
+  int32_t OnMsgDrawGlyphs(ppapi::host::HostMessageContext* host_context,
+                          ppapi::proxy::PPBFlash_DrawGlyphs_Params params);
+  int32_t OnMsgNavigate(ppapi::host::HostMessageContext* host_context,
+                        const ppapi::URLRequestInfoData& data,
+                        const std::string& target,
+                        bool from_user_action);
+  int32_t OnMsgIsRectTopmost(ppapi::host::HostMessageContext* host_context,
+                             const PP_Rect& rect);
+
+  RendererPpapiHost* host_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperFlashRendererHost);
 };
--- ppapi/thunk/ppb_flash_functions_api.h
+++ ppapi/thunk/ppb_flash_functions_api.h
@@ -12,6 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/shared_impl/singleton_resource_id.h"
 #include "ppapi/thunk/ppapi_thunk_export.h"
 
+struct PP_BrowserFont_Trusted_Description;
+
 namespace ppapi {
 namespace thunk {
 
@@ -28,6 +30,25 @@ class PPAPI_THUNK_EXPORT PPB_Flash_Functions_API {
   virtual PP_Bool SetCrashData(PP_Instance instance,
                                PP_FlashCrashKey key,
                                PP_Var value) = 0;
+  virtual void SetInstanceAlwaysOnTop(PP_Instance instance, PP_Bool on_top) = 0;
+  virtual PP_Bool DrawGlyphs(
+      PP_Instance instance,
+      PP_Resource pp_image_data,
+      const PP_BrowserFont_Trusted_Description* font_desc,
+      uint32_t color,
+      const PP_Point* position,
+      const PP_Rect* clip,
+      const float transformation[3][3],
+      PP_Bool allow_subpixel_aa,
+      uint32_t glyph_count,
+      const uint16_t glyph_indices[],
+      const PP_Point glyph_advances[]) = 0;
+  virtual int32_t Navigate(PP_Instance instance,
+                           PP_Resource request_info,
+                           const char* target,
+                           PP_Bool from_user_action) = 0;
+
+  virtual PP_Bool IsRectTopmost(PP_Instance instance, const PP_Rect* rect) = 0;
 
   static const SingletonResourceID kSingletonResourceID = FLASH_SINGLETON_ID;
 };
