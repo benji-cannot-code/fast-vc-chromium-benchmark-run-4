9873788124e724b53d9c7f7fb4cfcdfeb0b366c7
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 17:47:53 +0000

    Grayscale wallpaper thumbnails which aren't available when offline.
    This CL did the following:
    1. When launching wallpaper picker, if the device is offline, grayscale all
    displayed thumbnails that aren't availabel offline.
    2. When device becomes offline while wallpaper picker is open, react to offline
    event and grayscale all displayed thumbnails that aren't available offline.
    3. If the device becomes online again, clear all greyscale effects.
    
    BUG=162638
    TEST=
    1. Log in to a fresh device
    2. Launch wallpaper manager and click all categories to get all thumbnails cached
    3. Disconnect device. All thumbnails should be grayscaled
    4. Connect device, all thumbnails should be colored
    5. click a few of the thumbnails to change wallpapers (wait for each wallpaper changed then click next one)
    6, disconnect again, all thumbnails except the ones you clicked should be gray scaled.
    
    Review URL: https://codereview.chromium.org/11451020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172892 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/wallpaper_manager/js/wallpaper_images_grid.js
+++ chrome/browser/resources/chromeos/wallpaper_manager/js/wallpaper_images_grid.js
@@ -14,7 +14,7 @@ cr.define('wallpapers', function() {
   /**
    * Creates a new wallpaper thumbnails grid item.
    * @param {{baseURL: string, dynamicURL: string, layout: string,
-   *          author: string, authorWebsite: string}}
+   *          author: string, authorWebsite: string, availableOffline: boolean}}
    *     wallpaperInfo Wallpaper baseURL, dynamicURL, layout, author and
    *     author website.
    * @constructor
@@ -32,17 +32,22 @@ cr.define('wallpapers', function() {
     /** @override */
     decorate: function() {
       GridItem.prototype.decorate.call(this);
+      // Removes garbage created by GridItem.
+      this.innerText = '';
       var imageEl = cr.doc.createElement('img');
+      imageEl.classList.add('thumbnail');
+      cr.defineProperty(imageEl, 'offline', cr.PropertyKind.BOOL_ATTR);
+      imageEl.offline = this.dataItem.availableOffline;
+      this.appendChild(imageEl);
       var self = this;
       chrome.wallpaperPrivate.getThumbnail(this.dataItem.baseURL,
                                            function(data) {
         if (data) {
-          var blob = new Blob([new Int8Array(data)]);
+          var blob = new Blob([new Int8Array(data)], {'type' : 'image\/png'});
           imageEl.src = window.URL.createObjectURL(blob);
           imageEl.addEventListener('load', function(e) {
             window.URL.revokeObjectURL(this.src);
           });
-          self.appendChild(imageEl);
         } else {
           var xhr = new XMLHttpRequest();
           xhr.open('GET', self.dataItem.baseURL + ThumbnailSuffix, true);
@@ -53,14 +58,14 @@ cr.define('wallpapers', function() {
               self.textContent = '';
               chrome.wallpaperPrivate.saveThumbnail(self.dataItem.baseURL,
                                                     xhr.response);
-              var blob = new Blob([new Int8Array(xhr.response)]);
+              var blob = new Blob([new Int8Array(xhr.response)],
+                                  {'type' : 'image\/png'});
               imageEl.src = window.URL.createObjectURL(blob);
               // TODO(bshe): We currently use empty div to reserve space for
               // thumbnail. Use a placeholder like "loading" image may better.
               imageEl.addEventListener('load', function(e) {
                 window.URL.revokeObjectURL(this.src);
               });
-              self.appendChild(imageEl);
             }
           });
         }
