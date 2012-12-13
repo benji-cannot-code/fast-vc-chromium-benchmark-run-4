e04081af39c4db571870028c85026727bb70e3f6
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 23:18:45 +0000

    Add an attach a file option to Feedback. Add an attach a file option to the Feedback UI. This is only for ChromeOS.
    R=jhawkins@chromium.org
    BUG=163645
    TEST=Attach a file and verify that it successfully shows up on the report on the server.
    
    Review URL: https://codereview.chromium.org/11280316
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/feedback/feedback_data.h
+++ chrome/browser/feedback/feedback_data.h
@@ -36,6 +36,8 @@ class FeedbackData {
                   , const bool send_sys_info
                   , const bool sent_report
                   , const std::string& timestamp
+                  , const std::string& attached_filename
+                  , const std::string& attached_filedata
 #endif
                   );
 
@@ -77,6 +79,9 @@ class FeedbackData {
   // Content of the compressed system logs.
   std::string* zip_content_;
   std::string timestamp_;
+  std::string attached_filename_;
+  std::string attached_filedata_;
+
   // NOTE: Extra boolean sent_report_ is required because callback may
   // occur before or after we call SendReport().
   bool sent_report_;
