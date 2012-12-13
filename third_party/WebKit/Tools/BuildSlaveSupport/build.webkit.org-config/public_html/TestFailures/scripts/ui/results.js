1f3821fe11aab5ce075554a7488e67ffb8584ab2
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 23:27:42 +0000

    Revert r137407 - it didn't actually work right. https://bugs.webkit.org/show_bug.cgi?id=101976
    Unreviewed, build fix.
    
    Turns out that rebaselining was completely broken with this change :(
    We're passing in a list of test names rather than the list of
    failureInfo objects.
    
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/controllers.js:
    (.):
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/results.js:
    (.):
    * BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/results_unittests.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/results.js
+++ third_party/WebKit/Tools/BuildSlaveSupport/build.webkit.org-config/public_html/TestFailures/scripts/ui/results.js
@@ -188,7 +188,7 @@ function isAnyReftest(testName, resultsByTest)
     return Object.keys(resultsByTest[testName]).map(function(builder) {
         return resultsByTest[testName][builder];
     }).some(function(resultNode) {
-        return resultNode.reftest_type && resultNode.reftest_type.length;
+        return resultNode.is_reftest || resultNode.is_mismatch_reftest
     });
 }
 
