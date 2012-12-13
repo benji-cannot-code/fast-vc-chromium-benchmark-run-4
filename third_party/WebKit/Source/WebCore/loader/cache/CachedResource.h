bb6b6a5557e485c4f0ab535725ca78d14d1f63d6
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 17:37:33 +0000

    CachedResources should hang on to stripped fragment identifiers https://bugs.webkit.org/show_bug.cgi?id=104721
    Reviewed by Maciej Stachowiak.
    
    No new tests, this will be unused until bug
    https://bugs.webkit.org/show_bug.cgi?id=49246 re-lands. Then, it will
    allow http/tests/inspector/resource-parameters.html to continue passing.
    
    * loader/cache/CachedResource.cpp:
    (WebCore::CachedResource::CachedResource):
    (WebCore::CachedResource::load):
    * loader/cache/CachedResource.h:
    (CachedResource):
    * loader/cache/CachedResourceLoader.cpp:
    (WebCore::CachedResourceLoader::requestResource): We override the request's url with the
        local variable "url" here. The only difference between url and request.url() is that url
        has had any fragment identifier removed. Do this work later (in the CachedResource constructor)
        so that the CachedResource can save the fragment for later.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137604 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResource.h
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResource.h
@@ -309,6 +309,8 @@ private:
     void addAdditionalRequestHeaders(CachedResourceLoader*);
     void failBeforeStarting();
 
+    String m_fragmentIdentifierForRequest;
+
     RefPtr<CachedMetadata> m_cachedMetadata;
 
     ResourceError m_error;
