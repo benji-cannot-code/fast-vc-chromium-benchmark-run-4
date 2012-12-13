c8c0d6248d3f10ce10b0dbb66dc2384c75249419
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 21:41:21 +0000

    Extra compile assert in ipctest.cc
    Shishir ran into this earlier today, and it was confusing that this file, in an uncommon target, is the only error you get when you add a spurious semicolon to an IPC header. This compile assert is SHOUTING so that future generations do not waste so much time.
    
    R=shishir@chromium.org,thakis@chromium.org
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11568018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172968 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/ipclist/ipclist.cc
+++ chrome/tools/ipclist/ipclist.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "base/basictypes.h"
+
 // Include once to get the type definitions
 #include "chrome/common/all_messages.h"
 #include "content/common/all_messages.h"
@@ -34,6 +36,7 @@ static msginfo msgtable[] = {
 #include "content/common/all_messages.h"
 };
 #define MSGTABLE_SIZE (sizeof(msgtable)/sizeof(msgtable[0]))
+COMPILE_ASSERT(MSGTABLE_SIZE, CHECK_YOUR_HEADERS_FOR_AN_EXTRA_SEMICOLON);
 
 static bool check_msgtable() {
   bool result = true;
