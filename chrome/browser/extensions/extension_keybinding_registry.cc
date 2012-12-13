0cb081c9c8eb4c7e766e2f6998a0e1ad6a21ad08
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-13 02:48:59 +0000

    Remove commands from browser event router.
    BUG=165209
    
    
    Review URL: https://chromiumcodereview.appspot.com/11554004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@172790 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_keybinding_registry.cc
+++ chrome/browser/extensions/extension_keybinding_registry.cc
@@ -5,8 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/extension_keybinding_registry.h"
 
+#include "base/values.h"
 #include "chrome/browser/extensions/active_tab_permission_granter.h"
-#include "chrome/browser/extensions/browser_event_router.h"
+#include "chrome/browser/extensions/event_router.h"
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/profiles/profile.h"
@@ -70,9 +71,14 @@ void ExtensionKeybindingRegistry::CommandExecuted(
   if (granter)
     granter->GrantIfRequested(extension);
 
-  service->browser_event_router()->CommandExecuted(profile_,
-                                                   extension_id,
-                                                   command);
+  scoped_ptr<ListValue> args(new ListValue());
+  args->Append(Value::CreateStringValue(command));
+
+  scoped_ptr<Event> event(new Event("commands.onCommand", args.Pass()));
+  event->restrict_to_profile = profile_;
+  event->user_gesture = EventRouter::USER_GESTURE_ENABLED;
+  ExtensionSystem::Get(profile_)->event_router()->
+      DispatchEventToExtension(extension_id, event.Pass());
 }
 
 void ExtensionKeybindingRegistry::Observe(
