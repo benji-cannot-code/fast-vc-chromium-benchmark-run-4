c735c8d4d08fa82bb7a516e0010fe32158eea672
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 21:48:03 +0000

    <rdar://problem/12838354> Per-Tab WebProcess: DOM cookie access functions should respect private browsing         https://bugs.webkit.org/show_bug.cgi?id=104691
            Reviewed by Sam Weinig.
    
            Pass private browsing state from local context to remote one.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/NetworkingContext.h
+++ third_party/WebKit/Source/WebCore/platform/network/NetworkingContext.h
@@ -81,6 +81,7 @@ public:
 #endif
 
 #if PLATFORM(MAC) || USE(CFNETWORK)
+    virtual bool inPrivateBrowsingMode() const = 0;
     virtual CFURLStorageSessionRef storageSession() const = 0;
 #endif
 
--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/win/WebFrameNetworkingContext.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/win/WebFrameNetworkingContext.h
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrame.h"
 
 #include <WebCore/FrameNetworkingContext.h>
+#include <WebCore/NotImplemented.h>
 #include <WebCore/ResourceError.h>
 #include <WebCore/ResourceRequest.h>
 
@@ -45,6 +46,7 @@ private:
     virtual WebCore::ResourceError blockedError(const WebCore::ResourceRequest&) const;
 
 #if USE(CFNETWORK)
+    virtual bool inPrivateBrowsingMode() const;
     virtual CFURLStorageSessionRef storageSession() const;
 #endif
 
