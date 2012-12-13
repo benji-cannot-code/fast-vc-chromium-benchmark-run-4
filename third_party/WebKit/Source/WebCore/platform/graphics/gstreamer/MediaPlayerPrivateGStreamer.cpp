5a2a3d064dbd4f34c3a220941220ea510dfaf4dc
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-13 00:40:47 +0000

    Source/WebCore: Try to fix the GTK+ debug bulid
    Logging.h is no longer included in headers, so we need to include it manually in
    files that require logging macros.
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp: Include Logging.h.
    
    Source/WebKit/gtk: [Coverity] [GTK] Remove some redundant null checks in DumpRenderTree
    https://bugs.webkit.org/show_bug.cgi?id=104570
    
    Reviewed by Daniel Bates.
    
    * WebCoreSupport/DumpRenderTreeSupportGtk.cpp:
    (DumpRenderTreeSupportGtk::elementDoesAutoCompleteForElementWithId): Remove a redundant null check.
    
    Tools: [Coverity] [GTK] Remove some redundant null checks in DumpRenderTree
    https://bugs.webkit.org/show_bug.cgi?id=104570
    
    Reviewed by Daniel Bates.
    
    * DumpRenderTree/gtk/DumpRenderTree.cpp:
    (descriptionSuitableForTestResult): Remove a redundant null check.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ImageOrientation.h"
 #include "IntRect.h"
 #include "KURL.h"
+#include "Logging.h"
 #include "MIMETypeRegistry.h"
 #include "MediaPlayer.h"
 #include "NotImplemented.h"
