3ccb5f316fa48d73c228afb7f11ccef7dd6be989
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-14 18:47:59 +0000

    Add a DownloadsProxyMap class in preparation for moving the downloads map away from WebContext https://bugs.webkit.org/show_bug.cgi?id=104980
    Reviewed by Andreas Kling.
    
    Currently the DownloadProxyMap doesn't really do anything; this initial change is to make sure that
    adding the new files won't break any builds.
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * UIProcess/Downloads/DownloadProxyMap.cpp: Added.
    (WebKit):
    (WebKit::DownloadProxyMap::shared):
    (WebKit::DownloadProxyMap::DownloadProxyMap):
    (WebKit::DownloadProxyMap::~DownloadProxyMap):
    (WebKit::DownloadProxyMap::createDownloadProxy):
    * UIProcess/Downloads/DownloadProxyMap.h: Added.
    (WebKit):
    (DownloadProxyMap):
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::createDownloadProxy):
    * WebKit2.xcodeproj/project.pbxproj:
    * win/WebKit2.vcproj:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebContext.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebContext.h"
 
 #include "DownloadProxy.h"
+#include "DownloadProxyMap.h"
 #include "DownloadProxyMessages.h"
 #include "ImmutableArray.h"
 #include "Logging.h"
@@ -836,7 +837,7 @@ void WebContext::addVisitedLinkHash(LinkHash linkHash)
 
 DownloadProxy* WebContext::createDownloadProxy()
 {
-    RefPtr<DownloadProxy> downloadProxy = DownloadProxy::create(this);
+    RefPtr<DownloadProxy> downloadProxy = DownloadProxyMap::shared().createDownloadProxy(this);
     m_downloads.set(downloadProxy->downloadID(), downloadProxy);
     addMessageReceiver(Messages::DownloadProxy::messageReceiverName(), downloadProxy->downloadID(), this);
     return downloadProxy.get();
