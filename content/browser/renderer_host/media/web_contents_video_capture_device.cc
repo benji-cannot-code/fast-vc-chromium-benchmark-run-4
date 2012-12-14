e11d5958f124c09fbe0701c44843041ac72ca66e
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 07:14:36 +0000

    Remove resolution hack for TabCapture implementation
    Resolution can be set through MediaConstraints so remove the resolution hack.
    
    BUG=163286
    
    
    Review URL: https://chromiumcodereview.appspot.com/11565025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173115 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/web_contents_video_capture_device.cc
+++ content/browser/renderer_host/media/web_contents_video_capture_device.cc
@@ -70,11 +70,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkColor.h"
 #include "ui/gfx/rect.h"
 
-// TODO(miu): The caller of Allocate() should use an appropriate heuristic to
-// determine the frame size.  For now, hard-code the capture device to 720p
-// resolution.
-#define FORCE_1280x720_FRAME_SIZE
-
 // Used to self-trampoline invocation of methods to the approprate thread.  This
 // should be used sparingly, only when it's not clear which thread is invoking a
 // method.
@@ -740,13 +735,6 @@ void CaptureMachine::Allocate(int width, int height, int frame_rate) {
     Error();
     return;
   }
-
-#ifdef FORCE_1280x720_FRAME_SIZE
-  VLOG_IF(1, width != 1280 || height != 720)
-      << "Forced frame size to 1280x720.";
-  width = 1280;
-  height = 720;
-#endif
 
   // Frame dimensions must each be a positive, even integer, since the consumer
   // wants (or will convert to) YUV420.
