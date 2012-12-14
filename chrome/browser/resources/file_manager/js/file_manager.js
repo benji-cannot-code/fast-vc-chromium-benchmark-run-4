6a1269a3f102f90d20fa078fbc7354611b0aa5f9
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 16:16:06 +0000

    Fix 'Available offline' checkbox in Files app
    BUG=165986
    
    Review URL: https://codereview.chromium.org/11572038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1686,13 +1686,6 @@ DialogType.isModal = function(type) {
     }, 100);
   };
 
-  FileManager.prototype.onPinClick_ = function(checkbox, entry, event) {
-    var command = this.document_.querySelector('command#toggle-pinned');
-    command.canExecuteChange(checkbox);
-    command.execute(checkbox);
-    event.preventDefault();
-  };
-
   FileManager.prototype.selectDefaultPathInFilenameInput_ = function() {
     var input = this.filenameInput_;
     input.focus();
