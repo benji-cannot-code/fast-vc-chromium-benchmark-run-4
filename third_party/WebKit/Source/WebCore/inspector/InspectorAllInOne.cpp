359d0ac7736211fb5d1f0eae1caebbd5c42b5dec
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-14 09:30:22 +0000

    Unreviewed. Windows compilation fix after r137728.
    * inspector/InspectorAllInOne.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137732 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorAllInOne.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ContentSearchUtils.cpp"
 #include "DOMEditor.cpp"
 #include "DOMPatchSupport.cpp"
+#include "HeapGraphSerializer.cpp"
 #include "IdentifiersFactory.cpp"
 #include "InjectedScript.cpp"
 #include "InjectedScriptBase.cpp"
