e4e218be1049075926e22b4f7149b9c7743d1d06
nkang@chromium.org <nkang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 18:35:03 +0000

    Removed 'pyautolib' dependencies from install_test and removed some common code from run_install_test.py. Also, modified util.py and copied DoesUrlExist to it, so install_test doesn't have to depend on 'pyautolib.pyauto_utils'.
    BUG=163708,163718
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11553041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173176 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/pylib/common/util.py
+++ chrome/test/pylib/common/util.py
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Generic utilities for all python scripts."""
 
 import atexit
+import httplib
 import os
 import signal
 import stat
 import subprocess
 import sys
 import tempfile
+import urlparse
 
 
 def GetPlatformName():
@@ -124,3 +126,27 @@ def RunCommand(cmd, cwd=None):
   process = subprocess.Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, cwd=cwd)
   process.wait()
   return process.returncode
+
+
+def DoesUrlExist(url):
+  """Determines whether a resource exists at the given URL.
+
+  Args:
+    url: URL to be verified.
+
+  Returns:
+    True if url exists, otherwise False.
+  """
+  parsed = urlparse.urlparse(url)
+  try:
+    conn = httplib.HTTPConnection(parsed.netloc)
+    conn.request('HEAD', parsed.path)
+    response = conn.getresponse()
+  except (socket.gaierror, socket.error):
+    return False
+  finally:
+    conn.close()
+  # Follow both permanent (301) and temporary (302) redirects.
+  if response.status == 302 or response.status == 301:
+    return DoesUrlExist(response.getheader('location'))
+  return response.status == 200
