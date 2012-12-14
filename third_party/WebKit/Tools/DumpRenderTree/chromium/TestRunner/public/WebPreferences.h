2d21502df02b799596f6beacf309595f21559ada
vollick@chromium.org <vollick@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-14 04:48:11 +0000

    [chromium] Add a virtual test suite for enabling opt-in to composited scrolling https://bugs.webkit.org/show_bug.cgi?id=104911
    Reviewed by Dirk Pranke.
    
    Tools:
    
    Adds two virtual tests suites to run the tests in
    compositing/overflow/ and scrollbars/ with the flag
    --enable-accelerated-overflow-scroll, which I've also plumbed through
    in the usual way.
    
    * DumpRenderTree/chromium/DumpRenderTree.cpp:
    (main):
    * DumpRenderTree/chromium/TestRunner/public/WebPreferences.h:
    (WebPreferences):
    * DumpRenderTree/chromium/TestRunner/src/WebPreferences.cpp:
    (WebTestRunner::WebPreferences::reset):
    (WebTestRunner::WebPreferences::applyTo):
    * DumpRenderTree/chromium/TestShell.cpp:
    (TestShell::TestShell):
    (TestShell::resetWebSettings):
    * DumpRenderTree/chromium/TestShell.h:
    (TestShell::setAcceleratedCompositingForOverflowScrollEnabled):
    (TestShell):
    * Scripts/webkitpy/layout_tests/port/chromium.py:
    (ChromiumPort.virtual_test_suites):
    
    LayoutTests:
    
    Updated the test expectations for the virtual test suite to match the
    usual chromium test expectations.
    
    * platform/chromium/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebPreferences.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebPreferences.h
@@ -99,6 +99,7 @@ struct WebPreferences {
     bool caretBrowsingEnabled;
     bool acceleratedCompositingForVideoEnabled;
     bool acceleratedCompositingForFixedPositionEnabled;
+    bool acceleratedCompositingForOverflowScrollEnabled;
     bool acceleratedCompositingEnabled;
     bool forceCompositingMode;
     bool accelerated2dCanvasEnabled;
--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestShell.h
@@ -131,6 +131,7 @@ public:
 
     void setAcceleratedCompositingForVideoEnabled(bool enabled) { m_acceleratedCompositingForVideoEnabled = enabled; }
     void setAcceleratedCompositingForFixedPositionEnabled(bool enabled) { m_acceleratedCompositingForFixedPositionEnabled = enabled; }
+    void setAcceleratedCompositingForOverflowScrollEnabled(bool enabled) { m_acceleratedCompositingForOverflowScrollEnabled = enabled; }
     bool softwareCompositingEnabled() { return m_softwareCompositingEnabled; }
     void setSoftwareCompositingEnabled(bool enabled) { m_softwareCompositingEnabled = enabled; }
     void setThreadedCompositingEnabled(bool enabled) { m_threadedCompositingEnabled = enabled; }
@@ -233,6 +234,7 @@ private:
     bool m_allowExternalPages;
     bool m_acceleratedCompositingForVideoEnabled;
     bool m_acceleratedCompositingForFixedPositionEnabled;
+    bool m_acceleratedCompositingForOverflowScrollEnabled;
     bool m_softwareCompositingEnabled;
     bool m_threadedCompositingEnabled;
     bool m_forceCompositingMode;
