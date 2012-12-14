1ec03166f8949f49d466b80fbf30457f5851c556
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 05:14:28 +0000

    This should have multiple-item view matching the mock-ups pixel for pixel except for the time display and the small icon which aren't supported yet. The time display has been laid out to match the mock-up, it just needs to get the time in an appropriate string format.
    BUG=161101
    
    
    Review URL: https://chromiumcodereview.appspot.com/11434017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173094 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_view_multiple.h
+++ ui/message_center/message_view_multiple.h
@@ -21,13 +21,15 @@ class MessageViewMultiple : public MessageView {
                       const NotificationList::Notification& notification);
   virtual ~MessageViewMultiple();
 
-  // MessageView
+  // Overridden from MessageView.
   virtual void SetUpView() OVERRIDE;
 
  protected:
   MessageViewMultiple();
 
  private:
+  views::Border* CreateTopBorder(int height);
+
   DISALLOW_COPY_AND_ASSIGN(MessageViewMultiple);
 };
 
