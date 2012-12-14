11ae73a61289e090ea1a7bb02f5d4e49722a8517
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-14 04:56:22 +0000

    Seconds/Minutes field of date/time input UI should respect step attribute https://bugs.webkit.org/show_bug.cgi?id=104985
    Patch by Kunihiko Sakamoto <ksakamoto@chromium.org> on 2012-12-13
    Reviewed by Kent Tamura.
    
    Source/WebCore:
    
    This patch makes step-up/-down UI of the seconds and minutes fields respect
    step attribute when possible.
    
    Test: fast/forms/time-multiple-fields/time-multiple-fields-stepup-stepdown-from-renderer.html
    
    * html/shadow/DateTimeEditElement.cpp:
    (DateTimeEditBuilder):
    (WebCore::DateTimeEditBuilder::visitField): Compute step parameters for seconds and minutes fields, and pass it to the field element constructors.
    (WebCore::DateTimeEditBuilder::createNumericFieldParameters): Added.
    * html/shadow/DateTimeFieldElements.cpp:
    (WebCore::DateTimeMillisecondFieldElement::DateTimeMillisecondFieldElement): Use Parameters instead of step and stepBase.
    (WebCore::DateTimeMillisecondFieldElement::create): Ditto.
    (WebCore::DateTimeMinuteFieldElement::DateTimeMinuteFieldElement): Add Parameters argument.
    (WebCore::DateTimeMinuteFieldElement::create): Ditto.
    (WebCore::DateTimeSecondFieldElement::DateTimeSecondFieldElement): Ditto.
    (WebCore::DateTimeSecondFieldElement::create): Ditto.
    * html/shadow/DateTimeFieldElements.h:
    (DateTimeMillisecondFieldElement): Use Parameters instead of step and stepBase.
    (DateTimeMinuteFieldElement): Add Parameters argument.
    (DateTimeSecondFieldElement): Ditto.
    * html/shadow/DateTimeNumericFieldElement.cpp:
    (WebCore::DateTimeNumericFieldElement::DateTimeNumericFieldElement): Use Parameters instead of step and stepBase.
    * html/shadow/DateTimeNumericFieldElement.h:
    (DateTimeNumericFieldElement):
    (WebCore::DateTimeNumericFieldElement::Parameters::Parameters):
    (Parameters): Added.
    
    LayoutTests:
    
    * fast/forms/time-multiple-fields/time-multiple-fields-stepup-stepdown-from-renderer-expected.txt:
    * fast/forms/time-multiple-fields/time-multiple-fields-stepup-stepdown-from-renderer.html:
    Added test cases for seconds and minutes fields.
    Update an expectation for milliseconds: value=07:13:00.500, step=0 should round to 07:13:00.000
    (setInputAttributes): Set value attribute first to avoid the focused field becomes read-only and unfocused.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/DateTimeFieldElements.h
+++ third_party/WebKit/Source/WebCore/html/shadow/DateTimeFieldElements.h
@@ -92,10 +92,10 @@ class DateTimeMillisecondFieldElement : public DateTimeNumericFieldElement {
     WTF_MAKE_NONCOPYABLE(DateTimeMillisecondFieldElement);
 
 public:
-    static PassRefPtr<DateTimeMillisecondFieldElement> create(Document*, FieldOwner&, int step, int stepBase);
+    static PassRefPtr<DateTimeMillisecondFieldElement> create(Document*, FieldOwner&, const DateTimeNumericFieldElement::Parameters&);
 
 private:
-    DateTimeMillisecondFieldElement(Document*, FieldOwner&, int step, int stepBase);
+    DateTimeMillisecondFieldElement(Document*, FieldOwner&, const DateTimeNumericFieldElement::Parameters&);
 
     // DateTimeFieldElement functions.
     virtual void populateDateTimeFieldsState(DateTimeFieldsState&) OVERRIDE FINAL;
@@ -107,10 +107,10 @@ class DateTimeMinuteFieldElement : public DateTimeNumericFieldElement {
     WTF_MAKE_NONCOPYABLE(DateTimeMinuteFieldElement);
 
 public:
-    static PassRefPtr<DateTimeMinuteFieldElement> create(Document*, FieldOwner&);
+    static PassRefPtr<DateTimeMinuteFieldElement> create(Document*, FieldOwner&, const DateTimeNumericFieldElement::Parameters&);
 
 private:
-    DateTimeMinuteFieldElement(Document*, FieldOwner&);
+    DateTimeMinuteFieldElement(Document*, FieldOwner&, const DateTimeNumericFieldElement::Parameters&);
 
     // DateTimeFieldElement functions.
     virtual void populateDateTimeFieldsState(DateTimeFieldsState&) OVERRIDE FINAL;
@@ -137,10 +137,10 @@ class DateTimeSecondFieldElement : public DateTimeNumericFieldElement {
     WTF_MAKE_NONCOPYABLE(DateTimeSecondFieldElement);
 
 public:
-    static PassRefPtr<DateTimeSecondFieldElement> create(Document*, FieldOwner&);
+    static PassRefPtr<DateTimeSecondFieldElement> create(Document*, FieldOwner&, const DateTimeNumericFieldElement::Parameters&);
 
 private:
-    DateTimeSecondFieldElement(Document*, FieldOwner&);
+    DateTimeSecondFieldElement(Document*, FieldOwner&, const DateTimeNumericFieldElement::Parameters&);
 
     // DateTimeFieldElement functions.
     virtual void populateDateTimeFieldsState(DateTimeFieldsState&) OVERRIDE FINAL;
