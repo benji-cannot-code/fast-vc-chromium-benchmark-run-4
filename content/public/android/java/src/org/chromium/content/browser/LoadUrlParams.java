23be2d7d6bd5561d7fd9661f9a5d1176aa1df4c1
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 01:45:46 +0000

    Implement doUpdateVisitedHistory callback
    Convert WebContentsObserver::didNavigateAnyFrame callback to
    doUpdateVisitedHistory callback for WebView.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11567019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173041 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/LoadUrlParams.java
+++ content/public/android/java/src/org/chromium/content/browser/LoadUrlParams.java
@@ -121,6 +121,13 @@ public class LoadUrlParams {
     }
 
     /**
+     * Return the url.
+     */
+    public String getUrl() {
+        return mUrl;
+    }
+
+    /**
      * Set load type of this load. Defaults to LOAD_TYPE_DEFAULT.
      * @param loadType One of LOAD_TYPE static constants above.
      */
@@ -134,6 +141,13 @@ public class LoadUrlParams {
      */
     public void setTransitionType(int transitionType) {
         mTransitionType = transitionType;
+    }
+
+    /**
+     * Return the transition type.
+     */
+    public int getTransitionType() {
+        return mTransitionType;
     }
 
     /**
