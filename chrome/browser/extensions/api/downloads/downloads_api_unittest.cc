59e05be7d331428da01dad551b730890dc2d8a42
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-14 04:13:03 +0000

    Implement chrome.downloads.erase() BUG=12133
    Review URL: https://chromiumcodereview.appspot.com/10834129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173078 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/downloads/downloads_api_unittest.cc
+++ chrome/browser/extensions/api/downloads/downloads_api_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/json/json_reader.h"
-#include "base/json/json_writer.h"
 #include "base/message_loop.h"
 #include "base/stl_util.h"
 #include "base/stringprintf.h"
@@ -879,7 +878,7 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
 }
 
 IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
-                       DownloadExtensionTest_PauseResumeCancel) {
+                       DownloadExtensionTest_PauseResumeCancelErase) {
   DownloadItem* download_item = CreateSlowTestDownload();
   ASSERT_TRUE(download_item);
 
@@ -942,6 +941,22 @@ IN_PROC_BROWSER_TEST_F(DownloadExtensionTest,
       new DownloadsResumeFunction(), "[-42]");
   EXPECT_STREQ(download_extension_errors::kInvalidOperationError,
                error.c_str());
+
+  int id = download_item->GetId();
+  scoped_ptr<base::Value> result(RunFunctionAndReturnResult(
+      new DownloadsEraseFunction(),
+      base::StringPrintf("[{\"id\": %d}]", id)));
+  DownloadManager::DownloadVector items;
+  GetCurrentManager()->GetAllDownloads(&items);
+  EXPECT_EQ(0UL, items.size());
+  ASSERT_TRUE(result);
+  download_item = NULL;
+  base::ListValue* result_list = NULL;
+  ASSERT_TRUE(result->GetAsList(&result_list));
+  ASSERT_EQ(1UL, result_list->GetSize());
+  int element = -1;
+  ASSERT_TRUE(result_list->GetInteger(0, &element));
+  EXPECT_EQ(id, element);
 }
 
 scoped_refptr<UIThreadExtensionFunction> MockedGetFileIconFunction(
