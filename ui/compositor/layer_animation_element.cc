41e58fb0ace0d269f651472ba5ff794ac84d914c
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-15 18:47:38 +0000

    Make ui::LayerAnimationSequence and ui::LayerAnimationElement use absolute times
    Currently, LayerAnimationSequences and LayerAnimationElements only know
    about the relative offset from the start of the animation. However, for threaded 
    UI animations, a LayerAnimationElement will need to know its absolute start 
    time: in order to compute the delay incurred by dispatching the animation to the 
    compositor thread, it will need to compare its own start time with that of the 
    dispatched animation.
    
    BUG=164206
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11465026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173301 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/layer_animation_element.cc
+++ ui/compositor/layer_animation_element.cc
@@ -406,8 +406,10 @@ LayerAnimationElement::LayerAnimationElement(
 LayerAnimationElement::~LayerAnimationElement() {
 }
 
-bool LayerAnimationElement::Progress(base::TimeDelta elapsed,
+bool LayerAnimationElement::Progress(base::TimeTicks now,
                                      LayerAnimationDelegate* delegate) {
+  DCHECK(start_time_ != base::TimeTicks());
+  base::TimeDelta elapsed = now - start_time_;
   if (first_frame_)
     OnStart(delegate);
   double t = 1.0;
@@ -418,8 +420,9 @@ bool LayerAnimationElement::Progress(base::TimeDelta elapsed,
   return need_draw;
 }
 
-bool LayerAnimationElement::IsFinished(base::TimeDelta elapsed,
+bool LayerAnimationElement::IsFinished(base::TimeTicks time,
                                        base::TimeDelta* total_duration) {
+  base::TimeDelta elapsed = time - start_time_;
   if (elapsed >= duration_) {
     *total_duration = duration_;
     return true;
@@ -428,7 +431,11 @@ bool LayerAnimationElement::IsFinished(base::TimeDelta elapsed,
 }
 
 bool LayerAnimationElement::ProgressToEnd(LayerAnimationDelegate* delegate) {
-  return Progress(duration_, delegate);
+  if (first_frame_)
+    OnStart(delegate);
+  bool need_draw = OnProgress(1.0, delegate);
+  first_frame_ = true;
+  return need_draw;
 }
 
 void LayerAnimationElement::GetTargetValue(TargetValue* target) const {
