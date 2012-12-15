141dc652d15ae62115a1911b02def374d1fb8384
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-15 22:04:12 +0000

    DownloadProxy should keep a strong reference to its associated web context https://bugs.webkit.org/show_bug.cgi?id=105107
    Reviewed by Alexey Proskuryakov.
    
    While this does create a ref-cycle, it's broken when the download completes, fails, is canceled or
    if the process that's doing the download crashes.
    
    * UIProcess/Downloads/DownloadProxy.cpp:
    (WebKit::DownloadProxy::processDidClose):
    (WebKit::DownloadProxy::didStart):
    (WebKit::DownloadProxy::didReceiveAuthenticationChallenge):
    (WebKit::DownloadProxy::didReceiveResponse):
    (WebKit::DownloadProxy::didReceiveData):
    (WebKit::DownloadProxy::shouldDecodeSourceDataOfMIMEType):
    (WebKit::DownloadProxy::decideDestinationWithSuggestedFilename):
    (WebKit::DownloadProxy::didCreateDestination):
    (WebKit::DownloadProxy::didFinish):
    (WebKit::DownloadProxy::didFail):
    (WebKit::DownloadProxy::didCancel):
    * UIProcess/Downloads/DownloadProxy.h:
    (DownloadProxy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Downloads/DownloadProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/Downloads/DownloadProxy.cpp
@@ -83,7 +83,7 @@ void DownloadProxy::processDidClose()
     if (!m_webContext)
         return;
 
-    m_webContext->downloadClient().processDidCrash(m_webContext, this);
+    m_webContext->downloadClient().processDidCrash(m_webContext.get(), this);
 }
 
 void DownloadProxy::didReceiveMessage(CoreIPC::Connection* connection, CoreIPC::MessageID messageID, CoreIPC::MessageDecoder& decoder)
@@ -103,7 +103,7 @@ void DownloadProxy::didStart(const ResourceRequest& request)
     if (!m_webContext)
         return;
 
-    m_webContext->downloadClient().didStart(m_webContext, this);
+    m_webContext->downloadClient().didStart(m_webContext.get(), this);
 }
 
 void DownloadProxy::didReceiveAuthenticationChallenge(const AuthenticationChallenge& authenticationChallenge, uint64_t challengeID)
@@ -114,7 +114,7 @@ void DownloadProxy::didReceiveAuthenticationChallenge(const AuthenticationChalle
     // FIXME (Multi-WebProcess): <rdar://problem/12239483> Downloads shouldn't be handled in the web process.
     // Once this is fixed, remove WebContext::deprecatedSharedProcess().
     RefPtr<AuthenticationChallengeProxy> authenticationChallengeProxy = AuthenticationChallengeProxy::create(authenticationChallenge, challengeID, m_webContext->deprecatedSharedProcess());
-    m_webContext->downloadClient().didReceiveAuthenticationChallenge(m_webContext, this, authenticationChallengeProxy.get());
+    m_webContext->downloadClient().didReceiveAuthenticationChallenge(m_webContext.get(), this, authenticationChallengeProxy.get());
 }
 
 void DownloadProxy::didReceiveResponse(const ResourceResponse& response)
@@ -122,7 +122,7 @@ void DownloadProxy::didReceiveResponse(const ResourceResponse& response)
     if (!m_webContext)
         return;
 
-    m_webContext->downloadClient().didReceiveResponse(m_webContext, this, response);
+    m_webContext->downloadClient().didReceiveResponse(m_webContext.get(), this, response);
 }
 
 void DownloadProxy::didReceiveData(uint64_t length)
@@ -130,7 +130,7 @@ void DownloadProxy::didReceiveData(uint64_t length)
     if (!m_webContext)
         return;
 
-    m_webContext->downloadClient().didReceiveData(m_webContext, this, length);
+    m_webContext->downloadClient().didReceiveData(m_webContext.get(), this, length);
 }
 
 void DownloadProxy::shouldDecodeSourceDataOfMIMEType(const String& mimeType, bool& result)
@@ -138,7 +138,7 @@ void DownloadProxy::shouldDecodeSourceDataOfMIMEType(const String& mimeType, boo
     if (!m_webContext)
         return;
 
-    result = m_webContext->downloadClient().shouldDecodeSourceDataOfMIMEType(m_webContext, this, mimeType);
+    result = m_webContext->downloadClient().shouldDecodeSourceDataOfMIMEType(m_webContext.get(), this, mimeType);
 }
 
 void DownloadProxy::decideDestinationWithSuggestedFilename(const String& filename, String& destination, bool& allowOverwrite, SandboxExtension::Handle& sandboxExtensionHandle)
@@ -146,7 +146,7 @@ void DownloadProxy::decideDestinationWithSuggestedFilename(const String& filenam
     if (!m_webContext)
         return;
 
-    destination = m_webContext->downloadClient().decideDestinationWithSuggestedFilename(m_webContext, this, filename, allowOverwrite);
+    destination = m_webContext->downloadClient().decideDestinationWithSuggestedFilename(m_webContext.get(), this, filename, allowOverwrite);
 
     if (!destination.isNull())
         SandboxExtension::createHandle(destination, SandboxExtension::WriteOnly, sandboxExtensionHandle);
@@ -157,7 +157,7 @@ void DownloadProxy::didCreateDestination(const String& path)
     if (!m_webContext)
         return;
 
-    m_webContext->downloadClient().didCreateDestination(m_webContext, this, path);
+    m_webContext->downloadClient().didCreateDestination(m_webContext.get(), this, path);
 }
 
 void DownloadProxy::didFinish()
@@ -165,7 +165,7 @@ void DownloadProxy::didFinish()
     if (!m_webContext)
         return;
 
-    m_webContext->downloadClient().didFinish(m_webContext, this);
+    m_webContext->downloadClient().didFinish(m_webContext.get(), this);
 
     // This can cause the DownloadProxy object to be deleted.
     m_downloadProxyMap.downloadFinished(this);
@@ -186,7 +186,7 @@ void DownloadProxy::didFail(const ResourceError& error, const CoreIPC::DataRefer
 
     m_resumeData = createWebData(resumeData);
 
-    m_webContext->downloadClient().didFail(m_webContext, this, error);
+    m_webContext->downloadClient().didFail(m_webContext.get(), this, error);
 
     // This can cause the DownloadProxy object to be deleted.
     m_downloadProxyMap.downloadFinished(this);
@@ -196,7 +196,7 @@ void DownloadProxy::didCancel(const CoreIPC::DataReference& resumeData)
 {
     m_resumeData = createWebData(resumeData);
 
-    m_webContext->downloadClient().didCancel(m_webContext, this);
+    m_webContext->downloadClient().didCancel(m_webContext.get(), this);
 
     // This can cause the DownloadProxy object to be deleted.
     m_downloadProxyMap.downloadFinished(this);
