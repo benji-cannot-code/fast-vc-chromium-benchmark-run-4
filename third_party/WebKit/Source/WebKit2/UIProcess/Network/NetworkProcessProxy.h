35cee1ef5c940272e18c20987bb78cad17a61082
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-15 23:11:52 +0000

    Add a DownloadProxyMap object to the NetworkProcessProxy object https://bugs.webkit.org/show_bug.cgi?id=105112
    Reviewed by Sam Weinig.
    
    * UIProcess/Network/NetworkProcessProxy.cpp:
    (WebKit::NetworkProcessProxy::createDownloadProxy):
    (WebKit::NetworkProcessProxy::didClose):
    * UIProcess/Network/NetworkProcessProxy.h:
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::createDownloadProxy):
    * UIProcess/WebProcessProxy.cpp:
    * UIProcess/WebProcessProxy.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Network/NetworkProcessProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/Network/NetworkProcessProxy.h
@@ -31,12 +31,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ChildProcessProxy.h"
 #include "Connection.h"
+#include "MessageReceiverMap.h"
 #include "ProcessLauncher.h"
 #include "WebProcessProxyMessages.h"
 #include <wtf/Deque.h>
 
 namespace WebKit {
 
+class DownloadProxy;
+class DownloadProxyMap;
 class WebContext;
 struct NetworkProcessCreationParameters;
 
@@ -46,6 +49,8 @@ public:
     ~NetworkProcessProxy();
 
     void getNetworkProcessConnection(PassRefPtr<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply>);
+
+    DownloadProxy* createDownloadProxy();
 
 #if PLATFORM(MAC)
     void setApplicationIsOccluded(bool);
@@ -75,6 +80,9 @@ private:
     
     unsigned m_numPendingConnectionRequests;
     Deque<RefPtr<Messages::WebProcessProxy::GetNetworkProcessConnection::DelayedReply> > m_pendingConnectionReplies;
+
+    CoreIPC::MessageReceiverMap m_messageReceiverMap;
+    OwnPtr<DownloadProxyMap> m_downloadProxyMap;
 };
 
 } // namespace WebKit
--- third_party/WebKit/Source/WebKit2/UIProcess/WebProcessProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebProcessProxy.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebProcessProxy_h
 
 #include "ChildProcessProxy.h"
-#include "DownloadProxyMap.h"
 #include "MessageReceiverMap.h"
 #include "PlatformProcessIdentifier.h"
 #include "PluginInfoStore.h"
@@ -58,6 +57,7 @@ class SecItemRequestData;
 class SecItemResponseData;
 #endif
 
+class DownloadProxyMap;
 class WebBackForwardListItem;
 class WebContext;
 class WebPageGroup;
