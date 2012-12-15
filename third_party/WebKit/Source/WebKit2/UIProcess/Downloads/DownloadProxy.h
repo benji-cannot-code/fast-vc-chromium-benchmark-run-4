35a9af86c87d150db222278b9ad0d57663e5f013
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-15 00:48:13 +0000

    DownloadProxy objects should be message receivers https://bugs.webkit.org/show_bug.cgi?id=105068
    Reviewed by Andreas Kling.
    
    This simplifies the WebContext message dispatch code somewhat.
    
    * UIProcess/Downloads/DownloadProxy.cpp:
    (WebKit::DownloadProxy::didReceiveMessage):
    (WebKit::DownloadProxy::didReceiveSyncMessage):
    * UIProcess/Downloads/DownloadProxy.h:
    (DownloadProxy):
    * UIProcess/WebContext.cpp:
    (WebKit::WebContext::createDownloadProxy):
    (WebKit::WebContext::didReceiveMessage):
    (WebKit::WebContext::didReceiveSyncMessage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137790 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Downloads/DownloadProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/Downloads/DownloadProxy.h
@@ -46,7 +46,7 @@ class WebContext;
 class WebData;
 class WebPageProxy;
 
-class DownloadProxy : public APIObject {
+class DownloadProxy : public APIObject, public CoreIPC::MessageReceiver {
 public:
     static const Type APIType = TypeDownload;
 
@@ -73,6 +73,10 @@ private:
     explicit DownloadProxy(WebContext*);
 
     virtual Type type() const { return APIType; }
+
+    // CoreIPC::MessageReceiver
+    virtual void didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::MessageDecoder&) OVERRIDE;
+    virtual void didReceiveSyncMessage(CoreIPC::Connection*, CoreIPC::MessageID, CoreIPC::MessageDecoder&, OwnPtr<CoreIPC::MessageEncoder>&) OVERRIDE;
 
     // Message handlers.
     void didStart(const WebCore::ResourceRequest&);
