7fdcddeb35825b6a5cc0c1ac9a4b8c7d61032f04
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-15 20:17:33 +0000

    Integrating the QuicStreamFactory into the network stack.
    Review URL: https://chromiumcodereview.appspot.com/11416058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173311 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_stream_factory_impl_job.cc
+++ net/http/http_stream_factory_impl_job.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_server_properties.h"
 #include "net/http/http_stream_factory.h"
 #include "net/http/http_stream_factory_impl_request.h"
+#include "net/quic/quic_http_stream.h"
 #include "net/socket/client_socket_handle.h"
 #include "net/socket/client_socket_pool.h"
 #include "net/socket/client_socket_pool_manager.h"
@@ -90,6 +91,8 @@ HttpStreamFactoryImpl::Job::Job(HttpStreamFactoryImpl* stream_factory,
       waiting_job_(NULL),
       using_ssl_(false),
       using_spdy_(false),
+      using_quic_(false),
+      quic_request_(session_->quic_stream_factory()),
       force_spdy_always_(HttpStreamFactory::force_spdy_always()),
       force_spdy_over_ssl_(HttpStreamFactory::force_spdy_over_ssl()),
       spdy_certificate_error_(OK),
@@ -158,7 +161,7 @@ LoadState HttpStreamFactoryImpl::Job::GetLoadState() const {
     case STATE_RESOLVE_PROXY_COMPLETE:
       return session_->proxy_service()->GetLoadState(pac_request_);
     case STATE_CREATE_STREAM_COMPLETE:
-      return connection_->GetLoadState();
+      return using_quic_ ? LOAD_STATE_CONNECTING : connection_->GetLoadState();
     case STATE_INIT_CONNECTION_COMPLETE:
       return LOAD_STATE_SENDING_REQUEST;
     default:
@@ -639,6 +642,12 @@ bool HttpStreamFactoryImpl::Job::ShouldForceSpdyWithoutSSL() const {
   return rv && !HttpStreamFactory::HasSpdyExclusion(origin_);
 }
 
+bool HttpStreamFactoryImpl::Job::ShouldForceQuic() const {
+  return session_->params().origin_port_to_force_quic_on == origin_.port()
+      && session_->params().origin_port_to_force_quic_on != 0
+      && proxy_info_.is_direct();
+}
+
 int HttpStreamFactoryImpl::Job::DoWaitForJob() {
   DCHECK(blocking_job_);
   next_state_ = STATE_WAIT_FOR_JOB_COMPLETE;
@@ -660,6 +669,12 @@ int HttpStreamFactoryImpl::Job::DoInitConnection() {
 
   using_ssl_ = request_info_.url.SchemeIs("https") || ShouldForceSpdySSL();
   using_spdy_ = false;
+
+  if (ShouldForceQuic()) {
+    next_state_ = STATE_CREATE_STREAM;
+    using_quic_ = true;
+    return OK;
+  }
 
   // Check first if we have a spdy session for this group.  If so, then go
   // straight to using that.
@@ -904,7 +919,7 @@ int HttpStreamFactoryImpl::Job::DoWaitingUserAction(int result) {
 
 int HttpStreamFactoryImpl::Job::DoCreateStream() {
   DCHECK(connection_->socket() || existing_spdy_session_ ||
-         existing_available_pipeline_);
+         existing_available_pipeline_ || using_quic_);
 
   next_state_ = STATE_CREATE_STREAM_COMPLETE;
 
@@ -915,6 +930,11 @@ int HttpStreamFactoryImpl::Job::DoCreateStream() {
     SetSocketMotivation();
 
   const ProxyServer& proxy_server = proxy_info_.proxy_server();
+
+  if (using_quic_) {
+    return quic_request_.Request(HostPortProxyPair(origin_, proxy_server),
+                                 net_log_, io_callback_);
+  }
 
   if (!using_spdy_) {
     bool using_proxy = (proxy_info_.is_http() || proxy_info_.is_https()) &&
@@ -997,6 +1017,10 @@ int HttpStreamFactoryImpl::Job::DoCreateStream() {
 int HttpStreamFactoryImpl::Job::DoCreateStreamComplete(int result) {
   if (result < 0)
     return result;
+
+  if (using_quic_) {
+    stream_ = quic_request_.ReleaseStream();
+  }
 
   session_->proxy_service()->ReportSuccess(proxy_info_);
   next_state_ = STATE_NONE;
--- net/quic/quic_stream_factory_test.cc
+++ net/quic/quic_stream_factory_test.cc
@@ -26,7 +26,8 @@ class QuicStreamFactoryTest : public ::testing::Test {
  protected:
   QuicStreamFactoryTest()
       : factory_(&host_resolver_, &socket_factory_,
-                 base::Bind(&QuicStreamFactoryTest::GenerateGuid), &clock_),
+                 base::Bind(&QuicStreamFactoryTest::GenerateGuid),
+                 new MockClock()),
         host_port_proxy_pair_(HostPortPair("www.google.com", 443),
                               ProxyServer::Direct()) {
   }
@@ -108,7 +109,6 @@ class QuicStreamFactoryTest : public ::testing::Test {
 
   MockHostResolver host_resolver_;
   MockClientSocketFactory socket_factory_;
-  MockClock clock_;
   QuicStreamFactory factory_;
   HostPortProxyPair host_port_proxy_pair_;
   BoundNetLog net_log_;
