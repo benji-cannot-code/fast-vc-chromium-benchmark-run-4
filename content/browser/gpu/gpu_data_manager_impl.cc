236d418556ee9c3d7a54fecbc0da8ea631a7de98
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-15 00:45:40 +0000

    Only collect GL/D3D context specific GPUInfo in GPU process
    Instead of collecting full GPUInfo.  Basic GPUInfo are already collected at Browser process startup time.
    
    This avoids sandbox issue with libpci on linux.  Also, it makes GPU startup faster.
    
    BUG=162928,165374
    TEST=manual about:gpu
    TBR=joi
    Review URL: https://codereview.chromium.org/11574049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173248 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl.cc
+++ content/browser/gpu/gpu_data_manager_impl.cc
@@ -133,7 +133,7 @@ void GpuDataManagerImpl::Initialize() {
     return;
 
   GPUInfo gpu_info;
-  gpu_info_collector::CollectPreliminaryGraphicsInfo(&gpu_info);
+  gpu_info_collector::CollectBasicGraphicsInfo(&gpu_info);
 #if defined(ARCH_CPU_X86_FAMILY)
   if (!gpu_info.gpu.vendor_id || !gpu_info.gpu.device_id)
     gpu_info.finalized = true;
@@ -191,7 +191,11 @@ void GpuDataManagerImpl::RequestCompleteGpuInfoIfNeeded() {
   complete_gpu_info_already_requested_ = true;
 
   GpuProcessHost::SendOnIO(
+#if defined(OS_WIN)
       GpuProcessHost::GPU_PROCESS_KIND_UNSANDBOXED,
+#else
+      GpuProcessHost::GPU_PROCESS_KIND_SANDBOXED,
+#endif
       CAUSE_FOR_GPU_LAUNCH_GPUDATAMANAGER_REQUESTCOMPLETEGPUINFOIFNEEDED,
       new GpuMsg_CollectGraphicsInfo());
 }
@@ -225,7 +229,7 @@ void GpuDataManagerImpl::UpdateGpuInfo(const GPUInfo& gpu_info) {
 
   {
     base::AutoLock auto_lock(gpu_info_lock_);
-    gpu_info_ = gpu_info;
+    gpu_info_collector::MergeGPUInfo(&gpu_info_, gpu_info);
     complete_gpu_info_already_requested_ =
         complete_gpu_info_already_requested_ || gpu_info_.finalized;
   }
@@ -470,9 +474,6 @@ void GpuDataManagerImpl::AppendGpuCommandLine(
   } else {
     command_line->AppendSwitchASCII(switches::kSupportsDualGpus, "false");
   }
-
-  if (!gpu_blacklist_.get() || !gpu_blacklist_->needs_more_info())
-    command_line->AppendSwitch(switches::kSkipGpuFullInfoCollection);
 
   if (!swiftshader_path.empty())
     command_line->AppendSwitchPath(switches::kSwiftShaderPath,
