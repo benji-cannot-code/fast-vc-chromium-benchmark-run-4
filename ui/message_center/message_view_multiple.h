7271beae7ebaadf3350afd2a1e9a97d73f5fc756
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-16 20:14:13 +0000

    Added support for secondary icon to multi-item notification view
    TBR=derat@chromium.org for c/b/ui/views/ash/balloon_view_ash.*
    BUG=161101
    
    Review URL: https://chromiumcodereview.appspot.com/11474003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173387 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_view_multiple.h
+++ ui/message_center/message_view_multiple.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace message_center {
 
-// An early version of the multiple message notification view.
+// View that displays multiple-item notifications.
 //
 // TODO(dharcourt): Rename MessageCenter and MessageView* to NotificationCenter
 // and NotificationView*?
