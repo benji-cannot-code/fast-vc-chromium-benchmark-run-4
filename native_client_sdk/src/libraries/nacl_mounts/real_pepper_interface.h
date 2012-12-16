c38be6ab8a4db5e0167a7e4f6b87657696172ade
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-16 22:17:49 +0000

    Add support for simple run of "main"
    Converts calls to PostMessage and LogMesssage to prints to
    stderr and stdout.
    
    TBR=binji@chromium.org
    BUG=165626
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/11592003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173399 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_mounts/real_pepper_interface.h
+++ native_client_sdk/src/libraries/nacl_mounts/real_pepper_interface.h
@@ -8,13 +8,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ppapi/c/ppb.h>
 #include <ppapi/c/ppb_core.h>
+#include <ppapi/c/ppb_console.h>
 #include <ppapi/c/ppb_message_loop.h>
+#include <ppapi/c/ppb_messaging.h>
+#include <ppapi/c/ppb_var.h>
 #include "pepper_interface.h"
 
+class RealConsoleInterface;
 class RealDirectoryReaderInterface;
 class RealFileIoInterface;
 class RealFileRefInterface;
 class RealFileSystemInterface;
+class RealMessagingInterface;
 class RealVarInterface;
 
 class RealPepperInterface : public PepperInterface {
@@ -25,10 +30,12 @@ class RealPepperInterface : public PepperInterface {
   virtual PP_Instance GetInstance();
   virtual void AddRefResource(PP_Resource);
   virtual void ReleaseResource(PP_Resource);
+  virtual ConsoleInterface* GetConsoleInterface();
   virtual FileSystemInterface* GetFileSystemInterface();
   virtual FileRefInterface* GetFileRefInterface();
   virtual FileIoInterface* GetFileIoInterface();
   virtual DirectoryReaderInterface* GetDirectoryReaderInterface();
+  virtual MessagingInterface* GetMessagingInterface();
   virtual VarInterface* GetVarInterface();
 
   int32_t InitializeMessageLoop();
@@ -37,10 +44,12 @@ class RealPepperInterface : public PepperInterface {
   PP_Instance instance_;
   const PPB_Core* core_interface_;
   const PPB_MessageLoop* message_loop_interface_;
+  RealConsoleInterface* console_interface_;
   RealDirectoryReaderInterface* directory_reader_interface_;
   RealFileIoInterface* fileio_interface_;
   RealFileRefInterface* fileref_interface_;
   RealFileSystemInterface* filesystem_interface_;
+  RealMessagingInterface* messaging_interface_;
   RealVarInterface* var_interface_;
 };
 
