bda72b6f806566599141606922a36329bae79747
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-16 15:41:47 +0000

    [GStreamer] create playbin in ::load(), not in player constructor https://bugs.webkit.org/show_bug.cgi?id=103898
    Patch by Víctor Manuel Jáquez Leal <vjaquez@igalia.com> on 2012-12-16
    Reviewed by Philippe Normand.
    
    This patch moves the instantiation of the playbin pipeline into the
    WebCore::MediaPlayerPrivateGStreamer::load() method, so the pipeline
    layout can reflect other predefined settings such as audio pitch
    preservation in variable playback rate scenarios
    
    No new tests. Covered by existing tests.
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::MediaPlayerPrivateGStreamer):
    (WebCore::MediaPlayerPrivateGStreamer::load):
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    (MediaPlayerPrivateGStreamer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137838 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp
@@ -246,8 +246,6 @@ MediaPlayerPrivateGStreamer::MediaPlayerPrivateGStreamer(MediaPlayer* player)
     , m_totalBytes(-1)
     , m_originalPreloadWasAutoAndWasOverridden(false)
 {
-    if (initializeGStreamerAndRegisterWebKitElements())
-        createGSTPlayBin();
 }
 
 MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer()
@@ -294,6 +292,8 @@ MediaPlayerPrivateGStreamer::~MediaPlayerPrivateGStreamer()
 
 void MediaPlayerPrivateGStreamer::load(const String& url)
 {
+    if (!initializeGStreamerAndRegisterWebKitElements())
+        return;
 
     KURL kurl(KURL(), url);
     String cleanUrl(url);
@@ -301,6 +301,13 @@ void MediaPlayerPrivateGStreamer::load(const String& url)
     // Clean out everything after file:// url path.
     if (kurl.isLocalFile())
         cleanUrl = cleanUrl.substring(0, kurl.pathEnd());
+
+    if (!m_playBin) {
+        createGSTPlayBin();
+        setDownloadBuffering();
+    }
+
+    ASSERT(m_playBin);
 
     m_url = KURL(KURL(), cleanUrl);
     g_object_set(m_playBin, "uri", cleanUrl.utf8().data(), NULL);
@@ -1745,13 +1752,10 @@ MediaPlayer::MovieLoadType MediaPlayerPrivateGStreamer::movieLoadType() const
     return MediaPlayer::Download;
 }
 
-void MediaPlayerPrivateGStreamer::setPreload(MediaPlayer::Preload preload)
+void MediaPlayerPrivateGStreamer::setDownloadBuffering()
 {
-    m_originalPreloadWasAutoAndWasOverridden = m_preload != preload && m_preload == MediaPlayer::Auto;
-
-    m_preload = preload;
-
-    ASSERT(m_playBin);
+    if (!m_playBin)
+        return;
 
     GstPlayFlags flags;
     g_object_get(m_playBin, "flags", &flags, NULL);
@@ -1762,6 +1766,15 @@ void MediaPlayerPrivateGStreamer::setPreload(MediaPlayer::Preload preload)
         LOG_MEDIA_MESSAGE("Disabling on-disk buffering");
         g_object_set(m_playBin, "flags", flags & ~GST_PLAY_FLAG_DOWNLOAD, NULL);
     }
+}
+
+void MediaPlayerPrivateGStreamer::setPreload(MediaPlayer::Preload preload)
+{
+    m_originalPreloadWasAutoAndWasOverridden = m_preload != preload && m_preload == MediaPlayer::Auto;
+
+    m_preload = preload;
+
+    setDownloadBuffering();
 
     if (m_delayingLoad && m_preload != MediaPlayer::None) {
         m_delayingLoad = false;
