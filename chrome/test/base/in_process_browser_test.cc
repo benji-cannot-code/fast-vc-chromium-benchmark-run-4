5325d4182b6fbdd069b823e9763f13bf48ac875e
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 21:57:21 +0000

    Fix test-running in --single-process mode.
    * Avoid destroying the ContentRendererClient, since that asserts we're on the renderer's thread
    * Avoid asserting that all RenderProcessHosts are destroyed, since r170644 avoided destroying them in single-process mode.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11567053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173543 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/file_path.h"
 #include "base/file_util.h"
+#include "base/lazy_instance.h"
 #include "base/path_service.h"
 #include "base/string_number_conversions.h"
 #include "base/test/test_file_util.h"
@@ -60,6 +61,10 @@ namespace {
 
 // Passed as value of kTestType.
 const char kBrowserTestType[] = "browser";
+
+// Used when running in single-process mode.
+base::LazyInstance<chrome::ChromeContentRendererClient>::Leaky
+    g_chrome_content_renderer_client = LAZY_INSTANCE_INITIALIZER;
 
 }  // namespace
 
@@ -112,10 +117,8 @@ void InProcessBrowserTest::SetUp() {
   // Single-process mode is not set in BrowserMain, so process it explicitly,
   // and set up renderer.
   if (command_line->HasSwitch(switches::kSingleProcess)) {
-    single_process_renderer_client_.reset(
-        new chrome::ChromeContentRendererClient);
     content::GetContentClient()->set_renderer_for_testing(
-        single_process_renderer_client_.get());
+        &g_chrome_content_renderer_client.Get());
   }
 
 #if defined(OS_CHROMEOS)
