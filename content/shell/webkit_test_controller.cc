cc962970d1080007d90d3c4d9dfed53657493bce
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 09:50:06 +0000

    [content shell] if a layout test runs notifyDone() before it has finished loading, wait for the page to finish loading
    BUG=111316
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/11594006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173436 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_controller.cc
+++ content/shell/webkit_test_controller.cc
@@ -188,6 +188,7 @@ bool WebKitTestController::ResetAfterLayoutTest() {
   is_printing_ = false;
   should_stay_on_page_after_handling_before_unload_ = false;
   wait_until_done_ = false;
+  did_finish_load_ = false;
   prefs_.reset(new ShellWebPreferences);
   {
     base::AutoLock lock(lock_);
@@ -298,6 +299,7 @@ void WebKitTestController::TimeoutHandler() {
 }
 
 void WebKitTestController::OnDidFinishLoad() {
+  did_finish_load_ = true;
   if (wait_until_done_)
     return;
   CaptureDump();
@@ -376,6 +378,10 @@ void WebKitTestController::OnNotifyDone() {
   if (!wait_until_done_)
     return;
   watchdog_.Cancel();
+  if (!did_finish_load_) {
+    wait_until_done_ = false;
+    return;
+  }
   CaptureDump();
 }
 
