5cef97f60849bf4f64e4216241dda2d55754a101
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-17 10:37:15 +0000

    Fix the Qt Windows build after r137803 https://bugs.webkit.org/show_bug.cgi?id=105152
    Reviewed by Kentaro Hara.
    
    * Shared/CacheModel.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137889 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/CacheModel.h
+++ third_party/WebKit/Source/WebKit2/Shared/CacheModel.h
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CacheModel_h
 #define CacheModel_h
 
+#include <stdint.h>
+
 namespace WebKit {
 
 enum CacheModel {
