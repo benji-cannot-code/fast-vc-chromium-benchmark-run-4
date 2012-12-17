ed6f089e33d5bf64d1694d231422cd298980e6a4
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 19:21:18 +0000

    Enables storing shelf's auto_hide_behavior and alignment per display.
    BUG=165419
    TEST=on a dev-mode device, change the launcher's auto-hide behavior and see how it's stored in Preferences
    
    Review URL: https://codereview.chromium.org/11570017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173508 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/app_list_controller.cc
+++ ash/wm/app_list_controller.cc
@@ -286,7 +286,7 @@ void AppListController::OnWidgetClosing(views::Widget* widget) {
 
 ////////////////////////////////////////////////////////////////////////////////
 // AppListController, ShellObserver implementation:
-void AppListController::OnShelfAlignmentChanged() {
+void AppListController::OnShelfAlignmentChanged(aura::RootWindow* root_window) {
   if (view_) {
     view_->SetBubbleArrowLocation(GetBubbleArrowLocation(
         view_->GetWidget()->GetNativeView()));
