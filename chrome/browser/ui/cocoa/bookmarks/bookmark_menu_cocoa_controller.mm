c81b9edbc37047b861066b96e1a803b26c0023a3
yoichio@chromium.org <yoichio@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 06:29:55 +0000

    "Open All Bookmarks in Incognito Window" opens only valid URLs except it can't be opened in incognito window.
    BUG=106607
    
    Review URL: https://chromiumcodereview.appspot.com/11572031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173422 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_menu_cocoa_controller.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_menu_cocoa_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/app/chrome_command_ids.h"  // IDC_BOOKMARK_MENU
 #import "chrome/browser/app_controller_mac.h"
 #include "chrome/browser/bookmarks/bookmark_model.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
@@ -121,7 +122,7 @@ const NSUInteger kMaximumMenuPixelsWide = 300;
   if (!node || !browser)
     return; // shouldn't be reached
 
-  chrome::OpenAll(NULL, browser, node, disposition);
+  chrome::OpenAll(NULL, browser, node, disposition, browser->profile());
 
   if (disposition == NEW_FOREGROUND_TAB) {
     content::RecordAction(UserMetricsAction("OpenAllBookmarks"));
