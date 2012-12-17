1af83ef1faf7730ba1995372f0d4d84bbf6b2b61
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 00:16:22 +0000

    Disable the script bubble for now.
    Will revisit for the next milestone once we have addressed some feedback we got.
    
    Also remove an artificial bump in location bar event count in the Script _Badge_ unit test, that was accounting for a bump due to the Script _Bubble_. Once the Script Bubble is online again I hope to have converted it to use the Observer model so as to not have to rely on these LocationBarView updates (so I don't expect the hack to make a comeback).
    
    BUG=153137
    TEST=The script bubble should not be seen.
    
    Review URL: https://chromiumcodereview.appspot.com/11566029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173401 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/feature_switch.cc
+++ chrome/common/extensions/feature_switch.cc
@@ -32,11 +32,7 @@ class CommonSwitches {
             FeatureSwitch::DEFAULT_DISABLED),
         script_bubble(
             switches::kScriptBubble,
-#if !defined(OS_MACOSX)
             FeatureSwitch::DEFAULT_ENABLED),
-#else
-            FeatureSwitch::DEFAULT_DISABLED),
-#endif
         sideload_wipeout(
             switches::kSideloadWipeout,
             base::FieldTrialList::FindFullName("SideloadWipeout") == "Enabled" ?
