80999d3862f91772735e03546be0615d8c9a0e4a
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-17 12:08:39 +0000

    Cleanup: Remote metadata parameter from ApplyLocalChange
    As it's no longer necessary to pass in the file size to the drive uploader.
    
    BUG=none
    TEST=LocalFileSyncServiceTest.*
    
    Review URL: https://codereview.chromium.org/11602004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173450 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/mock_local_change_processor.cc
+++ chrome/browser/sync_file_system/mock_local_change_processor.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop_proxy.h"
 #include "webkit/fileapi/file_system_url.h"
 #include "webkit/fileapi/syncable/file_change.h"
-#include "webkit/fileapi/syncable/sync_file_metadata.h"
 
 using ::testing::_;
 using ::testing::Invoke;
@@ -19,7 +18,7 @@ using ::testing::Return;
 namespace sync_file_system {
 
 MockLocalChangeProcessor::MockLocalChangeProcessor() {
-  ON_CALL(*this, ApplyLocalChange(_, _, _, _, _))
+  ON_CALL(*this, ApplyLocalChange(_, _, _, _))
       .WillByDefault(Invoke(this,
                             &MockLocalChangeProcessor::ApplyLocalChangeStub));
 }
@@ -30,7 +29,6 @@ MockLocalChangeProcessor::~MockLocalChangeProcessor() {
 void MockLocalChangeProcessor::ApplyLocalChangeStub(
     const fileapi::FileChange& change,
     const FilePath& local_file_path,
-    const fileapi::SyncFileMetadata& local_file_metadata,
     const fileapi::FileSystemURL& url,
     const fileapi::SyncStatusCallback& callback) {
   base::MessageLoopProxy::current()->PostTask(
--- chrome/browser/sync_file_system/mock_local_change_processor.h
+++ chrome/browser/sync_file_system/mock_local_change_processor.h
@@ -18,10 +18,9 @@ class MockLocalChangeProcessor : public LocalChangeProcessor {
   virtual ~MockLocalChangeProcessor();
 
   // LocalChangeProcessor override.
-  MOCK_METHOD5(ApplyLocalChange,
+  MOCK_METHOD4(ApplyLocalChange,
                void(const fileapi::FileChange& change,
                     const FilePath& local_file_path,
-                    const fileapi::SyncFileMetadata& local_file_metadata,
                     const fileapi::FileSystemURL& url,
                     const fileapi::SyncStatusCallback& callback));
 
@@ -29,7 +28,6 @@ class MockLocalChangeProcessor : public LocalChangeProcessor {
   void ApplyLocalChangeStub(
       const fileapi::FileChange& change,
       const FilePath& local_file_path,
-      const fileapi::SyncFileMetadata& local_file_metadata,
       const fileapi::FileSystemURL& url,
       const fileapi::SyncStatusCallback& callback);
 
