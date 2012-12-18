ecf43fd4d04a8350c339831544c667600ecc4a85
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 12:57:16 +0000

    Web Inspector: [Network] Ordering of cookies displayed is nondeterministic. https://bugs.webkit.org/show_bug.cgi?id=105272
    Patch by Eugene Klyuchnikov <eustas@chromium.org> on 2012-12-18
    Reviewed by Alexander Pavlov.
    
    This patch fixes double-selection of request and sets default ordering
    of cookies.
    
    * inspector/front-end/CookiesTable.js: By default sort by name.
    * inspector/front-end/NetworkPanel.js: Remove redundant member, make
    rows "not-selectable", activate request selection on "mouse-down".
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138011 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkPanel.js
@@ -49,7 +49,6 @@ WebInspector.NetworkLogView = function()
     WebInspector.View.call(this);
     this.registerRequiredCSS("networkLogView.css");
 
-    this._allowRequestSelection = false;
     this._requests = [];
     this._requestsById = {};
     this._requestsByURL = {};
@@ -1490,7 +1489,6 @@ WebInspector.NetworkPanel.prototype = {
 
         this._networkLogView.switchToDetailedView();
         this._networkLogView.allowPopover = true;
-        this._networkLogView._allowRequestSelection = false;
     },
 
     _toggleViewingRequestMode: function()
@@ -1502,7 +1500,6 @@ WebInspector.NetworkPanel.prototype = {
         this.element.addStyleClass("viewing-resource");
         this.splitView.showMainElement();
         this._networkLogView.allowPopover = false;
-        this._networkLogView._allowRequestSelection = true;
         this._networkLogView.switchToBriefView();
     },
 
@@ -1847,8 +1844,9 @@ WebInspector.NetworkDataGridNode.prototype = {
         this._sizeCell = this._createDivInTD("size");
         this._timeCell = this._createDivInTD("time");
         this._createTimelineCell();
-        this._nameCell.addEventListener("click", this.select.bind(this), false);
+        this._nameCell.addEventListener("mousedown", this.select.bind(this), false);
         this._nameCell.addEventListener("dblclick", this._openInNewTab.bind(this), false);
+        this.selectable = false;
     },
 
     isFilteredOut: function()
@@ -1883,11 +1881,6 @@ WebInspector.NetworkDataGridNode.prototype = {
     _openInNewTab: function()
     {
         InspectorFrontendHost.openInNewTab(this._request.url);
-    },
-
-    get selectable()
-    {
-        return this._parentView._allowRequestSelection && !this.isFilteredOut();
     },
 
     _createDivInTD: function(columnIdentifier)
