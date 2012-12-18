b42c7fcda14d71eb4133008c8b87b0948b4b3a80
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 19:08:41 +0000

    [CSS Exclusions] shape-inside layout fails to adjust first line correctly for writing-mode: vertical-rl https://bugs.webkit.org/show_bug.cgi?id=104419
    Patch by Hans Muller <hmuller@adobe.com> on 2012-12-18
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    ExclusionShapes no longer maintain a private "internal" coordinate system,
    they're now defined in logical coordinates. The createExclusionShape() method
    now handles the one-time conversion from physical to logical coordinates.
    
    Test: fast/exclusions/shape-inside/shape-inside-rounded-rectangle-fit-003.html
    
    * rendering/ExclusionPolygon.cpp:
    (WebCore::ExclusionPolygon::getExcludedIntervals): Removed logical to internal coordinate conversions.
    (WebCore::ExclusionPolygon::getIncludedIntervals): Ditto.
    (WebCore::ExclusionPolygon::firstIncludedIntervalLogicalTop): Ditto.
    * rendering/ExclusionPolygon.h:
    * rendering/ExclusionRectangle.cpp:
    (WebCore::ExclusionRectangle::getExcludedIntervals): Removed logical to internal coordinate conversions.
    (WebCore::ExclusionRectangle::getIncludedIntervals): Ditto.
    (WebCore::ExclusionRectangle::firstIncludedIntervalLogicalTop): Ditto.
    * rendering/ExclusionRectangle.h:
    * rendering/ExclusionShape.cpp:
    (WebCore::physicalRectToLogical): Convert a FloatRect defined with coordinates to logical coordinates.
    (WebCore::physicalPointToLogical): Similar.
    (WebCore::physicalSizeToLogical): Simlar.
    (WebCore::ExclusionShape::createExclusionShape): Convert shapes from physical to logical coordinates.
    * rendering/ExclusionShape.h:
    (ExclusionShape): Removed internal to logical coordinate conversion utility methods.
    
    LayoutTests:
    
    For the vertical writing modes, verify that rounded rectangle shape-inside content is
    adjusted in the logical down direction when it will not fit betwen the rounded corners.
    
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-fit-003-expected.html: Added.
    * fast/exclusions/shape-inside/shape-inside-rounded-rectangle-fit-003.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138043 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/ExclusionPolygon.cpp
+++ third_party/WebKit/Source/WebCore/rendering/ExclusionPolygon.cpp
@@ -328,8 +328,8 @@ void ExclusionPolygon::getExcludedIntervals(float logicalTop, float logicalHeigh
     if (isEmpty())
         return;
 
-    float y1 = minYForLogicalLine(logicalTop, logicalHeight);
-    float y2 = maxYForLogicalLine(logicalTop, logicalHeight);
+    float y1 = logicalTop;
+    float y2 = y1 + logicalHeight;
 
     Vector<ExclusionInterval> y1XIntervals, y2XIntervals;
     computeXIntersections(y1, true, y1XIntervals);
@@ -355,8 +355,8 @@ void ExclusionPolygon::getIncludedIntervals(float logicalTop, float logicalHeigh
     if (isEmpty())
         return;
 
-    float y1 = minYForLogicalLine(logicalTop, logicalHeight);
-    float y2 = maxYForLogicalLine(logicalTop, logicalHeight);
+    float y1 = logicalTop;
+    float y2 = y1 + logicalHeight;
 
     Vector<ExclusionInterval> y1XIntervals, y2XIntervals;
     computeXIntersections(y1, true, y1XIntervals);
@@ -384,8 +384,8 @@ bool ExclusionPolygon::firstIncludedIntervalLogicalTop(float minLogicalIntervalT
     if (minLogicalIntervalSize.width() > m_boundingBox.width())
         return false;
 
-    float minY = minYForLogicalLine(minLogicalIntervalTop, minLogicalIntervalSize.height());
-    float maxY = maxYForLogicalLine(minLogicalIntervalTop, minLogicalIntervalSize.height());
+    float minY = std::max(m_boundingBox.y(), minLogicalIntervalTop);
+    float maxY = minY + minLogicalIntervalSize.height();
     if (minY < m_boundingBox.y() || maxY > m_boundingBox.maxY())
         return false;
 
