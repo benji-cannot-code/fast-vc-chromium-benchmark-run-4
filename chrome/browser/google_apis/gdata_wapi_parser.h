7128a636bceeb59ccef2db9b0379c625246462ca
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 07:32:12 +0000

    google_apis: Make default constructors of parsed WAPI objects public
    We should expose the default constructors so that we can construct
    parsed objects in tests.
    
    BUG=166572
    TEST=none
    
    Review URL: https://codereview.chromium.org/11570059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173673 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/gdata_wapi_parser.h
+++ chrome/browser/google_apis/gdata_wapi_parser.h
@@ -382,6 +382,7 @@ class FeedEntry {
 // refers to a file and a directory.
 class ResourceEntry : public FeedEntry {
  public:
+  ResourceEntry();
   virtual ~ResourceEntry();
 
   // Extracts "entry" dictionary from the JSON value, and parse the contents,
@@ -569,8 +570,6 @@ class ResourceEntry : public FeedEntry {
   friend class ResourceList;
   friend class ResumeUploadOperation;
 
-  ResourceEntry();
-
   // Fills the remaining fields where JSONValueConverter cannot catch.
   void FillRemainingFields();
 
@@ -607,6 +606,7 @@ class ResourceEntry : public FeedEntry {
 // URLs provided by GetNextFeedURL() method.
 class ResourceList : public FeedEntry {
  public:
+  ResourceList();
   virtual ~ResourceList();
 
   // Extracts "feed" dictionary from the JSON value, and parse the contents,
@@ -677,8 +677,6 @@ class ResourceList : public FeedEntry {
   }
 
  private:
-  ResourceList();
-
   // Parses and initializes data members from content of |value|.
   // Return false if parsing fails.
   bool Parse(const base::Value& value);
@@ -810,6 +808,7 @@ class InstalledApp {
 // account.
 class AccountMetadataFeed {
  public:
+  AccountMetadataFeed();
   virtual ~AccountMetadataFeed();
 
   // Creates feed from parsed JSON Value.  You should call this
@@ -854,8 +853,6 @@ class AccountMetadataFeed {
       base::JSONValueConverter<AccountMetadataFeed>* converter);
 
  private:
-  AccountMetadataFeed();
-
   // Parses and initializes data members from content of |value|.
   // Return false if parsing fails.
   bool Parse(const base::Value& value);
