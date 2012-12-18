66ec25df56404e699642cd43a344445267930cb2
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 22:49:40 +0000

    SunSpider/date-format-tofte shouldn't compile each of the tiny worthless eval's only to OSR exit in the prologue every time https://bugs.webkit.org/show_bug.cgi?id=105335
    Reviewed by Geoffrey Garen.
    
    The first thing I did was restructure the logic of canInlineResolveOperations(),
    because I didn't understand it. This was relevant because the OSR exits are
    caused by a resolve that the DFG cannot handle.
            
    I was then going to make it so that we didn't compile the resolve at all, but
    realized that this would not be the best fix: it didn't seem sensible to me to
    be optimizing these evals after only 60 invocations. Evals should have a higher
    threshold, since they often contain code for which the baseline JIT does a
    pretty good job already (if all you've got is a single heap access or a single
    hard-to-inline call, then the baseline JIT has got you covered), and typically
    if we see one eval code block we expect to see more (from the same eval site):
    so our typical low threshold could lead to a *lot* of compilation. As such, the
    main effect of this patch is to introduce an evalThresholdMultiplier, which is
    now set to 10.
            
    This is a ~5% speed-up on data-format-tofte. No regressions anywhere as far as
    I can see.
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::codeTypeThresholdMultiplier):
    (JSC):
    (JSC::CodeBlock::optimizationThresholdScalingFactor):
    (JSC::CodeBlock::exitCountThresholdForReoptimization):
    (JSC::CodeBlock::exitCountThresholdForReoptimizationFromLoop):
    * bytecode/CodeBlock.h:
    (CodeBlock):
    * dfg/DFGCapabilities.h:
    (JSC::DFG::canInlineResolveOperations):
    * dfg/DFGOSRExitCompiler.cpp:
    * runtime/Options.h:
    (JSC):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.cpp
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -2945,6 +2945,14 @@ void CodeBlock::countReoptimization()
         m_reoptimizationRetryCounter = Options::reoptimizationRetryCounterMax();
 }
 
+int32_t CodeBlock::codeTypeThresholdMultiplier() const
+{
+    if (codeType() == EvalCode)
+        return Options::evalThresholdMultiplier();
+    
+    return 1;
+}
+
 double CodeBlock::optimizationThresholdScalingFactor()
 {
     // This expression arises from doing a least-squares fit of
@@ -3015,9 +3023,9 @@ double CodeBlock::optimizationThresholdScalingFactor()
     
     double result = d + a * sqrt(instructionCount + b) + c * instructionCount;
 #if ENABLE(JIT_VERBOSE_OSR)
-    dataLog(*this, ": instruction count is ", instructionCount, ", scaling execution counter by ", result, "\n");
+    dataLog(*this, ": instruction count is ", instructionCount, ", scaling execution counter by ", result, " * ", codeTypeThresholdMultiplier(), "\n");
 #endif
-    return result;
+    return result * codeTypeThresholdMultiplier();
 }
 
 static int32_t clipThreshold(double threshold)
@@ -3103,12 +3111,12 @@ uint32_t CodeBlock::adjustedExitCountThreshold(uint32_t desiredThreshold)
 
 uint32_t CodeBlock::exitCountThresholdForReoptimization()
 {
-    return adjustedExitCountThreshold(Options::osrExitCountForReoptimization());
+    return adjustedExitCountThreshold(Options::osrExitCountForReoptimization() * codeTypeThresholdMultiplier());
 }
 
 uint32_t CodeBlock::exitCountThresholdForReoptimizationFromLoop()
 {
-    return adjustedExitCountThreshold(Options::osrExitCountForReoptimizationFromLoop());
+    return adjustedExitCountThreshold(Options::osrExitCountForReoptimizationFromLoop() * codeTypeThresholdMultiplier());
 }
 
 bool CodeBlock::shouldReoptimizeNow()
