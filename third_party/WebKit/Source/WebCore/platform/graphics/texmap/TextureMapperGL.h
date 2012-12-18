fc544ab74abb35a5a13fe8048ceda7f4d6e831e6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 05:23:27 +0000

    REGRESSION(r137442): Crash because of an uninitialized member. https://bugs.webkit.org/show_bug.cgi?id=105246
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-12-17
    Reviewed by Noam Rosenthal.
    
    TextureMapperGL defines m_context although TextureMapper defines m_context.
    r137442 removes the initialization code in TextureMapperGL, which is
    what causes this regression. This patch removes the duplicated member
    in TextureMapperGL.
    
    No new tests. Covered by existing tests.
    
    * platform/graphics/texmap/TextureMapper.cpp:
    (WebCore::TextureMapper::TextureMapper):
    * platform/graphics/texmap/TextureMapper.h:
    (WebCore::TextureMapper::setGraphicsContext):
    (WebCore::TextureMapper::graphicsContext):
    (TextureMapper):
    * platform/graphics/texmap/TextureMapperGL.h:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperGL.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperGL.h
@@ -34,7 +34,6 @@ namespace WebCore {
 class CustomFilterProgram;
 class CustomFilterCompiledProgram;
 class TextureMapperGLData;
-class GraphicsContext;
 class TextureMapperShaderProgram;
 
 // An OpenGL-ES2 implementation of TextureMapper.
@@ -65,9 +64,7 @@ public:
     virtual void endClip() OVERRIDE;
     virtual IntSize maxTextureSize() const OVERRIDE { return IntSize(2000, 2000); }
     virtual PassRefPtr<BitmapTexture> createTexture() OVERRIDE;
-    virtual GraphicsContext* graphicsContext() OVERRIDE { return m_context; }
     inline GraphicsContext3D* graphicsContext3D() const { return m_context3D.get(); }
-    virtual void setGraphicsContext(GraphicsContext* context) OVERRIDE { m_context = context; }
 
 #if ENABLE(CSS_FILTERS)
     void drawFiltered(const BitmapTexture& sourceTexture, const BitmapTexture& contentTexture, const FilterOperation&, int pass);
@@ -123,7 +120,6 @@ private:
     void bindDefaultSurface();
     ClipStack& clipStack();
     inline TextureMapperGLData& data() { return *m_data; }
-    GraphicsContext* m_context;
     RefPtr<GraphicsContext3D> m_context3D;
     TextureMapperGLData* m_data;
     ClipStack m_clipStack;
