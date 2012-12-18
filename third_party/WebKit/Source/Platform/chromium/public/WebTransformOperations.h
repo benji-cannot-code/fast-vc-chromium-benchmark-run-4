32de87c55a7e83168e74d5783f1d9b13c08f1df4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 19:44:00 +0000

    [chromium] Fix include style in WebTransformOperations.h https://bugs.webkit.org/show_bug.cgi?id=105259
    Patch by James Robinson <jamesr@chromium.org> on 2012-12-18
    Reviewed by Adrienne Walker.
    
    Changes include of WebTransformationMatrix.h from '#include <public/..h>' to '#include "....h". The latter
    always works, since #include "foo.h" always finds foo.h in the same directory, and doesn't require all
    downstream users of this header to have Source/Platform/chromium/ on the include path.
    
    * chromium/public/WebTransformOperations.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebTransformOperations.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebTransformOperations.h
@@ -26,7 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WebTransformOperations_h
 #define WebTransformOperations_h
 
-#include <public/WebTransformationMatrix.h>
+#include "WebTransformationMatrix.h"
 
 namespace WebKit {
 
