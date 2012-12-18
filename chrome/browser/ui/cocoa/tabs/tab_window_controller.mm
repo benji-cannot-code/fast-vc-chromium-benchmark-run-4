6738e580391f599d15b1b94a5001fa7543f5b092
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 21:34:03 +0000

    mac: Fix key view loop regression.
    This bit got lost when moving from a xib file to code.
    
    BUG=166385
    TEST=Focus omnibox, keep tab pressed. Focus should travel through the web contents too. Open a new tab, repeat.
    
    Review URL: https://codereview.chromium.org/11636003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173785 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_window_controller.mm
+++ chrome/browser/ui/cocoa/tabs/tab_window_controller.mm
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       [[FramedBrowserWindow alloc] initWithContentRect:contentRect
                                            hasTabStrip:hasTabStrip]);
   [window setReleasedWhenClosed:YES];
+  [window setAutorecalculatesKeyViewLoop:YES];
 
   if ((self = [super initWithWindow:window])) {
     [[self window] setDelegate:self];
