cd3111eca72f957dfa6eeb1f049e8697de25cbbb
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 07:32:52 +0000

    [Cleanup] Remove unused local variable from MediaGalleriesDialogController.
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11622013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173677 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_gallery/media_galleries_dialog_controller.cc
+++ chrome/browser/media_gallery/media_galleries_dialog_controller.cc
@@ -218,9 +218,6 @@ void MediaGalleriesDialogController::OnRemovableStorageDetached(
 
 void MediaGalleriesDialogController::InitializePermissions() {
   const MediaGalleriesPrefInfoMap& galleries = preferences_->known_galleries();
-  const MediaGalleryPrefIdSet permitted_galleries =
-      preferences_->GalleriesForExtension(*extension_);
-
   for (MediaGalleriesPrefInfoMap::const_iterator iter = galleries.begin();
        iter != galleries.end();
        ++iter) {
