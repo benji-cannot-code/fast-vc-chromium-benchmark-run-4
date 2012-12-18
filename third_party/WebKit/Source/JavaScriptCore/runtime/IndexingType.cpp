717e6a08b640b1672cdc077c463aa41981aed8f1
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 22:32:52 +0000

    Convert indexingTypeToString to IndexingTypeDump https://bugs.webkit.org/show_bug.cgi?id=105351
    Reviewed by Mark Hahnenberg.
    
    This gets rid of another case of static char buffer[thingy].
    
    * dfg/DFGGraph.cpp:
    (JSC::DFG::Graph::dump):
    * runtime/IndexingType.cpp:
    (JSC::dumpIndexingType):
    * runtime/IndexingType.h:
    (JSC):
    * runtime/JSValue.cpp:
    (JSC::JSValue::dump):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/IndexingType.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/IndexingType.cpp
@@ -72,9 +72,8 @@ IndexingType leastUpperBoundOfIndexingTypeAndValue(IndexingType indexingType, JS
     return leastUpperBoundOfIndexingTypeAndType(indexingType, speculationFromValue(value));
 }
 
-const char* indexingTypeToString(IndexingType indexingType)
+void dumpIndexingType(PrintStream& out, IndexingType indexingType)
 {
-    static char result[128];
     const char* basicName;
     switch (indexingType & AllArrayTypes) {
     case NonArray:
@@ -121,11 +120,7 @@ const char* indexingTypeToString(IndexingType indexingType)
         break;
     }
     
-    snprintf(
-        result, sizeof(result), "%s%s", basicName,
-        (indexingType & MayHaveIndexedAccessors) ? "|MayHaveIndexedAccessors" : "");
-    
-    return result;
+    out.printf("%s%s", basicName, (indexingType & MayHaveIndexedAccessors) ? "|MayHaveIndexedAccessors" : "");
 }
 
 } // namespace JSC
