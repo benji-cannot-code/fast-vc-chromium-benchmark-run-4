aa2372c0a0cc299337ac8666820c2fbbf55ef14e
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 10:35:21 +0000

    Add file browser handler resource throttler for intercepting downloads.
    The throttle will intercept requests with MIME types handleable by quick office
    extension, cancel them and send an event to the quick office extension to
    handle the request.
    
    BUG=162736
    TEST=unit_tests:FileBrowserResourceThrotlleTest;
    browser_tests:FileBrowserResourceThrottleExtensionApiTest
    
    
    Review URL: https://chromiumcodereview.appspot.com/11280264
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173713 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.cc
+++ android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.cc
@@ -238,6 +238,7 @@ void AwResourceDispatcherHostDelegate::DownloadStarting(
     int route_id,
     int request_id,
     bool is_content_initiated,
+    bool must_download,
     ScopedVector<content::ResourceThrottle>* throttles) {
   GURL url(request->url());
   std::string user_agent;
