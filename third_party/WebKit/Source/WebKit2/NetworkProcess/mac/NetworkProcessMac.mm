6ce578555c761d7391c98b1a633fd8f63d205fc9
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 03:44:54 +0000

    Try to fix the Mac build.
    * NetworkProcess/mac/NetworkProcessMac.mm:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/NetworkProcess/mac/NetworkProcessMac.mm
+++ third_party/WebKit/Source/WebKit2/NetworkProcess/mac/NetworkProcessMac.mm
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "NetworkProcessCreationParameters.h"
 #import "PlatformCertificateInfo.h"
 #import "SandboxExtension.h"
-#import <Foundation/NSURLRequestPrivate.h>
 #import <WebCore/LocalizedStrings.h>
 #import <WebKitSystemInterface.h>
 #import <mach/host_info.h>
@@ -41,6 +40,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <wtf/text/WTFString.h>
 
 using namespace WebCore;
+
+@interface NSURLRequest (Details) 
++ (void)setAllowsSpecificHTTPSCertificate:(NSArray *)allow forHost:(NSString *)host;
+@end
 
 namespace WebKit {
 
