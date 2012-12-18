d28d4a544ceb32de9b51854723438ccbaa52da0e
deymo@chromium.org <deymo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 06:55:41 +0000

    bluetooth: ConnectErrorCode added in the BluetoothDevice::Connect error callback
    Currently, only the interface is changed to accept an error callback with one
    ConnectErrorCode argument, but all the functions will pass UNKNOW_ERROR.
    
    BUG=chromium-os:27902
    TEST=build it for chromeos daisy.
    
    Review URL: https://chromiumcodereview.appspot.com/11485014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173656 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth_device_win.cc
+++ device/bluetooth/bluetooth_device_win.cc
@@ -66,7 +66,7 @@ bool BluetoothDeviceWin::ExpectingConfirmation() const {
 void BluetoothDeviceWin::Connect(
     PairingDelegate* pairing_delegate,
     const base::Closure& callback,
-    const ErrorCallback& error_callback) {
+    const ConnectErrorCallback& error_callback) {
   NOTIMPLEMENTED();
 }
 
