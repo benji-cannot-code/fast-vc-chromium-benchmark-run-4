a3aa2f7394ec655de152079caf21e4f6b4205ba3
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 18:43:14 +0000

    Enable new lock animations by default
    BUG=138171, 139461, 162646
    TBR=sky
    
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=173731
    
    Review URL: https://chromiumcodereview.appspot.com/11595008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173760 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker.cc
+++ chrome/browser/chromeos/login/screen_locker.cc
@@ -216,7 +216,9 @@ void ScreenLocker::OnLoginSuccess(
   authentication_capture_->using_oauth = using_oauth;
 
   CommandLine* command_line = CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(ash::switches::kAshNewLockAnimationsEnabled)) {
+  if (command_line->HasSwitch(ash::switches::kAshDisableNewLockAnimations)) {
+    UnlockOnLoginSuccess();
+  } else {
     // Add guard for case when something get broken in call chain to unlock
     // for sure.
     MessageLoop::current()->PostDelayedTask(
@@ -225,8 +227,6 @@ void ScreenLocker::OnLoginSuccess(
             weak_factory_.GetWeakPtr()),
         base::TimeDelta::FromMilliseconds(kUnlockGuardTimeoutMs));
     delegate_->AnimateAuthenticationSuccess();
-  } else {
-    UnlockOnLoginSuccess();
   }
 }
 
