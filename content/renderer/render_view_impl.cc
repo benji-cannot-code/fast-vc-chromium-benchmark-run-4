4caa93c3442229505a38e05c7b2d5db761a78d91
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 00:51:33 +0000

    Forward Webkit Gesture events to interested RenderViewObservers.
    The ChromeRenderViewObserver overrides the RenderViewObserver::DidHandleGestureEvent virtual and
    forwards the ChromeViewHostMsg_FocusedEditableNodeTouched IPC notification to the brower if the user
    tapped on an editable field on the page. 
    
    We will be handling this notification in the browser to display the Windows 8 on screen keyboard. That
    in a subsequent CL.
    
    BUG=166516
    R=jam
    Review URL: https://codereview.chromium.org/11622009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173601 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2088,6 +2088,8 @@ void RenderViewImpl::didHandleGestureEvent(const WebGestureEvent& event,
     UpdateTextInputState(SHOW_IME_IF_NEEDED);
   }
 #endif
+  FOR_EACH_OBSERVER(RenderViewObserver, observers_,
+                    DidHandleGestureEvent(event));
 }
 
 WebKit::WebColorChooser* RenderViewImpl::createColorChooser(
