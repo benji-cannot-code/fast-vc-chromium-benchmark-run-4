8eb51cd1e350841f418e072bc760d6eac9fe2519
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 13:37:25 +0000

    Fixed file save in the Photo Editor
    BUG=166607
    
    Review URL: https://codereview.chromium.org/11612019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/photo/gallery_item.js
+++ chrome/browser/resources/file_manager/js/photo/gallery_item.js
@@ -176,7 +176,7 @@ Gallery.Item.prototype.saveToFile = function(
         getFile.bind(null, dir, true /* create new file */));
   }
 
-  function saveToDir(dir) {
+  var saveToDir = function(dir) {
     if (overwrite) {
       checkExistence(dir);
     } else {
@@ -186,7 +186,7 @@ Gallery.Item.prototype.saveToFile = function(
         checkExistence(dir);
       }.bind(this));
     }
-  }
+  }.bind(this);
 
   if (overrideDir) {
     saveToDir(overrideDir);
