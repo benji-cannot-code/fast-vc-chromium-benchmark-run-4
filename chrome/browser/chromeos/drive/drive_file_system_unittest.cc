de896b7c75ef40a7e38857b1859a340f83779400
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-18 02:32:20 +0000

    Remove upload_id and everything else unused in DriveUploader.
    * Remove |upload_id|, |next_upload_id_|, |pending_uploads_|, and |RemoveUpload()|.
    The integer ID was used to indirectly expose UploadFileInfo to DriveDownloadObserver, but we don't need it now.
    
    * Remove unimplemented declaration of |InitiateUpload()|.
    * Remove unused |FileOpenType| enum.
    * Remove |UploadFileInfo::buf_len|. It is always 512KB.
    * Remove |UploadFileInfo::entry|. It is almost unused.
    * Remove |UploadFileInfo::start_position|. It is almost unused.
    * Moved UploadFileInfo to .cc and added bunch of const's.
    * Potential Fix: Change ReadSync to ReadUntilComplete, otherwise we can satisfy the WAPI requirement.
    
    BUG=165110
    TBR=tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/11592007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_file_system_unittest.cc
+++ chrome/browser/chromeos/drive/drive_file_system_unittest.cc
@@ -125,7 +125,7 @@ class FakeDriveUploader : public google_apis::DriveUploaderInterface {
 
   // Pretends that a new file was uploaded successfully, and returns the
   // contents of "gdata/uploaded_file.json" to the caller.
-  virtual int UploadNewFile(
+  virtual void UploadNewFile(
       const GURL& upload_location,
       const FilePath& drive_file_path,
       const FilePath& local_file_path,
@@ -146,15 +146,12 @@ class FakeDriveUploader : public google_apis::DriveUploaderInterface {
                    drive_file_path,
                    local_file_path,
                    base::Passed(&resource_entry)));
-
-    const int kUploadId = 123;
-    return kUploadId;
   }
 
   // Pretends that an existing file ("drive/File 1.txt") was uploaded
   // successfully, and returns an entry for the file in
   // "gdata/root_feed.json" to the caller.
-  virtual int UploadExistingFile(
+  virtual void UploadExistingFile(
       const GURL& upload_location,
       const FilePath& drive_file_path,
       const FilePath& local_file_path,
@@ -174,7 +171,7 @@ class FakeDriveUploader : public google_apis::DriveUploaderInterface {
     scoped_ptr<base::Value> value =
         google_apis::test_util::LoadJSONFile("gdata/root_feed.json");
     if (!value.get())
-      return -1;
+      return;
 
     base::DictionaryValue* as_dict = NULL;
     base::ListValue* entry_list = NULL;
@@ -191,7 +188,7 @@ class FakeDriveUploader : public google_apis::DriveUploaderInterface {
       }
     }
     if (!resource_entry)
-      return -1;
+      return;
 
     base::MessageLoopProxy::current()->PostTask(
         FROM_HERE,
@@ -200,9 +197,6 @@ class FakeDriveUploader : public google_apis::DriveUploaderInterface {
                    drive_file_path,
                    local_file_path,
                    base::Passed(&resource_entry)));
-
-    const int kUploadId = 123;
-    return kUploadId;
   }
 };
 
