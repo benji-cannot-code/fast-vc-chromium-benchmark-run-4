62ca8ebacd2e12d799a79fd85ac69aa50cab2273
bdakin@apple.com <bdakin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-18 00:44:40 +0000

    DidHitRelevantRepaintedObjectsAreaThreshold should not use the viewRect since that varies https://bugs.webkit.org/show_bug.cgi?id=105116 -and corresponding- <rdar://problem/12889449>
    Reviewed by Geoff Garen.
    
    DidHitRelevantRepaintedObjectsAreaThreshold should not use the viewRect since that 
    varies depending on window size. This can lead to a huge amount of variability in 
    the heuristic which is not desired. Instead, we should use a hard-coded rect.
    * page/Page.cpp:
    (WebCore::relevantViewRect):
    (WebCore):
    (WebCore::Page::addRelevantRepaintedObject):
    (WebCore::Page::addRelevantUnpaintedObject):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@137959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Page.cpp
+++ third_party/WebKit/Source/WebCore/page/Page.cpp
@@ -1192,6 +1192,23 @@ void Page::resetRelevantPaintedObjectCounter()
     m_relevantUnpaintedRegion = Region();
 }
 
+static LayoutRect relevantViewRect(RenderView* view)
+{
+    // DidHitRelevantRepaintedObjectsAreaThreshold is a LayoutMilestone intended to indicate that
+    // a certain relevant amount of content has been drawn to the screen. This is the rect that
+    // has been determined to be relevant in the context of this goal. We may choose to tweak
+    // the rect over time, much like we may choose to tweak gMinimumPaintedAreaRatio and
+    // gMaximumUnpaintedAreaRatio. But this seems to work well right now.
+    LayoutRect relevantViewRect = LayoutRect(0, 0, 980, 1300);
+
+    LayoutRect viewRect = view->viewRect();
+    // If the viewRect is wider than the relevantViewRect, center the relevantViewRect.
+    if (viewRect.width() > relevantViewRect.width())
+        relevantViewRect.setX((viewRect.width() - relevantViewRect.width()) / 2);
+
+    return relevantViewRect;
+}
+
 void Page::addRelevantRepaintedObject(RenderObject* object, const LayoutRect& objectPaintRect)
 {
     if (!isCountingRelevantRepaintedObjects())
@@ -1205,8 +1222,10 @@ void Page::addRelevantRepaintedObject(RenderObject* object, const LayoutRect& ob
     if (!view)
         return;
 
+    LayoutRect relevantRect = relevantViewRect(view);
+
     // The objects are only relevant if they are being painted within the viewRect().
-    if (!objectPaintRect.intersects(pixelSnappedIntRect(view->viewRect())))
+    if (!objectPaintRect.intersects(pixelSnappedIntRect(relevantRect)))
         return;
 
     IntRect snappedPaintRect = pixelSnappedIntRect(objectPaintRect);
@@ -1221,7 +1240,7 @@ void Page::addRelevantRepaintedObject(RenderObject* object, const LayoutRect& ob
 
     m_relevantPaintedRegion.unite(snappedPaintRect);
     
-    float viewArea = view->viewRect().width() * view->viewRect().height();
+    float viewArea = relevantRect.width() * relevantRect.height();
     float ratioOfViewThatIsPainted = m_relevantPaintedRegion.totalArea() / viewArea;
     float ratioOfViewThatIsUnpainted = m_relevantUnpaintedRegion.totalArea() / viewArea;
 
@@ -1238,9 +1257,9 @@ void Page::addRelevantUnpaintedObject(RenderObject* object, const LayoutRect& ob
     if (!isCountingRelevantRepaintedObjects())
         return;
 
-    // The objects are only relevant if they are being painted within the viewRect().
+    // The objects are only relevant if they are being painted within the relevantViewRect().
     if (RenderView* view = object->view()) {
-        if (!objectPaintRect.intersects(pixelSnappedIntRect(view->viewRect())))
+        if (!objectPaintRect.intersects(pixelSnappedIntRect(relevantViewRect(view))))
             return;
     }
 
