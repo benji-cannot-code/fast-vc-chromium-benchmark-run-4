156ddf2957b58eb7666528356bf7c05d6650918a
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 14:34:34 +0000

    Files.app's context menu on the wrench button should be closed after a delay when clicking on checkable items.
    Currently, after clicking on a checkable menu item, eg. "Do not use mobile data for sync", the menu is instantly closed and user can't see if the change occured. This patch resolved this issue by adding a small timeout (120ms) before the popup is closed. As a result user can notice that the change has occured.
    This patch changes behavior of each instance of Javascript MenuButton. It does not change behavior of normal context menus invoked by right mouse click.
    
    TEST=Follow the bug's description. The delay should be noticable. However on non-checkable menu items, there should not be any delay. Also, check normal context menus invoked by right mouse click, including in Process list. Behavior should not be changed there.
    BUG=140586
    
    
    Review URL: https://chromiumcodereview.appspot.com/11576068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173917 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/shared/js/cr/ui/menu_button.js
+++ chrome/browser/resources/shared/js/cr/ui/menu_button.js
@@ -6,8 +6,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('cr.ui', function() {
   /** @const */
   var Menu = cr.ui.Menu;
+
   /** @const */
   var positionPopupAroundElement = cr.ui.positionPopupAroundElement;
+
+  /**
+   * Timeout for hiding the menu for the HideType.DELAYED hide mode in ms.
+   * @type {number}
+   */
+  var DEFAULT_HIDE_DELAY_MS = 120;
+
+  /**
+   * Enum for type of hide. Delayed is used when called by clicking on a
+   * checkable menu item.
+   * @enum {number}
+   */
+  var HideType = {
+    INSTANT: 0,
+    DELAYED: 1
+  };
 
   /**
    * Creates a new menu button element.
@@ -100,13 +117,16 @@ cr.define('cr.ui', function() {
             }
           }
           break;
-
         case 'focus':
           if (!this.contains(e.target) && !this.menu.contains(e.target))
             this.hideMenu();
           break;
-
         case 'activate':
+          if (e.target instanceof cr.ui.MenuItem && e.target.checkable)
+            this.hideMenu(HideType.DELAYED);
+          else
+            this.hideMenu();
+          break;
         case 'resize':
           this.hideMenu();
           break;
@@ -146,13 +166,27 @@ cr.define('cr.ui', function() {
     /**
      * Hides the menu. If your menu can go out of scope, make sure to call this
      * first.
+     * @param {HideType=} opt_hideType Type of hide - instant or delayed,
+     *     default: INSTANT.
      */
-    hideMenu: function() {
+    hideMenu: function(opt_hideType) {
+      opt_hideType = opt_hideType || HideType.INSTANT;
+
       if (!this.isMenuShown())
         return;
 
-      this.removeAttribute('menu-shown');
-      this.menu.hidden = true;
+      switch (opt_hideType) {
+        case HideType.INSTANT:
+          this.removeAttribute('menu-shown');
+          this.menu.hidden = true;
+          break;
+        case HideType.DELAYED:
+          setTimeout(function() {
+            this.removeAttribute('menu-shown');
+            this.menu.hidden = true;
+          }.bind(this), DEFAULT_HIDE_DELAY_MS);
+          break;
+      }
 
       this.showingEvents_.removeAll();
       this.focus();
