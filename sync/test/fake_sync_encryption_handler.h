d27a6f43bbfddadbc30c7e1c27314ca2eaa36978
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 01:56:52 +0000

    [Sync] Add support for keystore key rotation.
    Key rotation will trigger a full re-encryption of all sync data with the newest
    keystore key. Previous keys will be added to the keybag as well. We detect key
    rotation by checking whether we have multiple keystore keys and the nigori's
    keybag is not encrypted with the current keystore key. In that case, we no
    longer support backwards compatibility with non-keystore supporting versions,
    and re-encrypt using the newest keystore key.
    
    This change also fixes two latent issues: lack of encryption of the keystore
    bootstrap and not properly posting OnPassphraseAccepted when we resolve pending
    keys.
    
    BUG=163744
    
    
    Review URL: https://chromiumcodereview.appspot.com/11434070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173830 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/test/fake_sync_encryption_handler.h
+++ sync/test/fake_sync_encryption_handler.h
@@ -49,8 +49,8 @@ class FakeSyncEncryptionHandler : public SyncEncryptionHandler,
       syncable::BaseTransaction* const trans) const OVERRIDE;
   virtual bool NeedKeystoreKey(
       syncable::BaseTransaction* const trans) const OVERRIDE;
-  virtual bool SetKeystoreKey(
-      const std::string& key,
+  virtual bool SetKeystoreKeys(
+      const google::protobuf::RepeatedPtrField<google::protobuf::string>& keys,
       syncable::BaseTransaction* const trans) OVERRIDE;
   virtual ModelTypeSet GetEncryptedTypes(
       syncable::BaseTransaction* const trans) const OVERRIDE;
