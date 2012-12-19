37305293dc5f957ab5456d317bd2af8b18f55cb8
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 02:43:36 +0000

    IndexedDB: Ensure performance tests can run in IE10.
    IE10's IndexedDB fails get() requests against indexes with key paths which
    are empty strings. This requires the test to use objects as values; Chrome's
    performance numbers will likely regress when this lands since the tests
    require slightly more processing.
    
    Also account for lack of |name| property on functions in IE, force standards
    mode, and add missing semicolons.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11617009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173843 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/indexeddb/perf_shared.js
+++ chrome/test/data/indexeddb/perf_shared.js
@@ -100,7 +100,7 @@ function createDatabase(
     db.onerror = function(ev) {
       console.log("db error", arguments, openRequest.webkitErrorMessage);
       errorHandler(ev);
-    }
+    };
     if (curVersion != baseVersion) {
       // This is the legacy path, which runs only in Chrome.
       var setVersionRequest = db.setVersion(baseVersion);
@@ -111,11 +111,11 @@ function createDatabase(
         var versionTransaction = setVersionRequest.result;
         versionTransaction.oncomplete = function() { handler(db); };
         versionTransaction.onerror = onError;
-      }
+      };
     } else {
       handler(db);
     }
-  }
+  };
 }
 
 // You must close all database connections before calling this.
@@ -125,8 +125,6 @@ function alterObjectStores(
   var openRequest = indexedDB.open(name, version);
   openRequest.onblocked = errorHandler;
   openRequest.onupgradeneeded = function(ev) {
-    // This is the spec-compliant path, which doesn't yet run in Chrome, but
-    // works in Firefox.
     doAlteration(ev.target.transaction);
     // onsuccess will get called after this exits.
   };
@@ -137,9 +135,9 @@ function alterObjectStores(
       console.log("error altering db", arguments,
           openRequest.webkitErrorMessage);
       errorHandler();
-    }
+    };
     if (db.version != version) {
-      // This is the legacy path, which runs only in Chrome.
+      // This is the legacy path, which runs only in Chrome before M23.
       var setVersionRequest = db.setVersion(version);
       setVersionRequest.onerror = errorHandler;
       setVersionRequest.onsuccess =
@@ -150,11 +148,11 @@ function alterObjectStores(
             versionTransaction.oncomplete = function() { handler(db); };
             versionTransaction.onerror = onError;
             doAlteration(versionTransaction);
-          }
+          };
     } else {
       handler(db);
     }
-  }
+  };
   function doAlteration(target) {
     for (var store in objectStoreNames) {
       func(target.objectStore(objectStoreNames[store]));
@@ -193,13 +191,17 @@ function getCompletionFunc(db, testName, startTime, onTestComplete) {
     automation.setStatus("Deleting database.");
     db.close();
     deleteDatabase(testName, onDeleted);
-  }
+  };
 }
 
 function getDisplayName(args) {
+  function functionName(f) {
+    // Function.prototype.name is nonstandard, and not implemented in IE10-
+    return f.name || f.toString().match(/^function\s*([^(\s]*)/)[1];
+  }
   // The last arg is the completion callback the test runner tacks on.
   // TODO(ericu): Make test errors delete the database automatically.
-  return getDisplayName.caller.name + (args.length > 1 ? "_" : "") +
+  return functionName(getDisplayName.caller) + (args.length > 1 ? "_" : "") +
       Array.prototype.slice.call(args, 0, args.length - 1).join("_");
 }
 
@@ -228,6 +230,10 @@ function getSimpleKey(i) {
 
 function getSimpleValue(i) {
   return "value " + padToWidth(i, 10);
+}
+
+function getIndexableValue(i) {
+  return { id: getSimpleValue(i) };
 }
 
 function getForwardIndexKey(i) {
@@ -377,7 +383,7 @@ function getValuesFromCursor(
     } else {
       assert(!numReadsLeft);
     }
-  }
+  };
   request.onerror = onError;
 }
 
