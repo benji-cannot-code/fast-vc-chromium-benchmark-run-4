d3da5e06936f3abbe5c1baf6ec6e1fa5f7c43243
digit@chromium.org <digit@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 11:07:55 +0000

    Android buildbot: Add missing @@@ echo to 'bb_run_unit_tests'
    Without this, a failure in this function is mistakenly reported
    as a failure in the previous bb_xxxxx function called. For example,
    see the following link where the failure is reported in
    'bb_reboot_phones' when it should not:
    
    http://build.chromium.org/p/chromium.linux/builders/Android%20Tests/builds/23/steps/Rebooting%20phones/logs/stdio
    
    BUG=none
    NOTRY=true
    TBR=yfriedman@chromium.org,ilevy@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11648009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173895 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/buildbot_functions.sh
+++ build/android/buildbot/buildbot_functions.sh
@@ -221,6 +221,7 @@ function bb_print_logcat {
 
 # Run tests on an actual device.  (Better have one plugged in!)
 function bb_run_unit_tests {
+  echo "@@@BUILD_STEP Run unit tests on device@@@"
   build/android/run_tests.py --xvfb --verbose
 }
 
