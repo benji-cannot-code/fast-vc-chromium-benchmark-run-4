4182b2bef7b86920ef065c02d99ae93f636530a0
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 09:15:46 +0000

    Use scoped_ptr<> to pass device list info in AudioInputDeviceManager and VideoCaptureManager.
    BUG=156829
    
    
    Review URL: https://chromiumcodereview.appspot.com/11583024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173878 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/video_capture_manager.h
+++ content/browser/renderer_host/media/video_capture_manager.h
@@ -107,7 +107,7 @@ class CONTENT_EXPORT VideoCaptureManager : public MediaStreamProvider {
   void OnOpened(MediaStreamType type, int capture_session_id);
   void OnClosed(MediaStreamType type, int capture_session_id);
   void OnDevicesEnumerated(MediaStreamType stream_type,
-                           const StreamDeviceInfoArray& devices);
+                           scoped_ptr<StreamDeviceInfoArray> devices);
   void OnError(MediaStreamType type, int capture_session_id,
                MediaStreamProviderError error);
 
@@ -116,7 +116,7 @@ class CONTENT_EXPORT VideoCaptureManager : public MediaStreamProvider {
   void PostOnOpened(MediaStreamType type, int capture_session_id);
   void PostOnClosed(MediaStreamType type, int capture_session_id);
   void PostOnDevicesEnumerated(MediaStreamType stream_type,
-                               const StreamDeviceInfoArray& devices);
+                               scoped_ptr<StreamDeviceInfoArray> devices);
   void PostOnError(int capture_session_id, MediaStreamProviderError error);
 
   // Helpers
