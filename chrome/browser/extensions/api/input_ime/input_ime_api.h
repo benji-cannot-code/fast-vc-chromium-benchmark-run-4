1538189baa0ddb0ef62b871debe3ffac1061f436
MHX348@motorola.com <MHX348@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 18:16:20 +0000

    Pull InputIme stuff out of ExtensionService
    Make InputImeAPI a PKS.
    
    Patch from Joe Thomas <mhx348@motorola.com>.
    
    BUG=159265
    TBR=ben@chromium.org for gyp
    
    
    Review URL: https://chromiumcodereview.appspot.com/11618005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173950 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/input_ime/input_ime_api.h
+++ chrome/browser/extensions/api/input_ime/input_ime_api.h
@@ -6,16 +6,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_INPUT_IME_INPUT_IME_API_H_
 #define CHROME_BROWSER_EXTENSIONS_API_INPUT_IME_INPUT_IME_API_H_
 
-#include "chrome/browser/extensions/extension_function.h"
+#include <map>
+#include <string>
+#include <vector>
 
 #include "base/memory/singleton.h"
 #include "base/values.h"
 #include "chrome/browser/chromeos/input_method/input_method_engine.h"
+#include "chrome/browser/extensions/extension_function.h"
+#include "chrome/browser/profiles/profile_keyed_service.h"
 #include "chrome/common/extensions/extension.h"
-
-#include <map>
-#include <string>
-#include <vector>
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 
 class Profile;
 
@@ -29,7 +31,6 @@ namespace extensions {
 class InputImeEventRouter {
  public:
   static InputImeEventRouter* GetInstance();
-  void Init();
 
   bool RegisterIme(Profile* profile,
                    const std::string& extension_id,
@@ -169,6 +170,24 @@ class KeyEventHandled : public AsyncExtensionFunction {
 
   // ExtensionFunction:
   virtual bool RunImpl() OVERRIDE;
+};
+
+class InputImeAPI : public ProfileKeyedService,
+                    public content::NotificationObserver {
+ public:
+  explicit InputImeAPI(Profile* profile);
+  virtual ~InputImeAPI();
+
+  // content::NotificationObserver implementation.
+  virtual void Observe(int type,
+                       const content::NotificationSource& source,
+                       const content::NotificationDetails& details) OVERRIDE;
+
+ private:
+  InputImeEventRouter* input_ime_event_router();
+
+  Profile* const profile_;
+  content::NotificationRegistrar registrar_;
 };
 
 }  // namespace extensions
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2458,6 +2458,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'sources!': [
             'browser/extensions/api/input_ime/input_ime_api.cc',
             'browser/extensions/api/input_ime/input_ime_api.h',
+            'browser/extensions/api/input_ime/input_ime_api_factory.cc',
+            'browser/extensions/api/input_ime/input_ime_api_factory.h',
             'browser/extensions/api/terminal/terminal_extension_helper.cc',
             'browser/extensions/api/terminal/terminal_extension_helper.h',
             'browser/extensions/api/terminal/terminal_private_api.cc',
