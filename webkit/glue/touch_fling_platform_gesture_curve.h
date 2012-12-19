ce583c716e3f216b3d5ba302ec043980b377b7ee
wjmaclean@chromium.org <wjmaclean@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 13:44:39 +0000

    Use analytic duration in fling curves.
    Since we seem to have standardized on the exponential curve for fling
    velocities and positions, we no longer need to treat finding the
    time for a given velocity as a search problem, it can be computer analytically.
    
    TESTS=cc_unittests
    BUG=None
    
    Review URL: https://codereview.chromium.org/11614033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173912 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/touch_fling_platform_gesture_curve.h
+++ webkit/glue/touch_fling_platform_gesture_curve.h
@@ -32,7 +32,6 @@ class TouchFlingGestureCurve : public WebKit::WebGestureCurve {
   WEBKIT_GLUE_EXPORT static WebGestureCurve* Create(
       const WebKit::WebFloatPoint& initial_velocity,
       float p0, float p1, float p2,
-      float curveDuration,
       const WebKit::WebSize& cumulativeScroll);
 
  virtual bool apply(double monotonicTime,
@@ -43,7 +42,6 @@ class TouchFlingGestureCurve : public WebKit::WebGestureCurve {
                          float p0,
                          float p1,
                          float p2,
-                         float curve_duration,
                          const WebKit::WebSize& cumulativeScroll);
   virtual ~TouchFlingGestureCurve();
 
