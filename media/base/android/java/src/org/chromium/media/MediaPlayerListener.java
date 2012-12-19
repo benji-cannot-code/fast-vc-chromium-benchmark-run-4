81a7132336f9906731bc99452ac990ad490c0f88
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 21:54:31 +0000

    check if app has READ_PHONE_STATE permission before listening to it
    BUG=166868
    
    
    Review URL: https://chromiumcodereview.appspot.com/11633022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174002 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/java/src/org/chromium/media/MediaPlayerListener.java
+++ media/base/android/java/src/org/chromium/media/MediaPlayerListener.java
@@ -123,11 +123,14 @@ class MediaPlayerListener extends PhoneStateListener implements MediaPlayer.OnPr
     @CalledByNative
     public void releaseResources() {
         if (mContext != null) {
-            // Unregister the listener.
-            TelephonyManager mgr =
-                    (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
-            if (mgr != null) {
-                mgr.listen(this, PhoneStateListener.LISTEN_NONE);
+            if (PackageManager.PERMISSION_GRANTED ==
+                mContext.checkCallingOrSelfPermission(permission.READ_PHONE_STATE)) {
+                // Unregister the listener.
+                TelephonyManager mgr =
+                        (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+                if (mgr != null) {
+                    mgr.listen(this, PhoneStateListener.LISTEN_NONE);
+                }
             }
 
             // Unregister the wish for audio focus.
@@ -154,10 +157,13 @@ class MediaPlayerListener extends PhoneStateListener implements MediaPlayer.OnPr
             mediaPlayer.setWakeMode(context, android.os.PowerManager.FULL_WAKE_LOCK);
         }
 
-        TelephonyManager mgr =
-                (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
-        if (mgr != null) {
-            mgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+        if (PackageManager.PERMISSION_GRANTED ==
+            context.checkCallingOrSelfPermission(permission.READ_PHONE_STATE)) {
+            TelephonyManager mgr =
+                    (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+            if (mgr != null) {
+                mgr.listen(listener, PhoneStateListener.LISTEN_CALL_STATE);
+            }
         }
 
         AudioManager am = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
