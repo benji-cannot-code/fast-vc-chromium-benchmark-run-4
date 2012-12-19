5e04ffe6693ec6f8c22626b2b35407f39eaafa48
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 22:42:44 +0000

    Re-enable Windows profile desktop shortcuts feature.
    BUG=165933
    TEST=Profile checkboxes appear in web UI dialogs and work as expected.
    
    Review URL: https://chromiumcodereview.appspot.com/11577006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174020 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_shortcut_manager_win.cc
+++ chrome/browser/profiles/profile_shortcut_manager_win.cc
@@ -306,7 +306,7 @@ string16 CreateProfileShortcutFlags(const FilePath& profile_path) {
 
 // static
 bool ProfileShortcutManager::IsFeatureEnabled() {
-  return false;
+  return true;
 }
 
 // static
