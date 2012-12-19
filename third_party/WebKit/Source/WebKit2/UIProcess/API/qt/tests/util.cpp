0e6991e14610aafdf58aa7728c1a2d4c3bc93ba6
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-19 12:49:16 +0000

    [Qt] Missing vtable for LoadStartedCatcher. https://bugs.webkit.org/show_bug.cgi?id=105313
    Properly splitting class declarations and function
    definitions into headers/cpp-files.
    This makes including util.moc redundant
    and avoids the well known vtable issues with QObject classes.
    
    Reviewed by Simon Hausmann.
    
    * UIProcess/API/qt/tests/util.cpp:
    (LoadSpy::LoadSpy):
    (LoadSpy::onLoadingChanged):
    * UIProcess/API/qt/tests/util.h:
    (LoadSpy):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/qt/tests/util.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/qt/tests/util.cpp
@@ -75,26 +75,6 @@ void suppressDebugOutput()
 }
 
 #if defined(HAVE_QTQUICK) && HAVE_QTQUICK
-class LoadSpy : public QEventLoop {
-    Q_OBJECT
-public:
-    LoadSpy(QQuickWebView* webView)
-    {
-        connect(webView, SIGNAL(loadingChanged(QWebLoadRequest*)), SLOT(onLoadingChanged(QWebLoadRequest*)));
-    }
-Q_SIGNALS:
-    void loadSucceeded();
-    void loadFailed();
-private Q_SLOTS:
-    void onLoadingChanged(QWebLoadRequest* loadRequest)
-    {
-        if (loadRequest->status() == QQuickWebView::LoadSucceededStatus)
-            emit loadSucceeded();
-        else if (loadRequest->status() == QQuickWebView::LoadFailedStatus)
-            emit loadFailed();
-    }
-};
-
 bool waitForLoadSucceeded(QQuickWebView* webView, int timeout)
 {
     QEventLoop loop;
@@ -134,6 +114,19 @@ bool waitForViewportReady(QQuickWebView* webView, int timeout)
     return waitForSignal(webView->experimental(), SIGNAL(loadVisuallyCommitted()), timeout);
 }
 
+LoadSpy::LoadSpy(QQuickWebView* webView)
+{
+    connect(webView, SIGNAL(loadingChanged(QWebLoadRequest*)), SLOT(onLoadingChanged(QWebLoadRequest*)));
+}
+
+void LoadSpy::onLoadingChanged(QWebLoadRequest* loadRequest)
+{
+    if (loadRequest->status() == QQuickWebView::LoadSucceededStatus)
+        emit loadSucceeded();
+    else if (loadRequest->status() == QQuickWebView::LoadFailedStatus)
+        emit loadFailed();
+}
+
 LoadStartedCatcher::LoadStartedCatcher(QQuickWebView* webView)
     : m_webView(webView)
 {
@@ -149,5 +142,3 @@ void LoadStartedCatcher::onLoadingChanged(QWebLoadRequest* loadRequest)
     }
 }
 #endif
-
-#include "util.moc"
