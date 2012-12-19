cabeb0b5258514cf7cf795761597b738b104b659
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 23:00:27 +0000

    Move messaging APIs from chrome.extension to chrome.runtime
    Also leave around aliases for backwards compatability.
    
    BUG=157952
    
    
    Review URL: https://chromiumcodereview.appspot.com/11645005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174027 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/messaging/app2/background.js
@@ -0,0 +1,21 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.runtime.onConnectExternal.addListener(function(port) {
+  port.onMessage.addListener(function(msg) {
+    if (msg == 'ok_to_disconnect') {
+      port.disconnect();
+    } else {
+      port.postMessage(msg + '_reply');
+    }
+  });
+});
+
+chrome.runtime.onMessageExternal.addListener(function(msg, sender, callback) {
+  if (msg == 'hello')
+    callback('hello_response');
+  else
+    callback();
+});
