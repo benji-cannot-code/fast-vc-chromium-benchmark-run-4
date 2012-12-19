4910aeec05adee1cfb35a9700adc1d95dd2eec4b
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 20:10:51 +0000

    Linux: use generated shim headers for opus.
    BUG=165264
    
    Review URL: https://codereview.chromium.org/11635009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173978 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/codec/audio_encoder_opus.cc
+++ remoting/codec/audio_encoder_opus.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "media/base/audio_bus.h"
 #include "media/base/multi_channel_resampler.h"
-#include "third_party/opus/opus.h"
+#include "third_party/opus/src/include/opus.h"
 
 namespace remoting {
 
