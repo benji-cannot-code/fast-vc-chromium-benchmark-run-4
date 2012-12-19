adcfe68319faa2fe8150064382c6cd68c13a9258
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-19 13:56:38 +0000

    Web Inspector: scripts to evaluate on load are not removed from inspector state when Page agent is disabled https://bugs.webkit.org/show_bug.cgi?id=105422
    Reviewed by Alexander Pavlov.
    
    - reset scripts to evaluate on load when InspectorPageAgent is disabled.
    - added InspectorState::remove();
    
    * inspector/InspectorPageAgent.cpp:
    (WebCore::InspectorPageAgent::disable):
    * inspector/InspectorState.cpp:
    (WebCore::InspectorState::remove):
    (WebCore):
    * inspector/InspectorState.h:
    (InspectorState):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138150 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorState.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorState.cpp
@@ -76,6 +76,11 @@ void InspectorState::setValue(const String& propertyName, PassRefPtr<InspectorVa
     updateCookie();
 }
 
+void InspectorState::remove(const String& propertyName)
+{
+    m_properties->remove(propertyName);
+}
+
 bool InspectorState::getBoolean(const String& propertyName)
 {
     InspectorObject::iterator it = m_properties->find(propertyName);
