f5451dbb12ad064404f890ed6a8b6683741292d2
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 02:23:51 +0000

    Created a browser test catching JS errors in Chrome OS file open/save dialogs.
    Existing tests check that a dialog opens and returns a correct selected path. This patch adds a check that no uncaught JS exceptions happened during the dialog initialization.
    
    BUG=165408
    
    
    Review URL: https://chromiumcodereview.appspot.com/11566035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173835 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -3,6 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+// This variable is checked in SelectFileDialogExtensionBrowserTest.
+var JSErrorCount = 0;
+
+/**
+ * Count uncaught exceptions.
+ */
+window.onerror = function() { JSErrorCount++ };
+
 /**
  * FileManager constructor.
  *
