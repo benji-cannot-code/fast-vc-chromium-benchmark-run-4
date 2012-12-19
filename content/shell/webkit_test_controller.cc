b0878806c5323342f8fb17899fca4b19a608ba29
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 09:25:47 +0000

    [content shell] when overriding the WebPreferences for layout tests, only override the ones specific to layout tests
    BUG=111316
    TEST=appcache tests don't time out
    
    Review URL: https://chromiumcodereview.appspot.com/11643004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173881 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_controller.cc
+++ content/shell/webkit_test_controller.cc
@@ -193,7 +193,7 @@ bool WebKitTestController::ResetAfterLayoutTest() {
   wait_until_done_ = false;
   did_finish_load_ = false;
   prefs_ = webkit_glue::WebPreferences();
-  ExportPreferences(WebPreferences(), &prefs_);
+  ExportLayoutTestSpecificPreferences(WebPreferences(), &prefs_);
   {
     base::AutoLock lock(lock_);
     can_open_windows_ = false;
@@ -212,6 +212,11 @@ void WebKitTestController::RendererUnresponsive() {
   DCHECK(CalledOnValidThread());
   if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kNoTimeout))
     printer_->AddErrorMessage("#PROCESS UNRESPONSIVE - renderer");
+}
+
+void WebKitTestController::OverrideWebkitPrefs(
+    webkit_glue::WebPreferences* prefs) {
+  CopyLayoutTestSpecificPreferences(prefs_, prefs);
 }
 
 bool WebKitTestController::CanOpenWindows() const {
