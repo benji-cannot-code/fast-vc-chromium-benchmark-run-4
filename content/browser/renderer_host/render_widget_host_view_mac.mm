215600bbb880840c72bcd60a8e74e7d2446a1c67
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 23:19:09 +0000

    Remove forward declaration; this is in the 10.6 SDK.
    BUG=166680
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11636019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174034 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -97,9 +97,6 @@ typedef NSUInteger NSEventPhase;
 
 @interface NSEvent (LionAPI)
 - (NSEventPhase)phase;
-+ (id)addLocalMonitorForEventsMatchingMask:(NSEventMask)mask
-                                   handler:(NSEvent* (^)(NSEvent*))block;
-+ (void)removeMonitor:(id)eventMonitor;
 @end
 
 @interface NSWindow (LionAPI)
