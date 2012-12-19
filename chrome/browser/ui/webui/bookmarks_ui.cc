0ec68a4bdc655be58fb36b9c4551d06f45e9994d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 18:53:55 +0000

    webui: Remove unused includes from bookmarks_ui.cc
    TBR=estade@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11635018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173964 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/bookmarks_ui.cc
+++ chrome/browser/ui/webui/bookmarks_ui.cc
@@ -6,25 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/bookmarks_ui.h"
 
 #include "base/memory/ref_counted_memory.h"
-#include "base/memory/singleton.h"
 #include "base/message_loop.h"
-#include "base/string_number_conversions.h"
-#include "base/stringprintf.h"
-#include "chrome/browser/bookmarks/bookmark_editor.h"
-#include "chrome/browser/bookmarks/bookmark_model.h"
-#include "chrome/browser/bookmarks/bookmark_utils.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_list.h"
 #include "chrome/browser/ui/webui/chrome_url_data_manager.h"
 #include "chrome/common/url_constants.h"
-#include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
-#include "googleurl/src/gurl.h"
 #include "grit/theme_resources.h"
 #include "ui/base/resource/resource_bundle.h"
-
-using content::WebContents;
 
 ////////////////////////////////////////////////////////////////////////////////
 //
@@ -70,6 +58,6 @@ BookmarksUI::BookmarksUI(content::WebUI* web_ui) : WebUIController(web_ui) {
 // static
 base::RefCountedMemory* BookmarksUI::GetFaviconResourceBytes(
       ui::ScaleFactor scale_factor) {
-  return ResourceBundle::GetSharedInstance().
+  return ui::ResourceBundle::GetSharedInstance().
       LoadDataResourceBytesForScale(IDR_BOOKMARKS_FAVICON, scale_factor);
 }
