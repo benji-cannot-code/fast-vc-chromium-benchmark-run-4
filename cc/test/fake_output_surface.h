2bd2683341e7aac6bf261fe8b39b4d737d5e6330
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-19 05:48:20 +0000

    cc: Chromium-stylify the FakeWebGraphicsContext3D class.
    R=jamesr,enne
    
    Review URL: https://codereview.chromium.org/11635003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@173866 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_output_surface.h
+++ cc/test/fake_output_surface.h
@@ -65,7 +65,7 @@ private:
 static inline scoped_ptr<cc::OutputSurface> createFakeOutputSurface()
 {
     return FakeOutputSurface::Create3d(
-        FakeWebGraphicsContext3D::create(
+        FakeWebGraphicsContext3D::Create(
             WebKit::WebGraphicsContext3D::Attributes())
         .PassAs<WebKit::WebGraphicsContext3D>())
         .PassAs<cc::OutputSurface>();
