1ea434ea7c12ce27f2b9dd7bd0590d5d26860923
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 07:46:50 +0000

    Uncomment frame message flag and disable content detection tests.
    The frame message is known to cause tests derived from ContentDetectionTestBase
    to become flaky.  While we investigate, re-enable the flag and disable
    the affected tests.
    
    BUG=166920,161945
    
    
    Review URL: https://chromiumcodereview.appspot.com/11640036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174113 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ClickListenerTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ClickListenerTest.java
@@ -15,8 +15,12 @@ import org.chromium.base.test.util.Feature;
  */
 public class ClickListenerTest extends ContentDetectionTestBase {
 
+    /*
     @MediumTest
     @Feature({"ContentDetection", "TabContents"})
+    Bug 166920
+    */
+    @DisabledTest
     public void testClickContentOnLink() throws Throwable {
         startActivityWithTestUrl("content/content_detection/click_listeners.html");
 
@@ -25,8 +29,12 @@ public class ClickListenerTest extends ContentDetectionTestBase {
         assertTrue(isCurrentTestUrl("content/content_detection/empty.html"));
     }
 
+    /*
     @MediumTest
     @Feature({"ContentDetection", "TabContents"})
+    Bug 166920
+    */
+    @DisabledTest
     public void testClickContentOnJSListener1() throws Throwable {
         startActivityWithTestUrl("content/content_detection/click_listeners.html");
 
@@ -36,8 +44,12 @@ public class ClickListenerTest extends ContentDetectionTestBase {
         assertTrue(isCurrentTestUrl("content/content_detection/empty.html"));
     }
 
+    /*
     @MediumTest
     @Feature({"ContentDetection", "TabContents"})
+    Bug 166920
+    */
+    @DisabledTest
     public void testClickContentOnJSListener2() throws Throwable {
         startActivityWithTestUrl("content/content_detection/click_listeners.html");
 
