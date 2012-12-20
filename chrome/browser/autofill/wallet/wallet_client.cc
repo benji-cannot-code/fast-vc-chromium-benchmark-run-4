f4b377d830299fa58279d3ca4ce1bbb2c8cf3013
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 12:03:14 +0000

    Fix leaks in WalletClientTest.
    BUG=167018
    
    Review URL: https://chromiumcodereview.appspot.com/11642036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174137 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/wallet/wallet_client.cc
+++ chrome/browser/autofill/wallet/wallet_client.cc
@@ -246,7 +246,7 @@ void WalletClient::OnURLFetchComplete(
         scoped_ptr<FullWallet> full_wallet(
             FullWallet::CreateFullWallet(*response_dict));
         if (full_wallet.get())
-          observer_->OnGetFullWallet(full_wallet.release());
+          observer_->OnGetFullWallet(full_wallet.get());
         else
           observer_->OnNetworkError(response_code);
       } else {
@@ -259,7 +259,7 @@ void WalletClient::OnURLFetchComplete(
         scoped_ptr<WalletItems> wallet_items(
             WalletItems::CreateWalletItems(*response_dict));
         if (wallet_items.get())
-          observer_->OnGetWalletItems(wallet_items.release());
+          observer_->OnGetWalletItems(wallet_items.get());
         else
           observer_->OnNetworkError(response_code);
       } else {
