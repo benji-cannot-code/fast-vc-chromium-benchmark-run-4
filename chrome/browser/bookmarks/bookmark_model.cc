f0454788a464c6e9e918046929837c35040b5afe
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 17:04:24 +0000

    bookmarks: Get rid of |was_bookmarked| parameter from BookmarkModel::AddNode() function.
    This parameter seems to be unused, so we can kill it now.
    
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11607010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model.cc
+++ chrome/browser/bookmarks/bookmark_model.cc
@@ -551,7 +551,7 @@ const BookmarkNode* BookmarkModel::AddFolder(const BookmarkNode* parent,
   new_node->SetTitle(title);
   new_node->set_type(BookmarkNode::FOLDER);
 
-  return AddNode(AsMutable(parent), index, new_node, false);
+  return AddNode(AsMutable(parent), index, new_node);
 }
 
 const BookmarkNode* BookmarkModel::AddURL(const BookmarkNode* parent,
@@ -575,8 +575,6 @@ const BookmarkNode* BookmarkModel::AddURLWithCreationTime(
     return NULL;
   }
 
-  bool was_bookmarked = IsBookmarked(url);
-
   // Syncing may result in dates newer than the last modified date.
   if (creation_time > parent->date_folder_modified())
     SetDateFolderModified(parent, creation_time);
@@ -592,7 +590,7 @@ const BookmarkNode* BookmarkModel::AddURLWithCreationTime(
     nodes_ordered_by_url_set_.insert(new_node);
   }
 
-  return AddNode(AsMutable(parent), index, new_node, was_bookmarked);
+  return AddNode(AsMutable(parent), index, new_node);
 }
 
 void BookmarkModel::SortChildren(const BookmarkNode* parent) {
@@ -804,8 +802,7 @@ void BookmarkModel::RemoveAndDeleteNode(BookmarkNode* delete_me) {
 
 BookmarkNode* BookmarkModel::AddNode(BookmarkNode* parent,
                                      int index,
-                                     BookmarkNode* node,
-                                     bool was_bookmarked) {
+                                     BookmarkNode* node) {
   parent->Add(node, index);
 
   if (store_.get())
