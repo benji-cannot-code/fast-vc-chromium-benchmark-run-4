70086fd2ed8a1b26799af2d27f73fb25e5e5506e
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 22:20:04 +0000

    win: AcceleratedPresenter replies to the GPU process as soon as shared texture is copied.
    This means the GPU does not deschedule for as long waiting until it can render to the back buffer safely.
    
    Review URL: https://chromiumcodereview.appspot.com/11645027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174257 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/surface/accelerated_surface_win.h
+++ ui/surface/accelerated_surface_win.h
@@ -27,9 +27,10 @@ class Rect;
 class SURFACE_EXPORT AcceleratedPresenter
     : public base::RefCountedThreadSafe<AcceleratedPresenter> {
  public:
-  typedef base::Callback<void(bool,
-                              base::TimeTicks,
-                              base::TimeDelta)> CompletionTask;
+  typedef base::Callback<void(bool)> CopyCompletionTask;
+
+  typedef base::Callback<void(base::TimeTicks,
+                              base::TimeDelta)> PresentCompletionTask;
 
   explicit AcceleratedPresenter(gfx::PluginWindowHandle window);
 
@@ -46,7 +47,8 @@ class SURFACE_EXPORT AcceleratedPresenter
   void AsyncPresentAndAcknowledge(
       const gfx::Size& size,
       int64 surface_handle,
-      const CompletionTask& completion_task);
+      const CopyCompletionTask& copy_completion_task,
+      const PresentCompletionTask& present_completion_task);
 
   // Schedule the presenter to free all its resources. This can be called on any
   // thread.
@@ -82,7 +84,8 @@ class SURFACE_EXPORT AcceleratedPresenter
   void DoPresentAndAcknowledge(
       const gfx::Size& size,
       int64 surface_handle,
-      const CompletionTask& completion_task);
+      const CopyCompletionTask& copy_completion_task,
+      const PresentCompletionTask& present_completion_task);
   void DoSuspend();
   void DoPresent(const base::Closure& composite_task);
   void DoReleaseSurface();
