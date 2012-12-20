6f27229ab6fa8cf9a1e776b9f6d31be0fefda387
ahutter@chromium.org <ahutter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 02:46:28 +0000

    Integrating Online Wallet into Chrome.  This CL is modeled after the Gaia OAuth client.
    BUG=163609
    
    Review URL: https://chromiumcodereview.appspot.com/11293078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174078 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/autofill/wallet/wallet_service_url.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_AUTOFILL_WALLET_WALLET_SERVICE_URL_H_
+#define CHROME_BROWSER_AUTOFILL_WALLET_WALLET_SERVICE_URL_H_
+
+class GURL;
+
+namespace wallet {
+
+extern const char kApiKey[];
+GURL GetGetWalletItemsUrl();
+GURL GetGetFullWalletUrl();
+GURL GetAcceptLegalDocumentsUrl();
+GURL GetSecureUrl();
+GURL GetSendStatusUrl();
+GURL GetSaveToWalletUrl();
+
+}  // namespace wallet
+
+#endif  // CHROME_BROWSER_AUTOFILL_WALLET_WALLET_SERVICE_URL_H_
+
