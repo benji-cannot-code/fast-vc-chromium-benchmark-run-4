66d534cd4648f7c35b9920e1d8a5863bd6757389
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-20 21:04:36 +0000

    REGRESSION (r138191): Tests fail in ResourceRequest::setStorageSession         https://bugs.webkit.org/show_bug.cgi?id=105563
            Reviewed by Sam Weinig.
    
            * platform/network/cf/ResourceHandleCFNet.cpp: (WebCore::ResourceHandle::willSendRequest):
            * platform/network/mac/ResourceHandleMac.mm: (WebCore::ResourceHandle::willSendRequest):
            Don't try to appky a session if client canceled the request by setting it ot a null one.
    
            * platform/network/cf/ResourceRequestCFNet.cpp: (WebCore::ResourceRequest::setStorageSession):
            * platform/network/mac/ResourceRequestMac.mm: (WebCore::ResourceRequest::setStorageSession):
            Update platform request, so that the session does not get lost once it's updated later.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138288 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/cf/ResourceRequestCFNet.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/cf/ResourceRequestCFNet.cpp
@@ -224,6 +224,8 @@ void ResourceRequest::doUpdateResourceRequest()
 
 void ResourceRequest::setStorageSession(CFURLStorageSessionRef storageSession)
 {
+    updatePlatformRequest();
+
     CFMutableURLRequestRef cfRequest = CFURLRequestCreateMutableCopy(0, m_cfRequest.get());
     wkSetRequestStorageSession(storageSession, cfRequest);
     m_cfRequest.adoptCF(cfRequest);
