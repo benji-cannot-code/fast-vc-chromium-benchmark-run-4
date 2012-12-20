4eb85eec5b8ea9c0f43c3172ffa5c8b73202bee3
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-20 21:20:23 +0000

    IndexedDB: Cursor and IndexWriter cleanup for refactor https://bugs.webkit.org/show_bug.cgi?id=105449
    Reviewed by Tony Chang.
    
    This is a cleanup of some code to make the refactor that is coming
    in https://bugs.webkit.org/show_bug.cgi?id=102741 cleaner. Some
    CursorType assertions have been removed because the code will be
    used in more generic circumstances in a future patch.
    
    The previously-private IndexWriter has been pulled into
    IDBObjectStoreBackendImpl and made to just use IDB*Metadata
    so it can be reused later.
    
    No new tests as this does not change the logic of executing code.
    
    * Modules/indexeddb/IDBCursorBackendImpl.cpp:
    (WebCore::IDBCursorBackendImpl::IDBCursorBackendImpl):
    (WebCore::IDBCursorBackendImpl::CursorPrefetchIterationOperation::perform):
    * Modules/indexeddb/IDBCursorBackendImpl.h:
    (WebCore::IDBCursorBackendImpl::create):
    (WebCore::IDBCursorBackendImpl::value):
    (IDBCursorBackendImpl):
    * Modules/indexeddb/IDBIndexBackendImpl.cpp:
    (WebCore::IDBIndexBackendImpl::OpenIndexCursorOperation::create):
    (WebCore::IDBIndexBackendImpl::OpenIndexCursorOperation::OpenIndexCursorOperation):
    (IDBIndexBackendImpl::OpenIndexCursorOperation):
    (WebCore::IDBIndexBackendImpl::OpenIndexCursorOperation::perform):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.cpp:
    (WebCore::IDBObjectStoreBackendImpl::IndexWriter::verifyIndexKeys):
    (WebCore::IDBObjectStoreBackendImpl::IndexWriter::writeIndexKeys):
    (WebCore::IDBObjectStoreBackendImpl::IndexWriter::addingKeyAllowed):
    (WebCore::IDBObjectStoreBackendImpl::makeIndexWriters):
    * Modules/indexeddb/IDBObjectStoreBackendImpl.h:
    (IndexWriter):
    (WebCore::IDBObjectStoreBackendImpl::IndexWriter::IndexWriter):
    (IDBObjectStoreBackendImpl):
    * Modules/indexeddb/IDBRequest.cpp:
    (WebCore::IDBRequest::IDBRequest):
    (WebCore::IDBRequest::setCursorDetails):
    (WebCore::IDBRequest::setResultCursor):
    (WebCore::IDBRequest::onSuccess):
    * Modules/indexeddb/IDBRequest.h:
    (IDBRequest):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBIndex.cpp
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBIndex.cpp
@@ -76,7 +76,7 @@ PassRefPtr<IDBRequest> IDBIndex::openCursor(ScriptExecutionContext* context, Pas
         return 0;
 
     RefPtr<IDBRequest> request = IDBRequest::create(context, IDBAny::create(this), m_transaction.get());
-    request->setCursorDetails(IDBCursorBackendInterface::IndexCursor, direction);
+    request->setCursorDetails(IDBCursorBackendInterface::KeyAndValue, direction);
     m_backend->openCursor(keyRange, direction, request, m_transaction->backend(), ec);
     ASSERT(!ec);
     return request;
@@ -133,7 +133,7 @@ PassRefPtr<IDBRequest> IDBIndex::openKeyCursor(ScriptExecutionContext* context, 
         return 0;
 
     RefPtr<IDBRequest> request = IDBRequest::create(context, IDBAny::create(this), m_transaction.get());
-    request->setCursorDetails(IDBCursorBackendInterface::IndexKeyCursor, direction);
+    request->setCursorDetails(IDBCursorBackendInterface::KeyOnly, direction);
     m_backend->openKeyCursor(keyRange, direction, request, m_transaction->backend(), ec);
     ASSERT(!ec);
     return request;
