388a2dfea0a0c471263cdb3130d56886c25840d5
ivankr@chromium.org <ivankr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 12:03:25 +0000

    [cros] Allow flipping user photo after it was taken.
    BUG=152481
    
    
    Review URL: https://chromiumcodereview.appspot.com/11640019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174138 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
@@ -281,13 +281,16 @@ void ChangePictureOptionsHandler::SendOldImage(const std::string& image_url) {
 
 void ChangePictureOptionsHandler::HandleSelectImage(const ListValue* args) {
   std::string image_url;
+  std::string image_type;
   if (!args ||
-      args->GetSize() != 1 ||
-      !args->GetString(0, &image_url)) {
+      args->GetSize() != 2 ||
+      !args->GetString(0, &image_url) ||
+      !args->GetString(1, &image_type)) {
     NOTREACHED();
     return;
   }
   DCHECK(!image_url.empty());
+  DCHECK(!image_type.empty());
 
   const User* user = UserManager::Get()->GetLoggedInUser();
   UserImageManager* user_image_manager =
@@ -295,10 +298,8 @@ void ChangePictureOptionsHandler::HandleSelectImage(const ListValue* args) {
   int image_index = User::kInvalidImageIndex;
   bool waiting_for_camera_photo = false;
 
-  if (image_url == previous_image_url_) {
+  if (image_type == "old") {
     // Previous image re-selected.
-    // This must come before the IsDefaultImageUrl check in case user has an
-    // old (no longer shown in stock) default image.
     if (previous_image_index_ == User::kExternalImageIndex) {
       DCHECK(!previous_image_.isNull());
       user_image_manager->SaveUserImage(
@@ -314,7 +315,8 @@ void ChangePictureOptionsHandler::HandleSelectImage(const ListValue* args) {
                               kHistogramImageOld,
                               kHistogramImagesCount);
     VLOG(1) << "Selected old user image";
-  } else if (IsDefaultImageUrl(image_url, &image_index)) {
+  } else if (image_type == "default" &&
+             IsDefaultImageUrl(image_url, &image_index)) {
     // One of the default user images.
     user_image_manager->SaveUserDefaultImageIndex(user->email(), image_index);
 
@@ -322,7 +324,7 @@ void ChangePictureOptionsHandler::HandleSelectImage(const ListValue* args) {
                               GetDefaultImageHistogramValue(image_index),
                               kHistogramImagesCount);
     VLOG(1) << "Selected default user image: " << image_index;
-  } else if (image_url == user_photo_data_url_) {
+  } else if (image_type == "camera") {
     // Camera image is selected.
     if (user_photo_.isNull()) {
       DCHECK(image_decoder_.get());
@@ -331,7 +333,7 @@ void ChangePictureOptionsHandler::HandleSelectImage(const ListValue* args) {
     } else {
       SetImageFromCamera(user_photo_);
     }
-  } else {
+  } else if (image_type == "profile") {
     // Profile image selected. Could be previous (old) user image.
     user_image_manager->SaveUserImageFromProfileImage(user->email());
 
@@ -346,6 +348,8 @@ void ChangePictureOptionsHandler::HandleSelectImage(const ListValue* args) {
                                 kHistogramImagesCount);
       VLOG(1) << "Selected profile image";
     }
+  } else {
+    NOTREACHED() << "Unexpected image type: " << image_type;
   }
 
   // Ignore the result of the previous decoding if it's no longer needed.
