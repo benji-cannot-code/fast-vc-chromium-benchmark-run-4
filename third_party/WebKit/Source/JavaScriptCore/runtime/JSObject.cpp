1d9c6263977c5942629a512bd79dabe2b6c9aa8c
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-20 00:19:03 +0000

    JSObject::ensure<IndexingType> should gracefully handle InterceptsGetOwn..., and should never be called when the 'this' is not an object https://bugs.webkit.org/show_bug.cgi?id=105468
    Reviewed by Mark Hahnenberg, Oliver Hunt, and Gavin Barraclough.
    
    Source/JavaScriptCore: 
    
    Changed JSObject::ensure<IndexingType> methods to gracefully handle
    InterceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero. Most of them handle it by returning
    null as a result of indexingShouldBeSparse() returning true, while ensureArrayStorage handles it
    by entering dictionary indexing mode, which forces the object to behave correctly even if there
    is proxying or weird prototype stuff going on.
            
    Changed DFGOperations entrypoints to reject non-objects, so that JSObject doesn't have to deal
    with pretending to be JSString. In particular, this would go wrong in the ArrayStorage case
    since we'd try to resize a butterfly on a JSString, but JSString has something other than
    m_butterfly at that offset.
            
    Finally, removed all InterceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero from JIT code
    since those are now redundant.
    
    * dfg/DFGOperations.cpp:
    * dfg/DFGOperations.h:
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::arrayify):
    * dfg/DFGSpeculativeJIT.h:
    (JSC::DFG::SpeculativeJIT::callOperation):
    * runtime/JSObject.cpp:
    (JSC::JSObject::enterDictionaryIndexingMode):
    (JSC::JSObject::ensureInt32Slow):
    (JSC::JSObject::ensureDoubleSlow):
    (JSC::JSObject::ensureContiguousSlow):
    (JSC::JSObject::ensureArrayStorageSlow):
    (JSC):
    (JSC::JSObject::putByIndexBeyondVectorLengthWithoutAttributes):
    * runtime/JSObject.h:
    (JSObject):
    
    LayoutTests: 
    
    * fast/js/dfg-ensure-array-storage-on-string-expected.txt: Added.
    * fast/js/dfg-ensure-array-storage-on-string.html: Added.
    * fast/js/dfg-ensure-contiguous-on-string-expected.txt: Added.
    * fast/js/dfg-ensure-contiguous-on-string.html: Added.
    * fast/js/jsc-test-list
    * fast/js/script-tests/dfg-ensure-array-storage-on-string.js: Added.
    (foo):
    * fast/js/script-tests/dfg-ensure-contiguous-on-string.js: Added.
    (foo):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138201 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.cpp
@@ -579,7 +579,7 @@ void JSObject::enterDictionaryIndexingMode(JSGlobalData& globalData)
     case ALL_CONTIGUOUS_INDEXING_TYPES:
         // NOTE: this is horribly inefficient, as it will perform two conversions. We could optimize
         // this case if we ever cared.
-        enterDictionaryIndexingModeWhenArrayStorageAlreadyExists(globalData, ensureArrayStorageSlowNoCheck(globalData));
+        enterDictionaryIndexingModeWhenArrayStorageAlreadyExists(globalData, ensureArrayStorageSlow(globalData));
         break;
     case ALL_ARRAY_STORAGE_INDEXING_TYPES:
         enterDictionaryIndexingModeWhenArrayStorageAlreadyExists(globalData, m_butterfly->arrayStorage());
@@ -952,8 +952,7 @@ void JSObject::convertDoubleToContiguousWhilePerformingSetIndex(JSGlobalData& gl
 
 WriteBarrier<Unknown>* JSObject::ensureInt32Slow(JSGlobalData& globalData)
 {
-    if (structure()->typeInfo().interceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero())
-        CRASH();
+    ASSERT(inherits(&s_info));
     
     switch (structure()->indexingType()) {
     case ALL_BLANK_INDEXING_TYPES:
@@ -977,8 +976,7 @@ WriteBarrier<Unknown>* JSObject::ensureInt32Slow(JSGlobalData& globalData)
 
 double* JSObject::ensureDoubleSlow(JSGlobalData& globalData)
 {
-    if (structure()->typeInfo().interceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero())
-        CRASH();
+    ASSERT(inherits(&s_info));
     
     switch (structure()->indexingType()) {
     case ALL_BLANK_INDEXING_TYPES:
@@ -1004,8 +1002,7 @@ double* JSObject::ensureDoubleSlow(JSGlobalData& globalData)
 
 WriteBarrier<Unknown>* JSObject::ensureContiguousSlow(JSGlobalData& globalData, DoubleToContiguousMode mode)
 {
-    if (structure()->typeInfo().interceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero())
-        CRASH();
+    ASSERT(inherits(&s_info));
     
     switch (structure()->indexingType()) {
     case ALL_BLANK_INDEXING_TYPES:
@@ -1043,8 +1040,10 @@ WriteBarrier<Unknown>* JSObject::rageEnsureContiguousSlow(JSGlobalData& globalDa
     return ensureContiguousSlow(globalData, RageConvertDoubleToValue);
 }
 
-ArrayStorage* JSObject::ensureArrayStorageSlowNoCheck(JSGlobalData& globalData)
+ArrayStorage* JSObject::ensureArrayStorageSlow(JSGlobalData& globalData)
 {
+    ASSERT(inherits(&s_info));
+    
     switch (structure()->indexingType()) {
     case ALL_BLANK_INDEXING_TYPES:
         if (UNLIKELY(indexingShouldBeSparse()))
@@ -1075,14 +1074,6 @@ ArrayStorage* JSObject::ensureArrayStorageSlowNoCheck(JSGlobalData& globalData)
         ASSERT_NOT_REACHED();
         return 0;
     }
-}
-
-ArrayStorage* JSObject::ensureArrayStorageSlow(JSGlobalData& globalData)
-{
-    if (structure()->typeInfo().interceptsGetOwnPropertySlotByIndexEvenWhenLengthIsNotZero())
-        CRASH();
-    
-    return ensureArrayStorageSlowNoCheck(globalData);
 }
 
 ArrayStorage* JSObject::ensureArrayStorageExistsAndEnterDictionaryIndexingMode(JSGlobalData& globalData)
@@ -1933,7 +1924,7 @@ void JSObject::putByIndexBeyondVectorLengthWithoutAttributes(ExecState* exec, un
         || (i >= MIN_SPARSE_ARRAY_INDEX
             && !isDenseEnoughForVector(i, countElements<indexingShape>(m_butterfly)))) {
         ASSERT(i <= MAX_ARRAY_INDEX);
-        ensureArrayStorageSlowNoCheck(globalData);
+        ensureArrayStorageSlow(globalData);
         SparseArrayValueMap* map = allocateSparseIndexMap(globalData);
         map->putEntry(exec, this, i, value, false);
         ASSERT(i >= arrayStorage()->length());
--- third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
@@ -980,7 +980,6 @@ private:
     WriteBarrier<Unknown>* ensureContiguousSlow(JSGlobalData&);
     WriteBarrier<Unknown>* rageEnsureContiguousSlow(JSGlobalData&);
     ArrayStorage* ensureArrayStorageSlow(JSGlobalData&);
-    ArrayStorage* ensureArrayStorageSlowNoCheck(JSGlobalData&);
     
     enum DoubleToContiguousMode { EncodeValueAsDouble, RageConvertDoubleToValue };
     template<DoubleToContiguousMode mode>
