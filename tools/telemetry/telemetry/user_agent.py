2294111eddad4dc7fde417843ac7adf3b95331c5
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 00:28:26 +0000

    Reland 173813 Updated user agent to latest version. > [Telemetry] Add desktop user_agent_type to top_25.json page set. > > The wpr archive for this page set was recorded with a desktop user agent. Then > espn site has a client side redirect which breaks the page on mobile without > this. > > BUG=None > TEST=tools/perf/run_multipage_benchmarks --browser=android-content-shell > scrolling_benchmark tools/perf/page_sets/top_25.json > > Review URL: https://codereview.chromium.org/11644003
    TBR=tonyg@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/11642034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174052 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/user_agent.py
+++ tools/telemetry/telemetry/user_agent.py
@@ -6,8 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 UA_TYPE_MAPPING = {
   'desktop':
       'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_3) '
-      'AppleWebKit/535.19 (KHTML, like Gecko) '
-      'Chrome/18.0.1025.151 Safari/535.19',
+      'AppleWebKit/537.22 (KHTML, like Gecko) '
+      'Chrome/25.0.1364.2 Safari/537.22',
   'mobile':
       'Mozilla/5.0 (Linux; Android 4.0.4; Galaxy Nexus Build/IMM76B) '
       'AppleWebKit/535.19 (KHTML, like Gecko) Chrome/18.0.1025.133 Mobile '
