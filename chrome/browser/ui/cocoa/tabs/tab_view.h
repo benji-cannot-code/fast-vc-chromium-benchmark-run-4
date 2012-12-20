bf789e5b077995bb1803a6540d57c149f7e788ce
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-20 01:27:33 +0000

    mac: Move the tab mask from vectors to bitmap.
    Creating the mask image on every paint, this improves cpu usage with
    8 spinners from ~15% cpu to ~10% cpu. With a per-tab single-element
    mask cache, this drops further to ~8.4% cpu, for a relative improvement
    of 78%.
    
    Based very loosely on a patch from Andrew Bonventre <andybons@chromium.org>
    
    BUG=19268,52468
    
    Review URL: https://codereview.chromium.org/11638027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/tab_view.h
+++ chrome/browser/ui/cocoa/tabs/tab_view.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 #include <ApplicationServices/ApplicationServices.h>
 
+#include "base/mac/scoped_cftyperef.h"
 #include "base/memory/scoped_nsobject.h"
 #import "chrome/browser/ui/cocoa/hover_close_button.h"
 
@@ -67,6 +68,12 @@ enum AlertState {
 
   // The tool tip text for this tab view.
   scoped_nsobject<NSString> toolTipText_;
+
+  // A one-element mask image cache.  This cache makes drawing roughly 16%
+  // faster.
+  base::mac::ScopedCFTypeRef<CGImageRef> maskCache_;
+  CGFloat maskCacheWidth_;
+  CGFloat maskCacheScale_;
 }
 
 @property(assign, nonatomic) NSCellStateValue state;
