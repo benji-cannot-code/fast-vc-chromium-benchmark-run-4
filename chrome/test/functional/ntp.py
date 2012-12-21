ec77d28468f375da1ba749157c3c89ce87103569
vclarke@chromium.org <vclarke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 17:43:00 +0000

    Revert 169105 > Fix failing NTP pyauto tests due to missing default app. > > > BUG=162171 > TEST=Tests passed on win chrome-qa bot. > NOTRY=true > > > Review URL: https://chromiumcodereview.appspot.com/11418109
    BUG=162171
    TBR=dyu@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11639056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/ntp.py
+++ chrome/test/functional/ntp.py
@@ -24,7 +24,6 @@ class NTPTest(pyauto.PyUITest):
     _EXPECTED_DEFAULT_APPS.append({u'title': u'Chrome'})
   else:
     _EXPECTED_DEFAULT_APPS.append({u'title': u'Cloud Print'})
-    _EXPECTED_DEFAULT_APPS.append({u'title': u'Settings'})
 
   # Default menu and thumbnail mode preferences are set in
   # ShownSectionsHandler::RegisterUserPrefs.
@@ -470,7 +469,6 @@ class NTPTest(pyauto.PyUITest):
     expected_app_url_start = 'chrome-extension://' + installed_app_id
     self.assertTrue(actual_tab_url.startswith(expected_app_url_start),
                     msg='The app was not launched in the new window.')
-
 
 if __name__ == '__main__':
   pyauto_functional.Main()
