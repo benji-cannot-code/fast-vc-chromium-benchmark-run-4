e564136e4fedc0cb4a9c39d889ce365f96aa4e37
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 00:13:29 +0000

    Cleanup:  Rename print preview tab to print preview dialog in BackgroundPrintingManager.
    BUG=163671
    Review URL: https://codereview.chromium.org/11636041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174286 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_preview_dialog_controller.cc
+++ chrome/browser/printing/print_preview_dialog_controller.cc
@@ -291,6 +291,11 @@ void PrintPreviewDialogController::Observe(
 }
 
 // static
+bool PrintPreviewDialogController::IsPrintPreviewDialog(WebContents* contents) {
+  return IsPrintPreviewURL(contents->GetURL());
+}
+
+// static
 bool PrintPreviewDialogController::IsPrintPreviewTab(WebContents* tab) {
   return IsPrintPreviewURL(tab->GetURL());
 }
