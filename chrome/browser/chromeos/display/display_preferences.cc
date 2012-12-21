c56d0b2cce37fa8f1457f5f60f9f29fe2221cb78
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 21:27:31 +0000

    Allows only valid users to store the display layout preferences.
    Guest users can change the settings but it's not stored.
    
    BUG=166964
    TEST=see the repro steps in the bug
    
    
    Review URL: https://chromiumcodereview.appspot.com/11640031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174458 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/display/display_preferences.cc
+++ chrome/browser/chromeos/display/display_preferences.cc
@@ -160,12 +160,11 @@ void RegisterDisplayLocalStatePrefs(PrefService* local_state) {
 void SetDisplayLayoutPref(const gfx::Display& display,
                           int layout,
                           int offset) {
-  PrefService* local_state = g_browser_process->local_state();
-
-  {
+  ash::DisplayLayout display_layout(
+      static_cast<ash::DisplayLayout::Position>(layout), offset);
+  if (IsValidUser()) {
+    PrefService* local_state = g_browser_process->local_state();
     DictionaryPrefUpdate update(local_state, prefs::kSecondaryDisplays);
-    ash::DisplayLayout display_layout(
-        static_cast<ash::DisplayLayout::Position>(layout), offset);
 
     std::string name = base::Int64ToString(display.id());
     DCHECK(!name.empty());
@@ -179,12 +178,13 @@ void SetDisplayLayoutPref(const gfx::Display& display,
     }
     if (ash::DisplayLayout::ConvertToValue(display_layout, layout_value.get()))
       pref_data->Set(name, layout_value.release());
+
+    local_state->SetInteger(prefs::kSecondaryDisplayLayout, layout);
+    local_state->SetInteger(prefs::kSecondaryDisplayOffset, offset);
   }
 
-  local_state->SetInteger(prefs::kSecondaryDisplayLayout, layout);
-  local_state->SetInteger(prefs::kSecondaryDisplayOffset, offset);
-
-  NotifyDisplayLayoutChanged();
+  ash::Shell::GetInstance()->display_controller()->SetLayoutForDisplayId(
+      display.id(), display_layout);
 }
 
 void StorePrimaryDisplayIDPref(int64 display_id) {
@@ -200,10 +200,7 @@ void StorePrimaryDisplayIDPref(int64 display_id) {
 
 void SetDisplayOverscan(const gfx::Display& display,
                         const gfx::Insets& insets) {
-  if (!IsValidUser())
-    return;
-
-  {
+  if (IsValidUser()) {
     DictionaryPrefUpdate update(
         g_browser_process->local_state(), prefs::kDisplayOverscans);
     const std::string id = base::Int64ToString(display.id());
@@ -214,7 +211,8 @@ void SetDisplayOverscan(const gfx::Display& display,
     pref_data->Set(id, insets_value);
   }
 
-  NotifyDisplayOverscans();
+  ash::Shell::GetInstance()->display_controller()->SetOverscanInsets(
+      display.id(), insets);
 }
 
 void SetPrimaryDisplayIDPref(int64 display_id) {
