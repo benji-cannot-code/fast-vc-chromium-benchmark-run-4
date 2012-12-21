53f8a4c95871df18195ddad5767bf4233ac63ab1
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 16:51:53 +0000

    Remove testing for iOS5+ as this is now the minimal version supported.
    BUG=None
    R=stuartmorgan@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11661006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174398 0039d316-1c4b-4281-b951-d872f2087c98

--- base/ios/ios_util.h
+++ base/ios/ios_util.h
@@ -12,9 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace base {
 namespace ios {
 
-// Returns whether the operation system is iOS 5 or later.
-BASE_EXPORT bool IsRunningOnIOS5OrLater();
-
 // Returns whether the operation system is iOS 6 or later.
 BASE_EXPORT bool IsRunningOnIOS6OrLater();
 
