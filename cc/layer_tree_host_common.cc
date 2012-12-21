563e4f713bfd7dcdb90d0c1924b9ae4ac5bee143
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 02:59:09 +0000

    cc: When the transform's scale is unknown, draw into render surfaces at the scale of the screen.
    When the transform's scale is not known, we have to use some fixed value. We
    were using 1.0 but this means if the transform is close to the identity, the
    layers in the surface are rendered small, and then scaled up when the surface
    is drawn to the screen by the device scale factor and page scale factor. This
    makes for fuzziness.
    
    So, when the transform's scale is not known we should use the device scale
    and page scale factors as the default value. This guarantees that the transforms
    from the contents of the surface's subtree to the screen will not require
    scaling up more than any CSS scales specified by the HTML.
    
    Tests:
    LayerTreeHostCommonTest.verifySurfaceLayerTransformsInHighDPI
    
    BUG=167021
    
    Review URL: https://chromiumcodereview.appspot.com/11635037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174327 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_common.cc
+++ cc/layer_tree_host_common.cc
@@ -391,7 +391,7 @@ static inline void updateLayerContentsScale(Layer* layer, const gfx::Transform& 
         rasterScale = 1;
 
         if (!animatingTransformToScreen && layer->automaticallyComputeRasterScale()) {
-            gfx::Vector2dF transformScale = MathUtil::computeTransform2dScaleComponents(combinedTransform);
+            gfx::Vector2dF transformScale = MathUtil::computeTransform2dScaleComponents(combinedTransform, 0.f);
             float combinedScale = std::max(transformScale.x(), transformScale.y());
             rasterScale = combinedScale / deviceScaleFactor;
             if (!layer->boundsContainPageScale())
@@ -661,7 +661,7 @@ static void calculateDrawPropertiesInternal(LayerType* layer, const gfx::Transfo
     gfx::Transform nextHierarchyMatrix = fullHierarchyMatrix;
     gfx::Transform sublayerMatrix;
 
-    gfx::Vector2dF renderSurfaceSublayerScale = MathUtil::computeTransform2dScaleComponents(combinedTransform);
+    gfx::Vector2dF renderSurfaceSublayerScale = MathUtil::computeTransform2dScaleComponents(combinedTransform, deviceScaleFactor * pageScaleFactor);
 
     if (subtreeShouldRenderToSeparateSurface(layer, combinedTransform.IsScaleOrTranslation())) {
         // Check back-face visibility before continuing with this surface and its subtree
