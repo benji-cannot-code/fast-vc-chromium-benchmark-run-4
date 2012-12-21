488bbb7e02655facddd381d4ddfe3c72f5858199
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 00:36:15 +0000

    Make qcms compile on Win64
    BUG=166496
    
    Review URL: https://chromiumcodereview.appspot.com/11645034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174294 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/qcms/src/transform_util.h
+++ third_party/qcms/src/transform_util.h
@@ -32,9 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //XXX: could use a bettername
 typedef uint16_t uint16_fract_t;
 
-float lut_interp_linear(double value, uint16_t *table, int length);
-float lut_interp_linear_float(float value, float *table, int length);
-uint16_t lut_interp_linear16(uint16_t input_value, uint16_t *table, int length);
+float lut_interp_linear(double value, uint16_t *table, size_t length);
+float lut_interp_linear_float(float value, float *table, size_t length);
+uint16_t lut_interp_linear16(uint16_t input_value, uint16_t *table, size_t length);
 
 
 static inline float lerp(float a, float b, float t)
