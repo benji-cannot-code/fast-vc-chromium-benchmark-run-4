bf2745195c26b7dc8bc2c8959c0697f3c3068a56
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-21 05:08:51 +0000

    [WebGL][EFL][Qt][GTK] Jelly fishes leave trails on webgl canvas. https://bugs.webkit.org/show_bug.cgi?id=105326
    Patch by Huang Dongsung <luxtella@company100.net> on 2012-12-20
    Reviewed by Noam Rosenthal.
    
    Clear a drawing buffer after compositing if preserveDrawingBuffer is
    false.
    http://www.khronos.org/registry/webgl/specs/latest/#2.2
    
    No new tests. Covered by existing webgl-preserve-drawing-buffer-repaint test.
    
    * platform/graphics/cairo/GraphicsContext3DPrivate.cpp:
    (WebCore::GraphicsContext3DPrivate::paintToTextureMapper):
    * platform/graphics/efl/GraphicsContext3DPrivate.cpp:
    (GraphicsContext3DPrivate::copyToGraphicsSurface):
    * platform/graphics/qt/GraphicsContext3DQt.cpp:
    (WebCore::GraphicsContext3DPrivate::paintToTextureMapper):
    (WebCore::GraphicsContext3DPrivate::copyToGraphicsSurface):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138346 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/qt/GraphicsContext3DQt.cpp
@@ -211,6 +211,7 @@ static inline quint32 swapBgrToRgb(quint32 pixel)
 #if USE(ACCELERATED_COMPOSITING)
 void GraphicsContext3DPrivate::paintToTextureMapper(TextureMapper* textureMapper, const FloatRect& targetRect, const TransformationMatrix& matrix, float opacity, BitmapTexture* mask)
 {
+    m_context->markLayerComposited();
     blitMultisampleFramebufferAndRestoreContext();
 
     if (textureMapper->accelerationMode() == TextureMapper::OpenGLMode) {
@@ -291,6 +292,7 @@ uint32_t GraphicsContext3DPrivate::copyToGraphicsSurface()
     if (!m_graphicsSurface)
         return 0;
 
+    m_context->markLayerComposited();
     blitMultisampleFramebufferAndRestoreContext();
     m_graphicsSurface->copyFromTexture(m_context->m_texture, IntRect(0, 0, m_context->m_currentWidth, m_context->m_currentHeight));
     return m_graphicsSurface->frontBuffer();
