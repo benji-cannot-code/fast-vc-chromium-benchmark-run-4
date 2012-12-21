6077c8a3af45275d2dfe9243644a22e7e92511bf
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 13:48:30 +0000

    [Android WebView] Fix some locale related TODOs.
    Implement AwContentBrowserClient::GetAcceptLangs as the classic
    WebView did - use the current local for the primary accept language
    and append en-US as an alternative if the primary language was something
    else.
    
    Use UTF-8 as the default accept charset.
    
    As far as I can tell
    AwContentBrowserClient::GetCanonicalEncodingNameByAliasName should
    simply echo the passed in string - we shouldn't get passed anything
    other than a canonical encoding name in android_webview/.
    
    Move the mapping of Android deprecated language codes from l10n_util.cc into base/android/java/.../LocaleUtils.java to make that logic easier to reuse by android_webview.
    
    Review URL: https://chromiumcodereview.appspot.com/11614002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174385 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/l10n/l10n_util.cc
+++ ui/base/l10n/l10n_util.cc
@@ -307,8 +307,7 @@ bool CheckAndResolveLocale(const std::string& locale,
     }
   }
 
-  // Google updater uses no, iw and en for our nb, he, and en-US, and
-  // Android uses iw-*, in-*, and ji-*.  We need to map them to our codes.
+  // Google updater uses no, tl, iw and en for our nb, fil, he, and en-US.
   struct {
     const char* source;
     const char* dest;
@@ -317,8 +316,6 @@ bool CheckAndResolveLocale(const std::string& locale,
       {"tl", "fil"},
       {"iw", "he"},
       {"en", "en-US"},
-      {"in", "id"},
-      {"ji", "yi"},
   };
 
   for (size_t i = 0; i < ARRAYSIZE_UNSAFE(alias_map); ++i) {
