00230bb78b4cd3e21bbe4614db3f62847a11476e
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 00:35:33 +0000

    Don't run VideoRendererBase::statistics_cb_ if we've stopped.
    BUG=151733
    
    
    Review URL: https://chromiumcodereview.appspot.com/11618045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174291 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base.cc
+++ media/filters/video_renderer_base.cc
@@ -256,6 +256,12 @@ void VideoRendererBase::ThreadMain() {
   uint32 frames_dropped = 0;
 
   for (;;) {
+    base::AutoLock auto_lock(lock_);
+
+    // Thread exit condition.
+    if (state_ == kStopped)
+      return;
+
     if (frames_dropped > 0) {
       PipelineStatistics statistics;
       statistics.video_frames_dropped = frames_dropped;
@@ -263,12 +269,6 @@ void VideoRendererBase::ThreadMain() {
 
       frames_dropped = 0;
     }
-
-    base::AutoLock auto_lock(lock_);
-
-    // Thread exit condition.
-    if (state_ == kStopped)
-      return;
 
     // Remain idle as long as we're not playing.
     if (state_ != kPlaying || playback_rate_ == 0) {
