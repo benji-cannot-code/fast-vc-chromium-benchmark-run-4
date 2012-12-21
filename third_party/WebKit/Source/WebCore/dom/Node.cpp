9240928530cd8193ac7f8eeec83d55ccf5f0aaea
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-21 02:42:11 +0000

    Replace documentFragmentIsShadowRoot with isTreeScope https://bugs.webkit.org/show_bug.cgi?id=105345
    Reviewed by Dimitri Glazkov.
    
    .:
    
    Expose isTreeScope symbol.
    
    * Source/autotools/symbols.filter:
    
    Source/WebCore:
    
    We can replace documentFragmentIsShadowRoot with isTreeScope() which is
    trival to detect by checking treeScope()->rootNode() == this;
    
    No new tests, just refactoring.
    
    * WebCore.exp.in:
    * dom/DocumentFragment.h:
    (DocumentFragment):
    * dom/Node.cpp:
    (WebCore::Node::isTreeScope):
    * dom/Node.h:
    (Node):
    (WebCore::Node::isShadowRoot):
    * dom/ShadowRoot.h:
    (ShadowRoot):
    
    Source/WebKit2:
    
    Expose isTreeScope symbol.
    
    * win/WebKit2.def.in:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138338 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.cpp
+++ third_party/WebKit/Source/WebCore/dom/Node.cpp
@@ -914,6 +914,11 @@ bool Node::isFocusable() const
     return true;
 }
 
+bool Node::isTreeScope() const
+{
+    return treeScope()->rootNode() == this;
+}
+
 bool Node::isKeyboardFocusable(KeyboardEvent*) const
 {
     return isFocusable() && tabIndex() >= 0;
@@ -922,12 +927,6 @@ bool Node::isKeyboardFocusable(KeyboardEvent*) const
 bool Node::isMouseFocusable() const
 {
     return isFocusable();
-}
-
-bool Node::documentFragmentIsShadowRoot() const
-{
-    ASSERT_NOT_REACHED();
-    return false;
 }
 
 Node* Node::focusDelegate()
