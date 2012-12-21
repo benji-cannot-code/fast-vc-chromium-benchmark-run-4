1e6240557a84955d60edb3d4513c0e42d7dcba37
marcheu@chromium.org <marcheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-21 20:04:09 +0000

    output_configurator: misc cleanups
    Turn const == var into var == const
    The output_configurator uses a mix of both currently, so switch to
    var == const style.
    
    Remove static function qualifier inside anonymous namespace.
    The static qualifier isn't needed because all these functions are living in
    the anonymous namespace, so remove it.
    
    BUG=none
    TEST=compiles and runs
    
    
    Review URL: https://chromiumcodereview.appspot.com/11637019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174441 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/display/output_configurator.cc
+++ chromeos/display/output_configurator.cc
@@ -122,7 +122,7 @@ const int kVerticalGap = 60;
 // than O(n) lookup time.  In many call sites, for example, the "next" mode is
 // typically what we are looking for so using this helper might be too
 // expensive.
-static XRRModeInfo* ModeInfoForID(XRRScreenResources* screen, RRMode modeID) {
+XRRModeInfo* ModeInfoForID(XRRScreenResources* screen, RRMode modeID) {
   XRRModeInfo* result = NULL;
   for (int i = 0; (i < screen->nmode) && (result == NULL); i++)
     if (modeID == screen->modes[i].id)
@@ -133,9 +133,9 @@ static XRRModeInfo* ModeInfoForID(XRRScreenResources* screen, RRMode modeID) {
 
 // A helper to call XRRSetCrtcConfig with the given options but some of our
 // default output count and rotation arguments.
-static void ConfigureCrtc(Display* display,
-                          XRRScreenResources* screen,
-                          CrtcConfig* config) {
+void ConfigureCrtc(Display* display,
+                   XRRScreenResources* screen,
+                   CrtcConfig* config) {
   VLOG(1) << "ConfigureCrtc crtc: " << config->crtc
           << ", mode " << config->mode
           << ", output " << config->output
@@ -217,13 +217,13 @@ void DestroyUnusedCrtcs(Display* display,
 
 // Called to set the frame buffer (underling XRR "screen") size.  Has a
 // side-effect of disabling all CRTCs.
-static void CreateFrameBuffer(Display* display,
-                              XRRScreenResources* screen,
-                              Window window,
-                              int width,
-                              int height,
-                              CrtcConfig* config1,
-                              CrtcConfig* config2) {
+void CreateFrameBuffer(Display* display,
+                       XRRScreenResources* screen,
+                       Window window,
+                       int width,
+                       int height,
+                       CrtcConfig* config1,
+                       CrtcConfig* config2) {
   VLOG(1) << "CreateFrameBuffer " << width << " by " << height;
 
   DestroyUnusedCrtcs(display, screen, window, config1, config2, width, height);
@@ -239,9 +239,9 @@ static void CreateFrameBuffer(Display* display,
 // |ctm| contains the desired transformation parameters.
 // The offsets in it should be normalized,
 // so that 1 corresponds to x or y axis size for the respectful offset.
-static void ConfigureCTM(Display* display,
-                         int touch_device_id,
-                         const CoordinateTransformation& ctm) {
+void ConfigureCTM(Display* display,
+                  int touch_device_id,
+                  const CoordinateTransformation& ctm) {
   int ndevices;
   XIDeviceInfo* info = XIQueryDevice(display, touch_device_id, &ndevices);
   Atom prop = XInternAtom(display, "Coordinate Transformation Matrix", False);
@@ -290,8 +290,8 @@ static void ConfigureCTM(Display* display,
 // |screen| is used to make X calls.
 // |output| is the output on which mirror mode is being applied.
 // Returns the transformation, which would be identity if computations fail.
-static CoordinateTransformation GetMirrorModeCTM(XRRScreenResources* screen,
-                                                 const OutputSnapshot* output) {
+CoordinateTransformation GetMirrorModeCTM(XRRScreenResources* screen,
+                                          const OutputSnapshot* output) {
   CoordinateTransformation ctm;  // Default to identity
   XRRModeInfo* native_mode_info = ModeInfoForID(screen, output->native_mode);
   XRRModeInfo* mirror_mode_info = ModeInfoForID(screen, output->mirror_mode);
@@ -325,10 +325,9 @@ static CoordinateTransformation GetMirrorModeCTM(XRRScreenResources* screen,
   return ctm;  // Same aspect ratio - return identity
 }
 
-static OutputState InferCurrentState(
-    Display* display,
-    XRRScreenResources* screen,
-    const std::vector<OutputSnapshot>& outputs) {
+OutputState InferCurrentState(Display* display,
+                              XRRScreenResources* screen,
+                              const std::vector<OutputSnapshot>& outputs) {
   OutputState state = STATE_INVALID;
   switch (outputs.size()) {
     case 0:
@@ -341,15 +340,15 @@ static OutputState InferCurrentState(
       RRMode primary_mode = outputs[0].current_mode;
       RRMode secondary_mode = outputs[1].current_mode;
 
-      if ((0 == outputs[0].y) && (0 == outputs[1].y)) {
+      if ((outputs[0].y == 0) && (outputs[1].y == 0)) {
         // Displays in the same spot so this is either mirror or unknown.
         // Note that we should handle no configured CRTC as a "wildcard" since
         // that allows us to preserve mirror mode state while power is switched
         // off on one display.
         bool primary_mirror = (outputs[0].mirror_mode == primary_mode) ||
-            (None == primary_mode);
+            (primary_mode == None);
         bool secondary_mirror = (outputs[1].mirror_mode == secondary_mode) ||
-            (None == secondary_mode);
+            (secondary_mode == None);
         if (primary_mirror && secondary_mirror) {
           state = STATE_DUAL_MIRROR;
         } else {
@@ -363,17 +362,17 @@ static OutputState InferCurrentState(
         // such that one is primary and another is correctly positioned as
         // secondary.  If any of these assumptions are false, this is an unknown
         // configuration.
-        bool primary_native = (outputs[0].native_mode == primary_mode) ||
-            (None == primary_mode);
-        bool secondary_native = (outputs[1].native_mode == secondary_mode) ||
-            (None == secondary_mode);
+        bool primary_native = (primary_mode == outputs[0].native_mode) ||
+            (primary_mode == None);
+        bool secondary_native = (secondary_mode == outputs[1].native_mode) ||
+            (secondary_mode == None);
         if (primary_native && secondary_native) {
           // Just check the relative locations.
           int secondary_offset = outputs[0].height + kVerticalGap;
           int primary_offset = outputs[1].height + kVerticalGap;
-          if ((0 == outputs[0].y) && (secondary_offset == outputs[1].y)) {
+          if ((outputs[0].y == 0) && (outputs[1].y == secondary_offset)) {
             state = STATE_DUAL_PRIMARY_ONLY;
-          } else if ((0 == outputs[1].y) && (primary_offset == outputs[0].y)) {
+          } else if ((outputs[1].y == 0) && (outputs[0].y == primary_offset)) {
             state = STATE_DUAL_SECONDARY_ONLY;
           } else {
             // Unexpected locations.
@@ -392,10 +391,10 @@ static OutputState InferCurrentState(
   return state;
 }
 
-static OutputState GetNextState(Display* display,
-                                XRRScreenResources* screen,
-                                OutputState current_state,
-                                const std::vector<OutputSnapshot>& outputs) {
+OutputState GetNextState(Display* display,
+                         XRRScreenResources* screen,
+                         OutputState current_state,
+                         const std::vector<OutputSnapshot>& outputs) {
   OutputState state = STATE_INVALID;
 
   switch (outputs.size()) {
@@ -428,14 +427,14 @@ static OutputState GetNextState(Display* display,
   return state;
 }
 
-static RRCrtc GetNextCrtcAfter(Display* display,
-                               XRRScreenResources* screen,
-                               RROutput output,
-                               RRCrtc previous) {
+RRCrtc GetNextCrtcAfter(Display* display,
+                        XRRScreenResources* screen,
+                        RROutput output,
+                        RRCrtc previous) {
   RRCrtc crtc = None;
   XRROutputInfo* output_info = XRRGetOutputInfo(display, screen, output);
 
-  for (int i = 0; (i < output_info->ncrtc) && (None == crtc); ++i) {
+  for (int i = 0; (i < output_info->ncrtc) && (crtc == None); ++i) {
     RRCrtc this_crtc = output_info->crtcs[i];
 
     if (previous != this_crtc)
@@ -445,8 +444,8 @@ static RRCrtc GetNextCrtcAfter(Display* display,
   return crtc;
 }
 
-static XRRScreenResources* GetScreenResourcesAndRecordUMA(Display* display,
-                                                          Window window) {
+XRRScreenResources* GetScreenResourcesAndRecordUMA(Display* display,
+                                                   Window window) {
   // This call to XRRGetScreenResources is implicated in a hang bug so
   // instrument it to see its typical running time (crbug.com/134449).
   // TODO(disher): Remove these UMA calls once crbug.com/134449 is resolved.
@@ -461,7 +460,7 @@ static XRRScreenResources* GetScreenResourcesAndRecordUMA(Display* display,
 
 // Determine if there is an "internal" output and how many outputs are
 // connected.
-static bool IsProjecting(const std::vector<OutputSnapshot>& outputs) {
+bool IsProjecting(const std::vector<OutputSnapshot>& outputs) {
   bool has_internal_output = false;
   int connected_output_count = outputs.size();
   for (size_t i = 0; i < outputs.size(); ++i)
@@ -473,8 +472,8 @@ static bool IsProjecting(const std::vector<OutputSnapshot>& outputs) {
 }
 
 // Returns whether the |output| is configured to preserve aspect when scaling.
-static bool IsOutputAspectPreservingScaling(Display* display,
-                                            RROutput output) {
+bool IsOutputAspectPreservingScaling(Display* display,
+                                     RROutput output) {
   bool ret = false;
 
   Atom scaling_prop = XInternAtom(display, "scaling mode", False);
@@ -667,7 +666,7 @@ bool OutputConfigurator::ScreenPowerSet(bool power_on, bool all_displays) {
       config.output = outputs[i].output;
       config.mode = None;
       if (power_on) {
-        config.mode = (STATE_DUAL_MIRROR == output_state_) ?
+        config.mode = (output_state_ == STATE_DUAL_MIRROR) ?
             outputs[i].mirror_mode : outputs[i].native_mode;
       } else if (connected_output_count_ > 1 && !all_displays &&
                  outputs[i].is_internal) {
@@ -816,12 +815,12 @@ std::vector<OutputSnapshot> OutputConfigurator::GetDualOutputs(
   for (int i = 0; (i < screen->noutput) && (outputs.size() < 2); ++i) {
     RROutput this_id = screen->outputs[i];
     XRROutputInfo* output_info = XRRGetOutputInfo(display, screen, this_id);
-    bool is_connected = (RR_Connected == output_info->connection);
+    bool is_connected = (output_info->connection == RR_Connected);
 
     if (is_connected) {
       OutputSnapshot to_populate;
 
-      if (0 == outputs.size()) {
+      if (outputs.size() == 0) {
         one_info = output_info;
       } else {
         two_info = output_info;
@@ -864,7 +863,7 @@ std::vector<OutputSnapshot> OutputConfigurator::GetDualOutputs(
     }
   }
 
-  if (2 == outputs.size()) {
+  if (outputs.size() == 2) {
     bool one_is_internal = IsInternalOutput(one_info);
     bool two_is_internal = IsInternalOutput(two_info);
     int internal_outputs = (one_is_internal ? 1 : 0) +
@@ -1156,7 +1155,7 @@ bool OutputConfigurator::EnterState(
       RRCrtc secondary_crtc =
           GetNextCrtcAfter(display, screen, outputs[1].output, primary_crtc);
 
-      if (STATE_DUAL_MIRROR == new_state) {
+      if (new_state == STATE_DUAL_MIRROR) {
         XRRModeInfo* mode_info = ModeInfoForID(screen, outputs[0].mirror_mode);
         if (mode_info == NULL) {
           UMA_HISTOGRAM_COUNTS("Display.EnterState.mirror_failures", 1);
