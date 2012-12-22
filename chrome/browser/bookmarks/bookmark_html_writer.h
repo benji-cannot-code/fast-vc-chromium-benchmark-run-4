e05ed0b29679d77de54b617510358a0dcf65f0f5
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-22 18:09:58 +0000

    bookmarks: Forward declare FaviconBitmapResult in bookmark_html_writer.h
    BUG=144783
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11624028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174535 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_html_writer.h
+++ chrome/browser/bookmarks/bookmark_html_writer.h
@@ -10,15 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 #include <string>
 
+#include "base/file_path.h"
 #include "base/memory/ref_counted.h"
-#include "chrome/browser/history/history.h"
+#include "base/memory/ref_counted_memory.h"
 #include "chrome/common/cancelable_task_tracker.h"
+#include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "net/base/file_stream.h"
 
 class BookmarkNode;
-class FilePath;
 class Profile;
+
+namespace history {
+struct FaviconBitmapResult;
+}
 
 // Observer for bookmark html output. Used only in tests.
 class BookmarksExportObserver {
