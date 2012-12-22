c935c942040e59644665942d783324fd42bdcc6b
tomfinegan@chromium.org <tomfinegan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-22 02:00:26 +0000

    Add wrapper class to media for support of VP9 video, and add a command line flag to enable the support.
    This initial version of the wrapper provides support for decoding VP9 video in WebM container files, and is disabled by default.
    
    New flag added: --enable-vp9-playback
    
    TBR=brettw,scherkus,xhwang
    BUG=166094
    TEST=VP9 video in WebM containers plays back in <video> elements when --enable-vp9-playback is specified on the command line.
    
    Review URL: https://codereview.chromium.org/11644078
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174488 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -802,6 +802,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDisableTouchAdjustment,
     switches::kEnableViewport,
     switches::kEnableOpusPlayback,
+    switches::kEnableVp9Playback,
     switches::kForceDeviceScaleFactor,
     switches::kFullMemoryCrashReport,
 #if !defined (GOOGLE_CHROME_BUILD)
