87f8b22ffcd7d40dbed766c9a3519d64db0cd88b
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-22 00:32:04 +0000

    Enable "Let me choose what to sync" checkbox in gaia sign in page. Enable interstial page for signing in to chrome. Clean up handling of gaia redirects during sign in (like youtube and blogger).
    BUG=165346
    TEST=The old one-click infobar is gone and now replaced with the full page
    interstitial.  The rules for when it show or not show are the same as for the
    one infobar.
    
    The checkbox on the gaia page should now take the user to the settings page
    to configure settings before sync is enabled.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11494003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174481 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_setup_handler.cc
+++ chrome/browser/ui/webui/sync_setup_handler.cc
@@ -1052,6 +1052,9 @@ void SyncSetupHandler::CloseSyncSetup() {
     }
 
     // Let the various services know that we're no longer active.
+    if (SyncPromoUI::UseWebBasedSigninFlow())
+      CloseGaiaSigninPage();
+
     GetLoginUIService()->LoginUIClosed(this);
   }
 
