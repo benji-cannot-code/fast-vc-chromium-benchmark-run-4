24b4fd209bb55d50bac5b20c191d30c6a1a2b54e
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-22 03:22:55 +0000

    Added onSyncStateChanged end to end browser test to check that remote notifications are sent to JavaScript Listeners.
    BUG=160939
    
    
    Review URL: https://chromiumcodereview.appspot.com/11437047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174494 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/sync_file_system/sync_file_system_apitest.cc
+++ chrome/browser/extensions/api/sync_file_system/sync_file_system_apitest.cc
@@ -5,12 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/file_util.h"
 #include "base/path_service.h"
+#include "base/run_loop.h"
+#include "chrome/browser/extensions/event_names.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/sync_file_system/local_change_processor.h"
 #include "chrome/browser/sync_file_system/mock_remote_file_sync_service.h"
 #include "chrome/browser/sync_file_system/sync_file_system_service.h"
 #include "chrome/common/chrome_version_info.h"
 #include "chrome/common/extensions/features/feature.h"
+#include "testing/gtest/include/gtest/gtest.h"
+#include "webkit/fileapi/syncable/sync_status_code.h"
 #include "webkit/quota/quota_manager.h"
 
 using sync_file_system::MockRemoteFileSyncService;
@@ -56,10 +60,14 @@ class SyncFileSystemApiTest : public ExtensionApiTest {
   int64 real_default_quota_;
 };
 
-}  // namespace
+ACTION_P(NotifyOkStateAndCallback, mock_remote_service) {
+  mock_remote_service->NotifyRemoteServiceStateUpdated(
+      sync_file_system::REMOTE_SERVICE_OK, "Test event description.");
+  base::MessageLoopProxy::current()->PostTask(
+      FROM_HERE, base::Bind(arg1, fileapi::SYNC_STATUS_OK));
+}
 
-// TODO(calvinlo): Add Chrome OS support for syncable file system
-#if !defined(OS_CHROMEOS)
+}  // namespace
 
 // TODO(calvinlo): Add Chrome OS support for syncable file system
 // (http://crbug.com/160693)
@@ -75,6 +83,14 @@ IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, GetUsageAndQuota) {
       << message_;
 }
 
+IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, OnSyncStateChanged) {
+  EXPECT_CALL(*mock_remote_service(),
+              RegisterOriginForTrackingChanges(_, _))
+      .WillOnce(NotifyOkStateAndCallback(mock_remote_service()));
+  ASSERT_TRUE(RunPlatformAppTest("sync_file_system/on_sync_state_changed"))
+      << message_;
+}
+
 IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, RequestFileSystem) {
   EXPECT_CALL(*mock_remote_service(),
               RegisterOriginForTrackingChanges(_, _)).Times(1);
@@ -86,8 +102,6 @@ IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, WriteFileThenGetUsage) {
   ASSERT_TRUE(RunPlatformAppTest("sync_file_system/write_file_then_get_usage"))
       << message_;
 }
-
-#endif  // !defined(OS_CHROMEOS)
 
 #endif  // !defined(OS_CHROMEOS)
 
---
+++ chrome/test/data/extensions/api_test/sync_file_system/on_sync_state_changed/test.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function setupListener() {
+  chrome.syncFileSystem.onSyncStateChanged.addListener(checkEventReceived);
+  chrome.syncFileSystem.requestFileSystem('drive', function() {});
+}
+
+function checkEventReceived(syncState) {
+  chrome.test.assertEq("drive", syncState.service_name);
+  chrome.test.assertEq("running", syncState.state);
+  chrome.test.assertEq("Test event description.", syncState.description);
+  chrome.test.succeed();
+}
+
+chrome.test.runTests([
+  setupListener
+]);
