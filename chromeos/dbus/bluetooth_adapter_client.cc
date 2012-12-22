3b622dc92c9893726f79864ba1c6f0749b932aae
deymo@chromium.org <deymo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-22 21:49:37 +0000

    Bluetooth: Fix error_message parsing on a dbus error response.
    Fix an incorrect use of "bool Message::PopString(std::string*)" where
    we were assigning a boolean value to the result.
    
    BUG=None
    TEST=Bluetooth error messages show in the log with the right error_message value instead of a \x01.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11669016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174541 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/bluetooth_adapter_client.cc
+++ chromeos/dbus/bluetooth_adapter_client.cc
@@ -641,7 +641,7 @@ class BluetoothAdapterClientImpl: public BluetoothAdapterClient,
     if (response) {
       dbus::MessageReader reader(response);
       error_name = response->GetErrorName();
-      error_message = reader.PopString(&error_message);
+      reader.PopString(&error_message);
     } else {
       error_name = kNoResponseError;
       error_message = "";
@@ -680,7 +680,7 @@ class BluetoothAdapterClientImpl: public BluetoothAdapterClient,
     if (response) {
       dbus::MessageReader reader(response);
       error_name = response->GetErrorName();
-      error_message = reader.PopString(&error_message);
+      reader.PopString(&error_message);
     } else {
       error_name = kNoResponseError;
       error_message = "";
