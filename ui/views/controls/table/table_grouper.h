c29042d69548678d167201ebd0fbb77c9cab6623
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-23 04:10:39 +0000

    Converts TableView from single selection to using ListSelectionModel. Also adds TableGrouper, which allows grouping cells (will replace GroupTableModel). Lastly adds TableViewRowBackgroundPainter, which may be used to paint the background of a row.
    Also, removes a couple of unused methods from TableViewObserver.
    
    BUG=none
    TEST=none
    R=sadrul@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11663007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174549 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/views/controls/table/table_grouper.h
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_CONTROLS_TABLE_TABLE_GROUPER_H_
+#define UI_VIEWS_CONTROLS_TABLE_TABLE_GROUPER_H_
+
+namespace views {
+
+struct GroupRange;
+
+// TableGrouper is used by TableView to group a set of rows and treat them
+// as one. Rows that fall in the same group are selected together and sorted
+// together.
+class TableGrouper {
+ public:
+  virtual void GetGroupRange(int model_index, GroupRange* range) = 0;
+
+ protected:
+  virtual ~TableGrouper() {}
+};
+
+}
+
+#endif  // UI_VIEWS_CONTROLS_TABLE_GROUP_TABLE_VIEW_H_
--- ui/views/controls/table/table_view_views.h
+++ ui/views/controls/table/table_view_views.h
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/memory/scoped_ptr.h"
+#include "ui/base/models/list_selection_model.h"
 #include "ui/base/models/table_model.h"
 #include "ui/base/models/table_model_observer.h"
 #include "ui/gfx/font.h"
@@ -31,8 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // sort by way of overriding TableModel::CompareValues().
 namespace views {
 
+struct GroupRange;
+class TableGrouper;
 class TableHeader;
 class TableViewObserver;
+class TableViewRowBackgroundPainter;
 class TableViewTestHelper;
 
 // The cells in the first column of a table can contain:
@@ -108,13 +113,21 @@ class VIEWS_EXPORT TableView
   void SetModel(ui::TableModel* model);
   ui::TableModel* model() const { return model_; }
 
-  // Returns a new ScrollPane that contains the receiver.
+  // Returns a new ScrollView that contains the receiver.
   View* CreateParentIfNecessary();
+
+  void SetRowBackgroundPainter(
+      scoped_ptr<TableViewRowBackgroundPainter> painter);
+
+  // Sets the TableGrouper. TableView does not own |grouper| (common use case is
+  // to have TableModel implement TableGrouper).
+  void SetGrouper(TableGrouper* grouper);
 
   // Returns the number of rows in the TableView.
   int RowCount() const;
 
   // Returns the number of selected rows.
+  // TODO(sky): remove this and force callers to use selection_model().
   int SelectedRowCount();
 
   // Selects the specified item, making sure it's visible.
@@ -123,10 +136,15 @@ class VIEWS_EXPORT TableView
   // Returns the first selected row in terms of the model.
   int FirstSelectedRow();
 
+  const ui::ListSelectionModel& selection_model() const {
+    return selection_model_;
+  }
+
   // Changes the visibility of the specified column (by id).
   void SetColumnVisibility(int id, bool is_visible);
   bool IsColumnVisible(int id) const;
 
+  // TODO(sky): rename to set_observer().
   void SetObserver(TableViewObserver* observer) {
     table_view_observer_ = observer;
   }
@@ -173,6 +191,7 @@ class VIEWS_EXPORT TableView
 
  private:
   friend class TableViewTestHelper;
+  struct GroupSortHelper;
   struct SortHelper;
 
   // Used during painting to determine the range of cells that need to be
@@ -187,6 +206,13 @@ class VIEWS_EXPORT TableView
     int max_row;
     int min_column;
     int max_column;
+  };
+
+  // Used by AdvanceSelection() to determine the direction to change the
+  // selection.
+  enum AdvanceDirection {
+    ADVANCE_DECREMENT,
+    ADVANCE_INCREMENT,
   };
 
   // Invoked when the number of rows changes in some way.
@@ -225,6 +251,9 @@ class VIEWS_EXPORT TableView
   // |canvas|.
   gfx::Rect GetPaintBounds(gfx::Canvas* canvas) const;
 
+  // Invokes SchedulePaint() for the selected rows.
+  void SchedulePaintForSelection();
+
   // Returns the index into |visible_columns_| to draw the icon at, or -1 if no
   // icon is to be drawn.
   int GetIconIndex();
@@ -234,6 +263,17 @@ class VIEWS_EXPORT TableView
 
   // Sets the selection to the specified index (in terms of the view).
   void SelectByViewIndex(int view_index);
+
+  // Sets the selection model to |new_selection|.
+  void SetSelectionModel(const ui::ListSelectionModel& new_selection);
+
+  // Advances the selection (from the active index) in the specified direction.
+  void AdvanceSelection(AdvanceDirection direction);
+
+  // Returns the range of the specified model index. If a TableGrouper has not
+  // been set this returns a group with a start of |model_index| and length of
+  // 1.
+  GroupRange GetGroupRange(int model_index) const;
 
   ui::TableModel* model_;
 
@@ -249,10 +289,11 @@ class VIEWS_EXPORT TableView
 
   const TableTypes table_type_;
 
+  // TODO(sky): rename to observer_.
   TableViewObserver* table_view_observer_;
 
-  // The selected row, in terms of the view.
-  int selected_row_;
+  // The selection, in terms of the model.
+  ui::ListSelectionModel selection_model_;
 
   gfx::Font font_;
 
@@ -268,6 +309,10 @@ class VIEWS_EXPORT TableView
   // Mappings used when sorted.
   std::vector<int> view_to_model_;
   std::vector<int> model_to_view_;
+
+  scoped_ptr<TableViewRowBackgroundPainter> row_background_painter_;
+
+  TableGrouper* grouper_;
 
   DISALLOW_COPY_AND_ASSIGN(TableView);
 };
