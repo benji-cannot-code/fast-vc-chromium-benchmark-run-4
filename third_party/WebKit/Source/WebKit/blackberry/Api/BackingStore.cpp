3d2066e6cb1f8d4372ad970e8615fe7d534e02e1
jpetsovits@rim.com <jpetsovits@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-23 17:52:41 +0000

    [BlackBerry] Specify tile usage enum for backingstore tiles. https://bugs.webkit.org/show_bug.cgi?id=105688 RIM PR 262120
    Reviewed by George Staikos.
    
    This enables us to have separate tile sizes for
    backingstore / root layer purposes and generic layers.
    
    * Api/BackingStore.cpp:
    (BlackBerry::WebKit::BackingStorePrivate::tileSize):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138424 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/BackingStore.cpp
@@ -1889,7 +1889,7 @@ int BackingStorePrivate::tileHeight()
 
 Platform::IntSize BackingStorePrivate::tileSize()
 {
-    static Platform::IntSize tileSize = BlackBerry::Platform::Settings::instance()->tileSize();
+    static Platform::IntSize tileSize = Platform::Settings::instance()->tileSize(Platform::BackingStoreTileUsage);
     return tileSize;
 }
 
