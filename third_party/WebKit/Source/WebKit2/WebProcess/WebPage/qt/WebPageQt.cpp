089fa8b2e09fc85a9a86e53902caa6aa5ca4242f
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-26 01:56:48 +0000

    Fix Qt build.
    * WebProcess/WebPage/qt/WebPageQt.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138467 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/WebPageQt.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/WebPageQt.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PopupMenuClient.h"
 #include "WebEditorClient.h"
 #include "WebEvent.h"
+#include "WebPageGroupProxy.h"
 #include "WebPageProxyMessages.h"
 #include "WebPopupMenu.h"
 #include "WebProcess.h"
