2f834debf0078f69e99f9ae87a4fbae31998e231
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-27 13:11:55 +0000

    Remove PrefValueStore's dependency on PrefModelAssociator.
    COLLABORATOR=kaiwang@chromium.org
    BUG=155525
    
    
    Review URL: https://chromiumcodereview.appspot.com/11636018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174680 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/pref_value_store.h
+++ chrome/browser/prefs/pref_value_store.h
@@ -11,12 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/callback.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
 #include "base/prefs/pref_store.h"
 #include "base/values.h"
 
-class PrefModelAssociator;
 class PrefNotifier;
 class PrefStore;
 
@@ -29,6 +29,8 @@ class PrefStore;
 // be called on the UI thread.
 class PrefValueStore {
  public:
+  typedef base::Callback<void(const std::string&)> PrefChangedCallback;
+
   // In decreasing order of precedence:
   //   |managed_prefs| contains all preferences from mandatory policies.
   //   |extension_prefs| contains preference values set by extensions.
@@ -60,9 +62,11 @@ class PrefValueStore {
                                      PrefStore* default_prefs,
                                      PrefNotifier* pref_notifier);
 
-  // TODO(joi): Remove this completely; the part PrefModelAssociator
-  // needs can be handled simply as a PrefObserver.
-  void set_sync_associator(PrefModelAssociator* sync_associator);
+  // A PrefValueStore can have exactly one callback that is directly
+  // notified of preferences changing in the store. This does not
+  // filter through the PrefNotifier mechanism, which may not forward
+  // certain changes (e.g. unregistered prefs).
+  void set_callback(const PrefChangedCallback& callback);
 
   // Gets the value for the given preference name that has the specified value
   // type. Values stored in a PrefStore that have the matching |name| but
@@ -238,12 +242,10 @@ class PrefValueStore {
   // Keeps the PrefStore references in order of precedence.
   PrefStoreKeeper pref_stores_[PREF_STORE_TYPE_MAX + 1];
 
-  // The associator for syncing preferences.
-  PrefModelAssociator* pref_sync_associator_;
+  PrefChangedCallback pref_changed_callback_;
 
-  // Used for generating PREF_CHANGED and PREF_INITIALIZATION_COMPLETED
-  // notifications. This is a weak reference, since the notifier is owned by the
-  // corresponding PrefService.
+  // Used for generating notifications. This is a weak reference,
+  // since the notifier is owned by the corresponding PrefService.
   PrefNotifier* pref_notifier_;
 
   // A mapping of preference names to their registered types.
--- chrome/browser/prefs/pref_value_store_unittest.cc
+++ chrome/browser/prefs/pref_value_store_unittest.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/bind.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/prefs/pref_notifier.h"
 #include "base/prefs/testing_pref_store.h"
 #include "base/values.h"
-#include "chrome/browser/prefs/pref_model_associator.h"
 #include "chrome/browser/prefs/pref_value_store.h"
 #include "chrome/common/pref_names.h"
 #include "testing/gmock/include/gmock/gmock.h"
@@ -29,7 +29,7 @@ class MockPrefNotifier : public PrefNotifier {
 };
 
 // Allows to capture sync model associator interaction.
-class MockPrefModelAssociator : public PrefModelAssociator {
+class MockPrefModelAssociator {
  public:
   MOCK_METHOD1(ProcessPrefChange, void(const std::string&));
 };
@@ -109,7 +109,9 @@ class PrefValueStoreTest : public testing::Test {
         default_pref_store_,
         &pref_notifier_));
 
-    pref_value_store_->set_sync_associator(sync_associator_.get());
+    pref_value_store_->set_callback(
+        base::Bind(&MockPrefModelAssociator::ProcessPrefChange,
+                   base::Unretained(sync_associator_.get())));
   }
 
   void CreateManagedPrefs() {
