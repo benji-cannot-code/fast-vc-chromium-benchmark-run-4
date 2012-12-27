66b4b7db94829a2fd17046b45c99a5857c8af572
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-27 22:30:39 +0000

    Remove ScopedAllowIO from font_fallback_win.cc.
    Reading from the registry is no longer disallowed on the UI
    thread after http://crrev.com/146894.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/11678010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174703 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/font_fallback_win.cc
+++ ui/gfx/font_fallback_win.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/string_split.h"
 #include "base/string_util.h"
-#include "base/threading/thread_restrictions.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/registry.h"
 #include "ui/gfx/font.h"
@@ -64,8 +63,6 @@ void AppendFont(const std::string& name, int size, std::vector<Font>* fonts) {
 void QueryLinkedFontsFromRegistry(const Font& font,
                                   std::map<std::string, std::string>* font_map,
                                   std::vector<Font>* linked_fonts) {
-  // TODO(asvitkine): Find a way to do this on a non-UI thread.
-  base::ThreadRestrictions::ScopedAllowIO allow_io;
   const wchar_t* kSystemLink =
       L"Software\\Microsoft\\Windows NT\\CurrentVersion\\FontLink\\SystemLink";
 
