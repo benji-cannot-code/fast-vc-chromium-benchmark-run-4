5364b1b55f9259599495167838e26a31c762bf46
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-27 16:29:05 +0000

    DevTools: move cookie permissions management from DevtoolsManager to RenderViewDevToolsAgentHost.
    BUG=166824
    
    Review URL: https://codereview.chromium.org/11688004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174684 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/render_view_devtools_agent_host.cc
+++ content/browser/devtools/render_view_devtools_agent_host.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base64.h"
 #include "base/basictypes.h"
 #include "base/lazy_instance.h"
+#include "content/browser/child_process_security_policy_impl.h"
 #include "content/browser/devtools/devtools_manager_impl.h"
 #include "content/browser/devtools/render_view_devtools_agent_host.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
@@ -82,6 +83,7 @@ bool DevToolsAgentHost::HasFor(RenderViewHost* rvh) {
   return FindAgentHost(rvh) != NULL;
 }
 
+// static
 bool DevToolsAgentHost::IsDebuggerAttached(WebContents* web_contents) {
   if (g_instances == NULL)
     return false;
@@ -150,6 +152,12 @@ void RenderViewDevToolsAgentHost::SendMessageToAgent(IPC::Message* msg) {
 }
 
 void RenderViewDevToolsAgentHost::NotifyClientAttaching() {
+  if (!render_view_host_)
+    return;
+
+  ChildProcessSecurityPolicyImpl::GetInstance()->GrantReadRawCookies(
+      render_view_host_->GetProcess()->GetID());
+
   NotificationService::current()->Notify(
       NOTIFICATION_DEVTOOLS_AGENT_ATTACHED,
       Source<BrowserContext>(
@@ -159,6 +167,27 @@ void RenderViewDevToolsAgentHost::NotifyClientAttaching() {
 }
 
 void RenderViewDevToolsAgentHost::NotifyClientDetaching() {
+  if (!render_view_host_)
+    return;
+
+  DevToolsManager* devtools_manager = DevToolsManager::GetInstance();
+  bool process_has_agents = false;
+  RenderProcessHost* render_process_host = render_view_host_->GetProcess();
+  for (Instances::iterator it = g_instances.Get().begin();
+       it != g_instances.Get().end(); ++it) {
+    if (*it == this || !devtools_manager->GetDevToolsClientHostFor(*it))
+      continue;
+    RenderViewHost* rvh = (*it)->render_view_host();
+    if (rvh && rvh->GetProcess() == render_process_host)
+      process_has_agents = true;
+  }
+
+  // We are the last to disconnect from the renderer -> revoke permissions.
+  if (!process_has_agents) {
+    ChildProcessSecurityPolicyImpl::GetInstance()->RevokeReadRawCookies(
+        render_process_host->GetID());
+  }
+
   NotificationService::current()->Notify(
       NOTIFICATION_DEVTOOLS_AGENT_DETACHED,
       Source<BrowserContext>(
