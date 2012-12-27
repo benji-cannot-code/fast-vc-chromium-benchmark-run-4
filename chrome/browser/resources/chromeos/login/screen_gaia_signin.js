05006a0d584cb86df3cb9f7232c5b1eabfd88876
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-27 09:21:25 +0000

    Fixed ErrorScreenHandler::ScheduleGaiaFrameReload()
    BUG=123579
    TEST=Manual tests on Linux ChromeOS
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11578070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174670 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screen_gaia_signin.js
@@ -481,6 +481,10 @@ cr.define('login', function() {
     $('gaia-signin').doReload();
   };
 
+  GaiaSigninScreen.scheduleRetry = function() {
+    $('gaia-signin').scheduleRetry();
+  };
+
   /**
    * Handler for iframe's error notification coming from the outside.
    * For more info see C++ class 'SnifferObserver' which calls this method.
