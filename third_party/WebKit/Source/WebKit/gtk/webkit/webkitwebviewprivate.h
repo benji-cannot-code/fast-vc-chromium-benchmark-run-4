8d276b860543a55a811873a6fb1d80468eaa2ce4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-27 16:29:37 +0000

    [GTK] invalid use of incomplete type WebCore::ResourceResponse in webkitwebviewprivate.h https://bugs.webkit.org/show_bug.cgi?id=105720
    Patch by ChangSeok Oh <shivamidow@gmail.com> on 2012-12-27
    Reviewed by Martin Robinson.
    
    If the acceleration backend is clutter, then css_filter feature is disabled.
    So ResourceResponse.h could not be reached with only existing headers
    in webkitwebviewprivate.h. For this reason, I add ResourceResponse.h explicitly
    to fix a build failure using incomplete type 'WebCore::ResourceResponse()'.
    
    * webkit/webkitwebviewprivate.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138505 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebviewprivate.h
+++ third_party/WebKit/Source/WebKit/gtk/webkit/webkitwebviewprivate.h
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GtkDragAndDropHelper.h"
 #include "Page.h"
 #include "ResourceHandle.h"
+#include "ResourceResponse.h"
 #include "WebViewInputMethodFilter.h"
 #include "WidgetBackingStore.h"
 #include <webkit/webkitwebview.h>
