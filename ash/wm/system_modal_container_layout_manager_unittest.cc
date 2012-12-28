d6f3047fd3b3cd3eac480e691e27ce0beede60ae
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-28 20:14:46 +0000

    Search next activate window from the active root window. The new test was also tested with --views-corewm-use-focus-controller.
    BUG=167416
    TEST=covered by test.
    
    Review URL: https://chromiumcodereview.appspot.com/11697003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174737 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/system_modal_container_layout_manager_unittest.cc
+++ ash/wm/system_modal_container_layout_manager_unittest.cc
@@ -32,7 +32,8 @@ aura::Window* GetModalContainer() {
 }
 
 bool AllRootWindowsHaveModalBackgroundsForContainer(int container_id) {
-  std::vector<aura::Window*> containers = Shell::GetAllContainers(container_id);
+  std::vector<aura::Window*> containers =
+      Shell::GetContainersFromAllRootWindows(container_id, NULL);
   bool has_modal_screen = !containers.empty();
   for (std::vector<aura::Window*>::iterator iter = containers.begin();
        iter != containers.end(); ++iter) {
