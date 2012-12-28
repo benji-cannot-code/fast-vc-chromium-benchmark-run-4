09ca907c2bc956c2542d5a2f49955a27ab07f8a5
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-28 00:11:36 +0000

    Fix AwBrowserProcess.loadLibrary to only load
    AwBrowserProcess.loadLibrary should only load the library, initialization
    needs to be deferred until the start() method is called, as only that
    method deals with ensuring initialization happens on the main thread.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/11665023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174712 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserProcess.java
@@ -32,7 +32,7 @@ public abstract class AwBrowserProcess {
     public static void loadLibrary() {
         PathUtils.setPrivateDataDirectorySuffix(PRIVATE_DATA_DIRECTORY_SUFFIX);
         LibraryLoader.setLibraryToLoad(NATIVE_LIBRARY);
-        LibraryLoader.ensureInitialized();
+        LibraryLoader.loadNow();
     }
 
     /**
@@ -47,6 +47,7 @@ public abstract class AwBrowserProcess {
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
             public void run() {
+                LibraryLoader.ensureInitialized();
                 AndroidBrowserProcess.initContentViewProcess(context,
                         AndroidBrowserProcess.MAX_RENDERERS_SINGLE_PROCESS);
             }
