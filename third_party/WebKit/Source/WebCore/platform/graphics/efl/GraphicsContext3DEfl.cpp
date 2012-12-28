6096ad640572da0970d6a7939a95cd548b1270f6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2012-12-28 09:06:50 +0000

    [EFL][WebGL] Add GLES2 support in GraphicsContext3DEfl. https://bugs.webkit.org/show_bug.cgi?id=105805
    Patch by Kondapally Kalyan <kalyan.kondapally@intel.com> on 2012-12-28
    Reviewed by Kenneth Rohde Christiansen.
    
    This is in preparation for adding GLES2 support.
    This patch adds OPENGL_ES_2 checks in GraphicsContext3DEFl.
    With this patch, GraphicsContext3DPrivate uses the FramebufferBlit extension supported by Extensions3D.
    
    * platform/graphics/GraphicsContext3D.h:
    (GraphicsContext3D):
    * platform/graphics/efl/GraphicsContext3DEfl.cpp:
    (WebCore::GraphicsContext3D::GraphicsContext3D):
    * platform/graphics/efl/GraphicsContext3DPrivate.cpp:
    (GraphicsContext3DPrivate::copyToGraphicsSurface):
    * platform/graphics/opengl/GLDefs.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138526 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/efl/GraphicsContext3DEfl.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/efl/GraphicsContext3DEfl.cpp
@@ -24,18 +24,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(3D_GRAPHICS) || USE(ACCELERATED_COMPOSITING)
 
+#include "GLDefs.h"
 #include "GraphicsContext3DPrivate.h"
 #include "Image.h"
 #include "ImageSource.h"
 #include "NotImplemented.h"
-#include "OpenGLShims.h"
 #include "PlatformContextCairo.h"
-
-#if USE(OPENGL_ES_2)
-#include "Extensions3DOpenGLES.h"
-#else
-#include "Extensions3DOpenGL.h"
-#endif
 
 namespace WebCore {
 
@@ -107,7 +101,7 @@ GraphicsContext3D::GraphicsContext3D(GraphicsContext3D::Attributes attrs, HostWi
 #if USE(OPENGL_ES_2)
             if (m_attrs.depth)
                 glGenRenderbuffers(1, &m_depthBuffer);
-            if (m_context->m_attrs.stencil)
+            if (m_attrs.stencil)
                 glGenRenderbuffers(1, &m_stencilBuffer);
 #endif
             if (m_attrs.stencil || m_attrs.depth)
--- third_party/WebKit/Source/WebCore/platform/graphics/opengl/GLDefs.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/opengl/GLDefs.h
@@ -29,18 +29,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(ACCELERATED_COMPOSITING)
 
+#define GL_GLEXT_PROTOTYPES 1
+
 #if USE(OPENGL_ES_2)
+#include "Extensions3DOpenGLES.h"
 #include "OpenGLESShims.h"
 #include <GLES2/gl2.h>
 #include <GLES2/gl2ext.h>
 #else
+#include "Extensions3DOpenGL.h"
 #include "OpenGLShims.h"
 #include <GL/gl.h>
 #include <GL/glext.h>
-#endif
-
 #if USE(GLX)
 #include <GL/glx.h>
+#endif
 #endif
 
 #if USE(EGL)
