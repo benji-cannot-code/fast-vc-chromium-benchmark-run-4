daa815021d099bba67925ebdf30270341353f0e0
darin@chromium.org <darin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2012-12-30 22:52:00 +0000

    Make the result of GetLastError() available in crash dumps.
    BUG=160401
    TBR=eroman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/11707003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174779 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/resource_dispatcher.cc
+++ content/common/resource_dispatcher.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/bind.h"
 #include "base/compiler_specific.h"
+#include "base/debug/alias.h"
 #include "base/file_path.h"
 #include "base/message_loop.h"
 #include "base/metrics/histogram.h"
@@ -31,6 +32,14 @@ using webkit_glue::ResourceRequestBody;
 using webkit_glue::ResourceResponseInfo;
 
 namespace content {
+
+static void CrashOnMapFailure() {
+#if defined(OS_WIN)
+  DWORD last_err = GetLastError();
+  base::debug::Alias(&last_err);
+#endif
+  CHECK(false);
+}
 
 // Each resource request is assigned an ID scoped to this process.
 static int MakeRequestID() {
@@ -341,7 +350,10 @@ void ResourceDispatcher::OnSetDataBuffer(const IPC::Message& message,
       new base::SharedMemory(shm_handle, true));  // read only
 
   bool ok = request_info->buffer->Map(shm_size);
-  CHECK(ok);
+  if (!ok) {
+    CrashOnMapFailure();
+    return;
+  }
 
   request_info->buffer_size = shm_size;
 }
