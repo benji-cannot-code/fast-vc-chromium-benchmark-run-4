2c027eaf9aca5f8860c362990090e9fe171e1538
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-02 20:38:47 +0000

    Missing Alt+Search in keyboard overlay.
    Fix the shortcut string after crrev.com/170596.
    
    BUG=167554
    TEST=local
    
    
    Review URL: https://chromiumcodereview.appspot.com/11744003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174839 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/keyboard_overlay_data.js
+++ chrome/browser/resources/chromeos/keyboard_overlay_data.js
@@ -15853,7 +15853,7 @@ var keyboardOverlayData = {
     'right<>CTRL<>SHIFT': 'keyboardOverlaySelectWordAtATime',
     's<>ALT<>SHIFT': 'keyboardOverlayShowStatusMenu',
     's<>CTRL': 'keyboardOverlaySave',
-    'search<>ALT': 'keyboardOverlayToggleCapsLock',
+    'ALT<>SEARCH': 'keyboardOverlayToggleCapsLock',
     'space<>CTRL': 'keyboardOverlaySelectPreviousInputMethod',
     'space<>SHIFT': 'keyboardOverlayScrollUpOnePage',
     'switch window<>CTRL': 'keyboardOverlayTakeScreenshot',
