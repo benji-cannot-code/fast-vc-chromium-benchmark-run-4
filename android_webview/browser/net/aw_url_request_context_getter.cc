f2a510fa98238754230d2240830f9925083e4998
pauljensen@chromium.org <pauljensen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-02 17:36:07 +0000

    Convert AwRequestInerceptor to net::URLRequestJobFactory::ProtocolHandler
    BUG=161534
    
    
    Review URL: https://chromiumcodereview.appspot.com/11678008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174807 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/net/aw_url_request_context_getter.cc
+++ android_webview/browser/net/aw_url_request_context_getter.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/proxy/proxy_service.h"
 #include "net/url_request/data_protocol_handler.h"
 #include "net/url_request/file_protocol_handler.h"
+#include "net/url_request/protocol_intercept_job_factory.h"
 #include "net/url_request/url_request_context_builder.h"
 #include "net/url_request/url_request_context.h"
 
@@ -102,7 +103,6 @@ void AwURLRequestContextGetter::Init() {
   set_protocol = job_factory->SetProtocolHandler(
       chrome::kDataScheme, new net::DataProtocolHandler());
   DCHECK(set_protocol);
-  job_factory->AddInterceptor(new AwRequestInterceptor());
 
   // TODO(mnaganov): Fix URLRequestContextBuilder to use proper threads.
   net::HttpNetworkSession::Params network_session_params;
@@ -119,6 +119,10 @@ void AwURLRequestContextGetter::Init() {
 
   job_factory_ = CreateAndroidJobFactoryAndCookieMonster(
       url_request_context_.get(), job_factory.Pass());
+  job_factory_.reset(new net::ProtocolInterceptJobFactory(
+      job_factory_.Pass(),
+      scoped_ptr<net::URLRequestJobFactory::ProtocolHandler>(
+          new AwRequestInterceptor())));
   url_request_context_->set_job_factory(job_factory_.get());
 }
 
