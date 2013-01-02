ca587e6819d34649bec254a7dfa76cc60bf74f8a
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-02 23:51:15 +0000

    Remove vestigial ScopedAllowIO from SQLitePersistentCookieStore.
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/11672005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174890 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/sqlite_persistent_cookie_store.cc
+++ chrome/browser/net/sqlite_persistent_cookie_store.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/thread.h"
-#include "base/threading/thread_restrictions.h"
 #include "base/time.h"
 #include "chrome/browser/diagnostics/sqlite_diagnostics.h"
 #include "chrome/browser/net/clear_on_exit_policy.h"
@@ -547,10 +546,8 @@ bool SQLitePersistentCookieStore::Backend::InitializeDatabase() {
   }
 
   int64 db_size = 0;
-  if (file_util::GetFileSize(path_, &db_size)) {
-    base::ThreadRestrictions::ScopedAllowIO allow_io;
+  if (file_util::GetFileSize(path_, &db_size))
     UMA_HISTOGRAM_COUNTS("Cookie.DBSizeInKB", db_size / 1024 );
-  }
 
   db_.reset(new sql::Connection);
   db_->set_error_delegate(new KillDatabaseErrorDelegate(this));
