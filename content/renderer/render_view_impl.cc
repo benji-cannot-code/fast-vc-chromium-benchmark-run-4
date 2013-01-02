564dde1a945e2d8873115050bc6f34fd521d6b53
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-02 22:45:02 +0000

    chromeos: Update focus ring color in WebKit.
    This sets WebKit's focus ring color to #4d90fe;
    USE_DEFAULT_RENDER_THEME-defining builds like Chrome OS
    appear to have been previously using the default of #e59700
    from RenderThemeChromiumSkia.
    
    It also makes RenderViewImpl honor the caret blink interval
    for USE_DEFAULT_RENDER_THEME instead of just for GTK.
    
    BUG=153080
    TEST=manual: focus rings are blue instead of orange
    
    
    Review URL: https://chromiumcodereview.appspot.com/11673011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174881 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -164,6 +164,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebUserMediaClient.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebWindowFeatures.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/default/WebRenderTheme.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebCString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebDragData.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebGraphicsContext3D.h"
@@ -231,7 +232,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   * theming
 #include "ui/native_theme/native_theme_win.h"
 #elif defined(USE_X11)
-#include "third_party/WebKit/Source/WebKit/chromium/public/default/WebRenderTheme.h"
 #include "ui/native_theme/native_theme.h"
 #elif defined(OS_MACOSX)
 #include "skia/ext/skia_utils_mac.h"
@@ -5311,24 +5311,25 @@ void RenderViewImpl::OnSetRendererPrefs(
   double old_zoom_level = renderer_preferences_.default_zoom_level;
   renderer_preferences_ = renderer_prefs;
   UpdateFontRenderingFromRendererPrefs();
-#if defined(TOOLKIT_GTK)
+
+#if defined(USE_DEFAULT_RENDER_THEME) || defined(TOOLKIT_GTK)
   WebColorName name = WebKit::WebColorWebkitFocusRingColor;
   WebKit::setNamedColors(&name, &renderer_prefs.focus_ring_color, 1);
   WebKit::setCaretBlinkInterval(renderer_prefs.caret_blink_interval);
+#if defined(TOOLKIT_GTK)
   ui::NativeTheme::instance()->SetScrollbarColors(
       renderer_prefs.thumb_inactive_color,
       renderer_prefs.thumb_active_color,
       renderer_prefs.track_color);
-#endif
+#endif  // defined(TOOLKIT_GTK)
 
-#if defined(USE_DEFAULT_RENDER_THEME) || defined(TOOLKIT_GTK)
   if (webview()) {
 #if defined(TOOLKIT_GTK)
     webview()->setScrollbarColors(
         renderer_prefs.thumb_inactive_color,
         renderer_prefs.thumb_active_color,
         renderer_prefs.track_color);
-#endif
+#endif  // defined(TOOLKIT_GTK)
     webview()->setSelectionColors(
         renderer_prefs.active_selection_bg_color,
         renderer_prefs.active_selection_fg_color,
@@ -5336,7 +5337,7 @@ void RenderViewImpl::OnSetRendererPrefs(
         renderer_prefs.inactive_selection_fg_color);
     webview()->themeChanged();
   }
-#endif
+#endif  // defined(USE_DEFAULT_RENDER_THEME) || defined(TOOLKIT_GTK)
 
   // If the zoom level for this page matches the old zoom default, and this
   // is not a plugin, update the zoom level to match the new default.
