9aaeb8ea7aceb91c01720f2a260a99f64a6d6e30
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-02 19:28:37 +0000

    Don't include a header that has been removed in r138413.
    * WebProcess/win/WebProcessWin.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/win/WebProcessWin.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/win/WebProcessWin.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(CFNETWORK)
 #include <CFNetwork/CFURLCachePriv.h>
 #include <CFNetwork/CFURLProtocolPriv.h>
-#include <WebCore/CookieStorageCFNet.h>
 #include <WebKitSystemInterface/WebKitSystemInterface.h> 
 #include <wtf/RetainPtr.h>
 #endif
