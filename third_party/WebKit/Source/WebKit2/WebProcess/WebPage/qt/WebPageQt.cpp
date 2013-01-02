5f20e49836bf6c302ed5eced39b4996b972f6d16
caio.oliveira@openbossa.org <caio.oliveira@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-02 17:10:24 +0000

    [Qt] Use WebPageGroup to create user scripts instead of Qt-specific function in WebPage https://bugs.webkit.org/show_bug.cgi?id=105921
    Reviewed by Simon Hausmann.
    
    After r131281 the WebPageGroup.h (in UIProcess) exposes a way to add user scripts, so we can
    use this directly to implement the user script functionality in Qt. The previous solution was
    a Qt-port specific function in WebPage that can be removed now.
    
    * UIProcess/API/qt/qquickwebview.cpp:
    (QQuickWebViewPrivate::updateUserScripts): Use WebPageGroup functions to manipulate user scripts directly.
    * UIProcess/WebPageProxy.h:
    (WebPageProxy): Remove now unused function.
    * UIProcess/qt/WebPageProxyQt.cpp: Ditto.
    * WebProcess/WebPage/WebPage.messages.in: Ditto.
    * WebProcess/WebPage/qt/WebPageQt.cpp: Remove now unused function and headers.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/WebPageQt.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/qt/WebPageQt.cpp
@@ -32,18 +32,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PopupMenuClient.h"
 #include "WebEditorClient.h"
 #include "WebEvent.h"
-#include "WebPageGroupProxy.h"
 #include "WebPageProxyMessages.h"
 #include "WebPopupMenu.h"
 #include "WebProcess.h"
 #include <QClipboard>
 #include <QGuiApplication>
-#include <WebCore/DOMWrapperWorld.h>
 #include <WebCore/FocusController.h>
 #include <WebCore/Frame.h>
 #include <WebCore/KeyboardEvent.h>
 #include <WebCore/Page.h>
-#include <WebCore/PageGroup.h>
 #include <WebCore/PlatformKeyboardEvent.h>
 #include <WebCore/Range.h>
 #include <WebCore/Settings.h>
@@ -333,15 +330,6 @@ void WebPage::applicationSchemeReply(const QtNetworkReplyData& replyData)
     QtNetworkReply* networkReply = m_applicationSchemeReplies.take(replyData.m_replyUuid);
     networkReply->setReplyData(replyData);
     networkReply->finalize();
-}
-
-void WebPage::setUserScripts(const Vector<String>& scripts)
-{
-    // This works because we keep an unique page group for each Page.
-    PageGroup* pageGroup = PageGroup::pageGroup(this->pageGroup()->identifier());
-    pageGroup->removeUserScriptsFromWorld(mainThreadNormalWorld());
-    for (unsigned i = 0; i < scripts.size(); ++i)
-        pageGroup->addUserScriptToWorld(mainThreadNormalWorld(), scripts.at(i), KURL(), Vector<String>(), Vector<String>(), InjectAtDocumentEnd, InjectInTopFrameOnly);
 }
 
 void WebPage::selectedIndex(int32_t newIndex)
