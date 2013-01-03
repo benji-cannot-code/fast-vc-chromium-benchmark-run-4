d143cfc72461fbb5038a0a751c1f5fb3d1d23284
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-03 22:18:56 +0000

    Move the rest of the plugin code under chrome\browser to chrome\browser\plugins. Review URL: https://codereview.chromium.org/11761018
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175030 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugins/chrome_plugin_service_filter.h
+++ chrome/browser/plugins/chrome_plugin_service_filter.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROME_PLUGIN_SERVICE_FILTER_H_
-#define CHROME_BROWSER_CHROME_PLUGIN_SERVICE_FILTER_H_
+#ifndef CHROME_BROWSER_PLUGINS_CHROME_PLUGIN_SERVICE_FILTER_H_
+#define CHROME_BROWSER_PLUGINS_CHROME_PLUGIN_SERVICE_FILTER_H_
 
 #include <map>
 #include <vector>
@@ -91,4 +91,4 @@ class ChromePluginServiceFilter : public content::PluginServiceFilter,
   std::vector<OverriddenPlugin> overridden_plugins_;
 };
 
-#endif  // CHROME_BROWSER_CHROME_PLUGIN_SERVICE_FILTER_H_
+#endif  // CHROME_BROWSER_PLUGINS_CHROME_PLUGIN_SERVICE_FILTER_H_
--- chrome/browser/printing/print_preview_dialog_controller.cc
+++ chrome/browser/printing/print_preview_dialog_controller.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/chrome_plugin_service_filter.h"
+#include "chrome/browser/plugins/chrome_plugin_service_filter.h"
 #include "chrome/browser/printing/print_view_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
