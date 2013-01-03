e33fd4736d0caa2621dc63fb8ce1f2a64fec4711
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-03 02:21:35 +0000

    Fix races in ScopedMTPDeviceMapEntry, part 2.
    Review URL: https://chromiumcodereview.appspot.com/11665003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174917 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_gallery/scoped_mtp_device_map_entry.h
+++ chrome/browser/media_gallery/scoped_mtp_device_map_entry.h
@@ -32,6 +32,9 @@ class ScopedMTPDeviceMapEntry
   ScopedMTPDeviceMapEntry(const FilePath::StringType& device_location,
                           const base::Closure& on_destruction_callback);
 
+  // Most be called after creating the ScopedMTPDeviceMapEntry.
+  void Init();
+
  private:
   // Friend declarations for ref counted implementation.
   friend struct content::BrowserThread::DeleteOnThread<
