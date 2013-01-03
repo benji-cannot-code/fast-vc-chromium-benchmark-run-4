1069f23a9afd5d342f80034172db9843579b20d0
kamil.jiwa@gmail.com <kamil.jiwa@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-03 12:30:52 +0000

    Small fixes to allow unit tests to compile with gcc 4.7.x (tested with gcc 4.7.2)
    Review URL: https://chromiumcodereview.appspot.com/11568033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174956 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/sync_encryption_handler_impl_unittest.cc
+++ sync/internal_api/sync_encryption_handler_impl_unittest.cc
@@ -1312,7 +1312,6 @@ TEST_F(SyncEncryptionHandlerImplTest, ReceiveOldMigratedNigori) {
   sync_pb::EncryptedData encrypted;
   KeyParams old_key = {"localhost", "dummy", kOldKey};
   KeyParams cur_key = {"localhost", "dummy", kCurKey};
-  KeyParams keystore_key = {"localhost", "dummy", kKeystoreKey};
   GetCryptographer()->AddKey(old_key);
   GetCryptographer()->AddKey(cur_key);
 
@@ -1875,7 +1874,6 @@ TEST_F(SyncEncryptionHandlerImplTest,
   sync_pb::EncryptedData encrypted;
   KeyParams old_key = {"localhost", "dummy", kOldKey};
   KeyParams cur_key = {"localhost", "dummy", kCurKey};
-  KeyParams keystore_key = {"localhost", "dummy", kKeystoreKey};
   GetCryptographer()->AddKey(old_key);
   GetCryptographer()->AddKey(cur_key);
 
