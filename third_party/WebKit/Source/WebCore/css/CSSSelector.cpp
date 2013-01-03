64eec2c0e3e3e8c1f92b82048eb81c1886858c09
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-03 14:30:22 +0000

    The 'body' of seamless IFrames should default to 'margin: 0' https://bugs.webkit.org/show_bug.cgi?id=90834
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    Documents displayed in seamless iframes should override the 'body'
    element's normal 8px default margins with 0px, in order to ensure that
    contents are displayed flush with the boundries of the seamless box.[1]
    
    This patch does so by adding a new pseudoclass to allow the user agent
    stylesheet to specify a different margin for seamless documents.
    
    [1]: http://lists.w3.org/Archives/Public/public-whatwg-archive/2012Jul/0039.html
    
    Test: fast/frames/seamless/seamless-body-margin.html
    
    * css/CSSSelector.cpp:
    (WebCore::CSSSelector::pseudoId):
    (WebCore::nameToPseudoTypeMap):
    (WebCore::CSSSelector::extractPseudoType):
    * css/CSSSelector.h:
    * css/SelectorChecker.cpp:
    (WebCore::SelectorChecker::checkOneSelector):
    * css/html.css:
    (body:-webkit-seamless-document):
    * rendering/style/RenderStyleConstants.h:
        Define a new pseudoclass: '-webkit-seamless-document', which
        matches for any element in a document that's rendered in a
        seamless IFrame.
    
    LayoutTests:
    
    * fast/frames/seamless/seamless-body-margin-expected.txt: Added.
    * fast/frames/seamless/seamless-body-margin.html: Added.
        New test to explicitly check 'body' margins.
    * fast/frames/seamless/resources/nested-seamless.html:
    * fast/frames/seamless/resources/quirks-square.html:
    * fast/frames/seamless/resources/two-inline-blocks.html:
    * fast/frames/seamless/seamless-inherited-origin-expected.txt:
    * fast/frames/seamless/seamless-inherited-origin.html:
        Updated these tests to remove the assumption that the body had an
        8px margin.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSSelector.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSSelector.cpp
@@ -224,6 +224,9 @@ PseudoId CSSSelector::pseudoId(PseudoType type)
 #if ENABLE(VIDEO_TRACK)
     case PseudoCue:
 #endif
+#if ENABLE(IFRAME_SEAMLESS)
+    case PseudoSeamlessDocument:
+#endif
         return NOPSEUDO;
     case PseudoNotParsed:
         ASSERT_NOT_REACHED();
@@ -309,6 +312,9 @@ static HashMap<AtomicStringImpl*, CSSSelector::PseudoType>* nameToPseudoTypeMap(
 #if ENABLE(VIDEO_TRACK)
     DEFINE_STATIC_LOCAL(AtomicString, cue, ("cue(", AtomicString::ConstructFromLiteral));
 #endif
+#if ENABLE(IFRAME_SEAMLESS)
+    DEFINE_STATIC_LOCAL(AtomicString, seamlessDocument, ("-webkit-seamless-document", AtomicString::ConstructFromLiteral));
+#endif
     DEFINE_STATIC_LOCAL(AtomicString, inRange, ("in-range", AtomicString::ConstructFromLiteral));
     DEFINE_STATIC_LOCAL(AtomicString, outOfRange, ("out-of-range", AtomicString::ConstructFromLiteral));
 
@@ -386,6 +392,9 @@ static HashMap<AtomicStringImpl*, CSSSelector::PseudoType>* nameToPseudoTypeMap(
 #endif
 #if ENABLE(VIDEO_TRACK)
         nameToPseudoType->set(cue.impl(), CSSSelector::PseudoCue);
+#endif
+#if ENABLE(IFRAME_SEAMLESS)
+        nameToPseudoType->set(seamlessDocument.impl(), CSSSelector::PseudoSeamlessDocument);
 #endif
         nameToPseudoType->set(inRange.impl(), CSSSelector::PseudoInRange);
         nameToPseudoType->set(outOfRange.impl(), CSSSelector::PseudoOutOfRange);
@@ -505,6 +514,9 @@ void CSSSelector::extractPseudoType() const
     case PseudoFullScreenDocument:
     case PseudoFullScreenAncestor:
     case PseudoAnimatingFullScreenTransition:
+#endif
+#if ENABLE(IFRAME_SEAMLESS)
+    case PseudoSeamlessDocument:
 #endif
     case PseudoInRange:
     case PseudoOutOfRange:
