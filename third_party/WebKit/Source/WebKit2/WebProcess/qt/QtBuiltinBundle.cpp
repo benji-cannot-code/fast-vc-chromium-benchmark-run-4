e63dfd9ba71a7bf24c4fd04878714ec78aa82ab0
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-03 23:04:09 +0000

    [Qt] Unreviewed debug buildfix after r138723.
    * WebProcess/qt/QtBuiltinBundle.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/qt/QtBuiltinBundle.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/qt/QtBuiltinBundle.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKNumber.h"
 #include "WKString.h"
 #include "WKStringQt.h"
+#include "WKType.h"
 #include <wtf/PassOwnPtr.h>
 
 namespace WebKit {
