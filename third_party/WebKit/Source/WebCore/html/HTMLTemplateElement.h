8821f9994806631bd9087ede290cced3eec65a91
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-03 23:41:16 +0000

    [HTMLTemplateElement] When adopting a template element, also adopt its content into the appropriate document https://bugs.webkit.org/show_bug.cgi?id=106039
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Implements the approach discussed in the spec bug:
    https://www.w3.org/Bugs/Public/show_bug.cgi?id=20129
    
    Test: fast/dom/HTMLTemplateElement/ownerDocument-adoptNode.html
    
    * html/HTMLTemplateElement.cpp:
    (WebCore::HTMLTemplateElement::didMoveToNewDocument):
    * html/HTMLTemplateElement.h:
    (HTMLTemplateElement):
    
    LayoutTests:
    
    * fast/dom/HTMLTemplateElement/ownerDocument-adoptNode-expected.txt: Added.
    * fast/dom/HTMLTemplateElement/ownerDocument-adoptNode.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLTemplateElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLTemplateElement.h
@@ -48,6 +48,7 @@ public:
 
 private:
     virtual PassRefPtr<Node> cloneNode(bool deep) OVERRIDE;
+    virtual void didMoveToNewDocument(Document* oldDocument) OVERRIDE;
 
     HTMLTemplateElement(const QualifiedName&, Document*);
 
