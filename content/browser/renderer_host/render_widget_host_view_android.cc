4dad0da1be26cfba83f9562d9da1db4291e8fa90
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-03 20:57:56 +0000

    Switch GetScaledContentTexture to scale from Size
    Using Size caused issues because we occasionally had the wrong Size, and we'd stretch
    the content texture to fit.  Now it scales it and returns the Size of the texture in
    an out parameter.
    
    BUG=166998
    
    
    Review URL: https://chromiumcodereview.appspot.com/11636057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175009 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/Platform/chromium/public/WebExternalTextureLayer.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebSize.h"
 #include "ui/gfx/android/java_bitmap.h"
+#include "ui/gfx/size_conversions.h"
 #include "webkit/compositor_bindings/web_compositor_support_impl.h"
 
 namespace content {
@@ -131,11 +132,23 @@ void RenderWidgetHostViewAndroid::SetBounds(const gfx::Rect& rect) {
 }
 
 WebKit::WebGLId RenderWidgetHostViewAndroid::GetScaledContentTexture(
-    const gfx::Size& size) {
+    float scale,
+    gfx::Size* out_size) {
+  gfx::Size size(gfx::ToCeiledSize(
+      gfx::ScaleSize(texture_size_in_layer_, scale)));
+
   if (!CompositorImpl::IsInitialized() ||
       texture_id_in_layer_ == 0 ||
-      texture_size_in_layer_.IsEmpty())
+      texture_size_in_layer_.IsEmpty() ||
+      size.IsEmpty()) {
+    if (out_size)
+        out_size->SetSize(0, 0);
+
     return 0;
+  }
+
+  if (out_size)
+    *out_size = size;
 
   GLHelper* helper = ImageTransportFactoryAndroid::GetInstance()->GetGLHelper();
   return helper->CopyAndScaleTexture(texture_id_in_layer_,
