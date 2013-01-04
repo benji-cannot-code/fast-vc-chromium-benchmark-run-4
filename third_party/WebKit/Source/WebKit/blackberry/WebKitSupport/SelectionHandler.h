58a06bc89a61f93bb8e24bc900790adf21553613
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-04 21:25:38 +0000

    [BlackBerry]  FCC will show up as you type if you tap on the same input bar twice before typing https://bugs.webkit.org/show_bug.cgi?id=106116
    Patch by Otto Derek Cheung <otcheung@rim.com> on 2013-01-04
    Reviewed by Yong Li.
    Internal reviewed by Gen Mak.
    PR 267177
    
    FCC is being invoked because the fatfinger result is no longer a reliable source
    to tell whether the caret change is caused by user touch input. Instead, we recognize all
    notifyCaretChange calls with userTouchInput as true except when caretPositionChanged is invoked
    by SelectionHandler:inputHandlerDidFinishProcessingChange.
    
    * WebKitSupport/SelectionHandler.cpp:
    (BlackBerry::WebKit::SelectionHandler::inputHandlerDidFinishProcessingChange):
    (BlackBerry::WebKit::SelectionHandler::notifyCaretPositionChangedIfNeeded):
    (BlackBerry::WebKit::SelectionHandler::caretPositionChanged):
    * WebKitSupport/SelectionHandler.h:
    (SelectionHandler):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138848 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/SelectionHandler.h
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/SelectionHandler.h
@@ -77,8 +77,8 @@ public:
     void inputHandlerDidFinishProcessingChange();
 
 private:
-    void notifyCaretPositionChangedIfNeeded();
-    void caretPositionChanged();
+    void notifyCaretPositionChangedIfNeeded(bool userTouchTriggered = true);
+    void caretPositionChanged(bool userTouchTriggered);
     void regionForTextQuads(WTF::Vector<WebCore::FloatQuad>&, BlackBerry::Platform::IntRectRegion&, bool shouldClipToVisibleContent = true) const;
     WebCore::IntRect clippingRectForVisibleContent() const;
     bool updateOrHandleInputSelection(WebCore::VisibleSelection& newSelection, const WebCore::IntPoint& relativeStart
