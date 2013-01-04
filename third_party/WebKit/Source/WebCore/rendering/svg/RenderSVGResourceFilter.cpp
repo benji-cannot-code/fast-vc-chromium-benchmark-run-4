eb3adc69f3b30d7214527df81d2cc36578fd70c4
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-04 20:40:25 +0000

    Invalidation of some SVG filter attributes on HTML content doesn't work https://bugs.webkit.org/show_bug.cgi?id=105635
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    Since HTML elements with filters are only added to m_clientLayers, not
    m_clients, and do not use the FilterInfo used by SVG filters on SVG
    elements, they must be invalidated separately.  Some filter attribute
    invalidations call markAllClientsForInvalidation(), but some call
    markClientForInvalidation() directly.  These invaldations must also
    invalidate m_clientLayers.
    
    Test: css3/filters/reference-filter-update-on-attribute-change.html
    
    * rendering/svg/RenderSVGResourceContainer.cpp:
    (WebCore::RenderSVGResourceContainer::markAllClientsForInvalidation):
    (WebCore::RenderSVGResourceContainer::markAllClientLayersForInvalidation):
    * rendering/svg/RenderSVGResourceContainer.h:
    Refactor client layer invalidation out into its own function.
    * rendering/svg/RenderSVGResourceFilter.cpp:
    (WebCore::RenderSVGResourceFilter::primitiveAttributeChanged):
    Mark all client layers for invalidation when a filter primitve attribute is
    changed.
    
    LayoutTests:
    
    New reftest to verify filter attribute change invalidations.
    
    * css3/filters/reference-filter-update-on-attribute-change-expected.html: Added.
    * css3/filters/reference-filter-update-on-attribute-change.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGResourceFilter.cpp
+++ third_party/WebKit/Source/WebCore/rendering/svg/RenderSVGResourceFilter.cpp
@@ -362,6 +362,7 @@ void RenderSVGResourceFilter::primitiveAttributeChanged(RenderObject* object, co
         // Repaint the image on the screen.
         markClientForInvalidation(it->key, RepaintInvalidation);
     }
+    markAllClientLayersForInvalidation();
 }
 
 }
