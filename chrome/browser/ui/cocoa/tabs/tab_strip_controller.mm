benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 6ee2e3ba93c61083cd902d22b76801e0d8d75b0f

mac: Tile background image of ntp button.
In some themes, the tab background bitmap is less wide than the new tab button,
so tile it to cover the whole button. NSDrawThreePartImage() does not support
a source rectangle, so rely on clipping to show only the top bits of the
background bitmap.

Also, only use a separate inactive window bitmap when using the default theme,
to match the tab look.

BUG=167798,52468
TEST=Install https://chrome.google.com/webstore/detail/slinky-elegant/bmanlajnpdncmhfkiccmbgeocgbncfln?hl=en
Look at new tab button in active and inactive windows. Also check the new tab
button with the default theme.

Review URL: https://codereview.chromium.org/11716003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@174778 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -197,11 +197,13 @@ NSImage* ApplyMask(NSImage* image, NSImage* mask) {
       const int kYOffset = 10;
       CGFloat width = size.width;
       CGFloat height = size.height;
-      [image drawAtPoint:NSZeroPoint
-                fromRect:NSMakeRect(0, [image size].height - height - kYOffset,
-                                    width, height)
-               operation:NSCompositeCopy
-                fraction:1.0];
+      // In some themes, the tab background image is narrower than the
+      // new tab button, so tile the background image.
+      NSDrawThreePartImage(
+          NSMakeRect(0, -([image size].height - height - kYOffset),
+                     width, [image size].height),
+          nil, image, nil, /*vertical=*/NO, NSCompositeCopy,
+          1.0, /*flipped=*/NO);
       [mask drawAtPoint:NSZeroPoint
                fromRect:NSMakeRect(0, 0, width, height)
               operation:NSCompositeDestinationIn
@@ -2178,7 +2180,8 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay) {
 }
 
 - (void)setNewTabImages {
-  ui::ThemeProvider* theme = [[tabStripView_ window] themeProvider];
+  ThemeService *theme =
+      static_cast<ThemeService*>([[tabStripView_ window] themeProvider]);
   if (!theme)
     return;
 
@@ -2190,8 +2193,6 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay) {
 
   NSImage* foreground = ApplyMask(
       theme->GetNSImageNamed(IDR_THEME_TAB_BACKGROUND, true), mask);
-  NSImage* background = ApplyMask(
-      theme->GetNSImageNamed(IDR_THEME_TAB_BACKGROUND_INACTIVE, true), mask);
 
   [[newTabButton_ cell] setImage:Overlay(foreground, normal)
                   forButtonState:image_button_cell::kDefaultState];
@@ -2199,10 +2200,21 @@ NSImage* Overlay(NSImage* ground, NSImage* overlay) {
                   forButtonState:image_button_cell::kHoverState];
   [[newTabButton_ cell] setImage:Overlay(foreground, pressed)
                     forButtonState:image_button_cell::kPressedState];
-  [[newTabButton_ cell] setImage:Overlay(background, normal)
-                  forButtonState:image_button_cell::kDefaultStateBackground];
-  [[newTabButton_ cell] setImage:Overlay(background, hover)
-                  forButtonState:image_button_cell::kHoverStateBackground];
+
+  // IDR_THEME_TAB_BACKGROUND_INACTIVE is only used with the default theme.
+  if (theme->UsingDefaultTheme()) {
+    NSImage* background = ApplyMask(
+        theme->GetNSImageNamed(IDR_THEME_TAB_BACKGROUND_INACTIVE, true), mask);
+    [[newTabButton_ cell] setImage:Overlay(background, normal)
+                    forButtonState:image_button_cell::kDefaultStateBackground];
+    [[newTabButton_ cell] setImage:Overlay(background, hover)
+                    forButtonState:image_button_cell::kHoverStateBackground];
+  } else {
+    [[newTabButton_ cell] setImage:nil
+                    forButtonState:image_button_cell::kDefaultStateBackground];
+    [[newTabButton_ cell] setImage:nil
+                    forButtonState:image_button_cell::kHoverStateBackground];
+  }
 }
 
 @end

commit 05ea76b2bf975aa037633d5e7d263ce30984c2e7

mac: Attempt to fix a startup perf regression caused by r174778
NOTRY=true
BUG=167798,168084
TEST=Install https://chrome.google.com/webstore/detail/slinky-elegant/bmanlajnpdncmhfkiccmbgeocgbncfln?hl=en
Look at new tab button in active and inactive windows. Also check the new tab
button with the default theme. Look at startup perf graphs, should improve by
~1.8ms.


Review URL: https://chromiumcodereview.appspot.com/11748026

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175108 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/tabs/tab_strip_controller.mm
@@ -197,13 +197,25 @@ NSImage* ApplyMask(NSImage* image, NSImage* mask) {
       const int kYOffset = 10;
       CGFloat width = size.width;
       CGFloat height = size.height;
+
       // In some themes, the tab background image is narrower than the
       // new tab button, so tile the background image.
-      NSDrawThreePartImage(
-          NSMakeRect(0, -([image size].height - height - kYOffset),
-                     width, [image size].height),
-          nil, image, nil, /*vertical=*/NO, NSCompositeCopy,
-          1.0, /*flipped=*/NO);
+      CGFloat x = 0;
+      // The floor() is to make sure images with odd widths don't draw to the
+      // same pixel twice on retina displays. (Using NSDrawThreePartImage()
+      // caused a startup perf regression, so that cannot be used.)
+      CGFloat tileWidth = floor(std::min(width, [image size].width));
+      while (x < width) {
+        [image drawAtPoint:NSMakePoint(x, 0)
+                  fromRect:NSMakeRect(0,
+                                      [image size].height - height - kYOffset,
+                                      tileWidth,
+                                      height)
+                 operation:NSCompositeCopy
+                  fraction:1.0];
+        x += tileWidth;
+      }
+
       [mask drawAtPoint:NSZeroPoint
                fromRect:NSMakeRect(0, 0, width, height)
               operation:NSCompositeDestinationIn