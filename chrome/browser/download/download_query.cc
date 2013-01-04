050f6e356ad5db6695098ef6a05d9a6f9089170f
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-04 00:09:50 +0000

    Fix times in chrome.downloads extension API to use ISO8601 instead of int32s, which are too small. Also implement DownloadItem.endTime, DownloadQuery.endTime/endedBefore/endedAfter, DownloadQuery.orderBy='endTime'. BUG=12133
    Review URL: https://chromiumcodereview.appspot.com/11593010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175062 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/download_query.cc
+++ chrome/browser/download/download_query.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stl_util.h"
 #include "base/string16.h"
 #include "base/string_split.h"
+#include "base/stringprintf.h"
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
@@ -84,10 +85,25 @@ static int64 GetStartTimeMsEpoch(const DownloadItem& item) {
   return (item.GetStartTime() - base::Time::UnixEpoch()).InMilliseconds();
 }
 
-// TODO(benjhayden) These timestamps don't fit in int32, and base::Value doesn't
-// support int64. Use ISO 8601 date-time strings instead.
-static int GetStartTime(const DownloadItem& item) {
-  return (item.GetStartTime() - base::Time::UnixEpoch()).InMilliseconds();
+static int64 GetEndTimeMsEpoch(const DownloadItem& item) {
+  return (item.GetEndTime() - base::Time::UnixEpoch()).InMilliseconds();
+}
+
+std::string TimeToISO8601(const base::Time& t) {
+  base::Time::Exploded exploded;
+  t.UTCExplode(&exploded);
+  return base::StringPrintf(
+      "%04d-%02d-%02dT%02d:%02d:%02d.%03dZ", exploded.year, exploded.month,
+      exploded.day_of_month, exploded.hour, exploded.minute, exploded.second,
+      exploded.millisecond);
+}
+
+static std::string GetStartTime(const DownloadItem& item) {
+  return TimeToISO8601(item.GetStartTime());
+}
+
+static std::string GetEndTime(const DownloadItem& item) {
+  return TimeToISO8601(item.GetEndTime());
 }
 
 static bool GetDangerAccepted(const DownloadItem& item) {
@@ -253,12 +269,18 @@ bool DownloadQuery::AddFilter(DownloadQuery::FilterType type,
       return GetAs(value, &query) &&
              AddFilter(base::Bind(&MatchesQuery, query));
     }
+    case FILTER_ENDED_AFTER:
+      return AddFilter(BuildFilter<std::string>(value, GT, &GetEndTime));
+    case FILTER_ENDED_BEFORE:
+      return AddFilter(BuildFilter<std::string>(value, LT, &GetEndTime));
+    case FILTER_END_TIME:
+      return AddFilter(BuildFilter<std::string>(value, EQ, &GetEndTime));
     case FILTER_STARTED_AFTER:
-      return AddFilter(BuildFilter<int>(value, GT, &GetStartTime));
+      return AddFilter(BuildFilter<std::string>(value, GT, &GetStartTime));
     case FILTER_STARTED_BEFORE:
-      return AddFilter(BuildFilter<int>(value, LT, &GetStartTime));
+      return AddFilter(BuildFilter<std::string>(value, LT, &GetStartTime));
     case FILTER_START_TIME:
-      return AddFilter(BuildFilter<int>(value, EQ, &GetStartTime));
+      return AddFilter(BuildFilter<std::string>(value, EQ, &GetStartTime));
     case FILTER_TOTAL_BYTES:
       return AddFilter(BuildFilter<int>(value, EQ, &GetTotalBytes));
     case FILTER_TOTAL_BYTES_GREATER:
@@ -348,6 +370,9 @@ bool DownloadQuery::DownloadComparator::operator() (
 void DownloadQuery::AddSorter(DownloadQuery::SortType type,
                               DownloadQuery::SortDirection direction) {
   switch (type) {
+    case SORT_END_TIME:
+      sorters_.push_back(Sorter::Build<int64>(direction, &GetEndTimeMsEpoch));
+      break;
     case SORT_START_TIME:
       sorters_.push_back(Sorter::Build<int64>(direction, &GetStartTimeMsEpoch));
       break;
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -952,7 +952,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/do_not_track_browsertest.cc',
         'browser/download/download_browsertest.cc',
         'browser/download/download_danger_prompt_browsertest.cc',
-        'browser/download/download_query_unittest.cc',
         'browser/download/save_page_browsertest.cc',
         'browser/errorpage_browsertest.cc',
         'browser/extensions/active_tab_apitest.cc',
