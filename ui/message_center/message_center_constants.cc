c1fdcc536ae488777745f8cbd6ffe2449a80412b
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-04 12:17:38 +0000

    Set a display limit of 8 on multiple-item notifications items.
    This limit is defined in the notification product requirement document,
    section "Inbox Format for Multiple Messages". The limit only affects
    the number of items displayed (to avoid notifications growing too tall).
    The back end still handles any number of items. The maximum number is
    declared in the public message_center_constants.h file so future code
    can use it, for example to output a warning or return an error if
    callers to the chrome.experimental.notification API ask for a
    notification with more than the maximum number of displayable items.
    
    BUG=161101
    
    
    Review URL: https://chromiumcodereview.appspot.com/11638035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175132 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center_constants.cc
+++ ui/message_center/message_center_constants.cc
@@ -8,5 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace message_center {
 
 const int kNotificationIconWidth = 80;
+const size_t kNotificationMaxItems = 8;
 
 }  // namespace message_center
--- ui/message_center/message_center_constants.h
+++ ui/message_center/message_center_constants.h
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_MESSAGE_CENTER_MESSAGE_CENTER_CONSTANTS_H_
 #define UI_MESSAGE_CENTER_MESSAGE_CENTER_CONSTANTS_H_
 
+#include "base/basictypes.h"
 #include "ui/message_center/message_center_export.h"
 
 namespace message_center {
 
 MESSAGE_CENTER_EXPORT extern const int kNotificationIconWidth;
+extern const size_t kNotificationMaxItems;
 
 }  // namespace message_center
 
