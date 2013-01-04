e5fb16df4e8903e1d16871e870d44233d476c690
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-04 20:05:24 +0000

    FlakyTestReporter should be re-enabled and taught how to post patches https://bugs.webkit.org/show_bug.cgi?id=103839
    Reviewed by Adam Barth.
    
    This moves LayoutTestResultsReader off of the DeprecatedPort object.
    It also adds a real Port object to the EWS and CommitQueue objects
    using a new PatchProcessingQueue superclass.
    PatchProcessingQueue doesn't seem to be the cleanest insertion point
    but works for this purpose.
    I also moved _upload_results_archive_for_patch into PatchProcessingQueue
    to resolve a FIXME.
    
    This required some hacks/shiming between the non-specific "old"
    port names which the EWS system is currently using, and the new
    port objects.  This shimming is done in PatchProcessingQueue._new_port_name_from_old.
    
    Hopefully this will fix the flaky test reporter and results uploading.
    Certainly it's another small step towards getting rid of the old ports.py
    infrastructure and making the NRWT Port class common webkitpy infrastructure.
    
    Now that DeprecatedPort is no longer used for the results directory
    I removed all the explicit results-dir code which webkit-patch was using
    in --non-interactive mode to tell NRWT to use a results-dir it understood. :)
    
    * Scripts/webkitpy/common/config/ports.py:
    (DeprecatedPort.run_perl_unittests_command):
    * Scripts/webkitpy/common/config/ports_mock.py:
    (MockPort.name):
    * Scripts/webkitpy/common/system/outputcapture.py:
    (OutputCapture.assert_outputs):
    * Scripts/webkitpy/layout_tests/port/apple.py:
    (ApplePort.determine_full_port_name):
    * Scripts/webkitpy/tool/bot/layouttestresultsreader.py:
    (LayoutTestResultsReader.__init__):
    (LayoutTestResultsReader):
    (LayoutTestResultsReader._read_file_contents):
    (LayoutTestResultsReader._create_layout_test_results):
    (LayoutTestResultsReader._create_unit_test_results):
    (LayoutTestResultsReader.results):
    (LayoutTestResultsReader.archive):
    * Scripts/webkitpy/tool/bot/layouttestresultsreader_unittest.py:
    (LayoutTestResultsReaderTest.test_missing_layout_test_results):
    (LayoutTestResultsReaderTest.test_create_unit_test_results):
    (test_missing_unit_test_results_path):
    (test_layout_test_results):
    (test_archive_last_layout_test_results):
    * Scripts/webkitpy/tool/commands/earlywarningsystem.py:
    (AbstractEarlyWarningSystem):
    (AbstractEarlyWarningSystem.__init__):
    (AbstractEarlyWarningSystem.begin_work_queue):
    (AbstractEarlyWarningSystem.run_command):
    * Scripts/webkitpy/tool/commands/queues.py:
    (AbstractPatchQueue.work_item_log_path):
    (PatchProcessingQueue):
    (PatchProcessingQueue._new_port_name_from_old):
    (PatchProcessingQueue.begin_work_queue):
    (PatchProcessingQueue._upload_results_archive_for_patch):
    (CommitQueue):
    (CommitQueue.begin_work_queue):
    (CommitQueue.run_command):
    (AbstractReviewQueue):
    (AbstractReviewQueue.__init__):
    (AbstractReviewQueue.begin_work_queue):
    * Scripts/webkitpy/tool/commands/queues_unittest.py:
    (AbstractPatchQueueTest.test_next_patch):
    (PatchProcessingQueueTest):
    (PatchProcessingQueueTest.test_upload_results_archive_for_patch):
    * Scripts/webkitpy/tool/steps/runtests.py:
    (RunTests.run):
    * Scripts/webkitpy/tool/steps/runtests_unittest.py:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138826 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/config/ports_mock.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/config/ports_mock.py
@@ -29,16 +29,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 class MockPort(object):
-    results_directory = "/mock-results"
-
     def name(self):
         return "MockPort"
-
-    def layout_tests_results_path(self):
-        return "/mock-results/full_results.json"
-
-    def unit_tests_results_path(self):
-        return "/mock-results/webkit_unit_tests_output.xml"
 
     def check_webkit_style_command(self):
         return ["mock-check-webkit-style"]
