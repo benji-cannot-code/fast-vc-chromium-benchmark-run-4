520cee23f20cef4682b151909709c9120396c74e
nghanavatian@rim.com <nghanavatian@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-04 20:51:08 +0000

    [BlackBerry] Allow defaultTabEventHandler to trigger on tab https://bugs.webkit.org/show_bug.cgi?id=106118
    Reviewed by Yong Li.
    
    In order to support tabbing through input elements, amongst
    other things, we need to bipass the processing of the tab key as
    '\t'. This will simply insert 4 spaces in the field. Remove tab
    support from the key down map so that we don't translate it to
    an editor command.
    
    Internally reviewed by Mike Fenton.
    
    * WebCoreSupport/EditorClientBlackBerry.cpp:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138839 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/EditorClientBlackBerry.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebCoreSupport/EditorClientBlackBerry.cpp
@@ -394,15 +394,6 @@ static const KeyDownEntry keyDownEntries[] = {
     { 'Z',       CtrlKey,            "Undo"                                        },
     { 'Z',       CtrlKey | ShiftKey, "Redo"                                        },
     { 'Y',       CtrlKey,            "Redo"                                        },
-
-    { VK_TAB,    0,                  "InsertTab"                                   },
-    { VK_TAB,    ShiftKey,           "InsertBacktab"                               },
-    { VK_RETURN, 0,                  "InsertNewline"                               },
-    { VK_RETURN, CtrlKey,            "InsertNewline"                               },
-    { VK_RETURN, AltKey,             "InsertNewline"                               },
-    { VK_RETURN, ShiftKey,           "InsertLineBreak"                             },
-    { VK_RETURN, AltKey | ShiftKey,  "InsertNewline"                               },
-
 };
 
 static const KeyPressEntry keyPressEntries[] = {
@@ -464,6 +455,9 @@ void EditorClientBlackBerry::handleKeyboardEvent(KeyboardEvent* event)
     ASSERT(frame);
 
     String commandName = interpretKeyEvent(event);
+
+    // Check to see we are not trying to insert text on key down.
+    ASSERT(!(event->type() == eventNames().keydownEvent && frame->editor()->command(commandName).isTextInsertion()));
 
     if (!commandName.isEmpty()) {
         // Hot key handling. Cancel processing mode.
