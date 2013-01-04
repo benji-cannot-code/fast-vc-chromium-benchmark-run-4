1b97cea82f30635acbf98fb3ad19f9817a615382
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-04 21:37:46 +0000

    Crash in WebCore::RenderBlock::willBeDestroyed https://bugs.webkit.org/show_bug.cgi?id=103455
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    It is not required to set beforeChild to :after child since DOM is
    now pseudo element aware. See trac.webkit.org/changeset/137336. We
    were incorrectly placing the inline continuation before the :after
    content.
    
    Test: fast/multicol/continuation-crash.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::addChildIgnoringAnonymousColumnBlocks): remove beforeChild
    setting to afterPseudoElementRenderer.
    * rendering/RenderRuby.cpp:
    (WebCore::RenderRubyAsInline::addChild): add missing beforeChild argument.
    setting to afterPseudoElementRenderer.
    (WebCore::RenderRubyAsBlock::addChild): add missing beforeChild argument.
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::addChild): remove beforeChild
    setting to afterPseudoElementRenderer.
    * rendering/RenderTableRow.cpp:
    (WebCore::RenderTableRow::addChild): remove beforeChild
    setting to afterPseudoElementRenderer.
    * rendering/RenderTableSection.cpp:
    (WebCore::RenderTableSection::addChild): remove beforeChild
    setting to afterPseudoElementRenderer.
    
    LayoutTests:
    
    * fast/multicol/continuation-crash-expected.txt: Added.
    * fast/multicol/continuation-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138850 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderRuby.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderRuby.cpp
@@ -185,7 +185,7 @@ void RenderRubyAsInline::addChild(RenderObject* child, RenderObject* beforeChild
     RenderRubyRun* lastRun = lastRubyRun(this);
     if (!lastRun || lastRun->hasRubyText()) {
         lastRun = RenderRubyRun::staticCreateRubyRun(this);
-        RenderInline::addChild(lastRun);
+        RenderInline::addChild(lastRun, beforeChild);
     }
     lastRun->addChild(child);
 }
@@ -292,7 +292,7 @@ void RenderRubyAsBlock::addChild(RenderObject* child, RenderObject* beforeChild)
     RenderRubyRun* lastRun = lastRubyRun(this);
     if (!lastRun || lastRun->hasRubyText()) {
         lastRun = RenderRubyRun::staticCreateRubyRun(this);
-        RenderBlock::addChild(lastRun);
+        RenderBlock::addChild(lastRun, beforeChild);
     }
     lastRun->addChild(child);
 }
