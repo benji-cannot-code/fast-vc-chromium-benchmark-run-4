60340256a03ce84b8b7b7df3057892ea118afe6f
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-04 18:37:29 +0000

    Tiny indent fix in prerender_link_manager.cc
    R=mmenke@chromium.org
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11746030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_link_manager.cc
+++ chrome/browser/prerender/prerender_link_manager.cc
@@ -80,7 +80,7 @@ void PrerenderLinkManager::OnAddPrerender(int launcher_child_id,
 
 void PrerenderLinkManager::OnCancelPrerender(int child_id, int prerender_id) {
   LinkPrerender* prerender = FindByLauncherChildIdAndPrerenderId(child_id,
-                                                             prerender_id);
+                                                                 prerender_id);
   if (!prerender)
     return;
 
@@ -99,7 +99,7 @@ void PrerenderLinkManager::OnCancelPrerender(int child_id, int prerender_id) {
 
 void PrerenderLinkManager::OnAbandonPrerender(int child_id, int prerender_id) {
   LinkPrerender* prerender = FindByLauncherChildIdAndPrerenderId(child_id,
-                                                             prerender_id);
+                                                                 prerender_id);
   if (!prerender)
     return;
 
