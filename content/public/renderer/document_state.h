5434c4076da886705ce8c09a45f24a5f4566920b
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-05 03:16:00 +0000

    Update some #includes in content/public/ for new Platform directory
    Review URL: https://chromiumcodereview.appspot.com/11789002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175273 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/document_state.h
+++ content/public/renderer/document_state.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/time.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebReferrerPolicy.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebURLRequest.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebDataSource.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebURLRequest.h"
 
 namespace webkit_glue {
 class AltErrorPageResourceFetcher;
