89e073fb3819423bba93817e3bb8f06afe5dcac3
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-05 00:32:10 +0000

    Don't access owner() in the constructor; eventually doing this won't work, because InfoBarDelegates won't be owned at creation.  (It's fine right now, but this is more efficient anyway, since the caller already has a Browser*.)
    BUG=none
    TEST=none
    Review URL: https://codereview.chromium.org/11740035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175242 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/session_crashed_prompt.cc
+++ chrome/browser/ui/startup/session_crashed_prompt.cc
@@ -31,7 +31,8 @@ namespace {
 class SessionCrashedInfoBarDelegate : public ConfirmInfoBarDelegate,
                                       public content::NotificationObserver {
  public:
-  explicit SessionCrashedInfoBarDelegate(InfoBarService* infobar_service);
+  SessionCrashedInfoBarDelegate(InfoBarService* infobar_service,
+                                Browser* browser);
 
  private:
   virtual ~SessionCrashedInfoBarDelegate();
@@ -57,11 +58,12 @@ class SessionCrashedInfoBarDelegate : public ConfirmInfoBarDelegate,
 };
 
 SessionCrashedInfoBarDelegate::SessionCrashedInfoBarDelegate(
-    InfoBarService* infobar_service)
+    InfoBarService* infobar_service,
+    Browser* browser)
     : ConfirmInfoBarDelegate(infobar_service),
       accepted_(false),
       removed_notification_received_(false),
-      browser_(chrome::FindBrowserWithWebContents(owner()->GetWebContents())) {
+      browser_(browser) {
   // TODO(pkasting,marja): Once InfoBars own they delegates, this is not needed
   // any more. Then we can rely on delegates getting destroyed, and we can
   // initiate the session storage scavenging only in the destructor. (Currently,
@@ -151,7 +153,7 @@ void ShowSessionCrashedPrompt(Browser* browser) {
     return;
 
   infobar_service->AddInfoBar(
-      new SessionCrashedInfoBarDelegate(infobar_service));
+      new SessionCrashedInfoBarDelegate(infobar_service, browser));
 }
 
 }  // namespace chrome
