0cd582f5f43b3fce21de88ceafd95aa2c4ea204f
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-07 15:50:15 +0000

    We don't want to tick animation controllers for non-active layers
    With this change, we will only tick animation controllers once one of their corresponding layers is active.
    
    R=enne@chromium.org
    NOTRY=true
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11633044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175338 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer.cc
+++ cc/layer.cc
@@ -749,6 +749,11 @@ void Layer::OnTransformAnimated(const gfx::Transform& transform)
     m_transform = transform;
 }
 
+bool Layer::IsActive() const
+{
+    return true;
+}
+
 bool Layer::addAnimation(scoped_ptr <ActiveAnimation> animation)
 {
     // WebCore currently assumes that accelerated animations will start soon
