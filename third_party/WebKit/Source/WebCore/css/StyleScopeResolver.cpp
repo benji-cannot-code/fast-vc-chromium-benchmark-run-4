32f3263157c1ba9940be69dd2782c0de0e89f349
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-07 09:07:01 +0000

    Cannot compile with ENABLE_SHADOW_DOM=0 and ENABLE_STYLE_SCOPED=1 https://bugs.webkit.org/show_bug.cgi?id=106207
    Reviewed by Hajime Morita.
    
    Guard StyleScopeResolver::addHostRule with SHADOW_DOM flag.
    
    No tests, as no change in behavior.
    
    * css/StyleScopeResolver.cpp:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138929 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleScopeResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleScopeResolver.cpp
@@ -171,6 +171,7 @@ inline RuleSet* StyleScopeResolver::atHostRuleSetFor(const ShadowRoot* shadowRoo
     return it != m_atHostRules.end() ? it->value.get() : 0;
 }
 
+#if ENABLE(SHADOW_DOM)
 void StyleScopeResolver::addHostRule(StyleRuleHost* hostRule, bool hasDocumentSecurityOrigin, const ContainerNode* scope)
 {
     if (!scope || !scope->isInShadowTree())
@@ -191,6 +192,7 @@ void StyleScopeResolver::addHostRule(StyleRuleHost* hostRule, bool hasDocumentSe
             rule->addStyleRule(static_cast<StyleRule*>(hostStylingRule), addRuleFlags);
     }
 }
+#endif
 
 bool StyleScopeResolver::styleSharingCandidateMatchesHostRules(const Element* element)
 {
