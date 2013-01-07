cb798b2af92355ea06cb4e063ffc33d3912a1275
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-07 22:34:38 +0000

    Fix a leak in KeyboardOverlayViewTest.OpenAcceleratorsClose.
    BUG=167165
    
    Review URL: https://codereview.chromium.org/11648037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175400 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/keyboard_overlay/keyboard_overlay_view_unittest.cc
+++ ash/keyboard_overlay/keyboard_overlay_view_unittest.cc
@@ -25,9 +25,10 @@ bool operator==(const KeyboardOverlayView::KeyEventData& lhs,
 
 // Verifies that the accelerators that open the keyboard overlay close it.
 TEST_F(KeyboardOverlayViewTest, OpenAcceleratorsClose) {
-  KeyboardOverlayView* view = new KeyboardOverlayView(
+  ui::test::TestWebDialogDelegate delegate(GURL("chrome://keyboardoverlay"));
+  KeyboardOverlayView view(
       Shell::GetInstance()->browser_context(),
-      new ui::test::TestWebDialogDelegate(GURL("chrome://keyboardoverlay")),
+      &delegate,
       new ui::test::TestWebContentsHandler);
   for (size_t i = 0; i < kAcceleratorDataLength; ++i) {
     if (kAcceleratorData[i].action != SHOW_KEYBOARD_OVERLAY)
@@ -38,7 +39,7 @@ TEST_F(KeyboardOverlayViewTest, OpenAcceleratorsClose) {
                           open_key_data.keycode,
                           open_key_data.modifiers,
                           false);
-    EXPECT_TRUE(view->IsCancelingKeyEvent(&open_key));
+    EXPECT_TRUE(view.IsCancelingKeyEvent(&open_key));
   }
 }
 
