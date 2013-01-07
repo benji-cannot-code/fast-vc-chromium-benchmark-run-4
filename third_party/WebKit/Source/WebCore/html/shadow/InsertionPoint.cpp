9310b8623ed732f89422593913320a82d9688db5
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-07 06:10:27 +0000

    [Shadow DOM]: crash in WebCore::ElementShadow::setValidityUndetermined https://bugs.webkit.org/show_bug.cgi?id=106203
    Reviewed by Hajime Morita.
    
    InsertionPoint should check whether ElementShadow is available or not
    before invoking its methods.
    
    No new tests. Webcomponents_fuzzer reported this bug. Would like to
    use the fuzzer to check this issue.
    
    * html/shadow/InsertionPoint.cpp:
    (WebCore::InsertionPoint::getDistributedNodes):
    (WebCore::InsertionPoint::childrenChanged):
    (WebCore::InsertionPoint::insertedInto):
    Added ifs to check whether ElementShadow is available or not before
    invoking ElementShadow's methods, i.e. setValidityUndetermined,
    invalidateDistribution, and ensureDistributionDocument.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@138923 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/InsertionPoint.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/InsertionPoint.cpp
@@ -100,8 +100,10 @@ bool InsertionPoint::isActive() const
 
 PassRefPtr<NodeList> InsertionPoint::getDistributedNodes() const
 {
-    if (treeScope()->rootNode()->isShadowRoot())
-        toShadowRoot(treeScope()->rootNode())->owner()->ensureDistributionFromDocument();
+    ContainerNode* rootNode = treeScope()->rootNode();
+    if (rootNode->isShadowRoot())
+        if (ElementShadow* rootOwner = toShadowRoot(rootNode)->owner())
+            rootOwner->ensureDistributionFromDocument();
 
     Vector<RefPtr<Node> > nodes;
 
@@ -120,7 +122,8 @@ void InsertionPoint::childrenChanged(bool changedByParser, Node* beforeChange, N
 {
     HTMLElement::childrenChanged(changedByParser, beforeChange, afterChange, childCountDelta);
     if (ShadowRoot* root = containingShadowRoot())
-        root->owner()->invalidateDistribution();
+        if (ElementShadow* rootOwner = root->owner())
+            rootOwner->invalidateDistribution();
 }
 
 Node::InsertionNotificationRequest InsertionPoint::insertedInto(ContainerNode* insertionPoint)
@@ -128,14 +131,15 @@ Node::InsertionNotificationRequest InsertionPoint::insertedInto(ContainerNode* i
     HTMLElement::insertedInto(insertionPoint);
 
     if (ShadowRoot* root = containingShadowRoot()) {
-        root->owner()->setValidityUndetermined();
-        root->owner()->invalidateDistribution();
-        if (isActive() && !m_registeredWithShadowRoot && insertionPoint->treeScope()->rootNode() == root) {
-            m_registeredWithShadowRoot = true;
-            root->registerInsertionPoint(this);
+        if (ElementShadow* rootOwner = root->owner()) {
+            rootOwner->setValidityUndetermined();
+            rootOwner->invalidateDistribution();
+            if (isActive() && !m_registeredWithShadowRoot && insertionPoint->treeScope()->rootNode() == root) {
+                m_registeredWithShadowRoot = true;
+                root->registerInsertionPoint(this);
+            }
         }
     }
-
 
     return InsertionDone;
 }
