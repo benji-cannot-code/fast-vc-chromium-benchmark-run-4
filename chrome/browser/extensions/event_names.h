3eac003b25584d9c99ce2cf0e2d95e07ecfacc6b
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-07 21:57:49 +0000

    Add tabs.onReplaced event to notify listeners of tabs being replaced due to prerendering or instant. Don't send onCreated/onRemoved notificatiosn anymore for those cases.
    BUG=85584
    
    Review URL: https://chromiumcodereview.appspot.com/11607012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175391 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/event_names.h
+++ chrome/browser/extensions/event_names.h
@@ -22,6 +22,7 @@ extern const char kOnTabHighlightChanged[];
 extern const char kOnTabHighlighted[];
 extern const char kOnTabMoved[];
 extern const char kOnTabRemoved[];
+extern const char kOnTabReplaced[];
 extern const char kOnTabSelectionChanged[];
 extern const char kOnTabUpdated[];
 
