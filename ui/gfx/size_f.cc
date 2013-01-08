359dea6cdaca005386f9a689a05a1d27d6ebabdf
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 16:02:05 +0000

    ui: Remove gfx::Size::ClampToNonNegative, prevent negative sizes always.
    This was added with the intention of using Size as a vector, replacing use of
    IntSize. Since we have Vector2d now, negative sizes should not exist, so clamp
    them in set_width/set_height and the constructor.
    
    Not covered by tests, as we can't test DCHECKs.
    
    TBR=sky
    BUG=160158
    Relanding: https://codereview.chromium.org/11365160/
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=167255
    
    Review URL: https://chromiumcodereview.appspot.com/11410024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175535 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/size_f.cc
+++ ui/gfx/size_f.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gfx/size_f.h"
 
-#include "base/logging.h"
 #include "base/stringprintf.h"
+#include "ui/gfx/size_base_impl.h"
 
 namespace gfx {
 
