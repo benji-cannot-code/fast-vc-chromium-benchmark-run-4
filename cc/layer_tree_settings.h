2be610341bb0c58ff1e1ac982a8bf05613a5f3ef
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 17:12:33 +0000

    cc: Fix low-res impl-side painting artifacts
    Instead of clipping cc::Picture on recorded pixel boundaries (which may be a
    partial pixel in the final rastered content if content scale < 1), clip only on
    integral pixels in the final rastered output.  This requires recording a small
    buffer of pixels around every picture, specified via a min contents scale.
    
    NOTRY=true
    R=nduca@chromium.org
    BUG=167590
    
    
    Review URL: https://chromiumcodereview.appspot.com/11678003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175542 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_settings.h
+++ cc/layer_tree_settings.h
@@ -31,6 +31,7 @@ class CC_EXPORT LayerTreeSettings {
   bool canUseLCDText;
   bool shouldClearRootRenderPass;
   bool useLinearFadeScrollbarAnimator;
+  float minimumContentsScale;
   double refreshRate;
   size_t maxPartialTextureUpdates;
   size_t numRasterThreads;
--- cc/picture.cc
+++ cc/picture.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkData.h"
 #include "third_party/skia/include/utils/SkPictureUtils.h"
 #include "ui/gfx/rect_conversions.h"
+#include "ui/gfx/skia_util.h"
 
 namespace {
 // URI label for a lazily decoded SkPixelRef.
@@ -89,10 +90,16 @@ void Picture::Record(ContentLayerClient* painter,
   opaque_rect_ = gfx::ToEnclosedRect(opaque_layer_rect);
 }
 
-void Picture::Raster(SkCanvas* canvas) {
+void Picture::Raster(
+    SkCanvas* canvas,
+    gfx::Rect content_rect,
+    float contents_scale) {
   TRACE_EVENT0("cc", "Picture::Raster");
   DCHECK(picture_);
+
   canvas->save();
+  canvas->clipRect(gfx::RectToSkRect(content_rect));
+  canvas->scale(contents_scale, contents_scale);
   canvas->translate(layer_rect_.x(), layer_rect_.y());
   canvas->drawPicture(*picture_);
   canvas->restore();
