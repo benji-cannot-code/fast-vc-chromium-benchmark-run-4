6c9bb23753b982f1599f20e419506d880d51187d
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 01:14:26 +0000

    requestAutocomplete: change comboboxes to dropdown menus
    Change comboboxes to dropdown menus, per the latest mocks. Allow users to switch between manual entry and pre-fill modes.
    
    lots of TODOs, but this seems like a good stopping point.
    
    BUG=157270,157273
    
    Review URL: https://chromiumcodereview.appspot.com/11743036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175439 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_models.cc
+++ chrome/browser/ui/autofill/autofill_dialog_models.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/autofill/autofill_dialog_comboboxes.h"
+#include "chrome/browser/ui/autofill/autofill_dialog_models.h"
 
 #include "base/string_number_conversions.h"
 #include "base/stringprintf.h"
@@ -12,27 +12,47 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace autofill {
 
-// SuggestionsComboboxModel ----------------------------------------------------
+SuggestionsMenuModelDelegate::~SuggestionsMenuModelDelegate() {}
 
-SuggestionsComboboxModel::SuggestionsComboboxModel() {}
+// SuggestionsMenuModel ----------------------------------------------------
 
-SuggestionsComboboxModel::~SuggestionsComboboxModel() {}
+SuggestionsMenuModel::SuggestionsMenuModel(
+    SuggestionsMenuModelDelegate* delegate)
+    : ALLOW_THIS_IN_INITIALIZER_LIST(ui::SimpleMenuModel(this)),
+      delegate_(delegate),
+      checked_item_(0) {}
 
-void SuggestionsComboboxModel::AddItem(
+SuggestionsMenuModel::~SuggestionsMenuModel() {}
+
+void SuggestionsMenuModel::AddKeyedItem(
     const std::string& key, const string16& item) {
   items_.push_back(std::make_pair(key, item));
+  AddCheckItem(items_.size() - 1, item);
 }
 
-std::string SuggestionsComboboxModel::GetItemKeyAt(int index) const {
+std::string SuggestionsMenuModel::GetItemKeyAt(int index) const {
   return items_[index].first;
 }
 
-int SuggestionsComboboxModel::GetItemCount() const {
-  return items_.size();
+bool SuggestionsMenuModel::IsCommandIdChecked(
+    int command_id) const {
+  return checked_item_ == command_id;
 }
 
-string16 SuggestionsComboboxModel::GetItemAt(int index) {
-  return items_[index].second;
+bool SuggestionsMenuModel::IsCommandIdEnabled(
+    int command_id) const {
+  return true;
+}
+
+bool SuggestionsMenuModel::GetAcceleratorForCommandId(
+    int command_id,
+    ui::Accelerator* accelerator) {
+  return false;
+}
+
+void SuggestionsMenuModel::ExecuteCommand(int command_id) {
+  checked_item_ = command_id;
+  delegate_->SuggestionItemSelected(*this);
 }
 
 // MonthComboboxModel ----------------------------------------------------------
