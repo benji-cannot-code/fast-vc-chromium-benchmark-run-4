6b63c2f25b10ba38ca4989541e21925b9b10709c
christophe.dumez@intel.com <christophe.dumez@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-08 06:03:20 +0000

    [CoordinatedGraphics] Support invalidation tracking for CoordinatedGraphicsLayer https://bugs.webkit.org/show_bug.cgi?id=106002
    Reviewed by Noam Rosenthal.
    
    Source/WebKit2:
    
    Add calls to addRepaintRect() to CoordinatedGraphicsLayer so that
    we can track invalidation in the composited layer.
    
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::setContentsNeedsDisplay):
    (WebCore::CoordinatedGraphicsLayer::setNeedsDisplay): Pass a FloatRect instead
    of an IntRect to setNeedsDisplayInRect() since it is what it expects.
    (WebCore::CoordinatedGraphicsLayer::setNeedsDisplayInRect):
    
    LayoutTests:
    
    Unskip several tests that require invalidation tracking for EFL WK2 now that
    it is supported by Coordinated Graphics.
    
    * platform/efl-wk2/TestExpectations:
    * platform/efl/TestExpectations:
    * platform/efl/compositing/repaint/invalidations-on-composited-layers-expected.txt: Added.
    Use same expectation as mac port.
    * platform/efl/fast/images/repaint-subrect-grid-expected.txt: Added. We are sometimes
    1 pixel off compared to the global expectation likely due to lack for subpixel layout
    support.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139031 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp
@@ -319,6 +319,8 @@ void CoordinatedGraphicsLayer::setContentsNeedsDisplay()
         m_canvasNeedsDisplay = true;
     if (client())
         client()->notifyFlushRequired(this);
+
+    addRepaintRect(contentsRect());
 }
 
 void CoordinatedGraphicsLayer::setContentsToCanvas(PlatformLayer* platformLayer)
@@ -430,14 +432,17 @@ void CoordinatedGraphicsLayer::setReplicatedByLayer(GraphicsLayer* layer)
 
 void CoordinatedGraphicsLayer::setNeedsDisplay()
 {
-    setNeedsDisplayInRect(IntRect(IntPoint::zero(), IntSize(size().width(), size().height())));
+    setNeedsDisplayInRect(FloatRect(FloatPoint(), size()));
 }
 
 void CoordinatedGraphicsLayer::setNeedsDisplayInRect(const FloatRect& rect)
 {
     if (m_mainBackingStore)
         m_mainBackingStore->invalidate(IntRect(rect));
+
     didChangeLayerState();
+
+    addRepaintRect(rect);
 }
 
 CoordinatedLayerID CoordinatedGraphicsLayer::id() const
