072b5e8fbeebef6afc1b683e31765d4ad17dbee1
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 03:08:50 +0000

    Fix hang in Android WebView release build tests
    SurfaceTexture.detachFromGLContext is not present in ICS and the old
    workaround of using a NullCompositor lead to the test creating
    TextureImageTransportSurface in ImageTransportSurface::CreateSurface.
    This path is wrong and not well tested in single-process mode and recently
    lead to a deadlock hang in release build only tests (see bug).
    
    Fixed by creating the real compositor but just avoid calling the missing
    method in ICS. The Java method is loaded lazily so this does not cause
    an error at run time.
    
    
    BUG=167236,161864
    Android only change. Ran through Android trybots.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11787031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175480 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/android/surface_texture_bridge.cc
+++ content/common/android/surface_texture_bridge.cc
@@ -7,6 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <android/native_window_jni.h>
 
+// TODO(boliu): Remove this include when we move off ICS.
+#include "base/android/build_info.h"
 #include "base/android/jni_android.h"
 #include "base/logging.h"
 #include "content/common/android/surface_texture_listener.h"
@@ -28,6 +30,16 @@ void RegisterNativesIfNeeded(JNIEnv* env) {
     g_jni_initialized = true;
   }
 }
+
+// TODO(boliu): Remove this method when when we move off ICS. See
+// http://crbug.com/161864.
+bool GlContextMethodsAvailable() {
+  bool available = base::android::BuildInfo::GetInstance()->sdk_int() >= 16;
+  if (!available)
+    LOG(WARNING) << "Running on unsupported device: rendering may not work";
+  return available;
+}
+
 }  // namespace
 
 namespace content {
@@ -109,17 +121,21 @@ void SurfaceTextureBridge::SetDefaultBufferSize(int width, int height) {
 }
 
 void SurfaceTextureBridge::AttachToGLContext(int texture_id) {
-  JNIEnv* env = AttachCurrentThread();
-  // Note: This method is only available on JB and greater.
-  JNI_SurfaceTexture::Java_SurfaceTexture_attachToGLContext(
-      env, j_surface_texture_.obj(), texture_id);
+  if (GlContextMethodsAvailable()) {
+    JNIEnv* env = AttachCurrentThread();
+    // Note: This method is only available on JB and greater.
+    JNI_SurfaceTexture::Java_SurfaceTexture_attachToGLContext(
+        env, j_surface_texture_.obj(), texture_id);
+  }
 }
 
 void SurfaceTextureBridge::DetachFromGLContext() {
-  JNIEnv* env = AttachCurrentThread();
-  // Note: This method is only available on JB and greater.
-  JNI_SurfaceTexture::Java_SurfaceTexture_detachFromGLContext(
-      env, j_surface_texture_.obj());
+  if (GlContextMethodsAvailable()) {
+    JNIEnv* env = AttachCurrentThread();
+    // Note: This method is only available on JB and greater.
+    JNI_SurfaceTexture::Java_SurfaceTexture_detachFromGLContext(
+        env, j_surface_texture_.obj());
+  }
 }
 
 ANativeWindow* SurfaceTextureBridge::CreateSurface() {
