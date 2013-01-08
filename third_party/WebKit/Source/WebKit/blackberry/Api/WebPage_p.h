c4fc634bdba94a80b2f4c06cae8a1e08ba4c56ac
zhajiang@rim.com <zhajiang@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-08 22:03:24 +0000

    [Blackberry][FullScreen] Create a helper method to adjust the fullscreen renderer dimensions https://bugs.webkit.org/show_bug.cgi?id=91748
    Patch by Jacky Jiang <zhajiang@rim.com>.
    Reviewed by Antonio Gomes.
    
    PR: 195524
    The was originally from Antonio Gomes on PR195524. Bug 105488 has
    partially upstreamed that patch. This patch upstreams the rest of it.
    
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::updateViewportSize):
    (BlackBerry::WebKit::WebPagePrivate::notifyTransformedScrollChanged):
    * Api/WebPage_p.h:
    (WebPagePrivate):
    * WebCoreSupport/ChromeClientBlackBerry.cpp:
    (WebCore::ChromeClientBlackBerry::fullScreenRendererChanged):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139110 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage_p.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage_p.h
@@ -220,6 +220,7 @@ public:
 #if ENABLE(FULLSCREEN_API)
     void enterFullScreenForElement(WebCore::Element*);
     void exitFullScreenForElement(WebCore::Element*);
+    void adjustFullScreenElementDimensionsIfNeeded();
 #endif
     void contentsSizeChanged(const WebCore::IntSize&);
     void overflowExceedsContentsSize();
