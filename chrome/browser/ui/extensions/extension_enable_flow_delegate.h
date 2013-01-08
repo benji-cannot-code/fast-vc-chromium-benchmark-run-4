90a73c9e2a4a5799c80579be026a97a238d8dd0d
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 03:19:03 +0000

    Put extension enable logic into a ExtensionEnableFlow.
    Extract the code from AppLauncherHandler into a ExtensionEnableFlow so that it
    can be reused by app list and launcher bar.
    
    BUG=157996
    TEST=No change. Wait until app list/launcher code changes land to test.
    
    R=estade@chromium.org,benwells@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11644077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175483 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/extensions/extension_enable_flow_delegate.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_ENABLE_FLOW_DELEGATE_H_
+#define CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_ENABLE_FLOW_DELEGATE_H_
+
+class ExtensionInstallPrompt;
+
+class ExtensionEnableFlowDelegate {
+ public:
+  // Invoked to create an ExtensionInstallPrompt for the flow to use. Caller
+  // takes ownership of the return ExtensionInstallPrompt;
+  virtual ExtensionInstallPrompt* CreateExtensionInstallPrompt() = 0;
+
+  // Invoked when |flow| is finished successfully.
+  virtual void ExtensionEnableFlowFinished() = 0;
+
+  // Invoked when |flow| is aborted.
+  virtual void ExtensionEnableFlowAborted(bool user_initiated) = 0;
+
+ protected:
+  virtual ~ExtensionEnableFlowDelegate() {}
+};
+
+#endif  // CHROME_BROWSER_UI_EXTENSIONS_EXTENSION_ENABLE_FLOW_DELEGATE_H_
