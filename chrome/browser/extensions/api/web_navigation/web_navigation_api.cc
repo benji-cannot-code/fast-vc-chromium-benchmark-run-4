e12b7c644f25da1cac7d71b16b8ca0450a734d87
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 12:42:00 +0000

    When an iframe displays its srcdoc, generate navigation events for about:srcdoc
    BUG=143337
    TEST=unit_tests:FrameNavigationStateTest.SrcDoc, browser_tests:WebNavigationApiTest.SrcDoc
    R=marja@chromium.org,creis@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11791018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175518 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_navigation/web_navigation_api.cc
+++ chrome/browser/extensions/api/web_navigation/web_navigation_api.cc
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/url_constants.h"
 #include "net/base/net_errors.h"
 
 namespace GetFrame = extensions::api::web_navigation::GetFrame;
@@ -377,7 +378,9 @@ void WebNavigationTabObserver::DidStartProvisionalLoadForFrame(
                                parent_frame_id,
                                validated_url,
                                is_main_frame,
-                               is_error_page);
+                               is_error_page,
+                               is_iframe_srcdoc);
+
   if (!navigation_state_.CanSendEvents(frame_id))
     return;
 
@@ -388,7 +391,7 @@ void WebNavigationTabObserver::DidStartProvisionalLoadForFrame(
       is_main_frame,
       parent_frame_num,
       navigation_state_.IsMainFrame(parent_frame_id),
-      validated_url);
+      navigation_state_.GetUrl(frame_id));
 }
 
 void WebNavigationTabObserver::DidCommitProvisionalLoadForFrame(
@@ -442,7 +445,7 @@ void WebNavigationTabObserver::DidCommitProvisionalLoadForFrame(
         web_contents(),
         frame_num,
         is_main_frame,
-        url,
+        navigation_state_.GetUrl(frame_id),
         transition_type);
   } else if (is_history_state_modification) {
     helpers::DispatchOnCommitted(
@@ -450,7 +453,7 @@ void WebNavigationTabObserver::DidCommitProvisionalLoadForFrame(
         web_contents(),
         frame_num,
         is_main_frame,
-        url,
+        navigation_state_.GetUrl(frame_id),
         transition_type);
   } else {
     if (navigation_state_.GetIsServerRedirected(frame_id)) {
@@ -462,7 +465,7 @@ void WebNavigationTabObserver::DidCommitProvisionalLoadForFrame(
         web_contents(),
         frame_num,
         is_main_frame,
-        url,
+        navigation_state_.GetUrl(frame_id),
         transition_type);
   }
 }
@@ -486,8 +489,12 @@ void WebNavigationTabObserver::DidFailProvisionalLoad(
   FrameNavigationState::FrameID frame_id(frame_num, render_view_host);
   if (navigation_state_.CanSendEvents(frame_id)) {
     helpers::DispatchOnErrorOccurred(
-        web_contents(), render_view_host->GetProcess()->GetID(), validated_url,
-        frame_num, is_main_frame, error_code);
+        web_contents(),
+        render_view_host->GetProcess()->GetID(),
+        navigation_state_.GetUrl(frame_id),
+        frame_num,
+        is_main_frame,
+        error_code);
   }
   navigation_state_.SetErrorOccurredInFrame(frame_id);
   if (stop_tracking_frames) {
@@ -525,10 +532,12 @@ void WebNavigationTabObserver::DidFinishLoad(
   navigation_state_.SetNavigationCompleted(frame_id);
   if (!navigation_state_.CanSendEvents(frame_id))
     return;
-  DCHECK_EQ(navigation_state_.GetUrl(frame_id), validated_url);
+  DCHECK(navigation_state_.GetUrl(frame_id) == validated_url ||
+         (navigation_state_.GetUrl(frame_id) == GURL(chrome::kAboutSrcDocURL) &&
+          validated_url == GURL(chrome::kAboutBlankURL)));
   DCHECK_EQ(navigation_state_.IsMainFrame(frame_id), is_main_frame);
   helpers::DispatchOnCompleted(web_contents(),
-                               validated_url,
+                               navigation_state_.GetUrl(frame_id),
                                is_main_frame,
                                frame_num);
 }
@@ -549,8 +558,12 @@ void WebNavigationTabObserver::DidFailLoad(
     return;
   if (navigation_state_.CanSendEvents(frame_id)) {
     helpers::DispatchOnErrorOccurred(
-        web_contents(), render_view_host->GetProcess()->GetID(), validated_url,
-        frame_num, is_main_frame, error_code);
+        web_contents(),
+        render_view_host->GetProcess()->GetID(),
+        navigation_state_.GetUrl(frame_id),
+        frame_num,
+        is_main_frame,
+        error_code);
   }
   navigation_state_.SetErrorOccurredInFrame(frame_id);
 }
