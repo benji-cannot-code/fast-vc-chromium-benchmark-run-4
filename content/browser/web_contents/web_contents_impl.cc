7d4a3592105e910097f114c84e0ffe5bc9b2b316
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-08 08:20:06 +0000

    Also filter the URLs from incoming non-provisional navigation IPCs.
    Without this, e.g. a navigation to about:srcdoc looks like a navigation to
    about:blank during the provisional phase, and suddendly the internal
    about:srcdoc URL is leaked to the embedder.
    
    BUG=168611
    TEST=WebContentsImplTest.FilterURLs
    R=creis@chromium.org
    
    Review URL: https://codereview.chromium.org/11801011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175501 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2137,8 +2137,11 @@ void WebContentsImpl::OnDocumentLoadedInFrame(int64 frame_id) {
 
 void WebContentsImpl::OnDidFinishLoad(
     int64 frame_id,
-    const GURL& validated_url,
+    const GURL& url,
     bool is_main_frame) {
+  GURL validated_url(url);
+  RenderProcessHost* render_process_host = message_source_->GetProcess();
+  RenderViewHost::FilterURL(render_process_host, false, &validated_url);
   FOR_EACH_OBSERVER(WebContentsObserver, observers_,
                     DidFinishLoad(frame_id, validated_url, is_main_frame,
                                   message_source_));
@@ -2146,10 +2149,13 @@ void WebContentsImpl::OnDidFinishLoad(
 
 void WebContentsImpl::OnDidFailLoadWithError(
     int64 frame_id,
-    const GURL& validated_url,
+    const GURL& url,
     bool is_main_frame,
     int error_code,
     const string16& error_description) {
+  GURL validated_url(url);
+  RenderProcessHost* render_process_host = message_source_->GetProcess();
+  RenderViewHost::FilterURL(render_process_host, false, &validated_url);
   FOR_EACH_OBSERVER(WebContentsObserver, observers_,
                     DidFailLoad(frame_id, validated_url, is_main_frame,
                                 error_code, error_description,
