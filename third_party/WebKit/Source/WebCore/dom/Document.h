1633613032edc115e088cb635f2f3cee59f7f9f9
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-09 17:41:10 +0000

    Document::setActiveNode() should be Document::setActiveElement() https://bugs.webkit.org/show_bug.cgi?id=106437
    Reviewed by Ojan Vafai.
    
    No new tests. No functoinal change.
    
    * dom/Document.cpp:
    (WebCore::Document::setActiveElement):
    (WebCore::Document::updateHoverActiveState):
    * dom/Document.h:
    (Document):
    * rendering/HitTestResult.cpp:
    (WebCore::HitTestResult::innerElement):
    (WebCore):
    * rendering/HitTestResult.h:
    (HitTestResult):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139199 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -700,7 +700,7 @@ public:
     void setHoverNode(PassRefPtr<Node>);
     Node* hoverNode() const { return m_hoverNode.get(); }
 
-    void setActiveNode(PassRefPtr<Node>);
+    void setActiveElement(PassRefPtr<Element>);
     Element* activeElement() const { return m_activeElement.get(); }
 
     void focusedNodeRemoved();
