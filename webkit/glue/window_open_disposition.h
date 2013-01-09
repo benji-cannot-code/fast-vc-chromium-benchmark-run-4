28752727dc43602c05ce75a3de921e67c91b9ed0
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 00:27:32 +0000

    Move NavigationPolicyToDisposition() function into render_view_impl.cc
    Since it's only used there. That way we will be able to move
    window_open_disposition.h out of webkit/
    
    BUG=146251
    R=jam@chromium.org
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/11798003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175629 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/window_open_disposition.h
+++ webkit/glue/window_open_disposition.h
@@ -6,9 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WEBKIT_GLUE_WINDOW_OPEN_DISPOSITION_H_
 #define WEBKIT_GLUE_WINDOW_OPEN_DISPOSITION_H_
 
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebNavigationPolicy.h"
-#include "webkit/glue/webkit_glue_export.h"
-
 enum WindowOpenDisposition {
   UNKNOWN,
   SUPPRESS_OPEN,
@@ -23,9 +20,5 @@ enum WindowOpenDisposition {
   OFF_THE_RECORD,
   IGNORE_ACTION
 };
-
-// Conversion function:
-WEBKIT_GLUE_EXPORT WindowOpenDisposition NavigationPolicyToDisposition(
-    WebKit::WebNavigationPolicy policy);
 
 #endif  // WEBKIT_GLUE_WINDOW_OPEN_DISPOSITION_H_
