e16e655d97c05df0211edc6fc7fe9f2a42c33aa2
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-09 21:51:10 +0000

    SVG-as-image: Throw out cached bitmap renderings after they sit unused for some time. <http://webkit.org/b/106484> <rdar://problem/12983216>
    Reviewed by Antti Koivisto.
    
    Add a one-shot timer to SVGImageCache to self-clear the bitmap cache 30 seconds after last access.
    This prevents us from keeping huge ImageBuffers around in memory for the lifetime of the elements
    referencing that particular SVG file. (The ownership model is actually a bit more complicated
    but that's irrelevant to this issue.)
    
    10234kB progression on Membuster3.
    
    * svg/graphics/SVGImageCache.cpp:
    (WebCore::SVGImageCache::SVGImageCache):
    (WebCore::SVGImageCache::~SVGImageCache):
    (WebCore::SVGImageCache::clearBitmapCache):
    (WebCore::SVGImageCache::cacheClearTimerFired):
    (WebCore::SVGImageCache::lookupOrCreateBitmapImageForRenderer):
    * svg/graphics/SVGImageCache.h:
    (SVGImageCache):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139236 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/graphics/SVGImageCache.cpp
+++ third_party/WebKit/Source/WebCore/svg/graphics/SVGImageCache.cpp
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2011 Research In Motion Limited. All rights reserved.
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -32,9 +33,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+static const int timeToKeepCachedBitmapsAfterLastUseInSeconds = 30;
+
 SVGImageCache::SVGImageCache(SVGImage* svgImage)
     : m_svgImage(svgImage)
     , m_redrawTimer(this, &SVGImageCache::redrawTimerFired)
+    , m_cacheClearTimer(this, &SVGImageCache::cacheClearTimerFired, timeToKeepCachedBitmapsAfterLastUseInSeconds)
 {
     ASSERT(m_svgImage);
 }
@@ -42,7 +46,11 @@ SVGImageCache::SVGImageCache(SVGImage* svgImage)
 SVGImageCache::~SVGImageCache()
 {
     m_sizeAndScalesMap.clear();
+    clearBitmapCache();
+}
 
+void SVGImageCache::clearBitmapCache()
+{
     ImageDataMap::iterator end = m_imageDataMap.end();
     for (ImageDataMap::iterator it = m_imageDataMap.begin(); it != end; ++it) {
         // Checks if the client (it->key) is still valid. The client should remove itself from this
@@ -126,6 +134,11 @@ void SVGImageCache::redrawTimerFired(Timer<SVGImageCache>*)
        redraw();
 }
 
+void SVGImageCache::cacheClearTimerFired(DeferrableOneShotTimer<SVGImageCache>*)
+{
+    clearBitmapCache();
+}
+
 Image* SVGImageCache::lookupOrCreateBitmapImageForRenderer(const RenderObject* renderer)
 {
     if (!renderer)
@@ -148,6 +161,9 @@ Image* SVGImageCache::lookupOrCreateBitmapImageForRenderer(const RenderObject* r
         scale = page->deviceScaleFactor() * page->pageScaleFactor();
 
     ASSERT(!size.isEmpty());
+
+    // (Re)schedule the oneshot timer to throw out all the cached ImageBuffers if they remain unused for too long.
+    m_cacheClearTimer.restart();
 
     // Lookup image for client in cache and eventually update it.
     ImageDataMap::iterator it = m_imageDataMap.find(client);
