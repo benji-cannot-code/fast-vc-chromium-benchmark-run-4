b41285227331e89a75d8bb28c34540d1447f5fc1
ppi@chromium.org <ppi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 16:01:23 +0000

    Add server certificate request parameters to be stored in SSLCertRequestInfo.
    Currently SSLCertRequestInfo provides a list of applicable client certificates,
    filtered against server request. This patch adds the server criteria to the
    class as a part of a larger refactoring effort.
    
    BUG=65546
    
    Review URL: https://chromiumcodereview.appspot.com/11739004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175807 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/test_data_directory.cc
+++ net/base/test_data_directory.cc
@@ -10,10 +10,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace net {
 
+namespace {
+const FilePath::CharType kCertificateRelativePath[] =
+    FILE_PATH_LITERAL("net/data/ssl/certificates");
+}  // namespace
+
 FilePath GetTestCertsDirectory() {
-  FilePath certs_dir;
-  PathService::Get(base::DIR_SOURCE_ROOT, &certs_dir);
-  return certs_dir.Append(FILE_PATH_LITERAL("net/data/ssl/certificates"));
+  FilePath src_root;
+  PathService::Get(base::DIR_SOURCE_ROOT, &src_root);
+  return src_root.Append(kCertificateRelativePath);
+}
+
+FilePath GetTestCertsDirectoryRelative() {
+  return FilePath(kCertificateRelativePath);
 }
 
 FilePath GetWebSocketTestDataDirectory() {
