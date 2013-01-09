3d253f80fc78b428cf3a3bd626bb048e9b19eeeb
cfleizach@apple.com <cfleizach@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-09 15:55:53 +0000

    AX: native popup buttons should not use textUnderElement for their title https://bugs.webkit.org/show_bug.cgi?id=106349
    Reviewed by Ryosuke Niwa.
    
    Chromium relies on the older title() method to return its title. As a result, we also
    need to handle the case of <select> element pop up buttons returning the textUnderElement().
    
    No new tests. Fix existing break.
    
    * accessibility/AccessibilityNodeObject.cpp:
    (WebCore::AccessibilityNodeObject::title):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139195 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/accessibility/AccessibilityNodeObject.cpp
+++ third_party/WebKit/Source/WebCore/accessibility/AccessibilityNodeObject.cpp
@@ -1516,6 +1516,9 @@ String AccessibilityNodeObject::title() const
 
     switch (roleValue()) {
     case PopUpButtonRole:
+        // Native popup buttons should not use their button children's text as a title. That value is retrieved through stringValue().
+        if (node->hasTagName(selectTag))
+            return String();
     case ButtonRole:
     case ToggleButtonRole:
     case CheckBoxRole:
