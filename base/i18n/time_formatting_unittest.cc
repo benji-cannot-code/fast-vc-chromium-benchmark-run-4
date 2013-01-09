d602305cd7b2653456a48655b28c64ef3c6efd54
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 23:58:08 +0000

    Enforce consistent include style for ICU headers.
    Instead of "unicode/...", "third_party/icu/public/..." should always be used
    in Chromium code. This matches the established style.
    
    BUG=165264
    
    Review URL: https://codereview.chromium.org/11791036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175929 0039d316-1c4b-4281-b951-d872f2087c98

--- base/i18n/time_formatting_unittest.cc
+++ base/i18n/time_formatting_unittest.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "unicode/uversion.h"
+#include "third_party/icu/public/common/unicode/uversion.h"
 
 namespace base {
 namespace {
