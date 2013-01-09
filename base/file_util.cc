0fe271a94dfc41e7742ca9c95d06191f2975a26b
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 00:38:59 +0000

    Don't allow path traversal paths on the base file helpers
    This forces explicit normalization of paths and make path escaping security bugs much harder to exploit. See for example bug 167122
    
    BUG=168890
    TEST=included tests
    Review URL: https://codereview.chromium.org/11782005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175642 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util.cc
+++ base/file_util.cc
@@ -153,6 +153,8 @@ bool TextContentsEqual(const FilePath& filename1, const FilePath& filename2) {
 }
 
 bool ReadFileToString(const FilePath& path, std::string* contents) {
+  if (path.ReferencesParent())
+    return false;
   FILE* file = OpenFile(path, "rb");
   if (!file) {
     return false;
