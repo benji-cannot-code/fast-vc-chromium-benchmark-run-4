fcb437544517183504dc5024f41bfd274d15233e
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 10:02:58 +0000

    Revert 175753 > Add a pass/fail test to verify that GPU memory usage doesn't grow beyond an acceptable level when using CSS (managed memory) and WebGL (unmanaged memory). > > BUG=135525 > > Review URL: https://chromiumcodereview.appspot.com/11667030
    This is causing linux debug compilation failures
    
    TBR=ccameron@chromium.org
    Review URL: https://codereview.chromium.org/11826022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175759 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/gpu_memory_stats.cc
+++ content/public/common/gpu_memory_stats.cc
@@ -7,9 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-GPUVideoMemoryUsageStats::GPUVideoMemoryUsageStats()
-    : bytes_allocated(0),
-      bytes_allocated_historical_max(0) {
+GPUVideoMemoryUsageStats::GPUVideoMemoryUsageStats() {
 }
 
 GPUVideoMemoryUsageStats::~GPUVideoMemoryUsageStats() {
