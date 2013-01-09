1f03fe7947a5f7a1aabe58fe588cd3f1d58e60c8
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 20:06:49 +0000

    Adding a missing header.
    The official build was broken by r175584:
    > host\desktop_process_main.cc(114): error C2039: 'InitializeCrashReporting' : is not a member of 'remoting'
    > host\desktop_process_main.cc(114): error C3861: 'InitializeCrashReporting': identifier not found
    
    Only the official builders were affected since remoting::InitializeCrashReporting() call is guarded by "#ifdef OFFICIAL_BUILD".
    
    BUG=134694
    
    Review URL: https://codereview.chromium.org/11827024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175865 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/desktop_process_main.cc
+++ remoting/host/desktop_process_main.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "base/win/windows_version.h"
 #include "remoting/base/auto_thread_task_runner.h"
+#include "remoting/base/breakpad.h"
 #include "remoting/host/desktop_process.h"
 #include "remoting/host/host_exit_codes.h"
 #include "remoting/host/logging.h"
