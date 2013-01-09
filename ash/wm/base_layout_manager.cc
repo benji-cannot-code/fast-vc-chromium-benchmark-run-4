a62c18a5a47a4d3accf99c5e3545b4d16029d6b9
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 00:49:30 +0000

    Remove ash::window::Show() from  BaseLayoutManager::OnWindowActivated
    This fixes the debug crash here:
    DCHECK(!wm::IsWindowMinimized(gained_active))
    
    This is testing kShowStateKey that has not yet been changed to the expected value.
    
    As far as I can tell, when called from OnWindowActivated is already from ash::window::Show
    when window.cc calls into the delegate:
       delegate_->OnWindowTargetVisibilityChanged(visible);
    
    Which triggers BrowserView::RestoreFocus which triggers a cascade of focus notifications
    ending up in aura::FocusManager::FocusWindow which calls ActivationController::ActivateWindow
    which calls BaseLayoutManager::OnWindowActivated
    
    In other word we re-enter and as a side effect the kShowStateKey has not yet been set.
    
    Full stack in the bug.
    
    BUG=166952
    TEST=see bug
    Review URL: https://codereview.chromium.org/11633029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175645 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/base_layout_manager.cc
+++ ash/wm/base_layout_manager.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/ui_base_types.h"
 #include "ui/compositor/layer.h"
 #include "ui/gfx/screen.h"
+#include "ui/views/corewm/corewm_switches.h"
 #include "ui/views/corewm/window_util.h"
 
 namespace ash {
@@ -155,9 +156,11 @@ void BaseLayoutManager::OnWindowDestroying(aura::Window* window) {
 
 void BaseLayoutManager::OnWindowActivated(aura::Window* gained_active,
                                           aura::Window* lost_active) {
-  if (gained_active && wm::IsWindowMinimized(gained_active)) {
-    gained_active->Show();
-    DCHECK(!wm::IsWindowMinimized(gained_active));
+  if (views::corewm::UseFocusController()) {
+    if (gained_active && wm::IsWindowMinimized(gained_active)) {
+      gained_active->Show();
+      DCHECK(!wm::IsWindowMinimized(gained_active));
+    }
   }
 }
 
