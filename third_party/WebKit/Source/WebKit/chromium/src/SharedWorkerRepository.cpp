ed147efc2898ddad28e8ef92153441175d777226
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-09 23:12:19 +0000

    [Chromium] Remove stray references to WebKitPlatformSupport.h https://bugs.webkit.org/show_bug.cgi?id=106493
    Reviewed by James Robinson.
    
    Part of a refactoring series. See tracking bug 82948.
    
    * src/SharedWorkerRepository.cpp:
    * tests/RunAllTests.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139246 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/SharedWorkerRepository.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/SharedWorkerRepository.cpp
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKit.h"
 #include "WebSharedWorker.h"
 #include "WebSharedWorkerRepository.h"
-#include "platform/WebKitPlatformSupport.h"
 #include "WorkerScriptLoader.h"
 #include "WorkerScriptLoaderClient.h"
 
