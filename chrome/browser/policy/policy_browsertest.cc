d3f13f2e831ee816fec37567a829d49f41f6c167
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 01:47:19 +0000

    Remove call to IsInstantExtendedAPIGoogleSearchUrl from ToolbarModel.
    Adds a mechanism to TemplateURL to optionally allow search term extraction only when a given key is present (for Google, this is "espv"). Uses this to ensure ToolbarModel no longer relies on the hardcoded "espv" parameter.
    
    Depends on:
      https://codereview.chromium.org/11552020/
    
    BUG=161602
    
    
    Review URL: https://chromiumcodereview.appspot.com/11576044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175656 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -790,6 +790,8 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, ReplaceSearchTerms) {
   const std::string kAlternateURL0(
       "https://www.google.com/search#q={searchTerms}");
   const std::string kAlternateURL1("https://www.google.com/#q={searchTerms}");
+  const std::string kSearchTermsReplacementKey(
+      "{google:instantExtendedEnabledKey}");
 
   TemplateURLService* service = TemplateURLServiceFactory::GetForProfile(
       browser()->profile());
@@ -816,6 +818,9 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, ReplaceSearchTerms) {
   alternate_urls->AppendString(kAlternateURL1);
   policies.Set(key::kDefaultSearchProviderAlternateURLs, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER, alternate_urls);
+  policies.Set(key::kDefaultSearchProviderSearchTermsReplacementKey,
+               POLICY_LEVEL_MANDATORY, POLICY_SCOPE_USER,
+               base::Value::CreateStringValue(kSearchTermsReplacementKey));
   provider_.UpdateChromePolicy(policies);
   default_search = service->GetDefaultSearchProvider();
   ASSERT_TRUE(default_search);
