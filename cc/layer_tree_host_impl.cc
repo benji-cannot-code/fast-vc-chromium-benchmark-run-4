79d7acb244af21307e89f6ddea0c101ecdd71deb
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 07:41:35 +0000

    cc: Preserve currently scrolling layer during tree activation
    BUG=168421
    
    
    Review URL: https://chromiumcodereview.appspot.com/11826017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175729 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_impl.cc
+++ cc/layer_tree_host_impl.cc
@@ -1026,6 +1026,8 @@ void LayerTreeHostImpl::activatePendingTreeIfNeeded()
 void LayerTreeHostImpl::activatePendingTree()
 {
     CHECK(m_pendingTree);
+
+    m_activeTree->PushPersistedState(m_pendingTree.get());
     m_activeTree.swap(m_pendingTree);
     // TODO(enne): consider recycling this tree to prevent layer churn
     m_pendingTree.reset();
