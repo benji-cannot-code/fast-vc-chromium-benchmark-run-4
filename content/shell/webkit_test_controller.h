3f0c2f7ca7112e9aa13b0ab4cfba0b705d214e3b
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 13:22:37 +0000

    [content shell] report the PID of a crashed renderer, and always report crashes
    BUG=111316
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/11819028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175789 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_controller.h
+++ content/shell/webkit_test_controller.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_path.h"
 #include "base/synchronization/lock.h"
 #include "base/threading/non_thread_safe.h"
+#include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/render_view_host_observer.h"
 #include "content/public/browser/web_contents_observer.h"
 #include "webkit/glue/webpreferences.h"
@@ -66,7 +68,8 @@ class WebKitTestResultPrinter {
 };
 
 class WebKitTestController : public base::NonThreadSafe,
-                             public WebContentsObserver {
+                             public WebContentsObserver,
+                             public NotificationObserver {
  public:
   static WebKitTestController* Get();
 
@@ -101,6 +104,11 @@ class WebKitTestController : public base::NonThreadSafe,
   virtual void RenderViewCreated(RenderViewHost* render_view_host) OVERRIDE;
   virtual void RenderViewGone(base::TerminationStatus status) OVERRIDE;
   virtual void WebContentsDestroyed(WebContents* web_contents) OVERRIDE;
+
+  // NotificationObserver implementation.
+  virtual void Observe(int type,
+                       const NotificationSource& source,
+                       const NotificationDetails& details) OVERRIDE;
 
  private:
   static WebKitTestController* instance_;
@@ -137,6 +145,8 @@ class WebKitTestController : public base::NonThreadSafe,
 
   Shell* main_window_;
 
+  int current_pid_;
+
   bool enable_pixel_dumping_;
   std::string expected_pixel_hash_;
 
@@ -157,6 +167,8 @@ class WebKitTestController : public base::NonThreadSafe,
   // Access to the following variables needs to be guarded by |lock_|.
   mutable base::Lock lock_;
   bool can_open_windows_;
+
+  NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(WebKitTestController);
 };
