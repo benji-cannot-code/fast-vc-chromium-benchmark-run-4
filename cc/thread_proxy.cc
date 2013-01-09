876cdb246aff1a3c851cd2f742ac10d2f9ee1442
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 00:28:11 +0000

    ThreadedTest that a non-null OutputSurface that fails initialization immediately doesn't crash
    Unit test to catch things like bug 168691
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11782026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175632 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/thread_proxy.cc
+++ cc/thread_proxy.cc
@@ -1014,7 +1014,8 @@ bool ThreadProxy::commitPendingForTesting()
 void ThreadProxy::commitPendingOnImplThreadForTesting(CommitPendingRequest* request)
 {
     DCHECK(isImplThread());
-    request->commitPending = m_schedulerOnImplThread->commitPending();
+    if (m_layerTreeHostImpl->outputSurface())
+        request->commitPending = m_schedulerOnImplThread->commitPending();
     request->completion.signal();
 }
 
