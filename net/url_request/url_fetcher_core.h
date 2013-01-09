c567c633a6fae4433308398c45785e67fc14ce26
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 22:02:24 +0000

    Added --lso-host switch that lets us override hosts for LSO URLs.
    BUG=168602
    TEST=existing unittests
    
    Review URL: https://codereview.chromium.org/11786006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175900 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_fetcher_core.h
+++ net/url_request/url_fetcher_core.h
@@ -125,6 +125,9 @@ class URLFetcherCore
   virtual void OnResponseStarted(URLRequest* request) OVERRIDE;
   virtual void OnReadCompleted(URLRequest* request,
                                int bytes_read) OVERRIDE;
+  virtual void OnCertificateRequested(
+      URLRequest* request,
+      SSLCertRequestInfo* cert_request_info) OVERRIDE;
 
   // Overridden from NetworkChangeNotifier::ConnectionTypeObserver:
   virtual void OnConnectionTypeChanged(
