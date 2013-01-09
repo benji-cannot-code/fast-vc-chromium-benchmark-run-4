d97d2d704cfa04c47f997227cf1f3de026210c46
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-09 03:23:05 +0000

    cc: Implement DelegatingRender::drawFrame() method.
    The method sends the render passes it is given to the parent compositor via
    the output surface. It should include a valid frame metadata with it.
    
    Tests:
    DelegatingRendererTestDraw.runSingleThread
    DelegatingRendererTestDraw.runMultiThread
    DelegatingRendererTestResources.runSingleThread
    DelegatingRendererTestResources.runMultiThread
    
    R=piman
    BUG=123444
    
    Review URL: https://chromiumcodereview.appspot.com/11777025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175690 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_unittest_animation.cc
+++ cc/layer_tree_host_unittest_animation.cc
@@ -181,7 +181,10 @@ class LayerTreeHostAnimationTestCheckerboardDoesNotStarveDraws :
       endTest();
   }
 
-  virtual bool prepareToDrawOnThread(LayerTreeHostImpl*) OVERRIDE {
+  virtual bool prepareToDrawOnThread(
+      LayerTreeHostImpl* host_impl,
+      LayerTreeHostImpl::FrameData& frame,
+      bool result) OVERRIDE {
     return false;
   }
 
--- cc/test/fake_output_surface.cc
+++ cc/test/fake_output_surface.cc
@@ -8,13 +8,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 FakeOutputSurface::FakeOutputSurface(
-    scoped_ptr<WebKit::WebGraphicsContext3D> context3d, bool has_parent) {
+    scoped_ptr<WebKit::WebGraphicsContext3D> context3d, bool has_parent)
+    : num_sent_frames_(0) {
   context3d_ = context3d.Pass();
   capabilities_.has_parent_compositor = has_parent;
 }
 
 FakeOutputSurface::FakeOutputSurface(
-    scoped_ptr<SoftwareOutputDevice> software_device, bool has_parent) {
+    scoped_ptr<SoftwareOutputDevice> software_device, bool has_parent)
+    : num_sent_frames_(0) {
   software_device_ = software_device.Pass();
   capabilities_.has_parent_compositor = has_parent;
 }
@@ -44,6 +46,10 @@ SoftwareOutputDevice* FakeOutputSurface::SoftwareDevice() const {
   return software_device_.get();
 }
 
-void FakeOutputSurface::SendFrameToParentCompositor(const CompositorFrame&) {}
+void FakeOutputSurface::SendFrameToParentCompositor(
+    CompositorFrame* frame) {
+  frame->AssignTo(&last_sent_frame_);
+  ++num_sent_frames_;
+}
 
 }  // namespace cc
