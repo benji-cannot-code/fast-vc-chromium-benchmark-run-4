4379afee9735c457ef4e7e189aff0a2e078aa9b8
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 14:40:53 +0000

    CSSParser does not allow the absence of whitespace between "and" and "expression" https://bugs.webkit.org/show_bug.cgi?id=106458
    Reviewed by Antti Koivisto.
    
    Source/WebCore:
    
    The issue was that a construct similar to "and(max-width: 480px)" looks like a function call (token type FUNCTION), even though
    it is actually a MEDIA_AND followed by a parenthesized expression.
    
    Test: fast/css/media-rule-no-whitespace.html
    
    * css/CSSParser.cpp:
    (WebCore::CSSParser::detectFunctionTypeToken): Return if the detection has been successful.
    (WebCore::CSSParser::realLex): Test for media query tokens if function type detection has failed.
    * css/CSSParser.h:
    
    LayoutTests:
    
    * fast/css/media-rule-no-whitespace-expected.txt: Added.
    * fast/css/media-rule-no-whitespace.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139316 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.h
+++ third_party/WebKit/Source/WebCore/css/CSSParser.h
@@ -446,7 +446,7 @@ private:
     template <typename CharacterType>
     bool parseNthChildExtra();
     template <typename CharacterType>
-    inline void detectFunctionTypeToken(int);
+    inline bool detectFunctionTypeToken(int);
     template <typename CharacterType>
     inline void detectMediaQueryToken(int);
     template <typename CharacterType>
