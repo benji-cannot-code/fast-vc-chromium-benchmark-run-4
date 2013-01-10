d83fac704825346e71eb61a5471a39a122ad7305
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 18:39:05 +0000

    Revert 176089 This patch appears to be responsible for failures on multiple Chrome OS bots. > Only show start/stop mirroring button when there are multiple displays. > > Also, redirect the page to chrome://settings when only one monitor is > connected, so we can stop showing the dialog for now. In the future > we may decide to show the page for some reason. > > BUG=169124 > TEST=manually on parrot > > Review URL: https://codereview.chromium.org/11818043
    TBR=hshi@google.com
    Review URL: https://codereview.chromium.org/11824070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176113 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/display_options.js
+++ chrome/browser/resources/options/chromeos/display_options.js
@@ -836,7 +836,6 @@ cr.define('options', function() {
         $('selected-display-data-container').hidden = true;
         $('display-configuration-arrow').hidden = true;
         $('display-options-set-primary').hidden = true;
-        $('display-options-toggle-mirroring').hidden = true;
         return;
       }
 
@@ -875,8 +874,6 @@ cr.define('options', function() {
 
       $('display-options-set-primary').hidden =
           this.displays_[this.focusedIndex_].isPrimary;
-      $('display-options-toggle-mirroring').hidden =
-          (this.displays_.length <= 1);
     },
 
     /**
@@ -1054,11 +1051,6 @@ cr.define('options', function() {
      * @private
      */
     onDisplayChanged_: function(mirroring, displays, layout, offset) {
-      if (displays.length <= 1) {
-        OptionsPage.showDefaultPage();
-        return;
-      }
-
       this.mirroring_ = mirroring;
       this.layout_ = layout;
       this.offset_ = offset;
