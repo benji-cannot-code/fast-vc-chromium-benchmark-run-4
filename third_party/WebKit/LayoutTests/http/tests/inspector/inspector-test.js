d1848a0ffc6bfa89fad139dc5ba39371d086429d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 18:25:39 +0000

    Web Inspector: Refactor InspectorTest to create output interface https://bugs.webkit.org/show_bug.cgi?id=106231
    Patch by John J. Barton <johnjbarton@chromium.org> on 2013-01-10
    Reviewed by Pavel Feldman.
    
    Refactor InspectorTest to extract InspectorTest.Output, the API
    used between InspectorTest and the test output reciever (the
    test-script page by default).
    
    * http/tests/inspector/inspector-test.js:
    (initialize_InspectorTest.InspectorTest.Output.testComplete):
    (initialize_InspectorTest.InspectorTest.Output.addResult):
    (initialize_InspectorTest.InspectorTest.Output.clearResults):
    (initialize_InspectorTest.InspectorTest.completeTest):
    (initialize_InspectorTest.InspectorTest.addResult):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139332 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/inspector-test.js
@@ -13,6 +13,23 @@ console.log = consoleOutputHook.bind(InspectorTest, "log");
 console.error = consoleOutputHook.bind(InspectorTest, "error");
 console.info = consoleOutputHook.bind(InspectorTest, "info");
 
+InspectorTest.Output = {   // override in window.initialize_yourName
+    testComplete: function() 
+    {
+        RuntimeAgent.evaluate("didEvaluateForTestInFrontend(" + InspectorTest.completeTestCallId + ", \"\")", "test");
+    },
+
+    addResult: function(text) 
+    {
+        InspectorTest.evaluateInPage("output(unescape('" + escape(text) + "'))");
+    },
+    
+    clearResults: function() 
+    {
+        InspectorTest.evaluateInPage("clearOutput()");
+    }
+};
+
 InspectorTest.completeTest = function()
 {
     function testDispatchQueueIsEmpty() {
@@ -21,7 +38,7 @@ InspectorTest.completeTest = function()
             setTimeout(testDispatchQueueIsEmpty, 10);
             return;
         }
-        RuntimeAgent.evaluate("didEvaluateForTestInFrontend(" + InspectorTest.completeTestCallId + ", \"\")", "test");
+        InspectorTest.Output.testComplete();
     }
     testDispatchQueueIsEmpty();
 }
@@ -74,22 +91,12 @@ InspectorTest.addResult = function(text)
 {
     results.push(text);
     if (resultsSynchronized)
-        addResultToPage(text);
+        InspectorTest.Output.addResult(text);
     else {
-        clearResults();
+        InspectorTest.Output.clearResults();
         for (var i = 0; i < results.length; ++i)
-            addResultToPage(results[i]);
+            InspectorTest.Output.addResult(results[i]);
         resultsSynchronized = true;
-    }
-
-    function clearResults()
-    {
-        InspectorTest.evaluateInPage("clearOutput()");
-    }
-
-    function addResultToPage(text)
-    {
-        InspectorTest.evaluateInPage("output(unescape('" + escape(text) + "'))");
     }
 }
 
