e09be393a5efeff40df009dffc5870efd82d1499
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 03:55:33 +0000

    [JSC] REGRESSION(r135093): A form control with name=length overrides length property on form.elements https://bugs.webkit.org/show_bug.cgi?id=105775
    Reviewed by Sam Weinig.
    
    Source/WebCore: 
    
    Fixed the bug by respecting properties on ancestor classes.
    
    Test: fast/dom/collection-length-should-not-be-overridden.html
    
    * bindings/js/JSDOMBinding.h:
    (WebCore::getStaticValueSlotEntryWithoutCaching): Added.
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateGetOwnPropertySlotBody): Use getStaticValueSlotEntryWithoutCaching to climb up the class
    hierarchy.
    
    LayoutTests: 
    
    Added a regression tests for all known HTMLCollection sublcasses except HTMLNameCollection,
    which is used only to implement named getters on window and document objects and HTMLPropertiesCollection
    since it's not enabled on all ports yet.
    
    * fast/dom/collection-length-should-not-be-overridden-expected.txt: Added.
    * fast/dom/collection-length-should-not-be-overridden.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139278 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.h
+++ third_party/WebKit/Source/WebCore/bindings/js/JSDOMBinding.h
@@ -48,6 +48,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Noncopyable.h>
 #include <wtf/Vector.h>
 
+namespace JSC {
+
+class HashEntry;
+
+}
+
 namespace WebCore {
 
 class DOMStringList;
@@ -511,6 +517,21 @@ enum ParameterDefaultPolicy {
     inline AtomicString propertyNameToAtomicString(JSC::PropertyName propertyName)
     {
         return AtomicString(propertyName.publicName());
+    }
+
+    template <class ThisImp>
+    inline const JSC::HashEntry* getStaticValueSlotEntryWithoutCaching(JSC::ExecState* exec, JSC::PropertyName propertyName)
+    {
+        const JSC::HashEntry* entry = ThisImp::s_info.propHashTable(exec)->entry(exec, propertyName);
+        if (!entry) // not found, forward to parent
+            return getStaticValueSlotEntryWithoutCaching<typename ThisImp::Base>(exec, propertyName);
+        return entry;
+    }
+
+    template <>
+    inline const JSC::HashEntry* getStaticValueSlotEntryWithoutCaching<JSDOMWrapper>(JSC::ExecState*, JSC::PropertyName)
+    {
+        return 0;
     }
 
 } // namespace WebCore
