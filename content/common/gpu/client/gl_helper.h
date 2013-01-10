ed6c5916f7aca125d46a223ffd2c96a395a5de88
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 04:44:45 +0000

    Update some #includes in content/common/gpu for headers in the new Platform directory
    Review URL: https://chromiumcodereview.appspot.com/11824042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176023 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/gl_helper.h
+++ content/common/gpu/client/gl_helper.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebGraphicsContext3D.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebGraphicsContext3D.h"
 
 namespace gfx {
 class Rect;
