74b16c3d691b2f7a17676758065ee51a3dbb083a
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 15:06:23 +0000

    Update some #includes in chrome/browser/renderer_host for headers in the new Platform directory
    Review URL: https://chromiumcodereview.appspot.com/11820038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_render_message_filter.cc
+++ chrome/browser/renderer_host/chrome_render_message_filter.cc
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/process_type.h"
 #include "extensions/common/constants.h"
 #include "googleurl/src/gurl.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebSecurityOrigin.h"
 #include "webkit/plugins/npapi/plugin_list.h"
 
