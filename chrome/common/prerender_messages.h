d23dcd9edf181f60ac28c5d7f6c9baa6a78e013f
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 00:33:30 +0000

    Update some #includes in chrome/common for headers in the new Platform directory
    Review URL: https://chromiumcodereview.appspot.com/11827034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/prerender_messages.h
+++ chrome/common/prerender_messages.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_message.h"
 #include "ipc/ipc_message_macros.h"
 #include "ipc/ipc_param_traits.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebReferrerPolicy.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebReferrerPolicy.h"
 #include "ui/gfx/size.h"
 
 #define IPC_MESSAGE_START PrerenderMsgStart
