f3cb191c9d7e3d20c04b0f4e3512492f0d347553
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 17:45:02 +0000

    NaCl: Clean up file descriptor setup in nacl_helper on linux
    There is no need to be using dup2() to set up an FD with a fixed
    number; this risks overwriting an FD.  The point of
    base::GlobalDescriptors is that it provides a level of indirection
    that allows any FD number to be used.
    
    Remove kNaClBrowserDescriptor.  Remove the browserdesc argument that
    is not used for anything other than an assertion and so isn't needed.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=2096
    TEST=NaCl tests in browser_tests
    
    Review URL: https://codereview.chromium.org/11819021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176106 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/nacl_helper_linux.h
+++ chrome/common/nacl_helper_linux.h
@@ -19,17 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // For communications between NaCl loader and browser.
 // See also content/common/zygote_main_linux.cc and
 // http://code.google.com/p/chromium/wiki/LinuxZygote
-#define kNaClBrowserDescriptor 3
+
 // For communications between NaCl loader and zygote.
-// We put the kNaClZygoteDescriptor on 3 together with the
-// kNaClBrowserDescriptor. They are never used at the same
-// time, and this prevents /dev/urandom from using the fd.
 #define kNaClZygoteDescriptor 3
 // For communications between the NaCl loader process and
 // the SUID sandbox.
 #define kNaClSandboxDescriptor 5
-// NOTE: kNaClBrowserDescriptor and kNaClSandboxDescriptor must match
-// content/browser/zygote_main_linux.cc kBrowserDescriptor and
+// NOTE: kNaClSandboxDescriptor must match
+// content/browser/zygote_main_linux.cc
 // kMagicSandboxIPCDescriptor.
 
 // A fork request from the Zygote to the helper includes an array
