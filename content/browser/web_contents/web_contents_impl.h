0a1139b6bfe4ce90f182532749de59f9f3256bf5
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 02:19:09 +0000

    Removes some dead code from WebContentsImpl. message_box_active_ is no longer used, and ironically is causing a crash.
    BUG=169059
    TEST=none
    R=jam@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11833020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175990 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -30,10 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/size.h"
 #include "webkit/glue/resource_type.h"
 
-#if defined(OS_WIN)
-#include "base/win/scoped_handle.h"
-#endif
-
 struct BrowserPluginHostMsg_CreateGuest_Params;
 struct BrowserPluginHostMsg_ResizeGuest_Params;
 struct ViewMsg_PostMessage_Params;
@@ -791,13 +787,6 @@ class CONTENT_EXPORT WebContentsImpl
   // Pointer to the JavaScript dialog creator, lazily assigned. Used because the
   // delegate of this WebContentsImpl is nulled before its destructor is called.
   JavaScriptDialogCreator* dialog_creator_;
-
-#if defined(OS_WIN)
-  // Handle to an event that's set when the page is showing a message box (or
-  // equivalent constrained window).  Plugin processes check this to know if
-  // they should pump messages then.
-  base::win::ScopedHandle message_box_active_;
-#endif
 
   // Set to true when there is an active "before unload" dialog.  When true,
   // we've forced the throbber to start in Navigate, and we need to remember to
