dcb7b70b613f9435e47ec918bb954814480cd22f
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 04:45:52 +0000

    Remove flags for nonFastScrollable and touchEventHandler regions getting changed
    Currently, two flags are controlling whether we push these regions to layerImpl from
    layer and with implSidePainting, we end up not pushing the regions at all since the
    flags are flipped back after pusing them once. Removing both flags to make sure we
    get the regions carried over.
    
    BUG=168929
    
    
    Review URL: https://chromiumcodereview.appspot.com/11829041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176024 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer.cc
+++ cc/layer.cc
@@ -36,8 +36,6 @@ Layer::Layer()
     , m_scrollable(false)
     , m_shouldScrollOnMainThread(false)
     , m_haveWheelEventHandlers(false)
-    , m_nonFastScrollableRegionChanged(false)
-    , m_touchEventHandlerRegionChanged(false)
     , m_anchorPoint(0.5, 0.5)
     , m_backgroundColor(0)
     , m_opacity(1.0)
@@ -484,7 +482,6 @@ void Layer::setNonFastScrollableRegion(const Region& region)
     if (m_nonFastScrollableRegion == region)
         return;
     m_nonFastScrollableRegion = region;
-    m_nonFastScrollableRegionChanged = true;
     setNeedsCommit();
 }
 
@@ -493,7 +490,6 @@ void Layer::setTouchEventHandlerRegion(const Region& region)
     if (m_touchEventHandlerRegion == region)
         return;
     m_touchEventHandlerRegion = region;
-    m_touchEventHandlerRegionChanged = true;
 }
 
 void Layer::setDrawCheckerboardForMissingTiles(bool checkerboard)
@@ -602,16 +598,8 @@ void Layer::pushPropertiesTo(LayerImpl* layer)
     layer->setScrollable(m_scrollable);
     layer->setShouldScrollOnMainThread(m_shouldScrollOnMainThread);
     layer->setHaveWheelEventHandlers(m_haveWheelEventHandlers);
-    // Copying a Region is more expensive than most layer properties, since it involves copying two Vectors that may be
-    // arbitrarily large depending on page content, so we only push the property if it's changed.
-    if (m_nonFastScrollableRegionChanged) {
-        layer->setNonFastScrollableRegion(m_nonFastScrollableRegion);
-        m_nonFastScrollableRegionChanged = false;
-    }
-    if (m_touchEventHandlerRegionChanged) {
-        layer->setTouchEventHandlerRegion(m_touchEventHandlerRegion);
-        m_touchEventHandlerRegionChanged = false;
-    }
+    layer->setNonFastScrollableRegion(m_nonFastScrollableRegion);
+    layer->setTouchEventHandlerRegion(m_touchEventHandlerRegion);
     layer->setContentsOpaque(m_contentsOpaque);
     if (!opacityIsAnimating())
         layer->setOpacity(m_opacity);
--- cc/layer.h
+++ cc/layer.h
@@ -166,11 +166,9 @@ public:
     bool haveWheelEventHandlers() const { return m_haveWheelEventHandlers; }
 
     void setNonFastScrollableRegion(const Region&);
-    void setNonFastScrollableRegionChanged() { m_nonFastScrollableRegionChanged = true; }
     const Region& nonFastScrollableRegion() const { return m_nonFastScrollableRegion; }
 
     void setTouchEventHandlerRegion(const Region&);
-    void setTouchEventHandlerRegionChanged() { m_touchEventHandlerRegionChanged = true; }
     const Region& touchEventHandlerRegion() const { return m_touchEventHandlerRegion; }
 
     void setLayerScrollClient(WebKit::WebLayerScrollClient* layerScrollClient) { m_layerScrollClient = layerScrollClient; }
@@ -365,9 +363,7 @@ private:
     bool m_shouldScrollOnMainThread;
     bool m_haveWheelEventHandlers;
     Region m_nonFastScrollableRegion;
-    bool m_nonFastScrollableRegionChanged;
     Region m_touchEventHandlerRegion;
-    bool m_touchEventHandlerRegionChanged;
     gfx::PointF m_position;
     gfx::PointF m_anchorPoint;
     SkColor m_backgroundColor;
