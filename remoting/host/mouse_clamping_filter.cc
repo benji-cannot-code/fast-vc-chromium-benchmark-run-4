fcfe5431c4657a8bee4b7ea2aa56369fbf942650
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 23:49:51 +0000

    The client's connection video stub is created only after the connection has been authenticated. This CL makes sure that the client session connects the video stub after if what created.
    BUG=169126
    
    Review URL: https://codereview.chromium.org/11827055
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176214 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/mouse_clamping_filter.cc
+++ remoting/host/mouse_clamping_filter.cc
@@ -11,10 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace remoting {
 
 MouseClampingFilter::MouseClampingFilter(
-    protocol::InputStub* input_stub,
-    protocol::VideoStub* video_stub)
+    protocol::InputStub* input_stub)
     : input_filter_(input_stub),
-      video_stub_(video_stub) {
+      video_stub_(NULL) {
 }
 
 MouseClampingFilter::~MouseClampingFilter() {
