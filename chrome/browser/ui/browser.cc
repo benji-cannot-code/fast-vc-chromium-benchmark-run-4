2d55f9c02690447192cfe63d920afcaed3bc2038
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 01:28:06 +0000

    Remove DeviceAttachedIntentSource.
    R=groby@chromium.org
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11828026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@175969 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -61,7 +61,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/file_select_helper.h"
 #include "chrome/browser/first_run/first_run.h"
 #include "chrome/browser/google/google_url_tracker.h"
-#include "chrome/browser/intents/device_attached_intent_source.h"
 #include "chrome/browser/intents/register_intent_handler_infobar_delegate.h"
 #include "chrome/browser/intents/web_intents_reporting.h"
 #include "chrome/browser/intents/web_intents_util.h"
@@ -426,12 +425,6 @@ Browser::Browser(const CreateParams& params)
 
   if (is_type_tabbed())
     instant_controller_.reset(new chrome::BrowserInstantController(this));
-
-#if 0
-  // Disabled for M22. See http://crbug.com/144326.
-  device_attached_intent_source_.reset(
-      new DeviceAttachedIntentSource(this, (this)));
-#endif
 
   UpdateBookmarkBarState(BOOKMARK_BAR_STATE_CHANGE_INIT);
 
