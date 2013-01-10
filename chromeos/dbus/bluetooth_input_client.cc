b6fe5e4092ca6772fdafacddaefb9b4f1ec713db
deymo@chromium.org <deymo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 19:42:50 +0000

    Bluetooth: Fix error_message parsing on a dbus connect error response.
    Fix the same incorrect use of "Message::PopString" for Connect previously
    fixed for CreateDevice and CreatePairedDevice. grep doesn't show any other missing
    usage to fix. See previous crrev.com/11669016 .
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/11734014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176131 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/bluetooth_input_client.cc
+++ chromeos/dbus/bluetooth_input_client.cc
@@ -219,7 +219,7 @@ class BluetoothInputClientImpl: public BluetoothInputClient,
     if (response) {
       dbus::MessageReader reader(response);
       error_name = response->GetErrorName();
-      error_message = reader.PopString(&error_message);
+      reader.PopString(&error_message);
     } else {
       error_name = kNoResponseError;
       error_message = "";
