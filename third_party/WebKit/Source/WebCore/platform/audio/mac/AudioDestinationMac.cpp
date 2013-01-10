9dec19fb09af331e4cb55a077f7bdb2fcff1becb
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 01:40:44 +0000

    Allow live/local audio input to be enabled only when needed https://bugs.webkit.org/show_bug.cgi?id=106490
    Source/Platform:
    
    Reviewed by Kenneth Russell.
    
    * chromium/public/Platform.h:
    (Platform):
    (WebKit::Platform::createAudioDevice):
    
    Source/WebCore:
    
    Reviewed by Kenneth Russell.
    
    WebAudio can process live/local audio input using a MediaStreamAudioSourceNode.
    But currently the audio back-end is not able to know when/if audio input will be
    needed, so it needs to assume the worst and initialize the system to support potential
    audio input in all cases.  For some audio back-ends this can end up being less efficient
    than initializing for audio output only.  This patch adds the ability for the audio back-end
    to be able to initialize itself for audio input later on, only when/if it's needed.
    
    * Modules/webaudio/AudioContext.cpp:
    (WebCore::AudioContext::createMediaStreamSource):
    * Modules/webaudio/AudioDestinationNode.h:
    (AudioDestinationNode):
    * Modules/webaudio/DefaultAudioDestinationNode.cpp:
    (WebCore::DefaultAudioDestinationNode::DefaultAudioDestinationNode):
    (WebCore::DefaultAudioDestinationNode::initialize):
    (WebCore::DefaultAudioDestinationNode::uninitialize):
    (WebCore::DefaultAudioDestinationNode::createDestination):
    (WebCore):
    (WebCore::DefaultAudioDestinationNode::enableInput):
    * Modules/webaudio/DefaultAudioDestinationNode.h:
    (DefaultAudioDestinationNode):
    * Modules/webaudio/OfflineAudioDestinationNode.h:
    (OfflineAudioDestinationNode):
    (WebCore::OfflineAudioDestinationNode::sampleRate):
    * platform/audio/AudioDestination.h:
    (AudioDestination):
    * platform/audio/gstreamer/AudioDestinationGStreamer.cpp:
    (WebCore::AudioDestination::create):
    * platform/audio/mac/AudioDestinationMac.cpp:
    (WebCore::AudioDestination::create):
    
    Source/WebKit/chromium:
    
    Reviewed by Kenneth Russell.
    
    * src/AudioDestinationChromium.cpp:
    (WebCore):
    (WebCore::AudioDestination::create):
    (WebCore::AudioDestinationChromium::AudioDestinationChromium):
    (WebCore::AudioDestinationChromium::render):
    * src/AudioDestinationChromium.h:
    (AudioDestinationChromium):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139262 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/mac/AudioDestinationMac.cpp
+++ third_party/WebKit/Source/WebCore/platform/audio/mac/AudioDestinationMac.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "AudioIOCallback.h"
 #include "FloatConversion.h"
+#include "Logging.h"
 #include "VectorMath.h"
 #include <CoreAudio/AudioHardware.h>
 
@@ -45,8 +46,16 @@ const float kLowThreshold = -1;
 const float kHighThreshold = 1;
 
 // Factory method: Mac-implementation
-PassOwnPtr<AudioDestination> AudioDestination::create(AudioIOCallback& callback, float sampleRate)
+PassOwnPtr<AudioDestination> AudioDestination::create(AudioIOCallback& callback, unsigned numberOfInputChannels, unsigned numberOfOutputChannels, float sampleRate)
 {
+    // FIXME: Add support for local/live audio input.
+    if (numberOfInputChannels)
+        LOG(Media, "AudioDestination::create(%u, %u, %f) - unhandled input channels", numberOfInputChannels, numberOfOutputChannels, sampleRate);
+
+    // FIXME: Add support for multi-channel (> stereo) output.
+    if (numberOfOutputChannels != 2)
+        LOG(Media, "AudioDestination::create(%u, %u, %f) - unhandled output channels", numberOfInputChannels, numberOfOutputChannels, sampleRate);
+
     return adoptPtr(new AudioDestinationMac(callback, sampleRate));
 }
 
