965766d2cdb4eea234d5157fdb0d0048b2cc44bc
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 00:09:41 +0000

    [GTK] accessibility/aria-labelledby-overrides-label.html requires a proper baseline https://bugs.webkit.org/show_bug.cgi?id=105638
    Patch by Joanmarie Diggs <jdiggs@igalia.com> on 2013-01-09
    Reviewed by Martin Robinson.
    
    The test was failing for two reasons:
    - AccessibilityRenderObject::correspondingLabelForControlElement() was
      not ignoring the ARIA labelled-by property
    - AccessibilityController::accessibleElementById() was not implemented
    
    Because getting an element by ID cannot be done in the UIProcess, the
    decision was made to expose the element's ID as an accessible attribute
    of the object.
    
    In addition, fixing the bug in AccessibilityRenderObject made it possible
    to eliminate the Gtk platform-specific expectations for another test.
    
    Source/WebCore:
    
    No new tests; instead, the failing test was unskipped and a proper baseline
    provided.
    
    * accessibility/AccessibilityRenderObject.cpp:
    (WebCore::AccessibilityRenderObject::correspondingLabelForControlElement):
    Return null for objects where hasTextAlternative() is true.
    * accessibility/atk/WebKitAccessibleWrapperAtk.cpp:
    (webkitAccessibleGetAttributes): Expose the element's ID as an accessible
    attribute.
    
    Tools:
    
    * DumpRenderTree/gtk/AccessibilityControllerGtk.cpp:
    (childElementById): Added. Walks the tree looking for the AtkObject
    Attribute which exposes the HTML element's ID.
    (AccessibilityController::accessibleElementById): Implemented.
    * DumpRenderTree/gtk/AccessibilityUIElementGtk.cpp:
    (AccessibilityUIElement::title): Modified to print out "AXTitle: " even
    when there is no title, as is done with the Mac port.
    * WebKitTestRunner/InjectedBundle/AccessibilityController.cpp:
    (WTR):
    * WebKitTestRunner/InjectedBundle/atk/AccessibilityControllerAtk.cpp:
    (WTR::childElementById): Added. Walks the tree looking for the AtkObject
    Attribute which exposes the HTML element's ID.
    (WTR):
    (WTR::AccessibilityController::accessibleElementById): Implemented.
    * WebKitTestRunner/InjectedBundle/atk/AccessibilityUIElementAtk.cpp:
    (WTR::AccessibilityUIElement::title): Modified to print out "AXTitle: "
    even when there is no title, as is done with the Mac port.
    
    LayoutTests:
    
    * accessibility/aria-labelledby-overrides-label-expected.txt: Added.
    * accessibility/radio-button-title-label-expected.txt: Modified based on
    null check which was added and which gets included in the test output.
    * accessibility/radio-button-title-label.html: Check to see that we have
    a titleUIElement before attempting to get its title.
    * platform/gtk/TestExpectations: Unskipped the previously failing test.
    * platform/gtk/accessibility/radio-button-title-label-expected.txt: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/gtk/AccessibilityControllerGtk.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/gtk/AccessibilityControllerGtk.cpp
@@ -36,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <atk/atk.h>
 #include <gtk/gtk.h>
 #include <webkit/webkit.h>
+#include <wtf/gobject/GOwnPtr.h>
 
 static bool loggingAccessibilityEvents = false;
 
@@ -71,10 +72,47 @@ AccessibilityUIElement AccessibilityController::rootElement()
     return AccessibilityUIElement(accessible);
 }
 
+static AtkObject* childElementById(AtkObject* parent, const char* id)
+{
+    if (!ATK_IS_OBJECT(parent))
+        return 0;
+
+    AtkAttributeSet* attributeSet = atk_object_get_attributes(parent);
+    for (GSList* attributes = attributeSet; attributes; attributes = attributes->next) {
+        AtkAttribute* attribute = static_cast<AtkAttribute*>(attributes->data);
+        if (!strcmp(attribute->name, "html-id")) {
+            if (!strcmp(attribute->value, id))
+                return parent;
+            break;
+        }
+    }
+
+    int childCount = atk_object_get_n_accessible_children(parent);
+    for (int i = 0; i < childCount; i++) {
+        AtkObject* result = childElementById(atk_object_ref_accessible_child(parent, i), id);
+        if (ATK_IS_OBJECT(result))
+            return result;
+    }
+
+    return 0;
+}
+
 AccessibilityUIElement AccessibilityController::accessibleElementById(JSStringRef id)
 {
-    // FIXME: implement
+    AtkObject* root = DumpRenderTreeSupportGtk::getRootAccessibleElement(mainFrame);
+    if (!root)
+        return 0;
+
+    size_t bufferSize = JSStringGetMaximumUTF8CStringSize(id);
+    GOwnPtr<gchar> idBuffer(static_cast<gchar*>(g_malloc(bufferSize)));
+    JSStringGetUTF8CString(id, idBuffer.get(), bufferSize);
+
+    AtkObject* result = childElementById(root, idBuffer.get());
+    if (ATK_IS_OBJECT(result))
+        return AccessibilityUIElement(result);
+
     return 0;
+
 }
 
 void AccessibilityController::setLogFocusEvents(bool)
