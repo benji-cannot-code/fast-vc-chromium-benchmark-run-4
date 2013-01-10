27ec3e290fc38593d3242afe12745ae48ce31579
victor@rosedu.org <victor@rosedu.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 17:45:41 +0000

    media/video-controls-captions.html fails after fixing https://bugs.webkit.org/show_bug.cgi?id=105536 https://bugs.webkit.org/show_bug.cgi?id=106230
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Until Chromium has proper controls for lists of text tracks, the caption button
    needs to preserve its functionality.
    
    Updated TestExpectations to reactivate test.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::setClosedCaptionsVisible): Added explicit call to updateTextTrackDisplay.
    This enables hiding the tracks immediately.
    * html/shadow/MediaControlElements.cpp:
    (WebCore::MediaControlTextTrackContainerElement::updateDisplay): Added extra check before rendering
    that captions should actually be displayed.
    
    LayoutTests:
    
    * platform/chromium/TestExpectations: Reactivated test.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139326 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
@@ -4137,6 +4137,7 @@ void HTMLMediaElement::setClosedCaptionsVisible(bool closedCaptionVisible)
         m_disableCaptions = !m_closedCaptionsVisible;
 
         markCaptionAndSubtitleTracksAsUnconfigured();
+        mediaControls()->updateTextTrackDisplay();
     }
 #else
     if (hasMediaControls())
