6c299f3e86ad922419f22f98e2fcc0df0850d763
ricow@chromium.org <ricow@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-10 14:33:16 +0000

    Revert 176061 > LTHI's bg animation timer should set the fake thread to impl. > > The single thread proxy expects this to be true. > > BUG=165399 > > > Review URL: https://chromiumcodereview.appspot.com/11532009 This is causing failures on mac:
    http://build.chromium.org/p/chromium.mac/buildstatus?builder=Mac%2010.6%20Tests%20%28dbg%29%281%29&number=32119
    http://build.chromium.org/p/chromium.mac/buildstatus?builder=Mac%2010.7%20Tests%20%28dbg%29%281%29&number=5885
    
    TBR=vollick@chromium.org
    Review URL: https://codereview.chromium.org/11820049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176075 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_impl.cc
+++ cc/layer_tree_host_impl.cc
@@ -72,7 +72,6 @@ public:
 
     virtual void onTimerTick() OVERRIDE
     {
-        DebugScopedSetImplThread setImplThread(m_layerTreeHostImpl->proxy());
         m_layerTreeHostImpl->animate(base::TimeTicks::Now(), base::Time::Now());
     }
 
