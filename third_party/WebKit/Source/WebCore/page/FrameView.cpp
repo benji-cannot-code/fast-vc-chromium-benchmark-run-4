0ba6f4ac3821d92390d28d331e360d149e70e858
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 21:52:40 +0000

    Regression(r129944): Heap-use-after-free in WebCore::computeNonFastScrollableRegion https://bugs.webkit.org/show_bug.cgi?id=99515
    Reviewed by Simon Fraser.
    
    The object used-after-freed is a destructed FrameView that is still in the m_scrollableAreas set of the parent FrameView. Actually it has been removed from m_scrollableAreas when setParent(0), but then is added back in updateScrollableAreaSet() because its frameViewParent() is still not 0 (though parent() is already 0).
    
    No new tests. The heap-use-after-free doesn't always cause crash so it can't be stably tested with a test case. Memory analysis tools like asan discovered the heap-use-after-free and verified that the patch can fix the issue.
    
    * page/FrameView.cpp:
    (WebCore::FrameView::parentFrameView): Checks if the FrameView has been removed from the parent.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FrameView.cpp
+++ third_party/WebKit/Source/WebCore/page/FrameView.cpp
@@ -3110,6 +3110,9 @@ bool FrameView::hasCustomScrollbars() const
 
 FrameView* FrameView::parentFrameView() const
 {
+    if (!parent())
+        return 0;
+
     if (Frame* parentFrame = m_frame->tree()->parent())
         return parentFrame->view();
 
