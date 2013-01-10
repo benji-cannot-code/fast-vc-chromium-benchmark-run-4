f45e65e550b7f902aa8d6d6e2c34ed7bb27c2b27
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-10 02:16:15 +0000

    Unreviewed, fix build after http://trac.webkit.org/changeset/139262
    * Modules/webaudio/DefaultAudioDestinationNode.cpp:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139266 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/DefaultAudioDestinationNode.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/DefaultAudioDestinationNode.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DefaultAudioDestinationNode.h"
 
 #include "Logging.h"
+#include <wtf/MainThread.h>
 
 const unsigned EnabledInputChannels = 2;
 
