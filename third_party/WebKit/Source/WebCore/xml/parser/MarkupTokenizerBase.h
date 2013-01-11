5112d4b0ed12c9644e79c55e6375093f56852ba6
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 22:20:13 +0000

    We should be able to checkpoint and restore the HTMLTokenizer across threads https://bugs.webkit.org/show_bug.cgi?id=106597
    Based on patch by Adam Barth.
    
    This has the ability to create a checkpoint any time the parser is blocked on a script.
    We clear m_appropriateEndTagName after each end tag is flushed so that the ASSERT in
    canCreateCheckpoint() will pass.
    
    Reviewed by Adam Barth.
    
    No new tests because no new functionality.
    
    * html/parser/HTMLDocumentParser.cpp:
    (WebCore::HTMLDocumentParser::HTMLDocumentParser):
    (WebCore::HTMLDocumentParser::pumpTokenizer):
    * html/parser/HTMLDocumentParser.h:
    (WebCore):
    (HTMLDocumentParser):
    * html/parser/HTMLTokenizer.cpp:
    (WebCore):
    (WebCore::HTMLTokenizer::canCreateCheckpoint):
    (WebCore::HTMLTokenizer::createCheckpoint):
    (WebCore::HTMLTokenizer::restoreFromCheckpoint):
    * html/parser/HTMLTokenizer.h:
    (HTMLTokenizer):
    (Checkpoint):
    (WebCore::HTMLTokenizer::Checkpoint::Checkpoint):
    * xml/parser/MarkupTokenizerBase.h:
    (WebCore::MarkupTokenizerBase::InputStreamPreprocessor::InputStreamPreprocessor):
    (WebCore::MarkupTokenizerBase::InputStreamPreprocessor::skipNextNewLine):
    (InputStreamPreprocessor):
    (WebCore::MarkupTokenizerBase::InputStreamPreprocessor::reset):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139497 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/xml/parser/MarkupTokenizerBase.h
+++ third_party/WebKit/Source/WebCore/xml/parser/MarkupTokenizerBase.h
@@ -62,9 +62,8 @@ protected:
     public:
         InputStreamPreprocessor(MarkupTokenizerBase<Token, State>* tokenizer)
             : m_tokenizer(tokenizer)
-            , m_nextInputCharacter('\0')
-            , m_skipNextNewLine(false)
         {
+            reset();
         }
 
         ALWAYS_INLINE UChar nextInputCharacter() const { return m_nextInputCharacter; }
@@ -123,6 +122,14 @@ protected:
             if (source.isEmpty())
                 return false;
             return peek(source);
+        }
+
+        bool skipNextNewLine() const { return m_skipNextNewLine; }
+
+        void reset(bool skipNextNewLine = false)
+        {
+            m_nextInputCharacter = '\0';
+            m_skipNextNewLine = skipNextNewLine;
         }
 
         static const UChar endOfFileMarker = 0;
