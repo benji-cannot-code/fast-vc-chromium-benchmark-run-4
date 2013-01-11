b8947a608e87a5c48523427bc136794e3a9a546e
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 03:32:10 +0000

    extension: Get rid of some duplicate code from external_filesystem_apitest.cc
    We should use GetTestFilePath() and LoadJSONFile() in google_apis/test_util.h
    
    Along the way, remove mock_drive_service.h/cc from chrome_tests.gypi as these
    files are not used in tests defined in the gypi file.
    
    BUG=none
    TEST=none
    
    Review URL: https://codereview.chromium.org/11853002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176253 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/external_filesystem_apitest.cc
+++ chrome/browser/chromeos/extensions/external_filesystem_apitest.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_test_message_listener.h"
 #include "chrome/browser/google_apis/dummy_drive_service.h"
 #include "chrome/browser/google_apis/gdata_wapi_parser.h"
+#include "chrome/browser/google_apis/test_util.h"
 #include "chrome/browser/google_apis/time_util.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
@@ -45,14 +46,14 @@ namespace {
 const char kTestFileContent[] = "hello, world!";
 
 // Contains a folder entry for the folder 'Folder' that will be 'created'.
-const char kTestDirectory[] = "new_folder_entry.json";
+const char kTestDirectory[] = "gdata/new_folder_entry.json";
 
 // Contains a folder named Folder that has a file File.aBc inside of it.
-const char kTestRootFeed[] = "remote_file_system_apitest_root_feed.json";
+const char kTestRootFeed[] = "gdata/remote_file_system_apitest_root_feed.json";
 
 // Contains metadata of the  document that will be "downloaded" in test.
 const char kTestDocumentToDownloadEntry[] =
-    "remote_file_system_apitest_document_to_download.json";
+    "gdata/remote_file_system_apitest_document_to_download.json";
 
 // The ID of the file browser extension.
 const char kFileBrowserExtensionId[] = "ddammdhioacbehjngdmkjcjbnfginlla";
@@ -87,33 +88,6 @@ class BackgroundObserver {
   content::WindowedNotificationObserver page_created_;
   content::WindowedNotificationObserver page_closed_;
 };
-
-// TODO(tbarzic): We should probably share GetTestFilePath and LoadJSONFile
-// with drive_file_system_unittest.
-// Generates file path in gdata test directory for a file with name |filename|.
-FilePath GetTestFilePath(const FilePath::StringType& filename) {
-  FilePath path;
-  std::string error;
-  PathService::Get(chrome::DIR_TEST_DATA, &path);
-   path = path.AppendASCII("chromeos")
-       .AppendASCII("gdata")
-       .AppendASCII(filename);
-  EXPECT_TRUE(file_util::PathExists(path)) <<
-      "Couldn't find " << path.value();
-  return path;
-}
-
-// Loads and deserializes a json file in gdata test directory whose name is
-// |filename|. Returns new Value object the file is deserialized to.
-base::Value* LoadJSONFile(const std::string& filename) {
-  FilePath path = GetTestFilePath(filename);
-  std::string error;
-  JSONFileValueSerializer serializer(path);
-  Value* value = serializer.Deserialize(NULL, &error);
-  EXPECT_TRUE(value) <<
-      "Parse error " << path.value() << ": " << error;
-  return value;
-}
 
 // Adds a next feed URL property to the given feed value.
 bool AddNextFeedURLToFeedValue(const std::string& url, base::Value* feed) {
@@ -154,7 +128,8 @@ class FakeDriveService : public google_apis::DummyDriveService {
       bool shared_with_me,
       const std::string& directory_resource_id,
       const google_apis::GetResourceListCallback& callback) OVERRIDE {
-    scoped_ptr<base::Value> value(LoadJSONFile(kTestRootFeed));
+    scoped_ptr<base::Value> value(
+        google_apis::test_util::LoadJSONFile(kTestRootFeed));
     if (!search_string.empty()) {
       // Search results will be returned in two parts:
       // 1. Search will be given empty initial feed url. The returned feed will
@@ -184,7 +159,7 @@ class FakeDriveService : public google_apis::DummyDriveService {
     EXPECT_EQ("file:1_file_resource_id", resource_id);
 
     scoped_ptr<base::Value> file_to_download_value(
-        LoadJSONFile(kTestDocumentToDownloadEntry));
+        google_apis::test_util::LoadJSONFile(kTestDocumentToDownloadEntry));
     scoped_ptr<google_apis::ResourceEntry> file_to_download(
         google_apis::ResourceEntry::ExtractAndParse(*file_to_download_value));
 
@@ -208,7 +183,8 @@ class FakeDriveService : public google_apis::DummyDriveService {
       const GURL& parent_content_url,
       const FilePath::StringType& directory_name,
       const google_apis::GetResourceEntryCallback& callback) OVERRIDE {
-    scoped_ptr<base::Value> dir_value(LoadJSONFile(kTestDirectory));
+    scoped_ptr<base::Value> dir_value(
+        google_apis::test_util::LoadJSONFile(kTestDirectory));
     scoped_ptr<google_apis::ResourceEntry> dir_resource_entry(
         google_apis::ResourceEntry::ExtractAndParse(*dir_value));
     base::MessageLoopProxy::current()->PostTask(
