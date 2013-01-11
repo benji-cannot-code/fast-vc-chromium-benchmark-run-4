6e8550172baa112d3512df47a80b4b3cf93bf326
simonjam@chromium.org <simonjam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 23:54:56 +0000

    [Resource Timing] XMLHttpRequests should have initiator type 'xmlhttprequest' https://bugs.webkit.org/show_bug.cgi?id=106409
    Reviewed by Nate Chapin.
    
    Source/WebCore:
    
    The initiator is passed through ThreadableLoaderOptions to the CachedResourceRequest. This is
    optional, so other users of ThreadableLoader will have the default initiator of 'request'. Note
    that synchronous XHRs don't show up in the Resource Timing buffer yet.
    
    Test: http/tests/w3c/webperf/submission/resource-timing/html/test_resource_initiator_types.html
    
    * loader/DocumentThreadableLoader.cpp:
    (WebCore::DocumentThreadableLoader::loadRequest):
    * loader/ThreadableLoader.h:
    (ThreadableLoaderOptions):
    * loader/cache/CachedResourceRequestInitiators.cpp:
    (WebCore::CachedResourceRequestInitiators::CachedResourceRequestInitiators):
    * loader/cache/CachedResourceRequestInitiators.h:
    (CachedResourceRequestInitiators):
    * xml/XMLHttpRequest.cpp:
    (WebCore::XMLHttpRequest::createRequest):
    
    LayoutTests:
    
    * http/tests/w3c/webperf/resources/all_resource_types.htm:
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_initiator_types-expected.txt:
    * http/tests/w3c/webperf/submission/resource-timing/html/test_resource_initiator_types.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/CachedResourceRequestInitiators.h
+++ third_party/WebKit/Source/WebCore/loader/cache/CachedResourceRequestInitiators.h
@@ -35,6 +35,7 @@ namespace WebCore {
 struct CachedResourceRequestInitiators {
     const AtomicString css;
     const AtomicString icon;
+    const AtomicString xmlhttprequest;
     WTF_MAKE_NONCOPYABLE(CachedResourceRequestInitiators); WTF_MAKE_FAST_ALLOCATED;
 private:
     CachedResourceRequestInitiators();
--- third_party/WebKit/Source/WebCore/xml/XMLHttpRequest.cpp
+++ third_party/WebKit/Source/WebCore/xml/XMLHttpRequest.cpp
@@ -65,6 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/UnusedParam.h>
 #include <wtf/text/CString.h>
 
+#if ENABLE(RESOURCE_TIMING)
+#include "CachedResourceRequestInitiators.h"
+#endif
+
 #if USE(JSC)
 #include "JSDOMBinding.h"
 #include "JSDOMWindow.h"
@@ -787,6 +791,9 @@ void XMLHttpRequest::createRequest(ExceptionCode& ec)
     options.allowCredentials = (m_sameOriginRequest || m_includeCredentials) ? AllowStoredCredentials : DoNotAllowStoredCredentials;
     options.crossOriginRequestPolicy = UseAccessControl;
     options.securityOrigin = securityOrigin();
+#if ENABLE(RESOURCE_TIMING)
+    options.initiator = cachedResourceRequestInitiators().xmlhttprequest;
+#endif
 
 #if ENABLE(XHR_TIMEOUT)
     if (m_timeoutMilliseconds)
