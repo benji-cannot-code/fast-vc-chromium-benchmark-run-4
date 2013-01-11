294c86528b6d9db61ffea4887a8140b4c7e1333c
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 11:31:49 +0000

    Remove unused android buildbot shell scripts
    Test logic has been consolidated into build/android/buildbot/bb_run_bot.py.
    This part of the patch had to wait for buildbot master restarts, which have been been completed.
    The main patch is here: https://src.chromium.org/viewvc/chrome?view=rev&revision=175698
    
    TBR=cmp
    
    Review URL: https://codereview.chromium.org/11864009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176313 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_fyi_tester.sh
+++
@@ -1,9 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#!/bin/bash
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# DO NOT MODIFY. Scheduled to be removed.
-# Buildbot configs will be changed to call bb_run_bot.py directly.
-exec "$(dirname $0)"/bb_run_bot.py "$@" --bot-id fyi-tests
