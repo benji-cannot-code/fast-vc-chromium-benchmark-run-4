f7c65c0df5004aedac7f22d421607189d3270cbf
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 19:59:48 +0000

    RenderGeometryMap and TransformState disagree with sub-pixel layout and translations https://bugs.webkit.org/show_bug.cgi?id=106047
    Reviewed by Simon Fraser.
    
    Source/WebCore:
    
    Mirror RenderGeometryMap's optimization for integer-translated transforms in TransformState.
    This avoids the current behavior where the two can disagree on mappings, since RenderGeometryMap
    pixel-snapped later when a translation occurred between two sub-pixel containers.
    
    Test: fast/layers/geometry-map-transform-state-translation-mismatch.html
    
    * platform/graphics/transforms/TransformState.h:
    (WebCore::TransformState::setQuad): Clear accumulatedOffset when setting a new quad. Note: this
    implementation only works properly when only tracking a quad.
    * platform/graphics/transforms/TransformState.cpp:
    (WebCore::TransformState::applyTransform): apply integral translations to the accumulatedOffset
    for performance and consistency with RenderGeometryMap.
    
    LayoutTests:
    
    * fast/layers/geometry-map-transform-state-translation-mismatch-expected.txt: Added.
    * fast/layers/geometry-map-transform-state-translation-mismatch.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/transforms/TransformState.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/transforms/TransformState.cpp
@@ -112,6 +112,11 @@ void TransformState::applyTransform(const TransformationMatrix& transformFromCon
     if (wasClamped)
         *wasClamped = false;
 
+    if (transformFromContainer.isIntegerTranslation()) {
+        move(LayoutSize(transformFromContainer.e(), transformFromContainer.f()), accumulate);
+        return;
+    }
+
     applyAccumulatedOffset();
 
     // If we have an accumulated transform from last time, multiply in this transform
