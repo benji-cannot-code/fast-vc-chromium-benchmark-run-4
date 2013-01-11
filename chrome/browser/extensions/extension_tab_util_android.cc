62c5e9e6f48e527f52e3699176884f7f22fa6646
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 20:21:27 +0000

    Revert 176406 > Do not pass URLs in onUpdated events to extensions unless they have the > "tabs" permission. > > BUG=168442 > > > Review URL: https://chromiumcodereview.appspot.com/11824004
    TBR=mvrable@chromium.org
    Review URL: https://codereview.chromium.org/11866012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176426 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tab_util_android.cc
+++ chrome/browser/extensions/extension_tab_util_android.cc
@@ -52,7 +52,8 @@ ListValue* ExtensionTabUtil::CreateTabList(
 DictionaryValue* ExtensionTabUtil::CreateTabValue(
     const WebContents* contents,
     TabStripModel* tab_strip,
-    int tab_index) {
+    int tab_index,
+    IncludePrivacySensitiveFields include_privacy_sensitive_fields) {
   NOTIMPLEMENTED();
   return NULL;
 }
