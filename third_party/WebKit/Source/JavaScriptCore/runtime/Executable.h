9ae456e3db887b53aefa08b76a7dd6802e115a19
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 23:32:44 +0000

    Fix function name typo ProgramExecutable::initalizeGlobalProperties() https://bugs.webkit.org/show_bug.cgi?id=106701
    Reviewed by Geoffrey Garen.
    
    * interpreter/Interpreter.cpp:
    (JSC::Interpreter::execute):
    * runtime/Executable.cpp:
    (JSC::ProgramExecutable::initializeGlobalProperties):
    * runtime/Executable.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139510 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Executable.h
@@ -501,7 +501,7 @@ namespace JSC {
         }
 
 
-        JSObject* initalizeGlobalProperties(JSGlobalData&, CallFrame*, JSScope*);
+        JSObject* initializeGlobalProperties(JSGlobalData&, CallFrame*, JSScope*);
 
         static void destroy(JSCell*);
 
