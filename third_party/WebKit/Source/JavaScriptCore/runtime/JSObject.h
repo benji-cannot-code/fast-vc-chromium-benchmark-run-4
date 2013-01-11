003e69d4e7d190939c5762688e75e562c58da06e
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 20:20:25 +0000

    Fixed some bogus PropertyOffset ASSERTs https://bugs.webkit.org/show_bug.cgi?id=106686
    Reviewed by Gavin Barraclough.
    
    The ASSERTs were passing a JSType instead of an inlineCapacity, due to
    an incomplete refactoring.
    
    The compiler didn't catch this because both types are int underneath.
    
    * runtime/JSObject.h:
    (JSC::JSObject::getDirect):
    (JSC::JSObject::getDirectLocation):
    (JSC::JSObject::offsetForLocation):
    * runtime/Structure.cpp:
    (JSC::Structure::addPropertyTransitionToExistingStructure): Validate against
    our inline capacity, as we intended.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSObject.h
@@ -505,14 +505,14 @@ public:
     JSValue getDirect(JSGlobalData& globalData, PropertyName propertyName) const
     {
         PropertyOffset offset = structure()->get(globalData, propertyName);
-        checkOffset(offset, structure()->typeInfo().type());
+        checkOffset(offset, structure()->inlineCapacity());
         return offset != invalidOffset ? getDirectOffset(offset) : JSValue();
     }
 
     WriteBarrierBase<Unknown>* getDirectLocation(JSGlobalData& globalData, PropertyName propertyName)
     {
         PropertyOffset offset = structure()->get(globalData, propertyName);
-        checkOffset(offset, structure()->typeInfo().type());
+        checkOffset(offset, structure()->inlineCapacity());
         return isValidOffset(offset) ? locationForOffset(offset) : 0;
     }
 
@@ -571,7 +571,7 @@ public:
             result = offsetInInlineStorage;
         else
             result = outOfLineStorage() - location + (firstOutOfLineOffset - 1);
-        validateOffset(result, structure()->typeInfo().type());
+        validateOffset(result, structure()->inlineCapacity());
         return result;
     }
 
