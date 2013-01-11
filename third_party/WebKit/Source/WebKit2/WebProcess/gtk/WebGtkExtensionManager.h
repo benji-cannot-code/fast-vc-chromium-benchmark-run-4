09657a2c20512662f106a8e5499981ebb8371630
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 12:26:37 +0000

    [GTK] Remove support for global extensions in WebKit2 GTK+ https://bugs.webkit.org/show_bug.cgi?id=106646
    Reviewed by Xan Lopez.
    
    We have now API to set the web extensions directory. A global web
    estensions directory doesn't make sense anymore.
    
    * WebProcess/gtk/WebGtkExtensionManager.cpp:
    (WebKit::WebGtkExtensionManager::scanModules): Only scan the given
    directory for modules.
    (WebKit::WebGtkExtensionManager::initialize): Load modules found
    in the given directory only.
    * WebProcess/gtk/WebGtkExtensionManager.h:
    (WebGtkExtensionManager): Remove appendModuleDirectories().
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/gtk/WebGtkExtensionManager.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/gtk/WebGtkExtensionManager.h
@@ -46,7 +46,6 @@ public:
 private:
     WebGtkExtensionManager();
 
-    void appendModuleDirectories(Vector<String>&);
     void scanModules(const String&, Vector<String>&);
 
     Vector<Module*> m_extensionModules;
