fa68d26af748b7e9b023136a51ee89304ab1ba12
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 15:25:20 +0000

    Update some #includes in chrome/browser/browsing_data for headers in the new Platform directory
    Review URL: https://chromiumcodereview.appspot.com/11824041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176347 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browsing_data/browsing_data_helper.cc
+++ chrome/browser/browsing_data/browsing_data_helper.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/child_process_security_policy.h"
 #include "extensions/common/constants.h"
 #include "googleurl/src/gurl.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
 
 // Static
 bool BrowsingDataHelper::IsWebScheme(const std::string& scheme) {
--- chrome/browser/browsing_data/browsing_data_helper_unittest.cc
+++ chrome/browser/browsing_data/browsing_data_helper_unittest.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "extensions/common/constants.h"
 #include "googleurl/src/gurl.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
 
 namespace {
 
