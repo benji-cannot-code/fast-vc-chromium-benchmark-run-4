671c30aac3a4292c39fd648ace45d8d1a9f7fe4f
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 01:59:37 +0000

    Revert 176228 > Improve thread safety for device changes on OSX. > > Device changes are hard, lets go shopping!  Sadly OSX does not handle property > listener callbacks in a thread safe manner.  On 10.6 we can set > kAudioHardwarePropertyRunLoop to account for this.  On 10.7 this is broken > and we need to create a dispatch queue to drive callbacks.  However code > running on the dispatch queue must be mutually exclusive with code running > on the audio thread. > > ExclusiveDispatchQueueTaskObserver works around this by pausing and resuming > the dispatch queue before and after each pumped task.  Benchmarks on my retina > 10.8.2 macbook pro show on average this takes < 30us in total.  No performance > issues were seen even using FakeAudioOutputStream which drives audio callbacks > on the message loop. > > Complicating matters it the fact that the AudioProperty(Add|Remove)ListenerBlock > functions are not available on 10.6... so we need to dynamically load them :-/ > > This is not ideal and long term we should replace the audio thread on OSX with a > dispatch queue to avoid having to suspend/resume the queue; this is non-trivial > and requires a significant refactoring of the AudioManager to support. > > BUG=158170 > TEST=media_unittests. manual device changes. > > Review URL: https://codereview.chromium.org/11824018
    Fails on 10.6 :(
    
    TBR=dalecurtis@google.com
    Review URL: https://codereview.chromium.org/11840005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176230 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/mac/audio_manager_mac.h
+++ media/audio/mac/audio_manager_mac.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/message_loop_proxy.h"
 #include "media/audio/audio_manager_base.h"
-#include "media/audio/mac/audio_device_listener_mac.h"
 
 namespace media {
 
@@ -39,15 +38,16 @@ class MEDIA_EXPORT AudioManagerMac : public AudioManagerBase {
   virtual AudioParameters GetPreferredLowLatencyOutputStreamParameters(
       const AudioParameters& input_params) OVERRIDE;
 
+  // Called by an internal device change listener.  Must be called on
+  // |creating_message_loop_|.
+  void OnDeviceChange();
+
  protected:
   virtual ~AudioManagerMac();
 
  private:
-  // Helper methods for constructing AudioDeviceListenerMac on the audio thread.
-  void CreateDeviceListener();
-  void DestroyDeviceListener();
-
-  scoped_ptr<AudioDeviceListenerMac> output_device_listener_;
+  bool listener_registered_;
+  scoped_refptr<base::MessageLoopProxy> creating_message_loop_;
 
   DISALLOW_COPY_AND_ASSIGN(AudioManagerMac);
 };
