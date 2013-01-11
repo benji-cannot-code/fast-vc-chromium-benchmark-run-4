8bd3784303368284f5805833f005f027cc22fc6a
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-11 16:36:47 +0000

    [android_webview] Add onMeasure support.
    This change makes it possible for the AwContents embeddder
    to implement the android.view.View#onMeasure method.
    
    A content size change listener is added to ContentViewCore
    to support the wrap_content layout mode. The layout
    logic is encapsulated in AwLayoutSizer class.
    
    BUG=None
    TEST=AndroidWebViewTests
    Java-only change, passes on the trybots.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11529016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176370 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -83,6 +83,10 @@ public class AwContents {
      * dispatching of view methods through the containing view.
      */
     public interface InternalAccessDelegate extends ContentViewCore.InternalAccessDelegate {
+        /**
+         * @see View#setMeasuredDimension(int, int)
+         */
+        void setMeasuredDimension(int measuredWidth, int measuredHeight);
     }
 
     private int mNativeAwContents;
@@ -91,7 +95,8 @@ public class AwContents {
     private AwContentsClient mContentsClient;
     private AwContentsIoThreadClient mIoThreadClient;
     private InterceptNavigationDelegateImpl mInterceptNavigationDelegate;
-    private ContentViewCore.InternalAccessDelegate mInternalAccessAdapter;
+    private InternalAccessDelegate mInternalAccessAdapter;
+    private final AwLayoutSizer mLayoutSizer;
     // This can be accessed on any thread after construction. See AwContentsIoThreadClient.
     private final AwSettings mSettings;
     private final ClientCallbackHandler mClientCallbackHandler;
@@ -272,9 +277,21 @@ public class AwContents {
         }
     }
 
+    private class AwLayoutSizerDelegate implements AwLayoutSizer.Delegate {
+        @Override
+        public void requestLayout() {
+            mContainerView.requestLayout();
+        }
+
+        @Override
+        public void setMeasuredDimension(int measuredWidth, int measuredHeight) {
+            mInternalAccessAdapter.setMeasuredDimension(measuredWidth, measuredHeight);
+        }
+    }
+
     // TODO(kristianm): Delete this when privateBrowsing parameter is removed in Android
     public AwContents(ViewGroup containerView,
-            ContentViewCore.InternalAccessDelegate internalAccessAdapter,
+            InternalAccessDelegate internalAccessAdapter,
             AwContentsClient contentsClient,
             NativeWindow nativeWindow, boolean privateBrowsing,
             boolean isAccessFromFileURLsGrantedByDefault) {
@@ -290,7 +307,7 @@ public class AwContents {
      * TODO(benm): Remove the nativeWindow parameter.
      */
     public AwContents(ViewGroup containerView,
-            ContentViewCore.InternalAccessDelegate internalAccessAdapter,
+            InternalAccessDelegate internalAccessAdapter,
             AwContentsClient contentsClient,
             NativeWindow nativeWindow,
             boolean isAccessFromFileURLsGrantedByDefault) {
@@ -310,6 +327,8 @@ public class AwContents {
                 new AwNativeWindow(mContainerView.getContext()),
                 isAccessFromFileURLsGrantedByDefault);
         mContentViewCore.setContentViewClient(mContentsClient);
+        mLayoutSizer = new AwLayoutSizer(new AwLayoutSizerDelegate());
+        mContentViewCore.setContentSizeChangeListener(mLayoutSizer);
         mContentsClient.installWebContentsObserver(mContentViewCore);
 
         mSettings = new AwSettings(mContentViewCore.getContext());
@@ -378,6 +397,10 @@ public class AwContents {
             int c = mContentViewCore.getBackgroundColor();
             canvas.drawRGB(Color.red(c), Color.green(c), Color.blue(c));
         }
+    }
+
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mLayoutSizer.onMeasure(widthMeasureSpec, heightMeasureSpec);
     }
 
     public int findAllSync(String searchString) {
--- android_webview/javatests/src/org/chromium/android_webview/test/AwTestContainerView.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwTestContainerView.java
@@ -26,7 +26,7 @@ import org.chromium.content.browser.ContentViewCore;
  */
 class AwTestContainerView extends FrameLayout {
     private AwContents mAwContents;
-    private ContentViewCore.InternalAccessDelegate mInternalAccessDelegate;
+    private AwContents.InternalAccessDelegate mInternalAccessDelegate;
 
     public AwTestContainerView(Context context) {
         super(context);
@@ -45,7 +45,7 @@ class AwTestContainerView extends FrameLayout {
         return mAwContents;
     }
 
-    public ContentViewCore.InternalAccessDelegate getInternalAccessDelegate() {
+    public AwContents.InternalAccessDelegate getInternalAccessDelegate() {
         return mInternalAccessDelegate;
     }
 
@@ -74,6 +74,11 @@ class AwTestContainerView extends FrameLayout {
     }
 
     @Override
+    public void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        mAwContents.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @Override
     public void onSizeChanged(int w, int h, int ow, int oh) {
         mAwContents.onSizeChanged(w, h, ow, oh);
     }
@@ -98,9 +103,9 @@ class AwTestContainerView extends FrameLayout {
         mAwContents.onDraw(canvas);
     }
 
-    // TODO: ContentViewCore could define a generic class that holds an implementation similar to
+    // TODO: AwContents could define a generic class that holds an implementation similar to
     // the one below.
-    private class InternalAccessAdapter implements ContentViewCore.InternalAccessDelegate {
+    private class InternalAccessAdapter implements AwContents.InternalAccessDelegate {
 
         @Override
         public boolean drawChild(Canvas canvas, View child, long drawingTime) {
@@ -145,6 +150,11 @@ class AwTestContainerView extends FrameLayout {
         @Override
         public boolean super_awakenScrollBars(int startDelay, boolean invalidate) {
             return AwTestContainerView.super.awakenScrollBars(startDelay, invalidate);
+        }
+
+        @Override
+        public void setMeasuredDimension(int measuredWidth, int measuredHeight) {
+            AwTestContainerView.super.setMeasuredDimension(measuredWidth, measuredHeight);
         }
     }
 }
