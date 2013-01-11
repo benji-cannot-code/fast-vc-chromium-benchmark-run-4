df1d67c0ad7d43961bb9d89327e409164baa467e
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-11 14:34:06 +0000

    Unreviewed. Build fix for the GTK debug builds of WebKit2 after r139415.
    * WebProcess/gtk/WebGtkExtensionManager.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139436 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/gtk/WebGtkExtensionManager.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/gtk/WebGtkExtensionManager.cpp
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "InjectedBundle.h"
 #include "WKBundleAPICast.h"
+#include "WKString.h"
+#include "WKType.h"
 #include "WebKitWebExtensionPrivate.h"
 #include <WebCore/FileSystem.h>
 #include <wtf/OwnPtr.h>
