fbfde0e682bc3d2e0f455e6797c507f0ab4d8a3e
bengr@google.com <bengr@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-12 20:06:14 +0000

    Defaulting SpdyProxyEnabled pref to true.
    BUG=157897
    
    Review URL: https://chromiumcodereview.appspot.com/11883002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176589 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile.cc
+++ chrome/browser/profiles/profile.cc
@@ -105,7 +105,7 @@ void Profile::RegisterUserPrefs(PrefServiceSyncable* prefs) {
                              false,
                              PrefServiceSyncable::UNSYNCABLE_PREF);
   prefs->RegisterBooleanPref(prefs::kSpdyProxyEnabled,
-                             false,
+                             true,
                              PrefServiceSyncable::UNSYNCABLE_PREF);
 #endif
 
