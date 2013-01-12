c5ae99bea5a8cf46effc3005eb135fff8436e8c2
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-12 00:04:07 +0000

    Remove LOG(WARNING) for SYN_REPLY to non-existent SPDY streams.
    This fires quite frequently due to suggest completions in the omnibox.
    
    If two characters are entered quickly, there is a SYN_STREAM for the first followed by a RST_STREAM. However, the server may have already sent a reply before processing the RST_STREAM.
    
    Also, we probably shouldn't be LOG(WARNING) for strange server responses which need to be gracefully handled.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11863009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176475 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -1486,7 +1486,6 @@ void SpdySession::OnSynReply(SpdyStreamId stream_id,
 
   if (!IsStreamActive(stream_id)) {
     // NOTE:  it may just be that the stream was cancelled.
-    LOG(WARNING) << "Received SYN_REPLY for invalid stream " << stream_id;
     return;
   }
 
