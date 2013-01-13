acdb783790585cdb2c0f42fbd332994defd05dce
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-13 00:42:22 +0000

    Move pending tile priorities to active on tree activation
    This prevents our tile priorites from being stale
    when the pending tree becomes active.
    
    BUG=166780
    
    
    Review URL: https://chromiumcodereview.appspot.com/11856003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176601 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_impl.h
+++ cc/layer_impl.h
@@ -272,6 +272,7 @@ public:
     virtual Region visibleContentOpaqueRegion() const;
 
     virtual void didUpdateTransforms() { }
+    virtual void didBecomeActive() { }
 
     // Indicates that the surface previously used to render this layer
     // was lost and that a new one has been created. Won't be called
