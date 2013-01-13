b132f5483fb7c3b6b114d7e51bd2132831d18b1a
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-13 21:48:28 +0000

    Source/WebCore: Unreviewed gardening. Rolling out r139537. It broke platforms without sub-pixel layout.
    * dom/Element.cpp:
    (WebCore::Element::offsetWidth):
    (WebCore::Element::offsetHeight):
    (WebCore::Element::clientWidth):
    (WebCore::Element::clientHeight):
    * rendering/RenderObject.h:
    (WebCore::adjustLayoutUnitForAbsoluteZoom):
    * rendering/style/RenderStyle.h:
    (WebCore::adjustLayoutUnitForAbsoluteZoom):
    
    LayoutTests: Unreviewed gardening. Rolling out 139537. It broke platforms not using sub-
    pixel layout.
    
    * fast/images/zoomed-offset-size.html: Removed.
    * fast/images/zoomed-offset-size-expected.txt: Removed.
    * platform/efl/TestExpectations:
    * platform/qt/TestExpectations
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -454,7 +454,7 @@ int Element::offsetWidth()
 {
     document()->updateLayoutIgnorePendingStylesheets();
     if (RenderBoxModelObject* renderer = renderBoxModelObject())
-        return adjustLayoutUnitForAbsoluteZoom(renderer->pixelSnappedOffsetWidth(), renderer).round();
+        return adjustForAbsoluteZoom(renderer->pixelSnappedOffsetWidth(), renderer);
     return 0;
 }
 
@@ -462,7 +462,7 @@ int Element::offsetHeight()
 {
     document()->updateLayoutIgnorePendingStylesheets();
     if (RenderBoxModelObject* renderer = renderBoxModelObject())
-        return adjustLayoutUnitForAbsoluteZoom(renderer->pixelSnappedOffsetHeight(), renderer).round();
+        return adjustForAbsoluteZoom(renderer->pixelSnappedOffsetHeight(), renderer);
     return 0;
 }
 
@@ -509,7 +509,7 @@ int Element::clientWidth()
     }
     
     if (RenderBox* renderer = renderBox())
-        return adjustLayoutUnitForAbsoluteZoom(renderer->pixelSnappedClientWidth(), renderer).round();
+        return adjustForAbsoluteZoom(renderer->pixelSnappedClientWidth(), renderer);
     return 0;
 }
 
@@ -530,7 +530,7 @@ int Element::clientHeight()
     }
     
     if (RenderBox* renderer = renderBox())
-        return adjustLayoutUnitForAbsoluteZoom(renderer->pixelSnappedClientHeight(), renderer).round();
+        return adjustForAbsoluteZoom(renderer->pixelSnappedClientHeight(), renderer);
     return 0;
 }
 
