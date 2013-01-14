e9ca81d592137be2a4797114f6581a30c9ffe31e
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-14 04:23:23 +0000

    [EFL] Unreviewed build fix after r139541 https://bugs.webkit.org/show_bug.cgi?id=106749
    undefined reference to `JSC::JSCell::methodTable() const'
    
    Patch by KwangYong Choi <ky0.choi@samsung.com> on 2013-01-13
    
    * ewk/ewk_js.cpp:
    * ewk/ewk_view.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_js.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_js.cpp
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(NETSCAPE_PLUGIN_API)
 
 #include "NP_jsobject.h"
+#include "Operations.h"
 #include "ewk_js_private.h"
 #include "ewk_private.h"
 #include "npruntime.h"
--- third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
+++ third_party/WebKit/Source/WebKit/efl/ewk/ewk_view.cpp
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSDOMBinding.h"
 #include "JSDOMWindow.h"
 #include "JSLock.h"
+#include "Operations.h"
 #include "PageClientEfl.h"
 #include "PageGroup.h"
 #include "PlatformMouseEvent.h"
