0be5918af9ea20cbc7514e7e2583598a4aa2af71
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-14 23:13:13 +0000

    [EFL] REGRESSION(r139189): MiniBrowser renders abnormally with -r option. https://bugs.webkit.org/show_bug.cgi?id=106512
    Patch by Huang Dongsung <luxtella@company100.net> on 2013-01-14
    Reviewed by Kenneth Rohde Christiansen.
    
    PageViewportController handles only pageScaleFactor after r139189. So
    PageViewportControllerClientEFL calls EwkView::setScaleFactor() with
    pageScaleFactor instead of pageScaleFactor * deviceScaleFactor.
    
    This patch makes EwkView handle pageScaleFactor.
    1. EwkView uses pageScaleFactor * deviceScaleFactor when drawing in
    the device unit.
    2. EwkView uses pageScaleFactor when notifying the scale to
    PageViewportController.
    
    * UIProcess/API/efl/EwkViewImpl.cpp:
    (EwkViewImpl::EwkViewImpl):
    (EwkViewImpl::deviceScaleFactor):
    (EwkViewImpl::transformFromScene):
    (EwkViewImpl::displayTimerFired):
    * UIProcess/API/efl/EwkViewImpl.h:
    (EwkViewImpl):
    (EwkViewImpl::setPageScaleFactor):
    (EwkViewImpl::pageScaleFactor):
    * UIProcess/PageViewportController.cpp:
    (WebKit::PageViewportController::didRenderFrame):
    * UIProcess/PageViewportControllerClient.h:
    (PageViewportControllerClient):
    * UIProcess/efl/PageClientLegacyImpl.cpp:
    (WebKit::PageClientLegacyImpl::updateViewportSize):
    * UIProcess/efl/PageViewportControllerClientEfl.cpp:
    (WebKit::PageViewportControllerClientEfl::setViewportPosition):
    (WebKit::PageViewportControllerClientEfl::setPageScaleFactor):
    (WebKit::PageViewportControllerClientEfl::didResumeContent):
    * UIProcess/efl/PageViewportControllerClientEfl.h:
    (PageViewportControllerClientEfl):
    * UIProcess/qt/PageViewportControllerClientQt.cpp:
    (WebKit::PageViewportControllerClientQt::setPageScaleFactor):
    * UIProcess/qt/PageViewportControllerClientQt.h:
    (PageViewportControllerClientQt):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139675 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/EwkViewImpl.h
@@ -123,6 +123,7 @@ public:
     bool isVisible() const;
 
     void setDeviceScaleFactor(float scale);
+    float deviceScaleFactor() const;
 
     WebCore::AffineTransform transformToScene() const;
     WebCore::AffineTransform transformFromScene() const;
@@ -200,8 +201,8 @@ public:
 #if USE(TILED_BACKING_STORE)
     WebKit::PageClientBase* pageClient() { return m_pageClient.get(); }
 
-    void setScaleFactor(float scaleFactor) { m_scaleFactor = scaleFactor; }
-    float scaleFactor() const { return m_scaleFactor; }
+    void setPageScaleFactor(float scaleFactor) { m_pageScaleFactor = scaleFactor; }
+    float pageScaleFactor() const { return m_pageScaleFactor; }
 
     void setPagePosition(const WebCore::FloatPoint& position) { m_pagePosition = position; }
     const WebCore::FloatPoint pagePosition() const { return m_pagePosition; }
@@ -264,7 +265,7 @@ private:
 #endif
     OwnPtr<EwkBackForwardList> m_backForwardList;
 #if USE(TILED_BACKING_STORE)
-    float m_scaleFactor;
+    float m_pageScaleFactor;
     WebCore::FloatPoint m_pagePosition;
 #endif
     OwnPtr<EwkSettings> m_settings;
