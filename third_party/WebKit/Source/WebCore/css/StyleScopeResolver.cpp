0e79906c2b7181b862e47ee798907125cfa04e68
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-14 09:12:30 +0000

    Web Inspector: Native Memory Instrumentation: fix instrumentation for already instrumented classes 3/3 https://bugs.webkit.org/show_bug.cgi?id=106764
    Reviewed by Vsevolod Vlasov.
    
    Last three classes with not instrumented members were fixed.
    
    * css/CSSGroupingRule.cpp:
    (WebCore::CSSGroupingRule::reportMemoryUsage):
    * css/StyleScopeResolver.cpp:
    (WebCore::StyleScopeResolver::reportMemoryUsage):
    * loader/cache/CachedResourceLoader.cpp:
    (WebCore::CachedResourceLoader::reportMemoryUsage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139590 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleScopeResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleScopeResolver.cpp
@@ -245,6 +245,7 @@ void StyleScopeResolver::reportMemoryUsage(MemoryObjectInfo* memoryObjectInfo) c
     info.addMember(m_authorStyles);
     info.addMember(m_stack);
     info.addMember(m_atHostRules);
+    info.addMember(m_stackParent);
 }
 
 }
