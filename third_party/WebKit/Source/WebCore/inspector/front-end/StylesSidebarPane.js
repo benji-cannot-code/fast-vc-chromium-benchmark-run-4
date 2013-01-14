88dc608000dea6a4304bd34721220865bfe1d0f0
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-14 10:13:20 +0000

    Web Inspector: [Styles] HTML color names not converted to RGB/HEX/HSL when "Color format" feature used https://bugs.webkit.org/show_bug.cgi?id=106767
    Reviewed by Vsevolod Vlasov.
    
    We used to render named colors as names regardless of the selected color format option.
    
    * inspector/front-end/StylesSidebarPane.js:
    (WebInspector.StylePropertyTreeElement.prototype.updateTitle.):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/StylesSidebarPane.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/StylesSidebarPane.js
@@ -1820,8 +1820,6 @@ WebInspector.StylePropertyTreeElement.prototype = {
                     var formatSetting = WebInspector.settings.colorFormat.get();
                     if (formatSetting === cf.Original)
                         format = cf.Original;
-                    else if (color.nickname)
-                        format = cf.Nickname;
                     else if (formatSetting === cf.RGB)
                         format = (color.simple ? cf.RGB : cf.RGBA);
                     else if (formatSetting === cf.HSL)
