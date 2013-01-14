9488dd08b9860bb57cf5ca2f19544f2e5c7589db
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-14 12:00:05 +0000

    cros: Don't reload gaia when there is a pending auth ui.
    BUG=166588
    TEST=Verify proxy auth dialog stays there until user dismiss it on login screen.
    
    R=nkostylev@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11826070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176657 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.h
@@ -1,11 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
 #ifndef CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_SIGNIN_SCREEN_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_CHROMEOS_LOGIN_SIGNIN_SCREEN_HANDLER_H_
 
+#include <set>
 #include <string>
 
 #include "base/callback.h"
@@ -21,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/login/network_state_informer.h"
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "content/public/browser/notification_observer.h"
+#include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_ui.h"
 
 class BrowsingDataRemover;
@@ -396,6 +398,13 @@ class SigninScreenHandler
 
   base::CancelableClosure update_state_closure_;
   base::CancelableClosure connecting_closure_;
+
+  content::NotificationRegistrar registrar_;
+
+  // Whether there is an auth UI pending. This flag is set on receiving
+  // NOTIFICATION_AUTH_NEEDED and reset on either NOTIFICATION_AUTH_SUPPLIED or
+  // NOTIFICATION_AUTH_CANCELLED.
+  bool has_pending_auth_ui_;
 
   DISALLOW_COPY_AND_ASSIGN(SigninScreenHandler);
 };
