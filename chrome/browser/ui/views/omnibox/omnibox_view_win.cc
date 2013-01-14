e888ead0b1d3ae9b707ad04263b4039dfdcc0654
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-14 11:02:44 +0000

    Fixed a clipboard bug in views/omnibox. The content was always copied as pure text, never as hyperlink.
    BUG=168583
    
    Review URL: https://chromiumcodereview.appspot.com/11789003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176656 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/omnibox_view_win.cc
+++ chrome/browser/ui/views/omnibox/omnibox_view_win.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/autocomplete/keyword_provider.h"
+#include "chrome/browser/bookmarks/bookmark_node_data.h"
 #include "chrome/browser/command_updater.h"
 #include "chrome/browser/net/url_fixer_upper.h"
 #include "chrome/browser/profiles/profile.h"
@@ -128,13 +129,18 @@ bool IsDrag(const POINT& origin, const POINT& current) {
       gfx::Point(current) - gfx::Point(origin));
 }
 
-// Write |text| and an optional |url| to the clipboard.
-void DoCopy(const string16& text, const GURL* url) {
+// Copies |selected_text| as text to the primary clipboard.
+void DoCopyText(const string16& selected_text) {
   ui::ScopedClipboardWriter scw(ui::Clipboard::GetForCurrentThread(),
                                 ui::Clipboard::BUFFER_STANDARD);
-  scw.WriteText(text);
-  if (url != NULL)
-    scw.WriteBookmark(text, url->spec());
+  scw.WriteText(selected_text);
+}
+
+// Writes |url| and |text| to the clipboard as a well-formed URL.
+void DoCopyURL(const GURL& url, const string16& text) {
+  BookmarkNodeData data;
+  data.ReadFromTuple(url, text);
+  data.WriteToClipboard(NULL);
 }
 
 }  // namespace
@@ -1094,7 +1100,7 @@ int OmniboxViewWin::OnPerformDropImpl(const ui::DropTargetEvent& event,
 }
 
 void OmniboxViewWin::CopyURL() {
-  DoCopy(toolbar_model()->GetText(false), &toolbar_model()->GetURL());
+  DoCopyURL(toolbar_model()->GetURL(), toolbar_model()->GetText(false));
 }
 
 bool OmniboxViewWin::SkipDefaultKeyEventProcessing(const ui::KeyEvent& event) {
@@ -1429,7 +1435,10 @@ void OmniboxViewWin::OnCopy() {
   // GetSel() doesn't preserve selection direction, so sel.cpMin will always be
   // the smaller value.
   model()->AdjustTextForCopy(sel.cpMin, IsSelectAll(), &text, &url, &write_url);
-  DoCopy(text, write_url ? &url : NULL);
+  if (write_url)
+    DoCopyURL(url, text);
+  else
+    DoCopyText(text);
 }
 
 LRESULT OmniboxViewWin::OnCreate(const CREATESTRUCTW* /*create_struct*/) {
