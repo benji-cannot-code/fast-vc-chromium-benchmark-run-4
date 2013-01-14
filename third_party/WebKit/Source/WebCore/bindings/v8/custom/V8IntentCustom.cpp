fccc32a92ad2216f1d76661fc8a9b8024a558266
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-14 11:10:36 +0000

    [V8] Pass an Isolate to associateObjectWithWrapper() https://bugs.webkit.org/show_bug.cgi?id=106773
    Reviewed by Adam Barth.
    
    In preparation for making an Isolate parameter mandatory
    in associateObjectWithWrapper(), this patch passes an
    Isolate to associateObjectWithWrapper().
    
    No tests. No change in behavior.
    
    * bindings/v8/custom/V8ArrayBufferCustom.cpp:
    (WebCore::V8ArrayBuffer::constructorCallbackCustom):
    * bindings/v8/custom/V8ArrayBufferViewCustom.h:
    (WebCore::wrapArrayBufferView):
    (WebCore::constructWebGLArray):
    * bindings/v8/custom/V8AudioContextCustom.cpp:
    (WebCore::V8AudioContext::constructorCallbackCustom):
    * bindings/v8/custom/V8DOMFormDataCustom.cpp:
    (WebCore::V8DOMFormData::constructorCallbackCustom):
    * bindings/v8/custom/V8DataViewCustom.cpp:
    (WebCore::V8DataView::constructorCallbackCustom):
    * bindings/v8/custom/V8HTMLImageElementConstructor.cpp:
    (WebCore::v8HTMLImageElementConstructorCallback):
    * bindings/v8/custom/V8IntentCustom.cpp:
    (WebCore::V8Intent::constructorCallbackCustom):
    * bindings/v8/custom/V8MessageChannelCustom.cpp:
    (WebCore::V8MessageChannel::constructorCallbackCustom):
    * bindings/v8/custom/V8MutationObserverCustom.cpp:
    (WebCore::V8MutationObserver::constructorCallbackCustom):
    * bindings/v8/custom/V8WebKitPointCustom.cpp:
    (WebCore::V8WebKitPoint::constructorCallbackCustom):
    * bindings/v8/custom/V8XMLHttpRequestCustom.cpp:
    (WebCore::V8XMLHttpRequest::constructorCallbackCustom):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8IntentCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8IntentCustom.cpp
@@ -56,7 +56,7 @@ v8::Handle<v8::Value> V8Intent::constructorCallbackCustom(const v8::Arguments& a
             return setDOMException(ec, args.GetIsolate());
 
         v8::Handle<v8::Object> wrapper = args.Holder();
-        V8DOMWrapper::associateObjectWithWrapper(impl.release(), &info, wrapper);
+        V8DOMWrapper::associateObjectWithWrapper(impl.release(), &info, wrapper, args.GetIsolate());
         return wrapper;
     }
 
@@ -79,7 +79,7 @@ v8::Handle<v8::Value> V8Intent::constructorCallbackCustom(const v8::Arguments& a
         return setDOMException(ec, args.GetIsolate());
 
     v8::Handle<v8::Object> wrapper = args.Holder();
-    V8DOMWrapper::associateObjectWithWrapper(impl.release(), &info, wrapper);
+    V8DOMWrapper::associateObjectWithWrapper(impl.release(), &info, wrapper, args.GetIsolate());
     return wrapper;
 }
 
