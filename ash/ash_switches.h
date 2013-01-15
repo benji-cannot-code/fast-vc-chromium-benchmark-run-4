3ffa0673a9624f71387a0c9eac9816b07851981c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 22:58:46 +0000

    add command-line flag to enable brigntness control
    This will be used to enable the brightness control for kiev.
    
    BUG=chrome-os-partner:17150
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/11889028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177000 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.h
+++ ash/ash_switches.h
@@ -30,6 +30,7 @@ ASH_EXPORT extern const char kAshDisablePanelFitting[];
 ASH_EXPORT extern const char kAshDisableBootAnimation2[];
 ASH_EXPORT extern const char kAshDisableNewLockAnimations[];
 ASH_EXPORT extern const char kAshEnableAdvancedGestures[];
+ASH_EXPORT extern const char kAshEnableBrightnessControl[];
 #if defined(OS_LINUX)
 ASH_EXPORT extern const char kAshEnableMemoryMonitor[];
 #endif
