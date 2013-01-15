0774b56ecf13f2076f5df227a04518ff67b02a0c
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-15 02:45:24 +0000

    [ANGLE] Update ANGLE in WebKit https://bugs.webkit.org/show_bug.cgi?id=106274
    Unreviewed attempted build fix.
    
    Source/ThirdParty/ANGLE:
    
    Remove the copy files stage. Make khrplatform.h a public header.
    
    * ANGLE.xcodeproj/project.pbxproj:
    * include/GLSLANG/ShaderLang.h:
    
    Source/WebKit:
    
    The khrplatform.h file is now a public header without a directory
    prefix. Update the include path to look in the right place.
    
    * PlatformEfl.cmake:
    
    Source/WebKit2:
    
    The khrplatform.h file is now a public header without a directory
    prefix. Update the include path to look in the right place.
    
    * PlatformEfl.cmake:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/ThirdParty/ANGLE/include/GLSLANG/ShaderLang.h
+++ third_party/WebKit/Source/ThirdParty/ANGLE/include/GLSLANG/ShaderLang.h
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define COMPILER_EXPORT
 #endif
 
-#include "KHR/khrplatform.h"
+#include "khrplatform.h"
 
 //
 // This is the platform independent interface between an OGL driver
