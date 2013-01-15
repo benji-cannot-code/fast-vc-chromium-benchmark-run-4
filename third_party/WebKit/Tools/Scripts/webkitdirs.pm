02aa174d79f4ba60f1657453c06da3dc4c98f005
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-15 19:49:50 +0000

    [Autotools] Add support for WebKit2-only builds https://bugs.webkit.org/show_bug.cgi?id=106889
    Patch by Zan Dobersek <zandobersek@gmail.com> on 2013-01-15
    Reviewed by Gustavo Noronha Silva.
    
    .: 
    
    * GNUmakefile.am: Only copy the WebKit1 documentation into the destination
    directory if building WebKit1.
    * configure.ac: Add a configuration option for disabling the WebKit1 build.
    Only conditionally copy WebKit1-specific targets from the input files. Some
    small style changes to the WebKit2 configuration flag included as well.
    
    Source/WebKit/gtk: 
    
    * GNUmakefile.am: Only build the WebKit1-specific targets if actually
    building WebKit1. This puts the libwebkitgtk library, the pkg-config file
    and the WebKit1-specific unit tests under the ENABLE_WEBKIT1 Automake
    conditional.
    
    Tools: 
    
    * GNUmakefile.am: Only build the GtkLauncher and DumpRenderTree if building WebKit1.
    * Scripts/build-webkit: Pass along the information on whether to build WebKit1 when
    building the GTK port through build-webkit.
    * Scripts/webkitdirs.pm:
    (buildAutotoolsProject): Add the --disable-webkit1 configuration flag to the build
    arguments when not building WebKit1.
    (buildGtkProject): Pass along the information on whether to build WebKit1 when building
    through the Autotools build system.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139769 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -2006,7 +2006,7 @@ sub mustReRunAutogen($@)
 
 sub buildAutotoolsProject($@)
 {
-    my ($project, $clean, $prefix, $makeArgs, $noWebKit2, @features) = @_;
+    my ($project, $clean, $prefix, $makeArgs, $noWebKit1, $noWebKit2, @features) = @_;
 
     my $make = 'make';
     my $dir = productDir();
@@ -2027,6 +2027,9 @@ sub buildAutotoolsProject($@)
     }
 
     my @buildArgs = @ARGV;
+    if ($noWebKit1) {
+        unshift(@buildArgs, "--disable-webkit1");
+    }
     if ($noWebKit2) {
         unshift(@buildArgs, "--disable-webkit2");
     }
@@ -2444,13 +2447,13 @@ EOF
 
 sub buildGtkProject
 {
-    my ($project, $clean, $prefix, $makeArgs, $noWebKit2, @features) = @_;
+    my ($project, $clean, $prefix, $makeArgs, $noWebKit1, $noWebKit2, @features) = @_;
 
     if ($project ne "WebKit" and $project ne "JavaScriptCore" and $project ne "WTF") {
         die "Unsupported project: $project. Supported projects: WebKit, JavaScriptCore, WTF\n";
     }
 
-    return buildAutotoolsProject($project, $clean, $prefix, $makeArgs, $noWebKit2, @features);
+    return buildAutotoolsProject($project, $clean, $prefix, $makeArgs, $noWebKit1, $noWebKit2, @features);
 }
 
 sub buildChromiumMakefile($$@)
