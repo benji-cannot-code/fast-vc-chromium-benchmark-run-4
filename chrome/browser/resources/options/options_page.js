fbe223a650fa3c8011f8351e5aaeac4e5d993504
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 07:17:38 +0000

    Rename options.OptionsBubble to cr.ui.AutoCloseBubble
    This CL makes the bubble implementation used on the options page into the
    cr.ui namespace so that it can be shared with the upcoming improved
    about:policy page.
    
    BUG=134849
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/11854012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176821 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/options_page.js
+++ chrome/browser/resources/options/options_page.js
@@ -409,7 +409,7 @@ cr.define('options', function() {
 
   /**
    * Returns the currently visible bubble, or null if no bubble is visible.
-   * @return {OptionsBubble} The bubble currently being shown.
+   * @return {AutoCloseBubble} The bubble currently being shown.
    */
   OptionsPage.getVisibleBubble = function() {
     var bubble = OptionsPage.bubble_;
@@ -429,7 +429,7 @@ cr.define('options', function() {
   OptionsPage.showBubble = function(content, target, domSibling, location) {
     OptionsPage.hideBubble();
 
-    var bubble = new options.OptionsBubble;
+    var bubble = new cr.ui.AutoCloseBubble;
     bubble.anchorNode = target;
     bubble.domSibling = domSibling;
     bubble.arrowLocation = location;
