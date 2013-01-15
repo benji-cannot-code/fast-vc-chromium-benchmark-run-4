b62e0ef5f7e67a639b150e6a10705036ce96bb90
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 06:46:57 +0000

    bookmarks: Update the pref_service includes.
    We just use PrefServiceSyncable, so just include the corresponding include for
    it, instead of including pref_service.h which will also include
    pref_service_simple.h.
    
    BUG=144783
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11821052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176813 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_prompt_prefs.cc
+++ chrome/browser/bookmarks/bookmark_prompt_prefs.cc
@@ -5,12 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/bookmarks/bookmark_prompt_prefs.h"
 
-#include "chrome/browser/prefs/pref_service.h"
+#include "chrome/browser/prefs/pref_service_syncable.h"
 #include "chrome/common/pref_names.h"
-
-namespace {
-
-}  // namespace
 
 BookmarkPromptPrefs::BookmarkPromptPrefs(PrefServiceBase* user_prefs)
     : prefs_(user_prefs) {
--- chrome/browser/bookmarks/bookmark_utils.cc
+++ chrome/browser/bookmarks/bookmark_utils.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/bookmarks/bookmark_node_data.h"
 #include "chrome/browser/history/query_parser.h"
-#include "chrome/browser/prefs/pref_service.h"
+#include "chrome/browser/prefs/pref_service_syncable.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/user_metrics.h"
