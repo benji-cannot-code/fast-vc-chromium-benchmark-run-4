772b0094251209bd7a03eb3eb15dac5fb84dc553
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 01:32:16 +0000

    Switch Android code to use ExecuteJavascriptInWebFrameCallbackResult.
    BUG=168169
    TEST=should all pass
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=175489
    
    Reverted: https://src.chromium.org/viewvc/chrome?view=rev&revision=175491
    
    Review URL: https://chromiumcodereview.appspot.com/11788005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176773 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/TestAwContentsClient.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/TestAwContentsClient.java
@@ -74,12 +74,6 @@ class TestAwContentsClient extends NullContentsClient {
     }
 
     @Override
-    public void onEvaluateJavaScriptResult(int id, String jsonResult) {
-        super.onEvaluateJavaScriptResult(id, jsonResult);
-        mOnEvaluateJavaScriptResultHelper.notifyCalled(id, jsonResult);
-    }
-
-    @Override
     public boolean onConsoleMessage(ConsoleMessage consoleMessage) {
         mAddMessageToConsoleHelper.setLevel(consoleMessage.messageLevel().ordinal());
         mAddMessageToConsoleHelper.setMessage(consoleMessage.message());
