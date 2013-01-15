8d9e46c520987a152bd3655c4cfdb0eb6241d5a4
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-15 09:46:29 +0000

    Web Inspector: [Canvas] introduce CallArgument type into the protocol https://bugs.webkit.org/show_bug.cgi?id=106640
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    CallArgument type abstraction to describe arguments of a canvas context call.
    Drive-by: Introduce a CanvasContext to fix the wrong contextId value in the protocol.
    The contextId should point to the Resource that represents a context (2D or 3D canvas).
    
    * inspector/InjectedScriptCanvasModuleSource.js:
    (.):
    * inspector/Inspector.json:
    * inspector/front-end/CanvasProfileView.js:
    
    LayoutTests:
    
    Replaced hacky stub with actual Cache implementation.
    
    * inspector/profiler/webgl/webgl-profiler-get-error.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/CanvasProfileView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/CanvasProfileView.js
@@ -218,7 +218,6 @@ WebInspector.CanvasProfileView.prototype = {
      */
     _createCallNode: function(index, call)
     {
-        var traceLogItem = document.createElement("div");
         var data = {};
         data[0] = index + 1;
         data[1] = call.functionName || "context." + call.property;
@@ -230,16 +229,18 @@ WebInspector.CanvasProfileView.prototype = {
             data[2] = this._linkifier.linkifyLocation(call.sourceURL, lineNumber, columnNumber);
         }
 
-        if (call.arguments)
-            data[1] += "(" + call.arguments.join(", ") + ")";
-        else
-            data[1] += " = " + call.value;
+        if (call.arguments) {
+            var args = call.arguments.map(function(argument) {
+                return argument.description;
+            });
+            data[1] += "(" + args.join(", ") + ")";
+        } else
+            data[1] += " = " + call.value.description;
 
         if (typeof call.result !== "undefined")
-            data[1] += " => " + call.result;
+            data[1] += " => " + call.result.description;
 
         var node = new WebInspector.DataGridNode(data);
-        node.call = call;
         node.index = index;
         node.selectable = true;
         return node;
