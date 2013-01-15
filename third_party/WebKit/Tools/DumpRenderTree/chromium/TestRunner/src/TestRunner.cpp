ca43733e180cd322bc342975c39fa8129f492a66
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-15 08:19:57 +0000

    [chromium] move remaining methods that just set a boolean flag to TestRunner library https://bugs.webkit.org/show_bug.cgi?id=106823
    Reviewed by Darin Fisher.
    
    * DumpRenderTree/chromium/DRTTestRunner.cpp:
    (DRTTestRunner::DRTTestRunner):
    (DRTTestRunner::reset):
    * DumpRenderTree/chromium/DRTTestRunner.h:
    (DRTTestRunner):
    * DumpRenderTree/chromium/TestRunner/public/WebTestRunner.h:
    (WebTestRunner::WebTestRunner::shouldDumpBackForwardList):
    (WebTestRunner::WebTestRunner::deferMainResourceDataLoad):
    (WebTestRunner::WebTestRunner::shouldDumpSelectionRect):
    (WebTestRunner::WebTestRunner::testRepaint):
    (WebTestRunner::WebTestRunner::sweepHorizontally):
    (WebTestRunner::WebTestRunner::isPrinting):
    (WebTestRunner::WebTestRunner::shouldStayOnPageAfterHandlingBeforeUnload):
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp:
    (WebTestRunner::TestRunner::TestRunner):
    (WebTestRunner::TestRunner::reset):
    (WebTestRunner::TestRunner::shouldDumpBackForwardList):
    (WebTestRunner):
    (WebTestRunner::TestRunner::deferMainResourceDataLoad):
    (WebTestRunner::TestRunner::shouldDumpSelectionRect):
    (WebTestRunner::TestRunner::testRepaint):
    (WebTestRunner::TestRunner::sweepHorizontally):
    (WebTestRunner::TestRunner::isPrinting):
    (WebTestRunner::TestRunner::shouldStayOnPageAfterHandlingBeforeUnload):
    (WebTestRunner::TestRunner::dumpBackForwardList):
    (WebTestRunner::TestRunner::setDeferMainResourceDataLoad):
    (WebTestRunner::TestRunner::dumpSelectionRect):
    (WebTestRunner::TestRunner::repaintSweepHorizontally):
    (WebTestRunner::TestRunner::setPrinting):
    (WebTestRunner::TestRunner::setShouldStayOnPageAfterHandlingBeforeUnload):
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.h:
    (TestRunner):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139722 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp
@@ -163,6 +163,13 @@ TestRunner::TestRunner()
     bindMethod("setAllowRunningOfInsecureContent", &TestRunner::setAllowRunningOfInsecureContent);
     bindMethod("dumpStatusCallbacks", &TestRunner::dumpWindowStatusChanges);
     bindMethod("dumpProgressFinishedCallback", &TestRunner::dumpProgressFinishedCallback);
+    bindMethod("dumpBackForwardList", &TestRunner::dumpBackForwardList);
+    bindMethod("setDeferMainResourceDataLoad", &TestRunner::setDeferMainResourceDataLoad);
+    bindMethod("dumpSelectionRect", &TestRunner::dumpSelectionRect);
+    bindMethod("testRepaint", &TestRunner::testRepaint);
+    bindMethod("repaintSweepHorizontally", &TestRunner::repaintSweepHorizontally);
+    bindMethod("setPrinting", &TestRunner::setPrinting);
+    bindMethod("setShouldStayOnPageAfterHandlingBeforeUnload", &TestRunner::setShouldStayOnPageAfterHandlingBeforeUnload);
 
     // The following methods interact with the WebTestProxy.
     bindMethod("sendWebIntentResponse", &TestRunner::sendWebIntentResponse);
@@ -255,6 +262,13 @@ void TestRunner::reset()
     m_dumpResourceResponseMIMETypes = false;
     m_dumpWindowStatusChanges = false;
     m_dumpProgressFinishedCallback = false;
+    m_dumpBackForwardList = false;
+    m_deferMainResourceDataLoad = true;
+    m_dumpSelectionRect = false;
+    m_testRepaint = false;
+    m_sweepHorizontally = false;
+    m_isPrinting = false;
+    m_shouldStayOnPageAfterHandlingBeforeUnload = false;
 
     m_globalFlag.set(false);
     m_platformName.set("chromium");
@@ -379,6 +393,41 @@ bool TestRunner::shouldDumpProgressFinishedCallback() const
     return m_dumpProgressFinishedCallback;
 }
 
+bool TestRunner::shouldDumpBackForwardList() const
+{
+    return m_dumpBackForwardList;
+}
+
+bool TestRunner::deferMainResourceDataLoad() const
+{
+    return m_deferMainResourceDataLoad;
+}
+
+bool TestRunner::shouldDumpSelectionRect() const
+{
+    return m_dumpSelectionRect;
+}
+
+bool TestRunner::testRepaint() const
+{
+    return m_testRepaint;
+}
+
+bool TestRunner::sweepHorizontally() const
+{
+    return m_sweepHorizontally;
+}
+
+bool TestRunner::isPrinting() const
+{
+    return m_isPrinting;
+}
+
+bool TestRunner::shouldStayOnPageAfterHandlingBeforeUnload() const
+{
+    return m_shouldStayOnPageAfterHandlingBeforeUnload;
+}
+
 void TestRunner::dumpPermissionClientCallbacks(const CppArgumentList&, CppVariant* result)
 {
     m_webPermissions->setDumpCallbacks(true);
@@ -438,6 +487,50 @@ void TestRunner::dumpWindowStatusChanges(const CppArgumentList&, CppVariant* res
 void TestRunner::dumpProgressFinishedCallback(const CppArgumentList&, CppVariant* result)
 {
     m_dumpProgressFinishedCallback = true;
+    result->setNull();
+}
+
+void TestRunner::dumpBackForwardList(const CppArgumentList&, CppVariant* result)
+{
+    m_dumpBackForwardList = true;
+    result->setNull();
+}
+
+void TestRunner::setDeferMainResourceDataLoad(const CppArgumentList& arguments, CppVariant* result)
+{
+    if (arguments.size() == 1)
+        m_deferMainResourceDataLoad = cppVariantToBool(arguments[0]);
+}
+
+void TestRunner::dumpSelectionRect(const CppArgumentList& arguments, CppVariant* result)
+{
+    m_dumpSelectionRect = true;
+    result->setNull();
+}
+
+void TestRunner::testRepaint(const CppArgumentList&, CppVariant* result)
+{
+    m_testRepaint = true;
+    result->setNull();
+}
+
+void TestRunner::repaintSweepHorizontally(const CppArgumentList&, CppVariant* result)
+{
+    m_sweepHorizontally = true;
+    result->setNull();
+}
+
+void TestRunner::setPrinting(const CppArgumentList& arguments, CppVariant* result)
+{
+    m_isPrinting = true;
+    result->setNull();
+}
+
+void TestRunner::setShouldStayOnPageAfterHandlingBeforeUnload(const CppArgumentList& arguments, CppVariant* result)
+{
+    if (arguments.size() == 1 && arguments[0].isBool())
+        m_shouldStayOnPageAfterHandlingBeforeUnload = arguments[0].toBoolean();
+
     result->setNull();
 }
 
