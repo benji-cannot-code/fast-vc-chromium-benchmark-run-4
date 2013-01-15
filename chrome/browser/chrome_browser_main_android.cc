667a802bf06f62be176b6d0477b5c602197f057e
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 22:05:33 +0000

    Add missing include breaking Chrome for Android release builds.
    BUG=105778
    
    Review URL: https://codereview.chromium.org/11937002
    Patch from Aurimas Liutikas <aurimas@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176980 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_android.cc
+++ chrome/browser/chrome_browser_main_android.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/path_service.h"
 #include "chrome/app/breakpad_linux.h"
+#include "chrome/common/env_vars.h"
 #include "content/public/browser/android/compositor.h"
 #include "content/public/common/main_function_params.h"
 #include "net/android/network_change_notifier_factory_android.h"
