6a4e07193bda2f92c92349efe4ad2a1f4ea65501
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 07:06:46 +0000

    Send ChromeViewMsg_NetErrorInfo on DNS probe results
    BUG=156415
    
    
    Review URL: https://chromiumcodereview.appspot.com/11828022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176817 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/net_error_tab_helper.cc
+++ chrome/browser/net/net_error_tab_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/net/net_error_info.h"
 #include "chrome/common/pref_names.h"
+#include "chrome/common/render_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "net/base/net_errors.h"
 
@@ -133,7 +134,8 @@ void NetErrorTabHelper::OnDnsProbeFinished(DnsProbeResult result) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(dns_probe_running_);
 
-  // TODO(ttuttle): Notify renderer of probe results.
+  // Notify renderer of DNS probe results.
+  Send(new ChromeViewMsg_NetErrorInfo(routing_id(), result));
 
   set_dns_probe_running(false);
 }
