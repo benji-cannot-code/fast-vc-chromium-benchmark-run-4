63e8fc762d096c92a3af1892fc7504e9cfedf62f
sheu@chromium.org <sheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 07:06:33 +0000

    Enforce non-negative BitstreamBuffer id
    There is some video decode accelerator code that assumes that a negative
    BitstreamBuffer id is never given, and uses it to mark error conditions.
    
    * Check for non-negative id when receving over IPC.
    * Make sure we don't send negative ids from our video decode pipeline (by
      handling wrapping)
    * Update PPAPI interface to document the fact that negative ids should not
      be used.
    
    BUG=chromium:168293
    TEST=local build, run on snow
    
    
    Review URL: https://chromiumcodereview.appspot.com/11826064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176816 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/c/dev/pp_video_dev.h
+++ ppapi/c/dev/pp_video_dev.h
@@ -62,7 +62,8 @@ PP_COMPILE_ASSERT_SIZE_IN_BYTES(PP_VideoDecoder_Profile, 4);
  */
 struct PP_VideoBitstreamBuffer_Dev {
   /**
-   * Client-specified identifier for the bitstream buffer.
+   * Client-specified identifier for the bitstream buffer. Valid values are
+   * non-negative.
    */
   int32_t id;
   /**
@@ -84,7 +85,7 @@ struct PP_PictureBuffer_Dev {
   /**
    * Client-specified id for the picture buffer. By using this value client can
    * keep track of the buffers it has assigned to the video decoder and how they
-   * are passed back to it.
+   * are passed back to it. Valid values are non-negative.
    */
   int32_t id;
   /**
