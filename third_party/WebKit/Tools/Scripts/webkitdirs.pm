13f31af8b3a8dec1330f01c56bdcfeddb5c3c94f
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-15 19:42:41 +0000

    [GTK] Stop passing the unstable features flag to configure script when using build-webkit https://bugs.webkit.org/show_bug.cgi?id=106579
    Patch by Zan Dobersek <zandobersek@gmail.com> on 2013-01-15
    Reviewed by Gustavo Noronha Silva.
    
    The --enable-unstable-features flag is not required to be passed anymore
    to the configure script as it was removed in r138252.
    
    * Scripts/webkitdirs.pm:
    (buildAutotoolsProject):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139766 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitdirs.pm
+++ third_party/WebKit/Tools/Scripts/webkitdirs.pm
@@ -2080,9 +2080,6 @@ sub buildAutotoolsProject($@)
         push @buildArgs, "--disable-debug";
     }
 
-    # Enable unstable features when building through build-webkit.
-    push @buildArgs, "--enable-unstable-features";
-
     if (checkForArgumentAndRemoveFromArrayRef("--update-gtk", \@buildArgs)) {
         # Force autogen to run, to catch the possibly updated libraries.
         system("rm -f previous-autogen-arguments.txt");
