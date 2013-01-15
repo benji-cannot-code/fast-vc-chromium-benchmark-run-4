8f259041146e876d9787528ee771a373d9a663d6
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 13:27:37 +0000

    Telemetry (memory benchmark): Add histogram Memory.RendererUsed.
    Also: handle empty histograms gracefully.
    
    BUG=NONE
    R=nduca
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11826053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176895 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/memory_benchmark.py
+++ tools/perf/perf_tools/memory_benchmark.py
@@ -7,7 +7,8 @@ from telemetry import multi_page_benchmark
 MEMORY_HISTOGRAMS = [
     {'name': 'V8.MemoryExternalFragmentationTotal', 'units': 'percent'},
     {'name': 'V8.MemoryHeapSampleTotalCommitted', 'units': 'kb'},
-    {'name': 'V8.MemoryHeapSampleTotalUsed', 'units': 'kb'}]
+    {'name': 'V8.MemoryHeapSampleTotalUsed', 'units': 'kb'},
+    {'name': 'Memory.RendererUsed', 'units': 'kb'}]
 
 class MemoryBenchmark(multi_page_benchmark.MultiPageBenchmark):
   def __init__(self):
@@ -22,6 +23,7 @@ class MemoryBenchmark(multi_page_benchmark.MultiPageBenchmark):
     # customizing, so that we get the same generic histograms produced for all
     # pages.
     options.AppendExtraBrowserArg('--disable-histogram-customizer')
+    options.AppendExtraBrowserArg('--memory-metrics')
 
   def CanRunForPage(self, page):
     return hasattr(page, 'stress_memory')
