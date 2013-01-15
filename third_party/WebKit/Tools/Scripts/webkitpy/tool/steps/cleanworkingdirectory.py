aedfbe6a8fa0100c97af0e89279e769db5d042ae
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-15 05:16:20 +0000

    Changing clean_working_directory/clean_local_commits and related functions to have consistent naming.
    https://bugs.webkit.org/show_bug.cgi?id=104198
    
    Patch by Tim 'mithro' Ansell <mithro@mithis.com> on 2013-01-14
    Reviewed by Adam Barth.
    
    * Scripts/webkitpy/common/checkout/scm/git.py:
    * Scripts/webkitpy/common/checkout/scm/scm.py:
    * Scripts/webkitpy/common/checkout/scm/scm_mock.py:
    * Scripts/webkitpy/common/checkout/scm/scm_unittest.py:
    * Scripts/webkitpy/tool/steps/cleanworkingdirectory.py:
    * Scripts/webkitpy/tool/steps/cleanworkingdirectory_unittest.py:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139712 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/cleanworkingdirectory.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/steps/cleanworkingdirectory.py
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from webkitpy.tool.steps.abstractstep import AbstractStep
 from webkitpy.tool.steps.options import Options
+from webkitpy.common.system.executive import ScriptError
 
 
 class CleanWorkingDirectory(AbstractStep):
@@ -47,5 +48,9 @@ class CleanWorkingDirectory(AbstractStep):
         if not self._options.clean:
             return
         if not self._allow_local_commits:
-            self._tool.scm().ensure_no_local_commits(self._options.force_clean)
-        self._tool.scm().ensure_clean_working_directory(force_clean=self._options.force_clean)
+            if self._tool.scm().has_local_commits() and not self._options.force_clean:
+                raise ScriptError("Repository has local commits, pass --force-clean to continue.")
+            self._tool.scm().discard_local_commits()
+        if self._tool.scm().has_working_directory_changes() and not self._options.force_clean:
+            raise ScriptError("Working directory has changes, pass --force-clean to continue.")
+        self._tool.scm().discard_working_directory_changes()
