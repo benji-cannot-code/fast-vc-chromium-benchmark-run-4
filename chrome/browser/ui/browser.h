725594bf3edc453688552f8afac991818160e84e
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-15 06:47:11 +0000

    Adding new default behavior to maximize new windows automatically if screen resolution is too small.
    BUG=168178
    TEST=unit tested
    
    
    Review URL: https://chromiumcodereview.appspot.com/11824069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@176814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.h
+++ chrome/browser/ui/browser.h
@@ -468,7 +468,7 @@ class Browser : public TabStripModelObserver,
   void MaybeUpdateBookmarkBarStateForInstantPreview(
       const chrome::search::Mode& mode);
 
-  FullscreenController* fullscreen_controller() {
+  FullscreenController* fullscreen_controller() const {
     return fullscreen_controller_.get();
   }
 
--- chrome/test/base/in_process_browser_test.cc
+++ chrome/test/base/in_process_browser_test.cc
@@ -329,6 +329,11 @@ void InProcessBrowserTest::RunTestOnMainThreadLoop() {
 
   if (!BrowserList::empty()) {
     browser_ = *BrowserList::begin();
+#if defined(USE_ASH)
+    // There are cases where windows get created maximized by default.
+    if (browser_->window()->IsMaximized())
+      browser_->window()->Restore();
+#endif
     content::WaitForLoadStop(chrome::GetActiveWebContents(browser_));
   }
 
