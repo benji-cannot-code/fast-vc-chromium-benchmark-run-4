1b91b3cddfef20adcf80150831aafa8bf5a7c082
dmazzoni@google.com <dmazzoni@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-16 00:13:15 +0000

    Use-after-free in AXObjectCache::notificationPostTimerFired https://bugs.webkit.org/show_bug.cgi?id=106106
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Fixes a crash that occurs when a Node is adopted by another document,
    in particular one that isn't part of the page, and then deleted,
    which wasn't triggering the code that removed the Node from
    AXObjectCache. Now, a Node is removed from the AXObjectCache whenever
    its Document changes.
    
    Test: accessibility/crash-adopt-node-from-new-document.html
    
    * dom/Node.cpp:
    (WebCore::Node::didMoveToNewDocument):
    
    LayoutTests:
    
    Test that demonstrates the crash when a Node with an
    AccessibilityObject changes its document and then isn't removed from
    the AXObjectCache when it's deleted.
    
    * accessibility/crash-adopt-node-from-new-document-expected.txt: Added.
    * accessibility/crash-adopt-node-from-new-document.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.cpp
+++ third_party/WebKit/Source/WebCore/dom/Node.cpp
@@ -2080,6 +2080,9 @@ void Node::didMoveToNewDocument(Document* oldDocument)
 {
     TreeScopeAdopter::ensureDidMoveToNewDocumentWasCalled(oldDocument);
 
+    if (AXObjectCache::accessibilityEnabled() && oldDocument && oldDocument->axObjectCacheExists())
+        oldDocument->axObjectCache()->remove(this);
+
     // FIXME: Event listener types for this node should be set on the new owner document here.
 
     const EventListenerVector& wheelListeners = getEventListeners(eventNames().mousewheelEvent);
