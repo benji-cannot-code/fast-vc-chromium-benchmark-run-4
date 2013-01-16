b9118f108594b6bb5aa22a7d6d7bb29a48d4b7d5
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 23:24:22 +0000

    Update some #includes for headers that have moved from WebKit/chromium/public/platform/ to Platform/chromium/public/
    Review URL: https://chromiumcodereview.appspot.com/11971020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177259 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/bench/bench_main.cc
+++ ui/aura/bench/bench_main.cc
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GL_GLEXT_PROTOTYPES
 #define GL_GLEXT_PROTOTYPES 1
 #endif
+#include "third_party/WebKit/Source/Platform/chromium/public/WebGraphicsContext3D.h"
 #include "third_party/khronos/GLES2/gl2ext.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebGraphicsContext3D.h"
 
 #if defined(USE_X11)
 #include "base/message_pump_aurax11.h"
