d8803ce798ca5b02adac573843a7f128bc90a598
jocelyn.turcotte@digia.com <jocelyn.turcotte@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-16 13:55:49 +0000

    [Qt] Crash in WebCore::CachedFrame::destroy https://bugs.webkit.org/show_bug.cgi?id=104525
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Add an assert to increase the chances of catching this crash
    early on in the future.
    
    * dom/Document.cpp:
    (WebCore::Document::takeDOMWindowFrom):
    
    Source/WebKit/qt:
    
    Remove the call to HistoryController::setCurrentItem which is ultimately
    causing the initial empty document of a page to be added to the page cache.
    
    This re-introduce the bug that was fixed by this line, which will be
    properly fixed in a follow-up patch.
    
    * Api/qwebhistory.cpp:
    (operator>>):
    * tests/qwebhistory/tst_qwebhistory.cpp:
    (tst_QWebHistory::saveAndRestore_crash_4): Cover the crash.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139876 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/tests/qwebhistory/tst_qwebhistory.cpp
+++ third_party/WebKit/Source/WebKit/qt/tests/qwebhistory/tst_qwebhistory.cpp
@@ -57,9 +57,12 @@ private Q_SLOTS:
     void serialize_1(); //QWebHistory countity
     void serialize_2(); //QWebHistory index
     void serialize_3(); //QWebHistoryItem
+    // Those tests shouldn't crash
     void saveAndRestore_crash_1();
     void saveAndRestore_crash_2();
     void saveAndRestore_crash_3();
+    void saveAndRestore_crash_4();
+
     void popPushState_data();
     void popPushState();
     void clear();
@@ -309,7 +312,6 @@ static void restoreHistory(QWebHistory* history, QByteArray* out)
     load >> *history;
 }
 
-/** The test shouldn't crash */
 void tst_QWebHistory::saveAndRestore_crash_1()
 {
     QByteArray buffer;
@@ -320,7 +322,6 @@ void tst_QWebHistory::saveAndRestore_crash_1()
     }
 }
 
-/** The test shouldn't crash */
 void tst_QWebHistory::saveAndRestore_crash_2()
 {
     QByteArray buffer;
@@ -334,7 +335,6 @@ void tst_QWebHistory::saveAndRestore_crash_2()
     delete page2;
 }
 
-/** The test shouldn't crash */
 void tst_QWebHistory::saveAndRestore_crash_3()
 {
     QByteArray buffer;
@@ -352,6 +352,27 @@ void tst_QWebHistory::saveAndRestore_crash_3()
         hist2->clear();
     }
     delete page2;
+}
+
+void tst_QWebHistory::saveAndRestore_crash_4()
+{
+    QByteArray buffer;
+    saveHistory(hist, &buffer);
+
+    QWebPage* page2 = new QWebPage(this);
+    // The initial crash was in PageCache.
+    page2->settings()->setMaximumPagesInCache(3);
+
+    // Load the history in a new page, waiting for the load to finish.
+    QEventLoop waitForLoadFinished;
+    QObject::connect(page2, SIGNAL(loadFinished(bool)), &waitForLoadFinished, SLOT(quit()), Qt::QueuedConnection);
+    QDataStream load(&buffer, QIODevice::ReadOnly);
+    load >> *page2->history();
+    waitForLoadFinished.exec();
+
+    delete page2;
+    // Give some time for the PageCache cleanup 0-timer to fire.
+    QTest::qWait(50);
 }
 
 void tst_QWebHistory::popPushState_data()
