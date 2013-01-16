7d845cd8533f48f9612fcb9ec764a2fa54975fb0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-16 22:39:46 +0000

    Add missing sys/mman.h include on Mac https://bugs.webkit.org/show_bug.cgi?id=98089
    Patch by Jonathan Liu <net147@gmail.com> on 2013-01-16
    Reviewed by Darin Adler.
    
    The madvise function and MADV_FREE constant require sys/mman.h.
    
    * jit/ExecutableAllocatorFixedVMPool.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139926 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jit/ExecutableAllocatorFixedVMPool.cpp
+++ third_party/WebKit/Source/JavaScriptCore/jit/ExecutableAllocatorFixedVMPool.cpp
@@ -37,6 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/PageReservation.h>
 #include <wtf/VMTags.h>
 
+#if OS(DARWIN)
+#include <sys/mman.h>
+#endif
+
 #if OS(LINUX)
 #include <stdio.h>
 #endif
