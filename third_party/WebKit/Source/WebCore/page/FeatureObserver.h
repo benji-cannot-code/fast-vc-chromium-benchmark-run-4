37eca62659168a76dc95d4562f0d5eb7f24a928f
alexis@webkit.org <alexis@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-16 21:58:45 +0000

    Monitor usage of unprefixed and prefixed DOM events for CSS Transitions. https://bugs.webkit.org/show_bug.cgi?id=107004
    Reviewed by Adam Barth.
    
    Monitor the usage of transitionEnd events as well as
    webkitTransitionEnd events so we can know in the future how much the
    prefixed version is used to remove it becomes irrelevant.
    
    No new tests : no behavior changes, we're just adding monitoring.
    
    * dom/EventTarget.cpp:
    (WebCore):
    (WebCore::EventTarget::fireEventListeners):
    * page/FeatureObserver.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139922 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/FeatureObserver.h
+++ third_party/WebKit/Source/WebCore/page/FeatureObserver.h
@@ -58,6 +58,9 @@ public:
         ContentSecurityPolicy,
         ContentSecurityPolicyReportOnly,
         PrefixedContentSecurityPolicyReportOnly,
+        PrefixedTransitionEndEvent,
+        UnprefixedTransitionEndEvent,
+        PrefixedAndUnprefixedTransitionEndEvent,
         // Add new features above this line.
         NumberOfFeatures, // This enum value must be last.
     };
