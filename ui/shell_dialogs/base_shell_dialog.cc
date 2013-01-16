9cf684ce1c18fa4b3bc1aac829b1305bed7dba30
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 23:08:27 +0000

    Bring back the file dialogs for chrome AURA.
    This CL copies the ui\base\dialogs sources to a new directory ui\shell_folders. This is
    done due to problems with gcl not working correctly in conjunction with svn mv and local edits leading
    to tryjob issues.
    
    The code in ui\shell_folders is not compiled at the moment.
    
    This will be compiled when CL https://codereview.chromium.org/11878031 lands.
    
    BUG=170483
    TBR=ben
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177251 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/shell_dialogs/base_shell_dialog.cc
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/base/dialogs/base_shell_dialog.h"
+
+namespace ui {
+
+BaseShellDialog::~BaseShellDialog() {}
+
+}  // namespace ui
