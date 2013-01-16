9dc35453c9428ca012a4d0b58221147b4713dc1e
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 09:13:11 +0000

    Fixed OAuth2 policy fetching retry logic.
    BUG=169999
    TEST=desktopui_ScreenLocker (ChromeOS BVT)
    
    
    Review URL: https://chromiumcodereview.appspot.com/11969010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/oauth2_policy_fetcher.h
+++ chrome/browser/chromeos/login/oauth2_policy_fetcher.h
@@ -11,17 +11,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback_forward.h"
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "google_apis/gaia/gaia_auth_consumer.h"
 #include "google_apis/gaia/oauth2_access_token_consumer.h"
+#include "net/url_request/url_request_context_getter.h"
 
 class GaiaAuthFetcher;
 class OAuth2AccessTokenFetcher;
-
-namespace net {
-class URLRequestContextGetter;
-}
 
 namespace chromeos {
 
@@ -91,6 +89,8 @@ class OAuth2PolicyFetcher : public base::SupportsWeakPtr<OAuth2PolicyFetcher>,
   void RetryOnError(const GoogleServiceAuthError& error,
                     const base::Closure& task);
 
+  scoped_refptr<net::URLRequestContextGetter> auth_context_getter_;
+  scoped_refptr<net::URLRequestContextGetter> system_context_getter_;
   scoped_ptr<GaiaAuthFetcher> refresh_token_fetcher_;
   scoped_ptr<OAuth2AccessTokenFetcher> access_token_fetcher_;
   std::string policy_token_;
