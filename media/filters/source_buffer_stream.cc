c86fd2f5c6ee7203df12cac9637c8b8a914e1060
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 01:25:07 +0000

    Add trace events for media source extension (MSE).
    Even though SourceBufferRange garbage collection (GC) performance has been
    improved dramatically in r176277, I still observed that Append() with GC
    triggered is much slower than Append() without GC (12ms vs 2ms on Linux debug
    build), for the same amount of data to be appended. The current performance
    is okay for most applications I see, so it's not the highest priority to
    improve the GC performance furthermore now. But it'll be nice to have tracing
    event in place so that whenever we see performance issue, we could turn on the
    trace events to investigate.
    
    BUG=169971
    TEST=Trace events works (choose "mse" category).
    
    
    Review URL: https://chromiumcodereview.appspot.com/11878033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177042 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/source_buffer_stream.cc
+++ media/filters/source_buffer_stream.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/bind.h"
+#include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
 namespace media {
@@ -365,6 +366,10 @@ void SourceBufferStream::OnNewMediaSegment(
 
 bool SourceBufferStream::Append(
     const SourceBufferStream::BufferQueue& buffers) {
+  TRACE_EVENT2("mse", "SourceBufferStream::Append",
+               "stream type", GetStreamTypeName(),
+               "buffers to append", buffers.size());
+
   DCHECK(!buffers.empty());
   DCHECK(media_segment_start_time_ != kNoTimestamp());
 
@@ -555,6 +560,10 @@ void SourceBufferStream::GarbageCollectIfNeeded() {
 
 int SourceBufferStream::FreeBuffers(int total_bytes_to_free,
                                     bool reverse_direction) {
+  TRACE_EVENT2("mse", "SourceBufferStream::FreeBuffers",
+               "total bytes to free", total_bytes_to_free,
+               "reverse direction", reverse_direction);
+
   DCHECK_GT(total_bytes_to_free, 0);
   int bytes_to_free = total_bytes_to_free;
   int bytes_freed = 0;
@@ -1233,6 +1242,16 @@ base::TimeDelta SourceBufferStream::FindKeyframeAfterTimestamp(
   // First check for a keyframe timestamp >= |timestamp|
   // in the current range.
   return (*itr)->NextKeyframeTimestamp(timestamp);
+}
+
+std::string SourceBufferStream::GetStreamTypeName() const {
+  if (!video_configs_.empty()) {
+    DCHECK(audio_configs_.empty());
+    return "VIDEO";
+  }
+
+  DCHECK(!audio_configs_.empty());
+  return "AUDIO";
 }
 
 SourceBufferRange::SourceBufferRange(
