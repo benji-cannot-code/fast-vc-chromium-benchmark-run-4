a79a58931f1eea35cac754daf879982d726db443
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 04:21:34 +0000

    Fixed flickering "Computing selection" label.
    In Files.app, while changing selection "computing selection..." label was shown too frequently causing flickering. This patch simplified the code by removing this redundant label.
    Along the way, the panel's contents disappearing has been fixed while changing selection very fast.
    
    TEST=Follow the bug description.
    BUG=167799
    
    
    Review URL: https://chromiumcodereview.appspot.com/11788026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177086 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/selection.js
+++ chrome/browser/resources/file_manager/js/selection.js
@@ -192,10 +192,6 @@ SelectionHandler.prototype.onSelectionChanged = function(event) {
   if (this.selectionUpdateTimer_) {
     clearTimeout(this.selectionUpdateTimer_);
     this.selectionUpdateTimer_ = null;
-
-    // The selection is changing quicker than we can update the UI.
-    // Clear the UI.
-    this.clearUI();
   }
 
   if (!indexes.length) {
@@ -205,7 +201,6 @@ SelectionHandler.prototype.onSelectionChanged = function(event) {
     return;
   }
 
-  this.previewText_.textContent = str('COMPUTING_SELECTION');
   this.hideCalculating_();
 
   // The rest of the selection properties are computed via (sometimes lengthy)
