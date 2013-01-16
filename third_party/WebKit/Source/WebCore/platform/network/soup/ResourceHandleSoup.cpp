a868c48ee96bf6dfaded1c944b7d586e77a56812
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-16 14:15:58 +0000

    [GStreamer][Soup] Let GStreamer provide the buffer data is downloaded to, to avoid copying https://bugs.webkit.org/show_bug.cgi?id=105552
    Patch by Gustavo Noronha Silva <gustavo.noronha@collabora.com> on 2013-01-15
    Reviewed by Philippe Normand.
    
    Makes it possible for the GStreamer media backend to provide the buffer to which
    the Soup networking backend will use to download data to. This makes copying
    memory unnecessary when ResourceHandle hands data over to the media player's
    StreamingClient. Thanks to Dan Winship for help designing the interface.
    
    No behaviour change, covered by existing tests.
    
    * platform/graphics/gstreamer/GStreamerVersioning.cpp:
    (getGstBufferSize): Abstract obtaining the size of the buffer, so the code
    is cleaner while still working for both GST 0.10 and 1.0.
    (setGstBufferSize): Ditto, but for setting the size.
    (getGstBufferDataPointer): Ditto, but for grabbing the data pointer.
    (mapGstBuffer): Convenience method to take care of mapping the buffer so that
    we can provide the data pointer to ResourceHandle.
    (unmapGstBuffer): Convenience method which takes care of unmapping the buffer
    and properly freeing the GstMapInfo.
    * platform/graphics/gstreamer/GStreamerVersioning.h:
    * platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (StreamingClient): New methods.
    (_WebKitWebSrcPrivate): We now store the GstBuffer we provided the data pointer from
    so we can later unmap it and push it to the pipeline.
    (webKitWebSrcDispose): Deal with the GstBuffer in case it exists when the source is
    destroyed.
    (webKitWebSrcStop): Also clear the GstBuffer in this case.
    (StreamingClient::didReceiveData): Handle the hand-over of the buffer.
    (StreamingClient::getBuffer): Provide ResourceHandle with a new GstBuffer's data pointer.
    * platform/network/ResourceHandleClient.h:
    (ResourceHandleClient):
    (WebCore::ResourceHandleClient::ResourceHandleClient): Constructor to initialize the buffer
    member variable to 0.
    (WebCore::ResourceHandleClient::~ResourceHandleClient): Destructor to free the buffer if it
    has been allocated.
    (WebCore::ResourceHandleClient::getBuffer): Default implementation which returns a
    newly allocated char pointer.
    * platform/network/ResourceHandleInternal.h:
    (WebCore::ResourceHandleInternal::ResourceHandleInternal):
    (ResourceHandleInternal): Store actual buffer size, which is no longer a constant.
    * platform/network/soup/ResourceHandleSoup.cpp:
    (WebCore::cleanupSoupRequestOperation): Clear the buffer pointer, the life-cycle of the
    buffer is handled by the ResourceHandleClient.
    (WebCore::nextMultipartResponsePartCallback): Get a new buffer from the client before reading.
    (WebCore::sendRequestCallback): Ditto.
    (WebCore::readCallback): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139877 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/soup/ResourceHandleSoup.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/soup/ResourceHandleSoup.cpp
@@ -561,8 +561,8 @@ static void cleanupSoupRequestOperation(ResourceHandle* handle, bool isDestroyin
     }
 
     if (d->m_buffer) {
-        g_slice_free1(READ_BUFFER_SIZE, d->m_buffer);
         d->m_buffer = 0;
+        d->m_bufferSize = 0;
     }
 
     if (d->m_timeoutSource) {
@@ -634,7 +634,9 @@ static void nextMultipartResponsePartCallback(GObject* /*source*/, GAsyncResult*
         return;
     }
 
-    g_input_stream_read_async(d->m_inputStream.get(), d->m_buffer, READ_BUFFER_SIZE,
+    d->m_buffer = client->getBuffer(READ_BUFFER_SIZE, &d->m_bufferSize);
+
+    g_input_stream_read_async(d->m_inputStream.get(), d->m_buffer, d->m_bufferSize,
         G_PRIORITY_DEFAULT, d->m_cancellable.get(), readCallback, handle.get());
 }
 
@@ -698,8 +700,6 @@ static void sendRequestCallback(GObject*, GAsyncResult* result, gpointer data)
         return;
     }
 
-    d->m_buffer = static_cast<char*>(g_slice_alloc(READ_BUFFER_SIZE));
-
     if (soupMessage && d->m_response.isMultipart()) {
         d->m_multipartInputStream = adoptGRef(soup_multipart_input_stream_new(soupMessage, inputStream.get()));
         soup_multipart_input_stream_next_part_async(d->m_multipartInputStream.get(), G_PRIORITY_DEFAULT,
@@ -708,7 +708,12 @@ static void sendRequestCallback(GObject*, GAsyncResult* result, gpointer data)
     }
 
     d->m_inputStream = inputStream;
-    g_input_stream_read_async(d->m_inputStream.get(), d->m_buffer, READ_BUFFER_SIZE,
+
+    ASSERT(!d->m_buffer);
+
+    d->m_buffer = client->getBuffer(READ_BUFFER_SIZE, &d->m_bufferSize);
+
+    g_input_stream_read_async(d->m_inputStream.get(), d->m_buffer, d->m_bufferSize,
                               G_PRIORITY_DEFAULT, d->m_cancellable.get(), readCallback, handle.get());
 }
 
@@ -1363,7 +1368,9 @@ static void readCallback(GObject*, GAsyncResult* asyncResult, gpointer data)
         return;
     }
 
-    g_input_stream_read_async(d->m_inputStream.get(), d->m_buffer, READ_BUFFER_SIZE, G_PRIORITY_DEFAULT,
+    d->m_buffer = client->getBuffer(READ_BUFFER_SIZE, &d->m_bufferSize);
+
+    g_input_stream_read_async(d->m_inputStream.get(), d->m_buffer, d->m_bufferSize, G_PRIORITY_DEFAULT,
                               d->m_cancellable.get(), readCallback, handle.get());
 }
 
