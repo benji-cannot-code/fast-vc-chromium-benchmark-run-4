b0b780e3b523b74f9ebe98a2ae8e3b3adecd30f4
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 09:12:45 +0000

    [cc] Use base::TimeTicks in RenderStats
    Review URL: https://chromiumcodereview.appspot.com/11896002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177112 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/bitmap_skpicture_content_layer_updater.cc
+++ cc/bitmap_skpicture_content_layer_updater.cc
@@ -30,7 +30,7 @@ void BitmapSkPictureContentLayerUpdater::Resource::update(ResourceUpdateQueue& q
     SkCanvas canvas(&device);
     base::TimeTicks paintBeginTime = base::TimeTicks::Now();
     updater()->paintContentsRect(&canvas, sourceRect, stats);
-    stats.totalPaintTimeInSeconds += (base::TimeTicks::Now() - paintBeginTime).InSecondsF();
+    stats.totalPaintTime += base::TimeTicks::Now() - paintBeginTime;
 
     ResourceUpdate upload = ResourceUpdate::Create(
         texture(), &m_bitmap, sourceRect, sourceRect, destOffset);
@@ -66,7 +66,7 @@ void BitmapSkPictureContentLayerUpdater::paintContentsRect(SkCanvas* canvas, con
                       contentRect().y() - sourceRect.y());
     base::TimeTicks rasterizeBeginTime = base::TimeTicks::Now();
     drawPicture(canvas);
-    stats.totalRasterizeTimeInSeconds += (base::TimeTicks::Now() - rasterizeBeginTime).InSecondsF();
+    stats.totalRasterizeTime += base::TimeTicks::Now() - rasterizeBeginTime;
     stats.totalPixelsRasterized += sourceRect.width() * sourceRect.height();
 }
 
