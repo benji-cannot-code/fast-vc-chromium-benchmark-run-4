24dec3e8c98de44ee316b38c1bb71e57e07d2414
agable@chromium.org <agable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 21:29:35 +0000

    Revert 177087.
    This change appears to have broken many perf bots.
    See bug at https://code.google.com/p/chromium/issues/detail?id=170310.
    
    > [Telemetry] Clean separation between tab (public API) and tab_backend (Chrome implementation). Flatten tab's public methods.
    > 
    > E.g. instead of tab.page.Navigate('url') it's now tab.Navigate('url'). This makes more sense for other browsers, who won't have the same domain divisions that Chrome DevTools does; and paves the way for future organization/separation between public API and backends.
    > 
    > BUG=None.
    > TEST=None.
    > 
    > Review URL: https://codereview.chromium.org/11819018
    
    TBR=dtu@chromium.org
    Review URL: https://codereview.chromium.org/11931026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177225 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/inspector_console.py
+++ tools/telemetry/telemetry/inspector_console.py
@@ -6,9 +6,10 @@ import json
 import logging
 
 class InspectorConsole(object):
-  def __init__(self, tab_backend):
-    self._tab_backend = tab_backend
-    self._tab_backend.RegisterDomain(
+  def __init__(self, inspector_backend, tab):
+    self._tab = tab
+    self._inspector_backend = inspector_backend
+    self._inspector_backend.RegisterDomain(
         'Console',
         self._OnNotification,
         self._OnClose)
@@ -35,13 +36,12 @@ class InspectorConsole(object):
   def _OnClose(self):
     pass
 
-  # False positive in PyLint 0.25.1: http://www.logilab.org/89092
   @property
-  def message_output_stream(self):  # pylint: disable=E0202
+  def MessageOutputStream(self):
     return self._message_output_stream
 
-  @message_output_stream.setter
-  def message_output_stream(self, stream):  # pylint: disable=E0202
+  @MessageOutputStream.setter
+  def MessageOutputStream(self, stream):
     self._message_output_stream = stream
     self._UpdateConsoleEnabledState()
 
@@ -54,7 +54,7 @@ class InspectorConsole(object):
       method_name = 'enable'
     else:
       method_name = 'disable'
-    self._tab_backend.SyncRequest({
+    self._inspector_backend.SyncRequest({
         'method': 'Console.%s' % method_name
         })
     self._console_enabled = enabled
--- tools/telemetry/telemetry/page_runner.py
+++ tools/telemetry/telemetry/page_runner.py
@@ -29,7 +29,7 @@ class _RunState(object):
 
   def Close(self):
     if self.tab:
-      self.tab.Close()
+      self.tab.Disconnect()
       self.tab = None
 
     if self.browser:
