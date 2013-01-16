77a2c28684450d63d9d6c1b6619e2358ca6b1819
halyavin@google.com <halyavin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 10:37:04 +0000

    NaCl test: Explicitly enable GDB debug stub logging via environment variable.
    We are increasing verbosity level for debug stub log messages.  Now in order to see them in the log,
    we need to pass verbosity option to Native Client module.
    
    The NaCl side of the change is here: https://codereview.chromium.org/11881048/
    
    BUG= none
    TEST= NaClGdbDebugStubTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/11875038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/test/gdb_debug_stub_browsertest.cc
+++ chrome/browser/nacl_host/test/gdb_debug_stub_browsertest.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/command_line.h"
+#include "base/environment.h"
 #include "base/path_service.h"
 #include "base/process_util.h"
 #include "base/string_number_conversions.h"
@@ -49,10 +50,14 @@ void NaClGdbDebugStubTest::StartTestScript(base::ProcessHandle* test_process,
 void NaClGdbDebugStubTest::RunDebugStubTest(const std::string& nacl_module,
                                             const std::string& test_name) {
   base::ProcessHandle test_script;
+  scoped_ptr<base::Environment> env(base::Environment::Create());
   NaClBrowser::GetInstance()->SetGdbDebugStubPortListener(
       base::Bind(&NaClGdbDebugStubTest::StartTestScript,
                  base::Unretained(this), &test_script, test_name));
+  // Turn on debug stub logging.
+  env->SetVar("NACLVERBOSITY", "1");
   RunTestViaHTTP(nacl_module);
+  env->UnSetVar("NACLVERBOSITY");
   NaClBrowser::GetInstance()->ClearGdbDebugStubPortListener();
   int exit_code;
   base::WaitForExitCode(test_script, &exit_code);
