cf6673c184e7819abafc870862065450c7a70c3d
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 20:16:05 +0000

    Update some #includes in webkit/media/crypto for headers that were moved from WebKit/chromium/public/platform to Platform/chromium/public
    Review URL: https://chromiumcodereview.appspot.com/11931019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177203 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/crypto/key_systems.cc
+++ webkit/media/crypto/key_systems.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "net/base/mime_util.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebCString.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebString.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebCString.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
 
 #include "widevine_cdm_version.h"  // In SHARED_INTERMEDIATE_DIR.
 
