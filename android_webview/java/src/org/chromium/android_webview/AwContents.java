6310dbdf0e7c66d5a5e1b2402a7cc781ad2f7a5a
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 01:29:17 +0000

    Avoid call nativeDrawSW after AwContents.destroy
    The view system pay still call onDraw after destroy is called on
    WebView.
    
    BUG=
    Android only change. Ran through android bots.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11946004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177043 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -384,6 +384,7 @@ public class AwContents {
     }
 
     public boolean onPrepareDrawGL(Canvas canvas) {
+        if (mNativeAwContents == 0) return false;
         nativeSetScrollForHWFrame(mNativeAwContents,
                 mContainerView.getScrollX(), mContainerView.getScrollY());
 
@@ -392,6 +393,7 @@ public class AwContents {
     }
 
     public void onDraw(Canvas canvas) {
+        if (mNativeAwContents == 0) return;
         if (!nativeDrawSW(mNativeAwContents, canvas)) {
             Log.w(TAG, "Native DrawSW failed; clearing to background color.");
             int c = mContentViewCore.getBackgroundColor();
