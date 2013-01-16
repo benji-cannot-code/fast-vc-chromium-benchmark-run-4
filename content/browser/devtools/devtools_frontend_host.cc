5e3c1c8cc9da72c442c48fbba4b892d74677e287
vsevik@chromium.org <vsevik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 11:12:11 +0000

    Support file system access in DevTools with isolated file system.
    BUG=167511
    TBR=jam@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11570081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177130 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/devtools_frontend_host.cc
+++ content/browser/devtools/devtools_frontend_host.cc
@@ -71,6 +71,10 @@ bool DevToolsFrontendHost::OnMessageReceived(
     IPC_MESSAGE_HANDLER(DevToolsHostMsg_OpenInNewTab, OnOpenInNewTab)
     IPC_MESSAGE_HANDLER(DevToolsHostMsg_Save, OnSave)
     IPC_MESSAGE_HANDLER(DevToolsHostMsg_Append, OnAppend)
+    IPC_MESSAGE_HANDLER(DevToolsHostMsg_RequestFileSystems,
+                        OnRequestFileSystems)
+    IPC_MESSAGE_HANDLER(DevToolsHostMsg_AddFileSystem, OnAddFileSystem)
+    IPC_MESSAGE_HANDLER(DevToolsHostMsg_RemoveFileSystem, OnRemoveFileSystem)
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   return handled;
@@ -109,6 +113,19 @@ void DevToolsFrontendHost::OnAppend(
     const std::string& url,
     const std::string& content) {
   delegate_->AppendToFile(url, content);
+}
+
+void DevToolsFrontendHost::OnRequestFileSystems() {
+  delegate_->RequestFileSystems();
+}
+
+void DevToolsFrontendHost::OnAddFileSystem() {
+  delegate_->AddFileSystem();
+}
+
+void DevToolsFrontendHost::OnRemoveFileSystem(
+    const std::string& file_system_path) {
+  delegate_->RemoveFileSystem(file_system_path);
 }
 
 void DevToolsFrontendHost::OnRequestSetDockSide(const std::string& side) {
