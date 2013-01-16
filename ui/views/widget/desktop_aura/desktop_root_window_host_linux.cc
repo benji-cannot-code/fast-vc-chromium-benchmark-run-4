d3f4a01be740cbd3b8c8c33044c258330dcb0243
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-16 21:34:55 +0000

    Revert 177209 - Enable the FocusController on DesktopAura too.
    http://crbug.com/162100
    R=sky@chromium.org
    Review URL: https://codereview.chromium.org/11885030
    
    TBR=ben@chromium.org
    Review URL: https://codereview.chromium.org/11969020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177226 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_root_window_host_linux.cc
+++ ui/views/widget/desktop_aura/desktop_root_window_host_linux.cc
@@ -24,13 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_util.h"
 #include "ui/native_theme/native_theme.h"
 #include "ui/views/corewm/compound_event_filter.h"
-#include "ui/views/corewm/corewm_switches.h"
-#include "ui/views/corewm/focus_controller.h"
 #include "ui/views/ime/input_method.h"
 #include "ui/views/widget/desktop_aura/desktop_activation_client.h"
 #include "ui/views/widget/desktop_aura/desktop_cursor_client.h"
 #include "ui/views/widget/desktop_aura/desktop_dispatcher_client.h"
-#include "ui/views/widget/desktop_aura/desktop_focus_rules.h"
 #include "ui/views/widget/desktop_aura/desktop_layout_manager.h"
 #include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
 #include "ui/views/widget/desktop_aura/desktop_screen_position_client.h"
@@ -95,10 +92,6 @@ DesktopRootWindowHostLinux::DesktopRootWindowHostLinux(
 
 DesktopRootWindowHostLinux::~DesktopRootWindowHostLinux() {
   root_window_->ClearProperty(kHostForRootWindow);
-  if (corewm::UseFocusController()) {
-    aura::client::SetFocusClient(root_window_, NULL);
-    aura::client::SetActivationClient(root_window_, NULL);
-  }
 }
 
 // static
@@ -222,17 +215,10 @@ aura::RootWindow* DesktopRootWindowHostLinux::InitRootWindow(
   // messages to us.
   X11DesktopHandler::get();
 
-  if (corewm::UseFocusController()) {
-    corewm::FocusController* focus_controller =
-        new corewm::FocusController(new DesktopFocusRules);
-    focus_client_.reset(focus_controller);
-    aura::client::SetFocusClient(root_window_, focus_controller);
-    aura::client::SetActivationClient(root_window_, focus_controller);
-  } else {
-    focus_client_.reset(new aura::FocusManager);
-    aura::client::SetFocusClient(root_window_, focus_client_.get());
-    activation_client_.reset(new DesktopActivationClient(root_window_));
-  }
+  focus_client_.reset(new aura::FocusManager);
+  aura::client::SetFocusClient(root_window_, focus_client_.get());
+
+  activation_client_.reset(new DesktopActivationClient(root_window_));
 
   dispatcher_client_.reset(new DesktopDispatcherClient);
   aura::client::SetDispatcherClient(root_window_,
