5d00b9699162acedcea162a294c67fc6fdba6adf
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-17 21:50:50 +0000

    Fix a logic error in AuthenticationChallengeMac         https://bugs.webkit.org/show_bug.cgi?id=107164
            Reviewed by Brady Eidson.
    
            No new tests, as this is not currently observable.
    
            * platform/network/cf/AuthenticationCF.cpp:
            (WebCore::createCF): Added a FIXME about making this more like Mac counterpart.
    
            * platform/network/cf/AuthenticationChallenge.h:
            (AuthenticationChallenge): Corrected an slightly misleading explanation.
    
            * platform/network/mac/AuthenticationMac.mm:
            (WebCore::AuthenticationChallenge::setAuthenticationClient): Don't create a dummy
            m_nsChallenge object in place of a nil one.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140037 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/mac/AuthenticationMac.mm
+++ third_party/WebKit/Source/WebCore/platform/network/mac/AuthenticationMac.mm
@@ -208,7 +208,8 @@ void AuthenticationChallenge::setAuthenticationClient(AuthenticationClient* clie
 {
     if (client) {
         m_sender.adoptNS([[WebCoreAuthenticationClientAsChallengeSender alloc] initWithAuthenticationClient:client]);
-        m_nsChallenge.adoptNS([[NSURLAuthenticationChallenge alloc] initWithAuthenticationChallenge:m_nsChallenge.get() sender:m_sender.get()]);
+        if (m_nsChallenge)
+            m_nsChallenge.adoptNS([[NSURLAuthenticationChallenge alloc] initWithAuthenticationChallenge:m_nsChallenge.get() sender:m_sender.get()]);
     } else {
         if ([m_sender.get() isMemberOfClass:[WebCoreAuthenticationClientAsChallengeSender class]])
             [(WebCoreAuthenticationClientAsChallengeSender *)m_sender.get() detachClient];
