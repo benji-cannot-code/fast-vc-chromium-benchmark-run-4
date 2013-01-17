8288c797203e1eb8092861f44e1a0ed885a2086d
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 21:56:37 +0000

    Activate the last used profile.
    BUG=150856
    
    
    Review URL: https://chromiumcodereview.appspot.com/11660013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177503 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator.h
+++ chrome/browser/ui/startup/startup_browser_creator.h
@@ -111,6 +111,7 @@ class StartupBrowserCreator {
   FRIEND_TEST_ALL_PREFIXES(StartupBrowserCreatorTest,
                            ReadingWasRestartedAfterRestart);
   FRIEND_TEST_ALL_PREFIXES(StartupBrowserCreatorTest, UpdateWithTwoProfiles);
+  FRIEND_TEST_ALL_PREFIXES(StartupBrowserCreatorTest, LastUsedProfileActivated);
 
   // Returns the list of URLs to open from the command line. The returned
   // vector is empty if the user didn't specify any URLs on the command line.
@@ -133,6 +134,10 @@ class StartupBrowserCreator {
       const FilePath& cur_dir,
       Profile* profile,
       Profile::CreateStatus status);
+
+  // Returns true once a profile was activated. Used by the
+  // StartupBrowserCreatorTest.LastUsedProfileActivated test.
+  static bool ActivatedProfile();
 
   // Additional tabs to open during first run.
   std::vector<GURL> first_run_tabs_;
