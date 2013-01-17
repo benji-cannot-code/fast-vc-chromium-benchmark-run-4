a9754ab9531025c0c301d2e982f90d21fd0940e9
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 17:59:19 +0000

    fix build error
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_data_source_unittest.cc
+++ chrome/browser/ui/webui/chrome_web_ui_data_source_unittest.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted_memory.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/ui/webui/chrome_web_ui_data_source.h"
-#include "grit/common_resources.h"
 #include "grit/generated_resources.h"
+#include "grit/webui_resources.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 class MockChromeWebUIDataSource : public ChromeWebUIDataSource {
