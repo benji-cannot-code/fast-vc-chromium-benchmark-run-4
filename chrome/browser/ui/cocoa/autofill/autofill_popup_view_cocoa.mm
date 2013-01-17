bbacfae560b124873b79ffa363ec3edb3b46e491
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 12:01:37 +0000

    [Autofill] Use system colors for the native Autofill popup on Mac.
    BUG=51644
    
    
    Review URL: https://chromiumcodereview.appspot.com/11878037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177390 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/autofill/autofill_popup_view_cocoa.mm
+++ chrome/browser/ui/cocoa/autofill/autofill_popup_view_cocoa.mm
@@ -20,15 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 NSColor* BorderColor() {
-  // TODO(isherman): This color does not match the other platforms, but it
-  // matches what the existing UI on Mac has as the color.  The other platforms
-  // have a strange color: the RGB values are almost, but not quite, identical
-  // to each other.
-  // TODO(isherman): Maybe use a system color for this?
-  return [NSColor colorWithCalibratedRed:127 / 255.0
-                                   green:157 / 255.0
-                                    blue:185 / 255.0
-                                   alpha:1.0];
+  return [NSColor colorForControlTint:[NSColor currentControlTint]];
 }
 
 NSColor* SeparatorColor() {
@@ -36,7 +28,7 @@ NSColor* SeparatorColor() {
 }
 
 NSColor* HighlightColor() {
-  return [NSColor colorWithCalibratedWhite:205 / 255.0 alpha:1];
+  return [NSColor selectedControlColor];
 }
 
 }  // anonymous namespace
@@ -120,11 +112,6 @@ NSColor* HighlightColor() {
   [[NSColor whiteColor] set];
   [border fill];
 
-  // TODO(isherman): This color does not match the other platforms, but it
-  // matches what the existing UI on Mac has as the color.  The other platforms
-  // have a strange color: the RGB values are almost, but not quite, identical
-  // to each other.
-  // TODO(isherman): Maybe use a system color for this?
   [BorderColor() set];
   [border stroke];
 
@@ -215,9 +202,6 @@ NSColor* HighlightColor() {
                      canDelete:(BOOL)canDelete {
   // If this row is selected, highlight it.
   if (isSelected) {
-    // TODO(isherman): The highlight color should match the system highlight
-    // color.  Maybe use controlHighlightColor or selectedTextBackgroundColor
-    // for this?
     [HighlightColor() set];
     [NSBezierPath fillRect:bounds];
   }
