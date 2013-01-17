531e46af57bc155e8706e1ec9b7d9246121b018b
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 20:46:57 +0000

    Update tools/git/update-copyrights.sh.
    Print out that updating copyright year is no longer necessary now.
    See https://groups.google.com/a/chromium.org/d/msg/chromium-dev/8p4JKV76kig/OiFYFjuZ6nAJ
    
    BUG=none
    TEST=none
    
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/11886101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177472 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/git/update-copyrights.sh
+++ tools/git/update-copyrights.sh
@@ -4,14 +4,5 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-tmp=$(mktemp -t chromium-update-copyrights.XXXXX)
-trap "rm -f $tmp" EXIT
-git diff --name-only $(git cl upstream)... | while read file; do
-    cp "$file" "$tmp"
-    # Rather than editing the temporary file, edit the original file in-place
-    # so that we preserve file modes.
-    sed -i -e '1,4s/Copyright .c. .* The Chromium Authors/Copyright (c) 2012 The Chromium Authors/' "$file"
-    if ! diff -q "$file" "$tmp" > /dev/null; then
-        echo "updated $file"
-    fi
-done
+echo 'Updating copyrights is no longer necessary.'
+echo 'See https://groups.google.com/a/chromium.org/d/msg/chromium-dev/8p4JKV76kig/OiFYFjuZ6nAJ'
