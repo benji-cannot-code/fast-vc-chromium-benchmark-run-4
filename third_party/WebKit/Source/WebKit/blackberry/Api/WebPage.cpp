a5b5c9eab5289492316cccf7fde2c2f8feaf38e5
yoli@rim.com <yoli@rim.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-17 22:38:23 +0000

    [BlackBerry] InRegionScroller should notify client before it deletes all the scrollables https://bugs.webkit.org/show_bug.cgi?id=107162
    Reviewed by Rob Buis.
    Internally reviewed by Gen Mak and Mike Fenton.
    
    Move the notification code from notifyInRegionScrollStopped() to reset() as there are
    other places where reset() gets called.
    
    * Api/InRegionScroller.cpp:
    (BlackBerry::WebKit::InRegionScrollerPrivate::reset):
    * Api/WebPage.cpp:
    (BlackBerry::WebKit::WebPagePrivate::notifyInRegionScrollStopped):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/Api/WebPage.cpp
@@ -1429,12 +1429,8 @@ void WebPagePrivate::deferredTasksTimerFired(WebCore::Timer<WebPagePrivate>*)
 
 void WebPagePrivate::notifyInRegionScrollStopped()
 {
-    if (m_inRegionScroller->d->isActive()) {
-        // Notify the client side to clear InRegion scrollable areas before we destroy them here.
-        std::vector<Platform::ScrollViewBase*> emptyInRegionScrollableAreas;
-        m_client->notifyInRegionScrollableAreasChanged(emptyInRegionScrollableAreas);
+    if (m_inRegionScroller->d->isActive())
         m_inRegionScroller->d->reset();
-    }
 }
 
 void WebPage::notifyInRegionScrollStopped()
