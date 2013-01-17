f4276373368a1fa989c73640f80dddeeb2477dde
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 06:40:50 +0000

    Revert 177288 > cc: Add point-based UV coordinate on TextureLayer > > Previously, the uv coordinates was using gfx::RectF as a struct. > This was limitating because it could not handle negative width > and heights. Soring them as 2 gfx::PointF for the Top-Left and > Bottom-Right points aleviate that restriction. > The current implementation of the phone UI rely on this feature. > > BUG=168927 > > Review URL: https://chromiumcodereview.appspot.com/11783094
    This is breaking the android internal chrome build, we need green for
    a release cut later tonight.  Let's reland tomorrow with the
    corresponding downstream fix.
    
    TBR=jscholler@chromium.org
    Review URL: https://codereview.chromium.org/11959026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177360 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/texture_draw_quad.h
+++ cc/texture_draw_quad.h
@@ -22,8 +22,7 @@ class CC_EXPORT TextureDrawQuad : public DrawQuad {
               gfx::Rect opaque_rect,
               unsigned resource_id,
               bool premultiplied_alpha,
-              gfx::PointF uv_top_left,
-              gfx::PointF uv_bottom_right,
+              const gfx::RectF& uv_rect,
               const float vertex_opacity[4],
               bool flipped);
 
@@ -34,15 +33,13 @@ class CC_EXPORT TextureDrawQuad : public DrawQuad {
               bool needs_blending,
               unsigned resource_id,
               bool premultiplied_alpha,
-              gfx::PointF uv_top_left,
-              gfx::PointF uv_bottom_right,
+              const gfx::RectF& uv_rect,
               const float vertex_opacity[4],
               bool flipped);
 
   unsigned resource_id;
   bool premultiplied_alpha;
-  gfx::PointF uv_top_left;
-  gfx::PointF uv_bottom_right;
+  gfx::RectF uv_rect;
   float vertex_opacity[4];
   bool flipped;
 
--- cc/texture_layer.cc
+++ cc/texture_layer.cc
@@ -38,8 +38,7 @@ TextureLayer::TextureLayer(TextureLayerClient* client, bool usesMailbox)
     , m_client(client)
     , m_usesMailbox(usesMailbox)
     , m_flipped(true)
-    , m_uvTopLeft(0.f, 0.f)
-    , m_uvBottomRight(1.f, 1.f)
+    , m_uvRect(0, 0, 1, 1)
     , m_premultipliedAlpha(true)
     , m_rateLimitContext(false)
     , m_contextLost(false)
@@ -75,10 +74,9 @@ void TextureLayer::setFlipped(bool flipped)
     setNeedsCommit();
 }
 
-void TextureLayer::setUV(gfx::PointF topLeft, gfx::PointF bottomRight)
+void TextureLayer::setUVRect(const gfx::RectF& rect)
 {
-    m_uvTopLeft = topLeft;
-    m_uvBottomRight = bottomRight;
+    m_uvRect = rect;
     setNeedsCommit();
 }
 
@@ -181,8 +179,7 @@ void TextureLayer::pushPropertiesTo(LayerImpl* layer)
 
     TextureLayerImpl* textureLayer = static_cast<TextureLayerImpl*>(layer);
     textureLayer->setFlipped(m_flipped);
-    textureLayer->setUVTopLeft(m_uvTopLeft);
-    textureLayer->setUVBottomRight(m_uvBottomRight);
+    textureLayer->setUVRect(m_uvRect);
     textureLayer->setVertexOpacity(m_vertexOpacity);
     textureLayer->setPremultipliedAlpha(m_premultipliedAlpha);
     if (m_usesMailbox) {
