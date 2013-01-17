7b4e8d9cdc6a71dca5f9e9d272345b031e0c1d48
MHX348@motorola.com <MHX348@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 00:40:12 +0000

    Move Details URL out of Extension class
    BUG=159265
    
    
    Review URL: https://chromiumcodereview.appspot.com/11826066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177289 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/manifest_url_handler.h
+++ chrome/common/extensions/manifest_url_handler.h
@@ -32,6 +32,9 @@ struct ManifestURL : public Extension::ManifestData {
 
   // Returns the Options Page for this extension.
   static const GURL& GetOptionsPage(const Extension* extension);
+
+  // Returns the webstore page URL for this extension.
+  static const GURL GetDetailsURL(const Extension* extension);
 };
 
 // A structure to hold the chrome URL overrides that may be specified
