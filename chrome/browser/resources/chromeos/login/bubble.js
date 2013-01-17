8ce1b84dafcb8201cb5fa8d5f8a99143fa8ea70b
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-17 18:07:27 +0000

    [cros, oobe a11y] Make sure that ChromeVox highlight moves out of a11y popup when it is closed.
    BUG=165223
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/11959038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177434 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/bubble.js
+++ chrome/browser/resources/chromeos/login/bubble.js
@@ -33,6 +33,10 @@ cr.define('cr.ui', function() {
     // Anchor element for this bubble.
     anchor_: undefined,
 
+    // If defined, sets focus to this element once bubble is closed. Focus is
+    // set to this element only if there's no any other focused element.
+    elementToFocusOnHide_: undefined,
+
     // Whether to hide bubble when key is pressed.
     hideOnKeyPress_: true,
 
@@ -47,6 +51,14 @@ cr.define('cr.ui', function() {
       window.addEventListener('blur', this.handleWindowBlur_.bind(this));
       this.addEventListener('webkitTransitionEnd',
                             this.handleTransitionEnd_.bind(this));
+    },
+
+    /**
+     * Element that should be focused on hide.
+     * @type {HTMLElement}
+     */
+    set elementToFocusOnHide(value) {
+      this.elementToFocusOnHide_ = value;
     },
 
     /**
@@ -245,8 +257,15 @@ cr.define('cr.ui', function() {
      * @private
      */
     handleTransitionEnd_: function(e) {
-      if (this.classList.contains('faded'))
+      if (this.classList.contains('faded')) {
         this.hidden = true;
+        if (this.elementToFocusOnHide_ &&
+            document.activeElement == document.body) {
+          // Restore focus to default element only if there's no other
+          // element that is focused.
+          this.elementToFocusOnHide_.focus();
+        }
+      }
     },
 
     /**
@@ -272,8 +291,11 @@ cr.define('cr.ui', function() {
         return;
       }
 
-      if (e.keyCode == 27 && !this.hidden)
+      if (e.keyCode == 27 && !this.hidden) {
+        if (this.elementToFocusOnHide_)
+          this.elementToFocusOnHide_.focus();
         this.hide();
+      }
     },
 
     /**
