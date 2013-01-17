0b6e9f8bdc4558234198019c975ab5956defdbd3
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-17 01:21:20 +0000

    Cursor stops blinking after clicking on scrollbar https://bugs.webkit.org/show_bug.cgi?id=106470
    Reviewed by Ojan Vafai.
    
    Restore the caret blinking when doing a mouseup on a
    scrollbar so scrolling a textarea doesn't cause the
    caret to freeze.
    
    No new tests, there doesn't seem to be any way to test
    cursor blinking.
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::handleMouseReleaseEvent):
    (WebCore::EventHandler::handleMouseDoubleClickEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139939 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -889,7 +889,6 @@ bool EventHandler::handleMouseReleaseEvent(const MouseEventWithHitTestResults& e
 
     // Used to prevent mouseMoveEvent from initiating a drag before
     // the mouse is pressed again.
-    m_frame->selection()->setCaretBlinkingSuspended(false);
     m_mousePressed = false;
     m_capturesDragging = false;
 #if ENABLE(DRAG_SUPPORT)
@@ -1554,6 +1553,8 @@ bool EventHandler::handleMouseDoubleClickEvent(const PlatformMouseEvent& mouseEv
 {
     RefPtr<FrameView> protector(m_frame->view());
 
+    m_frame->selection()->setCaretBlinkingSuspended(false);
+
     UserGestureIndicator gestureIndicator(DefinitelyProcessingUserGesture);
 
     // We get this instead of a second mouse-up 
@@ -1761,6 +1762,8 @@ void EventHandler::invalidateClick()
 bool EventHandler::handleMouseReleaseEvent(const PlatformMouseEvent& mouseEvent)
 {
     RefPtr<FrameView> protector(m_frame->view());
+
+    m_frame->selection()->setCaretBlinkingSuspended(false);
 
 #if ENABLE(TOUCH_EVENTS)
     bool defaultPrevented = dispatchSyntheticTouchEventIfEnabled(mouseEvent);
