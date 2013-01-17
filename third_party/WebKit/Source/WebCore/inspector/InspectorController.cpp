aa55d27cce3dc1ec181471506573cc3095192fc3
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-17 19:42:17 +0000

    Web Inspector: InstrumentingAgents should be registered in InspectorInstrumentation only when there is a front-end https://bugs.webkit.org/show_bug.cgi?id=107127
    Reviewed by Vsevolod Vlasov.
    
    InspectorInstrumentation::registerInstrumentingAgents is called when front-end
    is connected to corresponding InspectorController. When the front-end disconnects
    InspectorInstrumentation::unregisterInstrumentingAgents is called.
    
    * inspector/InspectorController.cpp:
    (WebCore::InspectorController::InspectorController):
    (WebCore::InspectorController::inspectedPageDestroyed):
    (WebCore::InspectorController::connectFrontend):
    (WebCore::InspectorController::disconnectFrontend):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorController.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorController.cpp
@@ -176,8 +176,6 @@ InspectorController::InspectorController(Page* page, InspectorClient* inspectorC
 #if ENABLE(JAVASCRIPT_DEBUGGER)
     runtimeAgent->setScriptDebugServer(&m_debuggerAgent->scriptDebugServer());
 #endif
-
-    InspectorInstrumentation::registerInstrumentingAgents(m_instrumentingAgents.get());
 }
 
 InspectorController::~InspectorController()
@@ -194,7 +192,6 @@ PassOwnPtr<InspectorController> InspectorController::create(Page* page, Inspecto
 void InspectorController::inspectedPageDestroyed()
 {
     disconnectFrontend();
-    InspectorInstrumentation::unregisterInstrumentingAgents(m_instrumentingAgents.get());
     m_injectedScriptManager->disconnect();
     m_inspectorClient->inspectorDestroyed();
     m_inspectorClient = 0;
@@ -232,6 +229,7 @@ void InspectorController::connectFrontend(InspectorFrontendChannel* frontendChan
 
     m_agents.setFrontend(m_inspectorFrontend.get());
 
+    InspectorInstrumentation::registerInstrumentingAgents(m_instrumentingAgents.get());
     InspectorInstrumentation::frontendCreated();
 
     ASSERT(m_inspectorClient);
@@ -258,6 +256,7 @@ void InspectorController::disconnectFrontend()
     // relese overlay page resources
     m_overlay->freePage();
     InspectorInstrumentation::frontendDeleted();
+    InspectorInstrumentation::unregisterInstrumentingAgents(m_instrumentingAgents.get());
 }
 
 void InspectorController::show()
