b22c2d88cc294e6300133fc69c42ebd08294cfc4
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-17 01:57:52 +0000

    Fix crash when destroying a plug-in with pending URL requests https://bugs.webkit.org/show_bug.cgi?id=107075 <rdar://problem/12513871>
    Reviewed by Sam Weinig.
    
    In PluginView::destroyPluginAndReset, make sure to cancel any outstanding URL requests since the PluginView
    object will stay around and would otherwise start loading URLs from stopped plug-ins.
    
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::Stream::start):
    Assert that the plug-in is alive.
    
    (WebKit::PluginView::destroyPluginAndReset):
    Empty out the pending URL requests vector and stop the timer.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@139943 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginView.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginView.cpp
@@ -145,6 +145,7 @@ PluginView::Stream::~Stream()
     
 void PluginView::Stream::start()
 {
+    ASSERT(m_pluginView->m_plugin);
     ASSERT(!m_loader);
 
     Frame* frame = m_pluginView->m_pluginElement->document()->frame();
@@ -309,6 +310,10 @@ void PluginView::destroyPluginAndReset()
         m_isBeingDestroyed = true;
         m_plugin->destroyPlugin();
         m_isBeingDestroyed = false;
+
+        m_pendingURLRequests.clear();
+        m_pendingURLRequestsTimer.stop();
+
 #if PLATFORM(MAC)
         if (m_webPage)
             pluginFocusOrWindowFocusChanged(false);
