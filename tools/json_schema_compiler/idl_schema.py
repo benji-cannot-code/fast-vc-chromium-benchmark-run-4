c60af25e52bccbece2b38de378f2dfa8bc4b64ee
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 14:11:30 +0000

    IDL schema compiler: Fixed pseudo-random order of methods, properties, and other elements due to them being parsed into an unordered dict (uses an OrderedDict instead).
    The API documentation now displays items in their original IDL order.
    
    BUG=168965
    
    
    Review URL: https://chromiumcodereview.appspot.com/11826020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177664 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/json_schema_compiler/idl_schema.py
+++ tools/json_schema_compiler/idl_schema.py
@@ -10,6 +10,7 @@ import os.path
 import re
 import sys
 
+from json_parse import OrderedDict
 import schema_util
 
 # This file is a peer to json_schema.py. Each of these files understands a
@@ -71,7 +72,7 @@ def ProcessComment(comment):
   parent_comment = (parent_comment.strip().replace('\n\n', '<br/><br/>')
                                           .replace('\n', ''))
 
-  params = {}
+  params = OrderedDict()
   for (cur_param, next_param) in itertools.izip_longest(parameter_starts,
                                                         parameter_starts[1:]):
     param_name = cur_param.group(1)
@@ -125,7 +126,7 @@ class Dictionary(object):
     self.node = dictionary_node
 
   def process(self, callbacks):
-    properties = {}
+    properties = OrderedDict()
     for node in self.node.children:
       if node.cls == 'Member':
         k, v = Member(node).process(callbacks)
@@ -148,13 +149,13 @@ class Member(object):
     self.node = member_node
 
   def process(self, callbacks):
-    properties = {}
+    properties = OrderedDict()
     name = self.node.GetName()
     for property_name in ('OPTIONAL', 'nodoc', 'nocompile'):
       if self.node.GetProperty(property_name):
         properties[property_name.lower()] = True
     is_function = False
-    parameter_comments = {}
+    parameter_comments = OrderedDict()
     for node in self.node.children:
       if node.cls == 'Comment':
         (parent_comment, parameter_comments) = ProcessComment(node.GetName())
@@ -184,7 +185,7 @@ class Typeref(object):
   function parameter, converts into a Python dictionary that the JSON schema
   compiler expects to see.
   '''
-  def __init__(self, typeref, parent, additional_properties={}):
+  def __init__(self, typeref, parent, additional_properties=OrderedDict()):
     self.typeref = typeref
     self.parent = parent
     self.additional_properties = additional_properties
@@ -201,7 +202,7 @@ class Typeref(object):
     for sibling in self.parent.GetChildren():
       if sibling.cls == 'Array' and sibling.GetName() == self.parent.GetName():
         properties['type'] = 'array'
-        properties['items'] = {}
+        properties['items'] = OrderedDict()
         properties = properties['items']
         break
 
@@ -218,7 +219,7 @@ class Typeref(object):
     elif self.typeref == 'object':
       properties['type'] = 'object'
       if 'additionalProperties' not in properties:
-        properties['additionalProperties'] = {}
+        properties['additionalProperties'] = OrderedDict()
       properties['additionalProperties']['type'] = 'any'
       instance_of = self.parent.GetProperty('instanceOf')
       if instance_of:
@@ -284,7 +285,7 @@ class Namespace(object):
     self.events = []
     self.functions = []
     self.types = []
-    self.callbacks = {}
+    self.callbacks = OrderedDict()
     self.permissions = permissions or []
 
   def process(self):
