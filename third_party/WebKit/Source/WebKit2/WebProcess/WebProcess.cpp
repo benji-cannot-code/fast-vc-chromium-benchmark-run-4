13510d36972952ef555436176e95bbf154c1c4d1
jonlee@apple.com <jonlee@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-18 02:09:21 +0000

    Do not track user interaction for plugins that are not snapshotted https://bugs.webkit.org/show_bug.cgi?id=107211 <rdar://problem/12967277>
    Reviewed by Dean Jackson.
    
    When the user interacts with a snapshotted plug-in, we extend that plug-in origin's
    expiration date. However, plug-ins may not be snapshotted for other reasons than the
    user's initial opt-in. In this case, that plug-in's origin might be not be in the cached
    table. This is an edge case that was not initially considered.
    
    * WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::plugInDidReceiveUserInteraction): Convert the assertion to an if
    check, and return early if the entry is not found in the cache table.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebProcess.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebProcess.cpp
@@ -817,7 +817,8 @@ void WebProcess::plugInDidReceiveUserInteraction(unsigned plugInOriginHash)
         return;
 
     HashMap<unsigned, double>::iterator it = m_plugInAutoStartOrigins.find(plugInOriginHash);
-    ASSERT(it != m_plugInAutoStartOrigins.end());
+    if (it == m_plugInAutoStartOrigins.end())
+        return;
     if (it->value - currentTime() > plugInAutoStartExpirationTimeUpdateThreshold)
         return;
 
