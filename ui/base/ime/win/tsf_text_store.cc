b5c752357ef19c641cdedf5ee89e2d9868d25583
Yukawa@chromium.org <Yukawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 14:22:11 +0000

    Refactor TSFInputScope as a preparation to support InputScope in desktop (non-Metro) Chrome.
    We need SetInputScope API to support InputScope in desktop (non-Metro) Chrome.
    This patch set adds a wrapper to this API into TSFInputScope.
    This patch set also refactors TSFInputScope.
    
    BUG=150077
    
    Review URL: https://chromiumcodereview.appspot.com/12026009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177671 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/win/tsf_text_store.cc
+++ ui/base/ime/win/tsf_text_store.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define INITGUID  // required for GUID_PROP_INPUTSCOPE
 #include "ui/base/ime/win/tsf_text_store.h"
 
+#include <InputScope.h>
 #include <OleCtl.h>
 
 #include "base/win/scoped_variant.h"
@@ -609,7 +610,7 @@ STDMETHODIMP TSFTextStore::RetrieveRequestedAttrs(
   attribute_buffer[0].dwOverlapId = 0;
   attribute_buffer[0].idAttr = GUID_PROP_INPUTSCOPE;
   attribute_buffer[0].varValue.vt = VT_UNKNOWN;
-  attribute_buffer[0].varValue.punkVal = new TSFInputScope(
+  attribute_buffer[0].varValue.punkVal = tsf_inputscope::CreateInputScope(
       text_input_client_->GetTextInputType());
   attribute_buffer[0].varValue.punkVal->AddRef();
   *attribute_buffer_copied = 1;
