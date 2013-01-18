c91fa2fda5aba57802c497ea5451ff4e0cb9871a
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-18 12:08:12 +0000

    Web Inspector: do not use localeCompare unless necessary https://bugs.webkit.org/show_bug.cgi?id=107242
    Reviewed by Vsevolod Vlasov.
    
    Saving on performance here.
    
    * inspector/front-end/CSSSelectorProfileView.js:
    (WebInspector.CSSSelectorProfileView.prototype._sortProfile.selectorComparator):
    (WebInspector.CSSSelectorProfileView.prototype._sortProfile.sourceComparator):
    * inspector/front-end/CookiesTable.js:
    (WebInspector.CookiesTable.prototype._sortCookies.compareTo):
    (WebInspector.CookiesTable.prototype._sortCookies):
    * inspector/front-end/DirectoryContentView.js:
    (.nameCompare):
    (.typeCompare):
    * inspector/front-end/FileSystemModel.js:
    (WebInspector.FileSystemModel.Entry.compare):
    * inspector/front-end/FileSystemView.js:
    (WebInspector.FileSystemView.EntryTreeElement.prototype._directoryContentReceived):
    * inspector/front-end/FilteredItemSelectionDialog.js:
    (WebInspector.OpenResourceDialog.compareFunction):
    (WebInspector.OpenResourceDialog):
    * inspector/front-end/NetworkRequest.js:
    * inspector/front-end/ScriptsSearchScope.js:
    (WebInspector.ScriptsSearchScope.prototype._sortedUISourceCodes.comparator):
    (WebInspector.ScriptsSearchScope.prototype._sortedUISourceCodes):
    * inspector/front-end/StylesSidebarPane.js:
    (WebInspector.ComputedStylePropertiesSection.prototype.onpopulate):
    * inspector/front-end/utilities.js:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140122 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/CSSSelectorProfileView.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/CSSSelectorProfileView.js
@@ -196,7 +196,7 @@ WebInspector.CSSSelectorProfileView.prototype = {
 
         function selectorComparator(a, b)
         {
-            var result = b.rawData.selector.localeCompare(a.rawData.selector);
+            var result = b.rawData.selector.compareTo(a.rawData.selector);
             return sortAscending ? -result : result;
         }
 
@@ -204,7 +204,7 @@ WebInspector.CSSSelectorProfileView.prototype = {
         {
             var aRawData = a.rawData;
             var bRawData = b.rawData;
-            var result = bRawData.url.localeCompare(aRawData.url);
+            var result = bRawData.url.compareTo(aRawData.url);
             if (!result)
                 result = bRawData.lineNumber - aRawData.lineNumber;
             return sortAscending ? -result : result;
