59b7ef7e22204025f092522ef33f5a5666b05956
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 20:41:10 +0000

    Allow message center and related bubbles to render on Windows.
    SetPaintToLayer causes Windows to render only black squares since
    there is no compositor there.
    
    BUG=168605
    
    
    Review URL: https://chromiumcodereview.appspot.com/11836003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177742 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/bubble/tray_bubble_view.cc
+++ ui/views/bubble/tray_bubble_view.cc
@@ -276,13 +276,14 @@ TrayBubbleView::TrayBubbleView(gfx::NativeView parent_window,
   set_notify_enter_exit_on_child(true);
   set_close_on_deactivate(init_params.close_on_deactivate);
   set_margins(gfx::Insets());
-  SetPaintToLayer(true);
-  SetFillsBoundsOpaquely(true);
-
   bubble_border_ = new TrayBubbleBorder(this, anchor_view(), params_);
+  if (get_use_acceleration_when_possible()) {
+    SetPaintToLayer(true);
+    SetFillsBoundsOpaquely(true);
 
-  bubble_content_mask_.reset(
-      new TrayBubbleContentMask(bubble_border_->GetBorderCornerRadius() - 1));
+    bubble_content_mask_.reset(
+        new TrayBubbleContentMask(bubble_border_->GetBorderCornerRadius() - 1));
+  }
 }
 
 TrayBubbleView::~TrayBubbleView() {
@@ -296,7 +297,8 @@ void TrayBubbleView::InitializeAndShowBubble() {
   SetAlignment(views::BubbleBorder::ALIGN_EDGE_TO_ANCHOR_EDGE);
   bubble_border_->UpdateArrowOffset();
 
-  layer()->parent()->SetMaskLayer(bubble_content_mask_->layer());
+  if (get_use_acceleration_when_possible())
+    layer()->parent()->SetMaskLayer(bubble_content_mask_->layer());
 
   Show();
   UpdateBubble();
@@ -304,7 +306,8 @@ void TrayBubbleView::InitializeAndShowBubble() {
 
 void TrayBubbleView::UpdateBubble() {
   SizeToContents();
-  bubble_content_mask_->layer()->SetBounds(layer()->bounds());
+  if (get_use_acceleration_when_possible())
+    bubble_content_mask_->layer()->SetBounds(layer()->bounds());
   GetWidget()->GetRootView()->SchedulePaint();
 }
 
@@ -404,7 +407,7 @@ void TrayBubbleView::ChildPreferredSizeChanged(View* child) {
 void TrayBubbleView::ViewHierarchyChanged(bool is_add,
                                           views::View* parent,
                                           views::View* child) {
-  if (is_add && child == this) {
+  if (get_use_acceleration_when_possible() && is_add && child == this) {
     parent->SetPaintToLayer(true);
     parent->SetFillsBoundsOpaquely(true);
     parent->layer()->SetMasksToBounds(true);
