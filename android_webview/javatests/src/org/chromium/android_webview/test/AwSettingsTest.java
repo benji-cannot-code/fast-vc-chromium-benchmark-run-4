dfe0c51b422df9a3c6345962f6df7e33f5f286a3
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 22:22:16 +0000

    Call FlushIndex in BackendImpl::ModifyStorageSize
    On Android, the app can be killed at any time without notice
    so it is possible for the disk cache header to get corrupted.
    Adding a flush to the index file at end of
    BackendImpl::ModifyStorageSize to make this less likely to
    happen.
    
    See bug for details of why other solutions are not followed.
    
    BUG=154805
    linux_aura cq broken on compile, all other bots passed
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11440026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177773 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1012,7 +1012,6 @@ public class AwSettingsTest extends AndroidWebViewTestBase {
             super(awContents, contentViewClient, true);
             mIndex = index;
             mWebServer = webServer;
-            AwSettingsTest.this.clearCacheOnUiThread(mAwContents, true);
         }
 
         @Override
