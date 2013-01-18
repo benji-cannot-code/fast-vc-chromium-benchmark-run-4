d468c87eef2bee51fb03b5b181ceb66105d98939
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 17:22:20 +0000

    Test the RestoreOnStartup policies with less tabs to avoid timeouts.
    These tests sometimes timeout on XP, because they have to open and restore 5 tabs. Testing with 2 tabs is sufficient to verify that the feature is working.
    
    BUG=170779
    
    
    Review URL: https://chromiumcodereview.appspot.com/11943012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177697 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -135,14 +135,6 @@ const char kHostedAppCrxId[] = "kbmnembihfiondgfjekmnmcbddelicoi";
 const FilePath::CharType kGoodCrxManifestName[] =
     FILE_PATH_LITERAL("good_update_manifest.xml");
 
-const char* kURLs[] = {
-  "http://aaa.com/empty.html",
-  "http://bbb.com/empty.html",
-  "http://ccc.com/empty.html",
-  "http://ddd.com/empty.html",
-  "http://eee.com/empty.html",
-};
-
 // Filters requests to the hosts in |urls| and redirects them to the test data
 // dir through URLRequestMockHTTPJobs.
 void RedirectHostsToTestData(const char* const urls[], size_t size) {
@@ -1545,6 +1537,11 @@ IN_PROC_BROWSER_TEST_F(PolicyTest, DisableAudioOutput) {
 
 namespace {
 
+static const char* kRestoredURLs[] = {
+  "http://aaa.com/empty.html",
+  "http://bbb.com/empty.html",
+};
+
 bool IsNonSwitchArgument(const CommandLine::StringType& s) {
   return s.empty() || s[0] != '-';
 }
@@ -1586,7 +1583,7 @@ class RestoreOnStartupPolicyTest
                            command_line->argv().begin()));
 
     // Redirect the test URLs to the test data directory.
-    RedirectHostsToTestData(kURLs, arraysize(kURLs));
+    RedirectHostsToTestData(kRestoredURLs, arraysize(kRestoredURLs));
   }
 
   void HomepageIsNotNTP() {
@@ -1602,10 +1599,10 @@ class RestoreOnStartupPolicyTest
         base::Value::CreateBooleanValue(false));
     policies.Set(
         key::kHomepageLocation, POLICY_LEVEL_MANDATORY, POLICY_SCOPE_USER,
-        base::Value::CreateStringValue(kURLs[1]));
+        base::Value::CreateStringValue(kRestoredURLs[1]));
     provider_.UpdateChromePolicy(policies);
 
-    expected_urls_.push_back(GURL(kURLs[1]));
+    expected_urls_.push_back(GURL(kRestoredURLs[1]));
   }
 
   void HomepageIsNTP() {
@@ -1627,9 +1624,9 @@ class RestoreOnStartupPolicyTest
   void ListOfURLs() {
     // Verifies that policy can set the startup pages to a list of URLs.
     base::ListValue urls;
-    for (size_t i = 0; i < arraysize(kURLs); ++i) {
-      urls.Append(base::Value::CreateStringValue(kURLs[i]));
-      expected_urls_.push_back(GURL(kURLs[i]));
+    for (size_t i = 0; i < arraysize(kRestoredURLs); ++i) {
+      urls.Append(base::Value::CreateStringValue(kRestoredURLs[i]));
+      expected_urls_.push_back(GURL(kRestoredURLs[i]));
     }
     PolicyMap policies;
     policies.Set(
@@ -1659,8 +1656,8 @@ class RestoreOnStartupPolicyTest
         base::Value::CreateIntegerValue(SessionStartupPref::kPrefValueLast));
     provider_.UpdateChromePolicy(policies);
     // This should restore the tabs opened at PRE_RunTest below.
-    for (size_t i = 0; i < arraysize(kURLs); ++i)
-      expected_urls_.push_back(GURL(kURLs[i]));
+    for (size_t i = 0; i < arraysize(kRestoredURLs); ++i)
+      expected_urls_.push_back(GURL(kRestoredURLs[i]));
   }
 
   std::vector<GURL> expected_urls_;
@@ -1670,12 +1667,12 @@ IN_PROC_BROWSER_TEST_P(RestoreOnStartupPolicyTest, PRE_RunTest) {
   // Open some tabs to verify if they are restored after the browser restarts.
   // Most policy settings override this, except kPrefValueLast which enforces
   // a restore.
-  ui_test_utils::NavigateToURL(browser(), GURL(kURLs[0]));
-  for (size_t i = 1; i < arraysize(kURLs); ++i) {
+  ui_test_utils::NavigateToURL(browser(), GURL(kRestoredURLs[0]));
+  for (size_t i = 1; i < arraysize(kRestoredURLs); ++i) {
     content::WindowedNotificationObserver observer(
         content::NOTIFICATION_LOAD_STOP,
         content::NotificationService::AllSources());
-    chrome::AddSelectedTabWithURL(browser(), GURL(kURLs[i]),
+    chrome::AddSelectedTabWithURL(browser(), GURL(kRestoredURLs[i]),
                                   content::PAGE_TRANSITION_LINK);
     observer.Wait();
   }
@@ -1690,9 +1687,6 @@ IN_PROC_BROWSER_TEST_P(RestoreOnStartupPolicyTest, RunTest) {
   }
 }
 
-#if !defined(OS_WIN)
-// http://crbug.com/170779 Timing out regularly on XP waterfall.
-
 INSTANTIATE_TEST_CASE_P(
     RestoreOnStartupPolicyTestInstance,
     RestoreOnStartupPolicyTest,
@@ -1701,8 +1695,6 @@ INSTANTIATE_TEST_CASE_P(
                     &RestoreOnStartupPolicyTest::ListOfURLs,
                     &RestoreOnStartupPolicyTest::NTP,
                     &RestoreOnStartupPolicyTest::Last));
-
-#endif
 
 // Similar to PolicyTest but sets a couple of policies before the browser is
 // started.
