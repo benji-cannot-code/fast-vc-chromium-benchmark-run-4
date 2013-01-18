a7eafd7aa80a4e0e8426d95b44a5e9d13ad97ebb
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-18 14:34:46 +0000

    Unreviewed Windows buildfix after r140097.
    * html/HTMLPlugInElement.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLPlugInElement.h
+++ third_party/WebKit/Source/WebCore/html/HTMLPlugInElement.h
@@ -25,6 +25,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HTMLPlugInElement_h
 
 #include "HTMLFrameOwnerElement.h"
+#include "Image.h"
+#include "ImageLoaderClient.h"
+
 #include "ScriptInstance.h"
 
 #if ENABLE(NETSCAPE_PLUGIN_API)
@@ -33,7 +36,6 @@ struct NPObject;
 
 namespace WebCore {
 
-class Image;
 class RenderEmbeddedObject;
 class RenderWidget;
 class Widget;
