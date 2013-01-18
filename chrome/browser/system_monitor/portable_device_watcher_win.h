b7120501786bef6e36793f1dec70c7f3aca3e64c
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 22:52:30 +0000

    Refactored removable_device_notifications_window_win_unittest.cc
    This a follow up of codereview.chromium.org/11297002.
    
    As a prep to fix MediaFileSystemRegistryTest.GalleryNameMTP on Windows, this CL has the following changes:
    
    (1) Moved test classes (TestPortableDeviceWatcherWin, TestVolumeMountWatcherWin and TestRemovableDeviceNotificationsWindowWin) from removable_device_notifications_window_win_unittest.cc to its own files.
    (2) Refactored RemovableDeviceNotificationsWindowWinTest to use the test classes.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/11886023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/system_monitor/portable_device_watcher_win.h
+++ chrome/browser/system_monitor/portable_device_watcher_win.h
@@ -25,6 +25,10 @@ class FilePath;
 
 namespace chrome {
 
+namespace test {
+class TestPortableDeviceWatcherWin;
+}
+
 // This class watches the portable device mount points and sends notifications
 // to base::SystemMonitor about the attached/detached media transfer protocol
 // (MTP) devices. This is a singleton class instantiated by
@@ -84,7 +88,7 @@ class PortableDeviceWatcherWin {
                                      string16* storage_object_id);
 
  private:
-  friend class TestPortableDeviceWatcherWin;
+  friend class test::TestPortableDeviceWatcherWin;
 
   // Key: MTP device storage unique id.
   // Value: Metadata for the given storage.
@@ -97,13 +101,13 @@ class PortableDeviceWatcherWin {
 
   // Helpers to enumerate existing MTP storage devices.
   virtual void EnumerateAttachedDevices();
-  virtual void OnDidEnumerateAttachedDevices(const Devices* devices,
-                                             const bool result);
+  void OnDidEnumerateAttachedDevices(const Devices* devices,
+                                     const bool result);
 
   // Helpers to handle device attach event.
   virtual void HandleDeviceAttachEvent(const string16& pnp_device_id);
-  virtual void OnDidHandleDeviceAttachEvent(
-      const DeviceDetails* device_details, const bool result);
+  void OnDidHandleDeviceAttachEvent(const DeviceDetails* device_details,
+                                    const bool result);
 
   // Handles the detach event of the device specified by |pnp_device_id|.
   void HandleDeviceDetachEvent(const string16& pnp_device_id);
--- chrome/browser/system_monitor/removable_device_notifications_window_win.h
+++ chrome/browser/system_monitor/removable_device_notifications_window_win.h
@@ -16,6 +16,10 @@ class FilePath;
 
 namespace chrome {
 
+namespace test {
+class TestRemovableDeviceNotificationsWindowWin;
+}
+
 class PortableDeviceWatcherWin;
 class VolumeMountWatcherWin;
 
@@ -43,7 +47,7 @@ class RemovableDeviceNotificationsWindowWin
 
  private:
   class PortableDeviceNotifications;
-  friend class TestRemovableDeviceNotificationsWindowWin;
+  friend class test::TestRemovableDeviceNotificationsWindowWin;
 
   // To support unit tests, this constructor takes |volume_mount_watcher| and
   // |portable_device_watcher| objects. These params are either constructed in
