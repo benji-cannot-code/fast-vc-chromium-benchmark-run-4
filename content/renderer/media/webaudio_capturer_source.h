bdb59d8854bce820df2045b8e29670c7524fd4bd
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-18 12:45:52 +0000

    Update some #includes in content/renderer/media for headers moved to the new Platform directory
    Review URL: https://chromiumcodereview.appspot.com/12003002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177658 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webaudio_capturer_source.h
+++ content/renderer/media/webaudio_capturer_source.h
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/audio_capturer_source.h"
 #include "media/base/audio_fifo.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebAudioDestinationConsumer.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebVector.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebVector.h"
 
 namespace content {
 
