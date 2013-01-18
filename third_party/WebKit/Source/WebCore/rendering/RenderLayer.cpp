a4089690c1414016cb52bf0c3e064a8a6a350566
leviw@chromium.org <leviw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-18 20:21:57 +0000

    LayoutUnit should round half consistently, not away from zero https://bugs.webkit.org/show_bug.cgi?id=107208
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    The current implementation of LayoutUnit::round() always rounds half away from zero. This can result in
    a shift of 1 pixel moving an element aligned at a half-pixel by 2. This problem is particularly common with
    RenderLayers, which pass the sub-pixel accumulation into their renderers after translating the graphics
    context passed in by their rounded location. This changes round() to always round 0.5 up.
    
    Test: fast/sub-pixel/sub-pixel-root-layer.html
    
    * platform/LayoutUnit.h:
    (WebCore::LayoutUnit::round): Always round 0.5 up.
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::paintLayerContents): We no longer need to prevent rootLayers from passing in a
    sub-pixel offset. This eliminates the shifting of elements when applying translateZ(0) or similar.
    
    Tools:
    
    * TestWebKitAPI/Tests/WebCore/LayoutUnit.cpp: Updating to reflect and
    cover this change in rounding behavior.
    
    LayoutTests:
    
    * fast/sub-pixel/sub-pixel-root-layer-expected.html: Added.
    * fast/sub-pixel/sub-pixel-root-layer.html: Added.
    * platform/chromium-linux/fast/regions/overflow-moving-below-floats-in-variable-width-regions-expected.png:
    * platform/chromium-linux/fast/regions/overflow-size-change-in-variable-width-regions-expected.png:
    * platform/chromium-linux/fast/regions/overflow-size-change-with-stacking-context-expected.png:
    * platform/chromium-win/fast/regions/overflow-moving-below-floats-in-variable-width-regions-expected.txt:
    * platform/chromium-win/fast/regions/overflow-size-change-in-variable-width-regions-expected.txt:
    * platform/chromium-win/fast/regions/overflow-size-change-with-stacking-context-expected.txt:
    * platform/chromium/TestExpectations:
    * platform/mac/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -3665,8 +3665,6 @@ void RenderLayer::paintLayerContents(GraphicsContext* context, const LayerPainti
         ClipRectsContext clipRectsContext(localPaintingInfo.rootLayer, localPaintingInfo.region, (localPaintFlags & PaintLayerTemporaryClipRects) ? TemporaryClipRects : PaintingClipRects, IgnoreOverlayScrollbarSize, localPaintFlags & PaintLayerPaintingOverflowContents ? IgnoreOverflowClip : RespectOverflowClip);
         calculateRects(clipRectsContext, localPaintingInfo.paintDirtyRect, layerBounds, damageRect, clipRectToApply, outlineRect, &offsetFromRoot);
         paintOffset = toPoint(layerBounds.location() - renderBoxLocation() + localPaintingInfo.subPixelAccumulation);
-        if (this == localPaintingInfo.rootLayer)
-            paintOffset = roundedIntPoint(paintOffset);
     }
 
     bool forceBlackText = localPaintingInfo.paintBehavior & PaintBehaviorForceBlackText;
