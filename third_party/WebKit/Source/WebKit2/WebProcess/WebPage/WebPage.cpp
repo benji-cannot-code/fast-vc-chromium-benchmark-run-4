8eacc439af2f7e6d3ea95010752cc01cf8abb122
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-19 02:10:41 +0000

    [wk2] Intrinsic content size should report a flexible width if the content is not wider than the minimum layout width https://bugs.webkit.org/show_bug.cgi?id=107334 <rdar://problem/12960009>
    Reviewed by Anders Carlsson.
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView _setIntrinsicContentSize:]): If the content is laid-out at or below the minimum layout width, it's flexible; otherwise, we need to inform autolayout that we require more space via our intrinsic content size.
    * WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp:
    (WebKit::WebFrameLoaderClient::transitionToCommittedForNewPage): Allow auto-sizing to expand its width beyond the minimum if needed.
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::setMinimumLayoutWidth): Allow auto-sizing to expand its width beyond the minimum if needed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.cpp
@@ -3789,8 +3789,10 @@ void WebPage::setMinimumLayoutWidth(double minimumLayoutWidth)
 
     m_minimumLayoutWidth = minimumLayoutWidth;
 
+    int maximumSize = std::numeric_limits<int>::max();
+
     if (minimumLayoutWidth > 0)
-        corePage()->mainFrame()->view()->enableAutoSizeMode(true, IntSize(minimumLayoutWidth, 1), IntSize(minimumLayoutWidth, INT_MAX));
+        corePage()->mainFrame()->view()->enableAutoSizeMode(true, IntSize(minimumLayoutWidth, 1), IntSize(maximumSize, maximumSize));
     else
         corePage()->mainFrame()->view()->enableAutoSizeMode(false, IntSize(), IntSize());
 }
