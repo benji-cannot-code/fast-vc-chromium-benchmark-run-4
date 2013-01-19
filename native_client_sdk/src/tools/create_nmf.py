21fe17fa6479927470157cf3a41d19e30a765e24
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-19 03:54:15 +0000

    Have create_nmf.py generate a hash of PNaCl pexes for caching.
    Without it, pnacl doesn't cache any translations which means
    that pnacl browser_tests won't be able to test caching.
    It also means users won't benefit from caching.
    
    Alternatively, we could use a sha embedded within the pexe
    that the pexe linker generates, but we have not converged
    on which is better...
    
    sort of related to
    BUG=http://code.google.com/p/nativeclient/issues/detail?id=2992
    
    Review URL: https://chromiumcodereview.appspot.com/12017030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177818 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/tools/create_nmf.py
+++ native_client_sdk/src/tools/create_nmf.py
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import errno
+import hashlib
 import json
 import optparse
 import os
@@ -391,8 +392,12 @@ class NmfUtils(object):
     manifest = {}
     manifest[PROGRAM_KEY] = {}
     manifest[PROGRAM_KEY][PORTABLE_KEY] = {}
+    sha = hashlib.sha256()
+    with open(self.main_files[0], 'rb') as f:
+      sha.update(f.read())
     manifest[PROGRAM_KEY][PORTABLE_KEY][TRANSLATE_KEY] = {
-      "url": os.path.basename(self.main_files[0])
+      "url": os.path.basename(self.main_files[0]),
+      "sha256": sha.hexdigest()
     }
     self.manifest = manifest
 
