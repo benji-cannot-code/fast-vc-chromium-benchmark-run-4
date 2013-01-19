1e5bec5648e89ee914dd769aed3f5fe652de15f9
msaboff@apple.com <msaboff@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-19 01:47:47 +0000

    Unreviewed build fix for release builds when DFG_ENABLE_DEBUG_PROPAGATION_VERBOSE is set to 1 in DFGCommon.h.
    * dfg/DFGCFAPhase.cpp: Added #include "Operations.h"
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGCFAPhase.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGCFAPhase.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DFGAbstractState.h"
 #include "DFGGraph.h"
 #include "DFGPhase.h"
+#include "Operations.h"
 
 namespace JSC { namespace DFG {
 
