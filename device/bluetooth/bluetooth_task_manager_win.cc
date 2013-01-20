1523d9df405a7ba3ce585e59f31a10757cb98ea4
youngki@chromium.org <youngki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-20 01:09:51 +0000

    Implemented BluetoothServiceRecordWin.
    I am not sure if unittest is worthwhile for BluetoothServiceRecordWin since the raw byte stream that BluetoothServiceRecordWin is getting is very windows-specific.. I could get some byte dumps and use that to implement a simple unittest that just verifies that we are parsing uuid correctly, but I am not sure if that's worthwhile.
    
    BUG=135470
    
    
    Review URL: https://chromiumcodereview.appspot.com/11969030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177861 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth_task_manager_win.cc
+++ device/bluetooth/bluetooth_task_manager_win.cc
@@ -5,12 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "device/bluetooth/bluetooth_task_manager_win.h"
 
-#include <BluetoothAPIs.h>
-#if defined(_WIN32_WINNT_WIN8) && _MSC_VER < 1700
-// The Windows 8 SDK defines FACILITY_VISUALCPP in winerror.h.
-#undef FACILITY_VISUALCPP
-#endif
-#include <delayimp.h>
 #include <string>
 
 #include "base/bind.h"
@@ -22,8 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/sequenced_worker_pool.h"
 #include "base/threading/thread_restrictions.h"
 #include "base/win/scoped_handle.h"
-
-#pragma comment(lib, "Bthprops.lib")
+#include "device/bluetooth/bluetooth_includes_win.h"
 
 namespace {
 
