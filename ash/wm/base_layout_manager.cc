cabeb81ac18b69ddf551bbb50edccbc21c1bdb8a
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-20 00:38:03 +0000

    * Call LayoutManager::OnChildWindowVisibilityChanged first because LayoutManager is responsible for updating the show state. * Show the gained_window only if it wasn't visible.
    While investigating DCHECK failure in BaseLayoutManager::OnWindowActivated (168383), I found the following:
    
    a) The window's show status is managed and updated by WorkspaceLayoutManager.
    b) OnWindowTargetVisibilityChanged sets the focus to the content area, which
       eventually activate the browser window.
    c) b) invokes BaseLayoutManager::OnWindowActivated. At this point, the
      window's state is still minimized, but it's already shown. so showing it again
      doesn't change the state, thus hit DCHECK.
    
    Strictly speaking, only base_layout_manager.cc change is necessary to fix
    the DCHECK failure. I changed the OnChildWindowVisiblityChanged because
    I think the delegate should see the latest state updated by the ash WM.
    
    BUG=168383
    TEST=covered by test.
    
    Review URL: https://chromiumcodereview.appspot.com/11867011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177856 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/base_layout_manager.cc
+++ ash/wm/base_layout_manager.cc
@@ -157,7 +157,8 @@ void BaseLayoutManager::OnWindowDestroying(aura::Window* window) {
 void BaseLayoutManager::OnWindowActivated(aura::Window* gained_active,
                                           aura::Window* lost_active) {
   if (views::corewm::UseFocusController()) {
-    if (gained_active && wm::IsWindowMinimized(gained_active)) {
+    if (gained_active && wm::IsWindowMinimized(gained_active) &&
+        !gained_active->IsVisible()) {
       gained_active->Show();
       DCHECK(!wm::IsWindowMinimized(gained_active));
     }
