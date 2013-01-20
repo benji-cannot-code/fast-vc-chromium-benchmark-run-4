93546093431bd8dfde4f3e15f1cc8db9f9375394
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-20 19:55:39 +0000

    cc: Use the size of the source frame along with the source frame's renderpasses
    Scale the contents of the source frame to fit exactly in the bounds of the
    delegated renderer layer.
    
    When the host window is being resized, it can keep the delegated renderer layer
    at the size it wants the output displayed, and clip it with a containing layer
    until the resize is complete and it has a new frame to show at the right size.
    
    Tests:
    DelegatedRendererLayerImplTestSharedData.SharedData
    DelegatedRendererLayerImplTestSharedData.DisplaySize
    
    BUG=123444
    R=enne
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/11821015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177878 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_impl.cc
+++ cc/layer_tree_host_impl.cc
@@ -536,6 +536,7 @@ bool LayerTreeHostImpl::calculateRenderPasses(FrameData& frame)
                != frame.renderPassesById.end());
     }
 #endif
+    DCHECK(frame.renderPasses.back()->output_rect.origin().IsOrigin());
 
     if (!activeTree()->has_transparent_background()) {
         frame.renderPasses.back()->has_transparent_background = false;
