2bfb32681b0d9820ffb88c6153ccedaf00d80336
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-20 02:49:22 +0000

    google_apis: Remove unused includes of chrome_paths.h from two unittests.
    BUG=146989
    TBR=satorux@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12018035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177870 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/drive_api_parser_unittest.cc
+++ chrome/browser/google_apis/drive_api_parser_unittest.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/gdata_wapi_parser.h"
 #include "chrome/browser/google_apis/test_util.h"
 #include "chrome/browser/google_apis/time_util.h"
-#include "chrome/common/chrome_paths.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::Value;
