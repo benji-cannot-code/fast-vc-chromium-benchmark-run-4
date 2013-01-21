419d584c87f6eb9eb3e0ef24f881723fcaa65e9d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-21 00:11:43 +0000

    Simplify a list of negative PLATFORM() tests https://bugs.webkit.org/show_bug.cgi?id=107365
    Patch by Laszlo Gombos <l.gombos@samsung.com> on 2013-01-20
    Reviewed by Eric Seidel.
    
    Simplify a list of negative PLATFORM() tests into a simpler list
    of positive tests for better readability and maintenance.
    
    No new tests as there is no new functionality.
    
    * config.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140283 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/config.h
+++ third_party/WebKit/Source/WebCore/config.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/Platform.h>
 
-#if OS(WINDOWS) && !OS(WINCE) && !PLATFORM(QT) && !PLATFORM(CHROMIUM) && !PLATFORM(GTK) && !PLATFORM(WX)
+#if PLATFORM(WIN) && !OS(WINCE)
 #include <WebCore/WebCoreHeaderDetection.h>
 #endif
 
