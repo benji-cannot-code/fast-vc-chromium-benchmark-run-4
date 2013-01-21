5e26c04d5776aaeb87f82704a9109cf1db28d1f1
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-21 03:20:06 +0000

    views: Break the dependency in SadTabView from SadTabHelper.
    This removes everything related to views from the cross-platform SadTabHelper
    API and move it into SadTabView.
    
    BUG=125846
    TEST=navigate to chrome://crash, chrome://kill or open the Task Manager and
    end some process. Every thing should work as before, no regressions observed.
    R=ben@chromium.org,sky@chromium.org,sail@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/11886088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177884 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/sad_tab_view.h
+++ chrome/browser/ui/views/sad_tab_view.h
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "chrome/browser/ui/sad_tab_types.h"
+#include "chrome/browser/ui/sad_tab.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/link_listener.h"
 #include "ui/views/view.h"
@@ -30,7 +30,8 @@ class TextButton;
 //  "sad tab" in the browser window when a renderer is destroyed unnaturally.
 //
 ///////////////////////////////////////////////////////////////////////////////
-class SadTabView : public views::View,
+class SadTabView : public chrome::SadTab,
+                   public views::View,
                    public views::LinkListener,
                    public views::ButtonListener {
  public:
@@ -55,6 +56,10 @@ class SadTabView : public views::View,
   virtual void OnPaint(gfx::Canvas* canvas) OVERRIDE;
 
  private:
+  // Overridden from chrome::SadTab:
+  virtual void Show() OVERRIDE;
+  virtual void Close() OVERRIDE;
+
   views::Label* CreateLabel(const string16& text);
   views::Link* CreateLink(const string16& text);
 
--- chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.cc
+++ chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/tab_contents/chrome_web_contents_view_delegate.h"
+#include "chrome/browser/ui/views/sad_tab_view.h"
 #include "chrome/browser/ui/views/tab_contents/render_view_context_menu_views.h"
 #include "chrome/browser/ui/web_contents_modal_dialog.h"
 #include "chrome/browser/ui/web_contents_modal_dialog_manager.h"
@@ -62,9 +63,9 @@ content::WebDragDestDelegate*
 bool ChromeWebContentsViewDelegateViews::Focus() {
   SadTabHelper* sad_tab_helper = SadTabHelper::FromWebContents(web_contents_);
   if (sad_tab_helper) {
-    views::Widget* sad_tab = sad_tab_helper->sad_tab();
+    SadTabView* sad_tab = static_cast<SadTabView*>(sad_tab_helper->sad_tab());
     if (sad_tab) {
-      sad_tab->GetContentsView()->RequestFocus();
+      sad_tab->RequestFocus();
       return true;
     }
   }
@@ -170,9 +171,9 @@ void ChromeWebContentsViewDelegateViews::SizeChanged(const gfx::Size& size) {
   SadTabHelper* sad_tab_helper = SadTabHelper::FromWebContents(web_contents_);
   if (!sad_tab_helper)
     return;
-  views::Widget* sad_tab = sad_tab_helper->sad_tab();
+  SadTabView* sad_tab = static_cast<SadTabView*>(sad_tab_helper->sad_tab());
   if (sad_tab)
-    sad_tab->SetBounds(gfx::Rect(size));
+    sad_tab->GetWidget()->SetBounds(gfx::Rect(size));
 }
 
 views::Widget* ChromeWebContentsViewDelegateViews::GetTopLevelWidget() {
