140d2d5ce0b34f6d6a99625b6ef00ca3510cebc5
mikelawther@chromium.org <mikelawther@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-21 07:05:16 +0000

    CSS3 calc: unprefix implementation https://bugs.webkit.org/show_bug.cgi?id=91951
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Add support for the unprefixed 'calc()' function, while retaining support for the prefixed version.
    
    Test: css3/calc/simple-calcs-prefixed.html
    
    * css/CSSCalculationValue.cpp:
    (WebCore::buildCssText):
    (WebCore::CSSCalcValue::create):
    * css/CSSParser.cpp:
    (WebCore::CSSParser::isCalculation):
    (WebCore::CSSParser::detectFunctionTypeToken):
    
    LayoutTests:
    
    Changed all -webkit-calc() occurrences to calc().
    Made a copy of the simplest test file so very basic tests for the prefixed version
    are retained.
    
    * css3/calc/background-position-parsing.html:
    * css3/calc/block-mask-overlay-image-outset.html:
    * css3/calc/border-radius.html:
    * css3/calc/border.html:
    * css3/calc/box-reflect.html:
    * css3/calc/calc-errors.html:
    * css3/calc/color-hsl.html:
    * css3/calc/color-rgb.html:
    * css3/calc/css3-radial-gradients.html:
    * css3/calc/cssom-expected.txt:
    * css3/calc/cssom.html:
    * css3/calc/font-monospace.html:
    * css3/calc/font-size-fractional.html:
    * css3/calc/font-size.html:
    * css3/calc/font.html:
    * css3/calc/getComputedStyle-margin-percentage.html:
    * css3/calc/gradient-color-stops.html:
    * css3/calc/img-size.html:
    * css3/calc/line-height.html:
    * css3/calc/margin.html:
    * css3/calc/negative-padding.html:
    * css3/calc/padding.html:
    * css3/calc/reflection-computed-style-expected.txt:
    * css3/calc/reflection-computed-style.html:
    * css3/calc/regression-62276.html:
    * css3/calc/simple-calcs-prefixed-expected.txt: Added.
    * css3/calc/simple-calcs-prefixed.html: Copied from LayoutTests/css3/calc/simple-calcs.html.
    * css3/calc/simple-calcs.html:
    * css3/calc/simple-composited-mask.html:
    * css3/calc/table-border-spacing.html:
    * css3/calc/table-calcs.html:
    * css3/calc/table-empty-cells.html:
    * css3/calc/transforms-scale.html:
    * css3/calc/transforms-translate.html:
    * css3/calc/transition-crash.html:
    * css3/calc/transition-crash2.html:
    * css3/calc/transition-crash3.html:
    * css3/calc/transition-crash4.html:
    * css3/calc/transitions-dependent.html:
    * css3/calc/transitions.html:
    * css3/calc/vertical-align-expected.html:
    * css3/calc/vertical-align.html:
    * css3/calc/zoom-with-em.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSParser.cpp
@@ -5851,7 +5851,8 @@ inline double CSSParser::parsedDouble(CSSParserValue *v, ReleaseParsedCalcValueC
 bool CSSParser::isCalculation(CSSParserValue* value) 
 {
     return (value->unit == CSSParserValue::Function) 
-        && (equalIgnoringCase(value->function->name, "-webkit-calc(")
+        && (equalIgnoringCase(value->function->name, "calc(")
+            || equalIgnoringCase(value->function->name, "-webkit-calc(")
             || equalIgnoringCase(value->function->name, "-webkit-min(")
             || equalIgnoringCase(value->function->name, "-webkit-max("));
 }
@@ -9708,6 +9709,13 @@ inline bool CSSParser::detectFunctionTypeToken(int length)
             return true;
         }
 #endif
+        return false;
+
+    case 4:
+        if (isEqualToCSSIdentifier(name, "calc")) {
+            m_token = CALCFUNCTION;
+            return true;
+        }
         return false;
 
     case 9:
