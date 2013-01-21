35889df97dae61f3fdbdcd3b4f267a6f9ce7e925
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-21 16:17:26 +0000

    REGRESSION (r140285): GTK uses WKPageResourceLoadClient which was removed https://bugs.webkit.org/show_bug.cgi?id=107421
    Source/WebKit2:
    
    Unreviewed buildfix.
    
    * GNUmakefile.list.am:
    * UIProcess/API/gtk/WebKitResourceLoadClient.cpp: Removed.
    * UIProcess/API/gtk/WebKitResourceLoadClient.h: Removed.
    * UIProcess/API/gtk/WebKitWebView.cpp: No longer attach unexisting ResourceLoadClient.
    (webkitWebViewConstructed):
    
    Tools:
    
    Unreviewed build fix.
    
    * Scripts/run-gtk-tests:
    (TestRunner): skip Resource tests.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitWebView.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitWebView.cpp
@@ -42,7 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKitPolicyClient.h"
 #include "WebKitPrintOperationPrivate.h"
 #include "WebKitPrivate.h"
-#include "WebKitResourceLoadClient.h"
 #include "WebKitResponsePolicyDecision.h"
 #include "WebKitScriptDialogPrivate.h"
 #include "WebKitSettingsPrivate.h"
@@ -465,7 +464,6 @@ static void webkitWebViewConstructed(GObject* object)
     attachLoaderClientToView(webView);
     attachUIClientToView(webView);
     attachPolicyClientToView(webView);
-    attachResourceLoadClientToView(webView);
     attachFullScreenClientToView(webView);
     attachContextMenuClientToView(webView);
     attachFormClientToView(webView);
