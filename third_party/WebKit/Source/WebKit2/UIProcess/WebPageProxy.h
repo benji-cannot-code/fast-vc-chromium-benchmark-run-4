b6b3c728f6a42f1ab381bf8b6dfa75bc0c361d70
weinig@apple.com <weinig@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-21 01:01:40 +0000

    Remove support getting per-resource callbacks in the UIProcess https://bugs.webkit.org/show_bug.cgi?id=107405
    Reviewed by Anders Carlsson.
    
    Removes support for the WKPageResourceLoadClient. Access to per-resource
    callbacks are still available in the bundle, and via the WebInspector.
    
    * UIProcess/API/C/WKPage.cpp:
    * UIProcess/API/C/WKPage.h:
    * UIProcess/WebPageProxy.cpp:
    * UIProcess/WebPageProxy.h:
    * UIProcess/WebPageProxy.messages.in:
    Stop handling per-resource messages and forwarding them to the API.
    
    * UIProcess/WebResourceLoadClient.cpp: Removed.
    * UIProcess/WebResourceLoadClient.h: Removed.
    * WebKit2.xcodeproj/project.pbxproj:
    Remove the files.
    
    * WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp:
    (WebKit::WebFrameLoaderClient::assignIdentifierToInitialRequest):
    (WebKit::WebFrameLoaderClient::dispatchWillSendRequest):
    (WebKit::WebFrameLoaderClient::dispatchDidReceiveResponse):
    (WebKit::WebFrameLoaderClient::dispatchDidReceiveContentLength):
    (WebKit::WebFrameLoaderClient::dispatchDidFinishLoading):
    (WebKit::WebFrameLoaderClient::dispatchDidFailLoading):
    Stop sending per-resource messages to the UIProcess.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
@@ -58,7 +58,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebPageContextMenuClient.h"
 #include "WebPolicyClient.h"
 #include "WebPopupMenuProxy.h"
-#include "WebResourceLoadClient.h"
 #include "WebUIClient.h"
 #include <WebCore/AlternativeTextClient.h>
 #include <WebCore/Color.h>
@@ -282,7 +281,6 @@ public:
     void initializeFormClient(const WKPageFormClient*);
     void initializeLoaderClient(const WKPageLoaderClient*);
     void initializePolicyClient(const WKPagePolicyClient*);
-    void initializeResourceLoadClient(const WKPageResourceLoadClient*);
     void initializeUIClient(const WKPageUIClient*);
 
     void initializeWebPage();
@@ -820,14 +818,6 @@ private:
 
     void willSubmitForm(uint64_t frameID, uint64_t sourceFrameID, const StringPairVector& textFieldValues, uint64_t listenerID, CoreIPC::MessageDecoder&);
 
-    // Resource load client
-    void didInitiateLoadForResource(uint64_t frameID, uint64_t resourceIdentifier, const WebCore::ResourceRequest&, bool pageIsProvisionallyLoading);
-    void didSendRequestForResource(uint64_t frameID, uint64_t resourceIdentifier, const WebCore::ResourceRequest&, const WebCore::ResourceResponse& redirectResponse);
-    void didReceiveResponseForResource(uint64_t frameID, uint64_t resourceIdentifier, const WebCore::ResourceResponse&);
-    void didReceiveContentLengthForResource(uint64_t frameID, uint64_t resourceIdentifier, uint64_t contentLength);
-    void didFinishLoadForResource(uint64_t frameID, uint64_t resourceIdentifier);
-    void didFailLoadForResource(uint64_t frameID, uint64_t resourceIdentifier, const WebCore::ResourceError&);
-
     // UI client
     void createNewPage(const WebCore::ResourceRequest&, const WebCore::WindowFeatures&, uint32_t modifiers, int32_t mouseButton, uint64_t& newPageID, WebPageCreationParameters&);
     void showPage();
@@ -1047,7 +1037,6 @@ private:
     WebLoaderClient m_loaderClient;
     WebPolicyClient m_policyClient;
     WebFormClient m_formClient;
-    WebResourceLoadClient m_resourceLoadClient;
     WebUIClient m_uiClient;
     WebFindClient m_findClient;
     WebFindMatchesClient m_findMatchesClient;
