eb9bf1354ff2f9a31e9186464fac6492f0ca046d
markusheintz@chromium.org <markusheintz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-21 15:32:37 +0000

    Remove unused const variables. The key names used to be used for accessing dictionary value of the obsolete MEDIASTREAM content setting.
    BUG=TODO
    
    
    Review URL: https://chromiumcodereview.appspot.com/11962040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177922 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/media_stream_devices_controller.cc
+++ chrome/browser/media/media_stream_devices_controller.cc
@@ -39,9 +39,6 @@ bool HasAnyAvailableDevice() {
   return !audio_devices.empty() || !video_devices.empty();
 };
 
-const char kAudioKey[] = "audio";
-const char kVideoKey[] = "video";
-
 }  // namespace
 
 MediaStreamDevicesController::MediaStreamDevicesController(
