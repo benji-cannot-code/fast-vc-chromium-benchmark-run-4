bf8d83dfde2786c9d4b7097f6c7f8681e502a424
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-21 12:06:57 +0000

    Web Inspector: Do not dispatch mousemove when emulating touch event and no touch is active https://bugs.webkit.org/show_bug.cgi?id=107419
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    mousemove event should not be dispatched when emulating touch events and mouse button is not pressed
    
    * page/EventHandler.cpp:
    (WebCore::EventHandler::dispatchSyntheticTouchEventIfEnabled):
    
    LayoutTests:
    
    * fast/events/touch/emulate-touch-events.html: Test that mousemove is dispatched not more than once
      and reset the touch event emulation flag in window.internals.settings upon test completion.
    * fast/events/touch/emulated-touch-iframe.html:
      Reset the touch event emulation flag in window.internals.settings upon test completion.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140321 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.cpp
+++ third_party/WebKit/Source/WebCore/page/EventHandler.cpp
@@ -3982,7 +3982,7 @@ bool EventHandler::dispatchSyntheticTouchEventIfEnabled(const PlatformMouseEvent
         return false;
 
     if (eventType == PlatformEvent::MouseMoved && !m_touchPressed)
-        return false;
+        return true;
 
     HitTestRequest request(HitTestRequest::Active);
     MouseEventWithHitTestResults mev = prepareMouseEvent(request, event);
