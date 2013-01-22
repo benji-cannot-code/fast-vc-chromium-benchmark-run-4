a09fb7c49c390b732ad04ae03d7686423ae4abc2
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-22 21:53:25 +0000

    Disable auto hide behavior when there are no visible windows.
    BUG=164262
    
    
    Review URL: https://chromiumcodereview.appspot.com/11941008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178110 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/shelf_layout_manager.cc
+++ ash/wm/shelf_layout_manager.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shell_window_ids.h"
 #include "ash/system/status_area_widget.h"
 #include "ash/wm/property_util.h"
+#include "ash/wm/window_cycle_controller.h"
+#include "ash/wm/window_util.h"
 #include "ash/wm/workspace_controller.h"
 #include "ash/wm/workspace/workspace_animations.h"
 #include "base/auto_reset.h"
@@ -859,8 +861,22 @@ ShelfAutoHideState ShelfLayoutManager::CalculateAutoHideState(
                        alignment_ == SHELF_ALIGNMENT_TOP ?
                            -kNotificationBubbleGapHeight : 0);
   }
-  return shelf_region.Contains(Shell::GetScreen()->GetCursorScreenPoint()) ?
-      SHELF_AUTO_HIDE_SHOWN : SHELF_AUTO_HIDE_HIDDEN;
+
+  if (shelf_region.Contains(Shell::GetScreen()->GetCursorScreenPoint()))
+    return SHELF_AUTO_HIDE_SHOWN;
+
+  const std::vector<aura::Window*> windows =
+      ash::WindowCycleController::BuildWindowList(NULL);
+
+  // Process the window list and check if there are any visible windows.
+  for (size_t i = 0; i < windows.size(); ++i) {
+    if (windows[i] && windows[i]->IsVisible() &&
+        !ash::wm::IsWindowMinimized(windows[i]))
+      return SHELF_AUTO_HIDE_HIDDEN;
+  }
+
+  // If there are no visible windows do not hide the shelf.
+  return SHELF_AUTO_HIDE_SHOWN;
 }
 
 void ShelfLayoutManager::UpdateHitTestBounds() {
