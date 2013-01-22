6eb611e98a4272803deaa14a9245848d09884910
falken@chromium.org <falken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-22 12:15:14 +0000

    Move top layer flag from ElementRareData to NodeFlags https://bugs.webkit.org/show_bug.cgi?id=107542
    Reviewed by Hajime Morita.
    
    This is a speculative fix for a perf regression (https://bugs.webkit.org/show_bug.cgi?id=106726)
    likely due to checking the top layer flag in Element::removedFrom. It
    also simplifies code dealing with top layer and makes it no longer
    necessary to allocate rare data for top layer.
    
    * dom/Element.cpp:
    (WebCore::Element::removedFrom): Check isInTopLayer first in case it helps fix the perf regression.
    (WebCore):
    * dom/Element.h: Move top layer functions from Element to Node.
    * dom/ElementRareData.h:
    (ElementRareData):
    (WebCore::ElementRareData::ElementRareData): Remove top layer flag from ElementRareData.
    * dom/Node.cpp: Move top layer functions from Element to Node.
    (WebCore):
    (WebCore::Node::setIsInTopLayer):
    * dom/Node.h:
    (Node):
    (WebCore::Node::isInTopLayer):
    * dom/NodeRenderingContext.cpp: Remove unnecessary casts to Element for isInTopLayer().
    (WebCore::isRendererReparented):
    (WebCore::NodeRenderingContext::previousRenderer):
    (WebCore::NodeRenderingContext::parentRenderer):
    * rendering/RenderLayer.cpp: Ditto.
    (WebCore::RenderLayer::isInTopLayer):
    (WebCore::RenderLayer::rebuildZOrderLists):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140411 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayer.cpp
@@ -4015,7 +4015,7 @@ Node* RenderLayer::enclosingElement() const
 bool RenderLayer::isInTopLayer() const
 {
     Node* node = renderer()->node();
-    return node && node->isElementNode() && toElement(node)->isInTopLayer();
+    return node && node->isInTopLayer();
 }
 
 bool RenderLayer::isInTopLayerSubtree() const
@@ -5168,8 +5168,7 @@ void RenderLayer::rebuildZOrderLists()
     if (isRootLayer()) {
         RenderObject* view = renderer()->view();
         for (RenderObject* child = view->firstChild(); child; child = child->nextSibling()) {
-            Element* childElement = (child->node() && child->node()->isElementNode()) ? toElement(child->node()) : 0;
-            if (childElement && childElement->isInTopLayer()) {
+            if (child->node() && child->node()->isInTopLayer()) {
                 RenderLayer* layer = toRenderLayerModelObject(child)->layer();
                 m_posZOrderList->append(layer);
             }
