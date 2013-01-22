7d88247a74284653a8e3a04297638c1bfae80d5c
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-22 21:20:26 +0000

    Remove uses of deprecated unittest.TestCase aliases https://bugs.webkit.org/show_bug.cgi?id=102252
    Reviewed by Adam Barth.
    
    Replace the deprecated assertEquals method of the TestCase interface
    used in some webkitpy unittests with the assertEqual method.
    
    * Scripts/webkitpy/common/system/profiler_unittest.py:
    (ProfilerFactoryTest._assert_default_profiler_name):
    (ProfilerFactoryTest.test_default_profiler_output):
    * Scripts/webkitpy/layout_tests/port/gtk_unittest.py:
    (GtkPortTest.test_expectations_files):
    * Scripts/webkitpy/layout_tests/servers/http_server_unittest.py:
    (TestHttpServer.test_win32_start_and_stop):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140456 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/servers/http_server_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/servers/http_server_unittest.py
@@ -99,4 +99,4 @@ class TestHttpServer(unittest.TestCase):
         server._wait_for_action = wait_for_action
 
         server.stop()
-        self.assertEquals(['taskkill.exe', '/f', '/t', '/pid', 42], host.executive.calls[1])
+        self.assertEqual(['taskkill.exe', '/f', '/t', '/pid', 42], host.executive.calls[1])
