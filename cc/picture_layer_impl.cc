fe03bed753ba23a6b9ca40a53d22a657762b1c42
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-22 21:38:56 +0000

    cc: Update picture pile on tiles to prevent memory leak
    A given PicturePileImpl corresponds to a given main thread frame.  It should go
    out of scope when that frame is no longer being used.  Currently, as Tile holds
    a ref on PicturePileImpl, it won't go out of scope if the Tile persists, which
    could happen if the Tile never gets invalidated.  To prevent this, the
    PicturePileImpl on each Tile is updated to the newest version of the
    PicturePileImpl when a tree is activated.  This will allow old PicturePileImpls
    to be destroyed on a more regular basis.
    
    Raster jobs in flight hold their own ref to a per-thead clone of the
    PicturePileImpl, so this will not affect them.  When the job finishes, the
    clone will also be destroyed.
    
    R=reveman@chromium.org
    BUG=171003
    
    
    Review URL: https://chromiumcodereview.appspot.com/11953005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178106 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_layer_impl.cc
+++ cc/picture_layer_impl.cc
@@ -180,7 +180,7 @@ void PictureLayerImpl::didUpdateTransforms() {
 }
 
 void PictureLayerImpl::didBecomeActive() {
-  tilings_.MoveTilePriorities(PENDING_TREE, ACTIVE_TREE);
+  tilings_.DidBecomeActive();
 }
 
 void PictureLayerImpl::didLoseOutputSurface() {
@@ -235,6 +235,10 @@ scoped_refptr<Tile> PictureLayerImpl::CreateTile(PictureLayerTiling* tiling,
       GL_RGBA,
       rect,
       tiling->contents_scale()));
+}
+
+void PictureLayerImpl::UpdatePile(Tile* tile) {
+  tile->set_picture_pile(pile_);
 }
 
 void PictureLayerImpl::SyncFromActiveLayer() {
--- cc/picture_layer_impl.h
+++ cc/picture_layer_impl.h
@@ -45,6 +45,7 @@ public:
   // PictureLayerTilingClient overrides.
   virtual scoped_refptr<Tile> CreateTile(PictureLayerTiling*,
                                          gfx::Rect) OVERRIDE;
+  virtual void UpdatePile(Tile* tile) OVERRIDE;
 
   // PushPropertiesTo active tree => pending tree
   void SyncFromActiveLayer();
