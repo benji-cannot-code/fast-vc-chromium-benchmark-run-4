3c1d6e3447e1ecfbf0e48a2946304e33c02eb2f5
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-22 10:24:30 +0000

    Turn on ENABLE_THREADED_HTML_PARSER for Chromium (it's still disabled at runtime) https://bugs.webkit.org/show_bug.cgi?id=107519
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Add some ASSERTs to be double-dog-sure that we're never using
    any of the threading code when threading is not enabled.
    
    I also removed an if-branch in the !ENABLE_THREADED_HTML_PARSER case
    as it's not needed.
    
    * html/parser/HTMLDocumentParser.cpp:
    (WebCore::HTMLDocumentParser::prepareToStopParsing):
    (WebCore::HTMLDocumentParser::startBackgroundParser):
    (WebCore::HTMLDocumentParser::stopBackgroundParser):
    
    Source/WebKit/chromium:
    
    This makes our development lives easier, and makes it possible for the bots
    to run threaded-parser-only tests by toggling the runtime enable
    via window.internals.settings.
    
    * features.gypi:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140402 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/WebCore/html/parser/HTMLDocumentParser.cpp
@@ -147,11 +147,13 @@ void HTMLDocumentParser::prepareToStopParsing()
     // but we need to ensure it isn't deleted yet.
     RefPtr<HTMLDocumentParser> protect(this);
 
+#if ENABLE(THREADED_HTML_PARSER)
     // NOTE: This pump should only ever emit buffered character tokens,
     // so ForceSynchronous vs. AllowYield should be meaningless.
     if (!shouldUseThreading())
+#endif
         pumpTokenizerIfPossible(ForceSynchronous);
-    
+
     if (isStopped())
         return;
 
@@ -455,6 +457,7 @@ void HTMLDocumentParser::insert(const SegmentedString& source)
 
 void HTMLDocumentParser::startBackgroundParser()
 {
+    ASSERT(shouldUseThreading());
     ASSERT(!m_haveBackgroundParser);
     m_haveBackgroundParser = true;
 
@@ -466,6 +469,7 @@ void HTMLDocumentParser::startBackgroundParser()
 
 void HTMLDocumentParser::stopBackgroundParser()
 {
+    ASSERT(shouldUseThreading());
     ASSERT(m_haveBackgroundParser);
     m_haveBackgroundParser = false;
 
