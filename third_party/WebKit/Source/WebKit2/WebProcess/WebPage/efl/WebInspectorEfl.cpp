51e4e0c942927a63e30e5bc344335994ccef10c3
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-22 01:04:34 +0000

    [EFL][WK2] Implement WebInspector::localizedStringsURL() on EFL https://bugs.webkit.org/show_bug.cgi?id=107363
    Patch by Sudarsana Nagineni <sudarsana.nagineni@intel.com> on 2013-01-21
    Reviewed by Benjamin Poulain.
    
    Implement WebInspector::localizedStringsURL() method to return the
    file URL of the localizedStrings.js.
    
    This prevents printing warnings about "Localized string not found" in
    the console and fixes Web inspector related failing tests on the bots.
    
    * WebProcess/WebPage/efl/WebInspectorEfl.cpp:
    (WebKit::WebInspector::localizedStringsURL):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140372 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/efl/WebInspectorEfl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/efl/WebInspectorEfl.cpp
@@ -29,15 +29,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(INSPECTOR)
 
-#include <WebCore/NotImplemented.h>
+#include <WebCore/EflInspectorUtilities.h>
 #include <wtf/text/WTFString.h>
 
 namespace WebKit {
 
 String WebInspector::localizedStringsURL() const
 {
-    notImplemented();
-    return String();
+    return "file://" + WebCore::inspectorResourcePath() + "/localizedStrings.js";
 }
 
 } // namespace WebKit
