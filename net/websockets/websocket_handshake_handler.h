e95d82cbcd60e6c95c290f4439ffe1ff6885058e
ricea@chromium.org <ricea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-22 03:43:31 +0000

    Trival fixups to websocket_handshake_handler.*.
    Stopped including some unused headers. Fixed some typos.
    
    No functional changes.
    
    BUG=
    TEST=net_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/12040005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@177976 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_handshake_handler.h
+++ net/websockets/websocket_handshake_handler.h
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // handshake headers.
 //
 // While we are working on updating WebSocket implementation in WebKit to
-// conform to the latest procotol draft, we need to accept both styles of
+// conform to the latest protocol draft, we need to accept both styles of
 // handshake. After we land the protocol changes in WebKit, we will be able to
 // drop codes handling old-style handshake.
 
@@ -32,11 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#include "base/memory/ref_counted.h"
 #include "net/base/net_export.h"
 #include "net/http/http_request_info.h"
 #include "net/http/http_response_info.h"
-#include "net/spdy/spdy_framer.h"
 #include "net/spdy/spdy_header_block.h"
 
 namespace net {
@@ -62,7 +60,7 @@ class NET_EXPORT_PRIVATE WebSocketHandshakeRequestHandler {
                      size_t headers_to_remove_len);
 
   // Gets request info to open WebSocket connection.
-  // Fills challange data (concatenation of key1, 2 and 3 for hybi-03 and
+  // Fills challenge data (concatenation of key1, 2 and 3 for hybi-03 and
   // earlier, or Sec-WebSocket-Key header value for hybi-04 and later)
   // in |challenge|.
   HttpRequestInfo GetRequestInfo(const GURL& url, std::string* challenge);
@@ -75,7 +73,7 @@ class NET_EXPORT_PRIVATE WebSocketHandshakeRequestHandler {
   // Gets WebSocket handshake raw request message to open WebSocket
   // connection.
   std::string GetRawRequest();
-  // Calling raw_length is valid only after GetRawRquest() call.
+  // Calling raw_length is valid only after GetRawRequest() call.
   size_t raw_length() const;
 
   // Returns the value of Sec-WebSocket-Version or Sec-WebSocket-Draft header
--- net/websockets/websocket_handshake_handler_spdy2_unittest.cc
+++ net/websockets/websocket_handshake_handler_spdy2_unittest.cc
@@ -6,16 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/websockets/websocket_handshake_handler.h"
 
 #include <string>
-#include <vector>
 
-#include "base/basictypes.h"
-#include "base/string_util.h"
 #include "googleurl/src/gurl.h"
-#include "net/http/http_response_headers.h"
-#include "net/http/http_util.h"
-#include "testing/gmock/include/gmock/gmock.h"
+#include "net/spdy/spdy_header_block.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "testing/platform_test.h"
 
 namespace net {
 
