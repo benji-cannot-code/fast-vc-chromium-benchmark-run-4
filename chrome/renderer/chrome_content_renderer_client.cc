2b987fa10f15dc22e23283f4576e6b01b262fc66
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-22 20:54:00 +0000

    Move TranslateHelper creation into ChromeRenderViewObserver
    TranslateHelper was created in ChromeContentRenderClient because
    TranslateHelper required AutofillAgent instance originally. But, currently
    it doesn't depend on it.
    This change move the creation of TranslateHelper instance into
    ChromeRenderViewObserver just for simplification.
    
    There is no functional change.
    
    BUG=n/a
    TEST=build all components
    
    
    Review URL: https://chromiumcodereview.appspot.com/12038017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -67,7 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/searchbox/searchbox_extension.h"
 #include "chrome/renderer/spellchecker/spellcheck.h"
 #include "chrome/renderer/spellchecker/spellcheck_provider.h"
-#include "chrome/renderer/translate_helper.h"
 #include "chrome/renderer/visitedlink_slave.h"
 #include "content/public/common/content_constants.h"
 #include "content/public/renderer/render_thread.h"
@@ -313,10 +312,9 @@ void ChromeContentRendererClient::RenderViewCreated(
   page_click_tracker->AddListener(password_autofill_manager);
   page_click_tracker->AddListener(autofill_agent);
 
-  TranslateHelper* translate = new TranslateHelper(render_view);
   new ChromeRenderViewObserver(
       render_view, content_settings, chrome_observer_.get(),
-      extension_dispatcher_.get(), translate);
+      extension_dispatcher_.get());
 
 #if defined(ENABLE_PLUGINS)
   new PepperHelper(render_view);
