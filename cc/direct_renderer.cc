3882c4b80a7c9cee2f93ad21f974b4f2a8b59b08
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-22 20:17:36 +0000

    Enable UMA reporting for CC histograms
    Some histograms lost their UMA reporting hooks recently.  This
    patch re-enables UMA reporting for those histograms.
    
    BUG=164518
    
    
    Review URL: https://chromiumcodereview.appspot.com/11820012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178079 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/direct_renderer.cc
+++ cc/direct_renderer.cc
@@ -162,7 +162,7 @@ void DirectRenderer::decideRenderPassAllocationsForFrame(const RenderPassList& r
 void DirectRenderer::drawFrame(RenderPassList& renderPassesInDrawOrder)
 {
     TRACE_EVENT0("cc", "DirectRenderer::drawFrame");
-    HISTOGRAM_COUNTS("Renderer4.renderPassCount", renderPassesInDrawOrder.size());
+    UMA_HISTOGRAM_COUNTS("Renderer4.renderPassCount", renderPassesInDrawOrder.size());
 
     const RenderPass* rootRenderPass = renderPassesInDrawOrder.back();
     DCHECK(rootRenderPass);
