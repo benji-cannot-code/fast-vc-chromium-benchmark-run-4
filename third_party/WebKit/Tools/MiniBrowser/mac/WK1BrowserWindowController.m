8e7ffa163879dbc5f304352a0f0f28237bde39b7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-22 07:39:52 +0000

    [Mac] WK1 MiniBrowser should clear delegates before releasing webview https://bugs.webkit.org/show_bug.cgi?id=107506
    Patch by Joseph Pecoraro <pecoraro@apple.com> on 2013-01-21
    Reviewed by Dan Bernstein.
    
    * MiniBrowser/mac/WK1BrowserWindowController.m:
    (-[WK1BrowserWindowController dealloc]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140388 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/MiniBrowser/mac/WK1BrowserWindowController.m
+++ third_party/WebKit/Tools/MiniBrowser/mac/WK1BrowserWindowController.m
@@ -49,6 +49,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)dealloc
 {
+    [_webView setFrameLoadDelegate:nil];
+    [_webView setUIDelegate:nil];
+    [_webView setResourceLoadDelegate:nil];
     [_webView release];
 
     [super dealloc];
