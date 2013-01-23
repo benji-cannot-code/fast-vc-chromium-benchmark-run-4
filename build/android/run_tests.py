24c4d010d8786cd3953743e0fa5419cce697f4bc
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 19:12:16 +0000

    [Android] Keep track of unknown test results at the TestRunner layer.
    If the entire gtest run crashes/failes/times out, the tests that
    were not run are marked as 'unknown'. These tests are reported
    by GetAllBroken() and are retried by BaseTestSharder.
    
    Get rid of overall_* flags which are now redundant.
    
    BUG=171466
    
    Review URL: https://codereview.chromium.org/11896050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178345 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/run_tests.py
+++ build/android/run_tests.py
@@ -197,7 +197,6 @@ class TestSharder(BaseTestSharder):
         test_type='Unit test',
         test_package=test_runners[0].test_package.test_suite_basename,
         build_type=self.build_type,
-        all_tests=self.all_tests,
         flakiness_server=self.flakiness_server)
     test_results.PrintAnnotation()
 
@@ -266,7 +265,7 @@ def _RunATestSuite(options):
   for buildbot_emulator in buildbot_emulators:
     buildbot_emulator.Shutdown()
 
-  return len(test_results.failed)
+  return len(test_results.GetAllBroken())
 
 
 def Dispatch(options):
