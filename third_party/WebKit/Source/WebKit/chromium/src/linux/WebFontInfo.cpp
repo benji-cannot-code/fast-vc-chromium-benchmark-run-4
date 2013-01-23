45a8514c05203da77bbb6c8b7e668d8e167bb35d
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-23 21:20:06 +0000

    [Chromium] Fix some #includes to headers in chromium/public/platform/ that have been moved to the new Platform directory https://bugs.webkit.org/show_bug.cgi?id=107722
    Reviewed by Darin Fisher.
    
    These headers just redirect to the new versions, so call the new
    versions directly.
    
    * src/linux/WebFontInfo.cpp:
    * src/mac/WebSubstringUtil.mm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/linux/WebFontInfo.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/linux/WebFontInfo.cpp
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFontInfo.h"
 
 #include "WebFontRenderStyle.h"
-#include "platform/linux/WebFontFamily.h"
 #include <fontconfig/fontconfig.h>
+#include <public/linux/WebFontFamily.h>
 #include <string.h>
 #include <unicode/utf16.h>
 
--- third_party/WebKit/Source/WebKit/chromium/src/mac/WebSubstringUtil.mm
+++ third_party/WebKit/Source/WebKit/chromium/src/mac/WebSubstringUtil.mm
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "RenderStyle.h"
 #include "TextIterator.h"
 #include "WebFrameImpl.h"
-#include "platform/WebRect.h"
+#include <public/WebRect.h>
 
 using namespace WebCore;
 
