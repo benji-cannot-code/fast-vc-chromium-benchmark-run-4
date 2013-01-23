754e12dba7b2d03617fe559bf4313f18abf193a6
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 05:37:00 +0000

    extensions: Remove unused browser_window_gtk.h include from omnibox_apitest.cc
    BUG=138280
    TBR=asargent@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12039031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178257 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/omnibox/omnibox_apitest.cc
+++ chrome/browser/extensions/api/omnibox/omnibox_apitest.cc
@@ -26,10 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_types.h"
 
-#if defined(TOOLKIT_GTK)
-#include "chrome/browser/ui/gtk/browser_window_gtk.h"
-#endif
-
 namespace {
 
 string16 AutocompleteResultAsString(const AutocompleteResult& result) {
