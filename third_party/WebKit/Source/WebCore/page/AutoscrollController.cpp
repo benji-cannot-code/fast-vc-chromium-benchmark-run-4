aa0727cb1e3bb3524376dc00ac17e145df9c1762
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-23 09:42:37 +0000

    REGRESSION(r139044): Pan icon remains on screen on Chromium-WinXP https://bugs.webkit.org/show_bug.cgi?id=107623
    Reviewed by Hajime Morita.
    
    Source/WebCore:
    
    The issue is caused by AutoscrollController::stopAutoscrollTimer()
    doesn't call View::removePanScrollIcon() because m_autoscrollType
    is reset before checking it.
    
    This patch changes to reset m_autoscrollType after all references.
    
    Test: platform/chromium-win/fast/events/panScroll-panIcon.html
    
    * page/AutoscrollController.cpp:
    (WebCore::AutoscrollController::stopAutoscrollTimer):
    
    LayoutTests:
    
    This test checks no pan icon after pan scroll.
    
    * platform/chromium-win/fast/events/panScroll-panIcon-expected.png: Added.
    * platform/chromium-win/fast/events/panScroll-panIcon-expected.txt: Added.
    * platform/chromium-win/fast/events/panScroll-panIcon.html: Added.
    * platform/chromium/TestExpectations: Changed to skip entry for panScroll-panIcon.html for Android, Linux and Mac.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140525 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/AutoscrollController.cpp
+++ third_party/WebKit/Source/WebCore/page/AutoscrollController.cpp
@@ -91,7 +91,6 @@ void AutoscrollController::stopAutoscrollTimer(bool rendererIsBeingDestroyed)
 {
     RenderBox* scrollable = m_autoscrollRenderer;
     m_autoscrollTimer.stop();
-    m_autoscrollType = NoAutoscroll;
     m_autoscrollRenderer = 0;
 
     if (!scrollable)
@@ -115,6 +114,8 @@ void AutoscrollController::stopAutoscrollTimer(bool rendererIsBeingDestroyed)
         }
     }
 #endif
+
+    m_autoscrollType = NoAutoscroll;
 
 #if ENABLE(PAN_SCROLLING)
     // If we're not in the top frame we notify it that we are not doing a panScroll any more.
