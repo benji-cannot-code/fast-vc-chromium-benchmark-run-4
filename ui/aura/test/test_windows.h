1daea2699c61083b03a875c2c502ca3eecdc9c45
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 12:02:39 +0000

    Revert 178251 - Suspected for CrOS ASAN break
    > Situation: There are 5 ways of knowing that the window hierarchy changed.
    > This is ridiculous!
    > We need to develop one universal standard that covers everyone's use cases.
    > Yeah!
    > ...
    > Situation: There are 6 ways of knowing that the window hierarchy changed.
    > 
    > (source: http://xkcd.com/927/)
    > 
    > Creates a single hierarchy change notification capable of expressing everything all our clients currently need and more (needed for FocusController).
    > This adds the notification and the unit tests, I will work on replacing the existing users in a future CL.
    > 
    > BUG=none
    > R=sky@chromium.org
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12042034
    
    TBR=ben@chromium.org
    Review URL: https://codereview.chromium.org/12040038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178280 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/test_windows.h
+++ ui/aura/test/test_windows.h
@@ -17,6 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace aura {
 namespace test {
 
+// These functions expect a non-NULL parent. If you want to let the
+// StackingClient determine where to place the windows, there are equivalent
+// methods in AshTestBase.
 Window* CreateTestWindowWithId(int id, Window* parent);
 Window* CreateTestWindowWithBounds(const gfx::Rect& bounds, Window* parent);
 Window* CreateTestWindow(SkColor color,
