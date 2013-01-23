71d1b8fb696b287e87a72149aaca30922687daf0
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 03:22:15 +0000

    cc: Reduce renderer_main thread priority when prefer_smoothness is set
    This tracks how many compositors prefer smooth frame-rate,
    and throttles/resets the main thread's priority as
    appropriate. Compositors in the same process share the same
    thread, so we track this count in a static int.
    
    All thread priorities use #ifdefs for reduced risk and
    easier merging. This needs to be abstracted and enabled
    on other platforms (http://crbug.com/170549)
    
    BUG=164651
    
    Review URL: https://chromiumcodereview.appspot.com/12026004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178225 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/compositor_output_surface.h
+++ content/renderer/gpu/compositor_output_surface.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/threading/non_thread_safe.h"
+#include "base/threading/platform_thread.h"
 #include "base/time.h"
 #include "cc/output_surface.h"
 #include "cc/software_output_device.h"
@@ -47,6 +48,10 @@ class CompositorOutputSurface
   virtual cc::SoftwareOutputDevice* SoftwareDevice() const OVERRIDE;
   virtual void SendFrameToParentCompositor(cc::CompositorFrame*) OVERRIDE;
 
+  // TODO(epenner): This seems out of place here and would be a better fit
+  // int CompositorThread after it is fully refactored (http://crbug/170828)
+  virtual void UpdateSmoothnessTakesPriority(bool prefer_smoothness) OVERRIDE;
+
  private:
   class CompositorOutputSurfaceProxy :
       public base::RefCountedThreadSafe<CompositorOutputSurfaceProxy> {
@@ -80,6 +85,8 @@ class CompositorOutputSurface
   struct Capabilities capabilities_;
   scoped_ptr<WebKit::WebGraphicsContext3D> context3D_;
   scoped_ptr<cc::SoftwareOutputDevice> software_device_;
+  bool prefers_smoothness_;
+  base::PlatformThreadId main_thread_id_;
 };
 
 }  // namespace content
