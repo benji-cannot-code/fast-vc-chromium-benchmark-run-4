b3c3a383827c9d40af21e589d1f9e99374e2df7e
leandrogracia@chromium.org <leandrogracia@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-23 02:25:05 +0000

    [Chromium] Expose didCommitCompositorFrame in WebWidgetClient. https://bugs.webkit.org/show_bug.cgi?id=107325
    Reviewed by James Robinson.
    
    Add a notification on WebWidgetClient called when the compositor commits a frame.
    
    * public/WebWidgetClient.h:
    (WebWidgetClient):
    (WebKit::WebWidgetClient::didCommitCompositorFrame):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::didCommit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -4163,8 +4163,10 @@ void WebViewImpl::willCommit()
 
 void WebViewImpl::didCommit()
 {
-    if (m_client)
+    if (m_client) {
+        m_client->didCommitCompositorFrame();
         m_client->didBecomeReadyForAdditionalInput();
+    }
 }
 
 void WebViewImpl::didCommitAndDrawFrame()
