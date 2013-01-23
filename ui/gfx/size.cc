8dc6e5b70d13e5bd5ba670d44c7a2f7375240bb9
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 03:51:36 +0000

    ui: Prevent negative sizes.
    Negative sizes are clamped to be 0, but keeping the setter methods inline, so without DCHECKs.
    
    TBR=sky
    BUG=160158
    Relanding https://codereview.chromium.org/11365160/ in pieces.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12042029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178237 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/size.cc
+++ ui/gfx/size.cc
@@ -9,18 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #endif
 
-#include "base/logging.h"
 #include "base/stringprintf.h"
-#include "ui/gfx/size_base.h"
 
 namespace gfx {
 
 template class SizeBase<Size, int>;
 
 #if defined(OS_MACOSX)
-Size::Size(const CGSize& s) : SizeBase<Size, int>(0, 0) {
-  set_width(s.width);
-  set_height(s.height);
+Size::Size(const CGSize& s)
+    : SizeBase<Size, int>(s.width, s.height) {
 }
 
 Size& Size::operator=(const CGSize& s) {
