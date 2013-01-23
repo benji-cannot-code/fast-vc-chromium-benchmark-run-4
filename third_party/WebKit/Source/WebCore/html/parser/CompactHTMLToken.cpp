0312cd912e1d5058db44a403b4e82ce8ae9cad96
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-23 20:36:01 +0000

    HTMLCompactToken needs to include the forceQuirks bool from HTMLToken https://bugs.webkit.org/show_bug.cgi?id=107713
    Reviewed by Tony Gentilcore.
    
    Before we were only getting "quirks mode" when the system/public identifiers
    were known quirks identifiers.  Now we'll correctly get quirks mode for any
    parse error during DOCTYPE parsing.
    This passes a bunch more tests.
    
    * html/parser/CompactHTMLToken.cpp:
    (WebCore::CompactHTMLToken::CompactHTMLToken):
    * html/parser/CompactHTMLToken.h:
    (WebCore::CompactHTMLToken::doctypeForcesQuirks):
    (CompactHTMLToken):
    * html/parser/HTMLToken.h:
    (WebCore::AtomicHTMLToken::AtomicHTMLToken):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140573 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/CompactHTMLToken.cpp
+++ third_party/WebKit/Source/WebCore/html/parser/CompactHTMLToken.cpp
@@ -46,6 +46,7 @@ COMPILE_ASSERT(sizeof(CompactHTMLToken) == sizeof(SameSizeAsCompactHTMLToken), C
 CompactHTMLToken::CompactHTMLToken(const HTMLToken& token, const TextPosition& textPosition)
     : m_type(token.type())
     , m_isAll8BitData(false)
+    , m_doctypeForcesQuirks(false)
     , m_textPosition(textPosition)
 {
     switch (m_type) {
@@ -59,6 +60,7 @@ CompactHTMLToken::CompactHTMLToken(const HTMLToken& token, const TextPosition& t
         String publicIdentifier(token.publicIdentifier().data(), token.publicIdentifier().size());
         String systemIdentifier(token.systemIdentifier().data(), token.systemIdentifier().size());
         m_attributes.append(CompactAttribute(publicIdentifier, systemIdentifier));
+        m_doctypeForcesQuirks = token.forceQuirks();
         break;
     }
     case HTMLTokenTypes::EndOfFile:
