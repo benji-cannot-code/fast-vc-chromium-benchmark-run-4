289250b32b45fdf6f38d0e2f3c9050480adffb88
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-23 22:58:20 +0000

    IndexedDB: Remove IDBVersionChangeRequest https://bugs.webkit.org/show_bug.cgi?id=107711
    Reviewed by Tony Chang.
    
    Source/WebCore:
    
    When the setVersion() API was removed from the Indexed DB spec the IDBVersionChangeRequest
    interface was replaced with IDBOpenDBRequest. We switched over for open(), this completes
    the work by switching over for deleteDatabase() and removing the old code. (On the Event
    side we still need to combine IDBVersionChangeEvent and IDBUpgradeNeededEvent.)
    
    Test: storage/indexeddb/intversion-long-queue.html
          storage/indexeddb/intversion-upgrades.html
    
    * CMakeLists.txt: Remove references to deleted code.
    * DerivedSources.make: Ditto.
    * GNUmakefile.list.am: Ditto.
    * Modules/indexeddb/IDBCallbacks.h: Remove unused onBlocked() overload.
    * Modules/indexeddb/IDBDatabase.cpp: Remove references to deleted code.
    * Modules/indexeddb/IDBDatabase.h: Ditto.
    * Modules/indexeddb/IDBDatabaseBackendImpl.cpp:
    (WebCore::IDBDatabaseBackendImpl::deleteDatabase): Fire onBlocked with current version.
    * Modules/indexeddb/IDBFactory.cpp:
    (WebCore::IDBFactory::openInternal): Don't need to specify source.
    (WebCore::IDBFactory::deleteDatabase): Use an IDBOpenDBRequest.
    * Modules/indexeddb/IDBFactory.h: Change return type of deleteDatabase()
    * Modules/indexeddb/IDBFactory.idl: Ditto.
    * Modules/indexeddb/IDBOpenDBRequest.cpp:
    (WebCore::IDBOpenDBRequest::create): Always use a null source.
    (WebCore::IDBOpenDBRequest::IDBOpenDBRequest): Pass null source to base class.
    (WebCore::IDBOpenDBRequest::dispatchEvent): Don't assume result is a database in existing
    special case.
    * Modules/indexeddb/IDBOpenDBRequest.h: Don't need a source argument (always null).
    * Modules/indexeddb/IDBVersionChangeRequest.cpp: Removed.
    * Modules/indexeddb/IDBVersionChangeRequest.h: Removed.
    * Modules/indexeddb/IDBVersionChangeRequest.idl: Removed.
    * WebCore.gypi: Remove references to deleted code.
    * WebCore.xcodeproj/project.pbxproj: Remove references to deleted code.
    * dom/EventTarget.h: Remove references to deleted code.
    * dom/EventTargetFactory.in: Ditto.
    
    Source/WebKit/chromium:
    
    * public/WebIDBCallbacks.h: Note to delete onBlocked() once Chromium is cleaned up.
    * src/IDBCallbacksProxy.cpp: Remove unused onBlocked() overload.
    * src/IDBCallbacksProxy.h: Ditto.
    * src/WebIDBCallbacksImpl.cpp: Ditto.
    * src/WebIDBCallbacksImpl.h: Ditto.
    * tests/IDBAbortOnCorruptTest.cpp: Ditto.
    * tests/IDBDatabaseBackendTest.cpp: Ditto.
    
    LayoutTests:
    
    Update expectations - change a handful of FAIL to PASS because past-us were awesome.
    
    * storage/indexeddb/intversion-long-queue-expected.txt:
    * storage/indexeddb/intversion-upgrades-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140602 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBFactory.cpp
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBFactory.cpp
@@ -44,7 +44,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IDBKey.h"
 #include "IDBKeyRange.h"
 #include "IDBOpenDBRequest.h"
-#include "IDBVersionChangeRequest.h"
 #include "Page.h"
 #include "PageGroup.h"
 #include "SecurityOrigin.h"
@@ -130,7 +129,7 @@ PassRefPtr<IDBOpenDBRequest> IDBFactory::openInternal(ScriptExecutionContext* co
 
     RefPtr<IDBDatabaseCallbacksImpl> databaseCallbacks = IDBDatabaseCallbacksImpl::create();
     int64_t transactionId = IDBDatabase::nextTransactionId();
-    RefPtr<IDBOpenDBRequest> request = IDBOpenDBRequest::create(context, IDBAny::createNull(), databaseCallbacks, transactionId, version);
+    RefPtr<IDBOpenDBRequest> request = IDBOpenDBRequest::create(context, databaseCallbacks, transactionId, version);
     m_backend->open(name, version, transactionId, request, databaseCallbacks, context->securityOrigin(), context, getIndexedDBDatabasePath(context));
     return request;
 }
@@ -140,7 +139,7 @@ PassRefPtr<IDBOpenDBRequest> IDBFactory::open(ScriptExecutionContext* context, c
     return openInternal(context, name, IDBDatabaseMetadata::NoIntVersion, ec);
 }
 
-PassRefPtr<IDBVersionChangeRequest> IDBFactory::deleteDatabase(ScriptExecutionContext* context, const String& name, ExceptionCode& ec)
+PassRefPtr<IDBOpenDBRequest> IDBFactory::deleteDatabase(ScriptExecutionContext* context, const String& name, ExceptionCode& ec)
 {
     if (name.isNull()) {
         ec = TypeError;
@@ -149,7 +148,7 @@ PassRefPtr<IDBVersionChangeRequest> IDBFactory::deleteDatabase(ScriptExecutionCo
     if (!isContextValid(context))
         return 0;
 
-    RefPtr<IDBVersionChangeRequest> request = IDBVersionChangeRequest::create(context, IDBAny::createNull(), "");
+    RefPtr<IDBOpenDBRequest> request = IDBOpenDBRequest::create(context, 0, 0, 0);
     m_backend->deleteDatabase(name, request, context->securityOrigin(), context, getIndexedDBDatabasePath(context));
     return request;
 }
