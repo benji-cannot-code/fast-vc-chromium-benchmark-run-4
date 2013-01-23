beb6c5dd2842ee799bc8bee95ac86f318976db10
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-23 07:37:06 +0000

    [V8] Make an Isolate parameter mandatory in throwTypeError() https://bugs.webkit.org/show_bug.cgi?id=107632
    Reviewed by Adam Barth.
    
    No tests. No change in behavior.
    
    * bindings/scripts/CodeGeneratorV8.pm:
    (GenerateParametersCheck):
    (GenerateConstructorHeader):
    (JSValueToNative):
    * bindings/scripts/test/V8/V8TestEventConstructor.cpp:
    (WebCore::V8TestEventConstructor::constructorCallback):
    * bindings/scripts/test/V8/V8TestInterface.cpp:
    (WebCore::V8TestInterface::constructorCallback):
    * bindings/scripts/test/V8/V8TestNamedConstructor.cpp:
    (WebCore::V8TestNamedConstructorConstructorCallback):
    * bindings/scripts/test/V8/V8TestNode.cpp:
    (WebCore::V8TestNode::constructorCallback):
    * bindings/scripts/test/V8/V8TestObj.cpp:
    (WebCore::TestObjV8Internal::methodWithSequenceArgCallback):
    (WebCore::V8TestObj::constructorCallback):
    * bindings/scripts/test/V8/V8TestOverloadedConstructors.cpp:
    (WebCore::V8TestOverloadedConstructors::constructorCallback):
    * bindings/scripts/test/V8/V8TestSerializedScriptValueInterface.cpp:
    (WebCore::TestSerializedScriptValueInterfaceV8Internal::acceptTransferListCallback):
    (WebCore::TestSerializedScriptValueInterfaceV8Internal::multiTransferListCallback):
    (WebCore::V8TestSerializedScriptValueInterface::constructorCallback):
    * bindings/v8/V8Binding.h:
    (WebCore):
    (WebCore::toRefPtrNativeArray):
    (WebCore::toV8Sequence):
    * bindings/v8/V8NodeFilterCondition.cpp:
    (WebCore::V8NodeFilterCondition::acceptNode):
    * bindings/v8/V8Utilities.cpp:
    (WebCore::extractTransferables):
    (WebCore::getMessagePortArray):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140514 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1757,7 +1757,7 @@ sub GenerateParametersCheck
                 $parameterCheckString .= "    ArrayBufferArray arrayBufferArray$TransferListName;\n";
                 $parameterCheckString .= "    if (args.Length() > $transferListIndex) {\n";
                 $parameterCheckString .= "        if (!extractTransferables(args[$transferListIndex], messagePortArray$TransferListName, arrayBufferArray$TransferListName, args.GetIsolate()))\n";
-                $parameterCheckString .= "            return throwTypeError(\"Could not extract transferables\");\n";
+                $parameterCheckString .= "            return throwTypeError(\"Could not extract transferables\", args.GetIsolate());\n";
                 $parameterCheckString .= "    }\n";
                 $useTransferList = 1;
             }
@@ -2194,7 +2194,7 @@ sub GenerateConstructorHeader
 {
     my $content = <<END;
     if (!args.IsConstructCall())
-        return throwTypeError("DOM object constructor cannot be called as a function.");
+        return throwTypeError("DOM object constructor cannot be called as a function.", args.GetIsolate());
 
     if (ConstructorMode::current() == ConstructorMode::WrapExistingObject)
         return args.Holder();
@@ -3814,7 +3814,7 @@ sub JSValueToNative
     if ($arrayOrSequenceType) {
         if ($codeGenerator->IsRefPtrType($arrayOrSequenceType)) {
             AddToImplIncludes("V8${arrayOrSequenceType}.h");
-            return "(toRefPtrNativeArray<${arrayOrSequenceType}, V8${arrayOrSequenceType}>($value))";
+            return "(toRefPtrNativeArray<${arrayOrSequenceType}, V8${arrayOrSequenceType}>($value, $getIsolate))";
         }
         return "toNativeArray<" . GetNativeType($arrayOrSequenceType) . ">($value)";
     }
