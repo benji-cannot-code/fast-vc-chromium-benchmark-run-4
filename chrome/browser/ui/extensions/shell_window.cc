7ac38fe77306528e1345c2c419280c2a0bef744a
rpaquay@chromium.org <rpaquay@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-23 00:06:53 +0000

    Use ImageLoader instead of ImageLoadingTracker (Part 10)
    BUG=163929
    
    
    Review URL: https://chromiumcodereview.appspot.com/11889022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/extensions/shell_window.cc
+++ chrome/browser/ui/extensions/shell_window.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_process_manager.h"
 #include "chrome/browser/extensions/extension_system.h"
+#include "chrome/browser/extensions/image_loader.h"
 #include "chrome/browser/extensions/shell_window_geometry_cache.h"
 #include "chrome/browser/extensions/shell_window_registry.h"
 #include "chrome/browser/extensions/suggest_permission_util.h"
@@ -118,7 +119,8 @@ ShellWindow::ShellWindow(Profile* profile,
       window_type_(WINDOW_TYPE_DEFAULT),
       ALLOW_THIS_IN_INITIALIZER_LIST(
           extension_function_dispatcher_(profile, this)),
-      ALLOW_THIS_IN_INITIALIZER_LIST(weak_ptr_factory_(this)) {
+      ALLOW_THIS_IN_INITIALIZER_LIST(weak_ptr_factory_(this)),
+      ALLOW_THIS_IN_INITIALIZER_LIST(image_loader_ptr_factory_(this)) {
 }
 
 void ShellWindow::Init(const GURL& url,
@@ -441,11 +443,8 @@ void ShellWindow::UpdateDraggableRegions(
   native_app_window_->UpdateDraggableRegions(regions);
 }
 
-void ShellWindow::OnImageLoaded(const gfx::Image& image,
-                                const std::string& extension_id,
-                                int index) {
+void ShellWindow::OnImageLoaded(const gfx::Image& image) {
   UpdateAppIcon(image);
-  app_icon_loader_.reset();
 }
 
 void ShellWindow::DidDownloadFavicon(int id,
@@ -476,13 +475,18 @@ void ShellWindow::UpdateAppIcon(const gfx::Image& image) {
 }
 
 void ShellWindow::UpdateExtensionAppIcon() {
-  app_icon_loader_.reset(new ImageLoadingTracker(this));
-  app_icon_loader_->LoadImage(
+  // Ensure previously enqueued callbacks are ignored.
+  image_loader_ptr_factory_.InvalidateWeakPtrs();
+
+  // Enqueue OnImageLoaded callback.
+  extensions::ImageLoader* loader = extensions::ImageLoader::Get(profile());
+  loader->LoadImageAsync(
       extension(),
       extension()->GetIconResource(kPreferredIconSize,
                                    ExtensionIconSet::MATCH_BIGGER),
       gfx::Size(kPreferredIconSize, kPreferredIconSize),
-      ImageLoadingTracker::CACHE);
+      base::Bind(&ShellWindow::OnImageLoaded,
+                 image_loader_ptr_factory_.GetWeakPtr()));
 }
 
 void ShellWindow::CloseContents(WebContents* contents) {
--- chrome/browser/ui/extensions/shell_window.h
+++ chrome/browser/ui/extensions/shell_window.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/extensions/extension_function_dispatcher.h"
 #include "chrome/browser/extensions/extension_keybinding_registry.h"
-#include "chrome/browser/extensions/image_loading_tracker.h"
 #include "chrome/browser/sessions/session_id.h"
 #include "chrome/browser/ui/base_window.h"
 #include "content/public/browser/notification_observer.h"
@@ -42,7 +41,6 @@ class ShellWindow : public content::NotificationObserver,
                     public content::WebContentsDelegate,
                     public content::WebContentsObserver,
                     public ExtensionFunctionDispatcher::Delegate,
-                    public ImageLoadingTracker::Observer,
                     public extensions::ExtensionKeybindingRegistry::Delegate {
  public:
   enum WindowType {
@@ -203,10 +201,7 @@ class ShellWindow : public content::NotificationObserver,
   // Load the app's image, firing a load state change when loaded.
   void UpdateExtensionAppIcon();
 
-  // ImageLoadingTracker::Observer implementation.
-  virtual void OnImageLoaded(const gfx::Image& image,
-                             const std::string& extension_id,
-                             int index) OVERRIDE;
+  void OnImageLoaded(const gfx::Image& image);
 
   // extensions::ExtensionKeybindingRegistry::Delegate implementation.
   virtual extensions::ActiveTabPermissionGranter*
@@ -238,9 +233,6 @@ class ShellWindow : public content::NotificationObserver,
   // Icon shown in the task bar.
   gfx::Image app_icon_;
 
-  // Used for loading app_icon_ from the extension.
-  scoped_ptr<ImageLoadingTracker> app_icon_loader_;
-
   // Icon URL to be used for setting the app icon. If not empty, app_icon_ will
   // be fetched and set using this URL.
   GURL app_icon_url_;
@@ -248,6 +240,8 @@ class ShellWindow : public content::NotificationObserver,
   scoped_ptr<NativeAppWindow> native_app_window_;
 
   base::WeakPtrFactory<ShellWindow> weak_ptr_factory_;
+
+  base::WeakPtrFactory<ShellWindow> image_loader_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ShellWindow);
 };
