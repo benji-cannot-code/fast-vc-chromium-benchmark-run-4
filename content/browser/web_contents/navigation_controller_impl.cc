30673ddbd83cf31c6e0112b079293408d8481099
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 20:46:01 +0000

    overscroll: Enable overscroll navigation by default.
    BUG=160668, 171777, 168873
    TBR=joi@ for switch rename
    
    Review URL: https://codereview.chromium.org/12036074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178643 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/navigation_controller_impl.cc
+++ content/browser/web_contents/navigation_controller_impl.cc
@@ -480,8 +480,8 @@ int NavigationControllerImpl::GetIndexForOffset(int offset) const {
 }
 
 void NavigationControllerImpl::TakeScreenshot() {
-  static bool overscroll_enabled = CommandLine::ForCurrentProcess()->
-      HasSwitch(switches::kEnableOverscrollHistoryNavigation);
+  static bool overscroll_enabled = !CommandLine::ForCurrentProcess()->
+      HasSwitch(switches::kDisableOverscrollHistoryNavigation);
   if (!overscroll_enabled)
     return;
 
@@ -491,6 +491,10 @@ void NavigationControllerImpl::TakeScreenshot() {
     return;
 
   RenderViewHost* render_view_host = web_contents_->GetRenderViewHost();
+  if (!static_cast<RenderViewHostImpl*>
+      (render_view_host)->overscroll_controller()) {
+    return;
+  }
   content::RenderWidgetHostView* view = render_view_host->GetView();
   if (!view)
     return;
@@ -528,8 +532,6 @@ void NavigationControllerImpl::OnScreenshotTaken(
   }
 
   if (!success) {
-    LOG(ERROR) << "Taking snapshot was unsuccessful for "
-               << unique_id;
     ClearScreenshot(entry);
     return;
   }
