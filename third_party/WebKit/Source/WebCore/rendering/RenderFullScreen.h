667cc175976b11a2735d7f8ed8fdeef55b4caf06
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-24 20:16:39 +0000

    Convert RenderFullScreen to use the non-deprecated flexbox https://bugs.webkit.org/show_bug.cgi?id=107746
    Patch by Christian Biesinger <cbiesinger@chromium.org> on 2013-01-24
    Reviewed by Ojan Vafai.
    
    Tests: covered by existing tests in fullscreen/.
    
    * rendering/RenderFullScreen.h:
    * rendering/RenderFullScreen.cpp:
    (RenderFullScreen::RenderFullScreen):
    (RenderFullScreen::willBeDestroyed):
    Inherit from RenderFlexibleBox
    
    (createFullScreenStyle):
    Use the new-style CSS properties (justify-content, etc)
    
    * css/fullscreen.css:
    (video:-webkit-full-screen, audio:-webkit-full-screen):
    * css/fullscreenQuickTime.css:
    (video:-webkit-full-screen::-webkit-media-controls-panel):
    (video:-webkit-full-screen::-webkit-media-controls-seek-back-button):
    (video:-webkit-full-screen::-webkit-media-controls-return-to-realtime-button):
    (video:-webkit-full-screen::-webkit-media-controls-seek-forward-button):
    * css/mediaControlsBlackBerryFullscreen.css:
    (video:-webkit-full-screen::-webkit-media-controls-panel):
    (video:-webkit-full-screen::-webkit-media-controls-button-group-container):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-time-display-container):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-play-button):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-timeline-container):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-current-time-display):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-time-remaining-display):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-timeline):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-fullscreen-button):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-button-divider):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-button-container, video:-webkit-full-screen::-webkit-media-controls-play-button-container):
    (video:-webkit-full-screen::-webkit-media-controls-fullscreen-button-container):
    (video:-webkit-full-screen::-webkit-media-controls-play-button-container):
    (video:-webkit-full-screen::-webkit-media-controls-placeholder):
    * css/mediaControlsQtFullscreen.css:
    (video::-webkit-media-controls-panel):
    (video::-webkit-media-controls-play-button):
    Update fullscreen-related CSS to use the new flexbox
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140705 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFullScreen.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderFullScreen.h
@@ -28,12 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(FULLSCREEN_API)
 
-#include "RenderDeprecatedFlexibleBox.h"
+#include "RenderFlexibleBox.h"
 #include "StyleInheritedData.h"
 
 namespace WebCore {
 
-class RenderFullScreen : public RenderDeprecatedFlexibleBox {
+class RenderFullScreen : public RenderFlexibleBox {
 public:
     static RenderFullScreen* createAnonymous(Document*);
 
