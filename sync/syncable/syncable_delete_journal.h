707f83be9746354cfc8c14f405d0d2437d68a2c0
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 20:38:17 +0000

    [sync] Componentize sync: Part 7: Eliminate more filename collisions in sync.gyp
    In order to move src/sync into its own compoent, all the static_library
    targets in sync.gyp need to be converted to targets of type 'none' and
    rolled into one sync target of type component. This is currently blocked
    by duplicate .cc filenames in syncable and internal_api, which flags an
    error while running gyp. The offending files are
    {internal_api|syncable}/delete_journal.cc.
    
    This patch renames syncable/delete_journal.{h|cc} to
    syncable/syncable_delete_journal.{h|cc}.
    
    This is a continuation of https://codereview.chromium.org/11638018.
    
    BUG=136928
    
    Review URL: https://codereview.chromium.org/12033084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178639 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/syncable/syncable_delete_journal.h
+++ sync/syncable/syncable_delete_journal.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef SYNC_SYNCABLE_DELETE_JOURNAL_H_
-#define SYNC_SYNCABLE_DELETE_JOURNAL_H_
+#ifndef SYNC_SYNCABLE_SYNCABLE_DELETE_JOURNAL_H_
+#define SYNC_SYNCABLE_SYNCABLE_DELETE_JOURNAL_H_
 
 #include <set>
 
@@ -101,4 +101,4 @@ class DeleteJournal {
 }  // namespace syncable
 }  // namespace syncer
 
-#endif  // SYNC_SYNCABLE_DELETE_JOURNAL_H_
+#endif  // SYNC_SYNCABLE_SYNCABLE_DELETE_JOURNAL_H_
