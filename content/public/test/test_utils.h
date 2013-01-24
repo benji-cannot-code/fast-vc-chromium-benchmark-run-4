549ff13c12b39c25a116cd977c94ab7f1af40078
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 21:54:00 +0000

    content: remove NOTIFICATION_SAVE_PACKAGE_SUCCESSFULLY_FINISHED
    BUG=170921
    
    Review URL: https://codereview.chromium.org/11867023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178661 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/test_utils.h
+++ content/public/test/test_utils.h
@@ -64,7 +64,8 @@ class MessageLoopRunner : public base::RefCounted<MessageLoopRunner> {
  public:
   MessageLoopRunner();
 
-  // Run the current MessageLoop.
+  // Run the current MessageLoop unless the quit closure
+  // has already been called.
   void Run();
 
   // Quit the matching call to Run (nested MessageLoops are unaffected).
@@ -80,6 +81,12 @@ class MessageLoopRunner : public base::RefCounted<MessageLoopRunner> {
  private:
   friend class base::RefCounted<MessageLoopRunner>;
   ~MessageLoopRunner();
+
+  // True when the message loop is running.
+  bool loop_running_;
+
+  // True after closure returned by |QuitClosure| has been called.
+  bool quit_closure_called_;
 
   base::RunLoop run_loop_;
 
