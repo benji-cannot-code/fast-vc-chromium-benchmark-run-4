a9a88097189036f378cc5787ac70a7d07c387162
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 16:16:04 +0000

    Locally managed users * Allow locking screen for locally managed users * Add flag
    BUG=171235,167661
    
    Review URL: https://codereview.chromium.org/12034009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178594 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/user.cc
+++ chrome/browser/chromeos/login/user.cc
@@ -69,6 +69,7 @@ class LocallyManagedUser : public User {
 
   // Overridden from User:
   virtual UserType GetType() const OVERRIDE;
+  virtual bool can_lock() const OVERRIDE;
   virtual std::string display_email() const OVERRIDE;
 
  private:
@@ -208,6 +209,10 @@ LocallyManagedUser::~LocallyManagedUser() {}
 
 User::UserType LocallyManagedUser::GetType() const {
   return USER_TYPE_LOCALLY_MANAGED;
+}
+
+bool LocallyManagedUser::can_lock() const {
+  return true;
 }
 
 std::string LocallyManagedUser::display_email() const {
