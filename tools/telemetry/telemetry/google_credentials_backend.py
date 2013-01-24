4d16297e4e8c9dae5bf62b6575255d68b55e7f17
hartmanng@chromium.org <hartmanng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 18:10:38 +0000

    Fix facebook_credentials_backend_unittest on android-content-shell.
    NOTRY=true
    BUG=170901
    
    
    Review URL: https://chromiumcodereview.appspot.com/11943014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178609 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/google_credentials_backend.py
+++ tools/telemetry/telemetry/google_credentials_backend.py
@@ -7,6 +7,10 @@ from telemetry import form_based_credentials_backend
 
 class GoogleCredentialsBackend(
     form_based_credentials_backend.FormBasedCredentialsBackend):
+  def IsAlreadyLoggedIn(self, tab):
+    return tab.EvaluateJavaScript(
+        'document.getElementById("gb")!== null')
+
   @property
   def credentials_type(self):
     return 'google'
@@ -18,10 +22,6 @@ class GoogleCredentialsBackend(
   @property
   def login_form_id(self):
     return 'gaia_loginform'
-
-  @property
-  def already_logged_in_element_id(self):
-    return 'gb'
 
   @property
   def login_input_id(self):
