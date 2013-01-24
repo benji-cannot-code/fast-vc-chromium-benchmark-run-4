cfedaa261f63c12b92b12d42f154f899031576ea
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 08:16:21 +0000

    Add debug code to figure out where we forget to delete TaskManagerRendererResources
    BUG=165138
    
    Review URL: https://codereview.chromium.org/11953049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178551 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager_resource_providers.h
+++ chrome/browser/task_manager/task_manager_resource_providers.h
@@ -11,11 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/debug/stack_trace.h"
 #include "base/process_util.h"
 #include "chrome/browser/task_manager/task_manager.h"
 #include "content/public/browser/child_process_data.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/render_view_host_observer.h"
 #include "content/public/common/process_type.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCache.h"
 
@@ -37,7 +39,8 @@ class Extension;
 
 // Base class for various types of render process resources that provides common
 // functionality like stats tracking.
-class TaskManagerRendererResource : public TaskManager::Resource {
+class TaskManagerRendererResource : public TaskManager::Resource,
+                                    public content::RenderViewHostObserver {
  public:
   TaskManagerRendererResource(base::ProcessHandle process,
                               content::RenderViewHost* render_view_host);
@@ -76,6 +79,10 @@ class TaskManagerRendererResource : public TaskManager::Resource {
   virtual void NotifyV8HeapStats(size_t v8_memory_allocated,
                                  size_t v8_memory_used) OVERRIDE;
 
+  // content::RenderViewHostObserver implementation.
+  virtual void RenderViewHostDestroyed(
+      content::RenderViewHost* render_view_host) OVERRIDE;
+
   content::RenderViewHost* render_view_host() const {
     return render_view_host_;
   }
@@ -102,6 +109,10 @@ class TaskManagerRendererResource : public TaskManager::Resource {
   size_t v8_memory_allocated_;
   size_t v8_memory_used_;
   bool pending_v8_memory_allocated_update_;
+
+  // TODO(jochen): Remove asap after debugging http://crbug.com/165138.
+  base::debug::StackTrace creation_stack_;
+  scoped_ptr<base::debug::StackTrace> destruction_stack_;
 
   DISALLOW_COPY_AND_ASSIGN(TaskManagerRendererResource);
 };
