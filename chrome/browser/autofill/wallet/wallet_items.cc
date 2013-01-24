c4dd983d1ff69598113833e173291a66649c0a12
ahutter@chromium.org <ahutter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 01:32:28 +0000

    GetWalletItems can return required actions without a transaction id.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12049041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/wallet/wallet_items.cc
+++ chrome/browser/autofill/wallet/wallet_items.cc
@@ -259,12 +259,6 @@ WalletItems::~WalletItems() {}
 
 scoped_ptr<WalletItems>
     WalletItems::CreateWalletItems(const base::DictionaryValue& dictionary) {
-  std::string google_transaction_id;
-  if (!dictionary.GetString("google_transaction_id", &google_transaction_id)) {
-    DLOG(ERROR) << "Response from Google wallet missing google transaction id";
-    return scoped_ptr<WalletItems>();
-  }
-
   std::vector<RequiredAction> required_action;
   const ListValue* required_action_list;
   if (dictionary.GetList("required_action", &required_action_list)) {
@@ -282,6 +276,13 @@ scoped_ptr<WalletItems>
     }
   } else {
     DVLOG(1) << "Response from Google wallet missing required actions";
+  }
+
+  std::string google_transaction_id;
+  if (!dictionary.GetString("google_transaction_id", &google_transaction_id) &&
+      required_action.empty()) {
+    DLOG(ERROR) << "Response from Google wallet missing google transaction id";
+    return scoped_ptr<WalletItems>();
   }
 
   std::string default_instrument_id;
