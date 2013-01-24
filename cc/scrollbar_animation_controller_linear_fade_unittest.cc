e033978ae1f790d09dd2ef34b2d1dd3fd8676d72
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 03:44:33 +0000

    Fix scrollbar geometry in impl-side painting
    BUG=169143
    
    
    Review URL: https://chromiumcodereview.appspot.com/12051028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178502 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scrollbar_animation_controller_linear_fade_unittest.cc
+++ cc/scrollbar_animation_controller_linear_fade_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/single_thread_proxy.h"
 #include "cc/test/fake_impl_proxy.h"
 #include "cc/test/fake_layer_tree_host_impl.h"
+#include "cc/test/fake_web_scrollbar_theme_geometry.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace cc {
@@ -25,7 +26,8 @@ protected:
     virtual void SetUp()
     {
         m_scrollLayer = LayerImpl::create(m_hostImpl.activeTree(), 1);
-        m_scrollbarLayer = ScrollbarLayerImpl::create(m_hostImpl.activeTree(), 2);
+        scoped_ptr<ScrollbarGeometryFixedThumb> geometry(ScrollbarGeometryFixedThumb::create(FakeWebScrollbarThemeGeometry::create(false)));
+        m_scrollbarLayer = ScrollbarLayerImpl::create(m_hostImpl.activeTree(), 2, geometry.Pass());
 
         m_scrollLayer->setMaxScrollOffset(gfx::Vector2d(50, 50));
         m_scrollLayer->setBounds(gfx::Size(50, 50));
