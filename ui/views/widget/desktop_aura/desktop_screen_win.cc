73dca23e20d71ea8a86e4c8b0bf00029de5eff06
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 04:50:02 +0000

    Gets sibling menus to work correctly in win-aura. GetNativeWindowFromHWND wasn't wired through correctly.
    BUG=none
    TEST=added coverage to interactive ui test.
    R=ben@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12065002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178518 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/desktop_aura/desktop_screen_win.cc
+++ ui/views/widget/desktop_aura/desktop_screen_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/root_window.h"
 #include "ui/aura/root_window_host.h"
 #include "ui/gfx/display.h"
+#include "ui/views/widget/desktop_aura/desktop_root_window_host_win.h"
 #include "ui/views/widget/desktop_aura/desktop_screen.h"
 
 namespace {
@@ -59,7 +60,7 @@ HWND DesktopScreenWin::GetHWNDFromNativeView(gfx::NativeView window) const {
 
 gfx::NativeWindow DesktopScreenWin::GetNativeWindowFromHWND(HWND hwnd) const {
   return (::IsWindow(hwnd)) ?
-      aura::RootWindow::GetForAcceleratedWidget(hwnd) : NULL;
+      DesktopRootWindowHostWin::GetContentWindowForHWND(hwnd) : NULL;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
