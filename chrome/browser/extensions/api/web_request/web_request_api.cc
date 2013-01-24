48c55e6389bda6b31807d60a117b6d3b3f7042ae
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 21:33:15 +0000

    Extensions that use WebRequest API should not interact with <webview>
    This patch exposes the flag that indicates whether a render process is a guest
    to the IO thread in the browser process. When the WebRequest API checks for
    matching listeners, it checks if the request is coming from a <webview> guest
    process and if so, it checks if the listeners have a target process/routing ID
    (no listeners currently do). Thus, the current WebRequest API no longer
    interacts with <webview>.
    
    Upcoming patches will enable the WebRequest API for guests via a slightly
    different API.
    
    BUG=171421
    Test=Manually
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=178528
    
    Review URL: https://chromiumcodereview.appspot.com/12052014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/web_request/web_request_api.cc
+++ chrome/browser/extensions/api/web_request/web_request_api.cc
@@ -160,6 +160,8 @@ void ExtractRequestInfoDetails(net::URLRequest* request,
                                int64* parent_frame_id,
                                int* tab_id,
                                int* window_id,
+                               int* render_process_host_id,
+                               int* routing_id,
                                ResourceType::Type* resource_type) {
   if (!request->GetUserData(NULL))
     return;
@@ -171,6 +173,8 @@ void ExtractRequestInfoDetails(net::URLRequest* request,
   *is_main_frame = info->IsMainFrame();
   *parent_frame_id = info->GetParentFrameID();
   *parent_is_main_frame = info->ParentIsMainFrame();
+  *render_process_host_id = info->GetChildID();
+  *routing_id = info->GetRouteID();
 
   // Restrict the resource type to the values we care about.
   if (helpers::IsRelevantResourceType(info->GetResourceType()))
@@ -191,10 +195,13 @@ void ExtractRequestInfo(net::URLRequest* request, DictionaryValue* out) {
   int parent_frame_id_for_extension = -1;
   int tab_id = -1;
   int window_id = -1;
+  int render_process_host_id = -1;
+  int routing_id = -1;
   ResourceType::Type resource_type = ResourceType::LAST_TYPE;
   ExtractRequestInfoDetails(request, &is_main_frame, &frame_id,
                             &parent_is_main_frame, &parent_frame_id, &tab_id,
-                            &window_id, &resource_type);
+                            &window_id, &render_process_host_id, &routing_id,
+                            &resource_type);
   frame_id_for_extension = GetFrameId(is_main_frame, frame_id);
   parent_frame_id_for_extension = GetFrameId(parent_is_main_frame,
                                              parent_frame_id);
@@ -375,6 +382,8 @@ struct ExtensionWebRequestEventRouter::EventListener {
   std::string sub_event_name;
   RequestFilter filter;
   int extra_info_spec;
+  int target_process_id;
+  int target_route_id;
   base::WeakPtr<IPC::Sender> ipc_sender;
   mutable std::set<uint64> blocked_requests;
 
@@ -1135,6 +1144,8 @@ bool ExtensionWebRequestEventRouter::AddEventListener(
     const std::string& sub_event_name,
     const RequestFilter& filter,
     int extra_info_spec,
+    int target_process_id,
+    int target_route_id,
     base::WeakPtr<IPC::Sender> ipc_sender) {
   if (!IsWebRequestEvent(event_name))
     return false;
@@ -1146,6 +1157,8 @@ bool ExtensionWebRequestEventRouter::AddEventListener(
   listener.filter = filter;
   listener.extra_info_spec = extra_info_spec;
   listener.ipc_sender = ipc_sender;
+  listener.target_process_id = target_process_id;
+  listener.target_route_id = target_route_id;
 
   if (listeners_[profile][event_name].count(listener) != 0u) {
     // This is likely an abuse of the API by a malicious extension.
@@ -1216,11 +1229,14 @@ bool ExtensionWebRequestEventRouter::IsPageLoad(
   int64 parent_frame_id = -1;
   int tab_id = -1;
   int window_id = -1;
+  int render_process_host_id = -1;
+  int routing_id = -1;
   ResourceType::Type resource_type = ResourceType::LAST_TYPE;
 
   ExtractRequestInfoDetails(request, &is_main_frame, &frame_id,
                             &parent_is_main_frame, &parent_frame_id,
-                            &tab_id, &window_id, &resource_type);
+                            &tab_id, &window_id, &render_process_host_id,
+                            &routing_id, &resource_type);
 
   return resource_type == ResourceType::MAIN_FRAME;
 }
@@ -1257,12 +1273,16 @@ void ExtensionWebRequestEventRouter::GetMatchingListenersImpl(
     const GURL& url,
     int tab_id,
     int window_id,
+    int render_process_host_id,
+    int routing_id,
     ResourceType::Type resource_type,
     bool is_async_request,
     bool is_request_from_extension,
     int* extra_info_spec,
     std::vector<const ExtensionWebRequestEventRouter::EventListener*>*
         matching_listeners) {
+  bool is_guest = ExtensionRendererState::GetInstance()->
+      IsGuestProcess(render_process_host_id);
   std::set<EventListener>& listeners = listeners_[profile][event_name];
   for (std::set<EventListener>::iterator it = listeners.begin();
        it != listeners.end(); ++it) {
@@ -1271,6 +1291,10 @@ void ExtensionWebRequestEventRouter::GetMatchingListenersImpl(
       // via a call to RemoveEventListener. For now, just skip it.
       continue;
     }
+
+    if (is_guest && (it->target_process_id != render_process_host_id||
+                     it->target_route_id != routing_id))
+      continue;
 
     if (!it->filter.urls.is_empty() && !it->filter.urls.MatchesURL(url))
       continue;
@@ -1325,12 +1349,15 @@ ExtensionWebRequestEventRouter::GetMatchingListeners(
   int64 parent_frame_id = -1;
   int tab_id = -1;
   int window_id = -1;
+  int render_process_host_id = -1;
+  int routing_id = -1;
   ResourceType::Type resource_type = ResourceType::LAST_TYPE;
   const GURL& url = request->url();
 
   ExtractRequestInfoDetails(request, &is_main_frame, &frame_id,
                             &parent_is_main_frame, &parent_frame_id,
-                            &tab_id, &window_id, &resource_type);
+                            &tab_id, &window_id, &render_process_host_id,
+                            &routing_id, &resource_type);
 
   std::vector<const ExtensionWebRequestEventRouter::EventListener*>
       matching_listeners;
@@ -1345,14 +1372,16 @@ ExtensionWebRequestEventRouter::GetMatchingListeners(
 
   GetMatchingListenersImpl(
       profile, extension_info_map, false, event_name, url,
-      tab_id, window_id, resource_type, is_async_request,
-      is_request_from_extension, extra_info_spec, &matching_listeners);
+      tab_id, window_id, render_process_host_id, routing_id, resource_type,
+      is_async_request, is_request_from_extension, extra_info_spec,
+      &matching_listeners);
   void* cross_profile = GetCrossProfile(profile);
   if (cross_profile) {
     GetMatchingListenersImpl(
         cross_profile, extension_info_map, true, event_name, url, tab_id,
-        window_id, resource_type, is_async_request, is_request_from_extension,
-        extra_info_spec, &matching_listeners);
+        window_id, render_process_host_id, routing_id, resource_type,
+        is_async_request, is_request_from_extension, extra_info_spec,
+        &matching_listeners);
   }
 
   return matching_listeners;
@@ -1778,7 +1807,6 @@ void ClearCacheQuotaHeuristic::OnPageLoad(Bucket* bucket) {
 
 bool WebRequestAddEventListener::RunImpl() {
   // Argument 0 is the callback, which we don't use here.
-
   ExtensionWebRequestEventRouter::RequestFilter filter;
   DictionaryValue* value = NULL;
   error_.clear();
@@ -1835,7 +1863,7 @@ bool WebRequestAddEventListener::RunImpl() {
       ExtensionWebRequestEventRouter::GetInstance()->AddEventListener(
           profile_id(), extension_id(), extension_name,
           event_name, sub_event_name, filter,
-          extra_info_spec, ipc_sender_weak());
+          extra_info_spec, -1, -1, ipc_sender_weak());
   EXTENSION_FUNCTION_VALIDATE(success);
 
   helpers::ClearCacheOnNavigation();
