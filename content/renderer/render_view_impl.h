da7e23c543981ad06e7323dae3004325c4e793c6
trchen@chromium.org <trchen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 00:36:56 +0000

    [Android] Should not update IME status when gesture needs to be disambiguated
    When a gesture needs to be disambiguated, WebKit doesn't update cursor focus.
    We added an extra status for didHandleGestureEvent(), so we can distinguish
    the case whether the event is actually delivered to the web page or cancelled.
    In the cancelled case we should not update the IME status.
    
    WebKit CL: https://webkit.org/b/104427
    
    BUG=163439
    
    Review URL: https://chromiumcodereview.appspot.com/11473027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178438 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.h
+++ content/renderer/render_view_impl.h
@@ -408,6 +408,8 @@ class CONTENT_EXPORT RenderViewImpl
   virtual void requestPointerUnlock();
   virtual bool isPointerLocked();
   virtual void didActivateCompositor(int input_handler_identifier);
+  virtual void didHandleGestureEvent(const WebKit::WebGestureEvent& event,
+                                     bool event_cancelled) OVERRIDE;
 
   // WebKit::WebViewClient implementation --------------------------------------
 
@@ -443,8 +445,6 @@ class CONTENT_EXPORT RenderViewImpl
   virtual void didChangeSelection(bool is_selection_empty);
   virtual void didExecuteCommand(const WebKit::WebString& command_name);
   virtual bool handleCurrentKeyboardEvent();
-  virtual void didHandleGestureEvent(const WebKit::WebGestureEvent& event,
-                                     bool event_swallowed) OVERRIDE;
   virtual WebKit::WebColorChooser* createColorChooser(
       WebKit::WebColorChooserClient*, const WebKit::WebColor& initial_color);
   virtual bool runFileChooser(
