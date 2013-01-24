13d122f2986ac7413ed23f4100e519bddb71b1a4
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 00:39:26 +0000

    [ChromeDriver] Add server which communicates over the HTTP json wire protocol.
    A fair amount of this is reused from the old chromedriver.
    Originally, it was planned to just have the shared library and submit a patch
    to the webdriver project to load it, but decided to go ahead and do this because it
    is quicker for now than to patch the webdriver project. May revisit this decision
    in the future.
    
    The server should require no updating for implementing new commands, unless
    they've just been added to the spec, in which case all is needed is a mapping from the URL
    to the command name.
    BUG=none
    TEST=ran with the webdriver python client
    
    Review URL: https://codereview.chromium.org/11867035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178442 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/chromedriver/util.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/test/chromedriver/util.h"
+
+#include "base/format_macros.h"
+#include "base/rand_util.h"
+#include "base/stringprintf.h"
+
+std::string GenerateId() {
+  uint64 msb = base::RandUint64();
+  uint64 lsb = base::RandUint64();
+  return base::StringPrintf("%016" PRIx64 "%016" PRIx64, msb, lsb);
+}
