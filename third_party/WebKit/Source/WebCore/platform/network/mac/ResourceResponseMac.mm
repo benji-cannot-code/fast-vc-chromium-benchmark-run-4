347a6380cbc6d4ee69057b82e82603a2ebc4b736
psolanki@apple.com <psolanki@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-24 23:28:07 +0000

    CertificateChain functions in ResourceResponse should be inside proper ifdefs https://bugs.webkit.org/show_bug.cgi?id=107865
    Reviewed by Brady Eidson.
    
    Move ResourceResponse::setCertificateChain() and ResourceResponse::certificateChain()
    be under the same ifdefs that they are declared in.
    
    No new tests because no change in functionality.
    
    * platform/network/mac/ResourceResponseMac.mm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/mac/ResourceResponseMac.mm
+++ third_party/WebKit/Source/WebCore/platform/network/mac/ResourceResponseMac.mm
@@ -172,6 +172,10 @@ bool ResourceResponse::platformCompare(const ResourceResponse& a, const Resource
     return a.nsURLResponse() == b.nsURLResponse();
 }
 
+#endif // USE(CFNETWORK)
+
+#if PLATFORM(MAC) || USE(CFNETWORK)
+
 void ResourceResponse::setCertificateChain(CFArrayRef certificateChain)
 {
     ASSERT(!wkCopyNSURLResponseCertificateChain(nsURLResponse()));
@@ -186,7 +190,7 @@ RetainPtr<CFArrayRef> ResourceResponse::certificateChain() const
     return adoptCF(wkCopyNSURLResponseCertificateChain(nsURLResponse()));
 }
 
-#endif // USE(CFNETWORK)
+#endif // PLATFORM(MAC) || USE(CFNETWORK)
 
 } // namespace WebCore
 
