24817d02603fa947980b1118cb9d6bfdd9d9ba0d
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 23:57:17 +0000

    Moved selection logic into PrepareFrameAndViewForPrint.
    BUG=83098
    
    Review URL: https://chromiumcodereview.appspot.com/12035017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178700 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/printing/print_web_view_helper.h
+++ chrome/renderer/printing/print_web_view_helper.h
@@ -16,10 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/renderer/render_view_observer_tracker.h"
 #include "printing/metafile_impl.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebCanvas.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebFrameClient.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebNode.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPrintParams.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebViewClient.h"
 #include "ui/gfx/size.h"
 
 struct PrintMsg_Print_Params;
@@ -45,18 +43,12 @@ class PrepareFrameAndViewForPrint;
 // of the document and creating a new WebView with the contents.
 class PrintWebViewHelper
     : public content::RenderViewObserver,
-      public content::RenderViewObserverTracker<PrintWebViewHelper>,
-      public WebKit::WebViewClient,
-      public WebKit::WebFrameClient {
+      public content::RenderViewObserverTracker<PrintWebViewHelper> {
  public:
   explicit PrintWebViewHelper(content::RenderView* render_view);
   virtual ~PrintWebViewHelper();
 
   void PrintNode(const WebKit::WebNode& node);
-
- protected:
-  // WebKit::WebViewClient override:
-  virtual void didStopLoading();
 
  private:
   friend class PrintWebViewHelperTestBase;
@@ -204,9 +196,7 @@ class PrintWebViewHelper
 
   // Page Printing / Rendering ------------------------------------------------
 
-  // Prints all the pages listed in |print_pages_params_|.
-  // It will implicitly revert the document to display CSS media type.
-  bool PrintPages(WebKit::WebFrame* frame, const WebKit::WebNode& node);
+  void PrintPages();
   bool PrintPagesNative(WebKit::WebFrame* frame,
                         const WebKit::WebNode& node,
                         int page_count,
@@ -260,8 +250,6 @@ class PrintWebViewHelper
                                  WebKit::WebCanvas* canvas);
 
   // Helper methods -----------------------------------------------------------
-
-  bool CopyAndPrint(WebKit::WebFrame* web_frame);
 
   bool CopyMetafileDataToSharedMem(Metafile* metafile,
                                    base::SharedMemoryHandle* shared_mem_handle);
@@ -324,7 +312,7 @@ class PrintWebViewHelper
   bool PreviewPageRendered(int page_number, Metafile* metafile);
 
   // WebView used only to print the selection.
-  WebKit::WebView* print_web_view_;
+  scoped_ptr<PrepareFrameAndViewForPrint> prep_frame_view_;
 
   scoped_ptr<PrintMsg_PrintPages_Params> print_pages_params_;
   bool is_preview_enabled_;
