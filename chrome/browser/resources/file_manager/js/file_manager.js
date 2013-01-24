3b5899e3bea7c026cc9b5bee899a59b27c1528db
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 14:44:11 +0000

    Displaying n bytes instead of 0 KB in Files.app.
    Before, for small files, eg. containing 10 bytes only, size was being reported as 0 KB. Now, for files smaller than 1 KB, size is reported as n bytes.
    Along the way, code has been simplified and hack for Hebrew language removed, since custom unit order can be now just specified in a localized string for each unit.
    
    TEST=Create one empty and one 10 bytes file. Observe how size is rendered in Files.app. It should be: 0 bytes, 10 bytes. Also, check if bigger files are ok.
    BUG=168264
    
    
    Review URL: https://chromiumcodereview.appspot.com/11962034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178570 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -168,7 +168,7 @@ DialogType.isModal = function(type) {
       sizeStatsResult, spaceInnerBar, spaceInfoLabel, spaceOuterBar) {
     spaceInnerBar.removeAttribute('pending');
     if (sizeStatsResult) {
-      var sizeStr = util.bytesToSi(sizeStatsResult.remainingSizeKB * 1024);
+      var sizeStr = util.bytesToString(sizeStatsResult.remainingSizeKB * 1024);
       spaceInfoLabel.textContent = strf('SPACE_AVAILABLE', sizeStr);
 
       var usedSpace =
@@ -328,12 +328,6 @@ DialogType.isModal = function(type) {
    */
   FileManager.prototype.initFileSystem_ = function() {
     util.installFileErrorToString();
-    // Replace the default unit in util to translated unit.
-    util.UNITS = [str('SIZE_KB'),
-                  str('SIZE_MB'),
-                  str('SIZE_GB'),
-                  str('SIZE_TB'),
-                  str('SIZE_PB')];
 
     metrics.startInterval('Load.FileSystem');
 
