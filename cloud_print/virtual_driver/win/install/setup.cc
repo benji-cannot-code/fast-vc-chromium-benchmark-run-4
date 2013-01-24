bf354806de1272b39b3c310b4a0810085d7e65cc
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-24 15:26:24 +0000

    Fixed failing overinstall.
    TBR=gene
    
    
    Review URL: https://chromiumcodereview.appspot.com/12049066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178590 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/virtual_driver/win/install/setup.cc
+++ cloud_print/virtual_driver/win/install/setup.cc
@@ -3,9 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <iomanip>
 #include <windows.h>
-#include <setupapi.h>  // Must be included after windows.h
 #include <winspool.h>
+#include <setupapi.h>  // Must be included after windows.h
 
 #include "base/at_exit.h"
 #include "base/command_line.h"
@@ -121,6 +122,7 @@ void SpoolerServiceCommand(const char* command) {
   base::LaunchOptions options;
   options.wait = true;
   options.start_hidden = true;
+  LOG(INFO) << command_line.GetCommandLineString();
   base::LaunchProcess(command_line, options, NULL);
 }
 
@@ -160,7 +162,6 @@ HRESULT RegisterPortMonitor(bool install, const FilePath& install_path) {
   // Use system32 path here because otherwise ::AddMonitor would fail.
   command_line.AppendArgPath(GetSystemPath(
       cloud_print::GetPortMonitorDllName()));
-
 
   base::LaunchOptions options;
   options.wait = true;
@@ -319,7 +320,7 @@ HRESULT InstallPpd(const FilePath& install_path) {
 }
 
 HRESULT UninstallPpd() {
-  int tries = 10;
+  int tries = 3;
   string16 driver_name = cloud_print::LoadLocalString(IDS_DRIVER_NAME);
   while (!DeletePrinterDriverEx(NULL,
                                 NULL,
@@ -481,21 +482,16 @@ void GetCurrentInstallPath(FilePath* install_path) {
   *install_path = FilePath(install_path_value);
 }
 
-HRESULT UnregisterVirtualDriver() {
+HRESULT TryUnregisterVirtualDriver() {
   HRESULT result = S_OK;
   result = UninstallPrinter();
   if (FAILED(result)) {
-    LOG(ERROR) << "Unable to uninstall Ppd.";
+    LOG(ERROR) << "Unable to delete printer.";
     return result;
   }
   result = UninstallPpd();
   if (FAILED(result)) {
-    LOG(ERROR) << "Unable to remove Ppd.";
-    // Put the printer back since we're not able to
-    // complete the uninstallation.
-    // TODO(abodenha@chromium.org) Figure out a better way to recover.
-    // See http://code.google.com/p/chromium/issues/detail?id=123039
-    InstallPrinter();
+    LOG(ERROR) << "Unable to remove PPD.";
     return result;
   }
   // The second argument is ignored if the first is false.
@@ -505,6 +501,20 @@ HRESULT UnregisterVirtualDriver() {
     return result;
   }
   return S_OK;
+}
+
+HRESULT UnregisterVirtualDriver() {
+  HRESULT hr = S_FALSE;
+  for (int i = 0; i < 2; ++i) {
+    hr = TryUnregisterVirtualDriver();
+    if (SUCCEEDED(hr)) {
+      break;
+    }
+    // Restart spooler and try again.
+    SpoolerServiceCommand("stop");
+    SpoolerServiceCommand("start");
+  }
+  return hr;
 }
 
 HRESULT DoLaunchUninstall(const FilePath& installer_source, bool wait) {
@@ -625,6 +635,8 @@ int WINAPI WinMain(__in  HINSTANCE hInstance,
   base::AtExitManager at_exit_manager;
   CommandLine::Init(0, NULL);
   HRESULT retval = ExecuteCommands();
+
+  LOG(INFO) << "HRESULT=0x" << std::setbase(16) << retval;
 
   // Installer is silent by default as required by Google Update.
   if (CommandLine::ForCurrentProcess()->HasSwitch("verbose")) {
