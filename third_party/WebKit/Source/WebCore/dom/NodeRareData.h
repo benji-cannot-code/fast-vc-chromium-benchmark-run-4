c92771ab82541d887509132a2d930d371aa0bd5e
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-24 19:25:53 +0000

    Fix a typo after r139838.
    * dom/NodeRareData.h:
    (NodeRareData):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140700 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/NodeRareData.h
+++ third_party/WebKit/Source/WebCore/dom/NodeRareData.h
@@ -334,7 +334,7 @@ public:
         m_connectedFrameCount -= amount;
     }
 
-    // This member function is intentionially not virtual to avoid adding a vtable pointer.
+    // This member function is intentionally not virtual to avoid adding a vtable pointer.
     void reportMemoryUsage(MemoryObjectInfo*) const;
 
 protected:
