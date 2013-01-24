8867f8032af9bbe1f253eba4a3f45b3e63e3e6d0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-24 04:56:03 +0000

    Enable autoplay when <video>'s src is from media stream https://bugs.webkit.org/show_bug.cgi?id=105224
    Patch by Wei Jia <wjia@chromium.org> on 2013-01-23
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Enable autoplay when <video>'s src is from media stream. This would avoid requesting multiple gestures when <video> is used for WebRTC.
    The test is done by modifying platform/chromium/media/video-capture-preview.html.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::loadResource):
    
    LayoutTests:
    
    Always turns on mediaPlaybackRequiresUserGesture at the beginning.
    HTMLMediaElement is supposed to turn it off when <video>'s src is from media stream.
    * platform/chromium/media/video-capture-preview.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140645 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
@@ -122,6 +122,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MediaSourceRegistry.h"
 #endif
 
+#if ENABLE(MEDIA_STREAM)
+#include "MediaStreamRegistry.h"
+#endif
+
 using namespace std;
 
 namespace WebCore {
@@ -1017,6 +1021,11 @@ void HTMLMediaElement::loadResource(const KURL& initialURL, ContentType& content
 #endif
 
     LOG(Media, "HTMLMediaElement::loadResource - m_currentSrc -> %s", urlForLoggingMedia(m_currentSrc).utf8().data());
+
+#if ENABLE(MEDIA_STREAM)
+    if (MediaStreamRegistry::registry().lookupMediaStreamDescriptor(url.string()))
+        removeBehaviorRestriction(RequireUserGestureForRateChangeRestriction);
+#endif
 
     if (m_sendProgressEvents) 
         startProgressEventTimer();
