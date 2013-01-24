da703504d42b6ca9f08fd409a8f0c5efba79cfb9
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-24 19:18:22 +0000

    Abandoned Memory: SVGFontElement and Corresponding SVGDocument Never Deconstructed https://bugs.webkit.org/show_bug.cgi?id=66438
    Reviewed by Dirk Schulze.
    
    The memory leak was caused by SVGFontFaceElement storing its own parent in a RefPtr.
    
    Fixed the bug by storing a raw pointer instead, and clearing the pointer in removedFrom
    when the node detached from the document. Also added several sanity check assertions.
    
    * svg/SVGFontFaceElement.cpp:
    (WebCore::SVGFontFaceElement::SVGFontFaceElement):
    (WebCore::SVGFontFaceElement::associatedFontElement):
    (WebCore::SVGFontFaceElement::rebuildFontFace):
    (WebCore::SVGFontFaceElement::insertedInto):
    (WebCore::SVGFontFaceElement::removedFrom):
    * svg/SVGFontFaceElement.h:
    (SVGFontFaceElement):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140698 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGFontFaceElement.h
+++ third_party/WebKit/Source/WebCore/svg/SVGFontFaceElement.h
@@ -62,7 +62,7 @@ private:
     virtual void removedFrom(ContainerNode*) OVERRIDE;
 
     RefPtr<StyleRuleFontFace> m_fontFaceRule;
-    RefPtr<SVGFontElement> m_fontElement;
+    SVGFontElement* m_fontElement;
 };
 
 } // namespace WebCore
