3bbec9836d89490155f60073373d5dbc1cff34cc
marcheu@chromium.org <marcheu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 04:50:30 +0000

    Implement support for monitor suspend.
    This speeds up idle resume by around 2.5 seconds.
    
    Previously, we would turn the panel off before idle suspend. Then we would
    suspend and resume. On resume, the kernel would restore the "off" state, and
    the Chrome monitor code would kick in to turn the panel on. This is a huge
    waste of time when each panel configuration step takes more than one second.
    
    Instead, just before suspending, we turn the backlight off and the panel on.
    On resume the kernel sets the panel state to on right away, and power_manager
    turns the backlight on. The Chrome monitor code still runs, but is a noop.
    
    This change needs this power_manager change to handle the backlight part:
    https://gerrit.chromium.org/gerrit/#/c/40971/
    
    This landed as r178195, but was reverted in r178288 because it crashed in the
    VM tests; unregistering suspenddelay from the destructor was assuming that the
    object proxy was still around which wasn't the case. I fixed the destructor
    to not unregister the suspenddelay.
    
    BUG=chrome-os-partner:13364
    TEST=By hand: reduce the idle suspend timeout, let the machine idle and
    TEST=suspend, press a key to resume and see how long it takes. The time
    TEST=it takes shrinks from ~4 seconds to ~1.5 second.
    TBR=sky@chromium.org
    
    Change-Id: I610995c12ed08624eb0bc91057d7c9bfa8002fbf
    
    Review URL: https://chromiumcodereview.appspot.com/12036092
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178754 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/power/suspend_observer.cc
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/power/suspend_observer.h"
+#include "chrome/browser/extensions/system/system_api.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/display/output_configurator.h"
+
+namespace chromeos {
+
+SuspendObserver::SuspendObserver() {
+  DBusThreadManager::Get()->GetPowerManagerClient()->AddObserver(this);
+}
+
+SuspendObserver::~SuspendObserver() {
+  DBusThreadManager::Get()->GetPowerManagerClient()->RemoveObserver(this);
+}
+
+void SuspendObserver::SuspendImminent() {
+  ash::Shell::GetInstance()->output_configurator()->SuspendDisplays();
+}
+
+}  // namespace chromeos
