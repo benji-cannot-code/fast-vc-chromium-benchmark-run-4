eaabb56cb4f535147b37d5938865dc39ecba59bd
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 08:04:14 +0000

    Fix the behavior of GetResourceList with search query parameter. This is not a perfect fix, but it is enough to pass existing tests.
    BUG=171620
    TEST=Run ./unit_tests --gtest_filter='FakeDriveServiceTest.*'
    
    
    Review URL: https://chromiumcodereview.appspot.com/12041038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178782 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/fake_drive_service.cc
+++ chrome/browser/google_apis/fake_drive_service.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/string_number_conversions.h"
 #include "base/string_split.h"
+#include "base/string_tokenizer.h"
+#include "base/string_util.h"
 #include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/google_apis/drive_api_parser.h"
@@ -21,6 +23,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 using content::BrowserThread;
 
 namespace google_apis {
+namespace {
+
+// Returns true if a resource entry matches with the search query.
+// Supports queries consist of following format.
+// - Phrases quoted by double/single quotes
+// - AND search for multiple words/phrases segmented by space
+// - Limited attribute search.  Only "title:" is supported.
+bool EntryMatchWithQuery(const ResourceEntry& entry,
+                         const std::string& query) {
+  StringTokenizer tokenizer(query, " ");
+  tokenizer.set_quote_chars("\"'");
+  while (tokenizer.GetNext()) {
+    std::string key, value;
+    const std::string& token = tokenizer.token();
+    if (token.find(':') == std::string::npos) {
+      TrimString(token, "\"'", &value);
+    } else {
+      StringTokenizer key_value(token, ":");
+      key_value.set_quote_chars("\"'");
+      if (!key_value.GetNext())
+        return false;
+      key = key_value.token();
+      if (!key_value.GetNext())
+        return false;
+      TrimString(key_value.token(), "\"'", &value);
+    }
+
+    // TODO(peria): Deal with other attributes than title.
+    if (!key.empty() && key != "title")
+      return false;
+    // Search query in the title.
+    if (UTF16ToUTF8(entry.title()).find(value) == std::string::npos)
+      return false;
+  }
+  return true;
+}
+
+}  // namespace
 
 FakeDriveService::FakeDriveService()
     : largest_changestamp_(0),
@@ -203,9 +243,9 @@ void FakeDriveService::GetResourceList(
 
     // If |search_query| is set, exclude the entry if it does not contain the
     // search query in the title.
-    if (!search_query.empty()) {
-      if (UTF16ToUTF8(entry->title()).find(search_query) == std::string::npos)
-        should_exclude = true;
+    if (!should_exclude && !search_query.empty() &&
+        !EntryMatchWithQuery(*entry, search_query)) {
+      should_exclude = true;
     }
 
     // If |start_changestamp| is set, exclude the entry if the
