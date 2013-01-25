064202d55ac24c62c4a26024f488d5c203eab61e
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-25 20:13:32 +0000

    Consider all ancestors not just parentElement when disconnecting frames https://bugs.webkit.org/show_bug.cgi?id=107769
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Previous we only used the parentElement of the frame owner to decide if
    we should disconnect the frame, but this means if you reparent a subtree
    that contains multiple frames from inside an unload handler we'll disconnect
    the subframes even though they're now in a different part of the document.
    
    We can fix this by using containsIncludingShadowDOM, and also simplify the
    code by removing ChildFrameDisconnector::Target.
    
    Test: fast/frames/unload-reparent-sibling-frame.html
    
    * dom/ContainerNodeAlgorithms.cpp:
    * dom/ContainerNodeAlgorithms.h:
    (ChildFrameDisconnector):
    (ChildFrameDisconnector::Target): Removed.
    (WebCore::ChildFrameDisconnector::collectFrameOwners):
    (WebCore::ChildFrameDisconnector::disconnectCollectedFrameOwners):
    
    LayoutTests:
    
    Add a test for moving frames around inside unload handlers.
    
    * fast/frames/unload-reparent-sibling-frame-expected.txt: Added.
    * fast/frames/unload-reparent-sibling-frame.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140856 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/ContainerNodeAlgorithms.cpp
+++ third_party/WebKit/Source/WebCore/dom/ContainerNodeAlgorithms.cpp
@@ -116,12 +116,6 @@ void ChildFrameDisconnector::collectFrameOwners(ElementShadow* shadow)
         collectFrameOwners(root);
 }
 
-void ChildFrameDisconnector::Target::disconnect()
-{
-    ASSERT(isValid());
-    toFrameOwnerElement(m_owner.get())->disconnectContentFrame();
-}
-
 #ifndef NDEBUG
 unsigned assertConnectedSubrameCountIsConsistent(Node* node)
 {
