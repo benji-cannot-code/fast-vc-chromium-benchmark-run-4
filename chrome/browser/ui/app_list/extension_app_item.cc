1b96c5dc6f03827e2eab836f0c4c46c8a59cd80f
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 08:04:45 +0000

    app_list: Ensure ExtensionAppItem has valid ordinals.
    BUG=167752
    TEST=Covered by AppsModelBuilderTest.InvalidOrdinal.
    
    R=benwells@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12058004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178784 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/extension_app_item.cc
+++ chrome/browser/ui/app_list/extension_app_item.cc
@@ -196,6 +196,8 @@ ExtensionAppItem::ExtensionAppItem(Profile* profile,
       controller_(controller),
       has_overlay_(HasOverlay(extension)) {
   Reload();
+  GetExtensionSorting(profile_)->EnsureValidOrdinals(extension_id_,
+                                                     syncer::StringOrdinal());
 }
 
 ExtensionAppItem::~ExtensionAppItem() {
