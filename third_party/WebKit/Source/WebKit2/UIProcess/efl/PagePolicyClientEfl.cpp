a2144e3c3dde17d73b06b60608b18cf1530d4b02
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-25 10:45:11 +0000

    [WK2][EFL] PagePolicyClientEfl should be based on C WK2 API https://bugs.webkit.org/show_bug.cgi?id=107854
    Reviewed by Benjamin Poulain.
    
    PagePolicyClientEfl should be based on C WK2 API in order not to
    break API layering.
    
    * UIProcess/efl/PagePolicyClientEfl.cpp:
    (WebKit::PagePolicyClientEfl::decidePolicyForResponseCallback):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140806 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/efl/PagePolicyClientEfl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/efl/PagePolicyClientEfl.cpp
@@ -30,7 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EwkView.h"
 #include "WKFrame.h"
 #include "WKFramePolicyListener.h"
-#include "WebFrameProxy.h"
+#include "WKString.h"
+#include "WKURLResponse.h"
 #include "ewk_navigation_policy_decision.h"
 #include "ewk_navigation_policy_decision_private.h"
 #include <WebCore/HTTPStatusCodes.h>
@@ -63,25 +64,21 @@ void PagePolicyClientEfl::decidePolicyForNewWindowAction(WKPageRef, WKFrameRef, 
 
 void PagePolicyClientEfl::decidePolicyForResponseCallback(WKPageRef, WKFrameRef frame, WKURLResponseRef response, WKURLRequestRef, WKFramePolicyListenerRef listener, WKTypeRef /*userData*/, const void* /*clientInfo*/)
 {
-    using namespace WebCore;
-
-    const ResourceResponse resourceResponse = toImpl(response)->resourceResponse();
-
     // Ignore responses with an HTTP status code of 204 (No Content)
-    if (resourceResponse.httpStatusCode() == HTTPNoContent) {
+    if (WKURLResponseHTTPStatusCode(response) == WebCore::HTTPNoContent) {
         WKFramePolicyListenerIgnore(listener);
         return;
     }
 
     // If the URL Response has "Content-Disposition: attachment;" header, then
     // we should download it.
-    if (resourceResponse.isAttachment()) {
+    if (WKURLResponseIsAttachment(response)) {
         WKFramePolicyListenerDownload(listener);
         return;
     }
 
-    String mimeType = toImpl(response)->resourceResponse().mimeType().lower();
-    bool canShowMIMEType = toImpl(frame)->canShowMIMEType(mimeType);
+    WKRetainPtr<WKStringRef> mimeType = adoptWK(WKURLResponseCopyMIMEType(response));
+    bool canShowMIMEType = WKFrameCanShowMIMEType(frame, mimeType.get());
     if (WKFrameIsMainFrame(frame)) {
         if (canShowMIMEType) {
             WKFramePolicyListenerUse(listener);
@@ -95,7 +92,8 @@ void PagePolicyClientEfl::decidePolicyForResponseCallback(WKPageRef, WKFrameRef 
 
     // We should ignore downloadable top-level content for subframes, with an exception for text/xml and application/xml so we can still support Acid3 test.
     // It makes the browser intentionally behave differently when it comes to text(application)/xml content in subframes vs. mainframe.
-    if (!canShowMIMEType && !(mimeType == "text/xml" || mimeType == "application/xml")) {
+    bool isXMLType = WKStringIsEqualToUTF8CString(mimeType.get(), "text/xml") || WKStringIsEqualToUTF8CString(mimeType.get(), "application/xml");
+    if (!canShowMIMEType && !isXMLType) {
         WKFramePolicyListenerIgnore(listener);
         return;
     }
