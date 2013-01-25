c4b64f0262c1a7682aa2d1cdbb76aaf367e3569c
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-25 03:03:33 +0000

    [EFL] Unreviewed build fix after r140752. https://bugs.webkit.org/show_bug.cgi?id=107886
    Build break after r140752.
    
    Patch by Huang Dongsung <luxtella@company100.net> on 2013-01-24
    
    * PlatformEfl.cmake: Move DownloadSoupErrorsEfl and DownloadSoup to Shared/Downloads.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
+++ third_party/WebKit/Source/WebKit2/PlatformEfl.cmake
@@ -14,6 +14,9 @@ list(APPEND WebKit2_SOURCES
 
     Shared/API/c/cairo/WKImageCairo.cpp
 
+    Shared/Downloads/efl/DownloadSoupErrorsEfl.cpp
+    Shared/Downloads/soup/DownloadSoup.cpp
+
     Shared/cairo/ShareableBitmapCairo.cpp
 
     Shared/efl/LayerTreeContextEfl.cpp
@@ -121,9 +124,6 @@ list(APPEND WebKit2_SOURCES
     WebProcess/Cookies/soup/WebCookieManagerSoup.cpp
     WebProcess/Cookies/soup/WebKitSoupCookieJarSqlite.cpp
 
-    WebProcess/Downloads/efl/DownloadSoupErrorsEfl.cpp
-    WebProcess/Downloads/soup/DownloadSoup.cpp
-
     WebProcess/InjectedBundle/efl/InjectedBundleEfl.cpp
 
     WebProcess/Plugins/Netscape/unix/PluginProxyUnix.cpp
@@ -161,6 +161,7 @@ list(APPEND WebKit2_INCLUDE_DIRECTORIES
     "${WEBCORE_DIR}/platform/network/soup"
     "${WEBCORE_DIR}/platform/text/enchant"
     "${WEBCORE_DIR}/svg/graphics"
+    "${WEBKIT2_DIR}/Shared/Downloads/soup"
     "${WEBKIT2_DIR}/Shared/efl"
     "${WEBKIT2_DIR}/Shared/soup"
     "${WEBKIT2_DIR}/UIProcess/API/C/efl"
@@ -169,7 +170,6 @@ list(APPEND WebKit2_INCLUDE_DIRECTORIES
     "${WEBKIT2_DIR}/UIProcess/API/efl"
     "${WEBKIT2_DIR}/UIProcess/efl"
     "${WEBKIT2_DIR}/UIProcess/soup"
-    "${WEBKIT2_DIR}/WebProcess/Downloads/soup"
     "${WEBKIT2_DIR}/WebProcess/efl"
     "${WEBKIT2_DIR}/WebProcess/soup"
     "${WEBKIT2_DIR}/WebProcess/WebCoreSupport/efl"
