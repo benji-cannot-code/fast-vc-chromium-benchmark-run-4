2365765ca3ce9efb00661978c5e3817026218a82
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 04:38:15 +0000

    Avoid going through WebWidget::composite to composite a RenderWidget
    When a content::RenderWidget is using accelerated compositing, the compositing
    is driven either by a WebLayerTreeView owned by the RenderWidget or a PepperWidget
    if the RenderWidget is actually a RenderWidgetFullscreenPepper. Neither case
    involves WebKit, so this avoids calling WebWidget::composite in favor of directly
    calling in to the appropriate compositing system.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/11787006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178746 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget_fullscreen_pepper.cc
+++ content/renderer/render_widget_fullscreen_pepper.cc
@@ -140,15 +140,6 @@ class PepperWidget : public WebWidget {
   }
 
   virtual void composite(bool finish) {
-    if (!widget_->plugin())
-      return;
-
-    WebGraphicsContext3DCommandBufferImpl* context = widget_->context();
-    DCHECK(context);
-    unsigned int texture = widget_->plugin()->GetBackingTextureId();
-    context->bindTexture(GL_TEXTURE_2D, texture);
-    context->drawArrays(GL_TRIANGLES, 0, 3);
-    widget_->SwapBuffers();
   }
 
   virtual void themeChanged() {
@@ -518,6 +509,21 @@ WebWidget* RenderWidgetFullscreenPepper::CreateWebWidget() {
 
 bool RenderWidgetFullscreenPepper::SupportsAsynchronousSwapBuffers() {
   return context_ != NULL;
+}
+
+// Fullscreen pepper widgets composite themselves into the plugin's backing
+// texture (as opposed to using the cc library to composite as normal
+// content::RenderWidgets do), so to produce a composited frame we just have to
+// draw this texture and swap.
+void RenderWidgetFullscreenPepper::Composite() {
+  if (!plugin_)
+    return;
+
+  DCHECK(context_);
+  unsigned int texture = plugin_->GetBackingTextureId();
+  context_->bindTexture(GL_TEXTURE_2D, texture);
+  context_->drawArrays(GL_TRIANGLES, 0, 3);
+  SwapBuffers();
 }
 
 void RenderWidgetFullscreenPepper::CreateContext() {
