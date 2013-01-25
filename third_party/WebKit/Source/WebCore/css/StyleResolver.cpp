b9c58fc1228ac5640728fadb7c103dc5f7c0bf51
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-25 18:11:07 +0000

    Build broken when svg is disabled. https://bugs.webkit.org/show_bug.cgi?id=92393.
    Unreviewed build fix.
    
    Patch by Chang Shu <cshu@webkit.org> on 2013-01-25
    
    * css/StyleResolver.cpp:
    (WebCore::StyleResolver::applyProperty):
    * rendering/FilterEffectRenderer.cpp:
    (WebCore::FilterEffectRenderer::buildReferenceFilter):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@140845 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
+++ third_party/WebKit/Source/WebCore/css/StyleResolver.cpp
@@ -4107,12 +4107,12 @@ void StyleResolver::applyProperty(CSSPropertyID id, CSSValue* value)
 #endif
         ASSERT_NOT_REACHED();
         return;
-#if ENABLE(SVG)
     default:
+#if ENABLE(SVG)
         // Try the SVG properties
         applySVGProperty(id, value);
-        return;
 #endif
+        return;
     }
 }
 
--- third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
+++ third_party/WebKit/Source/WebCore/rendering/FilterEffectRenderer.cpp
@@ -183,6 +183,7 @@ PassRefPtr<FilterEffect> FilterEffectRenderer::buildReferenceFilter(RenderObject
     }
     return effect;
 #else
+    UNUSED_PARAM(renderer);
     UNUSED_PARAM(previousEffect);
     UNUSED_PARAM(filterOperation);
     return 0;
