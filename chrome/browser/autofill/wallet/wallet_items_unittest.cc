60fc5b489bd2502cf4cf6c267278305dd2aafdce
ahutter@chromium.org <ahutter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 19:16:15 +0000

    Change CVC_RISK_CHALLENGE to VERIFY_CVV
    BUG=172128
    
    
    Review URL: https://chromiumcodereview.appspot.com/12049083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178866 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/wallet/wallet_items_unittest.cc
+++ chrome/browser/autofill/wallet/wallet_items_unittest.cc
@@ -228,7 +228,7 @@ const char kWalletItemsWithInvalidRequiredActions[] =
     "  \"obfuscated_gaia_id\":\"\","
     "  \"required_action\":"
     "  ["
-    "    \"cvc_risk_CHALLENGE\","
+    "    \"verify_CVV\","
     "    \"UPGRADE_MIN_ADDRESS\","
     "    \"update_EXPIRATION_date\","
     "    \" 忍者の正体 \""
