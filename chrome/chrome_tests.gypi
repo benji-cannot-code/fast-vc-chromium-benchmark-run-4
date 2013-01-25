0e5f75e1a242a73cc9783c8bf2916116e0f1452b
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 09:25:05 +0000

    drive: Replace local FakeDriveService with google_apis::FakeDriveService part 3
    external_filesystem_apitest.cc is now a lot simpler.
    
    BUG=169194
    TEST=none; test changes
    
    
    Review URL: https://chromiumcodereview.appspot.com/11938034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178796 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1162,6 +1162,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/first_run/try_chrome_dialog_view_browsertest.cc',
         'browser/geolocation/access_token_store_browsertest.cc',
         'browser/geolocation/geolocation_browsertest.cc',
+        'browser/google_apis/fake_drive_service.cc',
+        'browser/google_apis/fake_drive_service.h',
         'browser/history/history_browsertest.cc',
         'browser/history/multipart_browsertest.cc',
         'browser/history/redirect_browsertest.cc',
--- chrome/test/data/extensions/api_test/filebrowser_component/remote.js
+++ chrome/test/data/extensions/api_test/filebrowser_component/remote.js
@@ -15,14 +15,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The feeds are located in chrome/test/data/chromeos/drive/.
 var kDirectoryPath = 'drive/Folder';
 var kFileName = 'File.aBc';
-var kExpectedContents = 'hello, world!';
-var kWriteOffset = 12;
+// The drive service returns "x"s of the file size defined in the JSON file.
+// The file size for File.aBc is set to 13.
+var kExpectedContents = 'xxxxxxxxxxxxx';
+var kWriteOffset = 13;
 var kWriteData = '!!!';
-var kExpectedAfterWrite = 'hello, world!!!';
+var kExpectedAfterWrite = 'xxxxxxxxxxxxx!!!';
 var kTruncateShortLength = 5;
-var kExpectedAfterTruncateShort = 'hello';
+var kExpectedAfterTruncateShort = 'xxxxx';
 var kTruncateLongLength = 7;
-var kExpectedAfterTruncateLong = 'hello\0\0';
+var kExpectedAfterTruncateLong = 'xxxxx\0\0';
 var kNewDirectoryPath = 'drive/FolderNew';
 var kFileManagerExtensionId = 'hhaomjibdihmijegdhdafkllkbggdgoj';
 
