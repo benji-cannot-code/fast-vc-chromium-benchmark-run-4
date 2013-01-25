93c98184ca81eb5344ac0bd2bd93c74c1d8191e8
gbillock@chromium.org <gbillock@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 21:59:08 +0000

    [Media Galleries] Load device info in a SequencedWorkerPool
    If fetching device information blocked, a worker thread would
    freeze on shutdown. This change issues such requests in a threadpool
    using CONTINUE_ON_SHUTDOWN, which abandons such calls if they
    don't complete. Ordinarily, all calls should run quickly.
    
    BUG=155910
    
    
    Review URL: https://chromiumcodereview.appspot.com/11414313
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/system_monitor/media_device_notifications_utils.cc
+++ chrome/browser/system_monitor/media_device_notifications_utils.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome {
 
 bool IsMediaDevice(const FilePath::StringType& mount_point) {
-  DCHECK(content::BrowserThread::CurrentlyOn(content::BrowserThread::FILE));
+  DCHECK(!content::BrowserThread::CurrentlyOn(content::BrowserThread::UI));
 
   FilePath dcim_path(mount_point);
   FilePath::StringType dcim_dir(kDCIMDirectoryName);
