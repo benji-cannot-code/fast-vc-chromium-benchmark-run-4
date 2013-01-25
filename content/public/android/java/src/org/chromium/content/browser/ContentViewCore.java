5aeede428c3abfbb8abae85938a04950b80cb4c4
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-25 09:15:36 +0000

    Expose IME state changes to clients of ContentViewCore.
    Tweaks the flag for determining whether to begin a focus
    animation to be based on the window viewport changing as it
    allows clients to change the UI without triggering an early
    focus.
    
    BUG=161303
    
    
    Review URL: https://chromiumcodereview.appspot.com/12042097
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178794 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -243,8 +243,8 @@ public class ContentViewCore implements MotionEventDelegate, NavigationClient {
 
     // Temporary notification to tell onSizeChanged to focus a form element,
     // because the OSK was just brought up.
-    private boolean mFocusOnNextSizeChanged = false;
     private boolean mUnfocusOnNextSizeChanged = false;
+    private Rect mFocusPreOSKViewportRect = new Rect();
 
     private boolean mNeedUpdateOrientationChanged;
 
@@ -390,6 +390,7 @@ public class ContentViewCore implements MotionEventDelegate, NavigationClient {
 
                     @Override
                     public void onDismissInput() {
+                        getContentViewClient().onImeStateChangeRequested(false);
                     }
 
                     @Override
@@ -402,11 +403,15 @@ public class ContentViewCore implements MotionEventDelegate, NavigationClient {
                         return new ResultReceiver(new Handler()) {
                             @Override
                             public void onReceiveResult(int resultCode, Bundle resultData) {
+                                getContentViewClient().onImeStateChangeRequested(
+                                        resultCode == InputMethodManager.RESULT_SHOWN ||
+                                        resultCode == InputMethodManager.RESULT_UNCHANGED_SHOWN);
                                 if (resultCode == InputMethodManager.RESULT_SHOWN) {
                                     // If OSK is newly shown, delay the form focus until
                                     // the onSizeChanged (in order to adjust relative to the
                                     // new size).
-                                    mFocusOnNextSizeChanged = true;
+                                    getContainerView().getWindowVisibleDisplayFrame(
+                                            mFocusPreOSKViewportRect);
                                 } else if (resultCode ==
                                         InputMethodManager.RESULT_UNCHANGED_SHOWN) {
                                     // If the OSK was already there, focus the form immediately.
@@ -1315,9 +1320,13 @@ public class ContentViewCore implements MotionEventDelegate, NavigationClient {
     public void updateAfterSizeChanged() {
         // Execute a delayed form focus operation because the OSK was brought
         // up earlier.
-        if (mFocusOnNextSizeChanged) {
-            scrollFocusedEditableNodeIntoView();
-            mFocusOnNextSizeChanged = false;
+        if (!mFocusPreOSKViewportRect.isEmpty()) {
+            Rect rect = new Rect();
+            getContainerView().getWindowVisibleDisplayFrame(rect);
+            if (!rect.equals(mFocusPreOSKViewportRect)) {
+                scrollFocusedEditableNodeIntoView();
+                mFocusPreOSKViewportRect.setEmpty();
+            }
         } else if (mUnfocusOnNextSizeChanged) {
             undoScrollFocusedEditableNodeIntoViewIfNeeded(true);
             mUnfocusOnNextSizeChanged = false;
@@ -1378,6 +1387,7 @@ public class ContentViewCore implements MotionEventDelegate, NavigationClient {
      */
     @SuppressWarnings("javadoc")
     public void onFocusChanged(boolean gainFocus, int direction, Rect previouslyFocusedRect) {
+        if (!gainFocus) getContentViewClient().onImeStateChangeRequested(false);
         if (mNativeContentViewCore != 0) nativeSetFocus(mNativeContentViewCore, gainFocus);
     }
 
