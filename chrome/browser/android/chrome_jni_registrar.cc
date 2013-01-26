3357b14598af4d5e8c37e849bd559e7c68de0509
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-26 04:20:08 +0000

    Android implementation of WebsiteSettingsUi
    BUG=172013
    
    
    Review URL: https://chromiumcodereview.appspot.com/12045081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179027 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -19,8 +19,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/android/chrome_http_auth_handler.h"
 #include "chrome/browser/ui/android/javascript_app_modal_dialog_android.h"
 #include "chrome/browser/ui/android/navigation_popup.h"
+#include "chrome/browser/ui/android/website_settings_popup_android.h"
 #include "components/navigation_interception/component_jni_registrar.h"
 #include "components/web_contents_delegate_android/component_jni_registrar.h"
+
+bool RegisterCertificateViewer(JNIEnv* env);
 
 namespace chrome {
 namespace android {
@@ -34,6 +37,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "ApplicationLifetime", browser::RegisterApplicationLifetimeAndroid},
   { "AutofillPopup",
       AutofillPopupViewAndroid::RegisterAutofillPopupViewAndroid},
+  { "CertificateViewer", RegisterCertificateViewer},
   { "ChromeBrowserProvider",
       ChromeBrowserProvider::RegisterChromeBrowserProvider },
   { "ChromeHttpAuthHandler",
@@ -48,6 +52,8 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "NavigationPopup", NavigationPopup::RegisterNavigationPopup },
   { "ProcessUtils", RegisterProcessUtils },
   { "SqliteCursor", SQLiteCursor::RegisterSqliteCursor },
+  { "WebsiteSettingsPopupAndroid",
+      WebsiteSettingsPopupAndroid::RegisterWebsiteSettingsPopupAndroid },
 };
 
 bool RegisterJni(JNIEnv* env) {
