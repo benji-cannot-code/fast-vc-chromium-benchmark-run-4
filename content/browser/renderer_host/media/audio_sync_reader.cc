f6d7dedf2df1c6df61bb7f168a5451527d2b0854
dalecurtis@chromium.org <dalecurtis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-26 03:48:12 +0000

    Track UMA stats for when the renderer side audio device wasn't ready.
    A renderer that's not ready causes glitching, we should measure what
    percentage of users are having issues to determine how big of a problem
    this is.  To start, we'll record the percentage of clients on each
    platform who see more than 3 instances of an unprepared renderer.
    
    BUG=none
    TEST=<in progress> Run locally on each platform to ensure common case
    is zero.
    
    
    Review URL: https://chromiumcodereview.appspot.com/11975031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179011 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/audio_sync_reader.cc
+++ content/browser/renderer_host/media/audio_sync_reader.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "base/metrics/histogram.h"
 #include "base/process_util.h"
 #include "base/shared_memory.h"
 #include "media/audio/audio_buffers_state.h"
@@ -21,7 +22,9 @@ AudioSyncReader::AudioSyncReader(base::SharedMemory* shared_memory,
                                  const media::AudioParameters& params,
                                  int input_channels)
     : shared_memory_(shared_memory),
-      input_channels_(input_channels) {
+      input_channels_(input_channels),
+      renderer_callback_count_(0),
+      renderer_missed_callback_count_(0) {
   packet_size_ = media::PacketSizeInBytes(shared_memory_->created_size());
   int input_memory_size = 0;
   int output_memory_size = AudioBus::CalculateMemorySize(params);
@@ -38,6 +41,15 @@ AudioSyncReader::AudioSyncReader(base::SharedMemory* shared_memory,
 }
 
 AudioSyncReader::~AudioSyncReader() {
+  if (!renderer_callback_count_)
+    return;
+
+  // Recording the percentage of deadline misses gives us a rough overview of
+  // how many users might be running into audio glitches.
+  int percentage_missed =
+      100.0 * renderer_missed_callback_count_ / renderer_callback_count_;
+  UMA_HISTOGRAM_PERCENTAGE(
+      "Media.AudioRendererMissedDeadline", percentage_missed);
 }
 
 bool AudioSyncReader::DataReady() {
@@ -58,6 +70,10 @@ void AudioSyncReader::UpdatePendingBytes(uint32 bytes) {
 }
 
 int AudioSyncReader::Read(AudioBus* source, AudioBus* dest) {
+  ++renderer_callback_count_;
+  if (!DataReady())
+    ++renderer_missed_callback_count_;
+
   // Copy optional synchronized live audio input for consumption by renderer
   // process.
   if (source && input_bus_.get()) {
