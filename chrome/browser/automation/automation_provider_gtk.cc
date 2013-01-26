0685bd4d666ed1c413a63b501654dcae4abc07f6
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-26 17:17:55 +0000

    automation: Remove a bunch of unused includes from automation_provider_*.cc files.
    These two files contains only the definition of PrintAsync() method, which is
    marked as NOTIMPLEMENTED, so none of these includes are necessary.
    
    TBR=jam@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12095003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179069 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/automation_provider_gtk.cc
+++ chrome/browser/automation/automation_provider_gtk.cc
@@ -5,19 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/automation/automation_provider.h"
 
-#include <gtk/gtk.h>
-
-#include "base/bind.h"
-#include "base/bind_helpers.h"
-#include "base/callback.h"
-#include "base/utf_string_conversions.h"
-#include "chrome/browser/automation/automation_browser_tracker.h"
-#include "chrome/browser/automation/automation_window_tracker.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/gtk/browser_window_gtk.h"
-#include "chrome/common/automation_messages.h"
-#include "ui/gfx/point.h"
-#include "ui/gfx/rect.h"
+#include "base/logging.h"
 
 void AutomationProvider::PrintAsync(int tab_handle) {
   NOTIMPLEMENTED();
