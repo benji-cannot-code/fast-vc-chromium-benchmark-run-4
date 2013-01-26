ad839266a427d1d85c473f060ffb8429fa5c9511
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-26 04:09:10 +0000

    Fixed code disabled by invalide DCHECK use. Other minor cleanup.
    BUG=172198
    
    
    Review URL: https://chromiumcodereview.appspot.com/12038105
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179020 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/image_win.cc
+++ printing/image_win.cc
@@ -5,10 +5,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "printing/image.h"
 
+#include "base/win/scoped_gdi_object.h"
+#include "base/win/scoped_hdc.h"
+#include "base/win/scoped_select_object.h"
 #include "printing/metafile.h"
 #include "skia/ext/platform_device.h"
 #include "ui/gfx/gdi_util.h"  // EMF support
 #include "ui/gfx/rect.h"
+
 
 namespace {
 
@@ -51,7 +55,7 @@ bool Image::LoadMetafile(const Metafile& metafile) {
   DisableFontSmoothing disable_in_this_scope;
 
   // Create a temporary HDC and bitmap to retrieve the rendered data.
-  HDC hdc = CreateCompatibleDC(NULL);
+  base::win::ScopedCreateDC hdc(::CreateCompatibleDC(NULL));
   BITMAPV4HEADER hdr;
   DCHECK_EQ(rect.x(), 0);
   DCHECK_EQ(rect.y(), 0);
@@ -63,12 +67,12 @@ bool Image::LoadMetafile(const Metafile& metafile) {
 
   size_ = rect.size();
   gfx::CreateBitmapV4Header(rect.width(), rect.height(), &hdr);
-  void* bits;
-  HBITMAP bitmap = CreateDIBSection(hdc,
-                                    reinterpret_cast<BITMAPINFO*>(&hdr), 0,
-                                    &bits, NULL, 0);
+  unsigned char* bits = NULL;
+  base::win::ScopedBitmap bitmap(
+      ::CreateDIBSection(hdc, reinterpret_cast<BITMAPINFO*>(&hdr), 0,
+                         reinterpret_cast<void**>(&bits), NULL, 0));
   DCHECK(bitmap);
-  DCHECK(SelectObject(hdc, bitmap));
+  base::win::ScopedSelectObject select_object(hdc, bitmap);
 
   skia::InitializeDC(hdc);
 
@@ -78,11 +82,7 @@ bool Image::LoadMetafile(const Metafile& metafile) {
   size_t bytes = row_length_ * size_.height();
   DCHECK(bytes);
 
-  data_.resize(bytes);
-  memcpy(&*data_.begin(), bits, bytes);
-
-  DeleteDC(hdc);
-  DeleteObject(bitmap);
+  data_.assign(bits, bits + bytes);
 
   return success;
 }
