2b39c40bdbb09fb851d7fac76e3a9c2b68a1c38d
guohui@chromium.org <guohui@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-26 03:51:05 +0000

    Auto signin with the web identity when launcher is enabled.
    Mock at https://docs.google.com/a/google.com/folder/d/0B4tKgpQN7CIgTUJqaXBXY0xUZDA/edit?docId=0B4tKgpQN7CIgSUthZzN0TUY3cUU.
    
    Design doc at
    https://docs.google.com/a/google.com/document/d/1ErxqA0wEQHm7Q40-yHmTT9kQm1rSGgxLE4xkhg_PWs8/edit.
    
    BUG=159733
    
    Review URL: https://codereview.chromium.org/11896034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179014 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/signin_manager.h
+++ chrome/browser/signin/signin_manager.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_registrar.h"
 #include "google_apis/gaia/gaia_auth_consumer.h"
 #include "google_apis/gaia/google_service_auth_error.h"
+#include "net/cookies/canonical_cookie.h"
 
 class CookieSettings;
 class GaiaAuthFetcher;
@@ -208,6 +209,15 @@ class SigninManager : public GaiaAuthConsumer,
                         const std::string& username,
                         const std::string& password);
 
+  // Called to verify GAIA cookies asynchronously before starting auto sign-in
+  // without password.
+  void VerifyGaiaCookiesBeforeSignIn(const std::string& session_index);
+
+  // Called when GAIA cookies are fetched. If LSID cookie is valid, then start
+  // auto sign-in by exchanging cookies for an oauth code.
+  void OnGaiaCookiesFetched(
+      const std::string session_index, const net::CookieList& cookie_list);
+
   // Called when a new request to re-authenticate a user is in progress.
   // Will clear in memory data but leaves the db as such so when the browser
   // restarts we can use the old token(which might throw a password error).
@@ -266,6 +276,8 @@ class SigninManager : public GaiaAuthConsumer,
   // The list of SigninDiagnosticObservers.
   ObserverList<signin_internals_util::SigninDiagnosticsObserver>
       signin_diagnostics_observers_;
+
+  base::WeakPtrFactory<SigninManager> weak_pointer_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(SigninManager);
 };
