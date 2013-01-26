e17578cff54612b627e78735e063298a46416e63
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-26 00:07:06 +0000

    When the user says "don't show me again", make sure to respect it.
    In some cases, its not possible to maintain context information from http
    requests using URLRequest user data.  Redirects during the sign in process
    may use 200 OK responses with html that causes an auto refresh.  Use the
    io profile data instead to hold the state.
    
    BUG=169874
    TEST=See bug description for repro steps
    
    
    Review URL: https://chromiumcodereview.appspot.com/11938007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178947 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_helper_unittest.cc
+++ chrome/browser/ui/sync/one_click_signin_helper_unittest.cc
@@ -312,8 +312,6 @@ OneClickSigninHelperIOTest::~OneClickSigninHelperIOTest() {
 void OneClickSigninHelperIOTest::SetUp() {
   OneClickSigninHelperTest::SetUp();
   ASSERT_TRUE(testing_profile_manager_.SetUp());
-  OneClickSigninHelper::AssociateWithRequestForTesting(&request_,
-                                                       "user@gmail.com");
 }
 
 TestProfileIOData* OneClickSigninHelperIOTest::CreateTestProfileIOData(
@@ -324,6 +322,7 @@ TestProfileIOData* OneClickSigninHelperIOTest::CreateTestProfileIOData(
       CookieSettings::Factory::GetForProfile(profile_);
   TestProfileIOData* io_data = new TestProfileIOData(
       is_incognito, pref_service, local_state, cookie_settings);
+  io_data->set_reverse_autologin_pending_email("user@gmail.com");
   return io_data;
 }
 
