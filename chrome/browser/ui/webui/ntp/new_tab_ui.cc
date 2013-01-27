5aee0881949073c109b9475a4692d16cb470a75f
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-27 07:55:04 +0000

    Reland: Don't show the apps page on the NTP if the app launcher is installed.
    R=benwells@chromium.org,estade@chromium.org
    BUG=170526
    
    Originally committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=178769
    
    Review URL: https://codereview.chromium.org/11953021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179088 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/new_tab_ui.cc
+++ chrome/browser/ui/webui/ntp/new_tab_ui.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/defaults.h"
+#include "chrome/browser/extensions/app_launcher.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_types.h"
@@ -295,13 +296,12 @@ void NewTabUI::RegisterUserPrefs(PrefServiceSyncable* prefs) {
 
 // static
 bool NewTabUI::ShouldShowApps() {
-#if defined(USE_ASH) || defined(OS_ANDROID)
+#if defined(OS_ANDROID)
   // Ash shows apps in app list thus should not show apps page in NTP4.
   // Android does not have apps.
   return false;
 #else
-  return !CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kShowAppListShortcut);
+  return !extensions::IsAppLauncherEnabled();
 #endif
 }
 
