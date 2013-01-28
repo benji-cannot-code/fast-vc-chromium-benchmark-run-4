316634df095830d0ae51f9cf5fc4e507bb7649ab
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-28 23:53:12 +0000

    Adding upper limit to DiscardableMemoryAndroid
    On android there is a total fd limit of 1024 per process. This change adds a limit to the number of DiscardableMemory we can create.
    The cap is set to 128 conservatively as there are other objects consuming fds.
    When initialization succeeds, the caller increment the global fd counter by 1. Otherwise, it will keep the fd unchanged.
    When fd reaches its limit, all subsequent initialization calls will fail.
    The global fd counter will be decremented when a successfully initialized DiscardableMemory object goes away.
    
    BUG=164052
    
    Review URL: https://codereview.chromium.org/12088008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179217 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/discardable_memory.h
+++ base/memory/discardable_memory.h
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define BASE_MEMORY_DISCARDABLE_MEMORY_H_
 
 #include "base/base_export.h"
+#include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "base/synchronization/lock.h"
 
 namespace base {
 
@@ -57,6 +57,8 @@ class BASE_EXPORT DiscardableMemory {
 
   // Initialize the DiscardableMemory object. On success, this function returns
   // true and the memory is locked. This should only be called once.
+  // This call could fail because of platform-specific limitations and the user
+  // should stop using the DiscardableMemory afterwards.
   bool InitializeAndLock(size_t size);
 
   // Lock the memory so that it will not be purged by the system. Returns
@@ -93,6 +95,13 @@ class BASE_EXPORT DiscardableMemory {
 
   // Unmaps the discardable memory from the caller's address space.
   void Unmap();
+
+  // Reserve a file descriptor. When reaching the fd limit, this call returns
+  // false and initialization should fail.
+  bool ReserveFileDescriptor();
+
+  // Release a file descriptor so that others can reserve it.
+  void ReleaseFileDescriptor();
 #endif  // OS_ANDROID
 
   void* memory_;
