1c7ca450b2496175f81eac85845e0fcde950b70d
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-28 21:42:22 +0000

    PDFPlugin: getMainResourceDataOfFrame should return PDFPlugin's data so that Save... works https://bugs.webkit.org/show_bug.cgi?id=108060 <rdar://problem/13075454>
    Reviewed by Sam Weinig.
    
    * WebProcess/Plugins/Netscape/NetscapePlugin.h: Implement getResourceData.
    * WebProcess/Plugins/PDF/SimplePDFPlugin.h:
    * WebProcess/Plugins/PDF/SimplePDFPlugin.mm: Implement getResourceData, returning the accumulated
    data if it exists and has finished loading.
    (WebKit::SimplePDFPlugin::getResourceData):
    * WebProcess/Plugins/Plugin.h: Add getResourceData, which hands out a raw pointer/size pair
    to the plugin's "main resource" data.
    * WebProcess/Plugins/PluginProxy.h: Implement getResourceData.
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::getResourceData): Forward getResourceData through to the plugin.
    * WebProcess/Plugins/PluginView.h:
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::getMainResourceDataOfFrame): If the requested frame is backed by a PluginDocument,
    attempt to ask the PluginView for its resource data.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141004 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/NetscapePlugin.h
@@ -248,6 +248,8 @@ private:
     // converted (if the transformation matrix isn't invertible).
     bool convertFromRootView(const WebCore::IntPoint& pointInRootViewCoordinates, WebCore::IntPoint& pointInPluginCoordinates);
 
+    virtual bool getResourceData(const unsigned char*& bytes, unsigned& length) const OVERRIDE { return false; }
+
     void updateNPNPrivateMode();
 
 #if PLUGIN_ARCHITECTURE(WIN)
--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginProxy.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/PluginProxy.h
@@ -134,6 +134,8 @@ private:
 
     virtual WebCore::IntPoint convertToRootView(const WebCore::IntPoint&) const OVERRIDE;
 
+    virtual bool getResourceData(const unsigned char*& bytes, unsigned& length) const OVERRIDE { return false; }
+
     float contentsScaleFactor();
     bool needsBackingStore() const;
     bool updateBackingStore();
