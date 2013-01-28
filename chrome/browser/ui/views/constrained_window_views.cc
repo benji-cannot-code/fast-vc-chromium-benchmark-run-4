53161069605c2f5c30fda2d6c2593b1e76fd7282
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-28 18:00:25 +0000

    Use DialogFrameView for ConstrainedWindowViews with --enable-new-dialog-style.
    BUG=166075
    TEST=Views constrained windows use the new dialog style with --enable-new-dialog-style.
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12095005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179161 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_views.cc
+++ chrome/browser/ui/views/constrained_window_views.cc
@@ -42,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/client_view.h"
 #include "ui/views/window/dialog_client_view.h"
 #include "ui/views/window/dialog_delegate.h"
+#include "ui/views/window/dialog_frame_view.h"
 #include "ui/views/window/frame_background.h"
 #include "ui/views/window/non_client_view.h"
 #include "ui/views/window/window_resources.h"
@@ -634,10 +635,9 @@ gfx::NativeWindow ConstrainedWindowViews::GetNativeWindow() {
 }
 
 views::NonClientFrameView* ConstrainedWindowViews::CreateNonClientFrameView() {
+  if (views::DialogDelegate::UseNewStyle())
+    return new views::DialogFrameView(widget_delegate()->GetWindowTitle());
 #if defined(USE_ASH)
-  CommandLine* command_line = CommandLine::ForCurrentProcess();
-  if (command_line->HasSwitch(switches::kEnableNewDialogStyle))
-    return ash::Shell::GetInstance()->CreateDefaultNonClientFrameView(this);
   ConstrainedWindowFrameViewAsh* frame = new ConstrainedWindowFrameViewAsh;
   frame->Init(this);
   return frame;
