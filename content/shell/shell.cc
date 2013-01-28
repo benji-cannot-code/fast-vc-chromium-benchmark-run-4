b399572d271a6294d78283f41c2a1cf261123044
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-28 10:39:23 +0000

    [content shell] when closing all windows, first close all devtools windows
    BUG=111316
    R=marja@chromium.org
    TEST=less crashes on inspector tests
    
    Review URL: https://codereview.chromium.org/12082020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179124 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell.cc
+++ content/shell/shell.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_number_conversions.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
+#include "content/public/browser/devtools_manager.h"
 #include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/notification_details.h"
@@ -103,6 +104,7 @@ Shell* Shell::CreateShell(WebContents* web_contents) {
 
 void Shell::CloseAllWindows() {
   base::AutoReset<bool> auto_reset(&quit_message_loop_, false);
+  DevToolsManager::GetInstance()->CloseAllClientHosts();
   std::vector<Shell*> open_windows(windows_);
   for (size_t i = 0; i < open_windows.size(); ++i)
     open_windows[i]->Close();
