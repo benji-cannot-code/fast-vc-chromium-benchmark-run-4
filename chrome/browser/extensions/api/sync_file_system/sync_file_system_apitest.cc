d5c39581220259de0556552189330cfae91be09b
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-28 12:52:21 +0000

    Enable syncFileSystem API in Dev channel
    BUG=172567
    TEST=browser_tests:SyncFileSystemApiTest.*
    
    
    Review URL: https://chromiumcodereview.appspot.com/12082019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/sync_file_system/sync_file_system_apitest.cc
+++ chrome/browser/extensions/api/sync_file_system/sync_file_system_apitest.cc
@@ -11,8 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync_file_system/local_change_processor.h"
 #include "chrome/browser/sync_file_system/mock_remote_file_sync_service.h"
 #include "chrome/browser/sync_file_system/sync_file_system_service.h"
-#include "chrome/common/chrome_version_info.h"
-#include "chrome/common/extensions/features/feature.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "webkit/fileapi/file_system_url.h"
@@ -31,11 +29,7 @@ namespace {
 
 class SyncFileSystemApiTest : public ExtensionApiTest {
  public:
-  // Override the current channel to "trunk" as syncFileSystem is currently
-  // available only on trunk channel.
-  SyncFileSystemApiTest()
-      : current_channel_(VersionInfo::CHANNEL_UNKNOWN) {
-  }
+  SyncFileSystemApiTest() {}
 
   void SetUpInProcessBrowserTestFixture() OVERRIDE {
     mock_remote_service_ = new ::testing::NiceMock<MockRemoteFileSyncService>;
@@ -58,7 +52,6 @@ class SyncFileSystemApiTest : public ExtensionApiTest {
   }
 
  private:
-  extensions::Feature::ScopedCurrentChannel current_channel_;
   ::testing::NiceMock<MockRemoteFileSyncService>* mock_remote_service_;
   int64 real_default_quota_;
 };
@@ -144,4 +137,3 @@ IN_PROC_BROWSER_TEST_F(SyncFileSystemApiTest, WriteFileThenGetUsage) {
 #endif  // !defined(OS_CHROMEOS)
 
 }  // namespace chrome
-
