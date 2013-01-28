971fc2b247a53b3e1c593a9a9c90adfc1887816c
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-28 18:53:59 +0000

    Prevent decoding of unsupported channel layouts.
    FFmpeg may sometimes return an invalid channel count which overflows
    the AVFrame::extended_data[] array.  While this is a bug in FFmpeg
    and should be fixed upstream, some sanity checking of the values
    returned is prudent.
    
    BUG=171962
    TEST=No ASAN failures.
    
    Review URL: https://codereview.chromium.org/11953088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179173 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/audio_file_reader_unittest.cc
+++ media/filters/audio_file_reader_unittest.cc
@@ -74,6 +74,11 @@ class AudioFileReaderTest : public testing::Test {
     ReadAndVerify(hash, trimmed_frames);
   }
 
+  void RunFailingTest(const char* fn) {
+    Initialize(fn);
+    EXPECT_FALSE(reader_->Open());
+  }
+
  protected:
   scoped_refptr<DecoderBuffer> data_;
   scoped_ptr<InMemoryUrlProtocol> protocol_;
@@ -87,8 +92,7 @@ TEST_F(AudioFileReaderTest, WithoutOpen) {
 }
 
 TEST_F(AudioFileReaderTest, InvalidFile) {
-  Initialize("ten_byte_file");
-  ASSERT_FALSE(reader_->Open());
+  RunFailingTest("ten_byte_file");
 }
 
 TEST_F(AudioFileReaderTest, WithVideo) {
@@ -132,5 +136,9 @@ TEST_F(AudioFileReaderTest, AAC) {
           base::TimeDelta::FromMicroseconds(312001), 13759, 13312);
 }
 #endif
+
+TEST_F(AudioFileReaderTest, VorbisInvalidChannelLayout) {
+  RunFailingTest("9ch.ogg");
+}
 
 }  // namespace media
