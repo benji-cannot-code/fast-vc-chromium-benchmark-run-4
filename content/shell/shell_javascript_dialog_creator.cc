926ce93859d1bce67d8897d20a4d49938b54aff5
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-28 15:24:23 +0000

    [content shell] remove obsolete message printing code
    BUG=111316
    TEST=nothing should change
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/12091022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179151 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_javascript_dialog_creator.cc
+++ content/shell/shell_javascript_dialog_creator.cc
@@ -14,9 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/shell_switches.h"
 #include "content/shell/webkit_test_controller.h"
 #include "net/base/net_util.h"
-// TODO(jochen): Remove this include again after the next WebKit roll. We need
-// it to check for the TEST_RUNNER_MOVED_PRINTING define.
-#include "third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h"
 
 namespace content {
 
@@ -36,17 +33,6 @@ void ShellJavaScriptDialogCreator::RunJavaScriptDialog(
     const DialogClosedCallback& callback,
     bool* did_suppress_message) {
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kDumpRenderTree)) {
-#if !defined(TEST_RUNNER_MOVED_PRINTING)
-    WebKitTestResultPrinter* printer = WebKitTestController::Get()->printer();
-    if (javascript_message_type == JAVASCRIPT_MESSAGE_TYPE_ALERT) {
-      printer->AddMessage(std::string("ALERT: ") + UTF16ToUTF8(message_text));
-    } else if (javascript_message_type == JAVASCRIPT_MESSAGE_TYPE_CONFIRM) {
-      printer->AddMessage(std::string("CONFIRM: ") + UTF16ToUTF8(message_text));
-    } else {  // JAVASCRIPT_MESSAGE_TYPE_PROMPT
-      printer->AddMessage(std::string("PROMPT: ") + UTF16ToUTF8(message_text) +
-                         "default text: " + UTF16ToUTF8(default_prompt_text));
-    }
-#endif
     callback.Run(true, string16());
     return;
   }
@@ -92,11 +78,6 @@ void ShellJavaScriptDialogCreator::RunBeforeUnloadDialog(
     bool is_reload,
     const DialogClosedCallback& callback) {
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kDumpRenderTree)) {
-#if !defined(TEST_RUNNER_MOVED_PRINTING)
-    WebKitTestResultPrinter* printer = WebKitTestController::Get()->printer();
-    printer->AddMessage(
-        std::string("CONFIRM NAVIGATION: ") + UTF16ToUTF8(message_text));
-#endif
     WebKitTestController* controller = WebKitTestController::Get();
     callback.Run(
         !controller->should_stay_on_page_after_handling_before_unload(),
