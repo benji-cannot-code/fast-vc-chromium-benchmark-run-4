benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit b6f713587f10ea574a4818d467feb63be9544c30

Allow multiple WebUIControllerFactory objects to be registered. This makes is possible to implement webui inside content.
BUG=169170
Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=175822
Review URL: https://codereview.chromium.org/11783038

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179107 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chrome_content_browser_client.cc
@@ -732,11 +732,6 @@ void ChromeContentBrowserClient::RenderProcessHostDeleted(
     instant_service->RemoveInstantProcess(host->GetID());
 }
 
-content::WebUIControllerFactory*
-    ChromeContentBrowserClient::GetWebUIControllerFactory() {
-  return ChromeWebUIControllerFactory::GetInstance();
-}
-
 GURL ChromeContentBrowserClient::GetEffectiveURL(
     content::BrowserContext* browser_context, const GURL& url) {
   Profile* profile = Profile::FromBrowserContext(browser_context);

chrome/test/base/chrome_test_suite.cc
@@ -20,11 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_content_browser_client.h"
+#include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/testing_browser_process.h"
+#include "content/public/test/test_launcher.h"
 #include "net/base/mock_host_resolver.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
@@ -213,6 +215,16 @@ void ChromeTestSuite::Initialize() {
     PathService::Override(base::DIR_EXE, browser_dir_);
     PathService::Override(base::DIR_MODULE, browser_dir_);
   }
+
+#if !defined(OS_IOS)
+  if (!content::GetCurrentTestLauncherDelegate()) {
+    // Only want to do this for unit tests. For browser tests, this won't create
+    // the right object since TestChromeWebUIControllerFactory is used. That's
+    // created and registered in ChromeBrowserMainParts as in normal startup.
+    content::WebUIControllerFactory::RegisterFactory(
+        ChromeWebUIControllerFactory::GetInstance());
+  }
+#endif
 
   // Disable external libraries load if we are under python process in
   // ChromeOS.  That means we are autotest and, if ASAN is used,

chrome/browser/chrome_browser_main.cc
@@ -92,6 +92,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/uma_browsing_activity_observer.h"
 #include "chrome/browser/ui/user_data_dir_dialog.h"
+#include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "chrome/common/child_process_logging.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths.h"
@@ -1389,6 +1390,9 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
   std::vector<Profile*> last_opened_profiles =
       g_browser_process->profile_manager()->GetLastOpenedProfiles();
 #endif
+
+  content::WebUIControllerFactory::RegisterFactory(
+      ChromeWebUIControllerFactory::GetInstance());
 
   if (browser_creator_->Start(parsed_command_line(), FilePath(),
                               profile_, last_opened_profiles, &result_code)) {

chrome/browser/chrome_content_browser_client.h
@@ -67,7 +67,6 @@ class ChromeContentBrowserClient : public content::ContentBrowserClient {
       content::RenderProcessHost* host) OVERRIDE;
   virtual void RenderProcessHostDeleted(
       content::RenderProcessHost* host) OVERRIDE;
-  virtual content::WebUIControllerFactory* GetWebUIControllerFactory() OVERRIDE;
   virtual bool ShouldUseProcessPerSite(content::BrowserContext* browser_context,
                                        const GURL& effective_url) OVERRIDE;
   virtual GURL GetEffectiveURL(content::BrowserContext* browser_context,

chrome/browser/devtools/devtools_file_helper.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/url_constants.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
@@ -134,7 +135,7 @@ std::string RegisterFileSystem(WebContents* web_contents,
                                const FilePath& path,
                                std::string* registered_name) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  CHECK(content::GetContentClient()->HasWebUIScheme(web_contents->GetURL()));
+  CHECK(content::HasWebUIScheme(web_contents->GetURL()));
   std::string file_system_id = isolated_context()->RegisterFileSystemForPath(
       fileapi::kFileSystemTypeNativeLocal, path, registered_name);
 

chrome/browser/devtools/devtools_window.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/common/url_constants.h"
-#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/devtools_agent_host.h"
 #include "content/public/browser/devtools_manager.h"
 #include "content/public/browser/favicon_status.h"
@@ -50,6 +49,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/page_transition_types.h"
+#include "content/public/common/url_constants.h"
 #include "grit/generated_resources.h"
 
 typedef std::vector<DevToolsWindow*> DevToolsWindowList;
@@ -853,19 +853,19 @@ DictionaryValue* CreateFileSystemValue(
 } // namespace
 
 void DevToolsWindow::RequestFileSystems() {
-  CHECK(content::GetContentClient()->HasWebUIScheme(web_contents_->GetURL()));
+  CHECK(content::HasWebUIScheme(web_contents_->GetURL()));
   file_helper_->RequestFileSystems(
       Bind(&DevToolsWindow::FileSystemsLoaded, weak_factory_.GetWeakPtr()));
 }
 
 void DevToolsWindow::AddFileSystem() {
-  CHECK(content::GetContentClient()->HasWebUIScheme(web_contents_->GetURL()));
+  CHECK(content::HasWebUIScheme(web_contents_->GetURL()));
   file_helper_->AddFileSystem(
       Bind(&DevToolsWindow::FileSystemAdded, weak_factory_.GetWeakPtr()));
 }
 
 void DevToolsWindow::RemoveFileSystem(const std::string& file_system_path) {
-  CHECK(content::GetContentClient()->HasWebUIScheme(web_contents_->GetURL()));
+  CHECK(content::HasWebUIScheme(web_contents_->GetURL()));
   file_helper_->RemoveFileSystem(file_system_path);
   StringValue file_system_path_value(file_system_path);
   CallClientFunction("InspectorFrontendAPI.fileSystemRemoved",

chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/common/content_client.h"
+#include "content/public/common/url_constants.h"
 #include "extensions/common/error_utils.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
@@ -429,8 +430,7 @@ bool DebuggerFunction::InitWebContents() {
   }
   contents_ = web_contents;
 
-  if (content::GetContentClient()->HasWebUIScheme(
-          contents_->GetURL())) {
+  if (content::HasWebUIScheme(contents_->GetURL())) {
     error_ = ErrorUtils::FormatErrorMessage(
         keys::kAttachToWebUIError,
         contents_->GetURL().scheme());

chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -37,9 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/local_omnibox_popup/local_omnibox_popup_ui.h"
 #include "chrome/browser/ui/webui/media/media_internals_ui.h"
 #include "chrome/browser/ui/webui/media/webrtc_internals_ui.h"
-#if !defined(DISABLE_NACL)
-#include "chrome/browser/ui/webui/nacl_ui.h"
-#endif
 #include "chrome/browser/ui/webui/net_internals/net_internals_ui.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
 #include "chrome/browser/ui/webui/omnibox/omnibox_ui.h"
@@ -68,6 +65,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "googleurl/src/gurl.h"
 #include "ui/gfx/favicon_size.h"
 #include "ui/web_dialogs/web_dialog_ui.h"
+
+#if !defined(DISABLE_NACL)
+#include "chrome/browser/ui/webui/nacl_ui.h"
+#endif
 
 #if defined(ENABLE_CONFIGURATION_POLICY)
 #include "chrome/browser/ui/webui/policy_ui.h"
@@ -565,7 +566,7 @@ base::RefCountedMemory* ChromeWebUIControllerFactory::GetFaviconResourceBytes(
   }
 #endif
 
-  if (!content::GetContentClient()->HasWebUIScheme(page_url))
+  if (!content::HasWebUIScheme(page_url))
     return NULL;
 
 #if defined(OS_WIN)

commit 1a973cc044b314322e46664a4462e685c13ce318

Register ChromeWebUIControllerFactory at ChromeContentBrowserClient to try to fix the broken vmtests on cros after r179107.
BUG=169170
Review URL: https://codereview.chromium.org/12087021

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179113 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/chrome_content_browser_client.cc
@@ -488,6 +488,10 @@ namespace chrome {
 ChromeContentBrowserClient::ChromeContentBrowserClient() {
   for (size_t i = 0; i < arraysize(kPredefinedAllowedSocketOrigins); ++i)
     allowed_socket_origins_.insert(kPredefinedAllowedSocketOrigins[i]);
+
+  // Needs to be done this early, not in ChromeBrowserMain, for CrOS.
+  content::WebUIControllerFactory::RegisterFactory(
+        ChromeWebUIControllerFactory::GetInstance());
 }
 
 ChromeContentBrowserClient::~ChromeContentBrowserClient() {

chrome/test/base/chrome_test_suite.cc
@@ -20,13 +20,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/chrome_content_browser_client.h"
-#include "chrome/browser/ui/webui/chrome_web_ui_controller_factory.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_paths.h"
 #include "chrome/common/url_constants.h"
 #include "chrome/test/base/testing_browser_process.h"
-#include "content/public/test/test_launcher.h"
 #include "net/base/mock_host_resolver.h"
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
@@ -215,16 +213,6 @@ void ChromeTestSuite::Initialize() {
     PathService::Override(base::DIR_EXE, browser_dir_);
     PathService::Override(base::DIR_MODULE, browser_dir_);
   }
-
-#if !defined(OS_IOS)
-  if (!content::GetCurrentTestLauncherDelegate()) {
-    // Only want to do this for unit tests. For browser tests, this won't create
-    // the right object since TestChromeWebUIControllerFactory is used. That's
-    // created and registered in ChromeBrowserMainParts as in normal startup.
-    content::WebUIControllerFactory::RegisterFactory(
-        ChromeWebUIControllerFactory::GetInstance());
-  }
-#endif
 
   // Disable external libraries load if we are under python process in
   // ChromeOS.  That means we are autotest and, if ASAN is used,