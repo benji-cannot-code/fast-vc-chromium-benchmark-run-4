495d5001d4da789273d2de32375bee181c52b398
michael.bruning@digia.com <michael.bruning@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-29 09:41:31 +0000

    [Qt][WK1] Reflect recursion limit and loop checks also for list conversions. https://bugs.webkit.org/show_bug.cgi?id=107950
    Reviewed by Allan Sandfeld Jensen.
    
    No new tests, bugfix, no behavioral change.
    
    Make conversions from Javascript values to QLists take the maximum
    recursion depth into consideration and check for objects that were
    already visited. Otherwise, the conversion may recurse until the
    stack is full and then cause a segmentation fault.
    
    * bridge/qt/qt_runtime.cpp:
    (JSC::Bindings::convertToList):
    (JSC::Bindings::convertValueToQVariant):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141085 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bridge/qt/qt_runtime.cpp
+++ third_party/WebKit/Source/WebCore/bridge/qt/qt_runtime.cpp
@@ -237,7 +237,7 @@ static QVariantMap convertValueToQVariantMap(JSContextRef context, JSObjectRef o
 
 template <typename ItemType>
 QList<ItemType> convertToList(JSContextRef context, JSRealType type, JSObjectRef object,
-                              JSValueRef value, int* distance, JSValueRef* exception,
+                              JSValueRef value, int* distance, HashSet<JSObjectRef>* visitedObjects, int recursionLimit, JSValueRef* exception,
                               const QMetaType::Type typeId = static_cast<QMetaType::Type>(qMetaTypeId<ItemType>()))
 {
     QList<ItemType> list;
@@ -249,7 +249,7 @@ QList<ItemType> convertToList(JSContextRef context, JSRealType type, JSObjectRef
         for (size_t i = 0; i < length; ++i) {
             JSValueRef value = JSObjectGetPropertyAtIndex(context, object, i, exception);
             int itemDistance = -1;
-            QVariant variant = convertValueToQVariant(context, value, typeId, &itemDistance, exception);
+            QVariant variant = convertValueToQVariant(context, value, typeId, &itemDistance, visitedObjects, recursionLimit, exception);
             if (itemDistance >= 0)
                 list << variant.value<ItemType>();
             else
@@ -261,7 +261,7 @@ QList<ItemType> convertToList(JSContextRef context, JSRealType type, JSObjectRef
             *distance = 5;
     } else {
         int itemDistance = -1;
-        QVariant variant = convertValueToQVariant(context, value, typeId, &itemDistance, exception);
+        QVariant variant = convertValueToQVariant(context, value, typeId, &itemDistance, visitedObjects, recursionLimit, exception);
         if (itemDistance >= 0) {
             list << variant.value<ItemType>();
             if (distance)
@@ -482,11 +482,11 @@ QVariant convertValueToQVariant(JSContextRef context, JSValueRef value, QMetaTyp
             break;
 
         case QMetaType::QVariantList:
-            ret = QVariant(convertToList<QVariant>(context, type, object, value, &dist, exception, QMetaType::Void));
+            ret = QVariant(convertToList<QVariant>(context, type, object, value, &dist, visitedObjects, recursionLimit, exception, QMetaType::Void));
             break;
 
         case QMetaType::QStringList: {
-            ret = QVariant(convertToList<QString>(context, type, object, value, &dist, exception));
+            ret = QVariant(convertToList<QString>(context, type, object, value, &dist, visitedObjects, recursionLimit, exception));
             break;
         }
 
@@ -617,11 +617,11 @@ QVariant convertValueToQVariant(JSContextRef context, JSValueRef value, QMetaTyp
         default:
             // Non const type ids
             if (hint == (QMetaType::Type) qMetaTypeId<QObjectList>()) {
-                ret = QVariant::fromValue(convertToList<QObject*>(context, type, object, value, &dist, exception));
+                ret = QVariant::fromValue(convertToList<QObject*>(context, type, object, value, &dist, visitedObjects, recursionLimit, exception));
                 break;
             }
             if (hint == (QMetaType::Type) qMetaTypeId<QList<int> >()) {
-                ret = QVariant::fromValue(convertToList<int>(context, type, object, value, &dist, exception));
+                ret = QVariant::fromValue(convertToList<int>(context, type, object, value, &dist, visitedObjects, recursionLimit, exception));
                 break;
             }
             if (QtPixmapRuntime::canHandle(static_cast<QMetaType::Type>(hint))) {
