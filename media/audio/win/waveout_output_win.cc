b5929b04bedac5296eb9955affd171f33db17e62
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 23:48:10 +0000

    Avoid MemoryBarrier definition discrepencies between win32 and win64
    BUG=172904
    TBR=scherkus@chromium.org
    R=scherkus@chromium.org
    R=wolenetz@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12091044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179469 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/waveout_output_win.cc
+++ media/audio/win/waveout_output_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <mmsystem.h>
 #pragma comment(lib, "winmm.lib")
 
+#include "base/atomicops.h"
 #include "base/basictypes.h"
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
@@ -216,7 +217,7 @@ void PCMWaveOutAudioOutputStream::Start(AudioSourceCallback* callback) {
   // From now on |pending_bytes_| would be accessed by callback thread.
   // Most likely waveOutPause() or waveOutRestart() has its own memory barrier,
   // but issuing our own is safer.
-  MemoryBarrier();
+  base::subtle::MemoryBarrier();
 
   MMRESULT result = ::waveOutPause(waveout_);
   if (result != MMSYSERR_NOERROR) {
@@ -251,7 +252,7 @@ void PCMWaveOutAudioOutputStream::Stop() {
   if (state_ != PCMA_PLAYING)
     return;
   state_ = PCMA_STOPPING;
-  MemoryBarrier();
+  base::subtle::MemoryBarrier();
 
   // Stop watching for buffer event, wait till all the callbacks are complete.
   // Should be done before ::waveOutReset() call to avoid race condition when
