778f1c76aeb9c793b322dc0ef8dbf2359b6a41d2
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 01:24:18 +0000

    cc: Put orange debug border around PicureLayers.
    This is like tiled layers, since they are the replacement for tiled layers.
    
    R=enne,nduca
    BUG=172707
    
    
    Review URL: https://chromiumcodereview.appspot.com/12082031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179238 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_layer_impl.cc
+++ cc/picture_layer_impl.cc
@@ -85,6 +85,8 @@ void PictureLayerImpl::appendQuads(QuadSink& quadSink,
 
   SharedQuadState* sharedQuadState =
       quadSink.useSharedQuadState(createSharedQuadState());
+  appendDebugBorderQuad(quadSink, sharedQuadState, appendQuadsData);
+
   bool clipped = false;
   gfx::QuadF target_quad = MathUtil::mapQuad(
       drawTransform(),
@@ -581,6 +583,12 @@ void PictureLayerImpl::CleanUpUnusedTilings(
 
   for (size_t i = 0; i < to_remove.size(); ++i)
     tilings_->Remove(to_remove[i]);
+}
+
+void PictureLayerImpl::getDebugBorderProperties(
+    SkColor* color, float* width) const {
+  *color = DebugColors::TiledContentLayerBorderColor();
+  *width = DebugColors::TiledContentLayerBorderWidth(layerTreeImpl());
 }
 
 }  // namespace cc
