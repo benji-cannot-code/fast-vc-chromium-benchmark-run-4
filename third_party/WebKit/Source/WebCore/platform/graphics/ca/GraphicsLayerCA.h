fdc8070a9d8f2a59d0c3279ecb1406d23134888f
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-29 03:42:59 +0000

    Avoid doing work at 60fps for tiled layers when not necessary https://bugs.webkit.org/show_bug.cgi?id=108135
    Reviewed by Dean Jackson.
    
    When there were any tiled layers on the page, we would run a CVDisplayLink
    to cause GraphicsLayerCA to flush, in order to update tiled layer visible rects.
    This is overkill; we should only do this if the tiled layer is affected by
    an accelerated animation.
    
    Fix by tracking whether an ancestor has a running animation when committing
    GraphicsLayerCAs.
    
    * platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::flushCompositingState): Start with an empty CommitState.
    (WebCore::GraphicsLayerCA::recursiveCommitChanges): Push CommitState for
    each layer, which tracks whether an ancestor has a running transform animation.
    (WebCore::GraphicsLayerCA::commitLayerChangesBeforeSublayers): updateLayerAnimations() renamed.
    (WebCore::GraphicsLayerCA::updateAnimations): Renamed from updateLayerAnimations().
    (WebCore::GraphicsLayerCA::isRunningTransformAnimation): Look in the map of running
    animations for one affecting transform. This list is normally small (one item).
    * platform/graphics/ca/GraphicsLayerCA.h:
    (WebCore::GraphicsLayerCA::CommitState::CommitState):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141036 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -132,7 +132,13 @@ public:
     virtual void setMaintainsPixelAlignment(bool);
     virtual void deviceOrPageScaleFactorChanged();
 
-    void recursiveCommitChanges(const TransformState&, float pageScaleFactor = 1, const FloatPoint& positionRelativeToBase = FloatPoint(), bool affectedByPageScale = false);
+    struct CommitState {
+        bool ancestorHasTransformAnimation;
+        CommitState()
+            : ancestorHasTransformAnimation(false)
+        { }
+    };
+    void recursiveCommitChanges(const CommitState&, const TransformState&, float pageScaleFactor = 1, const FloatPoint& positionRelativeToBase = FloatPoint(), bool affectedByPageScale = false);
 
     virtual void flushCompositingState(const FloatRect&);
     virtual void flushCompositingStateForThisLayerOnly();
@@ -208,6 +214,8 @@ private:
     bool setFilterAnimationEndpoints(const KeyframeValueList&, const Animation*, PlatformCAAnimation*, int functionIndex, int internalFilterPropertyIndex);
     bool setFilterAnimationKeyframes(const KeyframeValueList&, const Animation*, PlatformCAAnimation*, int functionIndex, int internalFilterPropertyIndex, FilterOperation::OperationType);
 #endif
+
+    bool isRunningTransformAnimation() const;
 
     bool animationIsRunning(const String& animationName) const
     {
@@ -327,7 +335,7 @@ private:
     void updateMaskLayer();
     void updateReplicatedLayers();
 
-    void updateLayerAnimations();
+    void updateAnimations();
     void updateContentsNeedsDisplay();
     void updateAcceleratesDrawing();
     void updateDebugBorder();
