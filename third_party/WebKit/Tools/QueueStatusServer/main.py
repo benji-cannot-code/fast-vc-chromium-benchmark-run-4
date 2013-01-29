0f8c4995296b7ac26cd287387862759ad994d675
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-29 18:07:52 +0000

    QueueStatusServer needs pages to display historical queue data https://bugs.webkit.org/show_bug.cgi?id=107659
    Patch by Alan Cutter <alancutter@chromium.org> on 2013-01-29
    Reviewed by Eric Seidel.
    
    Created a /queue-charts/<queue-name> handler to present queue and patch data using Google Chart Tools.
    
    * QueueStatusServer/app.yaml:
    * QueueStatusServer/config/charts.py: Copied from Tools/QueueStatusServer/model/queuelog.py.
    (get_time_unit):
    * QueueStatusServer/filters/webkit_extras.py:
    (webkit_linkify):
    (webkit_bug_id):
    (webkit_attachment_id):
    (results_link):
    (queue_status_link):
    (queue_charts_link):
    * QueueStatusServer/handlers/queuecharts.py: Added.
    (QueueCharts):
    (QueueCharts.get):
    (QueueCharts._get_min_med_max):
    (QueueCharts._get_patch_data):
    (QueueCharts._get_patch_logs):
    (QueueCharts._get_queue_data):
    (QueueCharts._get_queue_logs):
    (QueueCharts._get_time_unit):
    (QueueCharts._get_timestamp):
    (QueueCharts._get_view_range):
    * QueueStatusServer/handlers/queuestatus.py:
    (QueueStatus.get):
    * QueueStatusServer/index.yaml:
    * QueueStatusServer/main.py:
    * QueueStatusServer/model/queuelog.py:
    (QueueLog):
    (QueueLog.create_key):
    (QueueLog.get_at):
    (QueueLog.get_current):
    (QueueLog.get_or_create):
    (QueueLog._get_or_create_txn):
    * QueueStatusServer/stylesheets/charts.css: Added.
    (.chart):
    (.choices):
    * QueueStatusServer/templates/queuecharts.html: Added.
    * QueueStatusServer/templates/queuestatus.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/QueueStatusServer/main.py
+++ third_party/WebKit/Tools/QueueStatusServer/main.py
@@ -40,6 +40,7 @@ from handlers.gc import GC
 from handlers.nextpatch import NextPatch
 from handlers.patch import Patch
 from handlers.patchstatus import PatchStatus
+from handlers.queuecharts import QueueCharts
 from handlers.queuestatus import QueueStatus
 from handlers.recentstatus import QueuesOverview
 from handlers.releasepatch import ReleasePatch
@@ -64,6 +65,7 @@ routes = [
     (r'/results/(.*)', ShowResults),
     (r'/status-bubble/(.*)', StatusBubble),
     (r'/svn-revision/(.*)', SVNRevision),
+    (r'/queue-charts/(.*)', QueueCharts),
     (r'/queue-status/(.*)/bots/(.*)', QueueStatus),
     (r'/queue-status/(.*)', QueueStatus),
     (r'/next-patch/(.*)', NextPatch),
