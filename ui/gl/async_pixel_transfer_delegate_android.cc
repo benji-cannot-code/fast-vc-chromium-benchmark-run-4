8f1003dce132429094603ca1ab3d31134703e69d
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 23:30:14 +0000

    gpu: Fix async upload race on Qualcomm.
    Take 2. The fence doesn't work on all devices but glFinish
    seems to. I'll talk to Qualcomm to see if this is even
    a valid work-around, but so far so good.
    
    BUG=172321
    
    
    Review URL: https://chromiumcodereview.appspot.com/12079038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179461 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/async_pixel_transfer_delegate_android.cc
+++ ui/gl/async_pixel_transfer_delegate_android.cc
@@ -145,7 +145,6 @@ class TransferStateInternal
         needs_late_bind_(false),
         transfer_in_progress_(false),
         egl_image_(EGL_NO_IMAGE_KHR),
-        egl_image_created_sync_(EGL_NO_SYNC_KHR),
         wait_for_uploads_(wait_for_uploads),
         wait_for_egl_images_(wait_for_egl_images) {
     static const AsyncTexImage2DParams zero_params = {0, 0, 0, 0, 0, 0, 0, 0};
@@ -183,7 +182,6 @@ class TransferStateInternal
     TRACE_EVENT0("gpu", "eglCreateImageKHR");
     DCHECK(texture_id);
     DCHECK_EQ(egl_image_, EGL_NO_IMAGE_KHR);
-    DCHECK_EQ(egl_image_created_sync_, EGL_NO_SYNC_KHR);
 
     EGLDisplay egl_display = eglGetCurrentDisplay();
     EGLContext egl_context = eglGetCurrentContext();
@@ -201,10 +199,6 @@ class TransferStateInternal
         egl_target,
         egl_buffer,
         egl_attrib_list);
-
-    if (wait_for_egl_images_)
-      egl_image_created_sync_ =
-          eglCreateSyncKHR(egl_display, EGL_SYNC_FENCE_KHR, NULL);
   }
 
   void CreateEglImageOnUploadThread() {
@@ -219,14 +213,11 @@ class TransferStateInternal
   void WaitOnEglImageCreation() {
     // On Qualcomm, we need to wait on egl image creation before
     // doing the first upload (or the texture remains black).
-    if (egl_image_created_sync_ != EGL_NO_SYNC_KHR) {
-      TRACE_EVENT0("gpu", "eglWaitSync");
-      EGLint flags = EGL_SYNC_FLUSH_COMMANDS_BIT_KHR;
-      EGLTimeKHR time = EGL_FOREVER_KHR;
-      EGLDisplay display = eglGetCurrentDisplay();
-      eglClientWaitSyncKHR(display, egl_image_created_sync_, flags, time);
-      eglDestroySyncKHR(display, egl_image_created_sync_);
-      egl_image_created_sync_ = EGL_NO_SYNC_KHR;
+    // A fence after image creation didn't always work, but glFinish
+    // seems to always work, and this only happens on the upload thread.
+    if (wait_for_egl_images_) {
+      TRACE_EVENT0("gpu", "glFinish");
+      glFinish();
     }
   }
 
@@ -260,10 +251,6 @@ class TransferStateInternal
       EGLDisplay display = eglGetCurrentDisplay();
       eglDestroyImageKHR(display, egl_image_);
     }
-    if (egl_image_created_sync_ != EGL_NO_SYNC_KHR) {
-      EGLDisplay display = eglGetCurrentDisplay();
-      eglDestroySyncKHR(display, egl_image_created_sync_);
-    }
     if (thread_texture_id_) {
       transfer_message_loop_proxy()->PostTask(FROM_HERE,
           base::Bind(&DeleteTexture, thread_texture_id_));
@@ -290,7 +277,6 @@ class TransferStateInternal
   // every upload, but I found that didn't work, so this stores
   // one for the lifetime of the texture.
   EGLImageKHR egl_image_;
-  EGLSyncKHR egl_image_created_sync_;
 
   // Time spent performing last transfer.
   base::TimeDelta last_transfer_time_;
