5008df1edb18ba072576ab03ac0d139273263558
ilevy@chromium.org <ilevy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 02:30:17 +0000

    Move andorid webkit tests to main waterfall and CQ
    They have been stable and fast on FYI bots for a week.
    
    TBR=yfriedman@chromium.org
    
    Review URL: https://codereview.chromium.org/12093034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179266 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/gtest/gtest_config.py
+++ build/android/pylib/gtest/gtest_config.py
@@ -7,15 +7,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 # Add new suites here before upgrading them to the stable list below.
 EXPERIMENTAL_TEST_SUITES = [
-    'TestWebKitAPI',
     'sandbox_linux_unittests',
-    'webkit_unit_tests',
 ]
 
 # Do not modify this list without approval of an android owner.
 # This list determines which suites are run by default, both for local
 # testing and on android trybots running on commit-queue.
 STABLE_TEST_SUITES = [
+    'TestWebKitAPI',
     'android_webview_unittests',
     'base_unittests',
     'cc_unittests',
@@ -30,4 +29,5 @@ STABLE_TEST_SUITES = [
     'ui_unittests',
     'unit_tests',
     'webkit_compositor_bindings_unittests',
+    'webkit_unit_tests',
 ]
