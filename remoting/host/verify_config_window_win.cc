d7d3a4d08df60ba117b92e83d0fa05a40282ce13
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-29 00:42:25 +0000

    Merged all Chromoting Host code into remoting_core.dll (Windows).
    Consolidated all installable Chromoting Host core into remoting_core.dll and converted all executables into thin wrappers around entry points exposed by remoting_core.dll. This reduces size of the installer by approximately 600KB.
    
    BUG=170200
    
    
    Review URL: https://chromiumcodereview.appspot.com/11970044
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179226 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/verify_config_window_win.cc
+++ remoting/host/verify_config_window_win.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/utf_string_conversions.h"
 #include "remoting/host/pin_hash.h"
-#include "remoting/host/win/elevated_controller_resource.h"
+#include "remoting/host/win/core_resource.h"
 #include "remoting/protocol/authentication_method.h"
 
 namespace remoting {
