9810da6eab0df862edd0e0ff9fa485618d4891a5
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-29 05:40:11 +0000

    IndexedDB: Pass metadata in to IDBOpenDBRequest.onUpgradeNeeded/onSuccess https://bugs.webkit.org/show_bug.cgi?id=103920
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Update IDBCallbacks::onSuccess and IDBCallbacks::onUpgradeNeeded to
    pass through a metadata parameter. While there, remove the unused
    IDBTransactionBackendInterface parameter to onUpgradeNeeded.
    
    As this is another step in the IDB refactor, I've simplified future cleanup
    work by making the WebKit API code still use the old API. This
    will make it possible to outright remove code on the chromium side rather
    than another three-step checkin.
    
    No new tests, as this is more refactoring.
    
    * Modules/indexeddb/IDBCallbacks.h:
    (WebCore::IDBCallbacks::onUpgradeNeeded): new method signature.
    (WebCore::IDBCallbacks::onSuccess): new method signature.
    * Modules/indexeddb/IDBDatabaseBackendImpl.cpp:
    (WebCore::IDBDatabaseBackendImpl::VersionChangeOperation::perform):
    (WebCore::IDBDatabaseBackendImpl::processPendingCalls):
    (WebCore::IDBDatabaseBackendImpl::openConnection):
    * Modules/indexeddb/IDBOpenDBRequest.cpp:
    (WebCore::IDBOpenDBRequest::onUpgradeNeeded): use passed-in metadata.
    (WebCore::IDBOpenDBRequest::onSuccess): use passed-in metadata.
    * Modules/indexeddb/IDBOpenDBRequest.h:
    (IDBOpenDBRequest):
    
    Source/WebKit/chromium:
    
    Support the new IDBCallbacks::onSuccess and IDBCallbacks::onUpgradeNeeded
    while maintaining chromium compatibility by shimming in the old API
    in the WebKit side. Future code will clean this up so that it is just a
    pass-through as it was before.
    
    * public/WebIDBCallbacks.h:
    (WebKit):
    (WebKit::WebIDBCallbacks::onSuccess): new method signature.
    (WebKit::WebIDBCallbacks::onUpgradeNeeded): new method signature.
    * src/IDBCallbacksProxy.cpp:
    (WebKit::IDBCallbacksProxy::onSuccess): call on new method signature proxies through old API.
    (WebKit):
    (WebKit::IDBCallbacksProxy::onUpgradeNeeded): call on new method signature proxies through old API.
    * src/IDBCallbacksProxy.h:
    (IDBCallbacksProxy):
    * src/WebIDBCallbacksImpl.cpp:
    (WebKit::WebIDBCallbacksImpl::onSuccess): call on old WebKit proxy signature calls new API.
    (WebKit):
    (WebKit::WebIDBCallbacksImpl::onUpgradeNeeded): call on old WebKit proxy signature calls new API.
    * src/WebIDBCallbacksImpl.h:
    (WebIDBCallbacksImpl):
    * tests/IDBAbortOnCorruptTest.cpp: new method signature.
    (WebCore::MockIDBCallbacks::onSuccess):
    * tests/IDBDatabaseBackendTest.cpp: new method signature.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141049 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/IDBCallbacksProxy.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/IDBCallbacksProxy.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IDBDatabaseBackendProxy.h"
 #include "IDBDatabaseCallbacksProxy.h"
 #include "IDBDatabaseError.h"
+#include "IDBMetadata.h"
 #include "IDBTransactionBackendInterface.h"
 #include "WebIDBCallbacks.h"
 #include "WebIDBCursorImpl.h"
@@ -85,6 +86,14 @@ void IDBCallbacksProxy::onSuccess(PassRefPtr<IDBCursorBackendInterface> idbCurso
 }
 
 void IDBCallbacksProxy::onSuccess(PassRefPtr<IDBDatabaseBackendInterface> backend)
+{
+    ASSERT(m_databaseCallbacks.get());
+    m_didComplete = true;
+    WebIDBDatabaseImpl* impl = m_didCreateProxy ? 0 : new WebIDBDatabaseImpl(backend, m_databaseCallbacks.release());
+    m_callbacks->onSuccess(impl);
+}
+
+void IDBCallbacksProxy::onSuccess(PassRefPtr<IDBDatabaseBackendInterface> backend, const IDBDatabaseMetadata&)
 {
     ASSERT(m_databaseCallbacks.get());
     m_didComplete = true;
@@ -160,8 +169,16 @@ void IDBCallbacksProxy::onBlocked(int64_t existingVersion)
 void IDBCallbacksProxy::onUpgradeNeeded(int64_t oldVersion, PassRefPtr<IDBTransactionBackendInterface> transaction, PassRefPtr<IDBDatabaseBackendInterface> database)
 {
     ASSERT(m_databaseCallbacks);
+    ASSERT(!transaction);
     m_didCreateProxy = true;
-    m_callbacks->onUpgradeNeeded(oldVersion, new WebIDBTransactionImpl(transaction), new WebIDBDatabaseImpl(database, m_databaseCallbacks));
+    m_callbacks->onUpgradeNeeded(oldVersion, 0, new WebIDBDatabaseImpl(database, m_databaseCallbacks));
+}
+
+void IDBCallbacksProxy::onUpgradeNeeded(int64_t oldVersion, PassRefPtr<IDBDatabaseBackendInterface> database, const IDBDatabaseMetadata&)
+{
+    ASSERT(m_databaseCallbacks);
+    m_didCreateProxy = true;
+    m_callbacks->onUpgradeNeeded(oldVersion, 0, new WebIDBDatabaseImpl(database, m_databaseCallbacks));
 }
 
 void IDBCallbacksProxy::setDatabaseCallbacks(PassRefPtr<IDBDatabaseCallbacksProxy> databaseCallbacks)
