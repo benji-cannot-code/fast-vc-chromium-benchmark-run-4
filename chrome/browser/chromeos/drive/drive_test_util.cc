cbb3b3cf7d69b44f043b66cccf0a8c6176f13c83
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 15:22:44 +0000

    Replace the argument of test_util::LoadChangeFeed from DriveFileSystem to DriveFeedLoader.
    By this CL, it is possible to reuse the method without instantiating a
    DriveFileSystem instance in unit tests.
    
    BUG=141596
    TEST=Ran unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/12077042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179637 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_test_util.cc
+++ chrome/browser/chromeos/drive/drive_test_util.cc
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/json/json_file_value_serializer.h"
 #include "base/message_loop.h"
 #include "chrome/browser/chromeos/drive/drive.pb.h"
 #include "chrome/browser/chromeos/drive/drive_feed_loader.h"
-#include "chrome/browser/chromeos/drive/drive_file_system.h"
 #include "chrome/browser/google_apis/drive_api_parser.h"
 
 namespace drive {
@@ -187,7 +187,7 @@ void CopyResultsFromCloseFileCallbackAndQuit(DriveFileError* out_error,
 }
 
 bool LoadChangeFeed(const std::string& relative_path,
-                    DriveFileSystem* file_system,
+                    DriveFeedLoader* feed_loader,
                     bool is_delta_feed,
                     int64 root_feed_changestamp) {
   scoped_ptr<Value> document =
@@ -205,7 +205,7 @@ bool LoadChangeFeed(const std::string& relative_path,
   ScopedVector<google_apis::ResourceList> feed_list;
   feed_list.push_back(document_feed.release());
 
-  file_system->feed_loader()->UpdateFromFeed(
+  feed_loader->UpdateFromFeed(
       feed_list,
       is_delta_feed,
       root_feed_changestamp,
