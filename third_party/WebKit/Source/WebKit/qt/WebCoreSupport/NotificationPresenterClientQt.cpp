6c13d2119c6337fc67f763c44b6fad7bba13ec49
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 09:28:19 +0000

    [Qt][WK1] Remember denied permission for notifications https://bugs.webkit.org/show_bug.cgi?id=107694
    Reviewed by Jocelyn Turcotte.
    
    Store denied permissions. According to the specification, we should
    ask the user again if he has already granted or denied permission.
    
    * WebCoreSupport/NotificationPresenterClientQt.cpp:
    (WebCore::NotificationPresenterClientQt::requestPermission):
    (WebCore::NotificationPresenterClientQt::setNotificationsAllowedForFrame):
    * WebCoreSupport/NotificationPresenterClientQt.h:
    (NotificationPresenterClientQt):
    * WebCoreSupport/QWebPageAdapter.cpp:
    (QWebPageAdapter::setNotificationsAllowedForFrame):
    * WebCoreSupport/QWebPageAdapter.h:
    * WidgetApi/qwebpage.cpp:
    (QWebPage::setFeaturePermission):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141233 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/NotificationPresenterClientQt.cpp
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/NotificationPresenterClientQt.cpp
@@ -311,6 +311,13 @@ void NotificationPresenterClientQt::requestPermission(ScriptExecutionContext* co
     if (dumpNotification)
         printf("DESKTOP NOTIFICATION PERMISSION REQUESTED: %s\n", QString(context->securityOrigin()->toString()).toUtf8().constData());
 
+    NotificationClient::Permission permission = checkPermission(context);
+    if (permission != NotificationClient::PermissionNotAllowed) {
+        if (callback)
+            callback->handleEvent();
+        return;
+    }
+
     QHash<ScriptExecutionContext*, CallbacksInfo >::iterator iter = m_pendingPermissionRequests.find(context);
     if (iter != m_pendingPermissionRequests.end())
         iter.value().m_voidCallbacks.append(callback);
@@ -333,6 +340,13 @@ void NotificationPresenterClientQt::requestPermission(ScriptExecutionContext* co
 {
     if (dumpNotification)
         printf("DESKTOP NOTIFICATION PERMISSION REQUESTED: %s\n", QString(context->securityOrigin()->toString()).toUtf8().constData());
+
+    NotificationClient::Permission permission = checkPermission(context);
+    if (permission != NotificationClient::PermissionNotAllowed) {
+        if (callback)
+            callback->handleEvent(Notification::permissionString(permission));
+        return;
+    }
 
     QHash<ScriptExecutionContext*, CallbacksInfo >::iterator iter = m_pendingPermissionRequests.find(context);
     if (iter != m_pendingPermissionRequests.end())
@@ -379,9 +393,14 @@ void NotificationPresenterClientQt::cancelRequestsForPermission(ScriptExecutionC
     page->notificationsPermissionRequestCancelled(frame);
 }
 
-void NotificationPresenterClientQt::allowNotificationForFrame(Frame* frame)
+void NotificationPresenterClientQt::setNotificationsAllowedForFrame(Frame* frame, bool allowed)
 {
-    m_cachedPermissions.insert(frame->document(), NotificationClient::PermissionAllowed);
+    ASSERT(frame->document());
+    if (!frame->document())
+        return;
+
+    NotificationClient::Permission permission = allowed ? NotificationClient::PermissionAllowed : NotificationClient::PermissionDenied;
+    m_cachedPermissions.insert(frame->document(), permission);
 
     QHash<ScriptExecutionContext*,  CallbacksInfo>::iterator iter = m_pendingPermissionRequests.begin();
     while (iter != m_pendingPermissionRequests.end()) {
@@ -403,7 +422,7 @@ void NotificationPresenterClientQt::allowNotificationForFrame(Frame* frame)
     QList<RefPtr<NotificationPermissionCallback> >& callbacks = iter.value().m_callbacks;
     Q_FOREACH(const RefPtr<NotificationPermissionCallback>& callback, callbacks) {
         if (callback)
-            callback->handleEvent(Notification::permissionString(NotificationClient::PermissionAllowed));
+            callback->handleEvent(Notification::permissionString(permission));
     }
 #endif
     m_pendingPermissionRequests.remove(iter.key());
--- third_party/WebKit/Source/WebKit/qt/WebCoreSupport/NotificationPresenterClientQt.h
+++ third_party/WebKit/Source/WebKit/qt/WebCoreSupport/NotificationPresenterClientQt.h
@@ -102,7 +102,7 @@ public:
 
     void cancel(NotificationWrapper*);
 
-    void allowNotificationForFrame(Frame*);
+    void setNotificationsAllowedForFrame(Frame*, bool allowed);
 
     static bool dumpNotification;
 
