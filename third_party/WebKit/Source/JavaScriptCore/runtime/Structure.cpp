77a3b3c4b86e857374fd210c8cd852d41ca17b70
mhahnenberg@apple.com <mhahnenberg@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 20:05:14 +0000

    Structure::m_outOfLineCapacity is unnecessary https://bugs.webkit.org/show_bug.cgi?id=108206
    Reviewed by Geoffrey Garen.
    
    Source/JavaScriptCore: 
    
    We can calculate our out of line capacity by using the outOfLineSize and our knowledge about our resize policy.
    According to GDB, this knocks Structures down from 136 bytes to 128 bytes (I'm guessing the extra bytes are from
    better alignment of object fields), which puts Structures in a smaller size class. Woohoo! Looks neutral on our 
    benchmarks.
    
    * runtime/Structure.cpp:
    (JSC::Structure::Structure):
    (JSC):
    (JSC::Structure::suggestedNewOutOfLineStorageCapacity):
    (JSC::Structure::addPropertyTransition):
    (JSC::Structure::addPropertyWithoutTransition):
    * runtime/Structure.h:
    (Structure):
    (JSC::Structure::outOfLineCapacity):
    (JSC::Structure::totalStorageCapacity):
    
    Source/WTF: 
    
    We're adding a new function that gives us the ability to round a value up to the next power of a certain base.
    
    * wtf/MathExtras.h:
    (WTF::roundUpToPowerOf):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141295 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Structure.cpp
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Structure.cpp
@@ -158,7 +158,6 @@ Structure::Structure(JSGlobalData& globalData, JSGlobalObject* globalObject, JSV
     , m_prototype(globalData, this, prototype)
     , m_classInfo(classInfo)
     , m_transitionWatchpointSet(InitializedWatching)
-    , m_outOfLineCapacity(0)
     , m_inlineCapacity(inlineCapacity)
     , m_offset(invalidOffset)
     , m_dictionaryKind(NoneDictionaryKind)
@@ -185,7 +184,6 @@ Structure::Structure(JSGlobalData& globalData)
     , m_prototype(globalData, this, jsNull())
     , m_classInfo(&s_info)
     , m_transitionWatchpointSet(InitializedWatching)
-    , m_outOfLineCapacity(0)
     , m_inlineCapacity(0)
     , m_offset(invalidOffset)
     , m_dictionaryKind(NoneDictionaryKind)
@@ -208,7 +206,6 @@ Structure::Structure(JSGlobalData& globalData, const Structure* previous)
     , m_prototype(globalData, this, previous->storedPrototype())
     , m_classInfo(previous->m_classInfo)
     , m_transitionWatchpointSet(InitializedWatching)
-    , m_outOfLineCapacity(previous->m_outOfLineCapacity)
     , m_inlineCapacity(previous->m_inlineCapacity)
     , m_offset(invalidOffset)
     , m_dictionaryKind(previous->m_dictionaryKind)
@@ -274,14 +271,9 @@ inline size_t nextOutOfLineStorageCapacity(size_t currentCapacity)
     return currentCapacity * outOfLineGrowthFactor;
 }
 
-void Structure::growOutOfLineCapacity()
-{
-    m_outOfLineCapacity = nextOutOfLineStorageCapacity(m_outOfLineCapacity);
-}
-
 size_t Structure::suggestedNewOutOfLineStorageCapacity()
 {
-    return nextOutOfLineStorageCapacity(m_outOfLineCapacity);
+    return nextOutOfLineStorageCapacity(outOfLineCapacity());
 }
  
 void Structure::despecifyDictionaryFunction(JSGlobalData& globalData, PropertyName propertyName)
@@ -366,8 +358,6 @@ Structure* Structure::addPropertyTransition(JSGlobalData& globalData, Structure*
         Structure* transition = toCacheableDictionaryTransition(globalData, structure);
         ASSERT(structure != transition);
         offset = transition->putSpecificValue(globalData, propertyName, attributes, specificValue);
-        if (transition->outOfLineSize() > transition->outOfLineCapacity())
-            transition->growOutOfLineCapacity();
         return transition;
     }
     
@@ -392,8 +382,6 @@ Structure* Structure::addPropertyTransition(JSGlobalData& globalData, Structure*
     }
 
     offset = transition->putSpecificValue(globalData, propertyName, attributes, specificValue);
-    if (transition->outOfLineSize() > transition->outOfLineCapacity())
-        transition->growOutOfLineCapacity();
 
     transition->m_offset = offset;
     checkOffset(transition->m_offset, transition->inlineCapacity());
@@ -665,10 +653,7 @@ PropertyOffset Structure::addPropertyWithoutTransition(JSGlobalData& globalData,
     
     pin();
 
-    PropertyOffset offset = putSpecificValue(globalData, propertyName, attributes, specificValue);
-    if (outOfLineSize() > outOfLineCapacity())
-        growOutOfLineCapacity();
-    return offset;
+    return putSpecificValue(globalData, propertyName, attributes, specificValue);
 }
 
 PropertyOffset Structure::removePropertyWithoutTransition(JSGlobalData& globalData, PropertyName propertyName)
