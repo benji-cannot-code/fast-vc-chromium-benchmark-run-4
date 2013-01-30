f70ce368741d394116e1bd0c3dbda54a17cdfe48
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 02:51:54 +0000

    The first step of server-side crypto handshake: handle ClientHello, negotiate parameters, and respond with ServerHello. Still incomplete.
    Add the CloseConnectionWithDetails and SendConnectionCloseWithDetails
    methods to allow specifying an error details string.
    
    Merge internal CL: 41642399
    
    R=rch@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/12078004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179519 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_connection.cc
+++ net/quic/quic_connection.cc
@@ -22,6 +22,7 @@ using std::make_pair;
 using std::min;
 using std::vector;
 using std::set;
+using std::string;
 
 namespace net {
 
@@ -883,10 +884,16 @@ QuicFecGroup* QuicConnection::GetFecGroup() {
 }
 
 void QuicConnection::SendConnectionClose(QuicErrorCode error) {
+  SendConnectionCloseWithDetails(error, string());
+}
+
+void QuicConnection::SendConnectionCloseWithDetails(QuicErrorCode error,
+                                                    const string& details) {
   DLOG(INFO) << "Force closing with error " << QuicUtils::ErrorToString(error)
              << " (" << error << ")";
   QuicConnectionCloseFrame frame;
   frame.error_code = error;
+  frame.error_details = details;
   frame.ack_frame = outgoing_ack_;
 
   PacketPair packetpair = packet_creator_.CloseConnection(&frame);
