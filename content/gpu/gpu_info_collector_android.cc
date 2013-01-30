8c59805aba02a476b7e28d462ac42ec9fd6d24b1
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 06:09:42 +0000

    Remove unused header include.
    TBR=apatrick
    
    Review URL: https://codereview.chromium.org/12082069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179543 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_info_collector_android.cc
+++ content/gpu/gpu_info_collector_android.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_split.h"
 #include "base/string_util.h"
 #include "content/public/common/content_switches.h"
-#include "ui/gfx/android/device_display_info.h"
 
 namespace {
 
