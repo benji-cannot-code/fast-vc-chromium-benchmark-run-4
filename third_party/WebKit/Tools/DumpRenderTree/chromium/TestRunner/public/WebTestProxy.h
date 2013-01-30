3f77773b3ae6a3796944e2544f33c1956a20636c
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 20:46:15 +0000

    [chromium] move custom policy delegate to TestRunner library https://bugs.webkit.org/show_bug.cgi?id=108328
    Reviewed by Adam Barth.
    
    * DumpRenderTree/chromium/TestRunner/public/WebTestDelegate.h:
    * DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h:
    (WebTestProxyBase):
    (WebTestRunner::WebTestProxy::decidePolicyForNavigation):
    * DumpRenderTree/chromium/TestRunner/public/WebTestRunner.h:
    (WebTestRunner::WebTestRunner::policyDelegateEnabled):
    (WebTestRunner::WebTestRunner::policyDelegateIsPermissive):
    (WebTestRunner::WebTestRunner::policyDelegateShouldNotifyDone):
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp:
    (WebTestRunner::TestRunner::reset):
    (WebTestRunner::TestRunner::policyDelegateEnabled):
    (WebTestRunner):
    (WebTestRunner::TestRunner::policyDelegateIsPermissive):
    (WebTestRunner::TestRunner::policyDelegateShouldNotifyDone):
    (WebTestRunner::TestRunner::setCustomPolicyDelegate):
    (WebTestRunner::TestRunner::waitForPolicyDelegate):
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.h:
    (TestRunner):
    * DumpRenderTree/chromium/TestRunner/src/WebTestProxy.cpp:
    (WebTestRunner::WebTestProxyBase::decidePolicyForNavigation):
    (WebTestRunner):
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::decidePolicyForNavigation):
    (WebViewHost::reset):
    * DumpRenderTree/chromium/WebViewHost.h:
    (WebViewHost):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebKit/chromium/public/WebDragOperation.h"
 #include "WebKit/chromium/public/WebEditingAction.h"
 #include "WebKit/chromium/public/WebNavigationPolicy.h"
+#include "WebKit/chromium/public/WebNavigationType.h"
 #include "WebKit/chromium/public/WebTextAffinity.h"
 #include "WebKit/chromium/public/WebTextDirection.h"
 #include <map>
@@ -147,6 +148,7 @@ protected:
     bool runModalConfirmDialog(WebKit::WebFrame*, const WebKit::WebString&);
     bool runModalPromptDialog(WebKit::WebFrame*, const WebKit::WebString& message, const WebKit::WebString& defaultValue, WebKit::WebString* actualValue);
     bool runModalBeforeUnloadDialog(WebKit::WebFrame*, const WebKit::WebString&);
+    WebKit::WebNavigationPolicy decidePolicyForNavigation(WebKit::WebFrame*, const WebKit::WebURLRequest&, WebKit::WebNavigationType, const WebKit::WebNode& originatingNode, WebKit::WebNavigationPolicy defaultPolicy, bool isRedirect);
 
 private:
     void locationChangeDone(WebKit::WebFrame*);
@@ -445,6 +447,13 @@ public:
     {
         WebTestProxyBase::runModalBeforeUnloadDialog(frame, message);
         return Base::runModalBeforeUnloadDialog(frame, message);
+    }
+    virtual WebKit::WebNavigationPolicy decidePolicyForNavigation(WebKit::WebFrame* frame, const WebKit::WebURLRequest& request, WebKit::WebNavigationType type, const WebKit::WebNode& originatingNode, WebKit::WebNavigationPolicy defaultPolicy, bool isRedirect)
+    {
+        WebKit::WebNavigationPolicy policy = WebTestProxyBase::decidePolicyForNavigation(frame, request, type, originatingNode, defaultPolicy, isRedirect);
+        if (policy == WebKit::WebNavigationPolicyIgnore)
+            return policy;
+        return Base::decidePolicyForNavigation(frame, request, type, originatingNode, defaultPolicy, isRedirect);
     }
 };
 
