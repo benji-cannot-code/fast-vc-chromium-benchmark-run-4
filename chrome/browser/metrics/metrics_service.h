892bb374fb310b8b83d913763164b28a50e4d487
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 00:15:36 +0000

    metrics: Remove bookmarks code since it's no longer relevant.
    This came out while trying to convert chrome::NOTIFICATION_BOOKMARK_MODEL_LOADED
    to BaseBookmarkModelObserver.
    
    BUG=144783
    R=isherman@chromium.org
    
    Review URL: https://codereview.chromium.org/12088030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179477 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.h
+++ chrome/browser/metrics/metrics_service.h
@@ -31,8 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/external_metrics.h"
 #endif
 
-class BookmarkModel;
-class BookmarkNode;
 class MetricsReportingScheduler;
 class PrefService;
 class PrefServiceSimple;
@@ -361,16 +359,6 @@ class MetricsService
 
   // Records that the browser was shut down cleanly.
   void LogCleanShutdown();
-
-  // Set the value in preferences for the number of bookmarks and folders
-  // in node. The pref key for the number of bookmarks in num_bookmarks_key and
-  // the pref key for number of folders in num_folders_key.
-  void LogBookmarks(const BookmarkNode* node,
-                    const char* num_bookmarks_key,
-                    const char* num_folders_key);
-
-  // Sets preferences for the number of bookmarks in model.
-  void LogBookmarks(BookmarkModel* model);
 
   // Records a child process related notification.  These are recorded to an
   // in-object buffer because these notifications are sent on page load, and we
