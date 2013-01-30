7f64d58449c11b1b95bf58d581c8a09d14723923
apatrick@chromium.org <apatrick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 00:01:30 +0000

    Delete GPU channel on failure to reallocate backbuffer.
    This will cause a lost context and the renderer process will attempt to recover. If it does not recover, it will fall back to software compositing.
    
    Currently SetBackbufferAllocation can only fail on Windows but we should detect failures on other platforms as well.
    
    This might potentially fix the referenced bug.
    
    BUG=170767
    
    Review URL: https://chromiumcodereview.appspot.com/12090027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179471 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.cc
+++ content/common/gpu/gpu_command_buffer_stub.cc
@@ -720,7 +720,8 @@ void GpuCommandBufferStub::OnDiscardBackbuffer() {
     DCHECK(!IsScheduled());
     channel_->RequeueMessage();
   } else {
-    surface_->SetBackbufferAllocation(false);
+    if (!surface_->SetBackbufferAllocation(false))
+      channel_->DestroySoon();
   }
 }
 
@@ -732,7 +733,8 @@ void GpuCommandBufferStub::OnEnsureBackbuffer() {
     DCHECK(!IsScheduled());
     channel_->RequeueMessage();
   } else {
-    surface_->SetBackbufferAllocation(true);
+    if (!surface_->SetBackbufferAllocation(true))
+      channel_->DestroySoon();
   }
 }
 
