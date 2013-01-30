4ec2001f41228d4693cf05030bcab882801a05bd
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 01:31:37 +0000

    Add some hardening to methodTable() https://bugs.webkit.org/show_bug.cgi?id=108253
    Reviewed by Mark Hahnenberg.
    
    When accessing methodTable() we now always make sure that our
    structure _could_ be valid.  Added a separate method to get a
    classes methodTable during destruction as it's not possible to
    validate the structure at that point.  This separation might
    also make it possible to improve the performance of methodTable
    access more generally in future.
    
    * heap/MarkedBlock.cpp:
    (JSC::MarkedBlock::callDestructor):
    * runtime/JSCell.h:
    (JSCell):
    * runtime/JSCellInlines.h:
    (JSC::JSCell::methodTableForDestruction):
    (JSC):
    (JSC::JSCell::methodTable):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSCell.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSCell.h
@@ -106,6 +106,7 @@ public:
     // Object operations, with the toObject operation included.
     const ClassInfo* classInfo() const;
     const MethodTable* methodTable() const;
+    const MethodTable* methodTableForDestruction() const;
     static void put(JSCell*, ExecState*, PropertyName, JSValue, PutPropertySlot&);
     static void putByIndex(JSCell*, ExecState*, unsigned propertyName, JSValue, bool shouldThrow);
         
