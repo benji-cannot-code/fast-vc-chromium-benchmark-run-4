b0646a5514eeb1c8a530f3ac8d8cc6b7fb0c4fb4
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 09:13:58 +0000

    Fix leaks in FTP tests and remove suppressions
    BUG=171722
    
    Review URL: https://codereview.chromium.org/12090046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179567 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_ftp_job_unittest.cc
+++ net/url_request/url_request_ftp_job_unittest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/memory/scoped_vector.h"
 #include "base/run_loop.h"
 #include "net/proxy/proxy_config_service.h"
 #include "net/socket/socket_test_util.h"
@@ -96,12 +97,12 @@ class URLRequestFtpJobTest : public testing::Test {
   }
 
  private:
+  ScopedVector<DeterministicSocketData> socket_data_;
   DeterministicMockClientSocketFactory socket_factory_;
   TestNetworkDelegate network_delegate_;
 
   // Owned by |request_context_|:
   ProxyService* proxy_service_;
-  std::vector<DeterministicSocketData*> socket_data_;
 
   FtpTestURLRequestContext request_context_;
 };
