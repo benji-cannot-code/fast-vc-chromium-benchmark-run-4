6ac01840abf533d68f22746e0c12799978cf6e9f
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 12:30:19 +0000

    Revert 179554 - suspected to break browser_tests on Mac 10.6
    > Refactor FullscreenController removing TogglePresentationMode & adding ToggleFullscreenWithChrome.
    > 
    > Part of a series of refactoring changes that will enable simpler code in FullscreenController as well as correcting behavior there. This change attempts to make the minimal modifications required in order to have FullscreenController::ToggleFullscreenMode consistently mean fullscreen with no browser chrome on all platforms.
    >  
    > Depends on https://codereview.chromium.org/12018007/
    > 
    > BUG=169138
    > 
    > Review URL: https://chromiumcodereview.appspot.com/11896104
    
    TBR=scheib@chromium.org
    Review URL: https://codereview.chromium.org/12089068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -40,10 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/url_constants.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 
-#if defined(OS_MACOSX)
-#include "chrome/browser/ui/browser_commands_mac.h"
-#endif
-
 #if defined(OS_WIN)
 #include "base/win/metro.h"
 #endif
@@ -426,11 +422,7 @@ void BrowserCommandController::ExecuteCommandWithDisposition(
       ConvertPopupToTabbedBrowser(browser_);
       break;
     case IDC_FULLSCREEN:
-#if defined(OS_MACOSX)
-      chrome::ToggleFullscreenWithChrome(browser_);
-#else
       chrome::ToggleFullscreenMode(browser_);
-#endif
       break;
 
 #if defined(USE_ASH)
@@ -459,7 +451,7 @@ void BrowserCommandController::ExecuteCommandWithDisposition(
 
 #if defined(OS_MACOSX)
     case IDC_PRESENTATION_MODE:
-      chrome::ToggleFullscreenMode(browser_);
+      browser_->TogglePresentationMode();
       break;
 #endif
     case IDC_EXIT:
