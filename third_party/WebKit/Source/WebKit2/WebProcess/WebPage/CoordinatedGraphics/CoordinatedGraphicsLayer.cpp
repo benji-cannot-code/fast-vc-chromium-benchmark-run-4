3c0738deb804677787bca2ce686616a3969801ad
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 11:56:42 +0000

    [EFL][Qt][WebGL] Avoid deleting an uncreated canvas. https://bugs.webkit.org/show_bug.cgi?id=106878
    Patch by Kondapally Kalyan <kalyan.kondapally@intel.com> on 2013-01-30
    Reviewed by Benjamin Poulain.
    
    Source/WebKit2:
    
    setContentsToCanvas is responsible for marking canvas for creation or deletion.
    The issue here is that the canvas is marked for deletion even though it has not
    been created. This causes an assert in LayerTreeRenderer::destroyCanvas.
    This patch adds a seperate check to ensure that CoordinatedGraphicsLayer
    tries to issue a request for canvas deletion only after request for canvas
    creation has been handled.
    
    New test: fast/canvas/webgl/canvas-resize-crash.html
    
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp:
    (WebCore::CoordinatedGraphicsLayer::CoordinatedGraphicsLayer):
    (WebCore::CoordinatedGraphicsLayer::setContentsToCanvas):
    (WebCore::CoordinatedGraphicsLayer::destroyCanvasIfNeeded):
    (WebCore::CoordinatedGraphicsLayer::createCanvasIfNeeded):
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.h:
    (CoordinatedGraphicsLayer):
    (WebCore::CoordinatedGraphicsLayer::fixedToViewport):
    
    LayoutTests:
    
    * fast/canvas/webgl/canvas-resize-crash-expected.txt: Added.
    * fast/canvas/webgl/canvas-resize-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedGraphicsLayer.cpp
@@ -117,11 +117,12 @@ CoordinatedGraphicsLayer::CoordinatedGraphicsLayer(GraphicsLayerClient* client)
     , m_shouldSyncImageBacking(true)
     , m_shouldSyncAnimations(true)
     , m_fixedToViewport(false)
-    , m_canvasNeedsDisplay(false)
-    , m_canvasNeedsCreate(false)
-    , m_canvasNeedsDestroy(false)
     , m_pendingContentsScaleAdjustment(false)
     , m_pendingVisibleRectAdjustment(false)
+#if USE(GRAPHICS_SURFACE)
+    , m_isValidCanvas(false)
+    , m_pendingCanvasOperation(None)
+#endif
     , m_coordinator(0)
     , m_contentsScale(1)
     , m_compositedNativeImagePtr(0)
@@ -315,8 +316,11 @@ void CoordinatedGraphicsLayer::setContentsRect(const IntRect& r)
 
 void CoordinatedGraphicsLayer::setContentsNeedsDisplay()
 {
+#if USE(GRAPHICS_SURFACE)
     if (m_canvasPlatformLayer)
-        m_canvasNeedsDisplay = true;
+        m_pendingCanvasOperation |= SyncCanvas;
+#endif
+
     if (client())
         client()->notifyFlushRequired(this);
 
@@ -328,16 +332,16 @@ void CoordinatedGraphicsLayer::setContentsToCanvas(PlatformLayer* platformLayer)
 #if USE(GRAPHICS_SURFACE)
     if (m_canvasPlatformLayer) {
         ASSERT(m_canvasToken.isValid());
-        if (!platformLayer)
-            m_canvasNeedsDestroy = true;
-        else if ((m_canvasSize != platformLayer->platformLayerSize()) || (m_canvasToken != platformLayer->graphicsSurfaceToken())) {
+        if (!platformLayer) {
+            m_pendingCanvasOperation |= DestroyCanvas;
+            m_pendingCanvasOperation &= ~CreateCanvas;
+        }  else if ((m_canvasSize != platformLayer->platformLayerSize()) || (m_canvasToken != platformLayer->graphicsSurfaceToken())) {
             // m_canvasToken can be different to platformLayer->graphicsSurfaceToken(), even if m_canvasPlatformLayer equals platformLayer.
-            m_canvasNeedsDestroy = true;
-            m_canvasNeedsCreate = true;
+            m_pendingCanvasOperation |= RecreateCanvas;
         }
     } else {
         if (platformLayer)
-            m_canvasNeedsCreate = true;
+            m_pendingCanvasOperation |= CreateAndSyncCanvas;
     }
 
     m_canvasPlatformLayer = platformLayer;
@@ -345,8 +349,7 @@ void CoordinatedGraphicsLayer::setContentsToCanvas(PlatformLayer* platformLayer)
     m_canvasSize = m_canvasPlatformLayer ? m_canvasPlatformLayer->platformLayerSize() : IntSize();
     m_canvasToken = m_canvasPlatformLayer ? m_canvasPlatformLayer->graphicsSurfaceToken() : GraphicsSurfaceToken();
     ASSERT(!(!m_canvasToken.isValid() && m_canvasPlatformLayer));
-    if (m_canvasPlatformLayer)
-        m_canvasNeedsDisplay = true;
+
     if (client())
         client()->notifyFlushRequired(this);
 #else
@@ -594,43 +597,50 @@ void CoordinatedGraphicsLayer::syncAnimations()
     m_coordinator->setLayerAnimations(m_id, m_animations);
 }
 
+#if USE(GRAPHICS_SURFACE)
 void CoordinatedGraphicsLayer::syncCanvas()
 {
     destroyCanvasIfNeeded();
     createCanvasIfNeeded();
 
-    if (!m_canvasNeedsDisplay)
+    if (!(m_pendingCanvasOperation & SyncCanvas))
         return;
 
-    ASSERT(m_canvasPlatformLayer);
-#if USE(GRAPHICS_SURFACE)
+    m_pendingCanvasOperation &= ~SyncCanvas;
+
+    if (!m_isValidCanvas)
+        return;
+
     m_coordinator->syncCanvas(m_id, m_canvasPlatformLayer);
-#endif
-    m_canvasNeedsDisplay = false;
 }
 
 void CoordinatedGraphicsLayer::destroyCanvasIfNeeded()
 {
-    if (!m_canvasNeedsDestroy)
+    if (!(m_pendingCanvasOperation & DestroyCanvas))
         return;
 
-#if USE(GRAPHICS_SURFACE)
-    m_coordinator->destroyCanvas(m_id);
-#endif
-    m_canvasNeedsDestroy = false;
+    if (m_isValidCanvas) {
+        m_coordinator->destroyCanvas(m_id);
+        m_isValidCanvas = false;
+    }
+
+    m_pendingCanvasOperation &= ~DestroyCanvas;
 }
 
 void CoordinatedGraphicsLayer::createCanvasIfNeeded()
 {
-    if (!m_canvasNeedsCreate)
+    if (!(m_pendingCanvasOperation & CreateCanvas))
         return;
 
     ASSERT(m_canvasPlatformLayer);
-#if USE(GRAPHICS_SURFACE)
-    m_coordinator->createCanvas(m_id, m_canvasPlatformLayer);
-#endif
-    m_canvasNeedsCreate = false;
+    if (!m_isValidCanvas) {
+        m_coordinator->createCanvas(m_id, m_canvasPlatformLayer);
+        m_isValidCanvas = true;
+    }
+
+    m_pendingCanvasOperation &= ~CreateCanvas;
 }
+#endif
 
 void CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly()
 {
@@ -648,7 +658,9 @@ void CoordinatedGraphicsLayer::flushCompositingStateForThisLayerOnly()
     syncFilters();
 #endif
     updateContentBuffers();
+#if USE(GRAPHICS_SURFACE)
     syncCanvas();
+#endif
 }
 
 bool CoordinatedGraphicsLayer::imageBackingVisible()
@@ -874,7 +886,12 @@ bool CoordinatedGraphicsLayer::hasPendingVisibleChanges()
             return true;
     }
 
-    if (!m_shouldSyncLayerState && !m_shouldSyncChildren && !m_shouldSyncFilters && !m_shouldSyncImageBacking && !m_shouldSyncAnimations && !m_canvasNeedsDisplay)
+    bool shouldSyncCanvas = false;
+#if USE(GRAPHICS_SURFACE)
+    shouldSyncCanvas = m_pendingCanvasOperation & SyncCanvas;
+#endif
+
+    if (!m_shouldSyncLayerState && !m_shouldSyncChildren && !m_shouldSyncFilters && !m_shouldSyncImageBacking && !m_shouldSyncAnimations && !shouldSyncCanvas)
         return false;
 
     return tiledBackingStoreVisibleRect().intersects(tiledBackingStoreContentsRect());
