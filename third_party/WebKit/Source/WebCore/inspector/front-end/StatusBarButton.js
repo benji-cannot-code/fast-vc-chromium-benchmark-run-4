cfb9f59febfae79396c51845f0e4cbfd92cbb2d7
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 12:01:07 +0000

    Web Inspector: [Canvas] support instrumenting canvases in iframes (frontend side) https://bugs.webkit.org/show_bug.cgi?id=108319
    Reviewed by Pavel Feldman.
    
    Add a frame selector to choose between frames with canvases. Show this selector in the status
    bar only if there are 2 or more frames with canvses. Otherwise, assume silently the only
    frame with canvases (most common use case).
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/CanvasProfileView.js:
    (WebInspector.CanvasProfileType):
    (WebInspector.CanvasProfileType.prototype.get statusBarItems):
    (WebInspector.CanvasProfileType.prototype._runSingleFrameCapturing):
    (WebInspector.CanvasProfileType.prototype._startFrameCapturing):
    (WebInspector.CanvasProfileType.prototype._frameAdded):
    (WebInspector.CanvasProfileType.prototype._addFrame):
    (WebInspector.CanvasProfileType.prototype._frameRemoved):
    (WebInspector.CanvasProfileType.prototype._contextCreated):
    (WebInspector.CanvasProfileType.prototype._selectedFrameId):
    (WebInspector.CanvasProfileType.prototype._dispatchViewUpdatedEvent):
    (WebInspector.CanvasDispatcher):
    (WebInspector.CanvasDispatcher.prototype.contextCreated):
    * inspector/front-end/ProfilesPanel.js:
    (WebInspector.ProfileType.prototype.createProfile):
    (WebInspector.ProfilesPanel.prototype._onProfileTypeSelected):
    (WebInspector.ProfilesPanel.prototype._updateProfileTypeSpecificUI):
    (WebInspector.ProfilesPanel.prototype._registerProfileType):
    * inspector/front-end/StatusBarButton.js:
    (WebInspector.StatusBarComboBox.prototype.size):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/StatusBarButton.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/StatusBarButton.js
@@ -290,6 +290,14 @@ WebInspector.StatusBarComboBox = function(changeHandler, className)
 
 WebInspector.StatusBarComboBox.prototype = {
     /**
+     * @return {number}
+     */
+    size: function()
+    {
+        return this._selectElement.childElementCount;
+    },
+
+    /**
      * @param {!Element} option
      */
     addOption: function(option)
