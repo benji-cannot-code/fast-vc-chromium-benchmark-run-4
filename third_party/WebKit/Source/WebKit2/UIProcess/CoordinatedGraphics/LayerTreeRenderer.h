0abfe174b44edff1ca2680a2922e433b21d688bb
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 08:22:57 +0000

    Coordinated Graphics: Remove m_pendingSyncBackingStores in LayerTreeRenderer. https://bugs.webkit.org/show_bug.cgi?id=107099
    Patch by Huang Dongsung <luxtella@company100.net> on 2013-01-30
    Reviewed by Noam Rosenthal.
    
    Source/WebCore:
    
    Add GraphicsLayerTextureMapper::setBackingStore() so that
    LayerTreeRenderer sets a backing store to GraphicsLayerTextureMapper.
    
    Remove three methods of TextureMapperLayer related to a backing store
    because they are no longer used.
    
    Covered by existing compositing tests.
    
    * platform/graphics/texmap/GraphicsLayerTextureMapper.cpp:
    (WebCore::GraphicsLayerTextureMapper::flushCompositingStateForThisLayerOnly):
    (WebCore::GraphicsLayerTextureMapper::prepareBackingStoreIfNeeded):
    (WebCore):
    (WebCore::GraphicsLayerTextureMapper::updateDebugBorderAndRepaintCount):
    (WebCore::GraphicsLayerTextureMapper::updateBackingStoreIfNeeded):
    (WebCore::GraphicsLayerTextureMapper::setBackingStore):
    * platform/graphics/texmap/GraphicsLayerTextureMapper.h:
    (GraphicsLayerTextureMapper):
    * platform/graphics/texmap/TextureMapperLayer.cpp:
    (WebCore::TextureMapperLayer::flushCompositingStateForThisLayerOnly):
    * platform/graphics/texmap/TextureMapperLayer.h:
    (TextureMapperLayer):
    
    Source/WebKit2:
    
    Instead of queuing the setting of backing stores in LayerTreeRenderer,
    and then setting them directly to TextureMapperLayer, we allow
    GraphicsLayerTextureMapper's existing queuing mechanism to handle that.
    Instead of a m_pendingSyncBackingStores queue, we have a m_backingStores
    queue which can be applied much more easily to the layer tree.
    
    In addition, LayerTreeRenderer::purgeGLResources() does not call
    TextureMapperLayer::clearBackingStoresRecursive() because
    TextureMapperLayer will be destructed soon.
    
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp:
    (WebKit::LayerTreeRenderer::deleteLayer):
    (WebKit::LayerTreeRenderer::createBackingStoreIfNeeded):
    (WebKit::LayerTreeRenderer::removeBackingStoreIfNeeded):
    (WebKit::LayerTreeRenderer::resetBackingStoreSizeToLayerSize):
    (WebKit::LayerTreeRenderer::createTile):
    (WebKit::LayerTreeRenderer::removeTile):
    (WebKit::LayerTreeRenderer::updateTile):
    (WebKit::LayerTreeRenderer::commitPendingBackingStoreOperations):
    (WebKit::LayerTreeRenderer::purgeGLResources):
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/LayerTreeRenderer.h
@@ -154,7 +154,6 @@ private:
     void ensureRootLayer();
     void commitPendingBackingStoreOperations();
 
-    CoordinatedBackingStore* getBackingStore(WebCore::GraphicsLayer*);
     void prepareContentBackingStore(WebCore::GraphicsLayer*);
     void createBackingStoreIfNeeded(WebCore::GraphicsLayer*);
     void removeBackingStoreIfNeeded(WebCore::GraphicsLayer*);
@@ -173,8 +172,8 @@ private:
     ImageBackingMap m_imageBackings;
     Vector<RefPtr<CoordinatedBackingStore> > m_releasedImageBackings;
 
-    typedef HashMap<WebCore::TextureMapperLayer*, RefPtr<CoordinatedBackingStore> > BackingStoreMap;
-    BackingStoreMap m_pendingSyncBackingStores;
+    typedef HashMap<WebCore::GraphicsLayer*, RefPtr<CoordinatedBackingStore> > BackingStoreMap;
+    BackingStoreMap m_backingStores;
 
     HashSet<RefPtr<CoordinatedBackingStore> > m_backingStoresWithPendingBuffers;
 
