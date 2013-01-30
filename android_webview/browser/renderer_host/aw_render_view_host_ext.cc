07a7e7c0148743c8197582601f15628d02ec33ca
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 00:29:43 +0000

    Android webview fix visited link
    FrameNavigateParams::base_url is empty most of the time. Plus also
    adding all redirect URLs to the history to match chrome behavior.
    
    BUG=
    TBR=mkosiba@chromium.org
    Android only change. Ran through android trybots
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12050007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179481 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/renderer_host/aw_render_view_host_ext.cc
+++ android_webview/browser/renderer_host/aw_render_view_host_ext.cc
@@ -80,7 +80,7 @@ void AwRenderViewHostExt::DidNavigateAnyFrame(
   DCHECK(CalledOnValidThread());
 
   AwBrowserContext::FromWebContents(web_contents())
-      ->AddVisitedURL(params.base_url);
+      ->AddVisitedURLs(params.redirects);
 }
 
 bool AwRenderViewHostExt::OnMessageReceived(const IPC::Message& message) {
