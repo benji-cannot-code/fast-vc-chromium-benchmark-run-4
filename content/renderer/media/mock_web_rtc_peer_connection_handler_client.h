143c116573e3e203362321324af18e18beee3ad8
tommyw@chromium.org <tommyw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 16:39:54 +0000

    Adding some headers for preparation of the rename. Basically only "include what you use" changes.
    BUG=173105
    
    
    Review URL: https://chromiumcodereview.appspot.com/12091063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179647 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/mock_web_rtc_peer_connection_handler_client.h
+++ content/renderer/media/mock_web_rtc_peer_connection_handler_client.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebMediaStreamDescriptor.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebRTCICECandidate.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebRTCPeerConnectionHandlerClient.h"
 
--- content/renderer/media/peer_connection_tracker.h
+++ content/renderer/media/peer_connection_tracker.h
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "content/public/renderer/render_process_observer.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebMediaStreamDescriptor.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebRTCPeerConnectionHandlerClient.h"
 #include "third_party/libjingle/source/talk/app/webrtc/jsep.h"
 
 namespace WebKit {
 class WebFrame;
-class WebMediaStreamDescriptor;
 class WebRTCICECandidate;
 class WebString;
 }  // namespace WebKit
