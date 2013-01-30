3124d7c32521e409b4fb1525e7c95a773e217ce8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-30 15:10:57 +0000

    Web Inspector: Filters on Console panel https://bugs.webkit.org/show_bug.cgi?id=107813
    Source/WebCore:
    
    The problem is that third-party libraries may spam javascript console with internal
    messages. Now there's filter context-menu option, which allows to hide/show messages
    sent from specific scripts or urls.
    
    Patch by Dmitry Zvorygin <zvorygin@chromium.org> on 2013-01-30
    Reviewed by Pavel Feldman.
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/ConsoleMessage.js:
    (WebInspector.ConsoleMessageImpl.prototype.appendUndefined):
    (WebInspector.ConsoleMessageImpl.prototype._printArray):
    (WebInspector.ConsoleMessageImpl.prototype._highlightSearchResultsInElement):
    * inspector/front-end/ConsolePanel.js:
    (WebInspector.ConsolePanel.prototype.performSearch):
    * inspector/front-end/ConsoleView.js:
    (WebInspector.ConsoleView.get this):
    (WebInspector.ConsoleView.prototype._consoleMessageAdded):
    (WebInspector.ConsoleView.prototype._appendConsoleMessage):
    (WebInspector.ConsoleView.prototype._consoleCleared):
    (WebInspector.ConsoleView.prototype._handleContextMenuEvent.get var):
    (WebInspector.ConsoleView.prototype._handleContextMenuEvent.set get contextMenu):
    (WebInspector.ConsoleView.prototype._shouldBeVisible):
    (WebInspector.ConsoleView.prototype._updateMessageList):
    (WebInspector.ConsoleGroup.prototype.addMessage):
    * inspector/front-end/ContextMenu.js:
    (WebInspector.ContextMenuItem.prototype.isEnabled):
    (WebInspector.ContextMenuItem.prototype.setEnabled):
    * inspector/front-end/Settings.js:
    
    LayoutTests:
    
    The problem is that third-party libraries may spam javascript console
    with internal  messages. Now there's filter context-menu option, which
    allows to hide/show messages sent from specific scripts or urls.
    
    Patch by Dmitry Zvorygin <zvorygin@chromium.org> on 2013-01-30
    Reviewed by Pavel Feldman.
    
    * http/tests/inspector/console-test.js:
    (initialize_ConsoleTest.InspectorTest.dumpConsoleMessages):
    (initialize_ConsoleTest.InspectorTest.dumpConsoleMessagesWithStyles):
    (initialize_ConsoleTest.InspectorTest.dumpConsoleMessagesWithClasses):
    (initialize_ConsoleTest.InspectorTest.expandConsoleMessages):
    (initialize_ConsoleTest.InspectorTest.checkConsoleMessagesDontHaveParameters):
    (initialize_ConsoleTest):
    * http/tests/inspector/stacktraces/resources/stacktrace-test.js:
    (test.addMessage):
    (test):
    * inspector/console/console-filter-test-expected.txt: Added.
    * inspector/console/console-filter-test.html: Added.
    * inspector/console/resources/log-source.js: Added.
    (log2):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141269 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/console-test.js
@@ -10,7 +10,7 @@ InspectorTest.showConsolePanel = function()
 InspectorTest.dumpConsoleMessages = function(printOriginatingCommand)
 {
     var result = [];
-    var messages = WebInspector.consoleView.messages;
+    var messages = WebInspector.consoleView._visibleMessages;
     for (var i = 0; i < messages.length; ++i) {
         var element = messages[i].toMessageElement();
         InspectorTest.addResult(element.textContent.replace(/\u200b/g, ""));
@@ -25,7 +25,7 @@ InspectorTest.dumpConsoleMessages = function(printOriginatingCommand)
 InspectorTest.dumpConsoleMessagesWithStyles = function(sortMessages)
 {
     var result = [];
-    var messages = WebInspector.consoleView.messages;
+    var messages = WebInspector.consoleView._visibleMessages;
     for (var i = 0; i < messages.length; ++i) {
         var element = messages[i].toMessageElement();
         InspectorTest.addResult(element.textContent.replace(/\u200b/g, ""));
@@ -37,7 +37,7 @@ InspectorTest.dumpConsoleMessagesWithStyles = function(sortMessages)
 
 InspectorTest.dumpConsoleMessagesWithClasses = function(sortMessages) {
     var result = [];
-    var messages = WebInspector.consoleView.messages;
+    var messages = WebInspector.consoleView._visibleMessages;
     for (var i = 0; i < messages.length; ++i) {
         var element = messages[i].toMessageElement();
         result.push(element.textContent.replace(/\u200b/g, "") + " " + element.getAttribute("class"));
@@ -50,7 +50,7 @@ InspectorTest.dumpConsoleMessagesWithClasses = function(sortMessages) {
 
 InspectorTest.expandConsoleMessages = function()
 {
-    var messages = WebInspector.consoleView.messages;
+    var messages = WebInspector.consoleView._visibleMessages;
     for (var i = 0; i < messages.length; ++i) {
         var element = messages[i].toMessageElement();
         var node = element;
@@ -64,7 +64,7 @@ InspectorTest.expandConsoleMessages = function()
 
 InspectorTest.checkConsoleMessagesDontHaveParameters = function()
 {
-    var messages = WebInspector.console.messages;
+    var messages = WebInspector.consoleView._visibleMessages;
     for (var i = 0; i < messages.length; ++i) {
         var m = messages[i];
         InspectorTest.addResult("Message[" + i + "]:");
--- third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleMessage.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ConsoleMessage.js
@@ -248,6 +248,7 @@ WebInspector.ConsoleMessageImpl.prototype = {
     },
 
     /**
+     * @param {Object} output
      * @param {boolean=} forceObjectFormat
      * @param {boolean=} includePreview
      */
@@ -448,7 +449,7 @@ WebInspector.ConsoleMessageImpl.prototype = {
         {
             if (index - lastNonEmptyIndex <= 1)
                 return;
-            var span = elem.createChild(span, "console-formatted-undefined");
+            var span = elem.createChild("span", "console-formatted-undefined");
             span.textContent = WebInspector.UIString("undefined × %d", index - lastNonEmptyIndex - 1);
         }
 
@@ -481,7 +482,7 @@ WebInspector.ConsoleMessageImpl.prototype = {
 
     _formatWithSubstitutionString: function(parameters, formattedResult)
     {
-        var formatters = {}
+        var formatters = {};
 
         function parameterFormatter(force, obj)
         {
@@ -596,7 +597,6 @@ WebInspector.ConsoleMessageImpl.prototype = {
         regexObject.lastIndex = 0;
         var text = element.textContent;
         var match = regexObject.exec(text);
-        var offset = 0;
         var matchRanges = [];
         while (match) {
             matchRanges.push({ offset: match.index, length: match[0].length });
