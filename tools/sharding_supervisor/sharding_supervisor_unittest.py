1e376d9e8ef56aec09e94861999a0402f99c22bd
maruel@chromium.org <maruel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 10:00:08 +0000

    Rename sharding_supervisor to sharding_supervisor_old
    It's done in prevision of replacing it with run_test_cases; sharding_supervisor
    will stay a thin script until the buildbot scripts are updated.
    
    R=csharp@chromium.org
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12089056
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179581 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/sharding_supervisor/sharding_supervisor_unittest.py
+++ tools/sharding_supervisor/sharding_supervisor_unittest.py
@@ -14,7 +14,7 @@ import unittest
 
 from xml.dom import minidom
 
-import sharding_supervisor
+import sharding_supervisor_old as sharding_supervisor
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 SHARDING_SUPERVISOR = os.path.join(ROOT_DIR, 'sharding_supervisor.py')
