f184e4bf955e6788269bf017914e04c7b5b79476
ben@chromium.org <ben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-30 22:02:17 +0000

    Changes FocusController to use the new window hierarchy notifications to correctly update focus/activation when the window hierarchy changes.
    Removes GetNextFocusableWindow() since it's no longer used.
    
    Disables the parent-hide and parent-destruction focus controller tests since they aren't really compatible with the way the WorkspaceManager works, and it was a new use case that the old focus system never supported anyway.
    
    BUG=none
    R=sky@chromium.org
    Review URL: https://codereview.chromium.org/12086026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179707 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/corewm/focus_controller.h
+++ ui/views/corewm/focus_controller.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/scoped_observer.h"
 #include "ui/aura/client/activation_client.h"
 #include "ui/aura/client/focus_client.h"
-#include "ui/aura/env_observer.h"
 #include "ui/aura/window_observer.h"
 #include "ui/views/views_export.h"
 
@@ -37,8 +36,7 @@ class FocusRules;
 class VIEWS_EXPORT FocusController : public aura::client::ActivationClient,
                                      public aura::client::FocusClient,
                                      public ui::EventHandler,
-                                     public aura::WindowObserver,
-                                     public aura::EnvObserver {
+                                     public aura::WindowObserver {
  public:
   // |rules| cannot be NULL.
   explicit FocusController(FocusRules* rules);
@@ -82,11 +80,10 @@ class VIEWS_EXPORT FocusController : public aura::client::ActivationClient,
   virtual void OnWindowVisibilityChanged(aura::Window* window,
                                          bool visible) OVERRIDE;
   virtual void OnWindowDestroying(aura::Window* window) OVERRIDE;
-  virtual void OnWindowDestroyed(aura::Window* window) OVERRIDE;
-  virtual void OnWindowRemovingFromRootWindow(aura::Window* window) OVERRIDE;
-
-  // Overridden from aura::EnvObserver:
-  virtual void OnWindowInitialized(aura::Window* window) OVERRIDE;
+  virtual void OnWindowHierarchyChanging(
+      const HierarchyChangeParams& params) OVERRIDE;
+  virtual void OnWindowHierarchyChanged(
+      const HierarchyChangeParams& params) OVERRIDE;
 
   // Internal implementations that set the focused/active windows, fire events
   // etc. These functions must be called with valid focusable/activatable
@@ -95,9 +92,10 @@ class VIEWS_EXPORT FocusController : public aura::client::ActivationClient,
   void SetActiveWindow(aura::Window* window);
 
   // Called when a window's disposition changed such that it and its hierarchy
-  // are no longer focusable/activatable. The system must determine what window
-  // to focus next based on rules.
-  void WindowLostFocusFromDispositionChange(aura::Window* window);
+  // are no longer focusable/activatable. |next| is a valid window that is used
+  // as a starting point for finding a window to focus next based on rules.
+  void WindowLostFocusFromDispositionChange(aura::Window* window,
+                                            aura::Window* next);
 
   // Called when an attempt is made to focus or activate a window via an input
   // event targeted at that window. Rules determine the best focusable window
