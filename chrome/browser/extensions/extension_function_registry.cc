7176f83efdd1abd0415144f08ee3316e6df05d95
oscarpan@google.com <oscarpan@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 23:15:42 +0000

    [Chrome OS ECHO] Get the OOBE timestamp and make it avaiable to echo extension
    Chrome OS Registration service need to know when the device is first activated
    and will use that information to determine the expiration date of some offer.
    The OOBE timestamp extracted from file /home/chronos/.oobe_completed is used
    to determine the activation date.
    
    BUG=chromium-os:38325
    TEST=unit tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/12091052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -184,6 +184,7 @@ void ExtensionFunctionRegistry::ResetFunctions() {
 
   // Echo
   RegisterFunction<GetRegistrationCodeFunction>();
+  RegisterFunction<GetOobeTimestampFunction>();
 
   // Terminal
   RegisterFunction<OpenTerminalProcessFunction>();
