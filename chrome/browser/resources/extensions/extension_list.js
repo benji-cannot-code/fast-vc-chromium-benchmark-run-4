e3606605fcec2141d2ecc99282c5c15608177c90
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 19:05:08 +0000

    Add a link (to view Permissions) to the Extension details on chrome://extensions.
    BUG=170742
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/12094034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179916 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extension_list.js
+++ chrome/browser/resources/extensions/extension_list.js
@@ -151,7 +151,7 @@ cr.define('options', function() {
         fileAccess.hidden = false;
       }
 
-      // The 'Options' checkbox.
+      // The 'Options' link.
       if (extension.enabled && extension.optionsUrl) {
         var options = node.querySelector('.options-link');
         options.addEventListener('click', function(e) {
@@ -160,6 +160,13 @@ cr.define('options', function() {
         });
         options.hidden = false;
       }
+
+      // The 'Permissions' link.
+      var permissions = node.querySelector('.permissions-link');
+      permissions.addEventListener('click', function(e) {
+        chrome.send('extensionSettingsPermissions', [extension.id]);
+        e.preventDefault();
+      });
 
       if (extension.allow_activity) {
         var activity = node.querySelector('.activity-link');
