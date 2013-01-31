5783c2f5fbfaded77228c4755ad2f803117a3224
paroga@webkit.org <paroga@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 20:41:21 +0000

    Remove PLATFORM(WIN_CAIRO) from NetscapePluginX11.cpp https://bugs.webkit.org/show_bug.cgi?id=108439
    Reviewed by Brent Fulgham.
    
    PLATFORM(WIN_CAIRO) is Windows only, where no X11 exists.
    
    * WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/Plugins/Netscape/x11/NetscapePluginX11.cpp
@@ -48,7 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <Ecore_X.h>
 #endif
 
-#if USE(CAIRO) && !PLATFORM(WIN_CAIRO)
+#if USE(CAIRO)
 #include "PlatformContextCairo.h"
 #include "RefPtrCairo.h"
 #include <cairo/cairo-xlib.h>
