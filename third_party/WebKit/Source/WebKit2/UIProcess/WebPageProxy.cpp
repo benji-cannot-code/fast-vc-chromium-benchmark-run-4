a82636864f1748618497d4f0e9b54f5aaf6fc41e
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 03:57:05 +0000

    [WK2] Cleanup MessageID parameter after r141332 https://bugs.webkit.org/show_bug.cgi?id=108419
    Unreviewed to fix build breaks.
    
    r141332 didn't remove MessageID parameter on some features.
    (battery, vibration, coordinate graphics, network info and so on)
    
    * UIProcess/CoordinatedGraphics/CoordinatedLayerTreeHostProxy.h:
    (CoordinatedLayerTreeHostProxy):
    * UIProcess/DrawingAreaProxyImpl.cpp:
    (WebKit::DrawingAreaProxyImpl::didReceiveCoordinatedLayerTreeHostProxyMessage):
    * UIProcess/WebBatteryManagerProxy.h:
    (WebBatteryManagerProxy):
    * UIProcess/WebNetworkInfoManagerProxy.h:
    (WebNetworkInfoManagerProxy):
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::didReceiveMessage):
    * UIProcess/WebVibrationProxy.h:
    (WebVibrationProxy):
    * UIProcess/soup/WebSoupRequestManagerProxy.h:
    (WebSoupRequestManagerProxy):
    * WebProcess/Battery/WebBatteryManager.h:
    (WebBatteryManager):
    * WebProcess/NetworkInfo/WebNetworkInfoManager.h:
    (WebNetworkInfoManager):
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.h:
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    (WebKit::DrawingAreaImpl::didReceiveCoordinatedLayerTreeHostMessage):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::didReceiveMessage):
    * WebProcess/soup/WebSoupRequestManager.h:
    (WebSoupRequestManager):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141366 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -1935,7 +1935,7 @@ void WebPageProxy::didReceiveMessage(CoreIPC::Connection* connection, CoreIPC::M
 
 #if USE(COORDINATED_GRAPHICS)
     if (decoder.messageReceiverName() == Messages::CoordinatedLayerTreeHostProxy::messageReceiverName()) {
-        m_drawingArea->didReceiveCoordinatedLayerTreeHostProxyMessage(connection, messageID, decoder);
+        m_drawingArea->didReceiveCoordinatedLayerTreeHostProxyMessage(connection, decoder);
         return;
     }
 #endif
