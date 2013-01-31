b73f9d2989ffda25d055f42a91fb07b2caec2bad
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 04:34:48 +0000

    overscroll: Allow taking screenshots only in tabbed browsers.
    Avoid taking screenshots for apps or panels, and make sure overscroll is allowed
    before taking the screenshot.
    
    BUG=160668, 172833
    
    Review URL: https://codereview.chromium.org/12082083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179792 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -2740,7 +2740,8 @@ void WebContentsImpl::DidNavigate(
     // RenderViewHostManager::DidNavigateMainFrame, because that can change
     // WebContents::GetRenderViewHost to return the new host, instead of the one
     // that may have just been swapped out.
-    controller_.TakeScreenshot();
+    if (delegate_ && delegate_->CanOverscrollContent())
+      controller_.TakeScreenshot();
 
     render_manager_.DidNavigateMainFrame(rvh);
   }
