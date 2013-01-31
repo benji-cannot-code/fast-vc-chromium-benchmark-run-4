dded0d461e36ebf8a7bea7f4dc6b906901d57e0b
rafael.lobo@openbossa.org <rafael.lobo@openbossa.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 23:26:01 +0000

    [Qt][WK2] Fix build after removal of MessageID.h https://bugs.webkit.org/show_bug.cgi?id=108534
    Reviewed by Anders Carlsson.
    
    * Platform/CoreIPC/unix/ConnectionUnix.cpp:
    (CoreIPC::MessageInfo::MessageInfo):
    (CoreIPC::MessageInfo::setMessageBodyIsOutOfLine):
    (CoreIPC::MessageInfo::isMessageBodyIsOutOfLine):
    (MessageInfo):
    (CoreIPC::Connection::processMessage):
    (CoreIPC::Connection::sendOutgoingMessage):
    * Target.pri:
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp:
    * UIProcess/DrawingAreaProxy.cpp:
    * WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.cpp:
    * WebProcess/soup/WebSoupRequestManager.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141495 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/DrawingAreaProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/DrawingAreaProxy.cpp
@@ -31,7 +31,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(COORDINATED_GRAPHICS)
 #include "CoordinatedLayerTreeHostProxy.h"
-#include <CoreIPC/MessageID.h>
 #endif
 
 using namespace WebCore;
--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/CoordinatedGraphics/CoordinatedLayerTreeHost.cpp
@@ -36,7 +36,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CoordinatedLayerTreeHostProxyMessages.h"
 #include "DrawingAreaImpl.h"
 #include "GraphicsContext.h"
-#include "MessageID.h"
 #include "SurfaceUpdateInfo.h"
 #include "WebCoordinatedSurface.h"
 #include "WebCoreArgumentCoders.h"
