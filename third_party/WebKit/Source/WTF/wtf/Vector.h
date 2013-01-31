68b29c0b3b62402e1bb97ddfaa8ac792de96ecf0
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 13:34:32 +0000

    Vector should consult allocator about ideal size when choosing capacity. <http://webkit.org/b/108410> <rdar://problem/13124002>
    Source/JavaScriptCore:
    
    Patch by Filip Pizlo <fpizlo@apple.com> on 2013-01-30
    Reviewed by Benjamin Poulain.
    
    Remove assertion about Vector capacity that won't hold anymore since capacity()
    may not be what you passed to reserveCapacity().
    
    * bytecode/CodeBlock.cpp:
    (JSC::CodeBlock::CodeBlock):
    
    Source/WTF:
    
    Reviewed by Benjamin Poulain.
    
    Added WTF::fastMallocGoodSize(), a workalike/wrapper for OS X's malloc_good_size().
    It returns the actual size of the block that will get allocated for a given byte size.
    
    Vector's internal buffer now checks with the allocator if the resulting allocation
    could actually house more objects and updates its capacity to make use of the space.
    
    * wtf/Deque.h:
    (WTF::::expandCapacity):
    * wtf/FastMalloc.cpp:
    (WTF::fastMallocGoodSize):
    * wtf/FastMalloc.h:
    * wtf/Vector.h:
    (WTF::VectorBufferBase::allocateBuffer):
    (WTF::VectorBufferBase::tryAllocateBuffer):
    (WTF::VectorBufferBase::reallocateBuffer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Vector.h
+++ third_party/WebKit/Source/WTF/wtf/Vector.h
@@ -253,10 +253,11 @@ namespace WTF {
         void allocateBuffer(size_t newCapacity)
         {
             ASSERT(newCapacity);
-            m_capacity = newCapacity;
             if (newCapacity > std::numeric_limits<size_t>::max() / sizeof(T))
                 CRASH();
-            m_buffer = static_cast<T*>(fastMalloc(newCapacity * sizeof(T)));
+            size_t sizeToAllocate = fastMallocGoodSize(newCapacity * sizeof(T));
+            m_capacity = sizeToAllocate / sizeof(T);
+            m_buffer = static_cast<T*>(fastMalloc(sizeToAllocate));
         }
 
         bool tryAllocateBuffer(size_t newCapacity)
@@ -265,9 +266,10 @@ namespace WTF {
             if (newCapacity > std::numeric_limits<size_t>::max() / sizeof(T))
                 return false;
 
+            size_t sizeToAllocate = fastMallocGoodSize(newCapacity * sizeof(T));
             T* newBuffer;
-            if (tryFastMalloc(newCapacity * sizeof(T)).getValue(newBuffer)) {
-                m_capacity = newCapacity;
+            if (tryFastMalloc(sizeToAllocate).getValue(newBuffer)) {
+                m_capacity = sizeToAllocate / sizeof(T);
                 m_buffer = newBuffer;
                 return true;
             }
@@ -282,10 +284,11 @@ namespace WTF {
         void reallocateBuffer(size_t newCapacity)
         {
             ASSERT(shouldReallocateBuffer(newCapacity));
-            m_capacity = newCapacity;
             if (newCapacity > std::numeric_limits<size_t>::max() / sizeof(T))
                 CRASH();
-            m_buffer = static_cast<T*>(fastRealloc(m_buffer, newCapacity * sizeof(T)));
+            size_t sizeToAllocate = fastMallocGoodSize(newCapacity * sizeof(T));
+            m_capacity = sizeToAllocate / sizeof(T);
+            m_buffer = static_cast<T*>(fastRealloc(m_buffer, sizeToAllocate));
         }
 
         void deallocateBuffer(T* bufferToDeallocate)
