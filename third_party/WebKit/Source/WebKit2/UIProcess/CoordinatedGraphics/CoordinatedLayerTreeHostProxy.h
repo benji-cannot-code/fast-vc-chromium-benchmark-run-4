49b90a5ef90bdd9276d29de7c127cb4a0e1de7b5
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 05:22:01 +0000

    Coordinated Graphics : Remove CoordinatedLayerTreeHostProxy dependency from LayerTreeRenderer https://bugs.webkit.org/show_bug.cgi?id=108164
    Patch by Jae Hyun Park <jae.park@company100.net> on 2013-01-30
    Reviewed by Benjamin Poulain.
    
    This is a preparation patch for Threaded Coordinated Graphics.
    
    LayerTreeRenderer should not depend on CoordinatedLayerTreeHostProxy so that it
    can be moved to WebCore.  This patch introduces LayerTreeRendererClient which
    is implemented in CoordinatedLayerTreeHostProxy. LayerTreeRenderer uses this
    client, instead of using CoordinatedLayerTreeHostProxy directly.
    
    * UIProcess/CoordinatedGraphics/CoordinatedLayerTreeHostProxy.h:
    (CoordinatedLayerTreeHostProxy):
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.cpp:
    (WebKit::LayerTreeRenderer::LayerTreeRenderer):
    (WebKit::LayerTreeRenderer::animationFrameReady):
    (WebKit::LayerTreeRenderer::updateViewport):
    (WebKit::LayerTreeRenderer::renderNextFrame):
    (WebKit::LayerTreeRenderer::purgeBackingStores):
    (WebKit::LayerTreeRenderer::detach):
    * UIProcess/CoordinatedGraphics/LayerTreeRenderer.h:
    (WebKit):
    (LayerTreeRendererClient):
    (LayerTreeRenderer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141376 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/CoordinatedLayerTreeHostProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/CoordinatedGraphics/CoordinatedLayerTreeHostProxy.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "CoordinatedGraphicsArgumentCoders.h"
 #include "CoordinatedLayerInfo.h"
 #include "DrawingAreaProxy.h"
+#include "LayerTreeRenderer.h"
 #include "Region.h"
 #include "SurfaceUpdateInfo.h"
 #include "WebCoordinatedSurface.h"
@@ -47,7 +48,7 @@ namespace WebKit {
 class CoordinatedLayerInfo;
 class LayerTreeRenderer;
 
-class CoordinatedLayerTreeHostProxy {
+class CoordinatedLayerTreeHostProxy : public LayerTreeRendererClient {
     WTF_MAKE_NONCOPYABLE(CoordinatedLayerTreeHostProxy);
     WTF_MAKE_FAST_ALLOCATED;
 public:
@@ -78,8 +79,6 @@ public:
     void clearImageBackingContents(CoordinatedImageBackingID);
     void removeImageBacking(CoordinatedImageBackingID);
     void didReceiveCoordinatedLayerTreeHostProxyMessage(CoreIPC::Connection*, CoreIPC::MessageDecoder&);
-    void updateViewport();
-    void renderNextFrame();
     void didChangeScrollPosition(const WebCore::FloatPoint& position);
 #if USE(GRAPHICS_SURFACE)
     void createCanvas(CoordinatedLayerID, const WebCore::IntSize&, const WebCore::GraphicsSurfaceToken&);
@@ -87,15 +86,21 @@ public:
     void destroyCanvas(CoordinatedLayerID);
 #endif
     void setLayerRepaintCount(CoordinatedLayerID, int value);
-    void purgeBackingStores();
     LayerTreeRenderer* layerTreeRenderer() const { return m_renderer.get(); }
     void setLayerAnimations(CoordinatedLayerID, const WebCore::GraphicsLayerAnimations&);
     void setAnimationsLocked(bool);
 #if ENABLE(REQUEST_ANIMATION_FRAME)
     void requestAnimationFrame();
-    void animationFrameReady();
 #endif
     void setBackgroundColor(const WebCore::Color&);
+
+    // LayerTreeRendererClient Methods.
+#if ENABLE(REQUEST_ANIMATION_FRAME)
+    virtual void animationFrameReady() OVERRIDE;
+#endif
+    virtual void updateViewport() OVERRIDE;
+    virtual void renderNextFrame() OVERRIDE;
+    virtual void purgeBackingStores() OVERRIDE;
 
 protected:
     void dispatchUpdate(const Function<void()>&);
