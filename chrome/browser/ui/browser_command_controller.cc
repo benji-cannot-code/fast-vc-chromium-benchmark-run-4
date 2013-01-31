036da4c82c6b6a657e7b0c03564ac9e487a06d75
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 00:16:01 +0000

    Fix failing test on Mac 10.6: TestInstallThemeInFullScreen
    The initial change in r179554 [1] presumed that IDC_FULLSCREEN would only be issued on Mac 10.7 and later. TestInstallThemeInFullScreen calls fullscreen without a platform & os version check.
    
    This solution is to tolerate IDC_FULLSCREEN and convert it to a IDC_PRESENTATION_MODE on Mac 10.6 and earlier. Both are conceptually the same "fullscreen with no browser chrome".
    
    [1] http://src.chromium.org/viewvc/chrome?view=rev&revision=179554
    
    BUG=169138
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12096063
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_command_controller.cc
+++ chrome/browser/ui/browser_command_controller.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/keycodes/keyboard_codes.h"
 
 #if defined(OS_MACOSX)
+#include "base/mac/mac_util.h"
 #include "chrome/browser/ui/browser_commands_mac.h"
 #endif
 
@@ -427,7 +428,10 @@ void BrowserCommandController::ExecuteCommandWithDisposition(
       break;
     case IDC_FULLSCREEN:
 #if defined(OS_MACOSX)
-      chrome::ToggleFullscreenWithChrome(browser_);
+      if (base::mac::IsOSLionOrLater())
+        chrome::ToggleFullscreenWithChrome(browser_);
+      else
+        chrome::ToggleFullscreenMode(browser_);
 #else
       chrome::ToggleFullscreenMode(browser_);
 #endif
