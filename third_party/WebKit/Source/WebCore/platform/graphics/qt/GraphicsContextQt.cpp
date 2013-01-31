516384efcb631852e39e76b6f4a23e4e0dd705b9
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 14:07:58 +0000

    [Qt] Box shadows on a transparency layer is very slow https://bugs.webkit.org/show_bug.cgi?id=107547
    Reviewed by Noam Rosenthal.
    
    Include the window boundaries in the clip returned by GraphicsContext,
    since QPainter may remember clips larger than the destination, but
    ShadowBlur uses the clipBounds to determine the size of the shadow layer.
    
    * platform/graphics/qt/GraphicsContextQt.cpp:
    (WebCore::GraphicsContext::clipBounds):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141413 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/qt/GraphicsContextQt.cpp
@@ -822,10 +822,10 @@ IntRect GraphicsContext::clipBounds() const
     QPainter* p = m_data->p();
     QRectF clipRect;
 
+    clipRect = p->transform().inverted().mapRect(p->window());
+
     if (p->hasClipping())
-        clipRect = m_data->clipBoundingRect();
-    else
-        clipRect = p->transform().inverted().mapRect(p->window());
+        clipRect = clipRect.intersected(m_data->clipBoundingRect());
 
     return enclosingIntRect(clipRect);
 }
