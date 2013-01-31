c4034b768535ecf3260bf35346cd710a1551c8f6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 17:44:08 +0000

    [chromium] Remove dead transitional code from WebViewImpl https://bugs.webkit.org/show_bug.cgi?id=107889
    Patch by James Robinson <jamesr@chromium.org> on 2013-01-31
    Reviewed by Adam Barth.
    
    Source/WebKit/chromium:
    
    The chromium side of this landed at r178256 and seems stable.
    
    * public/WebWidget.h:
    (WebKit::WebWidget::setCompositorSurfaceReady):
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::WebViewImpl):
    (WebKit::WebViewImpl::~WebViewImpl):
    (WebKit):
    (WebKit::WebViewImpl::setIsTransparent):
    (WebKit::WebViewImpl::setIsAcceleratedCompositingActive):
    * src/WebViewImpl.h:
    * tests/ScrollingCoordinatorChromiumTest.cpp:
    (WebKit::FakeWebViewClient::initializeLayerTreeView):
    (FakeWebViewClient):
    (WebKit::FakeWebViewClient::layerTreeView):
    (WebKit::ScrollingCoordinatorChromiumTest::ScrollingCoordinatorChromiumTest):
    (ScrollingCoordinatorChromiumTest):
    
    Tools:
    
    ScrollingCoordinatorChromiumTests need to initialize compositing, so its implementation of
    WebWidgetClient has to support the new compositor initialization path.
    
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::initializeLayerTreeView):
    (WebViewHost::setWebWidget):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -419,14 +419,12 @@ WebViewImpl::WebViewImpl(WebViewClient* client)
     , m_benchmarkSupport(this)
 #if USE(ACCELERATED_COMPOSITING)
     , m_layerTreeView(0)
-    , m_ownsLayerTreeView(false)
     , m_rootLayer(0)
     , m_rootGraphicsLayer(0)
     , m_isAcceleratedCompositingActive(false)
     , m_layerTreeViewCommitsDeferred(false)
     , m_compositorCreationFailed(false)
     , m_recreatingGraphicsContext(false)
-    , m_compositorSurfaceReady(false)
     , m_inputHandlerIdentifier(-1)
 #endif
 #if ENABLE(INPUT_SPEECH)
@@ -512,8 +510,6 @@ WebViewImpl::WebViewImpl(WebViewClient* client)
 
 WebViewImpl::~WebViewImpl()
 {
-    if (m_ownsLayerTreeView)
-        delete m_layerTreeView;
     ASSERT(!m_page);
 }
 
@@ -839,12 +835,6 @@ void WebViewImpl::transferActiveWheelFlingAnimation(const WebActiveWheelFlingPar
     OwnPtr<WebGestureCurve> curve = adoptPtr(Platform::current()->createFlingAnimationCurve(parameters.sourceDevice, WebFloatPoint(parameters.delta), parameters.cumulativeScroll));
     m_gestureAnimation = WebActiveGestureAnimation::createWithTimeOffset(curve.release(), this, parameters.startTime);
     scheduleAnimation();
-}
-
-void WebViewImpl::renderingStats(WebRenderingStats& stats) const
-{
-    if (m_layerTreeView)
-        m_layerTreeView->renderingStats(stats);
 }
 
 void WebViewImpl::startPageScaleAnimation(const IntPoint& targetPosition, bool useAnchor, float newScale, double durationInSeconds)
@@ -1792,13 +1782,6 @@ void WebViewImpl::updateBatteryStatus(const WebBatteryStatus& status)
 }
 #endif
 
-void WebViewImpl::setCompositorSurfaceReady()
-{
-    m_compositorSurfaceReady = true;
-    if (m_layerTreeView)
-        m_layerTreeView->setSurfaceReady();
-}
-
 void WebViewImpl::animate(double)
 {
 #if ENABLE(REQUEST_ANIMATION_FRAME)
@@ -1976,15 +1959,6 @@ void WebViewImpl::setNeedsRedraw()
     if (m_layerTreeView && isAcceleratedCompositingActive())
         m_layerTreeView->setNeedsRedraw();
 #endif
-}
-
-bool WebViewImpl::isInputThrottled() const
-{
-#if USE(ACCELERATED_COMPOSITING)
-    if (m_layerTreeView && isAcceleratedCompositingActive())
-        return m_layerTreeView->commitRequested();
-#endif
-    return false;
 }
 
 void WebViewImpl::enterFullScreenForElement(WebCore::Element* element)
@@ -3646,9 +3620,6 @@ void WebViewImpl::setIsTransparent(bool isTransparent)
 
     if (m_nonCompositedContentHost)
         m_nonCompositedContentHost->setOpaque(!isTransparent);
-
-    if (m_layerTreeView)
-        m_layerTreeView->setHasTransparentBackground(isTransparent);
 }
 
 bool WebViewImpl::isTransparent() const
@@ -4128,10 +4099,6 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
 
         m_client->initializeLayerTreeView(this, *m_rootLayer, layerTreeViewSettings);
         m_layerTreeView = m_client->layerTreeView();
-        if (!m_layerTreeView) {
-            m_layerTreeView = Platform::current()->compositorSupport()->createLayerTreeView(this, *m_rootLayer, layerTreeViewSettings);
-            m_ownsLayerTreeView = true;
-        }
         if (m_layerTreeView) {
             if (m_webSettings->applyDeviceScaleFactorInCompositor() && page()->deviceScaleFactor() != 1)
                 setDeviceScaleFactor(page()->deviceScaleFactor());
@@ -4139,8 +4106,6 @@ void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
             bool visible = page()->visibilityState() == PageVisibilityStateVisible;
             m_layerTreeView->setVisible(visible);
             m_layerTreeView->setPageScaleFactorAndLimits(pageScaleFactor(), m_minimumPageScaleFactor, m_maximumPageScaleFactor);
-            if (m_compositorSurfaceReady)
-                m_layerTreeView->setSurfaceReady();
             m_layerTreeView->setHasTransparentBackground(isTransparent());
             updateLayerTreeViewport();
             m_client->didActivateCompositor(m_inputHandlerIdentifier);
