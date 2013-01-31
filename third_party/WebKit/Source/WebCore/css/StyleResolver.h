87fb92a2669b94b552e956f67238c45e400d78b9
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-01-31 05:03:58 +0000

    Split CSSOMWrapper data and functions out from StyleResolver into its own class. https://bugs.webkit.org/show_bug.cgi?id=107779
    Reviewed by Dimitri Glazkov.
    
    Factored CSSOMWrapper logic and data out from StyleResolver into a
    InspectorCSSOMWrappers class.  Since InspectorCSSOMWrappers
    depends on static variables defined in StyleResolver.cpp, this
    patch does not extract a new class into a new file, which makes a
    review easier since it produces readable diffs.
    
    After we factor static variables related to default style sheets
    in a following patch (bug 107780), I'll move
    InspectorCSSOMWrappers into a its own file.
    
    No new tests, refactoring only.
    
    * css/StyleResolver.cpp:
    (WebCore):
    (WebCore::StyleResolver::appendAuthorStyleSheets):
    (WebCore::InspectorCSSOMWrappers::collectFromStyleSheetIfNeeded):
    (WebCore::InspectorCSSOMWrappers::collect):
    (WebCore::InspectorCSSOMWrappers::collectFromStyleSheetContents): Name it explicitly rather than overloading.
    (WebCore::InspectorCSSOMWrappers::collectFromStyleSheets): Name it explicitly rather than overloading.
    (WebCore::InspectorCSSOMWrappers::collectFromDocumentStyleSheetCollection): Name it explicitly rather than overloading.
    (WebCore::InspectorCSSOMWrappers::getWrapperForRuleInSheets): Renamed from ensureFullCSSOMWrapperForInspector().
    (WebCore::InspectorCSSOMWrappers::reportMemoryUsage):
    (WebCore::StyleResolver::reportMemoryUsage):
    * css/StyleResolver.h:
    (InspectorCSSOMWrappers):
    (WebCore):
    (StyleResolver):
    (WebCore::StyleResolver::inspectorCSSOMWrappers):
    * inspector/InspectorCSSAgent.cpp:
    (WebCore::InspectorCSSAgent::willMatchRule):
    (WebCore::InspectorCSSAgent::willProcessRule):
    (WebCore::InspectorCSSAgent::buildObjectForRule):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141373 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/StyleResolver.h
+++ third_party/WebKit/Source/WebCore/css/StyleResolver.h
@@ -133,6 +133,27 @@ enum RuleMatchingBehavior {
     MatchOnlyUserAgentRules,
 };
 
+class InspectorCSSOMWrappers {
+public:
+    // WARNING. This will construct CSSOM wrappers for all style rules and cache them in a map for significant memory cost.
+    // It is here to support inspector. Don't use for any regular engine functions.
+    CSSStyleRule* getWrapperForRuleInSheets(StyleRule*, DocumentStyleSheetCollection*);
+    void collectFromStyleSheetIfNeeded(CSSStyleSheet*);
+
+    void reportMemoryUsage(MemoryObjectInfo*) const;
+
+private:
+    template <class ListType>
+    void collect(ListType*);
+
+    void collectFromStyleSheetContents(HashSet<RefPtr<CSSStyleSheet> >& sheetWrapperSet, StyleSheetContents*);
+    void collectFromStyleSheets(const Vector<RefPtr<CSSStyleSheet> >&);
+    void collectFromDocumentStyleSheetCollection(DocumentStyleSheetCollection*);
+
+    HashMap<StyleRule*, RefPtr<CSSStyleRule> > m_styleRuleToCSSOMWrapperMap;
+    HashSet<RefPtr<CSSStyleSheet> > m_styleSheetCSSOMWrapperSet;
+};
+
 // This class selects a RenderStyle for a given element based on a collection of stylesheets.
 class StyleResolver {
     WTF_MAKE_NONCOPYABLE(StyleResolver); WTF_MAKE_FAST_ALLOCATED;
@@ -276,10 +297,6 @@ public:
     static bool createTransformOperations(CSSValue* inValue, RenderStyle* inStyle, RenderStyle* rootStyle, TransformOperations& outOperations);
     
     void invalidateMatchedPropertiesCache();
-    
-    // WARNING. This will construct CSSOM wrappers for all style rules and cache then in a map for significant memory cost.
-    // It is here to support inspector. Don't use for any regular engine functions.
-    CSSStyleRule* ensureFullCSSOMWrapperForInspector(StyleRule*);
 
 #if ENABLE(CSS_FILTERS)
     bool createFilterOperations(CSSValue* inValue, RenderStyle* inStyle, RenderStyle* rootStyle, FilterOperations& outOperations);
@@ -445,6 +462,7 @@ public:
     static Length convertToFloatLength(CSSPrimitiveValue*, RenderStyle*, RenderStyle* rootStyle, double multiplier = 1);
 
     CSSToStyleMap* styleMap() { return &m_styleMap; }
+    InspectorCSSOMWrappers& inspectorCSSOMWrappers() { return m_inspectorCSSOMWrappers; }
 
     void reportMemoryUsage(MemoryObjectInfo*) const;
     
@@ -536,9 +554,6 @@ private:
     bool m_applyPropertyToRegularStyle;
     bool m_applyPropertyToVisitedLinkStyle;
     const StyleBuilder& m_styleBuilder;
-    
-    HashMap<StyleRule*, RefPtr<CSSStyleRule> > m_styleRuleToCSSOMWrapperMap;
-    HashSet<RefPtr<CSSStyleSheet> > m_styleSheetCSSOMWrapperSet;
 
 #if ENABLE(CSS_SHADERS)
     bool m_hasPendingShaders;
@@ -550,6 +565,7 @@ private:
 
     OwnPtr<StyleScopeResolver> m_scopeResolver;
     CSSToStyleMap m_styleMap;
+    InspectorCSSOMWrappers m_inspectorCSSOMWrappers;
 
     friend class StyleBuilder;
     friend bool operator==(const MatchedProperties&, const MatchedProperties&);
