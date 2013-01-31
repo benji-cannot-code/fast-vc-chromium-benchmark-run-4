ec195f4dcab4a4dcc518c38905180053b12702d5
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 20:42:26 +0000

    Revert 179635 > [content shell] ShellRenderProcessObserver no longer depends on WebTestInterfaces. > > R=jochen > BUG=173079 > > Review URL: https://chromiumcodereview.appspot.com/12090066
    TBR=marja@chromium.org
    BUG=173456
    Review URL: https://codereview.chromium.org/12124002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179946 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_render_process_observer.h
+++ content/shell/shell_render_process_observer.h
@@ -18,6 +18,7 @@ class WebFrame;
 
 namespace WebTestRunner {
 class WebTestDelegate;
+class WebTestInterfaces;
 }
 
 namespace content {
@@ -44,6 +45,9 @@ class ShellRenderProcessObserver : public RenderProcessObserver {
   WebTestRunner::WebTestDelegate* test_delegate() const {
     return test_delegate_;
   }
+  WebTestRunner::WebTestInterfaces* test_interfaces() const {
+    return test_interfaces_.get();
+  }
   WebKitTestRunner* main_test_runner() const { return main_test_runner_; }
   const FilePath& webkit_source_dir() const { return webkit_source_dir_; }
 
@@ -52,6 +56,7 @@ class ShellRenderProcessObserver : public RenderProcessObserver {
   void OnResetAll();
   void OnSetWebKitSourceDir(const FilePath& webkit_source_dir);
 
+  scoped_ptr<WebTestRunner::WebTestInterfaces> test_interfaces_;
   RenderView* main_render_view_;
   WebKitTestRunner* main_test_runner_;
   WebTestRunner::WebTestDelegate* test_delegate_;
