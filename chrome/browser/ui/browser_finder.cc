d2c1fd6cc28ecaf2f789d87eddd2ef3ae36542e9
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-01-31 20:14:21 +0000

    Convert all methods in browser_finder.cc meant to go over all browsers to use BrowserIterator (multi-desktop aware) instead of BrowserList::iterator (native desktop only).
    Also removed chrome::FindHostDesktopTypeForWebContents(content::WebContents*) which was no longer used. Consumers can still use chrome::FindBrowserWithWebContents(content::WebContents*) and query the returned browser's host_desktop_type() OR chrome::GetHostDesktopTypeForNativeView(gfx::NativeView) passing in the web contents' GetNativeView() as a parameter.
    
    This also makes reopened tabs in Ash go in their previous browsers as desired (instead of creating a new browser since it previously wouldn't find it's previous browser by ID).
    
    BUG=129187, 170753
    
    Review URL: https://chromiumcodereview.appspot.com/12095086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@179939 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_finder.cc
+++ chrome/browser/ui/browser_finder.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_finder.h"
 
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/browser_list.h"
+#include "chrome/browser/ui/browser_iterator.h"
 #include "chrome/browser/ui/browser_list_impl.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tab_contents/tab_contents_iterator.h"
@@ -114,7 +114,7 @@ size_t GetBrowserCountImpl(Profile* profile,
       chrome::BrowserListImpl::GetInstance(desktop_type);
   size_t count = 0;
   if (browser_list_impl) {
-    for (BrowserList::const_iterator i = browser_list_impl->begin();
+    for (chrome::BrowserListImpl::const_iterator i = browser_list_impl->begin();
          i != browser_list_impl->end(); ++i) {
       if (BrowserMatches(*i, profile, Browser::FEATURE_NONE, match_types))
         count++;
@@ -158,17 +158,15 @@ Browser* FindBrowserWithProfile(Profile* profile,
 }
 
 Browser* FindBrowserWithID(SessionID::id_type desired_id) {
-  for (BrowserList::const_iterator i = BrowserList::begin();
-       i != BrowserList::end(); ++i) {
-    if ((*i)->session_id().id() == desired_id)
-      return *i;
+  for (BrowserIterator it; !it.done(); it.Next()) {
+    if (it->session_id().id() == desired_id)
+      return *it;
   }
   return NULL;
 }
 
 Browser* FindBrowserWithWindow(gfx::NativeWindow window) {
-  for (BrowserList::const_iterator it = BrowserList::begin();
-       it != BrowserList::end(); ++it) {
+  for (BrowserIterator it; !it.done(); it.Next()) {
     Browser* browser = *it;
     if (browser->window() && browser->window()->GetNativeWindow() == window)
       return browser;
@@ -183,12 +181,6 @@ Browser* FindBrowserWithWebContents(const WebContents* web_contents) {
       return it.browser();
   }
   return NULL;
-}
-
-HostDesktopType FindHostDesktopTypeForWebContents(
-    const WebContents* web_contents) {
-  Browser* browser = FindBrowserWithWebContents(web_contents);
-  return browser ? browser->host_desktop_type() : HOST_DESKTOP_TYPE_NATIVE;
 }
 
 Browser* FindLastActiveWithProfile(Profile* profile, HostDesktopType type) {
