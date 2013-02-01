0f86ed5c529932ac80ace04fe6f243e2e32f6a31
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 07:43:13 +0000

    Editor::m_compositionNode not updated on HTMLInputElement::setValue() https://bugs.webkit.org/show_bug.cgi?id=107737
    Patch by Aurimas Liutikas <aurimas@chromium.org> on 2013-01-31
    Reviewed by Ryosuke Niwa.
    
    Source/WebCore:
    
    Chromium has a bug where the IME composition did not get cancelled on JavaScript changes
    to the focused editing field. Most of other WebKit ports were already doing this check
    in their EditorClient::respondToChangedSelection. I took that logic and moved it to the
    Editor so every port and use the same code.
    
    An existing test editing/input/setting-input-value-cancel-ime-composition.html covers this change.
    This test used to have an expectation to fail on Chromium and after this patch it will start passing.
    
    * editing/Editor.cpp:
    (WebCore::Editor::cancelCompositionIfSelectionIsInvalid):
        Adding a call that can be used by any the port to cancel the composition if it's no longer valid.
    (WebCore):
    * editing/Editor.h:
    (Editor):
    
    Source/WebKit/chromium:
    
    * public/WebViewClient.h:
    (WebKit::WebViewClient::didCancelCompositionOnSelectionChange):
        Adding a callback to let the WebViewClient know that the composition has been cancelled.
    * src/EditorClientImpl.cpp:
    (WebKit::EditorClientImpl::respondToChangedSelection):
        Adding a call composition if it is no longer valid.
    
    Source/WebKit/efl:
    
    * WebCoreSupport/EditorClientEfl.cpp:
    (WebCore::EditorClientEfl::respondToChangedSelection):
        Adding a call to the newly refactored method.
    
    Source/WebKit/gtk:
    
    * WebCoreSupport/EditorClientGtk.cpp:
    (WebKit::EditorClient::respondToChangedSelection):
        Adding a call to the newly refactored Editor method.
    
    Source/WebKit/mac:
    
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView _updateSelectionForInputManager]):
    
    Source/WebKit/win:
    
    * WebView.cpp:
    (WebView::updateSelectionForIME):
        Adding a call to the newly refactored method.
    
    LayoutTests:
    
    * platform/chromium/TestExpectations: Removed fail expectation for the editing/input/setting-input-value-cancel-ime-composition.html since this patch fixes the bug https://bugs.webkit.org/show_bug.cgi?id=55560
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebViewClient.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebViewClient.h
@@ -191,6 +191,7 @@ public:
     virtual bool isSelectTrailingWhitespaceEnabled() { return true; }
 
     virtual void didBeginEditing() { }
+    virtual void didCancelCompositionOnSelectionChange() { }
     virtual void didChangeSelection(bool isSelectionEmpty) { }
     virtual void didChangeContents() { }
     virtual void didExecuteCommand(const WebString& commandName) { }
