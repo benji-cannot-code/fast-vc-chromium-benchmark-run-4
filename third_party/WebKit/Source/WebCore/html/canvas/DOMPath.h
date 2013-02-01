833b981f2cd64f4cfab7ad2c4bc03eaf7f9b86b8
krit@webkit.org <krit@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 20:11:41 +0000

    [canvas] Add more constructors to Path https://bugs.webkit.org/show_bug.cgi?id=108587
    Patch by Dirk Schulze <krit@webkit.org> on 2013-01-31
    Reviewed by Dean Jackson.
    
    Source/WebCore:
    
    Add more constructors to Path object to make it possible
    to copy Path objects and parse SVG strings into a Path
    object that can be used on the canvas context afterwards.
    
    Test: fast/canvas/canvas-path-constructors.html
    
    * html/canvas/DOMPath.h: New constructors for Path.
    (WebCore::DOMPath::create):
    (WebCore::DOMPath::DOMPath):
    * html/canvas/DOMPath.idl: Ditto.
    
    LayoutTests:
    
    Add tests for new constructors of Path objects.
    Since the feature is behind a flag, the test is skipped on
    all platforms for now.
    
    * fast/canvas/canvas-path-constructors-expected.txt: Added.
    * fast/canvas/canvas-path-constructors.html: Added.
    * fast/canvas/script-tests/canvas-path-constructors.js: Added.
    * platform/chromium/TestExpectations:
    * platform/efl/TestExpectations:
    * platform/gtk/TestExpectations:
    * platform/mac/TestExpectations:
    * platform/win/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/DOMPath.h
+++ third_party/WebKit/Source/WebCore/html/canvas/DOMPath.h
@@ -31,6 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(CANVAS_PATH)
 #include "CanvasPathMethods.h"
+#if ENABLE(SVG)
+#include "SVGPathUtilities.h"
+#endif
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
 
@@ -40,6 +43,9 @@ class DOMPath : public RefCounted<DOMPath>, public CanvasPathMethods {
     WTF_MAKE_NONCOPYABLE(DOMPath); WTF_MAKE_FAST_ALLOCATED;
 public:
     static PassRefPtr<DOMPath> create() { return adoptRef(new DOMPath); }
+    static PassRefPtr<DOMPath> create(const String& pathData) { return adoptRef(new DOMPath(pathData)); }
+    static PassRefPtr<DOMPath> create(DOMPath* path) { return adoptRef(new DOMPath(path)); }
+
     static PassRefPtr<DOMPath> create(const Path& path) { return adoptRef(new DOMPath(path)); }
 
     const Path& path() const { return m_path; }
@@ -52,6 +58,18 @@ private:
     {
         m_path = path;
     }
+    DOMPath(DOMPath* path)
+        : CanvasPathMethods()
+    {
+        m_path = path->path();
+    }
+#if ENABLE(SVG)
+    DOMPath(const String& pathData)
+        : CanvasPathMethods()
+    {
+        buildPathFromString(pathData, m_path);
+    }
+#endif
 };
 }
 #endif
