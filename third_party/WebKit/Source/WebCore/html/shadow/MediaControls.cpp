93012a6396df658ac6257798a39acb06a0009bed
vcarbune@chromium.org <vcarbune@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 04:03:04 +0000

    [Track] Closed Caption button shouldn't be visible if all the track resources have failed loading https://bugs.webkit.org/show_bug.cgi?id=106285
    Reviewed by Eric Carlson.
    
    Source/WebCore:
    
    Updated existing test cases.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::textTracksAreReady): Checked whether there are tracks not loaded
    when the algorithm began.
    (WebCore::HTMLMediaElement::textTrackReadyStateChanged): If the ready state changed to
    FailedToLoad the media controls should check whether there are other caption tracks
    and hide the button if not.
    (WebCore::HTMLMediaElement::didAddTrack): Added trigger to closedCaptionsTrackChanged.
    (WebCore::HTMLMediaElement::hasClosedCaptions): Updated check to skip tracks that
    failed to load.
    * html/shadow/MediaControls.cpp:
    (WebCore::MediaControls::reset): Used the newly added method.
    (WebCore::MediaControls::refreshClosedCaptionsButtonVisibility): Added container method for
    default behaviour for refreshing the visibility of the caption button.
    (WebCore::MediaControls::closedCaptionTracksChanged): Used the newly added method.
    (WebCore):
    * html/shadow/MediaControls.h:
    (MediaControls):
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::textTracksAreReady):
    (WebCore::HTMLMediaElement::textTrackReadyStateChanged):
    (WebCore::HTMLMediaElement::setReadyState):
    (WebCore::HTMLMediaElement::didAddTrack):
    (WebCore::HTMLMediaElement::hasClosedCaptions):
    * html/shadow/MediaControls.cpp:
    (WebCore::MediaControls::reset):
    (WebCore::MediaControls::refreshClosedCaptionsButtonVisibility):
    (WebCore::MediaControls::closedCaptionTracksChanged):
    (WebCore):
    * html/shadow/MediaControls.h:
    (MediaControls):
    
    LayoutTests:
    
    Updated tests to include improved behavior.
    
    * media/video-controls-captions-expected.txt: Updated.
    * media/video-controls-captions.html: Updated.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141531 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/MediaControls.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControls.cpp
@@ -110,12 +110,7 @@ void MediaControls::reset()
         }
     }
 
-    if (m_toggleClosedCaptionsButton) {
-        if (m_mediaController->hasClosedCaptions())
-            m_toggleClosedCaptionsButton->show();
-        else
-            m_toggleClosedCaptionsButton->hide();
-    }
+    refreshClosedCaptionsButtonVisibility();
 
     if (m_fullScreenButton) {
         if (m_mediaController->supportsFullscreen() && m_mediaController->hasVideo())
@@ -253,7 +248,7 @@ void MediaControls::changedClosedCaptionsVisibility()
         m_toggleClosedCaptionsButton->updateDisplayType();
 }
 
-void MediaControls::closedCaptionTracksChanged()
+void MediaControls::refreshClosedCaptionsButtonVisibility()
 {
     if (!m_toggleClosedCaptionsButton)
         return;
@@ -262,6 +257,11 @@ void MediaControls::closedCaptionTracksChanged()
         m_toggleClosedCaptionsButton->show();
     else
         m_toggleClosedCaptionsButton->hide();
+}
+
+void MediaControls::closedCaptionTracksChanged()
+{
+    refreshClosedCaptionsButtonVisibility();
 }
 
 void MediaControls::enteredFullscreen()
