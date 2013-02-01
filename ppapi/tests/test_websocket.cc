ed793b2ece52df25d7391617299d8535d71a00b4
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 06:54:42 +0000

    Revert 180093 > PPAPI: Fix WebSocket Var ref leak receiving binary > > BUG=173503 > > > Review URL: https://chromiumcodereview.appspot.com/12096099
    This seems to have broken WebSocket tests on the mac bots
    http://build.chromium.org/p/chromium.mac/builders/Mac10.6%20Tests%20%281%29/builds/35419
    http://build.chromium.org/p/chromium.mac/builders/Mac10.7%20Tests%20%281%29/builds/7829
    
    TBR=dmichael@chromium.org
    Review URL: https://codereview.chromium.org/12096107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180105 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_websocket.cc
+++ ppapi/tests/test_websocket.cc
@@ -465,7 +465,6 @@ std::string TestWebSocket::TestValidConnect() {
   PP_Var extensions = websocket_interface_->GetExtensions(ws);
   ASSERT_TRUE(AreEqualWithString(extensions, ""));
   core_interface_->ReleaseResource(ws);
-  ReleaseVar(extensions);
 
   PASS();
 }
