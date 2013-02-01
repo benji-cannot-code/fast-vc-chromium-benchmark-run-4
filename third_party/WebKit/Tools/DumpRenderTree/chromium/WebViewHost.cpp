09adc506c6cbfbf857344f67d7c51003ccfdc02e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 01:23:47 +0000

    [chromium] Notify the WebWidget when the WebViewHost is about to tear down compositing support https://bugs.webkit.org/show_bug.cgi?id=108518
    Patch by James Robinson <jamesr@chromium.org> on 2013-01-31
    Reviewed by Adrienne Walker.
    
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::shutdown):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141512 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -1004,6 +1004,7 @@ void WebViewHost::shutdown()
          it < m_popupmenus.end(); ++it)
         (*it)->close();
 
+    webWidget()->willCloseLayerTreeView();
     m_layerTreeView.clear();
     webWidget()->close();
     m_webWidget = 0;
