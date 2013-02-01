e824083cd7573575a58788957b32422e1a1b756a
carlosgc@webkit.org <carlosgc@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 17:18:16 +0000

    [GTK] Adapt WorkQueueGtk to the latest changes in WebKit2 after r141497 https://bugs.webkit.org/show_bug.cgi?id=108607
    Reviewed by Anders Carlsson.
    
    * Platform/gtk/WorkQueueGtk.cpp:
    (WorkQueue::EventSource::executeEventSource): Remove the is valid
    work queue check.
    (WorkQueue::EventSource): WorkQueue is now refcounted, so keep a
    reference when a new job is scheduled and unref it when it
    finishes.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141599 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/gtk/WorkQueueGtk.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/gtk/WorkQueueGtk.cpp
@@ -43,10 +43,6 @@ public:
         , m_cancellable(cancellable)
     {
     }
-    ~EventSource()
-    {
-        m_workQueue->deref();
-    }
 
     void cancel()
     {
@@ -92,7 +88,7 @@ public:
    
 public:
     Function<void()> m_function;
-    WorkQueue* m_workQueue;
+    RefPtr<WorkQueue> m_workQueue;
     GCancellable* m_cancellable;
 };
 
@@ -204,7 +200,6 @@ void WorkQueue::dispatchOnSource(GSource* dispatchSource, const Function<void()>
 
 void WorkQueue::dispatch(const Function<void()>& function)
 {
-    ref();
     GRefPtr<GSource> dispatchSource = adoptGRef(g_idle_source_new());
     ASSERT(dispatchSource);
     g_source_set_priority(dispatchSource.get(), G_PRIORITY_DEFAULT);
@@ -214,7 +209,6 @@ void WorkQueue::dispatch(const Function<void()>& function)
 
 void WorkQueue::dispatchAfterDelay(const Function<void()>& function, double delay)
 {
-    ref();
     GRefPtr<GSource> dispatchSource = adoptGRef(g_timeout_source_new(static_cast<guint>(delay * 1000)));
     ASSERT(dispatchSource);
 
@@ -223,7 +217,6 @@ void WorkQueue::dispatchAfterDelay(const Function<void()>& function, double dela
 
 void WorkQueue::dispatchOnTermination(WebKit::PlatformProcessIdentifier process, const Function<void()>& function)
 {
-    ref();
     GRefPtr<GSource> dispatchSource = adoptGRef(g_child_watch_source_new(process));
     ASSERT(dispatchSource);
 
