3cbd830ea7a3bcd89133d8f5b1f9c7883cc4f880
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 18:20:54 +0000

    Only permit plug-in loads in the browser if the plug-in isn't blocked or the user has authorized it with a browser-mediated interaction.
    (Reland https://codereview.chromium.org/12086077 with Android test tweak)
    
    BUG=172573
    R=jam@chromium.org
    Review URL: https://codereview.chromium.org/12092107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180159 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/content_settings/content_setting_bubble_model.cc
+++ chrome/browser/ui/content_settings/content_setting_bubble_model.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/custom_handlers/protocol_handler_registry.h"
 #include "chrome/browser/custom_handlers/protocol_handler_registry_factory.h"
 #include "chrome/browser/favicon/favicon_tab_helper.h"
+#include "chrome/browser/plugins/chrome_plugin_service_filter.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/blocked_content/blocked_content_tab_helper.h"
@@ -24,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/pref_names.h"
 #include "chrome/common/render_messages.h"
 #include "content/public/browser/notification_service.h"
+#include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
@@ -427,6 +429,10 @@ void ContentSettingPluginBubbleModel::OnCustomLinkClicked() {
   content::RecordAction(UserMetricsAction("ClickToPlay_LoadAll_Bubble"));
   DCHECK(web_contents());
   content::RenderViewHost* host = web_contents()->GetRenderViewHost();
+#if defined(ENABLE_PLUGINS)
+  ChromePluginServiceFilter::GetInstance()->AuthorizeAllPlugins(
+      host->GetProcess()->GetID());
+#endif
   // TODO(bauerb): We should send the identifiers of blocked plug-ins here.
   host->Send(new ChromeViewMsg_LoadBlockedPlugins(host->GetRoutingID(),
                                                   std::string()));
