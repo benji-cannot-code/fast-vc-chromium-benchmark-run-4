0ddeb011d9dd54fac3eafc037174ad6cfb038b44
enrica@apple.com <enrica@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 01:25:28 +0000

    Mac: Editor::baseWritingDirectionForSelectionStart should return WritingDirection instead of NSWritingDirection. https://bugs.webkit.org/show_bug.cgi?id=108519.
    Reviewed by Sam Weinig.
    
    Source/WebCore: 
    
    No new tests, no change in behavior.
    
    There is no need to use AppKit types here.
    baseWritingDirectionForSelectionStart now returns WritingDirection and
    WebHTMLView toggleWritingDirection has been modified accordingly.
            
    * editing/Editor.h:
    * editing/mac/EditorMac.mm:
    (WebCore::Editor::baseWritingDirectionForSelectionStart):
    
    Source/WebKit/mac: 
    
    There is no need to use AppKit types here.
    baseWritingDirectionForSelectionStart now returns WritingDirection and
    WebHTMLView toggleWritingDirection has been modified accordingly.
    
    * WebView/WebHTMLView.mm:
    (-[WebHTMLView toggleBaseWritingDirection:]):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141513 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/Editor.h
+++ third_party/WebKit/Source/WebCore/editing/Editor.h
@@ -378,7 +378,7 @@ public:
 #if PLATFORM(MAC)
     const SimpleFontData* fontForSelection(bool&) const;
     NSDictionary* fontAttributesForSelectionStart() const;
-    NSWritingDirection baseWritingDirectionForSelectionStart() const;
+    WritingDirection baseWritingDirectionForSelectionStart() const;
     bool canCopyExcludingStandaloneImages();
     void takeFindStringFromSelection();
     void writeSelectionToPasteboard(const String& pasteboardName, const Vector<String>& pasteboardTypes);
