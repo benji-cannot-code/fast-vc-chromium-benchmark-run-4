f21e12fc01ecd34e8c439421bb7e52cdc845423a
yefim@chromium.org <yefim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 00:20:12 +0000

    Changed MenuItemView to check if any menu item has a checkbox or radio button and then adjust it's layout accordingly.
    When MenuModelAdapter::BuildMenuImpl() was going recursively through all submenus it used to set has_icon to each submenu instead of top menu item. Changed it to set it to the top menu item.
    
    BUG=172990
    
    TEST=When testing please make sure other menus with checkboxes/radio buttons have proper padding.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12112002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180014 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_model_adapter.cc
+++ ui/views/controls/menu/menu_model_adapter.cc
@@ -181,6 +181,7 @@ void MenuModelAdapter::WillHideMenu(MenuItemView* menu) {
 void MenuModelAdapter::BuildMenuImpl(MenuItemView* menu, ui::MenuModel* model) {
   DCHECK(menu);
   DCHECK(model);
+  bool has_icons = model->HasIcons();
   const int item_count = model->GetItemCount();
   for (int i = 0; i < item_count; ++i) {
     MenuItemView* item = menu->AppendMenuItemFromModel(
@@ -195,12 +196,13 @@ void MenuModelAdapter::BuildMenuImpl(MenuItemView* menu, ui::MenuModel* model) {
       ui::MenuModel* submodel = model->GetSubmenuModelAt(i);
       DCHECK(submodel);
       BuildMenuImpl(item, submodel);
+      has_icons = has_icons || item->has_icons();
 
       menu_map_[item] = submodel;
     }
   }
 
-  menu->set_has_icons(model->HasIcons());
+  menu->set_has_icons(has_icons);
 }
 
 }  // namespace views
