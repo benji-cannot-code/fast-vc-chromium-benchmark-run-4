1e43873b8f23b09f1f8b3c3f98c639fe40a2ee26
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 10:51:59 +0000

    Coordinated Graphics : Sort Target.pri and class declarations in alphabetical order. https://bugs.webkit.org/show_bug.cgi?id=108590
    Patch by Jae Hyun Park <jae.park@company100.net> on 2013-02-01
    Reviewed by Noam Rosenthal.
    
    Source/WebCore:
    
    Sort CoordinatedGraphics files in Target.pri in alphabetical order.
    
    No new tests. No change in behavior.
    
    * Target.pri:
    
    Source/WebKit2:
    
    Sort class declarations in alphabetical order.
    
    * Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.h:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.h
+++ third_party/WebKit/Source/WebKit2/Shared/CoordinatedGraphics/CoordinatedGraphicsArgumentCoders.h
@@ -33,10 +33,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ArgumentCoders.h"
 
 namespace WebCore {
-class SurfaceUpdateInfo;
 class FloatPoint3D;
 class GraphicsLayerAnimation;
 class GraphicsLayerAnimations;
+class SurfaceUpdateInfo;
 class TransformationMatrix;
 class TransformOperations;
 struct CoordinatedLayerInfo;
