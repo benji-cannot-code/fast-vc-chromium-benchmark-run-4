3fafa69acf50936556bb2489fb94ac499061136e
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 21:40:40 +0000

    [Android] Remove OnTestsCompleted from gtest sharder and base sharder.
    BUG=168889
    TEST=run_tests.py
    
    Review URL: https://codereview.chromium.org/12090121
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180192 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/base/base_test_sharder.py
+++ build/android/pylib/base/base_test_sharder.py
@@ -75,10 +75,6 @@ class BaseTestSharder(object):
     """Called before starting the shards."""
     pass
 
-  def OnTestsCompleted(self, test_runners, test_results):
-    """Notifies that we completed the tests."""
-    pass
-
   def _KillHostForwarder(self):
     Forwarder.KillHost(self.build_type)
 
@@ -157,6 +153,5 @@ class BaseTestSharder(object):
       # We ran out retries, possibly out of healthy devices.
       # There's no recovery at this point.
       raise Exception('Unrecoverable error while retrying test runs.')
-    self.OnTestsCompleted(test_runners, final_results)
     self._KillHostForwarder()
     return final_results
