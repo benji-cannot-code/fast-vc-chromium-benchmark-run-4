9c249574eec38190f914c8c943389def0028ef90
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 06:36:02 +0000

    Unreviewed, followup fix for r141535.
    * tests/WebFrameTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141538 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -62,6 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <public/WebFloatRect.h>
 #include <public/WebURLResponse.h>
 #include <webkit/support/webkit_support.h>
+#include <wtf/Forward.h>
 
 using namespace WebKit;
 using WebCore::FloatRect;
