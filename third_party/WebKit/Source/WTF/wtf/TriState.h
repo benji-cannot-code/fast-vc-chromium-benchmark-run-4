36037a613ade147a2f6d199d50148b5bd493cd8b
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 15:01:34 +0000

    Added TriState to WTF and started using it in one place https://bugs.webkit.org/show_bug.cgi?id=108628
    Reviewed by Beth Dakin.
    
    ../JavaScriptCore: 
    
    * runtime/PrototypeMap.h:
    (JSC::PrototypeMap::isPrototype): Use TriState instead of boolean. In
    response to review feedback, this is an attempt to clarify that our
    'true' condition is actually just a 'maybe'.
    
    * runtime/PrototypeMap.h:
    (PrototypeMap):
    (JSC::PrototypeMap::isPrototype):
    
    ../WebCore: 
    
    * editing/EditingStyle.h:
    (WebCore): Moved TriState to WTF so it can be used in more places.
    
    ../WTF: 
    
    Useful for expressing "maybe" conditions in a boolean context.
    
    * WTF.xcodeproj/project.pbxproj:
    * wtf/TriState.h: Added.
    (WTF):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141588 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/TriState.h
+++ third_party/WebKit/Source/WTF/wtf/TriState.h
@@ -24,43 +24,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
  */
 
-#ifndef PrototypeMap_h
-#define PrototypeMap_h
+#ifndef TriState_h
+#define TriState_h
 
-#include "WeakGCMap.h"
-#include <wtf/TriState.h>
+namespace WTF {
 
-namespace JSC {
-
-class JSObject;
-class Structure;
-
-// Tracks the canonical structure an object should be allocated with when inheriting from a given prototype.
-class PrototypeMap {
-public:
-    JS_EXPORT_PRIVATE Structure* emptyObjectStructureForPrototype(JSObject*, unsigned inlineCapacity);
-    void clearEmptyObjectStructureForPrototype(JSObject*, unsigned inlineCapacity);
-    void addPrototype(JSObject*);
-    TriState isPrototype(JSObject*) const; // Returns a conservative estimate.
-
-private:
-    WeakGCMap<JSObject*, JSObject> m_prototypes;
-    typedef WeakGCMap<std::pair<JSObject*, unsigned>, Structure> StructureMap;
-    StructureMap m_structures;
+enum TriState {
+    FalseTriState,
+    TrueTriState,
+    MixedTriState
 };
 
-inline TriState PrototypeMap::isPrototype(JSObject* object) const
-{
-    if (!m_prototypes.contains(object))
-        return FalseTriState;
-
-    // We know that 'object' was used as a prototype at one time, so be
-    // conservative and say that it might still be so. (It would be expensive
-    // to find out for sure, and we don't know of any cases where being precise
-    // would improve performance.)
-    return MixedTriState;
 }
 
-} // namespace JSC
+using WTF::TriState;
+using WTF::FalseTriState;
+using WTF::TrueTriState;
+using WTF::MixedTriState;
 
-#endif // PrototypeMap_h
+#endif // TriState_h
--- third_party/WebKit/Source/WebCore/editing/EditingStyle.h
+++ third_party/WebKit/Source/WebCore/editing/EditingStyle.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2010 Google Inc. All rights reserved.
+ * Copyright (C) 2013 Apple Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -37,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Forward.h>
 #include <wtf/RefCounted.h>
 #include <wtf/RefPtr.h>
+#include <wtf/TriState.h>
 #include <wtf/Vector.h>
 #include <wtf/text/WTFString.h>
 
@@ -56,8 +58,6 @@ class RenderStyle;
 class StylePropertySet;
 class StyledElement;
 class VisibleSelection;
-
-enum TriState { FalseTriState, TrueTriState, MixedTriState };
 
 class EditingStyle : public RefCounted<EditingStyle> {
 public:
