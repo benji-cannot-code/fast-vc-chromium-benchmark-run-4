3df760fe0207060f019854a96136487af92705a4
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 17:25:03 +0000

    Move kChromeSearchScheme out of content since it's a chrome feature.
    Also remove ContentBrowserClient::RenderProcessHostDeleted since it duplicates an existing notification.
    Review URL: https://codereview.chromium.org/12087129
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180150 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1162,8 +1162,6 @@ void RenderProcessHostImpl::Cleanup() {
         Source<RenderProcessHost>(this),
         NotificationService::NoDetails());
 
-    GetContentClient()->browser()->RenderProcessHostDeleted(this);
-
     MessageLoop::current()->DeleteSoon(FROM_HERE, this);
     deleting_soon_ = true;
     // It's important not to wait for the DeleteTask to delete the channel
