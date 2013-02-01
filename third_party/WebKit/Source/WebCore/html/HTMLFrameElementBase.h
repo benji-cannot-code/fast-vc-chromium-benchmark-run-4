ea4137b7dadb490c1cbed11201ae989c237e7aa5
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 04:25:42 +0000

    Refactoring: Remove the default argument value for Node::setFocus https://bugs.webkit.org/show_bug.cgi?id=108554
    Reviewed by Kentaro Hara.
    
    There is no code to use the default argument.
    No new tests. Just a refactoring.
    
    * dom/Node.h: Remove default argument value for setFocus.
    * dom/ContainerNode.h:
    (ContainerNode): Ditto.
    * html/HTMLAreaElement.h:
    (HTMLAreaElement): Add OVERRIDE to setFocus.
    * html/HTMLFrameElementBase.h:
    (HTMLFrameElementBase): Ditto.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141533 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLFrameElementBase.h
+++ third_party/WebKit/Source/WebCore/html/HTMLFrameElementBase.h
@@ -57,7 +57,7 @@ protected:
 
 private:
     virtual bool supportsFocus() const;
-    virtual void setFocus(bool);
+    virtual void setFocus(bool) OVERRIDE;
     
     virtual bool isURLAttribute(const Attribute&) const OVERRIDE;
     virtual bool isFrameElementBase() const { return true; }
