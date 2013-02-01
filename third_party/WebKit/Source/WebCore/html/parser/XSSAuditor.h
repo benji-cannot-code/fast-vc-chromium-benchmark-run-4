405bc55291b8ba5ac61f098bd5a1d157d1604bd8
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-01 21:13:32 +0000

    Continue making XSSAuditor thread safe: Remove dependency on parser's Document URL https://bugs.webkit.org/show_bug.cgi?id=108655
    Reviewed by Adam Barth.
    
    No new tests because no new functionality.
    
    * html/parser/XSSAuditor.cpp:
    (WebCore::XSSAuditor::XSSAuditor):
    (WebCore::XSSAuditor::init):
    (WebCore::XSSAuditor::isLikelySafeResource):
    * html/parser/XSSAuditor.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141633 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
+++ third_party/WebKit/Source/WebCore/html/parser/XSSAuditor.h
@@ -86,6 +86,7 @@ private:
 
     // FIXME: Remove this dependency.
     HTMLDocumentParser* m_parser;
+    KURL m_documentURL;
     bool m_isEnabled;
     XSSProtectionDisposition m_xssProtection;
 
