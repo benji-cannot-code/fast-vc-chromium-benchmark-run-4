41e5f0fe73a26e96e2aeb69688804ba506d287e0
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-01 12:16:17 +0000

    Telemetry: add a metadata layer between page set and .wpr.
    The metadata file describes which pages in the page set are
    backed by which .wpr files. This allows us to update individual
    pages in the page set.
    
    BUG=155660
    NOTRY=true
    
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=180117
    
    Review URL: https://chromiumcodereview.appspot.com/11881051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180133 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page_unittest.py
+++ tools/telemetry/telemetry/page_unittest.py
@@ -9,6 +9,7 @@ from telemetry import page
 class TestPage(unittest.TestCase):
   def testGetUrlBaseDirAndFileForAbsolutePath(self):
     apage = page.Page('file:///somedir/otherdir/file.html',
+                      None, # In this test, we don't need a page set.
                       base_dir='basedir')
     dirname, filename = apage.url_base_dir_and_file
     self.assertEqual(dirname, 'basedir/somedir/otherdir')
@@ -16,6 +17,7 @@ class TestPage(unittest.TestCase):
 
   def testGetUrlBaseDirAndFileForRelativePath(self):
     apage = page.Page('file:///../../otherdir/file.html',
+                      None, # In this test, we don't need a page set.
                       base_dir='basedir')
     dirname, filename = apage.url_base_dir_and_file
     self.assertEqual(dirname, 'basedir/../../otherdir')
@@ -23,6 +25,7 @@ class TestPage(unittest.TestCase):
 
   def testGetUrlBaseDirAndFileForUrlBaseDir(self):
     apage = page.Page('file:///../../somedir/otherdir/file.html',
+                      None, # In this test, we don't need a page set.
                       base_dir='basedir')
     setattr(apage, 'url_base_dir', 'file:///../../somedir/')
     dirname, filename = apage.url_base_dir_and_file
@@ -30,9 +33,10 @@ class TestPage(unittest.TestCase):
     self.assertEqual(filename, 'otherdir/file.html')
 
   def testDisplayUrlForHttp(self):
-    self.assertEquals(page.Page('http://www.foo.com/').display_url,
+    self.assertEquals(page.Page('http://www.foo.com/', None).display_url,
                       'www.foo.com/')
 
   def testDisplayUrlForFile(self):
-    self.assertEquals(page.Page('file:///../../otherdir/file.html').display_url,
-                      'file.html')
+    self.assertEquals(
+        page.Page('file:///../../otherdir/file.html', None).display_url,
+        'file.html')
