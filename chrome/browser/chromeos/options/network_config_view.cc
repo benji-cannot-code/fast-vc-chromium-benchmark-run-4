dbb61323ef94d1095bbed6524ee55d90a65ad694
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-02 19:08:09 +0000

    cros: Fix crash in network connection dialog
    Connection error dialogs may open with no natural parent widget. Provide a
    fallback context on the active display.
    
    BUG=173462
    TEST=connect to password protected WiFi network, with bad password then with good password
    NOTRY=true
    (no try due to repeated win_rel flakes for this chromeos-only patch)
    
    
    Review URL: https://chromiumcodereview.appspot.com/12135002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/network_config_view.cc
+++ chrome/browser/chromeos/options/network_config_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <algorithm>
 
+#include "ash/shell.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/chromeos/login/base_login_display_host.h"
@@ -22,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "grit/locale_settings.h"
 #include "grit/theme_resources.h"
+#include "ui/aura/root_window.h"
 #include "ui/base/accessibility/accessible_view_state.h"
 #include "ui/base/l10n/l10n_util.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -32,6 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/layout/layout_constants.h"
 #include "ui/views/widget/widget.h"
+
+using views::Widget;
 
 namespace {
 
@@ -118,11 +122,7 @@ bool NetworkConfigView::Show(Network* network, gfx::NativeWindow parent) {
   if (GetActiveDialog() != NULL)
     return false;
   NetworkConfigView* view = new NetworkConfigView(network);
-  if (parent == NULL)
-    parent = GetDialogParent();
-  views::Widget* window = views::Widget::CreateWindowWithParent(view, parent);
-  window->SetAlwaysOnTop(true);
-  window->Show();
+  view->ShowDialog(parent);
   return true;
 }
 
@@ -132,11 +132,7 @@ bool NetworkConfigView::ShowForType(ConnectionType type,
   if (GetActiveDialog() != NULL)
     return false;
   NetworkConfigView* view = new NetworkConfigView(type);
-  if (parent == NULL)
-    parent = GetDialogParent();
-  views::Widget* window = views::Widget::CreateWindowWithParent(view, parent);
-  window->SetAlwaysOnTop(true);
-  window->Show();
+  view->ShowDialog(parent);
   return true;
 }
 
@@ -245,6 +241,18 @@ void NetworkConfigView::ViewHierarchyChanged(
   if (is_add && child == this) {
     AddChildView(child_config_view_);
   }
+}
+
+void NetworkConfigView::ShowDialog(gfx::NativeWindow parent) {
+  if (parent == NULL)
+    parent = GetDialogParent();
+  // Failed connections may result in a pop-up with no natural parent window,
+  // so provide a fallback context on the active display.
+  Widget* window = parent ?
+      Widget::CreateWindowWithParent(this, parent) :
+      Widget::CreateWindowWithContext(this, ash::Shell::GetActiveRootWindow());
+  window->SetAlwaysOnTop(true);
+  window->Show();
 }
 
 void NetworkConfigView::CreateAdvancedButton() {
