0c2d32ac330a9f51853f79c3569d59496efb6d09
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-02 00:59:15 +0000

    Warn for non-virtual protected destructors of RefCounted classes
    This would catch bugs like crbug.com/171760
    
    BUG=
    
    Review URL: https://codereview.chromium.org/12061005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180222 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/plugins/tests/base_refcounted.h
+++ tools/clang/plugins/tests/base_refcounted.h
@@ -64,7 +64,7 @@ class PublicRefCountedThreadSafeDtorInHeader
       PublicRefCountedThreadSafeDtorInHeader>;
 };
 
-// Safe; should not have errors.
+// Unsafe; should error.
 class ProtectedRefCountedDtorInHeader
     : public base::RefCounted<ProtectedRefCountedDtorInHeader> {
  public:
@@ -76,6 +76,20 @@ class ProtectedRefCountedDtorInHeader
  private:
   friend class base::RefCounted<ProtectedRefCountedDtorInHeader>;
 };
+
+// Safe; should not have errors
+class ProtectedRefCountedVirtualDtorInHeader
+    : public base::RefCounted<ProtectedRefCountedVirtualDtorInHeader> {
+ public:
+  ProtectedRefCountedVirtualDtorInHeader() {}
+
+ protected:
+  virtual ~ProtectedRefCountedVirtualDtorInHeader() {}
+
+ private:
+  friend class base::RefCounted<ProtectedRefCountedVirtualDtorInHeader>;
+};
+
 
 // Safe; should not have errors.
 class PrivateRefCountedDtorInHeader
@@ -91,16 +105,16 @@ class PrivateRefCountedDtorInHeader
 // Unsafe; A grandchild class ends up exposing their parent and grandparent's
 // destructors.
 class DerivedProtectedToPublicInHeader
-    : public ProtectedRefCountedDtorInHeader {
+    : public ProtectedRefCountedVirtualDtorInHeader {
  public:
   DerivedProtectedToPublicInHeader() {}
-  ~DerivedProtectedToPublicInHeader() {}
+  virtual ~DerivedProtectedToPublicInHeader() {}
 };
 
 // Unsafe; A grandchild ends up implicitly exposing their parent and
 // grantparent's destructors.
 class ImplicitDerivedProtectedToPublicInHeader
-    : public ProtectedRefCountedDtorInHeader {
+    : public ProtectedRefCountedVirtualDtorInHeader {
  public:
   ImplicitDerivedProtectedToPublicInHeader() {}
 };
