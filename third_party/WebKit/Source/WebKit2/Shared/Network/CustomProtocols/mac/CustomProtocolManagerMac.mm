c2007a993b6654fb5bb3c51e39b43156f35693c1
benjamin@webkit.org <benjamin@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-02 00:33:02 +0000

    Build fix for CustomProtocolManagerMac after r141658.
    Patch by Benjamin Poulain <bpoulain@apple.com> on 2013-02-01
    
    * Shared/Network/CustomProtocols/mac/CustomProtocolManagerMac.mm:
    (WebKit::CustomProtocolManager::supplementName): I accidentally typed "const" twice twice.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141663 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/Network/CustomProtocols/mac/CustomProtocolManagerMac.mm
+++ third_party/WebKit/Source/WebKit2/Shared/Network/CustomProtocols/mac/CustomProtocolManagerMac.mm
@@ -110,7 +110,7 @@ static uint64_t generateCustomProtocolID()
 
 namespace WebKit {
 
-const const char* CustomProtocolManager::supplementName()
+const char* CustomProtocolManager::supplementName()
 {
     return "CustomProtocolManager";
 }
