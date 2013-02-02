1fa8a06d5cab9003d2b24e1b9e6ed4bab20c16a5
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-02 19:04:52 +0000

    Use the correct coordinate to layout bubble when the anchor widget is not visible
    BUG=173520
    TEST=manual (see bug)
    
    Review URL: https://codereview.chromium.org/12183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180266 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/tray_background_view.cc
+++ ash/system/tray/tray_background_view.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/launcher/background_animator.h"
 #include "ash/root_window_controller.h"
+#include "ash/screen_ash.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
 #include "ash/system/status_area_widget.h"
@@ -351,12 +352,16 @@ gfx::Rect TrayBackgroundView::GetBubbleAnchorRect(
 
   // TODO(jennyz): May need to add left/right alignment in the following code.
   if (rect.IsEmpty()) {
-    rect = Shell::GetScreen()->GetPrimaryDisplay().bounds();
+    aura::RootWindow* target_root = anchor_widget ?
+        anchor_widget->GetNativeView()->GetRootWindow() :
+        Shell::GetPrimaryRootWindow();
+    rect = target_root->bounds();
     rect = gfx::Rect(
         base::i18n::IsRTL() ? kPaddingFromRightEdgeOfScreenBottomAlignment :
         rect.width() - kPaddingFromRightEdgeOfScreenBottomAlignment,
         rect.height() - kPaddingFromBottomOfScreenBottomAlignment,
         0, 0);
+    rect = ScreenAsh::ConvertRectToScreen(target_root, rect);
   }
   return rect;
 }
