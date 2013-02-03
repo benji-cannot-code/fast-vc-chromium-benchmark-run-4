9ec1565835ee42a95ec201d44db689658d6682ba
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-03 07:10:45 +0000

    linux: Fix missing include when breakpad is disabled.
    chrome/common/chrome_switches.h was only getting included in
    hrome/browser/chrome_browser_main_linux.cc when
    USE_LINUX_BREAKPAD was defined, but r179938 added a
    reference to a constant from it regardless of
    USE_LINUX_BREAKPAD.
    
    BUG=171742
    
    
    Review URL: https://chromiumcodereview.appspot.com/12197002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180296 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_linux.cc
+++ chrome/browser/chrome_browser_main_linux.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop_proxy.h"
 #include "chrome/browser/system_monitor/media_transfer_protocol_device_observer_linux.h"
+#include "chrome/common/chrome_switches.h"
 #include "device/media_transfer_protocol/media_transfer_protocol_manager.h"
 
 #if !defined(OS_CHROMEOS)
@@ -21,14 +22,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/linux_util.h"
 #include "chrome/app/breakpad_linux.h"
 #include "chrome/browser/prefs/pref_service.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/env_vars.h"
 #include "chrome/common/pref_names.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/chromeos/settings/cros_settings_names.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_version_info.h"
 #endif
 
