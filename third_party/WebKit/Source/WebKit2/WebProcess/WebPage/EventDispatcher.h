13fde4bd2ba43e6ab459a4c5aca024cc4c9614a7
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-04 23:07:46 +0000

    Update message generation to use the new queue client semantics https://bugs.webkit.org/show_bug.cgi?id=108865
    Reviewed by Andreas Kling.
    
    * Scripts/webkit2/messages.py:
    (connection_work_queue_message_statement):
    (async_message_statement):
    (generate_message_handler):
    * Scripts/webkit2/messages_unittest.py:
    * Shared/mac/SecItemShim.cpp:
    (WebKit::SecItemShim::didReceiveMessageOnConnectionWorkQueue):
    * Shared/mac/SecItemShim.h:
    (SecItemShim):
    * UIProcess/Storage/StorageManager.cpp:
    (WebKit::StorageManager::didReceiveMessageOnConnectionWorkQueue):
    * UIProcess/Storage/StorageManager.h:
    (StorageManager):
    * UIProcess/WebProcessProxy.cpp:
    (WebKit::WebProcessProxy::didReceiveMessageOnConnectionWorkQueue):
    * UIProcess/WebProcessProxy.h:
    (WebProcessProxy):
    * UIProcess/mac/SecItemShimProxy.cpp:
    (WebKit::SecItemShimProxy::didReceiveMessageOnConnectionWorkQueue):
    * UIProcess/mac/SecItemShimProxy.h:
    (SecItemShimProxy):
    * WebKit2.xcodeproj/project.pbxproj:
    * WebProcess/WebPage/EventDispatcher.cpp:
    (WebKit::EventDispatcher::didReceiveMessageOnConnectionWorkQueue):
    * WebProcess/WebPage/EventDispatcher.h:
    (EventDispatcher):
    * WebProcess/WebProcess.cpp:
    (WebKit::WebProcess::didReceiveMessageOnConnectionWorkQueue):
    * WebProcess/WebProcess.h:
    (WebProcess):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141820 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/EventDispatcher.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/EventDispatcher.h
@@ -65,7 +65,7 @@ private:
     virtual void didCloseOnConnectionWorkQueue(CoreIPC::Connection*) OVERRIDE;
 
     // Implemented in generated EventDispatcherMessageReceiver.cpp
-    void didReceiveEventDispatcherMessageOnConnectionWorkQueue(CoreIPC::Connection*, CoreIPC::MessageDecoder&, bool& didHandleMessage);
+    void didReceiveEventDispatcherMessageOnConnectionWorkQueue(CoreIPC::Connection*, OwnPtr<CoreIPC::MessageDecoder>&);
 
     // Message handlers
     void wheelEvent(CoreIPC::Connection*, uint64_t pageID, const WebWheelEvent&, bool canGoBack, bool canGoForward);
