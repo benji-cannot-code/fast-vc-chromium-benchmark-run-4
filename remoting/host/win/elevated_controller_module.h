190ed290c82d76be0270b8df496809e006efcb6b
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 19:07:30 +0000

    Relanding r180433, now without build/some.gyp:
    Merged all Chromoting Host code into remoting_core.dll (Windows).
    
    Consolidated all installable Chromoting Host code into remoting_core.dll and converted all executables into thin wrappers around entry points exposed by remoting_core.dll. This reduces size of the installer by approximately 600KB.
    
    This CL also ensures that proper manifests will be embedded into the binaries regardless whether msbuild or ninja is used.
    
    TBR=sergeyu@chromium.org
    
    BUG=170200, 173052
    
    Review URL: https://codereview.chromium.org/12189013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180468 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ remoting/host/win/elevated_controller_module.h
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef REMOTING_HOST_WIN_ELEVATED_CONTROLLER_MODULE_H_
+#define REMOTING_HOST_WIN_ELEVATED_CONTROLLER_MODULE_H_
+
+#include "remoting/host/host_export.h"
+
+namespace remoting {
+
+// The elevated controller's entry point exported from remoting_core.dll.
+HOST_EXPORT int ElevatedControllerMain();
+
+} // namespace remoting
+
+#endif  // REMOTING_HOST_WIN_ELEVATED_CONTROLLER_MODULE_H_
---
+++ remoting/host/win/host_process_entry_point.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <windows.h>
+
+#include "remoting/host/remoting_me2me_host.h"
+
+// The entry point of the host process binary. In order to be really small
+// the app doesn't link against the CRT.
+void HostProcessEntryPoint() {
+  int exit_code = remoting::HostProcessMain(0, NULL);
+  ExitProcess(exit_code);
+}
