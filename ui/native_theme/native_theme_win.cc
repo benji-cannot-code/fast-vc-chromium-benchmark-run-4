b569f9ef3a1e4f147328aebd695daedc16c19028
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 17:03:13 +0000

    Fix painting of scaled themes in Windows in high-DPI mode.
    BUG=149881
    
    
    Review URL: https://chromiumcodereview.appspot.com/12114049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180431 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/native_theme/native_theme_win.cc
+++ ui/native_theme/native_theme_win.cc
@@ -24,9 +24,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkColorPriv.h"
 #include "third_party/skia/include/core/SkShader.h"
+#include "ui/base/win/dpi.h"
 #include "ui/gfx/color_utils.h"
 #include "ui/gfx/gdi_util.h"
 #include "ui/gfx/rect.h"
+#include "ui/gfx/rect_conversions.h"
 #include "ui/native_theme/common_theme.h"
 
 // This was removed from Winvers.h but is still used.
@@ -1072,8 +1074,7 @@ HRESULT NativeThemeWin::PaintScrollbarArrow(
           break;
       }
     }
-
-    return draw_theme_(handle, hdc, SBP_ARROWBTN, state_id, &rect_win, NULL);
+    return PaintScaledTheme(handle, hdc, SBP_ARROWBTN, state_id, rect);
   }
 
   int classic_state = DFCS_SCROLLDOWN;
@@ -1162,7 +1163,7 @@ HRESULT NativeThemeWin::PaintScrollbarThumb(
   }
 
   if (handle && draw_theme_)
-    return draw_theme_(handle, hdc, part_id, state_id, &rect_win, NULL);
+    return PaintScaledTheme(handle, hdc, part_id, state_id, rect);
 
   // Draw it manually.
   if ((part_id == SBP_THUMBBTNHORZ) || (part_id == SBP_THUMBBTNVERT))
@@ -1590,6 +1591,35 @@ HRESULT NativeThemeWin::PaintTextField(HDC hdc,
   }
   DeleteObject(bg_brush);
   return hr;
+}
+
+HRESULT NativeThemeWin::PaintScaledTheme(HANDLE theme,
+                                         HDC hdc,
+                                         int part_id,
+                                         int state_id,
+                                         const gfx::Rect& rect) const {
+  // Correct the scaling and positioning of sub-components such as scrollbar
+  // arrows and thumb grippers in the event that the world transform applies
+  // scaling (e.g. in high-DPI mode).
+  XFORM save_transform;
+  if (GetWorldTransform(hdc, &save_transform)) {
+    float scale = save_transform.eM11;
+    if (scale != 1 && save_transform.eM12 == 0) {
+      ModifyWorldTransform(hdc, NULL, MWT_IDENTITY);
+      gfx::Rect scaled_rect = gfx::ToEnclosedRect(
+          gfx::ScaleRect(rect, scale));
+      RECT bounds = gfx::Rect(scaled_rect.x() + save_transform.eDx,
+                              scaled_rect.y() + save_transform.eDy,
+                              scaled_rect.width(),
+                              scaled_rect.height()).ToRECT();
+      HRESULT result = draw_theme_(theme, hdc, part_id, state_id, &bounds,
+                                   NULL);
+      SetWorldTransform(hdc, &save_transform);
+      return result;
+    }
+  }
+  RECT bounds = rect.ToRECT();
+  return draw_theme_(theme, hdc, part_id, state_id, &bounds, NULL);
 }
 
 // static
