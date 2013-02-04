9e60e99aa196d867f5c6ece29583a5aaa833e86a
alexeypa@google.com <alexeypa@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 17:18:03 +0000

    Revert 180433 because build\some.gyp was added to the CL by mistake. > Merged all Chromoting Host code into remoting_core.dll (Windows). > > Consolidated all installable Chromoting Host core into remoting_core.dll and converted all executables into thin wrappers around entry points exposed by remoting_core.dll. This reduces size of the installer by approximately 600KB. > > This is the 3rd attempt to land this CL. The previous attempt was reverted at r179373 because ninja sometimes failed to create a manifest for targets that use manifestXXX linker flags. This CL fixes that by manually embedding the manifests into binaries. > > BUG=170200,173052 > > Review URL: https://codereview.chromium.org/12092117
    TBR=alexeypa@chromium.org
    Review URL: https://codereview.chromium.org/12179013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180440 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/win/elevated_controller.cc
+++ remoting/host/win/elevated_controller.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "remoting/host/branding.h"
 #include "remoting/host/usage_stats_consent.h"
 #include "remoting/host/verify_config_window_win.h"
-#include "remoting/host/win/core_resource.h"
+#include "remoting/host/win/elevated_controller_resource.h"
 #include "remoting/host/win/security_descriptor.h"
 
 namespace remoting {
