d021d94be91afefaaff97c58d703c09a1bc9442d
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 19:41:36 +0000

    Add POST_WITHOUT_BODY to the UrlFetcher's method.
    This CL adds a new method enum POST_WITHOUT_BODY. In most cases, POST request has non-empty body and non-content-type header. The current implementation has some (D)CHECKs for it.
    However, there are some cases that we'd like to send POST request without body (or, we may say it is empty body), content-type and content length headers. The new method POST_WITHOUT_BODY is for supporting such cases.
    
    BUG=148628
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/12096093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180478 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_fetcher_core.cc
+++ net/url_request/url_fetcher_core.cc
@@ -756,6 +756,10 @@ void URLFetcherCore::StartURLRequest() {
           &URLFetcherCore::InformDelegateUploadProgress);
       break;
 
+    case URLFetcher::POST_WITHOUT_BODY:
+      request_->set_method("POST");
+      break;
+
     case URLFetcher::HEAD:
       request_->set_method("HEAD");
       break;
