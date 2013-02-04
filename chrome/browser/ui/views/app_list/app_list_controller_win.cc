19917e18fd2ddcb737a421c87aaaac5cc74547e0
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 00:48:01 +0000

    [win] Delay making app launcher always on top.
    This seems to help in some cases to prevent it appearing behind the taskbar.
    
    BUG=170543
    
    
    Review URL: https://chromiumcodereview.appspot.com/11871009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180327 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/app_list/app_list_controller_win.cc
+++ chrome/browser/ui/views/app_list/app_list_controller_win.cc
@@ -324,7 +324,6 @@ void AppListController::CreateAppList() {
   ::SetWindowText(hwnd, app_name.c_str());
   string16 icon_path = GetAppListIconPath();
   ui::win::SetAppIconForWindow(icon_path, hwnd);
-  current_view_->GetWidget()->SetAlwaysOnTop(true);
 #endif
 }
 
@@ -341,6 +340,7 @@ void AppListController::ShowAppList() {
   UpdateArrowPositionAndAnchorPoint(cursor);
   current_view_->Show();
   current_view_->GetWidget()->Activate();
+  current_view_->GetWidget()->SetAlwaysOnTop(true);
 #endif
 }
 
