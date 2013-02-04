ded6fc44eebecf4cf6ee38facea37cc93648546d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-04 14:43:25 +0000

    Web Inspector: Allow user to change dock side by dragging toolbar https://bugs.webkit.org/show_bug.cgi?id=108073
    Dragging toolbar to the right/bottom will change the dock side accordingly
    instead of changing the inspector window height (if dock to right is available).
    
    Patch by Dmitry Gozman <dgozman@chromium.org> on 2013-02-04
    Reviewed by Pavel Feldman.
    
    No new tests, because of pure inspector UI change.
    
    * inspector/front-end/DockController.js:
    (WebInspector.DockController.prototype.dockSide):
    * inspector/front-end/Toolbar.js:
    (WebInspector.Toolbar):
    (WebInspector.Toolbar.prototype._isDockedToBottom):
    (WebInspector.Toolbar.prototype._isUndocked):
    (WebInspector.Toolbar.prototype._toolbarDragStart):
    (WebInspector.Toolbar.prototype._toolbarDragEnd):
    (WebInspector.Toolbar.prototype._toolbarDrag):
    (WebInspector.Toolbar.prototype._toolbarDragMoveWindow):
    (WebInspector.Toolbar.prototype._toolbarDragChangeDocking):
    (WebInspector.Toolbar.prototype._toolbarDragChangeHeight):
    * inspector/front-end/UIUtils.js:
    (WebInspector._elementDragStart):
    (WebInspector._elementDragMove):
    (WebInspector._cancelDragEvents):
    (WebInspector._elementDragEnd):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DockController.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DockController.js
@@ -61,6 +61,14 @@ WebInspector.DockController.prototype = {
     },
 
     /**
+     * @return {string}
+     */
+    dockSide: function()
+    {
+        return this._dockSide;
+    },
+
+    /**
      * @param {string} dockSide
      */
     setDockSide: function(dockSide)
@@ -77,14 +85,6 @@ WebInspector.DockController.prototype = {
         else
             WebInspector.userMetrics.WindowUndocked.record();
         this._updateUI();
-    },
-
-    /**
-     * @return {boolean}
-     */
-    isDockedToBottom: function()
-    {
-        return this._dockSide == WebInspector.DockController.State.DockedToBottom;
     },
 
     /**
