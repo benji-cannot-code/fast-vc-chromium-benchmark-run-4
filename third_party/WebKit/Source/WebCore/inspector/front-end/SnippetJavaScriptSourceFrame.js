6203c6f4912eba5c244a7e455ed84d900dd29398
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-04 09:01:20 +0000

    Web Inspector: source location in statusbar has few bugs in it https://bugs.webkit.org/show_bug.cgi?id=108748
    Patch by Andrey Lushnikov <lushnikov@chromium.org> on 2013-02-04
    Reviewed by Vsevolod Vlasov.
    
    Call super method statusBarItems in SnippetJavaScriptSourceFrame class
    and merge its results. Fix css styles for source location in statusbar
    and rename its css class into a less confusing name.
    
    No new test: no change in behaviour.
    
    * inspector/front-end/SnippetJavaScriptSourceFrame.js:
    (WebInspector.SnippetJavaScriptSourceFrame.prototype.statusBarItems):
    * inspector/front-end/SourceFrame.js:
    (WebInspector.SourceFrame):
    * inspector/front-end/inspector.css:
    (.source-frame-cursor-position):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141747 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SnippetJavaScriptSourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SnippetJavaScriptSourceFrame.js
@@ -50,7 +50,7 @@ WebInspector.SnippetJavaScriptSourceFrame.prototype = {
      */
     statusBarItems: function()
     {
-        return [this._runButton.element];
+        return [this._runButton.element].concat(WebInspector.JavaScriptSourceFrame.prototype.statusBarItems.call(this));
     },
 
     _runButtonClicked: function()
