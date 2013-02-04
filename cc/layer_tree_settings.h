d682db8044a9ae23b071b9a845d1a6508fece4fe
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 13:33:03 +0000

    cc: Add flag to use raster cheapness estimator
    Add a command line flag to turn on the raster job cheapness estimator.
    It will be used to schedule raster jobs according to their estimated
    processing cost.
    
    BUG=173426
    TBR=piman
    
    Review URL: https://chromiumcodereview.appspot.com/12185015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180399 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_settings.h
+++ cc/layer_tree_settings.h
@@ -34,6 +34,7 @@ class CC_EXPORT LayerTreeSettings {
   bool useLinearFadeScrollbarAnimator;
   bool calculateTopControlsPosition;
   bool recordRenderingStats;
+  bool useCheapnessEstimator;
   float minimumContentsScale;
   float lowResContentsScaleFactor;
   float topControlsHeight;
