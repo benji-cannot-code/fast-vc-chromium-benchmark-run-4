0d50de48031f16ed93d69efba92a520afa74dde1
mgist@chromium.org <mgist@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 20:12:55 +0000

    [Sync] Add timestamp to TrafficRecorder records (reland)
    Relanding after revert. Original codereview at
    https://chromiumcodereview.appspot.com/12088080.
    
    Fixed issue requiring GG_INT64_C wrapper for large ints
    in unit test cases.
    
    BUG=161922
    
    
    Review URL: https://chromiumcodereview.appspot.com/12087139
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180489 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/engine/traffic_recorder.cc
+++ sync/engine/traffic_recorder.cc
@@ -12,15 +12,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sync/protocol/proto_value_conversions.h"
 #include "sync/protocol/sync.pb.h"
 #include "sync/sessions/sync_session.h"
+#include "sync/util/time.h"
 
 namespace syncer {
 
+// Return current time.
+base::Time TrafficRecorder::GetTime() {
+  return base::Time::Now();
+}
+
 TrafficRecorder::TrafficRecord::TrafficRecord(const std::string& message,
                                               TrafficMessageType message_type,
-                                              bool truncated) :
+                                              bool truncated,
+                                              base::Time time) :
     message(message),
     message_type(message_type),
-    truncated(truncated) {
+    truncated(truncated),
+    timestamp(time) {
 }
 
 TrafficRecorder::TrafficRecord::TrafficRecord()
@@ -77,6 +85,8 @@ DictionaryValue* TrafficRecorder::TrafficRecord::ToValue() const {
     NOTREACHED();
   }
 
+  value->SetString("timestamp", GetTimeDebugString(timestamp));
+
   return value.release();
 }
 
@@ -116,7 +126,7 @@ void TrafficRecorder::StoreProtoInQueue(
     msg.SerializeToString(&message);
   }
 
-  TrafficRecord record(message, type, truncated);
+  TrafficRecord record(message, type, truncated, GetTime());
   AddTrafficToQueue(&record);
 }
 
--- sync/engine/traffic_recorder.h
+++ sync/engine/traffic_recorder.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/gtest_prod_util.h"
+#include "base/time.h"
 #include "base/values.h"
 #include "sync/base/sync_export.h"
 #include "sync/protocol/sync.pb.h"
@@ -41,14 +42,18 @@ class SYNC_EXPORT_PRIVATE TrafficRecorder {
 
     TrafficRecord(const std::string& message,
                   TrafficMessageType message_type,
-                  bool truncated);
+                  bool truncated,
+                  base::Time time);
     TrafficRecord();
     ~TrafficRecord();
     DictionaryValue* ToValue() const;
+
+    // Time of record creation.
+    base::Time timestamp;
   };
 
   TrafficRecorder(unsigned int max_messages, unsigned int max_message_size);
-  ~TrafficRecorder();
+  virtual ~TrafficRecorder();
 
   void RecordClientToServerMessage(const sync_pb::ClientToServerMessage& msg);
   void RecordClientToServerResponse(
@@ -63,6 +68,9 @@ class SYNC_EXPORT_PRIVATE TrafficRecorder {
   void AddTrafficToQueue(TrafficRecord* record);
   void StoreProtoInQueue(const ::google::protobuf::MessageLite& msg,
                          TrafficMessageType type);
+
+  // Method to get record creation time.
+  virtual base::Time GetTime();
 
   // Maximum number of messages stored in the queue.
   unsigned int max_messages_;
