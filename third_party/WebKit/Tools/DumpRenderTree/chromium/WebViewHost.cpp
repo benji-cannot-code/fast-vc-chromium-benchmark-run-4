400e0dc0fb758a62249e5f94d93691a44c6fc9b4
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-04 18:59:01 +0000

    [chromium] remove WebTestPlugin from the public TestRunner API https://bugs.webkit.org/show_bug.cgi?id=108467
    Reviewed by Adam Barth.
    
    * DumpRenderTree/DumpRenderTree.gypi:
    * DumpRenderTree/chromium/TestRunner/public/WebTestPlugin.h: Removed.
    * DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h:
    (WebKit):
    (WebTestProxyBase):
    (WebTestRunner::WebTestProxy::createPlugin):
    * DumpRenderTree/chromium/TestRunner/src/TestPlugin.cpp: Renamed from Tools/DumpRenderTree/chromium/TestRunner/src/WebTestPlugin.cpp.
    (WebTestRunner::TestPlugin::TestPlugin):
    (WebTestRunner):
    (WebTestRunner::TestPlugin::~TestPlugin):
    (WebTestRunner::TestPlugin::initialize):
    (WebTestRunner::TestPlugin::destroy):
    (WebTestRunner::TestPlugin::updateGeometry):
    (WebTestRunner::TestPlugin::parsePrimitive):
    (WebTestRunner::TestPlugin::parseColor):
    (WebTestRunner::TestPlugin::parseOpacity):
    (WebTestRunner::TestPlugin::parseBoolean):
    (WebTestRunner::TestPlugin::initScene):
    (WebTestRunner::TestPlugin::drawScene):
    (WebTestRunner::TestPlugin::destroyScene):
    (WebTestRunner::TestPlugin::initProgram):
    (WebTestRunner::TestPlugin::initPrimitive):
    (WebTestRunner::TestPlugin::drawPrimitive):
    (WebTestRunner::TestPlugin::loadShader):
    (WebTestRunner::TestPlugin::loadProgram):
    (WebTestRunner::TestPlugin::handleInputEvent):
    (WebTestRunner::TestPlugin::handleDragStatusUpdate):
    (WebTestRunner::TestPlugin::create):
    (WebTestRunner::TestPlugin::mimeType):
    * DumpRenderTree/chromium/TestRunner/src/TestPlugin.h: Added.
    (WebTestRunner):
    (TestPlugin):
    (WebTestRunner::TestPlugin::scriptableObject):
    (WebTestRunner::TestPlugin::canProcessDrag):
    (WebTestRunner::TestPlugin::paint):
    (WebTestRunner::TestPlugin::updateFocus):
    (WebTestRunner::TestPlugin::updateVisibility):
    (WebTestRunner::TestPlugin::acceptsInputEvents):
    (WebTestRunner::TestPlugin::didReceiveResponse):
    (WebTestRunner::TestPlugin::didReceiveData):
    (WebTestRunner::TestPlugin::didFinishLoading):
    (WebTestRunner::TestPlugin::didFailLoading):
    (WebTestRunner::TestPlugin::didFinishLoadingFrameRequest):
    (WebTestRunner::TestPlugin::didFailLoadingFrameRequest):
    (WebTestRunner::TestPlugin::isPlaceholder):
    (WebTestRunner::TestPlugin::prepareTexture):
    (WebTestRunner::TestPlugin::context):
    (Scene):
    (WebTestRunner::TestPlugin::Scene::Scene):
    * DumpRenderTree/chromium/TestRunner/src/WebTestProxy.cpp:
    (WebTestRunner::WebTestProxyBase::createPlugin):
    (WebTestRunner):
    * DumpRenderTree/chromium/WebViewHost.cpp:
    (WebViewHost::createPlugin):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141785 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/WebViewHost.cpp
@@ -60,7 +60,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebScreenInfo.h"
 #include "WebSerializedScriptValue.h"
 #include "WebStorageNamespace.h"
-#include "WebTestPlugin.h"
 #include "WebUserMediaClientMock.h"
 #include "WebView.h"
 #include "WebWindowFeatures.h"
@@ -537,9 +536,6 @@ void WebViewHost::exitFullScreen()
 
 WebPlugin* WebViewHost::createPlugin(WebFrame* frame, const WebPluginParams& params)
 {
-    if (params.mimeType == WebTestPlugin::mimeType())
-        return WebTestPlugin::create(frame, params, this);
-
     return webkit_support::CreateWebPlugin(frame, params);
 }
 
