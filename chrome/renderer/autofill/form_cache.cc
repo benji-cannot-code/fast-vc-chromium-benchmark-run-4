e73e1dbb373f259ec9b52eeb3e623f7492f93b96
ramankk@chromium.org <ramankk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 05:37:02 +0000

    Renderer side changes to save the user supplied data and use it to autofill in multiple page in the flow. Primarily being used by Autocheckout. This is achived by * Saving the context of all form elements in the top most frame seen * Using filled form data from browser to fill form elements earlier seen
    BUG=172632
    
    
    Review URL: https://chromiumcodereview.appspot.com/12087122
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/autofill/form_cache.cc
+++ chrome/renderer/autofill/form_cache.cc
@@ -78,6 +78,13 @@ FormCache::~FormCache() {
 
 void FormCache::ExtractForms(const WebFrame& frame,
                              std::vector<FormData>* forms) {
+  ExtractFormsAndFormElements(frame, forms, NULL);
+}
+
+void FormCache::ExtractFormsAndFormElements(
+    const WebFrame& frame,
+    std::vector<FormData>* forms,
+    std::vector<WebFormElement>* web_form_elements) {
   // Reset the cache for this frame.
   ResetFrame(frame);
 
@@ -123,8 +130,11 @@ void FormCache::ExtractForms(const WebFrame& frame,
       continue;
 
     FormData form;
+    ExtractMask extract_mask =
+      static_cast<ExtractMask>(EXTRACT_VALUE | EXTRACT_OPTIONS);
+
     if (!WebFormElementToFormData(form_element, WebFormControlElement(),
-                                  REQUIRE_NONE, EXTRACT_VALUE, &form, NULL)) {
+                                  REQUIRE_NONE, extract_mask, &form, NULL)) {
       continue;
     }
 
@@ -132,8 +142,11 @@ void FormCache::ExtractForms(const WebFrame& frame,
     if (num_fields_seen > kMaxParseableFields)
       break;
 
-    if (form.fields.size() >= kRequiredAutofillFields)
+    if (form.fields.size() >= kRequiredAutofillFields) {
       forms->push_back(form);
+      if (web_form_elements)
+        web_form_elements->push_back(form_element);
+    }
   }
 }
 
