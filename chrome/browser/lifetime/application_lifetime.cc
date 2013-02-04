1a75a5a418757aa6df2e83d45b016b91872405af
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-04 17:13:49 +0000

    Add BrowserIterator::Reset() and use it to replace BrowserList::const_iterator in browser::CloseAllBrowsers().
    BUG=129187
    
    
    Review URL: https://chromiumcodereview.appspot.com/12134002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180436 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/lifetime/application_lifetime.cc
+++ chrome/browser/lifetime/application_lifetime.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
 #include "build/build_config.h"
 #include "chrome/browser/browser_process.h"
@@ -130,12 +131,13 @@ void CloseAllBrowsers() {
   chromeos::BootTimesLoader::Get()->AddLogoutTimeMarker(
       "StartedClosingWindows", false);
 #endif
-  for (BrowserList::const_iterator i = BrowserList::begin();
-       i != BrowserList::end();) {
-    Browser* browser = *i;
+  for (scoped_ptr<chrome::BrowserIterator> it_ptr(
+           new chrome::BrowserIterator());
+       !it_ptr->done();) {
+    Browser* browser = **it_ptr;
     browser->window()->Close();
     if (!session_ending) {
-      ++i;
+      it_ptr->Next();
     } else {
       // This path is hit during logoff/power-down. In this case we won't get
       // a final message and so we force the browser to be deleted.
@@ -147,8 +149,8 @@ void CloseAllBrowsers() {
       while (browser->tab_strip_model()->count())
         delete browser->tab_strip_model()->GetWebContentsAt(0);
       browser->window()->DestroyBrowser();
-      i = BrowserList::begin();
-      if (i != BrowserList::end() && browser == *i) {
+      it_ptr.reset(new chrome::BrowserIterator());
+      if (!it_ptr->done() && browser == **it_ptr) {
         // Destroying the browser should have removed it from the browser list.
         // We should never get here.
         NOTREACHED();
