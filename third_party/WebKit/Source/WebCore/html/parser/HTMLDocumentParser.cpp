d89ae0f7d802f201f8329eaa089bb1b4acd12573
tonyg@chromium.org <tonyg@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-05 17:55:51 +0000

    Continue making XSSAuditor thread safe: Remove dependency on parser's sourceForToken and TextResourceDecoder https://bugs.webkit.org/show_bug.cgi?id=108698
    Reviewed by Adam Barth.
    
    We'd like to be able to call filterToken() from the BackgroundHTMLParser where there is no HTMLDocumentParser. So we are removing the dependencies of
    filterToken() on the HTMLDocumentParser. This patch brings us one step closer to removing the m_parser member from XSSAuditor by passing in the
    TextResourceDecoder and HTMLSourceTracker to filterToken. To keep the number of parameters from blowing up, this introduces a FilterTokenRequest struct
    to hold its arguments. We expect to add one more member to this struct.
    
    No new tests because no new functionality.
    
    * html/parser/HTMLDocumentParser.cpp:
    (WebCore::HTMLDocumentParser::pumpTokenizer):
    * html/parser/HTMLDocumentParser.h:
    * html/parser/XSSAuditor.cpp:
    (WebCore::XSSAuditor::filterToken):
    (WebCore::XSSAuditor::filterStartToken):
    (WebCore::XSSAuditor::filterCharacterToken):
    (WebCore::XSSAuditor::filterScriptToken):
    (WebCore::XSSAuditor::filterObjectToken):
    (WebCore::XSSAuditor::filterParamToken):
    (WebCore::XSSAuditor::filterEmbedToken):
    (WebCore::XSSAuditor::filterAppletToken):
    (WebCore::XSSAuditor::filterIframeToken):
    (WebCore::XSSAuditor::filterMetaToken):
    (WebCore::XSSAuditor::filterBaseToken):
    (WebCore::XSSAuditor::filterFormToken):
    (WebCore::XSSAuditor::eraseDangerousAttributesIfInjected):
    (WebCore::XSSAuditor::eraseAttributeIfInjected):
    (WebCore::XSSAuditor::decodedSnippetForName):
    (WebCore::XSSAuditor::decodedSnippetForAttribute):
    (WebCore::XSSAuditor::decodedSnippetForJavaScript):
    * html/parser/XSSAuditor.h:
    (WebCore):
    (WebCore::FilterTokenRequest::FilterTokenRequest):
    (FilterTokenRequest):
    (XSSAuditor):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/parser/HTMLDocumentParser.cpp
+++ third_party/WebKit/Source/WebCore/html/parser/HTMLDocumentParser.cpp
@@ -379,7 +379,7 @@ void HTMLDocumentParser::pumpTokenizer(SynchronousMode mode)
 
             // We do not XSS filter innerHTML, which means we (intentionally) fail
             // http/tests/security/xssAuditor/dom-write-innerHTML.html
-            OwnPtr<DidBlockScriptRequest> request = m_xssAuditor.filterToken(token());
+            OwnPtr<DidBlockScriptRequest> request = m_xssAuditor.filterToken(FilterTokenRequest(token(), m_sourceTracker, document()->decoder()));
             if (request)
                 m_xssAuditorDelegate.didBlockScript(request.release());
         }
@@ -664,11 +664,6 @@ bool HTMLDocumentParser::isExecutingScript() const
     if (!m_scriptRunner)
         return false;
     return m_scriptRunner->isExecutingScript();
-}
-
-String HTMLDocumentParser::sourceForToken(const HTMLToken& token)
-{
-    return m_sourceTracker.sourceForToken(token);
 }
 
 OrdinalNumber HTMLDocumentParser::lineNumber() const
