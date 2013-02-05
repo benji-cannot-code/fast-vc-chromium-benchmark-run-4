b63ae2768493ee6c962b79058042194e8c0ed54e
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 07:31:20 +0000

    [cros] Allow file select dialogs from extension popups
    BUG=169148
    
    
    Review URL: https://chromiumcodereview.appspot.com/11961005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/shell_window_registry.cc
+++ chrome/browser/extensions/shell_window_registry.cc
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/shell_window_registry.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/extensions/native_app_window.h"
 #include "chrome/browser/ui/extensions/shell_window.h"
 #include "chrome/common/extensions/extension.h"
@@ -145,6 +147,24 @@ bool ShellWindowRegistry::HadDevToolsAttached(
     content::RenderViewHost* render_view_host) const {
   std::string key = GetWindowKeyForRenderViewHost(this, render_view_host);
   return key.empty() ? false : inspected_windows_.count(key) != 0;
+}
+
+// static
+ShellWindow* ShellWindowRegistry::GetShellWindowForNativeWindowAnyProfile(
+    gfx::NativeWindow window) {
+  std::vector<Profile*> profiles =
+      g_browser_process->profile_manager()->GetLoadedProfiles();
+  for (std::vector<Profile*>::const_iterator i(profiles.begin());
+       i < profiles.end(); ++i) {
+    extensions::ShellWindowRegistry* registry =
+        extensions::ShellWindowRegistry::Get(*i);
+    DCHECK(registry);
+    ShellWindow* shell_window = registry->GetShellWindowForNativeWindow(window);
+    if (shell_window)
+      return shell_window;
+  }
+
+  return NULL;
 }
 
 void ShellWindowRegistry::Observe(int type,
