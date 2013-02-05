da4bb9e876ad1fdc2aebd7f98932023b8e7501ce
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-05 00:46:59 +0000

    [skia] Remove use of SkSingleInputImageFilter. https://bugs.webkit.org/show_bug.cgi?id=108867
    Reviewed by James Robinson.
    
    This class is but a hollow shell of its former self, and has
    been removed in Skia.
    
    Covered by existing tests in css3/filters.
    
    * platform/graphics/filters/skia/DropShadowImageFilter.cpp:
    (WebCore::DropShadowImageFilter::DropShadowImageFilter):
    (WebCore::DropShadowImageFilter::onFilterImage):
    * platform/graphics/filters/skia/DropShadowImageFilter.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/filters/skia/DropShadowImageFilter.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/filters/skia/DropShadowImageFilter.h
@@ -25,12 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "SkColor.h"
+#include "SkImageFilter.h"
 #include "SkScalar.h"
-#include "SkSingleInputImageFilter.h"
 
 namespace WebCore {
 
-class DropShadowImageFilter : public SkSingleInputImageFilter {
+class DropShadowImageFilter : public SkImageFilter {
 public:
     DropShadowImageFilter(SkScalar dx, SkScalar dy, SkScalar sigma, SkColor, SkImageFilter* input = 0);
     SK_DECLARE_PUBLIC_FLATTENABLE_DESERIALIZATION_PROCS(DropShadowImageFilter)
