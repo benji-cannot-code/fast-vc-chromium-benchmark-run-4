1a98500cf9d1d5fb395db41e1f30a595e950a93c
nayankk@motorola.com <nayankk@motorola.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-05 03:30:01 +0000

    [WEBGL] Rename WEBKIT_WEBGL_compressed_texture_s3tc to WEBGL_compressed_texture_s3tc https://bugs.webkit.org/show_bug.cgi?id=108866
    Reviewed by Kenneth Russell.
    
    WEBGL_compressed_texture_s3tc is one of the community approved WebGL extension.
    Hence remove the vendor prefix from WEBKIT_WEBGL_compressed_texture_s3tc.
    Specification: http://www.khronos.org/registry/webgl/extensions/WEBGL_compressed_texture_s3tc/
    
    No tests currently present to test WEBKIT_WEBGL_compressed_texture_s3tc.
    
    * bindings/js/JSWebGLRenderingContextCustom.cpp:
    (WebCore::toJS):
    * bindings/v8/custom/V8WebGLRenderingContextCustom.cpp:
    (WebCore::toV8Object):
    * html/canvas/WebGLCompressedTextureS3TC.cpp:
    (WebCore::WebGLCompressedTextureS3TC::getName):
    * html/canvas/WebGLExtension.h:
    * html/canvas/WebGLRenderingContext.cpp:
    (WebCore):
    (WebCore::WebGLRenderingContext::getExtension):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/WebGLExtension.h
+++ third_party/WebKit/Source/WebCore/html/canvas/WebGLExtension.h
@@ -43,7 +43,7 @@ public:
         OESVertexArrayObjectName,
         WebGLDebugRendererInfoName,
         WebGLDebugShadersName,
-        WebKitWebGLCompressedTextureS3TCName, // WEBKIT_ prefix until extension is official
+        WebGLCompressedTextureS3TCName,
         WebKitWebGLDepthTextureName, // WEBKIT_ prefix until extension is official
         OESElementIndexUintName,
     };
