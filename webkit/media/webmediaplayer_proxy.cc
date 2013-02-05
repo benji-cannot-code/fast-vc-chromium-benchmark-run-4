14fa0d1d7162eecf0ffa924abc65be4ccf374fb0
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 00:35:16 +0000

    Replace VideoRendererBase Get/PutCurrentFrame() with a VideoFrame-containing callback.
    BUG=108435
    
    Review URL: https://codereview.chromium.org/12096081
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180578 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_proxy.cc
+++ webkit/media/webmediaplayer_proxy.cc
@@ -35,8 +35,11 @@ WebMediaPlayerProxy::~WebMediaPlayerProxy() {
   Detach();
 }
 
-void WebMediaPlayerProxy::Repaint() {
+void WebMediaPlayerProxy::FrameReady(
+    const scoped_refptr<media::VideoFrame>& frame) {
   base::AutoLock auto_lock(lock_);
+  current_frame_ = frame;
+
   if (outstanding_repaints_ < kMaxOutstandingRepaints) {
     ++outstanding_repaints_;
 
@@ -49,12 +52,11 @@ void WebMediaPlayerProxy::Paint(SkCanvas* canvas,
                                 const gfx::Rect& dest_rect,
                                 uint8_t alpha) {
   DCHECK(render_loop_->BelongsToCurrentThread());
-  if (frame_provider_) {
-    scoped_refptr<media::VideoFrame> video_frame;
-    frame_provider_->GetCurrentFrame(&video_frame);
-    video_renderer_.Paint(video_frame, canvas, dest_rect, alpha);
-    frame_provider_->PutCurrentFrame(video_frame);
-  }
+
+  // Use GetCurrentFrame() to avoid locking while painting in software.
+  scoped_refptr<media::VideoFrame> video_frame;
+  GetCurrentFrame(&video_frame);
+  video_renderer_.Paint(video_frame, canvas, dest_rect, alpha);
 }
 
 bool WebMediaPlayerProxy::HasSingleOrigin() {
@@ -81,7 +83,6 @@ void WebMediaPlayerProxy::Detach() {
   DCHECK(render_loop_->BelongsToCurrentThread());
   webmediaplayer_ = NULL;
   data_source_ = NULL;
-  frame_provider_ = NULL;
 }
 
 void WebMediaPlayerProxy::RepaintTask() {
@@ -98,14 +99,8 @@ void WebMediaPlayerProxy::RepaintTask() {
 
 void WebMediaPlayerProxy::GetCurrentFrame(
     scoped_refptr<media::VideoFrame>* frame_out) {
-  if (frame_provider_)
-    frame_provider_->GetCurrentFrame(frame_out);
-}
-
-void WebMediaPlayerProxy::PutCurrentFrame(
-    scoped_refptr<media::VideoFrame> frame) {
-  if (frame_provider_)
-    frame_provider_->PutCurrentFrame(frame);
+  base::AutoLock auto_lock(lock_);
+  *frame_out = current_frame_;
 }
 
 }  // namespace webkit_media
