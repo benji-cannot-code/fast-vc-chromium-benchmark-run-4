339d48cdf65354d077e88eba32961e7233a0895c
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 17:47:28 +0000

    ash: Turn immersive mode off by default for M26
    * Add back the --ash-immersive-mode flag
    * Change about:flags to just turn immersive mode on/off
    * Convert --ash-immersive-hide-tab-indicators as a command-line only flag
    
    BUG=174060
    TEST=browser_tests BrowserNonClientFrameViewAsh unit_tests ImmersiveModeController
    
    Review URL: https://codereview.chromium.org/12194022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180720 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -231,14 +231,6 @@ const Experiment::Choice kChromeCaptivePortalDetectionChoices[] = {
 };
 #endif
 
-#if defined(USE_ASH)
-const Experiment::Choice kAshImmersiveModeChoices[] = {
-  { IDS_GENERIC_EXPERIMENT_CHOICE_DEFAULT, "", ""},
-  { IDS_FLAGS_ASH_IMMERSIVE_HIDE_TAB_INDICATORS,
-    ash::switches::kAshImmersiveHideTabIndicators, ""}
-};
-#endif
-
 // RECORDING USER METRICS FOR FLAGS:
 // -----------------------------------------------------------------------------
 // The first line of the experiment is the internal name. If you'd like to
@@ -1113,7 +1105,7 @@ const Experiment kExperiments[] = {
     IDS_FLAGS_ASH_IMMERSIVE_MODE_NAME,
     IDS_FLAGS_ASH_IMMERSIVE_MODE_DESCRIPTION,
     kOsCrOS,
-    MULTI_VALUE_TYPE(kAshImmersiveModeChoices),
+    SINGLE_VALUE_TYPE(ash::switches::kAshImmersiveMode),
   },
 #if defined(OS_LINUX)
   { "ash-enable-memory-monitor",
