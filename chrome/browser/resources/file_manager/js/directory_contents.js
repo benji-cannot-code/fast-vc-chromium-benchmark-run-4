beb8df2f624bd5c53248fdf92d81f8774daeb248
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 07:52:11 +0000

    Fix comments in directory_contents.js
    BUG=none
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12091103
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180631 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_contents.js
+++ chrome/browser/resources/file_manager/js/directory_contents.js
@@ -4,10 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 /**
- * @constructor
  * @param {MetadataCache} metadataCache Metadata cache service.
  * @param {cr.ui.ArrayDataModel} fileList The file list.
  * @param {boolean} showHidden If files starting with '.' are shown.
+ * @constructor
  */
 function FileListContext(metadataCache, fileList, showHidden) {
   /**
@@ -87,8 +87,9 @@ FileListContext.prototype.filter = function(entry) {
  * and filling the fileList. Different descendants handle various types of
  * directory contents shown: basic directory, drive search results, local search
  * results.
- * @constructor
  * @param {FileListContext} context The file list context.
+ * @constructor
+ * @extends {cr.EventTarget}
  */
 function DirectoryContents(context) {
   this.context_ = context;
@@ -103,7 +104,7 @@ function DirectoryContents(context) {
 }
 
 /**
- * DirectoryModel extends cr.EventTarget.
+ * DirectoryContents extends cr.EventTarget.
  */
 DirectoryContents.prototype.__proto__ = cr.EventTarget.prototype;
 
@@ -279,10 +280,10 @@ DirectoryContents.prototype.createDirectory = function(
 
 
 /**
- * @constructor
- * @extends {DirectoryContents}
  * @param {FileListContext} context File list context.
  * @param {DirectoryEntry} entry DirectoryEntry for current directory.
+ * @constructor
+ * @extends {DirectoryContents}
  */
 function DirectoryContentsBasic(context, entry) {
   DirectoryContents.call(this, context);
@@ -402,13 +403,13 @@ DirectoryContentsDriveSearch.SCAN_DELAY = 200;
 DirectoryContentsDriveSearch.MAX_RESULTS = 999;
 
 /**
- * @constructor
- * @extends {DirectoryContents}
  * @param {FileListContext} context File list context.
  * @param {DirectoryEntry} dirEntry Current directory.
  * @param {DirectoryEntry} previousDirEntry DirectoryEntry that was current
  *     before the search.
  * @param {string} query Search query.
+ * @constructor
+ * @extends {DirectoryContents}
  */
 function DirectoryContentsDriveSearch(context,
                                       dirEntry,
@@ -521,11 +522,11 @@ DirectoryContentsDriveSearch.prototype.readNextChunk = function() {
 
 
 /**
- * @constructor
- * @extends {DirectoryContents}
  * @param {FileListContext} context File list context.
  * @param {DirectoryEntry} dirEntry Current directory.
  * @param {string} query Search query.
+ * @constructor
+ * @extends {DirectoryContents}
  */
 function DirectoryContentsLocalSearch(context, dirEntry, query) {
   DirectoryContents.call(this, context);
