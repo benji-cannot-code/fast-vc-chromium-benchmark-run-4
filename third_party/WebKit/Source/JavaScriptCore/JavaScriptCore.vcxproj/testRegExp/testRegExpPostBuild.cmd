02dfeb07cb9fa042ca07b8596830af9f2edc5768
roger_fong@apple.com <roger_fong@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-05 20:52:36 +0000

    Unreviewed. VS2010 JavaScriptCore projects touch-ups.
    * JavaScriptCore.vcxproj/jsc/jsc.vcxproj:
    * JavaScriptCore.vcxproj/testRegExp/testRegExpPostBuild.cmd:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141918 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/JavaScriptCore.vcxproj/testRegExp/testRegExpPostBuild.cmd
+++ third_party/WebKit/Source/JavaScriptCore/JavaScriptCore.vcxproj/testRegExp/testRegExpPostBuild.cmd
@@ -1,8 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
 
-echo "WOAHHHAHAHAHA"
-
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin"
 if exist "%WebKit_Libraries%\bin\icudt46.dll" xcopy /y /d "%WebKit_Libraries%\bin\icudt46.dll" "%CONFIGURATIONBUILDDIR%\bin"
 if exist "%WebKit_Libraries%\bin\icudt46.dll" xcopy /y /d "%WebKit_Libraries%\bin\icudt46.dll" "%CONFIGURATIONBUILDDIR%\bin"
