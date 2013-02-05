e864e2e5b3ecf84bbca04164067370dba3986439
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 18:17:28 +0000

    Fix overlay for breakpad.
     Version 1091 of breakpad change signature of some methods that were
    overlayed, resulting in the wrong method being called and preventing
    file and line number to be computed.
    
    R=mark@chromium.org
    BUG=174340
    
    
    Review URL: https://chromiumcodereview.appspot.com/12213014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180727 0039d316-1c4b-4281-b951-d872f2087c98

--- breakpad/pending/src/common/dwarf_cu_to_module.h
+++ breakpad/pending/src/common/dwarf_cu_to_module.h
@@ -50,7 +50,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace google_breakpad {
 
-using dwarf2reader::AttributeList;
 using dwarf2reader::DwarfAttribute;
 using dwarf2reader::DwarfForm;
 using dwarf2reader::DwarfLanguage;
@@ -200,8 +199,7 @@ class DwarfCUToModule: public dwarf2reader::RootDIEHandler {
                               enum DwarfForm form,
                               const string &data);
   bool EndAttributes();
-  DIEHandler *FindChildHandler(uint64 offset, enum DwarfTag tag,
-                               const AttributeList &attrs);
+  DIEHandler *FindChildHandler(uint64 offset, enum DwarfTag tag);
 
   // Assign all our source Lines to the Functions that cover their
   // addresses, and then add them to module_.
@@ -210,8 +208,7 @@ class DwarfCUToModule: public dwarf2reader::RootDIEHandler {
   bool StartCompilationUnit(uint64 offset, uint8 address_size,
                             uint8 offset_size, uint64 cu_length,
                             uint8 dwarf_version);
-  bool StartRootDIE(uint64 offset, enum DwarfTag tag,
-                    const AttributeList& attrs);
+  bool StartRootDIE(uint64 offset, enum DwarfTag tag);
 
  private:
 
