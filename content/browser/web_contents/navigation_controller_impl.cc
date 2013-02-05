df91441bcde42fc50cff86df583a37166883671a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 08:20:28 +0000

    Fix a valgrind failure.
    The failure started showin up in the linux-valgrind bots after
    crrev 180271.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/12178032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180644 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/navigation_controller_impl.cc
+++ content/browser/web_contents/navigation_controller_impl.cc
@@ -527,7 +527,7 @@ void NavigationControllerImpl::OnScreenshotTaken(
     return;
   }
 
-  if (!success) {
+  if (!success || bitmap.empty() || bitmap.isNull()) {
     ClearScreenshot(entry);
     return;
   }
