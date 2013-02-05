cba213c23b238af78811dbf91387906b8a21fb40
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 09:24:07 +0000

    Fix possible crash in RecursiveOperationDelegate and throttle # of parallel tasks
    - RecursiveOperationDelegate::ProcessFile() could fail /w calling its
      callback in an synchronous way, so it must be always called
      asynchronously or at the end of a synchronous block.
    - LocalFilesystemOperation::SetUp() has to be also called before calling into
      RecursiveOperationDelegate.
    - This also introduces a constant kMaxInflightOperations to cap the max # of
      inflight operations at the same time.
    
    BUG=146215
    TEST=tested with locally patched test, http://crbug.com/172424 will add one
    TEST=LocalFileSystemOperationTest.TestRemoveSuccessRecursive for throttle
    
    Review URL: https://codereview.chromium.org/12177006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180668 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/recursive_operation_delegate.h
+++ webkit/fileapi/recursive_operation_delegate.h
@@ -75,7 +75,8 @@ class RecursiveOperationDelegate
   FileSystemContext* file_system_context();
 
  private:
-  void ProcessNextDirectory(base::PlatformFileError error);
+  void ProcessNextDirectory();
+  void ProcessPendingFiles();
   void DidProcessFile(base::PlatformFileError error);
   void DidProcessDirectory(const FileSystemURL& url,
                            base::PlatformFileError error);
@@ -90,6 +91,7 @@ class RecursiveOperationDelegate
   LocalFileSystemOperation* original_operation_;
   StatusCallback callback_;
   std::queue<FileSystemURL> pending_directories_;
+  std::queue<FileSystemURL> pending_files_;
   int inflight_operations_;
 
   DISALLOW_COPY_AND_ASSIGN(RecursiveOperationDelegate);
