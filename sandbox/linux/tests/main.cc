c00bc11e7fa3976707910345d4d7460d0f8c3af0
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 19:01:51 +0000

    Linux sandbox: add warning if running tests multithreaded.
    BUG=169416
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12207004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180747 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/tests/main.cc
+++ sandbox/linux/tests/main.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "testing/gtest/include/gtest/gtest.h"
 
-
 int main(int argc, char *argv[]) {
   testing::InitGoogleTest(&argc, argv);
   // Always go through re-execution for death tests.
