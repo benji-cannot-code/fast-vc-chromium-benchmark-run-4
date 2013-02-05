64d0a9aadaeb2655fb66fbf27562eb316f34d48e
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 02:07:30 +0000

    Fix ServerBoundCertService::RequestHandle::OnRequestComplete possible use after free.
    BUG=173465
    
    
    Review URL: https://chromiumcodereview.appspot.com/12092104
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180603 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/server_bound_cert_service.cc
+++ net/base/server_bound_cert_service.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/callback_helpers.h"
 #include "base/compiler_specific.h"
 #include "base/location.h"
 #include "base/logging.h"
@@ -399,8 +400,9 @@ void ServerBoundCertService::RequestHandle::RequestStarted(
 
 void ServerBoundCertService::RequestHandle::OnRequestComplete(int result) {
   request_ = NULL;
-  callback_.Run(result);
-  callback_.Reset();
+  // Running the callback might delete |this|, so we can't touch any of our
+  // members afterwards. Reset callback_ first.
+  base::ResetAndReturn(&callback_).Run(result);
 }
 
 ServerBoundCertService::ServerBoundCertService(
