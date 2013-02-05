0539efbd7bcf7c06bd0fc977a0af56d436e289cc
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 00:14:14 +0000

    Attempt at fixing inability to  attach a tab.  We should only query touch location if the drag is the result of a gesture.
    BUG=172950
    TEST=none
    R=sadrul@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12179014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180571 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab_drag_controller.h
+++ chrome/browser/ui/views/tabs/tab_drag_controller.h
@@ -74,6 +74,12 @@ class TabDragController : public content::WebContentsDelegate,
     REORDER
   };
 
+  // Indicates the event source that initiated the drag.
+  enum EventSource {
+    EVENT_SOURCE_MOUSE,
+    EVENT_SOURCE_TOUCH,
+  };
+
   // Amount above or below the tabstrip the user has to drag before detaching.
   static const int kTouchVerticalDetachMagnetism;
   static const int kVerticalDetachMagnetism;
@@ -96,7 +102,8 @@ class TabDragController : public content::WebContentsDelegate,
             int source_tab_offset,
             const ui::ListSelectionModel& initial_selection_model,
             DetachBehavior detach_behavior,
-            MoveBehavior move_behavior);
+            MoveBehavior move_behavior,
+            EventSource event_source);
 
   // Returns true if there is a drag underway and the drag is attached to
   // |tab_strip|.
@@ -469,6 +476,8 @@ class TabDragController : public content::WebContentsDelegate,
 
   // Handles registering for notifications.
   content::NotificationRegistrar registrar_;
+
+  EventSource event_source_;
 
   // The TabStrip the drag originated from.
   TabStrip* source_tabstrip_;
