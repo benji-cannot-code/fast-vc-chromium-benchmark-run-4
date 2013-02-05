ad732d4dd9e44ffe62be8ccaac94e36b8e3a8bf2
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-05 13:52:22 +0000

    Web Inspector: Create separate project for each file system added to inspector. https://bugs.webkit.org/show_bug.cgi?id=108652
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Every file system added to web inspector is now represented by its own project in workspace.
    FileSystemMapping changed accrodingly.
    
    Test: inspector/file-system-mapping.html
    
    * inspector/front-end/FileMapping.js:
    (WebInspector.FileMapping.prototype._entryURIPrefix):
    * inspector/front-end/FileSystemMapping.js:
    (WebInspector.FileSystemMapping.prototype.fileForURI):
    (WebInspector.FileSystemMapping.prototype.uriForFile):
    (WebInspector.FileSystemMapping.prototype.uriPrefixForPathPrefix):
    (WebInspector.FileSystemMappingImpl):
    (WebInspector.FileSystemMappingImpl.prototype._loadFromSettings.get this):
    (WebInspector.FileSystemMappingImpl.prototype._loadFromSettings):
    (WebInspector.FileSystemMappingImpl.prototype._saveToSettings):
    (WebInspector.FileSystemMappingImpl.prototype.set _fileSystemName):
    (WebInspector.FileSystemMappingImpl.prototype.fileSystemId):
    (WebInspector.FileSystemMappingImpl.prototype.addFileSystemMapping):
    (WebInspector.FileSystemMappingImpl.prototype.removeFileSystemMapping):
    (WebInspector.FileSystemMappingImpl.prototype.fileSystemPaths):
    (WebInspector.FileSystemMappingImpl.prototype.fileForURI):
    (WebInspector.FileSystemMappingImpl.prototype.uriForFile):
    (WebInspector.FileSystemMappingImpl.prototype.uriPrefixForPathPrefix):
    * inspector/front-end/FileSystemWorkspaceProvider.js:
    (WebInspector.FileSystemWorkspaceProvider):
    (WebInspector.FileSystemWorkspaceProvider.prototype.innerCallback):
    (WebInspector.FileSystemWorkspaceProvider.prototype.requestFileContent):
    (WebInspector.FileSystemWorkspaceProvider.prototype.setFileContent):
    (WebInspector.FileSystemWorkspaceProvider.prototype._populate.filesLoaded):
    (WebInspector.FileSystemWorkspaceProvider.prototype._populate):
    (WebInspector.FileSystemWorkspaceProvider.prototype._addFile):
    (WebInspector.FileSystemWorkspaceProvider.prototype._removeFile):
    (WebInspector.FileSystemWorkspaceProvider.prototype.reset):
    * inspector/front-end/IsolatedFileSystemModel.js:
    (WebInspector.IsolatedFileSystemModel):
    (WebInspector.IsolatedFileSystemModel.prototype._innerAddFileSystem):
    (WebInspector.IsolatedFileSystemModel.prototype._fileSystemRemoved):
    * inspector/front-end/Workspace.js:
    (WebInspector.Project.prototype.searchInFileContent):
    (WebInspector.Project.prototype.dispose):
    (WebInspector.Workspace.prototype.removeProject):
    
    LayoutTests:
    
    * inspector/file-mapping.html:
    * inspector/file-system-mapping-expected.txt: Added.
    * inspector/file-system-mapping.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/FileMapping.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/FileMapping.js
@@ -59,7 +59,7 @@ WebInspector.FileMapping.prototype = {
      */
     _entryURIPrefix: function(entry)
     {
-        return this._fileSystemMapping.uriForPath(entry.pathPrefix);
+        return this._fileSystemMapping.uriPrefixForPathPrefix(entry.pathPrefix);
     },
     
     /**
