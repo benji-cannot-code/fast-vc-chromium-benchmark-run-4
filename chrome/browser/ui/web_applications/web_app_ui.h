0666419c82a757bc6be11ced742d83d94bbfd7ee
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-05 23:20:50 +0000

    Load icon before trying to create an app shortcut.
    This includes a refactor to make the app icon loading infrastructure more
    central.
    
    Review URL: https://codereview.chromium.org/12183015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180815 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/web_applications/web_app_ui.h
+++ chrome/browser/ui/web_applications/web_app_ui.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/callback.h"
 #include "chrome/browser/shell_integration.h"
 
 namespace content {
@@ -22,6 +23,12 @@ class Profile;
 
 namespace web_app {
 
+typedef base::Callback<void(const ShellIntegration::ShortcutInfo&)>
+    ShortcutInfoCallback;
+
+ShellIntegration::ShortcutInfo ShortcutInfoForExtensionAndProfile(
+    const extensions::Extension* extension, Profile* profile);
+
 // Extracts shortcut info of the given WebContents.
 void GetShortcutInfoForTab(content::WebContents* web_contents,
                            ShellIntegration::ShortcutInfo* info);
@@ -34,11 +41,16 @@ void GetShortcutInfoForTab(content::WebContents* web_contents,
 void UpdateShortcutForTabContents(content::WebContents* web_contents);
 
 // Updates the shortcut info for |extension| and |profile|.
-// TODO(benwells): make this download the icon as well to remove boilerplate
-// code from call sites.
 void UpdateShortcutInfoForApp(const extensions::Extension& extension,
                               Profile* profile,
                               ShellIntegration::ShortcutInfo* shortcut_info);
+
+// Fetches the icon for |extension| and calls |callback| with shortcut info
+// filled out as by UpdateShortcutInfoForApp.
+void UpdateShortcutInfoAndIconForApp(
+    const extensions::Extension& extension,
+    Profile* profile,
+    const ShortcutInfoCallback& callback);
 
 }  // namespace web_app
 
