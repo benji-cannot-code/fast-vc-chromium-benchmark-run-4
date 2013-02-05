41f88afb685f5a586f7f7b0fbb48ba5585c8d099
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-05 06:20:33 +0000

    Fix crash by <select> type change on focus https://bugs.webkit.org/show_bug.cgi?id=108830
    Reviewed by Abhishek Arya.
    
    Source/WebCore:
    
    Test: fast/forms/select/select-change-type-on-focus.html
    
    * html/HTMLSelectElement.cpp:
    (WebCore::HTMLSelectElement::menuListDefaultEventHandler):
    focus() calls may change the renderer type.
    
    LayoutTests:
    
    * fast/forms/select/select-change-type-on-focus-expected.txt: Added.
    * fast/forms/select/select-change-type-on-focus.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141858 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLSelectElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLSelectElement.cpp
@@ -1162,9 +1162,9 @@ void HTMLSelectElement::menuListDefaultEventHandler(Event* event)
             if (keyCode == ' ' || keyCode == '\r') {
                 focus();
 
-                // Calling focus() may cause us to lose our renderer, in which case
-                // do not want to handle the event.
-                if (!renderer())
+                // Calling focus() may remove the renderer or change the
+                // renderer type.
+                if (!renderer() || !renderer()->isMenuList())
                     return;
 
                 // Save the selection so it can be compared to the new selection
@@ -1180,9 +1180,9 @@ void HTMLSelectElement::menuListDefaultEventHandler(Event* event)
             if (keyCode == ' ') {
                 focus();
 
-                // Calling focus() may cause us to lose our renderer, in which case
-                // do not want to handle the event.
-                if (!renderer())
+                // Calling focus() may remove the renderer or change the
+                // renderer type.
+                if (!renderer() || !renderer()->isMenuList())
                     return;
 
                 // Save the selection so it can be compared to the new selection
