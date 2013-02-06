54fb480864488334d00fb92294d816b9b60e9d49
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 12:20:05 +0000

    Added more default expectations to MockDBusThreadManager.
    This prevents a lot of warnings when using a MockDBusThreadManager in a browser_test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12223011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180967 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/mock_dbus_thread_manager.cc
+++ chromeos/dbus/mock_dbus_thread_manager.cc
@@ -109,7 +109,11 @@ MockDBusThreadManager::MockDBusThreadManager()
       .WillRepeatedly(Return(mock_speech_synthesizer_client_.get()));
   EXPECT_CALL(*this, GetUpdateEngineClient())
       .WillRepeatedly(Return(mock_update_engine_client_.get()));
+  EXPECT_CALL(*this, GetIBusInputContextClient())
+      .WillRepeatedly(ReturnNull());
 
+  EXPECT_CALL(*this, GetSystemBus())
+      .WillRepeatedly(ReturnNull());
   EXPECT_CALL(*this, GetIBusBus())
       .WillRepeatedly(ReturnNull());
 
@@ -118,9 +122,15 @@ MockDBusThreadManager::MockDBusThreadManager()
       .Times(AnyNumber());
   EXPECT_CALL(*mock_power_manager_client_.get(), RemoveObserver(_))
       .Times(AnyNumber());
+  EXPECT_CALL(*mock_power_manager_client_.get(), NotifyUserActivity(_))
+      .Times(AnyNumber());
+  EXPECT_CALL(*mock_power_manager_client_.get(), NotifyVideoActivity(_, _))
+      .Times(AnyNumber());
   EXPECT_CALL(*mock_session_manager_client_.get(), AddObserver(_))
       .Times(AnyNumber());
   EXPECT_CALL(*mock_session_manager_client_.get(), RemoveObserver(_))
+      .Times(AnyNumber());
+  EXPECT_CALL(*mock_session_manager_client_.get(), HasObserver(_))
       .Times(AnyNumber());
   EXPECT_CALL(*mock_update_engine_client_.get(), AddObserver(_))
       .Times(AnyNumber());
@@ -154,6 +164,9 @@ MockDBusThreadManager::MockDBusThreadManager()
   // Called from DiskMountManager::Initialize(), ChromeBrowserMainPartsChromeos.
   EXPECT_CALL(*mock_cros_disks_client_.get(), SetUpConnections(_, _))
       .Times(AnyNumber());
+  EXPECT_CALL(*mock_cros_disks_client_.get(),
+              EnumerateAutoMountableDevices(_, _))
+      .Times(AnyNumber());
 
   // Called from BluetoothManagerImpl ctor.
   EXPECT_CALL(*mock_bluetooth_manager_client_.get(), DefaultAdapter(_))
@@ -168,6 +181,15 @@ MockDBusThreadManager::MockDBusThreadManager()
   // Called from BrightnessController::GetBrightnessPercent as part of ash tray
   // initialization.
   EXPECT_CALL(*mock_power_manager_client_.get(), GetScreenBrightnessPercent(_))
+      .Times(AnyNumber());
+
+  // Called from GeolocationHandler::Init().
+  EXPECT_CALL(*mock_shill_manager_client_.get(), GetProperties(_))
+      .Times(AnyNumber());
+  EXPECT_CALL(*mock_shill_manager_client_.get(), AddPropertyChangedObserver(_))
+      .Times(AnyNumber());
+  EXPECT_CALL(*mock_shill_manager_client_.get(),
+              RemovePropertyChangedObserver(_))
       .Times(AnyNumber());
 }
 
