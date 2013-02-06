e7156f6f96924d346c945af3c0d741d6edc19583
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-06 07:44:53 +0000

    Add #if USE(V8) to Node::V8CollectableDuringMinorGCFlag https://bugs.webkit.org/show_bug.cgi?id=109009
    Reviewed by Kent Tamura.
    
    Node flags should be saved. V8CollectableDuringMinorGCFlag is used by V8 only.
    
    * dom/Node.h:
    (Node):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141972 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Node.h
+++ third_party/WebKit/Source/WebCore/dom/Node.h
@@ -381,8 +381,10 @@ public:
     bool hasEventTargetData() const { return getFlag(HasEventTargetDataFlag); }
     void setHasEventTargetData(bool flag) { setFlag(flag, HasEventTargetDataFlag); }
 
+#if USE(V8)
     bool isV8CollectableDuringMinorGC() const { return getFlag(V8CollectableDuringMinorGCFlag); }
     void setV8CollectableDuringMinorGC(bool flag) { setFlag(flag, V8CollectableDuringMinorGCFlag); }
+#endif
 
     enum ShouldSetAttached {
         SetAttached,
@@ -719,7 +721,9 @@ private:
         HasCustomCallbacksFlag = 1 << 21,
         HasScopedHTMLStyleChildFlag = 1 << 22,
         HasEventTargetDataFlag = 1 << 23,
+#if USE(V8)
         V8CollectableDuringMinorGCFlag = 1 << 24,
+#endif
         NeedsShadowTreeWalkerFlag = 1 << 25,
         IsInShadowTreeFlag = 1 << 26,
 
