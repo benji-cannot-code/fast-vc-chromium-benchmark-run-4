bc2d3ff29736ce2ec75d27356b6a0e2344bffafc
shawnsingh@chromium.org <shawnsingh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 01:30:04 +0000

    Add about:flags experiment to enable/disable fixed position element compositing.
    We would like to begin pushing for this feature (composted fixed-position
    elements). Adding this flag is a first step.
    
    BUG=172738
    
    
    Review URL: https://chromiumcodereview.appspot.com/12088075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180847 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.cc
+++ webkit/glue/webpreferences.cc
@@ -94,7 +94,6 @@ WebPreferences::WebPreferences()
       unified_textchecker_enabled(false),
       accelerated_compositing_enabled(false),
       force_compositing_mode(false),
-      fixed_position_compositing_enabled(false),
       accelerated_compositing_for_3d_transforms_enabled(false),
       accelerated_compositing_for_animation_enabled(false),
       accelerated_compositing_for_video_enabled(false),
@@ -392,11 +391,6 @@ void WebPreferences::Apply(WebView* web_view) const {
 
   // Enable gpu-accelerated compositing if requested on the command line.
   settings->setAcceleratedCompositingEnabled(accelerated_compositing_enabled);
-
-  // Enable compositing for fixed position elements if requested
-  // on the command line.
-  settings->setAcceleratedCompositingForFixedPositionEnabled(
-      fixed_position_compositing_enabled);
 
   // Enable gpu-accelerated 2d canvas if requested on the command line.
   settings->setAccelerated2dCanvasEnabled(accelerated_2d_canvas_enabled);
