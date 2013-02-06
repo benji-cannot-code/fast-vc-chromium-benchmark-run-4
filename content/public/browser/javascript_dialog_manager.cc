78e6ef1a9ae184f645d7f61a74441f121ae9020d
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 22:22:26 +0000

    Implement browser-side intercept and handling of inspector Page.handleJavaScriptDialog. BUG=167113
    Review URL: https://chromiumcodereview.appspot.com/12088071
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181066 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/public/browser/javascript_dialog_manager.cc
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/public/browser/javascript_dialog_manager.h"
+
+namespace content {
+
+bool JavaScriptDialogManager::HandleJavaScriptDialog(WebContents* web_contents,
+                                                     bool accept) {
+  return false;
+}
+
+}  // namespace content
