710a8767785b57f538f3c0becd0ba70540904204
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 02:13:58 +0000

    cc: Add cheapness predictor success histogram
    For picture piles that are fairly cheap to rasterize, we want to do the step on the same thread (instead of dispatching it to a different thread). As a first step, this change adds an accuracy metric for the predictor. Three new histograms are added: CheapPredictorAccuracy measures overall success rate of the predictor; CheapPredictorSafelyWrong counts the number of times the prediction was "this is a slow raster", but in reality it was fast; CheapPredictorBadlyWrong counts the number of times the prediction was "this is a fast raster", but in reality it was slow.
    
    BUG=173426
    
    
    Review URL: https://chromiumcodereview.appspot.com/12192025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180863 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_picture_layer_tiling_client.cc
+++ cc/test/fake_picture_layer_tiling_client.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 FakePictureLayerTilingClient::FakePictureLayerTilingClient()
-    : tile_manager_(&tile_manager_client_, NULL, 1, false),
+    : tile_manager_(&tile_manager_client_, NULL, 1, false, false),
       pile_(PicturePileImpl::Create()) {
 }
 
