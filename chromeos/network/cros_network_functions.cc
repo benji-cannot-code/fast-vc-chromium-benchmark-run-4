37912331f5c49bd81b3a3d4054de7c21b7d7d93a
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 17:35:26 +0000

    Calling Shill's ConfigureService instead of GetService for configuring VPN.
    BUG=chromium-os:31523
    
    
    Review URL: https://chromiumcodereview.appspot.com/12039087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180998 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/cros_network_functions.cc
+++ chromeos/network/cros_network_functions.cc
@@ -146,6 +146,9 @@ bool GetTimeProperty(const base::DictionaryValue& dictionary,
 // Does nothing. Used as a callback.
 void DoNothingWithCallStatus(DBusMethodCallStatus call_status) {}
 
+// Does nothing. Used as a callback.
+void DoNothingWithObjectPath(const dbus::ObjectPath& object_path) {}
+
 // Ignores errors.
 void IgnoreErrors(const std::string& error_name,
                   const std::string& error_message) {}
@@ -493,10 +496,10 @@ void CrosRequestVirtualNetworkProperties(
       flimflam::kVPNDomainProperty,
       new base::StringValue(service_name));
 
-  // shill.Manger.GetService() will apply the property changes in
+  // shill.Manger.ConfigureService() will apply the property changes in
   // |properties| and pass a new or existing service to OnGetService().
   // OnGetService will then call GetProperties which will then call callback.
-  DBusThreadManager::Get()->GetShillManagerClient()->GetService(
+  DBusThreadManager::Get()->GetShillManagerClient()->ConfigureService(
       properties, base::Bind(&OnGetService, callback),
       base::Bind(&IgnoreErrors));
 }
@@ -652,7 +655,7 @@ void CrosRequestIPConfigRefresh(const std::string& ipconfig_path) {
 
 void CrosConfigureService(const base::DictionaryValue& properties) {
   DBusThreadManager::Get()->GetShillManagerClient()->ConfigureService(
-      properties, base::Bind(&DoNothing),
+      properties, base::Bind(&DoNothingWithObjectPath),
       base::Bind(&IgnoreErrors));
 }
 
