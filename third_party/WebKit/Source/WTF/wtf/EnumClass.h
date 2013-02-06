2b05d089b99a0ebbe0e702d5dd4a6ce296e5c5fe
mark.lam@apple.com <mark.lam@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-06 04:30:28 +0000

    Fix EnumClass so that it can be used with switch statements. https://bugs.webkit.org/show_bug.cgi?id=109004.
    Reviewed by Sam Weinig.
    
    * wtf/EnumClass.h:
    (WTF::EnumClass::operator==):
    (WTF::EnumClass::operator!=):
    (WTF::EnumClass::operator<):
    (WTF::EnumClass::operator<=):
    (WTF::EnumClass::operator>):
    (WTF::EnumClass::operator>=):
    (EnumClass):
    (WTF::EnumClass::operator Value):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/EnumClass.h
+++ third_party/WebKit/Source/WTF/wtf/EnumClass.h
@@ -55,6 +55,7 @@ namespace WTF {
 // Otherwise, it will use the EnumClass template below.
 
 #if COMPILER_SUPPORTS(CXX_STRONG_ENUMS)
+
 #define ENUM_CLASS(__enumName) \
     enum class __enumName
 
@@ -100,6 +101,15 @@ public:
     ALWAYS_INLINE bool operator<=(const EnumClass other) { return m_value <= other.m_value; }
     ALWAYS_INLINE bool operator>(const EnumClass other) { return m_value > other.m_value; }
     ALWAYS_INLINE bool operator>=(const EnumClass other) { return m_value >= other.m_value; }
+
+    ALWAYS_INLINE bool operator==(const Value value) { return m_value == value; }
+    ALWAYS_INLINE bool operator!=(const Value value) { return m_value != value; }
+    ALWAYS_INLINE bool operator<(const Value value) { return m_value < value; }
+    ALWAYS_INLINE bool operator<=(const Value value) { return m_value <= value; }
+    ALWAYS_INLINE bool operator>(const Value value) { return m_value > value; }
+    ALWAYS_INLINE bool operator>=(const Value value) { return m_value >= value; }
+
+    ALWAYS_INLINE operator Value() { return m_value; }
 
 private:
     Value m_value;
