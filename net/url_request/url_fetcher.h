4275f7243bbf35fae66ab538f1d4ce80f859c09d
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 20:14:15 +0000

    Allow no content-type for POST, PUT and PATCH methods.
    This CL allows to have no content type for UrlFetcher's POST, PUT and PATCH methods, if the body is empty.
    At the same time, POST_WITHOUT_BODY is deprecated, as POST
    should work such a case. The following CL will clean it up, after the code using it is fixed.
    
    BUG=148632
    TEST=Ran unit_tests.
    
    Review URL: https://chromiumcodereview.appspot.com/12226008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181039 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_fetcher.h
+++ net/url_request/url_fetcher.h
@@ -84,8 +84,9 @@ class NET_EXPORT URLFetcher {
     PUT,
     PATCH,
 
-    // This is POST method with no body, no content-type header and
-    // no content-length header.
+    // DEPRECATED: Now POST should work well for empty body, and this value
+    //   will be removed soon.
+    // This is POST method with no body, and no content-type header.
     POST_WITHOUT_BODY,
   };
 
@@ -289,8 +290,9 @@ class NET_EXPORT URLFetcher {
   // true, caller takes responsibility for the file, and it will not
   // be removed once the URLFetcher is destroyed.  User should not take
   // ownership more than once, or call this method after taking ownership.
-  virtual bool GetResponseAsFilePath(bool take_ownership,
-                                     base::FilePath* out_response_path) const = 0;
+  virtual bool GetResponseAsFilePath(
+      bool take_ownership,
+      base::FilePath* out_response_path) const = 0;
 };
 
 }  // namespace net
