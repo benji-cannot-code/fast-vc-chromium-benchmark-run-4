18758444e11e257710678cf5a8eb175ca059b269
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-06 12:36:43 +0000

    Web Inspector: update javascriptsourcetokenizer to produce "whitespaces" token https://bugs.webkit.org/show_bug.cgi?id=108945
    Patch by Andrey Lushnikov <lushnikov@chromium.org> on 2013-02-06
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Update re2c grammar for SourceJavaScriptTokenizer to produce
    "whitespace" token which holds consequtive whitespaces in it.
    
    Updated existing tests expectations.
    
    * inspector/front-end/DOMSyntaxHighlighter.js:
    (WebInspector.DOMSyntaxHighlighter.prototype.createSpan): Do not strip spaces from tokens with class "whitespaces".
    * inspector/front-end/SourceJavaScriptTokenizer.js: Regenerated.
    (WebInspector.SourceJavaScriptTokenizer.prototype.nextToken):
    * inspector/front-end/SourceJavaScriptTokenizer.re2js:
    
    LayoutTests:
    
    Update test expectations so that they have "whitespace" token in
    there.
    
    * inspector/editor/highlighter-basics-expected.txt:
    * inspector/editor/highlighter-chunk-limit-expected.txt:
    * inspector/editor/highlighter-long-line.html:
    * inspector/editor/text-editor-long-line-expected.txt:
    * inspector/syntax-highlight-html-expected.txt:
    * inspector/syntax-highlight-javascript-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141987 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DOMSyntaxHighlighter.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DOMSyntaxHighlighter.js
@@ -43,7 +43,7 @@ WebInspector.DOMSyntaxHighlighter.prototype = {
     {
         var span = document.createElement("span");
         span.className = "webkit-" + className;
-        if (this._stripExtraWhitespace)
+        if (this._stripExtraWhitespace && className !== "whitespace")
             content = content.replace(/^[\n\r]*/, "").replace(/\s*$/, "");
         span.appendChild(document.createTextNode(content));
         return span;
