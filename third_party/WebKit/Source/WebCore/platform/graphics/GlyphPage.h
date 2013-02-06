95c76f4c199de3b0a974ba063d33dbeb0a196927
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-06 08:27:21 +0000

    Unreviewed, rolling out r141961. http://trac.webkit.org/changeset/141961 https://bugs.webkit.org/show_bug.cgi?id=109019
    assertion failures on svn tests such as fonts-glyph-04-t.svg
    (Requested by falken on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-06
    
    * platform/graphics/GlyphPage.h:
    (WebCore::GlyphPage::create):
    (WebCore::GlyphPage::glyphDataForCharacter):
    (WebCore::GlyphPage::glyphDataForIndex):
    (WebCore::GlyphPage::fontDataForCharacter):
    (WebCore::GlyphPage::setGlyphDataForIndex):
    (GlyphPage):
    (WebCore::GlyphPage::copyFrom):
    (WebCore::GlyphPage::clear):
    (WebCore::GlyphPage::clearForFontData):
    (WebCore::GlyphPage::GlyphPage):
    * platform/graphics/GlyphPageTreeNode.cpp:
    (WebCore::GlyphPageTreeNode::initializePage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141976 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GlyphPage.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/GlyphPage.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- * Copyright (C) 2006, 2007, 2008, 2013 Apple Inc. All rights reserved.
+ * Copyright (C) 2006, 2007, 2008 Apple Inc. All rights reserved.
  * Copyright (C) Research In Motion Limited 2011. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Glyph.h"
 #include <wtf/PassRefPtr.h>
 #include <wtf/RefCounted.h>
-#include <wtf/RefPtr.h>
 #include <wtf/unicode/Unicode.h>
 
 namespace WebCore {
@@ -64,32 +63,9 @@ struct GlyphData {
 // to be overriding the parent's node, but provide no additional information.
 class GlyphPage : public RefCounted<GlyphPage> {
 public:
-    static PassRefPtr<GlyphPage> createUninitialized(GlyphPageTreeNode* owner)
+    static PassRefPtr<GlyphPage> create(GlyphPageTreeNode* owner)
     {
-        return adoptRef(new GlyphPage(owner, false));
-    }
-
-    static PassRefPtr<GlyphPage> createZeroedSystemFallbackPage(GlyphPageTreeNode* owner)
-    {
-        return adoptRef(new GlyphPage(owner, true));
-    }
-
-    PassRefPtr<GlyphPage> createCopiedSystemFallbackPage(GlyphPageTreeNode* owner) const
-    {
-        RefPtr<GlyphPage> page = GlyphPage::createUninitialized(owner);
-        memcpy(page->m_glyphs, m_glyphs, sizeof(m_glyphs));
-        page->m_fontDataForAllGlyphs = m_fontDataForAllGlyphs;
-        if (m_perGlyphFontData) {
-            page->m_perGlyphFontData = static_cast<const SimpleFontData**>(fastMalloc(size * sizeof(SimpleFontData*)));
-            memcpy(page->m_perGlyphFontData, m_perGlyphFontData, size * sizeof(SimpleFontData*));
-        }
-        return page.release();
-    }
-
-    ~GlyphPage()
-    {
-        if (m_perGlyphFontData)
-            fastFree(m_perGlyphFontData);
+        return adoptRef(new GlyphPage(owner));
     }
 
     static const size_t size = 256; // Covers Latin-1 in a single page.
@@ -97,18 +73,14 @@ public:
     unsigned indexForCharacter(UChar32 c) const { return c % size; }
     GlyphData glyphDataForCharacter(UChar32 c) const
     {
-        return glyphDataForIndex(indexForCharacter(c));
+        unsigned index = indexForCharacter(c);
+        return GlyphData(m_glyphs[index], m_glyphFontData[index]);
     }
 
     GlyphData glyphDataForIndex(unsigned index) const
     {
         ASSERT_WITH_SECURITY_IMPLICATION(index < size);
-        Glyph glyph = m_glyphs[index];
-        if (UNLIKELY(!glyph))
-            return GlyphData(0, 0);
-        if (UNLIKELY(!!m_perGlyphFontData))
-            return GlyphData(glyph, m_perGlyphFontData[index]);
-        return GlyphData(glyph, m_fontDataForAllGlyphs);
+        return GlyphData(m_glyphs[index], m_glyphFontData[index]);
     }
 
     Glyph glyphAt(unsigned index) const
@@ -119,7 +91,7 @@ public:
 
     const SimpleFontData* fontDataForCharacter(UChar32 c) const
     {
-        return glyphDataForIndex(indexForCharacter(c)).fontData;
+        return m_glyphFontData[indexForCharacter(c)];
     }
 
     void setGlyphDataForCharacter(UChar32 c, Glyph g, const SimpleFontData* f)
@@ -127,55 +99,36 @@ public:
         setGlyphDataForIndex(indexForCharacter(c), g, f);
     }
 
-    void setGlyphDataForIndex(unsigned index, Glyph glyph, const SimpleFontData* fontData)
+    void setGlyphDataForIndex(unsigned index, Glyph g, const SimpleFontData* f)
     {
         ASSERT_WITH_SECURITY_IMPLICATION(index < size);
-        m_glyphs[index] = glyph;
-
-        // GlyphPage getters will always return a null SimpleFontData* for glyph #0, so don't worry about the pointer for them.
-        if (!glyph)
-            return;
-
-        // A glyph index without a font data pointer makes no sense.
-        ASSERT(fontData);
-
-        if (m_perGlyphFontData) {
-            m_perGlyphFontData[index] = fontData;
-            return;
-        }
-
-        if (!m_fontDataForAllGlyphs)
-            m_fontDataForAllGlyphs = fontData;
-
-        if (m_fontDataForAllGlyphs == fontData)
-            return;
-
-        // This GlyphPage houses glyphs from multiple fonts, transition to an array of SimpleFontData pointers.
-        const SimpleFontData* oldFontData = m_fontDataForAllGlyphs;
-        m_perGlyphFontData = static_cast<const SimpleFontData**>(fastMalloc(size * sizeof(SimpleFontData*)));
-        for (unsigned i = 0; i < size; ++i)
-            m_perGlyphFontData[i] = oldFontData;
-        m_perGlyphFontData[index] = fontData;
+        m_glyphs[index] = g;
+        m_glyphFontData[index] = f;
     }
 
     void setGlyphDataForIndex(unsigned index, const GlyphData& glyphData)
     {
         setGlyphDataForIndex(index, glyphData.glyph, glyphData.fontData);
     }
+    
+    void copyFrom(const GlyphPage& other)
+    {
+        memcpy(m_glyphs, other.m_glyphs, sizeof(m_glyphs));
+        memcpy(m_glyphFontData, other.m_glyphFontData, sizeof(m_glyphFontData));
+    }
+
+    void clear()
+    {
+        memset(m_glyphs, 0, sizeof(m_glyphs));
+        memset(m_glyphFontData, 0, sizeof(m_glyphFontData));
+    }
 
     void clearForFontData(const SimpleFontData* fontData)
     {
-        if (!m_perGlyphFontData) {
-            if (m_fontDataForAllGlyphs == fontData) {
-                memset(m_glyphs, 0, sizeof(m_glyphs));
-                m_fontDataForAllGlyphs = 0;
-            }
-            return;
-        }
         for (size_t i = 0; i < size; ++i) {
-            if (m_perGlyphFontData[i] == fontData) {
+            if (m_glyphFontData[i] == fontData) {
                 m_glyphs[i] = 0;
-                m_perGlyphFontData[i] = 0;
+                m_glyphFontData[i] = 0;
             }
         }
     }
@@ -186,22 +139,18 @@ public:
     bool fill(unsigned offset, unsigned length, UChar* characterBuffer, unsigned bufferLength, const SimpleFontData*);
 
 private:
-    GlyphPage(GlyphPageTreeNode* owner, bool clearGlyphs)
-        : m_fontDataForAllGlyphs(0)
-        , m_perGlyphFontData(0)
-        , m_owner(owner)
+    GlyphPage(GlyphPageTreeNode* owner)
+        : m_owner(owner)
     {
-        if (clearGlyphs)
-            memset(m_glyphs, 0, sizeof(m_glyphs));
     }
 
-    const SimpleFontData* m_fontDataForAllGlyphs;
-    const SimpleFontData** m_perGlyphFontData;
+    // Separate arrays, rather than array of GlyphData, to save space.
+    Glyph m_glyphs[size];
+    const SimpleFontData* m_glyphFontData[size];
 
     GlyphPageTreeNode* m_owner;
-    Glyph m_glyphs[size];
 };
 
 } // namespace WebCore
 
-#endif // GlyphPage_h
+#endif // GlyphPageTreeNode_h
