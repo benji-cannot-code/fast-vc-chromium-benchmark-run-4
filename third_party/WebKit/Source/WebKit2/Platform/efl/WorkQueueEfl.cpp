635d95ae08f5243595f132cf652e4bab87a4fd2e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-06 18:54:06 +0000

    [EFL][WK2] Assertion failure on MiniBrowser exit https://bugs.webkit.org/show_bug.cgi?id=108932
    Patch by Sudarsana Nagineni <sudarsana.nagineni@intel.com> on 2013-02-06
    Reviewed by Anders Carlsson.
    
    WorkQueue is now refcounted after r141497, so increase ref
    count when a new job is scheduled and unref it when it finishes.
    
    * Platform/efl/WorkQueueEfl.cpp:
    (WorkQueue::performWork):
    (WorkQueue::performTimerWork):
    (WorkQueue::dispatch):
    (WorkQueue::dispatchAfterDelay):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142009 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Platform/efl/WorkQueueEfl.cpp
+++ third_party/WebKit/Source/WebKit2/Platform/efl/WorkQueueEfl.cpp
@@ -80,8 +80,10 @@ void WorkQueue::performWork()
             m_workItemQueue.swap(workItemQueue);
         }
 
-        for (size_t i = 0; i < workItemQueue.size(); ++i)
+        for (size_t i = 0; i < workItemQueue.size(); ++i) {
             workItemQueue[i]();
+            deref();
+        }
     }
 }
 
@@ -163,6 +165,7 @@ void WorkQueue::performTimerWork()
 
         // If a timer work item expired, dispatch the function of the work item.
         timerWorkItems[i]->dispatch();
+        deref();
     }
 }
 
@@ -211,6 +214,8 @@ void WorkQueue::unregisterSocketEventHandler(int fileDescriptor)
 
 void WorkQueue::dispatch(const Function<void()>& function)
 {
+    ref();
+
     {
         MutexLocker locker(m_workItemQueueLock);
         m_workItemQueue.append(function);
@@ -228,6 +233,7 @@ void WorkQueue::dispatchAfterDelay(const Function<void()>& function, double dela
     if (!timerWorkItem)
         return;
 
+    ref();
     insertTimerWorkItem(timerWorkItem.release());
     sendMessageToThread(wakupThreadMessage);
 }
