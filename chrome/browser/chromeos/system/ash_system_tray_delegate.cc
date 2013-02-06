e5ed63907b2e28baa049986dbb3fe89e54eb0f2b
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 17:08:47 +0000

    Cleanup network status code and strings
    This CL:
    * Cleans up the network status related strings
    * Cleans up some code in NetworkListDetailedView and removes some unused code
    
    BUG=171600
    For ash_strings.grd:
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/12087136
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180997 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/ash_system_tray_delegate.cc
+++ chrome/browser/chromeos/system/ash_system_tray_delegate.cc
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "device/bluetooth/bluetooth_adapter.h"
 #include "device/bluetooth/bluetooth_adapter_factory.h"
 #include "device/bluetooth/bluetooth_device.h"
+#include "grit/ash_strings.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
