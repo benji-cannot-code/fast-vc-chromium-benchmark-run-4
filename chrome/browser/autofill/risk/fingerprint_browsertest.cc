62bbb894a10ee19057df6d93056e6cdb2e73db7f
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 09:10:12 +0000

    [Autofill] Add logging to investigate why the Risk browser test is timing out.
    BUG=174296
    
    Review URL: https://codereview.chromium.org/12217032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180940 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/risk/fingerprint_browsertest.cc
+++ chrome/browser/autofill/risk/fingerprint_browsertest.cc
@@ -32,6 +32,9 @@ class AutofillRiskFingerprintTest : public InProcessBrowserTest {
         message_loop_(MessageLoop::TYPE_UI) {}
 
   void GetFingerprintTestCallback(scoped_ptr<Fingerprint> fingerprint) {
+    // TODO(isherman): Investigating http://crbug.com/174296
+    LOG(WARNING) << "Callback called.";
+
     // Verify that all fields Chrome can fill have been filled.
     ASSERT_TRUE(fingerprint->has_machine_characteristics());
     const Fingerprint_MachineCharacteristics& machine =
@@ -87,6 +90,8 @@ class AutofillRiskFingerprintTest : public InProcessBrowserTest {
               transient_state.outer_window_size().height());
     EXPECT_EQ(kGaiaId, fingerprint->metadata().gaia_id());
 
+    // TODO(isherman): Investigating http://crbug.com/174296
+    LOG(WARNING) << "Stopping the message loop.";
     message_loop_.Quit();
   }
 
@@ -103,12 +108,16 @@ IN_PROC_BROWSER_TEST_F(AutofillRiskFingerprintTest, GetFingerprint) {
   prefs.registry()->RegisterStringPref(prefs::kAcceptLanguages,
                                        kAcceptLanguages);
 
+  // TODO(isherman): Investigating http://crbug.com/174296
+  LOG(WARNING) << "Loading fingerprint.";
   GetFingerprint(
       kGaiaId, kWindowBounds, kContentBounds, prefs,
       base::Bind(&AutofillRiskFingerprintTest::GetFingerprintTestCallback,
                  base::Unretained(this)));
 
   // Wait for the callback to be called.
+  // TODO(isherman): Investigating http://crbug.com/174296
+  LOG(WARNING) << "Waiting for the callback to be called.";
   message_loop_.Run();
 }
 
