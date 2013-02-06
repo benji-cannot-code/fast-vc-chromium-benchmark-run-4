b95f2501241f825a3171db108bccbf2a35ebc526
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 08:40:38 +0000

    Add NULL check for GetSigninManager in app list signin delegate.
    BUG=174382
    
    
    Review URL: https://chromiumcodereview.appspot.com/12223020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/chrome_signin_delegate.cc
+++ chrome/browser/ui/app_list/chrome_signin_delegate.cc
@@ -49,6 +49,9 @@ bool ChromeSigninDelegate::NeedSignin()  {
   if (!profile_)
     return false;
 
+  if (!GetSigninManager(profile_))
+    return false;
+
   return GetSigninManager(profile_)->GetAuthenticatedUsername().empty();
 #endif
 }
