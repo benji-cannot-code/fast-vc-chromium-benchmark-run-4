05f81f5fd3703ab836cdb7787b46e21a4c7eda2b
egraether@chromium.org <egraether@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 23:10:08 +0000

    cc: Fix paint time collection for continuous painting mode
    This change moves the recordRenderingStats setting to LayerTreeDebugState and adds the new accessor LayerTreeDebugState::recordRenderingStats() that takes continuousPainting into account.
    
    BUG=174354
    
    Review URL: https://chromiumcodereview.appspot.com/12209018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181080 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host.cc
+++ cc/layer_tree_host.cc
@@ -407,7 +407,7 @@ void LayerTreeHost::didDeferCommit()
 
 void LayerTreeHost::renderingStats(RenderingStats* stats) const
 {
-    CHECK(m_settings.recordRenderingStats);
+    CHECK(m_debugState.recordRenderingStats());
     *stats = m_renderingStats;
     m_proxy->renderingStats(stats);
 }
@@ -698,7 +698,7 @@ bool LayerTreeHost::paintMasksForRenderSurface(Layer* renderSurfaceLayer, Resour
     // in code, we already know that at least something will be drawn into this render surface, so the
     // mask and replica should be painted.
 
-    RenderingStats* stats = m_settings.recordRenderingStats ? &m_renderingStats : NULL;
+    RenderingStats* stats = m_debugState.recordRenderingStats() ? &m_renderingStats : NULL;
 
     bool needMoreUpdates = false;
     Layer* maskLayer = renderSurfaceLayer->maskLayer();
@@ -727,7 +727,7 @@ bool LayerTreeHost::paintLayerContents(const LayerList& renderSurfaceLayerList, 
 
     prioritizeTextures(renderSurfaceLayerList, occlusionTracker.overdrawMetrics());
 
-    RenderingStats* stats = m_settings.recordRenderingStats ? &m_renderingStats : NULL;
+    RenderingStats* stats = m_debugState.recordRenderingStats() ? &m_renderingStats : NULL;
 
     LayerIteratorType end = LayerIteratorType::end(&renderSurfaceLayerList);
     for (LayerIteratorType it = LayerIteratorType::begin(&renderSurfaceLayerList); it != end; ++it) {
