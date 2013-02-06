60a7f66cc5f65805f69cc083215f118d654d6803
dsinclair@chromium.org <dsinclair@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 20:14:02 +0000

    Fixup GpuProcessHost TRACE_EVENT macros
    Fix a few GpuProcessHostUIShim TRACE_EVENT macros that
    should be GpuProcessHost.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12208030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181038 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -565,7 +565,7 @@ bool GpuProcessHost::OnMessageReceived(const IPC::Message& message) {
 }
 
 void GpuProcessHost::OnChannelConnected(int32 peer_pid) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::OnChannelConnected");
+  TRACE_EVENT0("gpu", "GpuProcessHost::OnChannelConnected");
 
   while (!queued_messages_.empty()) {
     Send(queued_messages_.front());
@@ -578,7 +578,7 @@ void GpuProcessHost::EstablishGpuChannel(
     bool share_context,
     const EstablishChannelCallback& callback) {
   DCHECK(CalledOnValidThread());
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::EstablishGpuChannel");
+  TRACE_EVENT0("gpu", "GpuProcessHost::EstablishGpuChannel");
 
   // If GPU features are already blacklisted, no need to establish the channel.
   if (!GpuDataManagerImpl::GetInstance()->GpuAccessAllowed()) {
@@ -599,7 +599,7 @@ void GpuProcessHost::CreateViewCommandBuffer(
     int client_id,
     const GPUCreateCommandBufferConfig& init_params,
     const CreateCommandBufferCallback& callback) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::CreateViewCommandBuffer");
+  TRACE_EVENT0("gpu", "GpuProcessHost::CreateViewCommandBuffer");
 
   DCHECK(CalledOnValidThread());
 
@@ -632,7 +632,7 @@ void GpuProcessHost::CreateImage(gfx::PluginWindowHandle window,
                                  int client_id,
                                  int image_id,
                                  const CreateImageCallback& callback) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::CreateImage");
+  TRACE_EVENT0("gpu", "GpuProcessHost::CreateImage");
 
   DCHECK(CalledOnValidThread());
 
@@ -646,7 +646,7 @@ void GpuProcessHost::CreateImage(gfx::PluginWindowHandle window,
 void GpuProcessHost::DeleteImage(int client_id,
                                  int image_id,
                                  int sync_point) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::DeleteImage");
+  TRACE_EVENT0("gpu", "GpuProcessHost::DeleteImage");
 
   DCHECK(CalledOnValidThread());
 
@@ -659,7 +659,7 @@ void GpuProcessHost::OnInitialized(bool result) {
 
 void GpuProcessHost::OnChannelEstablished(
     const IPC::ChannelHandle& channel_handle) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::OnChannelEstablished");
+  TRACE_EVENT0("gpu", "GpuProcessHost::OnChannelEstablished");
 
   EstablishChannelCallback callback = channel_requests_.front();
   channel_requests_.pop();
@@ -682,7 +682,7 @@ void GpuProcessHost::OnChannelEstablished(
 }
 
 void GpuProcessHost::OnCommandBufferCreated(const int32 route_id) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::OnCommandBufferCreated");
+  TRACE_EVENT0("gpu", "GpuProcessHost::OnCommandBufferCreated");
 
   if (create_command_buffer_requests_.empty())
     return;
@@ -694,7 +694,7 @@ void GpuProcessHost::OnCommandBufferCreated(const int32 route_id) {
 }
 
 void GpuProcessHost::OnDestroyCommandBuffer(int32 surface_id) {
-  TRACE_EVENT0("gpu", "GpuProcessHostUIShim::OnDestroyCommandBuffer");
+  TRACE_EVENT0("gpu", "GpuProcessHost::OnDestroyCommandBuffer");
 
 #if defined(TOOLKIT_GTK)
   SurfaceRefMap::iterator it = surface_refs_.find(surface_id);
