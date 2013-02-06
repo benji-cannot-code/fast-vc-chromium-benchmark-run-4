eaaa0d743219b7b9a3ba9762786a3833769e0ae2
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 16:29:27 +0000

    Allow macros in parametrized browser tests (IN_PROCESS_BROWSER_TEST_P).
    Adding one additional #define layer correctly expands the macro arguments. This
    allows the MAYBE_ prefix trick to conditionally disable tests on some platforms.
    
    Then, with this newly-acquired ability, fix encrypted_media_browsertest.cc to use the right style for disabling tests.
    
    BUG=174294
    
    Review URL: https://chromiumcodereview.appspot.com/12209016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180984 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/browser_test.h
+++ content/public/test/browser_test.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(HAS_OUT_OF_PROC_TEST_RUNNER)
 
 #include "base/compiler_specific.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 #define IN_PROC_BROWSER_TEST_(test_case_name, test_name, parent_class,\
                               parent_id)\
@@ -45,7 +46,7 @@ void GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::RunTestOnMainThread()
   IN_PROC_BROWSER_TEST_(test_fixture, test_name, test_fixture,\
                     ::testing::internal::GetTypeId<test_fixture>())
 
-#define IN_PROC_BROWSER_TEST_P(test_case_name, test_name) \
+#define IN_PROC_BROWSER_TEST_P_(test_case_name, test_name) \
   class GTEST_TEST_CLASS_NAME_(test_case_name, test_name) \
       : public test_case_name { \
    public: \
@@ -72,6 +73,11 @@ void GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::RunTestOnMainThread()
                              test_name)::gtest_registering_dummy_ = \
       GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::AddToRegistry(); \
   void GTEST_TEST_CLASS_NAME_(test_case_name, test_name)::RunTestOnMainThread()
+
+// Wrap the real macro with an outer macro to ensure that the parameters are
+// evaluated (e.g., if |test_name| is prefixed with MAYBE_).
+#define IN_PROC_BROWSER_TEST_P(test_case_name, test_name) \
+  IN_PROC_BROWSER_TEST_P_(test_case_name, test_name)
 
 #endif  // defined(HAS_OUT_OF_PROC_TEST_RUNNER)
 
