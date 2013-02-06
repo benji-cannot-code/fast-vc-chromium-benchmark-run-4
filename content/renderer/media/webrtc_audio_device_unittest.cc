b90768d46852f3a62dc5acda5eb5b14983a10f65
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 08:41:24 +0000

    Avoids irregular OnMoreData callbacks on Windows using Core Audio.
    Browser changes:
    
    - Improves how native audio buffer sizes are derived on Windows.
    - Forces user to always open up at native audio paramters.
    - Improved internal scheme to set up the actial endpoint buffer based on input size.
    - Refactored WSAPI output implementation and introduced CoreAudioUtil methods.
    - Harmonized WSAPI output implementation with exusting unified implementation (to prepare for future merge).
    - Changed GetAudioHardwareBufferSize() in audio_util.
    
    Render changes for WebRTC:
    
    - WebRTC now always asks for an output stream using native parameters to avoid rebuffering in the audio converter.
    - Any buffer-size mismatch is now taken care of in WebRtcAudioRendrer using a pull FIFO. Delay estimates are also compensated if FIFO is used.
    - Added DCHECKs to verify that methods are called on the expected threads.
    
    BUG=170498
    
    TEST=media_unittests, content_unittests, HTML5 audio tests in Chrome, WebAudio and Flash tests in Chrome, WebRTC tests in Chrome.
    
    Review URL: https://codereview.chromium.org/12049070
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@180936 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_device_unittest.cc
+++ content/renderer/media/webrtc_audio_device_unittest.cc
@@ -42,7 +42,7 @@ scoped_ptr<media::AudioHardwareConfig> CreateRealHardwareConfig() {
 }
 
 // Return true if at least one element in the array matches |value|.
-bool FindElementInArray(int* array, int size, int value) {
+bool FindElementInArray(const int* array, int size, int value) {
   return (std::find(&array[0], &array[0] + size, value) != &array[size]);
 }
 
@@ -57,7 +57,7 @@ bool HardwareSampleRatesAreValid() {
   // The actual WebRTC client can limit these ranges further depending on
   // platform but this is the maximum range we support today.
   int valid_input_rates[] = {16000, 32000, 44100, 48000, 96000};
-  int valid_output_rates[] = {44100, 48000, 96000};
+  int valid_output_rates[] = {16000, 32000, 44100, 48000, 96000};
 
   media::AudioHardwareConfig* hardware_config =
       RenderThreadImpl::current()->GetAudioHardwareConfig();
@@ -449,7 +449,7 @@ TEST_F(WebRTCAudioDeviceTest, DISABLED_PlayLocalFile) {
   // Play 2 seconds worth of audio and then quit.
   message_loop_.PostDelayedTask(FROM_HERE,
                                 MessageLoop::QuitClosure(),
-                                base::TimeDelta::FromSeconds(2));
+                                base::TimeDelta::FromSeconds(6));
   message_loop_.Run();
 
   renderer->Stop();
--- media/audio/win/audio_unified_win.cc
+++ media/audio/win/audio_unified_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/trace_event.h"
 #include "base/time.h"
 #include "base/win/scoped_com_initializer.h"
+#include "media/audio/audio_util.h"
 #include "media/audio/win/audio_manager_win.h"
 #include "media/audio/win/avrt_wrapper_win.h"
 #include "media/audio/win/core_audio_util_win.h"
@@ -77,6 +78,7 @@ WASAPIUnifiedStream::WASAPIUnifiedStream(AudioManagerWin* manager,
       share_mode_(CoreAudioUtil::GetShareMode()),
       audio_io_thread_(NULL),
       opened_(false),
+      volume_(1.0),
       endpoint_render_buffer_size_frames_(0),
       endpoint_capture_buffer_size_frames_(0),
       num_written_frames_(0),
@@ -247,12 +249,15 @@ void WASAPIUnifiedStream::Start(AudioSourceCallback* callback) {
     return;
   }
 
-  // Reset the counter for number of rendered frames taking into account the
-  // fact that we always initialize the render side with silence.
-  UINT32 num_queued_frames = 0;
-  audio_output_client_->GetCurrentPadding(&num_queued_frames);
-  DCHECK_EQ(num_queued_frames, endpoint_render_buffer_size_frames_);
-  num_written_frames_ = num_queued_frames;
+  // Ensure that the endpoint buffer is prepared with silence.
+  if (share_mode_ == AUDCLNT_SHAREMODE_SHARED) {
+    if (!CoreAudioUtil::FillRenderEndpointBufferWithSilence(
+             audio_output_client_, audio_render_client_)) {
+      DLOG(WARNING) << "Failed to prepare endpoint buffers with silence.";
+      return;
+    }
+  }
+  num_written_frames_ = endpoint_render_buffer_size_frames_;
 
   // Start output streaming data between the endpoint buffer and the audio
   // engine.
@@ -329,11 +334,15 @@ void WASAPIUnifiedStream::Close() {
 }
 
 void WASAPIUnifiedStream::SetVolume(double volume) {
-  NOTIMPLEMENTED();
+  DVLOG(1) << "SetVolume(volume=" << volume << ")";
+  if (volume < 0 || volume > 1)
+    return;
+  volume_ = volume;
 }
 
 void WASAPIUnifiedStream::GetVolume(double* volume) {
-  NOTIMPLEMENTED();
+  DVLOG(1) << "GetVolume()";
+  *volume = static_cast<double>(volume_);
 }
 
 // static
@@ -523,6 +532,13 @@ void WASAPIUnifiedStream::Run() {
           // |audio_data| as destination.
           render_bus_->ToInterleaved(
               packet_size_frames_, bytes_per_sample, audio_data);
+
+          // Perform in-place, software-volume adjustments.
+          media::AdjustVolume(audio_data,
+                              frames_filled * format_.Format.nBlockAlign,
+                              render_bus_->channels(),
+                              bytes_per_sample,
+                              volume_);
 
           // Release the buffer space acquired in the GetBuffer() call.
           audio_render_client_->ReleaseBuffer(packet_size_frames_, 0);
