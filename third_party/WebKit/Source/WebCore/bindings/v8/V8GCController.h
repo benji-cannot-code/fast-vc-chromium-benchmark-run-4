72641c6d342f9c07d29a9b28fea7be72ddbe88e2
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-06 15:50:56 +0000

    Unreviewed, rolling out r141983. http://trac.webkit.org/changeset/141983 https://bugs.webkit.org/show_bug.cgi?id=109055
    lots of new crashes in handlescope (Requested by gavinp on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-06
    
    * bindings/v8/DOMDataStore.h:
    (WebCore::DOMDataStore::setWrapperInObject):
    * bindings/v8/DOMWrapperWorld.h:
    (DOMWrapperWorld):
    (WebCore::DOMWrapperWorld::isolated):
    * bindings/v8/V8Binding.h:
    (WebCore):
    (WebCore::worldForEnteredContextIfIsolated):
    * bindings/v8/V8DOMWindowShell.cpp:
    (WebCore::V8DOMWindowShell::initializeIfNeeded):
    * bindings/v8/V8GCController.cpp:
    (WebCore::WrapperVisitor::WrapperVisitor):
    (WebCore):
    (WebCore::gcTree):
    (WebCore::V8GCController::didCreateWrapperForNode):
    (WebCore::V8GCController::gcPrologue):
    (WebCore::V8GCController::minorGCPrologue):
    (WebCore::V8GCController::majorGCPrologue):
    * bindings/v8/V8GCController.h:
    (V8GCController):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@141999 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8GCController.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8GCController.h
@@ -53,6 +53,7 @@ public:
     static void collectGarbage();
 
     static Node* opaqueRootForGC(Node*, v8::Isolate*);
+    static void didCreateWrapperForNode(Node*);
 
 private:
     static Vector<Node*>* m_edenNodes;
