5ab71cae1e879225299bf45f397478c97b219b99
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-06 23:15:34 +0000

    Add SpdyFramerDebugVisitorInterface for later usage in stats collecting and more!
    Lands server change 32048876.
    
    Also fixes test expectations when USE_SYSTEM_ZLIB is not set.
    
    Also add compiler_specific.h include for OVERRIDE keywords.
    
    Review URL: https://codereview.chromium.org/12224019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181087 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_framer.cc
+++ net/spdy/spdy_framer.cc
@@ -124,6 +124,7 @@ SpdyFramer::SpdyFramer(int version)
       current_frame_len_(0),
       enable_compression_(true),
       visitor_(NULL),
+      debug_visitor_(NULL),
       display_protocol_("SPDY"),
       spdy_version_(version),
       syn_frame_processed_(false),
@@ -1676,6 +1677,10 @@ SpdyControlFrame* SpdyFramer::CompressControlFrame(
 
   if (visitor_)
     visitor_->OnControlFrameCompressed(frame, *new_frame);
+
+  if (debug_visitor_ != NULL) {
+    debug_visitor_->OnCompressedHeaderBlock(payload_length, compressed_size);
+  }
 
   return new_frame.release();
 }
--- net/spdy/spdy_framer_test.cc
+++ net/spdy/spdy_framer_test.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <iostream>
 #include <limits>
 
+#include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
 #include "net/spdy/spdy_framer.h"
 #include "net/spdy/spdy_protocol.h"
@@ -308,7 +309,8 @@ void CompareCharArraysWithHexError(
       << HexDumpWithMarks(actual, actual_len, marks.get(), max_len);
 }
 
-class TestSpdyVisitor : public SpdyFramerVisitorInterface  {
+class TestSpdyVisitor : public SpdyFramerVisitorInterface,
+                        public SpdyFramerDebugVisitorInterface {
  public:
   static const size_t kDefaultHeaderBufferSize = 16 * 1024;
   static const size_t kDefaultCredentialBufferSize = 16 * 1024;
@@ -330,6 +332,8 @@ class TestSpdyVisitor : public SpdyFramerVisitorInterface  {
       control_frame_header_data_count_(0),
       zero_length_control_frame_header_data_count_(0),
       data_frame_count_(0),
+      last_decompressed_size_(0),
+      last_compressed_size_(0),
       header_buffer_(new char[kDefaultHeaderBufferSize]),
       header_buffer_length_(0),
       header_buffer_size_(kDefaultHeaderBufferSize),
@@ -475,6 +479,12 @@ class TestSpdyVisitor : public SpdyFramerVisitorInterface  {
     return true;
   }
 
+  virtual void OnCompressedHeaderBlock(size_t decompressed_size,
+                                       size_t compressed_size) OVERRIDE {
+    last_decompressed_size_ = decompressed_size;
+    last_compressed_size_ = compressed_size;
+  }
+
   // Convenience function which runs a framer simulation with particular input.
   void SimulateInFramer(const unsigned char* input, size_t size) {
     framer_.set_enable_compression(use_compression_);
@@ -542,6 +552,8 @@ class TestSpdyVisitor : public SpdyFramerVisitorInterface  {
   // The count of zero-length control frame header data chunks received.
   int zero_length_control_frame_header_data_count_;
   int data_frame_count_;
+  size_t last_decompressed_size_;
+  size_t last_compressed_size_;
 
   // Header block streaming state:
   scoped_array<char> header_buffer_;
@@ -903,7 +915,9 @@ TEST_P(SpdyFramerTest, BasicCompression) {
   headers["content-type"] = "text/html";
   headers["content-length"] = "12";
 
+  TestSpdyVisitor visitor(spdy_version_);
   SpdyFramer framer(spdy_version_);
+  framer.set_debug_visitor(&visitor);
   framer.set_enable_compression(true);
   scoped_ptr<SpdySynStreamControlFrame> frame1(
       framer.CreateSynStream(1,  // stream id
@@ -913,6 +927,21 @@ TEST_P(SpdyFramerTest, BasicCompression) {
                              CONTROL_FLAG_NONE,
                              true,  // compress
                              &headers));
+  if (IsSpdy2()) {
+    EXPECT_EQ(139u, visitor.last_decompressed_size_);
+#if defined(USE_SYSTEM_ZLIB)
+    EXPECT_EQ(93u, visitor.last_compressed_size_);
+#else  // !defined(USE_SYSTEM_ZLIB)
+    EXPECT_EQ(135u, visitor.last_compressed_size_);
+#endif  // !defined(USE_SYSTEM_ZLIB)
+  } else {
+    EXPECT_EQ(165u, visitor.last_decompressed_size_);
+#if defined(USE_SYSTEM_ZLIB)
+    EXPECT_EQ(72u, visitor.last_compressed_size_);
+#else  // !defined(USE_SYSTEM_ZLIB)
+    EXPECT_EQ(117u, visitor.last_compressed_size_);
+#endif  // !defined(USE_SYSTEM_ZLIB)
+  }
   scoped_ptr<SpdySynStreamControlFrame> frame2(
       framer.CreateSynStream(1,  // stream id
                              0,  // associated stream id
