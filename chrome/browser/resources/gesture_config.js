c9b66a1e386ed64b1107b6b37955a133e8281714
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 23:31:57 +0000

    Add a different title for each section of configurable properties in chrome://gesture
    BUG=174123
    Test=Manual
    1) Go to chrome://gesture
    2) observe the new title for the overscroll properties section
    
    Review URL: https://chromiumcodereview.appspot.com/12220031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/gesture_config.js
+++ chrome/browser/resources/gesture_config.js
@@ -11,11 +11,14 @@ var $ = function(id) { return document.getElementById(id); };
 /**
  * A generic WebUI for configuring preference values used by Chrome's gesture
  * recognition systems.
+ * @param {string} title The user-visible title to display for the configuration
+ *    section.
  * @param {string} prefix The prefix for the configuration fields.
- * @param {object} fields An array of fields that contain the name of the pref
+ * @param {!Object} fields An array of fields that contain the name of the pref
  *    and user-visible labels.
  */
-function GeneralConfig(prefix, fields) {
+function GeneralConfig(title, prefix, fields) {
+  this.title = title;
   this.prefix = prefix;
   this.fields = fields;
 }
@@ -36,12 +39,19 @@ GeneralConfig.prototype = {
   buildForm: function() {
     var buf = [];
 
+    var section = $('section-template').cloneNode(true);
+    section.removeAttribute('id');
+    var title = section.querySelector('.section-title');
+    title.textContent = this.title;
+
     for (var i = 0; i < this.fields.length; i++) {
       var field = this.fields[i];
 
-      var row = $('gesture-form-row').cloneNode(true);
+      var row = $('section-row-template').cloneNode(true);
+      row.removeAttribute('id');
+
       var label = row.querySelector('.row-label');
-      var input = row.querySelector('.row-input');
+      var input = row.querySelector('.input');
       var units = row.querySelector('.row-units');
 
       label.setAttribute('for', field.key);
@@ -52,10 +62,12 @@ GeneralConfig.prototype = {
       if (field.max) input.max = field.max;
       if (field.step) input.step = field.step;
 
-      $('gesture-form').appendChild(row);
       if (field.units)
         units.innerHTML = field.units;
+
+      section.querySelector('.section-properties').appendChild(row);
     }
+    $('gesture-form').appendChild(section);
   },
 
   /**
@@ -127,6 +139,9 @@ GeneralConfig.prototype = {
  * @return {object} A GeneralConfig object.
  */
 function GestureConfig() {
+  /** The title of the section for the gesture preferences. **/
+  /** @const */ var GESTURE_TITLE = 'Gesture Properties';
+
   /** Common prefix of gesture preferences. **/
   /** @const */ var GESTURE_PREFIX = 'gesture.';
 
@@ -263,7 +278,7 @@ function GestureConfig() {
     }
   ];
 
-  return new GeneralConfig(GESTURE_PREFIX, GESTURE_FIELDS);
+  return new GeneralConfig(GESTURE_TITLE, GESTURE_PREFIX, GESTURE_FIELDS);
 }
 
 /**
@@ -271,7 +286,9 @@ function GestureConfig() {
  * @return {object} A GeneralConfig object.
  */
 function OverscrollConfig() {
-  var OVERSCROLL_PREFIX = 'overscroll.';
+  /** @const */ var OVERSCROLL_TITLE = 'Overscroll Properties';
+
+  /** @const */ var OVERSCROLL_PREFIX = 'overscroll.';
 
   var OVERSCROLL_FIELDS = [
     {
@@ -301,7 +318,9 @@ function OverscrollConfig() {
     },
   ];
 
-  return new GeneralConfig(OVERSCROLL_PREFIX, OVERSCROLL_FIELDS);
+  return new GeneralConfig(OVERSCROLL_TITLE,
+                           OVERSCROLL_PREFIX,
+                           OVERSCROLL_FIELDS);
 }
 
 /**
