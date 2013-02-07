4773ebe42518a5678b585b0f3b2d308494f0cc2f
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 07:39:42 +0000

    chromeos: Change DriveCache::MarkAsMounted to take a resource-id/md5 pair as arguments
    MarkAsMounted and MarkAsUnmounted are the only methods which take FilePath as their arguments to perform cache operations.
    This is not quite right.
    
    BUG=160487
    TEST=unit_tests --gtest_filter="DriveCache*", try mounting zip files from both Downloads and Google Drive directories
    
    
    Review URL: https://chromiumcodereview.appspot.com/12208043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181249 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_cache.h
+++ chrome/browser/chromeos/drive/drive_cache.h
@@ -191,9 +191,11 @@ class DriveCache {
              const std::string& md5,
              const FileOperationCallback& callback);
 
-  // Set the state of the cache entry corresponding to file_path as mounted.
+  // Sets the state of the cache entry corresponding to |resource_id| and |md5|
+  // as mounted.
   // |callback| must not be null.
-  void MarkAsMounted(const FilePath& file_path,
+  void MarkAsMounted(const std::string& resource_id,
+                     const std::string& md5,
                      const GetFileFromCacheCallback& callback);
 
   // Set the state of the cache entry corresponding to file_path as unmounted.
@@ -327,7 +329,8 @@ class DriveCache {
 
   // Used to implement MarkAsMounted.
   scoped_ptr<GetFileResult> MarkAsMountedOnBlockingPool(
-      const FilePath& file_path);
+      const std::string& resource_id,
+      const std::string& md5);
 
   // Used to implement MarkAsUnmounted.
   DriveFileError MarkAsUnmountedOnBlockingPool(const FilePath& file_path);
