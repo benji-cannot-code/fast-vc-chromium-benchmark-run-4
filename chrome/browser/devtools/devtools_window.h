benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 112fa195d795fc29438671dd085932f167d48878

Alternate NTP: Don't hide bookmark bar on instant
Currently when typing in the omnibox the instant search results are shown next to omnibox by hiding the bookmark bar.

This causes the web contents to jump up by 26 pixels.

This CL works around this by changing the zorder such that the bookmark bar is under the web contents. With this approach there's no need to hide the bookmark bar and the web contents doesn't jump when typing.

Screen recording:
New: https://docs.google.com/file/d/0B0Odde3V7EhWcnBBRVBxUXdnN2c/edit
New Infobar: https://docs.google.com/file/d/0B0Odde3V7EhWOG4wVWZ6WGp0aDQ/edit
Old: https://docs.google.com/file/d/0B0Odde3V7EhWOFYzeTlVbzRXX2s/edit

Sample build:
  https://docs.google.com/file/d/0B0Odde3V7EhWLXdnYVpHa0xEcFk/edit

BUG=170835
TBR=thakis@chromium.org

Review URL: https://codereview.chromium.org/11876036

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@178367 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/browser_window_controller.h
@@ -114,10 +114,6 @@ class WebContents;
   // nil for those which don't).
   scoped_nsobject<NSView> floatingBarBackingView_;
 
-  // Tracks whether the floating bar is above or below the bookmark bar, in
-  // terms of z-order.
-  BOOL floatingBarAboveBookmarkBar_;
-
   // The borderless window used in fullscreen mode.  Lion reuses the original
   // window in fullscreen mode, so this is always nil on Lion.
   scoped_nsobject<NSWindow> fullscreenWindow_;
@@ -163,6 +159,10 @@ class WebContents;
   // The Extension Command Registry used to determine which keyboard events to
   // handle.
   scoped_ptr<ExtensionKeybindingRegistryCocoa> extension_keybinding_registry_;
+
+  // The offset between the bottom of the toolbar and web contents. This is used
+  // to push the web contents below the bookmark bar.
+  CGFloat toolbarToWebContentsOffset_;
 }
 
 // A convenience class method which gets the |BrowserWindowController| for a
@@ -202,6 +202,9 @@ class WebContents;
 // Return a weak pointer to the tab strip controller.
 - (TabStripController*)tabStripController;
 
+// Return a weak pointer to the find bar controller.
+- (FindBarCocoaController*)findBarCocoaController;
+
 // Access the ObjC controller that contains the infobars.
 - (InfoBarContainerController*)infoBarContainerController;
 
@@ -210,6 +213,12 @@ class WebContents;
 
 // Access the C++ bridge object representing the location bar.
 - (LocationBarViewMac*)locationBarBridge;
+
+// Returns a weak pointer to the floating bar backing view;
+- (NSView*)floatingBarBackingView;
+
+// Returns a weak pointer to the previewable contents controller.
+- (PreviewableContentsController*)previewableContentsController;
 
 // Access the Profile object that backs this Browser.
 - (Profile*)profile;
@@ -330,6 +339,8 @@ class WebContents;
 - (void)sheetDidEnd:(NSWindow*)sheet
          returnCode:(NSInteger)code
             context:(void*)context;
+
+- (void)updateBookmarkBarStateForInstantPreview;
 
 @end  // @interface BrowserWindowController
 

chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -68,6 +68,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/toolbar/toolbar_controller.h"
 #include "chrome/browser/ui/fullscreen/fullscreen_controller.h"
 #include "chrome/browser/ui/omnibox/location_bar.h"
+#include "chrome/browser/ui/search/search.h"
+#include "chrome/browser/ui/search/search_model.h"
 #include "chrome/browser/ui/tabs/dock_info.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_delegate.h"
@@ -393,7 +395,6 @@ enum {
              resizeDelegate:self]);
     [toolbarController_ setHasToolbar:[self hasToolbar]
                        hasLocationBar:[self hasLocationBar]];
-    [[[self window] contentView] addSubview:[toolbarController_ view]];
 
     // Create a sub-controller for the bookmark bar.
     bookmarkBarController_.reset(
@@ -402,21 +403,12 @@ enum {
                initialWidth:NSWidth([[[self window] contentView] frame])
                    delegate:self
              resizeDelegate:self]);
-
-    // Add bookmark bar to the view hierarchy, which also triggers the nib load.
-    // The bookmark bar is defined (in the nib) to be bottom-aligned to its
-    // parent view (among other things), so position and resize properties don't
-    // need to be set.
-    [[[self window] contentView] addSubview:[bookmarkBarController_ view]
-                                 positioned:NSWindowBelow
-                                 relativeTo:[toolbarController_ view]];
     [bookmarkBarController_ setBookmarkBarEnabled:[self supportsBookmarkBar]];
 
     // Create the infobar container view, so we can pass it to the
     // ToolbarController.
     infoBarContainerController_.reset(
         [[InfoBarContainerController alloc] initWithResizeDelegate:self]);
-    [[[self window] contentView] addSubview:[infoBarContainerController_ view]];
 
     // We don't want to try and show the bar before it gets placed in its parent
     // view, so this step shoudn't be inside the bookmark bar controller's
@@ -539,6 +531,10 @@ enum {
   return tabStripController_.get();
 }
 
+- (FindBarCocoaController*)findBarCocoaController {
+  return findBarCocoaController_.get();
+}
+
 - (InfoBarContainerController*)infoBarContainerController {
   return infoBarContainerController_.get();
 }
@@ -549,6 +545,14 @@ enum {
 
 - (LocationBarViewMac*)locationBarBridge {
   return [toolbarController_ locationBarBridge];
+}
+
+- (NSView*)floatingBarBackingView {
+  return floatingBarBackingView_;
+}
+
+- (PreviewableContentsController*)previewableContentsController {
+  return previewableContentsController_;
 }
 
 - (Profile*)profile {
@@ -1529,13 +1533,7 @@ enum {
 
   // Create a controller for the findbar.
   findBarCocoaController_.reset([findBarCocoaController retain]);
-  NSView* contentView = [[self window] contentView];
-  NSView* relativeView =
-      [self inPresentationMode] ? [toolbarController_ view] :
-                                  [infoBarContainerController_ view];
-  [contentView addSubview:[findBarCocoaController_ view]
-               positioned:NSWindowAbove
-               relativeTo:relativeView];
+  [self updateSubviewZOrder];
 
   // Place the find bar immediately below the toolbar/attached bookmark bar. In
   // presentation mode, it hangs off the top of the screen when the bar is
@@ -1543,7 +1541,7 @@ enum {
   CGFloat maxY = [self placeBookmarkBarBelowInfoBar] ?
       NSMinY([[toolbarController_ view] frame]) :
       NSMinY([[bookmarkBarController_ view] frame]);
-  CGFloat maxWidth = NSWidth([contentView frame]);
+  CGFloat maxWidth = NSWidth([[[self window] contentView] frame]);
   [findBarCocoaController_ positionFindBarViewAtMaxY:maxY maxWidth:maxWidth];
 
   // This allows the FindBarCocoaController to call |layoutSubviews| and get
@@ -1910,8 +1908,7 @@ enum {
 - (void)bookmarkBar:(BookmarkBarController*)controller
  didChangeFromState:(BookmarkBar::State)oldState
             toState:(BookmarkBar::State)newState {
-  [toolbarController_
-      setDividerOpacity:[bookmarkBarController_ toolbarDividerOpacity]];
+  [toolbarController_ setDividerOpacity:[self toolbarDividerOpacity]];
   [self adjustToolbarAndBookmarkBarForCompression:
           [controller getDesiredToolbarHeightCompression]];
 }
@@ -1920,8 +1917,7 @@ enum {
 - (void)bookmarkBar:(BookmarkBarController*)controller
 willAnimateFromState:(BookmarkBar::State)oldState
             toState:(BookmarkBar::State)newState {
-  [toolbarController_
-      setDividerOpacity:[bookmarkBarController_ toolbarDividerOpacity]];
+  [toolbarController_ setDividerOpacity:[self toolbarDividerOpacity]];
   [self adjustToolbarAndBookmarkBarForCompression:
           [controller getDesiredToolbarHeightCompression]];
 }
@@ -1990,6 +1986,12 @@ willAnimateFromState:(BookmarkBar::State)oldState
          returnCode:(NSInteger)code
             context:(void*)context {
   [sheet orderOut:self];
+}
+
+- (void)updateBookmarkBarStateForInstantPreview {
+  [toolbarController_ setDividerOpacity:[self toolbarDividerOpacity]];
+  [self updateContentOffsets];
+  [self updateSubviewZOrder];
 }
 
 @end  // @implementation BrowserWindowController

chrome/chrome_tests.gypi
@@ -1221,7 +1221,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/cocoa/applescript/window_applescript_test.mm',
         'browser/ui/cocoa/browser_window_cocoa_browsertest.mm',
         'browser/ui/cocoa/browser_window_controller_browsertest.mm',
-        'browser/ui/cocoa/bookmarks/bookmark_bar_controller_browsertest.mm',
         'browser/ui/cocoa/constrained_window/constrained_window_mac_browsertest.mm',
         'browser/ui/cocoa/content_settings/collected_cookies_mac_browsertest.mm',
         'browser/ui/cocoa/content_settings/content_setting_bubble_cocoa_unittest.mm',

chrome/browser/ui/browser.cc
@@ -1253,7 +1253,7 @@ void Browser::MaybeUpdateBookmarkBarStateForInstantPreview(
   // bookmark bar.
   if (mode.is_search_suggestions() &&
       bookmark_bar_state_ == BookmarkBar::SHOW) {
-    UpdateBookmarkBarState(BOOKMARK_BAR_STATE_CHANGE_INSTANT_PREVIEW_STATE);
+    UpdateBookmarkBarState(BOOKMARK_BAR_STATE_CHANGE_TAB_STATE);
   }
 }
 
@@ -2230,8 +2230,10 @@ void Browser::UpdateBookmarkBarState(BookmarkBarStateChangeReason reason) {
   }
 
   // Don't allow the bookmark bar to be shown in suggestions mode.
+#if !defined(OS_MACOSX)
   if (search_model_->mode().is_search_suggestions())
     state = BookmarkBar::HIDDEN;
+#endif
 
   if (state == bookmark_bar_state_)
     return;
@@ -2248,14 +2250,10 @@ void Browser::UpdateBookmarkBarState(BookmarkBarStateChangeReason reason) {
     return;
   }
 
-  // Don't animate if mode is |NTP| because the bookmark is attached at top when
-  // pref is on and detached at bottom when off.
-  BookmarkBar::AnimateChangeType animate_type =
-      (reason == BOOKMARK_BAR_STATE_CHANGE_PREF_CHANGE ||
-       reason == BOOKMARK_BAR_STATE_CHANGE_INSTANT_PREVIEW_STATE) ?
+  bool shouldAnimate = reason == BOOKMARK_BAR_STATE_CHANGE_PREF_CHANGE;
+  window_->BookmarkBarStateChanged(shouldAnimate ?
       BookmarkBar::ANIMATE_STATE_CHANGE :
-      BookmarkBar::DONT_ANIMATE_STATE_CHANGE;
-  window_->BookmarkBarStateChanged(animate_type);
+      BookmarkBar::DONT_ANIMATE_STATE_CHANGE);
 }
 
 bool Browser::ShouldHideUIForFullscreen() const {

chrome/browser/ui/browser.h
@@ -526,9 +526,6 @@ class Browser : public TabStripModelObserver,
 
     // Change is the result of window toggling in/out of fullscreen mode.
     BOOKMARK_BAR_STATE_CHANGE_TOGGLE_FULLSCREEN,
-
-    // Change is the result of a state change in the instant preview.
-    BOOKMARK_BAR_STATE_CHANGE_INSTANT_PREVIEW_STATE,
   };
 
   // Overridden from content::WebContentsDelegate:

chrome/browser/ui/cocoa/background_gradient_view.mm
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (void)setShowsDivider:(BOOL)show {
+  if (showsDivider_ == show)
+    return;
   showsDivider_ = show;
   [self setNeedsDisplay:YES];
 }

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.mm
@@ -258,10 +258,7 @@ void RecordAppLaunch(Profile* profile, GURL url) {
         rb.GetNativeImageNamed(IDR_DEFAULT_FAVICON).CopyNSImage());
 
     innerContentAnimationsEnabled_ = YES;
-    // Disable state animations (for example, showing or hiding the bookmark
-    // bar) if the detached bookmark bar will be shown at the bottom of the new
-    // tab page.
-    stateAnimationsEnabled_ = ![self shouldShowAtBottomWhenDetached];
+    stateAnimationsEnabled_ = YES;
 
     // Register for theme changes, bookmark button pulsing, ...
     NSNotificationCenter* defaultCenter = [NSNotificationCenter defaultCenter];
@@ -2360,7 +2357,7 @@ static BOOL ValueInRangeInclusive(CGFloat low, CGFloat value, CGFloat high) {
 }
 
 - (BOOL)shouldShowAtBottomWhenDetached {
-  return chrome::search::IsInstantExtendedAPIEnabled(browser_->profile());
+  return NO;
 }
 
 #pragma mark BookmarkButtonDelegate Protocol

chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller_browsertest.mm
@@ -1,144 +1,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-#import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_controller.h"
-
-#include "base/utf_string_conversions.h"
-#include "chrome/browser/bookmarks/bookmark_model.h"
-#include "chrome/browser/bookmarks/bookmark_model_factory.h"
-#include "chrome/browser/ui/browser.h"
-#include "chrome/browser/ui/browser_window.h"
-#import "chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.h"
-#import "chrome/browser/ui/cocoa/browser_window_controller.h"
-#import "chrome/browser/ui/cocoa/download/download_shelf_controller.h"
-#include "chrome/browser/ui/search/search.h"
-#include "chrome/browser/ui/search/search_ui.h"
-#include "chrome/common/url_constants.h"
-#include "chrome/test/base/in_process_browser_test.h"
-#include "content/public/test/test_utils.h"
-#include "googleurl/src/gurl.h"
-
-class BottomBookmarkBarControllerTest : public InProcessBrowserTest {
- public:
-  BottomBookmarkBarControllerTest() {
-    chrome::search::EnableInstantExtendedAPIForTesting();
-  }
-
-  virtual void SetUpOnMainThread() OVERRIDE {
-    AddTabAtIndex(1,
-                  GURL(chrome::kChromeUINewTabURL),
-                  content::PAGE_TRANSITION_LINK);
-    EXPECT_TRUE([GetBookmarkBarController() shouldShowAtBottomWhenDetached]);
-    EXPECT_FALSE([[GetBookmarkBarController() view] isHidden]);
-  }
-
-  BrowserWindowController* GetBrowserWindowController() const {
-    NSWindow* window = browser()->window()->GetNativeWindow();
-    return [BrowserWindowController browserWindowControllerForWindow:window];
-  }
-
-  BookmarkBarController* GetBookmarkBarController() const {
-    return [GetBrowserWindowController() bookmarkBarController];
-  }
-
- private:
-  DISALLOW_COPY_AND_ASSIGN(BottomBookmarkBarControllerTest);
-};
-
-// Verify that the bookmark bar is positioned correctly when detached and
-// attached.
-IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, Position) {
-  // Verify detached bookmark bar is at the bottom of content area.
-  NSRect bookmark_bar_frame = [[GetBookmarkBarController() view] frame];
-  EXPECT_EQ(0, NSMinY(bookmark_bar_frame));
-
-  // Attach the bookmark bar and verify it's above the content area.
-  browser()->window()->ToggleBookmarkBar();
-  content::RunAllPendingInMessageLoop();
-  NSRect content_frame = [[GetBrowserWindowController() tabContentArea] frame];
-  bookmark_bar_frame = [[GetBookmarkBarController() view] frame];
-  EXPECT_EQ(NSMinX(content_frame), NSMinX(bookmark_bar_frame));
-  EXPECT_EQ(NSMaxY(content_frame), NSMinY(bookmark_bar_frame));
-}
-
-// Test that detached bookmark bar is positioned correctly when the download
-// shelf is visible.
-IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, PositionWithDownloads) {
-  // Show the download shelf.
-  DownloadShelfController* download_shelf_controller =
-      [GetBrowserWindowController() downloadShelf];
-  [download_shelf_controller show:nil];
-  EXPECT_FALSE([[download_shelf_controller view] isHidden]);
-
-  // Verify that the bookmark bar is at the bottom of the content area but above
-  // the download shelf.
-  NSRect content_frame = [[GetBrowserWindowController() tabContentArea] frame];
-  NSRect bookmark_bar_frame = [[GetBookmarkBarController() view] frame];
-  EXPECT_EQ(NSMinY(content_frame), NSMinY(bookmark_bar_frame));
-  EXPECT_NE(0, NSMinY(bookmark_bar_frame));
-  EXPECT_EQ(NSMinY(content_frame), NSMinY(bookmark_bar_frame));
-}
-
-// Verify that the bookmark bar shrinks when the window is narrow.
-IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, NarrowWindow) {
-  NSWindow* window = browser()->window()->GetNativeWindow();
-  NSRect window_frame = [window frame];
-  CGFloat expected_width =
-      roundf(chrome::search::kMaxWidthForBottomBookmarkBar / 2.0);
-  window_frame.size.width = expected_width +
-      chrome::search::kHorizontalPaddingForBottomBookmarkBar * 2;
-  [window setFrame:window_frame display:YES];
-  EXPECT_FALSE([[GetBookmarkBarController() view] isHidden]);
-
-  NSRect frame = [[GetBookmarkBarController() view] frame];
-  EXPECT_EQ(chrome::search::kHorizontalPaddingForBottomBookmarkBar,
-            NSMinX(frame));
-  EXPECT_EQ(expected_width, NSWidth(frame));
-}
-
-// Verify that the bookmark bar's width doesn't go above its maximum value.
-IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, WideWindow) {
-  NSWindow* window = browser()->window()->GetNativeWindow();
-  NSRect window_frame = [window frame];
-  window_frame.size.width = chrome::search::kMaxWidthForBottomBookmarkBar +
-      chrome::search::kHorizontalPaddingForBottomBookmarkBar * 2 + 100;
-  [window setFrame:window_frame display:YES];
-  EXPECT_FALSE([[GetBookmarkBarController() view] isHidden]);
-
-  NSRect frame = [[GetBookmarkBarController() view] frame];
-  EXPECT_EQ(roundf((NSWidth(window_frame) - NSWidth(frame)) / 2.0),
-            NSMinX(frame));
-  EXPECT_EQ(chrome::search::kMaxWidthForBottomBookmarkBar, NSWidth(frame));
-}
-
-// Verify that the bookmark bar is hidden if the content area height falls
-// below a minimum value.
-IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, MinContentHeight) {
-  NSWindow* window = browser()->window()->GetNativeWindow();
-  NSRect window_frame = [window frame];
-  window_frame.size.height =
-      chrome::search::kMinContentHeightForBottomBookmarkBar;
-  [window setFrame:window_frame display:YES];
-  EXPECT_TRUE([[GetBookmarkBarController() view] isHidden]);
-}
-
-IN_PROC_BROWSER_TEST_F(BottomBookmarkBarControllerTest, NoItemContainer) {
-  EXPECT_TRUE([GetBookmarkBarController() isEmpty]);
-  BookmarkBarView* button_view = [GetBookmarkBarController() buttonView];
-  EXPECT_TRUE([[button_view noItemContainer] isHidden]);
-
-  // Add a bookmark.
-  BookmarkModel* model =
-      BookmarkModelFactory::GetForProfile(browser()->profile());
-  const BookmarkNode* node = model->bookmark_bar_node();
-  model->AddURL(node,
-                node->child_count(),
-                ASCIIToUTF16("title"),
-                GURL("http://www.google.com"));
-
-  // Item container should remain hidden.
-  EXPECT_FALSE([GetBookmarkBarController() isEmpty]);
-  EXPECT_TRUE([[button_view noItemContainer] isHidden]);
-}

commit 01968dcaa6eec5477b68e0ee3d2b22d46965e886

Alternate NTP: Don't overlap dev tools with bookmark bar
When docked vertically the dev tools view overlaps the bookmark bar.

This was a bug I introduced in r178367 when I changed the bookmark bar to overlap web contents.

To fix this I've made the following changes:
  - implement constrainSplitPosition: to prevent the splitter from being dragged past the bookmark bar
  - move the dev tools view into a container view. The container view resizes the dev tools view to be below the bookmark bar

BUG=173205
TEST=Open dev tools and verified that it never went under the bookmark bar.


Review URL: https://chromiumcodereview.appspot.com/12208060

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181301 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/devtools/devtools_window.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 class BrowserWindow;
+class DevToolsControllerTest;
 class PrefServiceSyncable;
 class Profile;
 
@@ -112,6 +113,7 @@ class DevToolsWindow : private content::NotificationObserver,
   void SetHeight(int height);
 
  private:
+  friend class DevToolsControllerTest;
   static DevToolsWindow* Create(Profile* profile,
                                 content::RenderViewHost* inspected_rvh,
                                 DevToolsDockSide dock_side,

chrome/browser/ui/cocoa/browser_window_controller.h
@@ -275,6 +275,8 @@ class WebContents;
 
 - (BookmarkBarController*)bookmarkBarController;
 
+- (DevToolsController*)devToolsController;
+
 - (BOOL)isDownloadShelfVisible;
 
 // Lazily creates the download shelf in visible state if it doesn't exist yet.

chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -1479,6 +1479,10 @@ enum {
   return bookmarkBarController_;
 }
 
+- (DevToolsController*)devToolsController {
+  return devToolsController_;
+}
+
 - (BOOL)isDownloadShelfVisible {
   return downloadShelfController_ != nil &&
       [downloadShelfController_ isVisible];

chrome/browser/ui/cocoa/dev_tools_controller.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <Cocoa/Cocoa.h>
 
+#import "base/mac/foundation_util.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/prefs/pref_service.h"
 #include "chrome/browser/profiles/profile.h"
@@ -37,11 +38,40 @@ using content::WebContents;
   return [NSColor darkGrayColor];
 }
 
+- (void)drawDividerInRect:(NSRect)aRect {
+  NSRect dividerRect = aRect;
+  if ([self isVertical]) {
+    dividerRect.size.height -= topContentOffset_;
+    dividerRect.origin.y += topContentOffset_;
+  }
+  [super drawDividerInRect:dividerRect];
+}
+
 - (NSView*)hitTest:(NSPoint)point {
   NSPoint viewPoint = [self convertPoint:point fromView:[self superview]];
   if (viewPoint.y < topContentOffset_)
     return nil;
   return [super hitTest:point];
+}
+
+@end
+
+// Superview for the dev tools contents view. This class ensures that dev tools
+// view doesn't overlap the toolbar when split vertically.
+@interface DevToolsContainerView : NSView
+@end
+
+@implementation DevToolsContainerView
+
+- (void)resizeSubviewsWithOldSize:(NSSize)oldBoundsSize {
+  NSRect subviewFrame = [self bounds];
+  GraySplitView* splitView =
+      base::mac::ObjCCastStrict<GraySplitView>([self superview]);
+  if ([splitView isVertical])
+    subviewFrame.size.height -= [splitView topContentOffset];
+
+  DCHECK_EQ(1u, [[self subviews] count]);
+  [[[self subviews] lastObject] setFrame:subviewFrame];
 }
 
 @end
@@ -133,7 +163,11 @@ using content::WebContents;
   // VIEW_ID_DEV_TOOLS_DOCKED here.
   NSView* devToolsView = devToolsContents->GetNativeView();
   view_id_util::SetID(devToolsView, VIEW_ID_DEV_TOOLS_DOCKED);
-  [splitView_ addSubview:devToolsView];
+
+  scoped_nsobject<DevToolsContainerView> devToolsContainerView(
+      [[DevToolsContainerView alloc] initWithFrame:[devToolsView bounds]]);
+  [devToolsContainerView addSubview:devToolsView];
+  [splitView_ addSubview:devToolsContainerView];
 
   BOOL isVertical = devToolsWindow_->dock_side() == DEVTOOLS_DOCK_SIDE_RIGHT;
   [splitView_ setVertical:isVertical];
@@ -189,6 +223,16 @@ using content::WebContents;
   if ([[splitView_ subviews] indexOfObject:subview] == 1)
     return NO;
   return YES;
+}
+
+- (CGFloat)splitView:(NSSplitView*)splitView
+    constrainSplitPosition:(CGFloat)proposedPosition
+               ofSubviewAt:(NSInteger)dividerIndex {
+  if (![splitView_ isVertical] &&
+      proposedPosition < [splitView_ topContentOffset]) {
+    return [splitView_ topContentOffset];
+  }
+  return proposedPosition;
 }
 
 -(void)splitViewWillResizeSubviews:(NSNotification *)notification {

chrome/browser/ui/cocoa/dev_tools_controller_browsertest.mm
@@ -1,0 +1,88 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/browser/ui/cocoa/dev_tools_controller.h"
+
+#include "chrome/browser/devtools/devtools_window.h"
+#include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_window.h"
+#include "chrome/browser/ui/cocoa/browser_window_controller.h"
+#include "chrome/test/base/in_process_browser_test.h"
+
+class DevToolsControllerTest : public InProcessBrowserTest {
+ public:
+  DevToolsControllerTest() : InProcessBrowserTest() {
+  }
+
+  virtual void SetUpOnMainThread() OVERRIDE {
+    DevToolsWindow::ToggleDevToolsWindow(browser(),
+                                         DEVTOOLS_TOGGLE_ACTION_SHOW);
+  }
+
+  DevToolsController* controller() {
+    NSWindow* window = browser()->window()->GetNativeWindow();
+    BrowserWindowController* window_controller =
+        [BrowserWindowController browserWindowControllerForWindow:window];
+    return [window_controller devToolsController];
+  }
+
+  void SetDockSide(DevToolsDockSide side) {
+    content::WebContents* web_contents =
+        browser()->tab_strip_model()->GetActiveWebContents();
+    DevToolsWindow* dev_tools =
+        DevToolsWindow::GetDockedInstanceForInspectedTab(web_contents);
+    dev_tools->SetDockSide(dev_tools->SideToString(side));
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(DevToolsControllerTest);
+};
+
+// Verify that in horizontal mode the splitter is not allowed to go past the
+// bookmark bar.
+IN_PROC_BROWSER_TEST_F(DevToolsControllerTest, ConstrainSplitter) {
+  [controller() setTopContentOffset:0];
+  EXPECT_EQ(0, [controller() splitView:[controller() splitView]
+                constrainSplitPosition:0
+                           ofSubviewAt:0]);
+
+  CGFloat offset = 50;
+  [controller() setTopContentOffset:offset];
+  EXPECT_EQ(offset, [controller() splitView:[controller() splitView]
+                     constrainSplitPosition:0
+                                ofSubviewAt:0]);
+
+  // Should not be constrained in vertical mode.
+  [[controller() splitView] setVertical:YES];
+  EXPECT_EQ(0, [controller() splitView:[controller() splitView]
+                constrainSplitPosition:0
+                           ofSubviewAt:0]);
+}
+
+// When docked to the right the dev tools view should be shrunk so that it
+// doesn't overlap the bookmark bar.
+IN_PROC_BROWSER_TEST_F(DevToolsControllerTest, ViewSize) {
+  EXPECT_EQ(2u, [[[controller() splitView] subviews] count]);
+  NSView* container_view = [[[controller() splitView] subviews] lastObject];
+  EXPECT_EQ(1u, [[container_view subviews] count]);
+  NSView* dev_tools_view = [[container_view subviews] lastObject];
+  CGFloat width = NSWidth([[controller() splitView] bounds]);
+  CGFloat height = NSHeight([[controller() splitView] bounds]);
+
+  SetDockSide(DEVTOOLS_DOCK_SIDE_BOTTOM);
+  EXPECT_EQ(width, NSWidth([dev_tools_view bounds]));
+
+  SetDockSide(DEVTOOLS_DOCK_SIDE_RIGHT);
+  EXPECT_EQ(height, NSHeight([dev_tools_view bounds]));
+
+  CGFloat offset = 50;
+  [controller() setTopContentOffset:offset];
+
+  SetDockSide(DEVTOOLS_DOCK_SIDE_BOTTOM);
+  EXPECT_EQ(width, NSWidth([dev_tools_view bounds]));
+
+  SetDockSide(DEVTOOLS_DOCK_SIDE_RIGHT);
+  EXPECT_EQ(height - offset, NSHeight([dev_tools_view bounds]));
+}

chrome/chrome_tests.gypi
@@ -1313,6 +1313,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/cocoa/constrained_window/constrained_window_mac_browsertest.mm',
         'browser/ui/cocoa/content_settings/collected_cookies_mac_browsertest.mm',
         'browser/ui/cocoa/content_settings/content_setting_bubble_cocoa_unittest.mm',
+        'browser/ui/cocoa/dev_tools_controller_browsertest.mm',
         'browser/ui/cocoa/extensions/extension_action_context_menu_browsertest.mm',
         'browser/ui/cocoa/extensions/extension_install_dialog_controller_browsertest.mm',
         'browser/ui/cocoa/extensions/extension_install_prompt_test_utils.h',