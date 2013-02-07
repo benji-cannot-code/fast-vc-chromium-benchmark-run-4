0de44469ba9d756c59422557879f735ba9e69377
gman@chromium.org <gman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-07 12:57:12 +0000

    Add WebGL Conformance Tests context folder. https://bugs.webkit.org/show_bug.cgi?id=109114
    Reviewed by Kenneth Russell.
    
    * webgl/conformance/context/constants-expected.txt: Added.
    * webgl/conformance/context/constants.html: Added.
    * webgl/conformance/context/context-attributes-alpha-depth-stencil-antialias-expected.txt: Added.
    * webgl/conformance/context/context-attributes-alpha-depth-stencil-antialias.html: Added.
    * webgl/conformance/context/context-lost-expected.txt: Added.
    * webgl/conformance/context/context-lost-restored-expected.txt: Added.
    * webgl/conformance/context/context-lost-restored.html: Added.
    * webgl/conformance/context/context-lost.html: Added.
    * webgl/conformance/context/context-release-upon-reload-expected.txt: Added.
    * webgl/conformance/context/context-release-upon-reload.html: Added.
    * webgl/conformance/context/context-release-with-workers-expected.txt: Added.
    * webgl/conformance/context/context-release-with-workers.html: Added.
    * webgl/conformance/context/context-type-test-expected.txt: Added.
    * webgl/conformance/context/context-type-test.html: Added.
    * webgl/conformance/context/incorrect-context-object-behaviour-expected.txt: Added.
    * webgl/conformance/context/incorrect-context-object-behaviour.html: Added.
    * webgl/conformance/context/methods-expected.txt: Added.
    * webgl/conformance/context/methods.html: Added.
    * webgl/conformance/context/premultiplyalpha-test-expected.txt: Added.
    * webgl/conformance/context/premultiplyalpha-test.html: Added.
    * webgl/conformance/context/resource-sharing-test-expected.txt: Added.
    * webgl/conformance/context/resource-sharing-test.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/00_test_list.txt: Added.
    * webgl/resources/webgl_test_files/conformance/context/constants.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/context-attributes-alpha-depth-stencil-antialias.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/context-lost-restored.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/context-lost.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/context-release-upon-reload.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/context-release-with-workers.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/context-type-test.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/incorrect-context-object-behaviour.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/methods.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/premultiplyalpha-test.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/resource-sharing-test.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/resources/context-release-child-with-worker.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/resources/context-release-upon-reload-child.html: Added.
    * webgl/resources/webgl_test_files/conformance/context/resources/context-release-worker.js: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/webgl/resources/webgl_test_files/conformance/context/resources/context-release-worker.js
@@ -0,0 +1,5 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Simple worker used to provoke WebGL context release bugs on Chrome
+
+postMessage("Hello World");
+close();
