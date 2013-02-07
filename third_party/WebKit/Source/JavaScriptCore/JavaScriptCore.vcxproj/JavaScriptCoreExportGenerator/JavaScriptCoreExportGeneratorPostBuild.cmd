49c663fa49fb06452d7b4d65f1bca538193e9b77
roger_fong@apple.com <roger_fong@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-07 02:00:12 +0000

    Unreviewed. Touchups to VS2010 WebKit solution. Fix an export generator script, modify some property sheets, add resouce file. Add WinLauncher projects to solution.
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorDebug.props:
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd:
    * JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorRelease.props:
    * JavaScriptCore.vcxproj/resource.h: Added.
    * WebKit.vcxproj/WebKit.sln:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd
+++ third_party/WebKit/Source/JavaScriptCore/JavaScriptCore.vcxproj/JavaScriptCoreExportGenerator/JavaScriptCoreExportGeneratorPostBuild.cmd
@@ -1,9 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 set GeneratorDirectory=%CONFIGURATIONBUILDDIR%\obj\JavaScriptCoreExportGenerator
-echo Exporting link definition output (%GeneratorDirectory%\JavaScriptCoreExports.def)
-if exist "%OUTDIR%\JavaScriptCoreExportGenerator.exe" "%OUTDIR%\JavaScriptCoreExportGenerator.exe" > "%GeneratorDirectory%\JavaScriptCoreExports.def"
 
 mkdir 2>NUL "%CONFIGURATIONBUILDDIR%\bin"
 if exist "%WebKit_Libraries%\bin\pthreadVC2.dll" xcopy /y /d "%WebKit_Libraries%\bin\pthreadVC2.dll" "%CONFIGURATIONBUILDDIR%\bin"
+
+echo Exporting link definition output (%GeneratorDirectory%\JavaScriptCoreExports.def)
+if exist "%OUTDIR%\JavaScriptCoreExportGenerator.exe" "%OUTDIR%\JavaScriptCoreExportGenerator.exe" > "%GeneratorDirectory%\JavaScriptCoreExports.def"
 
 if exist "%CONFIGURATIONBUILDDIR%\buildfailed" del "%CONFIGURATIONBUILDDIR%\buildfailed"
