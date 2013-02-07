b0b25f51c615a2d561f1b289190f9c0eafad28f9
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 20:12:22 +0000

    Also makes MessageSimpleView aware of background.
    Otherwise, MessageSimpleView has weird greyish background. This regression
    happened because I'm not so aware of crrev.com/12090011 when I committed
    crrev.com/12082064
    
    BUG=172010
    TEST=send a simple notification and open the message center
    
    
    Review URL: https://chromiumcodereview.appspot.com/12177026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181356 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_simple_view.h
+++ ui/message_center/message_simple_view.h
@@ -26,10 +26,15 @@ class MessageSimpleView : public MessageView {
                              const ui::Event& event) OVERRIDE;
 
  protected:
+  // Overridden from views::View:
+  virtual gfx::Size GetPreferredSize() OVERRIDE;
+  virtual void Layout() OVERRIDE;
+
   MessageSimpleView();
 
  private:
   scoped_ptr<views::ImageButton> old_style_close_button_;
+  scoped_ptr<views::View> content_view_;
 
   DISALLOW_COPY_AND_ASSIGN(MessageSimpleView);
 };
