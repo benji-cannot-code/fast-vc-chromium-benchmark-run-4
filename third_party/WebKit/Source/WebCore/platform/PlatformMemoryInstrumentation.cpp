7a36698f63ef44846d2e72a120aba43613b56dca
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-07 09:09:22 +0000

    Web Inspector: reduce number of native memory instrumentation categories https://bugs.webkit.org/show_bug.cgi?id=109146
    Reviewed by Pavel Feldman.
    
    Merged some of memory instrumentation categories.
    
    * dom/WebCoreMemoryInstrumentation.cpp:
    (WebCore):
    * inspector/front-end/NativeMemorySnapshotView.js:
    (WebInspector.MemoryBlockViewProperties._initialize):
    * platform/PlatformMemoryInstrumentation.cpp:
    (WebCore):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142081 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/PlatformMemoryInstrumentation.cpp
+++ third_party/WebKit/Source/WebCore/platform/PlatformMemoryInstrumentation.cpp
@@ -38,13 +38,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-MemoryObjectType PlatformMemoryTypes::Image = "Page.Image";
-MemoryObjectType PlatformMemoryTypes::Loader = "Page.Loader";
-MemoryObjectType PlatformMemoryTypes::Rendering = "Page.Rendering";
-MemoryObjectType PlatformMemoryTypes::Layers = "Page.Rendering.Layers";
+MemoryObjectType PlatformMemoryTypes::Image = "Image";
+MemoryObjectType PlatformMemoryTypes::Loader = "Resources";
+MemoryObjectType PlatformMemoryTypes::Rendering = "Rendering";
+MemoryObjectType PlatformMemoryTypes::Layers = "Rendering";
 
-MemoryObjectType PlatformMemoryTypes::Audio = "Page.Audio";
-MemoryObjectType PlatformMemoryTypes::AudioSharedData = "Page.Audio.SharedData";
+MemoryObjectType PlatformMemoryTypes::Audio = "Audio";
+MemoryObjectType PlatformMemoryTypes::AudioSharedData = "Audio";
 
 void PlatformMemoryInstrumentation::reportStaticMembersMemoryUsage(WTF::MemoryInstrumentation* memoryInstrumentation)
 {
