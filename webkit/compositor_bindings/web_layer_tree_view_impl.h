98714a09620b5f6e089200fa6a09f8fca6c91234
egraether@chromium.org <egraether@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 19:00:23 +0000

    cc: add WebLayerTreeView API for toggling composited layer borders
    BUG=71035
    
    
    Review URL: https://chromiumcodereview.appspot.com/12226018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181342 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/compositor_bindings/web_layer_tree_view_impl.h
+++ webkit/compositor_bindings/web_layer_tree_view_impl.h
@@ -60,6 +60,7 @@ public:
     virtual void renderingStats(WebRenderingStats&) const OVERRIDE;
     virtual void setShowFPSCounter(bool show);
     virtual void setShowPaintRects(bool show);
+    virtual void setShowDebugBorders(bool show);
     virtual void setContinuousPaintingEnabled(bool);
 
     // cc::LayerTreeHostClient implementation.
