504c78408b118fb9e3e340c03eadbb46b989ca33
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-07 00:41:07 +0000

    display:none file upload button crashes https://bugs.webkit.org/show_bug.cgi?id=109102
    Reviewed by Levi Weintraub.
    
    Source/WebCore:
    
    Test: fast/forms/file/display-none-upload-button.html
    
    * rendering/RenderFileUploadControl.cpp:
    (WebCore::nodeWidth):
    (WebCore::RenderFileUploadControl::paintObject):
    Having an upload button doesn't mean we have a rendered upload button.
    Null check the renderer before trying to access it.
    
    LayoutTests:
    
    * fast/forms/file/display-none-upload-button-expected.txt: Added.
    * fast/forms/file/display-none-upload-button.html: Added.
    Tests that we don't crash. Also exposes a bug that the baseline and height of
    the input don't include the height of the filename text.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142054 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFileUploadControl.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderFileUploadControl.cpp
@@ -90,7 +90,7 @@ void RenderFileUploadControl::updateFromElement()
 
 static int nodeWidth(Node* node)
 {
-    return node ? node->renderBox()->pixelSnappedWidth() : 0;
+    return (node && node->renderBox()) ? node->renderBox()->pixelSnappedWidth() : 0;
 }
 
 int RenderFileUploadControl::maxFilenameWidth() const
@@ -137,10 +137,14 @@ void RenderFileUploadControl::paintObject(PaintInfo& paintInfo, const LayoutPoin
             textX = contentLeft + buttonAndIconWidth;
         else
             textX = contentLeft + contentWidth() - buttonAndIconWidth - font.width(textRun);
+
+        LayoutUnit textY = 0;
         // We want to match the button's baseline
-        RenderButton* buttonRenderer = toRenderButton(button->renderer());
         // FIXME: Make this work with transforms.
-        LayoutUnit textY = paintOffset.y() + buttonRenderer->baselinePosition(AlphabeticBaseline, true, HorizontalLine, PositionOnContainingLine);
+        if (RenderButton* buttonRenderer = toRenderButton(button->renderer()))
+            textY = paintOffset.y() + buttonRenderer->baselinePosition(AlphabeticBaseline, true, HorizontalLine, PositionOnContainingLine);
+        else
+            textY = baselinePosition(AlphabeticBaseline, true, HorizontalLine, PositionOnContainingLine);
 
         paintInfo.context->setFillColor(style()->visitedDependentColor(CSSPropertyColor), style()->colorSpace());
         
