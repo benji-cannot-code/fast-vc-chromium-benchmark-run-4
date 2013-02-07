d4c86353dc92275263dd0bae4e39ac3e05eaa1a6
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 16:10:53 +0000

    Update some #includes in webkit/ for headers that have been moved to the new Platform directory upstream
    Review URL: https://chromiumcodereview.appspot.com/12208047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181313 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/tools/test_shell/simple_dom_storage_system.cc
+++ webkit/tools/test_shell/simple_dom_storage_system.cc
@@ -7,8 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "googleurl/src/gurl.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/WebStorageArea.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebURL.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebStorageArea.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebStorageEventDispatcher.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebStorageNamespace.h"
 #include "webkit/database/database_util.h"
