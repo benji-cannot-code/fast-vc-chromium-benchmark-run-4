124650a34183410547501cbe2bdd751523ec4c11
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 04:44:19 +0000

    Use correct TextDirection in SelectionHandleController
    The text direction is now being passed to ContentViewCore as a
    WebKit::WebTextDirection. This inverts RTL and LTR. Update
    SelectionHandleController to use this inversion.
    
    
    BUG=162468,142585
    
    
    Review URL: https://chromiumcodereview.appspot.com/12224021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181203 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_android.cc
+++ content/browser/renderer_host/render_widget_host_view_android.cc
@@ -34,22 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-namespace {
-
-// TODO(pliard): http://crbug.com/142585. Remove this helper function and update
-// the clients to deal directly with WebKit::WebTextDirection.
-base::i18n::TextDirection ConvertTextDirection(WebKit::WebTextDirection dir) {
-  switch (dir) {
-    case WebKit::WebTextDirectionDefault: return base::i18n::UNKNOWN_DIRECTION;
-    case WebKit::WebTextDirectionLeftToRight: return base::i18n::LEFT_TO_RIGHT;
-    case WebKit::WebTextDirectionRightToLeft: return base::i18n::RIGHT_TO_LEFT;
-  }
-  NOTREACHED() << "Unsupported text direction " << dir;
-  return base::i18n::UNKNOWN_DIRECTION;
-}
-
-}  // namespace
-
 RenderWidgetHostViewAndroid::RenderWidgetHostViewAndroid(
     RenderWidgetHostImpl* widget_host,
     ContentViewCoreImpl* content_view_core)
