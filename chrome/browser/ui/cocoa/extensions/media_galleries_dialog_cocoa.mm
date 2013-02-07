93494115875b1c85e470e6df41b580d955f07291
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-07 21:00:03 +0000

    Media Galleries: Make Mac permission dialog entry IDs unique.
    Review URL: https://chromiumcodereview.appspot.com/12208062
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181362 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/media_galleries_dialog_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/media_galleries_dialog_cocoa.mm
@@ -51,6 +51,14 @@ const CGFloat kCheckboxMaxWidth = 350;
 
 namespace chrome {
 
+namespace {
+
+NSString* GetUniqueIDForGallery(const MediaGalleryPrefInfo* gallery) {
+  return base::SysUTF8ToNSString(gallery->device_id + gallery->path.value());
+}
+
+}  // namespace
+
 MediaGalleriesDialogCocoa::MediaGalleriesDialogCocoa(
     MediaGalleriesDialogController* controller,
     MediaGalleriesCocoaController* cocoa_controller)
@@ -138,8 +146,8 @@ void MediaGalleriesDialogCocoa::OnCheckboxToggled(NSButton* checkbox) {
        const_reverse_iterator iter = permissions.rbegin();
        iter != permissions.rend(); iter++) {
     const MediaGalleryPrefInfo* gallery = &iter->second.pref_info;
-    NSString* device_id = base::SysUTF8ToNSString(gallery->device_id);
-    if ([[[checkbox cell] representedObject] isEqual:device_id]) {
+    NSString* unique_id = GetUniqueIDForGallery(gallery);
+    if ([[[checkbox cell] representedObject] isEqual:unique_id]) {
       controller_->DidToggleGallery(gallery, [checkbox state] == NSOnState);
       break;
     }
@@ -159,8 +167,8 @@ void MediaGalleriesDialogCocoa::UpdateGalleryCheckbox(
 
     scoped_nsobject<NSButton> new_checkbox(
         [[NSButton alloc] initWithFrame:NSZeroRect]);
-    NSString* device_id = base::SysUTF8ToNSString(gallery->device_id);
-    [[new_checkbox cell] setRepresentedObject:device_id];
+    NSString* unique_id = GetUniqueIDForGallery(gallery);
+    [[new_checkbox cell] setRepresentedObject:unique_id];
     [[new_checkbox cell] setLineBreakMode:NSLineBreakByTruncatingMiddle];
     [new_checkbox setButtonType:NSSwitchButton];
     [new_checkbox setTarget:cocoa_controller_];
@@ -194,10 +202,10 @@ void MediaGalleriesDialogCocoa::UpdateGallery(
     const MediaGalleryPrefInfo* gallery,
     bool permitted) {
   NSButton* checkbox = nil;
-  NSString* device_id = base::SysUTF8ToNSString(gallery->device_id);
+  NSString* unique_id = GetUniqueIDForGallery(gallery);
 
   for (NSButton* button in checkboxes_.get()) {
-    if ([[[button cell] representedObject] isEqual:device_id]) {
+    if ([[[button cell] representedObject] isEqual:unique_id]) {
       checkbox = button;
       break;
     }
