e5d43a37bfd9e1524c4fa016ca45c86888edba64
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-08 18:27:19 +0000

    chromeos: Stop allowing DriveFileSystem methods to be called on IO thread.
    Eliminate all 'OnUIThread' from DriveFileSystem.
    Stop holding WeakPtr.
    Stop mentioning about threads in header, all operations are done on UI thread now.
    
    BUG=165619
    TEST=unit_tests --gtest_filter="Drive*"
    
    Review URL: https://chromiumcodereview.appspot.com/12207074
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181524 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_file_system_interface.h
+++ chrome/browser/chromeos/drive/drive_file_system_interface.h
@@ -113,7 +113,6 @@ class DriveFileSystemInterface {
   // Finds an entry (file or directory) by using |resource_id|. This call
   // does not initiate content refreshing.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void GetEntryInfoByResourceId(
       const std::string& resource_id,
@@ -123,7 +122,6 @@ class DriveFileSystemInterface {
   // |remote_src_file_path| is the virtual source path on the Drive file system.
   // |local_dest_file_path| is the destination path on the local file system.
   //
-  // Must be called from *UI* thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void TransferFileFromRemoteToLocal(
       const FilePath& remote_src_file_path,
@@ -135,7 +133,6 @@ class DriveFileSystemInterface {
   // |remote_dest_file_path| is the virtual destination path within Drive file
   // system.
   //
-  // Must be called from *UI* thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void TransferFileFromLocalToRemote(
       const FilePath& local_src_file_path,
@@ -150,7 +147,6 @@ class DriveFileSystemInterface {
   // |CloseFile| must be called when the modification to the cache is done.
   // Otherwise, Drive file system does not pick up the file for uploading.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void OpenFile(const FilePath& file_path,
                         const OpenFileCallback& callback) = 0;
@@ -158,7 +154,6 @@ class DriveFileSystemInterface {
   // Closes a file at the virtual path |file_path| on the Drive file system,
   // which is opened via OpenFile(). It commits the dirty flag on the cache.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void CloseFile(const FilePath& file_path,
                          const FileOperationCallback& callback) = 0;
@@ -180,7 +175,6 @@ class DriveFileSystemInterface {
   // of |dest_file_path| need to be present in the in-memory representation
   // of the file system.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void Copy(const FilePath& src_file_path,
                     const FilePath& dest_file_path,
@@ -201,7 +195,6 @@ class DriveFileSystemInterface {
   // of |dest_file_path| need to be present in the in-memory representation
   // of the file system.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void Move(const FilePath& src_file_path,
                     const FilePath& dest_file_path,
@@ -215,7 +208,6 @@ class DriveFileSystemInterface {
   //
   // TODO(satorux): is_recursive is not supported yet. crbug.com/138282
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void Remove(const FilePath& file_path,
                       bool is_recursive,
@@ -226,7 +218,6 @@ class DriveFileSystemInterface {
   // |directory_path|. If |is_recursive| is true, the call creates parent
   // directories as needed just like mkdir -p does.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void CreateDirectory(const FilePath& directory_path,
                                bool is_exclusive,
@@ -238,7 +229,6 @@ class DriveFileSystemInterface {
   // an error if a directory or a hosted document is already present at the
   // path, or the parent directory of the path is not present yet.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread
   // |callback| must not be null.
   virtual void CreateFile(const FilePath& file_path,
                           bool is_exclusive,
@@ -249,8 +239,6 @@ class DriveFileSystemInterface {
   // system in order to be retrieved. If the file is not cached, the file
   // will be downloaded through GData API or Drive V2 API.
   //
-  // Can be called from UI/IO thread.
-  // |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void GetFileByPath(const FilePath& file_path,
                              const GetFileCallback& callback) = 0;
@@ -258,8 +246,6 @@ class DriveFileSystemInterface {
   // Gets a file by the given |resource_id| from the Drive server. Used for
   // fetching pinned-but-not-fetched files.
   //
-  // Can be called from UI/IO thread. |get_file_callback| and
-  // |get_content_callback| are run on the calling thread.
   // |get_file_callback| must not be null.
   // |get_content_callback| may be null.
   virtual void GetFileByResourceId(
@@ -274,8 +260,7 @@ class DriveFileSystemInterface {
   // TODO(satorux): As of now, the function only handles files with the dirty
   // bit committed. We should eliminate the restriction. crbug.com/134558.
   //
-  // Can be called from UI/IO thread. |callback| and is run on the calling
-  // thread.  |callback| must not be null.
+  // |callback| must not be null.
   virtual void UpdateFileByResourceId(
       const std::string& resource_id,
       const FileOperationCallback& callback) = 0;
@@ -283,7 +268,6 @@ class DriveFileSystemInterface {
   // Finds an entry (a file or a directory) by |file_path|. This call will also
   // retrieve and refresh file system content from server and disk cache.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void GetEntryInfoByPath(const FilePath& file_path,
                                   const GetEntryInfoCallback& callback) = 0;
@@ -291,7 +275,6 @@ class DriveFileSystemInterface {
   // Finds and reads a directory by |file_path|. This call will also retrieve
   // and refresh file system content from server and disk cache.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void ReadDirectoryByPath(
       const FilePath& file_path,
@@ -309,8 +292,6 @@ class DriveFileSystemInterface {
   // and the change is notified via Observer::OnDirectoryChanged(). Note that
   // this function ignores changes in directories in the target
   // directory. Changes in directories are handled via the delta feeds.
-  //
-  // Can be called from UI/IO thread.
   virtual void RequestDirectoryRefresh(const FilePath& file_path) = 0;
 
   // Does server side content search for |search_query|.
@@ -320,7 +301,6 @@ class DriveFileSystemInterface {
   // Search results will be returned as a list of results' |SearchResultInfo|
   // structs, which contains file's path and is_directory flag.
   //
-  // Can be called from UI/IO thread. |callback| is run on the calling thread.
   // |callback| must not be null.
   virtual void Search(const std::string& search_query,
                       bool shared_with_me,
@@ -335,7 +315,6 @@ class DriveFileSystemInterface {
   // the cache state. Adds a new file entry, and store its content from
   // |file_content_path| into the cache.
   //
-  // |callback| will be called on the UI thread upon completion of operation.
   // |callback| must not be null.
   virtual void AddUploadedFile(const FilePath& directory_path,
                                scoped_ptr<google_apis::ResourceEntry> doc_entry,
