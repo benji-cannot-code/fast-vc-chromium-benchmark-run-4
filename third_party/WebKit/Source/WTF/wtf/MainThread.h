21e17efcab91982f8c40984ff75d807de57e83a0
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-08 06:41:43 +0000

    Fix #endif comment from r142163 and r142183
    * wtf/MainThread.h:
    (WTF): s/PLATFORM/USE/
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142230 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/MainThread.h
+++ third_party/WebKit/Source/WTF/wtf/MainThread.h
@@ -58,7 +58,7 @@ WTF_EXPORT_PRIVATE bool isUIThread();
 #else
 inline bool isWebThread() { return isMainThread(); }
 inline bool isUIThread() { return isMainThread(); }
-#endif // PLATFORM(WEB_THREAD)
+#endif // USE(WEB_THREAD)
 
 void initializeGCThreads();
 
