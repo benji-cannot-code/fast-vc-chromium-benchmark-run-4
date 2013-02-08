3262b92589788d006f877dedee7c151ed6b77958
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-08 03:25:07 +0000

    [Mac] Allow NSExceptions in third-party webcam code.
    Chromium cannot control that code, and NSExceptions should be safe-ish
    since there is no Chromium code on the stack.
    
    BUG=174708
    
    
    Review URL: https://chromiumcodereview.appspot.com/12208053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181417 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/mac/video_capture_device_qtkit_mac.mm
+++ media/video/capture/mac/video_capture_device_qtkit_mac.mm
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/debug/crash_logging.h"
 #include "base/logging.h"
+#include "base/mac/scoped_nsexception_enabler.h"
 #include "media/video/capture/mac/video_capture_device_mac.h"
 #include "media/video/capture/video_capture_device.h"
 #include "media/video/capture/video_capture_types.h"
@@ -18,8 +19,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #pragma mark Class methods
 
 + (void)getDeviceNames:(NSMutableDictionary*)deviceNames {
+  // Third-party drivers often throw exceptions, which are fatal in
+  // Chromium (see comments in scoped_nsexception_enabler.h).  The
+  // following catches any exceptions and continues in an orderly
+  // fashion with no devices detected.
   NSArray* captureDevices =
-      [QTCaptureDevice inputDevicesWithMediaType:QTMediaTypeVideo];
+      base::mac::RunBlockIgnoringExceptions(^() {
+          return [QTCaptureDevice inputDevicesWithMediaType:QTMediaTypeVideo];
+      });
 
   for (QTCaptureDevice* device in captureDevices) {
     [deviceNames setObject:[device localizedDisplayName]
