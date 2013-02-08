d7c7f3d5bf5e7a1f0920a19710c44fe0af4231d8
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-08 10:14:23 +0000

    Fixed missing rename during refactoring breaking harness.
    TEST=Internal, check harness version of Files.app.
    BUG=160486
    
    Review URL: https://codereview.chromium.org/12209067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/mock_chrome.js
+++ chrome/browser/resources/file_manager/js/mock_chrome.js
@@ -417,7 +417,7 @@ chrome.fileBrowserPrivate = {
     online: true
   },
 
-  onNetworkConnectionChanged: new MockEventSource(),
+  onDriveConnectionStatusChanged: new MockEventSource(),
 
   getDriveConnectionState: function(callback) {
     setTimeout(callback, 0, cloneShallow(
