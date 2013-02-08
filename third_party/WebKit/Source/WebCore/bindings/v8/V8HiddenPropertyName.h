c59db1f1aff9471d397136ff70edf362d4e8a903
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-08 05:37:26 +0000

    [V8] enum V8HiddenPropertyCreationType is not used https://bugs.webkit.org/show_bug.cgi?id=109250
    Reviewed by Adam Barth.
    
    V8HiddenPropertyCreationType is always NewSymbol. We can remove the enum.
    
    No tests. No change in behavior.
    
    * bindings/v8/V8HiddenPropertyName.cpp:
    (WebCore::hiddenReferenceName):
    * bindings/v8/V8HiddenPropertyName.h:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8HiddenPropertyName.h
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8HiddenPropertyName.h
@@ -49,8 +49,6 @@ namespace WebCore {
     V(toStringString) \
     V(typedArrayHiddenCopyMethod)
 
-enum V8HiddenPropertyCreationType { NewSymbol, NewString };
-
 class V8HiddenPropertyName {
 public:
     V8HiddenPropertyName() { }
