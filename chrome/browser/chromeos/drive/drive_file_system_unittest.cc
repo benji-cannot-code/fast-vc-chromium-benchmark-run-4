167e8df589ceaaf72a87b8a2a374586b5d441970
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-08 06:52:57 +0000

    Rename content_url in the drive related codes to download_url.
    The name content_url is a bit confusing because URLs "/.../contents" is also
    used. So, this renames it to download_url following the fact that it is only
    used for file downloading.
    
    BUG=148627
    TEST=Ran unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/12212061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181442 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_file_system_unittest.cc
+++ chrome/browser/chromeos/drive/drive_file_system_unittest.cc
@@ -1436,7 +1436,7 @@ TEST_F(DriveFileSystemTest, MoveFileFromRootToSubDirectory) {
       dest_parent_path);
   ASSERT_TRUE(dest_parent_proto.get());
   ASSERT_TRUE(dest_parent_proto->file_info().is_directory());
-  EXPECT_FALSE(dest_parent_proto->content_url().empty());
+  EXPECT_FALSE(dest_parent_proto->download_url().empty());
 
   // Expect notification for both source and destination directories.
   EXPECT_CALL(*mock_directory_observer_, OnDirectoryChanged(
@@ -1478,7 +1478,7 @@ TEST_F(DriveFileSystemTest, MoveFileFromSubDirectoryToRoot) {
       src_parent_path);
   ASSERT_TRUE(src_parent_proto.get());
   ASSERT_TRUE(src_parent_proto->file_info().is_directory());
-  EXPECT_FALSE(src_parent_proto->content_url().empty());
+  EXPECT_FALSE(src_parent_proto->download_url().empty());
 
   // Expect notification for both source and destination directories.
   EXPECT_CALL(*mock_directory_observer_, OnDirectoryChanged(
@@ -1527,14 +1527,14 @@ TEST_F(DriveFileSystemTest, MoveFileBetweenSubDirectories) {
       src_parent_path);
   ASSERT_TRUE(src_parent_proto.get());
   ASSERT_TRUE(src_parent_proto->file_info().is_directory());
-  EXPECT_FALSE(src_parent_proto->content_url().empty());
+  EXPECT_FALSE(src_parent_proto->download_url().empty());
 
   ASSERT_TRUE(EntryExists(dest_parent_path));
   scoped_ptr<DriveEntryProto> dest_parent_proto = GetEntryInfoByPathSync(
       dest_parent_path);
   ASSERT_TRUE(dest_parent_proto.get());
   ASSERT_TRUE(dest_parent_proto->file_info().is_directory());
-  EXPECT_FALSE(dest_parent_proto->content_url().empty());
+  EXPECT_FALSE(dest_parent_proto->download_url().empty());
 
   EXPECT_FALSE(EntryExists(interim_file_path));
 
