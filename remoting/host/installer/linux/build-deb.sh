cfd58413a253b8456ecf0997461a18dac9557c23
mmoss@chromium.org <mmoss@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-08 21:29:43 +0000

    Integrate Chromoting Linux packaging into official build.
    This will output a remoting-me2me-host-linux.zip file containing the
    Linux chrome-remote-desktop packages (currently just .deb), and will
    archive it along with other build artifacts like remoting-webapp.zip.
    
    Review URL: https://codereview.chromium.org/12224068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181551 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/installer/linux/build-deb.sh
+++ remoting/host/installer/linux/build-deb.sh
@@ -5,8 +5,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-if [[ -z "$version_full" ]]; then
-  src_root=./../../../..
+SCRIPTDIR="$(dirname "$(readlink -f "$0")")"
+PACKAGE_NAME="chrome-remote-desktop"
+
+guess_filename() {
+  ARCH=$(dpkg-architecture | awk -F '=' '/DEB_BUILD_ARCH=/{print $2}')
+  VERSION_FULL=$(get_version_full)
+  echo ${PACKAGE_NAME}_${VERSION_FULL}_${ARCH}.deb
+}
+
+get_version_full() {
+  src_root=${src_root:-./../../../..}
   remoting_version_path=$src_root/remoting/VERSION
   chrome_version_path=$src_root/chrome/VERSION
   version_helper=$src_root/chrome/tools/build/version.py
@@ -21,6 +30,61 @@ if [[ -z "$version_full" ]]; then
   version_patch=$($version_helper -f $chrome_version_path \
                   -f $remoting_version_path -t "@PATCH@")
   version_full="$version_major.$version_minor.$version_build.$version_patch"
+  echo $version_full
+}
+
+usage() {
+  echo "usage: $(basename $0) [-hp] [-o path] [-s path]"
+  echo "-h     this help message"
+  echo "-p     just print the expected DEB filename that this will build."
+  echo "-s     path to the top of the src tree."
+  echo "-o     path to write the DEB file to."
+}
+
+while getopts ":s:o:ph" OPTNAME
+do
+  case $OPTNAME in
+    s )
+      src_root="$(readlink -f "$OPTARG")"
+      ;;
+    o )
+      OUTPUT_PATH="$(readlink -f "$OPTARG")"
+      ;;
+    p )
+      PRINTDEBNAME=1
+      ;;
+    h )
+      usage
+      exit 0
+      ;;
+    \: )
+      echo "'-$OPTARG' needs an argument."
+      usage
+      exit 1
+      ;;
+    * )
+      echo "invalid command-line option: $OPTARG"
+      usage
+      exit 1
+      ;;
+  esac
+done
+shift $(($OPTIND - 1))
+
+# This just prints the expected package filename, then exits. It's needed so the
+# gyp packaging target can track the output file, to know whether or not it
+# needs to be built/rebuilt.
+if [[ -n "$PRINTDEBNAME" ]]; then
+  guess_filename
+  exit 0
+fi
+
+# TODO: Make this all happen in a temp dir to keep intermediate files out of the
+# build tree?
+cd "$SCRIPTDIR"
+
+if [[ -z "$version_full" ]]; then
+  version_full=$(get_version_full)
 fi
 
 if [[ ! "$version_full" =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
@@ -33,9 +97,12 @@ fi
 merge_head="$(git merge-base HEAD origin/git-svn 2>/dev/null || true)"
 if [[ -n "$merge_head" ]]; then
   revision="$(git svn find-rev "$merge_head" 2>/dev/null || true)"
-  if [[ -n "$revision" ]]; then
-    revision_text="(r$revision)"
-  fi
+else
+  # Official builders still use svn-based builds.
+  revision="$(svn info . | awk '/^Revision: /{print $2}')"
+fi
+if [[ -n "$revision" ]]; then
+  revision_text="(r$revision)"
 fi
 
 # Don't strip binaries unless build options are already specified. This doesn't
@@ -49,10 +116,15 @@ echo "Building version $version_full $revision_text"
 export DEBEMAIL="The Chromium Authors <chromium-dev@chromium.org>"
 rm -f debian/changelog
 debchange --create \
-  --package chrome-remote-desktop \
+  --package "$PACKAGE_NAME" \
   --newversion "$version_full" \
   --force-distribution \
   --distribution unstable \
   "New Debian package $revision_text"
 
 dpkg-buildpackage -b -us -uc
+
+if [[ "$OUTPUT_PATH" ]]; then
+  mv ../${PACKAGE_NAME}_*.deb "$OUTPUT_PATH"/
+  mv ../${PACKAGE_NAME}_*.changes "$OUTPUT_PATH"/
+fi
