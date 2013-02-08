c3bd361c748e4e056ed614099e083ad08700cf38
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-08 11:15:27 +0000

    Unreviewed, rolling out r141695 and r141697. http://trac.webkit.org/changeset/141695 http://trac.webkit.org/changeset/141697 https://bugs.webkit.org/show_bug.cgi?id=109279
    broke on-disk buffering for http(s) media (Requested by philn
    on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-08
    
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.cpp:
    (WebCore::MediaPlayerPrivateGStreamer::load):
    * platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h:
    (MediaPlayerPrivateGStreamer):
    * platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp:
    (webKitWebSrcGetProtocols):
    (webKitWebSrcSetUri):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142251 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/MediaPlayerPrivateGStreamer.h
@@ -90,8 +90,6 @@ class MediaPlayerPrivateGStreamer : public MediaPlayerPrivateGStreamerBase {
             void sourceChanged();
             GstElement* audioSink() const;
 
-            static KURL convertPlaybinURL(const gchar* uri);
-
         private:
             MediaPlayerPrivateGStreamer(MediaPlayer*);
 
@@ -99,8 +97,6 @@ class MediaPlayerPrivateGStreamer : public MediaPlayerPrivateGStreamerBase {
 
             static void getSupportedTypes(HashSet<String>&);
             static MediaPlayer::SupportsType supportsType(const String& type, const String& codecs, const KURL&);
-
-            void setPlaybinURL(KURL&);
 
             static bool isAvailable();
 
--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/WebKitWebSourceGStreamer.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GRefPtrGStreamer.h"
 #include "GStreamerVersioning.h"
 #include "MediaPlayer.h"
-#include "MediaPlayerPrivateGStreamer.h"
 #include "NetworkingContext.h"
 #include "NotImplemented.h"
 #include "ResourceHandleClient.h"
@@ -579,7 +578,7 @@ static GstURIType webKitWebSrcUriGetType(GType)
 
 const gchar* const* webKitWebSrcGetProtocols(GType)
 {
-    static const char* const protocols[] = {"webkit+http", "webkit+https", 0 };
+    static const char* protocols[] = {"http", "https", 0 };
     return protocols;
 }
 
@@ -604,7 +603,7 @@ static gboolean webKitWebSrcSetUri(GstURIHandler* handler, const gchar* uri, GEr
     if (!uri)
         return TRUE;
 
-    KURL url = WebCore::MediaPlayerPrivateGStreamer::convertPlaybinURL(uri);
+    KURL url(KURL(), uri);
 
     if (!url.isValid() || !url.protocolIsInHTTPFamily()) {
         g_set_error(error, GST_URI_ERROR, GST_URI_ERROR_BAD_URI, "Invalid URI '%s'", uri);
@@ -623,7 +622,7 @@ static GstURIType webKitWebSrcUriGetType(void)
 
 static gchar** webKitWebSrcGetProtocols(void)
 {
-    static gchar* protocols[] = {(gchar*) "webkit+http", (gchar*) "webkit+https", 0 };
+    static gchar* protocols[] = {(gchar*) "http", (gchar*) "https", 0 };
     return protocols;
 }
 
@@ -648,7 +647,7 @@ static gboolean webKitWebSrcSetUri(GstURIHandler* handler, const gchar* uri)
     if (!uri)
         return TRUE;
 
-    KURL url = WebCore::MediaPlayerPrivateGStreamer::convertPlaybinURL(uri);
+    KURL url(KURL(), uri);
 
     if (!url.isValid() || !url.protocolIsInHTTPFamily()) {
         GST_ERROR_OBJECT(src, "Invalid URI '%s'", uri);
