3883d12bf130ab9d2be678cb1e1d6dd904aafa8c
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-09 19:49:12 +0000

    Add new status msg when sync is stopped via the dashboard
    BUG=174134
    
    Review URL: https://codereview.chromium.org/12221101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -1894,6 +1894,10 @@ void ProfileSyncService::StopAndSuppress() {
   ShutdownImpl(false);
 }
 
+bool ProfileSyncService::IsStartSuppressed() const {
+  return sync_prefs_.IsStartSuppressed();
+}
+
 void ProfileSyncService::UnsuppressAndStart() {
   DCHECK(profile_);
   sync_prefs_.SetStartSuppressed(false);
