94b4ae03df44f361675b318a74a221ecfc87b623
noelallen@google.com <noelallen@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-09 17:11:45 +0000

    Reassign FD
    Modify AssignFD to Free then reassign, so that the Free path
    happens atomically in the function.
    
    See: https://codereview.chromium.org/12207031/
    
    R=binji@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/12217098
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181623 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io_test/kernel_object_test.cc
+++ native_client_sdk/src/libraries/nacl_io_test/kernel_object_test.cc
@@ -155,7 +155,7 @@ TEST_F(KernelObjectTest, Referencing) {
   EXPECT_EQ(0, mount_count);
 }
 
-TEST_F(KernelObjectTest, AssignFD) {
+TEST_F(KernelObjectTest, FreeAndReassignFD) {
   EXPECT_EQ(0, handle_count);
 
   KernelHandle* handle = new KernelHandleRefMock(mnt, NULL, 0);
@@ -163,7 +163,8 @@ TEST_F(KernelObjectTest, AssignFD) {
   EXPECT_EQ(1, handle_count);
   EXPECT_EQ(1, handle->RefCount());
 
-  proxy->AssignFD(2, handle);
+  // Assign to a non-existant FD
+  proxy->FreeAndReassignFD(2, handle);
   EXPECT_EQ((KernelHandle*)NULL, proxy->AcquireHandle(0));
   EXPECT_EQ((KernelHandle*)NULL, proxy->AcquireHandle(1));
   EXPECT_EQ(handle, proxy->AcquireHandle(2));
@@ -172,7 +173,7 @@ TEST_F(KernelObjectTest, AssignFD) {
   EXPECT_EQ(1, handle_count);
   EXPECT_EQ(2, handle->RefCount());
 
-  proxy->AssignFD(0, handle);
+  proxy->FreeAndReassignFD(0, handle);
   EXPECT_EQ(handle, proxy->AcquireHandle(0));
   EXPECT_EQ((KernelHandle*)NULL, proxy->AcquireHandle(1));
   EXPECT_EQ(handle, proxy->AcquireHandle(2));
