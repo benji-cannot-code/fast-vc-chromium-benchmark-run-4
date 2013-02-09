cd83ef086dd14d0222c73eb6628a611726e5f9f7
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-09 02:42:51 +0000

    Rolling out r142333 and r142337 which broke Mac Release builds.
    git-svn-id: svn://svn.chromium.org/blink/trunk@142341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/InjectedBundlePageUIClient.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/InjectedBundle/InjectedBundlePageUIClient.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "APIClient.h"
 #include "WKBundlePage.h"
 #include "WebEvent.h"
+#include "WebImage.h"
 #include <WebCore/RenderSnapshottedPlugIn.h>
 #include <wtf/Forward.h>
 
@@ -71,6 +72,7 @@ public:
     void didReachApplicationCacheOriginQuota(WebPage*, WebSecurityOrigin*, int64_t totalBytesNeeded);
     uint64_t didExceedDatabaseQuota(WebPage*, WebSecurityOrigin*, const String& databaseName, const String& databaseDisplayName, uint64_t currentQuotaBytes, uint64_t currentOriginUsageBytes, uint64_t currentDatabaseUsageBytes, uint64_t expectedUsageBytes);
 
+    PassRefPtr<WebImage> plugInStartLabelImage(WebCore::RenderSnapshottedPlugIn::LabelSize) const;
     String plugInStartLabelTitle() const;
     String plugInStartLabelSubtitle() const;
     String plugInExtraStyleSheet() const;
