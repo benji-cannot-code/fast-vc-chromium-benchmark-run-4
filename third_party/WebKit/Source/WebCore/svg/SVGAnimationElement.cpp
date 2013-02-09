b036cf947f1b30fc33f665a04321d08923bf1224
pdr@google.com <pdr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-09 18:02:37 +0000

    Sanitize m_keyTimes for paced value animations https://bugs.webkit.org/show_bug.cgi?id=108828
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    SVG animations with calcMode=paced calculate new m_keyTimes in
    SVGAnimationElement::calculateKeyTimesForCalcModePaced() because paced animations do not
    specify keyTimes. If an error occurs while calculating m_keyTimes, and there exists
    user-specified values, a crash could occur because the user-specified values were not
    sanitized.
    
    This change clears user-specified keyTimes before calculating new ones.
    
    Test: svg/animations/animate-keytimes-crash.html
    
    * svg/SVGAnimationElement.cpp:
    (WebCore::SVGAnimationElement::calculateKeyTimesForCalcModePaced):
    
    LayoutTests:
    
    * svg/animations/animate-keytimes-crash-expected.html: Added.
    * svg/animations/animate-keytimes-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142365 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGAnimationElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGAnimationElement.cpp
@@ -386,6 +386,10 @@ void SVGAnimationElement::calculateKeyTimesForCalcModePaced()
     ASSERT(valuesCount >= 1);
     if (valuesCount == 1)
         return;
+
+    // FIXME, webkit.org/b/109010: m_keyTimes should not be modified in this function.
+    m_keyTimes.clear();
+
     Vector<float> keyTimesForPaced;
     float totalDistance = 0;
     keyTimesForPaced.append(0);
@@ -406,7 +410,7 @@ void SVGAnimationElement::calculateKeyTimesForCalcModePaced()
     keyTimesForPaced[keyTimesForPaced.size() - 1] = 1;
 
     // Use key times calculated based on pacing instead of the user provided ones.
-    m_keyTimes.swap(keyTimesForPaced);
+    m_keyTimes = keyTimesForPaced;
 }
 
 static inline double solveEpsilon(double duration) { return 1 / (200 * duration); }
