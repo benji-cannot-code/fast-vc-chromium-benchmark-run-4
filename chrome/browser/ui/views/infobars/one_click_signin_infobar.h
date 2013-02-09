0fc1799299f681437fc55556d8f570896d64bc05
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-09 06:09:43 +0000

    Add virtual to OneClickSigninInfobar destructor.
    Breaks my local build with the clang plugin.
    
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12223060
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181607 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/infobars/one_click_signin_infobar.h
+++ chrome/browser/ui/views/infobars/one_click_signin_infobar.h
@@ -17,7 +17,7 @@ class OneClickSigninInfoBar : public ConfirmInfoBar {
  public:
   OneClickSigninInfoBar(InfoBarService* owner,
                         OneClickSigninInfoBarDelegate* delegate);
-  ~OneClickSigninInfoBar();
+  virtual ~OneClickSigninInfoBar();
 
  private:
   // ConfirmInfoBar overrides.
