99290a9484c43d299b9c92ddbc84f75b949f42cc
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-10 16:39:16 +0000

    Unreviewed, rolling out r142347. http://trac.webkit.org/changeset/142347 https://bugs.webkit.org/show_bug.cgi?id=108273
    Because a depending change r142343 was rolled out.
    
    Source/WebKit2:
    
    * WebProcess/InjectedBundle/API/c/WKBundlePage.h:
    * WebProcess/InjectedBundle/InjectedBundlePageUIClient.cpp:
    (WebKit::InjectedBundlePageUIClient::plugInStartLabelTitle):
    (WebKit::InjectedBundlePageUIClient::plugInStartLabelSubtitle):
    
    Tools:
    
    * WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp:
    (WTR::InjectedBundlePage::InjectedBundlePage):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
+++ third_party/WebKit/Tools/WebKitTestRunner/InjectedBundle/InjectedBundlePage.cpp
@@ -369,6 +369,7 @@ InjectedBundlePage::InjectedBundlePage(WKBundlePageRef page)
         0, /*toolbarsAreVisible*/
         didReachApplicationCacheOriginQuota,
         didExceedDatabaseQuota,
+        0, /*plugInStartLabelImage*/
         0, /*plugInStartLabelTitle*/
         0, /*plugInStartLabelSubtitle*/
         0, /*plugInExtraStyleSheet*/
