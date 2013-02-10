c372e59c579b774c8a9b477aaf85b1f0f9809160
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-10 13:44:02 +0000

    RenderText: Access characters through m_text instead of caching data pointers separately. <http://webkit.org/b/109357>
    Reviewed by Antti Koivisto.
    
    Go through RenderText::m_text.impl() instead of caching the character data pointer.
    RenderText should never have a null String in m_text so it's safe to access impl() directly.
    We have assertions for this since before.
    
    Removing this pointer shrinks RenderText by 8 bytes, allowing it to fit into a snugger size class.
    749 KB progression on Membuster3.
    
    * rendering/RenderText.cpp:
    (SameSizeAsRenderText):
    (WebCore::RenderText::RenderText):
    (WebCore::RenderText::setTextInternal):
    * rendering/RenderText.h:
    (WebCore::RenderText::is8Bit):
    (WebCore::RenderText::characters8):
    (WebCore::RenderText::characters16):
    (WebCore::RenderText::characterAt):
    (WebCore::RenderText::operator[]):
    (RenderText):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142398 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderText.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderText.cpp
@@ -60,7 +60,7 @@ struct SameSizeAsRenderText : public RenderObject {
     uint32_t bitfields : 16;
     float widths[4];
     String text;
-    void* pointers[3];
+    void* pointers[2];
 };
 
 COMPILE_ASSERT(sizeof(RenderText) == sizeof(SameSizeAsRenderText), RenderText_should_stay_small);
@@ -157,11 +157,6 @@ RenderText::RenderText(Node* node, PassRefPtr<StringImpl> str)
     if (node && node->isDocumentNode())
         setDocumentForAnonymous(static_cast<Document*>(node));
 
-    m_is8Bit = m_text.is8Bit();
-    if (is8Bit())
-        m_data.characters8 = m_text.characters8();
-    else
-        m_data.characters16 = m_text.characters16();
     m_isAllASCII = m_text.containsOnlyASCII();
     m_canUseSimpleFontCodePath = computeCanUseSimpleFontCodePath();
     setIsText();
@@ -1428,11 +1423,6 @@ void RenderText::setTextInternal(PassRefPtr<StringImpl> text)
     ASSERT(m_text);
     ASSERT(!isBR() || (textLength() == 1 && m_text[0] == '\n'));
 
-    m_is8Bit = m_text.is8Bit();
-    if (is8Bit())
-        m_data.characters8 = m_text.characters8();
-    else
-        m_data.characters16 = m_text.characters16();
     m_isAllASCII = m_text.containsOnlyASCII();
     m_canUseSimpleFontCodePath = computeCanUseSimpleFontCodePath();
 }
