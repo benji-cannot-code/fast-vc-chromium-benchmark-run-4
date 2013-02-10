94305ed81323e20efc6560b63e4fd3fee5769c23
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-10 15:05:44 +0000

    [Chromium] Build fix for r142371 https://bugs.webkit.org/show_bug.cgi?id=109313
    * DumpRenderTree/chromium/TestRunner/src/EventSender.h:
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/EventSender.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/EventSender.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define EventSender_h
 
 #include "CppBoundClass.h"
+#include "WebContextMenuData.h"
 #include "WebDragOperation.h"
 #include "WebInputEvent.h"
 #include "WebTask.h"
@@ -48,7 +49,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 class WebDragData;
 class WebView;
-struct WebContextMenuData;
 }
 
 namespace WebTestRunner {
