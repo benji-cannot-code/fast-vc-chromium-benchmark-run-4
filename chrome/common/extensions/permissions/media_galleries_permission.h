ad161551091dab5f5b80c6c400ea0f1f0a7f1110
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-10 22:47:06 +0000

    Media Galleries: Disable write support.
    BUG=174986
    Review URL: https://codereview.chromium.org/12213076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181658 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/permissions/media_galleries_permission.h
+++ chrome/common/extensions/permissions/media_galleries_permission.h
@@ -16,7 +16,7 @@ namespace extensions {
 // Media Galleries permissions are as follows:
 //   <media-galleries-permission-pattern>
 //             := <access> | <access> 'allAutoDetected' | 'allAutoDetected'
-//   <access>  := 'read' | 'write'
+//   <access>  := 'read'
 class MediaGalleriesPermission
   : public SetDisjunctionPermission<MediaGalleriesPermissionData,
                                     MediaGalleriesPermission> {
@@ -41,7 +41,6 @@ class MediaGalleriesPermission
   // Permission strings.
   static const char kAllAutoDetectedPermission[];
   static const char kReadPermission[];
-  static const char kWritePermission[];
 };
 
 }  // namespace extensions
