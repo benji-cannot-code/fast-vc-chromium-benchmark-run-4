274ca6cfd8a57bc04a132df65ab45cf9735b2da2
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-11 11:49:43 +0000

    Web Inspector: highlight DOM nodes on hover while debugging https://bugs.webkit.org/show_bug.cgi?id=109355
    Reviewed by Vsevolod Vlasov.
    
    Along with showing the popover, highlight the remote object as node.
    
    * inspector/front-end/ObjectPopoverHelper.js:
    (WebInspector.ObjectPopoverHelper.prototype.):
    (WebInspector.ObjectPopoverHelper.prototype._showObjectPopover):
    (WebInspector.ObjectPopoverHelper.prototype._onHideObjectPopover):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142450 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ObjectPopoverHelper.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ObjectPopoverHelper.js
@@ -107,8 +107,9 @@ WebInspector.ObjectPopoverHelper.prototype = {
                     popoverContentElement.textContent = "\"" + popoverContentElement.textContent + "\"";
                 popover.show(popoverContentElement, anchorElement);
             } else {
+                if (result.subtype === "node")
+                    result.highlightAsDOMNode();
                 popoverContentElement = document.createElement("div");
-
                 this._titleElement = document.createElement("div");
                 this._titleElement.className = "source-frame-popover-title monospace";
                 this._titleElement.textContent = result.description;
@@ -135,6 +136,7 @@ WebInspector.ObjectPopoverHelper.prototype = {
 
     _onHideObjectPopover: function()
     {
+        WebInspector.domAgent.hideDOMNodeHighlight();
         if (this._linkifier) {
             this._linkifier.reset();
             delete this._linkifier;
