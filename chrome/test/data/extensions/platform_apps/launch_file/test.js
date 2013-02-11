469eb505442d2c90632c1a7bea634b41960bae60
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-11 17:53:37 +0000

    Try to fix a test that still checks for intents
    BUG=173194
    TBR=benwells
    
    Review URL: https://codereview.chromium.org/12213113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181712 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/launch_file/test.js
+++ chrome/test/data/extensions/platform_apps/launch_file/test.js
@@ -4,20 +4,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 chrome.app.runtime.onLaunched.addListener(function (launchData) {
-  // Test that there is a launchData.intent, it is set up proerly, and that the
-  // FileEntry in launchData.intent.data can be read.
+  // Test that the id and items fields in FileEntry can be read.
   chrome.test.runTests([
     function testFileHandler() {
-      // TODO(benwells): remove once we no longer support intents.
       chrome.test.assertFalse(!launchData, "No launchData");
-      chrome.test.assertFalse(!launchData.intent, "No launchData.intent");
-      chrome.test.assertEq(launchData.intent.action,
-          "http://webintents.org/view");
-      chrome.test.assertEq(launchData.intent.type,
-          "chrome-extension://fileentry");
-      chrome.test.assertFalse(!launchData.intent.data,
-          "No launchData.intent.data");
-
       chrome.test.assertEq(typeof launchData.id, 'string',
           "launchData.id not received");
       chrome.test.assertEq(launchData.items.length, 1);
