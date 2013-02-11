bab2fd27af1abd6dc0bb84c585262c022e051f35
mrowe@apple.com <mrowe@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-11 21:26:48 +0000

    Build fix.
    * platform/mac/PlatformSpeechSynthesizerMac.mm: Fix the case in the include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/mac/PlatformSpeechSynthesizerMac.mm
+++ third_party/WebKit/Source/WebCore/platform/mac/PlatformSpeechSynthesizerMac.mm
@@ -27,10 +27,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "PlatformSpeechSynthesizer.h"
 
-#include "PlatformSpeechSynthesisVoice.h"
 #include "PlatformSpeechSynthesisUtterance.h"
+#include "PlatformSpeechSynthesisVoice.h"
 #include <AppKit/NSSpeechSynthesizer.h>
-#include <WTF/RetainPtr.h>
+#include <wtf/RetainPtr.h>
 
 #if ENABLE(SPEECH_SYNTHESIS)
 
