469e06f2fc3ebc11a4d29212845060f9ca52b831
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-11 16:14:47 +0000

    [cros] Fix MobileConfigTest.Basic - use some valid future date for valid config
    BUG=167926
    TBR=antrim
    
    Review URL: https://codereview.chromium.org/12213112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181696 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/mobile_config_unittest.cc
+++ chrome/browser/chromeos/mobile_config_unittest.cc
@@ -33,7 +33,7 @@ const char kGoodMobileConfig[] =
     "        {\n"
     "          \"deal_id\" : \"0\",\n"
     "          \"locales\" : [ \"en-US\", ],\n"
-    "          \"expire_date\" : \"31/12/12 0:0\",\n"
+    "          \"expire_date\" : \"31/12/2099 0:0\",\n"
     "          \"notification_count\" : 1,\n"
     "          \"localized_content\" : {\n"
     "            \"en-US\" : {\n"
@@ -99,7 +99,7 @@ const char kLocalMobileConfig[] =
     "        {\n"
     "          \"deal_id\" : \"1\",\n"
     "          \"locales\" : [ \"en-GB\", ],\n"
-    "          \"expire_date\" : \"31/12/13 0:0\",\n"
+    "          \"expire_date\" : \"31/12/2099 0:0\",\n"
     "          \"notification_count\" : 2,\n"
     "          \"localized_content\" : {\n"
     "            \"en-GB\" : {\n"
@@ -124,8 +124,7 @@ const char kLocalMobileConfig[] =
 
 namespace chromeos {
 
-// This test crashes consistently with no obvious cause; see crbug.com/167926
-TEST(MobileConfigTest, DISABLED_Basic) {
+TEST(MobileConfigTest, Basic) {
   MobileConfig config(kGoodMobileConfig, "en-US");
   EXPECT_TRUE(config.IsReady());
 
@@ -149,7 +148,7 @@ TEST(MobileConfigTest, DISABLED_Basic) {
             deal->GetLocalizedString("en", "notification_text"));
 
   base::Time reference_time;
-  base::Time::FromString("31/12/12 0:00", &reference_time);
+  base::Time::FromString("31/12/2099 0:00", &reference_time);
   EXPECT_EQ(reference_time, deal->expire_date());
 
   const MobileConfig::LocaleConfig* locale_config;
@@ -234,7 +233,7 @@ TEST(MobileConfigTest, LocalConfig) {
   EXPECT_EQ("default_text from local.",
             deal->GetLocalizedString("en", "notification_text"));
   base::Time reference_time;
-  base::Time::FromString("31/12/13 0:00", &reference_time);
+  base::Time::FromString("31/12/2099 0:00", &reference_time);
   EXPECT_EQ(reference_time, deal->expire_date());
 
   // Now reload same global/local config files but with proper initial locale.
