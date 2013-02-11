c697fd32c23bf870e1a785af75627f8e4c911a60
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-11 18:01:01 +0000

    Web Inspector: [Extensions API] expose ExtensionServerClient to tests so tests use same port as extensions API https://bugs.webkit.org/show_bug.cgi?id=109443
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    Promote extensionServer var to the outer closure, so it may be accessed by platform-specific (or test) code.
    
    * inspector/front-end/ExtensionAPI.js:
    (buildExtensionAPIInjectedScript):
    
    LayoutTests:
    
    - replace additional message ports used for evaluating code in front-end with normal extension transport.
    
    * http/tests/inspector/extensions-test.js:
    (initialize_ExtensionsTest.window.buildPlatformExtensionAPI):
    (initialize_ExtensionsTest.InspectorTest._replyToExtension):
    (initialize_ExtensionsTest.onEvaluate):
    * http/tests/inspector/resources/extension-main.js:
    * inspector/extensions/extensions-audits.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142485 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ExtensionAPI.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ExtensionAPI.js
@@ -864,7 +864,8 @@ var Request = declareInterfaceClass(RequestImpl);
 var Resource = declareInterfaceClass(ResourceImpl);
 var Timeline = declareInterfaceClass(TimelineImpl);
 
-var extensionServer = new ExtensionServerClient();
+// Closure variable defined by the glue below.
+extensionServer = new ExtensionServerClient();
 
 return new InspectorExtensionAPI();
 }
@@ -883,6 +884,7 @@ function buildPlatformExtensionAPI(extensionInfo)
 function buildExtensionAPIInjectedScript(extensionInfo)
 {
     return "(function(injectedScriptHost, inspectedWindow, injectedScriptId){ " +
+        "var extensionServer;" +
         defineCommonExtensionSymbols.toString() + ";" +
         injectedExtensionAPI.toString() + ";" +
         buildPlatformExtensionAPI(extensionInfo) + ";" +
