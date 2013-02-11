d4c84914ff8c1abe0b40b16d519dad3527f49c26
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-11 14:43:33 +0000

    [GTK][Clang] Build errors in LocalizedStringsGtk.cpp https://bugs.webkit.org/show_bug.cgi?id=109418
    Reviewed by Philippe Normand.
    
    Use the C++ isfinite(float) and abs(float) (instead of fabsf(float))
    methods by including the WTF MathExtras.h header. Use a static cast to
    an integer type on the float return value of the abs(float) method call
    instead of the C-style cast.
    
    No new tests - no new functiolnality.
    
    * platform/gtk/LocalizedStringsGtk.cpp:
    (WebCore::localizedMediaTimeDescription):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142463 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/gtk/LocalizedStringsGtk.cpp
+++ third_party/WebKit/Source/WebCore/platform/gtk/LocalizedStringsGtk.cpp
@@ -35,12 +35,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/gobject/GOwnPtr.h>
 #include "IntSize.h"
 #include "NotImplemented.h"
+#include <wtf/MathExtras.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/WTFString.h>
 
 #include <glib/gi18n-lib.h>
 #include <gtk/gtk.h>
-#include <math.h>
 
 namespace WebCore {
 
@@ -627,7 +627,7 @@ String localizedMediaTimeDescription(float time)
     if (!isfinite(time))
         return String::fromUTF8(_("indefinite time"));
 
-    int seconds = (int)fabsf(time);
+    int seconds = static_cast<int>(abs(time));
     int days = seconds / (60 * 60 * 24);
     int hours = seconds / (60 * 60);
     int minutes = (seconds / 60) % 60;
