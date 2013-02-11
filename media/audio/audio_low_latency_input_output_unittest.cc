165ccda03a63d5b6894034662df5cf08c663646b
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-11 17:19:40 +0000

    Attempt to resolve crash in media::PCMWaveInAudioInputStream::WaveCallback.
    BUG=170798
    TEST=AudioLowLatencyInputOutputTest. FullDuplexDelayMeasurement where I forced usage of WaveIn on Windows 7. Repeated test 1000 times.
    
    Also did content_unittests.
    
    Review URL: https://codereview.chromium.org/12213109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181708 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_low_latency_input_output_unittest.cc
+++ media/audio/audio_low_latency_input_output_unittest.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #elif defined(OS_MACOSX)
 #include "media/audio/mac/audio_manager_mac.h"
 #elif defined(OS_WIN)
-#include "base/win/scoped_com_initializer.h"
 #include "media/audio/win/audio_manager_win.h"
 #include "media/audio/win/core_audio_util_win.h"
 #elif defined(OS_ANDROID)
@@ -332,9 +331,6 @@ class StreamWrapper {
 
   explicit StreamWrapper(AudioManager* audio_manager)
       :
-#if defined(OS_WIN)
-        com_init_(base::win::ScopedCOMInitializer::kMTA),
-#endif
         audio_manager_(audio_manager),
         format_(AudioParameters::AUDIO_PCM_LOW_LATENCY),
 #if defined(OS_ANDROID)
@@ -374,10 +370,6 @@ class StreamWrapper {
     EXPECT_TRUE(stream);
     return stream;
   }
-
-#if defined(OS_WIN)
-  base::win::ScopedCOMInitializer com_init_;
-#endif
 
   AudioManager* audio_manager_;
   AudioParameters::Format format_;
