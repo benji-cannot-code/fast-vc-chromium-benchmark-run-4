f59b9f11d249cbea78f93272a7b52a62748c814c
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 01:28:20 +0000

    Use page count from page preview. Page range is not enough for exact printed page calculation.
    TBR=gene
    
    Review URL: https://chromiumcodereview.appspot.com/12220093
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181808 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/print_job_constants.cc
+++ printing/print_job_constants.cc
@@ -94,6 +94,9 @@ const char kSettingMarginsCustom[] = "marginsCustom";
 // MarginType enum.
 const char kSettingMarginsType[] = "marginsType";
 
+// Number of pages to print.
+const char kSettingPreviewPageCount[] = "pageCount";
+
 // A page range.
 const char kSettingPageRange[] = "pageRange";
 
