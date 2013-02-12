a0afae567bceea7fbb1ec1898510518eec307734
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 06:34:18 +0000

    cc: Mark async texture uploads as completed from the upload thread.
    This reduces the latency between when an upload completes and when the
    client is notified.
    
    BUG=173802
    
    
    Review URL: https://chromiumcodereview.appspot.com/12213073
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181883 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gles2_cmd_decoder.cc
+++ gpu/command_buffer/service/gles2_cmd_decoder.cc
@@ -2720,6 +2720,9 @@ bool GLES2DecoderImpl::MakeCurrent() {
     return false;
   }
 
+  if (query_manager_.get())
+    query_manager_->ProcessPendingTransferQueries();
+
   // TODO(epenner): Is there a better place to do this? Transfers
   // can complete any time we yield the main thread. So we *must*
   // process transfers after any such yield, before resuming.
@@ -9116,6 +9119,8 @@ error::Error GLES2DecoderImpl::HandleEndQueryEXT(
   if (!query_manager_->EndQuery(state_.current_query, submit_count)) {
     return error::kOutOfBounds;
   }
+
+  query_manager_->ProcessPendingTransferQueries();
 
   state_.current_query = NULL;
   return error::kNoError;
