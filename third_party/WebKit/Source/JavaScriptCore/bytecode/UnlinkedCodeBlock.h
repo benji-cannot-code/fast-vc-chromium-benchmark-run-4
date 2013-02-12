046d74c3a5ed27e1b0715647bd87660d9c20debd
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-12 19:46:11 +0000

    Unreviewed, rolling out r142387. http://trac.webkit.org/changeset/142387 https://bugs.webkit.org/show_bug.cgi?id=109601
    caused all layout and jscore tests on windows to fail
    (Requested by kling on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-12
    
    * bytecode/UnlinkedCodeBlock.cpp:
    (JSC::UnlinkedCodeBlock::UnlinkedCodeBlock):
    * bytecode/UnlinkedCodeBlock.h:
    (UnlinkedCodeBlock):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/UnlinkedCodeBlock.h
@@ -487,6 +487,8 @@ private:
     RefCountedArray<UnlinkedInstruction> m_unlinkedInstructions;
 
     int m_numParameters;
+    JSGlobalData* m_globalData;
+
     int m_thisRegister;
     int m_argumentsRegister;
     int m_activationRegister;
@@ -497,21 +499,11 @@ private:
     bool m_isStrictMode : 1;
     bool m_isConstructor : 1;
     bool m_hasCapturedVariables : 1;
-    CodeType m_codeType : 2;
-    CodeFeatures m_features : 16;
-
     unsigned m_firstLine;
     unsigned m_lineCount;
 
-    unsigned m_resolveOperationCount;
-    unsigned m_putToBaseOperationCount;
-    unsigned m_arrayProfileCount;
-    unsigned m_arrayAllocationProfileCount;
-    unsigned m_objectAllocationProfileCount;
-    unsigned m_valueProfileCount;
-    unsigned m_llintCallLinkInfoCount;
-
-    JSGlobalData* m_globalData;
+    CodeFeatures m_features;
+    CodeType m_codeType;
 
     Vector<unsigned> m_jumpTargets;
 
@@ -532,6 +524,14 @@ private:
     Vector<Comment>  m_bytecodeComments;
     size_t m_bytecodeCommentIterator;
 #endif
+
+    unsigned m_resolveOperationCount;
+    unsigned m_putToBaseOperationCount;
+    unsigned m_arrayProfileCount;
+    unsigned m_arrayAllocationProfileCount;
+    unsigned m_objectAllocationProfileCount;
+    unsigned m_valueProfileCount;
+    unsigned m_llintCallLinkInfoCount;
 
 public:
     struct RareData {
