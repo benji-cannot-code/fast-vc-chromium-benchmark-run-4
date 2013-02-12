24bb1537b552609452f50906127701a7f819eb33
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 06:55:08 +0000

    This adds the onNetworkChanged event to networkingPrivate extension API.
    Trying to land this again after clang problem.  Original review was
    here: https://chromiumcodereview.appspot.com/12188027/
    
    (TBR'ing trivial change to profile_dependency_manager.cc)
    
    TBR=davemoore@chromium.org,kalman@chromium.org,pneubeck@chromium.org
    BUG=chromium:168713
    
    
    Review URL: https://chromiumcodereview.appspot.com/12225087
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181892 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/event_names.cc
+++ chrome/browser/extensions/event_names.cc
@@ -96,6 +96,9 @@ const char kOnNotificationClicked[] = "experimental.notification.onClicked";
 const char kOnNotificationButtonClicked[] =
     "experimental.notification.onButtonClicked";
 
+const char kOnNetworkChanged[] =
+    "networkingPrivate.onNetworkChanged";
+
 }  // namespace event_names
 
 }  // namespace extensions
--- chrome/browser/extensions/event_names.h
+++ chrome/browser/extensions/event_names.h
@@ -108,6 +108,9 @@ extern const char kOnNotificationClosed[];
 extern const char kOnNotificationClicked[];
 extern const char kOnNotificationButtonClicked[];
 
+// NetworkingPrivate
+extern const char kOnNetworkChanged[];
+
 }  // namespace event_names
 
 }  // namespace extensions
