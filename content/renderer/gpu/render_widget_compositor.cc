703fcb04a1907963de28007cf4b45b958a025d0b
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 01:09:39 +0000

    Fix compositorFrameMessage init order
    It's better to set values before testing them.
    
    BUG=147905
    
    Review URL: https://codereview.chromium.org/12248008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181805 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/render_widget_compositor.cc
+++ content/renderer/gpu/render_widget_compositor.cc
@@ -71,6 +71,9 @@ scoped_ptr<RenderWidgetCompositor> RenderWidgetCompositor::Create(
       settings.topControlsHeight = controls_height;
   }
 
+  settings.compositorFrameMessage =
+      cmd->HasSwitch(cc::switches::kEnableCompositorFrameMessage);
+
   if (settings.calculateTopControlsPosition &&
       (settings.topControlsHeight <= 0 || !settings.compositorFrameMessage)) {
     DCHECK(false) << "Top controls repositioning enabled without valid height "
@@ -78,8 +81,6 @@ scoped_ptr<RenderWidgetCompositor> RenderWidgetCompositor::Create(
     settings.calculateTopControlsPosition = false;
   }
 
-  settings.compositorFrameMessage =
-      cmd->HasSwitch(cc::switches::kEnableCompositorFrameMessage);
   settings.partialSwapEnabled =
       cmd->HasSwitch(cc::switches::kEnablePartialSwap);
   settings.backgroundColorInsteadOfCheckerboard =
