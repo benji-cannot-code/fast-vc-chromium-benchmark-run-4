72cb6e43f30f37a20df7a59078961d719f3882ae
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 23:07:12 +0000

    Disable AwContentsClientOnScaleChangedTest#testScaleUp
    BUG=175854
    TBR=benm@chromium.org
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/12211141
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182052 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnScaleChangedTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsClientOnScaleChangedTest.java
@@ -8,6 +8,7 @@ package org.chromium.android_webview.test;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.android_webview.AwContents;
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.content.browser.test.util.CallbackHelper;
 import org.chromium.android_webview.test.util.CommonResources;
@@ -55,7 +56,11 @@ public class AwContentsClientOnScaleChangedTest extends AndroidWebViewTestBase {
         super.tearDown();
     }
 
+    /*
     @SmallTest
+    This test is timing out on ICS bots including cq. See crbug.com/175854.
+    */
+    @DisabledTest
     public void testScaleUp() throws Throwable {
         getContentSettingsOnUiThread(mAwContents).setUseWideViewPort(true);
         loadDataSync(mAwContents, mContentsClient.getOnPageFinishedHelper(),
