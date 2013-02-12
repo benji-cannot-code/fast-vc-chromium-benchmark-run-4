441395d062ff4e7b7f084e26d5c65bb5aa0171c2
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 13:42:34 +0000

    [cleanup] Files.app: Remove Function declarations within blocks #3
    Use a variable initialized with a Function Expression instead.
    
    Function declarations within blocks are prohibited by the style guide.
    http://google-styleguide.googlecode.com/svn/trunk/javascriptguide.xml?showone=Function_Declarations_Within_Blocks#Function_Declarations_Within_Blocks
    
    In addition, they don't work in strict mode.
    
    BUG=174809
    TEST=presubmit passes, Files.app launched.
    
    Review URL: https://codereview.chromium.org/12212127
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181923 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1950,7 +1950,7 @@ DialogType.isModal = function(type) {
       this.refreshRemainingSpace_(true);  // Show loading caption.
 
     this.previousRootUrl_ = this.directoryModel_.getCurrentRootUrl();
-  },
+  };
 
   /**
    * Refreshes space info of the current volume.
@@ -1981,7 +1981,7 @@ DialogType.isModal = function(type) {
                           volumeSpaceInfoLabel,
                           volumeSpaceOuterBar);
         }.bind(this));
-  }
+  };
 
   /**
    * Update the UI when the current directory changes.
