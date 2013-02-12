0c7a91471bbb47d75d9aa5c029a2be2da84177de
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 06:34:51 +0000

    Fix ScreenCapturerMac handling of secondary display configurations.
    - Fix bugs in translating capture regions into display coordinates.
    - Fix MacDeskopConfiguration to support inverse-Cartesian coordinates.
    
    BUG=175261,174090
    
    
    Review URL: https://chromiumcodereview.appspot.com/12221103
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181885 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/screen/screen_capturer_mac.mm
+++ media/video/capture/screen/screen_capturer_mac.mm
@@ -587,8 +587,7 @@ void ScreenCapturerMac::CgBlitPreLion(const ScreenCaptureFrame& buffer,
       continue;
 
     // Translate the region to be copied into display-relative coordinates.
-    copy_region.translate(-desktop_config_.pixel_bounds.left(),
-                          -desktop_config_.pixel_bounds.top());
+    copy_region.translate(-display_bounds.left(), -display_bounds.top());
 
     // Calculate where in the output buffer the display's origin is.
     uint8* out_ptr = buffer.pixels() +
@@ -608,7 +607,7 @@ void ScreenCapturerMac::CgBlitPreLion(const ScreenCaptureFrame& buffer,
 }
 
 void ScreenCapturerMac::CgBlitPostLion(const ScreenCaptureFrame& buffer,
-                                           const SkRegion& region) {
+                                       const SkRegion& region) {
   const int buffer_height = buffer.dimensions().height();
 
   // Copy the entire contents of the previous capture buffer, to capture over.
@@ -634,8 +633,7 @@ void ScreenCapturerMac::CgBlitPostLion(const ScreenCaptureFrame& buffer,
       continue;
 
     // Translate the region to be copied into display-relative coordinates.
-    copy_region.translate(-desktop_config_.pixel_bounds.left(),
-                          -desktop_config_.pixel_bounds.top());
+    copy_region.translate(-display_bounds.left(), -display_bounds.top());
 
     // Create an image containing a snapshot of the display.
     base::mac::ScopedCFTypeRef<CGImageRef> image(
@@ -679,7 +677,8 @@ void ScreenCapturerMac::ScreenConfigurationChanged() {
   helper_.ClearInvalidRegion();
 
   // Refresh the cached desktop configuration.
-  desktop_config_ = MacDesktopConfiguration::GetCurrent();
+  desktop_config_ = MacDesktopConfiguration::GetCurrent(
+      MacDesktopConfiguration::TopLeftOrigin);
 
   // Re-mark the entire desktop as dirty.
   helper_.InvalidateScreen(
