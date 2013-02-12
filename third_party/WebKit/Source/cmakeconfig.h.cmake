7b7e0de10f0c6f0e8eb8c6e90d04abfb38f084a6
zandobersek@gmail.com <zandobersek@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-12 11:06:17 +0000

    Remove ENABLE_XHR_RESPONSE_BLOB handling from various build systems https://bugs.webkit.org/show_bug.cgi?id=109481
    Reviewed by Daniel Bates.
    
    The ENABLE_XHR_RESPONSE_BLOB feature define was removed from the code
    back in r120574. There are still occurrences of it in various build systems
    which should all be removed as they are useless.
    
    .: 
    
    * Source/cmake/OptionsBlackBerry.cmake:
    * Source/cmakeconfig.h.cmake:
    
    Source/WebKit/blackberry: 
    
    * WebCoreSupport/AboutDataEnableFeatures.in:
    
    Source/WebKit/chromium: 
    
    * features.gypi:
    
    Tools: 
    
    * Scripts/webkitperl/FeatureList.pm:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/cmakeconfig.h.cmake
+++ third_party/WebKit/Source/cmakeconfig.h.cmake
@@ -125,7 +125,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #cmakedefine01 ENABLE_WEB_SOCKETS
 #cmakedefine01 ENABLE_WEB_TIMING
 #cmakedefine01 ENABLE_WORKERS
-#cmakedefine01 ENABLE_XHR_RESPONSE_BLOB
 #cmakedefine01 ENABLE_XHR_TIMEOUT
 #cmakedefine01 ENABLE_XSLT
 #cmakedefine01 USE_SYSTEM_MALLOC
