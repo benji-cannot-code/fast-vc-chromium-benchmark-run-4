601056b0244069e3dbac8cbc7bb6aab898f402a0
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-12 05:13:50 +0000

    Alternate NTP: Fix bug where shadow is not removed
    Opening a link in a new tab from instant search results would cause the instant search result shadow to stay visible.
    
    The problem was that there were three ways the shadow could be hidden:
       (1) -showPreview:..:drawDropShadow:NO
       (2) - hidePreview
       (3) - onActivateTabWithContents:
    (1) and (2) correctly hid the shadow but (3) didn't.
    
    Fix was consolidate all the code into (1) and have everyone call that method.
    
    BUG=173778
    TEST=See comment 7 in bug 173778 for an easy way to reproduce this bug. Verified that without my fix the shadow is not removed and with my fix the shadow is removed.
    
    Review URL: https://codereview.chromium.org/12224052
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@181865 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tab_contents/previewable_contents_controller.mm
+++ chrome/browser/ui/cocoa/tab_contents/previewable_contents_controller.mm
@@ -51,12 +51,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [super dealloc];
 }
 
-- (void)showPreview:(content::WebContents*)preview
-             height:(CGFloat)height
-        heightUnits:(InstantSizeUnits)heightUnits
-     drawDropShadow:(BOOL)drawDropShadow {
-  DCHECK(preview);
-
+- (void)setPreview:(content::WebContents*)preview
+            height:(CGFloat)height
+       heightUnits:(InstantSizeUnits)heightUnits
+    drawDropShadow:(BOOL)drawDropShadow {
   // If drawing drop shadow, clip the bottom 1-px-thick separator out of
   // preview.
   // TODO(sail): remove this when GWS gives chrome the height without the
@@ -72,8 +70,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   }
 
   // Remove any old preview contents before showing the new one.
-  if (previewContents_)
+  if (previewContents_) {
     [previewContents_->GetNativeView() removeFromSuperview];
+    previewContents_->WasHidden();
+  }
 
   previewContents_ = preview;
   previewHeight_ = height;
@@ -81,9 +81,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   drawDropShadow_ = drawDropShadow;
 
   // Add the preview contents.
-  [[[self view] window] disableScreenUpdatesUntilFlush];
-  previewContents_->GetView()->SetAllowOverlappingViews(true);
-  [[self view] addSubview:previewContents_->GetNativeView()];
+  if (previewContents_) {
+    [[[self view] window] disableScreenUpdatesUntilFlush];
+    previewContents_->GetView()->SetAllowOverlappingViews(true);
+    [[self view] addSubview:previewContents_->GetNativeView()];
+  }
 
   if (drawDropShadow_) {
     if (!dropShadowView_) {
@@ -98,28 +100,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   [self layoutViews];
 
-  previewContents_->WasShown();
-}
-
-- (void)hidePreview {
-  // There may be no previewContents_ in the prerender case.
-  if (!previewContents_)
-    return;
-
-  // Remove the preview contents.
-  [previewContents_->GetNativeView() removeFromSuperview];
-  previewContents_->WasHidden();
-  previewContents_ = nil;
-
-  drawDropShadow_ = false;
-  [dropShadowView_ removeFromSuperview];
-  dropShadowView_.reset();
+  if (previewContents_)
+    previewContents_->WasShown();
 }
 
 - (void)onActivateTabWithContents:(content::WebContents*)contents {
   if (previewContents_ == contents) {
-    [previewContents_->GetNativeView() removeFromSuperview];
-    previewContents_ = nil;
+    if (previewContents_) {
+      [previewContents_->GetNativeView() removeFromSuperview];
+      previewContents_ = NULL;
+    }
+    [self setPreview:NULL
+              height:0
+         heightUnits:INSTANT_SIZE_PIXELS
+      drawDropShadow:NO];
   }
 }
 
