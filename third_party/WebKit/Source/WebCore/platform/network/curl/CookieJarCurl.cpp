ce4cd7c8bbe2e061450eae1e5aee8b9952119fef
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-12 04:34:21 +0000

    [Curl] setCookiesFromDOM function does not save cookies to disk. https://bugs.webkit.org/show_bug.cgi?id=109285
    Patch by peavo@outlook.com <peavo@outlook.com> on 2013-02-11
    Reviewed by Brent Fulgham.
    
    Write cookies to disk by using the Curl easy api.
    
    * platform/network/curl/CookieJarCurl.cpp:
    (WebCore::setCookiesFromDOM):Write cookie to disk.
    * platform/network/curl/ResourceHandleManager.cpp:
    (WebCore::ResourceHandleManager::getCurlShareHandle): Added method to get Curl share handle.
    (WebCore::ResourceHandleManager::getCookieJarFileName): Added method to get cookie file name.
    * platform/network/curl/ResourceHandleManager.h: Added methods to get cookie file name, and Curl share handle.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/curl/CookieJarCurl.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/curl/CookieJarCurl.cpp
@@ -20,6 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "Cookie.h"
 #include "KURL.h"
+#include "ResourceHandleManager.h"
+
 #include <wtf/HashMap.h>
 #include <wtf/text/StringHash.h>
 #include <wtf/text/WTFString.h>
@@ -31,6 +33,30 @@ static HashMap<String, String> cookieJar;
 void setCookiesFromDOM(const NetworkStorageSession&, const KURL&, const KURL& url, const String& value)
 {
     cookieJar.set(url.string(), value);
+
+    CURL* curl = curl_easy_init();
+
+    if (!curl)
+        return;
+
+    const char* cookieJarFileName = ResourceHandleManager::sharedInstance()->getCookieJarFileName();
+    CURLSH* curlsh = ResourceHandleManager::sharedInstance()->getCurlShareHandle();
+
+    curl_easy_setopt(curl, CURLOPT_COOKIEJAR, cookieJarFileName);
+    curl_easy_setopt(curl, CURLOPT_COOKIEFILE, cookieJarFileName);
+    curl_easy_setopt(curl, CURLOPT_SHARE, curlsh);
+
+    String cookie("Set-Cookie: ");
+    if (value.is8Bit())
+        cookie.append(value);
+    else
+        cookie.append(String::make8BitFrom16BitSource(value.characters16(), value.length()));
+
+    CString strCookie(reinterpret_cast<const char*>(cookie.characters8()), cookie.length());
+
+    curl_easy_setopt(curl, CURLOPT_COOKIELIST, strCookie.data());
+
+    curl_easy_cleanup(curl);
 }
 
 String cookiesForDOM(const NetworkStorageSession&, const KURL&, const KURL& url)
