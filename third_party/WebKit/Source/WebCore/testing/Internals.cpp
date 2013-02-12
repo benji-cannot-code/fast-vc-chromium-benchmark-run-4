7a7dc02d44f211bf6ac2eb02db8a881f5c2bbeaf
roger_fong@apple.com <roger_fong@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-12 20:56:06 +0000

    Unreviewed Windows build fix.
    * testing/Internals.cpp:
    (WebCore::Internals::resetToConsistentState):
    (WebCore::Internals::Internals):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.cpp
+++ third_party/WebKit/Source/WebCore/testing/Internals.cpp
@@ -260,7 +260,7 @@ void Internals::resetToConsistentState(Page* page)
     if (page->inspectorController())
         page->inspectorController()->setProfilerEnabled(false);
 #endif
-#if ENABLE(VIDEO_TRACK)
+#if ENABLE(VIDEO_TRACK) && !PLATFORM(WIN)
     page->group().captionPreferences()->setTestingMode(false);
 #endif
 }
@@ -268,7 +268,7 @@ void Internals::resetToConsistentState(Page* page)
 Internals::Internals(Document* document)
     : ContextDestructionObserver(document)
 {
-#if ENABLE(VIDEO_TRACK)
+#if ENABLE(VIDEO_TRACK) && !PLATFORM(WIN)
     if (document && document->page())
         document->page()->group().captionPreferences()->setTestingMode(true);
 #endif
