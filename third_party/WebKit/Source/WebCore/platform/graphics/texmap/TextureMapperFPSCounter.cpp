d5f8433dba766d4de1136cf4b76fb84df400bc6d
bruno.abinader@basyskom.com <bruno.abinader@basyskom.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-12 13:49:21 +0000

    [TexMap] Apply frames-per-second debug counter to WK1. https://bugs.webkit.org/show_bug.cgi?id=109540
    Reviewed by Noam Rosenthal.
    
    Adds basysKom copyright info to TextureMapperFPSCounter header.
    
    * platform/graphics/texmap/TextureMapperFPSCounter.cpp:
    * platform/graphics/texmap/TextureMapperFPSCounter.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142608 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperFPSCounter.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperFPSCounter.cpp
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
     Copyright (C) 2012 Nokia Corporation and/or its subsidiary(-ies)
     Copyright (C) 2012, 2013 Company 100, Inc.
+    Copyright (C) 2012, 2013 basysKom GmbH
 
     This library is free software; you can redistribute it and/or
     modify it under the terms of the GNU Library General Public
