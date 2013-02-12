c69a5b53b527de2dabfb2a68d0736d02a7ec970f
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-12 00:46:58 +0000

    Build fix after r142528 https://bugs.webkit.org/show_bug.cgi?id=109520
    Reviewed by Eric Seidel.
    
    r142528 changed GIFImageReader from a struct to a class.
    We also need to fix a forward declaration.
    
    No tests.
    
    * platform/image-decoders/gif/GIFImageDecoder.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142550 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/image-decoders/gif/GIFImageDecoder.h
+++ third_party/WebKit/Source/WebCore/platform/image-decoders/gif/GIFImageDecoder.h
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ImageDecoder.h"
 #include <wtf/OwnPtr.h>
 
-struct GIFImageReader;
+class GIFImageReader;
 
 namespace WebCore {
 
