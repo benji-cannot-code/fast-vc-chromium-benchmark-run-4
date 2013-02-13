538ebdc520862b471583858a8c331c221a324775
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 22:59:51 +0000

    Enhance net internals/net log output for QUIC.
    Review URL: https://chromiumcodereview.appspot.com/12207020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182331 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_client_session_test.cc
+++ net/quic/quic_client_session_test.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/stl_util.h"
+#include "net/base/capturing_net_log.h"
 #include "net/base/test_completion_callback.h"
 #include "net/quic/crypto/crypto_protocol.h"
 #include "net/quic/test_tools/quic_test_utils.h"
@@ -25,7 +26,7 @@ class QuicClientSessionTest : public ::testing::Test {
   QuicClientSessionTest()
       : guid_(1),
         connection_(new PacketSavingConnection(guid_, IPEndPoint())),
-        session_(connection_, NULL, NULL, kServerHostname) {
+        session_(connection_, NULL, NULL, kServerHostname, NULL) {
   }
 
   QuicGuid guid_;
