9b70df1a30f97d7c43ae23270e53378024664f07
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 19:10:51 +0000

    Browser Plugin: Navigating to the same URL after crash should load a new guest
    This patch contains a few other small changes in behavior:
    
    1. Removing the src attribute after navigation in <webview> will immediately restore it.
    2. browserPlugin.terminate() will not issue an IPC as long as the BrowserPlugin is in a crashed state.
    3. WebViewTest.Shim was misbehaving because it had lingering <webview>s in the DOM all in the same partition and so terminating one webview terminated them all, and caused multiple exit listeners to fire. This was fixed.
    4. Fixed a potential infinite loop that could happen if the src attribute is modified while in a crashed state.
    
    BUG=175206
    Test=WebViewTest.Shim: webViewAssignSrcAfterCrash, webViewRemoveSrcAttribute
    
    Review URL: https://chromiumcodereview.appspot.com/12224094
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182278 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/platform_apps/web_view/shim/main.js
+++ chrome/test/data/extensions/platform_apps/web_view/shim/main.js
@@ -10,13 +10,14 @@ var util = {};
 // sure that the <object> shim is created (asynchronously at this point) for the
 // <webview> tag. This makes the <webview> tag ready for add/removeEventListener
 // calls.
-util.createWebViewTagInDOM = function() {
+util.createWebViewTagInDOM = function(partitionName) {
   var webview = document.createElement('webview');
   webview.style.width = '300px';
   webview.style.height = '200px';
-  document.body.appendChild(webview);
   var urlDummy = 'data:text/html,<body>Initial dummy guest</body>';
   webview.setAttribute('src', urlDummy);
+  webview.setAttribute('partition', partitionName);
+  document.body.appendChild(webview);
   return webview;
 };
 
@@ -70,6 +71,7 @@ onload = function() {
         'terminate'
       ];
       var webview = document.createElement('webview');
+      webview.setAttribute('partition', arguments.callee.name);
       webview.addEventListener('loadstop', function(e) {
         for (var i = 0; i < apiMethodsToCheck.length; ++i) {
           chrome.test.assertEq('function',
@@ -80,7 +82,6 @@ onload = function() {
         chrome.test.assertEq('object', typeof webview.contentWindow);
         chrome.test.assertEq('function',
                              typeof webview.contentWindow.postMessage);
-
         chrome.test.succeed();
       });
       webview.setAttribute('src', 'data:text/html,webview check api');
@@ -89,8 +90,7 @@ onload = function() {
 
     function webViewEventName() {
       var webview = document.createElement('webview');
-      webview.setAttribute('src', 'data:text/html,webview check api');
-      document.body.appendChild(webview);
+      webview.setAttribute('partition', arguments.callee.name);
 
       webview.addEventListener('loadstart', function(evt) {
         chrome.test.assertEq('loadstart', evt.type);
@@ -107,6 +107,7 @@ onload = function() {
       });
 
       webview.setAttribute('src', 'data:text/html,trigger navigation');
+      document.body.appendChild(webview);
     },
 
     // This test registers two listeners on an event (loadcommit) and removes
@@ -114,7 +115,7 @@ onload = function() {
     // Current expected behavior is that the second event listener will still
     // fire without crashing.
     function webviewDestroyOnEventListener() {
-      var webview = util.createWebViewTagInDOM();
+      var webview = util.createWebViewTagInDOM(arguments.callee.name);
       var url = 'data:text/html,<body>Destroy test</body>';
 
       var loadCommitCount = 0;
@@ -124,8 +125,6 @@ onload = function() {
           return;
         ++loadCommitCount;
         if (loadCommitCount == 1) {
-          webview.parentNode.removeChild(webview);
-          webview = null;
           setTimeout(function() {
             chrome.test.succeed();
           }, 0);
@@ -136,6 +135,7 @@ onload = function() {
 
       // The test starts from here, by setting the src to |url|.
       webview.addEventListener('loadcommit', function(e) {
+        webview.parentNode.removeChild(webview);
         loadCommitCommon(e);
       });
       webview.addEventListener('loadcommit', function(e) {
@@ -148,7 +148,7 @@ onload = function() {
     // Each of the listener tries to change some properties on the event param,
     // which should not be possible.
     function cannotMutateEventName() {
-      var webview = util.createWebViewTagInDOM();
+      var webview = util.createWebViewTagInDOM(arguments.callee.name);
       var url = 'data:text/html,<body>Two</body>';
 
       var loadCommitACalled = false;
@@ -196,6 +196,7 @@ onload = function() {
     // been set raises an exception.
     function partitionRaisesException() {
       var webview = document.createElement('webview');
+      webview.setAttribute('partition', arguments.callee.name);
       webview.setAttribute('src', 'data:text/html,trigger navigation');
       document.body.appendChild(webview);
       setTimeout(function() {
@@ -210,6 +211,7 @@ onload = function() {
 
     function webViewExecuteScript() {
       var webview = document.createElement('webview');
+      webview.setAttribute('partition', arguments.callee.name);
       webview.addEventListener('loadstop', function() {
         webview.executeScript(
           {code:'document.body.style.backgroundColor = "red";'},
@@ -226,11 +228,8 @@ onload = function() {
     // This test calls terminate() on guest after it has already been
     // terminated. This makes sure we ignore the call gracefully.
     function webViewTerminateAfterExitDoesntCrash() {
-      var webview = document.querySelector('webview');
-      webview.addEventListener('loadstart', function(evt) {
-        chrome.test.assertEq('loadstart', evt.type);
-      });
-
+      var webview = document.createElement('webview');
+      webview.setAttribute('partition', arguments.callee.name);
       var loadstopSucceedsTest = false;
       webview.addEventListener('loadstop', function(evt) {
         chrome.test.assertEq('loadstop', evt.type);
@@ -256,6 +255,47 @@ onload = function() {
       });
 
       webview.setAttribute('src', 'data:text/html,test terminate() crash.');
+      document.body.appendChild(webview);
+    },
+
+    // This test verifies that assigning the src attribute the same value it had
+    // prior to a crash spawns off a new guest process.
+    function webViewAssignSrcAfterCrash() {
+      var webview = document.createElement('webview');
+      webview.setAttribute('partition', arguments.callee.name);
+      var terminated = false;
+      webview.addEventListener('loadstop', function(evt) {
+        if (!terminated) {
+          webview.terminate();
+          return;
+        }
+        // The guest has recovered after being terminated.
+        chrome.test.succeed();
+      });
+      webview.addEventListener('exit', function(evt) {
+        terminated = true;
+        webview.setAttribute('src', 'data:text/html,test page');
+      });
+      webview.setAttribute('src', 'data:text/html,test page');
+      document.body.appendChild(webview);
+    },
+
+    // This test verifies that <webview> restores the src attribute if it is
+    // removed after navigation.
+    function webViewRemoveSrcAttribute() {
+      var dataUrl = 'data:text/html,test page';
+      var webview = document.createElement('webview');
+      webview.setAttribute('partition', arguments.callee.name);
+      var terminated = false;
+      webview.addEventListener('loadstop', function(evt) {
+        webview.removeAttribute('src');
+        setTimeout(function() {
+          chrome.test.assertEq(dataUrl, webview.getAttribute('src'));
+          chrome.test.succeed();
+        }, 0);
+      });
+      webview.setAttribute('src', dataUrl);
+      document.body.appendChild(webview);
     }
   ]);
 };
