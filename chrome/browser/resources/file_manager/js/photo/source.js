c7f4625013c4572b15f85fddd701efa18a0c0695
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 15:23:09 +0000

    [cleanup] Files.app: Remove a doc for non-existing parameter.
    BUG=175657
    TEST=none
    TBR=mtomasz@chromium.org, haruki@chromium.org
    
    Review URL: https://codereview.chromium.org/12257008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182221 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/photo/source.js
+++ chrome/browser/resources/file_manager/js/photo/source.js
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @constructor
  * @param {PhotoSource} source The parent source.
  * @param {string} name The album name.
- * @param {Array.<FileEntry>} entries The entries list.
  */
 function Album(source, name) {
   this.source_ = source;
