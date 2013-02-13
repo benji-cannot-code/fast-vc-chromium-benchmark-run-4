fbfec348eb768cd3651c729e4af15e338ae3af3e
yosin@chromium.org <yosin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-13 03:59:50 +0000

    Unreviewed. Build fix for Chromium-Win. Add #include <functional> for std::bind1st.
    * tests/PrerenderingTest.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/PrerenderingTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/PrerenderingTest.cpp
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebView.h"
 #include "WebViewClient.h"
 
+#include <functional>
 #include <gtest/gtest.h>
 #include <list>
 #include <public/Platform.h>
