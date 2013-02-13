0a0edef3e15f26aa11a7160de4df421e715ed6be
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 11:52:56 +0000

    Fix issue 174250, crash when reloading packaged app.
    When a shell window was closed, it wasn't removed from the registry immediately
    (it was removed in a callback from OnNativeClose, which happens a bit later.)
    This meant that when the app was loaded again, the window was still in the
    registry with a stale pointer to the old extension. ExtensionSettingsHandler
    was tickling this by iterating shell windows on extension load.
    
    BUG=174250
    
    
    Review URL: https://chromiumcodereview.appspot.com/12210107
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182194 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
+++ chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/gtk/extensions/native_app_window_gtk.h"
 
+#include "base/message_loop.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/gtk/extensions/extension_keybinding_registry_gtk.h"
@@ -185,12 +186,15 @@ void NativeAppWindowGtk::Close() {
   // To help catch bugs in any event handlers that might get fired during the
   // destruction, set window_ to NULL before any handlers will run.
   window_ = NULL;
-
-  // OnNativeClose does a delete this so no other members should
-  // be accessed after. gtk_widget_destroy is safe (and must
-  // be last).
-  shell_window_->OnNativeClose();
   gtk_widget_destroy(window);
+
+  // On other platforms, the native window doesn't get destroyed synchronously.
+  // We simulate that here so that ShellWindow can assume that it doesn't get
+  // deleted immediately upon calling Close().
+  MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&ShellWindow::OnNativeClose,
+                 base::Unretained(shell_window_)));
 }
 
 void NativeAppWindowGtk::Activate() {
