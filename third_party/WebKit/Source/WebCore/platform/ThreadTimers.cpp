1616220827f00abeb64914c7808dcaf8c07be195
antti@apple.com <antti@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-13 23:09:34 +0000

    Don't restart shared timer if both the current and the new fire time are in the past https://bugs.webkit.org/show_bug.cgi?id=109731
    Reviewed by Andreas Kling.
    
    In 40-50% of cases we reschedule the shared timer both the old and the new fire time have already passed. This can happen at least when rescheduling 
    a zero duration timer and when stopping a timer that was ready to fire.
    
    We can skip rescheduling in this case, the shared timer will fire immediately anyway.
            
    Scheduling timers calls into platform layer and can be slow. This about halves the time under setSharedTimerFireInterval in PLT3
    for ~0.1% total CPU time reduction.
    
    * platform/ThreadTimers.cpp:
    (WebCore::ThreadTimers::ThreadTimers):
    (WebCore::ThreadTimers::setSharedTimer):
    (WebCore::ThreadTimers::updateSharedTimer):
    (WebCore::ThreadTimers::sharedTimerFiredInternal):
    * platform/ThreadTimers.h:
    (ThreadTimers):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142811 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/ThreadTimers.cpp
+++ third_party/WebKit/Source/WebCore/platform/ThreadTimers.cpp
@@ -55,6 +55,7 @@ static MainThreadSharedTimer* mainThreadSharedTimer()
 ThreadTimers::ThreadTimers()
     : m_sharedTimer(0)
     , m_firingTimers(false)
+    , m_pendingSharedTimerFireTime(0)
 {
     if (isMainThread())
         setSharedTimer(mainThreadSharedTimer());
@@ -67,6 +68,7 @@ void ThreadTimers::setSharedTimer(SharedTimer* sharedTimer)
     if (m_sharedTimer) {
         m_sharedTimer->setFiredFunction(0);
         m_sharedTimer->stop();
+        m_pendingSharedTimerFireTime = 0;
     }
     
     m_sharedTimer = sharedTimer;
@@ -82,10 +84,20 @@ void ThreadTimers::updateSharedTimer()
     if (!m_sharedTimer)
         return;
         
-    if (m_firingTimers || m_timerHeap.isEmpty())
+    if (m_firingTimers || m_timerHeap.isEmpty()) {
+        m_pendingSharedTimerFireTime = 0;
         m_sharedTimer->stop();
-    else
-        m_sharedTimer->setFireInterval(max(m_timerHeap.first()->m_nextFireTime - monotonicallyIncreasingTime(), 0.0));
+    } else {
+        double nextFireTime = m_timerHeap.first()->m_nextFireTime;
+        double currentMonotonicTime = monotonicallyIncreasingTime();
+        if (m_pendingSharedTimerFireTime) {
+            // No need to restart the timer if both the pending fire time and the new fire time are in the past.
+            if (m_pendingSharedTimerFireTime <= currentMonotonicTime && nextFireTime <= currentMonotonicTime)
+                return;
+        } 
+        m_pendingSharedTimerFireTime = nextFireTime;
+        m_sharedTimer->setFireInterval(max(nextFireTime - currentMonotonicTime, 0.0));
+    }
 }
 
 void ThreadTimers::sharedTimerFired()
@@ -100,6 +112,7 @@ void ThreadTimers::sharedTimerFiredInternal()
     if (m_firingTimers)
         return;
     m_firingTimers = true;
+    m_pendingSharedTimerFireTime = 0;
 
     double fireTime = monotonicallyIncreasingTime();
     double timeToQuit = fireTime + maxDurationOfFiringTimers;
