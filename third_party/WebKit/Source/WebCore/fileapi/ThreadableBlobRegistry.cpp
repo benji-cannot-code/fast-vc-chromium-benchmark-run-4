0b973c0afae63e9e5032677cfb5795ab8de6a7dc
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-13 22:39:03 +0000

    Fix !ENABLE(BLOB) build.
            Reviewed by NOBODY (OOPS!).
    
            * fileapi/ThreadableBlobRegistry.cpp:
            (WebCore::ThreadableBlobRegistry::getCachedOrigin):
            * page/SecurityOrigin.cpp:
            (WebCore::getCachedOrigin):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142801 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/fileapi/ThreadableBlobRegistry.cpp
+++ third_party/WebKit/Source/WebCore/fileapi/ThreadableBlobRegistry.cpp
@@ -156,7 +156,7 @@ void ThreadableBlobRegistry::unregisterBlobURL(const KURL&)
 {
 }
 
-PassRefPtr<SecurityOrigin> ThreadableBlobRegistry::getCachedOrigin(const KURL& url)
+PassRefPtr<SecurityOrigin> ThreadableBlobRegistry::getCachedOrigin(const KURL&)
 {
     return 0;
 }
