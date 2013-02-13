d949cfa8534b85876a90234470394677c7c20c70
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-13 21:36:34 +0000

    Crash when encountering <object style="resize:both;"> https://bugs.webkit.org/show_bug.cgi?id=109728
    Source/WebCore:
    
    See also https://code.google.com/p/chromium/issues/detail?id=175535
    This bug can be reproduced on
    http://dramalink.net/tudou.y/?xink=162601060
    
    Patch by Christian Biesinger <cbiesinger@chromium.org> on 2013-02-13
    Reviewed by Eric Seidel.
    
    Test: fast/css/resize-object-crash.html
    
    * rendering/RenderWidget.cpp:
    (WebCore::RenderWidget::paint):
    Only call paintResizer() if we have a layer and canResize() is true
    
    LayoutTests:
    
    See also https://code.google.com/p/chromium/issues/detail?id=175535
    
    Patch by Christian Biesinger <cbiesinger@chromium.org> on 2013-02-13
    Reviewed by Eric Seidel.
    
    * fast/css/resize-object-crash-expected.txt: Added.
    * fast/css/resize-object-crash.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142788 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderWidget.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderWidget.cpp
@@ -313,7 +313,7 @@ void RenderWidget::paint(PaintInfo& paintInfo, const LayoutPoint& paintOffset)
         paintInfo.context->fillRect(pixelSnappedIntRect(selectionRect()), selectionBackgroundColor(), style()->colorSpace());
     }
 
-    if (style()->resize() != RESIZE_NONE)
+    if (hasLayer() && layer()->canResize())
         layer()->paintResizer(paintInfo.context, roundedIntPoint(adjustedPaintOffset), paintInfo.rect);
 }
 
