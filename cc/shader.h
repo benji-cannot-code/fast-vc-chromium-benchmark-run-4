b4cf4bd15671684c6f2b4b3e989f5c789476853d
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 06:06:25 +0000

    Revert "cc: Clamp texture coordinates in all tile shaders"
    This reverts commit 55e4b3b013a4b7080323676cd8336de036e1677d.
    
    BUG=173804
    
    
    Review URL: https://chromiumcodereview.appspot.com/12208140
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182129 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/shader.h
+++ cc/shader.h
@@ -143,22 +143,6 @@ private:
     int m_alphaLocation;
 };
 
-class FragmentTexClampAlphaBinding {
-public:
-    FragmentTexClampAlphaBinding();
-
-    void init(WebKit::WebGraphicsContext3D*, unsigned program, bool usingBindUniform, int* baseUniformIndex);
-    int alphaLocation() const { return m_alphaLocation; }
-    int edgeLocation() const { return -1; }
-    int fragmentTexTransformLocation() const { return m_fragmentTexTransformLocation; }
-    int samplerLocation() const { return m_samplerLocation; }
-
-private:
-    int m_samplerLocation;
-    int m_alphaLocation;
-    int m_fragmentTexTransformLocation;
-};
-
 class FragmentTexOpaqueBinding {
 public:
     FragmentTexOpaqueBinding();
@@ -171,21 +155,6 @@ public:
 
 private:
     int m_samplerLocation;
-};
-
-class FragmentTexClampOpaqueBinding {
-public:
-    FragmentTexClampOpaqueBinding();
-
-    void init(WebKit::WebGraphicsContext3D*, unsigned program, bool usingBindUniform, int* baseUniformIndex);
-    int alphaLocation() const { return -1; }
-    int edgeLocation() const { return -1; }
-    int fragmentTexTransformLocation() const { return m_fragmentTexTransformLocation; }
-    int samplerLocation() const { return m_samplerLocation; }
-
-private:
-    int m_samplerLocation;
-    int m_fragmentTexTransformLocation;
 };
 
 class FragmentShaderRGBATexFlipVaryingAlpha : public FragmentTexOpaqueBinding {
@@ -203,11 +172,6 @@ public:
     std::string getShaderString() const;
 };
 
-class FragmentShaderRGBATexClampAlpha : public FragmentTexClampAlphaBinding {
-public:
-    std::string getShaderString() const;
-};
-
 class FragmentShaderRGBATexRectFlipVaryingAlpha : public FragmentTexAlphaBinding {
 public:
     std::string getShaderString() const;
@@ -218,7 +182,7 @@ public:
     std::string getShaderString() const;
 };
 
-class FragmentShaderRGBATexClampOpaque : public FragmentTexClampOpaqueBinding {
+class FragmentShaderRGBATexOpaque : public FragmentTexOpaqueBinding {
 public:
     std::string getShaderString() const;
 };
@@ -229,13 +193,13 @@ public:
 };
 
 // Swizzles the red and blue component of sampled texel with alpha.
-class FragmentShaderRGBATexClampSwizzleAlpha : public FragmentTexClampAlphaBinding {
+class FragmentShaderRGBATexSwizzleAlpha : public FragmentTexAlphaBinding {
 public:
     std::string getShaderString() const;
 };
 
 // Swizzles the red and blue component of sampled texel without alpha.
-class FragmentShaderRGBATexClampSwizzleOpaque : public FragmentTexClampOpaqueBinding {
+class FragmentShaderRGBATexSwizzleOpaque : public FragmentTexOpaqueBinding {
 public:
     std::string getShaderString() const;
 };
