ed75b6f6ea804a4af0df342dc803f303564e7d70
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 09:56:19 +0000

    [win] Remove app launcher install calls from app shortcut manager.
    The launcher install is now done by the webstore, so this code is no longer
    needed.
    
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/12208146
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182179 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/app_shortcut_manager.cc
+++ chrome/browser/extensions/app_shortcut_manager.cc
@@ -23,11 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "skia/ext/image_operations.h"
 #include "ui/base/resource/resource_bundle.h"
 
-#if defined(OS_WIN)
-#include "chrome/browser/extensions/app_host_installer_win.h"
-#include "chrome/installer/util/browser_distribution.h"
-#endif
-
 namespace extensions {
 
 AppShortcutManager::AppShortcutManager(Profile* profile)
@@ -51,20 +46,8 @@ void AppShortcutManager::Observe(int type,
           details).ptr();
       if (extension->is_platform_app() &&
           extension->location() != Manifest::COMPONENT) {
-#if defined(OS_WIN)
-        if (BrowserDistribution::GetDistribution()->AppHostIsSupported() &&
-            extensions::AppHostInstaller::GetInstallWithLauncher()) {
-          scoped_refptr<Extension> extension_ref(const_cast<Extension*>(
-              extension));
-          extensions::AppHostInstaller::EnsureAppHostInstalled(
-              base::Bind(&AppShortcutManager::OnAppHostInstallationComplete,
-                         weak_factory_.GetWeakPtr(), extension_ref));
-        } else {
-          UpdateApplicationShortcuts(extension);
-        }
-#else
-        UpdateApplicationShortcuts(extension);
-#endif  // defined(OS_WIN)
+        web_app::UpdateShortcutInfoAndIconForApp(*extension, profile_,
+          base::Bind(&web_app::UpdateAllShortcuts));
       }
 #endif  // !defined(OS_MACOSX)
       break;
@@ -78,24 +61,6 @@ void AppShortcutManager::Observe(int type,
     default:
       NOTREACHED();
   }
-}
-
-#if defined(OS_WIN)
-void AppShortcutManager::OnAppHostInstallationComplete(
-    scoped_refptr<Extension> extension, bool app_host_install_success) {
-  if (!app_host_install_success) {
-    // Do not create shortcuts if App Host fails to install.
-    LOG(ERROR) << "Application Runtime installation failed.";
-    return;
-  }
-  UpdateApplicationShortcuts(extension);
-}
-#endif
-
-void AppShortcutManager::UpdateApplicationShortcuts(
-    const Extension* extension) {
-  web_app::UpdateShortcutInfoAndIconForApp(*extension, profile_,
-      base::Bind(&web_app::UpdateAllShortcuts));
 }
 
 void AppShortcutManager::DeleteApplicationShortcuts(
