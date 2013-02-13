45743704d1415b2d2ab398a716ee11d00e3e15c9
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-13 19:18:43 +0000

    Make SecItemShimProxy be a WorkQueueMessageReceiver https://bugs.webkit.org/show_bug.cgi?id=109719
    Reviewed by Sam Weinig.
    
    This adds a WantsConnection message attribute to be used for messages whose handlers
    should take the connection the message was delivered to.
    
    * Platform/CoreIPC/HandleMessage.h:
    (CoreIPC::handleMessage):
    Add new handleMessage overload.
    
    * Scripts/webkit2/messages.py:
    (async_message_statement):
    (generate_message_handler):
    Handle the WantsMessage attribute.
    
    * UIProcess/mac/SecItemShimProxy.cpp:
    (WebKit::SecItemShimProxy::shared):
    Use dispatch_once and adoptRef.
    
    (WebKit::SecItemShimProxy::SecItemShimProxy):
    Initialize the queue.
    
    (WebKit::SecItemShimProxy::initializeConnection):
    Add the proxy as a work queue message receiver.
    
    (WebKit::SecItemShimProxy::secItemRequest):
    This no longer needs to call out to a dispatch queue, it's already on a queue.
    
    * UIProcess/mac/SecItemShimProxy.messages.in:
    This doesn't need to be a legacy receiver. Also, add the WantsConnection message.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142763 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Scripts/webkit2/messages.py
+++ third_party/WebKit/Source/WebKit2/Scripts/webkit2/messages.py
@@ -25,6 +25,7 @@ import collections
 import re
 from webkit2 import parser
 
+WANTS_CONNECTION_ATTRIBUTE = 'WantsConnection'
 LEGACY_RECEIVER_ATTRIBUTE = 'LegacyReceiver'
 DELAYED_ATTRIBUTE = 'Delayed'
 DISPATCH_ON_CONNECTION_QUEUE_ATTRIBUTE = 'DispatchOnConnectionQueue'
@@ -316,9 +317,13 @@ def connection_work_queue_message_statement(receiver, message):
 
 def async_message_statement(receiver, message):
     dispatch_function_args = ['decoder', 'this', '&%s' % handler_function(receiver, message)]
+
     dispatch_function = 'handleMessage'
     if message.has_attribute(VARIADIC_ATTRIBUTE):
         dispatch_function += 'Variadic'
+
+    if message.has_attribute(WANTS_CONNECTION_ATTRIBUTE):
+        dispatch_function_args.insert(0, 'connection')
 
     result = []
     result.append('    if (decoder.messageName() == Messages::%s::%s::name()) {\n' % (receiver.name, message.name))
@@ -553,7 +558,7 @@ def generate_message_handler(file):
         if receiver.has_attribute(LEGACY_RECEIVER_ATTRIBUTE):
             result.append('void %s::didReceive%sMessage(CoreIPC::Connection*, CoreIPC::MessageDecoder& decoder)\n' % (receiver.name, receiver.name))
         else:
-            result.append('void %s::didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageDecoder& decoder)\n' % (receiver.name))
+            result.append('void %s::didReceiveMessage(CoreIPC::Connection* connection, CoreIPC::MessageDecoder& decoder)\n' % (receiver.name))
 
         result.append('{\n')
         result += [async_message_statement(receiver, message) for message in async_messages]
