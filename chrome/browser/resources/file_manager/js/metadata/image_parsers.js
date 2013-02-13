6cafd65543a743ecc121f94c967b08faf62353ba
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-13 16:51:05 +0000

    Files.app: Add missing JSdoc for member methods/variables #2.
    No semantics are changed.
    
    BUG=175657
    TEST=Files.app launches.
    TBR=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/12254008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/metadata/image_parsers.js
+++ chrome/browser/resources/file_manager/js/metadata/image_parsers.js
@@ -12,6 +12,12 @@ function SimpleImageParser(parent, type, urlFilter, headerSize) {
 
 SimpleImageParser.prototype = {__proto__: ImageParser.prototype};
 
+/**
+ * @param {File} file  //TODO(JSDOC).
+ * @param {Object} metadata  //TODO(JSDOC).
+ * @param {function(Object)} callback  //TODO(JSDOC).
+ * @param {function(string)} errorCallback  //TODO(JSDOC).
+ */
 SimpleImageParser.prototype.parse = function(
     file, metadata, callback, errorCallback) {
   var self = this;
@@ -34,6 +40,10 @@ function PngParser(parent) {
 
 PngParser.prototype = {__proto__: SimpleImageParser.prototype};
 
+/**
+ * @param {Object} metadata  //TODO(JSDOC).
+ * @param {ByteReader} br  //TODO(JSDOC).
+ */
 PngParser.prototype.parseHeader = function(metadata, br) {
   br.setByteOrder(ByteReader.BIG_ENDIAN);
 
@@ -59,6 +69,10 @@ function BmpParser(parent) {
 
 BmpParser.prototype = {__proto__: SimpleImageParser.prototype};
 
+/**
+ * @param {Object} metadata  //TODO(JSDOC).
+ * @param {ByteReader} br  //TODO(JSDOC).
+ */
 BmpParser.prototype.parseHeader = function(metadata, br) {
   br.setByteOrder(ByteReader.LITTLE_ENDIAN);
 
@@ -80,6 +94,10 @@ function GifParser(parent) {
 
 GifParser.prototype = {__proto__: SimpleImageParser.prototype};
 
+/**
+ * @param {Object} metadata  //TODO(JSDOC).
+ * @param {ByteReader} br  //TODO(JSDOC).
+ */
 GifParser.prototype.parseHeader = function(metadata, br) {
   br.setByteOrder(ByteReader.LITTLE_ENDIAN);
 
