649a49b3777502d58c7a6723fbf18b8032f9242b
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 06:16:30 +0000

    [cc] A few tracing fixes
    Resolutions AsValue did not recognize NON_IDEAL_RESOLUTION
    
    Call rate of PictureLayerTiling::UpdateTilePriorities is very high. Dont trace it.
    
    R=ccameron
    BUG=176150
    
    Review URL: https://codereview.chromium.org/12253021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182398 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/tile_priority.cc
+++ cc/tile_priority.cc
@@ -81,6 +81,9 @@ scoped_ptr<base::Value> TileResolutionAsValue(
   case HIGH_RESOLUTION:
     return scoped_ptr<base::Value>(base::Value::CreateStringValue(
         "HIGH_RESOLUTION"));
+  case NON_IDEAL_RESOLUTION:
+      return scoped_ptr<base::Value>(base::Value::CreateStringValue(
+        "NON_IDEAL_RESOLUTION"));
   default:
     DCHECK(false) << "Unrecognized TileResolution value";
     return scoped_ptr<base::Value>(base::Value::CreateStringValue(
