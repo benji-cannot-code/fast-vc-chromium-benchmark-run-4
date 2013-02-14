d3216dac200ba1783294a2aba874dc9df79598b0
sergio@webkit.org <sergio@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-14 12:35:09 +0000

    Add logging support to IndexedDB for non-Chromium platforms https://bugs.webkit.org/show_bug.cgi?id=109809
    Reviewed by Kentaro Hara.
    
    Enable logging of IndexedDB through the StorageAPI log channel for
    non-Chromium architectures.
    
    No new tests required, we're just enabling logging for IndexedDB
    using the currently available logging framework.
    
    * Modules/indexeddb/IDBTracing.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142868 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBTracing.h
+++ third_party/WebKit/Source/WebCore/Modules/indexeddb/IDBTracing.h
@@ -39,7 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #else
 
-#define IDB_TRACE(a) ((void)0)
+#include "Logging.h"
+#define IDB_TRACE(a) LOG(StorageAPI, a)
 
 #endif //  PLATFORM(CHROMIUM)
 
