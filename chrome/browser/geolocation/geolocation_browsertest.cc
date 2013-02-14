8daed82de3022b761e07b9361690ee004c02a41a
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 17:01:55 +0000

    Disabling flaky GeolocationBrowserTest.* tests.
    BUG=176291
    TBR=bulach
    
    Review URL: https://codereview.chromium.org/12253038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182480 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/geolocation/geolocation_browsertest.cc
+++ chrome/browser/geolocation/geolocation_browsertest.cc
@@ -462,8 +462,9 @@ IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, NoInfobarForOffTheRecord) {
   CheckGeoposition(fake_latitude_, fake_longitude_);
 }
 
+// crbug.com/176291
 IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest,
-                       IFramesWithFreshPosition) {
+                       DISABLED_IFramesWithFreshPosition) {
   html_for_tests_ = "files/geolocation/iframes_different_origin.html";
   ASSERT_TRUE(Initialize(INITIALIZATION_IFRAMES));
   LoadIFrames(2);
@@ -545,7 +546,9 @@ IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest,
   CheckGeoposition(cached_position_latitude, cached_position_lognitude);
 }
 
-IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, CancelPermissionForFrame) {
+// crbug.com/176291
+IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest,
+                       DISABLED_CancelPermissionForFrame) {
   html_for_tests_ = "files/geolocation/iframes_different_origin.html";
   ASSERT_TRUE(Initialize(INITIALIZATION_IFRAMES));
   LoadIFrames(2);
@@ -640,7 +643,8 @@ IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, TwoWatchesInOneFrame) {
   CheckGeoposition(final_position_latitude, final_position_longitude);
 }
 
-IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, TabDestroyed) {
+// crbug.com/176291
+IN_PROC_BROWSER_TEST_F(GeolocationBrowserTest, DISABLED_TabDestroyed) {
   html_for_tests_ = "files/geolocation/tab_destroyed.html";
   ASSERT_TRUE(Initialize(INITIALIZATION_IFRAMES));
   LoadIFrames(3);
