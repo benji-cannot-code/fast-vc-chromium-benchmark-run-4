baf14297c6c86c9bb57fe9a359d272b882c30bbb
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 22:07:00 +0000

    Fix dependency on scoped_ptr::reset sequencing in IndexedDB code.
    scoped_ptr<T>::reset() currently guarantees that it deletes the old
    stored pointer before assigning its argument to the stored pointer.
    This is unsafe, because getting the deleter may result in the
    destruction of the scoped_ptr<T> itself. unique_ptr<T> addresses this by
    assigning its argument to the stored pointer before deleting the old
    value of the stored pointer.
    
    Unfortunately, this breaks code that assumes that the value of the
    scoped_ptr will not change during scoped_ptr::reset() before destruction
    of the old value is complete.
    
    BUG=176091
    
    Review URL: https://codereview.chromium.org/12253015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182554 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/in_process_webkit/indexed_db_dispatcher_host.h
+++ content/browser/in_process_webkit/indexed_db_dispatcher_host.h
@@ -116,6 +116,7 @@ class IndexedDBDispatcherHost : public BrowserMessageFilter {
     explicit DatabaseDispatcherHost(IndexedDBDispatcherHost* parent);
     ~DatabaseDispatcherHost();
 
+    void CloseAll();
     bool OnMessageReceived(const IPC::Message& message, bool *msg_is_ok);
     void Send(IPC::Message* message);
 
