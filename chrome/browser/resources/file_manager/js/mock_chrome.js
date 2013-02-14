b71ddc3c2c9234d5c2fe08657455b1ccccc3369d
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 07:39:54 +0000

    Revert 182235
    > Files.app: add mock namespaces and constructors to mock_chrome.js for test.
    > 
    > BUG=175657
    > TEST=mock Files.app works.
    > TBR=mtomasz@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/12230013
    
    TBR=yoshiki@chromium.org
    Review URL: https://codereview.chromium.org/12254032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182410 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/mock_chrome.js
+++ chrome/browser/resources/file_manager/js/mock_chrome.js
@@ -48,11 +48,6 @@ function cloneShallow(object) {
 }
 
 /**
- * Mock for |chrome| namespace.
- */
-var chrome = {};
-
-/**
  * Mock out the chrome.fileBrowserPrivate API for use in the harness.
  */
 chrome.fileBrowserPrivate = {
@@ -903,24 +898,3 @@ chrome.mediaPlayerPrivate = {
     this.onVideoLaunched.notify();
   }
 };
-
-/**
- * Mock for |v8Intl| namespace.
- */
-var v8Intl = {};
-
-/**
- * Mock constructor of |v8Intl.Collator|.
- * @constructor
- * @param {Array<*>} locales  //TODO(JSDOC).
- * @param {Object.<*, *>} options  //TODO(JSDOC).
- */
-v8Intl.Collator = function(locales, options) {};
-
-/**
- * Mock constructor of |v8Intl.DateTimeFormat|.
- * @constructor
- * @param {Array<*>} locales  //TODO(JSDOC).
- * @param {Object.<*, *>} options  //TODO(JSDOC).
- */
-v8Intl.DateTimeFormat = function(locales, options) {};
