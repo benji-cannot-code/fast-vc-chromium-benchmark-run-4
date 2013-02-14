143b4239274bd0321b109fca7f47bb7ff8803202
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 06:17:23 +0000

    Fix typo in AudioStreamIndicator from r182364 causing hangs.
    NOTRY=true
    TBR=sky,bauerb
    
    Review URL: https://chromiumcodereview.appspot.com/12260035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182401 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/audio_stream_indicator.cc
+++ chrome/browser/media/audio_stream_indicator.cc
@@ -65,7 +65,7 @@ void AudioStreamIndicator::UpdateWebContentsStatusOnUIThread(
   } else {
     std::map<RenderViewId, std::set<int> >::iterator it =
         audio_streams_.find(id);
-    if (it != audio_streams_.end())
+    if (it == audio_streams_.end())
       return;
 
     it->second.erase(stream_id);
