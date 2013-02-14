5b15a9e2cf6158feb2159cb57073e4698a54afa0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-14 17:52:04 +0000

    Web Inspector: fix closure compilation warnings caused by setVariableValue change https://bugs.webkit.org/show_bug.cgi?id=109488
    Patch by Peter Rybin <prybin@chromium.org> on 2013-02-14
    Reviewed by Pavel Feldman.
    
    Annotations are fixed as required by closure compiler.
    Parameters in Inspector.json are reordered as required.
    
    * inspector/InjectedScriptExterns.js:
    (InjectedScriptHost.prototype.setFunctionVariableValue):
    (JavaScriptCallFrame.prototype.setVariableValue):
    * inspector/InjectedScriptSource.js:
    (.):
    * inspector/Inspector.json:
    * inspector/InspectorDebuggerAgent.cpp:
    (WebCore::InspectorDebuggerAgent::setVariableValue):
    * inspector/InspectorDebuggerAgent.h:
    (InspectorDebuggerAgent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142888 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InjectedScriptExterns.js
+++ third_party/WebKit/Source/WebCore/inspector/InjectedScriptExterns.js
@@ -83,6 +83,14 @@ InjectedScriptHost.prototype.getEventListeners = function(object) { }
  */
 InjectedScriptHost.prototype.evaluate = function(expression) { }
 
+/**
+ * @param {function(...)} fun
+ * @param {number} scopeNumber
+ * @param {string} variableName
+ * @param {*} newValue
+ */
+InjectedScriptHost.prototype.setFunctionVariableValue = function(fun, scopeNumber, variableName, newValue) {}
+
 
 /**
  * @constructor
@@ -105,6 +113,13 @@ function JavaScriptCallFrame()
 JavaScriptCallFrame.prototype.scopeType = function(index) { }
 
 JavaScriptCallFrame.prototype.restart = function() { }
+
+/**
+ * @param {number} scopeNumber
+ * @param {string} variableName
+ * @param {*} newValue
+ */
+JavaScriptCallFrame.prototype.setVariableValue = function(scopeNumber, variableName, newValue) {}
 
 /**
  * @constructor
