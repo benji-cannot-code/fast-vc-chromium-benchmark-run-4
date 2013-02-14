488aac385bfe0f0778046528574a3b99c81fd4bc
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 20:07:38 +0000

    Remove SpdyRstStreamControlFrame struct. Useful for SPDY 4 development.
    This lands server change 42102944.
    
    Review URL: https://codereview.chromium.org/12262004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182521 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/buffered_spdy_framer.cc
+++ net/spdy/buffered_spdy_framer.cc
@@ -238,7 +238,7 @@ SpdyFrame* BufferedSpdyFramer::CreateSynReply(
   return spdy_framer_.CreateSynReply(stream_id, flags, compressed, headers);
 }
 
-SpdyRstStreamControlFrame* BufferedSpdyFramer::CreateRstStream(
+SpdyFrame* BufferedSpdyFramer::CreateRstStream(
     SpdyStreamId stream_id,
     SpdyRstStreamStatus status) const {
   return spdy_framer_.CreateRstStream(stream_id, status);
