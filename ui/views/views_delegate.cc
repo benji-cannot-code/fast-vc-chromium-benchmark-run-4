f45e0f760b5af874166f4332d83dd0086f65f81e
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 21:45:28 +0000

    Enable touch editing controller for views textfields behind a flag.
    BUG=115237
    
    
    Review URL: https://chromiumcodereview.appspot.com/12210154
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182542 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/views/views_delegate.cc
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "ui/views/views_delegate.h"
+
+#include "ui/views/touchui/touch_selection_controller_impl.h"
+
+namespace views {
+
+ViewsDelegate::ViewsDelegate() {
+  ui::TouchSelectionControllerFactory::SetInstance(
+      new views::ViewsTouchSelectionControllerFactory);
+}
+
+}  // namespace views
