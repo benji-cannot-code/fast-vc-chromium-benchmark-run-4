8ca0e95563221a92e919bd9dc35ef9134eb9b226
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-14 19:53:37 +0000

    More Extension unit tests, and better error exceptions.
    * Raise ExtensionNotFoundException instead of returning None in _CreateExtensionObject.
    * Raise ExtensionPathNonExistentException when extension path is not a directory.
    * Move BrowserConnectionGoneException to browser_gone_exception.py from extension_dict_backend.py and tab_list_backend.py.
    * Pull out setUp/tearDown from testExtension.
    * Add testDisconnect, testNonExistentExtensionPath and testExtensionNotLoaded unit tests.
    
    BUG=169954
    TEST=run_tests testExtensionNotLoaded testNonExistentExtensionPath testExtensionBasic testMultipleExtensions testDisconnect
    NOTRY=true
    Review URL: https://codereview.chromium.org/12212170
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182516 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/browser_gone_exception.py
+++ tools/telemetry/telemetry/browser_gone_exception.py
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 class BrowserGoneException(Exception):
-  """Represnets a crash of the entire browser.
+  """Represents a crash of the entire browser.
 
   In this state, all bets are pretty much off."""
   pass
 
+class BrowserConnectionGoneException(BrowserGoneException):
+  pass
