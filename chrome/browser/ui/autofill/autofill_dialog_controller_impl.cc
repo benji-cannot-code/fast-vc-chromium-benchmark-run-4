d9a5c6bbcecbd9718717e9a07a0c90a2b4f60b9f
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 07:22:28 +0000

    I think this check should be reversed. Otherwise the autofill dialog is crashing.
    BUG=None
    R=isherman@chromium.org
    TBR=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12271013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182638 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -412,7 +412,7 @@ scoped_ptr<DataModelWrapper> AutofillDialogControllerImpl::CreateWrapper(
 
   if (CanPayWithWallet()) {
     int index;
-    bool success = !base::StringToInt(item_key, &index);
+    bool success = base::StringToInt(item_key, &index);
     DCHECK(success);
 
     if (section == SECTION_CC) {
