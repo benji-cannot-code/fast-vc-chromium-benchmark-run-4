79420ce15e34fd0a9a22f1e50000ff15dfef13d3
eric.carlson@apple.com <eric.carlson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-15 19:15:33 +0000

    Crash occurs at WebCore::TextTrackList::length() when enabling closed captions in movie https://bugs.webkit.org/show_bug.cgi?id=109886
    Reviewed by Dean Jackson.
    
    Source/WebCore: 
    
    No new tests, media/media-captions.html does not crash with this change.
    
    * html/HTMLMediaElement.cpp:
    (WebCore::HTMLMediaElement::markCaptionAndSubtitleTracksAsUnconfigured): Early return when
        m_textTracks is NULL.
    
    LayoutTests: 
    
    * platform/mac/TestExpectations: Remove media/media-captions.html.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143021 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLMediaElement.cpp
@@ -4448,6 +4448,9 @@ void HTMLMediaElement::captionPreferencesChanged()
 
 void HTMLMediaElement::markCaptionAndSubtitleTracksAsUnconfigured()
 {
+    if (!m_textTracks)
+        return;
+
     // Mark all tracks as not "configured" so that configureTextTracks()
     // will reconsider which tracks to display in light of new user preferences
     // (e.g. default tracks should not be displayed if the user has turned off
