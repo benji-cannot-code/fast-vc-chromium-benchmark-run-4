2dd4043c214427d7d81afbb7ff4830888ace7304
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 17:24:58 +0000

    Remove processArchitecture value from manifests.
    BUG=166496
    TEST=Install component build of Chrome via "setup.exe --chrome --multi-install" and make sure you can launch chrome.exe.
    
    Review URL: https://chromiumcodereview.appspot.com/12252043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182740 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/tools/build/win/create_installer_archive.py
+++ chrome/tools/build/win/create_installer_archive.py
@@ -453,7 +453,7 @@ def DoComponentBuildTasks(staging_dir, build_dir, current_version):
         "<dependency>"
         "<dependentAssembly>"
         "<assemblyIdentity type='win32' name='chrome.{dll_name}' "
-        "version='0.0.0.0' processorArchitecture='*' language='*'/>"
+        "version='0.0.0.0' language='*'/>"
         "</dependentAssembly>"
         "</dependency>".format(dll_name=name))
 
@@ -477,7 +477,7 @@ def DoComponentBuildTasks(staging_dir, build_dir, current_version):
         "<assembly\n"
         "    xmlns='urn:schemas-microsoft-com:asm.v1' manifestVersion='1.0'>\n"
         "  <assemblyIdentity name='chrome.{dll_name}' version='0.0.0.0'\n"
-        "      type='win32' processorArchitecture='*'/>\n"
+        "      type='win32'/>\n"
         "  <file name='{dll_name}.dll'/>\n"
         "</assembly>".format(dll_name=name))
 
