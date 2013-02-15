6d3541ceee6c9be9e0521e615c11493ad45354f8
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 16:48:38 +0000

    Register all components for each policy domain at once.
    The PolicyService keeps track of (domain, component) pairs of policy namespaces,
    so that the policy providers know which namespaces have policy consumers. This
    change makes the registration of namespaces a bulk operation per policy domain,
    which avoids the re-registration issue and makes each registration a full
    snapshot of that domain (this allows flushing caches on startup, for example).
    
    BUG=163318
    
    
    Review URL: https://chromiumcodereview.appspot.com/12254038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182738 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_provider.cc
+++ chrome/browser/policy/configuration_policy_provider.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/policy/configuration_policy_provider.h"
 
-#include <string>
-
 #include "chrome/browser/policy/policy_map.h"
 #include "policy/policy_constants.h"
 
@@ -105,10 +103,8 @@ void ConfigurationPolicyProvider::RemoveObserver(Observer* observer) {
   observer_list_.RemoveObserver(observer);
 }
 
-void ConfigurationPolicyProvider::RegisterPolicyNamespace(
-    const PolicyNamespace& ns) {}
-
-void ConfigurationPolicyProvider::UnregisterPolicyNamespace(
-    const PolicyNamespace& ns) {}
+void ConfigurationPolicyProvider::RegisterPolicyDomain(
+    PolicyDomain domain,
+    const std::set<std::string>& component_ids) {}
 
 }  // namespace policy
