fce48c96d604e26f1fc98ac5aa8e80e7f15af01a
benquan@chromium.org <benquan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 07:46:56 +0000

    Include autocomplete=off fields in autofill request xml when autocheckout is enabled.
    BUG=172960
    
    
    Review URL: https://chromiumcodereview.appspot.com/12087067
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182641 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -1100,6 +1100,7 @@ void ChromeContentBrowserClient::AppendExtraCommandLineSwitches(
       switches::kEnableBenchmarking,
       switches::kEnableCrxlessWebApps,
       switches::kEnableExperimentalExtensionApis,
+      switches::kEnableExperimentalFormFilling,
       switches::kEnableIPCFuzzing,
       switches::kEnableInteractiveAutocomplete,
       switches::kEnableNaCl,
