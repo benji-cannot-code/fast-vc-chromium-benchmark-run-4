1f2ceb229102a21c13ba9973561d34602f33733d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 20:05:17 +0000

    ipc: Wrap unit test into IPC namespace.
    So we can remove the 'using namespace' directive.
    
    BUG=82078
    TBR=darin@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12286003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182802 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_message_utils_unittest.cc
+++ ipc/ipc_message_utils_unittest.cc
@@ -3,13 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "ipc/ipc_message_utils.h"
+
 #include "base/file_path.h"
 #include "ipc/ipc_message.h"
-#include "ipc/ipc_message_utils.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using namespace IPC;
-
+namespace IPC {
 namespace {
 
 // Tests nesting of messages as parameters to other messages.
@@ -72,3 +72,4 @@ TEST(IPCMessageUtilsTest, ParameterValidation) {
 }
 
 }  // namespace
+}  // namespace IPC
