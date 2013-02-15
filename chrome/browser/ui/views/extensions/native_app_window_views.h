a1863484709974d827684fde14c76792c23a28fe
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 00:33:25 +0000

    Revert 182559
    Seems to have cause WindowOpenPanelTest.WindowOpenFromPanel to fail
    
    > Add ShellWindowContents and AppWindow/Panel implementations.
    > 
    > This CL:
    > * Adds ShellWindowRenderInterface to abstract creating and observing
    >   web contents and passing messages to the extensions system
    > * Moves related ShellWindow to AppWindowRenderInterface
    > * Adds AshPanelRenderInterface for v1 ash panel specific impl
    > * Hooks up chrome.windows.create(type = panel) to use Ash Panels.
    > 
    > BUG=160645
    > 
    > Review URL: https://codereview.chromium.org/12209028
    
    TBR=stevenjb@chromium.org
    Review URL: https://codereview.chromium.org/12260050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/extensions/native_app_window_views.h
+++ chrome/browser/ui/views/extensions/native_app_window_views.h
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/extensions/native_app_window.h"
 #include "chrome/browser/ui/extensions/shell_window.h"
 #include "chrome/browser/ui/views/unhandled_keyboard_event_handler.h"
-#include "content/public/browser/web_contents_observer.h"
 #include "third_party/skia/include/core/SkRegion.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/rect.h"
