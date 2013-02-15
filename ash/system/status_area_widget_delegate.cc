7babc930182803736a457151faeee584f490db2b
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 01:10:10 +0000

    ash: Make sure the trays fill out the entire space in the widget.
    BUG=150770
    
    Review URL: https://codereview.chromium.org/12261040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182594 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/status_area_widget_delegate.cc
+++ ash/system/status_area_widget_delegate.cc
@@ -94,7 +94,7 @@ void StatusAreaWidgetDelegate::UpdateLayout() {
     for (int c = 0; c < child_count(); ++c) {
       if (c != 0)
         columns->AddPaddingColumn(0, kTraySpacing);
-      columns->AddColumn(views::GridLayout::CENTER, views::GridLayout::CENTER,
+      columns->AddColumn(views::GridLayout::CENTER, views::GridLayout::FILL,
                          0, /* resize percent */
                          views::GridLayout::USE_PREF, 0, 0);
     }
@@ -102,7 +102,7 @@ void StatusAreaWidgetDelegate::UpdateLayout() {
     for (int c = child_count() - 1; c >= 0; --c)
       layout->AddView(child_at(c));
   } else {
-    columns->AddColumn(views::GridLayout::CENTER, views::GridLayout::CENTER,
+    columns->AddColumn(views::GridLayout::FILL, views::GridLayout::CENTER,
                        0, /* resize percent */
                        views::GridLayout::USE_PREF, 0, 0);
     for (int c = child_count() - 1; c >= 0; --c) {
