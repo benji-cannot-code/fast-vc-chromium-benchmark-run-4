9f6aa3e90cd49a4caf652eac83be16e0cbed09ad
MHX348@motorola.com <MHX348@motorola.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 04:33:35 +0000

    Fix a bug where extensions created with locale info from the command line is broken.
    Register the ManifestHandler for default_locale
    
    BUG=176187
    TEST=1. Unzip the test.zip attachment from issue 176187.
    2. Use chrome to pack it as an extension (chrome --pack-extension=test).
    test.crx should be created successfully.
    
    Review URL: https://chromiumcodereview.appspot.com/12256041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/startup_helper.cc
+++ chrome/browser/extensions/startup_helper.cc
@@ -14,7 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/webstore_standalone_installer.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/extensions/api/i18n/default_locale_handler.h"
 #include "chrome/common/extensions/extension.h"
+#include "chrome/common/extensions/extension_manifest_constants.h"
+#include "chrome/common/extensions/manifest_handler.h"
 #include "content/public/browser/web_contents.h"
 #include "ipc/ipc_message.h"
 
@@ -28,7 +31,10 @@ void PrintPackExtensionMessage(const std::string& message) {
 
 namespace extensions {
 
-StartupHelper::StartupHelper() : pack_job_succeeded_(false) {}
+StartupHelper::StartupHelper() : pack_job_succeeded_(false) {
+  ManifestHandler::Register(extension_manifest_keys::kDefaultLocale,
+                            make_linked_ptr(new DefaultLocaleHandler));
+}
 
 void StartupHelper::OnPackSuccess(
     const base::FilePath& crx_path,
