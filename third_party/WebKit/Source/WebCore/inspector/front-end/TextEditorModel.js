6a0c9c626d4e1a23ef9e60bf3cc0fc28912bebaf
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-15 14:55:52 +0000

    Web Inspector: Redo in text editor should always collapse selection to end. https://bugs.webkit.org/show_bug.cgi?id=109907
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    * inspector/front-end/TextEditorModel.js:
    (WebInspector.TextEditorModel.endsWithBracketRegex.):
    
    LayoutTests:
    
    * inspector/editor/text-editor-undo-redo-expected.txt:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TextEditorModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TextEditorModel.js
@@ -505,7 +505,7 @@ WebInspector.TextEditorModel.prototype = {
         var range = this._doUndo(this._redoStack);
         delete this._inRedo;
 
-        return range;
+        return range ? range.collapseToEnd() : null;
     },
 
     /**
