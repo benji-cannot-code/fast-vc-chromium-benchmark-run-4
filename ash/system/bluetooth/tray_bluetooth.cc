ad19a47f20d307e613421afdb8d6a6150056e8d3
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 22:57:54 +0000

    Remove bluetooth detail page access from ash tray bubble when screen is locked.
    BUG=174485
    
    Review URL: https://codereview.chromium.org/12207194
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182856 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/bluetooth/tray_bluetooth.cc
+++ ash/system/bluetooth/tray_bluetooth.cc
@@ -33,8 +33,8 @@ namespace tray {
 
 class BluetoothDefaultView : public TrayItemMore {
  public:
-  explicit BluetoothDefaultView(SystemTrayItem* owner)
-      : TrayItemMore(owner, true) {
+  BluetoothDefaultView(SystemTrayItem* owner, bool show_more)
+      : TrayItemMore(owner, show_more) {
     ui::ResourceBundle& bundle = ui::ResourceBundle::GetSharedInstance();
     SetImage(bundle.GetImageNamed(IDR_AURA_UBER_TRAY_BLUETOOTH).ToImageSkia());
     UpdateLabel();
@@ -363,7 +363,8 @@ views::View* TrayBluetooth::CreateTrayView(user::LoginStatus status) {
 
 views::View* TrayBluetooth::CreateDefaultView(user::LoginStatus status) {
   CHECK(default_ == NULL);
-  default_ = new tray::BluetoothDefaultView(this);
+  default_ = new tray::BluetoothDefaultView(
+      this, status != user::LOGGED_IN_LOCKED);
   return default_;
 }
 
