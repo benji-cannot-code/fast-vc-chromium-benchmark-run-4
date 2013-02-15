4f83dbbfc644392a3d5b6e0d1a5b447ab3d00d28
leandrogracia@chromium.org <leandrogracia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 15:03:20 +0000

    [Android WebView] Migrate the rendering code to a separate set of classes.
    It takes from https://codereview.chromium.org/11823027/
    and assumes SW rendering and Capture Picture to be ready and enabled.
    
    Most changes just move around code. The major structural changes are:
    - Introduce a browser-layer view renderer interface and move the code to its implementation.
    - Take out the rendering-related IPC to its own separate set of host/renderer classes.
    - Change the way the view hierarchy and the compositor are initialized. Now they are created and set on BrowserViewRendererImpl construction.
    - Content is now provided via a ContentViewCore object when initialized, updating the layer to use and the WebContents to observe.
    - Remove/update the DEPS and gyp changes introduced to support rendering in the native layer.
    
    BUG=167913,167908,161409
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12041009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182710 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/android_webview_jni_registrar.cc
+++ android_webview/native/android_webview_jni_registrar.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "android_webview/native/cookie_manager.h"
 #include "android_webview/native/input_stream_impl.h"
 #include "android_webview/native/intercepted_request_data_impl.h"
+#include "android_webview/native/java_browser_view_renderer_helper.h"
 #include "android_webview/native/js_result_handler.h"
 #include "base/android/jni_android.h"
 #include "base/android/jni_registrar.h"
@@ -33,6 +34,7 @@ static base::android::RegistrationMethod kWebViewRegisteredMethods[] = {
   { "CookieManager", RegisterCookieManager },
   { "InterceptedRequestDataImpl", RegisterInterceptedRequestData },
   { "InputStream", RegisterInputStream },
+  { "JavaBrowserViewRendererHelper", RegisterJavaBrowserViewRendererHelper },
   { "JsResultHandler", RegisterJsResultHandler },
 };
 
