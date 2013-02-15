7bf972ef8bb14c17dffd1e9bc0d63ce4b15fbe3e
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-15 22:01:04 +0000

    Add a synchronous GetValues message to StorageManager https://bugs.webkit.org/show_bug.cgi?id=109968
    Reviewed by Sam Weinig.
    
    * Platform/CoreIPC/Connection.cpp:
    (CoreIPC::Connection::dispatchWorkQueueMessageReceiverMessage):
    Handle synchronous messages.
    
    (CoreIPC::Connection::processIncomingMessage):
    Check for work queue message receivers before doing any other processing.
    
    * UIProcess/Storage/StorageManager.cpp:
    (WebKit::StorageManager::getValues):
    Add empty stub.
    
    * UIProcess/Storage/StorageManager.h:
    * UIProcess/Storage/StorageManager.messages.in:
    Add GetValues message.
    
    * WebProcess/Storage/StorageAreaProxy.cpp:
    (WebKit::StorageAreaProxy::loadValuesIfNeeded):
    Send the GetValues message.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/Storage/StorageManager.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/Storage/StorageManager.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Connection.h"
 #include <wtf/PassRefPtr.h>
 #include <wtf/ThreadSafeRefCounted.h>
+#include <wtf/text/StringHash.h>
 
 class WorkQueue;
 
@@ -51,10 +52,12 @@ private:
 
     // CoreIPC::Connection::WorkQueueMessageReceiver.
     virtual void didReceiveMessage(CoreIPC::Connection*, CoreIPC::MessageDecoder&) OVERRIDE;
+    virtual void didReceiveSyncMessage(CoreIPC::Connection*, CoreIPC::MessageDecoder&, OwnPtr<CoreIPC::MessageEncoder>& replyEncoder) OVERRIDE;
 
     // Message handlers.
     void createStorageArea(uint64_t storageAreaID, uint64_t storageNamespaceID, const SecurityOriginData&);
     void destroyStorageArea(uint64_t storageAreaID);
+    void getValues(uint64_t storageAreaID, HashMap<String, String>& values);
 
     RefPtr<WorkQueue> m_queue;
 };
