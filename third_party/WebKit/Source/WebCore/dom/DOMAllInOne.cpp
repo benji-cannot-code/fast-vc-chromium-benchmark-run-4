d5235886823168cef62185d5cd45afc5b43cedb9
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-15 05:55:36 +0000

    Windows build fix after r142957.
    * dom/DOMAllInOne.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@142959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/DOMAllInOne.cpp
+++ third_party/WebKit/Source/WebCore/dom/DOMAllInOne.cpp
@@ -81,6 +81,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "EventException.cpp"
 #include "EventListenerMap.cpp"
 #include "EventNames.cpp"
+#include "EventRetargeter.cpp"
 #include "EventTarget.cpp"
 #include "ExceptionBase.cpp"
 #include "ExceptionCodePlaceholder.cpp"
