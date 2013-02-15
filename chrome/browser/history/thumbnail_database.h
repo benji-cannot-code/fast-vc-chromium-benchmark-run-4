f4a87dc8bbc7bc76737330358a3b6bffe5f897ab
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 18:59:00 +0000

    Add favicon count histogram
    Add History.NumFaviconsInDB, set on successful init, to track the number
    of favicons users have in their favicon dbs.
    
    R=sky@chromium.org
    BUG=154886
    
    
    Review URL: https://chromiumcodereview.appspot.com/12212180
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182771 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/thumbnail_database.h
+++ chrome/browser/history/thumbnail_database.h
@@ -59,6 +59,10 @@ class ThumbnailDatabase {
   static sql::InitStatus OpenDatabase(sql::Connection* db,
                                       const base::FilePath& db_name);
 
+  // Computes and records various metrics for the database. Should only be
+  // called once and only upon successful Init.
+  void ComputeDatabaseMetrics();
+
   // Transactions on the database.
   void BeginTransaction();
   void CommitTransaction();
