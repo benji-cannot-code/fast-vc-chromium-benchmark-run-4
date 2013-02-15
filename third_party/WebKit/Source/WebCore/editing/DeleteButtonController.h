cc7e5fde906f1334770384071fdfc2edbf1ea185
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-15 19:59:41 +0000

    DeleteButtonController::enable and disable should be called via a RAII object https://bugs.webkit.org/show_bug.cgi?id=109550
    Reviewed by Enrica Casucci.
    
    Added DeleteButtonControllerDisableScope, a friend class of DeleteButtonController,
    and made DeleteButtonController::enable/disable private.
    
    * dom/ContainerNode.cpp:
    * editing/CompositeEditCommand.cpp:
    (WebCore::EditCommandComposition::unapply):
    (WebCore::EditCommandComposition::reapply):
    (WebCore::CompositeEditCommand::apply):
    * editing/DeleteButtonController.h:
    (WebCore):
    (DeleteButtonController):
    (DeleteButtonControllerDisableScope):
    (WebCore::DeleteButtonControllerDisableScope::DeleteButtonControllerDisableScope):
    (WebCore::DeleteButtonControllerDisableScope::~DeleteButtonControllerDisableScope):
    * editing/markup.cpp:
    (WebCore::createMarkup):
    (WebCore::createFragmentFromNodes):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/DeleteButtonController.h
+++ third_party/WebKit/Source/WebCore/editing/DeleteButtonController.h
@@ -28,11 +28,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DeleteButtonController_h
 
 #include "DeleteButton.h"
+#include "Frame.h"
 
 namespace WebCore {
 
+#if ENABLE(DELETION_UI)
+
 class DeleteButton;
-class Frame;
 class HTMLElement;
 class RenderObject;
 class VisibleSelection;
@@ -51,15 +53,16 @@ public:
     void show(HTMLElement*);
     void hide();
 
-    void enable();
-    void disable();
-
     void deleteTarget();
 
 private:
     static const char* const buttonElementIdentifier;
     static const char* const outlineElementIdentifier;
     static const char* const containerElementIdentifier;
+    
+    void enable();
+    void disable();
+    friend class DeleteButtonControllerDisableScope;
 
     void createDeletionUI();
     bool enabled() const { return (!m_disableStack); }
@@ -73,6 +76,27 @@ private:
     bool m_wasAutoZIndex;
     unsigned m_disableStack;
 };
+
+class DeleteButtonControllerDisableScope {
+public:
+    DeleteButtonControllerDisableScope(Frame* frame)
+        : m_frame(frame)
+    {
+        if (frame)
+            frame->editor()->deleteButtonController()->disable();
+    }
+
+    ~DeleteButtonControllerDisableScope()
+    {
+        if (m_frame)
+            m_frame->editor()->deleteButtonController()->enable();
+    }
+
+private:
+    RefPtr<Frame> m_frame;
+};
+
+#endif
 
 } // namespace WebCore
 
