2a9ee8a0ad01d5ec2dd22c827af782801b2a9ce6
huangs@chromium.org <huangs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 14:01:28 +0000

    Setting RunAsUser in the install-application app command.
    Also, quick-enable-application-host has RunAsUser = false, but quick-enable-cf has RunAsUser = true.  These are wrong, and are fixed here.
    
    BUG=176317
    
    Review URL: https://chromiumcodereview.appspot.com/12252038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182701 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/installer/setup/install_worker.cc
+++ chrome/installer/setup/install_worker.cc
@@ -242,6 +242,7 @@ void AddInstallAppCommandWorkItems(const InstallerState& installer_state,
     AppCommand cmd(cmd_line.GetCommandLineString());
     cmd.set_sends_pings(true);
     cmd.set_is_web_accessible(true);
+    cmd.set_is_run_as_user(true);
     cmd.AddWorkItems(installer_state.root_key(), cmd_key, work_item_list);
   }
 }
@@ -327,6 +328,7 @@ void AddQuickEnableApplicationLauncherWorkItems(
     AppCommand cmd(cmd_line.GetCommandLineString());
     cmd.set_sends_pings(true);
     cmd.set_is_web_accessible(true);
+    cmd.set_is_run_as_user(true);
     cmd.AddWorkItems(installer_state.root_key(), cmd_key, work_item_list);
   }
 }
@@ -1657,7 +1659,6 @@ void AddQuickEnableChromeFrameWorkItems(const InstallerState& installer_state,
     AppCommand cmd(cmd_line.GetCommandLineString());
     cmd.set_sends_pings(true);
     cmd.set_is_web_accessible(true);
-    cmd.set_is_run_as_user(true);
     cmd.AddWorkItems(installer_state.root_key(), cmd_key, work_item_list);
   }
 }
