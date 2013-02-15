0478aaaa9cd4529f4b68d5e353ec9fad6e8abc39
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-15 19:42:17 +0000

    [Telemetry] Add more options to wait_action.
    You can now wait for all sorts of things! Disadvantage: you have to now specify what condition to wait for.
    
    Some wait actions, like wait for navigation and wait for href change, also run the previous action. So RunAction() now takes previous_action. A PageAction should override RunsPreviousAction() if it uses previous_action.
    
    CompoundAction is written so that its first child inherits its previous action.
    
    This patch should have a followup that replaces the old "wait_for_X" actions with { "action": "wait", "condition": "X" } actions and removes Page.WaitForPageToLoad().
    
    BUG=168431
    TEST=./run_tests in telemetry and perf.
    
    Review URL: https://codereview.chromium.org/12084037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182791 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/click_element_action_unittest.py
+++ tools/telemetry/telemetry/click_element_action_unittest.py
@@ -21,7 +21,7 @@ class ClickElementActionTest(tab_test_case.TabTestCase):
 
     data = {'selector': 'a[id="clickme"]', 'wait_for_navigation': True}
     i = click_element_action.ClickElementAction(data)
-    i.RunAction({}, self._tab)
+    i.RunAction(None, self._tab, None)
 
     self.assertEquals(
         self._tab.EvaluateJavaScript('document.location.pathname;'),
@@ -40,7 +40,7 @@ class ClickElementActionTest(tab_test_case.TabTestCase):
 
     data = {'text': 'Click me', 'wait_for_href_change': True}
     i = click_element_action.ClickElementAction(data)
-    i.RunAction({}, self._tab)
+    i.RunAction(None, self._tab, None)
 
     self.assertEquals(
         self._tab.EvaluateJavaScript('document.location.pathname;'),
