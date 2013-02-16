1252fb0773559796ae86b15cb2f22a8096a15623
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-16 18:15:50 +0000

    This fixes the overflows identified in the parsing function, and adds the dalvik-heap-limit.
    BUG=176201
    NOTRY=true
    
    No try since it has passed many times and is taking days in the CQ.
    
    Review URL: https://chromiumcodereview.appspot.com/12223064
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182986 0039d316-1c4b-4281-b951-d872f2087c98

--- base/sys_info.h
+++ base/sys_info.h
@@ -105,6 +105,7 @@ class BASE_EXPORT SysInfo {
   static std::string GetDeviceName();
 
   static int DalvikHeapSizeMB();
+  static int DalvikHeapGrowthLimitMB();
 #endif  // defined(OS_ANDROID)
 };
 
