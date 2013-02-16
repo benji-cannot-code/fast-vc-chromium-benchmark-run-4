335e8fe9e6ce5cc153e4e8b1b718b68dbc20a7f6
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-16 04:21:16 +0000

    Make panels support win-aura and reenable some tests.
    This patch did not fix all the issues. We still need to make resizing and stacking work.
    
    The following issues are fixed:
    1) Changed to call chrome::HWNDForWidge to get the right hwnd for both win and win-aura cases. Quite a few places that were previously disabled for win_aura can now work.
    2) Changed to compute the paint state on the fly instead of tracking it. This allows us to fix a failed test (bug 164976).
    3) Made tests ActivePanelWindowProperties and InactivePanelWindowProperties also do window style validation for win_aura.
    
    BUG=164976,171940
    TEST=existing tests
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=182790
    
    Review URL: https://chromiumcodereview.appspot.com/12258040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@182919 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/panels/panel_view_browsertest.cc
+++ chrome/browser/ui/views/panels/panel_view_browsertest.cc
@@ -17,6 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/link.h"
 #include "ui/views/controls/textfield/textfield.h"
 
+#if defined(OS_WIN)
+#include "chrome/browser/ui/views/hwnd_util.h"
+#endif
+
 // BasePanelBrowserTest now creates refactored Panels. Refactor
 // has only been done for Mac panels so far.
 class PanelViewTest : public BasePanelBrowserTest {
@@ -27,10 +31,6 @@ class PanelViewTest : public BasePanelBrowserTest {
   PanelView* GetPanelView(Panel* panel) const {
     return static_cast<PanelView*>(panel->native_panel());
   }
-
-  gfx::NativeWindow GetNativeWindow(Panel* panel) const {
-    return GetPanelView(panel)->GetNativePanelWindow();
-  }
 };
 
 IN_PROC_BROWSER_TEST_F(PanelViewTest, ActivePanelWindowProperties) {
@@ -40,8 +40,8 @@ IN_PROC_BROWSER_TEST_F(PanelViewTest, ActivePanelWindowProperties) {
 
   // Validate window styles. We want to ensure that the window is created
   // with expected styles regardless of its active state.
-#if defined(OS_WIN) && !defined(USE_AURA)
-  HWND native_window = GetNativeWindow(panel);
+#if defined(OS_WIN)
+  HWND native_window = chrome::HWNDForWidget(GetPanelView(panel)->window());
 
   LONG styles = ::GetWindowLong(native_window, GWL_STYLE);
   EXPECT_EQ(0, styles & WS_MAXIMIZEBOX);
@@ -68,8 +68,8 @@ IN_PROC_BROWSER_TEST_F(PanelViewTest, InactivePanelWindowProperties) {
 
   // Validate window styles. We want to ensure that the window is created
   // with expected styles regardless of its active state.
-#if defined(OS_WIN) && !defined(USE_AURA)
-  HWND native_window = GetNativeWindow(panel);
+#if defined(OS_WIN)
+  HWND native_window = chrome::HWNDForWidget(GetPanelView(panel)->window());
 
   LONG styles = ::GetWindowLong(native_window, GWL_STYLE);
   EXPECT_EQ(0, styles & WS_MAXIMIZEBOX);
