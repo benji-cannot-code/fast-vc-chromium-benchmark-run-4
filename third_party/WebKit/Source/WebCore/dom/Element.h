1bd67e6a65878e51e4cb9268f54af516921b9b58
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-16 22:57:02 +0000

    Element: Avoid unrelated attribute synchronization on other attribute access. <http://webkit.org/b/110025>
    Reviewed by Darin Adler.
    
    We've been extremely trigger happy with re-serializing the style attribute (and SVG animatables)
    whenever any Element attribute API was used. This patch narrows this down to (almost always)
    only synchronizing an attribute when someone specifically wants to read/update it.
    
    Also removed two more confusing ElementData accessors:
    
        - Element::elementDataWithSynchronizedAttributes()
        - Element::ensureElementDataWithSynchronizedAttributes()
    
    * dom/Element.h:
    * dom/Element.cpp:
    (WebCore::Element::hasAttributes):
    (WebCore::Element::hasEquivalentAttributes):
    (WebCore::Element::cloneAttributesFromElement):
    (WebCore::Element::synchronizeAllAttributes):
    
        Renamed updateInvalidAttributes() to synchronizeAllAttributes().
        This function should only be used when we need every single attribute to be up-to-date.
    
    (WebCore::Element::synchronizeAttribute):
    
        Broke out logic for synchronizing a specific attribute, given either a full QualifiedName
        or a localName.
    
    (WebCore::Element::setSynchronizedLazyAttribute):
    
        Don't call ensureUniqueElementData() indisciminately here. This avoids converting the attribute
        storage when re-serializing the inline style yields the same CSS text that was already in the
        style attribute.
    
    (WebCore::Element::hasAttribute):
    (WebCore::Element::hasAttributeNS):
    (WebCore::Element::getAttribute):
    (WebCore::Element::getAttributeNode):
    (WebCore::Element::getAttributeNodeNS):
    (WebCore::Element::setAttribute):
    (WebCore::Element::setAttributeNode):
    (WebCore::Element::removeAttributeNode):
    
        Only synchronize the attribute in question.
    
    * dom/Node.cpp:
    (WebCore::Node::compareDocumentPosition):
    
        Call synchronizeAllAttributes() when comparing two Attr nodes on the same Element instead
        of relying on the side-effects of another function doing this.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143112 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.h
+++ third_party/WebKit/Source/WebCore/dom/Element.h
@@ -370,9 +370,9 @@ public:
     void parserSetAttributes(const Vector<Attribute>&, FragmentScriptingPermission);
 
     const ElementData* elementData() const { return m_elementData.get(); }
-    const ElementData* elementDataWithSynchronizedAttributes() const;
-    const ElementData* ensureElementDataWithSynchronizedAttributes() const;
     UniqueElementData* ensureUniqueElementData();
+
+    void synchronizeAllAttributes() const;
 
     // Clones attributes only.
     void cloneAttributesFromElement(const Element&);
@@ -648,7 +648,8 @@ private:
     void didModifyAttribute(const QualifiedName&, const AtomicString&);
     void didRemoveAttribute(const QualifiedName&);
 
-    void updateInvalidAttributes() const;
+    void synchronizeAttribute(const QualifiedName&) const;
+    void synchronizeAttribute(const AtomicString& localName) const;
 
     void scrollByUnits(int units, ScrollGranularity);
 
@@ -775,20 +776,6 @@ inline Element* Element::nextElementSibling() const
     return static_cast<Element*>(n);
 }
 
-inline const ElementData* Element::elementDataWithSynchronizedAttributes() const
-{
-    updateInvalidAttributes();
-    return elementData();
-}
-
-inline const ElementData* Element::ensureElementDataWithSynchronizedAttributes() const
-{
-    updateInvalidAttributes();
-    if (elementData())
-        return elementData();
-    return const_cast<Element*>(this)->ensureUniqueElementData();
-}
-
 inline void Element::updateName(const AtomicString& oldName, const AtomicString& newName)
 {
     if (!inDocument() || isInShadowTree())
@@ -900,20 +887,6 @@ inline const Attribute* Element::getAttributeItem(const QualifiedName& name) con
 {
     ASSERT(elementData());
     return elementData()->getAttributeItem(name);
-}
-
-inline void Element::updateInvalidAttributes() const
-{
-    if (!elementData())
-        return;
-
-    if (elementData()->m_styleAttributeIsDirty)
-        updateStyleAttribute();
-
-#if ENABLE(SVG)
-    if (elementData()->m_animatedSVGAttributesAreDirty)
-        updateAnimatedSVGAttribute(anyQName());
-#endif
 }
 
 inline bool Element::hasID() const
