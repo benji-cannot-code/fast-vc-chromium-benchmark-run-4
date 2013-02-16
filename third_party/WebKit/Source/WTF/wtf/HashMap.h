b088dd8047024e1e2f08cf9f11d6f3e0cce131c5
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-16 01:01:03 +0000

    Add HashMap::isValidKey and HashSet::isValidValue https://bugs.webkit.org/show_bug.cgi?id=109977
    Reviewed by Sam Weinig and Darin Adler.
    
    Source/WebKit2:
    
    Just call HashMap::isValidKey directly.
    
    * UIProcess/WebProcessProxy.cpp:
    (WebKit::generatePageID):
    Initialize the id to 0 and use prefix increment.
    
    (WebKit::WebProcessProxy::webFrame):
    (WebKit::WebProcessProxy::canCreateFrame):
    (WebKit::WebProcessProxy::didDestroyFrame):
    
    Source/WTF:
    
    Add helper functions for determining whether keys are valid, i.e. if
    they are _not_ empty or deleted according to the hash traits.
    
    * wtf/HashMap.h:
    * wtf/HashSet.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143071 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/HashMap.h
+++ third_party/WebKit/Source/WTF/wtf/HashMap.h
@@ -132,6 +132,8 @@ namespace WTF {
 
         void checkConsistency() const;
 
+        static bool isValidKey(const KeyType&);
+
     private:
         AddResult inlineAdd(const KeyType&, MappedPassInReferenceType);
 
@@ -409,6 +411,23 @@ namespace WTF {
     inline void HashMap<T, U, V, W, X>::checkConsistency() const
     {
         m_impl.checkTableConsistency();
+    }
+
+    template<typename T, typename U, typename V, typename W, typename X>
+    inline bool HashMap<T, U, V, W, X>::isValidKey(const KeyType& key)
+    {
+        if (KeyTraits::isDeletedValue(key))
+            return false;
+
+        if (HashFunctions::safeToCompareToEmptyOrDeleted) {
+            if (key == KeyTraits::emptyValue())
+                return false;
+        } else {
+            if (isHashTraitsEmptyValue<KeyTraits>(key))
+                return false;
+        }
+
+        return true;
     }
 
     template<typename T, typename U, typename V, typename W, typename X>
