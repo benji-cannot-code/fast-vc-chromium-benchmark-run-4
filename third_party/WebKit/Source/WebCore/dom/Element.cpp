3b9349637457ff5d943a4eeded7d18bde26ab9c3
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-16 01:39:19 +0000

    Calling DOM Element.attributes shouldn't force creation of ElementData. <http://webkit.org/b/109976>
    Reviewed by Darin Adler.
    
    Don't create ElementData for an Element unnecessarily just because someone calls .attributes on it.
    Previously, JS like this would create empty ElementData when 'element' has no attributes:
    
        for (i = 0; i < element.attributes.length; ++i)
            doStuff(element.attributes[i]);
    
    Make NamedNodeMap::length() short-circuit and return 0 if !Element::hasAttributes().
    
    * dom/Element.cpp:
    (WebCore::Element::attributes):
    * dom/NamedNodeMap.cpp:
    (WebCore::NamedNodeMap::length):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143076 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Element.cpp
+++ third_party/WebKit/Source/WebCore/dom/Element.cpp
@@ -314,7 +314,6 @@ void Element::setBooleanAttribute(const QualifiedName& name, bool value)
 
 NamedNodeMap* Element::attributes() const
 {
-    ensureElementDataWithSynchronizedAttributes();
     ElementRareData* rareData = const_cast<Element*>(this)->ensureElementRareData();
     if (NamedNodeMap* attributeMap = rareData->attributeMap())
         return attributeMap;
