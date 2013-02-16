164a0b97141372326a1090c9d2da0dca495737fa
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-16 22:28:27 +0000

    [Telemetry] Reorganize everything.
    Split telemetry into 4 main directories:
    telemetry/core - stuff for dealing with options, browsers, tabs, extensions, credentials. should never reference anything in page.
    telemetry/core/chrome - chrome's implementation of core. should never be referenced outside of core.
    telemetry/page - actions, and basically everything with the word page in its name.
    telemetry/test - stuff related to testing, unit testing, discovery.
    
    TBR=nduca@chromium.org
    BUG=168429
    TEST=./run_tests in telemetry and perf. There are preexisting failures in telemetry. Also ./run_multipage_benchmarks.
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=182991
    
    Review URL: https://codereview.chromium.org/12278015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183009 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/skpicture_printer.py
+++ tools/perf/perf_tools/skpicture_printer.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 import os
 
-from telemetry import multi_page_benchmark
+from telemetry.page import multi_page_benchmark
 
 _JS = 'chrome.gpuBenchmarking.printToSkPicture("{0}");'
 
--- tools/perf/perf_tools/smoothness_benchmark_unittest.py
+++ tools/perf/perf_tools/smoothness_benchmark_unittest.py
@@ -2,11 +2,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2012 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
-from telemetry import multi_page_benchmark_unittest_base
-from telemetry import page
 from perf_tools import smoothness_benchmark
-
-from telemetry.page_benchmark_results import PageBenchmarkResults
+from telemetry.page import multi_page_benchmark_unittest_base
+from telemetry.page import page
+from telemetry.page.page_benchmark_results import PageBenchmarkResults
 
 class SmoothnessBenchmarkUnitTest(
   multi_page_benchmark_unittest_base.MultiPageBenchmarkUnitTestBase):
