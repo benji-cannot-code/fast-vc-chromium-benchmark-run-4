785ae04590b03e6d061ef02294aa9b334a847253
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-17 13:55:00 +0000

    Make npapi_test_plugin build on Win64
    Just made some timer callback signatures and a type 64-bit safe.
    
    BUG=166496
    R=bauerb@chromium.org
    Review URL: https://codereview.chromium.org/12298002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183036 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/test/plugin_npobject_lifetime_test.h
+++ webkit/plugins/npapi/test/plugin_npobject_lifetime_test.h
@@ -32,8 +32,8 @@ class NPObjectLifetimeTest : public PluginTest {
   NPObject* other_plugin_instance_object_;
 
 #if defined(OS_WIN)
-  static void CALLBACK TimerProc(HWND window, UINT message, UINT timer_id,
-                                 unsigned long elapsed_milli_seconds);
+  static void CALLBACK TimerProc(HWND window, UINT message, UINT_PTR timer_id,
+                                 DWORD elapsed_milli_seconds);
   UINT_PTR timer_id_;
 #endif
   DISALLOW_IMPLICIT_CONSTRUCTORS(NPObjectLifetimeTest);
@@ -70,8 +70,8 @@ class NPObjectDeletePluginInNPN_Evaluate : public PluginTest {
  protected:
   NPObject* plugin_instance_object_;
 #if defined(OS_WIN)
-  static void CALLBACK TimerProc(HWND window, UINT message, UINT timer_id,
-                                 unsigned long elapsed_milli_seconds);
+  static void CALLBACK TimerProc(HWND window, UINT message, UINT_PTR timer_id,
+                                 DWORD elapsed_milli_seconds);
   UINT_PTR timer_id_;
 #endif
 
