9df59b06ac8d6187a387d153a32fea490d6f3844
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-18 15:52:32 +0000

    Revert 183116
    This change appears to have broken HistoryApiTest.DeleteProhibited on Win7 Tests (2).
    
    > History: Pass min/max timestamps as query parameters to history server.
    > 
    > Queries to the history server should match the same time range as the
    > query to the history backend.
    > 
    > Also de-dupe and sort results before handing them to the JS frontend.
    > 
    > BUG=None
    > 
    > Review URL: https://codereview.chromium.org/12217125
    
    TBR=dubroy@chromium.org
    Review URL: https://codereview.chromium.org/12304010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183124 0039d316-1c4b-4281-b951-d872f2087c98

--- base/values.cc
+++ base/values.cc
@@ -1001,13 +1001,13 @@ bool ListValue::Remove(const Value& value, size_t* index) {
   return false;
 }
 
-ListValue::iterator ListValue::Erase(iterator iter, Value** out_value) {
+void ListValue::Erase(iterator iter, Value** out_value) {
   if (out_value)
     *out_value = *iter;
   else
     delete *iter;
 
-  return list_.erase(iter);
+  list_.erase(iter);
 }
 
 void ListValue::Append(Value* in_value) {
