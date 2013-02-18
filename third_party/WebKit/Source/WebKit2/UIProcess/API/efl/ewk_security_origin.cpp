51dd282d155ef1d8219c05a48dd54d7b9f33b438
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-18 21:36:57 +0000

    [EFL][WK2] Use C API inside ewk_security_origin https://bugs.webkit.org/show_bug.cgi?id=107923
    Patch by Christophe Dumez <christophe.dumez@intel.com> on 2013-02-18
    Reviewed by Alexey Proskuryakov.
    
    Use C API inside ewk_security_origin instead of accessing
    directly internal C++ classes, to avoid breaking API
    layering.
    
    * UIProcess/API/efl/EwkView.cpp:
    (EwkView::enterFullScreen):
    * UIProcess/API/efl/ewk_security_origin.cpp:
    (EwkSecurityOrigin::EwkSecurityOrigin):
    * UIProcess/API/efl/ewk_security_origin_private.h:
    (EwkSecurityOrigin::create):
    (EwkSecurityOrigin):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143258 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_security_origin.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_security_origin.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "WKAPICast.h"
 #include "WKSecurityOrigin.h"
-#include "WebSecurityOrigin.h"
+#include "WKString.h"
 #include "ewk_security_origin_private.h"
 #include <WebCore/SecurityOrigin.h>
 
@@ -42,8 +42,8 @@ EwkSecurityOrigin::EwkSecurityOrigin(WKSecurityOriginRef originRef)
     , m_protocol(AdoptWK, WKSecurityOriginCopyProtocol(originRef))
 { }
 
-EwkSecurityOrigin::EwkSecurityOrigin(const KURL& url)
-    : m_wkOrigin(AdoptWK, toAPI(WebSecurityOrigin::create(SecurityOrigin::create(url)).leakRef()))
+EwkSecurityOrigin::EwkSecurityOrigin(const char* url)
+    : m_wkOrigin(AdoptWK, WKSecurityOriginCreateFromString(adoptWK(WKStringCreateWithUTF8CString(url)).get()))
     , m_host(AdoptWK, WKSecurityOriginCopyHost(m_wkOrigin.get()))
     , m_protocol(AdoptWK, WKSecurityOriginCopyProtocol(m_wkOrigin.get()))
 { }
--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_security_origin_private.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_security_origin_private.h
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WKEinaSharedString.h"
 #include "WKSecurityOrigin.h"
 #include "ewk_object_private.h"
-#include <WebCore/KURL.h>
 #include <WebKit2/WKBase.h>
 #include <wtf/PassRefPtr.h>
 
@@ -43,7 +42,7 @@ public:
         return adoptRef(new EwkSecurityOrigin(originRef));
     }
 
-    static PassRefPtr<EwkSecurityOrigin> create(const WebCore::KURL& url)
+    static PassRefPtr<EwkSecurityOrigin> create(const char* url)
     {
         return adoptRef(new EwkSecurityOrigin(url));
     }
@@ -54,7 +53,7 @@ public:
 
 private:
     explicit EwkSecurityOrigin(WKSecurityOriginRef originRef);
-    explicit EwkSecurityOrigin(const WebCore::KURL& url);
+    explicit EwkSecurityOrigin(const char* url);
 
     WKRetainPtr<WKSecurityOriginRef> m_wkOrigin;
     WKEinaSharedString m_host;
