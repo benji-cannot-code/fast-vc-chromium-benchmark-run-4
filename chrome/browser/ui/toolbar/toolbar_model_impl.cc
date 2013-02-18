b112d3de09b3e888f38ef83205dbc6cab3501e81
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-18 16:15:40 +0000

    Don't extract search terms that the omnbox would treat as a navigation.
    Doing so might confuse users into believing that the search terms were
    the URL of the current page, and could cause problems if users hit enter
    in the omnibox expecting to reload the page.  This also covers the case
    of javascript: URLs.
    
    BUG=163192,173483
    TEST=With instant extended API enabled, paste the following URL into the omnibox and note that the whole URL is visible in the omnibox as the search results are shown. https://www.google.ca/webhp?sourceid=chrome-instant&espv=1&ie=UTF-8#hl=en&sugexp=les%3B&gs_rn=0&gs_ri=hp&cp=4&gs_id=0&xhr=t&q=http://www.shadybank.com/&pf=p&tbo=d&espv=1&output=search&sclient=chrome-search&oq=spam&gs_l=&pbx=1&bav=on.2,or.r_gc.r_pw.r_qf.&bvm=bv.41642243,d.aWc&fp=ba0fd5791f5165d1&biw=0&bih=0
    
    Review URL: https://chromiumcodereview.appspot.com/12086058
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183128 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/toolbar_model_impl.cc
+++ chrome/browser/ui/toolbar/toolbar_model_impl.cc
@@ -8,7 +8,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/prefs/pref_service.h"
 #include "base/utf_string_conversions.h"
+#include "chrome/browser/autocomplete/autocomplete_classifier.h"
+#include "chrome/browser/autocomplete/autocomplete_classifier_factory.h"
 #include "chrome/browser/autocomplete/autocomplete_input.h"
+#include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ssl/ssl_error_info.h"
 #include "chrome/browser/ui/search/search.h"
@@ -49,8 +52,7 @@ ToolbarModelImpl::~ToolbarModelImpl() {
 string16 ToolbarModelImpl::GetText(
     bool display_search_urls_as_search_terms) const {
   if (display_search_urls_as_search_terms) {
-    string16 search_terms =
-        chrome::search::GetSearchTerms(delegate_->GetActiveWebContents());
+    string16 search_terms = GetSearchTerms();
     if (!search_terms.empty())
       return search_terms;
   }
@@ -82,8 +84,7 @@ GURL ToolbarModelImpl::GetURL() const {
 }
 
 bool ToolbarModelImpl::WouldReplaceSearchURLWithSearchTerms() const {
-  const content::WebContents* contents = delegate_->GetActiveWebContents();
-  return !chrome::search::GetSearchTerms(contents).empty();
+  return !GetSearchTerms().empty();
 }
 
 bool ToolbarModelImpl::ShouldDisplayURL() const {
@@ -220,4 +221,25 @@ Profile* ToolbarModelImpl::GetProfile() const {
   return navigation_controller ?
       Profile::FromBrowserContext(navigation_controller->GetBrowserContext()) :
       NULL;
+}
+
+string16 ToolbarModelImpl::GetSearchTerms() const {
+  const WebContents* contents = delegate_->GetActiveWebContents();
+  string16 search_terms = chrome::search::GetSearchTerms(contents);
+
+  // Don't extract search terms that the omnibox would treat as a navigation.
+  // This might confuse users into believing that the search terms were the
+  // URL of the current page, and could cause problems if users hit enter in
+  // the omnibox expecting to reload the page.
+  if (!search_terms.empty()) {
+    AutocompleteMatch match;
+    Profile* profile =
+        Profile::FromBrowserContext(contents->GetBrowserContext());
+    AutocompleteClassifierFactory::GetForProfile(profile)->Classify(
+        search_terms, string16(), false, false, &match, NULL);
+    if (!AutocompleteMatch::IsSearchType(match.type))
+      search_terms.clear();
+  }
+
+  return search_terms;
 }
