af4fc260964b0764235a41dd7afb6d00bb6dc743
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-18 05:53:12 +0000

    Remove unnecessary public method DrawingAreaImpl::createGraphicsContext() https://bugs.webkit.org/show_bug.cgi?id=109893
    Patch by Jae Hyun Park <jae.park@company100.net> on 2013-02-17
    Reviewed by Anders Carlsson.
    
    DrawingAreaImpl::createGraphicsContext() is unnecessary since
    createGraphicsContext can be called directly using ShareableBitmap.
    
    * WebProcess/WebPage/DrawingAreaImpl.cpp:
    (WebKit::DrawingAreaImpl::display):
    * WebProcess/WebPage/DrawingAreaImpl.h:
    (DrawingAreaImpl):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143146 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/DrawingAreaImpl.cpp
@@ -653,11 +653,6 @@ static bool shouldPaintBoundsRect(const IntRect& bounds, const Vector<IntRect>& 
     return wastedSpace <= wastedSpaceThreshold;
 }
 
-PassOwnPtr<GraphicsContext> DrawingAreaImpl::createGraphicsContext(ShareableBitmap* bitmap)
-{
-    return bitmap->createGraphicsContext();
-}
-
 void DrawingAreaImpl::display(UpdateInfo& updateInfo)
 {
     ASSERT(!m_isPaintingSuspended);
@@ -708,7 +703,7 @@ void DrawingAreaImpl::display(UpdateInfo& updateInfo)
     m_scrollRect = IntRect();
     m_scrollOffset = IntSize();
 
-    OwnPtr<GraphicsContext> graphicsContext = createGraphicsContext(bitmap.get());
+    OwnPtr<GraphicsContext> graphicsContext = bitmap->createGraphicsContext();
     graphicsContext->applyDeviceScaleFactor(deviceScaleFactor);
     
     updateInfo.updateRectBounds = bounds;
