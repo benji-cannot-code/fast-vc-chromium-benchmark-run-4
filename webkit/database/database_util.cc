bc3d162fde336b6d5619f75bd2b1b7e80c27907c
aedla@chromium.org <aedla@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-18 22:21:04 +0000

    WebDatabase: check path traversal in origin_identifier
    BUG=172264
    
    
    Review URL: https://chromiumcodereview.appspot.com/12212091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183141 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/database/database_util.cc
+++ webkit/database/database_util.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "webkit/database/database_util.h"
 
+#include "base/basictypes.h"
 #include "base/utf_string_conversions.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebSecurityOrigin.h"
@@ -89,6 +90,17 @@ GURL DatabaseUtil::GetOriginFromIdentifier(const string16& origin_identifier) {
   }
 
   return GURL(web_security_origin.toString());
+}
+
+bool DatabaseUtil::IsValidOriginIdentifier(const string16& origin_identifier) {
+  string16 dotdot = ASCIIToUTF16("..");
+  char16 forbidden[] = {'\\', '/', '\0'};
+
+  string16::size_type pos = origin_identifier.find(dotdot);
+  if (pos == string16::npos)
+    pos = origin_identifier.find_first_of(forbidden, 0, arraysize(forbidden));
+
+  return pos == string16::npos;
 }
 
 }  // namespace webkit_database
--- webkit/database/database_util_unittest.cc
+++ webkit/database/database_util_unittest.cc
@@ -3,6 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/string_piece.h"
 #include "base/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "webkit/database/database_util.h"
@@ -32,6 +33,12 @@ static GURL ToAndFromOriginIdentifier(const GURL origin_url) {
   return DatabaseUtil::GetOriginFromIdentifier(id);
 }
 
+static void TestValidOriginIdentifier(bool expected_result,
+                                      const base::StringPiece id) {
+  EXPECT_EQ(expected_result,
+            DatabaseUtil::IsValidOriginIdentifier(ASCIIToUTF16(id)));
+}
+
 namespace webkit_database {
 
 // Test DatabaseUtil::CrackVfsFilePath on various inputs.
@@ -53,6 +60,15 @@ TEST(DatabaseUtilTest, OriginIdentifiers) {
   const GURL kHttpOrigin(GURL("http://bar/").GetOrigin());
   EXPECT_EQ(kFileOrigin, ToAndFromOriginIdentifier(kFileOrigin));
   EXPECT_EQ(kHttpOrigin, ToAndFromOriginIdentifier(kHttpOrigin));
+}
+
+TEST(DatabaseUtilTest, IsValidOriginIdentifier) {
+  TestValidOriginIdentifier(true,  "http_bar_0");
+  TestValidOriginIdentifier(true,  "");
+  TestValidOriginIdentifier(false, "bad..id");
+  TestValidOriginIdentifier(false, "bad/id");
+  TestValidOriginIdentifier(false, "bad\\id");
+  TestValidOriginIdentifier(false, base::StringPiece("bad\0id", 6));
 }
 
 }  // namespace webkit_database
