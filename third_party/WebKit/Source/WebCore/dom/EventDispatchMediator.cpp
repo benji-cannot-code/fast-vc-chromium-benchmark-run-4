2f47fbfc089f5554fbf55ffc973bfaf52bfa1e03
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-18 05:51:49 +0000

    Make EventDispatcher take an Event object in its constructor. https://bugs.webkit.org/show_bug.cgi?id=109898
    Reviewed by Dimitri Glazkov.
    
    That makes EventDispatcher more RAII-like so that we can calculate
    an EventPath in its constructor.  I'll remove
    EventDispatcher::ensureEventPath() in a following patch.
    
    No tests. No change in behavior.
    
    * dom/EventDispatchMediator.cpp:
    (WebCore::EventDispatchMediator::dispatchEvent):
    * dom/EventDispatcher.cpp:
    (WebCore::EventDispatcher::dispatchEvent):
    (WebCore::EventDispatcher::EventDispatcher):
    (WebCore::EventDispatcher::ensureEventPath):
    (WebCore::EventDispatcher::dispatchSimulatedClick):
    (WebCore::EventDispatcher::dispatch):
    (WebCore::EventDispatcher::dispatchEventPreProcess):
    (WebCore::EventDispatcher::dispatchEventAtCapturing):
    (WebCore::EventDispatcher::dispatchEventAtTarget):
    (WebCore::EventDispatcher::dispatchEventAtBubbling):
    (WebCore::EventDispatcher::dispatchEventPostProcess):
    * dom/EventDispatcher.h:
    (EventDispatcher):
    (WebCore::EventDispatcher::node):
    (WebCore::EventDispatcher::event):
    * dom/FocusEvent.cpp:
    (WebCore::FocusEventDispatchMediator::dispatchEvent):
    (WebCore::BlurEventDispatchMediator::dispatchEvent):
    (WebCore::FocusInEventDispatchMediator::dispatchEvent):
    (WebCore::FocusOutEventDispatchMediator::dispatchEvent):
    * dom/GestureEvent.cpp:
    (WebCore::GestureEventDispatchMediator::dispatchEvent):
    * dom/MouseEvent.cpp:
    (WebCore::MouseEventDispatchMediator::dispatchEvent):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/EventDispatchMediator.cpp
+++ third_party/WebKit/Source/WebCore/dom/EventDispatchMediator.cpp
@@ -51,7 +51,8 @@ EventDispatchMediator::EventDispatchMediator(PassRefPtr<Event> event)
 
 bool EventDispatchMediator::dispatchEvent(EventDispatcher* dispatcher) const
 {
-    return dispatcher->dispatchEvent(m_event.get());
+    ASSERT(m_event.get() == dispatcher->event());
+    return dispatcher->dispatch();
 }
 
 } // namespace WebCore
