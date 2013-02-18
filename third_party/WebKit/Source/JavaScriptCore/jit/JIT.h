359263712a3269e25aa522fa0c080945dedd83ed
ossy@webkit.org <ossy@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-18 07:18:56 +0000

    Unreviewed buildfix for JSVALUE32_64 builds after r143147.
    * jit/JIT.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
+++ third_party/WebKit/Source/JavaScriptCore/jit/JIT.h
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "JSInterfaceJIT.h"
 #include "LegacyProfiler.h"
 #include "Opcode.h"
+#include "ResultType.h"
 #include "UnusedPointer.h"
 #include <bytecode/SamplingTool.h>
 
