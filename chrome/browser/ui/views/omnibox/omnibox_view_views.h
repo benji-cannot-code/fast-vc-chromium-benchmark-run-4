38cea92e8192bdfd1aa72e0ee493f12ddcc6d92b
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-18 22:55:24 +0000

    Merge AutocompleteTextfield views::Textfield subclass into OmniboxView.
    Simple cleanup while I'm working on solving the related issues. 
    Merge AutocompleteTextfield views::Textfield subclass into OmniboxView.
    Reorder OVERRIDEs to match views::View, cleanup TODOs. 
    Inline DoCopyText, Handle* functions, and LocationEntryFocusable functions. 
    Remove redundant VIEW_ID_AUTOCOMPLETE in favor of VIEW_ID_OMNIBOX.
    
    TODO(msw): Consolidate RemoveBorder with set_border() in Init().
    TODO(msw): Compare selection/text length, not content in IsSelectAll().
    TODO(msw): Return values from Textfield::GetSelectedRange/GetSelectionModel.
    
    BUG=134701,176708
    TEST=No observable omnibox changes, especially CrOS (and Win with --enable-views-textfield).
    R=pkasting@chromium.org,oshima@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12299003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183146 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/omnibox_view_views.h
+++ chrome/browser/ui/views/omnibox/omnibox_view_views.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/toolbar_model.h"
 #include "ui/base/range/range.h"
 #include "ui/base/window_open_disposition.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
-#include "ui/views/view.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/chromeos/input_method/input_method_manager.h"
@@ -30,15 +30,9 @@ class OSExchangeData;
 }  // namespace ui
 
 // Views-implementation of OmniboxView. This is based on gtk implementation.
-// The following features are not yet supported.
-//
-// LTR support.
-// Drag and drop behavior.
-// Adjust paste behavior (should not autocomplete).
-// Custom context menu for omnibox.
-// Instant.
+// TODO(msw): Instant support for Win Aura (or --enable-views-textfield).
 class OmniboxViewViews
-    : public views::View,
+    : public views::Textfield,
       public OmniboxView,
 #if defined(OS_CHROMEOS)
       public
@@ -63,37 +57,18 @@ class OmniboxViewViews
   // Sets the colors of the text view according to the theme.
   void SetBaseColor();
 
-  // Called after key even is handled either by HandleKeyEvent or by Textfield.
-  bool HandleAfterKeyEvent(const ui::KeyEvent& event, bool handled);
-
-  // Called when KeyRelease event is generated on textfield.
-  bool HandleKeyReleaseEvent(const ui::KeyEvent& event);
-
-  // Called when mouse events are generated on the textfield.
-  // The views::Textfield implementations will be executed first.
-  void HandleMousePressEvent(const ui::MouseEvent& event);
-  void HandleMouseDragEvent(const ui::MouseEvent& event);
-  void HandleMouseReleaseEvent(const ui::MouseEvent& event);
-
-  // Called when a gesture event is generated on textfield.
-  void HandleGestureEvent(const ui::GestureEvent& event);
-
-  // Called when Focus is set/unset on textfield.
-  void HandleFocusIn();
-  void HandleFocusOut();
-
-  // Sets whether the location entry can accept focus.
-  void SetLocationEntryFocusable(bool focusable);
-
-  // Returns true if the location entry is focusable and visible in
-  // the root view.
-  bool IsLocationEntryFocusableInRootView() const;
-
-  // Implements views::View
-  virtual void Layout() OVERRIDE;
-  virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
+  // views::Textfield:
   virtual std::string GetClassName() const OVERRIDE;
+  virtual void OnGestureEvent(ui::GestureEvent* event) OVERRIDE;
+  virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
   virtual void OnBoundsChanged(const gfx::Rect& previous_bounds) OVERRIDE;
+  virtual bool OnMousePressed(const ui::MouseEvent& event) OVERRIDE;
+  virtual bool OnMouseDragged(const ui::MouseEvent& event) OVERRIDE;
+  virtual void OnMouseReleased(const ui::MouseEvent& event) OVERRIDE;
+  virtual bool OnKeyPressed(const ui::KeyEvent& event) OVERRIDE;
+  virtual bool OnKeyReleased(const ui::KeyEvent& event) OVERRIDE;
+  virtual void OnFocus() OVERRIDE;
+  virtual void OnBlur() OVERRIDE;
 
   // OmniboxView:
   virtual void SaveStateToTab(content::WebContents* tab) OVERRIDE;
@@ -163,11 +138,8 @@ class OmniboxViewViews
 #endif
 
  private:
-  class AutocompleteTextfield;
-
   // Return the number of characers in the current buffer.
   virtual int GetOmniboxTextLength() const OVERRIDE;
-  size_t GetTextLength() const;
 
   // Try to parse the current text as a URL and colorize the components.
   virtual void EmphasizeURLComponents() OVERRIDE;
@@ -189,8 +161,6 @@ class OmniboxViewViews
   // It is assumed this is invoked after a call to OnBeforePossibleChange() and
   // that after invoking this OnAfterPossibleChange() is invoked.
   void OnPaste();
-
-  views::Textfield* textfield_;
 
   // When true, the location bar view is read only and also is has a slightly
   // different presentation (smaller font size). This is used for popups.
