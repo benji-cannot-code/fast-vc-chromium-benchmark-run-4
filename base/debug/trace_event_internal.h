c9c73e2c4a8f2c302eaaf7275303b47edbf5ded6
dsinclair@chromium.org <dsinclair@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-19 20:14:22 +0000

    Add a missing base:: in the tracing macros.
    When the macro gets expanded outside of the base directory
    there are compile errors. Add the needed base:: to make
    everything happy.
    
    BUG=111509
    
    
    Review URL: https://chromiumcodereview.appspot.com/12289010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183274 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_internal.h
+++ base/debug/trace_event_internal.h
@@ -661,7 +661,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         trace_event_internal::AddTraceEventWithThreadIdAndTimestamp( \
             phase, INTERNAL_TRACE_EVENT_UID(catstatic), \
             name, trace_event_trace_id.data(), \
-            thread_id, TimeTicks::FromInternalValue(timestamp), \
+            thread_id, base::TimeTicks::FromInternalValue(timestamp), \
             trace_event_flags, ##__VA_ARGS__); \
       } \
     } while (0)
