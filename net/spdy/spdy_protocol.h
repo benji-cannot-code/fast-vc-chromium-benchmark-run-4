e91c127e3ba9766c7437ff559e040a65c2a0692d
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-19 22:10:02 +0000

    Remove SpdyDataFrame references from VTL.
    Introduces API for creating SPDY DATA frame headers without payload (to avoid extra payload copies).
    
    This lands server change 42554988.
    
    Review URL: https://codereview.chromium.org/12207193
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183305 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_protocol.h
+++ net/spdy/spdy_protocol.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/logging.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/string_piece.h"
 #include "base/sys_byteorder.h"
 #include "net/base/net_export.h"
@@ -575,17 +576,35 @@ class NET_EXPORT_PRIVATE SpdyFrameWithNameValueBlockIR
   DISALLOW_COPY_AND_ASSIGN(SpdyFrameWithNameValueBlockIR);
 };
 
-class SpdyDataIR : public SpdyFrameWithFinIR {
+class NET_EXPORT_PRIVATE SpdyDataIR
+    : public NON_EXPORTED_BASE(SpdyFrameWithFinIR) {
  public:
-  SpdyDataIR(SpdyStreamId stream_id, const base::StringPiece& data)
-      : SpdyFrameWithFinIR(stream_id) {
-    set_data(data);
-  }
+  // Performs deep copy on data.
+  SpdyDataIR(SpdyStreamId stream_id, const base::StringPiece& data);
+
+  // Use in conjunction with SetDataShallow() for shallow-copy on data.
+  explicit SpdyDataIR(SpdyStreamId stream_id);
+
+  virtual ~SpdyDataIR();
+
   base::StringPiece data() const { return data_; }
-  void set_data(const base::StringPiece& data) { data.CopyToString(&data_); }
+
+  // Deep-copy of data (keep private copy).
+  void SetDataDeep(const base::StringPiece& data) {
+    data_store_.reset(new std::string(data.data(), data.length()));
+    data_ = *(data_store_.get());
+  }
+
+  // Shallow-copy of data (do not keep private copy).
+  void SetDataShallow(const base::StringPiece& data) {
+    data_store_.reset();
+    data_ = data;
+  }
 
  private:
-  std::string data_;
+  // Used to store data that this SpdyDataIR should own.
+  scoped_ptr<std::string> data_store_;
+  base::StringPiece data_;
 
   DISALLOW_COPY_AND_ASSIGN(SpdyDataIR);
 };
