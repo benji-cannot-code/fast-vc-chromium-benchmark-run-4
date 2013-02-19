340bc213bdb68868b9d4f84ba79500e5294991b6
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-19 18:10:24 +0000

    Make the one method in RenderWidgetHostView that wasn't pure-virtual so. Review URL: https://codereview.chromium.org/12294005
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183249 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/test_render_view_host.cc
+++ content/browser/renderer_host/test_render_view_host.cc
@@ -244,6 +244,11 @@ gfx::GLSurfaceHandle TestRenderWidgetHostView::GetCompositingSurface() {
   return gfx::GLSurfaceHandle();
 }
 
+#if defined(OS_WIN) && !defined(USE_AURA)
+void TestRenderWidgetHostView::SetClickthroughRegion(SkRegion* region) {
+}
+#endif
+
 bool TestRenderWidgetHostView::LockMouse() {
   return false;
 }
