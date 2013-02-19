b101c1da1958fef88227e6e52e52c35f3aad7529
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-19 06:57:53 +0000

    [WK2][EFL] Stop using internal C++ API in ewk_error https://bugs.webkit.org/show_bug.cgi?id=108796
    Patch by Christophe Dumez <dchris@gmail.com> on 2013-02-18
    Reviewed by Benjamin Poulain.
    
    Stop using internal C++ API in ewk_error and use C API instead of avoid
    violating API layering.
    
    * UIProcess/API/efl/ewk_error.cpp:
    (EwkError::domain):
    (EwkError::isCancellation):
    (ewk_error_type_get):
    * UIProcess/API/efl/ewk_error_private.h:
    (EwkError):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143297 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_error_private.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/efl/ewk_error_private.h
@@ -28,10 +28,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ewk_error_private_h
 
 #include "WKEinaSharedString.h"
-#include <WKError.h>
 #include <WKRetainPtr.h>
+#include <WebKit2/WKBase.h>
 #include <wtf/PassOwnPtr.h>
-#include <wtf/text/WTFString.h>
 
 class EwkError {
 public:
@@ -45,7 +44,7 @@ public:
 
     const char* url() const;
     const char* description() const;
-    String domain() const;
+    WKRetainPtr<WKStringRef> domain() const;
     int errorCode() const;
     bool isCancellation() const;
 
