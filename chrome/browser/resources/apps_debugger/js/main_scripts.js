2ab6fb07b664852f56130c912b2c3153a4467a8e
grv@chromium.org <grv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-19 05:12:37 +0000

    Adds functionality to pack an extension / app from the developer_app.
    BUG=149036
    
    
    Review URL: https://chromiumcodereview.appspot.com/11794034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183156 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/apps_debugger/js/main_scripts.js
+++ chrome/browser/resources/apps_debugger/js/main_scripts.js
@@ -7,10 +7,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="../../../../../ui/webui/resources/js/cr/ui.js" />
 //<include
 //    src="../../../../../ui/webui/resources/js/i18n_template_no_process.js" />
+//<include src="../../../../../ui/webui/resources/js/cr/ui/alert_overlay.js" />
+//<include src="../../../../../ui/webui/resources/js/cr/ui/overlay.js" />
 //<include src="../../../../../ui/webui/resources/js/i18n_template2.js" />
 //<include src="../../../../../ui/webui/resources/js/load_time_data.js" />
 //<include src="../../../../../ui/webui/resources/js/util.js" />
 //<include src="../../uber/uber_utils.js" />
 //<include src="items_list.js"/>
 //<include src="items.js"/>
+//<include src="pack_item_overlay.js"/>
 //<include src="main.js"/>
