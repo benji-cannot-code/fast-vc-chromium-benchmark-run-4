5a1edf9c7819fa2abfa7d1839d084a39b2b239b5
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-19 06:27:31 +0000

    That didn't work either. Just make it public. This is why I hate nested classes. They just don't work.
    * rendering/RenderBlock.h:
    (RenderBlock):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143294 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.h
@@ -1138,6 +1138,8 @@ protected:
 
     void createFloatingObjects();
 
+public:
+
     class FloatingObjects {
         WTF_MAKE_NONCOPYABLE(FloatingObjects); WTF_MAKE_FAST_ALLOCATED;
     public:
@@ -1177,6 +1179,8 @@ protected:
 
         friend void RenderBlock::createFloatingObjects();
     };
+
+protected:
 
     OwnPtr<FloatingObjects> m_floatingObjects;
 
