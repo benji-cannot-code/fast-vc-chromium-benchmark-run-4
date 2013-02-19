92ddc6abf3741a461685e97f742eda4655cb644d
hyatt@apple.com <hyatt@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-19 23:23:39 +0000

    [New Multicolumn] REGRESSION: RenderMultiColumnSets broken by the RenderRegion -> RenderBlock subclassing. https://bugs.webkit.org/show_bug.cgi?id=110239.
    Reviewed by Simon Fraser.
    
    Source/WebCore: 
    
    Test: fast/multicol/newmulticol/column-rules-fixed-height.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::columnRectAt):
    Make sure the columnGap() in the old multicolumn code is always expressed as a LayoutUnit. This was the
    one place where it was still an int.
    
    * rendering/RenderFlowThread.cpp:
    (WebCore::RenderFlowThread::paintFlowThreadPortionInRegion):
    Rework the painting of flow thread portions to account for the fact that regions paint at an integral
    translation. This means you have to construct clipping around that integral destination. Subpixel layout
    regions did not clip correctly as a result of this issue.
    
    * rendering/RenderMultiColumnSet.cpp:
    (WebCore::RenderMultiColumnSet::columnRectAt):
    Fix the same bug with columnGap() that the old column code has, i.e., one spot where it was an int.
    
    (WebCore::RenderMultiColumnSet::paintObject):
    RenderMultiColumnSet should be using paintObject and not paint and it needs to check for visibility
    and phases now that it is a RenderBlock subclass.
    
    (WebCore::RenderMultiColumnSet::paintColumnRules):
    Fix the bug that Opera guys fixed in the old multi-column code. They didn't patch the new code, so this
    takes care of that.
    
    * rendering/RenderMultiColumnSet.h:
    (RenderMultiColumnSet):
    Change to use paintObject instead of paint.
    
    LayoutTests: 
    
    * fast/multicol/newmulticol: Added.
    * fast/multicol/newmulticol/column-rules-fixed-height-expected.html: Added.
    * fast/multicol/newmulticol/column-rules-fixed-height.html: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -5424,7 +5424,7 @@ LayoutRect RenderBlock::columnRectAt(ColumnInfo* colInfo, unsigned index) const
     LayoutUnit colLogicalHeight = colInfo->columnHeight();
     LayoutUnit colLogicalTop = borderBefore() + paddingBefore();
     LayoutUnit colLogicalLeft = logicalLeftOffsetForContent();
-    int colGap = columnGap();
+    LayoutUnit colGap = columnGap();
     if (colInfo->progressionAxis() == ColumnInfo::InlineAxis) {
         if (style()->isLeftToRightDirection() ^ colInfo->progressionIsReversed())
             colLogicalLeft += index * (colLogicalWidth + colGap);
