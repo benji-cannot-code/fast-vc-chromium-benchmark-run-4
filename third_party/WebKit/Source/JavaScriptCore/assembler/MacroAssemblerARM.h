90be8f2eaa2784e7b280d32e9d666bea00a5e797
zherczeg@webkit.org <zherczeg@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-19 17:12:41 +0000

    revertBranchPtrWithPatch is incorrect on ARM traditional https://bugs.webkit.org/show_bug.cgi?id=110201
    Reviewed by Oliver Hunt.
    
    Revert two instructions back to their original value.
    
    * assembler/ARMAssembler.h:
    (JSC::ARMAssembler::revertBranchPtrWithPatch):
    (ARMAssembler):
    * assembler/MacroAssemblerARM.h:
    (JSC::MacroAssemblerARM::branchPtrWithPatch):
    (JSC::MacroAssemblerARM::revertJumpReplacementToBranchPtrWithPatch):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143346 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARM.h
+++ third_party/WebKit/Source/JavaScriptCore/assembler/MacroAssemblerARM.h
@@ -981,6 +981,7 @@ public:
 
     Jump branchPtrWithPatch(RelationalCondition cond, RegisterID left, DataLabelPtr& dataLabel, TrustedImmPtr initialRightValue = TrustedImmPtr(0))
     {
+        ensureSpace(3 * sizeof(ARMWord), 2 * sizeof(ARMWord));
         dataLabel = moveWithPatch(initialRightValue, ARMRegisters::S1);
         Jump jump = branch32(cond, left, ARMRegisters::S1, true);
         return jump;
@@ -989,6 +990,7 @@ public:
     Jump branchPtrWithPatch(RelationalCondition cond, Address left, DataLabelPtr& dataLabel, TrustedImmPtr initialRightValue = TrustedImmPtr(0))
     {
         load32(left, ARMRegisters::S1);
+        ensureSpace(3 * sizeof(ARMWord), 2 * sizeof(ARMWord));
         dataLabel = moveWithPatch(initialRightValue, ARMRegisters::S0);
         Jump jump = branch32(cond, ARMRegisters::S0, ARMRegisters::S1, true);
         return jump;
@@ -1311,7 +1313,7 @@ public:
 
     static void revertJumpReplacementToBranchPtrWithPatch(CodeLocationLabel instructionStart, RegisterID reg, void* initialValue)
     {
-        ARMAssembler::revertJump(instructionStart.dataLocation(), reg, reinterpret_cast<uintptr_t>(initialValue) & 0xffff);
+        ARMAssembler::revertBranchPtrWithPatch(instructionStart.dataLocation(), reg, reinterpret_cast<uintptr_t>(initialValue) & 0xffff);
     }
 
     static void revertJumpReplacementToPatchableBranchPtrWithPatch(CodeLocationLabel instructionStart, Address, void* initialValue)
