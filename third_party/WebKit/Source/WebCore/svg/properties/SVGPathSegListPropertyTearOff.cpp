04d5a6bfd8fdc11bfdd7bb1dceb2813176298807
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-20 13:38:02 +0000

    Clear SVGPathSeg role on removal. https://bugs.webkit.org/show_bug.cgi?id=110058
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    SVGPathSegListPropertyTearOff::initialize() and SVGPathSegListPropertyTearOff::replaceItem()
    need to clear the context and role for segments being expunged from the list, similarly to
    removeItem(). Otherwise, processIncomingListItemValue() can get confused and attempt to
    remove stale segments.
    
    Test: svg/dom/SVGPathSegList-crash.html
    
    * svg/properties/SVGPathSegListPropertyTearOff.cpp:
    (WebCore::SVGPathSegListPropertyTearOff::clearContextAndRoles):
    (WebCore::SVGPathSegListPropertyTearOff::clear):
    (WebCore::SVGPathSegListPropertyTearOff::replaceItem):
    (WebCore):
    * svg/properties/SVGPathSegListPropertyTearOff.h:
    (WebCore::SVGPathSegListPropertyTearOff::initialize):
    (SVGPathSegListPropertyTearOff):
    
    LayoutTests:
    
    * svg/dom/SVGPathSegList-crash-expected.txt: Added.
    * svg/dom/SVGPathSegList-crash.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143454 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/properties/SVGPathSegListPropertyTearOff.cpp
+++ third_party/WebKit/Source/WebCore/svg/properties/SVGPathSegListPropertyTearOff.cpp
@@ -30,18 +30,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+void SVGPathSegListPropertyTearOff::clearContextAndRoles()
+{
+    ASSERT(m_values);
+    unsigned size = m_values->size();
+    for (unsigned i = 0; i < size; ++i) {
+        ListItemType item = m_values->at(i);
+        static_cast<SVGPathSegWithContext*>(item.get())->setContextAndRole(0, PathSegUndefinedRole);
+    }
+}
+
 void SVGPathSegListPropertyTearOff::clear(ExceptionCode& ec)
 {
     ASSERT(m_values);
     if (m_values->isEmpty())
         return;
 
-    unsigned size = m_values->size();
-    for (unsigned i = 0; i < size; ++i) {
-        ListItemType item = m_values->at(i);
-        static_cast<SVGPathSegWithContext*>(item.get())->setContextAndRole(0, PathSegUndefinedRole);
-    }
-
+    clearContextAndRoles();
     SVGPathSegListPropertyTearOff::Base::clearValues(ec);
 }
 
@@ -53,6 +58,24 @@ SVGPathSegListPropertyTearOff::PassListItemType SVGPathSegListPropertyTearOff::g
         ASSERT(static_cast<SVGPathSegWithContext*>(returnedItem.get())->role() == m_pathSegRole);
     }
     return returnedItem.release();
+}
+
+SVGPathSegListPropertyTearOff::PassListItemType SVGPathSegListPropertyTearOff::replaceItem(PassListItemType passNewItem, unsigned index, ExceptionCode& ec)
+{
+    // Not specified, but FF/Opera do it this way, and it's just sane.
+    if (!passNewItem) {
+        ec = SVGException::SVG_WRONG_TYPE_ERR;
+        return 0;
+    }
+
+    if (index < m_values->size()) {
+        ListItemType replacedItem = m_values->at(index);
+        ASSERT(replacedItem);
+        static_cast<SVGPathSegWithContext*>(replacedItem.get())->setContextAndRole(0, PathSegUndefinedRole);
+    }
+
+    ListItemType newItem = passNewItem;
+    return Base::replaceItemValues(newItem, index, ec);
 }
 
 SVGPathSegListPropertyTearOff::PassListItemType SVGPathSegListPropertyTearOff::removeItem(unsigned index, ExceptionCode& ec)
--- third_party/WebKit/Source/WebCore/svg/properties/SVGPathSegListPropertyTearOff.h
+++ third_party/WebKit/Source/WebCore/svg/properties/SVGPathSegListPropertyTearOff.h
@@ -77,6 +77,7 @@ public:
             return 0;
         }
 
+        clearContextAndRoles();
         ListItemType newItem = passNewItem;
         return Base::initializeValues(newItem, ec);
     }
@@ -95,17 +96,7 @@ public:
         return Base::insertItemBeforeValues(newItem, index, ec);
     }
 
-    PassListItemType replaceItem(PassListItemType passNewItem, unsigned index, ExceptionCode& ec)
-    {
-        // Not specified, but FF/Opera do it this way, and it's just sane.
-        if (!passNewItem) {
-            ec = SVGException::SVG_WRONG_TYPE_ERR;
-            return 0;
-        }
-
-        ListItemType newItem = passNewItem;
-        return Base::replaceItemValues(newItem, index, ec);
-    }
+    PassListItemType replaceItem(PassListItemType, unsigned index, ExceptionCode&);
 
     PassListItemType removeItem(unsigned index, ExceptionCode&);
 
@@ -130,6 +121,8 @@ private:
     }
 
     SVGPathElement* contextElement() const;
+
+    void clearContextAndRoles();
 
     using Base::m_role;
 
