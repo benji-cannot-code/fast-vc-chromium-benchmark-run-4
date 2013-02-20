3efc0ee6d0a24e7132b08c1a5a9f122d3138dbb1
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 18:48:08 +0000

    When signing in to chrome explicitly (first run, ntp, wrench) don't wait for signin manager and/or sync to finish before navigating away from the blank page.  The user will see the NTP much sooner, with the welcome bubble.  In the case of a subsequent error, the NTP is reloaded to show the error message in the bubble.
    BUG=176190
    
    
    Review URL: https://chromiumcodereview.appspot.com/12289003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183575 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_helper.cc
+++ chrome/browser/ui/sync/one_click_signin_helper.cc
@@ -890,7 +890,6 @@ void OneClickSigninHelper::RedirectToNTP() {
   contents->OpenURL(params);
 
   error_message_.clear();
-  signin_tracker_.reset();
 }
 
 void OneClickSigninHelper::CleanTransientState() {
@@ -1106,6 +1105,7 @@ void OneClickSigninHelper::DidStopLoading(
       if (source_ != SyncPromoUI::SOURCE_SETTINGS &&
           source_ != SyncPromoUI::SOURCE_WEBSTORE_INSTALL) {
         signin_tracker_.reset(new SigninTracker(profile, this));
+        RedirectToNTP();
       }
       break;
     }
@@ -1173,8 +1173,9 @@ void OneClickSigninHelper::SigninFailed(const GoogleServiceAuthError& error) {
   }
 
   RedirectToNTP();
+  signin_tracker_.reset();
 }
 
 void OneClickSigninHelper::SigninSuccess() {
-  RedirectToNTP();
+  signin_tracker_.reset();
 }
