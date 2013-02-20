2f82f7bbad26f950414a71c6f2953448f7ca732e
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 10:21:09 +0000

    [gtk] when a shell window is created, its web contents should get focus.
    BUG=159450
    
    
    Review URL: https://chromiumcodereview.appspot.com/12298025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183457 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
+++ chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
@@ -230,6 +230,10 @@ bool NativeAppWindowGtk::IsAlwaysOnTop() const {
   return false;
 }
 
+void NativeAppWindowGtk::RenderViewHostChanged() {
+  web_contents()->Focus();
+}
+
 gfx::Insets NativeAppWindowGtk::GetFrameInsets() const {
   if (frameless_)
     return gfx::Insets();
