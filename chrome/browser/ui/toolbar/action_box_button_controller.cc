1c1c384034e48c5368de67d644704b8b59baef32
rfevang@chromium.org <rfevang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 01:06:12 +0000

    Send selected text and page title to pageLauncher.OnClicked.
    To get to the selected text from the browser side, a method GetSelectedText()
    was added to RenderWidgetHostView.
    
    BUG=125307
    
    
    Review URL: https://chromiumcodereview.appspot.com/12262050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183352 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/toolbar/action_box_button_controller.cc
+++ chrome/browser/ui/toolbar/action_box_button_controller.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/extension_set.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
+#include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
 
@@ -92,14 +93,17 @@ void ActionBoxButtonController::ExecuteCommand(int command_id) {
   if (it != extension_command_ids_.end()) {
     WebContents* web_contents =
         browser_->tab_strip_model()->GetActiveWebContents();
-    // TODO(rfevang): Send page title and selected text.
+
+    std::string page_title = UTF16ToUTF8(web_contents->GetTitle());
+    std::string selected_text =
+        UTF16ToUTF8(web_contents->GetRenderWidgetHostView()->GetSelectedText());
     extensions::PageLauncherAPI::DispatchOnClickedEvent(
         browser_->profile(),
         it->second,
         web_contents->GetURL(),
         web_contents->GetContentsMimeType(),
-        NULL,
-        NULL);
+        page_title.empty() ? NULL : &page_title,
+        selected_text.empty() ? NULL : &selected_text);
     return;
   }
 
