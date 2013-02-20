9fe1dfb6bcac34bae30c21515d6bd9dd06351b3b
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 23:22:17 +0000

    IndexedDB: Restore maximum value size checks and tests
    The plumbing to limit IDB values to 64MB to avoid hitting IPC limitations was removed during various refactors and the tests were disabled. This restores the check and tests. The failure is now reported to script as an asynchronous error callback rather than as a synchronous exception.
    
    BUG=160577,120298
    R=alecflett@chromium.org,dgrogan@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12317023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183650 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/indexed_db/indexed_db_dispatcher.cc
+++ content/common/indexed_db/indexed_db_dispatcher.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/indexed_db/indexed_db_dispatcher.h"
 
+#include "base/format_macros.h"
 #include "base/lazy_instance.h"
+#include "base/stringprintf.h"
 #include "base/threading/thread_local.h"
 #include "content/common/child_thread.h"
 #include "content/common/indexed_db/indexed_db_messages.h"
@@ -30,6 +32,7 @@ using WebKit::WebIDBKey;
 using WebKit::WebIDBKeyRange;
 using WebKit::WebIDBMetadata;
 using WebKit::WebSerializedScriptValue;
+using WebKit::WebString;
 using WebKit::WebVector;
 using base::ThreadLocalPointer;
 using webkit_glue::WorkerTaskRunner;
@@ -400,6 +403,18 @@ void IndexedDBDispatcher::RequestIDBDatabasePut(
     const WebVector<long long>& index_ids,
     const WebVector<WebKit::WebVector<
       WebIDBKey> >& index_keys) {
+
+  if (value.size() > kMaxIDBValueSizeInBytes) {
+    callbacks->onError(WebIDBDatabaseError(
+        WebKit::WebIDBDatabaseExceptionUnknownError,
+        WebString::fromUTF8(
+            base::StringPrintf(
+                "The serialized value is too large"
+                " (size=%" PRIuS " bytes, max=%" PRIuS " bytes).",
+                value.size(), kMaxIDBValueSizeInBytes).c_str())));
+    return;
+  }
+
   ResetCursorPrefetchCaches();
   IndexedDBHostMsg_DatabasePut_Params params;
   init_params(params, callbacks);
--- content/test/data/indexeddb/value_size_test.js
+++ content/test/data/indexeddb/value_size_test.js
@@ -5,10 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 function test()
 {
-  indexedDBTest(prepareDatabase, function() {
-    setTimeout(testUnderLimit, 0);
-  });
+  indexedDBTest(prepareDatabase, testUnderLimit);
 }
+
 function prepareDatabase()
 {
   db = event.target.result;
@@ -32,6 +31,7 @@ var value;
 
 function testUnderLimit()
 {
+  debug("");
   debug('Creating test value under limit');
   var underLimit;
   setTimeout(
@@ -51,20 +51,26 @@ function testUnderLimit()
     request.onerror = unexpectedErrorCallback;
     request.onsuccess = function () {
       debug("store.add succeeded");
+      storeAddSucceeded = true;
+
       debug("store.put underLimit");
       request = store.put(underLimit, key++);
       request.onerror = unexpectedErrorCallback;
       request.onsuccess = function () {
         debug("store.put succeeded");
+        storePutSucceeded = true;
+
+        debug("cursor.update underLimit");
         request = store.openCursor();
         request.onerror = unexpectedErrorCallback;
         request.onsuccess = function () {
           var cursor = request.result;
-          debug("cursor.update underLimit");
           request = cursor.update(underLimit);
           request.onerror = unexpectedErrorCallback;
           request.onsuccess = function () {
             debug("cursor.update succeeded");
+            cursorUpdateSucceeded = true;
+
             transaction.abort();
           };
         };
@@ -72,12 +78,18 @@ function testUnderLimit()
     };
 
     transaction.oncomplete = unexpectedCompleteCallback;
-    transaction.onabort = testOverLimit;
+    transaction.onabort = function() {
+      shouldBeTrue("storeAddSucceeded");
+      shouldBeTrue("storePutSucceeded");
+      shouldBeTrue("cursorUpdateSucceeded");
+      testOverLimit();
+    };
   }
 }
 
 function testOverLimit()
 {
+  debug("");
   debug('Creating test value over limit');
   var overLimit;
   setTimeout(
@@ -90,50 +102,44 @@ function testOverLimit()
     var transaction = db.transaction("store", 'readwrite');
     var store = transaction.objectStore("store");
     var request;
-    try {
-      debug("store.add overLimit");
-      request = store.add(overLimit, key++);
-      request.onsuccess = unexpectedSuccessCallback;
-      fail('store.add - Expected exception, but none was raised');
-    } catch (e) {
-      debug('Exception (expected)');
-      ex = e;
-      shouldBe("ex.code", "0");
-      shouldBe("ex.name", "'DataError'");
-    }
 
-    try {
+    debug("store.add overLimit");
+    request = store.add(overLimit, key++);
+    request.onsuccess = unexpectedSuccessCallback;
+    request.onerror = function (e) {
+      debug("store.add failed: " + e.target.webkitErrorMessage);
+      storeAddFailed = true;
+      e.preventDefault();
+
       debug("store.put overLimit");
       request = store.put(overLimit, key++);
       request.onsuccess = unexpectedSuccessCallback;
-      fail('store.add - Expected exception, but none was raised');
-    } catch (e) {
-      debug('Exception (expected)');
-      ex = e;
-      shouldBe("ex.code", "0");
-      shouldBe("ex.name", "'DataError'");
-    }
+      request.onerror = function (e) {
+        debug("store.put failed: " + e.target.webkitErrorMessage);
+        storePutFailed = true;
+        e.preventDefault();
 
-    request = store.openCursor();
-    request.onerror = unexpectedErrorCallback;
-    request.onsuccess = function () {
-      var cursor = request.result;
-      try {
         debug("cursor.update overLimit");
-        request = cursor.update(overLimit);
-        request.onerror = unexpectedSuccessCallback;
-        fail('cursor.update - Expected exception, but none was raised');
-      } catch (e) {
-        debug('Exception (expected)');
-        ex = e;
-        shouldBe("ex.code", "0");
-        shouldBe("ex.name", "'DataError'");
-
-        transaction.abort();
-      }
+        request = store.openCursor();
+        request.onerror = unexpectedErrorCallback;
+        request.onsuccess = function () {
+          var cursor = request.result;
+          request = cursor.update(overLimit);
+          request.onsuccess = unexpectedSuccessCallback;
+          request.onerror = function (e) {
+            debug("cursor.update failed: " + e.target.webkitErrorMessage);
+            cursorUpdateFailed = true;
+          };
+        };
+      };
     };
 
     transaction.oncomplete = unexpectedCompleteCallback;
-    transaction.onabort = function() { done(); };
+    transaction.onabort = function() {
+      shouldBeTrue("storeAddFailed");
+      shouldBeTrue("storePutFailed");
+      shouldBeTrue("cursorUpdateFailed");
+      done();
+    };
   }
 }
