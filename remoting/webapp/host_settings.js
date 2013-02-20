0cd30cd44a0ad04739d74c94e1ae20c4c62553fc
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 11:58:25 +0000

    Fix remotingHostSettings to load all-hosts setting even if requested hostId has no settings.
    BUG=176581
    
    
    Review URL: https://chromiumcodereview.appspot.com/12279019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183488 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_settings.js
+++ remoting/webapp/host_settings.js
@@ -82,10 +82,12 @@ remoting.HostSettings.loadInternal_ = function(hostId, callback) {
   var onDone = function(allHosts) {
     var result = {};
     try {
-      var result = jsonParseSafe(allHosts[remoting.HostSettings.KEY_]);
-      if (typeof(result) == 'object' &&
-          hostId in result &&
-          typeof(result[hostId]) == 'object') {
+      result = jsonParseSafe(allHosts[remoting.HostSettings.KEY_]);
+      if (typeof(result) != 'object') {
+        console.error("Error loading host settings: Not an object");
+        result = {};
+      } else if (hostId in result &&
+                 typeof(result[hostId]) == 'object') {
         callback(result[hostId], /** @type {Object} */(result));
         return;
       }
@@ -93,7 +95,7 @@ remoting.HostSettings.loadInternal_ = function(hostId, callback) {
       var typedErr = /** @type {*} */ (err);
       console.error('Error loading host settings:', typedErr);
     }
-    callback({}, {});
+    callback({}, /** @type {Object} */(result));
   };
   chrome.storage.local.get(remoting.HostSettings.KEY_, onDone);
 };
