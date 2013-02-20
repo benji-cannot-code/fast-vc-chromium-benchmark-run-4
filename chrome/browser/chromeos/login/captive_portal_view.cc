328530ad6a35b0ae8660b8ea32f3d37d72506c00
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 14:16:19 +0000

    Typo fix.
    BUG=158059
    TBR=nkostylev@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12316008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/captive_portal_view.cc
+++ chrome/browser/chromeos/login/captive_portal_view.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kCaptivePortalStartURL[] = "http://client3.google.com/generate_204";
+const char kCaptivePortalStartURL[] = "http://clients3.google.com/generate_204";
 
 }  // namespace
 
