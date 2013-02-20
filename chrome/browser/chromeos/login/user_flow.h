26d0db3970958b52ac40a7fd3237777cf8078b7e
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 22:34:47 +0000

    Add user flow mechanism
    BUG=176277
    TBR=brettw@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12256039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183637 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/login/user_flow.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_LOGIN_USER_FLOW_H_
+#define CHROME_BROWSER_CHROMEOS_LOGIN_USER_FLOW_H_
+
+#include "base/compiler_specific.h"
+
+namespace chromeos {
+
+// Defines possible variants of user flow upon logging in.
+// See UserManager::SetUserFlow for usage contract.
+class UserFlow {
+ public:
+  virtual bool ShouldLaunchBrowser() = 0;
+  virtual bool ShouldSkipPostLoginScreens() = 0;
+  virtual void LaunchExtraSteps() = 0;
+};
+
+// UserFlow implementation for regular login flow.
+class DefaultUserFlow : public UserFlow {
+  virtual bool ShouldLaunchBrowser() OVERRIDE;
+  virtual bool ShouldSkipPostLoginScreens() OVERRIDE;
+  virtual void LaunchExtraSteps() OVERRIDE;
+};
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_LOGIN_USER_FLOW_H_
