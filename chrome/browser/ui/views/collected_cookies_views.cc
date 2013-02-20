8ce8286fa6a62915205fe6c48e532df2841b3a8b
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 15:08:57 +0000

    views: Remove set_lines_at_root from TreeView API.
    This function does nothing since r180647 - https://chromiumcodereview.appspot.com/12188026
    
    R=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12218176
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183533 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/collected_cookies_views.cc
+++ chrome/browser/ui/views/collected_cookies_views.cc
@@ -339,7 +339,6 @@ views::View* CollectedCookiesViews::CreateAllowedPane() {
   allowed_cookies_tree_->SetModel(allowed_cookies_tree_model_.get());
   allowed_cookies_tree_->SetRootShown(false);
   allowed_cookies_tree_->SetEditable(false);
-  allowed_cookies_tree_->set_lines_at_root(true);
   allowed_cookies_tree_->set_auto_expand_children(true);
   allowed_cookies_tree_->SetController(this);
 
@@ -399,7 +398,6 @@ views::View* CollectedCookiesViews::CreateBlockedPane() {
   blocked_cookies_tree_->SetModel(blocked_cookies_tree_model_.get());
   blocked_cookies_tree_->SetRootShown(false);
   blocked_cookies_tree_->SetEditable(false);
-  blocked_cookies_tree_->set_lines_at_root(true);
   blocked_cookies_tree_->set_auto_expand_children(true);
   blocked_cookies_tree_->SetController(this);
 
