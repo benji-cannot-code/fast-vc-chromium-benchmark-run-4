afc37c3b55131ad292e9644ab45d96c4d747fa95
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-20 18:45:22 +0000

    Remove stretchesToMinIntrinsicLogicalWidth https://bugs.webkit.org/show_bug.cgi?id=110266
    Reviewed by Emil Eklund.
    
    This was only used by fieldsets and now we have a way to express this
    concept in CSS.
    
    * WebCore.order:
    * css/html.css:
    Make fieldsets min-width: -webkit-min-content.
    
    * rendering/RenderBox.cpp:
    (WebCore::RenderBox::computeLogicalWidthInRegion):
    (WebCore::RenderBox::computePositionedLogicalWidth):
    * rendering/RenderBox.h:
    Remove all calls to stretchesToMinIntrinsicLogicalWidth.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderFieldset.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderFieldset.h
@@ -44,7 +44,6 @@ private:
 
     virtual void computePreferredLogicalWidths();
     virtual bool avoidsFloats() const { return true; }
-    virtual bool stretchesToMinIntrinsicLogicalWidth() const OVERRIDE;
 
     virtual void paintBoxDecorations(PaintInfo&, const LayoutPoint&);
     virtual void paintMask(PaintInfo&, const LayoutPoint&);
