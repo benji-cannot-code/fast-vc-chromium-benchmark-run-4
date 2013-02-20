878585b7c15a4ed044485c7912d9eeac7f8c3c85
dsinclair@chromium.org <dsinclair@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 23:02:14 +0000

    Hookup the GPUTrace events.
    Add a factory to GPUTrace to produce either a GL_ARB_timer_query GPUTrace object
    or a SystemTime GPUTrace object.
    
    Hookup GPUTraceController to GLES2DecoderImpl to process any received traces.
    
    BUG=111509
    
    Review URL: https://chromiumcodereview.appspot.com/11416117
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183648 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/build_gles2_cmd_buffer.py
+++ gpu/command_buffer/build_gles2_cmd_buffer.py
@@ -2275,6 +2275,9 @@ _FUNCTION_INFO = {
     'chromium': True,
   },
   'TraceEndCHROMIUM': {
+    'impl_func': False,
+    'immediate': False,
+    'client_test': False,
     'decoder_func': 'DoTraceEndCHROMIUM',
     'unit_test': False,
     'extension': True,
