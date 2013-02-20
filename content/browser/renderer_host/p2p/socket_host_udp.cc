0711501d59fcfdc1026410845860471f41204cad
mallinath@chromium.org <mallinath@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 08:12:11 +0000

    If there are some firewall restrictions are applied on the host and we are trying to send some data, currently we receive error EACCES. After receiving the above error, socket will be reset.
    But in libjingle a single socket can be shared by multiple cricket::Port(s).
    For example in WebRTC, a socket will be shared between UDP and STUN ports.
    
    If we block all packets going to STUN server, then we can't use UDP socket
    when establishing call between two peers within the same local network. 
    
    Also EACCES to me looks like transient error, as firewall rules can be
    dynamically changed and this error should't result in tearing down the 
    socket.
    
    BUG=http://code.google.com/p/webrtc/issues/detail?id=1207
    
    Review URL: https://chromiumcodereview.appspot.com/12304025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183432 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/p2p/socket_host_udp.cc
+++ content/browser/renderer_host/p2p/socket_host_udp.cc
@@ -21,7 +21,8 @@ const int kReadBufferSize = 65536;
 // from sendto() calls.
 bool IsTransientError(int error) {
   return error == net::ERR_ADDRESS_UNREACHABLE ||
-      error == net::ERR_ADDRESS_INVALID;
+         error == net::ERR_ADDRESS_INVALID ||
+         error == net::ERR_ACCESS_DENIED;
 }
 
 }  // namespace
