f36060f06eeeda4838703f870a0b4d8a5a745eea
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-20 02:07:07 +0000

    cc: Don't consider HUD layer for touches
    When paint rects are drawn, the HeadsUpDisplayLayerImpl is created
    and drawn. However, this causes scrolling touches not to hit the
    layer underneath, thus preventing the user from scrolling. The fix
    is to classify the layer as a non-interactive overlay layer and
    to not consider these layers as possible hit layers.
    
    BUG=172572
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/12280014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183371 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_common.cc
+++ cc/layer_tree_host_common.cc
@@ -8,10 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 
 #include "base/debug/trace_event.h"
+#include "cc/heads_up_display_layer_impl.h"
 #include "cc/layer.h"
 #include "cc/layer_impl.h"
 #include "cc/layer_iterator.h"
 #include "cc/layer_sorter.h"
+#include "cc/layer_tree_impl.h"
 #include "cc/math_util.h"
 #include "cc/render_surface.h"
 #include "cc/render_surface_impl.h"
@@ -1153,6 +1155,10 @@ LayerImpl* LayerTreeHostCommon::findLayerThatIsHitByPoint(const gfx::PointF& scr
         // the parents to ensure that the layer was not clipped in such a way that the
         // hit point actually should not hit the layer.
         if (pointIsClippedBySurfaceOrClipRect(screenSpacePoint, currentLayer))
+            continue;
+
+        // Skip the HUD layer.
+        if (currentLayer == currentLayer->layerTreeImpl()->hud_layer())
             continue;
 
         foundLayer = currentLayer;
