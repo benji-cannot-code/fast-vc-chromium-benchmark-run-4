89d2446614e9163bdad35ae051705bb48d7bbcb1
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-20 21:45:56 +0000

    Remove unnecessary includes for Platform.h from headers https://bugs.webkit.org/show_bug.cgi?id=110300
    Patch by Laszlo Gombos <l.gombos@samsung.com> on 2013-02-20
    Reviewed by Alexey Proskuryakov.
    
    Source/WebCore:
    
    wtf/Platform.h only need to be included in a few places directly
    as it is included in config.h. Remove the unnecessay duplicate as
    it can be confusing.
    
    No new tests, no change in functionality.
    
    * platform/SecureTextInput.h:
    * platform/audio/Biquad.h:
    * platform/audio/FFTFrame.h:
    * platform/graphics/cpu/arm/filters/FEBlendNEON.h:
    * platform/graphics/cpu/arm/filters/NEONHelpers.h:
    * platform/graphics/filters/FELighting.h:
    
    Source/WebKit2:
    
    wtf/Platform.h only need to be included in a few places directly
    as it is included in config.h. Remove the unnecessay duplicate as
    it can be confusing.
    
    * Shared/PrintInfo.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143499 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/SecureTextInput.h
+++ third_party/WebKit/Source/WebCore/platform/SecureTextInput.h
@@ -32,8 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SecureTextInput_h
 #define SecureTextInput_h
 
-#include <wtf/Platform.h>
-
 namespace WebCore {
 
 // Once enableSecureTextInput is called, secure text input mode is set until
--- third_party/WebKit/Source/WebCore/platform/graphics/filters/FELighting.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/filters/FELighting.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LightSource.h"
 #include "PointLightSource.h"
 #include "SpotLightSource.h"
-#include <wtf/Platform.h>
 #include <wtf/Uint8ClampedArray.h>
 
 // Common base class for FEDiffuseLighting and FESpecularLighting
