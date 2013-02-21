5843ab960201835c9dfd4480a44b0e9f05412b7e
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 14:34:19 +0000

    Remove a TODO that we don't plan to address (at least not for now). Add comment discouraging use.
    BUG=155525
    
    Review URL: https://codereview.chromium.org/12321008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183810 0039d316-1c4b-4281-b951-d872f2087c98

--- base/prefs/pref_observer.h
+++ base/prefs/pref_observer.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class PrefService;
 
-// TODO(joi): Switch to base::Callback and remove this.
+// Used internally to the Prefs subsystem to pass preference change
+// notifications between PrefService, PrefNotifierImpl and
+// PrefChangeRegistrar.
 class PrefObserver {
  public:
   virtual void OnPreferenceChanged(PrefService* service,
