2b755e27ce1ed215ed2066beaa7aeb6a0b9c5880
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 00:47:32 +0000

    Require authentication when clicking the preview button on the managed mode interstitial. Only when the user successfully authenticates, the preview is shown. The authentication is reset once the user does another navigation.
    BUG=171370
    TEST=Manual,Browser Test
    
    
    Review URL: https://chromiumcodereview.appspot.com/12330007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_mode_interstitial.h
+++ chrome/browser/managed_mode/managed_mode_interstitial.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
+#include "base/memory/weak_ptr.h"
 #include "content/public/browser/interstitial_page_delegate.h"
 #include "googleurl/src/gurl.h"
 
@@ -47,6 +48,9 @@ class ManagedModeInterstitial : public content::InterstitialPageDelegate {
   virtual void OnProceed() OVERRIDE;
   virtual void OnDontProceed() OVERRIDE;
 
+  // Will be called when the passphrase dialog is closed, which is shown after
+  // clicking the preview button.
+  void OnAuthorizationResult(bool success);
   void DispatchContinueRequest(bool continue_request);
 
   // Owns the interstitial, which owns us.
@@ -58,6 +62,7 @@ class ManagedModeInterstitial : public content::InterstitialPageDelegate {
   std::string languages_;
   GURL url_;
 
+  base::WeakPtrFactory<ManagedModeInterstitial> weak_ptr_factory_;
   base::Callback<void(bool)> callback_;
 
   DISALLOW_COPY_AND_ASSIGN(ManagedModeInterstitial);
