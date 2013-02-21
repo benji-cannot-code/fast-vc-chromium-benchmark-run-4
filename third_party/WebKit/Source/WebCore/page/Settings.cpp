a895777b3d0d7c6c7f907ec275f6021e6190ed47
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 23:50:20 +0000

    Unreviewed, rolling out r143536. http://trac.webkit.org/changeset/143536 https://bugs.webkit.org/show_bug.cgi?id=110523
    Causing a bunch of tests to fail on Windows, requires
    rebaselining (Requested by rfong on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-21
    
    Source/WebCore:
    
    * config.h:
    * page/Settings.cpp:
    (WebCore):
    (WebCore::Settings::setShouldPaintNativeControls):
    * page/Settings.h:
    (Settings):
    (WebCore::Settings::shouldPaintNativeControls):
    * platform/win/ScrollbarThemeWin.cpp:
    (WebCore):
    * rendering/RenderThemeWin.cpp:
    (WebCore):
    
    Source/WebKit/win:
    
    * WebKitClassFactory.cpp:
    (WebKitClassFactory::WebKitClassFactory):
    * WebView.cpp:
    (WebView::initWithFrame):
    (WebView::notifyPreferencesChanged):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143658 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Settings.cpp
+++ third_party/WebKit/Source/WebCore/page/Settings.cpp
@@ -89,6 +89,10 @@ double Settings::gDefaultMinDOMTimerInterval = 0.010; // 10 milliseconds
 double Settings::gDefaultDOMTimerAlignmentInterval = 0;
 double Settings::gHiddenPageDOMTimerAlignmentInterval = 1.0;
 
+#if USE(SAFARI_THEME)
+bool Settings::gShouldPaintNativeControls = true;
+#endif
+
 #if USE(AVFOUNDATION)
 bool Settings::gAVFoundationEnabled = false;
 #endif
@@ -482,6 +486,13 @@ FontRenderingMode Settings::fontRenderingMode() const
 {
     return static_cast<FontRenderingMode>(m_fontRenderingMode);
 }
+
+#if USE(SAFARI_THEME)
+void Settings::setShouldPaintNativeControls(bool shouldPaintNativeControls)
+{
+    gShouldPaintNativeControls = shouldPaintNativeControls;
+}
+#endif
 
 void Settings::setDNSPrefetchingEnabled(bool dnsPrefetchingEnabled)
 {
