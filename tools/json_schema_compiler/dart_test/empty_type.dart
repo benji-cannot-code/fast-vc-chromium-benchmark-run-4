2229919814ae16f2e758d6633f3fa59443ad3551
sashab@chromium.org <sashab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 01:49:06 +0000

    Added unit tests for the Dart Chrome.* API wrappers, testing the Dart output mode from JSON Schema Compiler.
    The physical tests I've added are initial dummies - kalman, please take a look at the *structure* of the test setup. It feels like there is a lot of repetition of test names, but I like using python's unittest framework so it runs as a presubmit check. Any hints?
    
    I have a lot more tests I'd like to add.
    
    Note that there needs to be functionality to disable tests, e.g. there will need to be some tests that will fail from the IDL return value type bug (https://code.google.com/p/chromium/issues/detail?id=170837).
    
    Thanks,
    
    Sasha
    
    Review URL: https://chromiumcodereview.appspot.com/12218151
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183709 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/json_schema_compiler/dart_test/empty_type.dart
@@ -0,0 +1,38 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+// Generated from namespace: empty_type
+
+part of chrome;
+
+/**
+ * Types
+ */
+
+class Empty_typeEmptyType extends ChromeObject {
+  /*
+   * Public constructor
+   */
+  Empty_typeEmptyType({}) {
+  }
+
+  /*
+   * Private constructor
+   */
+  Empty_typeEmptyType._proxy(_jsObject) : super._proxy(_jsObject);
+}
+
+/**
+ * Functions
+ */
+
+class API_empty_type {
+  /*
+   * API connection
+   */
+  Object _jsObject;
+  API_empty_type(this._jsObject) {
+  }
+}
