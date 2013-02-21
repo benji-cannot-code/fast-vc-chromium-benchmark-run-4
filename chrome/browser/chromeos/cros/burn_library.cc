3a3f2cdce6a35d425641faa8f777b4606b73a711
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 06:38:01 +0000

    Use base::Callback for UnmountDeviceRecursivelyCallbackType.
    There are two main goals of this refactoring.
    1) Standardize the async implementation in BurnLibrary.
    This is for the preparation of migration between BurnLibrary and BurnManager.
    2) Fix potential (but probably quite rare) crash issue.
    The crash would happen if the instance of BurnLibrary is deleted during
    UnmountDeviceRecursively.
    This CL is a kind of small refactoring, so the method body of
    UnmountDeviceRecursive and its name are cleaned a bit,
    as well as comments about potential issues with TODO related to the target
    method are added.
    
    BUG=126979
    TEST=Ran try server.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12255072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/burn_library.cc
+++ chrome/browser/chromeos/cros/burn_library.cc
@@ -49,7 +49,7 @@ class BurnLibraryImpl : public BurnLibrary {
  private:
   virtual void Init() OVERRIDE;
   void BurnImage();
-  static void DevicesUnmountedCallback(void* object, bool success);
+  void DevicesUnmountedCallback(bool success);
   void UpdateBurnStatus(const ImageBurnStatus& status, BurnEvent evt);
   void OnBurnFinished(const std::string& target_path,
                       bool success,
@@ -162,10 +162,10 @@ void BurnLibraryImpl::OnImageUnzipped(
 
   burning_ = true;
 
-  chromeos::disks::DiskMountManager::GetInstance()->
-      UnmountDeviceRecursive(target_device_path_,
-                             &BurnLibraryImpl::DevicesUnmountedCallback,
-                             this);
+  chromeos::disks::DiskMountManager::GetInstance()->UnmountDeviceRecursively(
+      target_device_path_,
+      base::Bind(&BurnLibraryImpl::DevicesUnmountedCallback,
+                 weak_ptr_factory_.GetWeakPtr()));
 }
 
 void BurnLibraryImpl::OnBurnFinished(const std::string& target_path,
@@ -184,13 +184,11 @@ void BurnLibraryImpl::OnBurnImageFail() {
   UpdateBurnStatus(ImageBurnStatus(0, 0), BURN_FAIL);
 }
 
-void BurnLibraryImpl::DevicesUnmountedCallback(void* object, bool success) {
-  DCHECK(object);
-  BurnLibraryImpl* self = static_cast<BurnLibraryImpl*>(object);
+void BurnLibraryImpl::DevicesUnmountedCallback(bool success) {
   if (success) {
-    self->BurnImage();
+    BurnImage();
   } else {
-    self->UpdateBurnStatus(ImageBurnStatus(0, 0), BURN_FAIL);
+    UpdateBurnStatus(ImageBurnStatus(0, 0), BURN_FAIL);
   }
 }
 
