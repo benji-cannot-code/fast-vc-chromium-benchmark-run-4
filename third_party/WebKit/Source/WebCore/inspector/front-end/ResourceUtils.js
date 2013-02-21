185f5193b4762bafb7e776b2c5f71e271d101e98
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 12:09:38 +0000

    Web Inspector: Get rid of parsedURL in uiSourceCode, use name and path for displayName instead. https://bugs.webkit.org/show_bug.cgi?id=110335
    Reviewed by Alexander Pavlov.
    
    Source/WebCore:
    
    Replaced UISourceCode.parsedURL with UISourceCode.displayName().
    
    * inspector/front-end/NavigatorView.js:
    (WebInspector.NavigatorView.prototype._updateScriptTitle):
    (WebInspector.NavigatorView.prototype.getOrCreateFolderTreeElement):
    (WebInspector.NavigatorFolderTreeElement):
    * inspector/front-end/ResourceUtils.js:
    (WebInspector.displayNameForURL):
    * inspector/front-end/RevisionHistoryView.js:
    (WebInspector.RevisionHistoryView.prototype._createUISourceCodeItem):
    * inspector/front-end/UISourceCode.js:
    (WebInspector.UISourceCode):
    (WebInspector.UISourceCode.prototype.displayName):
    (WebInspector.UISourceCode.prototype.urlChanged):
    
    Source/WebKit/chromium:
    
    * src/js/Tests.js:
    (.TestSuite.prototype._scriptsAreParsed):
    
    LayoutTests:
    
    * http/tests/inspector-enabled/dynamic-scripts.html:
    * http/tests/inspector/debugger-test.js:
    (initialize_DebuggerTest):
    * inspector/debugger/dynamic-scripts.html:
    * inspector/debugger/scripts-file-selector-expected.txt:
    * inspector/debugger/source-url-comment.html:
    * inspector/styles/edit-inspector-stylesheet.html:
    * inspector/styles/styles-add-new-rule.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143583 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/ResourceUtils.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/ResourceUtils.js
@@ -61,7 +61,7 @@ WebInspector.displayNameForURL = function(url)
 
     var uiSourceCode = WebInspector.workspace.uiSourceCodeForURL(url);
     if (uiSourceCode)
-        return uiSourceCode.parsedURL.displayName;
+        return uiSourceCode.displayName();
 
     if (!WebInspector.inspectedPageURL)
         return url.trimURL("");
