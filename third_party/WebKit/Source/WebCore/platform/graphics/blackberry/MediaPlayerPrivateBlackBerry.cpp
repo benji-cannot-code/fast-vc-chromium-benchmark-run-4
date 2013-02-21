f7f8c2d6da49aaed8079ff1500b9b5d5d7dc1297
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 15:38:04 +0000

    [BlackBerry] MediaPlayerPrivateBlackBerry: include Logging.h https://bugs.webkit.org/show_bug.cgi?id=110455
    Patch by Xan Lopez <xlopez@rim.com> on 2013-02-21
    Reviewed by Eric Carlson.
    
    LOG is used in this file, so it is needed.
    
    * platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143600 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/blackberry/MediaPlayerPrivateBlackBerry.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "GraphicsContext.h"
 #include "HTMLNames.h"
 #include "HostWindow.h"
+#include "Logging.h"
 #include "MediaStreamDescriptor.h"
 #include "MediaStreamRegistry.h"
 #include "NotImplemented.h"
