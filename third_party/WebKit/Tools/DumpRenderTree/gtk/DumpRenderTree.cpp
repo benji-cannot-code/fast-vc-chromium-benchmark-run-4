9412bdc04ee0a55d40d06fb289063621ac41883e
mrobinson@webkit.org <mrobinson@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 17:52:34 +0000

    gtk/DumpRenderTree.cpp is missing <locale.h> include https://bugs.webkit.org/show_bug.cgi?id=109386
    * DumpRenderTree/gtk/DumpRenderTree.cpp: Fix the WebKitGTK+ Mac build
    by include locale.h when we use setlocale.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143620 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/gtk/DumpRenderTree.cpp
@@ -51,6 +51,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cstring>
 #include <getopt.h>
 #include <gtk/gtk.h>
+#include <locale.h>
 #include <webkit/webkit.h>
 #include <wtf/Assertions.h>
 #include <wtf/gobject/GOwnPtr.h>
