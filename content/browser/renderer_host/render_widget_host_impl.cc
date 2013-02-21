3ac4e374274896ecdaff3380a49eb23dbae893a3
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 01:54:53 +0000

    [Autofill] Add ability to load Risk fingerprint data in AutofillDialogControllerImpl.
    BUG=166596
    
    
    Review URL: https://chromiumcodereview.appspot.com/12212057
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183714 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_impl.cc
+++ content/browser/renderer_host/render_widget_host_impl.cc
@@ -1255,6 +1255,13 @@ void RenderWidgetHostImpl::RemoveKeyboardListener(
   keyboard_listeners_.remove(listener);
 }
 
+void RenderWidgetHostImpl::GetWebScreenInfo(WebKit::WebScreenInfo* result) {
+  if (GetView())
+    static_cast<RenderWidgetHostViewPort*>(GetView())->GetScreenInfo(result);
+  else
+    RenderWidgetHostViewPort::GetDefaultScreenInfo(result);
+}
+
 void RenderWidgetHostImpl::NotifyScreenInfoChanged() {
   WebKit::WebScreenInfo screen_info;
   GetWebScreenInfo(&screen_info);
@@ -1413,14 +1420,6 @@ void RenderWidgetHostImpl::SetShouldAutoResize(bool enable) {
 bool RenderWidgetHostImpl::IsInOverscrollGesture() const {
   return overscroll_controller_.get() &&
          overscroll_controller_->overscroll_mode() != OVERSCROLL_NONE;
-}
-
-void RenderWidgetHostImpl::GetWebScreenInfo(WebKit::WebScreenInfo* result) {
-  if (GetView()) {
-    static_cast<RenderWidgetHostViewPort*>(GetView())->GetScreenInfo(result);
-  } else {
-    RenderWidgetHostViewPort::GetDefaultScreenInfo(result);
-  }
 }
 
 void RenderWidgetHostImpl::Destroy() {
