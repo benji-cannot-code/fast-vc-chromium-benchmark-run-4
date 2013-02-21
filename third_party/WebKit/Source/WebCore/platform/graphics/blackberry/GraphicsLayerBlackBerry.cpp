84bc91fe83a0314f02f0bcc463ab1fb4bdf9076c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 12:38:50 +0000

    [BlackBerry] GraphicsLayerBlackBerry: rename showDebugBorder to isShowingDebugBorder https://bugs.webkit.org/show_bug.cgi?id=110453
    Patch by Alberto Garcia <albgarcia@rim.com> on 2013-02-21
    Reviewed by Carlos Garcia Campos.
    
    This changed in r133517.
    
    * platform/graphics/blackberry/GraphicsLayerBlackBerry.cpp:
    (WebCore::GraphicsLayerBlackBerry::setupContentsLayer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143587 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/blackberry/GraphicsLayerBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/blackberry/GraphicsLayerBlackBerry.cpp
@@ -902,7 +902,7 @@ void GraphicsLayerBlackBerry::setupContentsLayer(LayerWebKitThread* contentsLaye
 
         updateContentsRect();
 
-        if (showDebugBorders()) {
+        if (isShowingDebugBorder()) {
             setLayerBorderColor(*m_contentsLayer, Color(0, 0, 128, 180));
             m_contentsLayer->setBorderWidth(1);
         }
