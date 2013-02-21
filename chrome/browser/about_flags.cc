83836c1b6a25fd0a39f8d08912eb94326798ddb2
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 00:47:15 +0000

    Add touch editing flag to about:flags
    BUG=115237
    
    
    Review URL: https://chromiumcodereview.appspot.com/12327021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183682 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1251,6 +1251,13 @@ const Experiment kExperiments[] = {
     kOsWin | kOsCrOS,
     SINGLE_VALUE_TYPE(switches::kEnableTouchDragDrop)
   },
+  {
+    "enable-touch-editing",
+    IDS_FLAGS_ENABLE_TOUCH_EDITING_NAME,
+    IDS_FLAGS_ENABLE_TOUCH_EDITING_DESCRIPTION,
+    kOsCrOS,
+    SINGLE_VALUE_TYPE(switches::kEnableTouchEditing)
+  },
 #if defined(ENABLE_MESSAGE_CENTER)
   {
     "enable-rich-notifications",
