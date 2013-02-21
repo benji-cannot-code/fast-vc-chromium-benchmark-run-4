e2d01ba99fd6c6d2f89587b750ba67f12df37df3
pilgrim@chromium.org <pilgrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-21 23:54:05 +0000

    Update #includes (and some comments) since WebKitPlatformSupport class is now defined in Platform.h upstream
    Review URL: https://chromiumcodereview.appspot.com/11824036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183905 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/test/unittest_test_suite.cc
+++ content/public/test/unittest_test_suite.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/rand_util.h"
 #include "base/test/test_suite.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/Platform.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h"
 #include "webkit/compositor_bindings/web_compositor_support_impl.h"
 
 namespace content {
--- webkit/glue/webkitplatformsupport_impl.h
+++ webkit/glue/webkitplatformsupport_impl.h
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/platform_file.h"
 #include "base/threading/thread_local_storage.h"
 #include "base/timer.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/platform/WebKitPlatformSupport.h"
+#include "third_party/WebKit/Source/Platform/chromium/public/Platform.h"
 #include "ui/base/layout.h"
 #include "webkit/glue/resource_loader_bridge.h"
 #include "webkit/glue/webkit_glue_export.h"
