2c2e2824385658aa06358838d915a57f4fbea281
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 03:51:40 +0000

    Provide WKView SPI to defer telling the WebPageProxy and WebProcess about changes in the hosting window https://bugs.webkit.org/show_bug.cgi?id=110415 <rdar://problem/13095405>
    Reviewed by Simon Fraser.
    
    * UIProcess/API/mac/WKView.mm: Add _viewInWindowChangesDeferredCount and _viewInWindowChangeWasDeferred to WKViewData.
    (-[WKView viewDidMoveToWindow]): If we're deferring window changes, don't include the ViewIsInWindow flag
    when sending viewStateDidChange. Instead, save the fact that we want to do that later aside.
    (-[WKView beginDeferringViewInWindowChanges]):
    (-[WKView endDeferringViewInWindowChanges]): Straightforward begin/end pair. When we exit the last pair and
    have a pending window change, let the WebPageProxy know. WebPageProxy will do the check to ensure that
    the in-window state has actually changed.
    (-[WKView isDeferringViewInWindowChanges]):
    * UIProcess/API/mac/WKViewPrivate.h: 
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKViewPrivate.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKViewPrivate.h
@@ -56,4 +56,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (NSView*)fullScreenPlaceholderView;
 
+- (void)beginDeferringViewInWindowChanges;
+- (void)endDeferringViewInWindowChanges;
+- (BOOL)isDeferringViewInWindowChanges;
+
 @end
