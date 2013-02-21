58046f20f1c11347a284df21abbea5c518ea6359
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-21 21:22:55 +0000

    Rebalance the running times of JSRegress somewhat.
    Rubber stamped by Mark Hahnenberg.
    
    * fast/js/regress/script-tests/adapt-to-double-divide.js:
    (foo):
    * fast/js/regress/script-tests/array-access-polymorphic-structure.js:
    * fast/js/regress/script-tests/int-overflow-local.js:
    * fast/js/regress/script-tests/method-on-number.js:
    (foo):
    * fast/js/regress/script-tests/rare-osr-exit-on-local.js:
    * fast/js/regress/script-tests/slow-convergence.js:
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/regress/script-tests/adapt-to-double-divide.js
+++ third_party/WebKit/LayoutTests/fast/js/regress/script-tests/adapt-to-double-divide.js
@@ -2,7 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function foo(a,b) {
     var c = (a + b) / 2;
     var d = a - b;
-    for (var i = 0; i < 100000; ++i) {
+    for (var i = 0; i < 50000; ++i) {
         c *= d;
         c += d;
         c /= d;
@@ -11,13 +11,11 @@ function foo(a,b) {
 }
 
 var result = 0;
-for (var i = 0; i < 100; ++i) {
+for (var i = 0; i < 50; ++i) {
     result += foo(5,6);
 }
 
-if (result != 10000650) {
-    print("Bad result: " + result);
+if (result != 2500325)
     throw "Bad result: " + result;
-}
 
 
--- third_party/WebKit/LayoutTests/fast/js/regress/script-tests/method-on-number.js
+++ third_party/WebKit/LayoutTests/fast/js/regress/script-tests/method-on-number.js
@@ -1,14 +1,12 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function foo(a) {
     var result = 0;
-    for (var i = 0 ; i < 5000000; ++i)
+    for (var i = 0 ; i < 500000; ++i)
         result += a.valueOf();
     return result;
 }
 
 var result = foo(5);
 
-if (result != 25000000) {
-    print("Bad result: " + result);
+if (result != 2500000)
     throw "Bad result: " + result;
-}
