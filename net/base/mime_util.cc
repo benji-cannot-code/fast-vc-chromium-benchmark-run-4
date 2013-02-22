4af3fc38566981051d1021e86a129adcb0fa994d
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 06:23:18 +0000

    net: Add missing "image/vnd.microsoft.icon" MIME type
    BUG=70360
    TEST=icon is displayed instead of downloaded when accessing an icon resource with "image/vnd.microsoft.icon" MIME type
    
    
    Review URL: https://chromiumcodereview.appspot.com/12303021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184050 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_util.cc
+++ net/base/mime_util.cc
@@ -148,6 +148,7 @@ static const MimeInfo secondary_mappings[] = {
   { "application/x-font-woff", "woff" },
   { "image/bmp", "bmp" },
   { "image/x-icon", "ico" },
+  { "image/vnd.microsoft.icon", "ico" },
   { "image/jpeg", "jfif,pjpeg,pjp" },
   { "image/tiff", "tiff,tif" },
   { "image/x-xbitmap", "xbm" },
@@ -256,6 +257,7 @@ static const char* const supported_image_types[] = {
   "image/png",
   "image/gif",
   "image/bmp",
+  "image/vnd.microsoft.icon",    // ico
   "image/x-icon",    // ico
   "image/x-xbitmap"  // xbm
 };
@@ -768,6 +770,7 @@ static const char* const kStandardImageTypes[] = {
   "image/png",
   "image/svg+xml",
   "image/tiff",
+  "image/vnd.microsoft.icon",
   "image/x-cmu-raster",
   "image/x-cmx",
   "image/x-icon",
