d7fddadd6515f91594769303cd0fa5abc34ef3e9
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-22 08:59:11 +0000

    Web Inspector: allow opting out from vertical split in the dock-to-right mode https://bugs.webkit.org/show_bug.cgi?id=110564
    Reviewed by Vsevolod Vlasov.
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/ElementsPanel.js:
    (WebInspector.ElementsPanel.prototype._splitVertically):
    * inspector/front-end/ScriptsPanel.js:
    * inspector/front-end/Settings.js:
    (WebInspector.Settings):
    * inspector/front-end/SettingsScreen.js:
    (WebInspector.GenericSettingsTab):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143701 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/English.lproj/localizedStrings.js
+++ third_party/WebKit/Source/WebCore/English.lproj/localizedStrings.js
@@ -872,3 +872,4 @@ localizedStrings["Create WebSocket"] = "Create WebSocket";
 localizedStrings["Send WebSocket Handshake"] = "Send WebSocket Handshake";
 localizedStrings["Receive WebSocket Handshake"] = "Receive WebSocket Handshake";
 localizedStrings["Destroy WebSocket"] = "Destroy WebSocket";
+localizedStrings["Split panels vertically when docked to right"] = "Split panels vertically when docked to right";
--- third_party/WebKit/Source/WebCore/inspector/front-end/Settings.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/Settings.js
@@ -124,6 +124,7 @@ WebInspector.Settings = function()
     this.workerInspectorWidth = this.createSetting("workerInspectorWidth", 600);
     this.workerInspectorHeight = this.createSetting("workerInspectorHeight", 600);
     this.messageURLFilters = this.createSetting("messageURLFilters", {});
+    this.splitVerticallyWhenDockedToRight = this.createSetting("splitVerticallyWhenDockedToRight", true);
 }
 
 WebInspector.Settings.prototype = {
