1bfa5e052e6383892685ee5787ec4e33a573baac
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 23:07:17 +0000

    Add support for a manager passphrase of a managed account.
    The managed user settings page will be shown with every control disabled. A click on the button "Unlock" shows the passphrase dialog. After a successful authentication, the controls of the settings page are enabled. The authentication is reset when the settings page
    is closed.
    
    BUG=171370
    TEST=browser_tests
    
    Review URL: https://chromiumcodereview.appspot.com/11783008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184206 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/managed_user_settings_handler.h
+++ chrome/browser/ui/webui/options/managed_user_settings_handler.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_OPTIONS_MANAGED_USER_SETTINGS_HANDLER_H_
 #define CHROME_BROWSER_UI_WEBUI_OPTIONS_MANAGED_USER_SETTINGS_HANDLER_H_
 
+#include "base/prefs/public/pref_change_registrar.h"
 #include "base/time.h"
 #include "base/values.h"
 #include "chrome/browser/ui/webui/options/options_ui.h"
@@ -21,6 +22,7 @@ class ManagedUserSettingsHandler : public OptionsPageUIHandler {
   virtual void GetLocalizedValues(
       base::DictionaryValue* localized_strings) OVERRIDE;
 
+  virtual void InitializeHandler() OVERRIDE;
   virtual void InitializePage() OVERRIDE;
   virtual void RegisterMessages() OVERRIDE;
 
@@ -28,8 +30,13 @@ class ManagedUserSettingsHandler : public OptionsPageUIHandler {
   // Save user metrics. Called from WebUI.
   void SaveMetrics(const base::ListValue* args);
 
+  // Called when the local passphrase changes.
+  void OnLocalPassphraseChanged();
+
   // For tracking how long the user spends on this page.
   base::TimeTicks start_time_;
+
+  PrefChangeRegistrar pref_change_registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(ManagedUserSettingsHandler);
 };
