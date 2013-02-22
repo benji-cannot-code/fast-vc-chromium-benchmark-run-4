07c4392e7f4e04d98c019d8bad09dfb18de8441b
shadi@chromium.org <shadi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 08:06:52 +0000

    Disable FrameChangeVideo on Win XP due to flakiness. Enable it on Linux after bug fix (crbug.com/174294).
    BUG=171937
    TBR=ddorwin,xhwang
    
    Review URL: https://chromiumcodereview.appspot.com/12326053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184072 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/encrypted_media_browsertest.cc
+++ content/browser/media/encrypted_media_browsertest.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/stringprintf.h"
 #include "base/utf_string_conversions.h"
+#include "base/win/windows_version.h"
 #include "content/browser/web_contents/web_contents_impl.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/test/browser_test_utils.h"
@@ -163,12 +164,12 @@ IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, Playback_VideoClearAudio_WebM) {
                          GetParam(), kExpected));
 }
 
-#if defined(OS_LINUX)
-#define MAYBE_FrameChangeVideo DISABLED_FrameChangeVideo
-#else
-#define MAYBE_FrameChangeVideo FrameChangeVideo
+IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, FrameChangeVideo) {
+  // Times out on Windows XP. http://crbug.com/171937
+#if defined(OS_WIN)
+  if (base::win::GetVersion() < base::win::VERSION_VISTA)
+    return;
 #endif
-IN_PROC_BROWSER_TEST_P(EncryptedMediaTest, MAYBE_FrameChangeVideo) {
   const string16 kExpected = ASCIIToUTF16("ENDED");
   ASSERT_NO_FATAL_FAILURE(TestFrameSizeChange(GetParam(), kExpected));
 }
