ab5427ea2690a1c2a323b958fe30a171f006b7d7
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 00:05:12 +0000

    Add a telemetry-based gpu_tests folder
    BUG=143317
    
    
    Review URL: https://chromiumcodereview.appspot.com/12252020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183915 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/test/gpu/gpu_tests/__init__.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+"""A library for Chrome GPU test code."""
+import os
+import sys
+
+def Init():
+  telemetry_path = os.path.join(
+    os.path.dirname(__file__),
+    '..', '..', '..', '..', 'tools', 'telemetry')
+  absolute_telemetry_path = os.path.abspath(telemetry_path)
+  sys.path.append(absolute_telemetry_path)
+
+Init()
