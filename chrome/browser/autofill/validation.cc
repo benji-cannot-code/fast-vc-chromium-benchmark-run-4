89e4d301e02fae987be9607a803f976da18272f9
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 23:16:16 +0000

    [Autofill] Validate e-mail address
    R=isherman@chromium.org
    BUG=170467
    
    
    Review URL: https://chromiumcodereview.appspot.com/12212178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184211 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/validation.cc
+++ chrome/browser/autofill/validation.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/validation.h"
 
 #include "base/string_util.h"
+#include "base/utf_string_conversions.h"
+#include "chrome/browser/autofill/autofill_regexes.h"
 #include "chrome/browser/autofill/credit_card.h"
 
 namespace autofill {
@@ -57,6 +59,14 @@ bool IsValidCreditCardSecurityCode(const string16& text) {
       return false;
   }
   return true;
+}
+
+bool IsValidEmailAddress(const string16& text) {
+  // E-Mail pattern as defined by the WhatWG. (4.10.7.1.5 E-Mail state)
+  const string16 kEmailPattern = ASCIIToUTF16(
+      "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@"
+      "[a-zA-Z0-9-]+(?:\\.[a-zA-Z0-9-]+)*$");
+  return autofill::MatchesPattern(text, kEmailPattern);
 }
 
 }  // namespace autofill
