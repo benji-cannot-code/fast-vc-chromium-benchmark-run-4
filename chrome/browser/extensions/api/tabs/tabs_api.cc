e6ff025eb835303e020060d4c8812267faef37d7
meacer@chromium.org <meacer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 22:17:38 +0000

    Add check for url parameter of windows.create API.
    B=177609
    
    Review URL: https://chromiumcodereview.appspot.com/12316059
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184192 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/tabs/tabs_api.cc
+++ chrome/browser/extensions/api/tabs/tabs_api.cc
@@ -539,7 +539,8 @@ bool WindowsCreateFunction::RunImpl() {
 
     // Initialize default window bounds according to window type.
     if (Browser::TYPE_TABBED == window_type ||
-        Browser::TYPE_POPUP == window_type) {
+        Browser::TYPE_POPUP == window_type ||
+        Browser::TYPE_PANEL == window_type) {
       // Try to position the new browser relative to its originating
       // browser window. The call offsets the bounds by kWindowTilePixels
       // (defined in WindowSizer to be 10).
@@ -595,6 +596,9 @@ bool WindowsCreateFunction::RunImpl() {
   }
 
   if (window_type == Browser::TYPE_PANEL) {
+    if (urls.empty())
+      urls.push_back(GURL(chrome::kChromeUINewTabURL));
+
 #if defined(OS_CHROMEOS)
     if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnablePanels) &&
         PanelManager::ShouldUsePanels(extension_id)) {
