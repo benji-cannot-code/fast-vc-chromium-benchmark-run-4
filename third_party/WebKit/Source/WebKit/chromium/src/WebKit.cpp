e6b9031e41a96520c948359dcc32680826cc10d9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-22 17:28:33 +0000

    Unreviewed, rolling out r143734. http://trac.webkit.org/changeset/143734 https://bugs.webkit.org/show_bug.cgi?id=110615
    Causes linux build failures. (Requested by vollick on
    #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-22
    
    * public/WebKit.h:
    (WebKit):
    * src/WebKit.cpp:
    (WebKit::initialize):
    (WebKit::initializeWithoutV8):
    (WebKit::webKitPlatformSupport):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
@@ -92,7 +92,7 @@ static WebThread::TaskObserver* s_endOfTaskRunner = 0;
 // Doing so may cause hard to reproduce crashes.
 static bool s_webKitInitialized = false;
 
-static Platform* s_webKitPlatformSupport = 0;
+static WebKitPlatformSupport* s_webKitPlatformSupport = 0;
 
 static bool generateEntropy(unsigned char* buffer, size_t length)
 {
@@ -110,7 +110,7 @@ static void assertV8RecursionScope()
 }
 #endif
 
-void initialize(Platform* webKitPlatformSupport)
+void initialize(WebKitPlatformSupport* webKitPlatformSupport)
 {
     initializeWithoutV8(webKitPlatformSupport);
 
@@ -129,7 +129,7 @@ void initialize(Platform* webKitPlatformSupport)
     }
 }
 
-void initializeWithoutV8(Platform* webKitPlatformSupport)
+void initializeWithoutV8(WebKitPlatformSupport* webKitPlatformSupport)
 {
     ASSERT(!s_webKitInitialized);
     s_webKitInitialized = true;
@@ -190,7 +190,7 @@ void shutdown()
     WebPrerenderingSupport::shutdown();
 }
 
-Platform* webKitPlatformSupport()
+WebKitPlatformSupport* webKitPlatformSupport()
 {
     return s_webKitPlatformSupport;
 }
