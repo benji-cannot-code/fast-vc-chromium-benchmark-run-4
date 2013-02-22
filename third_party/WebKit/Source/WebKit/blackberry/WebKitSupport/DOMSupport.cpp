a60722fde7c890f044550375c55617d8deb9f852
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-22 08:45:12 +0000

    [BlackBerry] DomSupport: use NodeTraversal APIs https://bugs.webkit.org/show_bug.cgi?id=110467
    Patch by Xan Lopez <xlopez@rim.com> on 2013-02-22
    Reviewed by Antonio Gomes.
    
    * WebKitSupport/DOMSupport.cpp:
    (BlackBerry::WebKit::DOMSupport::visibleTextQuads):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143697 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DOMSupport.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DOMSupport.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLNames.h"
 #include "HTMLTextAreaElement.h"
 #include "Node.h"
+#include "NodeTraversal.h"
 #include "Range.h"
 #include "RenderObject.h"
 #include "RenderText.h"
@@ -75,7 +76,7 @@ void visibleTextQuads(const Range& range, Vector<FloatQuad>& quads, bool useSele
         return;
 
     Node* stopNode = range.pastLastNode();
-    for (Node* node = range.firstNode(); node && node != stopNode; node = node->traverseNextNode()) {
+    for (Node* node = range.firstNode(); node && node != stopNode; node = NodeTraversal::next(node)) {
         RenderObject* r = node->renderer();
         if (!r || !r->isText())
             continue;
