af2efad2bdffb96b932a7b705e7348e13e7cce4a
beidson@apple.com <beidson@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-22 05:56:17 +0000

    Move fastlog2() to WTF/MathExtras.h so it can be used from multiple projects.
    Rubberstamped by Geoff Garen.
    
    Source/WebCore:
    
    * loader/cache/MemoryCache.cpp:
    (WebCore::MemoryCache::lruListFor): Remove the inline fastlog2 and use WTF::fastlog2.
    
    Source/WTF:
    
    * wtf/MathExtras.h:
    (WTF::fastLog2):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/MemoryCache.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/MemoryCache.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WorkerThread.h"
 #include <stdio.h>
 #include <wtf/CurrentTime.h>
+#include <wtf/MathExtras.h>
 #include <wtf/MemoryInstrumentationHashMap.h>
 #include <wtf/MemoryInstrumentationVector.h>
 #include <wtf/MemoryObjectInfo.h>
@@ -413,28 +414,10 @@ void MemoryCache::evict(CachedResource* resource)
     resource->deleteIfPossible();
 }
 
-static inline unsigned fastLog2(unsigned i)
-{
-    unsigned log2 = 0;
-    if (i & (i - 1))
-        log2 += 1;
-    if (i >> 16)
-        log2 += 16, i >>= 16;
-    if (i >> 8)
-        log2 += 8, i >>= 8;
-    if (i >> 4)
-        log2 += 4, i >>= 4;
-    if (i >> 2)
-        log2 += 2, i >>= 2;
-    if (i >> 1)
-        log2 += 1;
-    return log2;
-}
-
 MemoryCache::LRUList* MemoryCache::lruListFor(CachedResource* resource)
 {
     unsigned accessCount = max(resource->accessCount(), 1U);
-    unsigned queueIndex = fastLog2(resource->size() / accessCount);
+    unsigned queueIndex = WTF::fastLog2(resource->size() / accessCount);
 #ifndef NDEBUG
     resource->m_lruIndex = queueIndex;
 #endif
