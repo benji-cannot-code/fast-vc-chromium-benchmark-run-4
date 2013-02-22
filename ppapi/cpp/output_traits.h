024784bd2897d42c8a751f49401a2348a52f25de
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 08:14:42 +0000

    Revert 184054 > Implement ReadEntries API for PPB_DirectoryReader > > The ReadEntries API returns all file entries in the given directory at once. > > API discussion is here: https://codereview.chromium.org/12026008/ > > BUG=113086 > TEST=browser_tests --gtest_filter=\*DirectoryReader\* > > Review URL: https://chromiumcodereview.appspot.com/12090071
    TBR=nhiroki@chromium.org
    Review URL: https://codereview.chromium.org/12319065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184074 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/output_traits.h
+++ ppapi/cpp/output_traits.h
@@ -6,11 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef PPAPI_CPP_OUTPUT_TRAITS_H_
 #define PPAPI_CPP_OUTPUT_TRAITS_H_
 
-#include <vector>
-
 #include "ppapi/c/pp_resource.h"
 #include "ppapi/cpp/array_output.h"
-#include "ppapi/cpp/dev/directory_entry_dev.h"
 
 /// @file
 /// This file defines internal templates for defining how data is passed to the
@@ -233,24 +230,6 @@ struct CallbackOutputTraits< std::vector<pp::Var> > {
 
   // Retrieves the underlying vector that can be passed to the plugin.
   static inline std::vector<pp::Var>& StorageToPluginArg(StorageType& t) {
-    return t.output();
-  }
-};
-
-// A specialization of CallbackOutputTraits to provide the callback system the
-// information on how to handle vectors of pp::DirectoryEntry_Dev. This converts
-// PP_DirectoryEntry_Dev to pp::DirectoryEntry_Dev when passing to the plugin.
-template<>
-struct CallbackOutputTraits< std::vector<pp::DirectoryEntry_Dev> > {
-  typedef PP_ArrayOutput APIArgType;
-  typedef DirectoryEntryArrayOutputAdapterWithStorage StorageType;
-
-  static inline APIArgType StorageToAPIArg(StorageType& t) {
-    return t.pp_array_output();
-  }
-
-  static inline std::vector<pp::DirectoryEntry_Dev>& StorageToPluginArg(
-      StorageType& t) {
     return t.output();
   }
 };
