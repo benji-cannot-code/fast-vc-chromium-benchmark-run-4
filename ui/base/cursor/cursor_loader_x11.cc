426e53298b5f954f6cb8ad26aa059c811835e443
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 20:04:41 +0000

    Clears X-cursor cache after each test so that we don't attempt to use a cursor that is no longer valid. This is necessitated by each test creating a new XDisplay.
    BUG=none
    TEST=none
    R=erg@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12334031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184159 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/cursor/cursor_loader_x11.cc
+++ ui/base/cursor/cursor_loader_x11.cc
@@ -19,7 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 // Returns X font cursor shape from an Aura cursor.
-int CursorShapeFromNative(gfx::NativeCursor native_cursor) {
+int CursorShapeFromNative(const gfx::NativeCursor& native_cursor) {
   switch (native_cursor.native_type()) {
     case ui::kCursorMiddlePanning:
       return XC_fleur;
