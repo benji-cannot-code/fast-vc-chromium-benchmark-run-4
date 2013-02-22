1c0731f2ea2c655c8d4b3e0e3c4f05fc77fc6992
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 02:26:44 +0000

    Remove the notification registrar from NativeAppWindowCocoa.
    ShellWindow already listens to this notification and calls RenderViewChanged(),
    so implement that instead of listening again.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12298024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.h
+++ chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.h
@@ -14,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/ui/cocoa/browser_command_executor.h"
 #include "chrome/browser/ui/extensions/native_app_window.h"
 #include "chrome/browser/ui/extensions/shell_window.h"
-#include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "extensions/common/draggable_region.h"
 #include "ui/gfx/rect.h"
@@ -43,8 +42,7 @@ class SkRegion;
 @end
 
 // Cocoa bridge to AppWindow.
-class NativeAppWindowCocoa : public NativeAppWindow,
-                             public content::NotificationObserver {
+class NativeAppWindowCocoa : public NativeAppWindow {
  public:
   NativeAppWindowCocoa(ShellWindow* shell_window,
                        const ShellWindow::CreateParams& params);
@@ -113,7 +111,7 @@ class NativeAppWindowCocoa : public NativeAppWindow,
       const std::vector<extensions::DraggableRegion>& regions) OVERRIDE;
   virtual void HandleKeyboardEvent(
       const content::NativeWebKeyboardEvent& event) OVERRIDE;
-  virtual void RenderViewHostChanged() OVERRIDE {}
+  virtual void RenderViewHostChanged() OVERRIDE;
   virtual gfx::Insets GetFrameInsets() const OVERRIDE;
 
  private:
@@ -136,11 +134,6 @@ class NativeAppWindowCocoa : public NativeAppWindow,
       const extensions::DraggableRegion* draggable_area);
   void UpdateDraggableRegionsForCustomDrag(
       const std::vector<extensions::DraggableRegion>& regions);
-
-  // Overridden from content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
 
   ShellWindow* shell_window_; // weak - ShellWindow owns NativeAppWindow.
 
@@ -174,8 +167,6 @@ class NativeAppWindowCocoa : public NativeAppWindow,
   // The Extension Command Registry used to determine which keyboard events to
   // handle.
   scoped_ptr<ExtensionKeybindingRegistryCocoa> extension_keybinding_registry_;
-
-  content::NotificationRegistrar registrar_;
 
   DISALLOW_COPY_AND_ASSIGN(NativeAppWindowCocoa);
 };
--- chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.mm
@@ -265,9 +265,6 @@ NativeAppWindowCocoa::NativeAppWindowCocoa(
       window,
       extensions::ExtensionKeybindingRegistry::PLATFORM_APPS_ONLY,
       shell_window));
-  registrar_.Add(this, content::NOTIFICATION_RENDER_VIEW_HOST_CHANGED,
-                 content::Source<content::NavigationController>(
-                     &web_contents()->GetController()));
 }
 
 void NativeAppWindowCocoa::InstallView() {
@@ -644,17 +641,6 @@ void NativeAppWindowCocoa::InstallDraggableRegionViews() {
   }
 }
 
-void NativeAppWindowCocoa::Observe(
-    int type,
-    const content::NotificationSource& source,
-    const content::NotificationDetails& details) {
-  if (type == content::NOTIFICATION_RENDER_VIEW_HOST_CHANGED) {
-    web_contents()->Focus();
-    return;
-  }
-  NOTREACHED();
-}
-
 void NativeAppWindowCocoa::FlashFrame(bool flash) {
   if (flash) {
     attention_request_id_ = [NSApp requestUserAttention:NSInformationalRequest];
@@ -666,6 +652,10 @@ void NativeAppWindowCocoa::FlashFrame(bool flash) {
 
 bool NativeAppWindowCocoa::IsAlwaysOnTop() const {
   return false;
+}
+
+void NativeAppWindowCocoa::RenderViewHostChanged() {
+  web_contents()->Focus();
 }
 
 gfx::Insets NativeAppWindowCocoa::GetFrameInsets() const {
