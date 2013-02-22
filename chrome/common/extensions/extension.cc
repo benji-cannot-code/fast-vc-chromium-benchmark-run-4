fb99be60a444b13f14d099f3e3948ba2064c1038
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 11:27:53 +0000

    *Not* allow extension features if json file fails to load
    BUG=176381
    TEST=Specify a bogus IDR_EXTENSION_PERMISSION_FEATURES file and check that no API's are accessible.
    
    Review URL: https://chromiumcodereview.appspot.com/12326054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184093 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension.cc
+++ chrome/common/extensions/extension.cc
@@ -604,8 +604,10 @@ bool Extension::ParsePermissions(const char* key,
       // permission is added.
       DCHECK(feature);
       // http://crbug.com/176381
-      if (!feature)
+      if (!feature) {
+        to_remove.push_back(it->id());
         continue;
+      }
 
       Feature::Availability availability =
           feature->IsAvailableToManifest(
