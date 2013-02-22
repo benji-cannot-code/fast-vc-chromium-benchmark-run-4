9c7c979c6f66348b6164a7c64f9b0f9d330b7925
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 00:48:18 +0000

    [win aura] show signin screen in app launcher if user isn't signed in.
    This was defaulting for false for USE_ASH, instead of only for OS_CHROMEOS.
    
    BUG=176879
    
    
    Review URL: https://chromiumcodereview.appspot.com/12315026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183945 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/chrome_signin_delegate.cc
+++ chrome/browser/ui/app_list/chrome_signin_delegate.cc
@@ -34,7 +34,7 @@ ChromeSigninDelegate::ChromeSigninDelegate(Profile* profile)
     : profile_(profile) {}
 
 bool ChromeSigninDelegate::NeedSignin()  {
-#if defined(USE_ASH)
+#if defined(OS_CHROMEOS)
   return false;
 #else
   if (!profile_)
