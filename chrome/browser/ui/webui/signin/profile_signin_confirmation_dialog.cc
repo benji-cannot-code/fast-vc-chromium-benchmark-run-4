e17dc0f277d517c4b7216e06df0c36f414c8993d
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 21:16:06 +0000

    Use the active desktop instead of hardcoding HOST_DESKTOP_TYPE_FIRST in ProfileSigninConfirmationDialog.
    Also added an early return if no browser is found on the active desktop.
    
    BUG=177488
    TEST=Confirmation dialog now appears on the active desktop (as opposed to always on the native desktop).
    
    
    Review URL: https://chromiumcodereview.appspot.com/12334012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184177 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/signin/profile_signin_confirmation_dialog.cc
+++ chrome/browser/ui/webui/signin/profile_signin_confirmation_dialog.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/signin/profile_signin_confirmation_dialog.h"
 
 #include "base/json/json_writer.h"
+#include "base/logging.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_dialogs.h"
@@ -21,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/chromium_strings.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
+
+namespace content {
+class WebContents;
+}
 
 namespace {
 
@@ -120,10 +125,22 @@ ProfileSigninConfirmationDialog::ProfileSigninConfirmationDialog(
     signin_with_new_profile_(signin_with_new_profile),
     continue_signin_(continue_signin) {
   Browser* browser = FindBrowserWithProfile(profile,
-                                            chrome::HOST_DESKTOP_TYPE_FIRST);
-  DCHECK(browser);
-  delegate_ = CreateConstrainedWebDialog(
-      profile, this, NULL, browser->tab_strip_model()->GetActiveWebContents());
+                                            chrome::GetActiveDesktop());
+  if (!browser) {
+    DLOG(WARNING) << "No browser found to display the confirmation dialog";
+    cancel_signin_.Run();
+    return;
+  }
+
+  content::WebContents* web_contents =
+      browser->tab_strip_model()->GetActiveWebContents();
+  if (!web_contents) {
+    DLOG(WARNING) << "No web contents found to display the confirmation dialog";
+    cancel_signin_.Run();
+    return;
+  }
+
+  delegate_ = CreateConstrainedWebDialog(profile, this, NULL, web_contents);
 }
 
 ProfileSigninConfirmationDialog::~ProfileSigninConfirmationDialog() {
