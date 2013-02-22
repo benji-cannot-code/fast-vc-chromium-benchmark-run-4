20d37583677541b60052c93c1013efbbe194da86
danhn@chromium.org <danhn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 03:58:01 +0000

    Fixes Linux/Mac time-outs of page_cycler_2012Q2-netsim on perf bots.
    It's due to refactoring of benchmarking extension:
    http://src.chromium.org/viewvc/chrome?view=rev&revision=183611
    New flag enable-net-benchmarking needs to be added to the test.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12317051
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184009 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/perf.py
+++ chrome/test/functional/perf.py
@@ -2300,6 +2300,7 @@ class PageCyclerReplay(object):
           '--enable-experimental-extension-apis',
           '--enable-logging',
           '--enable-benchmarking',
+          '--enable-net-benchmarking',
           '--metrics-recording-only',
           '--activate-on-launch',
           '--no-first-run',
