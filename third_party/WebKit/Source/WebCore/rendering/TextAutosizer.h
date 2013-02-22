3b4aa77b000669fa2ccfa0d2495e63a08a9de018
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-22 18:20:19 +0000

    [TextAutosizing] Refactoring to eliminate boolean parameter. https://bugs.webkit.org/show_bug.cgi?id=110490
    Patch by Anton Vayvod <avayvod@chromium.org> on 2013-02-22
    Reviewed by Julien Chaffraix.
    
    A follow-up to the recent change that introduced a boolean parameter to
    processClusterInternal method of TextAutosizer. Boolean parameters are discouraged by the
    WebKit style guide. See http://trac.webkit.org/changeset/142866
    
    Refactoring so no new tests.
    
    * rendering/TextAutosizer.cpp:
    (WebCore::TextAutosizer::clusterMultiplier):
    
            Calculates the font size multiplier for the specified cluster.
    
    (WebCore::TextAutosizer::processClusterInternal):
    
            Accepts the font size multiplier instead of |shouldBeAutosized|.
    
    (WebCore::TextAutosizer::processCluster):
    (WebCore::TextAutosizer::processCompositeCluster):
    
            Both methods above now calculate the multiplier and then pass it to
            processClusterInternal.
    
    * rendering/TextAutosizer.h:
    
            Updated method declarations.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143749 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/TextAutosizer.h
+++ third_party/WebKit/Source/WebCore/rendering/TextAutosizer.h
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if ENABLE(TEXT_AUTOSIZING)
 
 #include "HTMLNames.h"
+#include "WritingMode.h"
 #include <wtf/Noncopyable.h>
 #include <wtf/PassOwnPtr.h>
 
@@ -63,7 +64,9 @@ private:
 
     explicit TextAutosizer(Document*);
 
-    void processClusterInternal(TextAutosizingClusterInfo&, RenderBlock* container, RenderObject* subtreeRoot, const TextAutosizingWindowInfo&, float textWidth, bool shouldBeAutosized);
+    float clusterMultiplier(WritingMode, const TextAutosizingWindowInfo&, float textWidth) const;
+
+    void processClusterInternal(TextAutosizingClusterInfo&, RenderBlock* container, RenderObject* subtreeRoot, const TextAutosizingWindowInfo&, float multiplier);
     void processCluster(TextAutosizingClusterInfo&, RenderBlock* container, RenderObject* subtreeRoot, const TextAutosizingWindowInfo&);
     void processCompositeCluster(Vector<TextAutosizingClusterInfo>&, const TextAutosizingWindowInfo&);
     void processContainer(float multiplier, RenderBlock* container, TextAutosizingClusterInfo&, RenderObject* subtreeRoot, const TextAutosizingWindowInfo&);
