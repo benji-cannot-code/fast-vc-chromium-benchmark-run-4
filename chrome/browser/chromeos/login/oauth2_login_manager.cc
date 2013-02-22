93dcadcdc81376e83137631f8007c71e7fba107e
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 21:18:13 +0000

    Added switch for passing oauth token from the command line.
    BUG=177608
    TEST=coming...
    
    
    Review URL: https://chromiumcodereview.appspot.com/12321069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184178 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/oauth2_login_manager.cc
+++ chrome/browser/chromeos/login/oauth2_login_manager.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/login/oauth2_login_manager.h"
 
+#include "base/command_line.h"
 #include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
 #include "base/string_util.h"
@@ -16,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/token_service.h"
 #include "chrome/browser/signin/token_service_factory.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/notification_service.h"
 #include "google_apis/gaia/gaia_constants.h"
@@ -76,6 +78,21 @@ void OAuth2LoginManager::RestoreSession(
 void OAuth2LoginManager::ContinueSessionRestore() {
   if (restore_from_auth_cookies_) {
     FetchOAuth2Tokens();
+    return;
+  }
+
+  // Save OAuth2 refresh token from the command line in forced
+  // app mode.
+  CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kForceAppMode) &&
+      command_line->HasSwitch(switches::kAppId) &&
+      command_line->HasSwitch(switches::kAppModeOAuth2Token) &&
+      !command_line->GetSwitchValueASCII(
+          switches::kAppModeOAuth2Token).empty()) {
+    GaiaAuthConsumer::ClientOAuthResult oauth2_tokens;
+    oauth2_tokens.refresh_token =
+        command_line->GetSwitchValueASCII(switches::kAppModeOAuth2Token);
+    StoreOAuth2Tokens(oauth2_tokens);
     return;
   }
 
@@ -163,8 +180,6 @@ void OAuth2LoginManager::Observe(
   switch (type) {
     case chrome::NOTIFICATION_TOKEN_LOADING_FINISHED: {
       refresh_token_ = token_service->GetOAuth2LoginRefreshToken();
-      // TODO(zelidrag): Figure out why just getting GaiaConstants::kGaiaService
-      // token does not do the trick here.
       RestoreSessionCookies();
       break;
     }
