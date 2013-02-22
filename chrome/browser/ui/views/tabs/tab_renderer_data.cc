36d3f9912f55cfbef855dae106c16b535157b7e3
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-22 02:04:24 +0000

    Audio indicator in each tab
    Basically a throbber animation that cycles over a number of
    frames on a bitmap. Currently only 6 frames, which is too little
    but good enough for testing.
    
    Note that the tab.cc logic should make it so that the audio indicator
    is low on the priority. That is, if the tab is recording or broadcasting and
    playing sounds that should show instead of the audio animation.
    
    BUG=3541
    TEST=see bug
    
    Review URL: https://chromiumcodereview.appspot.com/12328027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@183977 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab_renderer_data.cc
+++ chrome/browser/ui/views/tabs/tab_renderer_data.cc
@@ -14,7 +14,8 @@ TabRendererData::TabRendererData()
       mini(false),
       blocked(false),
       app(false),
-      capture_state(CAPTURE_STATE_NONE) {
+      capture_state(CAPTURE_STATE_NONE),
+      audio_state(AUDIO_STATE_NONE) {
 }
 
 TabRendererData::~TabRendererData() {}
@@ -32,5 +33,6 @@ bool TabRendererData::Equals(const TabRendererData& data) {
       mini == data.mini &&
       blocked == data.blocked &&
       app == data.app &&
-      capture_state == data.capture_state;
+      capture_state == data.capture_state &&
+      audio_state == data.audio_state;
 }
