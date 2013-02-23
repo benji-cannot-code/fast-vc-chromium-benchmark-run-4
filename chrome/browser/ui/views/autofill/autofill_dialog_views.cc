2c32002dace409913c2e86ee028b3f67792bd402
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-23 06:06:21 +0000

    Adds AutofillDialogViews::ContentsResized() to combine similar code.
    R=estade@chromium.org
    TBR=sky@chromium.org
    BUG=None
    TEST=compiles, no functional dialog change.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12319077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/autofill_dialog_views.cc
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -436,11 +436,7 @@ void AutofillDialogViews::UpdateAccountChooser() {
 void AutofillDialogViews::UpdateNotificationArea() {
   DCHECK(notification_area_);
   notification_area_->SetNotification(controller_->CurrentNotification());
-
-  if (GetWidget())
-    GetWidget()->SetSize(GetWidget()->non_client_view()->GetPreferredSize());
-
-  contents_->Layout();
+  ContentsPreferredSizeChanged();
 }
 
 void AutofillDialogViews::UpdateSection(DialogSection section) {
@@ -952,8 +948,7 @@ void AutofillDialogViews::UpdateDetailsGroupState(const DetailsGroup& group) {
     group.container->SetVisible(controller_->SectionIsActive(group.section));
   }
 
-  if (GetWidget())
-    GetWidget()->SetSize(GetWidget()->non_client_view()->GetPreferredSize());
+  ContentsPreferredSizeChanged();
 }
 
 bool AutofillDialogViews::AtLeastOneSectionIsEditing() {
@@ -1042,6 +1037,11 @@ void AutofillDialogViews::TextfieldEditedOrActivated(
   // If the field is marked as invalid, check if the text is now valid.
   if (decorated->invalid() && was_edit)
     decorated->SetInvalid(!controller_->InputIsValid(type, textfield->text()));
+}
+
+void AutofillDialogViews::ContentsPreferredSizeChanged() {
+  if (GetWidget())
+    GetWidget()->SetSize(GetWidget()->non_client_view()->GetPreferredSize());
 }
 
 AutofillDialogViews::DetailsGroup* AutofillDialogViews::GroupForSection(
