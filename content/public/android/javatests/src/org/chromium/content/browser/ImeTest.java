d47d6cb073d78d6b604ac4e4badab25796434703
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-23 00:43:17 +0000

    Trying to fix ImeTest flake.
    Trying to fix ImeTest flake on ImeTest$TestAdapterInputConnection.access.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12314082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184227 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
@@ -285,7 +285,8 @@ public class ImeTest extends ContentShellTestBase {
         assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
             @Override
             public boolean isSatisfied() {
-                return show == getImeAdapter().mIsShowWithoutHideOutstanding;
+                return show == getImeAdapter().mIsShowWithoutHideOutstanding &&
+                        (!show || getAdapterInputConnection() != null);
             }
         }));
     }
