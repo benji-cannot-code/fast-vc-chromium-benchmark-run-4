2e7f118c0c306ef7466efd1b2e2a90caaff0dff1
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-23 06:37:04 +0000

    Renaming some enums for clarity and removing unused DialogNotification::type().
    R=estade@chromium.org
    TBR=sky@chromium.org
    BUG=None
    TEST=compiles
    
    
    Review URL: https://chromiumcodereview.appspot.com/12330086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184302 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_types.h
+++ chrome/browser/ui/autofill/autofill_dialog_types.h
@@ -56,7 +56,7 @@ enum DialogSection {
 
 // Termination actions for the dialog.
 enum DialogAction {
-  ACTION_ABORT,
+  ACTION_CANCEL,
   ACTION_SUBMIT,
 };
 
@@ -69,9 +69,9 @@ class DialogNotification {
     NONE,
     REQUIRED_ACTION,
     SECURITY_WARNING,
-    SUBMISSION_OPTION,
     VALIDATION_ERROR,
     WALLET_ERROR,
+    WALLET_PROMO,
   };
 
   DialogNotification();
@@ -85,7 +85,6 @@ class DialogNotification {
   // Whether this notification has an arrow pointing up at the account chooser.
   bool HasArrow() const;
 
-  Type type() const { return type_; }
   const string16& display_text() const { return display_text_; }
 
  private:
