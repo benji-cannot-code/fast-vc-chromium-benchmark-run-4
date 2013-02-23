0d6c78ca234fa33664527a4b44b6b13d575dc5a9
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-23 01:07:36 +0000

    [content shell] enable touch events for layout tests
    Also add the touchDragDropEnabled setting to webpreferences.
    This will make fast/events/touch tests pass.
    
    BUG=111316
    R=tony@chromium.org,creis@chromium.org
    TEST=touch tests pass
    
    Review URL: https://chromiumcodereview.appspot.com/12314032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184242 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_main_delegate.cc
+++ content/shell/shell_main_delegate.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cookies/cookie_monster.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/ui_base_paths.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/gl/gl_switches.h"
 
 #include "ipc/ipc_message.h"  // For IPC_MESSAGE_LOG_ENABLED.
@@ -103,6 +104,8 @@ bool ShellMainDelegate::BasicStartupComplete(int* exit_code) {
     command_line.AppendSwitch(switches::kIgnoreGpuBlacklist);
     command_line.AppendSwitch(switches::kEnableExperimentalWebKitFeatures);
     command_line.AppendSwitch(switches::kEnableCssShaders);
+    command_line.AppendSwitchASCII(switches::kTouchEvents,
+                                   switches::kTouchEventsEnabled);
     if (command_line.HasSwitch(switches::kEnableSoftwareCompositing))
       command_line.AppendSwitch(switches::kEnableSoftwareCompositingGLAdapter);
 
