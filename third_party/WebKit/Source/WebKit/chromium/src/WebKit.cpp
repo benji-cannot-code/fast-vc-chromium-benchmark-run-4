1fcefb001bc4c6281fc2092f7ad4113359731f2d
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-23 19:57:05 +0000

    [Chromium] WebKit::initialize should take a Platform* now that WebKitPlatformSupport is empty https://bugs.webkit.org/show_bug.cgi?id=110605
    Reviewed by Adam Barth.
    
    Part of a larger refactoring series; see tracking bug 82948.
    
    * public/WebKit.h:
    (WebKit):
    * src/WebKit.cpp:
    (WebKit::initialize):
    (WebKit::initializeWithoutV8):
    (WebKit::webKitPlatformSupport):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143846 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebKit.cpp
@@ -92,7 +92,7 @@ static WebThread::TaskObserver* s_endOfTaskRunner = 0;
 // Doing so may cause hard to reproduce crashes.
 static bool s_webKitInitialized = false;
 
-static WebKitPlatformSupport* s_webKitPlatformSupport = 0;
+static Platform* s_webKitPlatformSupport = 0;
 
 static bool generateEntropy(unsigned char* buffer, size_t length)
 {
@@ -110,7 +110,7 @@ static void assertV8RecursionScope()
 }
 #endif
 
-void initialize(WebKitPlatformSupport* webKitPlatformSupport)
+void initialize(Platform* webKitPlatformSupport)
 {
     initializeWithoutV8(webKitPlatformSupport);
 
@@ -129,7 +129,7 @@ void initialize(WebKitPlatformSupport* webKitPlatformSupport)
     }
 }
 
-void initializeWithoutV8(WebKitPlatformSupport* webKitPlatformSupport)
+void initializeWithoutV8(Platform* webKitPlatformSupport)
 {
     ASSERT(!s_webKitInitialized);
     s_webKitInitialized = true;
@@ -190,7 +190,7 @@ void shutdown()
     WebPrerenderingSupport::shutdown();
 }
 
-WebKitPlatformSupport* webKitPlatformSupport()
+Platform* webKitPlatformSupport()
 {
     return s_webKitPlatformSupport;
 }
