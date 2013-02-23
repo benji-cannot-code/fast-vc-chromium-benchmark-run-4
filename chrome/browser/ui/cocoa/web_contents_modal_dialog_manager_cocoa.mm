68a872459d89dccc13889ee9948d7fe21723a8e1
wittman@chromium.org <wittman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-23 02:23:04 +0000

    Factor out uses of WebContentsModalDialog interface functions from WebContentsModalDialogManager
    This CL is part 3 of 6 in the work to remove the WebContentsModalDialog
    interface.  It removes uses of the WebContentsModalDialog interface from
    WebContentsModalDialogManager, in favor of delegating to the appropriate
    NativeWebContentsModalDialogManager subclass.  This adds new subclasses
    for Cocoa and Gtk to handle those platforms.
    
    This builds on part 2 in https://codereview.chromium.org/12276010.
    
    BUG=157161
    
    Review URL: https://chromiumcodereview.appspot.com/12286014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184259 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/web_contents_modal_dialog_manager_cocoa.mm
+++ chrome/browser/ui/cocoa/web_contents_modal_dialog_manager_cocoa.mm
@@ -5,8 +5,53 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/web_contents_modal_dialog_manager.h"
 
+#include "chrome/browser/ui/cocoa/constrained_window/constrained_window_mac.h"
+#include "chrome/browser/ui/native_web_contents_modal_dialog_manager.h"
+
+namespace {
+
+class NativeWebContentsModalDialogManagerCocoa
+    : public NativeWebContentsModalDialogManager {
+ public:
+  NativeWebContentsModalDialogManagerCocoa() {
+  }
+
+  virtual ~NativeWebContentsModalDialogManagerCocoa() {
+  }
+
+  // NativeWebContentsModalDialogManager overrides
+  virtual void ManageDialog(NativeWebContentsModalDialog dialog) OVERRIDE {
+  }
+
+  virtual void ShowDialog(NativeWebContentsModalDialog dialog) OVERRIDE {
+    GetConstrainedWindowMac(dialog)->ShowWebContentsModalDialog();
+  }
+
+  virtual void CloseDialog(NativeWebContentsModalDialog dialog) OVERRIDE {
+    GetConstrainedWindowMac(dialog)->CloseWebContentsModalDialog();
+  }
+
+  virtual void FocusDialog(NativeWebContentsModalDialog dialog) OVERRIDE {
+    GetConstrainedWindowMac(dialog)->FocusWebContentsModalDialog();
+  }
+
+  virtual void PulseDialog(NativeWebContentsModalDialog dialog) OVERRIDE {
+    GetConstrainedWindowMac(dialog)->PulseWebContentsModalDialog();
+  }
+
+ private:
+  static ConstrainedWindowMac* GetConstrainedWindowMac(
+      NativeWebContentsModalDialog dialog) {
+    return static_cast<ConstrainedWindowMac*>(dialog);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(NativeWebContentsModalDialogManagerCocoa);
+};
+
+}  // namespace
+
 NativeWebContentsModalDialogManager*
     WebContentsModalDialogManager::CreateNativeManager(
         NativeWebContentsModalDialogManagerDelegate* native_delegate) {
-  return NULL;
+  return new NativeWebContentsModalDialogManagerCocoa;
 }
