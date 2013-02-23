a6f114e1afc7a20d946396b23696a613cab7b880
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-23 01:08:03 +0000

    Fix a pair of bugs that broke exporting from about:profiler.
    1)  Fix use of undefined variable.
    2)  The content security policy doesn't allow changing the src
    of an iframe, so switch to using an anchor to start the download.
    
    R=eroman@chromium.org
    BUG=177778
    
    Review URL: https://chromiumcodereview.appspot.com/12340034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184244 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/profiler/profiler.js
+++ chrome/browser/resources/profiler/profiler.js
@@ -111,7 +111,7 @@ var MainView = (function() {
   var SNAPSHOT_FILE_LOADER_ID = 'snapshot-file-loader';
   var LOAD_ERROR_ID = 'file-load-error';
 
-  var DOWNLOAD_IFRAME_ID = 'download-iframe';
+  var DOWNLOAD_ANCHOR_ID = 'download-anchor';
 
   // --------------------------------------------------------------------------
   // Row keys
@@ -1800,9 +1800,10 @@ var MainView = (function() {
       };
 
       var dumpText = JSON.stringify(dump, null, ' ');
-      var blobBuilder = new Blob([dumpText, 'native'], {type: 'octet/stream'});
+      var textBlob = new Blob([dumpText, 'native'], { type: 'octet/stream' });
       var blobUrl = window.webkitURL.createObjectURL(textBlob);
-      $(DOWNLOAD_IFRAME_ID).src = blobUrl;
+      $(DOWNLOAD_ANCHOR_ID).href = blobUrl;
+      $(DOWNLOAD_ANCHOR_ID).click();
     },
 
     loadFileChanged_: function() {
