9773ee2ff080d82313a31fd3fab2f4e117b592c0
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-24 17:42:28 +0000

    [GTK] GTK+ 2 build broken since GTK_API_VERSION_2 moved to autoconfig.h https://bugs.webkit.org/show_bug.cgi?id=110702
    Reviewed by Martin Robinson.
    
    Source/WebCore:
    
    No tests. Build fix, no behaviour change.
    
    * platform/gtk/GtkVersioning.h: include the autotoolsconfig.h header,
    which contains the GTK_API_VERSION_2 define now.
    
    Source/WebKit/gtk:
    
    * tests/testapplicationcache.c: include autotoolsconfig.h.
    * tests/testatk.c: ditto.
    * tests/testatkroles.c: ditto.
    * tests/testcontextmenu.c: ditto.
    * tests/testcopyandpaste.c: ditto.
    * tests/testdomdocument.c: ditto.
    * tests/testdomdomwindow.c: ditto.
    * tests/testdomnode.c: ditto.
    * tests/testdownload.c: ditto.
    * tests/testfavicondatabase.c: ditto.
    * tests/testglobals.c: ditto.
    * tests/testhittestresult.c: ditto.
    * tests/testhttpbackend.c: ditto.
    * tests/testkeyevents.c: ditto.
    * tests/testloading.c: ditto.
    * tests/testmimehandling.c: ditto.
    * tests/testnetworkrequest.c: ditto.
    * tests/testnetworkresponse.c: ditto.
    * tests/testwebbackforwardlist.c: ditto.
    * tests/testwebdatasource.c: ditto.
    * tests/testwebframe.c: ditto.
    * tests/testwebhistoryitem.c: ditto.
    * tests/testwebinspector.c: ditto.
    * tests/testwebplugindatabase.c: ditto.
    * tests/testwebresource.c: ditto.
    * tests/testwebsettings.c: ditto.
    * tests/testwebview.c: ditto.
    * tests/testwindow.c: ditto.
    
    Tools:
    
    * GtkLauncher/main.c: include autotoolsconfig.h.
    * Scripts/webkitpy/style/checker.py: make GtkLauncher/main.c exempt of
    the include ordering check, since it uses autotoolsconfig.h which needs to
    come first.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/tests/testwebinspector.c
+++ third_party/WebKit/Source/WebKit/gtk/tests/testwebinspector.c
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * Boston, MA 02110-1301, USA.
  */
 
+#include "autotoolsconfig.h"
 #include "test_utils.h"
 
 #include <string.h>
