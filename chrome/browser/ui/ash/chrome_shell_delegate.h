9ab6c90d7eb724b8615c929fd72f06609bfda410
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-25 23:09:21 +0000

    ash: Remove chromeos ifdefs from chrome_shell_delegate.
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/12208049
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184509 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/chrome_shell_delegate.h
+++ chrome/browser/ui/ash/chrome_shell_delegate.h
@@ -14,12 +14,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 
+class Browser;
+
 namespace ash {
 class WindowPositioner;
-}
-
-namespace views {
-class View;
 }
 
 class ChromeLauncherController;
@@ -93,6 +91,12 @@ class ChromeShellDelegate : public ash::ShellDelegate,
                        const content::NotificationDetails& details) OVERRIDE;
 
  private:
+  void PlatformInit();
+
+  // Returns the browser for active ash window if any. Otherwise it searches
+  // for a browser or create one for default profile and returns it.
+  Browser* GetTargetBrowser();
+
   static ChromeShellDelegate* instance_;
 
   content::NotificationRegistrar registrar_;
