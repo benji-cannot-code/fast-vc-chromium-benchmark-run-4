b047374d18c554236c853ac202640322724a6104
jchaffraix@webkit.org <jchaffraix@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-25 18:08:50 +0000

    [CSS Grid Layout] Refactor RenderStyle's grid position storage in preparation to supporting spanning https://bugs.webkit.org/show_bug.cgi?id=110651
    Reviewed by Ojan Vafai.
    
    The current code stores grid-{row|column} as a single GridPosition value. While this works well currently,
    we want to be able to handle 2 GridPosition as this enables row / column spanning.
    
    That's what this refactoring achieves: it replaces the internal storage by a GridPositions that contains a
    single GridPosition for now. The rest is mechanical updates.
    
    Refactoring covered by existing tests.
    
    * css/CSSComputedStyleDeclaration.cpp:
    (WebCore::valueForGridPositions):
    (WebCore::CSSComputedStyleDeclaration::getPropertyCSSValue):
    Added the simple wrapper valueForGridPositions and updated the rest of the code.
    
    * css/StyleResolver.cpp:
    (WebCore::createGridPositions):
    (WebCore::StyleResolver::applyProperty):
    Ditto with createGridPositions.
    
    * rendering/RenderGrid.cpp:
    (WebCore::RenderGrid::maximumIndexInDirection):
    (WebCore::RenderGrid::placeItemsOnGrid):
    (WebCore::RenderGrid::placeSpecifiedMajorAxisItemsOnGrid):
    (WebCore::RenderGrid::placeAutoMajorAxisItemsOnGrid):
    (WebCore::RenderGrid::placeAutoMajorAxisItemOnGrid):
    (WebCore::RenderGrid::autoPlacementMajorAxisPositionsForChild):
    (WebCore::RenderGrid::autoPlacementMinorAxisPositionsForChild):
    Updated the code after adding the indirection: s/GridPosition/GridPositions/ and
    use the firstPosition helper.
    
    * rendering/RenderGrid.h:
    * rendering/style/GridPosition.h:
    (WebCore::GridPositions::GridPositions):
    (WebCore::GridPositions::firstPosition):
    (WebCore::GridPositions::operator==):
    Added this class to add the indirection. For now, it only wraps the single GridPosition.
    
    * rendering/style/RenderStyle.h:
    * rendering/style/StyleGridItemData.h:
    Updated to store / take / return a GridPositions.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143941 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
+++ third_party/WebKit/Source/WebCore/css/CSSComputedStyleDeclaration.cpp
@@ -1068,6 +1068,12 @@ static PassRefPtr<CSSValue> valueForGridPosition(const GridPosition& position)
 
     return cssValuePool().createValue(position.integerPosition(), CSSPrimitiveValue::CSS_NUMBER);
 }
+
+static PassRefPtr<CSSValue> valueForGridPositions(const GridPositions& positions)
+{
+    return valueForGridPosition(positions.firstPosition());
+}
+
 static PassRefPtr<CSSValue> createTransitionPropertyValue(const Animation* animation)
 {
     RefPtr<CSSValue> propertyValue;
@@ -1926,9 +1932,9 @@ PassRefPtr<CSSValue> CSSComputedStyleDeclaration::getPropertyCSSValue(CSSPropert
             return valueForGridTrackList(style->gridRows(), style.get(), m_node->document()->renderView());
 
         case CSSPropertyWebkitGridColumn:
-            return valueForGridPosition(style->gridItemColumn());
+            return valueForGridPositions(style->gridItemColumn());
         case CSSPropertyWebkitGridRow:
-            return valueForGridPosition(style->gridItemRow());
+            return valueForGridPositions(style->gridItemRow());
 
         case CSSPropertyHeight:
             if (renderer) {
--- third_party/WebKit/Source/WebCore/rendering/style/StyleGridItemData.h
+++ third_party/WebKit/Source/WebCore/rendering/style/StyleGridItemData.h
@@ -55,8 +55,8 @@ public:
         return !(*this == o);
     }
 
-    GridPosition m_gridColumn;
-    GridPosition m_gridRow;
+    GridPositions m_gridColumn;
+    GridPositions m_gridRow;
 
 private:
     StyleGridItemData();
