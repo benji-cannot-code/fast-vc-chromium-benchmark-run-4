ac7020ae6831195eae9deb4e25a452ff0832a7a3
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-25 17:18:25 +0000

    A recent change in Shill (https://gerrit.chromium.org/gerrit/#/c/41664/) uncovered that Chrome isn't sending the WiFi Mode to Shill in case of configuration by policy and user import.
    Changing the ONC to Shill translation code, so that the WiFi mode property will be sent in case of policies and user import.
    
    BUG=177661
    TBR=stevenjb@chromium.org (for test file changes in chromeos/test)
    
    Review URL: https://chromiumcodereview.appspot.com/12320065
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184425 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/onc/onc_translator_onc_to_shill.cc
+++ chromeos/network/onc/onc_translator_onc_to_shill.cc
@@ -136,6 +136,9 @@ void LocalTranslator::TranslateWiFi() {
   TranslateWithTableAndSet(security, kWiFiSecurityTable,
                            flimflam::kSecurityProperty);
 
+  // We currently only support managed and no adhoc networks.
+  shill_dictionary_->SetStringWithoutPathExpansion(flimflam::kModeProperty,
+                                                   flimflam::kModeManaged);
   CopyFieldsAccordingToSignature();
 }
 
