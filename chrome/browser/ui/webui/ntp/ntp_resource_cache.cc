4df61eb8f5ed5e64a6a0919572ac6d5913a78dd0
dconnelly@chromium.org <dconnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-25 19:09:20 +0000

    Add a policy to hide the Web Store on new tabs.
    This change adds a new policy and boolean pref named "HideWebStoreIcon".
    When true, neither the "Web Store" button in the footer of the New Tab
    page nor the "Chrome Web Store" app icon on the Apps page is visible.
    This policy can be dynamically refreshed.
    
    Contributed by dconnelly@chromium.org
    
    BUG=89360
    
    
    Review URL: https://chromiumcodereview.appspot.com/11859029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184454 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
+++ chrome/browser/ui/webui/ntp/ntp_resource_cache.cc
@@ -183,6 +183,7 @@ NTPResourceCache::NTPResourceCache(Profile* profile)
   pref_change_registrar_.Add(prefs::kShowBookmarkBar, callback);
   pref_change_registrar_.Add(prefs::kNtpShownPage, callback);
   pref_change_registrar_.Add(prefs::kSyncPromoShowNTPBubble, callback);
+  pref_change_registrar_.Add(prefs::kHideWebStoreIcon, callback);
 }
 
 NTPResourceCache::~NTPResourceCache() {}
@@ -408,6 +409,8 @@ void NTPResourceCache::CreateNewTabHTML() {
   load_time_data.SetBoolean("isSwipeTrackingFromScrollEventsEnabled",
                             is_swipe_tracking_from_scroll_events_enabled_);
   load_time_data.SetBoolean("showApps", should_show_apps_page_);
+  load_time_data.SetBoolean("showWebStoreIcon",
+                            !prefs->GetBoolean(prefs::kHideWebStoreIcon));
 
 #if defined(OS_CHROMEOS)
   load_time_data.SetString("expandMenu",
