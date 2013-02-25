7a2731508cb380e7fbba0ef86b4a72f27654d6fd
youngki@chromium.org <youngki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-25 17:56:11 +0000

    Added Adapter*Changed() observer calls to BluetoothAdapterWin.
    BUG=135470
    
    
    Review URL: https://chromiumcodereview.appspot.com/12212163
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184435 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth_adapter_win.cc
+++ device/bluetooth/bluetooth_adapter_win.cc
@@ -2,8 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Copyright (c) 2012 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-//
-// TODO(youngki): Implement this file.
 
 #include "device/bluetooth/bluetooth_adapter_win.h"
 
@@ -163,8 +161,18 @@ void BluetoothAdapterWin::AdapterStateChanged(
     const BluetoothTaskManagerWin::AdapterState& state) {
   DCHECK(thread_checker_.CalledOnValidThread());
   name_ = state.name;
+  bool was_present = IsPresent();
+  bool is_present = !state.address.empty();
   address_ = state.address;
-  powered_ = state.powered;
+  if (was_present != is_present) {
+    FOR_EACH_OBSERVER(BluetoothAdapter::Observer, observers_,
+                      AdapterPresentChanged(this, is_present));
+  }
+  if (powered_ != state.powered) {
+    powered_ = state.powered;
+    FOR_EACH_OBSERVER(BluetoothAdapter::Observer, observers_,
+                      AdapterPoweredChanged(this, powered_));
+  }
   if (!initialized_) {
     initialized_ = true;
     init_callback_.Run();
