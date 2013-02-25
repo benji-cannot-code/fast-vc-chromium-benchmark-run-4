5712d3bcd101ce55bae61efcdff1d1e800c92ec0
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-25 20:48:33 +0000

    DFG::Edge should have more bits for UseKind, and DFG::Allocator should be simpler https://bugs.webkit.org/show_bug.cgi?id=110722
    Reviewed by Oliver Hunt.
            
    This rolls out the DFG::Allocator part of http://trac.webkit.org/changeset/143654,
    and changes Edge to have more room for UseKinds and possibly other things.
            
    This is performance-neutral on both 32-bit and 64-bit. It reduces the size of
    DFG::Node on 64-bit (by virtue of getting rid of the 16-byte alignment of Node)
    and increases it slightly on 32-bit (by 4 bytes total - 16-byte alignment led to
    80 bytes, but the base size of Node plus the 12 bytes of new m_encodedWords in
    Edge gets 84 bytes). But, it will mean that we don't have to increase Node by
    another 16 bytes if we ever want to add more UseKinds or other things to Edge.
    
    * dfg/DFGAllocator.h:
    (DFG):
    (Allocator):
    (JSC::DFG::Allocator::Region::headerSize):
    (JSC::DFG::Allocator::Region::numberOfThingsPerRegion):
    (JSC::DFG::Allocator::Region::data):
    (JSC::DFG::Allocator::Region::isInThisRegion):
    (JSC::DFG::::Allocator):
    (JSC::DFG::::~Allocator):
    (JSC::DFG::::allocate):
    (JSC::DFG::::free):
    (JSC::DFG::::freeAll):
    (JSC::DFG::::reset):
    (JSC::DFG::::indexOf):
    (JSC::DFG::::allocatorOf):
    (JSC::DFG::::bumpAllocate):
    (JSC::DFG::::freeListAllocate):
    (JSC::DFG::::allocateSlow):
    (JSC::DFG::::freeRegionsStartingAt):
    (JSC::DFG::::startBumpingIn):
    * dfg/DFGEdge.h:
    (JSC::DFG::Edge::Edge):
    (Edge):
    (JSC::DFG::Edge::node):
    (JSC::DFG::Edge::setNode):
    (JSC::DFG::Edge::useKindUnchecked):
    (JSC::DFG::Edge::setUseKind):
    (JSC::DFG::Edge::operator==):
    (JSC::DFG::Edge::operator!=):
    (JSC::DFG::Edge::makeWord):
    * dfg/DFGNodeAllocator.h:
    (DFG):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@143958 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGAllocator.h
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGAllocator.h
@@ -44,11 +44,7 @@ namespace JSC { namespace DFG {
 //   anything. You can just call freeAll() instead.
 // - You call free() on all T's that you allocated, and never use freeAll().
 
-// forcedCellSize: set this to 0 if you're happy with the default alignment, or set it
-// to a non-zero value if you want to forcibly align the allocations to a certain
-// value. When you do this, Allocator will ASSERT that forcedCellAlignment >= sizeof(T).
-
-template<typename T, unsigned forcedCellAlignment>
+template<typename T>
 class Allocator {
 public:
     Allocator();
@@ -68,24 +64,13 @@ private:
     void* bumpAllocate();
     void* freeListAllocate();
     void* allocateSlow();
-    
-    static size_t cellSize()
-    {
-        if (!forcedCellAlignment)
-            return sizeof(T);
-        
-        ASSERT(forcedCellAlignment >= sizeof(T));
-        return forcedCellAlignment;
-    }
-    
+
     struct Region {
         static size_t size() { return 64 * KB; }
-        static size_t headerSize() { return std::max(sizeof(Region), cellSize()); }
-        static unsigned numberOfThingsPerRegion() { return (size() - headerSize()) / cellSize(); }
-        static size_t payloadSize() { return numberOfThingsPerRegion() * cellSize(); }
-        char* payloadBegin() { return bitwise_cast<char*>(this) + headerSize(); }
-        char* payloadEnd() { return payloadBegin() + payloadSize(); }
-        bool isInThisRegion(const T* pointer) { return static_cast<size_t>(bitwise_cast<char*>(pointer) - payloadBegin()) < payloadSize(); }
+        static size_t headerSize() { return std::max(sizeof(Region), sizeof(T)); }
+        static unsigned numberOfThingsPerRegion() { return (size() - headerSize()) / sizeof(T); }
+        T* data() { return bitwise_cast<T*>(bitwise_cast<char*>(this) + headerSize()); }
+        bool isInThisRegion(const T* pointer) { return static_cast<unsigned>(pointer - data()) < numberOfThingsPerRegion(); }
         static Region* regionFor(const T* pointer) { return bitwise_cast<Region*>(bitwise_cast<uintptr_t>(pointer) & ~(size() - 1)); }
         
         PageAllocationAligned m_allocation;
@@ -98,26 +83,26 @@ private:
     
     Region* m_regionHead;
     void** m_freeListHead;
-    char* m_bumpEnd;
-    size_t m_bumpRemaining;
+    T* m_bumpEnd;
+    unsigned m_bumpRemaining;
 };
 
-template<typename T, unsigned forcedCellAlignment>
-inline Allocator<T, forcedCellAlignment>::Allocator()
+template<typename T>
+inline Allocator<T>::Allocator()
     : m_regionHead(0)
     , m_freeListHead(0)
     , m_bumpRemaining(0)
 {
 }
 
-template<typename T, unsigned forcedCellAlignment>
-inline Allocator<T, forcedCellAlignment>::~Allocator()
+template<typename T>
+inline Allocator<T>::~Allocator()
 {
     reset();
 }
 
-template<typename T, unsigned forcedCellAlignment>
-ALWAYS_INLINE void* Allocator<T, forcedCellAlignment>::allocate()
+template<typename T>
+ALWAYS_INLINE void* Allocator<T>::allocate()
 {
     void* result = bumpAllocate();
     if (LIKELY(!!result))
@@ -125,8 +110,8 @@ ALWAYS_INLINE void* Allocator<T, forcedCellAlignment>::allocate()
     return freeListAllocate();
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void Allocator<T, forcedCellAlignment>::free(T* object)
+template<typename T>
+void Allocator<T>::free(T* object)
 {
     object->~T();
     
@@ -135,8 +120,8 @@ void Allocator<T, forcedCellAlignment>::free(T* object)
     m_freeListHead = cell;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void Allocator<T, forcedCellAlignment>::freeAll()
+template<typename T>
+void Allocator<T>::freeAll()
 {
     if (!m_regionHead) {
         ASSERT(!m_bumpRemaining);
@@ -158,8 +143,8 @@ void Allocator<T, forcedCellAlignment>::freeAll()
     startBumpingIn(m_regionHead);
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void Allocator<T, forcedCellAlignment>::reset()
+template<typename T>
+void Allocator<T>::reset()
 {
     freeRegionsStartingAt(m_regionHead);
     
@@ -168,38 +153,38 @@ void Allocator<T, forcedCellAlignment>::reset()
     m_bumpRemaining = 0;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-unsigned Allocator<T, forcedCellAlignment>::indexOf(const T* object)
+template<typename T>
+unsigned Allocator<T>::indexOf(const T* object)
 {
     unsigned baseIndex = 0;
     for (Region* region = m_regionHead; region; region = region->m_next) {
         if (region->isInThisRegion(object))
-            return baseIndex + (bitwise_cast<char*>(object) - region->payloadBegin()) / cellSize();
+            return baseIndex + (object - region->data());
         baseIndex += Region::numberOfThingsPerRegion();
     }
     CRASH();
     return 0;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-Allocator<T, forcedCellAlignment>* Allocator<T, forcedCellAlignment>::allocatorOf(const T* object)
+template<typename T>
+Allocator<T>* Allocator<T>::allocatorOf(const T* object)
 {
     return Region::regionFor(object)->m_allocator;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-ALWAYS_INLINE void* Allocator<T, forcedCellAlignment>::bumpAllocate()
+template<typename T>
+ALWAYS_INLINE void* Allocator<T>::bumpAllocate()
 {
-    if (size_t remaining = m_bumpRemaining) {
-        remaining -= cellSize();
+    if (unsigned remaining = m_bumpRemaining) {
+        remaining--;
         m_bumpRemaining = remaining;
-        return m_bumpEnd - (remaining + cellSize());
+        return m_bumpEnd - (remaining + 1);
     }
     return 0;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void* Allocator<T, forcedCellAlignment>::freeListAllocate()
+template<typename T>
+void* Allocator<T>::freeListAllocate()
 {
     void** result = m_freeListHead;
     if (UNLIKELY(!result))
@@ -208,8 +193,8 @@ void* Allocator<T, forcedCellAlignment>::freeListAllocate()
     return result;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void* Allocator<T, forcedCellAlignment>::allocateSlow()
+template<typename T>
+void* Allocator<T>::allocateSlow()
 {
     ASSERT(!m_freeListHead);
     ASSERT(!m_bumpRemaining);
@@ -232,8 +217,8 @@ void* Allocator<T, forcedCellAlignment>::allocateSlow()
     return result;
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void Allocator<T, forcedCellAlignment>::freeRegionsStartingAt(typename Allocator<T, forcedCellAlignment>::Region* region)
+template<typename T>
+void Allocator<T>::freeRegionsStartingAt(typename Allocator<T>::Region* region)
 {
     while (region) {
         Region* nextRegion = region->m_next;
@@ -242,11 +227,11 @@ void Allocator<T, forcedCellAlignment>::freeRegionsStartingAt(typename Allocator
     }
 }
 
-template<typename T, unsigned forcedCellAlignment>
-void Allocator<T, forcedCellAlignment>::startBumpingIn(typename Allocator<T, forcedCellAlignment>::Region* region)
+template<typename T>
+void Allocator<T>::startBumpingIn(typename Allocator<T>::Region* region)
 {
-    m_bumpEnd = region->payloadEnd();
-    m_bumpRemaining = Region::payloadSize();
+    m_bumpEnd = region->data() + Region::numberOfThingsPerRegion();
+    m_bumpRemaining = Region::numberOfThingsPerRegion();
 }
 
 } } // namespace JSC::DFG
