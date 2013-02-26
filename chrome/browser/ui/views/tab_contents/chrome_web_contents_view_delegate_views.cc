c8038b451f59a7a03c58d332bd3d3f627a6ec80a
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 19:09:16 +0000

    Remove WebContents::SetFocusToLocationBar since it duplicates a method in WebContentsDelegate. Review URL: https://codereview.chromium.org/12340068
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184684 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.cc
+++ chrome/browser/ui/views/tab_contents/chrome_web_contents_view_delegate_views.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/web_contents_delegate.h"
 #include "content/public/browser/web_contents_view.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/focus/view_storage.h"
@@ -191,7 +192,8 @@ views::FocusManager*
 
 void ChromeWebContentsViewDelegateViews::SetInitialFocus() {
   if (web_contents_->FocusLocationBarByDefault()) {
-    web_contents_->SetFocusToLocationBar(false);
+    if (web_contents_->GetDelegate())
+      web_contents_->GetDelegate()->SetFocusToLocationBar(false);
   } else {
     web_contents_->GetView()->Focus();
   }
