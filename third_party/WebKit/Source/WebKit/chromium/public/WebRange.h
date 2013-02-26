2c3eb7ad61c917a70fdf03418b4674cd9a278771
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-26 03:18:26 +0000

    Add expandedToParagraph() method to WebRange https://bugs.webkit.org/show_bug.cgi?id=110618
    Patch by Rouslan Solomakhin <rouslan@chromium.org> on 2013-02-25
    Reviewed by Adam Barth.
    
    Add a method expandedToParagraph() to WebRange to return a copy of the
    range expanded to paragraph boundaries. This method is to be used to
    query the spelling context around the misspelled word under cursor.
    
    * public/WebRange.h:
    (WebRange): Added expandedToParagraph() method declaration.
    * src/WebRange.cpp:
    (WebKit::WebRange::expandedToParagraph): Added method to copy range, expand the copied range to paragraph boundaries, and return the resulting range.
    (WebKit): Added expandedToParagraph() method definition.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144002 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebRange.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebRange.h
@@ -74,6 +74,8 @@ public:
     WEBKIT_EXPORT WebString toHTMLText() const;
     WEBKIT_EXPORT WebString toPlainText() const;
 
+    WEBKIT_EXPORT WebRange expandedToParagraph() const;
+
     WEBKIT_EXPORT static WebRange fromDocumentRange(WebFrame*, int start, int length);
 
     WEBKIT_EXPORT WebVector<WebFloatQuad> textQuads() const;
