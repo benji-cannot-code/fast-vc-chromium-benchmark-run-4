4c7bc99c9761c942d66718427251b2fcc6595e14
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 06:28:18 +0000

    ash: Use immersive mode for fullscreen
    * Remove immersive mode button from maximized window frame
    * Tie immersive mode to browser-fullscreen only, keep tab-fullscreen
      with existing UI 
    * Size button exits immersive mode
    * Add ImageButton::GetImage for testing
    
    BUG=177549
    TEST=added to browser_tests views_unittests ash_unittests
    
    
    Review URL: https://chromiumcodereview.appspot.com/12316086
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184600 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_cocoa.h
+++ chrome/browser/ui/cocoa/browser_window_cocoa.h
@@ -73,6 +73,7 @@ class BrowserWindowCocoa :
   virtual void UpdateFullscreenExitBubbleContent(
       const GURL& url,
       FullscreenExitBubbleType bubble_type) OVERRIDE;
+  virtual bool ShouldHideUIForFullscreen() const OVERRIDE;
   virtual bool IsFullscreen() const OVERRIDE;
   virtual bool IsFullscreenBubbleVisible() const OVERRIDE;
   virtual LocationBar* GetLocationBar() const OVERRIDE;
--- chrome/browser/ui/views/frame/browser_view.cc
+++ chrome/browser/ui/views/frame/browser_view.cc
@@ -122,6 +122,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/window/dialog_delegate.h"
 
 #if defined(USE_ASH)
+#include "ash/ash_switches.h"
 #include "ash/launcher/launcher.h"
 #include "ash/launcher/launcher_model.h"
 #include "ash/shell.h"
@@ -193,6 +194,19 @@ bool ShouldSaveOrRestoreWindowPos() {
     return false;
 #endif
   return true;
+}
+
+// Returns whether immersive mode should replace fullscreen, which should only
+// occur for "browser-fullscreen" and not for "tab-fullscreen" (which has a URL
+// for the tab entering fullscreen).
+bool UseImmersiveFullscreen(const GURL& url) {
+#if defined(USE_ASH)
+  bool is_browser_fullscreen = url.is_empty();
+  return is_browser_fullscreen &&
+      CommandLine::ForCurrentProcess()->HasSwitch(
+          ash::switches::kAshImmersiveMode);
+#endif
+  return false;
 }
 
 }  // namespace
@@ -767,7 +781,10 @@ void BrowserView::ExitFullscreen() {
 void BrowserView::UpdateFullscreenExitBubbleContent(
     const GURL& url,
     FullscreenExitBubbleType bubble_type) {
-  if (bubble_type == FEB_TYPE_NONE) {
+  // Immersive mode has no exit bubble because it has a visible strip at the
+  // top that gives the user a hover target.
+  // TODO(jamescook): Figure out what to do with mouse-lock.
+  if (bubble_type == FEB_TYPE_NONE || UseImmersiveFullscreen(url)) {
     fullscreen_bubble_.reset();
   } else if (fullscreen_bubble_.get()) {
     fullscreen_bubble_->UpdateContent(url, bubble_type);
@@ -775,6 +792,14 @@ void BrowserView::UpdateFullscreenExitBubbleContent(
     fullscreen_bubble_.reset(new FullscreenExitBubbleViews(
         GetWidget(), browser_.get(), url, bubble_type));
   }
+}
+
+bool BrowserView::ShouldHideUIForFullscreen() const {
+#if defined(USE_ASH)
+  // Immersive reveal needs UI for the slide-down top panel.
+  return IsFullscreen() && !immersive_mode_controller_->IsRevealed();
+#endif
+  return IsFullscreen();
 }
 
 bool BrowserView::IsFullscreen() const {
@@ -2283,7 +2308,9 @@ void BrowserView::ProcessFullscreen(bool fullscreen,
   browser_->WindowFullscreenStateChanged();
 
   if (fullscreen) {
-    if (!chrome::IsRunningInAppMode() && type != FOR_METRO) {
+    if (!chrome::IsRunningInAppMode() &&
+        type != FOR_METRO &&
+        !UseImmersiveFullscreen(url)) {
       fullscreen_bubble_.reset(new FullscreenExitBubbleViews(
           GetWidget(), browser_.get(), url, bubble_type));
     }
@@ -2296,6 +2323,9 @@ void BrowserView::ProcessFullscreen(bool fullscreen,
     }
 #endif
   }
+
+  if (UseImmersiveFullscreen(url))
+    immersive_mode_controller_->SetEnabled(fullscreen);
 
   // Undo our anti-jankiness hacks and force the window to re-layout now that
   // it's in its final position.
