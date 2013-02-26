710aa1f926bb94b494c1219b54145ab902f770fe
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-26 15:53:23 +0000

    Unreviewed, rolling out r144054. http://trac.webkit.org/changeset/144054 https://bugs.webkit.org/show_bug.cgi?id=110854
    broke builds
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * JavaScriptCore.gypi:
    * JavaScriptCore.vcproj/JavaScriptCore/JavaScriptCore.vcproj:
    * JavaScriptCore.vcxproj/JavaScriptCore.vcxproj:
    * JavaScriptCore.xcodeproj/project.pbxproj:
    * Target.pri:
    * runtime/JSGlobalData.cpp:
    (JSC::JSGlobalData::JSGlobalData):
    * runtime/JSGlobalData.h:
    (JSGlobalData):
    * runtime/PropertyMapHashTable.h:
    (PropertyTable):
    (JSC::PropertyTable::PropertyTable):
    (JSC):
    (JSC::PropertyTable::~PropertyTable):
    (JSC::PropertyTable::copy):
    * runtime/PropertyTable.cpp: Removed.
    * runtime/Structure.cpp:
    (JSC::Structure::materializePropertyMap):
    (JSC::Structure::addPropertyTransition):
    (JSC::Structure::changePrototypeTransition):
    (JSC::Structure::despecifyFunctionTransition):
    (JSC::Structure::attributeChangeTransition):
    (JSC::Structure::toDictionaryTransition):
    (JSC::Structure::preventExtensionsTransition):
    (JSC::Structure::nonPropertyTransition):
    (JSC::Structure::copyPropertyTable):
    (JSC::Structure::copyPropertyTableForPinning):
    (JSC::Structure::putSpecificValue):
    (JSC::Structure::createPropertyMap):
    (JSC::Structure::visitChildren):
    * runtime/Structure.h:
    (JSC):
    (JSC::Structure::putWillGrowOutOfLineStorage):
    (JSC::Structure::checkOffsetConsistency):
    (Structure):
    * runtime/StructureInlines.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144056 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/JSGlobalData.h
@@ -257,7 +257,6 @@ namespace JSC {
         Strong<Structure> unlinkedProgramCodeBlockStructure;
         Strong<Structure> unlinkedEvalCodeBlockStructure;
         Strong<Structure> unlinkedFunctionCodeBlockStructure;
-        Strong<Structure> propertyTableStructure;
 
         IdentifierTable* identifierTable;
         CommonIdentifiers* propertyNames;
