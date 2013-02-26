24ffc8a6c08f9d68f69fb99aa63602fdd7011cbc
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 04:43:36 +0000

    cc: Remove the pageScalePinchZoomEnabled flag.
    This flag hides dead code, as it is always on now for all
    platforms.
    
    Notably one unit test fails when moved to the new code path,
    so I've disabled the test and filed http://crbug.com/178295
    regarding fixing it.
    
    R=aelias
    
    
    Review URL: https://chromiumcodereview.appspot.com/12317120
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184584 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_tree_host_unittest.cc
+++ cc/layer_tree_host_unittest.cc
@@ -724,7 +724,8 @@ private:
     bool m_animationRequested;
 };
 
-TEST_F(LayerTreeHostTestStartPageScaleAnimation, runTest)
+// TODO(aelias): This test is currently broken: http://crbug.com/178295
+TEST_F(LayerTreeHostTestStartPageScaleAnimation, DISABLED_runTest)
 {
     runTest(true);
 }
