72ebb757b86e1a7adb982da609ecf55e60f9cd00
stuartmorgan@chromium.org <stuartmorgan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 12:33:59 +0000

    Split WebContents and navigation notifications out of GoogleURLTracker
    Separates the content-implementation-specific aspects of
    GoogleURLTracker--WebContents usage, and notifications from the
    implementation of NavigationController--into a helper class. This class
    also hides the extra complexity of Instant, so that it is transparent
    to GoogleURLTracker.
    
    This is primarily to support GoogleURLTracker use in iOS, which doesn't
    use WebContents or NavigationControllerImpl. An iOS implementation
    of GoogleURLTrackerNavigationObserver will follow at some later point.
    
    BUG=169452
    
    Review URL: https://codereview.chromium.org/11856011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184633 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google/google_url_tracker_map_entry.h
+++ chrome/browser/google/google_url_tracker_map_entry.h
@@ -8,30 +8,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
-#include "content/public/browser/notification_source.h"
 
 class GoogleURLTracker;
 class GoogleURLTrackerInfoBarDelegate;
 class InfoBarService;
+
+namespace content {
+class NavigationController;
+}
 
 class GoogleURLTrackerMapEntry : public content::NotificationObserver {
  public:
   GoogleURLTrackerMapEntry(
       GoogleURLTracker* google_url_tracker,
       InfoBarService* infobar_service,
-      const content::NotificationSource& navigation_controller_source,
-      const content::NotificationSource& web_contents_source);
+      const content::NavigationController* navigation_controller);
   virtual ~GoogleURLTrackerMapEntry();
 
   bool has_infobar() const { return !!infobar_; }
   GoogleURLTrackerInfoBarDelegate* infobar() { return infobar_; }
   void SetInfoBar(GoogleURLTrackerInfoBarDelegate* infobar);
 
-  const content::NotificationSource& navigation_controller_source() const {
-    return navigation_controller_source_;
-  }
-  const content::NotificationSource& web_contents_source() const {
-    return web_contents_source_;
+  const content::NavigationController* navigation_controller() const {
+    return navigation_controller_;
   }
 
   void Close(bool redo_search);
@@ -48,8 +47,7 @@ class GoogleURLTrackerMapEntry : public content::NotificationObserver {
   GoogleURLTracker* const google_url_tracker_;
   const InfoBarService* const infobar_service_;
   GoogleURLTrackerInfoBarDelegate* infobar_;
-  const content::NotificationSource navigation_controller_source_;
-  const content::NotificationSource web_contents_source_;
+  const content::NavigationController* const navigation_controller_;
 
   DISALLOW_COPY_AND_ASSIGN(GoogleURLTrackerMapEntry);
 };
