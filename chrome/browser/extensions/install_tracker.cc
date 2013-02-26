1c0d64f5f50db0d37a79bfebcf7b0d61316a3115
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 07:55:42 +0000

    Change NotifyAppList*() functions into observers on a ProfileKeyedService.
    This causes the events to also be delivered to the app list on ChromeOS, which would otherwise require separate plumbing.
    
    
    BUG=152854
    
    
    Review URL: https://chromiumcodereview.appspot.com/12298015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184604 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/install_tracker.cc
@@ -0,0 +1,48 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/extensions/install_tracker.h"
+
+namespace extensions {
+
+InstallTracker::InstallTracker() {
+}
+
+InstallTracker::~InstallTracker() {
+}
+
+void InstallTracker::AddObserver(InstallObserver* observer) {
+  observers_.AddObserver(observer);
+}
+
+void InstallTracker::RemoveObserver(InstallObserver* observer) {
+  observers_.RemoveObserver(observer);
+}
+
+void InstallTracker::OnBeginExtensionInstall(
+    const std::string& extension_id,
+    const std::string& extension_name,
+    const gfx::ImageSkia& installing_icon,
+    bool is_app) {
+  FOR_EACH_OBSERVER(InstallObserver, observers_,
+                    OnBeginExtensionInstall(extension_id,
+                                            extension_name,
+                                            installing_icon,
+                                            is_app));
+}
+
+void InstallTracker::OnDownloadProgress(const std::string& extension_id,
+                                        int percent_downloaded) {
+  FOR_EACH_OBSERVER(InstallObserver, observers_,
+                    OnDownloadProgress(extension_id, percent_downloaded));
+}
+
+void InstallTracker::OnInstallFailure(
+    const std::string& extension_id) {
+  FOR_EACH_OBSERVER(InstallObserver, observers_,
+                    OnInstallFailure(extension_id));
+}
+
+}  // namespace extensions
