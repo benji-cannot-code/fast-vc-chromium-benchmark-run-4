640dfd4918a760bab51f1b28085724fea689f733
deymo@chromium.org <deymo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 19:44:14 +0000

    Bluetooth: Requery service records when device reconnects after reboot.
    The service records of a bluetooth device can only be retrieved while the device
    is on, and connected. We collect this information immediatelly after pairing, but
    we don't persist it across reboots. This patch retrieves this information the first
    time the device reconnects.
    
    BUG=chromium-os:38410
    TEST=Connect to a BT mouse. Restart the chromebook. Move the mouse to issue a reconnect. Disconnect the mouse from the settings page. Connect button should be disabled.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12330125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184704 0039d316-1c4b-4281-b951-d872f2087c98

--- device/bluetooth/bluetooth_device_chromeos.cc
+++ device/bluetooth/bluetooth_device_chromeos.cc
@@ -39,6 +39,12 @@ using device::BluetoothOutOfBandPairingData;
 using device::BluetoothServiceRecord;
 using device::BluetoothSocket;
 
+namespace {
+
+void DoNothingServiceRecordList(const BluetoothDevice::ServiceRecordList&) {}
+
+} // namespace
+
 namespace chromeos {
 
 BluetoothDeviceChromeOS::BluetoothDeviceChromeOS(
@@ -333,6 +339,13 @@ void BluetoothDeviceChromeOS::Update(
   }
 
   if (update_state) {
+    // When the device reconnects and we don't have any service records for it,
+    // try to update the cache or fail silently.
+    if (!service_records_loaded_ && !connected_ &&
+        properties->connected.value())
+      GetServiceRecords(base::Bind(&DoNothingServiceRecordList),
+                        base::Bind(&base::DoNothing));
+
     // BlueZ uses paired to mean link keys exchanged, whereas the Bluetooth
     // spec refers to this as bonded. Use the spec name for our interface.
     bonded_ = properties->paired.value();
