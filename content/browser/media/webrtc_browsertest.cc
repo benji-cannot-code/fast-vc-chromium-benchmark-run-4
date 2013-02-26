9c4de77ebbee304515982157de581afb72576308
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 19:04:56 +0000

    Disabled test that was flaky on win xp.
    BUG=178403
    
    
    Review URL: https://chromiumcodereview.appspot.com/12313125
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184679 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/webrtc_browsertest.cc
+++ content/browser/media/webrtc_browsertest.cc
@@ -13,6 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/test/content_browser_test_utils.h"
 #include "net/test/test_server.h"
 
+#if defined(OS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 namespace content {
 
 class WebrtcBrowserTest: public ContentBrowserTest {
@@ -90,6 +94,12 @@ IN_PROC_BROWSER_TEST_F(WebrtcBrowserTest, MANUAL_CanSetupCallAndSendDtmf) {
 
 IN_PROC_BROWSER_TEST_F(WebrtcBrowserTest,
                        CanMakeEmptyCallThenAddStreamsAndRenegotiate) {
+#if defined(OS_WIN)
+  // This test is flaky on Win XP.
+  if (base::win::GetVersion() <= base::win::VERSION_XP)
+    return;
+#endif
+
   GURL url(test_server()->GetURL("files/media/peerconnection-call.html"));
   NavigateToURL(shell(), url);
 
