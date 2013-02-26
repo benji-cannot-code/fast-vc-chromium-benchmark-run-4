c35a404942f8ab74b1791a0b9eb4eba8cecaef83
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 02:55:05 +0000

    Fix prefs registration/migration for TranslatePrefs.
    All preferences should be registered _before_ a PrefService is created,
    so reading other prefs during registration is a no-no.
    
    BUG=155525
    
    
    Review URL: https://chromiumcodereview.appspot.com/12319069
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184565 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prefs/browser_prefs.cc
+++ chrome/browser/prefs/browser_prefs.cc
@@ -299,7 +299,7 @@ void RegisterUserPrefs(PrefService* user_prefs,
   RegisterBrowserUserPrefs(registry);
   SessionStartupPref::RegisterUserPrefs(registry);
   TemplateURLPrepopulateData::RegisterUserPrefs(registry);
-  TranslatePrefs::RegisterUserPrefs(user_prefs, registry);
+  TranslatePrefs::RegisterUserPrefs(registry);
 
 #if defined(ENABLE_CONFIGURATION_POLICY)
   policy::URLBlacklistManager::RegisterUserPrefs(registry);
@@ -371,6 +371,7 @@ void MigrateUserPrefs(Profile* profile) {
 
   PrefsTabHelper::MigrateUserPrefs(prefs);
   PromoResourceService::MigrateUserPrefs(prefs);
+  TranslatePrefs::MigrateUserPrefs(prefs);
 }
 
 void MigrateBrowserPrefs(Profile* profile, PrefService* local_state) {
