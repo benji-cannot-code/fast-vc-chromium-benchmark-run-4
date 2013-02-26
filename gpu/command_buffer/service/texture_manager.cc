6a9d1cdb31864db1599aabb55a40d2926df333a5
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 20:18:06 +0000

    Add GL State Restoring Functions
    BUG=none
    
    Review URL: https://codereview.chromium.org/12321068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184716 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/texture_manager.cc
+++ gpu/command_buffer/service/texture_manager.cc
@@ -1107,8 +1107,8 @@ TextureManager::TextureInfo* TextureManager::CreateTextureInfo(
 }
 
 TextureManager::TextureInfo* TextureManager::GetTextureInfo(
-    GLuint client_id) {
-  TextureInfoMap::iterator it = texture_infos_.find(client_id);
+    GLuint client_id) const {
+  TextureInfoMap::const_iterator it = texture_infos_.find(client_id);
   return it != texture_infos_.end() ? it->second : NULL;
 }
 
