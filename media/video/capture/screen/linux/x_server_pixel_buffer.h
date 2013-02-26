4dd693da4221fc837b3280d4909289e19bbadfa9
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 01:51:32 +0000

    Do not use division in ScreenCapturerLinux::SlowBlit().
    BUG=174381
    
    
    Review URL: https://chromiumcodereview.appspot.com/12315088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184553 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/screen/linux/x_server_pixel_buffer.h
+++ media/video/capture/screen/linux/x_server_pixel_buffer.h
@@ -58,10 +58,6 @@ class XServerPixelBuffer {
   int GetRedMask() const;
   int GetBlueMask() const;
   int GetGreenMask() const;
-  int GetRedShift() const;
-  int GetBlueShift() const;
-  int GetGreenShift() const;
-  bool IsRgb() const;
 
  private:
   void InitShm(int screen);
