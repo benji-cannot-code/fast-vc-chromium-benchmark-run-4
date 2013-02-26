fe504578632d2f36c5dd520afdfa757d4053a11b
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 11:58:07 +0000

    Replacing DictionaryValue::key_iterator by DictionaryValue::Iterator in chrome/browser/{chromeos,web_resource}.
    This is one of several CLs of a larger refactoring that prepares for the removal of key_iterator.
    
    BUG=162611
    
    
    Review URL: https://chromiumcodereview.appspot.com/12319095
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184626 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/mobile_config.h
+++ chrome/browser/chromeos/mobile_config.h
@@ -40,7 +40,7 @@ class MobileConfig : public CustomizationDocument  {
   // Carrier deal.
   class CarrierDeal {
    public:
-    explicit CarrierDeal(base::DictionaryValue* deal_dict);
+    explicit CarrierDeal(const base::DictionaryValue* deal_dict);
     ~CarrierDeal();
 
     // Returns string with the specified |locale| and |id|.
@@ -61,7 +61,7 @@ class MobileConfig : public CustomizationDocument  {
     std::string info_url_;
     int notification_count_;
     base::Time expire_date_;
-    base::DictionaryValue* localized_strings_;
+    const base::DictionaryValue* localized_strings_;
 
     DISALLOW_COPY_AND_ASSIGN(CarrierDeal);
   };
@@ -69,7 +69,7 @@ class MobileConfig : public CustomizationDocument  {
   // Carrier config.
   class Carrier {
    public:
-    Carrier(base::DictionaryValue* carrier_dict,
+    Carrier(const base::DictionaryValue* carrier_dict,
             const std::string& initial_locale);
     ~Carrier();
 
@@ -86,7 +86,7 @@ class MobileConfig : public CustomizationDocument  {
 
     // Initializes carrier from supplied dictionary.
     // Multiple calls supported (i.e. second call for local config).
-    void InitFromDictionary(base::DictionaryValue* carrier_dict,
+    void InitFromDictionary(const base::DictionaryValue* carrier_dict,
                             const std::string& initial_locale);
 
     // Removes all carrier deals. Might be executed when local config is loaded.
