6ab6a1d91af97f84735f98413f6bf4637fac615a
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-26 05:47:56 +0000

    SyncFS: implement FakeRemoteChangeProcessor
    FakeRemoteChangeProcessor records file change history for each local file. This will be used only for testing remote change processing.
    
    BUG=177162
    TEST=n/a
    
    Review URL: https://chromiumcodereview.appspot.com/12326096
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184596 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/fake_remote_change_processor.h
+++ chrome/browser/sync_file_system/fake_remote_change_processor.h
@@ -6,7 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_SYNC_FILE_SYSTEM_FAKE_REMOTE_CHANGE_PROCESSOR_H_
 #define CHROME_BROWSER_SYNC_FILE_SYSTEM_FAKE_REMOTE_CHANGE_PROCESSOR_H_
 
+#include <map>
 #include <string>
+#include <vector>
 
 #include "base/callback.h"
 #include "chrome/browser/sync_file_system/remote_change_processor.h"
@@ -17,7 +19,7 @@ class FilePath;
 }
 
 namespace fileapi {
-class FileChange;
+class FileSystemURL;
 }
 
 namespace sync_file_system {
@@ -26,6 +28,9 @@ class FileChange;
 
 class FakeRemoteChangeProcessor : public RemoteChangeProcessor {
  public:
+  typedef std::map<fileapi::FileSystemURL, std::vector<FileChange>,
+                   fileapi::FileSystemURL::Comparator> URLToFileChangesMap;
+
   FakeRemoteChangeProcessor();
   virtual ~FakeRemoteChangeProcessor();
 
@@ -47,7 +52,14 @@ class FakeRemoteChangeProcessor : public RemoteChangeProcessor {
       const FileChange& change,
       const fileapi::SyncStatusCallback& callback) OVERRIDE;
 
+  const URLToFileChangesMap& GetAppliedRemoteChanges() const;
+
  private:
+  // History of file changes given by ApplyRemoteChange(). Changes are arranged
+  // in chronological order, that is, the end of the vector represents the last
+  // change.
+  URLToFileChangesMap applied_changes_;
+
   DISALLOW_COPY_AND_ASSIGN(FakeRemoteChangeProcessor);
 };
 
