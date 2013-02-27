2f6d1b0f494b4be475d88054a82c395ce8f26f97
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-27 21:46:32 +0000

    Implement channel up-mixing and down-mixing rules https://bugs.webkit.org/show_bug.cgi?id=110812
    Reviewed by Kenneth Russell.
    
    Source/WebCore:
    
    Please see Web Audio specification for details of the AudioNode mixing rules attributes:
    https://dvcs.w3.org/hg/audio/raw-file/tip/webaudio/specification.html#UpMix
    
    Test: webaudio/audionode-channel-rules.html
    
    * Modules/webaudio/AudioNode.cpp:
    (WebCore::AudioNode::AudioNode):
    (WebCore::AudioNode::channelCount):
    (WebCore):
    (WebCore::AudioNode::setChannelCount):
    (WebCore::AudioNode::channelCountMode):
    (WebCore::AudioNode::setChannelCountMode):
    (WebCore::AudioNode::channelInterpretation):
    (WebCore::AudioNode::setChannelInterpretation):
    (WebCore::AudioNode::updateChannelsForInputs):
    * Modules/webaudio/AudioNode.h:
    (AudioNode):
    (WebCore::AudioNode::internalChannelCountMode):
    (WebCore::AudioNode::internalChannelInterpretation):
    * Modules/webaudio/AudioNode.idl:
    * Modules/webaudio/AudioNodeInput.cpp:
    (WebCore::AudioNodeInput::numberOfChannels):
    (WebCore::AudioNodeInput::bus):
    (WebCore::AudioNodeInput::internalSummingBus):
    (WebCore::AudioNodeInput::sumAllConnections):
    (WebCore::AudioNodeInput::pull):
    * Modules/webaudio/AudioNodeInput.h:
    (AudioNodeInput):
    * Modules/webaudio/ConvolverNode.cpp:
    (WebCore::ConvolverNode::ConvolverNode):
    * Modules/webaudio/DefaultAudioDestinationNode.cpp:
    (WebCore::DefaultAudioDestinationNode::DefaultAudioDestinationNode):
    * Modules/webaudio/PannerNode.cpp:
    (WebCore::PannerNode::PannerNode):
    * platform/audio/AudioBus.cpp:
    (WebCore::AudioBus::speakersCopyFrom):
    (WebCore::AudioBus::speakersSumFrom):
    (WebCore::AudioBus::speakersSumFrom5_1_ToMono):
    (WebCore):
    * platform/audio/AudioBus.h:
    (AudioBus):
    
    LayoutTests:
    
    * webaudio/audionode-channel-rules-expected.txt: Added.
    * webaudio/audionode-channel-rules.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144235 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioNode.h
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioNode.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef AudioNode_h
 #define AudioNode_h
 
+#include "AudioBus.h"
 #include <wtf/Forward.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
@@ -79,6 +80,12 @@ public:
         NodeTypeDynamicsCompressor,
         NodeTypeWaveShaper,
         NodeTypeEnd
+    };
+
+    enum ChannelCountMode {
+        Max,
+        ClampedMax,
+        Explicit
     };
 
     NodeType nodeType() const { return m_nodeType; }
@@ -162,6 +169,18 @@ public:
 
     void reportMemoryUsage(MemoryObjectInfo*) const;
 
+    unsigned long channelCount();
+    void setChannelCount(unsigned long, ExceptionCode&);
+
+    String channelCountMode();
+    void setChannelCountMode(const String&, ExceptionCode&);
+
+    String channelInterpretation();
+    void setChannelInterpretation(const String&, ExceptionCode&);
+
+    ChannelCountMode internalChannelCountMode() const { return m_channelCountMode; }
+    AudioBus::ChannelInterpretation internalChannelInterpretation() const { return m_channelInterpretation; }
+
 protected:
     // Inputs and outputs must be created before the AudioNode is initialized.
     void addInput(PassOwnPtr<AudioNodeInput>);
@@ -171,6 +190,9 @@ protected:
     // Each rendering quantum, the audio data for each of the AudioNode's inputs will be available after this method is called.
     // Called from context's audio thread.
     virtual void pullInputs(size_t framesToProcess);
+
+    // Force all inputs to take any channel interpretation changes into account.
+    void updateChannelsForInputs();
 
 private:
     volatile bool m_isInitialized;
@@ -189,11 +211,16 @@ private:
     
     bool m_isMarkedForDeletion;
     bool m_isDisabled;
-    
+
 #if DEBUG_AUDIONODE_REFERENCES
     static bool s_isNodeCountInitialized;
     static int s_nodeCount[NodeTypeEnd];
 #endif
+
+protected:
+    unsigned m_channelCount;
+    ChannelCountMode m_channelCountMode;
+    AudioBus::ChannelInterpretation m_channelInterpretation;
 };
 
 } // namespace WebCore
