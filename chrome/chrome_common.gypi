357578eb6bf5b0225bf03e53c3981aeededf4896
stevet@chromium.org <stevet@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 20:22:18 +0000

    Move uniformity trials to its own file.
    BUG=178748
    TEST=No user visible changes.
    
    Review URL: https://chromiumcodereview.appspot.com/12334124
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185018 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_common.gypi
+++ chrome/chrome_common.gypi
@@ -319,6 +319,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'common/metrics/metrics_service_base.h',
         'common/metrics/metrics_util.cc',
         'common/metrics/metrics_util.h',
+        'common/metrics/variations/uniformity_field_trials.cc',
+        'common/metrics/variations/uniformity_field_trials.h',
         'common/metrics/variations/variations_util.cc',
         'common/metrics/variations/variations_util.h',
         'common/multi_process_lock.h',
---
+++ chrome/common/metrics/variations/uniformity_field_trials.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_COMMON_METRICS_VARIATIONS_UNIFORMITY_FIELD_TRIALS_H_
+#define CHROME_COMMON_METRICS_VARIATIONS_UNIFORMITY_FIELD_TRIALS_H_
+
+#include "base/time.h"
+
+namespace chrome_variations {
+
+// A collection of one-time-randomized and session-randomized field trials
+// intended to test the uniformity and correctness of the field trial control,
+// bucketing and reporting systems.
+void SetupUniformityFieldTrials(const base::Time& install_date);
+
+}  // namespace chrome_variations
+
+#endif  // CHROME_COMMON_METRICS_VARIATIONS_UNIFORMITY_FIELD_TRIALS_H_
