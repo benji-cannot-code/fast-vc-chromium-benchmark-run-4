904c73a432cdeeff0466b24e44a55e5a42a713f3
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 05:27:29 +0000

    Remove browser-wide audio mirroring and --force-audio-mirroring flag.
    BUG=176700
    TEST=Ran media_unittests, content_unittests; and manually ran tab mirroring sessions in the browser to confirm no breakage.
    
    Review URL: https://chromiumcodereview.appspot.com/12298003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184882 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/virtual_audio_output_stream.h
+++ media/audio/virtual_audio_output_stream.h
@@ -17,7 +17,6 @@ class MessageLoopProxy;
 
 namespace media {
 
-class AudioManagerBase;
 class VirtualAudioInputStream;
 
 // VirtualAudioOutputStream attaches to a VirtualAudioInputStream when Start()
