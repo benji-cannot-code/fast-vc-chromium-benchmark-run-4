a6aa4eee78edc8fb46adf64983fd6d0ac7e718ef
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 19:45:21 +0000

    Revert 184995
    Looks to have caused the broken tests in http://build.chromium.org/p/chromium.win/builders/Vista%20Tests%20%282%29/builds/32494
    
    > Make sure if GPU process fails to launch, all observers waiting for full gpu info get a response.
    > 
    > BUG=174296
    > TEST=AutofillRiskFingerprintTest.GetFingerprint
    > Review URL: https://codereview.chromium.org/12335100
    
    TBR=zmo@chromium.org
    Review URL: https://codereview.chromium.org/12310178
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185004 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/gpu_info.cc
+++ content/public/common/gpu_info.cc
@@ -19,6 +19,7 @@ GPUInfo::GPUInfo()
       optimus(false),
       amd_switchable(false),
       can_lose_context(false),
+      gpu_accessible(true),
       software_rendering(false),
       sandboxed(false) {
 }
