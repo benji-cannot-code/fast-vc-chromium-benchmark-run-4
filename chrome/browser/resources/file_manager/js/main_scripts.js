511e7b46ed1bacf745ff85c9c9f7e7648c7605bf
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 03:03:40 +0000

    files: Add auto-complete for Drive
    Here's how it works: As soon as you start typing in the search box
    while you are on Drive in Files.app, Chrome will perform incremental
    searches over names of files on Drive, and show the matched files
    right below the search box as auto-complete suggestions.
    
    The auto-complete is much faster than the server side search, since
    auto-complete is completely performed locally with the the locally
    stored metadata.
    
    BUG=129213
    TEST=Open Drive in Files.app. Type something in the search box. Auto-complete suggestions should show up.
    
    Review URL: https://codereview.chromium.org/12295033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184852 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="../../../../../ui/webui/resources/js/cr/ui/list_single_selection_model.js"/>
 //<include src="../../../../../ui/webui/resources/js/cr/ui/list_selection_controller.js"/>
 //<include src="../../../../../ui/webui/resources/js/cr/ui/list.js"/>
+//<include src="../../../../../ui/webui/resources/js/cr/ui/autocomplete_list.js"/>
 //
 //<include src="../../../../../ui/webui/resources/js/cr/ui/splitter.js"/>
 //<include src="../../../../../ui/webui/resources/js/cr/ui/table/table_splitter.js"/>
