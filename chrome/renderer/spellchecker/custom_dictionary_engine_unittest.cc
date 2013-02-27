81eca685b35b8da58b7231baee3d5650d43631b4
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 03:13:15 +0000

    [Spellcheck] Added fix to prevent crashing on OSX async path.
    OSX async (WIP, not landed yet) needs the custom dictionary engine
    to handle empty strings with substring indices out of bounds.
    
    R=rlp@chromium.org
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12334108
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184855 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/renderer/spellchecker/custom_dictionary_engine_unittest.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+#include "chrome/renderer/spellchecker/custom_dictionary_engine.h"
+
+#include "testing/gtest/include/gtest/gtest.h"
+
+TEST(CustomDictionaryTest, HandlesEmptyWordWithInvalidSubstring) {
+  CustomDictionaryEngine engine;
+  std::vector<std::string> custom_words;
+
+  engine.Init(custom_words);
+  EXPECT_FALSE(engine.SpellCheckWord(string16().c_str(), 15, 23));
+}
+
