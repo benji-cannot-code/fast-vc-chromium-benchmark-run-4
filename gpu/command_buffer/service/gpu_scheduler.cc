2bca7718aa1fe27986dff9ce5ee2dc6702b09ebf
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 00:19:55 +0000

    do RescheduleTimeOut hack only pre-Vista
    When stopped in debugger, timeout causes DCHECKs. Remove
    on Vista+ since it shouldn't be necessary and there's no dev on
    XP.
    
    R=apatrick@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12315112
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184787 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_scheduler.cc
+++ gpu/command_buffer/service/gpu_scheduler.cc
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gl/gl_fence.h"
 #include "ui/gl/gl_switches.h"
 
+#if defined(OS_WIN)
+#include "base/win/windows_version.h"
+#endif
+
 using ::base::SharedMemory;
 
 namespace gpu {
@@ -141,14 +145,16 @@ void GpuScheduler::SetScheduled(bool scheduled) {
       TRACE_EVENT_ASYNC_BEGIN1("gpu", "ProcessingSwap", this,
                                "GpuScheduler", this);
 #if defined(OS_WIN)
-      // When the scheduler transitions from scheduled to unscheduled, post a
-      // delayed task that it will force it back into a scheduled state after a
-      // timeout.
-      MessageLoop::current()->PostDelayedTask(
-          FROM_HERE,
-          base::Bind(&GpuScheduler::RescheduleTimeOut,
-                     reschedule_task_factory_.GetWeakPtr()),
-          base::TimeDelta::FromMilliseconds(kRescheduleTimeOutDelay));
+      if (base::win::GetVersion() < base::win::VERSION_VISTA) {
+        // When the scheduler transitions from scheduled to unscheduled, post a
+        // delayed task that it will force it back into a scheduled state after
+        // a timeout. This should only be necessary on pre-Vista.
+        MessageLoop::current()->PostDelayedTask(
+            FROM_HERE,
+            base::Bind(&GpuScheduler::RescheduleTimeOut,
+                       reschedule_task_factory_.GetWeakPtr()),
+            base::TimeDelta::FromMilliseconds(kRescheduleTimeOutDelay));
+      }
 #endif
     }
 
