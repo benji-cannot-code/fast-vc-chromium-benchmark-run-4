fe39643ba90b0004f22e3d49ae50adda224f2e9a
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-27 22:39:19 +0000

    Threaded HTML Parser fails fast/dom/HTMLAnchorElement/anchor-no-multiple-windows.html in debug https://bugs.webkit.org/show_bug.cgi?id=110951
    Reviewed by Eric Seidel.
    
    We were triggering this ASSERT because we didn't understand that a
    given frame might have multiple DocumentLoaders in various states. That
    caused us to think that a DocumentLoader in the provisional state was
    actually loading.
    
    * dom/Document.cpp:
    (WebCore::Document::decrementActiveParserCount):
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::isLoading):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/DocumentLoader.h
+++ third_party/WebKit/Source/WebCore/loader/DocumentLoader.h
@@ -86,6 +86,7 @@ namespace WebCore {
         PassRefPtr<ResourceBuffer> mainResourceData() const;
         
         DocumentWriter* writer() const { return &m_writer; }
+        Document* document() const;
 
         const ResourceRequest& originalRequest() const;
         const ResourceRequest& originalRequestCopy() const;
