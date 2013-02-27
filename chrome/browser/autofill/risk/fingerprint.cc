af10c410db31d8498d336b66b89522afdac9d3e6
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 16:02:12 +0000

    Get rid of unnecessary empty overrides of GpuDataManagerObserver. Review URL: https://codereview.chromium.org/12334111
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184965 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/risk/fingerprint.cc
+++ chrome/browser/autofill/risk/fingerprint.cc
@@ -194,9 +194,6 @@ class FingerprintDataLoader : public content::GpuDataManagerObserver {
 
   // content::GpuDataManagerObserver:
   virtual void OnGpuInfoUpdate() OVERRIDE;
-  virtual void OnVideoMemoryUsageStatsUpdate(
-      const content::GPUVideoMemoryUsageStats& video_memory_usage_stats)
-      OVERRIDE;
 
   // Callbacks for asynchronously loaded data.
   void OnGotFonts(scoped_ptr<base::ListValue> fonts);
@@ -286,10 +283,6 @@ void FingerprintDataLoader::OnGpuInfoUpdate() {
 
   gpu_data_manager_->RemoveObserver(this);
   MaybeFillFingerprint();
-}
-
-void FingerprintDataLoader::OnVideoMemoryUsageStatsUpdate(
-    const content::GPUVideoMemoryUsageStats& video_memory_usage_stats) {
 }
 
 void FingerprintDataLoader::OnGotFonts(scoped_ptr<base::ListValue> fonts) {
--- chrome/browser/task_manager/task_manager.cc
+++ chrome/browser/task_manager/task_manager.cc
@@ -141,8 +141,6 @@ class TaskManagerModelGpuDataManagerObserver
         video_memory_usage_stats);
   }
 
-  virtual void OnGpuInfoUpdate() OVERRIDE {}
-
   virtual void OnVideoMemoryUsageStatsUpdate(
       const content::GPUVideoMemoryUsageStats& video_memory_usage_stats)
           OVERRIDE {
