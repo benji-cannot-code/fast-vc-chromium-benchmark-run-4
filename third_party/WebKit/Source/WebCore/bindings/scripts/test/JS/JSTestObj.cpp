87125527e4b08cd653eaa7683f2426fffc4e2b39
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-27 14:46:57 +0000

    Bindings test results update after r144126 https://bugs.webkit.org/show_bug.cgi?id=108196
    Unreviewed test update.
    
    * bindings/scripts/test/JS/JSTestObj.cpp: Add include of CallbackFunction.h
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144176 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestObj.cpp
+++ third_party/WebKit/Source/WebCore/bindings/scripts/test/JS/JSTestObj.cpp
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "JSTestObj.h"
 
+#include "CallbackFunction.h"
 #include "DOMStringList.h"
 #include "Dictionary.h"
 #include "Document.h"
