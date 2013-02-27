6a8ca041d0011b02dc126e86b225f76223bc216e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-27 13:34:06 +0000

    Bug in atomicIncrement implementation for MIPS GCC https://bugs.webkit.org/show_bug.cgi?id=110969
    Patch by Balazs Kilvady <kilvadyb@homejinni.com> on 2013-02-27
    Reviewed by Csaba Osztrogonác.
    
    Fix of __sync_[add|sub]_and_fetch_8 for GCC patch.
    
    * wtf/Atomics.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144174 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Atomics.cpp
+++ third_party/WebKit/Source/WTF/wtf/Atomics.cpp
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
+#include "Atomics.h"
 
 // Some architectures, like MIPS32, don't have GCC implementation for builtin __sync_* functions
 // with 64 bits variable size. Official GCC answer for the problem: If a target doesn't support
@@ -66,8 +67,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // functions for this purpose for all the GCC targets, but for current compilers we have to include
 // our own implementation.
 #if COMPILER(GCC) && !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8) && USE(LOCKFREE_THREADSAFEREFCOUNTED) && USE(PTHREADS)
-
-#include "Atomics.h"
 
 #include "ThreadingPrimitives.h"
 
