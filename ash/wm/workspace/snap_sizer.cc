8141a5c821b035f52c30d7dec3ea6fa9af62e924
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 08:09:54 +0000

    Re-land 184821 with updated unit tests.
    Fix a minor typo in SnapSizer::GetSnapBounds().
    
    The second operand for the "%" operator should be the array size of usable_width_,
    not the size minus 1.
    
    BUG=178561
    TEST=manually tested
    
    
    Review URL: https://chromiumcodereview.appspot.com/12334113
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184906 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/snap_sizer.cc
+++ ash/wm/workspace/snap_sizer.cc
@@ -152,7 +152,7 @@ gfx::Rect SnapSizer::GetSnapBounds(const gfx::Rect& bounds) {
       }
     }
   }
-  return GetTargetBoundsForSize(current % (usable_width_.size() - 1));
+  return GetTargetBoundsForSize(current % usable_width_.size());
 }
 
 void SnapSizer::SelectDefaultSizeAndDisableResize() {
