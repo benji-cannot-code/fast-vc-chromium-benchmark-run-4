6a34ff0c5be8c6913a6da362685862bca5241ce3
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-27 01:52:28 +0000

    RenderWidget side compositor scheduling logic
    This moves the compositor scheduling suppression logic out to RenderWidget /
    RenderWidgetCompositor so we don't have to bounce through WebKit just to do
    this. The suppression logic is needed to avoid producing unwanted frames during
    page transitions when in single-threaded compositing mode which will often be
    white flashes and which slow down the page cycler benchmarks.
    
    This implements WebKit API that will be added by https://bugs.webkit.org/show_bug.cgi?id=110640.
    This patch will compile before that patch lands and rolls, but will introduce
    page cycler regressions and flashes.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12319076
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@184826 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/render_widget_compositor.h
+++ content/renderer/gpu/render_widget_compositor.h
@@ -31,6 +31,8 @@ class RenderWidgetCompositor : public WebKit::WebLayerTreeView,
 
   cc::LayerTreeHost* layer_tree_host() const { return layer_tree_host_.get(); }
 
+  void SetSuppressScheduleComposite(bool suppress);
+
   // WebLayerTreeView implementation.
   virtual void setSurfaceReady();
   virtual void setRootLayer(const WebKit::WebLayer& layer);
@@ -96,6 +98,7 @@ private:
   bool initialize(cc::LayerTreeSettings settings);
 
   bool threaded_;
+  bool suppress_schedule_composite_;
   RenderWidget* widget_;
   WebKit::WebLayerTreeViewClient* client_;
   scoped_ptr<cc::LayerTreeHost> layer_tree_host_;
