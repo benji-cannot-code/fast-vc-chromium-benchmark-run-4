e6dc6a24702122ca922ca97bb957f3578a5f9d05
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-27 08:37:15 +0000

    Web Inspector: show raster tasks on Timeline https://bugs.webkit.org/show_bug.cgi?id=105851
    - add DeferPaint, Paint & RasterTask trace events handling to trace event processor;
    - upon begin frame, emit aggregated background event for all raster tasks related to inspected page.
    
    Reviewed by Pavel Feldman.
    
    * inspector/InspectorInstrumentation.cpp:
    (WebCore):
    (WebCore::InspectorInstrumentation::willPaintImpl):
    (InstrumentationEvents):
    (InstrumentationEventArguments):
    * inspector/InspectorInstrumentation.h:
    (InstrumentationEvents):
    (WebCore):
    (InstrumentationEventArguments):
    * inspector/InspectorTimelineAgent.cpp:
    (TimelineRecordType):
    (WebCore::InspectorTimelineAgent::stop):
    (WebCore::InspectorTimelineAgent::didBeginFrame):
    * inspector/InspectorTimelineAgent.h:
    (TimelineRecordType):
    (WebCore):
    * inspector/TimelineRecordFactory.cpp:
    (WebCore::TimelineRecordFactory::createRasterData):
    (WebCore):
    * inspector/TimelineRecordFactory.h:
    (TimelineRecordFactory):
    * inspector/TimelineTraceEventProcessor.cpp:
    (WebCore::TimelineTraceEventProcessor::TimelineTraceEventProcessor):
    (WebCore::TimelineTraceEventProcessor::~TimelineTraceEventProcessor):
    (WebCore):
    (WebCore::TimelineTraceEventProcessor::registerHandler):
    (WebCore::TimelineTraceEventProcessor::shutdown):
    (WebCore::TimelineTraceEventProcessor::TraceEvent::findParameter):
    (WebCore::TimelineTraceEventProcessor::TraceEvent::parameter):
    (WebCore::TimelineTraceEventProcessor::processEventOnAnyThread):
    (WebCore::TimelineTraceEventProcessor::onBeginFrame):
    (WebCore::TimelineTraceEventProcessor::onPaintLayerBegin):
    (WebCore::TimelineTraceEventProcessor::onPaintLayerEnd):
    (WebCore::TimelineTraceEventProcessor::onRasterTaskBegin):
    (WebCore::TimelineTraceEventProcessor::onRasterTaskEnd):
    (WebCore::TimelineTraceEventProcessor::onLayerDeleted):
    (WebCore::TimelineTraceEventProcessor::onPaint):
    (WebCore::TimelineTraceEventProcessor::flushRasterizerStatistics):
    (WebCore::TimelineTraceEventProcessor::sendTimelineRecord):
    (WebCore::TimelineTraceEventProcessor::processBackgroundEvents):
    * inspector/TimelineTraceEventProcessor.h:
    (WebCore::TimelineTraceEventProcessor::TraceEvent::TraceEvent):
    (WebCore::TimelineTraceEventProcessor::TraceEvent::id):
    (WebCore::TimelineTraceEventProcessor::TraceEvent::asInt):
    (WebCore::TimelineTraceEventProcessor::TraceEvent::asUInt):
    (TimelineTraceEventProcessor):
    * inspector/front-end/TimelineModel.js:
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel._initRecordStyles):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144154 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorTimelineAgent.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorTimelineAgent.h
@@ -64,6 +64,10 @@ class TimelineTraceEventProcessor;
 
 typedef String ErrorString;
 
+namespace TimelineRecordType {
+extern const char Rasterize[];
+};
+
 class InspectorTimelineAgent
     : public InspectorBaseAgent<InspectorTimelineAgent>,
       public ScriptGCEventListener,
--- third_party/WebKit/Source/WebCore/inspector/TimelineRecordFactory.h
+++ third_party/WebKit/Source/WebCore/inspector/TimelineRecordFactory.h
@@ -107,6 +107,7 @@ namespace WebCore {
             return data.release();
         }
 #endif
+        static PassRefPtr<InspectorObject> createRasterData(double totalCPUTime, int threadsUsed);
 
     private:
         TimelineRecordFactory() { }
