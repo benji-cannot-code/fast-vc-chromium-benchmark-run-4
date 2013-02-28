d2fe5e6286708ca38ea7568f58aa2fd24abe7678
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 09:18:59 +0000

    Require manifests for native messaging hosts.
    Previously native messaging hosts were discovered by looking in the user 
    data directory. That approach is hard to use and is not reliable. Now 
    chrome will look for a host manifest file in a system-specific directory 
    and load that file to find host binary.
    
    BUG=142915
    TBR=thakis@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12285015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185189 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/messaging/native_message_process_host_unittest.cc
+++ chrome/browser/extensions/api/messaging/native_message_process_host_unittest.cc
@@ -34,6 +34,7 @@ using content::BrowserThread;
 
 namespace {
 
+const char kTestHostId[] = "knldjmfmopnpolahpmmgbagdohdnhkik";
 const char kTestMessage[] = "{\"text\": \"Hello.\"}";
 
 base::FilePath GetTestDir() {
@@ -62,7 +63,8 @@ class FakeLauncher : public NativeProcessLauncher {
         NULL, NULL);
   }
 
-  virtual void Launch(const std::string& native_host_name,
+  virtual void Launch(const GURL& origin,
+                      const std::string& native_host_name,
                       LaunchedCallback callback) const OVERRIDE {
     callback.Run(base::GetCurrentProcessHandle(), read_file_, write_file_);
   }
@@ -152,7 +154,7 @@ TEST_F(NativeMessagingTest, SingleSendMessageRead) {
   scoped_ptr<NativeProcessLauncher> launcher(
       new FakeLauncher(temp_input_file, temp_output_file));
   native_message_process_host_ = NativeMessageProcessHost::CreateWithLauncher(
-      AsWeakPtr(), "empty_app.py", 0, launcher.Pass());
+      AsWeakPtr(), kTestHostId, "empty_app.py", 0, launcher.Pass());
   ASSERT_TRUE(native_message_process_host_.get());
   message_loop_.RunUntilIdle();
 
@@ -170,7 +172,7 @@ TEST_F(NativeMessagingTest, SingleSendMessageWrite) {
   scoped_ptr<NativeProcessLauncher> launcher(
       new FakeLauncher(temp_input_file, temp_output_file));
   native_message_process_host_ = NativeMessageProcessHost::CreateWithLauncher(
-      AsWeakPtr(), "empty_app.py", 0, launcher.Pass());
+      AsWeakPtr(), kTestHostId, "empty_app.py", 0, launcher.Pass());
   ASSERT_TRUE(native_message_process_host_.get());
   message_loop_.RunUntilIdle();
 
@@ -194,7 +196,7 @@ TEST_F(NativeMessagingTest, SingleSendMessageWrite) {
 // it recieved.
 TEST_F(NativeMessagingTest, DISABLED_EchoConnect) {
   native_message_process_host_ = NativeMessageProcessHost::Create(
-      AsWeakPtr(), "empty_app.py", 0);
+      AsWeakPtr(), kTestHostId, "empty_app.py", 0);
   ASSERT_TRUE(native_message_process_host_.get());
   message_loop_.RunUntilIdle();
 
--- chrome/test/data/extensions/api_test/native_messaging/test.js
+++ chrome/test/data/extensions/api_test/native_messaging/test.js
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-var appName = navigator.platform.match(/win/i) ? 'echo.bat' : 'echo.py';
+var appName = 'com.google.chrome.test.echo';
 
 chrome.test.getConfig(function(config) {
     chrome.test.runTests([
