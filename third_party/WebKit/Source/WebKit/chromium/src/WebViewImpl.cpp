2ee49a5756ebecea6d5c2260d7fd01c3e8f755ca
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 05:53:00 +0000

    [Chromium] Should not return WebTextInputTypeNone for date input element. https://bugs.webkit.org/show_bug.cgi?id=110740
    Patch by Seigo Nonaka <nona@chromium.org> on 2013-02-27
    Reviewed by Kent Tamura.
    
    In the case of Windows 8, text input state including on-screen keyboard is controlled by the
    value of WebTextInputType returned from WebViewImpl::textInputType().
    In past, it returned WebTextInputTypeDate for date text input but now it returns
    WebTextInputTypeNone.
    WebTextInputTypeNone is used for non editable node, so on-screen keyboard will be hidden if
    the date text input is focused.  So there is no way to input on Windows 8 tablet without
    physical keyboard except tapping small up/down arrow.
    
    * public/WebTextInputType.h: Introduces WebTextInputTypeDateTimeField.
    * src/WebViewImpl.cpp:
    (WebKit::WebViewImpl::textInputInfo): Fills type filed regardless of editable or not. It is
    safe because textInputType returns editable type only for known editable element.
    (WebKit::WebViewImpl::textInputType): Returns WebTextInputTypeDateTimeField for the date
    time field element.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144261 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -2283,6 +2283,10 @@ WebTextInputInfo WebViewImpl::textInputInfo()
     if (!focused)
         return info;
 
+    info.type = textInputType();
+    if (info.type == WebTextInputTypeNone)
+        return info;
+
     Editor* editor = focused->editor();
     if (!editor || !editor->canEdit())
         return info;
@@ -2293,10 +2297,6 @@ WebTextInputInfo WebViewImpl::textInputInfo()
 
     Node* node = selection->selection().rootEditableElement();
     if (!node)
-        return info;
-
-    info.type = textInputType();
-    if (info.type == WebTextInputTypeNone)
         return info;
 
     info.value = plainText(rangeOfContents(node).get());
@@ -2367,6 +2367,14 @@ WebTextInputType WebViewImpl::textInputType()
             return WebTextInputTypeNone;
         return WebTextInputTypeTextArea;
     }
+
+#if ENABLE(INPUT_MULTIPLE_FIELDS_UI)
+    if (node->isHTMLElement()) {
+        HTMLElement* element = toHTMLElement(node);
+        if (element->isDateTimeFieldElement())
+            return WebTextInputTypeDateTimeField;
+    }
+#endif
 
     if (node->shouldUseInputMethod())
         return WebTextInputTypeContentEditable;
