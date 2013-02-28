05b364bf4311acbb3c381e9ef0e0c3282f8551df
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 19:45:17 +0000

    DFG CFA should leave behind information in Edge that says if the Edge's type check is proven to succeed https://bugs.webkit.org/show_bug.cgi?id=110840
    Reviewed by Mark Hahnenberg.
            
    This doesn't add any observable functionality to the compiler, yet. But it does give
    every phase that runs after CFA the ability to know, in O(1) time, whether an edge
    will need to execute a type check.
    
    * dfg/DFGAbstractState.h:
    (JSC::DFG::AbstractState::filterEdgeByUse):
    (JSC::DFG::AbstractState::filterByType):
    * dfg/DFGCommon.cpp:
    (WTF):
    (WTF::printInternal):
    * dfg/DFGCommon.h:
    (JSC::DFG::isProved):
    (DFG):
    (JSC::DFG::proofStatusForIsProved):
    (WTF):
    * dfg/DFGEdge.cpp:
    (JSC::DFG::Edge::dump):
    * dfg/DFGEdge.h:
    (JSC::DFG::Edge::Edge):
    (JSC::DFG::Edge::setNode):
    (JSC::DFG::Edge::useKindUnchecked):
    (JSC::DFG::Edge::setUseKind):
    (Edge):
    (JSC::DFG::Edge::proofStatusUnchecked):
    (JSC::DFG::Edge::proofStatus):
    (JSC::DFG::Edge::setProofStatus):
    (JSC::DFG::Edge::isProved):
    (JSC::DFG::Edge::needsCheck):
    (JSC::DFG::Edge::shift):
    (JSC::DFG::Edge::makeWord):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144340 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGEdge.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGEdge.cpp
@@ -35,8 +35,11 @@ namespace JSC { namespace DFG {
 
 void Edge::dump(PrintStream& out) const
 {
-    if (useKind() != UntypedUse)
+    if (useKind() != UntypedUse) {
+        if (needsCheck())
+            out.print("Check:");
         out.print(useKind(), ":");
+    }
     out.print(node());
 }
 
