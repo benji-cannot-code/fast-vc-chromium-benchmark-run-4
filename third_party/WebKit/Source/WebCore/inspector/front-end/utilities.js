659270061169a85ed29c48fa32e1d7582610732f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 14:53:35 +0000

    Web Inspector: "loadScript" function to load scripts via xhr https://bugs.webkit.org/show_bug.cgi?id=110879
    Patch by Andrey Lushnikov <lushnikov@chromium.org> on 2013-02-28
    Reviewed by Pavel Feldman.
    
    Create an alias for "importScript" function and use it in those cases
    which assume lazy script loading.
    
    No new tests.
    
    * inspector/front-end/Panel.js:
    (WebInspector.PanelDescriptor.prototype.panel):
    * inspector/front-end/SourceFrame.js:
    (WebInspector.SourceFrame):
    * inspector/front-end/utilities.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144305 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/utilities.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/utilities.js
@@ -969,6 +969,12 @@ StringPool.prototype = {
 var _importedScripts = {};
 
 /**
+ * This function behavior depends on the "debug_devtools" flag value.
+ * - In debug mode it loads scripts synchronously via xhr request.
+ * - In release mode every occurrence of "importScript" gets replaced with
+ * the script source code on the compilation phase.
+ *
+ * To load scripts lazily in release mode call "loasScript" function.
  * @param {string} scriptName
  */
 function importScript(scriptName)
@@ -982,3 +988,5 @@ function importScript(scriptName)
     var sourceURL = WebInspector.ParsedURL.completeURL(window.location.href, scriptName); 
     window.eval(xhr.responseText + "\n//@ sourceURL=" + sourceURL);
 }
+
+var loadScript = importScript;
