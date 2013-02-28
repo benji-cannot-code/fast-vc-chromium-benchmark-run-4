385adf1f1a56745e19c934fda72442327cdce044
yhirano@chromium.org <yhirano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 03:39:40 +0000

    [nitpicking] Reduce include dependencies
    Replace header inclusions with forward declarations.
    Move header an inclusion to less-depended files.
    Fix header inclusion order.
    
    BUG=NONE
    
    
    Review URL: https://chromiumcodereview.appspot.com/12310039
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185130 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/socket_stream_host.cc
+++ content/browser/renderer_host/socket_stream_host.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "content/common/socket_stream.h"
 #include "net/socket_stream/socket_stream_job.h"
+#include "net/url_request/url_request_context.h"
 
 namespace content {
 namespace {
--- net/socket_stream/socket_stream.cc
+++ net/socket_stream/socket_stream.cc
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/net_errors.h"
 #include "net/base/net_util.h"
 #include "net/base/ssl_cert_request_info.h"
+#include "net/base/ssl_info.h"
 #include "net/http/http_auth_controller.h"
 #include "net/http/http_network_session.h"
 #include "net/http/http_request_headers.h"
@@ -41,6 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/socket/tcp_client_socket.h"
 #include "net/socket_stream/socket_stream_metrics.h"
 #include "net/url_request/url_request.h"
+#include "net/url_request/url_request_context.h"
 
 static const int kMaxPendingSendAllowed = 32768;  // 32 kilobytes.
 static const int kReadBufferSize = 4096;
