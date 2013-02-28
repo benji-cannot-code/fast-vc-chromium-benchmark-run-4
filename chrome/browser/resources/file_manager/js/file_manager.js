53ce478e007c328880fd0cf3f1ad286fe5db6f8b
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 06:28:09 +0000

    chromeos: Wait for fileBrowser.selectFile completion before closing file select dialog
    BUG=178659
    TEST=Select an uncached (not fetched before) large file (300KB+) from Drive folder to upload to web sites.
    
    Review URL: https://codereview.chromium.org/12315123
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185168 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2584,14 +2584,17 @@ DialogType.isModal = function(type) {
    * @param {Object} selection Contains urls, filterIndex and multiple fields.
    */
   FileManager.prototype.callSelectFilesApiAndClose_ = function(selection) {
+    var self = this;
+    function callback() {
+      self.onUnload_();
+      window.close();
+    }
     if (selection.multiple) {
-      chrome.fileBrowserPrivate.selectFiles(selection.urls);
+      chrome.fileBrowserPrivate.selectFiles(selection.urls, callback);
     } else {
       chrome.fileBrowserPrivate.selectFile(
-          selection.urls[0], selection.filterIndex);
+          selection.urls[0], selection.filterIndex, callback);
     }
-    this.onUnload_();
-    window.close();
   };
 
   /**
