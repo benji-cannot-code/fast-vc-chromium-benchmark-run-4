fe5c5515cd513e9e5d8902c10e123bf3da185ace
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 19:46:28 +0000

    Unreviewed, rolling out r144126 and r144176. http://trac.webkit.org/changeset/144126 http://trac.webkit.org/changeset/144176 https://bugs.webkit.org/show_bug.cgi?id=111096
    Caused fast/notifications/notifications-request-
    permission.html to crash (Requested by abarth on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-02-28
    
    Source/WebCore:
    
    * GNUmakefile.list.am:
    * Modules/notifications/Notification.h:
    (Notification):
    * Modules/notifications/Notification.idl:
    * Target.pri:
    * UseJSC.cmake:
    * UseV8.cmake:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.vcxproj/WebCore.vcxproj:
    * WebCore.vcxproj/WebCore.vcxproj.filters:
    * WebCore.xcodeproj/project.pbxproj:
    * bindings/js/JSNotificationCustom.cpp: Added.
    (WebCore):
    (WebCore::JSNotification::requestPermission):
    * bindings/scripts/CodeGeneratorJS.pm:
    (GenerateParametersCheck):
    * bindings/scripts/test/JS/JSTestObj.cpp:
    * bindings/v8/custom/V8NotificationCustom.cpp: Added.
    (WebCore):
    (WebCore::V8Notification::requestPermissionMethodCustom):
    
    LayoutTests:
    
    * fast/notifications/notifications-request-permission-optional-expected.txt: Removed.
    * fast/notifications/notifications-request-permission-optional.html: Removed.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/UseJSC.cmake
+++ third_party/WebKit/Source/WebCore/UseJSC.cmake
@@ -122,6 +122,7 @@ list(APPEND WebCore_SOURCES
     bindings/js/JSNodeFilterCustom.cpp
     bindings/js/JSNodeIteratorCustom.cpp
     bindings/js/JSNodeListCustom.cpp
+    bindings/js/JSNotificationCustom.cpp
     bindings/js/JSPluginElementFunctions.cpp
     bindings/js/JSPopStateEventCustom.cpp
     bindings/js/JSProcessingInstructionCustom.cpp
@@ -237,6 +238,7 @@ endif ()
 if (ENABLE_NOTIFICATIONS)
     list(APPEND WebCore_SOURCES
         bindings/js/JSDesktopNotificationsCustom.cpp
+        bindings/js/JSNotificationCustom.cpp
     )
 endif ()
 
