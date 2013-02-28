d5ae8b1ca2019d59ce5a9c010f29095a451fdd21
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 20:51:44 +0000

    Implement more StorageAreaProxy member functions https://bugs.webkit.org/show_bug.cgi?id=111103
    Reviewed by Sam Weinig.
    
    Source/WebCore:
    
    Export two more StorageMap symbols required by WebKit2.
    
    * WebCore.exp.in:
    
    Source/WebKit2:
    
    * Shared/SecurityOriginData.cpp:
    (WebKit::SecurityOriginData::securityOrigin):
    * Shared/SecurityOriginData.h:
    Add helper to create a WebCore::SecurityOrigin from a SecurityOriginData object.
    
    * WebProcess/Storage/StorageAreaProxy.cpp:
    (WebKit::StorageAreaProxy::key):
    Fill the storage map and call through to StorageMap::key.
    
    (WebKit::StorageAreaProxy::contains):
    Fill the storage map and call through to StorageMap::contains.
    
    (WebKit::StorageAreaProxy::memoryBytesUsedByCache):
    Return 0; this matches StorageAreaImpl.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144351 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/Shared/SecurityOriginData.cpp
+++ third_party/WebKit/Source/WebKit2/Shared/SecurityOriginData.cpp
@@ -47,6 +47,11 @@ SecurityOriginData SecurityOriginData::fromSecurityOrigin(SecurityOrigin* securi
     return securityOriginData;
 }
 
+PassRefPtr<SecurityOrigin> SecurityOriginData::securityOrigin() const
+{
+    return SecurityOrigin::create(protocol, host, port);
+}
+
 void SecurityOriginData::encode(CoreIPC::ArgumentEncoder& encoder) const
 {
     encoder << protocol;
--- third_party/WebKit/Source/WebKit2/UIProcess/Storage/StorageManager.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/Storage/StorageManager.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "StorageManager.h"
 
+#include "SecurityOriginData.h"
 #include "StorageAreaProxyMessages.h"
 #include "StorageManagerMessages.h"
 #include "WebProcessProxy.h"
