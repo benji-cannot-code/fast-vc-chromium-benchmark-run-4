eb095751b9622c7eba5357a60eab836fd2f98e7f
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 23:43:02 +0000

    Add the notion of an allowed connection to SessionStorageNamespace https://bugs.webkit.org/show_bug.cgi?id=111122
    Reviewed by Sam Weinig.
    
    Group together each session storage namespace with an allowed connection.
    This will help ensure that rouge web processes will not be able to access session storage
    from pages in other processes.
    
    * UIProcess/Storage/StorageManager.cpp:
    (StorageManager::SessionStorageNamespace):
    (WebKit::StorageManager::SessionStorageNamespace::allowedConnection):
    (WebKit::StorageManager::SessionStorageNamespace::create):
    (WebKit::StorageManager::SessionStorageNamespace::SessionStorageNamespace):
    (WebKit::StorageManager::SessionStorageNamespace::setAllowedConnection):
    Add an m_allowedConnection member variable, as well as setters and getters.
    
    (WebKit::StorageManager::createSessionStorageNamespace):
    Take an optional allowed connection. (It can be null if the process has not finished launching).
    
    (WebKit::StorageManager::setAllowedSessionStorageNamespaceConnection):
    New function to set the allowed connection for a session storage namespace.
    
    (WebKit::StorageManager::createStorageArea):
    Add another FIXME.
    
    (WebKit::StorageManager::createSessionStorageNamespaceInternal):
    Pass the connection to the SessionStorageNamespace constructor.
    
    (WebKit::StorageManager::setAllowedSessionStorageNamespaceConnectionInternal):
    Set the allowed connection.
    
    * UIProcess/WebPageProxy.cpp:
    (WebKit::WebPageProxy::WebPageProxy):
    Pass the connection to createSessionStorageNamespace.
    
    (WebKit::WebPageProxy::connectionWillOpen):
    Call setAllowedSessionStorageNamespaceConnection.
    
    (WebKit::WebPageProxy::connectionWillClose):
    Call setAllowedSessionStorageNamespaceConnection with a null connection.
    
    * UIProcess/WebProcessProxy.cpp:
    (WebKit::WebProcessProxy::connectionWillOpen):
    Call connectionWillOpen on all pages.
    
    (WebKit::WebProcessProxy::connectionWillClose):
    Call connectionWillClose on all pages.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144386 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.cpp
@@ -268,7 +268,7 @@ WebPageProxy::WebPageProxy(PageClient* pageClient, PassRefPtr<WebProcessProxy> p
 #endif
 
     m_process->addMessageReceiver(Messages::WebPageProxy::messageReceiverName(), m_pageID, this);
-    m_process->context()->storageManager().createSessionStorageNamespace(m_pageID);
+    m_process->context()->storageManager().createSessionStorageNamespace(m_pageID, m_process->isValid() ? m_process->connection() : 0);
 }
 
 WebPageProxy::~WebPageProxy()
@@ -2548,6 +2548,20 @@ void WebPageProxy::mouseDidMoveOverElement(const WebHitTestResult::Data& hitTest
     WebEvent::Modifiers modifiers = static_cast<WebEvent::Modifiers>(opaqueModifiers);
 
     m_uiClient.mouseDidMoveOverElement(this, hitTestResultData, modifiers, userData.get());
+}
+
+void WebPageProxy::connectionWillOpen(CoreIPC::Connection* connection)
+{
+    ASSERT(connection == m_process->connection());
+
+    m_process->context()->storageManager().setAllowedSessionStorageNamespaceConnection(m_pageID, connection);
+}
+
+void WebPageProxy::connectionWillClose(CoreIPC::Connection* connection)
+{
+    ASSERT(connection == m_process->connection());
+
+    m_process->context()->storageManager().setAllowedSessionStorageNamespaceConnection(m_pageID, 0);
 }
 
 String WebPageProxy::pluginInformationBundleIdentifierKey()
