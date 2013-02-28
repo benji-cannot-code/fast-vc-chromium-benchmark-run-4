3c06f4fa6fef410fcacc2354e869c34df9c2e512
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 19:13:27 +0000

    Split knowledge of WebContents out of OmniboxEditModel.
    Separates the elements of OmniboxEditModel that interact with the current page via WebContents into a new OmniboxCurrentPageDelegate.
    
    The goal of this change is to support OmniboxEditModel usage on iOS, which
    doesn't use WebContents. An iOS implementation of OmniboxCurrentPageDelegate will follow at a later point.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12304023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185289 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_edit_model.h
+++ chrome/browser/ui/omnibox/omnibox_edit_model.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class AutocompleteController;
 class AutocompleteResult;
 struct InstantSuggestion;
+class OmniboxCurrentPageDelegate;
 class OmniboxEditController;
 class OmniboxPopupModel;
 class OmniboxView;
@@ -412,15 +413,9 @@ class OmniboxEditModel : public AutocompleteControllerDelegate {
       const string16& new_text,
       size_t caret_position) const;
 
-  // Notifies the SearchTabHelper that autocomplete state has changed.
-  void NotifySearchTabHelper();
-
   // Tries to start an Instant preview for |match|. Returns true if Instant
   // processed the match.
   bool DoInstant(const AutocompleteMatch& match);
-
-  // Starts a prerender for the given |match|.
-  void DoPrerender(const AutocompleteMatch& match);
 
   // Starts a DNS prefetch for the given |match|.
   void DoPreconnect(const AutocompleteMatch& match);
@@ -455,6 +450,8 @@ class OmniboxEditModel : public AutocompleteControllerDelegate {
   OmniboxPopupModel* popup_;
 
   OmniboxEditController* controller_;
+
+  scoped_ptr<OmniboxCurrentPageDelegate> delegate_;
 
   OmniboxFocusState focus_state_;
 
