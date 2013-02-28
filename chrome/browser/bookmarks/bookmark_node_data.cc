7ee8b8fe40166ccd6c41b1804a373c3797090e43
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 16:16:10 +0000

    bookmarks: Include the right file to pick kJavaScriptScheme constant.
    BUG=144783
    TBR=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/12314160
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185252 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_node_data.cc
+++ chrome/browser/bookmarks/bookmark_node_data.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/common/url_constants.h"
+#include "content/public/common/url_constants.h"
 #include "net/base/escape.h"
 #include "ui/base/clipboard/scoped_clipboard_writer.h"
 
