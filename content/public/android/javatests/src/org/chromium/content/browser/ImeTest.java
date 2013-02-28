02e33de9b6f637d02ba1983a92d297532deb1c28
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 14:32:45 +0000

    Fix build break caused by typo /DisableTest/DisabledTest/
    BUG=None
    TEST=None
    NOTRY=True
    TBR=tommi
    
    
    Review URL: https://chromiumcodereview.appspot.com/12383023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185242 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
@@ -209,7 +209,7 @@ public class ImeTest extends ContentShellTestBase {
 
     @SmallTest
     @Feature({"TextInput"})
-    @DisableTest
+    @DisabledTest
     public void testImeSelectAndUnSelectAll() throws Exception {
         mImeAdapter.checkCompositionQueueAndCallNative("hello", 1, true);
         assertWaitForSetEditableCallback(2, mConnection);
