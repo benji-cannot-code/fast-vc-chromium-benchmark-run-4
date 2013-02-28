6f6b2d32c0be698f39bc44a62a12dac4eb88854c
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 01:38:52 +0000

    Don't re-get InstallTracker in AppsModelBuilder.
    Currently the destructor for AppsModelBuilder tries to reacquire InstallTracker
    via its factory, but at that point the service has been Shutdown().
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=185086
    
    Review URL: https://codereview.chromium.org/12377002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185098 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/install_tracker.h
+++ chrome/browser/extensions/install_tracker.h
@@ -33,6 +33,9 @@ class InstallTracker : public ProfileKeyedService {
                           int percent_downloaded);
   void OnInstallFailure(const std::string& extension_id);
 
+  // Overriddes for ProfileKeyedService:
+  virtual void Shutdown() OVERRIDE;
+
  private:
   ObserverList<InstallObserver> observers_;
 
--- chrome/browser/ui/app_list/apps_model_builder.h
+++ chrome/browser/ui/app_list/apps_model_builder.h
@@ -22,6 +22,10 @@ class ExtensionAppItem;
 class ExtensionSet;
 class Profile;
 
+namespace extensions {
+class InstallTracker;
+}
+
 namespace gfx {
 class ImageSkia;
 }
@@ -51,6 +55,7 @@ class AppsModelBuilder : public content::NotificationObserver,
                                   int percent_downloaded) OVERRIDE;
 
   virtual void OnInstallFailure(const std::string& extension_id) OVERRIDE;
+  virtual void OnShutdown() OVERRIDE;
 
   // Adds apps in |extensions| to |apps|.
   void AddApps(const ExtensionSet* extensions, Apps* apps);
@@ -100,6 +105,9 @@ class AppsModelBuilder : public content::NotificationObserver,
 
   content::NotificationRegistrar registrar_;
   PrefChangeRegistrar pref_change_registrar_;
+
+  // We listen to this to show app installing progress.
+  extensions::InstallTracker* tracker_;
 
   DISALLOW_COPY_AND_ASSIGN(AppsModelBuilder);
 };
