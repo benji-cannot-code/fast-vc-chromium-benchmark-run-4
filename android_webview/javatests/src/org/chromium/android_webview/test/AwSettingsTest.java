8a68cac1d871d3b3909a1fb3a502040c5d55afa5
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 14:50:14 +0000

    Try to unbreak FindBugs on the android bot after r185236
    The bot is red with:
    M D DLS: Dead store to contentViewCore in org.chromium.android_webview.test.AwSettingsTest.testSetInitialScale()  At AwSettingsTest.java
    
    BUG=none
    TBR=mnaganov
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/12383024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185245 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -2419,7 +2419,6 @@ public class AwSettingsTest extends AndroidWebViewTestBase {
         final AwTestContainerView testContainerView =
                 createAwTestContainerViewOnMainSync(contentClient);
         final AwContents awContents = testContainerView.getAwContents();
-        final ContentViewCore contentViewCore = awContents.getContentViewCore();
         final AwSettings awSettings = getAwSettingsOnUiThread(awContents);
         CallbackHelper onPageFinishedHelper = contentClient.getOnPageFinishedHelper();
 
