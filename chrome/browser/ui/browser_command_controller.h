0db23ede3a06f23b1739e52c7034a34ab8a90e7d
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 19:50:07 +0000

    content: convert interstitial notifications to observer usage
    BUG=170921
    
    Review URL: https://codereview.chromium.org/12383009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185300 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_command_controller.h
+++ chrome/browser/ui/browser_command_controller.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_BROWSER_COMMAND_CONTROLLER_H_
 #define CHROME_BROWSER_UI_BROWSER_COMMAND_CONTROLLER_H_
 
+#include <vector>
+
 #include "base/prefs/public/pref_change_registrar.h"
 #include "chrome/browser/api/sync/profile_sync_service_observer.h"
 #include "chrome/browser/command_updater.h"
@@ -13,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_info_cache_observer.h"
 #include "chrome/browser/sessions/tab_restore_service_observer.h"
 #include "chrome/browser/ui/tabs/tab_strip_model_observer.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
 #include "ui/base/window_open_disposition.h"
 
 class Browser;
@@ -29,7 +29,6 @@ struct NativeWebKeyboardEvent;
 namespace chrome {
 
 class BrowserCommandController : public CommandUpdaterDelegate,
-                                 public content::NotificationObserver,
                                  public ProfileInfoCacheObserver,
                                  public TabStripModelObserver,
                                  public TabRestoreServiceObserver {
@@ -79,6 +78,8 @@ class BrowserCommandController : public CommandUpdaterDelegate,
       Profile* profile);
 
  private:
+  class InterstitialObserver;
+
   enum FullScreenMode {
     // Not in fullscreen mode.
     FULLSCREEN_DISABLED,
@@ -95,11 +96,6 @@ class BrowserCommandController : public CommandUpdaterDelegate,
   virtual void ExecuteCommandWithDisposition(
       int id,
       WindowOpenDisposition disposition) OVERRIDE;
-
-  // Overridden from content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
 
   // Overridden from ProfileInfoCacheObserver:
   virtual void OnProfileAdded(const base::FilePath& profile_path) OVERRIDE;
@@ -205,7 +201,8 @@ class BrowserCommandController : public CommandUpdaterDelegate,
   // Stores the disposition type of the last blocked command.
   WindowOpenDisposition last_blocked_command_disposition_;
 
-  content::NotificationRegistrar registrar_;
+  std::vector<InterstitialObserver*> interstitial_observers_;
+
   PrefChangeRegistrar profile_pref_registrar_;
   PrefChangeRegistrar local_pref_registrar_;
 
