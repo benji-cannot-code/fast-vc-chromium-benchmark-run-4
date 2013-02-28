f1874db8541ac62af73591298b7b91fa85ad7a27
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 21:46:23 +0000

    cc: Without valid frame, delegated renderer layer is not drawable
    When the frame has no render passes, or is empty sized, then the
    delegated renderer layer passes an empty DelegatedFrameData to the
    impl tree and is considered !drawsContent().
    
    Tests:
    LayerTreeHostDelegatedTestLayerUsesFrameDamage.runSingleThread
    LayerTreeHostDelegatedTestLayerUsesFrameDamage.runMultiThread
    
    R=piman
    BUG=123444
    
    
    Review URL: https://chromiumcodereview.appspot.com/12310179
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185329 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/delegated_renderer_layer.h
+++ cc/delegated_renderer_layer.h
@@ -19,6 +19,7 @@ class CC_EXPORT DelegatedRendererLayer : public Layer {
   virtual scoped_ptr<LayerImpl> createLayerImpl(LayerTreeImpl* tree_impl)
       OVERRIDE;
   virtual void pushPropertiesTo(LayerImpl* impl) OVERRIDE;
+  virtual bool drawsContent() const OVERRIDE;
 
   // Set the size at which the frame should be displayed, with the origin at the
   // layer's origin. This must always contain at least the layer's bounds. A
@@ -35,6 +36,7 @@ class CC_EXPORT DelegatedRendererLayer : public Layer {
  private:
   scoped_ptr<DelegatedFrameData> frame_data_;
   gfx::RectF damage_in_frame_;
+  gfx::Size frame_size_;
   gfx::Size display_size_;
 };
 
