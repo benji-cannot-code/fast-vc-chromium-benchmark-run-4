cd53babc6bab4fdf40a91af51b5b487b7c9a4a32
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-02-28 11:34:43 +0000

    Enabled Mosaic view on each volume.
    Since the Image loader extension has been introduced, there is no image size bottle-neck anymore. Therefore, we can generate thumbnails for full-resolution images without affecting the ui. However, generating thumbnails from a full-resolution image takes time, therefore the mosaic mode's been modified, so it is possible to use the mosaic view while loading pictures. There is no need to wait until all the images are loaded.
    
    TEST=Check mosaic view on usb drive in downloads and on google drive.
    BUG=176216
    
    
    Review URL: https://chromiumcodereview.appspot.com/12316118
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185227 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_tasks.js
+++ chrome/browser/resources/file_manager/js/file_tasks.js
@@ -459,7 +459,6 @@ FileTasks.prototype.openGallery = function(urls) {
       metadataCache: fm.metadataCache_,
       pageState: this.params_,
       onClose: onClose,
-      allowMosaic: fm.isOnDrive(),
       onThumbnailError: function(imageURL) {
         fm.metadataCache_.refreshFileMetadata(imageURL);
       },
--- chrome/browser/resources/file_manager/js/media/media_util.js
+++ chrome/browser/resources/file_manager/js/media/media_util.js
@@ -13,9 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     default: IMAGE.
  * @param {Object=} opt_metadata Metadata object.
  * @param {string=} opt_mediaType Media type.
+ * @param {ThumbnailLoader.UseEmbedded=} opt_useEmbedded If to use embedded
+ *     jpeg thumbnail if available. Default: USE_EMBEDDED.
  * @constructor
  */
-function ThumbnailLoader(url, opt_loaderType, opt_metadata, opt_mediaType) {
+function ThumbnailLoader(
+    url, opt_loaderType, opt_metadata, opt_mediaType, opt_useEmbedded) {
+  opt_useEmbedded = opt_useEmbedded || ThumbnailLoader.UseEmbedded.USE_EMBEDDED;
+
   this.mediaType_ = opt_mediaType || FileType.getMediaType(url);
   this.loaderType_ = opt_loaderType || ThumbnailLoader.LoaderType.IMAGE;
   this.metadata_ = opt_metadata;
@@ -37,7 +42,8 @@ function ThumbnailLoader(url, opt_loaderType, opt_metadata, opt_mediaType) {
     }
   }
 
-  if (opt_metadata.thumbnail && opt_metadata.thumbnail.url) {
+  if (opt_metadata.thumbnail && opt_metadata.thumbnail.url &&
+      opt_useEmbedded == ThumbnailLoader.UseEmbedded.USE_EMBEDDED) {
     this.thumbnailUrl_ = opt_metadata.thumbnail.url;
     this.transform_ = opt_metadata.thumbnail.transform;
   } else if (FileType.isImage(url)) {
@@ -60,7 +66,7 @@ ThumbnailLoader.AUTO_FILL_THRESHOLD = 0.3;
 
 /**
  * Type of displaying a thumbnail within a box.
- * @enum
+ * @enum {number}
  */
 ThumbnailLoader.FillMode = {
   FILL: 0,  // Fill whole box. Image may be cropped.
@@ -70,7 +76,7 @@ ThumbnailLoader.FillMode = {
 
 /**
  * Optimization mode for downloading thumbnails.
- * @enum
+ * @enum {number}
  */
 ThumbnailLoader.OptimizationMode = {
   NEVER_DISCARD: 0,    // Never discards downloading. No optimization.
@@ -79,11 +85,21 @@ ThumbnailLoader.OptimizationMode = {
 
 /**
  * Type of element to store the image.
- * @enum
+ * @enum {number}
  */
 ThumbnailLoader.LoaderType = {
   IMAGE: 0,
   CANVAS: 1
+};
+
+/**
+ * Whether to use the embedded thumbnail, or not. The embedded thumbnail may
+ * be small.
+ * @enum {number}
+ */
+ThumbnailLoader.UseEmbedded = {
+  USE_EMBEDDED: 0,
+  NO_EMBEDDED: 1
 };
 
 /**
@@ -124,6 +140,7 @@ ThumbnailLoader.prototype.load = function(box, fillMode, opt_optimizationMode,
     return;
   }
 
+  this.cancel();
   this.canvasUpToDate_ = false;
   this.image_ = new Image();
   this.image_.onload = function() {
@@ -156,7 +173,7 @@ ThumbnailLoader.prototype.load = function(box, fillMode, opt_optimizationMode,
                          this.metadata_.filesystem &&
                          this.metadata_.filesystem.modificationTime &&
                          this.metadata_.filesystem.modificationTime.getTime();
-  var taskId = util.loadImage(
+  this.taskId_ = util.loadImage(
       this.image_,
       this.thumbnailUrl_,
       { maxWidth: ThumbnailLoader.THUMBNAIL_MAX_WIDTH,
@@ -173,8 +190,19 @@ ThumbnailLoader.prototype.load = function(box, fillMode, opt_optimizationMode,
         return true;
       });
 
-  if (!taskId)
+  if (!this.taskId_)
     this.image_.classList.add('cached');
+};
+
+/**
+ * Cancels loading the current image.
+ */
+ThumbnailLoader.prototype.cancel = function() {
+  if (this.taskId_) {
+    this.image_.onload = function() {};
+    this.image_.onerror = function() {};
+    util.cancelLoadImage(this.taskId_);
+  }
 };
 
 /**
@@ -218,6 +246,7 @@ ThumbnailLoader.prototype.loadDetachedImage = function(callback) {
     return;
   }
 
+  this.cancel();
   this.canvasUpToDate_ = false;
   this.image_ = new Image();
   this.image_.onload = callback.bind(null, true);
@@ -228,7 +257,7 @@ ThumbnailLoader.prototype.loadDetachedImage = function(callback) {
                          this.metadata_.filesystem &&
                          this.metadata_.filesystem.modificationTime &&
                          this.metadata_.filesystem.modificationTime.getTime();
-  var taskId = util.loadImage(
+  this.taskId_ = util.loadImage(
       this.image_,
       this.thumbnailUrl_,
       { maxWidth: ThumbnailLoader.THUMBNAIL_MAX_WIDTH,
@@ -236,7 +265,7 @@ ThumbnailLoader.prototype.loadDetachedImage = function(callback) {
         cache: true,
         timestamp: modificationTime });
 
-  if (!taskId)
+  if (!this.taskId_)
     this.image_.classList.add('cached');
 };
 
@@ -275,7 +304,7 @@ ThumbnailLoader.prototype.attachImage = function(container, fillMode) {
   util.applyTransform(container, this.transform_);
   ThumbnailLoader.centerImage_(
       container, attachableMedia, fillMode, this.isRotated_());
-  if (this.image_.parentNode != container) {
+  if (attachableMedia.parentNode != container) {
     container.textContent = '';
     container.appendChild(attachableMedia);
   }
