138a67146291beb075773a09383647ff353615d0
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-02-28 00:17:27 +0000

    Use FeatureObserver to see how often web sites use multipart main documents https://bugs.webkit.org/show_bug.cgi?id=111015
    Reviewed by Nate Chapin.
    
    Multipart main documents add sigificant complexity to the loader.  It
    would be interesting to know how often this complexity is used.
    
    * loader/MainResourceLoader.cpp:
    (WebCore::MainResourceLoader::responseReceived):
    * page/FeatureObserver.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/MainResourceLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/MainResourceLoader.cpp
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "DocumentLoadTiming.h"
 #include "DocumentLoader.h"
+#include "FeatureObserver.h"
 #include "FormState.h"
 #include "Frame.h"
 #include "FrameLoader.h"
@@ -428,8 +429,10 @@ void MainResourceLoader::responseReceived(CachedResource* resource, const Resour
         m_resource->clear();
     }
     
-    if (r.isMultipart())
+    if (r.isMultipart()) {
+        FeatureObserver::observe(m_documentLoader->frame()->document(), FeatureObserver::MultipartMainResource);
         m_loadingMultipartContent = true;
+    }
         
     // The additional processing can do anything including possibly removing the last
     // reference to this object; one example of this is 3266216.
