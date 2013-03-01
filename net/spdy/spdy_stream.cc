9bff99e908d1c5947b0ff0a7e6ef37593d60032f
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 22:12:20 +0000

    [SPDY] Implement per-session flow control
    Basically, we treat a WINDOW_UPDATE frame with stream_id 0
    as an update for the session window.
    
    Convert the flow_control boolean into a tri-state FlowControlState enum. Add a kProtoSPDY31 constant in next_proto.h and turn on per-session flow control only if SPDY 3.1 is negotiated.
    
    BUG=176592
    
    Review URL: https://codereview.chromium.org/12277015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185603 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream.cc
+++ net/spdy/spdy_stream.cc
@@ -248,7 +248,7 @@ void SpdyStream::PossiblyResumeIfStalled() {
 }
 
 void SpdyStream::AdjustSendWindowSize(int32 delta_window_size) {
-  DCHECK(session_->is_flow_control_enabled());
+  DCHECK_GE(session_->flow_control_state(), SpdySession::FLOW_CONTROL_STREAM);
 
   if (closed())
     return;
@@ -265,7 +265,7 @@ void SpdyStream::AdjustSendWindowSize(int32 delta_window_size) {
 }
 
 void SpdyStream::IncreaseSendWindowSize(int32 delta_window_size) {
-  DCHECK(session_->is_flow_control_enabled());
+  DCHECK_GE(session_->flow_control_state(), SpdySession::FLOW_CONTROL_STREAM);
 
   // Ignore late WINDOW_UPDATEs.
   if (closed())
@@ -297,7 +297,7 @@ void SpdyStream::IncreaseSendWindowSize(int32 delta_window_size) {
 }
 
 void SpdyStream::DecreaseSendWindowSize(int32 delta_window_size) {
-  DCHECK(session_->is_flow_control_enabled());
+  DCHECK_GE(session_->flow_control_state(), SpdySession::FLOW_CONTROL_STREAM);
 
   if (closed())
     return;
@@ -320,8 +320,13 @@ void SpdyStream::DecreaseSendWindowSize(int32 delta_window_size) {
 }
 
 void SpdyStream::IncreaseRecvWindowSize(int32 delta_window_size) {
-  if (!session_->is_flow_control_enabled())
+  if (session_->flow_control_state() < SpdySession::FLOW_CONTROL_STREAM)
     return;
+
+  // Call back into the session, since this is the only
+  // window-size-related function that is called by the delegate
+  // instead of by the session.
+  session_->IncreaseRecvWindowSize(delta_window_size);
 
   // By the time a read is processed by the delegate, this stream may
   // already be inactive.
@@ -343,7 +348,7 @@ void SpdyStream::IncreaseRecvWindowSize(int32 delta_window_size) {
   unacked_recv_window_bytes_ += delta_window_size;
   if (unacked_recv_window_bytes_ >
       session_->stream_initial_recv_window_size() / 2) {
-    session_->SendWindowUpdate(
+    session_->SendStreamWindowUpdate(
         stream_id_, static_cast<uint32>(unacked_recv_window_bytes_));
     unacked_recv_window_bytes_ = 0;
   }
@@ -493,7 +498,7 @@ void SpdyStream::OnDataReceived(const char* data, size_t length) {
     return;
   }
 
-  if (session_->is_flow_control_enabled())
+  if (session_->flow_control_state() >= SpdySession::FLOW_CONTROL_STREAM)
     DecreaseRecvWindowSize(static_cast<int32>(length));
 
   // Track our bandwidth.
@@ -853,7 +858,7 @@ void SpdyStream::UpdateHistograms() {
 
 void SpdyStream::DecreaseRecvWindowSize(int32 delta_window_size) {
   DCHECK(session_->IsStreamActive(stream_id_));
-  DCHECK(session_->is_flow_control_enabled());
+  DCHECK_GE(session_->flow_control_state(), SpdySession::FLOW_CONTROL_STREAM);
   DCHECK_GE(delta_window_size, 1);
 
   // Since we never decrease the initial window size,
