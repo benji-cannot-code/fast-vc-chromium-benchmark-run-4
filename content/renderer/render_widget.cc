255ed8bfc057324478ce38e758e3eef91964cdc9
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 07:21:18 +0000

    Plumb physical backing size from RWHV to renderer CC.
    This patch introduces a concept of "physical backing size" which is plumbed
    down from port-specific browser code to the renderer compositor.
    
    This is distinct from the usual concept of view size on high-DPI screens
    with >1 dpi scale.  Physical backing size represents the size of the
    texture CC draws to, whereas the view size is given to WebKit and
    specifies the CSS width/height.
    
    Before this patch, physical backing size was recalculated in the
    renderer by multiplying the view size by DPI scale.  There are two problems
    with this:
    
    - This introduces off-by-one errors when DPI scale is non-integral.
      Values such as 1.5 and 1.33125 are common on Android devices.
    - The Android port would like to more efficiently implement features like
      on-screen-keyboard bringup and URL-bar hiding without needing to
      resize the backing texture. Separating the value given to CC and to
      WebKit is the first step towards this.
    
    WebKit counterpart at https://bugs.webkit.org/show_bug.cgi?id=110727
    
    NOTRY=true
    BUG=152502
    
    Review URL: https://chromiumcodereview.appspot.com/12328080
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185475 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -330,6 +330,7 @@ bool RenderWidget::Send(IPC::Message* message) {
 }
 
 void RenderWidget::Resize(const gfx::Size& new_size,
+                          const gfx::Size& physical_backing_size,
                           const gfx::Rect& resizer_rect,
                           bool is_fullscreen,
                           ResizeAck resize_ack) {
@@ -341,7 +342,10 @@ void RenderWidget::Resize(const gfx::Size& new_size,
   if (!webwidget_)
     return;
 
-  // Remember the rect where the resize corner will be drawn.
+  if (compositor_)
+    compositor_->setViewportSize(new_size, physical_backing_size);
+
+  physical_backing_size_ = physical_backing_size_;
   resizer_rect_ = resizer_rect;
 
   // NOTE: We may have entered fullscreen mode without changing our size.
@@ -415,9 +419,11 @@ void RenderWidget::OnCreatingNewAck() {
 }
 
 void RenderWidget::OnResize(const gfx::Size& new_size,
+                            const gfx::Size& physical_backing_size,
                             const gfx::Rect& resizer_rect,
                             bool is_fullscreen) {
-  Resize(new_size, resizer_rect, is_fullscreen, SEND_RESIZE_ACK);
+  Resize(new_size, physical_backing_size, resizer_rect, is_fullscreen,
+         SEND_RESIZE_ACK);
 }
 
 void RenderWidget::OnChangeResizeRect(const gfx::Rect& resizer_rect) {
@@ -1348,6 +1354,7 @@ void RenderWidget::initializeLayerTreeView(
     return;
 
   compositor_->setRootLayer(root_layer);
+  compositor_->setViewportSize(size_, physical_backing_size_);
   if (init_complete_)
     compositor_->setSurfaceReady();
 }
