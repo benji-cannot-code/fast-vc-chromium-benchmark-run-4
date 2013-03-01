be49be643987d97498f13d7d8559e855e09f2dd8
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 00:16:38 +0000

    Require manifests for native messaging hosts.
    Previously native messaging hosts were discovered by looking in the user
    data directory. That approach is hard to use and is not reliable. Now
    chrome will look for a host manifest file in a system-specific directory
    and load that file to find host binary.
    
    This is a second attempt to land this CL as it was reverted (see
    https://codereview.chromium.org/12386018). The problem that caused it to
    be reverted was not related to this CL.
    
    BUG=142915
    TBR=mpcomplete@chromium.org
    
    Review URL: https://codereview.chromium.org/12389041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185382 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/messaging/native_message_process_host.h
+++ chrome/browser/extensions/api/messaging/native_message_process_host.h
@@ -41,8 +41,7 @@ class NativeMessageProcessHost
 #endif  // !defined(OS_POSIX)
  {
  public:
-  // Interface for classes that which to recieve messages from the native
-  // process.
+  // Interface for the object that receives messages from the native process.
   class Client {
    public:
     virtual ~Client() {}
@@ -56,12 +55,14 @@ class NativeMessageProcessHost
 
   static scoped_ptr<NativeMessageProcessHost> Create(
       base::WeakPtr<Client> weak_client_ui,
+      const std::string& source_extension_id,
       const std::string& native_host_name,
       int destination_port);
 
   // Create using specified |launcher|. Used in tests.
   static scoped_ptr<NativeMessageProcessHost> CreateWithLauncher(
       base::WeakPtr<Client> weak_client_ui,
+      const std::string& source_extension_id,
       const std::string& native_host_name,
       int destination_port,
       scoped_ptr<NativeProcessLauncher> launcher);
@@ -81,6 +82,7 @@ class NativeMessageProcessHost
 
  private:
   NativeMessageProcessHost(base::WeakPtr<Client> weak_client_ui,
+                           const std::string& source_extension_id,
                            const std::string& native_host_name,
                            int destination_port,
                            scoped_ptr<NativeProcessLauncher> launcher);
@@ -115,6 +117,9 @@ class NativeMessageProcessHost
   // The Client messages will be posted to. Should only be accessed from the
   // UI thread.
   base::WeakPtr<Client> weak_client_ui_;
+
+  // ID of the calling extension.
+  std::string source_extension_id_;
 
   // Name of the native messaging host.
   std::string native_host_name_;
