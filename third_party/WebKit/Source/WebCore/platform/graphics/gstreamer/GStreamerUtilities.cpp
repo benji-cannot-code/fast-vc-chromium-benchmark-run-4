53f6b03c65761f358171921aafcb557315e04bc1
ryuan.choi@samsung.com <ryuan.choi@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-01 01:53:51 +0000

    [EFL] Build break with --no-web-audio and --no-video https://bugs.webkit.org/show_bug.cgi?id=111055
    Unreviewed build fix.
    
    
    * platform/graphics/gstreamer/GStreamerUtilities.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/GStreamerUtilities.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/gstreamer/GStreamerUtilities.cpp
@@ -21,10 +21,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "GStreamerUtilities.h"
 
+#if USE(GSTREAMER)
 #include <gst/gst.h>
 #include <wtf/gobject/GOwnPtr.h>
-
-#if USE(GSTREAMER)
 
 namespace WebCore {
 
@@ -44,4 +43,4 @@ bool initializeGStreamer()
 
 }
 
-#endif // ENABLE(WEB_AUDIO) && USE(GSTREAMER)
+#endif // USE(GSTREAMER)
