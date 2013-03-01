40acae707d72802dbd4f3adc1210da1bdbbc85aa
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 20:02:51 +0000

    Mark IME tests flaky
    They started failing simulataneously on two bots.  Can't find cause
    yet.
    
    BUG=179455
    TBR=bulach,aurimas
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/12385054
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185581 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ImeTest.java
@@ -9,6 +9,7 @@ import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.MediumTest;
 import android.test.suitebuilder.annotation.SmallTest;
 import android.text.TextUtils;
@@ -127,8 +128,12 @@ public class ImeTest extends ContentShellTestBase {
         assertEquals(-1, mConnection.mCompositionEnd);
     }
 
-    @SmallTest
-    @Feature({"TextInput"})
+    /**
+     * @SmallTest
+     * @Feature({"TextInput"})
+     * BUG=179455
+     */
+    @FlakyTest
     public void testImeCopy() throws Exception {
         mImeAdapter.checkCompositionQueueAndCallNative("hello", 1, true);
         assertWaitForSetEditableCallback(2, mConnection);
@@ -170,8 +175,12 @@ public class ImeTest extends ContentShellTestBase {
         assertClipboardContents(getActivity(), "narf");
     }
 
-    @SmallTest
-    @Feature({"TextInput"})
+    /**
+     * @SmallTest
+     * @Feature({"TextInput"})
+     * BUG=179455
+     */
+    @FlakyTest
     public void testImePaste() throws Exception {
         ThreadUtils.runOnUiThreadBlocking(new Runnable() {
             @Override
@@ -232,8 +241,12 @@ public class ImeTest extends ContentShellTestBase {
         assertWaitForKeyboardStatus(false);
     }
 
-    @SmallTest
-    @Feature({"TextInput", "Main"})
+    /**
+     * @SmallTest
+     * @Feature({"TextInput", "Main"})
+     * BUG=179455
+     */
+    @FlakyTest
     public void testUpdatesGetIgnoredDuringBatchEdits() throws Throwable {
         mConnection.beginBatchEdit();
         assertWaitForSetIgnoreUpdates(true, mConnection);
@@ -270,8 +283,12 @@ public class ImeTest extends ContentShellTestBase {
         assertWaitForSetIgnoreUpdates(false, mConnection);
     }
 
-    @SmallTest
-    @Feature({"TextInput", "Main"})
+    /**
+     * @SmallTest
+     * @Feature({"TextInput", "Main"})
+     * BUG=179455
+     */
+    @FlakyTest
     public void testShowImeIfNeeded() throws Throwable {
         DOMUtils.focusNode(this, mContentView, mCallbackContainer, "input_radio");
         assertWaitForKeyboardStatus(false);
