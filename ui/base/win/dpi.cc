6d39e30f97ac363ed6508b9a7755c4b7522607dc
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 09:29:58 +0000

    Revert 185474 -- speculative. browser_tests stopped starting up on all xp bots. I'm guessing the GetProcAddressed() function might misbehave on XP.
    > Fix touch scaling for high dpi windows.
    > 
    > Touch events in windows are always expressed in screen coordinates, even if
    > the target app is not dpi-aware. This patch determines the dpi scale used
    > by the OS, and re-scales touch events accordingly. 
    > 
    > In some cases the touch events are sent to the wrong window. This is detected
    > and corrected.
    > 
    > BUG=175542
    > 
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12317157
    
    TBR=girard@chromium.org
    Review URL: https://codereview.chromium.org/12379040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185502 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/win/dpi.cc
+++ ui/base/win/dpi.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 
 #include "base/win/scoped_hdc.h"
-#include "base/win/registry.h"
 #include "ui/gfx/display.h"
 #include "ui/gfx/point_conversions.h"
 #include "ui/gfx/rect_conversions.h"
@@ -27,16 +26,6 @@ float GetDeviceScaleFactorImpl() {
 #else
   return 1.0f;
 #endif
-}
-
-BOOL IsProcessDPIAwareWrapper() {
-  typedef BOOL(WINAPI *IsProcessDPIAwarePtr)(VOID);
-  IsProcessDPIAwarePtr is_process_dpi_aware_func =
-      reinterpret_cast<IsProcessDPIAwarePtr>(
-          GetProcAddress(GetModuleHandleA("user32.dll"), "IsProcessDPIAware"));
-  if (is_process_dpi_aware_func)
-    return is_process_dpi_aware_func();
-  return FALSE;
 }
 
 }  // namespace
@@ -110,28 +99,6 @@ gfx::Size ScreenToDIPSize(const gfx::Size& size_in_pixels) {
 
 gfx::Size DIPToScreenSize(const gfx::Size& dip_size) {
   return gfx::ToFlooredSize(gfx::ScaleSize(dip_size, GetDeviceScaleFactor()));
-}
-
-double GetDPIScaleFromRegistry() {
-  static double scale = -1.0;
-  if (scale == -1.0) {
-    double result = 1.0;
-    if (!IsProcessDPIAwareWrapper()) {
-      //HKEY_CURRENT_USER\Control Panel\Desktop\WindowMetrics\AppliedDPI
-      base::win::RegKey key(HKEY_CURRENT_USER,
-                            L"Control Panel\\Desktop\\WindowMetrics",
-                            KEY_QUERY_VALUE);
-
-      if (key.Valid()) {
-        DWORD value = 0;
-        if (key.ReadValueDW(L"AppliedDPI", &value) == ERROR_SUCCESS) {
-          result = ((double)value) / kDefaultDPIX;
-        }
-      }
-    }
-    scale = result;
-  }
-  return scale;
 }
 
 }  // namespace win
