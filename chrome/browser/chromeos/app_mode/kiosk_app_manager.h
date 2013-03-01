d97a921e085ffb1de9daf6e34ce0d549fb22924b
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 02:41:53 +0000

    cros: More kiosk app launch business.
    - Add StartupAppLauncher to launch kiosk app to get rid of nested message loop
      and we no longer need "install-from-webstore" switch;
    - Implement auto launch from login screen;
    - Add a shortcut key (Ctrl-Alt-Shift-X) to bail out from app launch and return to
      login screen;
    
    BUG=173803
    TEST=Verify kiosk app could be launched via menu, auto launched and hold
         shortcut key on app splash screen would return to login screen.
    
    R=zelidrag@chromium.org,sky@chromium.org,asargent@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12382020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185420 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/app_mode/kiosk_app_manager.h
+++ chrome/browser/chromeos/app_mode/kiosk_app_manager.h
@@ -50,7 +50,9 @@ class KioskAppManager : public KioskAppPrefsObserver,
   // Name of a dictionary that holds kiosk app info.
   // Sample layout:
   //   "kiosk": {
-  //     "auto_launch": "app_id1",  // Exist iff using local state pref
+  //     "auto_launch": "app_id1",  // Exists if using local state pref
+  //     "suppress_auto_launch": true,  // Exists if when auto launch is
+  //                                    // suppressed. Default is false.
   //     "apps": {
   //       "app_id1" : {
   //         "name": "name of app1",
@@ -80,6 +82,10 @@ class KioskAppManager : public KioskAppPrefsObserver,
 
   // Sets |app_id| as the app to auto launch at start up.
   void SetAutoLaunchApp(const std::string& app_id);
+
+  // Gets/sets suppress auto launch flag.
+  bool GetSuppressAutoLaunch() const;
+  void SetSuppressAutoLaunch(bool suppress);
 
   // Adds/removes a kiosk app by id. When removed, all locally cached data
   // will be removed as well.
