02d80203e3cb5c3f667084cea8696f5b142f28f2
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 07:54:02 +0000

    [content shell] sync the navigation state to the browser before recording the navigation history
    In layout tests that dump the back/forward history, the state of the latest
    navigation entry (which is kept in the browser browser) does not reliably
    reflect the current state. This is because updates to the state are only send
    when a new navigation starts or else at regular intervals.
    
    Therefore, we force a sync of the session history state when running layout
    tests that dump the back/forward history.
    
    BUG=111316
    R=brettw@chromium.org
    TEST=remaining http/tests/navigation tests pass
    
    Review URL: https://codereview.chromium.org/12330099
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185490 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_runner.cc
+++ content/shell/webkit_test_runner.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
 #include "content/public/renderer/render_view.h"
+#include "content/public/renderer/render_view_visitor.h"
 #include "content/public/test/layouttest_support.h"
 #include "content/shell/shell_messages.h"
 #include "content/shell/shell_render_process_observer.h"
@@ -110,6 +111,19 @@ void CopyCanvasToBitmap(SkCanvas* canvas,  SkBitmap* snapshot) {
 #endif
 
 }
+
+class SyncNavigationStateVisitor : public RenderViewVisitor {
+ public:
+  SyncNavigationStateVisitor() {}
+  virtual ~SyncNavigationStateVisitor() {}
+
+  virtual bool Visit(RenderView* render_view) OVERRIDE {
+    SyncNavigationState(render_view);
+    return true;
+  }
+ private:
+  DISALLOW_COPY_AND_ASSIGN(SyncNavigationStateVisitor);
+};
 
 }  // namespace
 
@@ -334,6 +348,8 @@ void WebKitTestRunner::testFinished() {
       ShellRenderProcessObserver::GetInstance()->test_interfaces();
   interfaces->setTestIsRunning(false);
   if (interfaces->testRunner()->shouldDumpBackForwardList()) {
+    SyncNavigationStateVisitor visitor;
+    RenderView::ForEach(&visitor);
     Send(new ShellViewHostMsg_CaptureSessionHistory(routing_id()));
   } else {
     CaptureDump();
