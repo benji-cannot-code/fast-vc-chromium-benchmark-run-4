d5aed4411711762a17a940653f24811562ce05a2
mnaganov@chromium.org <mnaganov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 16:23:24 +0000

    [Android WebView] Add simple support for remote web debugging
    This is a non-final way of exposing web debugging for WebView,
    but it is extremely useful for anyone who needs to debug its
    web content in the new version of WebView.
    
    This patch unconditionally exposes 'webview_devtools_remote_<pid>'
    linux abstract socket for remote debugging. The DevTools frontend
    is already being bundled with WebView, so this patch doesn't add
    any new resources.
    
    Review URL: https://codereview.chromium.org/12378011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185543 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/aw_browser_main_parts.h
+++ android_webview/browser/aw_browser_main_parts.h
@@ -18,6 +18,7 @@ namespace android_webview {
 bool UseCompositorDirectDraw();
 
 class AwBrowserContext;
+class AwDevToolsDelegate;
 
 class AwBrowserMainParts : public content::BrowserMainParts {
  public:
@@ -29,12 +30,14 @@ class AwBrowserMainParts : public content::BrowserMainParts {
   virtual int PreCreateThreads() OVERRIDE;
   virtual void PreMainMessageLoopRun() OVERRIDE;
   virtual bool MainMessageLoopRun(int* result_code) OVERRIDE;
+  virtual void PostMainMessageLoopRun() OVERRIDE;
 
  private:
   // Android specific UI MessageLoop.
   scoped_ptr<MessageLoop> main_message_loop_;
 
   AwBrowserContext* browser_context_;  // weak
+  AwDevToolsDelegate* devtools_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(AwBrowserMainParts);
 };
