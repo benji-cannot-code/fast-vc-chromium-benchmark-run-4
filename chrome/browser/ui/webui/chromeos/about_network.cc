9b183c3364b210c560c14faedfcb95b4534b5596
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 02:31:32 +0000

    Escape text in chrome://network.
    BUG=chromium-os:39016
    
    Review URL: https://chromiumcodereview.appspot.com/12380009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185416 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/about_network.cc
+++ chrome/browser/ui/webui/chromeos/about_network.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "grit/generated_resources.h"
+#include "net/base/escape.h"
 #include "ui/base/l10n/l10n_util.h"
 
 namespace chromeos {
@@ -25,15 +26,15 @@ namespace {
 // Html output helper functions
 
 std::string WrapWithH3(const std::string& text) {
-  return "<h3>" + text + "</h3>";
+  return "<h3>" + net::EscapeForHTML(text) + "</h3>";
 }
 
 std::string WrapWithTH(const std::string& text) {
-  return "<th>" + text + "</th>";
+  return "<th>" + net::EscapeForHTML(text) + "</th>";
 }
 
 std::string WrapWithTD(const std::string& text) {
-  return "<td>" + text + "</td>";
+  return "<td>" + net::EscapeForHTML(text) + "</td>";
 }
 
 std::string WrapWithTR(const std::string& text) {
