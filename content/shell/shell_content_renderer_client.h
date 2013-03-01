71a4373de485142bace513237d1ce6b60d594e45
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 15:15:16 +0000

    [content shell] use the MockWebHyphenator for layout tests
    Copy GetChromiumRootDirFilePath() from webkit_support as content
    shell cannot depend on webkit_support
    
    BUG=111316
    TEST=text/fast/hyphenate-* passes
    R=thakis@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12377043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185537 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_content_renderer_client.h
+++ content/shell/shell_content_renderer_client.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/compiler_specific.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/platform_file.h"
 #include "content/public/renderer/content_renderer_client.h"
 
 namespace WebKit {
@@ -18,6 +19,10 @@ struct WebPluginParams;
 
 namespace WebTestRunner {
 class WebTestProxyBase;
+}
+
+namespace webkit_glue {
+class MockWebHyphenator;
 }
 
 class MockWebClipboardImpl;
@@ -31,6 +36,10 @@ class ShellContentRendererClient : public ContentRendererClient {
  public:
   ShellContentRendererClient();
   virtual ~ShellContentRendererClient();
+
+  void LoadHyphenDictionary(base::PlatformFile dict_file);
+
+  // ContentRendererClient implementation.
   virtual void RenderThreadStarted() OVERRIDE;
   virtual void RenderViewCreated(RenderView* render_view) OVERRIDE;
   virtual bool OverrideCreatePlugin(
@@ -45,6 +54,7 @@ class ShellContentRendererClient : public ContentRendererClient {
       WebKit::WebRTCPeerConnectionHandlerClient* client) OVERRIDE;
   virtual WebKit::WebClipboard* OverrideWebClipboard() OVERRIDE;
   virtual WebKit::WebMimeRegistry* OverrideWebMimeRegistry() OVERRIDE;
+  virtual WebKit::WebHyphenator* OverrideWebHyphenator() OVERRIDE;
   virtual bool AllowBrowserPlugin(
       WebKit::WebPluginContainer* container) const OVERRIDE;
 
@@ -55,6 +65,7 @@ class ShellContentRendererClient : public ContentRendererClient {
   scoped_ptr<ShellRenderProcessObserver> shell_observer_;
   scoped_ptr<MockWebClipboardImpl> clipboard_;
   scoped_ptr<TestShellWebMimeRegistryImpl> mime_registry_;
+  scoped_ptr<webkit_glue::MockWebHyphenator> hyphenator_;
 };
 
 }  // namespace content
