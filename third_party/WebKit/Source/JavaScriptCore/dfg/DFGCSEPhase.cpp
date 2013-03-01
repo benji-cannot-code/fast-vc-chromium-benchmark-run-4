77d9463eb7ad40b7ee7c37aae157e38f94aca4a9
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-01 20:40:05 +0000

    DFG CSE phase shouldn't rely on ref count of nodes, since it doesn't have to https://bugs.webkit.org/show_bug.cgi?id=111205
    Reviewed by Oliver Hunt.
            
    I don't understand the intuition behind setLocalStoreElimination() validating that the SetLocal's ref count
    is 1. I believe this is a hold-over from when setLocalStoreElimination() would match one SetLocal to another,
    and then try to eliminate the first SetLocal. But that's not how it works now. Now, setLocalStoreElimination()
    is actually Flush elimination: it eliminates any Flush that anchors a SetLocal if it proves that every path
    from the SetLocal to the Flush is devoid of operations that may observe the local. It doesn't actually kill
    the SetLocal itself: if the SetLocal is live because of other things (other Flushes or GetLocals in other
    basic blocks), then the SetLocal will naturally still be alive because th Flush was only keeping the SetLocal
    alive by one count rather than being solely responsible for its liveness.
    
    * dfg/DFGCSEPhase.cpp:
    (JSC::DFG::CSEPhase::setLocalStoreElimination):
    (JSC::DFG::CSEPhase::eliminate):
    (JSC::DFG::CSEPhase::performNodeCSE):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/dfg/DFGCSEPhase.cpp
+++ third_party/WebKit/Source/JavaScriptCore/dfg/DFGCSEPhase.cpp
@@ -940,8 +940,6 @@ private:
                     break;
                 if (node != expectedNode)
                     result.mayBeAccessed = true;
-                if (node->refCount() > 1)
-                    result.mayBeAccessed = true;
                 return result;
             }
                 
@@ -1044,7 +1042,6 @@ private:
         dataLogF("   Eliminating @%u", m_currentNode->index());
 #endif
         
-        ASSERT(m_currentNode->refCount() == 1);
         ASSERT(m_currentNode->mustGenerate());
         m_currentNode->convertToPhantom();
         eliminateIrrelevantPhantomChildren(m_currentNode);
@@ -1055,8 +1052,6 @@ private:
     void eliminate(Node* node, NodeType phantomType = Phantom)
     {
         if (!node)
-            return;
-        if (node->refCount() != 1)
             return;
         ASSERT(node->mustGenerate());
         node->setOpAndDefaultNonExitFlags(phantomType);
@@ -1209,7 +1204,6 @@ private:
             if (result.mayBeAccessed || result.mayClobberWorld)
                 break;
             ASSERT(replacement->op() == SetLocal);
-            ASSERT(replacement->refCount() == 1);
             ASSERT(replacement->shouldGenerate());
             // FIXME: Investigate using mayExit as a further optimization.
             node->convertToPhantom();
