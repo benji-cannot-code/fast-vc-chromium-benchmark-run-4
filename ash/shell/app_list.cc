820d08538a5f9b7206d018f918dd008142c5fb64
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 04:59:15 +0000

    [win] Remove shadow from tab overlay in app launcher.
    BUG=175621
    TBR=sky for ash example app list change
    
    Review URL: https://chromiumcodereview.appspot.com/12388040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185470 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/app_list.cc
+++ ash/shell/app_list.cc
@@ -44,7 +44,7 @@ class WindowTypeLauncherItem : public app_list::AppListItemModel {
   };
 
   explicit WindowTypeLauncherItem(Type type) : type_(type) {
-    SetIcon(GetIcon(type));
+    SetIcon(GetIcon(type), false);
     SetTitle(GetTitle(type));
   }
 
--- chrome/browser/ui/app_list/extension_app_item.cc
+++ chrome/browser/ui/app_list/extension_app_item.cc
@@ -278,7 +278,7 @@ void ExtensionAppItem::Move(const ExtensionAppItem* prev,
 
 void ExtensionAppItem::UpdateIcon() {
   if (!GetExtension()) {
-    SetIcon(installing_icon_);
+    SetIcon(installing_icon_, false);
     return;
   }
   gfx::ImageSkia icon = icon_->image_skia();
@@ -297,7 +297,7 @@ void ExtensionAppItem::UpdateIcon() {
     icon = gfx::ImageSkia(new TabOverlayImageSource(icon, size), size);
   }
 
-  SetIcon(icon);
+  SetIcon(icon, !HasOverlay());
 }
 
 const Extension* ExtensionAppItem::GetExtension() const {
