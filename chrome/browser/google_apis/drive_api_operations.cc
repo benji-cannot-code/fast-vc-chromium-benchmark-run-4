657cb388bd766e624c2d1098ffa7617ba36d5521
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-01 08:07:46 +0000

    Move the responsibility to convert from JSON to FileResource into drive_api_operations.
    These are two purposes of this change.
    1) Resolve the asymmetricity of the JSON and typed C++ conversion.
    Now, operation classes take the C++ parameters and format them into JSON.
    However, the response is converted in DriveAPIService. This CL resolves the
    asymmetricity for FileResource.
    2) This is also the preparation of the ResumeUploadOperation implementation.
    
    BUG=148632
    TEST=Ran unit_tests.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12387021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185495 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/drive_api_operations.cc
+++ chrome/browser/google_apis/drive_api_operations.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/drive_api_operations.h"
 
 #include "base/bind.h"
+#include "base/callback.h"
 #include "base/json/json_writer.h"
 #include "base/values.h"
 #include "chrome/browser/google_apis/drive_api_parser.h"
@@ -21,26 +22,27 @@ const char kContentTypeApplicationJson[] = "application/json";
 const char kDirectoryMimeType[] = "application/vnd.google-apps.folder";
 const char kParentLinkKind[] = "drive#fileLink";
 
-// Parses the JSON value to AboutResource and runs |callback| on the UI
+// Parses the JSON value to a resource typed |T| and runs |callback| on the UI
 // thread once parsing is done.
-void ParseAboutResourceAndRun(
-    const GetAboutResourceCallback& callback,
+template<typename T>
+void ParseJsonAndRun(
+    const base::Callback<void(GDataErrorCode, scoped_ptr<T>)>& callback,
     GDataErrorCode error,
     scoped_ptr<base::Value> value) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(!callback.is_null());
 
-  scoped_ptr<AboutResource> about_resource;
+  scoped_ptr<T> resource;
   if (value.get()) {
-    about_resource = AboutResource::CreateFrom(*value);
-    if (!about_resource) {
-      // Failed to parse the JSON value (although the JSON value is available),
-      // so let callback know the parsing error.
+    resource = T::CreateFrom(*value);
+    if (!resource) {
+      // Failed to parse the JSON value, although the JSON value is available,
+      // so let the callback know the parsing error.
       error = GDATA_PARSE_ERROR;
     }
   }
 
-  callback.Run(error, about_resource.Pass());
+  callback.Run(error, resource.Pass());
 }
 
 }  // namespace
@@ -53,7 +55,7 @@ GetAboutOperation::GetAboutOperation(
     const DriveApiUrlGenerator& url_generator,
     const GetAboutResourceCallback& callback)
     : GetDataOperation(registry, url_request_context_getter,
-                       base::Bind(&ParseAboutResourceAndRun, callback)),
+                       base::Bind(&ParseJsonAndRun<AboutResource>, callback)),
       url_generator_(url_generator) {
   DCHECK(!callback.is_null());
 }
@@ -133,8 +135,9 @@ GetFileOperation::GetFileOperation(
     net::URLRequestContextGetter* url_request_context_getter,
     const DriveApiUrlGenerator& url_generator,
     const std::string& file_id,
-    const GetDataCallback& callback)
-    : GetDataOperation(registry, url_request_context_getter, callback),
+    const FileResourceCallback& callback)
+    : GetDataOperation(registry, url_request_context_getter,
+                       base::Bind(&ParseJsonAndRun<FileResource>, callback)),
       url_generator_(url_generator),
       file_id_(file_id) {
   DCHECK(!callback.is_null());
@@ -156,8 +159,9 @@ CreateDirectoryOperation::CreateDirectoryOperation(
     const DriveApiUrlGenerator& url_generator,
     const std::string& parent_resource_id,
     const std::string& directory_name,
-    const GetDataCallback& callback)
-    : GetDataOperation(registry, url_request_context_getter, callback),
+    const FileResourceCallback& callback)
+    : GetDataOperation(registry, url_request_context_getter,
+                       base::Bind(&ParseJsonAndRun<FileResource>, callback)),
       url_generator_(url_generator),
       parent_resource_id_(parent_resource_id),
       directory_name_(directory_name) {
--- chrome/browser/google_apis/drive_api_operations.h
+++ chrome/browser/google_apis/drive_api_operations.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "base/callback_forward.h"
 #include "chrome/browser/google_apis/base_operations.h"
 #include "chrome/browser/google_apis/drive_api_url_generator.h"
 #include "chrome/browser/google_apis/drive_service_interface.h"
@@ -17,6 +18,15 @@ class URLRequestContextGetter;
 }  // namespace net
 
 namespace google_apis {
+
+class FileResource;
+
+// Callback used for operations that the server returns FileResource data
+// formatted into JSON value.
+typedef base::Callback<void(GDataErrorCode error,
+                            scoped_ptr<FileResource> entry)>
+    FileResourceCallback;
+
 
 //============================== GetAboutOperation =============================
 
@@ -126,7 +136,7 @@ class GetFileOperation : public GetDataOperation {
                    net::URLRequestContextGetter* url_request_context_getter,
                    const DriveApiUrlGenerator& url_generator,
                    const std::string& file_id,
-                   const GetDataCallback& callback);
+                   const FileResourceCallback& callback);
   virtual ~GetFileOperation();
 
  protected:
@@ -158,7 +168,7 @@ class CreateDirectoryOperation : public GetDataOperation {
       const DriveApiUrlGenerator& url_generator,
       const std::string& parent_resource_id,
       const std::string& directory_name,
-      const GetDataCallback& callback);
+      const FileResourceCallback& callback);
   virtual ~CreateDirectoryOperation();
 
  protected:
