adc4671477ae6bc7516e732f29cba1caa68263ce
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 17:14:29 +0000

    Mac: Audio indicator in each tab
    This is the Mac version of this views CL:
    https://chromiumcodereview.appspot.com/12328027/
    
    When audio is playing a throbber animation is displayed in the tab.
    
    BUG=3541
    TEST=
        Navigate to http://www.html5rocks.com/en/tutorials/video/basics/
        Play video. Verify that throbber is displayed over the fav icon.
        Pause video. Verify that throbber stops.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12374042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185715 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tabs/throbbing_image_view.h
+++ chrome/browser/ui/cocoa/tabs/throbbing_image_view.h
@@ -28,6 +28,9 @@ class ThrobbingImageViewAnimationDelegate;
     backgroundImage:(NSImage*)backgroundImage
          throbImage:(NSImage*)throbImage
          durationMS:(int)durationMS;
+
+- (void)setTweenType:(ui::Tween::Type)type;
+
 @end
 
 #endif  // CHROME_BROWSER_UI_COCOA_TABS_THROBBING_IMAGE_VIEW_H_
