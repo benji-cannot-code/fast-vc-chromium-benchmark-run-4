0b242dcaa7613bc296e80e969a2a115b4f1a9b08
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 01:12:27 +0000

    Close all delegates during OnFilterRemoved().
    Should resolve all crashes unless we're getting into a state where
    AddDelegate/RemoveDelegate is called by a delegate for which Start()
    had not yet been called.
    
    I suspect that's unlikely since at the time of RemoveFilter() the
    RenderThread should be in its dying throes, leaving no one else alive
    to issue such a call.
    
    I've also removed the CHECK() entirely since it's not thread safe.
    The message filters are not destructed on the io thread.
    
    This is all a product of AudioOutputIPC not being reference counted
    while IPC::ChannelProxy::MessageFilter is.  We end up with devices
    which hold uncounted references.
    
    BUG=178499
    TEST=none
    
    Review URL: https://codereview.chromium.org/12378035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185644 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/audio_input_message_filter.cc
+++ content/renderer/media/audio_input_message_filter.cc
@@ -23,7 +23,6 @@ AudioInputMessageFilter::AudioInputMessageFilter(
 }
 
 AudioInputMessageFilter::~AudioInputMessageFilter() {
-  CHECK(delegates_.IsEmpty());
   DCHECK_EQ(filter_, this);
   filter_ = NULL;
 }
@@ -67,7 +66,10 @@ void AudioInputMessageFilter::OnFilterAdded(IPC::Channel* channel) {
 
 void AudioInputMessageFilter::OnFilterRemoved() {
   DCHECK(io_message_loop_->BelongsToCurrentThread());
-  channel_ = NULL;
+
+  // Once removed, a filter will not be used again.  At this time all
+  // delegates must be notified so they release their reference.
+  OnChannelClosing();
 }
 
 void AudioInputMessageFilter::OnChannelClosing() {
