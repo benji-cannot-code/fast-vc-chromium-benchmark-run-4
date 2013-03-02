79ef961d6674ba75d7b265c024a967acbc3c8349
jeremycho@chromium.org <jeremycho@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 17:09:11 +0000

    Fix clipping of Most Visited titles.  Will follow-up with a search-side change to preserve the vertical positioning (updating .mv-title{bottom}).
    BUG=178315
    
    
    Review URL: https://chromiumcodereview.appspot.com/12314165
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185713 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/searchbox_api.js
+++ chrome/renderer/resources/extensions/searchbox_api.js
@@ -294,7 +294,7 @@ if (!chrome.embeddedSearch) {
       native function UndoMostVisitedDeletion();
 
       function SafeWrapMostVisited(restrictedText, width) {
-        return SafeWrap(restrictedText, width, 14, 11);
+        return SafeWrap(restrictedText, width, 18, 11);
       }
 
       function GetMostVisitedItemsWrapper() {
