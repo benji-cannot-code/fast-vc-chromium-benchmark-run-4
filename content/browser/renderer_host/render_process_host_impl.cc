35103523fe7e0546ac1e89548a189428d8e1fd6a
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 17:26:08 +0000

    Add a flag to disable gesture requirement for media playback
    For webGL, we need to enable video autoplay in some circumstances. Adding a flag so that developers to test their page.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12378045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185720 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -824,6 +824,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #if defined(OS_ANDROID)
     switches::kMediaPlayerInRenderProcess,
     switches::kNetworkCountryIso,
+    switches::kDisableGestureRequirementForMediaPlayback,
 #endif
     switches::kNoReferrers,
     switches::kNoSandbox,
