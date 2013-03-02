1f5606de2b1c87e5dafde88bab1139c66cb8721e
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 03:57:47 +0000

    Implement CopyFromCompositingSurfaceToVideoFrame for mac. Move ComputeLetterboxRegion and add ConvertRGBToVideoFrame to VideoUtil class. Fix intersecting the capture region with unscaled iosurface_size.
    BUG=175358
    TBR=kbr
    
    Review URL: https://chromiumcodereview.appspot.com/12326085
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185678 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_util.cc
+++ media/base/video_util.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "media/base/video_frame.h"
+#include "media/base/yuv_convert.h"
 
 namespace media {
 
@@ -185,6 +186,53 @@ void RotatePlaneByPixels(
     src += src_stride;
     dest += dest_row_step;
   }
+}
+
+gfx::Rect ComputeLetterboxRegion(const gfx::Rect& bounds,
+                                 const gfx::Size& content) {
+  int64 x = static_cast<int64>(content.width()) * bounds.height();
+  int64 y = static_cast<int64>(content.height()) * bounds.width();
+
+  gfx::Size letterbox(bounds.width(), bounds.height());
+  if (y < x)
+    letterbox.set_height(static_cast<int>(y / content.width()));
+  else
+    letterbox.set_width(static_cast<int>(x / content.height()));
+  gfx::Rect result = bounds;
+  result.ClampToCenteredSize(letterbox);
+  return result;
+}
+
+void CopyRGBToVideoFrame(const uint8* source,
+                         int stride,
+                         const gfx::Rect& region_in_frame,
+                         VideoFrame* frame) {
+  const int kY = VideoFrame::kYPlane;
+  const int kU = VideoFrame::kUPlane;
+  const int kV = VideoFrame::kVPlane;
+  CHECK_EQ(frame->stride(kU), frame->stride(kV));
+  const int uv_stride = frame->stride(kU);
+
+  if (region_in_frame != gfx::Rect(frame->coded_size())) {
+    // TODO(justinlin): Optimize this by clearing just the margins.
+    // http://crbug.com/178789
+    FillYUV(frame, 0x00, 0x80, 0x80);
+  }
+
+  const int y_offset = region_in_frame.x()
+                     + (region_in_frame.y() * frame->stride(kY));
+  const int uv_offset = region_in_frame.x() / 2
+                      + (region_in_frame.y() / 2 * uv_stride);
+
+  ConvertRGB32ToYUV(source,
+                    frame->data(kY) + y_offset,
+                    frame->data(kU) + uv_offset,
+                    frame->data(kV) + uv_offset,
+                    region_in_frame.width(),
+                    region_in_frame.height(),
+                    stride,
+                    frame->stride(kY),
+                    uv_stride);
 }
 
 }  // namespace media
--- media/base/video_util.h
+++ media/base/video_util.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "media/base/media_export.h"
+#include "ui/gfx/rect.h"
 #include "ui/gfx/size.h"
 
 namespace media {
@@ -57,6 +58,19 @@ MEDIA_EXPORT void RotatePlaneByPixels(
     int rotation,  // Clockwise.
     bool flip_vert,
     bool flip_horiz);
+
+// Return the largest centered rectangle with the same aspect ratio of |content|
+// that fits entirely inside of |bounds|.
+MEDIA_EXPORT gfx::Rect ComputeLetterboxRegion(const gfx::Rect& bounds,
+                                              const gfx::Size& content);
+
+// Copy an RGB bitmap into the specified |region_in_frame| of a YUV video frame.
+// Fills the regions outside |region_in_frame| with black.
+MEDIA_EXPORT void CopyRGBToVideoFrame(const uint8* source,
+                                      int stride,
+                                      const gfx::Rect& region_in_frame,
+                                      VideoFrame* frame);
+
 }  // namespace media
 
 #endif  // MEDIA_BASE_VIDEO_UTIL_H_
