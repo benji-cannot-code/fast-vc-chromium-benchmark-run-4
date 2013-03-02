0eee181dadea71bbdfb5e0940f62b6d9c9927de0
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 18:50:56 +0000

    SavePackageFilePickerChromeOS should only force MHTML when saving HTML, not when saving text/image/etc. BUG=166595
    Review URL: https://chromiumcodereview.appspot.com/12084075
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/download/save_package_file_picker_chromeos.h
+++ chrome/browser/download/save_package_file_picker_chromeos.h
@@ -23,6 +23,7 @@ class SavePackageFilePickerChromeOS : public ui::SelectFileDialog::Listener,
   SavePackageFilePickerChromeOS(
       content::WebContents* web_contents,
       const base::FilePath& suggested_path,
+      bool is_html,
       const content::SavePackagePathPickedCallback& callback);
 
   // Used to disable prompting the user for a directory/filename of the saved
@@ -43,6 +44,7 @@ class SavePackageFilePickerChromeOS : public ui::SelectFileDialog::Listener,
   virtual void FileSelectionCanceled(void* params) OVERRIDE;
 
   content::SavePackagePathPickedCallback callback_;
+  bool is_html_;
 
   // For managing select file dialogs.
   scoped_refptr<ui::SelectFileDialog> select_file_dialog_;
