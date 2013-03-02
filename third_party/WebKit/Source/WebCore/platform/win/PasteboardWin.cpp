b91eb2513f6d9e79024134419378f0d73dc94e6d
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-02 09:07:37 +0000

    Fix a typo in my previous commit (r144534).
    * platform/win/PasteboardWin.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144536 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/win/PasteboardWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/win/PasteboardWin.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Pasteboard.h"
 
 #include "BitmapInfo.h"
-#include "CacheImage.h"
+#include "CachedImage.h"
 #include "ClipboardUtilitiesWin.h"
 #include "Document.h"
 #include "DocumentFragment.h"
