77971635b95022f02b3d0254a60a4c5a482bc213
wittman@chromium.org <wittman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-02 04:07:34 +0000

    Add initial capability to display try job status for the user's active CLs
    This change includes the minimum capability necessary to get try job
    status display in the popup window, along with useful tooltip information.
    
    Remaining TODOs:
     - encapsulate CL and try job state representation for use from background and popup pages
     - provide notifications to popup on CL/try job state update
     - update popup display in response to state change notifications
     - source default username from codereview.chromium.org, if logged in
     - display link to options page if no try username set
    
    BUG=178588
    
    
    Review URL: https://chromiumcodereview.appspot.com/12316145
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185680 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/examples/extensions/buildbot/prefs.js
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function Prefs() {
+  this.defaults_ = {prefs: {use_notifications: false,
+                            try_job_username: ""}};
+}
+
+Prefs.prototype = {
+  get_: function(key, callback) {
+    chrome.storage.sync.get(this.defaults_,
+        function (storage) {
+          callback(storage.prefs[key]);
+        });
+  },
+
+  set_: function(key, value) {
+    chrome.storage.sync.get(this.defaults_,
+        function (storage) {
+          storage.prefs[key] = value;
+          chrome.storage.sync.set(storage);
+        });
+  },
+
+  getUseNotifications: function(callback) {
+    this.get_("use_notifications", callback);
+  },
+
+  setUseNotifications: function(use_notifications) {
+    this.set_("use_notifications", use_notifications);
+  },
+
+  getTryJobUsername: function(callback) {
+    this.get_("try_job_username", callback);
+  },
+
+  setTryJobUsername: function(try_job_username) {
+    this.set_("try_job_username", try_job_username);
+  }
+};
