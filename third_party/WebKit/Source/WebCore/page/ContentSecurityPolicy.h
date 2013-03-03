7e44f8512124d10d6227c06bfe6a31b4d84ffde4
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-03 11:06:19 +0000

    CSP 1.1: Support CSP 1.1 directives on the unprefixed header. https://bugs.webkit.org/show_bug.cgi?id=111254
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    We'd like to ensure that early adopters don't get stuck on a prefixed
    header; when CSP 1.1 is baked enough to be supported in multiple
    browsers, everything should Just Work™.
    
    This patch changes WebKit's behavior regarding CSP_NEXT features.
    Currently, they're only exposed on the prefixed header ('X-WebKit-CSP').
    This patch exposes those features on the canonical header, assuming
    that the runtime flag is set. This shouldn't have any effect at all on
    ports that haven't yet enabled CSP_NEXT, and will simply clear the way
    for a clean deployment to a wider audience once the specification
    process is further along.
    
    This change shouldn't have any effect on the existing tests: they
    should run just as they did before. The next step will be to adjust
    the LayoutTests for 1.1 to prefer the canonical header, but I'll do
    that in another patch to reduce churn.
    
    Related, the enum names no longer made sense: the difference between the
    canonical 'Content-Security-Policy' header and 'X-WebKit-CSP' is the
    prefix, not the functionality. This patch renames them for clarity.
    
    * dom/Document.cpp:
    (WebCore::Document::processHttpEquiv):
    * loader/FrameLoader.cpp:
    (WebCore::FrameLoader::didBeginDocument):
        Use the new enum names.
    * page/ContentSecurityPolicy.cpp:
    (CSPDirectiveList):
    (WebCore::CSPDirectiveList::CSPDirectiveList):
    (WebCore::CSPDirectiveList::addDirective):
        Drop the 'm_experimental' property from CSPDirectiveList; we'll
        control the behavior via the runtime flag from now on.
    
        Also, this fixes a small bug in 'addDirective()': we never threw
        unrecognized directive errors for users who were sending the
        prefixed header. Oops!
    (WebCore::ContentSecurityPolicy::deprecatedHeaderType):
        Use the new enum names.
    * page/ContentSecurityPolicy.h:
        Redefine the enum for clarity: 'PrefixedReport' and 'Report'
        rather than 'ReportAllDirectives' and 'ReportStableDirectives'.
    
    Source/WebKit/chromium:
    
    The enum names no longer made sense: the difference between the
    canonical 'Content-Security-Policy' header and 'X-WebKit-CSP' is the
    prefix, not the functionality. This patch renames them for clarity.
    
    * public/WebContentSecurityPolicy.h:
    * src/AssertMatchingEnums.cpp:
        Rename the Chromium side of the enums.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144571 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
+++ third_party/WebKit/Source/WebCore/page/ContentSecurityPolicy.h
@@ -61,10 +61,10 @@ public:
     void copyStateFrom(const ContentSecurityPolicy*);
 
     enum HeaderType {
-        ReportStableDirectives,
-        EnforceStableDirectives,
-        ReportAllDirectives,
-        EnforceAllDirectives
+        Report,
+        Enforce,
+        PrefixedReport,
+        PrefixedEnforce
     };
 
     enum ReportingStatus {
--- third_party/WebKit/Source/WebKit/chromium/public/WebContentSecurityPolicy.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebContentSecurityPolicy.h
@@ -35,10 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 enum WebContentSecurityPolicyType {
-    WebContentSecurityPolicyTypeReportStableDirectives,
-    WebContentSecurityPolicyTypeEnforceStableDirectives,
-    WebContentSecurityPolicyTypeReportAllDirectives,
-    WebContentSecurityPolicyTypeEnforceAllDirectives,
+    WebContentSecurityPolicyTypeReport,
+    WebContentSecurityPolicyTypeEnforce,
+    WebContentSecurityPolicyTypePrefixedReport,
+    WebContentSecurityPolicyTypePrefixedEnforce,
 };
 
 } // namespace WebKit
