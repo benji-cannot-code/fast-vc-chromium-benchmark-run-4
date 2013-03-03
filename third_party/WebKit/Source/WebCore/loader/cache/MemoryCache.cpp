be912897582a8f4036de2e8cccf1fc02ac1a45db
darin@apple.com <darin@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-03 06:55:45 +0000

    Another try at fixing the build.
    git-svn-id: svn://svn.chromium.org/blink/trunk@144557 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/cache/MemoryCache.cpp
+++ third_party/WebKit/Source/WebCore/loader/cache/MemoryCache.cpp
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MemoryCache.h"
 
 #include "CachedResource.h"
+#include "CachedResourceHandle.h"
 #include "CrossThreadTask.h"
 #include "Document.h"
 #include "FrameLoader.h"
