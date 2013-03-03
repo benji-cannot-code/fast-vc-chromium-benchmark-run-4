8cc79337493a4c73793017cbaf3e73722445b968
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-03 15:59:17 +0000

    BUILD FIX: RenderLayerFilterInfo.h needs to include Element.h with ENABLE(SVG)
    Fixes the following build failures:
    
        In file included from Source/WebCore/rendering/RenderLayerFilterInfo.cpp:33:
        Source/WebCore/rendering/RenderLayerFilterInfo.h:118:19: error: use of undeclared identifier 'Element'
            Vector<RefPtr<Element> > m_internalSVGReferences;
                          ^
        Source/WebCore/rendering/RenderLayerFilterInfo.h:118:28: error: expected a type
            Vector<RefPtr<Element> > m_internalSVGReferences;
                                   ^
        Source/WebCore/rendering/RenderLayerFilterInfo.h:118:30: error: private field 'm_internalSVGReferences' is not used [-Werror,-Wunused-private-field]
            Vector<RefPtr<Element> > m_internalSVGReferences;
                                     ^
        3 errors generated.
    
    * rendering/RenderLayerFilterInfo.h: Include Element.h within
    ENABLE(SVG).  Move ENABLE(SVG) block below unconditional
    headers.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144574 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerFilterInfo.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerFilterInfo.h
@@ -33,9 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(CSS_FILTERS)
 
-#if ENABLE(SVG)
-#include "CachedSVGDocument.h"
-#endif
 #include "FilterOperation.h"
 #include "LayoutRect.h"
 #include <wtf/HashMap.h>
@@ -44,6 +41,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(CSS_SHADERS)
 #include "CustomFilterProgramClient.h"
+#endif
+
+#if ENABLE(SVG)
+#include "CachedSVGDocument.h"
+#include "Element.h"
 #endif
 
 namespace WebCore {
