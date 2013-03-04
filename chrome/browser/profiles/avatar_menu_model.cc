bcddc33cdaaa1b18e81cef03743d528ac71eccd5
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 12:27:40 +0000

    Hide add new profile menu entry for managed users. Currently only implemented for gtk and aura. The screenshots can be found here: https://drive.google.com/a/google.com/folderview?id=0BzMCIwDgeNezVmNHSG5pUXoxVGs&usp=sharing
    BUG=171370
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/12379014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185877 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/avatar_menu_model.cc
+++ chrome/browser/profiles/avatar_menu_model.cc
@@ -30,6 +30,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
+#if defined(ENABLE_MANAGED_USERS)
+#include "chrome/browser/managed_mode/managed_user_service.h"
+#include "chrome/browser/managed_mode/managed_user_service_factory.h"
+#endif
+
 using content::BrowserThread;
 
 namespace {
@@ -161,6 +166,20 @@ size_t AvatarMenuModel::GetActiveProfileIndex() {
 const AvatarMenuModel::Item& AvatarMenuModel::GetItemAt(size_t index) {
   DCHECK_LT(index, items_.size());
   return *items_[index];
+}
+
+bool AvatarMenuModel::ShouldShowAddNewProfileLink() const {
+#if defined(ENABLE_MANAGED_USERS)
+  Profile* active_profile = NULL;
+  if (!browser_)
+    active_profile = ProfileManager::GetLastUsedProfile();
+  else
+    active_profile = browser_->profile();
+  ManagedUserService* service = ManagedUserServiceFactory::GetForProfile(
+      active_profile);
+  return !service->ProfileIsManaged();
+#endif
+  return true;
 }
 
 void AvatarMenuModel::Observe(int type,
