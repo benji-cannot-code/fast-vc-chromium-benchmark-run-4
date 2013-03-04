879b122cd34abd6da0af192a8a35e8e8983cb75a
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 15:45:34 +0000

    Added support of Esc button for cancellation of gaia and manged user creation screens.
    BUG=151207
    
    
    Review URL: https://chromiumcodereview.appspot.com/12398003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185903 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_gaia_signin.js
+++ chrome/browser/resources/chromeos/login/screen_gaia_signin.js
@@ -66,6 +66,10 @@ cr.define('login', function() {
     // Timer id of pending load.
     loadingTimer_: undefined,
 
+    // Whether user can cancel Gaia screen.
+    // @type {boolean}
+    cancelAllowed_: undefined,
+
     /** @override */
     decorate: function() {
       this.frame_ = $('signin-frame');
@@ -272,9 +276,9 @@ cr.define('login', function() {
       $('createAccount').hidden = !data.createAccount;
       $('guestSignin').hidden = !data.guestSignin;
       $('createLocallyManagedUser').hidden = !data.createLocallyManagedUser;
-      // Only show Cancel button when user pods can be displayed.
-      $('login-header-bar').allowCancel =
-          data.isShowUsers && $('pod-row').pods.length;
+      // Allow cancellation of screen only when user pods can be displayed.
+      this.cancelAllowed_ = data.isShowUsers && $('pod-row').pods.length;
+      $('login-header-bar').allowCancel = this.cancelAllowed_;
 
       // Sign-in right panel is hidden if all of its items are hidden.
       var noRightPanel = $('gaia-signin-reason').hidden &&
@@ -467,6 +471,17 @@ cr.define('login', function() {
         // Defer the bubble until the frame has been loaded.
         this.errorBubble_ = [loginAttempts, error];
       }
+    },
+
+    /**
+     * Called when user canceled signin.
+     */
+    cancel: function() {
+      if (!this.cancelAllowed_)
+        return;
+      $('pod-row').loadLastWallpaper();
+      Oobe.showScreen({id: SCREEN_ACCOUNT_PICKER});
+      Oobe.resetSigninUI(true);
     }
   };
 
