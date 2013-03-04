6b89e885a94fc68d4da0411a8fc9c09f5cf146bc
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 17:48:25 +0000

    Revert 185269 > Turn off shader name hashing by default. > > This has to be turned off due to a mac AMD driver regression. > > This is for M26, so once it's landed on trunk and merged back to M26, I will revert it on trunk. > > BUG=178287 > TEST=run MapsGL on a MacBook Pro with AMD GPU and the latest 10.8.3 12D68 seed, zoom in to a city, and ensure end caps of streets render correctly. > TBR=gman > Review URL: https://codereview.chromium.org/12379005
    TBR=zmo@chromium.org
    Review URL: https://codereview.chromium.org/12377083
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185920 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_switches.cc
+++ gpu/command_buffer/service/gpu_switches.cc
@@ -21,8 +21,8 @@ const char kDisableGLSLTranslator[]         = "disable-glsl-translator";
 const char kDisableGpuDriverBugWorkarounds[] =
     "disable-gpu-driver-bug-workarounds";
 
-// Turn on user-defined name hashing in shaders.
-const char kEnableShaderNameHashing[]      = "enable-shader-name-hashing";
+// Turn off user-defined name hashing in shaders.
+const char kDisableShaderNameHashing[]      = "disable-shader-name-hashing";
 
 // Turn on Logging GPU commands.
 const char kEnableGPUCommandLogging[]       = "enable-gpu-command-logging";
@@ -64,7 +64,7 @@ const char* kGpuSwitches[] = {
   kDisableGLErrorLimit,
   kDisableGLSLTranslator,
   kDisableGpuDriverBugWorkarounds,
-  kEnableShaderNameHashing,
+  kDisableShaderNameHashing,
   kEnableGPUCommandLogging,
   kEnableGPUDebugging,
   kEnableGPUServiceLoggingGPU,
