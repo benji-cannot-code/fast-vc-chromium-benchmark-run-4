14aeca2d2e550c2c44b85dbe144f4bad7c9c8120
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-04 13:34:47 +0000

    Web Inspector: Adding new rules broken if a <style> tag is added to document dynamically https://bugs.webkit.org/show_bug.cgi?id=111299
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    The CSSStyleSheet instance should be retrieved directly from the HTMLStyleElement just created,
    not from the document.styleSheets list.
    
    Test: inspector/styles/add-new-rule-with-style-after-body.html
    
    * inspector/InspectorCSSAgent.cpp:
    (WebCore::InspectorCSSAgent::viaInspectorStyleSheet):
    
    LayoutTests:
    
    * inspector/styles/add-new-rule-with-style-after-body-expected.txt: Added.
    * inspector/styles/add-new-rule-with-style-after-body.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorCSSAgent.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DOMWindow.h"
 #include "ExceptionCodePlaceholder.h"
 #include "HTMLHeadElement.h"
+#include "HTMLStyleElement.h"
 #include "InspectorDOMAgent.h"
 #include "InspectorHistory.h"
 #include "InspectorState.h"
@@ -1081,11 +1082,16 @@ InspectorStyleSheet* InspectorCSSAgent::viaInspectorStyleSheet(Document* documen
     }
     if (ec)
         return 0;
-    StyleSheetList* styleSheets = document->styleSheets();
-    StyleSheet* styleSheet = styleSheets->item(styleSheets->length() - 1);
-    if (!styleSheet || !styleSheet->isCSSStyleSheet())
+
+    HTMLElement* htmlElement = toHTMLElement(styleElement.get());
+    if (!htmlElement || !htmlElement->hasTagName(HTMLNames::styleTag))
         return 0;
-    CSSStyleSheet* cssStyleSheet = static_cast<CSSStyleSheet*>(styleSheet);
+
+    HTMLStyleElement* htmlStyleElement = static_cast<HTMLStyleElement*>(htmlElement);
+    CSSStyleSheet* cssStyleSheet = htmlStyleElement->sheet();
+    if (!cssStyleSheet)
+        return 0;
+
     String id = String::number(m_lastStyleSheetId++);
     inspectorStyleSheet = InspectorStyleSheet::create(m_domAgent->pageAgent(), id, cssStyleSheet, TypeBuilder::CSS::StyleSheetOrigin::Inspector, InspectorDOMAgent::documentURLString(document), this);
     m_idToInspectorStyleSheet.set(id, inspectorStyleSheet);
