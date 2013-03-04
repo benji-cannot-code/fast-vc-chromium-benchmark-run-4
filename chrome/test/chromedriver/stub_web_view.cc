50d097f77d2c2ee4981a0ea6ee2ee128ee56df6d
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 19:41:05 +0000

    [chromedriver] Implement remaining pure JS commands and screenshot. BUG=none
    Review URL: https://chromiumcodereview.appspot.com/12328101
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185946 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/stub_web_view.cc
+++ chrome/test/chromedriver/stub_web_view.cc
@@ -67,3 +67,7 @@ Status StubWebView::GetMainFrame(std::string* frame_id) {
 JavaScriptDialogManager* StubWebView::GetJavaScriptDialogManager() {
   return NULL;
 }
+
+Status StubWebView::CaptureScreenshot(std::string* screenshot) {
+  return Status(kOk);
+}
