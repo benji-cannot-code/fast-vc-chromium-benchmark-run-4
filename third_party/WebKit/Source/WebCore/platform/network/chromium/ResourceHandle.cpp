d2a7d2ab4af9e3fed017fa2a944d31dd551fd8f7
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-04 22:05:57 +0000

    Small BlobResourceHandle cleanup https://bugs.webkit.org/show_bug.cgi?id=111352
    Reviewed by Sam Weinig.
    
    1. Don't use PassRefPtr<BlobStorageData> when ownership is not being passed.
    2. Added OVERRIDE to some functions.
    3. Added const to ResourceHandle::firstRequest().
    
    * platform/network/BlobRegistryImpl.cpp:
    (WebCore::loadResourceSynchronously):
    (WebCore::BlobRegistryImpl::createResourceHandle):
    (WebCore::BlobRegistryImpl::getBlobDataFromURL):
    * platform/network/BlobRegistryImpl.h:
    (BlobRegistryImpl):
    * platform/network/BlobResourceHandle.cpp:
    (WebCore::BlobResourceHandle::createAsync):
    (WebCore::BlobResourceHandle::loadResourceSynchronously):
    * platform/network/BlobResourceHandle.h:
    (BlobResourceHandle):
    * platform/network/FormData.cpp:
    (WebCore::appendBlobResolved):
    * platform/network/ResourceHandle.cpp:
    (WebCore::ResourceHandle::firstRequest):
    * platform/network/ResourceHandle.h:
    (ResourceHandle):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144671 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceHandle.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceHandle.cpp
@@ -203,7 +203,7 @@ PassRefPtr<ResourceHandle> ResourceHandle::create(NetworkingContext* context,
     return 0;
 }
 
-ResourceRequest& ResourceHandle::firstRequest()
+ResourceRequest& ResourceHandle::firstRequest() const
 {
     return d->request();
 }
