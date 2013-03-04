504d0822bf225b8e609d85079de487a9e9ebb402
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-04 13:15:26 +0000

    Web Inspector: Unreviewed. Fix for closure type annotations.
    * inspector/front-end/FlameChart.js:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/FlameChart.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/FlameChart.js
@@ -130,6 +130,11 @@ WebInspector.FlameChart.prototype = {
         this._forEachNode(this._drawNode.bind(this));
     },
 
+    /**
+     * @param {!number} offset
+     * @param {!number} level
+     * @param {!ProfilerAgent.CPUProfileNode} node
+     */
     _drawNode: function(offset, level, node)
     {
         ++this._colorIndex;
@@ -139,6 +144,9 @@ WebInspector.FlameChart.prototype = {
         this._drawBar(this._context, offset, level, node, color);
     },
 
+    /**
+     * @param {!function(!number, !number, !ProfilerAgent.CPUProfileNode)} callback
+     */
     _forEachNode: function(callback)
     {
         var nodes = this._rootNodes();
@@ -170,7 +178,7 @@ WebInspector.FlameChart.prototype = {
      * @param {number} offset
      * @param {number} level
      * @param {!ProfilerAgent.CPUProfileNode} node
-     * @param {!WebInspector.Color} hslColor
+     * @param {!string} hslColor
      */
     _drawBar: function(context, offset, level, node, hslColor)
     {
@@ -191,5 +199,3 @@ WebInspector.FlameChart.prototype = {
 
     __proto__: WebInspector.View.prototype
 };
-
-//@ sourceURL=http://localhost/inspector/front-end/FlameChart.js
