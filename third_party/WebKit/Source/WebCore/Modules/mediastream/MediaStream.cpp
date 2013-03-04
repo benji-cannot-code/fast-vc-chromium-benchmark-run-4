6b3ae4e1a334de8bf8c3664b8a42fdcff65d82c7
adam.bergkvist@ericsson.com <adam.bergkvist@ericsson.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-04 10:07:39 +0000

    MediaStream API: local addTrack() and removeTrack() operations should not fire events. https://bugs.webkit.org/show_bug.cgi?id=111079
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Removed the scheduling of "addtrack" and "removetrack" events by
    addTrack and removeTrack(). Replaced the test since the old test was
    driven by the event firing removed by this patch.
    
    Test: fast/mediastream/MediaStream-add-remove-tracks.html
    
    * Modules/mediastream/MediaStream.cpp:
    (WebCore::MediaStream::addTrack):
    (WebCore::MediaStream::removeTrack):
    
    LayoutTests:
    
    Replaced the test since the old test was driven by the event firing
    removed by this patch.
    
    * fast/mediastream/MediaStream-add-remove-tracks-expected.txt: Added.
    * fast/mediastream/MediaStream-add-remove-tracks.html: Added.
    * fast/mediastream/MediaStreamTrackList-expected.txt: Removed.
    * fast/mediastream/MediaStreamTrackList.html: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144610 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/mediastream/MediaStream.cpp
+++ third_party/WebKit/Source/WebCore/Modules/mediastream/MediaStream.cpp
@@ -166,7 +166,6 @@ void MediaStream::addTrack(PassRefPtr<MediaStreamTrack> prpTrack, ExceptionCode&
     }
 
     MediaStreamCenter::instance().didAddMediaStreamTrack(m_descriptor.get(), newTrack->component());
-    scheduleDispatchEvent(MediaStreamTrackEvent::create(eventNames().addtrackEvent, false, false, newTrack.release()));
 }
 
 void MediaStream::removeTrack(PassRefPtr<MediaStreamTrack> prpTrack , ExceptionCode& ec)
@@ -203,7 +202,6 @@ void MediaStream::removeTrack(PassRefPtr<MediaStreamTrack> prpTrack , ExceptionC
     }
 
     MediaStreamCenter::instance().didRemoveMediaStreamTrack(m_descriptor.get(), track->component());
-    scheduleDispatchEvent(MediaStreamTrackEvent::create(eventNames().removetrackEvent, false, false, track.release()));
 }
 
 MediaStreamTrack* MediaStream::getTrackById(String id)
