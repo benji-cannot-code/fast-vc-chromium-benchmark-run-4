eb18024924b4017fdc5d37c04f5fb31d73e005b3
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 21:58:52 +0000

    Revert 185964 > Part II: clear the clipboard if a user copies something from onmibox of Incognito window and closes that window. > > > BUG=171974 > > > Review URL: https://chromiumcodereview.appspot.com/12334040
    TBR=vasilii@chromium.org
    Review URL: https://codereview.chromium.org/12391077
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185980 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_view_mac.mm
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/browser_process.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/cocoa/event_utils.h"
 #include "chrome/browser/ui/cocoa/location_bar/autocomplete_text_field_cell.h"
 #include "chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.h"
@@ -877,10 +876,6 @@ void OmniboxViewMac::CopyToPasteboard(NSPasteboard* pb) {
     [pb declareURLPasteboardWithAdditionalTypes:[NSArray array] owner:nil];
     [pb setDataForURL:base::SysUTF8ToNSString(url.spec()) title:nstext];
   }
-  ui::Clipboard::WriteSourceTag(pb,
-                                content::BrowserContext::
-                                    GetMarkerForOffTheRecordContext(
-                                        model()->profile()));
 }
 
 void OmniboxViewMac::CopyURLToPasteboard(NSPasteboard* pb) {
@@ -896,10 +891,6 @@ void OmniboxViewMac::CopyURLToPasteboard(NSPasteboard* pb) {
 
   [pb declareURLPasteboardWithAdditionalTypes:[NSArray array] owner:nil];
   [pb setDataForURL:base::SysUTF8ToNSString(url.spec()) title:nstext];
-  ui::Clipboard::WriteSourceTag(pb,
-                                content::BrowserContext::
-                                    GetMarkerForOffTheRecordContext(
-                                        model()->profile()));
 }
 
 void OmniboxViewMac::OnPaste() {
