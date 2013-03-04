baa8579d47053ff84dc745bdd74be6de75d7ac75
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-04 17:46:51 +0000

    Add simple vector traits for JSC::Identifier. <http://webkit.org/b/111323>
    Reviewed by Geoffrey Garen.
    
    Identifiers are really just Strings, giving them simple vector traits makes
    Vector move them with memcpy() instead of churning the refcounts.
    
    * runtime/Identifier.h:
    (WTF):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144641 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/runtime/Identifier.h
+++ third_party/WebKit/Source/JavaScriptCore/runtime/Identifier.h
@@ -259,4 +259,10 @@ namespace JSC {
 
 } // namespace JSC
 
+namespace WTF {
+
+template <> struct VectorTraits<JSC::Identifier> : SimpleClassVectorTraits { };
+
+} // namespace WTF
+
 #endif // Identifier_h
