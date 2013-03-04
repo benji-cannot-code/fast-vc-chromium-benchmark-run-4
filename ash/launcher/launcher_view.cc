d6d54f840b306d107aee4457d78f1818f540d899
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 23:06:01 +0000

    Disabling the first menu item
    BUG=178341
    TEST=visual
    
    
    Review URL: https://chromiumcodereview.appspot.com/12387024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186016 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_view.cc
+++ ash/launcher/launcher_view.cc
@@ -103,6 +103,7 @@ class LauncherMenuModelAdapter
 
   // Overriding MenuModelAdapter's MenuDelegate implementation.
   virtual const gfx::Font* GetLabelFont(int command_id) const OVERRIDE;
+  virtual bool IsCommandEnabled(int id) const OVERRIDE;
   virtual void GetHorizontalIconMargins(int id,
                                         int icon_size,
                                         int* left_margin,
@@ -132,6 +133,10 @@ const gfx::Font* LauncherMenuModelAdapter::GetLabelFont(
 
   ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
   return &rb.GetFont(ui::ResourceBundle::BoldFont);
+}
+
+bool LauncherMenuModelAdapter::IsCommandEnabled(int id) const {
+  return id != kCommandIdOfMenuName;
 }
 
 bool LauncherMenuModelAdapter::GetBackgroundColor(
