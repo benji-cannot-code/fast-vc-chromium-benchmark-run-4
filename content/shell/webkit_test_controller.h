a7614e3237af9cbd556b9aeac67a3e7ee668a45a
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-04 12:15:33 +0000

    [content shell] move sending the test configuration to a method
    This makes it easier in an upcoming change to send the test configuration
    again when preparing an existing web contents for the next test.
    
    BUG=171128
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/12395006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@185874 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_controller.h
+++ content/shell/webkit_test_controller.h
@@ -123,6 +123,7 @@ class WebKitTestController : public base::NonThreadSafe,
 
   void TimeoutHandler();
   void DiscardMainWindow(WhetherToQuitMessageLoop quit_message_loop);
+  void SendTestConfiguration();
 
   // Message handlers.
   void OnAudioDump(const std::vector<unsigned char>& audio_dump);
