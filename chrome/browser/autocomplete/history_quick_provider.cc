117317162d326fa3221b97e510f189e1a1e2c776
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 00:54:34 +0000

    Ensure that HistoryQuickProvider, HistoryContentsProvider and HistoryURLProvider do not provide URLs corresponding to search queries, as these should be generated by other providers or, when instant-extended is enabled, by the search engine itself.
    BUG=164522
    
    
    Review URL: https://chromiumcodereview.appspot.com/11464033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186048 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_quick_provider.cc
+++ chrome/browser/autocomplete/history_quick_provider.cc
@@ -29,6 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/scored_history_match.h"
 #include "chrome/browser/net/url_fixer_upper.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search_engines/template_url.h"
+#include "chrome/browser/search_engines/template_url_service.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/ui/search/search.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
@@ -291,6 +295,12 @@ void HistoryQuickProvider::DoAutocomplete() {
   // visited URLs to beat out any longer URLs, no matter how frequently
   // they're visited.)  The strength of this last reduction depends on the
   // likely score for the URL-what-you-typed result.
+
+  // |template_url_service| or |template_url| can be NULL in unit tests.
+  TemplateURLService* template_url_service =
+      TemplateURLServiceFactory::GetForProfile(profile_);
+  TemplateURL* template_url = template_url_service ?
+      template_url_service->GetDefaultSearchProvider() : NULL;
   int max_match_score = (PreventInlineAutocomplete(autocomplete_input_) ||
       !matches.begin()->can_inline) ?
       (AutocompleteResult::kLowestDefaultScore - 1) :
@@ -302,11 +312,17 @@ void HistoryQuickProvider::DoAutocomplete() {
   for (ScoredHistoryMatches::const_iterator match_iter = matches.begin();
        match_iter != matches.end(); ++match_iter) {
     const ScoredHistoryMatch& history_match(*match_iter);
-    // Set max_match_score to the score we'll assign this result:
-    max_match_score = std::min(max_match_score, history_match.raw_score);
-    matches_.push_back(QuickMatchToACMatch(history_match, max_match_score));
-    // Mark this max_match_score as being used:
-    max_match_score--;
+    // Culls results corresponding to queries from the default search engine.
+    // These are low-quality, difficult-to-understand matches for users, and the
+    // SearchProvider should surface past queries in a better way anyway.
+    if (!template_url ||
+        !template_url->IsSearchURL(history_match.url_info.url())) {
+      // Set max_match_score to the score we'll assign this result:
+      max_match_score = std::min(max_match_score, history_match.raw_score);
+      matches_.push_back(QuickMatchToACMatch(history_match, max_match_score));
+      // Mark this max_match_score as being used:
+      max_match_score--;
+    }
   }
 }
 
--- chrome/browser/autocomplete/history_url_provider.h
+++ chrome/browser/autocomplete/history_url_provider.h
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/history_provider.h"
 #include "chrome/browser/autocomplete/history_provider_util.h"
 #include "chrome/browser/autocomplete/url_prefix.h"
+#include "chrome/browser/search_engines/search_terms_data.h"
+#include "chrome/browser/search_engines/template_url.h"
 
 class MessageLoop;
 class Profile;
@@ -87,7 +89,9 @@ class URLDatabase;
 struct HistoryURLProviderParams {
   HistoryURLProviderParams(const AutocompleteInput& input,
                            bool trim_http,
-                           const std::string& languages);
+                           const std::string& languages,
+                           TemplateURL* default_search_provider,
+                           const SearchTermsData& search_terms_data);
   ~HistoryURLProviderParams();
 
   MessageLoop* message_loop;
@@ -128,6 +132,16 @@ struct HistoryURLProviderParams {
 
   // When true, we should avoid calling SuggestExactInput().
   bool dont_suggest_exact_input;
+
+  // The default search provider and search terms data necessary to cull results
+  // that correspond to searches (on the default engine).  These can only be
+  // obtained on the UI thread, so we have to copy them into here to pass them
+  // to the history thread.  We use a scoped_ptr<TemplateURL> for the DSP since
+  // TemplateURLs can't be copied by value. We use a scoped_ptr<SearchTermsData>
+  // so that we can store a snapshot of the SearchTermsData accessible from the
+  // history thread.
+  scoped_ptr<TemplateURL> default_search_provider;
+  scoped_ptr<SearchTermsData> search_terms_data;
 
  private:
   DISALLOW_COPY_AND_ASSIGN(HistoryURLProviderParams);
@@ -257,8 +271,12 @@ class HistoryURLProvider : public HistoryProvider {
 
   // Removes results that have been rarely typed or visited, and not any time
   // recently.  The exact parameters for this heuristic can be found in the
-  // function body.
-  void CullPoorMatches(history::HistoryMatches* matches) const;
+  // function body. Also culls results corresponding to queries from the default
+  // search engine. These are low-quality, difficult-to-understand matches for
+  // users, and the SearchProvider should surface past queries in a better way
+  // anyway.
+  void CullPoorMatches(history::HistoryMatches* matches,
+                       HistoryURLProviderParams* params) const;
 
   // Removes results that redirect to each other, leaving at most |max_results|
   // results.
