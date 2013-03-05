2948b69f96d8d68fed86076f0ada6cc09145c5c7
jpfau@apple.com <jpfau@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 22:30:06 +0000

    Clear associated cache partitions when deleting origins' cache https://bugs.webkit.org/show_bug.cgi?id=111383
    Reviewed by Maciej Stachowiak.
    
    Source/WebCore:
    
    Clear the cache partitions associated with the origin being cleared.
    
    Not possible to test with current automated test tools, must be tested manually.
    
    * loader/cache/MemoryCache.cpp:
    (WebCore::MemoryCache::removeResourcesWithOrigin):
    
    Source/WebKit2:
    
    Clear the cache partitions associated with the origin being cleared.
    
    * WebProcess/ResourceCache/cf/WebResourceCacheManagerCFNet.cpp:
    (WebKit):
    (WebKit::partitionName):
    (WebKit::WebResourceCacheManager::clearCFURLCacheForHostNames):
    
    WebKitLibraries:
    
    * WebKitSystemInterface.h:
    * libWebKitSystemInterfaceLion.a:
    * libWebKitSystemInterfaceMountainLion.a:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/ResourceCache/cf/WebResourceCacheManagerCFNet.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/ResourceCache/cf/WebResourceCacheManagerCFNet.cpp
@@ -29,12 +29,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if USE(CFURLCACHE)
 
+#if ENABLE(CACHE_PARTITIONING)
+#include <WebCore/PublicSuffix.h>
+#endif
+
 #if PLATFORM(MAC)
 #include "WebKitSystemInterface.h"
 #endif
 
 
 namespace WebKit {
+
+#if ENABLE(CACHE_PARTITIONING)
+static RetainPtr<CFStringRef> partitionName(CFStringRef domain)
+{
+#if ENABLE(PUBLIC_SUFFIX_LIST)
+    String highLevel = WebCore::topPrivatelyControlledDomain(domain);
+    if (highLevel.isNull())
+        return 0;
+    return highLevel.createCFString();
+#else
+    return domain;
+#endif
+}
+#endif
 
 RetainPtr<CFArrayRef> WebResourceCacheManager::cfURLCacheHostNames()
 {
@@ -44,6 +62,15 @@ RetainPtr<CFArrayRef> WebResourceCacheManager::cfURLCacheHostNames()
 void WebResourceCacheManager::clearCFURLCacheForHostNames(CFArrayRef hostNames)
 {
     WKCFURLCacheDeleteHostNamesInPersistentStore(hostNames);
+
+#if ENABLE(CACHE_PARTITIONING)
+    CFIndex size = CFArrayGetCount(hostNames);
+    for (CFIndex i = 0; i < size; ++i) {
+        RetainPtr<CFStringRef> partition = partitionName(static_cast<CFStringRef>(CFArrayGetValueAtIndex(hostNames, i)));
+        RetainPtr<CFArrayRef> partitionHostNames(AdoptCF, WKCFURLCacheCopyAllHostNamesInPersistentStoreForPartition(partition.get()));
+        WKCFURLCacheDeleteHostNamesInPersistentStoreForPartition(partitionHostNames.get(), partition.get());
+    }
+#endif
 }
 
 } // namespace WebKit
