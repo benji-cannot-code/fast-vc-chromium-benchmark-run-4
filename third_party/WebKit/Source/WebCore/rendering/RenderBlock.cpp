09d139ea2dfec61be4014dfa92133917db5a7aff
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 02:46:44 +0000

    REGRESSION (r143643): Buttons containing floats render differently https://bugs.webkit.org/show_bug.cgi?id=110933
    Patch by Christian Biesinger <cbiesinger@chromium.org> on 2013-03-04
    Reviewed by Ojan Vafai.
    
    Source/WebCore:
    
    Test: fast/forms/button-with-float.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::expandsToEncloseOverhangingFloats):
    Also include new flexbox as a renderer that needs to enclose
    overhanging floats. Flex boxes establish a block formatting context,
    so they need to contain floats:
    http://dev.w3.org/csswg/css3-flexbox/#flex-items
    
    LayoutTests:
    
    * css3/flexbox/float-inside-flexitem-expected.html: Added.
    * css3/flexbox/float-inside-flexitem.html: Added.
    * fast/forms/button-with-float-expected.html: Added.
    * fast/forms/button-with-float.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144706 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -1798,7 +1798,7 @@ void RenderBlock::addVisualOverflowFromTheme()
 
 bool RenderBlock::expandsToEncloseOverhangingFloats() const
 {
-    return isInlineBlockOrInlineTable() || isFloatingOrOutOfFlowPositioned() || hasOverflowClip() || (parent() && parent()->isDeprecatedFlexibleBox())
+    return isInlineBlockOrInlineTable() || isFloatingOrOutOfFlowPositioned() || hasOverflowClip() || (parent() && parent()->isFlexibleBoxIncludingDeprecated())
            || hasColumns() || isTableCell() || isTableCaption() || isFieldset() || isWritingModeRoot() || isRoot();
 }
 
