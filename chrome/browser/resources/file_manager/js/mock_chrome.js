7db2e155ef65f2a8c299ce5724993babf0fcd64a
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 07:22:38 +0000

    File.app: Add mocks for Closure Compiler
    BUG=none
    TEST=none
    R=mtomasz@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12310061
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186118 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/mock_chrome.js
+++ chrome/browser/resources/file_manager/js/mock_chrome.js
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -46,6 +46,12 @@ function cloneShallow(object) {
       clone[key] = object[key];
   return clone;
 }
+
+
+/**
+ * Mock for |chrome| namespace.
+ */
+var chrome = window.chrome || {};
 
 /**
  * Mock out the chrome.fileBrowserPrivate API for use in the harness.
