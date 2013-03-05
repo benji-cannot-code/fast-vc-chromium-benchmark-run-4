cc9e7fead2ba6453a902418e1771095efd5de3c6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 08:31:43 +0000

    [V8] Remove unused custom indexedPropertyGetter https://bugs.webkit.org/show_bug.cgi?id=111409
    Patch by Koji Hara <kojih@chromium.org> on 2013-03-05
    Reviewed by Kentaro Hara.
    
    Remove unused custom indexedPropertyGetter.
    There are 9 custom indexedPropertyGetter, including an unused one(NamedNodeMap).
    This commit assures all existing custom indexedPropertyGetter(for V8) are used.
    
    No new tests. Simple refactoring.
    
    * bindings/v8/custom/V8NamedNodeMapCustom.cpp: Remove indexedPropertyGetter
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144738 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/custom/V8NamedNodeMapCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/custom/V8NamedNodeMapCustom.cpp
@@ -43,16 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8NamedNodeMap::indexedPropertyGetter(uint32_t index, const v8::AccessorInfo& info)
-{
-    NamedNodeMap* imp = V8NamedNodeMap::toNative(info.Holder());
-    RefPtr<Node> result = imp->item(index);
-    if (!result)
-        return v8Undefined();
-
-    return toV8Fast(result.release(), info, imp);
-}
-
 v8::Handle<v8::Value> V8NamedNodeMap::namedPropertyGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
 {
     if (!info.Holder()->GetRealNamedPropertyInPrototypeChain(name).IsEmpty())
