0482c38b56743f9743b292c42d740045a73913eb
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 06:03:18 +0000

    Omnibox: Disable Mid-Term Matching
    Remove the code for the field trial that was trying this and make
    the new behavior the default.
    
    Also, revise tests to test the new behavior.
    
    BUG=161911
    
    
    Review URL: https://chromiumcodereview.appspot.com/12316048
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/scored_history_match.cc
+++ chrome/browser/history/scored_history_match.cc
@@ -41,7 +41,6 @@ const int kScoreRank[] = { 1450, 1200, 900, 400 };
 
 bool ScoredHistoryMatch::initialized_ = false;
 bool ScoredHistoryMatch::use_new_scoring = false;
-bool ScoredHistoryMatch::only_count_matches_at_word_boundaries = false;
 bool ScoredHistoryMatch::also_do_hup_like_scoring = false;
 
 ScoredHistoryMatch::ScoredHistoryMatch()
@@ -49,7 +48,6 @@ ScoredHistoryMatch::ScoredHistoryMatch()
       can_inline(false) {
   if (!initialized_) {
     InitializeNewScoringField();
-    InitializeOnlyCountMatchesAtWordBoundariesField();
     InitializeAlsoDoHUPLikeScoringField();
     initialized_ = true;
   }
@@ -67,7 +65,6 @@ ScoredHistoryMatch::ScoredHistoryMatch(const URLRow& row,
       can_inline(false) {
   if (!initialized_) {
     InitializeNewScoringField();
-    InitializeOnlyCountMatchesAtWordBoundariesField();
     InitializeAlsoDoHUPLikeScoringField();
     initialized_ = true;
   }
@@ -286,15 +283,11 @@ int ScoredHistoryMatch::ScoreComponentForMatches(
   if (provided_matches.empty())
     return 0;
 
-  TermMatches matches_at_word_boundaries;
-  if (only_count_matches_at_word_boundaries) {
-    MakeTermMatchesOnlyAtWordBoundaries(provided_matches, word_starts,
-                                        &matches_at_word_boundaries);
-  }
   // The actual matches we'll use for matching.  This is |provided_matches|
-  // with all the matches not at a word boundary removed (if told to do so).
-  const TermMatches& matches = only_count_matches_at_word_boundaries ?
-      matches_at_word_boundaries : provided_matches;
+  // with all the matches not at a word boundary removed.
+  TermMatches matches;
+  MakeTermMatchesOnlyAtWordBoundaries(provided_matches, word_starts,
+                                      &matches);
 
   if (matches.empty())
     return 0;
@@ -722,14 +715,6 @@ void ScoredHistoryMatch::InitializeNewScoringField() {
   UMA_HISTOGRAM_ENUMERATION(
       "Omnibox.HistoryQuickProviderNewScoringFieldTrialBeacon",
       new_scoring_option, NUM_OPTIONS);
-}
-
-void ScoredHistoryMatch::InitializeOnlyCountMatchesAtWordBoundariesField() {
-  only_count_matches_at_word_boundaries =
-      AutocompleteFieldTrial::
-          InHQPOnlyCountMatchesAtWordBoundariesFieldTrial() &&
-      AutocompleteFieldTrial::
-          InHQPOnlyCountMatchesAtWordBoundariesFieldTrialExperimentGroup();
 }
 
 void ScoredHistoryMatch::InitializeAlsoDoHUPLikeScoringField() {
