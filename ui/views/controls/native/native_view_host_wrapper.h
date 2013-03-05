9ac5be488937ceed3c9c5e0591ec6c9d64db1b8c
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 20:56:42 +0000

    Add VIEWS_EXPORT to NativeViewHostWrapper for r186238 to fix win_aura compile.
    BUG=177012
    TEST=win_aura bot.
    TBR=sky
    
    Review URL: https://codereview.chromium.org/12435003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186255 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/native/native_view_host_wrapper.h
+++ ui/views/controls/native/native_view_host_wrapper.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_VIEWS_CONTROLS_NATIVE_NATIVE_VIEW_HOST_WRAPPER_H_
 
 #include "ui/gfx/native_widget_types.h"
+#include "ui/views/views_export.h"
 
 namespace views {
 
@@ -15,7 +16,7 @@ class NativeViewHost;
 // An interface that implemented by an object that wraps a gfx::NativeView on
 // a specific platform, used to perform platform specific operations on that
 // native view when attached, detached, moved and sized.
-class NativeViewHostWrapper {
+class VIEWS_EXPORT NativeViewHostWrapper {
  public:
   virtual ~NativeViewHostWrapper() {}
 
