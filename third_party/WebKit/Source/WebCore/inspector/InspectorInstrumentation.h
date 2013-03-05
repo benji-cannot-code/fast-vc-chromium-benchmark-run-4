e8b8cc0a1b221b7e93b1fb6aed46d7ae3f20a196
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 16:06:19 +0000

    Unreviewed follow-up to r144756, fix broken inspector-less build.
    * inspector/InspectorInstrumentation.h:
    (WebCore::InspectorInstrumentation::willWriteHTML):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144761 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorInstrumentation.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorInstrumentation.h
@@ -1746,7 +1746,6 @@ inline InspectorInstrumentationCookie InspectorInstrumentation::willWriteHTML(Do
         return willWriteHTMLImpl(instrumentingAgents, startLine, document->frame());
 #else
     UNUSED_PARAM(document);
-    UNUSED_PARAM(length);
     UNUSED_PARAM(startLine);
 #endif
     return InspectorInstrumentationCookie();
