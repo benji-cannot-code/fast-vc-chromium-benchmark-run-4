206be74feba3126d99671d701e2ffd68ed37ca68
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 19:40:40 +0000

    Add a shortcut to open the Apps page from the bookmark bar.
    Initial patch by mad@chromium: https://codereview.chromium.org/12310109/
    
    BUG=177377
    TEST=Make sure that an app shortcut is available in the bookmark bar when instant extended is enabled, and that this button can be hidden/shown from the bookmark bar context menu.
    
    Review URL: https://chromiumcodereview.appspot.com/12386088
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_context_menu_controller_views.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_context_menu_controller_views.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/bookmarks/bookmark_utils.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/chrome_pages.h"
+#include "chrome/browser/ui/search/search.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/page_navigator.h"
@@ -146,6 +147,14 @@ void BookmarkContextMenuControllerViews::ExecuteCommand(int id) {
       chrome::ToggleBookmarkBarWhenVisible(profile_);
       break;
 
+    case IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT: {
+      PrefService* prefs = profile_->GetPrefs();
+      prefs->SetBoolean(
+          prefs::kShowAppsShortcutInBookmarkBar,
+          !prefs->GetBoolean(prefs::kShowAppsShortcutInBookmarkBar));
+      break;
+    }
+
     case IDC_BOOKMARK_MANAGER: {
       content::RecordAction(UserMetricsAction("ShowBookmarkManager"));
       if (selection_.size() != 1)
@@ -186,8 +195,14 @@ void BookmarkContextMenuControllerViews::ExecuteCommand(int id) {
 }
 
 bool BookmarkContextMenuControllerViews::IsItemChecked(int id) const {
-  DCHECK_EQ(IDC_BOOKMARK_BAR_ALWAYS_SHOW, id);
-  return profile_->GetPrefs()->GetBoolean(prefs::kShowBookmarkBar);
+  if (id == IDC_BOOKMARK_BAR_ALWAYS_SHOW)
+    return profile_->GetPrefs()->GetBoolean(prefs::kShowBookmarkBar);
+
+  // This should only be available when instant extended is enabled.
+  DCHECK(chrome::search::IsInstantExtendedAPIEnabled(profile_));
+  DCHECK_EQ(IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT, id);
+  return profile_->GetPrefs()->GetBoolean(
+      prefs::kShowAppsShortcutInBookmarkBar);
 }
 
 bool BookmarkContextMenuControllerViews::IsCommandEnabled(int id) const {
@@ -229,6 +244,10 @@ bool BookmarkContextMenuControllerViews::IsCommandEnabled(int id) const {
     case IDC_BOOKMARK_BAR_ALWAYS_SHOW:
       return !profile_->GetPrefs()->IsManagedPreference(
           prefs::kShowBookmarkBar);
+
+    case IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT:
+      return !profile_->GetPrefs()->IsManagedPreference(
+          prefs::kShowAppsShortcutInBookmarkBar);
 
     case IDC_COPY:
     case IDC_CUT:
@@ -288,6 +307,10 @@ void BookmarkContextMenuControllerViews::BuildMenu() {
 
   delegate_->AddSeparator();
   delegate_->AddItemWithStringId(IDC_BOOKMARK_MANAGER, IDS_BOOKMARK_MANAGER);
+  if (chrome::search::IsInstantExtendedAPIEnabled(profile_)) {
+    delegate_->AddCheckboxItem(IDC_BOOKMARK_BAR_SHOW_APPS_SHORTCUT,
+                               IDS_BOOKMARK_BAR_SHOW_APPS_SHORTCUT);
+  }
   delegate_->AddCheckboxItem(IDC_BOOKMARK_BAR_ALWAYS_SHOW,
                              IDS_SHOW_BOOKMARK_BAR);
 }
