7549c3d173403f5c192e6b1bc67887abce632c62
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 01:23:52 +0000

    [chromedriver] Print a new line after python tests.
    This is needed for buildbot annotation to work.
    
    TBR=craigdh@chromium.org
    
    BUG=
    
    Review URL: https://codereview.chromium.org/12398035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186051 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/run_all_tests.py
+++ chrome/test/chromedriver/run_all_tests.py
@@ -54,7 +54,7 @@ def RunPythonTests(chromedriver, chrome=None, android_package=None):
     os.environ['VERSIONER_PYTHON_PREFER_32_BIT'] = 'yes'
   code = util.RunCommand(cmd)
   if code:
-    print '@@@STEP_FAILURE@@@'
+    print '\n@@@STEP_FAILURE@@@'
   return code
 
 
