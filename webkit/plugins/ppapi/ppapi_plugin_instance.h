eed676ed7b5098fc7f316f2b0578521e7f33df94
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-05 20:39:40 +0000

    Pepper cleanup: USE_SKIA is always true.
    We always build with skia now. This change removes dead code, and unnecessary
    USE_SKIA guards.
    
    BUG=110881
    
    
    Review URL: https://chromiumcodereview.appspot.com/12437007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186249 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppapi_plugin_instance.h
+++ webkit/plugins/ppapi/ppapi_plugin_instance.h
@@ -671,7 +671,6 @@ class WEBKIT_PLUGINS_EXPORT PluginInstance :
   // variable to hold on to the pixels.
   scoped_refptr<PPB_ImageData_Impl> last_printed_page_;
 #endif  // defined(OS_MACOSX)
-#if defined(USE_SKIA)
   // Always when printing to PDF on Linux and when printing for preview on Mac
   // and Win, the entire document goes into one metafile.  However, when users
   // print only a subset of all the pages, it is impossible to know if a call
@@ -684,7 +683,6 @@ class WEBKIT_PLUGINS_EXPORT PluginInstance :
   SkRefPtr<WebKit::WebCanvas> canvas_;
   // An array of page ranges.
   std::vector<PP_PrintPageNumberRange_Dev> ranges_;
-#endif  // OS_LINUX || OS_WIN
 
   scoped_refptr< ::ppapi::Resource> gamepad_impl_;
 
