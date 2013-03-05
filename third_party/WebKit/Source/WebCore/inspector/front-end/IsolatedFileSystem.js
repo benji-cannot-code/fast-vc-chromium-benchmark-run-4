342a4d900e55d8123fb4a77a64ad468aadddb321
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 07:31:56 +0000

    Web Inspector: Do not accumulate file system files, add them to workspace immediately https://bugs.webkit.org/show_bug.cgi?id=111325
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    * inspector/front-end/FileSystemProjectDelegate.js:
    (WebInspector.FileSystemProjectDelegate.prototype.populate.fileLoaded):
    (WebInspector.FileSystemProjectDelegate.prototype.populate):
    * inspector/front-end/IsolatedFileSystem.js:
    (WebInspector.IsolatedFileSystem.prototype.innerCallback):
    (WebInspector.IsolatedFileSystem.prototype.requestFilesRecursive):
    
    LayoutTests:
    
    * inspector/file-system-project.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/IsolatedFileSystem.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/IsolatedFileSystem.js
@@ -114,7 +114,7 @@ WebInspector.IsolatedFileSystem.prototype = {
 
     /**
      * @param {string} path
-     * @param {function(Array.<string>)} callback
+     * @param {function(string)} callback
      */
     requestFilesRecursive: function(path, callback)
     {
@@ -130,8 +130,6 @@ WebInspector.IsolatedFileSystem.prototype = {
             this._requestEntries(domFileSystem, path, innerCallback.bind(this));
         }
 
-        var result = [];
-        var callbacksLeft = 1;
         /**
          * @param {Array.<FileEntry>} entries
          */
@@ -140,14 +138,10 @@ WebInspector.IsolatedFileSystem.prototype = {
             for (var i = 0; i < entries.length; ++i) {
                 var entry = entries[i];
                 if (!entry.isDirectory)
-                    result.push(entry.fullPath);
-                else {
-                    callbacksLeft++;
+                    callback(entry.fullPath);
+                else
                     this._requestEntries(domFileSystem, entry.fullPath, innerCallback.bind(this));
-                }
             }
-            if (!--callbacksLeft)
-                callback(result);
         }
     },
 
