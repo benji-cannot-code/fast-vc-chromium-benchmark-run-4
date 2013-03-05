389b1346e6465ce0fb175d3c433bea285eabefa8
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 23:59:52 +0000

    ASSERTION FAILED: i < size(), UNKNOWN in WebCore::RenderTableSection::layout https://bugs.webkit.org/show_bug.cgi?id=108838
    Source/WebCore: 
    
    Reviewed by Levi Weintraub.
            
    Ensure that RenderTable::recalcSections is called when moving a
    TableSection by calling setNeedsSectionRecalc in addChild for
    TableSections.
    
    Test: fullscreen/full-screen-table-section.html
    
    * rendering/RenderTable.cpp:
    (WebCore::RenderTable::addChild):
    
    LayoutTests: 
    
    Reviewed by Levi Weintraub.
            
    Add test for calling requestFullscreen on an element with a
    table-section display type.
    
    * fullscreen/full-screen-table-section-expected.txt: Added.
    * fullscreen/full-screen-table-section.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144837 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTable.cpp
@@ -155,6 +155,9 @@ void RenderTable::addChild(RenderObject* child, RenderObject* beforeChild)
     else
         wrapInAnonymousSection = true;
 
+    if (child->isTableSection())
+        setNeedsSectionRecalc();
+
     if (!wrapInAnonymousSection) {
         if (beforeChild && beforeChild->parent() != this)
             beforeChild = splitAnonymousBoxesAroundChild(beforeChild);
