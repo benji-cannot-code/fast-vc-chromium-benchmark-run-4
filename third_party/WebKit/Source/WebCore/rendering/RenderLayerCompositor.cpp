1a53f226b1bd3b7dedc87736358e790749fd1ba9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-05 03:23:35 +0000

    Fix some style issues in RenderLayerCompositor https://bugs.webkit.org/show_bug.cgi?id=111369
    Patch by Min Qin <qinmin@chromium.org> on 2013-03-04
    Reviewed by Simon Fraser.
    
    Just style fix, no test needed
    
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::frameViewDidScroll):
    (WebCore::RenderLayerCompositor::requiresCompositingForPosition):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderLayerCompositor.cpp
@@ -1221,9 +1221,10 @@ void RenderLayerCompositor::frameViewDidScroll()
     if (ScrollingCoordinator* scrollingCoordinator = this->scrollingCoordinator()) {
         if (scrollingCoordinator->coordinatesScrollingForFrameView(frameView))
             return;
-        if (Settings* settings = m_renderView->document()->settings())
+        if (Settings* settings = m_renderView->document()->settings()) {
             if (settings->compositedScrollingForFramesEnabled())
                 scrollingCoordinator->scrollableAreaScrollLayerDidChange(frameView);
+        }
     }
 
     m_scrollLayer->setPosition(FloatPoint(-scrollPosition.x(), -scrollPosition.y()));
@@ -2149,9 +2150,10 @@ bool RenderLayerCompositor::requiresCompositingForPosition(RenderObject* rendere
         return false;
 
     // FIXME: acceleratedCompositingForFixedPositionEnabled should probably be renamed acceleratedCompositingForViewportConstrainedPositionEnabled().
-    if (Settings* settings = m_renderView->document()->settings())
+    if (Settings* settings = m_renderView->document()->settings()) {
         if (!settings->acceleratedCompositingForFixedPositionEnabled())
             return false;
+    }
 
     if (isSticky)
         return true;
