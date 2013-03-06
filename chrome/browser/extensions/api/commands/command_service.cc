ae5f89e268caf3595a89f838f3277109d56cb06e
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 12:54:56 +0000

    Replacing DictionaryValue::key_iterator by DictionaryValue::Iterator in chrome/*/extensions/.
    This is one of several CLs of a larger refactoring that prepares for the removal of key_iterator. 
    
    BUG=162611
    
    
    Review URL: https://chromiumcodereview.appspot.com/12328082
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186409 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/commands/command_service.cc
+++ chrome/browser/extensions/api/commands/command_service.cc
@@ -202,10 +202,9 @@ ui::Accelerator CommandService::FindShortcutForCommand(
     const std::string& extension_id, const std::string& command) {
   const DictionaryValue* bindings =
       profile_->GetPrefs()->GetDictionary(prefs::kExtensionCommands);
-  for (DictionaryValue::key_iterator it = bindings->begin_keys();
-       it != bindings->end_keys(); ++it) {
+  for (DictionaryValue::Iterator it(*bindings); !it.IsAtEnd(); it.Advance()) {
     const DictionaryValue* item = NULL;
-    bindings->GetDictionary(*it, &item);
+    it.value().GetAsDictionary(&item);
 
     std::string extension;
     item->GetString(kExtension, &extension);
@@ -216,7 +215,7 @@ ui::Accelerator CommandService::FindShortcutForCommand(
     if (command != command_name)
       continue;
 
-    std::string shortcut = *it;
+    std::string shortcut = it.key();
     if (StartsWithASCII(shortcut, Command::CommandPlatform() + ":", true))
       shortcut = shortcut.substr(Command::CommandPlatform().length() + 1);
 
@@ -276,11 +275,9 @@ void CommandService::RemoveKeybindingPrefs(const std::string& extension_id,
 
   typedef std::vector<std::string> KeysToRemove;
   KeysToRemove keys_to_remove;
-  for (DictionaryValue::key_iterator it = bindings->begin_keys();
-       it != bindings->end_keys(); ++it) {
-    std::string key = *it;
-    DictionaryValue* item = NULL;
-    bindings->GetDictionary(key, &item);
+  for (DictionaryValue::Iterator it(*bindings); !it.IsAtEnd(); it.Advance()) {
+    const DictionaryValue* item = NULL;
+    it.value().GetAsDictionary(&item);
 
     std::string extension;
     item->GetString(kExtension, &extension);
@@ -295,7 +292,7 @@ void CommandService::RemoveKeybindingPrefs(const std::string& extension_id,
           continue;
       }
 
-      keys_to_remove.push_back(key);
+      keys_to_remove.push_back(it.key());
     }
   }
 
