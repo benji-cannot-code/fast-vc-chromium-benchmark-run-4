ba0d01e720c86cadff8a24decf464b62c2d09008
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-06 15:18:54 +0000

    Web Inspector: support stable remote object ids https://bugs.webkit.org/show_bug.cgi?id=109908
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Introduced a map from javascript object to its id. It guarantees
    that object is assined id only once (unless it is explicitely
    released). The map is per execution context and is stored in
    InjectedScriptManager.
    
    Test: inspector-protocol/persistent-id.html
    
    * bindings/js/JSInjectedScriptHostCustom.cpp:
    (WebCore::JSInjectedScriptHost::objectId):
    (WebCore):
    (WebCore::JSInjectedScriptHost::releaseObjectId):
    * bindings/js/ScriptObject.h:
    (WTF):
    (WTF::ScriptObjectHash::hash):
    (WTF::ScriptObjectHash::equal):
    (ScriptObjectHash):
    * bindings/v8/ScriptObject.h:
    (WTF):
    (WTF::ScriptObjectHash::hash):
    (WTF::ScriptObjectHash::equal):
    (ScriptObjectHash):
    * bindings/v8/custom/V8InjectedScriptHostCustom.cpp:
    (WebCore::V8InjectedScriptHost::objectIdMethodCustom):
    (WebCore):
    (WebCore::V8InjectedScriptHost::releaseObjectIdMethodCustom):
    * inspector/InjectedScriptHost.cpp:
    (WebCore::InjectedScriptHost::create):
    (WebCore::InjectedScriptHost::InjectedScriptHost):
    (WebCore::InjectedScriptHost::disconnect):
    (WebCore::InjectedScriptHost::objectId):
    (WebCore):
    (WebCore::InjectedScriptHost::releaseObjectId):
    * inspector/InjectedScriptHost.h:
    (WebCore):
    (InjectedScriptHost):
    * inspector/InjectedScriptHost.idl:
    * inspector/InjectedScriptManager.cpp:
    (InjectedScriptManager::ObjectIdMap):
    (WebCore::InjectedScriptManager::ObjectIdMap::ObjectIdMap):
    (WebCore::InjectedScriptManager::ObjectIdMap::objectId):
    (WebCore::InjectedScriptManager::ObjectIdMap::releaseObjectId):
    (WebCore):
    (WebCore::InjectedScriptManager::InjectedScriptManager):
    (WebCore::InjectedScriptManager::discardInjectedScripts):
    (WebCore::InjectedScriptManager::discardInjectedScriptsFor):
    (WebCore::InjectedScriptManager::objectId):
    (WebCore::InjectedScriptManager::releaseObjectId):
    * inspector/InjectedScriptManager.h:
    (InjectedScriptManager):
    * inspector/InjectedScriptSource.js:
    (.):
    
    LayoutTests:
    
    Test that same object will have same id if requested several times.
    
    * inspector-protocol/persistent-id-expected.txt: Added.
    * inspector-protocol/persistent-id.html: Added.
    * inspector/console/command-line-api-expected.txt:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144924 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InjectedScriptHost.h
+++ third_party/WebKit/Source/WebCore/inspector/InjectedScriptHost.h
@@ -41,6 +41,7 @@ namespace WebCore {
 
 class Database;
 class InjectedScript;
+class InjectedScriptManager;
 class InspectorAgent;
 class InspectorConsoleAgent;
 class InspectorDOMAgent;
@@ -60,7 +61,7 @@ struct EventListenerInfo;
 
 class InjectedScriptHost : public RefCounted<InjectedScriptHost> {
 public:
-    static PassRefPtr<InjectedScriptHost> create();
+    static PassRefPtr<InjectedScriptHost> create(InjectedScriptManager*);
     ~InjectedScriptHost();
 
     void init(InspectorAgent* inspectorAgent
@@ -107,6 +108,8 @@ public:
 
     void clearConsoleMessages();
     void copyText(const String& text);
+    unsigned objectId(const ScriptObject&);
+    unsigned releaseObjectId(const ScriptObject&);
 #if ENABLE(SQL_DATABASE)
     String databaseIdImpl(Database*);
 #endif
@@ -117,8 +120,9 @@ public:
 #endif
 
 private:
-    InjectedScriptHost();
+    explicit InjectedScriptHost(InjectedScriptManager*);
 
+    InjectedScriptManager* m_manager;
     InspectorAgent* m_inspectorAgent;
     InspectorConsoleAgent* m_consoleAgent;
 #if ENABLE(SQL_DATABASE)
