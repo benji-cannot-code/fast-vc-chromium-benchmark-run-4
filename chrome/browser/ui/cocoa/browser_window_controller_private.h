844e15794fbcee9cc30bc69c4417fced1abd47ee
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 04:15:24 +0000

    Alternate NTP Mac: Update look of detached bookmark bar
    This is the Mac version of this views CL: https://chromiumcodereview.appspot.com/12383033
    
    Mock: http://i.imgur.com/pI1mfEH.png
    Screenshot: http://i.imgur.com/aRy3sAT.png
    Screenshot with theme: http://i.imgur.com/eRTL3I4.png
    
    BUG=179163
    TEST=Ran chrome with --enable-instant-extended-api. Verified that the detached bookmark bar match the spec.
    
    Review URL: https://chromiumcodereview.appspot.com/12387068
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186354 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller_private.h
+++ chrome/browser/ui/cocoa/browser_window_controller_private.h
@@ -65,12 +65,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // call it with the appropriate |maxY| which depends on whether or not the
 // bookmark bar is shown as the NTP bubble or not (use
 // |-placeBookmarkBarBelowInfoBar|).
-- (CGFloat)layoutTopBookmarkBarAtMinX:(CGFloat)minX
-                                 maxY:(CGFloat)maxY
-                                width:(CGFloat)width;
-
-// Lays out the bookmark at the bottom of the content area.
-- (void)layoutBottomBookmarkBarInContentFrame:(NSRect)contentFrame;
+- (CGFloat)layoutBookmarkBarAtMinX:(CGFloat)minX
+                              maxY:(CGFloat)maxY
+                             width:(CGFloat)width;
 
 // Lay out the view which draws the background for the floating bar when in
 // presentation mode, with the given frame and presentation-mode-status. Should
