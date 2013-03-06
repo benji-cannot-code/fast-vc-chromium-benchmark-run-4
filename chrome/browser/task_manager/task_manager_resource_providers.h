cf2284b5e2d9d7c0c67c90eba2731d8e091cfef6
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 21:03:48 +0000

    Diagnostics to find how a RenderViewHost is being unexpectedly deleted.
    BUG=165138
    
    
    Review URL: https://chromiumcodereview.appspot.com/12486005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/task_manager/task_manager_resource_providers.h
+++ chrome/browser/task_manager/task_manager_resource_providers.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/child_process_data.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
+#include "content/public/browser/render_view_host_observer.h"
 #include "content/public/common/process_type.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebCache.h"
 
@@ -38,7 +39,8 @@ class Extension;
 
 // Base class for various types of render process resources that provides common
 // functionality like stats tracking.
-class TaskManagerRendererResource : public TaskManager::Resource {
+class TaskManagerRendererResource : public TaskManager::Resource,
+                                    public content::RenderViewHostObserver {
  public:
   TaskManagerRendererResource(base::ProcessHandle process,
                               content::RenderViewHost* render_view_host);
@@ -76,6 +78,10 @@ class TaskManagerRendererResource : public TaskManager::Resource {
 
   virtual void NotifyV8HeapStats(size_t v8_memory_allocated,
                                  size_t v8_memory_used) OVERRIDE;
+
+  // content::RenderViewHostObserver implementation.
+  virtual void RenderViewHostDestroyed(
+      content::RenderViewHost* render_view_host) OVERRIDE;
 
   content::RenderViewHost* render_view_host() const {
     return render_view_host_;
