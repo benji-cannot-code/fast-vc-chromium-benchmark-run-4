b97924dc67214e0b95ab346c87d004561aa352d3
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 09:01:20 +0000

    Align physical pixels of scrolled layers
    Non-integral position of scrolled layers causes blurry content.
    
    If a layer has simple transform and the translations in the transform are
    merely caused by scroll offsets, round the translations to align the pixels.
    
    BUG=174699
    
    
    Review URL: https://chromiumcodereview.appspot.com/12407002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186377 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/math_util.h
+++ cc/math_util.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_MATH_UTIL_H_
 #define CC_MATH_UTIL_H_
 
+#include <cmath>
+
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "cc/cc_export.h"
@@ -81,6 +83,9 @@ public:
 
     static float Deg2Rad(float deg)  { return deg * PI_FLOAT / 180; }
     static float Rad2Deg(float rad)  { return rad * 180 / PI_FLOAT; }
+
+    static float Round(float f)  { return (f > 0.f) ? std::floor(f + 0.5f) : std::ceil(f - 0.5f); }
+    static double Round(double d)  { return (d > 0.0) ? std::floor(d + 0.5) : std::ceil(d - 0.5); }
 
     // Background: Existing transform code does not do the right thing in
     // mapRect / mapQuad / projectQuad when there is a perspective projection that causes
