6784692c993fe3a2ef0520cafdee2c1c1f24dcab
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 03:41:50 +0000

    Fix the problem that the taskbar icon is not flashed sometimes when drawing attention for a stacked panel
    The fix is not to call FlashFrame second time if the flashing is not cleared. In addition, I changed to listen to the focus change event for the stack window in order to activate the most recently active panel when the user selects the stack via ALT-TAB.
    
    BUG=180072
    TEST=Manual test
    
    
    Review URL: https://chromiumcodereview.appspot.com/12378091
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186341 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/panels/panel_stack_view.h
+++ chrome/browser/ui/views/panels/panel_stack_view.h
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/memory/weak_ptr.h"
 #include "chrome/browser/ui/panels/native_panel_stack.h"
+#include "ui/views/focus/widget_focus_manager.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/widget/widget_observer.h"
 
@@ -20,7 +20,8 @@ class TaskbarWindowThumbnailerWin;
 // to make all panels appear as a single window on the taskbar or launcher.
 class PanelStackView : public NativePanelStack,
                        public views::WidgetObserver,
-                       public views::WidgetDelegateView {
+                       public views::WidgetDelegateView,
+                       public views::WidgetFocusChangeListener {
  public:
   explicit PanelStackView(
       scoped_ptr<StackedPanelCollection> stacked_collection);
@@ -48,6 +49,10 @@ class PanelStackView : public NativePanelStack,
   virtual void OnWidgetActivationChanged(views::Widget* widget,
                                          bool active) OVERRIDE;
 
+  // Overridden from views::WidgetFocusChangeListener:
+  virtual void OnNativeFocusChange(gfx::NativeView focused_before,
+                                   gfx::NativeView focused_now) OVERRIDE;
+
   void EnsureInitialized();
 
   // Updates the owner of the underlying window such that multiple panels
@@ -55,18 +60,13 @@ class PanelStackView : public NativePanelStack,
   // launcher.
   void UpdateWindowOwnerForTaskbarIconAppearance(Panel* panel);
 
-#if defined(OS_WIN)
-  void ActivateMostRecentlyActivePanel();
-#endif
-
   scoped_ptr<StackedPanelCollection> stacked_collection_;
 
   bool delay_initialized_;
 
-#if defined(OS_WIN)
-  // Owned by MessageLoop after posting.
-  base::WeakPtrFactory<PanelStackView> weak_factory_;
-#endif
+  // Is the taskbar icon of the underlying window being flashed in order to
+  // draw the user's attention?
+  bool is_drawing_attention_;
 
   views::Widget* window_;
 
