b598d3080f61480467934a1562a9ba531c08e5b6
james.wei@intel.com <james.wei@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 09:43:19 +0000

    Catch NullPointerException thrown by Java NetworkInterface
        This issue related with an Android bug:
        https://code.google.com/p/android/issues/detail?id=33661
    
        In some rare cases, NetworkInterface will throw
        NullPointerException, which should be caught.
    
    BUG=180041
    
    
    Review URL: https://chromiumcodereview.appspot.com/12433002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186397 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/java/src/org/chromium/net/AndroidNetworkLibrary.java
+++ net/android/java/src/org/chromium/net/AndroidNetworkLibrary.java
@@ -124,7 +124,7 @@ class AndroidNetworkLibrary {
         try {
             list = NetworkInterface.getNetworkInterfaces();
             if (list == null) return false;
-        } catch (SocketException e) {
+        } catch (Exception e) {
             Log.w(TAG, "could not get network interfaces: " + e);
             return false;
         }
