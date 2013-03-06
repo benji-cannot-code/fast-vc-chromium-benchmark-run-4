670a5baff06c1b7c63309b343929db0e1f8abe6c
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-06 18:46:20 +0000

    [chromium] Remove the contents of WebVideoFrame class https://bugs.webkit.org/show_bug.cgi?id=106037
    Reviewed by James Robinson.
    
    This class becomes an empty base class now, used only for the API
    boundary.
    
    * chromium/public/WebVideoFrame.h:
    (WebKit):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144946 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/WebVideoFrame.h
+++ third_party/WebKit/Source/Platform/chromium/public/WebVideoFrame.h
@@ -37,44 +37,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-// A proxy video frame interface to communicate frame data between chromium
-// and WebKit.
-// Keep in sync with chromium's media::VideoFrame::Format.
+// A base class for the container which holds a media::VideoFrame.
 class WebVideoFrame {
 public:
-    enum {
-        rgbPlane = 0,
-        numRGBPlanes = 1
-    };
-    enum {
-        yPlane = 0,
-        uPlane = 1,
-        vPlane = 2,
-        numYUVPlanes = 3
-    };
-    enum { maxPlanes = 3 };
-
-    enum Format {
-        FormatInvalid = 0,
-        FormatRGB32 = 4,
-        FormatYV12 = 6,
-        FormatYV16 = 7,
-        FormatEmpty = 9,
-        FormatI420 = 11,
-        FormatNativeTexture = 12,
-    };
-
     virtual ~WebVideoFrame() { }
-    virtual Format format() const { return FormatInvalid; }
-    virtual unsigned width() const { return 0; }
-    virtual unsigned height() const { return 0; }
-    virtual unsigned planes() const { return 0; }
-    virtual int stride(unsigned plane) const { return 0; }
-    virtual const void* data(unsigned plane) const { return 0; }
-    virtual unsigned textureId() const { return 0; }
-    virtual unsigned textureTarget() const { return 0; }
-    virtual WebKit::WebRect visibleRect() const { return WebKit::WebRect(); }
-    virtual WebKit::WebSize textureSize() const { return WebKit::WebSize(); }
 };
 
 } // namespace WebKit
