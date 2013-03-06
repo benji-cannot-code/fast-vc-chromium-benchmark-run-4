8965e407c65684b5ee02ac9870c73f8d1b35228c
yosin@chromium.org <yosin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 01:08:51 +0000

    Bookmark manager: Handle "copy", "cut"and "paste" events only when list or tree pane active.
    This caused by a change of getSelectedBookmarkNodes() in r172230. Before r177230, getSelectedBookmarkNodes() threw error unless list or tree is active. From r177230, getSelectedBookmarkNodes() returns selected items of list when list nor tree is active.
    
    Note: For a11y, when context menu or organize menu is open, menu takes focus. So, list/tree is no more active element when menu is opened.
    
    This causes "cut" event handler of document object does "cut" operation on an active item of list. In bug case, an active item is editing bookmark.
    
    This patch changes to check active element in "copy", "cut", and "paste" event handler.
    
    BUG=174613
    TEST=On bookmark manager, edit/rename bookmark/folder and hit Ctrl+X in edit field.
    
    Review URL: https://codereview.chromium.org/12387036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186312 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/bookmark_manager/js/main.js
+++ chrome/browser/resources/bookmark_manager/js/main.js
@@ -1698,6 +1698,8 @@ function handleCommand(e) {
 // shortcuts for these commands.
 function installEventHandlerForCommand(eventName, commandId) {
   function handle(e) {
+    if (document.activeElement != list || document.activeElement != tree)
+      return;
     var command = $(commandId);
     if (!command.disabled) {
       command.execute();
