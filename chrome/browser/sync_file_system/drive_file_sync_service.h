e90c38647ba8a0a182aba78cbe197a0e2c4c3f7a
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 02:03:30 +0000

    New DriveFileSyncService test naming convention 1. drive_file_sync_service_mock_unittests.cc which will be a rename of the original test file drive_file_sync_service_unittests.cc. The idea is to eventually move away from gMock used in these tests and to use the fake* instead. 2. drive_file_sync_service_sync_unittests.cc will contain the composite tests tzik has created using his new framework. 3. drive_file_sync_service_unittests.cc will be the new unittest file that uses FakeDriveFileSyncClient.
    BUG=
    
    Review URL: https://codereview.chromium.org/12451002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186329 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_file_sync_service.h
+++ chrome/browser/sync_file_system/drive_file_sync_service.h
@@ -111,7 +111,7 @@ class DriveFileSyncService
       const syncer::ObjectIdInvalidationMap& invalidation_map) OVERRIDE;
 
  private:
-  friend class DriveFileSyncServiceTest;
+  friend class DriveFileSyncServiceMockTest;
   friend class DriveFileSyncServiceSyncTest;
   class TaskToken;
   struct ProcessRemoteChangeParam;
