f20671e045ae4c99c2b5bdca27d582a240973bdd
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-06 01:12:21 +0000

    Source/WebCore: [WinCairo] Support for cookies is incomplete https://bugs.webkit.org/show_bug.cgi?id=110147
    Expired and HttpOnly cookies no longer accessible from JavaScript.
    Cookies set in JavaScript now have correct domain/path.
    
    Patch by Peter Nelson <peter@peterdn.com> on 2013-03-05
    Reviewed by Brent Fulgham.
    
    Test: http/tests/cookies/http-get-cookie-set-in-js.html
    
    * platform/network/curl/CookieJarCurl.cpp:
    (WebCore):
    (WebCore::addMatchingCurlCookie):
    (WebCore::getNetscapeCookieFormat):
    (WebCore::setCookiesFromDOM):
    (WebCore::cookieRequestHeaderFieldValue):
    
    LayoutTests: [WinCairo] Support for cookies is incomplete
    https://bugs.webkit.org/show_bug.cgi?id=110147
    
    Patch by Peter Nelson <peter@peterdn.com> on 2013-03-05
    Reviewed by Brent Fulgham.
    
    Re-enabled cookie tests for WinCairo.
    Added test to check whether cookie set in HTTP response is accessible in JS.
    
    * http/tests/cookies/http-get-cookie-set-in-js-expected.txt: Added.
    * http/tests/cookies/http-get-cookie-set-in-js.html: Added.
    * http/tests/cookies/resources/cookies-test-pre.js:
    (clearAllCookies): Cookies set in JS now correctly cleared.
    * platform/wincairo/TestExpectations:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144853 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/cookies/resources/cookies-test-pre.js
+++ third_party/WebKit/LayoutTests/http/tests/cookies/resources/cookies-test-pre.js
@@ -205,6 +205,10 @@ function clearAllCookies()
         var cookieName = cookieString.substr(0, cookieString.indexOf("=") || cookieString.length());
         cookies.push(cookieName);
         clearCookies();
+
+        // In case clearCookies.cgi failed, for example,
+        // the domain/path do not match exactly:
+        document.cookie = cookieName + "=;Max-Age=0";
     }
 }
 
