6c29e39f5eb801f586a012633e6f7725d181ce04
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 10:19:12 +0000

    Revert 186400
    Broke android build: BrowsingHistoryHandler::ReturnResultsToFrontEnd(bool, int):history_ui.cc(.text._ZN22BrowsingHistoryHandler23ReturnResultsToFrontEndEbi+0x26):error: undefined reference to 'ManagedUserServiceFactory::GetForProfile(Profile*)'
    
    > History: Create delete directives when local or remote history is deleted.
    > 
    > In order to support offline deletion of synced history entries, create a
    > delete directive whenever entries are deleted from the history page. The
    > directives will be synced to other clients, ensuring that it's possible
    > to delete remove history entries even when the history server cannot be
    > reached. This CL only creates delete directives when specific visits are
    > deleted, not for time range deletions from the Clear Browsing Data dialog.
    > 
    > This also adds a new HistoryEntry type, which can represent either a local
    > or remote history entry. Compared to using DictionaryValue, this is a much
    > better format for processing the entries before they are returned to the
    > history front-end. This will also make it easier to push the parsing of
    > the history server's JSON response into web_history_service.cc.
    > 
    > BUG=171693
    > 
    > Review URL: https://codereview.chromium.org/12319093
    
    TBR=dubroy@chromium.org
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/history_ui.h
+++ chrome/browser/ui/webui/history_ui.h
@@ -20,52 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_message_handler.h"
 #include "ui/base/layout.h"
 
-class BookmarkModel;
-class ManagedUserService;
-
 // The handler for Javascript messages related to the "history" view.
 class BrowsingHistoryHandler : public content::WebUIMessageHandler,
                                public content::NotificationObserver {
  public:
-  // Represents a history entry to be shown to the user, representing either
-  // a local or remote visit. A single entry can represent multiple visits,
-  // since only the most recent visit on a particular day is shown.
-  struct HistoryEntry {
-    HistoryEntry(const GURL& url, const string16& title, base::Time time,
-        const std::set<int64>& timestamps, bool is_search_result,
-        const string16& snippet);
-    HistoryEntry();
-    virtual ~HistoryEntry();
-
-    // Formats this entry's URL and title and adds them to |result|.
-    void SetUrlAndTitle(DictionaryValue* result);
-
-    // Converts the entry to a DictionaryValue to be owned by the caller.
-    scoped_ptr<DictionaryValue> ToValue(
-        BookmarkModel* bookmark_model,
-        ManagedUserService* managed_user_service);
-
-    // Comparison function for sorting HistoryEntries from newest to oldest.
-    static bool SortByTimeDescending(
-        const HistoryEntry& entry1, const HistoryEntry& entry2);
-
-    GURL url;
-    string16 title;  // Title of the entry. May be empty.
-
-    // The time of the entry. Usually this will be the time of the most recent
-    // visit to |url| on a particular day as defined in the local timezone.
-    base::Time time;
-
-    // Timestamps of all local or remote visits the same URL on the same day.
-    std::set<int64> all_timestamps;
-
-    // If true, this entry is a search result.
-    bool is_search_result;
-
-    // The entry's search snippet, if this entry is a search result.
-    string16 snippet;
-  };
-
   BrowsingHistoryHandler();
   virtual ~BrowsingHistoryHandler();
 
@@ -77,9 +35,6 @@ class BrowsingHistoryHandler : public content::WebUIMessageHandler,
 
   // Handler for the "removeUrlsOnOneDay" message.
   void HandleRemoveUrlsOnOneDay(const base::ListValue* args);
-
-  // Handler for the "removeVisits" message.
-  void HandleRemoveVisits(const base::ListValue* args);
 
   // Handler for "clearBrowsingData" message.
   void HandleClearBrowsingData(const base::ListValue* args);
@@ -110,10 +65,10 @@ class BrowsingHistoryHandler : public content::WebUIMessageHandler,
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
-  // Removes duplicate entries from the query results, only retaining the most
-  // recent visit to a URL on a particular day.
-  static void RemoveDuplicateResults(
-      std::vector<BrowsingHistoryHandler::HistoryEntry>* results);
+  // Removes duplicate visits from the given list of query results, only
+  // retaining the most recent visit to a URL on a particular day. |results|
+  // must already be sorted by visit time, most recent first.
+  static void RemoveDuplicateResults(base::ListValue* results);
 
  private:
   // The range for which to return results:
@@ -128,6 +83,11 @@ class BrowsingHistoryHandler : public content::WebUIMessageHandler,
 
   // Core implementation of history querying.
   void QueryHistory(string16 search_text, const history::QueryOptions& options);
+
+  // Creates a history query result value.
+  base::DictionaryValue* CreateQueryResultValue(
+      const GURL& url, const string16& title, base::Time visit_time,
+      bool is_search_result, const string16& snippet);
 
   // Sends the accumulated results of the query to the front end, truncating
   // the number to |max_count| if necessary. If |max_count| is 0, the results
@@ -162,7 +122,7 @@ class BrowsingHistoryHandler : public content::WebUIMessageHandler,
   bool ExtractIntegerValueAtIndex(
       const base::ListValue* value, int index, int* out_int);
 
-  // Sets the query options for a week-wide query, |offset| weeks ago.
+  // Set the query options for a week-wide query, |offset| weeks ago.
   void SetQueryTimeInWeeks(int offset, history::QueryOptions* options);
 
   // Sets the query options for a monthly query, |offset| months ago.
@@ -196,7 +156,7 @@ class BrowsingHistoryHandler : public content::WebUIMessageHandler,
   base::DictionaryValue results_info_value_;
 
   // The list of query results that is returned to the front end.
-  std::vector<HistoryEntry> query_results_;
+  base::ListValue results_value_;
 
   // Timer used to implement a timeout on a Web History response.
   base::OneShotTimer<BrowsingHistoryHandler> web_history_timer_;
