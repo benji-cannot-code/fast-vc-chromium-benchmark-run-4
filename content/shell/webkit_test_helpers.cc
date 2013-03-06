2239fe150296d0df55ffb7ee2d1b6b6b23ac0cb0
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 09:43:46 +0000

    [content shell] add the --enable-accelerated-2d-canvas flag
    It's used by the virtual/gpu layout tests
    
    BUG=111316
    TEST=virtual/gpu tests pass
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/12435010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186399 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_helpers.cc
+++ content/shell/webkit_test_helpers.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/shell/webkit_test_helpers.h"
 
+#include "base/command_line.h"
 #include "base/utf_string_conversions.h"
+#include "content/shell/shell_switches.h"
 #include "third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebPreferences.h"
 #include "webkit/glue/webpreferences.h"
 
@@ -58,6 +60,7 @@ void ExportLayoutTestSpecificPreferences(const WebPreferences& from,
 // of the defaults are controlled via command line flags which are
 // automatically set for layout tests.
 void ApplyLayoutTestDefaultPreferences(webkit_glue::WebPreferences* prefs) {
+  CommandLine& command_line = *CommandLine::ForCurrentProcess();
   prefs->allow_universal_access_from_file_urls = true;
   prefs->dom_paste_enabled = true;
   prefs->javascript_can_access_clipboard = true;
@@ -100,7 +103,8 @@ void ApplyLayoutTestDefaultPreferences(webkit_glue::WebPreferences* prefs) {
   prefs->asynchronous_spell_checking_enabled = false;
   prefs->user_style_sheet_enabled = true;
   prefs->threaded_html_parser = true;
-  prefs->accelerated_2d_canvas_enabled = false;
+  prefs->accelerated_2d_canvas_enabled =
+      command_line.HasSwitch(switches::kEnableAccelerated2DCanvas);
   prefs->accelerated_compositing_for_video_enabled = false;
   prefs->deferred_2d_canvas_enabled = false;
   prefs->mock_scrollbars_enabled = false;
