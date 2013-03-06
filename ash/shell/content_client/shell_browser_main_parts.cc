c559a9ef44ff3ddc1904275ebe6ac6d01090a71d
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 04:47:44 +0000

    Fix ash_shell to initialize the message center.
    Regression from crrev.com/12375004.
    
    BUG=NONE
    
    
    Review URL: https://chromiumcodereview.appspot.com/12488003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186365 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/content_client/shell_browser_main_parts.cc
+++ ash/shell/content_client/shell_browser_main_parts.cc
@@ -32,6 +32,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/focus/accelerator_handler.h"
 #include "ui/views/test/test_views_delegate.h"
 
+#if defined(ENABLE_MESSAGE_CENTER)
+#include "ui/message_center/message_center.h"
+#endif
+
 #if defined(OS_LINUX)
 #include "ui/base/touch/touch_factory.h"
 #endif
@@ -106,6 +110,11 @@ void ShellBrowserMainParts::PreMainMessageLoopRun() {
     views::ViewsDelegate::views_delegate = new ShellViewsDelegate;
 
   delegate_ = new ash::shell::ShellDelegateImpl;
+#if defined(ENABLE_MESSAGE_CENTER)
+  // The global message center state must be initialized absent
+  // g_browser_process.
+  message_center::MessageCenter::Initialize();
+#endif
   ash::Shell::CreateInstance(delegate_);
   ash::Shell::GetInstance()->set_browser_context(browser_context_.get());
 
@@ -135,6 +144,11 @@ void ShellBrowserMainParts::PostMainMessageLoopRun() {
   delegate_->SetWatcher(NULL);
   delegate_ = NULL;
   ash::Shell::DeleteInstance();
+#if defined(ENABLE_MESSAGE_CENTER)
+  // The global message center state must be shutdown absent
+  // g_browser_process.
+  message_center::MessageCenter::Shutdown();
+#endif
   aura::Env::DeleteInstance();
 }
 
