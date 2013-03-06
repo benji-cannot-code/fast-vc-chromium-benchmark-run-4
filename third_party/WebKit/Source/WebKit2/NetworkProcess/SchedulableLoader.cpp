ee262fa3c2d59c45d9ebdc0e2c80957d2da660bf
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-06 18:37:32 +0000

    Track sandbox extensions for blobs in NetworkProcess         https://bugs.webkit.org/show_bug.cgi?id=111484
            Reviewed by Sam Weinig.
    
            * WebKit2.xcodeproj/project.pbxproj:
            * NetworkProcess/FileAPI: Added.
            * NetworkProcess/FileAPI/NetworkBlobRegistry.cpp: Added.
            * NetworkProcess/FileAPI/NetworkBlobRegistry.h: Added.
            Added a registry that wraps normal in-process "BlobRegistryImpl", and also tracks
            sandbox extensions for blobs. It will also track process connections, so that
            blobs could be removed when a process crashes.
    
            * NetworkProcess/NetworkConnectionToWebProcess.cpp:
            (WebKit::NetworkConnectionToWebProcess::registerBlobURL):
            (WebKit::NetworkConnectionToWebProcess::registerBlobURLFromURL):
            (WebKit::NetworkConnectionToWebProcess::unregisterBlobURL):
            Use NetworkBlobRegistry.
    
            * NetworkProcess/SchedulableLoader.h:
            * NetworkProcess/SchedulableLoader.cpp:
            (WebKit::SchedulableLoader::SchedulableLoader): Add extensions for blobs, both in
            request URL and in request data.
            (WebKit::SchedulableLoader::consumeSandboxExtensions): Request may now have multiple
            extensions.
            (WebKit::SchedulableLoader::invalidateSandboxExtensions): Ditto.
    
            * Shared/FileAPI/BlobRegistrationData.cpp: Fixed extension creation - actually
            store the result.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/NetworkProcess/SchedulableLoader.cpp
+++ third_party/WebKit/Source/WebKit2/NetworkProcess/SchedulableLoader.cpp
@@ -29,6 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if ENABLE(NETWORK_PROCESS)
 
+#include "NetworkBlobRegistry.h"
+#include <WebCore/FormData.h>
+
+using namespace WebCore;
+
 namespace WebKit {
 
 SchedulableLoader::SchedulableLoader(const NetworkResourceLoadParameters& parameters, NetworkConnectionToWebProcess* connection)
@@ -47,7 +52,25 @@ SchedulableLoader::SchedulableLoader(const NetworkResourceLoadParameters& parame
         if (RefPtr<SandboxExtension> extension = SandboxExtension::create(parameters.requestBodySandboxExtensions()[i]))
             m_requestBodySandboxExtensions.append(extension);
     }
-    m_resourceSandboxExtension = SandboxExtension::create(parameters.resourceSandboxExtension());
+
+#if ENABLE(BLOB)
+    if (m_request.httpBody()) {
+        const Vector<FormDataElement>& elements = m_request.httpBody()->elements();
+        for (size_t i = 0, count = elements.size(); i < count; ++i) {
+            if (elements[i].m_type == FormDataElement::encodedBlob) {
+                Vector<RefPtr<SandboxExtension> > blobElementExtensions = NetworkBlobRegistry::shared().sandboxExtensions(elements[i].m_url);
+                m_requestBodySandboxExtensions.append(blobElementExtensions);
+            }
+        }
+    }
+
+    if (m_request.url().protocolIs("blob")) {
+        ASSERT(!SandboxExtension::create(parameters.resourceSandboxExtension()));
+        m_resourceSandboxExtensions = NetworkBlobRegistry::shared().sandboxExtensions(m_request.url());
+    } else
+#endif
+    if (RefPtr<SandboxExtension> resourceSandboxExtension = SandboxExtension::create(parameters.resourceSandboxExtension()))
+        m_resourceSandboxExtensions.append(resourceSandboxExtension);
 }
 
 SchedulableLoader::~SchedulableLoader()
@@ -67,8 +90,8 @@ void SchedulableLoader::consumeSandboxExtensions()
     for (size_t i = 0, count = m_requestBodySandboxExtensions.size(); i < count; ++i)
         m_requestBodySandboxExtensions[i]->consume();
 
-    if (m_resourceSandboxExtension)
-        m_resourceSandboxExtension->consume();
+    for (size_t i = 0, count = m_resourceSandboxExtensions.size(); i < count; ++i)
+        m_resourceSandboxExtensions[i]->consume();
 }
 
 void SchedulableLoader::invalidateSandboxExtensions()
@@ -76,8 +99,8 @@ void SchedulableLoader::invalidateSandboxExtensions()
     for (size_t i = 0, count = m_requestBodySandboxExtensions.size(); i < count; ++i)
         m_requestBodySandboxExtensions[i]->invalidate();
 
-    if (m_resourceSandboxExtension)
-        m_resourceSandboxExtension->invalidate();
+    for (size_t i = 0, count = m_resourceSandboxExtensions.size(); i < count; ++i)
+        m_resourceSandboxExtensions[i]->invalidate();
 }
 
 } // namespace WebKit
