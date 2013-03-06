1326b5e83826c4c2182c01f3a78ce7081582de44
morrita@google.com <morrita@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-06 02:52:34 +0000

    [Custom Elements][V8] Custom Element doesn't need its own WrapperTypeInfo https://bugs.webkit.org/show_bug.cgi?id=111411
    Reviewed by Kentaro Hara.
    
    No new tests. No observable change yet.
    
    This is a preparation for Bug 110436, where WebKit is going to
    allow any HTML element to be a superclass of custom elements.
    
    This change eliminates WrapperTypeInfo for custom element. Instead
    of that, the wrapper of a custom element picks WrapperTypeInfo
    from most descendant built-in HTML element. For example, think about
    following hierarchy:
    
    - ... <- HTMLElement <- HTMLDivElememnt <- ExistingCustomElement <- NewCustomElement
    
    In this case, both ExistingCustomElement and NewCustomElement has
    HTMLDivElememnt's type info. This is because HTMLDivElememnt is
    the most descendant (or "the nearest") built-in HTML
    element. (HTMLElement isn't the most descendant.
    ExistingCustomElement isn't a built-in.)
    
    Using such WrapperTypeInfos, created custom element wrappers can
    get appropriate set of method and properties regardless of its
    super-elements.
    
    This change tells the prototype object of each built-in object
    which WrapperTypeInfo the native backing wants:
    HTMLDivElememnt.prototype knows V8HTMLDivElement::info for
    example. The pointer to the type info is stored into an internal
    field. On wrapper construction, createWrapper() walks up the
    prototype chain and pick the first one.
    
    * bindings/v8/V8DOMConfiguration.cpp:
    (WebCore::V8DOMConfiguration::configureTemplate):
    * bindings/v8/V8HTMLCustomElement.cpp:
    (WebCore::findWrapperTypeOf):
    (WebCore::V8HTMLCustomElement::createWrapper):
    * bindings/v8/V8HTMLCustomElement.h:
    * bindings/v8/V8PerContextData.cpp:
    (WebCore::V8PerContextData::constructorForTypeSlowCase):
    * bindings/v8/WrapperTypeInfo.h:
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@144865 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/v8/V8DOMConfiguration.cpp
+++ third_party/WebKit/Source/WebCore/bindings/v8/V8DOMConfiguration.cpp
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-const int prototypeInternalFieldcount = 1;
-
 void V8DOMConfiguration::batchConfigureAttributes(v8::Handle<v8::ObjectTemplate> instance, v8::Handle<v8::ObjectTemplate> prototype, const BatchedAttribute* attributes, size_t attributeCount, v8::Isolate* isolate)
 {
     for (size_t i = 0; i < attributeCount; ++i)
@@ -69,7 +67,7 @@ v8::Local<v8::Signature> V8DOMConfiguration::configureTemplate(v8::Persistent<v8
         // This is needed since bug 110436 asks WebKit to tell native-initiated prototypes from pure-JS ones.
         // This doesn't mark kinds "root" classes like Node, where setting this changes prototype chain structure.
         v8::Local<v8::ObjectTemplate> prototype = functionDescriptor->PrototypeTemplate();
-        prototype->SetInternalFieldCount(prototypeInternalFieldcount);
+        prototype->SetInternalFieldCount(v8PrototypeInternalFieldcount);
     }
 
     if (attributeCount)
