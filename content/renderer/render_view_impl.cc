cf0ce7e2a5186e6364b16ad68cc5e30b4b914ce1
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-06 14:06:05 +0000

    Notify compositor about auto resized WebViews.
    In r185621 I changed the compositor to only get notified about viewport
    size changes in OnResize(), but auto resize mode WebViews change their
    own size in WebViewImpl and don't call OnResize at all.  Add an extra
    call from didAutoResize, and also from SetDeviceScaleFactor since that's
    another input to the size.
    
    NOTRY=true
    BUG=180378
    
    
    Review URL: https://chromiumcodereview.appspot.com/12524002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186429 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -612,6 +612,7 @@ RenderViewImpl::RenderViewImpl(RenderViewImplParams* params)
       send_content_state_immediately_(false),
       enabled_bindings_(0),
       send_preferred_size_changes_(false),
+      auto_resize_mode_(false),
       is_loading_(false),
       navigation_gesture_(NavigationGestureUnknown),
       opened_by_user_gesture_(true),
@@ -5435,6 +5436,7 @@ void RenderViewImpl::OnEnableAutoResize(const gfx::Size& min_size,
   DCHECK(disable_scrollbars_size_limit_.IsEmpty());
   if (!webview())
     return;
+  auto_resize_mode_ = true;
   webview()->enableAutoResizeMode(min_size, max_size);
 }
 
@@ -5442,6 +5444,7 @@ void RenderViewImpl::OnDisableAutoResize(const gfx::Size& new_size) {
   DCHECK(disable_scrollbars_size_limit_.IsEmpty());
   if (!webview())
     return;
+  auto_resize_mode_ = false;
   webview()->disableAutoResizeMode();
 
   Resize(new_size, physical_backing_size_, resizer_rect_, is_fullscreen_,
@@ -6199,6 +6202,8 @@ void RenderViewImpl::SetDeviceScaleFactor(float device_scale_factor) {
     webview()->settings()->setAcceleratedCompositingForFixedPositionEnabled(
         ShouldUseFixedPositionCompositing(device_scale_factor_));
   }
+  if (auto_resize_mode_)
+    AutoResizeCompositor();
 }
 
 ui::TextInputType RenderViewImpl::GetTextInputType() {
