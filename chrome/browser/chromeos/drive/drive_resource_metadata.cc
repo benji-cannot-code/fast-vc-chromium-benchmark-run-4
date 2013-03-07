d71be7682d94c5fc08eeac3231dd0191f05f1ed4
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 01:07:14 +0000

    drive: Introduce per-directory changestamp
    This field will be used to implement "fast fetch".
    
    BUG=178348
    TEST=none; no functional change
    
    Review URL: https://codereview.chromium.org/12526002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186565 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_resource_metadata.cc
+++ chrome/browser/chromeos/drive/drive_resource_metadata.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/drive_resource_metadata.h"
 
 #include <leveldb/db.h>
+#include <stack>
 #include <utility>
 
 #include "base/message_loop_proxy.h"
@@ -46,6 +47,20 @@ void PostGetEntryInfoWithFilePathCallbackError(
   base::MessageLoopProxy::current()->PostTask(
       FROM_HERE,
       base::Bind(callback, error, base::FilePath(), base::Passed(&proto)));
+}
+
+// Adds per-directory changestamps to |root| and all sub directories.
+void AddPerDirectoryChangestamps(DriveDirectoryProto* root, int64 changestamp) {
+  std::stack<DriveDirectoryProto*> stack;
+  stack.push(root);
+  while (!stack.empty()) {
+    DriveDirectoryProto* directory = stack.top();
+    stack.pop();
+    directory->mutable_drive_entry()->mutable_directory_specific_info()
+        ->set_changestamp(changestamp);
+    for (int i = 0; i < directory->child_directories_size(); ++i)
+      stack.push(directory->mutable_child_directories(i));
+  }
 }
 
 }  // namespace
@@ -858,6 +873,14 @@ bool DriveResourceMetadata::ParseFromString(
     LOG(ERROR) << "Incompatible proto detected (incompatible version): "
                << proto.version();
     return false;
+  }
+
+  // An old proto file might not have per-directory changestamps. Add them if
+  // needed.
+  const DriveDirectoryProto& root = proto.drive_directory();
+  if (!root.drive_entry().directory_specific_info().has_changestamp()) {
+    AddPerDirectoryChangestamps(proto.mutable_drive_directory(),
+                                proto.largest_changestamp());
   }
 
   root_->FromProto(proto.drive_directory());
