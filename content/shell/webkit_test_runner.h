56ce9e40c171932f1553fb102e523acbb9803a34
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 10:31:41 +0000

    Revert 186654
    Triggered a bug in WebContentsImpl
    
    > [content shell] eliminate the pause between reset and starting a new test
    > 
    > When receiving the configuration for the next test, we don't immediately set the
    > config but wait for the render view to navigate to the next test. Only then,
    > reset all test interfaces, and apply the new config
    > 
    > BUG=171128
    > R=marja@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/12597002
    
    TBR=jochen@chromium.org
    Review URL: https://codereview.chromium.org/12603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186676 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_runner.h
+++ content/shell/webkit_test_runner.h
@@ -12,12 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "content/public/renderer/render_view_observer.h"
 #include "content/public/renderer/render_view_observer_tracker.h"
-#include "content/shell/shell_test_configuration.h"
 #include "third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebPreferences.h"
 #include "third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestDelegate.h"
 #include "v8/include/v8.h"
 
 class SkCanvas;
+struct ShellViewMsg_SetTestConfiguration_Params;
 
 namespace WebKit {
 struct WebRect;
@@ -40,7 +40,6 @@ class WebKitTestRunner : public RenderViewObserver,
   // RenderViewObserver implementation.
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
   virtual void DidClearWindowObject(WebKit::WebFrame* frame) OVERRIDE;
-  virtual void Navigate(const GURL& url) OVERRIDE;
 
   // WebTestDelegate implementation.
   virtual void clearEditCommand();
@@ -96,7 +95,8 @@ class WebKitTestRunner : public RenderViewObserver,
 
  private:
   // Message handlers.
-  void OnSetTestConfiguration(const ShellTestConfiguration& params);
+  void OnSetTestConfiguration(
+      const ShellViewMsg_SetTestConfiguration_Params& params);
   void OnSessionHistory(
       const std::vector<int>& routing_ids,
       const std::vector<std::vector<std::string> >& session_histories,
@@ -115,17 +115,16 @@ class WebKitTestRunner : public RenderViewObserver,
 
   ::WebTestRunner::WebPreferences prefs_;
 
-  ShellTestConfiguration test_config_;
+  bool enable_pixel_dumping_;
+  int layout_test_timeout_;
+  bool allow_external_pages_;
+  std::string expected_pixel_hash_;
 
   std::vector<int> routing_ids_;
   std::vector<std::vector<std::string> > session_histories_;
   std::vector<unsigned> current_entry_indexes_;
 
   bool is_main_window_;
-
-  bool reset_on_next_navigation_;
-
-  bool test_is_running_;
 
   DISALLOW_COPY_AND_ASSIGN(WebKitTestRunner);
 };
