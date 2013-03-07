61e11eaa12dfb0ee909e0f9a67bc1cd1102a4815
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 22:02:48 +0000

    Add a didFocusTextField callback to the injected bundle form client https://bugs.webkit.org/show_bug.cgi?id=111771
    Reviewed by Andreas Kling.
    
    Source/WebCore:
    
    Export a symbol needed by WebKit2.
    
    * WebCore.exp.in:
    
    Source/WebKit2:
    
    Add a new client callback that's invoked whenever a text input field is focused.
    
    * Shared/APIClientTraits.cpp:
    (WebKit):
    * Shared/APIClientTraits.h:
    * WebProcess/InjectedBundle/API/c/WKBundlePage.h:
    * WebProcess/InjectedBundle/InjectedBundlePageFormClient.cpp:
    (WebKit::InjectedBundlePageFormClient::didFocusTextField):
    (WebKit):
    * WebProcess/InjectedBundle/InjectedBundlePageFormClient.h:
    (InjectedBundlePageFormClient):
    * WebProcess/WebCoreSupport/WebChromeClient.cpp:
    (WebKit::WebChromeClient::focusedNodeChanged):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebChromeClient.cpp
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <WebCore/FrameLoadRequest.h>
 #include <WebCore/FrameLoader.h>
 #include <WebCore/FrameView.h>
+#include <WebCore/HTMLInputElement.h>
 #include <WebCore/HTMLNames.h>
 #include <WebCore/HTMLParserIdioms.h>
 #include <WebCore/HTMLPlugInImageElement.h>
@@ -153,9 +154,19 @@ void WebChromeClient::takeFocus(FocusDirection direction)
     m_page->send(Messages::WebPageProxy::TakeFocus(direction));
 }
 
-void WebChromeClient::focusedNodeChanged(Node*)
+void WebChromeClient::focusedNodeChanged(Node* node)
 {
-    notImplemented();
+    if (!node)
+        return;
+    if (!node->hasTagName(inputTag))
+        return;
+
+    HTMLInputElement* inputElement = static_cast<HTMLInputElement*>(node);
+    if (!inputElement->isText())
+        return;
+
+    WebFrame* webFrame = static_cast<WebFrameLoaderClient*>(node->document()->frame()->loader()->client())->webFrame();
+    m_page->injectedBundleFormClient().didFocusTextField(m_page, inputElement, webFrame);
 }
 
 void WebChromeClient::focusedFrameChanged(Frame* frame)
