d093e7beb076bd50b8ecae1eb3b28980faa2c46d
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 05:55:29 +0000

    TileCache debug minimap should fit inside the intersection of the visible rect and exposed rect https://bugs.webkit.org/show_bug.cgi?id=111655 <rdar://problem/13355773>
    Reviewed by Simon Fraser.
    
    If content clips to the exposed rect, make the TileCache minimap fit in the exposed rect too.
    
    * page/scrolling/mac/ScrollingCoordinatorMac.mm:
    (WebCore::ScrollingCoordinatorMac::commitTreeState):
    * platform/graphics/TiledBacking.h:
    * platform/graphics/ca/mac/TileCache.mm:
    (WebCore::TileCache::updateTileCoverageMap):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/TileCache.mm
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/mac/TileCache.mm
@@ -820,6 +820,10 @@ void TileCache::updateTileCoverageMap()
 {
     FloatRect containerBounds = bounds();
     FloatRect visibleRect = this->visibleRect();
+
+    if (m_clipsToExposedRect)
+        visibleRect.intersect(m_exposedRect);
+
     visibleRect.contract(4, 4); // Layer is positioned 2px from top and left edges.
 
     float widthScale = 1;
@@ -835,6 +839,11 @@ void TileCache::updateTileCoverageMap()
     
     BEGIN_BLOCK_OBJC_EXCEPTIONS
     
+    if (m_clipsToExposedRect)
+        [m_tiledScrollingIndicatorLayer.get() setPosition:m_exposedRect.location() + FloatPoint(2, 2)];
+    else
+        [m_tiledScrollingIndicatorLayer.get() setPosition:CGPointMake(2, 2)];
+
     [m_tiledScrollingIndicatorLayer.get() setBounds:mapBounds];
     [m_tiledScrollingIndicatorLayer.get() setNeedsDisplay];
 
