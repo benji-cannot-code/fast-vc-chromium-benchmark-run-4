6a036ee9db3690e126bda847d5e7158e90da1605
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 19:13:39 +0000

    [Chromium] Update some #includes for WebFileInfo.h in WK/chromium/src/ https://bugs.webkit.org/show_bug.cgi?id=111741
    Reviewed by James Robinson.
    
    In preparation for removing WK/chromium/public/WebFileInfo.h.
    
    * src/AssertMatchingEnums.cpp:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145108 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/AssertMatchingEnums.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/AssertMatchingEnums.cpp
@@ -91,7 +91,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebCursorInfo.h"
 #include "WebEditingAction.h"
 #include "WebFileError.h"
-#include "WebFileInfo.h"
 #include "WebFontDescription.h"
 #if ENABLE(REQUEST_AUTOCOMPLETE)
 #include "WebFormElement.h"
@@ -122,6 +121,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebTextCheckingType.h"
 #include "WebView.h"
 #include <public/WebClipboard.h>
+#include <public/WebFileInfo.h>
 #include <public/WebFileSystem.h>
 #include <public/WebFilterOperation.h>
 #include <public/WebMediaStreamSource.h>
