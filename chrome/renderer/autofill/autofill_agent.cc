97dd864d2675fb627a9c27ec9826cbb511eb4c37
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 19:57:58 +0000

    Nail down DEPS of Autofill-specific code in chrome/renderer.
    Remove a couple of unnecessary includes, write DEPS that allows
    includes of files that will move to //components/autofill, and add a
    temporary allowance for an include we'll get rid of by moving
    Autofill-specific switches to a header in the Autofill component.
    
    Also, mark include of GaiaUrls file as permanently allowed, as I don't
    think there was any plan to get rid of this.
    
    TBR=ben@chromium.org
    BUG=140037
    
    
    Review URL: https://chromiumcodereview.appspot.com/12550007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186768 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/autofill/autofill_agent.cc
+++ chrome/renderer/autofill/autofill_agent.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_split.h"
 #include "base/time.h"
 #include "base/utf_string_conversions.h"
-#include "chrome/common/chrome_constants.h"
 #include "chrome/renderer/autofill/form_autofill_util.h"
 #include "chrome/renderer/autofill/password_autofill_manager.h"
 #include "components/autofill/common/autocheckout_status.h"
--- chrome/renderer/page_click_tracker.cc
+++ chrome/renderer/page_click_tracker.cc
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/page_click_tracker.h"
 
-#include "chrome/common/render_messages.h"
 #include "chrome/renderer/autofill/form_autofill_util.h"
 #include "chrome/renderer/page_click_listener.h"
 #include "content/public/renderer/render_view.h"
