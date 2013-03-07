e623b77b2e933132f9e7baa12db2cc7f21e402d0
eric@webkit.org <eric@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 02:22:36 +0000

    AtomicStringImpls are not safe to send between threads https://bugs.webkit.org/show_bug.cgi?id=111652
    Reviewed by Benjamin Poulain.
    
    We don't have this problem yet, but it's good to be safe.
    
    * wtf/text/WTFString.cpp:
    (WTF::String::isSafeToSendToAnotherThread):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145020 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/text/WTFString.cpp
+++ third_party/WebKit/Source/WTF/wtf/text/WTFString.cpp
@@ -666,6 +666,10 @@ bool String::isSafeToSendToAnotherThread() const
 {
     if (!impl())
         return true;
+    // AtomicStrings are not safe to send between threads as ~StringImpl()
+    // will try to remove them from the wrong AtomicStringTable.
+    if (impl()->isAtomic())
+        return false;
     if (impl()->hasOneRef())
         return true;
     if (isEmpty())
