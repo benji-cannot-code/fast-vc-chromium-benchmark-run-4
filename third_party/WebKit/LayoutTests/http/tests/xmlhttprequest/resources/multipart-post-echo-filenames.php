ddb3fdde1d7e3e6ccc3de4b061dde364981b8844
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 22:52:59 +0000

    FormData should allow setting filename to empty         https://bugs.webkit.org/show_bug.cgi?id=111687
            Reviewed by Brady Eidson.
    
            Tests: http/tests/local/formdata/send-form-data-with-empty-blob-filename.html
                   http/tests/local/formdata/send-form-data-with-empty-file-filename.html
    
            * platform/network/FormData.cpp: (WebCore::FormData::appendKeyValuePairItems):
            Missing value is a null string. If the string is empty, we should treat is as
            authoritative.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/xmlhttprequest/resources/multipart-post-echo-filenames.php
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+$first = True;
+foreach ($_FILES as $file) {
+    if (!$first)
+        echo ",";
+    echo $file['name'];
+    $first = False;
+}
+?>
