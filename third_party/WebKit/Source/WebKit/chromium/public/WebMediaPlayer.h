d1bf59c88b69b408116ea0bee759a38cc9315ae0
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 18:37:33 +0000

    [Chromium] Update some #includes for WebVideoFrame.h https://bugs.webkit.org/show_bug.cgi?id=111739
    Reviewed by James Robinson.
    
    In anticipation of removing WebVideoFrame.h from WK/chromium/public/.
    
    * public/WebMediaPlayer.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145099 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayer.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayer.h
@@ -34,9 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "../../../Platform/chromium/public/WebCanvas.h"
 #include "../../../Platform/chromium/public/WebString.h"
+#include "../../../Platform/chromium/public/WebVideoFrame.h"
 #include "WebMediaSource.h"
 #include "WebTimeRange.h"
-#include "WebVideoFrame.h"
 
 namespace WebKit {
 
