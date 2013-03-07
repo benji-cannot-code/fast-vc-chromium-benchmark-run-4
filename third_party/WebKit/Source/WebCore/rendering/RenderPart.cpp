e4582bcafae7d4a8341182774ead4e392a5416b2
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-07 18:35:02 +0000

    REGRESSION(r143727): Clicking / selecting inside an <embed> is broken https://bugs.webkit.org/show_bug.cgi?id=111217
    Reviewed by Julien Chaffraix.
    
    Source/WebCore:
    
    Hit-testing into child frames need to happen in RenderPart, otherwise
    we will miss frame views in <embed> and <object> elements.
    
    Test: fast/dom/nodesFromRect/nodesFromRect-embedded-frame-content.html
    
    * rendering/RenderFrameBase.cpp:
    * rendering/RenderFrameBase.h:
    (RenderFrameBase):
    * rendering/RenderPart.cpp:
    (WebCore::RenderPart::nodeAtPoint):
    * rendering/RenderPart.h:
    (RenderPart):
    
    LayoutTests:
    
    Test rect and point based hit-testing inside embed and object child frames.
    
    * fast/dom/nodesFromRect/nodesFromRect-embedded-frame-content-expected.txt: Added.
    * fast/dom/nodesFromRect/nodesFromRect-embedded-frame-content.html: Added.
    * fast/dom/nodesFromRect/resources/nodesFromRect.js:
    (checkPoint):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145098 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderPart.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderPart.cpp
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Frame.h"
 #include "FrameView.h"
 #include "HTMLFrameElementBase.h"
+#include "HitTestResult.h"
 #include "PluginViewBase.h"
+#include "RenderLayer.h"
 #include "RenderSVGRoot.h"
 #include "RenderView.h"
 
@@ -107,6 +109,44 @@ RenderBox* RenderPart::embeddedContentBox() const
     if (!node() || !widget() || !widget()->isFrameView())
         return 0;
     return static_cast<FrameView*>(widget())->embeddedContentBox();
+}
+
+bool RenderPart::nodeAtPoint(const HitTestRequest& request, HitTestResult& result, const HitTestLocation& locationInContainer, const LayoutPoint& accumulatedOffset, HitTestAction action)
+{
+    if (!widget() || !widget()->isFrameView() || !request.allowsChildFrameContent())
+        return RenderWidget::nodeAtPoint(request, result, locationInContainer, accumulatedOffset, action);
+
+    FrameView* childFrameView = static_cast<FrameView*>(widget());
+    RenderView* childRoot = childFrameView->renderView();
+
+    if (childRoot) {
+        LayoutPoint adjustedLocation = accumulatedOffset + location();
+        LayoutPoint contentOffset = LayoutPoint(borderLeft() + paddingLeft(), borderTop() + paddingTop()) - childFrameView->scrollOffset();
+        HitTestLocation newHitTestLocation(locationInContainer, -adjustedLocation - contentOffset);
+        HitTestRequest newHitTestRequest(request.type() | HitTestRequest::ChildFrameHitTest);
+        HitTestResult childFrameResult(newHitTestLocation);
+
+        bool isInsideChildFrame = childRoot->hitTest(newHitTestRequest, newHitTestLocation, childFrameResult);
+
+        if (newHitTestLocation.isRectBasedTest())
+            result.append(childFrameResult);
+        else if (isInsideChildFrame)
+            result = childFrameResult;
+
+        if (isInsideChildFrame)
+            return true;
+
+        if (request.allowsFrameScrollbars()) {
+            // ScrollView scrollbars are not the same as RenderLayer scrollbars tested by RenderLayer::hitTestOverflowControls,
+            // so we need to test ScrollView scrollbars separately here.
+            // FIXME: Consider if this test could be done unconditionally.
+            Scrollbar* frameScrollbar = childFrameView->scrollbarAtPoint(newHitTestLocation.roundedPoint());
+            if (frameScrollbar)
+                result.setScrollbar(frameScrollbar);
+        }
+    }
+
+    return RenderWidget::nodeAtPoint(request, result, locationInContainer, accumulatedOffset, action);
 }
 
 }
