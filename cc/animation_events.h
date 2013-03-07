b246b030cf319cb79e01799b566036a33aa7f765
wjmaclean@chromium.org <wjmaclean@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 16:16:41 +0000

    Allow impl-only animations, and return opacity values via AnimationEvents.
    We would like to allow animations to run only on the impl thread, so we need to make sure they don't get clobbered when finished animations are removed. Also, we need a mechanism to allow opacity values to propagate back to the main-thread so that impl-only opacity animations have their values reflected on the main thread when the animation is complete.
    
    BUG=165842
    
    
    Review URL: https://chromiumcodereview.appspot.com/12453010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186725 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/animation_events.h
+++ cc/animation_events.h
@@ -9,11 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "cc/animation.h"
+#include "ui/gfx/transform.h"
 
 namespace cc {
 
 struct AnimationEvent {
-    enum Type { Started, Finished };
+    enum Type { Started, Finished, PropertyUpdate };
 
     AnimationEvent(Type type, int layerId, int groupId, Animation::TargetProperty targetProperty, double monotonicTime)
         : type(type)
@@ -21,6 +22,7 @@ struct AnimationEvent {
         , groupId(groupId)
         , targetProperty(targetProperty)
         , monotonicTime(monotonicTime)
+        , opacity(0)
     {
     }
 
@@ -29,6 +31,8 @@ struct AnimationEvent {
     int groupId;
     Animation::TargetProperty targetProperty;
     double monotonicTime;
+    float opacity;
+    gfx::Transform transform;
 };
 
 typedef std::vector<AnimationEvent> AnimationEventsVector;
