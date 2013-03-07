685ac63f690c32ada3cc1943a0040250bc77e0c3
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 22:08:06 +0000

    [Mac] Convert ignore-exceptions helpers to block version.
    PerformSelectorIgnoringExceptions() -> RunBlockIgnoringExceptions(),
    as the block version is more generalized.
    
    BUG=none
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12342021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186789 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/mac/video_capture_device_qtkit_mac.mm
+++ media/video/capture/mac/video_capture_device_qtkit_mac.mm
@@ -24,7 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // following catches any exceptions and continues in an orderly
   // fashion with no devices detected.
   NSArray* captureDevices =
-      base::mac::RunBlockIgnoringExceptions(^() {
+      base::mac::RunBlockIgnoringExceptions(^{
           return [QTCaptureDevice inputDevicesWithMediaType:QTMediaTypeVideo];
       });
 
