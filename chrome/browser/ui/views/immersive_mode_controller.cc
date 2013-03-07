a4bb9e64477f4bc6598fa514203b57f8b096690f
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-07 08:38:18 +0000

    ash: Keep immersive mode open during 3f tab scrub
    * Lock immersive mode in reveal state during scrub
    * Animate the slide out at the end of scrub rather than popping
    
    BUG=none
    TEST=manual, turn on immersive mode, go into fullscreen, scrub left/right with three fingers
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12464010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186651 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/immersive_mode_controller.cc
+++ chrome/browser/ui/views/immersive_mode_controller.cc
@@ -336,6 +336,7 @@ ImmersiveModeController::ImmersiveModeController(BrowserView* browser_view)
     : browser_view_(browser_view),
       enabled_(false),
       revealed_(false),
+      reveal_locked_(false),
       hide_tab_indicators_(false),
       native_window_(NULL) {
 }
@@ -422,6 +423,16 @@ void ImmersiveModeController::MaybeStartReveal() {
 
 void ImmersiveModeController::CancelReveal() {
   EndReveal(ANIMATE_NO, LAYOUT_YES);
+}
+
+void ImmersiveModeController::RevealAndLock(bool reveal) {
+  if (!enabled_)
+    return;
+  reveal_locked_ = reveal;
+  if (reveal_locked_ && !revealed_)
+    StartReveal(ANIMATE_FAST);
+  else if (!reveal_locked_ && revealed_)
+    EndReveal(ANIMATE_FAST, LAYOUT_YES);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -542,13 +553,13 @@ void ImmersiveModeController::OnRevealViewLostMouse() {
   // TODO(jamescook): Consider stopping the reveal after a delay. This code
   // isn't using a MouseWatcher because it needs to know if the mouse re-enters
   // the RevealView before focus is lost.
-  if (!reveal_view_->ContainsFocusedView())
+  if (!reveal_view_->ContainsFocusedView() && !reveal_locked_)
     EndReveal(ANIMATE_FAST, LAYOUT_YES);
 }
 
 void ImmersiveModeController::OnRevealViewLostFocus() {
   // Stop the reveal if the mouse is outside the reveal view.
-  if (!reveal_view_->hovered())
+  if (!reveal_view_->hovered() && !reveal_locked_)
     EndReveal(ANIMATE_FAST, LAYOUT_YES);
 }
 
