ffd24210afe7e18e1f9ec81ab9d2dad026dacf72
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 23:37:39 +0000

    Revert 187048 based on http://build.chromium.org/p/chromium.win/builders/Win%20Aura%20Tests%20%282%29/builds/1968
    > Enable the "Web Audio Input" feature by default.
    > 
    > We now get the proper audio input device identifier passed into WebKit and back into chromium
    > to be able to determine if the correct device will be used that the user gave permission to
    > access in getUserMedia().
    > 
    > See related WebKit work:
    > https://bugs.webkit.org/show_bug.cgi?id=90110
    > https://bugs.webkit.org/show_bug.cgi?id=93756
    > https://bugs.webkit.org/show_bug.cgi?id=101815
    > https://bugs.webkit.org/show_bug.cgi?id=106490
    > https://bugs.webkit.org/show_bug.cgi?id=106816
    > https://bugs.webkit.org/show_bug.cgi?id=109494
    > https://bugs.webkit.org/show_bug.cgi?id=110796
    > 
    > and chromium work:
    > https://codereview.chromium.org/10830268/
    > https://codereview.chromium.org/10916105/
    > https://codereview.chromium.org/10909185/
    > https://codereview.chromium.org/10959068
    > https://codereview.chromium.org/11418125/
    > https://codereview.chromium.org/11827040/
    > https://codereview.chromium.org/11878032/
    > https://codereview.chromium.org/12218106/
    > https://codereview.chromium.org/12335072/
    > 
    > BUG=145092
    > TEST=none
    > manual tests:
    > http://chromium.googlecode.com/svn/trunk/samples/audio/visualizer-live.html
    > http://dashersw.github.com/pedalboard.js/demo/
    > Review URL: https://codereview.chromium.org/12383064
    
    TBR=crogers@google.com
    Review URL: https://codereview.chromium.org/12649007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187073 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -761,6 +761,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #else
     switches::kDisableWebAudio,
 #endif
+    switches::kEnableWebAudioInput,
     switches::kDisableWebSockets,
     switches::kDomAutomationController,
     switches::kEnableAccessibilityLogging,
