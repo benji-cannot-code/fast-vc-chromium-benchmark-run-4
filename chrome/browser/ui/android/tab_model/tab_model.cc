6ff31de01e78fe878dd60be518b6f7e314c37903
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 22:11:51 +0000

    Expose getSearchTerms to tab_model for Android
    This adds the necessary call to expose getSearchTerms to tab_model
    through the toolbar_model and ifdefs out the instant process checks
    for iOS and Android since neither uses instant for query extraction.
    
    BUG=177517
    
    
    Review URL: https://chromiumcodereview.appspot.com/12387026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/android/tab_model/tab_model.cc
+++ chrome/browser/ui/android/tab_model/tab_model.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/search_engines/search_terms_data.h"
 #include "chrome/browser/sync/glue/synced_window_delegate_android.h"
 #include "chrome/browser/ui/toolbar/toolbar_model_impl.h"
 #include "chrome/common/chrome_notification_types.h"
@@ -91,6 +92,17 @@ ToolbarModel* TabModel::GetToolbarModel() {
 
 ToolbarModel::SecurityLevel TabModel::GetSecurityLevelForCurrentTab() {
   return toolbar_model_->GetSecurityLevel();
+}
+
+string16 TabModel::GetSearchTermsForCurrentTab() {
+  return toolbar_model_->GetText(true);
+}
+
+std::string TabModel::GetQueryExtractionParam() {
+  if (!profile_)
+    return std::string();
+  UIThreadSearchTermsData search_terms_data(profile_);
+  return search_terms_data.InstantExtendedEnabledParam();
 }
 
 void TabModel::Observe(
