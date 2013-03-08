2875b28b43d4a38e5df3d93627fce573a44caf30
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 23:10:48 +0000

    [Media Gallery, Linux] Implement asynchronous MtpDeviceDelegateImplLinux.
    Previously, 
    (1) MtpDeviceDelegateImplLinux implemented a synchronous version of MTPDeviceDelegate. 
    (2) MtpDeviceDelegateImplLinux was operated on multiple threads (blocking pool thread and UI thread). 
    (3) Because of the synchronous behavior, WaitableEvent was used to block a blocking pool thread.
    
    Now, 
    (1) MtpDeviceDelegateImplLinux implements an asynchronous version of MTPDeviceDelegate. 
    (2) MtpDeviceDelegateImplLinux runs on the single thread (IO thread). 
    (3) MtpDeviceDelegateImplLinux supports weak pointers to cancel the callbacks that may run after the destruction of MtpDeviceDelegateImplLinux. 
    (4) Because of the asynchronous behavior, MtpDeviceDelegateImplLinux does a call-and-reply to the UI thread to dispatch the requests (such as GetFileInfo, ReadDirectory, CreateSnapshotFile, etc) to MediaTransferProtocolManager.
    (5) All the WaitableEvents are removed. Blocking pool threads are no longer used.
    
    BUG=154835
    TEST=none
    TBR=kinuko@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12212042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187068 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/media/mtp_device_file_system_config.h
+++ webkit/fileapi/media/mtp_device_file_system_config.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // Use asynchronous MTP device delegate API.
 // TODO(kmadhusu): remove this define and make this default.
-#if defined(OS_WIN)
+// Note that OS_LINUX implies OS_CHROMEOS
+#if defined(OS_WIN) || defined(OS_LINUX)
 #define USE_MTP_DEVICE_ASYNC_DELEGATE
 #endif
 
