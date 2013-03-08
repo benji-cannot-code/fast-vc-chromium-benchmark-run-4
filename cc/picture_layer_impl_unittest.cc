e4253cd15cc42b4445a61401b9b6e9c692c85d65
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 02:28:18 +0000

    cc: Chromify the Renderer class.
    Style-only change. Apply chromium style to the Renderer class.
    
    R=jamesr,enne
    
    
    Review URL: https://chromiumcodereview.appspot.com/12630005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186859 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_layer_impl_unittest.cc
+++ cc/picture_layer_impl_unittest.cc
@@ -514,7 +514,7 @@ TEST_F(PictureLayerImplTest, ManageTilingsCreatesTilings) {
   SetupTrees(pending_pile, active_pile);
   EXPECT_EQ(0u, pending_layer_->tilings().num_tilings());
 
-  float low_res_factor = host_impl_.settings().lowResContentsScaleFactor;
+  float low_res_factor = host_impl_.Settings().lowResContentsScaleFactor;
   EXPECT_LT(low_res_factor, 1.f);
 
   // These are included in the scale given to the layer.
@@ -601,7 +601,7 @@ TEST_F(PictureLayerImplTest, CleanUpTilings) {
   SetupTrees(pending_pile, active_pile);
   EXPECT_EQ(0u, pending_layer_->tilings().num_tilings());
 
-  float low_res_factor = host_impl_.settings().lowResContentsScaleFactor;
+  float low_res_factor = host_impl_.Settings().lowResContentsScaleFactor;
   EXPECT_LT(low_res_factor, 1.f);
 
   // These are included in the scale given to the layer.
