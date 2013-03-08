014089cb7c7701fe19e836c3a631a32890d2eec9
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 03:55:56 +0000

    Remove SpdyDataFlags from SpdyFramerVisitorInterface::OnStreamFrameData()
    This lands server change 43182358.
    
    TBR=rch@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12374038
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186874 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_test_util_common.cc
+++ net/spdy/spdy_test_util_common.cc
@@ -46,7 +46,7 @@ class PriorityGetter : public BufferedSpdyFramerVisitorInterface {
   virtual void OnStreamFrameData(SpdyStreamId stream_id,
                                  const char* data,
                                  size_t len,
-                                 SpdyDataFlags flags) OVERRIDE {}
+                                 bool fin) OVERRIDE {}
   virtual void OnSetting(
       SpdySettingsIds id, uint8 flags, uint32 value) OVERRIDE {}
   virtual void OnPing(uint32 unique_id) OVERRIDE {}
