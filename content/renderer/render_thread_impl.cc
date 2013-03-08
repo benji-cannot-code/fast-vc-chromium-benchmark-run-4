ccf2ef9936cb96634a52703e0b87d03668086800
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 23:51:18 +0000

    Leak non-main-thread shared contexts when RenderThreadImpl is being destroyed.
    If we do the shutdown path instead of just killing the process,
    the context3ds should just be leaked to avoid complex ordering
    problems and destroying the contexts on the wrong thread, since
    there are references held on the contexts across multiple threads.
    
    R=jamesr,piman
    
    
    Review URL: https://chromiumcodereview.appspot.com/12502007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187079 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -234,6 +234,19 @@ class RenderThreadImpl::GpuVDAContextLostCallback
   }
 };
 
+class RenderThreadImpl::RendererContextProviderCommandBuffer
+    : public ContextProviderCommandBuffer {
+ protected:
+  virtual ~RendererContextProviderCommandBuffer() {}
+
+  virtual scoped_ptr<WebGraphicsContext3DCommandBufferImpl>
+  CreateOffscreenContext3d() {
+    RenderThreadImpl* self = RenderThreadImpl::current();
+    DCHECK(self);
+    return self->CreateOffscreenContext3d().Pass();
+  }
+};
+
 RenderThreadImpl::HistogramCustomizer::HistogramCustomizer() {
   custom_histograms_.insert("V8.MemoryExternalFragmentationTotal");
   custom_histograms_.insert("V8.MemoryHeapSampleTotalCommitted");
@@ -426,6 +439,11 @@ RenderThreadImpl::~RenderThreadImpl() {
   // Clean up plugin channels before this thread goes away.
   NPChannelBase::CleanupChannels();
 #endif
+
+  // Leak shared contexts on other threads, as we can not get to the correct
+  // thread to destroy them.
+  if (shared_contexts_compositor_thread_)
+    shared_contexts_compositor_thread_->set_leak_on_destroy();
 }
 
 bool RenderThreadImpl::Send(IPC::Message* msg) {
@@ -926,19 +944,6 @@ RenderThreadImpl::CreateOffscreenContext3d() {
           attributes,
           GURL("chrome://gpu/RenderThreadImpl::CreateOffscreenContext3d")));
 }
-
-class RenderThreadImpl::RendererContextProviderCommandBuffer
-    : public ContextProviderCommandBuffer {
- protected:
-  virtual ~RendererContextProviderCommandBuffer() {}
-
-  virtual scoped_ptr<WebGraphicsContext3DCommandBufferImpl>
-  CreateOffscreenContext3d() {
-    RenderThreadImpl* self = RenderThreadImpl::current();
-    DCHECK(self);
-    return self->CreateOffscreenContext3d().Pass();
-  }
-};
 
 scoped_refptr<ContextProviderCommandBuffer>
 RenderThreadImpl::OffscreenContextProviderForMainThread() {
