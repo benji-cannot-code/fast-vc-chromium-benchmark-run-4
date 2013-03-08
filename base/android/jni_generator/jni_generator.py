c0be4818378bda52090cac929c44cf8a9e47a653
torne@chromium.org <torne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 13:52:50 +0000

    Android: fix race condition in jni_generator.
    Instead of checking if the directory exists and then creating it, just
    try and create it and catch the exception if it already exists. This
    fixes a race that seems to occur when using the Android build system for
    WebView, as it appears to run several instances of jni_generator at
    exactly the same time.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/12461009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186952 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_generator/jni_generator.py
+++ base/android/jni_generator/jni_generator.py
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 If you change this, please run and update the tests."""
 
 import collections
+import errno
 import optparse
 import os
 import re
@@ -950,8 +951,11 @@ def ExtractJarInputFile(jar_file, input_file, out_dir):
   jar_file = zipfile.ZipFile(jar_file)
 
   out_dir = os.path.join(out_dir, os.path.dirname(input_file))
-  if not os.path.exists(out_dir):
+  try:
     os.makedirs(out_dir)
+  except OSError as e:
+    if e.errno != errno.EEXIST:
+      raise
   extracted_file_name = os.path.join(out_dir, os.path.basename(input_file))
   with open(extracted_file_name, 'w') as outfile:
     outfile.write(jar_file.read(input_file))
