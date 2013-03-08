6719062f909fa7ba8db57d5046d8db9ba3eb3496
akling@apple.com <akling@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-08 02:24:34 +0000

    Resizing Cappuccino is very laggy on WebKit since Safari 5.1 <http://webkit.org/b/71354> <rdar://problem/10565998>
    Reviewed by Anders Carlsson.
    
    Source/WebCore:
    
    * WebCore.exp.in: Export FloatPoint(const NSPoint&)
    
    Source/WebKit2:
    
    On Mac, we already cache the window rect through the WindowAndViewFramesChanged
    message, so simply return that in WebChromeClient::windowRect() instead of
    sending a synchronous query to the UIProcess.
    
    If a programmatic resize is requested, we bypass the cache until we've received
    a new WindowAndViewFramesChanged message. This compromise allows us to return
    cached rects in the common case, and handles creating a new window and immediately
    resizing it gracefully.
    
    Changed WindowAndViewFramesChanged to pass FloatRect/FloatPoint rather than
    IntRect/IntPoint to match the ChromeClient::windowRect() return type.
    
    * UIProcess/API/mac/WKView.mm:
    (-[WKView _updateWindowAndViewFrames]):
    * UIProcess/WebPageProxy.h:
    (WebPageProxy):
    * UIProcess/mac/WebPageProxyMac.mm:
    (WebKit::WebPageProxy::windowAndViewFramesChanged):
    * WebProcess/Plugins/PluginView.cpp:
    (WebKit::PluginView::windowAndViewFramesChanged):
    * WebProcess/Plugins/PluginView.h:
    (PluginView):
    * WebProcess/WebCoreSupport/WebChromeClient.cpp:
    (WebKit::WebChromeClient::setWindowRect):
    (WebKit::WebChromeClient::windowRect):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::WebPage):
    (WebKit::WebPage::sendSetWindowFrame):
    (WebKit::WebPage::windowAndViewFramesChanged):
    * WebProcess/WebPage/WebPage.h:
    (WebPage):
    (WebKit::WebPage::windowFrameInScreenCoordinates):
    (WebKit::WebPage::viewFrameInWindowCoordinates):
    (WebKit::WebPage::hasCachedWindowFrame):
    (WebKit::WebPage::accessibilityPosition):
    * WebProcess/WebPage/WebPage.messages.in:
    * WebProcess/WebPage/mac/WKAccessibilityWebPageObject.mm:
    (-[WKAccessibilityWebPageObject accessibilityAttributeValue:]):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145169 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.h
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/WebPage.h
@@ -300,6 +300,8 @@ public:
     void clearMainFrameName();
     void sendClose();
 
+    void sendSetWindowFrame(const WebCore::FloatRect&);
+
     double textZoomFactor() const;
     void setTextZoomFactor(double);
     double pageZoomFactor() const;
@@ -353,8 +355,10 @@ public:
 
     bool windowIsVisible() const { return m_windowIsVisible; }
     void updatePluginsActiveAndFocusedState();
-    const WebCore::IntRect& windowFrameInScreenCoordinates() const { return m_windowFrameInScreenCoordinates; }
-    const WebCore::IntRect& viewFrameInWindowCoordinates() const { return m_viewFrameInWindowCoordinates; }
+    const WebCore::FloatRect& windowFrameInScreenCoordinates() const { return m_windowFrameInScreenCoordinates; }
+    const WebCore::FloatRect& viewFrameInWindowCoordinates() const { return m_viewFrameInWindowCoordinates; }
+
+    bool hasCachedWindowFrame() const { return m_hasCachedWindowFrame; }
 
     void setTopOverhangImage(PassRefPtr<WebImage>);
     void setBottomOverhangImage(PassRefPtr<WebImage>);
@@ -442,7 +446,7 @@ public:
 #if PLATFORM(MAC)
     void registerUIProcessAccessibilityTokens(const CoreIPC::DataReference& elemenToken, const CoreIPC::DataReference& windowToken);
     WKAccessibilityWebPageObject* accessibilityRemoteObject();
-    WebCore::IntPoint accessibilityPosition() const { return m_accessibilityPosition; }    
+    const WebCore::FloatPoint& accessibilityPosition() const { return m_accessibilityPosition; }
     
     void sendComplexTextInputToPlugin(uint64_t pluginComplexTextInputIdentifier, const String& textInput);
 
@@ -733,7 +737,7 @@ private:
     void performDictionaryLookupForRange(DictionaryPopupInfo::Type, WebCore::Frame*, WebCore::Range*, NSDictionary *options);
 
     void setWindowIsVisible(bool windowIsVisible);
-    void windowAndViewFramesChanged(const WebCore::IntRect& windowFrameInScreenCoordinates, const WebCore::IntRect& viewFrameInWindowCoordinates, const WebCore::IntPoint& accessibilityViewCoordinates);
+    void windowAndViewFramesChanged(const WebCore::FloatRect& windowFrameInScreenCoordinates, const WebCore::FloatRect& viewFrameInWindowCoordinates, const WebCore::FloatPoint& accessibilityViewCoordinates);
 
     RetainPtr<PDFDocument> pdfDocumentForPrintingFrame(WebCore::Frame*);
     void computePagesForPrintingPDFDocument(uint64_t frameID, const PrintInfo&, Vector<WebCore::IntRect>& resultPageRects);
@@ -842,17 +846,19 @@ private:
 #if PLATFORM(MAC)
     bool m_pdfPluginEnabled;
 
+    bool m_hasCachedWindowFrame;
+
     // Whether the containing window is visible or not.
     bool m_windowIsVisible;
 
     // The frame of the containing window in screen coordinates.
-    WebCore::IntRect m_windowFrameInScreenCoordinates;
+    WebCore::FloatRect m_windowFrameInScreenCoordinates;
 
     // The frame of the view in window coordinates.
-    WebCore::IntRect m_viewFrameInWindowCoordinates;
+    WebCore::FloatRect m_viewFrameInWindowCoordinates;
 
     // The accessibility position of the view.
-    WebCore::IntPoint m_accessibilityPosition;
+    WebCore::FloatPoint m_accessibilityPosition;
     
     // The layer hosting mode.
     LayerHostingMode m_layerHostingMode;
