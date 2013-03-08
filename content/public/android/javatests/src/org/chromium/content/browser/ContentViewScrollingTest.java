d7b37bf615394725fd7161939527b64c07980dde
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 03:56:28 +0000

    Fix ContentViewScrollingTest
    Needs a page scale factor fix.
    
    BUG=162967
    
    
    Review URL: https://chromiumcodereview.appspot.com/12633002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186876 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewScrollingTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewScrollingTest.java
@@ -5,10 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
-import android.test.FlakyTest;
+import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
+import org.chromium.content.browser.test.util.Criteria;
+import org.chromium.content.browser.test.util.CriteriaHelper;
 import org.chromium.content_shell.ContentShellTestBase;
 
 /*
@@ -17,20 +19,29 @@ import org.chromium.content_shell.ContentShellTestBase;
 public class ContentViewScrollingTest extends ContentShellTestBase {
 
     private static final String LARGE_PAGE = UrlUtils.encodeHtmlDataUri(
-            "<html>" +
-            "<head><style>body { width: 5000px; height: 5000px; }</style></head>" +
+            "<html><head>" +
+            "<meta name=\"viewport\" content=\"width=device-width, " +
+            "initial-scale=1.0, maximum-scale=1.0\" />" +
+            "<style>body { width: 5000px; height: 5000px; }</style></head>" +
             "<body>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</body>" +
             "</html>");
 
-    /*
-     * @SmallTest
-     * @Feature({"Android-WebView"})
-     * BUG 162967
-     */
-    @FlakyTest
+    private void assertWaitForPageScaleFactor(final float scale) throws InterruptedException {
+        assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
+            @Override
+            public boolean isSatisfied() {
+                return getContentViewCore().getScale() == scale;
+            }
+        }));
+    }
+
+    @SmallTest
+    @Feature({"Android-WebView"})
     public void testFling() throws Throwable {
         launchContentShellWithUrl(LARGE_PAGE);
-        waitForActiveShellToBeDoneLoading();
+        assertTrue("Page failed to load", waitForActiveShellToBeDoneLoading());
+        assertWaitForPageScaleFactor(1.0f);
+
         final ContentView view = getActivity().getActiveContentView();
 
         assertEquals(0, view.getContentViewCore().getNativeScrollXForTest());
