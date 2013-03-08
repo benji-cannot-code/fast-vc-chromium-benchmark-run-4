f9b653ede401b041ee2a01a8b5207b0822154ab8
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 04:08:47 +0000

    Get renderer compositor message loop override dynamically
    UI MessageLoop has not started when AwContentRedererClient is first
    constructed. So pass in a getter function instead. This still does not
    *guarantee* message loop will be constructed by the time Override is
    called, but it should be ok since it *should* only be called in the
    renderer. Add a DCHECK for null anyway.
    
    R=joth@chromium.org
    Android only change, ran through android bots.
    NOTRY=true
    BUG=179436
    
    
    Review URL: https://chromiumcodereview.appspot.com/12639002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186882 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/renderer/aw_content_renderer_client.cc
+++ android_webview/renderer/aw_content_renderer_client.cc
@@ -23,8 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace android_webview {
 
 AwContentRendererClient::AwContentRendererClient(
-    MessageLoop* compositor_message_loop)
-    : compositor_message_loop_(compositor_message_loop) {
+    CompositorMessageLoopGetter* compositor_message_loop_getter)
+    : compositor_message_loop_getter_(compositor_message_loop_getter) {
 }
 
 AwContentRendererClient::~AwContentRendererClient() {
@@ -98,7 +98,7 @@ void AwContentRendererClient::PrefetchHostName(const char* hostname,
 }
 
 MessageLoop* AwContentRendererClient::OverrideCompositorMessageLoop() const {
-  return compositor_message_loop_;
+  return (*compositor_message_loop_getter_)();
 }
 
 }  // namespace android_webview
