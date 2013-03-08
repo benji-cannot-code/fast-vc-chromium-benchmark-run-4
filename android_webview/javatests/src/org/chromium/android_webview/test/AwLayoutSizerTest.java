7431a8f6fd70b6b91f251f556db168349d801bff
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 14:03:07 +0000

    Add missing package definition to AwLayoutSizerTest.
    Review URL: https://chromiumcodereview.appspot.com/12630008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186957 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwLayoutSizerTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwLayoutSizerTest.java
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+package org.chromium.android_webview.test;
+
 import android.view.View;
 import android.view.View.MeasureSpec;
 import android.test.InstrumentationTestCase;
@@ -30,10 +32,10 @@ public class AwLayoutSizerTest extends InstrumentationTestCase {
         }
     }
 
-    private final int firstContentWidth = 101;
-    private final int firstContentHeight = 389;
-    private final int secondContentWidth = 103;
-    private final int secondContentHeight = 397;
+    private static final int firstContentWidth = 101;
+    private static final int firstContentHeight = 389;
+    private static final int secondContentWidth = 103;
+    private static final int secondContentHeight = 397;
 
     public void testCanQueryContentSize() {
         LayoutSizerDelegate delegate = new LayoutSizerDelegate();
