62980e7df9e8407d0ef5af8463110ca4f07cb098
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-08 17:12:24 +0000

    [BlackBerry] Add stubs for DNSResolveQueue platform specific methods https://bugs.webkit.org/show_bug.cgi?id=111841
    Patch by Carlos Garcia Campos <cgarcia@igalia.com> on 2013-03-08
    Reviewed by Rob Buis.
    
    Add empty implementations of
    DNSResolveQueue::platformProxyIsEnabledInSystemPreferences() and
    DNSResolveQueue::platformResolve() to DNSBlackBerry.cpp to make it
    build.
    
    * platform/network/blackberry/DNSBlackBerry.cpp:
    (WebCore::DNSResolveQueue::platformProxyIsEnabledInSystemPreferences):
    (WebCore::DNSResolveQueue::platformResolve):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145232 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/blackberry/DNSBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/blackberry/DNSBlackBerry.cpp
@@ -21,12 +21,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define DNSPrefetchBlackBerry_H
 
 #include "config.h"
+#include "DNS.h"
 
+#include "DNSResolveQueue.h"
+#include "NotImplemented.h"
 #include <BlackBerryPlatformCommonFunctions.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/WTFString.h>
 
 namespace WebCore {
+
+bool DNSResolveQueue::platformProxyIsEnabledInSystemPreferences()
+{
+    notImplemented();
+    return false;
+}
+
+void DNSResolveQueue::platformResolve(const String&)
+{
+    notImplemented();
+}
 
 void prefetchDNS(const String& host)
 {
