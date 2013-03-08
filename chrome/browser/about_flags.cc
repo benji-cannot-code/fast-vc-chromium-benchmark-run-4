3e06a2c5e21b71777f447df0debbe8e602a3fa4b
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 23:52:47 +0000

    Add flag to switch Chromium OS Bluetooth backend
    This flag is passed by the OS session manager when it detects the
    newer Bluetooth backend is being used, and selects the alternate
    implementation of BluetoothAdapter.
    
    BUG=chromium-os:38832
    TEST=started chrome with/without flag
    
    
    Review URL: https://chromiumcodereview.appspot.com/12660002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187080 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1264,6 +1264,15 @@ const Experiment kExperiments[] = {
     SINGLE_VALUE_TYPE(switches::kDisableGestureRequirementForMediaPlayback)
   },
 #endif
+#if defined(OS_CHROMEOS)
+  {
+    "enable-experimental-bluetooth",
+    IDS_FLAGS_ENABLE_EXPERIMENTAL_BLUETOOTH_NAME,
+    IDS_FLAGS_ENABLE_EXPERIMENTAL_BLUETOOTH_DESCRIPTION,
+    kOsCrOS,
+    SINGLE_VALUE_TYPE(chromeos::switches::kEnableExperimentalBluetooth)
+  },
+#endif
 };
 
 const Experiment* experiments = kExperiments;
