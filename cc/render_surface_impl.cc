3df21afebc7e33c453d5b71da92eb430743d23da
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 02:41:47 +0000

    cc: Chromify DamageTracker class.
    Style-only change. Make the DamageTracker class use chromium style.
    
    R=jamesr,enne
    
    
    Review URL: https://chromiumcodereview.appspot.com/12631007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186864 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/render_surface_impl.cc
+++ cc/render_surface_impl.cc
@@ -38,7 +38,7 @@ RenderSurfaceImpl::RenderSurfaceImpl(LayerImpl* owningLayer)
     , m_targetRenderSurfaceLayerIndexHistory(0)
     , m_currentLayerIndexHistory(0)
 {
-    m_damageTracker = DamageTracker::create();
+    m_damageTracker = DamageTracker::Create();
 }
 
 RenderSurfaceImpl::~RenderSurfaceImpl()
@@ -86,8 +86,8 @@ void RenderSurfaceImpl::dumpSurface(std::string* str, int indent) const
 
     str->append(indentStr);
     base::StringAppendF(str, "damageRect is pos(%f, %f), size(%f, %f)\n",
-        m_damageTracker->currentDamageRect().x(), m_damageTracker->currentDamageRect().y(),
-        m_damageTracker->currentDamageRect().width(), m_damageTracker->currentDamageRect().height());
+        m_damageTracker->current_damage_rect().x(), m_damageTracker->current_damage_rect().y(),
+        m_damageTracker->current_damage_rect().width(), m_damageTracker->current_damage_rect().height());
 }
 
 int RenderSurfaceImpl::owningLayerId() const
@@ -107,7 +107,7 @@ void RenderSurfaceImpl::setClipRect(const gfx::Rect& clipRect)
 
 bool RenderSurfaceImpl::contentsChanged() const
 {
-    return !m_damageTracker->currentDamageRect().IsEmpty();
+    return !m_damageTracker->current_damage_rect().IsEmpty();
 }
 
 void RenderSurfaceImpl::setContentRect(const gfx::Rect& contentRect)
@@ -165,7 +165,7 @@ void RenderSurfaceImpl::appendRenderPasses(RenderPassSink& passSink)
         m_contributingDelegatedRenderPassLayerList[i]->AppendContributingRenderPasses(&passSink);
 
     scoped_ptr<RenderPass> pass = RenderPass::Create();
-    pass->SetNew(renderPassId(), m_contentRect, m_damageTracker->currentDamageRect(), m_screenSpaceTransform);
+    pass->SetNew(renderPassId(), m_contentRect, m_damageTracker->current_damage_rect(), m_screenSpaceTransform);
     passSink.appendRenderPass(pass.Pass());
 }
 
