0efd20c81d22c4a11f29d4c178441fd46efd23dd
chrisgao@chromium.org <chrisgao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 01:58:50 +0000

    [chromedriver] Fix SyncWebSocketImplTest.Reconnect in pyauto bots.
    One purpose of this test is to check if the message queue of SyncWebSocket is cleared after reconnection.
    The failure is caused by stopping the server right after pushing request message to SyncWebSocket.
    The server has no time to response or even might not receive the request yet.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12586002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186847 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/net/sync_websocket_impl_unittest.cc
+++ chrome/test/chromedriver/net/sync_websocket_impl_unittest.cc
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/message_loop_proxy.h"
 #include "base/single_thread_task_runner.h"
+#include "base/threading/platform_thread.h"
 #include "base/threading/thread.h"
+#include "base/time.h"
 #include "chrome/test/chromedriver/net/sync_websocket_impl.h"
 #include "chrome/test/chromedriver/net/test_http_server.h"
 #include "chrome/test/chromedriver/net/url_request_context_getter.h"
@@ -115,6 +117,14 @@ TEST_F(SyncWebSocketImplTest, Reconnect) {
   SyncWebSocketImpl sock(context_getter_);
   ASSERT_TRUE(sock.Connect(server_.web_socket_url()));
   ASSERT_TRUE(sock.Send("1"));
+  // Wait for SyncWebSocket to receive the response from the server.
+  base::Time deadline = base::Time::Now() + base::TimeDelta::FromSeconds(20);
+  while (base::Time::Now() < deadline) {
+    if (sock.IsConnected() && !sock.HasNextMessage())
+      base::PlatformThread::Sleep(base::TimeDelta::FromMilliseconds(10));
+    else
+      break;
+  }
   server_.Stop();
   ASSERT_FALSE(sock.Send("2"));
   ASSERT_FALSE(sock.IsConnected());
