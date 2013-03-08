b333d60eb0dfeb95a627750937a680d902e0a4c0
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 04:08:31 +0000

    Set IsInitialNavigation to false after commit, not document load.
    BUG=9682
    TEST=Download tests pass.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12637002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186881 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/navigation_controller_impl.h
+++ content/browser/web_contents/navigation_controller_impl.h
@@ -80,7 +80,7 @@ class CONTENT_EXPORT NavigationControllerImpl
   virtual bool NeedsReload() const OVERRIDE;
   virtual void CancelPendingReload() OVERRIDE;
   virtual void ContinuePendingReload() OVERRIDE;
-  virtual bool IsInitialNavigation() OVERRIDE;
+  virtual bool IsInitialNavigation() const OVERRIDE;
   virtual void Reload(bool check_for_repost) OVERRIDE;
   virtual void ReloadIgnoringCache(bool check_for_repost) OVERRIDE;
   virtual void ReloadOriginalRequestURL(bool check_for_repost) OVERRIDE;
@@ -118,9 +118,6 @@ class CONTENT_EXPORT NavigationControllerImpl
   WebContentsImpl* web_contents() const {
     return web_contents_;
   }
-
-  // Called when a document has been loaded in a frame.
-  void DocumentLoadedInFrame();
 
   // For use by WebContentsImpl ------------------------------------------------
 
@@ -381,7 +378,7 @@ class CONTENT_EXPORT NavigationControllerImpl
   bool needs_reload_;
 
   // Whether this is the initial navigation.
-  // Becomes false when initial navigation is loaded.
+  // Becomes false when initial navigation commits.
   bool is_initial_navigation_;
 
   // Used to find the appropriate SessionStorageNamespace for the storage
