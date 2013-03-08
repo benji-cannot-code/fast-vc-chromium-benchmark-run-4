47ad3fbee6e9480702bb4b725bd26aeb8dec9972
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-08 04:36:58 +0000

    notifications: Remove unused webpreferences.h include.
    BUG=146251
    R=mukai@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12634004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@186887 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/balloon_host.cc
+++ chrome/browser/notifications/balloon_host.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/renderer_preferences.h"
 #include "ipc/ipc_message.h"
-#include "webkit/glue/webpreferences.h"
 
 using content::SiteInstance;
 using content::WebContents;
