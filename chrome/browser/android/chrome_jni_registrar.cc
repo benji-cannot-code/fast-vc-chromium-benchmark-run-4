e7babcfba6ca2403ca8fd4d389fb2c1e3bce0d63
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-09 02:29:08 +0000

    Introduce a Java wrapper for PersonalDataManager.
    The idea here is to move model/native logic from the Android
    downstream port into chromium and instead provide a Java API, exposing
    any necessary functions for consumption by the app. This also brings the
    Android code closer to other ports so it's easier for OWNERS to both
    see and review changes.
    
    BUG=169106
    TBR=sky
    
    Review URL: https://chromiumcodereview.appspot.com/12386011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187114 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/android/intent_helper.h"
 #include "chrome/browser/android/process_utils.h"
 #include "chrome/browser/android/provider/chrome_browser_provider.h"
+#include "chrome/browser/autofill/android/personal_data_manager_android.h"
 #include "chrome/browser/history/android/sqlite_cursor.h"
 #include "chrome/browser/lifetime/application_lifetime_android.h"
 #include "chrome/browser/search_engines/template_url_service_android.h"
@@ -55,6 +56,8 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "JavascriptAppModalDialog",
       JavascriptAppModalDialogAndroid::RegisterJavascriptAppModalDialog },
   { "NavigationPopup", NavigationPopup::RegisterNavigationPopup },
+  { "PersonalDataManagerAndroid",
+      PersonalDataManagerAndroid::Register},
   { "ProcessUtils", RegisterProcessUtils },
   { "ProfileSyncService", ProfileSyncServiceAndroid::Register },
   { "SqliteCursor", SQLiteCursor::RegisterSqliteCursor },
