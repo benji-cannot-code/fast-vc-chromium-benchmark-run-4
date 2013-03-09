4d5e3a0e1c7ffe5f5ee45b283f1b164b3d67433d
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-09 05:54:07 +0000

    (CVE-2013-0785) [SECURITY] XSS in show_bug.cgi when using an invalid page format <https://bugzilla.mozilla.org/show_bug.cgi?id=842038> <exp2://Ticket/14465628>
    Applied "patch for 3.6 and 4.0, v1" to bugs.webkit.org.
    
    * Bugzilla/Template.pm:
    (get_format):
    * show_bug.cgi:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145301 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Websites/bugs.webkit.org/Bugzilla/Template.pm
+++ third_party/WebKit/Websites/bugs.webkit.org/Bugzilla/Template.pm
@@ -148,6 +148,7 @@ sub get_format {
     return
     {
         'template'    => $template,
+        'format'      => $format,
         'extension'   => $ctype,
         'ctype'       => Bugzilla::Constants::contenttypes->{$ctype}
     };
