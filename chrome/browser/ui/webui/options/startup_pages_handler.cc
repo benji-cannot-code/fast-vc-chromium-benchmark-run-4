fa148e3563c77d4c3617f08cb37cfb2d9f81f2c6
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-09 14:46:42 +0000

    Really fix crash when dragging and dropping in chrome://settings/startup
    The root cause of this crash was that dropping in the <input> field
    dispatches a DragDropStartupPage event to the C++ handler. Since the
    <input> field doesn't have a corresponding index in the C++ model,
    CustomHomePagesModel would attempt to dereference an index past the end
    of |entries_|. The fix is to suppress drop events on the <input> field
    from triggering the C++ handler, since you can't move the <input> field
    anyway.
    
    Also clean up the code to use ints (and remove conversions to/from
    strings) where appropriate.
    
    BUG=132974
    TBR=jhawkins
    
    Review URL: https://chromiumcodereview.appspot.com/12548008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187153 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/startup_pages_handler.cc
+++ chrome/browser/ui/webui/options/startup_pages_handler.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/prefs/pref_service.h"
-#include "base/strings/string_number_conversions.h"
 #include "chrome/browser/autocomplete/autocomplete_classifier.h"
 #include "chrome/browser/autocomplete/autocomplete_controller.h"
 #include "chrome/browser/autocomplete/autocomplete_input.h"
@@ -114,7 +113,7 @@ void StartupPagesHandler::OnModelChanged() {
     entry->SetString("url", urls[i].spec());
     entry->SetString("tooltip",
                      startup_custom_pages_table_model_->GetTooltip(i));
-    entry->SetString("modelIndex", base::IntToString(i));
+    entry->SetInteger("modelIndex", i);
     startup_pages.Append(entry);
   }
 
@@ -141,11 +140,9 @@ void StartupPagesHandler::SetStartupPagesToCurrentPages(
 
 void StartupPagesHandler::RemoveStartupPages(const ListValue* args) {
   for (int i = args->GetSize() - 1; i >= 0; --i) {
-    std::string string_value;
-    CHECK(args->GetString(i, &string_value));
-
     int selected_index;
-    base::StringToInt(string_value, &selected_index);
+    CHECK(args->GetInteger(i, &selected_index));
+
     if (selected_index < 0 ||
         selected_index >= startup_custom_pages_table_model_->RowCount()) {
       NOTREACHED();
@@ -169,11 +166,9 @@ void StartupPagesHandler::AddStartupPage(const ListValue* args) {
 
 void StartupPagesHandler::EditStartupPage(const ListValue* args) {
   std::string url_string;
-  std::string index_string;
   int index;
   CHECK_EQ(args->GetSize(), 2U);
-  CHECK(args->GetString(0, &index_string));
-  CHECK(base::StringToInt(index_string, &index));
+  CHECK(args->GetInteger(0, &index));
   CHECK(args->GetString(1, &url_string));
 
   if (index < 0 || index > startup_custom_pages_table_model_->RowCount()) {
@@ -189,17 +184,9 @@ void StartupPagesHandler::EditStartupPage(const ListValue* args) {
 void StartupPagesHandler::DragDropStartupPage(const ListValue* args) {
   CHECK_EQ(args->GetSize(), 2U);
 
-  // TODO(dcheng): Due to http://crbug.com/122102, we can receive drag and drop
-  // events even if there are no entries in the model. Remove this check once
-  // that bug is fixed.
-  if (startup_custom_pages_table_model_->RowCount() == 0)
-    return;
-
-  std::string value;
   int to_index;
 
-  CHECK(args->GetString(0, &value));
-  base::StringToInt(value, &to_index);
+  CHECK(args->GetInteger(0, &to_index));
 
   const ListValue* selected;
   CHECK(args->GetList(1, &selected));
@@ -207,8 +194,7 @@ void StartupPagesHandler::DragDropStartupPage(const ListValue* args) {
   std::vector<int> index_list;
   for (size_t i = 0; i < selected->GetSize(); ++i) {
     int index;
-    CHECK(selected->GetString(i, &value));
-    base::StringToInt(value, &index);
+    CHECK(selected->GetInteger(i, &index));
     index_list.push_back(index);
   }
 
