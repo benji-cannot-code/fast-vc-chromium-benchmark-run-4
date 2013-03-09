98e3ce6013c7442a59cc7d6c647ada1e460225f9
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-09 09:50:49 +0000

    [GTK][WK2] Fix compilation warning missing-field-initializers in webkit-2.0 stable branch https://bugs.webkit.org/show_bug.cgi?id=111863
    Patch by Manuel Rego Casasnovas <rego@igalia.com> on 2013-03-09
    Reviewed by Benjamin Poulain.
    
    * UIProcess/API/gtk/WebKitUIClient.cpp:
    (attachUIClientToView): Include missing initializer for pluginLoadPolicy.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145308 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitUIClient.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/gtk/WebKitUIClient.cpp
@@ -209,6 +209,7 @@ void attachUIClientToView(WebKitWebView* webView)
         0, // unavailablePluginButtonClicked
         0, // showColorPicker
         0, // hideColorPicker
+        0, // pluginLoadPolicy
     };
     WKPageRef wkPage = toAPI(webkitWebViewBaseGetPage(WEBKIT_WEB_VIEW_BASE(webView)));
     WKPageSetPageUIClient(wkPage, &wkUIClient);
