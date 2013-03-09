c573ad05a5d7bf9ecd904cb8aa4e9dfee21275f5
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-09 02:29:34 +0000

    Change the animated effect on panels when drawing attention (OSX)
    BUG=125218
    
    
    Review URL: https://chromiumcodereview.appspot.com/12559006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187116 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/panels/panel_titlebar_view_cocoa.h
+++ chrome/browser/ui/cocoa/panels/panel_titlebar_view_cocoa.h
@@ -65,7 +65,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   // "Glint" animation is used in "Draw Attention" mode.
   scoped_nsobject<RepaintAnimation> glintAnimation_;
   scoped_nsobject<NSTimer> glintAnimationTimer_;
-  double glintInterval_;
+  int glintCounter_;
 
   // Drag support.
   scoped_nsobject<MouseDragController> dragController_;
