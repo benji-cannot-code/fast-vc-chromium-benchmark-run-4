32cd93a4441370b1510a6d879a09cbeaa1d88c63
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-10 18:39:38 +0000

    bookmarks: Remove unnecessary includes of profile.h
    BUG=144783
    TBR=sky@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12545029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_utils.cc
+++ chrome/browser/bookmarks/bookmark_utils.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/bookmarks/bookmark_node_data.h"
 #include "chrome/browser/history/query_parser.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/common/pref_names.h"
 #include "components/user_prefs/pref_registry_syncable.h"
 #include "components/user_prefs/user_prefs.h"
