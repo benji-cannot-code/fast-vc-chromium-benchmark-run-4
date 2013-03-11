deb7ebb611175733d4f0e672c6544429a52e548a
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 01:52:41 +0000

    Add C/C++ interfaces for PPB_Ext_Alarms_Dev and PPB_Ext_Events_Dev.
    This CL also adds necessary C++ utilities to support these interfaces.
    
    An example of using these APIs:
    https://codereview.chromium.org/12387051/
    
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/12295043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187227 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_parser.py
+++ ppapi/generators/idl_parser.py
@@ -1171,7 +1171,8 @@ def TestVersionFiles(filter):
   return errs
 
 
-default_dirs = ['.', 'trusted', 'dev', 'private']
+default_dirs = ['.', 'trusted', 'dev', 'private', 'extensions',
+                'extensions/dev']
 def ParseFiles(filenames):
   parser = IDLParser()
   filenodes = []
