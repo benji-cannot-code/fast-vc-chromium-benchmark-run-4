8d6b9c494f6dfea0556551913d9d0af30f6cde9d
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 20:09:03 +0000

    Updated Notifications Galore! test app for notifications API changes.
    Updated this test app to accomodate the API changes committed in
    r12212037 (type -> templateType, button{One,Two}* -> buttons, no more
    onError event) and in r12313115 (templateType -> type,
    chrome.experimental.notification -> chrome.notifications).
    
    Also fixed an issue which caused some image notification's images to not
    show up. The fix involved pre-fetching all images, which is a good idea
    anyway and may be required by the API in the future.
    
    Also increased the app-data separation by having the test notification
    data loaded dynamically. This will allow in the future different test
    data sets to be used with with app and it will allow the apps' test data
    to be changed without an app update. In addition, API calls are now
    fully defined by the test data through "api", "events" and "global"
    properties, so this test app's code will not need to change to adjust to
    changes in the notifications API.
    
    Also added support for generating notifications using the WebKit
    webkitNotifications.createNotification() API.
    
    Finally, also tweaked the user interface to: 1) Have correct cursors
    for dragging/button selection; 2) Allow dragging across notification
    buttons to change button selection; 3) Allow clearing of the events
    log (through an added menu and menu item); and 4) Remember the priority
    setting.
    
    BUG=174680
    R=miket
    
    Review URL: https://codereview.chromium.org/12489004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187353 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/notifications/galore/app/main.js
+++ chrome/test/data/extensions/api_test/notifications/galore/app/main.js
@@ -4,7 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 (function() {
-  var listener = Galore.controller.create.bind(Galore.controller);
+  var controller = Galore.controller.create();
+  var listener = controller.createWindow.bind(controller);
   chrome.app.runtime.onLaunched.addListener(listener);
   chrome.app.runtime.onRestarted.addListener(listener);
 }());
