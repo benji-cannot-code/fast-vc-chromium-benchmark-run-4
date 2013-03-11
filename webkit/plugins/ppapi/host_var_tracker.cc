f1732c12a6034b3eea8e0836f5aabcdd6eadc35e
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 19:33:53 +0000

    PPAPI: Remove threading options; it's always on
    This also re-enables thread checking for the host side resource and var trackers. Before, checking was disabled everywhere.
    
    BUG=159240,92909
    
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=186925
    Reverted: https://src.chromium.org/viewvc/chrome?view=rev&revision=186939 due to build errors
    
    Review URL: https://chromiumcodereview.appspot.com/12378050
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187340 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/host_var_tracker.cc
+++ webkit/plugins/ppapi/host_var_tracker.cc
@@ -17,7 +17,8 @@ using ppapi::NPObjectVar;
 namespace webkit {
 namespace ppapi {
 
-HostVarTracker::HostVarTracker() {
+HostVarTracker::HostVarTracker()
+  : VarTracker(SINGLE_THREADED) {
 }
 
 HostVarTracker::~HostVarTracker() {
@@ -28,7 +29,7 @@ ArrayBufferVar* HostVarTracker::CreateArrayBuffer(uint32 size_in_bytes) {
 }
 
 void HostVarTracker::AddNPObjectVar(NPObjectVar* object_var) {
-  DCHECK(CalledOnValidThread());
+  CheckThreadingPreconditions();
 
   InstanceMap::iterator found_instance = instance_map_.find(
       object_var->pp_instance());
@@ -48,7 +49,7 @@ void HostVarTracker::AddNPObjectVar(NPObjectVar* object_var) {
 }
 
 void HostVarTracker::RemoveNPObjectVar(NPObjectVar* object_var) {
-  DCHECK(CalledOnValidThread());
+  CheckThreadingPreconditions();
 
   InstanceMap::iterator found_instance = instance_map_.find(
       object_var->pp_instance());
@@ -73,7 +74,7 @@ void HostVarTracker::RemoveNPObjectVar(NPObjectVar* object_var) {
 
 NPObjectVar* HostVarTracker::NPObjectVarForNPObject(PP_Instance instance,
                                                     NPObject* np_object) {
-  DCHECK(CalledOnValidThread());
+  CheckThreadingPreconditions();
 
   InstanceMap::iterator found_instance = instance_map_.find(instance);
   if (found_instance == instance_map_.end())
@@ -88,7 +89,7 @@ NPObjectVar* HostVarTracker::NPObjectVarForNPObject(PP_Instance instance,
 }
 
 int HostVarTracker::GetLiveNPObjectVarsForInstance(PP_Instance instance) const {
-  DCHECK(CalledOnValidThread());
+  CheckThreadingPreconditions();
 
   InstanceMap::const_iterator found = instance_map_.find(instance);
   if (found == instance_map_.end())
@@ -97,7 +98,7 @@ int HostVarTracker::GetLiveNPObjectVarsForInstance(PP_Instance instance) const {
 }
 
 void HostVarTracker::DidDeleteInstance(PP_Instance instance) {
-  DCHECK(CalledOnValidThread());
+  CheckThreadingPreconditions();
 
   InstanceMap::iterator found_instance = instance_map_.find(instance);
   if (found_instance == instance_map_.end())
