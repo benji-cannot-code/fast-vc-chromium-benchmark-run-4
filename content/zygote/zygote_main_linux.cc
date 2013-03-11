10b99a7ec9f1f5d65a3728251a6a554515bdaa2d
reed@google.com <reed@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 13:36:35 +0000

    Revert 187283
    memcheck reported errors which need to be investigated
    
    > Call version of SkFontHost_fontconfig in Skia (cloned from here).
    > Retool the direct/remove plumbing to use SkFontConfigInterface.
    > 
    > This change allows Skia to interate on the details of SkFontHost w/o having
    > to synchromize with chrome on each change. It also means Skia's internal testing
    > can exercise exactly the same config that Chrome does.
    > Review URL: https://codereview.chromium.org/12391070
    
    TBR=reed@google.com
    Review URL: https://codereview.chromium.org/12771003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187287 0039d316-1c4b-4281-b951-d872f2087c98

--- content/zygote/zygote_main_linux.cc
+++ content/zygote/zygote_main_linux.cc
@@ -38,8 +38,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "crypto/nss_util.h"
 #include "sandbox/linux/services/libc_urandom_override.h"
 #include "sandbox/linux/suid/client/setuid_sandbox_client.h"
+#include "skia/ext/SkFontHost_fontconfig_control.h"
 #include "third_party/icu/public/i18n/unicode/timezone.h"
-#include "third_party/skia/include/ports/SkFontConfigInterface.h"
 
 #if defined(OS_LINUX)
 #include <sys/epoll.h>
@@ -369,8 +369,8 @@ static bool EnterSandbox(sandbox::SetuidSandboxClient* setuid_sandbox,
     return false;
 
   PreSandboxInit();
-  SkFontConfigInterface::SetGlobal(
-      new FontConfigIPC(Zygote::kMagicSandboxIPCDescriptor))->unref();
+  SkiaFontConfigSetImplementation(
+      new FontConfigIPC(Zygote::kMagicSandboxIPCDescriptor));
 
   if (setuid_sandbox->IsSuidSandboxChild()) {
     // Use the SUID sandbox.  This still allows the seccomp sandbox to
@@ -439,8 +439,8 @@ static bool EnterSandbox(sandbox::SetuidSandboxClient* setuid_sandbox,
     return false;
 
   PreSandboxInit();
-  SkFontConfigInterface::SetGlobal(
-      new FontConfigIPC(Zygote::kMagicSandboxIPCDescriptor)))->unref();
+  SkiaFontConfigSetImplementation(
+      new FontConfigIPC(Zygote::kMagicSandboxIPCDescriptor));
   return true;
 }
 
