90702850162f32851e57c7122ef4047afaa24fb4
dgorbik@apple.com <dgorbik@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 21:06:44 +0000

    Fix build for r145397
    Unreviewed.
    
    * html/shadow/MediaControls.cpp:
    (WebCore::MediaControls::createTextTrackDisplay):
    * html/shadow/MediaControlsChromium.cpp:
    (WebCore::MediaControlsChromium::createTextTrackDisplay):
    * html/shadow/MediaControlsGtk.cpp:
    (WebCore::MediaControlsGtk::createTextTrackDisplay):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145403 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/shadow/MediaControlsGtk.cpp
+++ third_party/WebKit/Source/WebCore/html/shadow/MediaControlsGtk.cpp
@@ -224,7 +224,7 @@ void MediaControlsGtk::createTextTrackDisplay()
         m_textDisplayContainer->setMediaController(m_mediaController);
 
     // Insert it before the first controller element so it always displays behind the controls.
-    insertBefore(textDisplayContainer.release(), m_enclosure, ASSERT_NO_EXCEPTION, true);
+    insertBefore(textDisplayContainer.release(), m_enclosure, ASSERT_NO_EXCEPTION, AttachLazily);
 }
 #endif
 
