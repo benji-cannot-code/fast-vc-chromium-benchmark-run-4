7d1f994da30601e1c60dc689ad93d1b52d737f3f
g.czajkowski@samsung.com <g.czajkowski@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 13:54:11 +0000

    [EFL] Build fix after r143192 when SPELLCHECK is off
    Unreviewed build fix when SPELLCHECK macro is off.
    
    * UIProcess/efl/TextCheckerEfl.cpp:
    (WebKit):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/efl/TextCheckerEfl.cpp
+++ third_party/WebKit/Source/WebKit2/UIProcess/efl/TextCheckerEfl.cpp
@@ -126,7 +126,7 @@ void TextChecker::closeSpellDocumentWithTag(int64_t tag)
 #endif
 }
 
-#if USE(UNIFIED_TEXT_CHECKING)
+#if ENABLE(SPELLCHECK)
 static int nextWordOffset(const UChar* text, int length, int currentOffset)
 {
     // FIXME: avoid creating textIterator object here, it could be passed as a parameter.
@@ -152,7 +152,9 @@ static int nextWordOffset(const UChar* text, int length, int currentOffset)
 
     return wordOffset;
 }
+#endif // ENABLE(SPELLCHECK)
 
+#if USE(UNIFIED_TEXT_CHECKING)
 Vector<TextCheckingResult> TextChecker::checkTextOfParagraph(int64_t spellDocumentTag, const UChar* text, int length, uint64_t checkingTypes)
 {
     Vector<TextCheckingResult> paragraphCheckingResult;
