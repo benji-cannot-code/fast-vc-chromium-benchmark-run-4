a69cc146a7add2a6241b9113bda7c2aa7ae60bab
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 22:01:19 +0000

    Don't overwrite the source_ member of OneClickSigninHelper if its already set to a valid value.  Otherwise, if the user changes the state of the "let me choose what to sync" checkbox during sign in, the code will not detect that there was a chabge.
    BUG=181159
    
    
    Review URL: https://chromiumcodereview.appspot.com/12637009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187387 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_helper.cc
+++ chrome/browser/ui/sync/one_click_signin_helper.cc
@@ -914,6 +914,10 @@ void OneClickSigninHelper::ShowInfoBarUIThread(
 
   if (auto_accept != AUTO_ACCEPT_NONE) {
     helper->auto_accept_ = auto_accept;
+  }
+
+  if (source != SyncPromoUI::SOURCE_UNKNOWN &&
+      helper->source_ == SyncPromoUI::SOURCE_UNKNOWN) {
     helper->source_ = source;
   }
 
