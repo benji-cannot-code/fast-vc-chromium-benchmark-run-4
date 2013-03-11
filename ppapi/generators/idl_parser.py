0d87418fa184590b96f424f11a2f4e1b0a2b960b
yzshen@chromium.org <yzshen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 03:19:56 +0000

    Revert 187227 since it caused buildbot failure in NativeClientSDK on linux-sdk-multi - Add C/C++ interfaces for PPB_Ext_Alarms_Dev and PPB_Ext_Events_Dev.
    This CL also adds necessary C++ utilities to support these interfaces.
    
    An example of using these APIs:
    https://codereview.chromium.org/12387051/
    
    BUG=None
    TEST=None
    
    Review URL: https://codereview.chromium.org/12295043
    
    TBR=yzshen@chromium.org
    Review URL: https://codereview.chromium.org/12760002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187229 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/generators/idl_parser.py
+++ ppapi/generators/idl_parser.py
@@ -1171,8 +1171,7 @@ def TestVersionFiles(filter):
   return errs
 
 
-default_dirs = ['.', 'trusted', 'dev', 'private', 'extensions',
-                'extensions/dev']
+default_dirs = ['.', 'trusted', 'dev', 'private']
 def ParseFiles(filenames):
   parser = IDLParser()
   filenodes = []
