fd5e24a59eaeb0842114fd72e9c91afa9585c6b7
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 20:03:10 +0000

    ChromeClient.h doesn’t need to include RenderSnapshottedPlugIn https://bugs.webkit.org/show_bug.cgi?id=111981
    Reviewed by Kentaro Hara.
    
    * page/ChromeClient.h: Remove the extraneous #include.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145393 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/ChromeClient.h
+++ third_party/WebKit/Source/WebCore/page/ChromeClient.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PopupMenu.h"
 #include "PopupMenuClient.h"
 #include "RenderEmbeddedObject.h"
-#include "RenderSnapshottedPlugIn.h"
 #include "ScrollTypes.h"
 #include "SearchPopupMenu.h"
 #include "WebCoreKeyboardUIMode.h"
