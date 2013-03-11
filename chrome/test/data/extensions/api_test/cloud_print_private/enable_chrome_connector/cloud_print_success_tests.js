ceead5f970675dee2698dfcb989413a10c416f96
noamsml@google.com <noamsml@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 22:12:23 +0000

    Added cloud print private API GetClientId
    Added a cloud print private API called GetClientId which returns the Client ID
    used to create OAuth2 tokens for cloud print services. This will be used in the
    connector.
    
    BUG=174263
    
    
    Review URL: https://chromiumcodereview.appspot.com/12559004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187391 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/cloud_print_private/enable_chrome_connector/cloud_print_success_tests.js
+++ chrome/test/data/extensions/api_test/cloud_print_private/enable_chrome_connector/cloud_print_success_tests.js
@@ -25,6 +25,13 @@ var tests = [
              chrome.test.assertNoLastError();
              chrome.test.assertEq(result, ['printer1', 'printer2']);
           }));
+  },
+  function getClientId() {
+    chrome.cloudPrintPrivate.getClientId(
+        chrome.test.callbackPass(function(result) {
+          chrome.test.assertNoLastError();
+          chrome.test.assertEq("TestAPIClient", result);
+        }));
   }
 ];
 
