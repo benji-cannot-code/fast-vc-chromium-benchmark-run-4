b473191e071ccba10dfc639f6efd87b33020c069
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 19:59:07 +0000

    [wk2] Search With [your search provider here] should use NSPerformService in WebKit2/Mac https://bugs.webkit.org/show_bug.cgi?id=111995 <rdar://problem/12975669>
    Reviewed by Sam Weinig.
    
    * UIProcess/WebPageProxy.h:
    (WebPageProxy): Add searchTheWeb().
    * UIProcess/WebPageProxy.messages.in: Add SearchTheWeb message.
    * UIProcess/mac/WebPageProxyMac.mm:
    (WebKit::WebPageProxy::searchTheWeb): Added. Call out to NSPerformService to search with the system default
    search provider, in the browser.
    
    * WebProcess/Plugins/PDF/PDFPlugin.h:
    (PDFPlugin): Add performWebSearch().
    * WebProcess/Plugins/PDF/PDFPlugin.mm:
    (-[WKPDFLayerControllerDelegate performWebSearch:]): Added, forward the delegate callback to PDFPlugin.
    (WebKit::PDFPlugin::performWebSearch): Added, forward the search string to WebPageProxy.
    * WebProcess/WebCoreSupport/WebContextMenuClient.cpp:
    (WebKit::WebContextMenuClient::searchWithGoogle): Don't build the old load-Google-in-the-main-frame code on Mac.
    * WebProcess/WebCoreSupport/mac/WebContextMenuClientMac.mm:
    (WebKit::WebContextMenuClient::searchWithGoogle): Instead, send the search string to WebPageProxy.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145390 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
+++ third_party/WebKit/Source/WebKit2/UIProcess/WebPageProxy.h
@@ -954,6 +954,8 @@ private:
 
     // Spotlight.
     void searchWithSpotlight(const String&);
+        
+    void searchTheWeb(const String&);
 
     // Dictionary.
     void didPerformDictionaryLookup(const AttributedString&, const DictionaryPopupInfo&);
