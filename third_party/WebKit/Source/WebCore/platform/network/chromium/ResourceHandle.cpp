30cd2919765a62b6c5a99a8792e6b2d7f79ae24e
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 22:32:29 +0000

    Chromium build fix. Forked files strike again.
            * platform/network/chromium/ResourceHandle.cpp:
            (WebCore::ResourceHandle::firstRequest):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145416 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceHandle.cpp
+++ third_party/WebKit/Source/WebCore/platform/network/chromium/ResourceHandle.cpp
@@ -203,7 +203,7 @@ PassRefPtr<ResourceHandle> ResourceHandle::create(NetworkingContext* context,
     return 0;
 }
 
-ResourceRequest& ResourceHandle::firstRequest() const
+ResourceRequest& ResourceHandle::firstRequest()
 {
     return d->request();
 }
