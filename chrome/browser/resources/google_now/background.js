0b61da550331194a841be452bb9e3a4eec015c7d
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 19:36:14 +0000

    Enabling testing Google Now component extension.
    Google Now component extension now can be turned on via chrome://flags. The server name needs to be set manually in JS debugger via local memory.
    
    BUG=164227
    TEST=Enable the extension via chrome://flags, set the server via local memory, restart, make sure cards appear. Check this on CrOS and Windows.
    When this starts working in Canary, set it up to see cards, wait till next day, update Canary to the next version, make sure cards appear on (1) the first start after the update; (2) following starts.
    
    Review URL: https://chromiumcodereview.appspot.com/12508004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187341 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/google_now/background.js
+++ chrome/browser/resources/google_now/background.js
@@ -26,14 +26,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(vadimt): Report internal and server errors. Collect UMAs on errors where
 // appropriate. Also consider logging errors or throwing exceptions.
 
-// TODO(vadimt): Figure out the server name. Use it in the manifest and for
 // TODO(vadimt): Consider processing errors for all storage.set calls.
+// TODO(vadimt): Figure out the server name. Use it in the manifest and for
 // NOTIFICATION_CARDS_URL. Meanwhile, to use the feature, you need to manually
-// edit NOTIFICATION_CARDS_URL before building Chrome.
+// set the server name via local storage.
 /**
  * URL to retrieve notification cards.
  */
-var NOTIFICATION_CARDS_URL = '';
+var NOTIFICATION_CARDS_URL = localStorage['server_url'];
 
 /**
  * Standard response code for successful HTTP requests. This is the only success
