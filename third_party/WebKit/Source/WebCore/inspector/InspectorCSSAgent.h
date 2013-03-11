8c2ab5f1bf326d0ccbe349434277b3fa969a9ef4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 10:56:27 +0000

    Unreviewed, rolling out r145349. http://trac.webkit.org/changeset/145349 https://bugs.webkit.org/show_bug.cgi?id=111966
    Missing code history of Element,PageRuleCollector. (Requested
    by tasak on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-03-11
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.gypi:
    * WebCore.xcodeproj/project.pbxproj:
    * css/CSSAllInOne.cpp:
    * css/DocumentRuleSets.cpp:
    (WebCore::ShadowDistributedRules::collectMatchRequests):
    * css/DocumentRuleSets.h:
    * css/ElementRuleCollector.cpp: Removed.
    * css/ElementRuleCollector.h: Removed.
    * css/PageRuleCollector.cpp: Removed.
    * css/PageRuleCollector.h: Removed.
    * css/StyleResolver.cpp:
    (WebCore::leftToRightDeclaration):
    (WebCore):
    (WebCore::rightToLeftDeclaration):
    (WebCore::StyleResolver::State::ensureRuleList):
    (WebCore::StyleResolver::State::clear):
    (WebCore::StyleResolver::addMatchedProperties):
    (WebCore::StyleResolver::addElementStyleProperties):
    (MatchingUARulesScope):
    (WebCore::MatchingUARulesScope::MatchingUARulesScope):
    (WebCore::MatchingUARulesScope::~MatchingUARulesScope):
    (WebCore::MatchingUARulesScope::isMatchingUARules):
    (WebCore::StyleResolver::collectMatchingRules):
    (WebCore::StyleResolver::collectMatchingRulesForRegion):
    (WebCore::StyleResolver::sortAndTransferMatchedRules):
    (WebCore::StyleResolver::matchScopedAuthorRules):
    (WebCore::StyleResolver::matchHostRules):
    (WebCore::StyleResolver::matchAuthorRules):
    (WebCore::StyleResolver::matchUserRules):
    (WebCore::StyleResolver::matchUARules):
    (WebCore::StyleResolver::collectMatchingRulesForList):
    (WebCore::compareRules):
    (WebCore::StyleResolver::sortMatchedRules):
    (WebCore::StyleResolver::matchAllRules):
    (WebCore::StyleResolver::State::initForStyleResolve):
    (WebCore::StyleResolver::styleSharingCandidateMatchesRuleSet):
    (WebCore::StyleResolver::styleForElement):
    (WebCore::StyleResolver::styleForKeyframe):
    (WebCore::StyleResolver::pseudoStyleForElement):
    (WebCore::StyleResolver::styleForPage):
    (WebCore::StyleResolver::pseudoStyleRulesForElement):
    (WebCore::StyleResolver::ruleMatches):
    (WebCore::StyleResolver::checkRegionSelector):
    (WebCore::comparePageRules):
    (WebCore::StyleResolver::matchPageRules):
    (WebCore::checkPageSelectorComponents):
    (WebCore::StyleResolver::matchPageRulesForList):
    (WebCore::StyleResolver::isLeftPage):
    (WebCore::StyleResolver::isFirstPage):
    (WebCore::StyleResolver::pageName):
    * css/StyleResolver.h:
    (WebCore::MatchRequest::MatchRequest):
    (MatchRequest):
    (StyleResolver):
    (MatchResult):
    (WebCore::StyleResolver::State::State):
    (State):
    (WebCore::StyleResolver::State::takeRuleList):
    (WebCore::StyleResolver::State::setSameOriginOnly):
    (WebCore::StyleResolver::State::isSameOriginOnly):
    (WebCore::StyleResolver::State::pseudoStyleRequest):
    (WebCore::StyleResolver::State::setMode):
    (WebCore::StyleResolver::State::mode):
    (WebCore::StyleResolver::State::matchedRules):
    (WebCore::StyleResolver::State::addMatchedRule):
    * inspector/InspectorCSSAgent.cpp:
    (WebCore::InspectorCSSAgent::willMatchRule):
    * inspector/InspectorCSSAgent.h:
    (WebCore):
    (InspectorCSSAgent):
    * inspector/InspectorInstrumentation.cpp:
    (WebCore):
    (WebCore::InspectorInstrumentation::willMatchRuleImpl):
    * inspector/InspectorInstrumentation.h:
    (WebCore):
    (InspectorInstrumentation):
    (WebCore::InspectorInstrumentation::willMatchRule):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145354 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorCSSAgent.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorCSSAgent.h
@@ -48,9 +48,7 @@ class CSSStyleDeclaration;
 class CSSStyleRule;
 class CSSStyleSheet;
 class Document;
-class DocumentStyleSheetCollection;
 class Element;
-class InspectorCSSOMWrappers;
 class InspectorFrontend;
 class InstrumentingAgents;
 class NameNodeMap;
@@ -128,7 +126,7 @@ public:
     virtual void stopSelectorProfiler(ErrorString*, RefPtr<TypeBuilder::CSS::SelectorProfile>&);
 
     PassRefPtr<TypeBuilder::CSS::SelectorProfile> stopSelectorProfilerImpl(ErrorString*, bool needProfile);
-    void willMatchRule(StyleRule*, InspectorCSSOMWrappers&, DocumentStyleSheetCollection*);
+    void willMatchRule(StyleRule*, StyleResolver*);
     void didMatchRule(bool);
     void willProcessRule(StyleRule*, StyleResolver*);
     void didProcessRule();
