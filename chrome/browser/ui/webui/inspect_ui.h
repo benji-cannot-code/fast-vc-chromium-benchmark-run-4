25787428d152f81aabff23b1819ab356a53a7ede
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 17:37:40 +0000

    DevTools: add backend for discovering connected ADB devices.
    TBR=jhawkins (for minor InspectUI changes)
    
    Review URL: https://chromiumcodereview.appspot.com/12559008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187310 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/inspect_ui.h
+++ chrome/browser/ui/webui/inspect_ui.h
@@ -6,10 +6,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_INSPECT_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_INSPECT_UI_H_
 
+#include <map>
+
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/devtools/devtools_adb_bridge.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/browser/web_ui_controller.h"
+#include "content/public/browser/web_ui_data_source.h"
 
 class InspectUI : public content::WebUIController,
                   public content::NotificationObserver {
@@ -22,6 +27,11 @@ class InspectUI : public content::WebUIController,
  private:
   class WorkerCreationDestructionListener;
 
+  static bool WeakHandleRequestCallback(
+      const base::WeakPtr<InspectUI>& inspect_ui,
+      const std::string& path,
+      const content::WebUIDataSource::GotDataCallback& callback);
+
   // content::NotificationObserver overrides.
   virtual void Observe(int type,
                        const content::NotificationSource& source,
@@ -29,10 +39,33 @@ class InspectUI : public content::WebUIController,
 
   void StopListeningNotifications();
 
+  content::WebUIDataSource* CreateInspectUIHTMLSource();
+
+  bool HandleRequestCallback(
+      const std::string& path,
+      const content::WebUIDataSource::GotDataCallback& callback);
+
+  bool HandleAdbQueryCallback(
+      const std::string& path,
+      const content::WebUIDataSource::GotDataCallback& callback);
+
+  bool HandleLocalXhrCallback(
+      const std::string& path,
+      const content::WebUIDataSource::GotDataCallback& callback);
+
+  void RespondOnUIThread(
+      const content::WebUIDataSource::GotDataCallback& callback,
+      const std::string& error,
+      const std::string& data);
+
+
   scoped_refptr<WorkerCreationDestructionListener> observer_;
 
   // A scoped container for notification registries.
   content::NotificationRegistrar registrar_;
+
+  scoped_refptr<DevToolsAdbBridge> adb_bridge_;
+  base::WeakPtrFactory<InspectUI> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(InspectUI);
 };
