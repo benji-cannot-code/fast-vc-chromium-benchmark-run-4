7c3bdce1c1df349785dd210b28ef2a74dbf77c3d
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-11 20:23:23 +0000

    Add BluetoothAdapterChromeOSExperimental (empty for now)
    BUG=chromium-os:38832
    TEST=chrome --enable-experimental-bluetooth
    
    
    Review URL: https://chromiumcodereview.appspot.com/12612008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187360 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ device/bluetooth/bluetooth_adapter_chromeos_experimental.cc
@@ -0,0 +1,81 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "device/bluetooth/bluetooth_adapter_chromeos_experimental.h"
+
+#include <string>
+
+using device::BluetoothAdapter;
+
+namespace chromeos {
+
+BluetoothAdapterChromeOSExperimental::BluetoothAdapterChromeOSExperimental()
+    : BluetoothAdapter(),
+      weak_ptr_factory_(this) {
+}
+
+BluetoothAdapterChromeOSExperimental::~BluetoothAdapterChromeOSExperimental() {
+}
+
+void BluetoothAdapterChromeOSExperimental::AddObserver(
+    BluetoothAdapter::Observer* observer) {
+}
+
+void BluetoothAdapterChromeOSExperimental::RemoveObserver(
+    BluetoothAdapter::Observer* observer) {
+}
+
+bool BluetoothAdapterChromeOSExperimental::IsInitialized() const {
+  return true;
+}
+
+bool BluetoothAdapterChromeOSExperimental::IsPresent() const {
+  return false;
+}
+
+bool BluetoothAdapterChromeOSExperimental::IsPowered() const {
+  return false;
+}
+
+void BluetoothAdapterChromeOSExperimental::SetPowered(bool powered,
+                                          const base::Closure& callback,
+                                          const ErrorCallback& error_callback) {
+  error_callback.Run();
+}
+
+bool BluetoothAdapterChromeOSExperimental::IsDiscovering() const {
+  return false;
+}
+
+bool BluetoothAdapterChromeOSExperimental::IsScanning() const {
+  return false;
+}
+
+void BluetoothAdapterChromeOSExperimental::StartDiscovering(
+    const base::Closure& callback,
+    const ErrorCallback& error_callback) {
+  error_callback.Run();
+}
+
+void BluetoothAdapterChromeOSExperimental::StopDiscovering(
+    const base::Closure& callback,
+    const ErrorCallback& error_callback) {
+  error_callback.Run();
+}
+
+void BluetoothAdapterChromeOSExperimental::ReadLocalOutOfBandPairingData(
+    const BluetoothAdapter::BluetoothOutOfBandPairingDataCallback& callback,
+    const ErrorCallback& error_callback) {
+  error_callback.Run();
+}
+
+void BluetoothAdapterChromeOSExperimental::TrackDefaultAdapter() {
+}
+
+void BluetoothAdapterChromeOSExperimental::FindAdapter(
+    const std::string& address) {
+}
+
+}  // namespace chromeos
