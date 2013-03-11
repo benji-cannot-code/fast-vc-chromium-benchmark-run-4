296217a9fff252ec0753b5c30d819b231235a239
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-11 22:25:51 +0000

    [BlackBerry] Add a method to find the ScrollView for a text selection. https://bugs.webkit.org/show_bug.cgi?id=112061
    Patch by Genevieve Mak <gmak@rim.com> on 2013-03-11
    Reviewed by Rob Buis.
    
    Internally reviewed by Mike Fenton and Jakob Petsovits.
    PR #278490
    
    Refactor the code in InRegionScroller so that we can add a method to find
    the InRegionScrollableArea for a text selection.
    
    * Api/InRegionScroller.cpp:
    (BlackBerry::WebKit::InRegionScrollerPrivate::clipToRect):
        - Pull out recursive clipping code into its own function.
    (WebKit):
    (BlackBerry::WebKit::InRegionScrollerPrivate::calculateInRegionScrollableAreasForPoint):
    (BlackBerry::WebKit::InRegionScrollerPrivate::firstScrollableInRegionForNode):
        - This new method allows us to get just the first scrollable region instead of looking
          for more nested ones. We may want to refactor in the future because some of the code is
          very similar to calculateInRegionScrollableAreasForPoint.
    (BlackBerry::WebKit::InRegionScrollerPrivate::clipAndCreateInRegionScrollableArea):
        - Add a convenience method for the previous function
    * Api/InRegionScroller.h:
    (WebKit):
    * Api/InRegionScroller_p.h:
    (InRegionScrollerPrivate):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145415 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/Api/InRegionScroller_p.h
+++ third_party/WebKit/Source/WebKit/blackberry/Api/InRegionScroller_p.h
@@ -54,6 +54,7 @@ public:
 
     void calculateInRegionScrollableAreasForPoint(const WebCore::IntPoint&);
     const std::vector<Platform::ScrollViewBase*>& activeInRegionScrollableAreas() const;
+    Platform::ScrollViewBase* firstScrollableInRegionForNode(const WebCore::Node*);
 
     void clearDocumentData(const WebCore::Document*);
 
@@ -70,9 +71,11 @@ private:
     void pushBackInRegionScrollable(InRegionScrollableArea*);
 
     void adjustScrollDelta(const WebCore::IntPoint& maxOffset, const WebCore::IntPoint& currentOffset, WebCore::IntSize& delta) const;
+    Platform::ScrollViewBase* clipAndCreateInRegionScrollableArea(WebCore::RenderLayer*);
 
     bool isValidScrollableLayerWebKitThread(WebCore::LayerWebKitThread*) const;
     bool isValidScrollableNode(WebCore::Node*) const;
+    WebCore::IntRect clipToRect(const WebCore::IntRect&, InRegionScrollableArea*);
     std::vector<Platform::ScrollViewBase*> m_activeInRegionScrollableAreas;
 };
 
