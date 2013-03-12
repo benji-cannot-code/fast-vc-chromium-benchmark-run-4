dfc7bf695945ab3037c5d318fe4ed5c0c67d47d0
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 22:58:34 +0000

    PPAPI: Make ImageDataCache OnTimer run with the lock
    BUG=
    
    Review URL: https://codereview.chromium.org/12660008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187681 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/proxy/ppb_image_data_proxy.cc
+++ ppapi/proxy/ppb_image_data_proxy.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/proxy/plugin_resource_tracker.h"
 #include "ppapi/proxy/ppapi_messages.h"
 #include "ppapi/shared_impl/host_resource.h"
+#include "ppapi/shared_impl/proxy_lock.h"
 #include "ppapi/shared_impl/resource.h"
 #include "ppapi/thunk/enter.h"
 #include "ppapi/thunk/thunk.h"
@@ -276,9 +277,9 @@ void ImageDataCache::Add(ImageData* image_data) {
   // Schedule a timer to invalidate this entry.
   MessageLoop::current()->PostDelayedTask(
       FROM_HERE,
-      base::Bind(&ImageDataCache::OnTimer,
-                 weak_factory_.GetWeakPtr(),
-                 image_data->pp_instance()),
+      RunWhileLocked(base::Bind(&ImageDataCache::OnTimer,
+                     weak_factory_.GetWeakPtr(),
+                     image_data->pp_instance())),
       base::TimeDelta::FromSeconds(kMaxAgeSeconds));
 }
 
