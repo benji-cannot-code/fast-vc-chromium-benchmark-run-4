70e2d41a9d0f9ab2d62b9bdc322dfd3ebbb4e8e6
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 19:14:48 +0000

    cc: Chromify util.h
    NOTRY=true
    R=danakj@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12796002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187627 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/util.h
+++ cc/util.h
@@ -8,10 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-template<typename T> 
-T RoundUp(T n, T mul)
-{
-    return ((n + mul - 1) / mul) * mul;
+template <typename T> T RoundUp(T n, T mul) {
+  return ((n + mul - 1) / mul) * mul;
 }
 
 }  // namespace cc
