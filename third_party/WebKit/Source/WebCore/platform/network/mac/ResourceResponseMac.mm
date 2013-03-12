3ff751f396b89e27b8c3c3ef70234f78b721d490
ap@apple.com <ap@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-12 21:10:45 +0000

    Improve logging in ResourceHandleMac.mm         https://bugs.webkit.org/show_bug.cgi?id=112185
            Reviewed by Brady Eidson.
    
            * platform/network/mac/ResourceHandleMac.mm:
            (WebCore::ResourceHandle::didReceiveAuthenticationChallenge): Removed an obsolete #if.
            (WebCore::ResourceHandle::receivedCredential):
            (WebCore::ResourceHandle::receivedRequestToContinueWithoutCredential):
            (WebCore::ResourceHandle::receivedCancellation):
            (-[WebCoreResourceHandleAsDelegate connection:didCancelAuthenticationChallenge:]): Added a FIXME.
            (-[WebCoreResourceHandleAsDelegate connection:canAuthenticateAgainstProtectionSpace:]):
    
            * platform/network/mac/ResourceResponseMac.mm: Removed an unneeded include.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/mac/ResourceResponseMac.mm
+++ third_party/WebKit/Source/WebCore/platform/network/mac/ResourceResponseMac.mm
@@ -31,9 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "WebCoreURLResponse.h"
 #import "WebCoreSystemInterface.h"
 #import <Foundation/Foundation.h>
-#import <wtf/StdLibExtras.h>
 #import <limits>
-#include <wtf/text/CString.h>
+#import <wtf/StdLibExtras.h>
 
 using namespace std;
 
