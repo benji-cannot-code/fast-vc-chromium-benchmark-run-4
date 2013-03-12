fe9cef0531401c5280b635660ffb13045cc7d065
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 02:49:39 +0000

    Android: Enable --composite-to-mailbox
    BUG=181640
    
    Review URL: https://chromiumcodereview.appspot.com/12734002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187465 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_android.cc
+++ chrome/browser/chrome_browser_main_android.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/path_service.h"
+#include "cc/switches.h"
 #include "chrome/app/breakpad_linux.h"
 #include "chrome/browser/android/crash_dump_manager.h"
 #include "chrome/common/chrome_switches.h"
@@ -61,6 +62,9 @@ void ChromeBrowserMainPartsAndroid::PreEarlyInitialization() {
   DCHECK(!main_message_loop_.get());
   main_message_loop_.reset(new MessageLoop(MessageLoop::TYPE_UI));
   MessageLoopForUI::current()->Start();
+
+  CommandLine::ForCurrentProcess()->AppendSwitch(
+      cc::switches::kCompositeToMailbox);
 
   ChromeBrowserMainParts::PreEarlyInitialization();
 }
