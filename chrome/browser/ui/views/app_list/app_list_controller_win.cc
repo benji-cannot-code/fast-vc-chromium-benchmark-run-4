671314b752cf4f22185b28fc4025856d16e82a0a
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 03:00:01 +0000

    Remove unused header from app_list_controller_win.cc.
    ash/shell.h is unused since http://crrev.com/184894.
    
    BUG=None
    TEST=Still compiles
    
    
    Review URL: https://chromiumcodereview.appspot.com/12385072
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187469 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/app_list/app_list_controller_win.cc
+++ chrome/browser/ui/views/app_list/app_list_controller_win.cc
@@ -59,8 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/widget.h"
 #include "win8/util/win8_util.h"
 
-#if defined(USE_ASH)
-#include "ash/shell.h"
+#if defined(USE_AURA)
 #include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
 #endif
