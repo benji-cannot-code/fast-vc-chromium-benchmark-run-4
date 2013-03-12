7659f9bce7d59e3379403e3762e1a585cc65a5e4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-12 15:17:58 +0000

    [BlackBerry] DisplayRefreshMonitor: rename m_timestamp back to m_monotonicAnimationStartTime https://bugs.webkit.org/show_bug.cgi?id=112139
    Patch by Alberto Garcia <agarcia@igalia.com> on 2013-03-12
    Reviewed by Rob Buis.
    
    This changed in r131131, but it was accidentally renamed back in r144465.
    
    * platform/graphics/blackberry/DisplayRefreshMonitorBlackBerry.cpp:
    (WebCore::DisplayRefreshMonitor::displayLinkFired):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145547 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/blackberry/DisplayRefreshMonitorBlackBerry.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/blackberry/DisplayRefreshMonitorBlackBerry.cpp
@@ -85,7 +85,7 @@ void DisplayRefreshMonitor::displayLinkFired()
 
     m_previousFrameDone = false;
 
-    m_timestamp = currentTime();
+    m_monotonicAnimationStartTime = monotonicallyIncreasingTime();
 
     callOnMainThread(handleDisplayRefreshedNotificationOnMainThread, this);
     m_mutex.unlock();
