94d309b8d02248879396fcd143e4e4e3572c4212
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-12 01:08:34 +0000

    Hit testing should use ancestorInThisScope to get the non-shadow ancestor https://bugs.webkit.org/show_bug.cgi?id=112068
    Patch by Christian Biesinger <cbiesinger@chromium.org> on 2013-03-11
    Reviewed by Dimitri Glazkov.
    
    Source/WebCore:
    
    Tests: media/nodesFromRect-shadowContent.html
    
    * rendering/HitTestResult.cpp:
    (WebCore::HitTestResult::setToNonShadowAncestor):
    (WebCore::HitTestResult::addNodeToRectBasedTestResult):
    Use ancestorInThisScope which is not deprecated and which produces
    correct results for nested shadow trees (e.g. in case of <video>,
    which contains <input> elements for the controls, which themselves
    have shadow trees)
    
    LayoutTests:
    
    * media/nodesFromRect-shadowContent.html: Extend the hit testing rect
    to guarantee reaching a nested shadow tree.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145448 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/HitTestResult.cpp
+++ third_party/WebKit/Source/WebCore/rendering/HitTestResult.cpp
@@ -116,11 +116,11 @@ void HitTestResult::setToNonShadowAncestor()
 {
     Node* node = innerNode();
     if (node)
-        node = node->deprecatedShadowAncestorNode();
+        node = node->document()->ancestorInThisScope(node);
     setInnerNode(node);
     node = innerNonSharedNode();
     if (node)
-        node = node->deprecatedShadowAncestorNode();
+        node = node->document()->ancestorInThisScope(node);
     setInnerNonSharedNode(node);
 }
 
@@ -558,7 +558,7 @@ bool HitTestResult::addNodeToRectBasedTestResult(Node* node, const HitTestReques
         return true;
 
     if (!request.allowsShadowContent())
-        node = node->deprecatedShadowAncestorNode();
+        node = node->document()->ancestorInThisScope(node);
 
     mutableRectBasedTestResult().add(node);
 
@@ -578,7 +578,7 @@ bool HitTestResult::addNodeToRectBasedTestResult(Node* node, const HitTestReques
         return true;
 
     if (!request.allowsShadowContent())
-        node = node->deprecatedShadowAncestorNode();
+        node = node->document()->ancestorInThisScope(node);
 
     mutableRectBasedTestResult().add(node);
 
