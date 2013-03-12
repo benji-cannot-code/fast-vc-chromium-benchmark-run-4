4a8ee69061093a488ce2075f22b6aeeee7ea9792
kaiwang@chromium.org <kaiwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 01:05:35 +0000

    Remove NOTIFICATION_AUTOFILL_CREDIT_CARD_CHANGED that is never observed
    BUG=181277
    
    
    Review URL: https://chromiumcodereview.appspot.com/12648003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187439 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/web_data_service_unittest.cc
+++ chrome/browser/webdata/web_data_service_unittest.cc
@@ -65,9 +65,6 @@ class AutofillDBThreadObserverHelper : public DBThreadObserverHelper {
     registrar_.Add(&observer_,
                    chrome::NOTIFICATION_AUTOFILL_PROFILE_CHANGED,
                    content::NotificationService::AllSources());
-    registrar_.Add(&observer_,
-                   chrome::NOTIFICATION_AUTOFILL_CREDIT_CARD_CHANGED,
-                   content::NotificationService::AllSources());
   }
 };
 
@@ -87,25 +84,28 @@ class WebDataServiceTest : public testing::Test {
     wdbs_->LoadDatabase(WebDatabaseService::InitCallback());
     wds_ = new WebDataService(wdbs_.get());
     wds_->Init();
-    base::WaitableEvent done(false, false);
-    BrowserThread::PostTask(BrowserThread::DB, FROM_HERE,
-        base::Bind(&base::WaitableEvent::Signal, base::Unretained(&done)));
-    done.Wait();
 
+    WaitForDatabaseThread();
   }
 
   virtual void TearDown() {
     wdbs_.reset(NULL);
     wds_->ShutdownOnUIThread();
     wds_ = NULL;
-    base::WaitableEvent done(false, false);
-    BrowserThread::PostTask(BrowserThread::DB, FROM_HERE,
-        base::Bind(&base::WaitableEvent::Signal, base::Unretained(&done)));
-    done.Wait();
+    WaitForDatabaseThread();
 
     db_thread_.Stop();
     MessageLoop::current()->PostTask(FROM_HERE, MessageLoop::QuitClosure());
     MessageLoop::current()->Run();
+  }
+
+  void WaitForDatabaseThread() {
+    base::WaitableEvent done(false, false);
+    BrowserThread::PostTask(
+        BrowserThread::DB,
+        FROM_HERE,
+        base::Bind(&base::WaitableEvent::Signal, base::Unretained(&done)));
+    done.Wait();
   }
 
   MessageLoopForUI message_loop_;
@@ -412,20 +412,8 @@ TEST_F(WebDataServiceAutofillTest, ProfileUpdate) {
 
 TEST_F(WebDataServiceAutofillTest, CreditAdd) {
   CreditCard card;
-  const AutofillCreditCardChange expected_change(
-      AutofillCreditCardChange::ADD, card.guid(), &card);
-
-  EXPECT_CALL(
-      *observer_helper_->observer(),
-      Observe(
-          int(chrome::NOTIFICATION_AUTOFILL_CREDIT_CARD_CHANGED),
-              content::Source<WebDataService>(wds_.get()),
-              Property(&content::Details<const AutofillCreditCardChange>::ptr,
-                       Pointee(expected_change)))).
-      WillOnce(SignalEvent(&done_event_));
-
   wds_->AddCreditCard(card);
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that it was added.
   AutofillWebDataServiceConsumer<std::vector<CreditCard*> > consumer;
@@ -441,10 +429,8 @@ TEST_F(WebDataServiceAutofillTest, CreditCardRemove) {
   CreditCard credit_card;
 
   // Add a credit card.
-  EXPECT_CALL(*observer_helper_->observer(), Observe(_, _, _)).
-      WillOnce(SignalEvent(&done_event_));
   wds_->AddCreditCard(credit_card);
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that it was added.
   AutofillWebDataServiceConsumer<std::vector<CreditCard*> > consumer;
@@ -456,18 +442,8 @@ TEST_F(WebDataServiceAutofillTest, CreditCardRemove) {
   STLDeleteElements(&consumer.result());
 
   // Remove the credit card.
-  const AutofillCreditCardChange expected_change(
-      AutofillCreditCardChange::REMOVE, credit_card.guid(), NULL);
-  EXPECT_CALL(
-      *observer_helper_->observer(),
-      Observe(
-          int(chrome::NOTIFICATION_AUTOFILL_CREDIT_CARD_CHANGED),
-              content::Source<WebDataService>(wds_.get()),
-              Property(&content::Details<const AutofillCreditCardChange>::ptr,
-                       Pointee(expected_change)))).
-      WillOnce(SignalEvent(&done_event_));
   wds_->RemoveCreditCard(credit_card.guid());
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that it was removed.
   AutofillWebDataServiceConsumer<std::vector<CreditCard*> > consumer2;
@@ -483,13 +459,9 @@ TEST_F(WebDataServiceAutofillTest, CreditUpdate) {
   CreditCard card2;
   card2.SetRawInfo(CREDIT_CARD_NAME, ASCIIToUTF16("Alice"));
 
-  EXPECT_CALL(*observer_helper_->observer(), Observe(_, _, _)).
-      Times(2).
-      WillOnce(DoDefault()).
-      WillOnce(SignalEvent(&done_event_));
   wds_->AddCreditCard(card1);
   wds_->AddCreditCard(card2);
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that they got added.
   AutofillWebDataServiceConsumer<std::vector<CreditCard*> > consumer;
@@ -503,20 +475,9 @@ TEST_F(WebDataServiceAutofillTest, CreditUpdate) {
 
   CreditCard card1_changed(card1);
   card1_changed.SetRawInfo(CREDIT_CARD_NAME, ASCIIToUTF16("Bill"));
-  const AutofillCreditCardChange expected_change(
-      AutofillCreditCardChange::UPDATE, card1.guid(), &card1_changed);
-
-  EXPECT_CALL(
-      *observer_helper_->observer(),
-      Observe(
-          int(chrome::NOTIFICATION_AUTOFILL_CREDIT_CARD_CHANGED),
-              content::Source<WebDataService>(wds_.get()),
-              Property(&content::Details<const AutofillCreditCardChange>::ptr,
-                       Pointee(expected_change)))).
-      WillOnce(SignalEvent(&done_event_));
 
   wds_->UpdateCreditCard(card1_changed);
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that the updates were made.
   AutofillWebDataServiceConsumer<std::vector<CreditCard*> > consumer2;
@@ -550,15 +511,12 @@ TEST_F(WebDataServiceAutofillTest, AutofillRemoveModifiedBetween) {
   STLDeleteElements(&profile_consumer.result());
 
   // Add a credit card.
-  EXPECT_CALL(*observer_helper_->observer(), Observe(_, _, _)).
-      WillOnce(SignalEvent(&done_event_));
   CreditCard credit_card;
   wds_->AddCreditCard(credit_card);
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that it was added.
-  AutofillWebDataServiceConsumer<std::vector<CreditCard*> >
-      card_consumer;
+  AutofillWebDataServiceConsumer<std::vector<CreditCard*> > card_consumer;
   handle = wds_->GetCreditCards(&card_consumer);
   MessageLoop::current()->Run();
   EXPECT_EQ(handle, card_consumer.handle());
@@ -577,22 +535,10 @@ TEST_F(WebDataServiceAutofillTest, AutofillRemoveModifiedBetween) {
                        Pointee(expected_profile_change)))).
       WillOnce(SignalEvent(&done_event_));
 
-  // Check that GUID-based notification was sent for the credit card.
-  const AutofillCreditCardChange expected_card_change(
-      AutofillCreditCardChange::REMOVE, credit_card.guid(), NULL);
-  EXPECT_CALL(
-      *observer_helper_->observer(),
-      Observe(
-          int(chrome::NOTIFICATION_AUTOFILL_CREDIT_CARD_CHANGED),
-              content::Source<WebDataService>(wds_.get()),
-              Property(&content::Details<const AutofillCreditCardChange>::ptr,
-                       Pointee(expected_card_change)))).
-      WillOnce(SignalEvent(&done_event_));
-
   // Remove the profile using time range of "all time".
   wds_->RemoveAutofillProfilesAndCreditCardsModifiedBetween(Time(), Time());
   done_event_.TimedWait(test_timeout_);
-  done_event_.TimedWait(test_timeout_);
+  WaitForDatabaseThread();
 
   // Check that the profile was removed.
   AutofillWebDataServiceConsumer<std::vector<AutofillProfile*> >
@@ -604,8 +550,7 @@ TEST_F(WebDataServiceAutofillTest, AutofillRemoveModifiedBetween) {
   ASSERT_EQ(0U, profile_consumer2.result().size());
 
   // Check that the credit card was removed.
-  AutofillWebDataServiceConsumer<std::vector<CreditCard*> >
-      card_consumer2;
+  AutofillWebDataServiceConsumer<std::vector<CreditCard*> > card_consumer2;
   handle2 = wds_->GetCreditCards(&card_consumer2);
   MessageLoop::current()->Run();
   EXPECT_EQ(handle2, card_consumer2.handle());
