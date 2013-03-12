9a7550e25dd5491bb9e436fc38352d5409dfc47b
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-12 22:21:28 +0000

    Replace static_casts with to* helper functions. https://bugs.webkit.org/show_bug.cgi?id=112164
    Reviewed by Philip Rogers.
    
    to* helper functions are preferred over static_cast calls since they
    help to catch bad casts easily on the testing infrastructure.
    
    Source/WebCore:
    
    * css/ElementRuleCollector.cpp:
    (WebCore::ElementRuleCollector::collectMatchingRulesForRegion):
    * platform/gtk/PasteboardGtk.cpp:
    (WebCore::getURLForImageNode):
    (WebCore::Pasteboard::writeImage):
    * platform/gtk/RenderThemeGtk.cpp:
    (WebCore::getMediaElementFromRenderObject):
    * platform/qt/RenderThemeQtMobile.cpp:
    (WebCore::RenderThemeQtMobile::checkMultiple):
    * rendering/HitTestResult.cpp:
    (WebCore::HitTestResult::title):
    (WebCore::HitTestResult::absoluteImageURL):
    (WebCore::HitTestResult::absolutePDFURL):
    * rendering/RenderLayer.cpp:
    (WebCore::RenderLayer::resize):
    (WebCore::RenderLayer::hitTest):
    * rendering/RenderMenuList.cpp:
    (WebCore::RenderMenuList::valueChanged):
    * rendering/RenderObject.cpp:
    (WebCore::RenderObject::addPDFURLRect):
    * rendering/RenderPart.cpp:
    (WebCore::RenderPart::requiresAcceleratedCompositing):
    * rendering/RenderRegion.cpp:
    (WebCore::RenderRegion::checkRegionStyle):
    * rendering/RenderTableCell.cpp:
    (WebCore::RenderTableCell::computePreferredLogicalWidths):
    * rendering/RenderTheme.cpp:
    (WebCore::RenderTheme::isEnabled):
    (WebCore::RenderTheme::isSpinUpButtonPartPressed):
    (WebCore::RenderTheme::isHovered):
    (WebCore::RenderTheme::isSpinUpButtonPartHovered):
    (WebCore::RenderTheme::paintSliderTicks):
    * rendering/RenderThemeWinCE.cpp:
    (WebCore::mediaElementParent):
    * rendering/RenderTreeAsText.cpp:
    (WebCore::isEmptyOrUnstyledAppleStyleSpan):
    (WebCore::RenderTreeAsText::writeRenderObject):
    (WebCore::writeRenderRegionList):
    * rendering/svg/SVGRenderTreeAsText.cpp:
    (WebCore::writeSVGResourceContainer):
    
    Source/WebKit/win:
    
    * DOMHTMLClasses.cpp:
    (DOMHTMLElement::idName):
    (DOMHTMLElement::innerText):
    (DOMHTMLElement::setInnerText):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderPart.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderPart.cpp
@@ -87,7 +87,7 @@ bool RenderPart::requiresAcceleratedCompositing() const
     if (!node() || !node()->isFrameOwnerElement())
         return false;
 
-    HTMLFrameOwnerElement* element = static_cast<HTMLFrameOwnerElement*>(node());
+    HTMLFrameOwnerElement* element = toFrameOwnerElement(node());
     if (Document* contentDocument = element->contentDocument()) {
         if (RenderView* view = contentDocument->renderView())
             return view->usesCompositing();
--- third_party/WebKit/Source/WebCore/rendering/RenderTreeAsText.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderTreeAsText.cpp
@@ -169,7 +169,7 @@ static bool isEmptyOrUnstyledAppleStyleSpan(const Node* node)
     if (!node || !node->isHTMLElement() || !node->hasTagName(spanTag))
         return false;
 
-    const HTMLElement* elem = static_cast<const HTMLElement*>(node);
+    const HTMLElement* elem = toHTMLElement(node);
     if (elem->getAttribute(classAttr) != "Apple-style-span")
         return false;
 
@@ -431,7 +431,7 @@ void RenderTreeAsText::writeRenderObject(TextStream& ts, const RenderObject& o, 
     if (behavior & RenderAsTextShowIDAndClass) {
         if (Node* node = o.node()) {
             if (node->hasID())
-                ts << " id=\"" + static_cast<Element*>(node)->getIdAttribute() + "\"";
+                ts << " id=\"" + toElement(node)->getIdAttribute() + "\"";
 
             if (node->hasClass()) {
                 ts << " class=\"";
@@ -670,7 +670,7 @@ static void writeRenderRegionList(const RenderRegionList& flowThreadRegionList, 
             if (!tagName.isEmpty())
                 ts << " {" << tagName << "}";
             if (renderRegion->generatingNode()->isElementNode() && renderRegion->generatingNode()->hasID()) {
-                Element* element = static_cast<Element*>(renderRegion->generatingNode());
+                Element* element = toElement(renderRegion->generatingNode());
                 ts << " #" << element->idForStyleResolution();
             }
             if (renderRegion->hasCustomRegionStyle())
