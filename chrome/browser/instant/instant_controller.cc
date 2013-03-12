4aa3df0457c4f82590064fec0b86cc6a769f0c89
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 00:52:53 +0000

    Themes don't work correctly on NTP
    Initializes the InstantService when a new InstantController
    is created.  This ensures that the ThemeSource is correctly
    installed prior to NTP appearing (which requires theme images).
    
    BUG=181428
    TEST=Manual, as per bug.
    R=sreeram@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12755007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187432 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.cc
+++ chrome/browser/instant/instant_controller.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/top_sites.h"
 #include "chrome/browser/instant/instant_ntp.h"
 #include "chrome/browser/instant/instant_overlay.h"
+#include "chrome/browser/instant/instant_service_factory.h"
 #include "chrome/browser/instant/instant_tab.h"
 #include "chrome/browser/instant/search.h"
 #include "chrome/browser/platform_util.h"
@@ -213,6 +214,11 @@ InstantController::InstantController(chrome::BrowserInstantController* browser,
       omnibox_bounds_(-1, -1, 0, 0),
       allow_overlay_to_show_search_suggestions_(false),
       ALLOW_THIS_IN_INITIALIZER_LIST(weak_ptr_factory_(this)) {
+
+  // When the InstantController lives, the InstantService should live.
+  // InstantService sets up profile-level facilities such as the ThemeSource for
+  // the NTP.
+  InstantServiceFactory::GetForProfile(browser_->profile());
 }
 
 InstantController::~InstantController() {
