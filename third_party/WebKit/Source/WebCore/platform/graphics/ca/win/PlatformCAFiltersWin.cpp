d2987913b6c3cd56fb845cb869ad9ab71ebd7c6e
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-12 06:20:19 +0000

    We'll get there eventually.
    * platform/graphics/ca/win/PlatformCAFiltersWin.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145486 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/win/PlatformCAFiltersWin.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/win/PlatformCAFiltersWin.cpp
@@ -29,7 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if USE(ACCELERATED_COMPOSITING)
 #if ENABLE(CSS_FILTERS)
 
-#import "PlatformCAFilters.h"
+#include "PlatformCAFilters.h"
 
 using namespace WebCore;
 
