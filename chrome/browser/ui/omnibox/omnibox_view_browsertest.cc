4856d158f59dcb66fbaf7f46a4167385fedd4874
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-12 14:26:49 +0000

    Fix flaky OmniboxViewTest.IncognitoCopy* tests
    BUG=171974
    
    
    Review URL: https://chromiumcodereview.appspot.com/12498009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187587 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
+++ chrome/browser/ui/omnibox/omnibox_view_browsertest.cc
@@ -1711,8 +1711,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, CopyURLToClipboard) {
 #endif
 }
 
-// Disabled due to flakiness.  http://crbug.com/171974
-IN_PROC_BROWSER_TEST_F(OmniboxViewTest, DISABLED_IncognitoCopyURLToClipboard) {
+IN_PROC_BROWSER_TEST_F(OmniboxViewTest, IncognitoCopyURLToClipboard) {
   EXPECT_FALSE(browser()->profile()->IsOffTheRecord());
   Browser* browser_incognito = CreateIncognitoBrowser();
   ui_test_utils::NavigateToURL(browser_incognito,
@@ -1740,9 +1739,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, DISABLED_IncognitoCopyURLToClipboard) {
   EXPECT_TRUE(omnibox_view->IsSelectAll());
   ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
   clipboard->Clear(ui::Clipboard::BUFFER_STANDARD);
-  ASSERT_NO_FATAL_FAILURE(SendKeyForBrowser(browser_incognito,
-                                            ui::VKEY_C,
-                                            kCtrlOrCmdMask));
+  EXPECT_TRUE(chrome::ExecuteCommand(browser_incognito, IDC_COPY));
   EXPECT_TRUE(clipboard->IsFormatAvailable(
       ui::Clipboard::GetPlainTextFormatType(), ui::Clipboard::BUFFER_STANDARD));
 
@@ -1770,8 +1767,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, DISABLED_IncognitoCopyURLToClipboard) {
       ui::Clipboard::GetPlainTextFormatType(), ui::Clipboard::BUFFER_STANDARD));
 }
 
-// Disabled due to flakiness.  http://crbug.com/171974
-IN_PROC_BROWSER_TEST_F(OmniboxViewTest, DISABLED_IncognitoCopyTextToClipboard) {
+IN_PROC_BROWSER_TEST_F(OmniboxViewTest, IncognitoCopyTextToClipboard) {
   EXPECT_FALSE(browser()->profile()->IsOffTheRecord());
   Browser* browser_incognito = CreateIncognitoBrowser();
   ui_test_utils::NavigateToURL(browser_incognito,
@@ -1793,9 +1789,7 @@ IN_PROC_BROWSER_TEST_F(OmniboxViewTest, DISABLED_IncognitoCopyTextToClipboard) {
   EXPECT_TRUE(omnibox_view->IsSelectAll());
   ui::Clipboard* clipboard = ui::Clipboard::GetForCurrentThread();
   clipboard->Clear(ui::Clipboard::BUFFER_STANDARD);
-  ASSERT_NO_FATAL_FAILURE(SendKeyForBrowser(browser_incognito,
-                                            ui::VKEY_C,
-                                            kCtrlOrCmdMask));
+  EXPECT_TRUE(chrome::ExecuteCommand(browser_incognito, IDC_COPY));
   EXPECT_TRUE(clipboard->IsFormatAvailable(
       ui::Clipboard::GetPlainTextFormatType(), ui::Clipboard::BUFFER_STANDARD));
   EXPECT_FALSE(clipboard->IsFormatAvailable(
