9e41f0d7aea7cfc837d19c6bf7ad111a183f6bd9
abucur@adobe.com <abucur@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-13 18:32:14 +0000

    [CSS Regions] Break after doesn't work correctly with auto-height regions https://bugs.webkit.org/show_bug.cgi?id=112241
    Reviewed by David Hyatt.
    
    Source/WebCore:
    
    The change correctly returns the break position during the normal layout phase for
    auto-height regions. The problem doesn't exist during the constrained layout phase
    so it only affects the size of the auto-height regions by incorrectly positioning
    the content inside the flow.
    
    Test: fast/regions/autoheight-break-after.html
    
    * rendering/RenderBlock.cpp:
    (WebCore::RenderBlock::applyAfterBreak): Include the margin offset in the returned break position.
    
    LayoutTests:
    
    This reference test verifies the correct break positioned is computed when there are auto-height regions during
    the normal layout phase.
    The test includes an auto-height region, a region with a specified size and an auto-height region. The first
    region ends with a paragraph with a forced break after. During the normal layout phase, if the paragraph margin
    is not included in the break offset, the next paragraph is incorrectly positioned and it will result in an
    incorrectly sized last region.
    In the constrained layout phase, the break position is returned correctly, the paragraph is positioned further down
    causing overflow in the auto-height region.
    
    * fast/regions/autoheight-break-after-expected.html: Added.
    * fast/regions/autoheight-break-after.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderBlock.cpp
@@ -7321,7 +7321,7 @@ LayoutUnit RenderBlock::applyAfterBreak(RenderBox* child, LayoutUnit logicalOffs
         if (checkRegionBreaks) {
             LayoutUnit offsetBreakAdjustment = 0;
             if (flowThread->addForcedRegionBreak(offsetFromLogicalTopOfFirstPage() + logicalOffset + marginOffset, child, false, &offsetBreakAdjustment))
-                return logicalOffset + offsetBreakAdjustment;
+                return logicalOffset + marginOffset + offsetBreakAdjustment;
         }
         return nextPageLogicalTop(logicalOffset, IncludePageBoundary);
     }
