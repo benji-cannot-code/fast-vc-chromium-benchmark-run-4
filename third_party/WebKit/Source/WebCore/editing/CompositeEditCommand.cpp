c05d69bd40a3806bd78744e38da08935227adf24
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-13 21:38:19 +0000

    Crash in CompositeEditCommand::insertNodeAt. https://bugs.webkit.org/show_bug.cgi?id=112280
    Reviewed by Ryosuke Niwa.
    
    * editing/CompositeEditCommand.cpp:
    (WebCore::CompositeEditCommand::insertNodeAt): Fix incorrect cast use of toElement.
    Subsitute with toContainerNode.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145754 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
+++ third_party/WebKit/Source/WebCore/editing/CompositeEditCommand.cpp
@@ -365,7 +365,7 @@ void CompositeEditCommand::insertNodeAt(PassRefPtr<Node> insertChild, const Posi
         if (child)
             insertNodeBefore(insertChild, child);
         else
-            appendNode(insertChild, toElement(refChild));
+            appendNode(insertChild, toContainerNode(refChild));
     } else if (caretMinOffset(refChild) >= offset)
         insertNodeBefore(insertChild, refChild);
     else if (refChild->isTextNode() && caretMaxOffset(refChild) > offset) {
