9717a93d53248ef498827fa65c24d56f20dd9b26
pastarmovj@google.com <pastarmovj@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 17:00:56 +0000

    Revert 187669 "Adjust the profile sync service to properly set p..."
    NOTE:Will recommit this one after we get session manager to change flags on ChromeOS.
    
    Adjust the profile sync service to properly set per user or per machine flags.
    
    Since flags are handled differntly on ChromeOS now the sync service needs to
    change the way it stores its flags until those are moved to proper prefs.
    
    BUG=chromium:181331
    TEST=Sync service experiments should still work.
    > 
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12549017
    
    TBR=pastarmovj@chromium.org
    Review URL: https://codereview.chromium.org/12426009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187881 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service.cc
+++ chrome/browser/sync/profile_sync_service.cc
@@ -80,10 +80,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sync/internal_api/public/read_transaction.h"
 #endif
 
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/login/user_manager.h"
-#endif
-
 using browser_sync::ChangeProcessor;
 using browser_sync::DataTypeController;
 using browser_sync::DataTypeManager;
@@ -938,7 +934,9 @@ void ProfileSyncService::OnExperimentsChanged(
     std::string experiment_name = GetExperimentNameForDataType(it.Get());
     if (experiment_name.empty())
       continue;
-    SetExperimentFlag(experiment_name);
+    about_flags::SetExperimentEnabled(g_browser_process->local_state(),
+                                      experiment_name,
+                                      true);
   }
 
   // Check if the user has "Keep Everything Synced" enabled. If so, we want
@@ -963,7 +961,9 @@ void ProfileSyncService::OnExperimentsChanged(
   // Now enable any non-datatype features.
   if (experiments.sync_tab_favicons) {
     DVLOG(1) << "Enabling syncing of tab favicons.";
-    SetExperimentFlag("sync-tab-favicons");
+    about_flags::SetExperimentEnabled(g_browser_process->local_state(),
+                                      "sync-tab-favicons",
+                                      true);
 #if defined(OS_ANDROID)
     // Android does not support about:flags and experiments, so we need to force
     // setting the experiments as command line switches.
@@ -971,11 +971,17 @@ void ProfileSyncService::OnExperimentsChanged(
 #endif
   }
 
-  if (experiments.keystore_encryption)
-    SetExperimentFlag(syncer::kKeystoreEncryptionFlag);
+  if (experiments.keystore_encryption) {
+    about_flags::SetExperimentEnabled(g_browser_process->local_state(),
+                                      syncer::kKeystoreEncryptionFlag,
+                                      true);
+  }
 
-  if (experiments.full_history_sync)
-    SetExperimentFlag(syncer::kFullHistorySyncFlag);
+  if (experiments.full_history_sync) {
+    about_flags::SetExperimentEnabled(g_browser_process->local_state(),
+                                      syncer::kFullHistorySyncFlag,
+                                      true);
+  }
 
   current_experiments_ = experiments;
 }
@@ -2021,17 +2027,4 @@ void ProfileSyncService::ResetForTest() {
       profile,
       signin,
       behavior);
-}
-
-void ProfileSyncService::SetExperimentFlag(const std::string& experiment) {
-#if defined(OS_CHROMEOS)
-  if (chromeos::UserManager::Get()->IsCurrentUserOwner())
-    about_flags::SetExperimentEnabled(NULL, experiment, true);
-  else
-    about_flags::SetExperimentEnabled(profile_->GetPrefs(), experiment, true);
-#else
-  about_flags::SetExperimentEnabled(g_browser_process->local_state(),
-                                    experiment,
-                                    true);
-#endif
 }
