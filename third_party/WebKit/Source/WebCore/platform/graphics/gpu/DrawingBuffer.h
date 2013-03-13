7b9e174e58b94026eb349cc435f080cfb67f94b6
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-13 01:14:34 +0000

    Unreviewed, rolling out r145280. http://trac.webkit.org/changeset/145280 https://bugs.webkit.org/show_bug.cgi?id=112217
    This patch broke chromium WebGL conformance tests (Requested
    by shawnsingh on #webkit).
    
    Patch by Sheriff Bot <webkit.review.bot@gmail.com> on 2013-03-12
    
    * platform/graphics/gpu/DrawingBuffer.cpp:
    (WebCore::DrawingBuffer::reset):
    * platform/graphics/gpu/DrawingBuffer.h:
    (DrawingBuffer):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145649 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/gpu/DrawingBuffer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/gpu/DrawingBuffer.h
@@ -141,8 +141,6 @@ private:
 
     void initialize(const IntSize&);
 
-    bool checkBufferIntegrity();
-
     PreserveDrawingBuffer m_preserveDrawingBuffer;
     AlphaRequirement m_alpha;
     bool m_scissorEnabled;
