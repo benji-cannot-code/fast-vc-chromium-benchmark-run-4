f55d81ea4ea824a483083ba9022623b0cb61e329
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 03:23:57 +0000

    Add QuicCryptoServerStream (and test) and get CryptoUtils working.
    Review URL: https://chromiumcodereview.appspot.com/12452007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187757 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/test_tools/crypto_test_utils.cc
+++ net/quic/test_tools/crypto_test_utils.cc
@@ -4,13 +4,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "net/quic/test_tools/crypto_test_utils.h"
-/*
+
 #include "net/quic/quic_crypto_client_stream.h"
 #include "net/quic/quic_crypto_server_stream.h"
 #include "net/quic/quic_crypto_stream.h"
 #include "net/quic/test_tools/quic_test_utils.h"
 #include "net/quic/test_tools/simple_quic_framer.h"
-#include "net/util/ipaddress.h"
 
 namespace net {
 namespace test {
@@ -65,7 +64,9 @@ void CryptoTestUtils::HandshakeWithFakeServer(
     PacketSavingConnection* client_conn,
     QuicCryptoStream* client) {
   QuicGuid guid(1);
-  SocketAddress addr(IPAddress::Loopback4(), 1);
+  IPAddressNumber ip;
+  CHECK(ParseIPLiteralToNumber("192.0.2.33", &ip));
+  IPEndPoint addr = IPEndPoint(ip, 1);
   PacketSavingConnection* server_conn =
       new PacketSavingConnection(guid, addr);
   TestSession server_session(server_conn, true);
@@ -82,7 +83,9 @@ void CryptoTestUtils::HandshakeWithFakeClient(
     PacketSavingConnection* server_conn,
     QuicCryptoStream* server) {
   QuicGuid guid(1);
-  SocketAddress addr(IPAddress::Loopback4(), 1);
+  IPAddressNumber ip;
+  CHECK(ParseIPLiteralToNumber("192.0.2.33", &ip));
+  IPEndPoint addr = IPEndPoint(ip, 1);
   PacketSavingConnection* client_conn =
       new PacketSavingConnection(guid, addr);
   TestSession client_session(client_conn, true);
@@ -95,4 +98,3 @@ void CryptoTestUtils::HandshakeWithFakeClient(
 }
 }  // namespace test
 }  // namespace net
-*/
