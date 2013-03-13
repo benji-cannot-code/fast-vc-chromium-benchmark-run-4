71cf913732bf4c1e9bd595fe63d14e92d5a2f9b1
acolwell@chromium.org <acolwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-13 15:37:57 +0000

    Cleanup dead MediaSource code now that the Chromium-side refactor is complete. https://bugs.webkit.org/show_bug.cgi?id=110371
    Reviewed by Adam Barth.
    
    * public/WebMediaPlayer.h:
    (WebKit):
    (WebMediaPlayer): Remove old MediaSource methods and default MediaSource load() implementation.
    * public/WebMediaPlayerClient.h: Remove old MediaSource methods.
    * src/WebMediaPlayerClientImpl.cpp: Remove WebMediaSourceClientImpl & WebSourceBufferImpl classes that
                                        are no longer needed.
    * src/WebMediaPlayerClientImpl.h: Remove old MediaSource methods.
    (WebMediaPlayerClientImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayer.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayer.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "../../../Platform/chromium/public/WebCanvas.h"
 #include "../../../Platform/chromium/public/WebString.h"
 #include "../../../Platform/chromium/public/WebVideoFrame.h"
-#include "WebMediaSource.h"
 #include "WebTimeRange.h"
 
 namespace WebKit {
@@ -43,6 +42,7 @@ namespace WebKit {
 class WebAudioSourceProvider;
 class WebAudioSourceProviderClient;
 class WebMediaPlayerClient;
+class WebMediaSource;
 class WebStreamTextureClient;
 class WebString;
 class WebURL;
@@ -83,18 +83,6 @@ public:
         PreloadAuto,
     };
 
-    enum AddIdStatus {
-        AddIdStatusOk,
-        AddIdStatusNotSupported,
-        AddIdStatusReachedIdLimit
-    };
-
-    enum EndOfStreamStatus {
-        EndOfStreamStatusNoError,
-        EndOfStreamStatusNetworkError,
-        EndOfStreamStatusDecodeError,
-    };
-
     // Represents synchronous exceptions that can be thrown from the Encrypted
     // Media methods. This is different from the asynchronous MediaKeyError.
     enum MediaKeyException {
@@ -112,9 +100,7 @@ public:
     virtual ~WebMediaPlayer() {}
 
     virtual void load(const WebURL&, CORSMode) = 0;
-    // FIXME: Remove the default implementation once the Chromium code implements this method.
-    // https://bugs.webkit.org/show_bug.cgi?id=110371
-    virtual void load(const WebURL& url, WebMediaSource* mediaSource, CORSMode corsMode) { delete mediaSource; load(url, corsMode); };
+    virtual void load(const WebURL&, WebMediaSource*, CORSMode) = 0;
     virtual void cancelLoad() = 0;
 
     // Playback controls.
@@ -188,16 +174,6 @@ public:
     virtual void setStreamTextureClient(WebStreamTextureClient*) { }
 
     virtual WebAudioSourceProvider* audioSourceProvider() { return 0; }
-
-    virtual AddIdStatus sourceAddId(const WebString& id, const WebString& type,
-                                    const WebVector<WebString>& codecs) { return AddIdStatusNotSupported; }
-    virtual bool sourceRemoveId(const WebString& id) { return false; }
-    virtual WebTimeRanges sourceBuffered(const WebString& id) { return WebTimeRanges(); };
-    virtual bool sourceAppend(const WebString& id, const unsigned char* data, unsigned length) { return false; }
-    virtual bool sourceAbort(const WebString& id) { return false; }
-    virtual void sourceSetDuration(double) { }
-    virtual void sourceEndOfStream(EndOfStreamStatus)  { }
-    virtual bool sourceSetTimestampOffset(const WebString& id, double offset) { return false; }
 
     // Returns whether keySystem is supported. If true, the result will be
     // reported by an event.
