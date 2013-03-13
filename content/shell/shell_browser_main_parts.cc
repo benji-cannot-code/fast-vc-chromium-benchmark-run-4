4cc5b01f91955b22bdbba7d37e8336e54a49e1b7
kerz@chromium.org <kerz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 20:31:48 +0000

    Revert 187465 "Android: Enable --composite-to-mailbox"
    > Android: Enable --composite-to-mailbox
    > 
    > BUG=181640
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12734002
    
    TBR=sievers@chromium.org
    Review URL: https://codereview.chromium.org/12733010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187933 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_browser_main_parts.cc
+++ content/shell/shell_browser_main_parts.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_number_conversions.h"
 #include "base/threading/thread.h"
 #include "base/threading/thread_restrictions.h"
-#include "cc/switches.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/main_function_params.h"
 #include "content/public/common/url_constants.h"
@@ -101,9 +100,6 @@ void ShellBrowserMainParts::PreEarlyInitialization() {
 #if defined(OS_ANDROID)
   net::NetworkChangeNotifier::SetFactory(
       new net::NetworkChangeNotifierFactoryAndroid());
-
-  CommandLine::ForCurrentProcess()->AppendSwitch(
-      cc::switches::kCompositeToMailbox);
 #endif
 }
 
