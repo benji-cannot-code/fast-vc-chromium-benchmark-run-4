dcc76581737d0afc4e58118f598df3a279ac39dd
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 20:28:33 +0000

    cc: Complete pending tile uploads if they are needed for tree activation
    When we are checking whether resources are ready for tree activation,
    force any pending tile upload to complete. This allows us to render
    immediately and avoid polling for tile upload completion.
    
    BUG=161828
    
    Review URL: https://chromiumcodereview.appspot.com/12321053
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187931 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_layer_impl.cc
+++ cc/picture_layer_impl.cc
@@ -508,6 +508,13 @@ bool PictureLayerImpl::AreVisibleResourcesReady() const {
   float min_acceptable_scale =
       std::min(raster_contents_scale, ideal_contents_scale_);
 
+  TreePriority tree_priority =
+      layer_tree_impl()->tile_manager()->GlobalState().tree_priority;
+  bool should_force_uploads =
+      tree_priority != SMOOTHNESS_TAKES_PRIORITY &&
+      layer_tree_impl()->animationRegistrar()->
+          active_animation_controllers().empty();
+
   if (PictureLayerImpl* twin = ActiveTwin()) {
     float twin_raster_contents_scale =
         twin->raster_page_scale_ *
@@ -532,6 +539,8 @@ bool PictureLayerImpl::AreVisibleResourcesReady() const {
                                            PictureLayerTiling::LayerDeviceAlignmentUnknown);
          iter;
          ++iter) {
+      if (should_force_uploads && iter)
+        layer_tree_impl()->tile_manager()->ForceTileUploadToComplete(*iter);
       // A null tile (i.e. no recording) is considered "ready".
       if (!*iter || iter->drawing_info().IsReadyToDraw())
         missing_region.Subtract(iter.geometry_rect());
--- cc/resource_provider.h
+++ cc/resource_provider.h
@@ -281,6 +281,7 @@ class CC_EXPORT ResourceProvider {
 
   // Asynchronously update pixels from acquired pixel buffer.
   void BeginSetPixels(ResourceId id);
+  void ForceSetPixelsToComplete(ResourceId id);
   bool DidSetPixelsComplete(ResourceId id);
   void AbortSetPixels(ResourceId id);
 
@@ -329,6 +330,7 @@ class CC_EXPORT ResourceProvider {
     bool exported;
     bool marked_for_deletion;
     bool pending_set_pixels;
+    bool set_pixels_completion_forced;
     bool allocated;
     bool enable_read_lock_fences;
     scoped_refptr<Fence> read_lock_fence;
