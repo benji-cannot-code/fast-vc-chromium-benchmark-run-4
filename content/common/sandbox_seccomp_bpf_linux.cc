595a97cdca3828228f1c724e7094f656822c2339
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 20:24:53 +0000

    Linux sandbox: allow sched_setaffinity in GPU
    In the GPU process we allow the sched_setaffinity system call.
    
    BUG=178172
    TBR=cevans
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12593010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187929 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_seccomp_bpf_linux.cc
+++ content/common/sandbox_seccomp_bpf_linux.cc
@@ -1280,6 +1280,7 @@ ErrorCode GpuProcessPolicy(Sandbox *sandbox, int sysno,
   switch(sysno) {
     case __NR_ioctl:
     case __NR_sched_getaffinity:
+    case __NR_sched_setaffinity:
       return ErrorCode(ErrorCode::ERR_ALLOWED);
     case __NR_open:
     case __NR_openat:
