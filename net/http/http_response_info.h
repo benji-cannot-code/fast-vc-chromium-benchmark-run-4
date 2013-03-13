fdeb13fffc9e38b3d4360655c1d8fcb612f12e47
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 03:51:27 +0000

    Add a ConnectionInfo enum which describes the kind of connection used to fetch a response.  This will eventually be made visible in window.chrome.loadTimings().
    Review URL: https://chromiumcodereview.appspot.com/12518020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187763 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_response_info.h
+++ net/http/http_response_info.h
@@ -25,6 +25,17 @@ class SSLCertRequestInfo;
 
 class NET_EXPORT HttpResponseInfo {
  public:
+  // Describes the kind of connection used to fetch this response.
+  enum ConnectionInfo {
+    CONNECTION_INFO_UNKNOWN,
+    CONNECTION_INFO_HTTP1,
+    CONNECTION_INFO_SPDY2,
+    CONNECTION_INFO_SPDY3,
+    CONNECTION_INFO_SPDY3_1,
+    CONNECTION_INFO_QUIC1_SPDY3,
+    NUM_OF_CONNECTION_INFOS,
+  };
+
   HttpResponseInfo();
   HttpResponseInfo(const HttpResponseInfo& rhs);
   ~HttpResponseInfo();
@@ -71,6 +82,9 @@ class NET_EXPORT HttpResponseInfo {
 
   // Protocol negotiated with the server.
   std::string npn_negotiated_protocol;
+
+  // The type of connection used for this response.
+  ConnectionInfo connection_info;
 
   // The time at which the request was made that resulted in this response.
   // For cached responses, this is the last time the cache entry was validated.
