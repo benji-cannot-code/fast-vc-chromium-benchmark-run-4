bcf9a2118228b512f2e13d5639c0d1ac2539a56c
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-13 18:14:38 +0000

    Web Inspector: coalesce repeating timeline records https://bugs.webkit.org/show_bug.cgi?id=112168
    Reviewed by Vsevolod Vlasov.
    
    - coalesce repeating top-level timeline events;
    - drive-by: only show categories that actually took time in aggregated stats;
    
    * English.lproj/localizedStrings.js:
    * inspector/front-end/TimelineFrameController.js:
    (WebInspector.TimelineFrameController.prototype._innerAddRecord):
    * inspector/front-end/TimelinePanel.js:
    (WebInspector.TimelinePanel.prototype._highlightRect):
    (WebInspector.TimelineCalculator.prototype.computeBarGraphPercentages):
    * inspector/front-end/TimelinePresentationModel.js:
    (WebInspector.TimelinePresentationModel.prototype.reset):
    (WebInspector.TimelinePresentationModel.prototype._innerAddRecord):
    (WebInspector.TimelinePresentationModel.prototype._updateAncestorStats):
    (WebInspector.TimelinePresentationModel.prototype._findCoalescedParent):
    (WebInspector.TimelinePresentationModel.prototype._replaceWithCoalescedRecord):
    (WebInspector.TimelinePresentationModel.Record):
    (WebInspector.TimelinePresentationModel.adoptRecord):
    (WebInspector.TimelinePresentationModel.insertRetrospectiveRecord):
    (WebInspector.TimelinePresentationModel.Record.prototype.get selfTime):
    (WebInspector.TimelinePresentationModel.Record.prototype._generatePopupContentWithImagePreview):
    (WebInspector.TimelinePresentationModel.Record.prototype._getRecordDetails):
    (WebInspector.TimelinePresentationModel.Record.prototype.calculateAggregatedStats):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TimelinePanel.js
@@ -1079,6 +1079,8 @@ WebInspector.TimelinePanel.prototype = {
 
     _highlightRect: function(record)
     {
+        if (record.coalesced)
+            return;
         if (this._highlightedRect === record.data)
             return;
         this._highlightedRect = record.data;
@@ -1294,7 +1296,7 @@ WebInspector.TimelineCalculator.prototype = {
         var start = (record.startTime - this._minimumBoundary) / this.boundarySpan() * 100;
         var end = (record.startTime + record.selfTime - this._minimumBoundary) / this.boundarySpan() * 100;
         var endWithChildren = (record.lastChildEndTime - this._minimumBoundary) / this.boundarySpan() * 100;
-        var cpuWidth = record.cpuTime / this.boundarySpan() * 100;
+        var cpuWidth = record.coalesced ? endWithChildren - start : record.cpuTime / this.boundarySpan() * 100;
         return {start: start, end: end, endWithChildren: endWithChildren, cpuWidth: cpuWidth};
     },
 
