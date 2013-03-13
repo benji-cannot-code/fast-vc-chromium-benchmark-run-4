f0f174075a1a799e49f24c37a61f5761b3ab7c4b
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 16:44:58 +0000

    cc: Chromify IOSurfaceLayer
    R=danakj@chromium.org
    TBR=ananta@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12471006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187879 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/io_surface_layer.h
+++ cc/io_surface_layer.h
@@ -12,23 +12,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 class CC_EXPORT IOSurfaceLayer : public Layer {
-public:
-    static scoped_refptr<IOSurfaceLayer> Create();
+ public:
+  static scoped_refptr<IOSurfaceLayer> Create();
 
-    void setIOSurfaceProperties(uint32_t ioSurfaceId, const gfx::Size&);
+  void SetIOSurfaceProperties(uint32_t io_surface_id, gfx::Size size);
 
-    virtual scoped_ptr<LayerImpl> CreateLayerImpl(LayerTreeImpl* treeImpl) OVERRIDE;
-    virtual bool DrawsContent() const OVERRIDE;
-    virtual void PushPropertiesTo(LayerImpl*) OVERRIDE;
+  virtual scoped_ptr<LayerImpl> CreateLayerImpl(LayerTreeImpl* tree_impl)
+      OVERRIDE;
+  virtual bool DrawsContent() const OVERRIDE;
+  virtual void PushPropertiesTo(LayerImpl* layer) OVERRIDE;
 
-protected:
-    IOSurfaceLayer();
+ protected:
+  IOSurfaceLayer();
 
-private:
-    virtual ~IOSurfaceLayer();
+ private:
+  virtual ~IOSurfaceLayer();
 
-    uint32_t m_ioSurfaceId;
-    gfx::Size m_ioSurfaceSize;
+  uint32_t io_surface_id_;
+  gfx::Size io_surface_size_;
 };
 
 }
