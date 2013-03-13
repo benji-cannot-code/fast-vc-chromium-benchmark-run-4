8c54618c9226fbd052b7211bbd57e60ce0071d87
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-13 16:35:17 +0000

    Do not push properties that are animated on impl only.
    We currently avoid pushing properties when the main thread layer indicates that
    it is animating. This code was incorrectly left behind after we started
    animating on the main thread. Recently, we've added support for impl-only
    animations. We should never push during one of these animations.
    
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12413020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@187875 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layer_impl.h
+++ cc/layer_impl.h
@@ -151,6 +151,7 @@ class CC_EXPORT LayerImpl : LayerAnimationValueObserver {
   void SetOpacity(float opacity);
   float opacity() const { return opacity_; }
   bool OpacityIsAnimating() const;
+  bool OpacityIsAnimatingOnImplOnly() const;
 
   void SetPosition(gfx::PointF position);
   gfx::PointF position() const { return position_; }
@@ -328,6 +329,7 @@ class CC_EXPORT LayerImpl : LayerAnimationValueObserver {
   void SetTransform(const gfx::Transform& transform);
   const gfx::Transform& transform() const { return transform_; }
   bool TransformIsAnimating() const;
+  bool TransformIsAnimatingOnImplOnly() const;
 
   void set_update_rect(const gfx::RectF& update_rect) {
     update_rect_ = update_rect;
