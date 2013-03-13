ab600bd1e2adffa601eecc8d790aed87b0e01888
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-13 03:59:32 +0000

    Unreviewed attempted Windows build fix after http://trac.webkit.org/changeset/145592.
    * wtf/SchedulePair.cpp:
    * wtf/SchedulePair.h:
    (WTF::SchedulePair::SchedulePair): Since linking is failing,
    try inlining this function.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145678 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/SchedulePair.cpp
+++ third_party/WebKit/Source/WTF/wtf/SchedulePair.cpp
@@ -32,13 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WTF {
 
-SchedulePair::SchedulePair(CFRunLoopRef runLoop, CFStringRef mode)
-    : m_runLoop(runLoop)
-{
-    if (mode)
-        m_mode.adoptCF(CFStringCreateCopy(0, mode));
-}
-
 bool SchedulePair::operator==(const SchedulePair& other) const
 {
     if (runLoop() != other.runLoop())
