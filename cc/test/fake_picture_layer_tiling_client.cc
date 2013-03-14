00d35bf8d4e53f7e9ff17a83e985f34a6cd47a71
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 16:20:32 +0000

    cc: Fix style errors in impl-side painting classes
    R=jamesr@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12819002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188094 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_picture_layer_tiling_client.cc
+++ cc/test/fake_picture_layer_tiling_client.cc
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-class FakeInfinitePicturePileImpl : public PicturePileImpl
-{
+class FakeInfinitePicturePileImpl : public PicturePileImpl {
  public:
   FakeInfinitePicturePileImpl() {
     gfx::Size size(std::numeric_limits<int>::max(),
