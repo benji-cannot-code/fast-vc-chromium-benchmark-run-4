9e87eec03a4b674ea66ec342b5bf8931d0e66466
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 21:31:34 +0000

    InstantExtendedManualTest: Set browser pointer in InstantTestBase.
    BUG=188149
    
    
    Review URL: https://chromiumcodereview.appspot.com/12663016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188200 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_test_utils.h
+++ chrome/browser/instant/instant_test_utils.h
@@ -64,6 +64,10 @@ class InstantTestBase {
   void SetupInstant(Browser* browser);
   void Init(const GURL& instant_url);
 
+  void set_browser(Browser* browser) {
+    browser_ = browser;
+  }
+
   InstantController* instant() {
     return browser_->instant_controller()->instant();
   }
