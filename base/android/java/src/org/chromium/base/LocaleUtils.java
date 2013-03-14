93615a2ddf994d008907985e46874350498619ec
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 15:55:20 +0000

    [Android] Use generated JNI bindings for locale utils.
    We're removing the often error-prone manual bindings with the JNI generator.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12598005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188087 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/java/src/org/chromium/base/LocaleUtils.java
+++ base/android/java/src/org/chromium/base/LocaleUtils.java
@@ -36,4 +36,14 @@ public class LocaleUtils {
         }
         return country.isEmpty() ? language : language + "-" + country;
     }
+
+    @CalledByNative
+    private static Locale getJavaLocale(String language, String country, String variant) {
+        return new Locale(language, country, variant);
+    }
+
+    @CalledByNative
+    private static String getDisplayNameForLocale(Locale locale, Locale displayLocale) {
+        return locale.getDisplayName(displayLocale);
+    }
 }
