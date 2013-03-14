992284e0db84237d69916c9bd3a6b92df1a59d13
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-14 13:36:00 +0000

    Web Inspector: Flame Chart. Support scroll and zoom with help of mouse wheel. https://bugs.webkit.org/show_bug.cgi?id=112337
    Reviewed by Yury Semikhatsky.
    
    New member variable _xOffset were introduced.
    
    * inspector/front-end/FlameChart.js:
    (WebInspector.FlameChart):
    (WebInspector.FlameChart.prototype._onMouseMove):
    (WebInspector.FlameChart.prototype._adjustXOffset):
    (WebInspector.FlameChart.prototype._adjustXScale):
    (WebInspector.FlameChart.prototype._onMouseWheel):
    (WebInspector.FlameChart.prototype._coordinatesToNodeIndex):
    (WebInspector.FlameChart.prototype.onResize):
    (WebInspector.FlameChart.prototype.draw):
    (WebInspector.FlameChart.prototype._scheduleUpdate):
    (WebInspector.FlameChart.prototype.update):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145807 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/FlameChart.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/FlameChart.js
@@ -42,10 +42,12 @@ WebInspector.FlameChart = function(cpuProfileView)
     this.element.className = "flame-chart";
     this._canvas = this.element.createChild("canvas");
     this._cpuProfileView = cpuProfileView;
-    this._xScaleFactor = 0.0;
+    this._xScaleFactor = 4.0;
+    this._xOffset = 0;
     this._barHeight = 10;
     this._minWidth = 1;
     this.element.onmousemove = this._onMouseMove.bind(this);
+    this.element.onmousewheel = this._onMouseWheel.bind(this);
     this.element.onclick = this._onClick.bind(this);
     this._popoverHelper = new WebInspector.PopoverHelper(this.element, this._getPopoverAnchor.bind(this), this._showPopover.bind(this));
     this._popoverHelper.setTimeout(250);
@@ -204,8 +206,35 @@ WebInspector.FlameChart.prototype = {
         var style = this._anchorElement.style;
         style.width = Math.floor(timelineData.durations[nodeIndex] * this._xScaleFactor) + "px";
         style.height = this._barHeight + "px";
-        style.left = Math.floor(timelineData.startTimes[nodeIndex] * this._xScaleFactor) + "px";
+        style.left = Math.floor(timelineData.startTimes[nodeIndex] * this._xScaleFactor - this._xOffset) + "px";
         style.top = Math.floor(this._canvas.height - (timelineData.depths[nodeIndex] + 1) * this._barHeight) + "px";
+    },
+
+    _adjustXOffset: function(direction)
+    {
+        var step = this._xScaleFactor * 5;
+        this._xOffset += direction > 0 ? step : -step;
+        if (this._xOffset < 0)
+            this._xOffset = 0;
+    },
+
+    _adjustXScale: function(direction, x)
+    {
+        if (direction > 0)
+            this._xScaleFactor /= 2;
+        else
+            this._xScaleFactor *= 2;
+    },
+
+    _onMouseWheel: function(e)
+    {
+        if (e.shiftKey)
+            this._adjustXScale(e.wheelDelta, e.x);
+        else
+            this._adjustXOffset(e.wheelDelta);
+
+        this._hidePopover();
+        this._scheduleUpdate();
     },
 
     /**
@@ -217,7 +246,7 @@ WebInspector.FlameChart.prototype = {
         var timelineData = this._timelineData;
         if (!timelineData)
             return -1;
-        var cursorTime = x / this._xScaleFactor;
+        var cursorTime = (x + this._xOffset) / this._xScaleFactor;
         var cursorLevel = Math.floor((this._canvas.height - y) / this._barHeight);
 
         for (var i = 0; i < timelineData.nodeCount; ++i) {
@@ -233,7 +262,7 @@ WebInspector.FlameChart.prototype = {
     onResize: function()
     {
         this._hidePopover();
-        this.update();
+        this._scheduleUpdate();
     },
 
     /**
@@ -247,17 +276,20 @@ WebInspector.FlameChart.prototype = {
             return;
         this._canvas.height = height;
         this._canvas.width = width;
-        var xScaleFactor = this._xScaleFactor = width / timelineData.totalTime;
+        var xScaleFactor = this._xScaleFactor;
         var barHeight = this._barHeight;
+        var xOffset = this._xOffset;
 
         var context = this._canvas.getContext("2d");
 
         for (var i = 0; i < timelineData.nodeCount; ++i) {
+            var x = Math.floor(timelineData.startTimes[i] * xScaleFactor) - xOffset;
+            if (x > width)
+                break;
+            var y = height - (timelineData.depths[i] + 1) * barHeight;
             var barWidth = Math.floor(timelineData.durations[i] * xScaleFactor);
             if (barWidth < this._minWidth)
                 continue;
-            var x = Math.floor(timelineData.startTimes[i] * xScaleFactor);
-            var y = height - (timelineData.depths[i] + 1) * barHeight;
 
             var colorPair = timelineData.colorPairs[i];
             var color;
@@ -273,8 +305,16 @@ WebInspector.FlameChart.prototype = {
         }
     },
 
+    _scheduleUpdate: function()
+    {
+        if (this._updateTimerId)
+            return;
+        this._updateTimerId = setTimeout(this.update.bind(this), 10);
+    },
+
     update: function()
     {
+        this._updateTimerId = 0;
         this.draw(this.element.clientWidth, this.element.clientHeight);
     },
 
