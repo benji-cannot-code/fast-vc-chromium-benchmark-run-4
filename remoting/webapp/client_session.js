c4ae2a6ede94337375c8ebdc8c5c9b9724d3b4eb
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 08:14:18 +0000

    Include host visible name in tool-bar.
    BUG=180120
    
    
    Review URL: https://chromiumcodereview.appspot.com/12793005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188020 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/client_session.js
+++ remoting/webapp/client_session.js
@@ -35,11 +35,13 @@ var remoting = remoting || {};
  * @param {string} hostId The host identifier for Me2Me, or empty for IT2Me.
  *     Mixed into authentication hashes for some authentication methods.
  * @param {remoting.ClientSession.Mode} mode The mode of this connection.
+ * @param {string} hostDisplayName The name of the host for display purposes.
  * @constructor
  */
 remoting.ClientSession = function(hostJid, clientJid,
                                   hostPublicKey, sharedSecret,
-                                  authenticationMethods, hostId, mode) {
+                                  authenticationMethods, hostId, mode,
+                                  hostDisplayName) {
   this.state = remoting.ClientSession.State.CREATED;
 
   this.hostJid = hostJid;
@@ -48,6 +50,8 @@ remoting.ClientSession = function(hostJid, clientJid,
   this.sharedSecret = sharedSecret;
   this.authenticationMethods = authenticationMethods;
   this.hostId = hostId;
+  /** @type {string} */
+  this.hostDisplayName = hostDisplayName;
   /** @type {remoting.ClientSession.Mode} */
   this.mode = mode;
   this.sessionId = '';
