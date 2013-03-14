0b27af616976f947ef92a4744e8afcba71a71311
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 12:12:49 +0000

    android: Ignore the first scroll delta to avoid a jump
    Ignore the very first scroll delta we get from the gesture detector to
    avoid a visible jump at the start of scrolling. This is needed because
    the gesture detector has a dead zone that inhibits detecting small
    movements of the finger as scrolling. Once the finger exists the
    dead zone, the very first scroll delta also includes the delta between
    where the finger touched the glass and where it exited the dead zone. If
    this delta is used to scroll, then the page will always jump by the size
    of the dead zone at the start of a scroll.
    
    BUG=181521
    
    
    Review URL: https://chromiumcodereview.appspot.com/12691005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188040 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewGestureHandler.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewGestureHandler.java
@@ -97,6 +97,8 @@ class ContentViewGestureHandler implements LongPressDelegate {
     // tellNativeScrollingHasEnded() to set it to false.
     private boolean mNativeScrolling;
 
+    private boolean mSeenFirstScrollEvent;
+
     private boolean mPinchInProgress = false;
 
     // Tracks whether a touch cancel event has been sent as a result of switching
@@ -230,6 +232,7 @@ class ContentViewGestureHandler implements LongPressDelegate {
                         mShowPressIsCalled = false;
                         mIgnoreSingleTap = false;
                         mNativeScrolling = false;
+                        mSeenFirstScrollEvent = false;
                         mSnapScrollController.resetSnapScrollMode();
                         mLastRawX = e.getRawX();
                         mLastRawY = e.getRawY();
@@ -249,6 +252,12 @@ class ContentViewGestureHandler implements LongPressDelegate {
                             } else {
                                 distanceX = 0;
                             }
+                        }
+
+                        if (!mSeenFirstScrollEvent) {
+                            // Ignore the first scroll delta to avoid a visible jump.
+                            mSeenFirstScrollEvent = true;
+                            return true;
                         }
 
                         boolean didUIStealScroll = mMotionEventDelegate.didUIStealScroll(
