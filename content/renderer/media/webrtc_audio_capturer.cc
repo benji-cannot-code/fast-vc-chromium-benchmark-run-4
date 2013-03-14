8b0391a2ba44b8bcbac4f1a99423e91a64390465
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 13:49:59 +0000

    Revert 188053 "StopStream for the local media stream should stop..."
    > StopStream for the local media stream should stop the capturer.
    > 
    > LocalMediaStream.Stop() is called explicitly to stop the recording, but this does not take any effect for audio, instead we are relying a device close signal to stop the capture. This patch fixes the problem by calling Stop() for the capturer when StopStream callback is triggered.
    > 
    > Neither peer connection nor the local renderer should be able to stop the capturer. Only the stream can do so.
    > BUG=189193
    > TEST=using https://webrtc-demos.appspot.com/html/pc1.html, make a call, then hangup, unmute the local renderer, you should hear sound.
    > using http://www.corp.google.com/~henrika/webrtc/local-rendering.html, comment out the WebRtcAudioCapturer::DoOnDeviceStopped(), calling start, stop multiple times.
    > 
    > Review URL: https://codereview.chromium.org/12526011
    
    TBR=xians@chromium.org
    Review URL: https://codereview.chromium.org/12540007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188056 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_capturer.cc
+++ content/renderer/media/webrtc_audio_capturer.cc
@@ -190,7 +190,6 @@ WebRtcAudioCapturer::WebRtcAudioCapturer()
 WebRtcAudioCapturer::~WebRtcAudioCapturer() {
   DCHECK(thread_checker_.CalledOnValidThread());
   DCHECK(sinks_.empty());
-  DCHECK(!running_);
   DVLOG(1) << "WebRtcAudioCapturer::~WebRtcAudioCapturer()";
 }
 
@@ -285,6 +284,11 @@ void WebRtcAudioCapturer::Stop() {
   {
     base::AutoLock auto_lock(lock_);
     if (!running_)
+      return;
+
+    // Ignore the Stop() request if we need to continue running for the
+    // local capturer.
+    if (sinks_.size() > 1)
       return;
 
     source = source_;
