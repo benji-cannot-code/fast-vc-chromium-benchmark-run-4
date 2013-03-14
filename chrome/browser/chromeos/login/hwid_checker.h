db0c889f1cc653d03b3a178bb1c2238d2dbd1f25
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 22:06:43 +0000

    Added support for HWID v3 in HWID-checker.
    BUG=174689
    TBR=nkostylev
    
    Review URL: https://chromiumcodereview.appspot.com/12843002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188211 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/hwid_checker.h
+++ chrome/browser/chromeos/login/hwid_checker.h
@@ -10,13 +10,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-// Calculates checksum for given string as it made for HWID v1 and v2.
-std::string CalculateHWIDChecksum(const std::string& data);
-
-// Checks if given HWID correct in terms of HWID v1 or v2.
+// Checks if given HWID correct.
 bool IsHWIDCorrect(const std::string& hwid);
 
-// Checks if current machine has correct HWID v1 or v2.
+// Checks if current machine has correct HWID.
 bool IsMachineHWIDCorrect();
 
 } // namespace chromeos
