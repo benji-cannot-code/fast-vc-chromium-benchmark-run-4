47e24ca41a6499a1f4adf2309bf3055feb4b71dd
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 22:16:52 +0000

    Remove unnecessary resource related methods in ContentVideoViewContextDelegate
    The resource id should be shared between android webview and content, refer to 
    
    content/public/android/java/resource_map/org/chromium/content/R.java
    
    This is part of patchs to fix static ContentVideoViewContextDelgate memeber in ContentVideoView.
    
    BUG=http://b/8315237
    
    Review URL: https://chromiumcodereview.appspot.com/12663014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188212 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ActivityContentVideoViewDelegate.java
+++ content/public/android/java/src/org/chromium/content/browser/ActivityContentVideoViewDelegate.java
@@ -14,7 +14,6 @@ import android.view.WindowManager;
 import android.widget.FrameLayout;
 
 import org.chromium.content.browser.ContentVideoViewContextDelegate;
-import org.chromium.content.R;
 
 /**
  * Uses an exisiting Activity to handle displaying video in full screen.
@@ -47,31 +46,6 @@ public class ActivityContentVideoViewDelegate implements ContentVideoViewContext
     @Override
     public Context getContext() {
         return mActivity;
-    }
-
-    @Override
-    public String getPlayBackErrorText() {
-        return mActivity.getString(R.string.media_player_error_text_invalid_progressive_playback);
-    }
-
-    @Override
-    public String getUnknownErrorText() {
-        return mActivity.getString(R.string.media_player_error_text_unknown);
-    }
-
-    @Override
-    public String getErrorButton() {
-        return mActivity.getString(R.string.media_player_error_button);
-    }
-
-    @Override
-    public String getErrorTitle() {
-        return mActivity.getString(R.string.media_player_error_title);
-    }
-
-    @Override
-    public String getVideoLoadingText() {
-        return mActivity.getString(R.string.media_player_loading_video);
     }
 
     @Override
