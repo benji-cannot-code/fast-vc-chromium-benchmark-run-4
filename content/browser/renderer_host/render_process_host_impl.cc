47bb22ec4680925ef800a4cdfce9dd4d59626ff2
mthiesse@chromium.org <mthiesse@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 20:46:34 +0000

    Cleanup of Browser Plugin pointerLock flag + IPC
    BUG=166228
    
    Review URL: https://chromiumcodereview.appspot.com/12755003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188182 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -793,7 +793,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kDomAutomationController,
     switches::kEnableAccessibilityLogging,
     switches::kEnableBrowserPluginForAllViewTypes,
-    switches::kEnableBrowserPluginPointerLock,
     switches::kEnableDCHECK,
     switches::kEnableDelegatedRenderer,
     switches::kDisableEncryptedMedia,
