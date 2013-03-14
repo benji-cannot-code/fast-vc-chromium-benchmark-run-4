0dc22a7564e8bd05cf8585f0b46739d2b6d6dcbe
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-14 11:26:39 +0000

    [TexMap] Painting into area of composited tile not cleared https://bugs.webkit.org/show_bug.cgi?id=112268
    Reviewed by Kenneth Rohde Christiansen.
    
    Clip painting to the area that has been cleared, so we
    do not accidentally overpaint other areas.
    
    Can be observed in ManualTests/scrollbars/scrollbars-in-composited-layers.html.
    
    * platform/graphics/texmap/TextureMapperImageBuffer.cpp:
    (WebCore::BitmapTextureImageBuffer::updateContents):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145803 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperImageBuffer.cpp
+++ third_party/WebKit/Source/WebCore/platform/graphics/texmap/TextureMapperImageBuffer.cpp
@@ -68,6 +68,7 @@ void BitmapTextureImageBuffer::updateContents(TextureMapper*, GraphicsLayer* sou
     IntRect sourceRect(targetRect);
     sourceRect.setLocation(sourceOffset);
     context->save();
+    context->clip(targetRect);
     context->translate(targetRect.x() - sourceOffset.x(), targetRect.y() - sourceOffset.y());
     sourceLayer->paintGraphicsLayerContents(*context, sourceRect);
     context->restore();
