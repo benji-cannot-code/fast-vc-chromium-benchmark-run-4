ca4fc626299abbf9bca8c8833a4c213f18dea75a
henrika@chromium.org <henrika@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 14:10:36 +0000

    Fixes WebRTC crash caused by calling RemoveCapturerSink() while capturing.
    BUG=none
    TEST=apprtc.appspot.com/?debug=loopback
    
    Review URL: https://codereview.chromium.org/12841004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188061 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_audio_capturer.h
+++ content/renderer/media/webrtc_audio_capturer.h
@@ -120,7 +120,8 @@ class CONTENT_EXPORT WebRtcAudioCapturer
   virtual ~WebRtcAudioCapturer();
 
  private:
-  typedef std::list<WebRtcAudioCapturerSink*> SinkList;
+  class SinkOwner;
+  typedef std::list<scoped_refptr<SinkOwner> > SinkList;
 
   WebRtcAudioCapturer();
 
