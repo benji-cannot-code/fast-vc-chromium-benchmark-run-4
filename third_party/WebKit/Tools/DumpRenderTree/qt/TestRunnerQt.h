4190a7c4d759133e8b8fd7f98b18f181276f54b9
zeno.albisser@digia.com <zeno.albisser@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-14 10:07:24 +0000

    [Qt] Port DRT to use TestRunner::dumpAsText() https://bugs.webkit.org/show_bug.cgi?id=112260
    Reviewed by Benjamin Poulain.
    
    * DumpRenderTree/qt/DumpRenderTreeQt.cpp:
    (WebCore::DumpRenderTree::open):
    (WebCore::methodNameStringForFailedTest):
        Change parameter from type TestRunnerQt* to TestRunner*.
    (WebCore::DumpRenderTree::dump):
        TestRunnerQt::shouldDumpPixels() always returned true in our case.
        Instead we should rely on TestRunner::generatePixelResults().
    * DumpRenderTree/qt/TestRunnerQt.cpp:
    (TestRunnerQt::reset):
    * DumpRenderTree/qt/TestRunnerQt.h:
    (TestRunnerQt):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/qt/TestRunnerQt.h
+++ third_party/WebKit/Tools/DumpRenderTree/qt/TestRunnerQt.h
@@ -60,9 +60,7 @@ public:
     TestRunnerQt(DumpRenderTree*);
 
     bool shouldDisallowIncreaseForApplicationCacheQuota() const { return m_disallowIncreaseForApplicationCacheQuota; }
-    bool shouldDumpAsText() const { return m_textDump; }
     bool shouldDumpAsAudio() const { return m_audioDump; }
-    bool shouldDumpPixels() const { return m_shouldDumpPixels; }
     bool shouldDumpBackForwardList() const { return m_dumpBackForwardList; }
     bool shouldDumpChildrenAsText() const { return m_dumpChildrenAsText; }
     bool shouldDumpChildFrameScrollPositions() const { return m_dumpChildFrameScrollPositions; }
@@ -100,7 +98,6 @@ Q_SIGNALS:
 public Q_SLOTS:
     void maybeDump(bool ok);
     void disallowIncreaseForApplicationCacheQuota() { m_disallowIncreaseForApplicationCacheQuota = true; }
-    void dumpAsText(bool shouldDumpPixels = false);
     void dumpChildFramesAsText() { m_dumpChildrenAsText = true; }
     void dumpChildFrameScrollPositions() { m_dumpChildFrameScrollPositions = true; }
     void dumpDatabaseCallbacks() { m_dumpDatabaseCallbacks = true; }
@@ -272,9 +269,7 @@ private:
 
 private:
     bool m_hasDumped;
-    bool m_textDump;
     bool m_audioDump;
-    bool m_shouldDumpPixels;
     bool m_disallowIncreaseForApplicationCacheQuota;
     bool m_dumpBackForwardList;
     bool m_dumpChildrenAsText;
