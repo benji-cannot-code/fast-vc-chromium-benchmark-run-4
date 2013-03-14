78d79d0f31e122ecd36f342c3d3c18985cc5b392
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 19:53:29 +0000

    Add callback to DiskMountManager::UnmountPath
    The callback makes it easier to handle failures (in comparison to observing
    mount manager for MountEvents).
    
    TEST=existing
    BUG=tested formatting still works
    
    
    Review URL: https://chromiumcodereview.appspot.com/12537016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188161 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/disks/mock_disk_mount_manager.cc
+++ chromeos/disks/mock_disk_mount_manager.cc
@@ -167,7 +167,7 @@ void MockDiskMountManager::SetupDefaultReplies() {
       .Times(AnyNumber());
   EXPECT_CALL(*this, MountPath(_, _, _, _))
       .Times(AnyNumber());
-  EXPECT_CALL(*this, UnmountPath(_, _))
+  EXPECT_CALL(*this, UnmountPath(_, _, _))
       .Times(AnyNumber());
   EXPECT_CALL(*this, FormatMountedDevice(_))
       .Times(AnyNumber());
