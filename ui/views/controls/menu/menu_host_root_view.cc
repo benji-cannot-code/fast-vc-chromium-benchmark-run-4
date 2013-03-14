1f8416729902f8c8b8a7f120c4beec78223f82db
yoichio@chromium.org <yoichio@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 17:24:08 +0000

    bookmarks: Show context menu on empty bookmark folder.
    BUG=110939
    TEST=interactive_ui_tests 
    
    Review URL: https://chromiumcodereview.appspot.com/12558006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188110 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_host_root_view.cc
+++ ui/views/controls/menu/menu_host_root_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/menu/menu_host_root_view.h"
 
 #include "ui/views/controls/menu/menu_controller.h"
+#include "ui/views/controls/menu/menu_item_view.h"
 #include "ui/views/controls/menu/submenu_view.h"
 
 namespace views {
@@ -20,7 +21,9 @@ MenuHostRootView::MenuHostRootView(Widget* widget,
 bool MenuHostRootView::OnMousePressed(const ui::MouseEvent& event) {
   forward_drag_to_menu_controller_ =
       !GetLocalBounds().Contains(event.location()) ||
-      !RootView::OnMousePressed(event);
+      !RootView::OnMousePressed(event) ||
+      DoesEventTargetEmptyMenuItem(event);
+
   if (forward_drag_to_menu_controller_ && GetMenuController())
     GetMenuController()->OnMousePressed(submenu_, event);
   return true;
@@ -76,6 +79,12 @@ void MenuHostRootView::DispatchGestureEvent(ui::GestureEvent* event) {
 
 MenuController* MenuHostRootView::GetMenuController() {
   return submenu_ ? submenu_->GetMenuItem()->GetMenuController() : NULL;
+}
+
+bool MenuHostRootView::DoesEventTargetEmptyMenuItem(
+    const ui::MouseEvent& event) {
+  View* view = GetEventHandlerForPoint(event.location());
+  return view && view->id() == MenuItemView::kEmptyMenuItemViewID;
 }
 
 }  // namespace views
