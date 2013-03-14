edf408ba3e5600d72ada64ae5ca96cb78b4f3dbb
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 18:52:59 +0000

    Cancel prerender if any resources (or redirects) have unsupported schemes. BUG=174616 R=mmenke Review URL: https://codereview.chromium.org/12817007
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188150 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -812,6 +812,11 @@ bool PrerenderManager::IsValidHttpMethod(const std::string& method) {
   return false;
 }
 
+// static
+bool PrerenderManager::DoesURLHaveValidScheme(const GURL& url) {
+  return IsWebURL(url);
+}
+
 DictionaryValue* PrerenderManager::GetAsValue() const {
   DCHECK(CalledOnValidThread());
   DictionaryValue* dict_value = new DictionaryValue();
