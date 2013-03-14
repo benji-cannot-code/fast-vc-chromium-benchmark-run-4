bb7342c3bc9e35104365b3153321537a6170e97e
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 20:34:40 +0000

    Focus the web contents when the panel window is focused
    We now listen to native focus change event that will be triggered no matter how the panel window gets the focus:
    1) Panel is created with initial focus.
    2) Panel is created without initial focus. Then panel window is focused via:
       a) window update API
       b) titlebar click
       c) taskbar icon click
       d) switch to by ALT-TAB or WIN-TAB
    
    BUG=171408
    TEST=manual test by creating panel and focusing on it
    
    
    Review URL: https://chromiumcodereview.appspot.com/12719004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188178 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/panels/panel_view.cc
+++ chrome/browser/ui/views/panels/panel_view.cc
@@ -287,6 +287,8 @@ PanelView::PanelView(Panel* panel, const gfx::Rect& bounds)
                                                 panel->profile()->GetPath()),
       views::HWNDForWidget(window_));
 #endif
+
+  views::WidgetFocusManager::GetInstance()->AddFocusChangeListener(this);
 }
 
 PanelView::~PanelView() {
@@ -295,12 +297,6 @@ PanelView::~PanelView() {
 void PanelView::ShowPanel() {
   ShowPanelInactive();
   ActivatePanel();
-
-  // Give web contents view a chance to set focus to the appropriate element
-  // when it is created for the first time.
-  content::WebContents* web_contents = panel_->GetWebContents();
-  if (web_contents)
-    web_contents->GetView()->RestoreFocus();
 }
 
 void PanelView::ShowPanelInactive() {
@@ -435,6 +431,8 @@ void PanelView::ClosePanel() {
     panel_->OnWindowClosing();
     return;
   }
+
+  views::WidgetFocusManager::GetInstance()->RemoveFocusChangeListener(this);
 
   panel_->OnNativePanelClosed();
   if (window_)
@@ -918,6 +916,17 @@ void PanelView::OnWidgetBoundsChanged(views::Widget* widget,
                                       const gfx::Rect& new_bounds) {
   if (user_resizing_)
     panel()->collection()->OnPanelResizedByMouse(panel(), new_bounds);
+}
+
+void PanelView::OnNativeFocusChange(gfx::NativeView focused_before,
+                                    gfx::NativeView focused_now) {
+  if (focused_now != window_->GetNativeView())
+    return;
+
+  // Give web contents view a chance to set focus to the appropriate element.
+  content::WebContents* web_contents = panel_->GetWebContents();
+  if (web_contents)
+    web_contents->GetView()->RestoreFocus();
 }
 
 bool PanelView::OnTitlebarMousePressed(const gfx::Point& mouse_location) {
