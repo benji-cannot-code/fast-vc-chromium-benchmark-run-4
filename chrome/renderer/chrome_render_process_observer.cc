575874806e25da11a6cad7642d49fc347d36a22f
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-14 16:46:27 +0000

    Remove unnecessary include for tcmalloc heap-profiler in chrome_render_process_observer.
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/12638032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188100 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_render_process_observer.cc
+++ chrome/renderer/chrome_render_process_observer.cc
@@ -53,10 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/iat_patch_function.h"
 #endif
 
-#if defined(USE_TCMALLOC)
-#include "third_party/tcmalloc/chromium/src/gperftools/heap-profiler.h"
-#endif
-
 using WebKit::WebCache;
 using WebKit::WebCrossOriginPreflightResultCache;
 using WebKit::WebFontCache;
