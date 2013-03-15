aeffc1cfbfbe1e5b5fd9ecee17c877ffb14221b0
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 07:48:24 +0000

    ui::Layer::SwitchToLayer should finish animations on its old cc::Layer
    Opacity and transform animations on a ui::Layer are handled by its
    cc::Layer. This CL ensures that when a ui::Layer changes its
    cc::Layer, it finishes animations that are being handled by the old
    cc::Layer.
    
    BUG=164206
    
    
    Review URL: https://chromiumcodereview.appspot.com/12609006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188291 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/layer.h
+++ ui/compositor/layer.h
@@ -317,6 +317,9 @@ class COMPOSITOR_EXPORT Layer
     return pending_threaded_animations_.size() != 0;
   }
 
+  // Triggers a call to SwitchToLayer.
+  void SwitchCCLayerForTest();
+
  private:
   // Stacks |child| above or below |other|.  Helper method for StackAbove() and
   // StackBelow().
