157594738bc10e225dc5746fa90ce18fcdf813c8
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 22:56:28 +0000

    Add views::RichLabel, a class which creates multi-line text with mixed links and plain text.
    BUG=168704
    
    Review URL: https://codereview.chromium.org/12543032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188496 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/link_listener.h
+++ ui/views/controls/link_listener.h
@@ -6,12 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_CONTROLS_LINK_LISTENER_H_
 #define UI_VIEWS_CONTROLS_LINK_LISTENER_H_
 
+#include "ui/views/views_export.h"
+
 namespace views {
 
 class Link;
 
 // An interface implemented by an object to let it know that a link was clicked.
-class LinkListener {
+class VIEWS_EXPORT LinkListener {
  public:
   virtual void LinkClicked(Link* source, int event_flags) = 0;
 
---
+++ ui/views/controls/styled_label_listener.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef UI_VIEWS_CONTROLS_STYLED_LABEL_LISTENER_H_
+#define UI_VIEWS_CONTROLS_STYLED_LABEL_LISTENER_H_
+
+namespace ui {
+class Range;
+}
+
+namespace views {
+
+// A listener interface for StyledLabel.
+class VIEWS_EXPORT StyledLabelListener {
+ public:
+  virtual void StyledLabelLinkClicked(const ui::Range& range,
+                                      int event_flags) = 0;
+
+ protected:
+  virtual ~StyledLabelListener() {}
+};
+
+}  // namespace views
+
+#endif  // UI_VIEWS_CONTROLS_STYLED_LABEL_LISTENER_H_
