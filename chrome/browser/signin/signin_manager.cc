e605c2fe4d7bb1c3fba2d69bb8e366680edbd116
bcwhite@chromium.org <bcwhite@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 09:13:40 +0000

    Allow encoded version of service=chromiumsync
    BUG=188688
    
    
    Review URL: https://chromiumcodereview.appspot.com/12817005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188313 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/signin_manager.cc
+++ chrome/browser/signin/signin_manager.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_auth_util.h"
 #include "google_apis/gaia/gaia_constants.h"
 #include "google_apis/gaia/gaia_urls.h"
+#include "net/base/escape.h"
 #include "net/url_request/url_request_context.h"
 #include "third_party/icu/public/i18n/unicode/regex.h"
 
@@ -88,8 +89,10 @@ bool SigninManager::IsWebBasedSigninFlowURL(const GURL& url) {
 
   // Any login UI URLs with signin=chromiumsync should be considered a web
   // URL (relies on GAIA keeping the "service=chromiumsync" query string
-  // fragment intact even when embedding inside a "continue" parameter).
-  return url.query().find(kChromiumSyncService) != std::string::npos;
+  // fragment present even when embedding inside a "continue" parameter).
+  return net::UnescapeURLComponent(
+      url.query(), net::UnescapeRule::URL_SPECIAL_CHARS)
+          .find(kChromiumSyncService) != std::string::npos;
 }
 
 // static
