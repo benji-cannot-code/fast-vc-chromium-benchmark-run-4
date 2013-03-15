395d70198a3f684d92dfa7f90f940389178c1cf0
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-15 23:14:55 +0000

    Convert <select> to new-flexbox https://bugs.webkit.org/show_bug.cgi?id=112395
    Patch by Christian Biesinger <cbiesinger@chromium.org> on 2013-03-15
    Reviewed by Ojan Vafai.
    
    Covered by existing tests.
    
    * css/html.css:
    (select):
    (select[size][multiple]):
    (select[size="1"]):
    Use new-style flexbox properties.
    * rendering/RenderMenuList.cpp:
    (WebCore::RenderMenuList::RenderMenuList):
    (WebCore::RenderMenuList::createInnerBlock):
    (WebCore::RenderMenuList::adjustInnerStyle):
    (WebCore::RenderMenuList::removeChild):
    RenderDeprecatedFlexibleBox -> RenderFlexibleBox, and use the
    new-style flexbox properties. Use margin:auto for centering to get
    the old safe-centering behavior.
    * rendering/RenderMenuList.h:
    (RenderMenuList):
    Inherit from RenderFlexibleBox. Override baseline methods to call the
    RenderBlock methods
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145959 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderMenuList.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderMenuList.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "LayoutRect.h"
 #include "PopupMenu.h"
 #include "PopupMenuClient.h"
-#include "RenderDeprecatedFlexibleBox.h"
+#include "RenderFlexibleBox.h"
 
 #if PLATFORM(MAC)
 #define POPUP_MENU_PULLS_DOWN 0
@@ -41,7 +41,7 @@ namespace WebCore {
 class HTMLSelectElement;
 class RenderText;
 
-class RenderMenuList : public RenderDeprecatedFlexibleBox, private PopupMenuClient {
+class RenderMenuList : public RenderFlexibleBox, private PopupMenuClient {
 
 public:
     RenderMenuList(Element*);
@@ -115,6 +115,15 @@ private:
     virtual PassRefPtr<Scrollbar> createScrollbar(ScrollableArea*, ScrollbarOrientation, ScrollbarControlSize) OVERRIDE;
 
     virtual bool hasLineIfEmpty() const { return true; }
+
+    // Flexbox defines baselines differently than regular blocks.
+    // For backwards compatibility, menulists need to do the regular block behavior.
+    virtual int baselinePosition(FontBaseline baseline, bool firstLine, LineDirectionMode direction, LinePositionMode position) const OVERRIDE
+    {
+        return RenderBlock::baselinePosition(baseline, firstLine, direction, position);
+    }
+    virtual int firstLineBoxBaseline() const OVERRIDE { return RenderBlock::firstLineBoxBaseline(); }
+    virtual int inlineBlockBaseline(LineDirectionMode direction) const OVERRIDE { return RenderBlock::inlineBlockBaseline(direction); }
 
     void getItemBackgroundColor(unsigned listIndex, Color&, bool& itemHasCustomBackgroundColor) const;
 
