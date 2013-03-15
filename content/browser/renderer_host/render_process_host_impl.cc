f236165e3ea1523339a301085e4442d13390a397
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 20:41:53 +0000

    cc: Enable solid color prediction by default
    This change flips the "use-color-estimator" flag to become
    "disable-color-estimator" flag and enables color prediction
    by default.
    
    BUG=181619
    
    
    Review URL: https://chromiumcodereview.appspot.com/12408011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188467 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -880,6 +880,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kVModule,
     switches::kWebCoreLogChannels,
     cc::switches::kBackgroundColorInsteadOfCheckerboard,
+    cc::switches::kDisableColorEstimator,
     cc::switches::kDisableImplSidePainting,
     cc::switches::kDisableThreadedAnimation,
     cc::switches::kEnableCompositorFrameMessage,
@@ -904,7 +905,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     cc::switches::kTraceAllRenderedFrames,
     cc::switches::kTraceOverdraw,
     cc::switches::kUseCheapnessEstimator,
-    cc::switches::kUseColorEstimator,
     cc::switches::kCompositeToMailbox,
   };
   renderer_cmd->CopySwitchesFrom(browser_cmd, kSwitchNames,
