a66a7c976c400e22c9abd9695c9b765204d26c7c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-15 13:50:32 +0000

    [BlackBerry] FontPlatformData: remove TextOrientation parameter https://bugs.webkit.org/show_bug.cgi?id=112135
    Patch by Alberto Garcia <agarcia@igalia.com> on 2013-03-15
    Reviewed by Rob Buis.
    
    I forgot to remove the #include "TextOrientation.h" line as
    well. This file no longer exists.
    
    * platform/graphics/blackberry/FontCustomPlatformData.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145902 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/blackberry/FontCustomPlatformData.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/blackberry/FontCustomPlatformData.h
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "FontOrientation.h"
 #include "FontRenderingMode.h"
 #include "FontWidthVariant.h"
-#include "TextOrientation.h"
 #include <wtf/Noncopyable.h>
 #include <wtf/text/WTFString.h>
 
