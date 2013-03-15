17d981f10194e23d0d4ea4df8860bb1b38d9a48b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-15 07:13:51 +0000

    Build fix for Tools/GtkLauncher/Programs_GtkLauncher-main.o if gstreamer is not installed https://bugs.webkit.org/show_bug.cgi?id=112394
    Patch by Tobias Mueller <tobiasmue@gnome.org> on 2013-03-15
    Reviewed by Philippe Normand.
    
    * GtkLauncher/main.c:
    (main): Guard using the gstreamer function with #ifdef WTF_USE_GSTREAMER
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/GtkLauncher/main.c
+++ third_party/WebKit/Tools/GtkLauncher/main.c
@@ -29,7 +29,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "autotoolsconfig.h"
 #include "LauncherInspectorWindow.h"
 #include <errno.h>
+#ifdef WTF_USE_GSTREAMER
 #include <gst/gst.h>
+#endif
 #include <gtk/gtk.h>
 #include <stdlib.h>
 #include <string.h>
@@ -490,8 +492,9 @@ int main(int argc, char* argv[])
     GOptionContext *context = g_option_context_new(0);
     g_option_context_add_main_entries(context, commandLineOptions, 0);
     g_option_context_add_group(context, gtk_get_option_group(TRUE));
+#ifdef WTF_USE_GSTREAMER
     g_option_context_add_group(context, gst_init_get_option_group());
-
+#endif
     webkitSettings = webkit_web_settings_new();
     g_object_set(webkitSettings, "enable-developer-extras", TRUE, NULL);
     if (!addWebSettingsGroupToContext(context, webkitSettings)) {
