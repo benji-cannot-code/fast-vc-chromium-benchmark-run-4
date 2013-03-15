6617e286a5c67cbb91ea8a1d272304e5db1a6c22
melevin@chromium.org <melevin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 12:41:57 +0000

    Fix "es_sm" parameter broken after refactor in r186390.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12811009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188371 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.cc
+++ chrome/browser/instant/instant_controller.cc
@@ -1444,7 +1444,8 @@ bool InstantController::GetInstantURL(Profile* profile,
     return true;
   }
 
-  const GURL instant_url_obj = chrome::search::GetInstantURL(profile);
+  const GURL instant_url_obj =
+      chrome::search::GetInstantURL(profile, omnibox_bounds_.x());
   if (!instant_url_obj.is_valid())
     return false;
 
