06fdb500c07444745567231df190419912ba822c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-15 16:51:15 +0000

    Get rid of useless forward declaration. https://bugs.webkit.org/show_bug.cgi?id=112449
    Patch by Konrad Piascik <kpiascik@blackberry.com> on 2013-03-15
    Reviewed by Stephen Chenney.
    
    No behavioural change
    
    * svg/SVGRectElement.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145909 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGRectElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGRectElement.cpp
@@ -34,8 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-class RenderSVGRect;
-
 // Animated property definitions
 DEFINE_ANIMATED_LENGTH(SVGRectElement, SVGNames::xAttr, X, x)
 DEFINE_ANIMATED_LENGTH(SVGRectElement, SVGNames::yAttr, Y, y)
