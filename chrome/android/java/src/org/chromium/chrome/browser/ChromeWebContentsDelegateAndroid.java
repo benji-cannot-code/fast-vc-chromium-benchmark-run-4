55697a788ccf94bec6a3d46f77fc60eb6a4bee15
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-15 22:35:34 +0000

    [Android] Cleanup JNI usage in ChromeWebContentsDelegateAndroid.
    Adds private accessors to create types needed by the public API to avoid manual bindings.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12827005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188492 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/ChromeWebContentsDelegateAndroid.java
+++ chrome/android/java/src/org/chromium/chrome/browser/ChromeWebContentsDelegateAndroid.java
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.chrome.browser;
 
+import android.graphics.Rect;
+import android.graphics.RectF;
+
 import org.chromium.base.CalledByNative;
 import org.chromium.components.web_contents_delegate_android.WebContentsDelegateAndroid;
 
@@ -22,5 +25,36 @@ public class ChromeWebContentsDelegateAndroid extends WebContentsDelegateAndroid
 
     @CalledByNative
     public void onFindMatchRectsAvailable(FindMatchRectsDetails result) {
+    }
+
+    // Helper functions used to create types that are part of the public interface
+    @CalledByNative
+    private static Rect createRect(int x, int y, int right, int bottom) {
+        return new Rect(x, y, right, bottom);
+    }
+
+    @CalledByNative
+    private static RectF createRectF(float x, float y, float right, float bottom) {
+        return new RectF(x, y, right, bottom);
+    }
+
+    @CalledByNative
+    private static FindNotificationDetails createFindNotificationDetails(
+            int numberOfMatches, Rect rendererSelectionRect,
+            int activeMatchOrdinal, boolean finalUpdate) {
+        return new FindNotificationDetails(numberOfMatches, rendererSelectionRect,
+                activeMatchOrdinal, finalUpdate);
+    }
+
+    @CalledByNative
+    private static FindMatchRectsDetails createFindMatchRectsDetails(
+            int version, int numRects, RectF activeRect) {
+        return new FindMatchRectsDetails(version, new RectF[numRects], activeRect);
+    }
+
+    @CalledByNative
+    private static void setMatchRectByIndex(
+            FindMatchRectsDetails findMatchRectsDetails, int index, RectF rect) {
+        findMatchRectsDetails.rects[index] = rect;
     }
 }
