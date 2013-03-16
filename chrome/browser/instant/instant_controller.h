b6a2e515242746dc6d246489be44d17c61a61f12
jeremycho@chromium.org <jeremycho@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 18:58:29 +0000

    Remove support for context and shown reason from the searchbox API. The signature of showOverlay() can't be updated yet, since GWS still calls it with a reason parameter.# Enter a description of the change. BUG=
    Review URL: https://chromiumcodereview.appspot.com/12378030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188608 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/instant/instant_controller.h
+++ chrome/browser/instant/instant_controller.h
@@ -243,7 +243,6 @@ class InstantController : public InstantPage::Delegate,
       const std::vector<InstantSuggestion>& suggestions) OVERRIDE;
   virtual void ShowInstantOverlay(
       const content::WebContents* contents,
-      InstantShownReason reason,
       int height,
       InstantSizeUnits units) OVERRIDE;
   virtual void FocusOmnibox(const content::WebContents* contents) OVERRIDE;
@@ -306,9 +305,7 @@ class InstantController : public InstantPage::Delegate,
 
   // Counterpart to HideOverlay(). Asks the |browser_| to display the overlay
   // with the given |height| in |units|.
-  void ShowOverlay(InstantShownReason reason,
-                   int height,
-                   InstantSizeUnits units);
+  void ShowOverlay(int height, InstantSizeUnits units);
 
   // Send the omnibox popup bounds to the page.
   void SendPopupBoundsToPage();
--- chrome/browser/resources/local_omnibox_popup/local_omnibox_popup.js
+++ chrome/browser/resources/local_omnibox_popup/local_omnibox_popup.js
@@ -206,8 +206,7 @@ function handleNativeSuggestions() {
   }
 
   var height = getDropdownHeight();
-  // TODO(jered): Remove deprecated "reason" argument.
-  apiHandle.showOverlay(2, height);
+  apiHandle.showOverlay(height);
 }
 
 /**
