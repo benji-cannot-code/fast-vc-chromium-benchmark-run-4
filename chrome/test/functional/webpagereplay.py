6c0bed52467942c4fcaa73f20303e30c97496f08
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 05:14:58 +0000

    [Telemetry] Limit webpagereplay logs to warning level.
    The default is debug and there were a lot of debug logs that were slowing down
    page loading in Telemetry because each one needs to synchronize across threads
    and the server uses a lot of threads.
    
    BUG=None
    TEST=None
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/12517014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/webpagereplay.py
+++ chrome/test/functional/webpagereplay.py
@@ -113,6 +113,7 @@ class ReplayServer(object):
         '--ssl_port', str(self._https_port),
         '--use_closest_match',
         '--no-dns_forwarding',
+        '--log_level', 'warning'
         ]
 
   def _CheckPath(self, label, path):
