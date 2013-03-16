daf15e34b5f827a7068885f2fdbf9454fc0132c9
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 16:14:22 +0000

    Fixed artifacts in drag-and-drop thumbnail in Files.app.
    When trying to drag-and-drop a file which is not draggable (eg. not available offline, while Drive is not available), then the thumbnail had not been cleaned causing artifacts during the next (but successful) drag-and-drop try on a draggable file. Also, when clicking fast, sometimes two thumbnails were displayed. This patch solves this issue by avoiding generation of a thumbnail when dragging event is cancelled.
    
    TEST=Disconnect from Internet. Try to drag a file which if not available offline. Later, try to drag another draggable file. Thumbnail should be fine.
    BUG=181508
    
    
    Review URL: https://chromiumcodereview.appspot.com/12739005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_transfer_controller.js
+++ chrome/browser/resources/file_manager/js/file_transfer_controller.js
@@ -239,7 +239,8 @@ FileTransferController.prototype = {
   preloadThumbnailImage_: function(entry) {
     var imageUrl = entry.toURL();
     var metadataTypes = 'thumbnail|filesystem';
-    this.preloadedThumbnailImageNode_ = this.document_.createElement('div');
+    var thumbnailContainer = this.document_.createElement('div');
+    this.preloadedThumbnailImageNode_ = thumbnailContainer;
     this.preloadedThumbnailImageNode_.className = 'img-container';
     this.directoryModel_.getMetadataCache().get(
         imageUrl,
@@ -248,7 +249,7 @@ FileTransferController.prototype = {
           new ThumbnailLoader(imageUrl,
                               ThumbnailLoader.LoaderType.IMAGE,
                               metadata).
-              load(this.preloadedThumbnailImageNode_,
+              load(thumbnailContainer,
                    ThumbnailLoader.FillMode.FILL);
         }.bind(this));
   },
@@ -314,8 +315,6 @@ FileTransferController.prototype = {
     }
 
     var dt = event.dataTransfer;
-    var dragThumbnail = this.renderThumbnail_();
-    dt.setDragImage(dragThumbnail, 1000, 1000);
 
     if (this.canCopyOrDrag_(dt)) {
       if (this.canCutOrDrag_(dt))
@@ -324,7 +323,11 @@ FileTransferController.prototype = {
         this.cutOrCopy_(dt, 'copy');
     } else {
       event.preventDefault();
+      return;
     }
+
+    var dragThumbnail = this.renderThumbnail_();
+    dt.setDragImage(dragThumbnail, 1000, 1000);
 
     window[DRAG_AND_DROP_GLOBAL_DATA] = {
       sourceRoot: this.directoryModel_.getCurrentRootPath()
