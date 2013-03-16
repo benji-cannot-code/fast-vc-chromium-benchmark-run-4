2daab36dd7869349787a8fbb263dd019c673c32b
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 09:15:12 +0000

    cc: Chromify Scheduler(Settings)
    R=jamesr@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12897002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188555 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler_settings.cc
+++ cc/scheduler_settings.cc
@@ -7,13 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-SchedulerSettings::SchedulerSettings()
-  : implSidePainting(false)
-{
-}
+SchedulerSettings::SchedulerSettings() : impl_side_painting(false) {}
 
-SchedulerSettings::~SchedulerSettings()
-{
-}
+SchedulerSettings::~SchedulerSettings() {}
 
 }  // namespace cc
--- cc/scheduler_settings.h
+++ cc/scheduler_settings.h
@@ -10,13 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-class CC_EXPORT SchedulerSettings
-{
-public:
+class CC_EXPORT SchedulerSettings {
+ public:
   SchedulerSettings();
   ~SchedulerSettings();
 
-  bool implSidePainting;
+  bool impl_side_painting;
 };
 
 }  // namespace cc
