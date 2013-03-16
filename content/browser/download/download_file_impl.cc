50153d3792636ca4cc500fda7314d201dbd7a97a
rdsmith@chromium.org <rdsmith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-16 09:35:19 +0000

    Partially integrate filename determination with resumption.
    Also substantially increased test coverage.
    
    BUG=7648
    R=asanka@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12308011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188562 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/download/download_file_impl.cc
+++ content/browser/download/download_file_impl.cc
@@ -77,6 +77,9 @@ void DownloadFileImpl::Initialize(const InitializeCallback& callback) {
 
   download_start_ = base::TimeTicks::Now();
 
+  // Primarily to make reset to zero in restart visible to owner.
+  SendUpdate();
+
   // Initial pull from the straw.
   StreamActive();
 
--- content/public/test/test_file_error_injector.cc
+++ content/public/test/test_file_error_injector.cc
@@ -140,6 +140,16 @@ void DownloadFileWithErrors::Initialize(
   if (OverwriteError(
           TestFileErrorInjector::FILE_OPERATION_INITIALIZE,
           &error_to_return)) {
+    if (DOWNLOAD_INTERRUPT_REASON_NONE != error_to_return) {
+      // Don't execute a, probably successful, Initialize; just
+      // return the error.
+      BrowserThread::PostTask(
+          BrowserThread::UI, FROM_HERE, base::Bind(
+              callback, error_to_return));
+      return;
+    }
+
+    // Otherwise, just wrap the return.
     callback_to_use = base::Bind(&InitializeErrorCallback, callback,
                                  error_to_return);
   }
@@ -164,6 +174,16 @@ void DownloadFileWithErrors::RenameAndUniquify(
   if (OverwriteError(
           TestFileErrorInjector::FILE_OPERATION_RENAME_UNIQUIFY,
           &error_to_return)) {
+    if (DOWNLOAD_INTERRUPT_REASON_NONE != error_to_return) {
+      // Don't execute a, probably successful, RenameAndUniquify; just
+      // return the error.
+      BrowserThread::PostTask(
+          BrowserThread::UI, FROM_HERE, base::Bind(
+              callback, error_to_return, base::FilePath()));
+      return;
+    }
+
+    // Otherwise, just wrap the return.
     callback_to_use = base::Bind(&RenameErrorCallback, callback,
                                  error_to_return);
   }
@@ -181,6 +201,16 @@ void DownloadFileWithErrors::RenameAndAnnotate(
   if (OverwriteError(
           TestFileErrorInjector::FILE_OPERATION_RENAME_ANNOTATE,
           &error_to_return)) {
+    if (DOWNLOAD_INTERRUPT_REASON_NONE != error_to_return) {
+      // Don't execute a, probably successful, RenameAndAnnotate; just
+      // return the error.
+      BrowserThread::PostTask(
+          BrowserThread::UI, FROM_HERE, base::Bind(
+              callback, error_to_return, base::FilePath()));
+      return;
+    }
+
+    // Otherwise, just wrap the return.
     callback_to_use = base::Bind(&RenameErrorCallback, callback,
                                  error_to_return);
   }
