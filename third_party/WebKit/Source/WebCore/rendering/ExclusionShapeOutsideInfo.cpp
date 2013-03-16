1adc4b3d2f5646ee84b91fad741510e4a8f2b9c7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-16 04:40:04 +0000

    [CSS Exclusions] shape-outside on floats for circle and ellipse shapes https://bugs.webkit.org/show_bug.cgi?id=98673
    Source/WebCore:
    
    Patch by Bem Jones-Bey <bjonesbe@adobe.com> on 2013-03-15
    Reviewed by Dirk Schulze.
    
    Enable circles and ellipses for shape-outside on floats. Most of the
    code already supports them, just needed to turn them on and add tests.
    
    Tests: fast/exclusions/shape-outside-floats/shape-outside-floats-simple-circle.html
           fast/exclusions/shape-outside-floats/shape-outside-floats-simple-ellipse.html
    
    * rendering/ExclusionShapeOutsideInfo.cpp:
    (WebCore::ExclusionShapeOutsideInfo::isEnabledFor): Enable circles and
        ellipses. Also add a check for if the RenderBox is floating, since
        that test should have been there all along, as shape outside is
        only supported on floats for now.
    
    LayoutTests:
    
    Patch by Bem Jones-Bey <bjonesbe@adobe.com> on 2013-03-15
    Reviewed by Dirk Schulze.
    
    Tests for circles and ellipses on floats.
    
    * fast/exclusions/resources/rounded-rectangle.js:
    (defined): Helper function to test and see if a js value is defined.
    (convertToRoundedRect): Convert a circle or ellipse dimensions to a rounded rect.
    (generateShapeOutsideOnFloat): Add ability to generate circles and
        ellipses, since they are just special cases of rounded rectangles.
    (generateSimulatedShapeOutsideOnFloat): Add ability to simulate
        circles and ellipses, by treating them as rounded rectangles. Also
        fix minor style issue with an if statement.
    (xOutset): Remove unneeded condition in if statement.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-simple-circle-expected.html: Added.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-simple-circle.html: Added.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-simple-ellipse-expected.html: Added.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-simple-ellipse.html: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@145982 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/ExclusionShapeOutsideInfo.cpp
+++ third_party/WebKit/Source/WebCore/rendering/ExclusionShapeOutsideInfo.cpp
@@ -39,13 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 bool ExclusionShapeOutsideInfo::isEnabledFor(const RenderBox* box)
 {
-    // FIXME: Enable shape outside for non-rectangular shapes! (bug 98664)
     ExclusionShapeValue* value = box->style()->shapeOutside();
-    return value && (value->type() == ExclusionShapeValue::SHAPE)
-        && (
-            (value->shape()->type() == BasicShape::BASIC_SHAPE_RECTANGLE)
-            || (value->shape()->type() == BasicShape::BASIC_SHAPE_POLYGON)
-        );
+    return (box->isFloating() && value && value->type() == ExclusionShapeValue::SHAPE) ? value->shape() : 0;
 }
 
 bool ExclusionShapeOutsideInfo::computeSegmentsForLine(LayoutUnit lineTop, LayoutUnit lineHeight)
