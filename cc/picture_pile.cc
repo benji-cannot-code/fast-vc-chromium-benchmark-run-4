306ec32b40b72d78208cc4a3982452c4e49c8e9c
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-17 06:22:17 +0000

    cc: Remove PushProperties functions from picture piles.
    This removes all PushProperties functions from picture piles and
    forces cloning to be done during initialization. Clones are also
    stored in a const member to make sure they are not accidentally
    modified.
    
    BUG=202666
    
    
    Review URL: https://chromiumcodereview.appspot.com/12544026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188626 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/picture_pile.cc
+++ cc/picture_pile.cc
@@ -24,7 +24,7 @@ const int kPixelDistanceToRecord = 8000;
 
 namespace cc {
 
-PicturePile::PicturePile() : num_raster_threads_(0) {
+PicturePile::PicturePile() {
 }
 
 PicturePile::~PicturePile() {
@@ -154,14 +154,6 @@ void PicturePile::InvalidateRect(
                                     pred),
                      picture_list.end());
   picture_list.push_back(Picture::Create(picture_rect));
-}
-
-
-void PicturePile::PushPropertiesTo(PicturePileImpl* other) {
-  // TODO(enne): Don't create clones or push anything if nothing has changed
-  // on this layer this frame.
-  PicturePileBase::PushPropertiesTo(other);
-  other->CloneForDrawing(num_raster_threads_);
 }
 
 }  // namespace cc
