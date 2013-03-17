c76eb66f898c159c8e385a9f7485a0990016d5bd
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-17 00:48:13 +0000

    cc: Add shader compilation unit test
    R=danakj@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12767013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188617 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/gl_renderer.h
+++ cc/gl_renderer.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
+class GLRendererShaderTest;
 class OutputSurface;
 class ScopedResource;
 class StreamVideoDrawQuad;
@@ -111,6 +112,8 @@ class CC_EXPORT GLRenderer :
   virtual void FinishDrawingQuadList() OVERRIDE;
 
  private:
+  friend class GLRendererShaderTest;
+
   static void ToGLMatrix(float* gl_matrix, const gfx::Transform& transform);
   static ManagedMemoryPolicy::PriorityCutoff PriorityCutoff(
       WebKit::WebGraphicsMemoryAllocation::PriorityCutoff priority_cutoff);
@@ -198,7 +201,7 @@ class CC_EXPORT GLRenderer :
   gfx::QuadF shared_geometry_quad_;
 
   // This block of bindings defines all of the programs used by the compositor
-  // itself.
+  // itself.  Add any new programs here to GLRendererShaderTest.
 
   // Tiled layer shaders.
   typedef ProgramBinding<VertexShaderTile, FragmentShaderRGBATexAlpha>
