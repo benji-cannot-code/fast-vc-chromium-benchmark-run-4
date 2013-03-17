3a98b6b6e717ea44b3a49242f125ed8173079b04
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-17 20:09:09 +0000

    BUILD FIX (r145592): AutodrainedPool.h moved to WTF <http://webkit.org/b/112171>
    Fixes the following build failure:
    
        Source/WebCore/platform/audio/mac/AudioBusMac.mm:32:9: fatal error: 'AutodrainedPool.h' file not found
        #import "AutodrainedPool.h"
                ^
        1 error generated.
    
    * platform/audio/mac/AudioBusMac.mm: Fix include.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/audio/mac/AudioBusMac.mm
+++ third_party/WebKit/Source/WebCore/platform/audio/mac/AudioBusMac.mm
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "AudioBus.h"
 
 #import "AudioFileReader.h"
-#import "AutodrainedPool.h"
+#import <wtf/AutodrainedPool.h>
 #import <wtf/OwnPtr.h>
 #import <wtf/PassOwnPtr.h>
 #import <Foundation/Foundation.h>
