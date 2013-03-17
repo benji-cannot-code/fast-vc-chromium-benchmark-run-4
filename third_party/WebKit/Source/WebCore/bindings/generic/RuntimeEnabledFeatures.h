3cb42411e21f5603b8c2a395e07f91b8626d3db5
abarth@webkit.org <abarth@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-17 21:39:31 +0000

    Legacy CSS vendor prefixes should only work for Dashboard https://bugs.webkit.org/show_bug.cgi?id=111890
    Reviewed by Eric Seidel.
    
    Source/WebCore:
    
    Our experience with the Chromium port is that these legacy CSS vendor
    prefixes (-apple- and -khtml-) are not needed for web compatibility.
    There is reason to believe, however, that they are needed for
    compatibility with Mac OS X Dashboard widgets.
    
    This patch makes the code for these legacy CSS vendor prefixes
    enabled at runtime and only enables them when running in Dashboard
    compatibility mode. This is the first step towards the plan outlined in
    https://lists.webkit.org/pipermail/webkit-dev/2013-March/024085.html.
    
    This patch also removes support for ENABLE(LEGACY_CSS_VENDOR_PREFIXES)
    from the V8 bindings because V8 is never used with Dashboard.
    
    * bindings/generic/RuntimeEnabledFeatures.cpp:
    (WebCore):
    * bindings/generic/RuntimeEnabledFeatures.h:
    (RuntimeEnabledFeatures):
    (WebCore::RuntimeEnabledFeatures::setLegacyCSSVendorPrefixesEnabled):
    (WebCore::RuntimeEnabledFeatures::legacyCSSVendorPrefixesEnabled):
    * bindings/js/JSCSSStyleDeclarationCustom.cpp:
    (WebCore::getCSSPropertyNamePrefix):
    (WebCore::cssPropertyIDForJSCSSPropertyName):
    * bindings/v8/custom/V8CSSStyleDeclarationCustom.cpp:
    (WebCore::cssPropertyInfo):
    * css/CSSParser.cpp:
    (WebCore::CSSParser::rewriteSpecifiers):
    
    Source/WebKit/mac:
    
    Enable legacy CSS vendor prefixes when we've been asked to turn on
    Dashboard compatibility mode.
    
    * WebView/WebView.mm:
    (-[WebView _setDashboardBehavior:to:]):
    
    LayoutTests:
    
    * inspector/styles/vendor-prefixes-expected.txt:
        - Update results to show our new behavior now that -apple- and
          -khtml- are not supported.
    * platform/mac/TestExpectations:
        - Skip a test that is testing that we support -apple- prefixes.
    * platform/mac/fast/css/dashboard-region-parser.html:
        - Update test to use -webkit- rather than -apple-.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146025 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
+++ third_party/WebKit/Source/WebCore/bindings/generic/RuntimeEnabledFeatures.h
@@ -202,6 +202,11 @@ public:
     static bool webkitRTCPeerConnectionEnabled() { return peerConnectionEnabled(); }
 #endif
 
+#if ENABLE(LEGACY_CSS_VENDOR_PREFIXES)
+    static void setLegacyCSSVendorPrefixesEnabled(bool isEnabled) { isLegacyCSSVendorPrefixesEnabled = isEnabled; }
+    static bool legacyCSSVendorPrefixesEnabled() { return isLegacyCSSVendorPrefixesEnabled; }
+#endif
+
 #if ENABLE(GAMEPAD)
     static void setWebkitGetGamepadsEnabled(bool isEnabled) { isGamepadEnabled = isEnabled; }
     static bool webkitGetGamepadsEnabled() { return isGamepadEnabled; }
@@ -338,6 +343,10 @@ private:
 
 #if ENABLE(GAMEPAD)
     static bool isGamepadEnabled;
+#endif
+
+#if ENABLE(LEGACY_CSS_VENDOR_PREFIXES)
+    static bool isLegacyCSSVendorPrefixesEnabled;
 #endif
 
 #if ENABLE(QUOTA)
--- third_party/WebKit/Source/WebCore/bindings/js/JSCSSStyleDeclarationCustom.cpp
+++ third_party/WebKit/Source/WebCore/bindings/js/JSCSSStyleDeclarationCustom.cpp
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HashTools.h"
 #include "JSCSSValue.h"
 #include "JSNode.h"
+#include "RuntimeEnabledFeatures.h"
 #include "Settings.h"
 #include "StylePropertySet.h"
 #include <runtime/StringPrototype.h>
@@ -120,7 +121,7 @@ static PropertyNamePrefix getCSSPropertyNamePrefix(const StringImpl& propertyNam
     switch (firstChar) {
 #if ENABLE(LEGACY_CSS_VENDOR_PREFIXES)
     case 'a':
-        if (matchesCSSPropertyNamePrefix(propertyName, "apple"))
+        if (RuntimeEnabledFeatures::legacyCSSVendorPrefixesEnabled() && matchesCSSPropertyNamePrefix(propertyName, "apple"))
             return PropertyNamePrefixApple;
         break;
 #endif
@@ -130,7 +131,7 @@ static PropertyNamePrefix getCSSPropertyNamePrefix(const StringImpl& propertyNam
         break;
 #if ENABLE(LEGACY_CSS_VENDOR_PREFIXES)
     case 'k':
-        if (matchesCSSPropertyNamePrefix(propertyName, "khtml"))
+        if (RuntimeEnabledFeatures::legacyCSSVendorPrefixesEnabled() && matchesCSSPropertyNamePrefix(propertyName, "khtml"))
             return PropertyNamePrefixKHTML;
         break;
 #endif
@@ -223,6 +224,7 @@ static CSSPropertyInfo cssPropertyIDForJSCSSPropertyName(PropertyName propertyNa
 #if ENABLE(LEGACY_CSS_VENDOR_PREFIXES)
     case PropertyNamePrefixApple:
     case PropertyNamePrefixKHTML:
+        ASSERT(RuntimeEnabledFeatures::legacyCSSVendorPrefixesEnabled());
         writeWebKitPrefix(bufferPtr);
         i += 5;
         break;
