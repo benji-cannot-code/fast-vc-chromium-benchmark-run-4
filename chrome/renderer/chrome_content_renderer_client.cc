bf7fb10c8dd1f2fb5ff3178eabbcdb29938c282c
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-18 21:22:04 +0000

    Fix OOP iframe prototype to allow BrowserPlugin outside webview. Only matters for --site-per-process --enable-browser-plugin-for-all-view-types.
    BUG=99379
    TEST=Pages with cross-site iframes should render.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12512009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188825 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_content_renderer_client.cc
+++ chrome/renderer/chrome_content_renderer_client.cc
@@ -1106,6 +1106,10 @@ void ChromeContentRendererClient::RegisterPPAPIInterfaceFactories(
 
 bool ChromeContentRendererClient::AllowBrowserPlugin(
     WebKit::WebPluginContainer* container) const {
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableBrowserPluginForAllViewTypes))
+    return true;
+
   // If this |BrowserPlugin| <object> in the |container| is not inside a
   // <webview> shadowHost, we disable instantiating this plugin. This is to
   // discourage and prevent developers from accidentally attaching <object>
@@ -1121,13 +1125,8 @@ bool ChromeContentRendererClient::AllowBrowserPlugin(
   if (container->element().shadowHost().isNull())
     return false;
 
-  if (container->element().shadowHost().tagName().equals(
-          WebKit::WebString::fromUTF8(kWebViewTagName))) {
-    return true;
-  } else {
-    return CommandLine::ForCurrentProcess()->HasSwitch(
-        switches::kEnableBrowserPluginForAllViewTypes);
-  }
+  return container->element().shadowHost().tagName().equals(
+      WebString::fromUTF8(kWebViewTagName));
 }
 
 }  // namespace chrome
