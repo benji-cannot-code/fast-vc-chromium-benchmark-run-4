2324bea4a1bae263b1f6a874055348c7688b0489
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-18 17:13:59 +0000

    Add web setting for smartInsertDeleteEnabled
    It is used in layout tests where the default values differ
    
    BUG=111316
    R=tony@chromium.org,jam@chromium.org
    
    Review URL: https://codereview.chromium.org/12772007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188757 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.cc
+++ webkit/glue/webpreferences.cc
@@ -135,6 +135,11 @@ WebPreferences::WebPreferences()
       supports_multiple_windows(true),
       viewport_enabled(false),
       initialize_at_minimum_page_scale(true),
+#if defined(OS_MACOSX)
+      smart_insert_delete_enabled(true),
+#else
+      smart_insert_delete_enabled(false),
+#endif
       cookie_enabled(true)
 #if defined(OS_ANDROID)
       ,
@@ -453,6 +458,8 @@ void WebPreferences::Apply(WebView* web_view) const {
 
   settings->setViewportEnabled(viewport_enabled);
   settings->setInitializeAtMinimumPageScale(initialize_at_minimum_page_scale);
+
+  settings->setSmartInsertDeleteEnabled(smart_insert_delete_enabled);
 
 #if defined(OS_ANDROID)
   settings->setAllowCustomScrollbarInMainFrame(false);
