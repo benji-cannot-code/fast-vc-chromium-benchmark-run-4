a617211db4fb6e15bacf1ca3641924b07d50ba70
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-18 14:13:00 +0000

    Web Inspector: TabbedEditorContainer does not restore last shown file on reload sometimes. https://bugs.webkit.org/show_bug.cgi?id=112561
    Reviewed by Alexander Pavlov.
    
    * inspector/front-end/TabbedEditorContainer.js:
    (WebInspector.TabbedEditorContainer.prototype.addUISourceCode):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146062 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/TabbedEditorContainer.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/TabbedEditorContainer.js
@@ -198,7 +198,7 @@ WebInspector.TabbedEditorContainer.prototype = {
 
         // Select tab if this file was the last to be shown.
         if (!index) {
-            this._innerShowFile(uiSourceCode, true);
+            this._innerShowFile(uiSourceCode, false);
             return;
         }
 
@@ -206,7 +206,7 @@ WebInspector.TabbedEditorContainer.prototype = {
         var addedProjectType = uiSourceCode.project().type();
         var snippetsProjectType = WebInspector.projectTypes.Snippets;
         if (this._history.index(this._currentFile.uri()) && currentProjectType === snippetsProjectType && addedProjectType !== snippetsProjectType)
-            this._innerShowFile(uiSourceCode, true);
+            this._innerShowFile(uiSourceCode, false);
     },
 
     /**
