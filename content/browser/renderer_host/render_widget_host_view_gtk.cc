ce564e8187a5a01d7bb24037df2ae6d7a5b8f0aa
mthiesse@chromium.org <mthiesse@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-18 22:27:53 +0000

    Change RenderWidgetHostViewGtk::HasFocus() to check global focus instead of top-level focus.
    See http://people.gnome.org/~shaunm/girdoc/C/Gtk.Widget.is_focus.html vs http://people.gnome.org/~shaunm/girdoc/C/Gtk.Widget.has_focus.html
    
    BUG=172623
    TEST=Please manually check via https://crbug.com/172623 description.
    
    Review URL: https://chromiumcodereview.appspot.com/12377084
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188851 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_gtk.cc
+++ content/browser/renderer_host/render_widget_host_view_gtk.cc
@@ -774,7 +774,7 @@ void RenderWidgetHostViewGtk::Blur() {
 }
 
 bool RenderWidgetHostViewGtk::HasFocus() const {
-  return gtk_widget_is_focus(view_.get());
+  return gtk_widget_has_focus(view_.get());
 }
 
 void RenderWidgetHostViewGtk::ActiveWindowChanged(GdkWindow* window) {
