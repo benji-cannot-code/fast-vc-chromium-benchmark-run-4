27d50ff6969504f803fff007eab2b1c1f9f3fe35
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-18 15:29:53 +0000

    Remember and use user gesture tokens in the PPAPI to manage user gesture state
    Before, the PPAPI would indicate up to 10s after the last user gesture that
    we're processing a user gesture. This is done so out of process plugins have
    a chance to respond to a user gesture/
    
    By using a user gesture token, we can make sure that the user gesture can only
    be consumed once within the 10s timeframe
    
    BUG=181500
    TEST=none
    R=dmichael@chromium.org
    
    Review URL: https://codereview.chromium.org/12700004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188743 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/ppapi/ppapi_plugin_instance.cc
+++ webkit/plugins/ppapi/ppapi_plugin_instance.cc
@@ -137,6 +137,7 @@ using WebKit::WebScopedUserGesture;
 using WebKit::WebString;
 using WebKit::WebURLRequest;
 using WebKit::WebUserGestureIndicator;
+using WebKit::WebUserGestureToken;
 using WebKit::WebView;
 
 namespace webkit {
@@ -782,6 +783,8 @@ bool PluginInstance::HandleInputEvent(const WebKit::WebInputEvent& event,
       if (WebUserGestureIndicator::isProcessingUserGesture()) {
         pending_user_gesture_ =
             ::ppapi::EventTimeToPPTimeTicks(event.timeStampSeconds);
+        pending_user_gesture_token_ =
+            WebUserGestureIndicator::currentUserGestureToken();
       }
 
       // Each input event may generate more than one PP_InputEvent.
@@ -1443,7 +1446,7 @@ bool PluginInstance::SetFullscreen(bool fullscreen) {
 
   if (fullscreen) {
     // Create the user gesture in case we're processing one that's pending.
-    WebScopedUserGesture user_gesture;
+    WebScopedUserGesture user_gesture(CurrentUserGestureToken());
     // WebKit does not resize the plugin to fill the screen in fullscreen mode,
     // so we will tweak plugin's attributes to support the expected behavior.
     KeepSizeAttributesBeforeFullscreen();
@@ -1516,7 +1519,7 @@ void PluginInstance::UpdateFlashFullscreenState(bool flash_fullscreen) {
     } else {
       // Open a user gesture here so the Webkit user gesture checks will succeed
       // for out-of-process plugins.
-      WebScopedUserGesture user_gesture;
+      WebScopedUserGesture user_gesture(CurrentUserGestureToken());
       if (!delegate()->LockMouse(this))
         lock_mouse_callback_->Run(PP_ERROR_FAILED);
     }
@@ -1750,7 +1753,14 @@ bool PluginInstance::IsProcessingUserGesture() {
       ::ppapi::TimeTicksToPPTimeTicks(base::TimeTicks::Now());
   // Give a lot of slack so tests won't be flaky.
   const PP_TimeTicks kUserGestureDurationInSeconds = 10.0;
-  return (now - pending_user_gesture_ < kUserGestureDurationInSeconds);
+  return pending_user_gesture_token_.hasGestures() &&
+         (now - pending_user_gesture_ < kUserGestureDurationInSeconds);
+}
+
+WebUserGestureToken PluginInstance::CurrentUserGestureToken() {
+  if (!IsProcessingUserGesture())
+    pending_user_gesture_token_ = WebUserGestureToken();
+  return pending_user_gesture_token_;
 }
 
 void PluginInstance::OnLockMouseACK(bool succeeded) {
@@ -1968,7 +1978,7 @@ PP_Var PluginInstance::ExecuteScript(PP_Instance instance,
   NPVariant result;
   bool ok = false;
   if (IsProcessingUserGesture()) {
-    WebKit::WebScopedUserGesture user_gesture;
+    WebKit::WebScopedUserGesture user_gesture(CurrentUserGestureToken());
     ok = WebBindings::evaluate(NULL, frame->windowObject(), &np_script,
                                &result);
   } else {
@@ -2240,7 +2250,7 @@ int32_t PluginInstance::LockMouse(PP_Instance instance,
   if (!FlashIsFullscreenOrPending() || flash_fullscreen()) {
     // Open a user gesture here so the Webkit user gesture checks will succeed
     // for out-of-process plugins.
-    WebScopedUserGesture user_gesture;
+    WebScopedUserGesture user_gesture(CurrentUserGestureToken());
     if (!delegate()->LockMouse(this))
       return PP_ERROR_FAILED;
   }
