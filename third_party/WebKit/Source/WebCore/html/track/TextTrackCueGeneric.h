71937ad16e1117f9eb9cbdd9a26056dc10ebabf7
jer.noble@apple.com <jer.noble@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-18 22:51:31 +0000

    Text track cues do not update sizes when entering or exiting full screen. https://bugs.webkit.org/show_bug.cgi?id=112472
    Reviewed by Eric Carlson.
    
    The style properties of the TextCueBoxes were not being updated after the size of the video bounds changed.
    Because getDisplayTree() will not do it when the cue itself has not changed, explicitly call videoSizeDidChange()
    from updateSizes().
    
    * html/shadow/MediaControlElements.cpp:
    (WebCore::MediaControlTextTrackContainerElement::updateSizes): call videoSizeDidChange() on every active cue.
    * html/track/TextTrackCue.h:
    (WebCore::TextTrackCue::hasDisplayTree): returns whether or not the cue has created a display tree without side effects.
    (WebCore::TextTrackCue::videoSizeDidChange): Overridable empty method.
    * html/track/TextTrackCueGeneric.cpp:
    (WebCore::TextTrackCueGeneric::videoSizeDidChange): Update the CSS height attribute of the cue box using the new video size.
    * html/track/TextTrackCueGeneric.h:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146140 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/track/TextTrackCueGeneric.h
+++ third_party/WebKit/Source/WebCore/html/track/TextTrackCueGeneric.h
@@ -69,6 +69,8 @@ public:
     Color backgroundColor() const { return m_backgroundColor; }
     void setBackgroundColor(RGBA32 color) { m_backgroundColor.setRGB(color); }
 
+    virtual void videoSizeDidChange(const IntSize&);
+
     virtual bool operator==(const TextTrackCue&) const OVERRIDE;
     virtual bool operator!=(const TextTrackCue& cue) const OVERRIDE
     {
