7bbc3b8717435fdd13e79ec20694b689585a9d66
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-18 16:13:33 +0000

    Access settings.defaultVideoPosterURL https://bugs.webkit.org/show_bug.cgi?id=112378
    Patch by Tao Bai <michaelbai@chromium.org> on 2013-03-18
    Reviewed by Dimitri Glazkov.
    
    The defaultVideoPosterURL setting has been added in
    https://bugs.webkit.org/show_bug.cgi?id=110263,
    this added methods to access it in Chromium.
    The settings will be used by Android WebView API
    WebChromeClient.getDefaultVideoPoster()
    
    * public/WebSettings.h: add setDefaultVideoPosterURL
    * src/WebSettingsImpl.cpp:
    (WebKit::WebSettingsImpl::setDefaultVideoPosterURL): set defaultVideoPosterURL
    * src/WebSettingsImpl.h: add setDefaultVideoPosterURL
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
@@ -83,6 +83,7 @@ public:
     virtual void setDefaultFontSize(int);
     virtual void setDefaultTextEncodingName(const WebString&);
     virtual void setDefaultTileSize(WebSize);
+    virtual void setDefaultVideoPosterURL(const WebString&);
     virtual void setDeferred2dCanvasEnabled(bool);
     virtual void setDeferredImageDecodingEnabled(bool);
     virtual void setDeveloperExtrasEnabled(bool);
