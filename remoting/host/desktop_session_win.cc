b1c97672c8d7940035e35e07383fe9be680d54a2
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-18 20:33:33 +0000

    Cosmetic changes split out from r188673:
    1. Added a DCHECK verifying that ConsoleSession::InjectSas() is called on the right thread. 
    2. Added a TODO explaining the role of RdpSession::InjectSas() and referencing the corresponding bug.
    
    BUG=137696
    TBR=jamiewalch@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12529018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188800 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/desktop_session_win.cc
+++ remoting/host/desktop_session_win.cc
@@ -192,6 +192,8 @@ void ConsoleSession::SetScreenResolution(const ScreenResolution& resolution) {
 }
 
 void ConsoleSession::InjectSas() {
+  DCHECK(caller_task_runner()->BelongsToCurrentThread());
+
   if (!sas_injector_)
     sas_injector_ = SasInjector::Create();
   if (!sas_injector_->InjectSas())
@@ -288,6 +290,8 @@ void RdpSession::SetScreenResolution(const ScreenResolution& resolution) {
 void RdpSession::InjectSas() {
   DCHECK(caller_task_runner()->BelongsToCurrentThread());
 
+  // TODO(alexeypa): implement SAS injection for RDP sessions here.
+  // See http://crbug.com/137696.
   NOTIMPLEMENTED();
 }
 
