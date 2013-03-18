4f6ef24a9379593d6e912b4a51942469a3bfe05f
dmazzoni@google.com <dmazzoni@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-18 06:25:47 +0000

    Support Windows HTMLSelectElement keystrokes on Chromium win https://bugs.webkit.org/show_bug.cgi?id=112460
    Reviewed by Kent Tamura.
    
    Compile in the windows-specific variant of
    HTMLSelectElement::platformHandleKeydownEvent
    on Chromium win, in addition to PLATFORM(WIN).
    
    * WebCore.gypi:
        Add html/HTMLSelectElementWin.cpp.
    * html/HTMLSelectElement.cpp:
        Don't compile generic platformHandleKeydownEvent on
        Chromium win.
    * html/HTMLSelectElementWin.cpp:
        Only compile platformHandleKeydownEvent on Windows.
    (WebCore):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146035 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/HTMLSelectElementWin.cpp
+++ third_party/WebKit/Source/WebCore/html/HTMLSelectElementWin.cpp
@@ -27,6 +27,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "HTMLSelectElement.h"
 
+#if OS(WINDOWS)
+
 #include "Element.h"
 #include "KeyboardEvent.h"
 #include "RenderMenuList.h"
@@ -56,3 +58,5 @@ bool HTMLSelectElement::platformHandleKeydownEvent(KeyboardEvent* event)
 }
 
 }
+
+#endif
