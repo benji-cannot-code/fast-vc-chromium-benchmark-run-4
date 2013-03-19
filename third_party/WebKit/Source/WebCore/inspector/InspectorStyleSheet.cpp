598110dfbd931343cdb14e3d8d67e11d099d2ba7
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 07:50:24 +0000

    Web Inspector: [Styles] Matching @host rule children are not displayed https://bugs.webkit.org/show_bug.cgi?id=112664
    Reviewed by Vsevolod Vlasov.
    
    Source/WebCore:
    
    Process host rules' contents akin to media rules' when handling the CSS model and source data.
    
    Test: platform/chromium/inspector/styles/host-rules.html
    
    * inspector/InspectorStyleSheet.cpp:
    (flattenSourceData):
    (WebCore::asCSSRuleList):
    
    LayoutTests:
    
    * platform/chromium/inspector/styles/host-rules-expected.txt: Added.
    * platform/chromium/inspector/styles/host-rules.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146184 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorStyleSheet.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorStyleSheet.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "InspectorStyleSheet.h"
 
+#include "CSSHostRule.h"
 #include "CSSImportRule.h"
 #include "CSSMediaRule.h"
 #include "CSSParser.h"
@@ -113,6 +114,10 @@ static void flattenSourceData(RuleSourceDataList* dataList, RuleSourceDataList* 
             target->append(data);
         else if (data->type == CSSRuleSourceData::MEDIA_RULE)
             flattenSourceData(&data->childRules, target);
+#if ENABLE(SHADOW_DOM)
+        else if (data->type == CSSRuleSourceData::HOST_RULE)
+            flattenSourceData(&data->childRules, target);
+#endif
 #if ENABLE(CSS3_CONDITIONAL_RULES)
         else if (data->type == CSSRuleSourceData::SUPPORTS_RULE)
             flattenSourceData(&data->childRules, target);
@@ -223,6 +228,11 @@ static PassRefPtr<CSSRuleList> asCSSRuleList(CSSRule* rule)
 
     if (rule->type() == CSSRule::WEBKIT_KEYFRAMES_RULE)
         return static_cast<WebKitCSSKeyframesRule*>(rule)->cssRules();
+
+#if ENABLE(SHADOW_DOM)
+    if (rule->type() == CSSRule::HOST_RULE)
+        return static_cast<CSSHostRule*>(rule)->cssRules();
+#endif
 
 #if ENABLE(CSS3_CONDITIONAL_RULES)
     if (rule->type() == CSSRule::SUPPORTS_RULE)
