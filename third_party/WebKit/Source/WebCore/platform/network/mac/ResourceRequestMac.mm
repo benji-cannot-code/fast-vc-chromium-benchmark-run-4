952954c292f13a4cd49eb125e4419be01cf608da
ddkilzer@apple.com <ddkilzer@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 04:15:53 +0000

    BUILD FIX (r146088): ResourceRequest::cfURLRequest() is defined twice on iOS <http://webkit.org/b/112387>
    * platform/network/mac/ResourceRequestMac.mm:
    (WebCore::ResourceRequest::cfURLRequest): Move method into
    !USE(CFNETWORK) section since it's also defined in
    cf/ResourceRequestCFNet.cpp on iOS.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146177 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/network/mac/ResourceRequestMac.mm
+++ third_party/WebKit/Source/WebCore/platform/network/mac/ResourceRequestMac.mm
@@ -56,11 +56,6 @@ NSURLRequest *ResourceRequest::nsURLRequest(HTTPBodyUpdatePolicy bodyPolicy) con
     return [[m_nsRequest.get() retain] autorelease];
 }
 
-CFURLRequestRef ResourceRequest::cfURLRequest(HTTPBodyUpdatePolicy bodyPolicy) const
-{
-    return [nsURLRequest(bodyPolicy) _CFURLRequest];
-}
-
 #if USE(CFNETWORK)
 
 ResourceRequest::ResourceRequest(NSURLRequest *nsRequest)
@@ -77,6 +72,11 @@ void ResourceRequest::updateNSURLRequest()
 }
 
 #else
+
+CFURLRequestRef ResourceRequest::cfURLRequest(HTTPBodyUpdatePolicy bodyPolicy) const
+{
+    return [nsURLRequest(bodyPolicy) _CFURLRequest];
+}
 
 void ResourceRequest::doUpdateResourceRequest()
 {
