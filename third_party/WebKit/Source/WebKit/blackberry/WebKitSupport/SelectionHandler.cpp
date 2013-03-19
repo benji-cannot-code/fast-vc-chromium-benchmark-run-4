4f37f406d01546de867c0c000b2a97c050a1e95f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 18:31:05 +0000

    [BlackBerry] When selecting an object, assign focus to new node instead of clearing previous focused node https://bugs.webkit.org/show_bug.cgi?id=112707
    Patch by Andy Chen <andchen@blackberry.com> on 2013-03-19
    Reviewed by Rob Buis.
    
    In SelectionHandler, when selecting a new object, we should set focus
    to the anchor node of selection instead of removing it from the old input.
    In InputHandler, when focusedNodeChanged is called, if the new node is
    not an Element node, we need to set focus to its Element parent node,
    instead of removing focus from the old node.
    PR 304631
    Internally reviewed by Mike Fenton.
    
    * WebKitSupport/InputHandler.cpp:
    (BlackBerry::WebKit::InputHandler::focusedNodeChanged):
    * WebKitSupport/SelectionHandler.cpp:
    (BlackBerry::WebKit::SelectionHandler::selectObject):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/SelectionHandler.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/SelectionHandler.cpp
@@ -24,12 +24,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "Document.h"
 #include "FatFingers.h"
 #include "FloatQuad.h"
+#include "FocusController.h"
 #include "Frame.h"
 #include "FrameSelection.h"
 #include "FrameView.h"
 #include "HitTestResult.h"
 #include "InputHandler.h"
 #include "IntRect.h"
+#include "Page.h"
 #include "RenderLayer.h"
 #include "SelectionOverlay.h"
 #include "TouchEventHandler.h"
@@ -881,12 +883,13 @@ void SelectionHandler::selectObject(const WebCore::IntPoint& location, TextGranu
         Platform::IntPoint(location).toString().c_str());
 
     WebCore::IntPoint relativePoint = DOMSupport::convertPointToFrame(m_webPage->mainFrame(), focusedFrame, location);
-    // Clear input focus if we're not selecting in old input field.
-    if (!m_webPage->m_inputHandler->boundingBoxForInputField().contains(relativePoint))
-        m_webPage->clearFocusNode();
 
     VisiblePosition pointLocation(focusedFrame->visiblePositionForPoint(relativePoint));
     VisibleSelection selection = VisibleSelection(pointLocation, pointLocation);
+
+    // Move focus to the new node if we're not selecting in old input field.
+    if (!m_webPage->m_inputHandler->boundingBoxForInputField().contains(relativePoint))
+        m_webPage->m_page->focusController()->setFocusedNode(selection.start().anchorNode(), focusedFrame);
 
     m_selectionActive = expandSelectionToGranularity(focusedFrame, selection, granularity, m_webPage->m_inputHandler->isInputMode());
 }
