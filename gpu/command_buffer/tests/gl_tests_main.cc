3e81009d85b24c736eeb2b2225c66ba275ecdcfd
gman@chromium.org <gman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 21:26:50 +0000

    Make gl_tests apk for android
    BUG=180432
    
    
    Review URL: https://chromiumcodereview.appspot.com/12502009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189109 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/tests/gl_tests_main.cc
+++ gpu/command_buffer/tests/gl_tests_main.cc
@@ -18,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 int main(int argc, char** argv) {
+#if !defined(OS_ANDROID)
   base::AtExitManager exit_manager;
+#endif
   CommandLine::Init(argc, argv);
 #if defined(OS_MACOSX)
   base::mac::ScopedNSAutoreleasePool pool;
