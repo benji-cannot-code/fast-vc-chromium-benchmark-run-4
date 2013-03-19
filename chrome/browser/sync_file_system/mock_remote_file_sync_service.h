0be6b61cae2b8272f6fb8a431dedc6eb11875aa8
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 20:37:06 +0000

    Revert 189036 "SyncFS: store disabled origins in DriveMetadataStore"
    ExtensionSettingsFrontendTest.UnlimitedStorageForLocalButNotSync started timing
    out after this landed:
    
    http://build.chromium.org/p/chromium.memory.fyi/builders/Chromium%20OS%20%28valgrind%29%286%29/builds/15684/steps/memory%20test%3A%20unit/logs/stdio
    
    (Speculative, I'll revert it back in if it doesn't help.)
    
    
    > SyncFS: store disabled origins in DriveMetadataStore
    > 
    > Store disabled origins when extensions are disabled. Current implementation stores origins only for installed and enabled apps (i.e. batch sync origins and incremental sync origins). Therefore we cannot access files and metadata related to installed but disabled apps.
    > 
    > This makes a difference between installing/uninstalling and enabling/disabling extensions.
    > - register an origin when an extension is launched for the first time.
    > - disable the origin when the extension is disabled (not unregister).
    > - enable the origin when the extension is enabled.
    > - unregister the origin when the extension is uninstalled.
    > 
    > BUG=184742
    > TEST=unit_test
    > TEST=manual test (launch, reload, restart, disable, enable and uninstall an app)
    > NOTRY=true
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12744008
    
    TBR=nhiroki@chromium.org
    Review URL: https://codereview.chromium.org/12478011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/mock_remote_file_sync_service.h
+++ chrome/browser/sync_file_system/mock_remote_file_sync_service.h
@@ -38,10 +38,6 @@ class MockRemoteFileSyncService : public RemoteFileSyncService {
                void(const GURL& origin, const SyncStatusCallback& callback));
   MOCK_METHOD2(UnregisterOriginForTrackingChanges,
                void(const GURL& origin, const SyncStatusCallback& callback));
-  MOCK_METHOD2(EnableOriginForTrackingChanges,
-               void(const GURL& origin, const SyncStatusCallback& callback));
-  MOCK_METHOD2(DisableOriginForTrackingChanges,
-               void(const GURL& origin, const SyncStatusCallback& callback));
   MOCK_METHOD2(DeleteOriginDirectory,
                void(const GURL& origin, const SyncStatusCallback& callback));
   MOCK_METHOD1(ProcessRemoteChange,
--- chrome/browser/sync_file_system/sync_file_system_service.h
+++ chrome/browser/sync_file_system/sync_file_system_service.h
@@ -122,11 +122,6 @@ class SyncFileSystemService
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
-  void HandleExtensionUnloaded(int type,
-                               const content::NotificationDetails& details);
-  void HandleExtensionEnabled(int type,
-                              const content::NotificationDetails& details);
-
   // ProfileSyncServiceObserver:
   virtual void OnStateChanged() OVERRIDE;
 
