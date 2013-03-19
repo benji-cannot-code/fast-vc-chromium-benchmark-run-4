d111c257ade8dac935f9b3531b5a0291b54dc60c
oliver@apple.com <oliver@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 20:59:14 +0000

    2013-03-19  Oliver Hunt  <oliver@apple.com>
            RELEASE_ASSERT fires in exception handler lookup
    
            RS=Geoff Garen.
    
            Temporarily switch this RELEASE_ASSERT into a regular ASSERT
            as currently this is producing fairly bad crashiness.
    
            * bytecode/CodeBlock.cpp:
            (JSC::CodeBlock::handlerForBytecodeOffset):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146255 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.cpp
+++ third_party/WebKit/Source/JavaScriptCore/bytecode/CodeBlock.cpp
@@ -2499,7 +2499,7 @@ void CodeBlock::stronglyVisitWeakReferences(SlotVisitor& visitor)
 
 HandlerInfo* CodeBlock::handlerForBytecodeOffset(unsigned bytecodeOffset)
 {
-    RELEASE_ASSERT(bytecodeOffset < instructions().size());
+    ASSERT(bytecodeOffset < instructions().size());
 
     if (!m_rareData)
         return 0;
