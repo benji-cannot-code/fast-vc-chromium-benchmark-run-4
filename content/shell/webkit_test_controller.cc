a6bce1a90cddb22cb43a609158af7fb73701568a
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 00:17:03 +0000

    content shell drt: Make sure to always print #CRASHED lines.
    Before, they were dropped if crashes happened after text output had already
    been printed (e.g. if a test crashed while generating the output bitmap).
    
    BUG=218886
    
    Review URL: https://codereview.chromium.org/12751008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188882 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_controller.cc
+++ content/shell/webkit_test_controller.cc
@@ -150,12 +150,12 @@ void WebKitTestResultPrinter::AddMessageRaw(const std::string& message) {
 }
 
 void WebKitTestResultPrinter::AddErrorMessage(const std::string& message) {
+  if (!capture_text_only_)
+    *error_ << message << "\n";
   if (state_ != DURING_TEST)
     return;
   PrintTextHeader();
   *output_ << message << "\n";
-  if (!capture_text_only_)
-    *error_ << message << "\n";
   PrintTextFooter();
   PrintImageFooter();
 }
