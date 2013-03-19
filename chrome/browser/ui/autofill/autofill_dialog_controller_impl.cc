38ba2589aebfb308366ee091e6a1601cde6681e7
ahutter@chromium.org <ahutter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 20:24:10 +0000

    Implement UpdateAddress API call for Online Wallet.
    BUG=180148
    
    
    Review URL: https://chromiumcodereview.appspot.com/12869004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189081 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -1180,6 +1180,13 @@ void AutofillDialogControllerImpl::OnDidSendAutocheckoutStatus() {
   NOTIMPLEMENTED();
 }
 
+void AutofillDialogControllerImpl::OnDidUpdateAddress(
+    const std::string& address_id,
+    const std::vector<wallet::RequiredAction>& required_actions) {
+  // TODO(dbeam): Handle this callback.
+  NOTIMPLEMENTED() << " address_id=" << address_id;
+}
+
 void AutofillDialogControllerImpl::OnDidUpdateInstrument(
     const std::string& instrument_id,
     const std::vector<wallet::RequiredAction>& required_actions) {
