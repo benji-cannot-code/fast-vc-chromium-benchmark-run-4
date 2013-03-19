4ae83ae2a7728576fa46049905f16fb4e7d398c7
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 17:52:35 +0000

    [BlackBerry] Enable USE_SYSTEM_MALLOC by default https://bugs.webkit.org/show_bug.cgi?id=112365
    Patch by Alberto Garcia <agarcia@igalia.com> on 2013-03-19
    Reviewed by Benjamin Poulain.
    
    Enable USE_SYSTEM_MALLOC by default via FeatureList.pm instead of
    harcoding it in wtf/Platform.h
    
    Source/WTF:
    
    * wtf/Platform.h:
    
    Tools:
    
    * Scripts/webkitperl/FeatureList.pm:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146222 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -498,10 +498,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
-#if OS(QNX)
-#define USE_SYSTEM_MALLOC 1
-#endif
-
 #if PLATFORM(BLACKBERRY)
 #define WTF_USE_SKIA 1
 #define WTF_USE_LOW_QUALITY_IMAGE_INTERPOLATION 1
