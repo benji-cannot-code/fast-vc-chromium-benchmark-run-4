bc46cb5ac8d2dce7be683c451f9268cacf17448c
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 23:32:19 +0000

    Revert 189135 "[content shell] close the main window after check..."
    It broke mac bots (http://build.chromium.org/p/chromium.mac/buildstatus?builder=Mac10.7%20Tests%20%281%29&number=9697)
    
    > [content shell] close the main window after checking layout system deps
    > 
    > BUG=none
    > 
    > Review URL: https://codereview.chromium.org/12594007
    
    TBR=jochen@chromium.org
    Review URL: https://codereview.chromium.org/12871018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189152 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_browser_main.cc
+++ content/shell/shell_browser_main.cc
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/utf_string_conversions.h"
 #include "content/public/browser/browser_main_runner.h"
-#include "content/shell/shell.h"
 #include "content/shell/shell_switches.h"
 #include "content/shell/webkit_test_controller.h"
 #include "net/base/net_util.h"
@@ -119,7 +118,6 @@ int ShellBrowserMain(const content::MainFunctionParams& parameters) {
         switches::kCheckLayoutTestSysDeps)) {
     MessageLoop::current()->PostTask(FROM_HERE, MessageLoop::QuitClosure());
     main_runner_->Run();
-    content::Shell::CloseAllWindows();
     main_runner_->Shutdown();
     return 0;
   }
