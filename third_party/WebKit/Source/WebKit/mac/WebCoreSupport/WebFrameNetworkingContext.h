0e058b5f67c4d07f941da2a1bd5e430cb72e9960
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 20:04:38 +0000

    Mac build fix attempt after r146239.
    * WebCoreSupport/WebFrameNetworkingContext.h:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146242 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebFrameNetworkingContext.h
+++ third_party/WebKit/Source/WebKit/mac/WebCoreSupport/WebFrameNetworkingContext.h
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 */
 
 #import <WebCore/FrameNetworkingContext.h>
+#import <wtf/SchedulePair.h>
 
 class WebFrameNetworkingContext : public WebCore::FrameNetworkingContext {
 public:
