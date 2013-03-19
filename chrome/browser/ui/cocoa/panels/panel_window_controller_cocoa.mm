a5f0fa74af64f01a7b1b63cef70a7c4a37e2b961
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 21:26:31 +0000

    Fix panel showing logic for multiple spaces under OSX: 1) Now docked panels will appear on all spaces. Detached panels will still be in current space. 2) When a chrome window enters fullscreen or presentation mode, all existing panels in current space will become hidden. 3) If the user switches to other space without leaving fullscreen or presentation mode for that chrome window, panels will appear. 4) If the chrome window leaves fullscreen or presentation mode, panels will appear.
    Docked panels uses NSWindowCollectionBehaviorCanJoinAllSpaces for the collection behavior in order to appear in all spaces.
    
    The fullscreen is on when the following criteria are all met:
    1) The last active browser window is in fullscreen or presentation mode.
    2) The last active browser window is in active work space.
    
    BUG=103473,153588
    TEST=Manual test by opening panels with multiple spaces on
    
    Review URL: https://chromiumcodereview.appspot.com/12461017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189108 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/panels/panel_window_controller_cocoa.mm
+++ chrome/browser/ui/cocoa/panels/panel_window_controller_cocoa.mm
@@ -398,7 +398,7 @@ NSCursor* LoadWebKitCursor(WebKit::WebCursorInfo::Type type) {
 
   [self updateWindowLevel];
 
-  [window setCollectionBehavior:NSWindowCollectionBehaviorParticipatesInCycle];
+  [self updateWindowCollectionBehavior];
 
   [titlebar_view_ attach];
 
@@ -852,7 +852,7 @@ NSCursor* LoadWebKitCursor(WebKit::WebCursorInfo::Type type) {
   // to same normal level will not move it below the full-screen window. Thus
   // we need to reorder the panel window.
   if (isFullScreen)
-    [[self window] orderBack:nil];
+    [[self window] orderOut:nil];
   else
     [[self window] orderFrontRegardless];
 }
@@ -881,10 +881,8 @@ NSCursor* LoadWebKitCursor(WebKit::WebCursorInfo::Type type) {
 - (void)updateWindowLevel:(BOOL)panelIsMinimized {
   if (![self isWindowLoaded])
     return;
-  // Make sure we don't draw on top of a window in full screen mode.
   Panel* panel = windowShim_->panel();
-  if (panel->manager()->display_settings_provider()->is_full_screen() ||
-      !panel->IsAlwaysOnTop()) {
+  if (!panel->IsAlwaysOnTop()) {
     [[self window] setLevel:NSNormalWindowLevel];
     return;
   }
@@ -908,6 +906,16 @@ NSCursor* LoadWebKitCursor(WebKit::WebCursorInfo::Type type) {
     return;
   }
   [[self window] setLevel:NSDockWindowLevel];
+}
+
+- (void)updateWindowCollectionBehavior {
+  if (![self isWindowLoaded])
+    return;
+  NSWindowCollectionBehavior collectionBehavior =
+      NSWindowCollectionBehaviorParticipatesInCycle;
+  if (windowShim_->panel()->IsAlwaysOnTop())
+    collectionBehavior |= NSWindowCollectionBehaviorCanJoinAllSpaces;
+  [[self window] setCollectionBehavior:collectionBehavior];
 }
 
 - (void)enableResizeByMouse:(BOOL)enable {
