00502330184a55e2ce97e6886c9c8c688fe14c5e
allan.jensen@digia.com <allan.jensen@digia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-19 16:00:47 +0000

    [Qt] Support kerning in fast path font rendering https://bugs.webkit.org/show_bug.cgi?id=106013
    Reviewed by Jocelyn Turcotte.
    
    To support kerning in the fast font path we need to implement SimpleFontData::applyTransforms.
    
    This patch changes the types used by the fast path GlyphBuffer to match those used by Qt,
    and implements SimpleFontData::applyTransforms using QRawFont::advancesForGlyphIndexes.
    
    * platform/graphics/GlyphBuffer.h:
    (GlyphBufferAdvance):
    (WebCore::GlyphBufferAdvance::GlyphBufferAdvance):
    (WebCore::GlyphBufferAdvance::setWidth):
    (WebCore::GlyphBufferAdvance::width):
    (WebCore::GlyphBufferAdvance::height):
    (WebCore::GlyphBuffer::add):
    * platform/graphics/SimpleFontData.h:
    (WebCore::SimpleFontData::applyTransforms):
    * platform/graphics/WidthIterator.h:
    (WebCore::WidthIterator::supportsTypesettingFeatures):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/GlyphBuffer.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/GlyphBuffer.h
@@ -57,6 +57,8 @@ class SimpleFontData;
 typedef cairo_glyph_t GlyphBufferGlyph;
 #elif OS(WINCE)
 typedef wchar_t GlyphBufferGlyph;
+#elif PLATFORM(QT)
+typedef quint32 GlyphBufferGlyph;
 #else
 typedef Glyph GlyphBufferGlyph;
 #endif
@@ -89,6 +91,18 @@ public:
 
 private:
     float advance;
+};
+#elif PLATFORM(QT)
+struct GlyphBufferAdvance : public QPointF {
+public:
+    GlyphBufferAdvance(const QPointF& advance)
+        : QPointF(advance)
+    {
+    }
+
+    void setWidth(qreal width) { QPointF::setX(width); }
+    qreal width() const { return QPointF::x(); }
+    qreal height() const { return QPointF::y(); }
 };
 #else
 typedef FloatSize GlyphBufferAdvance;
@@ -157,6 +171,8 @@ public:
         m_advances.append(advance);
 #elif OS(WINCE)
         m_advances.append(width);
+#elif PLATFORM(QT)
+        m_advances.append(QPointF(width, 0));
 #else
         m_advances.append(FloatSize(width, 0));
 #endif
