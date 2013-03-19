a181dcedd7eb20630a29842d350f3ab8f7b944c2
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 00:40:20 +0000

    Mac: "Import bookmarks..." right click shouldn't bring up context menu.
    XIB changes:
    Changed class of the import button item back to NSButton
    and unhooked the barView_ outlet.
    
    BUG=214212
    TEST=Right click on "Import bookmarks..." links in an empty BMB. No
    context menu should be shown.
    
    Review URL: https://chromiumcodereview.appspot.com/12538012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188887 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.h
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "third_party/GTM/AppKit/GTMUILocalizerAndLayoutTweaker.h"
 
 @class BookmarkBarController;
-@class BookmarkBarImportButton;
 @class BookmarkBarItemContainer;
 @class BookmarkBarTextField;
 
@@ -26,11 +25,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   IBOutlet BookmarkBarController* controller_;
   IBOutlet BookmarkBarTextField* noItemTextfield_;
-  IBOutlet BookmarkBarImportButton* importBookmarksButton_;
+  IBOutlet NSButton* importBookmarksButton_;
   BookmarkBarItemContainer* noItemContainer_;
 }
 - (BookmarkBarTextField*)noItemTextfield;
-- (BookmarkBarImportButton*)importBookmarksButton;
+- (NSButton*)importBookmarksButton;
 - (BookmarkBarController*)controller;
 
 @property(nonatomic, assign) IBOutlet BookmarkBarItemContainer* noItemContainer;
@@ -47,15 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // This is necessary when building with the 10.6 SDK because -rightMouseDown:
 // does not follow the responder chain.
 @interface BookmarkBarTextField : NSTextField {
- @private
-  IBOutlet BookmarkBarView* barView_;
-}
-@end
-
-// NSButton subclass responsible for routing -menu to the BookmarBarView.
-// This is necessary when building with the 10.6 SDK because -rightMouseDown:
-// does not follow the responder chain.
-@interface BookmarkBarImportButton : NSButton {
  @private
   IBOutlet BookmarkBarView* barView_;
 }
--- chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.mm
+++ chrome/browser/ui/cocoa/bookmarks/bookmark_bar_view.mm
@@ -99,11 +99,11 @@ using content::UserMetricsAction;
   return NO;
 }
 
--(BookmarkBarTextField*)noItemTextfield {
+- (BookmarkBarTextField*)noItemTextfield {
   return noItemTextfield_;
 }
 
--(BookmarkBarImportButton*)importBookmarksButton {
+- (NSButton*)importBookmarksButton {
   return importBookmarksButton_;
 }
 
@@ -114,14 +114,14 @@ using content::UserMetricsAction;
 // Internal method, needs to be called whenever a change has been made to
 // dropIndicatorShown_ or dropIndicatorPosition_ so it can get the controller
 // to reflect the change by moving buttons around.
--(void)dropIndicatorChanged {
+- (void)dropIndicatorChanged {
   if (dropIndicatorShown_)
     [controller_ setDropInsertionPos:dropIndicatorPosition_];
   else
     [controller_ clearDropInsertionPos];
 }
 
--(void)drawRect:(NSRect)dirtyRect {
+- (void)drawRect:(NSRect)dirtyRect {
   [super drawRect:dirtyRect];
 }
 
@@ -287,14 +287,6 @@ using content::UserMetricsAction;
 }
 
 @end  // @implementation BookmarkBarTextField
-
-@implementation BookmarkBarImportButton
-
-- (NSMenu*)menu {
-  return [barView_ menu];
-}
-
-@end  // @implementation BookmarkBarImportButton
 
 @implementation BookmarkBarItemContainer
 
