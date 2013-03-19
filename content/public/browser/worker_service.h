0deac1a0a29312a4447f2f2ab9ec90b68713aff1
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 10:18:40 +0000

    Support remote debugging for shared workers.
    BUG=None
    TBR=jam
    
    Review URL: https://chromiumcodereview.appspot.com/12817004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188978 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/worker_service.h
+++ content/public/browser/worker_service.h
@@ -10,8 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/process.h"
 #include "content/common/content_export.h"
-
-class GURL;
+#include "googleurl/src/gurl.h"
 
 namespace content {
 
