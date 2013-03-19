446933c06a3dcb494d60f333921d5bb301803d5d
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 10:31:02 +0000

    Fix UMA for managed user settings.
    The counting of when the managed user settings page is opened was wrong. This changelist should fix it.
    
    BUG=214849
    TEST=Manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/12438011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188987 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/managed_user_settings_handler.cc
+++ chrome/browser/ui/webui/options/managed_user_settings_handler.cc
@@ -42,8 +42,6 @@ void ManagedUserSettingsHandler::InitializeHandler() {
 }
 
 void ManagedUserSettingsHandler::InitializePage() {
-  start_time_ = base::TimeTicks::Now();
-  content::RecordAction(UserMetricsAction("ManagedMode_OpenSettings"));
   if (CommandLine::ForCurrentProcess()->HasSwitch(
           switches::kEnableManagedUsers)) {
     PrefService* pref_service = Profile::FromWebUI(web_ui())->GetPrefs();
@@ -53,6 +51,11 @@ void ManagedUserSettingsHandler::InitializePage() {
         "ManagedUserSettings.passphraseChanged",
         is_passphrase_set);
   }
+}
+
+void ManagedUserSettingsHandler::HandlePageOpened(const base::ListValue* args) {
+  start_time_ = base::TimeTicks::Now();
+  content::RecordAction(UserMetricsAction("ManagedMode_OpenSettings"));
 }
 
 void ManagedUserSettingsHandler::GetLocalizedValues(
@@ -95,6 +98,10 @@ void ManagedUserSettingsHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
       "confirmManagedUserSettings",
       base::Bind(&ManagedUserSettingsHandler::SaveMetrics,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "settingsPageOpened",
+      base::Bind(&ManagedUserSettingsHandler::HandlePageOpened,
                  base::Unretained(this)));
 }
 
