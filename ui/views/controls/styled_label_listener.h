8b31b97ff5267f36d0a5ad592bb21098b918fc02
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-19 09:27:49 +0000

    Add a dialog for getting user consent in the echo redeem flow.
    The dialog should have different content depending on whether the echo flow is
    enabled or not.
    
    BUG=175070,176979
    TEST=none (the code utilizing the dialog hasn't landed yet)
    (tested the dialog is shown properly with few local changes)
    
    Review URL: https://chromiumcodereview.appspot.com/12317109
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@188957 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/styled_label_listener.h
+++ ui/views/controls/styled_label_listener.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_VIEWS_CONTROLS_STYLED_LABEL_LISTENER_H_
 #define UI_VIEWS_CONTROLS_STYLED_LABEL_LISTENER_H_
 
+#include "ui/views/views_export.h"
+
 namespace ui {
 class Range;
 }
