108993195f934d67d5fedcecdb34fe2b8d937041
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 08:59:49 +0000

    Omnibox HistoryQuick: Store Recent Visits Information
    This change upgrades HistoryQuick provider private URL
    index data structure to store information about the ten
    most recent visits to each URL.  This will allow frecency-
    based scoring (see bug).
    
    To make this happen, this change:
    
    * updated the private index data structure format to
    store recent visit information.
    
    * enabled non-UI thread updates to the private data.  In
    particular, after a URL is visited (and hence needs to be
    newly indexed or at least updated), the private data class
    schedules a task on the historyDB thread to get the list
    of recent visits for the URL.  When the task returns, it
    updates that information in the private data structure.
    
    * updated the cache data structure that gets written and
    restored from a file to include the recent visits information.
    
    * forced the private data to get rebuilt from history if the
    cached data file come from a version that's not the most
    recent version.
    
    * revises tests with respect to the new interfaces to certain
    functions.
    
    * deletes some of the ------ comment lines in
    chrome/browser/history/url_index_private_data.cc
    per Peter's request.
    
    * rearranges the location of the implementation
    of the helper classes in 
    chrome/browser/history/url_index_private_data.cc
    per Peter's request.  I did not make any meaningful
    changes in the process of this cut-and-paste.  No need
    to review those.
    
    * adds tests
    - recent visits gets populated correctly
    - saving and restoring recent visits
    
    A later changelist will turn on using the
    recent  visits information in ranking URLs.
    
    BUG=175751
    
    Review URL: https://chromiumcodereview.appspot.com/12321041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189211 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/in_memory_url_index_types.cc
+++ chrome/browser/history/in_memory_url_index_types.cc
@@ -156,6 +156,11 @@ Char16Set Char16SetFromString16(const string16& term) {
   return characters;
 }
 
+// HistoryInfoMapValue ---------------------------------------------------------
+
+HistoryInfoMapValue::HistoryInfoMapValue() {}
+HistoryInfoMapValue::~HistoryInfoMapValue() {}
+
 // RowWordStarts ---------------------------------------------------------------
 
 RowWordStarts::RowWordStarts() {}
--- chrome/browser/history/visit_database.cc
+++ chrome/browser/history/visit_database.cc
@@ -18,11 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/page_transition_types.h"
 #include "sql/statement.h"
 
-// Rows, in order, of the visit table.
-#define HISTORY_VISIT_ROW_FIELDS \
-    " id,url,visit_time,from_visit,transition,segment_id,is_indexed," \
-    "visit_duration "
-
 namespace history {
 
 VisitDatabase::VisitDatabase() {
