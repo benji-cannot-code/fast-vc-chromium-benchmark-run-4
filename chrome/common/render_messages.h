f3266cec0e5b351b8acdbcb9a5100f33b338e1a5
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 23:00:05 +0000

    alternate ntp: add attribution url to ThemeBackgroundInfo
    BUG=221961
    TEST=verify per bug rpt
    
    
    Review URL: https://chromiumcodereview.appspot.com/12892006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189446 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/render_messages.h
+++ chrome/common/render_messages.h
@@ -209,6 +209,7 @@ IPC_STRUCT_TRAITS_BEGIN(ThemeBackgroundInfo)
   IPC_STRUCT_TRAITS_MEMBER(image_vertical_alignment)
   IPC_STRUCT_TRAITS_MEMBER(image_tiling)
   IPC_STRUCT_TRAITS_MEMBER(image_height)
+  IPC_STRUCT_TRAITS_MEMBER(has_attribution)
 IPC_STRUCT_TRAITS_END()
 
 IPC_STRUCT_TRAITS_BEGIN(WebKit::WebCache::ResourceTypeStat)
