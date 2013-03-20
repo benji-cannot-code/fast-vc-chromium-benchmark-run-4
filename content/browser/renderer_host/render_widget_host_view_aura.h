d4761c1be7e4cdb1f11dd307a0792b4018fe2cf3
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 01:58:41 +0000

    Fix RenderWidgetHostViewAura's accessible parent.
    views::WebView returns the accessible object from
    RenderWidgetHostViewAura as its own accessibility
    object. Hook up the reverse direction so that the
    accessible parent of RenderWidgetHostViewAura is
    the accessible object from the parent of the WebView.
    
    BUG=175156
    
    
    Review URL: https://chromiumcodereview.appspot.com/12917018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189195 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.h
+++ content/browser/renderer_host/render_widget_host_view_aura.h
@@ -113,6 +113,10 @@ class RenderWidgetHostViewAura
   virtual bool IsShowing() OVERRIDE;
   virtual gfx::Rect GetViewBounds() const OVERRIDE;
   virtual void SetBackground(const SkBitmap& background) OVERRIDE;
+#if defined(OS_WIN)
+  virtual void SetParentNativeViewAccessible(
+      gfx::NativeViewAccessible accessible_parent) OVERRIDE;
+#endif
 
   // Overridden from RenderWidgetHostViewPort:
   virtual void InitAsPopup(RenderWidgetHostView* parent_host_view,
@@ -556,6 +560,8 @@ class RenderWidgetHostViewAura
 #endif
 
   base::TimeTicks last_draw_ended_;
+
+  gfx::NativeViewAccessible accessible_parent_;
 
   DISALLOW_COPY_AND_ASSIGN(RenderWidgetHostViewAura);
 };
--- content/browser/renderer_host/render_widget_host_view_guest.h
+++ content/browser/renderer_host/render_widget_host_view_guest.h
@@ -61,6 +61,10 @@ class CONTENT_EXPORT RenderWidgetHostViewGuest
 #if defined(OS_WIN) && !defined(USE_AURA)
   virtual void SetClickthroughRegion(SkRegion* region) OVERRIDE;
 #endif
+#if defined(OS_WIN) && defined(USE_AURA)
+  virtual void SetParentNativeViewAccessible(
+      gfx::NativeViewAccessible accessible_parent) OVERRIDE;
+#endif
 
   // RenderWidgetHostViewPort implementation.
   virtual void InitAsPopup(RenderWidgetHostView* parent_host_view,
