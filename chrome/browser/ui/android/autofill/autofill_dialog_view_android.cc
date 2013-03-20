70d515fc8f9b8724d99d3e017ac70492df7cbc49
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 01:59:53 +0000

    [Android Payments] Adding selected index to UpdateSelection call.
    Adding an index of the selected menu item for the UpdateSection call.
    
    BUG=177382
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12945006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189198 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/android/autofill/autofill_dialog_view_android.cc
+++ chrome/browser/ui/android/autofill/autofill_dialog_view_android.cc
@@ -41,6 +41,12 @@ void AutofillDialogViewAndroid::Show() {
       reinterpret_cast<jint>(this),
       WindowAndroidHelper::FromWebContents(controller_->web_contents())->
           GetWindowAndroid()->GetJavaObject().obj()));
+
+  UpdateSection(SECTION_EMAIL);
+  UpdateSection(SECTION_CC);
+  UpdateSection(SECTION_BILLING);
+  UpdateSection(SECTION_CC_BILLING);
+  UpdateSection(SECTION_SHIPPING);
 }
 
 void AutofillDialogViewAndroid::Hide() {
@@ -97,7 +103,12 @@ void AutofillDialogViewAndroid::UpdateSection(DialogSection section) {
       Java_AutofillDialogGlue_createAutofillDialogMenuItemArray(env,
                                                                 itemCount);
 
+  int checkedItem = -1;
+
   for (int i = 0; i < itemCount; ++i) {
+    if (menuModel->IsItemCheckedAt(i))
+      checkedItem = i;
+
     ScopedJavaLocalRef<jstring> line1 =
         base::android::ConvertUTF16ToJavaString(env, menuModel->GetLabelAt(i));
     ScopedJavaLocalRef<jstring> line2 =
@@ -120,7 +131,8 @@ void AutofillDialogViewAndroid::UpdateSection(DialogSection section) {
                                         section,
                                         controller_->SectionIsActive(section),
                                         field_array.obj(),
-                                        menu_array.obj());
+                                        menu_array.obj(),
+                                        checkedItem);
 }
 
 void AutofillDialogViewAndroid::GetUserInput(DialogSection section,
