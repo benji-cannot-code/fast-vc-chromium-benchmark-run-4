fcb072c9d19956bcf46b749a063be723dfb81948
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 23:32:34 +0000

    Added app icon and logo.
    TBR=gene
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=189064
    
    Review URL: https://chromiumcodereview.appspot.com/12929008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189466 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/service/win/resource.h
+++ cloud_print/service/win/resource.h
@@ -5,3 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #define IDS_SERVICENAME       100
 #define IDR_CLOUDPRINTSERVICE 100
+
+#define IDI_ICON              201
+#define IDB_LOGO              202
