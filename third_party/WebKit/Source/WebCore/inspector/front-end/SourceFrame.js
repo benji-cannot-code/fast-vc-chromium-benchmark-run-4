1507dd658299c4ef4eb9331f2c2857c9ded41da8
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-20 14:59:52 +0000

    Web Inspector: fix SourceFrame.js compilation warning https://bugs.webkit.org/show_bug.cgi?id=112804
    Patch by Andrey Lushnikov <lushnikov@chromium.org> on 2013-03-20
    Reviewed by Yury Semikhatsky.
    
    No new tests: no change in behavior.
    
    - Explicitly parse lineNumber from object property name.
    - Add type annotations to ensure addMessageToSource is monomorphic.
    
    * inspector/front-end/SourceFrame.js:
    (WebInspector.SourceFrame.prototype.clearMessages):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -170,7 +170,8 @@ WebInspector.SourceFrame.prototype = {
     {
         for (var line in this._messageBubbles) {
             var bubble = this._messageBubbles[line];
-            this._textEditor.removeDecoration(line, bubble);
+            var lineNumber = parseInt(line, 10);
+            this._textEditor.removeDecoration(lineNumber, bubble);
         }
 
         this._messages = [];
@@ -501,6 +502,10 @@ WebInspector.SourceFrame.prototype = {
             this.addMessageToSource(this._messages[i].line - 1, this._messages[i]);
     },
 
+    /**
+     * @param {number} lineNumber
+     * @param {WebInspector.ConsoleMessage} msg
+     */
     addMessageToSource: function(lineNumber, msg)
     {
         if (lineNumber >= this._textEditor.linesCount)
@@ -577,6 +582,10 @@ WebInspector.SourceFrame.prototype = {
         rowMessage.repeatCountElement.textContent = WebInspector.UIString(" (repeated %d times)", rowMessage.repeatCount);
     },
 
+    /**
+     * @param {number} lineNumber
+     * @param {WebInspector.ConsoleMessage} msg
+     */
     removeMessageFromSource: function(lineNumber, msg)
     {
         if (lineNumber >= this._textEditor.linesCount)
