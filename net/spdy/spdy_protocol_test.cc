161c3a0f42848863cebbdf1db8386279aee2539b
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 19:53:52 +0000

    Clean up spdy_protocol* and spdy_test_utils.* files to match server version
    Remove unmaintained comments in spdy_protocol.h.
    
    Update link to SPDY/3 spec.
    
    Use base::HostToNetwork* instead of hton*.
    
    Add back accidentally-omitted SpdyProtocolDeathTest tests.
    
    Review URL: https://codereview.chromium.org/12782004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189372 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_protocol_test.cc
+++ net/spdy/spdy_protocol_test.cc
@@ -5,10 +5,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "net/spdy/spdy_protocol.h"
 
+#include <limits>
+
+#include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 #include "net/spdy/spdy_bitmasks.h"
 #include "net/spdy/spdy_framer.h"
-#include "testing/platform_test.h"
+#include "testing/gtest/include/gtest/gtest.h"
 
 namespace {
 
@@ -50,6 +53,7 @@ TEST_P(SpdyProtocolTest, ProtocolConstants) {
   EXPECT_EQ(7, GOAWAY);
   EXPECT_EQ(8, HEADERS);
   EXPECT_EQ(9, WINDOW_UPDATE);
+  EXPECT_EQ(std::numeric_limits<int32>::max(), kSpdyMaximumWindowSize);
 }
 
 class SpdyProtocolDeathTest : public SpdyProtocolTest {};
@@ -58,5 +62,22 @@ class SpdyProtocolDeathTest : public SpdyProtocolTest {};
 INSTANTIATE_TEST_CASE_P(SpdyProtocolDeathTests,
                         SpdyProtocolDeathTest,
                         ::testing::Values(SPDY2, SPDY3));
+
+#if GTEST_HAS_DEATH_TEST && !defined(NDEBUG)
+TEST_P(SpdyProtocolDeathTest, TestSpdySettingsAndIdOutOfBounds) {
+  scoped_ptr<SettingsFlagsAndId> flags_and_id;
+
+  EXPECT_DEBUG_DEATH(
+      {
+        flags_and_id.reset(new SettingsFlagsAndId(1, ~0));
+      },
+      "SPDY setting ID too large.");
+  // Make sure that we get expected values in opt mode.
+  if (flags_and_id.get() != NULL) {
+    EXPECT_EQ(1, flags_and_id->flags());
+    EXPECT_EQ(static_cast<SpdyPingId>(0xffffff), flags_and_id->id());
+  }
+}
+#endif  // GTEST_HAS_DEATH_TEST && !defined(NDEBUG)
 
 }  // namespace net
