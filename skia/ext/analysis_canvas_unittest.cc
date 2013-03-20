ec81ace2f3f38b6556b6fa8e70ee1a78a6d71983
tomhudson@chromium.org <tomhudson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 23:21:14 +0000

    Improve detection of cheap-to-rasterize SkPictures.
    Assumes most operations are very expensive; estimates cost of some
    simple operations (drawing rectangles and text) so that we can identify
    *some* cheap SkPictures.
    
    We can't currently predict the cost of any draw requiring a bitmap,
    because it could have a lazy decode pending or have been decoded but
    then evicted from cache. Until we have a better handle on things,
    assume all bitmaps are at least moderately expensive.
    
    Has a BadlyWrong error rate on top_25.json of well under 1%,
    even retaining the 1ms threshold for BadlyWrong (when 2-3ms is more
    reasonable) and even leaving the compositor render worker active
    (which can cause cheap tasks to be swapped out and thus falsely
    declared BadlyWrong).
    
    Has a SafelyWrong error rate of around 15-20%; this is space for
    improvement.
    
    BUG=173426,222031
    R=skyostil,junov
    
    
    Review URL: https://chromiumcodereview.appspot.com/12620013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189455 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/analysis_canvas_unittest.cc
+++ skia/ext/analysis_canvas_unittest.cc
@@ -47,7 +47,8 @@ class TestShader : public SkShader {
 
   SkShader::BitmapType asABitmap(SkBitmap* bitmap,
                                  SkMatrix*, TileMode xy[2]) const {
-    *bitmap = *bitmap_;
+    if (bitmap)
+      *bitmap = *bitmap_;
     return SkShader::kDefault_BitmapType;
   }
 
@@ -93,7 +94,7 @@ TEST(AnalysisCanvasTest, ClearCanvas) {
 
   EXPECT_TRUE(canvas.getColorIfSolid(&outputColor));
   EXPECT_FALSE(canvas.isTransparent());
-  EXPECT_TRUE(canvas.isCheap());
+  EXPECT_FALSE(canvas.isCheap());
   EXPECT_EQ(outputColor, color);
 
   // Translucent color
@@ -102,7 +103,7 @@ TEST(AnalysisCanvasTest, ClearCanvas) {
 
   EXPECT_FALSE(canvas.getColorIfSolid(&outputColor));
   EXPECT_FALSE(canvas.isTransparent());
-  EXPECT_TRUE(canvas.isCheap());
+  EXPECT_FALSE(canvas.isCheap());
 
   // Test helper methods
   solidColorFill(canvas);
@@ -146,7 +147,7 @@ TEST(AnalysisCanvasTest, ComplexActions) {
 
   EXPECT_FALSE(canvas.getColorIfSolid(&outputColor));
   EXPECT_FALSE(canvas.isTransparent());
-  EXPECT_TRUE(canvas.isCheap());
+  EXPECT_FALSE(canvas.isCheap());
 
   // Draw oval test.
   solidColorFill(canvas);
@@ -154,7 +155,7 @@ TEST(AnalysisCanvasTest, ComplexActions) {
 
   EXPECT_FALSE(canvas.getColorIfSolid(&outputColor));
   EXPECT_FALSE(canvas.isTransparent());
-  EXPECT_TRUE(canvas.isCheap());
+  EXPECT_FALSE(canvas.isCheap());
 
   // Draw bitmap test.
   solidColorFill(canvas);
@@ -164,7 +165,7 @@ TEST(AnalysisCanvasTest, ComplexActions) {
 
   EXPECT_FALSE(canvas.getColorIfSolid(&outputColor));
   EXPECT_FALSE(canvas.isTransparent());
-  EXPECT_TRUE(canvas.isCheap());
+  EXPECT_FALSE(canvas.isCheap());
 }
 
 TEST(AnalysisCanvasTest, SimpleDrawRect) {
