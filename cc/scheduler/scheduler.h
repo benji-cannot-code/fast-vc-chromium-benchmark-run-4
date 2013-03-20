084c7dd254f174752feb5ee2ceeb3a08fd8c22eb
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 00:47:12 +0000

    cc: Chromify FrameRateController
    Style-only change.
    
    BUG=144577
    
    
    Review URL: https://chromiumcodereview.appspot.com/12922002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189181 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler/scheduler.h
+++ cc/scheduler/scheduler.h
@@ -105,7 +105,7 @@ class CC_EXPORT Scheduler : FrameRateControllerClient {
   base::TimeTicks LastVSyncTime();
 
   // FrameRateControllerClient implementation
-  virtual void vsyncTick(bool throttled) OVERRIDE;
+  virtual void VSyncTick(bool throttled) OVERRIDE;
 
  private:
   Scheduler(SchedulerClient* client,
