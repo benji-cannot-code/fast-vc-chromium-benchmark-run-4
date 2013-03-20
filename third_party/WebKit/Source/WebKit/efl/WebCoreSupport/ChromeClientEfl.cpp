a91bdce72e5791e14df709777865c341e30f246f
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-20 10:01:22 +0000

    [EFL] Disable REQUEST_ANIMATION_FRAME_TIMER to render a new animation frame. https://bugs.webkit.org/show_bug.cgi?id=112114
    Patch by JungJik Lee <jungjik.lee@samsung.com> on 2013-03-20
    Reviewed by Kenneth Rohde Christiansen.
    
    .:
    
    Add a manual test to check the running of scripted transition animation.
    
    * ManualTests/animation/transition-on-and-offscreen-animation.html: Added.
    
    Source/WebKit/efl:
    
    Add dummy functions for WK1.
    
    Example test case: ManualTests/animation/transition-on-and-offscreen-animation.html
    
    * WebCoreSupport/ChromeClientEfl.cpp: Add dummy functions for WK1.
    (WebCore):
    (WebCore::ChromeClientEfl::scheduleAnimation):
    (WebCore::ChromeClientEfl::serviceScriptedAnimations):
    * WebCoreSupport/ChromeClientEfl.h:
    (ChromeClientEfl):
    
    Source/WTF:
    
    The issue is that if the animation starts outside of the area covered by keepRects,
    the web process does not create tiles of the animation layer and the layer moves
    without having any tiles. In order to fix this issue, CoordinatedLayerHost must call
    scheduleLayerFlush to create new tiles when the layer enters the area covered by keepRect.
    However EFL port didn't call scheduleLayerFlush periodically for animation.
    We can tie scripted animations with synchronization of the layer and that already
    has been implemented in r123786 by Qt port. This patch is for activating r123786 patch.
    
    The testing is covered by ManualTests/animation/transition-on-and-offscreen-animation.html
    
    * wtf/Platform.h: Disable REQUEST_ANIMATION_FRAME_TIMER.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/efl/WebCoreSupport/ChromeClientEfl.cpp
+++ third_party/WebKit/Source/WebKit/efl/WebCoreSupport/ChromeClientEfl.cpp
@@ -505,6 +505,18 @@ void ChromeClientEfl::setCursorHiddenUntilMouseMoves(bool)
     notImplemented();
 }
 
+#if ENABLE(REQUEST_ANIMATION_FRAME) && !USE(REQUEST_ANIMATION_FRAME_TIMER)
+void ChromeClientEfl::scheduleAnimation()
+{
+    notImplemented();
+}
+
+void ChromeClientEfl::serviceScriptedAnimations()
+{
+    notImplemented();
+}
+#endif
+
 void ChromeClientEfl::cancelGeolocationPermissionForFrame(Frame*, Geolocation*)
 {
     notImplemented();
