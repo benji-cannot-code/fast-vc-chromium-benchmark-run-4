d9bf274f59c60acdc89f574c47d8120f4744bad7
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 16:01:58 +0000

    Implement isOrphan() on WebLayerImpl.
    Afterward, we can add it to the WebLayer API as pure-virtual. This
    will be used by the WebMediaPlayerClientImpl to tell if a composited
    WebLayer is currently part of a layer tree that is part of a
    WebLayerTreeView.
    
    R=jamesr
    BUG=196810
    
    Review URL: https://chromiumcodereview.appspot.com/12812022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189299 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/compositor_bindings/web_layer_impl.cc
+++ webkit/compositor_bindings/web_layer_impl.cc
@@ -325,6 +325,8 @@ void WebLayerImpl::setScrollClient(
   layer_->set_layer_scroll_client(scroll_client);
 }
 
+bool WebLayerImpl::isOrphan() const { return !layer_->layer_tree_host(); }
+
 Layer* WebLayerImpl::layer() const { return layer_.get(); }
 
 }  // namespace WebKit
