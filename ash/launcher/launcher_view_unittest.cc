6f53fb570a9731dd3eaadba4c1bcaa97ff9b6172
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 00:35:52 +0000

    Launcher Item sizing/positioning
    Adjust the sizing and positioning of launcher items, specifically for the auto-hide animation, and launcher alignment adjustments.
    
    BUG=168824, 173298, 181913
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/12769007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189178 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_view_unittest.cc
+++ ash/launcher/launcher_view_unittest.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/launcher/launcher_model.h"
 #include "ash/launcher/launcher_tooltip_manager.h"
 #include "ash/root_window_controller.h"
+#include "ash/shelf/shelf_layout_manager.h"
 #include "ash/shelf/shelf_widget.h"
 #include "ash/shell.h"
 #include "ash/shell_window_ids.h"
@@ -176,7 +177,7 @@ TEST_F(LauncherViewIconObserverTest, MAYBE_AddRemoveWithMultipleDisplays) {
 }
 
 TEST_F(LauncherViewIconObserverTest, BoundsChanged) {
-  ShelfWidget* shelf = Shell::GetPrimaryRootWindowController()->shelf();
+  ash::ShelfWidget* shelf = Shell::GetPrimaryRootWindowController()->shelf();
   Launcher* launcher = Launcher::ForPrimaryDisplay();
   gfx::Size shelf_size =
       shelf->GetWindowBoundsInScreen().size();
@@ -303,6 +304,19 @@ class LauncherViewTest : public AshTestBase {
       ++map_index;
     }
     ASSERT_EQ(map_index, id_map.size());
+  }
+
+  void VerifyLauncherItemBoundsAreValid() {
+    for (int i=0;i <= test_api_->GetLastVisibleIndex(); ++i) {
+      if (test_api_->GetButton(i)) {
+        gfx::Rect launcher_view_bounds = launcher_view_->GetLocalBounds();
+        gfx::Rect item_bounds = test_api_->GetBoundsByIndex(i);
+        EXPECT_TRUE(item_bounds.x() >= 0);
+        EXPECT_TRUE(item_bounds.y() >= 0);
+        EXPECT_TRUE(item_bounds.right() <= launcher_view_bounds.width());
+        EXPECT_TRUE(item_bounds.bottom() <= launcher_view_bounds.height());
+      }
+    }
   }
 
   views::View* SimulateDrag(internal::LauncherButtonHost::Pointer pointer,
@@ -706,6 +720,19 @@ TEST_F(LauncherViewTest, LauncherItemStatusPlatformApp) {
   item.status = ash::STATUS_ATTENTION;
   model_->Set(index, item);
   ASSERT_EQ(internal::LauncherButton::STATE_ATTENTION, button->state());
+}
+
+// Confirm that launcher item bounds are correctly updated on shelf changes.
+TEST_F(LauncherViewTest, LauncherItemBoundsCheck) {
+  internal::ShelfLayoutManager* shelf_layout_manager =
+      Shell::GetPrimaryRootWindowController()->shelf()->shelf_layout_manager();
+  VerifyLauncherItemBoundsAreValid();
+  shelf_layout_manager->SetAutoHideBehavior(SHELF_AUTO_HIDE_BEHAVIOR_ALWAYS);
+  test_api_->RunMessageLoopUntilAnimationsDone();
+  VerifyLauncherItemBoundsAreValid();
+  shelf_layout_manager->SetAutoHideBehavior(SHELF_AUTO_HIDE_BEHAVIOR_NEVER);
+  test_api_->RunMessageLoopUntilAnimationsDone();
+  VerifyLauncherItemBoundsAreValid();
 }
 
 TEST_F(LauncherViewTest, LauncherTooltipTest) {
