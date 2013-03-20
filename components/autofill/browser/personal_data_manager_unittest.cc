83d537c21c4884997a6f4b305b2db5ff74698c9f
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 20:32:05 +0000

    Fix leaks introduced by WDS change
    Make sure tests give WDS time to shutdown its DB.
    
    TBR=atwilson@chromium.org
    (for Signin changes)
    
    R=joi@chromium.org
    BUG=222363
    
    Review URL: https://chromiumcodereview.appspot.com/12847010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189388 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/personal_data_manager_unittest.cc
+++ components/autofill/browser/personal_data_manager_unittest.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/guid.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop.h"
+#include "base/synchronization/waitable_event.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/password_manager/encryptor.h"
 #include "chrome/browser/webdata/web_data_service.h"
@@ -80,9 +81,15 @@ class PersonalDataManagerTest : public testing::Test {
     personal_data_.reset(NULL);
     profile_.reset(NULL);
 
-    db_thread_.Stop();
+    // Schedule another task on the DB thread to notify us that it's safe to
+    // stop the thread.
+    base::WaitableEvent done(false, false);
+    BrowserThread::PostTask(BrowserThread::DB, FROM_HERE,
+        base::Bind(&base::WaitableEvent::Signal, base::Unretained(&done)));
+    done.Wait();
     MessageLoop::current()->PostTask(FROM_HERE, MessageLoop::QuitClosure());
     MessageLoop::current()->Run();
+    db_thread_.Stop();
   }
 
   void ResetPersonalDataManager() {
