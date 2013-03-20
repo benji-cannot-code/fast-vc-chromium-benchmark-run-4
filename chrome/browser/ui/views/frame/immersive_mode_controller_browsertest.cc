c7699ede84afca16ef6093b4749342a89b3b26bd
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 00:16:11 +0000

    Disable immersive fullscreen for M27
    * It can be enabled with --ash-immersive-fullscreen
    * Updated test expectations
    
    BUG=none
    TEST=browser_tests ImmersiveModeControllerTest, BrowserNonClientFrameViewAshTest
    TBR=sky@chromium.org for trivial change to c/b/ui/browser_browsertest.cc
    
    
    Review URL: https://chromiumcodereview.appspot.com/12440046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189166 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/immersive_mode_controller_browsertest.cc
+++ chrome/browser/ui/views/frame/immersive_mode_controller_browsertest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/views/frame/immersive_mode_controller.h"
 
+#include "ash/ash_switches.h"
+#include "base/command_line.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/views/frame/browser_view.h"
@@ -33,11 +35,24 @@ gfx::Rect GetRectInWidget(views::View* view) {
 
 }  // namespace
 
-typedef InProcessBrowserTest ImmersiveModeControllerTest;
-
 // TODO(jamescook): If immersive mode becomes popular on CrOS, consider porting
 // it to other Aura platforms (win_aura, linux_aura).  http://crbug.com/163931
 #if defined(OS_CHROMEOS)
+
+class ImmersiveModeControllerTest : public InProcessBrowserTest {
+ public:
+  ImmersiveModeControllerTest() {}
+  virtual ~ImmersiveModeControllerTest() {}
+
+  // content::BrowserTestBase overrides:
+  virtual void SetUpCommandLine(CommandLine* command_line) OVERRIDE {
+    command_line->AppendSwitch(ash::switches::kAshImmersiveFullscreen);
+  }
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ImmersiveModeControllerTest);
+};
+
 IN_PROC_BROWSER_TEST_F(ImmersiveModeControllerTest, ImmersiveMode) {
   ui::ScopedAnimationDurationScaleMode zero_duration_mode(
       ui::ScopedAnimationDurationScaleMode::ZERO_DURATION);
