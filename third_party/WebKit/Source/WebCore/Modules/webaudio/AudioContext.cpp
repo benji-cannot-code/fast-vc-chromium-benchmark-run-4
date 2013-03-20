bc6c9443fa6b849742a1fc3019240b54f1d4a205
xingnan.wang@intel.com <xingnan.wang@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-20 17:33:12 +0000

    ASSERTION FAILED: !m_renderingAutomaticPullNodes.size() https://bugs.webkit.org/show_bug.cgi?id=112777
    Reviewed by Chris Rogers.
    
    * Modules/webaudio/AudioContext.cpp:
    (WebCore::AudioContext::~AudioContext):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webaudio/AudioContext.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webaudio/AudioContext.cpp
@@ -198,6 +198,8 @@ AudioContext::~AudioContext()
     ASSERT(!m_referencedNodes.size());
     ASSERT(!m_finishedNodes.size());
     ASSERT(!m_automaticPullNodes.size());
+    if (m_automaticPullNodesNeedUpdating)
+        m_renderingAutomaticPullNodes.resize(m_automaticPullNodes.size());
     ASSERT(!m_renderingAutomaticPullNodes.size());
 }
 
