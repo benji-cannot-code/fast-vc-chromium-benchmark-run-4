8656b5a0230a17ee9e110f21f2159d53770c4dba
eugenis@chromium.org <eugenis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 17:23:09 +0000

    Don't override system malloc when built with MemorySanitizer.
    This conflicts with MSan's malloc interception.
    We already do the same for ASan and TSan.
    
    BUG=178409
    
    R=mark@chromium.org,darin@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12521015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189327 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process_util_linux.cc
+++ base/process_util_linux.cc
@@ -749,9 +749,9 @@ void OnNoMemory() {
 
 }  // namespace
 
-extern "C" {
-#if !defined(USE_TCMALLOC) && !defined(ADDRESS_SANITIZER) && \
-    !defined(OS_ANDROID) && !defined(THREAD_SANITIZER)
+#if !defined(OS_ANDROID) && !defined(USE_TCMALLOC) && \
+    !defined(ADDRESS_SANITIZER) && !defined(MEMORY_SANITIZER) && \
+    !defined(THREAD_SANITIZER)
 
 extern "C" {
 void* __libc_malloc(size_t size);
@@ -760,7 +760,6 @@ void* __libc_calloc(size_t nmemb, size_t size);
 void* __libc_valloc(size_t size);
 void* __libc_pvalloc(size_t size);
 void* __libc_memalign(size_t alignment, size_t size);
-}  // extern "C"
 
 // Overriding the system memory allocation functions:
 //
@@ -830,8 +829,8 @@ int posix_memalign(void** ptr, size_t alignment, size_t size) {
   return 0;
 }
 
-#endif  // !defined(USE_TCMALLOC)
 }  // extern C
+#endif  // ANDROID, TCMALLOC, *_SANITIZER
 
 void EnableTerminationOnHeapCorruption() {
   // On Linux, there nothing to do AFAIK.
