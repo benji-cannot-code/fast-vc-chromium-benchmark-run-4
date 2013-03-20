4b4fb6fe9a41d3a3802f0c6b43666533d8dd0061
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 15:03:56 +0000

    Update the header guard macro.
    This header was moved in r181161 (https://codereview.chromium.org/12092078)
    but the header guard macro wasn't updated.
    
    R=brettw@chromium.org
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/12648009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189280 0039d316-1c4b-4281-b951-d872f2087c98

--- base/strings/string_number_conversions.h
+++ base/strings/string_number_conversions.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef BASE_STRING_NUMBER_CONVERSIONS_H_
-#define BASE_STRING_NUMBER_CONVERSIONS_H_
+#ifndef BASE_STRINGS_STRING_NUMBER_CONVERSIONS_H_
+#define BASE_STRINGS_STRING_NUMBER_CONVERSIONS_H_
 
 #include <string>
 #include <vector>
@@ -112,5 +112,5 @@ BASE_EXPORT bool HexStringToBytes(const std::string& input,
 
 }  // namespace base
 
-#endif  // BASE_STRING_NUMBER_CONVERSIONS_H_
+#endif  // BASE_STRINGS_STRING_NUMBER_CONVERSIONS_H_
 
