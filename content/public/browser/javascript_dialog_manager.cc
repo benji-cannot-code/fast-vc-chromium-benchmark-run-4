e0eac7aa94e936a49d1d3a4cacb930de6100b53a
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 02:14:12 +0000

    Add optional 'promptText' parameter to DevTools Page.handleJavaScriptDialog.
    If specified, this prompt text should be used when accepting a prompt dialog.
    BUG=chromedriver:246
    
    
    Review URL: https://chromiumcodereview.appspot.com/12603014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189203 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/javascript_dialog_manager.cc
+++ content/public/browser/javascript_dialog_manager.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-bool JavaScriptDialogManager::HandleJavaScriptDialog(WebContents* web_contents,
-                                                     bool accept) {
+bool JavaScriptDialogManager::HandleJavaScriptDialog(
+    WebContents* web_contents,
+    bool accept,
+    const string16* prompt_override) {
   return false;
 }
 
