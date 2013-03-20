7f358c89e644ab0afb187d56076b4c2030880ae1
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-20 18:06:53 +0000

    [Android] Enable running uiautomator tests.
    - uiautomator uses instrumentation runner for results reporting.
    - Separate the concept of test jar (for progaurd consumption) from test
      apk
    - clear the app before every uiautomator test
    
    BUG=162742
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/12921004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189343 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/adb_install_apk.py
+++ build/android/adb_install_apk.py
@@ -12,7 +12,7 @@ import sys
 
 from pylib import android_commands
 from pylib import constants
-from pylib.instrumentation import apk_info
+from pylib.utils import apk_helper
 from pylib.utils import test_options_parser
 
 
@@ -37,7 +37,7 @@ def main(argv):
     raise Exception('Error: no connected devices')
 
   if not options.apk_package:
-    options.apk_package = apk_info.GetPackageNameForApk(options.apk)
+    options.apk_package = apk_helper.GetPackageName(options.apk)
 
   pool = multiprocessing.Pool(len(devices))
   # Send a tuple (apk_path, apk_package, device) per device.
---
+++ build/android/pylib/instrumentation/test_package.py
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Class representing instrumentation test apk and jar."""
+
+import os
+
+from pylib.utils import apk_helper
+
+import test_jar
+
+
+class TestPackage(test_jar.TestJar):
+  def __init__(self, apk_path, jar_path):
+    test_jar.TestJar.__init__(self, jar_path)
+
+    if not os.path.exists(apk_path):
+      raise Exception('%s not found, please build it' % apk_path)
+    self._apk_path = apk_path
+
+  def GetApkPath(self):
+    return self._apk_path
+
+  def GetPackageName(self):
+    """Returns the package name of this APK."""
+    return apk_helper.GetPackageName(self._apk_path)
+
+  # Override.
+  def Install(self, adb):
+    adb.ManagedInstall(self.GetApkPath(), package_name=self.GetPackageName())
