1d73b00ab7828faa14e582b5a267e64167e63bc8
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-20 04:16:47 +0000

    Move testRunner.setTouchDragDropEnabled to internals.settings https://bugs.webkit.org/show_bug.cgi?id=112739
    Reviewed by Adam Barth.
    
    Tools:
    
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp:
    (WebTestRunner::TestRunner::TestRunner): Remove unnecessary code.
    * DumpRenderTree/chromium/TestRunner/src/TestRunner.h:
    (TestRunner): Remove unnecessary code.
    * DumpRenderTree/chromium/TestRunner/src/WebPreferences.cpp:
    (WebTestRunner::WebPreferences::reset):
    (WebTestRunner::WebPreferences::applyTo): Always set to false.
    
    LayoutTests:
    
    Update tests to use internals.settings. This testRunner method was only exposed on
    Chromium, but now that Settings.in generates internals.settings setters, we can
    use that instead.
    
    * fast/events/touch/gesture/long-press-on-draggable-element-in-iframe-triggers-drag.html:
    * fast/events/touch/gesture/long-press-on-draggable-element-in-nested-iframes-triggers-drag.html:
    * fast/events/touch/gesture/long-press-on-draggable-element-triggers-drag.html:
    * touchadjustment/touch-links-longpress.html:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146300 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.h
@@ -304,8 +304,6 @@ private:
     // Enable or disable plugins.
     void setPluginsEnabled(const CppArgumentList&, CppVariant*);
 
-    void setTouchDragDropEnabled(const CppArgumentList&, CppVariant*);
-
     ///////////////////////////////////////////////////////////////////////////
     // Methods that modify the state of TestRunner
 
--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/WebPreferences.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/WebPreferences.cpp
@@ -130,7 +130,6 @@ void WebPreferences::reset()
     cssCustomFilterEnabled = false;
     shouldRespectImageOrientation = false;
     asynchronousSpellCheckingEnabled = false;
-    touchDragDropEnabled = false;
 }
 
 void WebPreferences::applyTo(WebView* webView)
@@ -198,9 +197,9 @@ void WebPreferences::applyTo(WebView* webView)
     settings->setMockScrollbarsEnabled(mockScrollbarsEnabled);
     settings->setShouldRespectImageOrientation(shouldRespectImageOrientation);
     settings->setAsynchronousSpellCheckingEnabled(asynchronousSpellCheckingEnabled);
-    settings->setTouchDragDropEnabled(touchDragDropEnabled);
 
     // Fixed values.
+    settings->setTouchDragDropEnabled(false);
     settings->setTextDirectionSubmenuInclusionBehaviorNeverIncluded();
     settings->setDownloadableBinaryFontsEnabled(true);
     settings->setAllowScriptsToCloseWindows(false);
