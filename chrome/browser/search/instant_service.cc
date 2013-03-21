71e926a8d8b776ee62bee4dfc558021262ad9df9
dhollowa@chromium.org <dhollowa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 23:18:36 +0000

    Revert 189662 "13.0% chromium-rel-win7-webkit/startup/shutdown/s..."
    Still waiting on specific result with this test.  Bots are still cycling.  Will
    update the bug with results once they're available.
    
    
    > 13.0% chromium-rel-win7-webkit/startup/shutdown/simple-user-quit
    > 
    > Speculative "fix" for perf regression with Instant on Win7.  Will
    > revert once analysis is complete.
    > 
    > BUG=189163
    > TEST=performance_ui_tests --gtest_filter=ShutdownTest.SimpleUserQuit
    > TBR=sreeram@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/12550015
    
    TBR=dhollowa@chromium.org
    Review URL: https://codereview.chromium.org/12679009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/instant_service.cc
+++ chrome/browser/search/instant_service.cc
@@ -63,9 +63,6 @@ InstantService::InstantService(Profile* profile)
                    profile->GetResourceContext(), instant_io_context_));
   }
 
-/* TODO(dhollowa): Temporarily disabling to diagnose perf regression.
-   http://crbug.com/189163.
-
   // Set up the data sources that Instant uses on the NTP.
 #if defined(ENABLE_THEMES)
   content::URLDataSource::Add(profile, new ThemeSource(profile));
@@ -73,7 +70,6 @@ InstantService::InstantService(Profile* profile)
   content::URLDataSource::Add(profile, new ThumbnailSource(profile));
   content::URLDataSource::Add(profile, new FaviconSource(
       profile, FaviconSource::FAVICON));
-*/
   content::URLDataSource::Add(profile, new LocalOmniboxPopupSource());
 }
 
