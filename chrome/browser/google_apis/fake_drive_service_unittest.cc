ebdca3cea54b7405d01ea41dc56a3e70225613dc
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 07:00:38 +0000

    Eliminate CopyResultsFromUploadRangeCallback.
    Replace the method with CreateCopyResultCallback.
    
    BUG=180569
    TEST=Ran unit_tests --gtest_filter=FakeDriveService*
    
    Review URL: https://codereview.chromium.org/12529016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/fake_drive_service_unittest.cc
+++ chrome/browser/google_apis/fake_drive_service_unittest.cc
@@ -1288,8 +1288,7 @@ TEST_F(FakeDriveServiceTest, ResumeUpload_Offline) {
       upload_location,
       0, 13, 15, "test/foo",
       scoped_refptr<net::IOBuffer>(),
-      base::Bind(&test_util::CopyResultsFromUploadRangeCallback,
-                 &response, &entry));
+      test_util::CreateCopyResultCallback(&response, &entry));
   message_loop_.RunUntilIdle();
 
   EXPECT_EQ(GDATA_NO_CONNECTION, response.code);
@@ -1321,8 +1320,7 @@ TEST_F(FakeDriveServiceTest, ResumeUpload_NotFound) {
       GURL("https://foo.com/"),
       0, 13, 15, "test/foo",
       scoped_refptr<net::IOBuffer>(),
-      base::Bind(&test_util::CopyResultsFromUploadRangeCallback,
-                 &response, &entry));
+      test_util::CreateCopyResultCallback(&response, &entry));
   message_loop_.RunUntilIdle();
 
   EXPECT_EQ(HTTP_NOT_FOUND, response.code);
@@ -1354,8 +1352,7 @@ TEST_F(FakeDriveServiceTest, ResumeUpload_ExistingFile) {
       upload_location,
       0, 13, 15, "text/plain",
       scoped_refptr<net::IOBuffer>(),
-      base::Bind(&test_util::CopyResultsFromUploadRangeCallback,
-                 &response, &entry));
+      test_util::CreateCopyResultCallback(&response, &entry));
   message_loop_.RunUntilIdle();
 
   EXPECT_EQ(HTTP_RESUME_INCOMPLETE, response.code);
@@ -1367,8 +1364,7 @@ TEST_F(FakeDriveServiceTest, ResumeUpload_ExistingFile) {
       upload_location,
       14, 15, 15, "text/plain",
       scoped_refptr<net::IOBuffer>(),
-      base::Bind(&test_util::CopyResultsFromUploadRangeCallback,
-                 &response, &entry));
+      test_util::CreateCopyResultCallback(&response, &entry));
   message_loop_.RunUntilIdle();
 
   EXPECT_EQ(HTTP_SUCCESS, response.code);
@@ -1405,8 +1401,7 @@ TEST_F(FakeDriveServiceTest, ResumeUpload_NewFile) {
       upload_location,
       0, 13, 15, "test/foo",
       scoped_refptr<net::IOBuffer>(),
-      base::Bind(&test_util::CopyResultsFromUploadRangeCallback,
-                 &response, &entry));
+      test_util::CreateCopyResultCallback(&response, &entry));
   message_loop_.RunUntilIdle();
 
   EXPECT_EQ(HTTP_RESUME_INCOMPLETE, response.code);
@@ -1418,8 +1413,7 @@ TEST_F(FakeDriveServiceTest, ResumeUpload_NewFile) {
       upload_location,
       14, 15, 15, "test/foo",
       scoped_refptr<net::IOBuffer>(),
-      base::Bind(&test_util::CopyResultsFromUploadRangeCallback,
-                 &response, &entry));
+      test_util::CreateCopyResultCallback(&response, &entry));
   message_loop_.RunUntilIdle();
 
   EXPECT_EQ(HTTP_CREATED, response.code);
