389f877db0b5535571491097faf6a627ada3397e
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-21 09:05:45 +0000

    Merge MainResourceLoader into DocumentLoader https://bugs.webkit.org/show_bug.cgi?id=104969
    Patch by Nate Chapin <japhet@chromium.org> on 2013-03-21
    Reviewed by Adam Barth.
    
    No new tests, refactor only.
    
    * CMakeLists.txt:
    * GNUmakefile.list.am:
    * Target.pri:
    * WebCore.exp.in:
    * WebCore.gypi:
    * WebCore.vcproj/WebCore.vcproj:
    * WebCore.vcxproj/WebCore.vcxproj:
    * WebCore.vcxproj/WebCore.vcxproj.filters:
    * WebCore.xcodeproj/project.pbxproj:
    * loader/DocumentLoader.cpp:
    (WebCore::DocumentLoader::DocumentLoader):
    (WebCore::DocumentLoader::mainResourceLoader):
    (WebCore::DocumentLoader::~DocumentLoader): Move main resource clearing from
        ~MainResourceLoader.
    (WebCore::DocumentLoader::mainResourceData):
    (WebCore::DocumentLoader::stopLoading):
    (WebCore::DocumentLoader::notifyFinished): Moved from MainResourceLoader::notifyFinished.
    (WebCore):
    (WebCore::DocumentLoader::finishedLoading):
    (WebCore::DocumentLoader::handleSubstituteDataLoadNow):
    (WebCore::DocumentLoader::redirectReceived):
    (WebCore::DocumentLoader::continueAfterNavigationPolicy):
    (WebCore::DocumentLoader::responseReceived):
    (WebCore::DocumentLoader::continueAfterContentPolicy):
    (WebCore::DocumentLoader::reportMemoryUsage):
    (WebCore::DocumentLoader::dataReceived):
    (WebCore::DocumentLoader::clearMainResourceLoader):
    (WebCore::DocumentLoader::setDefersLoading):
    (WebCore::DocumentLoader::setMainResourceDataBufferingPolicy):
    (WebCore::DocumentLoader::addSubresourceLoader):
    (WebCore::DocumentLoader::maybeLoadEmpty):
    (WebCore::DocumentLoader::startLoadingMainResource): Don't keep separate checks
         for normal and empty loads, just use a single boolean.
    * loader/DocumentLoader.h:
    (WebCore::DocumentLoader::isLoadingMainResource):
    * loader/MainResourceLoader.cpp: Removed.
    * loader/MainResourceLoader.h: Removed.
    * loader/mac/DocumentLoaderMac.cpp:
    (WebCore::DocumentLoader::schedule):
    (WebCore::DocumentLoader::unschedule):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146449 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/mac/DocumentLoaderMac.cpp
+++ third_party/WebKit/Source/WebCore/loader/mac/DocumentLoaderMac.cpp
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 #include "DocumentLoader.h"
-#include "MainResourceLoader.h"
 #include "ResourceHandle.h"
 #include "ResourceLoader.h"
 #include <wtf/UnusedParam.h>
@@ -56,8 +55,8 @@ static void unscheduleAll(const ResourceLoaderSet& loaders, SchedulePair* pair)
 
 void DocumentLoader::schedule(SchedulePair* pair)
 {
-    if (m_mainResourceLoader && m_mainResourceLoader->loader() && m_mainResourceLoader->loader()->handle())
-        m_mainResourceLoader->loader()->handle()->schedule(pair);
+    if (mainResourceLoader() && mainResourceLoader()->handle())
+        mainResourceLoader()->handle()->schedule(pair);
     scheduleAll(m_subresourceLoaders, pair);
     scheduleAll(m_plugInStreamLoaders, pair);
     scheduleAll(m_multipartSubresourceLoaders, pair);
@@ -65,8 +64,8 @@ void DocumentLoader::schedule(SchedulePair* pair)
 
 void DocumentLoader::unschedule(SchedulePair* pair)
 {
-    if (m_mainResourceLoader && m_mainResourceLoader->loader() && m_mainResourceLoader->loader()->handle())
-        m_mainResourceLoader->loader()->handle()->unschedule(pair);
+    if (mainResourceLoader() && mainResourceLoader()->handle())
+        mainResourceLoader()->handle()->unschedule(pair);
     unscheduleAll(m_subresourceLoaders, pair);
     unscheduleAll(m_plugInStreamLoaders, pair);
     unscheduleAll(m_multipartSubresourceLoaders, pair);
