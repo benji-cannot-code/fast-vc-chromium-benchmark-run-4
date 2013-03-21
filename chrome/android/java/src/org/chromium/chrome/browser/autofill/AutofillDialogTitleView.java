2ec6fa92e0d36866cb87a7a021582b5044f2ca1c
yusufo@chromium.org <yusufo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 22:57:44 +0000

    Add Glue as delegate to AutofillDialog
    This starts making the layout transitions depend on calls coming from native.
    It adds calls that notify native for item selection and also makes it possible
    to change and select accounts. Also pulled default menu items to AutofillDialog
    to control the flow better making the UI logic simpler.
    
    BUG=196264
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12942004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillDialogTitleView.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillDialogTitleView.java
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 package org.chromium.chrome.browser.autofill;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -25,9 +24,8 @@ import org.chromium.chrome.R;
  * the content.
  */
 public class AutofillDialogTitleView extends FrameLayout {
-    private static final List<String> mConstantItems = new ArrayList<String>();
 
-    private List<String> mAccountNames;
+    private ArrayList<String> mAccountNames;
     private ArrayAdapter<String> mAdapter;
 
     /**
@@ -36,17 +34,12 @@ public class AutofillDialogTitleView extends FrameLayout {
      */
     public AutofillDialogTitleView(Context context) {
         super(context);
-        if (mConstantItems.isEmpty()) {
-            mConstantItems.add(getResources().getString(R.string.autofill_new_account));
-            mConstantItems.add(getResources().getString(R.string.autofill_use_local));
-        }
 
         LayoutInflater.from(context).inflate(R.layout.autofill_dialog_title, this, true);
-        Spinner accounts_spinner = (Spinner)findViewById(R.id.accounts_spinner);
+        Spinner accountsSpinner = (Spinner)findViewById(R.id.accounts_spinner);
         mAdapter = new ArrayAdapter<String>(context, android.R.layout.simple_spinner_item);
         mAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        mAdapter.addAll(mConstantItems);
-        accounts_spinner.setAdapter(mAdapter);
+        accountsSpinner.setAdapter(mAdapter);
     }
 
     /**
@@ -54,12 +47,28 @@ public class AutofillDialogTitleView extends FrameLayout {
      * @param context The context to create the title within.
      * @param accountNames The dropdown items to be listed.
      */
-    public AutofillDialogTitleView(Context context, List<String> accountNames) {
+    public AutofillDialogTitleView(Context context, ArrayList<String> accountNames) {
         this(context);
-        mAccountNames = accountNames;
+        updateAccountsAndSelect(accountNames, 0);
+    }
+
+    /**
+     * Update account chooser dropdown with given accounts and create the title view if needed.
+     * @param accountNames The dropdown items to be listed.
+     * @param selectedAccountIndex The index of a currently selected account or -1
+     *                             if the local payments should be used.
+     */
+    public void updateAccountsAndSelect(ArrayList<String> accountNames,
+            int selectedAccountIndex) {
         mAdapter.clear();
+        mAccountNames = accountNames;
         mAdapter.addAll(mAccountNames);
-        mAdapter.addAll(mConstantItems);
+        Spinner accountsSpinner = (Spinner)findViewById(R.id.accounts_spinner);
+        if (selectedAccountIndex >= 0) {
+            accountsSpinner.setSelection(selectedAccountIndex);
+        } else {
+            accountsSpinner.setSelection(accountsSpinner.getCount() - 1);
+        }
     }
 
     /**
