1806dc400fe9d460ebb0158e22a3eac83b27d12e
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 05:44:40 +0000

    Re-send spelling results after document markers were removed
    SpellCheckProvider can cancel spelling requests when WebKit
    has discarded the spelling markers and spellcheck results in
    the document. This behavior results in some misspellings missing
    underlines. The fix is to always send the last spelling results
    instead of canceling spellcheck requests.
    
    TEST=Follow these steps:
    1. Type "Icland is an icland ".
    2. Erase all text.
    3. Type "Icland is an icland " again.
    The second word should have spelling markers.
    
    BUG=215184
    
    
    Review URL: https://chromiumcodereview.appspot.com/12716025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189531 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/spellchecker/spellcheck_provider_test.cc
+++ chrome/renderer/spellchecker/spellcheck_provider_test.cc
@@ -23,7 +23,6 @@ FakeTextCheckingCompletion::~FakeTextCheckingCompletion() {}
 void FakeTextCheckingCompletion::didFinishCheckingText(
     const WebKit::WebVector<WebKit::WebTextCheckingResult>& results) {
   ++completion_count_;
-  last_results_ = results;
 }
 
 void FakeTextCheckingCompletion::didCancelCheckingText() {
@@ -33,7 +32,8 @@ void FakeTextCheckingCompletion::didCancelCheckingText() {
 
 TestingSpellCheckProvider::TestingSpellCheckProvider()
       : SpellCheckProvider(NULL, new MockSpellcheck),
-        offset_(-1) {
+        offset_(-1),
+        spelling_service_call_count_(0) {
 }
 
 TestingSpellCheckProvider::~TestingSpellCheckProvider() {
@@ -68,6 +68,7 @@ void TestingSpellCheckProvider::OnCallSpellingService(int route_id,
 #if defined (OS_MACOSX)
   NOTREACHED();
 #else
+  ++spelling_service_call_count_;
   WebKit::WebTextCheckingCompletion* completion =
       text_check_completions_.Lookup(identifier);
   if (!completion) {
@@ -77,9 +78,13 @@ void TestingSpellCheckProvider::OnCallSpellingService(int route_id,
   offset_ = offset;
   text_.assign(text);
   text_check_completions_.Remove(identifier);
-  completion->didFinishCheckingText(
-      std::vector<WebKit::WebTextCheckingResult>());
+  std::vector<WebKit::WebTextCheckingResult> results;
+  results.push_back(WebKit::WebTextCheckingResult(
+      WebKit::WebTextCheckingTypeSpelling,
+      0, 5, WebKit::WebString("hello")));
+  completion->didFinishCheckingText(results);
   last_request_ = text;
+  last_results_ = results;
 #endif
 }
 
