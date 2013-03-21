fc8b915103d2c46376ea5397526e125c7c4c2e46
ramankk@chromium.org <ramankk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 12:58:03 +0000

    Address TODO: Use formdata to carry around ssl_status  for rAc
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/12843007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/tab_autofill_manager_delegate.cc
+++ chrome/browser/ui/autofill/tab_autofill_manager_delegate.cc
@@ -156,7 +156,6 @@ void TabAutofillManagerDelegate::HideAutocheckoutBubble() {
 void TabAutofillManagerDelegate::ShowRequestAutocompleteDialog(
     const FormData& form,
     const GURL& source_url,
-    const content::SSLStatus& ssl_status,
     const AutofillMetrics& metric_logger,
     DialogType dialog_type,
     const base::Callback<void(const FormStructure*)>& callback) {
@@ -166,7 +165,6 @@ void TabAutofillManagerDelegate::ShowRequestAutocompleteDialog(
       new autofill::AutofillDialogControllerImpl(web_contents_,
                                                  form,
                                                  source_url,
-                                                 ssl_status,
                                                  metric_logger,
                                                  dialog_type,
                                                  callback);
--- components/autofill/browser/autofill_manager_unittest.cc
+++ components/autofill/browser/autofill_manager_unittest.cc
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "components/autofill/common/form_field_data.h"
 #include "components/user_prefs/user_prefs.h"
 #include "content/public/browser/web_contents.h"
-#include "content/public/common/ssl_status.h"
 #include "content/public/test/mock_render_process_host.h"
 #include "content/public/test/test_browser_thread.h"
 #include "googleurl/src/gurl.h"
@@ -3300,8 +3299,7 @@ TEST_F(AutofillManagerTest, DisabledAutofillDispatchesError) {
 
   autofill_manager_->set_autofill_enabled(false);
   autofill_manager_->OnRequestAutocomplete(FormData(),
-                                           GURL(),
-                                           content::SSLStatus());
+                                           GURL());
 
   EXPECT_EQ(1U, autofill_manager_->request_autocomplete_results().size());
   EXPECT_EQ(WebFormElement::AutocompleteResultErrorDisabled,
