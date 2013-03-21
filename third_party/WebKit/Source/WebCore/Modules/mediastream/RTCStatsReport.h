0dc73a3e97041a4a6c4a5fd7d3d2881bee655d4e
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-21 20:28:46 +0000

    Expose the Type field of an RTCStatsReport https://bugs.webkit.org/show_bug.cgi?id=112951
    Patch by Harald Alvestrand <hta@google.com> on 2013-03-21
    Reviewed by Adam Barth.
    
    Source/WebCore:
    
    Covered by an extension of an existing test.
    
    * Modules/mediastream/RTCStatsReport.h:
    (WebCore::RTCStatsReport::type):
    * Modules/mediastream/RTCStatsReport.idl:
    
    LayoutTests:
    
    * fast/mediastream/RTCPeerConnection-stats-expected.txt:
    * fast/mediastream/RTCPeerConnection-stats.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/mediastream/RTCStatsReport.h
+++ third_party/WebKit/Source/WebCore/Modules/mediastream/RTCStatsReport.h
@@ -40,6 +40,7 @@ public:
 
     double timestamp() const { return m_timestamp; }
     String id() { return m_id; }
+    String type() { return m_type; }
     String stat(const String& name) { return m_stats.get(name); }
     Vector<String> names() const;
 
