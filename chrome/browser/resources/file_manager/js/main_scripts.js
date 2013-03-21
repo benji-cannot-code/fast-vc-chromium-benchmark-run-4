a47623910f53f81893ef5058fec3ea6f6772f843
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 17:02:05 +0000

    Files.app: exporting namespaces to communicate between main frame and image loader.
    Enabling strict mode hides some namespaces with anonymous namespace, and it caused ReferenceError. This patch exports some namespace which is necessary for communicate.
    
    BUG=none
    TEST=images can be loaded on Files.app.
    R=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/12658012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -98,6 +98,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="main.js"/>
 
 // Exports
+window.util = util;
 window.FileCopyManagerWrapper = FileCopyManagerWrapper;
 
 // Exports
