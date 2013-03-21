78fe4a3b3f994be4ba81de40281b58d5e088be72
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-21 06:41:26 +0000

    Attach panel while dragging to bring it in front of other panels.
    BUG=181249
    TEST=PanelWindowResizerTest.DragMovesToPanelLayer
    
    
    Review URL: https://chromiumcodereview.appspot.com/12441010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189536 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/panels/panel_window_resizer_unittest.cc
+++ ash/wm/panels/panel_window_resizer_unittest.cc
@@ -106,6 +106,7 @@ class PanelWindowResizerTest : public test::AshTestBase {
     scoped_ptr<aura::Window> window(
         CreatePanelWindow(gfx::Rect(0, 0, 201, 201)));
     EXPECT_TRUE(window->GetProperty(kPanelAttachedKey));
+    EXPECT_EQ(internal::kShellWindowId_PanelContainer, window->parent()->id());
     DragStart(window.get());
     gfx::Rect initial_bounds = window->bounds();
 
@@ -123,6 +124,8 @@ class PanelWindowResizerTest : public test::AshTestBase {
     // The panel should be detached when the drag completes.
     DragEnd();
     EXPECT_FALSE(window->GetProperty(kPanelAttachedKey));
+    EXPECT_EQ(internal::kShellWindowId_WorkspaceContainer,
+              window->parent()->id());
 
     DragStart(window.get());
     // Drag the panel down.
@@ -134,6 +137,7 @@ class PanelWindowResizerTest : public test::AshTestBase {
     EXPECT_TRUE(window->GetProperty(kPanelAttachedKey));
     EXPECT_EQ(initial_bounds.x(), window->bounds().x());
     EXPECT_EQ(initial_bounds.y(), window->bounds().y());
+    EXPECT_EQ(internal::kShellWindowId_PanelContainer, window->parent()->id());
   }
 
   void TestWindowOrder(const std::vector<aura::Window*>& window_order) {
@@ -220,6 +224,25 @@ TEST_F(PanelWindowResizerTest, PanelDetachReattachTop) {
   ash::Shell* shell = ash::Shell::GetInstance();
   shell->SetShelfAlignment(SHELF_ALIGNMENT_TOP, shell->GetPrimaryRootWindow());
   DetachReattachTest(0, 1);
+}
+
+TEST_F(PanelWindowResizerTest, DragMovesToPanelLayer) {
+  scoped_ptr<aura::Window> window(CreatePanelWindow(gfx::Rect(0, 0, 201, 201)));
+  DragStart(window.get());
+  DragMove(0, -100);
+  DragEnd();
+  EXPECT_EQ(internal::kShellWindowId_WorkspaceContainer,
+            window->parent()->id());
+
+  // While moving the panel window should be moved to the panel container.
+  DragStart(window.get());
+  DragMove(20, 0);
+  EXPECT_EQ(internal::kShellWindowId_PanelContainer, window->parent()->id());
+  DragEnd();
+
+  // When dropped it should return to the default container.
+  EXPECT_EQ(internal::kShellWindowId_WorkspaceContainer,
+            window->parent()->id());
 }
 
 TEST_F(PanelWindowResizerTest, DragReordersPanelsHorizontal) {
