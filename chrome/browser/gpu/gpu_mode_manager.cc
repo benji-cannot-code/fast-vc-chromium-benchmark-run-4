db602bf1bdf951c1cb8bb4b9c729e592e421b217
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 23:48:53 +0000

    Get the init gpu mode from when chrome starts instead of when settings page starts
    If we open settings page, change gpu mode, open a new tab, close the original setting page.  Now on the new tab, go to settings page.
    
    Without this CL: it didn't show the restart link (which is wrong)
    With this CL: it shows.
    
    BUG=173130
    TEST=settings page (as described above)
    Review URL: https://codereview.chromium.org/12438035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/gpu/gpu_mode_manager.cc
+++ chrome/browser/gpu/gpu_mode_manager.cc
@@ -18,7 +18,8 @@ void GpuModeManager::RegisterPrefs(PrefRegistrySimple* registry) {
       prefs::kHardwareAccelerationModeEnabled, true);
 }
 
-GpuModeManager::GpuModeManager() {
+GpuModeManager::GpuModeManager()
+    : initial_gpu_mode_pref_(true) {
   if (g_browser_process->local_state()) {  // Skip for unit tests
     pref_registrar_.Init(g_browser_process->local_state());
     // Do nothing when the pref changes. It takes effect after
@@ -27,7 +28,9 @@ GpuModeManager::GpuModeManager() {
         prefs::kHardwareAccelerationModeEnabled,
         base::Bind(&base::DoNothing));
 
-    if (!IsGpuModePrefEnabled()) {
+    initial_gpu_mode_pref_ = IsGpuModePrefEnabled();
+
+    if (!initial_gpu_mode_pref_) {
       content::GpuDataManager* gpu_data_manager =
           content::GpuDataManager::GetInstance();
       DCHECK(gpu_data_manager);
@@ -37,6 +40,10 @@ GpuModeManager::GpuModeManager() {
 }
 
 GpuModeManager::~GpuModeManager() {
+}
+
+bool GpuModeManager::initial_gpu_mode_pref() const {
+  return initial_gpu_mode_pref_;
 }
 
 // static
