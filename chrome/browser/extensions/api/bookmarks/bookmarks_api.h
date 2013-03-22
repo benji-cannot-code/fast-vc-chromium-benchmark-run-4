94bb4b4490bd095005c7f671ce82494939adcf4f
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 15:20:02 +0000

    bookmarks: Pass Profile pointer to BookmarkEventRouter through its constructor.
    That we we don't need to retrieve the profile pointer through BookmarkModel.
    Which is desired, because we want to reduce the dependencies on c/b/profiles
    from c/b/bookmarks/.
    
    BUG=144783,219182
    R=sky@chromium.org
    TBR=asargent@chromium.org
    
    Review URL: https://codereview.chromium.org/12709004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189811 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bookmarks/bookmarks_api.h
+++ chrome/browser/extensions/api/bookmarks/bookmarks_api.h
@@ -18,6 +18,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_function.h"
 #include "ui/shell_dialogs/select_file_dialog.h"
 
+class Profile;
+
 namespace base {
 class FilePath;
 class ListValue;
@@ -29,7 +31,7 @@ namespace extensions {
 // the extension system.
 class BookmarkEventRouter : public BookmarkModelObserver {
  public:
-  explicit BookmarkEventRouter(BookmarkModel* model);
+  BookmarkEventRouter(Profile* profile, BookmarkModel* model);
   virtual ~BookmarkEventRouter();
 
   // BookmarkModelObserver:
@@ -58,10 +60,10 @@ class BookmarkEventRouter : public BookmarkModelObserver {
 
  private:
   // Helper to actually dispatch an event to extension listeners.
-  void DispatchEvent(Profile* profile,
-                     const char* event_name,
+  void DispatchEvent(const char* event_name,
                      scoped_ptr<base::ListValue> event_args);
 
+  Profile* profile_;
   BookmarkModel* model_;
 
   DISALLOW_COPY_AND_ASSIGN(BookmarkEventRouter);
