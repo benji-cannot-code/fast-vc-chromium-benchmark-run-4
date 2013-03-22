42f26443e039810d0d3fdee60f23fb5a13e8457b
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 01:00:57 +0000

    [android] Add notifications to Interactive Autocomplete.
    Start sending updateNotificationArea calls from c++ to Java.
    
    BUG=178536
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12544029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189729 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillDialogNotification.java
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.autofill;
+
+/**
+ * AutofillDialog notification container.
+ */
+class AutofillDialogNotification {
+    final int mBackgroundColor;
+    final int mTextColor;
+    final boolean mHasArrow;
+    final boolean mHasCheckbox;
+    final String mText;
+
+    public AutofillDialogNotification(int backgroundColor, int textColor,
+            boolean hasArrow, boolean hasCheckbox, String text) {
+        mBackgroundColor = backgroundColor;
+        mTextColor = textColor;
+        mHasArrow = hasArrow;
+        mHasCheckbox = hasCheckbox;
+        mText = text;
+    }
+}
