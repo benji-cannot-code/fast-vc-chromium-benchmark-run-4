61011295f6850a59a05663278b6dc37a896cb2aa
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 19:43:37 +0000

    cc: Fix style errors with indented returns
    A few remaining files had four-space indent.
    
    NOTRY=true
    R=danakj@chromium.org
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12992005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189882 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/quads/render_pass_draw_quad.cc
+++ cc/quads/render_pass_draw_quad.cc
@@ -17,7 +17,7 @@ RenderPassDrawQuad::~RenderPassDrawQuad() {
 }
 
 scoped_ptr<RenderPassDrawQuad> RenderPassDrawQuad::Create() {
-    return make_scoped_ptr(new RenderPassDrawQuad);
+  return make_scoped_ptr(new RenderPassDrawQuad);
 }
 
 scoped_ptr<RenderPassDrawQuad> RenderPassDrawQuad::Copy(
