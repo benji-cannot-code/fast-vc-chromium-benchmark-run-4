08beb0d14f2404bf72c17c49aa99a493f230a9d5
rcui@google.com <rcui@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 22:40:00 +0000

    Patch checklicenses with whitelists for chromite dependencies.
    These depedencies are whitelisted:
    
    third_party/chromite/third_party/argparse.py
    third_party/pyelftools/
    
    BUG=chromium:222615
    TEST=ran check_licenses.py
    
    Review URL: https://codereview.chromium.org/12422017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189932 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/checklicenses/checklicenses.py
+++ tools/checklicenses/checklicenses.py
@@ -180,6 +180,13 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     'third_party/bzip2': [
         'UNKNOWN',
     ],
+
+    # http://crbug.com/222828
+    # http://bugs.python.org/issue17514
+    'third_party/chromite/third_party/argparse.py': [
+        'UNKNOWN',
+    ],
+
     'third_party/cld/encodings/compact_lang_det': [  # http://crbug.com/98120
         'UNKNOWN',
     ],
@@ -330,6 +337,13 @@ PATH_SPECIFIC_WHITELISTED_LICENSES = {
     'third_party/protobuf': [  # http://crbug.com/98455
         'UNKNOWN',
     ],
+
+    # http://crbug.com/222831
+    # https://bitbucket.org/eliben/pyelftools/issue/12
+    'third_party/pyelftools': [
+        'UNKNOWN',
+    ],
+
     'third_party/pylib': [
         'UNKNOWN',
     ],
