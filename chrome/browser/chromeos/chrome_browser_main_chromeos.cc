c47983f1835b33f1f6802517e5e472e62c12f30e
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 13:47:23 +0000

    Show a warning before logging out the user on idle
    This CL adds a modal dialog that appears when powerd sends the
    IdleActionImminent signal, warning the user that a logout on idle is
    about to occur.
    
    BUG=195506
    TEST=Manual
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/12810008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/chrome_browser_main_chromeos.cc
+++ chrome/browser/chromeos/chrome_browser_main_chromeos.cc
@@ -59,6 +59,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/net/network_change_notifier_network_library.h"
 #include "chrome/browser/chromeos/net/network_portal_detector.h"
 #include "chrome/browser/chromeos/power/brightness_observer.h"
+#include "chrome/browser/chromeos/power/idle_action_warning_observer.h"
 #include "chrome/browser/chromeos/power/output_observer.h"
 #include "chrome/browser/chromeos/power/power_button_observer.h"
 #include "chrome/browser/chromeos/power/resume_observer.h"
@@ -647,6 +648,10 @@ void ChromeBrowserMainPartsChromeos::PostProfileInit() {
   automatic_reboot_manager_.reset(new system::AutomaticRebootManager(
       scoped_ptr<base::TickClock>(new base::DefaultTickClock)));
 
+  // This observer cannot be created earlier because it requires the shell to be
+  // available.
+  idle_action_warning_observer_.reset(new IdleActionWarningObserver());
+
   ChromeBrowserMainPartsLinux::PostProfileInit();
 }
 
@@ -747,6 +752,7 @@ void ChromeBrowserMainPartsChromeos::PostMainMessageLoopRun() {
   power_button_observer_.reset();
   screen_dimming_observer_.reset();
   screensaver_controller_.reset();
+  idle_action_warning_observer_.reset();
 
   // Delete ContactManager while |g_browser_process| is still alive.
   contact_manager_.reset();
