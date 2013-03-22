2daf175de45ce190ddeb1cd6c1a6784fc3d49126
ggaren@apple.com <ggaren@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-22 22:19:52 +0000

    Added a setting for whether JavaScript markup is enabled https://bugs.webkit.org/show_bug.cgi?id=112999
    Reviewed by Maciej Stachowiak.
    
    This setting is useful for clients that want protection from script
    injection attacks.
    
    ../WebCore: 
    
    * page/Settings.h:
    (Settings): Clarified which clients should call canExecuteScripts().
    
    * page/Settings.in: Added the new setting.
    
    ../WebKit2: 
    
    * Shared/WebPreferencesStore.h:
    * UIProcess/API/C/WKPreferences.cpp:
    (WKPreferencesGetJavaScriptMarkupEnabled):
    (WKPreferencesSetJavaScriptMarkupEnabled):
    (WKPreferencesGetJavaScriptEnabled):
    * UIProcess/API/C/WKPreferences.h:
    * UIProcess/API/mac/WKBrowsingContextGroup.mm:
    (-[WKBrowsingContextGroup allowsJavaScriptMarkup]):
    (-[WKBrowsingContextGroup setAllowsJavaScriptMarkup:]):
    * WebProcess/InjectedBundle/InjectedBundle.cpp:
    (WebKit::InjectedBundle::overrideBoolPreferenceForTestRunner):
    * WebProcess/WebPage/WebPage.cpp:
    (WebKit::WebPage::updatePreferences): Plumbed through to API.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKBrowsingContextGroup.mm
+++ third_party/WebKit/Source/WebKit2/UIProcess/API/mac/WKBrowsingContextGroup.mm
@@ -76,6 +76,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     WKPreferencesSetJavaScriptEnabled(WKPageGroupGetPreferences(self._pageGroupRef), allowsJavaScript);
 }
 
+- (BOOL)allowsJavaScriptMarkup
+{
+    return WKPreferencesGetJavaScriptMarkupEnabled(WKPageGroupGetPreferences(self._pageGroupRef));
+}
+
+- (void)setAllowsJavaScriptMarkup:(BOOL)allowsJavaScriptMarkup
+{
+    WKPreferencesSetJavaScriptMarkupEnabled(WKPageGroupGetPreferences(self._pageGroupRef), allowsJavaScriptMarkup);
+}
+
 - (BOOL)allowsPlugIns
 {
     return WKPreferencesGetPluginsEnabled(WKPageGroupGetPreferences(self._pageGroupRef));
