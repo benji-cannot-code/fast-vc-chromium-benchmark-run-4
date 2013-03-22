f9cebd6cf867a43907988b5566a3fb631f3b12a1
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 19:33:26 +0000

    Revert 189870 "Add speaker on/off control on Android for WebRTC "
    > Add speaker on/off control on Android for WebRTC 
    > 
    > Description: 
    > Application listens to the headset plug intent, turns on or off speaker when 
    > webrtc is running. It will help devices with dual mics to improve audio 
    > quality. 
    > 
    > BUG=222394
    > 
    > Contributed by leozwang@chromium.org (https://chromiumcodereview.appspot.com/12974004/)
    > Review URL: https://codereview.chromium.org/12702031
    
    Crashing content_unittests:
    AudioInputDeviceManagerTest.OpenNotExistingDevice
    AudioInputDeviceManagerTest.OpenDeviceTwice
    AudioInputDeviceManagerTest.OpenMultipleDevices
    AudioInputDeviceManagerTest.OpenAndCloseDevice
    AudioInputDeviceManagerTest.AccessInvalidSession
    AudioInputDeviceManagerTest.AccessAndCloseSession
    
    Crashing media_unittests:
    AudioInputDeviceTest.EnumerateDevices
    AudioOutputControllerTest.PlayAndClose
    AudioInputControllerTest.SamplesPerPacketTooLarge
    AudioOutputControllerTest.HardwareBufferTooLarge
    AudioOutputControllerTest.CreateAndClose
    AudioInputTest.SanityOnMakeParams
    
    Sample logs:
    http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/8997/steps/content_unittests/logs/stdio
    http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/8997/steps/media_unittests/logs/stdio
    
    TBR=wjia@chromium.org
    Review URL: https://codereview.chromium.org/12827031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189876 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
+++ media/base/android/java/src/org/chromium/media/AudioManagerAndroid.java
@@ -5,10 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.media;
 
-import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
 import android.media.AudioManager;
 
 import org.chromium.base.CalledByNative;
@@ -16,54 +13,12 @@ import org.chromium.base.JNINamespace;
 
 @JNINamespace("media")
 class AudioManagerAndroid {
-    private static final String TAG = AudioManagerAndroid.class.getSimpleName();
-
-    private final AudioManager mAudioManager;
-    private final Context mContext;
-
-    private BroadcastReceiver mReceiver;
-    private boolean mOriginalSpeakerStatus;
-
     @CalledByNative
-    public void setMode(int mode) {
-        mAudioManager.setMode(mode);
-    }
-
-    @CalledByNative
-    private static AudioManagerAndroid createAudioManagerAndroid(Context context) {
-        return new AudioManagerAndroid(context);
-    }
-
-    private AudioManagerAndroid(Context context) {
-        mContext = context;
-        mAudioManager = (AudioManager)mContext.getSystemService(Context.AUDIO_SERVICE);
-    }
-
-    @CalledByNative
-    public void registerHeadsetReceiver() {
-        if (mReceiver != null) {
-            return;
+    public static void setMode(Context context, int mode) {
+        AudioManager audioManager =
+                (AudioManager)context.getSystemService(Context.AUDIO_SERVICE);
+        if (null != audioManager) {
+            audioManager.setMode(mode);
         }
-
-        mOriginalSpeakerStatus = mAudioManager.isSpeakerphoneOn();
-        IntentFilter filter = new IntentFilter(Intent.ACTION_HEADSET_PLUG);
-
-        mReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                if (Intent.ACTION_HEADSET_PLUG.equals(intent.getAction())) {
-                    mAudioManager.setSpeakerphoneOn(
-                            intent.getIntExtra("state", 0) == 0);
-                }
-            }
-        };
-        mContext.registerReceiver(mReceiver, filter);
-    }
-
-    @CalledByNative
-    public void unregisterHeadsetReceiver() {
-        mContext.unregisterReceiver(mReceiver);
-        mReceiver = null;
-        mAudioManager.setSpeakerphoneOn(mOriginalSpeakerStatus);
     }
 }
