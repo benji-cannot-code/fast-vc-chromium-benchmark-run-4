c1ec0757f5eec9299acd2fda51d2d6f9dde02f1c
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 23:21:06 +0000

    [Android rAc] Fix typo in Dialog Submit Call
    BUG=177382
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12583009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillDialogGlue.java
+++ chrome/android/java/src/org/chromium/chrome/browser/autofill/AutofillDialogGlue.java
@@ -146,7 +146,7 @@ public class AutofillDialogGlue implements AutofillDialogDelegate {
 
     @Override
     public void dialogSubmit() {
-        nativeDialogCancel(mNativeDialogPopup);
+        nativeDialogSubmit(mNativeDialogPopup);
     }
 
     @Override
