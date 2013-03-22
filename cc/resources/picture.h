6b5dbf08abe1d1e572e178622750ed7e7d2ef989
eugenis@chromium.org <eugenis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 09:27:43 +0000

    Libcxx cleanup.
    Missing <vector> include in picture.h.
    map<> requires a complete type for the second template argument.
    
    BUG=178409
    
    R=reveman@chromium.org,matthewjheaney@chromium.org,acolwell@chromium.org
    
    Review URL: https://codereview.chromium.org/12950003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189777 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/picture.h
+++ cc/resources/picture.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_RESOURCES_PICTURE_H_
 
 #include <list>
+#include <vector>
 
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
