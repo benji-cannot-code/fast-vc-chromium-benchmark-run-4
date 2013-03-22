b3b8ddb1194b0bb62dfc7fbfd02f0d6e5d84443e
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-22 05:26:02 +0000

    Add a profile object for android to pass to java.
    This is to avoid passing the native pointer around in java land and make
    it more explicit what it being operated on.
    
    BUG=
    TBR=thakis@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/12782006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189764 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/android/java/src/org/chromium/chrome/browser/profiles/Profile.java
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.chrome.browser.profiles;
+
+import org.chromium.base.CalledByNative;
+
+/**
+ * Wrapper that allows passing a Profile reference around in the Java layer.
+ */
+public class Profile {
+
+    private int mNativeProfileAndroid;
+
+    private Profile(int nativeProfileAndroid) {
+        mNativeProfileAndroid = nativeProfileAndroid;
+    }
+
+    @CalledByNative
+    private static Profile create(int nativeProfileAndroid) {
+        return new Profile(nativeProfileAndroid);
+    }
+
+    @CalledByNative
+    private void destroy() {
+        mNativeProfileAndroid = 0;
+    }
+
+    @CalledByNative
+    private int getNativePointer() {
+        return mNativeProfileAndroid;
+    }
+}
--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autofill/android/personal_data_manager_android.h"
 #include "chrome/browser/history/android/sqlite_cursor.h"
 #include "chrome/browser/lifetime/application_lifetime_android.h"
+#include "chrome/browser/profiles/profile_android.h"
 #include "chrome/browser/search_engines/template_url_service_android.h"
 #include "chrome/browser/sync/profile_sync_service_android.h"
 #include "chrome/browser/ui/android/autofill/autofill_dialog_view_android.h"
@@ -60,6 +61,7 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "PersonalDataManagerAndroid",
       PersonalDataManagerAndroid::Register},
   { "ProcessUtils", RegisterProcessUtils },
+  { "ProfileAndroid", ProfileAndroid::RegisterProfileAndroid },
   { "ProfileSyncService", ProfileSyncServiceAndroid::Register },
   { "SqliteCursor", SQLiteCursor::RegisterSqliteCursor },
   { "SSLClientCertificateRequest",
