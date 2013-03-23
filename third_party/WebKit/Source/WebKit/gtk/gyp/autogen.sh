8bd3aa7ed0c804f6907f02f51065a98a6c081ea9
kov@webkit.org <kov@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-23 00:10:39 +0000

    Trivial fixes to the gyp-build autogen.sh script.
    Reviewed by Martin Robinson (mrobinson).
    
    * gyp/autogen.sh: add /bin/sh -e as hashbang, so stricter shells will be happy to
    run the script; deal with automake exiting with an error code because there is no
    Makefile.am for it to work on.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/gtk/gyp/autogen.sh
+++ third_party/WebKit/Source/WebKit/gtk/gyp/autogen.sh
@@ -1,4 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/bin/sh -e
+
 export SCRIPT_DIRECTORY=`dirname $0`
 export TOPLEVEL_DIRECTORY="$SCRIPT_DIRECTORY/../../../.."
 
@@ -36,7 +38,7 @@ autoreconf --verbose --install -I Macros $ACLOCAL_FLAGS
 # Automake is bizarrely responsible for copying some of the files necessary
 # for configure to run to the macros directory. So we invoke it here and hide
 # the warnings we get about not using automake in configure.ac.
-automake --add-missing --copy > /dev/null 2>&1
+automake --add-missing --copy > /dev/null 2>&1 || true
 
 if test -z "$NOCONFIGURE"; then
     ./configure $AUTOGEN_CONFIGURE_ARGS "$@" || exit $?
