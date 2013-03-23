132ca682571ceae71639c7f0d7fb87a589510540
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 18:25:05 +0000

    Adding a "Learn more" link to the Google DNS selection on the static IP configuration page in network settings.
    Also, adjust the size of the dialog so that custom DNS fields don't
    cause scrolling.
    
    BUG=chromium:172634
    
    
    Review URL: https://chromiumcodereview.appspot.com/12502012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190053 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/internet_detail.js
+++ chrome/browser/resources/options/chromeos/internet_detail.js
@@ -276,6 +276,9 @@ cr.define('options.internet', function() {
         this.handleNameServerTypeChange_);
       $('user-dns-radio').addEventListener('click',
         this.handleNameServerTypeChange_);
+
+      $('google-dns-label').innerHTML =
+        loadTimeData.getString('googleNameServers');
     },
 
     /**
