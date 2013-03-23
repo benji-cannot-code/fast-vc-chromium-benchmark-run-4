8a78a8b9e59a1e1d07e0a1a0d151ff9aa244e2e6
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 04:35:55 +0000

    Add getFileStatuses() function to JavaScript API
    Original review URL: https://codereview.chromium.org/12573009
    
    BUG=192734
    TEST=Manual. Will convert unpacked extension test to automated version in next patch.
    TBR=benwells,calvinlo
    
    Review URL: https://codereview.chromium.org/12573013
    Patch from Calvin Lo <calvinlo@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189987 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/sync_file_system/sync_file_system_api.h
+++ chrome/browser/extensions/api/sync_file_system/sync_file_system_api.h
@@ -6,9 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_SYNC_FILE_SYSTEM_SYNC_FILE_SYSTEM_API_H_
 #define CHROME_BROWSER_EXTENSIONS_API_SYNC_FILE_SYSTEM_SYNC_FILE_SYSTEM_API_H_
 
+#include <map>
+
 #include "base/platform_file.h"
 #include "chrome/browser/extensions/extension_function.h"
 #include "chrome/browser/sync_file_system/conflict_resolution_policy.h"
+#include "chrome/common/extensions/api/sync_file_system.h"
 #include "webkit/fileapi/syncable/sync_file_status.h"
 #include "webkit/fileapi/syncable/sync_status_code.h"
 #include "webkit/quota/quota_types.h"
@@ -50,6 +53,28 @@ class SyncFileSystemGetFileStatusFunction
   void DidGetFileStatus(
       const sync_file_system::SyncStatusCode sync_service_status,
       const sync_file_system::SyncFileStatus sync_file_status);
+};
+
+class SyncFileSystemGetFileStatusesFunction
+    : public AsyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("syncFileSystem.getFileStatuses",
+                             SYNCFILESYSTEM_GETFILESYNCSTATUSES)
+  SyncFileSystemGetFileStatusesFunction();
+
+ protected:
+  virtual ~SyncFileSystemGetFileStatusesFunction();
+  virtual bool RunImpl() OVERRIDE;
+
+ private:
+  unsigned int num_expected_results_;
+  unsigned int num_results_received_;
+  std::map<std::string, sync_file_system::SyncStatusCode> sync_status_codes_;
+  std::map<std::string, api::sync_file_system::FileStatus> file_sync_statuses_;
+  void DidGetFileStatus(
+      const std::string& file_path,
+      sync_file_system::SyncStatusCode sync_status_code,
+      sync_file_system::SyncFileStatus sync_file_statuses);
 };
 
 class SyncFileSystemGetUsageAndQuotaFunction
