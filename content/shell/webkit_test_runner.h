39341a499fc5e1a40c03111e96cde4ab5a9e250a
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 02:49:05 +0000

    [content shell] work around asynchronous history navigations killing layout tests
    Between tests, we load about:blank into the RenderView to avoid stray navigation
    signals from e.g. plugins. However, if a history navigation is pending, there might
    be an IPC in flight and change which page we're displaying even after we've
    navigated to about:blank.
    
    Work around this issue by ensuring during resetting that we're indeed at about:blank.
    
    BUG=111316
    R=marja@chromium.org
    TEST=we're not getting randomly killed for invalid navigations
    
    Review URL: https://codereview.chromium.org/12921005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189974 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_runner.h
+++ content/shell/webkit_test_runner.h
@@ -110,10 +110,6 @@ class WebKitTestRunner : public RenderViewObserver,
   // the TestRunner library and sends them to the browser process.
   void CaptureDump();
 
-  // Navigate the RenderView to about:blank and notify the browser process that
-  // the test is done.
-  void NavigateRenderViewAndNotify();
-
   ::WebTestRunner::WebTestProxyBase* proxy_;
 
   RenderView* focused_view_;
