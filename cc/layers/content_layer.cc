0e6e997de97ff6d730c67daf6136979215a24e6d
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 18:50:16 +0000

    Invalidate content-layer if LCD text setting changes.
    BUG=100666
    
    
    Review URL: https://chromiumcodereview.appspot.com/12543018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190061 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/content_layer.cc
+++ cc/layers/content_layer.cc
@@ -50,7 +50,9 @@ scoped_refptr<ContentLayer> ContentLayer::Create(ContentLayerClient* client) {
 
 ContentLayer::ContentLayer(ContentLayerClient* client)
     : TiledLayer(),
-      client_(client) {}
+      client_(client),
+      can_use_lcd_text_last_frame_(can_use_lcd_text()) {
+}
 
 ContentLayer::~ContentLayer() {}
 
@@ -74,6 +76,7 @@ void ContentLayer::Update(ResourceUpdateQueue* queue,
                                                   true);
 
     CreateUpdaterIfNeeded();
+    UpdateCanUseLCDText();
   }
 
   TiledLayer::Update(queue, occlusion, stats);
@@ -110,6 +113,15 @@ void ContentLayer::SetContentsOpaque(bool opaque) {
   Layer::SetContentsOpaque(opaque);
   if (updater_)
     updater_->SetOpaque(opaque);
+}
+
+void ContentLayer::UpdateCanUseLCDText() {
+  if (can_use_lcd_text_last_frame_ == can_use_lcd_text())
+    return;
+
+  can_use_lcd_text_last_frame_ = can_use_lcd_text();
+  if (client_)
+    client_->DidChangeLayerCanUseLCDText();
 }
 
 }  // namespace cc
--- ui/compositor/layer.h
+++ ui/compositor/layer.h
@@ -295,6 +295,7 @@ class COMPOSITOR_EXPORT Layer
   // ContentLayerClient
   virtual void PaintContents(
       SkCanvas* canvas, gfx::Rect clip, gfx::RectF* opaque) OVERRIDE;
+  virtual void DidChangeLayerCanUseLCDText() OVERRIDE {}
 
   cc::Layer* cc_layer() { return cc_layer_; }
 
