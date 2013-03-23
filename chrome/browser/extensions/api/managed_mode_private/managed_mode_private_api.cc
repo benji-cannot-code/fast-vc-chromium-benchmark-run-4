426b2fd09a4cf544dc159611a318f106210447ad
Samusaaron3@gmail.com <Samusaaron3@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 16:38:46 +0000

    Modify managed_mode_private extensions api to use JSON Schema Compiler
    BUG=121174
    
    Review URL: https://chromiumcodereview.appspot.com/12378079
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190029 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/managed_mode_private/managed_mode_private_api.cc
+++ chrome/browser/extensions/api/managed_mode_private/managed_mode_private_api.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/managed_mode/managed_mode.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/extensions/api/managed_mode_private.h"
 #include "chrome/common/pref_names.h"
 #include "content/public/browser/notification_details.h"
 
@@ -40,6 +41,9 @@ const char kEnterSuccessKey[] = "success";
 namespace keys = extensions::preference_api_constants;
 
 namespace extensions {
+
+namespace GetPolicy = api::managed_mode_private::GetPolicy;
+namespace SetPolicy = api::managed_mode_private::SetPolicy;
 
 ManagedModeEventRouter::ManagedModeEventRouter(
     Profile* profile) : profile_(profile) {
@@ -98,12 +102,12 @@ void ManagedModePrivateEnterFunction::SendResult(bool success) {
 ManagedModePrivateGetPolicyFunction::~ManagedModePrivateGetPolicyFunction() { }
 
 bool ManagedModePrivateGetPolicyFunction::RunImpl() {
-  std::string key;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &key));
+  scoped_ptr<GetPolicy::Params> params(GetPolicy::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
 #if defined(ENABLE_CONFIGURATION_POLICY)
   policy::ManagedModePolicyProvider* policy_provider =
       profile_->GetManagedModePolicyProvider();
-  const base::Value* policy = policy_provider->GetPolicy(key);
+  const base::Value* policy = policy_provider->GetPolicy(params->key);
   if (policy)
     SetResult(policy->DeepCopy());
 #endif
@@ -113,14 +117,12 @@ bool ManagedModePrivateGetPolicyFunction::RunImpl() {
 ManagedModePrivateSetPolicyFunction::~ManagedModePrivateSetPolicyFunction() { }
 
 bool ManagedModePrivateSetPolicyFunction::RunImpl() {
-  std::string key;
-  EXTENSION_FUNCTION_VALIDATE(args_->GetString(0, &key));
-  base::Value* value = NULL;
-  EXTENSION_FUNCTION_VALIDATE(args_->Get(1, &value));
+  scoped_ptr<SetPolicy::Params> params(SetPolicy::Params::Create(*args_));
+  EXTENSION_FUNCTION_VALIDATE(params.get());
 #if defined(ENABLE_CONFIGURATION_POLICY)
   policy::ManagedModePolicyProvider* policy_provider =
       profile_->GetManagedModePolicyProvider();
-  policy_provider->SetPolicy(key, value);
+  policy_provider->SetPolicy(params->key, params->value.get());
 #endif
   return true;
 }
