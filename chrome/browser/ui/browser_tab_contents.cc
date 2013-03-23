43d189a471be141c48d0f4e76cddffb1ebe5344f
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-23 16:30:49 +0000

    Restore SnapshotTabHelper temporarily until dependencies are fixed (reverts deletions in r189969). Do not use this class in Chrome.
    BUG=223374
    TBR=jam@, sky@
    
    Review URL: https://codereview.chromium.org/12770014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190027 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_tab_contents.cc
+++ chrome/browser/ui/browser_tab_contents.cc
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/sad_tab_helper.h"
 #include "chrome/browser/ui/search/search_tab_helper.h"
 #include "chrome/browser/ui/search_engines/search_engine_tab_helper.h"
+#include "chrome/browser/ui/snapshot_tab_helper.h"
 #include "chrome/browser/ui/sync/tab_contents_synced_tab_delegate.h"
 #include "chrome/browser/ui/tab_contents/core_tab_helper.h"
 #include "chrome/browser/ui/web_contents_modal_dialog_manager.h"
@@ -145,6 +146,7 @@ void BrowserTabContents::AttachTabHelpers(WebContents* web_contents) {
   safe_browsing::SafeBrowsingTabObserver::CreateForWebContents(web_contents);
   SearchEngineTabHelper::CreateForWebContents(web_contents);
   chrome::search::SearchTabHelper::CreateForWebContents(web_contents);
+  SnapshotTabHelper::CreateForWebContents(web_contents);
   SSLTabHelper::CreateForWebContents(web_contents);
   TabContentsSyncedTabDelegate::CreateForWebContents(web_contents);
   TabSpecificContentSettings::CreateForWebContents(web_contents);
