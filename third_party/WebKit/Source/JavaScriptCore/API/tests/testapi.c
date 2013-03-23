9144398810e542b8032b817bad78457664f023de
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-23 03:24:23 +0000

    Fix the typo: WIN -> WINDOWS.
    * API/tests/testapi.c:
    (main):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146708 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/API/tests/testapi.c
+++ third_party/WebKit/Source/JavaScriptCore/API/tests/testapi.c
@@ -135,7 +135,7 @@ static void assertEqualsAsCharactersPtr(JSValueRef value, const char* expectedVa
     JSStringRelease(valueAsString);
 }
 
-#if !OS(WIN)
+#if !OS(WINDOWS)
 static int leakedObject = 1;
 
 static void leakFinalize(JSObjectRef object)
@@ -1745,7 +1745,7 @@ int main(int argc, char* argv[])
         free(scriptUTF8);
     }
 
-#if !OS(WIN)
+#if !OS(WINDOWS)
     testLeakingPrototypesAcrossContexts();
 #endif
 
