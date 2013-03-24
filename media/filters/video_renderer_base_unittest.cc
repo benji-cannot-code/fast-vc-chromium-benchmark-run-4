6b0a40616170b83abfec8f86a461c2d402811071
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 04:06:44 +0000

    Introduce VideoFrameStream, the video frame provider in media pipeline.
    VideoFrameStream wraps DemuxerStreams and VideoDecoders to provide VideoFrames
    to it's client (e.g. VideoRendererBase). It hides details of the interraction
    between DemuxerStream and VideoDecoder. To it's client, VideoFrameStream works
    as a reliable VideoFrame source which, after initialization, simply returns
    VideoFrames until end-of-stream (EOS).
    
    BUG=141788
    TEST=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12818004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190181 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_renderer_base_unittest.cc
+++ media/filters/video_renderer_base_unittest.cc
@@ -60,7 +60,7 @@ class VideoRendererBaseTest : public ::testing::Test {
 
     // We expect these to be called but we don't care how/when.
     EXPECT_CALL(*decoder_, Stop(_))
-        .WillRepeatedly(RunClosure<0>());
+        .WillRepeatedly(Invoke(this, &VideoRendererBaseTest::StopRequested));
     EXPECT_CALL(statistics_cb_object_, OnStatistics(_))
         .Times(AnyNumber());
     EXPECT_CALL(*this, OnTimeUpdate(_))
@@ -341,6 +341,17 @@ class VideoRendererBaseTest : public ::testing::Test {
     message_loop_.PostTask(FROM_HERE, callback);
   }
 
+  void StopRequested(const base::Closure& callback) {
+    DCHECK_EQ(&message_loop_, MessageLoop::current());
+    decode_results_.clear();
+    if (!read_cb_.is_null()) {
+      QueueAbortedRead();
+      SatisfyPendingRead();
+    }
+
+    message_loop_.PostTask(FROM_HERE, callback);
+  }
+
   MessageLoop message_loop_;
 
   VideoDecoderConfig video_config_;
@@ -591,9 +602,6 @@ TEST_F(VideoRendererBaseTest, StopDuringOutstandingRead) {
 
   WaitableMessageLoopEvent event;
   renderer_->Stop(event.GetClosure());
-
-  QueueEndOfStream();
-  SatisfyPendingRead();
 
   event.RunAndWait();
 }
