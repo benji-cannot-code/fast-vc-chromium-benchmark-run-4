56aedf6cb471287483b64e26f4f014f51a2e02f2
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 00:27:45 +0000

    GPU: Merge the GpuChannel message filters.
    The order that the filters were added to the GpuChannel mattered for
    correctness. This is non-obvious and dangerous from a maintenance
    perspective. So merge the filters into one.
    
    BUG=none
    
    
    Review URL: https://chromiumcodereview.appspot.com/12702034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190132 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_channel.h
+++ content/common/gpu/gpu_channel.h
@@ -52,7 +52,7 @@ namespace content {
 class GpuChannelManager;
 struct GpuRenderingStats;
 class GpuWatchdog;
-class SyncPointMessageFilter;
+class GpuChannelMessageFilter;
 
 // Encapsulates an IPC channel between the GPU process and one renderer
 // process. On the renderer side there's a corresponding GpuChannelHost.
@@ -159,7 +159,7 @@ class GpuChannel : public IPC::Listener,
 
  private:
   friend class base::RefCountedThreadSafe<GpuChannel>;
-  friend class SyncPointMessageFilter;
+  friend class GpuChannelMessageFilter;
 
   void OnDestroy();
 
@@ -247,7 +247,7 @@ class GpuChannel : public IPC::Listener,
 
   base::WeakPtrFactory<GpuChannel> weak_factory_;
 
-  scoped_refptr<SyncPointMessageFilter> filter_;
+  scoped_refptr<GpuChannelMessageFilter> filter_;
   scoped_refptr<base::MessageLoopProxy> io_message_loop_;
 
   size_t num_stubs_descheduled_;
