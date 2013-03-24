620e3f262b9cc1dac5b294eef33fa71395ec48ff
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 01:36:58 +0000

    Ensure that the TransportDIB is large enough to act as the pixel data for the SkBitmap.
    The original patch was written by cdn@chromium.org.
    https://codereview.chromium.org/11756003/
    I added a test to cover that path.
    
    BUG=168050
    TEST=RenderWidgetHostSnapshotTaker.WidgetDidReceivePaintAtSizeAck
    
    Review URL: https://chromiumcodereview.appspot.com/12717009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190143 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/thumbnails/render_widget_snapshot_taker.h
+++ chrome/browser/thumbnails/render_widget_snapshot_taker.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/callback_forward.h"
+#include "base/gtest_prod_util.h"
 #include "base/memory/linked_ptr.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
@@ -53,6 +54,11 @@ class RenderWidgetSnapshotTaker : public content::NotificationObserver {
   void CancelSnapshot(content::RenderWidgetHost* renderer);
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(RenderWidgetSnapshotTakerTest,
+                           WidgetDidReceivePaintAtSizeAck);
+  FRIEND_TEST_ALL_PREFIXES(RenderWidgetSnapshotTakerTest,
+                           WidgetDidReceivePaintAtSizeAckFail);
+
   // content::NotificationObserver overrides.
   virtual void Observe(int type,
                        const content::NotificationSource& source,
