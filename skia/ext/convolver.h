ad14577cec823890eba5fe9c2cea80cff63c1e76
hubbe@chromium.org <hubbe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 09:39:57 +0000

    enable more SSE2
    BUG=181072
    
    Review URL: https://chromiumcodereview.appspot.com/12976003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190241 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/convolver.h
+++ skia/ext/convolver.h
@@ -14,10 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkTypes.h"
 
 #if defined(ARCH_CPU_X86_FAMILY)
-#if defined(__x86_64__) || defined(_M_X64) || defined(__SSE2__) || _M_IX86_FP==2
 // This is where we had compiler support for SSE2 instructions.
 #define SIMD_SSE2 1
-#endif
 #endif
 
 // avoid confusion with Mac OS X's math library (Carbon)
