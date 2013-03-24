645aa152b14f46b73a26ad94c381540db126af0d
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 06:46:30 +0000

    Removed obsolete enable-new-simple-notifications command line flag.
    This was necessary for a very short time while enable-rich-notifications
    wasn't quite ready yet, but it has been unnecessary and unused for a
    while now, and it's cleanup time.
    
    BUG=NONE.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12929028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190220 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center_switches.cc
+++ ui/message_center/message_center_switches.cc
@@ -10,7 +10,5 @@ namespace switches {
 
 const char kEnableRichNotifications[] = "enable-rich-notifications";
 
-const char kEnableNewSimpleNotifications[] = "enable-new-simple-notifications";
-
 }  // namespace switches
 }  // namespace message_center
