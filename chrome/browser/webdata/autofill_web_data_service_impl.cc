0b903cff724832225b153c2340553e3ee0b55bbe
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-24 17:12:21 +0000

    Move creation of WebDatabaseTable subclasses to WebDatabaseServiceFactory.
    Ownership still needs to stay with WebDatabaseService, but since WDS
    is intended to move out of //chrome, it must not know about all the
    individual types.
    
    TBR=isherman@chromium.org
    COLLABORATOR=caitkp@chromium.org
    COLLABORATOR=kaiwang@chromium.org
    BUG=181277
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=189721
    
    Review URL: https://codereview.chromium.org/12853004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190277 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/autofill_web_data_service_impl.cc
+++ chrome/browser/webdata/autofill_web_data_service_impl.cc
@@ -9,7 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webdata/web_data_service.h"
 
 AutofillWebDataService::AutofillWebDataService()
-    : WebDataServiceBase(WebDataServiceBase::ProfileErrorCallback()) {
+    : WebDataServiceBase(base::FilePath(),
+                         WebDataServiceBase::ProfileErrorCallback()) {
 }
 
 AutofillWebDataServiceImpl::AutofillWebDataServiceImpl(
