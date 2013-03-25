777240e96841e01e216e715a11cffcfb230594dd
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 18:23:55 +0000

    SyncFS: Copying-in foreign files into SyncFS should trigger Sync
    Partially reverting the past clean-up change made in:
    https://codereview.chromium.org/12336002/
    
    We should create FileSystemOperation for the dest_url (rather than for src_url)
    for Copy/Move so that we instantiate SyncableFileSystemOperation when
    we copy/move *into* SyncableFS (i.e. for src=non-syncfs and dest=syncfs case).
    
    (TODO: we should add automated test for this)
    
    BUG=223470
    TEST=manual (DnD-ing files into the sample app and see if sync starts)
    TEST=existing tests
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/12481030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190441 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/fileapi/fileapi_message_filter.cc
+++ content/browser/fileapi/fileapi_message_filter.cc
@@ -226,7 +226,7 @@ void FileAPIMessageFilter::OnMove(
     return;
   }
 
-  FileSystemOperation* operation = GetNewOperation(src_url, request_id);
+  FileSystemOperation* operation = GetNewOperation(dest_url, request_id);
   if (!operation)
     return;
   operation->Move(
@@ -247,7 +247,7 @@ void FileAPIMessageFilter::OnCopy(
     return;
   }
 
-  FileSystemOperation* operation = GetNewOperation(src_url, request_id);
+  FileSystemOperation* operation = GetNewOperation(dest_url, request_id);
   if (!operation)
     return;
   operation->Copy(
