4de76910b4b4b6729d476c4c268564325f94a3c2
nick@chromium.org <nick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 05:11:50 +0000

    In the tab capture code, use the FrameSubscriber mechanism and the notification service to learn of paint activity, and to initiate frame captures only when the underlying content changes.
    The control logic concerning when and how to initiate captures is moved from the "old" CaptureMachine into a new class called the CaptureOracle, which is the only multi-threaded object in the system. The CaptureMachine lives on the UI thread, and is now responsible mostly for tracking the active RenderWidgetHost and starting manual copies from it (the BackingStoreCopier is subsumed by the "new" CaptureMachine). For the active RWH, the CaptureMachine installs a ContentCaptureSubscription which routes events (meaning paints and timer events) from their origin to the Oracle.
    
    The old SynchronizedConsumer class is subsumed by the CaptureOracle. The Oracle has something called a SmoothEventSampler which handles the case where content is being produced faster than our target frame rate. Its implementation is dumb, but seems to work in the common cases.
    
    Parts of the old CaptureMachine become the WebContentsVideoCaptureDevice::Impl. I think the separating is pretty self-evident. This class no longer needs to be deleted from an outside thread, as it doesn't run its own "manager thread" anymore. A couple subtle problems are fixed in the way errors are handled by this class.
    
    BUG=181814,223427,181594
    TEST=content_unittests, manual testing with extensions
    
    Review URL: https://chromiumcodereview.appspot.com/12731011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190339 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/web_contents_video_capture_device.h
+++ content/browser/renderer_host/media/web_contents_video_capture_device.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-class CaptureMachine;  // Defined in web_contents_video_capture_device.cc.
 class RenderWidgetHost;
 
 // A virtualized VideoCaptureDevice that mirrors the displayed contents of a
@@ -61,15 +60,46 @@ class CONTENT_EXPORT WebContentsVideoCaptureDevice
   virtual const Name& device_name() OVERRIDE;
 
  private:
+  class Impl;
   WebContentsVideoCaptureDevice(const Name& name,
                                 int render_process_id,
                                 int render_view_id,
                                 const base::Closure& destroy_cb);
 
   Name device_name_;
-  scoped_refptr<CaptureMachine> capturer_;
+  scoped_refptr<Impl> capturer_;
 
   DISALLOW_COPY_AND_ASSIGN(WebContentsVideoCaptureDevice);
+};
+
+// Filters a sequence of events to achieve a target frequency.
+class CONTENT_EXPORT SmoothEventSampler {
+ public:
+  explicit SmoothEventSampler(base::TimeDelta capture_period,
+                              bool events_are_reliable);
+
+  // Add a new event to the event history, and return whether it ought to be
+  // sampled per to the sampling frequency limit. Even if this method returns
+  // true, the event is not recorded as a sample until RecordSample() is called.
+  bool AddEventAndConsiderSampling(base::Time now);
+
+  // Operates on the last event added by AddEventAndConsiderSampling(), marking
+  // it as sampled. After this point we are current in the stream of events, as
+  // we have sampled the most recent event.
+  void RecordSample();
+
+  // Returns true if, at time |now|, sampling should occur because too much time
+  // will have passed relative to the last event and/or sample.
+  bool IsOverdueForSamplingAt(base::Time now) const;
+
+  base::Time GetLastSampledEvent();
+
+ private:
+  const bool events_are_reliable_;
+  const base::TimeDelta capture_period_;
+  base::Time current_event_;
+  base::Time last_sample_;
+  DISALLOW_COPY_AND_ASSIGN(SmoothEventSampler);
 };
 
 }  // namespace content
