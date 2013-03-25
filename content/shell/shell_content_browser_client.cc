0ec5cc35feaf0cb670899c78d1c1d20dbe25b7c1
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 16:04:23 +0000

    [content shell] add QuotaPermissionContext implementation that always grants quota
    BUG=111316
    R=marja@chromium.org
    TEST=http/tests/filesystem/no-cache-filesystem-url.html passes
    
    Review URL: https://codereview.chromium.org/12965016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190411 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_content_browser_client.cc
+++ content/shell/shell_content_browser_client.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/shell/shell_devtools_delegate.h"
 #include "content/shell/shell_message_filter.h"
 #include "content/shell/shell_messages.h"
+#include "content/shell/shell_quota_permission_context.h"
 #include "content/shell/shell_resource_dispatcher_host_delegate.h"
 #include "content/shell/shell_switches.h"
 #include "content/shell/shell_web_contents_view_delegate_creator.h"
@@ -175,6 +176,11 @@ WebContentsViewDelegate* ShellContentBrowserClient::GetWebContentsViewDelegate(
 #else
   return NULL;
 #endif
+}
+
+QuotaPermissionContext*
+ShellContentBrowserClient::CreateQuotaPermissionContext() {
+  return new ShellQuotaPermissionContext();
 }
 
 #if defined(OS_ANDROID)
