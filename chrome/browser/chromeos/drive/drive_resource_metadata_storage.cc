23f8cc8da92fe1e690fe428e866861a5d833c99f
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 11:51:12 +0000

    chromeos: Reinitialize DriveResourceMeatadata from DriveSystemService::ClearCacheAndRemountFileSystem
    Clear existing data in DriveResourceMetadataStorage::Initialize.
    Call DriveResourceMetadata::Initialize from DriveSystemService::ClearCacheAndRemountFileSystem
    
    BUG=223484
    TEST=Open Files.app and move to Google Drive directory, hold Ctrl key and press the gear button on the right-top, press "Clean local cache"
    
    
    Review URL: https://chromiumcodereview.appspot.com/12987005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190366 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_resource_metadata_storage.cc
+++ chrome/browser/chromeos/drive/drive_resource_metadata_storage.cc
@@ -54,7 +54,9 @@ DriveResourceMetadataStorageMemory::~DriveResourceMetadataStorageMemory() {
 
 bool DriveResourceMetadataStorageMemory::Initialize() {
   base::ThreadRestrictions::AssertIOAllowed();
-  // Do nothing.
+  largest_changestamp_ = 0;
+  resource_map_.clear();
+  child_maps_.clear();
   return true;
 }
 
@@ -168,6 +170,9 @@ DriveResourceMetadataStorageDB::~DriveResourceMetadataStorageDB() {
 
 bool DriveResourceMetadataStorageDB::Initialize() {
   base::ThreadRestrictions::AssertIOAllowed();
+
+  resource_map_.reset();
+  child_map_.reset();
 
   const base::FilePath resource_map_path =
       directory_path_.Append(kResourceMapDBName);
