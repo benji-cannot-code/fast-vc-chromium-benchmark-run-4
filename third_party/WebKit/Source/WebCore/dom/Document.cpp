75c50dea70a6c886f1f00eefbeba6656126a1175
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-25 17:57:20 +0000

    Swap both the error and change event queue before processing fullscreen events https://bugs.webkit.org/show_bug.cgi?id=113194
    Reviewed by Jer Noble.
    
    Source/WebCore:
    
    While processing the change events, not only new change events but
    also new error events might get generated. However, for the timer, we
    should only process preexisting events.
    
    This is covered by fullscreen/full-screen-restrictions.html which times
    out depending on the delay between requesting fullscreen and granting
    fullscreen.
    
    * dom/Document.cpp:
    (WebCore::Document::fullScreenChangeDelayTimerFired):
    
    LayoutTests:
    
    * platform/chromium/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146787 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.cpp
+++ third_party/WebKit/Source/WebCore/dom/Document.cpp
@@ -5451,6 +5451,8 @@ void Document::fullScreenChangeDelayTimerFired(Timer<Document>*)
     RefPtr<Document> protectDocument(this);
     Deque<RefPtr<Node> > changeQueue;
     m_fullScreenChangeEventTargetQueue.swap(changeQueue);
+    Deque<RefPtr<Node> > errorQueue;
+    m_fullScreenErrorEventTargetQueue.swap(errorQueue);
 
     while (!changeQueue.isEmpty()) {
         RefPtr<Node> node = changeQueue.takeFirst();
@@ -5468,9 +5470,6 @@ void Document::fullScreenChangeDelayTimerFired(Timer<Document>*)
         node->dispatchEvent(Event::create(eventNames().webkitfullscreenchangeEvent, true, false));
     }
 
-    Deque<RefPtr<Node> > errorQueue;
-    m_fullScreenErrorEventTargetQueue.swap(errorQueue);
-    
     while (!errorQueue.isEmpty()) {
         RefPtr<Node> node = errorQueue.takeFirst();
         if (!node)
