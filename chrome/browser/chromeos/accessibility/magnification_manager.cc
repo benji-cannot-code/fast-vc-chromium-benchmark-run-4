70de03c0a45eaebf604902750e423f8825b2d9b9
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 03:42:25 +0000

    Disable lens magnifier
    This patch temporary disables lens magnifier on M27. This patch will be reverted after M27 branch point is passed.
    
    BUG=221981
    TEST=Confirms lens magnifier is disabled.
    R=zork@chromium.org
    TBR=jhawkins@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190333 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/accessibility/magnification_manager.cc
+++ chrome/browser/chromeos/accessibility/magnification_manager.cc
@@ -100,7 +100,7 @@ class MagnificationManagerImpl : public MagnificationManager,
       return;
 
     DCHECK(type == ash::MAGNIFIER_FULL || type == ash::MAGNIFIER_PARTIAL);
-    type_ = type;
+    type_ = ash::MAGNIFIER_FULL;  // (leave out for full magnifier)
 
     if (profile_) {
       PrefService* prefs = profile_->GetPrefs();
@@ -110,13 +110,6 @@ class MagnificationManagerImpl : public MagnificationManager,
     }
 
     NotifyMagnifierChanged();
-
-    if (enabled_) {
-      ash::Shell::GetInstance()->magnification_controller()->SetEnabled(
-          type_ == ash::MAGNIFIER_FULL);
-      ash::Shell::GetInstance()->partial_magnification_controller()->SetEnabled(
-          type_ == ash::MAGNIFIER_PARTIAL);
-    }
   }
 
   virtual void SaveScreenMagnifierScale(double scale) OVERRIDE {
@@ -153,17 +146,7 @@ class MagnificationManagerImpl : public MagnificationManager,
   }
 
   ash::MagnifierType GetMagnifierTypeFromPref() {
-    if (!profile_)
-      return ash::kDefaultMagnifierType;
-
-    DCHECK(profile_->GetPrefs());
-    ash::MagnifierType type = static_cast<ash::MagnifierType>(
-        profile_->GetPrefs()->GetInteger(prefs::kScreenMagnifierType));
-
-    if (type == ash::MAGNIFIER_FULL || type == ash::MAGNIFIER_PARTIAL)
-      return type;
-
-    return ash::kDefaultMagnifierType;
+    return ash::MAGNIFIER_FULL;
   }
 
   void SetProfile(Profile* profile) {
