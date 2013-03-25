8a96388fbe117931d02b5710eb4df0db958a397b
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 05:59:39 +0000

    google_apis: Remove use of 'feed' from where inappropriate
    WAPI (Documents List API) is based on ATOM hence the term "feed" is
    appropriate but "feed" is not appropriate for Drive API.
    
    gdata_* files are untouhced as "feed" is appropriate in these files.
    
    BUG=165395
    TEST=none. Just renames and fixes in comments
    
    Review URL: https://codereview.chromium.org/13057002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/dummy_drive_service.cc
+++ chrome/browser/google_apis/dummy_drive_service.cc
@@ -42,7 +42,7 @@ std::string DummyDriveService::GetRootResourceId() const {
 }
 
 void DummyDriveService::GetResourceList(
-    const GURL& feed_url,
+    const GURL& url,
     int64 start_changestamp,
     const std::string& search_query,
     bool shared_with_me,
