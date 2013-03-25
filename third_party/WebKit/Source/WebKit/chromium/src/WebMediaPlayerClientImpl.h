111fa0556316b007345e28bbae362b41acfc7dea
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-25 22:48:15 +0000

    [chromium] Move ownership of compositor VideoLayer to WebMediaPlayer https://bugs.webkit.org/show_bug.cgi?id=112971
    Reviewed by James Robinson.
    
    Source/Platform:
    
    Defines REMOVE_WEBVIDEOFRAME in related headers to enable this code
    path in chromium.
    
    * chromium/public/WebVideoFrame.h:
    * chromium/public/WebVideoFrameProvider.h:
    
    Source/WebKit/chromium:
    
    This moves ownership of the compositor's video layer over to the
    implementation of WebMediaPlayer, as well as all interaction
    between the media player system and the compositor layer or its
    interfaces such as cc::VideoFrameProvider.
    
    Now, the WebMediaPlayer implementation calls setWebLayer() to
    provide the WebLayer* to the WebMediaPlayerClientImpl so that
    it can be inserted into the GraphicsLayer tree appropriately.
    
    Defines REMOVE_WEBVIDEOFRAME in related headers to enable this code
    path in chromium.
    
    Once chromium removes its use of WebVideoFrame and its related
    classes, we can remove them from WebKit entirely.
    
    * WebKit.gypi:
    * public/WebMediaPlayer.h:
    (WebKit):
    * public/WebMediaPlayerClient.h:
    * public/WebStreamTextureClient.h:
    * src/WebMediaPlayerClientImpl.cpp:
    (WebKit::WebMediaPlayerClientImpl::~WebMediaPlayerClientImpl):
    (WebKit::WebMediaPlayerClientImpl::readyStateChanged):
    (WebKit::WebMediaPlayerClientImpl::repaint):
    (WebKit::WebMediaPlayerClientImpl::setOpaque):
    (WebKit::WebMediaPlayerClientImpl::setWebLayer):
    (WebKit::WebMediaPlayerClientImpl::loadRequested):
    (WebKit::WebMediaPlayerClientImpl::platformLayer):
    (WebKit::WebMediaPlayerClientImpl::paint):
    (WebKit::WebMediaPlayerClientImpl::needsWebLayer):
    (WebKit::WebMediaPlayerClientImpl::supportsAcceleratedRendering):
    (WebKit::WebMediaPlayerClientImpl::acceleratedRenderingInUse):
    (WebKit::WebMediaPlayerClientImpl::create):
    (WebKit::WebMediaPlayerClientImpl::supportsType):
    * src/WebMediaPlayerClientImpl.h:
    (WebKit):
    (WebMediaPlayerClientImpl):
    * tests/WebMediaPlayerClientImplTest.cpp: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146819 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebMediaPlayerClientImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebMediaPlayerClientImpl.h
@@ -38,8 +38,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MediaPlayerPrivate.h"
 #include "WebAudioSourceProviderClient.h"
 #include "WebMediaPlayerClient.h"
-#include "WebStreamTextureClient.h"
-#include <public/WebVideoFrameProvider.h>
 #include <wtf/OwnPtr.h>
 #include <wtf/PassOwnPtr.h>
 
@@ -50,16 +48,10 @@ namespace WebKit {
 class WebHelperPluginImpl;
 class WebAudioSourceProvider;
 class WebMediaPlayer;
-class WebVideoLayer;
 
 // This class serves as a bridge between WebCore::MediaPlayer and
 // WebKit::WebMediaPlayer.
-class WebMediaPlayerClientImpl : public WebCore::MediaPlayerPrivateInterface
-#if USE(ACCELERATED_COMPOSITING)
-                               , public WebVideoFrameProvider
-#endif
-                               , public WebMediaPlayerClient
-                               , public WebStreamTextureClient {
+class WebMediaPlayerClientImpl : public WebCore::MediaPlayerPrivateInterface, public WebMediaPlayerClient {
 
 public:
     static bool isEnabled();
@@ -91,7 +83,8 @@ public:
     virtual void keyNeeded(const WebString& keySystem, const WebString& sessionId, const unsigned char* initData, unsigned initDataLength);
     virtual WebPlugin* createHelperPlugin(const WebString& pluginType, WebFrame*);
     virtual void closeHelperPlugin();
-    virtual void disableAcceleratedCompositing();
+    virtual bool needsWebLayerForVideo() const;
+    virtual void setWebLayer(WebLayer*);
 
     // MediaPlayerPrivateInterface methods:
     virtual void load(const WTF::String& url);
@@ -151,24 +144,13 @@ public:
     virtual WebCore::AudioSourceProvider* audioSourceProvider();
 #endif
 
-#if USE(ACCELERATED_COMPOSITING)
     virtual bool supportsAcceleratedRendering() const;
-
-    // WebVideoFrameProvider methods:
-    virtual void setVideoFrameProviderClient(WebVideoFrameProvider::Client*);
-    virtual WebVideoFrame* getCurrentFrame();
-    virtual void putCurrentFrame(WebVideoFrame*);
-#endif
 
 #if ENABLE(ENCRYPTED_MEDIA)
     virtual WebCore::MediaPlayer::MediaKeyException generateKeyRequest(const String& keySystem, const unsigned char* initData, unsigned initDataLength) OVERRIDE;
     virtual WebCore::MediaPlayer::MediaKeyException addKey(const String& keySystem, const unsigned char* key, unsigned keyLength, const unsigned char* initData, unsigned initDataLength, const String& sessionId) OVERRIDE;
     virtual WebCore::MediaPlayer::MediaKeyException cancelKeyRequest(const String& keySystem, const String& sessionId) OVERRIDE;
 #endif
-
-    // WebStreamTextureClient methods:
-    virtual void didReceiveFrame();
-    virtual void didUpdateMatrix(const float*);
 
 protected:
     WebMediaPlayerClientImpl();
@@ -190,20 +172,15 @@ private:
     bool acceleratedRenderingInUse();
 #endif
 
-    Mutex m_webMediaPlayerMutex; // Guards the m_webMediaPlayer
     WebCore::MediaPlayer* m_mediaPlayer;
     OwnPtr<WebMediaPlayer> m_webMediaPlayer;
-    WebVideoFrame* m_currentVideoFrame;
     WebCore::KURL m_url;
     bool m_delayingLoad;
     WebCore::MediaPlayer::Preload m_preload;
     RefPtr<WebHelperPluginImpl> m_helperPlugin;
-#if USE(ACCELERATED_COMPOSITING)
-    OwnPtr<WebVideoLayer> m_videoLayer;
-    bool m_supportsAcceleratedCompositing;
+    WebLayer* m_videoLayer;
     bool m_opaque;
-    WebVideoFrameProvider::Client* m_videoFrameProviderClient;
-#endif
+    bool m_needsWebLayerForVideo;
     static bool m_isEnabled;
 
 #if ENABLE(WEB_AUDIO)
