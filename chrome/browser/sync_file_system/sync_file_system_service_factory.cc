3213da6c7c198382830f9b2bd11d496088ef8758
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 01:49:53 +0000

    Syncable FileSystem must be registered before DriveFileSyncService is created
    In DriveFileSyncService::DidInitializeMetadata it tries to append pending
    to-be-fetched changes if there're any, but it fails to create
    FileSystemURL for Syncable FileSystem if 'syncable' mount point is not
    registered yet.
    
    BUG=223417
    TEST=manual (Launch sample app in Debug build when to-be-fetched change is in metadata store and see if it doesn't crash)
    
    
    Review URL: https://chromiumcodereview.appspot.com/12769023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190318 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service_factory.cc
+++ chrome/browser/sync_file_system/sync_file_system_service_factory.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/sync_file_system/drive_file_sync_service.h"
 #include "chrome/browser/sync_file_system/sync_file_system_service.h"
+#include "webkit/fileapi/syncable/syncable_file_system_util.h"
 
 namespace sync_file_system {
 
@@ -52,10 +53,16 @@ ProfileKeyedService* SyncFileSystemServiceFactory::BuildServiceInstanceFor(
       new LocalFileSyncService(profile));
 
   scoped_ptr<RemoteFileSyncService> remote_file_service;
-  if (mock_remote_file_service_)
+  if (mock_remote_file_service_) {
     remote_file_service = mock_remote_file_service_.Pass();
-  else
+  } else {
+    // FileSystem needs to be registered before DriveFileSyncService runs
+    // its initialization code.
+    // TODO(kinuko): Clean up RegisterSyncableFileSystem in
+    // local_file_sync_context.cc, which is still there for testing.
+    RegisterSyncableFileSystem(DriveFileSyncService::kServiceName);
     remote_file_service.reset(new DriveFileSyncService(profile));
+  }
 
   if (CommandLine::ForCurrentProcess()->HasSwitch(kDisableLastWriteWin)) {
     remote_file_service->SetConflictResolutionPolicy(
