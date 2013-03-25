96e6ce28498a77c269bb98f4a49a99a713ffd84f
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 05:39:00 +0000

    Eliminate CopyResultFromFindFirstMissingParentDirectory.
    Replace the method with CreateCopyResultCallback.
    
    BUG=180569
    TEST=Ran unit_tests --gtest_filter=CreateDirectoryOperation*
    
    
    Review URL: https://chromiumcodereview.appspot.com/12450027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190341 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system/create_directory_operation_unittest.cc
+++ chrome/browser/chromeos/drive/file_system/create_directory_operation_unittest.cc
@@ -97,16 +97,6 @@ class CreateDirectoryOperationTest
     blocking_task_runner_ = NULL;
   }
 
-  // Copy the result from FindFirstMissingParentDirectory().
-  static void CopyResultFromFindFirstMissingParentDirectory(
-      CreateDirectoryOperation
-          ::FindFirstMissingParentDirectoryResult* out_result,
-      const CreateDirectoryOperation
-          ::FindFirstMissingParentDirectoryResult& result) {
-    DCHECK(out_result);
-    *out_result = result;
-  }
-
   virtual void OnDirectoryChangedByOperation(
       const base::FilePath& directory_path) OVERRIDE {
     // Do nothing.
@@ -154,8 +144,7 @@ TEST_F(CreateDirectoryOperationTest, FindFirstMissingParentDirectory) {
   base::FilePath dir_path(FILE_PATH_LITERAL("drive/New Folder 1"));
   operation()->FindFirstMissingParentDirectory(
       dir_path,
-      base::Bind(&CopyResultFromFindFirstMissingParentDirectory,
-                 &result));
+      google_apis::test_util::CreateCopyResultCallback(&result));
   google_apis::test_util::RunBlockingPoolTask();
   EXPECT_EQ(CreateDirectoryOperation::FIND_FIRST_FOUND_MISSING, result.error);
   EXPECT_EQ(base::FilePath(FILE_PATH_LITERAL("drive/New Folder 1")),
@@ -166,8 +155,7 @@ TEST_F(CreateDirectoryOperationTest, FindFirstMissingParentDirectory) {
   base::FilePath dir_path2(FILE_PATH_LITERAL("drive/Directory 1/New Folder 2"));
   operation()->FindFirstMissingParentDirectory(
       dir_path2,
-      base::Bind(&CopyResultFromFindFirstMissingParentDirectory,
-                 &result));
+      google_apis::test_util::CreateCopyResultCallback(&result));
   google_apis::test_util::RunBlockingPoolTask();
   EXPECT_EQ(CreateDirectoryOperation::FIND_FIRST_FOUND_MISSING, result.error);
   EXPECT_EQ(base::FilePath(FILE_PATH_LITERAL("drive/Directory 1/New Folder 2")),
@@ -179,8 +167,7 @@ TEST_F(CreateDirectoryOperationTest, FindFirstMissingParentDirectory) {
       dir_path2.Append(FILE_PATH_LITERAL("Another Folder"));
   operation()->FindFirstMissingParentDirectory(
       dir_path3,
-      base::Bind(&CopyResultFromFindFirstMissingParentDirectory,
-                 &result));
+      google_apis::test_util::CreateCopyResultCallback(&result));
   google_apis::test_util::RunBlockingPoolTask();
   EXPECT_EQ(CreateDirectoryOperation::FIND_FIRST_FOUND_MISSING, result.error);
   EXPECT_EQ(base::FilePath(FILE_PATH_LITERAL("drive/Directory 1/New Folder 2")),
@@ -190,16 +177,14 @@ TEST_F(CreateDirectoryOperationTest, FindFirstMissingParentDirectory) {
   // Folders on top of an existing file.
   operation()->FindFirstMissingParentDirectory(
       base::FilePath(FILE_PATH_LITERAL("drive/File 1.txt/BadDir")),
-      base::Bind(&CopyResultFromFindFirstMissingParentDirectory,
-                 &result));
+      google_apis::test_util::CreateCopyResultCallback(&result));
   google_apis::test_util::RunBlockingPoolTask();
   EXPECT_EQ(CreateDirectoryOperation::FIND_FIRST_FOUND_INVALID, result.error);
 
   // Existing folder.
   operation()->FindFirstMissingParentDirectory(
       base::FilePath(FILE_PATH_LITERAL("drive/Directory 1")),
-      base::Bind(&CopyResultFromFindFirstMissingParentDirectory,
-                 &result));
+      google_apis::test_util::CreateCopyResultCallback(&result));
   google_apis::test_util::RunBlockingPoolTask();
   EXPECT_EQ(CreateDirectoryOperation::FIND_FIRST_DIRECTORY_ALREADY_PRESENT,
             result.error);
