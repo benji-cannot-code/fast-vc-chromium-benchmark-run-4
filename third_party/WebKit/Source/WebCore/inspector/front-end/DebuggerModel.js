fcbb216e831849a791c64c5b2721dda49d4911d4
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-25 12:38:46 +0000

    Web Inspector: support changing local variables in frontend https://bugs.webkit.org/show_bug.cgi?id=112470
    Patch by Peter Rybin <prybin@chromium.org> on 2013-03-25
    Reviewed by Yury Semikhatsky.
    
    Source/WebCore:
    
    ScopeRef type is added to RemoteObject and setPropertyValue is patched accordingly.
    
    Test: inspector/debugger/debugger-change-variable.html
    
    * inspector/front-end/DebuggerModel.js:
    (WebInspector.DebuggerModel.CallFrame.prototype.get id):
    * inspector/front-end/ObjectPropertiesSection.js:
    (WebInspector.FunctionScopeMainTreeElement.prototype.onpopulate.didGetDetails):
    (WebInspector.FunctionScopeMainTreeElement.prototype.onpopulate):
    * inspector/front-end/RemoteObject.js:
    (WebInspector.RemoteObject):
    (WebInspector.RemoteObject.fromScopePayload):
    (WebInspector.RemoteObject.prototype.):
    (WebInspector.RemoteObject.prototype.setPropertyValue):
    (WebInspector.RemoteObject.prototype.setVariableValueCallback):
    (WebInspector.RemoteObject.prototype._setDeclarativeVariableValue):
    (WebInspector.ScopeRef):
    * inspector/front-end/ScopeChainSidebarPane.js:
    (WebInspector.ScopeChainSidebarPane.prototype.update):
    
    LayoutTests:
    
    * inspector/debugger/debugger-change-variable-expected.txt: Added.
    * inspector/debugger/debugger-change-variable.html: Added.
    * platform/chromium/inspector/debugger/debugger-change-variable-expected.txt: Added.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146759 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/DebuggerModel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/DebuggerModel.js
@@ -707,6 +707,14 @@ WebInspector.DebuggerModel.CallFrame.prototype = {
     },
 
     /**
+     * @return {string}
+     */
+    get id()
+    {
+        return this._payload.callFrameId;
+    },
+
+    /**
      * @return {Array.<DebuggerAgent.Scope>}
      */
     get scopeChain()
