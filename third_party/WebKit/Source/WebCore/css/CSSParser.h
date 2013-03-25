0de8d50917f045665dba8cae94f3045278f43703
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-25 16:28:29 +0000

    Web Inspector: Remove console warnings for *_ prefixed CSS styles https://bugs.webkit.org/show_bug.cgi?id=113175
    Patch by Sergey Ryazanov <serya@chromium.org> on 2013-03-25
    Reviewed by Pavel Feldman.
    
    Source/WebCore:
    
    Added separate error message for syntax CSS error in declaration list when no property detected.
    Added filter for messages starting with '*'.
    
    * css/CSSGrammar.y.in:
    
    LayoutTests:
    
    * inspector/console/console-css-warnings-expected.txt:
    * inspector/console/console-css-warnings.html:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/css/CSSParser.h
+++ third_party/WebKit/Source/WebCore/css/CSSParser.h
@@ -77,6 +77,10 @@ class CSSParser {
 
 public:
     struct Location;
+    enum SyntaxErrorType {
+        PropertyDeclarationError,
+        GeneralSyntaxError
+    };
 
     CSSParser(const CSSParserContext&);
 
@@ -420,7 +424,7 @@ public:
     PassRefPtr<CSSRuleSourceData> popRuleData();
     void resetPropertyRange() { m_propertyRange.start = m_propertyRange.end = UINT_MAX; }
     bool isExtractingSourceData() const { return !!m_currentRuleDataStack; }
-    void syntaxError(Location);
+    void syntaxError(const Location&, SyntaxErrorType = GeneralSyntaxError);
 
     inline int lex(void* yylval) { return (this->*m_lexFunc)(yylval); }
 
