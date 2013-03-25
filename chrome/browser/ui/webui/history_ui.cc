b25a383ba8bff3075479a96e5b2b4a12d18f2843
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-25 21:24:27 +0000

    History: Fix console errors on Android, and add some iOS ifdefs.
    Fix runtime JavaScript errors on chrome://history that are caused by
    some missing resources on Android.
    
    Also add some iOS ifdefs to match the Android ones.
    
    BUG=None
    TEST=Open chrome://history on Android, and verify that there are no
    console errors on the adb debug output.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12769024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190483 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/history_ui.cc
+++ chrome/browser/ui/webui/history_ui.cc
@@ -58,7 +58,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_ANDROID)
 #include "chrome/browser/ui/android/tab_model/tab_model.h"
 #include "chrome/browser/ui/android/tab_model/tab_model_list.h"
-#else
+#endif
+
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
 #include "chrome/browser/ui/webui/ntp/foreign_session_handler.h"
 #include "chrome/browser/ui/webui/ntp/ntp_login_handler.h"
 #endif
@@ -159,7 +161,9 @@ content::WebUIDataSource* CreateHistoryUIHTMLSource(Profile* profile) {
   source->SetUseJsonJSFormatV2();
   source->DisableDenyXFrameOptions();
 
-#if !defined(OS_ANDROID)
+#if defined(OS_ANDROID) || defined(OS_IOS)
+  source->AddBoolean("isManagedProfile", false);
+#else
   source->AddBoolean("isManagedProfile",
       ManagedUserServiceFactory::GetForProfile(profile)->ProfileIsManaged());
 #endif
@@ -379,7 +383,7 @@ void BrowsingHistoryHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback("removeBookmark",
       base::Bind(&BrowsingHistoryHandler::HandleRemoveBookmark,
                  base::Unretained(this)));
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
   web_ui()->RegisterMessageCallback("processManagedUrls",
       base::Bind(&BrowsingHistoryHandler::HandleProcessManagedUrls,
                  base::Unretained(this)));
@@ -590,7 +594,7 @@ void BrowsingHistoryHandler::HandleRemoveVisits(const ListValue* args) {
   }
 }
 
-#if !defined(OS_ANDROID)
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
 void BrowsingHistoryHandler::HandleProcessManagedUrls(const ListValue* args) {
   bool allow = false;
   if (!args->GetBoolean(0, &allow)) {
@@ -704,7 +708,7 @@ void BrowsingHistoryHandler::HandleProcessManagedUrls(const ListValue* args) {
   // should update the page.
   web_ui()->CallJavascriptFunction("updateEntries", results);
 }
-#endif  // defined(OS_ANDROID)
+#endif  // !defined(OS_ANDROID) && !defined(OS_IOS)
 
 void BrowsingHistoryHandler::HandleClearBrowsingData(const ListValue* args) {
 #if defined(OS_ANDROID)
@@ -1092,13 +1096,13 @@ void BrowsingHistoryHandler::Observe(
 HistoryUI::HistoryUI(content::WebUI* web_ui) : WebUIController(web_ui) {
   web_ui->AddMessageHandler(new BrowsingHistoryHandler());
 
-// Android deals with foreign sessions differently.
-#if !defined(OS_ANDROID)
+// On mobile we deal with foreign sessions differently.
+#if !defined(OS_ANDROID) && !defined(OS_IOS)
   if (chrome::search::IsInstantExtendedAPIEnabled()) {
     web_ui->AddMessageHandler(new browser_sync::ForeignSessionHandler());
     web_ui->AddMessageHandler(new NTPLoginHandler());
   }
-#endif  // !defined(OS_ANDROID)
+#endif
 
   // Set up the chrome://history-frame/ source.
   Profile* profile = Profile::FromWebUI(web_ui);
