07a9fb1e537aa0f160c9b5a015028b587ab11381
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 16:12:54 +0000

    Linux: add an option to link pulseaudio directly (DT_NEEDED).
    Useful for Linux distros.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/12634033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190692 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/pulse/audio_manager_pulse.cc
+++ media/audio/pulse/audio_manager_pulse.cc
@@ -17,13 +17,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/audio/linux/audio_manager_linux.h"
 #include "media/audio/pulse/pulse_input.h"
 #include "media/audio/pulse/pulse_output.h"
-#include "media/audio/pulse/pulse_stubs.h"
 #include "media/audio/pulse/pulse_util.h"
 #include "media/base/channel_layout.h"
+
+#if defined(DLOPEN_PULSEAUDIO)
+#include "media/audio/pulse/pulse_stubs.h"
 
 using media_audio_pulse::kModulePulse;
 using media_audio_pulse::InitializeStubs;
 using media_audio_pulse::StubPathMap;
+#endif  // defined(DLOPEN_PULSEAUDIO)
 
 namespace media {
 
@@ -179,6 +182,7 @@ int AudioManagerPulse::GetNativeSampleRate() {
 bool AudioManagerPulse::Init() {
   DCHECK(!input_mainloop_);
 
+#if defined(DLOPEN_PULSEAUDIO)
   StubPathMap paths;
 
   // Check if the pulse library is avialbale.
@@ -187,6 +191,7 @@ bool AudioManagerPulse::Init() {
     DLOG(WARNING) << "Failed on loading the Pulse library and symbols";
     return false;
   }
+#endif  // defined(DLOPEN_PULSEAUDIO)
 
   // Create a mainloop API and connect to the default server.
   // The mainloop is the internal asynchronous API event loop.
