ba49fdf378ba42925eb25ab4d5003a58fce29e67
mathp@chromium.org <mathp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 09:13:47 +0000

    [Instant] Fix in adjusting the Instant suggestion
    Fixing the case where a keystroke would make the default suggestion change from type URL (with inline text) to type SEARCH.
    
    Now using |last_user_text_| to fix the suggestion.
    
    BUG=196694
    
    Review URL: https://chromiumcodereview.appspot.com/12937002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/search_provider.h
+++ chrome/browser/autocomplete/search_provider.h
@@ -65,6 +65,7 @@ class SearchProvider : public AutocompleteProvider,
   // the instant result.
   void FinalizeInstantQuery(const string16& input_text,
                             const InstantSuggestion& suggestion);
+  void ClearInstantSuggestion();
 
   // AutocompleteProvider:
   virtual void Start(const AutocompleteInput& input,
