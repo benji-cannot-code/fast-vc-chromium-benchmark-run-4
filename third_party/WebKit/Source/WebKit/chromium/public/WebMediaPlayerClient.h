0b05a7f6ea1bc66b992e8b3a5ee105718289929f
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-26 03:02:47 +0000

    [chromium] Move ownership of compositor VideoLayer to WebMediaPlayer https://bugs.webkit.org/show_bug.cgi?id=112971
    Reviewed by James Robinson.
    
    Source/Platform:
    
    Defines REMOVE_WEBVIDEOFRAME in related headers to enable this code
    path in chromium.
    
    * chromium/public/WebVideoFrame.h:
    * chromium/public/WebVideoFrameProvider.h:
    
    Source/WebKit/chromium:
    
    This moves ownership of the compositor's video layer over to the
    implementation of WebMediaPlayer, as well as all interaction
    between the media player system and the compositor layer or its
    interfaces such as cc::VideoFrameProvider.
    
    Now, the WebMediaPlayer implementation calls setWebLayer() to
    provide the WebLayer* to the WebMediaPlayerClientImpl so that
    it can be inserted into the GraphicsLayer tree appropriately.
    
    Defines REMOVE_WEBVIDEOFRAME in related headers to enable this code
    path in chromium.
    
    Once chromium removes its use of WebVideoFrame and its related
    classes, we can remove them from WebKit entirely.
    
    * WebKit.gypi:
    * public/WebMediaPlayer.h:
    (WebKit):
    * public/WebMediaPlayerClient.h:
    * public/WebStreamTextureClient.h:
    * src/WebMediaPlayerClientImpl.cpp:
    (WebKit::WebMediaPlayerClientImpl::~WebMediaPlayerClientImpl):
    (WebKit::WebMediaPlayerClientImpl::readyStateChanged):
    (WebKit::WebMediaPlayerClientImpl::repaint):
    (WebKit::WebMediaPlayerClientImpl::setOpaque):
    (WebKit::WebMediaPlayerClientImpl::setWebLayer):
    (WebKit::WebMediaPlayerClientImpl::loadRequested):
    (WebKit::WebMediaPlayerClientImpl::platformLayer):
    (WebKit::WebMediaPlayerClientImpl::paint):
    (WebKit::WebMediaPlayerClientImpl::needsWebLayer):
    (WebKit::WebMediaPlayerClientImpl::supportsAcceleratedRendering):
    (WebKit::WebMediaPlayerClientImpl::acceleratedRenderingInUse):
    (WebKit::WebMediaPlayerClientImpl::create):
    (WebKit::WebMediaPlayerClientImpl::supportsType):
    * src/WebMediaPlayerClientImpl.h:
    (WebKit):
    (WebMediaPlayerClientImpl):
    * tests/WebMediaPlayerClientImplTest.cpp: Removed.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayerClient.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayerClient.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebFrame;
+class WebLayer;
 class WebMediaSource;
 class WebPlugin;
 class WebRequest;
@@ -81,7 +82,8 @@ public:
     // Returns 0 if the plugin could not be instantiated.
     virtual WebPlugin* createHelperPlugin(const WebString& pluginType, WebFrame*) = 0;
     virtual void closeHelperPlugin() = 0;
-    virtual void disableAcceleratedCompositing() = 0;
+    virtual bool needsWebLayerForVideo() const = 0;
+    virtual void setWebLayer(WebLayer*) = 0;
 protected:
     ~WebMediaPlayerClient() { }
 };
