31591e96caee66378d53b259c093f2afb9f82789
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 01:09:51 +0000

    Add display scaling and rotation shortcuts to keyboard overlay.
    Ctrl-Shift-Back -> Scale down UI
    Ctrl-Shift-Foward -> Scale up UI
    Ctrl-Shift-Reload -> Rotate screen
    
    BUG=196817
    
    Review URL: https://codereview.chromium.org/12617019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190538 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/keyboard_overlay_ui.cc
+++ chrome/browser/ui/webui/chromeos/keyboard_overlay_ui.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/webui/chromeos/keyboard_overlay_ui.h"
 
+#include "ash/display/display_manager.h"
+#include "ash/shell.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/command_line.h"
@@ -192,7 +194,9 @@ struct I18nContentToMessage {
   { "keyboardOverlayReopenLastClosedTab",
     IDS_KEYBOARD_OVERLAY_REOPEN_LAST_CLOSED_TAB },
   { "keyboardOverlayReportIssue", IDS_KEYBOARD_OVERLAY_REPORT_ISSUE },
+  { "keyboardOverlayResetScreenZoom", IDS_KEYBOARD_OVERLAY_RESET_SCREEN_ZOOM },
   { "keyboardOverlayResetZoom", IDS_KEYBOARD_OVERLAY_RESET_ZOOM },
+  { "keyboardOverlayRotateScreen", IDS_KEYBOARD_OVERLAY_ROTATE_SCREEN },
   { "keyboardOverlaySave", IDS_KEYBOARD_OVERLAY_SAVE },
   { "keyboardOverlayScreenshotRegion",
     IDS_KEYBOARD_OVERLAY_SCREENSHOT_REGION },
@@ -226,6 +230,8 @@ struct I18nContentToMessage {
   { "keyboardOverlayWordMove", IDS_KEYBOARD_OVERLAY_WORD_MOVE },
   { "keyboardOverlayZoomIn", IDS_KEYBOARD_OVERLAY_ZOOM_IN },
   { "keyboardOverlayZoomOut", IDS_KEYBOARD_OVERLAY_ZOOM_OUT },
+  { "keyboardOverlayZoomScreenIn", IDS_KEYBOARD_OVERLAY_ZOOM_SCREEN_IN },
+  { "keyboardOverlayZoomScreenOut", IDS_KEYBOARD_OVERLAY_ZOOM_SCREEN_OUT },
 };
 
 std::string ModifierKeyToLabel(ModifierKey modifier) {
@@ -247,11 +253,15 @@ content::WebUIDataSource* CreateKeyboardOverlayUIHTMLSource() {
   }
 
   source->AddString("keyboardOverlayLearnMoreURL", UTF8ToUTF16(kLearnMoreURL));
-  const char* has_diamond_key_value =
-      CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kHasChromeOSDiamondKey) ? "true" : "false";
-  source->AddString("keyboardOverlayHasChromeOSDiamondKey",
-                    has_diamond_key_value);
+  source->AddBoolean("keyboardOverlayHasChromeOSDiamondKey",
+                     CommandLine::ForCurrentProcess()->HasSwitch(
+                         switches::kHasChromeOSDiamondKey));
+  ash::Shell* shell = ash::Shell::GetInstance();
+  ash::internal::DisplayManager* display_manager = shell->display_manager();
+  source->AddBoolean("keyboardOverlayIsDisplayRotationEnabled",
+                     display_manager->IsDisplayRotationEnabled());
+  source->AddBoolean("keyboardOverlayIsDisplayUIScalingEnabled",
+                     display_manager->IsDisplayUIScalingEnabled());
   source->SetJsonPath("strings.js");
   source->SetUseJsonJSFormatV2();
   source->AddResourcePath("keyboard_overlay.js", IDR_KEYBOARD_OVERLAY_JS);
