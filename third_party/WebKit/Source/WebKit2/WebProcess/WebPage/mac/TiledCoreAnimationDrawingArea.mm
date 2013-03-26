652f55569ba75a8abc2fa1df58731cb494e7ce35
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-26 21:26:49 +0000

    [wk2] Don't send intrinsic content size updates more than once per runloop https://bugs.webkit.org/show_bug.cgi?id=113258 <rdar://problem/13492038>
    Reviewed by Simon Fraser.
    
    * WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm:
    (WebKit::TiledCoreAnimationDrawingArea::mainFrameContentSizeChanged):
    Only start the zero-delay timer if it's not already active.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146928 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebPage/mac/TiledCoreAnimationDrawingArea.mm
@@ -258,7 +258,8 @@ void TiledCoreAnimationDrawingArea::mainFrameContentSizeChanged(const IntSize&)
     if (m_inUpdateGeometry)
         return;
 
-    m_updateIntrinsicContentSizeTimer.startOneShot(0);
+    if (!m_updateIntrinsicContentSizeTimer.isActive())
+        m_updateIntrinsicContentSizeTimer.startOneShot(0);
 }
 
 void TiledCoreAnimationDrawingArea::updateIntrinsicContentSizeTimerFired(Timer<TiledCoreAnimationDrawingArea>*)
