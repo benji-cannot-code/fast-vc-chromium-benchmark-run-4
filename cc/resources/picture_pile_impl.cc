0adf0e15834eb3f600f10afc065cc4c4b2e60c66
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 07:20:25 +0000

    cc: Changed total_pixels_rasterized to be a return value
    It's a bit awkward calling Raster and passing an output
    parameter that is never used. Instead, it makes sense to
    make it a return value, and simply not capture it if its
    not required.
    
    BUG=223590
    
    
    Review URL: https://chromiumcodereview.appspot.com/12457031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190569 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/picture_pile_impl.cc
+++ cc/resources/picture_pile_impl.cc
@@ -72,11 +72,10 @@ PicturePileImpl* PicturePileImpl::GetCloneForDrawingOnThread(
   return clones_for_drawing_.clones_[thread_index];
 }
 
-void PicturePileImpl::Raster(
+int64 PicturePileImpl::Raster(
     SkCanvas* canvas,
     gfx::Rect canvas_rect,
-    float contents_scale,
-    int64* total_pixels_rasterized) {
+    float contents_scale) {
 
   DCHECK(contents_scale >= min_contents_scale_);
 
@@ -121,6 +120,8 @@ void PicturePileImpl::Raster(
   canvas->clipRect(gfx::RectToSkRect(content_rect),
                    SkRegion::kReplace_Op);
   Region unclipped(content_rect);
+
+  int64 total_pixels_rasterized = 0;
   for (TilingData::Iterator tile_iter(&tiling_, layer_rect);
        tile_iter; ++tile_iter) {
     PictureListMap::iterator map_iter =
@@ -159,7 +160,7 @@ void PicturePileImpl::Raster(
           SkRegion::kDifference_Op);
       unclipped.Subtract(content_clip);
 
-      *total_pixels_rasterized +=
+      total_pixels_rasterized +=
           content_clip.width() * content_clip.height();
     }
   }
@@ -178,6 +179,8 @@ void PicturePileImpl::Raster(
   DCHECK(!unclipped.Contains(content_rect));
 
   canvas->restore();
+
+  return total_pixels_rasterized;
 }
 
 void PicturePileImpl::GatherPixelRefs(
@@ -218,8 +221,7 @@ skia::RefPtr<SkPicture> PicturePileImpl::GetFlattenedPicture() {
       layer_rect.height(),
       SkPicture::kUsePathBoundsForClip_RecordingFlag);
 
-  int64 total_pixels_rasterized = 0;
-  Raster(canvas, layer_rect, 1.0, &total_pixels_rasterized);
+  Raster(canvas, layer_rect, 1.0);
   picture->endRecording();
 
   return picture;
@@ -240,8 +242,7 @@ void PicturePileImpl::AnalyzeInRect(const gfx::Rect& content_rect,
   skia::AnalysisDevice device(empty_bitmap);
   skia::AnalysisCanvas canvas(&device);
 
-  int64 total_pixels_rasterized = 0;
-  Raster(&canvas, content_rect, contents_scale, &total_pixels_rasterized);
+  Raster(&canvas, content_rect, contents_scale);
 
   analysis->is_transparent = canvas.isTransparent();
   analysis->is_solid_color = canvas.getColorIfSolid(&analysis->solid_color);
