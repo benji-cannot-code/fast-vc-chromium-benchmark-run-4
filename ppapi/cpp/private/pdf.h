5a6013560dc8dbadda035a86ee5cbb3998231fce
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 22:15:51 +0000

    Change PPB_PDF to use PP_BrowserFont_Trusted_Description instead of PP_FontDescription_Dev.
    This also changes the ppb_pdf_thunk (which is only used for the out of process
    case) to call directly into PPB_Flash_Font_File for font-related functions.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/12893016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190777 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/cpp/private/pdf.h
+++ ppapi/cpp/private/pdf.h
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ppapi/c/private/ppb_pdf.h"
 
+struct PP_BrowserFont_Trusted_Description;
+
 namespace pp {
 
 class ImageData;
@@ -25,9 +27,15 @@ class PDF {
                                 PP_ResourceString string_id);
   static ImageData GetResourceImage(const InstanceHandle& instance,
                                     PP_ResourceImage image_id);
+  // TODO(raymes): Remove this version when the PDF code is changed to use
+  // PP_BrowserFont_Trusted_Description.
   static PP_Resource GetFontFileWithFallback(
       const InstanceHandle& instance,
       const PP_FontDescription_Dev* description,
+      PP_PrivateFontCharset charset);
+  static PP_Resource GetFontFileWithFallback(
+      const InstanceHandle& instance,
+      const PP_BrowserFont_Trusted_Description* description,
       PP_PrivateFontCharset charset);
   static bool GetFontTableForPrivateFontFile(PP_Resource font_file,
                                              uint32_t table,
