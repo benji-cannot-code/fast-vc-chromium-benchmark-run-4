5d2933b6823c0ad742458e44f5c752871c19631f
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 05:24:22 +0000

    Revert 190531 "Get OAuth2TokenService working on Android."
    > Get OAuth2TokenService working on Android.
    > 
    > In order to get OAuth tokens on Android, we have to call out to Java. This CL
    > makes it possible. It's not the ideal solution but works for now.
    > 
    > BUG= 222271
    > 
    > Review URL: https://codereview.chromium.org/12880014
    
    TBR=dubroy@chromium.org
    Review URL: https://codereview.chromium.org/12782018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190558 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/android/java/src/org/chromium/sync/signin/AccountManagerHelper.java
+++ sync/android/java/src/org/chromium/sync/signin/AccountManagerHelper.java
@@ -307,11 +307,4 @@ public class AccountManagerHelper {
         getAuthTokenAsynchronously(
             null, account, authTokenType, callback, numTries, errorEncountered, null);
     }
-
-    /**
-     * Removes an auth token from the AccountManager's cache.
-     */
-    public void invalidateAuthToken(String accountType, String authToken) {
-        mAccountManager.invalidateAuthToken(accountType, authToken);
-    }
 }
