3c707a96ef3d0b36bd3552a40fdbb182d0251baa
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 06:09:37 +0000

    Fix data overwriting issue on ReadFromDownloadData.
    There is an issue which overwrites the download_data_ if the pending
    data is not yet consumed. This CL fixes it.
    
    BUG=223603
    TEST=Ran unit_tests and tested manually.
    
    Review URL: https://codereview.chromium.org/13065007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190561 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_url_request_job.h
+++ chrome/browser/chromeos/drive/drive_url_request_job.h
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "base/string_piece.h"
 #include "chrome/browser/chromeos/drive/drive_file_error.h"
@@ -36,6 +37,10 @@ class DriveFileSystemInterface;
 // requests for drive resources and DriveFileSytem.  It exposes content URLs
 // formatted as drive://<resource-id>.
 // The methods should be run on IO thread.
+// This class seems to have some timing issue and to handle edge cases somehow
+// wrongly. Also, probably because of the requirement change, the code path
+// is getting complicated.
+// TODO(hidehiko): Clean the code up and handles edge cases correctly.
 class DriveURLRequestJob : public net::URLRequestJob {
  public:
 
@@ -132,8 +137,7 @@ class DriveURLRequestJob : public net::URLRequestJob {
   bool streaming_download_;
   scoped_refptr<net::IOBuffer> read_buf_;
   base::StringPiece read_buf_remaining_;
-  std::string download_buf_;
-  base::StringPiece download_buf_remaining_;
+  ScopedVector<std::string> pending_downloaded_data_;
 
   // This should remain the last member so it'll be destroyed first and
   // invalidate its weak pointers before other members are destroyed.
