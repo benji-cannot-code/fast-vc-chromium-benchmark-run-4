2904fb49fa1c209d14abad65345a2e2c18014105
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-26 21:46:00 +0000

    JSC_enableProfiler=true should also cause JSGlobalData to save the profiler output somewhere https://bugs.webkit.org/show_bug.cgi?id=113144
    Reviewed by Geoffrey Garen.
            
    Forgot to include Geoff's requested change in the original commit.
    
    * profiler/ProfilerDatabase.cpp:
    (Profiler):
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/JavaScriptCore/profiler/ProfilerDatabase.cpp
+++ third_party/WebKit/Source/JavaScriptCore/profiler/ProfilerDatabase.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace JSC { namespace Profiler {
 
 static volatile int databaseCounter;
-static SpinLock registrationLock;
+static SpinLock registrationLock = SPINLOCK_INITIALIZER;
 static int didRegisterAtExit;
 static Database* firstDatabase;
 
