25688ee0f7e3a47992c661dd4b699aa5d1b75196
sbasi@google.com <sbasi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 08:26:28 +0000

    Move perf data into its own Autotest Dep.
    We are pushing perf data out of the telemetry dep as client side
    telemetry tests don't need these files and they are large.
    
    BUG=chromium:223237
    TEST=built-locally.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12865014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190595 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/chromeos/autotest/files/client/deps/perf_data_dep/perf_data_dep.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+#!/usr/bin/python
+
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Install the perf_data_dep dependency.
+
+This is done by the Chrome ebuild so this in fact has nothing to do.
+"""
+
+import os
+from autotest_lib.client.bin import utils
+
+version = 1
+
+def setup(top_dir):
+    return
+
+
+pwd = os.getcwd()
+utils.update_version(pwd + '/src', False, version, setup, None)
