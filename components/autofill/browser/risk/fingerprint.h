88094540e16e041abce80b2ee75c8c9929c7f867
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 12:47:18 +0000

    [Autofill:Risk] Add browser feature to the Risk protobuf.
    This allows the risk server to distinguish Autocheckout vs. RequestAutocomplete.
    
    BUG=222426
    
    
    Review URL: https://chromiumcodereview.appspot.com/12976002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190651 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/risk/fingerprint.h
+++ components/autofill/browser/risk/fingerprint.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback_forward.h"
 #include "base/memory/scoped_ptr.h"
+#include "components/autofill/browser/autofill_manager_delegate.h"
 
 class PrefService;
 
@@ -52,6 +53,7 @@ void GetFingerprint(
     const std::string& charset,
     const std::string& accept_languages,
     const base::Time& install_time,
+    DialogType dialog_type,
     const base::Callback<void(scoped_ptr<Fingerprint>)>& callback);
 
 // Exposed for testing:
@@ -66,6 +68,7 @@ void GetFingerprintInternal(
     const std::string& charset,
     const std::string& accept_languages,
     const base::Time& install_time,
+    DialogType dialog_type,
     const base::Callback<void(scoped_ptr<Fingerprint>)>& callback);
 
 }  // namespace internal
