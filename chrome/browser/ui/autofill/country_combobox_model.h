8d4fbeaf40985972d6beccf8a816bd9e19b6d77e
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 21:11:53 +0000

    add separators to ui::ComboboxModel, implement on views.
    also use a separator in the country combobox model.
    
    BUG=179496
    
    
    Review URL: https://chromiumcodereview.appspot.com/12521023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190766 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/country_combobox_model.h
+++ chrome/browser/ui/autofill/country_combobox_model.h
@@ -26,6 +26,7 @@ class CountryComboboxModel : public ui::ComboboxModel {
   // ui::Combobox implementation:
   virtual int GetItemCount() const OVERRIDE;
   virtual string16 GetItemAt(int index) OVERRIDE;
+  virtual bool IsItemSeparatorAt(int index) OVERRIDE;
 
   const std::vector<AutofillCountry*>& countries() const {
     return countries_.get();
