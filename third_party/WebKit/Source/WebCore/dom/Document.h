0855e7c3f81a40475c00ab3715bbb401c774dd8a
rniwa@webkit.org <rniwa@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-26 22:03:16 +0000

    Heap-use-after-free regression https://bugs.webkit.org/show_bug.cgi?id=113337
    Reviewed by Abhishek Arya and Alexey Proskuryakov.
    
    Source/WebCore: 
    
    Use RefPtr instead of raw pointer in m_associatedFormControls.
    
    * dom/Document.cpp:
    (WebCore::Document::didAssociateFormControlsTimerFired):
    * dom/Document.h:
    (Document):
    * loader/EmptyClients.h:
    (WebCore::EmptyChromeClient::didAssociateFormControls):
    * page/ChromeClient.h:
    (WebCore::ChromeClient::didAssociateFormControls):
    
    Source/WebKit/chromium: 
    
    * src/ChromeClientImpl.cpp:
    (WebKit::ChromeClientImpl::didAssociateFormControls):
    * src/ChromeClientImpl.h:
    (ChromeClientImpl):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146935 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/Document.h
+++ third_party/WebKit/Source/WebCore/dom/Document.h
@@ -1600,7 +1600,7 @@ private:
 #endif
 
     Timer<Document> m_didAssociateFormControlsTimer;
-    HashSet<Element*> m_associatedFormControls;
+    HashSet<RefPtr<Element> > m_associatedFormControls;
 
 };
 
