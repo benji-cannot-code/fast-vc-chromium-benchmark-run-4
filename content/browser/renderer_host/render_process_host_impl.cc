57cfad07201bb74621ba6409321e4e224deb1d09
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-26 19:47:21 +0000

    Add vsync notification command line switch
    This switch will make the renderer subscribe to a vsync notification from the browser instead of using a timer approximate the vsync pulse.
    
    BUG=149227
    
    Review URL: https://chromiumcodereview.appspot.com/11414308
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190740 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -828,6 +828,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableGpuBenchmarking,
     switches::kEnableMemoryBenchmarking,
     switches::kEnableLogging,
+    switches::kEnableVsyncNotification,
     switches::kDisableMediaSource,
     switches::kDisableRendererSideMixing,
     switches::kEnableStrictSiteIsolation,
