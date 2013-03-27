de19b2a7bbbfe69b3fc2c9e9492fbc8cdaf5a73d
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 18:32:36 +0000

    Revert 190980 "Alternate NTP Mac: Push down overlay in presentat..."
    > Alternate NTP Mac: Push down overlay in presentation mode
    > 
    > In presentation mode the browser toolbar and bookmark bar are in a
    > special floating view. This view obscures the instant overlay since
    > the overlay is at the top of the screen.
    > 
    > This CL pushes the overlay down so that it's not obscured when the
    > floating toolbar view is shown.
    > 
    > Test build: https://docs.google.com/file/d/0B0Odde3V7EhWTmtMTXBJYmtGS3M/edit?usp=sharing
    > 
    > BUG=167185
    > TEST=
    >   Launched Chrome with instant extended enabled.
    >   Enter presentation mode and type in the omnibox.
    >   Verify that the omnibox popup is below the floating toolbar view.
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12594015
    
    TBR=sail@chromium.org
    Review URL: https://codereview.chromium.org/13042017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190984 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tab_contents/overlayable_contents_controller.mm
+++ chrome/browser/ui/cocoa/tab_contents/overlayable_contents_controller.mm
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 @synthesize drawDropShadow = drawDropShadow_;
 @synthesize activeContainerOffset = activeContainerOffset_;
-@synthesize overlayContentsOffset = overlayContentsOffset_;
 
 - (id)initWithBrowser:(Browser*)browser
      windowController:(BrowserWindowController*)windowController {
@@ -147,14 +146,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   [self layoutViews];
 }
 
-- (void)setOverlayContentsOffset:(CGFloat)overlayContentsOffset {
-  if (overlayContentsOffset_ == overlayContentsOffset)
-    return;
-
-  overlayContentsOffset_ = overlayContentsOffset;
-  [self layoutViews];
-}
-
 - (void)viewDidResize:(NSNotification*)note {
   [self layoutViews];
 }
@@ -175,8 +166,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   if (overlayContents_) {
     NSRect overlayFrame = bounds;
     overlayFrame.size.height = [self overlayHeightInPixels];
-    overlayFrame.origin.y = NSMinY([topSeparatorView_ frame]) -
-                            NSHeight(overlayFrame) - overlayContentsOffset_;
+    overlayFrame.origin.y =
+        NSMinY([topSeparatorView_ frame]) - NSHeight(overlayFrame);
     [overlayContents_->GetView()->GetNativeView() setFrame:overlayFrame];
 
     if (dropShadowView_) {
@@ -202,8 +193,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 
 - (CGFloat)overlayHeightInPixels {
-  CGFloat height = NSHeight([[self view] bounds]) -
-                   NSHeight([topSeparatorView_ frame]) - overlayContentsOffset_;
+  CGFloat height =
+      NSHeight([[self view] bounds]) - NSHeight([topSeparatorView_ frame]);
   switch (overlayHeightUnits_) {
     case INSTANT_SIZE_PERCENT:
       return std::min(height, (height * overlayHeight_) / 100);
