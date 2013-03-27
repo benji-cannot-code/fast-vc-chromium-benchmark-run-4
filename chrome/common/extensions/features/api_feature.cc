eb4e33444cbdee8ccb426a435f5c2c52beee2632
cduvall@chromium.org <cduvall@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 06:53:45 +0000

    Implement API features for the Extension API feature system
    Now features can be declared in the _api_features.json file, and dependencies
    of the form "api:<api_name>" will refer to these features.
    
    This patch also prevents schemas for APIs using the feature system from being
    loaded when calling ExtensionAPI::IsAvailable().
    
    TBR=ben@chromium.org
    BUG=55316, 120070
    
    
    Review URL: https://chromiumcodereview.appspot.com/12846011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190836 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/features/api_feature.cc
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/common/extensions/features/api_feature.h"
+
+namespace extensions {
+
+APIFeature::APIFeature() {
+}
+
+APIFeature::~APIFeature() {
+}
+
+std::string APIFeature::Parse(const DictionaryValue* value) {
+  std::string error = SimpleFeature::Parse(value);
+  if (!error.empty())
+    return error;
+
+  if (GetContexts()->empty())
+    return name() + ": API features must specify at least one context.";
+
+  return "";
+}
+
+}  // namespace
--- chrome/common/extensions/features/permission_feature.h
+++ chrome/common/extensions/features/permission_feature.h
@@ -20,6 +20,8 @@ class PermissionFeature : public SimpleFeature {
       Feature::Context context,
       const GURL& url,
       Feature::Platform platform) const OVERRIDE;
+
+  virtual std::string Parse(const DictionaryValue* value) OVERRIDE;
 };
 
 }  // extensions
