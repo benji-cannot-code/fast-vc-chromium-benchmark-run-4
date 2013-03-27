cb1d56ca5ba282d4dc2a9188accb4cca55022f72
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 09:07:05 +0000

    Remove unused include to WebTextCaseSensitivity.h
    This include was used as part of using WebRegularExpression, which went
    away in r190763
    
    TBR=darin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12939028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190880 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/plugins/plugin_placeholder.cc
+++ chrome/renderer/plugins/plugin_placeholder.cc
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebMenuItemInfo.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebPluginContainer.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebScriptSource.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebTextCaseSensitivity.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "third_party/re2/re2/re2.h"
 #include "ui/base/l10n/l10n_util.h"
