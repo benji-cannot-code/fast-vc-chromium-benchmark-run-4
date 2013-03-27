474f5a0471e97ca9d076ccae9b1cc00ce0b2f160
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 10:32:00 +0000

    Add DriveFileStreamReader with empty implementation.
    The implementation will get a bit complicated. To make the progress step by
    step, this just adds an empty implementation class of DrvieFileStreamReader.
    The new class will be used to implement stream reading of the file contents
    from network, and DriveProtocolHandler.
    
    BUG=127129
    TEST=Ran unit_tests
    
    
    Review URL: https://chromiumcodereview.appspot.com/12496022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190904 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/drive/drive_file_stream_reader.cc
@@ -0,0 +1,32 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/chromeos/drive/drive_file_stream_reader.h"
+
+#include "base/logging.h"
+
+namespace drive {
+
+DriveFileStreamReader::DriveFileStreamReader() {
+}
+
+DriveFileStreamReader::~DriveFileStreamReader() {
+}
+
+int DriveFileStreamReader::Read(net::IOBuffer* buf, int buf_len,
+                                const net::CompletionCallback& callback) {
+  // TODO(hidehiko): Implement this.
+  NOTIMPLEMENTED();
+  return 0;
+}
+
+int DriveFileStreamReader::GetLength(
+    const net::Int64CompletionCallback& callback) {
+  // TODO(hidehiko): Implement this.
+  NOTIMPLEMENTED();
+  return 0;
+}
+
+}  // namespace drive
--- chrome/chrome_browser_chromeos.gypi
+++ chrome/chrome_browser_chromeos.gypi
@@ -220,6 +220,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/chromeos/drive/change_list_processor.h',
         'browser/chromeos/drive/drive_file_error.cc',
         'browser/chromeos/drive/drive_file_error.h',
+        'browser/chromeos/drive/drive_file_stream_reader.cc',
+        'browser/chromeos/drive/drive_file_stream_reader.h',
         'browser/chromeos/drive/drive_file_system.cc',
         'browser/chromeos/drive/drive_file_system.h',
         'browser/chromeos/drive/drive_file_system_interface.h',
