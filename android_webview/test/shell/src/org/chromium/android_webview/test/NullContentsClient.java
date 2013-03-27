dace93e5ac2053126d6b41944d09e6471db919d5
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 06:56:12 +0000

    [Android_WebView] Implement WebChromeClient.getDefaultVideoPoster.
    Intercept the defaultVideoPoster request and return the poster from AwContents.getDefaultVideoPoster();
    
    BUG=http://b/6294871
    
    
    Review URL: https://chromiumcodereview.appspot.com/12702015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190839 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/test/shell/src/org/chromium/android_webview/test/NullContentsClient.java
+++ android_webview/test/shell/src/org/chromium/android_webview/test/NullContentsClient.java
@@ -5,7 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.android_webview.test;
 
-import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Picture;
 import android.net.http.SslError;
@@ -173,6 +172,11 @@ public class NullContentsClient extends AwContentsClient {
 
     @Override
     protected View getVideoLoadingProgressView() {
+        return null;
+    }
+
+    @Override
+    public Bitmap getDefaultVideoPoster() {
         return null;
     }
 }
