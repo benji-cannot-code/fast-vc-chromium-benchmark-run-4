62dca10f703b20471af13c01fbaa7b716830f635
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-27 00:09:16 +0000

    Fix compile for OS(WIN) != PLATFORM(WIN) https://bugs.webkit.org/show_bug.cgi?id=113356
    Reviewed by Tim Horton.
    
    * wtf/ProcessID.h:
    (WTF::getCurrentProcessID):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/ProcessID.h
+++ third_party/WebKit/Source/WTF/wtf/ProcessID.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 #endif
 
-#if PLATFORM(WIN)
+#if OS(WIN)
 #include <windows.h>
 #endif
 
@@ -41,7 +41,7 @@ namespace WTF {
 
 inline int getCurrentProcessID()
 {
-#if PLATFORM(WIN)
+#if OS(WIN)
     return GetCurrentProcessId();
 #else
     return getpid();
