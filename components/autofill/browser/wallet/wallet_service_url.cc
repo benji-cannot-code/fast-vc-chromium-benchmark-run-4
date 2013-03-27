908af2e7daf18ae3b5ae4cc44031486f33199454
ahutter@chromium.org <ahutter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 08:44:04 +0000

    Setting up Finch trial for using production Wallet service.
    BUG=223293
    
    Review URL: https://chromiumcodereview.appspot.com/12869014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@190864 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/wallet/wallet_service_url.cc
+++ components/autofill/browser/wallet/wallet_service_url.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/command_line.h"
+#include "base/metrics/field_trial.h"
 #include "components/autofill/common/autofill_switches.h"
 #include "google_apis/gaia/gaia_urls.h"
 #include "googleurl/src/gurl.h"
@@ -25,13 +26,19 @@ const char kSandboxWalletServiceUrl[] =
 const char kSandboxWalletSecureServiceUrl[] =
     "https://wallet-web.sandbox.google.com/";
 
+bool IsWalletProductionEnabled() {
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+  return command_line.HasSwitch(switches::kWalletServiceUseProd) ||
+         base::FieldTrialList::FindFullName("WalletProductionService") == "Yes";
+}
+
 GURL GetWalletHostUrl() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   std::string wallet_service_hostname =
       command_line.GetSwitchValueASCII(switches::kWalletServiceUrl);
   if (!wallet_service_hostname.empty())
     return GURL(wallet_service_hostname);
-  if (command_line.HasSwitch(switches::kWalletServiceUseProd))
+  if (IsWalletProductionEnabled())
     return GURL(kProdWalletServiceUrl);
   return GURL(kSandboxWalletServiceUrl);
 }
@@ -50,7 +57,7 @@ GURL GetBaseSecureUrl() {
       command_line.GetSwitchValueASCII(switches::kWalletSecureServiceUrl);
   if (!wallet_secure_url.empty())
     return GURL(wallet_secure_url);
-  if (command_line.HasSwitch(switches::kWalletServiceUseProd))
+  if (IsWalletProductionEnabled())
     return GURL(kProdWalletServiceUrl);
   return GURL(kSandboxWalletSecureServiceUrl);
 }
@@ -91,7 +98,7 @@ GURL GetPassiveAuthUrl() {
 GURL GetEncryptionUrl() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   // TODO(ahutter): Stop checking these switches once we switch over to prod.
-  if (command_line.HasSwitch(switches::kWalletServiceUseProd) ||
+  if (IsWalletProductionEnabled() ||
       command_line.HasSwitch(switches::kWalletServiceUrl)) {
     return GetWalletHostUrl().Resolve(
         "online-secure/temporarydata/cvv?s7e=cvv");
@@ -104,7 +111,7 @@ GURL GetEncryptionUrl() {
 GURL GetEscrowUrl() {
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
   // TODO(ahutter): Stop checking these switches once we switch over to prod.
-  if (command_line.HasSwitch(switches::kWalletServiceUseProd) ||
+  if (IsWalletProductionEnabled() ||
       command_line.HasSwitch(switches::kWalletServiceUrl)) {
     return GetBaseSecureUrl().Resolve("dehEfe?s7e=cardNumber%3Bcvv");
   } else {
