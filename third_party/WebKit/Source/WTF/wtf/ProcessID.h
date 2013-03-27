3c5f8e0c840699723a0f89585e7bc201a62ba9cb
jamesr@google.com <jamesr@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-27 00:15:19 +0000

    Compile fix - OS(WINDOWS), not OS(WIN).
    * wtf/ProcessID.h:
    (WTF::getCurrentProcessID):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@146951 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/ProcessID.h
+++ third_party/WebKit/Source/WTF/wtf/ProcessID.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unistd.h>
 #endif
 
-#if OS(WIN)
+#if OS(WINDOWS)
 #include <windows.h>
 #endif
 
@@ -41,7 +41,7 @@ namespace WTF {
 
 inline int getCurrentProcessID()
 {
-#if OS(WIN)
+#if OS(WINDOWS)
     return GetCurrentProcessId();
 #else
     return getpid();
