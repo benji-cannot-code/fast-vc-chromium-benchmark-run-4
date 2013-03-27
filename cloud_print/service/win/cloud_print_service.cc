ee659320b0b11a985e012721ed475a60973ca39d
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-27 22:19:49 +0000

    Remove autorun values if it points to the same user data as service.
    BUG=224219
    
    
    Review URL: https://chromiumcodereview.appspot.com/13042016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191050 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/service/win/cloud_print_service.cc
+++ cloud_print/service/win/cloud_print_service.cc
@@ -183,6 +183,7 @@ class CloudPrintServiceModule
 
     user_data_dir_switch_ =
         command_line.GetSwitchValuePath(switches::kUserDataDir);
+    file_util::AbsolutePath(&user_data_dir_switch_);
     if (command_line.HasSwitch(kStopSwitch))
       return controller_->StopService();
 
