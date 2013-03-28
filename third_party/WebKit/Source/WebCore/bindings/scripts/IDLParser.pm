bad4061e75358a25a1bf5e3bff4d15bfc69e8d9b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-28 17:27:01 +0000

    Move preprocessing of idl to pre-pass for gyp https://bugs.webkit.org/show_bug.cgi?id=113448
    Patch by Scott Graham <scottmg@chromium.org> on 2013-03-28
    Reviewed by Tony Chang.
    
    Moves the preprocessor invocations when parsing IDL files to a
    pre-pass to avoid reinvocations when IDLs are referenced from others.
    This reduces the number of invocations of the preprocessor from 20071
    to 633, which reduces the runtime from 12m30 to 1m (on a Windows box).
    
    No new tests, this is a build time improvement, and shouldn't have any
    visible effect.
    
    * WebCore.gyp/WebCore.gyp:
    * bindings/scripts/CodeGenerator.pm:
    (ParseInterface):
    * bindings/scripts/IDLParser.pm:
    (Parse):
    * bindings/scripts/generate-bindings.pl:
    * bindings/scripts/generate-preprocessed-idls.pl: Added.
    * bindings/scripts/preprocessor.pm:
    (applyPreprocessor):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147130 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/IDLParser.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/IDLParser.pm
@@ -177,10 +177,11 @@ sub Parse
     my $fileName = shift;
     my $defines = shift;
     my $preprocessor = shift;
+    my $outputDirectory = shift;
 
     my @definitions = ();
 
-    my @lines = applyPreprocessor($fileName, $defines, $preprocessor);
+    my @lines = applyPreprocessor($fileName, $defines, $preprocessor, $outputDirectory);
     $self->{Line} = $lines[0];
     $self->{DocumentContent} = join(' ', @lines);
 
