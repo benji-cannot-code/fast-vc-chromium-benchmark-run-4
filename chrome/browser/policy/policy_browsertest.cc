d8c2db243c40b81b666a7b906152135b56687626
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 22:58:10 +0000

    Revert 189337 "Refactor MediaStreamDevicesController:"
    It broke Pepper video capture.
    
    BUG=224378
    
    > Refactor MediaStreamDevicesController:
    > Handle microphone and camera stream access requests separately. This allows to distinguish if which stream (mic and or cam) was actually accessed.
    > 
    > BUG=172780
    > TBR=sky@chromium.org
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12831002
    
    TBR=markusheintz@chromium.org
    Review URL: https://codereview.chromium.org/13236005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191228 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/policy_browsertest.cc
+++ chrome/browser/policy/policy_browsertest.cc
@@ -1980,7 +1980,7 @@ class MediaStreamDevicesControllerBrowserTest
         content_settings,
         request,
         base::Bind(&MediaStreamDevicesControllerBrowserTest::Accept, this));
-    controller.ProcessRequest();
+    controller.DismissInfoBarAndTakeActionOnSettings();
 
     MessageLoop::current()->QuitWhenIdle();
   }
@@ -1996,7 +1996,7 @@ class MediaStreamDevicesControllerBrowserTest
     MediaStreamDevicesController controller(
         browser()->profile(), content_settings, request,
         base::Bind(&MediaStreamDevicesControllerBrowserTest::Accept, this));
-    controller.ProcessRequest();
+    controller.DismissInfoBarAndTakeActionOnSettings();
 
     MessageLoop::current()->QuitWhenIdle();
   }
@@ -2035,7 +2035,6 @@ IN_PROC_BROWSER_TEST_P(MediaStreamDevicesControllerBrowserTest,
       content::MEDIA_DEVICE_VIDEO_CAPTURE, "fake_dev", "Fake Video Device");
   video_devices.push_back(fake_video_device);
 
-LOG(ERROR) << " *** Policy test";
   PolicyMap policies;
   policies.Set(key::kVideoCaptureAllowed, POLICY_LEVEL_MANDATORY,
                POLICY_SCOPE_USER,
