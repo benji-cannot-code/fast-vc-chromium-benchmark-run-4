515f5e0f1ea9316b97867e564aa128054da4911d
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-28 19:59:07 +0000

    [GTK][AC] Use transform property of ClutterActor https://bugs.webkit.org/show_bug.cgi?id=113317
    Patch by ChangSeok Oh <changseok.oh@collabora.com> on 2013-03-28
    Reviewed by Gustavo Noronha Silva.
    
    ClutterActor has a transform property to set transformation matrix directly
    since version 1.12. So we don't need to keep and use the matrix property of GraphicsLayerActor.
    
    No new tests because of no functionality change.
    
    * platform/graphics/clutter/GraphicsLayerActor.cpp:
    (_GraphicsLayerActorPrivate):
    (graphicsLayerActorDispose):
    (graphicsLayerActorApplyTransform):
    * platform/graphics/clutter/GraphicsLayerActor.h:
    * platform/graphics/clutter/GraphicsLayerClutter.cpp:
    (WebCore::GraphicsLayerClutter::updateTransform):
    * platform/graphics/clutter/PlatformClutterAnimation.cpp:
    (WebCore::PlatformClutterAnimation::addTransformTransition):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147145 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/clutter/GraphicsLayerActor.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/clutter/GraphicsLayerActor.h
@@ -82,7 +82,6 @@ void graphicsLayerActorRemoveAll(GraphicsLayerActor*);
 cairo_surface_t* graphicsLayerActorGetSurface(GraphicsLayerActor*);
 void graphicsLayerActorSetSurface(GraphicsLayerActor*, cairo_surface_t*);
 void graphicsLayerActorInvalidateRectangle(GraphicsLayerActor*, const WebCore::FloatRect&);
-void graphicsLayerActorSetTransform(GraphicsLayerActor*, const CoglMatrix*);
 void graphicsLayerActorSetAnchorPoint(GraphicsLayerActor*, float, float, float);
 void graphicsLayerActorGetAnchorPoint(GraphicsLayerActor*, float*, float*, float*);
 void graphicsLayerActorSetScrollPosition(GraphicsLayerActor*, float, float); 
