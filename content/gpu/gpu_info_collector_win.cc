19b700ab9293bedde74e0195c85f11026923cfe3
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 00:48:41 +0000

    Revert 190448 "Revert 189680 "Fix registry key reading bug""
    Turns out this is ok, we want this in m27.
    
    
    > Revert 189680 "Fix registry key reading bug"
    > 
    > caused major hangs on canary. will reland when there's a fix for crbug.com/181665
    > 
    > > Fix registry key reading bug
    > > 
    > > The current code succeeds even if the keys are not in the machine
    > > therefore dCute is detected and the videocard capabilities are
    > > downgraded
    > > 
    > > BUG=181665
    > > TEST=see bug
    > > Review URL: https://codereview.chromium.org/12534013
    > 
    > TBR=cpu@chromium.org
    > Review URL: https://codereview.chromium.org/12704020
    
    TBR=karen@chromium.org
    Review URL: https://codereview.chromium.org/12906015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191079 0039d316-1c4b-4281-b951-d872f2087c98

--- content/gpu/gpu_info_collector_win.cc
+++ content/gpu/gpu_info_collector_win.cc
@@ -190,15 +190,13 @@ Version DisplayLinkVersion() {
 bool IsLenovoDCuteInstalled() {
   base::win::RegKey key;
 
-  if (FAILED(key.Open(
-      HKEY_LOCAL_MACHINE, L"SOFTWARE", KEY_READ | KEY_WOW64_64KEY))) {
-    return false;
-  }
-
-  if (FAILED(key.OpenKey(L"Lenovo", KEY_READ | KEY_WOW64_64KEY)))
+  if (key.Open(HKEY_LOCAL_MACHINE, L"SOFTWARE", KEY_READ | KEY_WOW64_64KEY))
     return false;
 
-  if (FAILED(key.OpenKey(L"Lenovo dCute", KEY_READ | KEY_WOW64_64KEY)))
+  if (key.OpenKey(L"Lenovo", KEY_READ | KEY_WOW64_64KEY))
+    return false;
+
+  if (key.OpenKey(L"Lenovo dCute", KEY_READ | KEY_WOW64_64KEY))
     return false;
 
   return true;
@@ -376,7 +374,7 @@ namespace gpu_info_collector {
 
 #if !defined(GOOGLE_CHROME_BUILD)
 AMDVideoCardType GetAMDVideocardType() {
-  return UNKNOWN;
+  return STANDALONE;
 }
 #else
 // This function has a real implementation for official builds that can
