ff86079812140de93c22e4992aa568e8ce82b1f4
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 07:19:10 +0000

    cc: Add command line switches for impl-side painting constants.
    This adds necessary command line switches for adjusting
    the amount of prepainting allowed with impl-side painting.
    
    BUG=178317
    
    
    Review URL: https://chromiumcodereview.appspot.com/12594033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191112 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -916,6 +916,8 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     cc::switches::kEnableTopControlsPositionCalculation,
     cc::switches::kLowResolutionContentsScaleFactor,
     cc::switches::kNumRasterThreads,
+    cc::switches::kMaxPrepaintTileDistance,
+    cc::switches::kMaxTilesForInterestArea,
     cc::switches::kShowCompositedLayerBorders,
     cc::switches::kShowCompositedLayerTree,
     cc::switches::kShowFPSCounter,
