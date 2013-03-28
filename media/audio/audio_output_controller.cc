432f5f9332f09acb6ef033d697604bcf1ac46583
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 07:39:41 +0000

    Re-land silence detection for audio output UI favicon indicator with crash fixes.
    Consists of these two changes, which have been reviewed, and should land together:
    
    1. Rebased r189095: Silence detection for audio output UI favicon indicator.  https://codereview.chromium.org/12426002/
    2. Close gap (pun intended) causing duplicate deletes in AudioRendererHost.  https://codereview.chromium.org/13008004/
    
    BUG=178343,178934,222593,222597
    TBR=dalecurtis,scherkus,cpu,avi
    
    
    Review URL: https://chromiumcodereview.appspot.com/12710015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191121 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/audio_output_controller.cc
+++ media/audio/audio_output_controller.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/time.h"
 #include "build/build_config.h"
+#include "media/audio/audio_silence_detector.h"
 #include "media/audio/audio_util.h"
 #include "media/audio/shared_memory_util.h"
 
@@ -18,6 +19,17 @@ using base::Time;
 using base::TimeDelta;
 
 namespace media {
+
+// Amount of contiguous time where all audio is silent before considering the
+// stream to have transitioned and EventHandler::OnAudible() should be called.
+static const int kQuestionableSilencePeriodMillis = 50;
+
+// Sample value range below which audio is considered indistinguishably silent.
+//
+// TODO(miu): This value should be specified in dbFS units rather than full
+// scale.  See TODO in audio_silence_detector.h.
+static const float kIndistinguishableSilenceThreshold =
+    1.0f / 4096.0f;  // Note: This is approximately -72 dbFS.
 
 // Polling-related constants.
 const int AudioOutputController::kPollNumAttempts = 3;
@@ -108,14 +120,14 @@ void AudioOutputController::DoCreate(bool is_for_device_change) {
       audio_manager_->MakeAudioOutputStreamProxy(params_);
   if (!stream_) {
     state_ = kError;
-    handler_->OnError(this);
+    handler_->OnError();
     return;
   }
 
   if (!stream_->Open()) {
     DoStopCloseAndClearStream();
     state_ = kError;
-    handler_->OnError(this);
+    handler_->OnError();
     return;
   }
 
@@ -132,7 +144,7 @@ void AudioOutputController::DoCreate(bool is_for_device_change) {
 
   // And then report we have been created if we haven't done so already.
   if (!is_for_device_change)
-    handler_->OnCreated(this);
+    handler_->OnCreated();
 }
 
 void AudioOutputController::DoPlay() {
@@ -182,12 +194,20 @@ void AudioOutputController::StartStream() {
   DCHECK(message_loop_->BelongsToCurrentThread());
   state_ = kPlaying;
 
+  silence_detector_.reset(new AudioSilenceDetector(
+      params_.sample_rate(),
+      TimeDelta::FromMilliseconds(kQuestionableSilencePeriodMillis),
+      kIndistinguishableSilenceThreshold));
+
   // We start the AudioOutputStream lazily.
   AllowEntryToOnMoreIOData();
   stream_->Start(this);
 
-  // Tell the event handler that we are now playing.
-  handler_->OnPlaying(this);
+  // Tell the event handler that we are now playing, and also start the silence
+  // detection notifications.
+  handler_->OnPlaying();
+  silence_detector_->Start(
+      base::Bind(&EventHandler::OnAudible, base::Unretained(handler_)));
 }
 
 void AudioOutputController::StopStream() {
@@ -200,6 +220,8 @@ void AudioOutputController::StopStream() {
   } else if (state_ == kPlaying) {
     stream_->Stop();
     DisallowEntryToOnMoreIOData();
+    silence_detector_->Stop(true);
+    silence_detector_.reset();
     state_ = kPaused;
   }
 }
@@ -215,7 +237,7 @@ void AudioOutputController::DoPause() {
   // Send a special pause mark to the low-latency audio thread.
   sync_reader_->UpdatePendingBytes(kPauseMark);
 
-  handler_->OnPaused(this);
+  handler_->OnPaused();
 }
 
 void AudioOutputController::DoFlush() {
@@ -256,7 +278,7 @@ void AudioOutputController::DoSetVolume(double volume) {
 void AudioOutputController::DoReportError() {
   DCHECK(message_loop_->BelongsToCurrentThread());
   if (state_ != kClosed)
-    handler_->OnError(this);
+    handler_->OnError();
 }
 
 int AudioOutputController::OnMoreData(AudioBus* dest,
@@ -284,6 +306,8 @@ int AudioOutputController::OnMoreIOData(AudioBus* source,
   DCHECK_LE(0, frames);
   sync_reader_->UpdatePendingBytes(
       buffers_state.total_bytes() + frames * params_.GetBytesPerFrame());
+
+  silence_detector_->Scan(dest, frames);
 
   AllowEntryToOnMoreIOData();
   return frames;
