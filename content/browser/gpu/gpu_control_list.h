18eb0f8b64fa94e8da4a53a9ecb435e86d476ec4
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 01:46:12 +0000

    Add tests for classes in GpuControlList.
    BUG=
    TEST=content_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/13047013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191094 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_control_list.h
+++ content/browser/gpu/gpu_control_list.h
@@ -95,6 +95,13 @@ class CONTENT_EXPORT GpuControlList {
   void AddFeature(const std::string& feature_name, int feature_id);
 
  private:
+  friend class GpuControlListEntryTest;
+  friend class MachineModelInfoTest;
+  friend class NumberInfoTest;
+  friend class OsInfoTest;
+  friend class StringInfoTest;
+  friend class VersionInfoTest;
+
   enum BrowserVersionSupport {
     kSupported,
     kUnsupported,
@@ -112,7 +119,7 @@ class CONTENT_EXPORT GpuControlList {
     kUnknown  // Indicates the data is invalid.
   };
 
-  class VersionInfo {
+  class CONTENT_EXPORT VersionInfo {
    public:
     // If version_style is empty, it defaults to kNumerical.
     VersionInfo(const std::string& version_op,
@@ -146,10 +153,9 @@ class CONTENT_EXPORT GpuControlList {
     // Return 1 if version > version_ref,
     //        0 if version = version_ref,
     //       -1 if version < version_ref.
-    // Note that we only compare as many as segments as version_ref contains.
-    // If version_ref is xxx.yyy, it's considered as xxx.yyy.*
+    // Note that we only compare as many segments as both versions contain.
     // For example: Compare("10.3.1", "10.3") returns 0,
-    //              Compare("10.3", "10.3.1") returns -1.
+    //              Compare("10.3", "10.3.1") returns 0.
     // If "version_style" is Lexical, the first segment is compared
     // numerically, all other segments are compared lexically.
     // Lexical is used for AMD Linux driver versions only.
@@ -163,7 +169,7 @@ class CONTENT_EXPORT GpuControlList {
     std::vector<std::string> version2_;
   };
 
-  class OsInfo {
+  class CONTENT_EXPORT OsInfo {
    public:
     OsInfo(const std::string& os,
            const std::string& version_op,
@@ -187,7 +193,7 @@ class CONTENT_EXPORT GpuControlList {
     scoped_ptr<VersionInfo> version_info_;
   };
 
-  class StringInfo {
+  class CONTENT_EXPORT StringInfo {
    public:
     StringInfo(const std::string& string_op, const std::string& string_value);
 
@@ -213,7 +219,7 @@ class CONTENT_EXPORT GpuControlList {
     std::string value_;
   };
 
-  class FloatInfo {
+  class CONTENT_EXPORT FloatInfo {
    public:
     FloatInfo(const std::string& float_op,
               const std::string& float_value,
@@ -231,7 +237,7 @@ class CONTENT_EXPORT GpuControlList {
     float value2_;
   };
 
-  class IntInfo {
+  class CONTENT_EXPORT IntInfo {
    public:
     IntInfo(const std::string& int_op,
             const std::string& int_value,
@@ -249,7 +255,7 @@ class CONTENT_EXPORT GpuControlList {
     int value2_;
   };
 
-  class MachineModelInfo {
+  class CONTENT_EXPORT MachineModelInfo {
    public:
     MachineModelInfo(const std::string& name_op,
                      const std::string& name_value,
@@ -274,7 +280,8 @@ class CONTENT_EXPORT GpuControlList {
 
   typedef base::hash_map<std::string, int> FeatureMap;
 
-  class GpuControlListEntry : public base::RefCounted<GpuControlListEntry> {
+  class CONTENT_EXPORT GpuControlListEntry
+      : public base::RefCounted<GpuControlListEntry> {
    public:
     // Constructs GpuControlListEntry from DictionaryValue loaded from json.
     // Top-level entry must have an id number.  Others are exceptions.
