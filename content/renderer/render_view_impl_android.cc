a58b78f8392b2e65030f0f0ce104ed94b115d010
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 21:24:38 +0000

    Enable hiding/showing top controls persistently
    Pass down to the renderer whether or not we should allow showing the fullscreen controls
    as well as hiding.
    
    BUG=161303
    
    
    Review URL: https://chromiumcodereview.appspot.com/12954003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191210 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl_android.cc
+++ content/renderer/render_view_impl_android.cc
@@ -12,10 +12,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-void RenderViewImpl::OnEnableHidingTopControls(bool enable) {
+void RenderViewImpl::OnUpdateTopControlsState(bool enable_hiding,
+                                              bool enable_showing,
+                                              bool animate) {
   DCHECK(compositor_);
   if (compositor_) {
-    compositor_->EnableHidingTopControls(enable);
+    compositor_->UpdateTopControlsState(enable_hiding, enable_showing, animate);
   }
 }
 
