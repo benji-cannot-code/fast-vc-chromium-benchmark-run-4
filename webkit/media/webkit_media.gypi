8d446f43d7cb49329fed27b32f1beaecac5586c3
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 12:51:02 +0000

    Move WebSourceBufferImpl into its own .cc/.h files
    Review URL: https://chromiumcodereview.appspot.com/12971003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191139 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webkit_media.gypi
+++ webkit/media/webkit_media.gypi
@@ -98,6 +98,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'webmediaplayer_util.h',
         'webmediasourceclient_impl.cc',
         'webmediasourceclient_impl.h',
+        'websourcebuffer_impl.cc',
+        'websourcebuffer_impl.h',
       ],
       'conditions': [
         ['inside_chromium_build == 0', {
---
+++ webkit/media/websourcebuffer_impl.h
@@ -0,0 +1,42 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_MEDIA_WEBSOURCEBUFFER_IMPL_H_
+#define WEBKIT_MEDIA_WEBSOURCEBUFFER_IMPL_H_
+
+#include <string>
+
+#include "base/memory/ref_counted.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebSourceBuffer.h"
+
+namespace media {
+class ChunkDemuxer;
+}
+
+namespace webkit_media {
+
+class WebSourceBufferImpl : public WebKit::WebSourceBuffer {
+ public:
+  WebSourceBufferImpl(const std::string& id,
+                      scoped_refptr<media::ChunkDemuxer> demuxer);
+  virtual ~WebSourceBufferImpl();
+
+  // WebKit::WebSourceBuffer implementation.
+  virtual WebKit::WebTimeRanges buffered() OVERRIDE;
+  virtual void append(const unsigned char* data, unsigned length) OVERRIDE;
+  virtual void abort() OVERRIDE;
+  virtual bool setTimestampOffset(double offset) OVERRIDE;
+  virtual void removedFromMediaSource() OVERRIDE;
+
+ private:
+  std::string id_;
+  scoped_refptr<media::ChunkDemuxer> demuxer_;
+
+  DISALLOW_COPY_AND_ASSIGN(WebSourceBufferImpl);
+};
+
+}  // namespace webkit_media
+
+#endif  // WEBKIT_MEDIA_WEBSOURCEBUFFER_IMPL_H_
