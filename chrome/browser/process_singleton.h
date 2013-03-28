6d84c5b1cdad686d8793d6d7b817d770322056fc
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 12:40:38 +0000

    Fix a bug wherein a member method masked a windows API call.
    ProcessSingleton is supposed to flash/bring to the foreground the visible modal dialog in response to a new execution of chrome.exe. I verified that the functionality was broken by calling chrome.exe --try-chrome-again=0, sending the toast to the background, and then launching chrome.exe again. Before this CL, nothing happens. After this CL, the toast pops to the foreground.
    
    R=thakis@chromium.org
    BUG=None
    
    
    Review URL: https://chromiumcodereview.appspot.com/12918031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191134 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/process_singleton.h
+++ chrome/browser/process_singleton.h
@@ -95,7 +95,7 @@ class ProcessSingleton : public base::NonThreadSafe {
   }
 
   // Changes the foreground window without changing the locked state.
-  void SetForegroundWindow(gfx::NativeWindow foreground_window) {
+  void SetActiveModalDialog(gfx::NativeWindow foreground_window) {
     DCHECK(CalledOnValidThread());
     foreground_window_ = foreground_window;
   }
