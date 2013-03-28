e941243ac401c82d658d9d4034e9af59fd216b65
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 21:46:36 +0000

    This CL fixes reported problem of window width being expanded when restoring from minimized mode. This was Aura specific bug. Restore width problem was caused because of stale information being returned from WM_GETMINMAXINFO. To fix the problem we now let native_widget_delegate to update and store information after size change event. This stored information is indirectly used from WM_GETMINMAXINFO.
    BUG=175505
    TEST=In Aura mode, try minimize and restore of browser window and make sure browser restores to same width as it was before minimizing.
    Review URL: https://codereview.chromium.org/12326026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191220 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/widget_unittest.cc
+++ ui/views/widget/widget_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/test/views_test_base.h"
 #include "ui/views/views_delegate.h"
 #include "ui/views/widget/native_widget_delegate.h"
+#include "ui/views/window/native_frame_view.h"
 
 #if defined(USE_AURA)
 #include "ui/aura/client/aura_constants.h"
@@ -210,6 +211,21 @@ class GestureCaptureView : public View {
   }
 
   DISALLOW_COPY_AND_ASSIGN(GestureCaptureView);
+};
+
+// A view that implements GetMinimumSize.
+class MinimumSizeFrameView : public NativeFrameView {
+ public:
+  explicit MinimumSizeFrameView(Widget* frame): NativeFrameView(frame) {}
+  virtual ~MinimumSizeFrameView() {}
+
+ private:
+  // Overridden from View:
+  virtual gfx::Size GetMinimumSize() OVERRIDE {
+    return gfx::Size(300, 400);
+  }
+
+  DISALLOW_COPY_AND_ASSIGN(MinimumSizeFrameView);
 };
 
 // An event handler that simply keeps a count of the different types of events
@@ -1210,6 +1226,26 @@ TEST_F(WidgetTest, FocusChangesOnBubble) {
 
 // Desktop native widget Aura tests are for non Chrome OS platforms.
 #if !defined(OS_CHROMEOS)
+// Test to ensure that after minimize, view width is set to zero.
+TEST_F(WidgetTest, TestViewWidthAfterMinimizingWidget) {
+  // Create a widget.
+  Widget widget;
+  Widget::InitParams init_params =
+      CreateParams(Widget::InitParams::TYPE_WINDOW);
+  init_params.show_state = ui::SHOW_STATE_NORMAL;
+  gfx::Rect initial_bounds(0, 0, 300, 400);
+  init_params.bounds = initial_bounds;
+  init_params.ownership = Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
+  init_params.native_widget = new DesktopNativeWidgetAura(&widget);
+  widget.Init(init_params);
+  NonClientView* non_client_view = widget.non_client_view();
+  NonClientFrameView* frame_view = new MinimumSizeFrameView(&widget);
+  non_client_view->SetFrameView(frame_view);
+  widget.Show();
+  widget.Minimize();
+  EXPECT_EQ(0, frame_view->width());
+}
+
 // This class validates whether paints are received for a visible Widget.
 // To achieve this it overrides the Show and Close methods on the Widget class
 // and sets state whether subsequent paints are expected.
