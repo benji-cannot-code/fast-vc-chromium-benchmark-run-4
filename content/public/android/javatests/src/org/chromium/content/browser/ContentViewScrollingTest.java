344c70eb96bad6401e924c0f21c01b3f6eea63a7
dfalcantara@chromium.org <dfalcantara@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-28 01:46:49 +0000

    Disable testFling
    Flaky on the main waterfall.
    
    BUG=224458
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/13052005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191095 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/ContentViewScrollingTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/ContentViewScrollingTest.java
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 package org.chromium.content.browser;
 
+import android.test.FlakyTest;
 import android.test.suitebuilder.annotation.SmallTest;
 
 import org.chromium.base.test.util.Feature;
@@ -85,8 +86,12 @@ public class ContentViewScrollingTest extends ContentShellTestBase {
         assertEquals(0, getContentViewCore().getNativeScrollYForTest());
     }
 
-    @SmallTest
-    @Feature({"Main"})
+    /**
+     * @SmallTest
+     * @Feature({"Main"})
+     * BUG 224458
+     */
+    @FlakyTest
     public void testFling() throws Throwable {
         // Vertical fling to lower-left.
         fling(0, -1000);
