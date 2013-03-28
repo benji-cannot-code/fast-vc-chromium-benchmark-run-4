96bc6f4da64bcc247e60e4043d6ac6899f861338
fpizlo@apple.com <fpizlo@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-28 02:47:00 +0000

    JIT and DFG should NaN-check loads from Float32 arrays https://bugs.webkit.org/show_bug.cgi?id=113462 <rdar://problem/13490804>
    Reviewed by Mark Hahnenberg.
    
    Source/JavaScriptCore: 
    
    * dfg/DFGSpeculativeJIT.cpp:
    (JSC::DFG::SpeculativeJIT::compileGetByValOnFloatTypedArray):
    * jit/JITPropertyAccess.cpp:
    (JSC::JIT::emitFloatTypedArrayGetByVal):
    
    LayoutTests: 
    
    * fast/js/dfg-float32-array-nan-expected.txt: Added.
    * fast/js/dfg-float32-array-nan.html: Added.
    * fast/js/jit-float32-array-nan-expected.txt: Added.
    * fast/js/jit-float32-array-nan.html: Added.
    * fast/js/jsc-test-list:
    * fast/js/script-tests/dfg-float32-array-nan.js: Added.
    (foo):
    * fast/js/script-tests/jit-float32-array-nan.js: Added.
    
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/js/script-tests/jit-float32-array-nan.js
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description(
+"Tests what happens in the Baseline JIT if we load from a Float32 array that contains a weirdly formatted NaN."
+);
+
+var buffer = new ArrayBuffer(4);
+
+var int32View = new Int32Array(buffer);
+var floatView = new Float32Array(buffer);
+
+int32View[0] = -4587521;
+
+debug(int32View[0]);
+
+for (var i = 0; i < 100; ++i) {
+    debug(floatView[0]);
+    shouldBe("\"\" + floatView[0]", "\"NaN\"");
+}
+
+debug("I didn't crash, which is a good thing.");
