46669a8e3eec3f17254dadfde9f44472ae93088c
timothy_horton@apple.com <timothy_horton@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-28 22:07:16 +0000

    [wk2] REGRESSION (r125500): WebFrameLoaderClient's active m_pluginView can be zeroed by PluginDocuments leaving the page cache https://bugs.webkit.org/show_bug.cgi?id=113430 <rdar://problem/12729564>
    Reviewed by Brady Eidson.
    
    http://trac.webkit.org/changeset/147152 was wrong, and should have been
    (as the ChangeLog stated) testing the argument to redirectDataToPlugin,
    not the value of m_pluginView.
    
    * WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp:
    (WebKit::WebFrameLoaderClient::redirectDataToPlugin):
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147168 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebFrameLoaderClient.cpp
@@ -1347,7 +1347,7 @@ void WebFrameLoaderClient::recreatePlugin(Widget* widget)
 
 void WebFrameLoaderClient::redirectDataToPlugin(Widget* pluginWidget)
 {
-    if (m_pluginView)
+    if (pluginWidget)
         m_pluginView = static_cast<PluginView*>(pluginWidget);
 }
 
