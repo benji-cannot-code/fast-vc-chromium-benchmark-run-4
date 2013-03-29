764e9597287d6a501053fbe973f6398ee494db15
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-29 00:17:42 +0000

    Do not always request a new commit when memory limit is changed.
    Skip re-committing if there is already enough memory to for everything, 
    and the new memory limit hasn't changed that situation. 
    
    In situations where fluctuations in, say, WebGL's memory usage causes 
    the page's limit to move up and down frequently, this will prevent extra 
    commits unless the changes actually change what is being rendered. 
    
    Add tests for new LTH and PRM functions. 
    
    BUG=189199
    
    
    Review URL: https://chromiumcodereview.appspot.com/12958007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191252 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/prioritized_resource_unittest.cc
+++ cc/resources/prioritized_resource_unittest.cc
@@ -138,6 +138,8 @@ TEST_F(PrioritizedResourceTest, RequestTextureExceedingMaxLimit) {
             resource_manager->MemoryAboveCutoffBytes());
   EXPECT_LE(resource_manager->MemoryUseBytes(),
             resource_manager->MemoryAboveCutoffBytes());
+  EXPECT_EQ(TexturesMemorySize(2*kMaxTextures),
+            resource_manager->MaxMemoryNeededBytes());
 
   DebugScopedSetImplThreadAndMainThreadBlocked
   impl_thread_and_main_thread_blocked(&proxy_);
@@ -186,6 +188,8 @@ TEST_F(PrioritizedResourceTest, ChangeMemoryLimits) {
   EXPECT_EQ(TexturesMemorySize(5), resource_manager->MemoryAboveCutoffBytes());
   EXPECT_LE(resource_manager->MemoryUseBytes(),
             resource_manager->MemoryAboveCutoffBytes());
+  EXPECT_EQ(TexturesMemorySize(kMaxTextures),
+            resource_manager->MaxMemoryNeededBytes());
 
   // Set max limit to 4 textures
   resource_manager->SetMaxMemoryLimitBytes(TexturesMemorySize(4));
@@ -201,6 +205,8 @@ TEST_F(PrioritizedResourceTest, ChangeMemoryLimits) {
   EXPECT_EQ(TexturesMemorySize(4), resource_manager->MemoryAboveCutoffBytes());
   EXPECT_LE(resource_manager->MemoryUseBytes(),
             resource_manager->MemoryAboveCutoffBytes());
+  EXPECT_EQ(TexturesMemorySize(kMaxTextures),
+            resource_manager->MaxMemoryNeededBytes());
 
   DebugScopedSetImplThreadAndMainThreadBlocked
   impl_thread_and_main_thread_blocked(&proxy_);
@@ -505,6 +511,8 @@ TEST_F(PrioritizedResourceTest,
             resource_manager->MemoryForSelfManagedTextures());
   EXPECT_LE(resource_manager->MemoryUseBytes(),
             resource_manager->MemoryAboveCutoffBytes());
+  EXPECT_EQ(TexturesMemorySize(8),
+            resource_manager->MaxMemoryNeededBytes());
 
   DebugScopedSetImplThreadAndMainThreadBlocked
   impl_thread_and_main_thread_blocked(&proxy_);
@@ -551,6 +559,8 @@ TEST_F(PrioritizedResourceTest,
             resource_manager->MemoryForSelfManagedTextures());
   EXPECT_LE(resource_manager->MemoryUseBytes(),
             resource_manager->MemoryAboveCutoffBytes());
+  EXPECT_EQ(TexturesMemorySize(8),
+            resource_manager->MaxMemoryNeededBytes());
 
   DebugScopedSetImplThreadAndMainThreadBlocked
   impl_thread_and_main_thread_blocked(&proxy_);
