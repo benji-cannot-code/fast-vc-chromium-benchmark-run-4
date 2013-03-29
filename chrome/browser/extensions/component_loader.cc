b107241ddd53a21f1329d26837f3d613c2cdda4e
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-29 04:10:52 +0000

    Enable custom Wallpaper Picker in chromium build
    BUG=223566
    TEST=Build a dev build and verify if custom wallpaper picker can be opened
    from context menu or settings page.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12913027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191290 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/component_loader.cc
+++ chrome/browser/extensions/component_loader.cc
@@ -417,7 +417,6 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
 
 #if defined(OS_CHROMEOS)
   if (!skip_session_components) {
-#if defined(GOOGLE_CHROME_BUILD)
     Add(IDR_WALLPAPERMANAGER_MANIFEST,
         base::FilePath(FILE_PATH_LITERAL("chromeos/wallpaper_manager")));
 
@@ -430,7 +429,6 @@ void ComponentLoader::AddDefaultComponentExtensionsWithBackgroundPages(
                                   "/usr/share/chromeos-assets/quick_office")));
       }
     }
-#endif  // defined(OFFICIAL_BUILD)
 
     base::FilePath echo_extension_path(FILE_PATH_LITERAL(
         "/usr/share/chromeos-assets/echo"));
