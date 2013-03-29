6c59baa055dad329468bc11818d03f6b2d4292b2
dino@apple.com <dino@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-29 19:31:45 +0000

    Snapshotted plugins must be able to restart on appropriate mouseup events https://bugs.webkit.org/show_bug.cgi?id=113577
    Reviewed by Tim Horton.
    
    If the page content prevents the default behaviour of a mousedown event, then a snapshotted
    plugin would never receive a click event, and thus be unable to restart. We have to also
    look for a mouseup that happens with an associated mousedown, and trigger restart. This
    won't call any page code - it's just behind the scenes.
    
    * rendering/RenderSnapshottedPlugIn.cpp:
    (WebCore::RenderSnapshottedPlugIn::RenderSnapshottedPlugIn): Initialize new member variable.
    (WebCore::RenderSnapshottedPlugIn::handleEvent): Track the state of mousedown and up pairs, and restart
        if you see an appropriate mouseup.
    * rendering/RenderSnapshottedPlugIn.h: New member variable to track mouse state.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderSnapshottedPlugIn.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderSnapshottedPlugIn.h
@@ -59,6 +59,7 @@ private:
     virtual void layout() OVERRIDE;
 
     OwnPtr<RenderImageResource> m_snapshotResource;
+    bool m_isPotentialMouseActivation;
 };
 
 inline RenderSnapshottedPlugIn* toRenderSnapshottedPlugIn(RenderObject* object)
