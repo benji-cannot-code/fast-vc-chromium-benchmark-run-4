26d5e3b572b6beaa840b536fda5152e68b649d65
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-29 19:17:58 +0000

    Check in a custom-built import library for user32 exports up to Windows XP SP2/SP3, which is used by chrome.dll in preference to the Platform SDK's user32.lib import library. Custom build an import library for Chrome's post-WinXP imports, that binds to a fictional "user32-delay.dll". Implement and test a delay load hook that diverts dynamic loading of any dll "foo-delay.dll" to "foo.dll".
    R=cpu@chromium.org
    BUG=176040
    
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=191173
    
    Review URL: https://chromiumcodereview.appspot.com/12295040
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191418 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/app/delay_load_hook_win.h
@@ -0,0 +1,19 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_APP_DELAY_LOAD_HOOK_WIN_H_
+#define CHROME_APP_DELAY_LOAD_HOOK_WIN_H_
+
+#include <windows.h>
+
+// Fowd.
+struct DelayLoadInfo;
+
+// In release builds, the delay load hook redirects import entries to DLLs
+// named "FOO-delay.dll" to "FOO.dll".
+extern "C" FARPROC WINAPI ChromeDelayLoadHook(unsigned reason,
+                                              DelayLoadInfo* info);
+
+#endif  // CHROME_APP_DELAY_LOAD_HOOK_WIN_H_
