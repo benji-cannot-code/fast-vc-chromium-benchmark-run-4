d079788bf678d5db6b301ae4acab8ca571f58727
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-29 17:46:23 +0000

    Move shared_memory into base/memory subdirectory.
    This leaves a forwarding header in place so all calling code can be updated asynchronously.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13044024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191392 0039d316-1c4b-4281-b951-d872f2087c98

--- base/memory/shared_memory_posix.cc
+++ base/memory/shared_memory_posix.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/shared_memory.h"
+#include "base/memory/shared_memory.h"
 
 #include <errno.h>
 #include <fcntl.h>
--- base/memory/shared_memory_unittest.cc
+++ base/memory/shared_memory_unittest.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/mac/scoped_nsautorelease_pool.h"
 #endif
 #include "base/memory/scoped_ptr.h"
-#include "base/shared_memory.h"
+#include "base/memory/shared_memory.h"
 #include "base/sys_info.h"
 #include "base/test/multiprocess_test.h"
 #include "base/threading/platform_thread.h"
