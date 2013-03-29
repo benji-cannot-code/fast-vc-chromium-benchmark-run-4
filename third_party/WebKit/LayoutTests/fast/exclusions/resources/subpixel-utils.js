bd232be0eaeff87e00adebfc865a270389535272
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-29 20:34:17 +0000

    [CSS Exclusions] Incorrect margin corner radii formula https://bugs.webkit.org/show_bug.cgi?id=111186
    Reviewed by Dirk Schulze.
    
    Source/WebCore:
    
    Corrected an error in the forumla for the margin ellipse's radii.
    
    Tests: fast/exclusions/shape-outside-floats/shape-outside-floats-ellipse-margin-left.html
           fast/exclusions/shape-outside-floats/shape-outside-floats-ellipse-margin-right.html
    
    * rendering/ExclusionRectangle.cpp:
    (WebCore::FloatRoundedRect::marginBounds):
    
    LayoutTests:
    
    * fast/exclusions/resources/subpixel-utils.js:
    (SubPixelLayout): Added an initSubPixelLayout() function so that inline test code doesn't have to wait until the DOMContentLoaded event.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-ellipse-margin-left-expected.txt: Added.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-ellipse-margin-left.html: Added.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-ellipse-margin-right-expected.txt: Added.
    * fast/exclusions/shape-outside-floats/shape-outside-floats-ellipse-margin-right.html: Added.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147249 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/exclusions/resources/subpixel-utils.js
+++ third_party/WebKit/LayoutTests/fast/exclusions/resources/subpixel-utils.js
@@ -1,15 +1,17 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var SubPixelLayout = (function() {
     var _subPixelLayout = null;
-    document.addEventListener('DOMContentLoaded', function() {
+    function initSubPixelLayout() {
         var elem = document.createElement('div');
         elem.style.setProperty('width', '4.5px');
         document.body.appendChild(elem);
         var bounds = elem.getBoundingClientRect();
         _subPixelLayout =  (bounds.width != Math.floor(bounds.width));
         document.body.removeChild(elem);
-    });
+    }
+    document.addEventListener('DOMContentLoaded', initSubPixelLayout);
     return {
+        initSubPixelLayout: initSubPixelLayout,
         roundLineLeft: function(f) {
             if (!_subPixelLayout)
                 return Math.floor(f);
