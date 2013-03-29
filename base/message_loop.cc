991b3170cecc16aa0f967d0c29312b3669779428
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-29 19:22:51 +0000

    Create a new base/message_loop directory and move the message_loop_proxy.
    Eventually we'll move all the message loop and message pump classes into this
    directory. This leaves a forwarding header to avoid having to update all
    includes in this patch.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13333003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191422 0039d316-1c4b-4281-b951-d872f2087c98

--- base/message_loop.cc
+++ base/message_loop.cc
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/lazy_instance.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop_proxy_impl.h"
+#include "base/message_loop/message_loop_proxy_impl.h"
 #include "base/message_pump_default.h"
 #include "base/metrics/histogram.h"
 #include "base/metrics/statistics_recorder.h"
