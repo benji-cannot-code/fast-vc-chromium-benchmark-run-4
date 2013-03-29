13e7d10ab99096df9b31b790e83bfca3022097c5
simon.fraser@apple.com <simon.fraser@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-03-29 00:18:44 +0000

    GraphicsLayerCA::recomputeVisibleRects() shouldn't actually set the visible rect https://bugs.webkit.org/show_bug.cgi?id=113544
    Reviewed by Tim Horton.
    
    The functions added in r147058 to update the visible rects of GraphicsLayerCAs
    should not have actually set the m_visibleRect on the layer, because this
    caused a later call to adjustTiledLayerVisibleRect() to think that the visible
    rect had not changed, so it did no expansion.
    
    Clarify the logic to ask the GraphicsLayer whether, given a new visible
    rect, it would need to make new tiles (which happens later during a flush).
    
    Add a function to RenderLayerCompositor to call this code (but which has
    no callers yet).
    
    * platform/graphics/GraphicsLayer.h:
    (WebCore::GraphicsLayer::visibleRectChangeRequiresFlush): Make private.
    * platform/graphics/ca/GraphicsLayerCA.cpp:
    (WebCore::GraphicsLayerCA::recursiveVisibleRectChangeRequiresFlush):
    (WebCore::GraphicsLayerCA::visibleRectChangeRequiresFlush):
    * platform/graphics/ca/GraphicsLayerCA.h:
    * rendering/RenderLayerCompositor.cpp:
    (WebCore::RenderLayerCompositor::noteVisibleRectChanged):
    * rendering/RenderLayerCompositor.h:
    (RenderLayerCompositor):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147186 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/ca/GraphicsLayerCA.h
@@ -143,8 +143,7 @@ public:
     virtual void flushCompositingState(const FloatRect&);
     virtual void flushCompositingStateForThisLayerOnly();
 
-    void recursiveComputeVisibleRect(const TransformState&);
-    virtual void recomputeVisibleRects(const FloatRect& clipRect);
+    virtual bool visibleRectChangeRequiresFlush(const FloatRect& visibleRect) const OVERRIDE;
 
     virtual TiledBacking* tiledBacking() const OVERRIDE;
 
@@ -253,6 +252,8 @@ private:
     const FloatRect& visibleRect() const { return m_visibleRect; }
     
     FloatRect adjustTiledLayerVisibleRect(TiledBacking*, const FloatRect& oldVisibleRect, const FloatSize& oldSize) const;
+
+    bool recursiveVisibleRectChangeRequiresFlush(const TransformState&) const;
 
     // Used to track the path down the tree for replica layers.
     struct ReplicaState {
