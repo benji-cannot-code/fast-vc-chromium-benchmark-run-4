7b65a1cc536cb4747fd038981a0400f83158807d
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-03-30 20:32:51 +0000

    requestAutocomplete: fix crash
    if you press "sign in" twice, then press back, it will trigger a CHECK. Fix this by disabling the link while signing in.
    
    BUG=225103
    
    
    Review URL: https://chromiumcodereview.appspot.com/13351005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191557 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/autofill_dialog_views.cc
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.cc
@@ -178,6 +178,10 @@ void AutofillDialogViews::AccountChooser::Update() {
   InvalidateLayout();
 }
 
+void AutofillDialogViews::AccountChooser::SetSignInLinkEnabled(bool enabled) {
+  link_->SetEnabled(enabled);
+}
+
 bool AutofillDialogViews::AccountChooser::OnMousePressed(
     const ui::MouseEvent& event) {
   // Return true so we get the release event.
@@ -666,6 +670,7 @@ const content::NavigationController* AutofillDialogViews::ShowSignIn() {
   // TODO(abodenha) We should be able to use the WebContents of the WebView
   // to navigate instead of LoadInitialURL.  Figure out why it doesn't work.
 
+  account_chooser_->SetSignInLinkEnabled(false);
   sign_in_webview_->LoadInitialURL(wallet::GetSignInUrl());
   // TODO(abodenha) Resize the dialog to avoid the need for a scroll bar on
   // sign in. See http://crbug.com/169286
@@ -677,6 +682,7 @@ const content::NavigationController* AutofillDialogViews::ShowSignIn() {
 }
 
 void AutofillDialogViews::HideSignIn() {
+  account_chooser_->SetSignInLinkEnabled(true);
   sign_in_container_->SetVisible(false);
   main_container_->SetVisible(true);
 }
--- chrome/browser/ui/views/autofill/autofill_dialog_views.h
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -174,6 +174,9 @@ class AutofillDialogViews : public AutofillDialogView,
     // Updates the view based on the state that |controller_| reports.
     void Update();
 
+    // Sets the state of the sign in link.
+    void SetSignInLinkEnabled(bool enabled);
+
     // views::View implementation.
     virtual bool OnMousePressed(const ui::MouseEvent& event) OVERRIDE;
     virtual void OnMouseReleased(const ui::MouseEvent& event) OVERRIDE;
@@ -191,7 +194,7 @@ class AutofillDialogViews : public AutofillDialogView,
     // The drop arrow.
     views::ImageView* arrow_;
 
-    // The signin link.
+    // The sign in link.
     views::Link* link_;
 
     // The controller |this| queries for logic and state.
