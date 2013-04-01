e573c223da268fa1b2149b8046d04ba4d61890cb
glotov@chromium.org <glotov@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-01 19:36:48 +0000

    [cros] Temporaly add logging to login screen for events dealing with action box.
    We need this to understand the bug root on the link device (inaccessible locally).
    
    BUG=224526
    TEST=none
    NOTRY=true
    
    
    Review URL: https://chromiumcodereview.appspot.com/13375014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191640 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/user_pod_row.js
+++ chrome/browser/resources/chromeos/login/user_pod_row.js
@@ -470,6 +470,8 @@ cr.define('login', function() {
     handleActionAreaButtonClick_: function(e) {
       if (this.parentNode.disabled)
         return;
+      console.error('Action area clicked: ' + !this.isActionBoxMenuActive +
+                    ' at ' + e.x + ', ' + e.y);
       this.isActionBoxMenuActive = !this.isActionBoxMenuActive;
     },
 
@@ -483,8 +485,11 @@ cr.define('login', function() {
       switch (e.keyIdentifier) {
         case 'Enter':
         case 'U+0020':  // Space
-          if (this.parentNode.focusedPod_ && !this.isActionBoxMenuActive)
+          if (this.parentNode.focusedPod_ && !this.isActionBoxMenuActive) {
+            console.error('Action area keyed: ' + !this.isActionBoxMenuActive +
+                          ' at ' + e.x + ', ' + e.y);
             this.isActionBoxMenuActive = true;
+          }
           e.stopPropagation();
           break;
         case 'Up':
@@ -1186,6 +1191,9 @@ cr.define('login', function() {
     handleClick_: function(e) {
       if (this.disabled)
         return;
+
+      console.error('Document clicked at ' + e.x + ', ' + e.y +
+                    ', pod: ' + findAncestorByClass(e.target, 'pod'));
 
       // Clear all menus if the click is outside pod menu and its
       // button area.
