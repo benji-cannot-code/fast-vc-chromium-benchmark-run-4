496eba4ec2208e70b3881c4889cddbec01592269
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-01 09:43:50 +0000

    Add JS-side UMA stats.
    This CL starts to record UMA stats for file opening operations,
    grouped by file extensions.
    The concept is similar to what we do now in file_manager_util, but there is
    some difference:
    - name is "ViewingFileType".
    - variation of extensions. (added based on the list in the linked issue).
    - records all file opening operations. (currently only failed operations are
    recorded).
    
    BUG=189103
    TEST=Tested by opening some files manually, and made sure on chrome://histogram
    
    
    Review URL: https://chromiumcodereview.appspot.com/13146004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1420,7 +1420,7 @@ DialogType.isModal = function(type) {
             if (action == 'gallery') {
               tasks.openGallery(urls);
             } else if (action == 'archives') {
-              tasks.mountArchives_(urls);
+              tasks.mountArchives(urls);
             }
           }.bind(this);
           this.directoryModel_.addEventListener('scan-completed', listener);
