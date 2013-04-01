b6faa60e85914827b528822fb14dce86c55972e4
andersca@apple.com <andersca@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-01 21:19:01 +0000

    Didn't mean to commit this!
    * WebProcess/WebCoreSupport/WebPlatformStrategies.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebPlatformStrategies.cpp
+++ third_party/WebKit/Source/WebKit2/WebProcess/WebCoreSupport/WebPlatformStrategies.cpp
@@ -61,7 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 // FIXME: Remove this once it works well enough to be the default.
-#define ENABLE_UI_PROCESS_STORAGE 1
+#define ENABLE_UI_PROCESS_STORAGE 0
 
 using namespace WebCore;
 
