e401c3b333cb8f9fff2651504a17df2519d8c531
timothy@apple.com <timothy@apple.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-01 21:32:36 +0000

    Allow changing Web Inspector dock sides without undocking first.
    https://webkit.org/b/113661
    rdar://problem/13543127
    
    Reviewed by Joseph Pecoraro.
    
    Source/WebCore:
    
    * inspector/InspectorFrontendClientLocal.cpp:
    (WebCore::InspectorFrontendClientLocal::InspectorFrontendClientLocal): Initialize m_dockSide.
    
    (WebCore::InspectorFrontendClientLocal::canAttachWindow): If we are already attached, allow
    attaching again to allow switching sides.
    
    (WebCore::InspectorFrontendClientLocal::setAttachedWindow): Set m_dockSide.
    
    * inspector/InspectorFrontendClientLocal.h:
    (InspectorFrontendClientLocal): Added m_dockSide.
    
    Source/WebKit2:
    
    * UIProcess/WebInspectorProxy.cpp:
    (WebKit::WebInspectorProxy::createInspectorPage): Send AttachedBottom and
    AttachedRight messages so InspectorFrontendClientLocal knows which side we
    are attached.
    
    (WebKit::WebInspectorProxy::canAttach): If we are already attached, allow
    attaching again to allow switching sides.
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147362 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorFrontendClientLocal.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorFrontendClientLocal.h
@@ -124,6 +124,7 @@ private:
     RefPtr<InspectorFrontendHost> m_frontendHost;
     OwnPtr<InspectorFrontendClientLocal::Settings> m_settings;
     bool m_frontendLoaded;
+    DockSide m_dockSide;
     Vector<String> m_evaluateOnLoad;
     OwnPtr<InspectorBackendDispatchTask> m_dispatchTask;
 };
