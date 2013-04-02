68be06ad571f1a1fc843f4754fd010e73d3de44b
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-02 14:28:48 +0000

    Web Inspector: allow referencing of nodes that have not been pushed to the front-end https://bugs.webkit.org/show_bug.cgi?id=110921
    Patch by Dmitry Gozman <dgozman@chromium.org> on 2013-04-02
    Reviewed by Pavel Feldman.
    
    Nodes not yet pushed to front-end are referenced by BackendNodeId and retained.
    One can group nodes when generating BackendNodeIds to release them all together later.
    
    To resolve node references by BackendNodeId, frontend should use new pushNodeByBackendIdToFrontend method.
    This will invalidate the generated BackendNodeId.
    
    * inspector/Inspector.json:
    * inspector/InspectorDOMAgent.cpp:
    (WebCore::InspectorDOMAgent::discardBindings):
    (WebCore::InspectorDOMAgent::backendNodeIdForNode):
    (WebCore::InspectorDOMAgent::releaseBackendNodeIds):
    (WebCore::InspectorDOMAgent::pushNodeByBackendIdToFrontend):
    * inspector/InspectorDOMAgent.h:
    (WebCore):
    (InspectorDOMAgent):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.h
+++ third_party/WebKit/Source/WebCore/inspector/InspectorDOMAgent.h
@@ -76,6 +76,7 @@ class ShadowRoot;
 struct HighlightConfig;
 
 typedef String ErrorString;
+typedef int BackendNodeId;
 
 #if ENABLE(INSPECTOR)
 
@@ -142,6 +143,8 @@ public:
     virtual void setInspectModeEnabled(ErrorString*, bool enabled, const RefPtr<InspectorObject>* highlightConfig);
     virtual void requestNode(ErrorString*, const String& objectId, int* nodeId);
     virtual void pushNodeByPathToFrontend(ErrorString*, const String& path, int* nodeId);
+    virtual void pushNodeByBackendIdToFrontend(ErrorString*, BackendNodeId, int* nodeId);
+    virtual void releaseBackendNodeIds(ErrorString*, const String& nodeGroup);
     virtual void hideHighlight(ErrorString*);
     virtual void highlightRect(ErrorString*, int x, int y, int width, int height, const RefPtr<InspectorObject>* color, const RefPtr<InspectorObject>* outlineColor);
     virtual void highlightQuad(ErrorString*, const RefPtr<InspectorArray>& quad, const RefPtr<InspectorObject>* color, const RefPtr<InspectorObject>* outlineColor);
@@ -180,6 +183,7 @@ public:
     Node* nodeForId(int nodeId);
     int boundNodeId(Node*);
     void setDOMListener(DOMListener*);
+    BackendNodeId backendNodeIdForNode(Node*, const String& nodeGroup);
 
     static String documentURLString(Document*);
 
@@ -247,12 +251,16 @@ private:
     InspectorFrontend::DOM* m_frontend;
     DOMListener* m_domListener;
     NodeToIdMap m_documentNodeToIdMap;
+    typedef HashMap<RefPtr<Node>, BackendNodeId> NodeToBackendIdMap;
+    HashMap<String, NodeToBackendIdMap> m_nodeGroupToBackendIdMap;
     // Owns node mappings for dangling nodes.
     Vector<OwnPtr<NodeToIdMap> > m_danglingNodeToIdMaps;
     HashMap<int, Node*> m_idToNode;
     HashMap<int, NodeToIdMap*> m_idToNodesMap;
     HashSet<int> m_childrenRequested;
+    HashMap<BackendNodeId, std::pair<Node*, String> > m_backendIdToNode;
     int m_lastNodeId;
+    BackendNodeId m_lastBackendNodeId;
     RefPtr<Document> m_document;
     typedef HashMap<String, Vector<RefPtr<Node> > > SearchResults;
     SearchResults m_searchResults;
