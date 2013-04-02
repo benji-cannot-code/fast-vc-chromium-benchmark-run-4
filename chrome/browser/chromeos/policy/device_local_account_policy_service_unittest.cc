1f12aa2333b74154ec67be9565bc1b3af3bedce2
bartfab@chromium.org <bartfab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 17:12:40 +0000

    Exit public session on lid close
    The default lid-close action is "suspend", which is undesirable for a
    public session. This CL ensures that the lid-close action in public
    sessions always is "exit session" instead.
    
    BUG=224437
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/13471004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191846 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_local_account_policy_service_unittest.cc
+++ chrome/browser/chromeos/policy/device_local_account_policy_service_unittest.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud/mock_device_management_service.h"
 #include "chrome/browser/policy/cloud/policy_builder.h"
 #include "chrome/browser/policy/mock_configuration_policy_provider.h"
+#include "chromeos/dbus/power_policy_controller.h"
 #include "policy/policy_constants.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -47,6 +48,12 @@ class DeviceLocalAccountPolicyServiceTest
     DeviceSettingsTestBase::SetUp();
 
     // Values implicitly enforced for public accounts.
+    expected_policy_map_.Set(key::kLidCloseAction,
+                             POLICY_LEVEL_MANDATORY,
+                             POLICY_SCOPE_USER,
+                             base::Value::CreateIntegerValue(
+                                 chromeos::PowerPolicyController::
+                                     ACTION_STOP_SESSION));
     expected_policy_map_.Set(key::kShelfAutoHideBehavior,
                              POLICY_LEVEL_MANDATORY,
                              POLICY_SCOPE_USER,
--- chrome/browser/chromeos/policy/device_local_account_policy_store.cc
+++ chrome/browser/chromeos/policy/device_local_account_policy_store.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/cloud/device_management_service.h"
 #include "chrome/browser/policy/cloud/proto/device_management_backend.pb.h"
 #include "chrome/browser/policy/policy_types.h"
+#include "chromeos/dbus/power_policy_controller.h"
 #include "chromeos/dbus/session_manager_client.h"
 #include "policy/policy_constants.h"
 #include "policy/proto/cloud_policy.pb.h"
@@ -75,6 +76,15 @@ void DeviceLocalAccountPolicyStore::UpdatePolicy(
   }
 
   InstallPolicy(validator->policy_data().Pass(), validator->payload().Pass());
+  // Exit the session when the lid is closed. The default behavior is to
+  // suspend while leaving the session running, which is not desirable for
+  // public sessions.
+  policy_map_.Set(key::kLidCloseAction,
+                  POLICY_LEVEL_MANDATORY,
+                  POLICY_SCOPE_USER,
+                  base::Value::CreateIntegerValue(
+                      chromeos::PowerPolicyController::ACTION_STOP_SESSION));
+
   // Force the |ShelfAutoHideBehavior| policy to |Never|, ensuring that the ash
   // shelf does not auto-hide.
   policy_map_.Set(key::kShelfAutoHideBehavior,
