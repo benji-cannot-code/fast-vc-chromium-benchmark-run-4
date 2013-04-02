b9b02f8da8194af05cb8a3af80e58c8aa0c639fb
alexst@chromium.org <alexst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 18:16:28 +0000

    Enable ubercomp support for webview compositing.
    BUG=
    
    
    Review URL: https://chromiumcodereview.appspot.com/12681022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191859 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/browser_plugin/browser_plugin_messages.h
+++ content/common/browser_plugin/browser_plugin_messages.h
@@ -11,6 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process.h"
 #include "base/shared_memory.h"
 #include "base/values.h"
+#include "cc/output/compositor_frame.h"
+#include "cc/output/compositor_frame_ack.h"
 #include "content/common/browser_plugin/browser_plugin_message_enums.h"
 #include "content/common/content_export.h"
 #include "content/common/content_param_traits.h"
@@ -221,6 +223,13 @@ IPC_MESSAGE_ROUTED5(BrowserPluginHostMsg_BuffersSwappedACK,
                     std::string /* mailbox_name */,
                     uint32 /* sync_point */)
 
+// Acknowledge that we presented an ubercomp frame.
+IPC_MESSAGE_ROUTED4(BrowserPluginHostMsg_CompositorFrameACK,
+                    int /* instance_id */,
+                    int /* route_id */,
+                    int /* renderer_host_id */,
+                    cc::CompositorFrameAck /* ack */)
+
 // When a BrowserPlugin has been removed from the embedder's DOM, it informs
 // the browser process to cleanup the guest.
 IPC_MESSAGE_ROUTED1(BrowserPluginHostMsg_PluginDestroyed,
@@ -398,6 +407,12 @@ IPC_MESSAGE_CONTROL5(BrowserPluginMsg_BuffersSwapped,
                      std::string /* mailbox_name */,
                      int /* route_id */,
                      int /* gpu_host_id */)
+
+IPC_MESSAGE_CONTROL4(BrowserPluginMsg_CompositorFrameSwapped,
+                     int /* instance_id */,
+                     cc::CompositorFrame /* frame */,
+                     int /* route_id */,
+                     int /* renderer_host_id */)
 
 // When the guest requests permission, the browser process forwards this
 // request to the embeddder through this message.
