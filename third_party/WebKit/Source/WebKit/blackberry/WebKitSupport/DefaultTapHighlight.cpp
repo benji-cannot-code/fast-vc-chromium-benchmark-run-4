bf64af39e2d197d8173f38293cfbb7d6042fd90c
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-02 05:48:18 +0000

    [BlackBerry] Move Path into it's own header https://bugs.webkit.org/show_bug.cgi?id=113724
    Patch by Konrad Piascik <kpiascik@blackberry.com> on 2013-04-01
    Reviewed by Rob Buis.
    
    Source/WebCore:
    
    No behavioural change.
    
    * platform/graphics/blackberry/PathBlackBerry.cpp:
    
    Source/WebKit/blackberry:
    
    * WebKitSupport/DefaultTapHighlight.cpp:
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147386 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DefaultTapHighlight.cpp
+++ third_party/WebKit/Source/WebKit/blackberry/WebKitSupport/DefaultTapHighlight.cpp
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <BlackBerryPlatformGraphicsContext.h>
 #include <BlackBerryPlatformMessageClient.h>
+#include <BlackBerryPlatformPath.h>
 
 using namespace WebCore;
 
