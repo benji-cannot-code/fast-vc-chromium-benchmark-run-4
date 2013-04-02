7019ec0a5d8cb1064e9716661bfe4f836d5ca893
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-02 18:16:36 +0000

    Web Inspector: memory instrumentation for external strings is broken https://bugs.webkit.org/show_bug.cgi?id=113788
    Source/WebCore:
    
    Reviewed by Pavel Feldman.
    
    Native memory snapshot now provides information about JS external strings.
    
    Test: inspector/profiler/memory-instrumentation-external-string.html
    
    * inspector/InspectorMemoryAgent.cpp:
    
    LayoutTests:
    
    Test that native memory snapshot contains information about extenral strings.
    
    Reviewed by Pavel Feldman.
    
    * inspector/profiler/memory-instrumentation-external-string-expected.txt: Added.
    * inspector/profiler/memory-instrumentation-external-string.html: Added.
    * platform/efl/TestExpectations:
    * platform/gtk/TestExpectations:
    * platform/mac/TestExpectations:
    * platform/qt/TestExpectations:
    * platform/win/TestExpectations:
    
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/InspectorMemoryAgent.cpp
+++ third_party/WebKit/Source/WebCore/inspector/InspectorMemoryAgent.cpp
@@ -161,7 +161,7 @@ public:
 private:
     virtual void visitJSExternalString(StringImpl* string)
     {
-        m_memoryClassInfo->addMember(string, "externalString");
+        m_memoryClassInfo->addMember(string, "externalString", WTF::RetainingPointer);
     }
 
     mutable MemoryClassInfo* m_memoryClassInfo;
