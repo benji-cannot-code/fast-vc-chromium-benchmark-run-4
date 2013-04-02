ba92ebb493fa4cdbe347067a04e97bea3f528fd5
gauravsh@chromium.org <gauravsh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 23:24:28 +0000

    NetworkChangeNotifierChromeos: Handle IPConfig property changes on the default network
    BUG=164501
    TEST=verify that dns refreshes while in connected state result in a dns change.
    
    
    Review URL: https://chromiumcodereview.appspot.com/12634019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191929 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/network_state.h
+++ chromeos/network/network_state.h
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROMEOS_NETWORK_NETWORK_STATE_H_
 #define CHROMEOS_NETWORK_NETWORK_STATE_H_
 
+#include <string>
+#include <vector>
+
 #include "chromeos/network/managed_state.h"
 
 namespace base {
@@ -37,6 +40,7 @@ class CHROMEOS_EXPORT NetworkState : public ManagedState {
   // Accessors
   const std::string& security() const { return security_; }
   const std::string& ip_address() const { return ip_address_; }
+  const std::vector<std::string>& dns_servers() const { return dns_servers_; }
   const std::string& device_path() const { return device_path_; }
   const std::string& guid() const { return guid_; }
   const std::string& connection_state() const { return connection_state_; }
@@ -62,6 +66,10 @@ class CHROMEOS_EXPORT NetworkState : public ManagedState {
   static bool StateIsConnected(const std::string& connection_state);
   static bool StateIsConnecting(const std::string& connection_state);
 
+  // Helper to return a full prefixed version of an IPConfig property
+  // key.
+  static std::string IPConfigProperty(const char* key);
+
  private:
   friend class NetworkStateHandler;
   friend class NetworkChangeNotifierChromeosUpdateTest;
@@ -70,17 +78,24 @@ class CHROMEOS_EXPORT NetworkState : public ManagedState {
   void set_ip_address(const std::string& ip_address) {
     ip_address_ = ip_address;
   }
+  void set_dns_servers(const std::vector<std::string>& dns_servers) {
+    dns_servers_ = dns_servers;
+  }
 
   // Common Network Service properties
   std::string security_;
   std::string device_path_;
   std::string guid_;
-  std::string ip_address_;
   std::string connection_state_;
   std::string error_;
   bool auto_connect_;
   bool favorite_;
   int priority_;
+  // IPConfig properties.
+  // Note: These do not correspond to actual Shill.Service properties
+  // but are derived from the service's corresponding IPConfig object.
+  std::string ip_address_;
+  std::vector<std::string> dns_servers_;
   // Wireless properties
   int signal_strength_;
   // Cellular properties
