d6263d6f3144798d613c7c82765d2c68051c94b4
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 01:33:04 +0000

    Move ImmersiveModeController into an ash only file to cut down on the number of ifdefs.
    Bug=180359
    Test=None
    
    Review URL: https://chromiumcodereview.appspot.com/12593017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191742 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/views/frame/immersive_mode_controller_stub.cc
@@ -0,0 +1,51 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/ui/views/frame/immersive_mode_controller_stub.h"
+
+#include "base/logging.h"
+
+ImmersiveModeControllerStub::ImmersiveModeControllerStub() {
+}
+
+ImmersiveModeControllerStub::~ImmersiveModeControllerStub() {
+}
+
+void ImmersiveModeControllerStub::Init(BrowserView* browser_view) {
+}
+
+void ImmersiveModeControllerStub::SetEnabled(bool enabled) {
+  NOTREACHED();
+}
+
+bool ImmersiveModeControllerStub::IsEnabled() const {
+  return false;
+}
+
+bool ImmersiveModeControllerStub::ShouldHideTabIndicators() const {
+  return false;
+}
+
+bool ImmersiveModeControllerStub::ShouldHideTopViews() const {
+  return false;
+}
+
+bool ImmersiveModeControllerStub::IsRevealed() const {
+  return false;
+}
+
+void ImmersiveModeControllerStub::MaybeStackViewAtTop() {
+}
+
+void ImmersiveModeControllerStub::MaybeStartReveal() {
+}
+
+void ImmersiveModeControllerStub::CancelReveal() {
+}
+
+ImmersiveModeController::RevealedLock*
+    ImmersiveModeControllerStub::GetRevealedLock() {
+  return NULL;
+}
--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1486,7 +1486,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/ui/views/frame/app_non_client_frame_view_ash_browsertest.cc',
         'browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc',
         'browser/ui/views/frame/browser_view_browsertest.cc',
-        'browser/ui/views/frame/immersive_mode_controller_browsertest.cc',
+        'browser/ui/views/frame/immersive_mode_controller_ash_browsertest.cc',
         'browser/ui/views/select_file_dialog_extension_browsertest.cc',
         'browser/ui/views/sync/one_click_signin_bubble_view_browsertest.cc',
         'browser/ui/views/toolbar_view_browsertest.cc',
@@ -1828,6 +1828,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'browser/ui/ash/shelf_browsertest.cc',
             'browser/ui/views/frame/app_non_client_frame_view_ash_browsertest.cc',
             'browser/ui/views/frame/browser_non_client_frame_view_ash_browsertest.cc',
+            'browser/ui/views/frame/immersive_mode_controller_ash_browsertest.cc',
           ],
         }, { # else: OS != "win"
           'sources!': [
