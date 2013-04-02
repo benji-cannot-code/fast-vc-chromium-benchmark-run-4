c7cbfd3da6898b60d2c068b34964457066c3c215
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-02 18:04:18 +0000

    Web Inspector: resource preview of html/js/css types should be on a grey background https://bugs.webkit.org/show_bug.cgi?id=113791
    Patch by Andrey Lushnikov <lushnikov@chromium.org> on 2013-04-02
    Reviewed by Pavel Feldman.
    
    No new tests.
    
    - Add a css rule to set grey background for SourceFrame in resource view.
    - Add "fill" style to SourceFrame element to setup its height.
    
    * inspector/front-end/SourceFrame.js:
    (WebInspector.SourceFrame):
    * inspector/front-end/resourceView.css:
    (.resource-view .script-view):
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147461 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/SourceFrame.js
@@ -38,6 +38,7 @@ WebInspector.SourceFrame = function(contentProvider)
 {
     WebInspector.View.call(this);
     this.element.addStyleClass("script-view");
+    this.element.addStyleClass("fill");
 
     this._url = contentProvider.contentURL();
     this._contentProvider = contentProvider;
