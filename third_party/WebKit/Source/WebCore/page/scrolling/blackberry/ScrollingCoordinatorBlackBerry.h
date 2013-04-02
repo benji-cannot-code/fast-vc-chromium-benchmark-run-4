95101619f6a2a770897f626e1058ed1cd9640b29
commit-queue@webkit.org <commit-queue@webkit.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-02 12:16:41 +0000

    [BlackBerry] Implement ScrollingCoordinator::frameViewLayoutUpdated() https://bugs.webkit.org/show_bug.cgi?id=113763
    Patch by Carlos Garcia Campos <cgarcia@igalia.com> on 2013-04-02
    Reviewed by Rob Buis.
    
    PR 318945
    Internally reviewed by Jakob Petsovits.
    
    And remove the BlackBerry specific methods
    frameViewFrameRectDidChange and frameViewContentsSizeDidChange.
    
    * page/scrolling/blackberry/ScrollingCoordinatorBlackBerry.cpp:
    (WebCore::ScrollingCoordinatorBlackBerry::frameViewLayoutUpdated):
    Called when the frame view has been laid out. Update the contents
    rectangle and visible area of the scroll layer for the given frame
    view.
    * page/scrolling/blackberry/ScrollingCoordinatorBlackBerry.h:
    (ScrollingCoordinatorBlackBerry):
    * platform/graphics/blackberry/LayerWebKitThread.h:
    (WebCore::LayerWebKitThread::setFrameVisibleRect): Return early if
    value hasn't changed.
    (WebCore::LayerWebKitThread::setFrameContentsSize): Ditto.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147418 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/scrolling/blackberry/ScrollingCoordinatorBlackBerry.h
+++ third_party/WebKit/Source/WebCore/page/scrolling/blackberry/ScrollingCoordinatorBlackBerry.h
@@ -35,6 +35,9 @@ class ScrollingCoordinatorBlackBerry : public ScrollingCoordinator {
 public:
     explicit ScrollingCoordinatorBlackBerry(Page*);
 
+    // Should be called whenever the given frame view has been laid out.
+    virtual void frameViewLayoutUpdated(FrameView*);
+
     // Return whether this scrolling coordinator can keep fixed position layers fixed to their
     // containers while scrolling.
     virtual bool supportsFixedPositionLayers() const { return true; }
@@ -47,9 +50,6 @@ public:
 
     // Whether the layer is fixed the top or bottom edge, left or right edge.
     void setLayerFixedToContainerLayerEdge(GraphicsLayer*, bool fixedToTop, bool fixedToLeft);
-
-    virtual void frameViewFrameRectDidChange(FrameView*);
-    virtual void frameViewContentsSizeDidChange(FrameView*);
 };
 
 } // namespace WebCore
