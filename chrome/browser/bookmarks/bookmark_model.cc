6f6bc655264579437be916c572239bc87481a6fe
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 19:49:47 +0000

    Remove GetApplicationLocale call in BookmarkModel, and just have it use ICU's default locale.
    BUG=225830
    Review URL: https://codereview.chromium.org/13461011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191879 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_model.cc
+++ chrome/browser/bookmarks/bookmark_model.cc
@@ -27,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/history/history_service_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/chrome_notification_types.h"
-#include "content/public/browser/content_browser_client.h"
 #include "content/public/browser/notification_details.h"
 #include "content/public/browser/notification_source.h"
 #include "grit/generated_resources.h"
@@ -630,10 +629,7 @@ void BookmarkModel::SortChildren(const BookmarkNode* parent) {
   }
 
   UErrorCode error = U_ZERO_ERROR;
-  icu::Locale application_locale(
-      content::GetContentClient()->browser()->GetApplicationLocale().c_str());
-  scoped_ptr<icu::Collator> collator(
-      icu::Collator::createInstance(application_locale, error));
+  scoped_ptr<icu::Collator> collator(icu::Collator::createInstance(error));
   if (U_FAILURE(error))
     collator.reset(NULL);
   BookmarkNode* mutable_parent = AsMutable(parent);
