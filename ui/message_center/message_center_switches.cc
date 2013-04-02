e5b9d6684b55249967f57a414fcb6f4a21a42e48
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 20:01:23 +0000

    Reactivate rich notifications by default except on Mac.
    BUG=223372
    
    
    Review URL: https://chromiumcodereview.appspot.com/13371003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191883 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center_switches.cc
+++ ui/message_center/message_center_switches.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace message_center {
 namespace switches {
 
+const char kDisableRichNotifications[] = "disable-rich-notifications";
 const char kEnableRichNotifications[] = "enable-rich-notifications";
 
 }  // namespace switches
