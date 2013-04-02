9b6a6dd040952a601d414644fe645030c9d1eadf
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 19:43:42 +0000

    Revert 191782 "Use StorageInfo in Mac"
    Hitting a NOTREACHED on startup.
    
    BUG=225813
    
    > Use StorageInfo in Mac
    > 
    > BUG=
    > 
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12643002
    
    TBR=gbillock@chromium.org
    Review URL: https://codereview.chromium.org/13461014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191877 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/storage_monitor/storage_monitor_mac.h
+++ chrome/browser/storage_monitor/storage_monitor_mac.h
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <map>
 
 #include "base/mac/scoped_cftyperef.h"
-#include "base/memory/weak_ptr.h"
+#include "base/memory/ref_counted.h"
+#include "chrome/browser/storage_monitor/disk_info_mac.h"
 #include "chrome/browser/storage_monitor/storage_monitor.h"
 
 namespace chrome {
@@ -19,8 +20,9 @@ class ImageCaptureDeviceManager;
 
 // This class posts notifications to listeners when a new disk
 // is attached, removed, or changed.
-class StorageMonitorMac : public StorageMonitor,
-                          public base::SupportsWeakPtr<StorageMonitorMac> {
+class StorageMonitorMac
+    : public StorageMonitor,
+      public base::RefCountedThreadSafe<StorageMonitorMac> {
  public:
   enum UpdateType {
     UPDATE_DEVICE_ADDED,
@@ -31,13 +33,9 @@ class StorageMonitorMac : public StorageMonitor,
   // Should only be called by browser start up code.  Use GetInstance() instead.
   StorageMonitorMac();
 
-  virtual ~StorageMonitorMac();
-
   void Init();
 
-  void UpdateDisk(const std::string& bsd_name,
-                  const StorageInfo& info,
-                  UpdateType update_type);
+  void UpdateDisk(const DiskInfoMac& info, UpdateType update_type);
 
   virtual bool GetStorageInfoForPath(
       const base::FilePath& path,
@@ -53,21 +51,24 @@ class StorageMonitorMac : public StorageMonitor,
       base::Callback<void(EjectStatus)> callback) OVERRIDE;
 
  private:
+  friend class base::RefCountedThreadSafe<StorageMonitorMac>;
+  virtual ~StorageMonitorMac();
+
   static void DiskAppearedCallback(DADiskRef disk, void* context);
   static void DiskDisappearedCallback(DADiskRef disk, void* context);
   static void DiskDescriptionChangedCallback(DADiskRef disk,
                                              CFArrayRef keys,
                                              void *context);
 
-  bool ShouldPostNotificationForDisk(const StorageInfo& info) const;
+  bool ShouldPostNotificationForDisk(const DiskInfoMac& info) const;
   bool FindDiskWithMountPoint(const base::FilePath& mount_point,
-                              StorageInfo* info) const;
+                              DiskInfoMac* info) const;
 
   base::mac::ScopedCFTypeRef<DASessionRef> session_;
   // Maps disk bsd names to disk info objects. This map tracks all mountable
-  // devices on the system, though only notifications for removable devices are
+  // devices on the system though only notifications for removable devices are
   // posted.
-  std::map<std::string, StorageInfo> disk_info_map_;
+  std::map<std::string, DiskInfoMac> disk_info_map_;
 
   scoped_ptr<chrome::ImageCaptureDeviceManager> image_capture_device_manager_;
 
