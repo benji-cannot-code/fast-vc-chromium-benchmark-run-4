89497342d1e7b58119fcfd26d60d732ee25761c6
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 00:57:16 +0000

    Fixes menu of each notification.
    - somehow it's disabled. enabled.
    - always show 'settings' when rich notification is enabled
    - opens the settings dialog instead of settings page when rich notification is enabled
    
    BUG=225052
    
    
    Review URL: https://chromiumcodereview.appspot.com/13379002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191731 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_view.cc
+++ ui/message_center/views/message_view.cc
@@ -225,7 +225,7 @@ MenuModel::MenuModel(message_center::NotificationChangeObserver* observer,
                                        display_source));
   }
   // Add settings menu item.
-  if (!display_source.empty()) {
+  if (message_center::IsRichNotificationEnabled() || !display_source.empty()) {
     AddItem(kShowSettingsCommand,
             l10n_util::GetStringUTF16(IDS_MESSAGE_CENTER_SETTINGS));
   }
@@ -243,7 +243,7 @@ bool MenuModel::IsCommandIdChecked(int command_id) const {
 }
 
 bool MenuModel::IsCommandIdEnabled(int command_id) const {
-  return false;
+  return true;
 }
 
 bool MenuModel::GetAcceleratorForCommandId(int command_id,
@@ -260,7 +260,10 @@ void MenuModel::ExecuteCommand(int command_id, int event_flags) {
       observer_->OnDisableNotificationsByUrl(notification_id_);
       break;
     case kShowSettingsCommand:
-      observer_->OnShowNotificationSettings(notification_id_);
+      if (message_center::IsRichNotificationEnabled())
+        observer_->OnShowNotificationSettingsDialog(NULL);
+      else
+        observer_->OnShowNotificationSettings(notification_id_);
       break;
     default:
       NOTREACHED();
