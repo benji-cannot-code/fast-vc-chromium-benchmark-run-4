5da3fef074336631c7e6123becc60a3e306404bd
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 04:08:40 +0000

    Don't reload the web-app at any point in the client-side flow.
    This improves responsiveness and also means that the v2 app now works for more
    then one connection without needing to be reloaded explicitly.
    
    BUG=134213
    
    
    Review URL: https://chromiumcodereview.appspot.com/13191012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191768 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/session_connector.js
+++ remoting/webapp/session_connector.js
@@ -39,11 +39,37 @@ remoting.SessionConnector = function(pluginParent, onOk, onError) {
   this.onError_ = onError;
 
   /**
+   * @type {string}
+   * @private
+   */
+  this.clientJid_ = '';
+
+  /**
    * @type {remoting.ClientSession.Mode}
    * @private
    */
   this.connectionMode_ = remoting.ClientSession.Mode.ME2ME;
 
+  /**
+   * A timer that polls for an updated access token.
+   *
+   * @type {number}
+   * @private
+   */
+  this.wcsAccessTokenRefreshTimer_ = 0;
+
+  // Initialize/declare per-connection state.
+  this.reset();
+
+  // Pre-load WCS to improve connection time.
+  remoting.identity.callWithToken(this.loadWcs_.bind(this), this.onError_);
+};
+
+/**
+ * Reset the per-connection state so that the object can be re-used for a
+ * second connection. Note the none of the shared WCS state is reset.
+ */
+remoting.SessionConnector.prototype.reset = function() {
   /**
    * String used to identify the host to which to connect. For IT2Me, this is
    * the first 7 digits of the access code; for Me2Me it is the host identifier.
@@ -75,12 +101,6 @@ remoting.SessionConnector = function(pluginParent, onOk, onError) {
   this.hostPublicKey_ = '';
 
   /**
-   * @type {string}
-   * @private
-   */
-  this.clientJid_ = '';
-
-  /**
    * @type {boolean}
    * @private
    */
@@ -99,16 +119,8 @@ remoting.SessionConnector = function(pluginParent, onOk, onError) {
   this.pendingXhr_ = null;
 
   /**
-   * A timer that polls for an updated access token.
-   *
-   * @type {number}
-   * @private
-   */
-  this.wcsAccessTokenRefreshTimer_ = 0;
-
-  /**
    * Function to interactively obtain the PIN from the user.
-   * @param {function(string):void} onPinFetched Called when the PIN is fetched.
+   * @type {function(function(string):void):void}
    * @private
    */
   this.fetchPin_ = function(onPinFetched) {};
@@ -122,9 +134,6 @@ remoting.SessionConnector = function(pluginParent, onOk, onError) {
    * @private
    */
   this.hostDisplayName_ = '';
-
-  // Pre-load WCS to improve connection time.
-  remoting.identity.callWithToken(this.loadWcs_.bind(this), this.onError_);
 };
 
 /**
@@ -136,10 +145,14 @@ remoting.SessionConnector = function(pluginParent, onOk, onError) {
  * @return {void} Nothing.
  */
 remoting.SessionConnector.prototype.connectMe2Me = function(host, fetchPin) {
+  // Cancel any existing connect operation.
+  this.cancel();
+
   this.hostId_ = host.hostId;
   this.hostJid_ = host.jabberId;
   this.fetchPin_ = fetchPin;
   this.hostDisplayName_ = host.hostName;
+  this.connectionMode_ = remoting.ClientSession.Mode.ME2ME;
   this.createSessionIfReady_();
 };
 
@@ -153,6 +166,9 @@ remoting.SessionConnector.prototype.connectIT2Me = function(accessCode) {
   var kSupportIdLen = 7;
   var kHostSecretLen = 5;
   var kAccessCodeLen = kSupportIdLen + kHostSecretLen;
+
+  // Cancel any existing connect operation.
+  this.cancel();
 
   var normalizedAccessCode = this.normalizeAccessCode_(accessCode);
   if (normalizedAccessCode.length != kAccessCodeLen) {
@@ -168,6 +184,19 @@ remoting.SessionConnector.prototype.connectIT2Me = function(accessCode) {
 };
 
 /**
+ * Reconnect a closed connection.
+ *
+ * @return {void} Nothing.
+ */
+remoting.SessionConnector.prototype.reconnect = function() {
+  if (this.connectionMode_ == remoting.ClientSession.Mode.IT2ME) {
+    console.error('reconnect not supported for IT2Me.');
+    return;
+  }
+  this.createSessionIfReady_();
+};
+
+/**
  * Cancel a connection-in-progress.
  */
 remoting.SessionConnector.prototype.cancel = function() {
@@ -179,11 +208,7 @@ remoting.SessionConnector.prototype.cancel = function() {
     this.pendingXhr_.abort();
     this.pendingXhr_ = null;
   }
-  this.hostId_ = '';
-  this.hostJid_ = '';
-  this.passPhrase_ = '';
-  this.hostPublicKey_ = '';
-  this.refreshHostJidIfOffline_ = true;
+  this.reset();
 };
 
 /**
