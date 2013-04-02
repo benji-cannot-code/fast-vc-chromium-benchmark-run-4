48b68c4ec5b8137364d97902793835402034c118
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 22:58:33 +0000

    Add an 'enableAppLauncher' function to the webstorePrivate API.
    This is to enable an app launcher promo on the webstore.
    
    R=benwells@chromium.org,guohui@chromium.org
    BUG=178936
    
    
    Review URL: https://chromiumcodereview.appspot.com/12908003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191921 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -70,6 +70,7 @@ void ExtensionFunctionRegistry::ResetFunctions() {
   RegisterFunction<extensions::InstallBundleFunction>();
   RegisterFunction<extensions::BeginInstallWithManifestFunction>();
   RegisterFunction<extensions::CompleteInstallFunction>();
+  RegisterFunction<extensions::EnableAppLauncherFunction>();
   RegisterFunction<extensions::GetWebGLStatusFunction>();
   RegisterFunction<extensions::GetIsLauncherEnabledFunction>();
 
