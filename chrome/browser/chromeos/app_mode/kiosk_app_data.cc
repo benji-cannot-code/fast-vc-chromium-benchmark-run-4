48b22d73cc196838bc9dc32ef269871bc0f71189
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-02 00:26:02 +0000

    app_mode: Check kiosk_enabled manifest.
    Enforce that only platform apps with "kiosk_enabled" could be added as kiosk
    apps and allowed to run in kiosk app mode.
    
    BUG=223177
    
    
    Review URL: https://chromiumcodereview.appspot.com/13030012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191718 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/app_mode/kiosk_app_data.cc
+++ chrome/browser/chromeos/app_mode/kiosk_app_data.cc
@@ -21,6 +21,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/image_decoder.h"
 #include "chrome/browser/prefs/scoped_user_pref_update.h"
 #include "chrome/common/extensions/extension_constants.h"
+#include "chrome/common/extensions/extension_manifest_constants.h"
+#include "chrome/common/extensions/manifest.h"
 #include "content/public/browser/browser_thread.h"
 #include "ui/gfx/codec/png_codec.h"
 
@@ -57,6 +59,17 @@ void SaveIconToLocalOnBlockingPool(
   CHECK_EQ(static_cast<int>(raw_icon->size()),
            file_util::WriteFile(icon_path,
                                 raw_icon->data().c_str(), raw_icon->size()));
+}
+
+// Returns true for valid kiosk app manifest.
+bool IsValidKioskAppManifest(const extensions::Manifest& manifest) {
+  bool kiosk_enabled;
+  if (manifest.GetBoolean(extension_manifest_keys::kKioskEnabled,
+                          &kiosk_enabled)) {
+    return kiosk_enabled;
+  }
+
+  return false;
 }
 
 }  // namespace
@@ -192,25 +205,37 @@ class KioskAppData::WebstoreDataParser
 
   virtual ~WebstoreDataParser() {}
 
+  void ReportFailure() {
+    if (client_)
+      client_->OnWebstoreParseFailure();
+
+    delete this;
+  }
+
   // WebstoreInstallHelper::Delegate overrides:
   virtual void OnWebstoreParseSuccess(
       const std::string& id,
       const SkBitmap& icon,
       base::DictionaryValue* parsed_manifest) OVERRIDE {
     // Takes ownership of |parsed_manifest|.
-    scoped_ptr<base::DictionaryValue> manifest(parsed_manifest);
+    extensions::Manifest manifest(
+        extensions::Manifest::INVALID_LOCATION,
+        scoped_ptr<base::DictionaryValue>(parsed_manifest));
+
+    if (!IsValidKioskAppManifest(manifest)) {
+      ReportFailure();
+      return;
+    }
 
     if (client_)
-      client_->OnWebstoreParseSuccess(icon, manifest.release());
+      client_->OnWebstoreParseSuccess(icon);
     delete this;
   }
   virtual void OnWebstoreParseFailure(
       const std::string& id,
       InstallHelperResultCode result_code,
       const std::string& error_message) OVERRIDE {
-    if (client_)
-      client_->OnWebstoreParseFailure();
-    delete this;
+    ReportFailure();
   }
 
   base::WeakPtr<KioskAppData> client_;
@@ -335,12 +360,7 @@ void KioskAppData::OnIconLoadFailure() {
   StartFetch();
 }
 
-void KioskAppData::OnWebstoreParseSuccess(
-    const SkBitmap& icon,
-    base::DictionaryValue* parsed_manifest) {
-  // Takes ownership of |parsed_manifest|.
-  scoped_ptr<base::DictionaryValue> manifest(parsed_manifest);
-
+void KioskAppData::OnWebstoreParseSuccess(const SkBitmap& icon) {
   icon_ = gfx::ImageSkia::CreateFrom1xBitmap(icon);
   icon_.MakeThreadSafe();
 
