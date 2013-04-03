8fd4648b395439b2089a0b707f66766789e3aaf4
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 04:42:47 +0000

    Add chrome.debugger.getTargets method to discover available debug targets.
    BUG=179342
    
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=191151
    
    Review URL: https://chromiumcodereview.appspot.com/12377047
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@191982 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/api_test/debugger/background.js
+++ chrome/test/data/extensions/api_test/debugger/background.js
@@ -10,6 +10,9 @@ var tabId;
 var debuggee;
 var protocolVersion = "1.0";
 
+var SILENT_FLAG_REQUIRED = "Cannot attach to this target unless " +
+    "'silent-debugger-extension-api' flag is enabled.";
+
 chrome.test.runTests([
 
   function attachMalformedVersion() {
@@ -107,10 +110,10 @@ chrome.test.runTests([
         fail("No tab with given id " + missingDebuggee.tabId + "."));
   },
 
-  function attachToExtensionWithNoSilentFlag() {
-    debuggeeExtension = {extensionId: "foo"};
+  function attachToOwnBackgroundPageWithNoSilentFlag() {
+    var ownExtensionId = chrome.extension.getURL('').split('/')[2];
+    debuggeeExtension = {extensionId: ownExtensionId};
     chrome.debugger.attach(debuggeeExtension, protocolVersion,
-        fail("Cannot attach to an extension unless " +
-            "'silent-debugger-extension-api' flag is enabled."));
+        fail(SILENT_FLAG_REQUIRED));
   }
 ]);
