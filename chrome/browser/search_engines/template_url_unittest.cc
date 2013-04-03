5d94e7c60edc88b800184ae50104fd279dad0e27
beaudoin@chromium.org <beaudoin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 17:33:27 +0000

    Ensure TemplateURLService::UpdateKeywordSearchTermsForURL takes alternate_urls into account.
    Changes UpdateKeywordSearchTermsForURL so that it uses TemplateURL::ExtractSearchTermsFromURL.
    
    This CL is part of the refactoring that ensures TemplateURLService relies on the new search terms extraction mechanism of TemplateURL.
    
    BUG=155373
    
    
    Review URL: https://chromiumcodereview.appspot.com/13485002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192083 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_unittest.cc
+++ chrome/browser/search_engines/template_url_unittest.cc
@@ -760,7 +760,6 @@ TEST_F(TemplateURLTest, ExtractSearchTermsFromURL) {
 
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
       GURL("http://google.com/?q=something"), &result));
-
   EXPECT_EQ(ASCIIToUTF16("something"), result);
 
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
@@ -789,11 +788,14 @@ TEST_F(TemplateURLTest, ExtractSearchTermsFromURL) {
 
   EXPECT_TRUE(url.ExtractSearchTermsFromURL(
       GURL("http://google.com/alt/#espv=0&q=something"), &result));
-
   EXPECT_EQ(ASCIIToUTF16("something"), result);
 
   EXPECT_FALSE(url.ExtractSearchTermsFromURL(
       GURL("http://google.ca/?q=something"), &result));
+  EXPECT_EQ(string16(), result);
+
+  EXPECT_FALSE(url.ExtractSearchTermsFromURL(
+      GURL("http://google.ca/?q=something&q=anything"), &result));
   EXPECT_EQ(string16(), result);
 
   EXPECT_FALSE(url.ExtractSearchTermsFromURL(
