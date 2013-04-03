4bbc2676ad3b5801a0cf724dc64dafd3e84439dd
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 12:15:34 +0000

    Add UMA to passphrase dialog.
    Count how often the passphrase dialog is displayed and closed,
    and whether the entered passphrase was correct.
    
    Related changelist which defines ManagedMode_PassphraseCorrect in the histograms.xml:
    https://chromereviews.googleplex.com/7312014/
    
    BUG=171382
    TEST=Manual
    
    Review URL: https://chromiumcodereview.appspot.com/12604013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192055 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/managed_user_passphrase_dialog.cc
+++ chrome/browser/ui/webui/managed_user_passphrase_dialog.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
+#include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
 #include "base/values.h"
 #include "chrome/browser/managed_mode/managed_user_passphrase.h"
@@ -17,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/constrained_web_dialog_ui.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
+#include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "content/public/browser/web_ui_message_handler.h"
@@ -24,6 +26,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/gfx/size.h"
+
+using content::UserMetricsAction;
 
 namespace {
 
@@ -51,7 +55,8 @@ class ManagedUserPassphraseDialogMessageHandler
 };
 
 ManagedUserPassphraseDialogMessageHandler
-    ::ManagedUserPassphraseDialogMessageHandler() : weak_factory_(this) {
+    ::ManagedUserPassphraseDialogMessageHandler()
+    : ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)) {
 }
 
 void ManagedUserPassphraseDialogMessageHandler::RegisterMessages() {
@@ -84,9 +89,10 @@ void ManagedUserPassphraseDialogMessageHandler::CheckPassphrase(
 
   // Check if the entered passphrase is correct and give the result back to the
   // UI.
-  base::FundamentalValue passphrase_correct(
-      stored_passphrase_hash == encoded_passphrase_hash);
+  bool is_correct = stored_passphrase_hash == encoded_passphrase_hash;
+  base::FundamentalValue passphrase_correct(is_correct);
   web_ui()->CallJavascriptFunction("passphraseResult", passphrase_correct);
+  UMA_HISTOGRAM_BOOLEAN("ManagedMode_PassphraseCorrect", is_correct);
 }
 
 }  // namespace
@@ -94,6 +100,7 @@ void ManagedUserPassphraseDialogMessageHandler::CheckPassphrase(
 ManagedUserPassphraseDialog::ManagedUserPassphraseDialog(
     content::WebContents* web_contents,
     const PassphraseCheckedCallback& callback) : callback_(callback) {
+  content::RecordAction(UserMetricsAction("ManagedMode_OpenPassphraseDialog"));
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
   CreateDataSource(profile);
@@ -136,6 +143,8 @@ void ManagedUserPassphraseDialog::OnDialogClosed(
   if (!callback_.is_null()) {
     callback_.Run(!json_retval.empty());
     callback_.Reset();
+    content::RecordAction(
+        UserMetricsAction("ManagedMode_ClosePassphraseDialog"));
   }
 }
 
