1bcd4082dc887fa114d60053d171c3d7a0fb4e39
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 19:50:47 +0000

    Non-web-accessible extension URLs should not load in non-extension processes
    This is a slightly modified version of my previous CL: https://codereview.chromium.org/12218064/.
    The only difference is that we allow any resource request to succeed, if the extension has any web_acessible_resources. The reason for that we have been lax and allowed subresource loads, even if they are not explicitly added to the manifest (see crbug.com/179127 for details). This should be tightened up with a v3 manifest requirement to explicitly list all subresources.
    
    BUG=173688
    
    
    Review URL: https://chromiumcodereview.appspot.com/12457042
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192121 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/resource_request_policy.cc
+++ chrome/renderer/extensions/resource_request_policy.cc
@@ -25,6 +25,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace extensions {
 
+// This method does a security check whether chrome-extension:// URLs can be
+// requested by the renderer. Since this is in an untrusted process, the browser
+// has a similar check to enforce the policy, in case this process is exploited.
+// If you are changing this function, ensure equivalent checks are added to
+// extension_protocols.cc's AllowExtensionResourceLoad.
+
 // static
 bool ResourceRequestPolicy::CanRequestResource(
     const GURL& resource_url,
