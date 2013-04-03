67ce759870b96b6cf4fe500a3f7b12ab02a4e33f
mkwst@chromium.org <mkwst@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 11:03:53 +0000

    Temporarily disable 'X-Frame-Options' tests, as the behavior is changing.
    The behavior of 'X-Frame-Options' is changing in wkbug.com/112903 to make the
    frame behave as though it's cross-origin from its parent. This change breaks
    two tests which rely on being able to reach down into the frame to read its
    location.
    
    This patch disables ExtensionWebstorePrivateApiTest.FrameWebstorePageBlocked
    and ExtensionWebstorePrivateApiTest.FrameErrorPageBlocked. I'll rewrite and
    reenable them once the change rolls into Chromium.
    
    BUG=226018
    
    
    Review URL: https://chromiumcodereview.appspot.com/13517003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192049 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/webstore_private/webstore_private_apitest.cc
+++ chrome/browser/extensions/api/webstore_private/webstore_private_apitest.cc
@@ -257,8 +257,10 @@ class ExtensionWebstoreGetWebGLStatusTest : public InProcessBrowserTest {
 };
 
 // Test cases for webstore origin frame blocking.
+// TODO(mkwst): Disabled until new X-Frame-Options behavior rolls into
+// Chromium, see crbug.com/226018.
 IN_PROC_BROWSER_TEST_F(ExtensionWebstorePrivateApiTest,
-                       FrameWebstorePageBlocked) {
+                       DISABLED_FrameWebstorePageBlocked) {
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   string16 expected_title = UTF8ToUTF16("PASS: about:blank");
@@ -272,8 +274,10 @@ IN_PROC_BROWSER_TEST_F(ExtensionWebstorePrivateApiTest,
   EXPECT_EQ(expected_title, final_title);
 }
 
+// TODO(mkwst): Disabled until new X-Frame-Options behavior rolls into
+// Chromium, see crbug.com/226018.
 IN_PROC_BROWSER_TEST_F(ExtensionWebstorePrivateApiTest,
-                       FrameErrorPageBlocked) {
+                       DISABLED_FrameErrorPageBlocked) {
   content::WebContents* contents =
       browser()->tab_strip_model()->GetActiveWebContents();
   string16 expected_title = UTF8ToUTF16("PASS: about:blank");
