6cb095c5124c77e0bc799715530902f36ca23ad6
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 20:01:50 +0000

    Aura: Make LayerWithRealCompositorTest work with --ui-enable-threaded-compositing
    Plumb through a draw completed signal from ui::Compositor. Then wait for a draw
    to complete rather than wait for pending tasks (a draw on the same thread when
    single threaded) to finish.
    
    BUG=173417
    TEST=compositor_unittests  --gtest_filter="LayerWithRealCompositorTest*" --ui-enable-threaded-compositing
    
    
    Review URL: https://chromiumcodereview.appspot.com/13539005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192126 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/test/test_suite.cc
+++ ui/compositor/test/test_suite.cc
@@ -5,12 +5,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/compositor/test/test_suite.h"
 
+#include "base/command_line.h"
 #include "base/message_loop.h"
 #include "ui/base/ui_base_paths.h"
 #include "ui/compositor/compositor.h"
+#include "ui/compositor/compositor_switches.h"
 #include "ui/compositor/test/compositor_test_support.h"
 #include "ui/gfx/gfx_paths.h"
 #include "ui/gl/gl_implementation.h"
+
+#if defined(USE_X11)
+#include <X11/Xlib.h>
+#endif
 
 namespace ui {
 namespace test {
@@ -21,6 +27,12 @@ CompositorTestSuite::CompositorTestSuite(int argc, char** argv)
 CompositorTestSuite::~CompositorTestSuite() {}
 
 void CompositorTestSuite::Initialize() {
+  bool use_threaded_compositing = CommandLine::ForCurrentProcess()->
+      HasSwitch(switches::kUIEnableThreadedCompositing);
+#if defined(USE_X11)
+  if (use_threaded_compositing)
+    XInitThreads();
+#endif
 #if defined(OS_LINUX)
   gfx::InitializeGLBindings(gfx::kGLImplementationOSMesaGL);
 #endif
@@ -30,7 +42,7 @@ void CompositorTestSuite::Initialize() {
 
   message_loop_.reset(new MessageLoop(MessageLoop::TYPE_UI));
   CompositorTestSupport::Initialize();
-  Compositor::Initialize(false);
+  Compositor::Initialize(use_threaded_compositing);
 }
 
 void CompositorTestSuite::Shutdown() {
