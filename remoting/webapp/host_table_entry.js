7a2bbf4f016f67e0dd439626761354f749234f4e
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 07:46:34 +0000

    Restore clickable status after canceling host rename.
    BUG=224680
    
    
    Review URL: https://chromiumcodereview.appspot.com/13416003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192015 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_table_entry.js
+++ remoting/webapp/host_table_entry.js
@@ -237,8 +237,6 @@ remoting.HostTableEntry.prototype.commitRename_ = function() {
       this.host.hostName = editBox.value;
       this.onRename_(this);
     }
-    // Update the tool-top and event handler.
-    this.updateStatus();
     this.removeEditBox_();
   }
 };
@@ -307,6 +305,8 @@ remoting.HostTableEntry.prototype.removeEditBox_ = function() {
     // onblur will fire when the edit box is removed, so remove the hook.
     editBox.removeEventListener('blur', this.onBlurReference_, false);
   }
+  // Update the tool-top and event handler.
+  this.updateStatus();
   this.setHostName_();
 };
 
