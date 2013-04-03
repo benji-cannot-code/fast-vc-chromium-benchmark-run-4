b93641d043fbded021c4ae0c93ed431f2bdda395
sky@chromium.org <sky@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 23:00:52 +0000

    Makes table iterate/select arbitrary rows. When a TableGrouper is present I had made it so that clicking a row selects the first row in the group. This resulted in double clicking effecting the first row of the group, which was not necessarily the row that was clicked on. I've now made it so that the row you clicked on is made the active row. Additionally I made it so that the keyboard accelerators (up/down...) activate all rows. Lastly I made it so we draw a focus rect around the active row.
    BUG=225290
    TEST=covered by tests.
    R=sadrul@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/13460016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192182 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/table/table_view_unittest.cc
+++ ui/views/controls/table/table_view_unittest.cc
@@ -534,6 +534,10 @@ TEST_F(TableViewTest, KeyUpDown) {
 
   PressKey(ui::VKEY_DOWN);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=1 anchor=1 selection=0 1", SelectionStateAsString());
+
+  PressKey(ui::VKEY_DOWN);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
   EXPECT_EQ("active=2 anchor=2 selection=2", SelectionStateAsString());
 
   PressKey(ui::VKEY_DOWN);
@@ -541,12 +545,24 @@ TEST_F(TableViewTest, KeyUpDown) {
   EXPECT_EQ("active=3 anchor=3 selection=3 4", SelectionStateAsString());
 
   PressKey(ui::VKEY_DOWN);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
+
+  PressKey(ui::VKEY_DOWN);
   EXPECT_EQ(0, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
+
+  PressKey(ui::VKEY_UP);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
   EXPECT_EQ("active=3 anchor=3 selection=3 4", SelectionStateAsString());
 
   PressKey(ui::VKEY_UP);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
   EXPECT_EQ("active=2 anchor=2 selection=2", SelectionStateAsString());
+
+  PressKey(ui::VKEY_UP);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=1 anchor=1 selection=0 1", SelectionStateAsString());
 
   PressKey(ui::VKEY_UP);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
@@ -582,11 +598,27 @@ TEST_F(TableViewTest, KeyUpDown) {
 
   PressKey(ui::VKEY_DOWN);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
+
+  PressKey(ui::VKEY_DOWN);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
   EXPECT_EQ("active=0 anchor=0 selection=0 1", SelectionStateAsString());
 
   PressKey(ui::VKEY_DOWN);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=1 anchor=1 selection=0 1", SelectionStateAsString());
+
+  PressKey(ui::VKEY_DOWN);
   EXPECT_EQ(0, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=1 anchor=1 selection=0 1", SelectionStateAsString());
+
+  PressKey(ui::VKEY_UP);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
   EXPECT_EQ("active=0 anchor=0 selection=0 1", SelectionStateAsString());
+
+  PressKey(ui::VKEY_UP);
+  EXPECT_EQ(1, observer.GetChangedCountAndClear());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
 
   PressKey(ui::VKEY_UP);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
@@ -632,7 +664,7 @@ TEST_F(TableViewTest, HomeEnd) {
 
   PressKey(ui::VKEY_END);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
-  EXPECT_EQ("active=3 anchor=3 selection=3 4", SelectionStateAsString());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
 
   table_->SetObserver(NULL);
 }
@@ -668,12 +700,12 @@ TEST_F(TableViewTest, Multiselection) {
   // Click on the last row, should select it and the row before it.
   ClickOnRow(4, 0);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
-  EXPECT_EQ("active=3 anchor=3 selection=3 4", SelectionStateAsString());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
 
   // Shift click on the third row, should extend selection to it.
   ClickOnRow(2, ui::EF_SHIFT_DOWN);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
-  EXPECT_EQ("active=2 anchor=3 selection=2 3 4", SelectionStateAsString());
+  EXPECT_EQ("active=2 anchor=4 selection=2 3 4", SelectionStateAsString());
 
   // Control click on third row, should toggle it.
   ClickOnRow(2, ui::EF_CONTROL_DOWN);
@@ -688,7 +720,7 @@ TEST_F(TableViewTest, Multiselection) {
   // Click on last row again.
   ClickOnRow(4, 0);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
-  EXPECT_EQ("active=3 anchor=3 selection=3 4", SelectionStateAsString());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
 
   table_->SetObserver(NULL);
 }
@@ -728,12 +760,12 @@ TEST_F(TableViewTest, MultiselectionWithSort) {
   // Click on the third row, should select it and the second row.
   ClickOnRow(2, 0);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
-  EXPECT_EQ("active=3 anchor=3 selection=3 4", SelectionStateAsString());
+  EXPECT_EQ("active=4 anchor=4 selection=3 4", SelectionStateAsString());
 
   // Extend selection to first row.
   ClickOnRow(0, ui::EF_SHIFT_DOWN);
   EXPECT_EQ(1, observer.GetChangedCountAndClear());
-  EXPECT_EQ("active=2 anchor=3 selection=2 3 4", SelectionStateAsString());
+  EXPECT_EQ("active=2 anchor=4 selection=2 3 4", SelectionStateAsString());
 
   table_->SetObserver(NULL);
 }
