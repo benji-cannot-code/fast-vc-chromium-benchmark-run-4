ae383c9f3a9b4691e2642888493e47a1165bfa3e
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-03 23:00:20 +0000

    Revert issue 158379
    This reverts crbug.com/158379 which changed the apps v1 window creation behavior from outer bounds to inner bounds. That change broke "expected" behavior for a class of extensions, and made the behavior inconsistent between Create and Get/Update. There was some internal discussion about what expected behavior should be, and we decided to revert the behavior.
    
    A test was added for consistency between Create and Window properties, and the documentation was clarified.
    
    BUG=173831
    
    Review URL: https://codereview.chromium.org/13375002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192181 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/window_open/window_size/test.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function openSizedWindow() {
+    chrome.windows.create(
+      // Note: width and height must be larger than the minimum window size
+      // and smaller than the max (screen) size.
+      { 'url': chrome.extension.getURL('popup.html'), 'type': 'popup',
+        'width': 200, 'height': 200 },
+      chrome.test.callbackPass(function(win) {
+        chrome.test.assertEq(200, win.width);
+        chrome.test.assertEq(200, win.height);
+      }));
+  }
+]);
