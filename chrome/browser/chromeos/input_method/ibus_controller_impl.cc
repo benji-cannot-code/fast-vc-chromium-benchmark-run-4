71629a818394c60bd622f33430023a6aee5189c8
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-04 11:17:34 +0000

    Support Component Extension IME
    Component Extension IME is basically treated as system input method.
    
    BUG=164377
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/13527002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192293 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/ibus_controller_impl.cc
+++ chrome/browser/chromeos/input_method/ibus_controller_impl.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/ibus/ibus_input_context_client.h"
 #include "chromeos/dbus/ibus/ibus_panel_service.h"
 #include "chromeos/dbus/ibus/ibus_property.h"
+#include "chromeos/ime/component_extension_ime_manager.h"
 #include "chromeos/ime/extension_ime_util.h"
 #include "chromeos/ime/input_method_config.h"
 #include "chromeos/ime/input_method_property.h"
@@ -202,7 +203,8 @@ bool IBusControllerImpl::ChangeInputMethod(const std::string& id) {
   // Sanity checks.
   DCHECK(!InputMethodUtil::IsKeyboardLayout(id));
   if (!whitelist_.InputMethodIdIsWhitelisted(id) &&
-      !extension_ime_util::IsExtensionIME(id))
+      !extension_ime_util::IsExtensionIME(id) &&
+      !ComponentExtensionIMEManager::IsComponentExtensionIMEId(id))
     return false;
 
   // Clear input method properties unconditionally if |id| is not equal to
