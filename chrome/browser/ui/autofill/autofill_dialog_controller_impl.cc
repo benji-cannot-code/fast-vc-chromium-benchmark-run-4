c2cc67ce53833a38678a789ebc38c90b9f427cb2
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-04 18:45:46 +0000

    requestAutocomplete: HidePopup() on Submit.
    the popup hides whenever the input loses focus, or if you press escape, if the data models change, and for a slew of other reasons. This is just one more case where it needs to hide.
    
    BUG=225450
    
    Review URL: https://codereview.chromium.org/13165016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192383 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -366,7 +366,7 @@ void AutofillDialogControllerImpl::Show() {
               arraysize(kShippingInputs),
               &requested_shipping_fields_);
 
-  GenerateSuggestionsModels();
+  SuggestionsUpdated();
 
   // TODO(estade): don't show the dialog if the site didn't specify the right
   // fields. First we must figure out what the "right" fields are.
@@ -569,8 +569,7 @@ void AutofillDialogControllerImpl::OnWalletOrSigninUpdate() {
     }
   }
 
-  GenerateSuggestionsModels();
-  view_->ModelChanged();
+  SuggestionsUpdated();
   view_->UpdateAccountChooser();
   view_->UpdateNotificationArea();
 
@@ -1348,8 +1347,7 @@ void AutofillDialogControllerImpl::OnDidGetFullWallet(
     return;
   }
 
-  GenerateSuggestionsModels();
-  view_->ModelChanged();
+  SuggestionsUpdated();
   view_->UpdateNotificationArea();
   view_->UpdateButtonStrip();
 }
@@ -1472,9 +1470,7 @@ void AutofillDialogControllerImpl::OnNetworkError(int response_code) {
 // PersonalDataManagerObserver implementation.
 
 void AutofillDialogControllerImpl::OnPersonalDataChanged() {
-  HidePopup();
-  GenerateSuggestionsModels();
-  view_->ModelChanged();
+  SuggestionsUpdated();
 }
 
 void AutofillDialogControllerImpl::AccountChoiceChanged() {
@@ -1495,8 +1491,7 @@ void AutofillDialogControllerImpl::AccountChoiceChanged() {
   if (account_chooser_model_.WalletIsSelected())
     StartFetchingWalletItems();
 
-  GenerateSuggestionsModels();
-  view_->ModelChanged();
+  SuggestionsUpdated();
   view_->UpdateAccountChooser();
   view_->UpdateNotificationArea();
 }
@@ -1603,12 +1598,13 @@ bool AutofillDialogControllerImpl::IsFirstRun() const {
   return !prefs->HasPrefPath(prefs::kAutofillDialogPayWithoutWallet);
 }
 
-void AutofillDialogControllerImpl::GenerateSuggestionsModels() {
+void AutofillDialogControllerImpl::SuggestionsUpdated() {
   suggested_email_.Reset();
   suggested_cc_.Reset();
   suggested_billing_.Reset();
   suggested_cc_billing_.Reset();
   suggested_shipping_.Reset();
+  HidePopup();
 
   if (IsPayingWithWallet()) {
     // TODO(estade): fill in the email address.
@@ -1687,6 +1683,9 @@ void AutofillDialogControllerImpl::GenerateSuggestionsModels() {
   suggested_shipping_.AddKeyedItem(
       std::string(),
       l10n_util::GetStringUTF16(IDS_AUTOFILL_DIALOG_ADD_SHIPPING_ADDRESS));
+
+  if (view_)
+    view_->ModelChanged();
 }
 
 bool AutofillDialogControllerImpl::IsCompleteProfile(
