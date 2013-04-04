742b5ac9b459241b97ab33e407b19d79d2cb317d
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-04 23:45:33 +0000

    blink: Clean up a few #ifdefs from platform/graphics headers.
    No intended behavior change.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/13647016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147546 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/FloatPoint.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/FloatPoint.h
@@ -32,19 +32,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "IntPoint.h"
 #include <wtf/MathExtras.h>
 
-#if PLATFORM(BLACKBERRY)
-namespace BlackBerry {
-namespace Platform {
-class FloatPoint;
-}
-}
-#endif
-
-#if USE(CG) || USE(SKIA_ON_MAC_CHROMIUM)
+#if OS(DARWIN)
 typedef struct CGPoint CGPoint;
-#endif
 
-#if PLATFORM(MAC) || (PLATFORM(CHROMIUM) && OS(DARWIN))
 #ifdef NSGEOMETRY_TYPES_SAME_AS_CGGEOMETRY_TYPES
 typedef struct CGPoint NSPoint;
 #else
@@ -52,16 +42,7 @@ typedef struct _NSPoint NSPoint;
 #endif
 #endif
 
-#if PLATFORM(QT)
-#include "qglobal.h"
-QT_BEGIN_NAMESPACE
-class QPointF;
-QT_END_NAMESPACE
-#endif
-
-#if USE(SKIA)
 struct SkPoint;
-#endif
 
 namespace WebCore {
 
@@ -151,31 +132,17 @@ public:
         return FloatPoint(m_y, m_x);
     }
 
-#if USE(CG) || USE(SKIA_ON_MAC_CHROMIUM)
+#if OS(DARWIN)
     FloatPoint(const CGPoint&);
     operator CGPoint() const;
-#endif
-
-#if (PLATFORM(MAC) || (PLATFORM(CHROMIUM) && OS(DARWIN))) \
-        && !defined(NSGEOMETRY_TYPES_SAME_AS_CGGEOMETRY_TYPES)
+#if !defined(NSGEOMETRY_TYPES_SAME_AS_CGGEOMETRY_TYPES)
     FloatPoint(const NSPoint&);
     operator NSPoint() const;
 #endif
-
-#if PLATFORM(QT)
-    FloatPoint(const QPointF&);
-    operator QPointF() const;
 #endif
 
-#if PLATFORM(BLACKBERRY)
-    FloatPoint(const BlackBerry::Platform::FloatPoint&);
-    operator BlackBerry::Platform::FloatPoint() const;
-#endif
-
-#if USE(SKIA)
     operator SkPoint() const;
     FloatPoint(const SkPoint&);
-#endif
 
     FloatPoint matrixTransform(const TransformationMatrix&) const;
     FloatPoint matrixTransform(const AffineTransform&) const;
