75db9ace4cd2c324b9009e17a3acaf0fc34450d0
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-04 11:37:25 +0000

    Cleanup: A few more IWYU fixes.
    Review URL: https://chromiumcodereview.appspot.com/13582004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192300 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/omnibox/omnibox_view_mac_unittest.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_view_mac_unittest.mm
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/toolbar/toolbar_model_delegate.h"
 #include "chrome/browser/ui/toolbar/toolbar_model_impl.h"
 #include "testing/platform_test.h"
+#include "ui/gfx/image/image.h"
 
 namespace {
 
