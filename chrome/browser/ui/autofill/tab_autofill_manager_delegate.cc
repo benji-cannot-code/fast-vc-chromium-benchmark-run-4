aaeec54c24929cff55145e6e1412ecaca2552ff5
ahutter@chromium.org <ahutter@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-04 10:45:41 +0000

    Hide Autofill dialog on navigations if Autocheckout isn't running
    BUG= 225949
    
    
    Review URL: https://chromiumcodereview.appspot.com/13491002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192279 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/tab_autofill_manager_delegate.cc
+++ chrome/browser/ui/autofill/tab_autofill_manager_delegate.cc
@@ -217,8 +217,9 @@ void TabAutofillManagerDelegate::DidNavigateMainFrame(
     const content::LoadCommittedDetails& details,
     const content::FrameNavigateParams& params) {
   if (dialog_controller_ &&
-      dialog_controller_->dialog_type() ==
-          autofill::DIALOG_TYPE_REQUEST_AUTOCOMPLETE) {
+      (dialog_controller_->dialog_type() ==
+          autofill::DIALOG_TYPE_REQUEST_AUTOCOMPLETE ||
+       !dialog_controller_->AutocheckoutIsRunning())) {
     HideRequestAutocompleteDialog();
   }
 
