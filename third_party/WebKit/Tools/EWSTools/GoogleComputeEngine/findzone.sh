f0c0880bbe2e394664f29f6882713846db005def
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-05 08:12:51 +0000

    Remove unused tools
    This CL is the result of running clean-08-tools.py from pyclean.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147596 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/EWSTools/GoogleComputeEngine/findzone.sh
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-if [[ $# -ne 1 ]]; then
-    echo "Usage: findzone.sh PROJECT"
-fi
-
-echo $(gcutil --project=$1 listzones | grep UP | awk '{print $2}' | sort | tail -1)
