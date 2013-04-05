85134494512b56c3b93a1591f283620bb84380bd
jeremya@chromium.org <jeremya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 23:27:21 +0000

    [mac] Make app shims look in the correct user data dir on Canary.
    On Canary, the user data directory is affected by
    [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CrProductDirName"].
    Unfortunately in app shims, [NSBundle mainBundle] refers to the app shim's
    bundle, not Chrome's bundle. This patch parameterises the
    user-data-directory-figuring-out code on the NSBundle, so that app shims can
    pass an NSBundle for the real Chrome bundle.
    
    R=thakis@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/12663023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192644 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_main_app_mode_mac.mm
+++ chrome/app/chrome_main_app_mode_mac.mm
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/threading/thread.h"
 #include "chrome/common/chrome_paths.h"
+#include "chrome/common/chrome_paths_internal.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/mac/app_mode_common.h"
 #include "ipc/ipc_channel_proxy.h"
@@ -69,11 +70,16 @@ AppShimController::AppShimController() : channel_(NULL) {
 
 void AppShimController::Init() {
   DCHECK(g_io_thread);
+  NSString* chrome_bundle_path =
+      base::SysUTF8ToNSString(g_info->chrome_outer_bundle_path.value());
+  NSBundle* chrome_bundle = [NSBundle bundleWithPath:chrome_bundle_path];
   base::FilePath user_data_dir;
-  if (!PathService::Get(chrome::DIR_USER_DATA, &user_data_dir)) {
+  if (!chrome::GetUserDataDirectoryForBrowserBundle(chrome_bundle,
+                                                    &user_data_dir)) {
     Quit();
     return;
   }
+
   base::FilePath socket_path =
       user_data_dir.Append(app_mode::kAppShimSocketName);
   IPC::ChannelHandle handle(socket_path.value());
