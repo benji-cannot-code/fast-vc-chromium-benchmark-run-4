f2549841da94ba90de8ee3eb29a4dc00d005f041
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 05:58:32 +0000

    Minor cleanups in MediaStreamCaptureIndicator.
    Fixed IsCapturingUserMedia() and IsBeingMirrored() to use WebContents pointer
    to identify tabs.
    
    
    Review URL: https://chromiumcodereview.appspot.com/13355003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192512 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/tabs/tab_utils.cc
+++ chrome/browser/ui/tabs/tab_utils.cc
@@ -15,25 +15,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chrome {
 
 bool ShouldShowProjectingIndicator(content::WebContents* contents) {
-  int render_process_id = contents->GetRenderProcessHost()->GetID();
-  int render_view_id = contents->GetRenderViewHost()->GetRoutingID();
   scoped_refptr<MediaStreamCaptureIndicator> indicator =
       MediaCaptureDevicesDispatcher::GetInstance()->
           GetMediaStreamCaptureIndicator();
-  return indicator->IsBeingMirrored(render_process_id, render_view_id);
+  return indicator->IsBeingMirrored(contents);
 }
 
 bool ShouldShowRecordingIndicator(content::WebContents* contents) {
-  int render_process_id = contents->GetRenderProcessHost()->GetID();
-  int render_view_id = contents->GetRenderViewHost()->GetRoutingID();
   scoped_refptr<MediaStreamCaptureIndicator> indicator =
       MediaCaptureDevicesDispatcher::GetInstance()->
           GetMediaStreamCaptureIndicator();
   // The projecting indicator takes precedence over the recording indicator, but
   // if we are projecting and we don't handle the projecting case we want to
   // still show the recording indicator.
-  return indicator->IsCapturingUserMedia(render_process_id, render_view_id) ||
-         indicator->IsBeingMirrored(render_process_id, render_view_id);
+  return indicator->IsCapturingUserMedia(contents) ||
+         indicator->IsBeingMirrored(contents);
 }
 
 bool IsPlayingAudio(content::WebContents* contents) {
