dd1dff3b5745fd51d7ffe411589435541c1c6994
pasko@google.com <pasko@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 13:27:09 +0000

    Test cache creation retry via public interface only.
    This is a cleanup after:
    https://codereview.chromium.org/12794003/
    
    One test for backend force retry is enough, the other test
    (CreateBackend_MissingFile) is converted to be backend implementation specific.
    
    The CreateCacheViaPublicInterface() is called only once, but to eliminate it
    there is a need to make the cache_thread_ protected. Also, I am sure the name is
    not ideal, please suggest another.
    
    The New Eviction allows not to be very strict about version comparison as stored
    in the files on disk, this allows creating the BackendImpl with no flags and
    then being able to pass the common integrity check.
    
    BUG=173384
    
    
    Review URL: https://chromiumcodereview.appspot.com/13517004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192558 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/cache_creator.cc
+++ net/disk_cache/cache_creator.cc
@@ -12,7 +12,44 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/mem_backend_impl.h"
 #include "net/disk_cache/simple/simple_backend_impl.h"
 
-namespace disk_cache {
+namespace {
+
+// Builds an instance of the backend depending on platform, type, experiments
+// etc. Takes care of the retry state. This object will self-destroy when
+// finished.
+class CacheCreator {
+ public:
+  CacheCreator(const base::FilePath& path, bool force, int max_bytes,
+               net::CacheType type, uint32 flags,
+               base::MessageLoopProxy* thread, net::NetLog* net_log,
+               disk_cache::Backend** backend,
+               const net::CompletionCallback& callback);
+
+  // Creates the backend.
+  int Run();
+
+ private:
+  ~CacheCreator();
+
+  void DoCallback(int result);
+
+  void OnIOComplete(int result);
+
+  const base::FilePath& path_;
+  bool force_;
+  bool retry_;
+  int max_bytes_;
+  net::CacheType type_;
+  uint32 flags_;
+  scoped_refptr<base::MessageLoopProxy> thread_;
+  disk_cache::Backend** backend_;
+  net::CompletionCallback callback_;
+  disk_cache::Backend* created_cache_;
+  net::NetLog* net_log_;
+  bool use_simple_cache_backend_;
+
+  DISALLOW_COPY_AND_ASSIGN(CacheCreator);
+};
 
 CacheCreator::CacheCreator(
     const base::FilePath& path, bool force, int max_bytes,
@@ -103,6 +140,10 @@ void CacheCreator::OnIOComplete(int result) {
   int rv = Run();
   DCHECK_EQ(net::ERR_IO_PENDING, rv);
 }
+
+}  // namespace
+
+namespace disk_cache {
 
 int CreateCacheBackend(net::CacheType type, const base::FilePath& path,
                        int max_bytes,
