28debb7a3216714ca5fb1d33bc1963d0d6b02644
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-05 21:04:19 +0000

    Remove dead methods in EventHandler for mouse/drag events.
    These were only used by the Mac port and always returned false on other
    platforms.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13674019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147671 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/EventHandler.h
+++ third_party/WebKit/Source/WebCore/page/EventHandler.h
@@ -332,11 +332,6 @@ private:
 
     DragSourceAction updateDragSourceActionsAllowed() const;
 
-    // The following are called at the beginning of handleMouseUp and handleDrag.  
-    // If they return true it indicates that they have consumed the event.
-    bool eventLoopHandleMouseUp(const MouseEventWithHitTestResults&);
-    bool eventLoopHandleMouseDragged(const MouseEventWithHitTestResults&);
-
     void updateSelectionForMouseDrag(const HitTestResult&);
 
     void updateLastScrollbarUnderMouse(Scrollbar*, bool);
