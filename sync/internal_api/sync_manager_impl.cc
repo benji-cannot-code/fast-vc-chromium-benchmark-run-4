c0235eae906709ce9c83444860a6bdb43edd4ee0
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 04:34:03 +0000

    [Sync] Fix favicon updates to handle orphan nodes
    Image and tracking updates are now tracked separately, so that we properly
    distinguish between adds/updates.
    
    BUG=226539
    
    
    Review URL: https://chromiumcodereview.appspot.com/13666003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192503 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/sync_manager_impl.cc
+++ sync/internal_api/sync_manager_impl.cc
@@ -123,6 +123,8 @@ class NudgeStrategy {
        return ACCOMPANY_ONLY;
      case PREFERENCES:
      case SESSIONS:
+     case FAVICON_IMAGES:
+     case FAVICON_TRACKING:
        return CUSTOM;
      default:
        return IMMEDIATE;
@@ -150,6 +152,8 @@ class NudgeStrategy {
                kPreferencesNudgeDelayMilliseconds);
            break;
          case SESSIONS:
+         case FAVICON_IMAGES:
+         case FAVICON_TRACKING:
            delay = core->scheduler()->GetSessionsCommitDelay();
            break;
          default:
