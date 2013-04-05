409dc4ddbaf170c632b441c4e45ccc6878f1b1be
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 03:53:09 +0000

    cros: Explicitly manage app session lifetime.
    - Remove the EndKeepALive work arounds;
    - Manage app session lifetime explicitly by watching app
      window counts;
    
    BUG=226103
    
    Review URL: https://chromiumcodereview.appspot.com/13581008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192487 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/chromeos/app_mode/app_session_lifetime.h
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_APP_MODE_APP_SESSION_LIFETIME_H_
+#define CHROME_BROWSER_CHROMEOS_APP_MODE_APP_SESSION_LIFETIME_H_
+
+#include <string>
+
+class Profile;
+
+namespace chromeos {
+
+// Initializes an app session.
+void InitAppSession(Profile* profile, const std::string& app_id);
+
+}  // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_APP_MODE_APP_SESSION_LIFETIME_H_
--- chrome/browser/lifetime/application_lifetime_aura.cc
+++ chrome/browser/lifetime/application_lifetime_aura.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/lifetime/application_lifetime.h"
 
 #include "base/command_line.h"
-#include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
 #include "chrome/common/chrome_switches.h"
@@ -48,8 +47,7 @@ void HandleAppExitingForPlatform() {
   ash::Shell::GetInstance()->output_configurator()->Stop();
 
   if (!CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableZeroBrowsersOpenForTests) &&
-      !chrome::IsRunningInAppMode()) {
+      switches::kDisableZeroBrowsersOpenForTests)) {
     // App is exiting, call EndKeepAlive() on behalf of Aura Shell.
     EndKeepAlive();
     // Make sure we have notified the session manager that we are exiting.
