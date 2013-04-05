896c6edbaa46e1211abc0a8cbecc09b48441b875
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 04:34:53 +0000

    Update calls to scoped_array<T>::reset(NULL) to use the no-arg version.
    scoped_ptr<T[]>, which will replace scoped_array<T>, does not support
    passing NULL as an explicit argument to reset().
    
    BUG=171118
    
    Review URL: https://codereview.chromium.org/13692002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192506 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/win/waveout_output_win.cc
+++ media/audio/win/waveout_output_win.cc
@@ -169,7 +169,7 @@ void PCMWaveOutAudioOutputStream::FreeBuffers() {
   for (int ix = 0; ix != num_buffers_; ++ix) {
     ::waveOutUnprepareHeader(waveout_, GetBuffer(ix), sizeof(WAVEHDR));
   }
-  buffers_.reset(NULL);
+  buffers_.reset();
 }
 
 // Initially we ask the source to fill up all audio buffers. If we don't do
