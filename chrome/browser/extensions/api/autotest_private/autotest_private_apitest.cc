e57ccb38154ae1be3967a26ed5a417ed5d035273
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 23:55:17 +0000

    autotestPrivate.loginStatus returns a dictionary instead of a string.
    BUG=225911
    TEST=manual + browser test ExtensionApi.AutotestPrivate
    
    Review URL: https://chromiumcodereview.appspot.com/13520005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192655 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/autotest_private/autotest_private_apitest.cc
+++ chrome/browser/extensions/api/autotest_private/autotest_private_apitest.cc
@@ -7,15 +7,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/api/autotest_private/autotest_private_api_factory.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 
-// Times out on win asan, http://crbug.com/166026
-#if defined(OS_WIN) && defined(ADDRESS_SANITIZER)
-#define MAYBE_AutotestPrivate DISABLED_AutotestPrivate
-#else
-#define MAYBE_AutotestPrivate AutotestPrivate
-#endif
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_AutotestPrivate) {
+#if defined(OS_CHROMEOS)
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, AutotestPrivate) {
   // Turn on testing mode so we don't kill the browser.
   extensions::AutotestPrivateAPIFactory::GetForProfile(
       browser()->profile())->set_test_mode(true);
   ASSERT_TRUE(RunComponentExtensionTest("autotest_private")) << message_;
 }
+#endif
