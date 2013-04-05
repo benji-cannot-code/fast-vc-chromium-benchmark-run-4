24024348ff36dd423f0ebba8e7442e9a9f633895
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-05 06:13:31 +0000

    Clean up scoped_array<T> construction from NULL.
    This won't be allowed by unique_ptr<T> or scoped_ptr<T>.
    
    BUG=171118
    
    Review URL: https://codereview.chromium.org/13710002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192518 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/tools/compositor_model_bench/render_model_utils.cc
+++ gpu/tools/compositor_model_bench/render_model_utils.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 TextureGenerator::TextureGenerator(RenderNode* root)
     : stage_(DiscoveryStage),
-      tex_ids_(NULL),
-      image_data_(NULL),
       images_generated_(0) {
   DiscoverInputIDs(root);
   GenerateGLTexIDs();
