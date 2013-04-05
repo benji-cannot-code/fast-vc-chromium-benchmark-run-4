b80adfa6c2f08874f4c4f58fe6e51a2a7a92bddd
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-05 21:05:01 +0000

    Remove PLATFORM checks from platform Drag classes.
    BUG=
    
    Review URL: https://codereview.chromium.org/13601021
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147672 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/DragImage.h
+++ third_party/WebKit/Source/WebCore/platform/DragImage.h
@@ -27,23 +27,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DragImage_h
 #define DragImage_h
 
+#include "DragImageRef.h"
 #include "ImageOrientation.h"
 #include "IntSize.h"
 #include "FloatSize.h"
 #include <wtf/Forward.h>
 
-#if PLATFORM(MAC)
-#include <wtf/RetainPtr.h>
-OBJC_CLASS NSImage;
-#elif PLATFORM(QT)
-QT_BEGIN_NAMESPACE
-class QPixmap;
-QT_END_NAMESPACE
-#elif PLATFORM(WIN)
-typedef struct HBITMAP__* HBITMAP;
-#elif PLATFORM(CHROMIUM)
-#include "DragImageRef.h"
-#endif
 
 //We need to #define YOffset as it needs to be shared with WebKit
 #define DragLabelBorderYOffset 2
@@ -56,16 +45,6 @@ namespace WebCore {
     class KURL;
     class Range;
 
-#if PLATFORM(MAC)
-    typedef RetainPtr<NSImage> DragImageRef;
-#elif PLATFORM(QT)
-    typedef QPixmap* DragImageRef;
-#elif PLATFORM(WIN)
-    typedef HBITMAP DragImageRef;
-#elif PLATFORM(EFL) || PLATFORM(BLACKBERRY)
-    typedef void* DragImageRef;
-#endif
-    
     IntSize dragImageSize(DragImageRef);
     
     //These functions should be memory neutral, eg. if they return a newly allocated image, 
@@ -81,6 +60,5 @@ namespace WebCore {
     DragImageRef createDragImageForLink(KURL&, const String& label, Frame*);
     void deleteDragImage(DragImageRef);
 }
-
 
 #endif //!DragImage_h
