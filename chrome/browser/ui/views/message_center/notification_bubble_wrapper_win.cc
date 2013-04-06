6b27c47906717eaab1772007113904547f1f599e
dimich@chromium.org <dimich@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-06 15:55:58 +0000

    Change the widget type used for Bubbles on Win to prevent them from having buttons on taskbar.
    BUG=223685
    
    
    Review URL: https://chromiumcodereview.appspot.com/13726022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192753 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/message_center/notification_bubble_wrapper_win.cc
+++ chrome/browser/ui/views/message_center/notification_bubble_wrapper_win.cc
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/bubble/tray_bubble_view.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
+#include "ui/views/win/hwnd_util.h"
 
 namespace message_center {
 
@@ -46,6 +47,14 @@ NotificationBubbleWrapperWin::NotificationBubbleWrapperWin(
       tray_->GetBubbleWindowContainer(), NULL, this, &init_params);
 
   bubble_widget_ = views::BubbleDelegateView::CreateBubble(bubble_view_);
+
+  // Remove the bubbles for Notifications and Notification Center from taskbar
+  // and alt-tab rotation.
+  HWND hwnd = views::HWNDForWidget(bubble_widget_);
+  LONG_PTR ex_styles = ::GetWindowLongPtr(hwnd, GWL_EXSTYLE);
+  ex_styles |= WS_EX_TOOLWINDOW;
+  ::SetWindowLongPtr(hwnd, GWL_EXSTYLE, ex_styles);
+
   bubble_widget_->AddObserver(this);
   bubble_widget_->StackAtTop();
   bubble_widget_->SetAlwaysOnTop(true);
