0e49a058c927fd75d3491959f79e378bdf447ab0
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-06 00:45:30 +0000

    cc: Moved contents_swizzled out of TileManager
    Drawing info now has everything it needs to calculate
    the contents_swizzled instead of it being set in
    TileManager in two spots.
    
    BUG=225804
    
    
    Review URL: https://chromiumcodereview.appspot.com/13665003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192675 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/platform_color.h
+++ cc/resources/platform_color.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CC_RESOURCES_PLATFORM_COLOR_H_
 
 #include "base/basictypes.h"
+#include "base/logging.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebGraphicsContext3D.h"
 #include "third_party/khronos/GLES2/gl2.h"
 #include "third_party/khronos/GLES2/gl2ext.h"
