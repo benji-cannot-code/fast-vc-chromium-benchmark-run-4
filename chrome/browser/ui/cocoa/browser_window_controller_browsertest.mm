benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit a85408c2917e118370ab26419bc315a15a0c0f08

Alternate NTP: Hook up SearchModel::top_bars_visible
In r188994 kuan added a new top_bars_visible state to coordinate hiding and showing the bookmark bar.

This CL hooks up that state with the Mac UI.

BUG=176146
TEST=Ran chrome against an internal instant server. Typed in the Omnibox and verified that the bookmark bar was not hidden until instant results were available.


Review URL: https://chromiumcodereview.appspot.com/12973003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@189607 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/browser_window_controller_browsertest.mm
@@ -103,8 +103,10 @@ class BrowserWindowControllerTest : public InProcessBrowserTest {
     chrome::search::Mode mode(chrome::search::Mode::MODE_SEARCH_SUGGESTIONS,
                               chrome::search::Mode::ORIGIN_SEARCH);
     browser()->search_model()->SetMode(mode);
+    browser()->search_model()->SetTopBarsVisible(false);
     EXPECT_TRUE(browser()->search_model()->mode().is_search_suggestions());
-    EXPECT_TRUE([controller() isShowingInstantResults]);
+    EXPECT_EQ(browser_window_controller::kInstantUIFullPageResults,
+              [controller() currentInstantUIState]);
   }
 
   void ShowInstantNTP() {
@@ -112,8 +114,10 @@ class BrowserWindowControllerTest : public InProcessBrowserTest {
     chrome::search::Mode mode(chrome::search::Mode::MODE_NTP,
                               chrome::search::Mode::ORIGIN_NTP);
     browser()->search_model()->SetMode(mode);
+    browser()->search_model()->SetTopBarsVisible(true);
     EXPECT_TRUE(browser()->search_model()->mode().is_ntp());
-    EXPECT_FALSE([controller() isShowingInstantResults]);
+    EXPECT_EQ(browser_window_controller::kInstantUINone,
+              [controller() currentInstantUIState]);
   }
 
   void ShowInfoBar() {

chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -843,23 +843,24 @@ willPositionSheet:(NSWindow*)sheet
 }
 
 - (CGFloat)toolbarDividerOpacity {
-  if ([self isShowingInstantResults])
+  if ([self currentInstantUIState] != browser_window_controller::kInstantUINone)
     return 1;
   return [bookmarkBarController_ toolbarDividerOpacity];
 }
 
-- (BOOL)isShowingInstantResults {
+- (browser_window_controller::InstantUIState)currentInstantUIState {
   if (!browser_->search_model()->mode().is_search_suggestions())
-    return NO;
+    return browser_window_controller::kInstantUINone;
 
   // If the search suggestions are already being displayed in the overlay
-  // contents then return YES.
+  // contents then return kInstantUIOverlay.
   if ([overlayableContentsController_ isShowingOverlay])
-    return YES;
+    return browser_window_controller::kInstantUIOverlay;
 
-  // Search suggestions might be shown directly in the web contents in some
-  // cases.
-  return !browser_->search_model()->mode().is_origin_default();
+  if (browser_->search_model()->top_bars_visible())
+    return browser_window_controller::kInstantUINone;
+
+  return browser_window_controller::kInstantUIFullPageResults;
 }
 
 - (void)updateContentOffsets {
@@ -868,8 +869,8 @@ willPositionSheet:(NSWindow*)sheet
   // is on the search results page where the Instant results are shown inside
   // the page and not in the overlay contents as usual.
   CGFloat tabContentsOffset = toolbarToWebContentsOffset_;
-  if (browser_->search_model()->mode().is_search_suggestions() &&
-      !browser_->search_model()->mode().is_origin_default()) {
+  if ([self currentInstantUIState] ==
+      browser_window_controller::kInstantUIFullPageResults) {
     tabContentsOffset = 0;
   }
   [overlayableContentsController_ setActiveContainerOffset:tabContentsOffset];
@@ -878,7 +879,7 @@ willPositionSheet:(NSWindow*)sheet
   [[self tabContentArea] setContentOffset:toolbarToWebContentsOffset_];
 
   // Prevent the dev tools splitter from overlapping the bookmark bar.
-  if ([self isShowingInstantResults])
+  if ([self currentInstantUIState] != browser_window_controller::kInstantUINone)
     [devToolsController_ setTopContentOffset:0];
   else
     [devToolsController_ setTopContentOffset:toolbarToWebContentsOffset_];
@@ -926,12 +927,14 @@ willPositionSheet:(NSWindow*)sheet
   // The find bar is above everything except Instant search results.
   if (findBarCocoaController_) {
     NSView* relativeView = nil;
-    if (inPresentationMode)
-      relativeView =  toolbarView;
-    else if ([self isShowingInstantResults])
+    if (inPresentationMode) {
+      relativeView = toolbarView;
+    } else if ([self currentInstantUIState] !=
+               browser_window_controller::kInstantUINone) {
       relativeView = [infoBarContainerController_ view];
-    else
+    } else {
       relativeView = [self tabContentArea];
+    }
     [contentView cr_ensureSubview:[findBarCocoaController_ view]
                      isPositioned:NSWindowAbove
                        relativeTo:relativeView];

chrome/browser/ui/cocoa/browser_window_controller_private.h
@@ -8,6 +8,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #import "chrome/browser/ui/cocoa/browser_window_controller.h"
 
+namespace browser_window_controller {
+
+enum InstantUIState {
+  kInstantUINone,
+  // Instant suggestions are displayed in a overlay overlapping the tab
+  // contents.
+  kInstantUIOverlay,
+  // Instant suggestions are displayed in the main tab contents.
+  kInstantUIFullPageResults,
+};
+
+}  // namespace browser_window_controller
+
 // Private methods for the |BrowserWindowController|. This category should
 // contain the private methods used by different parts of the BWC; private
 // methods used only by single parts should be declared in their own file.
@@ -144,8 +157,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // The opacity for the toolbar divider; 0 means that it shouldn't be shown.
 - (CGFloat)toolbarDividerOpacity;
 
-// Returns YES if Instant results are being shown under the omnibox.
-- (BOOL)isShowingInstantResults;
+// This is used to check if either the instant overlay or full page instant
+// search results are currently being displayed.
+- (browser_window_controller::InstantUIState)currentInstantUIState;
 
 // Updates the content offets of the tab strip controller and the overlayable
 // contents controller. This is used to adjust the overlap between those views

chrome/browser/ui/cocoa/tab_contents/instant_overlay_controller_mac.mm
@@ -29,11 +29,8 @@ InstantOverlayControllerMac::~InstantOverlayControllerMac() {
 
 void InstantOverlayControllerMac::OverlayStateChanged(
     const InstantOverlayModel& model) {
-  // TODO(sail): migrate from InstantOverlayControllerViews::OverlayStateChanged
-  // to only call SetTopBarsVisible if we did something with overlay; this
-  // prevents the top bars from flashing in this transition
-  // DEFAULT->SUGGESTIONS->SERP.
-  bool has_overlay = false;
+  bool set_top_bars_visibility = chrome::search::IsInstantExtendedAPIEnabled();
+
   if (model.mode().is_ntp() || model.mode().is_search_suggestions()) {
     // Drop shadow is only needed if search mode is not |NTP| and overlay does
     // not fill up the entire contents page.
@@ -44,23 +41,26 @@ void InstantOverlayControllerMac::OverlayStateChanged(
                   height:model.height()
              heightUnits:model.height_units()
           drawDropShadow:drawDropShadow];
-    has_overlay = true;
-  } else {
+  } else if ([overlay_ isShowingOverlay]) {
     [overlay_ setOverlay:NULL
                   height:0
              heightUnits:INSTANT_SIZE_PIXELS
           drawDropShadow:NO];
+  } else {
+    set_top_bars_visibility = false;
   }
 
-  if (chrome::search::IsInstantExtendedAPIEnabled()) {
+  if (set_top_bars_visibility) {
     // Set top bars (bookmark and info bars) visibility for current tab via
     // |SearchTabHelper| of current active web contents: top bars are hidden if
     // there's overlay.
     chrome::search::SearchTabHelper* search_tab_helper =
         chrome::search::SearchTabHelper::FromWebContents(
             browser_->tab_strip_model()->GetActiveWebContents());
-    if (search_tab_helper)
-      search_tab_helper->model()->SetTopBarsVisible(!has_overlay);
+    if (search_tab_helper) {
+      search_tab_helper->model()->SetTopBarsVisible(
+          ![overlay_ isShowingOverlay]);
+    }
   }
 
   [window_ updateBookmarkBarStateForInstantOverlay];

commit 03b18be9c81f936c476f2743394ce85f712a1653

Instant Extended: Hide bookmark bar on search results page
When on a search results page the bookmark bar should be shown. This
was a regression from r189607.

Fix was to hide the bookmark bar in MODE_SEARCH_RESULTS as well as
MODE_SEARCH_SUGGESTIONS.

BUG=225767
TEST=
  Added a regression test, BrowserWindowControllerTest.InstantSearchResultsMode.
  Ran Chrome in instant extended mode. Did a search and clicked on the
  results page. Verified that the bookmark bar stayed hidden.


Review URL: https://chromiumcodereview.appspot.com/13726019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192680 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/ui/cocoa/browser_window_controller_browsertest.mm
@@ -490,3 +490,15 @@ IN_PROC_BROWSER_TEST_F(BrowserWindowControllerTest,
   EXPECT_TRUE([[contentView hitTest:point]
       isDescendantOf:[controller() tabContentArea]]);
 }
+
+// Verify that |currentInstantUIState| returns the correct value for search
+// results mode.
+IN_PROC_BROWSER_TEST_F(BrowserWindowControllerTest, InstantSearchResultsMode) {
+  chrome::EnableInstantExtendedAPIForTesting();
+  SearchMode mode(SearchMode::MODE_SEARCH_RESULTS, SearchMode::ORIGIN_SEARCH);
+  browser()->search_model()->SetMode(mode);
+  browser()->search_model()->SetTopBarsVisible(false);
+  EXPECT_TRUE(browser()->search_model()->mode().is_search_results());
+  EXPECT_EQ(browser_window_controller::kInstantUIFullPageResults,
+            [controller() currentInstantUIState]);
+}

chrome/browser/ui/cocoa/browser_window_controller_private.mm
@@ -863,7 +863,7 @@ willPositionSheet:(NSWindow*)sheet
 }
 
 - (browser_window_controller::InstantUIState)currentInstantUIState {
-  if (!browser_->search_model()->mode().is_search_suggestions())
+  if (!browser_->search_model()->mode().is_search())
     return browser_window_controller::kInstantUINone;
 
   // If the search suggestions are already being displayed in the overlay