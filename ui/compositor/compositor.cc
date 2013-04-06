3c40342649332ae8bf36a01e8e43b3fcd4b37dc1
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-06 00:47:05 +0000

    cc: Use refresh_rate from LayerTreeSettings
    We use it in compositor_unittests to have a faster refresh rate during test
    (when WGC3D is mocked out --- no hardware vsync).
    
    TEST=cc_unittests pass, compositor_unittests --ui-enable-threaded-compositing are faster
    
    Review URL: https://chromiumcodereview.appspot.com/13674006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192676 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.cc
+++ ui/compositor/compositor.cc
@@ -42,7 +42,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace {
 
 const double kDefaultRefreshRate = 60.0;
-const double kTestRefreshRate = 100.0;
+const double kTestRefreshRate = 200.0;
 
 enum SwapType {
   DRAW_SWAP,
