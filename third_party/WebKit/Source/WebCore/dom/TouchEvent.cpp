774de0996ab69e2b262f1b99eee90bea75540f65
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-07 21:02:15 +0000

    Remove PLATFORM defines from dom
    Review URL: https://codereview.chromium.org/13602011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147756 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/dom/TouchEvent.cpp
+++ third_party/WebKit/Source/WebCore/dom/TouchEvent.cpp
@@ -55,10 +55,6 @@ TouchEvent::TouchEvent(TouchList* touches, TouchList* targetTouches,
     , m_touches(touches)
     , m_targetTouches(targetTouches)
     , m_changedTouches(changedTouches)
-#if PLATFORM(BLACKBERRY)
-    , m_touchHold(false)
-    , m_doubleTap(false)
-#endif
 {
 }
 
@@ -85,11 +81,6 @@ void TouchEvent::initTouchEvent(TouchList* touches, TouchList* targetTouches,
     m_shiftKey = shiftKey;
     m_metaKey = metaKey;
     initCoordinates(IntPoint(clientX, clientY));
-#if PLATFORM(BLACKBERRY)
-    m_doubleTap = false;
-    m_touchHold = false;
-#endif
-
 }
 
 const AtomicString& TouchEvent::interfaceName() const
