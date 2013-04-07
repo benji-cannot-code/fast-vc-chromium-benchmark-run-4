c0721faa0b6e2225a0fe5e72a2b0ae8e29e6ac0a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-07 21:17:05 +0000

    ui: Update the includes of message_loop_proxy.h to its new location.
    As of r191422 (https://codereview.chromium.org/13333003) message_loop_proxy.h
    lives in base/message_loop/ directory.
    
    BUG=None
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/13718004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192783 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/shell_dialogs/select_file_dialog_win.cc
+++ ui/shell_dialogs/select_file_dialog_win.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/i18n/case_conversion.h"
 #include "base/message_loop.h"
-#include "base/message_loop_proxy.h"
+#include "base/message_loop/message_loop_proxy.h"
 #include "base/strings/string_split.h"
 #include "base/threading/thread.h"
 #include "base/utf_string_conversions.h"
--- ui/surface/accelerated_surface_win.cc
+++ ui/surface/accelerated_surface_win.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop_proxy.h"
+#include "base/message_loop/message_loop_proxy.h"
 #include "base/scoped_native_library.h"
 #include "base/stringprintf.h"
 #include "base/synchronization/waitable_event.h"
