192e04bed2cb8b545d86c4efbece00bb724aea3e
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-07 19:38:52 +0000

    Merge include that was missed when removing platform strategies in r147686.
    BUG=
    
    Review URL: https://codereview.chromium.org/13757004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147753 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/Modules/webdatabase/DatabaseManager.cpp
+++ third_party/WebKit/Source/WebCore/Modules/webdatabase/DatabaseManager.cpp
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DatabaseBackendSync.h"
 #include "DatabaseCallback.h"
 #include "DatabaseContext.h"
+#include "DatabaseServer.h"
 #include "DatabaseSync.h"
 #include "DatabaseTask.h"
 #include "ExceptionCode.h"
@@ -45,8 +46,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ScriptController.h"
 #include "ScriptExecutionContext.h"
 #include "SecurityOrigin.h"
-
-#include "DatabaseServer.h"
 
 namespace WebCore {
 
