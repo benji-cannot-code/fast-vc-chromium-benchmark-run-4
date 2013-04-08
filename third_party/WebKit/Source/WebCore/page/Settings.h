73aa389864886d7872fc75b33dc9574c4a042f44
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-08 18:48:56 +0000

    Remove unused runtime setting shouldusehighresolutiontimers
    This setting existed in WebKit but Chromium never changed it from its default value. We're removing all such unchanging settings in Blink, along with their associated code.
    
    R=abarth@chromium.org
    BUG=226711
    
    Review URL: https://codereview.chromium.org/13454014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147835 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/Settings.h
+++ third_party/WebKit/Source/WebCore/page/Settings.h
@@ -202,11 +202,6 @@ namespace WebCore {
         void setShowTiledScrollingIndicator(bool);
         bool showTiledScrollingIndicator() const { return m_showTiledScrollingIndicator; }
 
-#if PLATFORM(WIN) || (OS(WINDOWS) && PLATFORM(WX))
-        static void setShouldUseHighResolutionTimers(bool);
-        static bool shouldUseHighResolutionTimers() { return gShouldUseHighResolutionTimers; }
-#endif
-
         void setTiledBackingStoreEnabled(bool);
         bool tiledBackingStoreEnabled() const { return m_tiledBackingStoreEnabled; }
 
@@ -314,9 +309,6 @@ namespace WebCore {
 
 #if USE(SAFARI_THEME)
         static bool gShouldPaintNativeControls;
-#endif
-#if PLATFORM(WIN) || (OS(WINDOWS) && PLATFORM(WX))
-        static bool gShouldUseHighResolutionTimers;
 #endif
 
         static double gHiddenPageDOMTimerAlignmentInterval;
