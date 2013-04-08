814138ab841045ee096956c1370e7a3cf421e7a5
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-08 16:19:49 +0000

    Translate: Adding a flag to enable "alpha language" translation
    BUG=226895
    
    Review URL: https://codereview.chromium.org/13488014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192827 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager.cc
+++ chrome/browser/translate/translate_manager.cc
@@ -50,6 +50,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/browser_resources.h"
 #include "net/base/escape.h"
 #include "net/base/load_flags.h"
+#include "net/base/url_util.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
 #include "ui/base/resource/resource_bundle.h"
@@ -142,7 +143,12 @@ const char* const kTranslateScriptHeader =
 const char* const kReportLanguageDetectionErrorURL =
     "https://translate.google.com/translate_error";
 const char* const kLanguageListFetchURL =
-    "https://translate.googleapis.com/translate_a/l?client=chrome&cb=sl&hl=%s";
+    "https://translate.googleapis.com/translate_a/l?client=chrome&cb=sl";
+
+const char* const kLanguageListFetchLocaleQueryName = "hl";
+const char* const kLanguageListFetchAlphaLanguageQueryName = "alpha";
+const char* const kLanguageListFetchAlphaLanguageQueryValue = "1";
+
 const int kMaxRetryLanguageListFetch = 5;
 const int kTranslateScriptExpirationDelayDays = 1;
 
@@ -777,11 +783,23 @@ void TranslateManager::FetchLanguageListFromTranslateServer(
     return;
   }
 
-  GURL language_list_fetch_url = GURL(
-      base::StringPrintf(
-          kLanguageListFetchURL,
-          GetLanguageCode(g_browser_process->GetApplicationLocale()).c_str()));
+  GURL language_list_fetch_url = GURL(kLanguageListFetchURL);
+  language_list_fetch_url = net::AppendQueryParameter(
+      language_list_fetch_url,
+      kLanguageListFetchLocaleQueryName,
+      GetLanguageCode(g_browser_process->GetApplicationLocale()).c_str());
+
+  const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+  if (command_line.HasSwitch(switches::kEnableTranslateAlphaLanguages)) {
+    language_list_fetch_url = net::AppendQueryParameter(
+        language_list_fetch_url,
+        kLanguageListFetchAlphaLanguageQueryName,
+        kLanguageListFetchAlphaLanguageQueryValue);
+  }
   AddApiKeyToUrl(&language_list_fetch_url);
+  VLOG(9) << "Fetch supporting language list from: "
+          << language_list_fetch_url.spec().c_str();
+
   language_list_request_pending_.reset(net::URLFetcher::Create(
       1, language_list_fetch_url, net::URLFetcher::GET, this));
   language_list_request_pending_->SetLoadFlags(net::LOAD_DO_NOT_SEND_COOKIES |
