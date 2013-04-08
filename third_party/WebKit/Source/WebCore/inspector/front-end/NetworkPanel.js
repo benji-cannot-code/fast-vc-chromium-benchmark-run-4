c6746edc8c654568db01a6b835fd3a694b5088a0
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-08 18:07:55 +0000

    Revert "DevTools: [Network] dataURIs are listed as separate requests in HAR"
    This reverts commit 9cd36e18adbae9896ce6e592bffed64dcd6d313c.
    
    BUG=
    TBR=pfeldman
    
    Review URL: https://codereview.chromium.org/13770009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/front-end/NetworkPanel.js
+++ third_party/WebKit/Source/WebCore/inspector/front-end/NetworkPanel.js
@@ -85,7 +85,6 @@ WebInspector.NetworkLogView = function(coulmnsVisibilitySetting)
     WebInspector.networkLog.requests.forEach(this._appendRequest.bind(this));
 }
 
-WebInspector.NetworkLogView.HTTPSchemas = {"http": true, "https": true, "ws": true, "wss": true};
 WebInspector.NetworkLogView._defaultColumnsVisivility = {method: true, status: true, domain: false, type: true, initiator: true, cookies: false, setCookies: false, size: true, time: true};
 
 WebInspector.NetworkLogView.prototype = {
@@ -1079,7 +1078,7 @@ WebInspector.NetworkLogView.prototype = {
     _copyAll: function()
     {
         var harArchive = {
-            log: (new WebInspector.HARLog(this._requests.filter(WebInspector.NetworkLogView.HTTPRequestsFilter))).build()
+            log: (new WebInspector.HARLog(this._requests)).build()
         };
         InspectorFrontendHost.copyText(JSON.stringify(harArchive, null, 2));
     },
@@ -1117,7 +1116,7 @@ WebInspector.NetworkLogView.prototype = {
             var progressIndicator = new WebInspector.ProgressIndicator();
             this._progressBarContainer.appendChild(progressIndicator.element);
             var harWriter = new WebInspector.HARWriter();
-            harWriter.write(stream, this._requests.filter(WebInspector.NetworkLogView.HTTPRequestsFilter), progressIndicator);
+            harWriter.write(stream, this._requests, progressIndicator);
         }
     },
 
@@ -1436,15 +1435,6 @@ WebInspector.NetworkLogView.prototype = {
     }, 
 
     __proto__: WebInspector.View.prototype
-}
-
-/**
- * @param {!WebInspector.NetworkRequest} request
- * @return {boolean}
- */
-WebInspector.NetworkLogView.HTTPRequestsFilter = function(request)
-{
-    return request.parsedURL.isValid && (request.parsedURL.scheme in WebInspector.NetworkLogView.HTTPSchemas);
 }
 
 
