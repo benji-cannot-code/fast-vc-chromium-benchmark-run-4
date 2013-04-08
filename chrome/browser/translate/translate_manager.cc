8ff60ed9a34adecfb50e19e866d5759b4bbab947
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-08 23:25:32 +0000

    Translate: URL check should be done at once
    The URL for Chrome OS file manager extension is checked
    separately for now. But this must be done in IsTranslatableURL()
    at once.
    
    BUG=none
    TEST=unit_tests
    
    Review URL: https://codereview.chromium.org/13770003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager.cc
+++ chrome/browser/translate/translate_manager.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #ifdef FILE_MANAGER_EXTENSION
 #include "chrome/browser/chromeos/extensions/file_manager_util.h"
+#include "extensions/common/constants.h"
 #endif
 
 using content::NavigationController;
@@ -185,12 +186,19 @@ TranslateManager* TranslateManager::GetInstance() {
 // static
 bool TranslateManager::IsTranslatableURL(const GURL& url) {
   // A URLs is translatable unless it is one of the following:
+  // - empty (can happen for popups created with window.open(""))
   // - an internal URL (chrome:// and others)
   // - the devtools (which is considered UI)
+  // - Chrome OS file manager extension
   // - an FTP page (as FTP pages tend to have long lists of filenames that may
   //   confuse the CLD)
-  return !url.SchemeIs(chrome::kChromeUIScheme) &&
+  return !url.is_empty() &&
+         !url.SchemeIs(chrome::kChromeUIScheme) &&
          !url.SchemeIs(chrome::kChromeDevToolsScheme) &&
+#ifdef FILE_MANAGER_EXTENSION
+         !(url.SchemeIs(extensions::kExtensionScheme) &&
+           url.DomainIs(kFileBrowserDomain)) &&
+#endif
          !url.SchemeIs(chrome::kFtpScheme);
 }
 
@@ -488,13 +496,6 @@ TranslateManager::TranslateManager()
 
 void TranslateManager::InitiateTranslation(WebContents* web_contents,
                                            const std::string& page_lang) {
-#ifdef FILE_MANAGER_EXTENSION
-  const GURL& page_url = web_contents->GetURL();
-  if (page_url.SchemeIs("chrome-extension") &&
-      page_url.DomainIs(kFileBrowserDomain))
-    return;
-#endif
-
   Profile* profile =
       Profile::FromBrowserContext(web_contents->GetBrowserContext());
   PrefService* prefs = profile->GetOriginalProfile()->GetPrefs();
@@ -505,12 +506,6 @@ void TranslateManager::InitiateTranslation(WebContents* web_contents,
   // automated browser testing.
   if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kDisableTranslate))
     return;
-
-  NavigationEntry* entry = web_contents->GetController().GetActiveEntry();
-  if (!entry) {
-    // This can happen for popups created with window.open("").
-    return;
-  }
 
   std::string target_lang = GetTargetLanguage(prefs);
   std::string language_code = GetLanguageCode(page_lang);
@@ -525,8 +520,10 @@ void TranslateManager::InitiateTranslation(WebContents* web_contents,
   // - similar languages (ex: en-US to en).
   // - any user black-listed URLs or user selected language combination.
   // - any language the user configured as accepted languages.
-  if (!IsTranslatableURL(entry->GetURL()) || language_code == target_lang ||
-      !TranslatePrefs::CanTranslate(prefs, language_code, entry->GetURL()) ||
+  GURL page_url = web_contents->GetURL();
+  if (!IsTranslatableURL(page_url) ||
+      language_code == target_lang ||
+      !TranslatePrefs::CanTranslate(prefs, language_code, page_url) ||
       IsAcceptLanguage(web_contents, language_code)) {
     return;
   }
