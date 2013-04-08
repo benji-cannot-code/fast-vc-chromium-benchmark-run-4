6de857cfb7f6d082517907d8450aff0ed857a297
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-08 04:30:23 +0000

    Remove USE(JSC)
    Chromium does not use JSC
    
    Review URL: https://codereview.chromium.org/13556006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/text/StringImpl.cpp
+++ third_party/WebKit/Source/WTF/wtf/text/StringImpl.cpp
@@ -116,12 +116,6 @@ StringImpl::~StringImpl()
 
     if (isAtomic())
         AtomicString::remove(this);
-#if USE(JSC)
-    if (isIdentifier()) {
-        if (!wtfThreadData().currentIdentifierTable()->remove(this))
-            CRASH();
-    }
-#endif
 
     BufferOwnership ownership = bufferOwnership();
 
