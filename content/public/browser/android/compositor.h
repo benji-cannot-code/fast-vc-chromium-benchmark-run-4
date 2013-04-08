60da2dc38dd1892bc2982050214d4ddd4e1bc1e5
dtrainor@chromium.org <dtrainor@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-08 21:37:32 +0000

    Add swap buffers posted method
    BUG=228978,228897
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/13771021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@192909 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/browser/android/compositor.h
+++ content/public/browser/android/compositor.h
@@ -34,6 +34,9 @@ class CONTENT_EXPORT Compositor {
 
     // The compositor has completed swapping a frame.
     virtual void OnSwapBuffersCompleted() {}
+
+    // The compositor will eventually swap a frame.
+    virtual void OnSwapBuffersPosted() {}
   };
 
   virtual ~Compositor() {}
