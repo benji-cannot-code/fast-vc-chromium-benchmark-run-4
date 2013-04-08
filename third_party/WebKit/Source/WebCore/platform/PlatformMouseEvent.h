fa76e9caf1841291d63d064bf460a33c7fdb5a43
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-08 16:55:24 +0000

    Remove WindowsExtra.h.
    The functions were unused but the header was included in several files.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13749006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147823 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/PlatformMouseEvent.h
+++ third_party/WebKit/Source/WebCore/platform/PlatformMouseEvent.h
@@ -29,9 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "IntPoint.h"
 #include "PlatformEvent.h"
-#if OS(WINDOWS)
-#include "WindowsExtras.h"
-#endif
 
 #if PLATFORM(GTK)
 typedef struct _GdkEventButton GdkEventButton;
