5169f5664833c3b3944fea3971e3d48a564aa0da
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-08 07:32:14 +0000

    Remove INSPECTOR macro from IDL files and Source/WebKit.
    BUG=none
    TEST=build
    R=eseidel, yurys@chromium.org, pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/13771007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147782 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/FeatureDefines.h
+++ third_party/WebKit/Source/WTF/wtf/FeatureDefines.h
@@ -283,10 +283,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 #endif
 
-#if !defined(ENABLE_INSPECTOR)
-#define ENABLE_INSPECTOR 1
-#endif
-
 #if !defined(ENABLE_JAVASCRIPT_I18N_API)
 #define ENABLE_JAVASCRIPT_I18N_API 0
 #endif
--- third_party/WebKit/Source/WebKit/chromium/src/ApplicationCacheHost.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/ApplicationCacheHost.cpp
@@ -211,10 +211,8 @@ void ApplicationCacheHost::setDOMApplicationCache(DOMApplicationCache* domApplic
 
 void ApplicationCacheHost::notifyDOMApplicationCache(EventID id, int total, int done)
 {
-#if ENABLE(INSPECTOR)
     if (id != PROGRESS_EVENT)
         InspectorInstrumentation::updateApplicationCacheStatus(m_documentLoader->frame());
-#endif
 
     if (m_defersEvents) {
         // Event dispatching is deferred until document.onload has fired.
@@ -224,7 +222,6 @@ void ApplicationCacheHost::notifyDOMApplicationCache(EventID id, int total, int 
     dispatchDOMEvent(id, total, done);
 }
 
-#if ENABLE(INSPECTOR)
 ApplicationCacheHost::CacheInfo ApplicationCacheHost::applicationCacheInfo()
 {
     if (!m_internal)
@@ -248,7 +245,6 @@ void ApplicationCacheHost::fillResourceList(ResourceInfoList* resources)
             webResources[i].isForeign, webResources[i].isExplicit, webResources[i].size));
     }
 }
-#endif
 
 void ApplicationCacheHost::stopDeferringEvents()
 {
