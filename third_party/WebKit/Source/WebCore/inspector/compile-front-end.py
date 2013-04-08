096fa3d928bd247854900775bc6494f82f75878e
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-08 14:54:15 +0000

    Revert 147803 "DevTools: Compile CodeMirrorTextEditor"
    The frontend compilation state is too noisy to introduce more warnings at the moment.
    
    > DevTools: Compile CodeMirrorTextEditor
    > 
    > Add CodeMirrorTextEditor to closure compiler file list.
    > Patch by lushnikov@chromium.org
    > 
    > BUG=
    > 
    > Review URL: https://codereview.chromium.org/13788002/
    > 
    
    TBR=apavlov@chromium.org
    Review URL: https://codereview.chromium.org/13493009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147813 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/inspector/compile-front-end.py
+++ third_party/WebKit/Source/WebCore/inspector/compile-front-end.py
@@ -125,7 +125,6 @@ modules = [
         "sources": [
             "Checkbox.js",
             "ContextMenu.js",
-            "CodeMirrorTextEditor.js",
             "DOMSyntaxHighlighter.js",
             "DataGrid.js",
             "DefaultTextEditor.js",
