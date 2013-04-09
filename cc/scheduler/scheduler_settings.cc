9779a3246211509b04410d769a1c6e224a87c81b
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 04:37:35 +0000

    cc: Prevent checkerboarded animations from timing out during tests.
    During tests that verify behaviour with checkerboarding, we need
    preductable behaviour. Currently if the test takes too long and
    the compositor thread ticks too many times, the checkerboarded
    animation will time out and animate+draw. This is unpredictable
    flaky behaviour for the tests. So add a setting to disable this
    timeout-and-draw behaviour in the scheduler.
    
    R=jamesr
    BUG=228998
    
    Review URL: https://chromiumcodereview.appspot.com/13577005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193006 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler/scheduler_settings.cc
+++ cc/scheduler/scheduler_settings.cc
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-SchedulerSettings::SchedulerSettings() : impl_side_painting(false) {}
+SchedulerSettings::SchedulerSettings()
+    : impl_side_painting(false),
+      timeout_and_draw_when_animation_checkerboards(true) {}
 
 SchedulerSettings::~SchedulerSettings() {}
 
