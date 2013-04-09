36cdd6761048a290f1512bcee71a6e00b3b48e99
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 17:13:56 +0000

    Fixed URL for captive portal redirect.
    BUG=229307
    TESTS=manual tests on ChromeOS Lumpy + network_Portal autotest
    
    Review URL: https://chromiumcodereview.appspot.com/13928008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/captive_portal_view.cc
+++ chrome/browser/chromeos/login/captive_portal_view.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/login/captive_portal_view.h"
 
 #include "base/utf_string_conversions.h"
+#include "chrome/browser/captive_portal/captive_portal_detector.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/login/captive_portal_window_proxy.h"
 #include "chrome/browser/chromeos/net/connectivity_state_helper.h"
@@ -16,7 +17,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-const char kCaptivePortalStartURL[] = "http://clients3.google.com/generate_204";
+const char* CaptivePortalStartURL() {
+  return captive_portal::CaptivePortalDetector::kDefaultURL;
+}
 
 }  // namespace
 
@@ -33,7 +36,7 @@ CaptivePortalView::~CaptivePortalView() {
 }
 
 void CaptivePortalView::StartLoad() {
-  SimpleWebViewDialog::StartLoad(GURL(kCaptivePortalStartURL));
+  SimpleWebViewDialog::StartLoad(GURL(CaptivePortalStartURL()));
 }
 
 bool CaptivePortalView::CanResize() const {
@@ -72,8 +75,8 @@ void CaptivePortalView::NavigationStateChanged(
   GURL url = source->GetURL();
   // Note, |url| will be empty for "client3.google.com/generate_204" page.
   if (!redirected_  && url != GURL::EmptyGURL() &&
-      url != GURL(kCaptivePortalStartURL)) {
-    DLOG(INFO) << kCaptivePortalStartURL << " vs " << url.spec();
+      url != GURL(CaptivePortalStartURL())) {
+    DLOG(INFO) << CaptivePortalStartURL() << " vs " << url.spec();
     redirected_ = true;
     proxy_->OnRedirected();
   }
