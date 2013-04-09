f11a2687ffc095de07281317c3b7e1c1b26ab56c
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-09 19:24:39 +0000

    Add a RefPtr to FrameLoader::stopForUserCancel(). We've been seeing a wide variety of infrequent, difficult to reproduce crashes here. This is a speculative fix for this class of crashes.
    BUG=178269
    TEST=none, unfortunately
    Review URL: https://codereview.chromium.org/13467034
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147955 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -1530,6 +1530,8 @@ void FrameLoader::stopAllLoaders(ClearProvisionalItemPolicy clearProvisionalItem
 
 void FrameLoader::stopForUserCancel(bool deferCheckLoadComplete)
 {
+    // stopAllLoaders can detach the Frame, so protect it.
+    RefPtr<Frame> protect(m_frame);
     stopAllLoaders();
     
     if (deferCheckLoadComplete)
@@ -2346,6 +2348,8 @@ void FrameLoader::handledOnloadEvents()
 
 void FrameLoader::frameDetached()
 {
+    // stopAllLoaders can detach the Frame, so protect it.
+    RefPtr<Frame> protect(m_frame);
     stopAllLoaders();
     m_frame->document()->stopActiveDOMObjects();
     detachFromParent();
@@ -2353,6 +2357,7 @@ void FrameLoader::frameDetached()
 
 void FrameLoader::detachFromParent()
 {
+    // stopAllLoaders can detach the Frame, so protect it.
     RefPtr<Frame> protect(m_frame);
 
     closeURL();
@@ -2735,6 +2740,9 @@ void FrameLoader::continueLoadAfterNavigationPolicy(const ResourceRequest&, Pass
     // nil policyDataSource because loading the alternate page will have passed
     // through this method already, nested; otherwise, policyDataSource should still be set.
     ASSERT(m_policyDocumentLoader || !m_provisionalDocumentLoader->unreachableURL().isEmpty());
+
+    // stopAllLoaders can detach the Frame, so protect it.
+    RefPtr<Frame> protect(m_frame);
 
     bool isTargetItem = history()->provisionalItem() ? history()->provisionalItem()->isTargetItem() : false;
 
