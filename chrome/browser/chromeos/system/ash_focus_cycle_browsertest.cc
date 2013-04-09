52a3ec7fc1014b86ce980949ba4e7cd8c3a725f3
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 19:01:13 +0000

    CrOS: Wait for key press to be processed.
    --ui-enable-threaded-compositing changes the timing of task processing on
    the UI thread. It's insufficient to wait for the MessageLoop to go idle ---
    we must wait for a keypress to be processed.
    
    BUG=173417
    TEST=interactive_ui_tests --gtest_filter="AshFocusCycleTest*" --ui-enable-threaded-compositing
    
    
    Review URL: https://chromiumcodereview.appspot.com/13866011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193168 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system/ash_focus_cycle_browsertest.cc
+++ chrome/browser/chromeos/system/ash_focus_cycle_browsertest.cc
@@ -6,6 +6,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/focus_cycler.h"
 #include "ash/shell.h"
 #include "ash/wm/window_util.h"
+#include "base/bind.h"
+#include "base/memory/scoped_ptr.h"
+#include "base/run_loop.h"
 #include "base/stringprintf.h"
 #include "chrome/browser/chromeos/cros/cros_in_process_browser_test.h"
 #include "chrome/test/base/ui_controls.h"
@@ -45,9 +48,19 @@ class AshFocusCycleTest : public CrosInProcessBrowserTest {
   void PressEscape() {
     aura::Window* window = ash::wm::GetActiveWindow();
     CHECK(window);
-    ui_controls::SendKeyPress(window, ui::VKEY_ESCAPE,
-                              false, false, false, false);
-    content::RunAllPendingInMessageLoop();
+    wait_for_key_press_.reset(new base::RunLoop());
+    ui_controls::SendKeyPressNotifyWhenDone(
+        window, ui::VKEY_ESCAPE,
+        false, false, false, false,
+        base::Bind(&AshFocusCycleTest::QuitWaiting, base::Unretained(this)));
+    wait_for_key_press_->Run();
+  }
+
+ private:
+  scoped_ptr<base::RunLoop> wait_for_key_press_;
+
+  void QuitWaiting() {
+    wait_for_key_press_->Quit();
   }
 };
 
