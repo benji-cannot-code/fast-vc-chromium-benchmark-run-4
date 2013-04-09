a0dea5dadcf0665575bc4fb702b097847fbec319
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 19:35:07 +0000

    Revert r192391 (https://codereview.chromium.org/13652008/), which was a revert of r192111 (https://codereview.chromium.org/13097002/).
    Do not create one GL context per CompositingIOSurface, rather,
    share a single GL context across all CompositingIOSurfaces that
    are drawing to a given NSWindow (as determined by window number)
    and have the same window ordering (above or below).
    
    This cuts the regression in the ShutdownTests.TwentyTabs* from
    81% to 47%. Most of the rest is from the cost of destroying the
    renderers' GL contexts in the GPU process.
    
    NOTRY=true
    BUG=180463
    TBR=miu@chromium.org,kbr@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/13760002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193177 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.h
+++ content/browser/renderer_host/render_widget_host_view_mac.h
@@ -409,6 +409,8 @@ class RenderWidgetHostViewMac : public RenderWidgetHostViewBase,
     return frame_subscriber_.get();
   }
 
+  int window_number() const;
+
  private:
   friend class RenderWidgetHostView;
   friend class RenderWidgetHostViewMacTest;
