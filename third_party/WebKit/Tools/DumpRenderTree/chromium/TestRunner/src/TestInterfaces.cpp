9beec5ac40567225df04e3dffb2c3d457052a974
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-09 21:04:59 +0000

    Remove run-time flag for DataTransferItemList, part 1.
    This is pretty much always enabled for the Chromium port. There is a
    command-line flag to disable it... but the default is to enable it, and
    the flag is not exposed through chrome://flags.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13861018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147967 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestInterfaces.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestInterfaces.cpp
@@ -60,7 +60,6 @@ TestInterfaces::TestInterfaces()
 {
     WebKit::setLayoutTestMode(true);
 
-    WebRuntimeFeatures::enableDataTransferItems(true);
     WebRuntimeFeatures::enableDeviceMotion(false);
     WebRuntimeFeatures::enableGeolocation(true);
     WebRuntimeFeatures::enableIndexedDatabase(true);
