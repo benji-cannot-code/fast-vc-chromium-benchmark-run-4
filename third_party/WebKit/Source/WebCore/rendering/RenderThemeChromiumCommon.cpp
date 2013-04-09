b9f2f7cd253939f820ae9641ada87ec8e9e7fb63
commit-bot@chromium.org <commit-bot@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-09 20:32:58 +0000

    remove testing FIXME
    R=abarth@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/13820026
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147965 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderThemeChromiumCommon.cpp
+++ third_party/WebKit/Source/WebCore/rendering/RenderThemeChromiumCommon.cpp
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "InputTypeNames.h"
 #include "LayoutUnit.h"
 
-// FIXME: Merge this.
-
 namespace WebCore {
 
 bool RenderThemeChromiumCommon::supportsDataListUI(const AtomicString& type)
