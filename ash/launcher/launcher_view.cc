a94757f08ad93963620e7bcfcf127d88b6f74421
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 17:03:37 +0000

    Hide Toolip of LauncherItem when overflow button is clicked
    BUG=229382
    TEST=manual
    
    
    Review URL: https://chromiumcodereview.appspot.com/13813008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193122 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/launcher_view.cc
+++ ash/launcher/launcher_view.cc
@@ -1316,6 +1316,8 @@ void LauncherView::ButtonPressed(views::Button* sender,
   if (dragging())
     return;
 
+  tooltip_->Close();
+
   if (sender == overflow_button_) {
     ToggleOverflowBubble();
     return;
@@ -1330,8 +1332,6 @@ void LauncherView::ButtonPressed(views::Button* sender,
   // the call.
   if (!IsUsableEvent(event))
     return;
-
-  tooltip_->Close();
 
   {
     // Slow down activation animations if shift key is pressed.
