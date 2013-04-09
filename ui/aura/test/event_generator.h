ab5c56d5686a9a616bec51662455fd47f0c6d96a
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 21:20:32 +0000

    ash: Fix shelf dragging from auto-hidden state.
    The fixes include:
     * When shelf-dragging starts with the shelf hidden, the initial height of
       the shelf should start at the default auto-hide size, and then be adjusted
       for the drag.
     * During the drag, make sure the launcher has the same height as the shelf.
       This fixes the launcher highlights to always stick to the edge of the
       screen.
     * Make sure the shelf is always bottom aligned with the screen during the
       drag.
    
    This CL also adds mechanism to test the state of the UI after each step of a
    scroll sequence.
    
    BUG=222390, 222389, 222395
    
    Review URL: https://codereview.chromium.org/13825002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193213 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/event_generator.h
+++ ui/aura/test/event_generator.h
@@ -10,7 +10,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
+#include "ui/base/events/event_constants.h"
 #include "ui/base/keycodes/keyboard_codes.h"
 #include "ui/gfx/point.h"
 
@@ -35,6 +37,9 @@ class ScreenPositionClient;
 }
 
 namespace test {
+
+typedef base::Callback<void(ui::EventType, const gfx::Vector2dF&)>
+        ScrollStepCallback;
 
 // A delegate interface for EventGenerator that provides a way to
 // locate aura root window for given point.
@@ -185,6 +190,16 @@ class EventGenerator {
                              const gfx::Point& end,
                              const base::TimeDelta& duration,
                              int steps);
+
+  // The same as GestureScrollSequence(), with the exception that |callback| is
+  // called at each step of the scroll sequence. |callback| is called at the
+  // start of the sequence with ET_GESTURE_SCROLL_BEGIN, followed by one or more
+  // ET_GESTURE_SCROLL_UPDATE and ends with an ET_GESTURE_SCROLL_END.
+  void GestureScrollSequenceWithCallback(const gfx::Point& start,
+                                         const gfx::Point& end,
+                                         const base::TimeDelta& duration,
+                                         int steps,
+                                         const ScrollStepCallback& callback);
 
   // Generates press, move, release touch-events to generate a sequence of
   // multi-finger scroll events. |count| specifies the number of touch-points
