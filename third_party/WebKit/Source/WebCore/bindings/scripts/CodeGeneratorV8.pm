ba455db961ff12e4a2bf75b0c85d6c5bcd959d2b
jsbell@chromium.org <jsbell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-09 17:54:19 +0000

    Use [RaisesException] in IDL to trigger passing ExceptionCode parameter
    Our IDL files followed the older WebIDL model of a "raises (type)"
    suffix on operations and attributes, which would cause the code generator
    to append an "ExceptionCode& ec" parameter when calling the implementation.
    The "raises" suffix has been removed from WebIDL. It is assumed that any
    web platform method can throw exceptions at any time, which will be
    specified in prose.
    
    To inform the code generator, operations now use an [RaisesException]
    extended attribute instead of "raises (type)". Attributes now use
    [GetterRaisesException] and [SetterRaisesException] instead of
    "getter raises(type)" and "setter raises(type)".
    
    The IDL files were updated by script; as a side effect, the ordering
    in which extended attributes appear relative to the "attribute" was
    updated in a handful of IDL files unrelated to "raises" changes.
    
    R=abarth@chromium.org,haraken@chromium.org
    BUG=228950
    
    Review URL: https://codereview.chromium.org/13838004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147947 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/WebCore/bindings/scripts/CodeGeneratorV8.pm
@@ -1040,7 +1040,7 @@ END
         push(@implContentInternals, "    if (!BindingSecurity::shouldAllowAccessToNode(BindingState::instance(), imp->" . $attribute->signature->name . "()))\n        return v8::Handle<v8::Value>(v8Null(info.GetIsolate()));\n\n");
     }
 
-    my $useExceptions = 1 if @{$attribute->getterExceptions};
+    my $useExceptions = 1 if $attribute->signature->extendedAttributes->{"GetterRaisesException"};
     my $isNullable = $attribute->signature->isNullable;
     if ($useExceptions) {
         AddToImplIncludes("ExceptionCode.h");
@@ -1426,7 +1426,7 @@ END
 
     GenerateCustomElementInvocationScopeIfNeeded(\@implContentInternals, $attribute->signature->extendedAttributes);
 
-    my $useExceptions = 1 if @{$attribute->setterExceptions};
+    my $useExceptions = 1 if $attribute->signature->extendedAttributes->{"SetterRaisesException"};
 
     if ($useExceptions) {
         AddToImplIncludes("ExceptionCode.h");
@@ -1739,7 +1739,7 @@ END
 END
     }
 
-    my $raisesExceptions = @{$function->raisesExceptions};
+    my $raisesExceptions = $function->signature->extendedAttributes->{"RaisesException"};
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
             if ((!$parameter->extendedAttributes->{"Callback"} and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
@@ -2059,7 +2059,7 @@ sub GenerateSingleConstructorCallback
         $overloadedIndexString .= $function->{overloadedIndex};
     }
 
-    my $raisesExceptions = @{$function->raisesExceptions};
+    my $raisesExceptions = $function->signature->extendedAttributes->{"RaisesException"};
     if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         $raisesExceptions = 1;
     }
@@ -2256,7 +2256,7 @@ sub GenerateNamedConstructor
 
     my $interfaceName = $interface->name;
     my $v8InterfaceName = "V8$interfaceName";
-    my $raisesExceptions = @{$function->raisesExceptions};
+    my $raisesExceptions = $function->signature->extendedAttributes->{"RaisesException"};
     if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         $raisesExceptions = 1;
     }
@@ -3959,7 +3959,7 @@ sub GenerateFunctionCallString
         $index++;
     }
 
-    if (@{$function->raisesExceptions}) {
+    if ($function->signature->extendedAttributes->{"RaisesException"}) {
         push @arguments, "ec";
     }
 
@@ -3970,7 +3970,7 @@ sub GenerateFunctionCallString
 
     if ($returnType eq "void") {
         $result .= $indent . "$functionString;\n";
-    } elsif ($codeGenerator->ExtendedAttributeContains($callWith, "ScriptState") or @{$function->raisesExceptions}) {
+    } elsif ($codeGenerator->ExtendedAttributeContains($callWith, "ScriptState") or $function->signature->extendedAttributes->{"RaisesException"}) {
         $result .= $indent . $nativeReturnType . " result = $functionString;\n";
     } else {
         # Can inline the function call into the return statement to avoid overhead of using a Ref<> temporary
@@ -3982,7 +3982,7 @@ sub GenerateFunctionCallString
         }
     }
 
-    if (@{$function->raisesExceptions}) {
+    if ($function->signature->extendedAttributes->{"RaisesException"}) {
         $result .= $indent . "if (UNLIKELY(ec))\n";
         $result .= $indent . "    goto fail;\n";
     }
