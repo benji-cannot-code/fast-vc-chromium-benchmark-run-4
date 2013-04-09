a2d408f237a4d626c9c6fc87786110c8c9e1e889
benwells@chromium.org <benwells@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 14:15:35 +0000

    [win] Immediately enable app launcher on canary when it is enabled from the web store
    This previously would only happen after canary chrome was restarted. Now it happens immediately.
    
    BUG=196366
    
    
    Review URL: https://chromiumcodereview.appspot.com/13814004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193097 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_service.cc
+++ chrome/browser/ui/app_list/app_list_service.cc
@@ -132,6 +132,8 @@ Profile* AppListService::GetCurrentAppListProfile() { return NULL; }
 
 bool AppListService::IsAppListVisible() const { return false; }
 
+void AppListService::EnableAppList() {}
+
 void AppListService::OnProfileAdded(const base::FilePath& profilePath) {}
 
 void AppListService::OnProfileWillBeRemoved(
