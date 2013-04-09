200c9fdebebfa1168b24853d2d0d948d070fc0fb
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-09 03:46:02 +0000

    Remove compile-time / run-time flags for input[type=date].
    They are always on in all platforms.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/13888003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@147890 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/DateInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/DateInputType.cpp
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#if ENABLE(INPUT_TYPE_DATE)
 #include "DateInputType.h"
 
 #include "DateComponents.h"
@@ -135,4 +134,3 @@ bool DateInputType::isValidFormat(bool hasYear, bool hasMonth, bool hasWeek, boo
 #endif
 
 } // namespace WebCore
-#endif
--- third_party/WebKit/Source/WebCore/html/DateInputType.h
+++ third_party/WebKit/Source/WebCore/html/DateInputType.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DateInputType_h
 #define DateInputType_h
 
-#if ENABLE(INPUT_TYPE_DATE)
 #include "BaseChooserOnlyDateAndTimeInputType.h"
 #include "BaseMultipleFieldsDateAndTimeInputType.h"
 #include <wtf/RefPtr.h>
@@ -71,5 +70,4 @@ private:
 
 } // namespace WebCore
 
-#endif
 #endif // DateInputType_h
