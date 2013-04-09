57df1bae9d100898d0469ffb386eea513ff17ca8
craigdh@chromium.org <craigdh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-09 18:00:32 +0000

    [android] Improve logging output.
    BUG=160320
    TEST=ran the tests
    
    Review URL: https://codereview.chromium.org/13601030
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193143 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/cmd_helper.py
+++ build/android/pylib/cmd_helper.py
@@ -11,6 +11,7 @@ import subprocess
 
 import constants
 
+
 def RunCmd(args, cwd=None):
   """Opens a subprocess to execute a program and returns its return value.
 
@@ -24,8 +25,7 @@ def RunCmd(args, cwd=None):
     Return code from the command execution.
   """
   logging.info(str(args) + ' ' + (cwd or ''))
-  p = subprocess.Popen(args=args, cwd=cwd)
-  return p.wait()
+  return subprocess.call(args, cwd=cwd)
 
 
 def GetCmdOutput(args, cwd=None, shell=False):
@@ -44,6 +44,7 @@ def GetCmdOutput(args, cwd=None, shell=False):
   """
   (_, output) = GetCmdStatusAndOutput(args, cwd, shell)
   return output
+
 
 def GetCmdStatusAndOutput(args, cwd=None, shell=False):
   """Executes a subprocess and returns its exit code and output.
@@ -67,6 +68,7 @@ def GetCmdStatusAndOutput(args, cwd=None, shell=False):
     logging.critical(stderr)
   logging.info(stdout[:4096])  # Truncate output longer than 4k.
   return (exit_code, stdout)
+
 
 class OutDirectory(object):
   _out_directory = os.path.join(constants.CHROME_DIR, 'out')
