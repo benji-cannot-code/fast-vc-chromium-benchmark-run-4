c4bbf83b169480d768ef8bbb0503a6ce2c3d5ff1
commit-bot@chromium.org <commit-bot@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-10 15:22:34 +0000

    Implement FEFlood filter for the skia accelerated DAG.  For now (until filter effect regions are properly implemented), we use the size of the source bitmap for the size of the region to fill.
    BUG=229623
    
    
    Review URL: https://chromiumcodereview.appspot.com/13820029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148058 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/platform/graphics/filters/FEFlood.h
+++ third_party/WebKit/Source/WebCore/platform/graphics/filters/FEFlood.h
@@ -49,6 +49,7 @@ public:
 #if ENABLE(OPENCL)
     virtual bool platformApplyOpenCL();
 #endif
+    virtual SkImageFilter* createImageFilter(SkiaImageFilterBuilder*);
     virtual void dump();
 
     virtual void determineAbsolutePaintRect() { setAbsolutePaintRect(enclosingIntRect(maxEffectRect())); }
