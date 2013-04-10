9d9812fbe9d734a6c9fcf11ff307742b2dc32fad
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 20:24:36 +0000

    Don't set MODE_DEFAULT when adding HPKP header.
    Leave that to the default value, or whatever value an HSTS header set.
    
    TEST=net_unittests; after visiting https://www.mv-rechberghausen.de, query
    that domain in chrome://net-internal/#hsts and expect pins AND mode STRICT.
    
    BUG=226068
    
    
    Review URL: https://chromiumcodereview.appspot.com/13483007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193443 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_security_headers_unittest.cc
+++ net/http/http_security_headers_unittest.cc
@@ -423,5 +423,5 @@ TEST_F(HttpSecurityHeadersTest, ValidPinsHeadersSHA1) {
 TEST_F(HttpSecurityHeadersTest, ValidPinsHeadersSHA256) {
   TestValidPinsHeaders(HASH_VALUE_SHA256);
 }
-};
 
+};    // namespace net
