0cc40dd149c23cf6baa627d599cccaa9357c5884
felipeg@chromium.org <felipeg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 17:59:28 +0000

    Add Cache size to the Simple Index.
    Review URL: https://chromiumcodereview.appspot.com/13913010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193419 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/simple/simple_entry_impl.cc
+++ net/disk_cache/simple/simple_entry_impl.cc
@@ -151,7 +151,8 @@ int SimpleEntryImpl::ReadData(int index,
   index_->UseIfExists(key_);
   SynchronousOperationCallback sync_operation_callback =
       base::Bind(&SimpleEntryImpl::EntryOperationComplete,
-                 callback, weak_ptr_factory_.GetWeakPtr(), synchronous_entry_);
+                 index_, callback, weak_ptr_factory_.GetWeakPtr(),
+                 synchronous_entry_);
   WorkerPool::PostTask(FROM_HERE,
                        base::Bind(&SimpleSynchronousEntry::ReadData,
                                   base::Unretained(synchronous_entry_),
@@ -176,7 +177,8 @@ int SimpleEntryImpl::WriteData(int index,
   index_->UseIfExists(key_);
   SynchronousOperationCallback sync_operation_callback =
       base::Bind(&SimpleEntryImpl::EntryOperationComplete,
-                 callback, weak_ptr_factory_.GetWeakPtr(), synchronous_entry_);
+                 index_, callback, weak_ptr_factory_.GetWeakPtr(),
+                 synchronous_entry_);
   WorkerPool::PostTask(FROM_HERE,
                        base::Bind(&SimpleSynchronousEntry::WriteData,
                                   base::Unretained(synchronous_entry_),
@@ -274,10 +276,15 @@ void SimpleEntryImpl::CreationOperationComplete(
 
 // static
 void SimpleEntryImpl::EntryOperationComplete(
+    base::WeakPtr<SimpleIndex> index,
     const CompletionCallback& completion_callback,
     base::WeakPtr<SimpleEntryImpl> entry,
     SimpleSynchronousEntry* sync_entry,
     int result) {
+  DCHECK(sync_entry);
+  if (index)
+    index->UpdateEntrySize(sync_entry->key(), sync_entry->GetFileSize());
+
   if (entry) {
     DCHECK(entry->synchronous_entry_in_use_by_worker_);
     entry->synchronous_entry_in_use_by_worker_ = false;
