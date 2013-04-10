4ce186f5f6aebc5f3620eceab537388530c85898
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-10 23:58:33 +0000

    Extract Modules from WebCore
    This CL moves Source/WebCore/Modules to Source/modules. This CL tries to
    minimize the change to the GYP files. We'll likely want to do a more extensive
    modification shortly. This CL is based on the directory reorganization plan
    from:
    
    https://docs.google.com/a/chromium.org/document/d/1BCRkA5ERZtBMXkXB1aMFKKLl5LR_Wq5LpAHtlEf1j_c/edit
    
    Rubber-stamped by Eric Seidel.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "DatabaseManager.h"
 #include "RuntimeEnabledFeatures.h"
 #include "WebMediaPlayerClientImpl.h"
-#include "Modules/websockets/WebSocket.h"
+#include "modules/websockets/WebSocket.h"
 
 #include <wtf/UnusedParam.h>
 
