981bb10de623ec4b99af4c48ede4147a45771473
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 06:30:48 +0000

    Add UX for languages that don't have any associated input method
    BUG=172195
    
    
    Review URL: https://chromiumcodereview.appspot.com/13509006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193333 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/language_options.js
+++ chrome/browser/resources/options/language_options.js
@@ -386,6 +386,9 @@ cr.define('options', function() {
         var languageCode = languageCodes[i];
         var inputMethodIds = this.languageCodeToInputMethodIdsMap_[
             languageCode];
+        if (!inputMethodIds)
+          continue;
+
         // Check if we have active input methods associated with the language.
         for (var j = 0; j < inputMethodIds.length; j++) {
           var inputMethodId = inputMethodIds[j];
@@ -579,6 +582,9 @@ cr.define('options', function() {
           method.hidden = true;
         }
       }
+
+      $('language-options-input-method-none').hidden =
+          (languageCode in this.languageCodeToInputMethodIdsMap_);
 
       if (focusInputMethodId == 'add') {
         $('language-options-add-button').focus();
@@ -799,6 +805,11 @@ cr.define('options', function() {
       // associated with the language code.
       var enginesToBeRemovedSet = {};
       var inputMethodIds = this.languageCodeToInputMethodIdsMap_[languageCode];
+
+      // If this language doesn't have any input methods, it can be deleted.
+      if (!inputMethodIds)
+        return true;
+
       for (var i = 0; i < inputMethodIds.length; i++) {
         enginesToBeRemovedSet[inputMethodIds[i]] = true;
       }
@@ -816,6 +827,9 @@ cr.define('options', function() {
         // we don't remove this time.
         var inputMethodIdsForAnotherLanguage =
             this.languageCodeToInputMethodIdsMap_[languageCodes[i]];
+        if (!inputMethodIdsForAnotherLanguage)
+          continue;
+
         for (var j = 0; j < inputMethodIdsForAnotherLanguage.length; j++) {
           var inputMethodId = inputMethodIdsForAnotherLanguage[j];
           if (inputMethodId in enginesToBeRemovedSet) {
