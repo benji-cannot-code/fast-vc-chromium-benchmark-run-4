100afe2976c785f271bab45d91cf66f8926a3fff
dsinclair@chromium.org <dsinclair@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 22:19:35 +0000

    Allow entries to be closed in test.
    The test was not running an IO message loop where it's
    posting a message to have the entry close. This causes the
    entry to never close and the memory to leak.
    
    BUG=227301
    
    
    Review URL: https://chromiumcodereview.appspot.com/13925008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193475 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/shader_disk_cache_unittest.cc
+++ content/browser/gpu/shader_disk_cache_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread.h"
 #include "content/browser/browser_thread_impl.h"
 #include "content/browser/gpu/shader_disk_cache.h"
+#include "content/public/test/test_browser_thread.h"
 #include "net/base/test_completion_callback.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
@@ -22,7 +23,8 @@ const char kCacheValue[] = "cached value";
 class ShaderDiskCacheTest : public testing::Test {
  public:
   ShaderDiskCacheTest()
-      : cache_thread_(BrowserThread::CACHE, &message_loop_) {
+      : cache_thread_(BrowserThread::CACHE, &message_loop_),
+        io_thread_(BrowserThread::IO, &message_loop_) {
   }
 
   virtual ~ShaderDiskCacheTest() {}
@@ -42,7 +44,8 @@ class ShaderDiskCacheTest : public testing::Test {
 
   base::ScopedTempDir temp_dir_;
   MessageLoopForIO message_loop_;
-  BrowserThreadImpl cache_thread_;
+  TestBrowserThread cache_thread_;
+  TestBrowserThread io_thread_;
 
   DISALLOW_COPY_AND_ASSIGN(ShaderDiskCacheTest);
 };
