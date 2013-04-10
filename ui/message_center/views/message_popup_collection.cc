7f9f94140a04bd96443c74b168773a4750f36085
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 04:11:31 +0000

    Cancel notification toast timeoout for webkit ones.
    Currently webkit notifications are indistinguishable from TYPE_SIMPLE.
    Instead of adding a new type, it just adds a flag to cancel the timeout
    and only the webkit notification code path sets it.
    
    Note that this 'no-timeout' flag is slightly different from the persistency
    of the system notification for ChromeOS. In addition to no-timeout, the
    system notification should not disappear for open/close of message-center,
    i.e. MarkShownAsPopup should be canceled too. It will need an additional
    flag.
    
    BUG=229014
    
    
    Review URL: https://chromiumcodereview.appspot.com/13877002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193315 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_popup_collection.cc
+++ ui/message_center/views/message_popup_collection.cc
@@ -41,6 +41,10 @@ class ToastContentsView : public views::WidgetDelegateView {
     if (notification->priority() > DEFAULT_PRIORITY)
       seconds = kAutocloseHighPriorityDelaySeconds;
     delay_ = base::TimeDelta::FromSeconds(seconds);
+
+    // Creates the timer only when it does the timeout (i.e. not never-timeout).
+    if (!notification->never_timeout())
+      timer_.reset(new base::OneShotTimer<views::Widget>);
   }
 
   views::Widget* CreateWidget(gfx::NativeView parent) {
@@ -73,10 +77,14 @@ class ToastContentsView : public views::WidgetDelegateView {
   }
 
   void SuspendTimer() {
-    timer_.Stop();
+    if (timer_.get())
+      timer_->Stop();
   }
 
   void RestartTimer() {
+    if (!timer_.get())
+      return;
+
     delay_ -= base::Time::Now() - start_time_;
     if (delay_ < base::TimeDelta())
       GetWidget()->Close();
@@ -85,11 +93,14 @@ class ToastContentsView : public views::WidgetDelegateView {
   }
 
   void StartTimer() {
+    if (!timer_.get())
+      return;
+
     start_time_ = base::Time::Now();
-    timer_.Start(FROM_HERE,
-                 delay_,
-                 base::Bind(&views::Widget::Close,
-                            base::Unretained(GetWidget())));
+    timer_->Start(FROM_HERE,
+                  delay_,
+                  base::Bind(&views::Widget::Close,
+                             base::Unretained(GetWidget())));
   }
 
   // Overridden from views::WidgetDelegate:
@@ -98,7 +109,7 @@ class ToastContentsView : public views::WidgetDelegateView {
   }
 
   virtual void WindowClosing() OVERRIDE {
-    if (timer_.IsRunning())
+    if (timer_.get() && timer_->IsRunning())
       SuspendTimer();
   }
 
@@ -137,7 +148,7 @@ class ToastContentsView : public views::WidgetDelegateView {
  private:
   base::TimeDelta delay_;
   base::Time start_time_;
-  base::OneShotTimer<views::Widget> timer_;
+  scoped_ptr<base::OneShotTimer<views::Widget> > timer_;
   base::WeakPtr<MessagePopupCollection> collection_;
 
   DISALLOW_COPY_AND_ASSIGN(ToastContentsView);
