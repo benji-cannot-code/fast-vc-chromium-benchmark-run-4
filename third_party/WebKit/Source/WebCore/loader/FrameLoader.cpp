c308dcc3f8c2e49a8a45c2dd81b8501a6fb9146a
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-10 14:26:09 +0000

    Remove unused runtime setting needsdidfinishloadorderquirk
    This setting existed in WebKit but Chromium never changed it from its default value. We're removing all such unchanging settings in Blink, along with their associated code.
    
    R=abarth@chromium.org
    BUG=226711
    
    Review URL: https://codereview.chromium.org/13864004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148053 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/WebCore/loader/FrameLoader.cpp
@@ -2025,8 +2025,6 @@ void FrameLoader::checkLoadCompleteForThisFrame()
 {
     ASSERT(m_client->hasWebView());
 
-    Settings* settings = m_frame->settings();
-
     switch (m_state) {
         case FrameStateProvisional: {
             if (m_delegateIsHandlingProvisionalLoadError)
@@ -2101,12 +2099,10 @@ void FrameLoader::checkLoadCompleteForThisFrame()
             if (m_stateMachine.creatingInitialEmptyDocument() || !m_stateMachine.committedFirstRealDocumentLoad())
                 return;
 
-            if (!settings->needsDidFinishLoadOrderQuirk()) {
-                m_progressTracker->progressCompleted();
-                if (Page* page = m_frame->page()) {
-                    if (m_frame == page->mainFrame())
-                        page->resetRelevantPaintedObjectCounter();
-                }
+            m_progressTracker->progressCompleted();
+            if (Page* page = m_frame->page()) {
+                if (m_frame == page->mainFrame())
+                    page->resetRelevantPaintedObjectCounter();
             }
 
             const ResourceError& error = dl->mainDocumentError();
@@ -2118,14 +2114,6 @@ void FrameLoader::checkLoadCompleteForThisFrame()
             } else {
                 m_client->dispatchDidFinishLoad();
                 loadingEvent = AXObjectCache::AXLoadingFinished;
-            }
-
-            if (settings->needsDidFinishLoadOrderQuirk()) {
-                m_progressTracker->progressCompleted();
-                if (Page* page = m_frame->page()) {
-                    if (m_frame == page->mainFrame())
-                        page->resetRelevantPaintedObjectCounter();
-                }
             }
 
             // Notify accessibility.
