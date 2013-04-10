c7512dce36f12cac31c4281f2c0ef518ff41980b
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-10 01:47:09 +0000

    No Chromium build uses GLOBAL_FASTMALLOC_NEW so it can be removed.
    R=eseidel@chromium.org
    BUG=none
    TEST=git wktry
    
    Review URL: https://codereview.chromium.org/13811038
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WTF/wtf/Platform.h
+++ third_party/WebKit/Source/WTF/wtf/Platform.h
@@ -468,7 +468,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* We can't override the global operator new and delete on OS(DARWIN) because
  * some object are allocated by WebKit and deallocated by the embedder. */
-#define ENABLE_GLOBAL_FASTMALLOC_NEW 0
 #else /* !OS(DARWIN) */
 /* On non-OS(DARWIN), the "system malloc" is actually TCMalloc anyway, so there's
  * no need to use WebKit's copy of TCMalloc. */
@@ -551,10 +550,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 /* Include feature macros */
 #include <wtf/FeatureDefines.h>
-
-#if !defined(ENABLE_GLOBAL_FASTMALLOC_NEW)
-#define ENABLE_GLOBAL_FASTMALLOC_NEW 1
-#endif
 
 #define ENABLE_DEBUG_WITH_BREAKPOINT 0
 #define ENABLE_SAMPLING_COUNTERS 0
