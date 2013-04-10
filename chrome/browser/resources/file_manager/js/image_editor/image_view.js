57dcf1cd5af0c1ecc040ffe5b2f6e1959c283410
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 04:45:01 +0000

    Fixed cache eviction in Files.app's slide mode.
    Sometimes, prefetching was fast, which caused evicting the image being loaded from cache. As a result, full resolution image was not being loaded properly. This patch addresses this issue by evicting the full-resolution cache in advance, before loading an image from cache. As a result, there is always a space for cache, so there is a guarantee that the image just fetched from cache will not be evicted by the image being prefetched.
    
    TEST=Follow the bug's description.
    BUG=227264
    
    
    Review URL: https://chromiumcodereview.appspot.com/13775010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193324 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/image_editor/image_view.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_view.js
@@ -353,6 +353,11 @@ ImageView.prototype.load = function(url, metadata, effect,
     video.load();
     return;
   }
+
+  // Cache has to be evicted in advance, so the returned cached image is not
+  // evicted later by the prefetched image.
+  this.contentCache_.evictLRU();
+
   var cached = this.contentCache_.getItem(this.contentID_);
   if (cached) {
     displayMainImage(ImageView.LOAD_TYPE_CACHED_FULL,
@@ -805,9 +810,6 @@ ImageView.Cache.prototype.putItem = function(id, item, opt_keepLRU) {
   if ((pos >= 0) != (id in this.map_))
     throw new Error('Inconsistent cache state');
 
-  if ((pos >= 0) && (item != this.map_[id]))
-    this.deleteItem_(this.map_[id]);
-
   if (id in this.map_) {
     if (!opt_keepLRU) {
       // Move to the end (most recently used).
@@ -819,6 +821,8 @@ ImageView.Cache.prototype.putItem = function(id, item, opt_keepLRU) {
     this.order_.push(id);
   }
 
+  if ((pos >= 0) && (item != this.map_[id]))
+    this.deleteItem_(this.map_[id]);
   this.map_[id] = item;
 
   if (this.order_.length > this.capacity_)
