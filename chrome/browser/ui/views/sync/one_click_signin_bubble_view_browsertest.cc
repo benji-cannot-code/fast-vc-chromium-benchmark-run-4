1b8c16940fc0619b29b38a80f0bf0b1eefb782f2
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 17:12:04 +0000

    views: Convert one_click_signin_bubble_view* to LabelButton.
    BUG=155363
    R=msw@chromium.org
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/13991002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193402 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/sync/one_click_signin_bubble_view_browsertest.cc
+++ chrome/browser/ui/views/sync/one_click_signin_bubble_view_browsertest.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/in_process_browser_test.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/common/page_transition_types.h"
-#include "ui/views/controls/button/text_button.h"
+#include "ui/views/controls/button/label_button.h"
 
 class OneClickSigninBubbleViewBrowserTest : public InProcessBrowserTest {
  public:
