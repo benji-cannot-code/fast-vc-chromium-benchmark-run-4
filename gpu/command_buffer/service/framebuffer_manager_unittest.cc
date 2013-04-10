7dcff1b3a733e1e1349ee20782da0c47d6344304
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 02:52:38 +0000

    Force GPU switch with CGLSetVirtualScreen only for compositor
    Only compositor contexts are known to use only the subset of GL
    that can be safely migrated between the iGPU and the dGPU. Mark
    those contexts as safe to forcibly transition between the GPUs.
    
    In particular, Intel GPUs support using a GL_STENCIL_INDEX8
    attachment with no depth buffer, but NV GPUs don't. Skia will use
    this FBO combo if it is listed as available.
    
    If we use this combo on the iGPU and then forcibly transition the
    GL context using this combo from the iGPU to the dGPU, corruption
    occurs on NV, and a hang happens on AMD.
    
    Further, if we cache complete FBO combos, then we will cache this
    combo as valid when using the iGPU, and claim that it's valid when
    using the dGPU, resulting in GL errors.
    
    BUG=180876
    
    Review URL: https://chromiumcodereview.appspot.com/13746002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193302 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/framebuffer_manager_unittest.cc
+++ gpu/command_buffer/service/framebuffer_manager_unittest.cc
@@ -679,6 +679,11 @@ TEST_F(FramebufferInfoTest, GetStatus) {
   framebuffer_->GetStatus(&texture_manager_, GL_FRAMEBUFFER);
 
   // Check a second call for the same type does not call anything
+  if (!framebuffer_->AllowFramebufferComboCompleteMapForTesting()) {
+    EXPECT_CALL(*gl_, CheckFramebufferStatusEXT(GL_FRAMEBUFFER))
+        .WillOnce(Return(GL_FRAMEBUFFER_COMPLETE))
+        .RetiresOnSaturation();
+  }
   framebuffer_->GetStatus(&texture_manager_, GL_FRAMEBUFFER);
 
   // Check changing the attachments calls CheckFramebufferStatus.
@@ -690,6 +695,11 @@ TEST_F(FramebufferInfoTest, GetStatus) {
   framebuffer_->GetStatus(&texture_manager_, GL_FRAMEBUFFER);
 
   // Check a second call for the same type does not call anything.
+  if (!framebuffer_->AllowFramebufferComboCompleteMapForTesting()) {
+    EXPECT_CALL(*gl_, CheckFramebufferStatusEXT(GL_FRAMEBUFFER))
+        .WillOnce(Return(GL_FRAMEBUFFER_COMPLETE))
+        .RetiresOnSaturation();
+  }
   framebuffer_->GetStatus(&texture_manager_, GL_FRAMEBUFFER);
 
   // Check a second call with a different target calls CheckFramebufferStatus.
@@ -699,6 +709,11 @@ TEST_F(FramebufferInfoTest, GetStatus) {
   framebuffer_->GetStatus(&texture_manager_, GL_READ_FRAMEBUFFER);
 
   // Check a second call for the same type does not call anything.
+  if (!framebuffer_->AllowFramebufferComboCompleteMapForTesting()) {
+    EXPECT_CALL(*gl_, CheckFramebufferStatusEXT(GL_READ_FRAMEBUFFER))
+        .WillOnce(Return(GL_FRAMEBUFFER_COMPLETE))
+        .RetiresOnSaturation();
+  }
   framebuffer_->GetStatus(&texture_manager_, GL_READ_FRAMEBUFFER);
 
   // Check adding another attachment calls CheckFramebufferStatus.
@@ -709,6 +724,11 @@ TEST_F(FramebufferInfoTest, GetStatus) {
   framebuffer_->GetStatus(&texture_manager_, GL_READ_FRAMEBUFFER);
 
   // Check a second call for the same type does not call anything.
+  if (!framebuffer_->AllowFramebufferComboCompleteMapForTesting()) {
+    EXPECT_CALL(*gl_, CheckFramebufferStatusEXT(GL_READ_FRAMEBUFFER))
+        .WillOnce(Return(GL_FRAMEBUFFER_COMPLETE))
+        .RetiresOnSaturation();
+  }
   framebuffer_->GetStatus(&texture_manager_, GL_READ_FRAMEBUFFER);
 
   // Check changing the format calls CheckFramebuffferStatus.
@@ -727,6 +747,11 @@ TEST_F(FramebufferInfoTest, GetStatus) {
   framebuffer_->GetStatus(&texture_manager_, GL_READ_FRAMEBUFFER);
 
   // Check putting it back does not call CheckFramebufferStatus.
+  if (!framebuffer_->AllowFramebufferComboCompleteMapForTesting()) {
+    EXPECT_CALL(*gl_, CheckFramebufferStatusEXT(GL_READ_FRAMEBUFFER))
+        .WillOnce(Return(GL_FRAMEBUFFER_COMPLETE))
+        .RetiresOnSaturation();
+  }
   TestHelper::SetTexParameterWithExpectations(
       gl_.get(), decoder_.get(), &texture_manager_,
       texture2, GL_TEXTURE_WRAP_S, GL_REPEAT, GL_NO_ERROR);
@@ -734,6 +759,11 @@ TEST_F(FramebufferInfoTest, GetStatus) {
 
   // Check Unbinding does not call CheckFramebufferStatus
   framebuffer_->UnbindRenderbuffer(GL_RENDERBUFFER, renderbuffer1);
+  if (!framebuffer_->AllowFramebufferComboCompleteMapForTesting()) {
+    EXPECT_CALL(*gl_, CheckFramebufferStatusEXT(GL_READ_FRAMEBUFFER))
+        .WillOnce(Return(GL_FRAMEBUFFER_COMPLETE))
+        .RetiresOnSaturation();
+  }
   framebuffer_->GetStatus(&texture_manager_, GL_READ_FRAMEBUFFER);
 }
 
