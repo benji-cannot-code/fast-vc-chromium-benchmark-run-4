595616c1ad91c874a6539a9fcbaa825ed7ac8916
scheib@chromium.org <scheib@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 08:31:13 +0000

    Implement NativeAppWindowGtk::IsFullscreen.
    Previously it always returned false.
    This uses the same implementation as BrowserWindowGtk::IsFullscreen.
    
    BUG=222513
    
    Review URL: https://chromiumcodereview.appspot.com/12854008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193345 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
+++ chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
@@ -158,7 +158,7 @@ bool NativeAppWindowGtk::IsMinimized() const {
 }
 
 bool NativeAppWindowGtk::IsFullscreen() const {
-  return false;
+  return (state_ & GDK_WINDOW_STATE_FULLSCREEN);
 }
 
 gfx::NativeWindow NativeAppWindowGtk::GetNativeWindow() {
