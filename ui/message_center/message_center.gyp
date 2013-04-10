bc286a4ed3279f3ada04012ea7cf3479c7142e88
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-10 03:39:26 +0000

    Added text line limits to notifications.
    This is done through a new BoundedLabel views::Label subclass that
    supports a max_lines value. This class comes with unit tests covering
    its basic functionality.
    
    BUG=168939,168940
    R=mukai@chromium.org
    
    
    Review URL: https://chromiumcodereview.appspot.com/13544007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193309 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center.gyp
+++ ui/message_center/message_center.gyp
@@ -51,6 +51,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'notification_types.h',
         'notifier_settings.cc',
         'notifier_settings.h',
+        'views/bounded_label.cc',
+        'views/bounded_label.h',
         'views/message_bubble_base.cc',
         'views/message_bubble_base.h',
         'views/message_center_bubble.cc',
@@ -120,12 +122,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }],
         ['toolkit_views==1', {
           'dependencies': [
-            # Compositor is needed by message_center_view_unittest.cc.
+            # Compositor is needed by message_center_view_unittest.cc
+            # and for the fonts used by bounded_label_unittest.cc.
             '../compositor/compositor.gyp:compositor',
             '../compositor/compositor.gyp:compositor_test_support',
             '../views/views.gyp:views',
           ],
           'sources': [
+            'views/bounded_label_unittest.cc',
             'views/message_center_view_unittest.cc',
           ],
         }],
--- ui/message_center/views/notification_view.h
+++ ui/message_center/views/notification_view.h
@@ -11,12 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/message_center_export.h"
 #include "ui/message_center/views/message_view.h"
 
-namespace views {
-class Label;
-}  // namespace views
-
 namespace message_center {
 
+class BoundedLabel;
 class NotificationChangeObserver;
 
 // View that displays all current types of notification (web, basic, image, and
@@ -50,15 +47,14 @@ class MESSAGE_CENTER_EXPORT NotificationView : public MessageView {
                    bool expanded);
 
  private:
-  // Truncate the very long text if we should. See crbug.com/222151 for the
-  // details.
-  string16 MaybeTruncateText(const string16& text, size_t limit);
+  bool IsExpansionNeeded();
+  size_t GetMessageLineLimit();
 
   // Weak references to NotificationView descendants owned by their parents.
   views::View* background_view_;
   views::View* top_view_;
-  views::Label* title_view_;
-  views::Label* message_view_;
+  BoundedLabel* title_view_;
+  BoundedLabel* message_view_;
   std::vector<views::View*> item_views_;
   views::View* icon_view_;
   views::View* bottom_view_;
