f022093f4e6c2222dc455c88ce755445872bf06b
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 01:33:40 +0000

    [Telemetry] Move WillRunPageSet call to a point where browser.http_server exists
    The page cycler's implementation of WillRunPageSet will access http_server.UrlOf
    so http_server needs to be defined. This moves the call to a point after
    SetHTTPServerDirectories has been called.
    
    BUG=None
    TEST=None
    NOTRY=True
    
    
    Review URL: https://chromiumcodereview.appspot.com/13972004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193531 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/page_runner.py
+++ tools/telemetry/telemetry/page/page_runner.py
@@ -61,6 +61,7 @@ class PageRunner(object):
   """Runs a given test against a given test."""
   def __init__(self, page_set):
     self.page_set = page_set
+    self.has_called_will_run_page_set = False
 
   def __enter__(self):
     return self
@@ -142,7 +143,6 @@ class PageRunner(object):
 
     state = _RunState()
     last_archive_path = None
-    is_first_run = True
     results_for_current_run = out_results
 
     try:
@@ -156,7 +156,8 @@ class PageRunner(object):
           state.Close()
           state = _RunState()
           last_archive_path = page.archive_path
-        if (test.discard_first_result and is_first_run):
+        if (test.discard_first_result and
+            not self.has_called_will_run_page_set):
           # If discarding results, substitute a dummy object.
           results_for_current_run = (
             page_benchmark_results.PageBenchmarkResults())
@@ -181,10 +182,6 @@ class PageRunner(object):
               self._SetupTracingTab(state)
 
             self._WaitForThermalThrottlingIfNeeded(state.browser.platform)
-
-            if is_first_run:
-              is_first_run = False
-              test.WillRunPageSet(state.tab, results_for_current_run)
 
             try:
               self._RunPage(options, page, state.tab, test,
@@ -339,6 +336,10 @@ class PageRunner(object):
         logging.info(msg)
         results.AddFailure(page, msg, "")
         return False
+
+    if not self.has_called_will_run_page_set:
+      self.has_called_will_run_page_set = True
+      test.WillRunPageSet(tab, results)
 
     test.WillNavigateToPage(page, tab)
     tab.Navigate(target_side_url, page.script_to_evaluate_on_commit)
