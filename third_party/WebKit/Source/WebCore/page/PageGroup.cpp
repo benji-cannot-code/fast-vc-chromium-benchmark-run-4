bd9642aba916e70b1df5b8a14259ce2c538dc74f
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-11 20:04:55 +0000

    Remove unused source files previously used by PLATFORM(MAC)
    None of these files are referenced in any gyp or gypi file.
    
    R=abarth
    
    Review URL: https://chromiumcodereview.appspot.com/14123002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/page/PageGroup.cpp
+++ third_party/WebKit/Source/WebCore/page/PageGroup.cpp
@@ -40,11 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "StorageNamespace.h"
 
 #if ENABLE(VIDEO_TRACK)
-#if (PLATFORM(MAC) && !PLATFORM(IOS)) || HAVE(MEDIA_ACCESSIBILITY_FRAMEWORK)
-#include "CaptionUserPreferencesMac.h"
-#else
 #include "CaptionUserPreferences.h"
-#endif
 #endif
 
 #include "VisitedLinks.h"
@@ -407,11 +403,7 @@ void PageGroup::captionPreferencesChanged()
 CaptionUserPreferences* PageGroup::captionPreferences()
 {
     if (!m_captionPreferences)
-#if (PLATFORM(MAC) && !PLATFORM(IOS)) || HAVE(MEDIA_ACCESSIBILITY_FRAMEWORK)
-        m_captionPreferences = CaptionUserPreferencesMac::create(this);
-#else
         m_captionPreferences = CaptionUserPreferences::create(this);
-#endif
 
     return m_captionPreferences.get();
 }
