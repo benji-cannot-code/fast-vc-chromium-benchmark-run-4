ae0f4b6757212b720bd31b96151d07df6fc81107
benquan@chromium.org <benquan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 06:10:23 +0000

    Fail autocheckout if there is no forms in the page.
    BUG=229617
    
    
    Review URL: https://chromiumcodereview.appspot.com/13947035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/tab_autofill_manager_delegate.cc
+++ chrome/browser/ui/autofill/tab_autofill_manager_delegate.cc
@@ -100,6 +100,8 @@ void TabAutofillManagerDelegate::SetSyncStateChangedCallback(
 }
 
 void TabAutofillManagerDelegate::OnAutocheckoutError() {
+  // |dialog_controller_| is a WeakPtr, but we require it to be present when
+  // |OnAutocheckoutError| is called, so we intentionally do not do NULL check.
   dialog_controller_->OnAutocheckoutError();
 }
 
@@ -210,6 +212,8 @@ void TabAutofillManagerDelegate::HideAutofillPopup() {
 }
 
 void TabAutofillManagerDelegate::UpdateProgressBar(double value) {
+  // |dialog_controller_| is a WeakPtr, but we require it to be present when
+  // |UpdateProgressBar| is called, so we intentionally do not do NULL check.
   dialog_controller_->UpdateProgressBar(value);
 }
 
