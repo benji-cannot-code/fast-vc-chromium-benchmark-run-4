81b16fa8b9da0a22643a76d9dfdc266ba5ae0cb7
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 07:53:52 +0000

    Revert 193431 "CrOS: Enable threaded UI compositor by default."
    Suspected to have caused timeouts in CrOS asan/valgrind tests.
    
    > CrOS: Enable threaded UI compositor by default.
    > 
    > BUG=173417
    > 
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12088092
    
    TBR=backer@chromium.org
    Review URL: https://codereview.chromium.org/14157002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193598 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor_switches.h
+++ ui/compositor/compositor_switches.h
@@ -11,9 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 COMPOSITOR_EXPORT extern const char kDisableTestCompositor[];
-COMPOSITOR_EXPORT extern const char kUIDisableThreadedCompositing[];
-COMPOSITOR_EXPORT extern const char kUIEnableSoftwareCompositing[];
 COMPOSITOR_EXPORT extern const char kUIEnableThreadedCompositing[];
+COMPOSITOR_EXPORT extern const char kUIEnableSoftwareCompositing[];
 COMPOSITOR_EXPORT extern const char kUIMaxFramesPending[];
 COMPOSITOR_EXPORT extern const char kUIShowPaintRects[];
 
