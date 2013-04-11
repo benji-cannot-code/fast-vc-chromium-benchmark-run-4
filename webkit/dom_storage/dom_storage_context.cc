9bac341c8e4857f410b17178f7e2e4fc5937ec99
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 06:04:56 +0000

    Purge in-memory localStorage areas if the # of areas exceeds the limit
    BUG=178980
    TEST=manual (verified the memory stops increasing at a certain level)
    
    Review URL: https://chromiumcodereview.appspot.com/12398008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193585 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/dom_storage/dom_storage_context.cc
+++ webkit/dom_storage/dom_storage_context.cc
@@ -168,7 +168,7 @@ void DomStorageContext::PurgeMemory() {
   StorageNamespaceMap::iterator found =
       namespaces_.find(kLocalStorageNamespaceId);
   if (found != namespaces_.end())
-    found->second->PurgeMemory();
+    found->second->PurgeMemory(DomStorageNamespace::PURGE_AGGRESSIVE);
 }
 
 void DomStorageContext::Shutdown() {
