b0630c4e12c1d3f132903de539ced58a77f8df48
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 20:59:50 +0000

    Support custom randomization seed for VariationsService trials.
    BUG=229486
    TEST=New unit test.
    
    Review URL: https://chromiumcodereview.appspot.com/13928017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193739 0039d316-1c4b-4281-b951-d872f2087c98

--- base/metrics/field_trial.cc
+++ base/metrics/field_trial.cc
@@ -76,6 +76,11 @@ FieldTrial::EntropyProvider::~EntropyProvider() {
 }
 
 void FieldTrial::UseOneTimeRandomization() {
+  UseOneTimeRandomizationWithCustomSeed(0);
+}
+
+void FieldTrial::UseOneTimeRandomizationWithCustomSeed(
+    uint32 randomization_seed) {
   // No need to specify randomization when the group choice was forced.
   if (forced_)
     return;
@@ -90,7 +95,8 @@ void FieldTrial::UseOneTimeRandomization() {
   }
 
   random_ = static_cast<Probability>(
-      divisor_ * entropy_provider->GetEntropyForTrial(trial_name_));
+      divisor_ * entropy_provider->GetEntropyForTrial(trial_name_,
+                                                      randomization_seed));
 }
 
 void FieldTrial::Disable() {
