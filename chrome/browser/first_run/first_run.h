28a06d3f3fc8c6d9818abc3dbd9722104768c2eb
macourteau@chromium.org <macourteau@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 00:14:36 +0000

    Add a short delay before showing the first run bubble.
    R=grt@chromium.org
    BUG=223939
    
    
    Review URL: https://chromiumcodereview.appspot.com/13958003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193513 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/first_run/first_run.h
+++ chrome/browser/first_run/first_run.h
@@ -13,10 +13,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
 #include "base/memory/ref_counted.h"
+#include "base/memory/weak_ptr.h"
+#include "chrome/browser/ui/browser_list_observer.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "ui/gfx/native_widget_types.h"
 
+class Browser;
 class CommandLine;
 class GURL;
 class PrefRegistrySyncable;
@@ -185,7 +188,10 @@ ProcessMasterPreferencesResult ProcessMasterPreferences(
 
 // Show the first run search engine bubble at the first appropriate opportunity.
 // This bubble may be delayed by other UI, like global errors and sync promos.
-class FirstRunBubbleLauncher : public content::NotificationObserver {
+class FirstRunBubbleLauncher
+    : public content::NotificationObserver,
+      public chrome::BrowserListObserver,
+      public base::SupportsWeakPtr<FirstRunBubbleLauncher> {
  public:
   // Show the bubble at the first appropriate opportunity. This function
   // instantiates a FirstRunBubbleLauncher, which manages its own lifetime.
@@ -200,7 +206,13 @@ class FirstRunBubbleLauncher : public content::NotificationObserver {
                        const content::NotificationSource& source,
                        const content::NotificationDetails& details) OVERRIDE;
 
+  // chrome::BrowserListObserver override:
+  virtual void OnBrowserRemoved(Browser* browser) OVERRIDE;
+
+  void DoShowFirstRunBubble();
+
   content::NotificationRegistrar registrar_;
+  Browser* browser_;
 
   DISALLOW_COPY_AND_ASSIGN(FirstRunBubbleLauncher);
 };
