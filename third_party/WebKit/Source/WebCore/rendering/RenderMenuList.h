ab1a858944f74e6f868460999ceb8edc7d5142e0
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-11 14:47:29 +0000

    Remove PopMenuClient::shouldPopOver
    This function is never used and all the implementations
    return false. POPUP_MENU_PULLS_DOWN was used in the
    (now removed) RenderMenuList::shouldPopOver.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/14105004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148190 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/rendering/RenderMenuList.h
+++ third_party/WebKit/Source/WebCore/rendering/RenderMenuList.h
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "PopupMenuClient.h"
 #include "RenderFlexibleBox.h"
 
-#define POPUP_MENU_PULLS_DOWN 1
-
 namespace WebCore {
 
 class HTMLSelectElement;
