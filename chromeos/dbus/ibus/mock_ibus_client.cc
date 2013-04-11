3f309fb1be848f18c739f8a5154e9ab278b85beb
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 16:36:44 +0000

    Introduce InputMethodEngineIBusBrowserTest.
    This is first CL to add browser tests for Extension IME.
    I will add more tests if this is granted.
    
    BUG=179912
    TEST=ran browser_tests --gtest_filter=InputMethodEngine*
    
    
    Review URL: https://chromiumcodereview.appspot.com/13779004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193664 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/ibus/mock_ibus_client.cc
+++ chromeos/dbus/ibus/mock_ibus_client.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 MockIBusClient::MockIBusClient()
-    : create_input_context_call_count_(0) {
+    : create_input_context_call_count_(0),
+      register_component_call_count_(0) {
 }
 
 MockIBusClient::~MockIBusClient() {}
