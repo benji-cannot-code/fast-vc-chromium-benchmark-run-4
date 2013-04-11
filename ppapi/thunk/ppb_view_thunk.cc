677b55d2bf2f0348e00f6108f30eaaa84be56715
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 06:53:38 +0000

    Pepper API: Re-run generator.py.
    ppb_view_thunk.cc was missing some logs, causing the generator to change the
    file every time it is run. (As of SVN 193110.)
    
    BUG=230201
    
    
    Review URL: https://chromiumcodereview.appspot.com/14130002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193592 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/thunk/ppb_view_thunk.cc
+++ ppapi/thunk/ppb_view_thunk.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// From ppb_view.idl modified Thu Mar 28 11:12:59 2013.
+// From ppb_view.idl modified Wed Apr 10 14:15:15 2013.
 
 #include "ppapi/c/pp_errors.h"
 #include "ppapi/c/ppb_view.h"
@@ -66,6 +66,7 @@ PP_Bool GetClipRect(PP_Resource resource, struct PP_Rect* clip) {
 }
 
 float GetDeviceScale(PP_Resource resource) {
+  VLOG(4) << "PPB_View::GetDeviceScale()";
   EnterResource<PPB_View_API> enter(resource, true);
   if (enter.failed())
     return 0.0f;
@@ -73,6 +74,7 @@ float GetDeviceScale(PP_Resource resource) {
 }
 
 float GetCSSScale(PP_Resource resource) {
+  VLOG(4) << "PPB_View::GetCSSScale()";
   EnterResource<PPB_View_API> enter(resource, true);
   if (enter.failed())
     return 0.0f;
