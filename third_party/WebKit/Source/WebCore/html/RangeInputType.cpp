04c50a5c9ca025e7af10b3966b76573294402c24
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-11 16:05:57 +0000

    Remove the ENABLE_TOUCH_SLIDER compile-time flag.
    This purely mechanical change should have no web-visible effect, as Chromium
    never disabled the feature.
    
    Review URL: https://chromiumcodereview.appspot.com/14103006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148196 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/RangeInputType.cpp
+++ third_party/WebKit/Source/WebCore/html/RangeInputType.cpp
@@ -169,7 +169,6 @@ void RangeInputType::handleMouseDownEvent(MouseEvent* event)
 }
 
 #if ENABLE(TOUCH_EVENTS)
-#if ENABLE(TOUCH_SLIDER)
 void RangeInputType::handleTouchEvent(TouchEvent* event)
 {
     if (element()->isDisabledOrReadOnly())
@@ -193,7 +192,6 @@ bool RangeInputType::hasTouchEventHandler() const
 {
     return true;
 }
-#endif
 #endif
 
 void RangeInputType::handleKeydownEvent(KeyboardEvent* event)
