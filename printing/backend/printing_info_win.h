47caee85427efecf5368905e5fdd4a3f84cb7314
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 04:48:01 +0000

    Rewrite scoped_array<T> to scoped_ptr<T[]> in printing.
    This is a manual cleanup pass using sed for files which are not built on
    Linux.
    
    BUG=171111
    
    
    Review URL: https://chromiumcodereview.appspot.com/14115002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193577 0039d316-1c4b-4281-b951-d872f2087c98

--- printing/backend/printing_info_win.h
+++ printing/backend/printing_info_win.h
@@ -34,7 +34,7 @@ class PrinterInfo {
   }
 
  private:
-  scoped_array<uint8> buffer_;
+  scoped_ptr<uint8[]> buffer_;
 };
 
 // This class is designed to work with DRIVER_INFO_X structures
@@ -52,7 +52,7 @@ class DriverInfo {
   }
 
  private:
-  scoped_array<uint8> buffer_;
+  scoped_ptr<uint8[]> buffer_;
 };
 
 }  // namespace internal
