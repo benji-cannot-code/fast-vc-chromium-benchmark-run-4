f1166a1124f1b72a0aef1ee2615b36207f6ad1a6
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 16:22:50 +0000

    Revert 193657 "[chromeos] Remove more chrome dependencies from B..."
    > [chromeos] Remove more chrome dependencies from BurnManager.
    > 
    > BUG=180711
    > 
    > Review URL: https://codereview.chromium.org/13800022
    
    TBR=phajdan.jr@chromium.org
    Review URL: https://codereview.chromium.org/14172009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193659 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/imageburner/burn_controller.cc
+++ chrome/browser/chromeos/imageburner/burn_controller.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/cros/cros_library.h"
 #include "chrome/browser/chromeos/imageburner/burn_manager.h"
-#include "chromeos/network/network_state_handler.h"
 #include "googleurl/src/gurl.h"
 #include "grit/generated_resources.h"
 
@@ -128,7 +127,7 @@ class BurnControllerImpl
                               const base::FilePath& target_file_path) OVERRIDE {
     if (!target_device_path.empty() && !target_file_path.empty() &&
         state_machine_->new_burn_posible()) {
-      if (!NetworkStateHandler::Get()->DefaultNetwork()) {
+      if (!burn_manager_->IsNetworkConnected()) {
         delegate_->OnNoNetwork();
         return;
       }
