18e30962891bd4cd63e8884c3528827ce5fae5c4
ckocagil@chromium.org <ckocagil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-11 17:12:23 +0000

    NativeTextfieldViews: Show the drop cursor when dragging text. Enables RenderText users to draw cursors at specified positions through RenderText::DrawCursor. Uses this method to implement the drop cursor in NativeTextfieldViews.
    BUG=229661
    TEST=Use Views Textfield; select text in omnibox; drag it around the omnibox; a cursor should be displayed at where the text will be dropped
    
    Review URL: https://chromiumcodereview.appspot.com/13982003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193674 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text.h
+++ ui/gfx/render_text.h
@@ -276,6 +276,9 @@ class UI_EXPORT RenderText {
 
   void Draw(Canvas* canvas);
 
+  // Draws a cursor at |position|.
+  void DrawCursor(Canvas* canvas, const SelectionModel& position);
+
   // Draw the selected text without a cursor or selection highlight.
   void DrawSelectedText(Canvas* canvas);
 
@@ -450,9 +453,8 @@ class UI_EXPORT RenderText {
   // cursor is within the visible display area.
   void UpdateCachedBoundsAndOffset();
 
-  // Draw the selection and cursor.
+  // Draw the selection.
   void DrawSelection(Canvas* canvas);
-  void DrawCursor(Canvas* canvas);
 
   // Logical UTF-16 string data to be drawn.
   string16 text_;
