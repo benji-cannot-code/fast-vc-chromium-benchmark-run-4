c0487f24b46ea67af663ac557e74bd87fea7e150
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-11 01:42:20 +0000

    MediaStream should fire ended event when all tracks are ended
    BUG=227485
    
    Review URL: https://codereview.chromium.org/13776002
    Patch from Li Yin <li.yin@intel.com>.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148133 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediastream/MediaStreamTrack.cpp
+++ third_party/WebKit/Source/modules/mediastream/MediaStreamTrack.cpp
@@ -136,8 +136,18 @@ void MediaStreamTrack::sourceChangedState()
         break;
     case MediaStreamSource::ReadyStateEnded:
         dispatchEvent(Event::create(eventNames().endedEvent, false, false));
+        didEndTrack();
         break;
     }
+}
+
+void MediaStreamTrack::didEndTrack()
+{
+    MediaStreamDescriptorClient* client = m_component->stream()->client();
+    if (!client)
+        return;
+
+    client->trackEnded();
 }
 
 MediaStreamComponent* MediaStreamTrack::component()
@@ -148,6 +158,7 @@ MediaStreamComponent* MediaStreamTrack::component()
 void MediaStreamTrack::stop()
 {
     m_stopped = true;
+    didEndTrack();
 }
 
 const AtomicString& MediaStreamTrack::interfaceName() const
--- third_party/WebKit/Source/modules/mediastream/MediaStreamTrack.h
+++ third_party/WebKit/Source/modules/mediastream/MediaStreamTrack.h
@@ -54,6 +54,8 @@ public:
     bool enabled() const;
     void setEnabled(bool);
 
+    void didEndTrack();
+
     String readyState() const;
 
     DEFINE_ATTRIBUTE_EVENT_LISTENER(mute);
