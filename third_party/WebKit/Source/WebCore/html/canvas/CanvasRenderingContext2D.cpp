7b2c9992c27f34dcfbd43393ef8e056a0f3c1cb4
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-12 20:52:23 +0000

    Remove ACCELERATED_2D_CANVAS define
    Keeping the code as if ACCELERATED_2D_CANVAS were enabled.
    
    BUG=230224
    
    Review URL: https://chromiumcodereview.appspot.com/14208002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148306 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/WebCore/html/canvas/CanvasRenderingContext2D.cpp
@@ -149,14 +149,10 @@ CanvasRenderingContext2D::~CanvasRenderingContext2D()
 
 bool CanvasRenderingContext2D::isAccelerated() const
 {
-#if USE(IOSURFACE_CANVAS_BACKING_STORE) || ENABLE(ACCELERATED_2D_CANVAS)
     if (!canvas()->hasCreatedImageBuffer())
         return false;
     GraphicsContext* context = drawingContext();
     return context && context->isAcceleratedContext();
-#else
-    return false;
-#endif
 }
 
 void CanvasRenderingContext2D::reset()
@@ -1382,16 +1378,12 @@ void CanvasRenderingContext2D::drawImage(HTMLCanvasElement* sourceCanvas, const 
 
     checkOrigin(sourceCanvas);
 
-#if ENABLE(ACCELERATED_2D_CANVAS)
     // If we're drawing from one accelerated canvas 2d to another, avoid calling sourceCanvas->makeRenderingResultsAvailable()
     // as that will do a readback to software.
     CanvasRenderingContext* sourceContext = sourceCanvas->renderingContext();
     // FIXME: Implement an accelerated path for drawing from a WebGL canvas to a 2d canvas when possible.
     if (!isAccelerated() || !sourceContext || !sourceContext->isAccelerated() || !sourceContext->is2d())
         sourceCanvas->makeRenderingResultsAvailable();
-#else
-    sourceCanvas->makeRenderingResultsAvailable();
-#endif
 
     if (rectContainsCanvas(dstRect)) {
         c->drawImageBuffer(buffer, ColorSpaceDeviceRGB, dstRect, srcRect, state().m_globalComposite, state().m_globalBlend);
@@ -1732,7 +1724,6 @@ void CanvasRenderingContext2D::didDraw(const FloatRect& r, unsigned options)
     if (!state().m_invertibleCTM)
         return;
 
-#if ENABLE(ACCELERATED_2D_CANVAS)
     // If we are drawing to hardware and we have a composited layer, just call contentChanged().
     if (isAccelerated()) {
         RenderBox* renderBox = canvas()->renderBox();
@@ -1743,7 +1734,6 @@ void CanvasRenderingContext2D::didDraw(const FloatRect& r, unsigned options)
             return;
         }
     }
-#endif
 
     FloatRect dirtyRect = r;
     if (options & CanvasDidDrawApplyTransform) {
@@ -2243,12 +2233,10 @@ const Font& CanvasRenderingContext2D::accessFont()
     return state().m_font;
 }
 
-#if ENABLE(ACCELERATED_2D_CANVAS)
 PlatformLayer* CanvasRenderingContext2D::platformLayer() const
 {
     return canvas()->buffer() ? canvas()->buffer()->platformLayer() : 0;
 }
-#endif
 
 bool CanvasRenderingContext2D::webkitImageSmoothingEnabled() const
 {
