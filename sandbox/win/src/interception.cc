ef0179d44ba06e00360f9f5bd93693716adeb341
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 22:26:07 +0000

    Rewrite scoped_array<T> to scoped_ptr<T[]> in sandbox.
    This is a manual cleanup pass using sed for files which are not built on
    Linux.
    
    BUG=171111
    
    Review URL: https://chromiumcodereview.appspot.com/13888007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194027 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/win/src/interception.cc
+++ sandbox/win/src/interception.cc
@@ -121,7 +121,7 @@ bool InterceptionManager::InitializeInterceptions() {
     return true;  // Nothing to do here
 
   size_t buffer_bytes = GetBufferSize();
-  scoped_array<char> local_buffer(new char[buffer_bytes]);
+  scoped_ptr<char[]> local_buffer(new char[buffer_bytes]);
 
   if (!SetupConfigBuffer(local_buffer.get(), buffer_bytes))
     return false;
