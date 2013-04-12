8020f0d8690b9371f216b25e382548a6b658bcb5
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 22:26:24 +0000

    [Spellcheck] Remove dead IPC message
    The DocumentClosed message could never be sent, since the IPC::Sender
    is already gone by the time we know we need to send this.
    
    In detail: SpellcheckProvider is a RenderViewObserver. When a
    RenderView goes away, it goes through these steps:
    
    1) Call RenderViewObserver::RenderViewGone()
       This sets RenderViewObserver::render_view_ to NULL
    2) Call RenderViewObserver::OnDestruct();
    3) delete RenderViewObserver()
    
    render_view_ is needed to send an IPC message, but RenderViewGone is
    not overrideable. That means we cannot send a notification to the
    browser that a RenderView has left the building.
    
    TBR=tsepez@chromium.org
    R=rlp@chromium.org
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/13966004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194028 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_message_filter_mac.cc
+++ chrome/browser/spellchecker/spellcheck_message_filter_mac.cc
@@ -20,8 +20,6 @@ bool SpellCheckMessageFilterMac::OnMessageReceived(const IPC::Message& message,
                         OnCheckSpelling)
     IPC_MESSAGE_HANDLER(SpellCheckHostMsg_FillSuggestionList,
                         OnFillSuggestionList)
-    IPC_MESSAGE_HANDLER(SpellCheckHostMsg_DocumentClosed,
-                        OnDocumentClosed)
     IPC_MESSAGE_HANDLER(SpellCheckHostMsg_ShowSpellingPanel,
                         OnShowSpellingPanel)
     IPC_MESSAGE_HANDLER(SpellCheckHostMsg_UpdateSpellingPanelWithMisspelledWord,
@@ -47,12 +45,6 @@ void SpellCheckMessageFilterMac::OnFillSuggestionList(
   spellcheck_mac::FillSuggestionList(word, suggestions);
 }
 
-
-void SpellCheckMessageFilterMac::OnDocumentClosed(int route_id) {
-  spellcheck_mac::CloseDocumentWithTag(ToDocumentTag(route_id));
-  RetireDocumentTag(route_id);
-}
-
 void SpellCheckMessageFilterMac::OnShowSpellingPanel(bool show) {
   spellcheck_mac::ShowSpellingPanel(show);
 }
@@ -76,7 +68,11 @@ int SpellCheckMessageFilterMac::ToDocumentTag(int route_id) {
   return tag_map_[route_id];
 }
 
+// TODO(groby): We are currently not notified of retired tags. We need
+// to track destruction of RenderViewHosts on the browser process side
+// to update our mappings when a document goes away.
 void SpellCheckMessageFilterMac::RetireDocumentTag(int route_id) {
+  spellcheck_mac::CloseDocumentWithTag(ToDocumentTag(route_id));
   tag_map_.erase(route_id);
 }
 
--- chrome/renderer/spellchecker/spellcheck_provider.cc
+++ chrome/renderer/spellchecker/spellcheck_provider.cc
@@ -58,9 +58,6 @@ SpellCheckProvider::SpellCheckProvider(
 }
 
 SpellCheckProvider::~SpellCheckProvider() {
-#if defined(OS_MACOSX)
-  Send(new SpellCheckHostMsg_DocumentClosed(routing_id(), routing_id()));
-#endif
 }
 
 void SpellCheckProvider::RequestTextChecking(
