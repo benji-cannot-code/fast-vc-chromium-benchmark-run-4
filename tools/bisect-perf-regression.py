87c5e6ba4bb664c92f6fb75922635344fdf47ff0
simonhatch@chromium.org <simonhatch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 21:47:24 +0000

    Create the full path instead of just the leaf directory. On a fresh sync of the depot, the build directories don't exist and the function fails.
    BUG=
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14121017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194021 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/bisect-perf-regression.py
+++ tools/bisect-perf-regression.py
@@ -1567,7 +1567,7 @@ def RmTreeAndMkDir(path_to_dir):
       return False
 
   try:
-    os.mkdir(path_to_dir)
+    os.makedirs(path_to_dir)
   except OSError, e:
     if e.errno != errno.EEXIST:
       return False
