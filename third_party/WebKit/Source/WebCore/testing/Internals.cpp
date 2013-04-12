e9b939882ea86bfdbb1739133754d30d9aeb83fc
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-12 16:52:34 +0000

    Remove the Network Information API implementation.
    The feature is disabled for all of Chromium right now, with no plans
    to enable it.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13820033
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148293 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/testing/Internals.cpp
+++ third_party/WebKit/Source/WebCore/testing/Internals.cpp
@@ -106,11 +106,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "BatteryController.h"
 #endif
 
-#if ENABLE(NETWORK_INFO)
-#include "NetworkInfo.h"
-#include "NetworkInfoController.h"
-#endif
-
 #if ENABLE(PROXIMITY_EVENTS)
 #include "DeviceProximityController.h"
 #endif
@@ -1395,22 +1390,6 @@ void Internals::setBatteryStatus(Document* document, const String& eventType, bo
     UNUSED_PARAM(chargingTime);
     UNUSED_PARAM(dischargingTime);
     UNUSED_PARAM(level);
-#endif
-}
-
-void Internals::setNetworkInformation(Document* document, const String& eventType, double bandwidth, bool metered, ExceptionCode& ec)
-{
-    if (!document || !document->page()) {
-        ec = INVALID_ACCESS_ERR;
-        return;
-    }
-
-#if ENABLE(NETWORK_INFO)
-    NetworkInfoController::from(document->page())->didChangeNetworkInformation(eventType, NetworkInfo::create(bandwidth, metered));
-#else
-    UNUSED_PARAM(eventType);
-    UNUSED_PARAM(bandwidth);
-    UNUSED_PARAM(metered);
 #endif
 }
 
