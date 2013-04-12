4891469bca2177bac41374e9c18e97c87baa35f8
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 18:28:28 +0000

    [Android] Split uiautomator test runner from instrumentation.
    Also, some minor refactoring.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13989007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193967 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/uiautomator/dispatch.py
+++ build/android/pylib/uiautomator/dispatch.py
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-"""Dispatches the instrumentation tests."""
+"""Dispatches the uiautomator tests."""
 
 import logging
 import os
@@ -17,7 +17,7 @@ import test_runner
 
 
 def Dispatch(options):
-  """Dispatches instrumentation tests onto connected device(s).
+  """Dispatches uiautomator tests onto connected device(s).
 
   If possible, this method will attempt to shard the tests to
   all connected devices. Otherwise, dispatch and run tests on one device.
@@ -31,12 +31,12 @@ def Dispatch(options):
   Raises:
     Exception: when there are no attached devices.
   """
-  test_pkg = test_package.TestPackage(options.test_apk_path,
-                                      options.test_apk_jar_path)
+  test_pkg = test_package.TestPackage(
+      options.uiautomator_jar, options.uiautomator_info_jar)
   tests = test_pkg._GetAllMatchingTests(
       options.annotations, options.test_filter)
   if not tests:
-    logging.warning('No instrumentation tests to run with current args.')
+    logging.warning('No uiautomator tests to run with current args.')
     return base_test_result.TestRunResults()
 
   attached_devices = android_commands.GetAttachedDevices()
@@ -46,10 +46,6 @@ def Dispatch(options):
   if options.device:
     assert options.device in attached_devices
     attached_devices = [options.device]
-
-  if len(attached_devices) > 1 and options.wait_for_debugger:
-    logging.warning('Debugger can not be sharded, using first available device')
-    attached_devices = attached_devices[:1]
 
   def TestRunnerFactory(device, shard_index):
     return test_runner.TestRunner(
