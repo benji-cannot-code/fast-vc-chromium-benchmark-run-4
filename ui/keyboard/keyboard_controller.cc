c9b5345a34b1484c8af0b5b9959257109c3f6de2
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 20:46:34 +0000

    keyboard: Fix a crash when the keyboard is created with a valid text-input type.
    The KeyboardController attempts to shwo the keyboard-container after
    creation if the current text-client can accept input. However, the
    container hasn't been parented yet, and so it ends up causing a crash.
    So instead of this, decide the visibility of the keyboard only after
    it has been parented.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/14200002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194006 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/keyboard_controller.cc
+++ ui/keyboard/keyboard_controller.cc
@@ -118,6 +118,8 @@ KeyboardController::KeyboardController(KeyboardControllerProxy* proxy)
 }
 
 KeyboardController::~KeyboardController() {
+  if (container_)
+    container_->RemoveObserver(this);
   proxy_->GetInputMethod()->RemoveObserver(this);
 }
 
@@ -126,6 +128,7 @@ aura::Window* KeyboardController::GetContainerWindow() {
     container_ = new aura::Window(new KeyboardWindowDelegate());
     container_->SetName("KeyboardContainer");
     container_->Init(ui::LAYER_NOT_DRAWN);
+    container_->AddObserver(this);
 
     aura::Window* keyboard = proxy_->GetKeyboardWindow();
     keyboard->Show();
@@ -133,8 +136,6 @@ aura::Window* KeyboardController::GetContainerWindow() {
     container_->SetLayoutManager(
         new KeyboardLayoutManager(container_, keyboard));
     container_->AddChild(keyboard);
-
-    OnTextInputStateChanged(proxy_->GetInputMethod()->GetTextInputClient());
   }
   return container_;
 }
@@ -154,6 +155,16 @@ void KeyboardController::OnTextInputStateChanged(
   // TODO(bryeung): whenever the TextInputClient changes we need to notify the
   // keyboard (with the TextInputType) so that it can reset it's state (e.g.
   // abandon compositions in progress)
+}
+
+void KeyboardController::OnWindowParentChanged(aura::Window* window,
+                                               aura::Window* parent) {
+  OnTextInputStateChanged(proxy_->GetInputMethod()->GetTextInputClient());
+}
+
+void KeyboardController::OnWindowDestroying(aura::Window* window) {
+  DCHECK_EQ(container_, window);
+  container_ = NULL;
 }
 
 }  // namespace keyboard
--- ui/keyboard/keyboard_controller.h
+++ ui/keyboard/keyboard_controller.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "ui/aura/window_observer.h"
 #include "ui/base/ime/input_method_observer.h"
 #include "ui/keyboard/keyboard_export.h"
 
@@ -25,7 +26,8 @@ class KeyboardControllerProxy;
 
 // Provides control of the virtual keyboard, including providing a container,
 // managing object lifetimes and controlling visibility.
-class KEYBOARD_EXPORT KeyboardController : public ui::InputMethodObserver {
+class KEYBOARD_EXPORT KeyboardController : public ui::InputMethodObserver,
+                                           public aura::WindowObserver {
  public:
   // Takes ownership of |proxy|.
   explicit KeyboardController(KeyboardControllerProxy* proxy);
@@ -40,6 +42,11 @@ class KEYBOARD_EXPORT KeyboardController : public ui::InputMethodObserver {
       const ui::TextInputClient* client) OVERRIDE;
 
  private:
+  // aura::WindowObserver overrides
+  virtual void OnWindowParentChanged(aura::Window* window,
+                                     aura::Window* parent) OVERRIDE;
+  virtual void OnWindowDestroying(aura::Window* window) OVERRIDE;
+
   scoped_ptr<KeyboardControllerProxy> proxy_;
   aura::Window* container_;
 
