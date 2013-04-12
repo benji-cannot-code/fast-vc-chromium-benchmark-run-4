34a257353f278533b405c799a3807cc45c9cdb35
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 02:19:04 +0000

    Move various bits of UMA code from ChromeBrowserMain to a new ChromeBrowserMainExtraPartsMetrics class.
    Review URL: https://chromiumcodereview.appspot.com/13868004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193829 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -45,6 +45,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/geolocation/chrome_access_token_store.h"
 #include "chrome/browser/google/google_util.h"
 #include "chrome/browser/media/media_capture_devices_dispatcher.h"
+#include "chrome/browser/metrics/chrome_browser_main_extra_parts_metrics.h"
 #include "chrome/browser/nacl_host/nacl_process_host.h"
 #include "chrome/browser/net/chrome_net_log.h"
 #include "chrome/browser/notifications/desktop_notification_service.h"
@@ -560,6 +561,8 @@ content::BrowserMainParts* ChromeContentBrowserClient::CreateBrowserMainParts(
 #if defined(USE_AURA)
   chrome::AddAuraToolkitExtraParts(main_parts);
 #endif
+
+  chrome::AddMetricsExtraParts(main_parts);
 
   return main_parts;
 }
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -963,6 +963,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/memory_details_win.cc',
         'browser/memory_purger.cc',
         'browser/memory_purger.h',
+        'browser/metrics/chrome_browser_main_extra_parts_metrics.cc',
+        'browser/metrics/chrome_browser_main_extra_parts_metrics.h',
         'browser/metrics/field_trial_synchronizer.cc',
         'browser/metrics/field_trial_synchronizer.h',
         'browser/metrics/metric_event_duration_details.h',
