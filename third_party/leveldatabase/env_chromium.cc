aed9022156593f3c31e435498a30febdefc68520
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 23:50:36 +0000

    Add chrome://tracing entries for leveldb background threads
    R=dgrogan@chromium.org
    
    Review URL: https://codereview.chromium.org/13956014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194052 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/leveldatabase/env_chromium.cc
+++ third_party/leveldatabase/env_chromium.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <deque>
 
 #include "base/at_exit.h"
+#include "base/debug/trace_event.h"
 #include "base/file_util.h"
 #include "base/files/file_path.h"
 #include "base/lazy_instance.h"
@@ -672,6 +673,8 @@ class ChromiumEnv : public Env, public UMALogger {
                           MethodID method,
                           int limit);
 
+  std::string name_;
+
  private:
   const int kMaxRenameTimeMillis;
   // BGThread() is the body of the background thread
@@ -700,11 +703,12 @@ class ChromiumEnv : public Env, public UMALogger {
 };
 
 ChromiumEnv::ChromiumEnv()
-    : page_size_(::base::SysInfo::VMAllocationGranularity()),
+    : name_("LevelDBEnv"),
+      page_size_(::base::SysInfo::VMAllocationGranularity()),
       bgsignal_(&mu_),
       started_bgthread_(false),
       kMaxRenameTimeMillis(1000) {
-  InitHistograms("LevelDBEnv");
+  InitHistograms(name_);
 }
 
 void ChromiumEnv::MakePlatformFileErrorHistogram(
@@ -804,6 +808,8 @@ void ChromiumEnv::Schedule(void (*function)(void*), void* arg) {
 }
 
 void ChromiumEnv::BGThread() {
+  base::PlatformThread::SetName(name_.c_str());
+
   while (true) {
     // Wait until there is an item that is ready to run
     mu_.Acquire();
@@ -816,6 +822,7 @@ void ChromiumEnv::BGThread() {
     queue_.pop_front();
 
     mu_.Release();
+    TRACE_EVENT0("leveldb", "ChromiumEnv::BGThread-Task");
     (*function)(arg);
   }
 }
@@ -827,7 +834,8 @@ void ChromiumEnv::StartThread(void (*function)(void* arg), void* arg) {
 class IDBEnv : public ChromiumEnv {
  public:
   IDBEnv() : ChromiumEnv() {
-    InitHistograms("LevelDBEnv.IDB");
+    name_ = "LevelDBEnv.IDB";
+    InitHistograms(name_);
   }
 };
 
