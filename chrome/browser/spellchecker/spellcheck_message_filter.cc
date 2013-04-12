ffc470ccc5409dea096601650b5047ee14fb580a
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 20:28:32 +0000

    Pass callback identifier when calling spelling service client
    We're passing a 0 by mistake, which causes all spellcheck requests to be
    canceled.
    
    BUG=230879
    
    Review URL: https://chromiumcodereview.appspot.com/13915010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193996 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_message_filter.cc
+++ chrome/browser/spellchecker/spellcheck_message_filter.cc
@@ -132,6 +132,6 @@ void SpellCheckMessageFilter::CallSpellingService(const string16& text,
     base::Bind(&SpellCheckMessageFilter::OnTextCheckComplete,
                base::Unretained(this),
                route_id,
-               0));
+               identifier));
 }
 #endif
