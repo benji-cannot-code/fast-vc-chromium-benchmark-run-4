f809c4664d6b88605ebfa6c3af5b3e757a411ada
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 12:29:59 +0000

    Patch refactors the GpuProcessHost::SurfaceRef class that was previously only used on GTK to ensure that browser process drawing surface wasn't torn down until the GPU process was done with it so that it can be used on Android too.
    [Android notes...]
    Ensure that we don't release the Java side resources used by SurfaceTextureBridge until the gpu process is completely finished with the GL surface. Otherwise we end up with a race
    between the RenderView shutting down and releasing the Java Surface, yet the GPU process continuing to use it.
        
    Bug: b/7697692
    Bug: 119006
    
    Review URL: https://chromiumcodereview.appspot.com/12989029
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193907 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/surface_texture_transport_client_android.cc
+++ content/browser/renderer_host/surface_texture_transport_client_android.cc
@@ -16,13 +16,33 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/Platform/chromium/public/WebGraphicsContext3D.h"
 #include "webkit/compositor_bindings/web_compositor_support_impl.h"
 
+namespace content {
+
 namespace {
 
 static const uint32 kGLTextureExternalOES = 0x8D65;
 
-} // anonymous namespace
+class SurfaceRefAndroid : public GpuSurfaceTracker::SurfaceRef {
+ public:
+  SurfaceRefAndroid(
+      const scoped_refptr<SurfaceTextureBridge>& surface,
+      ANativeWindow* window)
+      : surface_(surface),
+        window_(window) {
+    ANativeWindow_acquire(window_);
+  }
 
-namespace content {
+ private:
+  virtual ~SurfaceRefAndroid() {
+    DCHECK(window_);
+    ANativeWindow_release(window_);
+  }
+
+  scoped_refptr<SurfaceTextureBridge> surface_;
+  ANativeWindow* window_;
+};
+
+} // anonymous namespace
 
 SurfaceTextureTransportClient::SurfaceTextureTransportClient()
     : window_(NULL),
@@ -31,12 +51,6 @@ SurfaceTextureTransportClient::SurfaceTextureTransportClient()
 }
 
 SurfaceTextureTransportClient::~SurfaceTextureTransportClient() {
-  if (surface_id_) {
-    GpuSurfaceTracker::Get()->SetNativeWidget(
-        surface_id_, gfx::kNullAcceleratedWidget);
-  }
-  if (window_)
-    ANativeWindow_release(window_);
 }
 
 scoped_refptr<cc::Layer> SurfaceTextureTransportClient::Initialize() {
@@ -57,10 +71,15 @@ SurfaceTextureTransportClient::GetCompositingSurface(int surface_id) {
   DCHECK(surface_id);
   surface_id_ = surface_id;
 
-  if (!window_)
+  if (!window_) {
     window_ = surface_texture_->CreateSurface();
 
-  GpuSurfaceTracker::Get()->SetNativeWidget(surface_id, window_);
+    GpuSurfaceTracker::Get()->SetNativeWidget(
+        surface_id, window_, new SurfaceRefAndroid(surface_texture_, window_));
+    // SurfaceRefAndroid took ownership (and an extra ref to) window_.
+    ANativeWindow_release(window_);
+  }
+
   return gfx::GLSurfaceHandle(gfx::kNullPluginWindow, gfx::NATIVE_DIRECT);
 }
 
