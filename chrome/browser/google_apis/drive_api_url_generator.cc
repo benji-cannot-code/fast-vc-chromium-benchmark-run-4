ab413c8b97ef34fc16517ea2d8998ed2e9f0de38
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 04:33:45 +0000

    Support includeDeleted query parameter on DriveApiUrlGenerator.
    By this CL, DriveApiUrlGenerator and GetChangelistOperation support includeDeleted parameter for
    change list url. It will be used to re-implement GetAllResourceList.
    
    BUG=224870
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/13928033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193846 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/drive_api_url_generator.cc
+++ chrome/browser/google_apis/drive_api_url_generator.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/google_apis/drive_api_url_generator.h"
 
+#include "base/logging.h"
 #include "base/stringprintf.h"
 #include "base/strings/string_number_conversions.h"
 #include "net/base/escape.h"
@@ -55,12 +56,22 @@ GURL DriveApiUrlGenerator::GetApplistUrl() const {
   return base_url_.Resolve(kDriveV2ApplistUrl);
 }
 
-GURL DriveApiUrlGenerator::GetChangelistUrl(int64 start_changestamp) const {
-  const GURL& url = base_url_.Resolve(kDriveV2ChangelistUrl);
-  return start_changestamp ?
-      net::AppendOrReplaceQueryParameter(
-          url, "startChangeId", base::Int64ToString(start_changestamp)) :
-      url;
+GURL DriveApiUrlGenerator::GetChangelistUrl(
+    bool include_deleted, int64 start_changestamp) const {
+  DCHECK_GE(start_changestamp, 0);
+
+  GURL url = base_url_.Resolve(kDriveV2ChangelistUrl);
+  if (!include_deleted) {
+    // If include_deleted is set to "false", set the query parameter,
+    // because its default parameter is "true".
+    url = net::AppendOrReplaceQueryParameter(url, "includeDeleted", "false");
+  }
+
+  if (start_changestamp > 0) {
+    url = net::AppendOrReplaceQueryParameter(
+        url, "startChangeId", base::Int64ToString(start_changestamp));
+  }
+  return url;
 }
 
 GURL DriveApiUrlGenerator::GetFilelistUrl(
