782064c329d22b154899dce18f4a80194c3e0494
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 14:28:00 +0000

    filemanager: Call getSizeStats for Google Drive when a special search item is selected.
    BUG=230747, 230745
    TEST=Open Files App and verify the correct remainig space is shown for the left nav items.
    
    Review URL: https://chromiumcodereview.appspot.com/14038003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193928 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -2072,10 +2072,10 @@ DialogType.isModal = function(type) {
         !this.isOnDrive();
 
     // If volume has changed, then fetch remaining space data.
-    if (this.previousRootUrl_ != this.directoryModel_.getCurrentRootUrl())
+    if (this.previousRootUrl_ != this.directoryModel_.getCurrentMountPointUrl())
       this.refreshRemainingSpace_(true);  // Show loading caption.
 
-    this.previousRootUrl_ = this.directoryModel_.getCurrentRootUrl();
+    this.previousRootUrl_ = this.directoryModel_.getCurrentMountPointUrl();
   };
 
   /**
@@ -2083,7 +2083,7 @@ DialogType.isModal = function(type) {
    * @param {boolean} showLoadingCaption Whether show loading caption or not.
    * @private
    */
-   FileManager.prototype.refreshRemainingSpace_ = function(showLoadingCaption) {
+  FileManager.prototype.refreshRemainingSpace_ = function(showLoadingCaption) {
     var volumeSpaceInfoLabel =
         this.dialogDom_.querySelector('#volume-space-info-label');
     var volumeSpaceInnerBar =
@@ -2098,10 +2098,11 @@ DialogType.isModal = function(type) {
       volumeSpaceInnerBar.style.width = '100%';
     }
 
-    var currentRootUrl = this.directoryModel_.getCurrentRootUrl();
+    var currentMountPointUrl = this.directoryModel_.getCurrentMountPointUrl();
     chrome.fileBrowserPrivate.getSizeStats(
-        this.directoryModel_.getCurrentRootUrl(), function(result) {
-          if (this.directoryModel_.getCurrentRootUrl() != currentRootUrl)
+        currentMountPointUrl, function(result) {
+          if (this.directoryModel_.getCurrentMountPointUrl() !=
+              currentMountPointUrl)
             return;
           updateSpaceInfo(result,
                           volumeSpaceInnerBar,
