b5942d6d544e6bd1ff0734d700cd7efb1df6b696
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-12 22:25:51 +0000

    Made WebKit notifications display 80x80 icons if they have them.
    In the rich notification center and toasts, WebKit notifications
    (those created with webkitNotifications.createNotification() or with
    chrome.notifications.create({type: "simple"})) usually display their
    icons in a 40x40 square, as that was the size used for notification
    icons before rich notifications.
    
    This code change causes icons that are exacly 80x80 pixels and that have
    no transparency to be displayed using the full 80x80 pixel space
    available with rich notifications. This allows developers to create
    notifications from web pages and from apps that need to work without
    rich notifications that will look as as good as they can with rich
    notifications.
        
    BUG=222175
    R=mukai@chromium.org
    TBR=pkotwicz@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/12879009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194026 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/notification_view.cc
+++ ui/message_center/views/notification_view.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/utf_string_conversions.h"
 #include "grit/ui_resources.h"
 #include "ui/base/accessibility/accessible_view_state.h"
+#include "ui/base/layout.h"
 #include "ui/base/resource/resource_bundle.h"
 #include "ui/base/text/text_elider.h"
 #include "ui/gfx/canvas.h"
@@ -27,14 +28,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/controls/label.h"
 #include "ui/views/layout/box_layout.h"
 #include "ui/views/layout/fill_layout.h"
+#include "ui/views/widget/widget.h"
 
 namespace {
 
 // Dimensions.
-const int kIconColumnWidth = message_center::kNotificationIconSize;
+const int kIconSize = message_center::kNotificationIconSize;
 const int kLegacyIconSize = 40;
-const int kTextLeftPadding = kIconColumnWidth +
-                             message_center::kIconToTextPadding;
+const int kTextLeftPadding = kIconSize + message_center::kIconToTextPadding;
 const int kTextBottomPadding = 12;
 const int kTextRightPadding = 23;
 const int kItemTitleToMessagePadding = 3;
@@ -86,6 +87,36 @@ views::Border* MakeTextBorder(int top, int bottom) {
 // static
 views::Border* MakeSeparatorBorder(int top, int left, SkColor color) {
   return views::Border::CreateSolidSidedBorder(top, left, 0, 0, color);
+}
+
+// static
+// Return true if and only if the image is null or has alpha.
+bool HasAlpha(gfx::ImageSkia& image, views::Widget* widget) {
+  // Determine which bitmap to use.
+  ui::ScaleFactor factor = ui::SCALE_FACTOR_100P;
+  if (widget) {
+    factor = ui::GetScaleFactorForNativeView(widget->GetNativeView());
+    if (factor == ui::SCALE_FACTOR_NONE)
+      factor = ui::SCALE_FACTOR_100P;
+  }
+
+  // Extract that bitmap's alpha and look for a non-opaque pixel there.
+  SkBitmap bitmap = image.GetRepresentation(factor).sk_bitmap();
+  if (!bitmap.isNull()) {
+    SkBitmap alpha;
+    alpha.setConfig(SkBitmap::kA1_Config, bitmap.width(), bitmap.height(), 0);
+    bitmap.extractAlpha(&alpha);
+    for (int y = 0; y < bitmap.height(); ++y) {
+      for (int x = 0; x < bitmap.width(); ++x) {
+        if (alpha.getColor(x, y) != SK_ColorBLACK) {
+          return true;
+        }
+      }
+    }
+  }
+
+  // If no opaque pixel was found, return false unless the bitmap is empty.
+  return bitmap.isNull();
 }
 
 // ItemView ////////////////////////////////////////////////////////////////////
@@ -386,16 +417,20 @@ NotificationView::NotificationView(const Notification& notification,
   }
 
   // Create the notification icon view.
-  if (notification.type() == NOTIFICATION_TYPE_SIMPLE) {
+  gfx::ImageSkia icon = notification.icon().AsImageSkia();
+  if (notification.type() == NOTIFICATION_TYPE_SIMPLE &&
+      (icon.width() != kIconSize ||
+       icon.height() != kIconSize ||
+       HasAlpha(icon, GetWidget()))) {
     views::ImageView* icon_view = new views::ImageView();
-    icon_view->SetImage(notification.icon().AsImageSkia());
+    icon_view->SetImage(icon);
     icon_view->SetImageSize(gfx::Size(kLegacyIconSize, kLegacyIconSize));
     icon_view->SetHorizontalAlignment(views::ImageView::CENTER);
     icon_view->SetVerticalAlignment(views::ImageView::CENTER);
     icon_view->set_background(MakeBackground(kLegacyIconBackgroundColor));
     icon_view_ = icon_view;
   } else {
-    icon_view_ = new ProportionalImageView(notification.icon().AsImageSkia());
+    icon_view_ = new ProportionalImageView(icon);
   }
 
   // Create the bottom_view_, which collects into a vertical box all content
@@ -452,8 +487,7 @@ int NotificationView::GetHeightForWidth(int width) {
   gfx::Insets insets = GetInsets();
   int top_height = top_view_->GetHeightForWidth(width - insets.width());
   int bottom_height = bottom_view_->GetHeightForWidth(width - insets.width());
-  int icon_size = message_center::kNotificationIconSize;
-  return std::max(top_height, icon_size) + bottom_height + insets.height();
+  return std::max(top_height, kIconSize) + bottom_height + insets.height();
 }
 
 void NotificationView::Layout() {
@@ -474,11 +508,10 @@ void NotificationView::Layout() {
   top_view_->SetBounds(insets.left(), insets.top(), content_width, top_height);
 
   // Icon.
-  int icon_size = message_center::kNotificationIconSize;
-  icon_view_->SetBounds(insets.left(), insets.top(), icon_size, icon_size);
+  icon_view_->SetBounds(insets.left(), insets.top(), kIconSize, kIconSize);
 
   // Bottom views.
-  int bottom_y = insets.top() + std::max(top_height, icon_size);
+  int bottom_y = insets.top() + std::max(top_height, kIconSize);
   int bottom_height = bottom_view_->GetHeightForWidth(content_width);
   bottom_view_->SetBounds(insets.left(), bottom_y,
                           content_width, bottom_height);
