6e9b87020280ede35bc18f99972c15e108616a95
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-13 14:15:58 +0000

    views/bookmarks: Refactor the way to get the default favicon.
    Add a simple function GetDefaultFavicon() to retrieve the default favicon in a
    way which is similar to the existing GetFolderIcon() function.
    
    TEST=launch chrome, add a couple of bookmarks (e.g, about:blank), no regressions
    observed.
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/13822002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194106 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
+++ chrome/browser/ui/views/bookmarks/bookmark_bar_view.cc
@@ -451,7 +451,14 @@ const int BookmarkBarView::kMaxButtonWidth = 150;
 const int BookmarkBarView::kNewtabHorizontalPadding = 8;
 const int BookmarkBarView::kNewtabVerticalPadding = 12;
 
-// Returns the image to use for starred folders.
+static const gfx::ImageSkia& GetDefaultFavicon() {
+  if (!kDefaultFavicon) {
+    ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+    kDefaultFavicon = rb.GetImageSkiaNamed(IDR_DEFAULT_FAVICON);
+  }
+  return *kDefaultFavicon;
+}
+
 static const gfx::ImageSkia& GetFolderIcon() {
   if (!kFolderIcon) {
     ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
@@ -1259,11 +1266,6 @@ void BookmarkBarView::Init() {
   // UpdateColors(), which will set the appropriate colors for all the objects
   // added in this function.
 
-  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-
-  if (!kDefaultFavicon)
-    kDefaultFavicon = rb.GetImageSkiaNamed(IDR_DEFAULT_FAVICON);
-
   // Child views are traversed in the order they are added. Make sure the order
   // they are added matches the visual order.
   overflow_button_ = CreateOverflowButton();
@@ -1415,7 +1417,7 @@ void BookmarkBarView::ConfigureButton(const BookmarkNode* node,
     if (!favicon.IsEmpty())
       button->SetIcon(*favicon.ToImageSkia());
     else
-      button->SetIcon(*kDefaultFavicon);
+      button->SetIcon(GetDefaultFavicon());
   }
   button->set_max_width(kMaxButtonWidth);
 }
