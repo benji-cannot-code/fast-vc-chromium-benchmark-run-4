09d95817b6c81b4b51bb67a350c3c1a8791e082e
gspencer@chromium.org <gspencer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-13 01:15:33 +0000

    This adds networkingPrivate.requestNetworkScan function to trigger a scan of networks to update the list of networks.
    BUG=chromium:225587
    
    Review URL: https://chromiumcodereview.appspot.com/14103014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194060 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/networking_private_api.cc
+++ chrome/browser/chromeos/extensions/networking_private_api.cc
@@ -177,6 +177,18 @@ bool NetworkingPrivateGetVisibleNetworksFunction::RunImpl() {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
+// NetworkingPrivateRequestNetworkScanFunction
+
+NetworkingPrivateRequestNetworkScanFunction::
+~NetworkingPrivateRequestNetworkScanFunction() {
+}
+
+bool NetworkingPrivateRequestNetworkScanFunction::RunImpl() {
+  NetworkStateHandler::Get()->RequestScan();
+  return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // NetworkingPrivateStartConnectFunction
 
 NetworkingPrivateStartConnectFunction::
