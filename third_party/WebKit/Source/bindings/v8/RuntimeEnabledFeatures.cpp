ba5e96cc2ca65f823ed3d008efa67d8a8d8af6d8
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-15 18:36:23 +0000

    Remove runtime flag for do not track module, it's always on
    BUG=230363
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/14264003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148383 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/RuntimeEnabledFeatures.cpp
+++ third_party/WebKit/Source/bindings/v8/RuntimeEnabledFeatures.cpp
@@ -192,8 +192,6 @@ bool RuntimeEnabledFeatures::areSeamlessIFramesEnabled = false;
 bool RuntimeEnabledFeatures::isFontLoadEventsEnabled = false;
 #endif
 
-bool RuntimeEnabledFeatures::isDoNotTrackEnabled = true;
-
 #if USE(WEBP)
 bool RuntimeEnabledFeatures::isWebPInAcceptHeaderEnabled = false;
 #endif
--- third_party/WebKit/Source/bindings/v8/RuntimeEnabledFeatures.h
+++ third_party/WebKit/Source/bindings/v8/RuntimeEnabledFeatures.h
@@ -97,9 +97,6 @@ public:
     static bool fontLoadEventsEnabled() { return false; }
 #endif
 
-    static void setDoNotTrackEnabled(bool isEnabled) { isDoNotTrackEnabled = isEnabled; }
-    static bool doNotTrackEnabled() { return isDoNotTrackEnabled; }
-
     // Mozilla version
     static bool webkitFullScreenAPIEnabled() { return isFullScreenAPIEnabled; }
     static void setWebkitFullScreenAPIEnabled(bool isEnabled) { isFullScreenAPIEnabled = isEnabled; }
@@ -337,8 +334,6 @@ private:
 #if ENABLE(FONT_LOAD_EVENTS)
     static bool isFontLoadEventsEnabled;
 #endif
-
-    static bool isDoNotTrackEnabled;
 
 #if USE(WEBP)
     static bool isWebPInAcceptHeaderEnabled;
