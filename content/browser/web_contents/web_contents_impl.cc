1f0e741f78b8dce3c8bc5505fdb3a5cee9390e2b
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-15 16:00:43 +0000

    push MouseMove through WebContents and back to delegate to fix status bubble avoid
    This fixes the status bubble not "dodging" the mouse on Aura.
    
    On non-Aura, the path to get to StatusBubbleViews::MouseMoved is:
    - RenderWidgetHostViewWin::OnMouseEvent
    - SendMessage to the parent window
    - WebContentsViewWin::OnMouseMove
    - Browser::ContentsMouseEvent
    - StatusBubbleViews::MouseMoved
    
    On Aura, the path is:
    - HWNDMessageHandler
    - DesktkopRootWindowHostWin::HandleMouseEvent
    - RootWindow::OnHostMoustEvent
    - EventDispatcher, etc.
    - content::RenderWidgetHostViewAura::OnMouseEvent
    - RenderViewHostImpl::ForwardMouseEvent
    - WebContentsImpl::HandleMouseMove  // ***
    - Browser::ContentsMoveEvent
    - StatusBubbleViews::MouseMoved.
    
    This patch implements HandleMouseMove in WebContentsImpl to delegate back to Browser for Aura at ***.
    
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/14234020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194184 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1252,6 +1252,13 @@ bool WebContentsImpl::PreHandleWheelEvent(
   return false;
 }
 
+void WebContentsImpl::HandleMouseMove() {
+  if (delegate_) {
+    delegate_->ContentsMouseEvent(
+        this, gfx::Screen::GetNativeScreen()->GetCursorScreenPoint(), true);
+  }
+}
+
 void WebContentsImpl::HandleMouseDown() {
   if (delegate_)
     delegate_->HandleMouseDown();
