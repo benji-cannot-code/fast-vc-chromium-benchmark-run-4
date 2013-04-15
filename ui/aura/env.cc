3322f527f3f5970aa40ef6b55424bc212a54337a
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-15 14:03:39 +0000

    Reland 193431 "CrOS: Enable threaded UI compositor by default."
    Reverted here:  https://codereview.chromium.org/14157002
    Original CL here:  https://chromiumcodereview.appspot.com/12088092
    
    BUG=173417
    
    Review URL: https://codereview.chromium.org/13975006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194175 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/env.cc
+++ ui/aura/env.cc
@@ -92,9 +92,7 @@ void Env::Init() {
   base::MessagePumpAuraX11::Current()->AddObserver(
       &device_list_updater_aurax11_);
 #endif
-  ui::Compositor::Initialize(
-      CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kUIEnableThreadedCompositing));
+  ui::Compositor::Initialize();
 }
 
 void Env::NotifyWindowInitialized(Window* window) {
