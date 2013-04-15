f830fd886ccfe672ce0b8390386374f5a13f258a
jparent@chromium.org <jparent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-15 22:01:44 +0000

    Also removes unit tests for webkit.org, and now unused code. Also fixes loader_unittests that were broken with temporary changes to get dashboards loading in blink world.
    BUG=227245
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/14026004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/loader_unittests.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/loader_unittests.js
@@ -45,10 +45,8 @@ test('loading steps', 1, function() {
         resourceLoader.load();
     }
 
-    try {
-        resourceLoader._loadingSteps = [loadingStep1, loadingStep2];
-        resourceLoader.load();
-    } 
+    resourceLoader._loadingSteps = [loadingStep1, loadingStep2];
+    resourceLoader.load();
 });
 
 // Total number of assertions is 1 for the deepEqual of the builder lists
@@ -85,7 +83,8 @@ test('results files loading', 11, function() {
 test('expectations files loading', 1, function() {
     resetGlobals();
     g_history.parseCrossDashboardParameters();
-    var expectedLoadedPlatforms = ["chromium", "chromium-android", "efl", "efl-wk1", "efl-wk2", "gtk",
+    // FIXME: re-enable once added back in flakiness_dashboard.js
+    var expectedLoadedPlatforms = [/* "chromium", "chromium-android", */"efl", "efl-wk1", "efl-wk2", "gtk",
                                    "gtk-wk2", "mac", "mac-lion", "mac-snowleopard", "qt", "win", "wk2"];
     var loadedPlatforms = [];
     var resourceLoader = new loader.Loader();
