0470782960b26bfcc5d659aeb4b960128689e85e
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-15 21:42:19 +0000

    garden-o-matic doesn't handle non-ascii XML file
    ReflectionHandler._serve_xml would throw an exception if the string
    it was given (an XML document) contained a non-ascii character.
    
    BUG=231412
    Test=test_svn_log_non_ascii
    
    Review URL: https://codereview.chromium.org/13950004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/reflectionhandler.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/servers/reflectionhandler.py
@@ -149,4 +149,5 @@ class ReflectionHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self._send_access_control_header()
         self.send_header("Content-type", "text/xml")
         self.end_headers()
+        xml = xml.encode('utf-8')
         self.wfile.write(xml)
