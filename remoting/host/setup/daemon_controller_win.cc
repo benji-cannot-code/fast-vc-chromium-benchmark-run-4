81d481a793dc136fa9aa5705bc549114e2925e3d
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-15 21:09:27 +0000

    Remove unnecessary #include.
    BUG=none
    TEST=none
    Review URL: https://codereview.chromium.org/13998021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194234 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/setup/daemon_controller_win.cc
+++ remoting/host/setup/daemon_controller_win.cc
@@ -5,8 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "remoting/host/setup/daemon_controller.h"
 
-#include <objbase.h>
-
 #include "base/basictypes.h"
 #include "base/bind.h"
 #include "base/bind_helpers.h"
