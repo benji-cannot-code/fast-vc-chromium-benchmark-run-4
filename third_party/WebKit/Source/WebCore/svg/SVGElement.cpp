5c31cb6dd97e2e956ea517d4bf9af6a3cfd2188e
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-15 19:02:12 +0000

    CSP blocks style application in cloned nodes
    Checks if the Element is being cloned, if so, the application of the style is allowed otherwise it relies on the CSP policy.
    
    Just re-landing the patch submitted to Webkit:
    https://bugs.webkit.org/show_bug.cgi?id=112270
    
    Patch written by Felipe Zimmerle <felipe@zimmerle.org>
    
    R=mkwst@chromium.org,abarth@chromium.org
    BUG=189184
    
    Review URL: https://codereview.chromium.org/14234005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/svg/SVGElement.cpp
+++ third_party/WebKit/Source/WebCore/svg/SVGElement.cpp
@@ -532,7 +532,7 @@ bool SVGElement::childShouldCreateRenderer(const NodeRenderingContext& childCont
     return false;
 }
 
-void SVGElement::attributeChanged(const QualifiedName& name, const AtomicString& newValue)
+void SVGElement::attributeChanged(const QualifiedName& name, const AtomicString& newValue, AttributeModificationReason)
 {
     StyledElement::attributeChanged(name, newValue);
 
