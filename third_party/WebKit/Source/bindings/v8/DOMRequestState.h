95f44f78ebce9d862cccdf042a1e454c4802a72b
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-16 23:40:49 +0000

    Use DOMWrapperWorld instead of WorldContextHandle in DOMRequestState
    R=abarth, jsbell
    BUG=230460
    
    Review URL: https://codereview.chromium.org/14301011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148508 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/DOMRequestState.h
+++ third_party/WebKit/Source/bindings/v8/DOMRequestState.h
@@ -27,25 +27,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef DOMRequestState_h
 #define DOMRequestState_h
 
+#include "DOMWrapperWorld.h"
+#include "ScriptExecutionContext.h"
 #include "V8Binding.h"
-#include "WorldContextHandle.h"
 #include "v8.h"
+#include <wtf/RefPtr.h>
 
 namespace WebCore {
-
-class ScriptExecutionContext;
 
 class DOMRequestState {
 public:
     explicit DOMRequestState(ScriptExecutionContext* scriptExecutionContext)
-        : m_worldContextHandle(UseCurrentWorld)
-        , m_scriptExecutionContext(scriptExecutionContext)
+        : m_scriptExecutionContext(scriptExecutionContext)
+        , m_world(DOMWrapperWorld::current(scriptExecutionContext))
     {
     }
 
     void clear()
     {
         m_scriptExecutionContext = 0;
+        m_world.clear();
     }
 
     class Scope {
@@ -61,12 +62,12 @@ public:
 
     v8::Local<v8::Context> context()
     {
-        return toV8Context(m_scriptExecutionContext, m_worldContextHandle);
+        return toV8Context(m_scriptExecutionContext, m_world.get());
     }
 
 private:
-    WorldContextHandle m_worldContextHandle;
     ScriptExecutionContext* m_scriptExecutionContext;
+    RefPtr<DOMWrapperWorld> m_world;
 };
 
 }
