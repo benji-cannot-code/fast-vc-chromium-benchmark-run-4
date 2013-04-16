98c0cea36a188675b3df5b715cf1365b321f82e9
nileshagrawal@chromium.org <nileshagrawal@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 23:38:25 +0000

    Android: Drop version extension for UMA.
    BUG=174002
    This extension is not needed anymore.
    
    Review URL: https://codereview.chromium.org/14301007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194486 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -142,10 +142,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/layout.h"
 #include "ui/base/resource/resource_bundle.h"
 
-#if defined(OS_ANDROID)
-#include "base/android/build_info.h"
-#endif
-
 #if !defined(OS_ANDROID)
 #include "chrome/browser/ui/active_tab_tracker.h"
 #endif
@@ -587,13 +583,6 @@ void ChromeBrowserMainParts::SetupMetricsAndFieldTrials() {
     MetricsLog::set_version_extension("-F");
 #elif defined(ARCH_CPU_64_BITS)
   MetricsLog::set_version_extension("-64");
-#elif defined(OS_ANDROID)
-  // Set version extension to identify Android releases.
-  // Example: 16.0.912.61-K88
-  std::string version_extension = "-K";
-  version_extension.append(
-      base::android::BuildInfo::GetInstance()->package_version_code());
-  MetricsLog::set_version_extension(version_extension);
 #endif  // defined(OS_WIN)
 
   // Initialize FieldTrialList to support FieldTrials that use one-time
