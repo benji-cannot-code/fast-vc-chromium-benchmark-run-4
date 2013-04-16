1e440e48a31f9381993a62ed1e0456496af4b5bd
bsalomon@google.com <bsalomon@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 13:07:07 +0000

    Initialize glCopyTexSubImage2D in Skia's GL interface
    Review URL: https://codereview.chromium.org/14234021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194353 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/gpu/gl_bindings_skia_cmd_buffer.cc
+++ webkit/gpu/gl_bindings_skia_cmd_buffer.cc
@@ -35,6 +35,7 @@ GrGLInterface* CreateCommandBufferSkiaGLBinding() {
   interface->fColorMask = glColorMask;
   interface->fCompileShader = glCompileShader;
   interface->fCompressedTexImage2D = glCompressedTexImage2D;
+  interface->fCopyTexSubImage2D = glCopyTexSubImage2D;
   interface->fCreateProgram = glCreateProgram;
   interface->fCreateShader = glCreateShader;
   interface->fCullFace = glCullFace;
