807c8a7be5b615b6d7df6eee11873f32dd7aa915
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-16 08:22:08 +0000

    DevTools: [Network] Orphaned live links are not disposed timely.
    Live links are managed by "Linkifier".
    Unfortunately, it can only dispose all links at once.
    
    Popover lifetime is much shorter than network log lifetime.
    So every time popover hides, some amount live links become orphaned.
    
    Also we are aimed to make network log table to be "viewport".
    So live links for different rows should be managed distinctly.
    
    Linkifier is a very lightweight object, so it is fair enough to
    construct as many instances as we need.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/14162003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148441 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
@@ -102,7 +102,7 @@ WebInspector.NetworkLogView.prototype = {
             this._setLargerRequests(this.useLargeRows);
 
         this._allowPopover = true;
-        this._popoverHelper = new WebInspector.PopoverHelper(this.element, this._getPopoverAnchor.bind(this), this._showPopover.bind(this));
+        this._popoverHelper = new WebInspector.PopoverHelper(this.element, this._getPopoverAnchor.bind(this), this._showPopover.bind(this), this._onHidePopover.bind(this));
         // Enable faster hint.
         this._popoverHelper.setTimeout(100);
 
@@ -801,7 +801,6 @@ WebInspector.NetworkLogView.prototype = {
 
         this._mainRequestLoadTime = -1;
         this._mainRequestDOMContentTime = -1;
-        this._linkifier.reset();
     },
 
     get requests()
@@ -961,6 +960,11 @@ WebInspector.NetworkLogView.prototype = {
         else
             content = WebInspector.RequestTimingView.createTimingTable(anchor.parentElement.request);
         popover.show(content, anchor);
+    },
+
+    _onHidePopover: function()
+    {
+        this._linkifier.reset();
     },
 
     /**
@@ -2015,6 +2019,7 @@ WebInspector.NetworkDataGridNode = function(parentView, request)
     WebInspector.DataGridNode.call(this, {});
     this._parentView = parentView;
     this._request = request;
+    this._linkifier = new WebInspector.Linkifier();
 }
 
 WebInspector.NetworkDataGridNode.prototype = {
@@ -2035,6 +2040,11 @@ WebInspector.NetworkDataGridNode.prototype = {
         this._createTimelineCell();
         this._nameCell.addEventListener("click", this._onClick.bind(this), false);
         this._nameCell.addEventListener("dblclick", this._openInNewTab.bind(this), false);
+    },
+
+    wasDetached: function()
+    {
+        this._linkifier.reset();
     },
 
     isFilteredOut: function()
@@ -2267,7 +2277,7 @@ WebInspector.NetworkDataGridNode.prototype = {
             break;
 
         case WebInspector.NetworkRequest.InitiatorType.Script:
-            var urlElement = this._parentView._linkifier.linkifyLocation(initiator.url, initiator.lineNumber - 1, initiator.columnNumber - 1);
+            var urlElement = this._linkifier.linkifyLocation(initiator.url, initiator.lineNumber - 1, initiator.columnNumber - 1);
             urlElement.title = "";
             this._initiatorCell.appendChild(urlElement);
             this._appendSubtitle(this._initiatorCell, WebInspector.UIString("Script"));
