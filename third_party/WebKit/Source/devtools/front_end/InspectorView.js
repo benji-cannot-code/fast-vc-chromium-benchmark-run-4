af4461517923980908088e32419c9e522850c47f
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-16 08:21:33 +0000

    DevTools: [Search] Wrong panel is affected by "cancelSearch" when panels are switched.
    BUG=229520
    
    Review URL: https://codereview.chromium.org/14028006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/InspectorView.js
+++ third_party/WebKit/Source/devtools/front_end/InspectorView.js
@@ -108,6 +108,9 @@ WebInspector.InspectorView.prototype = {
         if (this._currentPanel === x)
             return;
 
+        // FIXME: remove search controller.
+        WebInspector.searchController.cancelSearch();
+
         if (this._currentPanel)
             this._currentPanel.detach();
 
@@ -116,8 +119,6 @@ WebInspector.InspectorView.prototype = {
         if (x) {
             x.show();
             this.dispatchEventToListeners(WebInspector.InspectorView.Events.PanelSelected);
-            // FIXME: remove search controller.
-            WebInspector.searchController.cancelSearch();
         }
         for (var panelName in WebInspector.panels) {
             if (WebInspector.panels[panelName] === x) {
