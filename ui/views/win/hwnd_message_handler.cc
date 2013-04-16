d3d428b9c19ae311e0e6255c75a0a22bf80b0db0
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 01:18:04 +0000

    Ignore HTCLIENT in HWNDMessageHandler::OnSetCursor.
    http://crrev.com/188817 incorrectly overrides client-area cursors.
    Ignore HTCLIENT in OnSetCursor; defer to client-area mouse message handling.
    (RootView::OnMouseMoved calls widget_->SetCursor(mouse_move_handler_->GetCursor(moved_event)))
    
    BUG=226692
    TEST=Omnibox and textfield I-beam cursors do not flicker the pointer; no cursor regressions.
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/14103017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194270 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/win/hwnd_message_handler.cc
+++ ui/views/win/hwnd_message_handler.cc
@@ -1872,6 +1872,9 @@ LRESULT HWNDMessageHandler::OnSetCursor(UINT message,
     case HTBOTTOMLEFT:
       cursor = IDC_SIZENESW;
       break;
+    case HTCLIENT:
+      // Client-area mouse events set the proper cursor from View::GetCursor.
+      return 0;
     default:
       // Use the default value, IDC_ARROW.
       break;
