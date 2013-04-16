5917bca6a04afb6ffd7955ef744a7116562d9452
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 13:28:20 +0000

    Fix two includes of .cc files that should be includes of the corresponding headers.
    BUG=None
    
    Review URL: https://codereview.chromium.org/13929014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194358 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/streams_private/streams_private_apitest.cc
+++ chrome/browser/extensions/api/streams_private/streams_private_apitest.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/common/extensions/mime_types_handler.h"
-#include "chrome/common/pref_names.cc"
+#include "chrome/common/pref_names.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "content/public/browser/download_item.h"
 #include "content/public/browser/download_manager.h"
--- chrome/test/perf/perftests.cc
+++ chrome/test/perf/perftests.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop.h"
 #include "base/test/perf_test_suite.h"
-#include "chrome/common/chrome_paths.cc"
+#include "chrome/common/chrome_paths.h"
 
 int main(int argc, char **argv) {
   base::PerfTestSuite suite(argc, argv);
