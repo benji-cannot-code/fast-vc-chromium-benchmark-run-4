367456d3a1917e6f34d75aa579be99f94da32bc0
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 23:19:15 +0000

    ObsoleteOSInfoBar -> ObsoleteOSInfoBarDelegate, for consistency with other infobar delegates.
    BUG=none
    TEST=none
    Review URL: https://codereview.chromium.org/11773036
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194479 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/startup/startup_browser_creator_impl.cc
+++ chrome/browser/ui/startup/startup_browser_creator_impl.cc
@@ -62,7 +62,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/startup/autolaunch_prompt.h"
 #include "chrome/browser/ui/startup/bad_flags_prompt.h"
 #include "chrome/browser/ui/startup/default_browser_prompt.h"
-#include "chrome/browser/ui/startup/obsolete_os_info_bar.h"
+#include "chrome/browser/ui/startup/obsolete_os_infobar_delegate.h"
 #include "chrome/browser/ui/startup/session_crashed_prompt.h"
 #include "chrome/browser/ui/startup/startup_browser_creator.h"
 #include "chrome/browser/ui/tabs/pinned_tab_codec.h"
@@ -906,7 +906,7 @@ void StartupBrowserCreatorImpl::AddInfoBarsIfNecessary(
     // TODO(phajdan.jr): Always enable after migrating bots:
     // http://crbug.com/170262 .
     if (!command_line_.HasSwitch(switches::kTestType)) {
-      chrome::ObsoleteOSInfoBar::Create(
+      chrome::ObsoleteOSInfoBarDelegate::Create(
           InfoBarService::FromWebContents(
               browser->tab_strip_model()->GetActiveWebContents()));
     }
