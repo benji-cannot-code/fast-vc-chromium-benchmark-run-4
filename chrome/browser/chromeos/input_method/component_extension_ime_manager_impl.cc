06f63c7d12f6bbbd6f65568a080694cf47093e88
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 05:48:19 +0000

    Add input tools as the component extension IME.
    Input Tools provides various input methods powered by Google Input Tools.
    To enable it, https://gerrit-int.chromium.org/#/c/35603/ is required but it is safe to commit this partially.
    
    BUG=231107
    TEST=Manually checked Amharic input method is shown on lumpy.
    
    Review URL: https://codereview.chromium.org/14245004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194302 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/component_extension_ime_manager_impl.cc
+++ chrome/browser/chromeos/input_method/component_extension_ime_manager_impl.cc
@@ -31,6 +31,11 @@ struct WhitelistedComponentExtensionIME {
     "fpfbhcjppmaeaijcidgiibchfbnhbelj",
     "/usr/share/chromeos-assets/input_methods/nacl_mozc",
   },
+  {
+    // Google input tools.
+    "gjaehgfemfahhmlgpdfknkhdnemmolop",
+    "/usr/share/chromeos-assets/input_methods/input_tools",
+  },
 #else
   {
     // Open-sourced Mozc Japanese Input.
