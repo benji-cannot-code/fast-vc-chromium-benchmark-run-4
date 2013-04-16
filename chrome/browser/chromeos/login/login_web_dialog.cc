deb44213c70e95ac06912012d90db7a7bf4bee3d
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 12:12:49 +0000

    Part of multiprofile implementation.
    Created method for obtaining signin profile.
    Signin profile used instead of "DefaultProfile" during login phase.
    
    R=nkostylev@chromium.org
    TBR=brettw
    
    Review URL: https://codereview.chromium.org/13633003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_web_dialog.cc
+++ chrome/browser/chromeos/login/login_web_dialog.cc
@@ -7,7 +7,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/utf_string_conversions.h"
 #include "chrome/browser/chromeos/login/helper.h"
-#include "chrome/browser/profiles/profile_manager.h"
+#include "chrome/browser/chromeos/profiles/profile_helper.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser_dialogs.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/notification_types.h"
@@ -58,7 +59,7 @@ LoginWebDialog::~LoginWebDialog() {
 
 void LoginWebDialog::Show() {
   chrome::ShowWebDialog(parent_window_,
-                        ProfileManager::GetDefaultProfile(),
+                        ProfileHelper::GetSigninProfile(),
                         this);
   is_open_ = true;
 }
---
+++ chrome/browser/chromeos/profiles/profile_helper.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_CHROMEOS_PROFILES_PROFILE_HELPER_H_
+#define CHROME_BROWSER_CHROMEOS_PROFILES_PROFILE_HELPER_H_
+
+#include "base/basictypes.h"
+
+class Profile;
+
+namespace chromeos {
+
+class ProfileHelper {
+ public:
+  // Returns OffTheRecord profile for use during signing phase.
+  static Profile* GetSigninProfile();
+
+  // Initialize a bunch of services that are tied to a browser profile.
+  // TODO(dzhioev): Investigate whether or not this method is needed.
+  static void ProfileStartup(Profile* profile, bool process_startup);
+
+ private:
+  DISALLOW_IMPLICIT_CONSTRUCTORS(ProfileHelper);
+};
+
+} // namespace chromeos
+
+#endif  // CHROME_BROWSER_CHROMEOS_PROFILES_PROFILE_HELPER_H_
+
