2ab3fdf5e16231192d1c79300b963a131ef60ce1
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 22:29:31 +0000

    Revert 194465 "media: Add support for playback for VP8 Alpha vid..."
    Broke linux compilation http://build.chromium.org/p/chromium.linux/buildstatus?builder=Linux%20Builder%20%28dbg%29&number=47451
    > media: Add support for playback for VP8 Alpha video streams.
    > 
    > BUG=147355
    > TEST=VP8 Alpha video streams play
    > 
    > Review URL: https://codereview.chromium.org/12263013
    > 
    > Patch from Vignesh Venkatasubramanian <vigneshv@chromium.org>.
    
    TBR=tomfinegan@chromium.org
    
    Review URL: https://codereview.chromium.org/13972014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194469 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/simd/convert_yuv_to_rgb.h
+++ media/base/simd/convert_yuv_to_rgb.h
@@ -22,19 +22,6 @@ typedef void (*ConvertYUVToRGB32Proc)(const uint8*,
                                       int,
                                       YUVType);
 
-typedef void (*ConvertYUVAToARGBProc)(const uint8*,
-                                      const uint8*,
-                                      const uint8*,
-                                      const uint8*,
-                                      uint8*,
-                                      int,
-                                      int,
-                                      int,
-                                      int,
-                                      int,
-                                      int,
-                                      YUVType);
-
 void ConvertYUVToRGB32_C(const uint8* yplane,
                          const uint8* uplane,
                          const uint8* vplane,
@@ -43,19 +30,6 @@ void ConvertYUVToRGB32_C(const uint8* yplane,
                          int height,
                          int ystride,
                          int uvstride,
-                         int rgbstride,
-                         YUVType yuv_type);
-
-void ConvertYUVAToARGB_C(const uint8* yplane,
-                         const uint8* uplane,
-                         const uint8* vplane,
-                         const uint8* aplane,
-                         uint8* rgbframe,
-                         int width,
-                         int height,
-                         int ystride,
-                         int uvstride,
-                         int avstride,
                          int rgbstride,
                          YUVType yuv_type);
 
@@ -81,19 +55,6 @@ void ConvertYUVToRGB32_MMX(const uint8* yplane,
                            int rgbstride,
                            YUVType yuv_type);
 
-void ConvertYUVAToARGB_MMX(const uint8* yplane,
-                           const uint8* uplane,
-                           const uint8* vplane,
-                           const uint8* aplane,
-                           uint8* rgbframe,
-                           int width,
-                           int height,
-                           int ystride,
-                           int uvstride,
-                           int avstride,
-                           int rgbstride,
-                           YUVType yuv_type);
-
 }  // namespace media
 
 // Assembly functions are declared without namespace.
@@ -112,13 +73,6 @@ typedef void (*ConvertYUVToRGB32RowProc)(const uint8*,
                                           uint8*,
                                           ptrdiff_t);
 
-typedef void (*ConvertYUVAToARGBRowProc)(const uint8*,
-                                         const uint8*,
-                                         const uint8*,
-                                         const uint8*,
-                                         uint8*,
-                                         ptrdiff_t);
-
 typedef void (*ScaleYUVToRGB32RowProc)(const uint8*,
                                        const uint8*,
                                        const uint8*,
@@ -132,23 +86,9 @@ void ConvertYUVToRGB32Row_C(const uint8* yplane,
                             uint8* rgbframe,
                             ptrdiff_t width);
 
-void ConvertYUVAToARGBRow_C(const uint8* yplane,
-                            const uint8* uplane,
-                            const uint8* vplane,
-                            const uint8* aplane,
-                            uint8* rgbframe,
-                            ptrdiff_t width);
-
 void ConvertYUVToRGB32Row_MMX(const uint8* yplane,
                               const uint8* uplane,
                               const uint8* vplane,
-                              uint8* rgbframe,
-                              ptrdiff_t width);
-
-void ConvertYUVAToARGBRow_MMX(const uint8* yplane,
-                              const uint8* uplane,
-                              const uint8* vplane,
-                              const uint8* aplane,
                               uint8* rgbframe,
                               ptrdiff_t width);
 
