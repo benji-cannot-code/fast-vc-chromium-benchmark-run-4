ad1dc7ebf856c045685e2b8ba2ca49d968ca25a2
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-16 05:46:27 +0000

    Fix unlimited and installed app handling in QuotaManager
    Unlimited apps must be installed apps or extensions by default
    (unless we're given a special command-line), while currently
    we check both 'is_installed' and 'is_unlimited' in the QuotaManager,
    which is highly confusing.
    
    This patch fixes code, tests, comments and mock code to get it right.
    
    This also adds GetUsageAndQuotaForWebApps tests for installed/unlimited and for syncable storage case.
    
    BUG=230330
    TEST=content_unittests:QuotaManagerTest.*
    
    Review URL: https://codereview.chromium.org/14044005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194301 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/mock_special_storage_policy.h
+++ webkit/quota/mock_special_storage_policy.h
@@ -38,8 +38,6 @@ class MockSpecialStoragePolicy : public quota::SpecialStoragePolicy {
   }
 
   void AddInstalledApp(const GURL& origin) {
-    // Installed implies unlimited.
-    unlimited_.insert(origin);
     installed_.insert(origin);
   }
 
@@ -48,7 +46,7 @@ class MockSpecialStoragePolicy : public quota::SpecialStoragePolicy {
   }
 
   void SetAllUnlimited(bool all_unlimited) {
-      all_unlimited_ = all_unlimited;
+    all_unlimited_ = all_unlimited;
   }
 
   void Reset() {
