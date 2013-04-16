69d7d87aeed35b3240393dbfe6e776a4e2e0ded9
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-16 07:13:54 +0000

    Content Security Policy tests should use the unprefixed header.
    We used to require the prefixed header for experimental bits and pieces of
    the 1.1 spec. We've since changed direction, preferring the canonical
    header, and hiding the unstable functionality behind a runtime flag. There's
    no longer any reason at all to use 'X-WebKit-CSP'. In order to more clearly
    deprecate it, this patch changes all the tests to use the canonical header.
    
    Review URL: https://codereview.chromium.org/14046027
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148433 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/nph-cached-csp.pl
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/nph-cached-csp.pl
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #!/usr/bin/perl
-# Script to generate a 304 HTTP status with (illegal) X-WebKit-CSP headers.
+# Script to generate a 304 HTTP status with (illegal) Content-Security-Policy headers.
 # Relies on its nph- filename to invoke the CGI non-parsed-header facility.
 
 $protocol = $ENV{'SERVER_PROTOCOL'};
