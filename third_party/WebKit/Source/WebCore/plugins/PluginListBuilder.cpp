9c35d28bdab0dd333718fb5b1e69f2ca003d854a
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-16 07:33:46 +0000

    Fold WebCore/plugins/chromium into WebCore/plugins.
    This patch folds PluginDataChromium into PluginData, and moves
    PluginListBuilder into WebCore/plugins directly. The 'chromium'
    directory can then be safely removed.
    
    Review URL: https://codereview.chromium.org/13957004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148435 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebCore/plugins/PluginListBuilder.cpp
+++ third_party/WebKit/Source/WebCore/plugins/PluginListBuilder.cpp
@@ -1,11 +1,11 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2009 Google Inc. All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- * 
+ *
  *     * Redistributions of source code must retain the above copyright
  * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  *     * Neither the name of Google Inc. nor the names of its
  * contributors may be used to endorse or promote products derived from
  * this software without specific prior written permission.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -29,28 +29,36 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef PluginListBuilder_h
-#define PluginListBuilder_h
+#include "config.h"
+#include "PluginListBuilder.h"
 
 #include "PluginData.h"
-#include <public/WebPluginListBuilder.h>
+#include <public/WebString.h>
 #include <wtf/Vector.h>
 
 namespace WebCore {
 
-class PluginListBuilder : public WebKit::WebPluginListBuilder {
-public:
-    PluginListBuilder(Vector<WebCore::PluginInfo>* results) : m_results(results) { }
+void PluginListBuilder::addPlugin(const WebKit::WebString& name, const WebKit::WebString& description, const WebKit::WebString& fileName)
+{
+    PluginInfo info;
+    info.name = name;
+    info.desc = description;
+    info.file = fileName;
+    m_results->append(info);
+}
 
-    // WebPluginListBuilder methods:
-    virtual void addPlugin(const WebKit::WebString& name, const WebKit::WebString& description, const WebKit::WebString& fileName);
-    virtual void addMediaTypeToLastPlugin(const WebKit::WebString& name, const WebKit::WebString& description);
-    virtual void addFileExtensionToLastMediaType(const WebKit::WebString& extension);
+void PluginListBuilder::addMediaTypeToLastPlugin(const WebKit::WebString& name, const WebKit::WebString& description)
+{
+    MimeClassInfo info;
+    info.type = name;
+    info.desc = description;
+    m_results->last().mimes.append(info);
+}
 
-private:
-    Vector<PluginInfo>* m_results;
-};
+void PluginListBuilder::addFileExtensionToLastMediaType(const WebKit::WebString& extension)
+{
+    MimeClassInfo& info = m_results->last().mimes.last();
+    info.extensions.append(extension);
+}
 
 } // namespace WebCore
-
-#endif
--- third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/FrameLoaderClientImpl.cpp
@@ -53,7 +53,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MouseEvent.h"
 #include "Page.h"
 #include "PluginData.h"
-#include "PluginDataChromium.h"
 #include "ProgressTracker.h"
 #include "ResourceHandleInternal.h"
 #include "ResourceLoader.h"
