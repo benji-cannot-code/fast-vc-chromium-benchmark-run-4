5d7d3a957827c6fa148bb92e2c53290236f1b66f
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 21:51:46 +0000

    Sends Updated when message center is closed.
    Because open/close will change the notification status (read/shown),
    it should send Updated to the observers.
    
    BUG=232030
    TEST=manually (see repro steps in the bug)
    R=dewittj@chromium.org
    
    Review URL: https://codereview.chromium.org/14299007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194698 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/notification_list_unittest.cc
+++ ui/message_center/notification_list_unittest.cc
@@ -137,9 +137,9 @@ TEST_F(NotificationListTest, MessageCenterVisible) {
 
   // Toggle the message center visibility. It resets the unread count when
   // hidden.
-  notification_list()->SetMessageCenterVisible(true);
+  notification_list()->SetMessageCenterVisible(true, NULL);
   ASSERT_EQ(1u, notification_list()->unread_count());
-  notification_list()->SetMessageCenterVisible(false);
+  notification_list()->SetMessageCenterVisible(false, NULL);
   ASSERT_EQ(0u, notification_list()->unread_count());
 }
 
@@ -227,8 +227,8 @@ TEST_F(NotificationListTest, Priority) {
   EXPECT_EQ(NotificationList::kMaxVisiblePopupNotifications, GetPopupCounts());
 
   // Low priority: not visible to popups.
-  notification_list()->SetMessageCenterVisible(true);
-  notification_list()->SetMessageCenterVisible(false);
+  notification_list()->SetMessageCenterVisible(true, NULL);
+  notification_list()->SetMessageCenterVisible(false, NULL);
   EXPECT_EQ(0u, notification_list()->unread_count());
   AddPriorityNotification(-1);
   EXPECT_EQ(NotificationList::kMaxVisiblePopupNotifications + 2,
