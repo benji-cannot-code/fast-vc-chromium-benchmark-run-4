99bac5a6891bae6eff8a1bf0d48712db96a0a6f6
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 16:59:34 +0000

    Make panels turn on or off shadow effect.
    This is needed for stacking panels. The foreground panel windows will turn off the shadow while the background stack window will provider the shadow.
    
    BUG=223155,177770
    TEST=none due to no functionality change yet
    
    Review URL: https://codereview.chromium.org/13853010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194615 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/panels/panel_cocoa.h
+++ chrome/browser/ui/cocoa/panels/panel_cocoa.h
@@ -65,6 +65,7 @@ class PanelCocoa : public NativePanel {
 
   virtual void MinimizePanelBySystem() OVERRIDE;
   virtual bool IsPanelMinimizedBySystem() const OVERRIDE;
+  virtual void ShowShadow(bool show) OVERRIDE;
   virtual NativePanelTesting* CreateNativePanelTesting() OVERRIDE;
 
   Panel* panel() const;
