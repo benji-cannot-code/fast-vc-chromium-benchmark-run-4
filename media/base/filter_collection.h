808ababf066c80fe3b5938eda8382e8613584725
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 21:09:01 +0000

    Remove media::FilterCollection::Clear() because no one calls it.
    Review URL: https://codereview.chromium.org/14195007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194675 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/filter_collection.h
+++ media/base/filter_collection.h
@@ -40,9 +40,6 @@ class MEDIA_EXPORT FilterCollection {
   void SetVideoRenderer(scoped_ptr<VideoRenderer> video_renderer);
   scoped_ptr<VideoRenderer> GetVideoRenderer();
 
-  // Remove remaining filters.
-  void Clear();
-
   VideoDecoderList* GetVideoDecoders();
 
  private:
