841a63e835281a658f4238f62f9034416e641318
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 12:46:29 +0000

    Use classList#toggle instead of classList[whatever ? 'add' : 'remove'].
    BUG=232048
    
    Review URL: https://codereview.chromium.org/14296002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194583 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/screen_error_message.js
+++ chrome/browser/resources/chromeos/login/screen_error_message.js
@@ -197,7 +197,7 @@ cr.define('login', function() {
      * @private
      */
     allowGuestSignin_: function(allowed) {
-      this.classList[allowed ? 'add' : 'remove']('allow-guest-signin');
+      this.classList.toggle('allow-guest-signin', allowed);
       this.onContentChange_();
     },
 
@@ -206,7 +206,7 @@ cr.define('login', function() {
      * @private
      */
     allowOfflineLogin_: function(allowed) {
-      this.classList[allowed ? 'add' : 'remove']('allow-offline-login');
+      this.classList.toggle('allow-offline-login', allowed);
       this.onContentChange_();
     },
   };
--- chrome/browser/resources/chromeos/login/screen_locally_managed_user_creation.js
+++ chrome/browser/resources/chromeos/login/screen_locally_managed_user_creation.js
@@ -467,9 +467,9 @@ cr.define('login', function() {
      */
     set nameErrorVisible(value) {
       $('managed-user-creation-flow-name-error').
-          classList[value ? 'add' : 'remove']('error');
+          classList.toggle('error', value);
       $('managed-user-creation-flow-name').
-          classList[value ? 'add' : 'remove']('duplicate-name');
+          classList.toggle('duplicate-name', value);
       if (!value)
         $('managed-user-creation-flow-name-error').textContent = '';
     },
@@ -480,7 +480,7 @@ cr.define('login', function() {
      */
     set passwordErrorVisible(value) {
       $('managed-user-creation-flow-password-error').
-          classList[value ? 'add' : 'remove']('error');
+          classList.toggle('error', value);
       if (!value)
         $('managed-user-creation-flow-password-error').textContent = '';
     },
