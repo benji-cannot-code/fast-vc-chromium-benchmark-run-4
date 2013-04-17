50bb8c9d49aeba603de8e0fac3bc2523d851613d
bryeung@chromium.org <bryeung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 17:58:23 +0000

    Add a virtual keyboard webui at chrome://keyboard/
    This is just the old virtual keyboard that was removed a couple of years
    ago, being used as a temporary stand-in.
    
    BUG=222801
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=194579
    
    Review URL: https://codereview.chromium.org/13652010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194635 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/component_loader.h
+++ chrome/browser/extensions/component_loader.h
@@ -128,6 +128,7 @@ class ComponentLoader {
   void AddOrReloadEnterpriseWebStore();
 
   void AddChromeApp();
+  void AddKeyboardApp();
 
   // Unloads |component| from the memory.
   void UnloadComponent(ComponentExtensionInfo* component);
