313cf7300d38ce04efd1a656351365e030ba97f6
darin@chromium.org <darin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-17 19:25:05 +0000

    Revert "Combine ResourceResponse.{h,cpp} and ResourceResponseBase.{h,cpp}."
    This reverts commit 416f123185708ee17ffc2d38ba9b46ac2a29654b.
    
    TBR=dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/14329007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148569 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -85,7 +85,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "MIMETypeRegistry.h"
 #include "NodeRenderStyle.h"
 #include "NonCompositedContentHost.h"
-#include "NotImplemented.h"
 #include "Page.h"
 #include "PageGroup.h"
 #include "PageGroupLoadDeferrer.h"
