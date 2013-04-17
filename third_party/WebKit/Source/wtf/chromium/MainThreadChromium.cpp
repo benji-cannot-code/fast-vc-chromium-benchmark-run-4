8fc7616c89d3d6e5610513b1c27746c298ae1380
tfarina@chromium.org <tfarina@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-17 23:37:34 +0000

    Fix include path to ChromiumThreading.h
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/14329013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148591 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/chromium/MainThreadChromium.cpp
+++ third_party/WebKit/Source/wtf/chromium/MainThreadChromium.cpp
@@ -31,11 +31,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-#include "ChromiumThreading.h"
 #include "wtf/Assertions.h"
 #include "wtf/Functional.h"
 #include "wtf/MainThread.h"
 #include "wtf/Threading.h"
+#include "wtf/chromium/ChromiumThreading.h"
 
 namespace WTF {
 
