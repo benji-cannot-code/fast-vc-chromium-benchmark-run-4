11359826643b3fc6e2f3b726b6301cff720032a7
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 01:54:07 +0000

    Fix ExtensionSpecialStoragePolicy.IsInstalledApp
    We weren't adding apps to the installed_app set when it doesn't have
    any storage permissions.
    
    BUG=231167
    TEST=ExtensionSpecialStoragePolicyTest.IsInstalled
    
    Review URL: https://codereview.chromium.org/13994010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194511 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_special_storage_policy.cc
+++ chrome/browser/extensions/extension_special_storage_policy.cc
@@ -88,11 +88,10 @@ const ExtensionSet* ExtensionSpecialStoragePolicy::ExtensionsProtectingOrigin(
 void ExtensionSpecialStoragePolicy::GrantRightsForExtension(
     const extensions::Extension* extension) {
   DCHECK(extension);
-  if (!NeedsProtection(extension) &&
-      !extension->HasAPIPermission(
-          APIPermission::kUnlimitedStorage) &&
-      !extension->HasAPIPermission(
-          APIPermission::kFileBrowserHandler)) {
+  if (!(NeedsProtection(extension) ||
+        extension->HasAPIPermission(APIPermission::kUnlimitedStorage) ||
+        extension->HasAPIPermission(APIPermission::kFileBrowserHandler) ||
+        extension->is_app())) {
     return;
   }
   {
@@ -104,8 +103,7 @@ void ExtensionSpecialStoragePolicy::GrantRightsForExtension(
       installed_apps_.Add(extension);
     if (extension->HasAPIPermission(APIPermission::kUnlimitedStorage))
       unlimited_extensions_.Add(extension);
-    if (extension->HasAPIPermission(
-            APIPermission::kFileBrowserHandler))
+    if (extension->HasAPIPermission(APIPermission::kFileBrowserHandler))
       file_handler_extensions_.Add(extension);
   }
   NotifyChanged();
@@ -114,11 +112,10 @@ void ExtensionSpecialStoragePolicy::GrantRightsForExtension(
 void ExtensionSpecialStoragePolicy::RevokeRightsForExtension(
     const extensions::Extension* extension) {
   DCHECK(extension);
-  if (!NeedsProtection(extension) &&
-      !extension->HasAPIPermission(
-          APIPermission::kUnlimitedStorage) &&
-      !extension->HasAPIPermission(
-          APIPermission::kFileBrowserHandler)) {
+  if (!(NeedsProtection(extension) ||
+        extension->HasAPIPermission(APIPermission::kUnlimitedStorage) ||
+        extension->HasAPIPermission(APIPermission::kFileBrowserHandler) ||
+        extension->is_app())) {
     return;
   }
   {
