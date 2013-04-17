b9e60c7d5b3e852b0727666159fc96272a25ac88
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-17 14:23:38 +0000

    DevTools: [Cookies] Users can't see long cookie values.
    Currently users can't edit cookies, so there is no way
    to see full text of long cookie names / values.
    
    Added "longText" attribute to data-grid column descriptor.
    "longText" columns with automatically get title = text, so
    user can browse full cell text value.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/14235014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148558 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/DataGrid.js
+++ third_party/WebKit/Source/devtools/front_end/DataGrid.js
@@ -140,7 +140,7 @@ WebInspector.DataGrid = function(columnsArray, editCallback, deleteCallback, ref
     this._columnWidthsInitialized = false;
 }
 
-/** @typedef {{id: ?string, editable: boolean, sort: WebInspector.DataGrid.Order, sortable: boolean, align: WebInspector.DataGrid.Align}} */
+/** @typedef {{id: ?string, editable: boolean, longText: ?boolean, sort: WebInspector.DataGrid.Order, sortable: boolean, align: WebInspector.DataGrid.Align}} */
 WebInspector.DataGrid.ColumnDescriptor;
 
 WebInspector.DataGrid.Events = {
@@ -1343,8 +1343,11 @@ WebInspector.DataGridNode.prototype = {
         var div = document.createElement("div");
         if (data instanceof Node)
             div.appendChild(data);
-        else
+        else {
             div.textContent = data;
+            if (this.dataGrid.columns[columnIdentifier].longText)
+                div.title = data;
+        }
         cell.appendChild(div);
 
         if (columnIdentifier === this.dataGrid.disclosureColumnIdentifier) {
