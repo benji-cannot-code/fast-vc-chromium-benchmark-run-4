18f8e6fe4aa2f686b6d293b459f9ff8c09fc6d08
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 17:26:39 +0000

    Add the LoggedIn Predictor, to detect which websites a user is likely logged into (or has been logged into). re-submission of https://codereview.chromium.org/13903018
    Review URL: https://codereview.chromium.org/14065014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/predictors/predictor_database.cc
+++ chrome/browser/predictors/predictor_database.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/metrics/histogram.h"
 #include "base/stringprintf.h"
 #include "chrome/browser/predictors/autocomplete_action_predictor_table.h"
+#include "chrome/browser/predictors/logged_in_predictor_table.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor.h"
 #include "chrome/browser/predictors/resource_prefetch_predictor_tables.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
@@ -59,6 +60,7 @@ class PredictorDatabaseInternal
   // TODO(shishir): These tables may not need to be refcounted. Maybe move them
   // to using a WeakPtr instead.
   scoped_refptr<AutocompleteActionPredictorTable> autocomplete_table_;
+  scoped_refptr<LoggedInPredictorTable> logged_in_table_;
   scoped_refptr<ResourcePrefetchPredictorTables> resource_prefetch_tables_;
 
   DISALLOW_COPY_AND_ASSIGN(PredictorDatabaseInternal);
@@ -69,8 +71,8 @@ PredictorDatabaseInternal::PredictorDatabaseInternal(Profile* profile)
     : db_path_(profile->GetPath().Append(kPredictorDatabaseName)),
       db_(new sql::Connection()),
       autocomplete_table_(new AutocompleteActionPredictorTable()),
+      logged_in_table_(new LoggedInPredictorTable()),
       resource_prefetch_tables_(new ResourcePrefetchPredictorTables()) {
-  // TODO (tburkard): initialize logged_in_table_ member.
   ResourcePrefetchPredictorConfig config;
   is_resource_prefetch_predictor_enabled_ =
       IsSpeculativeResourcePrefetchingEnabled(profile, &config);
@@ -84,13 +86,15 @@ PredictorDatabaseInternal::~PredictorDatabaseInternal() {
 
 void PredictorDatabaseInternal::Initialize() {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
-  db_->set_exclusive_locking();
+  // TODO(tburkard): figure out if we need this.
+  //  db_->set_exclusive_locking();
   bool success = db_->Open(db_path_);
 
   if (!success)
     return;
 
   autocomplete_table_->Initialize(db_.get());
+  logged_in_table_->Initialize(db_.get());
   resource_prefetch_tables_->Initialize(db_.get());
 
   LogDatabaseStats();
@@ -100,6 +104,7 @@ void PredictorDatabaseInternal::SetCancelled() {
   CHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   autocomplete_table_->SetCancelled();
+  logged_in_table_->SetCancelled();
   resource_prefetch_tables_->SetCancelled();
 }
 
@@ -113,6 +118,7 @@ void PredictorDatabaseInternal::LogDatabaseStats() {
                           static_cast<int>(db_size / 1024));
 
   autocomplete_table_->LogDatabaseStats();
+  logged_in_table_->LogDatabaseStats();
   if (is_resource_prefetch_predictor_enabled_)
     resource_prefetch_tables_->LogDatabaseStats();
 }
@@ -133,6 +139,11 @@ void PredictorDatabase::Shutdown() {
 scoped_refptr<AutocompleteActionPredictorTable>
     PredictorDatabase::autocomplete_table() {
   return db_->autocomplete_table_;
+}
+
+scoped_refptr<LoggedInPredictorTable>
+    PredictorDatabase::logged_in_table() {
+  return db_->logged_in_table_;
 }
 
 scoped_refptr<ResourcePrefetchPredictorTables>
