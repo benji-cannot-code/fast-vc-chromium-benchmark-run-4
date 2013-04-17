b34c07e63d9a74b1aee504d600d759a5bd654d7d
simonhatch@chromium.org <simonhatch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 23:31:01 +0000

    Delete webkit directory on initial sync as well, as the previous bisection may have left the depot with an old copy of Webkit lying around.
    BUG=
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14265008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194718 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/bisect_utils.py
+++ tools/bisect_utils.py
@@ -9,6 +9,7 @@ used by the bisection scripts."""
 
 import errno
 import os
+import shutil
 import subprocess
 
 
@@ -30,6 +31,7 @@ solutions = [
 ]
 """
 GCLIENT_SPEC = ''.join([l for l in GCLIENT_SPEC.splitlines()])
+FILE_DEPS_GIT = '.DEPS.git'
 
 
 def OutputAnnotationStepStart(name):
@@ -104,6 +106,34 @@ def RunGClientAndCreateConfig():
   return return_code
 
 
+def IsDepsFileBlink():
+  """Reads .DEPS.git and returns whether or not we're using blink.
+
+  Returns:
+    True if blink, false if webkit.
+  """
+  locals = {'Var': lambda _: locals["vars"][_],
+            'From': lambda *args: None}
+  execfile(FILE_DEPS_GIT, {}, locals)
+  return 'blink.git' in locals['vars']['webkit_url']
+
+
+def RemoveThirdPartyWebkitDirectory():
+  """Removes third_party/WebKit.
+
+  Returns:
+    True on success.
+  """
+  try:
+    path_to_dir = os.path.join(os.getcwd(), 'third_party', 'WebKit')
+    if os.path.exists(path_to_dir):
+      shutil.rmtree(path_to_dir)
+  except OSError, e:
+    if e.errno != errno.ENOENT:
+      return False
+  return True
+
+
 def RunGClientAndSync(reset):
   """Runs gclient and does a normal sync.
 
@@ -138,7 +168,17 @@ def SetupGitDepot(output_buildbot_annotations, reset):
   passed = False
 
   if not RunGClientAndCreateConfig():
-    if not RunGClientAndSync(reset):
+    passed_deps_check = True
+    if os.path.isfile(os.path.join('src', FILE_DEPS_GIT)):
+      cwd = os.getcwd()
+      os.chdir('src')
+      if not IsDepsFileBlink():
+        passed_deps_check = RemoveThirdPartyWebkitDirectory()
+      else:
+        passed_deps_check = True
+      os.chdir(cwd)
+
+    if passed_deps_check and not RunGClientAndSync(reset):
       passed = True
 
   if output_buildbot_annotations:
