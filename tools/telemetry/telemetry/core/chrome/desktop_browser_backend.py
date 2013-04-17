1bbc76b749fee44c68feeacdc36a68a70f635bd4
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 02:47:02 +0000

    Better support for chrome for cros local builds.
    * Use ldd to detect chrome for cros local builds. 
    * Add browser types debug-cros and release-cros for chrome for cros on linux. 
    * Remove browser option --cros-desktop 
    
    BUG=229692
    TEST=run_tests works with cros-debug going through the login flow. 
    NOTRY=True
    Review URL: https://codereview.chromium.org/13891011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194522 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/chrome/desktop_browser_backend.py
+++ tools/telemetry/telemetry/core/chrome/desktop_browser_backend.py
@@ -17,7 +17,7 @@ class DesktopBrowserBackend(browser_backend.BrowserBackend):
   """The backend for controlling a locally-executed browser instance, on Linux,
   Mac or Windows.
   """
-  def __init__(self, options, executable, is_content_shell):
+  def __init__(self, options, executable, is_content_shell, use_login):
     super(DesktopBrowserBackend, self).__init__(
         is_content_shell=is_content_shell,
         supports_extensions=not is_content_shell, options=options)
@@ -26,6 +26,8 @@ class DesktopBrowserBackend(browser_backend.BrowserBackend):
     self._proc = None
     self._tmpdir = None
     self._tmp_output_file = None
+
+    self._use_login = use_login
 
     self._executable = executable
     if not self._executable:
@@ -46,7 +48,7 @@ class DesktopBrowserBackend(browser_backend.BrowserBackend):
       self._supports_net_benchmarking = False
       self._LaunchBrowser(options)
 
-    if self.options.cros_desktop:
+    if self._use_login:
       cros_util.NavigateLogin(self)
 
   def _LaunchBrowser(self, options):
@@ -84,7 +86,7 @@ class DesktopBrowserBackend(browser_backend.BrowserBackend):
           shutil.rmtree(self._tmpdir)
           shutil.copytree(self.options.profile_dir, self._tmpdir)
         args.append('--user-data-dir=%s' % self._tmpdir)
-      if self.options.cros_desktop:
+      if self._use_login:
         ext_path = os.path.join(os.path.dirname(__file__), 'chromeos_login_ext')
         args.extend(['--login-manager', '--login-profile=user',
                      '--stub-cros', '--login-screen=login',
