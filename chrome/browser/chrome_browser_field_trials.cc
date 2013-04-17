4a6f1599074de8ef76513630bb7bb34c94fad693
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 21:26:04 +0000

    Remove a couple of unused includes.
    BUG=none
    TEST=compiles
    
    Review URL: https://codereview.chromium.org/14225012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194683 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_field_trials.cc
+++ chrome/browser/chrome_browser_field_trials.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/auto_launch_trial.h"
 #include "chrome/browser/google/google_util.h"
 #include "chrome/browser/gpu/chrome_gpu_util.h"
-#include "chrome/browser/metrics/variations/variations_service.h"
 #include "chrome/browser/omnibox/omnibox_field_trial.h"
 #include "chrome/browser/prerender/prerender_field_trial.h"
 #include "chrome/browser/profiles/profile_manager.h"
--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -112,7 +112,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/env_vars.h"
 #include "chrome/common/logging_chrome.h"
-#include "chrome/common/metrics/variations/variations_util.h"
 #include "chrome/common/net/net_resource_provider.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/profiling.h"
