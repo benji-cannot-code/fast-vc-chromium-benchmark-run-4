cbcb446ed2fb8be0006d9f3d3267628c7cf0c06b
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 17:25:34 +0000

    reland of intsafe precompiled header fix from https://codereview.chromium.org/14201003/
    With fixed include path.
    
    TBR=cpu@chromium.org
    BUG=225822
    
    Review URL: https://codereview.chromium.org/14096011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194620 0039d316-1c4b-4281-b951-d872f2087c98

--- build/precompile.h
+++ build/precompile.h
@@ -108,21 +108,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
-// Workaround for:
-// http://connect.microsoft.com/VisualStudio/feedback/details/621653/
-// http://crbug.com/225822
-// Note that we can't actually include <stdint.h> here because there's other
-// code in third_party that has partial versions of stdint types that conflict.
-#include <intsafe.h>
-#undef INT8_MIN
-#undef INT16_MIN
-#undef INT32_MIN
-#undef INT64_MIN
-#undef INT8_MAX
-#undef UINT8_MAX
-#undef INT16_MAX
-#undef UINT16_MAX
-#undef INT32_MAX
-#undef UINT32_MAX
-#undef INT64_MAX
-#undef UINT64_MAX
+#include "intsafe_workaround.h"
--- win8/delegate_execute/delegate_execute.cc
+++ win8/delegate_execute/delegate_execute.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "build/intsafe_workaround.h"
+
 #include <atlbase.h>
 #include <atlcom.h>
 #include <atlctl.h>
