dc65c4a0ef7242690571a43cf745ecc4d69c40b5
glider@chromium.org <glider@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-17 09:29:18 +0000

    Change the default options for the official ASan build:  - remove quarantine_size, which is too greedy compared to the default value  (this may result in less UAFs caught by ASan, but is very unlikely. ClusterFuzz uses the default setting)  - temporarily set malloc_content_size=5 to avoid the enourmous growth of the stack trace repository  (this will result in less memory footprint and shorter stacks for heap allocations printed in the reports.  However we don't care much about the quality of ASan reports on the client side right now).
    BUG=address-sanitizer:177
    
    Review URL: https://codereview.chromium.org/13932014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/chrome_exe_main_gtk.cc
+++ chrome/app/chrome_exe_main_gtk.cc
@@ -12,10 +12,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // that extra layer with on linux.
 
 #if defined(ADDRESS_SANITIZER) && defined(GOOGLE_CHROME_BUILD)
-// Default AddressSanitizer options: limit the quarantine to 1Gb, disable the
-// strict memcmp() checking (http://crbug.com/178677 and
-// http://crbug.com/178404).
-const char *kAsanDefaultOptions = "quarantine_size=1048576 strict_memcmp=0";
+// Default AddressSanitizer options for the official build. These do not affect
+// tests or non-official Chromium builds.
+//  - disable the strict memcmp() checking (http://crbug.com/178677 and
+//    http://crbug.com/178404).
+//  - set the malloc_context_size (i.e. the size of stack traces collected by
+//    ASan for each malloc/free) to 5. These stack traces tend to accumulate
+//    very fast in applications using JIT (v8 in Chrome's case), see
+//    https://code.google.com/p/address-sanitizer/issues/detail?id=177
+const char *kAsanDefaultOptions = "malloc_context_size=5 strict_memcmp=0";
 
 // Override the default ASan options for the Google Chrome executable.
 // __asan_default_options should not be instrumented, because it is called
