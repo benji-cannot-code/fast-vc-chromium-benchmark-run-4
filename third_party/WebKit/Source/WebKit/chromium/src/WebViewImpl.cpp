511e0283329e9a8b9104460e4a5a642fd709d4ad
danakj@chromium.org <danakj@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-17 01:50:57 +0000

    WebViewImpl should setBackgroundColor in layout() not during paint.
    Changing the layer tree during paint is bad behaviour, and will soon
    not have effect for the frame being painted - which means we'll get
    a frame without the background color that WebViewImpl is expecting.
    
    This moves the call to setBackgroundColor into layout(), with
    appropriate early-outs to not crash, as Layout() is called
    before compositing is active, while painting was not.
    
    R=enne
    BUG=231925
    
    Review URL: https://codereview.chromium.org/13871018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148524 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebViewImpl.cpp
@@ -1807,6 +1807,12 @@ void WebViewImpl::layout()
 
     if (m_linkHighlight)
         m_linkHighlight->updateGeometry();
+
+    if (!m_page)
+        return;
+
+    FrameView* view = m_page->mainFrame()->view();
+    setBackgroundColor(view->documentBackgroundColor());
 }
 
 void WebViewImpl::enterForceCompositingMode(bool enter)
@@ -1823,6 +1829,7 @@ void WebViewImpl::enterForceCompositingMode(bool enter)
         if (!mainFrame)
             return;
         mainFrame->view()->updateCompositingLayersAfterStyleChange();
+        setBackgroundColor(mainFrame->view()->documentBackgroundColor());
     }
 }
 
@@ -3914,6 +3921,9 @@ NonCompositedContentHost* WebViewImpl::nonCompositedContentHost()
 
 void WebViewImpl::setBackgroundColor(const WebCore::Color& color)
 {
+    if (!m_nonCompositedContentHost)
+        return;
+
     WebCore::Color documentBackgroundColor = color.isValid() ? color : WebCore::Color::white;
     WebColor webDocumentBackgroundColor = documentBackgroundColor.rgb();
     m_nonCompositedContentHost->setBackgroundColor(documentBackgroundColor);
@@ -3961,8 +3971,7 @@ void WebViewImpl::paintRootLayer(GraphicsContext& context, const IntRect& conten
     double pixelsPerSec = (contentRect.width() * contentRect.height()) / (paintEnd - paintStart);
     WebKit::Platform::current()->histogramCustomCounts("Renderer4.AccelRootPaintDurationMS", (paintEnd - paintStart) * 1000, 0, 120, 30);
     WebKit::Platform::current()->histogramCustomCounts("Renderer4.AccelRootPaintMegapixPerSecond", pixelsPerSec / 1000000, 10, 210, 30);
-
-    setBackgroundColor(view->documentBackgroundColor());
+    ASSERT(m_nonCompositedContentHost->backgroundColor() == view->documentBackgroundColor());
 }
 
 void WebViewImpl::setIsAcceleratedCompositingActive(bool active)
