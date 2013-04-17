9efe63e6bb40a60d96f1b881bd3ab1a706b0f2ea
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-17 10:44:00 +0000

    DevTools: [CodeMirror] steal indent level from the previous line
    Use indentation of the previous line to indent current one.
    
    BUG=231947
    
    Review URL: https://codereview.chromium.org/13963008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148552 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
@@ -58,6 +58,7 @@ WebInspector.CodeMirrorTextEditor = function(url, delegate)
         lineNumbers: true,
         gutters: ["CodeMirror-linenumbers"],
         matchBrackets: true,
+        smartIndent: false,
         styleSelectedText: true,
         electricChars: false,
         autoCloseBrackets: WebInspector.experimentsSettings.textEditorSmartBraces.isEnabled()
@@ -758,6 +759,10 @@ WebInspector.CodeMirrorTextEditor.BlockIndentController.prototype = {
             codeMirror.execCommand("newlineAndIndent");
             codeMirror.setCursor(cursor);
             codeMirror.execCommand("newlineAndIndent");
+            codeMirror.execCommand("indentMore");
+        } else if (line.substr(cursor.ch-1, 1) === "{") {
+            codeMirror.execCommand("newlineAndIndent");
+            codeMirror.execCommand("indentMore");
         } else
             return CodeMirror.Pass;
     },
