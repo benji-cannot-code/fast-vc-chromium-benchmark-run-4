3c550fd92ea4c2d56a5e85a5d6cbfcbbf93421a9
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 01:52:46 +0000

    Rename SpecialStoragePolicy::IsInstalledApp() to CanQueryDiskSize()
    To fix layering leakage issue.
    
    BUG=none
    TEST=existing tests
    
    Review URL: https://codereview.chromium.org/14237003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194748 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/mock_extension_special_storage_policy.cc
+++ chrome/browser/extensions/mock_extension_special_storage_policy.cc
@@ -21,7 +21,7 @@ bool MockExtensionSpecialStoragePolicy::IsStorageSessionOnly(
   return false;
 }
 
-bool MockExtensionSpecialStoragePolicy::IsInstalledApp(const GURL& origin) {
+bool MockExtensionSpecialStoragePolicy::CanQueryDiskSize(const GURL& origin) {
   return false;
 }
 
