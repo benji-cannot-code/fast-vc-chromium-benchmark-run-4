f55c66f774eaaac1a6b7564d77a5e396901f7924
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-18 06:26:04 +0000

    Speed up ElementData::getAttributeItem, which is hot.
    The previous version had a couple of issues:
    - Incurred new WTF::Vector::operator[] checks.
    - Had a branch in _every_ loop iteration that checked whether the storage was Vector or array[] backed.
    
    Both these issues are fixed, and CloneNodes.html seems reliably at +2%.
    I also believe GetElement.html is maybe +1%.
    
    R=inferno@chromium.org,haraken@chromium.org
    BUG=none
    
    Review URL: https://codereview.chromium.org/13959007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148622 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -114,6 +114,7 @@ private:
     friend class SVGElement;
 #endif
 
+    const Attribute* attributeBase() const;
     const Attribute* getAttributeItem(const AtomicString& name, bool shouldIgnoreAttributeCase) const;
     size_t getAttributeItemIndexSlowCase(const AtomicString& name, bool shouldIgnoreAttributeCase) const;
 
@@ -965,10 +966,19 @@ inline const Attribute* ElementData::getAttributeItem(const AtomicString& name, 
     return 0;
 }
 
+inline const Attribute* ElementData::attributeBase() const
+{
+    if (m_isUnique)
+        return static_cast<const UniqueElementData*>(this)->m_attributeVector.begin();
+    return static_cast<const ShareableElementData*>(this)->m_attributeArray;
+}
+
 inline size_t ElementData::getAttributeItemIndex(const QualifiedName& name) const
 {
+    const Attribute* begin = attributeBase();
     for (unsigned i = 0; i < length(); ++i) {
-        if (attributeItem(i)->name().matches(name))
+        const Attribute& attribute = begin[i];
+        if (attribute.name().matches(name))
             return i;
     }
     return notFound;
@@ -982,10 +992,11 @@ inline size_t ElementData::getAttributeItemIndex(const AtomicString& name, bool 
     bool doSlowCheck = shouldIgnoreAttributeCase;
 
     // Optimize for the case where the attribute exists and its name exactly matches.
+    const Attribute* begin = attributeBase();
     for (unsigned i = 0; i < len; ++i) {
-        const Attribute* attribute = attributeItem(i);
-        if (!attribute->name().hasPrefix()) {
-            if (name == attribute->localName())
+        const Attribute& attribute = begin[i];
+        if (!attribute.name().hasPrefix()) {
+            if (name == attribute.localName())
                 return i;
         } else
             doSlowCheck = true;
@@ -998,19 +1009,19 @@ inline size_t ElementData::getAttributeItemIndex(const AtomicString& name, bool 
 
 inline const Attribute* ElementData::getAttributeItem(const QualifiedName& name) const
 {
+    const Attribute* begin = attributeBase();
     for (unsigned i = 0; i < length(); ++i) {
-        if (attributeItem(i)->name().matches(name))
-            return attributeItem(i);
+        const Attribute& attribute = begin[i];
+        if (attribute.name().matches(name))
+            return &attribute;
     }
     return 0;
 }
 
 inline const Attribute* ElementData::attributeItem(unsigned index) const
 {
-    ASSERT_WITH_SECURITY_IMPLICATION(index < length());
-    if (m_isUnique)
-        return &static_cast<const UniqueElementData*>(this)->m_attributeVector.at(index);
-    return &static_cast<const ShareableElementData*>(this)->m_attributeArray[index];
+    RELEASE_ASSERT(index < length());
+    return attributeBase() + index;
 }
 
 } // namespace
