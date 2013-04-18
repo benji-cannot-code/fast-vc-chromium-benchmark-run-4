7560ec2445103f1774f13133ae929acc2265fdcc
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 10:30:19 +0000

    add --enable-spatial-navigation flag for testing
    This depends on the following blink patch:
    https://codereview.chromium.org/13811041/
    
    Review URL: https://chromiumcodereview.appspot.com/14110006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194875 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/glue/webpreferences.cc
+++ webkit/glue/webpreferences.cc
@@ -138,6 +138,7 @@ WebPreferences::WebPreferences()
 #else
       smart_insert_delete_enabled(false),
 #endif
+      spatial_navigation_enabled(false),
       cookie_enabled(true)
 #if defined(OS_ANDROID)
       ,
@@ -457,6 +458,9 @@ void WebPreferences::Apply(WebView* web_view) const {
   settings->setInitializeAtMinimumPageScale(initialize_at_minimum_page_scale);
 
   settings->setSmartInsertDeleteEnabled(smart_insert_delete_enabled);
+
+  settings->setSpatialNavigationEnabled(spatial_navigation_enabled);
+
   settings->setSelectionIncludesAltImageText(true);
 
 #if defined(OS_ANDROID)
