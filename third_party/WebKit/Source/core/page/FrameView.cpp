13300d2dbdf7a0a52385a491e3d27dccbc5c451e
mthiesse@chromium.org <mthiesse@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-18 16:30:38 +0000

    Fix ASSERT(needsLayout()) failing when autosize mode is enabled
    This is just a revert of http://trac.webkit.org/changeset/147373
    
    BUG=229655
    
    Review URL: https://chromiumcodereview.appspot.com/14139021
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148662 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FrameView.cpp
+++ third_party/WebKit/Source/core/page/FrameView.cpp
@@ -2452,8 +2452,14 @@ void FrameView::autoSizeIfEnabled()
     if (!documentView || !documentElement)
         return;
 
-    // Start from the minimum height and allow it to grow.
-    resize(frameRect().width(), m_minAutoSize.height());
+    RenderBox* documentRenderBox = documentElement->renderBox();
+    if (!documentRenderBox)
+        return;
+
+    // If this is the first time we run autosize, start from small height and
+    // allow it to grow.
+    if (!m_didRunAutosize)
+        resize(frameRect().width(), m_minAutoSize.height());
 
     IntSize size = frameRect().size();
 
@@ -2463,7 +2469,7 @@ void FrameView::autoSizeIfEnabled()
         // Update various sizes including contentsSize, scrollHeight, etc.
         document->updateLayoutIgnorePendingStylesheets();
         int width = documentView->minPreferredLogicalWidth();
-        int height = documentView->documentRect().height();
+        int height = documentRenderBox->scrollHeight();
         IntSize newSize(width, height);
 
         // Check to see if a scrollbar is needed for a given dimension and
