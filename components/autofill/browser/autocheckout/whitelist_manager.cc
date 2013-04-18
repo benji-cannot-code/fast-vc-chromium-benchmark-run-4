66bb2f4894fddf8cb5c747e885c3bdd5fe6f2a5e
ramankk@chromium.org <ramankk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 04:52:33 +0000

    Autofill:Autocheckout: In addition to "Enable Experimental Form filling" flag, support second method of enabling Autocheckout: using finch experiment. Autocheckout will be enabled if either of the conditions is true. Will remove "Enable experimental form filling" flag, once finch experiment is setup completely.
    BUG=230026
    
    Review URL: https://chromiumcodereview.appspot.com/13811045
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194767 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/autocheckout/whitelist_manager.cc
+++ components/autofill/browser/autocheckout/whitelist_manager.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/metrics/field_trial.h"
 #include "base/string_util.h"
 #include "base/strings/string_split.h"
 #include "components/autofill/common/autofill_switches.h"
@@ -42,7 +43,8 @@ WhitelistManager::WhitelistManager()
     : callback_is_pending_(false),
       experimental_form_filling_enabled_(
           CommandLine::ForCurrentProcess()->HasSwitch(
-              switches::kEnableExperimentalFormFilling)),
+              switches::kEnableExperimentalFormFilling) ||
+          base::FieldTrialList::FindFullName("Autocheckout") == "Yes"),
       bypass_autocheckout_whitelist_(
           CommandLine::ForCurrentProcess()->HasSwitch(
                         switches::kBypassAutocheckoutWhitelist)) {
--- components/autofill/renderer/form_autofill_util.cc
+++ components/autofill/renderer/form_autofill_util.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/logging.h"
 #include "base/memory/scoped_vector.h"
+#include "base/metrics/field_trial.h"
 #include "base/string_util.h"
 #include "base/utf_string_conversions.h"
 #include "components/autofill/common/autofill_switches.h"
@@ -76,14 +77,18 @@ bool IsAutofillableElement(const WebFormControlElement& element) {
   return IsAutofillableInputElement(input_element) || IsSelectElement(element);
 }
 
+bool IsAutocheckoutEnabled() {
+  return base::FieldTrialList::FindFullName("Autocheckout") == "Yes" ||
+      CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableExperimentalFormFilling);
+}
+
 // Check whether the given field satisfies the REQUIRE_AUTOCOMPLETE requirement.
 // When Autocheckout is enabled, this requirement is enforced in the browser
 // process rather than in the renderer process, and hence all fields are
 // considered to satisfy this requirement.
 bool SatisfiesRequireAutocomplete(const WebInputElement& input_element) {
-  return input_element.autoComplete() ||
-      CommandLine::ForCurrentProcess()->HasSwitch(
-          switches::kEnableExperimentalFormFilling);
+  return input_element.autoComplete() || IsAutocheckoutEnabled();
 }
 
 // Appends |suffix| to |prefix| so that any intermediary whitespace is collapsed
