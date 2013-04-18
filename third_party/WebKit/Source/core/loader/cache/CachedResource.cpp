19baead7decd8fe23fef87112634d03eff86753a
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-18 21:26:50 +0000

    Add a couple more compile-fixing missing #includes that only mac complains about.
    BUG=none
    TEST=none
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/14246014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148688 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/cache/CachedResource.cpp
+++ third_party/WebKit/Source/core/loader/cache/CachedResource.cpp
@@ -43,6 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceHandle.h"
 #include "SecurityOrigin.h"
 #include "SecurityPolicy.h"
+#include "SharedBuffer.h"
 #include "ResourceLoader.h"
 #include "WebCoreMemoryInstrumentation.h"
 #include <wtf/CurrentTime.h>
