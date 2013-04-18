c4f2a9be2504f003dc1f28d66cb315fb0a8abc2f
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 05:34:26 +0000

    Remove public bool ChangeListLoader::loaded().
    Now, the only outside caller of the method is drive-internals.
    However, after we have moved to DB store, loaded is equivalent to
    having changestamp > 0. So there's no need to have both in the UI.
    
    BUG=none
    
    Review URL: https://codereview.chromium.org/14271007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194783 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_file_system_metadata.cc
+++ chrome/browser/chromeos/drive/drive_file_system_metadata.cc
@@ -9,7 +9,6 @@ namespace drive {
 
 DriveFileSystemMetadata::DriveFileSystemMetadata()
     : largest_changestamp(0),
-      loaded(false),
       refreshing(false),
       last_update_check_error(DRIVE_FILE_OK) {
 }
--- chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
+++ chrome/browser/ui/webui/chromeos/drive_internals_ui.cc
@@ -480,7 +480,6 @@ void DriveInternalsWebUIHandler::OnGetFilesystemMetadataForLocal(
   base::DictionaryValue local_metadata;
   local_metadata.SetDouble("account-largest-changestamp-local",
                            metadata.largest_changestamp);
-  local_metadata.SetBoolean("account-metadata-loaded", metadata.loaded);
   local_metadata.SetBoolean("account-metadata-refreshing", metadata.refreshing);
   web_ui()->CallJavascriptFunction("updateLocalMetadata", local_metadata);
 }
@@ -582,7 +581,6 @@ void DriveInternalsWebUIHandler::UpdateGCacheContentsSection() {
                  weak_ptr_factory_.GetWeakPtr(),
                  base::Owned(gcache_contents),
                  base::Owned(gcache_summary)));
-
 }
 
 void DriveInternalsWebUIHandler::UpdateFileSystemContentsSection() {
