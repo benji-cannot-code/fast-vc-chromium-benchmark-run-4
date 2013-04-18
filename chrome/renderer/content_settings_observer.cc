291c1500b2a0e45283ceb7894cfa877d18f95bc5
ckocagil@chromium.org <ckocagil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 12:41:45 +0000

    Whitelist Instant processes for content settings.
    This patch only grants JS and Image permissions to Instant processes.
    
    BUG=225758
    TEST=InstantTest.ContentSettingsWhitelist
    
    Review URL: https://chromiumcodereview.appspot.com/13516002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194900 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/content_settings_observer.cc
+++ chrome/renderer/content_settings_observer.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/renderer/content_settings_observer.h"
 
+#include "base/command_line.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/render_messages.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/renderer/document_state.h"
@@ -319,6 +321,10 @@ void ContentSettingsObserver::ClearBlockedContentSettings() {
 }
 
 bool ContentSettingsObserver::IsWhitelistedForContentSettings(WebFrame* frame) {
+  // Whitelist Instant processes.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kInstantProcess))
+    return true;
+
   // Whitelist ftp directory listings, as they require JavaScript to function
   // properly.
   webkit_glue::WebURLResponseExtraDataImpl* extra_data =
