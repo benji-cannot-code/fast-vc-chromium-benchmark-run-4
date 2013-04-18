a6b2b96230783c2cb6f1adbbc1db90d2b68ec843
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 05:01:39 +0000

    Restyled Files.app's file list, including scrollbars.
    This patch introduces new awesome scrollbars as well as restyles the file list, including: checkbox to the right side, removing the pinning checkbox, auto-sizing of the columns.
    
    TEST=Run with --file-manager-new-ui. Legacy version should also not be broken.
    BUG=226912
    
    Review URL: https://codereview.chromium.org/14096016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194769 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -887,10 +887,11 @@ DialogType.isModal = function(type) {
     // Populate the static localized strings.
     i18nTemplate.process(this.document_, loadTimeData);
 
-    // Initialize the new header.
+    // Initialize the new header and arrange the file list.
     if (util.platform.newUI()) {
       this.dialogDom_.querySelector('#app-name').innerText =
           chrome.runtime.getManifest().name;
+      this.table_.normalizeColumns();
     }
   };
 
@@ -1324,6 +1325,8 @@ DialogType.isModal = function(type) {
       }, 0);
     } else {
       this.table_.redraw();
+      if (util.platform.newUI())
+        this.table_.normalizeColumns();
     }
 
     if (!util.platform.newUI())
@@ -1685,6 +1688,8 @@ DialogType.isModal = function(type) {
    *     the table layout.
    */
   FileManager.prototype.shouldShowOfflineColumn = function() {
+    if (util.platform.newUI())
+      return false;
     return this.directoryModel_.getCurrentRootType() === RootType.DRIVE;
   };
 
--- chrome/browser/resources/file_manager/js/main_scripts.js
+++ chrome/browser/resources/file_manager/js/main_scripts.js
@@ -88,6 +88,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="file_tasks.js"/>
 //<include src="file_transfer_controller.js"/>
 //<include src="file_type.js"/>
+//<include src="scrollbar.js"/>
 //<include src="sidebar.js"/>
 //<include src="volume_manager.js"/>
 //<include src="media/media_util.js"/>
