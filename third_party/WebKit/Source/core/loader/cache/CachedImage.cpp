d0a84088afaa9a0f861047e2271bce78a4c048df
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-18 21:22:31 +0000

    Compile-fix for mac.
    Add a #include for SharedBuffer.h to CachedImage.cpp, which apparently didn't
    get it in the switch from ResourceBuffer to SharedBuffer.
    
    BUG=none
    TEST=none
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/14267006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/cache/CachedImage.cpp
+++ third_party/WebKit/Source/core/loader/cache/CachedImage.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ResourceLoader.h"
 #include "RuntimeEnabledFeatures.h"
 #include "Settings.h"
+#include "SharedBuffer.h"
 #include <wtf/CurrentTime.h>
 #include <wtf/MemoryInstrumentationHashMap.h>
 #include <wtf/MemoryObjectInfo.h>
