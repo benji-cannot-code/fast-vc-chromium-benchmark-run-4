6265779bf84001694b614f6c5356548517af794e
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 07:13:15 +0000

    Apply the namespace migration for "savefile.default_directory" user-pref as well.
    chrome_download_manager_delegate.cc might have propagated the value of "download.default_directory" to "savefile.default_directory" so we need to update it too.
    
    BUG=229248
    TEST=trybots
    
    Review URL: https://chromiumcodereview.appspot.com/14316012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194830 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/preferences_unittest.cc
+++ chrome/browser/chromeos/preferences_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/prefs/pref_member.h"
 #include "chrome/browser/chromeos/input_method/mock_input_method_manager.h"
+#include "chrome/browser/download/chrome_download_manager_delegate.h"
 #include "chrome/browser/download/download_prefs.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/testing_pref_service_syncable.h"
@@ -59,6 +60,7 @@ TEST(PreferencesTest, TestUpdatePrefOnBrowserScreenDetails) {
   TestingPrefServiceSyncable prefs;
   Preferences::RegisterUserPrefs(prefs.registry());
   DownloadPrefs::RegisterUserPrefs(prefs.registry());
+  ChromeDownloadManagerDelegate::RegisterUserPrefs(prefs.registry());
   // kSelectFileLastDirectory is registered for Profile. Here we register it for
   // testing.
   prefs.registry()->RegisterStringPref(prefs::kSelectFileLastDirectory,
