d191fc803f169d3f2b5df10b7d6a1b4a3d58a9da
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 05:52:23 +0000

    Removed unused thread checker.
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/14261006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194790 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/autofill_country.cc
+++ components/autofill/browser/autofill_country.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/singleton.h"
 #include "base/stl_util.h"
 #include "base/string_util.h"
-#include "base/threading/thread_checker.h"
 #include "base/utf_string_conversions.h"
 #include "grit/generated_resources.h"
 #include "third_party/icu/public/common/unicode/locid.h"
@@ -891,9 +890,6 @@ class CountryNames {
 
   // Maps ICU locale names to their corresponding collators.
   std::map<std::string, icu::Collator*> collators_;
-
-  // Verifies thread-safety of accesses to the application locale.
-  base::ThreadChecker thread_checker_;
 
   DISALLOW_COPY_AND_ASSIGN(CountryNames);
 };
