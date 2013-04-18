88e99fc6af74d33263b56e1896506a9a34bcd70e
justinlin@chromium.org <justinlin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-18 10:51:59 +0000

    Make the tabCapture.capture API require click of the browser action icon.
    BUG=230911
    
    Review URL: https://chromiumcodereview.appspot.com/14096015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@194876 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/tab_capture/tab_capture_api.cc
+++ chrome/browser/extensions/api/tab_capture/tab_capture_api.cc
@@ -7,19 +7,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/extensions/api/tab_capture/tab_capture_api.h"
 
+#include "base/command_line.h"
 #include "base/stringprintf.h"
 #include "base/values.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/extensions/api/tab_capture/tab_capture_registry.h"
 #include "chrome/browser/extensions/api/tab_capture/tab_capture_registry_factory.h"
 #include "chrome/browser/extensions/browser_event_router.h"
 #include "chrome/browser/extensions/event_names.h"
 #include "chrome/browser/extensions/extension_renderer_state.h"
+#include "chrome/browser/extensions/tab_helper.h"
+#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sessions/session_tab_helper.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/feature_switch.h"
+#include "chrome/common/extensions/features/base_feature_provider.h"
+#include "chrome/common/extensions/features/simple_feature.h"
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 
@@ -36,6 +41,8 @@ const char kCapturingSameTab[] = "Cannot capture a tab with an active stream.";
 const char kFindingTabError[] = "Error finding tab to capture.";
 const char kNoAudioOrVideo[] = "Capture failed. No audio or video requested.";
 const char kPermissionError[] = "Tab Capture API flag is not enabled.";
+const char kGrantError[] =
+    "Capture of this tab must be granted by clicking on the extension icon.";
 
 // Keys/values for media stream constraints.
 const char kMediaStreamSource[] = "chromeMediaSource";
@@ -67,6 +74,21 @@ bool TabCaptureCaptureFunction::RunImpl() {
       target_browser->tab_strip_model()->GetActiveWebContents();
   if (!target_contents) {
     error_ = kFindingTabError;
+    return false;
+  }
+
+  const Extension* extension = GetExtension();
+  const std::string& extension_id = extension->id();
+
+  // Make sure either we have been granted permission to capture through an
+  // extension icon click or our extension is whitelisted.
+  if (!TabHelper::FromWebContents(target_contents)->
+          active_tab_permission_granter()->IsGranted(extension) &&
+      CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+          switches::kWhitelistedExtensionID) != extension_id &&
+      !BaseFeatureProvider::GetPermissionFeatures()->GetFeature("tabCapture")->
+          IsIdInWhitelist(extension_id)) {
+    error_ = kGrantError;
     return false;
   }
 
@@ -116,7 +138,7 @@ bool TabCaptureCaptureFunction::RunImpl() {
       extensions::TabCaptureRegistryFactory::GetForProfile(profile());
   if (!registry->AddRequest(render_process_id,
                             routing_id,
-                            GetExtension()->id(),
+                            extension_id,
                             tab_id,
                             tab_capture::TAB_CAPTURE_STATE_NONE)) {
     error_ = kCapturingSameTab;
---
+++ chrome/test/data/extensions/api_test/tab_capture/experimental/active_tab_permission_test.js
@@ -0,0 +1,40 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var afterWhitelistExtension = function(msg) {
+  chrome.tabCapture.capture({audio: true, video: true}, function(stream) {
+    chrome.test.assertTrue(!!stream);
+    stream.stop();
+    chrome.test.succeed();
+  });
+};
+
+var afterOpenNewTab = function(msg) {
+  chrome.tabCapture.capture({audio: true, video: true}, function(stream) {
+    chrome.test.assertTrue(!stream);
+    chrome.test.sendMessage('ready4', afterWhitelistExtension);
+  });
+};
+
+var afterGrantPermission = function(msg) {
+  chrome.tabCapture.capture({audio: true, video: true}, function(stream) {
+    chrome.test.assertTrue(!!stream);
+    stream.stop();
+    chrome.test.sendMessage('ready3', afterOpenNewTab);
+  });
+};
+
+var afterOpenTab = function(msg) {
+  chrome.tabCapture.capture({audio: true, video: true}, function(stream) {
+    chrome.test.assertLastError(
+      'Capture of this tab must be granted by clicking on the extension icon.');
+    chrome.test.assertTrue(!stream);
+
+    chrome.test.sendMessage('ready2', afterGrantPermission);
+  });
+};
+
+chrome.test.notifyPass();
+chrome.test.sendMessage('ready1', afterOpenTab);
