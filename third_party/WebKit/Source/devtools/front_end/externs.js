6725eba09cff69c2447169e76c0590db0a579695
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-18 11:00:32 +0000

    DevTools: [CodeMirror] fix compilation warnings
    Fix compilation warning in CodeMirrorTextEditor.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/14242007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148642 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/externs.js
+++ third_party/WebKit/Source/devtools/front_end/externs.js
@@ -348,6 +348,7 @@ CodeMirror.prototype = {
     execCommand: function(cmd) { },
     extendSelection: function(from, to) { },
     findMarksAt: function(pos) { },
+    findMatchingBracket: function() { },
     findPosH: function(from, amount, unit, visually) { },
     findPosV: function(from, amount, unit, goalColumn) { },
     firstLine: function() { },
@@ -359,6 +360,9 @@ CodeMirror.prototype = {
     getHistory: function() { },
     getInputField: function(){ },
     getLine: function(line) { },
+    /**
+     * @return {{wrapClass: string}}
+     */
     getLineHandle: function(line) { },
     getLineNumber: function(line) { },
     getMode: function() { },
