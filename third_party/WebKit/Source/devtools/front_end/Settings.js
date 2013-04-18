c5a4fde99615723221d184d6ce5fa002427828a7
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-18 07:42:31 +0000

    DevTools: panel navigation shortcut to settings
    Move Ctrl/Cmd + 1-9 shortcut from experiments into settings.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14087011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Settings.js
+++ third_party/WebKit/Source/devtools/front_end/Settings.js
@@ -109,6 +109,7 @@ WebInspector.Settings = function()
     this.messageURLFilters = this.createSetting("messageURLFilters", {});
     this.splitVerticallyWhenDockedToRight = this.createSetting("splitVerticallyWhenDockedToRight", true);
     this.visiblePanels = this.createSetting("visiblePanels", {});
+    this.shortcutPanelSwitch = this.createSetting("shortcutPanelSwitch", false);
 }
 
 WebInspector.Settings.prototype = {
@@ -210,7 +211,6 @@ WebInspector.ExperimentsSettings = function()
     this.textEditorSmartBraces = this._createExperiment("textEditorSmartBraces", "Enable smart braces in text editor");
     this.customizableToolbar = this._createExperiment("customizableToolbar", "Enable toolbar customization");
     this.cpuFlameChart = this._createExperiment("cpuFlameChart", "Show Flame Chart in CPU Profiler");
-    this.shortcutPanelSwitch = this._createExperiment("shortcutPanelSwitch", "Enable Ctrl/Cmd + 1-9 shortcut to switch panels");
 
     this._cleanUpSetting();
 }
