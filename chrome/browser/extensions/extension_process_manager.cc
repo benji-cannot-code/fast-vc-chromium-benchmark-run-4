e1e8fae076872a91888f17d370011a24849b3b41
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 23:03:15 +0000

    Revert 193481 "Simplify ExtensionProcessManager to only load ext..."
    > Simplify ExtensionProcessManager to only load extension background pages when a browser window is opened, and skip the other case of also watching for when extension system is initialized. This gets rid of a dependency on browser finder.
    > 
    > BUG=157279
    > Review URL: https://codereview.chromium.org/13811030
    
    TBR=jam@chromium.org
    BUG=232062
    
    Review URL: https://codereview.chromium.org/13846009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195324 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_process_manager.cc
+++ chrome/browser/extensions/extension_process_manager.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
+#include "chrome/browser/ui/browser_finder.h"
 #include "chrome/common/chrome_notification_types.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/background_info.h"
@@ -138,6 +139,8 @@ ExtensionProcessManager::ExtensionProcessManager(Profile* profile)
   Profile* original_profile = profile->GetOriginalProfile();
   registrar_.Add(this, chrome::NOTIFICATION_BROWSER_WINDOW_READY,
                  content::NotificationService::AllSources());
+  registrar_.Add(this, chrome::NOTIFICATION_EXTENSIONS_READY,
+                 content::Source<Profile>(original_profile));
   registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_LOADED,
                  content::Source<Profile>(original_profile));
   registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_UNLOADED,
@@ -553,13 +556,23 @@ void ExtensionProcessManager::Observe(
         break;
 
       ExtensionService* service = GetProfile()->GetExtensionService();
-      if (!service)
+      if (!service || !service->is_ready())
         break;
 
-      CHECK(service->is_ready());
       CreateBackgroundHostsForProfileStartup();
       break;
     }
+    case chrome::NOTIFICATION_EXTENSIONS_READY: {
+      // Don't load background hosts now if the loading should be deferred.
+      // Instead they will be loaded when a browser window for this profile
+      // (or an incognito profile from this profile) is ready.
+      if (DeferLoadingBackgroundHosts())
+        break;
+
+      CreateBackgroundHostsForProfileStartup();
+      break;
+    }
+
     case chrome::NOTIFICATION_EXTENSION_LOADED: {
       Profile* profile = content::Source<Profile>(source).ptr();
       ExtensionService* service = profile->GetExtensionService();
@@ -760,6 +773,15 @@ void ExtensionProcessManager::ClearBackgroundPageData(
   }
 }
 
+bool ExtensionProcessManager::DeferLoadingBackgroundHosts() const {
+#if defined(OS_ANDROID)
+  return false;
+#else
+  return chrome::GetTotalBrowserCountForProfile(GetProfile()) == 0 &&
+         CommandLine::ForCurrentProcess()->HasSwitch(switches::kShowAppList);
+#endif
+}
+
 //
 // IncognitoExtensionProcessManager
 //
@@ -770,6 +792,13 @@ IncognitoExtensionProcessManager::IncognitoExtensionProcessManager(
       original_manager_(extensions::ExtensionSystem::Get(
           profile->GetOriginalProfile())->process_manager()) {
   DCHECK(profile->IsOffTheRecord());
+
+  // The original profile will have its own ExtensionProcessManager to
+  // load the background pages of the spanning extensions. This process
+  // manager need only worry about the split mode extensions, which is handled
+  // in the NOTIFICATION_BROWSER_WINDOW_READY notification handler.
+  registrar_.Remove(this, chrome::NOTIFICATION_EXTENSIONS_READY,
+                    content::Source<Profile>(profile->GetOriginalProfile()));
 }
 
 IncognitoExtensionProcessManager::~IncognitoExtensionProcessManager() {
