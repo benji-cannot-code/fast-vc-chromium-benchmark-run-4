9866d4f0a2e42275c2c51fcf3eb2f5d24d984d54
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 18:35:33 +0000

    Remove unused ResizeCorner code from BrowserView
    ResizeCorner was disabled in 2009 due to page cycler performance regressions.
    
    BUG=232971
    TEST=compiles, existing browser tests, and you can still resize the window and content paints correctly
    
    Review URL: https://chromiumcodereview.appspot.com/14362005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view.h
+++ chrome/browser/ui/views/frame/browser_view.h
@@ -559,9 +559,6 @@ class BrowserView : public BrowserWindow,
   void UpdateAcceleratorMetrics(const ui::Accelerator& accelerator,
                                 int command_id);
 
-  // Exposes resize corner size to BrowserViewLayout.
-  gfx::Size GetResizeCornerSize() const;
-
   // Create an icon for this window in the launcher (currently only for Ash).
   void CreateLauncherIcon();
 
