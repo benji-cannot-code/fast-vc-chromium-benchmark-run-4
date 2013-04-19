41d5d7818dfc3e9b9aea64ae18d185063c6688d3
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 21:55:39 +0000

    Escape backslash (\x5C) for string value of search query.
    This is not documented yet, but backslash characters should also be escaped.
    
    BUG=232352
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/14352007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195298 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/drive_api_util_unittest.cc
+++ chrome/browser/google_apis/drive_api_util_unittest.cc
@@ -15,6 +15,8 @@ TEST(DriveApiUtilTest, EscapeQueryStringValue) {
   EXPECT_EQ("abcde", EscapeQueryStringValue("abcde"));
   EXPECT_EQ("\\'", EscapeQueryStringValue("'"));
   EXPECT_EQ("\\'abcde\\'", EscapeQueryStringValue("'abcde'"));
+  EXPECT_EQ("\\\\", EscapeQueryStringValue("\\"));
+  EXPECT_EQ("\\\\\\'", EscapeQueryStringValue("\\'"));
 }
 
 TEST(DriveApiUtilTest, TranslateQuery) {
