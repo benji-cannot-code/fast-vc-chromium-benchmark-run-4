a63e3d6aa5b5c2cf22cfd103fc7328cef39d3bda
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 18:02:36 +0000

    Compiler error in Linux ChromiumOS (Clang dbg): third_party/libyuv/source/row_posix.cc:1825:5: error: ran out of registers during register allocation     "movdqa    %[kShuffleMaskARGBToRGB24_0],%%xmm5 \n"     ^
    Revert 195220 - roll clang 176256:179138
    
    BUG=none
    TBR=hans
    
    Review URL: https://chromiumcodereview.appspot.com/14169006
    
    TBR=thakis@chromium.org
    Review URL: https://codereview.chromium.org/14365021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195231 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/clang/scripts/update.sh
+++ tools/clang/scripts/update.sh
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Do NOT CHANGE this if you don't know what you're doing -- see
 # https://code.google.com/p/chromium/wiki/UpdatingClang
 # Reverting problematic clang rolls is safe, though.
-CLANG_REVISION=179138
+CLANG_REVISION=176256
 # ASan Mac builders are pinned to this revision, see http://crbug.com/170629.
 CLANG_ASAN_MAC_REVISION=170392
 
