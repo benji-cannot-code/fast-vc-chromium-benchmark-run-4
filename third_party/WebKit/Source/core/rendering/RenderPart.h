f212cdf2c3cc129e1f600d426e757a81236a881f
caseq@google.com <caseq@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-19 11:16:26 +0000

    Revert "Remove frame flattening support as Chromium has no intention of using it"
    This broke a bunch of tests in debug due to assert in ScrollView::setHasVerticalScrollbar()
    
    TBR=mkwst,jamesr
    BUG=
    
    Review URL: https://codereview.chromium.org/14367019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148720 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderPart.h
+++ third_party/WebKit/Source/core/rendering/RenderPart.h
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-// Renderer for frames and plug-ins, the latter via RenderEmbeddedObject.
+// Renderer for frames via RenderFrameBase, and plug-ins via RenderEmbeddedObject.
 class RenderPart : public RenderWidget {
 public:
     explicit RenderPart(Element*);
