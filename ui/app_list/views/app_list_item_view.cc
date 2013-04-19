ca59a9848413ddc871850d924167bd58c31c92d8
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 15:17:59 +0000

    app_list: Fix label misalignment after installation.
    App item title is manually centered to avoid the default eliding at both
    ends behavior. As such, it needs to re-layout when the title text changes.
    
    BUG=219248
    
    Review URL: https://chromiumcodereview.appspot.com/14327015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195207 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_item_view.cc
+++ ui/app_list/views/app_list_item_view.cc
@@ -183,6 +183,7 @@ void AppListItemView::ItemIconChanged() {
 void AppListItemView::ItemTitleChanged() {
   title_->SetText(UTF8ToUTF16(model_->title()));
   title_->Invalidate();
+  Layout();
 }
 
 void AppListItemView::ItemHighlightedChanged() {
