7239ec8d9a7d8b863afcbd878c20d2c0d80e26b0
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-19 18:42:03 +0000

    Prepare to eliminate WebExceptionCode
    This is a pre-patch which fixes the IDB backend API, as well 
    as the WebKit API, to add non-WebExceptionCode based APIs, since
    these parameters were optional anyway
    
    BUG=233361
    
    Review URL: https://chromiumcodereview.appspot.com/14367004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148742 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebIDBDatabase.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebIDBDatabase.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "../../../Platform/chromium/public/WebCommon.h"
 #include "WebDOMStringList.h"
-#include "WebExceptionCode.h"
 #include "WebIDBMetadata.h"
 
 namespace WebKit {
--- third_party/WebKit/Source/modules/indexeddb/IDBCursor.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBCursor.cpp
@@ -173,8 +173,7 @@ void IDBCursor::advance(unsigned long count, ExceptionCode& ec)
 
     m_request->setPendingCursor(this);
     m_gotValue = false;
-    m_backend->advance(count, m_request, ec);
-    ASSERT(!ec);
+    m_backend->advance(count, m_request);
 }
 
 void IDBCursor::continueFunction(ScriptExecutionContext* context, const ScriptValue& keyValue, ExceptionCode& ec)
@@ -222,8 +221,7 @@ void IDBCursor::continueFunction(PassRefPtr<IDBKey> key, ExceptionCode& ec)
     //        will be on the original context openCursor was called on. Is this right?
     m_request->setPendingCursor(this);
     m_gotValue = false;
-    m_backend->continueFunction(key, m_request, ec);
-    ASSERT(!ec);
+    m_backend->continueFunction(key, m_request);
 }
 
 PassRefPtr<IDBRequest> IDBCursor::deleteFunction(ScriptExecutionContext* context, ExceptionCode& ec)
@@ -244,8 +242,7 @@ PassRefPtr<IDBRequest> IDBCursor::deleteFunction(ScriptExecutionContext* context
         return 0;
     }
     RefPtr<IDBRequest> request = IDBRequest::create(context, IDBAny::create(this), m_transaction.get());
-    m_backend->deleteFunction(request, ec);
-    ASSERT(!ec);
+    m_backend->deleteFunction(request);
     return request.release();
 }
 
