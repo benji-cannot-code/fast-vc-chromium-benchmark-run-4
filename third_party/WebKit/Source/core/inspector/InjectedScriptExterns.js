ec3662bc6ea6fbb04d73a7be8087b21f869d6b50
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-19 11:22:54 +0000

    Fix DevTools frontend closure compilation
    Review URL: https://codereview.chromium.org/14267019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptExterns.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptExterns.js
@@ -33,6 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 var console = { }
 /** @param {...*} vararg */
 console.log = function(vararg) { }
+/** @param {...*} vararg */
+console.table = function(vararg) { }
 
 /**
  * @constructor
