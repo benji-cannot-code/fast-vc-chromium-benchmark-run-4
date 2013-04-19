ee75a2026a2cb877e9f438d603b0c2830d12c860
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-19 23:19:58 +0000

    Remove runtime enable flag for data transfer items.
    This is always enabled by default, so there's no point to keeping this
    flag.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14122015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148767 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/RuntimeEnabledFeatures.h
+++ third_party/WebKit/Source/bindings/v8/RuntimeEnabledFeatures.h
@@ -52,9 +52,6 @@ public:
     static void setApplicationCacheEnabled(bool isEnabled) { isApplicationCacheEnabled = isEnabled; }
     static bool applicationCacheEnabled() { return isApplicationCacheEnabled; }
 
-    static void setDataTransferItemsEnabled(bool isEnabled) { isDataTransferItemsEnabled = isEnabled; }
-    static bool dataTransferItemsEnabled() { return isDataTransferItemsEnabled; }
-
     static void setGeolocationEnabled(bool isEnabled) { isGeolocationEnabled = isEnabled; }
     static bool geolocationEnabled() { return isGeolocationEnabled; }
 
@@ -260,7 +257,6 @@ private:
     static bool isSessionStorageEnabled;
     static bool isWebkitNotificationsEnabled;
     static bool isApplicationCacheEnabled;
-    static bool isDataTransferItemsEnabled;
     static bool isGeolocationEnabled;
     static bool isIndexedDBEnabled;
     static bool isWebAudioEnabled;
