04197df6ef124a1b656ac886b5cc2dc2ea0e619a
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 13:27:31 +0000

    Sends updated event for click on notifications.
    BUG=232231
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/14017014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195159 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_popup_collection.h
+++ ui/message_center/views/message_popup_collection.h
@@ -29,6 +29,7 @@ FORWARD_DECLARE_TEST(WebNotificationTrayTest, ManyPopupNotifications);
 namespace message_center {
 namespace test {
 class MessagePopupCollectionTest;
+class MessagePopupCollectionWidgetsTest;
 }
 
 class MessageCenter;
@@ -57,6 +58,7 @@ class MESSAGE_CENTER_EXPORT MessagePopupCollection
   FRIEND_TEST_ALL_PREFIXES(ash::WebNotificationTrayTest,
                            ManyPopupNotifications);
   friend class test::MessagePopupCollectionTest;
+  friend class test::MessagePopupCollectionWidgetsTest;
   typedef std::map<std::string, ToastContentsView*> ToastContainer;
 
   void CloseAllWidgets();
@@ -87,6 +89,7 @@ class MESSAGE_CENTER_EXPORT MessagePopupCollection
       const std::string& notification_id) OVERRIDE;
 
   void SetWorkAreaForTest(const gfx::Rect& work_area);
+  views::Widget* GetWidgetForId(const std::string& id);
 
   gfx::NativeView parent_;
   MessageCenter* message_center_;
--- ui/message_center/views/message_popup_collection_unittest.cc
+++ ui/message_center/views/message_popup_collection_unittest.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <list>
 
 #include "base/message_loop.h"
+#include "base/strings/string_number_conversions.h"
+#include "base/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/gfx/rect.h"
 #include "ui/message_center/fake_message_center.h"
@@ -18,6 +20,48 @@ namespace message_center {
 namespace test {
 
 class MessagePopupCollectionTest : public views::ViewsTestBase {
+ public:
+  virtual void SetUp() OVERRIDE {
+    views::ViewsTestBase::SetUp();
+    MessageCenter::Initialize();
+    collection_.reset(
+        new MessagePopupCollection(GetContext(), MessageCenter::Get()));
+    collection_->SetWorkAreaForTest(gfx::Rect(0, 0, 1280, 1024));
+    id_ = 0;
+  }
+
+  virtual void TearDown() OVERRIDE {
+    collection_->CloseAllWidgets();
+    collection_.reset();
+    MessageCenter::Shutdown();
+    views::ViewsTestBase::TearDown();
+  }
+
+ protected:
+  size_t GetToastCounts() {
+    return collection_->toasts_.size();
+  }
+
+  bool IsToastShown(const std::string& id) {
+    views::Widget* widget = collection_->GetWidgetForId(id);
+    return widget && widget->IsVisible();
+  }
+
+  std::string AddNotification() {
+    std::string id = base::IntToString(id_++);
+    MessageCenter::Get()->AddNotification(
+        NOTIFICATION_TYPE_BASE_FORMAT, id, UTF8ToUTF16("test title"),
+        UTF8ToUTF16("test message"), string16() /* display_source */,
+        "" /* extension_id */, NULL);
+    return id;
+  }
+
+ private:
+  scoped_ptr<MessagePopupCollection> collection_;
+  int id_;
+};
+
+class MessagePopupCollectionWidgetsTest : public views::ViewsTestBase {
  public:
   virtual void SetUp() OVERRIDE {
     views::ViewsTestBase::SetUp();
@@ -70,7 +114,27 @@ class MessagePopupCollectionTest : public views::ViewsTestBase {
   scoped_ptr<MessagePopupCollection> collection_;
 };
 
-TEST_F(MessagePopupCollectionTest, RepositionWidgets) {
+TEST_F(MessagePopupCollectionTest, DismissOnClick) {
+  std::string id1 = AddNotification();
+  std::string id2 = AddNotification();
+  EXPECT_EQ(2u, GetToastCounts());
+  EXPECT_TRUE(IsToastShown(id1));
+  EXPECT_TRUE(IsToastShown(id2));
+
+  MessageCenter::Get()->ClickOnNotification(id2);
+  RunPendingMessages();
+  EXPECT_EQ(1u, GetToastCounts());
+  EXPECT_TRUE(IsToastShown(id1));
+  EXPECT_FALSE(IsToastShown(id2));
+
+  MessageCenter::Get()->ClickOnNotificationButton(id1, 0);
+  RunPendingMessages();
+  EXPECT_EQ(0u, GetToastCounts());
+  EXPECT_FALSE(IsToastShown(id1));
+  EXPECT_FALSE(IsToastShown(id2));
+}
+
+TEST_F(MessagePopupCollectionWidgetsTest, RepositionWidgets) {
   std::vector<gfx::Rect> rects;
   std::list<views::Widget*> widgets;
   rects.push_back(gfx::Rect(0, 0, 10, 10));
@@ -86,7 +150,7 @@ TEST_F(MessagePopupCollectionTest, RepositionWidgets) {
   EXPECT_EQ("0,60 10x40", GetWidgetRectAt(3).ToString());
 }
 
-TEST_F(MessagePopupCollectionTest, RepositionWidgetsWithTargetDown) {
+TEST_F(MessagePopupCollectionWidgetsTest, RepositionWidgetsWithTargetDown) {
   std::vector<gfx::Rect> rects;
   std::list<views::Widget*> widgets;
   rects.push_back(gfx::Rect(0, 180, 10, 10));
@@ -100,7 +164,7 @@ TEST_F(MessagePopupCollectionTest, RepositionWidgetsWithTargetDown) {
   EXPECT_EQ("0,110 10x40", GetWidgetRectAt(2).ToString());
 }
 
-TEST_F(MessagePopupCollectionTest, RepositionWidgetsWithTargetDownAll) {
+TEST_F(MessagePopupCollectionWidgetsTest, RepositionWidgetsWithTargetDownAll) {
   std::vector<gfx::Rect> rects;
   std::list<views::Widget*> widgets;
   rects.push_back(gfx::Rect(0, 150, 10, 20));
@@ -114,7 +178,7 @@ TEST_F(MessagePopupCollectionTest, RepositionWidgetsWithTargetDownAll) {
   EXPECT_EQ("0,90 10x40", GetWidgetRectAt(2).ToString());
 }
 
-TEST_F(MessagePopupCollectionTest, RepositionWidgetsWithTargetUp) {
+TEST_F(MessagePopupCollectionWidgetsTest, RepositionWidgetsWithTargetUp) {
   std::vector<gfx::Rect> rects;
   std::list<views::Widget*> widgets;
   rects.push_back(gfx::Rect(0, 180, 10, 10));
