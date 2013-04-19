960eeb877d1a3df449c028c751951550dafefdc2
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 21:18:53 +0000

    Android GLNonOwnedContext support GLVirtualContext
    GLVirtualContext expects the share group to have a "real" context.
    
    BUG=230195
    Android only change. Ran through android trybots.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14241033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195289 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_context_android.cc
+++ ui/gl/gl_context_android.cc
@@ -22,7 +22,7 @@ namespace {
 // that we do not have ownership of (draw callback).
 class GLNonOwnedContext : public GLContext {
  public:
-  GLNonOwnedContext();
+  GLNonOwnedContext(GLShareGroup* share_group);
 
   // Implement GLContext.
   virtual bool Initialize(GLSurface* compatible_surface,
@@ -44,7 +44,8 @@ class GLNonOwnedContext : public GLContext {
   DISALLOW_COPY_AND_ASSIGN(GLNonOwnedContext);
 };
 
-GLNonOwnedContext::GLNonOwnedContext() : GLContext(NULL) {}
+GLNonOwnedContext::GLNonOwnedContext(GLShareGroup* share_group)
+  : GLContext(share_group) {}
 
 bool GLNonOwnedContext::MakeCurrent(GLSurface* surface) {
   SetCurrent(this, surface);
@@ -70,7 +71,7 @@ scoped_refptr<GLContext> GLContext::CreateGLContext(
   if (compatible_surface->GetHandle())
     context = new GLContextEGL(share_group);
   else
-    context = new GLNonOwnedContext();
+    context = new GLNonOwnedContext(share_group);
   if (!context->Initialize(compatible_surface, gpu_preference))
     return NULL;
   return context;
