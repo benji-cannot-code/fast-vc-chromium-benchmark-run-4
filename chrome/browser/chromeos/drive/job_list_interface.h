660c8acc9ad2c9d95013f646491466225890e11e
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 13:27:46 +0000

    Pass error code for drive::JobListObserver::OnJobDone().
    Some clients need to distiniguish successful return and failure,
    for example, to show different notification icon.
    
    BUG=128079
    
    Review URL: https://chromiumcodereview.appspot.com/14188035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195160 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/job_list_interface.h
+++ chrome/browser/chromeos/drive/job_list_interface.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/files/file_path.h"
+#include "chrome/browser/chromeos/drive/drive_file_error.h"
 
 namespace drive {
 
@@ -88,7 +89,10 @@ class JobListObserver {
   virtual void OnJobAdded(const JobInfo& job_info) {}
 
   // Called when a job id finished.
-  virtual void OnJobDone(const JobInfo& job_info) {}
+  // |error| is DRIVE_FILE_OK when the job successfully finished, and a value
+  // telling the reason of failure when the jobs is failed.
+  virtual void OnJobDone(const JobInfo& job_info,
+                         DriveFileError error) {}
 
   // Called when a job status is updated.
   virtual void OnJobUpdated(const JobInfo& job_info) {}
