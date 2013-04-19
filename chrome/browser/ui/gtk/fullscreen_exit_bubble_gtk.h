cc3335a8243ac7093cded53c2fd5291c6e41e98e
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 21:07:40 +0000

    Animate the fullscreen exit bubble's opacity instead of its bounds when in immersive fullscreen.
    BUG=188567
    Test=Manual, see instructions below.
    
    1) Run Chrome with --ash-immersive-fullscreen
    2) Go to http://html5-demos.appspot.com/static/fullscreen.html
    3) Use F11 to toggle immersive fullscreen
    4) With the top-of-window views closed, click on the "Toggle Fullscreen" button on the web page.
    5) Hover at the top of the screen till the top-of-window views reveal. (Do this before the exit bubble fades out) Check that the exit fullscreen bubble animates with the top-of-window views.
    6) Once the exit bubble disappears, check that it cannot be brought back by hovering at the top of the screen.
    7) Exit tab fullscreen and browser fullscreen.
    8) Enter tab fullscreen again by pressing the "Toggle fullscreen" button on the web page
    9) Check that the exit fullscreen bubble slides in and out.
       (Note that when the exit bubble is initially shown it does not animate at all)
    10) Go to chrome://settings > Show advanced settings... > Content settings... > Mouse cursor > Manage exceptions...
    11) Remove an exception for www.html5rocks.com if there is one
    12) Go to www.html5rocks.com/en/tutorials/pointerlock/intro
    13) Use F11 to toggle immersive fullscreen
    14) With the top-of-window views closed, click on the "Click me!" yellow box on the web page. (in the Interactive Example) section.
    15) Hover at the top of the screen till the top-of-window views reveal. Check that the mouse lock bubble animates with the top-of-window views.
    16) Click on the "Allow" button in the bubble.
    17) Check that the mouse lock bubble fades out (as opposed to sliding out).
    18) Press Escape and F11 to exit mouse lock and fullscreen
    19) Click on the "Click me!" yellow box again. Check that the bubble slides out.
    
    Review URL: https://chromiumcodereview.appspot.com/13866033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195281 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/fullscreen_exit_bubble_gtk.h
+++ chrome/browser/ui/gtk/fullscreen_exit_bubble_gtk.h
@@ -43,6 +43,7 @@ class FullscreenExitBubbleGtk : public FullscreenExitBubble,
   virtual void Hide() OVERRIDE;
   virtual void Show() OVERRIDE;
   virtual bool IsAnimating() OVERRIDE;
+  virtual bool CanMouseTriggerSlideIn() const OVERRIDE;
 
  private:
   void InitWidgets();
