b29f52c8ce4c34ba899821499a5bc02745a73eea
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 00:29:21 +0000

    Revert 194184 "push MouseMove through WebContents and back to de..."
    Broke mouse dodge on Mac, and perhaps isn't correct for Win Aura anyhow.
    
    BUG=233299
    
    > push MouseMove through WebContents and back to delegate to fix status bubble avoid
    > 
    > This fixes the status bubble not "dodging" the mouse on Aura.
    > 
    > On non-Aura, the path to get to StatusBubbleViews::MouseMoved is:
    > - RenderWidgetHostViewWin::OnMouseEvent
    > - SendMessage to the parent window
    > - WebContentsViewWin::OnMouseMove
    > - Browser::ContentsMouseEvent
    > - StatusBubbleViews::MouseMoved
    > 
    > On Aura, the path is:
    > - HWNDMessageHandler
    > - DesktkopRootWindowHostWin::HandleMouseEvent
    > - RootWindow::OnHostMoustEvent
    > - EventDispatcher, etc.
    > - content::RenderWidgetHostViewAura::OnMouseEvent
    > - RenderViewHostImpl::ForwardMouseEvent
    > - WebContentsImpl::HandleMouseMove  // ***
    > - Browser::ContentsMoveEvent
    > - StatusBubbleViews::MouseMoved.
    > 
    > This patch implements HandleMouseMove in WebContentsImpl to delegate back to Browser for Aura at ***.
    > 
    > R=jam@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/14234020
    
    TBR=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/13893004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195027 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.cc
+++ content/browser/web_contents/web_contents_impl.cc
@@ -1258,13 +1258,6 @@ bool WebContentsImpl::PreHandleWheelEvent(
   return false;
 }
 
-void WebContentsImpl::HandleMouseMove() {
-  if (delegate_) {
-    delegate_->ContentsMouseEvent(
-        this, gfx::Screen::GetNativeScreen()->GetCursorScreenPoint(), true);
-  }
-}
-
 void WebContentsImpl::HandleMouseDown() {
   if (delegate_)
     delegate_->HandleMouseDown();
--- content/public/browser/web_contents_delegate.h
+++ content/public/browser/web_contents_delegate.h
@@ -257,7 +257,6 @@ class CONTENT_EXPORT WebContentsDelegate {
   virtual void HandleKeyboardEvent(WebContents* source,
                                    const NativeWebKeyboardEvent& event) {}
 
-  virtual void HandleMouseMove() {}
   virtual void HandleMouseDown() {}
   virtual void HandleMouseUp() {}
 
