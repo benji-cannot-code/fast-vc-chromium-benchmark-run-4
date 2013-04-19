88fdd64c782704a04483fc31daf09d4eac5c823c
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-19 22:12:26 +0000

    Change to update panel positions after panels moved to the new collection when the unstacking happens
    When the unstacking happens, we should first move the panels to the new collection and then update their positions. If we're update panel positions first, the existing stack window will enclose 2 sets of panels that are not aligned which causes the background window to be seen.
    
    BUG=223155
    TEST=existing tests
    
    Review URL: https://codereview.chromium.org/14175016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_drag_controller.cc
+++ chrome/browser/ui/panels/panel_drag_controller.cc
@@ -447,16 +447,6 @@ bool PanelDragController::TryUnstackFromTop(const gfx::Point& target_position) {
     return false;
   }
 
-  // Move the panel (and all the panels below if in a stack) to the new
-  // position.
-  for (StackedPanelCollection::Panels::const_iterator iter =
-           dragging_stack->panels().begin();
-       iter != dragging_stack->panels().end(); ++iter) {
-    if (!(*iter)->in_preview_mode())
-      break;
-    (*iter)->MoveByInstantly(delta);
-  }
-
   int num_panels_in_stack = dragging_stack->num_panels();
   DCHECK_GE(num_panels_in_stack, 2);
 
@@ -473,6 +463,7 @@ bool PanelDragController::TryUnstackFromTop(const gfx::Point& target_position) {
     MovePanelAndBelowToCollection(dragging_panel_,
                                   detached_collection,
                                   PanelCollection::KNOWN_POSITION);
+    dragging_panel_->MoveByInstantly(delta);
     return true;
   }
 
@@ -481,8 +472,9 @@ bool PanelDragController::TryUnstackFromTop(const gfx::Point& target_position) {
   // If only one panel (top panel) needs to unstack, move it out of the stack.
   if (num_panels_to_unstack == 1) {
     panel_manager_->MovePanelToCollection(dragging_panel_,
-                                          panel_manager_->detached_collection(),
+                                          detached_collection,
                                           PanelCollection::KNOWN_POSITION);
+    dragging_panel_->MoveByInstantly(delta);
     return true;
   }
 
@@ -490,8 +482,9 @@ bool PanelDragController::TryUnstackFromTop(const gfx::Point& target_position) {
   // bottom panel out of the stack.
   if (num_panels_in_stack - num_panels_to_unstack == 1) {
     panel_manager_->MovePanelToCollection(dragging_stack->bottom_panel(),
-                                          panel_manager_->detached_collection(),
+                                          detached_collection,
                                           PanelCollection::KNOWN_POSITION);
+    dragging_panel_->stack()->MoveAllDraggingPanelsInstantly(delta);
     return true;
   }
 
@@ -514,6 +507,7 @@ bool PanelDragController::TryUnstackFromTop(const gfx::Point& target_position) {
                                           new_stack,
                                           PanelCollection::KNOWN_POSITION);
   }
+  dragging_panel_->stack()->MoveAllDraggingPanelsInstantly(delta);
 
   return true;
 }
@@ -547,13 +541,7 @@ bool PanelDragController::TryUnstackFromBottom(
     return false;
   }
 
-  // Move the panel (and all the panels below if in a stack) to the new
-  // position.
   gfx::Vector2d delta = target_position - dragging_panel_->GetBounds().origin();
-  if (dragging_stack)
-    dragging_stack->MoveAllDraggingPanelsInstantly(delta);
-  else
-    dragging_panel_->MoveByInstantly(delta);
 
   // If there're only 2 panels in the stack, both panels should move out the
   // stack and the stack should be removed.
@@ -563,6 +551,7 @@ bool PanelDragController::TryUnstackFromBottom(
     MovePanelAndBelowToCollection(dragging_stack->top_panel(),
                                   detached_collection,
                                   PanelCollection::KNOWN_POSITION);
+    dragging_panel_->MoveByInstantly(delta);
     return true;
   }
 
@@ -574,6 +563,7 @@ bool PanelDragController::TryUnstackFromBottom(
     panel_manager_->MovePanelToCollection(dragging_panel_,
                                           detached_collection,
                                           PanelCollection::KNOWN_POSITION);
+    dragging_panel_->MoveByInstantly(delta);
     return true;
   }
 
@@ -584,6 +574,7 @@ bool PanelDragController::TryUnstackFromBottom(
     panel_manager_->MovePanelToCollection(dragging_stack->top_panel(),
                                           detached_collection,
                                           PanelCollection::KNOWN_POSITION);
+    dragging_panel_->stack()->MoveAllDraggingPanelsInstantly(delta);
     return true;
   }
 
@@ -596,6 +587,7 @@ bool PanelDragController::TryUnstackFromBottom(
   MovePanelAndBelowToCollection(dragging_panel_,
                                 new_stack,
                                 PanelCollection::KNOWN_POSITION);
+  dragging_panel_->stack()->MoveAllDraggingPanelsInstantly(delta);
 
   return true;
 }
