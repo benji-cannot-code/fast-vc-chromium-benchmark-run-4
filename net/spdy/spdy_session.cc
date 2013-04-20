36716664e7b94395375e2ba6171ef5e97739b3c7
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-20 00:56:30 +0000

    [SPDY] Address comments from review of r195309
    BUG=176592
    
    Review URL: https://codereview.chromium.org/14177006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195354 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -1857,7 +1857,7 @@ void SpdySession::OnWindowUpdate(SpdyStreamId stream_id,
           ERR_SPDY_PROTOCOL_ERROR,
           true,
           "Received WINDOW_UPDATE with an invalid delta_window_size " +
-          base::UintToString(delta_window_size));
+              base::UintToString(delta_window_size));
     } else {
       ResetStream(stream_id, RST_STREAM_FLOW_CONTROL_ERROR,
                   base::StringPrintf(
@@ -2256,9 +2256,9 @@ void SpdySession::IncreaseSendWindowSize(int32 delta_window_size) {
         ERR_SPDY_PROTOCOL_ERROR,
         true,
         "Received WINDOW_UPDATE [delta: " +
-        base::IntToString(delta_window_size) +
-        "] for session overflows session_send_window_size_ [current: " +
-        base::IntToString(session_send_window_size_) + "]");
+            base::IntToString(delta_window_size) +
+            "] for session overflows session_send_window_size_ [current: " +
+            base::IntToString(session_send_window_size_) + "]");
     return;
   }
 
@@ -2338,8 +2338,8 @@ void SpdySession::DecreaseRecvWindowSize(int32 delta_window_size) {
         ERR_SPDY_PROTOCOL_ERROR,
         true,
         "delta_window_size is " + base::IntToString(delta_window_size) +
-        " in DecreaseRecvWindowSize, which is larger than the receive " +
-        "window size of " + base::IntToString(session_recv_window_size_));
+            " in DecreaseRecvWindowSize, which is larger than the receive " +
+            "window size of " + base::IntToString(session_recv_window_size_));
     return;
   }
 
