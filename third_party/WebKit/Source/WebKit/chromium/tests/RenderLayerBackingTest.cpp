9bfe9b3e2a6195eeac6537f72df0d0705e545041
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 22:23:52 +0000

    Disable solid color graphics layer tests since we disabled the optimization
    We had to turn this feature off in r148856, so these tests are disabled until we
    fix the feature.
    
    R=scheib@chromium.org
    
    Review URL: https://codereview.chromium.org/14159017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148862 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/RenderLayerBackingTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/RenderLayerBackingTest.cpp
@@ -115,7 +115,7 @@ protected:
     WebKitTests::WebCompositorInitializer m_compositorInitializer;
 };
 
-TEST_F(RenderLayerBackingTest, GraphicsLayerBackgroundColor)
+TEST_F(RenderLayerBackingTest, DISABLED_GraphicsLayerBackgroundColor)
 {
     registerMockedHttpURLLoad("layer_background_color.html");
     navigateTo(m_baseURL + "layer_background_color.html");
