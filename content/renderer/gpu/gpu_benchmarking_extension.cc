4305ac03360db82f29522e0683c17ffee148caf3
scroggo@google.com <scroggo@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-22 21:27:21 +0000

    Use a bitmap encoder when serializing to SkPicture
    BUG=168220
    
    Review URL: https://chromiumcodereview.appspot.com/14270002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195602 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/gpu_benchmarking_extension.cc
+++ content/renderer/gpu/gpu_benchmarking_extension.cc
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFrame.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebViewBenchmarkSupport.h"
+#include "ui/gfx/codec/png_codec.h"
 #include "v8/include/v8.h"
 #include "webkit/compositor_bindings/web_rendering_stats_impl.h"
 
@@ -35,6 +36,15 @@ using WebKit::WebView;
 using WebKit::WebViewBenchmarkSupport;
 
 const char kGpuBenchmarkingExtensionName[] = "v8/GpuBenchmarking";
+
+static bool PNGEncodeBitmapToStream(SkWStream* stream, const SkBitmap& bm) {
+    std::vector<unsigned char> vector;
+    if (gfx::PNGCodec::EncodeBGRASkBitmap(bm, true, &vector)) {
+        if (stream->write(&vector.front() , vector.size()))
+            return true;
+    }
+    return false;
+}
 
 namespace {
 
@@ -74,7 +84,7 @@ class SkPictureRecorder : public WebViewBenchmarkSupport::PaintClient {
     DCHECK(!filepath.empty());
     SkFILEWStream file(filepath.c_str());
     DCHECK(file.isValid());
-    picture_.serialize(&file);
+    picture_.serialize(&file, &PNGEncodeBitmapToStream);
   }
 
  private:
