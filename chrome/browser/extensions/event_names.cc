a8aa6b3687d053bdd3ac9029b4442d119c4ddfa7
youngki@chromium.org <youngki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-22 16:34:43 +0000

    added AddProfile, RemoveProfile, and GetProfiles API (plumbing work)
    This is an empty implementation for bluetooth.addProfile, bluetooth.removeProfile, and bluetooth.getProfiles according to the new proposed Bluetooth JS API doc. I will add more implementations once BluetoothProfile CL is submitted.
    
    BUG=229636
    
    Review URL: https://chromiumcodereview.appspot.com/14241035
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195532 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/event_names.cc
+++ chrome/browser/extensions/event_names.cc
@@ -64,6 +64,7 @@ const char kOnTabCaptureStatusChanged[] = "tabCapture.onStatusChanged";
 
 const char kBluetoothOnAdapterStateChanged[] =
     "bluetooth.onAdapterStateChanged";
+const char kBluetoothOnConnection[] = "bluetooth.onConnection";
 const char kBluetoothOnDeviceDiscovered[] = "bluetooth.onDeviceDiscovered";
 const char kBluetoothOnDeviceSearchFinished[] =
     "bluetooth.onDeviceSearchFinished";
