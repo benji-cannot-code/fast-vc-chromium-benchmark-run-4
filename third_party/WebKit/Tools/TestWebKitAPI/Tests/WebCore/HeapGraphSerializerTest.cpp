58618f0da7024ee89c54bc039b869cb69f4494b6
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 23:36:09 +0000

    Fix mac build
    I'm not sure why this build error isn't showing up on the bots, but it doesn't
    build locally for me because I removed this header in a previous CL.
    
    TBR=ojan
    
    Review URL: https://codereview.chromium.org/14259011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148869 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/Tests/WebCore/HeapGraphSerializerTest.cpp
+++ third_party/WebKit/Tools/TestWebKitAPI/Tests/WebCore/HeapGraphSerializerTest.cpp
@@ -30,15 +30,27 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 #include "config.h"
-#include "WTFStringUtilities.h"
-#include <gtest/gtest.h>
 
 #include "HeapGraphSerializer.h"
 #include "MemoryInstrumentationImpl.h"
-#include <wtf/MemoryInstrumentation.h>
-#include <wtf/MemoryInstrumentationHashSet.h>
-#include <wtf/MemoryInstrumentationString.h>
-#include <wtf/MemoryObjectInfo.h>
+#include "wtf/Assertions.h"
+#include "wtf/MemoryInstrumentation.h"
+#include "wtf/MemoryInstrumentationHashSet.h"
+#include "wtf/MemoryInstrumentationString.h"
+#include "wtf/MemoryObjectInfo.h"
+#include "wtf/text/CString.h"
+#include "wtf/text/StringBuilder.h"
+#include "wtf/text/WTFString.h"
+#include <gtest/gtest.h>
+
+namespace WTF {
+
+inline std::ostream& operator<<(std::ostream& os, const String& string)
+{
+    return os << string.utf8().data();
+}
+
+}
 
 namespace TestWebKitAPI {
 
