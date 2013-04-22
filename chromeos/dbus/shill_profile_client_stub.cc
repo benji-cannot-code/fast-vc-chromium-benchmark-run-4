9a32151b76750c9d7313b2c8e930c0dceebcbf7b
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-22 08:00:41 +0000

    Extend networkingPrivate api_test to use NetworkConfigurationUpdater.
    This removes the api test's dependency on ManagedNetworkConfigurationHandler for policy application and instead uses the policy provider. Thus, this improves the testing of policy integration.
    
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/14057021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195461 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/shill_profile_client_stub.cc
+++ chromeos/dbus/shill_profile_client_stub.cc
@@ -23,6 +23,8 @@ namespace chromeos {
 
 namespace {
 
+const char kSharedProfilePath[] = "/profile/default";
+
 void PassEmptyDictionary(
     const ShillProfileClient::DictionaryValueCallbackWithoutStatus& callback) {
   base::DictionaryValue dictionary;
@@ -53,6 +55,7 @@ base::DictionaryValue* GetOrCreateDictionary(const std::string& key,
 }  // namespace
 
 ShillProfileClientStub::ShillProfileClientStub() {
+  AddProfile(kSharedProfilePath);
 }
 
 ShillProfileClientStub::~ShillProfileClientStub() {
