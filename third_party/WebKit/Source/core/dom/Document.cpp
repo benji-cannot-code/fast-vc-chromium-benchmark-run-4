8a40457eb334698176279cd485f310a7624484ac
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 02:54:51 +0000

    Remove isPluginElement hack in setFocusedNode.
    This hack is no longer necessary because [1] made isFocusable for plugin
    elements work correctly.
    
    [1] http://src.chromium.org/viewvc/blink?revision=147767&view=revision
    
    TEST=none; no behavior changes.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/14394002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -3268,7 +3268,7 @@ bool Document::setFocusedNode(PassRefPtr<Node> prpNewFocusedNode, FocusDirection
         }
     }
 
-    if (newFocusedNode && (newFocusedNode->isPluginElement() || newFocusedNode->isFocusable())) {
+    if (newFocusedNode && newFocusedNode->isFocusable()) {
         if (newFocusedNode->isRootEditableElement() && !acceptsEditingFocus(newFocusedNode.get())) {
             // delegate blocks focus change
             focusChangeBlocked = true;
