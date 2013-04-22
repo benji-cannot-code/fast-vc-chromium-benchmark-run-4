87bea36ca5d654c2b003a83abb621eadac9da2e3
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 16:19:29 +0000

    Replace YARR usage in inspector with RegularExpression
    We can replace the usage of YARR in ContentSearchUtils with
    RegularExpression and substring + find. This removes the only
    usage of YARR in blink outside RegularExpression.
    
    Review URL: https://codereview.chromium.org/14390003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148829 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/ContentSearchUtils.cpp
+++ third_party/WebKit/Source/core/inspector/ContentSearchUtils.cpp
@@ -29,15 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "config.h"
 
-
 #include "ContentSearchUtils.h"
 
 #include "InspectorValues.h"
 #include "RegularExpression.h"
-
-#include <wtf/BumpPointerAllocator.h>
 #include <wtf/StdLibExtras.h>
-#include <yarr/Yarr.h>
 
 using namespace std;
 
@@ -170,22 +166,20 @@ PassRefPtr<TypeBuilder::Array<TypeBuilder::Page::SearchMatch> > searchInTextByLi
 
 static String findMagicComment(const String& content, const String& name)
 {
-    String patternString = "//@[\040\t]" + name + "=[\040\t]*([^\\s\'\"]*)[\040\t]*$";
-    const char* error = 0;
-    JSC::Yarr::YarrPattern pattern(patternString, false, true, &error);
-    ASSERT(!error);
-    BumpPointerAllocator regexAllocator;
-    OwnPtr<JSC::Yarr::BytecodePattern> bytecodePattern = JSC::Yarr::byteCompile(pattern, &regexAllocator);
-    ASSERT(bytecodePattern);
+    ASSERT(name.find("=") == notFound);
+    String pattern = "//@[\040\t]" + createSearchRegexSource(name) + "=[\040\t]*[^\\s\'\"]*[\040\t]*$";
+    RegularExpression regex(pattern, TextCaseSensitive, MultilineEnabled);
 
-    ASSERT(pattern.m_numSubpatterns == 1);
-    Vector<int, 4> matches;
-    matches.resize(4);
-    unsigned result = JSC::Yarr::interpret(bytecodePattern.get(), content, 0, reinterpret_cast<unsigned*>(matches.data()));
-    if (result == JSC::Yarr::offsetNoMatch)
+    int matchLength;
+    int offset = regex.match(content, 0, &matchLength);
+    if (offset == -1)
         return String();
-    ASSERT(matches[2] > 0 && matches[3] > 0);
-    return content.substring(matches[2], matches[3] - matches[2]);
+
+    String match = content.substring(offset, matchLength);
+    size_t separator = match.find("=");
+    ASSERT(separator != notFound);
+
+    return match.substring(separator + 1).stripWhiteSpace();
 }
 
 String findSourceURL(const String& content)
