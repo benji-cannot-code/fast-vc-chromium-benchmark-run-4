a08559c1d482c8f7be7b6412512c690f8d06f03e
alecflett@chromium.org <alecflett@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 22:40:56 +0000

    Remove old WebExceptionCode methods from IDB backend.
    BUG=233361
    
    Review URL: https://codereview.chromium.org/14192008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148864 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebIDBCursor.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebIDBCursor.h
@@ -29,7 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "../../../Platform/chromium/public/WebCommon.h"
 #include "../../../Platform/chromium/public/WebString.h"
-#include "WebExceptionCode.h"
 #include "WebIDBCallbacks.h"
 #include "WebIDBKey.h"
 
@@ -47,30 +46,10 @@ public:
         PrevNoDuplicate = 3,
     };
 
-    virtual void advance(unsigned long, WebIDBCallbacks*, WebExceptionCode&) { WEBKIT_ASSERT_NOT_REACHED(); }
-    virtual void advance(unsigned long count, WebIDBCallbacks* callbacks)
-    {
-        WebExceptionCode ec;
-        advance(count, callbacks, ec);
-    }
-    virtual void continueFunction(const WebIDBKey&, WebIDBCallbacks*, WebExceptionCode&) { WEBKIT_ASSERT_NOT_REACHED(); }
-    virtual void continueFunction(const WebIDBKey& key, WebIDBCallbacks* callbacks)
-    {
-        WebExceptionCode ec;
-        continueFunction(key, callbacks, ec);
-    }
-    virtual void deleteFunction(WebIDBCallbacks*, WebExceptionCode&) { WEBKIT_ASSERT_NOT_REACHED(); }
-    virtual void deleteFunction(WebIDBCallbacks* callbacks)
-    {
-        WebExceptionCode ec;
-        deleteFunction(callbacks, ec);
-    }
-    virtual void prefetchContinue(int numberToFetch, WebIDBCallbacks*, WebExceptionCode&) { WEBKIT_ASSERT_NOT_REACHED(); }
-    virtual void prefetchContinue(int numberToFetch, WebIDBCallbacks* callbacks)
-    {
-        WebExceptionCode ec;
-        prefetchContinue(numberToFetch, callbacks, ec);
-    }
+    virtual void advance(unsigned long, WebIDBCallbacks*) { WEBKIT_ASSERT_NOT_REACHED(); }
+    virtual void continueFunction(const WebIDBKey&, WebIDBCallbacks*) { WEBKIT_ASSERT_NOT_REACHED(); }
+    virtual void deleteFunction(WebIDBCallbacks*) { WEBKIT_ASSERT_NOT_REACHED(); }
+    virtual void prefetchContinue(int numberToFetch, WebIDBCallbacks*) { WEBKIT_ASSERT_NOT_REACHED(); }
     virtual void prefetchReset(int usedPrefetches, int unusedPrefetches) { WEBKIT_ASSERT_NOT_REACHED(); }
     virtual void postSuccessHandlerCallback() { } // Only used in frontend.
 
