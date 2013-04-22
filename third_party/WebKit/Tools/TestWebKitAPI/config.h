dbb5f1011d930f86d55b80fdd300051c298daa5d
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 19:27:39 +0000

    Remove unused files in TestWebKitAPI
    These files are no longer used. This CL is one step towards deleting
    TestWebKitAPI entirely.
    
    Review URL: https://codereview.chromium.org/14061006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148847 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestWebKitAPI/config.h
+++ third_party/WebKit/Tools/TestWebKitAPI/config.h
@@ -24,15 +24,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#if defined(HAVE_CONFIG_H) && HAVE_CONFIG_H
-#include "autotoolsconfig.h"
-#endif
-
 #include <wtf/Platform.h>
 #include <wtf/ExportMacros.h>
-#if USE(JSC)
-#include <runtime/JSExportMacros.h>
-#endif
 
 #if defined(__APPLE__) && __APPLE__
 
