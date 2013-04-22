ffe31fd468722650de1271ca383f0b36072485c7
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-22 18:28:32 +0000

    Switch RegularExpression from YARR to V8
    Make RegularExpression use V8 instead of YARR. This removes all but one
    last usage of YARR in ContentSearchUtils which can be removed in a follow
    up patch.
    
    It's a little awkward to use V8's regular expression features from C++ since
    you need to interact with the JS object fields, but this is the best we can do
    for now. Eventually it'd be nice if V8 exposed a RegExp API directly.
    
    This patch also fixes a bug identified in the process of writing a test where
    having an invalid regex in the pattern attribute on an <input> results in an
    input that can never be valid. Instead we should ignore the attribute.
    
    http://www.whatwg.org/specs/web-apps/current-work/multipage/common-input-element-attributes.html#the-pattern-attribute
    
    Review URL: https://codereview.chromium.org/13896017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/text/RegularExpression.h
+++ third_party/WebKit/Source/core/platform/text/RegularExpression.h
@@ -1,6 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
  * Copyright (C) 2003, 2008, 2009 Apple Inc. All rights reserved.
+ * Copyright (C) 2013 Google Inc. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -27,11 +28,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef RegularExpression_h
 #define RegularExpression_h
 
-#include <wtf/BumpPointerAllocator.h>
+#include "ScopedPersistent.h"
 #include <wtf/Noncopyable.h>
-#include <wtf/PassOwnPtr.h>
 #include <wtf/text/WTFString.h>
-#include <yarr/Yarr.h>
+#include <v8.h>
 
 namespace WebCore {
 
@@ -47,12 +47,10 @@ public:
 
     int match(const String&, int startFrom = 0, int* matchLength = 0) const;
 
-private:
-    PassOwnPtr<JSC::Yarr::BytecodePattern> compile(const String&, TextCaseSensitivity, MultilineMode);
+    bool isValid() const { return !m_regex.isEmpty(); }
 
-    unsigned m_numSubpatterns;
-    OwnPtr<JSC::Yarr::BytecodePattern> m_regExpByteCode;
-    BumpPointerAllocator m_regexAllocator;
+private:
+    ScopedPersistent<v8::RegExp> m_regex;
 };
 
 void replace(String&, const RegularExpression&, const String&);
