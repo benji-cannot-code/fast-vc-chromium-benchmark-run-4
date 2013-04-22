ba4153bfaa7dc6d66276dbfe014ae4205e3d52f9
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-22 19:08:24 +0000

    retry 195316 "Remove "Use billing for shipping" checkbox in fav..."
    this time, don't break Mac.
    
    BUG=225093
    TBR=sky@chromium.org,isherman@chromium.org,aruslan@chromium.org,sail@chromium.org
    
    Review URL: https://codereview.chromium.org/13879013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195569 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_models.h
+++ chrome/browser/ui/autofill/autofill_dialog_models.h
@@ -24,7 +24,8 @@ class SuggestionsMenuModelDelegate {
   virtual ~SuggestionsMenuModelDelegate();
 
   // Called when a menu item has been activated.
-  virtual void SuggestionItemSelected(const SuggestionsMenuModel& model) = 0;
+  virtual void SuggestionItemSelected(SuggestionsMenuModel* model,
+                                      size_t index) = 0;
 };
 
 // A model for the dropdowns that allow the user to select from different
@@ -68,6 +69,10 @@ class SuggestionsMenuModel : public ui::SimpleMenuModel,
   // there are no items.
   std::string GetItemKeyForCheckedItem() const;
 
+  // Sets which item is checked.
+  void SetCheckedItem(const std::string& item_key);
+  void SetCheckedIndex(size_t index);
+
   int checked_item() { return checked_item_; }
 
   // ui::SimpleMenuModel::Delegate implementation.
@@ -86,7 +91,8 @@ class SuggestionsMenuModel : public ui::SimpleMenuModel,
 
   SuggestionsMenuModelDelegate* delegate_;
 
-  // The command id (and index) of the item which is currently checked.
+  // The command id (and index) of the item which is currently checked. Only one
+  // item is checked at a time.
   int checked_item_;
 
   DISALLOW_COPY_AND_ASSIGN(SuggestionsMenuModel);
