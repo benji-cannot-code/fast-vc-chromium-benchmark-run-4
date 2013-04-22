efa4edf7ddd5f1b5a283954458dee08f7ecf4857
courage@chromium.org <courage@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-22 21:28:31 +0000

    Identity API: Add token cache and identity.invalidateAuthToken.
    Identity.getAuthToken caches access tokens from GAIA. Tokens expirations
    are handled automatically, but if an access token goes bad for some other
    reason, the application can call identity.invalidateAuthToken to remove
    a bad token from the cache.
    
    BUG=228908
    (step #1 of the bug description)
    
    Review URL: https://chromiumcodereview.appspot.com/14329014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195604 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gaia/oauth2_mint_token_flow.cc
+++ google_apis/gaia/oauth2_mint_token_flow.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/string_util.h"
 #include "base/stringprintf.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/values.h"
 #include "google_apis/gaia/gaia_urls.h"
@@ -46,6 +47,7 @@ static const char kIssueAdviceValueAuto[] = "auto";
 static const char kIssueAdviceValueConsent[] = "consent";
 static const char kAccessTokenKey[] = "token";
 static const char kConsentKey[] = "consent";
+static const char kExpiresInKey[] = "expiresIn";
 static const char kScopesKey[] = "scopes";
 static const char kDescriptionKey[] = "description";
 static const char kDetailKey[] = "detail";
@@ -103,9 +105,10 @@ OAuth2MintTokenFlow::OAuth2MintTokenFlow(URLRequestContextGetter* context,
 
 OAuth2MintTokenFlow::~OAuth2MintTokenFlow() { }
 
-void OAuth2MintTokenFlow::ReportSuccess(const std::string& access_token) {
+void OAuth2MintTokenFlow::ReportSuccess(const std::string& access_token,
+                                        int time_to_live) {
   if (delegate_)
-    delegate_->OnMintTokenSuccess(access_token);
+    delegate_->OnMintTokenSuccess(access_token, time_to_live);
 
   // |this| may already be deleted.
 }
@@ -175,8 +178,9 @@ void OAuth2MintTokenFlow::ProcessApiCallSuccess(
       ReportFailure(GoogleServiceAuthError::FromConnectionError(101));
   } else {
     std::string access_token;
-    if (ParseMintTokenResponse(dict, &access_token))
-      ReportSuccess(access_token);
+    int time_to_live;
+    if (ParseMintTokenResponse(dict, &access_token, &time_to_live))
+      ReportSuccess(access_token, time_to_live);
     else
       ReportFailure(GoogleServiceAuthError::FromConnectionError(101));
   }
@@ -201,10 +205,15 @@ void OAuth2MintTokenFlow::ProcessMintAccessTokenFailure(
 
 // static
 bool OAuth2MintTokenFlow::ParseMintTokenResponse(
-    const base::DictionaryValue* dict, std::string* access_token) {
+    const base::DictionaryValue* dict, std::string* access_token,
+    int* time_to_live) {
   CHECK(dict);
   CHECK(access_token);
-  return dict->GetString(kAccessTokenKey, access_token);
+  CHECK(time_to_live);
+  std::string ttl_string;
+  return dict->GetString(kExpiresInKey, &ttl_string) &&
+      base::StringToInt(ttl_string, time_to_live) &&
+      dict->GetString(kAccessTokenKey, access_token);
 }
 
 // static
