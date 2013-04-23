5f64a81a2d55fdc2f9d4a0f7a4c408ee59f0f79d
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 03:05:52 +0000

    [Spellcheck, IWYU] Remove unneeded include
    R=rouslan@chromium.org
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/13855018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195697 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
+++ chrome/browser/spellchecker/spellcheck_custom_dictionary.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/md5.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
-#include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/spellchecker/spellcheck_host_metrics.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/spellcheck_messages.h"
