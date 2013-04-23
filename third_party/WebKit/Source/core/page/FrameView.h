3aa4aba2c42ae4f7d7257997621e97e9d0c6ee1c
inferno@chromium.org <inferno@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-23 21:31:42 +0000

    UpdateWidget() can fire beforeload event synchronously blowing away RenderArena and its associated RenderObjects. In that case, we bail-out and clear m_widgetUpdateSet set.
    BUG=226696
    
    Review URL: https://codereview.chromium.org/14329005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148933 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FrameView.h
+++ third_party/WebKit/Source/core/page/FrameView.h
@@ -419,7 +419,7 @@ private:
     double adjustedDeferredRepaintDelay() const;
 
     bool updateWidgets();
-    void updateWidget(RenderObject*);
+    bool updateWidget(RenderObject*);
     void scrollToAnchor();
     void scrollPositionChanged();
 
