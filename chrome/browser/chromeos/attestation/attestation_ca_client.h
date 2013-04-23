ea660a5d904a50002ee5b1e03f33a1a0ce1fa841
dkrahn@google.com <dkrahn@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 23:16:36 +0000

    Implemented AttestationCAClient and unit tests.
    This class handles communication with the Chrome OS attestation CA.
    
    BUG=chromium:219959
    TEST=unit, manual
    
    Review URL: https://chromiumcodereview.appspot.com/13846006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195934 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/attestation/attestation_ca_client.h
+++ chrome/browser/chromeos/attestation/attestation_ca_client.h
@@ -6,18 +6,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_CHROMEOS_ATTESTATION_ATTESTATION_CA_CLIENT_H_
 #define CHROME_BROWSER_CHROMEOS_ATTESTATION_ATTESTATION_CA_CLIENT_H_
 
+#include <map>
 #include <string>
 
 #include "base/basictypes.h"
 
 #include "chromeos/attestation/attestation_flow.h"
+#include "net/url_request/url_fetcher_delegate.h"
 
 namespace chromeos {
 namespace attestation {
 
 // This class is a ServerProxy implementation for the Chrome OS attestation
 // flow.  It sends all requests to an Attestation CA via HTTPS.
-class AttestationCAClient : public ServerProxy {
+class AttestationCAClient : public ServerProxy,
+                            public net::URLFetcherDelegate {
  public:
   AttestationCAClient();
   virtual ~AttestationCAClient();
@@ -28,7 +31,21 @@ class AttestationCAClient : public ServerProxy {
   virtual void SendCertificateRequest(const std::string& request,
                                       const DataCallback& on_response) OVERRIDE;
 
+  // net::URLFetcherDelegate:
+  virtual void OnURLFetchComplete(const net::URLFetcher* source) OVERRIDE;
+
  private:
+  typedef std::map<const net::URLFetcher*, DataCallback> FetcherCallbackMap;
+
+  // POSTs |request| data to |url| and calls |on_response| with the response
+  // data when the fetch is complete.
+  void FetchURL(const std::string& url,
+                const std::string& request,
+                const DataCallback& on_response);
+
+  // Tracks all URL requests we have started.
+  FetcherCallbackMap pending_requests_;
+
   DISALLOW_COPY_AND_ASSIGN(AttestationCAClient);
 };
 
