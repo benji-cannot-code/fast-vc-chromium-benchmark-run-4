a5c5de8fe16c94cb3752893e78b4bedc5de7695f
cramya@chromium.org <cramya@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 17:13:09 +0000

    Remove adding qib command line switch from chrome_startup_flags and add from startup flow of embedding application.
    BUG=233237
    
    Review URL: https://chromiumcodereview.appspot.com/14236008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195820 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_startup_flags.cc
+++ chrome/browser/android/chrome_startup_flags.cc
@@ -36,9 +36,6 @@ void SetChromeSpecificCommandLineFlags() {
   // Turn on autologin.
   SetCommandLineSwitch(switches::kEnableAutologin);
 
-  // Turn on query extraction on omnibox searches.
-  SetCommandLineSwitch(switches::kEnableInstantExtendedAPI);
-
   // Enable prerender for the omnibox.
   SetCommandLineSwitchASCII(
       switches::kPrerenderMode, switches::kPrerenderModeSwitchValueEnabled);
