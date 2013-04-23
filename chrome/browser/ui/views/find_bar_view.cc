07bbe5df5acdd668eac2d20dbb11d82cad8570cf
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 10:57:51 +0000

    Returning true from HandleKeyEvent in FindBarView to let the UI toolkit know that we've handled the event.
    BUG=226456
    
    Review URL: https://codereview.chromium.org/14036013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195773 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/find_bar_view.cc
+++ chrome/browser/ui/views/find_bar_view.cc
@@ -460,6 +460,7 @@ bool FindBarView::HandleKeyEvent(views::Textfield* sender,
                                     !key_event.IsShiftDown(),
                                     false);  // Not case sensitive.
     }
+    return true;
   }
 
   return false;
