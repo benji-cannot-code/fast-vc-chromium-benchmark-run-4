benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit cbb181ca01f26d4003449633e92953f665e6afcb

Remove some chrome-specific methods from content::WebUI.
HideFavicon wasn't used anymore. This is probably because now we show a search icon there.
FocusLocationBarByDefault can just be implemented in Browser (like Instant).
HideURL can just be implemented in ToolbarModelImpl (again, like Instant).
Review URL: https://codereview.chromium.org/14080004

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@193998 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/favicon/favicon_tab_helper.cc
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
-#include "content/public/browser/web_ui.h"
 #include "ui/gfx/codec/png_codec.h"
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_skia.h"
@@ -96,9 +95,6 @@ bool FaviconTabHelper::ShouldDisplayFavicon() {
   if (chrome::IsInstantNTP(web_contents()))
     return false;
 
-  content::WebUI* web_ui = web_contents()->GetWebUIForCurrentState();
-  if (web_ui)
-    return !web_ui->ShouldHideFavicon();
   return true;
 }
 

chrome/browser/extensions/extension_web_ui.cc
@@ -132,9 +132,6 @@ ExtensionWebUI::ExtensionWebUI(content::WebUI* web_ui, const GURL& url)
   const Extension* extension =
       service->extensions()->GetExtensionOrAppByURL(ExtensionURLInfo(url));
   DCHECK(extension);
-  // Only hide the url for internal pages (e.g. chrome-extension or packaged
-  // component apps like bookmark manager.
-  bool should_hide_url = !extension->is_hosted_app();
 
   // The base class defaults to enabling WebUI bindings, but we don't need
   // those (this is also reflected in ChromeWebUIControllerFactory::
@@ -145,20 +142,6 @@ ExtensionWebUI::ExtensionWebUI(content::WebUI* web_ui, const GURL& url)
   const CommandLine& browser_command_line = *CommandLine::ForCurrentProcess();
   if (browser_command_line.HasSwitch(switches::kChromeFrame))
     bindings |= content::BINDINGS_POLICY_EXTERNAL_HOST;
-  // For chrome:// overrides, some of the defaults are a little different.
-  GURL effective_url = web_ui->GetWebContents()->GetURL();
-  if (effective_url.SchemeIs(chrome::kChromeUIScheme)) {
-    if (effective_url.host() == chrome::kChromeUINewTabHost) {
-      web_ui->FocusLocationBarByDefault();
-    } else {
-      // Current behavior of other chrome:// pages is to display the URL.
-      should_hide_url = false;
-    }
-  }
-
-  if (should_hide_url)
-    web_ui->HideURL();
-
   web_ui->SetBindings(bindings);
 
   // Hack: A few things we specialize just for the bookmark manager.

chrome/browser/ui/browser.cc
@@ -150,6 +150,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/profiling.h"
 #include "chrome/common/search_types.h"
 #include "chrome/common/startup_metric_utils.h"
+#include "chrome/common/url_constants.h"
 #include "chrome/common/web_apps.h"
 #include "content/public/browser/color_chooser.h"
 #include "content/public/browser/devtools_manager.h"
@@ -1401,6 +1402,12 @@ void Browser::BeforeUnloadFired(WebContents* web_contents,
 bool Browser::ShouldFocusLocationBarByDefault(WebContents* source) {
   const content::NavigationEntry* entry =
       source->GetController().GetActiveEntry();
+  if (entry &&
+      entry->GetURL().SchemeIs(chrome::kChromeUIScheme) &&
+      entry->GetURL().host() == chrome::kChromeUINewTabHost) {
+    return true;
+  }
+
   return chrome::NavEntryIsInstantNTP(source, entry);
 }
 

chrome/browser/ui/browser_focus_uitest.cc
@@ -953,4 +953,43 @@ IN_PROC_BROWSER_TEST_F(BrowserFocusTest,
   EXPECT_FALSE(ui_test_utils::IsViewFocused(browser(), VIEW_ID_OMNIBOX));
 }
 
+IN_PROC_BROWSER_TEST_F(BrowserFocusTest, FocusOnNavigate) {
+  // Needed on Mac.
+  ASSERT_TRUE(ui_test_utils::BringBrowserWindowToFront(browser()));
+  // Load the NTP.
+  ui_test_utils::NavigateToURL(browser(), GURL(chrome::kChromeUINewTabURL));
+  EXPECT_TRUE(IsViewFocused(VIEW_ID_OMNIBOX));
+
+  // Navigate to another page.
+  const base::FilePath::CharType* kEmptyFile = FILE_PATH_LITERAL("empty.html");
+  GURL file_url(ui_test_utils::GetTestUrl(base::FilePath(
+      base::FilePath::kCurrentDirectory), base::FilePath(kEmptyFile)));
+  ui_test_utils::NavigateToURL(browser(), file_url);
+
+  ClickOnView(VIEW_ID_TAB_CONTAINER);
+
+  // Navigate back.  Should focus the location bar.
+  {
+    content::WindowedNotificationObserver back_nav_observer(
+        content::NOTIFICATION_NAV_ENTRY_COMMITTED,
+        content::NotificationService::AllSources());
+    chrome::GoBack(browser(), CURRENT_TAB);
+    back_nav_observer.Wait();
+  }
+
+  EXPECT_TRUE(IsViewFocused(VIEW_ID_OMNIBOX));
+
+  // Navigate forward.  Shouldn't focus the location bar.
+  ClickOnView(VIEW_ID_TAB_CONTAINER);
+  {
+    content::WindowedNotificationObserver forward_nav_observer(
+        content::NOTIFICATION_NAV_ENTRY_COMMITTED,
+        content::NotificationService::AllSources());
+    chrome::GoForward(browser(), CURRENT_TAB);
+    forward_nav_observer.Wait();
+  }
+
+  EXPECT_FALSE(IsViewFocused(VIEW_ID_OMNIBOX));
+}
+
 }  // namespace

chrome/browser/ui/toolbar/toolbar_model_impl.cc
@@ -150,9 +150,9 @@ bool ToolbarModelImpl::ShouldDisplayURL() const {
   // - The WebUI test must come before the extension scheme test because there
   //   can be WebUIs that have extension schemes (e.g. the bookmark manager). In
   //   that case, we should prefer what the WebUI instance says.
-  // - The view-source test must come before the WebUI test because of the case
+  // - The view-source test must come before the NTP test because of the case
   //   of view-source:chrome://newtab, which should display its URL despite what
-  //   chrome://newtab's WebUI says.
+  //   chrome://newtab says.
   NavigationController* controller = GetNavigationController();
   NavigationEntry* entry = controller ? controller->GetVisibleEntry() : NULL;
   if (entry) {
@@ -160,16 +160,21 @@ bool ToolbarModelImpl::ShouldDisplayURL() const {
         entry->GetPageType() == content::PAGE_TYPE_INTERSTITIAL) {
       return true;
     }
+
+    GURL url = entry->GetURL();
+    GURL virtual_url = entry->GetVirtualURL();
+    if (url.SchemeIs(chrome::kChromeUIScheme) ||
+        virtual_url.SchemeIs(chrome::kChromeUIScheme)) {
+      if (!url.SchemeIs(chrome::kChromeUIScheme))
+        url = virtual_url;
+      return url.host() != chrome::kChromeUINewTabHost;
+    }
+
+    if (url.SchemeIs(extensions::kExtensionScheme))
+      return false;
   }
 
-  WebContents* web_contents = delegate_->GetActiveWebContents();
-  if (web_contents && web_contents->GetWebUIForCurrentState())
-    return !web_contents->GetWebUIForCurrentState()->ShouldHideURL();
-
-  if (entry && entry->GetURL().SchemeIs(extensions::kExtensionScheme))
-    return false;
-
-  if (chrome::IsInstantNTP(web_contents))
+  if (chrome::IsInstantNTP(delegate_->GetActiveWebContents()))
     return false;
 
   return true;

chrome/browser/ui/webui/extensions/extension_activity_ui.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 ExtensionActivityUI::ExtensionActivityUI(content::WebUI* web_ui)
     : WebUIController(web_ui), extension_(NULL) {
-  web_ui->HideURL();
   web_ui->OverrideTitle(l10n_util::GetStringUTF16(
       IDS_EXTENSION_ACTIVITY_TITLE));
 

chrome/browser/ui/webui/ntp/new_tab_ui.cc
@@ -79,11 +79,6 @@ NewTabUI::NewTabUI(content::WebUI* web_ui)
     : WebUIController(web_ui),
       showing_sync_bubble_(false) {
   g_live_new_tabs.Pointer()->insert(this);
-  // Override some options on the Web UI.
-  web_ui->HideFavicon();
-
-  web_ui->FocusLocationBarByDefault();
-  web_ui->HideURL();
   web_ui->OverrideTitle(l10n_util::GetStringUTF16(IDS_NEW_TAB_TITLE));
 
   // We count all link clicks as AUTO_BOOKMARK, so that site can be ranked more

chrome/browser/ui/webui/sync_setup_handler_unittest.cc
@@ -246,18 +246,6 @@ class TestWebUI : public content::WebUI {
   virtual ui::ScaleFactor GetDeviceScaleFactor() const OVERRIDE {
     return ui::SCALE_FACTOR_100P;
   }
-  virtual bool ShouldHideFavicon() const OVERRIDE {
-    return false;
-  }
-  virtual void HideFavicon() OVERRIDE {}
-  virtual bool ShouldFocusLocationBarByDefault() const OVERRIDE {
-    return false;
-  }
-  virtual void FocusLocationBarByDefault() OVERRIDE {}
-  virtual bool ShouldHideURL() const OVERRIDE {
-    return false;
-  }
-  virtual void HideURL() OVERRIDE {}
   virtual const string16& GetOverriddenTitle() const OVERRIDE {
     return temp_string_;
   }

commit 3cedbf6602c218bc476e7a9123a5f48d1b865d8b

Don't show the favicon for NTP. This was a regression in r193998.
BUG=233870

Review URL: https://codereview.chromium.org/14146009

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195848 0039d316-1c4b-4281-b951-d872f2087c98

chrome/browser/favicon/favicon_tab_helper.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search/search.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_notification_types.h"
+#include "chrome/common/url_constants.h"
 #include "content/public/browser/favicon_status.h"
 #include "content/public/browser/invalidate_type.h"
 #include "content/public/browser/navigation_controller.h"
@@ -90,6 +91,12 @@ bool FaviconTabHelper::ShouldDisplayFavicon() {
   const NavigationController& controller = web_contents()->GetController();
   if (controller.GetLastCommittedEntry() && controller.GetPendingEntry())
     return true;
+
+  GURL url = web_contents()->GetURL();
+  if (url.SchemeIs(chrome::kChromeUIScheme) &&
+      url.host() == chrome::kChromeUINewTabHost) {
+    return false;
+  }
 
   // No favicon on Instant New Tab Pages.
   if (chrome::IsInstantNTP(web_contents()))