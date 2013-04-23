cf9a40d70249fdef238321587ab05530d0b7ceef
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 07:46:39 +0000

    Omnibox: Call Stop() on all Providers After Certain Amount of Time
    This is to prevent belated omnibox suggestions from disrupting
    the user's experience.
    
    This changelist enables this behavior but controls it by a field
    trial.  The field trial currently is not created; no users are in.
    I will use the server-side variations server to turn it on and control
    the size of the experiment population.
    
    Tested by increasing the timer within search provider and then
    using --force-fieldtrials to switch myself in and out of the
    experiment group and verifying that search provider suggestions
    were or were not displayed depending on how late they appeared.
    
    BUG=165252
    
    Review URL: https://chromiumcodereview.appspot.com/14366009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195756 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/omnibox/omnibox_field_trial.cc
+++ chrome/browser/omnibox/omnibox_field_trial.cc
@@ -25,6 +25,7 @@ const char kHUPCreateShorterMatchFieldTrialName[] =
     "OmniboxHUPCreateShorterMatch";
 const char kHQPReplaceHUPScoringFieldTrialName[] =
     "OmniboxHQPReplaceHUPProhibitTrumpingInlineableResult";
+const char kStopTimerFieldTrialName[] = "OmniboxStopTimer";
 
 // The autocomplete dynamic field trial name prefix.  Each field trial is
 // configured dynamically and is retrieved automatically by Chrome during
@@ -74,6 +75,9 @@ const base::FieldTrial::Probability
 const base::FieldTrial::Probability
     kHQPReplaceHUPScoringFieldTrialExperimentFraction = 0;
 
+// Experiment group names.
+
+const char kStopTimerExperimentGroupName[] = "UseStopTimer";
 
 // Field trial IDs.
 // Though they are not literally "const", they are set only once, in
@@ -288,4 +292,9 @@ bool OmniboxFieldTrial::InHQPReplaceHUPScoringFieldTrialExperimentGroup() {
   const int group = base::FieldTrialList::FindValue(
       kHQPReplaceHUPScoringFieldTrialName);
   return group == hqp_replace_hup_scoring_experiment_group;
+}
+
+bool OmniboxFieldTrial::InStopTimerFieldTrialExperimentGroup() {
+  return (base::FieldTrialList::FindFullName(kStopTimerFieldTrialName) ==
+          kStopTimerExperimentGroupName);
 }
