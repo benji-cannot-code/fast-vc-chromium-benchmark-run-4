fdc575174a791f184c101fa4b6d8f481a2711ce2
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 18:30:03 +0000

    Implement IAccessibleApplication
    This will provide a stable way for assistive technology
    to identify Chrome and Chrome-based browsers, rather than
    depending on the window class name as they did before.
    
    BUG=175156
    
    Review URL: https://chromiumcodereview.appspot.com/14268005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195851 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/accessibility/browser_accessibility_win.cc
+++ content/browser/accessibility/browser_accessibility_win.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "content/browser/accessibility/browser_accessibility_manager_win.h"
 #include "content/common/accessibility_messages.h"
+#include "content/public/common/content_client.h"
 #include "ui/base/accessibility/accessible_text_utils.h"
 #include "ui/base/win/accessibility_ids_win.h"
 #include "ui/base/win/accessibility_misc_utils.h"
@@ -850,6 +851,41 @@ STDMETHODIMP BrowserAccessibilityWin::get_groupPosition(
   }
 
   return E_NOTIMPL;
+}
+
+//
+// IAccessibleApplication methods.
+//
+
+STDMETHODIMP BrowserAccessibilityWin::get_appName(BSTR* app_name) {
+  std::string product_name = GetContentClient()->GetProduct();
+  *app_name = SysAllocString(UTF8ToUTF16(product_name).c_str());
+  DCHECK(*app_name);
+  return *app_name ? S_OK : E_FAIL;
+}
+
+STDMETHODIMP BrowserAccessibilityWin::get_appVersion(BSTR* app_version) {
+  std::string user_agent = GetContentClient()->GetUserAgent();
+  *app_version = SysAllocString(UTF8ToUTF16(user_agent).c_str());
+  DCHECK(*app_version);
+  return *app_version ? S_OK : E_FAIL;
+}
+
+STDMETHODIMP BrowserAccessibilityWin::get_toolkitName(BSTR* toolkit_name) {
+  // This is hard-coded; all products based on the Chromium engine
+  // will have the same toolkit name, so that assistive technology can
+  // detect any Chrome-based product.
+  *toolkit_name = SysAllocString(L"Chrome");
+  DCHECK(*toolkit_name);
+  return *toolkit_name ? S_OK : E_FAIL;
+}
+
+STDMETHODIMP BrowserAccessibilityWin::get_toolkitVersion(
+    BSTR* toolkit_version) {
+  std::string user_agent = GetContentClient()->GetUserAgent();
+  *toolkit_version = SysAllocString(UTF8ToUTF16(user_agent).c_str());
+  DCHECK(*toolkit_version);
+  return *toolkit_version ? S_OK : E_FAIL;
 }
 
 //
@@ -2532,6 +2568,7 @@ STDMETHODIMP BrowserAccessibilityWin::QueryService(REFGUID guidService,
   if (guidService == IID_IAccessible ||
       guidService == IID_IAccessible2 ||
       guidService == IID_IAccessibleAction ||
+      guidService == IID_IAccessibleApplication ||
       guidService == IID_IAccessibleHyperlink ||
       guidService == IID_IAccessibleHypertext ||
       guidService == IID_IAccessibleImage ||
