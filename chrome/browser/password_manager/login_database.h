41df4b986a596f1c0a0cefba99ece016ac235460
gcasto@chromium.org <gcasto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-23 04:55:09 +0000

    Update LoginDatabase to handle possible_usernames and password type (generated or not).
    BUG=188908
    
    Review URL: https://chromiumcodereview.appspot.com/13847002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195705 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/login_database.h
+++ chrome/browser/password_manager/login_database.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
+#include "base/pickle.h"
 #include "base/string16.h"
 #include "content/public/common/password_form.h"
 #include "sql/connection.h"
@@ -77,6 +78,8 @@ class LoginDatabase {
   bool DeleteAndRecreateDatabaseFile();
 
  private:
+  friend class LoginDatabaseTest;
+
   // Returns an encrypted version of plain_text.
   std::string EncryptedString(const string16& plain_text) const;
 
@@ -84,7 +87,7 @@ class LoginDatabase {
   string16 DecryptedString(const std::string& cipher_text) const;
 
   bool InitLoginsTable();
-  void MigrateOldVersionsAsNeeded();
+  bool MigrateOldVersionsAsNeeded();
 
   // Fills |form| from the values in the given statement (which is assumed to
   // be of the form used by the Get*Logins methods).
@@ -95,6 +98,10 @@ class LoginDatabase {
   // |forms|.
   bool GetAllLoginsWithBlacklistSetting(
       bool blacklisted, std::vector<content::PasswordForm*>* forms) const;
+
+  // Serialization routines for vectors.
+  Pickle SerializeVector(const std::vector<string16>& vec) const;
+  std::vector<string16> DeserializeVector(const Pickle& pickle) const;
 
   base::FilePath db_path_;
   mutable sql::Connection db_;
