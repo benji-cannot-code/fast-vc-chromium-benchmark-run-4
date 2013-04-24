06068b94fdc6770e78b3543dc9be225fcf3d2a52
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 00:22:34 +0000

    Cleanup various bits of BrowserThreadImpl and PluginServiceImpl.
    Review URL: https://chromiumcodereview.appspot.com/13896004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195957 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/plugin_service_impl.cc
+++ content/browser/plugin_service_impl.cc
@@ -17,13 +17,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/waitable_event.h"
 #include "base/threading/thread.h"
 #include "base/utf_string_conversions.h"
-#include "base/values.h"
 #include "content/browser/ppapi_plugin_process_host.h"
 #include "content/browser/renderer_host/render_process_host_impl.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/common/pepper_plugin_registry.h"
-#include "content/common/plugin_messages.h"
-#include "content/common/utility_messages.h"
 #include "content/common/view_messages.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/content_browser_client.h"
@@ -744,10 +741,7 @@ bool PluginServiceImpl::IsPluginUnstable(const base::FilePath& path) {
     return false;
   }
   base::TimeDelta delta = base::Time::Now() - i->second[0];
-  if (delta.InSeconds() <= kCrashesInterval) {
-    return true;
-  }
-  return false;
+  return delta.InSeconds() <= kCrashesInterval;
 }
 
 void PluginServiceImpl::RefreshPlugins() {
