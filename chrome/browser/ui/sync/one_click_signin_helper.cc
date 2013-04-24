dad21d353b53f7bd30f431ff7992c0d5ab4b0bec
bcwhite@chromium.org <bcwhite@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 00:02:50 +0000

    Only delete history item during redirect from sign-in landing page.
    BUG=234326
    
    Review URL: https://chromiumcodereview.appspot.com/14159014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195949 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/one_click_signin_helper.cc
+++ chrome/browser/ui/sync/one_click_signin_helper.cc
@@ -430,7 +430,7 @@ void CurrentHistoryCleaner::DidStopLoading(
     content::RenderViewHost* render_view_host) {
   content::NavigationController& nc = web_contents()->GetController();
   // Have to wait until something else gets added to history before removal.
-  if (history_index_to_remove_ != nc.GetLastCommittedEntryIndex()) {
+  if (history_index_to_remove_ < nc.GetLastCommittedEntryIndex()) {
     nc.RemoveEntryAtIndex(history_index_to_remove_);
     Observe(NULL);
     delete this;  /* success */
@@ -1043,7 +1043,6 @@ void OneClickSigninHelper::RedirectToNtpOrAppsPage(bool show_bubble) {
                                 CURRENT_TAB,
                                 content::PAGE_TRANSITION_AUTO_TOPLEVEL,
                                 false);
-  RemoveCurrentHistoryItem(contents);
   contents->OpenURL(params);
 
   error_message_.clear();
@@ -1154,6 +1153,7 @@ void OneClickSigninHelper::DidStopLoading(
   // explicit sign ins.
   if (!error_message_.empty() && auto_accept_ == AUTO_ACCEPT_EXPLICIT) {
     VLOG(1) << "OneClickSigninHelper::DidStopLoading: error=" << error_message_;
+    RemoveCurrentHistoryItem(contents);
     RedirectToNtpOrAppsPage(true);
     return;
   }
@@ -1188,6 +1188,7 @@ void OneClickSigninHelper::DidStopLoading(
     std::string unused_value;
     if (net::GetValueForKeyInQuery(url, "ntp", &unused_value)) {
       SyncPromoUI::SetUserSkippedSyncPromo(profile);
+      RemoveCurrentHistoryItem(contents);
       RedirectToNtpOrAppsPage(false);
     }
 
@@ -1365,6 +1366,7 @@ void OneClickSigninHelper::DidStopLoading(
       if (source_ != SyncPromoUI::SOURCE_SETTINGS &&
           source_ != SyncPromoUI::SOURCE_WEBSTORE_INSTALL) {
         signin_tracker_.reset(new SigninTracker(profile, this));
+        RemoveCurrentHistoryItem(contents);
         RedirectToNtpOrAppsPage(false);
       }
       break;
