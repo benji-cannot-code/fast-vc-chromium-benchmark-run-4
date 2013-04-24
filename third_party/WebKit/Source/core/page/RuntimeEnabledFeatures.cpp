6e5c39aa8f71c20596d31f5065372f3007f59f68
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-24 05:35:38 +0000

    Make runtime shadow dom flag true in default.
    This is a pre-requirement of chromium side patch, https://codereview.chromium.org/14167011/.
    We can avoid two-sided patches by this change.
    
    After this change, we can land https://codereview.chromium.org/14167011/ in chromium side.
    Then, we can land https://codereview.chromium.org/14096021/ in blink side.
    
    BUG=230744
    
    Review URL: https://chromiumcodereview.appspot.com/14461002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148970 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/RuntimeEnabledFeatures.cpp
+++ third_party/WebKit/Source/core/page/RuntimeEnabledFeatures.cpp
@@ -159,7 +159,7 @@ bool RuntimeEnabledFeatures::isVideoTrackEnabled = true;
 bool RuntimeEnabledFeatures::isEncryptedMediaEnabled = false;
 #endif
 
-bool RuntimeEnabledFeatures::isShadowDOMEnabled = false;
+bool RuntimeEnabledFeatures::isShadowDOMEnabled = true;
 
 bool RuntimeEnabledFeatures::isExperimentalShadowDOMEnabled = false;
 
