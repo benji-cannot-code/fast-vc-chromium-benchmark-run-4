3dc14d990c62985ecc5aacad6883661a46d13c24
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 10:30:29 +0000

    Change data source of chrome:drive-internals to drive::JobListInterface.
    - Added some utility function to job_list_interface.h
    (and renamed to job_list.{h,cc} since now it has some implementation.)
    - Switches the data source of drive-internals to job_list()->GetJobInfoList()
    - Along the way, fix JavaScript bug that it didn't properly cleared previous list of events.
    
    BUG=234899
    TEST=Open chrome:drive-internals and check "in-flight operations" section is
    properly shown.
    TBR=thestig@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/13877022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/drive_scheduler.h
+++ chrome/browser/chromeos/drive/drive_scheduler.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "chrome/browser/chromeos/drive/drive_file_system_interface.h"
-#include "chrome/browser/chromeos/drive/job_list_interface.h"
+#include "chrome/browser/chromeos/drive/job_list.h"
 #include "chrome/browser/google_apis/drive_service_interface.h"
 #include "chrome/browser/google_apis/drive_uploader.h"
 #include "net/base/network_change_notifier.h"
--- chrome/browser/chromeos/drive/job_list.h
+++ chrome/browser/chromeos/drive/job_list.h
@@ -3,8 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_CHROMEOS_DRIVE_JOB_LIST_INTERFACE_H_
-#define CHROME_BROWSER_CHROMEOS_DRIVE_JOB_LIST_INTERFACE_H_
+#ifndef CHROME_BROWSER_CHROMEOS_DRIVE_JOB_LIST_H_
+#define CHROME_BROWSER_CHROMEOS_DRIVE_JOB_LIST_H_
+
+#include <string>
 
 #include "base/basictypes.h"
 #include "base/files/file_path.h"
@@ -34,6 +36,9 @@ enum JobType {
   TYPE_UPLOAD_EXISTING_FILE,
 };
 
+// Returns the string representation of |type|.
+std::string JobTypeToString(JobType type);
+
 // Current state of the job.
 enum JobState {
   // The job is queued, but not yet executed.
@@ -45,6 +50,9 @@ enum JobState {
   // The job failed, but has been re-added to the queue.
   STATE_RETRY,
 };
+
+// Returns the string representation of |state|.
+std::string JobStateToString(JobState state);
 
 // Unique ID assigned to each job.
 typedef int32 JobID;
@@ -124,4 +132,4 @@ class JobListInterface {
 
 }  // namespace drive
 
-#endif  // CHROME_BROWSER_CHROMEOS_DRIVE_JOB_LIST_INTERFACE_H_
+#endif  // CHROME_BROWSER_CHROMEOS_DRIVE_JOB_LIST_H_
