65ec6cc568de50d490ef6905530d0a96833a5c28
rltoscano@chromium.org <rltoscano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 21:11:47 +0000

    Refactors duplex ticket item out of ticket store.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14330020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196236 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/native_layer.js
+++ chrome/browser/resources/print_preview/native_layer.js
@@ -172,7 +172,7 @@ cr.define('print_preview', function() {
 
         // NOTE: Even though the following fields don't directly relate to the
         // preview, they still need to be included.
-        'duplex': printTicketStore.isDuplexEnabled() ?
+        'duplex': printTicketStore.duplex.getValue() ?
             NativeLayer.DuplexMode.LONG_EDGE : NativeLayer.DuplexMode.SIMPLEX,
         'copies': printTicketStore.copies.getValueAsNumber(),
         'collate': printTicketStore.collate.getValue(),
@@ -230,7 +230,7 @@ cr.define('print_preview', function() {
         'headerFooterEnabled': printTicketStore.isHeaderFooterEnabled(),
         'marginsType': printTicketStore.getMarginsType(),
         'generateDraftData': true, // TODO(rltoscano): What should this be?
-        'duplex': printTicketStore.isDuplexEnabled() ?
+        'duplex': printTicketStore.duplex.getValue() ?
             NativeLayer.DuplexMode.LONG_EDGE : NativeLayer.DuplexMode.SIMPLEX,
         'copies': printTicketStore.copies.getValueAsNumber(),
         'collate': printTicketStore.collate.getValue(),
--- chrome/browser/resources/print_preview/print_header.js
+++ chrome/browser/resources/print_preview/print_header.js
@@ -149,7 +149,7 @@ cr.define('print_preview', function() {
 
       var numPages = this.printTicketStore_.getPageNumberSet().size;
       var numSheets = numPages;
-      if (!saveToPdf && this.printTicketStore_.isDuplexEnabled()) {
+      if (!saveToPdf && this.printTicketStore_.duplex.getValue()) {
         numSheets = Math.ceil(numPages / 2);
       }
 
