2f60b6dbc729bcc9f29372944d3a358117156d6f
motek@chromium.org <motek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 02:12:18 +0000

    Added missing include.
    BUG=234496
    
    Review URL: https://chromiumcodereview.appspot.com/14081023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195997 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/thumbnails/content_analysis_unittest.cc
+++ chrome/browser/thumbnails/content_analysis_unittest.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cmath>
 #include <cstdlib>
+#include <functional>
 #include <limits>
 #include <numeric>
 #include <vector>
