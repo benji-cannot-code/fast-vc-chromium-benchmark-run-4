002312c5553b2bafb21560e2539684cafa254173
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 07:41:45 +0000

    Supports MessageCenterObserver interface in MessageCenterView.
    Now it receives the add/remove/update events by itself. This is a
    preparation CL for repositioning on the message center, but this itself
    also allows partial-update in the center.
    
    BUG=224089
    TEST=no break on existing tests
    
    Review URL: https://chromiumcodereview.appspot.com/14358013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196080 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_center_bubble.cc
+++ ui/message_center/views/message_center_bubble.cc
@@ -29,13 +29,13 @@ class ContentsView : public views::View {
   virtual void ChildPreferredSizeChanged(View* child) OVERRIDE;
 
  private:
-  MessageCenterBubble* bubble_;  // Weak reference.
+  base::WeakPtr<MessageCenterBubble> bubble_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentsView);
 };
 
 ContentsView::ContentsView(MessageCenterBubble* bubble, views::View* contents)
-    : bubble_(bubble) {
+    : bubble_(bubble->AsWeakPtr()) {
   SetLayoutManager(new views::FillLayout());
   AddChildView(contents);
 }
@@ -52,7 +52,8 @@ int ContentsView::GetHeightForWidth(int width) {
 
 void ContentsView::ChildPreferredSizeChanged(View* child) {
   // TODO(dharcourt): Reduce the amount of updating this requires.
-  bubble_->bubble_view()->UpdateBubble();
+  if (bubble_.get())
+    bubble_->bubble_view()->UpdateBubble();
 }
 
 // MessageCenterBubble /////////////////////////////////////////////////////////
--- ui/message_center/views/message_center_bubble.h
+++ ui/message_center/views/message_center_bubble.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_MESSAGE_CENTER_VIEWS_MESSAGE_CENTER_BUBBLE_H_
 #define UI_MESSAGE_CENTER_VIEWS_MESSAGE_CENTER_BUBBLE_H_
 
+#include "base/memory/weak_ptr.h"
 #include "ui/message_center/message_center_export.h"
 #include "ui/message_center/views/message_bubble_base.h"
 
@@ -14,7 +15,9 @@ namespace message_center {
 class MessageCenterView;
 
 // Bubble for message center.
-class MESSAGE_CENTER_EXPORT MessageCenterBubble : public MessageBubbleBase {
+class MESSAGE_CENTER_EXPORT MessageCenterBubble
+    : public MessageBubbleBase,
+      public base::SupportsWeakPtr<MessageCenterBubble> {
  public:
   explicit MessageCenterBubble(MessageCenter* message_center);
 
