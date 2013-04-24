cc7d1e7c3caa5c85c781313f41cced9ee87d3271
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-24 12:24:24 +0000

    DevTools: implement DefaultTextEditor.isClean/markClean methods
    Implement DefaultTextEditor.isClean/markClean methods which rely
    on undo/redo stack state.
    
    BUG=
    
    Review URL: https://codereview.chromium.org/13892010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@148995 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TextEditorModel.js
+++ third_party/WebKit/Source/devtools/front_end/TextEditorModel.js
@@ -203,12 +203,14 @@ WebInspector.TextEditorModel.prototype = {
     /**
      * @return {boolean}
      */
-    isClean: function() {
-        return !this._undoStack.length;
+    isClean: function()
+    {
+        return this._cleanState === this._undoStack.length;
     },
 
-    markClean: function() {
-        this._resetUndoStack();
+    markClean: function()
+    {
+        this._cleanState = this._undoStack.length;
     },
 
     /**
@@ -546,8 +548,11 @@ WebInspector.TextEditorModel.prototype = {
         if (this._inUndo)
             this._redoStack.push(command);
         else {
-            if (!this._inRedo)
+            if (!this._inRedo) {
                 this._redoStack = [];
+                if (typeof this._cleanState === "number" && this._cleanState > this._undoStack.length)
+                    delete this._cleanState;
+            }
             this._undoStack.push(command);
         }
         return command;
@@ -618,7 +623,9 @@ WebInspector.TextEditorModel.prototype = {
 
     _resetUndoStack: function()
     {
+        delete this._cleanState;
         this._undoStack = [];
+        this._redoStack = [];
     },
 
     /**
