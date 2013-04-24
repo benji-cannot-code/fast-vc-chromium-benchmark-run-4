fc1ad5891d0a355b2add110f1097f07271e89975
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 14:37:22 +0000

    Docserver: disable samples when running via start_dev_server.py. The dev server doesn't support async fetch so it takes forever (about 5 minutes).
    BUG=167432
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/14404014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196140 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/server2/empty_dir_file_system.py
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from file_system import FileNotFoundError, FileSystem, StatInfo
+from future import Future
+
+class EmptyDirFileSystem(FileSystem):
+  '''A FileSystem with empty directories. Useful to inject places to disable
+  features such as samples.
+  '''
+  def Read(self, paths, binary=False):
+    result = {}
+    for path in paths:
+      if not path.endswith('/'):
+        raise FileNotFoundError('EmptyDirFileSystem cannot read %s' % path)
+      result[path] = []
+    return Future(value=result)
+
+  def Stat(self, path):
+    if not path.endswith('/'):
+      raise FileNotFoundError('EmptyDirFileSystem cannot stat %s' % path)
+    return StatInfo(0, child_versions=[])
