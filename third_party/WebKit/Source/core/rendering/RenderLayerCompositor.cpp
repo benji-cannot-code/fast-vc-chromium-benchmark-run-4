ed1c720a7542594ee849e27ce2105aba056f6d4b
ajuma@chromium.org <ajuma@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-24 23:04:38 +0000

    Measure effect of promoting RenderLayers with a webkit-transition on opacity or transform
    This gathers UMA stats to measure the effect on memory usage of promoting all
    RenderLayers that have a webkit-transition on opacity or transform. By
    promoting such layers ahead of time (rather than only promoting at the start
    of an animation), we would avoid jank at the start of an animation (caused by
    painting the promoted layer). Before making such a change, however, we need
    to better understand the magnitude of the impact it will have on memory usage.
    The stats gathered here will lower-bound that impact (since, e.g., promoting
    such layers may cause other layers to also be promoted, but we don't count
    the cost of these other layers).
    
    R=jamesr
    
    BUG=178119
    
    Review URL: https://chromiumcodereview.appspot.com/13620006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerCompositor.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "HTMLCanvasElement.h"
 #include "HTMLIFrameElement.h"
 #include "HTMLNames.h"
+#include "HistogramSupport.h"
 #include "HitTestResult.h"
 #include "InspectorInstrumentation.h"
 #include "Logging.h"
@@ -978,7 +979,17 @@ void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, Vect
     // Make the layer compositing if necessary, and set up clipping and content layers.
     // Note that we can only do work here that is independent of whether the descendant layers
     // have been processed. computeCompositingRequirements() will already have done the repaint if necessary.
-    
+
+    // Used for gathering UMA data about the effect on memory usage of promoting all layers
+    // that have a webkit-transition on opacity or transform.
+    static double pixelsWithoutPromotingAllTransitions = 0.0;
+    static double pixelsAddedByPromotingAllTransitions = 0.0;
+
+    if (!depth) {
+        pixelsWithoutPromotingAllTransitions = 0.0;
+        pixelsAddedByPromotingAllTransitions = 0.0;
+    }
+
     RenderLayerBacking* layerBacking = layer->backing();
     if (layerBacking) {
         // The compositing state of all our children has been updated already, so now
@@ -1005,6 +1016,12 @@ void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, Vect
 #endif
         if (layerBacking->hasUnpositionedOverflowControlsLayers())
             layer->positionNewlyCreatedOverflowControls();
+
+        pixelsWithoutPromotingAllTransitions += layer->size().height() * layer->size().width();
+    } else {
+        if (layer->renderer()->style()->transitionForProperty(CSSPropertyOpacity) ||
+            layer->renderer()->style()->transitionForProperty(CSSPropertyWebkitTransform))
+            pixelsAddedByPromotingAllTransitions += layer->size().height() * layer->size().width();
     }
 
     // If this layer has backing, then we are collecting its children, otherwise appending
@@ -1076,6 +1093,11 @@ void RenderLayerCompositor::rebuildCompositingLayerTree(RenderLayer* layer, Vect
         }
 
         childLayersOfEnclosingLayer.append(layerBacking->childForSuperlayers());
+    }
+
+    if (!depth) {
+        int percentageIncreaseInPixels = static_cast<int>(pixelsAddedByPromotingAllTransitions / pixelsWithoutPromotingAllTransitions * 100);
+        HistogramSupport::histogramCustomCounts("Renderer.PixelIncreaseFromTransitions", percentageIncreaseInPixels, 0, 1000, 50);
     }
 }
 
