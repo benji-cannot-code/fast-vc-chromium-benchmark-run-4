39f7b21c237be155631abb800ea8cab5bda3e9af
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 19:17:14 +0000

    Fix leak in PowerPolicyControllerTest::SetUp
    BUG=235070
    TBR=haruki@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/14311010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196195 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/power_policy_controller_unittest.cc
+++ chromeos/dbus/power_policy_controller_unittest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/dbus/power_policy_controller.h"
 
+#include "base/memory/scoped_ptr.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/fake_power_manager_client.h"
 #include "chromeos/dbus/mock_dbus_thread_manager_without_gmock.h"
@@ -25,18 +26,19 @@ class PowerPolicyControllerTest : public testing::Test {
     dbus_manager_ = new MockDBusThreadManagerWithoutGMock;
     DBusThreadManager::InitializeForTesting(dbus_manager_);  // Takes ownership.
 
-    policy_controller_ =
-        new PowerPolicyController(dbus_manager_, &fake_power_client_);
+    policy_controller_.reset(
+        new PowerPolicyController(dbus_manager_, &fake_power_client_));
   }
 
   virtual void TearDown() OVERRIDE {
+    policy_controller_.reset();
     DBusThreadManager::Shutdown();
   }
 
  protected:
   MockDBusThreadManagerWithoutGMock* dbus_manager_;  // Not owned.
   FakePowerManagerClient fake_power_client_;
-  PowerPolicyController* policy_controller_;  // Not owned.
+  scoped_ptr<PowerPolicyController> policy_controller_;
 };
 
 TEST_F(PowerPolicyControllerTest, Prefs) {
