4f196ea300545cb863d7a65617384d28303a4883
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-24 22:30:06 +0000

    Remove left-over PageCache stuff
    PageCache was removed in r148973. This patch removes the rest
    of the Settings code (except a stub which is waiting on a Chromium
    patch to take care of it) as well as various comments/logging code.
    
    Finally, it gets rid of LayoutTests that are no longer exercising
    anything interesting. In a few cases, the test still seems to be
    testing interesting behavior; these have been updated to remove
    their reference to the UsesPageCache pref.
    
    R=abarth
    BUG=229605
    
    Review URL: https://codereview.chromium.org/14481003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149044 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebSettingsImpl.h
@@ -124,7 +124,6 @@ public:
     virtual void setNeedsSiteSpecificQuirks(bool);
     virtual void setOfflineWebApplicationCacheEnabled(bool);
     virtual void setOpenGLMultisamplingEnabled(bool);
-    virtual void setPageCacheSupportsPlugins(bool);
     virtual void setPasswordEchoDurationInSeconds(double);
     virtual void setPasswordEchoEnabled(bool);
     virtual void setPerTilePaintingEnabled(bool);
@@ -162,7 +161,6 @@ public:
     virtual void setUnsafePluginPastingEnabled(bool);
     virtual void setUserStyleSheetLocation(const WebURL&);
     virtual void setUsesEncodingDetector(bool);
-    virtual void setUsesPageCache(bool);
     virtual void setUseWideViewport(bool);
     virtual void setValidationMessageTimerMagnification(int);
     virtual void setViewportEnabled(bool);
--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/WebPreferences.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/WebPreferences.cpp
@@ -97,8 +97,6 @@ void WebPreferences::reset()
     shrinksStandaloneImagesToFit = false;
     textAreasAreResizable = true;
     userStyleSheetLocation = WebURL();
-    usesPageCache = false;
-    pageCacheSupportsPlugins = false;
     webSecurityEnabled = true;
     caretBrowsingEnabled = false;
 
@@ -169,8 +167,6 @@ void WebPreferences::applyTo(WebView* webView)
     settings->setShrinksStandaloneImagesToFit(shrinksStandaloneImagesToFit);
     settings->setTextAreasAreResizable(textAreasAreResizable);
     settings->setUserStyleSheetLocation(userStyleSheetLocation);
-    settings->setUsesPageCache(usesPageCache);
-    settings->setPageCacheSupportsPlugins(pageCacheSupportsPlugins);
     settings->setWebSecurityEnabled(webSecurityEnabled);
     settings->setAllowUniversalAccessFromFileURLs(allowUniversalAccessFromFileURLs);
     settings->setEditingBehavior(editingBehavior);
