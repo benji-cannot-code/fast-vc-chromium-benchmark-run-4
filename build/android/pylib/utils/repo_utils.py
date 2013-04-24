bb6c1e640774ae1ebd77f410619c7d2f4f2fad78
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 10:31:33 +0000

    Android: extracts "repo_utils.py" from flakiness_dashboard_results_uploader.py.
    Small reusable module.
    
    BUG=
    TEST=
    
    Review URL: https://chromiumcodereview.appspot.com/14118014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196112 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/pylib/utils/repo_utils.py
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from pylib import cmd_helper
+
+
+def GetGitHeadSHA1(in_directory):
+  """Returns the git hash tag for the given directory.
+
+  Args:
+    in_directory: The directory where git is to be run.
+  """
+  command_line = ['git', 'log', '-1', '--pretty=format:%H']
+  output = cmd_helper.GetCmdOutput(command_line, cwd=in_directory)
+  return output[0:40]
