399eeb5be9376d170f46068f828cbfdbf98133dd
keybuk@chromium.org <keybuk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-24 01:41:54 +0000

    D-Bus: notify properties changed from fakes
    Call the property changed callback when fakes use ReplaceValue() or
    ReplaceValueWithSetValue(), since the actual implementation will
    always call the callback.
    
    This avoids issues with property changed notifications being missing
    when properties structures are built within fakes.
    
    BUG=none
    TEST=dbus_unittests, device_unittests
    R=satorux@chromium.org, youngki@chromium.org
    
    Review URL: https://codereview.chromium.org/14443003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@195977 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/fake_bluetooth_adapter_client.cc
+++ chromeos/dbus/fake_bluetooth_adapter_client.cc
@@ -66,7 +66,6 @@ void FakeBluetoothAdapterClient::Properties::Set(
   if (property->name() == powered.name() || property->name() == alias.name()) {
     callback.Run(true);
     property->ReplaceValueWithSetValue();
-    NotifyPropertyChanged(property->name());
   } else {
     callback.Run(false);
   }
@@ -142,7 +141,6 @@ void FakeBluetoothAdapterClient::StartDiscovery(
 
   if (discovering_count_ == 1) {
     properties_->discovering.ReplaceValue(true);
-    properties_->NotifyPropertyChanged(properties_->discovering.name());
 
     FakeBluetoothDeviceClient* device_client =
         static_cast<FakeBluetoothDeviceClient*>(
@@ -178,7 +176,6 @@ void FakeBluetoothAdapterClient::StopDiscovery(
     device_client->EndDiscoverySimulation(dbus::ObjectPath(kAdapterPath));
 
     properties_->discovering.ReplaceValue(false);
-    properties_->NotifyPropertyChanged(properties_->discovering.name());
   }
 }
 
@@ -247,7 +244,6 @@ void FakeBluetoothAdapterClient::OnPropertyChanged(
     if (discovering_count_) {
       discovering_count_ = 0;
       properties_->discovering.ReplaceValue(false);
-      properties_->NotifyPropertyChanged(properties_->discovering.name());
     }
   }
 
