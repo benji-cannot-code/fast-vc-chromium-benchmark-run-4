60e9da43fa3e17f39866efc5466cf126d36f9ad7
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-25 08:52:03 +0000

    dbus: Add FakeShillManagerClient
    The fake class does nothing for now. We can implement fake behaviors when necessary.
    
    BUG=234463
    TEST=trybots
    
    Review URL: https://chromiumcodereview.appspot.com/14428004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196366 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/mock_dbus_thread_manager_without_gmock.h
+++ chromeos/dbus/mock_dbus_thread_manager_without_gmock.h
@@ -26,6 +26,7 @@ class FakeBluetoothDeviceClient;
 class FakeBluetoothInputClient;
 class FakeBluetoothProfileManagerClient;
 class FakeCrosDisksClient;
+class FakeShillManagerClient;
 class MockIBusClient;
 class MockIBusConfigClient;
 class MockIBusEngineFactoryService;
@@ -116,6 +117,10 @@ class MockDBusThreadManagerWithoutGMock : public DBusThreadManager {
     return fake_bluetooth_profile_manager_client_.get();
   }
 
+  FakeShillManagerClient* fake_shill_manager_client() {
+    return fake_shill_manager_client_.get();
+  }
+
   MockIBusClient* mock_ibus_client() {
     return mock_ibus_client_.get();
   }
@@ -153,6 +158,7 @@ class MockDBusThreadManagerWithoutGMock : public DBusThreadManager {
   scoped_ptr<FakeBluetoothProfileManagerClient>
       fake_bluetooth_profile_manager_client_;
   scoped_ptr<FakeCrosDisksClient> fake_cros_disks_client_;
+  scoped_ptr<FakeShillManagerClient> fake_shill_manager_client_;
 
   scoped_ptr<MockIBusClient> mock_ibus_client_;
   scoped_ptr<MockIBusConfigClient> mock_ibus_config_client_;
