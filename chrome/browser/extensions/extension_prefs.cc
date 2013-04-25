cf0e59fcd4c3f7214eff688d87659843238c26e8
dvh@chromium.org <dvh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-25 23:03:02 +0000

    Removed the check that shows the following log since it's an expected situation. [7512:7512:0311/123233:ERROR:extension_prefs.cc(1540)] Bad or missing pref 'version' for extension 'aohghmighlieiainnegkcijnfilokake'
    It happens in the case of first launch when installing default extensions of Chrome.
    The following key kPrefExternalAcknowledged is added in the prefs dictionary of the extension while the extension is not yet installed.
    
    BUG=181642
    
    Review URL: https://chromiumcodereview.appspot.com/14374005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196525 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_prefs.cc
+++ chrome/browser/extensions/extension_prefs.cc
@@ -1518,10 +1518,7 @@ std::string ExtensionPrefs::GetVersionString(const std::string& extension_id) {
     return std::string();
 
   std::string version;
-  if (!extension->GetString(kPrefVersion, &version)) {
-    LOG(ERROR) << "Bad or missing pref 'version' for extension '"
-               << extension_id << "'";
-  }
+  extension->GetString(kPrefVersion, &version);
 
   return version;
 }
