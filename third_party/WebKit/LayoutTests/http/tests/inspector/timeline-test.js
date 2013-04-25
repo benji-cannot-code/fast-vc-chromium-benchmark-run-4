742b595744fa6c68d29d0037f73879e0ffdeb26d
caseq@google.com <caseq@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 16:14:35 +0000

    Fix timeline-network-resource.html to include networkTime field
    This replaces a fix in r149959 with one that follows devtools
    test conventions.
    
    TBR=ojan
    BUG=
    
    Review URL: https://codereview.chromium.org/14358044
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149113 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
+++ third_party/WebKit/LayoutTests/http/tests/inspector/timeline-test.js
@@ -23,6 +23,7 @@ InspectorTest.timelinePropertyFormatters = {
     identifier: "formatAsTypeName",
     clip: "formatAsTypeName",
     root: "formatAsTypeName",
+    networkTime: "formatAsTypeName",
 };
 
 InspectorTest.startTimeline = function(callback)
