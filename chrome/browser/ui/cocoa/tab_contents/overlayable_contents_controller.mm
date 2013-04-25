5d40b5d562615be043102a3790fea19c8011d187
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-25 04:34:29 +0000

    Speculative fix for instant overlay crash (2nd try)
    This is a follow up to crrev.com/14242004.
    
    rsesek thinks that removeFromSuperview: might be causing
    overlayContents_ to get set to NULL.
    
    To test this theory I'm moving the call to removeFromSuperview:
    down a bit.
    
    BUG=230880
    
    Review URL: https://chromiumcodereview.appspot.com/14489004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196307 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/tab_contents/overlayable_contents_controller.mm
+++ chrome/browser/ui/cocoa/tab_contents/overlayable_contents_controller.mm
@@ -76,9 +76,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
   // Remove any old overlay contents before showing the new one.
   if (overlayContents_) {
-    [overlayContents_->GetView()->GetNativeView() removeFromSuperview];
     if (overlayContents_ != overlay)
       overlayContents_->WasHidden();
+    [overlayContents_->GetView()->GetNativeView() removeFromSuperview];
   }
 
   overlayContents_ = overlay;
