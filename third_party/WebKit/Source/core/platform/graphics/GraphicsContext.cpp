f31afa10b268a46a3f593ef9f2aecfa00d7e70ac
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 21:08:50 +0000

    Removing all code that uses GraphicsContext::clipBounds.
    In turn, that means removing almost all of ShadowBlur's functionality.
    The only remaining user is in SVG's FEDropShadow filter software path.
    
    BUG=235167
    
    Review URL: https://codereview.chromium.org/14284015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149129 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsContext.cpp
@@ -1336,12 +1336,6 @@ void GraphicsContext::clipToImageBuffer(ImageBuffer* buffer, const FloatRect& re
     buffer->clip(this, rect);
 }
 
-IntRect GraphicsContext::clipBounds() const
-{
-    ASSERT_NOT_REACHED();
-    return IntRect();
-}
-
 void GraphicsContext::canvasClip(const Path& pathToClip, WindRule clipRule)
 {
     if (paintingDisabled())
