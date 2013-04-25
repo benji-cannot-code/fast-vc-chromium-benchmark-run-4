1ab0d34ea6500696d2a113295e065da58b60f561
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 10:35:00 +0000

    Moved matchXXXRules from ElementRuleCollector to StyleResolver.
    This is a part of style resolver refactoring.
    To make each ScopedStyleResolver check its own author style,
    ElementRuleCollector should collect matched rules from a given rule set and
    does not need to do matchXXXRules.
    
    BUG=229345
    
    Review URL: https://chromiumcodereview.appspot.com/14453005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149094 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/StyleResolver.h
+++ third_party/WebKit/Source/core/css/StyleResolver.h
@@ -78,6 +78,7 @@ class CustomFilterProgram;
 struct CustomFilterProgramMixSettings;
 class Document;
 class Element;
+class ElementRuleCollector;
 class Frame;
 class FrameView;
 class KURL;
@@ -357,6 +358,16 @@ public:
 
         void addMatchedProperties(const StylePropertySet* properties, StyleRule* = 0, unsigned linkMatchType = SelectorChecker::MatchAll, PropertyWhitelistType = PropertyWhitelistNone);
     };
+
+private:
+    void matchUARules(ElementRuleCollector&, RuleSet*);
+    void matchAuthorRules(ElementRuleCollector&, bool includeEmptyRules);
+    void matchShadowDistributedRules(ElementRuleCollector&, bool includeEmptyRules, RuleRange&);
+    void matchHostRules(ElementRuleCollector&, bool includeEmptyRules);
+    void matchScopedAuthorRules(ElementRuleCollector&, bool includeEmptyRules);
+    void matchAllRules(ElementRuleCollector&, bool matchAuthorAndUserStyles, bool includeSMILProperties);
+    void matchUARules(ElementRuleCollector&);
+    void matchUserRules(ElementRuleCollector&, bool includeEmptyRules);
 
 private:
     // This function fixes up the default font size if it detects that the current generic font family has changed. -dwh
