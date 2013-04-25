df94143542ea245a088e53935d319e17b5177e0a
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 08:53:32 +0000

    DevTools: [FlameChart] Show js source of a function when user clicks on a bar.
    We should switch active panel to Sources and point the user to the first line of the function.
    
    BUG=none
    TEST=none
    R= vsevik@chromium.org, pfeldman@chromium.org, yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/14358035
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149088 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/FlameChart.js
+++ third_party/WebKit/Source/devtools/front_end/FlameChart.js
@@ -73,8 +73,7 @@ WebInspector.FlameChart = function(cpuProfileView)
 }
 
 /**
- * @constructor            entries.push(new WebInspector.FlameChart.Entry(colorPair, level, node.totalTime, offset, node));
-/
+ * @constructor
  * @implements {WebInspector.TimelineGrid.Calculator}
  */
 WebInspector.FlameChart.Calculator = function()
