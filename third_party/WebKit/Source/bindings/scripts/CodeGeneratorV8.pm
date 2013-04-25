2ef12b4221aca525d467d3c3ab3d1352fc528657
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 04:40:33 +0000

    In bindings, add check for enum to avoid custom signature
    Enums are internally just DOMStrings, so they don't need a custom signature
    or include file for a special type.
    This is currently triggered by e.g. having a readonly enum attribute
    or a method with an enum argument.
    Adding a check for enum fixes this.
    
    Concretely:
    * add IsEnum check to IsWrapperType,
    * add readonly attribute to test,
    * rebaseline:
      - adds readonly attribute
      - removes custom signature and include for method with enum
    
    BUG=234925
    
    Review URL: https://chromiumcodereview.appspot.com/13982014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149075 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -4602,6 +4602,7 @@ sub IsWrapperType
 {
     my $type = shift;
     # FIXME: Should this return false for Sequence and Array types?
+    return 0 if $codeGenerator->IsEnumType($type);
     return !($non_wrapper_types{$type});
 }
 
