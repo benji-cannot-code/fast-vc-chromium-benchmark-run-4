8963321946aecd6463a8972ed75b342111bd6736
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 13:18:19 +0000

    DevTools: changing UISourceCode.isDirty() semantics
    UISourceCode.isDirty() returns true iff it has a working copy
    and working copy differs from its content.
    This patch changes it to return true iff it has a working copy.
    
    BUG=233193
    
    Review URL: https://codereview.chromium.org/14366021
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149100 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/UISourceCode.js
+++ third_party/WebKit/Source/devtools/front_end/UISourceCode.js
@@ -418,7 +418,8 @@ WebInspector.UISourceCode.prototype = {
 
     resetWorkingCopy: function()
     {
-        this.setWorkingCopy(this._content);
+        delete this._workingCopy;
+        this.dispatchEventToListeners(WebInspector.UISourceCode.Events.WorkingCopyChanged);
     },
 
     /**
@@ -427,10 +428,7 @@ WebInspector.UISourceCode.prototype = {
     setWorkingCopy: function(newWorkingCopy)
     {
         this._mimeType = this.canonicalMimeType();
-        if (this._content === newWorkingCopy)
-            delete this._workingCopy;
-        else
-            this._workingCopy = newWorkingCopy;
+        this._workingCopy = newWorkingCopy;
         this.dispatchEventToListeners(WebInspector.UISourceCode.Events.WorkingCopyChanged);
     },
 
@@ -458,7 +456,7 @@ WebInspector.UISourceCode.prototype = {
      */
     isDirty: function()
     {
-        return typeof this._workingCopy !== "undefined" && this._workingCopy !== this._content;
+        return typeof this._workingCopy !== "undefined";
     },
 
     /**
