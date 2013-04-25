f7e3128d81fbec71b88a8cdebf9c6e7b914d89a8
rltoscano@chromium.org <rltoscano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-25 00:11:02 +0000

    Refactors fit-to-page out of print ticket store.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14329017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/native_layer.js
+++ chrome/browser/resources/print_preview/native_layer.js
@@ -168,7 +168,7 @@ cr.define('print_preview', function() {
         'printWithCloudPrint': destination != null && !destination.isLocal,
         'deviceName': destination == null ? 'foo' : destination.id,
         'generateDraftData': printTicketStore.isDocumentModifiable,
-        'fitToPageEnabled': printTicketStore.isFitToPageEnabled(),
+        'fitToPageEnabled': printTicketStore.fitToPage.getValue(),
 
         // NOTE: Even though the following fields don't directly relate to the
         // preview, they still need to be included.
@@ -243,7 +243,7 @@ cr.define('print_preview', function() {
         'deviceName': destination.id,
         'isFirstRequest': false,
         'requestID': -1,
-        'fitToPageEnabled': printTicketStore.isFitToPageEnabled()
+        'fitToPageEnabled': printTicketStore.fitToPage.getValue()
       };
 
       if (!destination.isLocal) {
--- chrome/browser/resources/print_preview/settings/other_options_settings.js
+++ chrome/browser/resources/print_preview/settings/other_options_settings.js
@@ -26,6 +26,13 @@ cr.define('print_preview', function() {
     this.duplexTicketItem_ = printTicketStore.duplex;
 
     /**
+     * Fit-to-page ticket item, used to read/write the fit-to-page selection.
+     * @type {!print_preview.ticket_items.FitToPage}
+     * @private
+     */
+    this.fitToPageTicketItem_ = printTicketStore.fitToPage;
+
+    /**
      * Used to monitor the state of the print ticket.
      * @type {!print_preview.PrintTicketStore}
      * @private
@@ -157,6 +164,10 @@ cr.define('print_preview', function() {
           this.duplexTicketItem_,
           print_preview.ticket_items.TicketItem.EventType.CHANGE,
           this.onDuplexChange_.bind(this));
+      this.tracker.add(
+          this.fitToPageTicketItem_,
+          print_preview.ticket_items.TicketItem.EventType.CHANGE,
+          this.onFitToPageChange_.bind(this));
     },
 
     /** @override */
@@ -204,7 +215,7 @@ cr.define('print_preview', function() {
      */
     updateContainerState_: function() {
       if (this.printTicketStore_.hasHeaderFooterCapability() ||
-          this.printTicketStore_.hasFitToPageCapability() ||
+          this.fitToPageTicketItem_.isCapabilityAvailable() ||
           this.duplexTicketItem_.isCapabilityAvailable() ||
           this.printTicketStore_.hasCssBackgroundCapability() ||
           this.printTicketStore_.hasSelectionOnlyCapability()) {
@@ -230,7 +241,7 @@ cr.define('print_preview', function() {
      * @private
      */
     onFitToPageCheckboxClick_: function() {
-      this.printTicketStore_.updateFitToPage(this.fitToPageCheckbox_.checked);
+      this.fitToPageTicketItem_.updateValue(this.fitToPageCheckbox_.checked);
     },
 
     /**
@@ -272,11 +283,6 @@ cr.define('print_preview', function() {
       this.headerFooterCheckbox_.checked =
           this.printTicketStore_.isHeaderFooterEnabled();
 
-      setIsVisible(this.fitToPageContainer_,
-                   this.printTicketStore_.hasFitToPageCapability());
-      this.fitToPageCheckbox_.checked =
-          this.printTicketStore_.isFitToPageEnabled();
-
       setIsVisible(this.cssBackgroundContainer_,
                    this.printTicketStore_.hasCssBackgroundCapability());
       this.cssBackgroundCheckbox_.checked =
@@ -299,6 +305,18 @@ cr.define('print_preview', function() {
       setIsVisible(this.duplexContainer_,
                    this.duplexTicketItem_.isCapabilityAvailable());
       this.duplexCheckbox_.checked = this.duplexTicketItem_.getValue();
+      this.updateContainerState_();
+    },
+
+    /**
+     * Called when the fit-to-page ticket item has changed. Updates the
+     * fit-to-page checkbox.
+     * @private
+     */
+    onFitToPageChange_: function() {
+      setIsVisible(this.fitToPageContainer_,
+                   this.fitToPageTicketItem_.isCapabilityAvailable());
+      this.fitToPageCheckbox_.checked = this.fitToPageTicketItem_.getValue();
       this.updateContainerState_();
     }
   };
