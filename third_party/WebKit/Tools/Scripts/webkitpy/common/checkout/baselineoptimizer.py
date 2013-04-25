66c1883762d60f6462a614d3d3c29e1098c1f144
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-25 04:17:57 +0000

    Memoize some functions to make optimize-baselines faster.
    On my machine, this takes optimize-baselines for a single test from
    ~640ms to ~480ms.
    
    Review URL: https://codereview.chromium.org/14392008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/baselineoptimizer.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/checkout/baselineoptimizer.py
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import copy
 import logging
 
+from webkitpy.common.memoized import memoized
 
 _log = logging.getLogger(__name__)
 
@@ -54,6 +55,7 @@ class BaselineOptimizer(object):
         self._scm = host.scm()
         self._port_names = port_names
 
+    @memoized
     def _relative_baseline_search_paths(self, port_name):
         port = self._port_factory.get(port_name)
         relative_paths = [self._filesystem.relpath(path, port.webkit_base()) for path in port.baseline_search_path()]
@@ -77,6 +79,7 @@ class BaselineOptimizer(object):
                     break
         return results_by_port_name
 
+    @memoized
     def _directories_immediately_preceding_root(self):
         directories = set()
         for port_name in self._port_names:
