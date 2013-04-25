5effce3b78a7676f2b6c1fe221dd2faac1210363
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-25 22:50:26 +0000

    cc: RoundUp/RoundDown unittests.
    This adds unittests to RoundUp/RoundDown, and reorders the
    cc_tests.gyp files to be in alphabetical order.
    
    Review URL: https://chromiumcodereview.appspot.com/13896025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196517 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/base/util.h
+++ cc/base/util.h
@@ -6,15 +6,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_BASE_UTIL_H_
 #define CC_BASE_UTIL_H_
 
+#include <limits>
+
+#include "base/basictypes.h"
+
 namespace cc {
 
 template <typename T> T RoundUp(T n, T mul) {
+  COMPILE_ASSERT(std::numeric_limits<T>::is_integer, type_must_be_integral);
   return (n > 0) ? ((n + mul - 1) / mul) * mul
                  : (n / mul) * mul;
 }
 
 template <typename T> T RoundDown(T n, T mul) {
+  COMPILE_ASSERT(std::numeric_limits<T>::is_integer, type_must_be_integral);
   return (n > 0) ? (n / mul) * mul
+                 : (n == 0) ? 0
                  : ((n - mul + 1) / mul) * mul;
 }
 
