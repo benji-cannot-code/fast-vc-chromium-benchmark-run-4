9504e5e7d2017ad001e282a7312c5498f3b68ebe
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 07:42:08 +0000

    chromeos: Add pref for user-activity screen dim factor.
    This adds a preference to control the scaling of the
    power manager's screen-dimming delay when user activity is
    observed while the screen is dimmed or soon after the screen
    has been turned off.  The default scaling is left at 1.0 for
    now so that this won't have an adverse effect on public
    accounts (it can be increased once a policy has been added).
    
    BUG=212394
    
    Review URL: https://chromiumcodereview.appspot.com/14373023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196658 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/power_policy_controller.cc
+++ chromeos/dbus/power_policy_controller.cc
@@ -80,7 +80,8 @@ PowerPolicyController::PrefValues::PrefValues()
       use_audio_activity(true),
       use_video_activity(true),
       enable_screen_lock(false),
-      presentation_idle_delay_factor(2.0) {}
+      presentation_idle_delay_factor(2.0),
+      user_activity_screen_dim_delay_factor(1.0) {}
 
 // static
 std::string PowerPolicyController::GetPolicyDebugString(
@@ -101,6 +102,10 @@ std::string PowerPolicyController::GetPolicyDebugString(
   if (policy.has_presentation_idle_delay_factor()) {
     str += base::StringPrintf("presentation_idle_delay_factor=%f ",
         policy.presentation_idle_delay_factor());
+  }
+  if (policy.has_user_activity_screen_dim_delay_factor()) {
+    str += base::StringPrintf("user_activity_screen_dim_delay_factor=%f ",
+        policy.user_activity_screen_dim_delay_factor());
   }
   if (policy.has_reason())
     str += base::StringPrintf("reason=\"%s\" ", policy.reason().c_str());
@@ -167,6 +172,8 @@ void PowerPolicyController::ApplyPrefs(const PrefValues& values) {
   prefs_policy_.set_use_video_activity(values.use_video_activity);
   prefs_policy_.set_presentation_idle_delay_factor(
       values.presentation_idle_delay_factor);
+  prefs_policy_.set_user_activity_screen_dim_delay_factor(
+      values.user_activity_screen_dim_delay_factor);
 
   prefs_were_set_ = true;
   SendCurrentPolicy();
