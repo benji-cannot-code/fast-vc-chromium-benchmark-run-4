dd82639b8f3d1de2e87303f98ba53493c3d684e2
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 03:42:37 +0000

    Translate: browser test to verify whole translate process
    BUG=none
    TEST=browser_tests --gtest_filter='TranslateBrowserTest.Translate'
    
    Review URL: https://chromiumcodereview.appspot.com/13825006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196587 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -1499,6 +1499,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/task_manager/task_manager_browsertest_util.h',
         'browser/task_manager/task_manager_notification_browsertest.cc',
         'browser/themes/theme_service_browsertest.cc',
+        'browser/translate/translate_browsertest.cc',
         'browser/translate/translate_manager_browsertest.cc',
         'browser/ui/app_list/app_list_controller_browsertest.cc',
         'browser/ui/ash/caps_lock_delegate_chromeos_browsertest.cc',
---
+++ chrome/test/data/translate/pseudo_main.js
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+div_log = function (message) {
+  var div = document.getElementById('log');
+  var element = document.createElement('pre');
+  element.innerText = message;
+  div.appendChild(element);
+}
+if (typeof world != 'undefined') {
+  div_log('v8 isolation doesn\'t work');
+  document.title = 'FAIL';
+  return;
+}
+div_log('The first script pseudo_main.js is loaded');
+div_log('Loading the second script pseudo_element_main.js ...');
+var head = document.getElementsByTagName('head')[0];
+var script = document.createElement('script');
+script.src = element_main_script_url;
+head.appendChild(script);
