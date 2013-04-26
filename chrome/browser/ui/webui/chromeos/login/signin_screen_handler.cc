4789dfa720e24d500bb94461769e979a02439425
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 00:03:11 +0000

    cros: Split GaiaAuthHost from login screen.
    So that the auth extension could be used in app identity api signin and
    other webui too.
    
    BUG=224336
    
    Review URL: https://chromiumcodereview.appspot.com/14161015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196546 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -67,13 +67,6 @@ using content::RenderViewHost;
 
 namespace {
 
-// Start page of GAIA authentication extension.
-const char kGaiaExtStartPage[] =
-    "chrome-extension://mfffpogegjflfpflabcdkioaeobkgjik/main.html";
-// Same as above but offline version.
-const char kGaiaExtStartPageOffline[] =
-    "chrome-extension://mfffpogegjflfpflabcdkioaeobkgjik/offline.html";
-
 // User dictionary keys.
 const char kKeyUsername[] = "username";
 const char kKeyDisplayName[] = "displayName";
@@ -972,8 +965,7 @@ void SigninScreenHandler::LoadAuthExtension(
                     !email_.empty() && password_changed_for_.count(email_));
   if (delegate_)
     params.SetBoolean("isShowUsers", delegate_->IsShowUsers());
-  params.SetString("startUrl",
-                   offline ? kGaiaExtStartPageOffline : kGaiaExtStartPage);
+  params.SetBoolean("useOffline", offline);
   params.SetString("email", email_);
   email_.clear();
 
