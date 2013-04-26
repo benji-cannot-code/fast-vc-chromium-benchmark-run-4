11db7906189e4af644dfc4e6aee176c480d85637
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 19:57:28 +0000

    android_webview: Remove use of ALLOW_THIS_IN_INITIALIZER_LIST.
    It's no longer providing value as the MSVC warning is disabled during compilation. Refer to bug for details.
    
    BUG=234765
    
    Review URL: https://codereview.chromium.org/14497005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196810 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/aw_quota_manager_bridge_impl.cc
+++ android_webview/native/aw_quota_manager_bridge_impl.cc
@@ -158,7 +158,7 @@ jint GetDefaultNativeAwQuotaManagerBridge(JNIEnv* env, jclass clazz) {
 
 AwQuotaManagerBridgeImpl::AwQuotaManagerBridgeImpl(
     AwBrowserContext* browser_context)
-    : weak_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+    : weak_factory_(this),
       browser_context_(browser_context) {
 }
 
