9306560aff89dd67615d99cdc0d7faeabe913e59
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-26 17:29:59 +0000

    Move DeviceMotion related classes from core/dom to modules/device_orientation.
    BUG=135804
    
    Review URL: https://codereview.chromium.org/14472006
    
    Patch from Tim Volodine <timvolodine@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8DeviceMotionEventCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8DeviceMotionEventCustom.cpp
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8DeviceMotionEvent.h"
 
 #include "bindings/v8/V8Binding.h"
-#include "core/dom/DeviceMotionData.h"
+#include "modules/device_orientation/DeviceMotionData.h"
 
 #include <v8.h>
 
