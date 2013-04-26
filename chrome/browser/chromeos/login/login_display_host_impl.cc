c7ae24285b8bfd9455b1d5b9f46e626d7eca1618
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 10:15:48 +0000

    Listen NOTIFICATION_LOGIN_NETWORK_ERROR_SHOWN to show UI in case of errors
    BUG=235317
    TEST=manual
    
    Review URL: https://codereview.chromium.org/13969007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/login_display_host_impl.cc
+++ chrome/browser/chromeos/login/login_display_host_impl.cc
@@ -249,6 +249,8 @@ LoginDisplayHostImpl::LoginDisplayHostImpl(const gfx::Rect& background_bounds)
       && initialize_webui_hidden_) {
     registrar_.Add(this, chrome::NOTIFICATION_LOGIN_WEBUI_VISIBLE,
                    content::NotificationService::AllSources());
+    registrar_.Add(this, chrome::NOTIFICATION_LOGIN_NETWORK_ERROR_SHOWN,
+                   content::NotificationService::AllSources());
   }
   LOG(INFO) << "Login WebUI >> "
             << "zero_delay: " << zero_delay_enabled
@@ -502,7 +504,8 @@ void LoginDisplayHostImpl::Observe(
    registrar_.Remove(this,
                      chrome::NOTIFICATION_WALLPAPER_ANIMATION_FINISHED,
                      content::NotificationService::AllSources());
- } else if (chrome::NOTIFICATION_LOGIN_WEBUI_VISIBLE == type) {
+ } else if (chrome::NOTIFICATION_LOGIN_WEBUI_VISIBLE == type ||
+            chrome::NOTIFICATION_LOGIN_NETWORK_ERROR_SHOWN == type) {
    LOG(INFO) << "Login WebUI >> WEBUI_VISIBLE";
    if (waiting_for_user_pods_ && initialize_webui_hidden_) {
      waiting_for_user_pods_ = false;
@@ -514,6 +517,9 @@ void LoginDisplayHostImpl::Observe(
    }
    registrar_.Remove(this,
                      chrome::NOTIFICATION_LOGIN_WEBUI_VISIBLE,
+                     content::NotificationService::AllSources());
+   registrar_.Remove(this,
+                     chrome::NOTIFICATION_LOGIN_NETWORK_ERROR_SHOWN,
                      content::NotificationService::AllSources());
   } else if (type == chrome::NOTIFICATION_CLOSE_ALL_BROWSERS_REQUEST) {
     ShutdownDisplayHost(true);
