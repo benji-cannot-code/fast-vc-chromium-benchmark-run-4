c5a1b6ed401419ee30a5b66ecc4ea47ba098f10a
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-26 10:39:42 +0000

    Build fix: UseCounter::deprecationMessage returns String, not String*.
    Review URL: https://codereview.chromium.org/14008006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149191 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/UseCounter.cpp
+++ third_party/WebKit/Source/core/page/UseCounter.cpp
@@ -79,7 +79,7 @@ void UseCounter::observe(Document* document, Feature feature)
     if (!page)
         return;
 
-    ASSERT(page->useCounter()->deprecationMessage(feature)->isEmpty());
+    ASSERT(page->useCounter()->deprecationMessage(feature).isEmpty());
     page->useCounter()->recordObservation(feature);
 }
 
@@ -113,7 +113,7 @@ void UseCounter::measureDeprecatedFeature(Document* document, Feature feature)
         return;
 
     if (page->useCounter()->recordObservation(feature)) {
-        ASSERT(!page->useCounter()->deprecationMessage(feature)->isEmpty());
+        ASSERT(!page->useCounter()->deprecationMessage(feature).isEmpty());
         page->console()->addMessage(DeprecationMessageSource, WarningMessageLevel, page->useCounter()->deprecationMessage(feature));
     }
 }
