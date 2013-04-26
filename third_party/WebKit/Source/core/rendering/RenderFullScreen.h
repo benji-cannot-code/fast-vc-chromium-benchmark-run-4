0a40ce1dbae7de7e575777765f143c2192e02ea2
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-26 09:15:34 +0000

    Make MemoryInstrumentation compatible with FINAL classes.
    R=loislo@chromium.org,yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/13911008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149188 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderFullScreen.h
+++ third_party/WebKit/Source/core/rendering/RenderFullScreen.h
@@ -31,8 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-// FIXME: make FINAL once MemoryInstrumentation does not use inheritance.
-class RenderFullScreen : public RenderFlexibleBox {
+class RenderFullScreen FINAL : public RenderFlexibleBox {
 public:
     static RenderFullScreen* createAnonymous(Document*);
 
