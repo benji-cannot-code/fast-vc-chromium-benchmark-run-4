04bfd5ea73aa647abb1ffe95714b5dde7e33c95a
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 08:01:40 +0000

    cc: Remove use of ALLOW_THIS_IN_INITIALIZER_LIST.
    It's no longer providing value as the MSVC warning is disabled during compilation. Refer to bug for details.
    
    BUG=234765
    
    Review URL: https://chromiumcodereview.appspot.com/13998033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196664 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/scheduler/frame_rate_controller.cc
+++ cc/scheduler/frame_rate_controller.cc
@@ -40,7 +40,7 @@ FrameRateController::FrameRateController(scoped_refptr<TimeSource> timer)
       active_(false),
       swap_buffers_complete_supported_(true),
       is_time_source_throttling_(true),
-      weak_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+      weak_factory_(this),
       thread_(NULL) {
   time_source_client_adapter_ =
       FrameRateControllerTimeSourceAdapter::Create(this);
@@ -54,7 +54,7 @@ FrameRateController::FrameRateController(Thread* thread)
       active_(false),
       swap_buffers_complete_supported_(true),
       is_time_source_throttling_(false),
-      weak_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)),
+      weak_factory_(this),
       thread_(thread) {}
 
 FrameRateController::~FrameRateController() {
