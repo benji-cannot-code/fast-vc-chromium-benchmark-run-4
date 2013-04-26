ca3bef55a7b957061c3ff9b15534ac7965438aba
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 23:20:04 +0000

    Incremental changes to chrome.location API. 1. chrome.location.onLocationUpdate event will be called separately for each request. Location param will have not an array of request names, but a single name. 2. Correspondingly, on C++ side, every LocationRequest becomes a geolocation observer. 3. LocationRequests live in IO thread, LocationManager lives in UI thread. 4. LocationManager gets notified when the extension is unloaded and clears extension’s requests. 5. LocationRequest calls GeolocationProvider’s OnPermissionGranted() since “location” permission is required to use the API.
    BUG=230943
    
    Review URL: https://chromiumcodereview.appspot.com/14287007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196874 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_system.cc
+++ chrome/browser/extensions/extension_system.cc
@@ -339,7 +339,7 @@ void ExtensionSystemImpl::InitForRegularProfile(bool extensions_enabled) {
   shared_->info_map();
 
   extension_process_manager_.reset(ExtensionProcessManager::Create(profile_));
-  location_manager_ = new LocationManager(profile_);
+  location_manager_.reset(new LocationManager(profile_));
 
   serial_connection_manager_.reset(new ApiResourceManager<SerialConnection>(
       BrowserThread::FILE));
