2872594533c6284982b5018ceba5cada61ca2346
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 20:02:01 +0000

    google_apis: Remove use of ALLOW_THIS_IN_INITIALIZER_LIST.
    It's no longer providing value as the MSVC warning is disabled during compilation. Refer to bug for details.
    
    BUG=234765
    
    Review URL: https://codereview.chromium.org/14113028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196811 0039d316-1c4b-4281-b951-d872f2087c98

--- google_apis/gaia/oauth2_api_call_flow_unittest.cc
+++ google_apis/gaia/oauth2_api_call_flow_unittest.cc
@@ -58,7 +58,7 @@ class MockUrlFetcherFactory : public ScopedURLFetcherFactory,
                               public URLFetcherFactory {
  public:
   MockUrlFetcherFactory()
-      : ScopedURLFetcherFactory(ALLOW_THIS_IN_INITIALIZER_LIST(this)) {
+      : ScopedURLFetcherFactory(this) {
   }
   virtual ~MockUrlFetcherFactory() {}
 
