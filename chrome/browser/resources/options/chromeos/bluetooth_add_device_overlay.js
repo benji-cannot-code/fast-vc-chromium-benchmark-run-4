667542934bac09c0f46b823dabca3216c76d423f
kevers@chromium.org <kevers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 17:23:13 +0000

    Fix layout of bluetooth device lists.
    BUG=169585, 233360
    
    Review URL: https://chromiumcodereview.appspot.com/13890021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/bluetooth_add_device_overlay.js
+++ chrome/browser/resources/options/chromeos/bluetooth_add_device_overlay.js
@@ -71,7 +71,6 @@ cr.define('options', function() {
     createDeviceList_: function() {
       this.deviceList_ = $('bluetooth-unpaired-devices-list');
       options.system.bluetooth.BluetoothDeviceList.decorate(this.deviceList_);
-      this.deviceList_.autoExpands = true;
     }
   };
 
