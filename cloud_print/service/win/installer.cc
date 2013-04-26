61bda4ae733e982e2f0ebcb05e6cef30e6935d62
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 07:30:36 +0000

    Shortcut should be to *config.exe not *setup.exe
    Review URL: https://chromiumcodereview.appspot.com/14260007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196649 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/service/win/installer.cc
+++ cloud_print/service/win/installer.cc
@@ -22,6 +22,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+const wchar_t kConfigBinaryName[] = L"cloud_print_service_config.exe";
+
 base::FilePath GetShortcutPath(int dir_key, bool with_subdir) {
   base::FilePath path;
   if (!PathService::Get(dir_key, &path))
@@ -43,6 +45,7 @@ void CreateShortcut(int dir_key, bool with_subdir,
   base::FilePath exe_path;
   if (!PathService::Get(base::FILE_EXE, &exe_path))
     return;
+  exe_path = exe_path.DirName().Append(base::FilePath(kConfigBinaryName));
   properties.set_target(exe_path);
   properties.set_working_dir(exe_path.DirName());
   CreateOrUpdateShortcutLink(path, properties, operation);
