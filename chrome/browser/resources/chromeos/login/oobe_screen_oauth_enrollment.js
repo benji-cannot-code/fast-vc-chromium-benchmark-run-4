77b4e6613a114ea26aeea28f25d49efb47ed7ab1
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 19:26:50 +0000

    Fix DCHECK due to argument count mismatch in enrollment handler.
    After https://codereview.chromium.org/14208014, Web UI handlers now
    DCHECK if the number of parameters passed from JS doesn't match those of
    the C++ function. This change fixes the oauthEnrollCompleteLogin /
    EnrollmentScreenHandler::HandleCompleteLogin inconsistency to avoid the
    DCHECK.
    
    BUG=None
    TEST=Enrollment flow doesn't DCHECK with Debug builds.
    
    Review URL: https://chromiumcodereview.appspot.com/14002019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196800 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_oauth_enrollment.js
@@ -286,7 +286,7 @@ login.createScreen('OAuthEnrollmentScreen', 'oauth-enrollment', function() {
     onMessage_: function(m) {
       var msg = m.data;
       if (msg.method == 'completeLogin' && this.isSigninMessage_(m))
-        chrome.send('oauthEnrollCompleteLogin', [msg.email, msg.password]);
+        chrome.send('oauthEnrollCompleteLogin', [msg.email]);
     }
   };
 });
