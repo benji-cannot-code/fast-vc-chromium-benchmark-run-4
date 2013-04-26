c0d80488f3fae455be75632fa9968a51591e012e
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-26 08:42:09 +0000

    Rename bindings/v8/ScriptRunner with bindings/v8/V8ScriptRunner.
    It's confusing because there exists core/dom/ScriptRunner.
    
    R=abarth,jochen
    
    BUG=235499
    
    Review URL: https://codereview.chromium.org/14002014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptController.cpp
+++ third_party/WebKit/Source/bindings/v8/ScriptController.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "bindings/v8/ScriptController.h"
 
-#include "ScriptRunner.h"
 #include "V8DOMWindow.h"
 #include "V8Event.h"
 #include "V8HTMLEmbedElement.h"
@@ -49,6 +48,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8NPObject.h"
 #include "bindings/v8/V8PerContextData.h"
 #include "bindings/v8/V8RecursionScope.h"
+#include "bindings/v8/V8ScriptRunner.h"
 #include "bindings/v8/npruntime_impl.h"
 #include "bindings/v8/npruntime_priv.h"
 #include "core/dom/Document.h"
@@ -266,7 +266,7 @@ v8::Local<v8::Value> ScriptController::compileAndRunScript(const ScriptSourceCod
 
         // Keep Frame (and therefore ScriptController) alive.
         RefPtr<Frame> protect(m_frame);
-        result = ScriptRunner::runCompiledScript(script, m_frame->document());
+        result = V8ScriptRunner::runCompiledScript(script, m_frame->document());
         ASSERT(!tryCatch.HasCaught() || result.IsEmpty());
     }
 
--- third_party/WebKit/Source/bindings/v8/WorkerScriptController.cpp
+++ third_party/WebKit/Source/bindings/v8/WorkerScriptController.cpp
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/v8/WorkerScriptController.h"
 
-#include "ScriptRunner.h"
 #include "V8DedicatedWorkerContext.h"
 #include "V8SharedWorkerContext.h"
 #include "V8WorkerContext.h"
@@ -41,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScriptValue.h"
 #include "bindings/v8/V8GCController.h"
 #include "bindings/v8/V8Initializer.h"
+#include "bindings/v8/V8ScriptRunner.h"
 #include "bindings/v8/WrapperTypeInfo.h"
 #include "core/inspector/ScriptCallStack.h"
 #include "core/page/DOMTimer.h"
@@ -157,7 +157,7 @@ ScriptValue WorkerScriptController::evaluate(const String& script, const String&
 
     v8::Handle<v8::String> scriptString = v8String(script, isolate);
     v8::Handle<v8::Script> compiledScript = ScriptSourceCode::compileScript(scriptString, fileName, scriptStartPosition, 0, isolate);
-    v8::Local<v8::Value> result = ScriptRunner::runCompiledScript(compiledScript, m_workerContext);
+    v8::Local<v8::Value> result = V8ScriptRunner::runCompiledScript(compiledScript, m_workerContext);
 
     if (!block.CanContinue()) {
         m_workerContext->script()->forbidExecution();
