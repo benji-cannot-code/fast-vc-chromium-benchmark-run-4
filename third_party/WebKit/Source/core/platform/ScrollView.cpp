dcb490871cc9379ef23886d1e6a0d6755225a2f4
dongseong.hwang@intel.com <dongseong.hwang@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-26 08:16:55 +0000

    Remove fixed visible content rect from ScrollView
    Remove an unused funtionality, fixed visible content rect, from ScrollView
    because it is lagacy code for delegates scrolling of QT.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14081042
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/ScrollView.cpp
+++ third_party/WebKit/Source/core/platform/ScrollView.cpp
@@ -197,9 +197,6 @@ void ScrollView::setClipsRepaints(bool clipsRepaints)
 
 IntSize ScrollView::unscaledVisibleContentSize(VisibleContentRectIncludesScrollbars scrollbarInclusion) const
 {
-    if (!m_fixedVisibleContentRect.isEmpty())
-        return m_fixedVisibleContentRect.size();
-
     int verticalScrollbarWidth = 0;
     int horizontalScrollbarHeight = 0;
 
@@ -216,9 +213,6 @@ IntSize ScrollView::unscaledVisibleContentSize(VisibleContentRectIncludesScrollb
 
 IntRect ScrollView::visibleContentRect(VisibleContentRectIncludesScrollbars scollbarInclusion) const
 {
-    if (!m_fixedVisibleContentRect.isEmpty())
-        return m_fixedVisibleContentRect;
-
     FloatSize visibleContentSize = unscaledVisibleContentSize(scollbarInclusion);
     visibleContentSize.scale(1 / visibleContentScaleFactor());
     return IntRect(IntPoint(m_scrollOffset), expandedIntSize(visibleContentSize));
