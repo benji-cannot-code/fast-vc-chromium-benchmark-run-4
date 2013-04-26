34b59e3011a62398f9f6a935a2cc06a0af97c64e
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 07:20:18 +0000

    Notify the interstitial page's web contents delegate of focus.
    This mimics WebContentsViewAura::GotFocus in InterstitialPageImpl.
    ( fixes a Views Textfield focus issue; the same textfield is used in Aura )
    
    TODO(followup): Add an interatctive_ui_test for the expected behavior.
    
    BUG=233155
    TEST=Changing focus between interstitial pages and the [Views] Omnibox works as expected.
    R=creis@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/13843012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196642 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/interstitial_page_impl.cc
+++ content/browser/web_contents/interstitial_page_impl.cc
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/notification_source.h"
 #include "content/public/browser/storage_partition.h"
+#include "content/public/browser/web_contents_delegate.h"
 #include "content/public/common/bindings_policy.h"
 #include "content/public/common/page_transition_types.h"
 #include "net/base/escape.h"
@@ -777,6 +778,9 @@ void InterstitialPageImpl::InterstitialPageRVHDelegateView::UpdateDragCursor(
 }
 
 void InterstitialPageImpl::InterstitialPageRVHDelegateView::GotFocus() {
+  WebContents* web_contents = interstitial_page_->web_contents();
+  if (web_contents && web_contents->GetDelegate())
+    web_contents->GetDelegate()->WebContentsFocused(web_contents);
 }
 
 void InterstitialPageImpl::InterstitialPageRVHDelegateView::TakeFocus(
