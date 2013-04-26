f471014ecac72ef7ba6af2c305b316fc61e5873c
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-26 00:32:02 +0000

    Cleanup: Fix more lint errors in media galleries.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14340014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/linux/mtp_device_task_helper_map_service.h
+++ chrome/browser/media_galleries/linux/mtp_device_task_helper_map_service.h
@@ -1,7 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
- // Copyright 2013 The Chromium Authors. All rights reserved.
- // Use of this source code is governed by a BSD-style license that can be
- // found in the LICENSE file.
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #ifndef CHROME_BROWSER_MEDIA_GALLERIES_LINUX_MTP_DEVICE_TASK_HELPER_MAP_SERVICE_H_
 #define CHROME_BROWSER_MEDIA_GALLERIES_LINUX_MTP_DEVICE_TASK_HELPER_MAP_SERVICE_H_
--- chrome/browser/media_galleries/mac/mtp_device_delegate_impl_mac.h
+++ chrome/browser/media_galleries/mac/mtp_device_delegate_impl_mac.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_MEDIA_GALLERIES_MTP_DEVICE_DELEGATE_IMPL_MAC_H_
-#define CHROME_BROWSER_MEDIA_GALLERIES_MTP_DEVICE_DELEGATE_IMPL_MAC_H_
+#ifndef CHROME_BROWSER_MEDIA_GALLERIES_MAC_MTP_DEVICE_DELEGATE_IMPL_MAC_H_
+#define CHROME_BROWSER_MEDIA_GALLERIES_MAC_MTP_DEVICE_DELEGATE_IMPL_MAC_H_
 
 #include <list>
 #include <map>
@@ -140,4 +140,4 @@ class MTPDeviceDelegateImplMac : public MTPDeviceAsyncDelegate {
 
 }  // namespace chrome
 
-#endif  // CHROME_BROWSER_MEDIA_GALLERIES_MTP_DEVICE_DELEGATE_IMPL_MAC_H_
+#endif  // CHROME_BROWSER_MEDIA_GALLERIES_MAC_MTP_DEVICE_DELEGATE_IMPL_MAC_H_
