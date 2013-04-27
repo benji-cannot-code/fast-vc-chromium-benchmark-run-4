37f74e47cc7f8958ee66c73d194539c5bd9c50b4
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 02:06:32 +0000

    gesture nav: Encode screenshots in a worker thread.
    Screenshots can be large, and png-encoding them in the UI thread is
    just a very bad idea (because it can take a long time).
    
    BUG=235739
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/14335003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196919 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_screenshot_manager.cc
+++ content/browser/web_contents/web_contents_screenshot_manager.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/web_contents_screenshot_manager.h"
 
 #include "base/command_line.h"
+#include "base/threading/worker_pool.h"
 #include "content/browser/renderer_host/render_view_host_impl.h"
 #include "content/browser/web_contents/navigation_controller_impl.h"
 #include "content/browser/web_contents/navigation_entry_impl.h"
@@ -24,10 +25,45 @@ const int kMinScreenshotIntervalMS = 1000;
 
 namespace content {
 
+// Encodes an SkBitmap to PNG data in a worker thread.
+class ScreenshotData : public base::RefCountedThreadSafe<ScreenshotData> {
+ public:
+  ScreenshotData() {
+  }
+
+  void EncodeScreenshot(const SkBitmap& bitmap, base::Closure callback) {
+    if (!base::WorkerPool::PostTaskAndReply(FROM_HERE,
+            base::Bind(&ScreenshotData::EncodeOnWorker,
+                       this,
+                       bitmap),
+            callback,
+            true)) {
+      callback.Run();
+    }
+  }
+
+  scoped_refptr<base::RefCountedBytes> data() const { return data_; }
+
+ private:
+  friend class base::RefCountedThreadSafe<ScreenshotData>;
+  virtual ~ScreenshotData() {
+  }
+
+  void EncodeOnWorker(const SkBitmap& bitmap) {
+    std::vector<unsigned char> data;
+    if (gfx::PNGCodec::EncodeBGRASkBitmap(bitmap, true, &data))
+      data_ = new base::RefCountedBytes(data);
+  }
+
+  scoped_refptr<base::RefCountedBytes> data_;
+
+  DISALLOW_COPY_AND_ASSIGN(ScreenshotData);
+};
+
 WebContentsScreenshotManager::WebContentsScreenshotManager(
     NavigationControllerImpl* owner)
     : owner_(owner),
-      ALLOW_THIS_IN_INITIALIZER_LIST(take_screenshot_factory_(this)),
+      screenshot_factory_(this),
       min_screenshot_interval_ms_(kMinScreenshotIntervalMS) {
 }
 
@@ -86,7 +122,7 @@ void WebContentsScreenshotManager::TakeScreenshotImpl(
   host->CopyFromBackingStore(gfx::Rect(),
       host->GetView()->GetViewBounds().size(),
       base::Bind(&WebContentsScreenshotManager::OnScreenshotTaken,
-                 take_screenshot_factory_.GetWeakPtr(),
+                 screenshot_factory_.GetWeakPtr(),
                  entry->GetUniqueID()));
 }
 
@@ -118,13 +154,13 @@ void WebContentsScreenshotManager::OnScreenshotTaken(int unique_id,
     return;
   }
 
-  std::vector<unsigned char> data;
-  if (gfx::PNGCodec::EncodeBGRASkBitmap(bitmap, true, &data)) {
-    entry->SetScreenshotPNGData(data);
-    PurgeScreenshotsIfNecessary();
-  } else {
-    ClearScreenshot(entry);
-  }
+  scoped_refptr<ScreenshotData> screenshot = new ScreenshotData();
+  screenshot->EncodeScreenshot(
+      bitmap,
+      base::Bind(&WebContentsScreenshotManager::OnScreenshotEncodeComplete,
+                 screenshot_factory_.GetWeakPtr(),
+                 unique_id,
+                 screenshot));
 }
 
 int WebContentsScreenshotManager::GetScreenshotCount() const {
@@ -139,11 +175,33 @@ int WebContentsScreenshotManager::GetScreenshotCount() const {
   return screenshot_count;
 }
 
+void WebContentsScreenshotManager::OnScreenshotEncodeComplete(
+    int unique_id,
+    scoped_refptr<ScreenshotData> screenshot) {
+  NavigationEntryImpl* entry = NULL;
+  int entry_count = owner_->GetEntryCount();
+  for (int i = 0; i < entry_count; ++i) {
+    NavigationEntry* iter = owner_->GetEntryAtIndex(i);
+    if (iter->GetUniqueID() == unique_id) {
+      entry = NavigationEntryImpl::FromNavigationEntry(iter);
+      break;
+    }
+  }
+  if (!entry)
+    return;
+  entry->SetScreenshotPNGData(screenshot->data());
+  OnScreenshotSet(entry);
+}
+
+void WebContentsScreenshotManager::OnScreenshotSet(NavigationEntryImpl* entry) {
+  PurgeScreenshotsIfNecessary();
+}
+
 bool WebContentsScreenshotManager::ClearScreenshot(NavigationEntryImpl* entry) {
   if (!entry->screenshot())
     return false;
 
-  entry->SetScreenshotPNGData(std::vector<unsigned char>());
+  entry->SetScreenshotPNGData(NULL);
   return true;
 }
 
--- content/browser/web_contents/web_contents_view_aura_browsertest.cc
+++ content/browser/web_contents/web_contents_view_aura_browsertest.cc
@@ -34,7 +34,8 @@ class ScreenshotTracker : public WebContentsScreenshotManager {
  public:
   explicit ScreenshotTracker(NavigationControllerImpl* controller)
       : WebContentsScreenshotManager(controller),
-        screenshot_taken_for_(NULL) {
+        screenshot_taken_for_(NULL),
+        waiting_for_screenshots_(0) {
   }
 
   virtual ~ScreenshotTracker() {
@@ -50,15 +51,32 @@ class ScreenshotTracker : public WebContentsScreenshotManager {
     SetMinScreenshotIntervalMS(interval_ms);
   }
 
+  void WaitUntilScreenshotIsReady() {
+    if (!waiting_for_screenshots_)
+      return;
+    message_loop_runner_ = new content::MessageLoopRunner;
+    message_loop_runner_->Run();
+  }
+
  private:
   // Overridden from WebContentsScreenshotManager:
   virtual void TakeScreenshotImpl(RenderViewHost* host,
                                   NavigationEntryImpl* entry) OVERRIDE {
+    ++waiting_for_screenshots_;
     screenshot_taken_for_ = host;
     WebContentsScreenshotManager::TakeScreenshotImpl(host, entry);
   }
 
+  virtual void OnScreenshotSet(NavigationEntryImpl* entry) OVERRIDE {
+    --waiting_for_screenshots_;
+    WebContentsScreenshotManager::OnScreenshotSet(entry);
+    if (waiting_for_screenshots_ == 0 && message_loop_runner_)
+      message_loop_runner_->Quit();
+  }
+
   RenderViewHost* screenshot_taken_for_;
+  scoped_refptr<content::MessageLoopRunner> message_loop_runner_;
+  int waiting_for_screenshots_;
 
   DISALLOW_COPY_AND_ASSIGN(ScreenshotTracker);
 };
@@ -322,6 +340,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
   EXPECT_EQ(1, GetCurrentIndex());
   ExecuteSyncJSFunction(view_host, "navigate_next()");
   EXPECT_EQ(2, GetCurrentIndex());
+  screenshot_manager()->WaitUntilScreenshotIsReady();
 
   // The current entry won't have any screenshots. But the entries in the
   // history should now have screenshots.
@@ -340,6 +359,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
   // Navigate again. Index 2 should now have a screenshot.
   ExecuteSyncJSFunction(view_host, "navigate_next()");
   EXPECT_EQ(3, GetCurrentIndex());
+  screenshot_manager()->WaitUntilScreenshotIsReady();
 
   entry = NavigationEntryImpl::FromNavigationEntry(
       web_contents->GetController().GetEntryAtIndex(2));
@@ -365,6 +385,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
     string16 actual_title = title_watcher.WaitAndGetTitle();
     EXPECT_EQ(expected_title, actual_title);
     EXPECT_EQ(2, GetCurrentIndex());
+    screenshot_manager()->WaitUntilScreenshotIsReady();
     entry = NavigationEntryImpl::FromNavigationEntry(
         web_contents->GetController().GetEntryAtIndex(3));
     EXPECT_TRUE(entry->screenshot().get());
@@ -375,6 +396,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
   EXPECT_EQ(3, GetCurrentIndex());
   ExecuteSyncJSFunction(view_host, "navigate_next()");
   EXPECT_EQ(4, GetCurrentIndex());
+  screenshot_manager()->WaitUntilScreenshotIsReady();
   entry = NavigationEntryImpl::FromNavigationEntry(
       web_contents->GetController().GetEntryAtIndex(4));
   EXPECT_FALSE(entry->screenshot().get());
@@ -387,6 +409,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
     string16 actual_title = title_watcher.WaitAndGetTitle();
     EXPECT_EQ(expected_title, actual_title);
     EXPECT_EQ(3, GetCurrentIndex());
+    screenshot_manager()->WaitUntilScreenshotIsReady();
     entry = NavigationEntryImpl::FromNavigationEntry(
         web_contents->GetController().GetEntryAtIndex(4));
     EXPECT_TRUE(entry->screenshot().get());
@@ -432,6 +455,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
     RenderViewHost* old_host = web_contents->GetRenderViewHost();
     web_contents->GetController().LoadURLWithParams(params);
     WaitForLoadStop(web_contents);
+    screenshot_manager()->WaitUntilScreenshotIsReady();
 
     EXPECT_NE(old_host, web_contents->GetRenderViewHost())
         << navigations[i].url.spec();
@@ -456,6 +480,7 @@ IN_PROC_BROWSER_TEST_F(WebContentsViewAuraTest,
   params.transition_type = PageTransitionFromInt(navigations[0].transition);
   web_contents->GetController().LoadURLWithParams(params);
   WaitForLoadStop(web_contents);
+  screenshot_manager()->WaitUntilScreenshotIsReady();
 
   EXPECT_EQ(NULL, screenshot_manager()->screenshot_taken_for());
 }
