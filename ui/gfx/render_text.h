c4473979e43f4d8667cf1332e903b158a8bef584
ckocagil@chromium.org <ckocagil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 04:55:08 +0000

    NativeTextfieldViews: Do not allow dropping text in the same place
    BUG=229841
    
    Review URL: https://chromiumcodereview.appspot.com/14054002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196945 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text.h
+++ ui/gfx/render_text.h
@@ -320,12 +320,6 @@ class UI_EXPORT RenderText {
   // chosen.
   virtual std::vector<FontSpan> GetFontSpansForTesting() = 0;
 
-  // A convenience function to check whether the glyph attached to the caret
-  // is within the given range.
-  static bool RangeContainsCaret(const ui::Range& range,
-                                 size_t caret_pos,
-                                 LogicalCursorDirection caret_affinity);
-
  protected:
   RenderText();
 
@@ -428,6 +422,12 @@ class UI_EXPORT RenderText {
 
   // Applies text shadows to |renderer|.
   void ApplyTextShadows(internal::SkiaTextRenderer* renderer);
+
+  // A convenience function to check whether the glyph attached to the caret
+  // is within the given range.
+  static bool RangeContainsCaret(const ui::Range& range,
+                                 size_t caret_pos,
+                                 LogicalCursorDirection caret_affinity);
 
  private:
   friend class RenderTextTest;
