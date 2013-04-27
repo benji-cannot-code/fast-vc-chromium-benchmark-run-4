e8022cfa8337ffe233fcc3812afc91004d6497b9
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 00:11:43 +0000

    sync: Remove use of ALLOW_THIS_IN_INITIALIZER_LIST.
    It's no longer providing value as the MSVC warning is disabled during compilation. Refer to bug for details.
    
    BUG=234765
    
    Review URL: https://chromiumcodereview.appspot.com/14335008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196892 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/internal_api/js_mutation_event_observer.cc
+++ sync/internal_api/js_mutation_event_observer.cc
@@ -17,7 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace syncer {
 
 JsMutationEventObserver::JsMutationEventObserver()
-    : weak_ptr_factory_(ALLOW_THIS_IN_INITIALIZER_LIST(this)) {}
+    : weak_ptr_factory_(this) {}
 
 JsMutationEventObserver::~JsMutationEventObserver() {
   DCHECK(CalledOnValidThread());
