8c2c0eecb7a40d409f8a84f0ef2d0a188a581d2f
ccameron@chromium.org <ccameron@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 06:15:57 +0000

    Disable test  CaptivePortalBrowserTest.NavigateLoadingTabToTimeoutTwoSites on Mac
    BUG=236118
    TBR=danakj
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14061024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196951 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/captive_portal/captive_portal_browsertest.cc
+++ chrome/browser/captive_portal/captive_portal_browsertest.cc
@@ -1888,7 +1888,8 @@ IN_PROC_BROWSER_TEST_F(CaptivePortalBrowserTest,
 }
 
 // Fails on Windows only, mostly on Win7. http://crbug.com/170033
-#if defined(OS_WIN)
+// Fails on Mac with assert. http://crbug.com/236118
+#if defined(OS_WIN) || defined(OS_MACOSX)
 #define MAYBE_NavigateLoadingTabToTimeoutTwoSites \
         DISABLED_NavigateLoadingTabToTimeoutTwoSites
 #else
