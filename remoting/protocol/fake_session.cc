8d9b7011ac02b8deb3eb3979900945a315bdeca1
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 00:00:44 +0000

    remoting: Remove use of ALLOW_THIS_IN_INITIALIZER_LIST.
    It's no longer providing value as the MSVC warning is disabled during compilation. Refer to bug for details.
    
    BUG=234765
    
    Review URL: https://chromiumcodereview.appspot.com/14522010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196888 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/fake_session.cc
+++ remoting/protocol/fake_session.cc
@@ -28,7 +28,7 @@ FakeSocket::FakeSocket()
       read_buffer_size_(0),
       input_pos_(0),
       message_loop_(MessageLoop::current()),
-      ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)) {
+      weak_factory_(this) {
 }
 
 FakeSocket::~FakeSocket() {
@@ -280,7 +280,7 @@ FakeSession::FakeSession()
       jid_(kTestJid),
       error_(OK),
       closed_(false),
-      ALLOW_THIS_IN_INITIALIZER_LIST(weak_factory_(this)) {
+      weak_factory_(this) {
 }
 
 FakeSession::~FakeSession() { }
