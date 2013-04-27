70379059b5da9988bbc62c9d9055b413f0fd7649
jamescook@chromium.org <jamescook@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 00:57:51 +0000

    views: Warn if AuraTestBase SetUp/TearDown not called
    I think this code was mistakenly removed when AuraTestHelper was
    introduced. It's trying to warn if someone doesn't call the base
    class SetUp()/TearDown().
    
    Arguably it's not needed in AuraTestHelper, but I don't think it
    hurts to keep it there as well.
    
    BUG=none
    TEST=views_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/14314014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196903 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/aura_test_base.cc
+++ ui/aura/test/aura_test_base.cc
@@ -19,13 +19,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace aura {
 namespace test {
 
-AuraTestBase::AuraTestBase() {
+AuraTestBase::AuraTestBase()
+    : setup_called_(false),
+      teardown_called_(false) {
 }
 
 AuraTestBase::~AuraTestBase() {
+  CHECK(setup_called_)
+      << "You have overridden SetUp but never called super class's SetUp";
+  CHECK(teardown_called_)
+      << "You have overridden TearDown but never called super class's TearDown";
 }
 
 void AuraTestBase::SetUp() {
+  setup_called_ = true;
   testing::Test::SetUp();
   ui::TextInputTestSupport::Initialize();
 
@@ -72,6 +79,8 @@ void AuraTestBase::SetUp() {
 }
 
 void AuraTestBase::TearDown() {
+  teardown_called_ = true;
+
   // Flush the message loop because we have pending release tasks
   // and these tasks if un-executed would upset Valgrind.
   RunAllPendingInMessageLoop();
