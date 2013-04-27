aac5f16aa5bb73edcb825852298af9e7bacb6fb1
tbarzic@chromium.org <tbarzic@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 04:47:59 +0000

    views: Separate hit testing for tooltips from hit testing for events
    This is needed so labels could show tooltips (as label don't
    support mouse events, they are never found by hit testing for
    event handler)
    
    BUG=175070
    
    Review URL: https://chromiumcodereview.appspot.com/14366037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196943 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
+++ chrome/browser/ui/views/tabs/fake_base_tab_strip_controller.cc
@@ -10,20 +10,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 FakeBaseTabStripController::FakeBaseTabStripController()
     : tab_strip_(NULL),
-      num_tabs_(0) {
+      num_tabs_(0),
+      active_index_(-1) {
 }
 
 FakeBaseTabStripController::~FakeBaseTabStripController() {
 }
 
-void FakeBaseTabStripController::AddTab(int index) {
+void FakeBaseTabStripController::AddTab(int index, bool is_active) {
   num_tabs_++;
-  tab_strip_->AddTabAt(index, TabRendererData(), false);
+  tab_strip_->AddTabAt(index, TabRendererData(), is_active);
+  if (is_active)
+    active_index_ = index;
 }
 
 void FakeBaseTabStripController::RemoveTab(int index) {
   num_tabs_--;
   tab_strip_->RemoveTabAt(index);
+  if (active_index_ == index)
+    active_index_ = -1;
 }
 
 const ui::ListSelectionModel& FakeBaseTabStripController::GetSelectionModel() {
@@ -39,11 +44,13 @@ bool FakeBaseTabStripController::IsValidIndex(int index) const {
 }
 
 bool FakeBaseTabStripController::IsActiveTab(int index) const {
-  return false;
+  if (!IsValidIndex(index))
+    return false;
+  return active_index_ == index;
 }
 
 int FakeBaseTabStripController::GetActiveIndex() const {
-  return -1;
+  return active_index_;
 }
 
 bool FakeBaseTabStripController::IsTabSelected(int index) const {
