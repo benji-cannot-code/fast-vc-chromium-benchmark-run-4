d6ea8fa87553e186292928356b4c4466c2ae506f
miu@chromium.org <miu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-27 00:28:22 +0000

    Revert 196894 "First step to add audio track support to local st..."
    > First step to add audio track support to local stream.
    > This patch builds up a correct structure for the local audio track, and added a correct chromium audio track implementation to local media stream.
    > 
    > This patch does not change the current functionalities and behaviour of local media stream, it is more like a refactoring on the structure.
    > 
    > BUG=164813
    > TEST=content_unittests --gtest_filter="*WebRtcLocalAudio*"
    > apprtc.appspot.com and webrtc.googlecode.com/svn/trunk/samples/js/demos/html/local-audio-rendering.html
    > 
    > Review URL: https://chromiumcodereview.appspot.com/13601012
    
    TBR=xians@chromium.org
    
    Review URL: https://codereview.chromium.org/14307020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@196900 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_dependency_factory.cc
+++ content/renderer/media/media_stream_dependency_factory.cc
@@ -16,7 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/video_capture_impl_manager.h"
 #include "content/renderer/media/webaudio_capturer_source.h"
 #include "content/renderer/media/webrtc_audio_device_impl.h"
-#include "content/renderer/media/webrtc_local_audio_track.h"
 #include "content/renderer/media/webrtc_uma_histograms.h"
 #include "content/renderer/p2p/ipc_network_manager.h"
 #include "content/renderer/p2p/ipc_socket_factory.h"
@@ -559,11 +558,7 @@ scoped_refptr<webrtc::AudioTrackInterface>
 MediaStreamDependencyFactory::CreateLocalAudioTrack(
     const std::string& id,
     webrtc::AudioSourceInterface* source) {
-  // TODO(xians): Merge |source| to the capturer(). We can't do this today
-  // because only one capturer() is supported while one |source| is created
-  // for each audio track.
-  return WebRtcLocalAudioTrack::Create(id, GetWebRtcAudioDevice()->capturer(),
-                                       source);
+  return pc_factory_->CreateAudioTrack(id, source).get();
 }
 
 webrtc::SessionDescriptionInterface*
