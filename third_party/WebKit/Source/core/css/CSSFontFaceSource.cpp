06aeb1364fcbc70af2c4c383cd980946866439ed
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-28 20:32:56 +0000

    Remove unused include.
    BUG=236223
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/14445009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149298 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSFontFaceSource.cpp
+++ third_party/WebKit/Source/core/css/CSSFontFaceSource.cpp
@@ -37,7 +37,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/SimpleFontData.h"
 
 #if ENABLE(SVG_FONTS)
-#include "FontCustomPlatformData.h"
 #include "SVGNames.h"
 #include "core/svg/SVGFontData.h"
 #include "core/svg/SVGFontElement.h"
