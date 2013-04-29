9800ea1f1ea1472e19b0061f9a1dc91df7864605
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 21:48:29 +0000

    Set suggestion margins correctly and fix RTL.
    We need to reset margins the first time the window has a
    valid width and afterwards onmarginchange. Also fix alignment
    when rendering suggestions in right-to-left languages.
    
    TEST=Note the right margin of the selection border is
    correct. Run with --lang=ar and notice that RTL behavior
    makes sense.
    BUG=229016,235708
    R=dbeam@chromium.org, samarth@chromium.org
    
    Review URL: https://codereview.chromium.org/14253014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197151 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/local_ntp/local_ntp.js
+++ chrome/browser/resources/local_ntp/local_ntp.js
@@ -774,6 +774,16 @@ IframePool.prototype = {
     this.iframes_.push(iframe);
     iframe.style.top = OFF_SCREEN;
   },
+
+  /**
+   * Sets the text direction on each iframe element.
+   * @param {boolean} isRtl True if rendering rtl and false if ltr.
+   */
+  setTextDirection: function(isRtl) {
+    for (var i = 0; i < this.iframes_.length; i++) {
+      this.iframes_[i].dir = isRtl ? 'rtl' : 'ltr';
+    }
+  }
 };
 
 
@@ -1007,10 +1017,10 @@ SuggestionsBox.prototype = {
   repositionSuggestions: function() {
     // Note: This may be called before margins are ready. In that case,
     // suggestion iframes will initially be too large and then size down
-    // onresize.
+    // onmarginchange.
     var startMargin = searchboxApiHandle.startMargin;
     var totalMargin = window.innerWidth - searchboxApiHandle.width;
-    var isRtl = searchboxApiHandle.isRtl;
+    var isRtl = searchboxApiHandle.rtl;
     for (var i = 0; i < this.suggestions_.length; ++i) {
       this.suggestions_[i].reposition(isRtl, startMargin, totalMargin);
     }
@@ -1182,7 +1192,6 @@ function hideActiveSuggestions() {
  * Updates suggestions in response to a onchange or onnativesuggestions call.
  */
 function updateSuggestions() {
-  appendSuggestionStyles();
   if (pendingBox)
     pendingBox.destroy();
   pendingBox = null;
@@ -1221,15 +1230,14 @@ function updateSuggestions() {
 }
 
 /**
- * Appends a style node for suggestion properties that depend on apiHandle.
+ * Appends or replaces a style node for suggestion properties that depend on
+ * searchboxApiHandle.
  */
-function appendSuggestionStyles() {
-  if ($(IDS.SUGGESTION_STYLE))
-    return;
-
+function setSuggestionStyles() {
+  assert(window.innerWidth > 0);
   var isRtl = searchboxApiHandle.rtl;
   var startMargin = searchboxApiHandle.startMargin;
-  var style = document.createElement('style');
+  var style = $(IDS.SUGGESTION_STYLE) || document.createElement('style');
   style.type = 'text/css';
   style.id = IDS.SUGGESTION_STYLE;
   style.textContent =
@@ -1243,7 +1251,14 @@ function appendSuggestionStyles() {
       '  font: ' + searchboxApiHandle.fontSize + 'px "' +
           searchboxApiHandle.font + '";' +
       '}';
-  document.querySelector('head').appendChild(style);
+  $qs('head').appendChild(style);
+
+  if (activeBox)
+    activeBox.repositionSuggestions();
+
+  // This is bound only for initialization. Afterwards this style should only
+  // change onmarginchange.
+  window.removeEventListener('resize', setSuggestionStyles);
 }
 
 /**
@@ -1423,6 +1438,14 @@ function init() {
   searchboxApiHandle.onnativesuggestions = updateSuggestions;
   searchboxApiHandle.onchange = updateSuggestions;
   searchboxApiHandle.onkeypress = handleKeyPress;
+  // TODO(jered): Re-enable this after http://crbug.com/236492 is fixed.
+  // searchboxApiHandle.onmarginchange = setSuggestionStyles;
+  if (window.innerWidth > 0) {
+    setSuggestionStyles();
+  } else {
+    // Wait until the overlay is shown to initialize suggestion margins.
+    window.addEventListener('resize', setSuggestionStyles);
+  }
   searchboxApiHandle.onsubmit = function() {
     var value = searchboxApiHandle.value;
     if (!value) {
@@ -1434,6 +1457,7 @@ function init() {
       searchboxApiHandle.rtl ? 'rtl' : 'ltr';
   $qs('.' + CLASSES.PENDING_SUGGESTIONS_CONTAINER).dir =
       searchboxApiHandle.rtl ? 'rtl' : 'ltr';
+  iframePool.setTextDirection(searchboxApiHandle.rtl);
 
   if (fakebox) {
     // Listener for updating the fakebox focus.
