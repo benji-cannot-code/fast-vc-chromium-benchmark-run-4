de6e740464e01d14de1318023a7041e2dc1a730e
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 17:59:25 +0000

    Revert 197061 "Batches writes to the activitylog to minimize dis..."
    Error in other tests that don't have DB thread. Need to add check.
    
    > Batches writes to the activitylog to minimize disk activity
    > 
    > Some extensions make API calls more than once a second. We don't want to write to the disk that often. This patch batches writes so that we only write to disk at most every 2 minutes.
    > 
    > BUG=161002,226375
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12611032
    
    TBR=felt@chromium.org
    
    Review URL: https://codereview.chromium.org/14322019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/activity_log.cc
+++ chrome/browser/extensions/activity_log.cc
@@ -171,15 +171,11 @@ ActivityLog::ActivityLog(Profile* profile) {
   KillActivityDatabaseErrorDelegate* error_delegate =
       new KillActivityDatabaseErrorDelegate(this);
   db_->SetErrorDelegate(error_delegate);
-  ScheduleAndForget(&ActivityDatabase::Init, database_name);
+  ScheduleAndForget(&ActivityDatabase::Init,
+                    database_name);
 }
 
 ActivityLog::~ActivityLog() {
-  ScheduleAndForget(&ActivityDatabase::Close);
-}
-
-void ActivityLog::SetArgumentLoggingForTesting(bool log_arguments) {
-  testing_mode_ = log_arguments;
 }
 
 // static
@@ -421,11 +417,12 @@ void ActivityLog::GetActions(
     const int day,
     const base::Callback
         <void(scoped_ptr<std::vector<scoped_refptr<Action> > >)>& callback) {
+  if (!db_) return;
   BrowserThread::PostTaskAndReplyWithResult(
       BrowserThread::DB,
       FROM_HERE,
       base::Bind(&ActivityDatabase::GetActions,
-                 base::Unretained(db_),
+                 db_.get(),
                  extension_id,
                  day),
       callback);
@@ -474,7 +471,9 @@ void ActivityLog::OnScriptsExecuted(
 }
 
 void ActivityLog::KillActivityLogDatabase() {
-  ScheduleAndForget(&ActivityDatabase::KillDatabase);
+  if (db_) {
+    ScheduleAndForget(&ActivityDatabase::KillDatabase);
+  }
 }
 
 // static
