11d9c75d9e469063fd73e6c99c39cfe393921980
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-29 09:17:34 +0000

    DevTools: [CodeMirror] unable to type in a question mark
    Unable to type in a question mark in CodeMirror experiment because
    of correlation with Settings screen.
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/14268021
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149327 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/UIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/UIUtils.js
@@ -153,7 +153,7 @@ WebInspector.GlassPane.prototype = {
 
 WebInspector.isBeingEdited = function(element)
 {
-    if (element.hasStyleClass("text-prompt") || element.nodeName === "INPUT")
+    if (element.hasStyleClass("text-prompt") || element.nodeName === "INPUT" || element.nodeName === "TEXTAREA")
         return true;
 
     if (!WebInspector.__editingCount)
