f61caecdb2aedc69f05ba26027f53cc098ec096f
jinsukkim@chromium.org <jinsukkim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 17:05:17 +0000

    Allow infobar to have focus
    Allows infobar to have focus by dispatching key events to it. Also exposes methods for vscroll range/offset to control focus movement.
    
    BUG=226339
    
    Review URL: https://chromiumcodereview.appspot.com/14037012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197065 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentView.java
@@ -482,7 +482,11 @@ public class ContentView extends FrameLayout
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent event) {
-        return mContentViewCore.dispatchKeyEvent(event);
+        if (isFocused()) {
+            return mContentViewCore.dispatchKeyEvent(event);
+        } else {
+            return super.dispatchKeyEvent(event);
+        }
     }
 
     @Override
@@ -794,6 +798,24 @@ public class ContentView extends FrameLayout
      */
     public void exitFullscreen() {
         mContentViewCore.exitFullscreen();
+    }
+
+    /**
+     * Return content scroll y.
+     *
+     * @return The vertical scroll position in pixels.
+     */
+    public int getContentScrollY() {
+        return mContentViewCore.computeVerticalScrollOffset();
+    }
+
+    /**
+     * Return content height.
+     *
+     * @return The height of the content in pixels.
+     */
+    public int getContentHeight() {
+        return mContentViewCore.computeVerticalScrollRange();
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////////////
