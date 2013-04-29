86183450588352c18fc0ebfba7fceb463c2d6f91
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-29 14:56:46 +0000

    Fix a forward delcare of WebURLError class->struct.
    TBR=darin
    
    Review URL: https://codereview.chromium.org/14083014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149343 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/Platform/chromium/public/Platform.h
+++ third_party/WebKit/Source/Platform/chromium/public/Platform.h
@@ -78,12 +78,12 @@ class WebUnitTestSupport;
 class WebThemeEngine;
 class WebThread;
 class WebURL;
-class WebURLError;
 class WebURLLoader;
 class WebWorkerRunLoop;
 struct WebFloatPoint;
 struct WebLocalizedString;
 struct WebSize;
+struct WebURLError;
 
 class WEBKIT_EXPORT Platform {
 public:
