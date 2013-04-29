9cec55d9e6e974d65ba035693d935f104bc7c939
pasko@google.com <pasko@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 16:50:38 +0000

    Simple Cache: Test for Index operations before init.
    This is a test to augment crrev.com/195810.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/14358046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197060 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/simple/simple_index.h
+++ net/disk_cache/simple/simple_index.h
@@ -125,7 +125,7 @@ class NET_EXPORT_PRIVATE SimpleIndex
 
  private:
   FRIEND_TEST_ALL_PREFIXES(SimpleIndexTest, IsIndexFileStale);
-
+  FRIEND_TEST_ALL_PREFIXES(SimpleIndexTest, IndexSizeCorrectOnMerge);
   typedef base::Callback<void(scoped_ptr<EntrySet>, bool force_index_flush)>
       IndexCompletionCallback;
 
