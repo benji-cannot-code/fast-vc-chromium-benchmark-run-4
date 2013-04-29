4d5cb0915e3dd1ef24b94dcfdcd304d18ab63dba
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 13:37:50 +0000

    Add a command line flag for dumping trace events to VLOG
    Only affects cc_unittests at the moment.
    
    BUG=None
    R=danakj@chromium.org, jar@chromium.org, nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/14474006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197024 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_impl.h
+++ base/debug/trace_event_impl.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef BASE_DEBUG_TRACE_EVENT_IMPL_H_
 #define BASE_DEBUG_TRACE_EVENT_IMPL_H_
 
+#include <stack>
 #include <string>
 #include <vector>
 
@@ -262,7 +263,10 @@ class BASE_EXPORT TraceLog {
     RECORD_CONTINUOUSLY = 1 << 1,
 
     // Enable the sampling profiler.
-    ENABLE_SAMPLING = 1 << 2
+    ENABLE_SAMPLING = 1 << 2,
+
+    // Echo to VLOG. Events are discared.
+    ECHO_TO_VLOG = 1 << 3
   };
 
   static TraceLog* GetInstance();
@@ -497,6 +501,8 @@ class BASE_EXPORT TraceLog {
   ObserverList<EnabledStateChangedObserver> enabled_state_observer_list_;
 
   base::hash_map<int, std::string> thread_names_;
+  base::hash_map<int, std::stack<TimeTicks> > thread_event_start_times_;
+  base::hash_map<std::string, int> thread_colors_;
 
   // XORed with TraceID to make it unlikely to collide with other processes.
   unsigned long long process_id_hash_;
