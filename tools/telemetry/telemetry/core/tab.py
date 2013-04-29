bce71facfd45fa0932daab56c761b41375762fb7
marja@chromium.org <marja@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 07:31:31 +0000

    Telemetry: Make the GC action more robust.
    Some web pages, like vimeo.com, overwrite the window.gc property, and before the
    fix, the gc action would just fail.
    
    BUG=NONE
    NOTRY=true
    R=dtu@chromium.org, nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/13843005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197005 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/tab.py
+++ tools/telemetry/telemetry/core/tab.py
@@ -96,3 +96,6 @@ class Tab(web_contents.WebContents):
   def GetCookieByName(self, name, timeout=DEFAULT_TAB_TIMEOUT):
     """Returns the value of the cookie by the given |name|."""
     return self._inspector_backend.GetCookieByName(name, timeout)
+
+  def CollectGarbage(self):
+    self._inspector_backend.CollectGarbage()
