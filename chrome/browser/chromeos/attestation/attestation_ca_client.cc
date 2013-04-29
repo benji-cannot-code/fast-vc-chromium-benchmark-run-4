a96b17b31124f1ceb8a29559ec2499c0f3252ff6
dkrahn@google.com <dkrahn@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-29 22:51:28 +0000

    Added appropriate load flags to AttestationCAClient.
    Communication with the CA does not use cookies or cache.  Load flags to
    this effect are now explicitly set on the URLFetcher.
    
    BUG=chromium:219959
    TEST=unit
    
    Review URL: https://chromiumcodereview.appspot.com/14522016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197175 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/attestation/attestation_ca_client.cc
+++ chrome/browser/chromeos/attestation/attestation_ca_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/browser_process.h"
 #include "googleurl/src/gurl.h"
+#include "net/base/load_flags.h"
 #include "net/http/http_status_code.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
@@ -83,6 +84,9 @@ void AttestationCAClient::FetchURL(const std::string& url,
                                                      net::URLFetcher::POST,
                                                      this);
   fetcher->SetRequestContext(g_browser_process->system_request_context());
+  fetcher->SetLoadFlags(net::LOAD_DO_NOT_SEND_COOKIES |
+                        net::LOAD_DO_NOT_SAVE_COOKIES |
+                        net::LOAD_DISABLE_CACHE);
   fetcher->SetUploadData(kMimeContentType, request);
   pending_requests_[fetcher] = on_response;
   fetcher->Start();
