d51bfa22b1ada672b3f2fc2abb4226189e7b430c
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-30 04:16:38 +0000

    Remove dead code in MainThread
    Lots of code in MainThread is left over from Apple's
    main thread implementation but is unused now in Blink
    so we can remove it.
    
    Review URL: https://chromiumcodereview.appspot.com/13918019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149407 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.h
+++ third_party/WebKit/Source/core/dom/Node.h
@@ -836,7 +836,7 @@ inline void Node::setParentOrShadowHostNode(ContainerNode* parent)
 
 inline ContainerNode* Node::parentOrShadowHostNode() const
 {
-    ASSERT(isMainThreadOrGCThread());
+    ASSERT(isMainThread());
     return m_parentOrShadowHostNode;
 }
 
