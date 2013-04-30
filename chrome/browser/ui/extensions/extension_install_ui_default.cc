76d423e7176489ce4953c6259be72e27fee7b905
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-30 04:51:13 +0000

    Fix build break.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/extensions/extension_install_ui_default.cc
+++ chrome/browser/ui/extensions/extension_install_ui_default.cc
@@ -169,7 +169,7 @@ void ExtensionInstallUI::OpenAppInstalledUI(Profile* profile,
 
 // static
 void ExtensionInstallUI::DisableFailureUIForTests() {
-  disable_failure_ui_for_tests = true;
+  disable_failure_ui_for_tests_ = true;
 }
 
 // static
@@ -255,7 +255,7 @@ void ExtensionInstallUIDefault::OnInstallSuccess(const Extension* extension,
 void ExtensionInstallUIDefault::OnInstallFailure(
     const extensions::CrxInstallerError& error) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  if (disable_failure_ui_for_tests || skip_post_install_ui_)
+  if (disable_failure_ui_for_tests_ || skip_post_install_ui_)
     return;
 
   Browser* browser = chrome::FindLastActiveWithProfile(profile_,
