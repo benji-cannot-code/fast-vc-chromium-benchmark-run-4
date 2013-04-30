9f1c832b74776997f4fe9007bfe9b6396c880724
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-30 23:58:15 +0000

    Revert 197281 "Add ScopedTestingManifestHandlerRegistry for Mani..." Because it causes unit tests to crash when run in a certain order.
    > Add ScopedTestingManifestHandlerRegistry for ManifestHandlerTest.
    > 
    > All the other unit tests can assume a (forthcoming) well-initialized registry, but this one tests the registry specifically.
    > 
    > BUG=229467
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14061029
    
    TBR=yoz@chromium.org
    
    Review URL: https://codereview.chromium.org/14722006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/manifest_handler_unittest.cc
+++ chrome/common/extensions/manifest_handler_unittest.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/files/file_path.h"
-#include "base/memory/scoped_ptr.h"
 #include "base/stl_util.h"
 #include "base/utf_string_conversions.h"
 #include "chrome/common/extensions/extension.h"
@@ -26,20 +25,6 @@ std::vector<std::string> SingleKey(const std::string& key) {
 }
 
 }  // namespace
-
-class ScopedTestingManifestHandlerRegistry {
- public:
-  ScopedTestingManifestHandlerRegistry() {
-    old_registry_ = ManifestHandlerRegistry::SetForTesting(&registry_);
-  }
-
-  ~ScopedTestingManifestHandlerRegistry() {
-    ManifestHandlerRegistry::SetForTesting(old_registry_);
-  }
-
-  ManifestHandlerRegistry registry_;
-  ManifestHandlerRegistry* old_registry_;
-};
 
 class ManifestHandlerTest : public testing::Test {
  public:
@@ -99,6 +84,7 @@ class ManifestHandlerTest : public testing::Test {
     std::vector<std::string> prereqs_;
     ParsingWatcher* watcher_;
 
+   private:
     virtual const std::vector<std::string> Keys() const OVERRIDE {
       return keys_;
     }
@@ -162,15 +148,18 @@ class ManifestHandlerTest : public testing::Test {
       return keys_;
     }
 
- protected:
     bool return_value_;
     bool always_validate_;
     std::vector<std::string> keys_;
   };
+
+ protected:
+  virtual void TearDown() OVERRIDE {
+    ManifestHandler::ClearRegistryForTesting();
+  }
 };
 
 TEST_F(ManifestHandlerTest, DependentHandlers) {
-  ScopedTestingManifestHandlerRegistry registry;
   ParsingWatcher watcher;
   std::vector<std::string> prereqs;
   (new TestManifestHandler("A", SingleKey("a"), prereqs, &watcher))->Register();
@@ -211,7 +200,6 @@ TEST_F(ManifestHandlerTest, DependentHandlers) {
 }
 
 TEST_F(ManifestHandlerTest, FailingHandlers) {
-  ScopedTestingManifestHandlerRegistry registry;
   // Can't use ExtensionBuilder, because this extension will fail to
   // be parsed.
   scoped_ptr<base::DictionaryValue> manifest_a(
@@ -248,7 +236,6 @@ TEST_F(ManifestHandlerTest, FailingHandlers) {
 }
 
 TEST_F(ManifestHandlerTest, Validate) {
-  ScopedTestingManifestHandlerRegistry registry;
   scoped_refptr<Extension> extension = ExtensionBuilder()
       .SetManifest(DictionaryBuilder()
                    .Set("name", "no name")
