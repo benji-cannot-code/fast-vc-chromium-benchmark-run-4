fae0b8436e2a12cd49685a6da1b0e0ef243d9087
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-30 21:43:22 +0000

    Add voice input to virtual keyboard
    BUG=235932
    
    Review URL: https://chromiumcodereview.appspot.com/14547002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197484 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/resources/layout_us.js
+++ ui/keyboard/resources/layout_us.js
@@ -49,6 +49,7 @@ var KEYS_US = [
     new SvgKey('backspace', 'Backspace', true /* repeat */)
   ],
   [
+    new MicKey(),
     new SymbolKey(),
     new SpecialKey('comma', ',', ','),
     new SpecialKey('space', ' ', 'Spacebar'),
