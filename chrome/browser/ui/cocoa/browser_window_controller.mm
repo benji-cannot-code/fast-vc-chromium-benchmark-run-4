7ec6c772d32c8e1dee303fa0160dd4d3491ceafe
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-30 19:38:53 +0000

    DevTools: [Mac] dispatch Blur / Focus events on devtools window when it is docked.
    Before the change, selected tab's web contents was activated upon browser window focus gain regardless of whether it had docked devtools window that actually had focus upon blur.
    
    BUG=236433
    TEST=manual, monitorEvents(window, 'focus') in the docked DevTools window to see them flowing now.
    
    Review URL: https://chromiumcodereview.appspot.com/14106006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197427 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser_window_controller.mm
+++ chrome/browser/ui/cocoa/browser_window_controller.mm
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_editor.h"
 #include "chrome/browser/bookmarks/bookmark_model_factory.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/devtools/devtools_window.h"
 #include "chrome/browser/managed_mode/managed_mode.h"
 #include "chrome/browser/profiles/avatar_menu_model.h"
 #include "chrome/browser/profiles/profile.h"
@@ -629,6 +630,18 @@ enum {
   // selected WebContents's RenderWidgetHostView and tell it to activate.
   if (WebContents* contents =
           browser_->tab_strip_model()->GetActiveWebContents()) {
+
+    DevToolsWindow* devtoolsWindow =
+        DevToolsWindow::GetDockedInstanceForInspectedTab(contents);
+    if (devtoolsWindow) {
+      RenderWidgetHostView* devtoolsView =
+          devtoolsWindow->web_contents()->GetRenderWidgetHostView();
+      if (devtoolsView->HasFocus()) {
+        devtoolsView->SetActive(true);
+        return;
+      }
+    }
+
     if (RenderWidgetHostView* rwhv = contents->GetRenderWidgetHostView())
       rwhv->SetActive(true);
   }
@@ -647,6 +660,18 @@ enum {
   // selected WebContents's RenderWidgetHostView and tell it to deactivate.
   if (WebContents* contents =
           browser_->tab_strip_model()->GetActiveWebContents()) {
+
+    DevToolsWindow* devtoolsWindow =
+        DevToolsWindow::GetDockedInstanceForInspectedTab(contents);
+    if (devtoolsWindow) {
+      RenderWidgetHostView* devtoolsView =
+          devtoolsWindow->web_contents()->GetRenderWidgetHostView();
+      if (devtoolsView->HasFocus()) {
+        devtoolsView->SetActive(false);
+        return;
+      }
+    }
+
     if (RenderWidgetHostView* rwhv = contents->GetRenderWidgetHostView())
       rwhv->SetActive(false);
   }
