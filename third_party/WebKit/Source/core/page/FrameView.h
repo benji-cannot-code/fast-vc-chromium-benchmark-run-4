0450d1acbae67624a21fea9d7c655907f27d6792
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-30 21:31:13 +0000

    Remove remaining instances of ENABLE(THREADED_SCROLLING)
    Review URL: https://chromiumcodereview.appspot.com/14635002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149469 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/FrameView.h
+++ third_party/WebKit/Source/core/page/FrameView.h
@@ -368,8 +368,6 @@ private:
     void updateCanBlitOnScrollRecursively();
     bool contentsInCompositedLayer() const;
 
-    bool shouldUpdateFixedElementsAfterScrolling();
-
     void applyOverflowToViewport(RenderObject*, ScrollbarMode& hMode, ScrollbarMode& vMode);
     void applyPaginationToViewport();
 
