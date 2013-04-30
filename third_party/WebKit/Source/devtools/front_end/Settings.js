818bfa1a03f16557fd63fd9376cb7426bb2d1830
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-30 16:31:28 +0000

    Revert "DevTools: Make CodeMirrorTextEditor a default editor"
    This reverts commit 9fd6353c2a59ad0f64f636cf7bb027ccb519bee5.
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/14645007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149438 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Settings.js
+++ third_party/WebKit/Source/devtools/front_end/Settings.js
@@ -111,6 +111,7 @@ WebInspector.Settings = function()
     this.visiblePanels = this.createSetting("visiblePanels", {});
     this.shortcutPanelSwitch = this.createSetting("shortcutPanelSwitch", false);
     this.portForwardings = this.createSetting("portForwardings", []);
+    this.codemirror = this.createSetting("codemirror", false);
 }
 
 WebInspector.Settings.prototype = {
