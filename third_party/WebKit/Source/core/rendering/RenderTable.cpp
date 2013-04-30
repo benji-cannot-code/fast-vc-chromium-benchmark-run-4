a6ae5abfb596ffa732aea18a497f19634b7f8162
cbiesinger@chromium.org <cbiesinger@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-30 22:13:10 +0000

    Add asserts to baselinePosition implementations
    This makes it clear what LinePositionMode they are expecting,
    which helps understanding the code.
    
    R=leviw@chromium.org, ojan@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14697002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderTable.cpp
+++ third_party/WebKit/Source/core/rendering/RenderTable.cpp
@@ -1326,6 +1326,7 @@ void RenderTable::updateFirstLetter()
 
 int RenderTable::baselinePosition(FontBaseline baselineType, bool firstLine, LineDirectionMode direction, LinePositionMode linePositionMode) const
 {
+    ASSERT(linePositionMode == PositionOnContainingLine);
     LayoutUnit baseline = firstLineBoxBaseline();
     if (baseline != -1)
         return baseline;
