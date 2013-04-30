1785af2b929d30f86dc1eba1cb9c242013f78f75
felt@chromium.org <felt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-30 00:52:59 +0000

    Batches writes to the activitylog to minimize disk activity
    Some extensions make API calls more than once a second. We don't want to write to the disk that often. This patch batches writes so that we only write to disk at most every 2 minutes.
    
    BUG=161002,226375
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=197061
    
    Review URL: https://chromiumcodereview.appspot.com/12611032
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197191 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/activity_log_unittest.cc
+++ chrome/browser/extensions/activity_log_unittest.cc
@@ -44,6 +44,8 @@ class ActivityLogTest : public ChromeRenderViewHostTestHarness {
             &command_line, base::FilePath(), false);
     CommandLine::ForCurrentProcess()->AppendSwitch(
         switches::kEnableExtensionActivityUI);
+    CommandLine::ForCurrentProcess()->AppendSwitch(
+        switches::kEnableExtensionActivityLogTesting);
     ActivityLog::RecomputeLoggingIsEnabled();
   }
 
@@ -140,6 +142,7 @@ TEST_F(ActivityLogTest, LogAndFetchActions) {
 
 TEST_F(ActivityLogTest, LogWithoutArguments) {
   ActivityLog* activity_log = ActivityLog::GetInstance(profile_);
+  activity_log->SetArgumentLoggingForTesting(false);
   scoped_refptr<const Extension> extension =
       ExtensionBuilder()
           .SetManifest(DictionaryBuilder()
