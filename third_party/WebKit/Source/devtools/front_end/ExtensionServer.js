929bd1d5aabb44f2651d2bfab86d40150dff3235
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-04-30 04:10:33 +0000

    Revert 149365 "Fixes #236053, [devtools] ExtensionAPI.js apiPrivate.Commands.Reload is undefined"
    Breaks ExtensionApiTestWithSwitch.ExtensionDebugger
    
    TBR=johnbarton@chromium.org
    BUG=236053
    
    Review URL: https://codereview.chromium.org/14609002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149404 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ExtensionServer.js
+++ third_party/WebKit/Source/devtools/front_end/ExtensionServer.js
@@ -727,7 +727,6 @@ WebInspector.ExtensionServer.prototype = {
 
     _registerHandler: function(command, callback)
     {
-        console.assert(command);
         this._handlers[command] = callback;
     },
 
