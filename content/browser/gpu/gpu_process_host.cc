40fa0bf39b71490f427d11c108ec8fb017de12d5
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-04-30 02:41:13 +0000

    Fix potential deadlock in gpu process creation
    When the GPU process crashes, the renderer compositor will try to
    recreate the context. In this case, it is possible for it to try to
    connect to an already invalid GpuProcessHost, where there are some
    race conditions.
    
    Make sure IPC replies to GpuProcessHost::EstablishGpuChannel and
    GpuProcessHost::CreateViewCommandBuffer are always called.
    
    BUG=235942
    
    Review URL: https://chromiumcodereview.appspot.com/14522025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197235 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_process_host.cc
+++ content/browser/gpu/gpu_process_host.cc
@@ -1184,6 +1184,13 @@ void GpuProcessHost::SendOutstandingReplies() {
     channel_requests_.pop();
     callback.Run(IPC::ChannelHandle(), GPUInfo());
   }
+
+  while (!create_command_buffer_requests_.empty()) {
+    CreateCommandBufferCallback callback =
+        create_command_buffer_requests_.front();
+    create_command_buffer_requests_.pop();
+    callback.Run(MSG_ROUTING_NONE);
+  }
 }
 
 void GpuProcessHost::BlockLiveOffscreenContexts() {
