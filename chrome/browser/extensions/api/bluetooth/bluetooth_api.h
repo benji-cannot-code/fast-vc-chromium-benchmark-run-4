a560ab680b4a2802e9a900a4067e9a28cf2d02fd
timurrrr@chromium.org <timurrrr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 07:20:17 +0000

    Revert 197554 "Implemented chrome.bluetooth.addProfile and chrom..."
    This has introduced a few memory leaks on the Valgrind and HeapChecker bots.
    
    > Implemented chrome.bluetooth.addProfile and chrome.bluetooth.removeProfile.
    > 
    > We will reject the registration of the profiles that have already been registered.
    > When there is an incoming connection, we will route that connection to the extension that registered the profile.
    > 
    > BUG=229636
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14311007
    
    TBR=youngki@chromium.org
    
    Review URL: https://codereview.chromium.org/14682006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197580 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/bluetooth/bluetooth_api.h
+++ chrome/browser/extensions/api/bluetooth/bluetooth_api.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/event_router.h"
 #include "chrome/browser/profiles/profile_keyed_service.h"
 #include "device/bluetooth/bluetooth_device.h"
-#include "device/bluetooth/bluetooth_profile.h"
 
 namespace device {
 
@@ -62,30 +61,20 @@ class BluetoothAddProfileFunction : public AsyncExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("bluetooth.addProfile", BLUETOOTH_ADDPROFILE)
 
-  BluetoothAddProfileFunction();
+  virtual bool RunImpl() OVERRIDE;
 
  protected:
   virtual ~BluetoothAddProfileFunction() {}
-  virtual bool RunImpl() OVERRIDE;
-
-  virtual void RegisterProfile(
-      const device::BluetoothProfile::Options& options,
-      const device::BluetoothProfile::ProfileCallback& callback);
-
- private:
-  void OnProfileRegistered(device::BluetoothProfile* bluetooth_profile);
-
-  std::string uuid_;
 };
 
-class BluetoothRemoveProfileFunction : public SyncExtensionFunction {
+class BluetoothRemoveProfileFunction : public AsyncExtensionFunction {
  public:
   DECLARE_EXTENSION_FUNCTION("bluetooth.removeProfile",
                              BLUETOOTH_REMOVEPROFILE)
+  virtual bool RunImpl() OVERRIDE;
 
  protected:
   virtual ~BluetoothRemoveProfileFunction() {}
-  virtual bool RunImpl() OVERRIDE;
 };
 
 class BluetoothGetProfilesFunction : public BluetoothExtensionFunction {
