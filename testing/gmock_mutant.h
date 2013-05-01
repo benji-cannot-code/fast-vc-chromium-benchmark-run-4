6a564f95a89c076b9b909dcf81a3494c9d9e3e2a
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 16:11:03 +0000

    testing: Use base::MessageLoop.
    TBR=stoyan@chromium.org
    BUG=236029
    
    Review URL: https://codereview.chromium.org/14305033
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197630 0039d316-1c4b-4281-b951-d872f2087c98

--- testing/gmock_mutant.h
+++ testing/gmock_mutant.h
@@ -48,8 +48,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //   }
 //
 //   void QuitMessageLoop(int seconds) {
-//     MessageLoop* loop = MessageLoop::current();
-//     loop->PostDelayedTask(FROM_HERE, MessageLoop::QuitClosure(),
+//     base::MessageLoop* loop = base::MessageLoop::current();
+//     loop->PostDelayedTask(FROM_HERE, base::MessageLoop::QuitClosure(),
 //                           1000 * seconds);
 //   }
 // };
