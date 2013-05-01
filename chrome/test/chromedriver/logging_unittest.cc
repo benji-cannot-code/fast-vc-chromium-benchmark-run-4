b3440f0322214688da6944b4c33390e48c363bb5
klm@google.com <klm@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 03:24:54 +0000

    Renaming "profiler" log type to "performance". Selenium will kill the "profiler" type. Per discussion with danielwh,eranm,shs96c.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14656002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/logging_unittest.cc
+++ chrome/test/chromedriver/logging_unittest.cc
@@ -14,13 +14,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 TEST(Logging, CreatePerformanceLogger) {
   Capabilities capabilities;
   capabilities.logging_prefs.reset(new base::DictionaryValue());
-  capabilities.logging_prefs->SetString("profiler", "INFO");
+  capabilities.logging_prefs->SetString("performance", "INFO");
 
   ScopedVector<DevToolsEventLogger> loggers;
   Status status = CreateLoggers(capabilities, &loggers);
   ASSERT_TRUE(status.IsOk());
   ASSERT_EQ(1u, loggers.size());
-  ASSERT_STREQ("profiler", loggers[0]->GetLogType().c_str());
+  ASSERT_STREQ("performance", loggers[0]->GetLogType().c_str());
 }
 
 TEST(Logging, InvalidLogType) {
