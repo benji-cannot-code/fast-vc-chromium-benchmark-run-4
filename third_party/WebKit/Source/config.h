3ed61c834e004ec5832fb19041a3d56e94dd4252
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-01 21:02:17 +0000

    Chop off even more unused bits from config.h.
    HAVE_FUNC_USLEEP was only used in ContainerNode::setActive(), but that code
    was deleted in https://codereview.chromium.org/13652013 .
    
    BUG=none
    TBR=dglazkov
    
    Review URL: https://chromiumcodereview.appspot.com/14784004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/config.h
+++ third_party/WebKit/Source/config.h
@@ -22,10 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <wtf/Platform.h>
 
-#ifdef __APPLE__
-#define HAVE_FUNC_USLEEP 1
-#endif /* __APPLE__ */
-
 #if OS(WINDOWS)
 
 // If we don't define these, they get defined in windef.h.
