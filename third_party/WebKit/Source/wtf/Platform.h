2c3af8e11a2062d0e5b34485d7e1cd1544f4562c
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-01 23:47:49 +0000

    Remove some unused #define in Platform.h
    HAVE_COMPOSITOR_FILTER_OUTSETS was a left over from a rollout.
    The other ones must have been used by other platforms and are
    no longer required.
    
    Review URL: https://chromiumcodereview.appspot.com/14786003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149553 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Platform.h
+++ third_party/WebKit/Source/wtf/Platform.h
@@ -354,7 +354,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if OS(DARWIN)
 #define WTF_USE_CF 1
-#define WTF_USE_WK_SCROLLBAR_PAINTER 1
 
 /* We can't override the global operator new and delete on OS(DARWIN) because
  * some object are allocated by WebKit and deallocated by the embedder. */
@@ -379,14 +378,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_MMAP 1   
 #define HAVE_SIGNAL_H 1
 #define HAVE_STRINGS_H 1
-#define HAVE_SYS_PARAM_H 1
 #define HAVE_SYS_TIME_H 1 
 #define WTF_USE_PTHREADS 1
 #endif /* OS(UNIX) */
-
-#if OS(UNIX) && !OS(ANDROID)
-#define HAVE_LANGINFO_H 1
-#endif
 
 #if (OS(FREEBSD) || OS(OPENBSD)) && !defined(__GLIBC__)
 #define HAVE_PTHREAD_NP_H 1
@@ -411,13 +405,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if OS(DARWIN)
-#define HAVE_MERGESORT 1
 #define HAVE_SYS_TIMEB_H 1
-#define WTF_USE_ACCELERATE 1
 #define HAVE_DISPATCH_H 1
 #define HAVE_MADV_FREE 1
 #define HAVE_PTHREAD_SETNAME_NP 1
-#define HAVE_HOSTED_CORE_ANIMATION 1
 #define HAVE_MADV_FREE_REUSE 1
 #endif /* OS(DARWIN) */
 
@@ -426,12 +417,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define HAVE_ALIGNED_MALLOC 1
 #define HAVE_ISDEBUGGERPRESENT 1
 #define HAVE_VIRTUALALLOC 1
-#endif
-
-/* FIXME: When all platforms' compositors can compute their own filter outsets, we should remove this define. 
-   https://bugs.webkit.org/show_bug.cgi?id=112830 */
-#if USE(CG)
-#define HAVE_COMPOSITOR_FILTER_OUTSETS 1
 #endif
 
 #if !defined(ENABLE_THREADING_LIBDISPATCH) && HAVE(DISPATCH_H)
