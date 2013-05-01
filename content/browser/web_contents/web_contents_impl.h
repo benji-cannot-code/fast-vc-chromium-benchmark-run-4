00e74bd2ba2e1da1a74608556e2d67534ca1fb13
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 22:16:30 +0000

    Revert 197717 "Create a frame tree in WebContents on the browser..." for DCHECK fail on mac browser_tests.
    > Create a frame tree in WebContents on the browser side.
    > 
    > The first step to supporting out-of-process iframes is to replicate the frame tree for each page to the browser process. This CL adds a FrameTreeNode class and forms a tree of such nodes in WebContents. Follow up changes will utilize this class to hold frame-specific functionality.
    > 
    > BUG=235879
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14374002
    
    TBR=nasko@chromium.org
    
    Review URL: https://codereview.chromium.org/14598013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197727 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_impl.h
+++ content/browser/web_contents/web_contents_impl.h
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
 #include "base/process.h"
-#include "content/browser/renderer_host/frame_tree_node.h"
 #include "content/browser/renderer_host/render_view_host_delegate.h"
 #include "content/browser/renderer_host/render_widget_host_delegate.h"
 #include "content/browser/web_contents/navigation_controller_impl.h"
@@ -173,10 +172,6 @@ class CONTENT_EXPORT WebContentsImpl
 
   // Invoked before a form repost warning is shown.
   void NotifyBeforeFormRepostWarningShow();
-
-  FrameTreeNode* GetFrameTreeRootForTesting() {
-    return frame_tree_root_.get();
-  }
 
   // WebContents ------------------------------------------------------
   virtual WebContentsDelegate* GetDelegate() OVERRIDE;
@@ -482,7 +477,6 @@ class CONTENT_EXPORT WebContentsImpl
   FRIEND_TEST_ALL_PREFIXES(WebContentsImplTest,
                            CrossSiteCantPreemptAfterUnload);
   FRIEND_TEST_ALL_PREFIXES(WebContentsImplTest, PendingContents);
-  FRIEND_TEST_ALL_PREFIXES(WebContentsImplTest, FrameTreeShape);
   FRIEND_TEST_ALL_PREFIXES(FormStructureBrowserTest, HTMLFiles);
   FRIEND_TEST_ALL_PREFIXES(NavigationControllerTest, HistoryNavigate);
   FRIEND_TEST_ALL_PREFIXES(RenderViewHostManagerTest, PageDoesBackAndReload);
@@ -583,10 +577,7 @@ class CONTENT_EXPORT WebContentsImpl
                           const std::vector<SkBitmap>& bitmaps);
   void OnUpdateFaviconURL(int32 page_id,
                           const std::vector<FaviconURL>& candidates);
-  void OnFrameAttached(int64 parent_frame_id,
-                       int64 frame_id,
-                       const std::string& frame_name);
-  void OnFrameDetached(int64 parent_frame_id, int64 frame_id);
+  void OnFrameDetached(int64 frame_id);
 
   // Changes the IsLoading state and notifies delegate as needed
   // |details| is used to provide details on the load that just finished
@@ -695,8 +686,6 @@ class CONTENT_EXPORT WebContentsImpl
 
   RenderViewHostImpl* GetRenderViewHostImpl();
 
-  FrameTreeNode* FindFrameTreeNodeByID(int64 frame_id);
-
   // Removes browser plugin embedder if there is one.
   void RemoveBrowserPluginEmbedder();
 
@@ -799,9 +788,6 @@ class CONTENT_EXPORT WebContentsImpl
 
   // True if this is a secure page which displayed insecure content.
   bool displayed_insecure_content_;
-
-  // The frame tree structure of the current page.
-  scoped_ptr<FrameTreeNode> frame_tree_root_;
 
   // Data for misc internal state ----------------------------------------------
 
