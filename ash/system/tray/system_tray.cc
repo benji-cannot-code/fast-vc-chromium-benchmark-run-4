2fc8312b9662e3e78c944ca064df1e98347d3df8
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 00:22:01 +0000

    Changes the background color of status area.
    As is described in crbug.com/235992, the new status area will be:
    - as-is dark background, hover changes color gradually
    - when click, the background changes to blue color
    - the color and behavior should be shared among web notification and
      tray
    - the height should be same.
    
    This CL doesn't change the user icon layout. It will be done in
    another CL.
    
    BUG=235992
    R=derat@chromium.org, stevenjb@chromium.org
    
    Review URL: https://codereview.chromium.org/14198020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197529 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray.cc
+++ ash/system/tray/system_tray.cc
@@ -335,6 +335,7 @@ bool SystemTray::HasSystemBubbleType(SystemTrayBubble::BubbleType type) {
 void SystemTray::DestroySystemBubble() {
   system_bubble_.reset();
   detailed_item_ = NULL;
+  SetBubbleVisible(false);
 }
 
 void SystemTray::DestroyNotificationBubble() {
@@ -413,6 +414,7 @@ void SystemTray::ShowItems(const std::vector<SystemTrayItem*>& items,
     SystemTrayBubble* bubble = new SystemTrayBubble(this, items, bubble_type);
     system_bubble_.reset(new internal::SystemBubbleWrapper(bubble));
     system_bubble_->InitView(this, tray_container(), &init_params);
+    SetBubbleVisible(true);
   }
   // Save height of default view for creating detailed views directly.
   if (!detailed)
@@ -476,6 +478,7 @@ void SystemTray::SetShelfAlignment(ShelfAlignment alignment) {
   UpdateAfterShelfAlignmentChange(alignment);
   // Destroy any existing bubble so that it is rebuilt correctly.
   system_bubble_.reset();
+  SetBubbleVisible(false);
   // Rebuild any notification bubble.
   if (notification_bubble_) {
     notification_bubble_.reset();
