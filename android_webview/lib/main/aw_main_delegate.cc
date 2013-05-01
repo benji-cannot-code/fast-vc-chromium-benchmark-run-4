0a3da2818a6c312bac4b43cd508d564fc164d296
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 22:28:14 +0000

    Enable WGC3DIPCBI GLContextVirtual in Android WebView
    No-op change for now since WGC3DIPCBI is not used yet. But we
    will. Also stops https://codereview.chromium.org/14256014/
    from crashing.
    
    BUG=166777
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/13979016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197732 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/lib/main/aw_main_delegate.cc
+++ android_webview/lib/main/aw_main_delegate.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_main_runner.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/common/content_switches.h"
+#include "webkit/gpu/webgraphicscontext3d_in_process_command_buffer_impl.h"
 
 namespace android_webview {
 
@@ -30,6 +31,9 @@ AwMainDelegate::~AwMainDelegate() {
 
 bool AwMainDelegate::BasicStartupComplete(int* exit_code) {
   content::SetContentClient(&content_client_);
+
+  webkit::gpu::WebGraphicsContext3DInProcessCommandBufferImpl
+      ::EnableVirtualizedContext();
 
   CommandLine* cl = CommandLine::ForCurrentProcess();
   // Set the command line to enable synchronous API compatibility.
