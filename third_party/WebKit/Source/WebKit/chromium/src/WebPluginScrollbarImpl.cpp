dae34c87187a241d17d6fc51103fd7321143666e
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-01 05:09:31 +0000

    Make Scrollbar methods that always return true return void
    Scrollbar methods mouseMoved, mouseExited, mouseUp and mouseDown
    always return true so there's no reason to return anything at all.
    
    Review URL: https://chromiumcodereview.appspot.com/14728004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPluginScrollbarImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPluginScrollbarImpl.cpp
@@ -297,7 +297,8 @@ bool WebPluginScrollbarImpl::onMouseUp(const WebInputEvent& event)
     if (m_scrollbar->pressedPart() == WebCore::NoPart)
         return false;
 
-    return m_scrollbar->mouseUp(PlatformMouseEventBuilder(m_scrollbar.get(), mouseup));
+    m_scrollbar->mouseUp(PlatformMouseEventBuilder(m_scrollbar.get(), mouseup));
+    return true;
 }
 
 bool WebPluginScrollbarImpl::onMouseMove(const WebInputEvent& event)
@@ -307,7 +308,8 @@ bool WebPluginScrollbarImpl::onMouseMove(const WebInputEvent& event)
         || m_scrollbar->pressedPart() != WebCore::NoPart) {
         mousemove.x -= m_scrollbar->x();
         mousemove.y -= m_scrollbar->y();
-        return m_scrollbar->mouseMoved(PlatformMouseEventBuilder(m_scrollbar.get(), mousemove));
+        m_scrollbar->mouseMoved(PlatformMouseEventBuilder(m_scrollbar.get(), mousemove));
+        return true;
     }
 
     if (m_scrollbar->hoveredPart() != WebCore::NoPart && !m_scrollbar->isOverlayScrollbar())
