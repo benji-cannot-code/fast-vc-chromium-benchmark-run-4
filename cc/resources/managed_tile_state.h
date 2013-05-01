70407293199ce8c9d45d420b17891c0bc9cb2098
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-01 16:09:50 +0000

    cc: Reduce special casing of transparent tiles.
    This moves the transparent tile optimization down to the renderer and removes
    isTransparent() from AnalysisCanvas. Transparent becomes a special case of solid color.
    
    BUG=236590
    
    Review URL: https://chromiumcodereview.appspot.com/14002021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197627 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/managed_tile_state.h
+++ cc/resources/managed_tile_state.h
@@ -32,7 +32,6 @@ class CC_EXPORT ManagedTileState {
       enum Mode {
         RESOURCE_MODE,
         SOLID_COLOR_MODE,
-        TRANSPARENT_MODE,
         PICTURE_PILE_MODE,
         NUM_MODES
       };
@@ -85,11 +84,6 @@ class CC_EXPORT ManagedTileState {
         mode_ = RESOURCE_MODE;
         if (memory_state_ == NOT_ALLOWED_TO_USE_MEMORY)
           memory_state_ = CAN_USE_MEMORY;
-      }
-
-      void set_transparent() {
-        mode_ = TRANSPARENT_MODE;
-        memory_state_ = NOT_ALLOWED_TO_USE_MEMORY;
       }
 
       void set_solid_color(const SkColor& color) {
--- skia/ext/analysis_canvas.h
+++ skia/ext/analysis_canvas.h
@@ -31,8 +31,8 @@ class SK_API AnalysisCanvas : public SkCanvas {
   AnalysisCanvas(AnalysisDevice*);
   virtual ~AnalysisCanvas();
 
+  // Returns true when a SkColor can be used to represent result.
   bool getColorIfSolid(SkColor* color) const;
-  bool isTransparent() const;
   bool hasText() const;
   void consumeLazyPixelRefs(LazyPixelRefList* pixelRefs);
 
@@ -70,7 +70,6 @@ class SK_API AnalysisDevice : public SkDevice {
   virtual ~AnalysisDevice();
 
   bool getColorIfSolid(SkColor* color) const;
-  bool isTransparent() const;
   bool hasText() const;
   void consumeLazyPixelRefs(LazyPixelRefList* pixelRefs);
 
