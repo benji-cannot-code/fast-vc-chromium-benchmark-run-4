0310b53902fa19b953803110cfc7b6fb3b63ccf7
kojih@chromium.org <kojih@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-02 15:17:59 +0000

    Update binding test result changed by 149578
    (I forgot to update test result)
    
    BUG=234939
    R=haraken@chromium.org
    TEST=V8TestCallback.h
    
    Review URL: https://chromiumcodereview.appspot.com/14854009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149597 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestCallback.h
+++ third_party/WebKit/Source/bindings/tests/results/V8TestCallback.h
@@ -30,8 +30,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <wtf/Forward.h>
 
 namespace WebCore {
-
 class ScriptExecutionContext;
+
 
 class V8TestCallback : public TestCallback, public ActiveDOMCallback {
 public:
