409cb184682258a1aa2415b91d2af43bde0223d7
Hokein.Wu@gmail.com <Hokein.Wu@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-02 05:21:58 +0000

    Fix a comment nit in Task Manager commands.js
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/14772004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197826 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/task_manager/commands.js
+++ chrome/browser/resources/task_manager/commands.js
@@ -19,7 +19,7 @@ TaskManagerCommands = {
   },
 
   /**
-   * Sends command to kill a process.
+   * Sends command to open about memory tab.
    */
   openAboutMemory: function() {
     chrome.send('openAboutMemory');
