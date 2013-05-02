7203c6da1361e711b1913cfeb3def4c2ff72f912
robertshield@chromium.org <robertshield@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-02 18:53:37 +0000

    Make javascript dialogs into top-level windows for desktop aura.
    Also, correct aura window sizing on desktop aura for Windows.
    
    BUG=230935, 234558, 225252
    TEST=NONE (yet)
    
    Review URL: https://chromiumcodereview.appspot.com/13896011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197949 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/win/hwnd_util.h
+++ ui/views/win/hwnd_util.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define UI_VIEWS_WIN_HWND_UTIL_H_
 
 #include "ui/gfx/native_widget_types.h"
+#include "ui/gfx/rect.h"
 #include "ui/views/views_export.h"
 
 namespace views {
@@ -26,6 +27,8 @@ VIEWS_EXPORT HWND HWNDForNativeView(gfx::NativeView view);
 // Returns the HWND for the specified NativeWindow.
 VIEWS_EXPORT HWND HWNDForNativeWindow(gfx::NativeWindow window);
 
+VIEWS_EXPORT gfx::Rect GetWindowBoundsForClientBounds(
+    View* view, const gfx::Rect& client_bounds);
 }
 
 #endif  // UI_VIEWS_WIN_HWND_UTIL_H_
