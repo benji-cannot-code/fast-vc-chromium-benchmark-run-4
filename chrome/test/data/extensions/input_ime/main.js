8be2c58dfdce6e3e7099807457422c890e57c42e
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-02 07:05:15 +0000

    Add basic scenario extension IME API browser test.
    This test does not check argument value, just ensure each even should be called if the corresponding function is called.
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/14081034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/extensions/input_ime/main.js
+++ chrome/test/data/extensions/input_ime/main.js
@@ -17,7 +17,8 @@ IMEBase.prototype = {
   onInputContextUpdate: function(context) {},
   onKeyEvent: function(context, engine, keyData) { return false; },
   onCandidateClicked: function(candidateID, button) {},
-  onMenuItemActivated: function(name) {}
+  onMenuItemActivated: function(name) {},
+  onSurroundingTextChanged: function(text, focus, anchor) {}
 };
 
 /**
@@ -26,6 +27,50 @@ IMEBase.prototype = {
  **/
 var IdentityIME = function() {};
 IdentityIME.prototype = new IMEBase();
+
+/**
+ * This class echo back called function name to chrome.
+ * @constructor
+ **/
+var EchoBackIME = function() {};
+EchoBackIME.prototype = new IMEBase();
+EchoBackIME.prototype.onActivate = function() {
+  chrome.test.sendMessage('onActivate');
+};
+
+EchoBackIME.prototype.onDeactivated = function() {
+  chrome.test.sendMessage('onDeactivated');
+};
+
+EchoBackIME.prototype.onFocus = function(context) {
+  chrome.test.sendMessage('onFocus');
+};
+
+EchoBackIME.prototype.onBlur = function(contextID) {
+  chrome.test.sendMessage('onBlur');
+};
+
+EchoBackIME.prototype.onInputContextUpdate = function(context) {
+  chrome.test.sendMessage('onInputContextUpdate');
+  console.log('onInputContextUpdate');
+};
+
+EchoBackIME.prototype.onKeyEvent = function(context, engine, keyData) {
+  chrome.test.sendMessage('onKeyEvent');
+  return false;
+};
+
+EchoBackIME.prototype.onCandidateClicked = function(candidateID, button) {
+  chrome.test.sendMessage('onCandidateClicked');
+};
+
+EchoBackIME.prototype.onMenuItemActivated = function(name) {
+  chrome.test.sendMessage('onMenuItemActivated');
+};
+
+EchoBackIME.prototype.onSurroundingTextChanged = function(text, focus, anchor) {
+  chrome.test.sendMessage('onSurroundingTextChanged');
+};
 
 /**
  * This class provides an IME which capitalize given character.
@@ -167,6 +212,16 @@ EngineBridge.prototype = {
   },
 
   /**
+   * Called from chrome.input.ime.onSurroundingTextChanged.
+   * @private
+   * @this EngineBridge
+   **/
+  onSurroundingTextChanged_: function(engineID, object) {
+    this.engineInstance_[engineID].onSurroundingTextChanged(
+        object.text, object.focus, object.anchor);
+  },
+
+  /**
    * Add engine instance for |engineID|.
    * @this EngineBridge
    **/
@@ -190,6 +245,8 @@ EngineBridge.prototype = {
         this.onCandidateClicked_.bind(this));
     chrome.input.ime.onMenuItemActivated.addListener(
         this.onMenuItemActivated_.bind(this));
+    chrome.input.ime.onSurroundingTextChanged.addListener(
+        this.onSurroundingTextChanged_.bind(this));
   }
 };
 
@@ -199,5 +256,7 @@ document.addEventListener('readystatechange', function() {
     engineBridge.Initialize();
     engineBridge.addEngine('IdentityIME', new IdentityIME());
     engineBridge.addEngine('ToUpperIME', new ToUpperIME());
+    engineBridge.addEngine('EchoBackIME', new EchoBackIME());
+    chrome.test.sendMessage('ReadyToUseImeEvent');
   }
 });
