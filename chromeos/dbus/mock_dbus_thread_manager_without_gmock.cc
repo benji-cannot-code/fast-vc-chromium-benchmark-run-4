4ef04b22153d41cca23f11bffb2e673958cd5caf
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-02 08:08:21 +0000

    dbus: Use fakes for some clients and utils for MockDBusThreadManagerWithoutGMock
    Fakes are used for
    - PowerManagerClient (with a PowerPolicyController instance)
    - SessionManagerClient
    - UpdateEngineClient
    
    BUG=234084
    TEST=trybots
    R=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/14392007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197865 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/mock_dbus_thread_manager_without_gmock.cc
+++ chromeos/dbus/mock_dbus_thread_manager_without_gmock.cc
@@ -5,6 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chromeos/dbus/mock_dbus_thread_manager_without_gmock.h"
 
+#include "chromeos/dbus/bluetooth_adapter_client.h"
+#include "chromeos/dbus/bluetooth_device_client.h"
+#include "chromeos/dbus/bluetooth_manager_client.h"
 #include "chromeos/dbus/dbus_thread_manager_observer.h"
 #include "chromeos/dbus/fake_bluetooth_adapter_client.h"
 #include "chromeos/dbus/fake_bluetooth_agent_manager_client.h"
@@ -17,14 +20,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/fake_old_bluetooth_adapter_client.h"
 #include "chromeos/dbus/fake_old_bluetooth_device_client.h"
 #include "chromeos/dbus/fake_old_bluetooth_manager_client.h"
+#include "chromeos/dbus/fake_power_manager_client.h"
+#include "chromeos/dbus/fake_session_manager_client.h"
 #include "chromeos/dbus/fake_shill_manager_client.h"
 #include "chromeos/dbus/fake_system_clock_client.h"
+#include "chromeos/dbus/fake_update_engine_client.h"
 #include "chromeos/dbus/ibus/mock_ibus_client.h"
 #include "chromeos/dbus/ibus/mock_ibus_config_client.h"
 #include "chromeos/dbus/ibus/mock_ibus_engine_factory_service.h"
 #include "chromeos/dbus/ibus/mock_ibus_engine_service.h"
 #include "chromeos/dbus/ibus/mock_ibus_input_context_client.h"
 #include "chromeos/dbus/ibus/mock_ibus_panel_service.h"
+#include "chromeos/dbus/power_policy_controller.h"
 
 namespace chromeos {
 
@@ -38,8 +45,11 @@ MockDBusThreadManagerWithoutGMock::MockDBusThreadManagerWithoutGMock()
     fake_cros_disks_client_(new FakeCrosDisksClient),
     fake_cryptohome_client_(new FakeCryptohomeClient),
     fake_image_burner_client_(new FakeImageBurnerClient),
+    fake_session_manager_client_(new FakeSessionManagerClient),
     fake_shill_manager_client_(new FakeShillManagerClient),
     fake_system_clock_client_(new FakeSystemClockClient),
+    fake_power_manager_client_(new FakePowerManagerClient),
+    fake_update_engine_client_(new FakeUpdateEngineClient),
     fake_old_bluetooth_manager_client_(new FakeOldBluetoothManagerClient),
     fake_old_bluetooth_adapter_client_(new FakeOldBluetoothAdapterClient),
     fake_old_bluetooth_device_client_(new FakeOldBluetoothDeviceClient),
@@ -50,6 +60,8 @@ MockDBusThreadManagerWithoutGMock::MockDBusThreadManagerWithoutGMock()
     mock_ibus_engine_factory_service_(new MockIBusEngineFactoryService),
     mock_ibus_panel_service_(new MockIBusPanelService),
     ibus_bus_(NULL) {
+  power_policy_controller_.reset(
+      new PowerPolicyController(this, fake_power_manager_client_.get()));
 }
 
 MockDBusThreadManagerWithoutGMock::~MockDBusThreadManagerWithoutGMock() {
@@ -213,20 +225,17 @@ PermissionBrokerClient*
 }
 
 PowerManagerClient* MockDBusThreadManagerWithoutGMock::GetPowerManagerClient() {
-  NOTIMPLEMENTED();
-  return NULL;
+  return fake_power_manager_client_.get();
 }
 
 PowerPolicyController*
 MockDBusThreadManagerWithoutGMock::GetPowerPolicyController() {
-  NOTIMPLEMENTED();
-  return NULL;
+  return power_policy_controller_.get();
 }
 
 SessionManagerClient*
     MockDBusThreadManagerWithoutGMock::GetSessionManagerClient() {
-  NOTIMPLEMENTED();
-  return NULL;
+  return fake_session_manager_client_.get();
 }
 
 SMSClient* MockDBusThreadManagerWithoutGMock::GetSMSClient() {
@@ -239,8 +248,7 @@ SystemClockClient* MockDBusThreadManagerWithoutGMock::GetSystemClockClient() {
 }
 
 UpdateEngineClient* MockDBusThreadManagerWithoutGMock::GetUpdateEngineClient() {
-  NOTIMPLEMENTED();
-  return NULL;
+  return fake_update_engine_client_.get();
 }
 
 BluetoothOutOfBandClient*
