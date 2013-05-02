50f6279faceba3f2047e256643090afed9bed973
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-02 10:13:44 +0000

    Fixed leaks in UserCloudPolicyManagerChromeOSTest.
    These tests use a TestURLRequestContextGetter for a TestinProfile, which
    need an existing IO BrowserThread to cleanup.
    
    TBR=timurrrr@chromium.org
    BUG=236901
    
    Review URL: https://chromiumcodereview.appspot.com/14792002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197881 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos_unittest.cc
+++ chrome/browser/chromeos/policy/user_cloud_policy_manager_chromeos_unittest.cc
@@ -74,6 +74,7 @@ class UserCloudPolicyManagerChromeOSTest : public testing::Test {
  protected:
   UserCloudPolicyManagerChromeOSTest()
       : ui_thread_(content::BrowserThread::UI, &loop_),
+        io_thread_(content::BrowserThread::IO, &loop_),
         store_(NULL),
         profile_(NULL),
         signin_profile_(NULL) {}
@@ -131,6 +132,7 @@ class UserCloudPolicyManagerChromeOSTest : public testing::Test {
       manager_->RemoveObserver(&observer_);
       manager_->Shutdown();
     }
+    signin_profile_->ResetRequestContext();
   }
 
   void CreateManager(bool wait_for_fetch) {
@@ -262,6 +264,8 @@ class UserCloudPolicyManagerChromeOSTest : public testing::Test {
   // Required by the refresh scheduler that's created by the manager.
   MessageLoop loop_;
   content::TestBrowserThread ui_thread_;
+  // Required to cleanup the URLRequestContextGetter of the |signin_profile_|.
+  content::TestBrowserThread io_thread_;
 
   // Convenience policy objects.
   em::PolicyData policy_data_;
