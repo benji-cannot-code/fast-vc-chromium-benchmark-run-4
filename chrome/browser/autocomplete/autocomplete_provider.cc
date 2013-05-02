32f6dd8ff11af967e5cb629d99fe4824595911db
hfung@chromium.org <hfung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-02 22:13:18 +0000

    Add provider type and field trial support for ZeroSuggest.
    BUG=237284
    
    Review URL: https://chromiumcodereview.appspot.com/14646023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@197989 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_provider.cc
+++ chrome/browser/autocomplete/autocomplete_provider.cc
@@ -95,7 +95,7 @@ metrics::OmniboxEventProto_ProviderType AutocompleteProvider::
     case TYPE_SHORTCUTS:
       return metrics::OmniboxEventProto::SHORTCUTS;
     case TYPE_ZERO_SUGGEST:
-      // TODO: Add to OmniboxEventProto::ProviderType.
+      return metrics::OmniboxEventProto::ZERO_SUGGEST;
     default:
       NOTREACHED() << "Unhandled AutocompleteProvider::Type " << type_;
       return metrics::OmniboxEventProto::UNKNOWN_PROVIDER;
