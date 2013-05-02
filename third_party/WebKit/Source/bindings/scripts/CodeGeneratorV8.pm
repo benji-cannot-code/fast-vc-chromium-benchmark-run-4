95ba7cc1904f3dcdf853ec67365311a04d3a206c
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-02 17:22:18 +0000

    Rename IDL attributes about toV8() and wrap()
    - [SuppressToJSObject] => [DoNotGenerateToV8]
    - [NoWrapperCache] => [DoNotGenerateWrap]
    - [CustomToJSObject] => [CustomToV8]
    
    R=abarth,eseidel
    
    Review URL: https://chromiumcodereview.appspot.com/14629008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149601 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -859,8 +859,8 @@ END
 private:
 END
 
-    my $noToV8 = $interface->extendedAttributes->{"SuppressToJSObject"};
-    my $noWrap = $interface->extendedAttributes->{"NoWrapperCache"} || $noToV8;
+    my $noToV8 = $interface->extendedAttributes->{"DoNotGenerateToV8"};
+    my $noWrap = $interface->extendedAttributes->{"DoNotGenerateWrap"} || $noToV8;
     if (!$noWrap) {
         my $createWrapperArgumentType = GetPassRefPtrType($nativeType);
         AddToHeader(<<END);
@@ -880,7 +880,7 @@ public:
 
 END
 
-    my $customWrap = !!($interface->extendedAttributes->{"CustomToJSObject"});
+    my $customWrap = $interface->extendedAttributes->{"CustomToV8"};
     if ($noToV8) {
         die "Can't suppress toV8 for subclass\n" if @parents;
     } elsif ($noWrap) {
@@ -4237,7 +4237,7 @@ sub GenerateToV8Converters
     my $nativeType = shift;
     my $interfaceName = $interface->name;
 
-    if ($interface->extendedAttributes->{"NoWrapperCache"} || $interface->extendedAttributes->{"SuppressToJSObject"}) {
+    if ($interface->extendedAttributes->{"DoNotGenerateWrap"} || $interface->extendedAttributes->{"DoNotGenerateToV8"}) {
         return;
     }
 
