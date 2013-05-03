c551911c1265e0cc7259a3b5d4e45118cfce6386
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 01:07:20 +0000

    Fix memory leak in ExistingUserControllerAutoLoginTest.
    BUG=chromium:216359
    TEST=Chromium OS (valgrind)(5) doesn't show leaks in ExistingUserControllerAutoLoginTest.
    
    Review URL: https://chromiumcodereview.appspot.com/14857003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198024 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/mock_user_manager.cc
+++ chrome/browser/chromeos/login/mock_user_manager.cc
@@ -49,7 +49,9 @@ UserFlow* MockUserManager::GetUserFlow(const std::string&) const {
 }
 
 User* MockUserManager::CreatePublicAccountUser(const std::string& email) {
-  return User::CreatePublicAccountUser(email);
+  delete user_;
+  user_ = User::CreatePublicAccountUser(email);
+  return user_;
 }
 
 }  // namespace chromeos
