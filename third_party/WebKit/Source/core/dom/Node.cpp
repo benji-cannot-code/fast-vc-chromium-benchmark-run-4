d08954a7385c8f9ee69f791bd66630756719ce70
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-03 00:51:39 +0000

    Set a bit on Custom Elements on creation to simplify wrapping
    For now, this makes wrapping faster because the wrapper factory can
    check this bit instead of examining the tag name and "is" attribute.
    
    This bit will soon be required for correctness: Currently, because
    elements are only activated at creation time, there is no window in
    which the "is" attribute can be changed by script before the element
    is upgraded. Soon we will do post-hoc upgrade of custom elements
    created before their definitions are available. This bit will be
    necessary for opting elements into that processing.
    
    Committed in r149589; rolled out in r149593. Now avoids hitting assert.
    
    BUG=233775
    
    Review URL: https://chromiumcodereview.appspot.com/14834002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -2780,6 +2780,12 @@ bool Node::isUserActionElementFocused() const
     return document()->userActionElements().isFocused(this);
 }
 
+void Node::setIsCustomElement()
+{
+    ASSERT(isHTMLElement() || isSVGElement());
+    setFlag(IsCustomElement);
+}
+
 } // namespace WebCore
 
 #ifndef NDEBUG
