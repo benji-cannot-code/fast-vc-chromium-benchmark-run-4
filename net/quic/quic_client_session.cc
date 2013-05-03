226238e8f974be325bdfd2d2cf636d305db596b7
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 21:45:30 +0000

    Detect QUIC connection errors and add a test to verify that the alternate protocol is marked as broken.
    BUG=230022
    
    Review URL: https://chromiumcodereview.appspot.com/14765010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198197 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_client_session.cc
+++ net/quic/quic_client_session.cc
@@ -124,7 +124,7 @@ void QuicClientSession::OnCryptoHandshakeEvent(CryptoHandshakeEvent event) {
 
 void QuicClientSession::ConnectionClose(QuicErrorCode error, bool from_peer) {
   if (!callback_.is_null()) {
-    base::ResetAndReturn(&callback_).Run(error);
+    base::ResetAndReturn(&callback_).Run(ERR_QUIC_PROTOCOL_ERROR);
   }
   QuicSession::ConnectionClose(error, from_peer);
 }
