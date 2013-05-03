5febf7f05cc4d46bdc9f217d01f72b1f3ead9fbb
dconnelly@chromium.org <dconnelly@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 11:28:18 +0000

    Fix some clang compilation errors due to chromium-style.
    This morning a fresh Chromium checkout failed to compile under Clang:
    error: [chromium-style] Overriding method must have "virtual" keyword
    
    BUG=
    TBR=dcheng,pkasting
    
    Review URL: https://chromiumcodereview.appspot.com/14852005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198092 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/aura/tabs/dock_info_aurax11.cc
+++ chrome/browser/ui/aura/tabs/dock_info_aurax11.cc
@@ -42,7 +42,7 @@ class BaseWindowFinder : public ui::EnumerateWindowsDelegate {
   }
 
   // Returns true if iteration should stop, false otherwise.
-  virtual bool ShouldStopIterating(XID window) {
+  virtual bool ShouldStopIterating(XID window) OVERRIDE {
     return false;
   }
 
@@ -69,7 +69,7 @@ class TopMostFinder : public BaseWindowFinder {
   }
 
  protected:
-  virtual bool ShouldStopIterating(XID window) {
+  virtual bool ShouldStopIterating(XID window) OVERRIDE {
     if (BaseWindowFinder::ShouldIgnoreWindow(window))
       return false;
 
@@ -136,7 +136,7 @@ class LocalProcessWindowFinder : public BaseWindowFinder {
   }
 
  protected:
-  virtual bool ShouldStopIterating(XID window) {
+  virtual bool ShouldStopIterating(XID window) OVERRIDE {
     if (BaseWindowFinder::ShouldIgnoreWindow(window))
       return false;
 
