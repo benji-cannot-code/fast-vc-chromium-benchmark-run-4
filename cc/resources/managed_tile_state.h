6de6b9a5e45542cbce586afbd855f7c9f99973c7
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 05:32:59 +0000

    cc: Remove TileManager::GatherPixelRefsForTile.
    This function is no longer needed as it's now cheap to iterate over
    a tile's pixel refs. This also moves the recording of pixel ref
    gathering related stats to the Picture class and the main thread
    where the real cost is paid.
    
    BUG=237593
    
    Review URL: https://chromiumcodereview.appspot.com/14712006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198068 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/managed_tile_state.h
+++ cc/resources/managed_tile_state.h
@@ -6,8 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CC_RESOURCES_MANAGED_TILE_STATE_H_
 #define CC_RESOURCES_MANAGED_TILE_STATE_H_
 
-#include <list>
-
+#include "base/hash_tables.h"
 #include "base/memory/scoped_ptr.h"
 #include "cc/resources/platform_color.h"
 #include "cc/resources/resource_pool.h"
@@ -112,8 +111,8 @@ class CC_EXPORT ManagedTileState {
   scoped_ptr<base::Value> AsValue() const;
 
   // Persisted state: valid all the time.
-  bool need_to_gather_pixel_refs;
-  std::list<skia::LazyPixelRef*> pending_pixel_refs;
+  typedef base::hash_set<uint32_t> PixelRefSet;
+  PixelRefSet decoded_pixel_refs;
   DrawingInfo drawing_info;
   PicturePileImpl::Analysis picture_pile_analysis;
   bool picture_pile_analyzed;
