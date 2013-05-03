7c71d5ab2589bd63a809900820057c826017e416
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 11:26:38 +0000

    Support extension-based file-handlers for packaged apps.
    This rolls forward r197909 but without the windows breakage.
    Previously, file_handlers could only specify the file types they
    accepted by MIME type. This limited apps to only accepting types known
    to the local operating system or accepting any unknown type. This change
    allows file_handlers to restrict types by file extension.
    
    BUG=167950
    TEST=browser_tests PlatformAppBrowserTest.Launch*
    
    Review URL: https://chromiumcodereview.appspot.com/14887004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198091 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/extension_manifest_constants.h
+++ chrome/common/extensions/extension_manifest_constants.h
@@ -45,6 +45,7 @@ namespace extension_manifest_keys {
   extern const char kExternallyConnectable[];
   extern const char kFileAccessList[];
   extern const char kFileHandlers[];
+  extern const char kFileHandlerExtensions[];
   extern const char kFileHandlerTitle[];
   extern const char kFileHandlerTypes[];
   extern const char kFileFilters[];
@@ -242,6 +243,9 @@ namespace extension_manifest_errors {
   extern const char kInvalidFileFiltersList[];
   extern const char kInvalidFileFilterValue[];
   extern const char kInvalidFileHandlers[];
+  extern const char kInvalidFileHandlerExtension[];
+  extern const char kInvalidFileHandlerExtensionElement[];
+  extern const char kInvalidFileHandlerNoTypeOrExtension[];
   extern const char kInvalidFileHandlerTitle[];
   extern const char kInvalidFileHandlerType[];
   extern const char kInvalidFileHandlerTypeElement[];
