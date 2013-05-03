82305737eb7f726f7708d23c0e363a2b18aa2999
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 22:15:21 +0000

    Omnibox: Set Up Framework for Frecency Field Trial
    Remove current HQPReplaceHUP field trial and NewScoring field trial.
    
    Replace them with one common field trial that controls them both.  This
    is useful because new scoring is probably more useful in HQP-replace-HUP
    mode.  Hence, I want to test all 4 combinations of on-and-off simultaneously.
    This new trials allows me to direct users to each of those four groups
    separately.
    
    The trial is not created on the client.  It will be downloaded through
    the variations server.  Hence, I can decide the probabilities at a later
    date.  I'm likely looking at 70% control, 10% in each of the other
    three arms.  I plan to run comparisons of control versus each other
    arm as well as HQP-replace-HUP against HQP-replace-HUP-plus-new-scoring.
    
    Field trial groups names will be
    Standard
    HQPReplaceHUP
    NewScoring
    HQPReplaceHUP_NewScoring
    
    Tested used --force-fieldtrials
    
    TBR=pkasting@chromium.org
    
    BUG=175751
    
    Review URL: https://codereview.chromium.org/14801007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_url_provider.cc
+++ chrome/browser/autocomplete/history_url_provider.cc
@@ -325,9 +325,7 @@ HistoryURLProvider::HistoryURLProvider(AutocompleteProviderListener* listener,
           !OmniboxFieldTrial::
               InHUPCreateShorterMatchFieldTrialExperimentGroup()),
       search_url_database_(
-          !OmniboxFieldTrial::InHQPReplaceHUPScoringFieldTrial() ||
-          !OmniboxFieldTrial::
-              InHQPReplaceHUPScoringFieldTrialExperimentGroup()) {
+          !OmniboxFieldTrial::InHQPReplaceHUPScoringExperimentGroup()) {
 }
 
 // static
