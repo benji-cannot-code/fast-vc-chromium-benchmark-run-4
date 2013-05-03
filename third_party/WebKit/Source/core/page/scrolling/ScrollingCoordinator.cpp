54c693d40a0194deaafbe7732e0fd554b4b8f6f6
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-03 05:14:57 +0000

    Remove hard-coded value from ScrollingCoordinator
    Since ScrollingCoordinator and ScrollingCoordinatorChromium have been merged,
    supportsFixedPositionLayers() always returns true. It is possible to slightly
    simplify logic in a few places by removing this boolean function.
    
    Review URL: https://chromiumcodereview.appspot.com/14674006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149638 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
+++ third_party/WebKit/Source/core/page/scrolling/ScrollingCoordinator.cpp
@@ -592,9 +592,7 @@ MainThreadScrollingReasons ScrollingCoordinator::mainThreadScrollingReasons() co
 
     if (frameView->hasSlowRepaintObjects())
         mainThreadScrollingReasons |= HasSlowRepaintObjects;
-    if (!supportsFixedPositionLayers() && frameView->hasViewportConstrainedObjects())
-        mainThreadScrollingReasons |= HasViewportConstrainedObjectsWithoutSupportingFixedLayers;
-    if (supportsFixedPositionLayers() && hasVisibleSlowRepaintViewportConstrainedObjects(frameView))
+    if (hasVisibleSlowRepaintViewportConstrainedObjects(frameView))
         mainThreadScrollingReasons |= HasNonLayerViewportConstrainedObjects;
 
     return mainThreadScrollingReasons;
