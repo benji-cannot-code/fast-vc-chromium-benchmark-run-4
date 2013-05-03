3dd313e1e50b0033a9552d37cb987147e9e10fff
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-03 22:24:18 +0000

    Remove flaky/slow fast/js/ tests
    These tests don't provide useful coverage as they're testing things
    that are under the purview of V8's tests. It's especially useless to
    run them when they're slow and/or flaky.
    
    R=ojan@chromium.org
    BUG=237872
    
    Review URL: https://codereview.chromium.org/14596006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149668 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/js/script-tests/repeat-cached-vm-reentry.js
+++
@@ -1,6 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-description(
-"This test ensures that repeated use of the vm reentry cache does not eventually consume the entire register file."
-);
-var anArray = [1,2,3,4,5];
-shouldBe("for(var i = 0; i < 50000; i++) anArray.sort(function(){ return 1; })", "[1,2,3,4,5]");
