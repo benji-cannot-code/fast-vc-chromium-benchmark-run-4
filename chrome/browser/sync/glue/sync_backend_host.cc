699e7e8368aa2b288ac2a6f2d2b3250c9bc70bef
zea@chromium.org <zea@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-03 23:01:26 +0000

    [Sync] Fix gu source generation when downloading control types
    We now properly set either NEW_CLIENT (if !HasSyncSetupCompleted) or
    NEWLY_SUPPORTED_DATATYPE otherwise.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/14884006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198220 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/sync_backend_host.cc
+++ chrome/browser/sync/glue/sync_backend_host.cc
@@ -178,7 +178,7 @@ class SyncBackendHost::Core
   // Called at startup to download the control types. Will invoke
   // DoInitialProcessControlTypes on success, and OnControlTypesDownloadRetry
   // if an error occurred.
-  void DoDownloadControlTypes();
+  void DoDownloadControlTypes(syncer::ConfigureReason reason);
 
   // Ask the syncer to check for updates for the specified types.
   void DoRefreshTypes(syncer::ModelTypeSet types);
@@ -869,12 +869,18 @@ void SyncBackendHost::HandleSyncManagerInitializationOnFrontendLoop(
   notification_registrar_.Add(this, chrome::NOTIFICATION_SYNC_REFRESH_LOCAL,
                               content::Source<Profile>(profile_));
 
+  syncer::ConfigureReason reason =
+      (sync_prefs_->HasSyncSetupCompleted() ?
+       syncer::CONFIGURE_REASON_NEWLY_ENABLED_DATA_TYPE :
+       syncer::CONFIGURE_REASON_NEW_CLIENT);
+
   // Kick off the next step in SyncBackendHost initialization by downloading
   // any necessary control types.
   sync_thread_.message_loop()->PostTask(
       FROM_HERE,
       base::Bind(&SyncBackendHost::Core::DoDownloadControlTypes,
-                 core_.get()));
+                 core_.get(),
+                 reason));
 }
 
 void SyncBackendHost::Observe(
@@ -967,7 +973,8 @@ void SyncBackendHost::Core::OnSyncCycleCompleted(
       snapshot);
 }
 
-void SyncBackendHost::Core::DoDownloadControlTypes() {
+void SyncBackendHost::Core::DoDownloadControlTypes(
+    syncer::ConfigureReason reason) {
   syncer::ModelTypeSet new_control_types = registrar_->ConfigureDataTypes(
       syncer::ControlTypes(), syncer::ModelTypeSet());
   syncer::ModelSafeRoutingInfo routing_info;
@@ -977,7 +984,7 @@ void SyncBackendHost::Core::DoDownloadControlTypes() {
             << " added; calling DoConfigureSyncer";
 
   sync_manager_->ConfigureSyncer(
-      syncer::CONFIGURE_REASON_NEW_CLIENT,
+      reason,
       new_control_types,
       syncer::ModelTypeSet(),
       routing_info,
