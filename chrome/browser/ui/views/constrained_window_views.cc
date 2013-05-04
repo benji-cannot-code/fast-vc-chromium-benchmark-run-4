3950bc00892c33f89baab81b8c4df539a0de7ad1
wittman@chromium.org <wittman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-04 00:53:42 +0000

    Disable default Aura shadow for web contents modal dialogs
    The BubbleFrameView being used for these dialogs includes shadow, so
    the Aura-provided default shadow should be disabled.
    
    BUG=166075
    
    Review URL: https://chromiumcodereview.appspot.com/14857008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198269 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/constrained_window_views.cc
+++ chrome/browser/ui/views/constrained_window_views.cc
@@ -57,6 +57,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(USE_AURA)
 #include "ui/aura/window.h"
+#include "ui/views/corewm/shadow_types.h"
 #include "ui/views/corewm/visibility_controller.h"
 #include "ui/views/corewm/window_animations.h"
 #include "ui/views/corewm/window_modality_controller.h"
@@ -649,6 +650,14 @@ views::Widget* CreateWebContentsModalDialogViews(
   }
 
   dialog->Init(params);
+
+#if defined(USE_AURA)
+  if (views::DialogDelegate::UseNewStyle()) {
+    // TODO(msw): Add a matching shadow type and remove the bubble frame border?
+    views::corewm::SetShadowType(dialog->GetNativeWindow(),
+                                 views::corewm::SHADOW_TYPE_NONE);
+  }
+#endif
 
   if (dialog_host_observer) {
     dialog_host_observer->OnPositionRequiresUpdate();
