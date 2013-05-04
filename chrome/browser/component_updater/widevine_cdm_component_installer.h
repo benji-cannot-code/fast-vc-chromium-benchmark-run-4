ca71fa63c1ca8453430742cc11405b882476ebb2
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-04 04:28:13 +0000

    Copy both Widevine CDM and the adapter during component update.
    BUG=180260
    R=brettw@chromium.org, cpu@chromium.org, ddorwin@chromium.org, thestig@chromium.org
    
    Review URL: https://codereview.chromium.org/14800002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198283 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/widevine_cdm_component_installer.h
+++ chrome/browser/component_updater/widevine_cdm_component_installer.h
@@ -8,8 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class ComponentUpdateService;
 
-// Our job is to 1) find what Widevine CDM is installed (if any) and 2) register
-// with the component updater to download the latest version when available.
+// Our job is to:
+// 1) Find what Widevine CDM is installed (if any).
+// 2) Register with the component updater to download the latest version when
+//    available.
+// 3) Copy the Widevine CDM adapter bundled with chrome to the install path.
+// 4) Register the Widevine CDM (via the adapter) with Chrome.
 // The first part is IO intensive so we do it asynchronously in the file thread.
 void RegisterWidevineCdmComponent(ComponentUpdateService* cus);
 
