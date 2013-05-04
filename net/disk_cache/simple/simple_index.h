b3468020510bf435c1f9bead112e709a46c652c8
gavinp@chromium.org <gavinp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-04 05:00:24 +0000

    Have SimpleCache index listen to android activity status notifications.
    Based on earlier work by felipeg, together with some base/android
    changes from digit, we now respond to activity status notifications by
    greatly increasing the flush latency. This should ensure that we
    almost always have a fresh index on android at startup.
    
    R=felipeg@chromium.org, pliard@chromium.org
    BUG=233536
    
    Review URL: https://codereview.chromium.org/14771009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198290 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/simple/simple_index.h
+++ net/disk_cache/simple/simple_index.h
@@ -24,6 +24,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/completion_callback.h"
 #include "net/base/net_export.h"
 
+#if defined(OS_ANDROID)
+#include "base/android/activity_status.h"
+#endif
+
 class Pickle;
 class PickleIterator;
 
@@ -155,7 +159,14 @@ class NET_EXPORT_PRIVATE SimpleIndex
   void MergeInitializingSet(scoped_ptr<EntrySet> index_file_entries,
                             bool force_index_flush);
 
+#if defined(OS_ANDROID)
+  void OnActivityStateChange(base::android::ActivityState state);
+
+  scoped_ptr<base::android::ActivityStatus::Listener> activity_status_listener_;
+#endif
+
   EntrySet entries_set_;
+
   uint64 cache_size_;  // Total cache storage size in bytes.
   uint64 max_size_;
   uint64 high_watermark_;
@@ -185,6 +196,11 @@ class NET_EXPORT_PRIVATE SimpleIndex
 
   typedef std::list<net::CompletionCallback> CallbackList;
   CallbackList to_run_when_initialized_;
+
+  // Set to true when the app is on the background. When the app is in the
+  // background we can write the index much more frequently, to insure fresh
+  // index on next startup.
+  bool app_on_background_;
 };
 
 }  // namespace disk_cache
