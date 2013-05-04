232fe25397a7f0a8400b702f825919c9fb001776
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-04 05:04:16 +0000

    Add os/arch/nacl_arch to webstore install code
    We already had it for update checks, but need it for the initial install
    requests as well.
    
    BUG=235233
    
    Review URL: https://chromiumcodereview.appspot.com/14935002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198294 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/omaha_query_params/omaha_query_params.h
+++ chrome/common/omaha_query_params/omaha_query_params.h
@@ -16,8 +16,7 @@ class OmahaQueryParams {
  public:
   enum ProdId {
     CHROME = 0,
-    CHROMECRX,
-    CHROMIUMCRX,
+    CRX,
   };
 
   // Generates a string of URL query paramaters to be used when getting
