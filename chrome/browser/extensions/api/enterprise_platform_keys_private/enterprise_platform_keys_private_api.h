e73ffe78832eabc4b5b545e2b1afd920ea397619
davidyu@chromium.org <davidyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-05 03:25:57 +0000

    Fix the issue that SignEnterpriseChallenge won't succeed if the key does not exist.
    Add PrepareKey() which calls AttestationFlow::GetCertificate() when the key
    does not exist. Also, replace all MessageLoop::PostTask() with callback.Run().
    
    BUG=chromium:236624
    
    Review URL: https://chromiumcodereview.appspot.com/14468003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h
+++ chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h
@@ -10,8 +10,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/extensions/extension_function.h"
 #include "chrome/common/extensions/api/enterprise_platform_keys_private.h"
+#include "chromeos/attestation/attestation_constants.h"
+#include "chromeos/attestation/attestation_flow.h"
+#include "chromeos/dbus/cryptohome_client.h"
 #include "chromeos/dbus/dbus_method_call_status.h"
 #include "third_party/cros_system_api/dbus/service_constants.h"
 
@@ -34,6 +38,13 @@ namespace extensions {
 
 class EPKPChallengeKeyBase : public AsyncExtensionFunction {
  protected:
+  enum PrepareKeyResult {
+    PREPARE_KEY_OK = 0,
+    PREPARE_KEY_DBUS_ERROR,
+    PREPARE_KEY_USER_REJECTED,
+    PREPARE_KEY_GET_CERTIFICATE_FAILED,
+  };
+
   EPKPChallengeKeyBase();
   virtual ~EPKPChallengeKeyBase();
 
@@ -51,10 +62,38 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
   // Returns the enterprise virtual device ID.
   std::string GetDeviceId() const;
 
+  // Prepares the key for signing. It will first check if the key exists. If
+  // the key does not exist, it will call AttestationFlow::GetCertificate() to
+  // get a new one. If require_user_consent is true, it will explicitly ask for
+  // user consent before calling GetCertificate().
+  void PrepareKey(
+      chromeos::attestation::AttestationKeyType key_type,
+      const std::string& key_name,
+      chromeos::attestation::AttestationCertificateProfile certificate_profile,
+      bool require_user_consent,
+      const base::Callback<void(PrepareKeyResult)>& callback);
+
   chromeos::CryptohomeClient* cryptohome_client_;
   cryptohome::AsyncMethodCaller* async_caller_;
+  scoped_ptr<chromeos::attestation::AttestationFlow> attestation_flow_;
 
  private:
+  void DoesKeyExistCallback(
+      chromeos::attestation::AttestationCertificateProfile certificate_profile,
+      bool require_user_consent,
+      const base::Callback<void(PrepareKeyResult)>& callback,
+      chromeos::DBusMethodCallStatus status,
+      bool result);
+  void AskForUserConsent(const base::Callback<void(bool)>& callback) const;
+  void AskForUserConsentCallback(
+      chromeos::attestation::AttestationCertificateProfile certificate_profile,
+      const base::Callback<void(PrepareKeyResult)>& callback,
+      bool result);
+  void GetCertificateCallback(
+      const base::Callback<void(PrepareKeyResult)>& callback,
+      bool success,
+      const std::string& pem_certificate_chain);
+
   policy::EnterpriseInstallAttributes* install_attributes_;
 };
 
@@ -69,6 +108,8 @@ class EPKPChallengeMachineKey : public EPKPChallengeKeyBase {
 
   void GetDeviceAttestationEnabledCallback(const std::string& challenge,
                                            bool enabled);
+  void PrepareKeyCallback(const std::string& challenge,
+                          PrepareKeyResult result);
   void SignChallengeCallback(bool success, const std::string& response);
 
   DECLARE_EXTENSION_FUNCTION(
@@ -94,11 +135,12 @@ class EPKPChallengeUserKey : public EPKPChallengeKeyBase {
   void GetDeviceAttestationEnabledCallback(const std::string& challenge,
                                            bool register_key,
                                            const std::string& domain,
+                                           bool require_user_consent,
                                            bool enabled);
-  void UserConsentCallback(const std::string& challenge,
-                           bool register_key,
-                           const std::string& domain,
-                           bool action);
+  void PrepareKeyCallback(const std::string& challenge,
+                          bool register_key,
+                          const std::string& domain,
+                          PrepareKeyResult result);
   void SignChallengeCallback(bool register_key,
                              bool success,
                              const std::string& response);
@@ -106,7 +148,6 @@ class EPKPChallengeUserKey : public EPKPChallengeKeyBase {
                            bool success,
                            cryptohome::MountError return_code);
 
-  void AskForUserConsent(const base::Callback<void(bool)>& callback);
   bool IsExtensionWhitelisted() const;
   bool IsRemoteAttestationEnabledForUser() const;
   std::string GetUserDomain() const;
