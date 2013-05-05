a5b9235e947e6a4038ee9e4977dc894fbc450334
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-05 03:30:20 +0000

    MV iframes: Add HTML/JS/CSS resources for MV iframe rendering.
    There are two parts of Most Visited tiles we need to iframe for secure
    rendering, the thumbnail image or domain element, and the title element.
    These are the two sets of HTML/JS/CSS added here.
    
    They share a common set of utilities for reading their config out of URL
    parameters passed in as an iframe src.
    
    This is split off from CL 14578005.
    It depends on a JS API added in CL 14592003.
    
    BUG=228940
    R=arv@chromium.org, jhawkins@chromium.org, palmer@chromium.org, samarth@chromium.org
    
    Review URL: https://codereview.chromium.org/14817002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198347 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/resources/local_ntp/most_visited_title.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+/**
+ * @fileoverview Rendering for iframed most visited titles.
+ */
+
+window.addEventListener('DOMContentLoaded', function() {
+  'use strict';
+
+  fillMostVisited(window.location, function(params, data) {
+    document.body.appendChild(
+        createMostVisitedLink(params, data.url, data.title, data.title));
+  });
+});
--- chrome/browser/resources/omnibox_result_loader.js
+++ chrome/browser/resources/omnibox_result_loader.js
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  */
 
 (function() {
+<include src="local_ntp/instant_iframe_validation.js">
+
 /**
  * The origin of the embedding page.
  * This string literal must be in double quotes for proper escaping.
