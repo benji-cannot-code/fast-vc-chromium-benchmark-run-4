e2163cc04eb15a6f079809467316359f0d1658f9
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 18:17:16 +0000

    Close camera when tab is closed.
    Reland https://src.chromium.org/viewvc/chrome?view=rev&revision=198386
    
    A tab can be opened by calling window.open(). In this case, RenderViewImpl receives frameDetached signal when the tab is closed. Therefore, MediaStreamImpl needs to listen to frameDetached signal in order to close all opened media stream.
    
    When a tab (opened by window.open) is closed, RenderWidget will not send message to browser process. MediaStreamDispatcher needs to have its own Send() method in order to notify browser process to close the opened media streams (i.e., devices).
    
    BUG=235126
    TBR=tommi@chromium.org
    
    Review URL: https://codereview.chromium.org/15004002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198485 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_dispatcher.cc
+++ content/renderer/media/media_stream_dispatcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop_proxy.h"
 #include "content/common/media/media_stream_messages.h"
 #include "content/renderer/media/media_stream_dispatcher_eventhandler.h"
+#include "content/renderer/render_thread_impl.h"
 #include "content/renderer/render_view_impl.h"
 #include "googleurl/src/gurl.h"
 
@@ -202,6 +203,15 @@ void MediaStreamDispatcher::CloseDevice(const std::string& label) {
            << ", {label = " << label << "}";
 
   StopStream(label);
+}
+
+bool MediaStreamDispatcher::Send(IPC::Message* message) {
+  if (!RenderThread::Get()) {
+    delete message;
+    return false;
+  }
+
+  return RenderThread::Get()->Send(message);
 }
 
 bool MediaStreamDispatcher::OnMessageReceived(const IPC::Message& message) {
