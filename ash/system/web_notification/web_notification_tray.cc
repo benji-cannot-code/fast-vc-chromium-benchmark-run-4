fe353a24e76088ef88b667fa8261ba9c5fe21ed5
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 20:18:40 +0000

    Introduces a boolean flag for ShouldBlockLauncherAutoHide().
    As described in the comment, ShouldBlockLauncherAutoHide() check
    can be called during the instantiation of message_center_bubble_
    itself, therefore check using this member will cause a flicker
    of shelf.  See the bug for the details.
    
    BUG=181213
    
    Review URL: https://chromiumcodereview.appspot.com/14887005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198520 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/tray/tray_bubble_wrapper.h"
 #include "ash/system/tray/tray_constants.h"
 #include "ash/system/tray/tray_utils.h"
+#include "base/auto_reset.h"
 #include "base/i18n/number_formatting.h"
 #include "base/utf_string_conversions.h"
 #include "grit/ash_resources.h"
@@ -174,7 +175,8 @@ WebNotificationTray::WebNotificationTray(
     : TrayBackgroundView(status_area_widget),
       button_(NULL),
       show_message_center_on_unlock_(false),
-      should_update_tray_content_(false) {
+      should_update_tray_content_(false),
+      should_block_shelf_auto_hide_(false) {
   button_ = new internal::WebNotificationButton(this);
   button_->set_triggerable_event_flags(
       ui::EF_LEFT_MOUSE_BUTTON | ui::EF_RIGHT_MOUSE_BUTTON);
@@ -201,6 +203,7 @@ bool WebNotificationTray::ShowMessageCenter() {
   if (!ShouldShowMessageCenter())
     return false;
 
+  should_block_shelf_auto_hide_ = true;
   message_center::MessageCenterBubble* message_center_bubble =
       new message_center::MessageCenterBubble(message_center());
 
@@ -246,6 +249,7 @@ void WebNotificationTray::HideMessageCenter() {
   if (!message_center_bubble())
     return;
   message_center_bubble_.reset();
+  should_block_shelf_auto_hide_ = false;
   SetBubbleVisible(false);
   show_message_center_on_unlock_ = false;
   status_area_widget()->SetHideSystemNotifications(false);
@@ -302,6 +306,7 @@ bool WebNotificationTray::ShouldShowMessageCenter() {
 }
 
 void WebNotificationTray::ShowQuietModeMenu() {
+  base::AutoReset<bool> reset(&should_block_shelf_auto_hide_, true);
   scoped_ptr<ui::MenuModel> menu_model(
       message_center_tray_->CreateQuietModeMenu());
   quiet_mode_menu_runner_.reset(new views::MenuRunner(menu_model.get()));
@@ -347,8 +352,7 @@ void WebNotificationTray::UpdateAfterLoginStatusChange(
 }
 
 bool WebNotificationTray::ShouldBlockLauncherAutoHide() const {
-  return IsMessageCenterBubbleVisible() ||
-      (quiet_mode_menu_runner_.get() && quiet_mode_menu_runner_->IsRunning());
+  return should_block_shelf_auto_hide_;
 }
 
 bool WebNotificationTray::IsMessageCenterBubbleVisible() const {
