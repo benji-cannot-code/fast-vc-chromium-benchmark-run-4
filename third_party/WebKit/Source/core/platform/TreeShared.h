10ba4ef8d1ac3a8585eb5f580b31ccb169f510a4
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-06 22:24:12 +0000

    Waste less memory in TreeShared in DEBUG builds.
    Yes, we don't care about memory in Debug builds, but we do run them on
    the bots, and this takes 4 bytes out of a Node on 32 bit platforms.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14576004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149812 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/TreeShared.h
+++ third_party/WebKit/Source/core/platform/TreeShared.h
@@ -39,14 +39,12 @@ protected:
     TreeShared()
         : m_refCount(1)
 #ifndef NDEBUG
+        , m_deletionHasBegun(false)
+        , m_inRemovedLastRefFunction(false)
         , m_adoptionIsRequired(true)
 #endif
     {
         ASSERT(isMainThread());
-#ifndef NDEBUG
-        m_deletionHasBegun = false;
-        m_inRemovedLastRefFunction = false;
-#endif
     }
 
     ~TreeShared()
@@ -95,19 +93,15 @@ public:
         return m_refCount;
     }
 
-#ifndef NDEBUG
-    bool m_deletionHasBegun;
-    bool m_inRemovedLastRefFunction;
-#endif
-
 private:
-#ifndef NDEBUG
-    friend void adopted<>(TreeShared<NodeType>*);
-#endif
-
     int m_refCount;
 
 #ifndef NDEBUG
+public:
+    bool m_deletionHasBegun;
+    bool m_inRemovedLastRefFunction;
+private:
+    friend void adopted<>(TreeShared<NodeType>*);
     bool m_adoptionIsRequired;
 #endif
 };
