2ccaf055d007cfd31efe9401d38d011303ff3df4
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 18:49:11 +0000

    CrOS online EULA: disable "Accept" button while EULA is being loaded.
    BUG=237223
    
    Review URL: https://chromiumcodereview.appspot.com/14981003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198493 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/oobe_screen_eula.js
+++ chrome/browser/resources/chromeos/login/oobe_screen_eula.js
@@ -45,6 +45,7 @@ login.createScreen('EulaScreen', 'eula', function() {
         $('accept-button').disabled = false;
         $('eula').classList.remove('eula-loading');
       }
+      $('accept-button').disabled = true;
       $('cros-eula-frame').src = 'chrome://terms';
     },
 
