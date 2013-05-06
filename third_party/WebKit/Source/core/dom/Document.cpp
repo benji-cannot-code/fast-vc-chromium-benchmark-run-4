31209ce976663283c1a2749ca965950d5874963a
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-06 17:39:50 +0000

    Use shouldRecalcStyle in Document::recalcStyle
    We should use the shouldRecalcStyle helper in Document instead
    of duplicating the logic for checking if we should call recalcStyle.
    
    Review URL: https://chromiumcodereview.appspot.com/14562004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149751 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -1665,7 +1665,7 @@ void Document::recalcStyle(StyleChange change)
             if (!n->isElementNode())
                 continue;
             Element* element = toElement(n);
-            if (change >= Inherit || element->childNeedsStyleRecalc() || element->needsStyleRecalc())
+            if (shouldRecalcStyle(change, element))
                 element->recalcStyle(change);
         }
 
