0b676e1beb8e8b7f2ae68e2f40b1b234c9fdd0d2
yoshinori.sano@gmail.com <yoshinori.sano@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-06 11:22:02 +0000

    Remove unused header files in core/html/parser/CompactHTMLToken.h
    The following header files are included, however, are not used:
    - wtf/OwnPtr.h
    - wtf/PassOwnPtr.h
    - wtf/RefCounted.h
    - wtf/RefPtr.h
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/13811055
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/CompactHTMLToken.h
+++ third_party/WebKit/Source/core/html/parser/CompactHTMLToken.h
@@ -29,10 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/html/parser/HTMLIdentifier.h"
 #include "core/html/parser/HTMLToken.h"
-#include <wtf/OwnPtr.h>
-#include <wtf/PassOwnPtr.h>
-#include <wtf/RefCounted.h>
-#include <wtf/RefPtr.h>
 #include <wtf/text/TextPosition.h>
 #include <wtf/text/WTFString.h>
 #include <wtf/Vector.h>
