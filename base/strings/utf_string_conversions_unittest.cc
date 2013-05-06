a6083313b00c0b742d1f9de56799e0e4c3c62dbf
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 21:32:31 +0000

    Move utf_string_conversions to strings/ and add namespace.
    This keeps a forwarding header in the old location and adds "using" statements to avoid changing all callers.
    
    BUG=None
    R=tfarina@chromium.org
    
    Review URL: https://codereview.chromium.org/14126006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198542 0039d316-1c4b-4281-b951-d872f2087c98

--- base/strings/utf_string_conversions_unittest.cc
+++ base/strings/utf_string_conversions_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/string_util.h"
 #include "base/strings/string_piece.h"
-#include "base/utf_string_conversions.h"
+#include "base/strings/utf_string_conversions.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace base {
