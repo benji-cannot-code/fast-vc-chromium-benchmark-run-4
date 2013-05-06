13a40b83f93677471ba28fcdcfabd22d61d38045
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 12:23:14 +0000

    Fix BASE_EXPORT keyword position in activity_status.h.
    TBR=digit@chromium.org
    
    Review URL: https://codereview.chromium.org/14948004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198424 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/activity_status.h
+++ base/android/activity_status.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 
 #include "base/android/jni_android.h"
+#include "base/base_export.h"
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/singleton.h"
@@ -50,7 +51,7 @@ enum ActivityState {
 //    // Stop listening.
 //    delete my_listener
 //
-BASE_EXPORT class ActivityStatus {
+class BASE_EXPORT ActivityStatus {
  public:
   typedef base::Callback<void(ActivityState)> StateChangeCallback;
 
