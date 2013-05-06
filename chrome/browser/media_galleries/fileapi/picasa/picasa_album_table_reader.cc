33e34b564f25b03ffade960014bba6b921772213
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 20:51:48 +0000

    Clean up album table reader structs and naming.
    BUG=151701
    
    Review URL: https://chromiumcodereview.appspot.com/14736008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198527 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/fileapi/picasa/picasa_album_table_reader.cc
+++ chrome/browser/media_galleries/fileapi/picasa/picasa_album_table_reader.cc
@@ -31,21 +31,14 @@ base::Time TimeFromMicrosoftVariantTime(double variant_time) {
 }  // namespace
 
 AlbumInfo::AlbumInfo(const std::string& name, const base::Time& timestamp,
-                     const std::string& uid)
-    : name(name),
-      timestamp(timestamp),
-      uid(uid) {}
-
-AlbumInfo::~AlbumInfo() {}
-
-FolderInfo::FolderInfo(const std::string& name, const base::Time& timestamp,
-                       const std::string& uid, const base::FilePath& path)
+                     const std::string& uid, const base::FilePath& path)
     : name(name),
       timestamp(timestamp),
       uid(uid),
-      path(path) {}
+      path(path) {
+}
 
-FolderInfo::~FolderInfo() {}
+AlbumInfo::~AlbumInfo() {}
 
 PicasaAlbumTableReader::PicasaAlbumTableReader(
     const base::FilePath& directory_path)
@@ -70,22 +63,15 @@ base::FilePath PicasaAlbumTableReader::PicasaDB3Dir() {
   return path.Append(FPL("Google")).Append(FPL("Picasa2")).Append(FPL("db3"));
 }
 
-const std::vector<FolderInfo>& PicasaAlbumTableReader::folders() const {
+const std::vector<AlbumInfo>& PicasaAlbumTableReader::folders() const {
   DCHECK(initialized_);
   return folders_;
 }
 
-const std::vector<AlbumInfo>& PicasaAlbumTableReader::user_albums() const {
+const std::vector<AlbumInfo>& PicasaAlbumTableReader::albums() const {
   DCHECK(initialized_);
-  return user_albums_;
+  return albums_;
 }
-
-PicasaAlbumTableReader::PicasaAlbumTableReader(
-    const std::vector<FolderInfo>& folders,
-    const std::vector<AlbumInfo>& user_albums)
-    : initialized_(true),
-      folders_(folders),
-      user_albums_(user_albums) {}
 
 bool PicasaAlbumTableReader::Init() {
   if (initialized_)
@@ -124,14 +110,14 @@ bool PicasaAlbumTableReader::Init() {
     base::Time timestamp = TimeFromMicrosoftVariantTime(date);
 
     switch (category) {
-      case kAlbumCategoryUserAlbum: {
+      case kAlbumCategoryAlbum: {
         std::string token;
         if (!token_column->ReadString(i, &token) || token.empty() ||
             !StartsWithASCII(token, kAlbumTokenPrefix, false)) {
           continue;
         }
 
-        user_albums_.push_back(AlbumInfo(name, timestamp, uid));
+        albums_.push_back(AlbumInfo(name, timestamp, uid, base::FilePath()));
         break;
       }
       case kAlbumCategoryFolder: {
@@ -142,7 +128,7 @@ bool PicasaAlbumTableReader::Init() {
         base::FilePath path =
             base::FilePath(base::FilePath::FromUTF8Unsafe(filename));
 
-        folders_.push_back(FolderInfo(name, timestamp, uid, path));
+        folders_.push_back(AlbumInfo(name, timestamp, uid, path));
         break;
       }
       default: {
