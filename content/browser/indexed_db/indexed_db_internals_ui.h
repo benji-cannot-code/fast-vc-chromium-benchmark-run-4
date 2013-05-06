b7dd59d82be4d55337eebf6b227148068b96b564
alecflett@chromium.org <alecflett@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-06 16:26:25 +0000

    Implement download link in chrome://indexeddb-internals/
    This closes all IDB connections for the given origin, zips up
    the files and "downloads" them by initiating the download manager.
    This must bounce events through a number of threads to manage temporary
    files, callbacks into JavaScript, etc.
    
    BUG=174188
    TBR=jsbell@chromium.org
    
    Review URL: https://codereview.chromium.org/13949013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198469 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_internals_ui.h
+++ content/browser/indexed_db/indexed_db_internals_ui.h
@@ -12,11 +12,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "content/public/browser/indexed_db_context.h"
 #include "content/public/browser/web_ui_controller.h"
+#include "net/base/net_errors.h"
 
 namespace base { class ListValue; }
 
 namespace content {
 
+class DownloadItem;
+class IndexedDBContextImpl;
 class StoragePartition;
 
 // The implementation for the chrome://indexeddb-internals page.
@@ -29,14 +32,29 @@ class IndexedDBInternalsUI : public WebUIController {
   typedef std::vector<scoped_refptr<IndexedDBContext> > ContextList;
   void GetAllOrigins(const base::ListValue* args);
   void GetAllOriginsOnWebkitThread(
-      scoped_ptr<ContextList> contexts,
-      scoped_ptr<std::vector<base::FilePath> > context_paths);
+      const scoped_ptr<ContextList> contexts,
+      const scoped_ptr<std::vector<base::FilePath> > context_paths);
   void OnOriginsReady(scoped_ptr<std::vector<IndexedDBInfo> > origins,
                       const base::FilePath& path);
 
   static void AddContextFromStoragePartition(ContextList* contexts,
                                              std::vector<base::FilePath>* paths,
                                              StoragePartition* partition);
+
+  void DownloadOriginData(const base::ListValue* args);
+  void DownloadOriginDataOnWebkitThread(
+      const base::FilePath& partition_path,
+      const scoped_refptr<IndexedDBContextImpl> context,
+      const GURL& origin_url);
+  void OnDownloadDataReady(const base::FilePath& partition_path,
+                           const GURL& origin_url,
+                           const base::FilePath temp_path,
+                           const base::FilePath zip_path);
+  void OnDownloadStarted(const base::FilePath& partition_path,
+                         const GURL& origin_url,
+                         const base::FilePath& temp_path,
+                         content::DownloadItem* item,
+                         net::Error error);
 
   DISALLOW_COPY_AND_ASSIGN(IndexedDBInternalsUI);
 };
--- content/content_browser.gypi
+++ content/content_browser.gypi
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     '../skia/skia.gyp:skia',
     '../sql/sql.gyp:sql',
     '../third_party/re2/re2.gyp:re2',
+    '../third_party/zlib/zlib.gyp:zip',
     '../third_party/zlib/zlib.gyp:zlib',
     '../ui/snapshot/snapshot.gyp:snapshot',
     '../ui/ui.gyp:ui',
