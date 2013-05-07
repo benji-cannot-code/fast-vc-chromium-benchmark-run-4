8093c1b7c30747f68246f2eea842cc3a0ebe4fd8
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-07 00:20:18 +0000

    Revert "Clear highlight when the highlighted node gets removed from the document"
    This reverts http://src.chromium.org/viewvc/blink?view=revision&revision=149814, because the test that was landed with it
    is failing immediately after landing.
    
    TBR=trchen@chromium.org
    BUG=232926
    
    Review URL: https://codereview.chromium.org/14921004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/LinkHighlight.h
+++ third_party/WebKit/Source/WebKit/chromium/src/LinkHighlight.h
@@ -70,8 +70,6 @@ public:
     virtual WebLayer* layer() OVERRIDE;
     virtual void clearCurrentGraphicsLayer() OVERRIDE;
 
-    WebCore::GraphicsLayerChromium* currentGraphicsLayerForTesting() const { return m_currentGraphicsLayer; }
-
 private:
     LinkHighlight(WebCore::Node*, WebViewImpl*);
 
