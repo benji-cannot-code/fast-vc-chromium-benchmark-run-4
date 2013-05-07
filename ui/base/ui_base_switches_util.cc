ecaa3575ee29d950aaccc7eb361201bb4ae513b5
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 01:49:15 +0000

    Turn off touch editing and touch drag and drop by default.
    BUG=none.
    
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/14853004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198589 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ui_base_switches_util.cc
+++ ui/base/ui_base_switches_util.cc
@@ -11,23 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace switches {
 
 bool IsTouchDragDropEnabled() {
-#if defined(OS_CHROMEOS)
-  return !CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableTouchDragDrop);
-#else
   return CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnableTouchDragDrop);
-#endif
 }
 
 bool IsTouchEditingEnabled() {
-#if defined(OS_CHROMEOS)
-  return !CommandLine::ForCurrentProcess()->HasSwitch(
-      switches::kDisableTouchEditing);
-#else
   return CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kEnableTouchEditing);
-#endif
 }
 
 }  // namespace switches
