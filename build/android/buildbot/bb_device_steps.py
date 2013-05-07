6c830bb0c22fa6805756e8a54e3a171c5d06bf80
navabi@chromium.org <navabi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 18:39:18 +0000

    Revert 194601 "Android: Enable content_browsertests by default."
    > Android: Enable content_browsertests by default.
    > 
    > BUG=138275
    > 
    > Review URL: https://codereview.chromium.org/13445005
    
    TBR=nileshagrawal@chromium.org
    
    Review URL: https://codereview.chromium.org/15042004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198770 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_device_steps.py
+++ build/android/buildbot/bb_device_steps.py
@@ -147,7 +147,7 @@ def RunBrowserTestSuite(options):
   Args:
     options: options object.
   """
-  args = ['--verbose', '--num_retries=1']
+  args = ['--verbose']
   if options.target == 'Release':
     args.append('--release')
   if options.asan:
@@ -285,7 +285,6 @@ def MainTestWrapper(options):
     RunChromeDriverTests()
   if 'unit' in options.test_filter:
     RunTestSuites(options, gtest_config.STABLE_TEST_SUITES)
-    RunBrowserTestSuite(options)
   if 'ui' in options.test_filter:
     for test in INSTRUMENTATION_TESTS.itervalues():
       RunInstrumentationSuite(options, test)
@@ -299,6 +298,7 @@ def MainTestWrapper(options):
 
   if options.experimental:
     RunTestSuites(options, gtest_config.EXPERIMENTAL_TEST_SUITES)
+    RunBrowserTestSuite(options)
 
   # Print logcat, kill logcat monitor
   buildbot_report.PrintNamedStep('logcat_dump')
