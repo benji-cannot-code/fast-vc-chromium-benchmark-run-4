77a2ccb3dc41ed09cbad442e1bfada1c2bbad747
aedla@chromium.org <aedla@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-07 18:28:02 +0000

    Character reference parser: stop integer overflow for numeric character references.
    BUG=237459
    
    Review URL: https://chromiumcodereview.appspot.com/14923002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149880 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/parser/CharacterReferenceParserInlines.h
+++ third_party/WebKit/Source/core/xml/parser/CharacterReferenceParserInlines.h
@@ -32,9 +32,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+static const UChar32 kInvalidUnicode = -1;
+
 inline bool isHexDigit(UChar cc)
 {
     return (cc >= '0' && cc <= '9') || (cc >= 'a' && cc <= 'f') || (cc >= 'A' && cc <= 'F');
+}
+
+inline UChar asHexDigit(UChar cc)
+{
+    if (cc >= '0' && cc <= '9')
+      return cc - '0';
+    if (cc >= 'a' && cc <= 'z')
+      return 10 + cc - 'a';
+    if (cc >= 'A' && cc <= 'Z')
+      return 10 + cc - 'A';
+    ASSERT_NOT_REACHED();
+    return 0;
 }
 
 inline void unconsumeCharacters(SegmentedString& source, const StringBuilder& consumedCharacters)
@@ -121,13 +135,10 @@ bool consumeCharacterReference(SegmentedString& source, StringBuilder& decodedCh
             return false;
         }
         case Hex: {
-            if (cc >= '0' && cc <= '9')
-                result = result * 16 + cc - '0';
-            else if (cc >= 'a' && cc <= 'f')
-                result = result * 16 + 10 + cc - 'a';
-            else if (cc >= 'A' && cc <= 'F')
-                result = result * 16 + 10 + cc - 'A';
-            else if (cc == ';') {
+            if (isHexDigit(cc)) {
+                if (result != kInvalidUnicode)
+                    result = result * 16 + asHexDigit(cc);
+            } else if (cc == ';') {
                 source.advanceAndASSERT(cc);
                 decodedCharacter.append(ParserFunctions::legalEntityFor(result));
                 return true;
@@ -141,9 +152,10 @@ bool consumeCharacterReference(SegmentedString& source, StringBuilder& decodedCh
             break;
         }
         case Decimal: {
-            if (cc >= '0' && cc <= '9')
-                result = result * 10 + cc - '0';
-            else if (cc == ';') {
+            if (cc >= '0' && cc <= '9') {
+                if (result != kInvalidUnicode)
+                    result = result * 10 + cc - '0';
+            } else if (cc == ';') {
                 source.advanceAndASSERT(cc);
                 decodedCharacter.append(ParserFunctions::legalEntityFor(result));
                 return true;
@@ -160,6 +172,10 @@ bool consumeCharacterReference(SegmentedString& source, StringBuilder& decodedCh
             return ParserFunctions::consumeNamedEntity(source, decodedCharacter, notEnoughCharacters, additionalAllowedCharacter, cc);
         }
         }
+
+        if (result > UCHAR_MAX_VALUE)
+            result = kInvalidUnicode;
+
         consumedCharacters.append(cc);
         source.advanceAndASSERT(cc);
     }
