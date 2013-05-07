331f88138f019f2acd870abf10f7e4d53c53c91e
grv@chromium.org <grv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 17:02:17 +0000

    Return the ID of then loaded unpacked item. Also, whitelist apps-editor to use management api.
    Return ID of item as response for loadProject.
    
    Review URL: https://chromiumcodereview.appspot.com/14885005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198749 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/developer_private/developer_private_api.h
+++ chrome/browser/extensions/api/developer_private/developer_private_api.h
@@ -429,7 +429,7 @@ class DeveloperPrivateGetProjectsInfoFunction : public AsyncExtensionFunction {
    virtual bool RunImpl() OVERRIDE;
 };
 
-class DeveloperPrivateLoadProjectFunction : public SyncExtensionFunction {
+class DeveloperPrivateLoadProjectFunction : public AsyncExtensionFunction {
   public:
    DECLARE_EXTENSION_FUNCTION("developerPrivate.loadProject",
                               DEVELOPERPRIVATE_LOADPROJECT);
@@ -441,6 +441,9 @@ class DeveloperPrivateLoadProjectFunction : public SyncExtensionFunction {
 
    // ExtensionFunction
    virtual bool RunImpl() OVERRIDE;
+
+   void GetUnpackedExtension(const base::FilePath& path,
+                             const ExtensionSet* extensions);
 };
 
 }  // namespace api
