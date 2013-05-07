c8e00755587b32e3c06a293f995edef5e0c6793c
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-07 01:10:38 +0000

    Include scrollbar size in @media width/height
    Not including scrollbar size causes bad circular dependencies when
    non-overlay scrollbars are created or removed.
    
    CSS spec says we should include size of scroll bars when evaluate the
    MQ rules about width/height.
    
    BUG=236111
    TEST=LayoutTests/fast/media/mq-size-include-scrollbars.html
    
    Review URL: https://chromiumcodereview.appspot.com/14659008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
+++ third_party/WebKit/Source/core/css/MediaQueryEvaluator.cpp
@@ -246,11 +246,16 @@ static bool monochromeMediaFeatureEval(CSSValue* value, RenderStyle* style, Fram
     return colorMediaFeatureEval(value, style, frame, op);
 }
 
+static IntSize viewportSize(FrameView* view)
+{
+    return view->layoutSize(ScrollableArea::IncludeScrollbars);
+}
+
 static bool orientationMediaFeatureEval(CSSValue* value, RenderStyle*, Frame* frame, MediaFeaturePrefix)
 {
     FrameView* view = frame->view();
-    int width = view->layoutWidth();
-    int height = view->layoutHeight();
+    int width = viewportSize(view).width();
+    int height = viewportSize(view).height();
     if (value && value->isPrimitiveValue()) {
         const int id = static_cast<CSSPrimitiveValue*>(value)->getIdent();
         if (width > height) // Square viewport is portrait.
@@ -266,7 +271,7 @@ static bool aspect_ratioMediaFeatureEval(CSSValue* value, RenderStyle*, Frame* f
 {
     if (value) {
         FrameView* view = frame->view();
-        return compareAspectRatioValue(value, view->layoutWidth(), view->layoutHeight(), op);
+        return compareAspectRatioValue(value, viewportSize(view).width(), viewportSize(view).height(), op);
     }
 
     // ({,min-,max-}aspect-ratio)
@@ -476,8 +481,8 @@ static bool heightMediaFeatureEval(CSSValue* value, RenderStyle* style, Frame* f
 {
     FrameView* view = frame->view();
 
+    int height = viewportSize(view).height();
     if (value) {
-        int height = view->layoutHeight();
         if (RenderView* renderView = frame->document()->renderView())
             height = adjustForAbsoluteZoom(height, renderView);
         RenderStyle* rootStyle = frame->document()->documentElement()->renderStyle();
@@ -485,15 +490,15 @@ static bool heightMediaFeatureEval(CSSValue* value, RenderStyle* style, Frame* f
         return computeLength(value, !frame->document()->inQuirksMode(), style, rootStyle, length) && compareValue(height, length, op);
     }
 
-    return view->layoutHeight() != 0;
+    return height;
 }
 
 static bool widthMediaFeatureEval(CSSValue* value, RenderStyle* style, Frame* frame, MediaFeaturePrefix op)
 {
     FrameView* view = frame->view();
 
+    int width = viewportSize(view).width();
     if (value) {
-        int width = view->layoutWidth();
         if (RenderView* renderView = frame->document()->renderView())
             width = adjustForAbsoluteZoom(width, renderView);
         RenderStyle* rootStyle = frame->document()->documentElement()->renderStyle();
@@ -501,7 +506,7 @@ static bool widthMediaFeatureEval(CSSValue* value, RenderStyle* style, Frame* fr
         return computeLength(value, !frame->document()->inQuirksMode(), style, rootStyle, length) && compareValue(width, length, op);
     }
 
-    return view->layoutWidth() != 0;
+    return width;
 }
 
 // rest of the functions are trampolines which set the prefix according to the media feature expression used
