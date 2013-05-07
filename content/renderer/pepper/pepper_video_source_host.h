ccbf6534855d6318420f8bed290acdda48919288
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 00:39:02 +0000

    Glue code to connect PPAPI proxy to MediaStream sources and destinations. This is a bit rough but putting it up for review to make schedule.
    BUG=230980
    R=ronghuawu@chromium.org, tsepez@chromium.org, yzshen@chromium.org
    
    Review URL: https://codereview.chromium.org/14968002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198576 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_video_source_host.h
+++ content/renderer/pepper/pepper_video_source_host.h
@@ -7,22 +7,30 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CONTENT_RENDERER_PEPPER_PEPPER_VIDEO_SOURCE_HOST_H_
 
 #include "base/compiler_specific.h"
+#include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
+#include "content/renderer/media/video_source_handler.h"
 #include "ppapi/c/pp_time.h"
+#include "ppapi/host/host_message_context.h"
 #include "ppapi/host/resource_host.h"
 
 namespace content {
 
 class RendererPpapiHost;
 
-class CONTENT_EXPORT PepperVideoSourceHost : public ppapi::host::ResourceHost {
+class CONTENT_EXPORT PepperVideoSourceHost
+    : public ppapi::host::ResourceHost,
+      public content::FrameReaderInterface {
  public:
   PepperVideoSourceHost(RendererPpapiHost* host,
                         PP_Instance instance,
                         PP_Resource resource);
 
   virtual ~PepperVideoSourceHost();
+
+  // content::FrameReaderInterface implementation.
+  virtual bool GotFrame(cricket::VideoFrame* frame) OVERRIDE;
 
   virtual int32_t OnResourceMessageReceived(
       const IPC::Message& msg,
@@ -34,11 +42,26 @@ class CONTENT_EXPORT PepperVideoSourceHost : public ppapi::host::ResourceHost {
   int32_t OnHostMsgGetFrame(ppapi::host::HostMessageContext* context);
   int32_t OnHostMsgClose(ppapi::host::HostMessageContext* context);
 
+  void OnGotFrame(scoped_ptr<cricket::VideoFrame> frame);
+
+  int32_t ConvertFrame(ppapi::HostResource* image_data_resource,
+                       PP_TimeTicks* timestamp);
+  void SendFrame(const ppapi::HostResource& image_data_resource,
+                 PP_TimeTicks timestamp,
+                 int32_t result);
+  void Close();
+
   RendererPpapiHost* renderer_ppapi_host_;
 
   base::WeakPtrFactory<PepperVideoSourceHost> weak_factory_;
 
-  PP_TimeTicks last_timestamp_;
+  ppapi::host::ReplyMessageContext reply_context_;
+  scoped_refptr<base::MessageLoopProxy> main_message_loop_proxy_;
+
+  scoped_ptr<content::VideoSourceHandler> source_handler_;
+  std::string stream_url_;
+  scoped_ptr<cricket::VideoFrame> last_frame_;
+  bool get_frame_pending_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperVideoSourceHost);
 };
