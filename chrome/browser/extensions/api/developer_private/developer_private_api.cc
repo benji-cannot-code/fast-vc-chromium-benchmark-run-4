b870598a8206f998aecdb7a111c9c71857d1172e
grv@chromium.org <grv@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 16:58:08 +0000

    Directory support on syncfs has landed. Switch to it instead of maintaining a local copy of files.
    Switch to syncfs directories instead of localfs copies.
    
    Review URL: https://chromiumcodereview.appspot.com/14872006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198745 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/developer_private/developer_private_api.cc
+++ chrome/browser/extensions/api/developer_private/developer_private_api.cc
@@ -835,7 +835,7 @@ void DeveloperPrivateExportSyncfsFolderToLocalfsFunction::
   fileapi::FileSystemURL url(sync_file_system::CreateSyncableFileSystemURL(
       GURL(origin_url),
       sync_file_system::DriveFileSyncService::kServiceName,
-      base::FilePath()));
+      project_path.BaseName()));
 
   base::PlatformFileError error_code;
   fileapi::FileSystemOperation* op =
@@ -878,7 +878,7 @@ void DeveloperPrivateExportSyncfsFolderToLocalfsFunction::
     fileapi::FileSystemURL url(sync_file_system::CreateSyncableFileSystemURL(
         GURL(origin_url),
         sync_file_system::DriveFileSyncService::kServiceName,
-        base::FilePath(file_list[i].name)));
+        project_path.BaseName().Append(file_list[i].name)));
     base::FilePath target_path = project_path;
     target_path = target_path.Append(file_list[i].name);
 
