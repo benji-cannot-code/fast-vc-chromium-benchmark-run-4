17176e6d35703724b15afe810f5ca8e8d3ebb68b
bbudge@chromium.org <bbudge@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-07 13:26:05 +0000

    Fix usage of MessageLoopProxy in PepperPlatformAudioInputImpl.
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/15019004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198715 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_platform_audio_input_impl.h
+++ content/renderer/pepper/pepper_platform_audio_input_impl.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "media/audio/audio_input_ipc.h"
@@ -96,7 +97,7 @@ class PepperPlatformAudioInputImpl
   // I/O THREAD.
   scoped_ptr<media::AudioInputIPC> ipc_;
 
-  base::MessageLoopProxy* main_message_loop_proxy_;
+  scoped_refptr<base::MessageLoopProxy> main_message_loop_proxy_;
 
   // THIS MUST ONLY BE ACCESSED ON THE MAIN THREAD.
   base::WeakPtr<PepperPluginDelegateImpl> plugin_delegate_;
