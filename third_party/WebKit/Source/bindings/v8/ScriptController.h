24b80c98cd43fa0a2c1f03eccc76a49fd5511f85
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-07 18:28:46 +0000

    Remove ShouldReplaceDocumentIfJavaScriptURL enum and always replace document.
    This is a long-standing FIXME.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/14995011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149881 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptController.h
+++ third_party/WebKit/Source/bindings/v8/ScriptController.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "bindings/v8/ScriptInstance.h"
 #include "bindings/v8/ScriptValue.h"
-#include "core/loader/FrameLoaderTypes.h"
 
 #include <v8.h>
 #include "wtf/Forward.h"
@@ -87,7 +86,7 @@ public:
     ScriptValue callFunctionEvenIfScriptDisabled(v8::Handle<v8::Function>, v8::Handle<v8::Object>, int argc, v8::Handle<v8::Value> argv[]);
 
     // Returns true if argument is a JavaScript URL.
-    bool executeIfJavaScriptURL(const KURL&, ShouldReplaceDocumentIfJavaScriptURL shouldReplaceDocumentIfJavaScriptURL = ReplaceDocumentIfJavaScriptURL);
+    bool executeIfJavaScriptURL(const KURL&);
 
     // This function must be called from the main thread. It is safe to call it repeatedly.
     static void initializeThreading();
