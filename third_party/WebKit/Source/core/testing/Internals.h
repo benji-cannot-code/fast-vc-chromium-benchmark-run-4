f7b7c5464903af852b02db5eeef7d611a5932fea
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-07 03:20:34 +0000

    Remove Internals::createShadowRoot(element) and polyfill.js.
    We don't need Internals::createShadowRoot anymore since Element.webkitCreateShadowRoot() is always available.
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/14594006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149843 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/testing/Internals.h
+++ third_party/WebKit/Source/core/testing/Internals.h
@@ -84,7 +84,6 @@ public:
     PassRefPtr<CSSComputedStyleDeclaration> computedStyleIncludingVisitedInfo(Node*, ExceptionCode&) const;
 
     ShadowRoot* ensureShadowRoot(Element* host, ExceptionCode&);
-    ShadowRoot* createShadowRoot(Element* host, ExceptionCode&);
     ShadowRoot* shadowRoot(Element* host, ExceptionCode&);
     ShadowRoot* youngestShadowRoot(Element* host, ExceptionCode&);
     ShadowRoot* oldestShadowRoot(Element* host, ExceptionCode&);
