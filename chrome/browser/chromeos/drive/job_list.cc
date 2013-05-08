2aeaaff7c8c82bd05516ca2c115cb607a672997e
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-08 23:32:46 +0000

    drive: Add more event logging in JobScheduler
    - Change "Queue job" and "Start job" to "Job queued" and "Job started"
    - Add the job state and the queue info to each job event message.
    
    Example:
    
    2013-05-08T00:38:40.686: Job queued: TYPE_GET_APP_LIST STATE_NONE [1] - METADATA_QUEUE: pending: 1, running: 0
    2013-05-08T00:38:40.687: Job started: TYPE_GET_APP_LIST STATE_RUNNING [1] - METADATA_QUEUE: pending: 0, running: 1
    2013-05-08T00:38:41.000: Job done: TYPE_GET_APP_LIST STATE_RUNNING [1] => FILE_ERROR_OK (elapsed time: 313ms) - METADATA_QUEUE: pending: 0, running: 2
    
    BUG=238957
    TEST=open Drive in Files.app; go to chrome:drive-internals. Confirm these messages are shown.
    
    Review URL: https://chromiumcodereview.appspot.com/15027004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199053 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/job_list.cc
+++ chrome/browser/chromeos/drive/job_list.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/job_list.h"
 
 #include "base/logging.h"
+#include "base/stringprintf.h"
+#include "base/strings/string_number_conversions.h"
 
 namespace drive {
 
@@ -74,6 +76,23 @@ JobInfo::JobInfo(JobType in_job_type)
       state(STATE_NONE),
       num_completed_bytes(0),
       num_total_bytes(0) {
+}
+
+std::string JobInfo::ToString() const {
+  std::string output = base::StringPrintf(
+      "%s %s [%d]",
+      JobTypeToString(job_type).c_str(),
+      JobStateToString(state).c_str(),
+      job_id);
+  if (job_type == TYPE_DOWNLOAD_FILE ||
+      job_type == TYPE_UPLOAD_NEW_FILE ||
+      job_type == TYPE_UPLOAD_EXISTING_FILE) {
+    base::StringAppendF(&output,
+                        " bytes: %s/%s",
+                        base::Int64ToString(num_completed_bytes).c_str(),
+                        base::Int64ToString(num_total_bytes).c_str());
+  }
+  return output;
 }
 
 }  // namespace drive
