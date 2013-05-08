d2bddd0e94d18645a595dcae0c5d5a2164686495
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-08 22:02:36 +0000

    Make base:ReplaceFile return an informative error.
    It currently just returns true/false to indicate success.
    More information to diagnose and log failures would be
    helpful in IndexedDB.
    
    This patch also logs the new error when ReplaceFile fails in
    IndexedDB.
    
    BUG=229268
    
    Review URL: https://chromiumcodereview.appspot.com/14886003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199025 0039d316-1c4b-4281-b951-d872f2087c98

--- base/platform_file.h
+++ base/platform_file.h
@@ -20,14 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-#if defined(OS_WIN)
-typedef HANDLE PlatformFile;
-const PlatformFile kInvalidPlatformFileValue = INVALID_HANDLE_VALUE;
-#elif defined(OS_POSIX)
-typedef int PlatformFile;
-const PlatformFile kInvalidPlatformFileValue = -1;
-#endif
-
 // PLATFORM_FILE_(OPEN|CREATE).* are mutually exclusive. You should specify
 // exactly one of the five (possibly combining with other flags) when opening
 // or creating a file.
@@ -119,6 +111,16 @@ struct BASE_EXPORT PlatformFileInfo {
   // The creation time of a file.
   base::Time creation_time;
 };
+
+#if defined(OS_WIN)
+typedef HANDLE PlatformFile;
+const PlatformFile kInvalidPlatformFileValue = INVALID_HANDLE_VALUE;
+PlatformFileError LastErrorToPlatformFileError(DWORD saved_errno);
+#elif defined(OS_POSIX)
+typedef int PlatformFile;
+const PlatformFile kInvalidPlatformFileValue = -1;
+PlatformFileError ErrnoToPlatformFileError(int saved_errno);
+#endif
 
 // Creates or opens the given file. If |created| is provided, it will be set to
 // true if a new file was created [or an old one truncated to zero length to
