fb61e1313bf0744fde17293dcb0f600891674067
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-08 19:31:34 +0000

    Check that the files the renderer wants to preserve as part of a session restore are already available to the renderer.
    The fix involves cracking open the WebHistoryItem, extracting its file paths,
    and checking them against ChildProcessSecurityPolicy for the messages
    ViewHostMsg_UpdateState and ViewHostMsg_FrameNavigate.
    
    BUG=237263
    R=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/14727006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@198965 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_view_host_impl.h
+++ content/browser/renderer_host/render_view_host_impl.h
@@ -563,6 +563,8 @@ class CONTENT_EXPORT RenderViewHostImpl
 
   void ClearPowerSaveBlockers();
 
+  bool CanAccessFilesOfSerializedState(const std::string& state) const;
+
   // Our delegate, which wants to know about changes in the RenderView.
   RenderViewHostDelegate* delegate_;
 
