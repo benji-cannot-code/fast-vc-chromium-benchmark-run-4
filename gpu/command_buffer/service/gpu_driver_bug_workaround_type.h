5a478ab814afaf2c9eeb4b3d9d813f5f186cfacb
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-08 22:56:20 +0000

    Makes sure driver_bug_workarounds takes effect in Android.
    Right now the hardwired workarounds are still in place, but will be removed soon.
    
    With this CL, the workarounds should be obtained from the json file, and this should overwrite the hardwired settings.
    
    A few things in this CL:
    
    1) Generally the browser process decides which workarounds are needed, and pass down to command buffer through a commandline switch in GPU process launch. However, in Android, we don't have a GPU process, so we append the switch immediately after we compute it in browser startup, and later pick up by command buffer because gpu thread and browser main thread are in the same process, therefore the same commandline.
    
    2) add gl_extensions as a blacklist feature because this is the only reliable way to identify Vivante devices.
    
    BUG=227162
    TEST=android devices that need workaround get them, for example, hisilicon device
    R=epenner,kbr
    
    Review URL: https://chromiumcodereview.appspot.com/14762004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199034 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/gpu_driver_bug_workaround_type.h
+++ gpu/command_buffer/service/gpu_driver_bug_workaround_type.h
@@ -29,8 +29,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          exit_on_context_lost)                        \
   GPU_OP(FLUSH_ON_CONTEXT_SWITCH,                     \
          flush_on_context_switch)                     \
-  GPU_OP(UNBIND_FBO_ON_CONTEXT_SWITCH,                \
-         unbind_fbo_on_context_switch)                \
   GPU_OP(MAX_CUBE_MAP_TEXTURE_SIZE_LIMIT_1024,        \
          max_cube_map_texture_size_limit_1024)        \
   GPU_OP(MAX_CUBE_MAP_TEXTURE_SIZE_LIMIT_4096,        \
@@ -49,6 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          reverse_point_sprite_coord_origin)           \
   GPU_OP(SET_TEXTURE_FILTER_BEFORE_GENERATING_MIPMAP, \
          set_texture_filter_before_generating_mipmap) \
+  GPU_OP(UNBIND_FBO_ON_CONTEXT_SWITCH,                \
+         unbind_fbo_on_context_switch)                \
   GPU_OP(USE_CLIENT_SIDE_ARRAYS_FOR_STREAM_BUFFERS,   \
          use_client_side_arrays_for_stream_buffers)   \
   GPU_OP(USE_CURRENT_PROGRAM_AFTER_SUCCESSFUL_LINK,   \
