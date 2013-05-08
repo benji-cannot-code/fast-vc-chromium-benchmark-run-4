92fe86d270fb095a04b5175708f5ead4404b8c22
jww@chromium.org <jww@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-08 02:27:37 +0000

    Added casting check to WebPageSerializerImpl::endTagToString to stop ASSERT reach in 189084.
    BUG=189084
    
    Review URL: https://chromiumcodereview.appspot.com/14687013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149927 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebPageNewSerializerTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebPageNewSerializerTest.cpp
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 #include "config.h"
 
 #include "FrameTestHelpers.h"
@@ -36,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebFrame.h"
 #include "WebFrameClient.h"
 #include "WebPageSerializer.h"
+#include "WebPageSerializerClient.h"
 #include "WebScriptSource.h"
 #include "WebSettings.h"
 #include "WebView.h"
@@ -83,6 +83,21 @@ private:
 class TestWebFrameClient : public WebFrameClient {
 public:
     virtual ~TestWebFrameClient() { }
+};
+
+class LengthCountingWebPageSerializerClient : public WebPageSerializerClient {
+public:
+    LengthCountingWebPageSerializerClient(size_t* counter)
+        : m_counter(counter)
+    {
+    }
+
+    virtual void didSerializeDataForFrame(const WebURL& frameURL, const WebCString& data, PageSerializationStatus status) {
+        *m_counter += data.length();
+    }
+
+private:
+    size_t* m_counter;
 };
 
 class WebPageNewSerializeTest : public testing::Test {
@@ -350,6 +365,29 @@ TEST_F(WebPageNewSerializeTest, SVGImageDontCrash)
     WebCString mhtml = WebPageSerializer::serializeToMHTML(m_webView);
     // We expect some data to be generated.
     EXPECT_GT(mhtml.length(), 50U);
+}
+
+TEST_F(WebPageNewSerializeTest, NamespaceElementsDontCrash)
+{
+    WebURL pageUrl = toKURL("http://www.test.com");
+    registerMockedURLLoad(pageUrl, WebString::fromUTF8("namespace_element.html"), WebString::fromUTF8("pageserializer/"), htmlMimeType());
+
+    loadURLInTopFrame(pageUrl);
+
+    WebVector<WebURL> localLinks(static_cast<size_t>(1));
+    WebVector<WebString> localPaths(static_cast<size_t>(1));
+    localLinks[0] = pageUrl;
+    localPaths[0] = WebString("/");
+
+    size_t counter = 0;
+    LengthCountingWebPageSerializerClient client(&counter);
+
+    // We just want to make sure nothing crazy happens, namely that no
+    // assertions are hit. As a sanity check, we also make sure that some data
+    // was returned.
+    WebPageSerializer::serialize(m_webView->mainFrame(), true, &client, localLinks, localPaths, WebString(""));
+
+    EXPECT_GT(counter, 0U);
 }
 
 }
