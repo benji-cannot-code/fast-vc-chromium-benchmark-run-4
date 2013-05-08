d53676f191fe4dccdcb441c88bbd03cf9365c68e
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-08 12:38:20 +0000

    Bindings: Call transferArrayBuffers only if we have buffers to transfer.
    Currently, we call SerializedScriptValue::transferArrayBuffers every time we
    post a message. We can drop that complexity by calling it only if and when we
    have buffers we'd like to transfer.
    
    Review URL: https://chromiumcodereview.appspot.com/14940016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
@@ -2377,6 +2377,8 @@ inline void neuterBinding(T* object)
 
 PassOwnPtr<SerializedScriptValue::ArrayBufferContentsArray> SerializedScriptValue::transferArrayBuffers(ArrayBufferArray& arrayBuffers, bool& didThrow, v8::Isolate* isolate)
 {
+    ASSERT(arrayBuffers.size());
+
     for (size_t i = 0; i < arrayBuffers.size(); i++) {
         if (arrayBuffers[i]->isNeutered()) {
             setDOMException(INVALID_STATE_ERR, isolate);
@@ -2446,7 +2448,7 @@ SerializedScriptValue::SerializedScriptValue(v8::Handle<v8::Value> value, Messag
         return;
     case Serializer::Success:
         m_data = String(StringImpl::adopt(writer.data())).isolatedCopy();
-        if (arrayBuffers)
+        if (arrayBuffers && arrayBuffers->size())
             m_arrayBufferContentsArray = transferArrayBuffers(*arrayBuffers, didThrow, isolate);
         return;
     case Serializer::JSException:
