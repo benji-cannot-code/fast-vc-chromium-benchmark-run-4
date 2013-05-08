081b5c69f0920f98942fcf0cd4bf9ad64e549d1d
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-08 08:50:18 +0000

    Clean up expectations for animation helper tests.
    * Warn about tests running in real time.
    * Always dump as text.
    * Only require result element for pixel/ref tests.
    
    BUG=235752
    
    Review URL: https://chromiumcodereview.appspot.com/14805007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149946 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/animations/resources/animation-test-helpers.js
+++ third_party/WebKit/LayoutTests/animations/resources/animation-test-helpers.js
@@ -354,7 +354,8 @@ function comparePropertyValue(property, computedValue, expectedValue, tolerance)
 
 function endTest()
 {
-    document.getElementById('result').innerHTML = result;
+    var resultElement = useResultElement ? document.getElementById('result') : document.documentElement;
+    resultElement.innerHTML = result;
 
     if (window.testRunner)
         testRunner.notifyDone();
@@ -398,6 +399,7 @@ function startTest(checks)
         runChecksWithRAF(checks);
 }
 
+var useResultElement = false;
 var result = "";
 var hasPauseAnimationAPI;
 var animStartTime;
@@ -405,6 +407,8 @@ var animStartTime;
 // FIXME: remove deprecatedEvent, disablePauseAnimationAPI and doPixelTest
 function runAnimationTest(expected, callbacks, deprecatedEvent, disablePauseAnimationAPI, doPixelTest)
 {
+    if (disablePauseAnimationAPI)
+        result += 'Warning this test is running in real-time and may be flaky.<br>';
     if (deprecatedEvent)
         throw 'Event argument is deprecated!';
     if (!expected)
@@ -431,9 +435,11 @@ function runAnimationTest(expected, callbacks, deprecatedEvent, disablePauseAnim
         checks[timeMs].push(checkExpectedValue.bind(null, expected, i));
     }
 
+    var doPixelTest = Boolean(doPixelTest);
+    useResultElement = doPixelTest;
+
     if (window.testRunner) {
-        if (!doPixelTest)
-            testRunner.dumpAsText();
+        testRunner.dumpAsText(doPixelTest);
         testRunner.waitUntilDone();
     }
 
