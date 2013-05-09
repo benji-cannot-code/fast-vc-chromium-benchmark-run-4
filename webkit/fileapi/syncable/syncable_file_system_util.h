471e3693f6e55cde9c2e9353327cc8061973a39c
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 00:21:33 +0000

    Revert 199052 "Add enable-sync-directory-operation to about:flags"
    > Add enable-sync-directory-operation to about:flags
    > 
    > This adds a flag to about:flags to enable/disable syncfs directory support.
    > 
    > BUG=238877
    > 
    > Review URL: https://chromiumcodereview.appspot.com/15026008
    
    TBR=grv@chromium.org
    
    Review URL: https://codereview.chromium.org/14980003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199064 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/syncable/syncable_file_system_util.h
+++ webkit/fileapi/syncable/syncable_file_system_util.h
@@ -20,8 +20,6 @@ class LocalFileSystemOperation;
 
 namespace sync_file_system {
 
-WEBKIT_STORAGE_EXPORT const char* GetFlagForSyncFileSystemDirectoryOperation();
-
 // Registers a syncable filesystem with the given |service_name|.
 WEBKIT_STORAGE_EXPORT bool RegisterSyncableFileSystem(
     const std::string& service_name);
