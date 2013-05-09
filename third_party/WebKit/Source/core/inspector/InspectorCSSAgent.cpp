68d61cf0e4f90c43b42408897893388e90a2e3dc
rouslan@chromium.org <rouslan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-09 19:06:51 +0000

    Add includes to InspectorCSSAgent.cpp and CachedImageTest.cpp to fix compile
    InspectorCSSAgent.cpp is missing an include for WebCore::CachedResource.
    CachedImageTest.cpp is missing an include for WebCore::Image. These two issues
    prevent compiling Blink.
    
    BUG=239513
    
    Review URL: https://chromiumcodereview.appspot.com/15001015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150038 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorCSSAgent.cpp
@@ -55,6 +55,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorState.h"
 #include "core/inspector/InspectorValues.h"
 #include "core/inspector/InstrumentingAgents.h"
+#include "core/loader/cache/CachedResource.h"
 #include "core/page/ContentSecurityPolicy.h"
 #include "core/page/DOMWindow.h"
 #include "core/rendering/RenderRegion.h"
--- third_party/WebKit/Source/core/tests/CachedImageTest.cpp
+++ third_party/WebKit/Source/core/tests/CachedImageTest.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/cache/CachedImageClient.h"
 #include "core/loader/cache/CachedResourceHandle.h"
 #include "core/platform/SharedBuffer.h"
+#include "core/platform/graphics/Image.h"
 #include <gtest/gtest.h>
 
 using namespace WebCore;
