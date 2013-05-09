0e64bbabd3cf72b6a3c5a379baa8f4b5cdd3f811
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-09 08:27:11 +0000

    Fix file-reader-sandbox-iframe.html crash
    https://src.chromium.org/viewvc/blink?revision=150011&view=revision dropped the
    check for whether the protocolIs("blob"), which caused about five tests to
    crash. This CL adds the check back and fixes those tests.
    
    TBR=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/14697020
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150015 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fileapi/ThreadableBlobRegistry.cpp
+++ third_party/WebKit/Source/core/fileapi/ThreadableBlobRegistry.cpp
@@ -153,7 +153,9 @@ BlobOriginCache::BlobOriginCache()
 
 SecurityOrigin* BlobOriginCache::cachedOrigin(const KURL& url)
 {
-    return originMap()->get(url.string());
+    if (url.protocolIs("blob"))
+        return originMap()->get(url.string());
+    return 0;
 }
 
 } // namespace WebCore
