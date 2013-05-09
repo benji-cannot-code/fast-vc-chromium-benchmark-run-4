c8e05f8468be984cfd8b68cfe2bf3c45e5e7e4d3
bengr@chromium.org <bengr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 22:21:06 +0000

    Randomize proxy timeout on Android over a range.
    BUG=237876
    
    Review URL: https://chromiumcodereview.appspot.com/14864016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199288 0039d316-1c4b-4281-b951-d872f2087c98

--- net/proxy/proxy_list.cc
+++ net/proxy/proxy_list.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/callback.h"
 #include "base/logging.h"
+#include "base/rand_util.h"
 #include "base/strings/string_tokenizer.h"
 #include "base/time.h"
 #include "base/values.h"
@@ -194,11 +195,13 @@ bool ProxyList::Fallback(ProxyRetryInfoMap* proxy_retry_info,
 
 void ProxyList::UpdateRetryInfoOnFallback(
     ProxyRetryInfoMap* proxy_retry_info, const BoundNetLog& net_log) const {
-  // Number of minutes to wait before retrying a bad proxy server.
+  // Time to wait before retrying a bad proxy server.
 #if defined(OS_ANDROID)
-  const TimeDelta kProxyRetryDelay = TimeDelta::FromMinutes(1);
+  // Randomize the timeout over a range from one to five minutes.
+  const TimeDelta proxy_retry_delay =
+      TimeDelta::FromMilliseconds(base::RandInt(1 * 60 * 1000, 5 * 60 * 1000));
 #else
-  const TimeDelta kProxyRetryDelay = TimeDelta::FromMinutes(5);
+  const TimeDelta proxy_retry_delay = TimeDelta::FromMinutes(5);
 #endif
 
   if (proxies_.empty()) {
@@ -216,7 +219,7 @@ void ProxyList::UpdateRetryInfoOnFallback(
       iter->second.bad_until = TimeTicks::Now() + iter->second.current_delay;
     } else {
       ProxyRetryInfo retry_info;
-      retry_info.current_delay = kProxyRetryDelay;
+      retry_info.current_delay = proxy_retry_delay;
       retry_info.bad_until = TimeTicks().Now() + retry_info.current_delay;
       (*proxy_retry_info)[key] = retry_info;
     }
