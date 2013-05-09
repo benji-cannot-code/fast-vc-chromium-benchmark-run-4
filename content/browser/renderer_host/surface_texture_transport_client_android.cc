c1f8b8a93ba6e2caa7760cc66f2bcb23a63bb483
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 06:00:59 +0000

    Remove unused webkit/compositor_bindings include
    Just a cleanup.
    
    Review URL: https://chromiumcodereview.appspot.com/15017012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199142 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/surface_texture_transport_client_android.cc
+++ content/browser/renderer_host/surface_texture_transport_client_android.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebGraphicsContext3D.h"
 #include "ui/gl/android/surface_texture_bridge.h"
-#include "webkit/compositor_bindings/web_compositor_support_impl.h"
 
 namespace content {
 
