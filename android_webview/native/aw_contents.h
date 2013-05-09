7b6f9c4c78d1bd2717a5c0f24065dcee8aa3bf6a
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 18:06:00 +0000

    Hardware draw init in android_webview
    Add code for requesting a draw functor in invoke mode and all the pieces of
    assuming there is already a way synchronously initialize hardware draw in cc.
    
    BUG=230197
    Android only change. Ran through android bots
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14820020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199245 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/aw_contents.h
+++ android_webview/native/aw_contents.h
@@ -102,8 +102,8 @@ class AwContents : public FindHelper::Listener,
               jint clip_y,
               jint clip_w,
               jint clip_h);
-  void SetScrollForHWFrame(JNIEnv* env, jobject obj,
-                           int scroll_x, int scroll_y);
+  bool PrepareDrawGL(JNIEnv* env, jobject obj,
+                     int scroll_x, int scroll_y);
   jint GetAwDrawGLViewContext(JNIEnv* env, jobject obj);
   base::android::ScopedJavaLocalRef<jobject> CapturePicture(JNIEnv* env,
                                                             jobject obj);
@@ -138,6 +138,7 @@ class AwContents : public FindHelper::Listener,
   virtual void OnPageScaleFactorChanged(float page_scale_factor) OVERRIDE;
 
   // BrowserViewRenderer::Client implementation.
+  virtual void RequestProcessMode() OVERRIDE;
   virtual void Invalidate() OVERRIDE;
   virtual void OnNewPicture() OVERRIDE;
   virtual gfx::Point GetLocationOnScreen() OVERRIDE;
