cee2456fe9cbe248de68847597217514db4ff3ae
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-09 03:52:31 +0000

    REGRESSION: Calendar picker for input[type=datetime-local][step] doesn't show available days correctly.
    Calendar picker doesn't show two available days per a week for <input
    type=datetime-local value="2012-11-20T00:00" step="302400">.
    
    CalendarPicker.prototype._stepMismatch should assume the specified date is
    matched if a multiple of the step value is in 24 hours range of the day in a
    case of input[type=datetime-local].
    
    BUG=239328
    TEST=automated; This CL adds a layout test.
    
    Review URL: https://chromiumcodereview.appspot.com/14984007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150001 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/Resources/pagepopups/calendarPicker.js
+++ third_party/WebKit/Source/core/Resources/pagepopups/calendarPicker.js
@@ -3853,7 +3853,8 @@ CalendarPicker.prototype._setHighlight = function(dayOrWeekOrMonth) {
  * @return {!boolean}
  */
 CalendarPicker.prototype._stepMismatch = function(value) {
-    return (value - this.config.stepBase) % this.config.step != 0;
+    var nextAllowedValue = Math.ceil((value - this.config.stepBase) / this.config.step) * this.config.step + this.config.stepBase;
+    return nextAllowedValue >= value + this._dateTypeConstructor.DefaultStep;
 };
 
 /**
