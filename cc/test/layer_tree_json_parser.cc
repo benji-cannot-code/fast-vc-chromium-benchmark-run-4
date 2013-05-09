27554ddd7a5f67bfe75f76dad3944c50907ac680
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 23:26:19 +0000

    cc: Add perf test for scrolling with impl-side painting
    This patch adds cc perf test support for scrolling a page with impl-side
    painting. It also introduces a specific perf test for a page with
    several large transformed layers.
    
    BUG=b/8364918
    TEST=ImplSidePaintingPerfTest.HeavyPage
    
    Review URL: https://chromiumcodereview.appspot.com/15068002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199305 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/layer_tree_json_parser.cc
+++ cc/test/layer_tree_json_parser.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/content_layer.h"
 #include "cc/layers/layer.h"
 #include "cc/layers/nine_patch_layer.h"
+#include "cc/layers/picture_layer.h"
 #include "cc/layers/solid_color_layer.h"
 
 namespace cc {
@@ -63,6 +64,8 @@ scoped_refptr<Layer> ParseTreeFromValue(base::Value* val,
         gfx::Rect(aperture_x, aperture_y, aperture_width, aperture_height));
 
     new_layer = nine_patch_layer;
+  } else if (layer_type == "PictureLayer") {
+    new_layer = PictureLayer::Create(content_client);
   } else {  // Type "Layer" or "unknown"
     new_layer = Layer::Create();
   }
