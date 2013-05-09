412f03601e2e9fa271075d29774b4fd9133796bb
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 00:26:58 +0000

    Remove dependency with third_party/webkit for worker_devtools_manager.cc
    There does not seem any reason for including WebKit::WebDevToolsAgent
    
    
    BUG=237267
    
    Review URL: https://chromiumcodereview.appspot.com/14779005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199065 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/worker_devtools_manager.cc
+++ content/browser/devtools/worker_devtools_manager.cc
@@ -20,11 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/child_process_data.h"
 #include "content/public/common/process_type.h"
-#include "third_party/WebKit/Source/Platform/chromium/public/WebCString.h"
-#include "third_party/WebKit/Source/Platform/chromium/public/WebString.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebDevToolsAgent.h"
-
-using WebKit::WebDevToolsAgent;
 
 namespace content {
 
