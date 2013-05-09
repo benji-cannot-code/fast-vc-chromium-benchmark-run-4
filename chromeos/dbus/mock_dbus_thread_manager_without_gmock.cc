17c8efa16d52aa3956ec9e96261cdb6bb6a178a4
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 09:23:42 +0000

    dbus: Add FakeGsmSMSClient and replace the stub.
    BUG=234498
    TEST=trybots
    R=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/14949007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199173 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/mock_dbus_thread_manager_without_gmock.cc
+++ chromeos/dbus/mock_dbus_thread_manager_without_gmock.cc
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/fake_bluetooth_profile_manager_client.h"
 #include "chromeos/dbus/fake_cros_disks_client.h"
 #include "chromeos/dbus/fake_cryptohome_client.h"
+#include "chromeos/dbus/fake_gsm_sms_client.h"
 #include "chromeos/dbus/fake_image_burner_client.h"
 #include "chromeos/dbus/fake_old_bluetooth_adapter_client.h"
 #include "chromeos/dbus/fake_old_bluetooth_device_client.h"
@@ -44,6 +45,7 @@ MockDBusThreadManagerWithoutGMock::MockDBusThreadManagerWithoutGMock()
         new FakeBluetoothProfileManagerClient()),
     fake_cros_disks_client_(new FakeCrosDisksClient),
     fake_cryptohome_client_(new FakeCryptohomeClient),
+    fake_gsm_sms_client_(new FakeGsmSMSClient),
     fake_image_burner_client_(new FakeImageBurnerClient),
     fake_session_manager_client_(new FakeSessionManagerClient),
     fake_shill_manager_client_(new FakeShillManagerClient),
@@ -198,8 +200,7 @@ ShillServiceClient*
 }
 
 GsmSMSClient* MockDBusThreadManagerWithoutGMock::GetGsmSMSClient() {
-  NOTIMPLEMENTED();
-  return NULL;
+  return fake_gsm_sms_client_.get();
 }
 
 ImageBurnerClient* MockDBusThreadManagerWithoutGMock::GetImageBurnerClient() {
