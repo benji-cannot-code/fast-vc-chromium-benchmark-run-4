34a01c41e051128a7695bff9d4ec7d2829e69f15
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-09 19:29:37 +0000

    Remove more unneeded bits of webkit-patch and webkitpy.
    Remove bash completion for no longer existing commands.
    
    Remove from the Port class the no longer needed methods
    update_webkit_command(), prepare_changelog_command(),
    build_webkit_command(), run_javascriptcore_tests_command().
    
    Remove more webkkit-patch commands that used bugzilla or were
    for changelogs:
    analyze-changelog, update, commit-message, clean-pending-commit,
    clean-review-queue, assign-to-committer, obsolete-attachments,
    attach-to-bug, post, land-safely, has-landed, prepare, upload,
    edit-changelogs, post-commits, mark-bug-fixed, create-bug
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/15085008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150041 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/__init__.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/__init__.py
@@ -2,7 +2,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Required for Python to search this directory for module files
 
 from webkitpy.tool.commands.adduserstogroups import AddUsersToGroups
-from webkitpy.tool.commands.analyzechangelog import AnalyzeChangeLog
 from webkitpy.tool.commands.applywatchlistlocal import ApplyWatchListLocal
 from webkitpy.tool.commands.bugfortest import BugForTest
 from webkitpy.tool.commands.chromechannels import ChromeChannels
@@ -13,5 +12,4 @@ from webkitpy.tool.commands.prettydiff import PrettyDiff
 from webkitpy.tool.commands.queries import *
 from webkitpy.tool.commands.rebaseline import Rebaseline
 from webkitpy.tool.commands.rebaselineserver import RebaselineServer
-from webkitpy.tool.commands.upload import *
 from webkitpy.tool.commands.suggestnominations import *
