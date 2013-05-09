a2b7d3eae1cb64ac43ef18e8ce54a03bd1cd2bad
apiccion@chromium.org <apiccion@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-09 03:49:48 +0000

    Added JNI method to allow java code parse the scheme from a url string using GURL.
    BUG=223930
    
    Review URL: https://chromiumcodereview.appspot.com/14614005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199096 0039d316-1c4b-4281-b951-d872f2087c98

--- net/android/gurl_utils.cc
+++ net/android/gurl_utils.cc
@@ -18,6 +18,13 @@ jstring GetOrigin(JNIEnv* env, jclass clazz, jstring url) {
       host.GetOrigin().spec()).Release();
 }
 
+jstring GetScheme(JNIEnv* env, jclass clazz, jstring url) {
+  GURL host(base::android::ConvertJavaStringToUTF16(env, url));
+
+  return base::android::ConvertUTF8ToJavaString(env,
+      host.scheme()).Release();
+}
+
 bool RegisterGURLUtils(JNIEnv* env) {
   return RegisterNativesImpl(env);
 }
