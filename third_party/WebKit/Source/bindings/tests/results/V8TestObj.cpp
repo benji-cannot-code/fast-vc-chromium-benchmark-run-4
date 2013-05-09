e7f37ada476afb2e919bc766d78f58cc352fc406
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-09 01:26:02 +0000

    Make sure the main header is not included twice in the generated implementation
    r149976 updated the bindings generator so that the main header is not included
    twice in the generated implementation under most circumstances. However, the
    fix was incomplete as the bug is still reproduceable if an IDL interface refers
    to itself (i.e. one of the attributes or arguments has the interface as type).
    
    This patch addresses the issue by using the implIncludes hash map for the
    main header include, instead of hardcoding it in the header section. The main
    include is then treated as a special case in WriteData subroutine to make
    sure it is printed right after the "config.h" include and followed by a blank
    line.
    
    Review URL: https://chromiumcodereview.appspot.com/14670022
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@149994 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestObj.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestObj.cpp
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8ScriptProfile.h"
 #include "V8TestCallback.h"
 #include "V8TestNode.h"
-#include "V8TestObj.h"
 #include "V8TestSubObj.h"
 #include "V8bool.h"
 #include "bindings/v8/Dictionary.h"
