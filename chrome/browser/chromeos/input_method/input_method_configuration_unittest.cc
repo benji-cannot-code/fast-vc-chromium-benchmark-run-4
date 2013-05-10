768a4360d98130382a4d37c122d89e3753e14977
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 03:32:33 +0000

    Add unit tests for ibus-daemon connection and component extension ime initialization.
    With this change, ui_unittests is also need update.
    We can't call or even check the call count of IBusClient/IBUsInputContextClient stuff before InitIBusBus.
    
    
    BUG=None
    TEST=ran unit_tests both Release/Debug configuration.
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=198975
    
    Review URL: https://chromiumcodereview.appspot.com/14845003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199357 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_configuration_unittest.cc
+++ chrome/browser/chromeos/input_method/input_method_configuration_unittest.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/chromeos/input_method/input_method_configuration.h"
 #include "chrome/browser/chromeos/input_method/mock_input_method_manager.h"
+#include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/dbus/mock_dbus_thread_manager_without_gmock.h"
 #include "content/public/browser/browser_thread.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "ui/base/ime/text_input_test_support.h"
@@ -15,9 +17,12 @@ namespace input_method {
 class InputMethodConfigurationTest : public testing::Test {
  public:
   virtual void SetUp() {
+    chromeos::DBusThreadManager::InitializeForTesting(
+        new chromeos::MockDBusThreadManagerWithoutGMock());
   }
 
   virtual void TearDown() {
+    chromeos::DBusThreadManager::Shutdown();
   }
 };
 
