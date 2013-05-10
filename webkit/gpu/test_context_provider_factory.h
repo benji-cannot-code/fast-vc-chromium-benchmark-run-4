7033b93bc8523a021f10acd7a5055ddac1871e7f
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 02:42:09 +0000

    Clean up unused includes and code in webkit/gpu
    BUG=239107
    
    Review URL: https://chromiumcodereview.appspot.com/14759027
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199341 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/gpu/test_context_provider_factory.h
+++ webkit/gpu/test_context_provider_factory.h
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/ref_counted.h"
 #include "webkit/gpu/webkit_gpu_export.h"
-#include "webkit/support/webkit_support.h"
 
 namespace cc {
 class ContextProvider;
