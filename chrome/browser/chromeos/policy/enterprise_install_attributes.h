b038adb767f366075f85a96d543e4283864de3e8
davidyu@chromium.org <davidyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 05:00:51 +0000

    Add a unit test for Enterprise platform key API.
    BUG=chromium:236624
    TEST=unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/14638010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199374 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/enterprise_install_attributes.h
+++ chrome/browser/chromeos/policy/enterprise_install_attributes.h
@@ -52,7 +52,7 @@ class EnterpriseInstallAttributes {
   static const char kAttrEnterpriseOwned[];
   static const char kAttrEnterpriseUser[];
 
-  explicit EnterpriseInstallAttributes(
+  EnterpriseInstallAttributes(
       chromeos::CryptohomeLibrary* cryptohome,
       chromeos::CryptohomeClient* cryptohome_client);
   ~EnterpriseInstallAttributes();
@@ -97,6 +97,13 @@ class EnterpriseInstallAttributes {
   // are not locked yet will be DEVICE_MODE_UNKNOWN.
   DeviceMode GetMode();
 
+ protected:
+  bool device_locked_;
+  std::string registration_user_;
+  std::string registration_domain_;
+  std::string registration_device_id_;
+  DeviceMode registration_mode_;
+
  private:
   // Decodes the install attributes provided in |attr_map|.
   void DecodeInstallAttributes(
@@ -125,11 +132,6 @@ class EnterpriseInstallAttributes {
   chromeos::CryptohomeLibrary* cryptohome_;
   chromeos::CryptohomeClient* cryptohome_client_;
 
-  bool device_locked_;
-  std::string registration_user_;
-  std::string registration_domain_;
-  std::string registration_device_id_;
-  DeviceMode registration_mode_;
   base::WeakPtrFactory<EnterpriseInstallAttributes> weak_ptr_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(EnterpriseInstallAttributes);
--- chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h
+++ chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h
@@ -40,6 +40,12 @@ class PrefRegistrySyncable;
 namespace extensions {
 
 class EPKPChallengeKeyBase : public AsyncExtensionFunction {
+ public:
+  static const char kChallengeBadBase64Error[];
+  static const char kDevicePolicyDisabledError[];
+  static const char kResponseBadBase64Error[];
+  static const char kSignChallengeFailedError[];
+
  protected:
   enum PrepareKeyResult {
     PREPARE_KEY_OK = 0,
@@ -49,6 +55,11 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
   };
 
   EPKPChallengeKeyBase();
+  EPKPChallengeKeyBase(
+      chromeos::CryptohomeClient* cryptohome_client,
+      cryptohome::AsyncMethodCaller* async_caller,
+      chromeos::attestation::AttestationFlow* attestation_flow,
+      policy::EnterpriseInstallAttributes* install_attributes);
   virtual ~EPKPChallengeKeyBase();
 
   // Returns a trusted value from CroSettings indicating if the device
@@ -78,7 +89,8 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
 
   chromeos::CryptohomeClient* cryptohome_client_;
   cryptohome::AsyncMethodCaller* async_caller_;
-  scoped_ptr<chromeos::attestation::AttestationFlow> attestation_flow_;
+  chromeos::attestation::AttestationFlow* attestation_flow_;
+  scoped_ptr<chromeos::attestation::AttestationFlow> default_attestation_flow_;
 
  private:
   void DoesKeyExistCallback(
@@ -101,6 +113,18 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
 };
 
 class EPKPChallengeMachineKey : public EPKPChallengeKeyBase {
+ public:
+  static const char kDomainsDontMatch[];
+  static const char kGetCertificateFailedError[];
+  static const char kNonEnterpriseDeviceError[];
+
+  EPKPChallengeMachineKey();
+  EPKPChallengeMachineKey(
+      chromeos::CryptohomeClient* cryptohome_client,
+      cryptohome::AsyncMethodCaller* async_caller,
+      chromeos::attestation::AttestationFlow* attestation_flow,
+      policy::EnterpriseInstallAttributes* install_attributes);
+
  protected:
   virtual bool RunImpl() OVERRIDE;
 
@@ -125,6 +149,19 @@ typedef EPKPChallengeMachineKey
 
 class EPKPChallengeUserKey : public EPKPChallengeKeyBase {
  public:
+  static const char kDomainsDontMatchError[];
+  static const char kExtensionNotWhitelistedError[];
+  static const char kGetCertificateFailedError[];
+  static const char kKeyRegistrationFailedError[];
+  static const char kUserPolicyDisabledError[];
+
+  EPKPChallengeUserKey();
+  EPKPChallengeUserKey(
+      chromeos::CryptohomeClient* cryptohome_client,
+      cryptohome::AsyncMethodCaller* async_caller,
+      chromeos::attestation::AttestationFlow* attestation_flow,
+      policy::EnterpriseInstallAttributes* install_attributes);
+
   static void RegisterUserPrefs(user_prefs::PrefRegistrySyncable* registry);
 
  protected:
