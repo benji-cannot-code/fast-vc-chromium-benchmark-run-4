99736759241984f2210ccc8f238189e88fb4296a
dalecurtis@google.com <dalecurtis@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 00:30:15 +0000

    Remove function level static initializers from VectorMath.
    Since VectorMath routines (and other features) are used both
    in the renderer process and the browser process, relying on
    InitializeMediaLibrary() alone is insufficient.
    
    Enter: InitializeCPUSpecificMediaFeatures(), a method which
    can be called from the browser process without initializing
    any of the loadable media modules.
    
    BUG=224662
    TEST=media_unittests
    R=scherkus@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/15044005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199315 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -47,6 +47,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/result_codes.h"
 #include "crypto/nss_util.h"
 #include "media/audio/audio_manager.h"
+#include "media/base/media.h"
 #include "net/base/network_change_notifier.h"
 #include "net/socket/client_socket_factory.h"
 #include "net/ssl/ssl_config_service.h"
@@ -373,6 +374,8 @@ void BrowserMainLoop::MainMessageLoopStart() {
   power_monitor_.reset(new base::PowerMonitor);
   hi_res_timer_manager_.reset(new HighResolutionTimerManager);
   network_change_notifier_.reset(net::NetworkChangeNotifier::Create());
+
+  media::InitializeCPUSpecificMediaFeatures();
   audio_manager_.reset(media::AudioManager::Create());
 
 #if !defined(OS_IOS)
