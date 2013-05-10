daacd088b68f8e4d115a16a632013d8bca384e2a
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 21:19:15 +0000

    Stacking panel support for Linux.
    This patch implements NativePanelStackWindow interface for Linux (GTK). It includes:
    1) Creates a small background window that provides the aggregated taskbar icon.
    2) Bounds updates are performed immediately since no animation is supported on GTK.
    3) The system-minimize button is not shown for certain desktop environment, like Unity,
       that does not support it well.
    
    BUG=238904
    TEST=stacking tests enabled for GTK
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=199401
    
    Review URL: https://chromiumcodereview.appspot.com/14580018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199539 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/panels/panel_manager.cc
+++ chrome/browser/ui/panels/panel_manager.cc
@@ -23,6 +23,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/notification_source.h"
 
 #if defined(TOOLKIT_GTK)
+#include "base/environment.h"
+#include "base/nix/xdg_util.h"
 #include "ui/base/x/x11_util.h"
 #endif
 
@@ -145,10 +147,21 @@ bool PanelManager::ShouldUsePanels(const std::string& extension_id) {
 
 // static
 bool PanelManager::IsPanelStackingEnabled() {
-#if defined(OS_WIN) || defined(OS_MACOSX)
   return true;
+}
+
+// static
+bool PanelManager::CanUseSystemMinimize() {
+#if defined(TOOLKIT_GTK)
+  static base::nix::DesktopEnvironment desktop_env =
+      base::nix::DESKTOP_ENVIRONMENT_OTHER;
+  if (desktop_env == base::nix::DESKTOP_ENVIRONMENT_OTHER) {
+    scoped_ptr<base::Environment> env(base::Environment::Create());
+    desktop_env = base::nix::GetDesktopEnvironment(env.get());
+  }
+  return desktop_env != base::nix::DESKTOP_ENVIRONMENT_UNITY;
 #else
-  return false;
+  return true;
 #endif
 }
 
