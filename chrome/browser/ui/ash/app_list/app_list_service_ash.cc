b8a01596a8bedee7f6bab5e94a13a11c2e7bca68
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 13:44:53 +0000

    Introduce AppListServiceImpl to share parts of the AppListService implementation between platforms.
    Moves app_list_controller_cocoa.mm to app_list_service_mac.mm and gives
    Windows and OSX app list service implementations a common parent,
    AppListServiceImpl, to share implementation details while keeping the
    AppListService interface minimal (and now an pure abstract base).
    
    R=benwells@chromium.org
    BUG=138633
    TEST=Enabled AppListControllerBrowserTest.SwitchAppListProfiles for mac
    
    Review URL: https://chromiumcodereview.appspot.com/14603002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199474 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/app_list/app_list_service_ash.cc
+++ chrome/browser/ui/ash/app_list/app_list_service_ash.cc
@@ -9,12 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/singleton.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/ui/app_list/app_list_service.h"
+#include "chrome/browser/ui/app_list/app_list_service_impl.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_controller.h"
 
 namespace {
 
-class AppListServiceAsh : public AppListService {
+class AppListServiceAsh : public AppListServiceImpl {
  public:
   static AppListServiceAsh* GetInstance() {
     return Singleton<AppListServiceAsh,
@@ -32,6 +32,7 @@ class AppListServiceAsh : public AppListService {
   virtual void ShowAppList(Profile* default_profile) OVERRIDE;
   virtual bool IsAppListVisible() const OVERRIDE;
   virtual void DismissAppList() OVERRIDE;
+  virtual void EnableAppList() OVERRIDE;
 
   DISALLOW_COPY_AND_ASSIGN(AppListServiceAsh);
 };
@@ -58,6 +59,8 @@ void AppListServiceAsh::DismissAppList() {
     ash::Shell::GetInstance()->ToggleAppList(NULL);
 }
 
+void AppListServiceAsh::EnableAppList() {}
+
 }  // namespace
 
 namespace chrome {
@@ -67,3 +70,18 @@ AppListService* GetAppListServiceAsh() {
 }
 
 }  // namespace chrome
+
+// Windows Ash additionally supports a native UI. See app_list_service_win.cc.
+#if !defined(OS_WIN)
+
+// static
+AppListService* AppListService::Get() {
+  return chrome::GetAppListServiceAsh();
+}
+
+// static
+void AppListService::InitAll(Profile* initial_profile) {
+  Get()->Init(initial_profile);
+}
+
+#endif  // !defined(OS_WIN)
