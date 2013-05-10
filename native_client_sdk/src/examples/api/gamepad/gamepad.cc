fa2c580aaf85aa59f75a277d1ebab0543bf3aa70
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 23:50:09 +0000

    [NaCl SDK] More windows fixes.
    Missed the necessary changes for gamepad.cc
    
    * undef min, max
    * Disable warning about this in initializer list
    
    BUG=none
    TBR=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/15072004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199576 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/api/gamepad/gamepad.cc
+++ native_client_sdk/src/examples/api/gamepad/gamepad.cc
@@ -17,6 +17,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/cpp/var.h"
 #include "ppapi/utility/completion_callback_factory.h"
 
+#ifdef WIN32
+#undef min
+#undef max
+
+// Allow 'this' in initializer list
+#pragma warning(disable : 4355)
+#endif
+
 class GamepadInstance : public pp::Instance {
  public:
   explicit GamepadInstance(PP_Instance instance);
