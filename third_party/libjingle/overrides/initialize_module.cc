7ea3bb2244a6fc0b9f99021231a4b6719462c2b5
tommi@chromium.org <tommi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 12:57:48 +0000

    Fix libpeerconnection build after a recent libjingle roll that added an encoder factory to webrtc.
    TBR=sergeyu@chromium.org
    
    Review URL: https://codereview.chromium.org/14847015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199462 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libjingle/overrides/initialize_module.cc
+++ third_party/libjingle/overrides/initialize_module.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
+#include "init_webrtc.h"
 #include "talk/base/basictypes.h"
 #include "talk/media/webrtc/webrtcmediaengine.h"
 
@@ -20,13 +21,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ALLOC_EXPORT __attribute__((visibility("default")))
 #endif
 
-typedef cricket::MediaEngineInterface* (*CreateWebRtcMediaEngineFunction)(
-    webrtc::AudioDeviceModule* adm,
-    webrtc::AudioDeviceModule* adm_sc,
-    cricket::WebRtcVideoDecoderFactory* decoder_factory);
-typedef void (*DestroyWebRtcMediaEngineFunction)(
-    cricket::MediaEngineInterface* media_engine);
-
 #if !defined(OS_MACOSX)
 // These are used by our new/delete overrides in
 // allocator_shim/allocator_proxy.cc
@@ -37,7 +31,9 @@ DellocateFunction g_dealloc = NULL;
 // Forward declare of the libjingle internal factory and destroy methods for the
 // WebRTC media engine.
 cricket::MediaEngineInterface* CreateWebRtcMediaEngine(
-    webrtc::AudioDeviceModule* adm, webrtc::AudioDeviceModule* adm_sc,
+    webrtc::AudioDeviceModule* adm,
+    webrtc::AudioDeviceModule* adm_sc,
+    cricket::WebRtcVideoEncoderFactory* encoder_factory,
     cricket::WebRtcVideoDecoderFactory* decoder_factory);
 
 void DestroyWebRtcMediaEngine(cricket::MediaEngineInterface* media_engine);
@@ -69,6 +65,8 @@ bool InitializeModule(const CommandLine& command_line,
     // done the equivalent thing via the GetCommandLine() API.
     CommandLine::ForCurrentProcess()->AppendArguments(command_line, true);
 #endif
+
+    // TODO(tommi): Use SetLogMessageHandler.
     logging::InitLogging(
         FILE_PATH_LITERAL("libpeerconnection.log"),
         logging::LOG_TO_BOTH_FILE_AND_SYSTEM_DEBUG_LOG,
