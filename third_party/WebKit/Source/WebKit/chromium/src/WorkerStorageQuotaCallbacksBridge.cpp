5b25e474acbbb2e7e69fb368480f6d8de3d61ed5
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-10 02:24:17 +0000

    attempt to fix bots
    BUG=none
    TBR=dpranke
    
    Review URL: https://codereview.chromium.org/14621011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150070 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WorkerStorageQuotaCallbacksBridge.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WorkerStorageQuotaCallbacksBridge.cpp
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/CrossThreadTask.h"
 #include "core/workers/WorkerContext.h"
 #include "core/workers/WorkerLoaderProxy.h"
+#include "wtf/MainThread.h"
 
 using namespace WebCore;
 
