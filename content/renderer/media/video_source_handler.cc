ae4dee3ca8a4d4c0122ec626f91d6219a6a608ff
ronghuawu@chromium.org <ronghuawu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 09:53:14 +0000

    Replace ASSERT with DCHECK per chromium style guide.
    Review URL: https://chromiumcodereview.appspot.com/14969013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199442 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/video_source_handler.cc
+++ content/renderer/media/video_source_handler.cc
@@ -63,7 +63,7 @@ VideoSourceHandler::VideoSourceHandler(
 
 VideoSourceHandler::~VideoSourceHandler() {
   // All the opened readers should have been closed by now.
-  ASSERT(reader_to_receiver_.empty());
+  DCHECK(reader_to_receiver_.empty());
 }
 
 bool VideoSourceHandler::Open(const std::string& url,
@@ -89,7 +89,11 @@ bool VideoSourceHandler::Close(const std::string& url,
   }
   PpFrameReceiver* receiver =
       static_cast<PpFrameReceiver*>(GetReceiver(reader));
-  ASSERT(receiver != NULL);
+  if (!receiver) {
+    LOG(ERROR) << "VideoSourceHandler::Close - Failed to find receiver that "
+               << "is associated with the given reader.";
+    return false;
+  }
   receiver->SetReader(NULL);
   source->RemoveSink(receiver);
   reader_to_receiver_.erase(reader);
