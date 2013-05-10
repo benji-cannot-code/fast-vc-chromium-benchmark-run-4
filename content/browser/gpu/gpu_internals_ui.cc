98648ea31a1d13ad3cad0c986ca92c8cbb39e51e
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 20:42:52 +0000

    Refactor GpuDataManagerImpl to make it thread-safe, now and forever.
    The original impl of GpuDataManagerImpl is thread-safe, but gradurally it regressed.
    
    In order to make sure this class is thread-safe in the future, we move all code to GpuDataManagerImplPrivate, and make GpuDataManagerImpl a simple wrapper around GpuDataManagerImplPrivate's public functions, where each function call is guarded by lock, thus thread-safe.
    
    BUG=232556
    TEST=asan bots no longer crashes as described in crbug.com/232556
    R=joi@chromium.org, kbr@chromium.org, piman@chromium.org
    
    Review URL: https://codereview.chromium.org/14794006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199530 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_internals_ui.cc
+++ content/browser/gpu/gpu_internals_ui.cc
@@ -480,9 +480,7 @@ class GpuMessageHandler
 
   // GpuDataManagerObserver implementation.
   virtual void OnGpuInfoUpdate() OVERRIDE;
-
-  // Gpu switch handler.
-  void OnGpuSwitch();
+  virtual void OnGpuSwitching() OVERRIDE;
 
   // Messages
   void OnBrowserBridgeInitialized(const base::ListValue* list);
@@ -497,8 +495,6 @@ class GpuMessageHandler
   // DCHECK).
   bool observing_;
 
-  GpuDataManagerImpl::GpuSwitchCallback gpu_switch_callback_;
-
   DISALLOW_COPY_AND_ASSIGN(GpuMessageHandler);
 };
 
@@ -509,15 +505,11 @@ class GpuMessageHandler
 ////////////////////////////////////////////////////////////////////////////////
 
 GpuMessageHandler::GpuMessageHandler()
-    : observing_(false),
-      gpu_switch_callback_(base::Bind(&GpuMessageHandler::OnGpuSwitch,
-                                      base::Unretained(this))) {
+    : observing_(false) {
 }
 
 GpuMessageHandler::~GpuMessageHandler() {
   GpuDataManagerImpl::GetInstance()->RemoveObserver(this);
-  GpuDataManagerImpl::GetInstance()->RemoveGpuSwitchCallback(
-      gpu_switch_callback_);
 }
 
 /* BrowserBridge.callAsync prepends a requestID to these messages. */
@@ -584,11 +576,8 @@ void GpuMessageHandler::OnBrowserBridgeInitialized(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   // Watch for changes in GPUInfo
-  if (!observing_) {
+  if (!observing_)
     GpuDataManagerImpl::GetInstance()->AddObserver(this);
-    GpuDataManagerImpl::GetInstance()->AddGpuSwitchCallback(
-        gpu_switch_callback_);
-  }
   observing_ = true;
 
   // Tell GpuDataManager it should have full GpuInfo. If the
@@ -640,7 +629,7 @@ void GpuMessageHandler::OnGpuInfoUpdate() {
       *(gpu_info_val.get()));
 }
 
-void GpuMessageHandler::OnGpuSwitch() {
+void GpuMessageHandler::OnGpuSwitching() {
   GpuDataManagerImpl::GetInstance()->RequestCompleteGpuInfoIfNeeded();
 }
 
