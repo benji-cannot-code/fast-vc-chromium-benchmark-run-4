9a4c1883cfdc9504d54ea6bbc071db28c1f6ae24
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 22:02:40 +0000

    Revert 199535 "Add GetFullRequestHeaders, from URLRequestJob to ..."
    > Add GetFullRequestHeaders, from URLRequestJob to HttpNetworkTransaction.
    > 
    > Dev Tools displays the raw request and response headers.  Right now, it gets
    > them by snarfing them out of the NetLog, which is bad, as the NetLog is for
    > huamn consumption only.  I'm trying to refactor the DevToolsNetLogObserver
    > away; providing an alternate, supported path to get the request headers is the
    > first step.
    > 
    > BUG=196304
    > TEST=added to URLRequest, HttpTransaction, and HttpNetworkTransaction unittests
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12621011
    
    TBR=ttuttle@chromium.org
    
    Review URL: https://codereview.chromium.org/15054003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199553 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_transaction_unittest.h
+++ net/http/http_transaction_unittest.h
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/http/http_response_info.h"
 
 namespace net {
-class HttpRequestHeaders;
 class IOBuffer;
 }
 
@@ -188,9 +187,6 @@ class MockNetworkTransaction
                    const net::CompletionCallback& callback) OVERRIDE;
 
   virtual void StopCaching() OVERRIDE;
-
-  virtual bool GetFullRequestHeaders(
-      net::HttpRequestHeaders* headers) const OVERRIDE;
 
   virtual void DoneReading() OVERRIDE;
 
