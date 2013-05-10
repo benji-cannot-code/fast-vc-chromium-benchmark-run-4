2a2e0fa44adfb7697a5ceca84a9920f36428dc46
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 00:19:01 +0000

    Remove unused third_party/WebKit #includes from chrome/browser
    BUG=237267
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=198977
    
    Review URL: https://chromiumcodereview.appspot.com/15026005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199314 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/window_sizer/window_sizer_common_unittest.cc
+++ chrome/browser/ui/window_sizer/window_sizer_common_unittest.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_switches.h"
 #include "chrome/test/base/testing_profile.h"
 #include "testing/gtest/include/gtest/gtest.h"
-#include "third_party/WebKit/Source/WebKit/chromium/public/WebKit.h"
 
 TestMonitorInfoProvider::TestMonitorInfoProvider() {};
 
