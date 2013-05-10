2d60b68d3b547f09fa5ccfcea6f3ffd730eb1bbb
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-10 05:34:23 +0000

    Do not leak user agent Shadow DOM to script
    BUG=236139
    R=morrita@chromium.org
    
    Review URL: https://codereview.chromium.org/14731010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150074 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Node.cpp
+++ third_party/WebKit/Source/core/dom/Node.cpp
@@ -1285,7 +1285,10 @@ Element* Node::parentOrShadowHostElement() const
 
 Node* Node::insertionParentForBinding() const
 {
-    return resolveReprojection(this);
+    Node* node = resolveReprojection(this);
+    while (node && node->containingShadowRoot() && node->containingShadowRoot()->type() == ShadowRoot::UserAgentShadowRoot)
+        node = resolveReprojection(node);
+    return node;
 }
 
 bool Node::needsShadowTreeWalkerSlow() const
