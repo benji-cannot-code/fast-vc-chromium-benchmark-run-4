aaf67ff734523afe0e156c289843d1e2790dead5
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 07:22:57 +0000

    Revert 199406 "Restart apps that don't listen to onRestarted() b..."
    > Restart apps that don't listen to onRestarted() by sending them onLaunched().
    > 
    > We only do this if the app had windows opened the last time it was running,
    > otherwise apps that were running but had no windows may restart by opening
    > windows, which manifests as apps coming back on a restart after the user
    > already closed them.
    > 
    > BUG=230667,167740,162057
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14878008
    
    TBR=koz@chromium.org
    
    Review URL: https://codereview.chromium.org/14711009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199409 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/app_restore_service_factory.cc
+++ apps/app_restore_service_factory.cc
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "apps/app_restore_service_factory.h"
 
 #include "apps/app_restore_service.h"
-#include "chrome/browser/extensions/shell_window_registry.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_dependency_manager.h"
 
@@ -32,7 +31,6 @@ AppRestoreServiceFactory* AppRestoreServiceFactory::GetInstance() {
 AppRestoreServiceFactory::AppRestoreServiceFactory()
     : ProfileKeyedServiceFactory("AppRestoreService",
                                  ProfileDependencyManager::GetInstance()) {
-  DependsOn(extensions::ShellWindowRegistry::Factory::GetInstance());
 }
 
 AppRestoreServiceFactory::~AppRestoreServiceFactory() {
