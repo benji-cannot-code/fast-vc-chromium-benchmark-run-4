293543edd7e606e1ca618905fe9091594d092d57
sreeram@chromium.org <sreeram@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-10 06:26:47 +0000

    Instant: Fix keyword mode search.
    BUG=223929
    R=dougw@chromium.org
    TEST=See bug.
    
    Review URL: https://chromiumcodereview.appspot.com/14773023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199405 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/instant_controller.cc
+++ chrome/browser/ui/search/instant_controller.cc
@@ -318,6 +318,7 @@ bool InstantController::Update(const AutocompleteMatch& match,
     else
       HideOverlay();
     last_match_was_search_ = false;
+    last_suggestion_ = InstantSuggestion();
     return false;
   }
 
