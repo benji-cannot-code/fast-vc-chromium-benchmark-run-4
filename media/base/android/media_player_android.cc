f361cfd525acd746bc34711e4a49bb9312d0dfbc
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-11 19:29:22 +0000

    Fix the build for Google TV
    R=qinmin@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/15096003
    
    Patch from Yuncheol Heo <ycheo@chromium.org>.
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199628 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/media_player_android.cc
+++ media/base/android/media_player_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/base/android/media_player_android.h"
 
+#include "base/logging.h"
 #include "media/base/android/media_player_manager.h"
 
 namespace media {
