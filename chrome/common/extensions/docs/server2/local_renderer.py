886aec9db39a40ec54a56d2c8ec8c52b00a89111
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-12 04:37:14 +0000

    Revert 199633 "Doc server manifest page generation"
    > Doc server manifest page generation
    > 
    > The manifest page is now unique for apps and extensions and the property descriptions have their own pages.
    > 
    > BUG=233966
    > NOTRY=true
    > R=kalman@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/14273041
    
    TBR=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/14712010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199634 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/local_renderer.py
+++ chrome/common/extensions/docs/server2/local_renderer.py
@@ -19,7 +19,7 @@ class LocalRenderer(object):
   '''
   @staticmethod
   def Render(path):
-    assert not '\\' in path
+    assert not path.contains('\\')
     def render_path(path):
       return RenderServlet(Request(path, 'http://localhost', {}),
                            _LocalRenderServletDelegate(),
