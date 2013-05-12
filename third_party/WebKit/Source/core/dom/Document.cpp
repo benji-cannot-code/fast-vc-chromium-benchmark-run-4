1b2a223dfdc3e03cc8571cc168e572d5fa8ee776
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-12 07:33:18 +0000

    Remove redundant call to removeAllEventListeners in Document::open.
    Document::open already calls Document::removeAllEventListeners, which
    removes the event listeners from the DOMWindow it's attached to, so
    there's no need to do it manually here as well.
    
    Review URL: https://chromiumcodereview.appspot.com/15039024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150175 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2049,9 +2049,6 @@ void Document::open(Document* ownerDocument)
     if (ScriptableDocumentParser* parser = scriptableDocumentParser())
         parser->setWasCreatedByScript(true);
 
-    if (DOMWindow* domWindow = this->domWindow())
-        domWindow->removeAllEventListeners();
-
     if (m_frame)
         m_frame->loader()->didExplicitOpen();
 }
