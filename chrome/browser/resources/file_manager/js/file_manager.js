dca3caddb88d30246b00c476aecd52a9a4f9eadf
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 05:10:43 +0000

    Fix legacy UI in Files.app.
    This patch fixes javascript caused by performing operations on the middle panel which doesn't exist in the legacy UI.
    
    TEST=Run chrome in the legacy UI mode (about:flags or --file-manager-legacy) and run Files.app.
    BUG=240148
    R=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/14799005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199657 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -1063,10 +1063,11 @@ DialogType.isModal = function(type) {
   FileManager.prototype.initSidebar_ = function() {
     this.directoryTree_ = this.dialogDom_.querySelector('#directory-tree');
     DirectoryTree.decorate(this.directoryTree_, this.directoryModel_);
-    this.directoryTree_.addEventListener('content-updated', function() {
-      this.updateMiddleBarVisibility_(true);
-    }.bind(this));
     if (util.platform.newUI()) {
+      this.directoryTree_.addEventListener('content-updated', function() {
+        this.updateMiddleBarVisibility_(true);
+      }.bind(this));
+
       this.volumeList_ = this.dialogDom_.querySelector('#volume-list');
       VolumeList.decorate(this.volumeList_, this.directoryModel_);
     }
@@ -1429,11 +1430,12 @@ DialogType.isModal = function(type) {
           this.table_.normalizeColumns();
       }
       this.table_.redraw();
-      this.volumeList_.redraw();
     }
 
     if (!util.platform.newUI())
       this.breadcrumbs_.truncate();
+    else
+      this.volumeList_.redraw();
 
     // Hide the search box if there is not enough space.
     if (util.platform.newUI())
@@ -2548,7 +2550,8 @@ DialogType.isModal = function(type) {
       }
       this.table_.list.endBatchUpdates();
       this.grid_.endBatchUpdates();
-      this.updateMiddleBarVisibility_();
+      if (util.platform.newUI())
+        this.updateMiddleBarVisibility_();
       this.scanCompletedTimer_ = null;
     }.bind(this), 50);
   };
@@ -2581,7 +2584,8 @@ DialogType.isModal = function(type) {
       this.hideSpinnerLater_();
       this.table_.list.endBatchUpdates();
       this.grid_.endBatchUpdates();
-      this.updateMiddleBarVisibility_();
+      if (util.platform.newUI())
+        this.updateMiddleBarVisibility_();
       this.scanUpdatedTimer_ = null;
     }.bind(this), 200);
   };
@@ -2610,7 +2614,8 @@ DialogType.isModal = function(type) {
       this.scanInProgress_ = false;
       this.table_.list.endBatchUpdates();
       this.grid_.endBatchUpdates();
-      this.updateMiddleBarVisibility_();
+      if (util.platform.newUI())
+        this.updateMiddleBarVisibility_();
     }
   };
 
