2ff03ddc406612cb4dfb6dcdd4424985c2396841
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 09:54:28 +0000

    Use the correct output index to get state for outputs.
    BUG=239412
    TEST=manual. see bug for repro step.
    
    Review URL: https://chromiumcodereview.appspot.com/14699004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199695 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/display/output_configurator_unittest.cc
+++ chromeos/display/output_configurator_unittest.cc
@@ -210,7 +210,7 @@ class TestStateController : public OutputConfigurator::StateController {
 
   // OutputConfigurator::StateController overrides:
   virtual OutputState GetStateForOutputs(
-      const std::vector<OutputInfo>& outputs) const OVERRIDE { return state_; }
+      const OutputSnapshotList& outputs) const OVERRIDE { return state_; }
 
  private:
   OutputState state_;
