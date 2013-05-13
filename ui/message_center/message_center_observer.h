20f2c5e7ab258ec3e84185fd98c4d7d458476817
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 19:02:47 +0000

    Revert 199777 "mac: Minor tweaks to notifications code."
    Broke: NotificationControllerTest.Update: 
    ../../ui/message_center/cocoa/notification_controller_unittest.mm:161: Failure
    Value of: message_center::kNotificationIconSize
    Actual: 80
    Expected: NSHeight([[controller view] frame])
    Which is: 86
    
    (which the trybots don't run)
    
    > mac: Minor tweaks to notifications code.
    > 
    > Fixes the lower padding, and simplifies the code a bit.
    > 
    > BUG=none
    > TEST=Open a notification with more than 2 lines of text. Bottom margin doesn't
    > look too small.
    > 
    > R=rsesek@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/14735009
    
    TBR=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/14617022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199791 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/message_center_observer.h
+++ ui/message_center/message_center_observer.h
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef UI_MESSAGE_CENTER_MESSAGE_CENTER_OBSERVER_H_
 #define UI_MESSAGE_CENTER_MESSAGE_CENTER_OBSERVER_H_
 
-#include <string>
-
 #include "ui/message_center/message_center_export.h"
 
 namespace message_center {
