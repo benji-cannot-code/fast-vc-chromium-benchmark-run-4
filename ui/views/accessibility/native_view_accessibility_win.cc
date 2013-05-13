982dc7c42d2202ba778ad8e94f666e71605e260d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 15:53:52 +0000

    views: views target should not include from ui/views/controls/webview.
    Remove unused include of ui/views/controls/webview/ from accessibility file.
    It is wrong to include from it anyway because views target should not depend
    on content.
    
    TBR=ben@chromium.org,dmazzoni@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15105004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199740 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/accessibility/native_view_accessibility_win.cc
+++ ui/views/accessibility/native_view_accessibility_win.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/accessibility_misc_utils.h"
 #include "ui/base/win/atl_module.h"
 #include "ui/views/controls/button/custom_button.h"
-#include "ui/views/controls/webview/webview.h"
 #include "ui/views/focus/focus_manager.h"
 #include "ui/views/focus/view_storage.h"
 #include "ui/views/win/hwnd_util.h"
