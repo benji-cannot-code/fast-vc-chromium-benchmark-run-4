911c6bc8cd6c9d2ccd3f9513b6fe78472440dd99
rnk@chromium.org <rnk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 15:17:40 +0000

    Fix base::Value leak in new EPKP test introduced in r199374
    TBR=davidyu@chromium.org
    BUG=236624
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/14763007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199731 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api_unittest.cc
+++ chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api_unittest.cc
@@ -251,7 +251,7 @@ TEST_F(EPKPChallengeMachineKeyTest, Success) {
                   "google.com", "device_id", _, "challenge", _))
       .Times(1);
 
-  base::Value* value = utils::RunFunctionAndReturnSingleResult(
+  scoped_ptr<base::Value> value = utils::RunFunctionAndReturnSingleResult(
       func_.get(), kArgs, browser(), utils::NONE);
 
   std::string response;
@@ -407,7 +407,7 @@ TEST_F(EPKPChallengeUserKeyTest, Success) {
                                         "attest-ent-user", _))
       .Times(1);
 
-  base::Value* value = utils::RunFunctionAndReturnSingleResult(
+  scoped_ptr<base::Value> value = utils::RunFunctionAndReturnSingleResult(
       func_.get(), kArgs, browser(), utils::NONE);
 
   std::string response;
