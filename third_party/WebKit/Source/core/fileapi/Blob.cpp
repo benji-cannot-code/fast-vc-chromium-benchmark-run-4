731cc4e8aa7ca2b7ec4e7a708105b2d1dfa62221
tyoshino@chromium.org <tyoshino@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-13 05:15:35 +0000

    Clean up no longer used enum and inclusion for collecting Blob histogram.
    Review URL: https://chromiumcodereview.appspot.com/14984008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150193 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fileapi/Blob.cpp
+++ third_party/WebKit/Source/core/fileapi/Blob.cpp
@@ -37,20 +37,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/fileapi/File.h"
 #include "core/fileapi/ThreadableBlobRegistry.h"
 #include "core/inspector/ScriptCallStack.h"
-#include "core/platform/HistogramSupport.h"
 
 namespace WebCore {
-
-namespace {
-
-// Used in histograms to see when we can actually deprecate the prefixed slice.
-enum SliceHistogramEnum {
-    SliceWithoutPrefix,
-    SliceWithPrefix,
-    SliceHistogramEnumMax,
-};
-
-} // namespace
 
 Blob::Blob()
     : m_size(0)
