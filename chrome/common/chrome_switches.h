480ada6afba582fa58fcc7bd12330a8186f850d8
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 23:27:26 +0000

    Add a command-line switch for the URL of spelling service feedback
    This CL adds a command-line switch that enables changing the URL of where the
    spelling service feedback data will be sent. This switch is temporary present
    for testing. Once spelling service feedback testing is complete, rouslan@ will
    remove the flag.
    
    BUG=170514
    
    Review URL: https://chromiumcodereview.appspot.com/15054010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199849 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -347,6 +347,7 @@ extern const char kSpeculativeResourcePrefetchingDisabled[];
 extern const char kSpeculativeResourcePrefetchingLearning[];
 extern const char kSpdyProxyAuthOrigin[];
 extern const char kSpeculativeResourcePrefetchingEnabled[];
+extern const char kSpellingServiceFeedbackUrl[];
 extern const char kSSLVersionMax[];
 extern const char kSSLVersionMin[];
 extern const char kStartMaximized[];
