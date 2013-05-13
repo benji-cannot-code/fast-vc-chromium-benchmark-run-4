01dd39e4fb91bb6d2fdd4f8dfd340c37092b8f9b
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-13 07:16:44 +0000

    Fix wrong assertions in Range::textNodeSplit
    Text::splitText calls Node::insertBefore, in which a DOM mutatino event handler
    can update the DOM structure. The function calls Document::textNodeSplit too,
    and Range::textNodeSplit called by Document::textNodeSplit assumed no one
    updated the DOM structure.
    
    BUG=240163
    TEST=This CL adds a layout test.
    
    Review URL: https://chromiumcodereview.appspot.com/15067013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150198 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -1808,7 +1808,11 @@ static inline void boundaryTextNodesSplit(RangeBoundaryPoint& boundary, Text* ol
     unsigned boundaryOffset = boundary.offset();
     if (boundaryOffset <= oldNode->length())
         return;
-    boundary.set(oldNode->nextSibling(), boundaryOffset - oldNode->length(), 0);
+    Node* next = oldNode->nextSibling();
+    if (!next || !next->isTextNode())
+        boundary.set(oldNode, oldNode->length(), 0);
+    else
+        boundary.set(next, std::min(boundaryOffset - oldNode->length(), toText(next)->length()), 0);
 }
 
 void Range::textNodeSplit(Text* oldNode)
@@ -1817,8 +1821,6 @@ void Range::textNodeSplit(Text* oldNode)
     ASSERT(oldNode->document() == m_ownerDocument);
     ASSERT(oldNode->parentNode());
     ASSERT(oldNode->isTextNode());
-    ASSERT(oldNode->nextSibling());
-    ASSERT(oldNode->nextSibling()->isTextNode());
     boundaryTextNodesSplit(m_start, oldNode);
     boundaryTextNodesSplit(m_end, oldNode);
 }
