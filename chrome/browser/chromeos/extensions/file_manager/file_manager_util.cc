26b21e1bdad9e97c95bd1a2294b67601151b1f70
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-13 14:01:21 +0000

    Avoid reopening windows when closing and opening a lid while using a usb stick.
    After migrating to Files.app V2, reusing tabs stopped working, since a platform app's windows are not browser windows. Therefore, iterating over browser windows for finding a tab with the same url was always failing. As a resul, the new window was always being recreated. This patch fixes this problem by moving the reusing/creating decision logic to background.js, which is responsible for creating Files.app's windows.
    The new logic for Files.app V2 is as follows.
    - For ALWAYS_CREATE: create a new Files.app's window, even if there are other.
    - For FOCUS_ANY_OR_CREATE: if there is a window with the same path, focus it. If not, then focus any other window if none is focused yet. If there are no windows, then create.
    - For FOCUS_SAME_OR_CREATE: if there is a window with the same path, focus it. If not, then create.
    
    TEST=Tested manually. Follow the bug's repro steps.
    BUG=239833
    R=hashimoto@chromium.org, yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/14807008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199719 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_manager/file_manager_util.cc
+++ chrome/browser/chromeos/extensions/file_manager/file_manager_util.cc
@@ -273,13 +273,15 @@ void OnCRXDownloadCallback(Browser* browser,
   InstallCRX(browser, file);
 }
 
+// TODO(mtomasz): Remove this when dropping support for the legacy Files.app.
 enum TAB_REUSE_MODE {
   REUSE_ANY_FILE_MANAGER,
   REUSE_SAME_PATH,
   REUSE_NEVER
 };
 
-bool FileManageTabExists(const base::FilePath& path, TAB_REUSE_MODE mode) {
+// TODO(mtomasz): Remove this when dropping support for the legacy Files.app.
+bool FileManagerTabExists(const base::FilePath& path, TAB_REUSE_MODE mode) {
   if (mode == REUSE_NEVER)
     return false;
 
@@ -288,6 +290,7 @@ bool FileManageTabExists(const base::FilePath& path, TAB_REUSE_MODE mode) {
   const GURL origin(chrome::kChromeUIFileManagerURL);
   const std::string ref = std::string("/") + path.value();
 
+  // Check browser windows.
   for (chrome::BrowserIterator it; !it.done(); it.Next()) {
     Browser* browser = *it;
     TabStripModel* tab_strip = browser->tab_strip_model();
@@ -360,14 +363,12 @@ void ExecuteHandler(Profile* profile,
   executor->Execute(urls);
 }
 
+// The |mode| argument is only for legacy Files.app. Files.app V2 has the
+// reusing logic in background.js.
 void OpenFileBrowserImpl(const base::FilePath& path,
                          TAB_REUSE_MODE mode,
                          const std::string& action_id) {
   content::RecordAction(UserMetricsAction("ShowFileBrowserFullTab"));
-
-  if (FileManageTabExists(path, mode))
-    return;
-
   Profile* profile = ProfileManager::GetDefaultProfileOrOffTheRecord();
 
   if (IsFileManagerPackaged() && !path.value().empty()) {
@@ -380,6 +381,9 @@ void OpenFileBrowserImpl(const base::FilePath& path,
     ExecuteHandler(profile, kFileBrowserDomain, action_id, url);
     return;
   }
+
+  if (FileManagerTabExists(path, mode))
+    return;
 
   std::string url = chrome::kChromeUIFileManagerURL;
   if (action_id.size()) {
