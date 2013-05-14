f5a32efc17b6edc279ec3320c851d482beec2b92
pilgrim@chromium.org <pilgrim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-14 20:57:37 +0000

    Remove RaisesException keyword from getSimpleDuration function that doesn't raise an exception
    BUG=240693
    
    Review URL: https://chromiumcodereview.appspot.com/14643020
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGAnimationElement.h
+++ third_party/WebKit/Source/core/svg/SVGAnimationElement.h
@@ -75,7 +75,7 @@ public:
     // SVGAnimationElement
     float getStartTime() const;
     float getCurrentTime() const;
-    float getSimpleDuration(ExceptionCode&) const;
+    float getSimpleDuration() const;
 
     // ElementTimeControl
     virtual void beginElement();
