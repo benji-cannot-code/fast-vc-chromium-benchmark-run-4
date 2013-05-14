1a3f8585b525d50e751ffe5bd6c7c8f0eab2157d
kojih@chromium.org <kojih@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-14 18:58:07 +0000

    Replace custom indexed getter with auto-generated one
    auto-generate V8HTMLFormElement::indexedPropertyGetter
    
    R=haraken@chromium.org
    BUG=229740
    TEST=No tests. refactoring.
    
    Review URL: https://chromiumcodereview.appspot.com/14874024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150329 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8HTMLFormElementCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8HTMLFormElementCustom.cpp
@@ -41,16 +41,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8HTMLFormElement::indexedPropertyGetter(uint32_t index, const v8::AccessorInfo& info)
-{
-    HTMLFormElement* form = V8HTMLFormElement::toNative(info.Holder());
-
-    RefPtr<Node> formElement = form->elements()->item(index);
-    if (!formElement)
-        return v8Undefined();
-    return toV8Fast(formElement.release(), info, form);
-}
-
 v8::Handle<v8::Value> V8HTMLFormElement::namedPropertyGetter(v8::Local<v8::String> name, const v8::AccessorInfo& info)
 {
     HTMLFormElement* imp = V8HTMLFormElement::toNative(info.Holder());
