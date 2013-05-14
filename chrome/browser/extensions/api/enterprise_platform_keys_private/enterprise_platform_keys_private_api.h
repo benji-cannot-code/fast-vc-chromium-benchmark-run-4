3f0627e97959cdbdcb737bb15314321f5133694b
davidyu@chromium.org <davidyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 06:00:54 +0000

    Check the user domain for ChallengeMachineKey().
    BUG=chromium:240210
    TEST=unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/15127002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199932 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h
+++ chrome/browser/extensions/api/enterprise_platform_keys_private/enterprise_platform_keys_private_api.h
@@ -43,6 +43,7 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
  public:
   static const char kChallengeBadBase64Error[];
   static const char kDevicePolicyDisabledError[];
+  static const char kDomainsDontMatchError[];
   static const char kResponseBadBase64Error[];
   static const char kSignChallengeFailedError[];
 
@@ -72,6 +73,9 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
 
   // Returns the enterprise domain the device is enrolled to.
   std::string GetEnterpriseDomain() const;
+
+  // Returns the user domain.
+  std::string GetUserDomain() const;
 
   // Returns the enterprise virtual device ID.
   std::string GetDeviceId() const;
@@ -114,7 +118,6 @@ class EPKPChallengeKeyBase : public AsyncExtensionFunction {
 
 class EPKPChallengeMachineKey : public EPKPChallengeKeyBase {
  public:
-  static const char kDomainsDontMatch[];
   static const char kGetCertificateFailedError[];
   static const char kNonEnterpriseDeviceError[];
 
@@ -149,7 +152,6 @@ typedef EPKPChallengeMachineKey
 
 class EPKPChallengeUserKey : public EPKPChallengeKeyBase {
  public:
-  static const char kDomainsDontMatchError[];
   static const char kExtensionNotWhitelistedError[];
   static const char kGetCertificateFailedError[];
   static const char kKeyRegistrationFailedError[];
@@ -190,7 +192,6 @@ class EPKPChallengeUserKey : public EPKPChallengeKeyBase {
 
   bool IsExtensionWhitelisted() const;
   bool IsRemoteAttestationEnabledForUser() const;
-  std::string GetUserDomain() const;
 
   DECLARE_EXTENSION_FUNCTION(
       "enterprise.platformKeysPrivate.challengeUserKey",
