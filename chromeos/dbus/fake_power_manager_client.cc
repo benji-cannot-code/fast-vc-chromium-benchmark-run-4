610dcc85d7c6aa23c63cee96294f329944423fb3
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 09:34:12 +0000

    Use MockDBusThreadManagerWithoutGMock for AutomaticRebootManager{Basic,}Test
    Adding FakePowerManagerClient a counter for RequestRestart() call.
    
    BUG=223061
    TEST=unittests
    R=bartfab@chromium.org, satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/15068011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199956 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/fake_power_manager_client.cc
+++ chromeos/dbus/fake_power_manager_client.cc
@@ -8,7 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace chromeos {
 
-FakePowerManagerClient::FakePowerManagerClient() {
+FakePowerManagerClient::FakePowerManagerClient()
+  : request_restart_call_count_(0) {
 }
 
 FakePowerManagerClient::~FakePowerManagerClient() {
@@ -52,6 +53,7 @@ bool FakePowerManagerClient::HasObserver(Observer* observer) {
 }
 
 void FakePowerManagerClient::RequestRestart() {
+  ++request_restart_call_count_;
 }
 
 void FakePowerManagerClient::IncreaseKeyboardBrightness() {
