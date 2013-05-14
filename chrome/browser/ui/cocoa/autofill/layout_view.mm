1514fe0de5ded3d0f3a536a691011297a38418e9
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 04:49:09 +0000

    [Autofill] Add basic version of GridLayout
    R=sail@chromium.org
    BUG=157274
    
    Review URL: https://chromiumcodereview.appspot.com/14646021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199901 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/cocoa/autofill/layout_view.mm
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#import "chrome/browser/ui/cocoa/autofill/layout_view.h"
+
+#include "chrome/browser/ui/cocoa/autofill/simple_grid_layout.h"
+
+@implementation LayoutView
+
+- (void)setLayoutManager:(scoped_ptr<SimpleGridLayout>)layout {
+  layout_ = layout.Pass();
+}
+
+- (SimpleGridLayout*)layoutManager {
+  return layout_.get();
+}
+
+- (void)resizeSubviewsWithOldSize:(NSSize)oldBoundsSize {
+  [super resizeSubviewsWithOldSize:oldBoundsSize];
+  [self performLayout];
+}
+
+- (void)performLayout {
+  layout_->Layout(self);
+}
+
+- (CGFloat)preferredHeightForWidth:(CGFloat)width {
+  return layout_->GetPreferredHeightForWidth(width);
+}
+
+@end
