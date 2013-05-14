f50077008258c831934f5f4d269a5d0088e8eac3
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 13:50:42 +0000

    Make forwarder2 daemon implementation more robust.
    This removes the use of a PID file that can cause flakiness while killing the
    daemon due to corner cases (e.g. the daemon is still running but its PID file
    was removed).
    
    Instead the PID of the daemon process is now obtained through getsockopt().
    
    BUG=229685
    R=digit@chromium.org
    
    Review URL: https://codereview.chromium.org/14727013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199981 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/android/forwarder2/device_forwarder_main.cc
+++ tools/android/forwarder2/device_forwarder_main.cc
@@ -30,7 +30,6 @@ forwarder2::PipeNotifier* g_notifier = NULL;
 
 const int kBufSize = 256;
 
-const char kPIDFilePath[] = "/data/local/tmp/chrome_device_forwarder_pid";
 const char kDaemonIdentifier[] = "chrome_device_forwarder_daemon";
 
 const char kKillServerCommand[] = "kill-server";
@@ -162,7 +161,7 @@ int RunDeviceForwarder(int argc, char** argv) {
   ClientDelegate client_delegate(argv[1]);
   ServerDelegate daemon_delegate;
   const char kLogFilePath[] = "";  // Log to logcat.
-  Daemon daemon(kLogFilePath, kPIDFilePath, kDaemonIdentifier, &client_delegate,
+  Daemon daemon(kLogFilePath, kDaemonIdentifier, &client_delegate,
                 &daemon_delegate, &GetExitNotifierFD);
 
   if (command == kKillServerCommand)
