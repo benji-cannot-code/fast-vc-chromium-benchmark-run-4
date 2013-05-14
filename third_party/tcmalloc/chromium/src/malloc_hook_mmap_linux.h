fde765de6763222baea477dadb9d69a9c014a664
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 04:48:31 +0000

    Support Android in MallocHook for mmap, munmap and mremap.
    sbrk is not supported for now because __sbrk is not provided by Bionic.
    Chrome for Android doesn't look like using sbrk.
    
    BUG=162208
    
    Review URL: https://chromiumcodereview.appspot.com/14859005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@199899 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/tcmalloc/chromium/src/malloc_hook_mmap_linux.h
+++ third_party/tcmalloc/chromium/src/malloc_hook_mmap_linux.h
@@ -41,10 +41,29 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #include <unistd.h>
+#if defined(__ANDROID__)
+#include <sys/syscall.h>
+#include <sys/linux-syscalls.h>
+#else
 #include <syscall.h>
+#endif
 #include <sys/mman.h>
 #include <errno.h>
 #include "base/linux_syscall_support.h"
+
+// SYS_mmap2, SYS_munmap, SYS_mremap and __off64_t are not defined in Android.
+#if defined(__ANDROID__)
+#ifndef SYS_mmap2
+#define SYS_mmap2 __NR_mmap2
+#endif
+#ifndef SYS_munmap
+#define SYS_munmap __NR_munmap
+#endif
+#ifndef SYS_mremap
+#define SYS_mremap __NR_mremap
+#endif
+typedef off64_t __off64_t;
+#endif  // defined(__ANDROID__)
 
 // The x86-32 case and the x86-64 case differ:
 // 32b has a mmap2() syscall, 64b does not.
@@ -150,8 +169,10 @@ extern "C" {
   void* mremap(void* old_addr, size_t old_size, size_t new_size,
                int flags, ...) __THROW
     ATTRIBUTE_SECTION(malloc_hook);
+#if !defined(__ANDROID__)
   void* sbrk(ptrdiff_t increment) __THROW
     ATTRIBUTE_SECTION(malloc_hook);
+#endif
 }
 
 extern "C" void* mmap64(void *start, size_t length, int prot, int flags,
@@ -209,6 +230,8 @@ extern "C" void* mremap(void* old_addr, size_t old_size, size_t new_size,
   return result;
 }
 
+// Don't hook sbrk() in Android, since it doesn't expose __sbrk.
+#if !defined(__ANDROID__)
 // libc's version:
 extern "C" void* __sbrk(ptrdiff_t increment);
 
@@ -218,6 +241,7 @@ extern "C" void* sbrk(ptrdiff_t increment) __THROW {
   MallocHook::InvokeSbrkHook(result, increment);
   return result;
 }
+#endif  // !defined(__ANDROID__)
 
 /*static*/void* MallocHook::UnhookedMMap(void *start, size_t length, int prot,
                                          int flags, int fd, off_t offset) {
