88393049ae5ba68559326969eeffb976d52a8252
tburkard@chromium.org <tburkard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 20:08:30 +0000

    Add instrumentation to see how often a local predictor prerender could be swapped in, but is not.
    R=shishir@chromium.org
    
    Review URL: https://codereview.chromium.org/14949012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200050 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_tab_helper.cc
+++ chrome/browser/prerender/prerender_tab_helper.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/time.h"
 #include "chrome/browser/predictors/logged_in_predictor_table.h"
 #include "chrome/browser/prerender/prerender_histograms.h"
+#include "chrome/browser/prerender/prerender_local_predictor.h"
 #include "chrome/browser/prerender/prerender_manager.h"
 #include "chrome/browser/prerender/prerender_manager_factory.h"
 #include "chrome/browser/profiles/profile.h"
@@ -29,6 +30,23 @@ using content::WebContents;
 DEFINE_WEB_CONTENTS_USER_DATA_KEY(prerender::PrerenderTabHelper);
 
 namespace prerender {
+
+namespace {
+
+void ReportTabHelperURLSeenToLocalPredictor(
+    PrerenderManager* prerender_manager,
+    const GURL& url,
+    WebContents* web_contents) {
+  if (!prerender_manager)
+    return;
+  PrerenderLocalPredictor* local_predictor =
+      prerender_manager->local_predictor();
+  if (!local_predictor)
+    return;
+  local_predictor->OnTabHelperURLSeen(url, web_contents);
+}
+
+}  // namespace
 
 // Helper class to compute pixel-based stats on the paint progress
 // between when a prerendered page is swapped in and when the onload event
@@ -153,6 +171,8 @@ void PrerenderTabHelper::ProvisionalChangeToMainFrameUrl(
   if (prerender_manager->IsWebContentsPrerendering(web_contents(), NULL))
     return;
   prerender_manager->MarkWebContentsAsNotPrerendered(web_contents());
+  ReportTabHelperURLSeenToLocalPredictor(prerender_manager, url,
+                                         web_contents());
 }
 
 void PrerenderTabHelper::DidCommitProvisionalLoadForFrame(
@@ -173,6 +193,8 @@ void PrerenderTabHelper::DidCommitProvisionalLoadForFrame(
   if (prerender_manager->IsWebContentsPrerendering(web_contents(), NULL))
     return;
   prerender_manager->RecordNavigation(validated_url);
+  ReportTabHelperURLSeenToLocalPredictor(prerender_manager, validated_url,
+                                         web_contents());
 }
 
 void PrerenderTabHelper::DidStopLoading(
