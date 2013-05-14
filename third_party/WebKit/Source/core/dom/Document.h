b3184a91fe8af9cb02d8f6a354fb9763a48062c6
dglazkov@chromium.org <dglazkov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-14 03:03:00 +0000

    Kill Document::addIconURL.
    It looked useless, so I killed it. Also, move icon-handling code out of LinkLoader, because there ain't much "loading" going on.
    
    R=abarth
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14625014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.h
+++ third_party/WebKit/Source/core/dom/Document.h
@@ -894,7 +894,6 @@ public:
 
     const Vector<IconURL>& shortcutIconURLs();
     const Vector<IconURL>& iconURLs(int iconTypesMask);
-    void addIconURL(const String& url, const String& mimeType, const String& size, IconType);
 
     void setUseSecureKeyboardEntryWhenActive(bool);
     bool useSecureKeyboardEntryWhenActive() const;
