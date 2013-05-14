f9465a98a69570b81de30873543201da3725be17
grt@chromium.org <grt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 21:42:06 +0000

    Add an intermediate window between GCF's host window and the RHW.
    Previously, GCF+Aura parented the ChromeFrameAutomationClient window
    directly to the DesktopRootWindowHostWin window. This caused some focus
    warping (and possibly other) issues when SetParent was called again.
    
    BUG=235411
    
    Review URL: https://chromiumcodereview.appspot.com/14927013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200080 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/external_tab_container_win.h
+++ chrome/browser/ui/views/external_tab_container_win.h
@@ -46,6 +46,10 @@ class WebView;
 class Widget;
 }
 
+#if defined(USE_AURA)
+class ContainerWindow;
+#endif
+
 // This class serves as the container window for an external tab.
 // An external tab is a Chrome tab that is meant to displayed in an
 // external process. This class provides the FocusManger needed by the
@@ -328,6 +332,10 @@ class ExternalTabContainerWin : public ExternalTabContainer,
 
   // if this tab is a popup
   bool is_popup_window_;
+
+#if defined(USE_AURA)
+  base::WeakPtr<ContainerWindow> tab_container_window_;
+#endif
 
   DISALLOW_COPY_AND_ASSIGN(ExternalTabContainerWin);
 };
