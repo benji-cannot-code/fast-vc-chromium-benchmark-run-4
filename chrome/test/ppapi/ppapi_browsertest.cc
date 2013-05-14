0c4916923adf22be40d520e3855282c5e5e07cc8
victorhsieh@chromium.org <victorhsieh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-14 22:32:03 +0000

    NaCl: Let SerializedHandle carry open flag.
    The (deprecated) NaClDescIoDescMakeFromHandle used to assume the
    given file handle was opened read-write, which breaks mmap on
    Windows when the file is opened read-only.
    
    In this change, FileIO::RequestOSFileHandle now passes the actual
    open flag to the new NaClDescIoDescFromHandleAllocCtor.
    
    BUG=183015
    
    Review URL: https://chromiumcodereview.appspot.com/14836003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200088 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/ppapi/ppapi_browsertest.cc
+++ chrome/test/ppapi/ppapi_browsertest.cc
@@ -629,6 +629,7 @@ IN_PROC_BROWSER_TEST_F(PPAPITest, FileIO) {
       LIST_TEST(FileIO_TouchQuery)
       LIST_TEST(FileIO_WillWriteWillSetLength)
       LIST_TEST(FileIO_RequestOSFileHandle)
+      LIST_TEST(FileIO_Mmap)
   );
 }
 IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, FileIO) {
@@ -643,6 +644,7 @@ IN_PROC_BROWSER_TEST_F(OutOfProcessPPAPITest, FileIO) {
       LIST_TEST(FileIO_TouchQuery)
       LIST_TEST(FileIO_WillWriteWillSetLength)
       LIST_TEST(FileIO_RequestOSFileHandle)
+      LIST_TEST(FileIO_Mmap)
   );
 }
 IN_PROC_BROWSER_TEST_F(PPAPINaClNewlibTest, FileIO) {
@@ -658,6 +660,7 @@ IN_PROC_BROWSER_TEST_F(PPAPINaClNewlibTest, FileIO) {
       // The following test requires PPB_FileIO_Trusted, not available in NaCl.
       LIST_TEST(DISABLED_FileIO_WillWriteWillSetLength)
       LIST_TEST(FileIO_RequestOSFileHandle)
+      LIST_TEST(FileIO_Mmap)
   );
 }
 IN_PROC_BROWSER_TEST_F(PPAPINaClGLibcTest, MAYBE_GLIBC(FileIO)) {
@@ -673,6 +676,7 @@ IN_PROC_BROWSER_TEST_F(PPAPINaClGLibcTest, MAYBE_GLIBC(FileIO)) {
       // The following test requires PPB_FileIO_Trusted, not available in NaCl.
       LIST_TEST(DISABLED_FileIO_WillWriteWillSetLength)
       LIST_TEST(FileIO_RequestOSFileHandle)
+      LIST_TEST(FileIO_Mmap)
   );
 }
 IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClTest, FileIO) {
@@ -688,6 +692,7 @@ IN_PROC_BROWSER_TEST_F(PPAPINaClPNaClTest, FileIO) {
       // The following test requires PPB_FileIO_Trusted, not available in NaCl.
       LIST_TEST(DISABLED_FileIO_WillWriteWillSetLength)
       LIST_TEST(FileIO_RequestOSFileHandle)
+      LIST_TEST(FileIO_Mmap)
   );
 }
 
--- content/renderer/pepper/pepper_file_io_host.cc
+++ content/renderer/pepper/pepper_file_io_host.cc
@@ -86,6 +86,7 @@ PepperFileIOHost::PepperFileIOHost(RendererPpapiHost* host,
       file_system_type_(PP_FILESYSTEMTYPE_INVALID),
       quota_policy_(quota::kQuotaLimitTypeUnknown),
       is_running_in_process_(host->IsRunningInProcess()),
+      open_flags_(0),
       weak_factory_(this) {
   // TODO(victorhsieh): eliminate plugin_delegate_ as it's no longer needed.
   webkit::ppapi::PluginInstance* plugin_instance =
@@ -139,6 +140,7 @@ int32_t PepperFileIOHost::OnHostMsgOpen(
     return rv;
 
   int flags = 0;
+  open_flags_ = open_flags;
   if (!::ppapi::PepperFileOpenFlagsToPlatformFileFlags(open_flags, &flags))
     return PP_ERROR_BADARGUMENT;
 
@@ -440,8 +442,9 @@ int32_t PepperFileIOHost::OnHostMsgRequestOSFileHandle(
     return PP_ERROR_FAILED;
   ppapi::host::ReplyMessageContext reply_context =
       context->MakeReplyMessageContext();
-  reply_context.params.AppendHandle(ppapi::proxy::SerializedHandle(
-      ppapi::proxy::SerializedHandle::FILE, file));
+  ppapi::proxy::SerializedHandle file_handle;
+  file_handle.set_file_handle(file, open_flags_);
+  reply_context.params.AppendHandle(file_handle);
   host()->SendReply(reply_context,
                     PpapiPluginMsg_FileIO_RequestOSFileHandleReply());
   return PP_OK_COMPLETIONPENDING;
