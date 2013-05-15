68f280f713a8a9e28a27c829215588ea695ded7e
erikwright@chromium.org <erikwright@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 23:37:06 +0000

    Add support for a cookie retention priority field trial.
    For the experiment group, a new algorithm will be used to identify the cookies to be evicted when a given domain exceeds its limits. The new algorithm will only differ from the old algorithm when a given domain uses the 'priority' attribute in its cookies.
    
    BUG=241178
    
    Review URL: https://chromiumcodereview.appspot.com/14772046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200385 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/net/sqlite_persistent_cookie_store.cc
+++ content/browser/net/sqlite_persistent_cookie_store.cc
@@ -19,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/metrics/field_trial.h"
 #include "base/metrics/histogram.h"
 #include "base/sequenced_task_runner.h"
 #include "base/string_util.h"
@@ -1220,7 +1221,15 @@ net::CookieStore* CreatePersistentCookieStore(
               BrowserThread::GetBlockingPool()->GetSequenceToken()),
           restore_old_session_cookies,
           storage_policy);
-  return new net::CookieMonster(persistent_store, cookie_monster_delegate);
+  net::CookieMonster* cookie_monster =
+      new net::CookieMonster(persistent_store, cookie_monster_delegate);
+
+  const std::string cookie_priority_experiment_group =
+      base::FieldTrialList::FindFullName("CookieRetentionPriorityStudy");
+  cookie_monster->SetPriorityAwareGarbageCollection(
+      cookie_priority_experiment_group == "ExperimentOn");
+
+  return cookie_monster;
 }
 
 }  // namespace content
