58f4ba6f95efdeed866f98ee4983e7dc9a360cb8
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 12:50:05 +0000

    Do not update a generated file if the contents is the same
    After this CL, changes in CodeGeneratorV8.pm that don't change generated code
    won't cause rebuild of V8XXX.{h,cpp}.
    
    R=abarth,thakis
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=150279
    
    Review URL: https://chromiumcodereview.appspot.com/14970006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150405 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/derived_sources.gyp
+++ third_party/WebKit/Source/bindings/derived_sources.gyp
@@ -74,6 +74,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           '<(SHARED_INTERMEDIATE_DIR)/webkit/bindings/V8DerivedSources19.cpp',
         ],
       }],
+      # The bindings generator can not write generated files if they are identical
+      # to the already existing file – that way they don't need to be recompiled.
+      # However, a reverse dependency having a newer timestamp than a
+      # generated binding can confuse some build systems, so only use this on
+      # ninja which explicitly supports this use case (gyp turns all actions into
+      # ninja restat rules).
+      ['"<(GENERATOR)"=="ninja"', {
+        'write_file_only_if_changed': '--writeFileOnlyIfChanged 1'
+      },{
+        'write_file_only_if_changed': '--writeFileOnlyIfChanged 0'
+      }],
     ],
   },
 
@@ -205,6 +216,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           '<(webcore_test_support_idl_files)',
           '<(RULE_INPUT_PATH)',
           '<@(preprocessor)',
+          '<@(write_file_only_if_changed)',
         ],
         'message': 'Generating binding from <(RULE_INPUT_PATH)',
       }],
--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -122,6 +122,7 @@ my $preprocessor;
 my $defines;
 my $verbose;
 my $dependentIdlFiles;
+my $writeFileOnlyIfChanged;
 my $sourceRoot;
 
 # Cache of IDL file pathnames.
@@ -233,6 +234,8 @@ sub new
     $defines = shift;
     $verbose = shift;
     $dependentIdlFiles = shift;
+    $writeFileOnlyIfChanged = shift;
+
     $sourceRoot = getcwd();
 
     bless($reference, $object);
@@ -5113,11 +5116,11 @@ sub WriteData
         $implementation{includes}->add("#endif\n");
     }
     $implementation{includes}->add("\n") unless $interface->isCallback;
-    UpdateFile($implFileName, $implementation{root}->toString());
+    WriteFileIfNeeded($implFileName, $implementation{root}->toString());
 
     %implIncludes = ();
 
-    UpdateFile($headerFileName, $header{root}->toString());
+    WriteFileIfNeeded($headerFileName, $header{root}->toString());
 }
 
 sub ConvertToV8StringResource
@@ -5171,12 +5174,19 @@ sub GetPassRefPtrType
     return "PassRefPtr<${v8ClassName}${angleBracketSpace}>";
 }
 
-sub UpdateFile
+sub WriteFileIfNeeded
 {
     my $fileName = shift;
     my $contents = shift;
 
-    open FH, "> $fileName" or die "Couldn't open $fileName: $!\n";
+    if (-f $fileName && $writeFileOnlyIfChanged) {
+        open FH, "<", $fileName or die "Couldn't open $fileName: $!\n";
+        my @lines = <FH>;
+        my $oldContents = join "", @lines;
+        close FH;
+        return if $contents eq $oldContents;
+    }
+    open FH, ">", $fileName or die "Couldn't open $fileName: $!\n";
     print FH $contents;
     close FH;
 }
