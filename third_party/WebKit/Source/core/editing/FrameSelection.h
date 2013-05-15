3921e333237219812c125bf01be6bf019dee2d7e
csaavedra@igalia.com <csaavedra@igalia.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 17:41:09 +0000

    Ctrl+Shift+Right in Windows should select the spacing after the word
    Add WebCore::FrameSelection::nextWordPositionForPlatform() which takes
    into consideration platform-specific editing behavior to decide
    whether to skip the spacing after a word.
    
    Use this in methods used to extend selection.
    
    This changes the outcome of selection when editingBehavior
    is set to 'win'. Therefore, many tests neeed new expectations for
    chromium-win, and others have been updated.
    
    The original patch was reviewed by Ryosuke Niwa in WebKit:
    
    http://trac.webkit.org/changeset/148987
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14930006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/FrameSelection.h
+++ third_party/WebKit/Source/core/editing/FrameSelection.h
@@ -267,6 +267,7 @@ private:
     VisiblePosition positionForPlatform(bool isGetStart) const;
     VisiblePosition startForPlatform() const;
     VisiblePosition endForPlatform() const;
+    VisiblePosition nextWordPositionForPlatform(const VisiblePosition&);
 
     VisiblePosition modifyExtendingRight(TextGranularity);
     VisiblePosition modifyExtendingForward(TextGranularity);
