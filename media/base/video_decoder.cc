709632c874cab3656db7644bbe961c19c172406b
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 10:11:21 +0000

    Add VideoDecoder::NeedsBitStreamConversion().
    In the future world where VideoDecoder doesn't talk to DemuxerStream directly
    (see issue 141788), the owner of the VideoDecoder (e.g. VideoFrameStream) needs
    to get the info about whether the decoder needs bitstream conversion so that it
    can call DemuxerStream::EnableBitstreamConverter() accordingly.
    
    BUG=141788,240342
    TEST=Current media_unittests pass.
    
    Review URL: https://chromiumcodereview.appspot.com/15140002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200232 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/video_decoder.cc
+++ media/base/video_decoder.cc
@@ -15,6 +15,10 @@ bool VideoDecoder::HasAlpha() const {
   return false;
 }
 
+bool VideoDecoder::NeedsBitstreamConversion() const {
+  return false;
+}
+
 bool VideoDecoder::HasOutputFrameAvailable() const {
   return true;
 }
