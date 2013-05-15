4625d73bc69295e637a8733cfa91a49c7fbfb90a
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 08:43:44 +0000

    Export reload and unload functions from the anonymous namespace in Files.app.
    After introducing an anynomous namespace with the strict mode we lost access to global functions from outside. This caused problems in Files.app V2 which uses these methods to reload or to save state before unloading. This patch just exports these functions what fixes the problem.
    
    TBR=hirono@chromium.org
    TEST=Play an mp3 file in Files.app. Click on another. The player should show the new file.
    BUG=240635
    
    Review URL: https://codereview.chromium.org/15029011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200202 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
+++ chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
@@ -25,4 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="audio_player.js"/>
 //<include src="player_testapi.js"/>
 
+window.reload = reload;
+window.unload = unload;
+
 })();
--- chrome/browser/resources/file_manager/js/media/video_player_scripts.js
+++ chrome/browser/resources/file_manager/js/media/video_player_scripts.js
@@ -26,4 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //<include src="video_player.js"/>
 //<include src="player_testapi.js"/>
 
+window.reload = reload;
+window.unload = unload;
+
 })();
