96f661c43e1c5b954313725a3999088a34e76d3a
serya@chromium.org <serya@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 17:31:38 +0000

    Refactoring CSS error reporting.
    Removed word "Syntax" since this code also reports semantic errors.
    
    BUG=140608
    
    Review URL: https://chromiumcodereview.appspot.com/14858043
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150442 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser.h
+++ third_party/WebKit/Source/core/css/CSSParser.h
@@ -75,11 +75,11 @@ class CSSParser {
 
 public:
     class SourceDataHandler;
-    enum SyntaxErrorType {
-        NoSyntaxError,
+    enum ErrorType {
+        NoError,
         PropertyDeclarationError,
         InvalidPropertyValueError,
-        GeneralSyntaxError
+        GeneralError
     };
 
     CSSParser(const CSSParserContext&);
@@ -387,9 +387,9 @@ public:
     void startRuleBody();
     void endRuleBody(bool discard = false);
     void startProperty();
-    void endProperty(bool isImportantFound, bool isPropertyParsed, SyntaxErrorType = NoSyntaxError);
+    void endProperty(bool isImportantFound, bool isPropertyParsed, ErrorType = NoError);
     void startEndUnknownRule();
-    void syntaxError(const CSSParserLocation&, SyntaxErrorType = GeneralSyntaxError);
+    void reportError(const CSSParserLocation&, ErrorType = GeneralError);
 
     inline int lex(void* yylval) { return (this->*m_lexFunc)(yylval); }
 
@@ -661,7 +661,7 @@ public:
     virtual void endRuleBody(unsigned offset, bool error) = 0;
     virtual void startEndUnknownRule() = 0;
     virtual void startProperty(unsigned offset) = 0;
-    virtual void endProperty(bool isImportant, bool isParsed, unsigned offset, CSSParser::SyntaxErrorType) = 0;
+    virtual void endProperty(bool isImportant, bool isParsed, unsigned offset, CSSParser::ErrorType) = 0;
     virtual void startComment(unsigned offset) = 0;
     virtual void endComment(unsigned offset) = 0;
 };
