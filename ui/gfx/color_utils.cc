c5a80590bcf2ebe02a2609591ebb494d9ab3bae4
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 23:25:21 +0000

    Changes RelativeLuminance() to not add 0.05 directly, since that's only supposed to be done as part of calculating the contrast ratio.  Originally this function wasn't public so it didn't matter, but now that it is, it should act correctly.
    Also rewraps a few comments.
    
    BUG=none
    TEST=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/14823012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200384 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/color_utils.cc
+++ ui/gfx/color_utils.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace color_utils {
 
+
 // Helper functions -----------------------------------------------------------
 
 namespace {
@@ -62,14 +63,17 @@ SkColor LumaInvertColor(SkColor color) {
 }
 
 double ContrastRatio(double foreground_luminance, double background_luminance) {
-  // NOTE: Only pass in numbers obtained from RelativeLuminance(), since those
-  // are guaranteed to be > 0 and thus not cause a divide-by-zero error here.
+  DCHECK_GE(foreground_luminance, 0.0);
+  DCHECK_GE(background_luminance, 0.0);
+  foreground_luminance += 0.05;
+  background_luminance += 0.05;
   return (foreground_luminance > background_luminance) ?
       (foreground_luminance / background_luminance) :
       (background_luminance / foreground_luminance);
 }
 
 }  // namespace
+
 
 // ----------------------------------------------------------------------------
 
@@ -82,8 +86,8 @@ unsigned char GetLuminanceForColor(SkColor color) {
 
 double RelativeLuminance(SkColor color) {
   return (0.2126 * ConvertSRGB(SkColorGetR(color))) +
-      (0.7152 * ConvertSRGB(SkColorGetG(color))) +
-      (0.0722 * ConvertSRGB(SkColorGetB(color))) + 0.05;
+         (0.7152 * ConvertSRGB(SkColorGetG(color))) +
+         (0.0722 * ConvertSRGB(SkColorGetB(color)));
 }
 
 void SkColorToHSL(SkColor c, HSL* hsl) {
@@ -100,8 +104,8 @@ void SkColorToHSL(SkColor c, HSL* hsl) {
     double dr = (((vmax - r) / 6.0) + (delta / 2.0)) / delta;
     double dg = (((vmax - g) / 6.0) + (delta / 2.0)) / delta;
     double db = (((vmax - b) / 6.0) + (delta / 2.0)) / delta;
-    // We need to compare for the max value because comparing vmax to r,
-    // g or b can sometimes result in values overflowing registers.
+    // We need to compare for the max value because comparing vmax to r, g, or b
+    // can sometimes result in values overflowing registers.
     if (r >= g && r >= b)
       hsl->h = db - dg;
     else if (g >= r && g >= b)
@@ -123,8 +127,8 @@ SkColor HSLToSkColor(const HSL& hsl, SkAlpha alpha) {
   double saturation = hsl.s;
   double lightness = hsl.l;
 
-  // If there's no color, we don't care about hue and can do everything based
-  // on brightness.
+  // If there's no color, we don't care about hue and can do everything based on
+  // brightness.
   if (!saturation) {
     uint8 light;
 
@@ -170,9 +174,8 @@ SkColor HSLShift(SkColor color, const HSL& shift) {
   if (shift.l < 0)
     return result;
 
-  // Lightness shifts in the style of popular image editors aren't
-  // actually represented in HSL - the L value does have some effect
-  // on saturation.
+  // Lightness shifts in the style of popular image editors aren't actually
+  // represented in HSL - the L value does have some effect on saturation.
   double r = static_cast<double>(SkColorGetR(result));
   double g = static_cast<double>(SkColorGetG(result));
   double b = static_cast<double>(SkColorGetB(result));
@@ -192,12 +195,11 @@ SkColor HSLShift(SkColor color, const HSL& shift) {
 }
 
 void BuildLumaHistogram(const SkBitmap& bitmap, int histogram[256]) {
+  DCHECK_EQ(SkBitmap::kARGB_8888_Config, bitmap.config());
+
   SkAutoLockPixels bitmap_lock(bitmap);
   if (!bitmap.getPixels())
     return;
-
-  // Assume ARGB_8888 format.
-  DCHECK(bitmap.config() == SkBitmap::kARGB_8888_Config);
 
   int pixel_width = bitmap.width();
   int pixel_height = bitmap.height();
@@ -219,7 +221,7 @@ SkColor AlphaBlend(SkColor foreground, SkColor background, SkAlpha alpha) {
 
   double normalizer = (f_alpha * alpha + b_alpha * (255 - alpha)) / 255.0;
   if (normalizer == 0.0)
-    return SkColorSetARGB(0, 0, 0, 0);
+    return SK_ColorTRANSPARENT;
 
   double f_weight = f_alpha * alpha / normalizer;
   double b_weight = b_alpha * (255 - alpha) / normalizer;
