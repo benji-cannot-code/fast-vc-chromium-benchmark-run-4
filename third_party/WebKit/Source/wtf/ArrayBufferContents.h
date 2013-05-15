1a65a9d93fa1be127ae730f456811560f9e68f59
kbr@chromium.org <kbr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 01:50:49 +0000

    Revert 150127 "Introduce RawBuffer as a backing store for ArrayB..."
    Caused memory leaks identified in Issue 240922.
    
    > Introduce RawBuffer as a backing store for ArrayBufferContents.
    > 
    > Several bits and pieces of core are currently using ArrayBuffer as a generic
    > dumping ground for temporary byte streams. This impedes our ability to move
    > ArrayBuffer's implementation completely into V8, as we're creating and
    > manipulating ArrayBuffer objects in areas devoid of a V8 context. We need the
    > flexibility of dealing with a raw chunk of memory, but doing it through
    > ArrayBuffer is simply the wrong long-term solution.
    > 
    > This patch moves most of the heavy memory lifting out of ArrayBufferContents,
    > and into a new RawBuffer class which will be suitable for use outside the
    > context of V8. If you're thinking "Hey, ArrayBufferContents isn't doing much
    > anymore. Can't we kill it?", then you're a very clever person indeed. Handsome,
    > too.
    > 
    > Subsequent patches will start moving ArrayBuffer usage in core onto RawBuffer
    > piece by piece.
    > 
    > BUG=239049
    > R=jochen@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/14807007
    
    TBR=mkwst@chromium.org
    BUG=239049,240922
    
    Review URL: https://codereview.chromium.org/15187005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150387 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ArrayBufferContents.h
+++ third_party/WebKit/Source/wtf/ArrayBufferContents.h
@@ -30,13 +30,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/ArrayBufferDeallocationObserver.h"
 #include "wtf/Noncopyable.h"
-#include "wtf/RawBuffer.h"
 
 namespace WTF {
 
-class ArrayBufferContents : public RawBuffer {
+class ArrayBufferContents {
     WTF_MAKE_NONCOPYABLE(ArrayBufferContents);
 public:
+    enum InitializationPolicy {
+        ZeroInitialize,
+        DontInitialize
+    };
+
     ArrayBufferContents();
     ArrayBufferContents(unsigned numElements, unsigned elementByteSize, ArrayBufferContents::InitializationPolicy);
 
@@ -44,10 +48,17 @@ public:
 
     void clear();
 
+    void* data() const { return m_data; }
+    unsigned sizeInBytes() const { return m_sizeInBytes; }
+
     bool hasDeallocationObserver() const { return !!m_deallocationObserver; }
     void setDeallocationObserver(ArrayBufferDeallocationObserver* observer) { m_deallocationObserver = observer; }
 
+    void transfer(ArrayBufferContents& other);
+
 private:
+    void* m_data;
+    unsigned m_sizeInBytes;
     ArrayBufferDeallocationObserver* m_deallocationObserver;
 };
 
--- third_party/WebKit/Source/wtf/wtf.gypi
+++ third_party/WebKit/Source/wtf/wtf.gypi
@@ -110,8 +110,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'RandomNumber.cpp',
             'RandomNumber.h',
             'RandomNumberSeed.h',
-            'RawBuffer.cpp',
-            'RawBuffer.h',
             'RefCounted.h',
             'RefCountedLeakCounter.cpp',
             'RefCountedLeakCounter.h',
