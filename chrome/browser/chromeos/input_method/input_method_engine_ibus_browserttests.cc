635efc457d2fa3e6dac54687159defb747029a6f
haruki@chromium.org <haruki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 08:44:14 +0000

    Remove unused include
    BUG=
    TEST=
    R=nona@chromium.org
    
    Review URL: https://codereview.chromium.org/15090004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200203 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_engine_ibus_browserttests.cc
+++ chrome/browser/chromeos/input_method/input_method_engine_ibus_browserttests.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/dbus/ibus/mock_ibus_engine_factory_service.h"
 #include "chromeos/dbus/ibus/mock_ibus_engine_service.h"
 #include "chromeos/dbus/mock_dbus_thread_manager_without_gmock.h"
-#include "chromeos/dbus/mock_update_engine_client.h"
 #include "chromeos/ime/input_method_descriptor.h"
 #include "chromeos/ime/input_method_manager.h"
 #include "content/public/test/test_utils.h"
