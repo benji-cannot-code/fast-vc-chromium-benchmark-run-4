679b39cd9b3b40c5b8b87bc4263665f75f25f4f4
wez@chromium.org <wez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 22:16:56 +0000

    Track NPObject ownership by the originating plugins' NPP identifier. [1/3]
    This CL adds stubs for the new APIs required to implement NPP-based ownership tracking.
    
    This CL is required by Chrome CL crrev.com/15007012, which is itself required by Blink CL crrev.com/14019005.
    
    BUG=152006
    
    Review URL: https://chromiumcodereview.appspot.com/14989014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebPluginContainerImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPluginContainerImpl.cpp
@@ -400,6 +400,10 @@ void WebPluginContainerImpl::reportGeometry()
     }
 }
 
+void WebPluginContainerImpl::allowScriptObjects()
+{
+}
+
 void WebPluginContainerImpl::clearScriptObjects()
 {
     Frame* frame = m_element->document()->frame();
--- third_party/WebKit/Source/WebKit/chromium/src/WebPluginContainerImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebPluginContainerImpl.h
@@ -101,6 +101,7 @@ public:
     virtual void invalidateRect(const WebRect&);
     virtual void scrollRect(int dx, int dy, const WebRect&);
     virtual void reportGeometry();
+    virtual void allowScriptObjects();
     virtual void clearScriptObjects();
     virtual NPObject* scriptableObjectForElement();
     virtual WebString executeScriptURL(const WebURL&, bool popupsAllowed);
