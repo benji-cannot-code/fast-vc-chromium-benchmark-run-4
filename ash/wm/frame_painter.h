429d477faf27c4a45164f0d6dd7d446035fd1c02
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 09:50:31 +0000

    Fix the background flashing when an unthemed browser window is in immersive fullscreen
    Bug=236598
    Test=Manual, see bug
    
    Review URL: https://chromiumcodereview.appspot.com/15086004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200224 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/frame_painter.h
+++ ash/wm/frame_painter.h
@@ -55,6 +55,11 @@ class ASH_EXPORT FramePainter : public aura::WindowObserver,
     INACTIVE
   };
 
+  enum Themed {
+    THEMED_YES,
+    THEMED_NO
+  };
+
   // What happens when the |size_button_| is pressed.
   enum SizeButtonBehavior {
     SIZE_BUTTON_MINIMIZES,
@@ -91,6 +96,9 @@ class ASH_EXPORT FramePainter : public aura::WindowObserver,
 
   // Returns the amount that the theme background should be inset.
   int GetThemeBackgroundXInset() const;
+
+  // Returns true if the header should be painted using a minimalistic style.
+  bool ShouldUseMinimalHeaderStyle(Themed header_themed) const;
 
   // Paints the frame header.
   void PaintHeader(views::NonClientFrameView* view,
@@ -167,14 +175,14 @@ class ASH_EXPORT FramePainter : public aura::WindowObserver,
   // Returns the opacity value used to paint the header.
   int GetHeaderOpacity(HeaderMode header_mode,
                        int theme_frame_id,
-                       const gfx::ImageSkia* theme_frame_overlay);
+                       const gfx::ImageSkia* theme_frame_overlay) const;
 
   // Returns true if the user is cycling through workspaces.
   bool IsCyclingThroughWorkspaces() const;
 
   // Returns true if |window_->GetRootWindow()| should be drawing transparent
   // window headers.
-  bool UseSoloWindowHeader();
+  bool UseSoloWindowHeader() const;
 
   // Returns true if |root_window| has exactly one visible, normal-type window.
   // It ignores |ignore_window| while calculating the number of windows.
