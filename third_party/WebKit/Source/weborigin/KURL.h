971731e6a20b83b977f71f325ca51e35830f157c
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 13:19:59 +0000

    Merge KURLPrivate and KURL
    Now that we no longer support the KURL backend for KURL, there's no reason to
    have the KURLPrivate ghetto for the GURL backend. This CL merges this code into
    KURL.cpp and makes some mechanical cleanups.
    
    Review URL: https://chromiumcodereview.appspot.com/14581019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150409 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/weborigin/KURL.h
+++ third_party/WebKit/Source/weborigin/KURL.h
@@ -27,14 +27,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef KURL_h
 #define KURL_h
 
-#include "weborigin/KURLPrivate.h"
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
+#include "wtf/OwnPtr.h"
+#include "wtf/text/CString.h"
+#include "wtf/text/TextEncoding.h"
 #include "wtf/text/WTFString.h"
-
-namespace WTF{
-class TextEncoding;
-}
+#include <googleurl/src/url_canon.h>
+#include <googleurl/src/url_parse.h>
 
 namespace WebCore {
 
@@ -44,14 +44,21 @@ enum ParsedURLStringTag { ParsedURLString };
 
 class KURL {
 public:
-    // Generates a URL which contains a null string.
-    KURL() { invalidate(); }
+    KURL()
+        : m_isValid(false)
+        , m_protocolIsInHTTPFamily(false)
+    {
+    }
 
-    // The argument is an absolute URL string. The string is assumed to be output of KURL::string() called on a valid
-    // KURL object, or indiscernible from such.
-    // It is usually best to avoid repeatedly parsing a string, unless memory saving outweigh the possible slow-downs.
+    KURL(const KURL&);
+    KURL& operator=(const KURL&);
+
+    // The argument is an absolute URL string. The string is assumed to be
+    // output of KURL::string() called on a valid KURL object, or indiscernible
+    // from such. It is usually best to avoid repeatedly parsing a string,
+    // unless memory saving outweigh the possible slow-downs.
     KURL(ParsedURLStringTag, const String&);
-    explicit KURL(WTF::HashTableDeletedValueType) : m_url(WTF::HashTableDeletedValue) { }
+    explicit KURL(WTF::HashTableDeletedValueType);
 
     bool isHashTableDeletedValue() const { return string().isHashTableDeletedValue(); }
 
@@ -97,7 +104,7 @@ public:
     bool canSetPathname() const { return isHierarchical(); }
     bool isHierarchical() const;
 
-    const String& string() const { return m_url.string(); }
+    const String& string() const { return m_string; }
 
     String elidedString() const;
 
@@ -158,9 +165,9 @@ public:
 
     operator const String&() const { return string(); }
 
-    const url_parse::Parsed& parsed() const { return m_url.m_parsed; }
+    const url_parse::Parsed& parsed() const { return m_parsed; }
 
-    const KURL* innerURL() const { return m_url.innerURL(); }
+    const KURL* innerURL() const { return m_innerURL.get(); }
 
 #ifndef NDEBUG
     void print() const;
@@ -170,11 +177,23 @@ public:
     bool isSafeToSendToAnotherThread() const;
 
 private:
-    void invalidate();
-    static bool protocolIs(const String&, const char*);
+    void init(const KURL& base, const String& relative, const WTF::TextEncoding* queryEncoding);
 
-    friend class KURLPrivate;
-    KURLPrivate m_url;
+    String componentString(const url_parse::Component&) const;
+
+    typedef url_canon::Replacements<url_parse::UTF16Char> Replacements;
+    void replaceComponents(const Replacements&);
+
+    template <typename CHAR>
+    void init(const KURL& base, const CHAR* relative, int relativeLength, const WTF::TextEncoding* queryEncoding);
+    void initInnerURL();
+    void initProtocolIsInHTTPFamily();
+
+    bool m_isValid;
+    bool m_protocolIsInHTTPFamily;
+    url_parse::Parsed m_parsed;
+    String m_string;
+    OwnPtr<KURL> m_innerURL;
 };
 
 bool operator==(const KURL&, const KURL&);
@@ -242,11 +261,11 @@ inline bool operator!=(const String& a, const KURL& b)
 
 namespace WTF {
 
-    // KURLHash is the default hash for String
-    template<typename T> struct DefaultHash;
-    template<> struct DefaultHash<WebCore::KURL> {
-        typedef WebCore::KURLHash Hash;
-    };
+// KURLHash is the default hash for String
+template<typename T> struct DefaultHash;
+template<> struct DefaultHash<WebCore::KURL> {
+    typedef WebCore::KURLHash Hash;
+};
 
 } // namespace WTF
 
--- third_party/WebKit/Source/weborigin/weborigin.gypi
+++ third_party/WebKit/Source/weborigin/weborigin.gypi
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'DatabaseIdentifier.h',
       'KURL.cpp',
       'KURL.h',
-      'KURLPrivate.cpp',
-      'KURLPrivate.h',
       'KURLHash.h',
       'KnownPorts.cpp',
       'KnownPorts.h',
