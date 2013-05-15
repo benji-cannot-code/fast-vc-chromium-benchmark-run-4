9dd73ae47ba14fb71fe4f16f9e5ec1b9dedf26dc
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 08:38:24 +0000

    app_list: Split context menu code out of ExtensionAppItem.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/14594013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200196 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/ui/app_list/app_context_menu_delegate.h
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_UI_APP_LIST_APP_CONTEXT_MENU_DELEGATE_H_
+#define CHROME_BROWSER_UI_APP_LIST_APP_CONTEXT_MENU_DELEGATE_H_
+
+namespace app_list {
+
+class AppContextMenuDelegate {
+ public:
+  // Invoked to execute "Launch" command.
+  virtual void ExecuteLaunchCommand(int event_flags) = 0;
+
+ protected:
+  virtual ~AppContextMenuDelegate() {}
+};
+
+}  // namespace app_list
+
+#endif  // CHROME_BROWSER_UI_APP_LIST_APP_CONTEXT_MENU_DELEGATE_H_
