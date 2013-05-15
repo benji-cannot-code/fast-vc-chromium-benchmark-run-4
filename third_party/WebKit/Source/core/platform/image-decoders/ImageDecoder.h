068aa2d96ec41eac7a962aa3ab14f7c8b18bb2cd
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-15 14:37:15 +0000

    Remove ImageFrame::setColorProfile
    ImageFrame::setColorProfile() and the associated member are
    not used in chrome. Per the FIXME, remove them.
    
    TEST=Covered by existing image color profile layout tests.
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/14612011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150428 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/core/platform/image-decoders/ImageDecoder.h
@@ -120,7 +120,6 @@ namespace WebCore {
         void reportMemoryUsage(MemoryObjectInfo*) const;
 
         void setHasAlpha(bool alpha);
-        void setColorProfile(const ColorProfile&);
         void setOriginalFrameRect(const IntRect& r) { m_originalFrameRect = r; }
         void setStatus(FrameStatus status);
         void setDuration(unsigned duration) { m_duration = duration; }
@@ -406,8 +405,6 @@ namespace WebCore {
 
         RefPtr<SharedBuffer> m_data; // The encoded data.
         Vector<ImageFrame, 1> m_frameBufferCache;
-        // FIXME: Do we need m_colorProfile any more, for any port?
-        ColorProfile m_colorProfile;
         bool m_scaled;
         Vector<int> m_scaledColumns;
         Vector<int> m_scaledRows;
