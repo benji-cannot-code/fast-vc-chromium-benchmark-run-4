be5bcd12523e74f4d38f8a82b5da1a443d7dc300
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 19:19:21 +0000

    [NaCl SDK] Reduce use of wildcards when constructing the SDK.
    This change makes build_sdk more explicit about what
    it ships.
    
    It removed the following libraries from the SDK toolchain
    directories:
    
    libgio
    libimc_syscalls
    libplatform
    libsrpc
    libdynamic_annotations
    
    While working on this change I found that we were including
    "ppapi/lib" recursively in the SDK include folder.  This
    change removes this but leaved the suplicate copy copy of
    gl2ext_ppapi.h in ppapi/lib/gl/gles2/gl2ext_ppapi.h.  This
    is still needed in order to build libppapi_gles2 against
    a copy of the SDK.
    
    BUG=
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/15021016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200331 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/ppapi_main/ppapi_instance3d.cc
+++ native_client_sdk/src/libraries/ppapi_main/ppapi_instance3d.cc
@@ -5,12 +5,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <stdio.h>
 
+#include "GLES2/gl2.h"
+
 #include "ppapi/cpp/graphics_3d.h"
 #include "ppapi/cpp/size.h"
-
-#include "ppapi/lib/gl/gles2/gl2ext_ppapi.h"
-#include "ppapi/lib/gl/include/GLES2/gl2.h"
-
+#include "ppapi/gles2/gl2ext_ppapi.h"
 #include "ppapi/utility/completion_callback_factory.h"
 
 #include "ppapi_main/ppapi_instance.h"
