eb591024cf3cd98a7baf82cc2bf3cf9542bd8719
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-15 10:11:38 +0000

    Remove an unneeded UTF conversion.
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/15184002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200233 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/storage_monitor/storage_monitor_chromeos.cc
+++ chrome/browser/storage_monitor/storage_monitor_chromeos.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/stl_util.h"
+#include "base/string16.h"
 #include "base/string_util.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
@@ -60,8 +61,9 @@ bool GetDeviceInfo(const disks::DiskMountManager::MountPointInfo& mount_info,
   std::string unique_id = MakeDeviceUniqueId(*disk);
   // Keep track of device uuid and label, to see how often we receive empty
   // values.
-  chrome::MediaStorageUtil::RecordDeviceInfoHistogram(
-      true, unique_id, UTF8ToUTF16(disk->device_label()));
+  string16 device_label = UTF8ToUTF16(disk->device_label());
+  chrome::MediaStorageUtil::RecordDeviceInfoHistogram(true, unique_id,
+                                                      device_label);
   if (unique_id.empty())
     return false;
 
@@ -74,7 +76,7 @@ bool GetDeviceInfo(const disks::DiskMountManager::MountPointInfo& mount_info,
     info->location = mount_info.mount_path,
     info->vendor_name = UTF8ToUTF16(disk->vendor_name());
     info->model_name = UTF8ToUTF16(disk->product_name());
-    info->storage_label = UTF8ToUTF16(disk->device_label());
+    info->storage_label = device_label;
     info->total_size_in_bytes = disk->total_size_in_bytes();
   }
 
