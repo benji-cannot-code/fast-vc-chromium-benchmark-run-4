30c89d394feffb64ca2184cb249b5378460f45dd
dongseong.hwang@intel.com <dongseong.hwang@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-16 08:17:33 +0000

    Build fix TestShellX11 when using aura.
    Include unistd.h that defines R_OK, access() and etc..
    
    Review URL: https://chromiumcodereview.appspot.com/14890027
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestShellX11.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestShellX11.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "TestShell.h"
 
 #include <fontconfig/fontconfig.h>
+#include <unistd.h>
 
 #if USE(GTK)
 #include <gtk/gtk.h>
