bc21fb3234ac0c584a889fda321727df8e7ee6e8
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-16 02:43:24 +0000

    Fix grid items' computed 'display' values
    This change updates the grid items' 'display' after style resolution
    so that we match the specification's (see section 4. Grid Items).
    
    Changing 'display' also fixes a bug where we would insert an anonymous
    renderer between the grid element and a grid item. This would break
    the layout logic as we would consider the anonymous renderer during
    layout (thus missing any grid position set on the actual grid item).
    
    TEST=fast/css-grid-layout/grid-item-display.html
    
    Review URL: https://chromiumcodereview.appspot.com/15176005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150472 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
+++ third_party/WebKit/Source/core/css/resolver/StyleResolver.cpp
@@ -1484,6 +1484,11 @@ static bool isDisplayFlexibleBox(EDisplay display)
     return display == FLEX || display == INLINE_FLEX;
 }
 
+static bool isDisplayGridBox(EDisplay display)
+{
+    return display == GRID || display == INLINE_GRID;
+}
+
 void StyleResolver::adjustRenderStyle(RenderStyle* style, RenderStyle* parentStyle, Element *e)
 {
     ASSERT(parentStyle);
@@ -1571,7 +1576,7 @@ void StyleResolver::adjustRenderStyle(RenderStyle* style, RenderStyle* parentSty
         if (style->writingMode() != TopToBottomWritingMode && (style->display() == BOX || style->display() == INLINE_BOX))
             style->setWritingMode(TopToBottomWritingMode);
 
-        if (isDisplayFlexibleBox(parentStyle->display())) {
+        if (isDisplayFlexibleBox(parentStyle->display()) || isDisplayGridBox(parentStyle->display())) {
             style->setFloating(NoFloat);
             style->setDisplay(equivalentBlockDisplay(style->display(), style->isFloating(), !document()->inQuirksMode()));
         }
