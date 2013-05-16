6cf9f3ede9478f6a15a8f77481088e39a43f7a52
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-16 03:59:13 +0000

    Revert 150469 "Remove TypeCanFailConversion() from CodeGenerator..."
    > Remove TypeCanFailConversion() from CodeGeneratorV8.pm
    > 
    > TypeCanFailConversion() is just for "Attr" type arguments, which are used
    > in a couple of methods in Element.idl (setAttributeNode(), removeAttributeNode(),
    > setAttributeNodeNS()). This should be replaced with [StrictTypeChecking].
    > The only difference is the exception name. [StrictTypeChecking] throws a TypeError
    > whereas TypeCanFailConversion() throws TYPE_MISMATCH_ERR. Given that those three methods
    > are already deprecated in the spec, a slight change in the exception name won't
    > cause a compatibility problem.
    > 
    > R=adamk@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/14898010
    
    TBR=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/14791017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150477 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -2205,7 +2205,7 @@ END
     my $raisesExceptions = $function->signature->extendedAttributes->{"RaisesException"};
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
-            if (!IsCallbackInterface($parameter->type) or $parameter->extendedAttributes->{"IsIndex"}) {
+            if ((!IsCallbackInterface($parameter->type) and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
                 $raisesExceptions = 1;
             }
         }
@@ -2373,6 +2373,13 @@ sub GenerateParametersCheck
             $parameterCheckString .= "    $nativeType $parameterName = SerializedScriptValue::create(args[$paramIndex], 0, 0, ${parameterName}DidThrow, args.GetIsolate());\n";
             $parameterCheckString .= "    if (${parameterName}DidThrow)\n";
             $parameterCheckString .= "        return v8Undefined();\n";
+        } elsif (TypeCanFailConversion($parameter)) {
+            $parameterCheckString .= "    $nativeType $parameterName = " .
+                 JSValueToNative($parameter, "args[$paramIndex]", "args.GetIsolate()") . ";\n";
+            $parameterCheckString .= "    if (UNLIKELY(!$parameterName)) {\n";
+            $parameterCheckString .= "        ec = TYPE_MISMATCH_ERR;\n";
+            $parameterCheckString .= "        goto fail;\n";
+            $parameterCheckString .= "    }\n";
         } elsif ($parameter->isVariadic) {
             my $nativeElementType = GetNativeType($parameter->type);
             if ($nativeElementType =~ />$/) {
@@ -2492,7 +2499,7 @@ sub GenerateSingleConstructorCallback
     }
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
-            if (!IsCallbackInterface($parameter->type) or $parameter->extendedAttributes->{"IsIndex"}) {
+            if ((!IsCallbackInterface($parameter->type) and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
                 $raisesExceptions = 1;
             }
         }
@@ -2720,7 +2727,7 @@ sub GenerateNamedConstructor
     }
     if (!$raisesExceptions) {
         foreach my $parameter (@{$function->parameters}) {
-            if (!IsCallbackInterface($parameter->type) or $parameter->extendedAttributes->{"IsIndex"}) {
+            if ((!IsCallbackInterface($parameter->type) and TypeCanFailConversion($parameter)) or $parameter->extendedAttributes->{"IsIndex"}) {
                 $raisesExceptions = 1;
             }
         }
@@ -4458,6 +4465,16 @@ sub GetNativeTypeForCallbacks
 
     # Callbacks use raw pointers, so pass isParameter = 1
     return GetNativeType($type, 1);
+}
+
+sub TypeCanFailConversion
+{
+    my $signature = shift;
+    my $type = $signature->type;
+
+    AddToImplIncludes("core/dom/ExceptionCode.h") if $type eq "Attr";
+    return 1 if $type eq "Attr";
+    return 0;
 }
 
 sub JSValueToNative
