cf9eeea56a62b5012131fb28454197df96464db7
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 16:41:25 +0000

    [Android WebView] Plumb across window focus changes to ContentViewCore.
    Motivated by getting accessibility support in WebView working. ChromeVox
    requires that the window has focus before functioning as expected, so
    we plumb that through to ContentViewCore from AwContents.
    
    R=bulach@chromium.org, mkosiba@chromium.org
    
    Review URL: https://codereview.chromium.org/15167006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200551 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -138,6 +138,9 @@ public class AwContents {
     private int mLastGlobalVisibleWidth;
     private int mLastGlobalVisibleHeight;
 
+    private boolean mContainerViewFocused;
+    private boolean mWindowFocused;
+
     private static final class DestroyRunnable implements Runnable {
         private int mNativeAwContents;
         private DestroyRunnable(int nativeAwContents) {
@@ -1120,13 +1123,16 @@ public class AwContents {
      * @see android.view.View#onWindowFocusChanged()
      */
     public void onWindowFocusChanged(boolean hasWindowFocus) {
+        mWindowFocused = hasWindowFocus;
+        mContentViewCore.onFocusChanged(mContainerViewFocused && mWindowFocused);
     }
 
     /**
      * @see android.view.View#onFocusChanged()
      */
     public void onFocusChanged(boolean focused, int direction, Rect previouslyFocusedRect) {
-        mContentViewCore.onFocusChanged(focused, direction, previouslyFocusedRect);
+        mContainerViewFocused = focused;
+        mContentViewCore.onFocusChanged(mContainerViewFocused && mWindowFocused);
     }
 
     /**
