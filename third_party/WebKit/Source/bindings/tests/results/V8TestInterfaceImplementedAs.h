978e61f9cbc0c9053e18db85d5e481650f6514d2
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-16 04:23:51 +0000

    Rebaseline run-bindings-tests
    Review URL: https://codereview.chromium.org/14676017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceImplementedAs.h
+++ third_party/WebKit/Source/bindings/tests/results/V8TestInterfaceImplementedAs.h
@@ -26,6 +26,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8DOMWrapper.h"
 #include "bindings/v8/WrapperTypeInfo.h"
+#include "wtf/HashMap.h"
+#include "wtf/text/StringHash.h"
+#include <v8.h>
 
 namespace WebCore {
 
