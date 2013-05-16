f1ab88138d01116d60356ac0f8ba255f29556f75
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 15:15:17 +0000

    Revert 200477 "Fixes reposition logic for adding a new one when ..."
    Somewhat speculative. interactive_ui_tests have been broken on
    Linux ChromiumOS Tests (2) consistently since
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%282%29/builds/13927
    
    The list of failing tests looks notifications-related, and this seeems to be
    the only notifications CL in that build.
    
    Failing test list:
    NoHTMLNotifications
    NotificationsHasPermission
    NotificationsNoPermission
    TabCurrentWindow
    GetLastFocusedWindow
    PopupStaysClosed
    
    > Fixes reposition logic for adding a new one when the center opens.
    > 
    > BUG=240906
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14573009
    
    TBR=mukai@chromium.org
    
    Review URL: https://codereview.chromium.org/14793025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200535 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/views/message_center_view.cc
+++ ui/message_center/views/message_center_view.cc
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/painter.h"
 #include "ui/views/widget/widget.h"
 
+
 namespace message_center {
 
 namespace {
@@ -642,14 +643,11 @@ void RichMessageListView::DoUpdateIfPossible() {
   }
 
   if (!last_child || reposition_top_ < last_child->bounds().y()) {
-    const int initial_top = std::max(reposition_top_, child_area.y());
-    int top = initial_top;
+    int top = std::max(reposition_top_, child_area.y());
     for (int i = 0; i < child_count(); ++i) {
       views::View* child = child_at(i);
-      if (adding_views_.find(child) == adding_views_.end() &&
-          child->bounds().y() < initial_top) {
+      if (child->bounds().y() < top)
         continue;
-      }
       int height = child->GetHeightForWidth(width);
       AnimateChild(child, top, height);
       if (IsValidChild(child))
