d59c574442107801e4a7a49cef0e4f04570436b0
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 23:55:38 +0000

    Remove now-unused sharedOffscreenGraphicsContext3D() methods.
    The sharedOffscreenGraphicsContext3D() and sharedOffscreenGrContext()
    methods are no longer used, as they were replaced by
    createSharedOffscreenGraphicsContext3DProvider() so they can be
    removed now.
    
    R=jamesr
    BUG=236572
    
    Review URL: https://chromiumcodereview.appspot.com/14708011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200658 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/test_webkit_platform_support.h
+++ webkit/support/test_webkit_platform_support.h
@@ -91,8 +91,6 @@ class TestWebKitPlatformSupport :
 
   virtual WebKit::WebGraphicsContext3D* createOffscreenGraphicsContext3D(
       const WebKit::WebGraphicsContext3D::Attributes&);
-  virtual WebKit::WebGraphicsContext3D* sharedOffscreenGraphicsContext3D();
-  virtual GrContext* sharedOffscreenGrContext();
   virtual WebKit::WebGraphicsContext3DProvider*
       createSharedOffscreenGraphicsContext3DProvider();
   virtual bool canAccelerate2dCanvas();
