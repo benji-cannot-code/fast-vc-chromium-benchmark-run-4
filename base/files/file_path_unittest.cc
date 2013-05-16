821757b21af21c8b0005857a4e6e50caa183de8d
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 19:29:39 +0000

    Make Windows traversal checking handle pathological cases
    Different versions of Windows have undocumented quirks in handling path components 
    (e.g. truncating or ignoring certain leading or trailing characters). In order to avoid potential 
    security bugs we're going to treat components more loosely and risk a few unlikely false 
    positives from FilePath::ReferencesParent(). 
    
    BUG=181617
    R=brettw@chromium.org, ericu@chromium.org
    
    Review URL: https://codereview.chromium.org/12771015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200603 0039d316-1c4b-4281-b951-d872f2087c98

--- base/files/file_path_unittest.cc
+++ base/files/file_path_unittest.cc
@@ -402,6 +402,9 @@ TEST_F(FilePathTest, IsAbsolute) {
     { FPL("//a"),    true },
     { FPL("c:a/b"),  false },
     { FPL("?:/a"),   false },
+    { FPL("/.. "),   false },
+    { FPL("/ .."),   false },
+    { FPL("/..."),   false },
 #if defined(FILE_PATH_USES_DRIVE_LETTERS)
     { FPL("/"),      false },
     { FPL("/a"),     false },
@@ -429,6 +432,9 @@ TEST_F(FilePathTest, IsAbsolute) {
     { FPL("//a"),    true },
     { FPL("c:a\\b"), false },
     { FPL("?:\\a"),  false },
+    { FPL("\\.. "),  false },
+    { FPL("\\ .."),  false },
+    { FPL("\\..."),  false },
 #if defined(FILE_PATH_USES_DRIVE_LETTERS)
     { FPL("\\"),     false },
     { FPL("\\a"),    false },
