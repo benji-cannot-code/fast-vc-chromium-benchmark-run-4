f585fcc54062e87845903c62d5835c0ea440ed32
keishi@chromium.org <keishi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 03:30:47 +0000

    Use the default theme as the fallback UI in Blink
    Most native themes don't support zooming or styling required by Blink.
    We will be using the default theme as fallback.
    
    We introduce NativeThemeDefault that can be used in parallel with NativeThemeMac/Gtk/Win/Android.
    Rename webthemeengine_impl_default to webdefaultthemeengine_impl.
    
    Blink side change: https://codereview.chromium.org/14108007/
    
    BUG=84973
    
    Review URL: https://chromiumcodereview.appspot.com/14424007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200431 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ webkit/glue/webfallbackthemeengine_impl.h
@@ -0,0 +1,27 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef WEBKIT_GLUE_WEBFALLBACKTHEMEENGINE_IMPL_H_
+#define WEBKIT_GLUE_WEBFALLBACKTHEMEENGINE_IMPL_H_
+
+#include "third_party/WebKit/Source/Platform/chromium/public/WebFallbackThemeEngine.h"
+
+namespace webkit_glue {
+
+class WebFallbackThemeEngineImpl : public WebKit::WebFallbackThemeEngine {
+ public:
+  // WebFallbackThemeEngine methods:
+  virtual WebKit::WebSize getSize(WebKit::WebFallbackThemeEngine::Part);
+  virtual void paint(
+      WebKit::WebCanvas* canvas,
+      WebKit::WebFallbackThemeEngine::Part part,
+      WebKit::WebFallbackThemeEngine::State state,
+      const WebKit::WebRect& rect,
+      const WebKit::WebFallbackThemeEngine::ExtraParams* extra_params);
+};
+
+}  // namespace webkit_glue
+
+#endif  // WEBKIT_GLUE_WEBFALLBACKTHEMEENGINE_IMPL_H_
