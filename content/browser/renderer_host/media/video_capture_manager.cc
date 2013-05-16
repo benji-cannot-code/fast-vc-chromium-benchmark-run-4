aa174e90bbce46fe20e64eb8b3c242d93e668e2f
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 17:42:25 +0000

    [Android WebView] Fix downstream Android WebView build.
    Android WebView does not checkout third_party/webrtc and
    https://chromiumcodereview.appspot.com/13983010/ introduced
    a dependency on it via an include in the content/ layer.
    
    Correctly ifdef the content/ include as it's not used by all
    platforms.
    
    R=torne@chromium.org, wjia@chromium.org
    
    Review URL: https://codereview.chromium.org/14579007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200567 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/video_capture_manager.cc
+++ content/browser/renderer_host/media/video_capture_manager.cc
@@ -19,8 +19,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/common/content_switches.h"
 #include "content/public/common/media_stream_request.h"
 #include "media/video/capture/fake_video_capture_device.h"
-#include "media/video/capture/screen/screen_capture_device.h"
 #include "media/video/capture/video_capture_device.h"
+
+#if (defined(OS_LINUX) && defined(USE_X11)) || \
+    defined(OS_MACOSX) || defined(OS_WIN)
+#include "media/video/capture/screen/screen_capture_device.h"
+#endif
 
 namespace content {
 
