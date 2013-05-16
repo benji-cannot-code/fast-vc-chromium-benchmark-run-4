f23d21087298c44b728bb654e70599c4363e5ac1
cevans@chromium.org <cevans@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 02:16:31 +0000

    Fix compile fail due to old headers in Daisy build environment.
    TBR=akalin@chromium.org
    
    Review URL: https://codereview.chromium.org/14956008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200416 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_seccomp_bpf_linux.cc
+++ content/common/sandbox_seccomp_bpf_linux.cc
@@ -22,6 +22,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#if defined(__arm__) && !defined(MAP_STACK)
+#define MAP_STACK 0x20000  // Daisy build environment has old headers.
+#endif
+
 #include "base/basictypes.h"
 #include "base/command_line.h"
 #include "base/logging.h"
