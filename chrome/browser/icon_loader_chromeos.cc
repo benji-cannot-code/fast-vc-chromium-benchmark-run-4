720a0288f87b83714c7575dc44e492dd3bbf6b38
jamesr@chromium.org <jamesr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 03:17:25 +0000

    Remove unused webkit/glue/image_decoder.h include from icon_loader_chromeos
    The include, it does nothing!
    
    BUG=237267
    
    Review URL: https://chromiumcodereview.appspot.com/14932018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200428 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/icon_loader_chromeos.cc
+++ chrome/browser/icon_loader_chromeos.cc
@@ -25,7 +25,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image.h"
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/image/image_skia_operations.h"
-#include "webkit/glue/image_decoder.h"
 
 namespace {
 
