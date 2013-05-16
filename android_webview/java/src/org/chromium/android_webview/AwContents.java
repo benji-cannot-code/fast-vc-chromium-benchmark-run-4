01fa4a2e7c61b1465fa6402b5dc311f1426f3bd9
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 17:26:35 +0000

    [Android WebView] Add AwContents.supportsAccessibilityAction.
    Plumb ContentViewCore.supportsAccessibilityAction through AwContents.
    
    R=mkosiba@chromium.org
    
    Review URL: https://codereview.chromium.org/14793024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200561 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1243,6 +1243,10 @@ public class AwContents {
         mContentViewCore.onInitializeAccessibilityEvent(event);
     }
 
+    public boolean supportsAccessibilityAction(int action) {
+        return mContentViewCore.supportsAccessibilityAction(action);
+    }
+
     /**
      * @see android.webkit.WebView#performAccessibilityAction(int, Bundle)
      */
