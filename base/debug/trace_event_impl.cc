12bb583e748ec8def92c64cf4a44079752d64af1
wangxianzhu@chromium.org <wangxianzhu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-16 10:56:38 +0000

    Ensure ATrace flag is applied on Android
    ATrace is enabled when the systrace 'view' tag is enabled, no matter
    whether the Chromium's trace event is enabled. We should ensure the
    ATrace flag is applied onto the g_category_group_enabled elements when
    needed.
    
    Last change to TraceEventImpl missed some cases that need to apply
    the ATrace flag on Android, e.g. when a new category is created when
    the Chromium's trace event is not enabled.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/14984011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200510 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/trace_event_impl.cc
+++ base/debug/trace_event_impl.cc
@@ -830,6 +830,10 @@ const char* TraceLog::GetCategoryGroupName(
 void TraceLog::EnableIncludedCategoryGroup(int category_index) {
   bool is_enabled = category_filter_.IsCategoryGroupEnabled(
       g_category_groups[category_index]);
+  SetCategoryGroupEnabled(category_index, is_enabled);
+}
+
+void TraceLog::SetCategoryGroupEnabled(int category_index, bool is_enabled) {
   g_category_group_enabled[category_index] =
       is_enabled ? TraceLog::CATEGORY_ENABLED : 0;
 
@@ -877,7 +881,7 @@ const unsigned char* TraceLog::GetCategoryGroupEnabledInternal(
         // thereby enabling this category group.
         EnableIncludedCategoryGroup(new_index);
       } else {
-        g_category_group_enabled[new_index] = 0;
+        SetCategoryGroupEnabled(new_index, false);
       }
       category_group_enabled = &g_category_group_enabled[new_index];
     } else {
@@ -988,7 +992,7 @@ void TraceLog::SetDisabled() {
   watch_category_ = NULL;
   watch_event_name_ = "";
   for (int i = 0; i < g_category_index; i++)
-    g_category_group_enabled[i] = 0;
+    SetCategoryGroupEnabled(i, false);
   AddThreadNameMetadataEvents();
 }
 
