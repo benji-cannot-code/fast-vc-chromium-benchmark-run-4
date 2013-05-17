fce3dc34d489679529a8b19993e3a8439387f99d
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-17 00:20:43 +0000

    Remove the idle logout dialog extra border.
    IdleLogoutDialogView uses Widget::CreateWindowWithParent. 
    Use DialogDelegate::CreateDialogWidget instead. 
    Does not change the old-style appearance or behavior. 
    See before/after pics at http://crbug.com/166075#c77
    I triggered the dialog with debug code in Patch Set 1.
    
    BUG=166075
    TEST=new-style idle logout dialog does not have an extra border. 
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15042008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/ui/idle_logout_dialog_view.cc
+++ chrome/browser/chromeos/ui/idle_logout_dialog_view.cc
@@ -150,8 +150,8 @@ void IdleLogoutDialogView::Show() {
 
   UpdateCountdown();
 
-  views::Widget::CreateWindowWithContext(this,
-                                         ash::Shell::GetPrimaryRootWindow());
+  views::DialogDelegate::CreateDialogWidget(
+      this, ash::Shell::GetPrimaryRootWindow(), NULL);
   GetWidget()->SetAlwaysOnTop(true);
   GetWidget()->Show();
 
