1bfba888a404e9cb79f4475dccce50b120c23fc5
junov@chromium.org <junov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-17 14:02:16 +0000

    Revert 150550 "Finish cleaning visit link hashing"
    Reason: causes a large number of layout tests to crash on many platforms.
    
    > Finish cleaning visit link hashing
    > 
    > This CL completes my cleanup of the visiting link hashing code. I was hoping to
    > simplify VisitedLinkState::determineLinkStateSlowCase more, but each of the
    > remaining cases appears to be needed.
    > 
    > R=eseidel@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/14634016
    
    TBR=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/15302003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/VisitedLinkState.h
+++ third_party/WebKit/Source/core/dom/VisitedLinkState.h
@@ -34,8 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/Element.h"
 #include "core/platform/LinkHash.h"
 #include "core/rendering/style/RenderStyleConstants.h"
-#include "wtf/HashSet.h"
-#include "wtf/OwnPtr.h"
+#include <wtf/HashSet.h>
+#include <wtf/OwnPtr.h>
 
 namespace WebCore {
 
@@ -51,7 +51,7 @@ public:
     EInsideLink determineLinkState(Element*);
 
 private:
-    explicit VisitedLinkState(Document*);
+    VisitedLinkState(Document*);
 
     EInsideLink determineLinkStateSlowCase(Element*);
 
