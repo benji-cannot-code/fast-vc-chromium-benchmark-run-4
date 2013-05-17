8745530ab8f67c181aa75a1a49b1195d16174c8f
junov@chromium.org <junov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-17 14:34:05 +0000

    Revert 150567 "Add CSS property tracking to UseCounter. "
    Gardening: causing crash in Layout tests:
    http/tests/css/shared-stylesheet-mutation-preconstruct.html 
    http/tests/css/shared-stylesheet-mutation.html
    
    > Add CSS property tracking to UseCounter. 
    > 
    > Creates new histogram for CSS Property usage data.
    > 
    > From an original patch by Kassy Coan (kassycoan@gmail.com)
    > 
    > This was previously rolled out in r149664 due to causing a memory regression.
    > It's been rewritten to no longer use a RefPtr to a Document, as this was
    > causing Document to never be destroyed. Instead, a UseCounter is
    > passed directly into a CSSParser upon construction.
    > 
    > BUG=230243
    > 
    > Review URL: https://chromiumcodereview.appspot.com/15023020
    
    TBR=mikelawther@chromium.org
    
    Review URL: https://codereview.chromium.org/15298007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser.h
+++ third_party/WebKit/Source/core/css/CSSParser.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/css/CSSPropertySourceData.h"
 #include "core/css/CSSSelector.h"
 #include "core/css/MediaQuery.h"
-#include "core/page/UseCounter.h"
 #include "core/platform/graphics/Color.h"
 #include <wtf/HashMap.h>
 #include <wtf/HashSet.h>
@@ -84,7 +83,7 @@ public:
         GeneralError
     };
 
-    CSSParser(const CSSParserContext&, UseCounter* = 0);
+    CSSParser(const CSSParserContext&);
 
     ~CSSParser();
 
@@ -621,8 +620,6 @@ private:
     bool isCalculation(CSSParserValue*);
 
     inline unsigned safeUserStringTokenOffset();
-
-    UseCounter* m_useCounter;
 
     friend class TransformOperationInfo;
     friend class FilterOperationInfo;
