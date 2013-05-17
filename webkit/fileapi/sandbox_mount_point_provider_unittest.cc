1a608da100dfb9644686f204c7f02ffd97bdce87
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-17 02:27:46 +0000

    Make Windows traversal checking handle pathological cases
    Different versions of Windows have undocumented quirks in handling path components 
    (e.g. truncating or ignoring certain leading or trailing characters). In order to avoid potential 
    security bugs we're going to treat components more loosely and risk a few unlikely false 
    positives from FilePath::ReferencesParent(). 
    
    BUG=181617
    R=brettw@chromium.org, ericu@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=200603
    
    Review URL: https://codereview.chromium.org/12771015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200707 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/fileapi/sandbox_mount_point_provider_unittest.cc
+++ webkit/fileapi/sandbox_mount_point_provider_unittest.cc
@@ -181,10 +181,10 @@ TEST(SandboxMountPointProviderTest, AccessPermissions) {
   EXPECT_EQ(FILE_PERMISSION_ALWAYS_ALLOW,
             provider.GetPermissionPolicy(CreateFileSystemURL(". "),
                                          kCreateFilePermissions));
-  EXPECT_EQ(FILE_PERMISSION_ALWAYS_ALLOW,
+  EXPECT_EQ(FILE_PERMISSION_ALWAYS_DENY,
             provider.GetPermissionPolicy(CreateFileSystemURL(" .."),
                                          kCreateFilePermissions));
-  EXPECT_EQ(FILE_PERMISSION_ALWAYS_ALLOW,
+  EXPECT_EQ(FILE_PERMISSION_ALWAYS_DENY,
             provider.GetPermissionPolicy(CreateFileSystemURL(".. "),
                                          kCreateFilePermissions));
   EXPECT_EQ(FILE_PERMISSION_ALWAYS_ALLOW,
