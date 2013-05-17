49c90b7ff3fdbae2cc0718a2e639831b6408850d
dpolukhin@chromium.org <dpolukhin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-17 19:47:49 +0000

    Postponed posting pref change even to UI until it is visible
    Sending messages to UI cause switch from account picker screen to account picker screen
    that inhibits animation completion signal so correct initialization could not happen.
    
    BUG=180520
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/15279002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -346,7 +346,7 @@ SigninScreenHandler::SigninScreenHandler(
       network_state_informer_(network_state_informer),
       weak_factory_(this),
       webui_visible_(false),
-      login_ui_active_(false),
+      preferences_changed_delayed_(false),
       error_screen_actor_(error_screen_actor),
       is_first_update_state_call_(true),
       offline_login_active_(false),
@@ -841,10 +841,11 @@ void SigninScreenHandler::OnUserImageChanged(const User& user) {
 }
 
 void SigninScreenHandler::OnPreferencesChanged() {
-  // Make sure that one of the login UI is active now, otherwise
+  // Make sure that one of the login UI is fully functional now, otherwise
   // preferences update would be picked up next time it will be shown.
-  if (!login_ui_active_) {
-    LOG(WARNING) << "Login UI is not active - ignoring prefs change.";
+  if (!webui_visible_) {
+    LOG(WARNING) << "Login UI is not active - postponed prefs change.";
+    preferences_changed_delayed_ = true;
     return;
   }
 
@@ -854,6 +855,7 @@ void SigninScreenHandler::OnPreferencesChanged() {
     SendUserList(false);
     UpdateUIState(UI_STATE_ACCOUNT_PICKER, NULL);
   }
+  preferences_changed_delayed_ = false;
 }
 
 void SigninScreenHandler::ResetSigninScreenHandlerDelegate() {
@@ -1407,6 +1409,8 @@ void SigninScreenHandler::HandleLoginVisible(const std::string& source) {
         content::NotificationService::NoDetails());
   }
   webui_visible_ = true;
+  if (preferences_changed_delayed_)
+    OnPreferencesChanged();
 }
 
 void SigninScreenHandler::HandleCancelPasswordChangedFlow() {
@@ -1428,6 +1432,9 @@ void SigninScreenHandler::HandleResyncUserData() {
 
 void SigninScreenHandler::HandleLoginUIStateChanged(const std::string& source,
                                                     bool new_value) {
+  LOG(INFO) << "Login WebUI >> active: " << new_value << ", "
+            << "source: " << source;
+
   if (source == kSourceGaiaSignin) {
     ui_state_ = UI_STATE_GAIA_SIGNIN;
   } else if (source == kSourceAccountPicker) {
@@ -1436,10 +1443,6 @@ void SigninScreenHandler::HandleLoginUIStateChanged(const std::string& source,
     NOTREACHED();
     return;
   }
-
-  LOG(INFO) << "Login WebUI >> active: " << new_value << ", "
-            << "source: " << source;
-  login_ui_active_ = new_value;
 }
 
 void SigninScreenHandler::HandleUnlockOnLoginSuccess() {
