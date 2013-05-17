29829c9190e7bfc0d0fa789060fff8c67130734c
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-17 01:42:18 +0000

    Add renderer overscroll hooks to the browser
    As per crrev.com/14203007/, the browser can request overscroll events be
    forwarded from the renderer. This patch enables forwarding for Android, and adds
    hooks to RenderWidgetHost* for receiving and dispatching overscroll messages.
    
    BUG=135975
    
    Review URL: https://chromiumcodereview.appspot.com/14402020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200690 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -866,6 +866,7 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableVsyncNotification,
     switches::kEnableWebPInAcceptHeader,
     switches::kDisableWebKitMediaSource,
+    switches::kEnableOverscrollNotifications,
     switches::kEnableStrictSiteIsolation,
     switches::kDisableFullScreen,
     switches::kEnableNewDialogStyle,
--- content/browser/renderer_host/render_widget_host_view_base.h
+++ content/browser/renderer_host/render_widget_host_view_base.h
@@ -78,6 +78,8 @@ class CONTENT_EXPORT RenderWidgetHostViewBase
   virtual void EndFrameSubscription() OVERRIDE;
   virtual void OnSwapCompositorFrame(
       scoped_ptr<cc::CompositorFrame> frame) OVERRIDE {}
+  virtual void OnOverscrolled(gfx::Vector2dF accumulated_overscroll,
+                              gfx::Vector2dF current_fling_velocity) OVERRIDE;
 
   void SetBrowserAccessibilityManager(BrowserAccessibilityManager* manager);
 
