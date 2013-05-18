c557097f0f49654703d0cfdb37860e758fada1a2
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-18 01:56:59 +0000

    Fix the ash_shell check fail issue caused by PowerManagerHandler not initialized.
    BUG=241690
    TBR=sky
    
    Review URL: https://chromiumcodereview.appspot.com/15305006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200953 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shell/content_client/shell_browser_main_parts.cc
+++ ash/shell/content_client/shell_browser_main_parts.cc
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_CHROMEOS)
 #include "chromeos/audio/cras_audio_handler.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
+#include "chromeos/power/power_manager_handler.h"
 #endif
 
 namespace ash {
@@ -120,6 +121,7 @@ void ShellBrowserMainParts::PreMainMessageLoopRun() {
     // is absent.
     chromeos::CrasAudioHandler::InitializeForTesting();
   }
+  chromeos::PowerManagerHandler::Initialize();
 #endif
 
   ash::Shell::CreateInstance(delegate_);
@@ -155,6 +157,7 @@ void ShellBrowserMainParts::PostMainMessageLoopRun() {
   message_center::MessageCenter::Shutdown();
 
 #if defined(OS_CHROMEOS)
+  chromeos::PowerManagerHandler::Shutdown();
   if (ash::switches::UseNewAudioHandler())
     chromeos::CrasAudioHandler::Shutdown();
 #endif
