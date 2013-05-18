af145d5b0cfe8f2f0ff5bc9d150766d5e987fa52
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-18 08:44:32 +0000

    Revert 200954 "Revert "InstantExtended: Prevent spurious themech..." 200954 breaks mac interactive_ui_tests:InstantExtendedTest.ReloadSearchAfterBackReloadsCorrectQuery
    > Revert "InstantExtended: Prevent spurious themechanged/mostvisitedchanged events"
    > 
    > This reverts commit 9fa73739c47280860c0e5a44af62885bee391e12.
    > 
    > BUG=238671
    > 
    > Review URL: https://chromiumcodereview.appspot.com/15297013
    
    TBR=jered@chromium.org
    
    Review URL: https://codereview.chromium.org/15404003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200966 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/instant_types.h
+++ chrome/common/instant_types.h
@@ -140,6 +140,8 @@ enum ThemeBackgroundImageTiling {
   THEME_BKGRND_IMAGE_REPEAT,
 };
 
+// Update IsThemeInfoEqual in chrome/renderer/searchbox/searchbox.cc
+// whenever any fields are added/removed.
 struct ThemeBackgroundInfo {
   ThemeBackgroundInfo();
   ~ThemeBackgroundInfo();
@@ -175,6 +177,8 @@ struct ThemeBackgroundInfo {
   bool has_attribution;
 };
 
+// Update AreMostVisitedItemsEqual in chrome/renderer/searchbox/searchbox.cc
+// whenever any fields are added/removed.
 struct InstantMostVisitedItem {
   // The URL of the Most Visited item.
   GURL url;
