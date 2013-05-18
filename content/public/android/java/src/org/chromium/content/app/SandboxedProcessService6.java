4dae727939d393a327d018b56b719f2afcd097c5
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-18 08:48:58 +0000

    Define containers for more SandboxedProcessServices.
    More available, isolated renderers gives us a greater margin of safety.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/14622024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@200970 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/public/android/java/src/org/chromium/content/app/SandboxedProcessService6.java
@@ -0,0 +1,13 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+package org.chromium.content.app;
+
+// This is needed to register multiple SandboxedProcess services so that we can have
+// more than one sandboxed process.
+
+public class SandboxedProcessService6 extends SandboxedProcessService {
+
+}
