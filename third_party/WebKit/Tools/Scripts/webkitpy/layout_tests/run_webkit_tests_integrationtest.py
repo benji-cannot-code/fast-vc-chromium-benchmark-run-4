ce82ce924f79b76f9c45b28e057abca4723e08ba
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-19 03:31:15 +0000

    Generate a failing_results.json file.
    full_results.json is trying to be too many things and is doing poorly at all of them.
    Instead, full_results.json should be the complete information about the test run and
    failing_results.json should just include failures. garden-o-matic and results.html
    will use failing_results.json and the flakiness dashboard will use full_results.json.
    
    This patch starts by generating failing_results.json. Once we have been uploading it for
    a while to the bots, we can switch garden-o-matic over to using failing_results.json
    and then change full_results.json to actually be the full results.
    
    Review URL: https://chromiumcodereview.appspot.com/15409007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150634 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests_integrationtest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/run_webkit_tests_integrationtest.py
@@ -207,15 +207,18 @@ class RunTest(unittest.TestCase, StreamTestingMixin):
         self.assertTrue(one_line_summary in logging_stream.buflist)
 
         # Ensure the results were summarized properly.
-        self.assertEqual(details.summarized_results['num_regressions'], details.exit_code)
+        self.assertEqual(details.summarized_failing_results['num_regressions'], details.exit_code)
 
         # Ensure the image diff percentage is in the results.
-        self.assertEqual(details.summarized_results['tests']['failures']['expected']['image.html']['image_diff_percent'], 1)
+        self.assertEqual(details.summarized_failing_results['tests']['failures']['expected']['image.html']['image_diff_percent'], 1)
 
         # Ensure the results were written out and displayed.
+        failing_results_text = host.filesystem.read_text_file('/tmp/layout-test-results/failing_results.json')
+        json_to_eval = failing_results_text.replace("ADD_RESULTS(", "").replace(");", "")
+        self.assertEqual(json.loads(json_to_eval), details.summarized_failing_results)
+
         full_results_text = host.filesystem.read_text_file('/tmp/layout-test-results/full_results.json')
-        json_to_eval = full_results_text.replace("ADD_RESULTS(", "").replace(");", "")
-        self.assertEqual(json.loads(json_to_eval), details.summarized_results)
+        self.assertEqual(json.loads(full_results_text), details.summarized_full_results)
 
         self.assertEqual(host.user.opened_urls, [path.abspath_to_uri(MockHost().platform, '/tmp/layout-test-results/results.html')])
 
@@ -916,7 +919,7 @@ class RebaselineTest(unittest.TestCase, StreamTestingMixin):
             tests_included=True, host=host, new_results=True)
         file_list = host.filesystem.written_files.keys()
         self.assertEqual(details.exit_code, 0)
-        self.assertEqual(len(file_list), 7)
+        self.assertEqual(len(file_list), 8)
         self.assertBaselines(file_list, "passes/image", [".txt", ".png"], err)
         self.assertBaselines(file_list, "failures/expected/missing_image", [".txt", ".png"], err)
 
@@ -932,7 +935,7 @@ class RebaselineTest(unittest.TestCase, StreamTestingMixin):
             tests_included=True, host=host, new_results=True)
         file_list = host.filesystem.written_files.keys()
         self.assertEqual(details.exit_code, 0)
-        self.assertEqual(len(file_list), 9)
+        self.assertEqual(len(file_list), 10)
         self.assertBaselines(file_list, "failures/unexpected/missing_text", [".txt"], err)
         self.assertBaselines(file_list, "platform/test/failures/unexpected/missing_image", [".png"], err)
         self.assertBaselines(file_list, "platform/test/failures/unexpected/missing_render_tree_dump", [".txt"], err)
@@ -946,7 +949,7 @@ class RebaselineTest(unittest.TestCase, StreamTestingMixin):
             tests_included=True, host=host, new_results=True)
         file_list = host.filesystem.written_files.keys()
         self.assertEqual(details.exit_code, 0)
-        self.assertEqual(len(file_list), 7)
+        self.assertEqual(len(file_list), 8)
         self.assertBaselines(file_list,
             "platform/test-mac-leopard/passes/image", [".txt", ".png"], err)
         self.assertBaselines(file_list,
