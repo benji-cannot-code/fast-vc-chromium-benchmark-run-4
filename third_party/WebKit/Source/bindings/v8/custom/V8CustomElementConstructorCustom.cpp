600fac08ee4e0a98a6990327a45f862fd298a093
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 05:14:53 +0000

    Rename [CustomCall] with [LegacyCall]
    [CustomCall] corresponds to a 'legacycall' keyword in the Web IDL spec.
    Although it is not easy to implement the keyword, at least we should
    rename [CustomCall] to [LegacyCall].
    
    R=abarth,eseidel
    
    Review URL: https://chromiumcodereview.appspot.com/15303006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150647 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8CustomElementConstructorCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8CustomElementConstructorCustom.cpp
@@ -41,7 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-v8::Handle<v8::Value> V8CustomElementConstructor::callAsFunctionCallback(const v8::Arguments& args)
+v8::Handle<v8::Value> V8CustomElementConstructor::legacyCallCustom(const v8::Arguments& args)
 {
     if (!args.IsConstructCall())
         return throwTypeError("DOM object constructor cannot be called as a function.", args.GetIsolate());
