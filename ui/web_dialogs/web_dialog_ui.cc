4c94f1ae8837401464cbed628b8d9a3b7b5fae4a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-20 06:11:17 +0000

    web_dialogs: Remove unused notification_service.h include.
    BUG=170921
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15420002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201044 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/web_dialogs/web_dialog_ui.cc
+++ ui/web_dialogs/web_dialog_ui.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/lazy_instance.h"
 #include "base/values.h"
-#include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
