e0cc8a36d50517d700c5c4b555c008eb616aa91f
olaru@adobe.com <olaru@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 22:50:04 +0000

    Addding support for the background-blend-mode CSS property.
    The patch forwards the background blend modes from WebCore to Skia, enabling background images to blend between themeselves.
    
    Added 3 pixel tests. The first two, effect-background-blend-mode and effect-background-blend-mode-stacking are just readded after being removed in CL 149989. They were removed because they failed, since background blend mode was not implemented, and TestExpectations entries were ignored.
    
    The effect-background-blend-mode-tiled test is new, and causes Image::drawPattern to be called (since the other background blending tests did not go through that function).
    
    Add logic to override the acceleratedCompositingEnabled setting on testRunner through the WebKitAcceleratedCompositingEnabled key. This was necessary because blending was rendered in software in Skia until not long ago, and there was no hardware path available.
    
    R=eric@chromium.org
    BUG=229166
    
    Review URL: https://chromiumcodereview.appspot.com/14208013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150710 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/skia/ImageSkia.cpp
+++ third_party/WebKit/Source/core/platform/graphics/skia/ImageSkia.cpp
@@ -441,7 +441,7 @@ void Image::drawPattern(GraphicsContext* context,
                         ColorSpace styleColorSpace,
                         CompositeOperator compositeOp,
                         const FloatRect& destRect,
-                        BlendMode)
+                        BlendMode blendMode)
 {
     TRACE_EVENT0("skia", "Image::drawPattern");
     RefPtr<NativeImageSkia> bitmap = nativeImageForCurrentFrame();
@@ -521,7 +521,8 @@ void Image::drawPattern(GraphicsContext* context,
 
     SkPaint paint;
     paint.setShader(shader)->unref();
-    paint.setXfermodeMode(WebCoreCompositeToSkiaComposite(compositeOp));
+    paint.setXfermodeMode(WebCoreCompositeToSkiaComposite(compositeOp, blendMode));
+
     paint.setFilterBitmap(resampling == RESAMPLE_LINEAR);
 
     context->drawRect(destRect, paint);
