3d24995f3d7cdba5a51b5a8513a72a89660ecaa3
keishi@chromium.org <keishi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-20 14:35:10 +0000

    Use the default theme as the fallback UI in Blink
    Most native themes don't support zooming or styling required by Blink.
    This introduces a FallbackTheme that can be used in these situations.
    
    Blink side change: https://codereview.chromium.org/14108007/
    
    BUG=84973
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=200431
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=200785
    
    Review URL: https://chromiumcodereview.appspot.com/14424007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201085 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/native_theme/native_theme_aura.h
+++ ui/native_theme/native_theme_aura.h
@@ -8,12 +8,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
-#include "ui/native_theme/native_theme_base.h"
+#include "ui/native_theme/fallback_theme.h"
 
 namespace ui {
 
 // Aura implementation of native theme support.
-class NATIVE_THEME_EXPORT NativeThemeAura : public NativeThemeBase {
+class NATIVE_THEME_EXPORT NativeThemeAura : public FallbackTheme {
  public:
   static NativeThemeAura* instance();
 
@@ -22,7 +22,6 @@ class NATIVE_THEME_EXPORT NativeThemeAura : public NativeThemeBase {
   virtual ~NativeThemeAura();
 
   // Overridden from NativeThemeBase:
-  virtual SkColor GetSystemColor(ColorId color_id) const OVERRIDE;
   virtual void PaintMenuPopupBackground(
       SkCanvas* canvas,
       const gfx::Size& size,
