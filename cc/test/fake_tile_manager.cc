2b075177cdc854f8f156e8428387cddf92f36e2f
vmpstr@chromium.org <vmpstr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-20 19:20:45 +0000

    cc: Decouple tile manager from raster worker pool
    In order to unittest tile manager, it would be nice if we
    can fake raster worker pool (and possibly other components)
    that tile manager currently creates in its constructor.
    
    This should be a small step towards making tile manager more
    testable.
    
    Review URL: https://chromiumcodereview.appspot.com/14803016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201118 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_tile_manager.cc
+++ cc/test/fake_tile_manager.cc
@@ -5,9 +5,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "cc/test/fake_tile_manager.h"
 
+#include "cc/resources/raster_worker_pool.h"
+
 namespace cc {
 
 FakeTileManager::FakeTileManager(TileManagerClient* client)
-    : TileManager(client, NULL, 1, false, NULL) {
-}
+    : TileManager(client,
+                  NULL,
+                  RasterWorkerPool::Create(1),
+                  1,
+                  false,
+                  NULL) {}
 }
