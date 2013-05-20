5e38efdce68106d8f6cf52667070f33a5ed0591b
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 18:18:14 +0000

    Move compileScript() and precompileScript() to V8ScriptRunner
    {compile,precompile,run}Script() should be put in V8ScriptRunner.
    I'll share the code with other places in follow-up patches to aggregate
    call sites of Script::XXX() into one place.
    
    BUG=241743
    R=abarth,eseidel
    
    Review URL: https://chromiumcodereview.appspot.com/15300014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/bindings.gypi
+++ third_party/WebKit/Source/bindings/bindings.gypi
@@ -57,7 +57,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'v8/ScriptProfiler.h',
             'v8/ScriptScope.cpp',
             'v8/ScriptScope.h',
-            'v8/ScriptSourceCode.cpp',
             'v8/ScriptSourceCode.h',
             'v8/ScriptState.cpp',
             'v8/ScriptState.h',
--- third_party/WebKit/Source/bindings/v8/V8ScriptRunner.h
+++ third_party/WebKit/Source/bindings/v8/V8ScriptRunner.h
@@ -27,14 +27,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8ScriptRunner_h
 #define V8ScriptRunner_h
 
+#include "wtf/PassOwnPtr.h"
+#include "wtf/text/TextPosition.h"
+#include "wtf/text/WTFString.h"
 #include <v8.h>
 
 namespace WebCore {
 
+class CachedScript;
 class ScriptExecutionContext;
 
 class V8ScriptRunner {
 public:
+    static PassOwnPtr<v8::ScriptData> precompileScript(v8::Handle<v8::String>, CachedScript*);
+    static v8::Handle<v8::Script> compileScript(v8::Handle<v8::String>, const String&, const TextPosition&, v8::ScriptData*, v8::Isolate*);
     static v8::Local<v8::Value> runCompiledScript(v8::Handle<v8::Script>, ScriptExecutionContext*);
     static v8::Local<v8::Value> compileAndRunInternalScript(v8::Handle<v8::String>, v8::Isolate*);
 };
