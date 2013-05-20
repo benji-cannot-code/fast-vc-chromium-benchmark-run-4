68d2fd373b6aba4641080403d8810977ded2b289
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 20:59:03 +0000

    Call dispatchDidReceiveServerRedirectForProvisionalLoad before checking navigation policy for a redirect.
    Repro steps:
    1. Load an application that uses the Chromium WebKit API.
    2. Redirect a request via net::URLRequestJob::IsRedirectResponse by populating
    |location| and returning true.
    
    The |isRedirect| value passed to WebFrameClient::decidePolicyForNavigation in
    FrameLoaderClientImpl::dispatchDecidePolicyForNavigationAction should be true. However, WebKit revision 137607 (http://trac.webkit.org/changeset/137607) changed the implementation of MainResourceLoader::load() such that WebDataSourceImpl::appendRedirect() is no longer called for the redirected URL following this code path. As a result the |isRedirect| value is now false.
    
    BUG=229154
    
    Review URL: https://chromiumcodereview.appspot.com/15303010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -492,6 +492,7 @@ void DocumentLoader::willSendRequest(ResourceRequest& newRequest, const Resource
     if (redirectResponse.isNull())
         return;
 
+    frameLoader()->client()->dispatchDidReceiveServerRedirectForProvisionalLoad();
     if (!shouldContinueForNavigationPolicy(newRequest))
         stopLoadingForPolicyChange();
 }
--- third_party/WebKit/Source/core/loader/ResourceLoader.cpp
+++ third_party/WebKit/Source/core/loader/ResourceLoader.cpp
@@ -329,9 +329,6 @@ void ResourceLoader::willSendRequest(ResourceHandle*, ResourceRequest& request, 
 
     m_request = request;
 
-    if (!redirectResponse.isNull() && !m_documentLoader->isCommitted())
-        frameLoader()->client()->dispatchDidReceiveServerRedirectForProvisionalLoad();
-
     if (request.isNull())
         cancel();
 }
