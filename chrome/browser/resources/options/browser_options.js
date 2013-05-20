5257630b1d246e53a09c78f9f4c2c1097bf7bc63
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-20 20:06:06 +0000

    [sync] Close advanced sync settings dialog if sync gets disabled
    If the advanced sync settings dialog is visible, and the user does a
    dashboard clear, the dialog remains visible, even though the main
    settings page realizes that sync has been disabled, and shows the right
    message.
    
    This patch adds a detection mechanism to the browser options page that
    closes the sync settings overlay in case sync gets disabled in the
    background.
    
    BUG=238938
    TEST=Enable sync, bring up the advanced settings dialog, and do a dashboard clear in the background. The dialog must be dismissed.
    R=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/15347005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -30,6 +30,13 @@ cr.define('options', function() {
     signedIn_: false,
 
     /**
+     * Keeps track of whether the user has completed sync setup or not.
+     * @type {boolean}
+     * @private
+     */
+    setupCompleted_: false,
+
+    /**
      * Keeps track of whether |onShowHomeButtonChanged_| has been called. See
      * |onShowHomeButtonChanged_|.
      * @type {boolean}
@@ -698,12 +705,16 @@ cr.define('options', function() {
 
       $('sync-section').hidden = false;
 
-      // If the user gets signed out while the advanced sync settings dialog is
-      // visible, say, due to a dashboard clear, close the dialog.
-      if (this.signedIn_ && !syncData.signedIn)
+      // If the user gets signed out or if sync gets disabled while the advanced
+      // sync settings dialog is visible, say, due to a dashboard clear, close
+      // the dialog.
+      if ((this.signedIn_ && !syncData.signedIn) ||
+          (this.setupCompleted_ && !syncData.setupCompleted)) {
         SyncSetupOverlay.closeOverlay();
+      }
 
       this.signedIn_ = syncData.signedIn;
+      this.setupCompleted_ = syncData.setupCompleted;
 
       // Display the "advanced settings" button if we're signed in and sync is
       // not managed/disabled. If the user is signed in, but sync is disabled,
