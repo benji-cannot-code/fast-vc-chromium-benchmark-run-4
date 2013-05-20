b8a784e37653c5aa1a776485bab3d0855a0b325a
cbiesinger@chromium.org <cbiesinger@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 22:43:34 +0000

    Don't force layout for mouse event hit tests
    Before the initial document layout, we shouldn't force a layout for
    mouse events, because they are not intended for the new document anyway -
    the user can't see the document yet! Just ignore those events by making
    the hit tests match nothing.
    
    Otherwise, the hit test would force a layout, which is undesirable
    because that leads to a flash of an empty document.
    
    BUG=1373
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/14859016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150709 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/images/script-tests/imagemap-dynamic-area-updates.js
+++ third_party/WebKit/LayoutTests/fast/images/script-tests/imagemap-dynamic-area-updates.js
@@ -26,6 +26,9 @@ map.appendChild(area);
 document.body.appendChild(image);
 document.body.appendChild(map);
 
+// Mouse events only work after an initial layout
+document.body.offsetLeft;
+
 function setArea(shape, coords)
 {
     area.setAttribute('shape', shape);
