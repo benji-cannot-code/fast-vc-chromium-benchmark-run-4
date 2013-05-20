c0da92a7ecd46473bc4abf5de20aabca333b2b11
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 13:36:47 +0000

    DevTools: Introduce backend support for @sourceURL comments in stylesheets
    BUG=241741
    
    Review URL: https://chromiumcodereview.appspot.com/15296002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150661 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorStyleSheet.h
+++ third_party/WebKit/Source/core/inspector/InspectorStyleSheet.h
@@ -216,6 +216,8 @@ protected:
     virtual unsigned ruleIndexByStyle(CSSStyleDeclaration*) const;
     virtual bool ensureParsedDataReady();
     virtual PassRefPtr<InspectorStyle> inspectorStyleForId(const InspectorCSSId&);
+    virtual String sourceMapURL() const;
+    virtual String sourceURL() const;
 
     // Also accessed by friend class InspectorStyle.
     virtual bool setStyleText(CSSStyleDeclaration*, const String&);
@@ -267,6 +269,8 @@ protected:
     virtual unsigned ruleIndexByStyle(CSSStyleDeclaration*) const { return 0; }
     virtual bool ensureParsedDataReady();
     virtual PassRefPtr<InspectorStyle> inspectorStyleForId(const InspectorCSSId&);
+    virtual String sourceMapURL() const OVERRIDE { return String(); }
+    virtual String sourceURL() const OVERRIDE { return String(); }
 
     // Also accessed by friend class InspectorStyle.
     virtual bool setStyleText(CSSStyleDeclaration*, const String&);
