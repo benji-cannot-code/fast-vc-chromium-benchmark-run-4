c8a983dcfe73c7c2f86f3e9ed5fa2afb4a13a896
matthewjheaney@chromium.org <matthewjheaney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 18:46:13 +0000

    WebInbandTextTrack should be deallocated by caller
    An object that inherits from WebInbandTextTrack is created
    on the Chromium side of the WebKit interface, and handed to
    the InbandTextTrackPrivateImpl (via its constructor). The
    InbandTextTrackPrivateImpl object currently assumes
    ownership of the pointer, and so the
    InbandTextTrackPrivateImpl object deallocates the
    WebInbandTextTrack object (via its destructor).
    
    However, this means that abstractions on the Chromium side
    of the WebKit interface can potentially outlive the
    abstractions on the WebKit side on which they depend.
    
    To ameliorate such lifetime issues, the responsibility for
    deallocating the WebInbandTextTrack object was moved to the
    Chromium side, hence the InbandTextTrackPrivateImpl object
    is no longer required to deallocate that object.
    
    Note that the this change does not have a dedicated unit
    test, because no code exists that actually calls this code.
    These changes to WebKit are a prerequisite for the change
    that adds support for inband text track rendering to Chrome,
    as enunciated here:
    
    https://chromiumcodereview.appspot.com/13419002/
    
    The requisite infrastructure for testing all of the changes
    in toto will be included when that cited change is
    committed.
    
    BUG=230708
    
    Review URL: https://chromiumcodereview.appspot.com/15236002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150686 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/InbandTextTrackPrivateImpl.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/InbandTextTrackPrivateImpl.cpp
@@ -33,12 +33,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebInbandTextTrack.h"
 #include "core/platform/graphics/InbandTextTrackPrivateClient.h"
 #include "public/WebString.h"
-#include <wtf/PassOwnPtr.h>
 
 namespace WebKit {
 
 InbandTextTrackPrivateImpl::InbandTextTrackPrivateImpl(WebInbandTextTrack* track)
-    : m_track(adoptPtr(track))
+    : m_track(track)
 {
     ASSERT(track);
     track->setClient(this);
--- third_party/WebKit/Source/WebKit/chromium/src/InbandTextTrackPrivateImpl.h
+++ third_party/WebKit/Source/WebKit/chromium/src/InbandTextTrackPrivateImpl.h
@@ -34,7 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/platform/graphics/InbandTextTrackPrivate.h"
 #include "WebInbandTextTrackClient.h"
-#include <wtf/OwnPtr.h>
 
 namespace WebCore {
 class InbandTextTrackPrivateClient;
@@ -73,7 +72,7 @@ public:
                               const WebString& settings);
 
 private:
-    OwnPtr<WebInbandTextTrack> m_track;
+    WebInbandTextTrack* m_track;
 };
 
 }
