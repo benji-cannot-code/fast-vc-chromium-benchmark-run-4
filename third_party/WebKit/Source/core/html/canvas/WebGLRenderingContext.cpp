e9c5eb52bb3ed821e0944f8bbf06ceb4b905090c
gman@chromium.org <gman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-20 21:21:37 +0000

    Make WebGL finish a flush
    finish in WebGL has no meaning. Users use it thinking they know
    how it works and getting incorrect results. Changing it to a
    flush will have no semantic changes but will discourage incorrect
    usage.
    
    BUG=242210
    
    Review URL: https://chromiumcodereview.appspot.com/15470009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150699 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/WebGLRenderingContext.cpp
+++ third_party/WebKit/Source/core/html/canvas/WebGLRenderingContext.cpp
@@ -1854,7 +1854,7 @@ void WebGLRenderingContext::finish()
 {
     if (isContextLost())
         return;
-    m_context->finish();
+    m_context->flush(); // Intentionally a flush, not a finish.
 }
 
 void WebGLRenderingContext::flush()
