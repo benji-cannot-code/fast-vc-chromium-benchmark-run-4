5c29ce10510ca47f9c07edb648c429965d734586
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 07:46:30 +0000

    Revert "Dirty the first RootInlineBox, always"
    This reverts commit a3177c91b7ebf41f71cb74815e85ae93b491e188.
    
    The test added here (editing/selection/rtl-delete-within-line-crash.html)
    hits an ASSERT in RenderTable[1]. Stack trace pasted into
    http://crbug.com/242440.
    
    [1]: http://src.chromium.org/viewvc/blink/trunk/Source/core/rendering/RenderTable.cpp?revision=149477#l314
    
    BUG=242440
    TBR=schenney@chromium.org,eseidel@chromium.org
    
    > Dirty the first RootInlineBox, always
    >
    > The current code does not dirty the first RootInlineBox in a text
    > run if the change affects the first InlineTextBox. It is unclear
    > why it was not considered necessary. However, several problems with
    > rtl text editing have revealed the need to dirty the root box
    > regardless of whether we have already dirtued the line. I believe
    > this is because rtl text layout creates line boxes where they would
    > otherwise not exist.
    >
    > This patch also modifies the fix for an existing SVG rtl bug, which
    > avoids duplciating the same clear operations. Apparently this also
    > fixes a layout test that previously had a failing result.
    >
    > BUG=230726
    >
    > Review URL: https://chromiumcodereview.appspot.com/15271005
    
    Review URL: https://codereview.chromium.org/15538004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150757 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/svg/SVGRootInlineBox.h
+++ third_party/WebKit/Source/core/rendering/svg/SVGRootInlineBox.h
@@ -47,8 +47,6 @@ public:
 
     virtual void paint(PaintInfo&, const LayoutPoint&, LayoutUnit lineTop, LayoutUnit lineBottom) OVERRIDE FINAL;
 
-    virtual void markDirty(bool dirty = true) OVERRIDE FINAL;
-
     void computePerCharacterLayoutInformation();
 
     InlineBox* closestLeafChildForPosition(const LayoutPoint&);
