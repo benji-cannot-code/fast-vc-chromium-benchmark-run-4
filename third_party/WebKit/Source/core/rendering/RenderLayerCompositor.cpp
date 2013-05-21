35561c9b10ca2f21dc2610e3807c1bb2ef020212
trchen@chromium.org <trchen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 01:53:49 +0000

    Remove NonCompositedContentHost
    Safari uses a hybrid scrolling approach. For contents that require
    compositing, they will create compositing layers and will use layer-based
    scrolling. For non-composited contents, they will be painted to a native
    window and will be scrolled with a OS-provided backing.
    
    Chromium doesn't use this native window based scrolling. Instead,
    NonCompositedContentHost was introduced to provide a replacement for
    the OS-provided backing.
    
    Now we no longer need to maintain compatibility with the native window
    approach. This patch removes RenderLayerBacking::paintsIntoWindow().
    Main frame contents will be painted onto its RenderView layer (which was
    always created but only used as container for child layer), and
    NonCompositedContentHost becomes redundant which gets removed too.
    
    R=jamesr@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/13959008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150733 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayerCompositor.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayerCompositor.cpp
@@ -1149,10 +1149,8 @@ void RenderLayerCompositor::frameViewDidScroll()
     // If there's a scrolling coordinator that manages scrolling for this frame view,
     // it will also manage updating the scroll layer position.
     if (ScrollingCoordinator* scrollingCoordinator = this->scrollingCoordinator()) {
-        if (scrollingCoordinator->coordinatesScrollingForFrameView(frameView))
-            return;
         if (Settings* settings = m_renderView->document()->settings()) {
-            if (settings->compositedScrollingForFramesEnabled())
+            if (isMainFrame() || settings->compositedScrollingForFramesEnabled())
                 scrollingCoordinator->scrollableAreaScrollLayerDidChange(frameView);
         }
     }
@@ -1516,7 +1514,6 @@ bool RenderLayerCompositor::requiresOwnBackingStore(const RenderLayer* layer, co
     RenderObject* renderer = layer->renderer();
     if (compositingAncestorLayer
         && !(compositingAncestorLayer->backing()->graphicsLayer()->drawsContent()
-            || compositingAncestorLayer->backing()->paintsIntoWindow()
             || compositingAncestorLayer->backing()->paintsIntoCompositedAncestor()))
         return true;
 
@@ -2493,7 +2490,6 @@ void RenderLayerCompositor::attachRootLayer(RootLayerAttachment attachment)
     }
 
     m_rootLayerAttachment = attachment;
-    rootLayerAttachmentChanged();
 }
 
 void RenderLayerCompositor::detachRootLayer()
@@ -2528,7 +2524,6 @@ void RenderLayerCompositor::detachRootLayer()
     }
 
     m_rootLayerAttachment = RootLayerUnattached;
-    rootLayerAttachmentChanged();
 }
 
 void RenderLayerCompositor::updateRootLayerAttachment()
@@ -2539,15 +2534,6 @@ void RenderLayerCompositor::updateRootLayerAttachment()
 bool RenderLayerCompositor::isMainFrame() const
 {
     return !m_renderView->document()->ownerElement();
-}
-
-void RenderLayerCompositor::rootLayerAttachmentChanged()
-{
-    // The attachment can affect whether the RenderView layer's paintsIntoWindow() behavior,
-    // so call updateGraphicsLayerGeometry() to udpate that.
-    RenderLayer* layer = m_renderView->layer();
-    if (RenderLayerBacking* backing = layer ? layer->backing() : 0)
-        backing->updateDrawsContent();
 }
 
 // IFrames are special, because we hook compositing layers together across iframe boundaries
--- third_party/WebKit/Source/core/rendering/RenderView.cpp
+++ third_party/WebKit/Source/core/rendering/RenderView.cpp
@@ -435,10 +435,8 @@ void RenderView::paintBoxDecorations(PaintInfo& paintInfo, const LayoutPoint&)
         }
 
         if (RenderLayer* compositingLayer = layer->enclosingCompositingLayerForRepaint()) {
-            if (!compositingLayer->backing()->paintsIntoWindow()) {
-                frameView()->setCannotBlitToWindow();
-                break;
-            }
+            frameView()->setCannotBlitToWindow();
+            break;
         }
     }
 
