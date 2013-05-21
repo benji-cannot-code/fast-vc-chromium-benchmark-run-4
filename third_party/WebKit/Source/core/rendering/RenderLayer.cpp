b2a15836539f4ff2ebb245786ff1fc41efd5430f
yael.aharon@intel.com <yael.aharon@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 01:08:06 +0000

    Remove flag DISABLE_ROUNDED_CORNER_CLIPPING.
    It has been disabled in http://trac.webkit.org/changeset/130355 and is no longer needed.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/15492005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150729 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayer.cpp
+++ third_party/WebKit/Source/core/rendering/RenderLayer.cpp
@@ -3512,7 +3512,6 @@ void RenderLayer::paintOverlayScrollbars(GraphicsContext* context, const LayoutR
     m_containsDirtyOverlayScrollbars = false;
 }
 
-#ifndef DISABLE_ROUNDED_CORNER_CLIPPING
 static bool inContainingBlockChain(RenderLayer* startLayer, RenderLayer* endLayer)
 {
     if (startLayer == endLayer)
@@ -3526,7 +3525,6 @@ static bool inContainingBlockChain(RenderLayer* startLayer, RenderLayer* endLaye
     
     return false;
 }
-#endif
 
 void RenderLayer::clipToRect(RenderLayer* rootLayer, GraphicsContext* context, const LayoutRect& paintDirtyRect, const ClipRect& clipRect,
                              BorderRadiusClippingRule rule)
@@ -3539,7 +3537,6 @@ void RenderLayer::clipToRect(RenderLayer* rootLayer, GraphicsContext* context, c
     if (!clipRect.hasRadius())
         return;
 
-#ifndef DISABLE_ROUNDED_CORNER_CLIPPING
     // If the clip rect has been tainted by a border radius, then we have to walk up our layer chain applying the clips from
     // any layers with overflow. The condition for being able to apply these clips is that the overflow object be in our
     // containing block chain so we check that also.
@@ -3553,7 +3550,6 @@ void RenderLayer::clipToRect(RenderLayer* rootLayer, GraphicsContext* context, c
         if (layer == rootLayer)
             break;
     }
-#endif
 }
 
 void RenderLayer::restoreClip(GraphicsContext* context, const LayoutRect& paintDirtyRect, const ClipRect& clipRect)
