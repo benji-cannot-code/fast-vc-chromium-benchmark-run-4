cb3be359a0dc2ca71727bc8a2db11e6b266a2761
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 02:24:06 +0000

    [Autofill] Set origins for all new profiles and credit cards.
    Not included in this CL: Android and Sync changes.
    
    BUG=170401, 231029
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/15051007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201197 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -87,6 +87,11 @@ const char kAddNewItemKey[] = "add-new-item";
 const char kManageItemsKey[] = "manage-items";
 const char kSameAsBillingKey[] = "same-as-billing";
 
+// This string is stored along with saved addresses and credit cards in the
+// WebDB, and hence should not be modified, so that it remains consistent over
+// time.
+const char kAutofillDialogOrigin[] = "Chrome Autofill dialog";
+
 // Returns true if |input| should be shown when |field_type| has been requested.
 bool InputTypeMatchesFieldType(const DetailInput& input,
                                AutofillFieldType field_type) {
@@ -1939,6 +1944,7 @@ void AutofillDialogControllerImpl::FillOutputForSectionWithComparator(
 
     if (section == SECTION_CC) {
       CreditCard card;
+      card.set_origin(kAutofillDialogOrigin);
       FillFormGroupFromOutputs(output, &card);
 
       if (ShouldSaveDetailsLocally())
@@ -1950,6 +1956,7 @@ void AutofillDialogControllerImpl::FillOutputForSectionWithComparator(
       SetCvcResult(GetValueForType(output, CREDIT_CARD_VERIFICATION_CODE));
     } else {
       AutofillProfile profile;
+      profile.set_origin(kAutofillDialogOrigin);
       FillFormGroupFromOutputs(output, &profile);
 
       // For billing, the profile name has to come from the CC section.
