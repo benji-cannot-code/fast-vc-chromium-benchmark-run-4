a7aa5204257a512b7d9f67fbb6556b71c74b8640
mnaganov@google.com <mnaganov@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 11:54:36 +0000

    [Android] Add required include into GraphicsContext3DImagePacking.cpp
    Compilation fix after r150746 (https://chromiumcodereview.appspot.com/15501004)
    
    BUG=241820
    
    TBR=bajones,kbr
    
    Review URL: https://codereview.chromium.org/15554002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/GraphicsContext3D.cpp
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsContext3D.cpp
@@ -40,7 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/Image.h"
 #include "core/platform/graphics/ImageBuffer.h"
 #include "core/platform/graphics/ImageObserver.h"
-#include "core/platform/graphics/cpu/arm/GraphicsContext3DNEON.h"
 #include "core/platform/graphics/gpu/DrawingBuffer.h"
 #include "core/platform/image-decoders/ImageDecoder.h"
 #include "wtf/ArrayBufferView.h"
--- third_party/WebKit/Source/core/platform/graphics/GraphicsContext3DImagePacking.cpp
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsContext3DImagePacking.cpp
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/ImageData.h"
 #include "core/html/canvas/CheckedInt.h"
 #include "core/platform/graphics/ImageObserver.h"
+#include "core/platform/graphics/cpu/arm/GraphicsContext3DNEON.h"
 #include "core/platform/image-decoders/ImageDecoder.h"
 
 namespace WebCore {
