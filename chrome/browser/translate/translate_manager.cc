9dd7218b57aa642bd97c7b3178317cc0fb485203
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 08:36:23 +0000

    Translate: enable context menu against pages having notranslate meta tag
    <meta name="google" value="notranslate"> tag disables Chrome Translate
    feature completely. But, it is better to enable it in context menu.
    
     - enable context menu against pages having notranslate meta tag
     - rename confusing parameter name |page_translatable| to |page_needs_translation|
     - rename confusing function name IsPageTranslatable to IsTranslationAllowed
     - implement GetMetaElementsWithAttribute directly since this function is removed from webkit/glue
    
    BUG=172593
    TBR=palmer@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15271002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager.cc
+++ chrome/browser/translate/translate_manager.cc
@@ -356,8 +356,8 @@ void TranslateManager::Observe(int type,
 
       // When doing a page reload, TAB_LANGUAGE_DETERMINED is not sent,
       // so the translation needs to be explicitly initiated, but only when the
-      // page is translatable.
-      if (!translate_tab_helper->language_state().page_translatable())
+      // page needs translation.
+      if (!translate_tab_helper->language_state().page_needs_translation())
         return;
       // Note that we delay it as the TranslateManager gets this notification
       // before the WebContents and the WebContents processing might remove the
@@ -382,7 +382,7 @@ void TranslateManager::Observe(int type,
         return;
 
       LanguageState& language_state = translate_tab_helper->language_state();
-      if (language_state.page_translatable() &&
+      if (language_state.page_needs_translation() &&
           !language_state.translation_pending() &&
           !language_state.translation_declined() &&
           !language_state.IsPageTranslated()) {
