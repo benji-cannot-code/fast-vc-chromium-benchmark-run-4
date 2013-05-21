808658f0601e842603725d80a0fd76ef1f928fa6
rltoscano@chromium.org <rltoscano@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 05:19:55 +0000

    Begins preview generation as soon as initial state is loaded. Doesn't wait for print destination to load.
    BUG=239588
    
    Review URL: https://chromiumcodereview.appspot.com/15495007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webui/print_preview.js
+++ chrome/test/data/webui/print_preview.js
@@ -225,6 +225,10 @@ function checkElementDisplayed(el, isDisplayed) {
 
 // Test that disabled settings hide the disabled sections.
 TEST_F('PrintPreviewWebUITest', 'TestSectionsDisabled', function() {
+  checkSectionVisible($('layout-settings'), true);
+  checkSectionVisible($('color-settings'), true);
+  checkSectionVisible($('copies-settings'), true);
+
   var initialSettingsSetEvent =
       new cr.Event(print_preview.NativeLayer.EventType.INITIAL_SETTINGS_SET);
   initialSettingsSetEvent.initialSettings = this.initialSettings_;
@@ -234,10 +238,6 @@ TEST_F('PrintPreviewWebUITest', 'TestSectionsDisabled', function() {
       new cr.Event(print_preview.NativeLayer.EventType.LOCAL_DESTINATIONS_SET);
   localDestsSetEvent.destinationInfos = this.localDestinationInfos_;
   this.nativeLayer_.dispatchEvent(localDestsSetEvent);
-
-  checkSectionVisible($('layout-settings'), true);
-  checkSectionVisible($('color-settings'), true);
-  checkSectionVisible($('copies-settings'), true);
 
   var capsSetEvent =
       new cr.Event(print_preview.NativeLayer.EventType.CAPABILITIES_SET);
