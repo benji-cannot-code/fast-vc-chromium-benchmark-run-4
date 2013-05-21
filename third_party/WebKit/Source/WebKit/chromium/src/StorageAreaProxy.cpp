a3dfc7ca4d1b96f3489966ad4c3fdb0e7d15a45b
kaznacheev@chromium.org <kaznacheev@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 15:30:37 +0000

    Assimilated InspectorInstrumentation::didDispatchDOMStorageEvent implementation
    BUG=232160
    TBR=pfeldman
    
    Review URL: https://chromiumcodereview.appspot.com/15566002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/StorageAreaProxy.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/StorageAreaProxy.cpp
@@ -162,7 +162,7 @@ void StorageAreaProxy::dispatchLocalStorageEvent(const String& key, const String
             if (storage && frame->document()->securityOrigin()->equal(securityOrigin) && !isEventSource(storage, sourceAreaInstance))
                 frame->document()->enqueueWindowEvent(StorageEvent::create(eventNames().storageEvent, key, oldValue, newValue, pageURL, storage));
         }
-        InspectorInstrumentation::didDispatchDOMStorageEvent(key, oldValue, newValue, LocalStorage, securityOrigin, *it);
+        InspectorInstrumentation::didDispatchDOMStorageEvent(*it, key, oldValue, newValue, LocalStorage, securityOrigin);
     }
 }
 
@@ -192,7 +192,7 @@ void StorageAreaProxy::dispatchSessionStorageEvent(const String& key, const Stri
         if (storage && frame->document()->securityOrigin()->equal(securityOrigin) && !isEventSource(storage, sourceAreaInstance))
             frame->document()->enqueueWindowEvent(StorageEvent::create(eventNames().storageEvent, key, oldValue, newValue, pageURL, storage));
     }
-    InspectorInstrumentation::didDispatchDOMStorageEvent(key, oldValue, newValue, SessionStorage, securityOrigin, page);
+    InspectorInstrumentation::didDispatchDOMStorageEvent(page, key, oldValue, newValue, SessionStorage, securityOrigin);
 }
 
 bool StorageAreaProxy::isEventSource(Storage* storage, WebKit::WebStorageArea* sourceAreaInstance)
