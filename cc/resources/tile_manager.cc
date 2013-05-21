0c669b70fb6b32e81735dd1f3e4c39bd6da6af13
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 10:24:31 +0000

    Remove cc counters that aren't getting a lot of use
    These counters are burning a lot of valuable vertical real estate, but
    after talking to people, they're never being read. I propose we just kill them.
    
    R=enne
    
    Review URL: https://chromiumcodereview.appspot.com/14699006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201282 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/tile_manager.cc
+++ cc/resources/tile_manager.cc
@@ -287,13 +287,10 @@ void TileManager::AssignBinsToTiles() {
     for (int i = 0; i < NUM_BIN_PRIORITIES; ++i)
       mts.bin[i] = bin_map[mts.bin[i]];
   }
-
-  TRACE_COUNTER_ID1("cc", "TileCount", this, tiles_.size());
 }
 
 void TileManager::SortTiles() {
   TRACE_EVENT0("cc", "TileManager::SortTiles");
-  TRACE_COUNTER_ID1("cc", "TileCount", this, tiles_.size());
 
   // Sort by bin, resolution and time until needed.
   std::sort(tiles_.begin(), tiles_.end(), BinComparator());
@@ -301,7 +298,6 @@ void TileManager::SortTiles() {
 
 void TileManager::ManageTiles() {
   TRACE_EVENT0("cc", "TileManager::ManageTiles");
-  TRACE_COUNTER_ID1("cc", "TileCount", this, tiles_.size());
 
   manage_tiles_pending_ = false;
   ++manage_tiles_call_count_;
