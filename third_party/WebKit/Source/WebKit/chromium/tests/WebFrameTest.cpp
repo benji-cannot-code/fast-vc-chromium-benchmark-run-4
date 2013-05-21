9a953f73236045917a19b39f49e83513297dceb9
aelias@chromium.org <aelias@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 05:59:39 +0000

    Refactor viewport initialization logic out of WebViewImpl.
    This centralizes and brings some structure to viewport initialization,
    expressing it as a cascade of constraints expressed as
    ViewportAttributes objects.  When any of the constraints changes, a
    dirty bit is set, and the page scale factor and fixed-layout size is
    recalculated on the next layout.
    
    This refactor mostly maintains existing behavior, but also includes two bugfixes
    (with new tests for them):
    1. Page scale is now reset to minimum when the content width increases,
    even if it was reset in the past.
    2. setIgnoreViewportTagMaximumScale now also ignores the minimum (I'll
    need to rename this WebKit API).
    
    NOTRY=true
    BUG=176998,161695,165688
    
    Review URL: https://chromiumcodereview.appspot.com/14813025
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150743 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -358,6 +358,9 @@ TEST_F(WebFrameTest, DeviceScaleFactorUsesDefaultWithoutViewportTag)
 
 TEST_F(WebFrameTest, FixedLayoutInitializeAtMinimumPageScale)
 {
+    WebCore::Settings::setMockScrollbarsEnabled(true);
+    WebCore::Settings::setUsesOverlayScrollbars(true);
+
     registerMockedHttpURLLoad("fixed_layout.html");
 
     FixedLayoutTestWebViewClient client;
@@ -375,6 +378,48 @@ TEST_F(WebFrameTest, FixedLayoutInitializeAtMinimumPageScale)
     int defaultFixedLayoutWidth = 980;
     float minimumPageScaleFactor = viewportWidth / (float) defaultFixedLayoutWidth;
     EXPECT_EQ(minimumPageScaleFactor, m_webView->pageScaleFactor());
+    EXPECT_EQ(minimumPageScaleFactor, m_webView->minimumPageScaleFactor());
+
+    // Assume the user has pinch zoomed to page scale factor 2.
+    float userPinchPageScaleFactor = 2;
+    m_webView->setPageScaleFactorPreservingScrollOffset(userPinchPageScaleFactor);
+    m_webView->layout();
+
+    // Make sure we don't reset to initial scale if the page continues to load.
+    bool isNewNavigation;
+    WebViewImpl* webViewImpl = static_cast<WebViewImpl*>(m_webView);
+    webViewImpl ->didCommitLoad(&isNewNavigation, false);
+    webViewImpl ->didChangeContentsSize();
+    EXPECT_EQ(userPinchPageScaleFactor, m_webView->pageScaleFactor());
+
+    // Make sure we don't reset to initial scale if the viewport size changes.
+    m_webView->resize(WebSize(viewportWidth, viewportHeight + 100));
+    EXPECT_EQ(userPinchPageScaleFactor, m_webView->pageScaleFactor());
+}
+
+TEST_F(WebFrameTest, WideDocumentInitializeAtMinimumPageScale)
+{
+    WebCore::Settings::setMockScrollbarsEnabled(true);
+    WebCore::Settings::setUsesOverlayScrollbars(true);
+
+    registerMockedHttpURLLoad("wide_document.html");
+
+    FixedLayoutTestWebViewClient client;
+    client.m_screenInfo.deviceScaleFactor = 1;
+    int viewportWidth = 640;
+    int viewportHeight = 480;
+
+    // Make sure we initialize to minimum scale, even if the window size
+    // only becomes available after the load begins.
+    m_webView = FrameTestHelpers::createWebViewAndLoad(m_baseURL + "wide_document.html", true, 0, &client);
+    m_webView->enableFixedLayoutMode(true);
+    m_webView->settings()->setViewportEnabled(true);
+    m_webView->resize(WebSize(viewportWidth, viewportHeight));
+
+    int wideDocumentWidth = 1500;
+    float minimumPageScaleFactor = viewportWidth / (float) wideDocumentWidth;
+    EXPECT_EQ(minimumPageScaleFactor, m_webView->pageScaleFactor());
+    EXPECT_EQ(minimumPageScaleFactor, m_webView->minimumPageScaleFactor());
 
     // Assume the user has pinch zoomed to page scale factor 2.
     float userPinchPageScaleFactor = 2;
@@ -406,6 +451,7 @@ TEST_F(WebFrameTest, setInitializeAtMinimumPageScaleToFalse)
     m_webView->enableFixedLayoutMode(true);
     m_webView->settings()->setViewportEnabled(true);
     m_webView->settings()->setInitializeAtMinimumPageScale(false);
+    m_webView->settings()->setSupportDeprecatedTargetDensityDPI(true);
     m_webView->resize(WebSize(viewportWidth, viewportHeight));
 
     // The page must be displayed at 100% zoom.
@@ -426,6 +472,7 @@ TEST_F(WebFrameTest, SetInitializeAtMinimumPageScaleToFalseAndNoWideViewport)
     m_webView->settings()->setViewportEnabled(true);
     m_webView->settings()->setInitializeAtMinimumPageScale(false);
     m_webView->settings()->setUseWideViewport(false);
+    m_webView->settings()->setSupportDeprecatedTargetDensityDPI(true);
     m_webView->resize(WebSize(viewportWidth, viewportHeight));
 
     // The page must be displayed at 100% zoom, despite that it hosts a wide div element.
@@ -517,35 +564,35 @@ TEST_F(WebFrameTest, PageViewportInitialScaleOverridesInitializeAtMinimumScale)
 
 TEST_F(WebFrameTest, setInitialPageScaleFactorPermanently)
 {
+    WebCore::Settings::setMockScrollbarsEnabled(true);
+    WebCore::Settings::setUsesOverlayScrollbars(true);
+
     registerMockedHttpURLLoad("fixed_layout.html");
 
     FixedLayoutTestWebViewClient client;
     client.m_screenInfo.deviceScaleFactor = 1;
-    float enforcedPageScalePactor = 2.0f;
+    float enforcedPageScaleFactor = 2.0f;
 
     m_webView = FrameTestHelpers::createWebViewAndLoad(m_baseURL + "fixed_layout.html", true, 0, &client);
-    m_webView->setInitialPageScaleOverride(enforcedPageScalePactor);
+    m_webView->settings()->setSupportDeprecatedTargetDensityDPI(true);
+    m_webView->settings()->setInitializeAtMinimumPageScale(false);
+    m_webView->setInitialPageScaleOverride(enforcedPageScaleFactor);
+    m_webView->enableFixedLayoutMode(true);
+    m_webView->settings()->setViewportEnabled(true);
+    m_webView->layout();
 
-    EXPECT_EQ(enforcedPageScalePactor, m_webView->pageScaleFactor());
+    EXPECT_EQ(enforcedPageScaleFactor, m_webView->pageScaleFactor());
 
     int viewportWidth = 640;
     int viewportHeight = 480;
-    m_webView->enableFixedLayoutMode(true);
-    m_webView->settings()->setViewportEnabled(true);
     m_webView->resize(WebSize(viewportWidth, viewportHeight));
     m_webView->layout();
 
-    EXPECT_EQ(enforcedPageScalePactor, m_webView->pageScaleFactor());
-
-    m_webView->enableFixedLayoutMode(false);
-    m_webView->settings()->setViewportEnabled(false);
-    m_webView->layout();
-
-    EXPECT_EQ(enforcedPageScalePactor, m_webView->pageScaleFactor());
+    EXPECT_EQ(enforcedPageScaleFactor, m_webView->pageScaleFactor());
 
     m_webView->setInitialPageScaleOverride(-1);
     m_webView->layout();
-    EXPECT_EQ(1.0f, m_webView->pageScaleFactor());
+    EXPECT_EQ(1.0, m_webView->pageScaleFactor());
 }
 
 TEST_F(WebFrameTest, PermanentInitialPageScaleFactorOverridesInitializeAtMinimumScale)
@@ -751,6 +798,8 @@ TEST_F(WebFrameTest, pageScaleFactorDoesNotApplyCssTransform)
 
 TEST_F(WebFrameTest, targetDensityDpiHigh)
 {
+    WebCore::Settings::setMockScrollbarsEnabled(true);
+    WebCore::Settings::setUsesOverlayScrollbars(true);
     registerMockedHttpURLLoad("viewport-target-densitydpi-high.html");
 
     FixedLayoutTestWebViewClient client;
@@ -772,6 +821,8 @@ TEST_F(WebFrameTest, targetDensityDpiHigh)
 
 TEST_F(WebFrameTest, targetDensityDpiDevice)
 {
+    WebCore::Settings::setMockScrollbarsEnabled(true);
+    WebCore::Settings::setUsesOverlayScrollbars(true);
     registerMockedHttpURLLoad("viewport-target-densitydpi-device.html");
 
     float deviceScaleFactors[] = { 1.0f, 4.0f / 3.0f, 2.0f };
@@ -976,8 +1027,11 @@ TEST_F(WebFrameTest, pageScaleFactorUpdatesScrollbars)
     EXPECT_EQ(view->scrollSize(WebCore::VerticalScrollbar), view->contentsSize().height() - view->visibleContentRect().height());
 }
 
-TEST_F(WebFrameTest, CanOverrideMaximumScaleFactor)
+TEST_F(WebFrameTest, CanOverrideScaleLimits)
 {
+    WebCore::Settings::setMockScrollbarsEnabled(true);
+    WebCore::Settings::setUsesOverlayScrollbars(true);
+
     registerMockedHttpURLLoad("no_scale_for_you.html");
 
     FixedLayoutTestWebViewClient client;
@@ -990,12 +1044,20 @@ TEST_F(WebFrameTest, CanOverrideMaximumScaleFactor)
     m_webView->settings()->setViewportEnabled(true);
     m_webView->resize(WebSize(viewportWidth, viewportHeight));
 
-    EXPECT_EQ(1.0f, m_webView->maximumPageScaleFactor());
+    EXPECT_EQ(2.0f, m_webView->minimumPageScaleFactor());
+    EXPECT_EQ(2.0f, m_webView->maximumPageScaleFactor());
 
-    m_webView->setIgnoreViewportTagMaximumScale(true);
+    m_webView->setIgnoreViewportTagScaleLimits(true);
     m_webView->layout();
 
-    EXPECT_EQ(4.0f, m_webView->maximumPageScaleFactor());
+    EXPECT_EQ(1.0f, m_webView->minimumPageScaleFactor());
+    EXPECT_EQ(5.0f, m_webView->maximumPageScaleFactor());
+
+    m_webView->setIgnoreViewportTagScaleLimits(false);
+    m_webView->layout();
+
+    EXPECT_EQ(2.0f, m_webView->minimumPageScaleFactor());
+    EXPECT_EQ(2.0f, m_webView->maximumPageScaleFactor());
 }
 
 TEST_F(WebFrameTest, updateOverlayScrollbarLayers)
