d7616ff6b4fe7f0a38c3e385c15ae95d3d8f0493
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 17:44:51 +0000

    [NaCl SDK] Remove build_utils.py
    BUG=none
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/15392002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201329 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/build_app.py
+++ native_client_sdk/src/build_tools/build_app.py
@@ -15,7 +15,7 @@ if sys.version_info < (2, 6, 0):
 
 import buildbot_common
 import build_projects
-import build_utils
+import build_version
 import easy_template
 import parse_dsc
 
@@ -77,7 +77,7 @@ def main(args):
 
   toolchains = ['newlib', 'glibc']
 
-  pepper_ver = str(int(build_utils.ChromeMajorVersion()))
+  pepper_ver = str(int(build_version.ChromeMajorVersion()))
   pepperdir = os.path.join(OUT_DIR, 'pepper_' + pepper_ver)
   app_dir = os.path.join(OUT_DIR, 'naclsdk_app')
   app_examples_dir = os.path.join(app_dir, 'examples')
@@ -115,7 +115,7 @@ def main(args):
       'pointerLock',
       'unlimitedStorage',
     ],
-    'version': build_utils.ChromeVersionNoTrunk()
+    'version': build_version.ChromeVersionNoTrunk()
   }
   easy_template.RunTemplateFile(
       os.path.join(sdk_resources_dir, 'manifest.json.template'),
