5a4ae63ef4dcd1ef318a5bb2312efcf69d3aed07
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 08:47:38 +0000

    sync: deferred initialization support for autofill profiles
    (based off of https://codereview.chromium.org/15408004/, which I'll land before this CL is committed.)
    
    BUG=80194
    
    Review URL: https://chromiumcodereview.appspot.com/15410002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201268 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/web_data_service_factory.cc
+++ chrome/browser/webdata/web_data_service_factory.cc
@@ -42,7 +42,7 @@ void ProfileErrorCallback(sql::InitStatus status) {
 
 void InitSyncableServicesOnDBThread(
     scoped_refptr<AutofillWebDataService> autofill_web_data,
-    const syncer::SyncableService::StartSyncFlare& flare,
+    const base::FilePath& profile_path,
     const std::string& app_locale) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::DB));
 
@@ -50,9 +50,13 @@ void InitSyncableServicesOnDBThread(
   // all the database data should migrate to this API over time.
   AutocompleteSyncableService::CreateForWebDataService(autofill_web_data);
   AutocompleteSyncableService::FromWebDataService(
-      autofill_web_data)->InjectStartSyncFlare(flare);
+      autofill_web_data)->InjectStartSyncFlare(
+          sync_start_util::GetFlareForSyncableService(profile_path));
   AutofillProfileSyncableService::CreateForWebDataService(
       autofill_web_data, app_locale);
+  AutofillProfileSyncableService::FromWebDataService(
+      autofill_web_data)->InjectStartSyncFlare(
+          sync_start_util::GetFlareForSyncableService(profile_path));
 }
 
 }  // namespace
@@ -100,7 +104,7 @@ WebDataServiceWrapper::WebDataServiceWrapper(Profile* profile) {
       BrowserThread::DB, FROM_HERE,
       base::Bind(&InitSyncableServicesOnDBThread,
                  autofill_web_data_,
-                 sync_start_util::GetFlareForSyncableService(profile_path),
+                 profile_path,
                  g_browser_process->GetApplicationLocale()));
 }
 
