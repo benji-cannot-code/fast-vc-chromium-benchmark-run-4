cc1716355258b740fc3844051ca20be762eb88c4
rouslan@chromium.org <rouslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 07:24:04 +0000

    Add ability to send feedback for spelling service
    This CL adds the ability to send user feedback for spelling suggestions from
    spelling service. This feedback will be used to improve the spelling
    suggestions. The feedback will be collected in a follow-up CL. This CL does not
    actually send out feedback, because no feedback is collected yet.
    
    BUG=170514
    
    Review URL: https://chromiumcodereview.appspot.com/14721010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201246 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/spellchecker/spellcheck_service.h
+++ chrome/browser/spellchecker/spellcheck_service.h
@@ -11,9 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/prefs/pref_change_registrar.h"
+#include "chrome/browser/spellchecker/feedback_sender.h"
 #include "chrome/browser/spellchecker/spellcheck_custom_dictionary.h"
 #include "chrome/browser/spellchecker/spellcheck_hunspell_dictionary.h"
-#include "chrome/browser/spellchecker/spelling_service_feedback.h"
 #include "chrome/common/spellcheck_common.h"
 #include "components/browser_context_keyed_service/browser_context_keyed_service.h"
 #include "content/public/browser/notification_observer.h"
@@ -95,7 +95,7 @@ class SpellcheckService : public ProfileKeyedService,
   SpellcheckHunspellDictionary* GetHunspellDictionary();
 
   // Returns the instance of the spelling service feedback sender.
-  SpellingServiceFeedback* GetFeedbackSender();
+  spellcheck::FeedbackSender* GetFeedbackSender();
 
   // Load a dictionary from a given path. Format specifies how the dictionary
   // is stored. Return value is true if successful.
@@ -159,7 +159,7 @@ class SpellcheckService : public ProfileKeyedService,
 
   scoped_ptr<SpellcheckHunspellDictionary> hunspell_dictionary_;
 
-  SpellingServiceFeedback feedback_sender_;
+  scoped_ptr<spellcheck::FeedbackSender> feedback_sender_;
 
   base::WeakPtrFactory<SpellcheckService> weak_ptr_factory_;
 
