5215bd41872dcf2317d0d8cd2809c91ba9cb0f2c
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 00:57:00 +0000

    recalcStyle should always start at the Document
    After r150713 all recalcStyle calls start at the Document level. We
    should add an ASSERT to make sure it stays this way.
    
    Review URL: https://chromiumcodereview.appspot.com/15425003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150725 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1400,6 +1400,8 @@ PassRefPtr<RenderStyle> Element::styleForRenderer()
 
 void Element::recalcStyle(StyleChange change)
 {
+    ASSERT(document()->inStyleRecalc());
+
     if (hasCustomStyleCallbacks())
         willRecalcStyle(change);
 
