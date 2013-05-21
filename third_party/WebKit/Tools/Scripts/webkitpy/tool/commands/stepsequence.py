b99ae054f1fe96b68883b092d71bcca344712d26
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 00:19:20 +0000

    Remove a bunch of dead code from webkitpy
    Review URL: https://chromiumcodereview.appspot.com/15416008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150721 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/stepsequence.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/tool/commands/stepsequence.py
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (C) 2009 Google Inc. All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are
 # met:
-# 
+#
 #     * Redistributions of source code must retain the above copyright
 # notice, this list of conditions and the following disclaimer.
 #     * Redistributions in binary form must reproduce the above
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #     * Neither the name of Google Inc. nor the names of its
 # contributors may be used to endorse or promote products derived from
 # this software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 # "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 # LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -28,12 +28,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import logging
+import sys
 
 from webkitpy.tool import steps
 
 from webkitpy.common.checkout.scm import CheckoutNeedsUpdate
 from webkitpy.common.system.executive import ScriptError
-from webkitpy.tool.bot.queueengine import QueueEngine
 
 _log = logging.getLogger(__name__)
 
@@ -67,6 +67,14 @@ class StepSequence(object):
         for step in self._steps:
             step(tool, options).run(state)
 
+    # Child processes exit with a special code to the parent queue process can detect the error was handled.
+    handled_error_code = 2
+
+    @classmethod
+    def exit_after_handled_error(cls, error):
+        _log.error(error)
+        sys.exit(cls.handled_error_code)
+
     def run_and_handle_errors(self, tool, options, state=None):
         if not state:
             state = {}
@@ -77,11 +85,11 @@ class StepSequence(object):
             if options.parent_command:
                 command = tool.command_by_name(options.parent_command)
                 command.handle_checkout_needs_update(tool, state, options, e)
-            QueueEngine.exit_after_handled_error(e)
+            self.exit_after_handled_error(e)
         except ScriptError, e:
             if not options.quiet:
                 _log.error(e.message_with_output())
             if options.parent_command:
                 command = tool.command_by_name(options.parent_command)
                 command.handle_script_error(tool, state, e)
-            QueueEngine.exit_after_handled_error(e)
+            self.exit_after_handled_error(e)
