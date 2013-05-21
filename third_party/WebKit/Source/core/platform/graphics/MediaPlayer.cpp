18aa127310e325f370e1843ca7003ca3908cee3c
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-21 01:00:26 +0000

    Remove dead MediaPlayerClient methods and their implementations
    Many methods of MediaPlayerClient were never called in Blink. This patch
    removes those methods, their (never-invoked) callers in MediaPlayer (if
    present), and their implementations in HTMLMediaElement (if present).
    
    To help keep HTMLMediaElement up-to-date in future, made all
    MediaPlayerClient's methods pure virtual and marked the overrides in
    HTMLMediaElement with OVERRIDE.
    
    R=acolwell, eseidel@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15503002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150727 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/MediaPlayer.cpp
+++ third_party/WebKit/Source/core/platform/graphics/MediaPlayer.cpp
@@ -735,18 +735,6 @@ void MediaPlayer::playbackStateChanged()
         m_mediaPlayerClient->mediaPlayerPlaybackStateChanged(this);
 }
 
-void MediaPlayer::firstVideoFrameAvailable()
-{
-    if (m_mediaPlayerClient)
-        m_mediaPlayerClient->mediaPlayerFirstVideoFrameAvailable(this);
-}
-
-void MediaPlayer::characteristicChanged()
-{
-    if (m_mediaPlayerClient)
-        m_mediaPlayerClient->mediaPlayerCharacteristicChanged(this);
-}
-
 #if ENABLE(WEB_AUDIO)
 AudioSourceProvider* MediaPlayer::audioSourceProvider()
 {
@@ -787,30 +775,6 @@ bool MediaPlayer::keyNeeded(Uint8Array* initData)
     return false;
 }
 #endif
-
-String MediaPlayer::referrer() const
-{
-    if (!m_mediaPlayerClient)
-        return String();
-
-    return m_mediaPlayerClient->mediaPlayerReferrer();
-}
-
-String MediaPlayer::userAgent() const
-{
-    if (!m_mediaPlayerClient)
-        return String();
-    
-    return m_mediaPlayerClient->mediaPlayerUserAgent();
-}
-
-CachedResourceLoader* MediaPlayer::cachedResourceLoader()
-{
-    if (!m_mediaPlayerClient)
-        return 0;
-
-    return m_mediaPlayerClient->mediaPlayerCachedResourceLoader();
-}
 
 void MediaPlayer::addTextTrack(PassRefPtr<InbandTextTrackPrivate> track)
 {
