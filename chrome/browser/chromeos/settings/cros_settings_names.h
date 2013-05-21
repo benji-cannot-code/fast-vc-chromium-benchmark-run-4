f5b6905cee7577cc1227243e9af5d6bc3bb07a49
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 18:34:30 +0000

    Remove migration code from device proxy settings to per network settings.
    Also
    - removed the then unused Pref kSettingProxyEverywhere.
    - removed some unused debugging code from ProxyConfigServiceImpl.
    
    This shouldn't affect the device proxy policy, as it is still converted to the policy kProxySettings.
    
    BUG=168140
    TEST=Ensure that device proxy policy still works for retail mode v1.
    
    Review URL: https://chromiumcodereview.appspot.com/15269004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201330 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings/cros_settings_names.h
+++ chrome/browser/chromeos/settings/cros_settings_names.h
@@ -33,8 +33,6 @@ enum DeviceLocalAccountType {
   DEVICE_LOCAL_ACCOUNT_TYPE_KIOSK_APP,
 };
 
-extern const char kSettingProxyEverywhere[];
-
 extern const char kSignedDataRoamingEnabled[];
 
 extern const char kSystemTimezonePolicy[];
