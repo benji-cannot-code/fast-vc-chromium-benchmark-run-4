a4ad0f934bb55b61d5080f2b8b5e0265c0ad5037
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 22:51:19 +0000

    Adding Online Wallet shipping address manage link.
    R=isherman@chromium.org
    TEST=unit_tests, manual
    BUG=238990
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15141008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201379 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/wallet/wallet_service_url.h
+++ components/autofill/browser/wallet/wallet_service_url.h
@@ -13,7 +13,8 @@ namespace wallet {
 
 GURL GetGetWalletItemsUrl();
 GURL GetGetFullWalletUrl();
-GURL GetManageItemsUrl();
+GURL GetManageInstrumentsUrl();
+GURL GetManageAddressesUrl();
 GURL GetAcceptLegalDocumentsUrl();
 GURL GetAuthenticateInstrumentUrl();
 GURL GetEncryptionUrl();
