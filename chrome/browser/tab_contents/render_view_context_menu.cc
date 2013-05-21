3898f661e121739f38d903fcc6f844312215dc6b
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-21 10:01:14 +0000

    Translate: use IsTranslatableURL to check if it should be in the context menu
    Replace ShouldShowTranslateItem with TranslateManager::IsTranslatableURL.
    It should be the same, but these two are slightly different due to lack
    of maintenance.
    
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/15257003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/tab_contents/render_view_context_menu.cc
+++ chrome/browser/tab_contents/render_view_context_menu.cc
@@ -94,10 +94,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/favicon_size.h"
 #include "webkit/glue/webmenuitem.h"
 
-#ifdef FILE_MANAGER_EXTENSION
-#include "chrome/browser/chromeos/extensions/file_manager/file_manager_util.h"
-#endif
-
 using WebKit::WebContextMenuData;
 using WebKit::WebMediaPlayerAction;
 using WebKit::WebPluginAction;
@@ -349,19 +345,6 @@ void AddCustomItemsToMenu(const std::vector<WebMenuItem>& items,
         break;
     }
   }
-}
-
-bool ShouldShowTranslateItem(const GURL& page_url) {
-  if (page_url.SchemeIs("chrome"))
-    return false;
-
-#ifdef FILE_MANAGER_EXTENSION
-  if (page_url.SchemeIs("chrome-extension") &&
-      page_url.DomainIs(kFileBrowserDomain))
-    return false;
-#endif
-
-  return true;
 }
 
 void DevToolsInspectElementAt(RenderViewHost* rvh, int x, int y) {
@@ -923,7 +906,7 @@ void RenderViewContextMenu::AppendPageItems() {
                                   IDS_CONTENT_CONTEXT_SAVEPAGEAS);
   menu_model_.AddItemWithStringId(IDC_PRINT, IDS_CONTENT_CONTEXT_PRINT);
 
-  if (ShouldShowTranslateItem(params_.page_url)) {
+  if (TranslateManager::IsTranslatableURL(params_.page_url)) {
     std::string locale = g_browser_process->GetApplicationLocale();
     locale = TranslateManager::GetLanguageCode(locale);
     string16 language = l10n_util::GetDisplayNameForLocale(locale, locale,
@@ -1224,7 +1207,6 @@ bool RenderViewContextMenu::IsCommandIdEnabled(int id) const {
                  TranslateManager::IsSupportedLanguage(original_lang)) &&
              !translate_tab_helper->language_state().IsPageTranslated() &&
              !source_web_contents_->GetInterstitialPage() &&
-             TranslateManager::IsTranslatableURL(params_.page_url) &&
              // There are some application locales which can't be used as a
              // target language for translation.
              TranslateManager::IsSupportedLanguage(target_lang);
