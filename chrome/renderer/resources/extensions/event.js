d6f2d64654f5471c641a9499ab943717a5753d09
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 15:58:12 +0000

    Remove chromeHidden.dispatchOnError, it's not used.
    R=mpcomplete@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15377003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201542 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/event.js
+++ chrome/renderer/resources/extensions/event.js
@@ -467,8 +467,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     }
   };
 
-  chromeHidden.dispatchError = function(msg) {
-    console.error(msg);
-  };
-
   chrome.Event = Event;
