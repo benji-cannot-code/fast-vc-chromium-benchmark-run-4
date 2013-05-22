ca9bf7520aeb8602f987cdf329345ebc33671456
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 07:49:06 +0000

    [Android] De-flake insertion handle tests
    First, inline the test pages used by these tests and open them as
    data:text/html instead of file:.
    
    Second, extract TestInputMethodManagerWrapper from ImeTest and use this
    to avoid flakiness caused by the default keyboard.
    
    TBR=yfriedman@chromium.org
    BUG=169648,155548
    
    Review URL: https://chromiumcodereview.appspot.com/14900016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201464 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
+++ content/public/android/java/src/org/chromium/content/browser/input/InputMethodManagerWrapper.java
@@ -14,7 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 /**
  * Wrapper around Android's InputMethodManager
  */
-class InputMethodManagerWrapper {
+public class InputMethodManagerWrapper {
     private final Context mContext;
 
     public InputMethodManagerWrapper(Context context) {
