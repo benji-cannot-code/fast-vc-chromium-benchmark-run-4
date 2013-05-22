07a736b3bf443a9ab564c68540455eb896dba248
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 18:31:07 +0000

    Log details when the Oauth2TokenFetcher fails.
    Also cleaned up some unused constants.
    
    BUG=242623
    
    Review URL: https://chromiumcodereview.appspot.com/15708003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/online_attempt.cc
+++ chrome/browser/chromeos/login/online_attempt.cc
@@ -28,15 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 using content::BrowserThread;
 
-namespace {
-
-// The service scope of the OAuth v2 token that ChromeOS login will be
-// requesting.
-const char kServiceScopeChromeOS[] =
-    "https://www.googleapis.com/auth/chromesync";
-
-}
-
 namespace chromeos {
 
 // static
