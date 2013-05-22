f71d0269438bfe4e8ee28cfbd32a863d52c76d7f
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 00:19:03 +0000

    autofill constants to both sides for split_link
    autofill_constants.cc is just constants, shouldn't cause any problem to
    duplicate these.
    
    NOTRY=true
    R=cpu@chromium.org
    BUG=237249
    
    Review URL: https://chromiumcodereview.appspot.com/15579003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201401 0039d316-1c4b-4281-b951-d872f2087c98

--- build/split_link_partition.py
+++ build/split_link_partition.py
@@ -126,6 +126,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   # .lib files.
   'all_from_libs': [
     (r'autofill_common\.lib$', r'switches\.obj$'),
+    (r'autofill_common\.lib$', r'autofill_constants\.obj$'),
     (r'\bbase\.lib$', r'string_util_constants\.obj$'),
     (r'\bbase_i18n\.lib$', r'i18n_constants\.obj$'),
     (r'base_static\.lib$', r'base_switches\.obj$'),
