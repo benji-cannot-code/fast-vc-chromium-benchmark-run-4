6c6287316eeab016c511d5ea2e7a73d26a777119
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 21:06:08 +0000

    [content shell] disable threaded animations during layout tests
    Otherwise, reading back from the compositor might schedule a composite
    which in turn damages the view again, and we fail to produce pixel
    results
    
    BUG=111316
    TEST=transitions/default-timing-function.html doesn't trigger an assertion on linux
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15752003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201609 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/app/shell_main_delegate.cc
+++ content/shell/app/shell_main_delegate.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/path_service.h"
+#include "cc/base/switches.h"
 #include "content/public/browser/browser_main_runner.h"
 #include "content/public/common/content_switches.h"
 #include "content/public/common/url_constants.h"
@@ -117,6 +118,7 @@ bool ShellMainDelegate::BasicStartupComplete(int* exit_code) {
     command_line.AppendSwitch(switches::kEnableCssShaders);
     command_line.AppendSwitchASCII(switches::kTouchEvents,
                                    switches::kTouchEventsEnabled);
+    command_line.AppendSwitch(cc::switches::kDisableThreadedAnimation);
     if (command_line.HasSwitch(switches::kEnableSoftwareCompositing))
       command_line.AppendSwitch(switches::kEnableSoftwareCompositingGLAdapter);
 
