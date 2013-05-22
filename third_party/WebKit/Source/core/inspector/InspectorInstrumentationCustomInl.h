90e84f90ae2e44fcc8783f54aec2aae35156d9bd
kaznacheev@chromium.org <kaznacheev@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-22 16:58:00 +0000

    Assimilated InspectorInstrumentation::willEvaluateWorkerScript implementation
    BUG=232160
    
    Review URL: https://chromiumcodereview.appspot.com/15562005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorInstrumentationCustomInl.h
+++ third_party/WebKit/Source/core/inspector/InspectorInstrumentationCustomInl.h
@@ -48,7 +48,6 @@ bool collectingHTMLParseErrorsImpl(InstrumentingAgents*);
 bool canvasAgentEnabled(ScriptExecutionContext*);
 bool consoleAgentEnabled(ScriptExecutionContext*);
 bool timelineAgentEnabled(ScriptExecutionContext*);
-void willEvaluateWorkerScript(WorkerContext*, int workerThreadStartMode);
 
 inline bool profilerEnabled(Page* page)
 {
--- third_party/WebKit/Source/core/inspector/WorkerRuntimeAgent.h
+++ third_party/WebKit/Source/core/inspector/WorkerRuntimeAgent.h
@@ -50,7 +50,7 @@ public:
     // Protocol commands.
     virtual void run(ErrorString*);
 
-    void pauseWorkerContext(WorkerContext*);
+    void willEvaluateWorkerScript(WorkerContext*, int workerThreadStartMode);
 
 private:
     WorkerRuntimeAgent(InstrumentingAgents*, InspectorCompositeState*, InjectedScriptManager*, WorkerContext*);
