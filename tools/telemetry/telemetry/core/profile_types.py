d1e5a8d39390005eb0aa514cd0cf3656e83f2985
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 15:26:59 +0000

    [Telemetry] Move discover.py to core/ and add unit test
    discover.py is used in several places in telemetry unrelated to the unit tests, move it to core/.
    
    This CL also adds a telemetry.test.test_data package.  The motivation for this is that the profile creators code also wants to test the import of classes so it's useful to have a package that we can pass to discover.
    
    BUG=None
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15484009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201527 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/profile_types.py
+++ tools/telemetry/telemetry/core/profile_types.py
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import os
 
+from telemetry.core import discover
 from telemetry.core import profile_creator
-from telemetry.test import discover
 
 BASE_PROFILE_TYPES = ['clean', 'default']
 
--- tools/telemetry/telemetry/page/actions/all_page_actions.py
+++ tools/telemetry/telemetry/page/actions/all_page_actions.py
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 import os
 
+from telemetry.core import discover
 from telemetry.page.actions import page_action
-from telemetry.test import discover
 
 _page_action_classes = discover.DiscoverClasses(
     os.path.dirname(__file__),
