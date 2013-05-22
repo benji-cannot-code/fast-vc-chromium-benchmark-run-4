2af8abf342837759207751805a6fa7cc9ed4fafa
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 09:00:44 +0000

    Added a validator for JSON v3 schemas.
    This function checks if the input string is a valid JSON v3 schema, with some
    additional restrictions (e.g. "pattern" is not supported). The DictionaryValue
    returned can be used to build a JSONSchemaValidator, if the check succeeds.
    
    BUG=108992
    
    Review URL: https://chromiumcodereview.appspot.com/14830007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201473 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/json_schema/json_schema_validator.h
+++ chrome/common/json_schema/json_schema_validator.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/basictypes.h"
+#include "base/memory/scoped_ptr.h"
 
 namespace base {
 class DictionaryValue;
@@ -98,6 +99,15 @@ class JSONSchemaValidator {
   static std::string FormatErrorMessage(const std::string& format,
                                         const std::string& s1,
                                         const std::string& s2);
+
+  // Verifies if |schema| is a valid JSON v3 schema. When this validation passes
+  // then |schema| is valid JSON that can be parsed into a DictionaryValue,
+  // and that DictionaryValue can be used to build a JSONSchemaValidator.
+  // Returns the parsed DictionaryValue when |schema| validated, otherwise
+  // returns NULL. In that case, |error| contains an error description.
+  static scoped_ptr<base::DictionaryValue> IsValidSchema(
+      const std::string& schema,
+      std::string* error);
 
   // Creates a validator for the specified schema.
   //
