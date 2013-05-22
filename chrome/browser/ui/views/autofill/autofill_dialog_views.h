19102ed64d88ea1293feb54884da52adced038ee
abodenha@chromium.org <abodenha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 06:53:28 +0000

    Prevent backspace from causing the rAc dialog to close.
    Add a no-op accelerator for backspace to prevent the standard one from firing
    and causing the page to navigate (which closes our dialog).
    
    BUG=226735
    
    Review URL: https://chromiumcodereview.appspot.com/15500004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201452 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/autofill_dialog_views.h
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/autofill/autofill_dialog_controller.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_view.h"
 #include "chrome/browser/ui/autofill/testable_autofill_dialog_view.h"
+#include "ui/base/accelerators/accelerator.h"
 #include "ui/views/controls/button/button.h"
 #include "ui/views/controls/button/menu_button_listener.h"
 #include "ui/views/controls/combobox/combobox_listener.h"
@@ -69,7 +70,8 @@ class AutofillDialogViews : public AutofillDialogView,
                             public views::FocusChangeListener,
                             public views::LinkListener,
                             public views::ComboboxListener,
-                            public views::StyledLabelListener {
+                            public views::StyledLabelListener,
+                            public ui::AcceleratorTarget {
  public:
   explicit AutofillDialogViews(AutofillDialogController* controller);
   virtual ~AutofillDialogViews();
@@ -102,6 +104,10 @@ class AutofillDialogViews : public AutofillDialogView,
                                       const string16& contents) OVERRIDE;
   virtual void ActivateInput(const DetailInput& input) OVERRIDE;
   virtual void OnSignInResize(const gfx::Size& pref_size) OVERRIDE;
+
+  // ui::AcceleratorTarget implementation:
+  virtual bool AcceleratorPressed(const ui::Accelerator& accelerator) OVERRIDE;
+  virtual bool CanHandleAccelerators() const OVERRIDE;
 
   // views::DialogDelegate implementation:
   virtual string16 GetWindowTitle() const OVERRIDE;
