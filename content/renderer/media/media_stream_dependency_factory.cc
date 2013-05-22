a5ab45109941a5374e379da7e24c688669a1d678
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-22 12:52:54 +0000

    Add flag for enabling WebRTC AEC debug recordings.
    BUG= https://code.google.com/p/webrtc/issues/detail?id=830
    
    Once libjingle has been rolled and sticks a hard coded string shall be changed to a variable, see todo.
    
    Review URL: https://chromiumcodereview.appspot.com/14893006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201499 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/media_stream_dependency_factory.cc
+++ content/renderer/media/media_stream_dependency_factory.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/synchronization/waitable_event.h"
 #include "base/utf_string_conversions.h"
+#include "content/public/common/content_switches.h"
 #include "content/renderer/media/media_stream_source_extra_data.h"
 #include "content/renderer/media/rtc_media_constraints.h"
 #include "content/renderer/media/rtc_peer_connection_handler.h"
@@ -300,6 +302,14 @@ void MediaStreamDependencyFactory::CreateNativeMediaSources(
         sources_created.Run(description, false);
         return;
       }
+    }
+
+    const CommandLine& command_line = *CommandLine::ForCurrentProcess();
+    // TODO(grunell): Change hard coded string to
+    // RTCMediaConstraints::kInternalAecDump when libjingle has been rolled.
+    if (command_line.HasSwitch(switches::kEnableWebRtcAecRecordings)) {
+      native_audio_constraints.AddOptional(
+          "internalAecDump", "true");
     }
 
     // Creates a LocalAudioSource object which holds audio options.
--- content/renderer/media/rtc_media_constraints.cc
+++ content/renderer/media/rtc_media_constraints.cc
@@ -5,7 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/rtc_media_constraints.h"
 
 #include "base/logging.h"
-
+#include "base/string_util.h"
 #include "content/common/media/media_stream_options.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebMediaConstraints.h"
 #include "third_party/WebKit/Source/Platform/chromium/public/WebCString.h"
@@ -27,6 +27,14 @@ void GetNativeMediaConstraints(
     if (new_constraint.key == kMediaStreamSource ||
         new_constraint.key == kMediaStreamSourceId)
       continue;
+
+    // Ignore internal constraints set by JS.
+    // TODO(jiayl): replace the hard coded string with
+    // webrtc::MediaConstraintsInterface::kInternalConstraintPrefix when
+    // the Libjingle change is rolled.
+    if (StartsWithASCII(new_constraint.key, "internal", true))
+      continue;
+
     DVLOG(3) << "MediaStreamConstraints:" << new_constraint.key
              << " : " <<  new_constraint.value;
     native_constraints->push_back(new_constraint);
@@ -57,6 +65,11 @@ RTCMediaConstraints::GetMandatory() const  {
 const webrtc::MediaConstraintsInterface::Constraints&
 RTCMediaConstraints::GetOptional() const {
   return optional_;
+}
+
+void RTCMediaConstraints::AddOptional(const std::string& key,
+                                      const std::string& value) {
+  optional_.push_back(Constraint(key, value));
 }
 
 }  // namespace content
