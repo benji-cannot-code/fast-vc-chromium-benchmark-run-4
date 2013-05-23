41aefda6a3f9a864df8956c77da7d75308b3b384
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 00:33:38 +0000

    Move TSF IME initialization code from chrome to content.
    Current the TSF IME handler (TextServicesMessageFilter) is created in chrome (chrome/browser/chrome_browser_main_win.cc).
    This should be moved to content/, that is IME should be functional for content, including being functional in content_shell.
    
    BUG=171176
    TEST=run content_shell.exe on Win8 with --enable-text-services-framework and check manually if IME works
    
    Review URL: https://chromiumcodereview.appspot.com/15449002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201649 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_win.cc
+++ chrome/browser/chrome_browser_main_win.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "base/utf_string_conversions.h"
 #include "base/win/metro.h"
-#include "base/win/text_services_message_filter.h"
 #include "base/win/windows_version.h"
 #include "base/win/wrapped_window_proc.h"
 #include "chrome/browser/browser_util_win.h"
@@ -198,21 +197,6 @@ void ChromeBrowserMainPartsWin::PreMainMessageLoopStart() {
   if (!parameters().ui_task) {
     // Make sure that we know how to handle exceptions from the message loop.
     InitializeWindowProcExceptions();
-  }
-}
-
-void ChromeBrowserMainPartsWin::PostMainMessageLoopStart() {
-  DCHECK_EQ(MessageLoop::TYPE_UI, MessageLoop::current()->type());
-
-  if (base::win::IsTSFAwareRequired()) {
-    // Create a TSF message filter for the message loop. MessageLoop takes
-    // ownership of the filter.
-    scoped_ptr<base::win::TextServicesMessageFilter> tsf_message_filter(
-      new base::win::TextServicesMessageFilter);
-    if (tsf_message_filter->Init()) {
-      MessageLoopForUI::current()->SetMessageFilter(
-        tsf_message_filter.PassAs<MessageLoopForUI::MessageFilter>());
-    }
   }
 }
 
--- content/browser/browser_main_loop.cc
+++ content/browser/browser_main_loop.cc
@@ -70,6 +70,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <commctrl.h>
 #include <shellapi.h>
 
+#include "base/win/text_services_message_filter.h"
 #include "content/browser/system_message_window_win.h"
 #include "content/common/sandbox_win.h"
 #include "net/base/winsock_init.h"
@@ -403,6 +404,17 @@ void BrowserMainLoop::MainMessageLoopStart() {
 
 #if defined(OS_WIN)
   system_message_window_.reset(new SystemMessageWindowWin);
+
+  if (base::win::IsTSFAwareRequired()) {
+    // Create a TSF message filter for the message loop. MessageLoop takes
+    // ownership of the filter.
+    scoped_ptr<base::win::TextServicesMessageFilter> tsf_message_filter(
+      new base::win::TextServicesMessageFilter);
+    if (tsf_message_filter->Init()) {
+      MessageLoopForUI::current()->SetMessageFilter(
+        tsf_message_filter.PassAs<MessageLoopForUI::MessageFilter>());
+    }
+  }
 #endif
 
   if (parts_)
