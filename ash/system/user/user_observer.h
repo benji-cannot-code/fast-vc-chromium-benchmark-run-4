d11e793a33ef8578d0469addbcb1febd1733c76c
skuhne@google.com <skuhne@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 14:52:33 +0000

    Fixing build stopper
    BUG=-
    TBR=nkostylev@chromium.org
    
    Review URL: https://codereview.chromium.org/15884003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201798 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/user/user_observer.h
+++ ash/system/user/user_observer.h
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_SYSTEM_USER_USER_OBSERVER_H_
 #define ASH_SYSTEM_USER_USER_OBSERVER_H_
 
+#include "ash/ash_export.h"
+
 namespace ash {
 
-class UserObserver {
+class ASH_EXPORT UserObserver {
  public:
   virtual ~UserObserver() {}
 
