f20b45f4c279d3cf60927daddf495bd4f3477ef4
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 15:09:07 +0000

    Clear |event_handler_| in UnprivilegedProcessDelegate::KillProcess() so that the object could be reused to launch another process.
    BUG=238821
    
    Review URL: https://chromiumcodereview.appspot.com/15747007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201801 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/win/unprivileged_process_delegate.cc
+++ remoting/host/win/unprivileged_process_delegate.cc
@@ -350,6 +350,7 @@ void UnprivilegedProcessDelegate::KillProcess() {
   DCHECK(CalledOnValidThread());
 
   channel_.reset();
+  event_handler_ = NULL;
 
   if (worker_process_.IsValid()) {
     TerminateProcess(worker_process_, CONTROL_C_EXIT);
