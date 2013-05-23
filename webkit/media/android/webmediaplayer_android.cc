4618e566e8ba5c422a272e85ddb81415bdddab80
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 14:41:34 +0000

    Revert 201791 "Fix various MediaSource related crashes on Android."
    > Fix various MediaSource related crashes on Android.
    > 
    > This change fixes several issues that were causing crashes.
    > - MediaPlayerHostMsg_DemuxerReady_Params was not initializing the xxx_codec
    >   member variables so audio-only or video-only content would randomly be
    >   flagged as A/V content with a random codec ID.
    > - ChunkDemuxer::Stop() was not being called which could cause the delegate
    >   to be accessed after it was destroyed.
    > - MediaSourceDelegate was signalling that it had metadata before the 
    >   ChunkDemuxer was even initialized. This caused the HTMLMediaElement to
    >   signal that the load completed way too early and caused play() to be
    >   called when the delegate wasn't ready.
    > 
    > BUG=233420
    > TEST=webkitmediasource-play.html LayoutTest reliably runs w/o crashing now.
    > 
    > Review URL: https://chromiumcodereview.appspot.com/15754004
    
    TBR=acolwell@chromium.org
    
    Review URL: https://codereview.chromium.org/15838006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201797 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/android/webmediaplayer_android.cc
+++ webkit/media/android/webmediaplayer_android.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayerClient.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebMediaSource.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebView.h"
+#include "webkit/media/android/media_source_delegate.h"
 #include "webkit/media/android/webmediaplayer_manager_android.h"
 #include "webkit/media/android/webmediaplayer_proxy_android.h"
 #include "webkit/media/webmediaplayer_util.h"
@@ -552,7 +553,6 @@ void WebMediaPlayerAndroid::Detach() {
     stream_id_ = 0;
   }
 
-  media_source_delegate_.reset();
   current_frame_ = NULL;
   manager_ = NULL;
   proxy_ = NULL;
--- webkit/media/android/webmediaplayer_android.h
+++ webkit/media/android/webmediaplayer_android.h
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/Source/Platform/chromium/public/WebURL.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayer.h"
 #include "ui/gfx/rect_f.h"
-#include "webkit/media/android/media_source_delegate.h"
 #include "webkit/media/android/stream_texture_factory_android.h"
 
 namespace media {
@@ -36,6 +35,8 @@ class WebLayerImpl;
 }
 
 namespace webkit_media {
+
+class MediaSourceDelegate;
 class WebMediaPlayerManagerAndroid;
 class WebMediaPlayerProxyAndroid;
 
@@ -304,8 +305,7 @@ class WebMediaPlayerAndroid
   gfx::RectF last_computed_rect_;
 #endif
 
-  scoped_ptr<MediaSourceDelegate,
-             MediaSourceDelegate::Destroyer> media_source_delegate_;
+  scoped_ptr<MediaSourceDelegate> media_source_delegate_;
 
   // Proxy object that delegates method calls on Render Thread.
   // This object is created on the Render Thread and is only called in the
