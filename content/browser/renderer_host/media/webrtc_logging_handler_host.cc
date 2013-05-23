fa10dd8bd662216857b2c41d1c4a00f35d4e6d6d
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 19:05:38 +0000

    Smaller WebRTC log buffer on Android.
    BUG=229829
    
    Review URL: https://chromiumcodereview.appspot.com/15371010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201844 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/media/webrtc_logging_handler_host.cc
+++ content/browser/renderer_host/media/webrtc_logging_handler_host.cc
@@ -11,7 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+#if defined(OS_ANDROID)
+const size_t kWebRtcLogSize = 1 * 1024 * 1024;  // 1 MB
+#else
 const size_t kWebRtcLogSize = 6 * 1024 * 1024;  // 6 MB
+#endif
 
 WebRtcLoggingHandlerHost::WebRtcLoggingHandlerHost() {
 }
