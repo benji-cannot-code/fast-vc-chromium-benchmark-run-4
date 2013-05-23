0b527bb5a4a6dada6e9fd57c12736a603ff22c28
adamk@chromium.org <adamk@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-23 23:59:35 +0000

    Update run-binding-tests baselines.
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/15948002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151028 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8Float64Array.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8Float64Array.cpp
@@ -49,7 +49,6 @@ namespace WebCore {
 } // namespace WebCore
 using WebCore::ScriptWrappable;
 using WebCore::V8Float64Array;
-using WebCore::Float64Array;
 #endif
 void initializeScriptWrappableForInterface(Float64Array* object)
 {
