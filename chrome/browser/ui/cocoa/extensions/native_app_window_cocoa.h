6c37ce5f89528279a5f687488e198551e0801847
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 06:58:36 +0000

    Fixed app window size updates for app reloads on Mac OS.
    Chrome application windows were sometimes being resized incorrectly on
    Mac OS after application reloads. This happened for windows whose size
    was the maximum allowed and whose maximum size was changed by the
    reload. This did not occur for the same changes on Chrome OS or Linux.
    
    The source of the problem was that NativeAppWindowCocoa incorrectly
    assumed a window was maximized simply because its size was equal to its
    maximum size (this is the logic in the NSWindow isZoomed method which
    NativeAppWindowCocoa used). When that maximum size was then changed, the
    window was getting resized to the new maximum size when it shouldn't
    have been. The fix was to have NativeAppWindowCocoa maintain its own
    state for whether it had been maximized or not.
    
    TBR=tapted@chromium.org
    BUG=161095
    
    Review URL: https://codereview.chromium.org/15648005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.h
+++ chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.h
@@ -90,6 +90,9 @@ class NativeAppWindowCocoa : public NativeAppWindow {
   // Called when the window is un-minimized.
   void WindowDidDeminiaturize();
 
+  // Called when the window is zoomed (maximized or de-maximized).
+  void WindowWillZoom();
+
   // Called to handle a key event.
   bool HandledByExtensionCommand(NSEvent* event);
 
@@ -149,6 +152,7 @@ class NativeAppWindowCocoa : public NativeAppWindow {
 
   bool has_frame_;
 
+  bool is_maximized_;
   bool is_fullscreen_;
   NSRect restored_bounds_;
 
