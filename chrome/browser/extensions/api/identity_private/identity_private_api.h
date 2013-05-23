e05e00c8a1410790d032e88356996bc9cadf682c
courage@chromium.org <courage@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 10:34:44 +0000

    Identity API: Add component app for auth flow UI
    All Identity API UI is moving into a webview tag inside a component
    app, allowing Chrome Apps to be isolated from the browser.
    
    This change adds a component app that displays a webview based dialog
    box, and a private API used to launch the app and load its resources.
    
    BUG=228908
    (This is part of step #6 in the description.)
    
    Review URL: https://chromiumcodereview.appspot.com/14768009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201736 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/extensions/api/identity_private/identity_private_api.h
@@ -0,0 +1,31 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_EXTENSIONS_API_IDENTITY_PRIVATE_IDENTITY_PRIVATE_API_H_
+#define CHROME_BROWSER_EXTENSIONS_API_IDENTITY_PRIVATE_IDENTITY_PRIVATE_API_H_
+
+#include "chrome/browser/extensions/extension_function.h"
+
+namespace extensions {
+
+class IdentityPrivateGetResourcesFunction : public SyncExtensionFunction {
+ public:
+  DECLARE_EXTENSION_FUNCTION("identityPrivate.getResources",
+                             IDENTITYPRIVATE_GETRESOURCES);
+  IdentityPrivateGetResourcesFunction();
+
+ protected:
+  virtual ~IdentityPrivateGetResourcesFunction();
+
+  // ExtensionFunction:
+  virtual bool RunImpl() OVERRIDE;
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(IdentityPrivateGetResourcesFunction);
+};
+
+}  // extensions
+
+#endif  // CHROME_BROWSER_EXTENSIONS_API_IDENTITY_PRIVATE_IDENTITY_PRIVATE_API_H_
