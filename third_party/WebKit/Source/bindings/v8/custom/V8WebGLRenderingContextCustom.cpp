8e292417eb926f4de4f54f59c2df2d0f15647c70
gman@chromium.org <gman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-23 02:17:41 +0000

    Order WeBGL extensions alphabetically
    This slightly decreases the possibly of conflicts when
    adding new extensions over appending at the end of the list
    which is always guaranteed to conflict.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/15743016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150945 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8WebGLRenderingContextCustom.cpp
@@ -178,10 +178,6 @@ static v8::Handle<v8::Value> toV8Object(WebGLExtension* extension, v8::Handle<v8
     v8::Handle<v8::Value> extensionObject;
     const char* referenceName = 0;
     switch (extension->getName()) {
-    case WebGLExtension::WebGLLoseContextName:
-        extensionObject = toV8(static_cast<WebGLLoseContext*>(extension), contextObject, isolate);
-        referenceName = "webGLLoseContextName";
-        break;
     case WebGLExtension::EXTDrawBuffersName:
         extensionObject = toV8(static_cast<EXTDrawBuffers*>(extension), contextObject, isolate);
         referenceName = "extDrawBuffersName";
@@ -189,6 +185,10 @@ static v8::Handle<v8::Value> toV8Object(WebGLExtension* extension, v8::Handle<v8
     case WebGLExtension::EXTTextureFilterAnisotropicName:
         extensionObject = toV8(static_cast<EXTTextureFilterAnisotropic*>(extension), contextObject, isolate);
         referenceName = "extTextureFilterAnisotropicName";
+        break;
+    case WebGLExtension::OESElementIndexUintName:
+        extensionObject = toV8(static_cast<OESElementIndexUint*>(extension), contextObject, isolate);
+        referenceName = "oesElementIndexUintName";
         break;
     case WebGLExtension::OESStandardDerivativesName:
         extensionObject = toV8(static_cast<OESStandardDerivatives*>(extension), contextObject, isolate);
@@ -214,18 +214,6 @@ static v8::Handle<v8::Value> toV8Object(WebGLExtension* extension, v8::Handle<v8
         extensionObject = toV8(static_cast<OESVertexArrayObject*>(extension), contextObject, isolate);
         referenceName = "oesVertexArrayObjectName";
         break;
-    case WebGLExtension::OESElementIndexUintName:
-        extensionObject = toV8(static_cast<OESElementIndexUint*>(extension), contextObject, isolate);
-        referenceName = "oesElementIndexUintName";
-        break;
-    case WebGLExtension::WebGLDebugRendererInfoName:
-        extensionObject = toV8(static_cast<WebGLDebugRendererInfo*>(extension), contextObject, isolate);
-        referenceName = "webGLDebugRendererInfoName";
-        break;
-    case WebGLExtension::WebGLDebugShadersName:
-        extensionObject = toV8(static_cast<WebGLDebugShaders*>(extension), contextObject, isolate);
-        referenceName = "webGLDebugShadersName";
-        break;
     case WebGLExtension::WebGLCompressedTextureATCName:
         extensionObject = toV8(static_cast<WebGLCompressedTextureATC*>(extension), contextObject, isolate);
         referenceName = "webGLCompressedTextureATCName";
@@ -237,9 +225,21 @@ static v8::Handle<v8::Value> toV8Object(WebGLExtension* extension, v8::Handle<v8
         extensionObject = toV8(static_cast<WebGLCompressedTextureS3TC*>(extension), contextObject, isolate);
         referenceName = "webGLCompressedTextureS3TCName";
         break;
+    case WebGLExtension::WebGLDebugRendererInfoName:
+        extensionObject = toV8(static_cast<WebGLDebugRendererInfo*>(extension), contextObject, isolate);
+        referenceName = "webGLDebugRendererInfoName";
+        break;
+    case WebGLExtension::WebGLDebugShadersName:
+        extensionObject = toV8(static_cast<WebGLDebugShaders*>(extension), contextObject, isolate);
+        referenceName = "webGLDebugShadersName";
+        break;
     case WebGLExtension::WebGLDepthTextureName:
         extensionObject = toV8(static_cast<WebGLDepthTexture*>(extension), contextObject, isolate);
         referenceName = "webGLDepthTextureName";
+        break;
+    case WebGLExtension::WebGLLoseContextName:
+        extensionObject = toV8(static_cast<WebGLLoseContext*>(extension), contextObject, isolate);
+        referenceName = "webGLLoseContextName";
         break;
     }
     ASSERT(!extensionObject.IsEmpty());
