2df312f45873df99d8df188abc3decc5195405cf
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 12:59:54 +0000

    Split ProcessMetrics out of base/process_util.h and into base/process/process_metrics.h.
    BUG=242290
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/15564006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201775 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ base/process/process_metrics_posix.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/process/process_metrics.h"
+
+#include <sys/time.h>
+
+namespace base {
+
+int64 TimeValToMicroseconds(const struct timeval& tv) {
+  static const int kMicrosecondsPerSecond = 1000000;
+  int64 ret = tv.tv_sec;  // Avoid (int * int) integer overflow.
+  ret *= kMicrosecondsPerSecond;
+  ret += tv.tv_usec;
+  return ret;
+}
+
+ProcessMetrics::~ProcessMetrics() { }
+
+}  // namespace base
--- base/process_util_ios.mm
+++ base/process_util_ios.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/process_util.h"
 
 #import <Foundation/Foundation.h>
-#include <mach/task.h>
 #include <stdio.h>
 #include <sys/resource.h>
 
@@ -16,19 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // link.  In general, process_util isn't valid on iOS.
 
 namespace base {
-
-namespace {
-
-bool GetTaskInfo(task_basic_info_64* task_info_data) {
-  mach_msg_type_number_t count = TASK_BASIC_INFO_64_COUNT;
-  kern_return_t kr = task_info(mach_task_self(),
-                               TASK_BASIC_INFO_64,
-                               reinterpret_cast<task_info_t>(task_info_data),
-                               &count);
-  return kr == KERN_SUCCESS;
-}
-
-}  // namespace
 
 ProcessId GetCurrentProcId() {
   return getpid();
@@ -65,29 +51,6 @@ size_t GetMaxFds() {
     max_fds = INT_MAX;
 
   return static_cast<size_t>(max_fds);
-}
-
-ProcessMetrics::ProcessMetrics(ProcessHandle process) {}
-
-ProcessMetrics::~ProcessMetrics() {}
-
-// static
-ProcessMetrics* ProcessMetrics::CreateProcessMetrics(ProcessHandle process) {
-  return new ProcessMetrics(process);
-}
-
-size_t ProcessMetrics::GetPagefileUsage() const {
-  task_basic_info_64 task_info_data;
-  if (!GetTaskInfo(&task_info_data))
-    return 0;
-  return task_info_data.virtual_size;
-}
-
-size_t ProcessMetrics::GetWorkingSetSize() const {
-  task_basic_info_64 task_info_data;
-  if (!GetTaskInfo(&task_info_data))
-    return 0;
-  return task_info_data.resident_size;
 }
 
 }  // namespace base
