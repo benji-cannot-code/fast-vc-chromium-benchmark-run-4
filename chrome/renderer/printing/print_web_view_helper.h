c2694bebcb16a26ed681479b054d4adc363dc2c3
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 19:18:02 +0000

    Don't store pointers to WebFrame*. Sync events may run message loop, change layout and delete referenced frame.
    We know for sure only one place in attached bug. I still changed PrintPreviewContext and PrepareFrameAndViewForPrint
    to make sure that we crash on dereferencing NULL pointer, not on use of deleted memory.
    
    BUG=236147
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15531002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201852 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/printing/print_web_view_helper.h
+++ chrome/renderer/printing/print_web_view_helper.h
@@ -38,6 +38,24 @@ namespace printing {
 struct PageSizeMargins;
 class PrepareFrameAndViewForPrint;
 
+// Stores reference to frame using WebVew and unique name.
+// Workaround to modal dialog issue on Linux. crbug.com/236147.
+class FrameReference {
+ public:
+  explicit FrameReference(const WebKit::WebFrame* frame);
+  FrameReference();
+  ~FrameReference();
+
+  void Reset(const WebKit::WebFrame* frame);
+
+  WebKit::WebFrame* GetFrame();
+  WebKit::WebView* view();
+
+ private:
+  WebKit::WebView* view_;
+  WebKit::WebString frame_name_;
+};
+
 // PrintWebViewHelper handles most of the printing grunt work for RenderView.
 // We plan on making print asynchronous and that will require copying the DOM
 // of the document and creating a new WebView with the contents.
@@ -390,8 +408,8 @@ class PrintWebViewHelper
     // Helper functions
     int GetNextPageNumber();
     bool IsRendering() const;
-    bool IsModifiable() const;
-    bool HasSelection() const;
+    bool IsModifiable();
+    bool HasSelection();
     bool IsLastPageOfPrintReadyMetafile() const;
     bool IsFinalPageRendered() const;
 
@@ -430,7 +448,7 @@ class PrintWebViewHelper
     void ClearContext();
 
     // Specifies what to render for print preview.
-    WebKit::WebFrame* source_frame_;
+    FrameReference source_frame_;
     WebKit::WebNode source_node_;
 
     scoped_ptr<PrepareFrameAndViewForPrint> prep_frame_view_;
