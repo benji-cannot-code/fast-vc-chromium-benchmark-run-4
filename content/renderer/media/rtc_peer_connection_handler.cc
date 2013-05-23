f43fe6970b13aa8096b4f6cb1abcb4a10b9e3270
jiayl@chromium.org <jiayl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 22:22:22 +0000

    Pass the sctp command-line flag to Libjingle as an internal-only constraint.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/15120009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201899 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/rtc_peer_connection_handler.cc
+++ content/renderer/media/rtc_peer_connection_handler.cc
@@ -9,10 +9,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <utility>
 #include <vector>
 
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/stl_util.h"
 #include "base/utf_string_conversions.h"
+#include "content/public/common/content_switches.h"
 #include "content/renderer/media/media_stream_dependency_factory.h"
 #include "content/renderer/media/peer_connection_tracker.h"
 #include "content/renderer/media/remote_media_stream_impl.h"
@@ -347,6 +349,15 @@ bool RTCPeerConnectionHandler::initialize(
   GetNativeIceServers(server_configuration, &servers);
 
   RTCMediaConstraints constraints(options);
+
+  if (CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableSCTPDataChannels)) {
+    // TODO(jiayl): replace the hard coded string with
+    // webrtc::MediaConstraintsInterface::kEnableSctpDataChannels when
+    // the Libjingle change is rolled.
+    constraints.AddOptional("internalSctpDataChannels", "true");
+  }
+
   native_peer_connection_ =
       dependency_factory_->CreatePeerConnection(
           servers, &constraints, frame_, this);
