a633739028feca9c022cfe103b3c9d234e2cb214
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 16:29:05 +0000

    [Telemetry] Add a unit test for profile creator object discovery
    BUG=136664
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15834005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201820 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/profile_types_unittest.py
+++ tools/telemetry/telemetry/core/profile_types_unittest.py
@@ -2,6 +2,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+import os
 import unittest
 
 from telemetry.core import profile_types
@@ -15,3 +16,16 @@ class ProfileTypesTest(unittest.TestCase):
 
   def testGetProfileDir(self):
     self.assertFalse(profile_types.GetProfileDir('typical_user') is None)
+
+  def testGetProfileCreatorTypes(self):
+    profile_creators_dir = os.path.join(os.path.dirname(__file__), '..', '..',
+        'unittest_data','discoverable_classes')
+
+    profile_types.FindProfileCreators(profile_creators_dir)
+    types = profile_types.GetProfileTypes()
+    self.assertTrue(len(types) > 0)
+    self.assertTrue('dummy_profile' in types)
+
+    dummy_profile_creator = profile_types.GetProfileCreator('dummy_profile')
+    self.assertTrue(dummy_profile_creator.__name__ == 'DummyProfileCreator')
+    profile_types.ClearProfieCreatorsForTests()
