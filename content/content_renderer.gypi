e0ed1660f6250bf782591bb287be65cdf9eeb65e
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 06:58:29 +0000

    Delay selection bounds updates while handling ime events
    While handling an IME event, the selection bounds may temporarily
    change to incorrect values (just like other parts of the text input
    state). Currently, we delay updates of the text input state until we
    finish handling the IME event, but we process changes to the selection
    bounds normally.
    
    Instead, we should delay the selection bounds update until we have
    finished handling the ime event.
    
    So that both of these are handled consistently (and easily), introduce
    an ImeEventGuard that guards handling of IME events with:
    prologue:
      check handling_ime_event == false
      set handling_ime_event = true
    epilogue:
      set handling_ime_event = false
      update text input state
      update selection bounds
    
    Note that this fixes one instance of handling ime events inconsistently.
    
    TBR=jam@chromium.org
    BUG=238444
    
    Review URL: https://chromiumcodereview.appspot.com/14757009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201721 0039d316-1c4b-4281-b951-d872f2087c98

--- content/content_renderer.gypi
+++ content/content_renderer.gypi
@@ -263,6 +263,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'renderer/fetchers/multi_resolution_image_resource_fetcher.h',
     'renderer/fetchers/resource_fetcher.cc',
     'renderer/fetchers/resource_fetcher.h',
+    'renderer/ime_event_guard.cc',
+    'renderer/ime_event_guard.h',
     'renderer/render_process.h',
     'renderer/render_process_impl.cc',
     'renderer/render_process_impl.h',
---
+++ content/renderer/ime_event_guard.h
@@ -0,0 +1,24 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef IME_EVENT_GUARD_H_
+#define IME_EVENT_GUARD_H_
+
+namespace content {
+class RenderWidget;
+
+// Simple RAII object for handling IME events. Calls StartHandlingImeEvent on
+// construction and FinishHandlingImeEvent on destruction.
+class ImeEventGuard {
+ public:
+  explicit ImeEventGuard(RenderWidget* widget);
+  ~ImeEventGuard();
+ private:
+  RenderWidget* widget_;
+};
+}
+
+#endif
+
