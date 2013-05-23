6580eaa1ed3d3cc85224e28c6fb6dd872e8d3d66
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 16:18:53 +0000

    views/examples: Remove two more unused includes that were missed in r201585
    See https://chromiumcodereview.appspot.com/15637002/
    
    TBR=sky@chromium.org,msw@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15737020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201816 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/examples_window_with_content.cc
+++ ui/views/examples/examples_window_with_content.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/command_line.h"
 #include "base/memory/scoped_vector.h"
 #include "base/utf_string_conversions.h"
 #include "content/public/browser/browser_context.h"
@@ -41,10 +40,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/layout/grid_layout.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_delegate.h"
-
-#if defined(USE_AURA)
-#include "ui/views/widget/desktop_aura/desktop_native_widget_aura.h"
-#endif
 
 namespace views {
 namespace examples {
