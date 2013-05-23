c69a8374645272938e2c29d2ba5b222a845a87c2
phajdan.jr@chromium.org <phajdan.jr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 17:13:06 +0000

    GTTF: Make EmbeddedTestServer use ephemeral port
    instead of a fixed port with a retry loop.
    
    This makes it more friendly to running tests in parallel,
    and also just much more robust. Also simplifies the code.
    
    BUG=none
    R=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/15433003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201832 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/embedded_test_server/embedded_test_server.cc
+++ net/test/embedded_test_server/embedded_test_server.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/string_util.h"
 #include "base/stringprintf.h"
 #include "base/threading/thread_restrictions.h"
+#include "net/base/ip_endpoint.h"
+#include "net/base/net_errors.h"
 #include "net/test/embedded_test_server/http_connection.h"
 #include "net/test/embedded_test_server/http_request.h"
 #include "net/test/embedded_test_server/http_response.h"
@@ -23,10 +25,6 @@ namespace net {
 namespace test_server {
 
 namespace {
-
-const int kPort = 8040;
-const char kIp[] = "127.0.0.1";
-const int kRetries = 10;
 
 // Callback to handle requests with default predefined response for requests
 // matching the address |url|.
@@ -107,7 +105,7 @@ bool EmbeddedTestServer::InitializeAndWaitUntilReady() {
   }
   run_loop.Run();
 
-  return Started();
+  return Started() && base_url_.is_valid();
 }
 
 bool EmbeddedTestServer::ShutdownAndWaitUntilComplete() {
@@ -130,22 +128,20 @@ void EmbeddedTestServer::InitializeOnIOThread() {
   DCHECK(io_thread_->BelongsToCurrentThread());
   DCHECK(!Started());
 
-  int retries_left = kRetries + 1;
-  int try_port = kPort;
+  SocketDescriptor socket_descriptor =
+      TCPListenSocket::CreateAndBindAnyPort("127.0.0.1", &port_);
+  if (socket_descriptor == TCPListenSocket::kInvalidSocket)
+    return;
 
-  while (retries_left > 0) {
-    SocketDescriptor socket_descriptor = TCPListenSocket::CreateAndBind(
-        kIp,
-        try_port);
-    if (socket_descriptor != TCPListenSocket::kInvalidSocket) {
-      listen_socket_ = new HttpListenSocket(socket_descriptor, this);
-      listen_socket_->Listen();
-      base_url_ = GURL(base::StringPrintf("http://%s:%d", kIp, try_port));
-      port_ = try_port;
-      break;
-    }
-    retries_left--;
-    try_port++;
+  listen_socket_ = new HttpListenSocket(socket_descriptor, this);
+  listen_socket_->Listen();
+
+  IPEndPoint address;
+  int result = listen_socket_->GetLocalAddress(&address);
+  if (result == OK) {
+    base_url_ = GURL(std::string("http://") + address.ToString());
+  } else {
+    LOG(ERROR) << "GetLocalAddress failed: " << ErrorToString(result);
   }
 }
 
