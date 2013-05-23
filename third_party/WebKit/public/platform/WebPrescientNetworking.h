ffda1f4ee33c85bc7be8be2846d7308aa0ceeb51
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-23 01:36:47 +0000

    Add prefetchDNS() to WebPrescientNetworking to prepare for prefetchDNS refactoring
    Review URL: https://chromiumcodereview.appspot.com/15699002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@150942 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebPrescientNetworking.h
+++ third_party/WebKit/public/platform/WebPrescientNetworking.h
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebPrescientNetworking_h
 
 #include "WebCommon.h"
+#include "WebString.h"
 
 namespace WebKit {
 
@@ -52,6 +53,10 @@ enum WebPreconnectMotivation {
 class WebPrescientNetworking {
 public:
     virtual ~WebPrescientNetworking() { }
+
+    // When a page navigation is speculated, DNS prefetch is triggered to hide
+    // the host resolution latency.
+    virtual void prefetchDNS(const WebString& hostname) { }
 
     // When a page navigation is speculated, preconnect is triggered to hide
     // session initialization latency to the server providing the page resource.
