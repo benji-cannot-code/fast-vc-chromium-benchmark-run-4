cad3d0fd50b63ec7d209214909306d7f84f9ae33
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 12:00:10 +0000

    add a macro that really identifies glibc
    uClibc pretends to be glibc, so just checking for __GLIBC__ doesn't always
    work.  Rather than check for defined(__GLIBC__) && !defined(__UCLIBC__) in
    multiple places, do it once and define LIBC_GLIBC if we're certain that we're
    really using glibc.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15405003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201761 0039d316-1c4b-4281-b951-d872f2087c98

--- build/build_config.h
+++ build/build_config.h
@@ -35,6 +35,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(TOOLKIT_VIEWS) && defined(USE_X11)
 #define TOOLKIT_GTK
 #endif
+#if defined(__GLIBC__) && !defined(__UCLIBC__)
+// we really are using glibc, not uClibc pretending to be glibc
+#define LIBC_GLIBC
+#endif
 #elif defined(_WIN32)
 #define OS_WIN 1
 #define TOOLKIT_VIEWS 1
