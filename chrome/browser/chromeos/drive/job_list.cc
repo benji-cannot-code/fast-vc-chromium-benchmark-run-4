a43c7e294cba2be474f4079fb555d3b11b42d796
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-23 04:43:14 +0000

    Add JobScheduler::TouchResource.
    This CL adds TouchResource method to the JobScheduler, which is newly added
    to support TouchFile on FileSystem.
    
    BUG=144369
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/15694002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201695 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/job_list.cc
+++ chrome/browser/chromeos/drive/job_list.cc
@@ -37,6 +37,8 @@ std::string JobTypeToString(JobType type) {
       return "TYPE_COPY_HOSTED_DOCUMENT";
     case TYPE_RENAME_RESOURCE:
       return "TYPE_RENAME_RESOURCE";
+    case TYPE_TOUCH_RESOURCE:
+      return "TYPE_TOUCH_RESOURCE";
     case TYPE_ADD_RESOURCE_TO_DIRECTORY:
       return "TYPE_ADD_RESOURCE_TO_DIRECTORY";
     case TYPE_REMOVE_RESOURCE_FROM_DIRECTORY:
@@ -118,6 +120,7 @@ bool IsActiveFileTransferJobInfo(const JobInfo& job_info) {
     case TYPE_COPY_RESOURCE:
     case TYPE_COPY_HOSTED_DOCUMENT:
     case TYPE_RENAME_RESOURCE:
+    case TYPE_TOUCH_RESOURCE:
     case TYPE_ADD_RESOURCE_TO_DIRECTORY:
     case TYPE_REMOVE_RESOURCE_FROM_DIRECTORY:
     case TYPE_ADD_NEW_DIRECTORY:
