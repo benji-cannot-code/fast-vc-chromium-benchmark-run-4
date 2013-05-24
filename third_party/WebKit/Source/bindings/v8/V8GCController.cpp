fc1bf4f5649950b220edd6f157b66c6ac88ab16b
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 20:56:01 +0000

    When walking the DOM tree during minor GCs, also walk down shadows
    We already walk up - if we start the tree walk inside a shadow, we walk
    out of the shadow, but never back in. This patch adds support for
    walking back in again.
    
    See crbug.com/243856 for an example of where this happens
    
    BUG=243856
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/15896011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151132 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8GCController.cpp
+++ third_party/WebKit/Source/bindings/v8/V8GCController.cpp
@@ -42,6 +42,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8RecursionScope.h"
 #include "bindings/v8/WrapperTypeInfo.h"
 #include "core/dom/Attr.h"
+#include "core/dom/shadow/ElementShadow.h"
+#include "core/dom/shadow/ShadowRoot.h"
 #include "core/html/HTMLImageElement.h"
 #include "core/platform/MemoryUsageSupport.h"
 #include "core/platform/chromium/TraceEvent.h"
@@ -175,6 +177,20 @@ private:
             if (node->firstChild()) {
                 node = node->firstChild();
                 continue;
+            }
+            if (node->isShadowRoot()) {
+                if (ShadowRoot* youngerShadowRoot = toShadowRoot(node)->youngerShadowRoot()) {
+                    node = youngerShadowRoot;
+                    continue;
+                }
+            }
+            if (node->isElementNode()) {
+                if (ElementShadow* shadow = toElement(node)->shadow()) {
+                    if (ShadowRoot* oldestShadowRoot = shadow->oldestShadowRoot()) {
+                        node = oldestShadowRoot;
+                        continue;
+                    }
+                }
             }
             while (!node->nextSibling()) {
                 if (!node->parentOrShadowHostNode())
