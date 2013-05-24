53b4e092568b31c0043c0709f55f36b946baf53f
alexis.menard@intel.com <alexis.menard@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 20:01:05 +0000

    CSSParser should return ImmutableStylePropertySets.
    The StylePropertySets created by CSSParser were already immutable.
    Let this be reflected in the pointer types returned.
    
    This is a backport from WebKit r148399. Patch by Andreas Kling reviewed by Anders Carlsson.
    
    R=eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/16020007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151127 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser.cpp
+++ third_party/WebKit/Source/core/css/CSSParser.cpp
@@ -1299,7 +1299,7 @@ void CSSParser::parseSelector(const String& string, CSSSelectorList& selectorLis
     m_selectorListForParseSelector = 0;
 }
 
-PassRefPtr<StylePropertySet> CSSParser::parseInlineStyleDeclaration(const String& string, Element* element)
+PassRefPtr<ImmutableStylePropertySet> CSSParser::parseInlineStyleDeclaration(const String& string, Element* element)
 {
     Document* document = element->document();
     CSSParserContext context = document->elementSheet()->contents()->parserContext();
@@ -1307,7 +1307,7 @@ PassRefPtr<StylePropertySet> CSSParser::parseInlineStyleDeclaration(const String
     return CSSParser(context, UseCounter::getFrom(document)).parseDeclaration(string, document->elementSheet()->contents());
 }
 
-PassRefPtr<StylePropertySet> CSSParser::parseDeclaration(const String& string, StyleSheetContents* contextStyleSheet)
+PassRefPtr<ImmutableStylePropertySet> CSSParser::parseDeclaration(const String& string, StyleSheetContents* contextStyleSheet)
 {
     setStyleSheet(contextStyleSheet);
 
@@ -1318,7 +1318,7 @@ PassRefPtr<StylePropertySet> CSSParser::parseDeclaration(const String& string, S
     if (m_hasFontFaceOnlyValues)
         deleteFontFaceOnlyValues();
 
-    RefPtr<StylePropertySet> style = createStylePropertySet();
+    RefPtr<ImmutableStylePropertySet> style = createStylePropertySet();
     clearProperties();
     return style.release();
 }
@@ -1393,7 +1393,7 @@ static inline void filterProperties(bool important, const CSSParser::ParsedPrope
     }
 }
 
-PassRefPtr<StylePropertySet> CSSParser::createStylePropertySet()
+PassRefPtr<ImmutableStylePropertySet> CSSParser::createStylePropertySet()
 {
     BitArray<numCSSProperties> seenProperties;
     size_t unusedEntries = m_parsedProperties.size();
