032a16cc583b49b11defe4e452de1b767a4c8cf6
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 23:04:31 +0000

    Migrate the IndexedDB backend from Blink to Chromium
    To get the IDB backend off the (deprecated) WebKit thread, remove
    intermediate proxying, and let us take advantage of base utilities,
    we're moving the code from Blink to Chromium.
    
    This patch is basically a glorified copy/paste of the Blink IDB
    backend code, with Chromium coding style applied, WTF dependencies
    replaced with STL and base/, redundant classes removed, etc. It
    introduces some new temporary proxy classes
    (content/browser/webidb*_impl.*) to allow us build both the old and
    new backends.
    
    The new backend is currently disabled by default. It can be enabled
    using a new (and temporary) command line switch: --new-indexeddb Once
    we've done some further cleanup and are confident that the new backend
    is stable, and the bots have moved from DumpRenderTree to
    content_shell, we'll switch to the new backend by default. Once that
    has survived through a dev channel release, we'll delete the Blink
    code and eliminate unnecessary proxy classes.
    
    BUG=234278
    R=alecflett@chromium.org, dgrogan@chromium.org, piman@chromium.org
    
    Review URL: https://codereview.chromium.org/15564008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202215 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/indexed_db/indexed_db_param_traits.cc
+++ content/common/indexed_db/indexed_db_param_traits.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/common/indexed_db/indexed_db_param_traits.h"
 
+#include <string>
+#include <vector>
 #include "content/common/indexed_db/indexed_db_key.h"
 #include "content/common/indexed_db/indexed_db_key_path.h"
 #include "content/common/indexed_db/indexed_db_key_range.h"
@@ -20,7 +22,7 @@ using WebKit::WebIDBKeyPath;
 namespace IPC {
 
 void ParamTraits<IndexedDBKey>::Write(Message* m, const param_type& p) {
-  WriteParam(m, int(p.type()));
+  WriteParam(m, static_cast<int>(p.type()));
   switch (p.type()) {
     case WebIDBKey::ArrayType:
       WriteParam(m, p.array());
@@ -37,8 +39,7 @@ void ParamTraits<IndexedDBKey>::Write(Message* m, const param_type& p) {
     case WebIDBKey::InvalidType:
     case WebIDBKey::NullType:
       return;
-    default:
-      // This is a placeholder for WebKit::WebIDBKey::MinType
+    case WebIDBKey::MinType:
       NOTREACHED();
       return;
   }
@@ -79,16 +80,17 @@ bool ParamTraits<IndexedDBKey>::Read(const Message* m,
     case WebIDBKey::NullType:
       *r = IndexedDBKey(web_type);
       return true;
-    default:
-      // This is a placeholder for WebKit::WebIDBKey::MinType
+    case WebIDBKey::MinType:
       NOTREACHED();
       return false;
   }
+  NOTREACHED();
+  return false;
 }
 
 void ParamTraits<IndexedDBKey>::Log(const param_type& p, std::string* l) {
   l->append("<IndexedDBKey>(");
-  LogParam(int(p.type()), l);
+  LogParam(static_cast<int>(p.type()), l);
   l->append(", ");
   l->append("[");
   std::vector<IndexedDBKey>::const_iterator it = p.array().begin();
@@ -108,7 +110,7 @@ void ParamTraits<IndexedDBKey>::Log(const param_type& p, std::string* l) {
 }
 
 void ParamTraits<IndexedDBKeyPath>::Write(Message* m, const param_type& p) {
-  WriteParam(m, int(p.type()));
+  WriteParam(m, static_cast<int>(p.type()));
   switch (p.type()) {
     case WebIDBKeyPath::ArrayType:
       WriteParam(m, p.array());
@@ -154,7 +156,7 @@ bool ParamTraits<IndexedDBKeyPath>::Read(const Message* m,
 
 void ParamTraits<IndexedDBKeyPath>::Log(const param_type& p, std::string* l) {
   l->append("<IndexedDBKeyPath>(");
-  LogParam(int(p.type()), l);
+  LogParam(static_cast<int>(p.type()), l);
   l->append(", ");
   LogParam(p.string(), l);
   l->append(", ");
