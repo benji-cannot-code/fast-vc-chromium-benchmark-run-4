b5a676f62f89f892be1ed28769c595d1bec2d86c
marja@chromium.org <marja@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 10:54:33 +0000

    Add an isolate parameter to ScopedPersistent::set.
    This gets rid of one call site of v8::Isolate::GetCurrent, which we're trying to
    get rid of.
    
    BUG=none
    R=dcarney@chromium.org, jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/15825002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151068 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8DOMWindowShell.cpp
+++ third_party/WebKit/Source/bindings/v8/V8DOMWindowShell.cpp
@@ -203,7 +203,7 @@ bool V8DOMWindowShell::initializeIfNeeded()
     v8::Context::Scope contextScope(context);
 
     if (m_global.isEmpty()) {
-        m_global.set(context->Global());
+        m_global.set(m_isolate, context->Global());
         if (m_global.isEmpty()) {
             disposeContext();
             return false;
@@ -293,7 +293,7 @@ void V8DOMWindowShell::createContext()
     v8::ExtensionConfiguration extensionConfiguration(index, extensionNames.get());
 
     v8::HandleScope handleScope(m_isolate);
-    m_context.set(v8::Context::New(m_isolate, &extensionConfiguration, globalTemplate, m_global.newLocal(m_isolate)));
+    m_context.set(m_isolate, v8::Context::New(m_isolate, &extensionConfiguration, globalTemplate, m_global.newLocal(m_isolate)));
 
     double contextCreationDurationInMilliseconds = (currentTime() - contextCreationStartInSeconds) * 1000;
     const char* histogramName = "WebCore.V8DOMWindowShell.createContext.MainWorld";
@@ -338,7 +338,7 @@ bool V8DOMWindowShell::installDOMWindow()
 void V8DOMWindowShell::updateDocumentWrapper(v8::Handle<v8::Object> wrapper)
 {
     ASSERT(m_world->isMainWorld());
-    m_document.set(wrapper);
+    m_document.set(m_isolate, wrapper);
 }
 
 void V8DOMWindowShell::updateDocumentProperty()
