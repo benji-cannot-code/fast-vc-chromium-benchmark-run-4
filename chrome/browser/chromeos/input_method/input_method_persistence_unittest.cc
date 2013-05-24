133e0cc9f10abee441156f67a2a9434ea5c1883e
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 20:59:23 +0000

    Add ProfileManager::AllowGetDefaultProfile()
    This should prevent calls to GetDefaultProfile until CreateProfile is called.
    
    BUG=243364
    
    Review URL: https://chromiumcodereview.appspot.com/15695010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202165 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_persistence_unittest.cc
+++ chrome/browser/chromeos/input_method/input_method_persistence_unittest.cc
@@ -43,6 +43,7 @@ class InputMethodPersistenceTest : public testing::Test {
     CommandLine *cl = CommandLine::ForCurrentProcess();
     cl->AppendSwitchASCII(switches::kLoginProfile, kProfileName);
     mock_profile_manager_.SetLoggedIn(true);
+    ProfileManager::AllowGetDefaultProfile();
     EXPECT_TRUE(ProfileManager::GetDefaultProfile() != NULL);
     mock_user_prefs_ = mock_profile->GetTestingPrefService();
   }
