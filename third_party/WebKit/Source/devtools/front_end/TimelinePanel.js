c7afc42d5390eb615b73d63f61f0a08a1cbbb1d4
caseq@google.com <caseq@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 08:33:47 +0000

    Timeline: fix switching from Events to Frame mode
    We create frame controller after a call to repopulateRecords(),
    so it may batch-pull all records from model. If repopulateRecords()
    calls refresh synchronously, refresh() does not have frame controller and
    does not render frame bars.
    
    BUG=234297
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/15755008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151057 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TimelinePanel.js
+++ third_party/WebKit/Source/devtools/front_end/TimelinePanel.js
@@ -633,7 +633,7 @@ WebInspector.TimelinePanel.prototype = {
         var records = this._model.records;
         for (var i = 0; i < records.length; ++i)
             this._innerAddRecordToTimeline(records[i]);
-        this._invalidateAndScheduleRefresh(false, true);
+        this._invalidateAndScheduleRefresh(false, false);
     },
 
     _onTimelineEventRecorded: function(event)
