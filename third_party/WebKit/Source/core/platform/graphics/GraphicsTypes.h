67f966eab8bc3a96adbbeb37db52e53b73c20536
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 23:09:11 +0000

    Improve GraphicsContext inlining and remove some redundant methods.
    Move all of the single line methods to be inlined, and move some
    multi-line previously inlined methods to no longer be inlined.
    Also rearranged the methods to make much more sense.
    
    And remove the paintingDisabled checks for methods that directly call
    other methods that do the check. Furthermore, the only methods
    that need paintingDisabled checks are those that try to access the
    canvas. All others cannot possible cause problems because there
    is no way to set the canvas so a GraphicsContext that is initially
    paintingDisabled will always be paintingDisabled, and it doesn't
    matter what its state is. This means we can make all the getters
    and setters of state do not need the extra if statement.
    
    Finally, a couple of methods are no needed as duplicates existed,
    and we don't need to store the int-packed color info because it is
    trivial and efficient to get it from the Color.
    
    R=pdr@chromium.org
    BUG=239585
    
    Review URL: https://codereview.chromium.org/15227003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151141 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/GraphicsTypes.h
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsTypes.h
@@ -27,7 +27,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef GraphicsTypes_h
 #define GraphicsTypes_h
 
-#include <wtf/Forward.h>
+#include "third_party/skia/include/core/SkPaint.h"
+
+#include "wtf/Forward.h"
 
 namespace WebCore {
 
@@ -93,9 +95,17 @@ enum GradientSpreadMethod {
     SpreadMethodRepeat
 };
 
-enum LineCap { ButtCap, RoundCap, SquareCap };
+enum LineCap {
+    ButtCap = SkPaint::kButt_Cap,
+    RoundCap = SkPaint::kRound_Cap,
+    SquareCap = SkPaint::kSquare_Cap
+};
 
-enum LineJoin { MiterJoin, RoundJoin, BevelJoin };
+enum LineJoin {
+    MiterJoin = SkPaint::kMiter_Join,
+    RoundJoin = SkPaint::kRound_Join,
+    BevelJoin = SkPaint::kBevel_Join
+};
 
 enum HorizontalAlignment { AlignLeft, AlignRight, AlignHCenter };
 
