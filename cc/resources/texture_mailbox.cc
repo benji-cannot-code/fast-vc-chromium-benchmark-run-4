3a837d145a7ed3879c62307c22ff80937385baf1
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 10:40:07 +0000

    cc: Fix uninitialized field in TextureMailbox
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/15826003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202044 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/texture_mailbox.cc
+++ cc/resources/texture_mailbox.cc
@@ -11,7 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace cc {
 
 TextureMailbox::TextureMailbox()
-    : sync_point_(0) {
+    : target_(GL_TEXTURE_2D),
+      sync_point_(0) {
 }
 
 TextureMailbox::TextureMailbox(
