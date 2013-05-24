55456061846f00699af6b59494397cf77a4e7c88
ddorwin@chromium.org <ddorwin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 17:41:32 +0000

    Enable ENCRYPTED_MEDIA_V2 (correct description for r151111)
    (r151111 was committed with the wrong description. This is the correct one.)
    
    This enables the existing implementation of the new API and fixes build errors.
    The fucntionality is behind a runtime flag and not yet implemented in Chromium.
    
    The dependency on Source/modules/encryptedmedia/ from Source/core/ will be removed in an upcoming CL (http://crbug.com/242754).
    
    BUG=224791
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/15536008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -91,6 +91,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #endif
 
 #if ENABLE(ENCRYPTED_MEDIA_V2)
+// FIXME: Remove dependency on modules/encryptedmedia (http://crbug.com/242754).
 #include "modules/encryptedmedia/MediaKeyNeededEvent.h"
 #include "modules/encryptedmedia/MediaKeys.h"
 #endif
