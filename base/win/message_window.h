5d61c23551343306378ff5ceb94c1c24205cd4ca
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 01:34:51 +0000

    Allow multiple base::MessagePumpForUI instances to be created simultanenously on Windows.
    The current implementation of base::MessagePumpForUI on Windows registers a window class with a predefined name in order to create a message-only window. The window class is unregistered when base::MessagePumpForUI is deleted. This causes issues when two or more instances of base::MessagePumpForUI are created/destroyed simultanenously on different threads. For instance once thread can unregister the window class right before the other thread is trying to create a window using this class.
    
    The CL addresses this problem by switching MessageWindow to implement a message-only window. It also moves MessageWindow from remoting/host/win to base/win along with the corresponding unit test.
    
    MessageWindow registers a uniquely named window class per MessageWindow instance making sure that different MessageWindow objects do not share any resources. In the future this can be optimized further by registering a common window class shared by all MessageWindow objects in a thread-safe manner (by using LazyInstance for example).
    
    BUG=241939
    
    Review URL: https://chromiumcodereview.appspot.com/15261005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201955 0039d316-1c4b-4281-b951-d872f2087c98

--- base/win/message_window.h
+++ base/win/message_window.h
@@ -3,25 +3,26 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef REMOTING_HOST_WIN_MESSAGE_WINDOW_H_
-#define REMOTING_HOST_WIN_MESSAGE_WINDOW_H_
+#ifndef BASE_WIN_MESSAGE_WINDOW_H_
+#define BASE_WIN_MESSAGE_WINDOW_H_
 
 #include <windows.h>
 
 #include <string>
 
+#include "base/base_export.h"
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/threading/non_thread_safe.h"
 
-namespace remoting {
+namespace base {
 namespace win {
 
 // Implements a message-only window.
-class MessageWindow : base::NonThreadSafe {
+class BASE_EXPORT MessageWindow : base::NonThreadSafe {
  public:
   // Handles incoming window messages.
-  class Delegate {
+  class BASE_EXPORT Delegate {
    public:
     virtual ~Delegate() {}
 
@@ -33,7 +34,6 @@ class MessageWindow : base::NonThreadSafe {
   };
 
   MessageWindow();
-  MessageWindow(const std::string& class_name, HINSTANCE instance);
   ~MessageWindow();
 
   // Registers the window class and creates the window. The incoming messages
@@ -63,6 +63,6 @@ class MessageWindow : base::NonThreadSafe {
 };
 
 }  // namespace win
-}  // namespace remoting
+}  // namespace base
 
-#endif  // REMOTING_HOST_WIN_MESSAGE_WINDOW_H_
+#endif  // BASE_WIN_MESSAGE_WINDOW_H_
