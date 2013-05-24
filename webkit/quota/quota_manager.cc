a05fbc0c8466bcde0e841aeafff6221d4e629c24
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 07:42:08 +0000

    [Quota][Clean up] Drop StorageType in GlobalUsageCallback
    This CL drops non-informative StorageType parameter from GlobalUsageCall.
    The value should be known by the original caller that makes the callback.
    
    TEST=should not break existing test
    TBR=arv@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15695003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202013 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/quota/quota_manager.cc
+++ webkit/quota/quota_manager.cc
@@ -399,8 +399,7 @@ class UsageAndQuotaCallbackDispatcher
     CheckCompleted();
   }
 
-  void DidGetGlobalUsage(StorageType type,
-                         int64 usage,
+  void DidGetGlobalUsage(int64 usage,
                          int64 unlimited_usage) {
     if (status_ == kQuotaStatusUnknown)
       status_ = kQuotaStatusOk;
@@ -488,13 +487,16 @@ class QuotaManager::GetUsageInfoTask : public QuotaTask {
     // This will populate cached hosts and usage info.
     manager()->GetUsageTracker(kStorageTypeTemporary)->GetGlobalUsage(
         base::Bind(&GetUsageInfoTask::DidGetGlobalUsage,
-                   weak_factory_.GetWeakPtr()));
+                   weak_factory_.GetWeakPtr(),
+                   kStorageTypeTemporary));
     manager()->GetUsageTracker(kStorageTypePersistent)->GetGlobalUsage(
         base::Bind(&GetUsageInfoTask::DidGetGlobalUsage,
-                   weak_factory_.GetWeakPtr()));
+                   weak_factory_.GetWeakPtr(),
+                   kStorageTypePersistent));
     manager()->GetUsageTracker(kStorageTypeSyncable)->GetGlobalUsage(
         base::Bind(&GetUsageInfoTask::DidGetGlobalUsage,
-                   weak_factory_.GetWeakPtr()));
+                   weak_factory_.GetWeakPtr(),
+                   kStorageTypeSyncable));
   }
 
   virtual void Completed() OVERRIDE {
@@ -1347,13 +1349,12 @@ void QuotaManager::ReportHistogram() {
 }
 
 void QuotaManager::DidGetTemporaryGlobalUsageForHistogram(
-    StorageType type,
     int64 usage,
     int64 unlimited_usage) {
   UMA_HISTOGRAM_MBYTES("Quota.GlobalUsageOfTemporaryStorage", usage);
 
   std::set<GURL> origins;
-  GetCachedOrigins(type, &origins);
+  GetCachedOrigins(kStorageTypeTemporary, &origins);
 
   size_t num_origins = origins.size();
   size_t protected_origins = 0;
@@ -1370,13 +1371,12 @@ void QuotaManager::DidGetTemporaryGlobalUsageForHistogram(
 }
 
 void QuotaManager::DidGetPersistentGlobalUsageForHistogram(
-    StorageType type,
     int64 usage,
     int64 unlimited_usage) {
   UMA_HISTOGRAM_MBYTES("Quota.GlobalUsageOfPersistentStorage", usage);
 
   std::set<GURL> origins;
-  GetCachedOrigins(type, &origins);
+  GetCachedOrigins(kStorageTypePersistent, &origins);
 
   size_t num_origins = origins.size();
   size_t protected_origins = 0;
