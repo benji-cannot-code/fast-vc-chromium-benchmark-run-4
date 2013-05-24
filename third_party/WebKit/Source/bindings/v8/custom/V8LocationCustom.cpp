d62a9a285c1c7541630e662ffcb73b51b2382aef
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 00:11:12 +0000

    Remove custom binding for Location::toString()
    The generated code is exactly the same as the existing custom binding.
    
    R=abarth,eseidel
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15715004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151030 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8LocationCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8LocationCustom.cpp
@@ -32,18 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8Location.h"
 
-#include "V8DOMWindow.h"
-#include "bindings/v8/BindingSecurity.h"
-#include "bindings/v8/V8Binding.h"
-#include "bindings/v8/V8EventListener.h"
-#include "bindings/v8/V8Utilities.h"
-#include "core/dom/Document.h"
-#include "core/loader/FrameLoader.h"
-#include "core/page/Frame.h"
-#include "core/page/Location.h"
-#include "core/platform/KURL.h"
-#include "wtf/text/WTFString.h"
-
 namespace WebCore {
 
 v8::Handle<v8::Value> V8Location::valueOfMethodCustom(const v8::Arguments& args)
@@ -54,16 +42,6 @@ v8::Handle<v8::Value> V8Location::valueOfMethodCustom(const v8::Arguments& args)
     // objects, since that would provide a hook to change the string
     // conversion behavior of location objects.
     return args.This();
-}
-
-v8::Handle<v8::Value> V8Location::toStringMethodCustom(const v8::Arguments& args)
-{
-    v8::Handle<v8::Object> holder = args.Holder();
-    Location* imp = V8Location::toNative(holder);
-    if (!BindingSecurity::shouldAllowAccessToFrame(imp->frame()))
-        return v8::Undefined();
-    String result = imp->href();
-    return v8String(result, args.GetIsolate());
 }
 
 }  // namespace WebCore
