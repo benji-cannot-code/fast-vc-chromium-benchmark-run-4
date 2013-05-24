92f24d3a4934e8ef89f1d6954bef25c9092cb81e
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 09:20:17 +0000

    Add to be fetched files instead of DriveMetadataStore.batch_sync_origins_
    This persists files to be fetched to the DB so they can be
    restored in case Chrome crashes. The origin itself is no
    longer saved but it does get added back to
    pending_batch_sync_origins anyway when the SyncFS app is
    loaded again.
    
    Pre-requisite to deprecating batch_sync_origins from
    DriveMetadataStore
    
    BUG=229764
    TEST=Existing unit_tests --gtest_filter=DriveFileSyncService*
    
    Review URL: https://chromiumcodereview.appspot.com/15023022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202025 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_file_sync_service.h
+++ chrome/browser/sync_file_system/drive_file_sync_service.h
@@ -243,6 +243,7 @@ class DriveFileSyncService : public RemoteFileSyncService,
   void DidGetDirectoryContentForBatchSync(
       const SyncStatusCallback& callback,
       const GURL& origin,
+      const std::string& resource_id,
       int64 largest_changestamp,
       google_apis::GDataErrorCode error,
       scoped_ptr<google_apis::ResourceList> feed);
@@ -401,7 +402,7 @@ class DriveFileSyncService : public RemoteFileSyncService,
 
   int64 largest_fetched_changestamp_;
 
-  std::set<GURL> pending_batch_sync_origins_;
+  std::map<GURL, std::string> pending_batch_sync_origins_;
 
   // Is set to true when there's a fair possibility that we have some
   // remote changes that haven't been fetched yet.
