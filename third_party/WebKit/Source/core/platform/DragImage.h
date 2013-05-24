1f7300fc2757edc2edee59a77f8961ff479eea08
varunjain@chromium.org <varunjain@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 17:50:58 +0000

    Provide a drag image for dragging a link.
    BUG=none
    R=dcheng@chromium.org, eseidel@chromium.org
    
    Review URL: https://codereview.chromium.org/14625008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151118 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/DragImage.h
+++ third_party/WebKit/Source/core/platform/DragImage.h
@@ -29,12 +29,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/platform/chromium/DragImageRef.h"
 #include "core/platform/graphics/FloatSize.h"
+#include "core/platform/graphics/FontRenderingMode.h"
 #include "core/platform/graphics/ImageOrientation.h"
 #include "core/platform/graphics/IntSize.h"
 #include <wtf/Forward.h>
-
-//We need to #define YOffset as it needs to be shared with WebKit
-#define DragLabelBorderYOffset 2
 
 namespace WebCore {
     
@@ -54,9 +52,9 @@ namespace WebCore {
     DragImageRef dissolveDragImageToFraction(DragImageRef image, float delta);
     
     DragImageRef createDragImageFromImage(Image*, RespectImageOrientationEnum = DoNotRespectImageOrientation);
-    DragImageRef createDragImageForSelection(Frame*);    
+    DragImageRef createDragImageForSelection(DragImageRef, float dragImageAlpha);
     DragImageRef createDragImageIconForCachedImage(CachedImage*);
-    DragImageRef createDragImageForLink(KURL&, const String& label, Frame*);
+    DragImageRef createDragImageForLink(const KURL&, const String& label, FontRenderingMode, float deviceScaleFactor);
     void deleteDragImage(DragImageRef);
 }
 
