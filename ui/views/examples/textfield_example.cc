1fad6b069cc71a9cb2654a340b0a5579405d8f76
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 01:43:09 +0000

    Refine Views textfield state interaction, testing, and examples.
    Ignore Views textfield CTRL+<key> shortcuts with Alt pressed. 
    Matches Win and fixes ctrl+alt+[a/z/etc.] AltGr input for Polish, etc.
    (alternate grapheme input was triggering undo/select-all/etc.)
    
    Enforce and test read-only and obscured states more rigorously.
    (prevent cut/copy/paste as needed, this was not always checked before)
    
    Do not change the focusable state of textfields when changing read-only.
    (this matches windows native behavior better)
    
    Expand and refine a bunch of unit tests and the Views Example for textfields.
    
    BUG=238105,131660
    TEST=Read-only and obscured Textfields work as intended. Polish input (right-Alt)+a, (right-Alt)+z, etc. work as expected. Ctrl+Alt+a, Ctrl+Alt+z, etc. are no-op on views textfields.
    R=oshima@chromium.org
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15684004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@201961 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/examples/textfield_example.cc
+++ ui/views/examples/textfield_example.cc
@@ -18,7 +18,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace views {
 namespace examples {
 
-TextfieldExample::TextfieldExample() : ExampleBase("Textfield") {
+TextfieldExample::TextfieldExample()
+   : ExampleBase("Textfield"),
+     name_(NULL),
+     password_(NULL),
+     read_only_(NULL),
+     show_password_(NULL),
+     clear_all_(NULL),
+     append_(NULL),
+     set_(NULL),
+     set_style_(NULL) {
 }
 
 TextfieldExample::~TextfieldExample() {
@@ -28,6 +37,9 @@ void TextfieldExample::CreateExampleView(View* container) {
   name_ = new Textfield();
   password_ = new Textfield(Textfield::STYLE_OBSCURED);
   password_->set_placeholder_text(ASCIIToUTF16("password"));
+  read_only_ = new Textfield();
+  read_only_->SetReadOnly(true);
+  read_only_->SetText(ASCIIToUTF16("read only"));
   show_password_ = new LabelButton(this, ASCIIToUTF16("Show password"));
   clear_all_ = new LabelButton(this, ASCIIToUTF16("Clear All"));
   append_ = new LabelButton(this, ASCIIToUTF16("Append"));
@@ -51,6 +63,9 @@ void TextfieldExample::CreateExampleView(View* container) {
   layout->AddView(new Label(ASCIIToUTF16("Password:")));
   layout->AddView(password_);
   layout->StartRow(0, 0);
+  layout->AddView(new Label(ASCIIToUTF16("Read Only:")));
+  layout->AddView(read_only_);
+  layout->StartRow(0, 0);
   layout->AddView(show_password_);
   layout->StartRow(0, 0);
   layout->AddView(clear_all_);
@@ -68,6 +83,8 @@ void TextfieldExample::ContentsChanged(Textfield* sender,
     PrintStatus("Name [%s]", UTF16ToUTF8(new_contents).c_str());
   } else if (sender == password_) {
     PrintStatus("Password [%s]", UTF16ToUTF8(new_contents).c_str());
+  } else if (sender == read_only_) {
+    PrintStatus("Read Only [%s]", UTF16ToUTF8(new_contents).c_str());
   }
 }
 
@@ -89,10 +106,15 @@ void TextfieldExample::ButtonPressed(Button* sender, const ui::Event& event) {
     string16 empty;
     name_->SetText(empty);
     password_->SetText(empty);
+    read_only_->SetText(empty);
   } else if (sender == append_) {
     name_->AppendText(ASCIIToUTF16("[append]"));
+    password_->AppendText(ASCIIToUTF16("[append]"));
+    read_only_->AppendText(ASCIIToUTF16("[append]"));
   } else if (sender == set_) {
     name_->SetText(ASCIIToUTF16("[set]"));
+    password_->SetText(ASCIIToUTF16("[set]"));
+    read_only_->SetText(ASCIIToUTF16("[set]"));
   } else if (sender == set_style_) {
     if (!name_->text().empty()) {
       name_->SetColor(SK_ColorGREEN);
--- ui/views/examples/textfield_example.h
+++ ui/views/examples/textfield_example.h
@@ -47,6 +47,7 @@ class TextfieldExample : public ExampleBase,
   // Textfields for name and password.
   Textfield* name_;
   Textfield* password_;
+  Textfield* read_only_;
 
   // Various buttons to control textfield.
   LabelButton* show_password_;
