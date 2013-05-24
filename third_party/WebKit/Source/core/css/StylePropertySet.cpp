8ecbc4b590ce32edfd436f72df6dbe644977b9fd
alexis.menard@intel.com <alexis.menard@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 14:34:08 +0000

    Move parseDeclaration() and clear() to MutableStylePropertySet.
    ...and remove ASSERT(isMutable()) from the function bodies.
    
    In svg/SVGFontFaceElement.cpp use clear() directly instead of taking
    an empty string through the CSS parser.
    
    This is a backport from WebKit r148397. Patch by Andreas Kling reviewed by Anders Carlsson.
    
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/16007002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151103 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/StylePropertySet.cpp
+++ third_party/WebKit/Source/core/css/StylePropertySet.cpp
@@ -287,10 +287,8 @@ bool StylePropertySet::setProperty(CSSPropertyID propertyID, int identifier, boo
     return true;
 }
 
-void StylePropertySet::parseDeclaration(const String& styleDeclaration, StyleSheetContents* contextStyleSheet)
+void MutableStylePropertySet::parseDeclaration(const String& styleDeclaration, StyleSheetContents* contextStyleSheet)
 {
-    ASSERT(isMutable());
-
     mutablePropertyVector().clear();
 
     CSSParserContext context(cssParserMode());
@@ -392,9 +390,8 @@ static const Vector<CSSPropertyID>& blockProperties()
     return properties;
 }
 
-void StylePropertySet::clear()
+void MutableStylePropertySet::clear()
 {
-    ASSERT(isMutable());
     mutablePropertyVector().clear();
 }
 
