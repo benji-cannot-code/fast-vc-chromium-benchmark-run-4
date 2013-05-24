8f2ead60fd7761da3b6a065f72d92a49bd82d02e
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 00:08:08 +0000

    Move TRACE_EVENT macros for V8 compilation and execution to V8ScriptRunner
    Now call sites of Script::Compile() and Script::Run() are aggregated to V8ScriptRunner.
    Thus we should put TRACE_EVENT macros in V8ScriptRunner.
    
    BUG=241743
    R=abarth,eseidel
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15865002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/V8ScriptRunner.cpp
+++ third_party/WebKit/Source/bindings/v8/V8ScriptRunner.cpp
@@ -33,11 +33,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/ScriptExecutionContext.h"
 #include "core/loader/CachedMetadata.h"
 #include "core/loader/cache/CachedScript.h"
+#include "core/platform/chromium/TraceEvent.h"
 
 namespace WebCore {
 
 PassOwnPtr<v8::ScriptData> V8ScriptRunner::precompileScript(v8::Handle<v8::String> code, CachedScript* cachedScript)
 {
+    TRACE_EVENT0("v8", "v8.compile");
     // A pseudo-randomly chosen ID used to store and retrieve V8 ScriptData from
     // the CachedScript. If the format changes, this ID should be changed too.
     static const unsigned dataTypeID = 0xECC13BD7;
@@ -63,6 +65,7 @@ PassOwnPtr<v8::ScriptData> V8ScriptRunner::precompileScript(v8::Handle<v8::Strin
 
 v8::Local<v8::Script> V8ScriptRunner::compileScript(v8::Handle<v8::String> code, const String& fileName, const TextPosition& scriptStartPosition, v8::ScriptData* scriptData, v8::Isolate* isolate)
 {
+    TRACE_EVENT0("v8", "v8.compile");
     v8::Handle<v8::String> name = v8String(fileName, isolate);
     v8::Handle<v8::Integer> line = v8Integer(scriptStartPosition.m_line.zeroBasedInt(), isolate);
     v8::Handle<v8::Integer> column = v8Integer(scriptStartPosition.m_column.zeroBasedInt(), isolate);
@@ -72,6 +75,7 @@ v8::Local<v8::Script> V8ScriptRunner::compileScript(v8::Handle<v8::String> code,
 
 v8::Local<v8::Value> V8ScriptRunner::runCompiledScript(v8::Handle<v8::Script> script, ScriptExecutionContext* context)
 {
+    TRACE_EVENT0("v8", "v8.run");
     if (script.IsEmpty())
         return v8::Local<v8::Value>();
 
@@ -101,6 +105,7 @@ v8::Local<v8::Value> V8ScriptRunner::runCompiledScript(v8::Handle<v8::Script> sc
 
 v8::Local<v8::Value> V8ScriptRunner::compileAndRunInternalScript(v8::Handle<v8::String> source, v8::Isolate* isolate, v8::Local<v8::Context> context, const String& fileName, const TextPosition& scriptStartPosition, v8::ScriptData* scriptData)
 {
+    TRACE_EVENT0("v8", "v8.run");
     v8::Local<v8::Value> result;
     if (context.IsEmpty())
         context = v8::Context::New(isolate);
