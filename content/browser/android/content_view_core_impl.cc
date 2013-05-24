5773d528110fbcc45457525386e068653be857ce
wonsik@chromium.org <wonsik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 21:33:36 +0000

    Fix TV build
    BUG=NONE
    R=yfriedman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15700006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202179 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_view_core_impl.cc
+++ content/browser/android/content_view_core_impl.cc
@@ -1289,8 +1289,11 @@ void ContentViewCoreImpl::AttachExternalVideoSurface(JNIEnv* env,
 #if defined(GOOGLE_TV)
   RenderViewHostImpl* rvhi = static_cast<RenderViewHostImpl*>(
       web_contents_->GetRenderViewHost());
-  if (rvhi && rvhi->media_player_manager()) {
-    rvhi->media_player_manager()->AttachExternalVideoSurface(
+  MediaPlayerManagerImpl* media_player_manager_impl =
+      rvhi ? static_cast<MediaPlayerManagerImpl*>(rvhi->media_player_manager())
+           : NULL;
+  if (media_player_manager_impl) {
+    media_player_manager_impl->AttachExternalVideoSurface(
         static_cast<int>(player_id), jsurface);
   }
 #endif
@@ -1302,8 +1305,11 @@ void ContentViewCoreImpl::DetachExternalVideoSurface(JNIEnv* env,
 #if defined(GOOGLE_TV)
   RenderViewHostImpl* rvhi = static_cast<RenderViewHostImpl*>(
       web_contents_->GetRenderViewHost());
-  if (rvhi && rvhi->media_player_manager()) {
-    rvhi->media_player_manager()->DetachExternalVideoSurface(
+  MediaPlayerManagerImpl* media_player_manager_impl =
+      rvhi ? static_cast<MediaPlayerManagerImpl*>(rvhi->media_player_manager())
+           : NULL;
+  if (media_player_manager_impl) {
+    media_player_manager_impl->DetachExternalVideoSurface(
         static_cast<int>(player_id));
   }
 #endif
