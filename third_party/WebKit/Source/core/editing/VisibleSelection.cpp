d4b6164e42e0a657b62b2d0fe14ebd204ec83ed4
cjhopman@chromium.org <cjhopman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 19:33:46 +0000

    WebFrame::selectRange should behave like moveCaret
    selectRange and moveCaret are used to alter the current selection by a
    user dragging an insertion/selection handle on Android and ChromeOS.
    There are two expected behaviors that this change fixes. First, if the
    current selection is in an editable element, the new selection will be
    within the same editable element. Second, the selection should stay
    horizontally aligned with the handle.
    
    Currently, selectRange doesn't even consider the second behavior and it
    sort of gets the first behavior by restricting the resulting selection
    to not cross editable boundaries and assuming that the focus point is
    within the current editable element. This is incredibly fragile.
    
    This extracts the logic used for determining selection when dragging the 
    mouse, selectionExtentRespectingEditingBoundary, from EventHandler.cpp so 
    that it can also be used for selectRange/moveCaret so that all these 
    behaviors can be consistent.
    
    To better reflect what these functions do (and that they are really
    paired together) this change introduces new names for these two
    functions: WebFrame::moveCaretSelection and WebFrame::moveRangeSelection.
    
    NOTRY=true
    BUG=232385,238329,238334
    
    Review URL: https://chromiumcodereview.appspot.com/15017002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151125 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/VisibleSelection.cpp
+++ third_party/WebKit/Source/core/editing/VisibleSelection.cpp
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/VisiblePosition.h"
 #include "core/editing/VisibleUnits.h"
 #include "core/editing/htmlediting.h"
+#include "core/platform/graphics/LayoutPoint.h"
+#include "core/rendering/RenderObject.h"
 #include <wtf/Assertions.h>
 #include <wtf/text/CString.h>
 #include <wtf/text/StringBuilder.h>
@@ -624,6 +626,27 @@ void VisibleSelection::adjustSelectionToAvoidCrossingEditingBoundaries()
     if (baseEditableAncestor != lowestEditableAncestor(m_extent.containerNode()))
         m_extent = m_baseIsFirst ? m_end : m_start;
 }
+
+VisiblePosition VisibleSelection::visiblePositionRespectingEditingBoundary(const LayoutPoint& localPoint, Node* targetNode) const
+{
+    if (!targetNode->renderer())
+        return VisiblePosition();
+
+    LayoutPoint selectionEndPoint = localPoint;
+    Element* editableElement = rootEditableElement();
+
+    if (editableElement && !editableElement->contains(targetNode)) {
+        if (!editableElement->renderer())
+            return VisiblePosition();
+
+        FloatPoint absolutePoint = targetNode->renderer()->localToAbsolute(FloatPoint(selectionEndPoint));
+        selectionEndPoint = roundedLayoutPoint(editableElement->renderer()->absoluteToLocal(absolutePoint));
+        targetNode = editableElement;
+    }
+
+    return targetNode->renderer()->positionForPoint(selectionEndPoint);
+}
+
 
 bool VisibleSelection::isContentEditable() const
 {
--- third_party/WebKit/Source/core/editing/VisibleSelection.h
+++ third_party/WebKit/Source/core/editing/VisibleSelection.h
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
+class LayoutPoint;
 class Position;
 
 const EAffinity SEL_DEFAULT_AFFINITY = DOWNSTREAM;
@@ -105,6 +106,9 @@ public:
     // Returns a shadow tree node for legacy shadow trees, a child of the
     // ShadowRoot node for new shadow trees, or 0 for non-shadow trees.
     Node* nonBoundaryShadowTreeRootNode() const;
+
+    VisiblePosition visiblePositionRespectingEditingBoundary(const LayoutPoint& localPoint, Node* targetNode) const;
+
 
 #ifndef NDEBUG
     void debugPosition() const;
