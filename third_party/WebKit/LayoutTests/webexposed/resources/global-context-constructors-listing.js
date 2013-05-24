1c23b3657335614f8e6ca091c48bbd7d318e9219
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-24 15:20:33 +0000

    Add webexposed/ layout test that documents all the global constructors in the Worker context
    This new layout test documents all the global constructors that are exposed on the
    Worker context.
    
    R=haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/15879005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151105 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/webexposed/resources/global-context-constructors-listing.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (this.importScripts)
+    importScripts('../../fast/js/resources/js-test-pre.js');
+
+function isConstructor(propertyName) {
+    var descriptor = Object.getOwnPropertyDescriptor(this, propertyName);
+    if (descriptor.value == undefined || descriptor.value.prototype == undefined)
+        return false;
+    return descriptor.writable && !descriptor.enumerable && descriptor.configurable;
+}
+
+var constructorNames = [];
+var propertyNames = Object.getOwnPropertyNames(this);
+for (var i = 0; i < propertyNames.length; i++) {
+    if (isConstructor(propertyNames[i]))
+        constructorNames[constructorNames.length] = propertyNames[i];
+}
+
+
+constructorNames.sort();
+for (var i = 0; i < constructorNames.length; i++)
+    debug(constructorNames[i]);
+
+if (isWorker())
+  finishJSTest();
