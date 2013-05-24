aa3f631d1da85b8828954dab4bc07938051e4f4e
nduca@chromium.org <nduca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 22:33:03 +0000

    Store picture parameters outside the b64-encoded data
    This will allow us to save skp files from js without having to round-trip
    through C++ to strip out the header data.
    
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/15784007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202203 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/picture.h
+++ cc/resources/picture.h
@@ -25,6 +25,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/skia/include/core/SkTileGridPicture.h"
 #include "ui/gfx/rect.h"
 
+namespace base {
+class Value;
+}
+
 namespace skia {
 class AnalysisCanvas;
 }
@@ -42,8 +46,7 @@ class CC_EXPORT Picture
   typedef base::hash_map<PixelRefMapKey, PixelRefs> PixelRefMap;
 
   static scoped_refptr<Picture> Create(gfx::Rect layer_rect);
-  static scoped_refptr<Picture> CreateFromBase64String(
-      const std::string& encoded_string);
+  static scoped_refptr<Picture> CreateFromValue(const base::Value* value);
 
   gfx::Rect LayerRect() const { return layer_rect_; }
   gfx::Rect OpaqueRect() const { return opaque_rect_; }
@@ -75,7 +78,7 @@ class CC_EXPORT Picture
               float contents_scale,
               bool enable_lcd_text);
 
-  void AsBase64String(std::string* output) const;
+  scoped_ptr<base::Value> AsValue() const;
 
   class CC_EXPORT PixelRefIterator {
    public:
@@ -112,7 +115,7 @@ class CC_EXPORT Picture
 
  private:
   explicit Picture(gfx::Rect layer_rect);
-  Picture(const std::string& encoded_string, bool* success);
+  Picture(const base::Value*, bool* success);
   // This constructor assumes SkPicture is already ref'd and transfers
   // ownership to this picture.
   Picture(const skia::RefPtr<SkPicture>&,
