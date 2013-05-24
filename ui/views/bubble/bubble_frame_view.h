d0f90c257fe587a8e62c9f598e0932f04e50c4da
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-24 07:06:19 +0000

    Add resize support for Views new style dialogs.
    Implements proper bubble non-client frame view hit testing.
    Use NonClientFrameView::GetHTComponentForFrame hit testing.
    (this is similar to the implementation of CustomFrameView)
    Remove the |can_drag_| flag, use WidgetDelegate::CanResize().
    
    BUG=166075
    TEST=New dialog style windows resize, drag, and generally allow mouse interaction as expected.
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15667004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202000 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/bubble/bubble_frame_view.h
+++ ui/views/bubble/bubble_frame_view.h
@@ -57,8 +57,6 @@ class VIEWS_EXPORT BubbleFrameView : public NonClientFrameView,
   void SetShowCloseButton(bool show);
   void SetTitlebarExtraView(View* view);
 
-  void set_can_drag(bool can_drag) { can_drag_ = can_drag; }
-
   // Given the size of the contents and the rect to point at, returns the bounds
   // of the bubble window. The bubble's arrow location may change if the bubble
   // does not fit on the monitor and |adjust_if_offscreen| is true.
@@ -98,9 +96,6 @@ class VIEWS_EXPORT BubbleFrameView : public NonClientFrameView,
   // When supplied, this view is placed in the titlebar between the title and
   // (x) close button.
   View* titlebar_extra_view_;
-
-  // A flag controlling the ability to drag this frame.
-  bool can_drag_;
 
   DISALLOW_COPY_AND_ASSIGN(BubbleFrameView);
 };
