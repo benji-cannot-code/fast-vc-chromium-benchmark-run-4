aaf50287a45f616556da5336516cf08f2d37bda6
dharcourt@chromium.org <dharcourt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-25 04:03:37 +0000

    Fixed style nit in Mac code so no one else has to deal with it.
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/15758004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202265 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.mm
+++ chrome/browser/ui/cocoa/extensions/native_app_window_cocoa.mm
@@ -78,7 +78,7 @@ enum {
     appWindow_->WindowDidDeminiaturize();
 }
 
-- (BOOL)windowShouldZoom:(NSWindow *)window
+- (BOOL)windowShouldZoom:(NSWindow*)window
                  toFrame:(NSRect)newFrame {
   if (appWindow_)
     appWindow_->WindowWillZoom();
