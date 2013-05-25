9930d65cfdc23de4c4296dae97e8a00b3fe80dea
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-25 17:42:23 +0000

    Destroy all Singletons and LazyInstances between each test.
    This modifies base::TestSuite to add a test listener that runs the AtExitManager
    between each test.
    
    BUG=110594, 156433, 238654
    TEST=*unit*tests
    R=apatrick@chromium.org, gspencer@chromium.org, phajdan.jr@chromium.org, pneubeck@chromium.org, rsleevi@chromium.org
    
    Review URL: https://codereview.chromium.org/8947021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202305 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/onc/onc_certificate_importer_unittest.cc
+++ chromeos/network/onc/onc_certificate_importer_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/string_number_conversions.h"
+#include "base/threading/platform_thread.h"
 #include "base/values.h"
 #include "chromeos/network/onc/onc_constants.h"
 #include "chromeos/network/onc/onc_test_utils.h"
@@ -81,6 +82,10 @@ class ONCCertificateImporterTest : public testing::Test {
   void AddCertificatesFromFile(
       std::string filename,
       CertificateImporter::ParseResult expected_parse_result) {
+    // This 1 second sleep is necessary because NSS has a caching bug. This
+    // can be removed once http://crbug.com/238654 is fixed.
+    base::PlatformThread::Sleep(base::TimeDelta::FromSeconds(1));
+
     scoped_ptr<base::DictionaryValue> onc =
         test_utils::ReadTestDictionary(filename);
     base::Value* certificates_value = NULL;
