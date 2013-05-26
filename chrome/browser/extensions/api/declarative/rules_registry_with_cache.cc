14a3d9faef65bf4290e535cbeebdaa1ae60b76ad
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-26 03:24:51 +0000

    Revert 202152 "RulesRegistryWithCache::ProcessChangedRules speed-up"
    > RulesRegistryWithCache::ProcessChangedRules speed-up
    >
    > Postpone rules->value conversion:
    > Currently the rules are converted to base::Value on the rules registry thread before ProcessChangedRules finishes, and then a task is posted to UI which will trigger writing that to a rule store on yet another thread.
    > This CL postpones the conversion to the task running on UI thread.
    >
    > This change cuts down the running time for AddRules to 75%, but of course this is cheating, since the work only gets done later. The point is, however, that network requests are not blocked until the rules are written.
    >
    > BUG=236368
    >
    > Review URL: https://chromiumcodereview.appspot.com/14851011
    
    TBR=vabr@chromium.org
    
    Review URL: https://codereview.chromium.org/15647003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202342 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/declarative/rules_registry_with_cache.cc
+++ chrome/browser/extensions/api/declarative/rules_registry_with_cache.cc
@@ -45,7 +45,6 @@ std::vector<linked_ptr<extensions::RulesRegistry::Rule> > RulesFromValue(
   if (!value || !value->GetAsList(&list))
     return rules;
 
-  rules.reserve(list->GetSize());
   for (size_t i = 0; i < list->GetSize(); ++i) {
     const base::DictionaryValue* dict = NULL;
     if (!list->GetDictionary(i, &dict))
@@ -82,7 +81,7 @@ RulesRegistryWithCache::RulesRegistryWithCache(
     bool log_storage_init_delay,
     scoped_ptr<RuleStorageOnUI>* ui_part)
     : RulesRegistry(owner_thread, event_name),
-      weak_ptr_factory_(this),
+      weak_ptr_factory_((profile) ? this : NULL),
       storage_on_ui_((profile
                           ? (new RuleStorageOnUI(profile,
                                                  GetDeclarativeRuleStorageKey(
@@ -92,8 +91,7 @@ RulesRegistryWithCache::RulesRegistryWithCache(
                                                  weak_ptr_factory_.GetWeakPtr(),
                                                  log_storage_init_delay))
                                 ->GetWeakPtr()
-                          : base::WeakPtr<RuleStorageOnUI>())),
-      process_changed_rules_requested_(false) {
+                          : base::WeakPtr<RuleStorageOnUI>())) {
   if (!profile) {
     CHECK(!ui_part);
     return;
@@ -134,13 +132,7 @@ std::string RulesRegistryWithCache::AddRules(
     rules_[key] = *i;
   }
 
-  if (!process_changed_rules_requested_) {
-    process_changed_rules_requested_ = true;
-    ready_.Post(FROM_HERE,
-                base::Bind(&RulesRegistryWithCache::ProcessChangedRules,
-                           weak_ptr_factory_.GetWeakPtr(),
-                           extension_id));
-  }
+  ProcessChangedRules(extension_id);
   return kSuccess;
 }
 
@@ -161,13 +153,7 @@ std::string RulesRegistryWithCache::RemoveRules(
     rules_.erase(lookup_key);
   }
 
-  if (!process_changed_rules_requested_) {
-    process_changed_rules_requested_ = true;
-    ready_.Post(FROM_HERE,
-                base::Bind(&RulesRegistryWithCache::ProcessChangedRules,
-                           weak_ptr_factory_.GetWeakPtr(),
-                           extension_id));
-  }
+  ProcessChangedRules(extension_id);
   return kSuccess;
 }
 
@@ -189,13 +175,7 @@ std::string RulesRegistryWithCache::RemoveAllRules(
       rules_.erase(key);
   }
 
-  if (!process_changed_rules_requested_) {
-    process_changed_rules_requested_ = true;
-    ready_.Post(FROM_HERE,
-                base::Bind(&RulesRegistryWithCache::ProcessChangedRules,
-                           weak_ptr_factory_.GetWeakPtr(),
-                           extension_id));
-  }
+  ProcessChangedRules(extension_id);
   return kSuccess;
 }
 
@@ -262,8 +242,6 @@ void RulesRegistryWithCache::DeserializeAndAddRules(
 void RulesRegistryWithCache::ProcessChangedRules(
     const std::string& extension_id) {
   DCHECK(content::BrowserThread::CurrentlyOn(owner_thread()));
-
-  process_changed_rules_requested_ = false;
 
   std::vector<linked_ptr<RulesRegistry::Rule> > new_rules;
   std::string error = GetAllRules(extension_id, &new_rules);
