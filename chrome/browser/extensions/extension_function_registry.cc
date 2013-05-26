98f963c867153cfa5a426b0da20be0f58f2c77b8
patrickriordan177@gmail.com <patrickriordan177@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-26 02:03:40 +0000

    Implement chrome.runtime.setUninstallUrl. Continued from https://codereview.chromium.org/10825265/
    BUG=84556
    
    Review URL: https://chromiumcodereview.appspot.com/14059004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_registry.cc
+++ chrome/browser/extensions/extension_function_registry.cc
@@ -77,6 +77,7 @@ void ExtensionFunctionRegistry::ResetFunctions() {
 
   // Runtime
   RegisterFunction<extensions::RuntimeGetBackgroundPageFunction>();
+  RegisterFunction<extensions::RuntimeSetUninstallUrlFunction>();
   RegisterFunction<extensions::RuntimeReloadFunction>();
   RegisterFunction<extensions::RuntimeRequestUpdateCheckFunction>();
 
