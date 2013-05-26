791f2fec4443987f225022f21b7960c4d1992dfe
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-26 02:25:15 +0000

    cros: Exclude user has synced app pins from default pin trial.
    - Add helper methods to PrefServiceSyncable to figure out whether a pref
      is pulled from sync;
    - Use the helper to exclude users that has synced app pins from trial;
    
    BUG=234415
    
    Review URL: https://chromiumcodereview.appspot.com/15911003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202332 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/default_pinned_apps_field_trial.cc
+++ chrome/browser/chromeos/login/default_pinned_apps_field_trial.cc
@@ -14,10 +14,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/prefs/pref_service.h"
 #include "base/values.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/chromeos/login/user_manager.h"
+#include "chrome/browser/prefs/pref_service_syncable.h"
 #include "chrome/browser/prefs/scoped_user_pref_update.h"
+#include "chrome/browser/profiles/profile.h"
+#include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/ash/chrome_launcher_prefs.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/extension_constants.h"
+#include "chrome/common/pref_names.h"
 
 namespace chromeos {
 namespace default_pinned_apps_field_trial {
@@ -57,6 +62,12 @@ void AddUserToExperiment(const std::string& username) {
   users->AppendString(username);
 }
 
+// Removes user from the experiment user list.
+void RemoveUserFromExperiment(const std::string& username) {
+  ListPrefUpdate users(g_browser_process->local_state(), kExperimentUsers);
+  users->Remove(base::StringValue(username), NULL);
+}
+
 // Gets click target type for given app id. Returns false if the app id is
 // not interesting.
 bool GetClickTargetForApp(const std::string& app_id,
@@ -87,6 +98,24 @@ bool GetClickTargetForApp(const std::string& app_id,
   }
 
   return false;
+}
+
+// Check if the current user has default pinned apps pulled from sync. If that
+// is the case, kick the user out of the trial and returns true.
+bool ProcessIfUserHasSyncedAppPins() {
+  static bool has_synced_pref = false;
+  if (has_synced_pref)
+    return true;
+
+  Profile* user_profile = ProfileManager::GetDefaultProfile();
+  has_synced_pref = PrefServiceSyncable::FromProfile(user_profile)
+      ->IsPrefSynced(prefs::kPinnedLauncherApps);
+  if (has_synced_pref) {
+    const std::string username = UserManager::Get()->GetActiveUser()->email();
+    RemoveUserFromExperiment(username);
+  }
+
+  return has_synced_pref;
 }
 
 }  // namespace
@@ -154,6 +183,9 @@ void RecordShelfClick(ClickTarget click_target) {
   // The experiment does not include certain user types, such as public account,
   // retail mode user, local user and ephemeral user.
   if (!trial_configured)
+    return;
+
+  if (ProcessIfUserHasSyncedAppPins())
     return;
 
   UMA_HISTOGRAM_ENUMERATION("Cros.ClickOnShelf",
