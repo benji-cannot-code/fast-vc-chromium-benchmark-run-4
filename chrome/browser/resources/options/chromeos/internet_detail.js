568011fe1a9dc9b61024357932be4c2a178b6d1e
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-27 22:03:56 +0000

    Fix incorrect loadTimeData checks in internet_details.js.
    BUG=241840
    
    Review URL: https://chromiumcodereview.appspot.com/15997006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/internet_detail.js
+++ chrome/browser/resources/options/chromeos/internet_detail.js
@@ -289,7 +289,7 @@ cr.define('options.internet', function() {
       // stored) is not accessible from the login screen.
       // TODO(pneubeck): Remove this once i18n of the proxy dialog on the login
       // page is fixed. http://crbug.com/242865
-      if (loadTimeData.data) {
+      if (loadTimeData.data_) {
         $('google-dns-label').innerHTML =
           loadTimeData.getString('googleNameServers');
       }
@@ -406,7 +406,7 @@ cr.define('options.internet', function() {
      */
     updateProxyBannerVisibility_: function() {
       var bannerDiv = $('network-proxy-info-banner');
-      if (!loadTimeData.data) {
+      if (!loadTimeData.data_) {
         // TODO(pneubeck): This temporarily prevents an exception below until
         // i18n of the proxy dialog on the login page is
         // fixed. http://crbug.com/242865
