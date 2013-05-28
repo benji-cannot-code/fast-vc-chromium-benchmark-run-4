d7342989753af40b23a63f6119c3199cc1008495
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 22:27:27 +0000

    Fix MediaPlayerDelegate error handling so it passes the webkitmediasource-errors.html LayoutTest.
    BUG=233420
    TEST=http/tests/media/media-source/webkitmediasource-errors.html LayoutTests
          now passes w/o crashing.
    
    Review URL: https://chromiumcodereview.appspot.com/15927003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202671 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/media/webmediaplayer_util.h
+++ webkit/media/webmediaplayer_util.h
@@ -7,7 +7,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WEBKIT_MEDIA_WEBMEDIAPLAYER_UTIL_H_
 
 #include "base/time.h"
+#include "media/base/pipeline_status.h"
 #include "media/base/ranges.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebMediaPlayer.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebTimeRange.h"
 
 namespace webkit_media {
@@ -20,6 +22,9 @@ base::TimeDelta ConvertSecondsToTimestamp(double seconds);
 
 WebKit::WebTimeRanges ConvertToWebTimeRanges(
     const media::Ranges<base::TimeDelta>& ranges);
+
+WebKit::WebMediaPlayer::NetworkState PipelineErrorToNetworkState(
+    media::PipelineStatus error);
 
 }  // namespace webkit_media
 
