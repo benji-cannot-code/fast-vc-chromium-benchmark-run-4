e3436b16e1f47d565d7a28ad678649bcf849880d
vapier@chromium.org <vapier@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 22:09:03 +0000

    drop unused execinfo.h header
    The execinfo.h header only provides prototypes for backtrace funcs.
    These two files don't use those funcs, so the include is unused.
    Punt it to make building on non-glibc systems easier.
    
    BUG=None
    TEST=trybot for linux/nacl/cros work
    
    Review URL: https://chromiumcodereview.appspot.com/16120002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202666 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/debugger_posix.cc
+++ base/debug/debugger_posix.cc
@@ -18,10 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
-#if !defined(OS_ANDROID) && !defined(OS_NACL)
-#include <execinfo.h>
-#endif
-
 #if defined(__GLIBCXX__)
 #include <cxxabi.h>
 #endif
--- base/sys_info_chromeos.cc
+++ base/sys_info_chromeos.cc
@@ -14,8 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_tokenizer.h"
 #include "base/threading/thread_restrictions.h"
 
-#include <execinfo.h>
-
 namespace base {
 
 static const char* kLinuxStandardBaseVersionKeys[] = {
