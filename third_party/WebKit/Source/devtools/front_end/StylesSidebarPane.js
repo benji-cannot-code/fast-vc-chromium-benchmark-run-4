7db391e69a9a9ea00d221ac0420262b136eb70ae
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 11:38:26 +0000

    DevTools: Get rid of the resource binding in CSSStyleModel.
    This patch removes resource binding form CSSStyleModel now that we've migrated
    all the mappings to the styleSheetHeader and do not depend on resources anymore.
    NetworkUISourceCodeProvider is now creating uiSourceCodes for inspector stylesheets
    based on the headers with appropriate origin instead of doing this based on pseudo-resources.
    
    Review URL: https://chromiumcodereview.appspot.com/15941008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151242 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/StylesSidebarPane.js
+++ third_party/WebKit/Source/devtools/front_end/StylesSidebarPane.js
@@ -398,7 +398,7 @@ WebInspector.StylesSidebarPane.prototype = {
             // Add rules in reverse order to match the cascade order.
             for (var j = pseudoElementCSSRules.rules.length - 1; j >= 0; --j) {
                 var rule = pseudoElementCSSRules.rules[j];
-                styleRules.push({ style: rule.style, selectorText: rule.selectorText, media: rule.media, sourceURL: rule.sourceURL, rule: rule, editable: !!(rule.style && rule.style.id) });
+                styleRules.push({ style: rule.style, selectorText: rule.selectorText, media: rule.media, sourceURL: rule.resourceURL(), rule: rule, editable: !!(rule.style && rule.style.id) });
             }
             usedProperties = {};
             this._markUsedProperties(styleRules, usedProperties);
@@ -466,7 +466,7 @@ WebInspector.StylesSidebarPane.prototype = {
                 addedAttributesStyle = true;
                 addAttributesStyle();
             }
-            styleRules.push({ style: rule.style, selectorText: rule.selectorText, media: rule.media, sourceURL: rule.sourceURL, rule: rule, editable: !!(rule.style && rule.style.id) });
+            styleRules.push({ style: rule.style, selectorText: rule.selectorText, media: rule.media, sourceURL: rule.resourceURL(), rule: rule, editable: !!(rule.style && rule.style.id) });
         }
 
         if (!addedAttributesStyle)
@@ -508,7 +508,7 @@ WebInspector.StylesSidebarPane.prototype = {
                     insertInheritedNodeSeparator(parentNode);
                     separatorInserted = true;
                 }
-                styleRules.push({ style: rule.style, selectorText: rule.selectorText, media: rule.media, sourceURL: rule.sourceURL, rule: rule, isInherited: true, parentNode: parentNode, editable: !!(rule.style && rule.style.id) });
+                styleRules.push({ style: rule.style, selectorText: rule.selectorText, media: rule.media, sourceURL: rule.resourceURL(), rule: rule, isInherited: true, parentNode: parentNode, editable: !!(rule.style && rule.style.id) });
             }
             parentNode = parentNode.parentNode;
         }
@@ -940,7 +940,7 @@ WebInspector.StylePropertiesSection = function(parentPane, styleRule, editable, 
         else {
             // Check this is a real CSSRule, not a bogus object coming from WebInspector.BlankStylePropertiesSection.
             if (this.rule.id)
-                this.navigable = this.rule.isSourceNavigable();
+                this.navigable = !!this.rule.resourceURL();
         }
         this.titleElement.addStyleClass("styles-selector");
     }
@@ -1244,15 +1244,8 @@ WebInspector.StylePropertiesSection.prototype = {
             return document.createTextNode(WebInspector.UIString("user agent stylesheet"));
         if (this.rule.isUser)
             return document.createTextNode(WebInspector.UIString("user stylesheet"));
-        if (this.rule.isViaInspector) {
-            var element = document.createElement("span");
-            var resource = WebInspector.cssModel.viaInspectorResourceForRule(this.rule);
-            if (resource)
-                element.appendChild(linkifyUncopyable(resource.url, this.rule.lineNumberInSource()));
-            else
-                element.textContent = WebInspector.UIString("via inspector");
-            return element;
-        }
+        if (this.rule.isViaInspector)
+            return document.createTextNode(WebInspector.UIString("via inspector"));
         return document.createTextNode("");
     },
 
@@ -1367,7 +1360,7 @@ WebInspector.StylePropertiesSection.prototype = {
             }
 
             this.rule = newRule;
-            this.styleRule = { section: this, style: newRule.style, selectorText: newRule.selectorText, media: newRule.media, sourceURL: newRule.sourceURL, rule: newRule };
+            this.styleRule = { section: this, style: newRule.style, selectorText: newRule.selectorText, media: newRule.media, sourceURL: newRule.resourceURL(), rule: newRule };
 
             this._parentPane.update(selectedNode);
 
@@ -1546,7 +1539,7 @@ WebInspector.BlankStylePropertiesSection.prototype = {
 
         function successCallback(newRule, doesSelectorAffectSelectedNode)
         {
-            var styleRule = { section: this, style: newRule.style, selectorText: newRule.selectorText, sourceURL: newRule.sourceURL, rule: newRule };
+            var styleRule = { section: this, style: newRule.style, selectorText: newRule.selectorText, sourceURL: newRule.resourceURL(), rule: newRule };
             this.makeNormal(styleRule);
 
             if (!doesSelectorAffectSelectedNode) {
