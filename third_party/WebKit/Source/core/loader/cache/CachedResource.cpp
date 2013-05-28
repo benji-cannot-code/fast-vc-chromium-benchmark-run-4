e8a762808027f5d5affbf24572a6a3364f6a7c42
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 23:48:32 +0000

    Make revalidation handling internal to CachedResource.
    Also, move some ASSERTs from ResourceLoader to CachedResource that are
    asserting CachedResource state.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/15911002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151331 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/cache/CachedResource.cpp
+++ third_party/WebKit/Source/core/loader/cache/CachedResource.cpp
@@ -267,6 +267,8 @@ void CachedResource::checkNotify()
 
 void CachedResource::appendData(const char* data, int length)
 {
+    ASSERT(!m_resourceToRevalidate);
+    ASSERT(!errorOccurred());
     if (m_options.dataBufferingPolicy == DoNotBufferData)
         return;
     if (m_data)
@@ -278,6 +280,9 @@ void CachedResource::appendData(const char* data, int length)
 
 void CachedResource::error(CachedResource::Status status)
 {
+    if (m_resourceToRevalidate)
+        revalidationFailed();
+
     setStatus(status);
     ASSERT(errorOccurred());
     m_data.clear();
@@ -292,8 +297,11 @@ void CachedResource::finishOnePart()
     checkNotify();
 }
 
-void CachedResource::finish()
+void CachedResource::finish(double finishTime)
 {
+    ASSERT(!m_resourceToRevalidate);
+    ASSERT(!errorOccurred());
+    m_loadFinishTime = finishTime;
     finishOnePart();
     if (!errorOccurred())
         m_status = Cached;
@@ -359,6 +367,13 @@ void CachedResource::responseReceived(const ResourceResponse& response)
     String encoding = response.textEncodingName();
     if (!encoding.isNull())
         setEncoding(encoding);
+
+    if (!m_resourceToRevalidate)
+        return;
+    if (response.httpStatusCode() == 304)
+        revalidationSucceeded(response);
+    else
+        revalidationFailed();
 }
 
 void CachedResource::setSerializedCachedMetadata(const char* data, size_t size)
@@ -367,6 +382,7 @@ void CachedResource::setSerializedCachedMetadata(const char* data, size_t size)
     // If this triggers, it indicates an efficiency problem which is most
     // likely unexpected in code designed to improve performance.
     ASSERT(!m_cachedMetadata);
+    ASSERT(!m_resourceToRevalidate);
 
     m_cachedMetadata = CachedMetadata::deserialize(data, size);
 }
--- third_party/WebKit/Source/core/loader/cache/CachedResource.h
+++ third_party/WebKit/Source/core/loader/cache/CachedResource.h
@@ -157,7 +157,7 @@ public:
 
     // Computes the status of an object after loading.  
     // Updates the expire date on the cache entry file
-    void finish();
+    void finish(double finishTime = 0.0);
 
     // FIXME: Remove the stringless variant once all the callsites' error messages are updated.
     bool passesAccessControlCheck(SecurityOrigin*);
@@ -222,6 +222,7 @@ public:
     bool mustRevalidateDueToCacheHeaders(CachePolicy) const;
     bool isCacheValidator() const { return m_resourceToRevalidate; }
     CachedResource* resourceToRevalidate() const { return m_resourceToRevalidate; }
+    void setResourceToRevalidate(CachedResource*);
     
     bool isPurgeable() const;
     bool wasPurged() const;
@@ -231,15 +232,9 @@ public:
     // better way to handle the archive case.
     bool makePurgeable(bool purgeable);
     
-    // HTTP revalidation support methods for CachedResourceLoader.
-    void setResourceToRevalidate(CachedResource*);
-    void revalidationSucceeded(const ResourceResponse&);
-    void revalidationFailed();
-    
     virtual void didSendData(unsigned long long /* bytesSent */, unsigned long long /* totalBytesToBeSent */) { }
     virtual void didDownloadData(int) { }
 
-    void setLoadFinishTime(double finishTime) { m_loadFinishTime = finishTime; }
     double loadFinishTime() const { return m_loadFinishTime; }
 
     virtual void reportMemoryUsage(MemoryObjectInfo*) const;
@@ -294,6 +289,9 @@ protected:
 private:
     bool addClientToSet(CachedResourceClient*);
     void decodedDataDeletionTimerFired(Timer<CachedResource>*);
+
+    void revalidationSucceeded(const ResourceResponse&);
+    void revalidationFailed();
 
     virtual PurgePriority purgePriority() const { return PurgeDefault; }
 
