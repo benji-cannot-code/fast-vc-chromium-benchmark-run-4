4c2253e691427ff681cb03cd37b05390adaf829c
caitkp@google.com <caitkp@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 18:52:34 +0000

    Re-disable flaky android InsertionHandle tests
    TBR=dharcourt@chromium.org
    BUG=169648
    
    Review URL: https://codereview.chromium.org/16165003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202614 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/browser/input/InsertionHandleTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/InsertionHandleTest.java
@@ -13,6 +13,7 @@ import android.test.suitebuilder.annotation.MediumTest;
 import android.text.Editable;
 import android.text.Selection;
 
+import org.chromium.base.test.util.DisabledTest;
 import org.chromium.base.test.util.Feature;
 import org.chromium.base.test.util.UrlUtils;
 import org.chromium.content.browser.ContentView;
@@ -96,6 +97,7 @@ public class InsertionHandleTest extends ContentShellTestBase {
         assertTrue(waitForHandleShowingEquals(false));
     }
 
+
     @MediumTest
     @Feature({"TextSelection", "TextInput", "Main"})
     public void testDragInsertionHandle() throws Throwable {
@@ -114,6 +116,7 @@ public class InsertionHandleTest extends ContentShellTestBase {
         dragHandleTo(dragToX, dragToY);
         assertWaitForHandleNear(dragToX, dragToY);
     }
+
 
     @MediumTest
     @Feature({"TextSelection", "TextInput", "Main"})
@@ -134,8 +137,12 @@ public class InsertionHandleTest extends ContentShellTestBase {
         assertTrue(waitForHandleShowingEquals(false));
     }
 
-    @MediumTest
-    @Feature({"TextSelection", "TextInput", "Main"})
+    /**
+     * @MediumTest
+     * @Feature({"TextSelection", "TextInput", "Main"})
+     * http://crbug.com/169648
+     */
+    @DisabledTest
     public void testDragInsertionHandleInputText() throws Throwable {
         launchWithUrl(INPUT_TEXT_DATA_URL);
 
@@ -161,8 +168,12 @@ public class InsertionHandleTest extends ContentShellTestBase {
         assertWaitForHandleNear(initialX, initialY);
     }
 
-    @MediumTest
-    @Feature({"TextSelection", "TextInput", "Main"})
+    /**
+     * @MediumTest
+     * @Feature({"TextSelection", "TextInput", "Main"})
+     * http://crbug.com/169648
+     */
+    @DisabledTest
     public void testDragInsertionHandleInputTextOutsideBounds() throws Throwable {
         launchWithUrl(INPUT_TEXT_DATA_URL);
 
