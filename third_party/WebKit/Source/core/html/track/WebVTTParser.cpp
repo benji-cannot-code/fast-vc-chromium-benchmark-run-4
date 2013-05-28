c6a213ec96582e82833308a1127b41a177a29dc1
caitpotter88@gmail.com <caitpotter88@gmail.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 12:16:26 +0000

    [WebVTT] HTML5 "space" characters around "-->" are not required
    Make whitespace around cue-timings separator optional, in order to comply with WebVTT draft: 
    
    From http://dev.w3.org/html5/webvtt/#dfn-collect-webvtt-cue-timings-and-settings 
    
    ``` 
    4. Collect a WebVTT timestamp. If that algorithm fails, then abort these steps and return failure. Otherwise, let cue's text track cue start time be the collected time. 
    
    5. Skip whitespace. 
    
    6. If the character at position is not a U+002D HYPHEN-MINUS character (-) then abort these steps and return failure. Otherwise, move position forwards one character. 
    
    7. If the character at position is not a U+002D HYPHEN-MINUS character (-) then abort these steps and return failure. Otherwise, move position forwards one character. 
    
    8. If the character at position is not a U+003E GREATER-THAN SIGN character (>) then abort these steps and return failure. Otherwise, move position forwards one character. 
    
    9. Skip whitespace. 
    
    10. Collect a WebVTT timestamp. If that algorithm fails, then abort these steps and return failure. Otherwise, let cue's text track cue end time be the collected time. 
    
    ``` 
    
    "Skip whitespace" refers to terms defined in the HTML standard. http://www.w3.org/html/wg/drafts/html/master/single-page.html#skip-whitespace 
    
    ``` 
    The step skip whitespace means that the user agent must collect a sequence of characters that are space characters. The step skip White_Space characters means that the user agent must collect a sequence of characters that are White_Space characters. In both cases, the collected characters are not used. 
    ``` 
    
    The step "collect a sequence of characters" does not forbid the sequence from being empty, and as such, the whitespace is in fact optional. 
    
    Additionally, the skip whitespace step is not limited to skipping SPACE U+0020 and TAB U+0009 characters, but also includes FORM FEED, LINE FEED and CARRIAGE RETURN (Although, in practice, CR/LF should not be encountered in this section). 
    
    This patch removes the expected '\t' || ' ', a rule which seems to have been inspired by the "SYNTAX" section of the WebVTT spec, which I am told by editors is meant to be ignored by parser implementors, as silly as that is. 
    
    https://code.google.com/p/chromium/issues/detail?id=242158
    
    Review URL: https://chromiumcodereview.appspot.com/15651003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151250 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/track/WebVTTParser.cpp
+++ third_party/WebKit/Source/core/html/track/WebVTTParser.cpp
@@ -278,9 +278,7 @@ WebVTTParser::ParseState WebVTTParser::collectTimingsAndSettings(const String& l
         return BadCue;
     if (position >= line.length())
         return BadCue;
-    char nextChar = line[position++];
-    if (nextChar != ' ' && nextChar != '\t')
-        return BadCue;
+
     skipWhiteSpace(line, &position);
 
     // 6-9 - If the next three characters are not "-->", abort and return failure.
@@ -289,9 +287,7 @@ WebVTTParser::ParseState WebVTTParser::collectTimingsAndSettings(const String& l
     position += 3;
     if (position >= line.length())
         return BadCue;
-    nextChar = line[position++];
-    if (nextChar != ' ' && nextChar != '\t')
-        return BadCue;
+
     skipWhiteSpace(line, &position);
 
     // 10-11 - Collect a WebVTT timestamp. If that fails, then abort and return failure. Otherwise, let cue's text track cue end time be the collected time.
