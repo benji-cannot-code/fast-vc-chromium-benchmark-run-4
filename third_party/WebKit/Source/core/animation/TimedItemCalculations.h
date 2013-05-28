cba131ec5a481fe7b39f9ce375a7768f864764a0
steveblock@chromium.org <steveblock@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 06:39:46 +0000

    Hook up timing functions to Web Animations model
    BUG=240653
    R=dstockwell
    
    Review URL: https://chromiumcodereview.appspot.com/15695002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151213 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TimedItemCalculations.h
+++ third_party/WebKit/Source/core/animation/TimedItemCalculations.h
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/animation/TimedItem.h"
 #include "core/animation/Timing.h"
+#include "core/platform/animation/AnimationUtilities.h"
 #include "wtf/MathExtras.h"
 
 namespace WebCore {
@@ -150,8 +151,9 @@ static inline double calculateTransformedTime(double currentIteration, double it
     ASSERT(isNull(iterationTime) || (iterationTime >= 0 && iterationTime <= iterationDuration));
 
     double directedTime = calculateDirectedTime(currentIteration, iterationDuration, iterationTime, specified);
-    ASSERT(!specified.timingFunction); // FIXME: evaluate timing functions
-    return directedTime;
+    return specified.timingFunction ?
+        iterationDuration * specified.timingFunction->evaluate(directedTime / iterationDuration, accuracyForDuration(iterationDuration)) :
+        directedTime;
 }
 
 } // namespace WebCore
