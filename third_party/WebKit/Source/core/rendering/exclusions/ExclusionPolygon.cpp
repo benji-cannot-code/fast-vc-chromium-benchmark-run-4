47f26171bc0212d07fcf0b9d6e071254ed1f31da
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 22:44:25 +0000

    First round of changes to correct exclusion tests that started failing when subpixel layout was turned off, and to correct some problems that disabling subpixel layout exposed.
    This set of changes just corrects four tests. It also revises the subpixel-utils code to bring it into line with the most recent LayoutUnit et al changes and to simplify its use a little. The ExclusionPolygon appendArc() method was corrected, so that the 3rd of 5 additional vertices is in the center of the arc. The firstIncludedIntervalLogicalTop() methods now use ceiledLayoutUnit() to convert a float to a LayoutUnit within the shape.
    
    BUG=237568
    R=leviw@chromium.org
    
    Review URL: https://codereview.chromium.org/14959014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151323 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/exclusions/ExclusionPolygon.cpp
+++ third_party/WebKit/Source/core/rendering/exclusions/ExclusionPolygon.cpp
@@ -32,8 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/rendering/exclusions/ExclusionPolygon.h"
 
 #include "core/platform/graphics/LayoutPoint.h"
-
-#include <wtf/MathExtras.h>
+#include "wtf/MathExtras.h"
 
 namespace WebCore {
 
@@ -121,12 +120,12 @@ static inline void appendArc(Vector<FloatPoint>& vertices, const FloatPoint& arc
     if (endAngle < 0)
         endAngle += twoPI;
     float angle = (startAngle > endAngle) ? (startAngle - endAngle) : (startAngle + twoPI - endAngle);
-    const float arcSegmentCount = 5; // An odd number so that one arc vertex will be eactly arcRadius from arcCenter.
-    float angle5 =  ((padding) ? -angle : twoPI - angle) / arcSegmentCount;
+    const float arcSegmentCount = 6; // An even number so that one arc vertex will be eactly arcRadius from arcCenter.
+    float arcSegmentAngle =  ((padding) ? -angle : twoPI - angle) / arcSegmentCount;
 
     vertices.append(startArcVertex);
     for (unsigned i = 1; i < arcSegmentCount; ++i) {
-        float angle = startAngle + angle5 * i;
+        float angle = startAngle + arcSegmentAngle * i;
         vertices.append(arcCenter + FloatPoint(cos(angle) * arcRadius, sin(angle) * arcRadius));
     }
     vertices.append(endArcVertex);
