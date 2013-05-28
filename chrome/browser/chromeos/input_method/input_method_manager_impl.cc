6aa7e3b34c54dae53c7bee6d7551e1626dcdee4d
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 18:31:40 +0000

    Reset pending input method id.
    With this change, the responsibility of clearing |pending_input_method_| is
    moved to ChangeInputMethodInternal.
    
    BUG=243149
    TEST=Manually checked on link device.
    
    Review URL: https://chromiumcodereview.appspot.com/15954008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202607 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/input_method_manager_impl.cc
+++ chrome/browser/chromeos/input_method/input_method_manager_impl.cc
@@ -310,6 +310,7 @@ bool InputMethodManagerImpl::ChangeInputMethodInternal(
     return false;
   }
 
+  pending_input_method_.clear();
   IBusInputContextClient* input_context =
       chromeos::DBusThreadManager::Get()->GetIBusInputContextClient();
   const std::string current_input_method_id = current_input_method_.id();
@@ -402,10 +403,8 @@ void InputMethodManagerImpl::OnComponentExtensionInitialized(
 
   LoadNecessaryComponentExtensions();
 
-  if (!pending_input_method_.empty()) {
-    if (ChangeInputMethodInternal(pending_input_method_, false))
-      pending_input_method_.clear();
-  }
+  if (!pending_input_method_.empty())
+    ChangeInputMethodInternal(pending_input_method_, false);
 
 }
 
@@ -723,10 +722,8 @@ void InputMethodManagerImpl::OnConnected() {
     }
   }
 
-  if (!pending_input_method_.empty()) {
-    if (ChangeInputMethodInternal(pending_input_method_, false))
-      pending_input_method_.clear();
-  }
+  if (!pending_input_method_.empty())
+    ChangeInputMethodInternal(pending_input_method_, false);
 }
 
 void InputMethodManagerImpl::OnDisconnected() {
