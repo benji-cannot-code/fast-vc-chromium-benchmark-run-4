17c29cb4cd1ae3cb757ee155bf773321297aef12
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 08:07:58 +0000

    Remove unnecessary frameBufferAtIndex argument
    Apply blink style [names-variable-name-in-function-decl] rule.
    
    TEST=Compiles
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/15725004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.h
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.h
@@ -46,7 +46,7 @@ public:
 
     virtual String filenameExtension() const { return "webp"; }
     virtual bool isSizeAvailable();
-    virtual ImageFrame* frameBufferAtIndex(size_t index);
+    virtual ImageFrame* frameBufferAtIndex(size_t);
 
 private:
     bool decode(bool onlySize);
