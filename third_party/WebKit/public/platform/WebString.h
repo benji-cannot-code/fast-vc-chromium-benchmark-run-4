ecda9fb104ee49ebc92f071ca661c0f78bba5a49
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 17:14:31 +0000

    Reduce usage of WebString::data
    WebString::data forces a widening of the underlying String from 8 bits to 16
    bits. We should remove this function because most callers of the API don't
    understand these implementation details of WebString. In order to remove the
    API, we need to remove all the callers. This CL removes all the callers I could
    find in Blink. The next CL will sweep across Chromium.
    
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/15806014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151268 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebString.h
+++ third_party/WebKit/public/platform/WebString.h
@@ -79,6 +79,9 @@ public:
     WEBKIT_EXPORT size_t length() const;
     WEBKIT_EXPORT const WebUChar* data() const;
 
+    // Caller must check bounds.
+    WEBKIT_EXPORT WebUChar at(unsigned) const;
+
     bool isEmpty() const { return !length(); }
     bool isNull() const { return !m_private; }
 
@@ -160,6 +163,7 @@ public:
 
 private:
     void assign(WebStringPrivate*);
+
     WebStringPrivate* m_private;
 };
 
