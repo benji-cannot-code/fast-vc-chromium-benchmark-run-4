fe46fcbe77c52defc6ff9d0850dd7e0a416ed6d9
kojih@chromium.org <kojih@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-28 07:27:07 +0000

    Auto-generate V8Storage::namedPropertySetter
    Remove V8Storage::namedPropertySetter
    in V8StorageCustom.cpp
    
    Also support [OverrideBuiltins] in setter generation
    because storage needs property check and
    thus [OverrideBuiltins] is unnecessary.
    See discussion:
    https://codereview.chromium.org/15440002/
    
    On the other hand, DOMStringMap needs [OverrideBuiltins]
    See discussion:
    https://codereview.chromium.org/15512003/#msg3
    
    R=haraken@chromium.org
    BUG=229740
    TEST=TestEventTarget.idl
    
    Review URL: https://chromiumcodereview.appspot.com/15944010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/storage/Storage.h
+++ third_party/WebKit/Source/core/storage/Storage.h
@@ -56,6 +56,7 @@ namespace WebCore {
 
         String anonymousIndexedGetter(unsigned, ExceptionCode&);
         String anonymousNamedGetter(const AtomicString&, ExceptionCode&);
+        bool anonymousNamedSetter(const AtomicString& name, const AtomicString& value, ExceptionCode&);
 
     private:
         Storage(Frame*, PassRefPtr<StorageArea>);
