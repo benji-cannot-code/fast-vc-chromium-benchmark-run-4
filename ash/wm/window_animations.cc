eddf77044fa41f085333c472336f5132861a6649
nkostylev@chromium.org <nkostylev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 20:40:18 +0000

    Cleanup legacy flags and switches
    kLoginScreen
    kLoginScreenSize
    
    kAshBootAnimationFunction2
    kAshBootAnimationFunction3
    kAshDisableBootAnimation2
    + related flags
    
    See also https://gerrit.chromium.org/gerrit/#/c/56735/
    
    BUG=244224
    TEST=compiles, existing browser_tests and autotests work
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16105005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202646 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_animations.cc
+++ ash/wm/window_animations.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
-#include "ash/ash_switches.h"
 #include "ash/launcher/launcher.h"
 #include "ash/screen_ash.h"
 #include "ash/shell.h"
@@ -478,13 +477,6 @@ std::vector<ui::LayerAnimationSequence*>
 CreateBrightnessGrayscaleAnimationSequence(float target_value,
                                            base::TimeDelta duration) {
   ui::Tween::Type animation_type = ui::Tween::EASE_OUT;
-  if (CommandLine::ForCurrentProcess()->HasSwitch(
-          ash::switches::kAshBootAnimationFunction2)) {
-    animation_type = ui::Tween::EASE_OUT_2;
-  } else if (CommandLine::ForCurrentProcess()->HasSwitch(
-                  ash::switches::kAshBootAnimationFunction3)) {
-    animation_type = ui::Tween::EASE_OUT_3;
-  }
   scoped_ptr<ui::LayerAnimationSequence> brightness_sequence(
       new ui::LayerAnimationSequence());
   scoped_ptr<ui::LayerAnimationSequence> grayscale_sequence(
