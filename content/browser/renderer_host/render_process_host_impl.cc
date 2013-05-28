f60972f079dd07ee6860b45fb9ed8a85ac7d7692
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 21:58:18 +0000

    Revert 202620 "Collect tab timing information for use in telemen..."
    > Collect tab timing information for use in telementry-based startup tests
    > 
    > Motivation: Data collection exposed in this CL is needed by upcoming startup tests we're writing using Telemtry.
    > 
    > Expose a new window.statsCollectionController object to JS and move existing histogram reading code into it since that seemed misplaced in DOMAutomationController.
    > 
    > Add a new --enable-stats-collection-bindings to activate said object.
    > 
    > Example usage in telemtry:
    > with browser.Create() as b:
    >   b.tabs[0].Navigate("http://www.google.com")
    >   b.tabs[0].WaitForDocumentReadyStateToBeComplete()
    >   print b.tabs[0].EvaluateJavaScript('statsCollectionController.tabLoadTiming()')
    > 
    > BUG=None
    > 
    > Review URL: https://chromiumcodereview.appspot.com/12389073
    
    TBR=jeremy@chromium.org
    
    Review URL: https://codereview.chromium.org/16160006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202662 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -943,7 +943,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kRendererStartupDialog,
     switches::kShowPaintRects,
     switches::kSitePerProcess,
-    switches::kStatsCollectionController,
     switches::kTestSandbox,
     switches::kTouchEvents,
     switches::kTraceStartup,
