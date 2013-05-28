b767bd6a1bf7d551b8772af515d339f024f7e9b2
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-28 04:08:08 +0000

    Cleanup: Remove ScopedGenericObj usage in media/.
    Review URL: https://chromiumcodereview.appspot.com/15659006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202497 0039d316-1c4b-4281-b951-d872f2087c98

--- media/webm/chromeos/webm_encoder.cc
+++ media/webm/chromeos/webm_encoder.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/file_util.h"
 #include "base/logging.h"
-#include "base/memory/scoped_generic_obj.h"
+#include "base/memory/scoped_ptr.h"
 #include "libyuv/convert.h"
 #include "libyuv/video_common.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -37,16 +37,14 @@ static void Ebml_SerializeUnsigned32(EbmlGlobal* ebml,
 }
 
 // Wrapper functor for vpx_codec_destroy().
-class VpxCodecDestroyHelper {
- public:
+struct VpxCodecDeleter {
   void operator()(vpx_codec_ctx_t* codec) {
     vpx_codec_destroy(codec);
   }
 };
 
 // Wrapper functor for vpx_img_free().
-class VpxImgFreeHelper {
- public:
+struct VpxImgDeleter {
   void operator()(vpx_image_t* image) {
     vpx_img_free(image);
   }
@@ -90,7 +88,7 @@ bool WebmEncoder::EncodeFromSprite(const SkBitmap& sprite,
   vpx_image_t image;
   vpx_img_alloc(&image, VPX_IMG_FMT_I420, width_, height_, 16);
   // Ensure that image is freed after return.
-  ScopedGenericObj<vpx_image_t*, VpxImgFreeHelper> image_ptr(&image);
+  scoped_ptr<vpx_image_t, VpxImgDeleter> image_ptr(&image);
 
   const vpx_codec_iface_t* codec_iface = vpx_codec_vp8_cx();
   DCHECK(codec_iface);
@@ -114,7 +112,7 @@ bool WebmEncoder::EncodeFromSprite(const SkBitmap& sprite,
   if (ret != VPX_CODEC_OK)
     return false;
   // Ensure that codec context is freed after return.
-  ScopedGenericObj<vpx_codec_ctx_t*, VpxCodecDestroyHelper> codec_ptr(&codec);
+  scoped_ptr<vpx_codec_ctx_t, VpxCodecDeleter> codec_ptr(&codec);
 
   SkAutoLockPixels lock_sprite(sprite);
 
