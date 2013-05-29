5a7dd4fca129b93c3eecad19b8c51e3d3eaf23e3
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-29 06:51:47 +0000

    Use V8ScriptRunner::callInternalFunction in PageScriptDebugServer
    This is one of steps to aggregate call sites of Function::Call.
    
    R=abarth,eseidel
    BUG=241743
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/15806013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151356 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/PageScriptDebugServer.cpp
+++ third_party/WebKit/Source/bindings/v8/PageScriptDebugServer.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8DOMWindowShell.h"
 #include "bindings/v8/V8RecursionScope.h"
+#include "bindings/v8/V8ScriptRunner.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/inspector/ScriptDebugListener.h"
 #include "core/page/Frame.h"
@@ -100,11 +101,7 @@ void PageScriptDebugServer::addListener(ScriptDebugListener* listener, Page* pag
     v8::Local<v8::Context> context = shell->context();
     v8::Handle<v8::Function> getScriptsFunction = v8::Local<v8::Function>::Cast(m_debuggerScript.get()->Get(v8::String::NewSymbol("getScripts")));
     v8::Handle<v8::Value> argv[] = { context->GetEmbedderData(0) };
-    v8::Handle<v8::Value> value;
-    {
-        V8RecursionScope::MicrotaskSuppression scope;
-        value = getScriptsFunction->Call(m_debuggerScript.get(), 1, argv);
-    }
+    v8::Handle<v8::Value> value = V8ScriptRunner::callInternalFunction(getScriptsFunction, context, m_debuggerScript.get(), 1, argv, m_isolate);
     if (value.IsEmpty())
         return;
     ASSERT(!value->IsUndefined() && value->IsArray());
