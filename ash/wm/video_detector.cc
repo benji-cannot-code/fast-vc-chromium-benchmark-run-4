8d1d49d04fea1aaf8d04a34ae84cf5c3903bacb8
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 08:15:53 +0000

    ash: Change video threshold from 300x225 to 333x250.
    This increases the minimum dimensions that ash uses when
    detecting video for the purpose of preventing the screen
    from being dimmed. It's unlikely that any given set of
    numbers will be perfect here, but this may help avoid some
    false positives caused by animated advertisements.
    
    BUG=171456
    
    Review URL: https://chromiumcodereview.appspot.com/16174002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202817 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/video_detector.cc
+++ ash/wm/video_detector.cc
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ash {
 
-const int VideoDetector::kMinUpdateWidth = 300;
-const int VideoDetector::kMinUpdateHeight = 225;
+const int VideoDetector::kMinUpdateWidth = 333;
+const int VideoDetector::kMinUpdateHeight = 250;
 const int VideoDetector::kMinFramesPerSecond = 15;
 const double VideoDetector::kNotifyIntervalSec = 1.0;
 
