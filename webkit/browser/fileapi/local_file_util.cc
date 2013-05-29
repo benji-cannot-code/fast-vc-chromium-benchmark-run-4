afa2cb3ac379097694dda4cb2b5aa9acc5aefe0f
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 11:47:18 +0000

    Remove unnecessary LocalFileUtil::GetLocalPath() decl
    Also remove unnecessary code in local_file_util.cc.
    
    BUG=241701
    
    Review URL: https://chromiumcodereview.appspot.com/15686005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202846 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/local_file_util.cc
+++ webkit/browser/fileapi/local_file_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_util_proxy.h"
 #include "googleurl/src/gurl.h"
 #include "webkit/browser/fileapi/file_system_context.h"
-#include "webkit/browser/fileapi/file_system_mount_point_provider.h"
 #include "webkit/browser/fileapi/file_system_operation_context.h"
 #include "webkit/browser/fileapi/file_system_url.h"
 #include "webkit/browser/fileapi/native_file_util.h"
@@ -162,9 +161,6 @@ PlatformFileError LocalFileUtil::GetLocalFilePath(
     FileSystemOperationContext* context,
     const FileSystemURL& url,
     base::FilePath* local_file_path) {
-  FileSystemMountPointProvider* provider =
-      context->file_system_context()->GetMountPointProvider(url.type());
-  DCHECK(provider);
   base::FilePath root = context->root_path();
   if (root.empty())
     return base::PLATFORM_FILE_ERROR_NOT_FOUND;
