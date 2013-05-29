d55dca90c9033b084337ea033b9bc50a8b30ed5a
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 22:02:17 +0000

    Reorder the NativeViews attached to a view via the kViewHostKey according to the position of the view in the view hierarchy.
    BUG=237650
    Test=NativeWidgetAuraTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/15114002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202987 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_aura.cc
+++ ui/views/widget/native_widget_aura.cc
@@ -40,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/widget/tooltip_manager_aura.h"
 #include "ui/views/widget/widget_aura_utils.h"
 #include "ui/views/widget/widget_delegate.h"
+#include "ui/views/widget/window_reorderer.h"
 
 #if defined(OS_WIN)
 #include "base/win/scoped_gdi_object.h"
@@ -173,6 +174,9 @@ void NativeWidgetAura::InitNativeWidget(const Widget::InitParams& params) {
                        GetWidget()->widget_delegate()->CanMaximize());
   window_->SetProperty(aura::client::kCanResizeKey,
                        GetWidget()->widget_delegate()->CanResize());
+
+  window_reorderer_.reset(new WindowReorderer(window_,
+      GetWidget()->GetRootView()));
 }
 
 NonClientFrameView* NativeWidgetAura::CreateNonClientFrameView() {
@@ -222,6 +226,10 @@ ui::Compositor* NativeWidgetAura::GetCompositor() {
 
 ui::Layer* NativeWidgetAura::GetLayer() {
   return window_->layer();
+}
+
+void NativeWidgetAura::ReorderNativeViews() {
+  window_reorderer_->ReorderChildWindows();
 }
 
 void NativeWidgetAura::ViewRemoved(View* view) {
