78bcb9718a3232d6cb1dfce412dd48f51ed99107
shawnsingh@chromium.org <shawnsingh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 11:48:49 +0000

    Add CompositingReasons to compositor layers.
    This is a re-land after first version (r202580) was reverted. The problem was
    that extra invalidations occurred due to a call to SetNeedsCommit(). The
    solution is to avoid calling SetNeedsCommit(), and accept that this debug info
    will be stale on the impl-thread until something else commits.
    
    BUG=240946
    
    Review URL: https://chromiumcodereview.appspot.com/16023024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202848 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/layer.h
+++ cc/layers/layer.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/base/cc_export.h"
 #include "cc/base/region.h"
 #include "cc/base/scoped_ptr_vector.h"
+#include "cc/layers/compositing_reasons.h"
 #include "cc/layers/draw_properties.h"
 #include "cc/layers/layer_lists.h"
 #include "cc/layers/layer_position_constraint.h"
@@ -285,6 +286,7 @@ class CC_EXPORT Layer : public base::RefCounted<Layer>,
   virtual void ReduceMemoryUsage() {}
 
   void SetDebugName(const std::string& debug_name);
+  void SetCompositingReasons(CompositingReasons reasons);
 
   virtual void PushPropertiesTo(LayerImpl* layer);
 
@@ -452,6 +454,7 @@ class CC_EXPORT Layer : public base::RefCounted<Layer>,
   gfx::PointF anchor_point_;
   SkColor background_color_;
   std::string debug_name_;
+  CompositingReasons compositing_reasons_;
   float opacity_;
   skia::RefPtr<SkImageFilter> filter_;
   WebKit::WebFilterOperations filters_;
