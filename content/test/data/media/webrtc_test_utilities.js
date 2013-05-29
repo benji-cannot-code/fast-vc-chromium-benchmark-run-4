ce039c48899dedc9d28f2ee497122b5d1122bb0e
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 18:19:16 +0000

    Made sure empty-call-then-add-one-stream test doesn't do simultaneous negotiations.
    This should solve the flakiness we've been seeing.
    
    BUG=236102
    
    Review URL: https://chromiumcodereview.appspot.com/15688006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202915 0039d316-1c4b-4281-b951-d872f2087c98

--- content/test/data/media/webrtc_test_utilities.js
+++ content/test/data/media/webrtc_test_utilities.js
@@ -45,6 +45,16 @@ function waitForVideo(videoElement) {
   detectVideoIn(videoElement, function () { eventOccured(); });
 }
 
+function waitForConnectionToStabilize(peerConnection) {
+  addExpectedEvent();
+  var waitForStabilization = setInterval(function() {
+    if (peerConnection.signalingState == 'stable') {
+      clearInterval(waitForStabilization);
+      eventOccured();
+    }
+  }, 100);
+}
+
 function addExpectedEvent() {
   ++gNumberOfExpectedEvents;
 }
