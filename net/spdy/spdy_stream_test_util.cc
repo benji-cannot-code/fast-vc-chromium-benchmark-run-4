638f16ce83f636dda6fa84565de144972ecf7938
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 00:22:33 +0000

    [SPDY] Refactor SpdyStream::Delegate
    Merge some member functions and rename others.
    
    Clean up SpdyHttpStream logic a bit.
    
    BUG=243643
    
    Review URL: https://chromiumcodereview.appspot.com/15936003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202703 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_stream_test_util.cc
+++ net/spdy/spdy_stream_test_util.cc
@@ -21,17 +21,11 @@ ClosingDelegate::ClosingDelegate(
 
 ClosingDelegate::~ClosingDelegate() {}
 
-void ClosingDelegate::OnSendRequestHeadersComplete() {}
+void ClosingDelegate::OnRequestHeadersSent() {}
 
-void ClosingDelegate::OnSendBody() {
-  ADD_FAILURE() << "OnSendBody should not be called";
-}
-
-void ClosingDelegate::OnSendBodyComplete() {}
-
-int ClosingDelegate::OnResponseReceived(const SpdyHeaderBlock& response,
-                                        base::Time response_time,
-                                        int status) {
+int ClosingDelegate::OnResponseHeadersReceived(const SpdyHeaderBlock& response,
+                                               base::Time response_time,
+                                               int status) {
   return OK;
 }
 
@@ -57,15 +51,16 @@ StreamDelegateBase::StreamDelegateBase(
 StreamDelegateBase::~StreamDelegateBase() {
 }
 
-void StreamDelegateBase::OnSendRequestHeadersComplete() {
+void StreamDelegateBase::OnRequestHeadersSent() {
   stream_id_ = stream_->stream_id();
   EXPECT_NE(stream_id_, 0u);
   send_headers_completed_ = true;
 }
 
-int StreamDelegateBase::OnResponseReceived(const SpdyHeaderBlock& response,
-                                           base::Time response_time,
-                                           int status) {
+int StreamDelegateBase::OnResponseHeadersReceived(
+    const SpdyHeaderBlock& response,
+    base::Time response_time,
+    int status) {
   EXPECT_TRUE(send_headers_completed_);
   response_ = response;
   return status;
@@ -117,12 +112,6 @@ StreamDelegateDoNothing::StreamDelegateDoNothing(
 StreamDelegateDoNothing::~StreamDelegateDoNothing() {
 }
 
-void StreamDelegateDoNothing::OnSendBody() {
-  ADD_FAILURE() << "OnSendBody should not be called";
-}
-
-void StreamDelegateDoNothing::OnSendBodyComplete() {}
-
 StreamDelegateSendImmediate::StreamDelegateSendImmediate(
     const base::WeakPtr<SpdyStream>& stream,
     base::StringPiece data)
@@ -132,23 +121,16 @@ StreamDelegateSendImmediate::StreamDelegateSendImmediate(
 StreamDelegateSendImmediate::~StreamDelegateSendImmediate() {
 }
 
-void StreamDelegateSendImmediate::OnSendBody() {
-  ADD_FAILURE() << "OnSendBody should not be called";
-}
-
-void StreamDelegateSendImmediate::OnSendBodyComplete() {
-  ADD_FAILURE() << "OnSendBodyComplete should not be called";
-}
-
-int StreamDelegateSendImmediate::OnResponseReceived(
+int StreamDelegateSendImmediate::OnResponseHeadersReceived(
     const SpdyHeaderBlock& response,
     base::Time response_time,
     int status) {
   status =
-      StreamDelegateBase::OnResponseReceived(response, response_time, status);
+      StreamDelegateBase::OnResponseHeadersReceived(
+          response, response_time, status);
   if (data_.data()) {
     scoped_refptr<StringIOBuffer> buf(new StringIOBuffer(data_.as_string()));
-    stream()->SendStreamData(buf, buf->size(), MORE_DATA_TO_SEND);
+    stream()->SendData(buf, buf->size(), MORE_DATA_TO_SEND);
   }
   return status;
 }
@@ -162,11 +144,10 @@ StreamDelegateWithBody::StreamDelegateWithBody(
 StreamDelegateWithBody::~StreamDelegateWithBody() {
 }
 
-void StreamDelegateWithBody::OnSendBody() {
-  stream()->SendStreamData(buf_.get(), buf_->size(), NO_MORE_DATA_TO_SEND);
+void StreamDelegateWithBody::OnRequestHeadersSent() {
+  StreamDelegateBase::OnRequestHeadersSent();
+  stream()->SendData(buf_.get(), buf_->size(), NO_MORE_DATA_TO_SEND);
 }
-
-void StreamDelegateWithBody::OnSendBodyComplete() {}
 
 } // namespace test
 
