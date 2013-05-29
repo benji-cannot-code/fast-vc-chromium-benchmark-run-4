3a07bb81f65bdb681513f6fce3dc9a24a9679ded
dvh@chromium.org <dvh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 11:37:09 +0000

    Implemented separate tabs for unpacked apps and extensions in App Developer Tools.
    BUG=237277
    
    Review URL: https://chromiumcodereview.appspot.com/15810003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202840 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/apps_debugger/js/items_list.js
+++ chrome/browser/resources/apps_debugger/js/items_list.js
@@ -6,14 +6,17 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 cr.define('apps_dev_tool', function() {
   'use strict';
 
-  // The list of all apps & extensions.
+  // The list of all packed/unpacked apps and extensions.
   var completeList = [];
 
-  // The list of all apps.
-  var appList = [];
+  // The list of all packed apps.
+  var packedAppList = [];
 
-  // The list of all extensions.
-  var extensionList = [];
+  // The list of all packed extensions.
+  var packedExtensionList = [];
+
+  // The list of all unpacked apps or extensions.
+  var unpackedList = [];
 
   /** const*/ var AppsDevTool = apps_dev_tool.AppsDevTool;
 
@@ -53,10 +56,13 @@ cr.define('apps_dev_tool', function() {
    * Refreshes the app.
    */
   function reloadAppDisplay() {
-    var extensions = new ItemsList($('extension-settings-list'), extensionList);
-    var apps = new ItemsList($('app-settings-list'), appList);
+    var extensions = new ItemsList($('packed-extension-list'),
+                                   packedExtensionList);
+    var apps = new ItemsList($('packed-app-list'), packedAppList);
+    var unpacked = new ItemsList($('unpacked-list'), unpackedList);
     extensions.showItemNodes();
     apps.showItemNodes();
+    unpacked.showItemNodes();
   }
 
   /**
@@ -64,17 +70,20 @@ cr.define('apps_dev_tool', function() {
    * @param {string} filter Curent string in the search box.
    */
   function rebuildAppList(filter) {
-    appList = [];
-    extensionList = [];
+    packedAppList = [];
+    packedExtensionList = [];
+    unpackedList = [];
 
     for (var i = 0; i < completeList.length; i++) {
       var item = completeList[i];
       if (filter && item.name.toLowerCase().search(filter.toLowerCase()) < 0)
         continue;
-      if (item.isApp)
-        appList.push(item);
+      if (item.is_unpacked)
+        unpackedList.push(item);
+      else if (item.isApp)
+        packedAppList.push(item);
       else
-        extensionList.push(item);
+        packedExtensionList.push(item);
     }
   }
 
