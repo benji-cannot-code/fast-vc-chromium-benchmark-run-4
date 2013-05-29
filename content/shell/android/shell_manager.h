12f8a46136982de4922a96e0a0613e24f2604210
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 12:41:52 +0000

    Android: implement content::Shell::Close()
    This patch implements the Shell::Close() method for Content Shell on Android.
    
    BUG=138226
    
    Review URL: https://chromiumcodereview.appspot.com/14977010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202852 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/android/shell_manager.h
+++ content/shell/android/shell_manager.h
@@ -25,6 +25,9 @@ namespace content {
 // object.
 jobject CreateShellView(Shell* shell);
 
+// Closes a previously created shell view.
+void CloseShellView(jobject shell_view);
+
 // Registers the ShellManager native methods.
 bool RegisterShellManager(JNIEnv* env);
 
