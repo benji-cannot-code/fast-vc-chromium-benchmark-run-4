9bbb4573ed9374e18ee01213a31ee3057e27c16f
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-29 07:47:18 +0000

    drive: Stop mentioning about symlinks
    BUG=244343
    R=satorux@chromium.org
    
    Review URL: https://codereview.chromium.org/15878006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@202803 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_cache.h
+++ chrome/browser/chromeos/drive/file_cache.h
@@ -123,9 +123,7 @@ class FileCache {
   // Gets the cache entry for file corresponding to |resource_id| and |md5|
   // and runs |callback| with true and the entry found if entry exists in cache
   // map.  Otherwise, runs |callback| with false.
-  // |md5| can be empty if only matching |resource_id| is desired, which may
-  // happen when looking for pinned entries where symlinks' filenames have no
-  // extension and hence no md5.
+  // |md5| can be empty if only matching |resource_id| is desired.
   // |callback| must not be null.
   // Must be called on the UI thread.
   void GetCacheEntryOnUIThread(const std::string& resource_id,
@@ -207,11 +205,7 @@ class FileCache {
                                       FileOperationType file_operation_type,
                                       const FileOperationCallback& callback);
 
-  // Modifies cache state, which involves the following:
-  // - moves |source_path| to |dest_path| in persistent dir if
-  //   file is not dirty
-  // - creates symlink in pinned dir that references downloaded or locally
-  //   modified file
+  // Pins the specified entry.
   // |callback| must not be null.
   // Must be called on the UI thread.
   void PinOnUIThread(const std::string& resource_id,
@@ -226,9 +220,7 @@ class FileCache {
                        const std::string& md5,
                        const FileOperationCallback& callback);
 
-  // Modifies cache state, which involves the following:
-  // - moves the local file from pinned directory to unpinned directory.
-  // - update cache metadata.
+  // Unpins the specified entry.
   FileError Unpin(const std::string& resource_id, const std::string& md5);
 
   // Sets the state of the cache entry corresponding to |resource_id| and |md5|
@@ -245,18 +237,14 @@ class FileCache {
   void MarkAsUnmountedOnUIThread(const base::FilePath& file_path,
                                  const FileOperationCallback& callback);
 
-  // Modifies cache state, which involves the following:
-  // - moves |source_path| to |dest_path| in persistent dir, where
-  //   |source_path| has .<md5> extension and |dest_path| has .local extension
-  // - if file is pinned, updates symlink in pinned dir to reference dirty file
+  // Marks the specified entry dirty.
   // |callback| must not be null.
   // Must be called on the UI thread.
   void MarkDirtyOnUIThread(const std::string& resource_id,
                            const std::string& md5,
                            const FileOperationCallback& callback);
 
-  // Modifies cache state, i.e. creates symlink in outgoing
-  // dir to reference dirty file in persistent dir.
+  // Commits changes for the specified dirty entry.
   // |callback| must not be null.
   // Must be called on the UI thread.
   void CommitDirtyOnUIThread(const std::string& resource_id,
