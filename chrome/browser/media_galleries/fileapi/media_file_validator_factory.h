f5a7d128ab729b39c44f41b987104cbe394eee0a
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 03:24:45 +0000

    Validate image files before writing them to media galleries.
    Review URL: https://chromiumcodereview.appspot.com/15624003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203038 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/media_galleries/fileapi/media_file_validator_factory.h
@@ -0,0 +1,41 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef CHROME_BROWSER_MEDIA_GALLERIES_FILEAPI_MEDIA_FILE_VALIDATOR_FACTORY_H_
+#define CHROME_BROWSER_MEDIA_GALLERIES_FILEAPI_MEDIA_FILE_VALIDATOR_FACTORY_H_
+
+#include "base/basictypes.h"
+#include "webkit/browser/fileapi/copy_or_move_file_validator.h"
+
+namespace base {
+class FilePath;
+}
+
+namespace fileapi {
+class FileSystemURL;
+}
+
+namespace chrome {
+
+// A factory for media file validators. A media file validator will use various
+// strategies (depending on the file type) to attempt to verify that the file
+// is a valid media file.
+class MediaFileValidatorFactory
+    : public fileapi::CopyOrMoveFileValidatorFactory {
+ public:
+  MediaFileValidatorFactory();
+  virtual ~MediaFileValidatorFactory();
+
+  // CopyOrMoveFileValidatorFactory implementation.
+  virtual fileapi::CopyOrMoveFileValidator* CreateCopyOrMoveFileValidator(
+            const fileapi::FileSystemURL& src,
+            const base::FilePath& platform_path) OVERRIDE;
+ private:
+  DISALLOW_COPY_AND_ASSIGN(MediaFileValidatorFactory);
+};
+
+}  // namespace chrome
+
+#endif  // CHROME_BROWSER_MEDIA_GALLERIES_FILEAPI_MEDIA_FILE_VALIDATOR_FACTORY_H_
--- webkit/browser/fileapi/mock_file_system_context.h
+++ webkit/browser/fileapi/mock_file_system_context.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WEBKIT_BROWSER_FILEAPI_MOCK_FILE_SYSTEM_CONTEXT_H_
 
 #include "base/files/file_path.h"
+#include "base/memory/scoped_vector.h"
 
 namespace quota {
 class QuotaManagerProxy;
@@ -16,9 +17,17 @@ class SpecialStoragePolicy;
 namespace fileapi {
 
 class FileSystemContext;
+class FileSystemMountPointProvider;
 
 FileSystemContext* CreateFileSystemContextForTesting(
     quota::QuotaManagerProxy* quota_manager_proxy,
+    const base::FilePath& base_path);
+
+// The caller is responsible for including TestMountPointProvider in
+// |additional_providers| if needed.
+FileSystemContext* CreateFileSystemContextWithAdditionalProvidersForTesting(
+    quota::QuotaManagerProxy* quota_manager_proxy,
+    ScopedVector<FileSystemMountPointProvider> additional_providers,
     const base::FilePath& base_path);
 
 }  // namespace fileapi
