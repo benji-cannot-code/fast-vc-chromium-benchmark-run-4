6e1754f114d341389f54ebae7fac6a2a26f32ac1
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 14:17:10 +0000

    Refacotring: fix styles of i18n-content names (concerned with the language options).
    Replaced i18n-content names from c_hacker_style to camelCaseStyle except for some names which are generated by chromeos::options::GetI18nContentValue.
    
    This CL is forked from https://codereview.chromium.org/16141002/.
    
    BTW, I couldn't find where the below names are used:
    - extension_ime_description
    - extension_ime_label
    
    Review URL: https://chromiumcodereview.appspot.com/16025007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/language_add_language_overlay.js
+++ chrome/browser/resources/options/language_add_language_overlay.js
@@ -15,7 +15,7 @@ cr.define('options', function() {
    */
   function AddLanguageOverlay() {
     OptionsPage.call(this, 'addLanguage',
-                     loadTimeData.getString('add_button'),
+                     loadTimeData.getString('addButton'),
                      'add-language-overlay-page');
   }
 
