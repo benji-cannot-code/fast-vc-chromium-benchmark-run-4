5218ab6a4bf2c4ae07b825ff3a8c2b19e2dedbae
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 07:58:02 +0000

    Cleanup: Remove unneeded includes of chrome/browser/storage_monitor/media_storage_util.h
    Review URL: https://chromiumcodereview.appspot.com/15764006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203109 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/media_galleries_dialog_controller.cc
+++ chrome/browser/media_galleries/media_galleries_dialog_controller.cc
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/media_galleries/media_file_system_registry.h"
 #include "chrome/browser/profiles/profile.h"
-#include "chrome/browser/storage_monitor/media_storage_util.h"
 #include "chrome/browser/storage_monitor/storage_info.h"
 #include "chrome/browser/storage_monitor/storage_monitor.h"
 #include "chrome/browser/ui/chrome_select_file_policy.h"
--- chrome/browser/storage_monitor/image_capture_device.mm
+++ chrome/browser/storage_monitor/image_capture_device.mm
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "chrome/browser/storage_monitor/image_capture_device.h"
 
 #include "base/file_util.h"
-#include "chrome/browser/storage_monitor/media_storage_util.h"
 #include "content/public/browser/browser_thread.h"
 
 namespace {
