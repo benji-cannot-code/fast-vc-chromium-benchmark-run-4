d92e2ad6da4a59c9022544d665a46dbbbbb6d28d
paulnavin@google.com <paulnavin@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 19:18:42 +0000

    Color Picker for  <input type=color>
    Thanks for all the review comments!  I think this is now ready.  :-)
    
    I've replied to as many comments as possible, I hope my replies make
    sense.  Thanks again, I'm learning a lot!  :-)
        
    References:
     - The mockup I'm following is here:
    https://docs.google.com/a/google.com/file/d/0B2S_KwX0QwGFb2JVTlhPYlRqaEE/edit
     - There's an APK here:
    https://drive.google.com/a/google.com/folderview?id=0B8nL-JlqpL9nLURUVnJsdTFDYjQ
    
    BUG=135771
    
    Review URL: https://chromiumcodereview.appspot.com/14170009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203195 0039d316-1c4b-4281-b951-d872f2087c98

--- components/web_contents_delegate_android/android/java/src/org/chromium/components/web_contents_delegate_android/ColorChooserAndroid.java
+++ components/web_contents_delegate_android/android/java/src/org/chromium/components/web_contents_delegate_android/ColorChooserAndroid.java
@@ -11,6 +11,7 @@ import org.chromium.base.CalledByNative;
 import org.chromium.base.JNINamespace;
 import org.chromium.content.browser.ContentViewCore;
 import org.chromium.ui.ColorPickerDialog;
+import org.chromium.ui.OnColorChangedListener;
 
 /**
  * ColorChooserAndroid communicates with the java ColorPickerDialog and the
@@ -23,11 +24,9 @@ public class ColorChooserAndroid {
 
     private ColorChooserAndroid(int nativeColorChooserAndroid,
             Context context, int initialColor) {
-        ColorPickerDialog.OnColorChangedListener listener =
-                new ColorPickerDialog.OnColorChangedListener() {
-
+        OnColorChangedListener listener = new OnColorChangedListener() {
           @Override
-          public void colorChanged(int color) {
+          public void onColorChanged(int color) {
               mDialog.dismiss();
               nativeOnColorChosen(mNativeColorChooserAndroid, color);
           }
