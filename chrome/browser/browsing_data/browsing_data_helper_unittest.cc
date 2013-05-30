895b46f8caabce3cd68175839156528a12f47680
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 22:13:48 +0000

    content: Move kViewSourceScheme constant into content namespace.
    BUG=None
    R=thestig@chromium.org,grt@chromium.org,joi@chromium.org,eroman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15950011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browsing_data/browsing_data_helper_unittest.cc
+++ chrome/browser/browsing_data/browsing_data_helper_unittest.cc
@@ -84,7 +84,7 @@ TEST_F(BrowsingDataHelperTest, ChromeSchemesAreNotWebSafe) {
   EXPECT_FALSE(IsWebScheme(chrome::kMailToScheme));
   EXPECT_FALSE(IsWebScheme(chrome::kMetadataScheme));
   EXPECT_FALSE(IsWebScheme(chrome::kSwappedOutScheme));
-  EXPECT_FALSE(IsWebScheme(chrome::kViewSourceScheme));
+  EXPECT_FALSE(IsWebScheme(content::kViewSourceScheme));
 }
 
 TEST_F(BrowsingDataHelperTest, WebSafeSchemesAreNotExtensions) {
@@ -109,7 +109,7 @@ TEST_F(BrowsingDataHelperTest, ChromeSchemesAreNotAllExtension) {
   EXPECT_FALSE(IsExtensionScheme(chrome::kMailToScheme));
   EXPECT_FALSE(IsExtensionScheme(chrome::kMetadataScheme));
   EXPECT_FALSE(IsExtensionScheme(chrome::kSwappedOutScheme));
-  EXPECT_FALSE(IsExtensionScheme(chrome::kViewSourceScheme));
+  EXPECT_FALSE(IsExtensionScheme(content::kViewSourceScheme));
 }
 
 TEST_F(BrowsingDataHelperTest, TestMatches) {
