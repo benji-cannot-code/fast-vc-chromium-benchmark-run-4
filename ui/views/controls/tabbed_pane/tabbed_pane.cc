98ac0d1864713696d66909bd90636c706f3af48c
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 00:18:00 +0000

    Rename ClampToMin and ClampToMax
    I find these function names confusing. I have to stop and read the code each
    time I try to use them to figure out what they're going to do.
    
    Part of the confusion is that ClampToMin sets the components to the _maximum_
    of the two sizes/vectors. It's also not clear from the function name which of
    the two objects is acting as the 'Min'. This is mitigated by the parameter names
    and local variables passed to the function, but it would be nice if the name
    of the function itself made this clear.
    
    a.ClampToLowerBound(b) makes it clear both that b is acting as the lower bound
    and how and when a is going to be altered.
    
    Other names I've considered (for ClampToMin -- the suggestions for ClampToMax
    are analagous):
      ClampIfSmallerThan(other)
      SetComponentsToMax(other)
      Union(other) (for sizes)
    
    R=danakj,sky
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/14367021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203017 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/tabbed_pane/tabbed_pane.cc
+++ ui/views/controls/tabbed_pane/tabbed_pane.cc
@@ -309,7 +309,7 @@ void TabbedPane::SelectTab(Tab* tab) {
 gfx::Size TabbedPane::GetPreferredSize() {
   gfx::Size size;
   for (int i = 0; i < contents_->child_count(); ++i)
-    size.ClampToMin(contents_->child_at(i)->GetPreferredSize());
+    size.SetToMax(contents_->child_at(i)->GetPreferredSize());
   size.Enlarge(0, tab_strip_->GetPreferredSize().height());
   return size;
 }
