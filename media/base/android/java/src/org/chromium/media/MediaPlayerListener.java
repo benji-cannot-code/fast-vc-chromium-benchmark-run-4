21c734deecf924c5fa5ef70561f9291a74192306
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 16:06:05 +0000

    allow multiple media players to play
    When a player starts playing, we always request audio focus.
    This will cause other players to pause.
    We should always allow other players to lower their output level and play at the same time.
    
    BUG=244696
    R=miguelg@chromium.org
    
    Review URL: https://codereview.chromium.org/15937017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203154 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/java/src/org/chromium/media/MediaPlayerListener.java
+++ media/base/android/java/src/org/chromium/media/MediaPlayerListener.java
@@ -145,7 +145,7 @@ class MediaPlayerListener implements MediaPlayer.OnPreparedListener,
                 AudioManager.STREAM_MUSIC,
 
                 // Request permanent focus.
-                AudioManager.AUDIOFOCUS_GAIN);
+                AudioManager.AUDIOFOCUS_GAIN_TRANSIENT_MAY_DUCK);
         return listener;
     }
 
