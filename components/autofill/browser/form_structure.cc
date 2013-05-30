8781363fc157cb539078217a03e8cca44f4fce26
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 06:16:30 +0000

    [Autofill] Pass struct by const-reference.
    BUG=none
    R=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16077005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203094 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/browser/form_structure.cc
+++ components/autofill/browser/form_structure.cc
@@ -659,7 +659,7 @@ bool FormStructure::IsAutocheckoutEnabled() const {
   return !autocheckout_url_prefix_.empty();
 }
 
-bool FormStructure::ShouldSkipField(const FormFieldData field) const {
+bool FormStructure::ShouldSkipField(const FormFieldData& field) const {
   return (field.is_checkable || field.form_control_type == "password") &&
       !IsAutocheckoutEnabled();
 }
