5c034d39e6d89b315dddc91d8df3267605f315c4
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-30 23:47:42 +0000

    Handle DemuxerStream config change in VideoFrameStream.
    This CL handles DemuxerStream::kConfigChanged in VideoFrameStream and hides it from the downstream VideoDecoders. So after this CL, kConfigChanged handling code in all VideoDecoder implementations can be deleted!
    
    Once kConfigChanged is detected, the internal state switches to STATE_FLUSHING_DECODER. The VideoFrameStream flushes the VideoDecoder before trying to reinitialize it. In this state, the VideoFrameStream stops pulling any data from the real DemuxerStream and always feeds the VideoDecoder with EOS buffer. After the VideoDecoder is flushed, it is reinitialized with the new config.
    
    It's a bit tricky to handle the Reset() call during this process. Once the VideoFrameStream is in the STATE_FLUSHING_DECODER state, it has to finish the VideoDecoder reinitialization process so that it is ready for the next ReadFrame() call.
    
    BUG=141788
    TEST=Current tests passes. Tested normal playback on Window/Linux. Tested a lot of config changes with MSE/EME.
    
    Review URL: https://chromiumcodereview.appspot.com/14779008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203265 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/video_frame_stream_unittest.cc
+++ media/filters/video_frame_stream_unittest.cc
@@ -59,7 +59,8 @@ class VideoFrameStreamTest : public testing::TestWithParam<bool> {
   }
 
   ~VideoFrameStreamTest() {
-    Stop();
+    if (is_initialized_)
+      Stop();
     EXPECT_FALSE(is_initialized_);
   }
 
@@ -217,8 +218,6 @@ class VideoFrameStreamTest : public testing::TestWithParam<bool> {
   }
 
   void Stop() {
-    if (!is_initialized_)
-      return;
     EnterPendingState(DECODER_STOP);
     SatisfyPendingCallback(DECODER_STOP);
   }
@@ -315,7 +314,10 @@ TEST_P(VideoFrameStreamTest, Reset_AfterConfigChangeRead) {
 }
 
 TEST_P(VideoFrameStreamTest, Stop_BeforeInitialization) {
-  Stop();
+  EXPECT_CALL(*this, OnStopped());
+  video_frame_stream_->Stop(
+      base::Bind(&VideoFrameStreamTest::OnStopped, base::Unretained(this)));
+  message_loop_.RunUntilIdle();
 }
 
 TEST_P(VideoFrameStreamTest, Stop_DuringInitialization) {
