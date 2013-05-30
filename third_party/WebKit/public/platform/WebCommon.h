39a84639b2d87d13ce7ab2e80fe1c72449e45105
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-30 19:47:26 +0000

    Revert 151425 "Teach WebString about 8 bit strings"
    This CL caused the iOS build to break because iOS is still pulling Blink API
    headers from svn.webkit.org and cannot see the definition of WebLChar.
    
    > Teach WebString about 8 bit strings
    > 
    > Previously we always upconverted strings backing WebString to have both a 16
    > bit and an 8 bit buffer whenever we converted a WebString to a string16.
    > There's no reason to store a shadow 16 bit buffer in the String for this use
    > case. It's just as fast to copy out an 8 bit buffer into a string16 as it is to
    > copy out a 16 bit buffer.
    > 
    > This CL removes the vast majority of the string up converstions for Mobile
    > Gmail. In a future CL, we should remove WebString::data() to prevent Chromium
    > code from accidentially upconverting the underlying String.
    > 
    > R=brettw@chromium.org, eseidel@chromium.org, jamesr@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/15866003
    
    TBR=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/15988007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151504 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebCommon.h
+++ third_party/WebKit/public/platform/WebCommon.h
@@ -78,9 +78,6 @@ typedef wchar_t WebUChar;
 typedef unsigned short WebUChar;
 #endif
 
-// Latin-1 character type
-typedef unsigned char WebLChar;
-
 // -----------------------------------------------------------------------------
 // Assertions
 
