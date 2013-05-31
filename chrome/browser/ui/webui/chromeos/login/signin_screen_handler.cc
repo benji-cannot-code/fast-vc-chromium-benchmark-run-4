1c8961cc75a1f88dd97aabcf6118e95b787038f5
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 09:36:16 +0000

    Enable Finch experiment on ChromeOS
    BUG=229420
    R=nkostylev@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15930007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203381 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/profiles/profile_helper.h"
 #include "chrome/browser/chromeos/settings/cros_settings.h"
 #include "chrome/browser/io_thread.h"
+#include "chrome/browser/managed_mode/managed_user_service.h"
 #include "chrome/browser/policy/browser_policy_connector.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/webui/chromeos/login/error_screen_handler.h"
@@ -1005,9 +1006,7 @@ void SigninScreenHandler::UpdateAuthParams(DictionaryValue* params) {
   // bool single_user = users.size() == 1;
   // chromeos::CrosSettings::Get()->GetString(chromeos::kDeviceOwner, &owner);
 
-  const CommandLine* command_line = CommandLine::ForCurrentProcess();
-  bool managed_users_enabled =
-      command_line->HasSwitch(::switches::kEnableManagedUsers);
+  bool managed_users_enabled = ManagedUserService::AreManagedUsersEnabled();
   bool managed_users_can_create = false;
   if (managed_users_enabled)
     managed_users_can_create = delegate_->GetUsers().size() > 0;
