f81438a6aa3dc3df9712369ff11598fff98ee75c
dongseong.hwang@intel.com <dongseong.hwang@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 01:48:46 +0000

    Remove unused methods in WebGraphicsContext3D: isGLES2Compliant() and getPlatformTextureId()
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16200002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203301 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
+++ content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h
@@ -119,8 +119,6 @@ class WebGraphicsContext3DCommandBufferImpl
   virtual int width();
   virtual int height();
 
-  virtual bool isGLES2Compliant();
-
   virtual bool setParentContext(WebGraphicsContext3D* parent_context);
 
   virtual unsigned int insertSyncPoint();
@@ -136,7 +134,6 @@ class WebGraphicsContext3DCommandBufferImpl
   virtual bool readBackFramebuffer(unsigned char* pixels, size_t buffer_size,
                                    WebGLId framebuffer, int width, int height);
 
-  virtual WebGLId getPlatformTextureId();
   virtual void prepareTexture();
   virtual void postSubBufferCHROMIUM(int x, int y, int width, int height);
 
