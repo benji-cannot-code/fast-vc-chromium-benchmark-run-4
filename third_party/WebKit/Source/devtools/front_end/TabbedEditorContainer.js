953d1a29709ce3c692ade23268616e9d8d05bb4d
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-31 12:31:53 +0000

    DevTools: improve go-to-file scoring via adding defaults and boosting exact matches.
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/15894012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151549 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TabbedEditorContainer.js
+++ third_party/WebKit/Source/devtools/front_end/TabbedEditorContainer.js
@@ -113,6 +113,14 @@ WebInspector.TabbedEditorContainer.prototype = {
         this._innerShowFile(uiSourceCode, true);
     },
 
+    /**
+     * @return {Array.<string>}
+     */
+    historyUris: function()
+    {
+        return this._history._urls();
+    },
+
     _addScrollAndSelectionListeners: function()
     {
         if (!this._currentView)
@@ -651,6 +659,18 @@ WebInspector.TabbedEditorContainer.History.prototype = {
                 break;
         }
         return serializedHistory;
+    },
+
+
+    /**
+     * @return {Array.<string>}
+     */
+    _urls: function()
+    {
+        var result = [];
+        for (var i = 0; i < this._items.length; ++i)
+            result.push(this._items[i].url);
+        return result;
     },
 
     __proto__: WebInspector.Object.prototype
