d5fe80c4429d3248326583da73d0712c0f82ad78
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-31 20:24:42 +0000

    Parse SVG as HTML
    This change switches the SVG parser to be the HTML parser all the time. This
    work is being done behind a flag to guage the potential compat risk.
    
    BUG=243882
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/15796009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/WebRuntimeFeatures.h
+++ third_party/WebKit/Source/WebKit/chromium/public/WebRuntimeFeatures.h
@@ -155,6 +155,9 @@ public:
     WEBKIT_EXPORT static void enablePagePopup(bool);
     WEBKIT_EXPORT static bool isPagePopupEnabled();
 
+    WEBKIT_EXPORT static void enableParseSVGAsHTML(bool);
+    WEBKIT_EXPORT static bool isParseSVGAsHTMLEnabled();
+
     WEBKIT_EXPORT static void enablePeerConnection(bool);
     WEBKIT_EXPORT static bool isPeerConnectionEnabled();
 
