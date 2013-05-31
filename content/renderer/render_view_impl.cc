31a74e60f832f9e6c5e6e07e19abba97d0aa8536
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 17:13:08 +0000

    Reland: Use a shared thread for media operations.
    This reduces the number of threads created per media element by one.
    
    BUG=61293
    TBR=acolwell, jamesr, jochen
    
    Review URL: https://codereview.chromium.org/15649017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203429 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_view_impl.cc
+++ content/renderer/render_view_impl.cc
@@ -2838,6 +2838,7 @@ WebMediaPlayer* RenderViewImpl::createMediaPlayer(
   }
 
   webkit_media::WebMediaPlayerParams params(
+      RenderThreadImpl::current()->GetMediaThreadMessageLoopProxy(),
       sink, gpu_factories, new RenderMediaLog());
   WebMediaPlayer* media_player =
       GetContentClient()->renderer()->OverrideCreateWebMediaPlayer(
