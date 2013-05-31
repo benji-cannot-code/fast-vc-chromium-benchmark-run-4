38fa7d03f340d5fc04489866a0b3d079c7f008ab
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 19:59:23 +0000

    Prevent 'restart chrome' dialog to be shown in win8 mode.
    Because the dialog being regular MessageBox hangs metro, see for example
    my tribulations in the below bug.
    
    BUG=244453
    TEST=see bug.
    
    Review URL: https://chromiumcodereview.appspot.com/16144003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203461 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_win.cc
+++ chrome/app/breakpad_win.cc
@@ -25,6 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/stringprintf.h"
 #include "base/strings/string_split.h"
 #include "base/utf_string_conversions.h"
+#include "base/win/metro.h"
 #include "base/win/pe_image.h"
 #include "base/win/registry.h"
 #include "base/win/win_util.h"
@@ -835,6 +836,10 @@ bool ShowRestartDialogIfCrashed(bool* exit_now) {
                                  NULL, 0)) {
     return false;
   }
+
+  // If we are being launched in metro mode don't try to show the dialog.
+  if (base::win::IsMetroProcess())
+    return false;
 
   // Only show this for the browser process. See crbug.com/132119.
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
