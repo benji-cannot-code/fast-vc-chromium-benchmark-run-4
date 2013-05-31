dcd89cb07d4a20e30c186a63ef115478c9b43d33
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 12:12:01 +0000

    Don't return writable file entries from sensitive directories.
    This changes chrome.fileSystem.getWritableEntry and
    chrome.fileSystem.chooseEntry to return an error when requesting a
    writable FileEntry in the app's directory, the user data dir or the
    directory containing the Chrome binary.
    
    BUG=177208
    
    Review URL: https://chromiumcodereview.appspot.com/15876013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203393 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/file_system/get_writable_file_entry_non_writable_file/test.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function getWritableEntry() {
+    chrome.fileSystem.chooseEntry({suggestedName: 'test.js'},
+                                  chrome.test.callbackPass(function(entry) {
+      chrome.test.assertEq('test.js', entry.name);
+      // Test that we cannot get a writable entry when it's in a disallowed
+      // location.
+      chrome.fileSystem.getWritableEntry(entry, chrome.test.callbackFail(
+          'Cannot write to file in a restricted location'));
+    }));
+  }
+]);
