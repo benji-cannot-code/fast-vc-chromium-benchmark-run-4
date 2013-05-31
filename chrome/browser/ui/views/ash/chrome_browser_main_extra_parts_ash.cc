5406f1d040d928b9f92892a0385c8d03d78d534f
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 14:39:51 +0000

    Use component virtual keyboard at login screen
    BUG=245064
    R=bryeung@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/15713005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/ash/chrome_browser_main_extra_parts_ash.cc
+++ chrome/browser/ui/views/ash/chrome_browser_main_extra_parts_ash.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/env.h"
 #include "ui/gfx/screen.h"
 #include "ui/gfx/screen_type_delegate.h"
+#include "ui/keyboard/keyboard.h"
+#include "ui/keyboard/keyboard_util.h"
 #include "ui/views/widget/desktop_aura/desktop_screen.h"
 
 #if defined(FILE_MANAGER_EXTENSION)
@@ -74,6 +76,12 @@ void ChromeBrowserMainExtraPartsAsh::PreProfileInit() {
     ui::SelectFileDialog::SetShellDialogsDelegate(
         &g_shell_dialogs_delegate.Get());
   }
+#else
+  // For OS_CHROMEOS, virtual keyboard needs to be initialized before profile
+  // initialized. Otherwise, virtual keyboard extension will not load at login
+  // screen.
+  if (keyboard::IsKeyboardEnabled())
+    keyboard::InitializeKeyboard();
 #endif
 
 #if defined(FILE_MANAGER_EXTENSION)
