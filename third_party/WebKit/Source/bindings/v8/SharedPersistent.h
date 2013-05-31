95abf487258731d1bda83eb2e0cde6f9ad01ff14
dcarney@chromium.org <dcarney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-05-31 11:19:50 +0000

    remove most remaining Persistent copy ctors in blink
    BUG=
    R=marja@chromium.org
    
    Review URL: https://codereview.chromium.org/16232017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151541 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/SharedPersistent.h
+++ third_party/WebKit/Source/bindings/v8/SharedPersistent.h
@@ -46,7 +46,7 @@ namespace WebCore {
         {
             return adoptRef(new SharedPersistent<T>(value));
         }
-        inline v8::Persistent<T> get()
+        inline v8::Handle<T> get()
         {
             return m_value.get();
         }
--- third_party/WebKit/Source/bindings/v8/V8AbstractEventListener.h
+++ third_party/WebKit/Source/bindings/v8/V8AbstractEventListener.h
@@ -95,9 +95,9 @@ namespace WebCore {
 
         // Provides access to the underlying handle for GC. Returned
         // value is a weak handle and so not guaranteed to stay alive.
-        v8::Persistent<v8::Object> existingListenerObjectPersistentHandle()
+        v8::Persistent<v8::Object>& existingListenerObjectPersistentHandle()
         {
-            return m_listener.get();
+            return m_listener.getUnsafe();
         }
 
         bool hasExistingListenerObject()
