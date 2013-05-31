68ec10dbe7922dde139aa0870aaca5073b772826
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-05-31 22:43:49 +0000

    Use Date.now() rather than new Date().getTime().
    Because Date.now() is faster.
    
    Review URL: https://chromiumcodereview.appspot.com/16228003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203502 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/time_util.js
+++ chrome/browser/resources/net_internals/time_util.js
@@ -50,7 +50,7 @@ var timeutil = (function() {
    * @return {number} Milliseconds since the Unix epoch.
    */
   function getCurrentTime() {
-    return (new Date()).getTime();
+    return Date.now();
   }
 
   /**
