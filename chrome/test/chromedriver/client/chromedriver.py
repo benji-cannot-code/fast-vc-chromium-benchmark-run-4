c4e3dbc4efc3e119f08851046da8b98bb2721a28
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 04:01:34 +0000

    [chromedriver] Convert python client to use server instead of dll. BUG=none
    Review URL: https://chromiumcodereview.appspot.com/16264003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203558 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/client/chromedriver.py
+++ chrome/test/chromedriver/client/chromedriver.py
@@ -1,12 +1,19 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2012 The Chromium Authors. All rights reserved.
+# Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-import ctypes
-import json
+import atexit
+import os
+import socket
+import subprocess
+import time
+import urllib2
 
+import command_executor
+from command_executor import Command
 from webelement import WebElement
+
 
 class ChromeDriverException(Exception):
   pass
@@ -57,13 +64,14 @@ def _ExceptionForResponse(response):
   msg = response['value']['message']
   return exception_class_map.get(status, ChromeDriverException)(msg)
 
+
 class ChromeDriver(object):
   """Starts and controls a single Chrome instance on this machine."""
 
-  def __init__(self, lib_path, chrome_binary=None, android_package=None,
+  def __init__(self, server_url, chrome_binary=None, android_package=None,
                chrome_switches=None, chrome_extensions=None,
                chrome_log_path=None):
-    self._lib = ctypes.CDLL(lib_path)
+    self._executor = command_executor.CommandExecutor(server_url)
 
     options = {}
     if android_package:
@@ -89,7 +97,8 @@ class ChromeDriver(object):
       }
     }
 
-    self._session_id = self._ExecuteCommand('newSession', params)['sessionId']
+    self._session_id = self._executor.Execute(
+        Command.NEW_SESSION, params)['sessionId']
 
   def _WrapValue(self, value):
     """Wrap value from client side for chromedriver side."""
@@ -121,59 +130,42 @@ class ChromeDriver(object):
     else:
       return value
 
-  def _ExecuteCommand(self, name, params={}, session_id=''):
-    cmd = {
-      'name': name,
-      'parameters': params,
-      'sessionId': session_id
-    }
-    cmd_json = json.dumps(cmd)
-    response_data = ctypes.c_char_p()
-    response_size = ctypes.c_uint()
-    self._lib.ExecuteCommand(
-        ctypes.c_char_p(cmd_json),
-        ctypes.c_uint(len(cmd_json)),
-        ctypes.byref(response_data),
-        ctypes.byref(response_size))
-    response_json = ctypes.string_at(response_data, response_size.value)
-    self._lib.Free(response_data)
-    response = json.loads(response_json)
+  def ExecuteCommand(self, command, params={}):
+    params['sessionId'] = self._session_id
+    params = self._WrapValue(params)
+    response = self._executor.Execute(command, params)
     if response['status'] != 0:
       raise _ExceptionForResponse(response)
-    return response
-
-  def ExecuteSessionCommand(self, name, params={}):
-    params = self._WrapValue(params)
-    return self._UnwrapValue(
-        self._ExecuteCommand(name, params, self._session_id)['value'])
+    return self._UnwrapValue(response['value'])
 
   def GetWindowHandles(self):
-    return self.ExecuteSessionCommand('getWindowHandles')
+    return self.ExecuteCommand(Command.GET_WINDOW_HANDLES)
 
   def SwitchToWindow(self, handle_or_name):
-    self.ExecuteSessionCommand('switchToWindow', {'name': handle_or_name})
+    self.ExecuteCommand(Command.SWITCH_TO_WINDOW, {'name': handle_or_name})
 
   def GetCurrentWindowHandle(self):
-    return self.ExecuteSessionCommand('getCurrentWindowHandle')
+    return self.ExecuteCommand(Command.GET_CURRENT_WINDOW_HANDLE)
 
   def CloseWindow(self):
-    self.ExecuteSessionCommand('close')
+    self.ExecuteCommand(Command.CLOSE)
 
   def Load(self, url):
-    self.ExecuteSessionCommand('get', {'url': url})
+    self.ExecuteCommand(Command.GET, {'url': url})
 
   def ExecuteScript(self, script, *args):
     converted_args = list(args)
-    return self.ExecuteSessionCommand(
-        'executeScript', {'script': script, 'args': converted_args})
+    return self.ExecuteCommand(
+        Command.EXECUTE_SCRIPT, {'script': script, 'args': converted_args})
 
   def ExecuteAsyncScript(self, script, *args):
     converted_args = list(args)
-    return self.ExecuteSessionCommand(
-        'executeAsyncScript', {'script': script, 'args': converted_args})
+    return self.ExecuteCommand(
+        Command.EXECUTE_ASYNC_SCRIPT,
+        {'script': script, 'args': converted_args})
 
   def SwitchToFrame(self, id_or_name):
-    self.ExecuteSessionCommand('switchToFrame', {'id': id_or_name})
+    self.ExecuteCommand(Command.SWITCH_TO_FRAME, {'id': id_or_name})
 
   def SwitchToFrameByIndex(self, index):
     self.SwitchToFrame(index)
@@ -182,34 +174,34 @@ class ChromeDriver(object):
     self.SwitchToFrame(None)
 
   def GetTitle(self):
-    return self.ExecuteSessionCommand('getTitle')
+    return self.ExecuteCommand(Command.GET_TITLE)
 
   def GetPageSource(self):
-    return self.ExecuteSessionCommand('getPageSource')
+    return self.ExecuteCommand(Command.GET_PAGE_SOURCE)
 
   def FindElement(self, strategy, target):
-    return self.ExecuteSessionCommand(
-        'findElement', {'using': strategy, 'value': target})
+    return self.ExecuteCommand(
+        Command.FIND_ELEMENT, {'using': strategy, 'value': target})
 
   def FindElements(self, strategy, target):
-    return self.ExecuteSessionCommand(
-        'findElements', {'using': strategy, 'value': target})
+    return self.ExecuteCommand(
+        Command.FIND_ELEMENTS, {'using': strategy, 'value': target})
 
   def SetTimeout(self, type, timeout):
-    return self.ExecuteSessionCommand(
-        'setTimeout', {'type' : type, 'ms': timeout})
+    return self.ExecuteCommand(
+        Command.SET_TIMEOUT, {'type' : type, 'ms': timeout})
 
   def GetCurrentUrl(self):
-    return self.ExecuteSessionCommand('getCurrentUrl')
+    return self.ExecuteCommand(Command.GET_CURRENT_URL)
 
   def GoBack(self):
-    return self.ExecuteSessionCommand('goBack')
+    return self.ExecuteCommand(Command.GO_BACK)
 
   def GoForward(self):
-    return self.ExecuteSessionCommand('goForward')
+    return self.ExecuteCommand(Command.GO_FORWARD)
 
   def Refresh(self):
-    return self.ExecuteSessionCommand('refresh')
+    return self.ExecuteCommand(Command.REFRESH)
 
   def MouseMoveTo(self, element=None, x_offset=None, y_offset=None):
     params = {}
@@ -219,69 +211,134 @@ class ChromeDriver(object):
       params['xoffset'] = x_offset
     if y_offset is not None:
       params['yoffset'] = y_offset
-    self.ExecuteSessionCommand('mouseMoveTo', params)
+    self.ExecuteCommand(Command.MOUSE_MOVE_TO, params)
 
   def MouseClick(self, button=0):
-    self.ExecuteSessionCommand('mouseClick', {'button': button})
+    self.ExecuteCommand(Command.MOUSE_CLICK, {'button': button})
 
   def MouseButtonDown(self, button=0):
-    self.ExecuteSessionCommand('mouseButtonDown', {'button': button})
+    self.ExecuteCommand(Command.MOUSE_BUTTON_DOWN, {'button': button})
 
   def MouseButtonUp(self, button=0):
-    self.ExecuteSessionCommand('mouseButtonUp', {'button': button})
+    self.ExecuteCommand(Command.MOUSE_BUTTON_UP, {'button': button})
 
   def MouseDoubleClick(self, button=0):
-    self.ExecuteSessionCommand('mouseDoubleClick', {'button': button})
+    self.ExecuteCommand(Command.MOUSE_DOUBLE_CLICK, {'button': button})
 
   def GetCookies(self):
-    return self.ExecuteSessionCommand('getCookies')
+    return self.ExecuteCommand(Command.GET_COOKIES)
 
   def AddCookie(self, cookie):
-    self.ExecuteSessionCommand('addCookie', {'cookie': cookie})
+    self.ExecuteCommand(Command.ADD_COOKIE, {'cookie': cookie})
 
   def DeleteCookie(self, name):
-    self.ExecuteSessionCommand('deleteCookie', {'name': name})
+    self.ExecuteCommand(Command.DELETE_COOKIE, {'name': name})
 
   def DeleteAllCookies(self):
-    self.ExecuteSessionCommand('deleteAllCookies')
+    self.ExecuteCommand(Command.DELETE_ALL_COOKIES)
 
   def IsAlertOpen(self):
-    return self.ExecuteSessionCommand('getAlert')
+    return self.ExecuteCommand(Command.GET_ALERT)
 
   def GetAlertMessage(self):
-    return self.ExecuteSessionCommand('getAlertText')
+    return self.ExecuteCommand(Command.GET_ALERT_TEXT)
 
   def HandleAlert(self, accept, prompt_text=''):
     if prompt_text:
-      self.ExecuteSessionCommand('setAlertValue', {'text': prompt_text})
+      self.ExecuteCommand(Command.SET_ALERT_VALUE, {'text': prompt_text})
     if accept:
-      cmd = 'acceptAlert'
+      cmd = Command.ACCEPT_ALERT
     else:
-      cmd = 'dismissAlert'
-    self.ExecuteSessionCommand(cmd)
+      cmd = Command.DISMISS_ALERT
+    self.ExecuteCommand(cmd)
 
   def IsLoading(self):
-    return self.ExecuteSessionCommand('isLoading')
+    return self.ExecuteCommand(Command.IS_LOADING)
 
   def GetWindowPosition(self):
-    position = self.ExecuteSessionCommand('getWindowPosition')
+    position = self.ExecuteCommand(Command.GET_WINDOW_POSITION,
+                                   {'windowHandle': 'current'})
     return [position['x'], position['y']]
 
   def SetWindowPosition(self, x, y):
-    self.ExecuteSessionCommand('setWindowPosition',
-                               {'x': x, 'y': y})
+    self.ExecuteCommand(Command.SET_WINDOW_POSITION,
+                        {'windowHandle': 'current', 'x': x, 'y': y})
 
   def GetWindowSize(self):
-    size = self.ExecuteSessionCommand('getWindowSize')
+    size = self.ExecuteCommand(Command.GET_WINDOW_SIZE,
+                               {'windowHandle': 'current'})
     return [size['width'], size['height']]
 
   def SetWindowSize(self, width, height):
-    self.ExecuteSessionCommand('setWindowSize',
-                               {'width': width, 'height': height})
+    self.ExecuteCommand(
+        Command.SET_WINDOW_SIZE,
+        {'windowHandle': 'current', 'width': width, 'height': height})
 
   def MaximizeWindow(self):
-    self.ExecuteSessionCommand('maximizeWindow')
+    self.ExecuteCommand(Command.MAXIMIZE_WINDOW, {'windowHandle': 'current'})
 
   def Quit(self):
     """Quits the browser and ends the session."""
-    self.ExecuteSessionCommand('quit')
+    self.ExecuteCommand(Command.QUIT)
+
+
+class Server(object):
+  """A running ChromeDriver server."""
+
+  def __init__(self, exe_path):
+    """Starts the ChromeDriver server and waits for it to be ready.
+
+    Args:
+      exe_path: path to the ChromeDriver executable
+    Raises:
+      RuntimeError if ChromeDriver fails to start
+    """
+    if not os.path.exists(exe_path):
+      raise RuntimeError('ChromeDriver exe not found at: ' + exe_path)
+
+    port = self._FindOpenPort()
+    chromedriver_args = [exe_path, '--port=%d' % port]
+    self._process = subprocess.Popen(chromedriver_args)
+    self._url = 'http://127.0.0.1:%d' % port
+    if self._process is None:
+      raise RuntimeError('ChromeDriver server cannot be started')
+
+    max_time = time.time() + 10
+    while not self.IsRunning():
+      if time.time() > max_time:
+        self._process.terminate()
+        raise RuntimeError('ChromeDriver server did not start')
+      time.sleep(0.1)
+
+    atexit.register(self.Kill)
+
+  def _FindOpenPort(self):
+    for port in range(9500, 10000):
+      try:
+        socket.create_connection(('127.0.0.1', port), 0.2).close()
+      except socket.error:
+        return port
+    raise RuntimeError('Cannot find open port to launch ChromeDriver')
+
+  def GetUrl(self):
+    return self._url
+
+  def IsRunning(self):
+    """Returns whether the server is up and running."""
+    try:
+      urllib2.urlopen(self.GetUrl() + '/status')
+      return True
+    except urllib2.URLError:
+      return False
+
+  def Kill(self):
+    """Kills the ChromeDriver server, if it is running."""
+    if self._process is None:
+      return
+
+    try:
+      urllib2.urlopen(self.GetUrl() + '/shutdown', timeout=10).close()
+    except:
+      self._process.terminate()
+    self._process.wait()
+    self._process = None
--- chrome/test/chromedriver/client/webelement.py
+++ chrome/test/chromedriver/client/webelement.py
@@ -1,7 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Copyright 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
+
+from command_executor import Command
+
 
 class WebElement(object):
   """Represents an HTML element."""
@@ -13,27 +16,27 @@ class WebElement(object):
     if params is None:
       params = {}
     params['id'] = self._id;
-    return self._chromedriver.ExecuteSessionCommand(command, params)
+    return self._chromedriver.ExecuteCommand(command, params)
 
   def FindElement(self, strategy, target):
     return self._Execute(
-        'findChildElement', {'using': strategy, 'value': target})
+        Command.FIND_CHILD_ELEMENT, {'using': strategy, 'value': target})
 
   def FindElements(self, strategy, target):
     return self._Execute(
-        'findChildElements', {'using': strategy, 'value': target})
+        Command.FIND_CHILD_ELEMENTS, {'using': strategy, 'value': target})
 
   def HoverOver(self):
-    self._Execute('hoverOverElement')
+    self._Execute(Command.HOVER_OVER_ELEMENT)
 
   def Click(self):
-    self._Execute('clickElement')
+    self._Execute(Command.CLICK_ELEMENT)
 
   def SingleTap(self):
-    self._Execute('touchSingleTap')
+    self._Execute(Command.TOUCH_SINGLE_TAP)
 
   def Clear(self):
-    self._Execute('clearElement')
+    self._Execute(Command.CLEAR_ELEMENT)
 
   def SendKeys(self, *values):
     typing = []
@@ -42,4 +45,4 @@ class WebElement(object):
         value = str(value)
       for i in range(len(value)):
         typing.append(value[i])
-    self._Execute('sendKeysToElement', {'value': typing})
+    self._Execute(Command.SEND_KEYS_TO_ELEMENT, {'value': typing})
