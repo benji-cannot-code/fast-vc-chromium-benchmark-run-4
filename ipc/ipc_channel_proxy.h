bdedfd0373bd4bf9fce908b4622c77b2e346813a
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 04:11:27 +0000

    Update ipc/ to use scoped_refptr<T>::get() rather than implicit "operator T*"
    Linux fixes
    
    BUG=110610
    TBR=darin
    
    Review URL: https://chromiumcodereview.appspot.com/15703006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203562 0039d316-1c4b-4281-b951-d872f2087c98

--- ipc/ipc_channel_proxy.h
+++ ipc/ipc_channel_proxy.h
@@ -198,7 +198,7 @@ class IPC_EXPORT ChannelProxy : public Sender, public base::NonThreadSafe {
     Context(Listener* listener, base::SingleThreadTaskRunner* ipc_thread);
     void ClearIPCTaskRunner();
     base::SingleThreadTaskRunner* ipc_task_runner() const {
-      return ipc_task_runner_;
+      return ipc_task_runner_.get();
     }
     const std::string& channel_id() const { return channel_id_; }
 
@@ -269,7 +269,7 @@ class IPC_EXPORT ChannelProxy : public Sender, public base::NonThreadSafe {
     base::ProcessId peer_pid_;
   };
 
-  Context* context() { return context_; }
+  Context* context() { return context_.get(); }
 
   OutgoingMessageFilter* outgoing_message_filter() {
     return outgoing_message_filter_;
