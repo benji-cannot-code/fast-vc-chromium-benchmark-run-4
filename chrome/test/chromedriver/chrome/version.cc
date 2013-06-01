99686dda824d439ecfd071cc6c6baa2ce4e42865
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 00:36:42 +0000

    [chromedriver] Release ChromeDriver 2.0. BUG=none NOTRY=true
    Review URL: https://chromiumcodereview.appspot.com/15968013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203532 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/chrome/version.cc
+++ chrome/test/chromedriver/chrome/version.cc
@@ -9,12 +9,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
+// This variable must be able to be found and parsed by the upload script.
 const int kMinimumSupportedChromeVersion[] = {27, 0, 1453, 0};
 
 }  // namespace
 
 // This variable must be able to be found and parsed by the upload script.
-const char kChromeDriverVersion[] = "0.9";
+const char kChromeDriverVersion[] = "2.0";
 
 const int kMinimumSupportedChromeBuildNo = kMinimumSupportedChromeVersion[2];
 
