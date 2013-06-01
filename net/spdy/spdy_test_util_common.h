21753d4d49c65a3990aa9cc75b8c2438a85674a1
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 04:31:45 +0000

    [SPDY] Merge the SpdyStream unit tests and parametrize them on a NextProto
    Add support for SPDY/3.1 and SPDY/4a2.
    
    Make SpdyTestUtil::*Key() methods public.
    
    Add a utility function to inject the right headers for
    a URL into a SpdyHeaderBlock.
    
    BUG=226192
    
    Review URL: https://chromiumcodereview.appspot.com/15731012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203570 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_test_util_common.h
+++ net/spdy/spdy_test_util_common.h
@@ -250,6 +250,10 @@ class SpdyTestUtil {
  public:
   explicit SpdyTestUtil(NextProto protocol);
 
+  // Add the appropriate headers to put |url| into |block|.
+  void AddUrlToHeaderBlock(base::StringPiece url,
+                           SpdyHeaderBlock* headers) const;
+
   scoped_ptr<SpdyHeaderBlock> ConstructGetHeaderBlock(
       base::StringPiece url) const;
   scoped_ptr<SpdyHeaderBlock> ConstructPostHeaderBlock(
@@ -459,7 +463,6 @@ class SpdyTestUtil {
   bool is_spdy2() const { return protocol_ < kProtoSPDY3; }
   scoped_ptr<SpdyFramer> CreateFramer() const;
 
- private:
   const char* GetMethodKey() const;
   const char* GetStatusKey() const;
   const char* GetHostKey() const;
@@ -467,6 +470,7 @@ class SpdyTestUtil {
   const char* GetVersionKey() const;
   const char* GetPathKey() const;
 
+ private:
   const NextProto protocol_;
   const SpdyMajorVersion spdy_version_;
 };
