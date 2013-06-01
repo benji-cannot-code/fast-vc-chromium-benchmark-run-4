1b77963f1d7d34dd26e5a57e4ee817692d6b2bde
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 00:40:58 +0000

    Enable/Disable QUIC via a field trial (or about:flags).
    BUG=230028
    
    Review URL: https://chromiumcodereview.appspot.com/15736025
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203536 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "base/debug/leak_tracker.h"
 #include "base/logging.h"
+#include "base/metrics/field_trial.h"
 #include "base/prefs/pref_registry_simple.h"
 #include "base/prefs/pref_service.h"
 #include "base/stl_util.h"
@@ -96,6 +97,9 @@ class SafeBrowsingURLRequestContext;
 // Quit task, so base::Bind() calls are not refcounted.
 
 namespace {
+
+const char kQuicFieldTrialName[] = "QUIC";
+const char kQuicFieldTrialEnabledGroupName[] = "Enabled";
 
 #if defined(OS_MACOSX) && !defined(OS_IOS)
 void ObserveKeychainEvents() {
@@ -532,9 +536,7 @@ void IOThread::Init() {
     globals_->testing_fixed_https_port =
         GetSwitchValueAsInt(command_line, switches::kTestingFixedHttpsPort);
   }
-  if (command_line.HasSwitch(switches::kEnableQuic)) {
-    globals_->enable_quic.set(true);
-  }
+  globals_->enable_quic.set(ShouldEnableQuic(command_line));
   if (command_line.HasSwitch(switches::kOriginPortToForceQuicOn)) {
     globals_->origin_port_to_force_quic_on.set(
         GetSwitchValueAsInt(command_line,
@@ -937,4 +939,20 @@ void IOThread::InitSystemRequestContextOnIOThread() {
 
 void IOThread::UpdateDnsClientEnabled() {
   globals()->host_resolver->SetDnsClientEnabled(*dns_client_enabled_);
+}
+
+bool IOThread::ShouldEnableQuic(const CommandLine& command_line) {
+  // Always fetch the field trial group to ensure it is reported correctly.
+  // The command line flags will be associated with a group that is reported
+  // so long as trial is actually queried.
+  std::string quic_trial_group =
+      base::FieldTrialList::FindFullName(kQuicFieldTrialName);
+
+  if (command_line.HasSwitch(switches::kDisableQuic))
+    return false;
+
+  if (command_line.HasSwitch(switches::kEnableQuic))
+    return true;
+
+  return quic_trial_group == kQuicFieldTrialEnabledGroupName;
 }
--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -113,6 +113,7 @@ extern const char kDisablePopupBlocking[];
 extern const char kDisablePreconnect[];
 extern const char kDisablePrerenderLocalPredictor[];
 extern const char kDisablePromptOnRepost[];
+extern const char kDisableQuic[];
 extern const char kDisableRestoreBackgroundContents[];
 extern const char kDisableRestoreSessionState[];
 extern const char kDisableScriptedPrintThrottling[];
