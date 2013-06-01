a59dea1f83d1227e687b3cc85e1df3d81d1f0fca
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 00:56:24 +0000

    IndexedDB: Make new backend (still disabled) pass all tests
    Two bugs were introduced in the new (still disabled) backend by
    platform-specific fixes while trying to land it:
    * Zero-length slices were blocked (while avoiding iterator deferences)
    * Filenames were incorrectly computed (while moving to FilePath)
    
    Also ran clang-format over the new files to correct a
    few formatting nits.
    
    R=alecflett@chromium.org
    BUG=234278
    
    Review URL: https://chromiumcodereview.appspot.com/16085008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203540 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/leveldb/fixed_array.h
+++ content/browser/indexed_db/leveldb/fixed_array.h
@@ -35,7 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-template <typename T, size_t Size> class FixedArray {
+template <typename T, size_t Size>
+class FixedArray {
  public:
   T& operator[](size_t i) {
 #if defined(ADDRESS_SANITIZER)
--- content/browser/indexed_db/list_set.h
+++ content/browser/indexed_db/list_set.h
@@ -20,7 +20,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Member types of value (primitives and objects by value), raw pointers
 // and scoped_refptr<> are supported.
 //
-template <typename T> class list_set {
+template <typename T>
+class list_set {
  public:
   list_set() {}
   list_set(const list_set<T>& other) : list_(other.list_), set_(other.set_) {}
@@ -154,6 +155,7 @@ template <typename T> class list_set {
 // Prevent instantiation of list_set<scoped_ptr<T>> as the current
 // implementation would fail.
 // TODO(jsbell): Support scoped_ptr through specialization.
-template <typename T> class list_set<scoped_ptr<T> >;
+template <typename T>
+class list_set<scoped_ptr<T> >;
 
 #endif  // CONTENT_BROWSER_INDEXED_DB_LIST_SET_H_
