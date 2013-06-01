3b9ad9223a4e0aad21189a53c0ef831bf7b2ab6c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-01 04:02:12 +0000

    Accessor to compositor thread  Taken from https://chromiumcodereview.appspot.com/15020003/ to use for compositor based mirroring.
    BUG=239776
    
    Review URL: https://chromiumcodereview.appspot.com/16272008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203560 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.h
+++ ui/compositor/compositor.h
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class SkBitmap;
 
 namespace base {
+class MessageLoopProxy;
 class RunLoop;
 }
 
@@ -261,6 +262,7 @@ class COMPOSITOR_EXPORT Compositor
 
   static void Initialize();
   static bool WasInitializedWithThread();
+  static scoped_refptr<base::MessageLoopProxy> GetCompositorMessageLoop();
   static void Terminate();
 
   // Schedules a redraw of the layer tree associated with this compositor.
