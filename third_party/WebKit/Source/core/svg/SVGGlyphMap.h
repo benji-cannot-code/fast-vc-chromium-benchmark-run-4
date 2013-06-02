920ea3c2f67011319fdc17ae9f14b99a0f88e720
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-02 22:19:21 +0000

    Don't pre-reserve massive amounts of memory in SVGGlyphMap.
    This reduces the size of SVGFontElement from 20KB_ to ~200 bytes.
    
    R=pdr@chromium.org
    
    Review URL: https://codereview.chromium.org/15941019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151616 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGGlyphMap.h
+++ third_party/WebKit/Source/core/svg/SVGGlyphMap.h
@@ -156,7 +156,7 @@ public:
 
 private:
     GlyphMapLayer m_rootLayer;
-    Vector<SVGGlyph, 256> m_glyphTable;
+    Vector<SVGGlyph> m_glyphTable;
     HashMap<String, Glyph> m_namedGlyphs;
     int m_currentPriority;
 };
