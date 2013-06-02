cf58b2e5499b9929d61c26bebc1454997cbf2c9c
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-02 19:17:35 +0000

    Avoid creating PriorityResourceManager if not needed
    Do not create PriorityResourceManager if using
    impl_side_painting and solid_scroll_bars.
    
    BUG=230197
    
    Review URL: https://chromiumcodereview.appspot.com/15650020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203616 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/single_thread_proxy.cc
+++ cc/trees/single_thread_proxy.cc
@@ -186,8 +186,10 @@ void SingleThreadProxy::DoCommit(scoped_ptr<ResourceUpdateQueue> queue) {
 
     layer_tree_host_impl_->BeginCommit();
 
-    layer_tree_host_->contents_texture_manager()->
-        PushTexturePrioritiesToBackings();
+    if (layer_tree_host_->contents_texture_manager()) {
+      layer_tree_host_->contents_texture_manager()->
+          PushTexturePrioritiesToBackings();
+    }
     layer_tree_host_->BeginCommitOnImplThread(layer_tree_host_impl_.get());
 
     scoped_ptr<ResourceUpdateController> update_controller =
@@ -391,7 +393,10 @@ bool SingleThreadProxy::CommitAndComposite(
       created_offscreen_context_provider_ = true;
   }
 
-  layer_tree_host_->contents_texture_manager()->UnlinkAndClearEvictedBackings();
+  if (layer_tree_host_->contents_texture_manager()) {
+    layer_tree_host_->contents_texture_manager()
+        ->UnlinkAndClearEvictedBackings();
+  }
 
   scoped_ptr<ResourceUpdateQueue> queue =
       make_scoped_ptr(new ResourceUpdateQueue);
