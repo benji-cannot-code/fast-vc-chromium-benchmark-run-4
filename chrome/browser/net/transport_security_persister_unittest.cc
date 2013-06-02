a61e241b4026670c64dc3746f02b7b8c7e4c8b1f
palmer@chromium.org <palmer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-02 23:49:07 +0000

    We don't need DomainState::Equals.
    It never gets called in the current implementation, and the concept of
    DomainState is changing to better support HPKP and TACK.
    
    BUG=171025
    TBR=cbentzel,agl
    
    Review URL: https://chromiumcodereview.appspot.com/16236007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203639 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/transport_security_persister_unittest.cc
+++ chrome/browser/net/transport_security_persister_unittest.cc
@@ -153,7 +153,6 @@ TEST_F(TransportSecurityPersisterTest, SerializeData3) {
   size_t count = 0;
   TransportSecurityState::Iterator j(state_);
   while (j.HasNext()) {
-    EXPECT_TRUE(saved[j.hostname()].Equals(j.domain_state()));
     count++;
     j.Advance();
   }
