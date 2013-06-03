7020e5ad777c2aa933a73fcdbb774de22989a878
kojih@chromium.org <kojih@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-03 16:48:40 +0000

    Auto-generate V8StyleSheetList::namedPropertyGetter
    This patch is once landed (150663)
    but I noticed my mistake and reverted.
    Now it's OK because Blink checks own property
    if [OverrideBuiltins] not specified.
    
    R=haraken@chromium.org 
    BUG=229740
    TEST=No tests. refactoring.
    
    Review URL: https://chromiumcodereview.appspot.com/16321004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151669 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/StyleSheetList.cpp
+++ third_party/WebKit/Source/core/css/StyleSheetList.cpp
@@ -81,4 +81,12 @@ HTMLStyleElement* StyleSheetList::getNamedItem(const String& name) const
     return 0;
 }
 
+CSSStyleSheet* StyleSheetList::anonymousNamedGetter(const AtomicString& name)
+{
+    HTMLStyleElement* item = getNamedItem(name);
+    if (!item)
+        return 0;
+    return item->sheet();
+}
+
 } // namespace WebCore
