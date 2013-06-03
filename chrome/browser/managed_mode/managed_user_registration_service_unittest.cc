7e4f47d39bd2e285049ff69c6f194b447dbf0427
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-03 16:27:34 +0000

    Reland 203015 "Add ManagedUserTokenFetcher to fetch scoped-down tokens"
    The memory leak in the original version is fixed, by running the message loop at test tear down.
    
    TBR=atwilson@chromium.org,rogerta@chromium.org
    
    BUG=228833
    
    Review URL: https://chromiumcodereview.appspot.com/16173008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203723 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_user_registration_service_unittest.cc
+++ chrome/browser/managed_mode/managed_user_registration_service_unittest.cc
@@ -6,7 +6,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/bind.h"
+#include "base/message_loop.h"
+#include "base/run_loop.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/managed_mode/managed_user_refresh_token_fetcher.h"
 #include "chrome/browser/managed_mode/managed_user_registration_service.h"
 #include "chrome/browser/prefs/scoped_user_pref_update.h"
 #include "chrome/common/pref_names.h"
@@ -17,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sync/protocol/sync.pb.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
-using ::sync_pb::ManagedUserSpecifics;
+using sync_pb::ManagedUserSpecifics;
 using syncer::MANAGED_USERS;
 using syncer::SyncChange;
 using syncer::SyncChangeList;
@@ -29,6 +32,8 @@ using syncer::SyncErrorFactory;
 using syncer::SyncMergeResult;
 
 namespace {
+
+const char kManagedUserToken[] = "managedusertoken";
 
 class MockChangeProcessor : public SyncChangeProcessor {
  public:
@@ -63,12 +68,30 @@ SyncChange MockChangeProcessor::GetChange(const std::string& id) const {
   return SyncChange();
 }
 
+class MockManagedUserRefreshTokenFetcher
+    : public ManagedUserRefreshTokenFetcher {
+ public:
+  MockManagedUserRefreshTokenFetcher() {}
+  virtual ~MockManagedUserRefreshTokenFetcher() {}
+
+  // ManagedUserRefreshTokenFetcher implementation:
+  virtual void Start(const std::string& managed_user_id,
+                     const string16& name,
+                     const std::string& device_name,
+                     const TokenCallback& callback) OVERRIDE {
+    GoogleServiceAuthError error(GoogleServiceAuthError::NONE);
+    callback.Run(error, kManagedUserToken);
+  }
+};
+
 }  // namespace
 
 class ManagedUserRegistrationServiceTest : public ::testing::Test {
  public:
   ManagedUserRegistrationServiceTest();
   virtual ~ManagedUserRegistrationServiceTest();
+
+  virtual void TearDown() OVERRIDE;
 
  protected:
   scoped_ptr<SyncChangeProcessor> CreateChangeProcessor();
@@ -95,6 +118,8 @@ class ManagedUserRegistrationServiceTest : public ::testing::Test {
   void OnManagedUserRegistered(const GoogleServiceAuthError& error,
                                const std::string& token);
 
+  base::MessageLoop message_loop_;
+  base::RunLoop run_loop_;
   base::WeakPtrFactory<ManagedUserRegistrationServiceTest> weak_ptr_factory_;
   TestingPrefServiceSyncable prefs_;
   scoped_ptr<ManagedUserRegistrationService> service_;
@@ -115,16 +140,23 @@ class ManagedUserRegistrationServiceTest : public ::testing::Test {
 
 ManagedUserRegistrationServiceTest::ManagedUserRegistrationServiceTest()
     : weak_ptr_factory_(this),
-      service_(new ManagedUserRegistrationService(&prefs_)),
       change_processor_(NULL),
       sync_data_id_(0),
       received_callback_(false),
       error_(GoogleServiceAuthError::NUM_STATES) {
   ManagedUserRegistrationService::RegisterUserPrefs(prefs_.registry());
+  scoped_ptr<ManagedUserRefreshTokenFetcher> token_fetcher(
+      new MockManagedUserRefreshTokenFetcher);
+  service_.reset(
+      new ManagedUserRegistrationService(&prefs_, token_fetcher.Pass()));
 }
 
 ManagedUserRegistrationServiceTest::~ManagedUserRegistrationServiceTest() {
   EXPECT_FALSE(weak_ptr_factory_.HasWeakPtrs());
+}
+
+void ManagedUserRegistrationServiceTest::TearDown() {
+  base::RunLoop().RunUntilIdle();
 }
 
 scoped_ptr<SyncChangeProcessor>
@@ -181,6 +213,8 @@ void ManagedUserRegistrationServiceTest::Acknowledge() {
                    SyncData::CreateRemoteData(++sync_data_id_, specifics)));
   }
   service()->ProcessSyncChanges(FROM_HERE, new_changes);
+
+  run_loop_.Run();
 }
 
 void ManagedUserRegistrationServiceTest::ResetService() {
@@ -195,6 +229,7 @@ void ManagedUserRegistrationServiceTest::OnManagedUserRegistered(
   received_callback_ = true;
   error_ = error;
   token_ = token;
+  run_loop_.Quit();
 }
 
 TEST_F(ManagedUserRegistrationServiceTest, MergeEmpty) {
