b7913a5ed620c978c35b4ffaff52eadf12d4971d
xians@chromium.org <xians@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-03 11:09:05 +0000

    Try relanding this CL, the original CL passed the try bots but failed the mac 10.7 bot because the oroginal CL queried the device info while there is no device on the bot.
    Hook up the device selection to the WebAudio live audio.
    WebAudio live audio needs to pass the session_id to the browser process so that Chrome can open the correct input device for unitfied IO.
    
    This CL looks big because it touches quite some interfaces from the render to the browser. But the change is simple and basically adding a session_id/device_id to the classes. All the changes some together and it is very hard to break it down.
    It also makes the media output code more similar to the media input code as well, and it will be easier to merge them for the future.
    
    TBR=henrika@chormium.org
    
    BUG=147327
    TEST=http://chromium.googlecode.com/svn/trunk/samples/audio/visualizer-live.html
    Change the device using the camera icon on the right of the omnibox, then reload. Verify the sound is coming from the correct input device.
    
    Review URL: https://codereview.chromium.org/15979015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203695 0039d316-1c4b-4281-b951-d872f2087c98

--- media/audio/android/audio_manager_android.h
+++ media/audio/android/audio_manager_android.h
@@ -25,7 +25,8 @@ class MEDIA_EXPORT AudioManagerAndroid : public AudioManagerBase {
       const std::string& device_id) OVERRIDE;
 
   virtual AudioOutputStream* MakeAudioOutputStream(
-      const AudioParameters& params) OVERRIDE;
+      const AudioParameters& params,
+      const std::string& input_device_id) OVERRIDE;
   virtual AudioInputStream* MakeAudioInputStream(
       const AudioParameters& params, const std::string& device_id) OVERRIDE;
   virtual void ReleaseOutputStream(AudioOutputStream* stream) OVERRIDE;
@@ -35,7 +36,8 @@ class MEDIA_EXPORT AudioManagerAndroid : public AudioManagerBase {
   virtual AudioOutputStream* MakeLinearOutputStream(
       const AudioParameters& params) OVERRIDE;
   virtual AudioOutputStream* MakeLowLatencyOutputStream(
-      const AudioParameters& params) OVERRIDE;
+      const AudioParameters& params,
+      const std::string& input_device_id) OVERRIDE;
   virtual AudioInputStream* MakeLinearInputStream(
       const AudioParameters& params, const std::string& device_id) OVERRIDE;
   virtual AudioInputStream* MakeLowLatencyInputStream(
