645b591737681de60370dbf4e78b8b2fe135d89d
marja@chromium.org <marja@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-03 09:38:58 +0000

    Don't copy peristents when getting the opaque root for GC.
    This gets rid of more places where Persistent handles are copied
    (see bug).
    
    BUG=236290
    
    Review URL: https://chromiumcodereview.appspot.com/15736022
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151640 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8NodeListCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8NodeListCustom.cpp
@@ -43,9 +43,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-void* V8NodeList::opaqueRootForGC(void* object, v8::Persistent<v8::Object> wrapper, v8::Isolate* isolate)
+void* V8NodeList::opaqueRootForGC(void* object, v8::Isolate* isolate)
 {
-    ASSERT(V8NodeList::HasInstanceInAnyWorld(wrapper, isolate));
     NodeList* impl = static_cast<NodeList*>(object);
     if (!impl->isLiveNodeList())
         return object;
