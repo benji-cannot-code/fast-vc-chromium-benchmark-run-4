b2620a75a231f1763c4612b9e6fee2121aea7448
jochen@chromium.org <jochen@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-03 11:14:40 +0000

    Don't notify the embedder that a provisional load failed after the test finished
    As soon as a test finishes, the TestRunner will delete the provisional
    loader, and the embedder is confused if didFailProvisionalLoadForFrame
    is invoked without a provisional loader
    
    BUG=240272
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/16210004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151648 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/public/WebTestProxy.h
@@ -181,7 +181,7 @@ protected:
     void didCancelClientRedirect(WebKit::WebFrame*);
     void didStartProvisionalLoad(WebKit::WebFrame*);
     void didReceiveServerRedirectForProvisionalLoad(WebKit::WebFrame*);
-    void didFailProvisionalLoad(WebKit::WebFrame*, const WebKit::WebURLError&);
+    bool didFailProvisionalLoad(WebKit::WebFrame*, const WebKit::WebURLError&);
     void didCommitProvisionalLoad(WebKit::WebFrame*, bool isNewNavigation);
     void didReceiveTitle(WebKit::WebFrame*, const WebKit::WebString& title, WebKit::WebTextDirection);
     void didChangeIcon(WebKit::WebFrame*, WebKit::WebIconURL::Type);
@@ -468,7 +468,10 @@ public:
     }
     virtual void didFailProvisionalLoad(WebKit::WebFrame* frame, const WebKit::WebURLError& error)
     {
-        WebTestProxyBase::didFailProvisionalLoad(frame, error);
+        // If the test finished, don't notify the embedder of the failed load,
+        // as we already destroyed the document loader.
+        if (WebTestProxyBase::didFailProvisionalLoad(frame, error))
+            return;
         Base::didFailProvisionalLoad(frame, error);
     }
     virtual void didCommitProvisionalLoad(WebKit::WebFrame* frame, bool isNewNavigation)
