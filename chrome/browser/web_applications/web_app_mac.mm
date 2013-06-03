08a4b5ac18daa9985f79637a5add745d116d1ef8
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-03 10:19:57 +0000

    Only launch shim if --enable-app-shims is set.
    BUG=168080
    
    Review URL: https://chromiumcodereview.appspot.com/16300002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203689 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_mac.mm
+++ chrome/browser/web_applications/web_app_mac.mm
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/web_applications/web_app_ui.h"
 #include "chrome/browser/web_applications/web_app.h"
 #include "chrome/common/chrome_paths_internal.h"
+#include "chrome/common/chrome_switches.h"
 #include "chrome/common/mac/app_mode_common.h"
 #include "content/public/browser/browser_thread.h"
 #include "grit/chromium_strings.h"
@@ -345,6 +346,9 @@ base::FilePath GetAppInstallPath(
 }
 
 void MaybeLaunchShortcut(const ShellIntegration::ShortcutInfo& shortcut_info) {
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kEnableAppShims))
+    return;
+
   content::BrowserThread::PostTask(
       content::BrowserThread::FILE, FROM_HERE,
       base::Bind(&LaunchShimOnFileThread, shortcut_info));
