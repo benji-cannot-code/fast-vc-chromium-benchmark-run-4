d2b5e5105eea6334711ea6fd248aae88d70bbe14
simonjam@chromium.org <simonjam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-04 03:22:22 +0000

    Fix resource scheduling on documents with <frameset>.
    The ResourceScheduler relies on a signal that the parser has seen enough content
    before it starts loading images. Normally the signal is sent when the parser
    reaches the <body>. However, documents that use <frameset> typically don't have
    a <body>, so they don't issue the signal to the scheduler. In extreme cases,
    such as "forever frames," that means images are never loaded, because the
    scheduler never receives any signals from the renderer that it's ok to do so.
    
    Now, the same signal is sent to the scheduler when the parser encounters either <body> or <frameset>.
    
    BUG=244043
    
    Review URL: https://chromiumcodereview.appspot.com/16140024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151718 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLFrameSetElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLFrameSetElement.cpp
@@ -35,6 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/dom/NodeRenderingContext.h"
 #include "core/html/HTMLCollection.h"
 #include "core/html/HTMLFrameElement.h"
+#include "core/loader/FrameLoaderClient.h"
+#include "core/page/Frame.h"
 #include "core/platform/Length.h"
 #include "core/rendering/RenderFrameSet.h"
 
@@ -196,6 +198,15 @@ void HTMLFrameSetElement::defaultEventHandler(Event* evt)
         }
     }
     HTMLElement::defaultEventHandler(evt);
+}
+
+Node::InsertionNotificationRequest HTMLFrameSetElement::insertedInto(ContainerNode* insertionPoint)
+{
+    if (insertionPoint->inDocument() && document()->frame()) {
+        // A document using <frameset> likely won't literally have a body, but as far as the client is concerned, the frameset is effectively the body.
+        document()->frame()->loader()->client()->dispatchWillInsertBody();
+    }
+    return HTMLElement::insertedInto(insertionPoint);
 }
 
 void HTMLFrameSetElement::willRecalcStyle(StyleChange)
