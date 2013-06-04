d62a4ad89fcfd6e680f36d31d1fd870a35256dee
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-04 14:27:41 +0000

    DevTools: Suspend DevTools frontend in willEnterModalLoop if it is running in a separate process.
    Added InspectorPageGroup and made a single static instance of it.
    WebView::willEnterModalLoop now suspends inspectorGroup when sharedGroup is empty.
    This is a preliminary step in supporting debugging DevTools in DevTools without missing breakpoints.
    The newly added inspectorGroup is only needed to support running debugger layout tests in DRT. It could be removed after DRT removal, the patch is here: https://chromiumcodereview.appspot.com/15980010/
    
    R=pfeldman@chromium.org, yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/16358004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/Page.cpp
+++ third_party/WebKit/Source/core/page/Page.cpp
@@ -332,6 +332,9 @@ void Page::setGroupType(PageGroupType type)
     clearPageGroup();
 
     switch (type) {
+    case InspectorPageGroup:
+        m_group = PageGroup::inspectorGroup();
+        break;
     case PrivatePageGroup:
         m_group = PageGroup::create();
         break;
