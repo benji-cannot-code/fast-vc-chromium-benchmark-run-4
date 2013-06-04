8b85768ed7ee895875e3d70fdba2bcfbd2d6864d
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-04 11:11:57 +0000

    Fix for finding a disabled origin after it was already moved to enabled.
    BUG=245268
    TEST=
    DriveFileSyncServiceTest.DisableOriginForTrackingChangesPendingOrigin
    DriveFileSyncServiceTest.DisableOriginForTrackingChangesIncrementalOrigin
    DriveFileSyncServiceTest.ReenableOriginForTrackingChanges
    
    Review URL: https://chromiumcodereview.appspot.com/16232019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203927 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service.cc
+++ chrome/browser/sync_file_system/sync_file_system_service.cc
@@ -463,7 +463,7 @@ void SyncFileSystemService::Observe(
   // Update:          INSTALLED.
   // Uninstall:       UNLOADED(UNINSTALL).
   // Launch, Close:   No notification.
-  // Enable:          EABLED.
+  // Enable:          ENABLED.
   // Disable:         UNLOADED(DISABLE).
   // Reload, Restart: UNLOADED(DISABLE) -> INSTALLED -> ENABLED.
   //
