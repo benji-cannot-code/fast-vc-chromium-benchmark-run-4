1c428c45c65c223e163df2cefb7b1c6ee397f5ae
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-04 00:35:18 +0000

    Generate & use builder data from builders.jsonp.
    Modify generate_builders_json.py to generate group/master-related data. The old data is a top-level field 'masters', and a sibling field is 'groups'.
    
    Adapt builder.js and other pieces to use the generated data. A couple of pieces assumed this lookup was stateful (e.g. pieces which removed unloadable builders), so for now store the most recently generated list of builders in a stateful global.
    
    I have to check in a new version builders.jsonp with this change, since I've changed the format slightly.
    
    Committed on behalf of mmontgomery@chromium.org.
    
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/14113036
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151711 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/history.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/history.js
@@ -37,7 +37,7 @@ history.DEFAULT_CROSS_DASHBOARD_STATE_VALUES = {
     showAllRuns: false,
     testType: 'layout-tests',
     useTestData: false,
-}    
+}
 
 history.validateParameter = function(state, key, value, validateFn)
 {
@@ -164,7 +164,7 @@ history.History.prototype = {
         var oldDashboardSpecificState = this.dashboardSpecificState;
 
         this.parseCrossDashboardParameters();
-        
+
         // Some parameters require loading different JSON files when the value changes. Do a reload.
         if (Object.keys(oldCrossDashboardState).length) {
             for (var key in this.crossDashboardState) {
@@ -217,10 +217,7 @@ history.History.prototype = {
         case 'group':
             history.validateParameter(this.crossDashboardState, key, value,
                 function() {
-                  return value in LAYOUT_TESTS_BUILDER_GROUPS ||
-                      value in CHROMIUM_GPU_TESTS_BUILDER_GROUPS ||
-                      value in CHROMIUM_INSTRUMENTATION_TESTS_BUILDER_GROUPS ||
-                      value in CHROMIUM_GTESTS_BUILDER_GROUPS;
+                    return builders.getAllGroupNames().indexOf(value) != -1;
                 });
             return true;
 
@@ -236,7 +233,7 @@ history.History.prototype = {
     queryParameterValue: function(parameter)
     {
         return this.dashboardSpecificState[parameter] || this.crossDashboardState[parameter];
-    }, 
+    },
     // Sets the page state. Takes varargs of key, value pairs.
     setQueryParameter: function(var_args)
     {
@@ -281,7 +278,7 @@ history.History.prototype = {
                 state.push(key + '=' + encodeURIComponent(value));
         }
         return state.join('&');
-    }, 
+    },
     _permaLinkURLHash: function(opt_state)
     {
         var state = opt_state || this._combinedDashboardState();
@@ -292,7 +289,7 @@ history.History.prototype = {
         var combinedState = Object.create(this.dashboardSpecificState);
         for (var key in this.crossDashboardState)
             combinedState[key] = this.crossDashboardState[key];
-        return combinedState;    
+        return combinedState;
     },
     _defaultValue: function(key)
     {
--- third_party/WebKit/Tools/TestResultServer/static-dashboards/loader.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/loader.js
@@ -96,7 +96,7 @@ loader.Loader.prototype = {
     {
         this._loadNext();
     },
-    showErrors: function() 
+    showErrors: function()
     {
         this._errors.show();
     },
@@ -112,7 +112,7 @@ loader.Loader.prototype = {
     },
     _loadBuildersList: function()
     {
-        loadBuildersList(currentBuilderGroupName(), this._history.crossDashboardState.testType);
+        builders.loadBuildersList(currentBuilderGroupName(), this._history.crossDashboardState.testType);
         this._loadNext();
     },
     _loadResultsFiles: function()
@@ -164,6 +164,8 @@ loader.Loader.prototype = {
     {
         var builds = JSON.parse(fileData);
 
+        if (builderName == 'version' || builderName == 'failure_map')
+             return;
         // If a test suite stops being run on a given builder, we don't want to show it.
         // Assume any builder without a run in two weeks for a given test suite isn't
         // running that suite anymore.
@@ -199,8 +201,8 @@ loader.Loader.prototype = {
     },
     _haveResultsFilesLoaded: function()
     {
-        for (var builder in currentBuilders()) {
-            if (!g_resultsByBuilder[builder])
+        for (var builderName in currentBuilders()) {
+            if (!g_resultsByBuilder[builderName] && this._buildersThatFailedToLoad.indexOf(builderName) < 0)
                 return false;
         }
         return true;
