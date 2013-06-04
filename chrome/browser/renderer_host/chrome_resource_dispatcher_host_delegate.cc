60e1b091ffa6b60abe526a5f7ea0ee6fac9cf6db
pan.deng@intel.com <pan.deng@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-04 13:51:20 +0000

    Expose net::LoadTimingInfo through content_shell
    Since blink use content_shell to run layout test, we need to expose net::LoadTimingInfo through content_shell
    
    Contributed by pan.deng@intel.com
    
    BUG=246117
    
    Review URL: https://chromiumcodereview.appspot.com/16324002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203959 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
+++ chrome/browser/renderer_host/chrome_resource_dispatcher_host_delegate.cc
@@ -504,10 +504,6 @@ void ChromeResourceDispatcherHostDelegate::OnResponseStarted(
     content::ResourceContext* resource_context,
     content::ResourceResponse* response,
     IPC::Sender* sender) {
-  // TODO(mmenke):  Figure out if LOAD_ENABLE_LOAD_TIMING is safe to remove.
-  if (request->load_flags() & net::LOAD_ENABLE_LOAD_TIMING)
-    request->GetLoadTimingInfo(&response->head.load_timing);
-
   const ResourceRequestInfo* info = ResourceRequestInfo::ForRequest(request);
 
   if (request->url().SchemeIsSecure()) {
@@ -565,10 +561,6 @@ void ChromeResourceDispatcherHostDelegate::OnRequestRedirected(
     net::URLRequest* request,
     content::ResourceContext* resource_context,
     content::ResourceResponse* response) {
-  // TODO(mmenke):  Figure out if LOAD_ENABLE_LOAD_TIMING is safe to remove.
-  if (request->load_flags() & net::LOAD_ENABLE_LOAD_TIMING)
-    request->GetLoadTimingInfo(&response->head.load_timing);
-
   ProfileIOData* io_data = ProfileIOData::FromResourceContext(resource_context);
 
 #if defined(ENABLE_ONE_CLICK_SIGNIN)
