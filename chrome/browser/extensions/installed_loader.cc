268019ac355e034c2a8ece98861b615fb76c7844
mpcomplete@chromium.org <mpcomplete@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-04 06:56:19 +0000

    Collect UMA data for extension manifest version and background page type.
    BUG=245033
    
    Review URL: https://chromiumcodereview.appspot.com/16295026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203879 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/installed_loader.cc
+++ chrome/browser/extensions/installed_loader.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/extensions/api/managed_mode_private/managed_mode_handler.h"
+#include "chrome/common/extensions/background_info.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/common/extensions/extension_file_util.h"
 #include "chrome/common/extensions/extension_l10n_util.h"
@@ -38,6 +39,8 @@ using extensions::Manifest;
 
 namespace errors = extension_manifest_errors;
 
+namespace extensions {
+
 namespace {
 
 // The following enumeration is used in histograms matching
@@ -48,6 +51,14 @@ enum ManifestReloadReason {
   UNPACKED_DIR,  // Unpacked directory.
   NEEDS_RELOCALIZATION,  // The locale has changed since we read this extension.
   NUM_MANIFEST_RELOAD_REASONS
+};
+
+// Used in histogram Extension.BackgroundPageType. Values may be added, as
+// long as existing values are not changed.
+enum BackgroundPageType {
+  NO_BACKGROUND_PAGE = 0,
+  BACKGROUND_PAGE_PERSISTENT = 1,
+  EVENT_PAGE = 2,
 };
 
 ManifestReloadReason ShouldReloadExtensionManifest(const ExtensionInfo& info) {
@@ -62,6 +73,14 @@ ManifestReloadReason ShouldReloadExtensionManifest(const ExtensionInfo& info) {
     return NEEDS_RELOCALIZATION;
 
   return NOT_NEEDED;
+}
+
+BackgroundPageType GetBackgroundPageType(const Extension* extension) {
+  if (!BackgroundInfo::HasBackgroundPage(extension))
+    return NO_BACKGROUND_PAGE;
+  if (BackgroundInfo::HasPersistentBackgroundPage(extension))
+    return BACKGROUND_PAGE_PERSISTENT;
+  return EVENT_PAGE;
 }
 
 void DispatchOnInstalledEvent(
@@ -80,8 +99,6 @@ void DispatchOnInstalledEvent(
 }
 
 }  // namespace
-
-namespace extensions {
 
 InstalledLoader::InstalledLoader(ExtensionService* extension_service)
     : extension_service_(extension_service),
@@ -281,6 +298,15 @@ void InstalledLoader::LoadAllExtensions() {
     // feature.
     if (Manifest::IsUnpackedLocation(location))
       continue;
+
+    UMA_HISTOGRAM_ENUMERATION("Extensions.ManifestVersion",
+                              (*ex)->manifest_version(), 10);
+
+    if (type == Manifest::TYPE_EXTENSION) {
+      BackgroundPageType background_page_type = GetBackgroundPageType(*ex);
+      UMA_HISTOGRAM_ENUMERATION("Extensions.BackgroundPageType",
+                                background_page_type, 10);
+    }
 
     // Using an enumeration shows us the total installed ratio across all users.
     // Using the totals per user at each startup tells us the distribution of
