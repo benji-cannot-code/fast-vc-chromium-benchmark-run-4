904af095bc5d05a591a894672c1abe83ccd070f7
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-04 23:21:10 +0000

    Added flag to enable WebGL Draft Extensions to about:flags
    BUG=167037
    R=kbr@chromium.org, thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/16358011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204068 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1517,6 +1517,13 @@ const Experiment kExperiments[] = {
     MULTI_VALUE_TYPE(kTabCaptureDownscaleQualityChoices)
   },
 #endif
+  {
+    "enable-webgl-draft-extensions",
+    IDS_FLAGS_ENABLE_WEBGL_DRAFT_EXTENSIONS_NAME,
+    IDS_FLAGS_ENABLE_WEBGL_DRAFT_EXTENSIONS_DESCRIPTION,
+    kOsAll,
+    SINGLE_VALUE_TYPE(switches::kEnableWebGLDraftExtensions)
+  },
 };
 
 const Experiment* experiments = kExperiments;
