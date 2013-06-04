34d04623eb94532d1cdb1e4a2da146e6483273ce
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-04 02:30:27 +0000

    Reland r203814.  Original message:
    Increase omnibox size by 2 px.  Increase nominal font size in omnibox to 16 px.
    
    The old omnibox background asset was semitransparent (for CrOS) and had to be
    drawn-over with the appropriate background color.  The new assets split the
    border (used by everyone) from the filling (used by CrOS alone).  As a result,
    when the filling is not in use, we can now draw the background behind the
    omnibox instead of in front, resulting in better antialiasing.  (Sadly, we still
    need to draw a shape that fits "inside" the borders since the borders are also
    semitransparent and have to overlay the toolbar in some places and the omnibox
    background in others; this is unavoidable unless we want to change to a fixed-
    color or programmatically-drawn omnibox border.)
    
    All PNGs were compressed with build/sanitize-png-files.sh -o2.
    
    BUG=231005
    TEST=Omnibox is 2 px larger, contents still look OK
    R=sky@chromium.org
    Review URL: https://codereview.chromium.org/16286003
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203816 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/content_setting_image_view.cc
+++ chrome/browser/ui/views/location_bar/content_setting_image_view.cc
@@ -21,11 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 namespace {
-const int kBackgroundImages[] = {
-  IDR_OMNIBOX_CONTENT_SETTING_BUBBLE_LEFT,
-  IDR_OMNIBOX_CONTENT_SETTING_BUBBLE_CENTER,
-  IDR_OMNIBOX_CONTENT_SETTING_BUBBLE_RIGHT,
-};
+const int kBackgroundImages[] = IMAGE_GRID(IDR_OMNIBOX_CONTENT_SETTING_BUBBLE);
 const int kStayOpenTimeMS = 3200;  // Time spent with animation fully open.
 }
 
@@ -46,7 +42,8 @@ ContentSettingImageView::ContentSettingImageView(
       content_setting_image_model_(
           ContentSettingImageModel::CreateContentSettingImageModel(
               content_type)),
-      background_painter_(new views::HorizontalPainter(kBackgroundImages)),
+      background_painter_(
+          views::Painter::CreateImageGridPainter(kBackgroundImages)),
       icon_(new views::ImageView),
       text_label_(new views::Label),
       slide_animator_(this),
@@ -70,7 +67,7 @@ ContentSettingImageView::ContentSettingImageView(
   // sit atop.
   const SkBitmap& bitmap(
       ui::ResourceBundle::GetSharedInstance().GetImageSkiaNamed(
-          kBackgroundImages[1])->GetRepresentation(
+          kBackgroundImages[4])->GetRepresentation(
           ui::SCALE_FACTOR_100P).sk_bitmap());
   SkAutoLockPixels pixel_lock(bitmap);
   SkColor background_image_color =
--- chrome/browser/ui/views/location_bar/ev_bubble_view.cc
+++ chrome/browser/ui/views/location_bar/ev_bubble_view.cc
@@ -9,11 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 namespace {
-const int kBackgroundImages[] = {
-  IDR_OMNIBOX_EV_BUBBLE_LEFT,
-  IDR_OMNIBOX_EV_BUBBLE_CENTER,
-  IDR_OMNIBOX_EV_BUBBLE_RIGHT,
-};
+const int kBackgroundImages[] = IMAGE_GRID(IDR_OMNIBOX_EV_BUBBLE);
 }
 
 
