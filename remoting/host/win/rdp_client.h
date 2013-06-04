8766ee0c48ce3e088e7ef4e1decfbe12bd50acb9
alexeypa@chromium.org <alexeypa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-04 06:46:59 +0000

    Emulate pressing Ctrl+Alt+End making the RDP ActiveX control to send Secure Attention Sequence to the session.
    BUG=246409
    
    Review URL: https://chromiumcodereview.appspot.com/15894034
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@203874 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/win/rdp_client.h
+++ remoting/host/win/rdp_client.h
@@ -44,6 +44,9 @@ class RdpClient : public base::NonThreadSafe {
       EventHandler* event_handler);
   virtual ~RdpClient();
 
+  // Sends Secure Attention Sequence to the session.
+  void InjectSas();
+
  private:
   // The actual implementation resides in Core class.
   class Core;
