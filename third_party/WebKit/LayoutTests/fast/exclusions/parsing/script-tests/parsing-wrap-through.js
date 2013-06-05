3a5c7d0a8c1b19d50e613c9174863382fc96cfdf
hmuller@adobe.com <hmuller@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-05 21:01:47 +0000

    [CSS Exclusions] property parsing tests should be revised
    This is just a cleanup of the existing parsing tests. None of the tests
    have effectively changed. The cleaned-up version should be simpler,
    more regular, and shorter: 
    - Moved all parsing tests into a fast/exclusions/parsing subdirectory.
    - Remove the obsolete "wrap-" prefix from file names.
    - Eiminated excessive and needless stringification in many of the test functions.
    - Moved common code into parsing-test-utils.js and refactored redundant code.
    
    BUG=244922
    
    Review URL: https://chromiumcodereview.appspot.com/16077013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151863 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/exclusions/parsing/script-tests/parsing-wrap-through.js
@@ -0,0 +1,33 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+description('Test parsing of the CSS wrap-through property.');
+
+if (window.internals)
+    window.internals.settings.setCSSExclusionsEnabled(true);
+
+// The test functions and globals used here are defined parsing-test-utils.js.
+
+applyToEachArglist(
+    testShapeSpecifiedProperty,
+    [// [property, value, expectedValue]
+     ["-webkit-wrap-through", "wrap", "wrap"],
+     ["-webkit-wrap-through", "none", "none"],
+     ["-webkit-wrap-through", ";", ""],
+     ["-webkit-wrap-through", "5", ""],
+     ["-webkit-wrap-through", "-1.2", ""],
+     ["-webkit-wrap-through", "\'string\'", ""]]
+);
+
+applyToEachArglist(
+    testShapeComputedProperty,
+    [// [property, value, expectedValue]
+     ["-webkit-wrap-through", "wrap", "wrap"],
+     ["-webkit-wrap-through", "5", "wrap"],
+     ["-webkit-wrap-through", "\'string\'", "wrap"]]
+);
+
+applyToEachArglist(
+    testNotInheritedShapeChildProperty,
+    [// [property, parentValue, childValue, expectedChildValue]
+     ["-webkit-wrap-through", "wrap", "none", "none"],
+     ["-webkit-wrap-through", "none", "wrap", "wrap"]]
+);
