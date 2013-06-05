17f037b774536843375842647c61abd215265a5b
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 03:08:13 +0000

    [chromedriver] Use unix style newlines when logging. BUG=none
    Review URL: https://chromiumcodereview.appspot.com/16366002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204140 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/chrome/log.cc
+++ chrome/test/chromedriver/chrome/log.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/json/json_reader.h"
 #include "base/json/json_writer.h"
+#include "base/string_util.h"
 #include "base/stringprintf.h"
 #include "base/values.h"
 #include "chrome/test/chromedriver/logging.h"
@@ -56,8 +57,7 @@ void TruncateContainedStrings(base::Value* value) {
   }
 }
 
-// Pretty prints encapsulated JSON and truncates long strings for display.
-std::string ConvertForDisplay(const std::string& input) {
+std::string ConvertForDisplayInternal(const std::string& input) {
   size_t left = input.find("{");
   size_t right = input.rfind("}");
   if (left == std::string::npos || right == std::string::npos)
@@ -74,6 +74,14 @@ std::string ConvertForDisplay(const std::string& input) {
   std::string display = input.substr(0, left) + json;
   if (input.length() > right)
     display += input.substr(right + 1);
+  return display;
+}
+
+// Pretty prints encapsulated JSON and truncates long strings for display.
+std::string ConvertForDisplay(const std::string& input) {
+  std::string display = ConvertForDisplayInternal(input);
+  char remove_chars[] = {'\r'};
+  RemoveChars(display, remove_chars, &display);
   return display;
 }
 
