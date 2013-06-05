940ee2a5a32e2b7a331f1ee525784617d73be6e9
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 13:21:13 +0000

    Refactoring: separate TranslateAcceptLanguages from TranslateManager
    Separated the function which concerns with Accept languages from TranslateManager.
    
    Review URL: https://chromiumcodereview.appspot.com/16232018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204247 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_manager.h
+++ chrome/browser/translate/translate_manager.h
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_TRANSLATE_TRANSLATE_MANAGER_H_
 
 #include <map>
-#include <set>
 #include <string>
 #include <vector>
 
@@ -15,7 +14,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/observer_list.h"
-#include "base/prefs/pref_change_registrar.h"
 #include "base/time.h"
 #include "chrome/common/translate_errors.h"
 #include "content/public/browser/notification_observer.h"
@@ -28,6 +26,7 @@ struct LanguageDetectionDetails;
 struct PageTranslatedDetails;
 class PrefService;
 struct ShortcutConfiguration;
+class TranslateAcceptLanguages;
 struct TranslateErrorDetails;
 class TranslateInfoBarDelegate;
 class TranslateLanguageList;
@@ -170,15 +169,6 @@ class TranslateManager : public content::NotificationObserver,
   void PageTranslated(content::WebContents* web_contents,
                       PageTranslatedDetails* details);
 
-  // Returns true if the passed language has been configured by the user as an
-  // accept language.
-  bool IsAcceptLanguage(content::WebContents* web_contents,
-                        const std::string& language);
-
-  // Initializes the |accept_languages_| language table based on the associated
-  // preference in |prefs|.
-  void InitAcceptLanguages(PrefService* prefs);
-
   // Fetches the JS translate script (the script that is injected in the page
   // to translate it).
   void RequestTranslateScript();
@@ -202,16 +192,6 @@ class TranslateManager : public content::NotificationObserver,
   static ShortcutConfiguration ShortcutConfig();
 
   content::NotificationRegistrar notification_registrar_;
-
-  // Each PrefChangeRegistrar only tracks a single PrefService, so a map from
-  // each PrefService used to its registrar is needed.
-  typedef std::map<PrefService*, PrefChangeRegistrar*> PrefServiceRegistrarMap;
-  PrefServiceRegistrarMap pref_change_registrars_;
-
-  // A map that associates a profile with its parsed "accept languages".
-  typedef std::set<std::string> LanguageSet;
-  typedef std::map<PrefService*, LanguageSet> PrefServiceLanguagesMap;
-  PrefServiceLanguagesMap accept_languages_;
 
   base::WeakPtrFactory<TranslateManager> weak_method_factory_;
 
@@ -238,6 +218,10 @@ class TranslateManager : public content::NotificationObserver,
 
   // An instance of TranslateLanguageList which manages supported language list.
   scoped_ptr<TranslateLanguageList> language_list_;
+
+  // An instance of TranslateAcceptLanguages which manages Accept languages of
+  // each profiles.
+  scoped_ptr<TranslateAcceptLanguages> accept_languages_;
 
   DISALLOW_COPY_AND_ASSIGN(TranslateManager);
 };
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -2246,6 +2246,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'browser/toolkit_extra_parts.h',
         'browser/translate/options_menu_model.cc',
         'browser/translate/options_menu_model.h',
+        'browser/translate/translate_accept_languages.h',
+        'browser/translate/translate_accept_languages.cc',
         'browser/translate/translate_error_details.h',
         'browser/translate/translate_infobar_delegate.cc',
         'browser/translate/translate_infobar_delegate.h',
