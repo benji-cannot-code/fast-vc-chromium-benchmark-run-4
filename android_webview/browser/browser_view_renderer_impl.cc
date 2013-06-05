a90d4c29db20c2e9e7319c877989e2c1f76d5082
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 03:17:06 +0000

    Fix error in fallback SW rendering
    The clip-observing auxiliary bitmap needs to be translated on screen
    in accordance with the top-left corner of the clipping.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16290010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204142 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/browser_view_renderer_impl.cc
+++ android_webview/browser/browser_view_renderer_impl.cc
@@ -354,7 +354,8 @@ bool BrowserViewRendererImpl::DrawSW(jobject java_canvas,
     }
 
     ScopedJavaLocalRef<jobject> jcanvas(env, java_canvas);
-    java_helper_->DrawBitmapIntoCanvas(env, jbitmap, jcanvas);
+    java_helper_->DrawBitmapIntoCanvas(env, jbitmap, jcanvas,
+                                       clip.x(), clip.y());
     return true;
   }
 
