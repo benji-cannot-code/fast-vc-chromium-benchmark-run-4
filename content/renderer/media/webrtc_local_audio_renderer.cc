9a032a263641cd590d2b236b6a6dfca3b4e7fd35
wjia@chromium.org <wjia@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 17:56:12 +0000

    Use native buffer size preferred by hardware for local audio rendering on Android
    The usage of native buffer size allows low latency playback.
    
    BUG=243506
    R=henrika@chromium.org, xians@chromium.org
    
    Review URL: https://codereview.chromium.org/15795005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204290 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/webrtc_local_audio_renderer.cc
+++ content/renderer/media/webrtc_local_audio_renderer.cc
@@ -11,9 +11,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/synchronization/lock.h"
 #include "content/renderer/media/audio_device_factory.h"
 #include "content/renderer/media/webrtc_audio_capturer.h"
+#include "content/renderer/render_thread_impl.h"
 #include "media/audio/audio_output_device.h"
 #include "media/base/audio_bus.h"
 #include "media/base/audio_fifo.h"
+#include "media/base/audio_hardware_config.h"
 
 namespace content {
 
@@ -126,11 +128,25 @@ void WebRtcLocalAudioRenderer::Start() {
   loopback_fifo_.reset(new media::AudioFifo(
       audio_params_.channels(), 10 * audio_params_.frames_per_buffer()));
 
+#if defined(OS_ANDROID)
+  media::AudioHardwareConfig* hardware_config =
+      RenderThreadImpl::current()->GetAudioHardwareConfig();
+#endif
+
   media::AudioParameters sink_params(audio_params_.format(),
                                      audio_params_.channel_layout(),
                                      audio_params_.sample_rate(),
                                      audio_params_.bits_per_sample(),
-                                     2 * audio_params_.frames_per_buffer());
+#if defined(OS_ANDROID)
+  // On Android, input and output are using same sampling rate. In order to
+  // achieve low latency mode, we need use buffer size suggested by
+  // AudioManager for the sink paramters which will be used to decide
+  // buffer size for shared memory buffer.
+                                     hardware_config->GetOutputBufferSize()
+#else
+                                     2 * audio_params_.frames_per_buffer()
+#endif
+                                    );
   sink_ = AudioDeviceFactory::NewOutputDevice(source_render_view_id_);
 
   // TODO(henrika): we could utilize the unified audio here instead and do
