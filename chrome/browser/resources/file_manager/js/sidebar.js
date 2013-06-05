74edd8eeb6666d300445027914584be1266a1167
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 16:22:14 +0000

    Files.app: Fix the scrollbar position with transparent preview panel
    Previously, when the transparent preview panel was shown, the scrollbar didn't take cake of it and the scrollbar overlapped over the preview panel. This patch fixes the it. 
    
    This patch depends a patch of making the preview panel transparent. 
    https://codereview.chromium.org/15275005/ 
    
    BUG=239845
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/16007019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204275 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/sidebar.js
+++ chrome/browser/resources/file_manager/js/sidebar.js
@@ -568,8 +568,10 @@ DirectoryTree.prototype.decorate = function(directoryModel) {
   chrome.fileBrowserPrivate.onDirectoryChanged.addListener(
       this.privateOnDirectoryChangedBound_);
 
-  if (util.platform.newUI())
-    ScrollBar.createVertical(this.parentNode, this);
+  if (util.platform.newUI()) {
+    this.scrollBar_ = MainPanelScrollBar();
+    this.scrollBar_.initialize(this.parentNode, this);
+  }
 
   if (!util.platform.newUI())
     this.onRootsListChanged_();
@@ -783,6 +785,18 @@ DirectoryTree.prototype.clearTree_ = function(redraw) {
     this.redraw(false);
     cr.dispatchSimpleEvent(this, 'content-updated');
   }
+};
+
+/**
+ * Sets the margin height for the transparent preview panel at the bottom.
+ * @param {number} margin Margin to be set in px.
+ */
+DirectoryTree.prototype.setBottomMarginForPanel = function(margin) {
+  if (!util.platform.newUI())
+    return;
+
+  this.style.paddingBottom = margin + 'px';
+  this.scrollBar_.setBottomMarginForPanel(margin);
 };
 
 /**
