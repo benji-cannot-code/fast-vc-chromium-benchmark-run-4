b204104e9f759efcf2c3297a7dbd31bde711c852
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 05:55:09 +0000

    Fix OS X use of WeakPtr<T>'s operator T* conversion
    BUG=245942
    TBR=darin@chromium.org
    
    Review URL: https://codereview.chromium.org/15984008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204164 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/global_error_bubble_controller.mm
+++ chrome/browser/ui/cocoa/global_error_bubble_controller.mm
@@ -81,7 +81,7 @@ class Bridge : public GlobalErrorBubbleViewBase {
 - (void)awakeFromNib {
   [super awakeFromNib];
 
-  DCHECK(error_);
+  DCHECK(error_.get());
 
   gfx::Image image = error_->GetBubbleViewIcon();
   DCHECK(!image.IsEmpty());
@@ -139,7 +139,7 @@ class Bridge : public GlobalErrorBubbleViewBase {
 }
 
 - (void)close {
-  if (error_)
+  if (error_.get())
     error_->BubbleViewDidClose(browser_);
   bridge_.reset();
   BrowserWindowController* bwc = [BrowserWindowController
@@ -149,13 +149,13 @@ class Bridge : public GlobalErrorBubbleViewBase {
 }
 
 - (IBAction)onAccept:(id)sender {
-  if (error_)
+  if (error_.get())
     error_->BubbleViewAcceptButtonPressed(browser_);
   [self close];
 }
 
 - (IBAction)onCancel:(id)sender {
-  if (error_)
+  if (error_.get())
     error_->BubbleViewCancelButtonPressed(browser_);
   [self close];
 }
