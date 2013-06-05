e8ad6041eab785761bd37731109c5fbec26ec118
jaredshumway94@gmail.com <jaredshumway94@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 08:56:46 +0000

    Docserver: inline return and item types. Fix a couple of bugs.
    BUG=187494
    
    Review URL: https://chromiumcodereview.appspot.com/15637003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204215 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/template_data_source.py
+++ chrome/common/extensions/docs/server2/template_data_source.py
@@ -4,16 +4,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 import logging
-import os
 
 from branch_utility import BranchUtility
-import compiled_file_system as compiled_fs
 from docs_server_utils import FormatKey
 from file_system import FileNotFoundError
 from third_party.handlebar import Handlebar
 import url_constants
 
-EXTENSIONS_URL = '/chrome/extensions'
+_EXTENSIONS_URL = '/chrome/extensions'
+
+_STRING_CONSTANTS = {
+  'app': 'app',
+  'apps_title': 'Apps',
+  'extension': 'extension',
+  'extensions_title': 'Extensions',
+  'events': 'events',
+  'functions': 'functions',
+  'properties': 'properties',
+  }
 
 def _MakeChannelDict(channel_name):
   channel_dict = {
@@ -124,10 +132,6 @@ class TemplateDataSource(object):
       'partials': self,
       'samples': self._samples_data_source,
       'static': self._static_resources,
-      'app': 'app',
-      'extension': 'extension',
-      'apps_title': 'Apps',
-      'extensions_title': 'Extensions',
       'apps_samples_url': url_constants.GITHUB_BASE,
       # TODO(kalman): this is wrong, it's always getting from trunk, but meh
       # it hardly ever shows up (only in the "cannot fetch samples" message).
@@ -135,6 +139,7 @@ class TemplateDataSource(object):
       # being persisent. In any case, when the channel distinctions are gone
       # this can go away, so, double meh.
       'extensions_samples_url': url_constants.EXTENSIONS_SAMPLES,
+      'strings': _STRING_CONSTANTS,
       'true': True,
       'false': False
     })
