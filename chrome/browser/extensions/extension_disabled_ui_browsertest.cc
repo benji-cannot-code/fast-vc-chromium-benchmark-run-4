fa4efa6f9eb099b2df6c5c1ba7ae9392d4ab180c
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 11:39:47 +0000

    Speculative attempt to fix ExtensionDisabledGlobalErrorTest flakiness.
    I've mostly been unable to reproduce flakiness locally, but it should be due to the asynchronous image loading.
    
    BUG=246431
    
    Review URL: https://chromiumcodereview.appspot.com/16341003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204232 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_disabled_ui_browsertest.cc
+++ chrome/browser/extensions/extension_disabled_ui_browsertest.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
+#include "base/run_loop.h"
 #include "chrome/app/chrome_command_ids.h"
 #include "chrome/browser/extensions/extension_browsertest.h"
 #include "chrome/browser/extensions/extension_prefs.h"
@@ -79,6 +80,7 @@ class ExtensionDisabledGlobalErrorTest : public ExtensionBrowserTest {
     size_t size_before = service_->extensions()->size();
     if (UpdateExtension(extension->id(), crx_path, expected_change))
       return NULL;
+    base::RunLoop().RunUntilIdle();
     EXPECT_EQ(size_before + expected_change, service_->extensions()->size());
     if (service_->disabled_extensions()->size() != 1u)
       return NULL;
@@ -209,6 +211,7 @@ IN_PROC_BROWSER_TEST_F(ExtensionDisabledGlobalErrorTest,
   EXPECT_FALSE(service_->ProcessExtensionSyncData(sync_data));
 
   WaitForExtensionInstall();
+  base::RunLoop().RunUntilIdle();
 
   extension = service_->GetExtensionById(extension_id, true);
   ASSERT_TRUE(extension);
