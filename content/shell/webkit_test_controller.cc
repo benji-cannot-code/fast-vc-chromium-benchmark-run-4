31ff4b88a308d041742cd15d891b33caff6a2c2a
loislo@chromium.org <loislo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 12:01:33 +0000

    LayoutTests: remove timeout support from content_shell
    When content_shell is used as a browser it doesn't need a timeout mechanics.
    When it is used as DRT it doesn't need the timeout mechanics because test framework does this job.
    Lets remove timeout mechanics from WebKit and chromium parts of content_shell.
    
    upstream patch https://chromiumcodereview.appspot.com/15930008/
    BUG=none
    TEST=layout_tests
    R=jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/16057015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204236 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_controller.cc
+++ content/shell/webkit_test_controller.cc
@@ -34,10 +34,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
-const int kTestTimeoutMilliseconds = 30 * 1000;
-// 0x20000000ms is big enough for the purpose to avoid timeout in debugging.
-const int kCloseEnoughToInfinity = 0x20000000;
-
 const int kTestSVGWindowWidthDip = 480;
 const int kTestSVGWindowHeightDip = 360;
 
@@ -247,14 +243,6 @@ bool WebKitTestController::PrepareForLayoutTest(
   }
   main_window_->web_contents()->GetRenderViewHost()->SetActive(true);
   main_window_->web_contents()->GetRenderViewHost()->Focus();
-  if (!CommandLine::ForCurrentProcess()->HasSwitch(switches::kNoTimeout)) {
-    watchdog_.Reset(base::Bind(&WebKitTestController::TimeoutHandler,
-                               base::Unretained(this)));
-    base::MessageLoop::current()->PostDelayedTask(
-        FROM_HERE,
-        watchdog_.callback(),
-        base::TimeDelta::FromMilliseconds(kTestTimeoutMilliseconds + 1000));
-  }
   return true;
 }
 
@@ -270,7 +258,6 @@ bool WebKitTestController::ResetAfterLayoutTest() {
   test_url_ = GURL();
   prefs_ = WebPreferences();
   should_override_prefs_ = false;
-  watchdog_.Cancel();
   return true;
 }
 
@@ -447,10 +434,6 @@ void WebKitTestController::SendTestConfiguration() {
   params.temp_path = temp_path_;
   params.test_url = test_url_;
   params.enable_pixel_dumping = enable_pixel_dumping_;
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kNoTimeout))
-    params.layout_test_timeout = kCloseEnoughToInfinity;
-  else
-    params.layout_test_timeout = kTestTimeoutMilliseconds;
   params.allow_external_pages = CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kAllowExternalPages);
   params.expected_pixel_hash = expected_pixel_hash_;
@@ -459,14 +442,7 @@ void WebKitTestController::SendTestConfiguration() {
       render_view_host->GetRoutingID(), params));
 }
 
-void WebKitTestController::OnTestFinished(bool did_timeout) {
-  watchdog_.Cancel();
-  if (did_timeout) {
-    printer_->AddErrorMessage(
-        "FAIL: Timed out waiting for notifyDone to be called");
-    DiscardMainWindow();
-    return;
-  }
+void WebKitTestController::OnTestFinished() {
   test_phase_ = CLEAN_UP;
   if (!printer_->output_finished())
     printer_->PrintImageFooter();
