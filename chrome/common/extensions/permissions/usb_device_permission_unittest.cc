ea7cc43a33ae503e05b5c6fda7ce29cb8422d95c
zelidrag@chromium.org <zelidrag@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 07:52:17 +0000

    Added ability to define explicit interface access permission to chrome.usb.* API.
    This piece of information is needed in order to get ChromeOS permission broker grant access to devices that have some interfaces unclaimed by the kernel - see https://gerrit.chromium.org/gerrit/#/c/57136 for details. 
    
    BUG=243798
    TEST=existing USB unit and browser tests
    
    Review URL: https://chromiumcodereview.appspot.com/16125016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204197 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/permissions/usb_device_permission_unittest.cc
+++ chrome/common/extensions/permissions/usb_device_permission_unittest.cc
@@ -20,10 +20,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace extensions {
 
 TEST(USBDevicePermissionTest, PermissionDataOrder) {
-  EXPECT_LT(UsbDevicePermissionData(0x02ad, 0x138c),
-            UsbDevicePermissionData(0x02ad, 0x138d));
-  ASSERT_LT(UsbDevicePermissionData(0x02ad, 0x138d),
-            UsbDevicePermissionData(0x02ae, 0x138c));
+  EXPECT_LT(UsbDevicePermissionData(0x02ad, 0x138c, -1),
+            UsbDevicePermissionData(0x02ad, 0x138d, -1));
+  ASSERT_LT(UsbDevicePermissionData(0x02ad, 0x138d, -1),
+            UsbDevicePermissionData(0x02ae, 0x138c, -1));
+  EXPECT_LT(UsbDevicePermissionData(0x02ad, 0x138c, -1),
+            UsbDevicePermissionData(0x02ad, 0x138c, 0));
 }
 
 #if defined(ENABLE_EXTENSIONS)
@@ -42,11 +44,11 @@ TEST(USBDevicePermissionTest, MAYBE_PermissionMessage) {
   // Prepare data set
   scoped_ptr<base::ListValue> permission_list(new base::ListValue());
   permission_list->Append(
-      UsbDevicePermissionData(0x02ad, 0x138c).ToValue()->DeepCopy());
+      UsbDevicePermissionData(0x02ad, 0x138c, -1).ToValue()->DeepCopy());
   permission_list->Append(
-      UsbDevicePermissionData(0x02ad, 0x138d).ToValue()->DeepCopy());
+      UsbDevicePermissionData(0x02ad, 0x138d, -1).ToValue()->DeepCopy());
   permission_list->Append(
-      UsbDevicePermissionData(0x02ae, 0x138d).ToValue()->DeepCopy());
+      UsbDevicePermissionData(0x02ae, 0x138d, -1).ToValue()->DeepCopy());
 
   UsbDevicePermission permission(
       PermissionsInfo::GetInstance()->GetByID(APIPermission::kUsbDevice));
