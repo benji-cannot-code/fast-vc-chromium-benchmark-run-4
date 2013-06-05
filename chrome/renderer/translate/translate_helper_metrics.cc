652be38d5db798b389b2b852983dbecbe52f6cd2
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 17:02:41 +0000

    Translate: CLD should not disagree similar languages
    Currently, infobar doesn't appear if a page providing language meta
    information is different from a CLD determined language.
    But, some language pairs are difficult for CLD to distinguish correctly.
    This change make CLD not disagree on these similar languages.
    
    BUG=243763
    TEST=unit_tests --gtest_filter='TranslateHelper*Test.*'
    
    Review URL: https://chromiumcodereview.appspot.com/16107005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204280 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/translate/translate_helper_metrics.cc
+++ chrome/renderer/translate/translate_helper_metrics.cc
@@ -21,6 +21,7 @@ const char kTranslateTimeToLoad[] = "Translate.TimeToLoad";
 const char kTranslateTimeToTranslate[] = "Translate.TimeToTranslate";
 const char kTranslateUserActionDuration[] = "Translate.UserActionDuration";
 const char kTranslatePageScheme[] = "Translate.PageScheme";
+const char kTranslateSimilarLanguageMatch[] = "Translate.SimilarLanguageMatch";
 
 const char kSchemeHttp[] = "http";
 const char kSchemeHttps[] = "https";
@@ -50,6 +51,8 @@ const MetricsEntry kMetricsEntries[] = {
     kTranslateUserActionDuration },
   { TranslateHelperMetrics::UMA_PAGE_SCHEME,
     kTranslatePageScheme },
+  { TranslateHelperMetrics::UMA_SIMILAR_LANGUAGE_MATCH,
+    kTranslateSimilarLanguageMatch },
 };
 
 COMPILE_ASSERT(arraysize(kMetricsEntries) == TranslateHelperMetrics::UMA_MAX,
@@ -127,6 +130,10 @@ void ReportLanguageDetectionTime(base::TimeTicks begin, base::TimeTicks end) {
 }
 
 #endif  // defined(ENABLE_LANGUAGE_DETECTION)
+
+void ReportSimilarLanguageMatch(bool match) {
+  UMA_HISTOGRAM_BOOLEAN(kTranslateSimilarLanguageMatch, match);
+}
 
 const char* GetMetricsName(MetricsNameIndex index) {
   for (size_t i = 0; i < arraysize(kMetricsEntries); ++i) {
