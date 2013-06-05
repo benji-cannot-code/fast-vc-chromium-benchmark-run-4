66fbf77ecc0aaded9847c6adf7c32f204a162cf7
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 13:23:38 +0000

    Monitor failures when deleting files in Files.app.
    This patch adds monitoring for results of deleting instead of blindly assuming that it succeded. This is necessary to remove jank while removing items on Drive. Also, adds removing internal metadata observers before re-adding them.
    
    Additionaly, an error message has been added in case of delete failures.
    
    TEST=Go to Drive, remove a directory. It should disappear immediately, without blinking.
    BUG=242199
    R=hashimoto@chromium.org, yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/16105021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204248 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_copy_manager.js
+++ chrome/browser/resources/file_manager/js/file_copy_manager.js
@@ -1207,13 +1207,19 @@ FileCopyManager.prototype.serviceAllDeleteTasks_ = function() {
     // big task logically, so there is only one BEGIN/SUCCESS event pair for
     // these continuous tasks.
     self.sendDeleteEvent_(self.deleteTasks_[0], 'PROGRESS');
-    self.serviceDeleteTask_(self.deleteTasks_[0], onTaskSuccess);
+    self.serviceDeleteTask_(self.deleteTasks_[0], onTaskSuccess, onTaskFailure);
+  };
+
+  var onTaskFailure = function(task) {
+    self.deleteTasks_ = [];
+    self.sendDeleteEvent_(task, 'ERROR');
+    self.maybeScheduleCloseBackgroundPage_();
   };
 
   // If the queue size is 1 after pushing our task, it was empty before,
   // so we need to kick off queue processing and dispatch BEGIN event.
   this.sendDeleteEvent_(this.deleteTasks_[0], 'BEGIN');
-  this.serviceDeleteTask_(this.deleteTasks_[0], onTaskSuccess);
+  this.serviceDeleteTask_(this.deleteTasks_[0], onTaskSuccess, onTaskFailure);
 };
 
 /**
@@ -1222,15 +1228,22 @@ FileCopyManager.prototype.serviceAllDeleteTasks_ = function() {
  * @param {Object} task The delete task (see deleteEntries function).
  * @param {function(Object)} onComplete Completion callback with the task
  *     as an argument.
+ * @param {function(Object)} onFailure Failure callback with the task as an
+ *     argument.
  * @private
  */
 FileCopyManager.prototype.serviceDeleteTask_ = function(
-    task, onComplete) {
+    task, onComplete, onFailure) {
   var downcount = task.entries.length;
 
   var onEntryComplete = function() {
     if (--downcount == 0)
       onComplete(task);
+  }.bind(this);
+
+  var onEntryFailure = function() {
+    if (--downcount == 0)
+      onFailure(task);
   }.bind(this);
 
   if (downcount == 0)
@@ -1241,7 +1254,7 @@ FileCopyManager.prototype.serviceDeleteTask_ = function(
     util.removeFileOrDirectory(
         entry,
         onEntryComplete,
-        onEntryComplete); // We ignore error, because we can't do anything here.
+        onEntryFailure);
   }
 };
 
