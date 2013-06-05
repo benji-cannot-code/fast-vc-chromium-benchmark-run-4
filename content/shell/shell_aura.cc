53299903541a83fcffa3dac560de01c8ac4ce602
dongseong.hwang@intel.com <dongseong.hwang@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 19:42:35 +0000

    Remove the duplicated clear of ViewsDelegate::views_delegate in ~ShellViewsDelegateAura().
    ~TestViewsDelegate() aleady clears ViewsDelegate::views_delegate, so
    ~ShellViewsDelegateAura() does not need to clear. ShellViewsDelegateAura extends
    DesktopTestViewsDelegate that extends TestViewsDelegate.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/16451004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204318 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/shell_aura.cc
+++ content/shell/shell_aura.cc
@@ -42,7 +42,6 @@ class ShellViewsDelegateAura : public views::DesktopTestViewsDelegate {
   }
 
   virtual ~ShellViewsDelegateAura() {
-    ViewsDelegate::views_delegate = NULL;
   }
 
   void SetUseTransparentWindows(bool transparent) {
