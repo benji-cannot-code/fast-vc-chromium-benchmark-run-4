98ce0c1744449a86520e65b105a837bccaf25010
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-05 07:16:20 +0000

    Revert 204181 "Enable users of NotificationUIManager to specify ..."
    Broke Mac builders.
    http://build.chromium.org/p/chromium.mac/builders/Mac%20Builder/builds/14414
    http://build.chromium.org/p/chromium.mac/builders/Mac%20Builder%20%28dbg%29/builds/46042
    
    > Enable users of NotificationUIManager to specify binary images.
    > 
    > This refactors message_center::Notification to hold a class
    > containing the optional data associated with the
    > notification.  It also alters chrome/browser/notification so
    > that you can manually set this.
    > 
    > Ash tests only updated to use new API.
    > 
    > TBR=sky@chromium.org,stevenjb@chromium.org
    > BUG=227093
    > 
    > Review URL: https://chromiumcodereview.appspot.com/14631005
    
    TBR=dewittj@chromium.org
    
    Review URL: https://codereview.chromium.org/16429004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204185 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/notification_types.cc
+++ ui/message_center/notification_types.cc
@@ -7,8 +7,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace message_center {
 
+const char kMessageIntentKey[] = "message_intent";
 const char kPriorityKey[] = "priority";
 const char kTimestampKey[] = "timestamp";
+const char kUnreadCountKey[] = "unread_count";
 const char kButtonOneTitleKey[] = "button_one_title";
 const char kButtonOneIconUrlKey[] = "button_one_icon_url";
 const char kButtonTwoTitleKey[] = "button_two_title";
