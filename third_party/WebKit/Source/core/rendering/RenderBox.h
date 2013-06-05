80497825ff16962b59ba74b9b8f9500993ea93da
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-05 05:29:13 +0000

    Fix regression with dynamically sized replaced elements.
    http://src.chromium.org/viewvc/blink?view=rev&revision=142931
    fixed a bug where we treat have the minPreferredLogicalWidth
    of percentage height replaced elements be 0. This exposed
    a bug where we were not marking the preferred width as dirty.
    Specifically, a percentage height replaced element that maintains
    aspect ratio needs preferred widths recalculated if the height
    of it's containingBlock changes.
    
    Also, rolled hasViewportPercentageLogicalHeight into
    hasRelativeLogicalHeight. The two were always called together.
    There's a slight change in behavior here where we won't
    mark children of the RenderView that have viewport relative heights
    as needing layout where we used to, but I don't think that
    was needed anyways.
    
    BUG=245013
    
    Review URL: https://chromiumcodereview.appspot.com/16003005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151805 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.h
+++ third_party/WebKit/Source/core/rendering/RenderBox.h
@@ -544,7 +544,6 @@ public:
 
     virtual bool hasRelativeDimensions() const;
     virtual bool hasRelativeLogicalHeight() const;
-    bool hasViewportPercentageLogicalHeight() const;
 
     bool hasHorizontalLayoutOverflow() const
     {
