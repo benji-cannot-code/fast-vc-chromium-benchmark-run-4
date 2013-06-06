390c490091f0d68525b2d50cd31a30988f82f597
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 22:25:19 +0000

    [Mac] Notify the message center that a notification in the tray is clicked
    The fix is to observe the mouse down event in the notification controller view. And we don't need the logic in the popup window any more.
    
    BUG=244610
    TEST=manual test by clicking on the notification in the try
    R=dewittj@chromium.org, rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/16147011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204626 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/notification_controller.mm
+++ ui/message_center/cocoa/notification_controller.mm
@@ -90,6 +90,32 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 }
 @end
 
+@interface MCNotificationView : NSBox {
+ @private
+  MCNotificationController* controller_;
+}
+
+- (id)initWithController:(MCNotificationController*)controller
+                   frame:(NSRect)frame;
+@end
+
+@implementation MCNotificationView
+- (id)initWithController:(MCNotificationController*)controller
+                   frame:(NSRect)frame {
+  if ((self = [super initWithFrame:frame]))
+    controller_ = controller;
+  return self;
+}
+
+- (void)mouseDown:(NSEvent*)event {
+  if ([event type] != NSLeftMouseDown) {
+    [super mouseDown:event];
+    return;
+  }
+  [controller_ notificationClicked];
+}
+@end
+
 @interface MCNotificationController (Private)
 // Returns a string with item's title in title color and item's message in
 // message color.
@@ -147,7 +173,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   NSRect rootFrame = NSMakeRect(0, 0,
       message_center::kNotificationPreferredImageSize,
       message_center::kNotificationIconSize);
-  scoped_nsobject<NSBox> rootView([[NSBox alloc] initWithFrame:rootFrame]);
+  scoped_nsobject<MCNotificationView> rootView(
+      [[MCNotificationView alloc] initWithController:self
+                                               frame:rootFrame]);
   [self configureCustomBox:rootView];
   [rootView setFillColor:gfx::SkColorToCalibratedNSColor(
       message_center::kNotificationBackgroundColor)];
@@ -355,6 +383,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (const std::string&)notificationID {
   return notificationID_;
+}
+
+- (void)notificationClicked {
+  messageCenter_->ClickOnNotification([self notificationID]);
 }
 
 // Private /////////////////////////////////////////////////////////////////////
