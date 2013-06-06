9475ae8345cceb72500a6f05c79e75144eca3193
zork@chromium.org <zork@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 23:14:48 +0000

    Add unit tests for sniffing Office Docs
    BUG=247189
    
    Review URL: https://chromiumcodereview.appspot.com/16500005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204648 0039d316-1c4b-4281-b951-d872f2087c98

--- net/base/mime_sniffer.cc
+++ net/base/mime_sniffer.cc
@@ -543,7 +543,7 @@ static bool IsOfficeType(const std::string& type_hint) {
 // set, but are not actually Office files.
 //
 // If this is not actually an Office file, |*result| is set to
-// "unknown/unknown", otherwise it is not modified.
+// "application/octet-stream", otherwise it is not modified.
 //
 // Returns false if additional data is required to determine the file type, or
 // true if there is enough data to make a decision.
@@ -560,7 +560,7 @@ static bool SniffForInvalidOfficeDocs(const char* content,
   if (!CheckForMagicNumbers(content, size,
                             kOfficeMagicNumbers, arraysize(kOfficeMagicNumbers),
                             NULL, &office_version)) {
-    *result = "unknown/unknown";
+    *result = "application/octet-stream";
   }
 
   // We have enough information to determine if this was a Microsoft Office
