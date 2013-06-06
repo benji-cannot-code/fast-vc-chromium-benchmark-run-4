dde868efa12461499d960fb3c7fb22b177650b5b
mkosiba@chromium.org <mkosiba@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-06 17:40:59 +0000

    Revert 151926 "Maintain aspect ratio in wide vieport mode."
    > Maintain aspect ratio in wide vieport mode.
    > 
    > Wide viewport mode should maintain the aspect ratio of the viewport,
    > otherwise we run into problems when android_webview is in autosizing
    > mode.
    > 
    > BUG=247002
    > R=abarth@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/16123020
    
    TBR=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/16023029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151948 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/WebFrameTest.cpp
@@ -548,7 +548,6 @@ TEST_F(WebFrameTest, NoWideViewportIgnoresPageViewportWidth)
     // The page sets viewport width to 3000, but with UseWideViewport == false is must be ignored.
     WebViewImpl* webViewImpl = static_cast<WebViewImpl*>(m_webView);
     EXPECT_EQ(viewportWidth, webViewImpl->mainFrameImpl()->frameView()->contentsSize().width());
-    EXPECT_EQ(viewportHeight, webViewImpl->mainFrameImpl()->frameView()->contentsSize().height());
 }
 
 TEST_F(WebFrameTest, NoWideViewportIgnoresPageViewportWidthButAccountsScale)
@@ -571,7 +570,6 @@ TEST_F(WebFrameTest, NoWideViewportIgnoresPageViewportWidthButAccountsScale)
     // While the initial scale specified by the page must be accounted.
     WebViewImpl* webViewImpl = static_cast<WebViewImpl*>(m_webView);
     EXPECT_EQ(viewportWidth / 2, webViewImpl->mainFrameImpl()->frameView()->contentsSize().width());
-    EXPECT_EQ(viewportHeight / 2, webViewImpl->mainFrameImpl()->frameView()->contentsSize().height());
 }
 
 TEST_F(WebFrameTest, WideViewportSetsTo980WithAutoWidth)
@@ -592,7 +590,6 @@ TEST_F(WebFrameTest, WideViewportSetsTo980WithAutoWidth)
 
     WebViewImpl* webViewImpl = static_cast<WebViewImpl*>(m_webView);
     EXPECT_EQ(980, webViewImpl->mainFrameImpl()->frameView()->contentsSize().width());
-    EXPECT_EQ(980.0 / viewportWidth * viewportHeight, webViewImpl->mainFrameImpl()->frameView()->contentsSize().height());
 }
 
 TEST_F(WebFrameTest, PageViewportInitialScaleOverridesLoadWithOverviewMode)
