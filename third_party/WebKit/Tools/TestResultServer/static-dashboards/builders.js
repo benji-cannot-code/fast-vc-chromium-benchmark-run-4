f2ad812f650c3571f188d726d7c94194728d026e
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-06 16:48:03 +0000

    Remove hard-coded list of groups from the flakiness dashboard.
    Now that we load builders.jsonp synchronously, we know that it will
    be available before anyone calls builders.getAllGroupNames, so we
    don't need to hard-code the list of group names.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16418004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151944 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/builders.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/builders.js
@@ -33,28 +33,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function LOAD_BUILDBOT_DATA(builderData)
 {
     builders.masters = {};
+    var groups = {};
     builderData['masters'].forEach(function(master) {
         builders.masters[master.name] = new builders.BuilderMaster(master.name, master.url, master.tests, master.groups);
+        master.groups.forEach(function(group) { groups[group] = true; });
     });
-    builders.groups = builderData['groups'];
+    builders.groups = Object.keys(groups);
 }
 
 var builders = builders || {};
 
 (function() {
 
-// FIXME: Move some of this loading logic into loader.js.
-
 builders._currentBuilderGroup = {};
-
-// FIXME: this list should be built on demand.
-builders._GROUP_NAMES = [
-    '@DEPS - chromium.org',
-    '@DEPS CrOS - chromium.org',
-    '@DEPS FYI - chromium.org',
-    '@ToT - chromium.org',
-    'Content Shell @ToT - chromium.org',
-]
 
 builders.getBuilderGroup = function(groupName, testType)
 {
@@ -97,7 +88,7 @@ builders.loadBuildersList = function(groupName, testType)
 
 builders.getAllGroupNames = function()
 {
-    return builders._GROUP_NAMES;
+    return builders.groups;
 }
 
 builders.BuilderMaster = function(name, basePath, tests, groups)
