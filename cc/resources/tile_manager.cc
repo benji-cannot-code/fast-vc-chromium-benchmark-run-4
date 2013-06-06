1426e5cbee5978d75de1e5b98a53f372a389d985
tomhudson@chromium.org <tomhudson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 21:11:33 +0000

    Track set of known decoded images in ManageTiles().
    Calling CreateRasterTask() can take 600us-1ms
    (https://code.google.com/p/chromium/issues/detail?id=239632)
    because PrepareForDecode() is slow.
    https://codereview.chromium.org/16432002/ adds MaybeDecoded(),
    which drops that down to 100us. When we can have 100 tiles being
    scheduled in a single call to ManageTiles(), that's 10-100ms on
    the compositor thread.
    
    This patch avoids making either call if it can, by only calling
    PrepareForDecode() the first time it encounters any pixel ref.
    Average CreateRasterTask() time drops to 120us; it ought to be
    combinable with 16432002 for even further savings.
    
    BUG=246986
    R=reveman
    
    Review URL: https://chromiumcodereview.appspot.com/16452008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204596 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/tile_manager.cc
+++ cc/resources/tile_manager.cc
@@ -553,6 +553,7 @@ void TileManager::FreeResourcesForTile(Tile* tile) {
 void TileManager::ScheduleTasks() {
   TRACE_EVENT0("cc", "TileManager::ScheduleTasks");
   RasterWorkerPool::RasterTask::Queue tasks;
+  PixelRefSet decoded_images;
 
   // Build a new task queue containing all task currently needed. Tasks
   // are added in order of priority, highest priority task first.
@@ -567,7 +568,7 @@ void TileManager::ScheduleTasks() {
 
     // Create raster task for this tile if necessary.
     if (mts.raster_task.is_null())
-      mts.raster_task = CreateRasterTask(tile);
+      mts.raster_task = CreateRasterTask(tile, &decoded_images);
 
     // Finally append raster task.
     tasks.Append(mts.raster_task);
@@ -615,7 +616,9 @@ TileManager::RasterTaskMetadata TileManager::GetRasterTaskMetadata(
   return metadata;
 }
 
-RasterWorkerPool::RasterTask TileManager::CreateRasterTask(Tile* tile) {
+RasterWorkerPool::RasterTask TileManager::CreateRasterTask(
+    Tile* tile,
+    PixelRefSet* decoded_images) {
   TRACE_EVENT0("cc", "TileManager::CreateRasterTask");
 
   scoped_ptr<ResourcePool::Resource> resource =
@@ -644,8 +647,12 @@ RasterWorkerPool::RasterTask TileManager::CreateRasterTask(Tile* tile) {
       continue;
     }
 
+    if (decoded_images->find(id) != decoded_images->end())
+      continue;
+
     // TODO(qinmin): passing correct image size to PrepareToDecode().
     if (pixel_ref->PrepareToDecode(skia::LazyPixelRef::PrepareParams())) {
+      decoded_images->insert(id);
       rendering_stats_instrumentation_->IncrementDeferredImageCacheHitCount();
       continue;
     }
