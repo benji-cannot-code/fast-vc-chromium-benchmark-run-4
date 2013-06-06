ff44643c2f4821b297992a96ba5b2310670004a4
varunjain@chromium.org <varunjain@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 21:46:44 +0000

    Change ordering of Shell event handlers: crrev.com/14184010 made the DragDropController to be the first handler everytime a drag starts. That was done under the assumption that once we are in a drag drop session, no other handler needs to see the event. This assumption turned out to be false because the MouseCursorEventFilter needs to handle events even during a drag drop session so that the cursor can move across displays (causing the bug listed below). Hence, we make the DragDropController the second event handler while MouseCursorEvedntFilter becomes the first.
    BUG=244508
    
    Review URL: https://chromiumcodereview.appspot.com/16463002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204612 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/drag_drop/drag_drop_controller.cc
+++ ash/drag_drop/drag_drop_controller.cc
@@ -149,7 +149,7 @@ DragDropController::DragDropController()
       drag_drop_window_delegate_(new DragDropTrackerDelegate(this)),
       current_drag_event_source_(ui::DragDropTypes::DRAG_EVENT_SOURCE_MOUSE),
       weak_factory_(this) {
-  Shell::GetInstance()->AddPreTargetHandler(this);
+  Shell::GetInstance()->PrependPreTargetHandler(this);
 }
 
 DragDropController::~DragDropController() {
@@ -226,12 +226,6 @@ int DragDropController::StartDragAndDrop(
   // Ends cancel animation if it's in progress.
   if (cancel_animation_)
     cancel_animation_->End();
-
-  // Become the first event handler since we should get first shot at handling
-  // any events during the drag drop session.
-  Shell::GetInstance()->RemovePreTargetHandler(this);
-  Shell::GetInstance()->PrependPreTargetHandler(this);
-
 
 #if !defined(OS_MACOSX)
   if (should_block_during_drag_drop_) {
