5a6a5a82cd60ae7ba3c9a3b0615f715fc27dadef
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 10:00:10 +0000

    Suppressing the creation of multiple V2 applications through fast clicking.
    BUG=246498
    TEST=visual
    
    Review URL: https://chromiumcodereview.appspot.com/16493006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204475 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/launcher/app_shortcut_launcher_item_controller.cc
+++ chrome/browser/ui/ash/launcher/app_shortcut_launcher_item_controller.cc
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/launcher/app_shortcut_launcher_item_controller.h"
 
 #include "ash/wm/window_util.h"
+#include "chrome/browser/extensions/extension_process_manager.h"
+#include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/favicon/favicon_tab_helper.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_app_menu_item.h"
 #include "chrome/browser/ui/ash/launcher/chrome_launcher_app_menu_item_tab.h"
@@ -87,7 +89,12 @@ void AppShortcutLauncherItemController::Launch(int event_flags) {
 void AppShortcutLauncherItemController::Activate() {
   content::WebContents* content = GetLRUApplication();
   if (!content) {
-    Launch(ui::EF_NONE);
+    // The initial launch of a V2 app will be done by this controller. Starting
+    // a hosted application takes time and if the user clicks fast, he can come
+    // here multiple times before the ShellLauncherItemController takes control.
+    // To avoid that situation we check for running applications.
+    if (!IsV2AppAndRunning())
+      Launch(ui::EF_NONE);
     return;
   }
   ActivateContent(content);
@@ -274,4 +281,14 @@ bool AppShortcutLauncherItemController::AdvanceToNextApp() {
     }
   }
   return false;
+}
+
+bool AppShortcutLauncherItemController::IsV2AppAndRunning() {
+  const Extension* extension =
+      app_controller_->GetExtensionForAppID(app_id());
+  extensions::ExtensionSystem* extension_system =
+      extensions::ExtensionSystem::Get(app_controller_->profile());
+  return extension && extension->is_platform_app() && extension_system &&
+         extension_system->process_manager()->GetBackgroundHostForExtension(
+             app_id());
 }
