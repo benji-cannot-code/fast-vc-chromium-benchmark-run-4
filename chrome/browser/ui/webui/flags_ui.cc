62da4a380fd63c26390fc80c913f141470232d36
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 23:13:10 +0000

    Promote the beta and dev channels on the about:flags page.
    BUG=177161
    
    Review URL: https://chromiumcodereview.appspot.com/16042016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204646 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/flags_ui.cc
+++ chrome/browser/ui/webui/flags_ui.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/browser_process.h"
 #include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
+#include "chrome/common/chrome_version_info.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/web_contents.h"
@@ -54,6 +55,9 @@ content::WebUIDataSource* CreateFlagsUIHTMLSource() {
                              IDS_FLAGS_NO_EXPERIMENTS_AVAILABLE);
   source->AddLocalizedString("flagsWarningHeader", IDS_FLAGS_WARNING_HEADER);
   source->AddLocalizedString("flagsBlurb", IDS_FLAGS_WARNING_TEXT);
+  source->AddLocalizedString("channelPromoBeta",
+                             IDS_FLAGS_PROMOTE_BETA_CHANNEL);
+  source->AddLocalizedString("channelPromoDev", IDS_FLAGS_PROMOTE_DEV_CHANNEL);
   source->AddLocalizedString("flagsUnsupportedTableTitle",
                              IDS_FLAGS_UNSUPPORTED_TABLE_TITLE);
   source->AddLocalizedString("flagsNoUnsupportedExperiments",
@@ -147,6 +151,16 @@ void FlagsDOMHandler::HandleRequestFlagsExperiments(const ListValue* args) {
   results.Set("unsupportedExperiments", unsupported_experiments.release());
   results.SetBoolean("needsRestart",
                      about_flags::IsRestartNeededToCommitChanges());
+#if defined(OS_WIN) || defined(OS_MACOSX) || defined(OS_CHROMEOS)
+  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
+  results.SetBoolean("showBetaChannelPromotion",
+                     channel == chrome::VersionInfo::CHANNEL_STABLE);
+  results.SetBoolean("showDevChannelPromotion",
+                     channel == chrome::VersionInfo::CHANNEL_BETA);
+#else
+  results.SetBoolean("showBetaChannelPromotion", false);
+  results.SetBoolean("showDevChannelPromotion", false);
+#endif
   web_ui()->CallJavascriptFunction("returnFlagsExperiments", results);
 }
 
