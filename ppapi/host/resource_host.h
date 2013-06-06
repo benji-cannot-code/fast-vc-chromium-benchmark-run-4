13af6c33fec0d57629682f1d315f87fd0479d6fc
raymes@chromium.org <raymes@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 23:25:06 +0000

    Add simple hand-rolled RTTI for Pepper ResourceHosts
    This allows Pepper ResourceHosts to be safely dynamically cast to a given type. For example, you may have a PP_Resource provided by the plugin which corresponds to a ResourceHost object which should be of type X. But statically casting to type X would be unsafe because the plugin is untrusted. 
    
    I've kept this simple for now because we don't have too many types which need to be dynamically cast. To add RTTI for a new class, you need to add a new AsPepperXXXHost virtual function in ResourceHost and override it in your derived class.
    
    This also cleans up the previous RTTI checks for Graphics2D and the unsafe checks that were being used for FileSystem.
    
    BUG=246396
    
    Review URL: https://chromiumcodereview.appspot.com/16057021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204652 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/host/resource_host.h
+++ ppapi/host/resource_host.h
@@ -15,6 +15,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/host/resource_message_handler.h"
 #include "ppapi/shared_impl/host_resource.h"
 
+namespace content {
+class PepperFileSystemHost;
+class PepperGraphics2DHost;
+}
+
 namespace IPC {
 class Message;
 }
@@ -48,14 +53,16 @@ class PPAPI_HOST_EXPORT ResourceHost : public ResourceMessageHandler {
   // host. This will notify subclasses by calling
   // DidConnectPendingHostToResource.
   //
-  // The current PP_Resource for all pending hosts should be 0.  See
+  // The current PP_Resource for all pending hosts should be 0. See
   // PpapiHostMsg_AttachToPendingHost.
   void SetPPResourceForPendingHost(PP_Resource pp_resource);
 
   virtual void SendReply(const ReplyMessageContext& context,
                          const IPC::Message& msg) OVERRIDE;
 
-  virtual bool IsGraphics2DHost() const { return false; }
+  // Simple RTTI. Overidden by subclasses that implement the interface.
+  virtual content::PepperFileSystemHost* AsPepperFileSystemHost();
+  virtual content::PepperGraphics2DHost* AsPepperGraphics2DHost();
 
  protected:
   // Adds a ResourceMessageFilter to handle resource messages. Incoming
