1b1999b467e6a374c1c509cb3a80fb243e996986
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 21:42:13 +0000

    Move FrameTreeNode to content/browser/web_contents, since it's part of WebContents.
    BUG=247412
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/15984013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204608 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/frame_tree_node.h
+++ content/browser/web_contents/frame_tree_node.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_BROWSER_RENDERER_HOST_FRAME_TREE_NODE_H_
-#define CONTENT_BROWSER_RENDERER_HOST_FRAME_TREE_NODE_H_
+#ifndef CONTENT_BROWSER_WEB_CONTENTS_FRAME_TREE_NODE_H_
+#define CONTENT_BROWSER_WEB_CONTENTS_FRAME_TREE_NODE_H_
 
 #include <string>
 #include <vector>
@@ -59,4 +59,4 @@ class CONTENT_EXPORT FrameTreeNode {
 
 }  // namespace content
 
-#endif  // CONTENT_BROWSER_RENDERER_HOST_FRAME_TREE_NODE_H_
+#endif  // CONTENT_BROWSER_WEB_CONTENTS_FRAME_TREE_NODE_H_
