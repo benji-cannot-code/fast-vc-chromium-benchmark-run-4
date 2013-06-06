b849dfee6ec0fdca0bfee8d2e53b0284b4d6eca6
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-06 02:33:48 +0000

    Remove stale keys from the flakiness dashboard results json.
    These keys are no longer used by the dashboard and will be removed
    completely when we switch gtests over to uploading the full_results.json
    format.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16512002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/bot_test_expectations.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/layout_package/bot_test_expectations.py
@@ -47,7 +47,6 @@ _log = logging.getLogger(__name__)
 # {
 #  'version': 4,
 #  'builder name' : {
-#     'fixableCounts': {},
 #     'blinkRevision': [],
 #     'tests': {
 #       'directory' { # Each path component is a dictionary.
@@ -61,8 +60,6 @@ _log = logging.getLogger(__name__)
 #   }
 #  'buildNumbers': [],
 #  'secondsSinceEpoch': [],
-#  'fixableCount': [],
-#  'allFixableCount': [],
 #  'chromeRevision': [],
 #  'failure_map': { } # Map from letter code to expectation name.
 # },
