51551c202b77b9ecec8f59c3e563be3364da695f
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-06 09:48:34 +0000

    Support the new TLS 1.2 HMAC-SHA256 cipher suites specified in RFC 5246 and RFC 5289.
    To avoid making ClientHello too big, the new DH_DSS, DH_RSA,
    DHE_DSS, DH_anon, ECDH_ECDSA, and ECDH_RSA are not added.
    
    Do not generate client_write_IV and server_write_IV in TLS 1.1+
    for CBC block ciphers because 1) they aren't used, and 2) a
    buffer in the NSS softoken is not big enough if the HMAC key
    is 32 bytes (for HMAC-SHA256) and client_write_IV and
    server_write_IV are still generated.
    
    Do not downgrade to TLS 1.1 silently when SSL_BYPASS_PKCS11
    mode is requested because we won't be able to test the new
    TLS 1.2 only cipher suites in PKCS #11 bypass mode. Instead,
    silently turn off PKCS #11 bypass if TLS 1.2 is enabled.
    
    R=agl@chromium.org
    BUG=90392
    TEST=none (done in NSS upstream)
    
    Review URL: https://chromiumcodereview.appspot.com/16394004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204467 0039d316-1c4b-4281-b951-d872f2087c98

--- net/third_party/nss/patches/applypatches.sh
+++ net/third_party/nss/patches/applypatches.sh
@@ -54,3 +54,5 @@ patch -p4 < $patches_dir/tls12.patch
 patch -p4 < $patches_dir/tls12chromium.patch
 
 patch -p4 < $patches_dir/tls12certrequest.patch
+
+patch -p4 < $patches_dir/tls12hmacsha256.patch
