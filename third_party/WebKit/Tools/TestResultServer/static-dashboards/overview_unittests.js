ff8af4ce30b2c225c0ad6f9758a2db57f9af7e0e
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-06 18:18:08 +0000

    Stop hard-coding the list of test types in the flakiness dashboard.
    Grab this data out of builders.jsonp. Filter out some test suites
    that don't make sense to ever show in the dashboard. There are still
    some test suites left after filtering that don't have any builders
    that upload to the dashboard, but that's it's own problem.
    
    Now that the list of test suites is much longer (~3x), by default
    don't show the test suites in the overview that have no flaky tests
    to make the page a bit more manageable/useful.
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16522003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@151952 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/static-dashboards/overview_unittests.js
+++ third_party/WebKit/Tools/TestResultServer/static-dashboards/overview_unittests.js
@@ -64,6 +64,10 @@ test('getFlakyData', 2, function() {
     var flipCountThreshold = 1;
     deepEqual(overview._getFlakyData(testTypes, resultsByTestType, flipCountThreshold), {
         'MockTestType': {
+            "flakyBelowThreshold": {
+                "TestSuite.Flaky": true,
+                "TestSuite.VeryFlaky": true
+            },
             'flaky': {
                 'TestSuite.Flaky': true,
                 'TestSuite.VeryFlaky': true
@@ -76,7 +80,11 @@ test('getFlakyData', 2, function() {
     flipCountThreshold = 5;
     deepEqual(overview._getFlakyData(testTypes, resultsByTestType, flipCountThreshold), {
         'MockTestType': {
+            "flakyBelowThreshold": {
+                "TestSuite.VeryFlaky": true
+            },
             'flaky': {
+                "TestSuite.Flaky": true,
                 'TestSuite.VeryFlaky': true
             },
             'testCount': 11
@@ -88,10 +96,15 @@ test('htmlForFlakyTests', 6, function() {
     var flakyData = {
         'browser_tests': {
             'testCount': 0,
+            "flakyBelowThreshold": {},
             'flaky': {}
         },
         'layout-tests': {
             'testCount': 4,
+            "flakyBelowThreshold": {
+                'css3/foo.html': true,
+                'css3/bar.html': true
+            },
             'flaky': {
                 'css3/foo.html': true,
                 'css3/bar.html': true
