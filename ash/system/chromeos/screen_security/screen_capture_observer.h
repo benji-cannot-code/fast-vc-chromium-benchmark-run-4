4f5b71a0e0ddf719da773a3472de1372572df4c8
tengs@chromium.org <tengs@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 00:08:25 +0000

    Implement screen sharing system tray and notification UI.
    Because this UI is very similar to the existing screen capture
    notification UI, we also refactor the screen capture code.
    The assets are placeholders. 
    
    BUG=246277
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/16007015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204669 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/chromeos/screen_security/screen_capture_observer.h
+++ ash/system/chromeos/screen_security/screen_capture_observer.h
@@ -1,5 +1,5 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
@@ -17,6 +17,7 @@ class ScreenCaptureObserver {
   virtual void OnScreenCaptureStart(
       const base::Closure& stop_callback,
       const base::string16& screen_capture_status) = 0;
+
   // Called when screen capture is stopped.
   virtual void OnScreenCaptureStop() = 0;
 
--- ash/system/tray/system_tray.cc
+++ ash/system/tray/system_tray.cc
@@ -55,7 +55,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/chromeos/network/tray_sms.h"
 #include "ash/system/chromeos/network/tray_vpn.h"
 #include "ash/system/chromeos/power/tray_power.h"
-#include "ash/system/chromeos/screen_capture/tray_screen_capture.h"
+#include "ash/system/chromeos/screen_security/screen_capture_tray_item.h"
+#include "ash/system/chromeos/screen_security/screen_share_tray_item.h"
 #include "ash/system/chromeos/settings/tray_settings.h"
 #include "ash/system/chromeos/tray_display.h"
 #endif
@@ -173,7 +174,8 @@ void SystemTray::CreateItems(SystemTrayDelegate* delegate) {
   AddTrayItem(new internal::TrayLocale(this));
 #if defined(OS_CHROMEOS)
   AddTrayItem(new internal::TrayDisplay(this));
-  AddTrayItem(new internal::TrayScreenCapture(this));
+  AddTrayItem(new internal::ScreenCaptureTrayItem(this));
+  AddTrayItem(new internal::ScreenShareTrayItem(this));
   AddTrayItem(new internal::TrayAudio(this));
 #endif
 #if !defined(OS_WIN)
