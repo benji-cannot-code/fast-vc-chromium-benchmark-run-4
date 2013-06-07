5c628fc9d6fd927a77aefe0053ebc51672c329b6
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-07 08:28:39 +0000

    mv public/webview to public/webpage
    Based on the discussion in https://codereview.chromium.org/16358003/, it sounds
    like folks prefer the name public/webpage to public/webview.
    
    R=jamesr@chromium.org
    BUG=239545
    
    Review URL: https://codereview.chromium.org/16623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152007 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/public/mac/WebScreenInfoFactory.h
+++ third_party/WebKit/Source/WebKit/chromium/public/mac/WebScreenInfoFactory.h
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include "../../../../../public/webview/mac/WebScreenInfoFactory.h"
+#include "../../../../../public/webpage/mac/WebScreenInfoFactory.h"
--- third_party/WebKit/Source/WebKit/chromium/public/x11/WebScreenInfoFactory.h
+++ third_party/WebKit/Source/WebKit/chromium/public/x11/WebScreenInfoFactory.h
@@ -1,2 +1,2 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#include "../../../../../public/webview/x11/WebScreenInfoFactory.h"
+#include "../../../../../public/webpage/x11/WebScreenInfoFactory.h"
