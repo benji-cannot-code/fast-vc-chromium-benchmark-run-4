0e1fc8a67879063e507661791c7b81733cbc57d1
earthdok@google.com <earthdok@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 22:09:32 +0000

    Annotate LeakyLazyInstance as a leak. Remove HeapChecker suppressions.
    The two suppressions removed by this CL have not fired for a long time,
    for some unknown reason. It's still a good idea to annotate all
    intentional leaks though.
    
    BUG=83345
    R=jar@chromium.org,darin@chromium.org
    CC=glider@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16440006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204953 0039d316-1c4b-4281-b951-d872f2087c98

--- base/lazy_instance.h
+++ base/lazy_instance.h
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/atomicops.h"
 #include "base/base_export.h"
 #include "base/basictypes.h"
+#include "base/debug/leak_annotations.h"
 #include "base/logging.h"
 #include "base/memory/aligned_memory.h"
 #include "base/third_party/dynamic_annotations/dynamic_annotations.h"
@@ -92,6 +93,7 @@ struct LeakyLazyInstanceTraits {
   static const bool kAllowedToAccessOnNonjoinableThread = true;
 
   static Type* New(void* instance) {
+    ANNOTATE_SCOPED_MEMORY_LEAK;
     return DefaultLazyInstanceTraits<Type>::New(instance);
   }
   static void Delete(Type* instance) {
