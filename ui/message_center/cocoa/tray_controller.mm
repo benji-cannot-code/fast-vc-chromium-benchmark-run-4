939894421a3358b4ad36aafc0cbf8fdf92b04bbe
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 19:57:40 +0000

    Scroll to the topmost notification when the message center tray opens.
    BUG=247705
    TEST=manaul test by creating lots of notifications and clicking on tray icon
    R=dewittj@chromium.org, rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/16617003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204903 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/tray_controller.mm
+++ ui/message_center/cocoa/tray_controller.mm
@@ -58,6 +58,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
   frame.origin.x = point.x;
   frame.origin.y = point.y - NSHeight(frame);
   [[self window] setFrame:frame display:YES];
+  [viewController_ scrollToTop];
   [self showWindow:nil];
 }
 
