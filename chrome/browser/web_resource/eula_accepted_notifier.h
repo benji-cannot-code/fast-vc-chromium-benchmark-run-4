1c4a56bfaadf9eab9e5dac8410ec5020398e9965
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 08:53:01 +0000

    Move ResourceRequestAllowedNotifier related code to chrome/browser/web_resource
    Relocate EulaAcceptedNotifier and ResourceRequestAllowedNotifier from
    chrome/browser/metrics/variations/ to chrome/browser/web_resource
    in order to use them from chrome/browser/translate.
    
    BUG=247352
    TEST=build all
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16146009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204763 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_resource/eula_accepted_notifier.h
+++ chrome/browser/web_resource/eula_accepted_notifier.h
@@ -1,10 +1,10 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_METRICS_VARIATIONS_EULA_ACCEPTED_NOTIFIER_H_
-#define CHROME_BROWSER_METRICS_VARIATIONS_EULA_ACCEPTED_NOTIFIER_H_
+#ifndef CHROME_BROWSER_WEB_RESOURCE_EULA_ACCEPTED_NOTIFIER_H_
+#define CHROME_BROWSER_WEB_RESOURCE_EULA_ACCEPTED_NOTIFIER_H_
 
 #include "base/basictypes.h"
 #include "base/prefs/pref_change_registrar.h"
@@ -57,4 +57,4 @@ class EulaAcceptedNotifier {
   DISALLOW_COPY_AND_ASSIGN(EulaAcceptedNotifier);
 };
 
-#endif  // CHROME_BROWSER_METRICS_VARIATIONS_EULA_ACCEPTED_NOTIFIER_H_
+#endif  // CHROME_BROWSER_WEB_RESOURCE_EULA_ACCEPTED_NOTIFIER_H_
