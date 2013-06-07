9665fdb27948f29aa7e2794b5a98ab13218a9bcf
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-07 20:00:57 +0000

    Revert WebKit patch http://trac.webkit.org/changeset/145788. This is breaking mouseup handling in many canvas/WebGL sites.
    BUG=244289
    
    Review URL: https://chromiumcodereview.appspot.com/15949024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152042 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLCanvasElement.h
+++ third_party/WebKit/Source/core/html/HTMLCanvasElement.h
@@ -132,8 +132,6 @@ public:
 
     float deviceScaleFactor() const { return m_deviceScaleFactor; }
 
-    virtual bool canContainRangeEndPoint() const { return false; }
-
     virtual void reportMemoryUsage(MemoryObjectInfo*) const OVERRIDE;
 
 private:
