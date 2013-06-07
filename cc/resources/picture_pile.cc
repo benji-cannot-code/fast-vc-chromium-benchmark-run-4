ef57206f45b6301ae05d8367cc4ac2d90dced2ea
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 07:50:56 +0000

    cc: Audit use of Rect::Contains with empty rects.
    A rect R contains an empty rect E if the origin of E is within the bounds
    of R. This change makes sure we guard for empty rects to get consistent
    behaviour regardless of the position of the empty rect for cases where we
    don't care about the position, only the pixels it covers.
    
    R=enne
    BUG=246760
    
    Review URL: https://chromiumcodereview.appspot.com/15973005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204733 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/resources/picture_pile.cc
+++ cc/resources/picture_pile.cc
@@ -122,7 +122,8 @@ class FullyContainedPredicate {
  public:
   explicit FullyContainedPredicate(gfx::Rect rect) : layer_rect_(rect) {}
   bool operator()(const scoped_refptr<Picture>& picture) {
-    return layer_rect_.Contains(picture->LayerRect());
+    return picture->LayerRect().IsEmpty() ||
+        layer_rect_.Contains(picture->LayerRect());
   }
   gfx::Rect layer_rect_;
 };
@@ -131,6 +132,7 @@ void PicturePile::InvalidateRect(
     PictureList& picture_list,
     gfx::Rect invalidation) {
   DCHECK(!picture_list.empty());
+  DCHECK(!invalidation.IsEmpty());
 
   std::vector<PictureList::iterator> overlaps;
   for (PictureList::iterator i = picture_list.begin();
