253b945c9653894842d49d01ad2ef4de6fdbc26b
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 21:39:28 +0000

    Add HiDPI flag for Windows.
    The flag is currently disabled by default.
    
    BUG=243445
    
    Review URL: https://chromiumcodereview.appspot.com/16196008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204939 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/win/dpi.cc
+++ ui/base/win/dpi.cc
@@ -6,10 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/win/dpi.h"
 
 #include <windows.h>
-
+#include "base/command_line.h"
 #include "base/win/scoped_hdc.h"
 #include "ui/base/layout.h"
 #include "base/win/registry.h"
+#include "ui/base/ui_base_switches.h"
 #include "ui/gfx/display.h"
 #include "ui/gfx/point_conversions.h"
 #include "ui/gfx/rect_conversions.h"
@@ -20,16 +21,26 @@ namespace {
 int kDefaultDPIX = 96;
 int kDefaultDPIY = 96;
 
+bool IsHighDPIEnabled() {
+  // Default is disabled.
+  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kHighDPISupport)) {
+    return CommandLine::ForCurrentProcess()->GetSwitchValueASCII(
+        switches::kHighDPISupport).compare("1") == 0;
+  }
+  return false;
+}
+
 float GetDeviceScaleFactorImpl() {
 #if defined(ENABLE_HIDPI)
-  float scale = gfx::Display::HasForceDeviceScaleFactor() ?
-      gfx::Display::GetForcedDeviceScaleFactor() : ui::GetDPIScale();
-  // Quantize to nearest supported scale factor.
-  scale = ui::GetScaleFactorScale(ui::GetScaleFactorFromScale(scale));
-  return scale;
-#else
-  return 1.0f;
+  if (IsHighDPIEnabled()) {
+    float scale = gfx::Display::HasForceDeviceScaleFactor() ?
+        gfx::Display::GetForcedDeviceScaleFactor() : ui::GetDPIScale();
+    // Quantize to nearest supported scale factor.
+    scale = ui::GetScaleFactorScale(ui::GetScaleFactorFromScale(scale));
+    return scale;
+  }
 #endif
+  return 1.0f;
 }
 
 BOOL IsProcessDPIAwareWrapper() {
