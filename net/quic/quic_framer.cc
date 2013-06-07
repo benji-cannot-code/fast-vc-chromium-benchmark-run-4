cb2d0cd957ae4d02b94b9f537a5a7457fa759f10
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 08:12:50 +0000

    Small cleanup changes that came up during merging chromium changes with the server code.
    R=rch@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16295024
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204750 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_framer.cc
+++ net/quic/quic_framer.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/crypto/quic_encrypter.h"
 #include "net/quic/quic_data_reader.h"
 #include "net/quic/quic_data_writer.h"
-#include "net/quic/quic_utils.h"
 
 using base::StringPiece;
 using std::make_pair;
