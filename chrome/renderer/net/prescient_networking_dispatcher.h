6a117a239140e8c40c48052461ed9f71f434c177
kouhei@chromium.org <kouhei@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-07 21:39:11 +0000

    Implement WebPrescientNetworking::prefetchDNS.
    This is to obsolete prefetchDNS() API which lives in platform API. I would like to move it under WebPrescientNetworking.
    
    The corresponding Blink patch is http://crrev.com/15699002
    
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/15963006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/net/prescient_networking_dispatcher.h
+++ chrome/renderer/net/prescient_networking_dispatcher.h
@@ -7,14 +7,22 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_RENDERER_NET_PRESCIENT_NETWORKING_DISPATCHER_H_
 
 #include "base/compiler_specific.h"
+#include "chrome/renderer/net/renderer_net_predictor.h"
 #include "third_party/WebKit/public/platform/WebPrescientNetworking.h"
 
 class PrescientNetworkingDispatcher : public WebKit::WebPrescientNetworking {
  public:
+  PrescientNetworkingDispatcher();
   virtual ~PrescientNetworkingDispatcher();
+
+  virtual void prefetchDNS(const WebKit::WebString& hostname) OVERRIDE;
 
   virtual void preconnect(const WebKit::WebURL& url,
                           WebKit::WebPreconnectMotivation motivation) OVERRIDE;
+ private:
+  RendererNetPredictor net_predictor_;
+
+  DISALLOW_COPY_AND_ASSIGN(PrescientNetworkingDispatcher);
 };
 
 #endif  // CHROME_RENDERER_NET_PRESCIENT_NETWORKING_DISPATCHER_H_
