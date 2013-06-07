c36daf6079068ac9f6c98a5ed1b5e2705ab551b8
skyostil@chromium.org <skyostil@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-07 10:05:58 +0000

    Emulate packed depth/stencil with two renderbuffers for WebGL
    The WebGL specification allows users to allocate a packed depth and
    stencil renderbuffer using the DEPTH_STENCIL internal format. Currently
    this is implemented directly with the OES/EXT_packed_depth_stencil
    OpenGL extension. However, because OpenGL ES does not require this
    extension, some devices do not support it and hence fail the WebGL
    conformance test.
    
    This patch emulates DEPTH_STENCIL using two distinct renderbuffers for
    depth and stencil. The auxiliary stencil buffer is allocated on demand in
    renderbufferStorage() and framebufferRenderbuffer(). When framebufferRenderbuffer() is called
    with the DEPTH_STENCIL_ATTACHEMENT, the respective buffers are bound to
    the DEPTH_ATTACHMENT and STENCIL_ATTACHMENT. This emulation is only
    performed if OES/EXT_packed_depth_stencil is not available.
    
    BUG=246639
    TEST=WebGL renderbuffer conformance test[1] using a Nexus 7.
    
    [1] https://www.khronos.org/registry/webgl/conformance-suites/1.0.1/conformance/renderbuffers/framebuffer-object-attachment.html
    
    Review URL: https://chromiumcodereview.appspot.com/15793012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152011 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/WebGLRenderbuffer.cpp
+++ third_party/WebKit/Source/core/html/canvas/WebGLRenderbuffer.cpp
@@ -48,7 +48,6 @@ WebGLRenderbuffer::WebGLRenderbuffer(WebGLRenderingContext* ctx)
     , m_initialized(false)
     , m_width(0)
     , m_height(0)
-    , m_isValid(true)
     , m_hasEverBeenBound(false)
 {
     ScriptWrappable::init(this);
@@ -58,6 +57,15 @@ WebGLRenderbuffer::WebGLRenderbuffer(WebGLRenderingContext* ctx)
 void WebGLRenderbuffer::deleteObjectImpl(GraphicsContext3D* context3d, Platform3DObject object)
 {
     context3d->deleteRenderbuffer(object);
+    deleteEmulatedStencilBuffer(context3d);
+}
+
+void WebGLRenderbuffer::deleteEmulatedStencilBuffer(GraphicsContext3D* context3d)
+{
+    if (!m_emulatedStencilBuffer)
+        return;
+    m_emulatedStencilBuffer->deleteObject(context3d);
+    m_emulatedStencilBuffer.clear();
 }
 
 }
