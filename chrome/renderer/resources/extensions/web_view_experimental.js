7e3d0876381b23121a1411c2be81248d94ec8536
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-08 00:49:04 +0000

    <webview>: Remove spurious WebRequest message
    The following log message was coming up a lot when <webview> was used:
    
    [3103:3847:0530/155855:INFO:CONSOLE(431)] "chrome.webRequest is not available: You do not have permission to access this API. Ensure that the required permission or manifest property is included in your manifest.json.", source: binding (431)
    
    
    This CL addresses this issue by moving WebRequestEvent into webRequestInternal.
    
    BUG=245399
    Test=manually, all existing WebRequest tests pass.
    
    Review URL: https://chromiumcodereview.appspot.com/16408015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@204999 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/web_view_experimental.js
+++ chrome/renderer/resources/extensions/web_view_experimental.js
@@ -12,10 +12,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // permission API would only be available for channels CHANNEL_DEV and
 // CHANNEL_CANARY.
 
+var WebRequestEvent = require('webRequestInternal').WebRequestEvent;
+var webRequestSchema =
+    requireNative('schema_registry').GetSchema('webRequest');
 var WebView = require('webView').WebView;
-var GetExtensionAPIDefinitions =
-      requireNative('apiDefinitions').GetExtensionAPIDefinitions;
-var WebRequestEvent = require('webRequest').WebRequestEvent;
 var forEach = require('utils').forEach;
 
 /** @type {Array.<string>} */
@@ -207,12 +207,9 @@ WebView.prototype.setupNewWindowEvent_ = function() {
 WebView.prototype.setupWebRequestEvents_ = function() {
   var self = this;
   // Populate the WebRequest events from the API definition.
-  var webRequestDefinition = GetExtensionAPIDefinitions().filter(function(api) {
-    return api.namespace == 'webRequest';
-  })[0];
-  for (var i = 0; i < webRequestDefinition.events.length; ++i) {
+  for (var i = 0; i < webRequestSchema.events.length; ++i) {
     Object.defineProperty(self.webviewNode_,
-                          webRequestDefinition.events[i].name, {
+                          webRequestSchema.events[i].name, {
       get: function(webRequestEvent) {
         return function() {
           if (!self[webRequestEvent.name + '_']) {
@@ -225,7 +222,7 @@ WebView.prototype.setupWebRequestEvents_ = function() {
           }
           return self[webRequestEvent.name + '_'];
         }
-      }(webRequestDefinition.events[i]),
+      }(webRequestSchema.events[i]),
       // No setter.
       enumerable: true
     });
