e941ee576b13a816d8c684448d031f9c501021bc
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-08 12:58:52 +0000

    Revert 205044 "Compositor reflector, which copies texture onto a..."
    This broke CromeOS compile
    
    cc1plus: warnings being treated as errors
    ../../ash/display/display_info.cc:40:error: this decimal constant is unsigned only in ISO C90
    ninja: build stopped: subcommand failed.
    
    http://build.chromium.org/p/chromium.chrome/builders/Google%20Chrome%20ChromeOS/builds/54421
    
    > Compositor reflector, which copies texture onto another compositor.
    > 
    > - added GL utility functions to create and copy texture for mirroring
    > - made sure we don't create two compositor for the same display.
    > - update the mirroring compositor size when the mirror window size
    >   changed.
    > - fix a bug that was using int for display ID. changed the
    >   initial value for faked display id so that we can catch
    >   such error early in test.
    > 
    > 
    > BUG=239776
    > TEST=added new tests, plus manually on daisy (mirroring now works on daisy)
    > 
    > Review URL: https://chromiumcodereview.appspot.com/16232013
    
    TBR=oshima@chromium.org
    Review URL: https://codereview.chromium.org/16434007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205056 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/compositor.h
+++ ui/compositor/compositor.h
@@ -43,7 +43,6 @@ class GLSurface;
 class GLShareGroup;
 class Point;
 class Rect;
-class Size;
 }
 
 namespace WebKit {
@@ -57,8 +56,6 @@ class CompositorObserver;
 class ContextProviderFromContextFactory;
 class Layer;
 class PostedSwapQueue;
-class Reflector;
-class Texture;
 struct LatencyInfo;
 
 // This class abstracts the creation of the 3D context for the compositor. It is
@@ -85,14 +82,6 @@ class COMPOSITOR_EXPORT ContextFactory {
   // with all compositors.
   virtual WebKit::WebGraphicsContext3D* CreateOffscreenContext() = 0;
 
-  // Creates a reflector that copies the content of the |mirrored_compositor|
-  // onto |mirroing_layer|.
-  virtual scoped_refptr<Reflector> CreateReflector(
-      Compositor* mirrored_compositor,
-      Layer* mirroring_layer) = 0;
-  // Removes the reflector, which stops the mirroring.
-  virtual void RemoveReflector(scoped_refptr<Reflector> reflector) = 0;
-
   virtual scoped_refptr<cc::ContextProvider>
       OffscreenContextProviderForMainThread() = 0;
   virtual scoped_refptr<cc::ContextProvider>
@@ -112,12 +101,6 @@ class COMPOSITOR_EXPORT DefaultContextFactory : public ContextFactory {
   virtual cc::OutputSurface* CreateOutputSurface(
       Compositor* compositor) OVERRIDE;
   virtual WebKit::WebGraphicsContext3D* CreateOffscreenContext() OVERRIDE;
-
-  virtual scoped_refptr<Reflector> CreateReflector(
-      Compositor* compositor,
-      Layer* layer) OVERRIDE;
-  virtual void RemoveReflector(scoped_refptr<Reflector> reflector) OVERRIDE;
-
   virtual scoped_refptr<cc::ContextProvider>
       OffscreenContextProviderForMainThread() OVERRIDE;
   virtual scoped_refptr<cc::ContextProvider>
@@ -149,12 +132,6 @@ class COMPOSITOR_EXPORT TestContextFactory : public ContextFactory {
   virtual cc::OutputSurface* CreateOutputSurface(
       Compositor* compositor) OVERRIDE;
   virtual WebKit::WebGraphicsContext3D* CreateOffscreenContext() OVERRIDE;
-
-  virtual scoped_refptr<Reflector> CreateReflector(
-      Compositor* mirrored_compositor,
-      Layer* mirroring_layer) OVERRIDE;
-  virtual void RemoveReflector(scoped_refptr<Reflector> reflector) OVERRIDE;
-
   virtual scoped_refptr<cc::ContextProvider>
       OffscreenContextProviderForMainThread() OVERRIDE;
   virtual scoped_refptr<cc::ContextProvider>
@@ -212,20 +189,6 @@ class COMPOSITOR_EXPORT CompositorDelegate {
 
  protected:
   virtual ~CompositorDelegate() {}
-};
-
-class COMPOSITOR_EXPORT Reflector
-    : public base::RefCountedThreadSafe<Reflector> {
- public:
-  Reflector() {}
-
-  virtual void OnMirroringCompositorResized() {}
-
- protected:
-  friend class base::RefCountedThreadSafe<Reflector>;
-  virtual ~Reflector() {}
-
-  DISALLOW_COPY_AND_ASSIGN(Reflector);
 };
 
 // This class represents a lock on the compositor, that can be used to prevent
