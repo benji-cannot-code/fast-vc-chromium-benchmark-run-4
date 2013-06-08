d1f99adea31a28b696d794a5f4d23236b57a0467
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-08 17:48:31 +0000

    Only update details for current network
    It's not clear to me how this every worked correctly, but it appears to
    have been broken for a while at least. I went with the simpler fix of
    ignoring updates for networks other than the displayed one because I
    don't want to risk missing updates if the JS and C++ get out of sync
    somehow. We can improve this when we convert to NetworkStateHandler
    (in progress).
    
    BUG=244360
    
    Review URL: https://chromiumcodereview.appspot.com/16676003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205079 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/internet_detail.js
+++ chrome/browser/resources/options/chromeos/internet_detail.js
@@ -760,6 +760,9 @@ cr.define('options.internet', function() {
     if (!data)
       return;
 
+    if (update.servicePath != data.servicePath)
+      return;
+
     // Update our cached data object.
     updateDataObject(data, update);
 
