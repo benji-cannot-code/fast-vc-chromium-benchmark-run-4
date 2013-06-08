8a3a3bd098723874cf27837a669b9d6ca7d19cb7
kaanb@chromium.org <kaanb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-08 05:03:33 +0000

    Pass the command buffer sizes in the WGC3DCBI constructor
    BUG=243703
    
    Review URL: https://chromiumcodereview.appspot.com/16666002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205023 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/compositor_impl_android.cc
+++ content/browser/renderer_host/compositor_impl_android.cc
@@ -362,7 +362,7 @@ scoped_ptr<cc::OutputSurface> CompositorImpl::CreateOutputSurface() {
                                                   url,
                                                   factory,
                                                   weak_factory_.GetWeakPtr()));
-    if (!context->Initialize(
+    if (!context->InitializeWithDefaultBufferSizes(
         attrs,
         false,
         CAUSE_FOR_GPU_LAUNCH_WEBGRAPHICSCONTEXT3DCOMMANDBUFFERIMPL_INITIALIZE)) {
