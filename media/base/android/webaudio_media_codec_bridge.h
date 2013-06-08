fc6dbcc41a54040840fde3d6add6f1b251c14206
rtoy@google.com <rtoy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-08 00:51:01 +0000

    Fix webaudio hang by saving the data to a file and reading from the file.
    BUG=239498
    
    See also b/9261835.
    
    Review URL: https://chromiumcodereview.appspot.com/16525005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205000 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/webaudio_media_codec_bridge.h
+++ media/base/android/webaudio_media_codec_bridge.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <jni.h>
 
 #include "base/file_descriptor_posix.h"
-#include "base/shared_memory.h"
+#include "base/memory/shared_memory.h"
 #include "media/base/media_export.h"
 
 namespace media {
@@ -56,9 +56,14 @@ class MEDIA_EXPORT WebAudioMediaCodecBridge {
   // Returns true if decoding was successful.
   bool DecodeInMemoryAudioFile();
 
+  // Save encoded audio data to a temporary file and return the file
+  // descriptor to that file.  -1 is returned if the audio data could
+  // not be saved for any reason.
+  int SaveEncodedAudioToFile(JNIEnv*, jobject);
+
   // The encoded audio data is read from this file descriptor for the
   // shared memory that holds the encoded data.
-  int encoded_audio_handle_;
+  base::SharedMemoryHandle encoded_audio_handle_;
 
   // The audio file information and decoded pcm data are written to
   // this file descriptor. We take ownership of this descriptor.
