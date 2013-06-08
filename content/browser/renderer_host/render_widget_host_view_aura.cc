7aab2816372d74cc43b867ebf1564209a27b673f
hubbe@chromium.org <hubbe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-08 16:45:41 +0000

    Multi-render target optimization of GPU RGB -> YUV implementation. Cuts YUV conversion time in half (or better). Saves 0.3ms on my desktop, probably more on slow machines.
    Same as CL 16235007 but with fixes for android
    
    Review URL: https://chromiumcodereview.appspot.com/16599005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205071 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -1287,6 +1287,7 @@ void RenderWidgetHostViewAura::CopyFromCompositingSurfaceToVideoFrame(
                                              src_subrect_in_pixel,
                                              target->coded_size(),
                                              region_in_frame,
+                                             true,
                                              true));
   }
 
