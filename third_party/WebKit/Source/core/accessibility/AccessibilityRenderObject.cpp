e4f89d56688b2a82552eb69d9342d9fb85d62b8d
dmazzoni@chromium.org <dmazzoni@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-09 20:10:05 +0000

    Fix crash when assuming that an AREA's parent must be a MAP.
    BUG=246635
    
    Review URL: https://chromiumcodereview.appspot.com/16647009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152090 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AccessibilityRenderObject.cpp
+++ third_party/WebKit/Source/core/accessibility/AccessibilityRenderObject.cpp
@@ -2189,7 +2189,12 @@ AccessibilityObject* AccessibilityRenderObject::accessibilityImageMapHitTest(HTM
     if (!area)
         return 0;
 
-    HTMLMapElement* map = static_cast<HTMLMapElement*>(area->parentNode());
+    Element* mapParent = area->parentElement();
+    while (mapParent && !mapParent->hasTagName(mapTag))
+        mapParent = mapParent->parentElement();
+    if (!mapParent)
+        return 0;
+    HTMLMapElement* map = static_cast<HTMLMapElement*>(mapParent);
     AccessibilityObject* parent = accessibilityParentForImageMap(map);
     if (!parent)
         return 0;
