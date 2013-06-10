b89dc129e7efde4ad2c5cf2976e40d149e818fb8
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-10 05:40:47 +0000

    Changes to closing contents with beforeunload/unload handlers: - Closing a single tab, run beforeunload (if needed), then detached the tab from    tab strip and close it asynchronously (no ui). - Closing a window, run all beforeunload handlers (same as before), then detach all tabs with unload handlers. Close any remaining tabs and hide the browser window while waiting for the unload handlers to complete.
    This CL started with fast-tab-closure and has grown to include fast-window-closure too.
    
    BUG=142458,156896
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=195108
    
    R=avi@chromium.org, ben@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=203922
    
    Review URL: https://codereview.chromium.org/11016023
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205149 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser.cc
+++ chrome/browser/ui/browser.cc
@@ -587,6 +587,14 @@ bool Browser::ShouldCloseWindow() {
   return unload_controller_->ShouldCloseWindow();
 }
 
+bool Browser::TabsNeedBeforeUnloadFired() {
+  return unload_controller_->TabsNeedBeforeUnloadFired();
+}
+
+bool Browser::HasCompletedUnloadProcessing() const {
+  return unload_controller_->HasCompletedUnloadProcessing();
+}
+
 bool Browser::IsAttemptingToCloseBrowser() const {
   return unload_controller_->is_attempting_to_close_browser();
 }
@@ -630,8 +638,6 @@ void Browser::OnWindowClosing() {
       chrome::NOTIFICATION_BROWSER_CLOSING,
       content::Source<Browser>(this),
       content::NotificationService::NoDetails());
-
-  tab_strip_model_->CloseAllTabs();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -1166,10 +1172,6 @@ bool Browser::PreHandleKeyboardEvent(content::WebContents* source,
 void Browser::HandleKeyboardEvent(content::WebContents* source,
                                   const NativeWebKeyboardEvent& event) {
   window()->HandleKeyboardEvent(event);
-}
-
-bool Browser::TabsNeedBeforeUnloadFired() {
-  return unload_controller_->TabsNeedBeforeUnloadFired();
 }
 
 bool Browser::IsMouseLocked() const {
