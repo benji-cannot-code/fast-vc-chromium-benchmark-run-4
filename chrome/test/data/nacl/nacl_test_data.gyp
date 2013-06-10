a5907e1875ba44bcfcc4ba99dffe6c173288579c
garykac@chromium.org <garykac@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-10 23:16:58 +0000

    Revert 205329 "Split pnacl and nacl mime types"
    NaCl integration tests fail:
    run_ppapi_bad_browser_test (scons-out/nacl-x86-64/test_results/ppapi_bad_browser_test.out) failed: Source `tests/ppapi_browser/bad/ppapi_bad_doesnotexist_nexe_only.nmf' not found, needed by target `scons-out/nacl-x86-64/test_results/ppapi_bad_browser_test.out'.
    
    
    > Split pnacl and nacl mime types
    > 
    > Manifest parsing now knows which mime type was used, and does not allow
    > pnacl to be used from nacl manifests or vice-versa.
    > 
    > BUG= https://code.google.com/p/nativeclient/issues/detail?id=3440
    > R=bradnelson@chromium.org, jam@chromium.org, jvoung@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/16296005
    
    TBR=sehr@chromium.org
    
    Review URL: https://codereview.chromium.org/15709012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205340 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/nacl/nacl_test_data.gyp
+++ chrome/test/data/nacl/nacl_test_data.gyp
@@ -160,23 +160,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       ]
     },
     {
-      'target_name': 'pnacl_mime_type_test',
-      'type': 'none',
-      'variables': {
-        'build_newlib': 1,
-        'build_glibc': 1,
-        'build_pnacl_newlib': 1,
-        'nexe_destination_dir': 'nacl_test_data',
-        # No need to translate AOT.
-        'enable_x86_32': 0,
-        'enable_x86_64': 0,
-        'enable_arm': 0,
-        'test_files': [
-          'pnacl_mime_type/pnacl_mime_type.html',
-        ],
-      },
-    },
-    {
       'target_name': 'pnacl_options_test',
       'type': 'none',
       'variables': {
--- ppapi/native_client/tests/ppapi_browser/bad/ppapi_bad.js
+++ ppapi/native_client/tests/ppapi_browser/bad/ppapi_bad.js
@@ -1,19 +1,14 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 // Helper routines for generating bad load tests.
 // Webpage must have an 'embeds' div for injecting NaCl modules.
 // Depends on nacltest.js.
 
-function createModule(id, src, type) {
+function createModule(id, src) {
   return createNaClEmbed({
     id: id,
     src: src,
     width: 100,
-    height: 20,
-    type: type
+    height: 20
   });
 }
 
@@ -28,9 +23,9 @@ function removeModule(module) {
 }
 
 
-function badLoadTest(tester, id, src, type, error_string) {
+function badLoadTest(tester, id, src, error_string) {
   tester.addAsyncTest(id, function(test){
-    var module = createModule(id, src, type);
+    var module = createModule(id, src);
 
     test.expectEvent(module, 'load', function(e) {
       removeModule(module);
