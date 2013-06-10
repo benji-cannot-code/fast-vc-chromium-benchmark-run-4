ade4d21fca50fabb1efaf11c770eccba8d6f3b2b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-10 20:13:42 +0000

    Increase the gap between touch-mode tabs by 5 px to not look so squished.
    BUG=157219
    TEST=Run with --touch-optimized-ui=enabled, tabs should have reasonable gaps
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/16370003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205286 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/tabs/tab_strip.cc
+++ chrome/browser/ui/views/tabs/tab_strip.cc
@@ -149,7 +149,7 @@ int tab_h_offset() {
         value = -26;
         break;
       case ui::LAYOUT_TOUCH:
-        value = -39;
+        value = -34;
         break;
       default:
         NOTREACHED();
