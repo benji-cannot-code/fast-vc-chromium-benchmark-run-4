e952c7d6195f3f7a07e46941ee1b3f80b05e4269
thakis@chromium.org <thakis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-10 19:28:45 +0000

    mac notifications: Let <esc> close the settings view and the tray.
    To close the settings view, give the back button a key equivalent of \e
    (a view controller isn't in the view's responder chain, so implementing
    cancelOperation: doesn't help).
    
    To close the tray, override the window's cancelOperation: and order the window
    out which will tell the controller that key was resigned, which then informs
    the model classes (implementing cancelOperation: on the controller doesn't work
    because NSWindow implements cancelOperation: and so won't forward it along the
    responder chain).
    
    While here, also remove -forceWindowSizeUpdate which was just another name
    for -updateTrayViewAndWindow.
    
    BUG=none
    TEST=open tray, open settings. hit esc, settings close. hit esc, tray closes.
    R=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/16721002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205273 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/tray_view_controller.mm
+++ ui/message_center/cocoa/tray_view_controller.mm
@@ -42,10 +42,6 @@ const int kBackButtonSize = 16;
 
 // Step 3: finalize the tray view and window to get rid of the empty space.
 - (void)finalizeTrayViewAndWindow;
-
-// Informs the window controller that it should update the window's size based
-// on the size of the tray view.
-- (void)forceWindowSizeUpdate;
 @end
 
 namespace {
@@ -230,7 +226,7 @@ const CGFloat kTrayBottomMargin = 75;
 
   [[[self view] window] recalculateKeyViewLoop];
 
-  [self forceWindowSizeUpdate];
+  [self updateTrayViewAndWindow];
 }
 
 - (void)hideSettings:(id)sender {
@@ -244,7 +240,7 @@ const CGFloat kTrayBottomMargin = 75;
   [clearAllButton_ setEnabled:YES];
 
   [[[self view] window] recalculateKeyViewLoop];
-  [self forceWindowSizeUpdate];
+  [self updateTrayViewAndWindow];
 }
 
 - (void)scrollToTop {
@@ -329,6 +325,7 @@ const CGFloat kTrayBottomMargin = 75;
   [backButton_ setAction:@selector(hideSettings:)];
   configureButton(backButton_);
   [backButton_ setHidden:YES];
+  [backButton_ setKeyEquivalent:@"\e"];
   [[self view] addSubview:backButton_];
 
   // Create the divider line between the control area and the notifications.
@@ -563,10 +560,6 @@ const CGFloat kTrayBottomMargin = 75;
 
   // Check if there're more notifications pending removal.
   [self closeNotificationsByUser];
-}
-
-- (void)forceWindowSizeUpdate {
-  [self updateTrayViewAndWindow];
 }
 
 @end
