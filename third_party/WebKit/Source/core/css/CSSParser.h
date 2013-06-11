e9a60468c155e00e7a56613e9b68a44190e91048
rune@opera.com <rune@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-11 12:27:56 +0000

    Parse media attributes and CSSOM media text as media_query_list.
    This is per HTML5 and CSSOM specifications.
    
    This change makes the remaining fails in the W3C Media Queries test suite pass.
    
    Some resulting changes:
    
    - Brackets/parentheses balanced correctly.
    - Queries split between correct commas.
    - Spaces is an empty media query list like the empty string.
    - Unexpected EOF like missing parentheses makes the query valid as per CSS21
      spec.
    - Syntax errors become "not all" as per CSSMQ spec.
    - Renamed @-webkit-mediaquery to @-internal-mediaquery to be consistent with
      other internal @-rules.
    - Removed MediaQueryParserMode which is now the same for all MQ parsing.
    
    BUG=247657
    
    Review URL: https://chromiumcodereview.appspot.com/15679021
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152218 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser.h
+++ third_party/WebKit/Source/core/css/CSSParser.h
@@ -102,7 +102,7 @@ public:
     PassRefPtr<CSSPrimitiveValue> parseValidPrimitive(CSSValueID ident, CSSParserValue*);
     bool parseDeclaration(MutableStylePropertySet*, const String&, SourceDataHandler*, StyleSheetContents* contextStyleSheet);
     static PassRefPtr<ImmutableStylePropertySet> parseInlineStyleDeclaration(const String&, Element*);
-    PassOwnPtr<MediaQuery> parseMediaQuery(const String&);
+    PassRefPtr<MediaQuerySet> parseMediaQueryList(const String&);
 
     void addPropertyWithPrefixingVariant(CSSPropertyID, PassRefPtr<CSSValue>, bool important, bool implicit = false);
     void addProperty(CSSPropertyID, PassRefPtr<CSSValue>, bool important, bool implicit = false);
@@ -357,7 +357,7 @@ public:
     StyleSheetContents* m_styleSheet;
     RefPtr<StyleRuleBase> m_rule;
     RefPtr<StyleKeyframe> m_keyframe;
-    OwnPtr<MediaQuery> m_mediaQuery;
+    RefPtr<MediaQuerySet> m_mediaList;
     OwnPtr<CSSParserValueList> m_valueList;
     bool m_supportsCondition;
 
