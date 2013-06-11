8959dc5c030fc9a4e914d8e0f294353c3d5dad50
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 04:27:38 +0000

    Use a direct include of strings headers in chrome/nacl/.
    BUG=247723
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15961008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205406 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_exe_win_64.cc
+++ chrome/nacl/nacl_exe_win_64.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop.h"
 #include "base/power_monitor/power_monitor.h"
 #include "base/process_util.h"
-#include "base/string_util.h"
+#include "base/strings/string_util.h"
 #include "chrome/app/breakpad_win.h"
 #include "chrome/common/chrome_result_codes.h"
 #include "chrome/common/chrome_switches.h"
