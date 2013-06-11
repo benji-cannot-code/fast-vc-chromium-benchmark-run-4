d510942cf76922c90b18ab2f128d78f3f7306d63
zork@chromium.org <zork@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-11 12:14:47 +0000

    Extend WebBlobRegistry API to support Stream.
    This will provide a bridge between the Streams implementation in content/ and the W3C Stream API.
    
    registerBlobURL(const WebURL&, const WebURL& srcURL) and unregisterBlobURL which are applicable for both Blob and Stream could be renamed to ones without "Blob" or with "Stream". Left FIXME to do that later.
    
    Blob is planned to be a subclass of Stream but it's still under discussion. See the thread "Intent to Implement: Streams API" at blink-dev@ for more info. 
    
    BUG=169957
    
    Review URL: https://chromiumcodereview.appspot.com/14628004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152217 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/platform/WebBlobRegistry.h
+++ third_party/WebKit/public/platform/WebBlobRegistry.h
@@ -37,6 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 class WebBlobData;
+class WebThreadSafeData;
 class WebURL;
 
 class WebBlobRegistry {
@@ -47,10 +48,27 @@ public:
 
     // Registers a blob URL referring to the specified blob data.
     virtual void registerBlobURL(const WebURL&, WebBlobData&) = 0;
-    
-    // Registers a blob URL referring to the blob data identified by the specified srcURL.
+
+    // Registers a stream URL.
+    virtual void registerStreamURL(const WebURL&) { WEBKIT_ASSERT_NOT_REACHED(); };
+
+    // Registers a blob or stream URL referring to the blob data or stream
+    // identified by the specified srcURL.
+    //
+    // FIXME: Rename this to registerURL or registerStream after experimental
+    // implementation is done.
     virtual void registerBlobURL(const WebURL&, const WebURL& srcURL) = 0;
 
+    // Add data to the stream referred by the URL.
+    virtual void addDataToStream(const WebURL&, WebThreadSafeData&) { WEBKIT_ASSERT_NOT_REACHED(); }
+
+    // Tell the registry that this stream won't receive any more data.
+    virtual void finalizeStream(const WebURL&) { WEBKIT_ASSERT_NOT_REACHED(); }
+
+    // Unregisters a blob or stream referred by the URL.
+    //
+    // FIXME: Rename this to unregisterURL or unregisterStreamURL after
+    // experimental implementation is done.
     virtual void unregisterBlobURL(const WebURL&) = 0;
 };
 
