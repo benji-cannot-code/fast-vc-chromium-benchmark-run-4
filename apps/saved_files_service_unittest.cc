ad5f2eaade66e5dd097779d0b6859b73ac29722c
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 04:57:52 +0000

    Use a direct include of strings headers in apps/.
    BUG=247723
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15855014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205417 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/saved_files_service_unittest.cc
+++ apps/saved_files_service_unittest.cc
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "apps/saved_files_service.h"
 #include "base/files/file_path.h"
-#include "base/string_number_conversions.h"
+#include "base/strings/string_number_conversions.h"
 #include "base/test/values_test_util.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_prefs.h"
--- apps/shortcut_manager.cc
+++ apps/shortcut_manager.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
-#include "base/string16.h"
+#include "base/strings/string16.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/ui/web_applications/web_app_ui.h"
