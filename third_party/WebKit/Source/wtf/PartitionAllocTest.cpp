44e477d99a0eb98fbb70f4b823d0f59fe459c8ad
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-11 10:54:57 +0000

    Run PartitionAllocTest on all UNIX-like systems.
    This required fixing the allocation mask for 32-bit systems, and led to discovery + fix of a pretty silly alignment bug that could potentially trigger on any system.
    
    BUG=246860
    R=tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/16701002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152211 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
+++ third_party/WebKit/Source/wtf/PartitionAllocTest.cpp
@@ -37,7 +37,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <string.h>
 
-#if OS(LINUX) && CPU(X86_64) && defined(NDEBUG)
+#if OS(UNIX)
+#include <sys/mman.h>
+
+#ifndef MAP_ANONYMOUS
+#define MAP_ANONYMOUS MAP_ANON
+#endif
+#endif // OS(UNIX)
+
+#if OS(UNIX) && defined(NDEBUG)
 
 namespace {
 
@@ -80,6 +88,8 @@ static WTF::PartitionPageHeader* GetFullPage()
     EXPECT_EQ(reinterpret_cast<size_t>(first) & WTF::kPageMask, reinterpret_cast<size_t>(last) & WTF::kPageMask);
     EXPECT_EQ(numSlots, static_cast<size_t>(bucket->currPage->numAllocatedSlots));
     EXPECT_EQ(0, bucket->currPage->freelistHead);
+    EXPECT_TRUE(bucket->currPage);
+    EXPECT_TRUE(bucket->currPage != &root.seedPage);
     return bucket->currPage;
 }
 
@@ -240,6 +250,35 @@ TEST(WTF_PartitionAlloc, PageTransitions)
     TestShutdown();
 }
 
+#if OS(UNIX)
+
+// Test correct handling if our mapping collides with another.
+TEST(WTF_PartitionAlloc, MappingCollision)
+{
+    TestSetup();
+    char* pageBase = root.pageBase;
+
+    WTF::PartitionPageHeader* page1 = GetFullPage();
+    // Map a single system page either side of the mapping for our allocations,
+    // with the goal of tripping up alignment of the next mapping.
+    void* map1 = mmap(pageBase - WTF::kSystemPageSize, WTF::kSystemPageSize, PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    EXPECT_TRUE(map1 && map1 != MAP_FAILED);
+    void* map2 = mmap(pageBase + WTF::kPageSize, WTF::kSystemPageSize, PROT_NONE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0);
+    EXPECT_TRUE(map2 && map2 != MAP_FAILED);
+
+    WTF::PartitionPageHeader* page2 = GetFullPage();
+    EXPECT_EQ(0, reinterpret_cast<uintptr_t>(page2) & ~WTF::kPageMask);
+    FreeFullPage(page2);
+
+    FreeFullPage(page1);
+    munmap(map1, WTF::kSystemPageSize);
+    munmap(map2, WTF::kSystemPageSize);
+
+    TestShutdown();
+}
+
+#endif // OS(UNIX)
+
 } // namespace
 
-#endif // OS(LINUX) && CPU(X86_64)
+#endif // OS(UNIX) && defined(NDEBUG)
