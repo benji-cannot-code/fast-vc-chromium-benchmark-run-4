ba3cd19f65e92e2c041b0d1915cc133627d324cb
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 04:27:53 +0000

    Call context_'s member method directly in SocketStream.
    We have some helper methods to access context_, but they're used once or
    twice. To make it clear where context_ is used, remove them for now.
    
    Review URL: https://chromiumcodereview.appspot.com/16258005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205407 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket_stream/socket_stream.cc
+++ net/socket_stream/socket_stream.cc
@@ -175,7 +175,7 @@ void SocketStream::Connect() {
   DCHECK_EQ(base::MessageLoop::TYPE_IO, base::MessageLoop::current()->type())
       << "The current base::MessageLoop must be TYPE_IO";
   if (context_.get()) {
-    ssl_config_service()->GetSSLConfig(&server_ssl_config_);
+    context_->ssl_config_service()->GetSSLConfig(&server_ssl_config_);
     proxy_ssl_config_ = server_ssl_config_;
   }
   CheckPrivacyMode();
@@ -603,6 +603,7 @@ int SocketStream::DoBeforeConnectComplete(int result) {
 }
 
 int SocketStream::DoResolveProxy() {
+  DCHECK(context_);
   DCHECK(!pac_request_);
   next_state_ = STATE_RESOLVE_PROXY_COMPLETE;
 
@@ -619,7 +620,7 @@ int SocketStream::DoResolveProxy() {
   // connection might be the first one. At that time, we should check
   // Alternate-Protocol header here for ws:// or TLS NPN extension for wss:// .
 
-  return proxy_service()->ResolveProxy(
+  return context_->proxy_service()->ResolveProxy(
       proxy_url_, &proxy_info_, io_callback_, &pac_request_, net_log_);
 }
 
@@ -1317,14 +1318,6 @@ int SocketStream::HandleCertificateError(int result) {
 
   delegate_->OnSSLCertificateError(this, ssl_info, fatal);
   return ERR_IO_PENDING;
-}
-
-SSLConfigService* SocketStream::ssl_config_service() const {
-  return context_->ssl_config_service();
-}
-
-ProxyService* SocketStream::proxy_service() const {
-  return context_->proxy_service();
 }
 
 }  // namespace net
