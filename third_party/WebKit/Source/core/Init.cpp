3bf49894c36fc6576d607049e7e2d13a08392d8d
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-11 21:59:36 +0000

    Release the Node partition at shutdown.
    We can now release resources upon graceful shutdown, subsequent to having
    landed Chromium change:
    http://src.chromium.org/viewvc/chrome?revision=204981&view=revision
    
    BUG=246860
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/16608003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152244 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/Init.cpp
+++ third_party/WebKit/Source/core/Init.cpp
@@ -68,8 +68,7 @@ void init()
 
 void shutdown()
 {
-    // We cannot call Node::shutdown() yet because some tests have an
-    // incorrect destruction ordering, e.g. PasswordAutofillAgentTest.*
+    Node::shutdown();
 }
 
 } // namespace WebCore
