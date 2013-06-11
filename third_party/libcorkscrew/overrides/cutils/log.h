4cc6105bc821302366edf4410d155028b4cf78af
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 20:32:38 +0000

    Adding libcorkscrew.
     libcorkscrew is an Android specific library to compute stack traces.
    
    Review URL: https://chromiumcodereview.appspot.com/16376004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205626 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ third_party/libcorkscrew/overrides/cutils/log.h
@@ -0,0 +1,15 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef THIRD_PARTY_LIBCORKSCREW_OVERRIDES_CUTILS_LOG_H_
+#define THIRD_PARTY_LIBCORKSCREW_OVERRIDES_CUTILS_LOG_H_
+
+#include <android/log.h>
+
+#define ALOGV(...) \
+    __android_log_print(ANDROID_LOG_VERBOSE, LOG_TAG, __VA_ARGS__)
+#define ALOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
+#endif  // THIRD_PARTY_LIBCORKSCREW_OVERRIDES_CUTILS_LOG_H_
