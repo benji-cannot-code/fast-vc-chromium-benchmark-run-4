00d07483075b964fb4b44c6120b6446292adf358
calvinlo@chromium.org <calvinlo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 13:53:00 +0000

    chrome://syncfs-internals Service status updates via events
    Now able to monitor sync service status in real time.
    
    BUG=226353
    TEST=manual
    do not sign into chrome
    go to chrome://syncfs-internals and service status should say "running"
    open up any syncfs app
    chrome://syncfs-internals sync service status should change to "authentication_required".
    sign into chrome
    chrome://syncfs-internals sync service status should change back to "running"
    
    Review URL: https://chromiumcodereview.appspot.com/14847012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205532 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/sync_file_system_internals/sync_file_system_internals_handler.h
+++ chrome/browser/ui/webui/sync_file_system_internals/sync_file_system_internals_handler.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_WEBUI_SYNC_FILE_SYSTEM_INTERNALS_SYNC_FILE_SYSTEM_INTERNALS_HANDLER_H_
 
 #include "base/compiler_specific.h"
+#include "chrome/browser/sync_file_system/sync_event_observer.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
 class Profile;
@@ -15,11 +16,26 @@ namespace syncfs_internals {
 
 // This class handles message from WebUI page of chrome://syncfs-internals/.
 // All methods in this class should be called on UI thread.
-class SyncFileSystemInternalsHandler : public content::WebUIMessageHandler {
+class SyncFileSystemInternalsHandler
+    : public content::WebUIMessageHandler,
+      public sync_file_system::SyncEventObserver {
  public:
   explicit SyncFileSystemInternalsHandler(Profile* profile);
   virtual ~SyncFileSystemInternalsHandler();
+
+  // content::WebUIMessageHandler implementation.
   virtual void RegisterMessages() OVERRIDE;
+
+  // sync_file_system::SyncEventObserver interface implementation.
+  virtual void OnSyncStateUpdated(
+      const GURL& app_origin,
+      sync_file_system::SyncServiceState state,
+      const std::string& description) OVERRIDE;
+  virtual void OnFileSynced(
+      const fileapi::FileSystemURL& url,
+      sync_file_system::SyncFileStatus status,
+      sync_file_system::SyncAction action,
+      sync_file_system::SyncDirection direction) OVERRIDE;
 
  private:
   void GetServiceStatus(const base::ListValue* args);
@@ -33,6 +49,6 @@ class SyncFileSystemInternalsHandler : public content::WebUIMessageHandler {
 
   DISALLOW_COPY_AND_ASSIGN(SyncFileSystemInternalsHandler);
 };
-}  // syncfs_internals
+}  // namespace syncfs_internals
 
 #endif  // CHROME_BROWSER_UI_WEBUI_SYNC_FILE_SYSTEM_INTERNALS_SYNC_FILE_SYSTEM_INTERNALS_HANDLER_H_
