fc710258a3ca2d6e416874ad0be0b959c8ed411f
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 22:16:34 +0000

    Cleanup InternetOptionsHandler TODOs
    Minor cleanup of a bunch of TOD statements in preparation for 
    re-factoring with NetworkStateHandler.
    
    BUG=189080
    For chrome/browser/resources/options
    TBR=jhawkins@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16455013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205652 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/chromeos/network_list.js
+++ chrome/browser/resources/options/chromeos/network_list.js
@@ -43,7 +43,6 @@ cr.define('options.network', function() {
                              'wimax',
                              'cellular',
                              'vpn',
-                             'airplaneMode',
                              'addConnection'];
 
   /**
@@ -777,14 +776,6 @@ cr.define('options.network', function() {
       // Wi-Fi control is always visible.
       this.update({key: 'wifi', networkList: []});
 
-      if (airplaneModeAvailable_()) {
-        this.update({key: 'airplaneMode',
-                     subtitle: loadTimeData.getString('airplaneModeLabel'),
-                     command: function() {
-                       chrome.send('toggleAirplaneMode');
-                     }});
-      }
-
       var entryAddWifi = {
         label: loadTimeData.getString('addConnectionWifi'),
         command: createAddConnectionCallback_(Constants.TYPE_WIFI)
@@ -957,16 +948,6 @@ cr.define('options.network', function() {
     wimaxAvailable_ = data.wimaxAvailable;
     wimaxEnabled_ = data.wimaxEnabled;
 
-    if (data.accessLocked) {
-      $('network-locked-message').hidden = false;
-      networkList.disabled = true;
-      $('use-shared-proxies').setDisabled('network-lock', true);
-    } else {
-      $('network-locked-message').hidden = true;
-      networkList.disabled = false;
-      $('use-shared-proxies').setDisabled('network-lock', false);
-    }
-
     // Only show Ethernet control if connected.
     var ethernetConnection = getConnection_(data.wiredList);
     if (ethernetConnection) {
@@ -989,8 +970,8 @@ cr.define('options.network', function() {
     else
       addEnableNetworkButton_('wifi', 'enableWifi', 'wifi');
 
-    // Only show cellular control if available and not in airplane mode.
-    if (data.cellularAvailable && !data.airplaneMode) {
+    // Only show cellular control if available.
+    if (data.cellularAvailable) {
       if (data.cellularEnabled)
         loadData_('cellular', data.wirelessList, data.rememberedList);
       else
@@ -999,8 +980,8 @@ cr.define('options.network', function() {
       networkList.deleteItem('cellular');
     }
 
-    // Only show cellular control if available and not in airplane mode.
-    if (data.wimaxAvailable && !data.airplaneMode) {
+    // Only show cellular control if available.
+    if (data.wimaxAvailable) {
       if (data.wimaxEnabled)
         loadData_('wimax', data.wirelessList, data.rememberedList);
       else
@@ -1016,7 +997,6 @@ cr.define('options.network', function() {
       loadData_('vpn', data.vpnList, data.rememberedList);
     else
       networkList.deleteItem('vpn');
-    networkList.updateToggleControl('airplaneMode', data.airplaneMode);
     networkList.endBatchUpdates();
   };
 
@@ -1161,17 +1141,6 @@ cr.define('options.network', function() {
         return entry;
     }
     return null;
-  }
-
-  /**
-   * Queries if airplane mode is available.
-   * @return {boolean} Indicates if airplane mode is available.
-   * @private
-   */
-  function airplaneModeAvailable_() {
-     // TODO(kevers): Use library callback to determine if airplane mode is
-     // available once back-end suport is in place.
-     return false;
   }
 
   /**
