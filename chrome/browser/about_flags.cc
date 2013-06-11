3ecdba8aefd3880b9668edac9d498010a745f996
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 12:09:17 +0000

    Add a command line flag for translate-settings in chrome://settings/languages
    As the release date was postponed to M30, a flag to hide this feature is needed for M29.
    
    BUG=243235
    TEST=manual
    
    Review URL: https://chromiumcodereview.appspot.com/16160018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/about_flags.cc
+++ chrome/browser/about_flags.cc
@@ -1541,6 +1541,13 @@ const Experiment kExperiments[] = {
     SINGLE_VALUE_TYPE(switches::kEnableQuickofficeEdit),
   },
 #endif
+  {
+    "enable-translate-settings",
+    IDS_FLAGS_ENABLE_TRANSLATE_SETTINGS_NAME,
+    IDS_FLAGS_ENABLE_TRANSLATE_SETTINGS_DESCRIPTION,
+    kOsAll,
+    SINGLE_VALUE_TYPE(switches::kEnableTranslateSettings)
+  },
 };
 
 const Experiment* experiments = kExperiments;
