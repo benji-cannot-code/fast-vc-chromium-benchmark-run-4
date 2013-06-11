0982f25d6a2633910fceef6c10535c45e2efd928
andrewhayden@chromium.org <andrewhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 16:01:17 +0000

    Add more tracing to IOThread::Init()
    BUG=222350
    
    Review URL: https://chromiumcodereview.appspot.com/15769006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205555 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/io_thread.cc
+++ chrome/browser/io_thread.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/compiler_specific.h"
 #include "base/debug/leak_tracker.h"
+#include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/metrics/field_trial.h"
 #include "base/prefs/pref_registry_simple.h"
@@ -126,6 +127,7 @@ class SystemURLRequestContext : public net::URLRequestContext {
 };
 
 scoped_ptr<net::HostResolver> CreateGlobalHostResolver(net::NetLog* net_log) {
+  TRACE_EVENT0("startup", "IOThread::CreateGlobalHostResolver");
   const CommandLine& command_line = *CommandLine::ForCurrentProcess();
 
   net::HostResolver::Options options;
@@ -463,6 +465,7 @@ net::URLRequestContextGetter* IOThread::system_url_request_context_getter() {
 }
 
 void IOThread::Init() {
+  TRACE_EVENT0("startup", "IOThread::Init");
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
 
 #if defined(USE_NSS) || defined(OS_IOS)
@@ -523,8 +526,10 @@ void IOThread::Init() {
   globals_->http_user_agent_settings.reset(
       new BasicHttpUserAgentSettings(std::string()));
   if (command_line.HasSwitch(switches::kHostRules)) {
+    TRACE_EVENT_BEGIN0("startup", "IOThread::Init:SetRulesFromString");
     globals_->host_mapping_rules->SetRulesFromString(
         command_line.GetSwitchValueASCII(switches::kHostRules));
+    TRACE_EVENT_END0("startup", "IOThread::Init:SetRulesFromString");
   }
   if (command_line.HasSwitch(switches::kIgnoreCertificateErrors))
     globals_->ignore_certificate_errors = true;
@@ -557,10 +562,12 @@ void IOThread::Init() {
   session_params.proxy_service =
       globals_->proxy_script_fetcher_proxy_service.get();
 
+  TRACE_EVENT_BEGIN0("startup", "IOThread::Init:HttpNetworkSession");
   scoped_refptr<net::HttpNetworkSession> network_session(
       new net::HttpNetworkSession(session_params));
   globals_->proxy_script_fetcher_http_transaction_factory
       .reset(new net::HttpNetworkLayer(network_session.get()));
+  TRACE_EVENT_END0("startup", "IOThread::Init:HttpNetworkSession");
   scoped_ptr<net::URLRequestJobFactoryImpl> job_factory(
       new net::URLRequestJobFactoryImpl());
   job_factory->SetProtocolHandler(chrome::kDataScheme,
