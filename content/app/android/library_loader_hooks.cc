e6eaf531367775ad48535c726fbe1e85fad89065
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 04:29:00 +0000

    Use a direct include of strings headers in content/app/.
    BUG=247723
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16716003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205409 0039d316-1c4b-4281-b951-d872f2087c98

--- content/app/android/library_loader_hooks.cc
+++ content/app/android/library_loader_hooks.cc
@@ -15,7 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/trace_event.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
-#include "base/string_util.h"
+#include "base/strings/string_util.h"
 #include "base/tracked_objects.h"
 #include "content/app/android/app_jni_registrar.h"
 #include "content/browser/android/browser_jni_registrar.h"
