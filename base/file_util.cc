aca554d53978b669a4331b82c81f882dc174bcfc
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 03:50:25 +0000

    Make CreateDirectory return an error code instead of just a bool.
    Make use of the new error code in IndexedDB, where we'll
    histogram which errors can be recovered from by retrying
    and which can't.
    
    We're going to try retrying CreateDirectory because LevelDB
    doesn't pay attention to what env_->CreateDir returns and a
    frequent error on the next operation (locking the lock file)
    is that the directory doesn't exist. I want to find out
    what's causing the directories to not be created in the
    first place, and which errors can be considered ephemeral.
    
    BUG=225051
    
    Review URL: https://chromiumcodereview.appspot.com/15812007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205386 0039d316-1c4b-4281-b951-d872f2087c98

--- base/file_util.cc
+++ base/file_util.cc
@@ -186,6 +186,10 @@ FILE* CreateAndOpenTemporaryFile(FilePath* path) {
   return CreateAndOpenTemporaryFileInDir(directory, path);
 }
 
+bool CreateDirectory(const base::FilePath& full_path) {
+  return CreateDirectoryAndGetError(full_path, NULL);
+}
+
 bool GetFileSize(const FilePath& file_path, int64* file_size) {
   base::PlatformFileInfo info;
   if (!GetFileInfo(file_path, &info))
