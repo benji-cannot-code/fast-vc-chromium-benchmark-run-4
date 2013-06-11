d2b12d07d66e216aee4965dba176751050800f6c
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 06:21:45 +0000

    Use a direct include of strings headers in chrome/third_party/.
    BUG=247723
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/15703011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205430 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/third_party/mozilla_security_manager/nsNSSCertHelper.cpp
+++ chrome/third_party/mozilla_security_manager/nsNSSCertHelper.cpp
@@ -47,8 +47,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <unicode/uidna.h>
 
 #include "base/i18n/number_formatting.h"
-#include "base/stringprintf.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/common/net/x509_certificate_model.h"
 #include "crypto/scoped_nss_types.h"
