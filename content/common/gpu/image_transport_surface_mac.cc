1a8033b152e36182169df2e0723f771b838cff9b
sievers@chromium.org <sievers@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 10:25:13 +0000

    Virtual context MakeCurrent tweaks.
    Do not allow GLContextVirtual::MakeCurrent() without a decoder (state
    restorer), since this only leads to state bugs.
    
    For this reason remove the necessity of having a current context during surface destruction (it's ugly anyway, since GLSurface is RefCounted).
    
    BUG=248395
    
    Review URL: https://chromiumcodereview.appspot.com/15925007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205481 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/image_transport_surface_mac.cc
+++ content/common/gpu/image_transport_surface_mac.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/mac/scoped_cftyperef.h"
 #include "base/memory/scoped_ptr.h"
+#include "content/common/gpu/gpu_command_buffer_stub.h"
 #include "content/common/gpu/gpu_messages.h"
 #include "ui/gfx/native_widget_types.h"
 #include "ui/gl/gl_bindings.h"
@@ -34,8 +35,10 @@ int RoundUpSurfaceDimension(int number) {
 
 // We are backed by an offscreen surface for the purposes of creating
 // a context, but use FBOs to render to texture backed IOSurface
-class IOSurfaceImageTransportSurface : public gfx::NoOpGLSurfaceCGL,
-                                       public ImageTransportSurface {
+class IOSurfaceImageTransportSurface
+    : public gfx::NoOpGLSurfaceCGL,
+      public ImageTransportSurface,
+      public GpuCommandBufferStub::DestructionObserver {
  public:
   IOSurfaceImageTransportSurface(GpuChannelManager* manager,
                                  GpuCommandBufferStub* stub,
@@ -62,6 +65,9 @@ class IOSurfaceImageTransportSurface : public gfx::NoOpGLSurfaceCGL,
   virtual void OnResizeViewACK() OVERRIDE;
   virtual void OnResize(gfx::Size size, float scale_factor) OVERRIDE;
   virtual void SetLatencyInfo(const ui::LatencyInfo&) OVERRIDE;
+
+  // GpuCommandBufferStub::DestructionObserver implementation.
+  virtual void OnWillDestroyStub() OVERRIDE;
 
  private:
   virtual ~IOSurfaceImageTransportSurface() OVERRIDE;
@@ -139,7 +145,6 @@ IOSurfaceImageTransportSurface::IOSurfaceImageTransportSurface(
 }
 
 IOSurfaceImageTransportSurface::~IOSurfaceImageTransportSurface() {
-  Destroy();
 }
 
 bool IOSurfaceImageTransportSurface::Initialize() {
@@ -152,7 +157,14 @@ bool IOSurfaceImageTransportSurface::Initialize() {
 
   if (!helper_->Initialize())
     return false;
-  return NoOpGLSurfaceCGL::Initialize();
+
+  if (!NoOpGLSurfaceCGL::Initialize()) {
+    helper_->Destroy();
+    return false;
+  }
+
+  helper_->stub()->AddDestructionObserver(this);
+  return true;
 }
 
 void IOSurfaceImageTransportSurface::Destroy() {
@@ -312,6 +324,11 @@ void IOSurfaceImageTransportSurface::OnResize(gfx::Size size,
 void IOSurfaceImageTransportSurface::SetLatencyInfo(
     const ui::LatencyInfo& latency_info) {
   latency_info_ = latency_info;
+}
+
+void IOSurfaceImageTransportSurface::OnWillDestroyStub() {
+  helper_->stub()->RemoveDestructionObserver(this);
+  Destroy();
 }
 
 void IOSurfaceImageTransportSurface::UnrefIOSurface() {
