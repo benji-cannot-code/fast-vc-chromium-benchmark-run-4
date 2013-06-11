e97ade8bfbbd72164172a7a0e57cfafe6bbce9a9
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-11 04:07:20 +0000

    cc: Don't add low res tilings during animation/pinch
    R=danakj@chromium.org
    BUG=247465
    
    Review URL: https://chromiumcodereview.appspot.com/16674004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205399 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/test/fake_layer_tree_host_impl.cc
+++ cc/test/fake_layer_tree_host_impl.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "cc/test/fake_layer_tree_host_impl.h"
+#include "cc/trees/layer_tree_impl.h"
 
 namespace cc {
 
@@ -28,5 +29,12 @@ FakeLayerTreeHostImpl::FakeLayerTreeHostImpl(
 }
 
 FakeLayerTreeHostImpl::~FakeLayerTreeHostImpl() {}
+
+void FakeLayerTreeHostImpl::CreatePendingTree() {
+  LayerTreeHostImpl::CreatePendingTree();
+  float arbitrary_large_page_scale = 100000.f;
+  pending_tree()->SetPageScaleFactorAndLimits(
+      1.f, 1.f / arbitrary_large_page_scale, arbitrary_large_page_scale);
+}
 
 }  // namespace cc
--- cc/test/fake_layer_tree_host_impl.h
+++ cc/test/fake_layer_tree_host_impl.h
@@ -25,6 +25,8 @@ class FakeLayerTreeHostImpl : public LayerTreeHostImpl {
     DidDrawAllLayers(frame_data);
   }
 
+  virtual void CreatePendingTree() OVERRIDE;
+
   using LayerTreeHostImpl::ActivatePendingTree;
 
  private:
