b628436cf52dd3896875200ef4e0e1d0ff834c1d
derat@chromium.org <derat@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 19:56:28 +0000

    chromeos: Replace scale-delays-for-presentation-mode pref.
    This replaces the kPowerPresentationIdleDelayFactor pref
    with kPowerPresentationScreenDimDelayFactor. The former
    scaled the idle delay and then adjusted the other delays;
    the latter scales the screen-dim delay before adjusting
    other delays.
    
    It also updates the cros_system_api dependency to d2d441c1.
    
    BUG=chromium:248276
    
    Review URL: https://chromiumcodereview.appspot.com/16677007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/pref_names.h
+++ chrome/common/pref_names.h
@@ -314,6 +314,7 @@ extern const char kPowerUseAudioActivity[];
 extern const char kPowerUseVideoActivity[];
 extern const char kPowerAllowScreenWakeLocks[];
 extern const char kPowerPresentationIdleDelayFactor[];
+extern const char kPowerPresentationScreenDimDelayFactor[];
 extern const char kPowerUserActivityScreenDimDelayFactor[];
 extern const char kTermsOfServiceURL[];
 extern const char kUsedPolicyCertificatesOnce[];
