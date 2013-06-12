0aa767b9e95b34ca3a3551e7ed0c9e7b71a95617
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-12 18:33:05 +0000

    Use "Constructor" prefix for IDL extended attributes applying to the constructor
    [CallWith] and [RaisesException] IDL extended attributes are now renamed to
    [ConstructorCallWith] and [ConstructorRaisesException] respectively for IDL
    interfaces. This clarifies that those only apply to the constructor (and not all
    methods of the interface). This is also consistent with WebKit naming.
    
    Review URL: https://chromiumcodereview.appspot.com/15821017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152290 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -2508,7 +2508,7 @@ sub GenerateSingleConstructorCallback
     }
 
     my $raisesExceptions = $function->signature->extendedAttributes->{"RaisesException"};
-    if ($interface->extendedAttributes->{"RaisesException"}) {
+    if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         $raisesExceptions = 1;
     }
     if (!$raisesExceptions) {
@@ -2540,7 +2540,7 @@ END
     my ($parameterCheckString, $paramIndex, %replacements) = GenerateParametersCheck($function, $interface, "");
     $code .= $parameterCheckString;
 
-    if ($interface->extendedAttributes->{"CallWith"} && $interface->extendedAttributes->{"CallWith"} eq "ScriptExecutionContext") {
+    if ($interface->extendedAttributes->{"ConstructorCallWith"} && $interface->extendedAttributes->{"ConstructorCallWith"} eq "ScriptExecutionContext") {
         push(@beforeArgumentList, "context");
         $code .= <<END;
 
@@ -2548,7 +2548,7 @@ END
 END
     }
 
-    if ($interface->extendedAttributes->{"RaisesException"}) {
+    if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         push(@afterArgumentList, "ec");
     }
 
@@ -2569,7 +2569,7 @@ END
     $code .= "    RefPtr<${implClassName}> impl = ${implClassName}::create(${argumentString});\n";
     $code .= "    v8::Handle<v8::Object> wrapper = args.Holder();\n";
 
-    if ($interface->extendedAttributes->{"RaisesException"}) {
+    if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         $code .= "    if (ec) {\n";
         $code .= "        setDOMException(ec, args.GetIsolate());\n";
         $code .= "        return;\n";
@@ -2732,7 +2732,7 @@ sub GenerateNamedConstructor
     my $implClassName = GetImplName($interface);
     my $v8ClassName = GetV8ClassName($interface);
     my $raisesExceptions = $function->signature->extendedAttributes->{"RaisesException"};
-    if ($interface->extendedAttributes->{"RaisesException"}) {
+    if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         $raisesExceptions = 1;
     }
     if (!$raisesExceptions) {
@@ -2795,7 +2795,7 @@ END
 
     push(@beforeArgumentList, "document");
 
-    if ($interface->extendedAttributes->{"RaisesException"}) {
+    if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         push(@afterArgumentList, "ec");
     }
 
@@ -2816,7 +2816,7 @@ END
     $code .= "    RefPtr<${implClassName}> impl = ${implClassName}::createForJSConstructor(${argumentString});\n";
     $code .= "    v8::Handle<v8::Object> wrapper = args.Holder();\n";
 
-    if ($interface->extendedAttributes->{"RaisesException"}) {
+    if ($interface->extendedAttributes->{"ConstructorRaisesException"}) {
         $code .= "    if (ec) {\n";
         $code .= "        setDOMException(ec, args.GetIsolate());\n";
         $code .= "        return;\n";
