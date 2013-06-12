eea574c549d6f70f7a0f51ab98345ec6f6307764
weitaosu@chromium.org <weitaosu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 06:33:19 +0000

    Enable Resize_desktop_to_fit by default for Me2Me connections.
    BUG=239008
    
    Review URL: https://chromiumcodereview.appspot.com/16796003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205734 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/client_session.js
+++ remoting/webapp/client_session.js
@@ -81,7 +81,7 @@ remoting.ClientSession = function(hostJid, clientJid, hostPublicKey, accessCode,
   /** @private */
   this.shrinkToFit_ = true;
   /** @private */
-  this.resizeToClient_ = false;
+  this.resizeToClient_ = true;
   /** @private */
   this.remapKeys_ = '';
   /** @private */
