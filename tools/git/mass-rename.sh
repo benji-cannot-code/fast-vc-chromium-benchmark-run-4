a37a2d21ead44127c3b2b5d65817fecdf6f0afa1
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 06:32:25 +0000

    mass-rename.py for windows and fix mffr.py on windows
    R=jamesr@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16787006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205731 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/git/mass-rename.sh
+++ tools/git/mass-rename.sh
@@ -15,15 +15,4 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #   for f in $(git diff --name-only origin); do ./tools/sort-headers.py $f; done
 
 DIR="$( cd "$( dirname "$0" )" && pwd )"
-
-# Make the 'read' used in the while loop split only on tabs/newlines.
-IFS=$'\t\n'
-
-git diff --cached --raw -M | while read attrs from to; do
-    type=$(echo "$attrs" | cut -d' ' -f5)
-    if echo "$type" | grep -q "^R"; then
-        python $DIR/move_source_file.py --already-moved "$from" "$to"
-    else
-        echo "Skipping: $from -- not a rename?"
-    fi
-done
+python $DIR/mass-rename.py "$*"
