060aedc0caad94290652bdc6b4ab931242db5c5d
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 13:10:46 +0000

    SyncFS: Replace MockDriveService with FakeDriveService in APIUtilTest
    BUG=243616
    TEST=unit_tests --gtest_filter=\*APIUtilTest\*
    
    Review URL: https://chromiumcodereview.appspot.com/15704006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205742 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive/api_util.cc
+++ chrome/browser/sync_file_system/drive/api_util.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/drive_api_service.h"
 #include "chrome/browser/google_apis/drive_uploader.h"
 #include "chrome/browser/google_apis/gdata_wapi_service.h"
-#include "chrome/browser/google_apis/gdata_wapi_url_generator.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync_file_system/drive_file_sync_util.h"
 #include "chrome/browser/sync_file_system/logger.h"
@@ -44,6 +43,8 @@ const char kMimeTypeOctetStream[] = "application/octet-stream";
 // This path is not actually used but is required by DriveUploaderInterface.
 const base::FilePath::CharType kDummyDrivePath[] =
     FILE_PATH_LITERAL("/dummy/drive/path");
+
+const char kFakeServerBaseUrl[] = "https://fake_server/";
 
 void EmptyGDataErrorCodeCallback(google_apis::GDataErrorCode error) {}
 
@@ -159,11 +160,13 @@ APIUtil::APIUtil(Profile* profile)
 
 scoped_ptr<APIUtil> APIUtil::CreateForTesting(
     Profile* profile,
-    const GURL& base_url,
     scoped_ptr<google_apis::DriveServiceInterface> drive_service,
     scoped_ptr<google_apis::DriveUploaderInterface> drive_uploader) {
   return make_scoped_ptr(new APIUtil(
-      profile, base_url, drive_service.Pass(), drive_uploader.Pass()));
+      profile,
+      GURL(kFakeServerBaseUrl),
+      drive_service.Pass(),
+      drive_uploader.Pass()));
 }
 
 APIUtil::APIUtil(Profile* profile,
