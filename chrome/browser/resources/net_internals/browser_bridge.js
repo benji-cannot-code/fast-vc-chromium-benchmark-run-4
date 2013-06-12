179ee7469caf01fb4b2ee9f17ab71fd8856d5414
shinfan@chromium.org <shinfan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 13:20:57 +0000

    Add callback helper function for getSystemCall()
    Add a pollableDataHelpers function so that a callback function could be binded
    for getSystemCall().
    
    BUG=246674
    
    Review URL: https://chromiumcodereview.appspot.com/15813015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/net_internals/browser_bridge.js
+++ chrome/browser/resources/net_internals/browser_bridge.js
@@ -82,6 +82,11 @@ var BrowserBridge = (function() {
     this.pollableDataHelpers_.extensionInfo =
         new PollableDataHelper('onExtensionInfoChanged',
                                this.sendGetExtensionInfo.bind(this));
+    if (cr.isChromeOS) {
+      this.pollableDataHelpers_.systemLog =
+          new PollableDataHelper('onSystemLogChanged',
+                               this.getSystemLog.bind(this, 'syslog'));
+    }
 
     // Setting this to true will cause messages from the browser to be ignored,
     // and no messages will be sent to the browser, either.  Intended for use
@@ -425,6 +430,10 @@ var BrowserBridge = (function() {
       this.pollableDataHelpers_.extensionInfo.update(extensionInfo);
     },
 
+    getSystemLogCallback: function(systemLog) {
+      this.pollableDataHelpers_.systemLog.update(systemLog);
+    },
+
     //--------------------------------------------------------------------------
 
     /**
@@ -693,6 +702,17 @@ var BrowserBridge = (function() {
      */
     addExtensionInfoObserver: function(observer, ignoreWhenUnchanged) {
       this.pollableDataHelpers_.extensionInfo.addObserver(
+          observer, ignoreWhenUnchanged);
+    },
+
+    /**
+     * Adds a listener of system log information. |observer| will be called
+     * back when data is received, through:
+     *
+     *   observer.onSystemLogChanged(systemLogInfo)
+     */
+    addSystemLogObserver: function(observer, ignoreWhenUnchanged) {
+      this.pollableDataHelpers_.systemLog.addObserver(
           observer, ignoreWhenUnchanged);
     },
 
