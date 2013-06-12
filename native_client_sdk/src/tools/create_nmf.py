006e171d306863474b21db2572b0217f3293cd34
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 00:20:15 +0000

    [NaCl SDK] Add tool for creating html scaffolding.
    This tool can create and basic html page for a nexe
    as well as calling create_nmf to create the nmf.
    
    It can be given a .nexe or a .nmf file.
    
    BUG=243407
    R=binji@chromium.org
    
    Review URL: https://codereview.chromium.org/16599018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205682 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/tools/create_nmf.py
+++ native_client_sdk/src/tools/create_nmf.py
@@ -111,7 +111,10 @@ def ParseElfHeader(path):
   with open(path, 'rb') as f:
     header = f.read(elf_header_size)
 
-  header = struct.unpack(elf_header_format, header)
+  try:
+    header = struct.unpack(elf_header_format, header)
+  except struct.error:
+    raise Error("error parsing elf header: %s" % path)
   e_ident, _, e_machine = header[:3]
 
   elf_magic = '\x7fELF'
@@ -726,7 +729,6 @@ def main(argv):
   return 0
 
 
-# Invoke this file directly for simple testing.
 if __name__ == '__main__':
   try:
     rtn = main(sys.argv[1:])
