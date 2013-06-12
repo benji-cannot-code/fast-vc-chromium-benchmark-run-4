d2c2a6d0ebd7975e54aa5c04deded9b1352e6f51
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 21:12:45 +0000

    Make sure overlay goes away also when non-extension files are dropped into chrome://extensions.
    BUG=242414
    R=kalman@chromium.org
    
    Review URL: https://codereview.chromium.org/16300010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205931 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extensions.js
+++ chrome/browser/resources/extensions/extensions.js
@@ -47,6 +47,8 @@ cr.define('extensions', function() {
     },
     // @inheritdoc
     doDrop: function(e) {
+      ExtensionSettings.showOverlay(null);
+
       // Only process files that look like extensions. Other files should
       // navigate the browser normally.
       if (!e.dataTransfer.files.length ||
@@ -55,7 +57,6 @@ cr.define('extensions', function() {
       }
 
       chrome.send('installDroppedFile');
-      ExtensionSettings.showOverlay(null);
       e.preventDefault();
     }
   };
