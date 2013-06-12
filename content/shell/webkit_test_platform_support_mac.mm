7c4dfa3a53039f9118354a42cc32927768ca2f08
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 19:53:03 +0000

    [content shell] disable scroll animations for layout tests
    BUG=243360,243366
    R=marja@chromium.org
    TEST=editing/input/reveal-edit-on-input-vertically.html passes
    
    Review URL: https://chromiumcodereview.appspot.com/16520008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205903 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_platform_support_mac.mm
+++ content/shell/webkit_test_platform_support_mac.mm
@@ -37,6 +37,8 @@ void SetDefaultsToLayoutTestValues(void) {
                forKey:@"AppleOtherHighlightColor"];
   [defaults setObject:[NSArray arrayWithObject:@"en"]
                forKey:@"AppleLanguages"];
+  [defaults setBool:NO
+             forKey:@"AppleScrollAnimationEnabled"];
 }
 
 }  // namespace
