57e797c961d644fe2dacab5679f3c68daa663e44
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 06:32:40 +0000

    Use URLFetcherFactory for 3-arg overload of URLFetcher::Create
    The fact that only one of the overloads was affected by instantiating a
    TestURLFetcherFactory/FakeURLFetcherFactory caused me to spend half an
    hour wondering why another one of my changes didn't work.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/16792005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205732 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_fetcher.cc
+++ net/url_request/url_fetcher.cc
@@ -17,7 +17,7 @@ URLFetcher* net::URLFetcher::Create(
     const GURL& url,
     URLFetcher::RequestType request_type,
     URLFetcherDelegate* d) {
-  return new URLFetcherImpl(url, request_type, d);
+  return URLFetcher::Create(0, url, request_type, d);
 }
 
 // static
@@ -27,8 +27,8 @@ URLFetcher* net::URLFetcher::Create(
     URLFetcher::RequestType request_type,
     URLFetcherDelegate* d) {
   URLFetcherFactory* factory = URLFetcherImpl::factory();
-  return factory ? factory->CreateURLFetcher(id, url, request_type, d) :
-                   new URLFetcherImpl(url, request_type, d);
+  return factory ? factory->CreateURLFetcher(id, url, request_type, d)
+                 : new URLFetcherImpl(url, request_type, d);
 }
 
 // static
