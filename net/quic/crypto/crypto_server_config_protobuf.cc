ada5ccc314c44ddfc058f16232e44420649b98f5
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 21:52:51 +0000

    QUIC: support for rotating QUIC server configs.
    In the limit, we would want to rotate QUIC server configs in the same fashion
    as we rotate TLS SessionTicket keys because the initial client application data
    isn't forward secure.
    
    Merge internal change: 47706026
    
    R=rch@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16203007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205946 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/crypto/crypto_server_config_protobuf.cc
+++ net/quic/crypto/crypto_server_config_protobuf.cc
@@ -6,10 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/quic/crypto/crypto_server_config_protobuf.h"
 
 #include "base/stl_util.h"
+#include "net/quic/quic_time.h"
 
 namespace net {
 
-QuicServerConfigProtobuf::QuicServerConfigProtobuf() {
+QuicServerConfigProtobuf::QuicServerConfigProtobuf()
+    : primary_time_(QuicWallTime::Zero().ToUNIXSeconds()) {
 }
 
 QuicServerConfigProtobuf::~QuicServerConfigProtobuf() {
