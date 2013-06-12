e4edeaa5ff8a7b3f4a586fc44338031b645ec7b8
sehr@chromium.org <sehr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 17:40:51 +0000

    Split pnacl and nacl mime types
    Manifest parsing now knows which mime type was used, and does not allow
    pnacl to be used from nacl manifests or vice-versa.
    
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3440
    R=bradnelson@chromium.org, jam@chromium.org, jvoung@chromium.org
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=205329
    
    Review URL: https://codereview.chromium.org/16296005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205855 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl/nacl_browsertest_util.cc
+++ chrome/test/nacl/nacl_browsertest_util.cc
@@ -178,6 +178,15 @@ static bool GetNaClVariantRoot(const base::FilePath::StringType& variant,
   return true;
 }
 
+static void AddPnaclParm(const base::FilePath::StringType& url,
+                         base::FilePath::StringType* url_with_parm) {
+  if (url.find(FILE_PATH_LITERAL("?")) == base::FilePath::StringType::npos) {
+    *url_with_parm = url + FILE_PATH_LITERAL("?pnacl=1");
+  } else {
+    *url_with_parm = url + FILE_PATH_LITERAL("&pnacl=1");
+  }
+}
+
 NaClBrowserTestBase::NaClBrowserTestBase() {
 }
 
@@ -225,7 +234,11 @@ bool NaClBrowserTestBase::RunJavascriptTest(const GURL& url,
 void NaClBrowserTestBase::RunLoadTest(
     const base::FilePath::StringType& test_file) {
   LoadTestMessageHandler handler;
-  bool ok = RunJavascriptTest(TestURL(test_file), &handler);
+  base::FilePath::StringType test_file_with_parm = test_file;
+  if (IsPnacl()) {
+    AddPnaclParm(test_file, &test_file_with_parm);
+  }
+  bool ok = RunJavascriptTest(TestURL(test_file_with_parm), &handler);
   ASSERT_TRUE(ok) << handler.error_message();
   ASSERT_TRUE(handler.test_passed()) << "Test failed.";
 }
@@ -233,7 +246,11 @@ void NaClBrowserTestBase::RunLoadTest(
 void NaClBrowserTestBase::RunNaClIntegrationTest(
     const base::FilePath::StringType& url_fragment) {
   NaClIntegrationMessageHandler handler;
-  bool ok = RunJavascriptTest(TestURL(url_fragment), &handler);
+  base::FilePath::StringType url_fragment_with_parm = url_fragment;
+  if (IsPnacl()) {
+    AddPnaclParm(url_fragment, &url_fragment_with_parm);
+  }
+  bool ok = RunJavascriptTest(TestURL(url_fragment_with_parm), &handler);
   ASSERT_TRUE(ok) << handler.error_message();
   ASSERT_TRUE(handler.test_passed()) << "Test failed.";
 }
