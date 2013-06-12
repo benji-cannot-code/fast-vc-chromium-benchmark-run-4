08266d04a21489bf0cce3ce597e7114f41001cc3
backer@chromium.org <backer@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 21:16:55 +0000

    CC: Call SwapBuffers instead of PostSubBuffer on full frame update.
    This allows us to remove a driver workaround and send less information
    in the GLFrameData.
    
    TEST=cc_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/16509008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205932 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/output/gl_frame_data.cc
+++ cc/output/gl_frame_data.cc
@@ -7,10 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace cc {
 
-GLFrameData::GLFrameData()
-    : sync_point(0),
-      partial_swap_allowed(false) {
-}
+GLFrameData::GLFrameData() : sync_point(0) {}
 
 GLFrameData::~GLFrameData() {}
 
