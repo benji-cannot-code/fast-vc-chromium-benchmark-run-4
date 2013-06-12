e271c8448b974d741062b8ef476b910540d9f3c5
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 16:33:56 +0000

    Pepper: Add append support to FileIO.
    This change adds a PP_FILEOPENFLAG_APPEND flag to PP_FileOpenFlags which will
    force all writes to be appended to the end of the file instead of the current
    file position. This flag is mutually exclusive with PP_FILEOPENFLAG_WRITE.
    
    BUG=242383
    
    Review URL: https://chromiumcodereview.appspot.com/16765005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205817 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_file_io.cc
+++ ppapi/tests/test_file_io.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
 #include <sys/types.h>
@@ -414,6 +415,22 @@ std::string TestFileIO::TestReadWriteSetLength() {
                       callback_type());
   ASSERT_EQ(PP_OK, rv);
   ASSERT_EQ(std::string("testtest\0\0\0\0", 12), read_buffer);
+
+  // Append to the end of the file.
+  pp::FileIO file_io2(instance_);
+  callback.WaitForResult(file_io2.Open(file_ref,
+                                       PP_FILEOPENFLAG_CREATE |
+                                       PP_FILEOPENFLAG_READ |
+                                       PP_FILEOPENFLAG_APPEND,
+                                       callback.GetCallback()));
+  rv = WriteEntireBuffer(instance_->pp_instance(), &file_io2, 0, "appended",
+                         callback_type());
+  ASSERT_EQ(PP_OK, rv);
+  read_buffer.clear();
+  rv = ReadEntireFile(instance_->pp_instance(), &file_io2, 0, &read_buffer,
+                      callback_type());
+  ASSERT_EQ(PP_OK, rv);
+  ASSERT_EQ(std::string("testtesttest\0\0\0\0appended", 24), read_buffer);
 
   PASS();
 }
