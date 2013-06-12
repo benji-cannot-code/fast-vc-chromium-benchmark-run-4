1c27de6d35b342808199595a994f26ba86cadcc1
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 17:10:46 +0000

    Add notification when the host successfully starts.
    This adds a listener for when first heartbeat is successful, which I think is the best signal that everything is working and the host is able to receive connections.
    
    On Linux, that can send a signal if requested by the caller. On the script side, when running as a daemon, we create a pipe that allows the parent process to wait for that signal (or a permanent failure), and dump all Python log input to the STDERR. This gives users a much better feedback about whether starting the host worked or not.
    
    BUG=179419
    
    Review URL: https://chromiumcodereview.appspot.com/16448005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205837 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -72,6 +72,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_POSIX)
 #include <signal.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include "base/file_descriptor_posix.h"
 #include "remoting/host/pam_authorization_factory_posix.h"
 #include "remoting/host/posix/signal_handler.h"
@@ -104,6 +106,10 @@ const char kApplicationName[] = "chromoting";
 // linux.
 const char kAudioPipeSwitchName[] = "audio-pipe-name";
 
+// The command line switch used by the parent to request the host to signal it
+// when it is successfully started.
+const char kSignalParentSwitchName[] = "signal-parent";
+
 void QuitMessageLoop(base::MessageLoop* message_loop) {
   message_loop->PostTask(FROM_HERE, base::MessageLoop::QuitClosure());
 }
@@ -131,6 +137,7 @@ class HostProcess
   virtual void OnChannelError() OVERRIDE;
 
   // HeartbeatSender::Listener overrides.
+  virtual void OnHeartbeatSuccessful() OVERRIDE;
   virtual void OnUnknownHostIdError() OVERRIDE;
 
   // HostChangeNotificationListener::Listener overrides.
@@ -279,6 +286,7 @@ class HostProcess
 #endif  // defined(REMOTING_MULTI_PROCESS)
 
   int* exit_code_out_;
+  bool signal_parent_;
 };
 
 HostProcess::HostProcess(scoped_ptr<ChromotingHostContext> context,
@@ -291,7 +299,8 @@ HostProcess::HostProcess(scoped_ptr<ChromotingHostContext> context,
       desktop_session_connector_(NULL),
 #endif  // defined(REMOTING_MULTI_PROCESS)
       self_(this),
-      exit_code_out_(exit_code_out) {
+      exit_code_out_(exit_code_out),
+      signal_parent_(false) {
   StartOnUiThread();
 }
 
@@ -372,6 +381,9 @@ bool HostProcess::InitWithCommandLine(const CommandLine* cmd_line) {
   }
   xmpp_server_config_.use_tls = service_urls->xmpp_server_use_tls();
   directory_bot_jid_ = service_urls->directory_bot_jid();
+
+  signal_parent_ = cmd_line->HasSwitch(kSignalParentSwitchName);
+
   return true;
 }
 
@@ -621,6 +633,16 @@ void HostProcess::ShutdownOnUiThread() {
 void HostProcess::OnUnknownHostIdError() {
   LOG(ERROR) << "Host ID not found.";
   ShutdownHost(kInvalidHostIdExitCode);
+}
+
+void HostProcess::OnHeartbeatSuccessful() {
+  LOG(INFO) << "Host ready to receive connections.";
+#if defined(OS_POSIX)
+  if (signal_parent_) {
+    kill(getppid(), SIGUSR1);
+    signal_parent_ = false;
+  }
+#endif
 }
 
 void HostProcess::OnHostDeleted() {
