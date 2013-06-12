5d770108742e76d387ab813a4498bb38fe895f1f
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-12 21:41:43 +0000

    Change ByteOrder.h to include CPU.h
    ByteOrder.h depends on CPU.h in addition to Platform.h, this is required to compile the file on windows.
    
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/16841002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152305 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ByteOrder.h
+++ third_party/WebKit/Source/wtf/ByteOrder.h
@@ -32,7 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_ByteOrder_h
 #define WTF_ByteOrder_h
 
-#include <wtf/Platform.h>
+#include "wtf/CPU.h"
+#include "wtf/Platform.h"
 
 #if OS(UNIX)
 #include <arpa/inet.h>
