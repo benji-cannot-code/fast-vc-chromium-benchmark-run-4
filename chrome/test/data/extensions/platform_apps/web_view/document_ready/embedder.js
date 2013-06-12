bb8bb1f9e8ec00b4ebf08fb0a834389a80d3373c
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-12 16:00:26 +0000

    Copy properties from <webview> shim to <object> too in the beginning (not just attributes). Right now we copy the value from <webview> to <object> via hasAttribute/getAttribute, which does not work when document.DOMContentLoaded fires, this CL reads the value via .property if has/getAttribute didn't find the attribute.
    BUG=246283
    Test=Added test that fails without this change (WebViewTest.SetPropertyOnDocumentReady).
    
    Review URL: https://chromiumcodereview.appspot.com/16276012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205802 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/web_view/document_ready/embedder.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+document.addEventListener('DOMContentLoaded', function(e) {
+  var webview = document.querySelector('webview');
+  webview.addEventListener('loadstop', function(e) {
+    // Note that we are relying on .partition property to read the partition.
+    // The other way would be to read this value from BrowserPluginGuest in cpp
+    // code, which is slower and requires mode code (hurts readability of the
+    // test).
+    var partitionName = webview.partition;
+    chrome.test.runTests([
+      function checkPartition() {
+        if (partitionName == 'persist:test-partition') {
+          chrome.test.succeed();
+        } else {
+          chrome.test.fail();
+        }
+      }
+    ]);
+  });
+  webview.partition = 'persist:test-partition';
+  webview.setAttribute('src', 'data:text/html,<body>Test</body>');
+});
