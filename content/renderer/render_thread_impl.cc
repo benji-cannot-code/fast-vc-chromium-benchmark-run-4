1528f1440f053302ae0a665b949bfc2e1a9a3099
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 01:43:34 +0000

    Restore thread safety to GpuChannelHost.
    RefCountedThreadSafe + SupportsWeakPtr is generally a bad combo.
    This changes how thread safety is handled in GpuChannelHost:
    - Shared data/state set by the IO thread is moved onto the MessageFilter,
      protected by a lock.
    - MessageFilter doesn't need to post tasks to the GpuChannelHost on the main
      thread.
    - Most of the GpuChannelHost fields are constant, the remaining ones are atomic
      or protected by a lock.
    
    It also includes various cleanup:
    - Reduced the scope of some locks. In particular we mostly avoid taking locks
      while sending messages.
    - Removed GpuChannelHostFactory::IsIOThread which isn't used.
    - Simplifies channel state. We always are "connected" until we are "lost".
    
    A behavior change is that the "lost" state is set directly on the IO thread, as
    soon as we receive the channel error. This makes recreation logic less dependent
    on precise task order.
    
    BUG=242826
    R=apatrick@chromium.org
    
    Review URL: https://codereview.chromium.org/16228004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@205992 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_thread_impl.cc
+++ content/renderer/render_thread_impl.cc
@@ -1008,11 +1008,6 @@ bool RenderThreadImpl::IsMainThread() {
   return !!current();
 }
 
-bool RenderThreadImpl::IsIOThread() {
-  return base::MessageLoop::current() ==
-         ChildProcess::current()->io_message_loop();
-}
-
 base::MessageLoop* RenderThreadImpl::GetMainLoop() {
   return message_loop();
 }
@@ -1138,9 +1133,8 @@ GpuChannelHost* RenderThreadImpl::EstablishGpuChannelSync(
   if (gpu_channel_.get()) {
     // Do nothing if we already have a GPU channel or are already
     // establishing one.
-    if (gpu_channel_->state() == GpuChannelHost::kUnconnected ||
-        gpu_channel_->state() == GpuChannelHost::kConnected)
-      return GetGpuChannel();
+    if (!gpu_channel_->IsLost())
+      return gpu_channel_.get();
 
     // Recreate the channel if it has been lost.
     gpu_channel_ = NULL;
@@ -1159,18 +1153,13 @@ GpuChannelHost* RenderThreadImpl::EstablishGpuChannelSync(
 #endif
       channel_handle.name.empty()) {
     // Otherwise cancel the connection.
-    gpu_channel_ = NULL;
     return NULL;
   }
 
-  gpu_channel_ = new GpuChannelHost(this, 0, client_id);
-  gpu_channel_->set_gpu_info(gpu_info);
   GetContentClient()->SetGpuInfo(gpu_info);
-
-  // Connect to the GPU process if a channel name was received.
-  gpu_channel_->Connect(channel_handle);
-
-  return GetGpuChannel();
+  gpu_channel_ = GpuChannelHost::Create(
+      this, 0, client_id, gpu_info, channel_handle);
+  return gpu_channel_.get();
 }
 
 WebKit::WebMediaStreamCenter* RenderThreadImpl::CreateMediaStreamCenter(
@@ -1209,7 +1198,7 @@ GpuChannelHost* RenderThreadImpl::GetGpuChannel() {
   if (!gpu_channel_.get())
     return NULL;
 
-  if (gpu_channel_->state() != GpuChannelHost::kConnected)
+  if (gpu_channel_->IsLost())
     return NULL;
 
   return gpu_channel_.get();
