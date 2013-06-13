9b0e34e78bbfc5192dafcbacee8fe1c135e4f9bd
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 17:55:18 +0000

    Linux: refactor setuid sandbox activation.
    - Add a new --disable-setuid-sandbox flag to disable the setuid
      sandbox without disabling seccomp-bpf.
    - Knowledge of whether or not the sandbox is desired is in
      browser_main_loop.cc, so move the error message.
    - State intent to die with FATAL on unsupported configurations.
    
    BUG=245376
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/16866004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206114 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/content_switches.cc
+++ content/public/common/content_switches.cc
@@ -188,6 +188,9 @@ const char kDisableSeccompFilterSandbox[]   = "disable-seccomp-filter-sandbox";
 // Disable session storage.
 const char kDisableSessionStorage[]         = "disable-session-storage";
 
+// Disable the setuid sandbox (Linux only).
+const char kDisableSetuidSandbox[]          = "disable-setuid-sandbox";
+
 // Enable shared workers. Functionality not yet complete.
 const char kDisableSharedWorkers[]          = "disable-shared-workers";
 
