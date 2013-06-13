d8646d34cd6f3538df4d0e90c9cf0877de6171b5
shishir@chromium.org <shishir@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 19:57:40 +0000

    Prerendering: Defering load of WebMediaSource while in prerender mode.
    Currently prerendered pages can end up playing video/audio in the back ground
    if they are using Media Source. After this change the loading of the files
    will be deferred till the prerender is swapped in.
    
    TEST=PrerenderBrowserTest.PrerenderHTML5MediaSourceVideo
    BUG=136262
    
    Review URL: https://chromiumcodereview.appspot.com/16797002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206144 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/prerender/prerender_webmediaplayer.cc
+++ chrome/renderer/prerender/prerender_webmediaplayer.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/prerender_messages.h"
 #include "content/public/renderer/render_view.h"
 #include "third_party/WebKit/public/platform/WebURL.h"
+#include "third_party/WebKit/Source/WebKit/chromium/public/WebMediaSource.h"
 #include "webkit/renderer/media/webmediaplayer_delegate.h"
 
 namespace prerender {
@@ -32,11 +33,27 @@ void PrerenderWebMediaPlayer::load(const WebKit::WebURL& url,
   DCHECK(!url_loaded_);
   if (is_prerendering_) {
     url_to_load_.reset(new WebKit::WebURL(url));
+    media_source_to_load_.reset();
     cors_mode_ = cors_mode;
     return;
   }
   url_loaded_ = true;
   WebMediaPlayerImpl::load(url, cors_mode);
+}
+
+void PrerenderWebMediaPlayer::load(const WebKit::WebURL& url,
+                                   WebKit::WebMediaSource* media_source,
+                                   CORSMode cors_mode) {
+  DCHECK(!url_loaded_);
+  if (is_prerendering_) {
+    url_to_load_.reset(new WebKit::WebURL(url));
+    media_source_to_load_.reset(media_source);
+    cors_mode_ = cors_mode;
+    return;
+  }
+
+  url_loaded_ = true;
+  WebMediaPlayerImpl::load(url, media_source, cors_mode);
 }
 
 bool PrerenderWebMediaPlayer::OnMessageReceived(const IPC::Message& message) {
@@ -52,11 +69,17 @@ void PrerenderWebMediaPlayer::OnSetIsPrerendering(bool is_prerendering) {
   // navigation, so no PrerenderWebMediaPlayer should see the notification
   // that enables prerendering.
   DCHECK(!is_prerendering);
-  if (is_prerendering_ && !is_prerendering) {
-    is_prerendering_ = false;
-    if (url_to_load_.get())
-      load(*url_to_load_, cors_mode_);
-  }
+  if (!is_prerendering_ || is_prerendering)
+    return;
+
+  is_prerendering_ = false;
+  if (!url_to_load_)
+    return;
+
+  if (media_source_to_load_)
+    load(*url_to_load_, media_source_to_load_.release(), cors_mode_);
+  else
+    load(*url_to_load_, cors_mode_);
 }
 
 }  // namespace prerender
