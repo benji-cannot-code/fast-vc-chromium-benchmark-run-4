16d8766d57da1ee4b85d4461c42d54a320add014
brianderson@chromium.org <brianderson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 06:59:20 +0000

    cc: Emulate BeginFrame in OutputSurfaces that don't support it natively
    This includes two small fixes for the original version of this
    patch that broke software compositing and WebView.
    
    This will allow us to avoid having two different code paths
    in the Scheduler. It also allows us to more easily remove the
    VSyncTimeSource and FrameRateController from the Scheduler.
    
    This patch instantiates the FrameRateController inside of
    OutputSurface for now, but the FrameRateController could be
    removed in future patches.
    
    BUG=245920
    BUG=243497
    TBR=nduca@chromium.org,sievers@chromium.org,kbr@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16833003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206020 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/compositor_output_surface.cc
+++ content/renderer/gpu/compositor_output_surface.cc
@@ -69,7 +69,7 @@ CompositorOutputSurface::CompositorOutputSurface(
 
 CompositorOutputSurface::~CompositorOutputSurface() {
   DCHECK(CalledOnValidThread());
-  if (!client_)
+  if (!HasClient())
     return;
   UpdateSmoothnessTakesPriority(false);
   if (output_surface_proxy_.get())
@@ -96,6 +96,7 @@ bool CompositorOutputSurface::BindToClient(
 void CompositorOutputSurface::SwapBuffers(cc::CompositorFrame* frame) {
   if (use_swap_compositor_frame_message_) {
     Send(new ViewHostMsg_SwapCompositorFrame(routing_id_, *frame));
+    DidSwapBuffers();
     return;
   }
 
@@ -114,7 +115,7 @@ void CompositorOutputSurface::SwapBuffers(cc::CompositorFrame* frame) {
 
 void CompositorOutputSurface::OnMessageReceived(const IPC::Message& message) {
   DCHECK(CalledOnValidThread());
-  if (!client_)
+  if (!HasClient())
     return;
   IPC_BEGIN_MESSAGE_MAP(CompositorOutputSurface, message)
     IPC_MESSAGE_HANDLER(ViewMsg_UpdateVSyncParameters, OnUpdateVSyncParameters);
@@ -128,23 +129,24 @@ void CompositorOutputSurface::OnMessageReceived(const IPC::Message& message) {
 void CompositorOutputSurface::OnUpdateVSyncParameters(
     base::TimeTicks timebase, base::TimeDelta interval) {
   DCHECK(CalledOnValidThread());
-  client_->OnVSyncParametersChanged(timebase, interval);
+  OnVSyncParametersChanged(timebase, interval);
 }
 
 #if defined(OS_ANDROID)
 void CompositorOutputSurface::SetNeedsBeginFrame(bool enable) {
   DCHECK(CalledOnValidThread());
   Send(new ViewHostMsg_SetNeedsBeginFrame(routing_id_, enable));
+  OutputSurface::SetNeedsBeginFrame(enable);
 }
 
 void CompositorOutputSurface::OnBeginFrame(base::TimeTicks frame_time) {
   DCHECK(CalledOnValidThread());
-  client_->BeginFrame(frame_time);
+  BeginFrame(frame_time);
 }
 #endif  // defined(OS_ANDROID)
 
 void CompositorOutputSurface::OnSwapAck(const cc::CompositorFrameAck& ack) {
-  client_->OnSwapBuffersComplete(&ack);
+  OnSwapBuffersComplete(&ack);
 }
 
 bool CompositorOutputSurface::Send(IPC::Message* message) {
