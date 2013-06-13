30baf95130de1a0e166d14c5a4a2795d9443a4f9
timvolodine@chromium.org <timvolodine@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-13 23:32:48 +0000

    Consistently autosize list item markers (e.g. bullets and numbers).
    This patch implements correct autosizing of list item markers when the list item is not a direct parent of an autosized text node, which happens when the list items contain marked up text such as e.g. links, bold and italic text or autosized text as part of an anonymous render block.
    
    BUG=135854,169484
    
    Review URL: https://chromiumcodereview.appspot.com/16714002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152391 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/page/Settings.h"
 #include "core/platform/graphics/IntSize.h"
+#include "core/rendering/RenderListItem.h"
 #include "core/rendering/RenderObject.h"
 #include "core/rendering/RenderText.h"
 #include "core/rendering/RenderView.h"
@@ -79,6 +80,15 @@ static const Vector<QualifiedName>& formInputTags()
         formInputTags.append(selectTag);
     }
     return formInputTags;
+}
+
+static RenderListItem* getAncestorListItem(const RenderObject* renderer)
+{
+    RenderObject* ancestor = renderer->parent();
+    while (ancestor && (ancestor->isRenderInline() || ancestor->isAnonymousBlock()))
+        ancestor = ancestor->parent();
+
+    return (ancestor && ancestor->isListItem()) ? toRenderListItem(ancestor) : 0;
 }
 
 TextAutosizer::TextAutosizer(Document* document)
@@ -207,8 +217,11 @@ void TextAutosizer::processContainer(float multiplier, RenderBlock* container, T
             if (localMultiplier != 1 && descendant->style()->textAutosizingMultiplier() == 1) {
                 setMultiplier(descendant, localMultiplier);
                 setMultiplier(descendant->parent(), localMultiplier); // Parent does line spacing.
+                if (RenderListItem* listItemAncestor = getAncestorListItem(descendant)) {
+                    if (listItemAncestor->style()->textAutosizingMultiplier() == 1)
+                        setMultiplier(listItemAncestor, localMultiplier);
+                }
             }
-            // FIXME: Increase list marker size proportionately.
         } else if (isAutosizingContainer(descendant)) {
             RenderBlock* descendantBlock = toRenderBlock(descendant);
             TextAutosizingClusterInfo descendantClusterInfo(descendantBlock);
