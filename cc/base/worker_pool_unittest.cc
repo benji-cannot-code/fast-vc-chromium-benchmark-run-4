81cef13f96e416151f1d13031b60fef2b8189d3d
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 14:34:36 +0000

    cc: Move WorkerPool task graph construction to derived classes.
    This makes it easier to unit test the task graph construction code and
    gives derived worker pool classes the ability to more efficiently construct
    the graph.
    
    BUG=247922
    TEST=cc_unittests --gtest_filter=WorkerPoolTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/16378006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206070 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/base/worker_pool_unittest.cc
+++ cc/base/worker_pool_unittest.cc
@@ -73,7 +73,11 @@ class FakeWorkerPool : public WorkerPool {
                          &tasks));
 
     scheduled_tasks_completion_.reset(new CompletionEvent);
-    WorkerPool::ScheduleTasks(completion_task.get());
+
+    TaskGraph graph;
+    BuildTaskGraph(completion_task.get(), &graph);
+    WorkerPool::SetTaskGraph(&graph);
+    root_.swap(completion_task);
   }
 
   void WaitForTasksToComplete() {
@@ -87,6 +91,7 @@ class FakeWorkerPool : public WorkerPool {
     scheduled_tasks_completion_->Signal();
   }
 
+  scoped_refptr<FakeTaskImpl> root_;
   scoped_ptr<CompletionEvent> scheduled_tasks_completion_;
 };
 
