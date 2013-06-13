5a78a3dc68fc84d7886890e0e3fdb00146f93e0e
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 18:24:02 +0000

    Hooking up all EME messages with MediaSourcePlayer and introduce MediaDrmBridge
    MediaDrmBridge will be implemented later, but adding all the calls so people can start working on related classes.
    
    BUG=163552
    R=tedchoc@chromium.org, xhwang@chromium.org
    
    Review URL: https://codereview.chromium.org/16652002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206126 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/media_player_manager_impl.h
+++ content/browser/android/media_player_manager_impl.h
@@ -20,6 +20,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "media/base/android/media_player_manager.h"
 #include "ui/gfx/rect_f.h"
 
+namespace media {
+class MediaDrmBridge;
+}
+
 namespace content {
 
 class WebContents;
@@ -66,26 +70,25 @@ class CONTENT_EXPORT MediaPlayerManagerImpl
       int player_id,
       media::DemuxerStream::Type type,
       bool seek_done) OVERRIDE;
-  virtual void RequestMediaResources(
-      media::MediaPlayerAndroid* player) OVERRIDE;
-  virtual void ReleaseMediaResources(
-      media::MediaPlayerAndroid* player) OVERRIDE;
+  virtual void RequestMediaResources(int player_id) OVERRIDE;
+  virtual void ReleaseMediaResources(int player_id) OVERRIDE;
   virtual media::MediaResourceGetter* GetMediaResourceGetter() OVERRIDE;
   virtual media::MediaPlayerAndroid* GetFullscreenPlayer() OVERRIDE;
   virtual media::MediaPlayerAndroid* GetPlayer(int player_id) OVERRIDE;
+  virtual media::MediaDrmBridge* GetDrmBridge(int media_keys_id) OVERRIDE;
   virtual void DestroyAllMediaPlayers() OVERRIDE;
   virtual void OnMediaSeekRequest(int player_id, base::TimeDelta time_to_seek,
                                   unsigned seek_request_id) OVERRIDE;
   virtual void OnMediaConfigRequest(int player_id) OVERRIDE;
-  virtual void OnKeyAdded(int player_id,
+  virtual void OnKeyAdded(int media_keys_id,
                           const std::string& key_system,
                           const std::string& session_id) OVERRIDE;
-  virtual void OnKeyError(int player_id,
+  virtual void OnKeyError(int media_keys_id,
                           const std::string& key_system,
                           const std::string& session_id,
                           media::MediaKeys::KeyError error_code,
                           int system_code) OVERRIDE;
-  virtual void OnKeyMessage(int player_id,
+  virtual void OnKeyMessage(int media_keys_id,
                             const std::string& key_system,
                             const std::string& session_id,
                             const std::string& message,
@@ -124,16 +127,16 @@ class CONTENT_EXPORT MediaPlayerManagerImpl
       int player_id,
       const media::MediaPlayerHostMsg_ReadFromDemuxerAck_Params& params);
   void OnMediaSeekRequestAck(int player_id, unsigned seek_request_id);
-  void OnGenerateKeyRequest(int player_id,
+  void OnGenerateKeyRequest(int media_keys_id,
                             const std::string& key_system,
                             const std::string& type,
                             const std::vector<uint8>& init_data);
-  void OnAddKey(int player_id,
+  void OnAddKey(int media_keys_id,
                 const std::string& key_system,
                 const std::vector<uint8>& key,
                 const std::vector<uint8>& init_data,
                 const std::string& session_id);
-  void OnCancelKeyRequest(int player_id,
+  void OnCancelKeyRequest(int media_keys_id,
                           const std::string& key_system,
                           const std::string& session_id);
   void OnDurationChanged(int player_id, const base::TimeDelta& duration);
@@ -149,9 +152,15 @@ class CONTENT_EXPORT MediaPlayerManagerImpl
   // Removes the player with the specified id.
   void RemovePlayer(int player_id);
 
+  // Removes the DRM bridge with the specified id.
+  void RemoveDrmBridge(int key_id);
+
  private:
   // An array of managed players.
   ScopedVector<media::MediaPlayerAndroid> players_;
+
+  // An array of managed media DRM bridges.
+  ScopedVector<media::MediaDrmBridge> drm_bridges_;
 
   // The fullscreen video view object or NULL if video is not played in
   // fullscreen.
