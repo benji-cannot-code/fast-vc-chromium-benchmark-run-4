db373490f537f0e8e7b086a53e3ade60ffd0df52
arbesfeld@chromium.org <arbesfeld@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-13 22:42:06 +0000

    Add bold text string for CanvasRenderingContext2D.
    BUG=164007
    
    Review URL: https://chromiumcodereview.appspot.com/16838004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152387 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.cpp
@@ -752,7 +752,7 @@ void CanvasRenderingContext2D::setTransform(float m11, float m12, float m21, flo
         return;
 
     realizeSaves();
-    
+
     c->setCTM(canvas()->baseTransform());
     modifiableState().m_transform = AffineTransform();
     m_path.transform(ctm);
@@ -899,7 +899,7 @@ static bool parseWinding(const String& windingRuleString, WindRule& windRule)
         windRule = RULE_EVENODD;
     else
         return false;
-    
+
     return true;
 }
 
@@ -934,7 +934,7 @@ void CanvasRenderingContext2D::fill(const String& windingRuleString)
             c->fillPath(m_path);
             didDraw(m_path.boundingRect());
         }
-        
+
         c->setFillRule(windRule);
     }
 }
@@ -994,7 +994,7 @@ bool CanvasRenderingContext2D::isPointInPath(const float x, const float y, const
     WindRule windRule = RULE_NONZERO;
     if (!parseWinding(windingRuleString, windRule))
         return false;
-    
+
     return m_path.contains(transformedPoint, windRule);
 }
 
@@ -1834,7 +1834,7 @@ PassRefPtr<ImageData> CanvasRenderingContext2D::getImageData(ImageBuffer::Coordi
     if (sw < 0) {
         sx += sw;
         sw = -sw;
-    }    
+    }
     if (sh < 0) {
         sy += sh;
         sh = -sh;
@@ -1946,6 +1946,8 @@ String CanvasRenderingContext2D::font() const
 
     if (fontDescription.italic())
         serializedFont.appendLiteral("italic ");
+    if (fontDescription.weight() == FontWeightBold)
+        serializedFont.appendLiteral("bold ");
     if (fontDescription.smallCaps() == FontSmallCapsOn)
         serializedFont.appendLiteral("small-caps ");
 
