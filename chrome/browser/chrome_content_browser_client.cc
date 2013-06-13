95943390a011b9880b8f8e4824d8a2fde45cd1f4
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 15:07:53 +0000

    Enable hardware acceleration for Files.app.
    Formerly, accelerated compositing and canvas were disabled for components. This patch enables hardware acceleration for Files.app only making it so much faster! Yay!
    
    TEST=Run Files.app, scroll.
    BUG=249269
    
    Review URL: https://chromiumcodereview.appspot.com/16947002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206071 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -188,6 +188,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/media/webrtc_logging_handler_host.h"
 #endif
 
+#if defined(FILE_MANAGER_EXTENSION)
+#include "chrome/browser/chromeos/extensions/file_manager/file_manager_util.h"
+#endif
+
 using base::FileDescriptor;
 using content::AccessTokenStore;
 using content::BrowserChildProcessHostIterator;
@@ -2042,7 +2046,8 @@ void ChromeContentBrowserClient::OverrideWebkitPrefs(
   // Override the default of suppressing HW compositing for WebUI pages for the
   // file manager, which is implemented using WebUI but wants HW acceleration
   // for video decode & render.
-  if (url.spec() == chrome::kChromeUIFileManagerURL) {
+  if (url.SchemeIs(extensions::kExtensionScheme) &&
+      url.host() == kFileBrowserDomain) {
     web_prefs->accelerated_compositing_enabled = true;
     web_prefs->accelerated_2d_canvas_enabled = true;
   }
