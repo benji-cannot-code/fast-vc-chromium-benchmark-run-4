e1c243f257ed3d775eb9d37a336fdcaa388d57ca
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 16:10:20 +0000

    Remove test-specific code in NotificationBitmapFetcher.
    Use FakeURLFetcherFactory to eliminate the need to expose a setter for
    the URLFetcher, which eliminates a conditional that was only needed for
    test purposes.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/16034020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206082 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/sync_notifier/notification_bitmap_fetcher_browsertest.cc
+++ chrome/browser/notifications/sync_notifier/notification_bitmap_fetcher_browsertest.cc
@@ -6,12 +6,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/sync_notifier/notification_bitmap_fetcher.h"
 
 #include "base/compiler_specific.h"
+#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/in_process_browser_test.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/test/test_utils.h"
 #include "net/url_request/test_url_fetcher_factory.h"
 #include "net/url_request/url_fetcher.h"
-#include "net/url_request/url_request_status.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/codec/png_codec.h"
@@ -30,13 +30,16 @@ class NotificationBitmapFetcherTestDelegate
     : public NotificationBitmapFetcherDelegate {
  public:
   explicit NotificationBitmapFetcherTestDelegate(bool async)
-      : success_(false), async_(async) {}
+      : called_(false), success_(false), async_(async) {}
 
-  virtual ~NotificationBitmapFetcherTestDelegate() {}
+  virtual ~NotificationBitmapFetcherTestDelegate() {
+    EXPECT_TRUE(called_);
+  }
 
   // Method inherited from NotificationBitmapFetcherDelegate.
   virtual void OnFetchComplete(const GURL url,
                                const SkBitmap* bitmap) OVERRIDE {
+    called_ = true;
     url_ = url;
     if (NULL != bitmap) {
       success_ = true;
@@ -54,6 +57,7 @@ class NotificationBitmapFetcherTestDelegate
   const SkBitmap& bitmap() const { return bitmap_; }
 
  private:
+  bool called_;
   GURL url_;
   bool success_;
   bool async_;
@@ -62,7 +66,16 @@ class NotificationBitmapFetcherTestDelegate
   DISALLOW_COPY_AND_ASSIGN(NotificationBitmapFetcherTestDelegate);
 };
 
-typedef InProcessBrowserTest NotificationBitmapFetcherBrowserTest;
+class NotificationBitmapFetcherBrowserTest : public InProcessBrowserTest {
+ public:
+  virtual void SetUp() OVERRIDE {
+    url_fetcher_factory_.reset(new net::FakeURLFetcherFactory(NULL));
+    InProcessBrowserTest::SetUp();
+  }
+
+ protected:
+  scoped_ptr<net::FakeURLFetcherFactory> url_fetcher_factory_;
+};
 
 // WARNING:  These tests work with --single_process, but not
 // --single-process.  The reason is that the sandbox does not get created
@@ -70,7 +83,7 @@ typedef InProcessBrowserTest NotificationBitmapFetcherBrowserTest;
 
 IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
                        StartTest) {
-  GURL url("http://localhost");
+  GURL url("http://example.com/this-should-work");
 
   // Put some realistic looking bitmap data into the url_fetcher.
   SkBitmap image;
@@ -92,13 +105,11 @@ IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
 
   NotificationBitmapFetcher fetcher(url, &delegate);
 
-  scoped_ptr<net::URLFetcher> url_fetcher(new net::FakeURLFetcher(
-      url, &fetcher, image_string, true));
-  fetcher.SetURLFetcherForTest(url_fetcher.Pass());
+  url_fetcher_factory_->SetFakeResponse(url.spec(), image_string, true);
 
   // We expect that the image decoder will get called and return
   // an image in a callback to OnImageDecoded().
-  fetcher.Start(NULL);
+  fetcher.Start(browser()->profile());
 
   // Blocks until test delegate is notified via a callback.
   content::RunMessageLoop();
@@ -112,7 +123,7 @@ IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
                        OnImageDecodedTest) {
-  GURL url("http://localhost");
+  GURL url("http://example.com/this-should-work-as-well");
   SkBitmap image;
 
   // Put a real bitmap into "image".  2x2 bitmap of green 16 bit pixels.
@@ -135,7 +146,7 @@ IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
                        OnURLFetchFailureTest) {
-  GURL url("http://localhost");
+  GURL url("http://example.com/this-should-be-fetch-failure");
 
   // We intentionally put no data into the bitmap to simulate a failure.
 
@@ -144,11 +155,9 @@ IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
 
   NotificationBitmapFetcher fetcher(url, &delegate);
 
-  scoped_ptr<net::URLFetcher> url_fetcher(new net::FakeURLFetcher(
-      url, &fetcher, std::string(), false));
-  fetcher.SetURLFetcherForTest(url_fetcher.Pass());
+  url_fetcher_factory_->SetFakeResponse(url.spec(), std::string(), false);
 
-  fetcher.Start(NULL);
+  fetcher.Start(browser()->profile());
 
   // Blocks until test delegate is notified via a callback.
   content::RunMessageLoop();
@@ -158,14 +167,13 @@ IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
 
 IN_PROC_BROWSER_TEST_F(NotificationBitmapFetcherBrowserTest,
                        HandleImageFailedTest) {
-  GURL url("http://localhost");
+  GURL url("http://example.com/this-should-be-a-decode-failure");
   NotificationBitmapFetcherTestDelegate delegate(kAsyncCall);
   NotificationBitmapFetcher fetcher(url, &delegate);
-  scoped_ptr<net::URLFetcher> url_fetcher(new net::FakeURLFetcher(
-      url, &fetcher, std::string("Not a real bitmap"), true));
-  fetcher.SetURLFetcherForTest(url_fetcher.Pass());
+  url_fetcher_factory_->SetFakeResponse(
+      url.spec(), std::string("Not a real bitmap"), true);
 
-  fetcher.Start(NULL);
+  fetcher.Start(browser()->profile());
 
   // Blocks until test delegate is notified via a callback.
   content::RunMessageLoop();
