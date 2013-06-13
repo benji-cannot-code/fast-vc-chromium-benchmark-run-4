68f0cefff036f9ee6c8fd886752a993d8252dac3
xiyuan@chromium.org <xiyuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 18:16:03 +0000

    cros: Implement a special caps lock logic.
    BUG=248284
    
    Review URL: https://chromiumcodereview.appspot.com/16770004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206120 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/event_rewriter.cc
+++ chrome/browser/ui/ash/event_rewriter.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/chromeos/chromeos_version.h"
 #include "base/command_line.h"
+#include "chrome/browser/chromeos/caps_lock_rewriter.h"
 #include "chrome/browser/chromeos/login/login_display_host_impl.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/xinput_hierarchy_changed_event_listener.h"
@@ -130,7 +131,6 @@ bool IsMod3UsedByCurrentInputMethod() {
       chromeos::input_method::InputMethodManager::Get();
   return manager->GetCurrentInputMethod().id() == kNeo2LayoutId ||
       manager->GetCurrentInputMethod().id() == kCaMultixLayoutId;
-
 }
 #endif
 
@@ -147,6 +147,7 @@ EventRewriter::EventRewriter()
     : last_device_id_(kBadDeviceId),
 #if defined(OS_CHROMEOS)
       xkeyboard_(NULL),
+      caps_lock_rewriter_(new chromeos::CapsLockRewriter),
 #endif
       pref_service_(NULL) {
   // The ash shell isn't instantiated for our unit tests.
@@ -358,6 +359,9 @@ void EventRewriter::Rewrite(ui::KeyEvent* event) {
   RewriteNumPadKeys(event);
   RewriteExtendedKeys(event);
   RewriteFunctionKeys(event);
+#if defined(OS_CHROMEOS)
+  caps_lock_rewriter_->RewriteEvent(event, base::TimeTicks::Now());
+#endif
 }
 
 bool EventRewriter::IsAppleKeyboard() const {
@@ -450,11 +454,11 @@ bool EventRewriter::RewriteModifiers(ui::KeyEvent* event) {
   // restart chrome process. In future this is to be changed.
   // TODO(glotov): remove the following condition when we do not restart chrome
   // when user logs in as guest.
- #if defined(OS_CHROMEOS)
-   if (chromeos::UserManager::Get()->IsLoggedInAsGuest() &&
-       chromeos::LoginDisplayHostImpl::default_host())
-     return false;
- #endif  // defined(OS_CHROMEOS)
+#if defined(OS_CHROMEOS)
+  if (chromeos::UserManager::Get()->IsLoggedInAsGuest() &&
+      chromeos::LoginDisplayHostImpl::default_host())
+    return false;
+#endif  // defined(OS_CHROMEOS)
   const PrefService* pref_service =
       pref_service_ ? pref_service_ : GetPrefService();
   if (!pref_service)
