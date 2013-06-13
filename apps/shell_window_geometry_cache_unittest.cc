da71c8a9e85b022a5329533f399ee89ac65d337d
rockot@chromium.org <rockot@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 06:37:42 +0000

    The cache was designed to load an extension's stored geometry from sync only when the extension is first loaded. Because the cache was being lazily created on first access, it would never exist on browser startup when all installed extensions were being loaded.
    This will attempt to load an extension's geometry from storage 
    on first access as well, if it is not already loaded.
    
    BUG=246810
    
    Review URL: https://chromiumcodereview.appspot.com/16012019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206011 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/shell_window_geometry_cache_unittest.cc
+++ apps/shell_window_geometry_cache_unittest.cc
@@ -80,7 +80,7 @@ void ShellWindowGeometryCacheTest::WaitForSync() {
 
 void ShellWindowGeometryCacheTest::LoadExtension(
     const std::string& extension_id) {
-  cache_->OnExtensionLoaded(extension_id);
+  cache_->LoadGeometryFromStorage(extension_id);
   WaitForSync();
 }
 
@@ -171,10 +171,6 @@ TEST_F(ShellWindowGeometryCacheTest, SaveGeometryAndStateToStore) {
   ASSERT_EQ(bounds.height(), v);
   ASSERT_TRUE(dict->GetInteger(window_id + ".state", &v));
   ASSERT_EQ(state, v);
-
-  // check to make sure cache indeed doesn't know about this extension anymore
-  ASSERT_FALSE(cache_->GetGeometry(
-      extension_id, window_id, &new_bounds, &new_state));
 
   // reload extension
   LoadExtension(extension_id);
