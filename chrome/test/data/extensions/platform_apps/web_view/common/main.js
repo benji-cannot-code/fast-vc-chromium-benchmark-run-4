edd975c95f7e057f2593d18111ba7751d84caea5
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-13 19:46:59 +0000

    Begin to merge dup code for embedder in webview tests.
    BUG=244912
    
    Review URL: https://chromiumcodereview.appspot.com/15998003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206139 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/web_view/common/main.js
@@ -0,0 +1,26 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+var startTest = function(testDir) {
+  LOG('startTest: ' + testDir);
+  // load bootstrap script.
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = testDir + '/bootstrap.js';
+  document.getElementsByTagName('head')[0].appendChild(script);
+};
+
+var onloadFunction = function() {
+  window.console.log('app.onload');
+  chrome.test.getConfig(function(config) {
+    LOG('embeder.common got config: ' + config);
+    LOG('customArg: ' + config.customArg);
+    loadFired = true;
+    window['startTest'](config.customArg);
+  });
+};
+
+onload = onloadFunction;
+
