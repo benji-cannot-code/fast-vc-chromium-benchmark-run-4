4c2afc56cd10d754774a9ceba54caeecf61b996c
aa@chromium.org <aa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 01:31:11 +0000

    Adding missing forward declaration of GURL.
    Review URL: https://chromiumcodereview.appspot.com/17008002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206270 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/render_view_observer.h
+++ content/public/renderer/render_view_observer.h
@@ -13,6 +13,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_sender.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebIconURL.h"
 
+class GURL;
+
 namespace ppapi {
 namespace host {
 class PpapiHost;
