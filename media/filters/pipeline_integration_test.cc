5ad21b5a49ffa4cea691de7b72e599e0cc4fc70a
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 21:53:50 +0000

    Revert 206460 "Libvpx roll to r206072"
    > Libvpx roll to r206072
    > 
    > From r197977
    > 
    > This roll adds support for the finalized VP9 bitstream.
    > 
    > See:
    > https://codereview.chromium.org/16109020/
    > https://codereview.chromium.org/16863002/
    > https://codereview.chromium.org/16970002/
    > https://codereview.chromium.org/17028002/
    > 
    > for the actual changes.
    > 
    > BUG=165504
    > 
    > Review URL: https://chromiumcodereview.appspot.com/16370012
    
    TBR=fgalligan@chromium.org
    
    Review URL: https://codereview.chromium.org/17120002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206498 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/pipeline_integration_test.cc
+++ media/filters/pipeline_integration_test.cc
@@ -481,9 +481,7 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_MediaSource) {
   Stop();
 }
 
-// TODO(fgalligan): Enable after new vp9 files are landed. crbug.com/180280
-TEST_F(PipelineIntegrationTest,
-       DISABLED_BasicPlayback_MediaSource_VideoOnly_VP9_WebM) {
+TEST_F(PipelineIntegrationTest, BasicPlayback_MediaSource_VideoOnly_VP9_WebM) {
   MockMediaSource source("bear-vp9.webm", kWebMVP9, 19678);
   StartPipelineWithMediaSource(&source);
   source.EndOfStream();
@@ -942,8 +940,7 @@ TEST_F(PipelineIntegrationTest, BasicPlayback_AudioOnly_Opus_WebM) {
 }
 
 // Verify that VP9 video in WebM containers can be played back.
-// TODO(fgalligan): Enable after new vp9 files are landed. crbug.com/180280
-TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VideoOnly_VP9_WebM) {
+TEST_F(PipelineIntegrationTest, BasicPlayback_VideoOnly_VP9_WebM) {
   ASSERT_TRUE(Start(GetTestDataFilePath("bear-vp9.webm"),
                     PIPELINE_OK));
   Play();
@@ -952,8 +949,7 @@ TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VideoOnly_VP9_WebM) {
 
 // Verify that VP9 video and Opus audio in the same WebM container can be played
 // back.
-// TODO(fgalligan): Enable after new vp9 files are landed. crbug.com/180280
-TEST_F(PipelineIntegrationTest, DISABLED_BasicPlayback_VP9_Opus_WebM) {
+TEST_F(PipelineIntegrationTest, BasicPlayback_VP9_Opus_WebM) {
   ASSERT_TRUE(Start(GetTestDataFilePath("bear-vp9-opus.webm"),
                     PIPELINE_OK));
   Play();
