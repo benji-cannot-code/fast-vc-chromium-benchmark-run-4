b59e9b27c50bac6ac0ce60e6357da3d45e3287ca
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 00:13:35 +0000

    Add a flag to turn on TCP server socket for WebRTC
    New flag to enable TCP server socket.
    
    Review URL: https://chromiumcodereview.appspot.com/16876003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206239 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/p2p/port_allocator.cc
+++ content/renderer/p2p/port_allocator.cc
@@ -6,9 +6,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/p2p/port_allocator.h"
 
 #include "base/bind.h"
+#include "base/command_line.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/string_split.h"
 #include "base/strings/string_util.h"
+#include "content/public/common/content_switches.h"
 #include "content/renderer/p2p/host_address_request.h"
 #include "jingle/glue/utils.h"
 #include "net/base/escape.h"
@@ -77,7 +79,10 @@ P2PPortAllocator::P2PPortAllocator(
   set_flags(flags);
   // TODO(ronghuawu): crbug/138185 add ourselves to the firewall list in browser
   // process and then remove below line.
-  set_allow_tcp_listen(false);
+  if (!CommandLine::ForCurrentProcess()->HasSwitch(
+          switches::kEnableWebRtcTcpServerSocket)) {
+    set_allow_tcp_listen(false);
+  }
 }
 
 P2PPortAllocator::~P2PPortAllocator() {
