a8734143e461cbc5917c5c8b22c6d89773b754e2
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 15:46:10 +0000

    Add DEPS rules for not including cc's base from content and chrome.
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/16890002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206419 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_widget.cc
+++ content/renderer/render_widget.cc
@@ -16,8 +16,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
 #include "cc/base/switches.h"
-#include "cc/base/thread.h"
-#include "cc/base/thread_impl.h"
 #include "cc/output/output_surface.h"
 #include "cc/trees/layer_tree_host.h"
 #include "content/common/gpu/client/webgraphicscontext3d_command_buffer_impl.h"
