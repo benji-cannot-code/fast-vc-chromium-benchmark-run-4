bfa7ea81d0477d045064774428935c809d2a1091
cpu@chromium.org <cpu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 00:13:17 +0000

    Give a default HMONITOR for Pepper's OnHostMsgGetHmonitor
    So we can do an end-to-end testing of the functionality with Flash.
    
    BUG=249135
    TEST=included
    
    Review URL: https://chromiumcodereview.appspot.com/16873002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206238 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/pepper/pepper_flash_drm_host.cc
+++ chrome/browser/renderer_host/pepper/pepper_flash_drm_host.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/renderer_host/pepper/pepper_flash_drm_host.h"
 
+#if defined(OS_WIN)
+#include <Windows.h>
+#endif
+
 #include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/logging.h"
@@ -73,9 +77,16 @@ int32_t PepperFlashDRMHost::OnHostMsgGetDeviceID(
 
 int32_t PepperFlashDRMHost::OnHostMsgGetHmonitor(
     ppapi::host::HostMessageContext* context) {
-  // TODO(cpu): Get the HMONITOR.
-  context->reply_msg = PpapiPluginMsg_FlashDRM_GetHmonitorReply(0);
+#if defined(OS_WIN)
+  // TODO(cpu): Get the real HMONITOR. See bug 249135.
+  POINT pt = {1,1};
+  HMONITOR monitor = ::MonitorFromPoint(pt, MONITOR_DEFAULTTOPRIMARY);
+  int64_t monitor_id = reinterpret_cast<int64_t>(monitor);
+  context->reply_msg = PpapiPluginMsg_FlashDRM_GetHmonitorReply(monitor_id);
   return PP_OK;
+#else
+  return PP_ERROR_FAILED;
+#endif
 }
 
 void PepperFlashDRMHost::GotDeviceID(
