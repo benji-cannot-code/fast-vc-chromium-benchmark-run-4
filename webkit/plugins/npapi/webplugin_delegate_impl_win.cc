6eef49bd5ae1aff8bdc75e98ba4f8cd6f0df9ad5
girard@chromium.org <girard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 02:48:44 +0000

    Scale npapi plugins according to DPI setting.
    BUG=248176
    
    Review URL: https://chromiumcodereview.appspot.com/15652007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206275 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/webplugin_delegate_impl_win.cc
+++ webkit/plugins/npapi/webplugin_delegate_impl_win.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/win/windows_version.h"
 #include "skia/ext/platform_canvas.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebInputEvent.h"
+#include "ui/base/win/dpi.h"
 #include "ui/base/win/hwnd_util.h"
 #include "webkit/common/cursors/webcursor.h"
 #include "webkit/plugins/npapi/plugin_constants_win.h"
@@ -827,13 +828,15 @@ bool WebPluginDelegateImpl::CreateDummyWindowForActivation() {
 }
 
 bool WebPluginDelegateImpl::WindowedReposition(
-    const gfx::Rect& window_rect,
-    const gfx::Rect& clip_rect) {
+    const gfx::Rect& window_rect_in_dip,
+    const gfx::Rect& clip_rect_in_dip) {
   if (!windowed_handle_) {
     NOTREACHED();
     return false;
   }
 
+  gfx::Rect window_rect = ui::win::DIPToScreenRect(window_rect_in_dip);
+  gfx::Rect clip_rect = ui::win::DIPToScreenRect(clip_rect_in_dip);
   if (window_rect_ == window_rect && clip_rect_ == clip_rect)
     return false;
 
@@ -1025,7 +1028,6 @@ LRESULT CALLBACK WebPluginDelegateImpl::NativeWndProc(
     // only synchronously calling RedrawWindow once at a time.
     if (old_message != custom_msg)
       flags |= RDW_UPDATENOW;
-
     RECT rect = invalid_rect.ToRECT();
     RedrawWindow(hwnd, &rect, NULL, flags);
     result = FALSE;
