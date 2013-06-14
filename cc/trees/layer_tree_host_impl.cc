e43391f3bb1900fff67de76b5ead0d7e0415923e
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 08:47:50 +0000

    Add support for LatencyInfo to software compositor.
    It reports the LatencyInfo to the RenderWidgetHostImpl when the browser compositor swaps to the screen. Also, the LatencyInfo is now added to the CompositorFrameMetadata in all cases, not just when there's a RootScrollLayer.
    
    BUG=155367
    R=nduca@chromium.org, piman@chromium.org, skaslev@chromium.org
    
    Review URL: https://codereview.chromium.org/16566005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206373 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -1098,6 +1098,7 @@ CompositorFrameMetadata LayerTreeHostImpl::MakeCompositorFrameMetadata() const {
   metadata.root_layer_size = active_tree_->ScrollableSize();
   metadata.min_page_scale_factor = active_tree_->min_page_scale_factor();
   metadata.max_page_scale_factor = active_tree_->max_page_scale_factor();
+  metadata.latency_info = active_tree_->GetLatencyInfo();
   if (top_controls_manager_) {
     metadata.location_bar_offset =
         gfx::Vector2dF(0.f, top_controls_manager_->controls_top_offset());
@@ -1110,7 +1111,6 @@ CompositorFrameMetadata LayerTreeHostImpl::MakeCompositorFrameMetadata() const {
     return metadata;
 
   metadata.root_scroll_offset = RootScrollLayer()->TotalScrollOffset();
-  metadata.latency_info = active_tree_->GetLatencyInfo();
 
   return metadata;
 }
--- content/browser/renderer_host/image_transport_factory.cc
+++ content/browser/renderer_host/image_transport_factory.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/gpu/gpu_data_manager_impl.h"
 #include "content/browser/gpu/gpu_process_host.h"
 #include "content/browser/gpu/gpu_surface_tracker.h"
+#include "content/browser/renderer_host/render_widget_host_impl.h"
 #include "content/common/gpu/client/context_provider_command_buffer.h"
 #include "content/common/gpu/client/gl_helper.h"
 #include "content/common/gpu/client/gpu_channel_host.h"
@@ -977,6 +978,20 @@ void CompositorSwapClient::OnLostContext() {
   // Note: previous line destroyed this. Don't access members from now on.
 }
 
+class SoftwareOutputSurface : public cc::OutputSurface {
+ public:
+  explicit SoftwareOutputSurface(
+      scoped_ptr<cc::SoftwareOutputDevice> software_device)
+      : cc::OutputSurface(software_device.Pass()) {}
+
+  virtual void SwapBuffers(cc::CompositorFrame* frame) OVERRIDE {
+    ui::LatencyInfo latency_info = frame->metadata.latency_info;
+    latency_info.swap_timestamp = base::TimeTicks::HighResNow();
+    RenderWidgetHostImpl::CompositorFrameDrawn(latency_info);
+    cc::OutputSurface::SwapBuffers(frame);
+  }
+};
+
 class SoftwareContextFactory : public ui::ContextFactory {
  public:
   SoftwareContextFactory() {}
@@ -989,12 +1004,12 @@ class SoftwareContextFactory : public ui::ContextFactory {
 #if defined(OS_WIN)
     scoped_ptr<SoftwareOutputDeviceWin> software_device(
         new SoftwareOutputDeviceWin(compositor));
-    return new cc::OutputSurface(
+    return new SoftwareOutputSurface(
         software_device.PassAs<cc::SoftwareOutputDevice>());
 #elif defined(USE_X11)
     scoped_ptr<SoftwareOutputDeviceX11> software_device(
         new SoftwareOutputDeviceX11(compositor));
-    return new cc::OutputSurface(
+    return new SoftwareOutputSurface(
         software_device.PassAs<cc::SoftwareOutputDevice>());
 #else
     NOTIMPLEMENTED();
