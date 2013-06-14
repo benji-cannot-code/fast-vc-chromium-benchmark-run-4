0a3d56b8c07cc0dc7f08021229b9eb753d46b116
nbarth@chromium.org <nbarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-14 06:35:39 +0000

    Make overloadedIndex an attribute of domFunction
    This CL removes a hack, making the IDL JSON serialization simpler.
    No changes to generated .cpp/.h, only simpler Perl and JSON.
    
    overloadedIndex is the one data structure hack in IDLParser,
    where instead of setting the value of an attribute on an object,
    a key is attached to the underlying hash.
    
    This currently needs to be hacked around in JSON serialization/deserialization.
    
    This CL removes this hack by making overloadedIndex an attribute,
    which simplifies the serialization code and means we don't need to hack
    around it in Python when porting.
    
    (There are many such hacks in CodeGeneratorV8.pm, where various keys are
     attached to objects, but we don't need to fix them now;
     this is the only one that affects the JSON.)
    
    BUG=242795
    
    Review URL: https://chromiumcodereview.appspot.com/16971004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152417 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/IDLParser.pm
+++ third_party/WebKit/Source/bindings/scripts/IDLParser.pm
@@ -61,6 +61,7 @@ struct( domFunction => {
     isStatic => '$',
     signature => '$',    # Return type/Object name/extended attributes
     parameters => '@',    # List of 'domSignature'
+    overloadedIndex => '$',
 });
 
 # Used to represent domInterface contents (name of attribute, signature)
@@ -2197,7 +2198,7 @@ sub applyExtendedAttributeList
             $constructor->signature->name("Constructor");
             $constructor->signature->extendedAttributes($extendedAttributeList);
             $constructor->parameters($param);
-            $constructor->{overloadedIndex} = $index++;
+            $constructor->overloadedIndex($index++);
             push(@{$interface->constructors}, $constructor);
         }
         delete $extendedAttributeList->{"Constructors"};
@@ -2223,7 +2224,7 @@ sub applyExtendedAttributeList
             $customConstructor->signature->name("CustomConstructor");
             $customConstructor->signature->extendedAttributes($extendedAttributeList);
             $customConstructor->parameters($param);
-            $customConstructor->{overloadedIndex} = $index++;
+            $customConstructor->overloadedIndex($index++);
             push(@{$interface->customConstructors}, $customConstructor);
         }
         delete $extendedAttributeList->{"CustomConstructors"};
