1227c88c9832f97ac7e80d3abe0b2a930695e0c4
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 13:41:02 +0000

    Remove obsolete UseCompositorDirectDraw() method
    This is no longer called.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16992002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206411 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/aw_browser_main_parts.cc
+++ android_webview/browser/aw_browser_main_parts.cc
@@ -24,10 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace android_webview {
 
-bool UseCompositorDirectDraw() {
-  return base::android::BuildInfo::GetInstance()->sdk_int() >= 16;
-}
-
 AwBrowserMainParts::AwBrowserMainParts(AwBrowserContext* browser_context)
     : browser_context_(browser_context) {
 }
