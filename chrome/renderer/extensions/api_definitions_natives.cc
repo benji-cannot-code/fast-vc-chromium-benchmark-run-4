c4f63881eca96db6d2896051e0ca2657e07db898
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 19:13:47 +0000

    Remove an unused const kInvalidExtensionNamespace.
    BUG=None
    Test=None
    
    Review URL: https://chromiumcodereview.appspot.com/17032002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206468 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/api_definitions_natives.cc
+++ chrome/renderer/extensions/api_definitions_natives.cc
@@ -9,10 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/extensions/features/base_feature_provider.h"
 
-namespace {
-const char kInvalidExtensionNamespace[] = "Invalid extension namespace";
-}
-
 namespace extensions {
 
 ApiDefinitionsNatives::ApiDefinitionsNatives(Dispatcher* dispatcher,
