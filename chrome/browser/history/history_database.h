3072706738fef49a48b6a9327b26dcaeeae8266a
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 00:56:14 +0000

    Omnibox: Turn on Frecency Scoring in HQP By Default (try 2)
    This is a copy of
    https://codereview.chromium.org/16132011/
    which was reverted
    ( see revert: https://codereview.chromium.org/16860004/ )
    
    This changelist is identical except it corrects an out-of-bounds subscript in ScoredHistoryMatchTest.Scoring.
    The error was on line 150 and 151 of chrome/browser/history/scored_history_match_unittest.cc.
    
    
    Omnibox: Turn on Frecency Scoring in HQP By Default
    
    Also, disable the field trial that was used to help test the
    new scoring mode.
    
    Revises tests so they apply to the new scoring code.
    
    I left in the remnants of the HUP-like-scoring code
    because I may re-enable that at some point.
    
    BUG=175751
    TBR=pkasting,sky
    
    Review URL: https://chromiumcodereview.appspot.com/16887002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206258 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/history_database.h
+++ chrome/browser/history/history_database.h
@@ -28,6 +28,8 @@ namespace base {
 class FilePath;
 }
 
+class HistoryQuickProviderTest;
+
 namespace history {
 
 // Encapsulates the SQL connection for the history database. This class holds
@@ -171,6 +173,7 @@ class HistoryDatabase : public DownloadDatabase,
   friend class AndroidProviderBackend;
   FRIEND_TEST_ALL_PREFIXES(AndroidURLsMigrationTest, MigrateToVersion22);
 #endif
+  friend class ::HistoryQuickProviderTest;
   friend class InMemoryURLIndexTest;
   FRIEND_TEST_ALL_PREFIXES(IconMappingMigrationTest, TestIconMappingMigration);
 
