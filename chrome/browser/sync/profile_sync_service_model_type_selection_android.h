50d8cfa6b60319df4680dc78159862e7d1247e06
janx@chromium.org <janx@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 13:00:22 +0000

    Fix grammar in recurring macro comment
    Trivial patch changing "generate enum" to "generate enum values" in comments
    justifying the lack of include guards in xmacro value lists.
    
    NOTRY=true
    TBR=yfriedman,atwilson,gavinp
    
    Review URL: https://chromiumcodereview.appspot.com/16147015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206405 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/profile_sync_service_model_type_selection_android.h
+++ chrome/browser/sync/profile_sync_service_model_type_selection_android.h
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 // This file intentionally does not have header guards, it's included
-// inside a macro to generate enum.
+// inside a macro to generate enum values.
 
 // This file contains the list of sync ModelTypes that Android can register for
 // invalidations for.
