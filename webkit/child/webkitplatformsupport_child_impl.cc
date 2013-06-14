c958069ce9775b6e9e4e06d2124fcb7b1bc21eb4
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-14 18:21:38 +0000

    move webkit/glue/webthread_impl* to webkit/child/
    (pipelined on https://codereview.chromium.org/16834004/)
    
    TBR=jamesr@chromium.org, sievers@chromium.org
    
    BUG=237249
    
    Review URL: https://codereview.chromium.org/16864002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206455 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/child/webkitplatformsupport_child_impl.cc
+++ webkit/child/webkitplatformsupport_child_impl.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebInputEvent.h"
 #include "webkit/child/fling_curve_configuration.h"
+#include "webkit/child/webthread_impl.h"
 #include "webkit/child/worker_task_runner.h"
 
 #if defined(OS_ANDROID)
@@ -16,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace webkit_glue {
 
 WebKitPlatformSupportChildImpl::WebKitPlatformSupportChildImpl()
-    : fling_curve_configuration_(new FlingCurveConfiguration) {}
+    : current_thread_slot_(&DestroyCurrentThread),
+      fling_curve_configuration_(new FlingCurveConfiguration) {}
 
 WebKitPlatformSupportChildImpl::~WebKitPlatformSupportChildImpl() {}
 
@@ -44,6 +46,27 @@ WebKitPlatformSupportChildImpl::createFlingAnimationCurve(
                                                        cumulative_scroll);
 }
 
+WebKit::WebThread* WebKitPlatformSupportChildImpl::createThread(
+    const char* name) {
+  return new WebThreadImpl(name);
+}
+
+WebKit::WebThread* WebKitPlatformSupportChildImpl::currentThread() {
+  WebThreadImplForMessageLoop* thread =
+      static_cast<WebThreadImplForMessageLoop*>(current_thread_slot_.Get());
+  if (thread)
+    return (thread);
+
+  scoped_refptr<base::MessageLoopProxy> message_loop =
+      base::MessageLoopProxy::current();
+  if (!message_loop.get())
+    return NULL;
+
+  thread = new WebThreadImplForMessageLoop(message_loop.get());
+  current_thread_slot_.Set(thread);
+  return thread;
+}
+
 void WebKitPlatformSupportChildImpl::didStartWorkerRunLoop(
     const WebKit::WebWorkerRunLoop& runLoop) {
   WorkerTaskRunner* worker_task_runner = WorkerTaskRunner::Instance();
@@ -54,6 +77,13 @@ void WebKitPlatformSupportChildImpl::didStopWorkerRunLoop(
     const WebKit::WebWorkerRunLoop& runLoop) {
   WorkerTaskRunner* worker_task_runner = WorkerTaskRunner::Instance();
   worker_task_runner->OnWorkerRunLoopStopped(runLoop);
+}
+
+// static
+void WebKitPlatformSupportChildImpl::DestroyCurrentThread(void* thread) {
+  WebThreadImplForMessageLoop* impl =
+      static_cast<WebThreadImplForMessageLoop*>(thread);
+  delete impl;
 }
 
 }  // namespace webkit_glue
--- webkit/child/webthread_impl.cc
+++ webkit/child/webthread_impl.cc
@@ -6,7 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // An implementation of WebThread in terms of base::MessageLoop and
 // base::Thread
 
-#include "webkit/glue/webthread_impl.h"
+#include "webkit/child/webthread_impl.h"
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
