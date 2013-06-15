e6730783e256cf1e4d544aa640e077012dcfb00a
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-15 15:35:25 +0000

    In components/autofill, move common/ to shared/
    This change is part of moving autofill into its eventual structure of a layered
    component. autofill/components/shared/ is a new directory that will hold code
    that is shared by all platforms (including iOS). This CL populates that
    directory with the code that is currently in autofill/components/common/; a
    follow-up CL will move the contents of autofill/components/browser into this
    directory as well.
    
    This CL additionally imposes the correct DEPS restrictions on
    autofill/components/shared/, with temporary allowances for the current
    violating includes.
    
    TBR=joi, thakis, benm
    
    BUG=247015
    
    Review URL: https://chromiumcodereview.appspot.com/17084005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206593 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/autofill/form_autocomplete_browsertest.cc
+++ chrome/renderer/autofill/form_autocomplete_browsertest.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/time.h"
 #include "chrome/test/base/chrome_render_view_test.h"
-#include "components/autofill/common/autofill_messages.h"
-#include "components/autofill/common/form_data.h"
+#include "components/autofill/shared/autofill_messages.h"
+#include "components/autofill/shared/form_data.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebDocument.h"
 #include "third_party/WebKit/Source/WebKit/chromium/public/WebFormElement.h"
