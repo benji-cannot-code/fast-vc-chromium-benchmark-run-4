540dd8a2526a646a72a2f677e5b2e0aab0e67ee9
aelias@chromium.org <aelias@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-15 01:20:05 +0000

    Fix scrollbar fade animation scheduling.
    In r195531, most of the uses of base::TimeTicks::Now() were replaced by
    current frame time, but this is incorrect in the case of scrollbar
    fade scheduling.  Although the fade itself is animated within draw
    frames, the delay until it starts is managed outside frames (intentionally
    so -- we want to avoid spamming animation frames before the fade has
    actually begun).  Revert that part of r195531 and add more test
    coverage.
    
    NOTRY=true
    BUG=236351
    
    Review URL: https://chromiumcodereview.appspot.com/16925007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206532 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_impl.cc
+++ cc/trees/layer_tree_impl.cc
@@ -157,7 +157,7 @@ void LayerTreeImpl::SetCurrentlyScrollingLayer(LayerImpl* layer) {
   if (currently_scrolling_layer_ &&
       currently_scrolling_layer_->scrollbar_animation_controller())
     currently_scrolling_layer_->scrollbar_animation_controller()->
-        DidScrollGestureEnd(CurrentFrameTimeTicks());
+        DidScrollGestureEnd(CurrentPhysicalTimeTicks());
   currently_scrolling_layer_ = layer;
   if (layer && layer->scrollbar_animation_controller())
     layer->scrollbar_animation_controller()->DidScrollGestureBegin();
@@ -471,6 +471,10 @@ base::TimeTicks LayerTreeImpl::CurrentFrameTimeTicks() const {
 
 base::Time LayerTreeImpl::CurrentFrameTime() const {
   return layer_tree_host_impl_->CurrentFrameTime();
+}
+
+base::TimeTicks LayerTreeImpl::CurrentPhysicalTimeTicks() const {
+  return layer_tree_host_impl_->CurrentPhysicalTimeTicks();
 }
 
 void LayerTreeImpl::SetNeedsCommit() {
