0a34641910b2e7f8b9a489c9ad76336b5bd1d90c
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-15 17:19:36 +0000

    Revert move of file access control into browser context
    Follow up to crrev.com/206516 - in the end that patch didn't need this
    part of the change and it turns out to be awkward to provide this info
    at browser context creation time anyway.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17132003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206609 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
+++ android_webview/java/src/org/chromium/android_webview/AwBrowserContext.java
@@ -25,14 +25,7 @@ public class AwBrowserContext {
     private AwGeolocationPermissions mGeolocationPermissions;
     private AwCookieManager mCookieManager;
     private AwFormDatabase mFormDatabase;
-    boolean mIsFileAccessGrantedByDefault;
 
-    public AwBrowserContext(SharedPreferences sharedPreferences,
-            boolean isFileAccessGrantedByDefault) {
-        mIsFileAccessGrantedByDefault = isFileAccessGrantedByDefault;
-    }
-
-    @Deprecated
     public AwBrowserContext(SharedPreferences sharedPreferences) {
         mSharedPreferences = sharedPreferences;
     }
