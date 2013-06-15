bf15c11b05c1e068173eddf316ef7b004e9dec65
zhengchuangscu@gmail.com <zhengchuangscu@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-15 04:41:56 +0000

    |WebContentsViewWin::GetTopLevelNativeWindow()| is perhaps not correct for 3th dev
    In content layer, everywhere get the top level window from |WebContentsViewWin::GetTopLevelNativeWindow() |.
    In another way,  content has the dependence of toplevel windows, but in the impl of ||WebContentsViewWin::GetTopLevelNativeWindow() |
    we just use the hard code |getparent|.  we thinks the toplevel window must be the contents parents, it is not correct for 3th dev.
    however content is a Transparent layer for 3th dev. It is not a good designer. content should not suppose toplevel window is content view's parent.
    
    Review URL: https://chromiumcodereview.appspot.com/16323010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206548 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/web_contents/web_contents_view_win.cc
+++ content/browser/web_contents/web_contents_view_win.cc
@@ -112,7 +112,7 @@ gfx::NativeView WebContentsViewWin::GetContentNativeView() const {
 }
 
 gfx::NativeWindow WebContentsViewWin::GetTopLevelNativeWindow() const {
-  return GetParent(GetNativeView());
+  return ::GetAncestor(GetNativeView(), GA_ROOT);
 }
 
 void WebContentsViewWin::GetContainerBounds(gfx::Rect *out) const {
