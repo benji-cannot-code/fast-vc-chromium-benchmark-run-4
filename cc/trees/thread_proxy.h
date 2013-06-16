09a5873edabaf8168df5b7ee2265a611d4d78f49
ajuma@chromium.org <ajuma@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-16 03:06:58 +0000

    Estimate draw duration in SchedulerClient
    This adds a DrawDurationEstimate method to SchedulerClient. The
    implementation of this method (in ThreadProxy) produces an
    estimate based on recent draw times.
    
    This also adds UMA stats measuring the duration of each draw and
    the amount by which this duration is underestimated or
    overestimated.
    
    BUG=243459
    
    Review URL: https://chromiumcodereview.appspot.com/16574002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206624 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/thread_proxy.h
+++ cc/trees/thread_proxy.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/animation/animation_events.h"
 #include "cc/base/completion_event.h"
 #include "cc/resources/resource_update_controller.h"
+#include "cc/scheduler/rolling_time_delta_history.h"
 #include "cc/scheduler/scheduler.h"
 #include "cc/trees/layer_tree_host_impl.h"
 #include "cc/trees/proxy.h"
@@ -102,6 +103,7 @@ class ThreadProxy : public Proxy,
   virtual void ScheduledActionBeginOutputSurfaceCreation() OVERRIDE;
   virtual void ScheduledActionAcquireLayerTexturesForMainThread() OVERRIDE;
   virtual void DidAnticipatedDrawTimeChange(base::TimeTicks time) OVERRIDE;
+  virtual base::TimeDelta DrawDurationEstimate() OVERRIDE;
 
   // ResourceUpdateControllerClient implementation
   virtual void ReadyToFinalizeTextureUpdates() OVERRIDE;
@@ -243,6 +245,8 @@ class ThreadProxy : public Proxy,
 
   base::TimeTicks smoothness_takes_priority_expiration_time_;
   bool renew_tree_priority_on_impl_thread_pending_;
+
+  RollingTimeDeltaHistory draw_duration_history_;
 
   DISALLOW_COPY_AND_ASSIGN(ThreadProxy);
 };
