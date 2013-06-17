db364769bac192f6170a6b0c66a4ce3c55cefaf1
tom.cassiotis@gmail.com <tom.cassiotis@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 23:36:40 +0000

    New UndoService for multiple level undo and redo of bookmarks.
    This is a staged commit of a larger feature to allow multi-level undo, 
    redo functionality for bookmarks.
    
    This introduces an |UndoService| that is a |BrowserContextKeyedService|.
    Currently this service does not do anything, but will do so in subsequent
    commit.
    
    BUG=126092
    R=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16947008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206833 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -90,6 +90,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/global_error/global_error_service_factory.h"
 #include "chrome/browser/ui/tabs/pinned_tab_service_factory.h"
 #include "chrome/browser/ui/webui/ntp/ntp_resource_cache_factory.h"
+#include "chrome/browser/undo/undo_service_factory.h"
 #include "chrome/browser/user_style_sheet_watcher_factory.h"
 #include "chrome/browser/webdata/web_data_service_factory.h"
 
@@ -163,6 +164,7 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   BackgroundContentsServiceFactory::GetInstance();
 #endif
   BookmarkModelFactory::GetInstance();
+  UndoServiceFactory::GetInstance();
 #if defined(ENABLE_CAPTIVE_PORTAL_DETECTION)
   captive_portal::CaptivePortalServiceFactory::GetInstance();
 #endif
