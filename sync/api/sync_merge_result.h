ff53f97fe5bfa8cd1adf2b6c37388824d73da73c
haitaol@chromium.org <haitaol@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 19:42:10 +0000

    Add model version to association stats and client debug info.
    BUG=249978
    
    Review URL: https://chromiumcodereview.appspot.com/16854019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206774 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/api/sync_merge_result.h
+++ sync/api/sync_merge_result.h
@@ -35,6 +35,7 @@ class SYNC_EXPORT SyncMergeResult {
   void set_num_items_added(int num_items_added);
   void set_num_items_deleted(int num_items_deleted);
   void set_num_items_modified(int num_items_modified);
+  void set_pre_association_version(int64 version);
 
   // Getters.
   ModelType model_type() const;
@@ -44,6 +45,7 @@ class SYNC_EXPORT SyncMergeResult {
   int num_items_added() const;
   int num_items_deleted() const;
   int num_items_modified() const;
+  int64 pre_association_version() const;
 
  private:
   // Make |this| into a copy of |other|.
@@ -67,6 +69,9 @@ class SYNC_EXPORT SyncMergeResult {
   int num_items_added_;
   int num_items_deleted_;
   int num_items_modified_;
+
+  // Version of model before association.
+  int64 pre_association_version_;
 };
 
 }  // namespace syncer
