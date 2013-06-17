f10a064f56f807bb0debd1bb039975121f170218
sergiu@chromium.org <sergiu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 10:09:18 +0000

    Fix switching to a newly created managed user
    Changes 'self' to 'this' since we're in a member function (using self didn't
    work properly).
    
    R=pam@chromium.org, jhawkins@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17202003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206696 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/managed_user_create_confirm.js
+++ chrome/browser/resources/options/managed_user_create_confirm.js
@@ -136,7 +136,7 @@ cr.define('options', function() {
      * @private
      */
     setProfileInfo_: function(info) {
-      self.profileInfo_ = info;
+      this.profileInfo_ = info;
       $('managed-user-created-title').textContent =
           loadTimeData.getStringF('managedUserCreateConfirmTitle', info.name);
       $('managed-user-created-switch').textContent =
