a061e344bea2e731f21d90e951649de7262560cc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 05:18:52 +0000

    bookmarks: Remove unused includes from bookmark_node_data_mac.cc
    Since r203627 those two includes are not more necessary in that, this cleanup
    removes them.
    
    BUG=None
    TBR=sky@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17192005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206666 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/bookmarks/bookmark_node_data_mac.cc
+++ chrome/browser/bookmarks/bookmark_node_data_mac.cc
@@ -6,8 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/bookmarks/bookmark_node_data.h"
 
 #include "chrome/browser/bookmarks/bookmark_pasteboard_helper_mac.h"
-#include "chrome/browser/profiles/profile.h"
-#include "content/public/browser/browser_context.h"
 
 // static
 bool BookmarkNodeData::ClipboardContainsBookmarks() {
