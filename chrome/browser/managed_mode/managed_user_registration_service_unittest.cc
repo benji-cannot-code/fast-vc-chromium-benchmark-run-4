6e8a4c2b8442a4df25214a8aeaa7600e34e876f0
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 14:36:58 +0000

    Add support for master key upon LMU creation
    BUG=243342
    R=bauerb@chromium.org, nkostylev@chromium.org
    TBR=atwilson@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17061006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206722 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_user_registration_service_unittest.cc
+++ chrome/browser/managed_mode/managed_user_registration_service_unittest.cc
@@ -340,7 +340,8 @@ TEST_F(ManagedUserRegistrationServiceTest, MergeExisting) {
 
 TEST_F(ManagedUserRegistrationServiceTest, Register) {
   StartInitialSync();
-  service()->Register(ASCIIToUTF16("Dug"), GetRegistrationCallback());
+  service()->Register(ManagedUserRegistrationInfo(ASCIIToUTF16("Dug")),
+      GetRegistrationCallback());
   EXPECT_EQ(1u, prefs()->GetDictionary(prefs::kManagedUsers)->size());
   Acknowledge();
 
@@ -350,7 +351,8 @@ TEST_F(ManagedUserRegistrationServiceTest, Register) {
 }
 
 TEST_F(ManagedUserRegistrationServiceTest, RegisterBeforeInitialSync) {
-  service()->Register(ASCIIToUTF16("Nemo"), GetRegistrationCallback());
+  service()->Register(ManagedUserRegistrationInfo(ASCIIToUTF16("Nemo")),
+      GetRegistrationCallback());
   EXPECT_EQ(1u, prefs()->GetDictionary(prefs::kManagedUsers)->size());
   StartInitialSync();
   Acknowledge();
@@ -362,7 +364,8 @@ TEST_F(ManagedUserRegistrationServiceTest, RegisterBeforeInitialSync) {
 
 TEST_F(ManagedUserRegistrationServiceTest, Shutdown) {
   StartInitialSync();
-  service()->Register(ASCIIToUTF16("Remy"), GetRegistrationCallback());
+  service()->Register(ManagedUserRegistrationInfo(ASCIIToUTF16("Remy")),
+      GetRegistrationCallback());
   EXPECT_EQ(1u, prefs()->GetDictionary(prefs::kManagedUsers)->size());
   ResetService();
   EXPECT_EQ(0u, prefs()->GetDictionary(prefs::kManagedUsers)->size());
@@ -373,7 +376,8 @@ TEST_F(ManagedUserRegistrationServiceTest, Shutdown) {
 
 TEST_F(ManagedUserRegistrationServiceTest, StopSyncing) {
   StartInitialSync();
-  service()->Register(ASCIIToUTF16("Mike"), GetRegistrationCallback());
+  service()->Register(ManagedUserRegistrationInfo(ASCIIToUTF16("Mike")),
+      GetRegistrationCallback());
   EXPECT_EQ(1u, prefs()->GetDictionary(prefs::kManagedUsers)->size());
   service()->StopSyncing(MANAGED_USERS);
   EXPECT_EQ(0u, prefs()->GetDictionary(prefs::kManagedUsers)->size());
