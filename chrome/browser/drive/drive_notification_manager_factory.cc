0d731e201c6a07d4f6531591155346dba1d2dae0
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 08:47:51 +0000

    drive: Introduce chrome/browser/drive
    This directory will contain utility code shared by SyncFileSystem
    and Google Drive integration on Chrome OS. The shared utility code
    has been placed in chrome/browser/google_apis, but it wasn't the
    right place as c/b/google_apis was meant to contain low level code
    for communicating with the Google API servers. This is why we had
    lots of exceptions in c/b/google_apis/DEPS. These exceptions will
    be all gone once the move is complete.
    
    Move DriveNotificationManager as a first step.
    
    BUG=244325
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/17137003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206689 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drive/drive_notification_manager_factory.cc
+++ chrome/browser/drive/drive_notification_manager_factory.cc
@@ -3,9 +3,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/google_apis/drive_notification_manager_factory.h"
+#include "chrome/browser/drive/drive_notification_manager_factory.h"
 
-#include "chrome/browser/google_apis/drive_notification_manager.h"
+#include "chrome/browser/drive/drive_notification_manager.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
