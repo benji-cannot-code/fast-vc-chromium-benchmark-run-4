a2df40dfb9159bd98ce3643c1578ea07ee590d60
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 18:27:56 +0000

    Disable ExtensionApiTest.TabOnRemoved on Windows.
    The test was disabled on linux and mac long time ago. Now it is flaking
    on Windows as well.
    
    BUG=59198
    TBR=tim@chromium.org
    
    Review URL: https://codereview.chromium.org/17289004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206758 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -15,12 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/url_constants.h"
 #include "net/dns/mock_host_resolver.h"
 
-// Possible race in ChromeURLDataManager. http://crbug.com/59198
-#if defined(OS_MACOSX) || defined(OS_LINUX)
-#define MAYBE_TabOnRemoved DISABLED_TabOnRemoved
-#else
-#define MAYBE_TabOnRemoved TabOnRemoved
-#endif
 
 // Window resizes are not completed by the time the callback happens,
 // so these tests fail on linux/gtk. http://crbug.com/72369
@@ -151,7 +145,8 @@ IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_TabConnect) {
   ASSERT_TRUE(RunExtensionTest("tabs/connect")) << message_;
 }
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, MAYBE_TabOnRemoved) {
+// Possible race in ChromeURLDataManager. http://crbug.com/59198
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_TabOnRemoved) {
   ASSERT_TRUE(RunExtensionTest("tabs/on_removed")) << message_;
 }
 
