000794d606abbdb0ce507ecb1871cf0fe4f29953
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 09:43:11 +0000

    drive: Stop persisting 'mounted' state of cache entries
    There is no need to persist this state to the next boot.
    Fix FileCache::Remove's behavior to return FILE_ERROR_IN_USE when the entry is dirty or mounted.
    
    BUG=160487
    TEST=unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/17052002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206694 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/drive_internals.js
+++ chrome/browser/resources/chromeos/drive_internals.js
@@ -82,7 +82,6 @@ function updateCacheContents(cacheEntry) {
   tr.appendChild(createElementFromText('td', cacheEntry.is_present));
   tr.appendChild(createElementFromText('td', cacheEntry.is_pinned));
   tr.appendChild(createElementFromText('td', cacheEntry.is_dirty));
-  tr.appendChild(createElementFromText('td', cacheEntry.is_mounted));
 
   $('cache-contents').appendChild(tr);
 }
