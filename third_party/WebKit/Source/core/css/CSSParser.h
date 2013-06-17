bf2f0ef1820576d5718d732a83d7744c1d8e682c
serya@chromium.org <serya@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-17 11:56:16 +0000

    Generating CSS warnings for @-rule headers.
    Warnings for the @namespace rule are not generated for now.
    
    BUG=140608
    
    Review URL: https://chromiumcodereview.appspot.com/17057006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152543 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser.h
+++ third_party/WebKit/Source/core/css/CSSParser.h
@@ -454,6 +454,9 @@ private:
     inline CharacterType* tokenStart();
 
     template <typename CharacterType>
+    inline CharacterType* dataStart();
+
+    template <typename CharacterType>
     inline void setTokenStart(CharacterType*);
 
     inline unsigned tokenStartOffset();
