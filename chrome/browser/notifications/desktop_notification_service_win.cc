ba2ad38b9fd1cb81b587cabbaee5394a4be846d7
shrikant@chromium.org <shrikant@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 18:00:45 +0000

    Fixing issue of invalid number of parameters. Found this issue while debugging reported crash. This could be likely cause of the crash, which is not repro at this time on my VM with outlined steps.
    To test fixed issue:
    Open chrome in metro mode with a html page which shows notification like following.
      var notification = webkitNotifications.createNotification("", "Notification Created", "Notification created", "");
      notification.show();
    
    BUG=240260
    Test=As outlined in description above.
    R=cpu,ananta,dewittj
    
    Review URL: https://chromiumcodereview.appspot.com/16865017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206750 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notification_service_win.cc
+++ chrome/browser/notifications/desktop_notification_service_win.cc
@@ -13,21 +13,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification_ui_manager.h"
 #include "win8/util/win8_util.h"
 
-typedef void (*MetroDisplayNotification)(
-    const char* origin_url, const char* icon_url, const wchar_t* title,
-    const wchar_t* body, const wchar_t* display_source,
-    const char* notification_id);
-
-typedef bool (*MetroCancelNotification)(const char* notification_id);
-
 bool DesktopNotificationService::CancelDesktopNotification(
     int process_id, int route_id, int notification_id) {
   scoped_refptr<NotificationObjectProxy> proxy(
       new NotificationObjectProxy(process_id, route_id, notification_id,
                                   false));
   if (win8::IsSingleWindowMetroMode()) {
-    MetroCancelNotification cancel_metro_notification =
-        reinterpret_cast<MetroCancelNotification>(GetProcAddress(
+    base::win::MetroCancelNotification cancel_metro_notification =
+        reinterpret_cast<base::win::MetroCancelNotification>(GetProcAddress(
             base::win::GetMetroModule(), "CancelNotification"));
     DCHECK(cancel_metro_notification);
     if (cancel_metro_notification(proxy->id().c_str()))
@@ -39,8 +32,8 @@ bool DesktopNotificationService::CancelDesktopNotification(
 void DesktopNotificationService::ShowNotification(
     const Notification& notification) {
   if (win8::IsSingleWindowMetroMode()) {
-    MetroDisplayNotification display_metro_notification =
-        reinterpret_cast<MetroDisplayNotification>(GetProcAddress(
+    base::win::MetroNotification display_metro_notification =
+        reinterpret_cast<base::win::MetroNotification>(GetProcAddress(
             base::win::GetMetroModule(), "DisplayNotification"));
     DCHECK(display_metro_notification);
     if (!notification.is_html()) {
@@ -49,7 +42,8 @@ void DesktopNotificationService::ShowNotification(
                                  notification.title().c_str(),
                                  notification.message().c_str(),
                                  notification.display_source().c_str(),
-                                 notification.notification_id().c_str());
+                                 notification.notification_id().c_str(),
+                                 NULL, NULL);
       return;
     } else {
       NOTREACHED() << "We don't support HTML notifications in Windows 8 metro";
