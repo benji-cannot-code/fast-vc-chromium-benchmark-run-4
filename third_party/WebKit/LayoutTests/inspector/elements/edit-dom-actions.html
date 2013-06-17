benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 306d5063f2648a7def2c7b46305dc91d9e1033af

DevTools: Retain attribute casing when editing them in the DOM tree.
Parse the temporary SPAN element with attributes as XML into a DocumentFragment
created against the context element's document,
since parsing as HTML lowercases attribute names.

BUG=237338
R=vsevik@chromium.org

Review URL: https://codereview.chromium.org/14761008

git-svn-id: svn://svn.chromium.org/blink/trunk@150320 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -701,38 +701,39 @@ void InspectorDOMAgent::setAttributesAsText(ErrorString* errorString, int elemen
     if (!element)
         return;
 
-    RefPtr<HTMLElement> parsedElement = createHTMLElement(element->document(), spanTag);
-    ExceptionCode ec = 0;
-    parsedElement.get()->setInnerHTML("<span " + text + "></span>", ec);
-    if (ec) {
-        *errorString = InspectorDOMAgent::toErrorString(ec);
-        return;
-    }
+    String markup = "<span " + text + "></span>";
+    RefPtr<DocumentFragment> fragment = element->document()->createDocumentFragment();
+    fragment->parseXML(markup, 0, DisallowScriptingContent);
 
-    Node* child = parsedElement->firstChild();
-    if (!child) {
+    Element* parsedElement = fragment->firstChild() && fragment->firstChild()->isElementNode() ? toElement(fragment->firstChild()) : 0;
+    if (!parsedElement) {
         *errorString = "Could not parse value as attributes";
         return;
     }
 
-    Element* childElement = toElement(child);
-    if (!childElement->hasAttributes() && name) {
-        m_domEditor->removeAttribute(element, *name, errorString);
+    bool shouldIgnoreCase = element->document()->isHTMLDocument() && element->isHTMLElement();
+    String caseAdjustedName = name ? (shouldIgnoreCase ? name->lower() : *name) : String();
+
+    if (!parsedElement->hasAttributes() && name) {
+        m_domEditor->removeAttribute(element, caseAdjustedName, errorString);
         return;
     }
 
     bool foundOriginalAttribute = false;
-    unsigned numAttrs = childElement->attributeCount();
+    unsigned numAttrs = parsedElement->attributeCount();
     for (unsigned i = 0; i < numAttrs; ++i) {
         // Add attribute pair
-        const Attribute* attribute = childElement->attributeItem(i);
-        foundOriginalAttribute = foundOriginalAttribute || (name && attribute->name().toString() == *name);
-        if (!m_domEditor->setAttribute(element, attribute->name().toString(), attribute->value(), errorString))
+        const Attribute* attribute = parsedElement->attributeItem(i);
+        String attributeName = attribute->name().toString();
+        if (shouldIgnoreCase)
+            attributeName = attributeName.lower();
+        foundOriginalAttribute |= name && attributeName == caseAdjustedName;
+        if (!m_domEditor->setAttribute(element, attributeName, attribute->value(), errorString))
             return;
     }
 
     if (!foundOriginalAttribute && name && !name->stripWhiteSpace().isEmpty())
-        m_domEditor->removeAttribute(element, *name, errorString);
+        m_domEditor->removeAttribute(element, caseAdjustedName, errorString);
 }
 
 void InspectorDOMAgent::removeAttribute(ErrorString* errorString, int elementId, const String& name)

third_party/WebKit/LayoutTests/inspector-protocol/dom-svg-attribute-case-expected.txt
@@ -1,0 +1,11 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+Test that DOM attribute case is preserved when modified in XML documents. 
+Original attributes:
+id=main
+xmlns=http://www.w3.org/2000/svg
+width=600
+height=500
+viewBox=0 0 100 120
+Modified attribute:
+viewBox=0 0 120 120
+

third_party/WebKit/LayoutTests/inspector-protocol/dom-svg-attribute-case.html
@@ -1,0 +1,88 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<html>
+<head>
+<script type="text/javascript" src="../http/tests/inspector-protocol/resources/protocol-test.js"></script>
+<script>
+
+function test()
+{
+    var rootNodeId;
+    var nodeId;
+
+    InspectorTest.eventHandler["DOM.attributeModified"] = onAttributeModified;
+
+    InspectorTest.sendCommand("DOM.getDocument", {}, onGotDocument);
+
+    function onGotDocument(msg)
+    {
+        if (msg.error) {
+            InspectorTest.log(msg.error.message);
+            InspectorTest.completeTest();
+            return;
+        }
+        rootNodeId = msg.result.root.nodeId;
+        getMainNodeId();
+    }
+
+    function getMainNodeId(next)
+    {
+        InspectorTest.sendCommand("DOM.querySelector", { "nodeId": rootNodeId, "selector": "#main" }, onQuery);
+        function onQuery(msg)
+        {
+            if (!checkError(msg))
+                return;
+            nodeId = msg.result.nodeId;
+            onGotMainNodeId();
+        }
+    }
+
+    function onGotMainNodeId()
+    {
+        InspectorTest.log("Original attributes:");
+        dumpMainElementAttributes(onDumpedOriginal);
+    }
+
+    function onDumpedOriginal()
+    {
+        InspectorTest.sendCommand("DOM.setAttributesAsText", { "nodeId": nodeId, "name": "viewBox", "text": "viewBox=\"0 0 120 120\"" });
+    }
+
+    function onAttributeModified(msg) {
+        var result = msg.params;
+        InspectorTest.log("Modified attribute:");
+        InspectorTest.log(result.name + "=" + result.value);
+        InspectorTest.completeTest();
+    }
+
+    function dumpMainElementAttributes(next)
+    {
+        InspectorTest.sendCommand("DOM.getAttributes", { "nodeId": nodeId }, onAttributes);
+
+        function onAttributes(msg)
+        {
+            if (!checkError(msg))
+                return;
+            var array = msg.result.attributes;
+            for (var i = 0; i < array.length; i += 2)
+                InspectorTest.log(array[i] + "=" + array[i + 1]);
+            next();
+        }
+    }
+
+    function checkError(msg)
+    {
+        if (msg.error) {
+            InspectorTest.log(msg.error.message);
+            InspectorTest.completeTest();
+            return false;
+        }
+        return true;
+    }
+}
+</script>
+</head>
+<body onload="runTest()">
+Test that DOM attribute case is preserved when modified in XML documents.
+<svg id="main" xmlns="http://www.w3.org/2000/svg" width="600" height="500" viewBox="0 0 100 120" />
+</body>
+</html>

commit 063a1f9a083f790a5449d34a12adc1f9cfee681c

DevTools: Accept unquoted attribute values in the Elements panel for HTML elements
This regressed in r150320.

R=pfeldman, vsevik, yurys@chromium.org
BUG=249628

Review URL: https://chromiumcodereview.appspot.com/17244002

git-svn-id: svn://svn.chromium.org/blink/trunk@152545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

third_party/WebKit/Source/core/inspector/InspectorDOMAgent.cpp
@@ -678,7 +678,13 @@ void InspectorDOMAgent::setAttributesAsText(ErrorString* errorString, int elemen
 
     String markup = "<span " + text + "></span>";
     RefPtr<DocumentFragment> fragment = element->document()->createDocumentFragment();
-    fragment->parseXML(markup, 0, DisallowScriptingContent);
+
+    bool shouldIgnoreCase = element->document()->isHTMLDocument() && element->isHTMLElement();
+    // Not all elements can represent the context (i.e. IFRAME), hence using document.body.
+    if (shouldIgnoreCase && element->document()->body())
+        fragment->parseHTML(markup, element->document()->body(), DisallowScriptingContent);
+    else
+        fragment->parseXML(markup, 0, DisallowScriptingContent);
 
     Element* parsedElement = fragment->firstChild() && fragment->firstChild()->isElementNode() ? toElement(fragment->firstChild()) : 0;
     if (!parsedElement) {
@@ -686,7 +692,6 @@ void InspectorDOMAgent::setAttributesAsText(ErrorString* errorString, int elemen
         return;
     }
 
-    bool shouldIgnoreCase = element->document()->isHTMLDocument() && element->isHTMLElement();
     String caseAdjustedName = name ? (shouldIgnoreCase ? name->lower() : *name) : String();
 
     if (!parsedElement->hasAttributes() && name) {

third_party/WebKit/LayoutTests/inspector/elements/edit-dom-actions-expected.txt
@@ -78,6 +78,16 @@ Running: testAddAttribute
       <div id="node-to-add-attribute" newattr="new-value"></div>
   </div>
 
+Running: testAddAttributeUnquotedValue
+==== before ====
+- <div id="testAddAttributeUnquotedValue">
+      <div id="node-to-add-attribute-unquoted-value"></div>
+  </div>
+==== after ====
+- <div id="testAddAttributeUnquotedValue">
+      <div id="node-to-add-attribute-unquoted-value" newattr="unquotedValue"></div>
+  </div>
+
 Running: testEditCommentAsHTML
 ==== before ====
 - <div id="testEditCommentAsHTML">

third_party/WebKit/LayoutTests/inspector/elements/edit-dom-actions.html
@@ -82,23 +82,12 @@ function test()
 
         function testAddAttribute(next)
         {
-            domActionTestForNodeId("testAddAttribute", "node-to-add-attribute", testBody, next);
-         
-            function testBody(node, done)
-            {
-                var editorElement = editNodePart(node, "webkit-html-attribute");
-                editorElement.dispatchEvent(InspectorTest.createKeyEvent("U+0009")); // Tab
-                
-                InspectorTest.runAfterPendingDispatches(testContinuation);
-                
-                function testContinuation()
-                {
-                    var editorElement = window.getSelection().anchorNode.parentElement;
-                    editorElement.textContent = "newattr=\"new-value\"";
-                    editorElement.dispatchEvent(InspectorTest.createKeyEvent("Enter"));
-                    InspectorTest.addSniffer(WebInspector.ElementsTreeUpdater.prototype, "_updateModifiedNodes", done);
-                }
-            }
+            doAddAttribute("testAddAttribute", "node-to-add-attribute", "newattr=\"new-value\"", next);
+        },
+
+        function testAddAttributeUnquotedValue(next)
+        {
+            doAddAttribute("testAddAttributeUnquotedValue", "node-to-add-attribute-unquoted-value", "newattr=unquotedValue", next);
         },
 
         function testEditCommentAsHTML(next)
@@ -191,6 +180,27 @@ function test()
             }
         }
     ]);
+
+    function doAddAttribute(testName, dataNodeId, attributeText, next)
+    {
+        domActionTestForNodeId(testName, dataNodeId, testBody, next);
+
+        function testBody(node, done)
+        {
+            var editorElement = editNodePart(node, "webkit-html-attribute");
+            editorElement.dispatchEvent(InspectorTest.createKeyEvent("U+0009")); // Tab
+
+            InspectorTest.runAfterPendingDispatches(testContinuation);
+
+            function testContinuation()
+            {
+                var editorElement = window.getSelection().anchorNode.parentElement;
+                editorElement.textContent = attributeText;
+                editorElement.dispatchEvent(InspectorTest.createKeyEvent("Enter"));
+                InspectorTest.addSniffer(WebInspector.ElementsTreeUpdater.prototype, "_updateModifiedNodes", done);
+            }
+        }
+    }
 
     function domActionTestForNodeId(testName, dataNodeId, testBody, next)
     {
@@ -286,6 +296,10 @@ Tests that user can mutate DOM by means of elements panel.
 
     <div id="testAddAttribute">
         <div id="node-to-add-attribute"></div>
+    </div>
+
+    <div id="testAddAttributeUnquotedValue">
+        <div id="node-to-add-attribute-unquoted-value"></div>
     </div>
 
     <div id="testEditAsHTML">