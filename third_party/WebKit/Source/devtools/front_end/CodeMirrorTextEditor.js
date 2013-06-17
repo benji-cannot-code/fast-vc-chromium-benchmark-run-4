bca5198ec7a5a2707872c446503523c7a0e4d714
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-17 08:58:05 +0000

    DevTools: [CodeMirror] highlight tab characters
    This patch adds highlight for tab characters in CodeMirrorTextEditor
    in case the "show whitespace characters" option is on.
    
    BUG=
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/15659016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152529 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
@@ -156,7 +156,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
 
     _setupWhitespaceHighlight: function()
     {
-        if (WebInspector.CodeMirrorTextEditor._whitespaceStyleInjected)
+        if (WebInspector.CodeMirrorTextEditor._whitespaceStyleInjected || !WebInspector.settings.showWhitespacesInEditor.get())
             return;
         WebInspector.CodeMirrorTextEditor._whitespaceStyleInjected = true;
         const classBase = ".cm-whitespace-";
@@ -168,6 +168,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
             var rule = classBase + i + "::before { content: '" + spaceChars + "';}\n";
             rules += rule;
         }
+        rules += ".cm-tab:before { display: block !important; }\n";
         var style = document.createElement("style");
         style.textContent = rules;
         document.head.appendChild(style);
