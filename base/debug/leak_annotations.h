ccc9c6b29cfecb0c09b8a60845826ade23019267
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-17 19:08:06 +0000

    Fix LeakSanitizer annotations for NaCl.
    NaCl code is not built with LSan runtime, so annotations should not be used.
    
    R=glider@chromium.org, jar@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/16845006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206766 0039d316-1c4b-4281-b951-d872f2087c98

--- base/debug/leak_annotations.h
+++ base/debug/leak_annotations.h
@@ -34,7 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define ANNOTATE_LEAKING_OBJECT_PTR(X) \
     HeapLeakChecker::IgnoreObject(X)
 
-#elif defined(LEAK_SANITIZER)
+#elif defined(LEAK_SANITIZER) && !defined(OS_NACL)
 
 extern "C" {
 void __lsan_disable();
