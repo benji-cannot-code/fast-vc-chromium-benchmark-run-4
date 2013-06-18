d7fc8cd2b6375e8b4c57ef56a3afa891ea26ee95
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 21:23:44 +0000

    Re-enable AwContents create/destroy stress tests
    This was disabled awaiting new rendering mode; now we have that,
    re-enable it.
    
    NOTRY=true
    BUG=176940
    
    Review URL: https://chromiumcodereview.appspot.com/17162004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207088 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwContentsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwContentsTest.java
@@ -124,12 +124,8 @@ public class AwContentsTest extends AwTestBase {
         destroyAwContentsOnMainSync(awTestContainerView.getAwContents());
     }
 
-    /*
-     * @LargeTest
-     * @Feature({"AndroidWebView"})
-     * Disabled until we switch to final rendering pipeline.
-     */
-    @DisabledTest
+    @LargeTest
+    @Feature({"AndroidWebView"})
     public void testCreateLoadDestroyManyTimes() throws Throwable {
         final int CREATE_AND_DESTROY_REPEAT_COUNT = 10;
         for (int i = 0; i < CREATE_AND_DESTROY_REPEAT_COUNT; ++i) {
@@ -141,12 +137,8 @@ public class AwContentsTest extends AwTestBase {
         }
     }
 
-    /*
-     * @LargeTest
-     * @Feature({"AndroidWebView"})
-     * Disabled until we switch to final rendering pipeline.
-     */
-    @DisabledTest
+    @LargeTest
+    @Feature({"AndroidWebView"})
     public void testCreateLoadDestroyManyAtOnce() throws Throwable {
         final int CREATE_AND_DESTROY_REPEAT_COUNT = 10;
         AwTestContainerView views[] = new AwTestContainerView[CREATE_AND_DESTROY_REPEAT_COUNT];
