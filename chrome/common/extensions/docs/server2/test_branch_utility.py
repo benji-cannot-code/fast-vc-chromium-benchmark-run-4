6cad93c737ee90daeb71749d4d39908b365f0f0f
evan.peterson.EP@gmail.com <evan.peterson.EP@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 01:43:50 +0000

    This is the first of several smaller patches that will be created from the larger intro-tables patch (found here: https://codereview.chromium.org/12996003/).
    This patch contains changes pertaining to the BranchUtility class.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16443005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206871 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/test_branch_utility.py
+++ chrome/common/extensions/docs/server2/test_branch_utility.py
@@ -3,11 +3,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from branch_utility import BranchUtility
+from branch_utility import BranchUtility, ChannelInfo
 
 class TestBranchUtility(object):
   '''Mimics BranchUtility to return valid-ish data without needing omahaproxy
   data.
   '''
-  def GetBranchForChannel(self, channel_name):
-    return channel_name
+  def GetAllChannelInfo(self):
+    return [self.GetChannelInfo(channel)
+            for channel in BranchUtility.GetAllChannelNames()]
+
+  def GetChannelInfo(self, channel):
+    return ChannelInfo(channel,
+                       'fakebranch-%s' % channel,
+                       'fakeversion-%s' % channel)
+
+  def GetBranchForVersion(self, version):
+    return 'fakebranch-%s' % version
