7e18a0a7ee1db2d3de60017ad4636c1bb0b2585c
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-18 07:29:52 +0000

    [HTML Imports] Make HTMLLinkElement.import a Document.
    This follows spec change on https://www.w3.org/Bugs/Public/show_bug.cgi?id=22305
    which asks imported fragment should be imported "document" to allow it to have
    URL and other APIs.
    
    BUG=240592
    TEST=imported-document.html
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/16936002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152614 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImportsController.h
+++ third_party/WebKit/Source/core/html/HTMLImportsController.h
@@ -67,7 +67,7 @@ public:
     virtual Type type() const OVERRIDE { return Import; }
     virtual void ownerRemoved() OVERRIDE;
 
-    DocumentFragment* importedFragment() const;
+    Document* importedDocument() const;
     const KURL& url() const { return m_url; }
     void importDestroyed();
     bool isDone() const { return m_state == StateReady || m_state == StateError; }
@@ -85,7 +85,7 @@ private:
     KURL m_url;
     State m_state;
     CachedResourceHandle<CachedScript> m_resource;
-    RefPtr<DocumentFragment> m_importedFragment;
+    RefPtr<Document> m_importedDocument;
 };
 
 
@@ -99,7 +99,6 @@ public:
 
     void addImport(PassRefPtr<LinkImport>);
     void showSecurityErrorMessage(const String&);
-    PassRefPtr<DocumentFragment> createDocumentFragment() const;
     PassRefPtr<LinkImport> findLinkFor(const KURL&) const;
     SecurityOrigin* securityOrigin() const;
     bool haveLoaded() const;
@@ -108,7 +107,6 @@ public:
 private:
 
     Document* m_master;
-    RefPtr<Document> m_importedFragmentOwner;
 
     // List of import which has been loaded or being loaded.
     typedef Vector<RefPtr<LinkImport> > ImportList;
