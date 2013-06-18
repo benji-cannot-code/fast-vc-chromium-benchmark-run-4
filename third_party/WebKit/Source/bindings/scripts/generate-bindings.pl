7174ddbf255f38e632753bff1a511625a2c42f50
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-18 18:25:40 +0000

    Implement WebIDL implements
    Implement basic support for Web IDL implements statements in the
    bindings generator:
    http://www.w3.org/TR/WebIDL/#dfn-implements-statement
    
    Introduce ParentNode interface and refactor Element, Document and
    and DocumentFragment to use implements and match the spec:
    http://dom.spec.whatwg.org/#parentnode
    
    BUG=239236
    
    Review URL: https://chromiumcodereview.appspot.com/17376006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152655 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/generate-bindings.pl
+++ third_party/WebKit/Source/bindings/scripts/generate-bindings.pl
@@ -135,7 +135,7 @@ foreach my $idlFile (@supplementedIdlFiles) {
     my $document = $parser->Parse($idlFile, $defines, $preprocessor);
 
     foreach my $interface (@{$document->interfaces}) {
-        if ($interface->isPartial and $interface->name eq $targetInterfaceName) {
+        if (!$interface->isPartial || $interface->name eq $targetInterfaceName) {
             my $targetDataNode;
             foreach my $interface (@{$targetDocument->interfaces}) {
                 if ($interface->name eq $targetInterfaceName) {
@@ -148,7 +148,7 @@ foreach my $idlFile (@supplementedIdlFiles) {
             # Support for attributes of partial interfaces.
             foreach my $attribute (@{$interface->attributes}) {
                 # Record that this attribute is implemented by $interfaceName.
-                $attribute->extendedAttributes->{"ImplementedBy"} = $interfaceName;
+                $attribute->extendedAttributes->{"ImplementedBy"} = $interfaceName if $interface->isPartial;
 
                 # Add interface-wide extended attributes to each attribute.
                 applyInterfaceExtendedAttributes($interface, $attribute->extendedAttributes);
@@ -159,7 +159,7 @@ foreach my $idlFile (@supplementedIdlFiles) {
             # Support for methods of partial interfaces.
             foreach my $function (@{$interface->functions}) {
                 # Record that this method is implemented by $interfaceName.
-                $function->extendedAttributes->{"ImplementedBy"} = $interfaceName;
+                $function->extendedAttributes->{"ImplementedBy"} = $interfaceName if $interface->isPartial;
 
                 # Add interface-wide extended attributes to each method.
                 applyInterfaceExtendedAttributes($interface, $function->extendedAttributes);
@@ -170,7 +170,7 @@ foreach my $idlFile (@supplementedIdlFiles) {
             # Support for constants of partial interfaces.
             foreach my $constant (@{$interface->constants}) {
                 # Record that this constant is implemented by $interfaceName.
-                $constant->extendedAttributes->{"ImplementedBy"} = $interfaceName;
+                $constant->extendedAttributes->{"ImplementedBy"} = $interfaceName if $interface->isPartial;
 
                 # Add interface-wide extended attributes to each constant.
                 applyInterfaceExtendedAttributes($interface, $constant->extendedAttributes);
--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -86,6 +86,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'dom/Notation.idl',
             'dom/OverflowEvent.idl',
             'dom/PageTransitionEvent.idl',
+            'dom/ParentNode.idl',
             'dom/PopStateEvent.idl',
             'dom/ProcessingInstruction.idl',
             'dom/ProgressEvent.idl',
