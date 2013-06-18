7c040d620877bed5c805d9c094087108fe767369
peria@chromium.org <peria@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 14:46:05 +0000

    Add memory internals' URL in chrome://about
    BUG=168642
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/17230002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206993 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browser_about_handler.cc
+++ chrome/browser/browser_about_handler.cc
@@ -31,6 +31,7 @@ const char* const kPaths[] = {
   chrome::kChromeUIHistoryHost,
   chrome::kChromeUIIPCHost,
   chrome::kChromeUIMemoryHost,
+  chrome::kChromeUIMemoryInternalsHost,
 #if defined(OS_ANDROID) || defined(OS_IOS)
   chrome::kChromeUINetExportHost,
 #endif
