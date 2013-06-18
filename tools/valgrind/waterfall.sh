24204d6ac83b8a1ea3f7cff2547c4d72e7da1c25
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 13:52:57 +0000

    [Memory Sheriff]
    Modified waterfall.sh to pass arguments on to match_suppressions.
    
    Needed for new features (top-symbols/symbol-filter)
    
    R=thestig@chromium.org
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/17286004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206976 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/valgrind/waterfall.sh
+++ tools/valgrind/waterfall.sh
@@ -178,30 +178,33 @@ EOF
 NUMBUILDS=3
 
 CMD=$1
-shift
-
-while getopts “hn:” OPTION
-do
-     case $OPTION in
-         h)
-             usage
-             exit
-             ;;
-         n)
-             NUMBUILDS=$OPTARG
-             ;;
-         ?)
-             usage
-             exit
-             ;;
-     esac
-done
-
-shift $((OPTIND-1))
-
 if [ $# != 0 ]; then
-  usage
-  exit 1
+  shift
+fi
+
+# Arguments for "match" are handled in match_suppressions
+if [ "$CMD" != "match" ]; then
+  while getopts “hn:” OPTION
+  do
+    case $OPTION in
+      h)
+        usage
+        exit
+        ;;
+      n)
+        NUMBUILDS=$OPTARG
+        ;;
+      ?)
+        usage
+        exit
+        ;;
+    esac
+  done
+  shift $((OPTIND-1))
+  if [ $# != 0 ]; then
+    usage
+    exit 1
+  fi
 fi
 
 if [ "$CMD" = "fetch" ]; then
@@ -209,7 +212,7 @@ if [ "$CMD" = "fetch" ]; then
   fetch_logs $WATERFALL_PAGE
   fetch_logs $WATERFALL_FYI_PAGE
 elif [ "$CMD" = "match" ]; then
-  match_suppressions ${@:2}
+  match_suppressions $@
   match_gtest_excludes
 elif [ "$CMD" = "blame" ]; then
   echo The blame command died of bitrot. If you need it, please reimplement it.
