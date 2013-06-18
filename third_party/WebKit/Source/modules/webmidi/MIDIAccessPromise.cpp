f2dbfed28e5cc3ead3f2e119de800b890be0a300
toyoshim@chromium.org <toyoshim@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-18 05:56:11 +0000

    Web MIDI API: fix an assertion failure on debug build
    MIDIAccessPromise called ActiveDOMObject::setPendingActivity in it's
    constructor, but it was dangerous and resulted in an assertion failure.
    
    TEST=LayoutTests/webmidi/request-midi-access.html
    BUG=248973
    
    Review URL: https://chromiumcodereview.appspot.com/17223007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152612 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIAccessPromise.cpp
+++ third_party/WebKit/Source/modules/webmidi/MIDIAccessPromise.cpp
@@ -42,6 +42,7 @@ namespace WebCore {
 PassRefPtr<MIDIAccessPromise> MIDIAccessPromise::create(ScriptExecutionContext* context, const Dictionary& options)
 {
     RefPtr<MIDIAccessPromise> midiAccessPromise(adoptRef(new MIDIAccessPromise(context, options)));
+    midiAccessPromise->setPendingActivity(midiAccessPromise.get());
     midiAccessPromise->suspendIfNeeded();
     return midiAccessPromise.release();
 }
@@ -53,7 +54,6 @@ MIDIAccessPromise::MIDIAccessPromise(ScriptExecutionContext* context, const Dict
     , m_access(MIDIAccess::create(context, this))
 {
     ScriptWrappable::init(this);
-    setPendingActivity(this);
 }
 
 MIDIAccessPromise::~MIDIAccessPromise()
