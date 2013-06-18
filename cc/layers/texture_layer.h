0b7c6e2ace2b76c3ca434a89200ecda0d731e080
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 21:12:43 +0000

    Reland r206537 - cc: Don't return mailboxes if the TextureLayer is removed from the tree.
    When the TextureLayer is removed from the tree, the TextureLayerImpl is
    destroyed, so it will return the mailbox to the main thread. Prior to this patch
    we would return the mailbox to the client. When re-attaching the TextureLayer,
    we would then not push the mailbox to the new TextureLayerImpl, and stop drawing
    the layer.
    Instead, keep the mailbox on the TextureLayer. If the TextureLayer is re-attached to the tree, we
    push the mailbox again (which is ok to do now).
    Only after the impl side has returned the mailbox *and* the mailbox is replaced
    on the TextureLayer (or it is destroyed), will we call the callback.
    
    To do that we add a refcounted MailboxHolder whose job is to call the callback
    when it's destroyed. It's referenced by the TextureLayer itself, while being the
    current mailbox, and the closures we give to the impl thread.
        
    BUG=249535
    
    Review URL: https://chromiumcodereview.appspot.com/16888015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207081 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/texture_layer.h
+++ cc/layers/texture_layer.h
@@ -15,6 +15,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit { class WebGraphicsContext3D; }
 
+namespace base {
+class MessageLoopProxy;
+}
+
 namespace cc {
 
 class TextureLayerClient;
@@ -85,6 +89,50 @@ class CC_EXPORT TextureLayer : public Layer {
   virtual ~TextureLayer();
 
  private:
+  class MailboxHolder : public base::RefCountedThreadSafe<MailboxHolder> {
+   public:
+    class MainThreadReference {
+     public:
+      explicit MainThreadReference(MailboxHolder* holder);
+      ~MainThreadReference();
+      MailboxHolder* holder() { return holder_; }
+
+     private:
+      scoped_refptr<MailboxHolder> holder_;
+      DISALLOW_COPY_AND_ASSIGN(MainThreadReference);
+    };
+
+    static scoped_ptr<MainThreadReference> Create(
+        const TextureMailbox& mailbox);
+
+    const TextureMailbox& mailbox() const { return mailbox_; }
+    void Return(unsigned sync_point, bool is_lost);
+
+    // Gets a ReleaseCallback that can be called from another thread. Note: the
+    // caller must ensure the callback is called.
+    TextureMailbox::ReleaseCallback GetCallbackForImplThread();
+
+   private:
+    friend class base::RefCountedThreadSafe<MailboxHolder>;
+    friend class MainThreadReference;
+    explicit MailboxHolder(const TextureMailbox& mailbox);
+    ~MailboxHolder();
+    void InternalAddRef();
+    void InternalRelease();
+    void ReturnAndReleaseOnMainThread(unsigned sync_point, bool is_lost);
+    void ReturnAndReleaseOnImplThread(unsigned sync_point, bool is_lost);
+
+    // Thread safety notes: except for the thread-safe message_loop_, all fields
+    // are only used on the main thread, or on the impl thread during commit
+    // where the main thread is blocked.
+    const scoped_refptr<base::MessageLoopProxy> message_loop_;
+    unsigned internal_references_;
+    TextureMailbox mailbox_;
+    unsigned sync_point_;
+    bool is_lost_;
+    DISALLOW_COPY_AND_ASSIGN(MailboxHolder);
+  };
+
   TextureLayerClient* client_;
   bool uses_mailbox_;
 
@@ -99,8 +147,8 @@ class CC_EXPORT TextureLayer : public Layer {
   bool content_committed_;
 
   unsigned texture_id_;
-  TextureMailbox texture_mailbox_;
-  bool own_mailbox_;
+  scoped_ptr<MailboxHolder::MainThreadReference> holder_ref_;
+  bool needs_set_mailbox_;
 
   DISALLOW_COPY_AND_ASSIGN(TextureLayer);
 };
