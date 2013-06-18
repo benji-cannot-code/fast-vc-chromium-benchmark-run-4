72af35f687b5459ae473c843b987bdee74015050
groby@chromium.org <groby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 14:01:43 +0000

    [Memory Sheriff] Ensure MessageLoop is run.
    SuggestExactInput didn't run the MessageLoop, which left a bunch
    of tasks in the work queue. TestBrowserThreadBundle both ensures
    that the queue is emptied, and takes care of setting up both
    MessageLoop and necessary threads.
    
    R=sky@chromium.org
    BUG=250946
    
    Review URL: https://chromiumcodereview.appspot.com/17106008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/history_url_provider_unittest.cc
+++ chrome/browser/autocomplete/history_url_provider_unittest.cc
@@ -24,13 +24,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/test/base/testing_browser_process.h"
 #include "chrome/test/base/testing_profile.h"
-#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 using base::Time;
 using base::TimeDelta;
 
-using content::BrowserThread;
+using content::TestBrowserThreadBundle;
 
 struct TestURLInfo {
   const char* url;
@@ -134,9 +134,7 @@ class HistoryURLProviderTest : public testing::Test,
                                public AutocompleteProviderListener {
  public:
   HistoryURLProviderTest()
-      : ui_thread_(BrowserThread::UI, &message_loop_),
-        file_thread_(BrowserThread::FILE, &message_loop_),
-        sort_matches_(false) {
+      : sort_matches_(false) {
     HistoryQuickProvider::set_disabled(true);
   }
 
@@ -188,9 +186,7 @@ class HistoryURLProviderTest : public testing::Test,
 
   void RunAdjustOffsetTest(const string16 text, size_t expected_offset);
 
-  base::MessageLoopForUI message_loop_;
-  content::TestBrowserThread ui_thread_;
-  content::TestBrowserThread file_thread_;
+  content::TestBrowserThreadBundle thread_bundle_;
   ACMatches matches_;
   scoped_ptr<TestingProfile> profile_;
   HistoryService* history_service_;
