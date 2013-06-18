ea04b1ba05074e1503b610f5b3d5322d0aaca3f8
mazda@chromium.org <mazda@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 06:48:23 +0000

    Make ifdef condition consistent in platform_thread_linux.cc.
    BUG=170549
    
    Review URL: https://chromiumcodereview.appspot.com/16703022
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206908 0039d316-1c4b-4281-b951-d872f2087c98

--- base/threading/platform_thread_linux.cc
+++ base/threading/platform_thread_linux.cc
@@ -16,7 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/thread_restrictions.h"
 #include "base/tracked_objects.h"
 
-#if defined(OS_LINUX)
+#if !defined(OS_NACL)
 #include <sys/prctl.h>
 #include <sys/resource.h>
 #include <sys/syscall.h>
