77ddd3eb8d71efd9980f48166695b0a40666a82d
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 18:38:50 +0000

    Keep screen on when media source is playing
    When media source is playing, we should keep the screen on.
    Use the same code path as desktop chrome and webrtc does by relying on the PowerSaveBlocker.
    
    BUG=233420
    
    Review URL: https://chromiumcodereview.appspot.com/17295005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207038 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/renderer/media/android/webmediaplayer_android.cc
+++ webkit/renderer/media/android/webmediaplayer_android.cc
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/renderer/media/android/webmediaplayer_manager_android.h"
 #include "webkit/renderer/media/android/webmediaplayer_proxy_android.h"
 #include "webkit/renderer/media/crypto/key_systems.h"
+#include "webkit/renderer/media/webmediaplayer_delegate.h"
 #include "webkit/renderer/media/webmediaplayer_util.h"
 
 #if defined(GOOGLE_TV)
@@ -60,12 +61,14 @@ namespace webkit_media {
 WebMediaPlayerAndroid::WebMediaPlayerAndroid(
     WebKit::WebFrame* frame,
     WebKit::WebMediaPlayerClient* client,
+    base::WeakPtr<WebMediaPlayerDelegate> delegate,
     WebMediaPlayerManagerAndroid* manager,
     WebMediaPlayerProxyAndroid* proxy,
     StreamTextureFactory* factory,
     media::MediaLog* media_log)
     : frame_(frame),
       client_(client),
+      delegate_(delegate),
       buffered_(1u),
       main_loop_(base::MessageLoopProxy::current()),
       ignore_metadata_duration_change_(false),
@@ -135,6 +138,10 @@ WebMediaPlayerAndroid::~WebMediaPlayerAndroid() {
 
   if (base::MessageLoop::current())
     base::MessageLoop::current()->RemoveDestructionObserver(this);
+
+  if (source_type_ == MediaPlayerAndroid::SOURCE_TYPE_MSE && delegate_)
+    delegate_->PlayerGone(this);
+
 #if defined(GOOGLE_TV)
   if (audio_renderer_) {
     if (audio_renderer_->IsLocalRenderer()) {
@@ -253,7 +260,7 @@ void WebMediaPlayerAndroid::play() {
 
   if (paused())
     proxy_->Start(player_id_);
-  is_playing_ = true;
+  UpdatePlayingState(true);
 }
 
 void WebMediaPlayerAndroid::pause() {
@@ -262,7 +269,7 @@ void WebMediaPlayerAndroid::pause() {
     audio_renderer_->Pause();
 #endif
   proxy_->Pause(player_id_);
-  is_playing_ = false;
+  UpdatePlayingState(false);
 }
 
 void WebMediaPlayerAndroid::seek(double seconds) {
@@ -801,6 +808,12 @@ void WebMediaPlayerAndroid::SetNeedsEstablishPeer(bool needs_establish_peer) {
 
 void WebMediaPlayerAndroid::UpdatePlayingState(bool is_playing) {
   is_playing_ = is_playing;
+  if (source_type_ != MediaPlayerAndroid::SOURCE_TYPE_MSE || !delegate_)
+    return;
+  if (is_playing)
+    delegate_->DidPlay(this);
+  else
+    delegate_->DidPause(this);
 }
 
 #if defined(GOOGLE_TV)
