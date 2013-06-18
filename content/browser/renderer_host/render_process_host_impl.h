3e3c23c0a47683b69cdeed57ccf52d48e1c2c42d
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 14:58:27 +0000

    Delete kEnableWebViewSynchronousAPIs flag
    As of crrev.com/206108 this is no longer used in webview, so is
    obsolete.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17116004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@206995 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.h
+++ content/browser/renderer_host/render_process_host_impl.h
@@ -12,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "base/process.h"
-#include "base/synchronization/waitable_event.h"
 #include "base/timer.h"
 #include "content/browser/child_process_launcher.h"
 #include "content/common/content_export.h"
@@ -307,15 +306,6 @@ class CONTENT_EXPORT RenderProcessHostImpl
 
   // Records the last time we regarded the child process active.
   base::TimeTicks child_process_activity_time_;
-
-#if defined(OS_ANDROID)
-  // Android WebView needs to use a SyncChannel to block the browser process
-  // for synchronous find-in-page API support. In that case the shutdown event
-  // makes no sense as the Android port doesn't shutdown, but gets killed.
-  // SyncChannel still expects a shutdown event, so create a dummy one that
-  // will never will be signaled.
-  base::WaitableEvent dummy_shutdown_event_;
-#endif
 
   // Indicates whether this is a RenderProcessHost that has permission to embed
   // Browser Plugins.
