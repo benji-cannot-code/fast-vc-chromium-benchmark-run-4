cfb64357fc11ff411c3f6c2119fcf535cf1ec000
lambroslambrou@chromium.org <lambroslambrou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 20:05:58 +0000

    Add error-handling to host_dispatcher.js interface
    Each method of HostDispatcher, and HostNativeMessaging, now takes an
    additional onError callback parameter.
    
    BUG=249970
    TEST=Test host setup, with NPAPI enabled/disabled, also test with
    Native Messaging
    
    Review URL: https://chromiumcodereview.appspot.com/17115007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207071 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/host_controller.js
+++ remoting/webapp/host_controller.js
@@ -31,11 +31,9 @@ remoting.HostController = function() {
     }
   };
 
-  try {
-    this.hostDispatcher_.getDaemonVersion(printVersion);
-  } catch (err) {
+  this.hostDispatcher_.getDaemonVersion(printVersion, function() {
     console.log('Host version not available.');
-  }
+  });
 }
 
 // Note that the values in the enums below are copied from
@@ -65,7 +63,8 @@ remoting.HostController.AsyncResult = {
  *     called when done.
  */
 remoting.HostController.prototype.getConsent = function(callback) {
-  this.hostDispatcher_.getUsageStatsConsent(callback);
+  this.hostDispatcher_.getUsageStatsConsent(callback,
+                                            remoting.showErrorMessage);
 };
 
 /**
@@ -122,7 +121,8 @@ remoting.HostController.prototype.start = function(hostPin, consent, callback) {
 
     if (success) {
       that.hostDispatcher_.getPinHash(newHostId, hostPin,
-          startHostWithHash.bind(null, hostName, publicKey, privateKey, xhr));
+          startHostWithHash.bind(null, hostName, publicKey, privateKey, xhr),
+          remoting.showErrorMessage);
     } else {
       console.log('Failed to register the host. Status: ' + xhr.status +
                   ' response: ' + xhr.responseText);
@@ -151,7 +151,8 @@ remoting.HostController.prototype.start = function(hostPin, consent, callback) {
     var onStartDaemon = function(result) {
       onStarted(callback, result, hostName, publicKey);
     };
-    that.hostDispatcher_.startDaemon(hostConfig, consent, onStartDaemon);
+    that.hostDispatcher_.startDaemon(hostConfig, consent, onStartDaemon,
+                                     remoting.showErrorMessage);
   }
 
   /**
@@ -202,10 +203,12 @@ remoting.HostController.prototype.start = function(hostPin, consent, callback) {
    * @return {void} Nothing.
    */
   function startWithHostname(hostName) {
-    that.hostDispatcher_.generateKeyPair(onKeyGenerated.bind(null, hostName));
+    that.hostDispatcher_.generateKeyPair(onKeyGenerated.bind(null, hostName),
+                                         remoting.showErrorMessage);
   }
 
-  this.hostDispatcher_.getHostName(startWithHostname);
+  this.hostDispatcher_.getHostName(startWithHostname,
+                                   remoting.showErrorMessage);
 };
 
 /**
@@ -242,7 +245,7 @@ remoting.HostController.prototype.stop = function(callback) {
     that.getLocalHostId(unregisterHost.bind(null, result));
   };
 
-  this.hostDispatcher_.stopDaemon(onStopped);
+  this.hostDispatcher_.stopDaemon(onStopped, remoting.showErrorMessage);
 };
 
 /**
@@ -274,7 +277,8 @@ remoting.HostController.prototype.updatePin = function(newPin, callback) {
     }
     /** @type {string} */
     var hostId = config['host_id'];
-    that.hostDispatcher_.getPinHash(hostId, newPin, updateDaemonConfigWithHash);
+    that.hostDispatcher_.getPinHash(hostId, newPin, updateDaemonConfigWithHash,
+                                    remoting.showErrorMessage);
   }
 
   /** @param {string} pinHash */
@@ -282,12 +286,13 @@ remoting.HostController.prototype.updatePin = function(newPin, callback) {
     var newConfig = {
       host_secret_hash: pinHash
     };
-    that.hostDispatcher_.updateDaemonConfig(newConfig, callback);
+    that.hostDispatcher_.updateDaemonConfig(newConfig, callback,
+                                            remoting.showErrorMessage);
   }
 
   // TODO(sergeyu): When crbug.com/121518 is fixed: replace this call
   // with an unprivileged version if that is necessary.
-  this.hostDispatcher_.getDaemonConfig(onConfig);
+  this.hostDispatcher_.getDaemonConfig(onConfig, remoting.showErrorMessage);
 };
 
 /**
@@ -297,7 +302,9 @@ remoting.HostController.prototype.updatePin = function(newPin, callback) {
  *     Completion callback.
  */
 remoting.HostController.prototype.getLocalHostState = function(onDone) {
-  this.hostDispatcher_.getDaemonState(onDone);
+  this.hostDispatcher_.getDaemonState(onDone, function() {
+    onDone(remoting.HostController.State.NOT_IMPLEMENTED);
+  });
 };
 
 /**
@@ -316,11 +323,10 @@ remoting.HostController.prototype.getLocalHostId = function(onDone) {
     }
     onDone(hostId);
   };
-  try {
-    this.hostDispatcher_.getDaemonConfig(onConfig);
-  } catch (err) {
+
+  this.hostDispatcher_.getDaemonConfig(onConfig, function(error) {
     onDone(null);
-  }
+  });
 };
 
 /** @type {remoting.HostController} */
