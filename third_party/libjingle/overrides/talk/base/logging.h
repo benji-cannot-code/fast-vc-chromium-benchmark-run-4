8c768a7a62fe7b80434fa9f4e8e3e83ef93e6975
grunell@chromium.org <grunell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-18 21:26:13 +0000

    Adding timestamp to log messages sent to WebRTC diagnostic log.
    Review URL: https://chromiumcodereview.appspot.com/17297003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207089 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/libjingle/overrides/talk/base/logging.h
+++ third_party/libjingle/overrides/talk/base/logging.h
@@ -124,6 +124,8 @@ class DiagnosticLogMessage {
                        bool log_to_chrome);
   ~DiagnosticLogMessage();
 
+  uint32 LogStartTime();
+
   std::ostream& stream() { return print_stream_; }
 
  private:
@@ -133,6 +135,7 @@ class DiagnosticLogMessage {
   const bool log_to_chrome_;
 
   std::ostringstream print_stream_;
+  std::ostringstream print_stream_with_timestamp_;
 };
 
 // This class is used to explicitly ignore values in the conditional
