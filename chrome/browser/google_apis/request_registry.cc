bc62f8f1a25ca1c2e474e27b264f6492c5c1be1c
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 09:42:39 +0000

    Remove DCHECK in RequestRegistry.
    The class is almost not used anymore, and the DCHECK condition
    may not hold if a session shuts down immediately after it starts up.
    
    BUG=none
    R=hashimoto@chromium.org
    
    Review URL: https://codereview.chromium.org/17137010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207206 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/request_registry.cc
+++ chrome/browser/google_apis/request_registry.cc
@@ -29,8 +29,6 @@ RequestRegistry::Request::Request(RequestRegistry* registry,
 }
 
 RequestRegistry::Request::~Request() {
-  DCHECK(progress_status_.transfer_state == REQUEST_COMPLETED ||
-         progress_status_.transfer_state == REQUEST_FAILED);
 }
 
 void RequestRegistry::Request::Cancel() {
@@ -50,8 +48,6 @@ void RequestRegistry::Request::NotifyStart() {
 void RequestRegistry::Request::NotifyFinish(
     RequestTransferState status) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
-  DCHECK(progress_status_.transfer_state >= REQUEST_STARTED);
-  DCHECK(status == REQUEST_COMPLETED || status == REQUEST_FAILED);
   progress_status_.transfer_state = status;
   registry_->OnRequestFinish(progress_status().request_id);
 }
@@ -79,11 +75,9 @@ void RequestRegistry::OnRequestStart(
 void RequestRegistry::OnRequestFinish(RequestID id) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
-  Request* request = in_flight_requests_.Lookup(id);
-  DCHECK(request);
-
   DVLOG(1) << "Request[" << id << "] finished.";
-  in_flight_requests_.Remove(id);
+  if (in_flight_requests_.Lookup(id))
+    in_flight_requests_.Remove(id);
 }
 
 }  // namespace google_apis
