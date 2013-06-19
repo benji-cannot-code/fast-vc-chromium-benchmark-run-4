eb449b5564c4fee919100ecec41f70103d2272c5
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 13:19:52 +0000

    Pass along risk params on save/update Online Wallet calls
    BUG=247830
    R=ahutter@chromium.org
    TEST=unit_tests and Online Wallet submit flow still works.
    
    Review URL: https://chromiumcodereview.appspot.com/17253004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_unittest.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_unittest.cc
@@ -1065,6 +1065,7 @@ TEST_F(AutofillDialogControllerTest, SaveAddress) {
       controller()->MenuModelForSection(SECTION_SHIPPING);
   shipping_model->ActivatedAt(shipping_model->GetItemCount() - 1);
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 TEST_F(AutofillDialogControllerTest, SaveInstrument) {
@@ -1076,6 +1077,7 @@ TEST_F(AutofillDialogControllerTest, SaveInstrument) {
   wallet_items->AddAddress(wallet::GetTestShippingAddress());
   controller()->OnDidGetWalletItems(wallet_items.Pass());
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 TEST_F(AutofillDialogControllerTest, SaveInstrumentWithInvalidInstruments) {
@@ -1088,6 +1090,7 @@ TEST_F(AutofillDialogControllerTest, SaveInstrumentWithInvalidInstruments) {
   wallet_items->AddInstrument(wallet::GetTestMaskedInstrumentInvalid());
   controller()->OnDidGetWalletItems(wallet_items.Pass());
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 TEST_F(AutofillDialogControllerTest, SaveInstrumentAndAddress) {
@@ -1096,6 +1099,7 @@ TEST_F(AutofillDialogControllerTest, SaveInstrumentAndAddress) {
 
   controller()->OnDidGetWalletItems(wallet::GetTestWalletItems());
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 // Tests that editing an address (in wallet mode0 and submitting the dialog
@@ -1111,6 +1115,7 @@ TEST_F(AutofillDialogControllerTest, UpdateAddress) {
 
   controller()->EditClickedForSection(SECTION_SHIPPING);
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 // Tests that editing an instrument (CC + address) in wallet mode updates an
@@ -1123,6 +1128,7 @@ TEST_F(AutofillDialogControllerTest, UpdateInstrument) {
 
   controller()->EditClickedForSection(SECTION_CC_BILLING);
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 
   EXPECT_TRUE(
       controller()->GetTestingWalletClient()->updated_billing_address());
@@ -1140,6 +1146,7 @@ TEST_F(AutofillDialogControllerTest, UpdateInstrumentSaveAddress) {
 
   controller()->EditClickedForSection(SECTION_CC_BILLING);
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 
   EXPECT_TRUE(
       controller()->GetTestingWalletClient()->updated_billing_address());
@@ -1159,6 +1166,7 @@ TEST_F(AutofillDialogControllerTest, SaveInstrumentUpdateAddress) {
 
   controller()->EditClickedForSection(SECTION_SHIPPING);
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 MATCHER(UsesLocalBillingAddress, "uses the local billing address") {
@@ -1180,6 +1188,7 @@ TEST_F(AutofillDialogControllerTest, BillingForShipping) {
   UseBillingForShipping();
 
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 // Tests that when using billing address for shipping, and there is an exact
@@ -1204,6 +1213,7 @@ TEST_F(AutofillDialogControllerTest, BillingForShippingHasMatch) {
   UseBillingForShipping();
 
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 // Tests that adding new instrument and also using billing address for shipping,
@@ -1220,6 +1230,7 @@ TEST_F(AutofillDialogControllerTest, BillingForShippingNewInstrument) {
   UseBillingForShipping();
 
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 }
 
 // Test that the local view contents is used when saving a new instrument and
@@ -1245,6 +1256,7 @@ TEST_F(AutofillDialogControllerTest, SaveInstrumentSameAsBilling) {
   EXPECT_CALL(*controller()->GetTestingWalletClient(),
               SaveAddress(UsesLocalBillingAddress(), _)).Times(1);
   controller()->OnAccept();
+  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
 
   EXPECT_TRUE(
       controller()->GetTestingWalletClient()->updated_billing_address());
@@ -1920,23 +1932,6 @@ TEST_F(AutofillDialogControllerTest, RiskNeverLoadsWithPendingLegalDocuments) {
   wallet_items->AddLegalDocument(wallet::GetTestLegalDocument());
   controller()->OnDidGetWalletItems(wallet_items.Pass());
   controller()->OnAccept();
-
-  EXPECT_EQ("no pagers", controller()->GetRiskData());
-}
-
-TEST_F(AutofillDialogControllerTest, RiskLoadsWithoutPendingLegalDocuments) {
-  EXPECT_CALL(*controller(), LoadRiskFingerprintData()).Times(1);
-
-  scoped_ptr<wallet::WalletItems> wallet_items = wallet::GetTestWalletItems();
-  wallet_items->AddInstrument(wallet::GetTestMaskedInstrument());
-  wallet_items->AddAddress(wallet::GetTestShippingAddress());
-  controller()->OnDidGetWalletItems(wallet_items.Pass());
-  controller()->OnAccept();
-
-  EXPECT_EQ("no pagers", controller()->GetRiskData());
-
-  controller()->OnDidLoadRiskFingerprintData(GetFakeFingerprint().Pass());
-  EXPECT_EQ(kFakeFingerprintEncoded, controller()->GetRiskData());
 }
 
 TEST_F(AutofillDialogControllerTest, RiskLoadsAfterAcceptingLegalDocuments) {
@@ -1950,7 +1945,6 @@ TEST_F(AutofillDialogControllerTest, RiskLoadsAfterAcceptingLegalDocuments) {
   EXPECT_CALL(*controller(), LoadRiskFingerprintData()).Times(1);
 
   controller()->OnAccept();
-  EXPECT_EQ("no pagers", controller()->GetRiskData());
 
   // Simulate a risk load and verify |GetRiskData()| matches the encoded value.
   controller()->OnDidAcceptLegalDocuments();
