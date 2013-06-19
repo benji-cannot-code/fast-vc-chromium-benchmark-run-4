9bca63be7a31f22595bf4ed5f3833da45bd261ee
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-19 02:27:46 +0000

    Collapse ImageBufferSkia.cpp and ImageBufferDataSkia.h into their main files. There are no non-Skia implementations anymore.
    BUG=251289
    
    Review URL: https://chromiumcodereview.appspot.com/17420002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152687 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/TransparencyWinTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/TransparencyWinTest.cpp
@@ -35,6 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/GraphicsContext.h"
 #include "core/platform/graphics/ImageBuffer.h"
 #include "core/platform/graphics/transforms/AffineTransform.h"
+#include "skia/ext/platform_canvas.h"
 
 #include <windows.h>
 #include <gtest/gtest.h>
