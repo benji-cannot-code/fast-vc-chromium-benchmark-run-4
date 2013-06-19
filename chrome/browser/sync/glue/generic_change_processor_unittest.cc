9a101c2663f13f7c69a4095fc968bd2898f67a04
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 13:29:38 +0000

    Cleanup: Fix a style error in generic_change_processor_unittest.cc.
    Review URL: https://chromiumcodereview.appspot.com/15841014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207226 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/generic_change_processor_unittest.cc
+++ chrome/browser/sync/glue/generic_change_processor_unittest.cc
@@ -30,7 +30,7 @@ class GenericChangeProcessorTest : public testing::Test {
   static const syncer::ModelType kType = syncer::PREFERENCES;
 
   GenericChangeProcessorTest() :
-      loop(base::MessageLoop::TYPE_UI),
+      loop_(base::MessageLoop::TYPE_UI),
       sync_merge_result_(kType),
       merge_result_ptr_factory_(&sync_merge_result_),
       syncable_service_ptr_factory_(&fake_syncable_service_) {
@@ -71,7 +71,7 @@ class GenericChangeProcessorTest : public testing::Test {
   }
 
  private:
-  base::MessageLoop loop;
+  base::MessageLoop loop_;
 
   syncer::SyncMergeResult sync_merge_result_;
   base::WeakPtrFactory<syncer::SyncMergeResult> merge_result_ptr_factory_;
