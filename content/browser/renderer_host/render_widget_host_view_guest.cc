6bcd2892b57d5a580019a64de49f26bc33592a44
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 04:16:43 +0000

    Show page's context menu for guest (instead of falling back to embedder's ctx menu).
    BUG=140315
    TEST=Manually: a. with selecting text, copying, with input fields, copy pasting.
    b. with <video> elements, play/pause items appear correctly.
    c. Does not work: Links are not recognized within guests from blink, so no copy link address yet.
    
    Review URL: https://chromiumcodereview.appspot.com/16466004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207177 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_guest.cc
+++ content/browser/renderer_host/render_widget_host_view_guest.cc
@@ -449,7 +449,7 @@ GdkEventButton* RenderWidgetHostViewGuest::GetLastMouseDown() {
 }
 
 gfx::NativeView RenderWidgetHostViewGuest::BuildInputMethodsGtkMenu() {
-  return gfx::NativeView();
+  return platform_view_->BuildInputMethodsGtkMenu();
 }
 #endif  // defined(TOOLKIT_GTK)
 
--- content/browser/web_contents/web_contents_view_guest.h
+++ content/browser/web_contents/web_contents_view_guest.h
@@ -31,7 +31,8 @@ class CONTENT_EXPORT WebContentsViewGuest
   // |platform_view|.
   WebContentsViewGuest(WebContentsImpl* web_contents,
                        BrowserPluginGuest* guest,
-                       WebContentsViewPort* platform_view);
+                       WebContentsViewPort* platform_view,
+                       RenderViewHostDelegateView* platform_view_delegate_view);
   virtual ~WebContentsViewGuest();
 
   WebContents* web_contents();
@@ -98,6 +99,9 @@ class CONTENT_EXPORT WebContentsViewGuest
   // Calls to this WebContentsViewGuest are forwarded to |platform_view_|.
   WebContentsViewPort* platform_view_;
   gfx::Size size_;
+
+  // Delegate view for guest's platform view.
+  RenderViewHostDelegateView* platform_view_delegate_view_;
 
   DISALLOW_COPY_AND_ASSIGN(WebContentsViewGuest);
 };
