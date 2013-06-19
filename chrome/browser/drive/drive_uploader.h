703ae9783d6b385dfcb9bcc142f79e74a556c828
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 05:06:00 +0000

    drive: Move DriveUploader to c/b/drive
    This shouldn't be in 'google_apis' where only low level
    network code should be placed.
    
    BUG=244325
    TEST=none
    R=kinaba@chromium.org, kinuko@chromium.org
    
    Review URL: https://codereview.chromium.org/17395008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207182 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drive/drive_uploader.h
+++ chrome/browser/drive/drive_uploader.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_GOOGLE_APIS_DRIVE_UPLOADER_H_
-#define CHROME_BROWSER_GOOGLE_APIS_DRIVE_UPLOADER_H_
+#ifndef CHROME_BROWSER_DRIVE_DRIVE_UPLOADER_H_
+#define CHROME_BROWSER_DRIVE_DRIVE_UPLOADER_H_
 
 #include <string>
 
@@ -205,4 +205,4 @@ class DriveUploader : public DriveUploaderInterface {
 
 }  // namespace google_apis
 
-#endif  // CHROME_BROWSER_GOOGLE_APIS_DRIVE_UPLOADER_H_
+#endif  // CHROME_BROWSER_DRIVE_DRIVE_UPLOADER_H_
