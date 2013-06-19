fc0ff4f8c3cdbe96aaa82593cc9def90c01e552a
zmo@chromium.org <zmo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-19 03:43:54 +0000

    Do not set alpha channel mask if MRT is enabled and some buffers have alpha.
    There is no perfect solution, but this is try to enable the alpha channel workaround in as many places as possible.
    
    BUG=222419
    TEST=webgl-draw-buffers.html pass on linux
    R=gman@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17379012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207162 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/framebuffer_manager.h
+++ gpu/command_buffer/service/framebuffer_manager.h
@@ -122,6 +122,9 @@ class GPU_EXPORT Framebuffer : public base::RefCounted<Framebuffer> {
 
   void SetDrawBuffers(GLsizei n, const GLenum* bufs);
 
+  // Return true if any draw buffers has an alpha channel.
+  bool HasAlphaMRT() const;
+
   static void ClearFramebufferCompleteComboMap();
 
   static bool AllowFramebufferComboCompleteMapForTesting() {
