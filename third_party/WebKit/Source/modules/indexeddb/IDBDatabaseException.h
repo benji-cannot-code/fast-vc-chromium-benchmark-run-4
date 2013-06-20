aba0724618872550bb6d6f000ee5b9ef96c16f1b
arv@chromium.org <arv@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 17:25:28 +0000

    Make ExceptionCode be an enum that does not encode the "code" property
    This adds a new enum for the legacy error codes and use that to expose
    the code in the binding.
    
    BUG=251437
    
    Review URL: https://chromiumcodereview.appspot.com/17379011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152800 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBDatabaseException.h
+++ third_party/WebKit/Source/modules/indexeddb/IDBDatabaseException.h
@@ -73,7 +73,7 @@ public:
     static bool initializeDescription(ExceptionCode, ExceptionCodeDescription*);
     static String getErrorName(ExceptionCode);
     static String getErrorDescription(ExceptionCode);
-    static ExceptionCode getLegacyErrorCode(ExceptionCode);
+    static int getLegacyErrorCode(ExceptionCode);
 
 private:
     IDBDatabaseException(const ExceptionCodeDescription& description)
