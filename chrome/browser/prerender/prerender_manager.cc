39fa48c4c83a8581b066b4dc2487fd3084a02eef
cbentzel@chromium.org <cbentzel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 22:10:24 +0000

    [prerender]: data: subresources are allowed
    BUG=249566
    
    Review URL: https://chromiumcodereview.appspot.com/17356003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/prerender/prerender_manager.cc
+++ chrome/browser/prerender/prerender_manager.cc
@@ -888,7 +888,9 @@ bool PrerenderManager::IsValidHttpMethod(const std::string& method) {
 
 // static
 bool PrerenderManager::DoesURLHaveValidScheme(const GURL& url) {
-  return IsWebURL(url) || url.SchemeIs(extensions::kExtensionScheme);
+  return (IsWebURL(url) ||
+          url.SchemeIs(extensions::kExtensionScheme) ||
+          url.SchemeIs("data"));
 }
 
 DictionaryValue* PrerenderManager::GetAsValue() const {
