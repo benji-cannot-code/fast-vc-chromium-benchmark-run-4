d3d1b9f8340c2794a83b515d0547db47f1a779c5
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 08:11:55 +0000

    [chromedriver] Pass command line args from desired caps on Android.
    Also:
    - Add --ignore-certificate-errors to Android to bypass interstitial SSL
      page
    - Add --metrics-recording-only to desktop to avoid polluting stats
    
    BUG=chromedriver:365
    
    Review URL: https://chromiumcodereview.appspot.com/16794004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/capabilities.h
+++ chrome/test/chromedriver/capabilities.h
@@ -35,6 +35,7 @@ struct Capabilities {
 
   std::string android_package;
   std::string device_serial;
+  std::string android_args;
 
   std::string log_path;
   CommandLine command;
--- chrome/test/chromedriver/chrome_launcher.cc
+++ chrome/test/chromedriver/chrome_launcher.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/base64.h"
+#include "base/basictypes.h"
 #include "base/command_line.h"
 #include "base/file_util.h"
 #include "base/files/file_path.h"
@@ -40,6 +41,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/chromedriver/net/url_request_context_getter.h"
 
 namespace {
+
+const char* kCommonSwitches[] = {
+  "ignore-certificate-errors", "metrics-recording-only"};
 
 Status UnpackAutomationExtension(const base::FilePath& temp_dir,
                                  base::FilePath* automation_extension) {
@@ -197,7 +201,8 @@ Status LaunchDesktopChrome(
   base::ScopedTempDir extension_dir;
   PrepareCommandLine(port, capabilities,
                      &command, &user_data_dir, &extension_dir);
-  command.AppendSwitch("ignore-certificate-errors");
+  for (size_t i = 0; i < arraysize(kCommonSwitches); i++)
+    command.AppendSwitch(kCommonSwitches[i]);
   base::LaunchOptions options;
 
 #if !defined(OS_WIN)
@@ -293,7 +298,13 @@ Status LaunchAndroidChrome(
   }
   if (!status.IsOk())
     return status;
-  status = device->StartChrome(capabilities.android_package, port);
+
+  std::string args(capabilities.android_args);
+  for (size_t i = 0; i < arraysize(kCommonSwitches); i++)
+    args += "--" + std::string(kCommonSwitches[i]) + " ";
+  args += "--disable-fre --enable-remote-debugging";
+
+  status = device->StartChrome(capabilities.android_package, port, args);
   if (!status.IsOk())
     return status;
 
