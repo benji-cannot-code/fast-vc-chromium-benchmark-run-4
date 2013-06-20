9adb8fc3c5810354108ea4d85165c903af064675
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 13:43:47 +0000

    ui/base/models: Use base::string16 now that string16 was moved into base namespace.
    base/string16.h was moved into base namespace in r191198 by Brett.
    
    BUG=None
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17443002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207419 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/models/button_menu_item_model.cc
+++ ui/base/models/button_menu_item_model.cc
@@ -14,9 +14,9 @@ bool ButtonMenuItemModel::Delegate::IsItemForCommandIdDynamic(
   return false;
 }
 
-string16 ButtonMenuItemModel::Delegate::GetLabelForCommandId(
+base::string16 ButtonMenuItemModel::Delegate::GetLabelForCommandId(
     int command_id) const {
-  return string16();
+  return base::string16();
 }
 
 bool ButtonMenuItemModel::Delegate::IsCommandIdEnabled(int command_id) const {
@@ -31,7 +31,7 @@ bool ButtonMenuItemModel::Delegate::DoesCommandIdDismissMenu(
 struct ButtonMenuItemModel::Item {
   int command_id;
   ButtonType type;
-  string16 label;
+  base::string16 label;
   int icon_idr;
   bool part_of_group;
 };
@@ -55,7 +55,7 @@ void ButtonMenuItemModel::AddGroupItemWithStringId(
 
 void ButtonMenuItemModel::AddItemWithImage(int command_id,
                                            int icon_idr) {
-  Item item = { command_id, TYPE_BUTTON, string16(), icon_idr, false };
+  Item item = { command_id, TYPE_BUTTON, base::string16(), icon_idr, false };
   items_.push_back(item);
 }
 
@@ -66,7 +66,7 @@ void ButtonMenuItemModel::AddButtonLabel(int command_id, int string_id) {
 }
 
 void ButtonMenuItemModel::AddSpace() {
-  Item item = { 0, TYPE_SPACE, string16(), -1, false };
+  Item item = { 0, TYPE_SPACE, base::string16(), -1, false };
   items_.push_back(item);
 }
 
@@ -89,7 +89,7 @@ bool ButtonMenuItemModel::IsItemDynamicAt(int index) const {
   return false;
 }
 
-string16 ButtonMenuItemModel::GetLabelAt(int index) const {
+base::string16 ButtonMenuItemModel::GetLabelAt(int index) const {
   if (IsItemDynamicAt(index))
     return delegate_->GetLabelForCommandId(GetCommandIdAt(index));
   return items_[index].label;
--- ui/base/models/menu_model.h
+++ ui/base/models/menu_model.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/image/image_skia.h"
 #include "ui/gfx/native_widget_types.h"
 
-
 namespace gfx {
 class Font;
 class Image;
@@ -57,11 +56,11 @@ class UI_EXPORT MenuModel {
   virtual int GetCommandIdAt(int index) const = 0;
 
   // Returns the label of the item at the specified index.
-  virtual string16 GetLabelAt(int index) const = 0;
+  virtual base::string16 GetLabelAt(int index) const = 0;
 
   // Returns the sublabel of the item at the specified index. The sublabel
   // is rendered beneath the label and using the font GetSublabelFontAt().
-  virtual string16 GetSublabelAt(int index) const;
+  virtual base::string16 GetSublabelAt(int index) const;
 
   // Returns true if the menu item (label/sublabel/icon) at the specified
   // index can change over the course of the menu's lifetime. If this function
