922cb39ff9c3c846f2d0e85c34bc1153793f927c
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 20:15:35 +0000

    Fix browser crash when enabling managed extensions in incognito.
    DisableExtension is aborting early for managed extensions because the user is not allowed to disable managed extensions, which causes a CHECK() to fail. However, it should be ok to disable managed extensions for reload purposes.
    
    BUG=249294
    
    Review URL: https://chromiumcodereview.appspot.com/17451006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207532 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_service.cc
+++ chrome/browser/extensions/extension_service.cc
@@ -889,6 +889,7 @@ void ExtensionService::DisableExtension(
   // |extension| can be NULL if sync disables an extension that is not
   // installed yet.
   if (extension &&
+      disable_reason != Extension::DISABLE_RELOAD &&
       !system_->management_policy()->UserMayModifySettings(extension, NULL)) {
     return;
   }
