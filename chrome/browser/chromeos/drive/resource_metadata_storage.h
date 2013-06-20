070cbd7ae5c0d0b94a3b0bd89f3591328e13e754
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 08:06:36 +0000

    drive: Stop returning scoped_ptr from ResourceMetadataStorage::GetHeader
    BUG=None
    TEST=unit_tests
    R=kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/17210003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207349 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/resource_metadata_storage.h
+++ chrome/browser/chromeos/drive/resource_metadata_storage.h
@@ -148,7 +148,7 @@ class ResourceMetadataStorage {
   bool PutHeader(const ResourceMetadataHeader& header);
 
   // Gets header.
-  scoped_ptr<ResourceMetadataHeader> GetHeader();
+  bool GetHeader(ResourceMetadataHeader* out_header);
 
   // Checks validity of the data.
   bool CheckValidity();
