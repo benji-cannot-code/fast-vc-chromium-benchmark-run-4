bbdcc635d18f1df2f036c86edf01278f9d13bea8
aurimas@chromium.org <aurimas@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 16:43:32 +0000

    Remove use of WebWidget::textInputType()
    Remove the last uses of WebWidget::textInputType() so that this function can be removed.
    
    BUG=251815
    
    Review URL: https://chromiumcodereview.appspot.com/16922013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207451 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/chrome_render_view_observer.cc
+++ chrome/renderer/chrome_render_view_observer.cc
@@ -667,7 +667,7 @@ void ChromeRenderViewObserver::DidHandleGestureEvent(
     return;
 
   WebKit::WebTextInputType text_input_type =
-      render_view()->GetWebView()->textInputType();
+      render_view()->GetWebView()->textInputInfo().type;
 
   render_view()->Send(new ChromeViewHostMsg_FocusedNodeTouched(
       routing_id(),
