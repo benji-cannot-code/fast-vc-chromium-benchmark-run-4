85f7bde4d481ef3ac12b0f4cf0ca56a5d1095970
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 13:37:30 +0000

    Revert 207409 "[Mac][MC] Re-add the global event monitor to clos..."
    > [Mac][MC] Re-add the global event monitor to close the bubble.
    > 
    > This is needed because clicking on other NSStatusItems and menubar extras does
    > not generate a -windowDidResignKey: notification.
    > 
    > BUG=243128
    > TEST=See comment 8 in the bug for steps.
    > R=dewittj@chromium.org, sail@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/17469002
    
    TBR=rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/17218008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207417 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/tray_controller.mm
+++ ui/message_center/cocoa/tray_controller.mm
@@ -49,24 +49,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSView* contentView = [viewController_ view];
     [window setFrame:[contentView frame] display:NO];
     [window setContentView:contentView];
-
-    // The global event monitor will close the tray in response to events
-    // delivered to other applications, and -windowDidResignKey: will catch
-    // events within the application.
-    clickEventMonitor_ =
-        [NSEvent addGlobalMonitorForEventsMatchingMask:NSLeftMouseDownMask |
-                                                       NSRightMouseDownMask |
-                                                       NSOtherMouseDownMask
-            handler:^(NSEvent* event) {
-                [self windowDidResignKey:nil];
-            }];
   }
   return self;
-}
-
-- (void)dealloc {
-  [NSEvent removeMonitor:clickEventMonitor_];
-  [super dealloc];
 }
 
 - (void)close {
