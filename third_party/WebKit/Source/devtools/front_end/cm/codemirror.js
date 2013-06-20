75f30f9e8f4a04dfe8553595f148da3061e52556
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 16:47:53 +0000

    DevTools: [CodeMirror] CodeMirrorTextEditor to support long lines
    This patch does couple of things which result in bearable long lines
    support:
    [1] This CL patches codemirror.js so that it becomes capable of handling
    long lines. These changes are not upstreamed yet (refer to
    https://github.com/marijnh/CodeMirror/pull/1613 for details) as they are not
    mature enough for the general case, but they suit our needs under certain
    conditions (see [2])
    
    [2] This CL adds "longLinesMode" to CodeMirrorTextEditor which gets switched
    on in case of long lines. This mode:
    - switches off selected text foreground (thus leaves text syntax-highlighted).
    This is an implication of poor 'CodeMirror.markText' support (caused by [1])
    and the fact that selected text foreground gets highlighted by the method.
    - switches off bracket matching as it works far too much for long lines.
    This change will be reverted as soon as 'matchbrackets' addon will respect
    its 'maxHighlightLineLength' setting.
    
    [3] This CL reduces amount of highlighted symbols per line up to 1000. Thus
    said, everything that lays after position #1000 will not be highlighted.
    
    BUG=247320
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/17437005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152798 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/cm/codemirror.js
+++ third_party/WebKit/Source/devtools/front_end/cm/codemirror.js
@@ -1,6 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// CodeMirror version 3.14
-//
 // CodeMirror is the only global var we claim
 window.CodeMirror = (function() {
   "use strict";
@@ -963,7 +961,7 @@ window.CodeMirror = (function() {
     data = data || measureLine(cm, line);
 
     for (var pos = ch;; pos += dir) {
-      var r = data[pos];
+      var r = data.get(pos);
       if (r) break;
       if (dir < 0 && pos == 0) dir = 1;
     }
@@ -996,7 +994,13 @@ window.CodeMirror = (function() {
     if (cached) return cached.measure;
 
     // Failing that, recompute and store result in cache
-    var measure = measureLineInner(cm, line);
+    var measure = measureLineInner(cm, line, 0, 10);
+    measure.get = function(num) {
+      if (this[num]) return this[num];
+      if (num < 0 || num >= line.text.length) return null;
+      measureLineInner(cm, line, Math.max(num - 50, 0), Math.min(num + 50, line.text.length), this);
+      return this[num];
+    };
     var cache = cm.display.measureLineCache;
     var memo = {text: line.text, width: cm.display.scroller.clientWidth,
                 markedSpans: line.markedSpans, measure: measure,
@@ -1006,9 +1010,9 @@ window.CodeMirror = (function() {
     return measure;
   }
 
-  function measureLineInner(cm, line) {
+  function measureLineInner(cm, line, from, to, oldMeasure) {
     var display = cm.display, measure = emptyArray(line.text.length);
-    var pre = lineContent(cm, line, measure);
+    var pre = lineContent(cm, line, measure, from, to);
 
     // IE does not cache element positions of inline elements between
     // calls to getBoundingClientRect. This makes the loop below,
@@ -1038,7 +1042,7 @@ window.CodeMirror = (function() {
     removeChildrenAndAdd(display.measure, pre);
 
     var outer = getRect(display.lineDiv);
-    var vranges = [], data = emptyArray(line.text.length), maxBot = pre.offsetHeight;
+    var vranges = [], data = oldMeasure || emptyArray(line.text.length), maxBot = pre.offsetHeight;
     // Work around an IE7/8 bug where it will sometimes have randomly
     // replaced our pre with a clone at this point.
     if (ie_lt9 && display.measure.first != pre)
@@ -1082,7 +1086,7 @@ window.CodeMirror = (function() {
       if (cur.measureRight) right = getRect(cur.measureRight).left;
       data[i] = {left: size.left - outer.left, right: right - outer.left, top: vCat};
     }
-    for (var i = 0, cur; i < data.length; ++i) if (cur = data[i]) {
+    for (var i = 0, cur; i < data.length; ++i) if (measure[i] && (cur = data[i])) {
       var vr = cur.top, vrRight = cur.topRight;
       cur.top = vranges[vr]; cur.bottom = vranges[vr+1];
       if (vrRight != null) { cur.topRight = vranges[vrRight]; cur.bottomRight = vranges[vrRight+1]; }
@@ -4208,13 +4212,13 @@ window.CodeMirror = (function() {
       (styleToClassCache[style] = "cm-" + style.replace(/ +/g, " cm-"));
   }
 
-  function lineContent(cm, realLine, measure) {
+  function lineContent(cm, realLine, measure, from, to) {
     var merged, line = realLine, empty = true;
     while (merged = collapsedSpanAtStart(line))
       line = getLine(cm.doc, merged.find().from.line);
 
     var builder = {pre: elt("pre"), col: 0, pos: 0, display: !measure,
-                   measure: null, measuredSomething: false, cm: cm};
+                   measure: null, measuredSomething: false, cm: cm, from: from, to: to};
     if (line.textClass) builder.pre.className = line.textClass;
 
     do {
@@ -4296,6 +4300,14 @@ window.CodeMirror = (function() {
   }
 
   function buildTokenMeasure(builder, text, style, startStyle, endStyle) {
+    //FIXME consider 2-byte chars
+    if (builder.from || builder.to) {
+      if (builder.from >= builder.pos + text.length) {
+        buildToken(builder, text, style, startStyle, endStyle);
+        builder.pos += text.length;
+        return;
+      }
+    }
     var wrapping = builder.cm.options.lineWrapping;
     for (var i = 0; i < text.length; ++i) {
       var ch = text.charAt(i), start = i == 0;
@@ -4349,8 +4361,27 @@ window.CodeMirror = (function() {
   function insertLineContent(line, builder, styles) {
     var spans = line.markedSpans, allText = line.text, at = 0;
     if (!spans) {
-      for (var i = 1; i < styles.length; i+=2)
-        builder.addToken(builder, allText.slice(at, at = styles[i]), styleToClass(styles[i+1]));
+      if (builder.to || builder.from) {
+        for (var i = 1; i < styles.length && (!builder.to || builder.pos < builder.to); i+=2) {
+          var text = allText.slice(at, at = styles[i]);
+          var textLength = text.length;
+          var resolvedStyles = styleToClass(styles[i+1]);
+          if (builder.pos + textLength <= builder.from) {
+            builder.addToken(builder, text, resolvedStyles);
+            continue;
+          }
+          // if we are not inside, but will get over it
+          if (builder.pos < builder.from && builder.pos + textLength > builder.from) {
+            var overlap = builder.from - builder.pos;
+            builder.addToken(builder, text.substring(0, overlap), resolvedStyles);
+            text = text.substring(overlap);
+          }
+          builder.addToken(builder, text.substr(0, Math.min(builder.to - builder.from, text.length)), resolvedStyles);
+        }
+      } else {
+        for (var i = 1; i < styles.length; i+=2)
+          builder.addToken(builder, allText.slice(at, at = styles[i]), styleToClass(styles[i+1]));
+      }
       return;
     }
 
@@ -4393,6 +4424,8 @@ window.CodeMirror = (function() {
             var tokenText = end > upto ? text.slice(0, upto - pos) : text;
             builder.addToken(builder, tokenText, style ? style + spanStyle : spanStyle,
                              spanStartStyle, pos + tokenText.length == nextChange ? spanEndStyle : "");
+            if (builder.to && builder.pos >= builder.to)
+              return;
           }
           if (end >= upto) {text = text.slice(upto - pos); pos = upto; break;}
           pos = end;
