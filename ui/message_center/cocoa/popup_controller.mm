1157b8c90ea5ccb926f3b9ba4bdc3a3ec6e9474e
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 17:20:52 +0000

    [Mac] Fix the problem that a popup could be closed while it is still in animation.
    This occurs when the test code creates a popup and close the collection immediately. The fix is to close the popup without animation for this case.
    
    All the popup animations are coordinated by the popup collection. Added DCHECKs in the popup controller to guard the places that might trigger the 2nd animation while the previous animation has not ended.
    
    BUG=249131
    TEST=reenable the disabled test
    R=dewittj@chromium.org, rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/17468007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207470 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/popup_controller.mm
+++ ui/message_center/cocoa/popup_controller.mm
@@ -214,6 +214,7 @@ enum {
     NSViewAnimationEndFrameKey : [NSValue valueWithRect:newBounds],
     NSViewAnimationEffectKey : NSViewAnimationFadeInEffect
   };
+  DCHECK(!boundsAnimation_);
   boundsAnimation_.reset([[NSViewAnimation alloc]
       initWithViewAnimations:[NSArray arrayWithObject:animationDict]]);
   [boundsAnimation_ setDuration:[popupCollection_ popupAnimationDuration]];
@@ -238,6 +239,7 @@ enum {
     NSViewAnimationTargetKey : [self window],
     NSViewAnimationEffectKey : NSViewAnimationFadeOutEffect
   };
+  DCHECK(!boundsAnimation_);
   boundsAnimation_.reset([[NSViewAnimation alloc]
       initWithViewAnimations:[NSArray arrayWithObject:animationDict]]);
   [boundsAnimation_ setDuration:[popupCollection_ popupAnimationDuration]];
@@ -262,6 +264,7 @@ enum {
     NSViewAnimationTargetKey :   [self window],
     NSViewAnimationEndFrameKey : [NSValue valueWithRect:newBounds]
   };
+  DCHECK(!boundsAnimation_);
   boundsAnimation_.reset([[NSViewAnimation alloc]
       initWithViewAnimations:[NSArray arrayWithObject:animationDict]]);
   [boundsAnimation_ setDuration:[popupCollection_ popupAnimationDuration]];
