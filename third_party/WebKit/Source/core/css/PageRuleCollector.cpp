7f46f30169893816491603b775b7fb7a359dd577
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 23:35:44 +0000

    Add a call to compactRulesIfNeeded() in PageRuleCollector to fix an assert.
    printing/page-format-data.html and printing/page-format-data-display-none.html were crashing on win dbg after r152757.
    
    R=abarth@chromium.org
    BUG=252521
    
    Review URL: https://codereview.chromium.org/17514004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152832 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/PageRuleCollector.cpp
+++ third_party/WebKit/Source/core/css/PageRuleCollector.cpp
@@ -72,6 +72,7 @@ void PageRuleCollector::matchPageRules(RuleSet* rules)
     if (!rules)
         return;
 
+    rules->compactRulesIfNeeded();
     Vector<StyleRulePage*> matchedPageRules;
     matchPageRulesForList(matchedPageRules, rules->pageRules(), m_isLeftPage, m_isFirstPage, m_pageName);
     if (matchedPageRules.isEmpty())
