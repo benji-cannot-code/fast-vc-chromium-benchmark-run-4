c7627b24875f543ec757b234ffb2ec07a855a815
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 12:38:38 +0000

    Fix a history bug where a reload is given a standard load's history entry.
    If a resource load is started after the load event and a reload is triggered
    before that resource load finishes, the load will be cancelled and in doing
    so will incorrectly revert the new load's load type to FrameLoadTypeStandard.
    This was regressed in r148684
    
    BUG=241581
    TEST=http/tests/history/reload-during-load-after-load-event.html
    
    Review URL: https://chromiumcodereview.appspot.com/17463006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152781 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -1126,14 +1126,13 @@ void FrameLoader::loadWithNavigationAction(const ResourceRequest& request, const
     if (type == FrameLoadTypeRedirectWithLockedBackForwardList)
         loader->setIsClientRedirect(true);
 
-    m_loadType = type;
     bool isFormSubmission = formState;
 
     if (shouldPerformFragmentNavigation(isFormSubmission, request.httpMethod(), type, request.url()))
-        checkNavigationPolicyAndContinueFragmentScroll(action);
+        checkNavigationPolicyAndContinueFragmentScroll(action, type != FrameLoadTypeRedirectWithLockedBackForwardList);
     else {
         setPolicyDocumentLoader(loader.get());
-        checkNavigationPolicyAndContinueLoad(formState);
+        checkNavigationPolicyAndContinueLoad(formState, type);
     }
 }
 
@@ -1931,7 +1930,7 @@ void FrameLoader::receivedMainResourceError(const ResourceError& error)
         checkLoadComplete();
 }
 
-void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction& action)
+void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const NavigationAction& action, bool isNewNavigation)
 {
     m_documentLoader->setTriggeringAction(action);
 
@@ -1944,7 +1943,7 @@ void FrameLoader::checkNavigationPolicyAndContinueFragmentScroll(const Navigatio
         m_provisionalDocumentLoader->stopLoading();
         setProvisionalDocumentLoader(0);
     }
-    loadInSameDocument(request.url(), 0, m_loadType != FrameLoadTypeRedirectWithLockedBackForwardList);
+    loadInSameDocument(request.url(), 0, isNewNavigation);
 }
 
 bool FrameLoader::shouldPerformFragmentNavigation(bool isFormSubmission, const String& httpMethod, FrameLoadType loadType, const KURL& url)
@@ -2041,7 +2040,7 @@ bool FrameLoader::fireBeforeUnloadEvent(Chrome& chrome)
     return chrome.runBeforeUnloadConfirmPanel(text, m_frame);
 }
 
-void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> formState)
+void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> formState, FrameLoadType type)
 {
     // If we loaded an alternate page to replace an unreachableURL, we'll get in here with a
     // nil policyDataSource because loading the alternate page will have passed
@@ -2105,6 +2104,7 @@ void FrameLoader::checkNavigationPolicyAndContinueLoad(PassRefPtr<FormState> for
     }
 
     setProvisionalDocumentLoader(m_policyDocumentLoader.get());
+    m_loadType = type;
     setState(FrameStateProvisional);
 
     setPolicyDocumentLoader(0);
