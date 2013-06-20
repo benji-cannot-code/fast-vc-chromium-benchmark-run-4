b9c9d50d4424fe5d2db28cdc9b8a87b785dcc5a4
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 06:41:18 +0000

    Disable theme buttons for managed users.
    Also make sure that the webui backend does not process such
    requests if the user somehow managed to enable the buttons.
    
    BUG=251230
    
    Review URL: https://chromiumcodereview.appspot.com/17390006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207335 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -201,6 +201,10 @@ cr.define('options', function() {
           $('profiles-create').disabled = true;
           $('profiles-delete').disabled = true;
           $('profiles-list').canDeleteItems = false;
+          if ($('themes-GTK-button'))
+            $('themes-GTK-button').disabled = true;
+          $('themes-reset').disabled = true;
+          $('themes-gallery').disabled = true;
         }
       }
 
