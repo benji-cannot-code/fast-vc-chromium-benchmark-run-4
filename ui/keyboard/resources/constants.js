4564a091ebb653d45171ccf3e61a2988fb660464
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 16:32:33 +0000

    Web Component Virtual Keyboard
    Convert existing keyboard to web component keyboard. Also fix touch down state of keys.
    
    BUG=242539, 240346
    R=bryeung@chromium.org, miket@chromium.org, stevet@chromium.org
    
    Review URL: https://codereview.chromium.org/15176004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207448 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ ui/keyboard/resources/constants.js
@@ -0,0 +1,25 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+/**
+ * Aspect ratio of keyboard.
+ * @type {number}
+ */
+var ASPECT_RATIO = 4.5;
+
+/**
+ * Ratio of key height and font size.
+ * @type {number}
+ */
+var FONT_SIZE_RATIO = 3.5;
+
+/**
+ * The number of rows in each keyset.
+ * @type {number}
+ */
+// TODO(bshe): The number of rows should equal to the number of kb-row elements
+// in kb-keyset. Remove this variable once figure out how to calculate the
+// number from keysets.
+var ROW_LENGTH = 4;
