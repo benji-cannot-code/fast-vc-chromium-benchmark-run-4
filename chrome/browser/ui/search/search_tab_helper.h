efa5651d8eb2c622d890f6611ae58d1bae075f32
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 14:08:54 +0000

    Move instant support to SearchTabHelper. This will also fix the search for RTL languages.
    Previously, instant_tab_ never "loses" instant support. But an instant_tab_ can lose the instant support by navigating to a non-extended page.
    
    In this CL, whenever a navigation entry is committed, we reset the instant support state of the instant_tab_ to INSTANT_SUPPORT_UNKNOWN and we determine the support once the page contents are loaded.
    
    BUG=247727, 246323
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/16035020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207428 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/search_tab_helper.h
+++ chrome/browser/ui/search/search_tab_helper.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
+#include "base/gtest_prod_util.h"
 #include "chrome/browser/ui/search/search_model.h"
 #include "chrome/common/instant_types.h"
 #include "content/public/browser/notification_observer.h"
@@ -21,8 +22,15 @@ namespace content {
 class WebContents;
 }
 
+class InstantPageTest;
+
 // Per-tab search "helper".  Acts as the owner and controller of the tab's
 // search UI model.
+//
+// When the page is finished loading, SearchTabHelper determines the instant
+// support for the page. When a navigation entry is committed (except for
+// in-page navigations), SearchTabHelper resets the instant support state to
+// INSTANT_SUPPORT_UNKNOWN and cause support to be determined again.
 class SearchTabHelper : public content::NotificationObserver,
                         public content::WebContentsObserver,
                         public content::WebContentsUserData<SearchTabHelper> {
@@ -51,8 +59,23 @@ class SearchTabHelper : public content::NotificationObserver,
   bool UpdateLastKnownMostVisitedItems(
       const std::vector<InstantMostVisitedItem>& items);
 
+  // Invoked to update the instant support state.
+  void InstantSupportChanged(bool supports_instant);
+
+  // Returns true if the page supports instant. If the instant support state is
+  // not determined or if the page does not support instant returns false.
+  bool SupportsInstant() const;
+
  private:
   friend class content::WebContentsUserData<SearchTabHelper>;
+  friend class InstantPageTest;
+  FRIEND_TEST_ALL_PREFIXES(InstantPageTest,
+                           DetermineIfPageSupportsInstant_Local);
+  FRIEND_TEST_ALL_PREFIXES(InstantPageTest,
+                           DetermineIfPageSupportsInstant_NonLocal);
+  FRIEND_TEST_ALL_PREFIXES(InstantPageTest,
+                           PageURLDoesntBelongToInstantRenderer);
+  FRIEND_TEST_ALL_PREFIXES(InstantPageTest, PageSupportsInstant);
 
   explicit SearchTabHelper(content::WebContents* web_contents);
 
@@ -63,9 +86,22 @@ class SearchTabHelper : public content::NotificationObserver,
 
   // Overridden from contents::WebContentsObserver:
   virtual bool OnMessageReceived(const IPC::Message& message) OVERRIDE;
+  virtual void DidFinishLoad(
+      int64 frame_id,
+      const GURL& validated_url,
+      bool is_main_frame,
+      content::RenderViewHost* render_view_host) OVERRIDE;
 
   // Sets the mode of the model based on the current URL of web_contents().
   void UpdateMode();
+
+  // Tells the renderer to determine if the page supports the Instant API, which
+  // results in a call to OnInstantSupportDetermined() when the reply
+  // is received.
+  void DetermineIfPageSupportsInstant();
+
+  // Handler for when Instant support has been determined.
+  void OnInstantSupportDetermined(int page_id, bool supports_instant);
 
   // Handlers for SearchBox API to show and hide top bars (bookmark and info
   // bars).
