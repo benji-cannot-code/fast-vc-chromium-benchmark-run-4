8509ebdd4be462e4230ae2fb6d6bb7a4ad8509f3
paulgazz@chromium.org <paulgazz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 10:17:53 +0000

    Factored out common ELF processing into a common superclass, an x86 subclass, and an initial ARM subclass.
    Created a new disassembler virtual class, DisassemblerElf32, that
    implements the common functions for processing ELF 32-bit files.  This
    class is defined in disassembler_elf_32.{h|cc}, and has nearly all of
    the methods and data that used to be in
    disassembler_elf_32_x86.{h|cc}.  This class has two subclasses, one
    for x86 and one for arm, which implement the architecture-specific
    parts for generating courgette patches, namely extracting relative and
    absolute addresses.  The ARM subclass is just a shell, but still
    yields correct courgette patches.
    
    The rest of the files add new enums for ARM and use the ARM subclasses
    for the courgette command-line tool.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17325003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207388 0039d316-1c4b-4281-b951-d872f2087c98

--- courgette/disassembler.cc
+++ courgette/disassembler.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "courgette/assembly_program.h"
 #include "courgette/courgette.h"
+#include "courgette/disassembler_elf_32_arm.h"
 #include "courgette/disassembler_elf_32_x86.h"
 #include "courgette/disassembler_win32_x86.h"
 #include "courgette/encoded_program.h"
@@ -36,6 +37,12 @@ Disassembler* DetectDisassembler(const void* buffer, size_t length) {
     delete disassembler;
 
   disassembler = new DisassemblerElf32X86(buffer, length);
+  if (disassembler->ParseHeader())
+    return disassembler;
+  else
+    delete disassembler;
+
+  disassembler = new DisassemblerElf32ARM(buffer, length);
   if (disassembler->ParseHeader())
     return disassembler;
   else
