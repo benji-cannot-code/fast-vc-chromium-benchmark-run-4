1ebe47cd9d99e5baf5b24e64c104cce045095c74
pan.deng@intel.com <pan.deng@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 11:40:24 +0000

    Reland [Resource Timing] Expose redirect timing information
    This change contains redirect timing information for W3C Resource Timing spec, and tests.
    To fix previous land which break dbg build bot, this change enable reportLoadTiming for 
    XHR preflight, and allow null pointer m_timing in PerformanceResourceTiming for appcache case.
    
    Contributed by pan.deng@intel.com
    
    BUG=252066
    
    Review URL: https://chromiumcodereview.appspot.com/16826017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152773 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/core.gypi
+++ third_party/WebKit/Source/core/core.gypi
@@ -1197,6 +1197,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
             'page/PointerLockController.h',
             'page/PopupOpeningObserver.h',
             'page/PrintContext.cpp',
+            'page/ResourceTimingInfo.h',
             'page/RuntimeCSSEnabled.cpp',
             'page/RuntimeCSSEnabled.h',
             'page/RuntimeEnabledFeaturesCustom.cpp',
--- third_party/WebKit/Source/core/loader/cache/CachedResourceLoader.h
+++ third_party/WebKit/Source/core/loader/cache/CachedResourceLoader.h
@@ -56,6 +56,7 @@ class DocumentLoader;
 class Frame;
 class ImageLoader;
 class KURL;
+class ResourceTimingInfo;
 
 // The CachedResourceLoader provides a per-context interface to the MemoryCache
 // and enforces a bunch of security checks and rules for resource revalidation.
@@ -114,6 +115,7 @@ public:
 
     void loadDone(CachedResource*);
     void garbageCollectDocumentResources();
+    void redirectReceived(CachedResource*, const ResourceResponse&);
     
     void incrementRequestCount(const CachedResource*);
     void decrementRequestCount(const CachedResource*);
@@ -174,7 +176,8 @@ private:
 
     Timer<CachedResourceLoader> m_garbageCollectDocumentResourcesTimer;
 
-    HashMap<CachedResource*, CachedResourceInitiatorInfo> m_initiatorMap;
+    typedef HashMap<CachedResource*, RefPtr<ResourceTimingInfo> > ResourceTimingInfoMap;
+    ResourceTimingInfoMap m_resourceTimingInfoMap;
 
     // 29 bits left
     bool m_autoLoadImages : 1;
