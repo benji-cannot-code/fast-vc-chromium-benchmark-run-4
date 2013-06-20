e723f1467398979a2f62f95afc75d221aae86006
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 16:29:54 +0000

    Add limited user icon to the avatar menu (views).
    The screenshot can be found towards the end of the bug
    description (views_avatar_menu_opened.png)
    
    BUG=241387
    
    Review URL: https://chromiumcodereview.appspot.com/16273011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207446 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/avatar_menu_bubble_view.h
+++ chrome/browser/ui/views/avatar_menu_bubble_view.h
@@ -26,6 +26,7 @@ class WebContents;
 
 namespace views {
 class CustomButton;
+class ImageView;
 class Label;
 class Link;
 class Separator;
@@ -113,6 +114,7 @@ class AvatarMenuBubbleView : public views::BubbleDelegateView,
   // avatar_menu_model_->GetManagedUserInformation() returns a non-empty string.
   // See OnAvatarMenuModelChanged().
   views::Label* managed_user_info_;
+  views::ImageView* icon_view_;
   views::Separator* separator_switch_users_;
   views::Link* switch_profile_link_;
 
