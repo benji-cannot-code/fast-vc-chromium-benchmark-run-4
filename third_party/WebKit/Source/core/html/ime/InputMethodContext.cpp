0b34e099a3466326c756b84df3501cf6c9ba66bd
kochi@chromium.org <kochi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 11:41:55 +0000

    Implement confirmComposition() for InputMethodContext.
    confirmComposition() is expected to commit IME composition text if any.  The behavior is same as when an editor loses focus.
    
    This code assumes that the the node associated with the context is editable.
    
    BUG=238585
    TEST=layout test
    
    Review URL: https://chromiumcodereview.appspot.com/16093003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152774 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/ime/InputMethodContext.cpp
+++ third_party/WebKit/Source/core/html/ime/InputMethodContext.cpp
@@ -32,7 +32,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/html/ime/InputMethodContext.h"
 
+#include "core/editing/Editor.h"
 #include "core/html/ime/Composition.h"
+#include "core/page/EditorClient.h"
+#include "core/page/Frame.h"
+#include "core/page/Page.h"
 
 namespace WebCore {
 
@@ -81,7 +85,30 @@ String InputMethodContext::locale() const
 
 void InputMethodContext::confirmComposition()
 {
-    // FIXME: Implement this.
+    Frame* frame = m_element->document()->frame();
+    if (!frame)
+        return;
+    Editor* editor = frame->editor();
+    if (!editor->hasComposition())
+        return;
+
+    const Node* node = frame->document()->focusedNode();
+    if (!node || !node->isHTMLElement() || m_element != toHTMLElement(node))
+        return;
+
+    // We should verify the parent node of this IME composition node are
+    // editable because JavaScript may delete a parent node of the composition
+    // node. In this case, WebKit crashes while deleting texts from the parent
+    // node, which doesn't exist any longer.
+    RefPtr<Range> range = editor->compositionRange();
+    if (range) {
+        Node* node = range->startContainer();
+        if (!node || !node->isContentEditable())
+            return;
+    }
+
+    // This resets input method and the composition string is committed.
+    editor->client()->willSetInputMethodState();
 }
 
 void InputMethodContext::setCaretRectangle(Node* anchor, int x, int y, int w, int h)
