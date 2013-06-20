a940dd5d7ce4dc7639ba7e3f3f6d5117b9cd649c
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 02:38:35 +0000

    Add scroll prediction as a configurable gesture parameter.
    BUG=143242
    TEST=Enable scroll prediction flag and set scroll prediction value in chrome://gesture, log out and log back in. Scroll with finger and verify that scroll is keeping up better with finger movements.
    TEST=GestureRecognizerTest.GestureEventScrollPrediction
    
    Review URL: https://chromiumcodereview.appspot.com/15995037
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207279 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gesture_prefs_observer_factory_aura.cc
+++ chrome/browser/ui/gesture_prefs_observer_factory_aura.cc
@@ -312,6 +312,8 @@ void GesturePrefsObserver::Update() {
   GestureConfiguration::set_rail_start_proportion(
       prefs_->GetDouble(
           prefs::kRailStartProportion));
+  GestureConfiguration::set_scroll_prediction_seconds(
+      prefs_->GetDouble(prefs::kScrollPredictionSeconds));
 
   UpdateOverscrollPrefs();
   UpdateImmersiveModePrefs();
@@ -560,6 +562,10 @@ void GesturePrefsObserverFactoryAura::RegisterUserPrefs(
   registry->RegisterDoublePref(
       prefs::kRailStartProportion,
       GestureConfiguration::rail_start_proportion(),
+      user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
+  registry->RegisterDoublePref(
+      prefs::kScrollPredictionSeconds,
+      GestureConfiguration::scroll_prediction_seconds(),
       user_prefs::PrefRegistrySyncable::UNSYNCABLE_PREF);
 
   // Register for migration.
