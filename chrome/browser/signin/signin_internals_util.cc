45bfb7e5cad6191d808612041bf4978abcb4e635
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-20 03:12:03 +0000

    Remove an unused include.
    BUG=233552
    
    Review URL: https://chromiumcodereview.appspot.com/17058003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207297 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/signin_internals_util.cc
+++ chrome/browser/signin/signin_internals_util.cc
@@ -9,9 +9,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_number_conversions.h"
+#include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/common/chrome_version_info.h"
-#include "chrome/common/extensions/extension_messages.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/web_contents.h"
 #include "crypto/sha2.h"
