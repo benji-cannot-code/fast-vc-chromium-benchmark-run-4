e82d6e57d86ec83e651a18095da10df168a86e78
kaznacheev@chromium.org <kaznacheev@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-20 14:14:59 +0000

    InspectorInstrumentation: generate willProcessRule inline method
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/17351005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152786 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorInstrumentationCustomInl.h
+++ third_party/WebKit/Source/core/inspector/InspectorInstrumentationCustomInl.h
@@ -38,7 +38,6 @@ namespace InspectorInstrumentation {
 
 bool profilerEnabledImpl(InstrumentingAgents*);
 bool isDebuggerPausedImpl(InstrumentingAgents*);
-InspectorInstrumentationCookie willProcessRuleImpl(InstrumentingAgents*, StyleRule*, StyleResolver*);
 bool collectingHTMLParseErrorsImpl(InstrumentingAgents*);
 
 bool canvasAgentEnabled(ScriptExecutionContext*);
@@ -58,16 +57,6 @@ inline bool isDebuggerPaused(Frame* frame)
     if (InstrumentingAgents* instrumentingAgents = instrumentingAgentsForFrame(frame))
         return isDebuggerPausedImpl(instrumentingAgents);
     return false;
-}
-
-inline InspectorInstrumentationCookie willProcessRule(Document* document, StyleRule* rule, StyleResolver* styleResolver)
-{
-    FAST_RETURN_IF_NO_FRONTENDS(InspectorInstrumentationCookie());
-    if (!rule)
-        return InspectorInstrumentationCookie();
-    if (InstrumentingAgents* instrumentingAgents = instrumentingAgentsForDocument(document))
-        return willProcessRuleImpl(instrumentingAgents, rule, styleResolver);
-    return InspectorInstrumentationCookie();
 }
 
 inline bool collectingHTMLParseErrors(Page* page)
