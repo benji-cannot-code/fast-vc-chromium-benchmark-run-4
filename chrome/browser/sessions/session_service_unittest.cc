466db63802d52675cbb6d04042e9d5dca4ca67a8
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 19:37:29 +0000

    remove (unnecessary) include of third_party/WebKit WebHistoryItem.h
    BUG=237267
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/17550011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207905 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sessions/session_service_unittest.cc
+++ chrome/browser/sessions/session_service_unittest.cc
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/WebKit/public/platform/WebData.h"
 #include "third_party/WebKit/public/platform/WebHTTPBody.h"
-#include "third_party/WebKit/public/web/WebHistoryItem.h"
 
 using content::NavigationEntry;
 using sessions::SerializedNavigationEntry;
