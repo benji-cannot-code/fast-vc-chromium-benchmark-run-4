b5bfd03b08efa4dc84b073b5080a8e43b18b11d8
dmazzoni@chromium.org <dmazzoni@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 17:52:40 +0000

    Native Android accessibility.
    This is the final changelist that completes the initial implementation.
    When accessibility is on and script injection is turned off or
    unavailable, constructs a BrowserAccessibilityManager for the
    ContentViewCore that provides a native accessibility implementation.
    
    BUG=242953
    R=benm@chromium.org, bulach@chromium.org, dtrainor@chromium.org
    
    Review URL: https://codereview.chromium.org/15741009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207875 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_view_core_impl.cc
+++ content/browser/android/content_view_core_impl.cc
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/browser/web_contents/web_contents_view_android.h"
 #include "content/common/input_messages.h"
 #include "content/common/view_messages.h"
+#include "content/public/browser/browser_accessibility_state.h"
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/favicon_status.h"
 #include "content/public/browser/notification_details.h"
@@ -1570,6 +1571,19 @@ void ContentViewCoreImpl::SetUseDesktopUserAgent(
     NavigationControllerImpl& controller =
         static_cast<NavigationControllerImpl&>(web_contents_->GetController());
     controller.ReloadOriginalRequestURL(false);
+  }
+}
+
+void ContentViewCoreImpl::SetAccessibilityEnabled(JNIEnv* env, jobject obj,
+                                                  bool enabled) {
+  RenderWidgetHostImpl* host_impl = RenderWidgetHostImpl::From(
+      GetRenderWidgetHostViewAndroid()->GetRenderWidgetHost());
+  if (enabled) {
+    BrowserAccessibilityState::GetInstance()->EnableAccessibility();
+    host_impl->SetAccessibilityMode(AccessibilityModeComplete);
+  } else {
+    BrowserAccessibilityState::GetInstance()->DisableAccessibility();
+    host_impl->SetAccessibilityMode(AccessibilityModeOff);
   }
 }
 
--- content/public/android/java/src/org/chromium/content/browser/ContentView.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentView.java
@@ -18,6 +18,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.accessibility.AccessibilityEvent;
 import android.view.accessibility.AccessibilityNodeInfo;
+import android.view.accessibility.AccessibilityNodeProvider;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
 import android.widget.FrameLayout;
@@ -489,6 +490,14 @@ public class ContentView extends FrameLayout
         return mContentViewCore.onGenericMotionEvent(event);
     }
 
+    @Override
+    public boolean dispatchHoverEvent(MotionEvent event) {
+        if (mContentViewCore.dispatchHoverEvent(event)) {
+            return true;
+        }
+        return super.dispatchHoverEvent(event);
+    }
+
     /**
      * Sets the current amount to offset incoming touch events by.  This is used to handle content
      * moving and not lining up properly with the android input system.
@@ -577,6 +586,16 @@ public class ContentView extends FrameLayout
 
     public int getSingleTapY()  {
         return mContentViewCore.getContentViewGestureHandler().getSingleTapY();
+    }
+
+    @Override
+    public AccessibilityNodeProvider getAccessibilityNodeProvider() {
+        AccessibilityNodeProvider provider = mContentViewCore.getAccessibilityNodeProvider();
+        if (provider != null) {
+            return provider;
+        } else {
+            return super.getAccessibilityNodeProvider();
+        }
     }
 
     @Override
