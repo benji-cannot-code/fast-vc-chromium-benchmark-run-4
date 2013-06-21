c07c3be5876ad91e3a69aee3df79c09b4baa17a9
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 07:15:47 +0000

    Move ProfileLoader to chrome/browser/profiles.
    The class fits better here because it only deals with profiles.
    Added profile_loader_unittest.cc
    
    BUG=168080
    TEST=There should be no change in behavior.
    
    Review URL: https://chromiumcodereview.appspot.com/16766003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207745 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_service_impl.h
+++ chrome/browser/ui/app_list/app_list_service_impl.h
@@ -14,8 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_info_cache_observer.h"
+#include "chrome/browser/profiles/profile_loader.h"
 #include "chrome/browser/ui/app_list/app_list_service.h"
-#include "chrome/browser/ui/app_list/profile_loader.h"
 #include "content/public/browser/notification_observer.h"
 #include "content/public/browser/notification_registrar.h"
 
