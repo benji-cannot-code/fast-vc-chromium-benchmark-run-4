03c5b3e9b1fae55fc0c27481c1db66304b2ebf24
nasko@chromium.org <nasko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 15:23:39 +0000

    Implement WebFrameClient in RenderFrame and proxy all calls to RenderView (for now).
    This is continuation of the original CL (https://codereview.chromium.org/16223006/), which didn't account for Blink tests using WebTestProxy.
    
    BUG=245126
    
    Review URL: https://chromiumcodereview.appspot.com/17114006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207825 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/render_frame_impl.cc
+++ content/renderer/render_frame_impl.cc
@@ -10,6 +10,31 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace content {
 
+static RenderFrameImpl* (*g_create_render_frame_impl)(RenderViewImpl*, int32) =
+    NULL;
+
+// static
+RenderFrameImpl* RenderFrameImpl::Create(
+    RenderViewImpl* render_view,
+    int32 routing_id) {
+  DCHECK(routing_id != MSG_ROUTING_NONE);
+
+  RenderFrameImpl* render_frame = NULL;
+  if (g_create_render_frame_impl)
+    render_frame = g_create_render_frame_impl(render_view, routing_id);
+  else
+    render_frame = new RenderFrameImpl(render_view, routing_id);
+
+  return render_frame;
+}
+
+// static
+void RenderFrameImpl::InstallCreateHook(
+    RenderFrameImpl* (*create_render_frame_impl)(RenderViewImpl*, int32)) {
+  CHECK(!g_create_render_frame_impl);
+  g_create_render_frame_impl = create_render_frame_impl;
+}
+
 RenderFrameImpl::RenderFrameImpl(RenderViewImpl* render_view, int routing_id)
     : render_view_(render_view),
       routing_id_(routing_id) {
