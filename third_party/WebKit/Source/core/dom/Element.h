6cbeb3e1c044905007181bebd019bf492c4ec3be
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-21 07:19:51 +0000

    Replace static_cast<Element*> with toElement
    Turns out only Node had some left.
    
    Review URL: https://chromiumcodereview.appspot.com/17094020
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152866 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -771,7 +771,7 @@ inline Element* Node::previousElementSibling() const
     Node* n = previousSibling();
     while (n && !n->isElementNode())
         n = n->previousSibling();
-    return static_cast<Element*>(n);
+    return toElement(n);
 }
 
 inline Element* Node::nextElementSibling() const
@@ -779,7 +779,7 @@ inline Element* Node::nextElementSibling() const
     Node* n = nextSibling();
     while (n && !n->isElementNode())
         n = n->nextSibling();
-    return static_cast<Element*>(n);
+    return toElement(n);
 }
 
 inline bool Element::fastHasAttribute(const QualifiedName& name) const
