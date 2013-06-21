c0f2dff68e226fbb68c71c2d38b59495abacb322
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 10:25:35 +0000

    Implement 'invalid' AutocompleteErrorEvent#reason
    R=isherman@chromium.org
    BUG=168967
    
    Review URL: https://chromiumcodereview.appspot.com/17391012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207791 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -598,6 +598,10 @@ void AutofillDialogControllerImpl::OnAutocheckoutSuccess() {
   view_->UpdateButtonStrip();
 }
 
+TestableAutofillDialogView* AutofillDialogControllerImpl::GetTestableView() {
+  return view_ ? view_->GetTestableView() : NULL;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 // AutofillDialogController implementation.
 
