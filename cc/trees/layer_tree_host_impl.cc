3ab519692e87f5667d16c13ffc05c99be9fa09e1
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 02:49:15 +0000

    Remove NULL ResourceProvider, TileManager early outs
    These early outs were added in r201247 as part of resourceless software
    mode in which ResourceProvider and TileManager were not created. This
    is no longer the case after r207424, so remove these early outs.
    
    For Android WebView, this does not affect software mode, and does not
    make hardware any more broken than it already is.
    
    BUG=245935
    
    Review URL: https://chromiumcodereview.appspot.com/17176012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207673 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_impl.cc
+++ cc/trees/layer_tree_host_impl.cc
@@ -752,10 +752,8 @@ bool LayerTreeHostImpl::CalculateRenderPasses(FrameData* frame) {
   RemoveRenderPasses(CullRenderPassesWithNoQuads(), frame);
   if (!output_surface_->ForcedDrawToSoftwareDevice())
     renderer_->DecideRenderPassAllocationsForFrame(frame->render_passes);
-  if (renderer_) {
-    RemoveRenderPasses(CullRenderPassesWithCachedTextures(renderer_.get()),
-                       frame);
-  }
+  RemoveRenderPasses(CullRenderPassesWithCachedTextures(renderer_.get()),
+                     frame);
 
   // If we're making a frame to draw, it better have at least one render pass.
   DCHECK(!frame->render_passes.empty());
@@ -1223,8 +1221,7 @@ void LayerTreeHostImpl::DidDrawAllLayers(const FrameData& frame) {
 
   // Once all layers have been drawn, pending texture uploads should no
   // longer block future uploads.
-  if (resource_provider_)
-    resource_provider_->MarkPendingUploadsAsNonBlocking();
+  resource_provider_->MarkPendingUploadsAsNonBlocking();
 }
 
 void LayerTreeHostImpl::FinishAllRendering() {
