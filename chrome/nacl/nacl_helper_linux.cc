1b347ef10c0c3d35f86c64059f477b6933487cce
bsy@google.com <bsy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 03:57:58 +0000

    Init NaClChromeMainArgs number_of_cores member so apps can size threadpools
    The outer sandbox on Linux (and OSX?) was preventing
    sysconf(_SC_NPROCESSORS_ONLN) from succeeding, so that NaCl
    applications that need the number of processors/cores to, e.g., figure
    out the appropriate size for threadpools were getting a bogus value
    (-1 when using newlib and 1 when using glibc).
    
    This is a reland of r207350
    (https://chromiumcodereview.appspot.com/16761002) which was reverted
    because of test failures on Win XP and Aura build bots -- even though
    the test passed on an old Win XP laptop. This includes a bugfix
    (thanks bradn) in _snwprintf_s, which *shouldn't* have been the
    culprit, but may have been.
    
    The interesting (and smelly) bit is that the same change (modulo minor
    variations to get more debugging output) has landed and reverted due
    to consistent failures on a small set of bots each time: first it was
    XP, next Win7 and Win Aura, and the most recent, XP and Aura; however,
    on other Windows bots it runs fine.
    
    Update: bradn checked _snwprintf_s behavior, and the conjecture that
    it always puts a NUL at the end of the buffer is correct.  Thus, it
    seems likely that stack content variations allowed the test to pass on
    most of the Windows bots, and to fail on others.  However, I'll leave
    the test disabled for Windows for now, and try re-enablement in another
    CL.
    
    The test is disabled for Windows for now, with re-enablement after
    more investigation.  See
    https://code.google.com/p/chromium/issues/detail?id=252166 which
    tracks that.
    
    TEST= browser_tests --gtest_filter=*Sysconf*
    BUG=176522
    
    Review URL: https://chromiumcodereview.appspot.com/17448020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207685 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/nacl/nacl_helper_linux.cc
+++ chrome/nacl/nacl_helper_linux.cc
@@ -41,7 +41,8 @@ namespace {
 // side of the fork. See zygote_main_linux.cc:HandleForkRequest from
 //   if (!child) {
 void BecomeNaClLoader(const std::vector<int>& child_fds,
-                      size_t prereserved_sandbox_size) {
+                      size_t prereserved_sandbox_size,
+                      int number_of_cores) {
   VLOG(1) << "NaCl loader: setting up IPC descriptor";
   // don't need zygote FD any more
   if (HANDLE_EINTR(close(kNaClZygoteDescriptor)) != 0)
@@ -57,6 +58,7 @@ void BecomeNaClLoader(const std::vector<int>& child_fds,
   base::MessageLoopForIO main_message_loop;
   NaClListener listener;
   listener.set_prereserved_sandbox_size(prereserved_sandbox_size);
+  listener.set_number_of_cores(number_of_cores);
   listener.Listen();
   _exit(0);
 }
@@ -64,7 +66,8 @@ void BecomeNaClLoader(const std::vector<int>& child_fds,
 // Some of this code was lifted from
 // content/browser/zygote_main_linux.cc:ForkWithRealPid()
 void HandleForkRequest(const std::vector<int>& child_fds,
-                       size_t prereserved_sandbox_size) {
+                       size_t prereserved_sandbox_size,
+                       int number_of_cores) {
   VLOG(1) << "nacl_helper: forking";
   pid_t childpid = fork();
   if (childpid < 0) {
@@ -102,7 +105,7 @@ void HandleForkRequest(const std::vector<int>& child_fds,
     if (HANDLE_EINTR(close(child_fds[kNaClParentFDIndex])) != 0)
       LOG(ERROR) << "close(child_fds[kNaClParentFDIndex]) failed";
     if (validack) {
-      BecomeNaClLoader(child_fds, prereserved_sandbox_size);
+      BecomeNaClLoader(child_fds, prereserved_sandbox_size, number_of_cores);
     } else {
       LOG(ERROR) << "Failed to synch with zygote";
     }
@@ -240,6 +243,7 @@ int main(int argc, char* argv[]) {
 #endif
   std::vector<int> empty; // for SendMsg() calls
   size_t prereserved_sandbox_size = CheckReservedAtZero();
+  int number_of_cores = sysconf(_SC_NPROCESSORS_ONLN);
 
   CheckRDebug(argv[0]);
 
@@ -276,7 +280,7 @@ int main(int argc, char* argv[]) {
     } else if (msglen == sizeof(kNaClForkRequest) - 1 &&
                memcmp(buf, kNaClForkRequest, msglen) == 0) {
       if (kNaClParentFDIndex + 1 == fds.size()) {
-        HandleForkRequest(fds, prereserved_sandbox_size);
+        HandleForkRequest(fds, prereserved_sandbox_size, number_of_cores);
         continue;  // fork succeeded. Note: child does not return
       } else {
         LOG(ERROR) << "nacl_helper: unexpected number of fds, got "
