8afd3a11bebde5c55a8a8fb49b5c47de94b64c8d
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 04:08:57 +0000

    Call OnDisplayBoundsChanged when bounds are changed due to layout.
    BUG=247059
    TEST=covered by test
    
    Review URL: https://chromiumcodereview.appspot.com/17379003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207694 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_manager.h
+++ ash/display/display_manager.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_export.h"
+#include "ash/display/display_controller.h"
 #include "ash/display/display_info.h"
 #include "base/compiler_specific.h"
 #include "base/gtest_prod_util.h"
@@ -54,6 +55,12 @@ class ASH_EXPORT DisplayManager :
 
   // Returns next valid UI scale.
   static float GetNextUIScale(const DisplayInfo& info, bool up);
+
+  // Updates the bounds of |secondary_display| according to |layout|.
+  static void UpdateDisplayBoundsForLayout(
+      const DisplayLayout& layout,
+      const gfx::Display& primary_display,
+      gfx::Display* secondary_display);
 
   // When set to true, the MonitorManager calls OnDisplayBoundsChanged
   // even if the display's bounds didn't change. Used to swap primary
@@ -188,7 +195,7 @@ class ASH_EXPORT DisplayManager :
   void SetSoftwareMirroring(bool enabled);
 #endif
 
- private:
+private:
   FRIEND_TEST_ALL_PREFIXES(ExtendedDesktopTest, ConvertPoint);
   FRIEND_TEST_ALL_PREFIXES(DisplayManagerTest, TestNativeDisplaysChanged);
   FRIEND_TEST_ALL_PREFIXES(DisplayManagerTest,
@@ -226,6 +233,14 @@ class ASH_EXPORT DisplayManager :
 
   // Creates a display object from the DisplayInfo for |display_id|.
   gfx::Display CreateDisplayFromDisplayInfoById(int64 display_id);
+
+  // Updates the bounds of the secondary display in |display_list|
+  // using the layout registered for the display pair and set the
+  // index of display updated to |updated_index|. Returns true
+  // if the secondary display's bounds has been changed from current
+  // value, or false otherwise.
+  bool UpdateSecondaryDisplayBoundsForLayout(DisplayList* display_list,
+                                             size_t* updated_index) const;
 
   int64 first_display_id_;
 
