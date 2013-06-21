be048cde45a038de7dc8e0a9ce7f6489a69df723
blundell@chromium.org <blundell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 06:04:46 +0000

    Move IPC reception handling from AutofillManager to AutofillDriverImpl.
    As AutofillManager will be shared by iOS, it should not receive IPC directly.
    Instead, AutofillDriverImpl now handles IPC reception and forwards to
    AutofillManager as appropriate.
    
    Similarly, this CL also abstracts the content-level concepts of handling frame
    navigation out of AutofillManager into AutofillDriverImpl.
    
    BUG=247015
    
    Review URL: https://chromiumcodereview.appspot.com/17382007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207726 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autocomplete_history_manager.cc
+++ components/autofill/core/browser/autocomplete_history_manager.cc
@@ -20,7 +20,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/browser_context.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
-#include "ipc/ipc_message_macros.h"
 
 using content::BrowserContext;
 using content::WebContents;
