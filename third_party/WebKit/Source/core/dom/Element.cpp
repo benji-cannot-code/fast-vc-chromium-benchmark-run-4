6db1fa26008a3ce10427aa68cf29a004eb7092e3
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-21 03:48:39 +0000

    DevTools: didRecalculateStyleForElement should only be called if style really was recalced
    Currently we call didRecalculateStyleForElement as we walk down the tree to find
    the elements that actually need a style recalc and we don't ever call it when
    doing a reattach of the element.
    
    This moves the call into the if statement in Element::recalcStyle so we'll only
    do it when actually recomputing the style or when doing an attach from inside
    recalcStyle (which handles lazyAttach).
    
    The count of the number of elements that went through a "Recalculate Style"
    in the web inspector should be much more accurate.
    
    I also updated timeline-recalculate-styles.html to exercise this new behavior. The
    expected result is the same, but the number of nodes we walk through is much
    higher.
    
    TEST=LayoutTests/inspector/timeline/timeline-recalculate-styles.html
    
    Review URL: https://chromiumcodereview.appspot.com/14842007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152854 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.cpp
+++ third_party/WebKit/Source/core/dom/Element.cpp
@@ -1331,6 +1331,12 @@ void Element::attach(const AttachContext& context)
             data->setNeedsFocusAppearanceUpdateSoonAfterAttach(false);
         }
     }
+
+    // FIXME: It doesn't appear safe to call didRecalculateStyleForElement when
+    // not in a Document::recalcStyle. Since we're hopefully going to always
+    // lazyAttach in the future that problem should go away.
+    if (document()->inStyleRecalc())
+        InspectorInstrumentation::didRecalculateStyleForElement(this);
 }
 
 void Element::unregisterNamedFlowContentNode()
@@ -1455,6 +1461,8 @@ void Element::recalcStyle(StyleChange change)
             return;
         }
 
+        InspectorInstrumentation::didRecalculateStyleForElement(this);
+
         if (RenderObject* renderer = this->renderer()) {
             if (localChange != NoChange || pseudoStyleCacheIsInvalid(currentStyle.get(), newStyle.get()) || (change == Force && renderer->requiresForcedStyleRecalcPropagation()) || styleChangeType() == SyntheticStyleChange)
                 renderer->setAnimatableStyle(newStyle.get());
@@ -1520,10 +1528,9 @@ void Element::recalcStyle(StyleChange change)
 
     clearNeedsStyleRecalc();
     clearChildNeedsStyleRecalc();
-    
+
     if (hasCustomStyleCallbacks())
         didRecalcStyle(change);
-    InspectorInstrumentation::didRecalculateStyleForElement(this);
 }
 
 ElementShadow* Element::shadow() const
