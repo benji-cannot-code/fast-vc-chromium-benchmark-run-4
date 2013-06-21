761552e314befb3a24afff8635a4cb3b39aa375e
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-21 10:35:52 +0000

    Match the NativeTextfieldViews cursor color to the text color.
    The NativeTextfieldViews cursor color is hard-coded black.
    Instead, set the cursor color to Textfield::GetTextColor.
    This supports more Windows high-contrast black themes, etc.
    However, if Windows has red text, they'll get red cursors.
    (I think accessibility trumps that questionable coloring)
    (also, the native behavior is worse in my ugly test theme)
    
    BUG=252385,131660
    TEST=Views Textfields cursors match their overall text color; meets accessibility requirements without other seriously negative effects.
    R=oshima@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/17517005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207795 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/textfield/native_textfield_views.cc
+++ ui/views/controls/textfield/native_textfield_views.cc
@@ -55,9 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace {
 
-// Default "system" color for text cursor.
-const SkColor kDefaultCursorColor = SK_ColorBLACK;
-
 void ConvertRectToScreen(const views::View* src, gfx::Rect* r) {
   DCHECK(src);
 
@@ -1119,7 +1116,7 @@ void NativeTextfieldViews::UpdateColorsFromTheme(const ui::NativeTheme* theme) {
   UpdateTextColor();
   UpdateBackgroundColor();
   gfx::RenderText* render_text = GetRenderText();
-  render_text->set_cursor_color(kDefaultCursorColor);
+  render_text->set_cursor_color(textfield_->GetTextColor());
   render_text->set_selection_color(theme->GetSystemColor(
       ui::NativeTheme::kColorId_TextfieldSelectionColor));
   render_text->set_selection_background_focused_color(theme->GetSystemColor(
