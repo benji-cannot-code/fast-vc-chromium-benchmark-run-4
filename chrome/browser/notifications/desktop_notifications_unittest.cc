feef43c3b86df1ec90bbe01eb9d13d22d70a655e
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-22 15:01:30 +0000

    Remove ActiveDesktopMonitor from DesktopNotificationsUnittests
    An ActiveDesktopMonitor is actually not necessary here I believe (a window is never activated so the monitor simply returns the default host desktop type (which is the same as the type returned when there is no monitor).
    
    BUG=179830
    
    Review URL: https://chromiumcodereview.appspot.com/17074007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208051 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/desktop_notifications_unittest.cc
+++ chrome/browser/notifications/desktop_notifications_unittest.cc
@@ -22,7 +22,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(USE_ASH)
 #include "ash/shell.h"
 #include "ash/test/test_shell_delegate.h"
-#include "chrome/browser/ui/aura/active_desktop_monitor.h"
 #include "third_party/WebKit/public/web/WebKit.h"
 #include "ui/aura/env.h"
 #include "ui/aura/root_window.h"
@@ -114,7 +113,6 @@ void DesktopNotificationsTest::SetUp() {
   // MockBalloonCollection retrieves information about the screen on creation.
   // So it is necessary to make sure the desktop gets created first.
   ash::Shell::CreateInstance(new ash::test::TestShellDelegate);
-  active_desktop_monitor_.reset(new ActiveDesktopMonitor);
 #endif
 
   chrome::RegisterLocalState(local_state_.registry());
@@ -131,7 +129,6 @@ void DesktopNotificationsTest::TearDown() {
   ui_manager_.reset(NULL);
   profile_.reset(NULL);
 #if defined(USE_ASH)
-  active_desktop_monitor_.reset();
   ash::Shell::DeleteInstance();
   // The message center is notmally shutdown on |g_browser_process| which
   // is not created for these tests.
