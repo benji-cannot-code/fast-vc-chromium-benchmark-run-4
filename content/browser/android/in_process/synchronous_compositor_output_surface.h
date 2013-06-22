6815211349ad8e684d64e80c225dbce073fe4cfa
joth@chromium.org <joth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-22 07:06:14 +0000

    Force spontaneous draws into the SW path
    Any unrequested draws will cause UI corruption if they hit the GL path.
    The strategy is to disallow any such draws, but to enable diagnosis
    force them into the SW path where they cause less UI damange but we can
    log or assert as appropriate.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17553012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208014 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/in_process/synchronous_compositor_output_surface.h
+++ content/browser/android/in_process/synchronous_compositor_output_surface.h
@@ -80,6 +80,7 @@ class SynchronousCompositorOutputSurface
 
   int routing_id_;
   bool needs_begin_frame_;
+  bool invoking_composite_;
   bool did_swap_buffer_;
 
   // Only valid (non-NULL) during a DemandDrawSw() call.
