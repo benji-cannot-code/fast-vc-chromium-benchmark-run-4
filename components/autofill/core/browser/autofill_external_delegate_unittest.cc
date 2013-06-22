f97b58af72beb677b127c2dac204c20de84bf566
nyquist@chromium.org <nyquist@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-22 13:48:42 +0000

    [autofill] Add support for PSL domain matching for password autofill.
    * Adds support for using the PSL for matching the origin when looking
    up passwords using the default password store.
    * On Android, change the layout for label and sublabels in autofill
    popups.
    
    The feature is currently behind the flag
    --enable-password-autofill-psl-domain-matching
    
    BUG=176386
    
    Review URL: https://chromiumcodereview.appspot.com/15660018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208022 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autofill_external_delegate_unittest.cc
+++ components/autofill/core/browser/autofill_external_delegate_unittest.cc
@@ -301,8 +301,11 @@ TEST_F(AutofillExternalDelegateUnitTest,
 // suggestions.
 TEST_F(AutofillExternalDelegateUnitTest, ExternalDelegatePasswordSuggestions) {
   static const base::string16 kUsername = ASCIIToUTF16("username");
+  static const base::string16 kSignonRealm = ASCIIToUTF16("http://foo.com/");
   std::vector<base::string16> suggestions;
   suggestions.push_back(kUsername);
+  std::vector<base::string16> realms;
+  realms.push_back(kSignonRealm);
 
   FormFieldData field;
   field.is_focusable = true;
@@ -325,6 +328,7 @@ TEST_F(AutofillExternalDelegateUnitTest, ExternalDelegatePasswordSuggestions) {
                   _));
 
   external_delegate_->OnShowPasswordSuggestions(suggestions,
+                                                realms,
                                                 field,
                                                 element_bounds);
 
