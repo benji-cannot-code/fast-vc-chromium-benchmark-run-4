98f5c856ace2fc3added7aaac166d9a0837f11ca
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-22 22:34:13 +0000

    Add logging to the QUIC write path.
    Review URL: https://chromiumcodereview.appspot.com/17518002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208098 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_packet_generator.cc
+++ net/quic/quic_packet_generator.cc
@@ -14,8 +14,10 @@ using base::StringPiece;
 namespace net {
 
 QuicPacketGenerator::QuicPacketGenerator(DelegateInterface* delegate,
+                                         DebugDelegateInterface* debug_delegate,
                                          QuicPacketCreator* creator)
     : delegate_(delegate),
+      debug_delegate_(debug_delegate),
       packet_creator_(creator),
       should_flush_(true),
       should_send_ack_(false),
@@ -78,7 +80,7 @@ QuicConsumedData QuicPacketGenerator::ConsumeData(QuicStreamId id,
     QuicFrame frame;
     size_t bytes_consumed = packet_creator_->CreateStreamFrame(
         id, data, offset + total_bytes_consumed, fin, &frame);
-    bool success = packet_creator_->AddSavedFrame(frame);
+    bool success = AddFrame(frame);
     DCHECK(success);
 
     total_bytes_consumed += bytes_consumed;
@@ -159,7 +161,7 @@ bool QuicPacketGenerator::HasPendingFrames() const {
 bool QuicPacketGenerator::AddNextPendingFrame() {
   if (should_send_ack_) {
     pending_ack_frame_.reset(delegate_->CreateAckFrame());
-    if (!packet_creator_->AddSavedFrame(QuicFrame(pending_ack_frame_.get()))) {
+    if (!AddFrame(QuicFrame(pending_ack_frame_.get()))) {
       // packet was full
       return false;
     }
@@ -169,8 +171,7 @@ bool QuicPacketGenerator::AddNextPendingFrame() {
 
   if (should_send_feedback_) {
     pending_feedback_frame_.reset(delegate_->CreateFeedbackFrame());
-    if (!packet_creator_->AddSavedFrame(QuicFrame(
-            pending_feedback_frame_.get()))) {
+    if (!AddFrame(QuicFrame(pending_feedback_frame_.get()))) {
       // packet was full
       return false;
     }
@@ -179,12 +180,20 @@ bool QuicPacketGenerator::AddNextPendingFrame() {
   }
 
   DCHECK(!queued_control_frames_.empty());
-  if (!packet_creator_->AddSavedFrame(queued_control_frames_.back())) {
+  if (!AddFrame(queued_control_frames_.back())) {
     // packet was full
     return false;
   }
   queued_control_frames_.pop_back();
   return true;
+}
+
+bool QuicPacketGenerator::AddFrame(const QuicFrame& frame) {
+  bool success = packet_creator_->AddSavedFrame(frame);
+  if (success && debug_delegate_) {
+    debug_delegate_->OnFrameAddedToPacket(frame);
+  }
+  return success;
 }
 
 void QuicPacketGenerator::SerializeAndSendPacket() {
