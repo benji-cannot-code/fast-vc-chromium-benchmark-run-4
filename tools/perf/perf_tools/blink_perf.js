1b449275a9c0b8a6dd569477a539d89b89925772
tonyg@chromium.org <tonyg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-22 00:36:37 +0000

    [Telemetry] Expose v8's window.gc() method to the PerfTestRunner.
    Blink's PerfTestRunner has a gc method which tries to call
    window.GCController.collect(), if it doesn't exist, it allocates a whole
    bunch of memory to try to trigger a gc.
    
    Since the blink_perf Measurement already passes --js-flags=--expose_gc,
    we can call the window.gc() method instead of this hackery.
    
    I'm hopeful that this will fix the Bindings/gc-tree.html crashes on
    android.
    
    BUG=252897
    R=dtu@chromium.org
    
    Review URL: https://codereview.chromium.org/17472007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@207975 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/perf_tools/blink_perf.js
+++ tools/perf/perf_tools/blink_perf.js
@@ -12,4 +12,10 @@ testRunner.dumpAsText = function() {};
 testRunner.notifyDone = function() {
   this.isDone = true;
 };
+
+window.GCController = {};
+
+GCController.collect = function() {
+  gc();
+};
 })();
