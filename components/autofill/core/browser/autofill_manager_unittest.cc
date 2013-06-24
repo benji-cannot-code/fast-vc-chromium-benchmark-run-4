aa4d826feafa7b1f554dd6941ccc5050f9bdd35f
joi@chromium.org <joi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-24 16:21:12 +0000

    Removing a bunch of unnecessary exclusions from components/autofill/core/browser/DEPS.
    TBR=caitkp@chromium.org
    BUG=140037
    
    Review URL: https://chromiumcodereview.appspot.com/17155015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208216 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autofill_manager_unittest.cc
+++ components/autofill/core/browser/autofill_manager_unittest.cc
@@ -24,7 +24,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/browser/ui/autofill/tab_autofill_manager_delegate.h"
-#include "chrome/browser/ui/browser.h"
 #include "chrome/test/base/chrome_render_view_host_test_harness.h"
 #include "chrome/test/base/testing_profile.h"
 #include "components/autofill/core/browser/autocomplete_history_manager.h"
