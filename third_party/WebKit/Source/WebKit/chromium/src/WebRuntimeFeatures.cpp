54c0cf46b188d37d8698a78a53775d6631047bff
urvang@google.com <urvang@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-24 07:19:34 +0000

    Remove WebPInAcceptHeader flag from blink
    This feature is now enabled, so removing the flag.
    
    BUG=249474
    
    Review URL: https://chromiumcodereview.appspot.com/17341007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152939 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/WebKit/chromium/src/WebRuntimeFeatures.cpp
@@ -583,16 +583,6 @@ bool WebRuntimeFeatures::isWebMIDIEnabled()
     return RuntimeEnabledFeatures::webMIDIEnabled();
 }
 
-void WebRuntimeFeatures::enableWebPInAcceptHeader(bool enable)
-{
-    RuntimeEnabledFeatures::setWebPInAcceptHeaderEnabled(enable);
-}
-
-bool WebRuntimeFeatures::isWebPInAcceptHeaderEnabled()
-{
-    return RuntimeEnabledFeatures::webPInAcceptHeaderEnabled();
-}
-
 void WebRuntimeFeatures::enableDataListElement(bool enable)
 {
     RuntimeEnabledFeatures::setDataListElementEnabled(enable);
