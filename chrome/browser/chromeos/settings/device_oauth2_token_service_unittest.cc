434a60353e24587af6060815dc77d4bb847adbb6
joaodasilva@chromium.org <joaodasilva@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-24 16:05:27 +0000

    Fixed leaks in DeviceOAuth2TokenServiceTest.
    The OAuth2TokenService::Fetcher objects are deleted with a DeleteSoon on the
    main loop, and the loop must be flushed to ensure they are deleted before the
    test is done.
    
    TBR=davidroche@chromium.org
    BUG=253434
    
    Review URL: https://chromiumcodereview.appspot.com/17578003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208214 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/settings/device_oauth2_token_service_unittest.cc
+++ chrome/browser/chromeos/settings/device_oauth2_token_service_unittest.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/message_loop.h"
 #include "base/prefs/testing_pref_service.h"
+#include "base/run_loop.h"
 #include "chrome/browser/signin/oauth2_token_service_test_util.h"
 #include "chrome/common/pref_names.h"
 #include "chrome/test/base/scoped_testing_local_state.h"
@@ -84,6 +85,7 @@ class DeviceOAuth2TokenServiceTest : public testing::Test {
 
   virtual void TearDown() OVERRIDE {
     CryptohomeLibrary::SetForTest(NULL);
+    base::RunLoop().RunUntilIdle();
   }
 
   // Utility method to set a value in Local State for the device refresh token
