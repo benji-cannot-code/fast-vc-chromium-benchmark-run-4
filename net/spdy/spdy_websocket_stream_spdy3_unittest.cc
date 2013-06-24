4e65b1f2e5e0e5bc4edd7c6fe48e242595fb3845
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-24 23:28:49 +0000

    [SPDY] Unfork spdy_websocket_test_util_spdy{2,3}.* files
    Introduce new SpdyWebSocketTestUtil class and use it in all
    SPDY WebSocket tests.
    
    BUG=226192
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/17601002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208295 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_websocket_stream_spdy3_unittest.cc
+++ net/spdy/spdy_websocket_stream_spdy3_unittest.cc
@@ -17,11 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/spdy/spdy_http_utils.h"
 #include "net/spdy/spdy_protocol.h"
 #include "net/spdy/spdy_session.h"
-#include "net/spdy/spdy_test_util_spdy3.h"
-#include "net/spdy/spdy_websocket_test_util_spdy3.h"
+#include "net/spdy/spdy_websocket_test_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
-
-using namespace net::test_spdy3;
 
 namespace net {
 
@@ -190,7 +187,12 @@ class SpdyWebSocketStreamSpdy3Test : public testing::Test {
  protected:
   SpdyWebSocketStreamSpdy3Test()
       : spdy_util_(kProtoSPDY3),
-        session_deps_(kProtoSPDY3) {}
+        spdy_settings_id_to_set_(SETTINGS_MAX_CONCURRENT_STREAMS),
+        spdy_settings_flags_to_set_(SETTINGS_FLAG_PLEASE_PERSIST),
+        spdy_settings_value_to_set_(1),
+        session_deps_(kProtoSPDY3),
+        stream_id_(0),
+        created_stream_id_(0) {}
   virtual ~SpdyWebSocketStreamSpdy3Test() {}
 
   virtual void SetUp() {
@@ -200,10 +202,6 @@ class SpdyWebSocketStreamSpdy3Test : public testing::Test {
     spdy_session_key_ = SpdySessionKey(host_port_pair_,
                                        ProxyServer::Direct(),
                                        kPrivacyModeDisabled);
-
-    spdy_settings_id_to_set_ = SETTINGS_MAX_CONCURRENT_STREAMS;
-    spdy_settings_flags_to_set_ = SETTINGS_FLAG_PLEASE_PERSIST;
-    spdy_settings_value_to_set_ = 1;
 
     spdy_settings_to_send_[spdy_settings_id_to_set_] =
         SettingsFlagsAndValue(
@@ -217,21 +215,22 @@ class SpdyWebSocketStreamSpdy3Test : public testing::Test {
   void Prepare(SpdyStreamId stream_id) {
     stream_id_ = stream_id;
 
-    request_frame_.reset(ConstructSpdyWebSocketSynStream(
+    request_frame_.reset(spdy_util_.ConstructSpdyWebSocketSynStream(
         stream_id_,
         "/echo",
         "example.com",
         "http://example.com/wsdemo"));
 
-    response_frame_.reset(ConstructSpdyWebSocketSynReply(stream_id_));
+    response_frame_.reset(
+        spdy_util_.ConstructSpdyWebSocketSynReply(stream_id_));
 
-    message_frame_.reset(ConstructSpdyWebSocketDataFrame(
+    message_frame_.reset(spdy_util_.ConstructSpdyWebSocketDataFrame(
         kMessageFrame,
         kMessageFrameLength,
         stream_id_,
         false));
 
-    closing_frame_.reset(ConstructSpdyWebSocketDataFrame(
+    closing_frame_.reset(spdy_util_.ConstructSpdyWebSocketDataFrame(
         kClosingFrame,
         kClosingFrameLength,
         stream_id_,
@@ -285,7 +284,7 @@ class SpdyWebSocketStreamSpdy3Test : public testing::Test {
     websocket_stream_->SendRequest(headers.Pass());
   }
 
-  SpdyTestUtil spdy_util_;
+  SpdyWebSocketTestUtil spdy_util_;
   SpdySettingsIds spdy_settings_id_to_set_;
   SpdySettingsFlags spdy_settings_flags_to_set_;
   uint32 spdy_settings_value_to_set_;
