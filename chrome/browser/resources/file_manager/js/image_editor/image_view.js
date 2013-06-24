fb53394b92b674f122ab95824aa540daf585daed
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-24 09:47:45 +0000

    Fix rendering thumbnail previews in Files.app's gallery.
    Previous video fix introduced a regression causing rendering the thumbnails twice. This patch resolves this issue and cleans up the code a little by removing some redundant code.
    
    TEST=Test playing both videos, also test loading an image from Frive to see the thumbnail preview.
    BUG=177371
    R=hirono@chromium.org
    
    Review URL: https://codereview.chromium.org/17575009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208181 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/image_editor/image_view.js
+++ chrome/browser/resources/file_manager/js/image_editor/image_view.js
@@ -550,6 +550,7 @@ ImageView.prototype.replaceContent_ = function(
     this.videoElement_ = content;
     this.screenImage_ = content;
     this.screenImage_.className = 'image';
+    this.container_.appendChild(this.screenImage_);
     this.videoElement_.play();
     return;
   }
@@ -639,7 +640,6 @@ ImageView.prototype.replace = function(
   if (!opt_effect) {
     if (oldScreenImage)
       oldScreenImage.parentNode.removeChild(oldScreenImage);
-    this.container_.appendChild(content);
     return;
   }
 
@@ -648,7 +648,6 @@ ImageView.prototype.replace = function(
   if (oldScreenImage)
     ImageUtil.setAttribute(newScreenImage, 'fade', true);
   this.setTransform(newScreenImage, opt_effect, 0 /* instant */);
-  this.container_.appendChild(newScreenImage);
 
   setTimeout(function() {
     this.setTransform(newScreenImage, null,
