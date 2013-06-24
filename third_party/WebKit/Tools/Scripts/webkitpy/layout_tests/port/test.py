9b68ce3e72fa74c85252ff7a42f15701995f3514
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-24 22:45:09 +0000

    Clean up remaining DumpRenderTree references and flags.
    content_shell/Content Shell everywhere now :).
    
    R=jochen@chromium.org
    BUG=252558
    
    Review URL: https://chromiumcodereview.appspot.com/17564007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152973 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/test.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/test.py
@@ -536,7 +536,7 @@ class TestPort(Port):
 
 
 class TestDriver(Driver):
-    """Test/Dummy implementation of the DumpRenderTree interface."""
+    """Test/Dummy implementation of the driver interface."""
     next_pid = 1
 
     def __init__(self, *args, **kwargs):
