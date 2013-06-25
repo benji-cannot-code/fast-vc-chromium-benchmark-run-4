40e020ab041bc5b7c6a1eb25b93f47b6ddffd53a
alph@chromium.org <alph@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-25 17:03:50 +0000

    DevTools: Fix Document DOM Trees container distance to be 0.
    BUG=
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/17705002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153012 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/JSHeapSnapshot.js
+++ third_party/WebKit/Source/devtools/front_end/JSHeapSnapshot.js
@@ -139,6 +139,8 @@ WebInspector.JSHeapSnapshot.prototype = {
     {
         if (node.isUserRoot())
             return 0;
+        if (node.isDocumentDOMTreesRoot())
+            return 0;
         return this._noDistance;
     },
 
