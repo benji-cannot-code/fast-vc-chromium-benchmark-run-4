5a02134aebf41ddb79a62a12e36d1286d1e78617
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 17:29:20 +0000

    Prevent a crash if pasting an image that NSImage doesn't like.
    BUG=251856
    TEST=as in bug
    
    Review URL: https://chromiumcodereview.appspot.com/17626002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208516 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/clipboard/clipboard_mac.mm
+++ ui/base/clipboard/clipboard_mac.mm
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/mac/mac_util.h"
 #include "base/mac/scoped_cftyperef.h"
+#import "base/mac/scoped_nsexception_enabler.h"
 #include "base/mac/scoped_nsobject.h"
 #include "base/stl_util.h"
 #include "base/strings/sys_string_conversions.h"
@@ -344,8 +345,12 @@ SkBitmap Clipboard::ReadImage(Buffer buffer) const {
   DCHECK(CalledOnValidThread());
   DCHECK_EQ(buffer, BUFFER_STANDARD);
 
-  base::scoped_nsobject<NSImage> image(
-      [[NSImage alloc] initWithPasteboard:GetPasteboard()]);
+  // If the pasteboard's image data is not to its liking, the guts of NSImage
+  // may throw, and that exception will leak. Prevent a crash in that case;
+  // a blank image is better.
+  base::scoped_nsobject<NSImage> image(base::mac::RunBlockIgnoringExceptions(^{
+      return [[NSImage alloc] initWithPasteboard:GetPasteboard()];
+  }));
   if (!image.get())
     return SkBitmap();
 
