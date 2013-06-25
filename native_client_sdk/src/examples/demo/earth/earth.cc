065fdfc54e4bd3856dfbd32d04f326c02420887f
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 09:01:14 +0000

    [NaCl SDK] Remove include of missing header file.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17609005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208461 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/examples/demo/earth/earth.cc
+++ native_client_sdk/src/examples/demo/earth/earth.cc
@@ -30,7 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "sdk_util/macros.h"
-#include "sdk_util/string_parse.h"
 #include "sdk_util/thread_pool.h"
 
 // Global properties used to setup Earth demo.
