30d137869fa9eae3e2011919495982b2f5c06650
keishi@chromium.org <keishi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-25 05:31:08 +0000

    Fix style issues in the calendar picker
    - Move the week column cells one pixel up
    - Move the column labels one pixel down
    
    BUG=246513
    
    Review URL: https://chromiumcodereview.appspot.com/15993006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@152988 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/Resources/pagepopups/calendarPicker.js
+++ third_party/WebKit/Source/core/Resources/pagepopups/calendarPicker.js
@@ -3012,7 +3012,7 @@ function DayCell() {
     this.element.classList.add(DayCell.ClassNameDayCell);
     this.element.style.width = DayCell.Width + "px";
     this.element.style.height = DayCell.Height + "px";
-    this.element.style.lineHeight = (DayCell.Height - DayCell.BorderWidth * 2) + "px";
+    this.element.style.lineHeight = (DayCell.Height - DayCell.PaddingSize * 2) + "px";
     /**
      * @type {?Day}
      */
@@ -3023,7 +3023,7 @@ DayCell.prototype = Object.create(ListCell.prototype);
 
 DayCell.Width = 34;
 DayCell.Height = hasInaccuratePointingDevice() ? 34 : 20;
-DayCell.BorderWidth = 1;
+DayCell.PaddingSize = 1;
 DayCell.ClassNameDayCell = "day-cell";
 DayCell.ClassNameHighlighted = "highlighted";
 DayCell.ClassNameDisabled = "disabled";
@@ -3108,8 +3108,9 @@ DayCell.prototype.reset = function(day) {
 function WeekNumberCell() {
     ListCell.call(this);
     this.element.classList.add(WeekNumberCell.ClassNameWeekNumberCell);
-    this.element.style.width = (WeekNumberCell.Width - WeekNumberCell.RightBorderWidth) + "px";
+    this.element.style.width = (WeekNumberCell.Width - WeekNumberCell.SeparatorWidth) + "px";
     this.element.style.height = WeekNumberCell.Height + "px";
+    this.element.style.lineHeight = (WeekNumberCell.Height - WeekNumberCell.PaddingSize * 2) + "px";
     /**
      * @type {?Week}
      */
@@ -3120,7 +3121,8 @@ WeekNumberCell.prototype = Object.create(ListCell.prototype);
 
 WeekNumberCell.Width = 48;
 WeekNumberCell.Height = DayCell.Height;
-WeekNumberCell.RightBorderWidth = 1;
+WeekNumberCell.SeparatorWidth = 1;
+WeekNumberCell.PaddingSize = 1;
 WeekNumberCell.ClassNameWeekNumberCell = "week-number-cell";
 WeekNumberCell.ClassNameHighlighted = "highlighted";
 WeekNumberCell.ClassNameDisabled = "disabled";
