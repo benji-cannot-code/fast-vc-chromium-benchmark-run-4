dd79d13866e0f3ff1c5dfadee092e7aad220dbb1
jered@chromium.org <jered@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 20:22:31 +0000

    InstantExtended: Don't drop focus omnibox IPC.
    This IPC isn't obsolete. Dropping it made the fakebox stop working.
    
    BUG=253982
    TEST=Manual.
    R=kmadhusu@chromium.org
    
    Review URL: https://codereview.chromium.org/17714004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208538 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/search/instant_tab.cc
+++ chrome/browser/ui/search/instant_tab.cc
@@ -22,6 +22,10 @@ bool InstantTab::ShouldProcessAboutToNavigateMainFrame() {
   return true;
 }
 
+bool InstantTab::ShouldProcessFocusOmnibox() {
+  return true;
+}
+
 bool InstantTab::ShouldProcessNavigateToURL() {
   return true;
 }
