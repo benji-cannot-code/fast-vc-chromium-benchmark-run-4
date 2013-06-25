4b8216253f9b9f2dce4617ce993715d2b3ce71e6
binji@chromium.org <binji@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 23:26:21 +0000

    [NaCl SDK] nacl_io: Some refcounting fixes, nacl_io_test fixes.
    These fix the life/flock examples, the nacl_io_test, and the waterfall.
    
    BUG=none
    TBR=noelallen@chromium.org
    
    Review URL: https://codereview.chromium.org/17730002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208573 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io_test/mount_html5fs_test.cc
+++ native_client_sdk/src/libraries/nacl_io_test/mount_html5fs_test.cc
@@ -39,9 +39,7 @@ class MountHtml5FsMock : public MountHtml5Fs {
     Init(1, map, ppapi);
   }
 
-  ~MountHtml5FsMock() {
-    Destroy();
-  }
+  ~MountHtml5FsMock() {}
 };
 
 class MountHtml5FsTest : public ::testing::Test {
@@ -314,7 +312,7 @@ TEST_F(MountHtml5FsTest, FilesystemType) {
   StringMap_t map;
   map["type"] = "PERSISTENT";
   map["expected_size"] = "100";
-  MountHtml5FsMock mnt(map, ppapi_);
+  ScopedRef<MountHtml5FsMock> mnt(new MountHtml5FsMock(map, ppapi_));
 }
 
 TEST_F(MountHtml5FsTest, Access) {
@@ -347,9 +345,9 @@ TEST_F(MountHtml5FsTest, Access) {
   EXPECT_CALL(*ppapi_, ReleaseResource(fileref_resource));
 
   StringMap_t map;
-  MountHtml5FsMock mnt(map, ppapi_);
+  ScopedRef<MountHtml5FsMock> mnt(new MountHtml5FsMock(map, ppapi_));
 
-  ASSERT_EQ(0, mnt.Access(Path(path), R_OK | W_OK | X_OK));
+  ASSERT_EQ(0, mnt->Access(Path(path), R_OK | W_OK | X_OK));
 }
 
 TEST_F(MountHtml5FsTest, AccessFileNotFound) {
@@ -383,9 +381,9 @@ TEST_F(MountHtml5FsTest, AccessFileNotFound) {
   EXPECT_CALL(*ppapi_, ReleaseResource(fileref_resource));
 
   StringMap_t map;
-  MountHtml5FsMock mnt(map, ppapi_);
+  ScopedRef<MountHtml5FsMock> mnt(new MountHtml5FsMock(map, ppapi_));
 
-  ASSERT_EQ(ENOENT, mnt.Access(Path(path), F_OK));
+  ASSERT_EQ(ENOENT, mnt->Access(Path(path), F_OK));
 }
 
 TEST_F(MountHtml5FsTest, Mkdir) {
@@ -404,10 +402,10 @@ TEST_F(MountHtml5FsTest, Mkdir) {
   EXPECT_CALL(*ppapi_, ReleaseResource(fileref_resource));
 
   StringMap_t map;
-  MountHtml5FsMock mnt(map, ppapi_);
+  ScopedRef<MountHtml5FsMock> mnt(new MountHtml5FsMock(map, ppapi_));
 
   const int permissions = 0;  // unused.
-  int32_t result = mnt.Mkdir(Path(path), permissions);
+  int32_t result = mnt->Mkdir(Path(path), permissions);
   ASSERT_EQ(0, result);
 }
 
@@ -427,9 +425,9 @@ TEST_F(MountHtml5FsTest, Remove) {
   EXPECT_CALL(*ppapi_, ReleaseResource(fileref_resource));
 
   StringMap_t map;
-  MountHtml5FsMock mnt(map, ppapi_);
+  ScopedRef<MountHtml5FsMock> mnt(new MountHtml5FsMock(map, ppapi_));
 
-  int32_t result = mnt.Remove(Path(path));
+  int32_t result = mnt->Remove(Path(path));
   ASSERT_EQ(0, result);
 }
 
