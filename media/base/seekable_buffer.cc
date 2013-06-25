f2946b93a5c5a71a5937d9d194ab1eb6497fa246
tyoverby@chromium.org <tyoverby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-25 08:41:01 +0000

    Refactored DataBuffer to use unix_hacker style methods.
    DataBuffer went from having pure virtual methods to having concrete
    implementations.  However, the style of the method name remained
    in UpperCamelCase.  This patch renames the methods to fit with
    the unix_hacker_style that is used for concrete implementations.
    
    BUG=251986
    
    Review URL: https://chromiumcodereview.appspot.com/17315021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208453 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/seekable_buffer.cc
+++ media/base/seekable_buffer.cc
@@ -12,8 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
-SeekableBuffer::SeekableBuffer(int backward_capacity,
-                               int forward_capacity)
+SeekableBuffer::SeekableBuffer(int backward_capacity, int forward_capacity)
     : current_buffer_offset_(0),
       backward_capacity_(backward_capacity),
       backward_bytes_(0),
@@ -50,20 +49,20 @@ bool SeekableBuffer::GetCurrentChunk(const uint8** data, int* size) const {
   int current_buffer_offset = current_buffer_offset_;
   // Advance position if we are in the end of the current buffer.
   while (current_buffer != buffers_.end() &&
-         current_buffer_offset >= (*current_buffer)->GetDataSize()) {
+         current_buffer_offset >= (*current_buffer)->data_size()) {
     ++current_buffer;
     current_buffer_offset = 0;
   }
   if (current_buffer == buffers_.end())
     return false;
-  *data = (*current_buffer)->GetData() + current_buffer_offset;
-  *size = (*current_buffer)->GetDataSize() - current_buffer_offset;
+  *data = (*current_buffer)->data() + current_buffer_offset;
+  *size = (*current_buffer)->data_size() - current_buffer_offset;
   return true;
 }
 
 bool SeekableBuffer::Append(const scoped_refptr<DataBuffer>& buffer_in) {
-  if (buffers_.empty() && buffer_in->GetTimestamp() != kNoTimestamp()) {
-    current_time_ = buffer_in->GetTimestamp();
+  if (buffers_.empty() && buffer_in->timestamp() != kNoTimestamp()) {
+    current_time_ = buffer_in->timestamp();
   }
 
   // Since the forward capacity is only used to check the criteria for buffer
@@ -78,7 +77,7 @@ bool SeekableBuffer::Append(const scoped_refptr<DataBuffer>& buffer_in) {
   }
 
   // Update the |forward_bytes_| counter since we have more bytes.
-  forward_bytes_ += buffer_in->GetDataSize();
+  forward_bytes_ += buffer_in->data_size();
 
   // Advise the user to stop append if the amount of forward bytes exceeds
   // the forward capacity. A false return value means the user should stop
@@ -157,7 +156,7 @@ bool SeekableBuffer::SeekBackward(int size) {
       --current_buffer_;
       // Set the offset into the current buffer to be the buffer size as we
       // are preparing for rewind for next iteration.
-      current_buffer_offset_ = (*current_buffer_)->GetDataSize();
+      current_buffer_offset_ = (*current_buffer_)->data_size();
     }
   }
 
@@ -174,7 +173,7 @@ void SeekableBuffer::EvictBackwardBuffers() {
     if (i == current_buffer_)
       break;
     scoped_refptr<DataBuffer> buffer = *i;
-    backward_bytes_ -= buffer->GetDataSize();
+    backward_bytes_ -= buffer->data_size();
     DCHECK_GE(backward_bytes_, 0);
 
     buffers_.erase(i);
@@ -200,7 +199,7 @@ int SeekableBuffer::InternalRead(uint8* data, int size,
     scoped_refptr<DataBuffer> buffer = *current_buffer;
 
     int remaining_bytes_in_buffer =
-        buffer->GetDataSize() - current_buffer_offset;
+        buffer->data_size() - current_buffer_offset;
 
     if (bytes_to_skip == 0) {
       // Find the right amount to copy from the current buffer referenced by
@@ -210,7 +209,7 @@ int SeekableBuffer::InternalRead(uint8* data, int size,
 
       // |data| is NULL if we are seeking forward, so there's no need to copy.
       if (data)
-        memcpy(data + taken, buffer->GetData() + current_buffer_offset, copied);
+        memcpy(data + taken, buffer->data() + current_buffer_offset, copied);
 
       // Increase total number of bytes copied, which regulates when to end this
       // loop.
@@ -226,7 +225,7 @@ int SeekableBuffer::InternalRead(uint8* data, int size,
     }
 
     // The buffer has been consumed.
-    if (current_buffer_offset == buffer->GetDataSize()) {
+    if (current_buffer_offset == buffer->data_size()) {
       if (advance_position) {
         // Next buffer may not have timestamp, so we need to update current
         // timestamp before switching to the next buffer.
@@ -266,12 +265,13 @@ int SeekableBuffer::InternalRead(uint8* data, int size,
 void SeekableBuffer::UpdateCurrentTime(BufferQueue::iterator buffer,
                                        int offset) {
   // Garbage values are unavoidable, so this check will remain.
-  if (buffer != buffers_.end() && (*buffer)->GetTimestamp() != kNoTimestamp()) {
-    int64 time_offset = ((*buffer)->GetDuration().InMicroseconds() *
-                         offset) / (*buffer)->GetDataSize();
+  if (buffer != buffers_.end() &&
+      (*buffer)->timestamp() != kNoTimestamp()) {
+    int64 time_offset = ((*buffer)->duration().InMicroseconds() * offset) /
+                        (*buffer)->data_size();
 
-    current_time_ = (*buffer)->GetTimestamp() +
-        base::TimeDelta::FromMicroseconds(time_offset);
+    current_time_ = (*buffer)->timestamp() +
+                    base::TimeDelta::FromMicroseconds(time_offset);
   }
 }
 
--- media/filters/audio_renderer_impl.cc
+++ media/filters/audio_renderer_impl.cc
@@ -357,7 +357,7 @@ void AudioRendererImpl::DecodedAudioReady(
 
 bool AudioRendererImpl::HandleSplicerBuffer(
     const scoped_refptr<DataBuffer>& buffer) {
-  if (buffer->IsEndOfStream()) {
+  if (buffer->end_of_stream()) {
     received_end_of_stream_ = true;
 
     // Transition to kPlaying if we are currently handling an underflow since
@@ -371,7 +371,7 @@ bool AudioRendererImpl::HandleSplicerBuffer(
       NOTREACHED();
       return false;
     case kPaused:
-      if (!buffer->IsEndOfStream())
+      if (!buffer->end_of_stream())
         algorithm_->EnqueueBuffer(buffer);
       DCHECK(!pending_read_);
       base::ResetAndReturn(&pause_cb_).Run();
@@ -380,7 +380,7 @@ bool AudioRendererImpl::HandleSplicerBuffer(
       if (IsBeforePrerollTime(buffer))
         return true;
 
-      if (!buffer->IsEndOfStream()) {
+      if (!buffer->end_of_stream()) {
         algorithm_->EnqueueBuffer(buffer);
         if (!algorithm_->IsQueueFull())
           return false;
@@ -391,7 +391,7 @@ bool AudioRendererImpl::HandleSplicerBuffer(
     case kPlaying:
     case kUnderflow:
     case kRebuffering:
-      if (!buffer->IsEndOfStream())
+      if (!buffer->end_of_stream())
         algorithm_->EnqueueBuffer(buffer);
       return false;
     case kStopped:
@@ -456,8 +456,8 @@ void AudioRendererImpl::SetPlaybackRate(float playback_rate) {
 
 bool AudioRendererImpl::IsBeforePrerollTime(
     const scoped_refptr<DataBuffer>& buffer) {
-  return (state_ == kPrerolling) && buffer.get() && !buffer->IsEndOfStream() &&
-         (buffer->GetTimestamp() + buffer->GetDuration()) < preroll_timestamp_;
+  return (state_ == kPrerolling) && buffer.get() && !buffer->end_of_stream() &&
+         (buffer->timestamp() + buffer->duration()) < preroll_timestamp_;
 }
 
 int AudioRendererImpl::Render(AudioBus* audio_bus,
