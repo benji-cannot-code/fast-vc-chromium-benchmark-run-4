29155f960ece8c63416968d53abb65b348dd59d5
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-26 06:28:18 +0000

    This patch changes how the --timing flag works in run-webkit-tests.
    --timing used to imply --verbose, and would log the per-test time along with the test name.
    
    With this change, --timing no longer implies verbose. 
    
    Instead, by default, it modifies the "one line summary" to containing timing information for the test run as a whole (total wall clock time, time spent running serially outside of the tests, and the relative speedup). 
    
    If --verbose is also passed, we will log the per-test time as well.
    
    Review URL: https://chromiumcodereview.appspot.com/17762005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153047 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/layout_test_runner.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/controllers/layout_test_runner.py
@@ -111,6 +111,7 @@ class LayoutTestRunner(object):
 
         self._printer.write_update('Starting %s ...' % grammar.pluralize('worker', num_workers))
 
+        start_time = time.time()
         try:
             with message_pool.get(self, self._worker_factory, num_workers, self._port.worker_startup_delay_secs(), self._port.host) as pool:
                 pool.run(('test_list', shard.name, shard.test_inputs) for shard in all_shards)
@@ -124,6 +125,8 @@ class LayoutTestRunner(object):
         except Exception, e:
             _log.debug('%s("%s") raised, exiting' % (e.__class__.__name__, str(e)))
             raise
+        finally:
+            run_results.run_time = time.time() - start_time
 
         return run_results
 
