ac3d47a22c5b00fe597875a767383142e6680385
thakis@chromium.org <thakis@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-26 16:51:34 +0000

    HashMap: reverse the order of the template arguments at alternate 'find', 'contains' and 'add' methods
    The order of the template arguments at HashMap alternate 'find', 'contains' and 'add' methods is reversed so that callers can just
    pass the translator and let the compiler deduce input argument type.
    
    Another rationale is consistency with HashSet class.
    
    BUG=253893
    R=thakis@chromium.org
    
    Review URL: https://codereview.chromium.org/17623002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153079 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/HashMap.h
+++ third_party/WebKit/Source/wtf/HashMap.h
@@ -118,9 +118,9 @@ namespace WTF {
         // must have the following function members:
         //   static unsigned hash(const T&);
         //   static bool equal(const ValueType&, const T&);
-        template<typename T, typename HashTranslator> iterator find(const T&);
-        template<typename T, typename HashTranslator> const_iterator find(const T&) const;
-        template<typename T, typename HashTranslator> bool contains(const T&) const;
+        template<typename HashTranslator, typename T> iterator find(const T&);
+        template<typename HashTranslator, typename T> const_iterator find(const T&) const;
+        template<typename HashTranslator, typename T> bool contains(const T&) const;
 
         // An alternate version of add() that finds the object by hashing and comparing
         // with some other type, to avoid the cost of type conversion if the object is already
@@ -128,7 +128,7 @@ namespace WTF {
         //   static unsigned hash(const T&);
         //   static bool equal(const ValueType&, const T&);
         //   static translate(ValueType&, const T&, unsigned hashCode);
-        template<typename T, typename HashTranslator> AddResult add(const T&, MappedPassInType);
+        template<typename HashTranslator, typename T> AddResult add(const T&, MappedPassInType);
 
         void checkConsistency() const;
 
@@ -313,7 +313,7 @@ namespace WTF {
     }
 
     template<typename T, typename U, typename V, typename W, typename X>
-    template<typename TYPE, typename HashTranslator>
+    template<typename HashTranslator, typename TYPE>
     inline typename HashMap<T, U, V, W, X>::iterator
     HashMap<T, U, V, W, X>::find(const TYPE& value)
     {
@@ -321,7 +321,7 @@ namespace WTF {
     }
 
     template<typename T, typename U, typename V, typename W, typename X>
-    template<typename TYPE, typename HashTranslator>
+    template<typename HashTranslator, typename TYPE>
     inline typename HashMap<T, U, V, W, X>::const_iterator 
     HashMap<T, U, V, W, X>::find(const TYPE& value) const
     {
@@ -329,7 +329,7 @@ namespace WTF {
     }
 
     template<typename T, typename U, typename V, typename W, typename X>
-    template<typename TYPE, typename HashTranslator>
+    template<typename HashTranslator, typename TYPE>
     inline bool
     HashMap<T, U, V, W, X>::contains(const TYPE& value) const
     {
@@ -356,7 +356,7 @@ namespace WTF {
     }
 
     template<typename T, typename U, typename V, typename W, typename X>
-    template<typename TYPE, typename HashTranslator>
+    template<typename HashTranslator, typename TYPE>
     typename HashMap<T, U, V, W, X>::AddResult
     HashMap<T, U, V, W, X>::add(const TYPE& key, MappedPassInType value)
     {
