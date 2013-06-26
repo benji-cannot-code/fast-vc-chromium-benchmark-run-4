b8ffb61dc2669c1068a1bfe64065ffe916019540
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 22:37:41 +0000

    OmniboxPopupViewMac refactoring Part 1
    This CL just moves code around. There are no code changes. Things in
    this CL
      - move button cell into omnibox_popup_button_cell.mm
      - move matrix into omnibox_popup_matrix.mm
      - reorder all functions to match declaration order
      - rename AutocompleteButtonCell to OmniboxPopupCell
      - rename AutocompleteMatrix to OmniboxPopupMatrix
    
    BUG=9977
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/17786002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208807 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.h
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_view_mac.h
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/omnibox/omnibox_popup_view.h"
 #include "ui/gfx/font.h"
 
-@class AutocompleteMatrix;
 class OmniboxEditModel;
+@class OmniboxPopupMatrix;
 class OmniboxPopupModel;
 class OmniboxView;
 
@@ -63,14 +63,14 @@ class OmniboxPopupViewMac : public OmniboxPopupView {
 
   virtual gfx::Rect GetTargetBounds() OVERRIDE;
 
-  // Set |line| to be selected.
-  void SetSelectedLine(size_t line);
-
   // This is only called by model in SetSelectedLine() after updating
   // everything.  Popup should already be visible.
   virtual void PaintUpdatesNow() OVERRIDE;
 
   virtual void OnDragCanceled() OVERRIDE {}
+
+  // Set |line| to be selected.
+  void SetSelectedLine(size_t line);
 
   // Opens the URL corresponding to the given |row|.  If |force_background| is
   // true, forces the URL to open in a background tab.  Otherwise, determines
@@ -130,7 +130,7 @@ class OmniboxPopupViewMac : public OmniboxPopupView {
   base::scoped_nsobject<NSWindow> popup_;
   NSRect targetPopupFrame_;
 
-  base::scoped_nsobject<AutocompleteMatrix> autocomplete_matrix_;
+  base::scoped_nsobject<OmniboxPopupMatrix> autocomplete_matrix_;
 
   DISALLOW_COPY_AND_ASSIGN(OmniboxPopupViewMac);
 };
