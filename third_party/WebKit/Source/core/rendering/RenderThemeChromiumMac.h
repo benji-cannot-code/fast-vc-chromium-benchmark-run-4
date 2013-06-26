29515431cf1521f9b7f3f39e3b517fbfbd16e06b
keishi@chromium.org <keishi@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-26 15:19:03 +0000

    Fix slider appearance and support zoom on Mac
    Slider appearance had some issues:
    - Track didn't have rounded corners
    - Thumb was upside down
    - Thumb and track weren't aligned
    - Slider didn't zoom
    
    This patch fixes them by drawing the slider with GraphicsContext instead of using NSSliderCell. This means the slider appearance on Snow Leopard will change to the Lion one, but it fixes so many issues that its worth it.
    
    Test=Tested by existing range-appearance-* tests.
    BUG=130259
    
    Review URL: https://chromiumcodereview.appspot.com/16455007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153073 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderThemeChromiumMac.h
+++ third_party/WebKit/Source/core/rendering/RenderThemeChromiumMac.h
@@ -167,8 +167,6 @@ private:
     NSPopUpButtonCell* popupButton() const;
     NSSearchFieldCell* search() const;
     NSMenu* searchMenuTemplate() const;
-    NSSliderCell* sliderThumbHorizontal() const;
-    NSSliderCell* sliderThumbVertical() const;
     NSTextFieldCell* textField() const;
 
     NSLevelIndicatorStyle levelIndicatorStyleFor(ControlPart) const;
@@ -213,13 +211,8 @@ private:
     mutable RetainPtr<NSPopUpButtonCell> m_popupButton;
     mutable RetainPtr<NSSearchFieldCell> m_search;
     mutable RetainPtr<NSMenu> m_searchMenuTemplate;
-    mutable RetainPtr<NSSliderCell> m_sliderThumbHorizontal;
-    mutable RetainPtr<NSSliderCell> m_sliderThumbVertical;
     mutable RetainPtr<NSLevelIndicatorCell> m_levelIndicator;
     mutable RetainPtr<NSTextFieldCell> m_textField;
-
-    bool m_isSliderThumbHorizontalPressed;
-    bool m_isSliderThumbVerticalPressed;
 
     mutable HashMap<int, RGBA32> m_systemColorCache;
 
