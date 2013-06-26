9d5b1bba5a3894795858f3f8d0c5b26f385a7394
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 15:01:02 +0000

    Do not show remove option for owner
    BUG=254140
    R=ygorshenin@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/17901002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208704 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/user_pod_row.js
+++ chrome/browser/resources/chromeos/login/user_pod_row.js
@@ -408,7 +408,7 @@ cr.define('login', function() {
         return;
 
       if (active) {
-        this.actionBoxMenuRemoveElement.hidden = false;
+        this.actionBoxMenuRemoveElement.hidden = !this.user_.canRemove;
         this.actionBoxRemoveManagedUserWarningElement.hidden = true;
 
         // Clear focus first if another pod is focused.
