7b1ba487ce09916b5a1ba36c10a94dc94eee5cf0
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-26 17:36:26 +0000

    Implement Skia version of FETile filter.
    BUG=229680
    R=schenney@chromium.org
    
    Review URL: https://codereview.chromium.org/17838004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153082 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/filters/FETile.h
+++ third_party/WebKit/Source/core/platform/graphics/filters/FETile.h
@@ -33,6 +33,8 @@ class FETile : public FilterEffect {
 public:
     static PassRefPtr<FETile> create(Filter* filter);
 
+    virtual SkImageFilter* createImageFilter(SkiaImageFilterBuilder*);
+
     virtual void determineAbsolutePaintRect() { setAbsolutePaintRect(enclosingIntRect(maxEffectRect())); }
 
     virtual FilterEffectType filterEffectType() const { return FilterEffectTypeTile; }
