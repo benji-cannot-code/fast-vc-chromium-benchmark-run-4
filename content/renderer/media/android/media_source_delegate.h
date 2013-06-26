fe676ba136e95bd50a12816ba728dfd11f3c89a6
xhwang@chromium.org <xhwang@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 19:10:57 +0000

    MediaSourceDelegate::Seek() waits for Demuxer::Seek() to ack seek request.
    All pending demuxer reads and previous seeks will be canceled. Only the last
    seek request will be acknowledged.
    
    BUG=251876
    TEST=Seek test in conformance test passes; Random seeking seems to work.
    
    Review URL: https://chromiumcodereview.appspot.com/17283012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208753 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/media/android/media_source_delegate.h
+++ content/renderer/media/android/media_source_delegate.h
@@ -68,9 +68,10 @@ class MediaSourceDelegate : public media::DemuxerHost {
   size_t AudioDecodedByteCount() const;
   size_t VideoDecodedByteCount() const;
 
-  void Seek(base::TimeDelta time);
-
-  void CancelPendingSeek(base::TimeDelta time);
+  // Seeks the demuxer and acknowledges the seek request with |seek_request_id|
+  // after the seek has been completed. This method can be called during pending
+  // seeks, in which case only the last seek request will be acknowledged.
+  void Seek(base::TimeDelta time, unsigned seek_request_id);
 
   void NotifyKeyAdded(const std::string& key_system);
 
@@ -98,6 +99,8 @@ class MediaSourceDelegate : public media::DemuxerHost {
 
   // Callbacks for ChunkDemuxer.
   void OnDemuxerInitDone(media::PipelineStatus status);
+  void OnDemuxerSeekDone(unsigned seek_request_id,
+                         media::PipelineStatus status);
   void OnDemuxerStopDone();
   void OnDemuxerOpened();
   void OnNeedKey(const std::string& type,
@@ -158,6 +161,9 @@ class MediaSourceDelegate : public media::DemuxerHost {
   scoped_ptr<media::MediaPlayerHostMsg_ReadFromDemuxerAck_Params> video_params_;
 
   bool seeking_;
+  base::TimeDelta last_seek_time_;
+  unsigned last_seek_request_id_;
+
   bool key_added_;
   size_t access_unit_size_;
 
