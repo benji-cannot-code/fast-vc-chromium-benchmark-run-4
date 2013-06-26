93fab79b3d4033f22c998932cf4cb5166366ab62
davidroche@chromium.org <davidroche@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 04:11:34 +0000

    Better document GaiaOAuthClient via comments and updated method name.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/17696002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208615 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/signaling_connector.h
+++ remoting/host/signaling_connector.h
@@ -81,7 +81,7 @@ class SignalingConnector
                                    int expires_seconds) OVERRIDE;
   virtual void OnRefreshTokenResponse(const std::string& access_token,
                                       int expires_in_seconds) OVERRIDE;
-  virtual void OnGetUserInfoResponse(const std::string& user_email) OVERRIDE;
+  virtual void OnGetUserEmailResponse(const std::string& user_email) OVERRIDE;
   virtual void OnOAuthError() OVERRIDE;
   virtual void OnNetworkError(int response_code) OVERRIDE;
 
