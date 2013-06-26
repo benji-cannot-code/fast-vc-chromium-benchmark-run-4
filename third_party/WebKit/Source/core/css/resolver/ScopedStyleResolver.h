11a26fcadde9a65b32fe834845ed4ad8d091df91
tasak@google.com <tasak@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-26 11:32:01 +0000

    Added buildInDocumentOrder to ScopedStyleTree.
    While building scoped style tree, scoping nodes of @host @-rules don't appear in document order.
    So we need buildInDocumentOrder flag and need to reparent existing ScopedStyleResolvers in ScopedStyleTree.
    
    BUG=244314
    
    Review URL: https://chromiumcodereview.appspot.com/17576004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153060 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.h
+++ third_party/WebKit/Source/core/css/resolver/ScopedStyleResolver.h
@@ -53,7 +53,6 @@ public:
 
     static const ContainerNode* scopingNodeFor(const CSSStyleSheet*);
 
-    // methods for building tree.
     const ContainerNode* scopingNode() const { return m_scopingNode; }
     const TreeScope* treeScope() const { return m_scopingNode->treeScope(); }
     void prepareEmptyRuleSet() { m_authorStyle = RuleSet::create(); }
@@ -89,7 +88,7 @@ private:
 class ScopedStyleTree {
     WTF_MAKE_NONCOPYABLE(ScopedStyleTree); WTF_MAKE_FAST_ALLOCATED;
 public:
-    ScopedStyleTree() : m_scopedResolverForDocument(0) { }
+    ScopedStyleTree() : m_scopedResolverForDocument(0), m_buildInDocumentOrder(true) { }
 
     ScopedStyleResolver* ensureScopedStyleResolver(const ContainerNode* scopingNode);
     ScopedStyleResolver* scopedStyleResolverFor(const ContainerNode* scopingNode);
@@ -107,6 +106,8 @@ public:
     void popStyleCache(const ContainerNode* scopingNode);
 
     void collectFeaturesTo(RuleFeatureSet& features);
+    void setBuildInDocumentOrder(bool enabled) { m_buildInDocumentOrder = enabled; }
+    bool buildInDocumentOrder() const { return m_buildInDocumentOrder; }
 
     void reportMemoryUsage(MemoryObjectInfo*) const;
 private:
@@ -119,6 +120,7 @@ private:
 private:
     HashMap<const ContainerNode*, OwnPtr<ScopedStyleResolver> > m_authorStyles;
     ScopedStyleResolver* m_scopedResolverForDocument;
+    bool m_buildInDocumentOrder;
 
     struct ScopedStyleCache {
         ScopedStyleResolver* scopedResolver;
