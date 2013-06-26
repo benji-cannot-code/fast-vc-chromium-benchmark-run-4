340f209a099702d503d42af3881ce3f2251b0877
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 02:42:02 +0000

    Use sql::Connection::Delete() in login_database.
    New API deletes main database and any associated files.
    
    Also IWYU pass.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/17595004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208601 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/password_manager/login_database.cc
+++ chrome/browser/password_manager/login_database.cc
@@ -9,17 +9,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <limits>
 
 #include "base/command_line.h"
-#include "base/file_util.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/metrics/histogram.h"
 #include "base/pickle.h"
-#include "base/strings/string_number_conversions.h"
 #include "base/strings/string_util.h"
-#include "base/strings/utf_string_conversions.h"
 #include "base/time.h"
 #include "chrome/common/chrome_switches.h"
 #include "net/base/registry_controlled_domains/registry_controlled_domain.h"
+#include "sql/connection.h"
 #include "sql/statement.h"
 #include "sql/transaction.h"
 
@@ -537,7 +535,7 @@ bool LoginDatabase::DeleteAndRecreateDatabaseFile() {
   DCHECK(db_.is_open());
   meta_table_.Reset();
   db_.Close();
-  file_util::Delete(db_path_, false);
+  sql::Connection::Delete(db_path_);
   return Init(db_path_);
 }
 
