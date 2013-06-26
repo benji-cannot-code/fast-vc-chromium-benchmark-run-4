cfad9c4e2f8a8132f2134d8e510de5d03191fc7e
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 08:22:20 +0000

    Window focus fixes for Core Animation
    When changing window focus various controls in the toolbar wouldn't
    update. For example:
      - blue focus ring in the omnibox and find bar wouldn't hide
      - icons in buttons wouldn't change to the light gray version
    
    The problem was that these controls weren't being invalidted. Fix
    was to recursively invalidate all toolbar controls when switching
    window focus.
    
    BUG=245900
    TEST=Run chrome with --use-core-animation. Open a new window and
    verify that the blue focus ring in the omnibox of the old window
    is hidden.
    
    Review URL: https://chromiumcodereview.appspot.com/17645003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208665 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/background_gradient_view.mm
+++ chrome/browser/ui/cocoa/background_gradient_view.mm
@@ -113,7 +113,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 - (void)windowFocusDidChange:(NSNotification*)notification {
   // The background color depends on the window's focus state.
-  [self setNeedsDisplay:YES];
+  [self cr_recursivelySetNeedsDisplay:YES];
 }
 
 - (void)viewWillMoveToWindow:(NSWindow*)window {
