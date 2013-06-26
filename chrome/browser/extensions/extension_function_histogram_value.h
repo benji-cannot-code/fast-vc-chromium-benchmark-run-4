3d560624205475ff826eb110508ccc3cf92022b3
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-26 06:03:40 +0000

    <webview>: Move back, forward, canGoBack, canGoForward, go from content to chrome
    BUG=166165
    Test=WebViewTest.Navigation
    
    Review URL: https://chromiumcodereview.appspot.com/17447005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208642 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_function_histogram_value.h
+++ chrome/browser/extensions/extension_function_histogram_value.h
@@ -557,6 +557,7 @@ enum HistogramValue {
   DIAGNOSTICS_SENDPACKET,
   METRICSPRIVATE_GETFIELDTRIAL,
   FILEBROWSERPRIVATE_ZOOM,
+  WEBVIEW_GO,
   ENUM_BOUNDARY // Last entry: Add new entries above.
 };
 
