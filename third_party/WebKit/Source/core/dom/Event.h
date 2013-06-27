051710443105a6dba626180f57891e968f2970c5
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-27 05:15:23 +0000

    Remove unused and dangerous Event::cloneFor functions
    These functions were added in http://src.chromium.org/viewvc/blink?revision=126256&view=revision
    to support seamless iframes, but aren't reachable. They are dangerous since they produce Events
    with type()s that do not match the actual type of the object.
    
    BUG=244882
    
    Review URL: https://chromiumcodereview.appspot.com/17912006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153117 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Event.h
+++ third_party/WebKit/Source/core/dom/Event.h
@@ -167,8 +167,6 @@ public:
 
     virtual void reportMemoryUsage(MemoryObjectInfo*) const;
 
-    virtual PassRefPtr<Event> cloneFor(HTMLIFrameElement*) const;
-
 protected:
     Event();
     Event(const AtomicString& type, bool canBubble, bool cancelable);
