a80a0f1b236377cd86f0898d0c523224afdf127a
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-27 23:53:39 +0000

    Get rid of the extra vector when setting up the orderIterator.
    -Rename computeMainAxisPreferredSizes. This name has been wrong for
    a long time.
    -Populate m_orderValues directly instead of first popuplating
    an on-stack vector and then copying the values into m_orderValues.
    -Avoid doing extra buffer allocations when resetting the m_orderValues
    entries.
    
    Review URL: https://chromiumcodereview.appspot.com/18133003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153185 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderFlexibleBox.h
+++ third_party/WebKit/Source/core/rendering/RenderFlexibleBox.h
@@ -85,10 +85,11 @@ private:
         RenderBox* next();
         void reset();
 
+        Vector<int> m_orderValues;
+
     private:
         const RenderFlexibleBox* m_flexibleBox;
         RenderBox* m_currentChild;
-        Vector<int> m_orderValues;
         Vector<int>::const_iterator m_orderValuesIterator;
     };
 
@@ -152,7 +153,7 @@ private:
     LayoutUnit marginBoxAscentForChild(RenderBox*);
 
     LayoutUnit computeChildMarginValue(Length margin, RenderView*);
-    void computeMainAxisPreferredSizes(Vector<int>&);
+    void prepareOrderIteratorAndMargins();
     LayoutUnit adjustChildSizeForMinAndMax(RenderBox*, LayoutUnit childSize);
     bool computeNextFlexLine(OrderedFlexItemList& orderedChildren, LayoutUnit& preferredMainAxisExtent, double& totalFlexGrow, double& totalWeightedFlexShrink, LayoutUnit& minMaxAppliedMainAxisExtent, bool& hasInfiniteLineLength);
 
