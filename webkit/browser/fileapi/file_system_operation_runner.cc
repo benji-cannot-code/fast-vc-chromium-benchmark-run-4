fbe36c1e29b504376f1bcc5431cbbbf0fd268fb5
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-27 14:39:32 +0000

    Let LocalFileSystemOperation::SyncGetPlatformPath failed if context returns NULL for GetFileUtil.
    This is a part of {Local,Remote}FileSystemOperation merging. After the merging
    the context may not support GetFileUtil(). In such a case, lets the method fail.
    Returned error code should be the same one for the current
    RemoteFileSystemOperation case.
    
    BUG=110121
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/18016002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208918 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/file_system_operation_runner.cc
+++ webkit/browser/fileapi/file_system_operation_runner.cc
@@ -443,9 +443,8 @@ base::PlatformFileError FileSystemOperationRunner::SyncGetPlatformPath(
   if (!operation)
     return error;
 
-  operation->AsLocalFileSystemOperation()->SyncGetPlatformPath(
+  return operation->AsLocalFileSystemOperation()->SyncGetPlatformPath(
       url, platform_path);
-  return base::PLATFORM_FILE_OK;
 }
 
 FileSystemOperationRunner::FileSystemOperationRunner(
