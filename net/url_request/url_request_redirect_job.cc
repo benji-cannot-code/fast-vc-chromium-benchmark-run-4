46d7539e1423e51596d184fe7f3942a8e9e1fba9
pan.deng@intel.com <pan.deng@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-27 14:16:40 +0000

    Expose receive header end time for URLRequestRedirectJob
    Contributed by pan.deng@intel.com
    
    BUG=252621
    
    Review URL: https://chromiumcodereview.appspot.com/16901013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208909 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/url_request_redirect_job.cc
+++ net/url_request/url_request_redirect_job.cc
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/compiler_specific.h"
 #include "base/message_loop.h"
+#include "net/base/load_timing_info.h"
 
 namespace net {
 
@@ -37,7 +38,17 @@ bool URLRequestRedirectJob::IsRedirectResponse(GURL* location,
 URLRequestRedirectJob::~URLRequestRedirectJob() {}
 
 void URLRequestRedirectJob::StartAsync() {
+  receive_headers_end_ = base::TimeTicks::Now();
   NotifyHeadersComplete();
+}
+
+void URLRequestRedirectJob::GetLoadTimingInfo(
+    LoadTimingInfo* load_timing_info) const {
+  // Set send_start and send_end to receive_headers_end_ to keep consistent
+  // with network cache behavior.
+  load_timing_info->send_start = receive_headers_end_;
+  load_timing_info->send_end = receive_headers_end_;
+  load_timing_info->receive_headers_end = receive_headers_end_;
 }
 
 }  // namespace net
