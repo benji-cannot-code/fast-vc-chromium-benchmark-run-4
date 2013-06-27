e07325f29e016b1f3812a18a4de96becef86e58c
tommyw@chromium.org <tommyw@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-27 08:09:26 +0000

    Revert "Refactoring: Simplify DocumentWriter by reorganizing its lifetime."
    This reverts commit 5adb71bcfa08a062b72a176c7ab5ff5a11fd598c.
    
    NOTRY=True
    TBR=morrita@chromium.org,abarth@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/18029002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153124 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -2153,7 +2153,7 @@ void Document::cancelParsing()
     explicitClose();
 }
 
-PassRefPtr<DocumentParser> Document::implicitOpen()
+void Document::implicitOpen()
 {
     cancelParsing();
 
@@ -2171,8 +2171,6 @@ PassRefPtr<DocumentParser> Document::implicitOpen()
     m_parser = createParser();
     setParsing(true);
     setReadyState(Loading);
-
-    return m_parser;
 }
 
 HTMLElement* Document::body() const
