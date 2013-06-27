8f7cd5b3115b6f5ac6989e39e0d78c8c944950dd
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-27 20:14:07 +0000

    Fill out more data types on OSExchangeDataProviderAuraX11.
    This also makes OSExchangeDataProvider unit tests that are cross platform
    actually cross platform.
    
    BUG=130806
    
    Review URL: https://chromiumcodereview.appspot.com/17776002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@208985 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/dragdrop/os_exchange_data_provider_aurax11.cc
+++ ui/base/dragdrop/os_exchange_data_provider_aurax11.cc
@@ -103,6 +103,12 @@ SelectionFormatMap OSExchangeDataProviderAuraX11::GetFormatMap() const {
   return selection_owner_.selection_format_map();
 }
 
+OSExchangeData::Provider* OSExchangeDataProviderAuraX11::Clone() const {
+  OSExchangeDataProviderAuraX11* ret = new OSExchangeDataProviderAuraX11();
+  ret->format_map_ = format_map_;
+  return ret;
+}
+
 void OSExchangeDataProviderAuraX11::SetString(const base::string16& text_data) {
   std::string utf8 = UTF16ToUTF8(text_data);
   scoped_refptr<base::RefCountedMemory> mem(
@@ -116,7 +122,21 @@ void OSExchangeDataProviderAuraX11::SetString(const base::string16& text_data) {
 
 void OSExchangeDataProviderAuraX11::SetURL(const GURL& url,
                                            const base::string16& title) {
-  NOTIMPLEMENTED();
+  // Mozilla's URL format: (UTF16: URL, newline, title)
+  if (url.is_valid()) {
+    string16 spec = UTF8ToUTF16(url.spec());
+
+    std::vector<unsigned char> data;
+    ui::AddString16ToVector(spec, &data);
+    ui::AddString16ToVector(ASCIIToUTF16("\n"), &data);
+    ui::AddString16ToVector(title, &data);
+    scoped_refptr<base::RefCountedMemory> mem(
+        base::RefCountedBytes::TakeVector(&data));
+
+    format_map_.Insert(atom_cache_.GetAtom(kMimeTypeMozillaURL), mem);
+
+    SetString(spec);
+  }
 }
 
 void OSExchangeDataProviderAuraX11::SetFilename(const base::FilePath& path) {
@@ -130,8 +150,16 @@ void OSExchangeDataProviderAuraX11::SetFilenames(
 
 void OSExchangeDataProviderAuraX11::SetPickledData(
     const OSExchangeData::CustomFormat& format,
-    const Pickle& data) {
-  NOTIMPLEMENTED();
+    const Pickle& pickle) {
+  const unsigned char* data =
+      reinterpret_cast<const unsigned char*>(pickle.data());
+
+  std::vector<unsigned char> bytes;
+  bytes.insert(bytes.end(), data, data + pickle.size());
+  scoped_refptr<base::RefCountedMemory> mem(
+      base::RefCountedBytes::TakeVector(&bytes));
+
+  format_map_.Insert(atom_cache_.GetAtom(format.ToString().c_str()), mem);
 }
 
 bool OSExchangeDataProviderAuraX11::GetString(base::string16* result) const {
@@ -213,8 +241,19 @@ bool OSExchangeDataProviderAuraX11::GetFilenames(
 
 bool OSExchangeDataProviderAuraX11::GetPickledData(
     const OSExchangeData::CustomFormat& format,
-    Pickle* data) const {
-  NOTIMPLEMENTED();
+    Pickle* pickle) const {
+  std::vector< ::Atom> requested_types;
+  requested_types.push_back(atom_cache_.GetAtom(format.ToString().c_str()));
+
+  ui::SelectionData data(format_map_.GetFirstOf(requested_types));
+  if (data.IsValid()) {
+    // Note that the pickle object on the right hand side of the assignment
+    // only refers to the bytes in |data|. The assignment copies the data.
+    *pickle = Pickle(reinterpret_cast<const char*>(data.GetData()),
+                     static_cast<int>(data.GetSize()));
+    return true;
+  }
+
   return false;
 }
 
@@ -249,7 +288,16 @@ bool OSExchangeDataProviderAuraX11::HasCustomFormat(
 
 void OSExchangeDataProviderAuraX11::SetHtml(const base::string16& html,
                                             const GURL& base_url) {
-  NOTIMPLEMENTED();
+  std::vector<unsigned char> bytes;
+  // Manually jam a UTF16 BOM into bytes because otherwise, other programs will
+  // assume UTF-8.
+  bytes.push_back(0xFF);
+  bytes.push_back(0xFE);
+  ui::AddString16ToVector(html, &bytes);
+  scoped_refptr<base::RefCountedMemory> mem(
+      base::RefCountedBytes::TakeVector(&bytes));
+
+  format_map_.Insert(atom_cache_.GetAtom(Clipboard::kMimeTypeHTML), mem);
 }
 
 bool OSExchangeDataProviderAuraX11::GetHtml(base::string16* html,
@@ -307,7 +355,15 @@ bool OSExchangeDataProviderAuraX11::Dispatch(const base::NativeEvent& event) {
 }
 
 bool OSExchangeDataProviderAuraX11::GetPlainTextURL(GURL* url) const {
-  NOTIMPLEMENTED();
+  string16 text;
+  if (GetString(&text)) {
+    GURL test_url(text);
+    if (test_url.is_valid()) {
+      *url = test_url;
+      return true;
+    }
+  }
+
   return false;
 }
 
--- ui/ui_unittests.gypi
+++ ui/ui_unittests.gypi
@@ -297,6 +297,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         }],
         ['use_aura==1 or toolkit_views==1',  {
           'sources': [
+            'base/dragdrop/os_exchange_data_unittest.cc',
             'base/gestures/velocity_calculator_unittest.cc',
           ],
         }, {
