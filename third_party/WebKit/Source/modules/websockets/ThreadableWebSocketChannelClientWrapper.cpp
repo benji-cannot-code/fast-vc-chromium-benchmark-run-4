5bf33895f4b1bf3d2e117b9b4898aba117d46809
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-27 20:40:12 +0000

    Rename more characters() to bloatedCharacters()
    This CL renames all the callers of characters() that are hit in the Linux
    build. See the bug for more context.
    
    TBR=eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/18023006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153172 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/websockets/ThreadableWebSocketChannelClientWrapper.cpp
+++ third_party/WebKit/Source/modules/websockets/ThreadableWebSocketChannelClientWrapper.cpp
@@ -112,7 +112,7 @@ void ThreadableWebSocketChannelClientWrapper::setSubprotocol(const String& subpr
     unsigned length = subprotocol.length();
     m_subprotocol.resize(length);
     if (length)
-        memcpy(m_subprotocol.data(), subprotocol.characters(), sizeof(UChar) * length);
+        memcpy(m_subprotocol.data(), subprotocol.bloatedCharacters(), sizeof(UChar) * length);
 }
 
 String ThreadableWebSocketChannelClientWrapper::extensions() const
@@ -127,7 +127,7 @@ void ThreadableWebSocketChannelClientWrapper::setExtensions(const String& extens
     unsigned length = extensions.length();
     m_extensions.resize(length);
     if (length)
-        memcpy(m_extensions.data(), extensions.characters(), sizeof(UChar) * length);
+        memcpy(m_extensions.data(), extensions.bloatedCharacters(), sizeof(UChar) * length);
 }
 
 WebSocketChannel::SendResult ThreadableWebSocketChannelClientWrapper::sendRequestResult() const
