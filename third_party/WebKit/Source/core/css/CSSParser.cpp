a10793135811001a3f9e4224a06b519471abe184
jchaffraix@chromium.org <jchaffraix@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-27 03:21:17 +0000

    Unprefix -webkit-min-content and -webkit-max-content for grid layout
    This change adds the unprefixed version of the 2 values so that grid
    layout can use them. As the parsing / style resolution changes are
    grid specific, the impact of this is very minimal.
    
    The only impact outside grid is the following:
    
    element.style.width = "-webkit-min-content";
    width = window.getComputedStyle(element, null).getPropertyValue("width")
    
    |width| was "-webkit-min-content" before this change but now will be
    "min-content". Same for -webkit-max-content.
    
    TESTS=LayoutTests/fast/css-grid-layout/
    BUG=245878
    
    Review URL: https://chromiumcodereview.appspot.com/16404015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153111 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/CSSParser.cpp
+++ third_party/WebKit/Source/core/css/CSSParser.cpp
@@ -4662,7 +4662,7 @@ PassRefPtr<CSSPrimitiveValue> CSSParser::parseGridTrackSize()
 
 PassRefPtr<CSSPrimitiveValue> CSSParser::parseGridBreadth(CSSParserValue* currentValue)
 {
-    if (currentValue->id == CSSValueWebkitMinContent || currentValue->id == CSSValueWebkitMaxContent)
+    if (currentValue->id == CSSValueMinContent || currentValue->id == CSSValueMaxContent)
         return cssValuePool().createIdentifierValue(currentValue->id);
 
     if (currentValue->unit == CSSPrimitiveValue::CSS_FR) {
