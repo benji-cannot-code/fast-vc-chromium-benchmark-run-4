39b9bd145780d9f4a06d5e1964f8746cdf1ec751
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-28 14:11:24 +0000

    Revert "WebEntities shouldn't use bloated characters"
    This reverts commit e10e118c7f925c8a60072e789bc7303bf78dd322.
    (broke DomSerializerTests.SerializeHTMLDOMWithEntitiesInAttributeValue)
    
    TBR=abarth,dglazkov
    
    Review URL: https://codereview.chromium.org/18034019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153240 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/src/WebEntities.h
+++ third_party/WebKit/Source/WebKit/chromium/src/WebEntities.h
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebKit {
 
-// FIXME: This class is wrong and needs to be removed!
+// FIXME: This class is wrong and needs to be removed.
 class WebEntities {
 public:
     // &apos;, &percnt;, &nsup;, &supl; are not defined by the HTML standards.
