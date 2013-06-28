62b2aa5088a1e9bcc9811f0bacae934f5add0268
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-28 04:02:41 +0000

    Use forward declarations instead of Skia headers in GraphicsContext.h.
    We should try to isolate Skia within the graphics subsystem, and not include its headers for all GraphicsContext.h users.
    
    R=schenney@chromium.org,senorblanco@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18034010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153192 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/Canvas2DLayerBridgeTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/Canvas2DLayerBridgeTest.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/ImageBuffer.h"
 #include "public/platform/Platform.h"
 #include "public/platform/WebThread.h"
+#include "third_party/skia/include/core/SkDevice.h"
 
 #include <gmock/gmock.h>
 #include <gtest/gtest.h>
