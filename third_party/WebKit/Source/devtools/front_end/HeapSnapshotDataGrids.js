e9770e83c0bf16aee02fb28bae485b689bcfcb48
yurys@chromium.org <yurys@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-28 09:28:38 +0000

    Preserve selection in the Heap profiler Construtor's view
    BUG=233892
    TEST=None
    R=loislo@chromium.org
    
    Review URL: https://codereview.chromium.org/18076004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153224 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/HeapSnapshotDataGrids.js
+++ third_party/WebKit/Source/devtools/front_end/HeapSnapshotDataGrids.js
@@ -310,6 +310,8 @@ WebInspector.HeapSnapshotViewportDataGrid.prototype = {
             ++i;
         }
 
+        var selectedNode = this.selectedNode;
+
         this.rootNode().removeChildren();
         // The height of the view port + invisible top part.
         var heightToFill = viewPortHeight + (scrollTop - topPadding);
@@ -329,6 +331,15 @@ WebInspector.HeapSnapshotViewportDataGrid.prototype = {
         }
 
         this._addPaddingRows(topPadding, bottomPadding);
+
+        if (selectedNode) {
+            if (selectedNode.parent) {
+                selectedNode.select(true);
+            } else {
+                // Keep selection even if the node is not in the current viewport.
+                this.selectedNode = selectedNode;
+            }
+        }
     },
 
     appendTopLevelNode: function(node)
