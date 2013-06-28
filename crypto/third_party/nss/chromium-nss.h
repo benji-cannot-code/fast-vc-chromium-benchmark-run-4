0e68f03f0d9db74ed3d01cb7bf2f3ab8998fc5a6
wtc@chromium.org <wtc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-28 17:46:53 +0000

    Add SignatureVerifier::VerifyInitRSAPSS for verifying RSA-PSS signatures.
    Change the OpenSSL-based SignatureVerifier to use EVP_DigestVerifyInit
    instead of EVP_VerifyInit_ex.
    
    Copy the PSS padding verification code from NSS to the NSS-based
    SignatureVerifier because the RSA-PSS code in the NSS softoken isn't
    exposed via the NSS PK11_ or VFY_ functions yet.
    
    R=agl@chromium.org,rsleevi@chromium.org
    BUG=none
    TEST=to be added to net_unittests via testing net::quic::ProofVerifier.
    
    Review URL: https://chromiumcodereview.appspot.com/17776003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209178 0039d316-1c4b-4281-b951-d872f2087c98

--- crypto/third_party/nss/chromium-nss.h
+++ crypto/third_party/nss/chromium-nss.h
@@ -40,10 +40,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // This file contains some functions we borrowed from NSS.
 
+#include <prtypes.h>
+#include <hasht.h>
 #include <keyhi.h>
 #include <secmod.h>
 
 #include "crypto/crypto_export.h"
+
+extern "C" SECStatus emsa_pss_verify(const unsigned char *mHash,
+                                     const unsigned char *em,
+                                     unsigned int emLen,
+                                     HASH_HashType hashAlg,
+                                     HASH_HashType maskHashAlg,
+                                     unsigned int sLen);
 
 // Like PK11_ImportEncryptedPrivateKeyInfo, but hardcoded for EC, and returns
 // the SECKEYPrivateKey.
