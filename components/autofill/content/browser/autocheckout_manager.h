759409bad17d47b1e0f25ba1a8c196c86503ef0c
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-28 16:50:58 +0000

    Delete time forwarding headers.
    BUG=254986
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/18084012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209164 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/content/browser/autocheckout_manager.h
+++ components/autofill/content/browser/autocheckout_manager.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/strings/string16.h"
 #include "base/threading/thread_checker.h"
-#include "base/time.h"
+#include "base/time/time.h"
 #include "components/autofill/content/browser/autocheckout_page_meta_data.h"
 #include "components/autofill/content/browser/autocheckout_statistic.h"
 #include "components/autofill/core/common/autocheckout_status.h"
