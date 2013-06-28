ef4ad059d3421ad62c164815aa2048c261463dd2
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-28 16:22:10 +0000

    Remove SVGTests related code from SVGGraphicsElement subclasses
    Remove SVGTests related code from SVGGraphicsElement subclasses and it
    add to the SVGGraphicsElement class instead. SVGGraphicsElement is the
    one subclassing SVGTests after r153166.
    
    Review URL: https://chromiumcodereview.appspot.com/18054014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153248 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGPolyElement.h
+++ third_party/WebKit/Source/core/svg/SVGPolyElement.h
@@ -61,11 +61,6 @@ private:
         DECLARE_ANIMATED_BOOLEAN(ExternalResourcesRequired, externalResourcesRequired)
     END_DECLARE_ANIMATED_PROPERTIES
 
-    // SVGTests
-    virtual void synchronizeRequiredFeatures() { SVGTests::synchronizeRequiredFeatures(this); }
-    virtual void synchronizeRequiredExtensions() { SVGTests::synchronizeRequiredExtensions(this); }
-    virtual void synchronizeSystemLanguage() { SVGTests::synchronizeSystemLanguage(this); }
-
 protected:
     mutable SVGSynchronizableAnimatedProperty<SVGPointList> m_points;
 };
