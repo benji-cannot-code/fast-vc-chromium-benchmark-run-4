c211dbf9f16957c6a01d84a3dae3f751afecbd78
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-28 05:52:10 +0000

    Revert 209071 "components/webdata: Break the dependency on chrom..."
    speculative revert to see if it fixes the android bots.
    
    > components/webdata: Break the dependency on chrome/test/base
    > 
    > BUG=181277
    > TBR=joi@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/17788002
    
    TBR=tfarina@chromium.org
    
    Review URL: https://codereview.chromium.org/18177003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209112 0039d316-1c4b-4281-b951-d872f2087c98

--- components/webdata/common/web_database_migration_unittest.cc
+++ components/webdata/common/web_database_migration_unittest.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/scoped_temp_dir.h"
 #include "base/guid.h"
 #include "base/message_loop.h"
-#include "base/path_service.h"
 #include "base/stl_util.h"
 #include "base/strings/string16.h"
 #include "base/strings/string_number_conversions.h"
@@ -21,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/webdata/token_service_table.h"
 #include "chrome/browser/webdata/web_apps_table.h"
 #include "chrome/browser/webdata/web_intents_table.h"
+#include "chrome/test/base/ui_test_utils.h"
 #include "components/autofill/core/browser/autofill_country.h"
 #include "components/autofill/core/browser/autofill_profile.h"
 #include "components/autofill/core/browser/autofill_type.h"
@@ -208,18 +208,13 @@ class WebDatabaseMigrationTest : public testing::Test {
   }
 
   // The textual contents of |file| are read from
-  // "components/test/data/web_database" and returned in the string |contents|.
+  // "chrome/test/data/web_database" and returned in the string |contents|.
   // Returns true if the file exists and is read successfully, false otherwise.
   bool GetWebDatabaseData(const base::FilePath& file, std::string* contents) {
-    base::FilePath source_path;
-    PathService::Get(base::DIR_SOURCE_ROOT, &source_path);
-    source_path = source_path.AppendASCII("components");
-    source_path = source_path.AppendASCII("test");
-    source_path = source_path.AppendASCII("data");
-    source_path = source_path.AppendASCII("web_database");
-    source_path = source_path.Append(file);
-    return file_util::PathExists(source_path) &&
-        file_util::ReadFileToString(source_path, contents);
+    base::FilePath path = ui_test_utils::GetTestFilePath(
+        base::FilePath(FILE_PATH_LITERAL("web_database")), file);
+    return file_util::PathExists(path) &&
+        file_util::ReadFileToString(path, contents);
   }
 
   static int VersionFromConnection(sql::Connection* connection) {
