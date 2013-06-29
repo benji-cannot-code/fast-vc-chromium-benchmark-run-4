19e45205f07d40930ed0ae4f3897b6d5e90b65ef
fmalita@chromium.org <fmalita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-29 01:32:45 +0000

    Streamline GraphicsContext::clipToImageBuffer().
    We can save a couple of innocent stack frames by getting rid of the following (no longer needed) indirection:
    
    GraphicsContext::clipToImageBuffer -> ImageBuffer::clip -> GraphicsContext::beginLayerClippedToImage.
    
    R=schenney@chromium.org,senorblanco@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18177009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/GraphicsContext.cpp
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsContext.cpp
@@ -434,7 +434,7 @@ void GraphicsContext::endTransparencyLayer()
 #endif
 }
 
-void GraphicsContext::beginLayerClippedToImage(const FloatRect& rect, const ImageBuffer* imageBuffer)
+void GraphicsContext::clipToImageBuffer(const ImageBuffer* imageBuffer, const FloatRect& rect)
 {
     if (paintingDisabled())
         return;
@@ -1413,11 +1413,6 @@ void GraphicsContext::clipConvexPolygon(size_t numPoints, const FloatPoint* poin
     SkPath path;
     setPathFromConvexPoints(&path, numPoints, points);
     clipPath(path, antialiased ? AntiAliased : NotAntiAliased);
-}
-
-void GraphicsContext::clipToImageBuffer(ImageBuffer* buffer, const FloatRect& rect)
-{
-    buffer->clip(this, rect);
 }
 
 void GraphicsContext::clipOutRoundedRect(const RoundedRect& rect)
