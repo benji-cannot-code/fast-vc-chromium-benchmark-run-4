f77c977b85c5431004f72931767564fd9d47d364
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-29 13:34:54 +0000

    drive: Replace FileCache::Iterate with FileCache::GetIterator
    BUG=242818
    TEST=unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/18153006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209298 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_cache.h
+++ chrome/browser/chromeos/drive/file_cache.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/drive/file_errors.h"
+#include "chrome/browser/chromeos/drive/resource_metadata_storage.h"
 
 class Profile;
 
@@ -41,8 +42,6 @@ typedef base::Callback<void(const std::string& resource_id,
     CacheIterateCallback;
 
 namespace internal {
-
-class ResourceMetadataStorage;
 
 // Callback for GetFileFromCache.
 typedef base::Callback<void(FileError error,
@@ -74,6 +73,8 @@ class FileCache {
     FILE_OPERATION_MOVE = 0,
     FILE_OPERATION_COPY,
   };
+
+  typedef ResourceMetadataStorage::CacheEntryIterator Iterator;
 
   // Name of the cache metadata DB previously used.
   // TODO(hashimoto): Remove this at some point.
@@ -121,10 +122,8 @@ class FileCache {
   void IterateOnUIThread(const CacheIterateCallback& iteration_callback,
                          const base::Closure& completion_callback);
 
-  // Iterates all files in the cache and calls |iteration_callback| for each
-  // file. |completion_callback| is run upon completion.
-  // TODO(hashimoto): Stop using callbacks for this method. crbug.com/242818
-  void Iterate(const CacheIterateCallback& iteration_callback);
+  // Returns an object to iterate over entries.
+  scoped_ptr<Iterator> GetIterator();
 
 
   // Runs FreeDiskSpaceIfNeededFor() on |blocking_task_runner_|, and calls
