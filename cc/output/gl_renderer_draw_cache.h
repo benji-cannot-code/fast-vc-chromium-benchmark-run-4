8d9b7215f89c7944e6ff5a140cf258ddc0f1b504
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-06-29 00:35:12 +0000

    Premultiply alpha in shader for non-premultiplied textures.
    Instead of using different blending equations we can perform
    the pre-multiplication in the shader itself.
    
    I am working on improving the performance of compositing canvas
    elements with opaque background. This requires blending the
    canvas texture with the background color in the shader. The
    implementation of this blending equation becomes much easier if
    we can assume that textures are always premultiplied.
    
    BUG=236982
    TEST=compositing/webgl/webgl-nonpremultiplied-blend.html
    
    Review URL: https://chromiumcodereview.appspot.com/16943021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209246 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/output/gl_renderer_draw_cache.h
+++ cc/output/gl_renderer_draw_cache.h
@@ -32,7 +32,6 @@ struct TexturedQuadDrawCache {
   // Values tracked to determine if textured quads may be coalesced.
   int program_id;
   int resource_id;
-  bool use_premultiplied_alpha;
   bool needs_blending;
 
   // Information about the program binding that is required to draw.
@@ -46,6 +45,7 @@ struct TexturedQuadDrawCache {
   std::vector<float> vertex_opacity_data;
   std::vector<Float16> matrix_data;
 
+ private:
   DISALLOW_COPY_AND_ASSIGN(TexturedQuadDrawCache);
 };
 
