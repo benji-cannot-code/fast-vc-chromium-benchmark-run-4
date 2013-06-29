d7d86154c7107932ce671d775bfeb0f62a6d9aa9
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-06-29 03:07:14 +0000

    Remove unused "identifier" concept from WTFString
    We don't use this bit anywhere. It was probably used to integrate with JSC.
    
    R=eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/18243004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153291 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/StringImpl.h
+++ third_party/WebKit/Source/wtf/text/StringImpl.h
@@ -149,7 +149,7 @@ private:
         , m_buffer(0)
         , m_refCount(s_refCountFlagIsStaticString)
         , m_length(length)
-        , m_hashAndFlags(s_hashFlagIsIdentifier | BufferOwned)
+        , m_hashAndFlags(BufferOwned)
     {
         // Ensure that the hash is computed so that AtomicStringHash can call existingHash()
         // with impunity. The empty string is special because it is never entered into
@@ -167,7 +167,7 @@ private:
         , m_buffer(0)
         , m_refCount(s_refCountFlagIsStaticString)
         , m_length(length)
-        , m_hashAndFlags(s_hashFlag8BitBuffer | s_hashFlagIsIdentifier | BufferOwned)
+        , m_hashAndFlags(s_hashFlag8BitBuffer | BufferOwned)
     {
         // Ensure that the hash is computed so that AtomicStringHash can call existingHash()
         // with impunity. The empty string is special because it is never entered into
@@ -450,15 +450,6 @@ public:
 
     bool has16BitShadow() const { return m_hashAndFlags & s_hashFlagHas16BitShadow; }
     void upconvertCharacters(unsigned, unsigned) const;
-    bool isIdentifier() const { return m_hashAndFlags & s_hashFlagIsIdentifier; }
-    void setIsIdentifier(bool isIdentifier)
-    {
-        ASSERT(!isStatic());
-        if (isIdentifier)
-            m_hashAndFlags |= s_hashFlagIsIdentifier;
-        else
-            m_hashAndFlags &= ~s_hashFlagIsIdentifier;
-    }
 
     bool isEmptyUnique() const
     {
@@ -715,7 +706,6 @@ private:
     static const unsigned s_hashFlagHasTerminatingNullCharacter = 1u << 5;
     static const unsigned s_hashFlagIsAtomic = 1u << 4;
     static const unsigned s_hashFlagDidReportCost = 1u << 3;
-    static const unsigned s_hashFlagIsIdentifier = 1u << 2;
     static const unsigned s_hashMaskBufferOwnership = 1u | (1u << 1);
 
 #ifdef STRING_STATS
