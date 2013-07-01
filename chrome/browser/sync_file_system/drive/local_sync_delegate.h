e14acf6543f900e36ca7ed31dae0efdfcf4773c3
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 11:19:30 +0000

    Extract DriveFileSyncService::ResolveConflictResolution
    Also refactor conflict resolution handling for RemoteSync
    (so that it becomes easier to factor out RemoteSyncDelegate).
    
    BUG=242134
    TEST=green bots
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/18050016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209411 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive/local_sync_delegate.h
+++ chrome/browser/sync_file_system/drive/local_sync_delegate.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace sync_file_system {
 
+class ConflictResolutionResolver;
 class DriveMetadataStore;
 
 namespace drive {
@@ -116,6 +117,7 @@ class LocalSyncDelegate {
   DriveMetadataStore* metadata_store();
   drive::APIUtilInterface* api_util();
   RemoteChangeHandler* remote_change_handler();
+  ConflictResolutionResolver* conflict_resolution_resolver();
 
   DriveFileSyncService* sync_service_;  // Not owned.
 
