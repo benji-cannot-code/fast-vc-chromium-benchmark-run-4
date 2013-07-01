3b398ae30c5f4f0e2a6f688d02ebf95217ec898b
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-01 07:05:40 +0000

    Group the ImageDecoder::ZeroFill* functions together
    This improve the placement and also permits us to cleanly revert
    blink 153004
    
    TBR=tkent@chromium.org
    BUG=254462
    
    Review URL: https://codereview.chromium.org/18146004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153322 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/ImageDecoder.h
+++ third_party/WebKit/Source/core/platform/image-decoders/ImageDecoder.h
@@ -78,6 +78,7 @@ namespace WebCore {
         // These do not touch other metadata, only the raw pixel data.
         void clearPixelData();
         void zeroFillPixelData();
+        void zeroFillFrameRect(const IntRect&);
 
         // Makes this frame have an independent copy of the provided image's
         // pixel data, so that modifications in one frame are not reflected in
@@ -215,8 +216,6 @@ namespace WebCore {
 
             *dest = SkPackARGB32NoCheck(a, r, g, b);
         }
-
-        void zeroFillFrameRect(const IntRect&);
 
     private:
         int width() const
