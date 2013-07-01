3bfac58ec9ffb148c1158633fbaf71eb22aa24be
noelallen@chromium.org <noelallen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 17:56:22 +0000

    Adds the ability to process   identifier      or   identifier = ConstValue
    and removes
      string
    
    from Enums for Pepper IDL.
    
    BUG=224150
    TEST=PRESUBMIT or ./run_tests.py
    R=nfullagar@chromium.org
    
    Review URL: https://codereview.chromium.org/18273002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209454 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/idl_parser/PRESUBMIT.py
+++ tools/idl_parser/PRESUBMIT.py
@@ -4,7 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 
-WHITELIST = [ r'.+_test.py$' ]
+WHITELIST = [ r'^.+_test\.py$' ]
 
 def CheckChangeOnUpload(input_api, output_api):
   return input_api.canned_checks.RunUnitTestsInDirectory(
