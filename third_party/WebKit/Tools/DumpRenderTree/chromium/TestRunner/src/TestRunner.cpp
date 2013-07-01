01370b8fc71627b56a66ae0040c2e4e06e02bf16
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-01 16:14:04 +0000

    Reland "Remove our logic to handle stopping a load inside startProvisionaLoad callbacks"
    This reverts commit 5c418ddb584ec86e340626e8140ef04d0eb5e5fc.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/18153008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153351 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp
+++ third_party/WebKit/Tools/DumpRenderTree/chromium/TestRunner/src/TestRunner.cpp
@@ -236,7 +236,6 @@ TestRunner::TestRunner(TestInterfaces* interfaces)
     bindMethod("setAudioData", &TestRunner::setAudioData);
     bindMethod("dumpFrameLoadCallbacks", &TestRunner::dumpFrameLoadCallbacks);
     bindMethod("dumpUserGestureInFrameLoadCallbacks", &TestRunner::dumpUserGestureInFrameLoadCallbacks);
-    bindMethod("setStopProvisionalFrameLoads", &TestRunner::setStopProvisionalFrameLoads);
     bindMethod("dumpTitleChanges", &TestRunner::dumpTitleChanges);
     bindMethod("dumpCreateView", &TestRunner::dumpCreateView);
     bindMethod("setCanOpenWindows", &TestRunner::setCanOpenWindows);
@@ -392,7 +391,6 @@ void TestRunner::reset()
     m_dumpAsAudio = false;
     m_dumpFrameLoadCallbacks = false;
     m_dumpUserGestureInFrameLoadCallbacks = false;
-    m_stopProvisionalFrameLoads = false;
     m_dumpTitleChanges = false;
     m_dumpCreateView = false;
     m_canOpenWindows = false;
@@ -519,11 +517,6 @@ void TestRunner::setShouldDumpFrameLoadCallbacks(bool value)
 bool TestRunner::shouldDumpUserGestureInFrameLoadCallbacks() const
 {
     return m_testIsRunning && m_dumpUserGestureInFrameLoadCallbacks;
-}
-
-bool TestRunner::stopProvisionalFrameLoads() const
-{
-    return m_stopProvisionalFrameLoads;
 }
 
 bool TestRunner::shouldDumpTitleChanges() const
@@ -1906,12 +1899,6 @@ void TestRunner::dumpUserGestureInFrameLoadCallbacks(const CppArgumentList&, Cpp
 {
     m_dumpUserGestureInFrameLoadCallbacks = true;
     result->setNull();
-}
-
-void TestRunner::setStopProvisionalFrameLoads(const CppArgumentList&, CppVariant* result)
-{
-    result->setNull();
-    m_stopProvisionalFrameLoads = true;
 }
 
 void TestRunner::dumpTitleChanges(const CppArgumentList&, CppVariant* result)
