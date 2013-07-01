212f9b108d831ccfb7e06e4b05b51f14dfce15b6
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 21:00:30 +0000

    Fix and reenable ExtensionApiTest.UpdateWindowShowState for Windows
    The test failed on try bots that are configured with smaller resolution and the maximium panel size is based on a ratio of the working area. The fix is to use a smaller height for creating panel window.
    
    BUG=176683
    TEST=test reenabled
    
    Review URL: https://chromiumcodereview.appspot.com/18331008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209496 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/extension_tabs_apitest.cc
+++ chrome/browser/extensions/extension_tabs_apitest.cc
@@ -23,15 +23,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define MAYBE_UpdateWindowShowState DISABLED_UpdateWindowShowState
 #else
 
-#if defined(USE_AURA) || defined(OS_MACOSX) || defined(OS_WIN)
+#if defined(USE_AURA) || defined(OS_MACOSX)
 // Maximizing/fullscreen popup window doesn't work on aura's managed mode.
 // See bug crbug.com/116305.
 // Mac: http://crbug.com/103912
-// Failing on Win7: http://crbug.com/176683
 #define MAYBE_UpdateWindowShowState DISABLED_UpdateWindowShowState
 #else
 #define MAYBE_UpdateWindowShowState UpdateWindowShowState
-#endif  // defined(USE_AURA) || defined(OS_MACOSX) || defined(OS_WIN)
+#endif  // defined(USE_AURA) || defined(OS_MACOSX)
 
 #define MAYBE_UpdateWindowResize UpdateWindowResize
 #endif  // defined(OS_LINUX) && !defined(USE_AURA)
