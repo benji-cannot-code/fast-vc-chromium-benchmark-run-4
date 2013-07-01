14cb600dca921585f40a80781298d534dbca9fef
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-01 15:36:22 +0000

    CSP: "Value values" should be "Valid values" in 'reflected-xss' error message.
    Review URL: https://chromiumcodereview.appspot.com/18323002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153349 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/ContentSecurityPolicy.cpp
+++ third_party/WebKit/Source/core/page/ContentSecurityPolicy.cpp
@@ -1803,7 +1803,7 @@ void ContentSecurityPolicy::reportInvalidSandboxFlags(const String& invalidFlags
 
 void ContentSecurityPolicy::reportInvalidReflectedXSS(const String& invalidValue) const
 {
-    logToConsole("The 'reflected-xss' Content Security Policy directive has the invalid value \"" + invalidValue + "\". Value values are \"allow\", \"filter\", and \"block\".");
+    logToConsole("The 'reflected-xss' Content Security Policy directive has the invalid value \"" + invalidValue + "\". Valid values are \"allow\", \"filter\", and \"block\".");
 }
 
 void ContentSecurityPolicy::reportInvalidDirectiveValueCharacter(const String& directiveName, const String& value) const
