db06ee9483dba05d04e9ece97175a0c526f0d420
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-01 10:35:32 +0000

    [DevTools] Network Panel: add columns for common response headers.
    EOM
    
    BUG=129637
    
    Review URL: https://chromiumcodereview.appspot.com/15735025
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153337 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/NetworkRequest.js
+++ third_party/WebKit/Source/devtools/front_end/NetworkRequest.js
@@ -59,6 +59,8 @@ WebInspector.NetworkRequest = function(requestId, url, documentURL, frameId, loa
     this._contentEncoded = false;
     this._pendingContentCallbacks = [];
     this._frames = [];
+
+    this._responseHeaderValues = {};
 }
 
 WebInspector.NetworkRequest.Events = {
@@ -574,6 +576,7 @@ WebInspector.NetworkRequest.prototype = {
         this._responseHeaders = x;
         delete this._sortedResponseHeaders;
         delete this._responseCookies;
+        this._responseHeaderValues = {};
 
         this.dispatchEventToListeners(WebInspector.NetworkRequest.Events.ResponseHeadersChanged);
     },
@@ -626,7 +629,12 @@ WebInspector.NetworkRequest.prototype = {
      */
     responseHeaderValue: function(headerName)
     {
-        return this._headerValue(this.responseHeaders, headerName);
+        var value = this._responseHeaderValues[headerName];
+        if (value === undefined) {
+            value = this._headerValue(this.responseHeaders, headerName);
+            this._responseHeaderValues[headerName] = (value !== undefined) ? value : null;
+        }
+        return (value !== null) ? value : undefined;
     },
 
     /**
