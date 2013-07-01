8b0a40bb9ebcabdd58e975e8198254ef88d90873
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 23:08:05 +0000

    Remove old GPU latency test.
    This is mostly redundant with the new telemetry-based latency tests.
    
    
    BUG=249207
    
    Review URL: https://chromiumcodereview.appspot.com/17847009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209533 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/chrome_tests.gypi
+++ chrome/chrome_tests.gypi
@@ -2133,7 +2133,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'test/base/chrome_test_launcher.cc',
         'test/perf/browser_perf_test.cc',
         'test/perf/browser_perf_test.h',
-        'test/perf/rendering/latency_tests.cc',
         'test/perf/rendering/throughput_tests.cc',
       ],
       'rules': [
--- ui/surface/accelerated_surface_win.cc
+++ ui/surface/accelerated_surface_win.cc
@@ -842,10 +842,6 @@ void AcceleratedPresenter::DoPresentAndAcknowledge(
 
     hr = swap_chain_->Present(&rect, &rect, window_, NULL, 0);
 
-    // For latency_tests.cc:
-    UNSHIPPED_TRACE_EVENT_INSTANT0("test_gpu", "CompositorSwapBuffersComplete",
-                                   TRACE_EVENT_SCOPE_THREAD);
-
     if (FAILED(hr)) {
       if (present_thread_->IsDeviceLost())
         present_thread_->ResetDevice();
@@ -1024,10 +1020,6 @@ void AcceleratedPresenter::PresentWithGDI(HDC dc) {
   }
 
   system_surface->UnlockRect();
-
-  // For latency_tests.cc:
-  UNSHIPPED_TRACE_EVENT_INSTANT0("test_gpu", "CompositorSwapBuffersComplete",
-                                 TRACE_EVENT_SCOPE_THREAD);
 }
 
 gfx::Size AcceleratedPresenter::GetWindowSize() {
