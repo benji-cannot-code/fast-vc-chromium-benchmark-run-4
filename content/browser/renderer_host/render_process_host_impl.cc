58b99328279036ec4d66236cf29a3f6b91516716
wonsik@chromium.org <wonsik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-01 13:43:01 +0000

    Revert "Add a switch to retain video playback on lost focus"
    This reverts commit fb4b78c65d3bd6d2afe00a86a1e92668e9283713.
    
    The switch is no longer used.
    
    BUG=NONE
    R=qinmin@chromium.org,ycheo@chromium.org,jochen@chromium.org,joi@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18272003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209422 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -967,7 +967,6 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
 #if defined(OS_ANDROID)
     switches::kNetworkCountryIso,
     switches::kDisableGestureRequirementForMediaPlayback,
-    switches::kRetainVideoPlaybackOnLostFocus,
 #endif
 #if defined(GOOGLE_TV)
     switches::kUseExternalVideoSurfaceThresholdInPixels,
