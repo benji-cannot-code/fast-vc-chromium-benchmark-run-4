4fadb942fbf0f051aca0e46070c866b3a01b1413
sbc@chromium.org <sbc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 18:11:57 +0000

    [NaCl SDK] Allow use of goma for building libs/examples.
    This also enables users of our build system to use things
    like cache and distcc via the NACL_COMPILER_PREFIX setting.
    
    Goma is not enabled on the bots, or by default just yet.
    
    R=binji@chromium.org, noelallen@chromium.org
    
    Review URL: https://codereview.chromium.org/17759004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209728 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/tools/host_gcc.mk
+++ native_client_sdk/src/tools/host_gcc.mk
@@ -15,8 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # We use the C++ compiler for everything and then use the -Wl,-as-needed flag
 # in the linker to drop libc++ unless it's actually needed.
 #
-HOST_CC ?= gcc
-HOST_CXX ?= g++
+HOST_CC ?= $(NACL_COMPILER_PREFIX) gcc
+HOST_CXX ?= $(NACL_COMPILER_PREFIX) g++
 HOST_LINK ?= g++
 HOST_LIB ?= ar
 HOST_STRIP ?= strip
