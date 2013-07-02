911f604f14c8172db2abe50f943cf02732b1f3ee
dslomov@chromium.org <dslomov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-02 12:09:06 +0000

    Use V8 implementation of ArrayBuffer in Blink.
    Fixes chromium test failures introduced in previous version of this patch
    (https://src.chromium.org/viewvc/blink?view=rev&revision=153314).
    
    R=kbr@chromium.org,jochen@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18233012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153419 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8FileReaderCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8FileReaderCustom.cpp
@@ -33,8 +33,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "V8FileReader.h"
 
-#include "V8ArrayBuffer.h"
 #include "bindings/v8/V8Binding.h"
+#include "bindings/v8/custom/V8ArrayBufferCustom.h"
 #include "core/dom/ScriptExecutionContext.h"
 
 namespace WebCore {
--- third_party/WebKit/Source/wtf/ArrayBuffer.cpp
+++ third_party/WebKit/Source/wtf/ArrayBuffer.cpp
@@ -52,6 +52,9 @@ bool ArrayBuffer::transfer(ArrayBufferContents& result, Vector<RefPtr<ArrayBuffe
         current->neuter();
         neuteredViews.append(current);
     }
+
+    m_isNeutered = true;
+
     return true;
 }
 
