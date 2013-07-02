c8c7824a489a90d8182ac0b5b7f3070d2d436311
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 21:05:11 +0000

    Introduces cross-fade animation for maximize/restore.
    BUG=249518
    R=jamescook@chromium.org
    TEST=no break on ash_unittests
    
    Review URL: https://chromiumcodereview.appspot.com/18427002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209774 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_layout_manager.cc
+++ ash/wm/workspace/workspace_layout_manager.cc
@@ -233,11 +233,8 @@ void WorkspaceLayoutManager::OnWindowPropertyChanged(Window* window,
   if (key == internal::kWindowTrackedByWorkspaceKey &&
       GetTrackedByWorkspace(window)) {
     workspace_manager()->OnTrackedByWorkspaceChanged(workspace_, window);
-    if (wm::IsWindowMaximized(window)) {
-      SetChildBoundsDirect(
-          window, ScreenAsh::GetMaximizedWindowBoundsInParent(
-              window->parent()->parent()));
-    }
+    if (wm::IsWindowMaximized(window))
+      SetMaximizedOrFullscreenBounds(window);
   }
 
   if (key == aura::client::kAlwaysOnTopKey &&
@@ -358,7 +355,7 @@ void WorkspaceLayoutManager::UpdateBoundsFromShowState(Window* window) {
         gfx::Rect bounds_in_parent =
             ScreenAsh::ConvertRectFromScreen(window->parent()->parent(),
                                              *restore);
-        SetChildBoundsDirect(
+        CrossFadeToBounds(
             window,
             BaseLayoutManager::BoundsWithScreenEdgeVisible(
                 window->parent()->parent(),
@@ -369,6 +366,9 @@ void WorkspaceLayoutManager::UpdateBoundsFromShowState(Window* window) {
     }
 
     case ui::SHOW_STATE_MAXIMIZED:
+      CrossFadeToBounds(window, ScreenAsh::GetMaximizedWindowBoundsInParent(
+          window->parent()->parent()));
+      break;
     case ui::SHOW_STATE_FULLSCREEN:
       SetMaximizedOrFullscreenBounds(window);
       break;
