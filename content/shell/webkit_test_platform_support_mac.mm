b9cdffaa48d8fd70183da034797096726251eaf6
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 15:22:10 +0000

    Set AppleShowScrollBars to Always in SetDefaultsToLayoutTestValues().
    BUG=255173
    
    Review URL: https://chromiumcodereview.appspot.com/18053011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209704 0039d316-1c4b-4281-b951-d872f2087c98

--- content/shell/webkit_test_platform_support_mac.mm
+++ content/shell/webkit_test_platform_support_mac.mm
@@ -39,6 +39,8 @@ void SetDefaultsToLayoutTestValues(void) {
                forKey:@"AppleLanguages"];
   [defaults setBool:NO
              forKey:@"AppleScrollAnimationEnabled"];
+  [defaults setObject:@"Always"
+               forKey:@"AppleShowScrollBars"];
 }
 
 }  // namespace
