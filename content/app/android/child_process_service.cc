a9d2c578cdb6517132e3114aa26529d9749e3dbf
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 12:48:22 +0000

    MemoryPressure: sends the signal to the renderer.
    BUG=251723
    
    Review URL: https://chromiumcodereview.appspot.com/17351006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209686 0039d316-1c4b-4281-b951-d872f2087c98

--- content/app/android/child_process_service.cc
+++ content/app/android/child_process_service.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <cpu-features.h>
 
 #include "base/android/jni_array.h"
+#include "base/android/memory_pressure_listener_android.h"
 #include "base/logging.h"
 #include "base/posix/global_descriptors.h"
 #include "content/child/child_thread.h"
@@ -102,6 +103,7 @@ void InternalInitChildProcess(const std::vector<int>& file_ids,
   content::SurfaceTexturePeer::InitInstance(
       new SurfaceTexturePeerChildImpl(service));
 
+  base::android::MemoryPressureListenerAndroid::RegisterSystemCallback(env);
 }
 
 }  // namespace <anonymous>
