08e9b25011d399a9beee1b3f6ca77c1e7d0b95cd
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 22:24:57 +0000

    Make --force-fieldtrials not activate them in the browser process.
    This is to make this testing flag more consistent with what happens
    with a server-side field trial, so that the field trial doesn't
    appear in about:version until client code explicitly queries its
    state via a group() call.
    
    Does not change the behaviour of the --force-fieldtrials flag
    when passes to the renderer process, where it does need to
    activate the trials so that they show up in crash reports, etc.
    
    BUG=255142
    TEST=Start a debug build of Chrome with the command line
    flag --force-fieldtrials=Bat/Man/ and check that it doesn't
    show up in chrome://version.
    
    Review URL: https://chromiumcodereview.appspot.com/17945002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/search_unittest.cc
+++ chrome/browser/search/search_unittest.cc
@@ -152,8 +152,8 @@ TEST_F(InstantExtendedAPIEnabledTest, EnabledViaCommandLineFlag) {
 }
 
 TEST_F(InstantExtendedAPIEnabledTest, EnabledViaFinchFlag) {
-  ASSERT_TRUE(base::FieldTrialList::CreateTrialsFromString(
-      "InstantExtended/Group1 espv:42/"));
+  ASSERT_TRUE(base::FieldTrialList::CreateFieldTrial("InstantExtended",
+                                                     "Group1 espv:42"));
   EXPECT_TRUE(IsInstantExtendedAPIEnabled());
   EXPECT_EQ(42ul, EmbeddedSearchPageVersion());
   ValidateMetrics(INSTANT_EXTENDED_NOT_SET);
@@ -161,8 +161,8 @@ TEST_F(InstantExtendedAPIEnabledTest, EnabledViaFinchFlag) {
 
 TEST_F(InstantExtendedAPIEnabledTest, DisabledViaCommandLineFlag) {
   GetCommandLine()->AppendSwitch(switches::kDisableInstantExtendedAPI);
-  ASSERT_TRUE(base::FieldTrialList::CreateTrialsFromString(
-      "InstantExtended/Group1 espv:2/"));
+  ASSERT_TRUE(base::FieldTrialList::CreateFieldTrial("InstantExtended",
+                                                     "Group1 espv:2"));
   EXPECT_FALSE(IsInstantExtendedAPIEnabled());
   EXPECT_EQ(0ul, EmbeddedSearchPageVersion());
   ValidateMetrics(INSTANT_EXTENDED_OPT_OUT);
@@ -452,8 +452,8 @@ TEST_F(SearchTest, IsInstantCheckboxEnabledExtendedEnabledWithInstant) {
   EnableInstantExtendedAPIForTesting();
 
   // Enable Instant.
-  ASSERT_TRUE(base::FieldTrialList::CreateTrialsFromString(
-      "InstantExtended/Group1 allow_instant:1/"));
+  ASSERT_TRUE(base::FieldTrialList::CreateFieldTrial("InstantExtended",
+                                                     "Group1 allow_instant:1"));
   ASSERT_TRUE(IsInstantCheckboxVisible());
 
   // Enable suggest.
