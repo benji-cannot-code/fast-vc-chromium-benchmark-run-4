43fd86bf2e0a274d3f65561779bfa4654b20f962
antrim@chromium.org <antrim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 18:39:25 +0000

    Timeout in LMUCreationController now covers whole process (cryptohome, registration service, storing tokens). Cancelling process does not result in RegistrationCallback anymore.
    BUG=244477
    
    Review URL: https://chromiumcodereview.appspot.com/18445005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209735 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/managed/locally_managed_user_creation_controller.cc
+++ chrome/browser/chromeos/login/managed/locally_managed_user_creation_controller.cc
@@ -92,6 +92,11 @@ void LocallyManagedUserCreationController::SetManagerProfile(
 void LocallyManagedUserCreationController::StartCreation() {
   DCHECK(creation_context_);
   VLOG(1) << "Starting supervised user creation";
+  timeout_timer_.Start(
+      FROM_HERE, base::TimeDelta::FromSeconds(kUserCreationTimeoutSeconds),
+      this,
+      &LocallyManagedUserCreationController::CreationTimedOut);
+
   UserManager::Get()->StartLocallyManagedUserCreationTransaction(
       creation_context_->display_name);
 
@@ -112,6 +117,7 @@ void LocallyManagedUserCreationController::StartCreation() {
 
 void LocallyManagedUserCreationController::OnAuthenticationFailure(
     ManagedUserAuthenticator::AuthState error) {
+  timeout_timer_.Stop();
   ErrorCode code = NO_ERROR;
   switch (error) {
     case ManagedUserAuthenticator::NO_MOUNT:
@@ -147,11 +153,6 @@ void LocallyManagedUserCreationController::OnMountSuccess(
 }
 
 void LocallyManagedUserCreationController::OnAddKeySuccess() {
-  timeout_timer_.Start(
-      FROM_HERE, base::TimeDelta::FromSeconds(kUserCreationTimeoutSeconds),
-      this,
-      &LocallyManagedUserCreationController::CreationTimedOut);
-
   creation_context_->service =
       ManagedUserRegistrationServiceFactory::GetForProfile(
           creation_context_->manager_profile);
@@ -168,20 +169,18 @@ void LocallyManagedUserCreationController::OnAddKeySuccess() {
 void LocallyManagedUserCreationController::RegistrationCallback(
     const GoogleServiceAuthError& error,
     const std::string& token) {
-  timeout_timer_.Stop();
   if (error.state() == GoogleServiceAuthError::NONE) {
     TokenFetched(token);
   } else {
-    // Do not report error if we cancelled request.
+    timeout_timer_.Stop();
     LOG(ERROR) << "Managed user creation failed. Error code " << error.state();
-    if (error.state() == GoogleServiceAuthError::REQUEST_CANCELED)
-      return;
     if (consumer_)
       consumer_->OnCreationError(CLOUD_SERVER_ERROR);
   }
 }
 
 void LocallyManagedUserCreationController::CreationTimedOut() {
+  LOG(ERROR) << "Supervised user creation timed out.";
   if (consumer_)
     consumer_->OnCreationTimeout();
 }
@@ -219,6 +218,7 @@ void LocallyManagedUserCreationController::TokenFetched(
 
 void LocallyManagedUserCreationController::OnManagedUserFilesStored(
     bool success) {
+  timeout_timer_.Stop();
   if (!success) {
     if (consumer_)
       consumer_->OnCreationError(TOKEN_WRITE_FAILED);
