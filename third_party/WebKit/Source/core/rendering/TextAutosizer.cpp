e0d3bd9e0aee569911bd0bfab18d054a5471d639
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-02 20:25:56 +0000

    Add trace for text auto sizing
    Locally added trace showed that text auto sizing is a big factor
    in layout performance. It would be useful to show the time used for
    text auto sizing during layout.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/18401003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153440 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
+++ third_party/WebKit/Source/core/rendering/TextAutosizer.cpp
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/html/HTMLElement.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/page/Settings.h"
+#include "core/platform/chromium/TraceEvent.h"
 #include "core/platform/graphics/IntSize.h"
 #include "core/rendering/RenderListItem.h"
 #include "core/rendering/RenderObject.h"
@@ -124,6 +125,8 @@ void TextAutosizer::recalculateMultipliers()
 
 bool TextAutosizer::processSubtree(RenderObject* layoutRoot)
 {
+    TRACE_EVENT0("webkit", "TextAutosizer::processSubtree");
+
     // FIXME: Text Autosizing should only be enabled when m_document->page()->mainFrame()->view()->useFixedLayout()
     // is true, but for now it's useful to ignore this so that it can be tested on desktop.
     if (!m_document->settings() || !m_document->settings()->textAutosizingEnabled() || layoutRoot->view()->printing() || !m_document->page())
