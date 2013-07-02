9efad3f57fdede1b0b0ef34666d442f3981ca291
simon.hong81@gmail.com <simon.hong81@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 22:35:20 +0000

    Alt-tab should provide visible feedback if there is only one window.
    When user presses alt-tab, active window shows bouncing effect
    when there is only one window.
    
    R=jamescook@chromium.org
    BUG=154150
    TEST=Compiles, Visual test
    
    Review URL: https://chromiumcodereview.appspot.com/18255004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209800 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_cycle_list.cc
+++ ash/wm/window_cycle_list.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/wm/window_util.h"
 #include "ui/aura/window.h"
+#include "ui/views/corewm/window_animations.h"
 
 namespace ash {
 
@@ -42,8 +43,12 @@ void WindowCycleList::Step(Direction direction) {
     // provided window list.  Just switch to the first (or last) one.
     current_index_ = (direction == FORWARD ? 0 : windows_.size() - 1);
   } else {
-    if (windows_.size() == 1)
+    // When there is only one window, we should give a feedback to user.
+    if (windows_.size() == 1) {
+      AnimateWindow(windows_[0],
+                    views::corewm::WINDOW_ANIMATION_TYPE_BOUNCE);
       return;
+    }
     // We're in a valid cycle, so step forward or backward.
     current_index_ += (direction == FORWARD ? 1 : -1);
   }
