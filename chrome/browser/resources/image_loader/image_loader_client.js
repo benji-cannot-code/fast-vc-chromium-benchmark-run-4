f4c6b9ed20ad608756969bc75de41563d11ad083
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 12:51:23 +0000

    Check for cached images within a queue, instead of before.
    Before, the Image Loader extension was checking for availability in cache outside of the priority queue, assuming that it is fast, and if the images are in cache, then they can be very quickly served. However, checking for the images involves IO operations, which caused the image with higher priority to be loaded with a significant delay. For example, 15 consecutive requests for images, caused around even 5 seconds of delay.
    
    This patch resolves this issue by checking for cache availability when the request is picked from the priority queue. This causes that images with lower priority will not affect loading times of images with higher priority.
    
    Along the way, the strict mode has been enabled.
    
    TEST=Images should load faster. For correctness, browser_tests.
    BUG=239237
    
    Review URL: https://chromiumcodereview.appspot.com/18468003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209688 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/image_loader/image_loader_client.js
+++ chrome/browser/resources/image_loader/image_loader_client.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+'use strict';
+
 /**
  * Client used to connect to the remote ImageLoader extension. Client class runs
  * in the extension, where the client.js is included (eg. Files.app).
--- chrome/browser/resources/image_loader/request.js
+++ chrome/browser/resources/image_loader/request.js
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+'use strict';
+
 /**
  * Creates and starts downloading and then resizing of the image. Finally,
  * returns the image using the callback.
@@ -232,6 +234,7 @@ Request.prototype.downloadOriginal_ = function(onSuccess, onFailure) {
  * @private
  */
 Request.prototype.sendImage_ = function(imageChanged) {
+  var imageData;
   if (!imageChanged) {
     // The image hasn't been processed, so the raw data can be directly
     // forwarded for speed (no need to encode the image again).
