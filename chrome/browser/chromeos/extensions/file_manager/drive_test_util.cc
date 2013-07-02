deb9d42795451f162d8efecee533a54f70673160
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 11:07:46 +0000

    file_manager: Stop defining drive mount point name constant in drive_test_util.cc
    Use the same constant as used in DriveIntegrationService::AddDriveMountPoint
    
    BUG=None
    TEST=browser_tests --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*"
    R=hidehiko@chromium.org
    
    Review URL: https://codereview.chromium.org/18486002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209670 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/file_manager/drive_test_util.cc
+++ chrome/browser/chromeos/extensions/file_manager/drive_test_util.cc
@@ -3,11 +3,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/bind.h"
+#include "chrome/browser/chromeos/extensions/file_manager/drive_test_util.h"
+
 #include "base/files/file_path.h"
 #include "base/run_loop.h"
 #include "chrome/browser/chromeos/drive/drive_integration_service.h"
-#include "chrome/browser/chromeos/drive/file_system.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/browser_context.h"
 #include "webkit/browser/fileapi/external_mount_points.h"
@@ -15,8 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace drive_test_util {
 
 namespace {
-
-const char kDriveMountPointName[] = "drive";
 
 // Helper class used to wait for |OnFileSystemMounted| event from a drive file
 // system.
@@ -66,16 +64,18 @@ void WaitUntilDriveMountPointIsAdded(Profile* profile) {
           profile);
   DCHECK(integration_service);
 
-  DriveMountPointWaiter mount_point_waiter(integration_service);
-
+  const std::string drive_mount_point_name =
+      drive::util::GetDriveMountPointPath().BaseName().AsUTF8Unsafe();
   base::FilePath ignored;
   // GetRegisteredPath succeeds iff the mount point exists.
-  if (!content::BrowserContext::GetMountPoints(profile)->
-      GetRegisteredPath(kDriveMountPointName, &ignored)) {
-    LOG(WARNING) << "Waiting for drive mount point to get mounted.";
-    mount_point_waiter.Wait();
-    LOG(WARNING) << "Drive mount point found.";
-  }
+  if (content::BrowserContext::GetMountPoints(profile)->
+      GetRegisteredPath(drive_mount_point_name, &ignored))
+    return;
+
+  DriveMountPointWaiter mount_point_waiter(integration_service);
+  LOG(INFO) << "Waiting for drive mount point to get mounted.";
+  mount_point_waiter.Wait();
+  LOG(INFO) << "Drive mount point found.";
 }
 
 }  // namespace drive_test_util
