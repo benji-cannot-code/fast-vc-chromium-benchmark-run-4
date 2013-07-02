f43f48b529f3e793c0fe3394c805a2e9c5502bc0
skyostil@chromium.org <skyostil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 11:55:50 +0000

    gpu: Propagate lost context signal to all contexts
    When any individual command buffer context encounters a context lost
    event, propagate the context lost status to all other contexts if
    gfx::GLContext::LosesAllContextsOnContextLost() is true. This allows
    all command buffer clients to get notified of GPU resets. This is
    useful with virtual contexts because the reset is generally picked
    up by a random virtual context.
    
    Also, if virtual contexts are being used, all contexts should avoid
    doing GL object cleanup during teardown because the real context has
    already been lost at that point.
    
    BUG=232449
    TEST=https://www.khronos.org/registry/webgl/conformance-suites/1.0.1/extra/lots-of-polys-example.html
    
    Review URL: https://chromiumcodereview.appspot.com/17550012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209673 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/gpu_command_buffer_stub.h
+++ content/common/gpu/gpu_command_buffer_stub.h
@@ -138,6 +138,8 @@ class GpuCommandBufferStub
 
   void SetLatencyInfoCallback(const LatencyInfoCallback& callback);
 
+  void MarkContextLost();
+
  private:
   GpuMemoryManager* GetMemoryManager();
   bool MakeCurrent();
@@ -197,6 +199,8 @@ class GpuCommandBufferStub
   bool HasMoreWork();
 
   void ScheduleDelayedWork(int64 delay);
+
+  bool CheckContextLost();
 
   // The lifetime of objects of this class is managed by a GpuChannel. The
   // GpuChannels destroy all the GpuCommandBufferStubs that they own when they
