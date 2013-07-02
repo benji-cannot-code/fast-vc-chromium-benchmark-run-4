7076fe9645363d9c57c29b259be6d1d661d72ede
dcheng@chromium.org <dcheng@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 00:11:54 +0000

    Remove PushMessagingApiTest's dependency in TiclInvalidationService.
    This doesn't actually do very much. The dependency appears to have gone
    away on its own.
    
    BUG=243482
    
    Review URL: https://chromiumcodereview.appspot.com/18390002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209551 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/invalidation/ticl_invalidation_service.h
+++ chrome/browser/invalidation/ticl_invalidation_service.h
@@ -27,11 +27,6 @@ namespace syncer {
 class Invalidator;
 }
 
-// TODO: Remove this dependency.  See crbug.com/243482.
-namespace extensions {
-class PushMessagingApiTest;
-}
-
 namespace invalidation {
 
 // This InvalidationService wraps the C++ Invalidation Client (TICL) library.
@@ -96,7 +91,6 @@ class TiclInvalidationService
   void InitForTest(syncer::Invalidator* invalidator);
 
   friend class TiclInvalidationServiceTestDelegate;
-  friend class extensions::PushMessagingApiTest;
 
  private:
   bool IsReadyToStart();
