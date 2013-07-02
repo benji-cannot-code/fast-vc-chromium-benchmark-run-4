c19bf6d43aef00967dd316ab7841b2212685a2be
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 20:20:10 +0000

    Remove the import PYAUTO tests.
    They have now all been replaced by importer browser_tests (http://crrev.com/196518) and ProfileWriter unit tests (http://crrev.com/204421).
    
    This is a prerequisite for https://codereview.chromium.org/18052010/
    
    BUG=243423, 254672, 114244, 89768, 63001
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/18556002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209764 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/automation/testing_automation_provider.h
+++ chrome/browser/automation/testing_automation_provider.h
@@ -18,7 +18,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/automation/automation_provider.h"
 #include "chrome/browser/automation/automation_provider_json.h"
 #include "chrome/browser/history/history_service.h"
-#include "chrome/browser/importer/importer_list_observer.h"
 #include "chrome/browser/ui/browser_list_observer.h"
 #include "content/public/browser/notification_registrar.h"
 #include "content/public/common/page_type.h"
@@ -32,7 +31,6 @@ class PowerManagerClientObserverForTesting;
 #endif  // defined(OS_CHROMEOS)
 
 class CreditCard;
-class ImporterList;
 
 namespace base {
 class DictionaryValue;
@@ -54,7 +52,6 @@ struct WebPluginInfo;
 // This is an automation provider containing testing calls.
 class TestingAutomationProvider : public AutomationProvider,
                                   public chrome::BrowserListObserver,
-                                  public importer::ImporterListObserver,
                                   public content::NotificationObserver {
  public:
   explicit TestingAutomationProvider(Profile* profile);
@@ -66,22 +63,11 @@ class TestingAutomationProvider : public AutomationProvider,
   virtual void OnChannelError() OVERRIDE;
 
  private:
-  // Storage for ImportSettings() to resume operations after a callback.
-  struct ImportSettingsData {
-    string16 browser_name;
-    int import_items;
-    Browser* browser;
-    IPC::Message* reply_message;
-  };
-
   virtual ~TestingAutomationProvider();
 
   // chrome::BrowserListObserver:
   virtual void OnBrowserAdded(Browser* browser) OVERRIDE;
   virtual void OnBrowserRemoved(Browser* browser) OVERRIDE;
-
-  // importer::ImporterListObserver:
-  virtual void OnSourceProfilesLoaded() OVERRIDE;
 
   // content::NotificationObserver:
   virtual void Observe(int type,
@@ -456,12 +442,6 @@ class TestingAutomationProvider : public AutomationProvider,
   void SaveTabContents(Browser* browser,
                        base::DictionaryValue* args,
                        IPC::Message* reply_message);
-
-  // Import the given settings from the given browser.
-  // Uses the JSON interface for input/output.
-  void ImportSettings(Browser* browser,
-                      base::DictionaryValue* args,
-                      IPC::Message* reply_message);
 
   // Add a new entry to the password store based on the password information
   // provided. This method can also be used to add a blacklisted site (which
@@ -1453,12 +1433,6 @@ class TestingAutomationProvider : public AutomationProvider,
   std::map<std::string, BrowserJsonHandler> browser_handler_map_;
 
   content::NotificationRegistrar registrar_;
-
-  // Used to enumerate browser profiles.
-  scoped_refptr<ImporterList> importer_list_;
-
-  // The stored data for the ImportSettings operation.
-  ImportSettingsData import_settings_data_;
 
   // The automation event observer queue. It is lazily created when an observer
   // is added to avoid overhead when not needed.
