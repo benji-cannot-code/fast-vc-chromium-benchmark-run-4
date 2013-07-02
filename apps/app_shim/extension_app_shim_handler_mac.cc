23040f28d992e6e27d7717e76be9abe5987e94e2
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 06:49:48 +0000

    Maybe terminate Chrome on last app quit.
    At the moment, when an app is started with a shim, Chrome is started.
    When the app quits, Chrome continues to run.
    With this CL, Chrome will be terminated if quitting an app leaves no
    other apps running, and no browser windows were ever open. 
    
    BUG=168080
    TEST=Start Chrome with an app shim.
    Quit the shim, Chrome should terminate.
    TEST=Start Chrome with an app shim.
    Click on Chrome to start a browser window.
    Quit the browser window, then quit the shim.
    Chrome should stay running.
    
    Review URL: https://chromiumcodereview.appspot.com/17764007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209626 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/app_shim/extension_app_shim_handler_mac.cc
+++ apps/app_shim/extension_app_shim_handler_mac.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_service.h"
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/extensions/shell_window_registry.h"
+#include "chrome/browser/lifetime/application_lifetime.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/profiles/profile_manager.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
@@ -81,8 +82,16 @@ void ExtensionAppShimHandler::Delegate::LaunchShim(
       web_app::ShortcutInfoForExtensionAndProfile(extension, profile));
 }
 
+void ExtensionAppShimHandler::Delegate::MaybeTerminate() {
+  bool shell_windows_left =
+      extensions::ShellWindowRegistry::IsShellWindowRegisteredInAnyProfile(0);
+  if (!shell_windows_left)
+    chrome::AttemptExit();
+}
+
 ExtensionAppShimHandler::ExtensionAppShimHandler()
-    : delegate_(new Delegate) {
+    : delegate_(new Delegate),
+      browser_opened_ever_(false) {
   // This is instantiated in BrowserProcessImpl::PreMainMessageLoopRun with
   // AppShimHostManager. Since PROFILE_CREATED is not fired until
   // ProfileManager::GetLastUsedProfile/GetLastOpenedProfiles, this should catch
@@ -92,6 +101,8 @@ ExtensionAppShimHandler::ExtensionAppShimHandler()
   registrar_.Add(this, chrome::NOTIFICATION_PROFILE_DESTROYED,
                  content::NotificationService::AllBrowserContextsAndSources());
   registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_UNINSTALLED,
+                 content::NotificationService::AllBrowserContextsAndSources());
+  registrar_.Add(this, chrome::NOTIFICATION_BROWSER_OPENED,
                  content::NotificationService::AllBrowserContextsAndSources());
 }
 
@@ -208,6 +219,9 @@ void ExtensionAppShimHandler::OnShimQuit(Host* host) {
 
   DCHECK_NE(0u, hosts_.count(make_pair(profile, host->GetAppId())));
   hosts_.find(make_pair(profile, host->GetAppId()))->second->OnAppClosed();
+
+  if (!browser_opened_ever_ && hosts_.empty())
+    delegate_->MaybeTerminate();
 }
 
 void ExtensionAppShimHandler::set_delegate(Delegate* delegate) {
@@ -218,6 +232,14 @@ void ExtensionAppShimHandler::Observe(
     int type,
     const content::NotificationSource& source,
     const content::NotificationDetails& details) {
+  if (type == chrome::NOTIFICATION_BROWSER_OPENED) {
+    registrar_.Remove(
+        this, chrome::NOTIFICATION_BROWSER_OPENED,
+        content::NotificationService::AllBrowserContextsAndSources());
+    browser_opened_ever_ = true;
+    return;
+  }
+
   Profile* profile = content::Source<Profile>(source).ptr();
   if (profile->IsOffTheRecord())
     return;
