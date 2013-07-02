855e04c9df2c9048c9055d7cd4e87ae1b1411d35
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 22:58:46 +0000

    Revert 209710 "Reland "Shouldn't access LayerTreeTest::layer_tre..."
    > Reland "Shouldn't access LayerTreeTest::layer_tree_host on impl thread"
    > 
    > As Dana mentioned on the bug, the troublesome test doesn't make sense with impl painting. I've disabled it as suggested.
    > 
    > BUG=235188
    > 
    > Review URL: https://chromiumcodereview.appspot.com/18454002
    
    TBR=vollick@chromium.org
    
    Review URL: https://codereview.chromium.org/18543011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209805 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_unittest_animation.cc
+++ cc/trees/layer_tree_host_unittest_animation.cc
@@ -292,6 +292,7 @@ class LayerTreeHostAnimationTestTickAnimationWhileBackgrounded
 SINGLE_AND_MULTI_THREAD_TEST_F(
     LayerTreeHostAnimationTestTickAnimationWhileBackgrounded);
 
+// Ensures that animations continue to be ticked when we are backgrounded.
 class LayerTreeHostAnimationTestAddAnimationWithTimingFunction
     : public LayerTreeHostAnimationTest {
  public:
@@ -301,7 +302,6 @@ class LayerTreeHostAnimationTestAddAnimationWithTimingFunction
     LayerTreeHostAnimationTest::SetupTree();
     content_ = FakeContentLayer::Create(&client_);
     content_->SetBounds(gfx::Size(4, 4));
-    content_->set_layer_animation_delegate(this);
     layer_tree_host()->root_layer()->AddChild(content_);
   }
 
@@ -309,7 +309,9 @@ class LayerTreeHostAnimationTestAddAnimationWithTimingFunction
     PostAddAnimationToMainThread(content_.get());
   }
 
-  virtual void NotifyAnimationStarted(double wallClockTime) OVERRIDE {
+  virtual void AnimateLayers(
+      LayerTreeHostImpl* host_impl,
+      base::TimeTicks monotonic_time) OVERRIDE {
     LayerAnimationController* controller =
         layer_tree_host()->root_layer()->children()[0]->
         layer_animation_controller();
@@ -330,8 +332,14 @@ class LayerTreeHostAnimationTestAddAnimationWithTimingFunction
     // because of the default ease timing function.
     EXPECT_FLOAT_EQ(linearly_interpolated_opacity, curve->GetValue(time));
 
-    controller->RemoveAnimation(animation->id());
+    LayerAnimationController* controller_impl =
+        host_impl->active_tree()->root_layer()->children()[0]->
+        layer_animation_controller();
+    Animation* animation_impl =
+        controller_impl->GetAnimation(Animation::Opacity);
 
+    controller->RemoveAnimation(animation->id());
+    controller_impl->RemoveAnimation(animation_impl->id());
     EndTest();
   }
 
