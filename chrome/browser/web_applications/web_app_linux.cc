5e452131ab9f1361cf5b6af15a2ce706ea43a569
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 10:30:31 +0000

    Windows: Deleting a profile deletes all app shortcuts associated with it.
    This will search all locations where app shortcuts reside and delete any that
    open Chrome with the profile being deleted.
    
    Mac and Linux have empty stubs, to be completed in a follow-up CL.
    
    BUG=236353
    
    Review URL: https://chromiumcodereview.appspot.com/16139004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209658 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_linux.cc
+++ chrome/browser/web_applications/web_app_linux.cc
@@ -54,6 +54,10 @@ void UpdatePlatformShortcuts(
   CreatePlatformShortcuts(web_app_path, shortcut_info, creation_locations);
 }
 
+void DeleteAllShortcutsForProfile(const base::FilePath& profile_path) {
+  // TODO(mgiuca): Implement this on Linux.
+}
+
 }  // namespace internals
 
 }  // namespace web_app
