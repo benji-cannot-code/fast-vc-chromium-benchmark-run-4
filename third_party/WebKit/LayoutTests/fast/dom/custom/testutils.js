a1df977dfb6b4fb6e578b934be5a37b152038e03
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-02 02:40:04 +0000

    Harden Custom Element registration processing against context destruction.
    This adds tests for destroying the containing iframe when:
    
    - Retrieving the 'prototype' option during registration.
    - Retrieving the 'ready' callback during registration.
    - Upgrading an element as a result of registration.
    
    It also adds a test for destroying the containing iframe, then trying
    to register a new custom element.
    
    BUG=234509
    R=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/18335002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153395 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/dom/custom/testutils.js
@@ -0,0 +1,43 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+(function () {
+
+function isParentFrame() {
+    return window.parent === window;
+}
+
+var childDoneMessage = 'PASS child done';
+
+fork = function () {
+    if (isParentFrame()) {
+        window.addEventListener('message', function (event) {
+            debug(event.data);
+            if (event.data == childDoneMessage)
+                finishJSTest();
+        });
+
+        var iframe = document.createElement('iframe');
+        iframe.src = window.location;
+        document.body.appendChild(iframe);
+        iframe = null;
+    }
+
+    return isParentFrame();
+};
+
+if (!isParentFrame()) {
+    var parent = window.parent;
+    log = function (msg) {
+        parent.postMessage(msg, '*');
+    };
+
+    done = function () {
+        log(childDoneMessage);
+    };
+
+    destroyContext = function () {
+        parent.document.querySelector('iframe').remove();
+        log('PASS destroyed context');
+    };
+}
+
+})();
