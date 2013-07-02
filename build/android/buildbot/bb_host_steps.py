f135d5d8d7d38db32dcab5172e96e84f290651d4
sivachandra@chromium.org <sivachandra@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 14:02:15 +0000

    [Android] Refactor the buildbot scripts to prepare for downstream usage.
    BUG=249997
    
    Review URL: https://chromiumcodereview.appspot.com/17114007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209694 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_host_steps.py
+++ build/android/buildbot/bb_host_steps.py
@@ -105,7 +105,7 @@ def BisectPerfRegression(_):
           '-p', bb_utils.GOMA_DIR])
 
 
-def GetHostSteps():
+def GetHostStepCmds():
   return [
       ('compile', Compile),
       ('extract_build', ExtractBuild),
@@ -116,7 +116,7 @@ def GetHostSteps():
   ]
 
 
-def main(argv):
+def GetHostStepsOptParser():
   parser = bb_utils.GetParser()
   parser.add_option('--steps', help='Comma separated list of host tests.')
   parser.add_option('--build-targets', default='All',
@@ -124,13 +124,19 @@ def main(argv):
   parser.add_option('--experimental', action='store_true',
                     help='Indicate whether to compile experimental targets.')
 
+  return parser
+
+
+def main(argv):
+  parser = GetHostStepsOptParser()
   options, args = parser.parse_args(argv[1:])
   if args:
     return sys.exit('Unused args %s' % args)
 
   setattr(options, 'target', options.factory_properties.get('target', 'Debug'))
 
-  bb_utils.RunSteps(GetHostSteps(), options)
+  if options.steps:
+    bb_utils.RunSteps(options.steps.split(','), GetHostStepCmds(), options)
 
 
 if __name__ == '__main__':
