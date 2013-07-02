a55ba79ac34a6ea013b661811169b90bb34b6c58
charliemooney@chromium.org <charliemooney@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-02 02:20:36 +0000

    This should add a Salsa App chrome:// page for ChromeOS
    Salsa is a rapid user testing system developed for use by the ChromeOS
    touch team to quickly run blind studies on various settings.  There are
    a lot of subjective decisions to be made about tuning various
    parameters, so being able to quickly take a blind poll to see which
    people prefer can be very valuable.
    
    Salsa already exists for testing the "X Input" parameters.  These are
    the settings you can change with the xinput command at a shell on your
    Chromebook.  However, previously you could only do those.  This change
    adds a chrome://salsa page to Chrome for ChromeOS only where you can
    do a similar experiment but changing the Chrome gesture configurations
    and preferences (those that you can change manually at chrome://gesture)
    as your experiment instead
    
    The way it works:
        1) An experiment is made, which generates a URL for the experiment
        that can be emailed out to the participants.
    
        2) Participants will click on the link.  The page shows them a form
        for ranking their experiences, and an encoded string to use when
        running the test.  In this case it will be something like:
    
        chrome://salsa/#A_HEX_ENCODED_STRING_FOR_THE_EXPERIMENT
    
        3) The new salsa page parses this string into "treatments" and
        applies them one at a time while the user can try them out and see
        which they prefer before reporting their results.
    
    BUG=chromium:249762
    TEST=manually built Chrome and tested with an experiment string I made.
    
    Review URL: https://chromiumcodereview.appspot.com/17094006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209588 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -101,6 +101,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/chromeos/login/oobe_ui.h"
 #include "chrome/browser/ui/webui/chromeos/mobile_setup_ui.h"
 #include "chrome/browser/ui/webui/chromeos/proxy_settings_ui.h"
+#include "chrome/browser/ui/webui/chromeos/salsa_ui.h"
 #include "chrome/browser/ui/webui/chromeos/sim_unlock_ui.h"
 #include "chrome/browser/ui/webui/chromeos/system_info_ui.h"
 #endif
@@ -350,6 +351,8 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<chromeos::OobeUI>;
   if (url.host() == chrome::kChromeUIProxySettingsHost)
     return &NewWebUI<chromeos::ProxySettingsUI>;
+  if (url.host() == chrome::kChromeUISalsaHost)
+    return &NewWebUI<SalsaUI>;
   if (url.host() == chrome::kChromeUISimUnlockHost)
     return &NewWebUI<chromeos::SimUnlockUI>;
   if (url.host() == chrome::kChromeUISystemInfoHost)
