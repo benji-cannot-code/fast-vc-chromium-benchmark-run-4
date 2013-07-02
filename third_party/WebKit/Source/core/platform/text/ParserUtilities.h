69c247e26464cd3ff1e84a0967e67608ac4a14a1
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-02 01:56:54 +0000

    SVGViewSpec shouldn't use bloatedCharacters()
    Fixing SVGViewSpec required fixing a bunch of code that it depends on to
    understand narrow strings.
    
    R=pdr,eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/18418002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153394 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/text/ParserUtilities.h
+++ third_party/WebKit/Source/core/platform/text/ParserUtilities.h
@@ -27,28 +27,42 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-    inline bool skipString(const UChar*& ptr, const UChar* end, const UChar* name, int length)
-    {
-        if (end - ptr < length)
-            return false;
-        if (memcmp(name, ptr, sizeof(UChar) * length))
-            return false;
-        ptr += length;
-        return true;
-    }
+template<typename CharType>
+inline bool skipString(const CharType*& ptr, const CharType* end, const CharType* name, int length)
+{
+    if (end - ptr < length)
+        return false;
+    if (memcmp(name, ptr, sizeof(CharType) * length))
+        return false;
+    ptr += length;
+    return true;
+}
 
-    inline bool skipString(const UChar*& ptr, const UChar* end, const char* str)
-    {
-        int length = strlen(str);
-        if (end - ptr < length)
+inline bool skipString(const UChar*& ptr, const UChar* end, const LChar* name, int length)
+{
+    if (end - ptr < length)
+        return false;
+    for (int i = 0; i < length; ++i) {
+        if (ptr[i] != name[i])
             return false;
-        for (int i = 0; i < length; ++i) {
-            if (ptr[i] != str[i])
-                return false;
-        }
-        ptr += length;
-        return true;
     }
+    ptr += length;
+    return true;
+}
+
+template<typename CharType>
+inline bool skipString(const CharType*& ptr, const CharType* end, const char* str)
+{
+    int length = strlen(str);
+    if (end - ptr < length)
+        return false;
+    for (int i = 0; i < length; ++i) {
+        if (ptr[i] != str[i])
+            return false;
+    }
+    ptr += length;
+    return true;
+}
 
 } // namspace WebCore
 
