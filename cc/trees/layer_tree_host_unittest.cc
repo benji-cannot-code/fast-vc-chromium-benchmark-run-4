2f042e58cc5caf748798a98145bc483f75d47cea
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 17:09:47 +0000

    Re-land: cc: Add raster finished signals to RasterWorkerPool.
    This adds to important signals to the RasterWorkerPoolClient
    interface. Each signal is generated as a result of calling
    ScheduleTasks() and the signals always reflect the state of the
    last call to ScheduleTasks().
        
    BUG=253508,254135,252787
    TEST=cc_unittests --gtest_filter=TileManagerTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/17351017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209995 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_unittest.cc
+++ cc/trees/layer_tree_host_unittest.cc
@@ -751,6 +751,14 @@ class LayerTreeHostTestFrameTimeUpdatesAfterActivationFails
   }
 
   virtual bool CanActivatePendingTree(LayerTreeHostImpl* impl) OVERRIDE {
+    if (frame_ >= 1)
+      return true;
+
+    return false;
+  }
+
+  virtual bool CanActivatePendingTreeIfNeeded(LayerTreeHostImpl* impl)
+      OVERRIDE {
     frame_++;
     if (frame_ == 1) {
       first_frame_time_ = impl->CurrentFrameTimeTicks();
@@ -2899,6 +2907,7 @@ class LayerTreeHostTestDeferredInitialize : public LayerTreeHostTest {
 
   virtual void BeginTest() OVERRIDE {
     initialized_gl_ = false;
+    num_draws_ = 0;
     PostSetNeedsCommitToMainThread();
   }
 
@@ -2923,8 +2932,11 @@ class LayerTreeHostTestDeferredInitialize : public LayerTreeHostTest {
           base::Unretained(this),
           base::Unretained(host_impl)));
     } else {
-      EXPECT_EQ(2u, layer_impl->append_quads_count());
-      EndTest();
+      if (!num_draws_) {
+        EXPECT_EQ(2u, layer_impl->append_quads_count());
+        EndTest();
+      }
+      num_draws_++;
     }
   }
 
@@ -2950,6 +2962,7 @@ class LayerTreeHostTestDeferredInitialize : public LayerTreeHostTest {
   FakeContentLayerClient client_;
   scoped_refptr<FakePictureLayer> layer_;
   bool initialized_gl_;
+  int num_draws_;
 };
 
 MULTI_THREAD_TEST_F(LayerTreeHostTestDeferredInitialize);
