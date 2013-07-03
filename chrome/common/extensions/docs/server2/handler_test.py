9422427f92a9898faf79a291b3a0837a15ed1666
jaredshumway94@gmail.com <jaredshumway94@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 20:16:16 +0000

    Doc server redirection through configuration
    The doc server can now be configured using a configuration file to redirect paths to different URLs.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/16959019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210034 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/handler_test.py
+++ chrome/common/extensions/docs/server2/handler_test.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 import unittest
 
+from cron_servlet import CronServlet
 from fake_fetchers import ConfigureFakeFetchers
 from handler import Handler
 from servlet import Request
@@ -14,24 +15,10 @@ class HandlerTest(unittest.TestCase):
   def setUp(self):
     ConfigureFakeFetchers()
 
-  def testCodeGoogleRedirect(self):
-    response = Handler(Request('chrome/extensions/storage.html',
-                               'http://code.google.com',
-                               {})).Get()
-    self.assertEqual(302, response.status)
-    self.assertEqual('http://developer.chrome.com/extensions/storage.html',
-                     response.headers.get('Location'))
+  def testInvalid(self):
+    handler = Handler(Request.ForTest('_notreal'))
 
-  def testDeveloperGoogleRedirect(self):
-    response = Handler(Request.ForTest('')).Get()
-    self.assertEqual(302, response.status)
-    self.assertEqual('http://developer.google.com/chrome',
-                     response.headers.get('Location'))
-
-  def testAppRedirect(self):
-    response = Handler(Request.ForTest('apps.html')).Get()
-    self.assertEqual(302, response.status)
-    self.assertEqual('/apps/about_apps.html', response.headers.get('Location'))
+    self.assertEqual(404, handler.Get().status)
 
 if __name__ == '__main__':
   unittest.main()
