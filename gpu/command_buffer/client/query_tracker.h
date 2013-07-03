dbed58595bd211e7c21d083eb6e81c192a076ff3
reveman@chromium.org <reveman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 14:24:41 +0000

    gpu: Fix removal of pending queries.
    This moves all queries to a "removed queries" list at removal
    time. The memory used by the queries is not freed until the
    query has completed.
    
    BUG=253488
    TEST=gpu_unittests --gtest_filter=QueryTrackerTest.Remove
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/18340003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209977 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/client/query_tracker.h
+++ gpu/command_buffer/client/query_tracker.h
@@ -9,6 +9,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <GLES2/gl2.h>
 
 #include <deque>
+#include <list>
+
 #include "gles2_impl_export.h"
 #include "gpu/command_buffer/client/hash_tables.h"
 #include "gpu/command_buffer/common/gles2_cmd_format.h"
@@ -155,13 +157,16 @@ class GLES2_IMPL_EXPORT QueryTracker {
 
   Query* CreateQuery(GLuint id, GLenum target);
   Query* GetQuery(GLuint id);
-  void RemoveQuery(GLuint id, bool context_lost);
+  void RemoveQuery(GLuint id);
   void Shrink();
+  void FreeCompletedQueries();
 
  private:
   typedef gpu::hash_map<GLuint, Query*> QueryMap;
+  typedef std::list<Query*> QueryList;
 
   QueryMap queries_;
+  QueryList removed_queries_;
   QuerySyncManager query_sync_manager_;
 
   DISALLOW_COPY_AND_ASSIGN(QueryTracker);
