ba7be79c66bcaf786e8ce5cd55870684872f40d5
vitalybuka@chromium.org <vitalybuka@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 22:59:25 +0000

    Fixed focus when page range was mouse clicked.
    BUG=256121
    
    Review URL: https://chromiumcodereview.appspot.com/18048003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210059 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/print_preview/settings/page_settings.js
+++ chrome/browser/resources/print_preview/settings/page_settings.js
@@ -98,6 +98,8 @@ cr.define('print_preview', function() {
       this.tracker.add(
           this.customInput_, 'blur', this.onCustomInputBlur_.bind(this));
       this.tracker.add(
+          this.customInput_, 'focus', this.onCustomInputFocus_.bind(this));
+      this.tracker.add(
           this.customInput_, 'keyup', this.onCustomInputKeyUp_.bind(this));
       this.tracker.add(
           this.pageRangeTicketItem_,
@@ -159,7 +161,6 @@ cr.define('print_preview', function() {
      */
     onCustomRadioClick_: function() {
       this.customInput_.focus();
-      this.pageRangeTicketItem_.updateValue(this.customInput_.value);
     },
 
     /**
@@ -170,8 +171,16 @@ cr.define('print_preview', function() {
     onCustomInputBlur_: function() {
       if (this.customInput_.value == '') {
         this.allRadio_.checked = true;
-        this.customRadio_.checked = false;
       }
+    },
+
+    /**
+     * Called when the custom input is focused.
+     * @private
+     */
+    onCustomInputFocus_: function() {
+      this.customRadio_.checked = true;
+      this.pageRangeTicketItem_.updateValue(this.customInput_.value);
     },
 
     /**
@@ -186,7 +195,6 @@ cr.define('print_preview', function() {
       if (event.keyIdentifier == 'Enter') {
         this.pageRangeTicketItem_.updateValue(this.customInput_.value);
       } else {
-        this.allRadio_.checked = false;
         this.customRadio_.checked = true;
         this.customInputTimeout_ = setTimeout(
             this.onCustomInputTimeout_.bind(this),
@@ -218,11 +226,9 @@ cr.define('print_preview', function() {
             this.customInput_.value = pageRangeStr;
           }
           this.customRadio_.checked = true;
-          this.allRadio_.checked = false;
           this.setInvalidStateVisible_(!this.pageRangeTicketItem_.isValid());
         } else {
           this.allRadio_.checked = true;
-          this.customRadio_.checked = false;
           this.setInvalidStateVisible_(false);
         }
         fadeInOption(this.getElement());
