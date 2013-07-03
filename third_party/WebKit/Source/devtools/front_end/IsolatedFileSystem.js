fbe6d8031162a1f6c4a5120e980c8a7146c52344
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-03 10:25:42 +0000

    DevTools: [Sass] Automatically detect changes to compiled CSS (workspace and network)
    Use the FileEntry Metadata in the workspace case and the HTTP 304 status code in the network case
    to detect if the CSS has been modified by the Sass compiler after saving a Sass source.
    
    R=pfeldman, vsevik
    BUG=244430
    
    Review URL: https://chromiumcodereview.appspot.com/18052003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153491 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/IsolatedFileSystem.js
+++ third_party/WebKit/Source/devtools/front_end/IsolatedFileSystem.js
@@ -189,7 +189,7 @@ WebInspector.IsolatedFileSystem.prototype = {
          */
         function fileSystemLoaded(domFileSystem)
         {
-            domFileSystem.root.getFile(path, null, fileEntryLoaded, errorHandler);
+            domFileSystem.root.getFile(path, null, fileEntryLoaded, errorHandler.bind(this));
         }
 
         /**
@@ -197,7 +197,7 @@ WebInspector.IsolatedFileSystem.prototype = {
          */
         function fileEntryLoaded(entry)
         {
-            entry.file(fileLoaded, errorHandler);
+            entry.file(fileLoaded, errorHandler.bind(this));
         }
 
         /**
--- third_party/WebKit/Source/devtools/front_end/UISourceCode.js
+++ third_party/WebKit/Source/devtools/front_end/UISourceCode.js
@@ -271,7 +271,10 @@ WebInspector.UISourceCode.prototype = {
             this._project.requestFileContent(this, this._fireContentAvailable.bind(this));
     },
 
-    checkContentUpdated: function()
+    /**
+     * @param {function()=} callback
+     */
+    checkContentUpdated: function(callback)
     {
         if (!this._project.canSetFileContent())
             return;
@@ -287,21 +290,29 @@ WebInspector.UISourceCode.prototype = {
                 this._commitContent("", false);
                 this.setWorkingCopy(workingCopy);
                 delete this._checkingContent;
+                if (callback)
+                    callback();
                 return;
             }
             if (typeof this._lastAcceptedContent === "string" && this._lastAcceptedContent === updatedContent) {
                 delete this._checkingContent;
+                if (callback)
+                    callback();
                 return;
             }
             if (this._content === updatedContent) {
                 delete this._lastAcceptedContent;
                 delete this._checkingContent;
+                if (callback)
+                    callback();
                 return;
             }
 
             if (!this.isDirty()) {
                 this._commitContent(updatedContent, false);
                 delete this._checkingContent;
+                if (callback)
+                    callback();
                 return;
             }
 
@@ -311,6 +322,8 @@ WebInspector.UISourceCode.prototype = {
             else
                 this._lastAcceptedContent = updatedContent;
             delete this._checkingContent;
+            if (callback)
+                callback();
         }
     },
 
