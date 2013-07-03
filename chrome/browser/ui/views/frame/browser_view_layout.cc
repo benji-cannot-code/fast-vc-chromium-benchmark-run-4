b1562dac4c251e213313067ab9d1aa9b0325b506
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 18:01:39 +0000

    Make the bookmark bar not overlap the tab strip when detached in immersive fullscreen
    BUG=226674
    TEST=Manual, see bug
    
    Review URL: https://chromiumcodereview.appspot.com/18331010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210006 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_view_layout.cc
+++ chrome/browser/ui/views/frame/browser_view_layout.cc
@@ -183,9 +183,7 @@ gfx::Size BrowserViewLayout::GetMinimumSize() {
       bookmark_bar_->visible() &&
       browser()->SupportsWindowFeature(Browser::FEATURE_BOOKMARKBAR)) {
     bookmark_bar_size = bookmark_bar_->GetMinimumSize();
-    bookmark_bar_size.Enlarge(0,
-        -(views::NonClientFrameView::kClientEdgeThickness +
-            bookmark_bar_->GetToolbarOverlap(true)));
+    bookmark_bar_size.Enlarge(0, -bookmark_bar_->GetToolbarOverlap());
   }
   // TODO: Adjust the minimum height for the find bar.
 
@@ -486,8 +484,7 @@ int BrowserViewLayout::LayoutBookmarkBar(int top) {
 
   bookmark_bar_->set_infobar_visible(InfobarVisible());
   int bookmark_bar_height = bookmark_bar_->GetPreferredSize().height();
-  y -= views::NonClientFrameView::kClientEdgeThickness +
-      bookmark_bar_->GetToolbarOverlap(false);
+  y -= bookmark_bar_->GetToolbarOverlap();
   bookmark_bar_->SetBounds(vertical_layout_rect_.x(),
                            y,
                            vertical_layout_rect_.width(),
@@ -576,7 +573,7 @@ int BrowserViewLayout::GetContentsOffsetForBookmarkBar() {
 
   // Offset for the detached bookmark bar.
   return bookmark_bar_->height() -
-      views::NonClientFrameView::kClientEdgeThickness;
+      bookmark_bar_->GetFullyDetachedToolbarOverlap();
 }
 
 int BrowserViewLayout::GetTopMarginForActiveContent() {
