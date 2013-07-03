ad1176b4a40033714bed3ab315ec226b9763d4d9
koz@chromium.org <koz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 08:16:42 +0000

    [win] Differentiate between fast and non-fast startup path UMAs.
    On windows we can complete the ProcessSingleton rendesvouz before we
    load chrome.dll and proceed with the rest of chrome startup. If we do it
    is known as a fast start. This change adds fast versions of the UMAs
    Startup.ShowAppListWarmStart and
    Startup.WarmStartTimeFromRemoteProcessStart so we can compare the
    timings of the fast and non-fast cases.
    
    Review URL: https://chromiumcodereview.appspot.com/18150005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209914 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.h
+++ chrome/common/chrome_switches.h
@@ -211,6 +211,7 @@ extern const char kExtensionProcess[];
 extern const char kExtensionsUpdateFrequency[];
 extern const char kExtraSearchQueryParams[];
 extern const char kFakeVariationsChannel[];
+extern const char kFastStart[];
 extern const char kFlagSwitchesBegin[];
 extern const char kFlagSwitchesEnd[];
 extern const char kFeedbackServer[];
