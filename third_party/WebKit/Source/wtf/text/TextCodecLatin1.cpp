1b56e66ec29d83bb4507e31967ed192fc48d36d9
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-03 08:11:27 +0000

    Teach TextEncoding to avoid bloatedCharacters()
    This CL teaches the TextEncoding and TextCodec system about narrow characters.
    Instead of passing a UChar* into encode(), we now pass a String, which lets
    TextEncoding use LChars when appropriate. The LChar path can skip some of the
    more complicated encoding work, but in this CL, I've only skipped the NFC
    normalization and left futher optimizations to future CLs.
    
    This CL removes a number of calls to bloatedCharacters(). The one disadvantage
    of this CL is that we can end up copying an LChar array into a UChar array when
    using the ICU text codec. However, I just removed a memcpy from this codepath
    in http://src.chromium.org/viewvc/blink?view=revision&revision=153465 so the
    net effect is the same.
    
    R=eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/18605002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153481 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/TextCodecLatin1.cpp
+++ third_party/WebKit/Source/wtf/text/TextCodecLatin1.cpp
@@ -213,7 +213,8 @@ useLookupTable16:
     return result16;
 }
 
-static CString encodeComplexWindowsLatin1(const UChar* characters, size_t length, UnencodableHandling handling)
+template<typename CharType>
+static CString encodeComplexWindowsLatin1(const CharType* characters, size_t length, UnencodableHandling handling)
 {
     Vector<char> result(length);
     char* bytes = result.data();
@@ -245,7 +246,8 @@ static CString encodeComplexWindowsLatin1(const UChar* characters, size_t length
     return CString(bytes, resultLength);
 }
 
-CString TextCodecLatin1::encode(const UChar* characters, size_t length, UnencodableHandling handling)
+template<typename CharType>
+CString TextCodecLatin1::encodeCommon(const CharType* characters, size_t length, UnencodableHandling handling)
 {
     {
         char* bytes;
@@ -265,6 +267,16 @@ CString TextCodecLatin1::encode(const UChar* characters, size_t length, Unencoda
 
     // If it wasn't all ASCII, call the function that handles more-complex cases.
     return encodeComplexWindowsLatin1(characters, length, handling);
+}
+
+CString TextCodecLatin1::encode(const UChar* characters, size_t length, UnencodableHandling handling)
+{
+    return encodeCommon(characters, length, handling);
+}
+
+CString TextCodecLatin1::encode(const LChar* characters, size_t length, UnencodableHandling handling)
+{
+    return encodeCommon(characters, length, handling);
 }
 
 } // namespace WTF
