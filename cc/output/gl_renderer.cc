6a5436add15d04712eab202f218053740abc2228
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 09:59:23 +0000

    cc: Remove unused WebKit includes.
    BUG=None
    TEST=cc_unittests
    R=danakj@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18156005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209941 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/output/gl_renderer.cc
+++ cc/output/gl_renderer.cc
@@ -55,7 +55,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect_conversions.h"
 
 using WebKit::WebGraphicsContext3D;
-using WebKit::WebGraphicsMemoryAllocation;
 
 namespace cc {
 
