f97c42a5ed799d5c700d72dd03f6a8cb93719260
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 08:38:51 +0000

    Validate invalid suggestions (again).
    Originally I had a larger fix in mind, but for now let's just do this.
    
    R=estade@chromium.org,ahutter@chromium.org
    BUG=252950
    
    Review URL: https://chromiumcodereview.appspot.com/18570004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209925 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -861,8 +861,7 @@ void AutofillDialogControllerImpl::OnWalletFormFieldError(
   if (!wallet_server_validation_recoverable_)
     DisableWallet(wallet::WalletClient::UNKNOWN_ERROR);
 
-  if (view_)
-    view_->UpdateForErrors();
+  UpdateForErrors();
 }
 
 void AutofillDialogControllerImpl::EnsureLegalDocumentsText() {
@@ -1003,6 +1002,26 @@ void AutofillDialogControllerImpl::RestoreUserInputFromSnapshot(
 void AutofillDialogControllerImpl::UpdateSection(DialogSection section) {
   if (view_)
     view_->UpdateSection(section);
+}
+
+void AutofillDialogControllerImpl::UpdateForErrors() {
+  if (!view_)
+    return;
+
+  // Currently, the view should only need to be updated if there are
+  // |wallet_errors_| or validating a suggestion that's based on existing data.
+  bool should_update = !wallet_errors_.empty();
+  if (!should_update) {
+    for (size_t i = SECTION_MIN; i <= SECTION_MAX; ++i) {
+      if (IsEditingExistingData(static_cast<DialogSection>(i))) {
+        should_update = true;
+        break;
+      }
+    }
+  }
+
+  if (should_update)
+    view_->UpdateForErrors();
 }
 
 const DetailInputs& AutofillDialogControllerImpl::RequestedFieldsForSection(
@@ -1924,6 +1943,7 @@ void AutofillDialogControllerImpl::SuggestionItemSelected(
   ResetSectionInput(section);
   ShowEditUiIfBadSuggestion(section);
   UpdateSection(section);
+  UpdateForErrors();
 
   LogSuggestionItemSelectedMetric(*model);
 }
@@ -2506,6 +2526,8 @@ void AutofillDialogControllerImpl::SuggestionsUpdated() {
     ShowEditUiIfBadSuggestion(section);
     UpdateSection(section);
   }
+
+  UpdateForErrors();
 }
 
 void AutofillDialogControllerImpl::FillOutputForSectionWithComparator(
