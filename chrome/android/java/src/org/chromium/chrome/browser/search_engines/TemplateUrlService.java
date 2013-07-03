869bfc52a6be7d55039bb1b299c76bfb9f7dc00a
csorba@chromium.org <csorba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 11:29:29 +0000

    Adding the forced searchengine to the list of localizedSearchEngines.
    BUG=256979
    
    Review URL: https://chromiumcodereview.appspot.com/18351003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209954 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/android/java/src/org/chromium/chrome/browser/search_engines/TemplateUrlService.java
+++ chrome/android/java/src/org/chromium/chrome/browser/search_engines/TemplateUrlService.java
@@ -126,6 +126,9 @@ public class TemplateUrlService {
         nativeSetDefaultSearchProvider(mNativeTemplateUrlServiceAndroid, selectedIndex);
     }
 
+    public boolean isSearchProviderManaged() {
+        return nativeIsSearchProviderManaged(mNativeTemplateUrlServiceAndroid);
+    }
     /**
      * Registers a listener for the TEMPLATE_URL_SERVICE_LOADED notification.
      */
@@ -153,4 +156,5 @@ public class TemplateUrlService {
     private native void nativeSetDefaultSearchProvider(int nativeTemplateUrlServiceAndroid,
             int selectedIndex);
     private native int nativeGetDefaultSearchProvider(int nativeTemplateUrlServiceAndroid);
+    private native boolean nativeIsSearchProviderManaged(int nativeTemplateUrlServiceAndroid);
 }
