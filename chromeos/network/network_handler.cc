df5d6519563c2ebedf07f3bf70722700a8f6a027
armansito@chromium.org <armansito@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-03 22:02:09 +0000

    Implement NetworkDeviceHandler
    This CL adds NetworkDeviceHandler, which provides hooks to make calls to Shill
    Device API methods. This initial implementation provides basic error
    translation and does no state keeping, providing a thin wrapper for the DBus
    layer.
    
    BUG=254125
    
    Review URL: https://chromiumcodereview.appspot.com/18457002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210049 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/network_handler.cc
+++ chromeos/network/network_handler.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chromeos/network/managed_network_configuration_handler.h"
 #include "chromeos/network/network_configuration_handler.h"
 #include "chromeos/network/network_connection_handler.h"
+#include "chromeos/network/network_device_handler.h"
 #include "chromeos/network/network_event_log.h"
 #include "chromeos/network/network_profile_handler.h"
 #include "chromeos/network/network_profile_observer.h"
@@ -28,6 +29,7 @@ NetworkHandler::NetworkHandler() {
 
   cert_loader_.reset(new CertLoader);
   network_state_handler_.reset(new NetworkStateHandler());
+  network_device_handler_.reset(new NetworkDeviceHandler());
   network_profile_handler_.reset(new NetworkProfileHandler());
   network_configuration_handler_.reset(new NetworkConfigurationHandler());
   managed_network_configuration_handler_.reset(
@@ -86,6 +88,10 @@ CertLoader* NetworkHandler::cert_loader() {
 
 NetworkStateHandler* NetworkHandler::network_state_handler() {
   return network_state_handler_.get();
+}
+
+NetworkDeviceHandler* NetworkHandler::network_device_handler() {
+  return network_device_handler_.get();
 }
 
 NetworkProfileHandler* NetworkHandler::network_profile_handler() {
