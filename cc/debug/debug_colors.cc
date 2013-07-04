a17f91b92fd6fa211d46b85291a050293de4b99b
egraether@chromium.org <egraether@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-04 14:22:43 +0000

    cc: Fix HudLayer is blurry on high-DPI screens
    This change fixes the Hudlayer's resolution on high-DPI screens and
    refactors scaling of debug rects and widgets.
    
    BUG=256286
    
    Review URL: https://chromiumcodereview.appspot.com/18348006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210185 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/debug/debug_colors.cc
+++ cc/debug/debug_colors.cc
@@ -168,9 +168,7 @@ SkColor DebugColors::EvictedTileCheckerboardColor() {
 SkColor DebugColors::PaintRectBorderColor() {
   return SkColorSetARGB(255, 255, 0, 0);
 }
-int DebugColors::PaintRectBorderWidth(const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::PaintRectBorderWidth() { return 2; }
 SkColor DebugColors::PaintRectFillColor() {
   return SkColorSetARGB(30, 255, 0, 0);
 }
@@ -179,10 +177,7 @@ SkColor DebugColors::PaintRectFillColor() {
 SkColor DebugColors::PropertyChangedRectBorderColor() {
   return SkColorSetARGB(255, 0, 0, 255);
 }
-int DebugColors::PropertyChangedRectBorderWidth(
-    const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::PropertyChangedRectBorderWidth() { return 2; }
 SkColor DebugColors::PropertyChangedRectFillColor() {
   return SkColorSetARGB(30, 0, 0, 255);
 }
@@ -191,9 +186,7 @@ SkColor DebugColors::PropertyChangedRectFillColor() {
 SkColor DebugColors::SurfaceDamageRectBorderColor() {
   return SkColorSetARGB(255, 200, 100, 0);
 }
-int DebugColors::SurfaceDamageRectBorderWidth(const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::SurfaceDamageRectBorderWidth() { return 2; }
 SkColor DebugColors::SurfaceDamageRectFillColor() {
   return SkColorSetARGB(30, 200, 100, 0);
 }
@@ -202,10 +195,7 @@ SkColor DebugColors::SurfaceDamageRectFillColor() {
 SkColor DebugColors::ScreenSpaceLayerRectBorderColor() {
   return SkColorSetARGB(255, 100, 200, 0);
 }
-int DebugColors::ScreenSpaceLayerRectBorderWidth(
-    const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::ScreenSpaceLayerRectBorderWidth() { return 2; }
 SkColor DebugColors::ScreenSpaceLayerRectFillColor() {
   return SkColorSetARGB(30, 100, 200, 0);
 }
@@ -214,10 +204,7 @@ SkColor DebugColors::ScreenSpaceLayerRectFillColor() {
 SkColor DebugColors::ScreenSpaceSurfaceReplicaRectBorderColor() {
   return SkColorSetARGB(255, 100, 0, 200);
 }
-int DebugColors::ScreenSpaceSurfaceReplicaRectBorderWidth(
-    const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::ScreenSpaceSurfaceReplicaRectBorderWidth() { return 2; }
 SkColor DebugColors::ScreenSpaceSurfaceReplicaRectFillColor() {
   return SkColorSetARGB(10, 100, 0, 200);
 }
@@ -226,9 +213,7 @@ SkColor DebugColors::ScreenSpaceSurfaceReplicaRectFillColor() {
 SkColor DebugColors::OccludingRectBorderColor() {
   return SkColorSetARGB(255, 245, 136, 255);
 }
-int DebugColors::OccludingRectBorderWidth(const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::OccludingRectBorderWidth() { return 2; }
 SkColor DebugColors::OccludingRectFillColor() {
   return SkColorSetARGB(10, 245, 136, 255);
 }
@@ -237,9 +222,7 @@ SkColor DebugColors::OccludingRectFillColor() {
 SkColor DebugColors::NonOccludingRectBorderColor() {
   return SkColorSetARGB(255, 200, 0, 100);
 }
-int DebugColors::NonOccludingRectBorderWidth(const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::NonOccludingRectBorderWidth() { return 2; }
 SkColor DebugColors::NonOccludingRectFillColor() {
   return SkColorSetARGB(10, 200, 0, 100);
 }
@@ -248,10 +231,7 @@ SkColor DebugColors::NonOccludingRectFillColor() {
 SkColor DebugColors::TouchEventHandlerRectBorderColor() {
   return SkColorSetARGB(255, 239, 229, 60);
 }
-int DebugColors::TouchEventHandlerRectBorderWidth(
-    const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::TouchEventHandlerRectBorderWidth() { return 2; }
 SkColor DebugColors::TouchEventHandlerRectFillColor() {
   return SkColorSetARGB(30, 239, 229, 60);
 }
@@ -260,10 +240,7 @@ SkColor DebugColors::TouchEventHandlerRectFillColor() {
 SkColor DebugColors::WheelEventHandlerRectBorderColor() {
   return SkColorSetARGB(255, 189, 209, 57);
 }
-int DebugColors::WheelEventHandlerRectBorderWidth(
-    const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::WheelEventHandlerRectBorderWidth() { return 2; }
 SkColor DebugColors::WheelEventHandlerRectFillColor() {
   return SkColorSetARGB(30, 189, 209, 57);
 }
@@ -272,10 +249,7 @@ SkColor DebugColors::WheelEventHandlerRectFillColor() {
 SkColor DebugColors::NonFastScrollableRectBorderColor() {
   return SkColorSetARGB(255, 238, 163, 59);
 }
-int DebugColors::NonFastScrollableRectBorderWidth(
-    const LayerTreeImpl* tree_impl) {
-  return Scale(2, tree_impl);
-}
+int DebugColors::NonFastScrollableRectBorderWidth() { return 2; }
 SkColor DebugColors::NonFastScrollableRectFillColor() {
   return SkColorSetARGB(30, 238, 163, 59);
 }
