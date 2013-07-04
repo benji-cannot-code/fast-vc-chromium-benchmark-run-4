5bc1744bc73fb9574ff09a49dcf2f61e8ecc9b7b
siggi@chromium.org <siggi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-04 18:56:20 +0000

    Disable DuplicateHandle patch in Syzygy-instrumented binaries.
    TBR=joi@chromium.org
    
    BUG=257137
    
    Review URL: https://codereview.chromium.org/18701003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210213 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_win.cc
+++ content/common/sandbox_win.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/debug/debugger.h"
+#include "base/debug/profiler.h"
 #include "base/debug/trace_event.h"
 #include "base/file_util.h"
 #include "base/hash.h"
@@ -527,7 +528,11 @@ bool InitBrokerServices(sandbox::BrokerServices* broker_services) {
 #ifndef OFFICIAL_BUILD
   BOOL is_in_job = FALSE;
   CHECK(::IsProcessInJob(::GetCurrentProcess(), NULL, &is_in_job));
-  if (!is_in_job && !g_iat_patch_duplicate_handle.is_patched()) {
+  // In a Syzygy-profiled binary, instrumented for import profiling, this
+  // patch will end in infinite recursion on the attempted delegation to the
+  // original function.
+  if (!base::debug::IsBinaryInstrumented() &&
+      !is_in_job && !g_iat_patch_duplicate_handle.is_patched()) {
     HMODULE module = NULL;
     wchar_t module_name[MAX_PATH];
     CHECK(::GetModuleHandleEx(GET_MODULE_HANDLE_EX_FLAG_FROM_ADDRESS,
