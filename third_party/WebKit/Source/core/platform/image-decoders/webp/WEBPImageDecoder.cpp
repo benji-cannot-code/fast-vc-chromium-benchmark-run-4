9038b7308df2a5d9b1db95f034fff2b0658014f7
noel@chromium.org <noel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-04 14:26:20 +0000

    Regression(r153187): Crash reading webp image color profile
    Based on a patch by Pascal Massimino.
    
    setData() may have shuffled the encoded data pointers inbetween the
    creation of m_demux object and its use.  Ensure m_demux is properly
    updated prior to reading color profile data.
    
    TBR=inferno@chromium.org
    BUG=255931
    
    Review URL: https://chromiumcodereview.appspot.com/18691002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153582 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.cpp
@@ -63,6 +63,7 @@ WEBPImageDecoder::WEBPImageDecoder(ImageSource::AlphaOption alphaOption,
     , m_demuxState(WEBP_DEMUX_PARSING_HEADER)
     , m_haveAlreadyParsedThisData(false)
     , m_haveReadAnimationParameters(false)
+    , m_hasProfile(false)
     , m_repetitionCount(cAnimationLoopOnce)
     , m_decodedHeight(0)
 {
@@ -79,6 +80,7 @@ void WEBPImageDecoder::clear()
     if (m_transform)
         qcms_transform_release(m_transform);
     m_transform = 0;
+    m_haveReadProfile = false;
 #endif
     WebPDemuxDelete(m_demux);
     m_demux = 0;
@@ -167,7 +169,7 @@ void WEBPImageDecoder::setData(SharedBuffer* data, bool allDataReceived)
 
     ImageDecoder::setData(data, allDataReceived);
 
-    if (m_demuxState != WEBP_DEMUX_DONE)
+    if (m_demuxState != WEBP_DEMUX_DONE || (m_hasProfile && !m_haveReadProfile))
         m_haveAlreadyParsedThisData = false;
 }
 
@@ -216,6 +218,7 @@ bool WEBPImageDecoder::updateDemuxer()
     if (!ImageDecoder::isSizeAvailable()) {
         m_formatFlags = WebPDemuxGetI(m_demux, WEBP_FF_FORMAT_FLAGS);
         hasAnimation = (m_formatFlags & ANIMATION_FLAG);
+        m_hasProfile = (m_formatFlags & ICCP_FLAG) && !ignoresGammaAndColorProfile();
         if (hasAnimation && !RuntimeEnabledFeatures::animatedWebPEnabled())
             return setFailed();
         if (!setSize(WebPDemuxGetI(m_demux, WEBP_FF_CANVAS_WIDTH), WebPDemuxGetI(m_demux, WEBP_FF_CANVAS_HEIGHT)))
@@ -394,7 +397,7 @@ void WEBPImageDecoder::applyPostProcessing(size_t frameIndex)
     const int top = frameRect.y();
 
 #if USE(QCMSLIB)
-    if ((m_formatFlags & ICCP_FLAG) && !ignoresGammaAndColorProfile()) {
+    if (m_hasProfile) {
         if (!m_haveReadProfile) {
             readColorProfile();
             m_haveReadProfile = true;
@@ -506,7 +509,7 @@ bool WEBPImageDecoder::decode(const uint8_t* dataBytes, size_t dataSize, bool on
         if (!m_premultiplyAlpha)
             mode = outputMode(false);
 #if USE(QCMSLIB)
-        if ((m_formatFlags & ICCP_FLAG) && !ignoresGammaAndColorProfile())
+        if (m_hasProfile)
             mode = MODE_RGBA; // Decode to RGBA for input to libqcms.
 #endif
         WebPInitDecBuffer(&m_decoderBuffer);
--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.h
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.h
@@ -78,6 +78,7 @@ private:
     WebPDemuxState m_demuxState;
     bool m_haveAlreadyParsedThisData;
     bool m_haveReadAnimationParameters;
+    bool m_hasProfile;
     int m_repetitionCount;
     int m_decodedHeight;
 
