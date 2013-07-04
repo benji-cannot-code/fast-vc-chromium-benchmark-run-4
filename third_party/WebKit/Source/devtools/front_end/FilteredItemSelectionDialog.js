9292c6fd5a17083935f67394afc2d84d241e0d26
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-04 14:22:03 +0000

    DevTools: Main resource should be shown in open resource dialog even if it doesn't have name.
    Resources without name are now named as (index).
    
    
    BUG=255410
    
    Review URL: https://chromiumcodereview.appspot.com/18068014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153581 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/FilteredItemSelectionDialog.js
+++ third_party/WebKit/Source/devtools/front_end/FilteredItemSelectionDialog.js
@@ -584,7 +584,7 @@ WebInspector.SelectUISourceCodeDialog = function(defaultScores)
     this._uiSourceCodes = [];
     var projects = WebInspector.workspace.projects().filter(this.filterProject.bind(this));
     for (var i = 0; i < projects.length; ++i)
-        this._uiSourceCodes = this._uiSourceCodes.concat(projects[i].uiSourceCodes().filter(this.filterUISourceCode.bind(this)));
+        this._uiSourceCodes = this._uiSourceCodes.concat(projects[i].uiSourceCodes());
     this._defaultScores = defaultScores;
     this._scorer = new WebInspector.FilePathScoreFunction("");
     WebInspector.workspace.addEventListener(WebInspector.Workspace.Events.UISourceCodeAdded, this._uiSourceCodeAdded, this);
@@ -610,14 +610,6 @@ WebInspector.SelectUISourceCodeDialog.prototype = {
     },
 
     /**
-     * @param {WebInspector.UISourceCode} uiSourceCode
-     */
-    filterUISourceCode: function(uiSourceCode)
-    {
-        return uiSourceCode.name();
-    },
-
-    /**
      * @return {number}
      */
     itemCount: function()
@@ -631,7 +623,7 @@ WebInspector.SelectUISourceCodeDialog.prototype = {
      */
     itemKeyAt: function(itemIndex)
     {
-        return this._uiSourceCodes[itemIndex].fullName();
+        return this._uiSourceCodes[itemIndex].fullDisplayName();
     },
 
     /**
@@ -651,7 +643,7 @@ WebInspector.SelectUISourceCodeDialog.prototype = {
             this._scorer = new WebInspector.FilePathScoreFunction(query);
         }
 
-        var path = uiSourceCode.fullName();
+        var path = uiSourceCode.fullDisplayName();
         return score + 10 * this._scorer.score(path, null);
     },
 
@@ -665,8 +657,8 @@ WebInspector.SelectUISourceCodeDialog.prototype = {
     {
         query = this.rewriteQuery(query);
         var uiSourceCode = this._uiSourceCodes[itemIndex];
-        titleElement.textContent = uiSourceCode.name().trimEnd(100) + (this._queryLineNumber ? this._queryLineNumber : "");
-        subtitleElement.textContent = uiSourceCode.fullName();
+        titleElement.textContent = uiSourceCode.displayName() + (this._queryLineNumber ? this._queryLineNumber : "");
+        subtitleElement.textContent = uiSourceCode.fullDisplayName().trimEnd(100);
 
         var indexes = [];
         var score = new WebInspector.FilePathScoreFunction(query).score(subtitleElement.textContent, indexes);
@@ -714,7 +706,7 @@ WebInspector.SelectUISourceCodeDialog.prototype = {
     _uiSourceCodeAdded: function(event)
     {
         var uiSourceCode = /** @type {WebInspector.UISourceCode} */ (event.data);
-        if (!this.filterUISourceCode(uiSourceCode))
+        if (!this.filterProject(uiSourceCode.project()))
             return;
         this._uiSourceCodes.push(uiSourceCode)
         this.refresh();
