d94b4e9a857e2ad6cbb08d1857e2ac3a42f1c2ee
kojih@chromium.org <kojih@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-04 09:50:26 +0000

    [Binding] use V8TRYCATCH_VOID and raw pointer in indexed setter
    We use V8TRYCATCH_VOID when we assign converted JS value
    into native variable, and the native variable type is
    raw pointer if the type is not primitive.
    (occur in attribute setter and function)
    
    However, in generated indexed setter
    they are simple assignment without try-catch and variable type is RefPtr.
    
    This patch unify the situation by using V8TRYCATCH_VOID and raw pointer.
    
    R=haraken@chromium.org
    BUG=239771
    TEST=TestEventTarget.idl
    
    Review URL: https://chromiumcodereview.appspot.com/18677002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153567 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
+++ third_party/WebKit/Source/bindings/scripts/CodeGeneratorV8.pm
@@ -3352,9 +3352,6 @@ sub GenerateImplementationIndexedPropertySetter
         $code .= "    ExceptionCode ec = 0;\n";
         $extraArguments = ", ec";
     }
-    my $passNativeValue = "propertyValue";
-    $passNativeValue .= ".release()" if (IsRefPtrType($type));
-
     my @conditions = ();
     my @statements = ();
     if ($treatNullAs && $treatNullAs ne "NullString") {
@@ -3366,7 +3363,7 @@ sub GenerateImplementationIndexedPropertySetter
         push @statements, "collection->${treatUndefinedAs}(index$extraArguments);";
     }
     push @conditions, "";
-    push @statements, "collection->${methodName}(index, $passNativeValue$extraArguments);";
+    push @statements, "collection->${methodName}(index, propertyValue$extraArguments);";
     $code .= GenerateIfElseStatement("bool", "result", \@conditions, \@statements);
 
     $code .= "    if (!result)\n";
@@ -3651,7 +3648,7 @@ sub GenerateNativeValueDefinition
     my $treatNullAs = $parameter->extendedAttributes->{"TreatNullAs"} || "";
     my $treatUndefinedAs = $parameter->extendedAttributes->{"TreatUndefinedAs"} || "";
     my $code = "";
-    my $nativeType = GetNativeType($parameter->type);
+    my $nativeType = GetNativeType($parameter->type, 1);
     my $nativeValue = JSValueToNative($parameter->type, $function->extendedAttributes, $jsValue, $getIsolate);
     if ($parameter->type eq "DOMString") {
         my $nullCheck = "";
@@ -3663,7 +3660,7 @@ sub GenerateNativeValueDefinition
         }
         $code .= "    V8TRYCATCH_FOR_V8STRINGRESOURCE_VOID(V8StringResource<${nullCheck}>, ${nativeValueName}, ${jsValue});\n";
     } else {
-        $code .= "    ${nativeType} ${nativeValueName} = ${nativeValue};\n";
+        $code .= "    V8TRYCATCH_VOID(${nativeType}, ${nativeValueName}, ${nativeValue});\n";
     }
     return $code;
 }
