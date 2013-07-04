6270c3a31f1dfabbf463fb5e2d6d295eeacf3686
eustas@chromium.org <eustas@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-04 07:07:36 +0000

    [DevTools] NetworkPanel: Highlight row with current search result.
    1) When search jumps to next result - flash row with result
    2) Turn flash animation to be yellow (that is common for other panels)
    3) Cleanup row highlight alongside other highlights.
    
    Review URL: https://chromiumcodereview.appspot.com/18325014
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153561 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
+++ third_party/WebKit/Source/devtools/front_end/NetworkPanel.js
@@ -1203,6 +1203,7 @@ WebInspector.NetworkLogView.prototype = {
 
     _removeAllHighlights: function()
     {
+        this._removeAllNodeHighlights();
         for (var i = 0; i < this._highlightedSubstringChanges.length; ++i)
             WebInspector.revertDomChanges(this._highlightedSubstringChanges[i]);
         this._highlightedSubstringChanges = [];
@@ -1225,8 +1226,10 @@ WebInspector.NetworkLogView.prototype = {
             this._toggleLargerRequests();
         var highlightedSubstringChanges = node._highlightMatchedSubstring(regExp);
         this._highlightedSubstringChanges.push(highlightedSubstringChanges);
-        if (reveal)
+        if (reveal) {
             node.reveal();
+            this._highlightNode(node);
+        }
     },
 
     /**
@@ -1274,7 +1277,7 @@ WebInspector.NetworkLogView.prototype = {
 
         this.dispatchEventToListeners(WebInspector.NetworkLogView.EventTypes.SearchCountUpdated, this._matchedRequests.length);
         if (shouldJump)
-            this._highlightNthMatchedRequestForSearch(newMatchedRequestIndex, false);
+            this._highlightNthMatchedRequestForSearch(newMatchedRequestIndex, true);
     },
 
     /**
