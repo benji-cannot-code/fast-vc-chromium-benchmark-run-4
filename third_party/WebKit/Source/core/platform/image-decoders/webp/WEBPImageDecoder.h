0363cfadb42d31ed22fda8e212e0b968508df3f8
peter@chromium.org <peter@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-04 15:04:47 +0000

    Revert "Regression(r153187): Crash reading webp image color profile"
    This reverts commit 86752a43ff6b67cfc6225ec8b2ba6e0999071148.
    
    WEBPImageDecoder::setData() uses the m_haveReadProfile property,
    which is only available when QCMS is used for color profile support.
    Android does not enable QCMS yet, which broke the build.
    
    TBR=noel, inferno
    BUG=255931
    
    Review URL: https://codereview.chromium.org/18704002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153588 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.h
+++ third_party/WebKit/Source/core/platform/image-decoders/webp/WEBPImageDecoder.h
@@ -78,7 +78,6 @@ private:
     WebPDemuxState m_demuxState;
     bool m_haveAlreadyParsedThisData;
     bool m_haveReadAnimationParameters;
-    bool m_hasProfile;
     int m_repetitionCount;
     int m_decodedHeight;
 
