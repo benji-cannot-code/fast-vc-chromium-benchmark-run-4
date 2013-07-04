76a1cc999b9a867463f8b36dcee6c9671387c88c
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-04 05:54:46 +0000

    Deprecate FileSystemMountPointProvider::DeleteFileSystem
    FileSystem deletion (reset) should only work for sandboxed
    filesystems, and the method must not be on the generic
    FileSystemMountPointProvider interface.
    
    BUG=241701
    TEST=manual (devtools -> delete root -> see the filesystem goes away), layout tests
    NOTRY=true
    R=satorux@chromium.org, tzik@chromium.org, vandebo@chromium.org
    
    Review URL: https://codereview.chromium.org/18586006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210126 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/fileapi/media_file_system_mount_point_provider.cc
+++ chrome/browser/media_galleries/fileapi/media_file_system_mount_point_provider.cc
@@ -209,13 +209,4 @@ MediaFileSystemMountPointProvider::GetQuotaUtil() {
   return NULL;
 }
 
-void MediaFileSystemMountPointProvider::DeleteFileSystem(
-    const GURL& origin_url,
-    fileapi::FileSystemType type,
-    FileSystemContext* context,
-    const DeleteFileSystemCallback& callback) {
-  NOTREACHED();
-  callback.Run(base::PLATFORM_FILE_ERROR_INVALID_OPERATION);
-}
-
 }  // namespace chrome
