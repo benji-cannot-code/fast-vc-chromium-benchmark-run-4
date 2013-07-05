d68257de07ec3e4e3e13b983a7be5223214c3932
dslomov@chromium.org <dslomov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-05 10:08:21 +0000

    Reland "Use V8 implementation of ArrayBuffer in Blink".
    This is exactly the same patch as in https://codereview.chromium.org/18233012/. 
    
    It got reverted from Blink because it failed ExtensionWebRequestApiTest.PostData2 
    in chromium browser_test. The problem with that test has been identified 
    (http://crbug.com/257128), the test will be disabled before the Blink roll and 
    updated afterwards.
    
    R=kbr@chromium.org,jochen@chromium.org
    BUG=257123,257122
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=153595
    
    Review URL: https://chromiumcodereview.appspot.com/18503008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ArrayBuffer.h
+++ third_party/WebKit/Source/wtf/ArrayBuffer.h
@@ -61,7 +61,7 @@ public:
     void removeView(ArrayBufferView*);
 
     bool transfer(ArrayBufferContents&, Vector<RefPtr<ArrayBufferView> >& neuteredViews);
-    bool isNeutered() { return !m_contents.data(); }
+    bool isNeutered() { return m_isNeutered; }
 
     bool hasDeallocationObserver() { return m_contents.hasDeallocationObserver(); }
     void setDeallocationObserver(ArrayBufferDeallocationObserver* observer) { m_contents.setDeallocationObserver(observer); }
@@ -78,6 +78,7 @@ private:
 
     ArrayBufferContents m_contents;
     ArrayBufferView* m_firstView;
+    bool m_isNeutered;
 };
 
 int ArrayBuffer::clampValue(int x, int left, int right)
@@ -129,7 +130,7 @@ PassRefPtr<ArrayBuffer> ArrayBuffer::create(unsigned numElements, unsigned eleme
 }
 
 ArrayBuffer::ArrayBuffer(ArrayBufferContents& contents)
-    : m_firstView(0)
+    : m_firstView(0), m_isNeutered(false)
 {
     contents.transfer(m_contents);
 }
