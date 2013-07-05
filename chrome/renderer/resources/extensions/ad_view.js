bb2d7b4494745895416d7574f69d863878d7646a
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-05 08:25:52 +0000

    Browser Plugin: Don't expose guest-initiated navigations to embedder in content
    Not all view types might want to expose guest initiated navigations to the embedder.
    
    This CL moves updating the src attribute on navigation to the webview/adview shim instead
    of via IPC to BrowserPlugin. Thus, the content layer BrowserPlugin is no longer aware
    of guest-initiated navigations.
    
    BUG=166165
    Test=WebViewTest.*, AdViewTest.*, BrowserPluginHostTest.*, BrowserPluginTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/18345015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210275 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/ad_view.js
+++ chrome/renderer/resources/extensions/ad_view.js
@@ -87,6 +87,11 @@ var AD_VIEW_EXT_EVENTS = {
     fields: ['url', 'isTopLevel', 'reason']
   },
   'loadcommit': {
+    customHandler: function(adview, event) {
+      if (event.isTopLevel) {
+        adview.browserPluginNode_.setAttribute('src', event.url);
+      }
+    },
     evt: AdviewLoadCommitEvent,
     fields: ['url', 'isTopLevel']
   }
