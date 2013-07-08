78031b2d28ed6222e0b23ef705d45b8a918d6cf9
raphael.kubo.da.costa@intel.com <raphael.kubo.da.costa@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-08 12:48:11 +0000

    The most recent version of the HTML Media Capture spec changes the `capture' attribute from an enum to a boolean (the values that used to be passed to the enum are now supposed to be extracted from the `accept' attribute).
    For now, add a `mediaCaptureEnabled' field to WebFileChooserParams and
    FileChooserSettings that coexists with the existing `capture' field while
    the Chromium side of the changes is done.
    
    R=abarth@chromium.org,eseidel@chromium.org,jochen@chromium.org
    BUG=240252
    
    Review URL: https://chromiumcodereview.appspot.com/15015006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153676 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/FileChooser.h
+++ third_party/WebKit/Source/core/platform/FileChooser.h
@@ -58,6 +58,7 @@ struct FileChooserSettings {
     Vector<String> selectedFiles;
 #if ENABLE(MEDIA_CAPTURE)
     String capture;
+    bool useMediaCapture;
 #endif
 
     // Returns a combined vector of acceptMIMETypes and acceptFileExtensions.
