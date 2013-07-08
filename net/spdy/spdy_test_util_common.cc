4b95b2b68eab88f81ecbfb1a0c4dc0a42db108cd
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-08 04:29:59 +0000

    [SPDY] Make SpdySessionPool keep track of available sessions
    Split a SpdySessionPool's sessions into available ones and unavailable ones.
    Keep all sessions in a set, and all the available sessions in a map keyed
    by SpdySessionKey. This is in preparation for making SpdySessionPool
    own a session even if it receives a GOAWAY frame.
    
    Split SpdySessionPool::Remove() into two functions -- MakeSessionUnavailable()
    and RemoveUnavailableSession(). For now, sessions call them at the same
    time, but in the future RemoveUnavailableSession() may be called later
    than MakeSessionUnavailable() (for the GOAWAY case).
    
    Don't add a SpdySession to a pool if it encountered an error during
    initialization. Also, don't do some work in SpdySession::InitializeWithSocket()
    if initialization fails.
    
    Rename functions and variables in SpdySessionPool to be concise and
    consistent. Inline a bunch of one-off functions. Rewrite the loop
    in SpdySessionPool::RemoveAliases().
    
    BUG=255701
    
    Review URL: https://chromiumcodereview.appspot.com/18600010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210344 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_test_util_common.cc
+++ net/spdy/spdy_test_util_common.cc
@@ -483,7 +483,7 @@ SpdyURLRequestContext::~SpdyURLRequestContext() {
 }
 
 bool HasSpdySession(SpdySessionPool* pool, const SpdySessionKey& key) {
-  return pool->GetIfExists(key, BoundNetLog()) != NULL;
+  return pool->FindAvailableSession(key, BoundNetLog()) != NULL;
 }
 
 namespace {
@@ -543,7 +543,7 @@ scoped_refptr<SpdySession> CreateSpdySessionHelper(
   scoped_refptr<SpdySession> spdy_session;
   EXPECT_EQ(
       OK,
-      http_session->spdy_session_pool()->GetSpdySessionFromSocket(
+      http_session->spdy_session_pool()->CreateAvailableSessionFromSocket(
           key, connection.Pass(), net_log, OK, &spdy_session,
           is_secure));
   EXPECT_TRUE(HasSpdySession(http_session->spdy_session_pool(), key));
@@ -629,7 +629,7 @@ scoped_refptr<SpdySession> CreateFakeSpdySession(SpdySessionPool* pool,
   handle->set_socket(new FakeSpdySessionClientSocket());
   EXPECT_EQ(
       OK,
-      pool->GetSpdySessionFromSocket(
+      pool->CreateAvailableSessionFromSocket(
           key, handle.Pass(), BoundNetLog(), OK, &spdy_session,
           true /* is_secure */));
   EXPECT_TRUE(HasSpdySession(pool, key));
