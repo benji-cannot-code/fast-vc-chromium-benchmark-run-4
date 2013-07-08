5af40865a31af10aa1e2be55186badcd665dbf64
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-08 04:32:47 +0000

    DecodeEscapeSequences shouldn't use bloatedCharacters()
    We're sweeping the codebase to remove callers of bloatedCharacters so we can
    remove the mechanism entirely. This code doesn't need to use bloatedCharacters.
    
    R=tsepez@chromium.org
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/18328025
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153659 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/text/DecodeEscapeSequences.h
+++ third_party/WebKit/Source/core/platform/text/DecodeEscapeSequences.h
@@ -52,7 +52,9 @@ struct Unicode16BitEscapeSequence {
         }
         return runEnd;
     }
-    static String decodeRun(const UChar* run, size_t runLength, const WTF::TextEncoding&)
+
+    template<typename CharType>
+    static String decodeRun(const CharType* run, size_t runLength, const WTF::TextEncoding&)
     {
         // Each %u-escape sequence represents a UTF-16 code unit.
         // See <http://www.w3.org/International/iri-edit/draft-duerst-iri.html#anchor29>.
@@ -97,14 +99,16 @@ struct URLEscapeSequence {
         }
         return runEnd;
     }
-    static String decodeRun(const UChar* run, size_t runLength, const WTF::TextEncoding& encoding)
+
+    template<typename CharType>
+    static String decodeRun(const CharType* run, size_t runLength, const WTF::TextEncoding& encoding)
     {
         // For URL escape sequences, we know that findEndOfRun() has given us a run where every %-sign introduces
         // a valid escape sequence, but there may be characters between the sequences.
         Vector<char, 512> buffer;
         buffer.resize(runLength); // Unescaping hex sequences only makes the length smaller.
         char* p = buffer.data();
-        const UChar* runEnd = run + runLength;
+        const CharType* runEnd = run + runLength;
         while (run < runEnd) {
             if (run[0] == '%') {
                 *p++ = (toASCIIHexValue(run[1]) << 4) | toASCIIHexValue(run[2]);
@@ -135,7 +139,10 @@ String decodeEscapeSequences(const String& string, const WTF::TextEncoding& enco
             continue;
         }
 
-        String decoded = EscapeSequence::decodeRun(string.bloatedCharacters() + encodedRunPosition, encodedRunEnd - encodedRunPosition, encoding);
+        String decoded = string.is8Bit() ?
+            EscapeSequence::decodeRun(string.characters8() + encodedRunPosition, encodedRunEnd - encodedRunPosition, encoding) :
+            EscapeSequence::decodeRun(string.characters16() + encodedRunPosition, encodedRunEnd - encodedRunPosition, encoding);
+
         if (decoded.isEmpty())
             continue;
 
