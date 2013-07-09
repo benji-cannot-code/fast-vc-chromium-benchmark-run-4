981005a476cd03d85aceee603b22ab109b707efb
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 21:13:11 +0000

    Comment improvements for chrome/browser/infobars.
    Also includes a few other tiny tweaks to arg names/DCHECKs and kills a using
    directive.
    
    BUG=none
    TEST=none
    R=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/18532009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210623 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/infobars/insecure_content_infobar_delegate.cc
+++ chrome/browser/infobars/insecure_content_infobar_delegate.cc
@@ -15,7 +15,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
 
-using content::OpenURLParams;
 
 // static
 void InsecureContentInfoBarDelegate::Create(InfoBarService* infobar_service,
@@ -109,9 +108,9 @@ string16 InsecureContentInfoBarDelegate::GetLinkText() const {
 
 bool InsecureContentInfoBarDelegate::LinkClicked(
     WindowOpenDisposition disposition) {
-  web_contents()->OpenURL(OpenURLParams(
-      google_util::AppendGoogleLocaleParam(GURL(
-      "https://www.google.com/support/chrome/bin/answer.py?answer=1342714")),
+  web_contents()->OpenURL(content::OpenURLParams(
+      google_util::AppendGoogleLocaleParam(GURL("https://www.google.com/"
+          "support/chrome/bin/answer.py?answer=1342714")),
       content::Referrer(),
       (disposition == CURRENT_TAB) ? NEW_FOREGROUND_TAB : disposition,
       content::PAGE_TRANSITION_LINK, false));
