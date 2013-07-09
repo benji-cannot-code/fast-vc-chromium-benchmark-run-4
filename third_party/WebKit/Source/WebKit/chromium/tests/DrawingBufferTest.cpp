e76f801158366f013679632e6e23346c2380a6c2
piman@chromium.org <piman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-09 23:05:25 +0000

    Remove out-of-date and asserting test
    This is part of https://codereview.chromium.org/18868008 but needs to land first before we can roll.
    
    BUG=None
    TBR=dimich
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/18340009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153824 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/WebKit/chromium/tests/DrawingBufferTest.cpp
+++ third_party/WebKit/Source/WebKit/chromium/tests/DrawingBufferTest.cpp
@@ -48,11 +48,6 @@ using testing::_;
 
 namespace {
 
-class MockWebTextureUpdater : public WebTextureUpdater {
-public:
-    MOCK_METHOD3(appendCopy, void(unsigned, unsigned, WebSize));
-};
-
 class FakeContextEvictionManager : public ContextEvictionManager {
 public:
     void forciblyLoseOldestContext(const String& reason) { }
@@ -75,19 +70,6 @@ protected:
 };
 
 namespace {
-
-TEST_F(DrawingBufferTest, verifyNoNewBuffersAfterContextLostWithTextures)
-{
-    // Tell the buffer its contents changed and context was lost.
-    m_drawingBuffer->markContentsChanged();
-    m_drawingBuffer->releaseResources();
-
-    MockWebTextureUpdater updater;
-    EXPECT_CALL(updater, appendCopy(_, _, _)).Times(0);
-
-    unsigned zeroTexture = 0;
-    EXPECT_EQ(zeroTexture, m_drawingBuffer->prepareTexture(updater));
-}
 
 TEST_F(DrawingBufferTest, verifyNoNewBuffersAfterContextLostWithMailboxes)
 {
