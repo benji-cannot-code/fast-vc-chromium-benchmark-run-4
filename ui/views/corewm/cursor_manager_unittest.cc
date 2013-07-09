a4efb39d874c58b99f4a088a4bf53347e64e5c85
ananta@chromium.org <ananta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 21:16:15 +0000

    Make the SetCursorResourceModuleName function in the CursorLoaderWin class a static function and remove it from the CursorLoader and CursorClient interface.
    This function is currently called from the ctor of the ChromeContentBrowserClient class which ensures that it happens
    when the browser starts up and avoids any race conditions due to code attempting to read the module name before it
    was set.
    
    This ensures that the non standard webkit cursors can be loaded correctly when needed.
    Fixes bug https://code.google.com/p/chromium/issues/detail?id=257983
    
    Bug=257983
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/18132011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210625 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/corewm/cursor_manager_unittest.cc
+++ ui/views/corewm/cursor_manager_unittest.cc
@@ -45,9 +45,6 @@ class TestingCursorManager : public views::corewm::NativeCursorManager {
     delegate->CommitScale(scale);
   }
 
-  virtual void SetCursorResourceModule(const string16& module_name) OVERRIDE {
-  }
-
  private:
   gfx::NativeCursor cursor_;
 };
