ada40a843973e08db929cf0579223c0777a5d165
hellner@chromium.org <hellner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 08:50:41 +0000

    WebRTC: adds webrtc specific unit tests to test_runner.py and gtest_config.py for running them in a chromium workspace.
    BUG=https://code.google.com/p/webrtc/issues/detail?id=1882
    
    Review URL: https://chromiumcodereview.appspot.com/18358005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210534 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/gtest/test_runner.py
+++ build/android/pylib/gtest/test_runner.py
@@ -235,6 +235,11 @@ def _GetDataFilesForTestSuite(product_dir, test_suite_basename):
         'content/test/data/media',
         'third_party/webgl_conformance',
     ]
+  elif test_suite_basename == 'modules_unittests':
+    return [
+        'resources',
+        'data',
+    ]
   return []
 
 
