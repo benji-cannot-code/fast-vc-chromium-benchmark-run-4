cc1b149cb84b1b43dee890eb4e0f243e5a6d3fac
tim@chromium.org <tim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 08:02:14 +0000

    sync: deferred initialization support for apps + extensions
    If a syncable extension or apps change occurs before MergeDataAndStartSyncing
    has been called, tell the sync system to start as soon as possible.
    
    Note: This change has no real effect unless --sync-enable-deferred-startup
    is passed on the command line.  Bug 256795 will need more investigation
    before we're ready to flip that flag.
    
    BUG=80194, 256795
    
    Review URL: https://chromiumcodereview.appspot.com/15362004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210511 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/app_sync_bundle.h
+++ chrome/browser/extensions/app_sync_bundle.h
@@ -73,9 +73,6 @@ class AppSyncBundle {
   void AddPendingApp(const std::string& id,
                      const AppSyncData& app_sync_data);
 
-  // Returns true if |extension| should be handled by this sync bundle.
-  bool HandlesApp(const Extension& extension) const;
-
   // Returns a vector of all the pending sync data.
   std::vector<AppSyncData> GetPendingData() const;
 
@@ -83,6 +80,9 @@ class AppSyncBundle {
   void GetAppSyncDataListHelper(
       const ExtensionSet& extensions,
       std::vector<extensions::AppSyncData>* sync_data_list) const;
+
+  // Returns true if SetupSync has been called, false otherwise.
+  bool IsSyncing() const;
 
  private:
   // Add a synced app.
