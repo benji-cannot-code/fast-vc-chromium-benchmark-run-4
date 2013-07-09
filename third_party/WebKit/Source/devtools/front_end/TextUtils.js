712785809f83c68ca2c03e7a290b29ce7d18c890
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-09 08:14:17 +0000

    DevTools: [CodeMirror] indent auto-detection option
    This patch adds "auto-detect" option into "indent" setting for
    editor. This option will analyze given text and try to setup
    a desired indentation level based on the content indentation.
    
    BUG=
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/18768003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153762 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/TextUtils.js
+++ third_party/WebKit/Source/devtools/front_end/TextUtils.js
@@ -128,5 +128,6 @@ WebInspector.TextUtils.Indent = {
     TwoSpaces: "  ",
     FourSpaces: "    ",
     EightSpaces: "        ",
-    TabCharacter: "\t"
+    TabCharacter: "\t",
+    AutoDetect: "AutoDetect"
 }
