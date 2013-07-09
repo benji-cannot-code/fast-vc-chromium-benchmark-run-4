9225058237069552b7e0b7dbc2789358cf26bea9
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 21:12:02 +0000

    Spawn a FILE thread in ProfileSigninConfirmationHelper tests.
    Prevent leaks of objects which are scheduled for deletion on the FILE thread.
    
    BUG=189194
    R=dconnelly@chromium.org, akalin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18052020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210619 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/sync/profile_signin_confirmation_helper_unittest.cc
+++ chrome/browser/ui/sync/profile_signin_confirmation_helper_unittest.cc
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/test/base/testing_profile.h"
 #include "chrome/test/base/ui_test_utils.h"
 #include "components/user_prefs/pref_registry_syncable.h"
-#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "content/public/test/test_utils.h"
 #include "testing/gmock/include/gmock/gmock.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -117,8 +117,7 @@ static scoped_refptr<extensions::Extension> CreateExtension(
 class ProfileSigninConfirmationHelperTest : public testing::Test {
  public:
   ProfileSigninConfirmationHelperTest()
-      : ui_thread_(content::BrowserThread::UI, &message_loop_),
-        user_prefs_(NULL),
+      : user_prefs_(NULL),
         model_(NULL) {
   }
 
@@ -154,12 +153,11 @@ class ProfileSigninConfirmationHelperTest : public testing::Test {
     // TestExtensionSystem uses DeleteSoon, so we need to delete the profile
     // and then run the message queue to clean up.
     profile_.reset();
-    base::MessageLoop::current()->RunUntilIdle();
+    base::RunLoop().RunUntilIdle();
   }
 
  protected:
-  base::MessageLoopForUI message_loop_;
-  content::TestBrowserThread ui_thread_;
+  content::TestBrowserThreadBundle thread_bundle_;
   scoped_ptr<TestingProfile> profile_;
   TestingPrefStoreWithCustomReadError* user_prefs_;
   BookmarkModel* model_;
