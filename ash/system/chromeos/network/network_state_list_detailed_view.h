0badf55031dc4f37372a494594d9eaad758fd5c1
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 21:41:13 +0000

    Add debugging UI for NetworkStateHandler Favorites
    BUG=251922
    R=jennyz@chromium.org
    
    Review URL: https://codereview.chromium.org/18031020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210643 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/chromeos/network/network_state_list_detailed_view.h
+++ ash/system/chromeos/network/network_state_list_detailed_view.h
@@ -45,6 +45,7 @@ class NetworkStateListDetailedView
  public:
   enum ListType {
     LIST_TYPE_NETWORK,
+    LIST_TYPE_DEBUG_PREFERRED,
     LIST_TYPE_VPN
   };
 
@@ -88,6 +89,8 @@ class NetworkStateListDetailedView
 
   void UpdateNetworks(
       const chromeos::NetworkStateHandler::NetworkStateList& networks);
+  void UpdatePreferred(
+      const chromeos::NetworkStateHandler::FavoriteStateList& favorites);
   void UpdateNetworkList();
   bool CreateOrUpdateInfoLabel(
       int index, const base::string16& text, views::Label** label);
@@ -134,6 +137,7 @@ class NetworkStateListDetailedView
   TrayPopupLabelButton* turn_on_wifi_;
   TrayPopupLabelButton* other_mobile_;
   TrayPopupLabelButton* other_vpn_;
+  TrayPopupLabelButton* toggle_debug_preferred_networks_;
   TrayPopupLabelButton* settings_;
   TrayPopupLabelButton* proxy_settings_;
   views::Label* scanning_view_;
