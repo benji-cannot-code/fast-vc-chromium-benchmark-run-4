f8ac5ecd39f85e9ba20da9ed27fe39974e0639fe
zvorygin@chromium.org <zvorygin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-09 16:00:00 +0000

    [DevTools] Switched ConsoleView visible messages storage to index-based. Fixed minor search issues when operate filters while searching.
    BUG=
    R=apavlov@chromium.org
    
    Review URL: https://codereview.chromium.org/18615006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153789 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ConsolePanel.js
+++ third_party/WebKit/Source/devtools/front_end/ConsolePanel.js
@@ -35,8 +35,6 @@ WebInspector.ConsolePanel = function()
 {
     WebInspector.Panel.call(this, "console");
 
-    WebInspector.consoleView.addEventListener(WebInspector.ConsoleView.Events.EntryAdded, this._consoleMessageAdded, this);
-    WebInspector.consoleView.addEventListener(WebInspector.ConsoleView.Events.ConsoleCleared, this._consoleCleared, this);
     this._view = WebInspector.consoleView;
 }
 
@@ -89,7 +87,7 @@ WebInspector.ConsolePanel.prototype = {
      */
     performFilter: function(query)
     {
-        this._view.performFilter(query, this);
+        this._view.performFilter(query);
     },
 
     jumpToNextSearchResult: function()
@@ -100,27 +98,6 @@ WebInspector.ConsolePanel.prototype = {
     jumpToPreviousSearchResult: function()
     {
         this._view.jumpToPreviousSearchResult(this);
-    },
-
-    _consoleMessageAdded: function(event)
-    {
-        if (!this._searchRegex || !this.isShowing())
-            return;
-        var message = event.data;
-        this._searchRegex.lastIndex = 0;
-        if (message.matchesRegex(this._searchRegex)) {
-            this._searchResults.push(message);
-            WebInspector.searchController.updateSearchMatchesCount(this._searchResults.length, this);
-        }
-    },
-
-    _consoleCleared: function()
-    {
-        if (!this._searchResults)
-            return;
-        this._searchResults.length = 0;
-        if (this.isShowing())
-            WebInspector.searchController.updateSearchMatchesCount(0, this);
     },
 
     __proto__: WebInspector.Panel.prototype
