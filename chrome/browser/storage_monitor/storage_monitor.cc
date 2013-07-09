83897bfa26822399cf46ead1dc540b6de7d476c7
Hokein.Wu@gmail.com <Hokein.Wu@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 17:40:40 +0000

    Adjustment for StorageMonitor After SystemInfo Storage API Rewrite
    Since systemInfo.storage API is rewritten using StorageMonitor, here are some
    adjustments and updates in StorageMonitor:
    
      * Update the StorageMonitor comments since its storage_map_ now contains fixed
        and removable storage devices' id.
      * Remove IsRemovableDevice DCHECK in TransientDeviceIds since
        systemInfo.storage API now exposes transient_id for fixed storage devices
        through TransientDeviceIds class.
    
    BUG=None
    TEST=unit_tests --gtest_filter=StorageMonitorTest.*
    TEST=unit_tests --gtest_filter=StorageMonitorWinTest.*
    TEST=manually, invoke systeminfo.storage.get API in Chromium Debug
    version, and see results.
    
    Review URL: https://chromiumcodereview.appspot.com/18802003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210599 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/storage_monitor/storage_monitor.cc
+++ chrome/browser/storage_monitor/storage_monitor.cc
@@ -157,7 +157,7 @@ void StorageMonitor::ProcessAttach(const StorageInfo& info) {
     storage_map_.insert(std::make_pair(info.device_id(), info));
   }
 
-  DVLOG(1) << "RemovableStorageAttached with name " << UTF16ToUTF8(info.name())
+  DVLOG(1) << "StorageAttached with name " << UTF16ToUTF8(info.name())
            << " and id " << info.device_id();
   if (StorageInfo::IsRemovableDevice(info.device_id())) {
     observer_list_->Notify(
@@ -176,7 +176,7 @@ void StorageMonitor::ProcessDetach(const std::string& id) {
     storage_map_.erase(it);
   }
 
-  DVLOG(1) << "RemovableStorageDetached for id " << id;
+  DVLOG(1) << "StorageDetached for id " << id;
   if (StorageInfo::IsRemovableDevice(info.device_id())) {
     observer_list_->Notify(
         &RemovableStorageObserver::OnRemovableStorageDetached, info);
