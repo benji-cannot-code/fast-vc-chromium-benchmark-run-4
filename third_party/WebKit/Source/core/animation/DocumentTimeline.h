39196683fb8dd359341db698f0ef21a8884692c1
rjwright@chromium.org <rjwright@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-09 05:40:14 +0000

    Implemented DocumentTimeline::pauseAnimationsForTesting.
    pauseAnimationsForTesting is used in some of the old WebKit layout tests for other animations APIs, and we want to be able to use those tests for Web Animations.
    
    Tested in DocumentTimeLineTest.pauseForTesting.
    
    Review URL: https://chromiumcodereview.appspot.com/18815002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153750 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/DocumentTimeline.h
+++ third_party/WebKit/Source/core/animation/DocumentTimeline.h
@@ -50,6 +50,7 @@ public:
     void serviceAnimations(double);
     PassRefPtr<Player> play(TimedItem*);
     double currentTime() { return m_currentTime; }
+    void pauseAnimationsForTesting(double);
 
 private:
     DocumentTimeline(Document*);
