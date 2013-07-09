b3acab3f7521d6994d694bff0e705a9c53b18609
rsimha@chromium.org <rsimha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-09 13:57:39 +0000

    [sync] Refactor initialization and display of sync setup web ui page
    When a user responds to the "Sync Error: Sign in again..." error, we
    handle it by navigating to chrome://settings/syncSetup. In the presence
    of an auth error, this page displays a blank gray box in place of a
    proper setup overlay dialog, and presents the user with a gaia re-auth
    page in a new tab. This is because SyncSetupHandler::OpenSyncSetup()
    neglects to show any meaningful UI, like a config dialog or a spinner,
    resulting in a blank overlay with just a gray shadow and a close button.
    
    In addition, we have separate UI flows based on whether the user
    responds to an auth error via the "Sync Error: Sign in again..." item on
    the hotdog menu, or via the "Account sign-in details are out of date.
    Sign in again" link on the settings page. This is confusing.
    
    Furthermore, with r204694, all uses of non-web-based sign-in were
    removed. However, some of its artifacts were left remaining in the code.
    In particular, we used to have two methods to display the initial sync
    setup page based on whether it was in response to an error, or as part
    of setup. This is unnecessary, since SyncSetupHandler::OpenSyncSetup()
    is now capable of determining the error state and displaying the correct
    UI.
    
    Finally, SyncSetupHandler::DisplayGaiaLogin() has an unused parameter
    |fatal_error| that was being used in the non-web-ui sign-in flow, but is
    no longer useful.
    
    This patch does the following:
    1) In SyncSetupHandler::OpenSyncSetup(), the cases of auth due to
       initial setup and re-auth due to rejected credentials are now handled
       separately.
       a) In case of auth for initial setup, we display gaia auth in a new
          tab.
       b) In case of re-auth due to rejected credentials, we show the sync
          setup page with a spinner dialog, and then display gaia auth in
          a new tab.
    2) In SyncSetupHandler, we remove the method HandleShowErrorUI and
       roll its funcationality into HandleShowSetupUI.
    3) In SyncSetupHandler::DisplayGaiaLogin(), the parameter |fatal_error|
       wasn't being used now that non-web-based sign-in is no longer
       possible. We remove the unused parameter.
    4) In SyncSetupHandlerTest, we remove CheckShowSyncSetupArgs, a helper
       function that was previously being used for the non-web-based sign-in
       flow, and is no longer required.
    5) We rewrite SyncSetupWebUITestAsync.VerifySignIn, which was a previously
       disabled browser test. It relied on older non-web-ui login elements that no
       longer exist. It now uses the new web-ui sync flow.
    
    With the above changes, we unify the multiple UI flows the user sees
    while responding to an auth error into a single, non-confusing flow. We
    also remove unused artifacts from the older non-web-based sign-in flow.
    
    R=atwilson,rogerta
    TBR=estade
    BUG=177217, 253702, 92948
    
    Review URL: https://chromiumcodereview.appspot.com/18647007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210552 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/sync_setup_overlay.js
+++ chrome/browser/resources/sync_setup_overlay.js
@@ -513,7 +513,7 @@ cr.define('options', function() {
       // between its drop-down menu items as follows:
       // "Sync everything": Show encryption and passphrase sections, and disable
       // and check all data type checkboxes.
-      // "Sync everything": Hide encryption and passphrase sections, and disable
+      // "Sync nothing": Hide encryption and passphrase sections, and disable
       // and uncheck all data type checkboxes.
       // "Choose what to sync": Show encryption and passphrase sections, enable
       // data type checkboxes, and restore their checked state to the last time
@@ -813,14 +813,6 @@ cr.define('options', function() {
     },
 
     /**
-     * Steps into the appropriate Sync Setup error UI.
-     * @private
-     */
-    showErrorUI_: function() {
-      chrome.send('SyncSetupShowErrorUI');
-    },
-
-    /**
      * Determines the appropriate page to show in the Sync Setup UI based on
      * the state of the Sync backend. Does nothing if the user is not signed in.
      * @private
@@ -850,10 +842,6 @@ cr.define('options', function() {
   // These methods are for general consumption.
   SyncSetupOverlay.closeOverlay = function() {
     SyncSetupOverlay.getInstance().closeOverlay_();
-  };
-
-  SyncSetupOverlay.showErrorUI = function() {
-    SyncSetupOverlay.getInstance().showErrorUI_();
   };
 
   SyncSetupOverlay.showSetupUI = function() {
