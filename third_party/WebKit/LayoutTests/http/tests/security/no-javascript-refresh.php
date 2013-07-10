6e54e81ea3119568e5a75e44cab81a6141bb8f21
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-10 18:02:31 +0000

    Do not allow HTTP refresh headers to refresh to javascript: URLs.
    This behaviour has been standard in IE since IE7.  This makes us both
    more compatible and less vulnerable to XSS.
    
    BUG=258151
    R=abarth@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18865003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153912 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/http/tests/security/no-javascript-refresh.php
@@ -0,0 +1,14 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+<?php
+  header('HTTP/1.0 200 OK');
+  header('Content-type: text/html');
+  header('Refresh: 0;URL=javascript:window.top.location="about:blank"');
+?>
+<!DOCTYPE html>
+<body>
+<script>
+if (window.testRunner)
+    testRunner.dumpAsText();
+</script>
+This it the content that appears in place of a refresh.
+</body>
