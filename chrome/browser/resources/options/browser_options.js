f5ceb3f5387b3bbcf00c60a9e674799aa8e93798
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 16:35:00 +0000

    Fix bug with settings page.
    Don't show the sync section for a managed user on Chrome. Some elements there are only present on ChromeOS.
    
    BUG=258773
    TEST=Open Settings page with a supervised user profile.
    
    Review URL: https://chromiumcodereview.appspot.com/18083041
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210861 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/browser_options.js
+++ chrome/browser/resources/options/browser_options.js
@@ -699,7 +699,8 @@ cr.define('options', function() {
      * @private
      */
     updateSyncState_: function(syncData) {
-      if (!syncData.signinAllowed && !syncData.supervisedUser) {
+      if (!syncData.signinAllowed &&
+          (!syncData.supervisedUser || !cr.isChromeOS)) {
         $('sync-section').hidden = true;
         return;
       }
