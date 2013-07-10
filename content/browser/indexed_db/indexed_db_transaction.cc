a7ce50b9264a7383d356f0d340d04717594d5811
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 23:37:14 +0000

    IndexedDB: Remove transaction timer, just post tasks
    In the Blink implementation transactions used a timer to
    keep to schedule asynchronous processing of tasks. This
    was retained in the Chromium port, but with access to the
    message loop we can post tasks instead.
    
    Note that transactions still maintain an internal list of
    tasks and all that is posted is a "process the queue" task.
    Future changes may integrate the transaction task queue and
    the message loop more tightly.
    
    BUG=246629
    
    Review URL: https://chromiumcodereview.appspot.com/18612007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210969 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/indexed_db/indexed_db_transaction.cc
+++ content/browser/indexed_db/indexed_db_transaction.cc
@@ -5,7 +5,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/indexed_db/indexed_db_transaction.h"
 
+#include "base/bind.h"
 #include "base/logging.h"
+#include "base/message_loop/message_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/browser/indexed_db/indexed_db_backing_store.h"
 #include "content/browser/indexed_db/indexed_db_cursor.h"
@@ -77,6 +79,7 @@ IndexedDBTransaction::IndexedDBTransaction(
       callbacks_(callbacks),
       database_(database),
       transaction_(database->BackingStore().get()),
+      should_process_queue_(false),
       pending_preemptive_events_(0) {
   database_->transaction_coordinator().DidCreateTransaction(this);
 }
@@ -104,13 +107,14 @@ void IndexedDBTransaction::ScheduleTask(IndexedDBDatabase::TaskType type,
   if (abort_task)
     abort_task_stack_.push(abort_task);
 
-  if (state_ == UNUSED)
+  if (state_ == UNUSED) {
     Start();
-  else if (state_ == RUNNING && !task_timer_.IsRunning())
-    task_timer_.Start(FROM_HERE,
-                      base::TimeDelta::FromSeconds(0),
-                      this,
-                      &IndexedDBTransaction::TaskTimerFired);
+  } else if (state_ == RUNNING && !should_process_queue_) {
+    should_process_queue_ = true;
+    base::MessageLoop::current()->PostTask(
+        FROM_HERE,
+        base::Bind(&IndexedDBTransaction::ProcessTaskQueue, this));
+  }
 }
 
 void IndexedDBTransaction::Abort() {
@@ -131,7 +135,7 @@ void IndexedDBTransaction::Abort(const IndexedDBDatabaseError& error) {
   scoped_refptr<IndexedDBTransaction> protect(this);
 
   state_ = FINISHED;
-  task_timer_.Stop();
+  should_process_queue_ = false;
 
   if (was_running)
     transaction_.Rollback();
@@ -185,15 +189,14 @@ void IndexedDBTransaction::UnregisterOpenCursor(IndexedDBCursor* cursor) {
 }
 
 void IndexedDBTransaction::Run() {
-  // TransactionCoordinator has started this transaction. Schedule a timer
-  // to process the first task.
+  // TransactionCoordinator has started this transaction.
   DCHECK(state_ == START_PENDING || state_ == RUNNING);
-  DCHECK(!task_timer_.IsRunning());
+  DCHECK(!should_process_queue_);
 
-  task_timer_.Start(FROM_HERE,
-                    base::TimeDelta::FromSeconds(0),
-                    this,
-                    &IndexedDBTransaction::TaskTimerFired);
+  should_process_queue_ = true;
+  base::MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&IndexedDBTransaction::ProcessTaskQueue, this));
 }
 
 void IndexedDBTransaction::Start() {
@@ -262,9 +265,15 @@ void IndexedDBTransaction::Commit() {
   database_ = NULL;
 }
 
-void IndexedDBTransaction::TaskTimerFired() {
-  IDB_TRACE("IndexedDBTransaction::TaskTimerFired");
+void IndexedDBTransaction::ProcessTaskQueue() {
+  IDB_TRACE("IndexedDBTransaction::ProcessTaskQueue");
+
+  // May have been aborted.
+  if (!should_process_queue_)
+    return;
+
   DCHECK(!IsTaskQueueEmpty());
+  should_process_queue_ = false;
 
   if (state_ == START_PENDING) {
     transaction_.Begin();
