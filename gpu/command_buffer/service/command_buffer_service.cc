ac5e36f6e61229df77d5b71653489959d05a65ee
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 23:17:05 +0000

    add missing includes in gpu/command_buffer/*
    gpu/command_buffer/service/command_buffer_service.cc uses DCHECK* but
    doesn't include base/logging.h
    
    Similarly in gpu/command_buffer/service/transfer_buffer_manager.cc, which also lacks scoped_ptr.h.
    
    BUG=259043
    
    Review URL: https://chromiumcodereview.appspot.com/18998002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210963 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/command_buffer_service.cc
+++ gpu/command_buffer/service/command_buffer_service.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <limits>
 
+#include "base/logging.h"
 #include "base/process_util.h"
 #include "base/debug/trace_event.h"
 #include "gpu/command_buffer/common/cmd_buffer_common.h"
