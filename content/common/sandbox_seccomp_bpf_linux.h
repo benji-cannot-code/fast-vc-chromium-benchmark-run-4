d00bf43382798a5a7e5562846d5764db5f155826
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 00:50:52 +0000

    Linux Sandbox: forward declare Errorcode in sandbox_bpf_policy_forward.h
    sandbox_bpf_policy_forward.h can forward declare ErrorCode. This should fix
    a Chromium on MIPS compilation issue.
    
    BUG=130022
    NOTRY=true
    R=markus@chromium.org
    
    Review URL: https://codereview.chromium.org/18943004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210691 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_seccomp_bpf_linux.h
+++ content/common/sandbox_seccomp_bpf_linux.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CONTENT_COMMON_SANDBOX_SECCOMP_BPF_LINUX_H_
 #define CONTENT_COMMON_SANDBOX_SECCOMP_BPF_LINUX_H_
 
+#include <string>
+
 #include "base/basictypes.h"
 #include "sandbox/linux/seccomp-bpf/sandbox_bpf_policy_forward.h"
 
--- sandbox/linux/seccomp-bpf/sandbox_bpf_policy_forward.h
+++ sandbox/linux/seccomp-bpf/sandbox_bpf_policy_forward.h
@@ -7,12 +7,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define SANDBOX_LINUX_SECCOMP_BPF_SANDBOX_BPF_POLICY_FORWARD_H_
 
 #include "base/callback_forward.h"
-#include "sandbox/linux/seccomp-bpf/errorcode.h"
 
 namespace playground2 {
 
 class Sandbox;
-typedef playground2::ErrorCode BpfSandboxPolicy(
+class ErrorCode;
+typedef ErrorCode BpfSandboxPolicy(
     Sandbox* sandbox_compiler,
     int system_call_number,
     void* aux);
