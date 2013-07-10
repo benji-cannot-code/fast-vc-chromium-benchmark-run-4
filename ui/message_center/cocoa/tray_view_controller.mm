620b79a00286e55ed5e46ee0d2ec7235015c418a
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 06:33:01 +0000

    Mac: Highlight do-not-disturb button properly.
    Currently the notification center always displays the initial state
    of the do not disturb button as the off state.  This adds initializer
    code that sets the proper image when the message center is in DnD.
    
    R=rsesek@chromium.org
    BUG=256147
    
    Review URL: https://chromiumcodereview.appspot.com/18402009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210764 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/message_center/cocoa/tray_view_controller.mm
+++ ui/message_center/cocoa/tray_view_controller.mm
@@ -70,6 +70,9 @@ const int kBackButtonSize = 16;
 // When all visible notificatons slide out, re-enable controls and remove
 // notifications from the message center.
 - (void)finalizeClearAll;
+
+// Sets the images of the quiet mode button based on the message center state.
+- (void)updateQuietModeButtonImage;
 @end
 
 namespace {
@@ -231,18 +234,12 @@ const CGFloat kTrayBottomMargin = 75;
 }
 
 - (void)toggleQuietMode:(id)sender {
-  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
-  if (messageCenter_->IsQuietMode()) {
+  if (messageCenter_->IsQuietMode())
     messageCenter_->SetQuietMode(false);
-    [pauseButton_ setTrackingEnabled:YES];
-    [pauseButton_ setDefaultImage:
-        rb.GetNativeImageNamed(IDR_NOTIFICATION_DO_NOT_DISTURB).ToNSImage()];
-  } else {
+  else
     messageCenter_->EnterQuietModeWithExpire(base::TimeDelta::FromDays(1));
-    [pauseButton_ setTrackingEnabled:NO];
-    [pauseButton_ setDefaultImage: rb.GetNativeImageNamed(
-        IDR_NOTIFICATION_DO_NOT_DISTURB_PRESSED).ToNSImage()];
-  }
+
+  [self updateQuietModeButtonImage];
 }
 
 - (void)clearAllNotifications:(id)sender {
@@ -490,17 +487,13 @@ const CGFloat kTrayBottomMargin = 75;
   [view addSubview:clearAllButton_];
 
   // Create the pause button.
-  defaultImage = rb.GetNativeImageNamed(
-      IDR_NOTIFICATION_DO_NOT_DISTURB).ToNSImage();
   NSRect pauseButtonFrame = getButtonFrame(
       NSMinX(clearAllButtonFrame) - kButtonXMargin,
       defaultImage);
   pauseButton_.reset([[HoverImageButton alloc] initWithFrame:pauseButtonFrame]);
-  [pauseButton_ setDefaultImage:defaultImage];
+  [self updateQuietModeButtonImage];
   [pauseButton_ setHoverImage: rb.GetNativeImageNamed(
       IDR_NOTIFICATION_DO_NOT_DISTURB_HOVER).ToNSImage()];
-  [pauseButton_ setPressedImage: rb.GetNativeImageNamed(
-      IDR_NOTIFICATION_DO_NOT_DISTURB_PRESSED).ToNSImage()];
   [pauseButton_ setToolTip:
       l10n_util::GetNSString(IDS_MESSAGE_CENTER_QUIET_MODE_BUTTON_TOOLTIP)];
   [[pauseButton_ cell]
@@ -735,6 +728,19 @@ const CGFloat kTrayBottomMargin = 75;
   [clipView_ setFrozen:NO];
 
   messageCenter_->RemoveAllNotifications(true);
+}
+
+- (void)updateQuietModeButtonImage {
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  if (messageCenter_->IsQuietMode()) {
+    [pauseButton_ setTrackingEnabled:NO];
+    [pauseButton_ setDefaultImage: rb.GetNativeImageNamed(
+        IDR_NOTIFICATION_DO_NOT_DISTURB_PRESSED).ToNSImage()];
+  } else {
+    [pauseButton_ setTrackingEnabled:YES];
+    [pauseButton_ setDefaultImage:
+        rb.GetNativeImageNamed(IDR_NOTIFICATION_DO_NOT_DISTURB).ToNSImage()];
+  }
 }
 
 @end
