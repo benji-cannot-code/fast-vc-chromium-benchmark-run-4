3cda4acfb611770e131ae87905888dc680bdf0da
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 07:55:18 +0000

    Add on_close_callback to AsyncFileUtil::CreateOrOpenCallback.
    This is preparation of the migration between LocalFileSystemOperation and
    RemoteFileSytemOperation. Especially, on ChromeOS Drive File System, it is
    necessary to call CloseFile on file closing, but currently we have no way
    to do it with AsyncFileUtil.
    This CL adds a callback for the behavior. The Drive File System side
    implementation will be done in another CL.
    
    BUG=258303
    TEST=Ran unit_tests and broser_tests --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*"
    
    Review URL: https://chromiumcodereview.appspot.com/18856009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210785 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/async_file_util_adapter.cc
+++ webkit/browser/fileapi/async_file_util_adapter.cc
@@ -132,7 +132,7 @@ void RunCreateOrOpenCallback(
     base::PlatformFileError result,
     base::PassPlatformFile file,
     bool created) {
-  callback.Run(result, file);
+  callback.Run(result, file, base::Closure());
 }
 
 }  // namespace
