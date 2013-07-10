820741bb3d992eee7d8aa417a9166965abb28515
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 13:13:35 +0000

    bookmarks: Simplify BookmarkLoadObserver implementation.
    It's easier and simpler to just inherit from BaseBookmarkModelObserver, since
    all we care there is about the Loaded() event, nothing more, so we don't have
    to have a bunch of empty overrides.
    
    BUG=144783
    TEST=unit_tests
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/18606002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210828 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/base/bookmark_load_observer.cc
+++ chrome/test/base/bookmark_load_observer.cc
@@ -5,12 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/test/base/bookmark_load_observer.h"
 
-#include "chrome/test/base/ui_test_utils.h"
-
 BookmarkLoadObserver::BookmarkLoadObserver(const base::Closure& quit_task)
     : quit_task_(quit_task) {}
 
 BookmarkLoadObserver::~BookmarkLoadObserver() {}
+
+void BookmarkLoadObserver::BookmarkModelChanged() {}
 
 void BookmarkLoadObserver::Loaded(BookmarkModel* model, bool ids_reassigned) {
   quit_task_.Run();
