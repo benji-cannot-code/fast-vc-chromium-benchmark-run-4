cb86fa83ecf733b7aacd9d297fd267832f8b42f7
zhchbin@gmail.com <zhchbin@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 06:31:17 +0000

    Use scoped_ptr for ShellWindowLinkDelegate to prevent memory leak.
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/18051015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210761 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/apps/chrome_shell_window_delegate.h
+++ chrome/browser/ui/apps/chrome_shell_window_delegate.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_UI_APPS_CHROME_SHELL_WINDOW_DELEGATE_H_
 
 #include "apps/shell_window.h"
+#include "base/memory/scoped_ptr.h"
 #include "chrome/browser/profiles/profile.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
@@ -14,6 +15,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/gfx/rect.h"
 
 namespace chrome {
+
+class ShellWindowLinkDelegate : public content::WebContentsDelegate {
+ public:
+  ShellWindowLinkDelegate();
+  virtual ~ShellWindowLinkDelegate();
+
+ private:
+  virtual content::WebContents* OpenURLFromTab(
+      content::WebContents* source,
+      const content::OpenURLParams& params) OVERRIDE;
+
+  DISALLOW_COPY_AND_ASSIGN(ShellWindowLinkDelegate);
+};
 
 class ChromeShellWindowDelegate : public apps::ShellWindow::Delegate {
  public:
@@ -51,6 +65,8 @@ class ChromeShellWindowDelegate : public apps::ShellWindow::Delegate {
                                      bool blocked) OVERRIDE;
   virtual bool IsWebContentsVisible(
       content::WebContents* web_contents) OVERRIDE;
+
+  scoped_ptr<ShellWindowLinkDelegate> shell_window_link_delegate_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromeShellWindowDelegate);
 };
