d4151e97717978c9de8b977623501fd6d4aa7321
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 04:18:15 +0000

    Revert 210731 "Host offline status reporting."
    Broke the build: maybe base::Time was indirectly resolved but the indirection was gone?
    
    http://build.chromium.org/p/chromium.linux/builders/Linux%20Builder/builds/10505/steps/compile/logs/stdio
    
    ../../remoting/host/host_status_sender_unittest.cc: In member function 'void remoting::HostStatusSenderTest::ValidateSignature(buzz::XmlElement*, remoting::HostStatusSender::HostStatus)':
    ../../remoting/host/host_status_sender_unittest.cc:195:40: error: 'base::Time' has not been declared
    
    
    > Host offline status reporting.
    > 
    > This is change #1 of the Host offline status reporting work: 
    > 1. Define the structure of the offline status message.
    > 2. The host sends the offline status message if the underlying XMPP connection is live.
    > 
    > 
    > BUG=247245
    > 
    > Review URL: https://chromiumcodereview.appspot.com/18075003
    
    TBR=weitaosu@chromium.org
    
    Review URL: https://codereview.chromium.org/18357005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210737 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/protocol/name_value_map.h
+++ remoting/protocol/name_value_map.h
@@ -5,10 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 //
 // Helper functions that allow to map enum values to strings.
 
-#include <stddef.h>
-
-#include "base/logging.h"
-
 namespace remoting {
 namespace protocol {
 
