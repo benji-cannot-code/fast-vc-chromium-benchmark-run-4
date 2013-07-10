1e4caac5fef2ce52a70d4b32edb6bb74eb33ea1f
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-10 00:35:18 +0000

    Include the subprocess output in ScriptError's message.
    This is useful for when a nested webkit-patch call throws a ScriptError.
    Instead of just getting a stacktrace, you also get the output from the
    webkit-patch command.
    
    R=dpranke@chromium.org
    
    Review URL: https://codereview.chromium.org/18964002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153828 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/workspace_unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/workspace_unittest.py
@@ -64,7 +64,7 @@ class WorkspaceTest(unittest.TestCase):
 Workspace.create_zip failed in /source/path:
 MOCK ScriptError
 
-MOCK output of child process
+output: MOCK output of child process
 """
         class MockZipFile(object):
             def __init__(self, path):
