837a4aeb31c7a0bb47162214797578aefc585971
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 22:11:03 +0000

    Fix compile break due to last-minute .h revert.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210948 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/debugger/debugger_api.cc
+++ chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/render_widget_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/browser/worker_service.h"
 #include "content/public/common/content_client.h"
 #include "content/public/common/url_utils.h"
 #include "extensions/common/error_utils.h"
