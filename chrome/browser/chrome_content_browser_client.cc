9c09824a148024d64edea6a9e897824575c36c27
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 02:08:48 +0000

    Prevent DesktopNotificationService API call during shutdown.
    Sometimes webkit notification API may be called after the main message
    loop runs, since CheckDesktopNotificationPermission runs in IO thread.
    This can unsafely touch already-released-objects. This method should be
    ignored in such case.
    
    BUG=256638
    R=dewittj@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18615012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210714 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/app_mode/app_mode_utils.h"
 #include "chrome/browser/browser_about_handler.h"
 #include "chrome/browser/browser_process.h"
+#include "chrome/browser/browser_shutdown.h"
 #include "chrome/browser/browsing_data/browsing_data_helper.h"
 #include "chrome/browser/browsing_data/browsing_data_remover.h"
 #include "chrome/browser/character_encoding.h"
@@ -1805,6 +1806,11 @@ WebKit::WebNotificationPresenter::Permission
         int render_process_id) {
 #if defined(ENABLE_NOTIFICATIONS)
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  // Sometimes a notification may be invoked during the shutdown.
+  // See http://crbug.com/256638
+  if (browser_shutdown::IsTryingToQuit())
+    return WebKit::WebNotificationPresenter::PermissionNotAllowed;
+
   ProfileIOData* io_data = ProfileIOData::FromResourceContext(context);
 
   DesktopNotificationService* notification_service =
