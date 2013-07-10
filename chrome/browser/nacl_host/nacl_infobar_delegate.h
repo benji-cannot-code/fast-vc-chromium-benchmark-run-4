86f97269e972029ff4203f8d7b7a99950c270b01
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 00:58:00 +0000

    Misc. NaCl infobar cleanup, part 2.
    * Remove unneeded member
    * Remove using directives
    * Shorten/simplify
    
    BUG=none
    TEST=none
    R=jvoung@chromium.org
    
    Review URL: https://codereview.chromium.org/18340010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210692 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/nacl_infobar_delegate.h
+++ chrome/browser/nacl_host/nacl_infobar_delegate.h
@@ -8,10 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/infobars/confirm_infobar_delegate.h"
 
-namespace content {
-class WebContents;
-}
-
 class NaClInfoBarDelegate : public ConfirmInfoBarDelegate {
  public:
   // Creates a NaCl delegate and adds it to the infobar service corresponding to
@@ -19,15 +15,13 @@ class NaClInfoBarDelegate : public ConfirmInfoBarDelegate {
   static void Create(int render_process_id, int render_view_id);
 
  private:
-  NaClInfoBarDelegate(content::WebContents* wc, InfoBarService* ibs) :
-      ConfirmInfoBarDelegate(ibs), wc_(wc) {}
+  explicit NaClInfoBarDelegate(InfoBarService* infobar_service);
+  virtual ~NaClInfoBarDelegate();
 
   virtual string16 GetMessageText() const OVERRIDE;
   virtual int GetButtons() const OVERRIDE;
   virtual string16 GetLinkText() const OVERRIDE;
   virtual bool LinkClicked(WindowOpenDisposition disposition) OVERRIDE;
-
-  content::WebContents* wc_;
 
   DISALLOW_COPY_AND_ASSIGN(NaClInfoBarDelegate);
 };
