237542824c6da58eeea16fec715db5d32d1a4c08
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-10 18:36:09 +0000

    Split out process killing functions from base/process_util.h into base/process/kill.h.
    BUG=242290
    R=brettw@chromium.org
    
    Review URL: https://codereview.chromium.org/18555002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210893 0039d316-1c4b-4281-b951-d872f2087c98

--- base/process/kill.cc
+++ base/process/kill.cc
@@ -1,13 +1,16 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2011 The Chromium Authors. All rights reserved.
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "base/process_util.h"
+#include "base/process/kill.h"
+
+#include "base/process/process_iterator.h"
 
 namespace base {
 
-bool KillProcesses(const FilePath::StringType& executable_name, int exit_code,
+bool KillProcesses(const FilePath::StringType& executable_name,
+                   int exit_code,
                    const ProcessFilter* filter) {
   bool result = true;
   NamedProcessIterator iter(executable_name, filter);
