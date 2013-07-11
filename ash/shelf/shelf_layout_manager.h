ba601835303103991423ceccee09190fc7c240ef
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 09:32:58 +0000

    Sets the background color of shelf is opaque black when maximized (2nd)
    Previously the black color was achieved by hiding the desktop background.
    Now shelf's background color is opaque black instead.
    
    The first patch (r210763) was reverted due to test failure in win8.
    
    BUG=252422
    TEST=covered by the new tests
    TBR=jamescook@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18637004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211053 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/shelf/shelf_layout_manager.h
+++ ash/shelf/shelf_layout_manager.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/shelf/shelf_types.h"
 #include "ash/shell_observer.h"
 #include "ash/system/status_area_widget.h"
+#include "ash/wm/workspace/workspace_types.h"
 #include "base/basictypes.h"
 #include "base/compiler_specific.h"
 #include "base/logging.h"
@@ -93,6 +94,9 @@ class ASH_EXPORT ShelfLayoutManager :
   }
 
   bool in_layout() const { return in_layout_; }
+
+  // Clears internal data for shutdown process.
+  void PrepareForShutdown();
 
   // Returns whether the shelf and its contents (launcher, status) are visible
   // on the screen.
@@ -218,6 +222,7 @@ class ASH_EXPORT ShelfLayoutManager :
   struct State {
     State() : visibility_state(SHELF_VISIBLE),
               auto_hide_state(SHELF_AUTO_HIDE_HIDDEN),
+              window_state(WORKSPACE_WINDOW_STATE_DEFAULT),
               is_screen_locked(false) {}
 
     // Returns true if the two states are considered equal. As
@@ -228,11 +233,13 @@ class ASH_EXPORT ShelfLayoutManager :
       return other.visibility_state == visibility_state &&
           (visibility_state != SHELF_AUTO_HIDE ||
            other.auto_hide_state == auto_hide_state) &&
+          other.window_state == window_state &&
           other.is_screen_locked == is_screen_locked;
     }
 
     ShelfVisibilityState visibility_state;
     ShelfAutoHideState auto_hide_state;
+    WorkspaceWindowState window_state;
     bool is_screen_locked;
   };
 
@@ -259,8 +266,8 @@ class ASH_EXPORT ShelfLayoutManager :
   // Updates the background of the shelf.
   void UpdateShelfBackground(BackgroundAnimator::ChangeType type);
 
-  // Returns whether the launcher should draw a background.
-  bool GetLauncherPaintsBackground() const;
+  // Returns how the shelf background is painted.
+  ShelfBackgroundType GetShelfBackgroundType() const;
 
   // Updates the auto hide state immediately.
   void UpdateAutoHideStateNow();
