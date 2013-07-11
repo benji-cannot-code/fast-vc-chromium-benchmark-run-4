bf247f6b2d626af565d4b84adb44ac4b3649dc95
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 00:48:39 +0000

    Drag drop test for <webview> for chromeos. This test drags an element to another element within a <webview>. Test seems stable on multiple trybot runs (~250 times).
    BUG=161112
    Test=|self|
    
    Review URL: https://chromiumcodereview.appspot.com/18291007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@210992 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/platform_apps/web_view/dnd_within_webview/main.js
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+window.lastDropData = 'Uninitialized';
+
+// Called from web_view_interactive_ui_tests.cc.
+window.getLastDropData = function() {
+  return window.lastDropData;
+};
+
+var startTest = function() {
+  window.addEventListener('message', onPostMessageReceived, false);
+  var webview = document.getElementById('webview');
+  webview.addEventListener('loadstop', function(e) {
+    webview.contentWindow.postMessage(
+        JSON.stringify(['create-channel']), '*');
+  });
+};
+
+var onPostMessageReceived = function(e) {
+  window.console.log('embedder.onPostMessageReceived');
+  var data = JSON.parse(e.data);
+  window.console.log('data: ' + data);
+  switch (data[0]) {
+    case 'connected':
+      chrome.test.sendMessage('connected');
+      break;
+    case 'guest-got-drop':
+      window.lastDropData = data[1];
+      chrome.test.sendMessage('guest-got-drop');
+      break;
+    default:
+      window.console.log('ERR: curious message received in emb: ' + data);
+      break;
+  }
+};
+
+chrome.test.getConfig(function(config) {
+  var guestURL = 'http://localhost:' + config.testServer.port +
+      '/extensions/platform_apps/web_view/dnd_within_webview/guest.html';
+  document.querySelector('#webview-tag-container').innerHTML =
+      '<webview id=\'webview\' style="width: 300px; height: 150px; ' +
+      'margin: 0; padding: 0;"' +
+      ' src="' + guestURL + '"' +
+      '></webview>';
+  startTest();
+});
