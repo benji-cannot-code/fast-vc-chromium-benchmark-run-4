b4323e7057fccf9043df98b2483120847ac0c168
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 23:16:33 +0000

    [Autofill] Remove last instance of old Autofill UI.
    This was the last location where the old UI could get triggered from.
    
    BUG=257034
    
    Review URL: https://chromiumcodereview.appspot.com/18787002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211247 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autofill_manager_unittest.cc
+++ components/autofill/core/browser/autofill_manager_unittest.cc
@@ -69,12 +69,6 @@ namespace {
 // an IPC message back to the renderer.
 const int kDefaultPageID = 137;
 
-typedef Tuple5<int,
-               std::vector<base::string16>,
-               std::vector<base::string16>,
-               std::vector<base::string16>,
-               std::vector<int> > AutofillParam;
-
 class TestPersonalDataManager : public PersonalDataManager {
  public:
   TestPersonalDataManager() : PersonalDataManager("en-US") {
