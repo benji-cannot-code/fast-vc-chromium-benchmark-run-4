benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit 9c990e87e10114c5e6715be01632e71f29815786

Do not focus on button controls by mouse press in the options page.
Buttons are mouse-focusable now because of Blink r153039. However we
want to keep the existing behavior in WebUI pages. So, FocusManger prevents
mouse focus of buttons by default.

BUG=256123
R=arv@chromium.org

Review URL: https://codereview.chromium.org/18463002

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@209817 0039d316-1c4b-4281-b951-d872f2087c98

ui/webui/resources/js/cr/ui/focus_manager.js
@@ -178,6 +178,17 @@ cr.define('cr.ui', function() {
           true);
       document.addEventListener('keydown', this.onDocumentKeyDown_.bind(this),
           true);
+
+      document.addEventListener('mousedown', function(event) {
+        var tagName = event.target.tagName;
+        if (tagName != 'BUTTON' && tagName != 'INPUT')
+          return;
+        var type = event.target.type;
+        if (type == 'button' || type == 'reset' || type == 'submit' ||
+            type == 'radio' || type == 'checkbox') {
+          event.preventDefault();
+        }
+      }, false);
     },
   };
 

commit 2c8d1116da0f054f63d9cea738cbbfaed4a58e72

Settings: Home page url remains even after deleting the url.
The bug is a regression by r209817.
HomePageOverlay assumes a 'change' event is dispatched when 'OK'
button is pressed. 'blur' event for a URL field, which triggers a
'change' event, has never been dispatched since r209817. This CL
corrects the behavior so that mousedown on buttons remove focus of
another form control.

BUG=258345
R=arv@chromium.org

Review URL: https://codereview.chromium.org/18603015

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211231 0039d316-1c4b-4281-b951-d872f2087c98

ui/webui/resources/js/cr/ui/focus_manager.js
@@ -202,6 +202,8 @@ cr.define('cr.ui', function() {
       var type = node.type;
       if (type == 'button' || type == 'reset' || type == 'submit' ||
           type == 'radio' || type == 'checkbox') {
+        if (document.activeElement != node)
+          document.activeElement.blur();
         event.preventDefault();
       }
     }, false);