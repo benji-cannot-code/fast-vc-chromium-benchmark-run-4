c1e10c40825e654b1684bb589b11291f2b33e3af
apavlov@chromium.org <apavlov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-11 08:01:46 +0000

    Revert "Ensure that ArrayBuffers actively being used by the Web Audio API cannot be neutered"
    The original change resulted in a few webaudio/{audiobuffersource,up-mixing}* layout tests
    timing out on SnowLeopard and Lucid64 debug bots.
    
    This reverts commit 2e7aade768be780e3cd103b4204b8684a1cc800f.
    
    TBR=crogers@google.com
    
    Review URL: https://codereview.chromium.org/18860006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153985 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webaudio/AudioBuffer.cpp
+++ third_party/WebKit/Source/modules/webaudio/AudioBuffer.cpp
@@ -67,7 +67,6 @@ AudioBuffer::AudioBuffer(unsigned numberOfChannels, size_t numberOfFrames, float
 
     for (unsigned i = 0; i < numberOfChannels; ++i) {
         RefPtr<Float32Array> channelDataArray = Float32Array::create(m_length);
-        channelDataArray->setNeuterable(false);
         m_channels.append(channelDataArray);
     }
 }
@@ -83,7 +82,6 @@ AudioBuffer::AudioBuffer(AudioBus* bus)
     m_channels.reserveCapacity(numberOfChannels);
     for (unsigned i = 0; i < numberOfChannels; ++i) {
         RefPtr<Float32Array> channelDataArray = Float32Array::create(m_length);
-        channelDataArray->setNeuterable(false);
         channelDataArray->setRange(bus->channel(i)->data(), m_length, 0);
         m_channels.append(channelDataArray);
     }
@@ -94,15 +92,14 @@ void AudioBuffer::releaseMemory()
     m_channels.clear();
 }
 
-PassRefPtr<Float32Array> AudioBuffer::getChannelData(unsigned channelIndex, ExceptionCode& ec)
+Float32Array* AudioBuffer::getChannelData(unsigned channelIndex, ExceptionCode& ec)
 {
     if (channelIndex >= m_channels.size()) {
         ec = SyntaxError;
         return 0;
     }
 
-    Float32Array* channelData = m_channels[channelIndex].get();
-    return Float32Array::create(channelData->buffer(), channelData->byteOffset(), channelData->length());
+    return m_channels[channelIndex].get();
 }
 
 Float32Array* AudioBuffer::getChannelData(unsigned channelIndex)
