6254fddf1073d1970ee0da24f6b95c11bea5cf0e
noelallen@chromium.org <noelallen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 18:40:26 +0000

    [NaCl SDK] Add TC includes, and Key expansion in make
    Fix missing expansion for dictioray: KEY = $(($TOOLCHAIN)_KEY))
    Add toolchain specific include paths to support adding glibc compatibility to newlib/pnacl toolchains.
    
    BUG=none
    NOTRY=true
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/18137009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211174 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/build_tools/library.mk
+++ native_client_sdk/src/build_tools/library.mk
@@ -21,7 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 [[    out.extend(post)]]
 {{key}}_{{subkey}} = {{' '.join(out)}}
 [[  ]]
-{{key}} = {{key}}_$(TOOLCHAIN)
+{{key}} = $({{key}}_$(TOOLCHAIN))
 [[]]
 
 [[target = targets[0] ]]
