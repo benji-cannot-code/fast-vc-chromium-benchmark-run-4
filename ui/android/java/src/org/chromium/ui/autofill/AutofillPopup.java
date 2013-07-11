4c9699a4129d9b7933e58dbd92cbe5f8233ae9a0
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 15:22:51 +0000

    [Android] Don't use keyed tag for AutoFill tests.
    The version of the View.setTag API that takes a key mandates
    that the key must be an application specific resource ID. This
    is fine for chromium builds but breaks webview at
    runtime, as WebView isn't itself an application.
    
    Work around this issue by attaching a single tag without a key.
    
    Review URL: https://chromiumcodereview.appspot.com/18624003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211119 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/android/java/src/org/chromium/ui/autofill/AutofillPopup.java
+++ ui/android/java/src/org/chromium/ui/autofill/AutofillPopup.java
@@ -84,7 +84,7 @@ public class AutofillPopup extends ListPopupWindow implements AdapterView.OnItem
 
         mAnchorView = mViewAndroidDelegate.acquireAnchorView();
         mAnchorView.setId(R.id.autofill_popup_window);
-        mAnchorView.setTag(R.id.autofill_popup_window, this);
+        mAnchorView.setTag(this);
 
         mViewAndroidDelegate.setAnchorViewPosition(mAnchorView, mAnchorX, mAnchorY, mAnchorWidth,
                 mAnchorHeight);
@@ -162,7 +162,7 @@ public class AutofillPopup extends ListPopupWindow implements AdapterView.OnItem
     public void hide() {
         super.dismiss();
         mAnchorView.removeOnLayoutChangeListener(mLayoutChangeListener);
-        mAnchorView.setTag(R.id.autofill_popup_window, null);
+        mAnchorView.setTag(null);
         mViewAndroidDelegate.releaseAnchorView(mAnchorView);
     }
 
