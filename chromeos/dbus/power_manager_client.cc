a1c5331ed9f7df079f037410ffefa35e80595852
jfweitz@chromium.org <jfweitz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 01:34:21 +0000

    Start PowerManagerClientStub timer only if the stub flag is set
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/18292026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211005 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/dbus/power_manager_client.cc
+++ chromeos/dbus/power_manager_client.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/callback.h"
+#include "base/command_line.h"
 #include "base/format_macros.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -18,6 +19,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/threading/platform_thread.h"
 #include "base/time/time.h"
 #include "base/timer/timer.h"
+#include "chromeos/chromeos_switches.h"
 #include "chromeos/dbus/power_manager/input_event.pb.h"
 #include "chromeos/dbus/power_manager/peripheral_battery_status.pb.h"
 #include "chromeos/dbus/power_manager/policy.pb.h"
@@ -662,10 +664,13 @@ class PowerManagerClientStubImpl : public PowerManagerClient {
         pause_count_(2),
         cycle_count_(0),
         weak_ptr_factory_(this) {
-    const int kStatusUpdateMs = 1000;
-    update_timer_.Start(FROM_HERE,
-        base::TimeDelta::FromMilliseconds(kStatusUpdateMs), this,
-        &PowerManagerClientStubImpl::UpdateStatus);
+    if (CommandLine::ForCurrentProcess()->HasSwitch(
+        chromeos::switches::kEnableStubInteractive)) {
+      const int kStatusUpdateMs = 1000;
+      update_timer_.Start(FROM_HERE,
+          base::TimeDelta::FromMilliseconds(kStatusUpdateMs), this,
+          &PowerManagerClientStubImpl::UpdateStatus);
+    }
   }
 
   virtual ~PowerManagerClientStubImpl() {}
