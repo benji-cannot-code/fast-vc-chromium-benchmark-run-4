b85fc31ea582d6a6500a4203504cc19bc432731d
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-11 17:54:38 +0000

    checkTextOfParagraph shouldn't require bloatedCharacters
    This CL changes checkTextOfParagraph to accept a String rather than a UChar*.
    That means the callers don't need to call bloatedCharacters. Internally,
    checkTextOfParagraph does copy the data out into a Vector<UChar> in order to
    interface with wordBreakIterator.
    
    R=eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/19032002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154027 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/editing/TextCheckingHelper.h
+++ third_party/WebKit/Source/core/editing/TextCheckingHelper.h
@@ -44,9 +44,9 @@ public:
     int offsetTo(const Position&, ExceptionCode&) const;
     void expandRangeToNextEnd();
 
-    int textLength() const { return text().length(); }
+    const String& text() const;
+    // Why not let clients call these functions on text() themselves?
     String textSubstring(unsigned pos, unsigned len = INT_MAX) const { return text().substring(pos, len); }
-    const UChar* textCharacters() const { return text().bloatedCharacters(); }
     UChar textCharAt(int index) const { return text()[static_cast<unsigned>(index)]; }
 
     bool isEmpty() const;
@@ -65,7 +65,6 @@ private:
     void invalidateParagraphRangeValues();
     PassRefPtr<Range> checkingRange() const { return m_checkingRange; }
     PassRefPtr<Range> offsetAsRange() const;
-    const String& text() const;
 
     RefPtr<Range> m_checkingRange;
     mutable RefPtr<Range> m_paragraphRange;
@@ -96,8 +95,7 @@ private:
     bool unifiedTextCheckerEnabled() const;
 };
 
-void checkTextOfParagraph(TextCheckerClient*, const UChar* text, int length,
-    TextCheckingTypeMask checkingTypes, Vector<TextCheckingResult>& results);
+void checkTextOfParagraph(TextCheckerClient*, const String&, TextCheckingTypeMask, Vector<TextCheckingResult>&);
 
 bool unifiedTextCheckerEnabled(const Frame*);
 
