bfbae0222a6212e79e2ad8aa617e24ff96e53995
petewil@chromium.org <petewil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-11 19:58:05 +0000

    Add implementation and tests for clicking on a notification.
    To round out the feature of clicking on the body of a notification or on a
    text button, we should navigate to the appropriate URL for the background
    or button.  This change adds code to do that, and the proper browser tests.
    
    This change also refactors the button code to be able to accept an arbitrary
    number of buttons from the synced notification.  Note that Rich Notifications
    is still limited to two buttons, though, so only two buttons are passed to Rich
    Notifications.
    
    This change also includes a refactoring to return GURL everywhere we have a
    URL instead of returning a string.
    
    BUG=247564
    
    Review URL: https://chromiumcodereview.appspot.com/17450021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211193 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/sync_notifier/chrome_notifier_service.cc
+++ chrome/browser/notifications/sync_notifier/chrome_notifier_service.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/notifications/notification.h"
 #include "chrome/browser/notifications/notification_ui_manager.h"
 #include "chrome/browser/profiles/profile.h"
+#include "googleurl/src/gurl.h"
 #include "grit/ui_strings.h"
 #include "sync/api/sync_change.h"
 #include "sync/api/sync_change_processor.h"
@@ -77,7 +78,7 @@ syncer::SyncMergeResult ChromeNotifierService::MergeDataAndStartSyncing(
     // Process each incoming remote notification.
     const std::string& key = incoming->GetKey();
     DCHECK_GT(key.length(), 0U);
-    SyncedNotification* found = FindNotificationByKey(key);
+    SyncedNotification* found = FindNotificationById(key);
 
     if (NULL == found) {
       // If there are no conflicts, copy in the data from remote.
@@ -249,8 +250,8 @@ scoped_ptr<SyncedNotification>
 
 // This returns a pointer into a vector that we own.  Caller must not free it.
 // Returns NULL if no match is found.
-SyncedNotification* ChromeNotifierService::FindNotificationByKey(
-    const std::string& key) {
+SyncedNotification* ChromeNotifierService::FindNotificationById(
+    const std::string& notification_id) {
   // TODO(petewil): We can make a performance trade off here.
   // While the vector has good locality of reference, a map has faster lookup.
   // Based on how big we expect this to get, maybe change this to a map.
@@ -259,7 +260,7 @@ SyncedNotification* ChromeNotifierService::FindNotificationByKey(
       it != notification_data_.end();
       ++it) {
     SyncedNotification* notification = *it;
-    if (key == notification->GetKey())
+    if (notification_id == notification->GetKey())
       return *it;
   }
 
@@ -289,7 +290,7 @@ void ChromeNotifierService::GetSyncedNotificationServices(
 
 void ChromeNotifierService::MarkNotificationAsDismissed(
     const std::string& key) {
-  SyncedNotification* notification = FindNotificationByKey(key);
+  SyncedNotification* notification = FindNotificationById(key);
   CHECK(notification != NULL);
 
   notification->NotificationHasBeenDismissed();
