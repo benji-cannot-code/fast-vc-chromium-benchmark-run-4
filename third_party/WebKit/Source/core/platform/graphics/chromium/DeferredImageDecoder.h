970352e0db4963770e449ae525aeb1e03d32307f
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-11 00:50:12 +0000

    Reland change to enable deferred image decoding for animated GIFs
    I landed changes to avoid crashes on debug build. Additional change in
    this CL is to avoid deferred image decoding for ICO images which
    prevents an ASSERT.
    
    Tested this change on both release and debug builds. No layout test is
    crashing.
    
    BUG=169377
    
    Review URL: https://chromiumcodereview.appspot.com/18292024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@153949 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
+++ third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
@@ -31,7 +31,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/ImageSource.h"
 #include "core/platform/graphics/IntSize.h"
 #include "core/platform/image-decoders/ImageDecoder.h"
-#include <wtf/Forward.h>
+#include "wtf/Forward.h"
+#include "wtf/OwnPtr.h"
+#include "wtf/Vector.h"
 
 namespace WebCore {
 
@@ -72,7 +74,9 @@ public:
 
 private:
     explicit DeferredImageDecoder(PassOwnPtr<ImageDecoder> actualDecoder);
-    SkBitmap createLazyDecodingBitmap();
+    void prepareLazyDecodedFrames();
+    SkBitmap createLazyDecodingBitmap(size_t index);
+    void activateLazyDecoding();
     void setData(PassRefPtr<SharedBuffer>, bool allDataReceived);
 
     RefPtr<SharedBuffer> m_data;
@@ -82,8 +86,9 @@ private:
     String m_filenameExtension;
     IntSize m_size;
     ImageOrientation m_orientation;
+    int m_repetitionCount;
 
-    ImageFrame m_lazyDecodedFrame;
+    Vector<OwnPtr<ImageFrame> > m_lazyDecodedFrames;
     RefPtr<ImageFrameGenerator> m_frameGenerator;
 
     static bool s_enabled;
