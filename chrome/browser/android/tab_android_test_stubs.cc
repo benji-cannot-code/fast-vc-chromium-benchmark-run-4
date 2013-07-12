c4baee502d333acecef2bb294a3ab0b02d1cfa4a
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 00:20:50 +0000

    Other half of Android build fix.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/tab_android_test_stubs.cc
+++ chrome/browser/android/tab_android_test_stubs.cc
@@ -27,7 +27,7 @@ TabAndroid* TabAndroid::GetNativeTab(JNIEnv* env, jobject obj) {
 AutoLoginInfoBarDelegateAndroid::AutoLoginInfoBarDelegateAndroid(
     InfoBarService* owner,
     const AutoLoginInfoBarDelegate::Params& params)
-    : AutoLoginInfoBarDelegate(owner, params) {}
+    : AutoLoginInfoBarDelegate(owner, params), params_() {}
 
 AutoLoginInfoBarDelegateAndroid::~AutoLoginInfoBarDelegateAndroid() {}
 
