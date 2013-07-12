5d2fc936ee7f73cadc11b2caea182806cb6c1f8b
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 08:22:18 +0000

    Log network events in NetworkingPrivateEventRouter
    Temporary measure to track down a bug. This will only log events whe an
    extension adds itself as a listener so normally will not have any 
    effect.
    
    BUG=256881
    
    Review URL: https://chromiumcodereview.appspot.com/19054015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211366 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/networking_private_event_router.cc
+++ chrome/browser/chromeos/extensions/networking_private_event_router.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_system_factory.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/common/extensions/api/networking_private.h"
+#include "chromeos/network/network_event_log.h"
 #include "chromeos/network/network_state.h"
 #include "chromeos/network/network_state_handler.h"
 #include "chromeos/network/onc/onc_constants.h"
@@ -97,8 +98,14 @@ void NetworkingPrivateEventRouter::NetworkListChanged() {
   EventRouter* event_router = ExtensionSystem::Get(profile_)->event_router();
   NetworkStateHandler::NetworkStateList networks;
   NetworkHandler::Get()->network_state_handler()->GetNetworkList(&networks);
-  if (!event_router->HasEventListener(kOnNetworkListChanged))
+  if (!event_router->HasEventListener(kOnNetworkListChanged)) {
+    // TODO(stevenjb): Remove logging once crbug.com/256881 is fixed
+    // (or at least reduce to LOG_DEBUG). Same with NET_LOG events below.
+    NET_LOG_EVENT("NetworkingPrivate.NetworkListChanged: No Listeners", "");
     return;
+  }
+
+  NET_LOG_EVENT("NetworkingPrivate.NetworkListChanged", "");
 
   std::vector<std::string> changes;
   for (NetworkStateHandler::NetworkStateList::const_iterator iter =
@@ -118,9 +125,13 @@ void NetworkingPrivateEventRouter::NetworkListChanged() {
 void NetworkingPrivateEventRouter::NetworkPropertiesUpdated(
     const NetworkState* network) {
   EventRouter* event_router = ExtensionSystem::Get(profile_)->event_router();
-  if (!event_router->HasEventListener(kOnNetworksChanged))
+  if (!event_router->HasEventListener(kOnNetworksChanged)) {
+    NET_LOG_EVENT("NetworkingPrivate.NetworkPropertiesUpdated: No Listeners",
+                  network->path());
     return;
-
+  }
+  NET_LOG_EVENT("NetworkingPrivate.NetworkPropertiesUpdated",
+                network->path());
   scoped_ptr<base::ListValue> args(api::OnNetworksChanged::Create(
       std::vector<std::string>(1, network->path())));
   scoped_ptr<extensions::Event> extension_event(
