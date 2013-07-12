607b9e903d3e345bdb506407b19087c32156e894
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 16:21:01 +0000

    Add NetworkDeviceHandler::RequestRefreshIPConfigs
    This is needed for https://codereview.chromium.org/17995002/.
    
    BUG=189080
    R=armansito@chromium.org, pneubeck@chromium.org
    
    Review URL: https://codereview.chromium.org/18873007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211410 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/network_handler_callbacks.h
+++ chromeos/network/network_handler_callbacks.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/callback.h"
 #include "chromeos/chromeos_export.h"
+#include "chromeos/dbus/dbus_method_call_status.h"
 
 namespace base {
 class DictionaryValue;
@@ -56,6 +57,17 @@ CHROMEOS_EXPORT void ShillErrorCallbackFunction(
     const ErrorCallback& error_callback,
     const std::string& dbus_error_name,
     const std::string& dbus_error_message);
+
+// Callback for property getters used by NetworkConfigurationHandler
+// (for Network Services) and by NetworkDeviceHandler. Used to translate
+// the DBus Dictionary callback into one that calls the error callback
+// if |call_status| != DBUS_METHOD_CALL_SUCCESS.
+CHROMEOS_EXPORT void GetPropertiesCallback(
+    const network_handler::DictionaryResultCallback& callback,
+    const network_handler::ErrorCallback& error_callback,
+    const std::string& path,
+    DBusMethodCallStatus call_status,
+    const base::DictionaryValue& value);
 
 }  // namespace network_handler
 }  // namespace chromeos
