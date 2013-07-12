76bfc7e2e234424136be955ee87f181fd1855be3
jsbell@chromium.org <jsbell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 19:36:16 +0000

    IndexedDB: Test for "close" event when connection is force-closed.
    The event is implemented in Blink in http://crrev.com/18042007 - this
    is just test.
    
    BUG=249470
    R=dgrogan@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18660012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211444 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/test/data/indexeddb/force_close_event.js
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function test() {
+  indexedDBTest(onUpgradeNeeded, onOpenSuccess);
+}
+
+function onUpgradeNeeded(e) {
+}
+
+function onOpenSuccess(e) {
+  // Don't let GC claim the connection
+  window.db = e.target.result;
+  window.db.onclose = onDatabaseClose;
+  document.title = 'connection ready';
+}
+
+function onDatabaseClose(e)
+{
+  document.title = 'connection closed';
+}
