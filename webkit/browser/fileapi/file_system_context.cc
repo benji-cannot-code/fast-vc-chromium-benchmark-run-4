74c9026b4af15d900690bad4b871400c6217382f
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 03:50:02 +0000

    FileAPI: Factor out getting root URI and FS name parts into each FS backend
    We plan to introduce SyncFileSystemBackend to remove SyncFS related code from
    webkit/browser/fileapi. To do that, this change moves getting root URI and
    filesystem name parts from FileSystemContext to each FileSystemBackend.
    
    BUG=242422
    TEST=content_unittests
    R=kinuko@chromium.org, satorux@chromium.org, tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/18300006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211318 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/file_system_context.cc
+++ webkit/browser/fileapi/file_system_context.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/browser/fileapi/sandbox_file_system_backend.h"
 #include "webkit/browser/fileapi/syncable/local_file_change_tracker.h"
 #include "webkit/browser/fileapi/syncable/local_file_sync_context.h"
-#include "webkit/browser/fileapi/syncable/syncable_file_system_util.h"
 #include "webkit/browser/fileapi/test_file_system_backend.h"
 #include "webkit/browser/quota/quota_manager.h"
 #include "webkit/browser/quota/special_storage_policy.h"
@@ -271,16 +270,8 @@ void FileSystemContext::OpenFileSystem(
     return;
   }
 
-  GURL root_url;
-  if (type == kFileSystemTypeSyncable)
-    root_url = sync_file_system::GetSyncableFileSystemRootURI(origin_url);
-  else
-    root_url = GetFileSystemRootURI(origin_url, type);
-  std::string name = GetFileSystemName(origin_url, type);
-
-  backend->OpenFileSystem(
-      origin_url, type, mode,
-      base::Bind(&DidOpenFileSystem, callback, root_url, name));
+  backend->OpenFileSystem(origin_url, type, mode,
+                          base::Bind(&DidOpenFileSystem, callback));
 }
 
 void FileSystemContext::DeleteFileSystem(
