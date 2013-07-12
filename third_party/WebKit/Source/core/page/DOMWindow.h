0bd2a52bdbce4c78b0847113dc5d3aec95bd7a72
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-12 19:03:54 +0000

    Move logic for creating a new window to a separate file.
    This logic is currently spread between DOMWindow.cpp and FrameLoader.cpp, in
    two different static functions. These function could certainly be cleaned up,
    but isolating them seems to be a good start.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/19104002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154138 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/DOMWindow.h
+++ third_party/WebKit/Source/core/page/DOMWindow.h
@@ -398,6 +398,8 @@ namespace WebCore {
         void willDetachDocumentFromFrame();
         DOMWindow* anonymousIndexedGetter(uint32_t);
 
+        bool isInsecureScriptAccess(DOMWindow* activeWindow, const String& urlString);
+
     private:
         explicit DOMWindow(Frame*);
 
@@ -410,11 +412,6 @@ namespace WebCore {
         virtual void derefEventTarget() { deref(); }
         virtual EventTargetData* eventTargetData();
         virtual EventTargetData* ensureEventTargetData();
-
-        static Frame* createWindow(const String& urlString, const AtomicString& frameName, const WindowFeatures&,
-            DOMWindow* activeWindow, Frame* firstFrame, Frame* openerFrame,
-            PrepareDialogFunction = 0, void* functionContext = 0);
-        bool isInsecureScriptAccess(DOMWindow* activeWindow, const String& urlString);
 
         void resetDOMWindowProperties();
         void willDestroyDocumentInFrame();
