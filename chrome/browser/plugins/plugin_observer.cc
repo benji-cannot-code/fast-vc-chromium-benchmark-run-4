2486375892837e1037e72b86e2b5f283202e762b
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 00:11:03 +0000

    Add plugin identifier as a crash key and set it to help track down bug 259446.
    BUG=259446
    TEST=none
    
    Review URL: https://codereview.chromium.org/19072002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211266 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugins/plugin_observer.cc
+++ chrome/browser/plugins/plugin_observer.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/auto_reset.h"
 #include "base/bind.h"
+#include "base/debug/crash_logging.h"
 #include "base/metrics/histogram.h"
 #include "base/process_util.h"
 #include "base/stl_util.h"
@@ -298,7 +299,8 @@ void PluginObserver::OnBlockedOutdatedPlugin(int placeholder_id,
   PluginInstaller* installer = NULL;
   scoped_ptr<PluginMetadata> plugin;
   bool ret = finder->FindPluginWithIdentifier(identifier, &installer, &plugin);
-  DCHECK(ret);
+  base::debug::ScopedCrashKey identifier_key("plugin_identifier", identifier);
+  CHECK(ret);
 
   plugin_placeholders_[placeholder_id] =
       new PluginPlaceholderHost(this, placeholder_id,
@@ -405,7 +407,8 @@ void PluginObserver::OnNPAPINotSupported(const std::string& identifier) {
   scoped_ptr<PluginMetadata> plugin;
   bool ret = PluginFinder::GetInstance()->FindPluginWithIdentifier(
           identifier, NULL, &plugin);
-  DCHECK(ret);
+  base::debug::ScopedCrashKey identifier_key("plugin_identifier", identifier);
+  CHECK(ret);
 
   PluginMetroModeInfoBarDelegate::Create(
       InfoBarService::FromWebContents(web_contents()),
