f0c4e16d60a2474667d2a1911b13f7f4dd7dedd3
navabi@google.com <navabi@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 01:40:09 +0000

    Deploy adb reconnect on disconnect daemon on WebKit (ContentShell) Android bot.
    NOTRY=True
    BUG=256353
    
    Review URL: https://chromiumcodereview.appspot.com/18652008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211283 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/buildbot/bb_device_steps.py
+++ build/android/buildbot/bb_device_steps.py
@@ -15,6 +15,7 @@ import bb_utils
 import bb_annotations
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+import provision_devices
 from pylib import android_commands
 from pylib import constants
 from pylib.gtest import gtest_config
@@ -252,7 +253,10 @@ def ProvisionDevices(options):
   bb_annotations.PrintNamedStep('provision_devices')
   if options.reboot:
     RebootDevices()
-  RunCmd(['build/android/provision_devices.py', '-t', options.target])
+  provision_cmd = ['build/android/provision_devices.py', '-t', options.target]
+  if options.auto_reconnect:
+    provision_cmd.append('--auto-reconnect')
+  RunCmd(provision_cmd)
 
 
 def DeviceStatusCheck(_):
@@ -380,6 +384,7 @@ def main(argv):
   setattr(options, 'target', options.factory_properties.get('target', 'Debug'))
 
   MainTestWrapper(options)
+  provision_devices.KillHostHeartbeat()
 
 
 if __name__ == '__main__':
