3445e98adbb9de90116e5e878b7d0ba880eb5313
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 07:02:45 +0000

    cros: Remove CrosLibrary::libcros_loaded()
    This method is no longer in use.
    
    BUG=126719
    TEST=none
    R=hashimoto@chromium.org
    
    Review URL: https://codereview.chromium.org/18435015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211351 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/cros/cros_library.h
+++ chrome/browser/chromeos/cros/cros_library.h
@@ -10,10 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
 
-namespace base {
-template <typename T> struct DefaultLazyInstanceTraits;
-}
-
 namespace chromeos {
 
 class NetworkLibrary;
@@ -42,11 +38,6 @@ class CrosLibrary {
   // CrosLibrary will take the ownership. The existing network library will
   // be deleted. Passing NULL will just delete the existing network library.
   void SetNetworkLibrary(NetworkLibrary* network_library);
-
-  // Note: Since we are no longer loading Libcros, we can return true here
-  // whenever the used libraries are not stub.
-  // TODO(hashimoto): Remove this method.
-  bool libcros_loaded() { return !use_stub_impl_; }
 
  private:
   explicit CrosLibrary(bool use_stub);
