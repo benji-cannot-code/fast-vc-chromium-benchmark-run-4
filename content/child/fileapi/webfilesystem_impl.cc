0528391c4fadd96c2da27b623894b3a751ee5bf3
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-12 03:44:49 +0000

    Cleanup: split FileSystemDispatcher::Create into CreateFile and CreateDirectory
    Remotely for crbug.com/257349
    
    BUG=none
    TEST=none
    R=dmichael@chromium.org, tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/18282010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211316 0039d316-1c4b-4281-b951-d872f2087c98

--- content/child/fileapi/webfilesystem_impl.cc
+++ content/child/fileapi/webfilesystem_impl.cc
@@ -83,8 +83,8 @@ void WebFileSystemImpl::createFile(const WebURL& path,
                                    WebFileSystemCallbacks* callbacks) {
   FileSystemDispatcher* dispatcher =
       ChildThread::current()->file_system_dispatcher();
-  dispatcher->Create(
-      GURL(path), exclusive, false /* directory */, false /* recursive */,
+  dispatcher->CreateFile(
+      GURL(path), exclusive,
       base::Bind(&FileStatusCallbackAdapter, callbacks));
 }
 
@@ -93,8 +93,8 @@ void WebFileSystemImpl::createDirectory(const WebURL& path,
                                         WebFileSystemCallbacks* callbacks) {
   FileSystemDispatcher* dispatcher =
       ChildThread::current()->file_system_dispatcher();
-  dispatcher->Create(
-      GURL(path), exclusive, true /* directory */, false /* recursive */,
+  dispatcher->CreateDirectory(
+      GURL(path), exclusive, false /* recursive */,
       base::Bind(&FileStatusCallbackAdapter, callbacks));
 }
 
