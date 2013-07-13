341fa2503b7841eba8af717c29fcdcd0fb1b0ec0
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-13 04:31:49 +0000

    add missing headers in components/tracing/child_trace_message_filter.h and content/child/child_resource_message_filter.cc
    BUG=259043
    
    Review URL: https://chromiumcodereview.appspot.com/18863004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211542 0039d316-1c4b-4281-b951-d872f2087c98

--- components/tracing/child_trace_message_filter.h
+++ components/tracing/child_trace_message_filter.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef COMPONENTS_TRACING_CHILD_TRACE_MESSAGE_FILTER_H_
 #define COMPONENTS_TRACING_CHILD_TRACE_MESSAGE_FILTER_H_
 
+#include "base/bind.h"
+#include "base/memory/ref_counted_memory.h"
 #include "ipc/ipc_channel_proxy.h"
 
 namespace base {
--- content/child/child_resource_message_filter.cc
+++ content/child/child_resource_message_filter.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/child/child_resource_message_filter.h"
 
+#include "base/bind.h"
+#include "base/single_thread_task_runner.h"
 #include "base/thread_task_runner_handle.h"
 #include "content/child/resource_dispatcher.h"
 #include "content/common/resource_messages.h"
