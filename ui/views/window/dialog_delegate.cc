fe2035a8f95c142b4fb77a045f8812cef8339755
msw@chromium.org <msw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-13 04:34:00 +0000

    Use Windows native textfields and old dialogs on Win8 Metro.
    This fixes TSF IME integration for non-Aura Win8 Metro browser UI textfields.
    This should be merged to M-29, and reverted with the real fix for Issue 239690.
    See screenshots and initial testing details at http://crbug.com/259125
    
    Revert Views Textfields on Win8 Metro Chrome to Windows native controls.
    (impacts Omnibox, and Find-Bar/Bubble/Dialog (non-web-content) textfields)
    
    Use the old dialog style when using Windows textfields (just Metro by default).
    Necessary because transparent dialogs can't host native control child HWNDs.
    
    BUG=239690,259125
    TEST=Windows IME works everywhere, Win8 Metro dialogs use the old style.
    R=kochi@chromium.org,yukawa@chromium.org,nona@chromium.org,sadrul@chromium.org
    TBR=sky@chromium.org,ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/18948006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211544 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/window/dialog_delegate.cc
+++ ui/views/window/dialog_delegate.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/bubble/bubble_border.h"
 #include "ui/views/bubble/bubble_frame_view.h"
 #include "ui/views/controls/button/label_button.h"
+#include "ui/views/controls/textfield/textfield.h"
 #include "ui/views/widget/widget.h"
 #include "ui/views/widget/widget_observer.h"
 #include "ui/views/window/dialog_client_view.h"
@@ -30,7 +31,8 @@ DialogDelegate::~DialogDelegate() {
 
 // static
 bool DialogDelegate::UseNewStyle() {
-  return switches::IsNewDialogStyleEnabled();
+  return switches::IsNewDialogStyleEnabled() &&
+         Textfield::IsViewsTextfieldEnabled();
 }
 
 // static
