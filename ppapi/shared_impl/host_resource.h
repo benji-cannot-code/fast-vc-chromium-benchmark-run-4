e5b117b94002b7926d6ea03ec9493fe82f5d260f
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-15 20:18:35 +0000

    Pepper: Add usage comment to HostResource.
    HostResource is only useful for "old"-style resources. This is confusing; it's
    hard when looking at the codebase to know when PP_Resource values differ on the
    host and plugin, and when they're the same.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19106002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211676 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/shared_impl/host_resource.h
+++ ppapi/shared_impl/host_resource.h
@@ -12,6 +12,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ppapi {
 
+// For "old" style resources, PP_Resource values differ on the host and plugin
+// side. Implementations of those should be careful to use HostResource to
+// prevent confusion. "New" style resources use the same PP_Resource value on
+// the host and plugin sides, and should not use HostResource.
+//
+// Old style resources match these file specs:
+//   Proxy: ppapi/proxy/ppb_*_proxy.*
+//   Host: webkit/plugins/ppapi/*
+// New style resources match these file specs:
+//   Proxy: ppapi/proxy/*_resource.*
+//   Browser: (content|chrome)/browser/renderer_host/pepper/pepper_*_host.*
+//   Renderer: (content|chrome)/renderer/pepper/pepper_*_host.*
+//
+//
 // Represents a PP_Resource sent over the wire. This just wraps a PP_Resource.
 // The point is to prevent mistakes where the wrong resource value is sent.
 // Resource values are remapped in the plugin so that it can talk to multiple
