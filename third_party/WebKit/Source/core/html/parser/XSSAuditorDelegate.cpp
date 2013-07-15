254a38b261b8d1c48b1c867a0b4e27e8b6c44fcc
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-15 10:32:21 +0000

    CSP: Update violation report 'Content-Type' header.
    The spec recently changed the type of violation reports from
    'application/json' to 'application/csp-report'[1]. This patch implements
    that change by adding a type flag to the relevant PingLoader method.
    
    [1]: https://dvcs.w3.org/hg/content-security-policy/rev/9c1ebbb88a76
    
    Review URL: https://chromiumcodereview.appspot.com/19091002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154215 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/parser/XSSAuditorDelegate.cpp
+++ third_party/WebKit/Source/core/html/parser/XSSAuditorDelegate.cpp
@@ -108,7 +108,7 @@ void XSSAuditorDelegate::didBlockScript(const XSSInfo& xssInfo)
         frameLoader->client()->didDetectXSS(m_document->url(), xssInfo.m_didBlockEntirePage);
 
         if (!m_reportURL.isEmpty())
-            PingLoader::sendViolationReport(m_document->frame(), m_reportURL, generateViolationReport());
+            PingLoader::sendViolationReport(m_document->frame(), m_reportURL, generateViolationReport(), PingLoader::XSSAuditorViolationReport);
     }
 
     if (xssInfo.m_didBlockEntirePage)
