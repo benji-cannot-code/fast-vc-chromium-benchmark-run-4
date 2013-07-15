4901b8854160738c7dc61b1d55ac55c680d142a0
dcheng@chromium.org <dcheng@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-15 19:58:39 +0000

    Use EventHandler::mouseMoved() instead of handleMouseMoveEvent().
    Simplifies the code a bit since mouseMoved() also handles things
    like tooltips, so WebViewImpl doesn't have to manually clear the
    mouseover URL on a mouse leave.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/18147013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154247 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorInputAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorInputAgent.cpp
@@ -147,7 +147,7 @@ void InspectorInputAgent::dispatchMouseEvent(ErrorString* error, const String& t
         handler->handleMouseReleaseEvent(event);
         break;
     case PlatformEvent::MouseMoved:
-        handler->handleMouseMoveEvent(event);
+        handler->mouseMoved(event);
         break;
     default:
         *error = "Unhandled type: " + type;
