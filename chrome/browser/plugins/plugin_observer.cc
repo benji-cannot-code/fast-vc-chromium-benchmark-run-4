5a4c1536f855e36ddc6a1572da0673b69aabea25
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-15 21:48:04 +0000

    Undo r211266, and fix crash caused by r210931.
    Don't deref a smart pointer in the same function arg list where that pointer is
    Pass()ed, since depending on evaluation order, this causes a NULL deref.
    
    BUG=259446
    TEST=On a Mac, have an outdated Silverlight install, and go to a page using Silverlight.  You should see an outdated plugin infobar and the browser shouldn't crash.
    R=bauerb@chromium.org, rsesek@chromium.org
    
    Review URL: https://codereview.chromium.org/19280004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211710 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugins/plugin_observer.cc
+++ chrome/browser/plugins/plugin_observer.cc
@@ -299,8 +299,7 @@ void PluginObserver::OnBlockedOutdatedPlugin(int placeholder_id,
   PluginInstaller* installer = NULL;
   scoped_ptr<PluginMetadata> plugin;
   bool ret = finder->FindPluginWithIdentifier(identifier, &installer, &plugin);
-  base::debug::ScopedCrashKey identifier_key("plugin_identifier", identifier);
-  CHECK(ret);
+  DCHECK(ret);
 
   plugin_placeholders_[placeholder_id] =
       new PluginPlaceholderHost(this, placeholder_id,
@@ -407,8 +406,7 @@ void PluginObserver::OnNPAPINotSupported(const std::string& identifier) {
   scoped_ptr<PluginMetadata> plugin;
   bool ret = PluginFinder::GetInstance()->FindPluginWithIdentifier(
           identifier, NULL, &plugin);
-  base::debug::ScopedCrashKey identifier_key("plugin_identifier", identifier);
-  CHECK(ret);
+  DCHECK(ret);
 
   PluginMetroModeInfoBarDelegate::Create(
       InfoBarService::FromWebContents(web_contents()),
