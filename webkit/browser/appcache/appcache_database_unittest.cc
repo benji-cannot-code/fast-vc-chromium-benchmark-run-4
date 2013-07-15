f5889dd5bac9af2bd12a66e1746b71343544a289
shess@chromium.org <shess@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-15 21:09:19 +0000

    [sql] Convert webkit tests to use ScopedErrorIgnorer.
    Get rid of some error callbacks by converting to ScopedErrorIgnorer.
    Also make the error suppression more specific.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/18051005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211696 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/appcache/appcache_database_unittest.cc
+++ webkit/browser/appcache/appcache_database_unittest.cc
@@ -10,20 +10,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "sql/connection.h"
 #include "sql/meta_table.h"
 #include "sql/statement.h"
+#include "sql/test/scoped_error_ignorer.h"
 #include "sql/transaction.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/sqlite/sqlite3.h"
 #include "webkit/browser/appcache/appcache_database.h"
 #include "webkit/browser/appcache/appcache_entry.h"
 
 namespace {
 
 const base::Time kZeroTime;
-
-// Set this error callback to "handle" errors by ignoring them.  This
-// causes the sql/ machinery to not throw fatal reports and allow
-// higher-level code to return failures.
-void IgnoreDatabaseErrors(int error, sql::Statement* stmt) {
-}
 
 }  // namespace
 
@@ -113,7 +109,13 @@ TEST(AppCacheDatabaseTest, EntryRecords) {
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
 
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
+  sql::ScopedErrorIgnorer ignore_errors;
+  // TODO(shess): Suppressing SQLITE_CONSTRAINT because the code
+  // expects that and handles the resulting error.  Consider revising
+  // the code to use INSERT OR IGNORE (which would not throw
+  // SQLITE_CONSTRAINT) and then check ChangeCount() to see if any
+  // changes were made.
+  ignore_errors.IgnoreError(SQLITE_CONSTRAINT);
 
   AppCacheDatabase::EntryRecord entry;
 
@@ -179,6 +181,8 @@ TEST(AppCacheDatabaseTest, EntryRecords) {
   EXPECT_TRUE(db.DeleteEntriesForCache(1));
   EXPECT_FALSE(db.AddEntryFlags(GURL("http://blah/1"), 1,
                                 AppCacheEntry::FOREIGN));
+
+  ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
 }
 
 TEST(AppCacheDatabaseTest, CacheRecords) {
@@ -186,7 +190,9 @@ TEST(AppCacheDatabaseTest, CacheRecords) {
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
 
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
+  sql::ScopedErrorIgnorer ignore_errors;
+  // TODO(shess): See EntryRecords test.
+  ignore_errors.IgnoreError(SQLITE_CONSTRAINT);
 
   const AppCacheDatabase::CacheRecord kZeroRecord;
   AppCacheDatabase::CacheRecord record;
@@ -221,6 +227,8 @@ TEST(AppCacheDatabaseTest, CacheRecords) {
   EXPECT_FALSE(db.FindCacheForGroup(1, &record));
 
   EXPECT_TRUE(db.DeleteCache(1));
+
+  ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
 }
 
 TEST(AppCacheDatabaseTest, GroupRecords) {
@@ -228,7 +236,9 @@ TEST(AppCacheDatabaseTest, GroupRecords) {
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
 
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
+  sql::ScopedErrorIgnorer ignore_errors;
+  // TODO(shess): See EntryRecords test.
+  ignore_errors.IgnoreError(SQLITE_CONSTRAINT);
 
   const GURL kManifestUrl("http://blah/manifest");
   const GURL kOrigin(kManifestUrl.GetOrigin());
@@ -348,6 +358,8 @@ TEST(AppCacheDatabaseTest, GroupRecords) {
   EXPECT_EQ(1, record.group_id);
   EXPECT_EQ(kManifest2, record.manifest_url);
   EXPECT_EQ(kOrigin2, record.origin);
+
+  ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
 }
 
 TEST(AppCacheDatabaseTest, NamespaceRecords) {
@@ -355,7 +367,9 @@ TEST(AppCacheDatabaseTest, NamespaceRecords) {
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
 
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
+  sql::ScopedErrorIgnorer ignore_errors;
+  // TODO(shess): See EntryRecords test.
+  ignore_errors.IgnoreError(SQLITE_CONSTRAINT);
 
   const GURL kFooNameSpace1("http://foo/namespace1");
   const GURL kFooNameSpace2("http://foo/namespace2");
@@ -460,14 +474,14 @@ TEST(AppCacheDatabaseTest, NamespaceRecords) {
   EXPECT_EQ(2U, fallbacks.size());
   EXPECT_TRUE(fallbacks[0].namespace_.is_pattern);
   EXPECT_TRUE(fallbacks[1].namespace_.is_pattern);
+
+  ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
 }
 
 TEST(AppCacheDatabaseTest, OnlineWhiteListRecords) {
   const base::FilePath kEmptyPath;
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
-
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
 
   const GURL kFooNameSpace1("http://foo/namespace1");
   const GURL kFooNameSpace2("http://foo/namespace2");
@@ -516,7 +530,9 @@ TEST(AppCacheDatabaseTest, DeletableResponseIds) {
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
 
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
+  sql::ScopedErrorIgnorer ignore_errors;
+  // TODO(shess): See EntryRecords test.
+  ignore_errors.IgnoreError(SQLITE_CONSTRAINT);
 
   std::vector<int64> ids;
 
@@ -579,6 +595,8 @@ TEST(AppCacheDatabaseTest, DeletableResponseIds) {
   EXPECT_EQ(5U, ids.size());
   for (int i = 0; i < static_cast<int>(ids.size()); ++i)
     EXPECT_EQ(i + 5, ids[i]);
+
+  ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
 }
 
 TEST(AppCacheDatabaseTest, OriginUsage) {
@@ -591,8 +609,6 @@ TEST(AppCacheDatabaseTest, OriginUsage) {
   const base::FilePath kEmptyPath;
   AppCacheDatabase db(kEmptyPath);
   EXPECT_TRUE(db.LazyOpen(true));
-
-  db.db_->set_error_callback(base::Bind(&IgnoreDatabaseErrors));
 
   std::vector<AppCacheDatabase::CacheRecord> cache_records;
   EXPECT_EQ(0, db.GetOriginUsage(kOrigin));
--- webkit/browser/dom_storage/dom_storage_database_unittest.cc
+++ webkit/browser/dom_storage/dom_storage_database_unittest.cc
@@ -11,7 +11,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "sql/statement.h"
+#include "sql/test/scoped_error_ignorer.h"
 #include "testing/gtest/include/gtest/gtest.h"
+#include "third_party/sqlite/sqlite3.h"
 
 namespace dom_storage {
 
@@ -346,6 +348,9 @@ TEST(DomStorageDatabaseTest, TestCanOpenFileThatIsNotADatabase) {
   file_util::WriteFile(file_name, kData, strlen(kData));
 
   {
+    sql::ScopedErrorIgnorer ignore_errors;
+    ignore_errors.IgnoreError(SQLITE_IOERR_SHORT_READ);
+
     // Try and open the file. As it's not a database, we should end up deleting
     // it and creating a new, valid file, so everything should actually
     // succeed.
@@ -357,9 +362,14 @@ TEST(DomStorageDatabaseTest, TestCanOpenFileThatIsNotADatabase) {
     EXPECT_TRUE(db.IsOpen());
 
     CheckValuesMatch(&db, values);
+
+    ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
   }
 
   {
+    sql::ScopedErrorIgnorer ignore_errors;
+    ignore_errors.IgnoreError(SQLITE_CANTOPEN);
+
     // Try to open a directory, we should fail gracefully and not attempt
     // to delete it.
     DomStorageDatabase db(temp_dir.path());
@@ -376,6 +386,8 @@ TEST(DomStorageDatabaseTest, TestCanOpenFileThatIsNotADatabase) {
     EXPECT_FALSE(db.IsOpen());
 
     EXPECT_TRUE(base::PathExists(temp_dir.path()));
+
+    ASSERT_TRUE(ignore_errors.CheckIgnoredErrors());
   }
 }
 
