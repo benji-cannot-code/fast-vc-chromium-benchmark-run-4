ceebacf58db5971eb6495ab0ff7330dec273fbfc
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-16 19:54:40 +0000

    Only allow leveldb to use the minimum amount of file descriptors.
    By default, leveldb will use up to 1000 file descriptors at
    a time. The per-process limit on CrOS is 1024. This can
    cause file descriptor exhaustion and, as issue 14137 points
    out, chrome doesn't handle that situation gracefully.
    
    This change will make leveldb only keep 64 files open in
    its cache. The target size for each file is 2mb, so
    databases under ~128mb will not be affected.
    Larger databases that perform many random reads might see a
    negligible performance decrease.
    
    BUG=251385
    R=derat@chromium.org, hashimoto@chromium.org, jsbell@chromium.org, marja@chromium.org, mpcomplete@chromium.org, nhiroki@chromium.org, rdevlin.cronin@chromium.org, tzik@chromium.org
    
    Review URL: https://codereview.chromium.org/19092006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211829 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/browser/fileapi/sandbox_directory_database.cc
+++ webkit/browser/fileapi/sandbox_directory_database.cc
@@ -716,6 +716,7 @@ bool SandboxDirectoryDatabase::Init(RecoveryOption recovery_option) {
       FilePathToString(filesystem_data_directory_.Append(
           kDirectoryDatabaseName));
   leveldb::Options options;
+  options.max_open_files = 0;  // Use minimum.
   options.create_if_missing = true;
   leveldb::DB* db;
   leveldb::Status status = leveldb::DB::Open(options, path, &db);
@@ -762,7 +763,9 @@ bool SandboxDirectoryDatabase::Init(RecoveryOption recovery_option) {
 
 bool SandboxDirectoryDatabase::RepairDatabase(const std::string& db_path) {
   DCHECK(!db_.get());
-  if (!leveldb::RepairDB(db_path, leveldb::Options()).ok())
+  leveldb::Options options;
+  options.max_open_files = 0;  // Use minimum.
+  if (!leveldb::RepairDB(db_path, options).ok())
     return false;
   if (!Init(FAIL_ON_CORRUPTION))
     return false;
--- webkit/browser/fileapi/syncable/local_file_change_tracker.cc
+++ webkit/browser/fileapi/syncable/local_file_change_tracker.cc
@@ -307,6 +307,7 @@ SyncStatusCode LocalFileChangeTracker::TrackerDB::Init(
   std::string path = fileapi::FilePathToString(
       base_path_.Append(kDatabaseName));
   leveldb::Options options;
+  options.max_open_files = 0;  // Use minimum.
   options.create_if_missing = true;
   leveldb::DB* db;
   leveldb::Status status = leveldb::DB::Open(options, path, &db);
@@ -335,7 +336,9 @@ SyncStatusCode LocalFileChangeTracker::TrackerDB::Repair(
   DCHECK(!db_.get());
   LOG(WARNING) << "Attempting to repair TrackerDB.";
 
-  if (leveldb::RepairDB(db_path, leveldb::Options()).ok() &&
+  leveldb::Options options;
+  options.max_open_files = 0;  // Use minimum.
+  if (leveldb::RepairDB(db_path, options).ok() &&
       Init(FAIL_ON_CORRUPTION) == SYNC_STATUS_OK) {
     // TODO(nhiroki): perform some consistency checks between TrackerDB and
     // syncable file system.
