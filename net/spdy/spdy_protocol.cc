5c54728d5cbb0193f312c8bf115cdd349bfa7bd6
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-16 21:15:44 +0000

    Declaration of SPDY 4 PUSH_PROMISE; not yet used.
    This lands server change 48855179.
    
    R=rtenneti@chromium.org
    
    Review URL: https://codereview.chromium.org/19269003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211843 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_protocol.cc
+++ net/spdy/spdy_protocol.cc
@@ -68,7 +68,6 @@ SpdyCredentialIR::SpdyCredentialIR(int16 slot) {
 
 SpdyCredentialIR::~SpdyCredentialIR() {}
 
-
 void SpdyCredentialIR::Visit(SpdyFrameVisitor* visitor) const {
   return visitor->VisitCredential(*this);
 }
@@ -77,4 +76,8 @@ void SpdyBlockedIR::Visit(SpdyFrameVisitor* visitor) const {
   return visitor->VisitBlocked(*this);
 }
 
-}  // namespace
+void SpdyPushPromiseIR::Visit(SpdyFrameVisitor* visitor) const {
+  return visitor->VisitPushPromise(*this);
+}
+
+}  // namespace net
