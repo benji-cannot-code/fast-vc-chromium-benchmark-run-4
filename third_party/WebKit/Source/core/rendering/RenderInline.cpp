c5dd585631bdf90838a9a197435117ffa404ac68
bjonesbe@adobe.com <bjonesbe@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-16 20:39:44 +0000

    With the latest Editor's Draft of the CSS Shapes specification, shape-outside no longer affects the positioning of floats. This is the first step in changing the implementation to reflect this specification change. The bulk of this change is reverting the code that was needed to position floats based on the bounding box of the shape.
    This change removes the paintOffset methods and variables that were
    originally added because the original positioning model for
    shape-outside caused floats with shape-outside to behave like they were
    positioned. Since that has been removed, the methods can be named as
    they were originally: offsetForInFlowPosition, since they only handle
    in flow positioned objects again.
    
    This is a port of http://wkb.ug/118080
    
    BUG=260357
    R=ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/19295003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154321 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderInline.cpp
+++ third_party/WebKit/Source/core/rendering/RenderInline.cpp
@@ -1025,7 +1025,7 @@ LayoutRect RenderInline::clippedOverflowRectForRepaint(const RenderLayerModelObj
             break;
         }
         if (inlineFlow->style()->hasInFlowPosition() && inlineFlow->hasLayer())
-            repaintRect.move(toRenderInline(inlineFlow)->layer()->paintOffset());
+            repaintRect.move(toRenderInline(inlineFlow)->layer()->offsetForInFlowPosition());
     }
 
     LayoutUnit outlineSize = style()->outlineSize();
@@ -1072,7 +1072,7 @@ void RenderInline::computeRectForRepaint(const RenderLayerModelObject* repaintCo
         if (v->layoutStateEnabled() && !repaintContainer) {
             LayoutState* layoutState = v->layoutState();
             if (style()->hasInFlowPosition() && layer())
-                rect.move(layer()->paintOffset());
+                rect.move(layer()->offsetForInFlowPosition());
             rect.move(layoutState->m_paintOffset);
             if (layoutState->m_clipped)
                 rect.intersect(layoutState->m_clipRect);
@@ -1105,7 +1105,7 @@ void RenderInline::computeRectForRepaint(const RenderLayerModelObject* repaintCo
         // is translated, but the render box isn't, so we need to do this to get the
         // right dirty rect. Since this is called from RenderObject::setStyle, the relative or sticky position
         // flag on the RenderObject has been cleared, so use the one on the style().
-        topLeft += layer()->paintOffset();
+        topLeft += layer()->offsetForInFlowPosition();
     }
     
     // FIXME: We ignore the lightweight clipping rect that controls use, since if |o| is in mid-layout,
@@ -1160,7 +1160,7 @@ void RenderInline::mapLocalToContainer(const RenderLayerModelObject* repaintCont
             LayoutState* layoutState = v->layoutState();
             LayoutSize offset = layoutState->m_paintOffset;
             if (style()->hasInFlowPosition() && layer())
-                offset += layer()->paintOffset();
+                offset += layer()->offsetForInFlowPosition();
             transformState.move(offset);
             return;
         }
