17f2085fa551f9ba8017a82fbd9902b10691f310
tsepez@chromium.org <tsepez@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-17 01:59:29 +0000

    IDBAny need not be scriptwrappable.
    V8IDBAnyCustom.cpp will call into toV8() for the appropriate type, which will in turn invoke wrap() for the appropriate type, which will perform the bindings integrity check.  Since IDBAny doesn't have its own wrap() method, there's no place that the check would actually be applied.  The presence of the IDBAny.idl was what triggered the conversion to scriptwrappable in the first place.
    
    This does no harm, but wastes a few bytes of memory.
    
    BUG=260757
    R=jsbell@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19368006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/indexeddb/IDBAny.cpp
+++ third_party/WebKit/Source/modules/indexeddb/IDBAny.cpp
@@ -57,7 +57,6 @@ IDBAny::IDBAny(Type type)
     , m_integer(0)
 {
     ASSERT(type == UndefinedType || type == NullType);
-    ScriptWrappable::init(this);
 }
 
 IDBAny::~IDBAny()
@@ -135,7 +134,6 @@ IDBAny::IDBAny(PassRefPtr<DOMStringList> value)
     , m_domStringList(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(PassRefPtr<IDBCursorWithValue> value)
@@ -143,7 +141,6 @@ IDBAny::IDBAny(PassRefPtr<IDBCursorWithValue> value)
     , m_idbCursorWithValue(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(PassRefPtr<IDBCursor> value)
@@ -151,7 +148,6 @@ IDBAny::IDBAny(PassRefPtr<IDBCursor> value)
     , m_idbCursor(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(PassRefPtr<IDBDatabase> value)
@@ -159,7 +155,6 @@ IDBAny::IDBAny(PassRefPtr<IDBDatabase> value)
     , m_idbDatabase(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(PassRefPtr<IDBFactory> value)
@@ -175,7 +170,6 @@ IDBAny::IDBAny(PassRefPtr<IDBIndex> value)
     , m_idbIndex(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(PassRefPtr<IDBTransaction> value)
@@ -183,7 +177,6 @@ IDBAny::IDBAny(PassRefPtr<IDBTransaction> value)
     , m_idbTransaction(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(PassRefPtr<IDBObjectStore> value)
@@ -191,7 +184,6 @@ IDBAny::IDBAny(PassRefPtr<IDBObjectStore> value)
     , m_idbObjectStore(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(const ScriptValue& value)
@@ -199,7 +191,6 @@ IDBAny::IDBAny(const ScriptValue& value)
     , m_scriptValue(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(const IDBKeyPath& value)
@@ -207,7 +198,6 @@ IDBAny::IDBAny(const IDBKeyPath& value)
     , m_idbKeyPath(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(const String& value)
@@ -215,14 +205,12 @@ IDBAny::IDBAny(const String& value)
     , m_string(value)
     , m_integer(0)
 {
-    ScriptWrappable::init(this);
 }
 
 IDBAny::IDBAny(int64_t value)
     : m_type(IntegerType)
     , m_integer(value)
 {
-    ScriptWrappable::init(this);
 }
 
 } // namespace WebCore
