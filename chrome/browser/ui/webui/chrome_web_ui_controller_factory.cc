26f995f7683b70108f82b651e1a89b3d14185547
gene@chromium.org <gene@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 14:05:10 +0000

    Adding placeholder for chrome://devices page.
    Review URL: https://chromiumcodereview.appspot.com/18822003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212032 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
+++ chrome/browser/ui/webui/chrome_web_ui_controller_factory.cc
@@ -34,6 +34,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/inline_login_ui.h"
 #include "chrome/browser/ui/webui/inspect_ui.h"
 #include "chrome/browser/ui/webui/instant_ui.h"
+#include "chrome/browser/ui/webui/local_discovery/local_discovery_ui.h"
 #include "chrome/browser/ui/webui/memory_internals/memory_internals_ui.h"
 #include "chrome/browser/ui/webui/net_internals/net_internals_ui.h"
 #include "chrome/browser/ui/webui/ntp/new_tab_ui.h"
@@ -214,6 +215,11 @@ WebUIFactoryFunction GetWebUIFactoryFunction(WebUI* web_ui,
     return &NewWebUI<ConstrainedWebDialogUI>;
   if (url.host() == chrome::kChromeUICrashesHost)
     return &NewWebUI<CrashesUI>;
+  if (url.host() == chrome::kChromeUIDevicesHost &&
+      CommandLine::ForCurrentProcess()->HasSwitch(
+            switches::kEnableDeviceDiscovery)) {
+    return &NewWebUI<LocalDiscoveryUI>;
+  }
   if (url.host() == chrome::kChromeUIFlagsHost)
     return &NewWebUI<FlagsUI>;
   if (url.host() == chrome::kChromeUIHistoryFrameHost)
