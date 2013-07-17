e5af9556301a836f8a999e502d6ed6b771022440
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-17 00:52:26 +0000

    Get rid of special casing for 'operator' from the bindings generator
    Get rid of special casing for 'operator' from the bindings generator
    by using [ImplementedAs=svgOperator] extended attribute for the
    following attributes:
    - SVGFECompositeElement.operator
    - SVGFEMorphologyElement.operator
    
    R=haraken
    
    Review URL: https://chromiumcodereview.appspot.com/19368004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154341 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/deprecated_code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/deprecated_code_generator_v8.pm
@@ -5771,6 +5771,7 @@ sub FirstLetterToUpperCase
     $ret =~ s/Xml/XML/ if $ret =~ /^Xml[^a-z]/;
     $ret =~ s/Css/CSS/ if $ret =~ /^Css[^T]/;  # css -> setCSS, except setCssText.
     $ret =~ s/Ime/IME/ if $ret =~ /^Ime/;  # ime -> setIME
+    $ret =~ s/Svg/SVG/ if $ret =~ /^Svg/;  # svg -> setSVG
     return $ret;
 }
 
@@ -5826,9 +5827,6 @@ sub AttributeNameForGetterAndSetter
         $attributeName = $attribute->extendedAttributes->{"ImplementedAs"};
     }
     my $attributeType = $attribute->type;
-
-    # Avoid clash with C++ keyword.
-    $attributeName = "_operator" if $attributeName eq "operator";
 
     return $attributeName;
 }
