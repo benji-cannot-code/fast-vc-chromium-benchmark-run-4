57323788b97fefd2eaf2c2d4871ceb6d256b61e9
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 16:48:12 +0000

    Refactor InspectUI and DevToolsAdbBridge to move the polling logic out of Javascript code.
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/19302003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212065 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/inspect/inspect.js
+++ chrome/browser/resources/inspect/inspect.js
@@ -3,20 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-function adbPages() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'adb-pages', false);
-  xhr.send(null);
-  if (xhr.status !== 200)
-    return null;
-
-  try {
-    return JSON.parse(xhr.responseText);
-  } catch (e) {
-  }
-  return null;
-}
-
 function requestData() {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', 'targets-data.json', false);
@@ -41,7 +27,6 @@ function removeChildren(element_id) {
 
 function onload() {
   populateLists();
-  populateDeviceLists();
 }
 
 function populateLists() {
@@ -64,13 +49,11 @@ function populateLists() {
   }
 }
 
-function populateDeviceLists() {
-  var pages = adbPages();
+function populateDeviceLists(pages) {
   var pagesDigest = JSON.stringify(pages);
-  if (!pages || pagesDigest == window.pagesDigest) {
-    setTimeout(populateDeviceLists, 1000);
+  if (!pages || pagesDigest == window.pagesDigest)
     return;
-  }
+
   window.pagesDigest = pagesDigest;
 
   // Clear existing entries
@@ -113,8 +96,6 @@ function populateDeviceLists() {
 
     addTargetToList(page, packageId, ['faviconUrl', 'name', 'url']);
   }
-
-  setTimeout(populateDeviceLists, 1000);
 }
 
 function addToPagesList(data) {
--- chrome/browser/ui/webui/inspect_ui.h
+++ chrome/browser/ui/webui/inspect_ui.h
@@ -17,7 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui_data_source.h"
 
 class InspectUI : public content::WebUIController,
-                  public content::NotificationObserver {
+                  public content::NotificationObserver,
+                  public DevToolsAdbBridge::Listener {
  public:
   explicit InspectUI(content::WebUI* web_ui);
   virtual ~InspectUI();
@@ -45,13 +46,9 @@ class InspectUI : public content::WebUIController,
       const std::string& path,
       const content::WebUIDataSource::GotDataCallback& callback);
 
-  bool HandleAdbPagesCallback(
-      const std::string& path,
-      const content::WebUIDataSource::GotDataCallback& callback);
-
-  void OnAdbPages(const content::WebUIDataSource::GotDataCallback& callback,
-                  int result,
-                  DevToolsAdbBridge::RemotePages* pages);
+  // DevToolsAdbBridge::Listener overrides.
+  virtual void RemotePagesChanged(
+      DevToolsAdbBridge::RemotePages* pages) OVERRIDE;
 
   scoped_refptr<WorkerCreationDestructionListener> observer_;
 
