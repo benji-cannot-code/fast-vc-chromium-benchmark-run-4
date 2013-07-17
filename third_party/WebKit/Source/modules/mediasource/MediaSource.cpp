889c90a8c0260b65cffddaf196b46475d8ed0f80
loislo@chromium.org <loislo@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-17 12:18:29 +0000

    Build fix for mac.
    BUG=none
    TBR=yurys@chromium.org
    
    Review URL: https://codereview.chromium.org/19572002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/mediasource/MediaSource.cpp
+++ third_party/WebKit/Source/modules/mediasource/MediaSource.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/SourceBufferPrivate.h"
 #include "modules/mediasource/MediaSourceRegistry.h"
 #include "wtf/Uint8Array.h"
+#include "wtf/text/CString.h"
 
 namespace WebCore {
 
