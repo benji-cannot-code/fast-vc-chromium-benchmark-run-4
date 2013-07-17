ba983fd7e0edd7a9a760b64b0a3298a26aa7e3b1
bauerb@chromium.org <bauerb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 15:39:52 +0000

    Add ScopedAllowIO to suppress DCHECK in chrome::PathProvider().
    NOTRY=true
    BUG=259796
    
    Review URL: https://chromiumcodereview.appspot.com/19002004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212049 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_paths.cc
+++ chrome/common/chrome_paths.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/path_service.h"
 #include "base/strings/string_util.h"
 #include "base/sys_info.h"
+#include "base/threading/thread_restrictions.h"
 #include "base/version.h"
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_paths_internal.h"
@@ -501,6 +502,8 @@ bool PathProvider(int key, base::FilePath* result) {
       return false;
   }
 
+  // TODO(bauerb): http://crbug.com/259796
+  base::ThreadRestrictions::ScopedAllowIO allow_io;
   if (create_dir && !base::PathExists(cur) &&
       !file_util::CreateDirectory(cur))
     return false;
