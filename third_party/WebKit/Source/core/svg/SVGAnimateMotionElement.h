b2612236da8710c968f721e9edbb201911ee88c1
pavan.e@samsung.com <pavan.e@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-17 23:25:17 +0000

    [SVG] Refactoring accumulate logic for animateMotion
    made some changes to simplify the logic for accumulation
    
    R=pdr@chromium.org, schenney@chromium.org, dschulze@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19500005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154444 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGAnimateMotionElement.h
+++ third_party/WebKit/Source/core/svg/SVGAnimateMotionElement.h
@@ -58,7 +58,6 @@ private:
         RotateAutoReverse
     };
     RotateMode rotateMode() const;
-    void buildTransformForProgress(AffineTransform*, float percentage);
 
     bool m_hasToPointAtEndOfDuration;
 
