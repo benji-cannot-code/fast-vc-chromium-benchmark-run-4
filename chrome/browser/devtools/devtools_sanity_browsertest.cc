0362c9ba4d659ac3e8775af54a9db3fb2595dbfc
acolwell@chromium.org <acolwell@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 22:08:53 +0000

    Disable DevToolsSanityTest.TestRendererProcessNativeMemorySize
    Test was broken by Blink r154395. Temporarily disabling the test.
    
    BUG=261362
    TBR=loislo
    
    Review URL: https://codereview.chromium.org/19682003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212142 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_sanity_browsertest.cc
+++ chrome/browser/devtools/devtools_sanity_browsertest.cc
@@ -477,7 +477,7 @@ IN_PROC_BROWSER_TEST_F(DevToolsExtensionTest,
 
 // Tests that renderer process native memory is feasible.
 IN_PROC_BROWSER_TEST_F(DevToolsSanityTest,
-                       TestRendererProcessNativeMemorySize) {
+                       DISABLED_TestRendererProcessNativeMemorySize) {
   RunTest("testRendererProcessNativeMemorySize", std::string());
 }
 
