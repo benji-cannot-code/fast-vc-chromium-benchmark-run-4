b523d62e3a263124d28c6ac34fd869d718218483
dmichael@chromium.org <dmichael@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 20:15:21 +0000

    PPAPI: Test cleanup, make postconditions work
    This CL does a few things for our tests:
    - Remove cookie-related junk that is no longer valid. We use domAutomationController to signal test completion/progress/failure now, not cookies.
    - Make CheckPostConditions work again by using domAutomationController instead of the cookies, which were ignored.
    - Change the way we decide whether to remove the plugin after the test is run.
    - Update TestInstanceDeprecated to better reflect actual shutdown behavior.
    
    R=bbudge@chromium.org, teravest@chromium.org
    
    Review URL: https://codereview.chromium.org/15976003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212121 0039d316-1c4b-4281-b951-d872f2087c98

--- ppapi/tests/test_instance_deprecated.h
+++ ppapi/tests/test_instance_deprecated.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 class TestInstance : public TestCase {
  public:
   TestInstance(TestingInstance* instance);
+  virtual ~TestInstance();
 
   // TestCase implementation.
   virtual bool Init();
