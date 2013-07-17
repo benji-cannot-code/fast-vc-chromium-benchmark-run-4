f5d49ca1564f0f5032dd88e4b2de30941f06831e
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 19:42:59 +0000

    Remove last non-tests usage of PluginList from chrome. I could have exposed the used method through the content API, however the usage here was trivial and didn't need to check for wildcards etc so it was just a trivial looping around a vector.
    This is in preparation for moving webkit/plugins to content/.
    
    BUG=237249
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/19636006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212110 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/plugins/plugin_metadata.cc
+++ chrome/browser/plugins/plugin_metadata.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/logging.h"
 #include "base/strings/string_util.h"
-#include "webkit/plugins/npapi/plugin_list.h"
 #include "webkit/plugins/npapi/plugin_utils.h"
 #include "webkit/plugins/webplugininfo.h"
 
@@ -62,12 +61,15 @@ bool PluginMetadata::HasMimeType(const std::string& mime_type) const {
 }
 
 bool PluginMetadata::MatchesPlugin(const webkit::WebPluginInfo& plugin) {
-  using webkit::npapi::PluginList;
-
   for (size_t i = 0; i < matching_mime_types_.size(); ++i) {
     // To have a match, every one of the |matching_mime_types_|
     // must be handled by the plug-in.
-    if (!PluginList::SupportsType(plugin, matching_mime_types_[i], false))
+    size_t j = 0;
+    for (; j < plugin.mime_types.size(); ++j) {
+      if (plugin.mime_types[j].mime_type == matching_mime_types_[i])
+        break;
+    }
+    if (j == plugin.mime_types.size())
       return false;
   }
 
