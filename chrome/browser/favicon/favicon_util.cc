2b5a74c91c0155ca49dcadd3ba60667245b143cc
pedrosimonetti@chromium.org <pedrosimonetti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 18:05:42 +0000

    2nd try: Supporting high dpi favicons in Instant Extended.
    Original CL: https://codereview.chromium.org/15388002/
    
    BUG=227087
    
    Review URL: https://chromiumcodereview.appspot.com/5805043437535232
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212088 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/favicon/favicon_util.cc
+++ chrome/browser/favicon/favicon_util.cc
@@ -5,8 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/favicon/favicon_util.h"
 
-#include "chrome/browser/favicon/favicon_types.h"
 #include "chrome/browser/history/select_favicon_frames.h"
+#include "chrome/common/favicon/favicon_types.h"
 #include "skia/ext/image_operations.h"
 #include "third_party/skia/include/core/SkBitmap.h"
 #include "ui/gfx/codec/png_codec.h"
--- chrome/common/favicon/favicon_types.cc
+++ chrome/common/favicon/favicon_types.cc
@@ -3,7 +3,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/favicon/favicon_types.h"
+#include "chrome/common/favicon/favicon_types.h"
 
 namespace chrome {
 
