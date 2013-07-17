70e262d241f746c8d93aa028c2c88e424834e7a5
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 14:52:01 +0000

    include missing headers in skia/ext/analysis_canvas.{h,cc}
    skia/ext/analysis_canvas.h uses the OVERRIDE macro, but does not
    include the header file that defines it.
    
    And analysis_canvas.cc lacks the header for DCHECK.
    
    BUG=259043
    
    Review URL: https://chromiumcodereview.appspot.com/18881003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212042 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/analysis_canvas.cc
+++ skia/ext/analysis_canvas.cc
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // found in the LICENSE file.
 
 #include "base/debug/trace_event.h"
+#include "base/logging.h"
 #include "skia/ext/analysis_canvas.h"
 #include "third_party/skia/include/core/SkDevice.h"
 #include "third_party/skia/include/core/SkDraw.h"
--- skia/ext/analysis_canvas.h
+++ skia/ext/analysis_canvas.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SKIA_EXT_ANALYSIS_CANVAS_H_
 #define SKIA_EXT_ANALYSIS_CANVAS_H_
 
+#include "base/compiler_specific.h"
 #include "third_party/skia/include/core/SkCanvas.h"
 #include "third_party/skia/include/core/SkDevice.h"
 #include "third_party/skia/include/core/SkPicture.h"
