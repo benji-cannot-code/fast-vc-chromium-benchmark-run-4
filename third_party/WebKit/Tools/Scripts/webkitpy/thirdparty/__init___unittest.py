0047048122d3c24ca12c3fd5e37e1b8f77e74b7c
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-17 03:48:44 +0000

    Check in pep8.py so that we don't auto-install it.
    R=eseidel@chromium.org, ojan@chromium.org
    BUG=230706
    
    Review URL: https://chromiumcodereview.appspot.com/19481002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154360 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/thirdparty/__init___unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/thirdparty/__init___unittest.py
@@ -40,4 +40,3 @@ class ThirdpartyTest(unittest.TestCase):
         import webkitpy.thirdparty.autoinstalled.irc.irclib
         import webkitpy.thirdparty.autoinstalled.mechanize
         import webkitpy.thirdparty.autoinstalled.pylint
-        import webkitpy.thirdparty.autoinstalled.pep8
