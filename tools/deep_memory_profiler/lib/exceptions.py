ae355f6d6543a0cafcac7439202ef3ac0f1d3090
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 06:28:58 +0000

    Refactor dmprof: Split dmprof.py into modules.
    No changes in behavior and the main code. Changes are:
    
    - .*Commands are moved into subcommands/.
    - Other classes and functions are moved into into lib/.
    - Constants are defined in their appropriate files in lib/.
    - External libraries are imported via lib/.
    - "Command" is renamed to "SubCommand".
    
    BUG=123750
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/19346002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@211976 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/deep_memory_profiler/lib/exceptions.py
@@ -0,0 +1,23 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+class EmptyDumpException(Exception):
+  def __str__(self):
+    return repr(self.args[0])
+
+
+class ParsingException(Exception):
+  def __str__(self):
+    return repr(self.args[0])
+
+
+class InvalidDumpException(ParsingException):
+  def __str__(self):
+    return "invalid heap profile dump: %s" % repr(self.args[0])
+
+
+class ObsoleteDumpVersionException(ParsingException):
+  def __str__(self):
+    return "obsolete heap profile dump version: %s" % repr(self.args[0])
