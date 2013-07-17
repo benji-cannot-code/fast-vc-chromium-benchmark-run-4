dcd057bd60c3249e030ada9a3fb02cd01cd4b713
harrym@chromium.org <harrym@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 23:37:01 +0000

    Enable gesture(touch/drag) for app list icon in alternate shelf layout.
    BUG=259037
    
    Review URL: https://chromiumcodereview.appspot.com/18179013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212165 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/launcher/alternate_app_list_button.cc
+++ ash/launcher/alternate_app_list_button.cc
@@ -21,6 +21,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/compositor/scoped_layer_animation_settings.h"
 #include "ui/gfx/canvas.h"
 #include "ui/gfx/image/image_skia_operations.h"
+#include "ui/views/controls/button/image_button.h"
 
 namespace ash {
 namespace internal {
@@ -132,6 +133,27 @@ void AlternateAppListButton::GetAccessibleState(
     ui::AccessibleViewState* state) {
   state->role = ui::AccessibilityTypes::ROLE_PUSHBUTTON;
   state->name = host_->GetAccessibleName(this);
+}
+
+void AlternateAppListButton::OnGestureEvent(ui::GestureEvent* event) {
+  switch (event->type()) {
+    case ui::ET_GESTURE_SCROLL_BEGIN:
+      host_->PointerPressedOnButton(this, LauncherButtonHost::TOUCH, *event);
+      event->SetHandled();
+      return;
+    case ui::ET_GESTURE_SCROLL_UPDATE:
+      host_->PointerDraggedOnButton(this, LauncherButtonHost::TOUCH, *event);
+      event->SetHandled();
+      return;
+    case ui::ET_GESTURE_SCROLL_END:
+    case ui::ET_SCROLL_FLING_START:
+      host_->PointerReleasedOnButton(this, LauncherButtonHost::TOUCH, false);
+      event->SetHandled();
+      return;
+    default:
+      ImageButton::OnGestureEvent(event);
+      return;
+  }
 }
 
 }  // namespace internal
