50d52ad65bdb8046b0f01dd33bf81c0842896a88
wolenetz@chromium.org <wolenetz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-17 19:55:45 +0000

    Setup DemuxerReady duration_ms parameter in MSP unittests
    Should fix bug 260868, to be confirmed once fyi bot cycles green.
    
    BUG=260868
    R=qinmin@chromium.org,acolwell@chromium.org
    TEST=media_unittests suite passes locally on Nexus 4
    
    Review URL: https://chromiumcodereview.appspot.com/19423003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212117 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/media_source_player_unittest.cc
+++ media/base/android/media_source_player_unittest.cc
@@ -17,6 +17,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+static const int kDefaultDurationInMs = 10000;
+
 // Mock of MediaPlayerManager for testing purpose
 class MockMediaPlayerManager : public MediaPlayerManager {
  public:
@@ -105,6 +107,7 @@ class MediaSourcePlayerTest : public testing::Test {
     params.audio_channels = 2;
     params.audio_sampling_rate = 44100;
     params.is_audio_encrypted = false;
+    params.duration_ms = kDefaultDurationInMs;
     scoped_refptr<DecoderBuffer> buffer = ReadTestDataFile("vorbis-extradata");
     params.audio_extra_data = std::vector<uint8>(
         buffer->GetData(),
@@ -117,6 +120,7 @@ class MediaSourcePlayerTest : public testing::Test {
     params.video_codec = kCodecVP8;
     params.video_size = gfx::Size(320, 240);
     params.is_video_encrypted = false;
+    params.duration_ms = kDefaultDurationInMs;
     Start(params);
   }
 
@@ -187,6 +191,7 @@ TEST_F(MediaSourcePlayerTest, StartAudioDecoderWithInvalidConfig) {
   params.audio_channels = 2;
   params.audio_sampling_rate = 44100;
   params.is_audio_encrypted = false;
+  params.duration_ms = kDefaultDurationInMs;
   uint8 invalid_codec_data[] = { 0x00, 0xff, 0xff, 0xff, 0xff };
   params.audio_extra_data.insert(params.audio_extra_data.begin(),
                                  invalid_codec_data, invalid_codec_data + 4);
@@ -291,6 +296,7 @@ TEST_F(MediaSourcePlayerTest, StartAfterSeekFinish) {
   params.audio_channels = 2;
   params.audio_sampling_rate = 44100;
   params.is_audio_encrypted = false;
+  params.duration_ms = kDefaultDurationInMs;
   player_->DemuxerReady(params);
   EXPECT_EQ(NULL, GetMediaDecoderJob(true));
   EXPECT_EQ(0, manager_->num_requests());
@@ -361,6 +367,7 @@ TEST_F(MediaSourcePlayerTest, DecoderJobsCannotStartWithoutAudio) {
   params.video_codec = kCodecVP8;
   params.video_size = gfx::Size(320, 240);
   params.is_video_encrypted = false;
+  params.duration_ms = kDefaultDurationInMs;
   Start(params);
   EXPECT_EQ(0, manager_->num_requests());
 
