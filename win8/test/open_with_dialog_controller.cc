cb24e6ffef5cacda89f6ed77cce0dbb25f491255
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 02:06:34 +0000

    Use a direct include of the message_loop header in third_party/, win8/.
    BUG=260807
    TEST=none
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19510006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212205 0039d316-1c4b-4281-b951-d872f2087c98

--- win8/test/open_with_dialog_controller.cc
+++ win8/test/open_with_dialog_controller.cc
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop.h"
+#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/thread_task_runner_handle.h"
 #include "base/threading/thread_checker.h"
