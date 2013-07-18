86c0e21cb89b255e95cc17dd99b78410cc078a99
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 04:06:31 +0000

    Check in the irc lib from sourceforge so that we don't need to autoinstall it.
    R=ojan@chromium.org
    BUG=230706
    
    Review URL: https://codereview.chromium.org/19676003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154465 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/thirdparty/__init___unittest.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/thirdparty/__init___unittest.py
@@ -35,6 +35,4 @@ class ThirdpartyTest(unittest.TestCase):
     def test_imports(self):
         # This method tests that we can actually import everything.
         # unused-variable, import failures - pylint: disable-msg=W0612,E0611,F0401
-        import webkitpy.thirdparty.autoinstalled.irc.ircbot
-        import webkitpy.thirdparty.autoinstalled.irc.irclib
         import webkitpy.thirdparty.autoinstalled.mechanize
