6d2db709d4cf8c5b29bc0d3966c174e14a16b750
jaredshumway94@gmail.com <jaredshumway94@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 04:18:37 +0000

    Doc server broken link detection
    The integration test, when run with -a, now detects and reports broken links and orphaned pages.
    
    Broken links either link to an invalid page (404) or have an invalid anchor. Orphaned pages are pages that cannot be navigated to from the home pages just by clicking links.
    
    BUG=147747
    
    Review URL: https://chromiumcodereview.appspot.com/17816005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212240 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/handler.py
+++ chrome/common/extensions/docs/server2/handler.py
@@ -4,8 +4,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 
 from cron_servlet import CronServlet
-from patch_servlet import PatchServlet
 from instance_servlet import InstanceServlet
+from patch_servlet import PatchServlet
 from servlet import Servlet, Request, Response
 
 _DEFAULT_SERVLET = InstanceServlet.GetConstructor()
