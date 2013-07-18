62da1a2ffb5503d86ad9af6ffea7ba3676304f11
pkotwicz@chromium.org <pkotwicz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 19:13:09 +0000

    Fix drawing the inactive tab background when using --use-core-animation
    BUG=260139
    TEST=Manual, see bug
    R=ccameron1
    TBR=sail
    
    Review URL: https://chromiumcodereview.appspot.com/18968003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212388 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/cocoa/nsgraphics_context_additions.mm
+++ ui/base/cocoa/nsgraphics_context_additions.mm
@@ -13,7 +13,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     NSPoint bottomLeft = NSZeroPoint;
     if ([view isFlipped])
       bottomLeft.y = NSMaxY([view bounds]);
-    phase.y -= [view convertPoint:bottomLeft toView:nil].y;
+    NSPoint offset = [view convertPoint:bottomLeft toView:nil];
+    phase.x -= offset.x;
+    phase.y -= offset.y;
   }
   [self setPatternPhase:phase];
 }
