7c19dcbfc6dde7c0fbb509f3ddce644a24d1080a
yfriedman@chromium.org <yfriedman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 02:32:46 +0000

    [Android] Unify all string log tags to use a simple constant.
    Per attached bug, this is cheaper and it's good to a have a consistent
    style.
    
    BUG=146559
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/19485004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212215 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
+++ content/public/android/java/src/org/chromium/content/browser/accessibility/BrowserAccessibilityManager.java
@@ -35,7 +35,7 @@ import java.util.List;
  */
 @JNINamespace("content")
 public class BrowserAccessibilityManager {
-    private static final String TAG = BrowserAccessibilityManager.class.getSimpleName();
+    private static final String TAG = "BrowserAccessibilityManager";
 
     private ContentViewCore mContentViewCore;
     private AccessibilityManager mAccessibilityManager;
--- sync/android/java/src/org/chromium/sync/signin/ChromeSigninController.java
+++ sync/android/java/src/org/chromium/sync/signin/ChromeSigninController.java
@@ -22,7 +22,7 @@ public class ChromeSigninController {
         void onClearSignedInUser();
     }
 
-    public static final String TAG = ChromeSigninController.class.getSimpleName();
+    public static final String TAG = "ChromeSigninController";
 
     @VisibleForTesting
     public static final String SIGNED_IN_ACCOUNT_KEY = "google.services.username";
