ceab5a9b052a685ac28a807d38baa1ac1475e623
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 04:53:50 +0000

    Remove RemoteFileSystemProxy from WebkitFileStreamWriterImpl.
    This CL removes RemoteFileSystemProxy from WebkitFileWriterImpl, as a
    preparation of removing RemoteFileSystemProxy. The implementation of
    CreateWritableSnapshotFile, which WebkitFileStreamWriterImpl depends, is
    extracted from FileSystemProxy to fileapi_worker.
    The arguments of WebkitFileStreamWriterImpl are re-sorted based on
    WebkitFileStreamReaderImpl.
    
    BUG=253837
    TEST=Ran unit_tests and browser_tests --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*"
    
    Review URL: https://chromiumcodereview.appspot.com/19034005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212255 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system_backend_delegate.cc
+++ chrome/browser/chromeos/drive/file_system_backend_delegate.cc
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/drive/file_system_backend_delegate.h"
 
 #include "base/bind.h"
+#include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/chromeos/drive/async_file_util.h"
 #include "chrome/browser/chromeos/drive/file_system_util.h"
@@ -17,7 +18,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "webkit/browser/blob/file_stream_reader.h"
 #include "webkit/browser/fileapi/async_file_util.h"
 #include "webkit/browser/fileapi/external_mount_points.h"
+#include "webkit/browser/fileapi/file_system_context.h"
 #include "webkit/browser/fileapi/file_system_task_runners.h"
+#include "webkit/browser/fileapi/file_system_url.h"
 #include "webkit/browser/fileapi/remote_file_system_proxy.h"
 
 using content::BrowserThread;
@@ -71,14 +74,14 @@ FileSystemBackendDelegate::CreateFileStreamWriter(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
   DCHECK_EQ(fileapi::kFileSystemTypeDrive, url.type());
 
-  fileapi::RemoteFileSystemProxyInterface* proxy =
-      mount_points_->GetRemoteFileSystemProxy(url.filesystem_id());
-  if (!proxy)
+  base::FilePath file_path = util::ExtractDrivePathFromFileSystemUrl(url);
+  if (file_path.empty())
     return scoped_ptr<fileapi::FileStreamWriter>();
 
   return scoped_ptr<fileapi::FileStreamWriter>(
       new internal::WebkitFileStreamWriterImpl(
-          proxy, url, offset, context->task_runners()->file_task_runner()));
+          base::Bind(&util::GetFileSystemByProfileId, profile_id_),
+          context->task_runners()->file_task_runner(),file_path, offset));
 }
 
 }  // namespace drive
