5b40734d7df8209f50408ecd9319d1b4516cd0b2
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 22:08:14 +0000

    One more change that should have been in r212446.
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212450 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/debugger/debugger_api.cc
+++ chrome/browser/extensions/api/debugger/debugger_api.cc
@@ -85,7 +85,7 @@ class ExtensionDevToolsClientHost : public DevToolsClientHost,
       const std::string& extension_id,
       const std::string& extension_name,
       const Debuggee& debuggee,
-      ExtensionDevToolsInfoBarDelegate* infobar_delegate);
+      ExtensionDevToolsInfoBarDelegate* infobar);
 
   virtual ~ExtensionDevToolsClientHost();
 
@@ -120,7 +120,7 @@ class ExtensionDevToolsClientHost : public DevToolsClientHost,
   typedef std::map<int, scoped_refptr<DebuggerSendCommandFunction> >
       PendingRequests;
   PendingRequests pending_requests_;
-  ExtensionDevToolsInfoBarDelegate* infobar_delegate_;
+  ExtensionDevToolsInfoBarDelegate* infobar_;
   OnDetach::Reason detach_reason_;
 
   DISALLOW_COPY_AND_ASSIGN(ExtensionDevToolsClientHost);
@@ -306,12 +306,12 @@ ExtensionDevToolsClientHost::ExtensionDevToolsClientHost(
     const std::string& extension_id,
     const std::string& extension_name,
     const Debuggee& debuggee,
-    ExtensionDevToolsInfoBarDelegate* infobar_delegate)
+    ExtensionDevToolsInfoBarDelegate* infobar)
     : profile_(profile),
       agent_host_(agent_host),
       extension_id_(extension_id),
       last_request_id_(0),
-      infobar_delegate_(infobar_delegate),
+      infobar_(infobar),
       detach_reason_(OnDetach::REASON_TARGET_CLOSED) {
   CopyDebuggee(&debuggee_, debuggee);
 
@@ -331,8 +331,8 @@ ExtensionDevToolsClientHost::ExtensionDevToolsClientHost(
   DevToolsManager::GetInstance()->RegisterDevToolsClientHostFor(
       agent_host_.get(), this);
 
-  if (infobar_delegate_) {
-    infobar_delegate_->set_client_host(this);
+  if (infobar_) {
+    infobar_->set_client_host(this);
     registrar_.Add(
         this, chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED,
         content::Source<InfoBarService>(InfoBarService::FromWebContents(
@@ -346,10 +346,10 @@ ExtensionDevToolsClientHost::~ExtensionDevToolsClientHost() {
   // Close() us.
   registrar_.RemoveAll();
 
-  if (infobar_delegate_) {
-    infobar_delegate_->set_client_host(NULL);
+  if (infobar_) {
+    infobar_->set_client_host(NULL);
     InfoBarService::FromWebContents(WebContents::FromRenderViewHost(
-        agent_host_->GetRenderViewHost()))->RemoveInfoBar(infobar_delegate_);
+        agent_host_->GetRenderViewHost()))->RemoveInfoBar(infobar_);
   }
   AttachedClientHosts::GetInstance()->Remove(this);
 }
@@ -417,9 +417,8 @@ void ExtensionDevToolsClientHost::Observe(
     Close();
   } else {
     DCHECK_EQ(chrome::NOTIFICATION_TAB_CONTENTS_INFOBAR_REMOVED, type);
-    if (content::Details<InfoBarRemovedDetails>(details)->first ==
-        infobar_delegate_) {
-      infobar_delegate_ = NULL;
+    if (content::Details<InfoBarRemovedDetails>(details)->first == infobar_) {
+      infobar_ = NULL;
       SendDetachedEvent();
       Close();
     }
