f1653364514aa0607f59d9ffc5c28de9961913eb
jennyz@chromium.org <jennyz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 04:42:43 +0000

    Enable audio selection menu by default and fix unitializated memory issue in tray_audio.cc.
    BUG=259989
    TBR=jamescook
    
    Review URL: https://chromiumcodereview.appspot.com/19645002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212250 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash_switches.cc
+++ ash/ash_switches.cc
@@ -45,6 +45,13 @@ const char kAshDefaultGuestWallpaperSmall[] =
 const char kAshDefaultWallpaperLarge[] = "ash-default-wallpaper-large";
 const char kAshDefaultWallpaperSmall[] = "ash-default-wallpaper-small";
 
+#if defined(OS_CHROMEOS)
+// Disable the status tray volume menu for allowing the user to choose an audio
+// input and output device.
+const char kAshDisableAudioDeviceMenu[] =
+    "ash-disable-audio-device-menu";
+#endif
+
 // Disable auto window maximization logic.
 const char kAshDisableAutoMaximizing[] = "ash-disable-auto-maximizing";
 
@@ -57,8 +64,10 @@ const char kAshDisableAutoWindowPlacement[] =
 const char kAshDisableDisplayChangeLimiter[] =
     "ash-disable-display-change-limiter";
 
+#if defined(OS_CHROMEOS)
 // Disable the new cras audio handler.
 const char kAshDisableNewAudioHandler[] = "ash-disable-new-audio-handler";
+#endif
 
 // If present new lock animations are enabled.
 const char kAshDisableNewLockAnimations[] = "ash-disable-new-lock-animations";
@@ -84,11 +93,6 @@ const char kAshDisableSoftwareMirroring[] = "ash-disable-software-mirroring";
 const char kAshDisableUsbChargerNotification[] =
     "ash-disable-usb-charger-notification";
 #endif
-
-// Extend the status tray volume item to allow the user to choose an audio
-// input and output device.
-const char kAshEnableAudioDeviceMenu[] =
-    "ash-enable-audio-device-menu";
 
 // Enable advanced gestures (e.g. for window management).
 const char kAshEnableAdvancedGestures[] = "ash-enable-advanced-gestures";
@@ -182,6 +186,12 @@ const char kAshDisableDragAndDropAppListToLauncher[] =
 // in maximized mode.
 const char kForcedMaximizeMode[] = "forced-maximize-mode";
 
+bool UseAlternateShelfLayout() {
+  return CommandLine::ForCurrentProcess()->
+        HasSwitch(ash::switches::kAshUseAlternateShelfLayout);
+}
+
+#if defined(OS_CHROMEOS)
 bool UseNewAudioHandler() {
   return !CommandLine::ForCurrentProcess()->
       HasSwitch(ash::switches::kAshDisableNewAudioHandler);
@@ -189,16 +199,10 @@ bool UseNewAudioHandler() {
 
 bool ShowAudioDeviceMenu() {
   return ash::switches::UseNewAudioHandler() &&
-      CommandLine::ForCurrentProcess()->
-          HasSwitch(ash::switches::kAshEnableAudioDeviceMenu);
+      !CommandLine::ForCurrentProcess()->
+          HasSwitch(ash::switches::kAshDisableAudioDeviceMenu);
 }
 
-bool UseAlternateShelfLayout() {
-  return CommandLine::ForCurrentProcess()->
-        HasSwitch(ash::switches::kAshUseAlternateShelfLayout);
-}
-
-#if defined(OS_CHROMEOS)
 bool UseUsbChargerNotification() {
   return !CommandLine::ForCurrentProcess()->
         HasSwitch(ash::switches::kAshDisableUsbChargerNotification);
