461c636be5710b5962cc6823bb0bc8f7ef5be112
asargent@chromium.org <asargent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 18:04:20 +0000

    Revert 212359 "Delete DeviceController instances before HostCont..."
    This broke the Android builder with the following errors:
    
    http://build.chromium.org/p/chromium.linux/buildstatus?builder=Android%20Tests&number=6275
    
    
    > Delete DeviceController instances before HostController ones get deleted.
    > 
    > While the Forwarder class on the device could get deleted when it detected an
    > error on a socket, the corresponding DeviceController could stick around. This
    > resulted in an excessive amount of file descriptors staying open that could
    > make the net_unittests fail.
    > 
    > This CL adds an explicit unmap command that gets sent by the host to the device
    > right before the host controller gets deleted. When the device receives this
    > message, it deletes the DeviceController instance corresponding to the port
    > being unmapped.
    > 
    > BUG=242846
    > R=bulach@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/19519007
    
    TBR=pliard@chromium.org
    
    Review URL: https://codereview.chromium.org/19771005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212368 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/android/forwarder2/host_controller.h
+++ tools/android/forwarder2/host_controller.h
@@ -59,8 +59,6 @@ class HostController {
   // descriptors.
   scoped_ptr<Socket> CreateSocket();
 
-  void SelfDelete();
-
   Socket adb_control_socket_;
   int device_port_;
   const std::string forward_to_host_;
