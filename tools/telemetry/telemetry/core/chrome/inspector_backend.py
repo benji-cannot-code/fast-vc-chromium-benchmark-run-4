62c10d122256b8f1c12996254c4946689ccccbd3
bulach@chromium.org <bulach@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 22:56:30 +0000

    Telemetry: pass exc_info for TabCrashException.
    This exception on itself can be misleading, pass more information to help
    debugging.
    
    BUG=253307
    
    Review URL: https://chromiumcodereview.appspot.com/19763002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212464 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/chrome/inspector_backend.py
+++ tools/telemetry/telemetry/core/chrome/inspector_backend.py
@@ -49,7 +49,7 @@ class InspectorBackend(object):
       self._socket = websocket.create_connection(self._debugger_url)
     except (websocket.WebSocketException):
       if self._browser_backend.IsBrowserRunning():
-        raise exceptions.TabCrashException()
+        raise exceptions.TabCrashException(sys.exc_info()[1])
       else:
         raise exceptions.BrowserGoneException()
 
@@ -287,7 +287,7 @@ class InspectorBackend(object):
             self._debugger_url):
           raise util.TimeoutException(
             'Timed out waiting for reply. This is unusual.')
-        raise exceptions.TabCrashException()
+        raise exceptions.TabCrashException(sys.exc_info()[1])
 
       res = json.loads(data)
       logging.debug('got [%s]', data)
