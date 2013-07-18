78cc279c21515816aa04e9ae51e8cdcac3e8f676
kenneth.r.christiansen@intel.com <kenneth.r.christiansen@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 00:22:41 +0000

    Atomicize HTMLAnchorElement.hash before passing it to JS
    Backport of WebKit patch by Andreas Kling <akling@apple.com>,
    reviewed by Sam Weinig (http://webkit.org/b/118748)
    
    Instead of generating a unique string from '#' + the URL hash every time
    HTMLAnchorElement.hash is queried, turn it into an AtomicString. This
    prevents massive memory usage in scripts that fetch .hash a lot.
    
    Reduces memory consumption by 4.65 MB when viewing the full HTML5 spec at
    <http://whatwg.org/c>
    
    Review URL: https://chromiumcodereview.appspot.com/19677003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154447 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLAnchorElement.cpp
@@ -376,7 +376,9 @@ String HTMLAnchorElement::target() const
 String HTMLAnchorElement::hash() const
 {
     String fragmentIdentifier = href().fragmentIdentifier();
-    return fragmentIdentifier.isEmpty() ? emptyString() : "#" + fragmentIdentifier;
+    if (fragmentIdentifier.isEmpty())
+        return emptyString();
+    return AtomicString(String("#" + fragmentIdentifier));
 }
 
 void HTMLAnchorElement::setHash(const String& value)
