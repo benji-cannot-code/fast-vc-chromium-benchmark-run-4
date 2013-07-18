1ea94b61ffae47aa66ba4f13f0bca461d589811f
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 18:55:20 +0000

    Add explicit "ppapi/" to #includes in chrome/test/data/nacl
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=2500
    TEST=build
    
    Review URL: https://codereview.chromium.org/19740002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212380 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/nacl/pnacl_dyncode_syscall_disabled/pnacl_dyncode_syscall_disabled.cc
+++ chrome/test/data/nacl/pnacl_dyncode_syscall_disabled/pnacl_dyncode_syscall_disabled.cc
@@ -11,8 +11,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 
 #include "native_client/src/shared/platform/nacl_check.h"
-#include "native_client/tests/ppapi_test_lib/get_browser_interface.h"
-#include "native_client/tests/ppapi_test_lib/test_interface.h"
+#include "ppapi/native_client/tests/ppapi_test_lib/get_browser_interface.h"
+#include "ppapi/native_client/tests/ppapi_test_lib/test_interface.h"
 
 namespace {
 
