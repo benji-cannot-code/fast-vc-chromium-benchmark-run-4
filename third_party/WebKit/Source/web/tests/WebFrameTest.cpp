e0d6671a61eb122db9e71a79137d2acf469fae69
gavinp@chromium.org <gavinp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 19:49:02 +0000

    Mark WebFrameTest.ContextNotificationsLoadUnload as Flaky.
    This test fails sometimes, it's been causing false alarms in
    garden-o-matic.
    
    TBR=cbiesinger@chromium.org
    BUG=261691
    
    Review URL: https://codereview.chromium.org/19771006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154509 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/tests/WebFrameTest.cpp
+++ third_party/WebKit/Source/web/tests/WebFrameTest.cpp
@@ -1668,7 +1668,8 @@ public:
     }
 };
 
-TEST_F(WebFrameTest, ContextNotificationsLoadUnload)
+// TODO(aa): Deflake this test.
+TEST_F(WebFrameTest, FLAKY_ContextNotificationsLoadUnload)
 {
     v8::HandleScope handleScope;
 
