bdd4ed2349dd3e7f19ace527004ee8cd6b5ef6f0
csharp@chromium.org <csharp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 18:50:12 +0000

    [Autofill] Allow the data list to change when the Popup is Visible
    If the Autofill popup is showing and the data list values for the
    element change, allow the popup to update to reflect the new changes.
    
    TBR=jln@chromium.org
    BUG=131003
    
    Review URL: https://chromiumcodereview.appspot.com/18102002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212377 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/autofill_manager.cc
+++ components/autofill/core/browser/autofill_manager.cc
@@ -617,7 +617,7 @@ void AutofillManager::OnDidShowAutofillSuggestions(bool is_new_popup) {
 
 void AutofillManager::OnHideAutofillUi() {
   if (!IsAutofillEnabled())
-   return;
+    return;
 
   manager_delegate_->HideAutofillPopup();
   manager_delegate_->HideAutocheckoutBubble();
@@ -686,19 +686,11 @@ void AutofillManager::OnShowPasswordSuggestions(
 }
 
 void AutofillManager::OnSetDataList(const std::vector<base::string16>& values,
-                                    const std::vector<base::string16>& labels,
-                                    const std::vector<base::string16>& icons,
-                                    const std::vector<int>& unique_ids) {
-  if (labels.size() != values.size() ||
-      icons.size() != values.size() ||
-      unique_ids.size() != values.size()) {
+                                    const std::vector<base::string16>& labels) {
+  if (values.size() != labels.size())
     return;
-  }
 
-  external_delegate_->SetCurrentDataListValues(values,
-                                               labels,
-                                               icons,
-                                               unique_ids);
+  external_delegate_->SetCurrentDataListValues(values, labels);
 }
 
 void AutofillManager::OnRequestAutocomplete(
