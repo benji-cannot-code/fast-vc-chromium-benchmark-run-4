2e8535ffebb36f1bf857c96e66afda949006ebe1
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 20:10:01 +0000

    Fix moved header
    BUG=NONE
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/19592010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212407 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/fileapi/itunes_data_provider_browsertest.cc
+++ chrome/browser/media_galleries/fileapi/itunes_data_provider_browsertest.cc
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/format_macros.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop.h"
+#include "base/message_loop/message_loop.h"
 #include "base/run_loop.h"
 #include "base/strings/stringprintf.h"
 #include "chrome/browser/media_galleries/fileapi/itunes_data_provider.h"
