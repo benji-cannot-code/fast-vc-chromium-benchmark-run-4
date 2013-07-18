359827e443dcb813bac12d68bee45a68dac0b88e
vandebo@chromium.org <vandebo@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 19:01:57 +0000

    Add a test for ITunesDataProvider (try two).
    It's a browser test instead of a unit test because the utility process gets invoked.
    Patch set one is what was reviewed and committed in https://codereview.chromium.org/17616006/
    
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/18348019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212384 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/imported_media_gallery_registry.h
+++ chrome/browser/media_galleries/imported_media_gallery_registry.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace itunes {
 class ITunesDataProvider;
+class ITunesDataProviderTest;
 }
 
 namespace picasa {
@@ -50,6 +51,7 @@ class ImportedMediaGalleryRegistry {
 
  private:
   friend struct base::DefaultLazyInstanceTraits<ImportedMediaGalleryRegistry>;
+  friend class itunes::ITunesDataProviderTest;
 
   ImportedMediaGalleryRegistry();
   virtual ~ImportedMediaGalleryRegistry();
