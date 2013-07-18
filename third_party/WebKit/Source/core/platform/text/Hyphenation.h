1093feaf6f48833045b6e687e1979781c313f594
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 19:07:30 +0000

    lastHyphenLocation shouldn't require bloatedCharacters
    Rather than bloating characters to create a UChar* and passing that array to
    Chromium, I've taught Chromium how to receive a WebString in
    https://codereview.chromium.org/19646004. That means we can use substring here
    without doing extra work.
    
    R=eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/19652005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154506 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/text/Hyphenation.h
+++ third_party/WebKit/Source/core/platform/text/Hyphenation.h
@@ -33,7 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 bool canHyphenate(const AtomicString& localeIdentifier);
-size_t lastHyphenLocation(const UChar*, size_t length, size_t beforeIndex, const AtomicString& localeIdentifier);
+size_t lastHyphenLocation(const String& word, size_t beforeIndex, const AtomicString& localeIdentifier);
 
 } // namespace WebCore
 
