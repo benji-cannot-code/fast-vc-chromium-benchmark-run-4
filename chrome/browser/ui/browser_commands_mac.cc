fa0dc6d42feebf5d36c11a32e8725bc86ef203b2
rohitrao@chromium.org <rohitrao@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 19:02:41 +0000

    Disables presentation mode when simplified fullscreen is enabled.
    In simplified fullscreen, presentation mode is removed and fullscreen takes on a 10.7-style UI.  When HTML5 fullscreen is triggered, the tabstrip and toolbar are removed completely and only the web content is shown, with no overlay.
    
    BUG=None
    TEST=With --enable-simplified-fullscreen, the menu shortcuts for both "Enter Fullscreen" and "Enter Presentation Mode" should enter a fullscreen mode where the toolbar and tabstrip are always visible.
    
    Review URL: https://chromiumcodereview.appspot.com/18551008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212386 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/browser_commands_mac.cc
+++ chrome/browser/ui/browser_commands_mac.cc
@@ -5,16 +5,23 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/browser_commands_mac.h"
 
+#include "base/command_line.h"
 #include "base/mac/mac_util.h"
 #include "chrome/browser/fullscreen.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/fullscreen/fullscreen_controller.h"
+#include "chrome/common/chrome_switches.h"
 
 namespace chrome {
 
 void ToggleFullscreenWithChromeOrFallback(Browser* browser) {
-  if (chrome::mac::SupportsSystemFullscreen())
+  // In simplified fullscreen mode, the "WithChrome" variant does not exist.
+  // Call into the standard cross-platform fullscreen method instead.
+  const CommandLine* command_line = CommandLine::ForCurrentProcess();
+  if (command_line->HasSwitch(switches::kEnableSimplifiedFullscreen))
+    ToggleFullscreenMode(browser);
+  else if (chrome::mac::SupportsSystemFullscreen())
     browser->fullscreen_controller()->ToggleFullscreenWithChrome();
   else
     ToggleFullscreenMode(browser);
