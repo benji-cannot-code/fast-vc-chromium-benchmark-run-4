d49c373637c78c9731ace7cc05660d1bb2674c68
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 18:08:38 +0000

    Delete some more unused or singly-use FrameLoaderClient functions.
    didFinishLoad() exists to call a plugin load observer that will never be there
    for a same document navigation.
    
    canShowMimeType() is called once in DocumentLoader, so make it a private static
    in DocumentLoader.cpp
    
    generatedMIMETypeForURLScheme() is never called.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/19696006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154498 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/DocumentLoader.cpp
+++ third_party/WebKit/Source/core/loader/DocumentLoader.cpp
@@ -56,6 +56,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/page/Page.h"
 #include "core/page/Settings.h"
 #include "core/platform/Logging.h"
+#include "core/plugins/PluginData.h"
+#include "public/platform/Platform.h"
+#include "public/platform/WebMimeRegistry.h"
 #include "weborigin/SchemeRegistry.h"
 #include "weborigin/SecurityPolicy.h"
 #include "wtf/Assertions.h"
@@ -500,6 +503,14 @@ void DocumentLoader::willSendRequest(ResourceRequest& newRequest, const Resource
         stopLoadingForPolicyChange();
 }
 
+static bool canShowMIMEType(const String& mimeType, Page* page)
+{
+    if (WebKit::Platform::current()->mimeRegistry()->supportsMIMEType(mimeType) == WebKit::WebMimeRegistry::IsSupported)
+        return true;
+    PluginData* pluginData = page->pluginData();
+    return !mimeType.isEmpty() && pluginData && pluginData->supportsMimeType(mimeType);
+}
+
 bool DocumentLoader::shouldContinueForResponse() const
 {
     if (m_substituteData.isValid())
@@ -518,7 +529,7 @@ bool DocumentLoader::shouldContinueForResponse() const
         return false;
     }
 
-    if (!frameLoader()->client()->canShowMIMEType(m_response.mimeType()))
+    if (!canShowMIMEType(m_response.mimeType(), m_frame->page()))
         return false;
 
     // Prevent remote web archives from loading because they can claim to be from any domain and thus avoid cross-domain security checks.
@@ -917,8 +928,7 @@ bool DocumentLoader::maybeLoadEmpty()
 
     if (m_request.url().isEmpty() && !frameLoader()->stateMachine()->creatingInitialEmptyDocument())
         m_request.setURL(blankURL());
-    String mimeType = shouldLoadEmpty ? "text/html" : frameLoader()->client()->generatedMIMETypeForURLScheme(m_request.url().protocol());
-    m_response = ResourceResponse(m_request.url(), mimeType, 0, String(), String());
+    m_response = ResourceResponse(m_request.url(), "text/html", 0, String(), String());
     finishedLoading(monotonicallyIncreasingTime());
     return true;
 }
