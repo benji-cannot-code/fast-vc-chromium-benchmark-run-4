096418b92da678a66f974ada325718fe4d6fba0b
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 04:19:57 +0000

    Remove autoinstaller code from webkitpy (no longer needed!).
    R=ojan@chromium.org
    BUG=230706
    
    Review URL: https://codereview.chromium.org/19696004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154468 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/common/system/outputcapture.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/common/system/outputcapture.py
@@ -30,9 +30,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Class for unittest support.  Used for capturing stderr/stdout.
 
 import logging
-import unittest  # Don't use unittest2 here as the autoinstaller may not have it yet.
 import sys
 from StringIO import StringIO
+
+import webkitpy.thirdparty.unittest2 as unittest
 
 
 class OutputCapture(object):
