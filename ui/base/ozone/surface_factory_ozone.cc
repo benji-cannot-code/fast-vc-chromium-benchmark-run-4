d785739ca3d7707b3f580f98763e55e12330eac7
rjkroege@chromium.org <rjkroege@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 20:37:28 +0000

    Cleaned up includes in SurfaceFactoryOzone.cc
    BUG=none
    R=sky@chromium.org
    
    Review URL: https://codereview.chromium.org/19235012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212416 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ozone/surface_factory_ozone.cc
+++ ui/base/ozone/surface_factory_ozone.cc
@@ -5,8 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/base/ozone/surface_factory_ozone.h"
 
-#include "base/lazy_instance.h"
-#include "base/memory/scoped_ptr.h"
+#include <stdlib.h>
 
 namespace ui {
 
