f678039ad5a344ca2ac6c7d10ab388c0672856b8
marja@chromium.org <marja@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-18 08:47:08 +0000

    Remove ScopedPersistent::deprecatedGet usage from ExceptionState.
    ScopedPersistent::deprecatedGet will be removed soon.
    
    BUG=236290
    
    Review URL: https://chromiumcodereview.appspot.com/19373002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154479 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ExceptionState.h
+++ third_party/WebKit/Source/bindings/v8/ExceptionState.h
@@ -62,7 +62,7 @@ public:
             throwDOMException(m_code);
         }
 
-        V8ThrowException::throwError(m_exception.deprecatedGet());
+        V8ThrowException::throwError(m_exception.newLocal(m_isolate));
         return true;
     }
 
