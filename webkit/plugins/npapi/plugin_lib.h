032fd19bb534a3713bf2c0fa41b536123664bfd3
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-18 18:12:48 +0000

    Remove support for internal NPAPI plugins (i.e. those linked into the binary). We don't use that anymore.
    BUG=237249
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/19745002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212369 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/plugins/npapi/plugin_lib.h
+++ webkit/plugins/npapi/plugin_lib.h
@@ -13,9 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/ref_counted.h"
 #include "base/native_library.h"
 #include "build/build_config.h"
-#include "webkit/plugins/npapi/plugin_list.h"
+#include "third_party/npapi/bindings/nphostapi.h"
 #include "webkit/plugins/npapi/webplugin.h"
 #include "webkit/plugins/webkit_plugins_export.h"
+#include "webkit/plugins/webplugininfo.h"
 
 namespace base {
 class FilePath;
@@ -25,6 +26,18 @@ namespace webkit {
 namespace npapi {
 
 class PluginInstance;
+
+// This struct holds entry points into a plugin.  The entry points are
+// slightly different between Win/Mac and Unixes.  Note that the interface for
+// querying plugins is synchronous and it is preferable to use a higher-level
+// asynchronous information to query information.
+struct PluginEntryPoints {
+#if !defined(OS_POSIX) || defined(OS_MACOSX)
+  NP_GetEntryPointsFunc np_getentrypoints;
+#endif
+  NP_InitializeFunc np_initialize;
+  NP_ShutdownFunc np_shutdown;
+};
 
 // A PluginLib is a single NPAPI Plugin Library, and is the lifecycle
 // manager for new PluginInstances.
@@ -50,8 +63,6 @@ class WEBKIT_PLUGINS_EXPORT PluginLib : public base::RefCounted<PluginLib> {
   // Gets information about this plugin and the mime types that it
   // supports.
   const webkit::WebPluginInfo& plugin_info() { return web_plugin_info_; }
-
-  bool internal() { return internal_; }
 
   //
   // NPAPI functions
@@ -87,9 +98,7 @@ class WEBKIT_PLUGINS_EXPORT PluginLib : public base::RefCounted<PluginLib> {
   friend class base::RefCounted<PluginLib>;
 
   // Creates a new PluginLib.
-  // |entry_points| is non-NULL for internal plugins.
-  PluginLib(const webkit::WebPluginInfo& info,
-            const PluginEntryPoints* entry_points);
+  explicit PluginLib(const webkit::WebPluginInfo& info);
 
   virtual ~PluginLib();
 
@@ -104,7 +113,6 @@ class WEBKIT_PLUGINS_EXPORT PluginLib : public base::RefCounted<PluginLib> {
   void Shutdown();
 
  private:
-  bool internal_;  // True for plugins that are built-in into chrome binaries.
   webkit::WebPluginInfo web_plugin_info_;  // Supported mime types, description
   base::NativeLibrary library_;  // The opened library reference.
   NPPluginFuncs plugin_funcs_;  // The struct of plugin side functions.
--- webkit/plugins/npapi/plugin_list_posix.cc
+++ webkit/plugins/npapi/plugin_list_posix.cc
@@ -30,6 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/sys_string_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "build/build_config.h"
+#include "third_party/npapi/bindings/nphostapi.h"
 
 // These headers must be included in this order to make the declaration gods
 // happy.
