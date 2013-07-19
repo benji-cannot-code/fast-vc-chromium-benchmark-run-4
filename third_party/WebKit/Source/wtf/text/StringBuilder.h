add298b61ebe7e019fdeed392d78efe59595f93b
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-19 03:36:43 +0000

    Finish removing bloatedCharacters
    This removes the last vestiges of bloatedCharacters, including the API for
    creating them. A future CL will remove the underlying mechanism for storing the
    16 bit shadow buffer.
    
    R=eseidel
    BUG=254708
    
    Review URL: https://chromiumcodereview.appspot.com/19729012
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/StringBuilder.h
+++ third_party/WebKit/Source/wtf/text/StringBuilder.h
@@ -43,7 +43,6 @@ public:
         : m_bufferCharacters8(0)
         , m_length(0)
         , m_is8Bit(true)
-        , m_valid16BitShadowLength(0)
     {
     }
 
@@ -256,7 +255,6 @@ public:
         m_buffer = 0;
         m_bufferCharacters8 = 0;
         m_is8Bit = true;
-        m_valid16BitShadowLength = 0;
     }
 
     void swap(StringBuilder& stringBuilder)
@@ -265,7 +263,6 @@ public:
         m_string.swap(stringBuilder.m_string);
         m_buffer.swap(stringBuilder.m_buffer);
         std::swap(m_is8Bit, stringBuilder.m_is8Bit);
-        std::swap(m_valid16BitShadowLength, stringBuilder.m_valid16BitShadowLength);
         std::swap(m_bufferCharacters8, stringBuilder.m_bufferCharacters8);
     }
 
@@ -291,7 +288,6 @@ private:
     };
     unsigned m_length;
     bool m_is8Bit;
-    mutable unsigned m_valid16BitShadowLength;
 };
 
 template <>
--- third_party/WebKit/Source/wtf/unicode/UTF8.cpp
+++ third_party/WebKit/Source/wtf/unicode/UTF8.cpp
@@ -419,7 +419,8 @@ unsigned calculateStringHashAndLengthFromUTF8MaskingTop8Bits(const char* data, c
     return stringHasher.hashWithTop8BitsMasked();
 }
 
-bool equalUTF16WithUTF8(const UChar* a, const UChar* aEnd, const char* b, const char* bEnd)
+template<typename CharType>
+ALWAYS_INLINE bool equalWithUTF8Internal(const CharType* a, const CharType* aEnd, const char* b, const char* bEnd)
 {
     while (b < bEnd) {
         if (isASCII(*b)) {
@@ -455,6 +456,16 @@ bool equalUTF16WithUTF8(const UChar* a, const UChar* aEnd, const char* b, const 
     }
 
     return a == aEnd;
+}
+
+bool equalUTF16WithUTF8(const UChar* a, const UChar* aEnd, const char* b, const char* bEnd)
+{
+    return equalWithUTF8Internal(a, aEnd, b, bEnd);
+}
+
+bool equalLatin1WithUTF8(const LChar* a, const LChar* aEnd, const char* b, const char* bEnd)
+{
+    return equalWithUTF8Internal(a, aEnd, b, bEnd);
 }
 
 } // namespace Unicode
