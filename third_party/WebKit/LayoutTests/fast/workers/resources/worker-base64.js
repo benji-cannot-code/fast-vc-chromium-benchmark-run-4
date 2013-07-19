bccd3b3abe4594d6e998374d6d519f6757b8a2e0
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-19 16:58:41 +0000

    Expose atob() / btoa() to workers
    Expose atob() / btoa() to workers as per the latest Web Workers
    specification:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/workers.html#WorkerGlobalScope-partial
    http://www.whatwg.org/specs/web-apps/current-work/multipage/webappapis.html#windowbase64
    
    This behavior is consistent with Firefox and IE10.
    
    BUG=143502
    R=arv@chromium.org, haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/19749003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154578 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/workers/resources/worker-base64.js
@@ -0,0 +1,10 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+if (this.importScripts)
+    importScripts('../../js/resources/js-test-pre.js');
+
+description("Tests that atob() / btoa() functions are exposed to workers");
+
+shouldBeTrue("typeof atob === 'function'");
+shouldBeTrue("typeof btoa === 'function'");
+
+finishJSTest();
