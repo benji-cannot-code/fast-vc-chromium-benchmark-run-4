a0fabba1067874e712cc10702917481d7ad17d45
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-19 12:44:08 +0000

    DevTools: [CanvasProfiler] Measure replay time in backend instead of front-end.
    We used to show replay time measured in front-end, which included time spent on passing the data from backend to front-end.
    Now the time is measured in backend, and gives a first bit of information about performance.
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/19276004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154568 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CanvasProfileView.js
+++ third_party/WebKit/Source/devtools/front_end/CanvasProfileView.js
@@ -247,12 +247,12 @@ WebInspector.CanvasProfileView.prototype = {
             return;
         this._lastReplayCallIndex = index;
         this._pendingReplayTraceLogEvent = true;
-        var time = Date.now();
         /**
          * @param {?Protocol.Error} error
          * @param {CanvasAgent.ResourceState} resourceState
+         * @param {number} replayTime
          */
-        function didReplayTraceLog(error, resourceState)
+        function didReplayTraceLog(error, resourceState, replayTime)
         {
             delete this._pendingReplayTraceLogEvent;
 
@@ -263,7 +263,7 @@ WebInspector.CanvasProfileView.prototype = {
                 this._currentResourceStates["auto"] = resourceState;
                 this._currentResourceStates[resourceState.id] = resourceState;
 
-                this._debugInfoElement.textContent = "Replay time: " + (Date.now() - time) + "ms";
+                this._debugInfoElement.textContent = "Replay time: " + Number(replayTime).toFixed() + "ms";
                 this._onReplayContextChanged();
             }
 
