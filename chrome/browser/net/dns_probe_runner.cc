352238e1f50c2a73eb09e264759dc3de249d5de7
ttuttle@chromium.org <ttuttle@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 19:46:39 +0000

    Handle invalid DnsConfig in DnsProbeRunner.
    Don't crash in RunProbe if the DnsConfig is invalid; add a test case to the
    DnsProbeRunnerTest for invalid config.
    
    BUG=156415,261588
    TEST=added test case to DnsProbeRunnerTest
    
    Review URL: https://chromiumcodereview.appspot.com/19678015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212631 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/dns_probe_runner.cc
+++ chrome/browser/net/dns_probe_runner.cc
@@ -25,6 +25,7 @@ using net::BoundNetLog;
 using net::DnsClient;
 using net::DnsResponse;
 using net::DnsTransaction;
+using net::DnsTransactionFactory;
 using net::IPAddressNumber;
 using net::IPEndPoint;
 using net::NetLog;
@@ -45,7 +46,7 @@ DnsProbeRunner::Result EvaluateResponse(
       break;
 
     // ERR_NAME_NOT_RESOLVED maps to NXDOMAIN, which means the server is working
-    // but gave us a wrong answer.
+    // but returned a wrong answer.
     case net::ERR_NAME_NOT_RESOLVED:
       return DnsProbeRunner::INCORRECT;
 
@@ -69,15 +70,12 @@ DnsProbeRunner::Result EvaluateResponse(
   TimeDelta ttl;
   DnsResponse::Result result = response->ParseToAddressList(&addr_list, &ttl);
 
-  if (result != DnsResponse::DNS_PARSE_OK) {
+  if (result != DnsResponse::DNS_PARSE_OK)
     return DnsProbeRunner::FAILING;
-  }
-
-  if (addr_list.empty()) {
+  else if (addr_list.empty())
     return DnsProbeRunner::INCORRECT;
-  }
-
-  return DnsProbeRunner::CORRECT;
+  else
+    return DnsProbeRunner::CORRECT;
 }
 
 }  // namespace
@@ -97,7 +95,20 @@ void DnsProbeRunner::RunProbe(const base::Closure& callback) {
   DCHECK(!transaction_.get());
 
   callback_ = callback;
-  transaction_ = client_->GetTransactionFactory()->CreateTransaction(
+  DnsTransactionFactory* factory = client_->GetTransactionFactory();
+  if (!factory) {
+    // If the DnsTransactionFactory is NULL, then the DnsConfig is invalid, so
+    // the runner can't run a transaction.  Return UNKNOWN asynchronously.
+    result_ = UNKNOWN;
+    BrowserThread::PostTask(
+        BrowserThread::IO,
+        FROM_HERE,
+        base::Bind(&DnsProbeRunner::CallCallback,
+                   weak_factory_.GetWeakPtr()));
+    return;
+  }
+
+  transaction_ = factory->CreateTransaction(
       kKnownGoodHostname,
       net::dns_protocol::kTypeA,
       base::Bind(&DnsProbeRunner::OnTransactionComplete,
