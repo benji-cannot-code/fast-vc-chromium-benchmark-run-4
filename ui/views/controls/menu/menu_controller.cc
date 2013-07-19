c8cb15eb89dddee026244b8a920a1c2dd2ba42fc
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 20:29:23 +0000

    views: Fix menu/combobox dropdown auto-scrolling
    this fixes the regression caused by r209598, and improves some menu behaviors.
    
    Before r209598:
    1. starting to type a menu item would not scroll the menu if the item were off the edge
    2. pressing the down arrow would move the selection down, but scroll the entire menu contents up (if scrollbars were visible).
    
    After r209598:
    1. fixed
    2. same
    3. regression: mousing over an item would cause the menu to scroll, placing a new item under the cursor, which would cause the menu to scroll, which would place a new item under the cursor, etc. until the end of the menu was reached
    
    After this patch:
    1. fixed
    2. pressing the down arrow moves the selection down, but only scrolls the menu just enough such that item is visible (matches behavior of <select>s in renderer)
    3. fixed. Bonus: mousing over a partially visible menu item will scroll the menu till that item is visible.
    
    BUG=258182
    
    Review URL: https://chromiumcodereview.appspot.com/19695003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212638 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/controls/menu/menu_controller.cc
+++ ui/views/controls/menu/menu_controller.cc
@@ -92,11 +92,6 @@ bool TitleMatchesMnemonic(MenuItemView* menu, char16 key) {
 
 }  // namespace
 
-// Convenience for scrolling the view such that the origin is visible.
-static void ScrollToVisible(View* view) {
-  view->ScrollRectToVisible(view->GetLocalBounds());
-}
-
 // Returns the first descendant of |view| that is hot tracked.
 static View* GetFirstHotTrackedView(View* view) {
   if (!view)
@@ -797,7 +792,7 @@ void MenuController::SetSelection(MenuItemView* menu_item,
 
   // Notify the new path it is selected.
   for (size_t i = paths_differ_at; i < new_size; ++i) {
-    ScrollToVisible(new_path[i]);
+    new_path[i]->ScrollRectToVisible(new_path[i]->GetLocalBounds());
     new_path[i]->SetSelected(true);
   }
 
@@ -1890,7 +1885,6 @@ void MenuController::IncrementSelection(int delta) {
     // select the first menu item.
     if (item->GetSubmenu()->GetMenuItemCount()) {
       SetSelection(item->GetSubmenu()->GetMenuItemAt(0), SELECTION_DEFAULT);
-      ScrollToVisible(item->GetSubmenu()->GetMenuItemAt(0));
       return;
     }
   }
@@ -1929,7 +1923,6 @@ void MenuController::IncrementSelection(int delta) {
               FindNextSelectableMenuItem(parent, i, delta);
           if (!to_select)
             break;
-          ScrollToVisible(to_select);
           SetSelection(to_select, SELECTION_DEFAULT);
           View* to_make_hot = GetInitialFocusableView(to_select, delta == 1);
           if (to_make_hot && !strcmp(to_make_hot->GetClassName(),
