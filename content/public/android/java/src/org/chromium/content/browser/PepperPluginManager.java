7c5db4b145e3b0569eb6430f2d00d9e5362ec598
kjyoun@google.com <kjyoun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 08:50:54 +0000

    Change chrome-for-tv to load plugins only on Webapp
    BUG=261442
    
    Review URL: https://chromiumcodereview.appspot.com/19689007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212550 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/PepperPluginManager.java
+++ content/public/android/java/src/org/chromium/content/browser/PepperPluginManager.java
@@ -18,6 +18,8 @@ import android.util.Log;
 
 import java.util.List;
 
+import org.chromium.base.ContextTypes;
+
 /**
  * {@link PepperPluginManager} collects meta data about plugins from preloaded android apps
  * that reply to PEPPERPLUGIN intent query.
@@ -89,6 +91,11 @@ public class PepperPluginManager {
      * @return        Description string for plugins
      */
     public static String getPlugins(final Context context) {
+        if (DeviceUtils.isTv(context) &&
+                !ContextTypes.isRunningInWebapp(context)) {
+            // Chrome-for-tv enables plugins only on webapp mode.
+            return null;
+        }
         StringBuffer ret = new StringBuffer();
         PackageManager pm = context.getPackageManager();
         List<ResolveInfo> plugins = pm.queryIntentServices(
