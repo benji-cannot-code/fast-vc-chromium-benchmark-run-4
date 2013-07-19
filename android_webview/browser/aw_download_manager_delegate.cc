5e706b81043a4c04f0a23f4db908180718044cef
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 18:51:33 +0000

    [Android WebView] Fix download kInvalidId DCHECK
    The default implementation of DownloadManagerDelegate::GetNextId just
    returns kInvalidId and so is causing this DCHECK. Fix by overriding in
    AwDownloadManagerDelegate with a static variable implementation similar
    to content shell.
    
    See bug for details why it's not turning android bots red.
    
    BUG=262184
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/19796004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212615 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/aw_download_manager_delegate.cc
+++ android_webview/browser/aw_download_manager_delegate.cc
@@ -41,4 +41,10 @@ bool AwDownloadManagerDelegate::ShouldOpenDownload(
   return true;
 }
 
+void AwDownloadManagerDelegate::GetNextId(
+    const content::DownloadIdCallback& callback) {
+  static uint32 next_id = content::DownloadItem::kInvalidId + 1;
+  callback.Run(next_id++);
+}
+
 }  // namespace android_webview
--- android_webview/browser/aw_download_manager_delegate.h
+++ android_webview/browser/aw_download_manager_delegate.h
@@ -26,6 +26,7 @@ class AwDownloadManagerDelegate : public content::DownloadManagerDelegate {
   virtual bool ShouldOpenDownload(
       content::DownloadItem* item,
       const content::DownloadOpenDelayedCallback& callback) OVERRIDE;
+  virtual void GetNextId(const content::DownloadIdCallback& callback) OVERRIDE;
 };
 
 }  // namespace android_webview
