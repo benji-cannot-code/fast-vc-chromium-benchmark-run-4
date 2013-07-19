409a026f97824169c76607ee229f3e1b1a72c339
gavinp@chromium.org <gavinp@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-19 16:27:02 +0000

    Revert "Reland change to enable deferred image decoding for animated GIFs"
    Lots of image diffs showed up on http://test-results.appspot.com/dashboards/flakiness_dashboard.html#tests=virtual%2Fdeferred%2Ffast%2Fimages%2Fwebp-color-profile-lossy.html
    
    This reverts commit 4110425f5132d8d6f94395799c2d901068e38f6e.
    
    TBR=hclam@chromium.org
    BUG=169377,259786,259783
    
    Review URL: https://codereview.chromium.org/19504009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154576 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
+++ third_party/WebKit/Source/core/platform/graphics/chromium/DeferredImageDecoder.h
@@ -31,9 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/ImageSource.h"
 #include "core/platform/graphics/IntSize.h"
 #include "core/platform/image-decoders/ImageDecoder.h"
-#include "wtf/Forward.h"
-#include "wtf/OwnPtr.h"
-#include "wtf/Vector.h"
+#include <wtf/Forward.h>
 
 namespace WebCore {
 
@@ -74,9 +72,7 @@ public:
 
 private:
     explicit DeferredImageDecoder(PassOwnPtr<ImageDecoder> actualDecoder);
-    void prepareLazyDecodedFrames();
-    SkBitmap createLazyDecodingBitmap(size_t index);
-    void activateLazyDecoding();
+    SkBitmap createLazyDecodingBitmap();
     void setData(PassRefPtr<SharedBuffer>, bool allDataReceived);
 
     RefPtr<SharedBuffer> m_data;
@@ -86,9 +82,8 @@ private:
     String m_filenameExtension;
     IntSize m_size;
     ImageOrientation m_orientation;
-    int m_repetitionCount;
 
-    Vector<OwnPtr<ImageFrame> > m_lazyDecodedFrames;
+    ImageFrame m_lazyDecodedFrame;
     RefPtr<ImageFrameGenerator> m_frameGenerator;
 
     static bool s_enabled;
