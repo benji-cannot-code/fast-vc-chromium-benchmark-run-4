c63efd8df92ce6dbb3f00bb250b9fa33fbfd0e41
mmenke@chromium.org <mmenke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 19:06:12 +0000

    Make DnsTransaction.Start() never complete synchronously. This is an uncommon case, currently only done on certain errors, and complicates logic elsewhere.  Since HostResolverImpl::Jobs have to complete asynchronously anyways, even in those rare cases, there was no performance gain.
    This is a prerequsite for at least one potential
    implementation of simultaneous A/AAAA queries.
    
    BUG=174992
    
    Review URL: https://chromiumcodereview.appspot.com/19776005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212616 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/net/dns_probe_runner.cc
+++ chrome/browser/net/dns_probe_runner.cc
@@ -104,9 +104,7 @@ void DnsProbeRunner::RunProbe(const base::Closure& callback) {
                  weak_factory_.GetWeakPtr()),
       BoundNetLog());
 
-  int rv = transaction_->Start();
-  if (rv != net::ERR_IO_PENDING)
-    OnTransactionComplete(transaction_.get(), rv, NULL);
+  transaction_->Start();
 }
 
 bool DnsProbeRunner::IsRunning() const {
--- net/dns/dns_test_util.cc
+++ net/dns/dns_test_util.cc
@@ -35,7 +35,7 @@ class MockTransaction : public DnsTransaction,
                   const std::string& hostname,
                   uint16 qtype,
                   const DnsTransactionFactory::CallbackType& callback)
-      : result_(MockDnsClientRule::FAIL_SYNC),
+      : result_(MockDnsClientRule::FAIL),
         hostname_(hostname),
         qtype_(qtype),
         callback_(callback),
@@ -60,15 +60,12 @@ class MockTransaction : public DnsTransaction,
     return qtype_;
   }
 
-  virtual int Start() OVERRIDE {
+  virtual void Start() OVERRIDE {
     EXPECT_FALSE(started_);
     started_ = true;
-    if (MockDnsClientRule::FAIL_SYNC == result_)
-      return ERR_NAME_NOT_RESOLVED;
     // Using WeakPtr to cleanly cancel when transaction is destroyed.
     base::MessageLoop::current()->PostTask(
         FROM_HERE, base::Bind(&MockTransaction::Finish, AsWeakPtr()));
-    return ERR_IO_PENDING;
   }
 
  private:
@@ -123,7 +120,7 @@ class MockTransaction : public DnsTransaction,
         EXPECT_TRUE(response.InitParse(nbytes, query));
         callback_.Run(this, OK, &response);
       } break;
-      case MockDnsClientRule::FAIL_ASYNC:
+      case MockDnsClientRule::FAIL:
         callback_.Run(this, ERR_NAME_NOT_RESOLVED, NULL);
         break;
       case MockDnsClientRule::TIMEOUT:
