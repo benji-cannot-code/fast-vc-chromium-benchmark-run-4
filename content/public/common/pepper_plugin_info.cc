5ddde23607ea20775090a31b81182e70ec5fbce3
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-19 16:47:09 +0000

    Small cleanup on WebPluginInfo before moving it to content.
    BUG=237249
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/19812002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212591 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/common/pepper_plugin_info.cc
+++ content/public/common/pepper_plugin_info.cc
@@ -5,6 +5,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/public/common/pepper_plugin_info.h"
 
+#include "base/strings/utf_string_conversions.h"
+
 namespace content {
 
 PepperPluginInfo::PepperPluginInfo()
@@ -15,6 +17,26 @@ PepperPluginInfo::PepperPluginInfo()
 }
 
 PepperPluginInfo::~PepperPluginInfo() {
+}
+
+webkit::WebPluginInfo PepperPluginInfo::ToWebPluginInfo() const {
+  webkit::WebPluginInfo info;
+
+  info.type = is_out_of_process ?
+      (is_sandboxed ?
+        webkit::WebPluginInfo::PLUGIN_TYPE_PEPPER_OUT_OF_PROCESS :
+        webkit::WebPluginInfo::PLUGIN_TYPE_PEPPER_UNSANDBOXED) :
+      webkit::WebPluginInfo::PLUGIN_TYPE_PEPPER_IN_PROCESS;
+
+  info.name = name.empty() ?
+      path.BaseName().LossyDisplayName() : UTF8ToUTF16(name);
+  info.path = path;
+  info.version = ASCIIToUTF16(version);
+  info.desc = ASCIIToUTF16(description);
+  info.mime_types = mime_types;
+  info.pepper_permissions = permissions;
+
+  return info;
 }
 
 }  // namespace content
