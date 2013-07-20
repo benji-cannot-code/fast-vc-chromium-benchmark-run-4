36f557e009e1b8732ea1cfbac7f4aa96718008a8
qinmin@chromium.org <qinmin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-20 05:25:43 +0000

    This change fixes some video-only MSE bugs:
    1.Fix a bug that thumb is stuck at the last seek point after playback completes
    In MediaDecoderJob::DecodeInternal(), when size==0 and EOS is reached, we break out the switch statement and call MediaSourcePlayer::MediaDecoderCallback() with start_presentation_timestamp.
    However, we should not use this presentation_timestamp to call UpdateTimestamps().
    Similarly, we cannot use timestamp for all other non success cases.
    
    2.Fix a bug that the thumb can go back to 0 after a seek.
    After seek, Android MediaCodec may return several frames
    with 0 presentation timestamps. This will cause the thumb to jump back to 0. 
    
    BUG=260517
    
    Review URL: https://chromiumcodereview.appspot.com/19327002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212743 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaCodecBridge.java
@@ -32,12 +32,21 @@ class MediaCodecBridge {
     // INFO_MEDIA_CODEC_ERROR in media_codec_bridge.h.
     private static final int MEDIA_CODEC_ERROR = -1000;
 
+    // After a flush(), dequeueOutputBuffer() can often produce empty presentation timestamps
+    // for several frames. As a result, the player may find that the time does not increase
+    // after decoding a frame. To detect this, we check whether the presentation timestamp from
+    // dequeueOutputBuffer() is larger than input_timestamp - MAX_PRESENTATION_TIMESTAMP_SHIFT_US
+    // after a flush. And we set the presentation timestamp from dequeueOutputBuffer() to be
+    // non-decreasing for the remaining frames.
+    private static final long MAX_PRESENTATION_TIMESTAMP_SHIFT_US = 100000;
+
     private ByteBuffer[] mInputBuffers;
     private ByteBuffer[] mOutputBuffers;
 
     private MediaCodec mMediaCodec;
-
     private AudioTrack mAudioTrack;
+    private boolean mFlushed;
+    private long mLastPresentationTimeUs;
 
     private static class DequeueOutputResult {
         private final int mIndex;
@@ -73,6 +82,8 @@ class MediaCodecBridge {
 
     private MediaCodecBridge(String mime) {
         mMediaCodec = MediaCodec.createDecoderByType(mime);
+        mLastPresentationTimeUs = 0;
+        mFlushed = true;
     }
 
     @CalledByNative
@@ -107,6 +118,7 @@ class MediaCodecBridge {
     @CalledByNative
     private void flush() {
         mMediaCodec.flush();
+        mFlushed = true;
         if (mAudioTrack != null) {
             mAudioTrack.flush();
         }
@@ -143,6 +155,7 @@ class MediaCodecBridge {
     @CalledByNative
     private void queueInputBuffer(
             int index, int offset, int size, long presentationTimeUs, int flags) {
+        resetLastPresentationTimeIfNeeded(presentationTimeUs);
         try {
             mMediaCodec.queueInputBuffer(index, offset, size, presentationTimeUs, flags);
         } catch(IllegalStateException e) {
@@ -154,6 +167,7 @@ class MediaCodecBridge {
     private void queueSecureInputBuffer(
             int index, int offset, byte[] iv, byte[] keyId, int[] numBytesOfClearData,
             int[] numBytesOfEncryptedData, int numSubSamples, long presentationTimeUs) {
+        resetLastPresentationTimeIfNeeded(presentationTimeUs);
         try {
             MediaCodec.CryptoInfo cryptoInfo = new MediaCodec.CryptoInfo();
             cryptoInfo.set(numSubSamples, numBytesOfClearData, numBytesOfEncryptedData,
@@ -180,6 +194,13 @@ class MediaCodecBridge {
         int index = MEDIA_CODEC_ERROR;
         try {
             index = mMediaCodec.dequeueOutputBuffer(info, timeoutUs);
+            if (info.presentationTimeUs < mLastPresentationTimeUs) {
+                // TODO(qinmin): return a special code through DequeueOutputResult
+                // to notify the native code the the frame has a wrong presentation
+                // timestamp and should be skipped.
+                info.presentationTimeUs = mLastPresentationTimeUs;
+            }
+            mLastPresentationTimeUs = info.presentationTimeUs;
         } catch (IllegalStateException e) {
             Log.e(TAG, "Cannot dequeue output buffer " + e.toString());
         }
@@ -262,6 +283,14 @@ class MediaCodecBridge {
                 Log.i(TAG, "Failed to send all data to audio output, expected size: " +
                         buf.length + ", actual size: " + size);
             }
+        }
+    }
+
+    private void resetLastPresentationTimeIfNeeded(long presentationTimeUs) {
+        if (mFlushed) {
+            mLastPresentationTimeUs =
+                    Math.max(presentationTimeUs - MAX_PRESENTATION_TIMESTAMP_SHIFT_US, 0);
+            mFlushed = false;
         }
     }
 }
