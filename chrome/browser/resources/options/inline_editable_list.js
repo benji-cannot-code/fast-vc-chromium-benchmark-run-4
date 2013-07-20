a0023dd5cba96f88eeca3d2b86a97962e7b20b2b
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-20 18:15:04 +0000

    Settings: "Allow" dropdown is not showing on the first click, take 2.
    r211763 didn't work because document.activeElement is not a SELECT
    element yet when the editing flag is updated. By this CL we make sure
    the SELECT element is focused before we try to update the editing
    flag.
    
    Also, this CL moves the code to check activeElement from |set editing|
    to |updateEditState| because ignoring |true| argument in |set editing|
    is tricky.
    
    BUG=257954
    
    Review URL: https://chromiumcodereview.appspot.com/19592012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212795 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/inline_editable_list.js
+++ chrome/browser/resources/options/inline_editable_list.js
@@ -96,8 +96,10 @@ cr.define('options', function() {
      * Updates the edit state based on the current selected and lead states.
      */
     updateEditState: function() {
-      if (this.editable)
-        this.editing = this.selected && this.lead;
+      if (this.editable) {
+        this.editing = this.selected && this.lead &&
+          !this.isExtraFocusableControl(document.activeElement);
+      }
     },
 
     /**
@@ -110,9 +112,6 @@ cr.define('options', function() {
     set editing(editing) {
       if (this.editing == editing)
         return;
-
-      if (this.isExtraFocusableControl(document.activeElement))
-        editing = false;
 
       if (editing)
         this.setAttribute('editing', '');
@@ -344,6 +343,11 @@ cr.define('options', function() {
         return;
 
       var clickTarget = e.target;
+      if (this.isExtraFocusableControl(clickTarget)) {
+        clickTarget.focus();
+        return;
+      }
+
       var editFields = this.editFields_;
       for (var i = 0; i < editFields.length; i++) {
         if (editFields[i] == clickTarget ||
