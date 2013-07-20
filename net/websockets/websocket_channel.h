14904795ad89609274fe6a99000d9f1ecc25003d
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-20 00:09:38 +0000

    net: Fix references to googleurl.
    googleurl is deprecated and moved to src/url/.
    
    BUG=229660
    R=eroman@chromium.org
    
    Review URL: https://codereview.chromium.org/19492015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212703 0039d316-1c4b-4281-b951-d872f2087c98

--- net/websockets/websocket_channel.h
+++ net/websockets/websocket_channel.h
@@ -13,10 +13,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/callback.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/scoped_vector.h"
-#include "googleurl/src/gurl.h"
 #include "net/base/net_export.h"
 #include "net/websockets/websocket_frame.h"
 #include "net/websockets/websocket_stream.h"
+#include "url/gurl.h"
 
 namespace net {
 
