b080fb66a21dee56dccdd9af59253852b8aeaefc
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-20 16:09:39 +0000

    cc: No need to include trees/single_thread_proxy.h in animation/ unittest.
    It's also good to remove this unused include because animation/ can't really depend on trees/.
    
    BUG=None
    TEST=cc_unittests
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/18238016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212793 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/animation/scrollbar_animation_controller_linear_fade_unittest.cc
+++ cc/animation/scrollbar_animation_controller_linear_fade_unittest.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/scrollbar_layer_impl.h"
 #include "cc/test/fake_impl_proxy.h"
 #include "cc/test/fake_layer_tree_host_impl.h"
-#include "cc/trees/single_thread_proxy.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace cc {
