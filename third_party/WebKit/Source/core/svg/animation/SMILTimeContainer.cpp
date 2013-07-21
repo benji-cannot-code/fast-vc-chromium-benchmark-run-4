34650c85aef5ab06345817f4e1acb7711472382f
mrunal.kapade@intel.com <mrunal.kapade@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-21 03:10:16 +0000

    Correct the elapsedTime calculation in SVG animations
    As per the SVG standard, SVG document on receiving the
    pauseAnimations() should also pause the animation clock corresponding
    to the SVG document. Whereas current implmentation also takes into
    account the accumulated pause time which is not required.
    So replacing the m_accumulatedPauseTime with m_accumulatedActiveTime
    and other corresponding calculations.
    Added the layout Test.
    
    BUG=254067
    
    Review URL: https://chromiumcodereview.appspot.com/18374004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154603 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
+++ third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
@@ -41,7 +41,8 @@ static const double animationFrameDelay = 0.025;
 SMILTimeContainer::SMILTimeContainer(SVGSVGElement* owner) 
     : m_beginTime(0)
     , m_pauseTime(0)
-    , m_accumulatedPauseTime(0)
+    , m_resumeTime(0)
+    , m_accumulatedActiveTime(0)
     , m_presetStartTime(0)
     , m_documentOrderIndexesDirty(false)
     , m_timer(this, &SMILTimeContainer::timerFired)
@@ -108,7 +109,11 @@ SMILTime SMILTimeContainer::elapsed() const
 {
     if (!m_beginTime)
         return 0;
-    return currentTime() - m_beginTime - m_accumulatedPauseTime;
+
+    if (isPaused())
+        return m_accumulatedActiveTime;
+
+    return currentTime() + m_accumulatedActiveTime - lastResumeTime();
 }
 
 bool SMILTimeContainer::isActive() const
@@ -148,16 +153,17 @@ void SMILTimeContainer::pause()
     ASSERT(!isPaused());
     m_pauseTime = currentTime();
 
-    if (m_beginTime)
+    if (m_beginTime) {
+        m_accumulatedActiveTime += m_pauseTime - lastResumeTime();
         m_timer.stop();
+    }
+    m_resumeTime = 0;
 }
 
 void SMILTimeContainer::resume()
 {
     ASSERT(isPaused());
-
-    if (m_beginTime)
-        m_accumulatedPauseTime += currentTime() - m_pauseTime;
+    m_resumeTime = currentTime();
 
     m_pauseTime = 0;
     startTimer(0);
@@ -177,9 +183,12 @@ void SMILTimeContainer::setElapsed(SMILTime time)
     double now = currentTime();
     m_beginTime = now - time.value();
 
-    m_accumulatedPauseTime = 0;
-    if (m_pauseTime)
-        m_pauseTime = now;
+    if (m_pauseTime) {
+        m_resumeTime = m_pauseTime = now;
+        m_accumulatedActiveTime = time.value();
+    } else {
+        m_resumeTime = 0;
+    }
 
 #ifndef NDEBUG
     m_preventScheduledAnimationsChanges = true;
