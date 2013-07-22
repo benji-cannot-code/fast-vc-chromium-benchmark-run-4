da1b1752e3ed7e9526236fdf222c9004fbfe40dd
rjkroege@chromium.org <rjkroege@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 17:46:39 +0000

    Fixed CHECK failure in ozone=1 unittests.
    A change in window creation logic to resolve a TODO in RootWindowHostOzone
    broke multiple ozone=1 unit tests because of the absence of a SurfaceFactoryOzone
    implementation. This CL adds the creation of an appropriate stub
    SurfaceFactoryOzone to the aura test helper code.
    
    BUG=262277
    R=ben@chromium.org
    
    Review URL: https://codereview.chromium.org/19482017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212907 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/test/aura_test_helper.cc
+++ ui/aura/test/aura_test_helper.cc
@@ -26,6 +26,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/x/x11_util.h"
 #endif
 
+#if defined(USE_OZONE)
+#include "ui/base/ozone/surface_factory_ozone.h"
+#endif
+
 namespace aura {
 namespace test {
 
@@ -40,6 +44,10 @@ AuraTestHelper::AuraTestHelper(base::MessageLoopForUI* message_loop)
       ui::ScopedAnimationDurationScaleMode::ZERO_DURATION));
 #if defined(USE_X11)
   test::SetUseOverrideRedirectWindowByDefault(true);
+#endif
+#if defined(USE_OZONE)
+  surface_factory_.reset(ui::SurfaceFactoryOzone::CreateTestHelper());
+  ui::SurfaceFactoryOzone::SetInstance(surface_factory_.get());
 #endif
 }
 
