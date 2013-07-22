100b0d2eef3174d6df5d402ec77d95afb4f781e7
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 08:57:49 +0000

    Disable flaky ExtensionApiTest.WallpaperPicker.
    BUG=262854
    NOTRY=true
    TBR=bshe@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19773019
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212851 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/extensions/wallpaper_private_apitest.cc
+++ chrome/browser/chromeos/extensions/wallpaper_private_apitest.cc
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "net/dns/mock_host_resolver.h"
 
-IN_PROC_BROWSER_TEST_F(ExtensionApiTest, WallpaperPicker) {
+// Flaky. See http://crbug.com/262854 .
+IN_PROC_BROWSER_TEST_F(ExtensionApiTest, DISABLED_WallpaperPicker) {
   host_resolver()->AddRule("a.com", "127.0.0.1");
   ASSERT_TRUE(StartEmbeddedTestServer());
   ASSERT_TRUE(RunComponentExtensionTest("wallpaper_manager")) << message_;
