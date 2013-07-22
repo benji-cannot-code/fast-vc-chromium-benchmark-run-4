912c2cd2c7f873f9f76c68356113281ad533a4be
prybin@chromium.org <prybin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-22 21:10:55 +0000

    Fix breakpoint condition wrongly becoming undefined
    Review URL: https://chromiumcodereview.appspot.com/19947004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154681 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/JavaScriptSourceFrame.js
+++ third_party/WebKit/Source/devtools/front_end/JavaScriptSourceFrame.js
@@ -379,6 +379,9 @@ WebInspector.JavaScriptSourceFrame.prototype = {
             this.textEditor.removeDecoration(lineNumber, this._conditionElement);
             delete this._conditionEditorElement;
             delete this._conditionElement;
+            if (!committed)
+                return;
+
             if (breakpoint)
                 breakpoint.setCondition(newText);
             else
