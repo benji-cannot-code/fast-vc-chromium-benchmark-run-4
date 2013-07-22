3d1f03a243e3feb6ce1cb0728406b70ad63494d6
wolenetz@chromium.org <wolenetz@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-22 18:35:54 +0000

    Suppress more media timeupdate events when seeking
    Playback progress timer firing should not cause timeupdate event
    scheduling while seeking, and neither should active text track cue
    updating cause timeupdate event scheduling while seeking.
    Timeupdate event is scheduled during finishSeek for these scenarios.
    
    BUG=261722
    R=acolwell@chromium.org,adamk@chromium.org
    TEST=no media layout test regression on linux, video-seek-to-[duration,middle] pass on Chrome for Android
    
    Review URL: https://chromiumcodereview.appspot.com/19842005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154667 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLMediaElement.cpp
@@ -1011,7 +1011,7 @@ void HTMLMediaElement::updateActiveTextTrackCues(double movieTime)
     // element. (In the other cases, such as explicit seeks, relevant events get
     // fired as part of the overall process of changing the current playback
     // position.)
-    if (m_lastSeekTime <= lastTime)
+    if (!m_seeking && m_lastSeekTime <= lastTime)
         scheduleTimeupdateEvent(false);
 
     // Explicitly cache vector sizes, as their content is constant from here.
@@ -2449,7 +2449,8 @@ void HTMLMediaElement::playbackProgressTimerFired(Timer<HTMLMediaElement>*)
         }
     }
 
-    scheduleTimeupdateEvent(true);
+    if (!m_seeking)
+        scheduleTimeupdateEvent(true);
 
     if (!m_playbackRate)
         return;
