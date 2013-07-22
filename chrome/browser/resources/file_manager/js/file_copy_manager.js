03d94dcd96994787fe670d5389e3bc4ff839ca01
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 14:41:43 +0000

    Remove chrome.extension from FileCopyManager and FileCopyManagerWrapper.
    Historically, the code was used, but we don't need it because we switched to
    v2.
    
    BUG=246976
    TEST=Ran browser_test --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*" and tested manually.
    
    Review URL: https://chromiumcodereview.appspot.com/19641005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212883 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_copy_manager.js
+++ chrome/browser/resources/file_manager/js/file_copy_manager.js
@@ -5,12 +5,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 'use strict';
 
-if (chrome.extension) {
-  var getContentWindows = function() {
-    return chrome.extension.getViews();
-  };
-}
-
 /**
  * @constructor
  */
