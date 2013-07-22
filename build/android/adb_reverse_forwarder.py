0bb5c57dc69aab9274882b282f14bcfdb9f303cd
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 15:56:17 +0000

    Revert "Reland r212020: Move Python setup/tear down logic into Forwarder ..."
    This reverts r212020 which caused some timeouts on the net_unittests every ~15
    builds.
    
    Conflicts:
    	build/android/pylib/base/test_dispatcher.py
    	build/android/pylib/instrumentation/test_runner.py
    	chrome/test/chromedriver/run_py_tests.py
    
    TBR=bulach@chromium.org
    BUG=262573,242846
    
    Review URL: https://codereview.chromium.org/19844006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212888 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/adb_reverse_forwarder.py
+++ build/android/adb_reverse_forwarder.py
@@ -18,6 +18,7 @@ import time
 
 from pylib import android_commands, forwarder
 from pylib.utils import run_tests_helper
+from pylib.valgrind_tools import CreateTool
 
 
 def main(argv):
@@ -51,14 +52,17 @@ def main(argv):
     sys.exit(1)
 
   adb = android_commands.AndroidCommands(options.device)
+  tool = CreateTool(None, adb)
+  forwarder_instance = forwarder.Forwarder(adb, options.build_type)
   try:
-    forwarder.Forwarder.Map(port_pairs, adb, options.build_type)
+    forwarder_instance.Run(port_pairs, tool)
     while True:
       time.sleep(60)
   except KeyboardInterrupt:
     sys.exit(0)
   finally:
-    forwarder.Forwarder.UnmapAllDevicePorts(adb)
+    forwarder_instance.Close()
+
 
 if __name__ == '__main__':
   main(sys.argv)
