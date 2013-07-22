24828e1f5ebf98bc08d23e9915017481367d3652
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 16:17:16 +0000

    Remove QUIC from NPN strings so as to save space in ALPN advertisements.  Refactor HttpStreamFactory::SetNextProtos to traffic in enums instead of strings.
    BUG=
    R=akalin@chromium.org
    
    Review URL: https://codereview.chromium.org/19696009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212894 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_server_properties.h
+++ net/http/http_server_properties.h
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/host_port_pair.h"
 #include "net/base/net_export.h"
 #include "net/http/http_pipelined_host_capability.h"
+#include "net/socket/next_proto.h"
 #include "net/spdy/spdy_framer.h"  // TODO(willchan): Reconsider this.
 
 namespace net {
@@ -34,6 +35,8 @@ enum AlternateProtocol {
 NET_EXPORT const char* AlternateProtocolToString(AlternateProtocol protocol);
 NET_EXPORT AlternateProtocol AlternateProtocolFromString(
     const std::string& protocol);
+NET_EXPORT_PRIVATE AlternateProtocol AlternateProtocolFromNextProto(
+    NextProto next_proto);
 
 struct NET_EXPORT PortAlternateProtocolPair {
   bool Equals(const PortAlternateProtocolPair& other) const {
