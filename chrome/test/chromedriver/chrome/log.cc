24b2b12c61199f71a9a198cdabbf9ce44fccfc3b
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 19:20:22 +0000

    [chromedriver] Add DEPS files for chrome automation and net dir.
    These modules should not depend on chromedriver code.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/19467004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212935 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/chrome/log.cc
+++ chrome/test/chromedriver/chrome/log.cc
@@ -3,6 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/test/chromedriver/chrome/log.h"
+
 #include <stdio.h>
 
 #include "base/json/json_reader.h"
@@ -10,7 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/stringprintf.h"
 #include "base/values.h"
-#include "chrome/test/chromedriver/logging.h"
 
 namespace {
 
