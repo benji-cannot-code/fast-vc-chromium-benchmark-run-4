8aabfee83bb4ba13a4a984397be989689110e21c
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 18:25:58 +0000

    More misc. cleanup:
    * Switch ExtensionInfoBarGtk to using a GetDelegate() method, in order to make
      the eventual transition to not having a |delegate_| member seamless.  (Views
      already does this.)
    * Definition order should match declaration order
    * Eliminate unnecessary #include
    * Small comment tweak
    
    BUG=none
    TEST=none
    R=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/19715003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212920 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/infobars/infobar_gtk.cc
+++ chrome/browser/ui/gtk/infobars/infobar_gtk.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/debug/trace_event.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chrome_notification_types.h"
-#include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/themes/theme_properties.h"
 #include "chrome/browser/ui/gtk/browser_window_gtk.h"
