a857e777ef9a9075fab917d682a84c506dfaf685
hirono@chromium.org <hirono@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 20:52:35 +0000

    Handle the case the list items are removed but the selection index is not changed.
    Originally, the change event of the file list selection is dispatched only when
    the index of the selection is changed. But it should be also dispatched when the
    list item is removed because if a list item is removed, the item referredby the
    selection index is changed.
    
    This CL added a helper function that can force the selection to dispatch the
    change event and handle the problem by using the helper function.
    
    Along the way, this CL also fixed a bug that the keyboard event dispatched at the
    initialization stage causes error messages.
    
    BUG=253887
    TEST=manually
    
    Review URL: https://chromiumcodereview.appspot.com/18319003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212957 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -3922,6 +3922,10 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
    */
   FileManager.prototype.setCtrlKeyPressed_ = function(flag) {
     this.ctrlKeyPressed_ = flag;
-    this.document_.querySelector('#drive-clear-local-cache').canExecuteChange();
+    // Before the DOM is constructed, the key event can be handled.
+    var cacheClearCommand =
+        this.document_.querySelector('#drive-clear-local-cache');
+    if (cacheClearCommand)
+      cacheClearCommand.canExecuteChange();
   };
 })();
