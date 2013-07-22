4b53efa15964fe9d2293247d6c6f500cc9cb7233
dubroy@chromium.org <dubroy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-22 17:02:52 +0000

    History: Update remove button after removing an individual entry.
    Fixes a small UI issue: if a single history entry is checked, and then
    that element is removed via the action menu, the 'Remove selected items'
    button remains enabled, but it should be disabled.
    
    BUG=260628
    TEST=Open chrome://history. Check the checkbox for a single entry, then
    delete that entry using the drop down menu. The 'Remove selected items'
    button should be disabled again.
    
    Review URL: https://chromiumcodereview.appspot.com/19942004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@212901 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/history/history.js
+++ chrome/browser/resources/history/history.js
@@ -1764,9 +1764,8 @@ function updateParentCheckbox(checkbox) {
 
 function entryBoxMousedown(event) {
   // Prevent text selection when shift-clicking to select multiple entries.
-  if (event.shiftKey) {
+  if (event.shiftKey)
     event.preventDefault();
-  }
 }
 
 /**
@@ -1789,12 +1788,20 @@ function removeNodeWithoutTransition(node) {
   node.parentNode.removeChild(node);
 }
 
-function removeNode(node) {
+/**
+ * Triggers a fade-out animation, and then removes |node| from the DOM.
+ * @param {Node} node The node to be removed.
+ * @param {Function?} onRemove A function to be called after the node
+ *     has been removed from the DOM.
+ */
+function removeNode(node, onRemove) {
   node.classList.add('fade-out'); // Trigger CSS fade out animation.
 
   // Delete the node when the animation is complete.
   node.addEventListener('webkitTransitionEnd', function() {
     removeNodeWithoutTransition(node);
+    if (onRemove)
+      onRemove();
   });
 }
 
@@ -1807,17 +1814,17 @@ function removeEntryFromView(entry) {
   var nextEntry = entry.nextSibling;
   var previousEntry = entry.previousSibling;
 
-  removeNode(entry);
+  removeNode(entry, function() {
+    historyView.updateSelectionEditButtons();
+  });
 
   // if there is no previous entry, and the next entry is a gap, remove it
-  if (!previousEntry && nextEntry && nextEntry.className == 'gap') {
+  if (!previousEntry && nextEntry && nextEntry.className == 'gap')
     removeNode(nextEntry);
-  }
 
   // if there is no next entry, and the previous entry is a gap, remove it
-  if (!nextEntry && previousEntry && previousEntry.className == 'gap') {
+  if (!nextEntry && previousEntry && previousEntry.className == 'gap')
     removeNode(previousEntry);
-  }
 
   // if both the next and previous entries are gaps, remove one
   if (nextEntry && nextEntry.className == 'gap' &&
