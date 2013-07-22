625d19425d2459fc91d2be4bd36625cde56fdc62
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-22 02:22:04 +0000

    Use toSVGPathElement() instead of static_cast<>
    Though there is toSVGPathElement(), some files still use static_cast<SVGPathElement*>.
    To remove all static_cast<> use, we need to change argument from SVGElement to Element.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19914002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154621 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/properties/SVGPathSegListPropertyTearOff.cpp
+++ third_party/WebKit/Source/core/svg/properties/SVGPathSegListPropertyTearOff.cpp
@@ -90,8 +90,7 @@ SVGPathElement* SVGPathSegListPropertyTearOff::contextElement() const
 {
     SVGElement* contextElement = m_animatedProperty->contextElement();
     ASSERT(contextElement);
-    ASSERT(contextElement->hasTagName(SVGNames::pathTag));
-    return static_cast<SVGPathElement*>(contextElement);
+    return toSVGPathElement(contextElement);
 }
 
 bool SVGPathSegListPropertyTearOff::processIncomingListItemValue(const ListItemType& newItem, unsigned* indexToModify)
