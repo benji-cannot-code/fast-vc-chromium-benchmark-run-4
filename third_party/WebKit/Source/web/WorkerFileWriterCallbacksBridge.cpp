ffd9ee6ee6154ad18635cc8e552948b12715fe4b
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 02:29:16 +0000

    Stop observing worker context after WorkerFileWriterCallbacksBridge gets shutdown
    BUG=258949
    TEST=fast/filesystem/workers/*
    R=ericu@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19587004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154706 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WorkerFileWriterCallbacksBridge.cpp
+++ third_party/WebKit/Source/web/WorkerFileWriterCallbacksBridge.cpp
@@ -85,6 +85,7 @@ void WorkerFileWriterCallbacksBridge::postShutdownToMainThread(PassRefPtr<Worker
 {
     ASSERT(m_workerGlobalScope->isContextThread());
     m_clientOnWorkerThread = 0;
+    stopObserving();
     dispatchTaskToMainThread(createCallbackTask(&shutdownOnMainThread, bridge));
 }
 
