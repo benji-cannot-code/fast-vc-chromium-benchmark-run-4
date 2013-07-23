2a8a24009fc385b7f8218dad33430ef9de2c3f91
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 15:02:52 +0000

    DevTools: [CodeMirror] respect CRLF line endings
    This patch adds a functionality to respect CRLF line endings in
    CodeMirrorTextEditor; this functionality was initially missing in it.
    A test to verify this behaviour is also brought in.
    
    BUG=263364
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/19540027
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
+++ third_party/WebKit/Source/devtools/front_end/CodeMirrorTextEditor.js
@@ -136,6 +136,9 @@ WebInspector.CodeMirrorTextEditor = function(url, delegate)
     this._codeMirror.setOption("maxHighlightLength", 1000);
     this._codeMirror.setOption("mode", null);
 
+    this._shouldClearHistory = true;
+    this._lineSeparator = "\n";
+
     this._tokenHighlighter = new WebInspector.CodeMirrorTextEditor.TokenHighlighter(this._codeMirror);
     this._blockIndentController = new WebInspector.CodeMirrorTextEditor.BlockIndentController(this._codeMirror);
     this._fixWordMovement = new WebInspector.CodeMirrorTextEditor.FixWordMovement(this._codeMirror);
@@ -1029,12 +1032,24 @@ WebInspector.CodeMirrorTextEditor.prototype = {
     /**
      * @param {string} text
      */
+    _detectLineSeparator: function(text)
+    {
+        this._lineSeparator = text.indexOf("\r\n") >= 0 ? "\r\n" : "\n";
+    },
+
+    /**
+     * @param {string} text
+     */
     setText: function(text)
     {
         this._muteTextChangedEvent = true;
         this._codeMirror.setValue(text);
         this._updateEditorIndentation();
-        this._codeMirror.clearHistory();
+        if (this._shouldClearHistory) {
+            this._codeMirror.clearHistory();
+            this._shouldClearHistory = false;
+        }
+        this._detectLineSeparator(text);
         delete this._muteTextChangedEvent;
     },
 
@@ -1043,7 +1058,7 @@ WebInspector.CodeMirrorTextEditor.prototype = {
      */
     text: function()
     {
-        return this._codeMirror.getValue();
+        return this._codeMirror.getValue().replace(/\n/g, this._lineSeparator);
     },
 
     /**
--- third_party/WebKit/Source/devtools/front_end/SourceFrame.js
+++ third_party/WebKit/Source/devtools/front_end/SourceFrame.js
@@ -347,7 +347,7 @@ WebInspector.SourceFrame.prototype = {
         } else {
             var firstLine = this._textEditor.firstVisibleLine();
             var selection = this._textEditor.selection();
-            this._textEditor.editRange(this._textEditor.range(), content || "");
+            this._textEditor.setText(content || "");
             this._textEditor.scrollToLine(firstLine);
             this._textEditor.setSelection(selection);
         }
