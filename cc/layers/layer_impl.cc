49431fa13a3457089019dcaa0417530de81fda6a
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 23:30:11 +0000

    cc: Remove some DCHECKs in CopyOutputRequest code.
    These DCHECKs fire if we ever commit to the impl side twice without
    performing a draw in between. There are various ways that this
    could happen, and it doesn't really matter to the CopyOutputRequest
    code anyways, so remove the DCHECKs and deal with the case gracefully
    instead.
    
    Test: LayerTreeHostTestAsyncTwoReadbacksWithoutDraw
    
    BUG=263486
    R=enne,piman
    
    Review URL: https://chromiumcodereview.appspot.com/19668008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213270 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/layer_impl.cc
+++ cc/layers/layer_impl.cc
@@ -109,8 +109,6 @@ void LayerImpl::PassCopyRequests(ScopedPtrVector<CopyOutputRequest>* requests) {
   if (requests->empty())
     return;
 
-  DCHECK(copy_requests_.empty());
-
   copy_requests_.insert_and_take(copy_requests_.end(), *requests);
   requests->clear();
 
