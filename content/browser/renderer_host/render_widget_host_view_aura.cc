b3bbd6ef8936c67b77f96eec935c4ff2e8428150
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 23:01:33 +0000

    Update the screen info if the display related property has changed when resized.  This has to be done in RWHVA::SetBounds because this is called before other observers (aura::WindowObserver, gfx::DisplayObserver) get called.
     Also updated TestScreen to update the display when UI scale is set.
    
    Removed screen_info_out_of_date_ as this doesn't seem to be used properly.
    
    
    BUG=253917
    TEST=covered by test. see bug for repro step.
    
    Review URL: https://chromiumcodereview.appspot.com/19293002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213252 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_aura.cc
+++ content/browser/renderer_host/render_widget_host_view_aura.cc
@@ -768,6 +768,9 @@ void RenderWidgetHostViewAura::SetSize(const gfx::Size& size) {
 }
 
 void RenderWidgetHostViewAura::SetBounds(const gfx::Rect& rect) {
+  if (HasDisplayPropertyChanged(window_))
+    host_->InvalidateScreenInfo();
+
   window_->SetBounds(rect);
   host_->WasResized();
   MaybeCreateResizeLock();
