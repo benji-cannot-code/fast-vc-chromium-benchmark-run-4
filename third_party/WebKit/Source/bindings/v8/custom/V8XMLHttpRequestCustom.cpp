2545ce96163fe69d356f5cfbb8e8a43273c818eb
dslomov@chromium.org <dslomov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 09:16:27 +0000

    Use V8 implementation of TypedArrays and DataView in Blink
    R=jochen@chromium.org,kbr@chromium.org
    BUG=259731
    
    Review URL: https://chromiumcodereview.appspot.com/19230002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154741 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/custom/V8XMLHttpRequestCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8XMLHttpRequestCustom.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "V8XMLHttpRequest.h"
 
-#include "V8ArrayBufferView.h"
 #include "V8Blob.h"
 #include "V8Document.h"
 #include "V8FormData.h"
@@ -41,6 +40,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8Binding.h"
 #include "bindings/v8/V8Utilities.h"
 #include "bindings/v8/custom/V8ArrayBufferCustom.h"
+#include "bindings/v8/custom/V8ArrayBufferViewCustom.h"
 #include "core/dom/Document.h"
 #include "core/inspector/InspectorInstrumentation.h"
 #include "core/page/Frame.h"
--- third_party/WebKit/Source/web/WebArrayBufferView.cpp
+++ third_party/WebKit/Source/web/WebArrayBufferView.cpp
@@ -30,7 +30,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebArrayBufferView.h"
 
-#include "V8ArrayBufferView.h"
+#include "bindings/v8/custom/V8ArrayBufferViewCustom.h"
 #include "wtf/ArrayBufferView.h"
 
 using namespace WTF;
@@ -64,7 +64,7 @@ unsigned WebArrayBufferView::byteLength() const
 
 WebArrayBufferView* WebArrayBufferView::createFromV8Value(v8::Handle<v8::Value> value)
 {
-    if (!WebCore::V8ArrayBufferView::HasInstanceInAnyWorld(value, v8::Isolate::GetCurrent()))
+    if (!value->IsArrayBufferView())
         return 0;
     ArrayBufferView* view = WebCore::V8ArrayBufferView::toNative(value->ToObject());
     return new WebArrayBufferView(view);
