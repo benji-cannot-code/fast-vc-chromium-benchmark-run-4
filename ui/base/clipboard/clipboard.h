bcaec0ef268fc50acd5bb73ad7f260484dcbb34f
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 20:44:45 +0000

    Update include paths in ui for base/process changes.
    BUG=242290
    R=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19781003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213219 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/clipboard/clipboard.h
+++ ui/base/clipboard/clipboard.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/gtest_prod_util.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/shared_memory.h"
-#include "base/process.h"
+#include "base/process/process.h"
 #include "base/strings/string16.h"
 #include "base/threading/platform_thread.h"
 #include "base/threading/thread_checker.h"
