275fba36a7c78ccc8fcf630e218e29d020dd5253
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 17:39:37 +0000

    Update include paths in skia for base/process changes.
    BUG=242290
    R=reed@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19647008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213153 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/SkMemory_new_handler.cpp
+++ skia/ext/SkMemory_new_handler.cpp
@@ -7,7 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <stdlib.h>
 #include <new>
 
-#include "base/process_util.h"
+#include "base/process/memory.h"
 
 #include "third_party/skia/include/core/SkTypes.h"
 #include "third_party/skia/include/core/SkThread.h"
