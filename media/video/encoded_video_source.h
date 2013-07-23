d10bb3f348d0e407cd57d80840ad46fe87594245
hshi@chromium.org <hshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 17:04:45 +0000

    Encoded video capture: wire up key frame and bitrate settings.
    Define RequestKeyFrame in IPC message header.
    
    Wire up RequestKeyFrame and TrySetBitstreamConfig with the corresponding entry points
    in WebRTC video encoder.
    
    BUG=221441
    TEST=trybot
    
    Review URL: https://chromiumcodereview.appspot.com/19860002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213143 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/encoded_video_source.h
+++ media/video/encoded_video_source.h
@@ -69,6 +69,9 @@ class EncodedVideoSource {
   // signaling successful change.
   virtual void TrySetBitstreamConfig(
       const RuntimeVideoEncodingParameters& params) = 0;
+
+  // RequestKeyFrame requests a key frame.
+  virtual void RequestKeyFrame() = 0;
 };
 
 }  // namespace media
