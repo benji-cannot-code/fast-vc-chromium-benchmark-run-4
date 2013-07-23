16bd487afe47682093f42bacd633205f6672eff8
phoglund@chromium.org <phoglund@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 13:27:49 +0000

    Re-landing https://codereview.chromium.org/18184004/.
    This new attempt will not stop the test dead in its tracks if a sleep fails, just log a warning. I'll try this and keep an eye on the bot logs to see if it happens a lot. It's the best I can do for now until I figure out why the sleep fails every now and then.
    
    Original description:
    Fixed spinlocking poll method in WebRTC browser tests, fixed duplication.
    
    BUG=225698
    R=tommi@chromium.org
    
    Review URL: https://codereview.chromium.org/19675011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213089 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
+++ chrome/browser/media/chrome_webrtc_video_quality_browsertest.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/infobars/infobar.h"
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/media/media_stream_infobar_delegate.h"
+#include "chrome/browser/media/webrtc_browsertest_common.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_tabstrip.h"
@@ -170,30 +171,6 @@ class WebrtcVideoQualityBrowserTest : public InProcessBrowserTest {
         << "Failed to shut down pywebsocket server!";
   }
 
-  // TODO(phoglund): This ugly poll method is only here while we transition
-  // the test javascript to just post events when things happen. Right now they
-  // don't because the webrtc_call.py and other tests use this polling way of
-  // communicating when we are waiting from an asynchronous event in the
-  // javascript. This method is meant to emulate WaitUntil in the PyAuto
-  // framework.
-  bool UglyPollingWaitUntil(const std::string& javascript,
-                            const std::string& evaluates_to,
-                            content::WebContents* tab_contents) {
-    base::Time start_time = base::Time::Now();
-    base::TimeDelta timeout = TestTimeouts::action_max_timeout();
-    std::string result;
-
-    while (base::Time::Now() - start_time < timeout) {
-      result = ExecuteJavascript(javascript, tab_contents);
-      if (evaluates_to == result)
-        return true;
-    }
-    LOG(ERROR) << "Timed out while waiting for " << javascript
-               << " to evaluate to " << evaluates_to << "; last result was '"
-               << result << "'";
-    return false;
-  }
-
   // Convenience method which executes the provided javascript in the context
   // of the provided web contents and returns what it evaluated to.
   std::string ExecuteJavascript(const std::string& javascript,
@@ -222,7 +199,7 @@ class WebrtcVideoQualityBrowserTest : public InProcessBrowserTest {
     media_infobar->Accept();
 
     // Wait for WebRTC to call the success callback.
-    EXPECT_TRUE(UglyPollingWaitUntil(
+    EXPECT_TRUE(PollingWaitUntil(
         "obtainGetUserMediaResult();", "ok-got-stream", tab_contents));
   }
 
@@ -252,9 +229,9 @@ class WebrtcVideoQualityBrowserTest : public InProcessBrowserTest {
     EXPECT_EQ("ok-negotiating", ExecuteJavascript("negotiateCall()", from_tab));
 
     // Ensure the call gets up on both sides.
-    EXPECT_TRUE(UglyPollingWaitUntil(
+    EXPECT_TRUE(PollingWaitUntil(
         "getPeerConnectionReadyState()", "active", from_tab));
-    EXPECT_TRUE(UglyPollingWaitUntil(
+    EXPECT_TRUE(PollingWaitUntil(
         "getPeerConnectionReadyState()", "active", to_tab));
   }
 
@@ -263,7 +240,7 @@ class WebrtcVideoQualityBrowserTest : public InProcessBrowserTest {
   }
 
   void WaitUntilHangupVerified(content::WebContents* tab_contents) {
-    EXPECT_TRUE(UglyPollingWaitUntil(
+    EXPECT_TRUE(PollingWaitUntil(
         "getPeerConnectionReadyState()", "no-peer-connection", tab_contents));
   }
 
@@ -499,9 +476,14 @@ IN_PROC_BROWSER_TEST_F(WebrtcVideoQualityBrowserTest,
   AssertNoAsynchronousErrors(left_tab);
   AssertNoAsynchronousErrors(right_tab);
 
+  // Poll slower here to avoid flooding the log with messages: capturing and
+  // sending frames take quite a bit of time.
+  int polling_interval_msec = 1000;
+
   // TODO(phoglund): (de-dupe later) different from original flow.
-  EXPECT_TRUE(UglyPollingWaitUntil(
-      "doneFrameCapturing()", "done-capturing", right_tab));
+  EXPECT_TRUE(PollingWaitUntil(
+      "doneFrameCapturing()", "done-capturing", right_tab,
+      polling_interval_msec));
 
   HangUp(left_tab);
   WaitUntilHangupVerified(left_tab);
@@ -511,8 +493,9 @@ IN_PROC_BROWSER_TEST_F(WebrtcVideoQualityBrowserTest,
   AssertNoAsynchronousErrors(right_tab);
 
   // TODO(phoglund): (de-dupe later) different from original flow.
-  EXPECT_TRUE(UglyPollingWaitUntil(
-      "haveMoreFramesToSend()", "no-more-frames", right_tab));
+  EXPECT_TRUE(PollingWaitUntil(
+      "haveMoreFramesToSend()", "no-more-frames", right_tab,
+      polling_interval_msec));
 
   RunARGBtoI420Converter(
       kVgaWidth, kVgaHeight, GetWorkingDir().Append(kCapturedYuvFileName));
