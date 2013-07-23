68ad2b7c2c153a299eecc82813463f5f6fd2d5d1
eae@chromium.org <eae@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 16:55:59 +0000

    Change FilterOperationResolver & TransformBuilder to take const RenderStyle*
    FilterOperationResolver and TransformBuilder take RenderStyle pointers
    but do not modify the styles in any way. Mark the style and rootStyle
    pointers as const.
    
    R=leviw@chromium.org
    BUG=263253
    
    Change computeLength to take const RenderStyle*
    
    The various computeLength functions all take two RenderStyle pointers
    (style and rootStyle).
    Mark those as const as no implementation of computeLength modifies the
    style.
    
    R=leviw@chromium.org
    BUG=263253
    
    Review URL: https://chromiumcodereview.appspot.com/19593009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154765 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/resolver/TransformBuilder.h
+++ third_party/WebKit/Source/core/css/resolver/TransformBuilder.h
@@ -46,7 +46,7 @@ public:
     TransformBuilder();
     ~TransformBuilder();
 
-    static bool createTransformOperations(CSSValue* inValue, RenderStyle* inStyle, RenderStyle* rootStyle, TransformOperations& outOperations);
+    static bool createTransformOperations(CSSValue* inValue, const RenderStyle* inStyle, const RenderStyle* rootStyle, TransformOperations& outOperations);
 };
 
 } // namespace WebCore
