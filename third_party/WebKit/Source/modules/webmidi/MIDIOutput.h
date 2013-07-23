a6c47418c4f4f9d12a3227289abca944a450b6db
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 01:10:30 +0000

    Revert 154701 "Fix build breakage from missing #include"
    > Fix build breakage from missing #include
    > 
    > fix compile error caused by 154697
    > 
    > TBR=crogers@google.com
    > 
    > Review URL: https://codereview.chromium.org/19872006
    
    TBR=crogers@google.com
    
    Review URL: https://codereview.chromium.org/20018002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/webmidi/MIDIOutput.h
+++ third_party/WebKit/Source/modules/webmidi/MIDIOutput.h
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef MIDIOutput_h
 #define MIDIOutput_h
 
-#include "modules/webmidi/MIDIAccess.h"
 #include "modules/webmidi/MIDIPort.h"
 #include "wtf/RefPtr.h"
 #include "wtf/Uint8Array.h"
