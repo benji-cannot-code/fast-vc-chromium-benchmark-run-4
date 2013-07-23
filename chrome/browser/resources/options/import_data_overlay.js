6935832e24ece72aeb7948845424f6d19fc0a493
zhchbin@gmail.com <zhchbin@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 00:46:05 +0000

    Enable import button correctly under chrome://settings/importData.
    BUG=170314
    TEST=Please refer to the bug.
    
    Review URL: https://chromiumcodereview.appspot.com/19944002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213015 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/import_data_overlay.js
+++ chrome/browser/resources/options/import_data_overlay.js
@@ -237,6 +237,7 @@ cr.define('options', function() {
     // Make sure that any previous import success message is hidden, and
     // we're showing the UI to import further data.
     ImportDataOverlay.getInstance().updateSuccessState_(false);
+    ImportDataOverlay.getInstance().validateCommitButton_();
 
     OptionsPage.navigateToPage('importData');
   };
