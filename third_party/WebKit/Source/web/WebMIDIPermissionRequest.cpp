44fbe1f4a6fc33baade11e324e4616c0b0e46237
crogers@google.com <crogers@google.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 01:12:12 +0000

    Revert 154697 "Implement MIDIOutput.send()"
    > Implement MIDIOutput.send()
    > 
    > This fully implements the Web MIDI API sending of MIDI data in Blink, but further work is still
    > needed in the chromium back-end.
    > 
    > BUG=163795
    > TEST=requestmidiaccess.html updated to test send() method
    > R=abarth@chromium.org, toyoshim@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/18858006
    
    TBR=crogers@google.com
    
    Review URL: https://codereview.chromium.org/19542006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154703 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebMIDIPermissionRequest.cpp
+++ third_party/WebKit/Source/web/WebMIDIPermissionRequest.cpp
@@ -68,7 +68,7 @@ WebSecurityOrigin WebMIDIPermissionRequest::securityOrigin() const
 
 void WebMIDIPermissionRequest::setIsAllowed(bool allowed)
 {
-    m_private->setSysExEnabled(allowed);
+    m_private->enableSysEx(allowed);
 }
 
 } // namespace WebKit
