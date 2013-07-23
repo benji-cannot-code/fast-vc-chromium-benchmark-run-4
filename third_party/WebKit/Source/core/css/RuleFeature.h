d2adfa2fa00a3b73b1b32e38d1bb08f8797550b5
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 05:06:58 +0000

    Move CSS RuleFeature away from AtomicStringImpl.
    We now use AtomicString directly instead, to avoid an object lifetime issue.
    
    BUG=257145
    TEST=LayoutTests under ASAN
    
    Review URL: https://chromiumcodereview.appspot.com/19991003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/css/RuleFeature.h
+++ third_party/WebKit/Source/core/css/RuleFeature.h
@@ -25,7 +25,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "wtf/Forward.h"
 #include "wtf/HashSet.h"
-#include "wtf/text/AtomicString.h"
+#include "wtf/text/AtomicStringHash.h"
 
 namespace WebCore {
 
@@ -78,9 +78,9 @@ public:
         return idsInRules.contains(idValue.impl());
     }
 
-    HashSet<AtomicStringImpl*> idsInRules;
-    HashSet<AtomicStringImpl*> classesInRules;
-    HashSet<AtomicStringImpl*> attrsInRules;
+    HashSet<AtomicString> idsInRules;
+    HashSet<AtomicString> classesInRules;
+    HashSet<AtomicString> attrsInRules;
     Vector<RuleFeature> siblingRules;
     Vector<RuleFeature> uncommonAttributeRules;
 private:
