0dd86be80cc70301671318327f9a82ac4fac9a4e
isimha@nvidia.com <isimha@nvidia.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 05:25:30 +0000

    Add "DSI" to list of acceptable internal display names
    Nvidia's Dalmore and Venice platforms use "DSI" as the internal
    output name.
    
    BUG=None
    TEST=Ran pwoer_BacklightControl autotest, which decreases
    brightness to 0, and "DSI" display turns off (because it
    is internal).
    
    Review URL: https://chromiumcodereview.appspot.com/19724002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213044 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/display/output_util.cc
+++ chromeos/display/output_util.cc
@@ -20,6 +20,7 @@ namespace {
 // Prefixes for the built-in displays.
 const char kInternal_LVDS[] = "LVDS";
 const char kInternal_eDP[] = "eDP";
+const char kInternal_DSI[] = "DSI";
 
 // Returns 64-bit persistent ID for the specified manufacturer's ID and
 // product_code_hash, and the index of the output it is connected to.
@@ -317,7 +318,8 @@ bool ParseOutputOverscanFlag(const unsigned char* prop,
 }
 
 bool IsInternalOutputName(const std::string& name) {
-  return name.find(kInternal_LVDS) == 0 || name.find(kInternal_eDP) == 0;
+  return name.find(kInternal_LVDS) == 0 || name.find(kInternal_eDP) == 0 ||
+      name.find(kInternal_DSI) == 0;
 }
 
 }  // namespace chromeos
