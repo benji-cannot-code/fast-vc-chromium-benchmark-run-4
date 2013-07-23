a2791a0a4cbe0190276aba2fe5bbd8d85dd21602
pdr@chromium.org <pdr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-23 06:15:09 +0000

    Remove virual RenderFlexibleBox::computePreferredLogicalWidths()
    RenderFlexibleBox does not need this duplicated code from RenderBlock.
    This patch removes the overridden function.
    
    Note that RenderFlexibleBox will now go through the
    updateFirstLetter() check but first letter is disabled for
    flexible box and this call is short-circuited immediately
    in RenderBlock::updateFirstLetter()
    
    BUG=263225
    
    Review URL: https://chromiumcodereview.appspot.com/19631006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154730 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderFlexibleBox.h
+++ third_party/WebKit/Source/core/rendering/RenderFlexibleBox.h
@@ -61,7 +61,6 @@ public:
 
 protected:
     virtual void computeIntrinsicLogicalWidths(LayoutUnit& minLogicalWidth, LayoutUnit& maxLogicalWidth) const OVERRIDE;
-    virtual void computePreferredLogicalWidths() OVERRIDE;
 
     virtual void styleDidChange(StyleDifference, const RenderStyle* oldStyle);
 
