da7d4e66a889a340fb1db9746bb15b563b72dd5f
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 15:39:44 +0000

    Catch extension errors when running callbacks in the chrome.app and chrome.webstore APIs.
    BUG=258526
    R=mpcomplete@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/19723015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213130 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/webstore_custom_bindings.js
+++ chrome/renderer/resources/extensions/webstore_custom_bindings.js
@@ -12,9 +12,8 @@ function Installer() {
 }
 
 Installer.prototype.install = function(url, onSuccess, onFailure) {
-  if (this._pendingInstall) {
+  if (this._pendingInstall)
     throw 'A Chrome Web Store installation is already pending.';
-  }
   var installId = webstoreNatives.Install(url, onSuccess, onFailure);
   if (installId !== undefined) {
     this._pendingInstall = {
@@ -37,6 +36,9 @@ Installer.prototype.onInstallResponse = function(installId, success, error) {
       pendingInstall.onSuccess();
     else if (!success && pendingInstall.onFailure)
       pendingInstall.onFailure(error);
+  } catch (e) {
+    console.error('Exception in chrome.webstore.install response handler: ' +
+                  e.stack);
   } finally {
     this._pendingInstall = null;
   }
