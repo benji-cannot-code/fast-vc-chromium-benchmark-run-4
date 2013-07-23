d4a7f4e42c8dbc26514bddca6c4d796af4c4b0ec
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 20:09:42 +0000

    Makes RenderViewImpl::GetSSLStatusOfFrame() actually do something.
    R=jamesr@chromium.org
    BUG=256815
    
    Review URL: https://chromiumcodereview.appspot.com/18651006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213202 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/ssl_status_serialization.h
+++ content/common/ssl_status_serialization.h
@@ -8,15 +8,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
+#include "content/common/content_export.h"
 #include "net/cert/cert_status_flags.h"
 
 namespace content {
 
 // Convenience methods for serializing/deserializing the security info.
-std::string SerializeSecurityInfo(int cert_id,
-                                  net::CertStatus cert_status,
-                                  int security_bits,
-                                  int connection_status);
+CONTENT_EXPORT std::string SerializeSecurityInfo(int cert_id,
+                                                 net::CertStatus cert_status,
+                                                 int security_bits,
+                                                 int connection_status);
 
 bool DeserializeSecurityInfo(const std::string& state,
                              int* cert_id,
