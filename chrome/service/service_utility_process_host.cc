fbf4075b6163564532121714a0c7be9278a21534
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 14:52:31 +0000

    Update include paths in chrome/service for base/process changes.
    BUG=242290
    R=scottbyer@chromium.org
    
    Review URL: https://codereview.chromium.org/19638016
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213112 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/service/service_utility_process_host.cc
+++ chrome/service/service_utility_process_host.cc
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_loop_proxy.h"
-#include "base/process_util.h"
+#include "base/process/kill.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/chrome_utility_messages.h"
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if defined(OS_WIN)
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/process/launch.h"
 #include "base/win/scoped_handle.h"
 #include "content/public/common/sandbox_init.h"
 #include "content/public/common/sandboxed_process_launcher_delegate.h"
