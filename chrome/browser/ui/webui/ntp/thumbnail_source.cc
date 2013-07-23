bcba7db22b98df2f912feb2d03f53b06e3959f1a
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 21:41:33 +0000

    [Clean up] Remove unused headerfiles from FaviconSource and ThumbnailSource.
    BUG=none
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/19994005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213234 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/ntp/thumbnail_source.cc
+++ chrome/browser/ui/webui/ntp/thumbnail_source.cc
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/message_loop/message_loop.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/instant_io_context.h"
-#include "chrome/browser/search/instant_service.h"
-#include "chrome/browser/search/instant_service_factory.h"
 #include "chrome/browser/thumbnails/thumbnail_service.h"
 #include "chrome/browser/thumbnails/thumbnail_service_factory.h"
 #include "chrome/common/url_constants.h"
