f358cf683f3c7b420ecb547f67bd5b75290bdffe
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-23 14:31:53 +0000

    Update include paths in sandbox for base/process changes.
    BUG=242290
    R=rvargas@chromium.org
    
    Review URL: https://codereview.chromium.org/19824004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213104 0039d316-1c4b-4281-b951-d872f2087c98

--- sandbox/linux/services/broker_process.h
+++ sandbox/linux/services/broker_process.h
@@ -11,7 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/pickle.h"
-#include "base/process.h"
+#include "base/process/process.h"
 
 namespace sandbox {
 
--- sandbox/win/tests/common/controller.cc
+++ sandbox/win/tests/common/controller.cc
@@ -7,8 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <string>
 
-#include "base/process.h"
-#include "base/process_util.h"
+#include "base/process/process.h"
 #include "base/strings/sys_string_conversions.h"
 #include "base/win/windows_version.h"
 #include "sandbox/win/src/sandbox_factory.h"
