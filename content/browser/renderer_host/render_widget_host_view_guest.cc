a3ab114a09a92bdb772c1fbf1e6873fc3915203b
lazyboy@chromium.org <lazyboy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 23:47:42 +0000

    Position guest context menu correctly in aura and windows. Reland r212489 (crrev.com/18175007), (reverted r212532): This broke "Win Aura Tests (1)".
    > Position guest context menu correctly in aura and windows.
    > We need to put the web_contents_view's native view to the view hierarchy
    > so that coordinates/bounds are correct and ConvertPointToScreen* conversion
    > works properly. We also need to override the origin/position of the 
    > native view before showing context menu.
    > Other code paths that calculate web_contents_view's bounds/position
    > doesn't use this path and therefore this was not required before.
    
    New changes are in render_widget_host_view_guest.cc
    
    BUG=254069, 238691
    Test=Chromeos + linux aura + windows + windows aura.
    Windows 7, tested with use_aura=1 and linux chromoes=1:
    interactive_ui_tests:WebViewInteractiveTest.*
    content_browsertests:BrowserPluginHostTest.*
    content_browsertests:BrowserPluginTest.*
    browser_tests:WebViewTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/19723011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213550 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_guest.cc
+++ content/browser/renderer_host/render_widget_host_view_guest.cc
@@ -153,9 +153,10 @@ void RenderWidgetHostViewGuest::RenderProcessGone(
 }
 
 void RenderWidgetHostViewGuest::Destroy() {
-  platform_view_->Destroy();
   // The RenderWidgetHost's destruction led here, so don't call it.
   DestroyGuestView();
+
+  platform_view_->Destroy();
 }
 
 void RenderWidgetHostViewGuest::SetTooltipText(const string16& tooltip_text) {
@@ -502,6 +503,7 @@ void RenderWidgetHostViewGuest::SetParentNativeViewAccessible(
 #endif
 
 void RenderWidgetHostViewGuest::DestroyGuestView() {
+  host_->SetView(NULL);
   host_ = NULL;
   base::MessageLoop::current()->DeleteSoon(FROM_HERE, this);
 }
