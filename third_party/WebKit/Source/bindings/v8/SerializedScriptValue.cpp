4211cd366d4c00807be5d79f4224b6a583c79ed2
haraken@chromium.org <haraken@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-24 09:26:37 +0000

    Revert 154802 "Don't use Vector<UChar> to build strings"
    This CL broke webkit_unit_tests in Android bots.
    http://build.chromium.org/p/chromium.linux/builders/Android%20Tests%20%28dbg%29/builds/12998/steps/webkit_unit_tests/logs/stdio
    
    
    > Don't use Vector<UChar> to build strings
    > 
    > This CL removes a bunch of code sites that were using Vector<UChar> to
    > construct strings. That used to be a good way to construct strings, but in the
    > modern world, we have much better alternatives in the form of StringBuffer and
    > StringBuilder. (You should use StringBuffer if you know exactly how much memory
    > you need and you should use StringBuilder if you require a variable amount of
    > memory.)
    > 
    > This CL removes more clients of the StringImpl pointer. There are still a few
    > left, which require more study.
    > 
    > R=eseidel
    > BUG=262320
    > 
    > Review URL: https://chromiumcodereview.appspot.com/20002008
    
    TBR=abarth@chromium.org
    
    Review URL: https://codereview.chromium.org/19982008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154825 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
+++ third_party/WebKit/Source/bindings/v8/SerializedScriptValue.cpp
@@ -77,7 +77,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/Uint8Array.h"
 #include "wtf/Uint8ClampedArray.h"
 #include "wtf/Vector.h"
-#include "wtf/text/StringBuffer.h"
 #include "wtf/text/StringUTF8Adaptor.h"
 
 // FIXME: consider crashing in debug mode on deserialization errors
@@ -2252,9 +2251,9 @@ PassRefPtr<SerializedScriptValue> SerializedScriptValue::createFromWireBytes(con
     // Decode wire data from big endian to host byte order.
     ASSERT(!(data.size() % sizeof(UChar)));
     size_t length = data.size() / sizeof(UChar);
-    StringBuffer<UChar> buffer(length);
+    Vector<UChar> buffer(length);
     const UChar* src = reinterpret_cast<const UChar*>(data.data());
-    UChar* dst = buffer.characters();
+    UChar* dst = buffer.data();
     for (size_t i = 0; i < length; i++)
         dst[i] = ntohs(src[i]);
 
