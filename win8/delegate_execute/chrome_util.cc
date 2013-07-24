c1d1b1d0f112775a40e02d1fe1b2ad92c862799a
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 22:17:11 +0000

    Fix Chrome build after r213503.
    BUG=242290
    TBR=dgrogan@chromium.org
    
    Review URL: https://codereview.chromium.org/20106004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213530 0039d316-1c4b-4281-b951-d872f2087c98

--- win8/delegate_execute/chrome_util.cc
+++ win8/delegate_execute/chrome_util.cc
@@ -16,6 +16,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/file_util.h"
 #include "base/files/file_path.h"
 #include "base/md5.h"
+#include "base/process/kill.h"
+#include "base/process/launch.h"
 #include "base/process/process_handle.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
