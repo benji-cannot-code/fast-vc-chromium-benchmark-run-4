b8b1edb1a9ab7cd0cbd662141f9d099296bb68f8
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 17:26:20 +0000

    Update include paths in cloud_print for base/process changes.
    BUG=242290
    TBR=abodenha@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19525006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213455 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/virtual_driver/win/port_monitor/port_monitor.h
+++ cloud_print/virtual_driver/win/port_monitor/port_monitor.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <windows.h>
 #include <string>
 #include "base/file_util.h"
-#include "base/process.h"
+#include "base/process/process.h"
 #include "base/strings/string16.h"
 
 namespace cloud_print {
