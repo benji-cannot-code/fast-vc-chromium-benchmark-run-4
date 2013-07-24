8a78c3e685fca7127c2e6636a62d0aa81023876a
dstockwell@chromium.org <dstockwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-24 04:02:50 +0000

    Web Animations: Fix handing of m_player on TimedItem
    Missed initialization and inappropriate assertion were causing flaky failures
    on the mac_layout bots.
    
    TBR=esprehn
    BUG=232273
    
    Review URL: https://chromiumcodereview.appspot.com/19888008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154810 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TimedItem.cpp
+++ third_party/WebKit/Source/core/animation/TimedItem.cpp
@@ -37,6 +37,7 @@ namespace WebCore {
 
 TimedItem::TimedItem(const Timing& timing)
     : m_parent(0)
+    , m_player(0)
     , m_startTime(0)
     , m_specified(timing)
     , m_calculated()
@@ -46,7 +47,6 @@ TimedItem::TimedItem(const Timing& timing)
 
 void TimedItem::updateInheritedTime(double inheritedTime) const
 {
-    ASSERT(player());
     const double localTime = inheritedTime - m_startTime;
     const double iterationDuration = m_specified.hasIterationDuration
         ? m_specified.iterationDuration
