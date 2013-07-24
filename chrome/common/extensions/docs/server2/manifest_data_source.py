9b0f1898506a6b5634a965b5152ac36fca998731
kalman@chromium.org <kalman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 21:16:52 +0000

    Fix OrderedDict import in manifest_data_source.py.
    TBR=jyasskin@chromium.org
    
    Review URL: https://codereview.chromium.org/20142002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213515 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/manifest_data_source.py
+++ chrome/common/extensions/docs/server2/manifest_data_source.py
@@ -3,11 +3,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from collections import OrderedDict
 from copy import deepcopy
 from operator import itemgetter
 
-from third_party.json_schema_compiler.json_parse import Parse
+from third_party.json_schema_compiler.json_parse import OrderedDict, Parse
 
 class ManifestDataSource(object):
   '''Provides a template with access to manifest properties specific to apps or
