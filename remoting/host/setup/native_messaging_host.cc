bf57097ee2a4759b08dcfd81975908f67a3dcd97
jamiewalch@chromium.org <jamiewalch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 10:01:20 +0000

    Fix NPAPI and native messaging hosts to cope with NULL pairing registry.
    Since there are no Windows or Mac pairing registry delegates yet, the host plugin currently crashes when the web-app starts up on those platforms.
    
    Review URL: https://chromiumcodereview.appspot.com/19460020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213381 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/setup/native_messaging_host.cc
+++ remoting/host/setup/native_messaging_host.cc
@@ -151,9 +151,13 @@ bool NativeMessagingHost::ProcessHello(
 bool NativeMessagingHost::ProcessClearPairedClients(
     const base::DictionaryValue& message,
     scoped_ptr<base::DictionaryValue> response) {
-  pairing_registry_->ClearAllPairings(
-      base::Bind(&NativeMessagingHost::SendBooleanResult, weak_ptr_,
-                 base::Passed(&response)));
+  if (pairing_registry_) {
+    pairing_registry_->ClearAllPairings(
+        base::Bind(&NativeMessagingHost::SendBooleanResult, weak_ptr_,
+                   base::Passed(&response)));
+  } else {
+    SendBooleanResult(response.Pass(), false);
+  }
   return true;
 }
 
@@ -167,9 +171,13 @@ bool NativeMessagingHost::ProcessDeletePairedClient(
     return false;
   }
 
-  pairing_registry_->DeletePairing(
-      client_id, base::Bind(&NativeMessagingHost::SendBooleanResult, weak_ptr_,
-                            base::Passed(&response)));
+  if (pairing_registry_) {
+    pairing_registry_->DeletePairing(
+        client_id, base::Bind(&NativeMessagingHost::SendBooleanResult,
+                              weak_ptr_, base::Passed(&response)));
+  } else {
+    SendBooleanResult(response.Pass(), false);
+  }
   return true;
 }
 
@@ -238,9 +246,14 @@ bool NativeMessagingHost::ProcessGetDaemonConfig(
 bool NativeMessagingHost::ProcessGetPairedClients(
     const base::DictionaryValue& message,
     scoped_ptr<base::DictionaryValue> response) {
-  pairing_registry_->GetAllPairings(
-      base::Bind(&NativeMessagingHost::SendPairedClientsResponse, weak_ptr_,
-                 base::Passed(&response)));
+  if (pairing_registry_) {
+    pairing_registry_->GetAllPairings(
+        base::Bind(&NativeMessagingHost::SendPairedClientsResponse, weak_ptr_,
+                   base::Passed(&response)));
+  } else {
+    scoped_ptr<base::ListValue> no_paired_clients(new base::ListValue);
+    SendPairedClientsResponse(response.Pass(), no_paired_clients.Pass());
+  }
   return true;
 }
 
