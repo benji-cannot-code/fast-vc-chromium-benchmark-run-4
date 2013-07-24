2df74af249ed3723d642c3fa7ec4ee1b8104b6b8
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 22:09:33 +0000

    Misc. tiny cleanups to ThreeDAPIInfoBar.
    BUG=none
    TEST=none
    TBR=kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/19637014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213528 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/three_d_api_observer.cc
+++ chrome/browser/three_d_api_observer.cc
@@ -10,18 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/infobars/infobar_service.h"
 #include "chrome/browser/tab_contents/tab_util.h"
 #include "content/public/browser/gpu_data_manager.h"
-#include "content/public/browser/web_contents.h"
-#include "content/public/common/three_d_api_types.h"
 #include "grit/generated_resources.h"
 #include "ui/base/l10n/l10n_util.h"
-#include "url/gurl.h"
 
 
-// ThreeDAPIInfoBarDelegate ---------------------------------------------
+// ThreeDAPIInfoBarDelegate ---------------------------------------------------
 
 class ThreeDAPIInfoBarDelegate : public ConfirmInfoBarDelegate {
  public:
-  // Creates a 3D API delegate and adds it to |infobar_service|.
+  // Creates a 3D API infobar delegate and adds it to |infobar_service|.
   static void Create(InfoBarService* infobar_service,
                      const GURL& url,
                      content::ThreeDAPIType requester);
@@ -155,7 +152,7 @@ bool ThreeDAPIInfoBarDelegate::LinkClicked(WindowOpenDisposition disposition) {
 }
 
 
-// ThreeDAPIObserver ----------------------------------------------------
+// ThreeDAPIObserver ----------------------------------------------------------
 
 ThreeDAPIObserver::ThreeDAPIObserver() {
   content::GpuDataManager::GetInstance()->AddObserver(this);
