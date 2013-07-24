2313518650b4cfd764b421751b8150d62803013a
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 00:28:25 +0000

    Update include paths in tools for base/process changes.
    BUG=242290
    R=mark@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19960008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213294 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/imagediff/image_diff.cc
+++ tools/imagediff/image_diff.cc
@@ -20,7 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/process_util.h"
+#include "base/process/memory.h"
 #include "base/safe_numerics.h"
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
