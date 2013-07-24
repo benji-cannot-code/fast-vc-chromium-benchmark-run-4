bb737f9a8cbf0c6c8c56a916e02b8b8914d4f7a0
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 19:20:18 +0000

    Move QuotaDispatcherHost from browser/renderer_host to browser/
    Now it's serving IPCs from renderers and workers, keeping it
    under renderer_host is confusing.
    
    BUG=259660
    
    Review URL: https://chromiumcodereview.appspot.com/20041003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213485 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/quota_dispatcher_host.cc
+++ content/browser/quota_dispatcher_host.cc
@@ -1,9 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/browser/renderer_host/quota_dispatcher_host.h"
+#include "content/browser/quota_dispatcher_host.h"
 
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
