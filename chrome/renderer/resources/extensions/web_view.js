f2f31732fcc60e3736b67a6eca9f1f15a11570e7
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 07:30:53 +0000

    <webview>: Implement dialog API
    This CL implements a JavaScript Dialog API for <webview>. This API allows the embedder to intercept requests for alert/confirm/prompt dialogs, giving the embedder an opportunity to asynchronously display UI to respond to these requests.
    
    Note: This API blocks the guest until one of three things happens:
    
    1. The default is not prevented, and so the default action is equivalent to canceling the dialog.
    2. The default is prevented and the event.dialog object becomes unreachable in JS and is garbage collected.
    3. An action is taken: event.dialog.ok(...) or event.dialog.cancel();
    
    BUG=260745
    Test=WebViewTest.Dialog_*
    
    Review URL: https://chromiumcodereview.appspot.com/19679002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213369 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/web_view.js
+++ chrome/renderer/resources/extensions/web_view.js
@@ -424,7 +424,7 @@ WebView.prototype.setupNewWindowEvent_ = function() {
     var detail = e.detail ? JSON.parse(e.detail) : {};
     if (detail.id != requestId)
       return;
-    browserPluginNode['-internal-setPermission'](requestId, false);
+    browserPluginNode['-internal-setPermission'](requestId, false, '');
   }
 
   browserPluginNode.addEventListener('-internal-newwindow', function(e) {
@@ -464,12 +464,12 @@ WebView.prototype.setupNewWindowEvent_ = function() {
           // then we will fail and it will be treated as if the new window
           // was rejected. The permission API plumbing is used here to clean
           // up the state created for the new window if attaching fails.
-          browserPluginNode['-internal-setPermission'](requestId, attached);
+          browserPluginNode['-internal-setPermission'](requestId, attached, '');
         }, 0);
       },
       discard: function() {
         validateCall();
-        browserPluginNode['-internal-setPermission'](requestId, false);
+        browserPluginNode['-internal-setPermission'](requestId, false, '');
       }
     };
     evt.window = window;
@@ -484,7 +484,7 @@ WebView.prototype.setupNewWindowEvent_ = function() {
     if (!actionTaken && !defaultPrevented) {
       actionTaken = true;
       // The default action is to discard the window.
-      browserPluginNode['-internal-setPermission'](requestId, false);
+      browserPluginNode['-internal-setPermission'](requestId, false, '');
       console.warn(WARNING_MSG_NEWWINDOW_BLOCKED);
     }
   });
@@ -553,7 +553,7 @@ WebView.prototype.setupPermissionEvent_ = function() {
     var detail = e.detail ? JSON.parse(e.detail) : {};
     if (detail.id != requestId)
       return;
-    browserPluginNode['-internal-setPermission'](requestId, false);
+    browserPluginNode['-internal-setPermission'](requestId, false, '');
   }
 
   browserPluginNode.addEventListener(internalevent, function(e) {
@@ -576,7 +576,7 @@ WebView.prototype.setupPermissionEvent_ = function() {
           if (decisionMade) {
             throw new Error(ERROR_MSG_PERMISSION_ALREADY_DECIDED);
           } else {
-            browserPluginNode['-internal-setPermission'](requestId, true);
+            browserPluginNode['-internal-setPermission'](requestId, true, '');
             decisionMade = true;
           }
         },
@@ -584,7 +584,7 @@ WebView.prototype.setupPermissionEvent_ = function() {
           if (decisionMade) {
             throw new Error(ERROR_MSG_PERMISSION_ALREADY_DECIDED);
           } else {
-            browserPluginNode['-internal-setPermission'](requestId, false);
+            browserPluginNode['-internal-setPermission'](requestId, false, '');
             decisionMade = true;
           }
         }
@@ -601,7 +601,7 @@ WebView.prototype.setupPermissionEvent_ = function() {
       var defaultPrevented = !node.dispatchEvent(evt);
       if (!decisionMade && !defaultPrevented) {
         decisionMade = true;
-        browserPluginNode['-internal-setPermission'](requestId, false);
+        browserPluginNode['-internal-setPermission'](requestId, false, '');
       }
     }
   });
