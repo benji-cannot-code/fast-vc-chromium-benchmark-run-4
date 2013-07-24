e130d551d80dcbe99281e24dd57213dc4e6a2cf9
wez@chromium.org <wez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 05:05:53 +0000

    Remove unnecessary SupportsWeakPtr base from ChromotingInstance.
    Review URL: https://chromiumcodereview.appspot.com/19579010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213346 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/client/plugin/chromoting_instance.cc
+++ remoting/client/plugin/chromoting_instance.cc
@@ -295,7 +295,7 @@ void ChromotingInstance::HandleMessage(const pp::Var& message) {
     if (use_async_pin_dialog_) {
       config.fetch_secret_callback =
           base::Bind(&ChromotingInstance::FetchSecretFromDialog,
-                     this->AsWeakPtr());
+                     weak_factory_.GetWeakPtr());
     } else {
       std::string shared_secret;
       if (!data->GetString("sharedSecret", &shared_secret)) {
@@ -573,7 +573,7 @@ protocol::CursorShapeStub* ChromotingInstance::GetCursorShapeStub() {
 scoped_ptr<protocol::ThirdPartyClientAuthenticator::TokenFetcher>
 ChromotingInstance::GetTokenFetcher(const std::string& host_public_key) {
   return scoped_ptr<protocol::ThirdPartyClientAuthenticator::TokenFetcher>(
-      new PepperTokenFetcher(this->AsWeakPtr(), host_public_key));
+      new PepperTokenFetcher(weak_factory_.GetWeakPtr(), host_public_key));
 }
 
 void ChromotingInstance::InjectClipboardEvent(
@@ -702,7 +702,8 @@ void ChromotingInstance::Connect(const ClientConfig& config) {
   // Setup the PepperSignalStrategy.
   signal_strategy_.reset(new PepperSignalStrategy(
       config.local_jid,
-      base::Bind(&ChromotingInstance::SendOutgoingIq, AsWeakPtr())));
+      base::Bind(&ChromotingInstance::SendOutgoingIq,
+                 weak_factory_.GetWeakPtr())));
 
   scoped_ptr<cricket::HttpPortAllocatorBase> port_allocator(
       PepperPortAllocator::Create(this));
@@ -714,7 +715,8 @@ void ChromotingInstance::Connect(const ClientConfig& config) {
 
   // Start timer that periodically sends perf stats.
   plugin_task_runner_->PostDelayedTask(
-      FROM_HERE, base::Bind(&ChromotingInstance::SendPerfStats, AsWeakPtr()),
+      FROM_HERE, base::Bind(&ChromotingInstance::SendPerfStats,
+                            weak_factory_.GetWeakPtr()),
       base::TimeDelta::FromMilliseconds(kPerfStatsIntervalMs));
 }
 
@@ -875,7 +877,8 @@ void ChromotingInstance::SendPerfStats() {
   }
 
   plugin_task_runner_->PostDelayedTask(
-      FROM_HERE, base::Bind(&ChromotingInstance::SendPerfStats, AsWeakPtr()),
+      FROM_HERE, base::Bind(&ChromotingInstance::SendPerfStats,
+                            weak_factory_.GetWeakPtr()),
       base::TimeDelta::FromMilliseconds(kPerfStatsIntervalMs));
 
   scoped_ptr<base::DictionaryValue> data(new base::DictionaryValue());
--- remoting/client/plugin/chromoting_instance.h
+++ remoting/client/plugin/chromoting_instance.h
@@ -72,8 +72,7 @@ class ChromotingInstance :
       public ClientUserInterface,
       public protocol::ClipboardStub,
       public protocol::CursorShapeStub,
-      public pp::Instance,
-      public base::SupportsWeakPtr<ChromotingInstance> {
+      public pp::Instance {
  public:
   // Plugin API version. This should be incremented whenever the API
   // interface changes.
@@ -259,6 +258,7 @@ class ChromotingInstance :
 
   base::WeakPtr<PepperTokenFetcher> pepper_token_fetcher_;
 
+  // Weak reference to this instance, used for global logging and task posting.
   base::WeakPtrFactory<ChromotingInstance> weak_factory_;
 
   DISALLOW_COPY_AND_ASSIGN(ChromotingInstance);
