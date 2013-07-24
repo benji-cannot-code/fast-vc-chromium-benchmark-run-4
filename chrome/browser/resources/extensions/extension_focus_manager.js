62216d5df46c718ecb3154f595f9a25d4d5c0d45
tkent@chromium.org <tkent@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 04:45:37 +0000

    Extension settings: Should not focus on buttons by mouse click.
    We don't need the focus-buttons-by-mouse behavior in the extension
    settings page.
    
    BUG=257544,262925
    
    Review URL: https://chromiumcodereview.appspot.com/19471002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213340 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/extensions/extension_focus_manager.js
+++ chrome/browser/resources/extensions/extension_focus_manager.js
@@ -7,6 +7,7 @@ cr.define('extensions', function() {
   var FocusManager = cr.ui.FocusManager;
 
   function ExtensionFocusManager() {
+    FocusManager.disableMouseFocusOnButtons();
   }
 
   cr.addSingletonGetter(ExtensionFocusManager);
