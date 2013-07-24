5456303e019b1dd52899e1b4212452704dccfcca
aedla@chromium.org <aedla@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 23:47:18 +0000

    Fix MonitorFinder UaF.
    Don't call Bind(..., this) before taking the first reference.
    
    BUG=262291
    
    Review URL: https://chromiumcodereview.appspot.com/19637013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213549 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/renderer_host/pepper/pepper_flash_drm_host.cc
+++ chrome/browser/renderer_host/pepper/pepper_flash_drm_host.cc
@@ -51,7 +51,6 @@ class MonitorFinder : public base::RefCountedThreadSafe<MonitorFinder> {
         render_id_(render_id),
         monitor_(NULL),
         request_sent_(0) {
-    GetMonitor();
   }
 
   int64_t GetMonitor() {
@@ -132,6 +131,7 @@ PepperFlashDRMHost::PepperFlashDRMHost(BrowserPpapiHost* host,
 
   fetcher_ = new DeviceIDFetcher(render_process_id);
   monitor_finder_ = new MonitorFinder(render_process_id, render_view_id);
+  monitor_finder_->GetMonitor();
 }
 
 PepperFlashDRMHost::~PepperFlashDRMHost() {
