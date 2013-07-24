50aa95f1ee1c90373f2e6a2724072f90a03ba81b
rsesek@chromium.org <rsesek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-24 18:52:04 +0000

    Update include paths in ash/system/ for base/process changes.
    BUG=242290
    TBR=stevenjb@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/19915004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213476 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/monitor/tray_monitor.cc
+++ ash/system/monitor/tray_monitor.cc
@@ -6,7 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ash/system/monitor/tray_monitor.h"
 
 #include "ash/system/tray/tray_item_view.h"
-#include "base/process_util.h"
+#include "base/process/memory.h"
+#include "base/process/process_metrics.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "content/public/browser/gpu_data_manager.h"
