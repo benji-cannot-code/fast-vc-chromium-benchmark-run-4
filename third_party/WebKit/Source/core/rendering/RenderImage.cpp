64284e343dd0be72f3c0005d403392a7030d4ffb
schenney@chromium.org <schenney@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-25 15:17:23 +0000

    Fix a missing GraphicsContext save
    R=eae@chromium.org, senorblanco@chromium.org
    BUG=257352
    
    Review URL: https://codereview.chromium.org/20186003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154917 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderImage.cpp
+++ third_party/WebKit/Source/core/rendering/RenderImage.cpp
@@ -41,6 +41,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/Font.h"
 #include "core/platform/graphics/FontCache.h"
 #include "core/platform/graphics/GraphicsContext.h"
+#include "core/platform/graphics/GraphicsContextStateSaver.h"
 #include "core/rendering/HitTestResult.h"
 #include "core/rendering/PaintInfo.h"
 #include "core/rendering/RenderView.h"
@@ -419,6 +420,7 @@ void RenderImage::paintAreaElementFocusRing(PaintInfo& paintInfo)
 
     // FIXME: Clip path instead of context when Skia pathops is ready.
     // https://crbug.com/251206
+    GraphicsContextStateSaver savedContext(*paintInfo.context);
     paintInfo.context->clip(absoluteContentBox());
     paintInfo.context->drawFocusRing(path, outlineWidth,
         areaElementStyle->outlineOffset(),
