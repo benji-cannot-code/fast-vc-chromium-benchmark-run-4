7efb0ea0a7d30dc64495641cb3cb38eddc5e2a44
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-25 05:28:15 +0000

    Introduce toSVGRectElement(), use it
    As a step to change static_cast with toSVGXXX, static_cast<SVGRectElement*> can
    be changed with toSVGRectElement().
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19781008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154897 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGRectElement.h
+++ third_party/WebKit/Source/core/svg/SVGRectElement.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGRectElement_h
 #define SVGRectElement_h
 
+#include "SVGNames.h"
 #include "core/svg/SVGAnimatedBoolean.h"
 #include "core/svg/SVGAnimatedLength.h"
 #include "core/svg/SVGExternalResourcesRequired.h"
@@ -58,6 +59,12 @@ private:
         DECLARE_ANIMATED_BOOLEAN(ExternalResourcesRequired, externalResourcesRequired)
     END_DECLARE_ANIMATED_PROPERTIES
 };
+
+inline SVGRectElement* toSVGRectElement(Node* node)
+{
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || node->hasTagName(SVGNames::rectTag));
+    return static_cast<SVGRectElement*>(node);
+}
 
 } // namespace WebCore
 
