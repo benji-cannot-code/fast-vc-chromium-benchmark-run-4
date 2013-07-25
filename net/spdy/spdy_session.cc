1ad7fe6912b2ed0d822695a70047aae798aba6d5
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-25 01:54:07 +0000

    [SPDY] Allow a stream to be created while a SpdySession is in an IO loop
    This can happen if a stream is created when another stream is closed due
    to received data.
    
    BUG=263690
    R=rch@chromium.org
    
    Review URL: https://codereview.chromium.org/20135003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213574 0039d316-1c4b-4281-b951-d872f2087c98

--- net/spdy/spdy_session.cc
+++ net/spdy/spdy_session.cc
@@ -555,8 +555,11 @@ int SpdySession::GetPushStream(
   return OK;
 }
 
+// {,Try}CreateStream() and TryAccessStream() can be called with
+// |in_io_loop_| set if a stream is being created in response to
+// another being closed due to received data.
+
 Error SpdySession::TryAccessStream(const GURL& url) {
-  CHECK(!in_io_loop_);
   DCHECK_NE(availability_state_, STATE_CLOSED);
 
   if (is_secure_ && certificate_error_code_ != OK &&
@@ -576,7 +579,6 @@ Error SpdySession::TryAccessStream(const GURL& url) {
 int SpdySession::TryCreateStream(SpdyStreamRequest* request,
                                  base::WeakPtr<SpdyStream>* stream) {
   CHECK(request);
-  CHECK(!in_io_loop_);
 
   if (availability_state_ == STATE_GOING_AWAY)
     return ERR_FAILED;
@@ -603,7 +605,6 @@ int SpdySession::TryCreateStream(SpdyStreamRequest* request,
 
 int SpdySession::CreateStream(const SpdyStreamRequest& request,
                               base::WeakPtr<SpdyStream>* stream) {
-  CHECK(!in_io_loop_);
   DCHECK_GE(request.priority(), MINIMUM_PRIORITY);
   DCHECK_LT(request.priority(), NUM_PRIORITIES);
 
