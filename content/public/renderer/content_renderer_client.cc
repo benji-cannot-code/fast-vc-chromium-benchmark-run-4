b518efcc49628852c1dbbc238c7c8e5f5d922a3e
eroman@chromium.org <eroman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-25 20:05:56 +0000

    Use a mock implementation of Platform::crypto() for layout tests.
    BUG=245025
    R=darin@chromium.org, jochen@chromium.org
    
    Review URL: https://codereview.chromium.org/19594005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213680 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/renderer/content_renderer_client.cc
+++ content/public/renderer/content_renderer_client.cc
@@ -84,6 +84,10 @@ WebKit::WebSpeechSynthesizer* ContentRendererClient::OverrideSpeechSynthesizer(
   return NULL;
 }
 
+WebKit::WebCrypto* ContentRendererClient::OverrideWebCrypto() {
+  return NULL;
+}
+
 bool ContentRendererClient::RunIdleHandlerWhenWidgetsHidden() {
   return true;
 }
