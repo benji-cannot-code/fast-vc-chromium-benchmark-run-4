e6966d8f164af59e141032604884b0358ca81ca3
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-25 13:44:04 +0000

    Convert AuthService to use OAuth2TokenService instead of TokenService.
    BUG=258107,162157
    R=atwilson@chromium.org, kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/19158005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213622 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/google_apis/auth_service.h
+++ chrome/browser/google_apis/auth_service.h
@@ -13,9 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/observer_list.h"
 #include "base/threading/thread_checker.h"
 #include "chrome/browser/google_apis/auth_service_interface.h"
-#include "chrome/browser/google_apis/gdata_errorcode.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
+#include "chrome/browser/signin/oauth2_token_service.h"
 
 class Profile;
 
@@ -32,7 +30,7 @@ class AuthServiceObserver;
 // (TokenService) and provides OAuth2 token refresh infrastructure.
 // All public functions must be called on UI thread.
 class AuthService : public AuthServiceInterface,
-                    public content::NotificationObserver {
+                    public OAuth2TokenService::Observer {
  public:
   // |url_request_context_getter| is used to perform authentication with
   // URLFetcher.
@@ -53,10 +51,11 @@ class AuthService : public AuthServiceInterface,
   virtual void ClearAccessToken() OVERRIDE;
   virtual void ClearRefreshToken() OVERRIDE;
 
-  // Overridden from content::NotificationObserver:
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // Overridden from OAuth2TokenService::Observer:
+  virtual void OnRefreshTokenAvailable(const std::string& account_id) OVERRIDE;
+  virtual void OnRefreshTokenRevoked(
+      const std::string& account_id,
+      const GoogleServiceAuthError& error) OVERRIDE;
 
   // Sets the access_token as specified.  This should be used only for testing.
   void set_access_token_for_testing(const std::string& token) {
@@ -69,6 +68,9 @@ class AuthService : public AuthServiceInterface,
   static bool CanAuthenticate(Profile* profile);
 
  private:
+  // Called when the state of the refresh token changes.
+  void OnHandleRefreshToken(bool has_refresh_token);
+
   // Called when authentication request from StartAuthentication() is
   // completed.
   void OnAuthCompleted(const AuthStatusCallback& callback,
@@ -77,13 +79,11 @@ class AuthService : public AuthServiceInterface,
 
   Profile* profile_;
   net::URLRequestContextGetter* url_request_context_getter_;  // Not owned.
-  std::string refresh_token_;
+  bool has_refresh_token_;
   std::string access_token_;
   std::vector<std::string> scopes_;
   ObserverList<AuthServiceObserver> observers_;
   base::ThreadChecker thread_checker_;
-
-  content::NotificationRegistrar registrar_;
 
   // Note: This should remain the last member so it'll be destroyed and
   // invalidate its weak pointers before any other members are destroyed.
