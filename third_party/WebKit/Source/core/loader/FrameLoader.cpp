7685b105cc58d826c979947fef4b19d6151ce467
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-25 02:51:19 +0000

    Refactoring: Clarifying DOMWindow ownership by removing Frame::setDOMWindow()
    This chagne replaces Frame::setDOMWindow() by Frame::clearDOMWindow() and
    Frame::ensureDOMWindow() to clarify the owner of DOMWindow objects.
    Now it is created and owned by Frame.
    
    BUG=none
    TEST=none
    R=abarth, japhet
    
    Review URL: https://chromiumcodereview.appspot.com/19798011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoader.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoader.cpp
@@ -430,10 +430,10 @@ void FrameLoader::cancelAndClear()
     if (!m_isComplete)
         closeURL();
 
-    clear(false);
+    clear(ClearScriptObjects | ClearWindowObject);
 }
 
-void FrameLoader::clear(bool clearWindowProperties, bool clearScriptObjects, bool clearFrameView)
+void FrameLoader::clear(ClearOptions options)
 {
     m_frame->editor()->clear();
 
@@ -449,7 +449,7 @@ void FrameLoader::clear(bool clearWindowProperties, bool clearScriptObjects, boo
     }
 
     // Do this after detaching the document so that the unload event works.
-    if (clearWindowProperties) {
+    if (options & ClearWindowProperties) {
         InspectorInstrumentation::frameWindowDiscarded(m_frame, m_frame->domWindow());
         m_frame->domWindow()->reset();
         m_frame->script()->clearWindowShell();
@@ -457,16 +457,17 @@ void FrameLoader::clear(bool clearWindowProperties, bool clearScriptObjects, boo
 
     m_frame->selection()->prepareForDestruction();
     m_frame->eventHandler()->clear();
-    if (clearFrameView && m_frame->view())
+    if (m_frame->view())
         m_frame->view()->clear();
 
     // Do not drop the DOMWindow (and Document) before the ScriptController and view are cleared
     // as some destructors might still try to access the document.
-    m_frame->setDOMWindow(0);
+    if (options & ClearWindowObject)
+        m_frame->clearDOMWindow();
 
     m_subframeLoader.clear();
 
-    if (clearScriptObjects)
+    if (options & ClearScriptObjects)
         m_frame->script()->clearScriptObjects();
 
     m_frame->script()->enableEval();
--- third_party/WebKit/Source/core/page/Frame.h
+++ third_party/WebKit/Source/core/page/Frame.h
@@ -87,7 +87,9 @@ namespace WebCore {
         Page* page() const;
         HTMLFrameOwnerElement* ownerElement() const;
 
-        void setDOMWindow(PassRefPtr<DOMWindow>);
+        void clearDOMWindow();
+        void ensureDOMWindow();
+
         DOMWindow* domWindow() const;
         Document* document() const;
         FrameView* view() const;
