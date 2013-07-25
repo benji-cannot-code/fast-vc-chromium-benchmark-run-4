dc0ac25b5b865a371a12b737362095c0bcdc70f5
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-25 13:13:13 +0000

    Add 'self.onerror' to Worker tests.
    We're currently not testing 'self.onerror' for same-domain or cross-domain
    calls to 'importScript'. This patch adds tests for this functionality, but makes
    no functional changes.
    
    R=ch.dumez@sisa.samsung.com
    
    Review URL: https://codereview.chromium.org/19605005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154914 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/workers/resources/worker-importscripts-onerror-crossorigin.js
+++ third_party/WebKit/LayoutTests/http/tests/workers/resources/worker-importscripts-onerror-crossorigin.js
@@ -1,2 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.onerror = function (message, filename, lineno, column) {
+    postMessage({ 'message': message, 'filename': filename, 'lineno': lineno, 'column': column });
+};
+
 importScripts('http://localhost:8000/workers/resources/worker-importScripts-throw.js');
--- third_party/WebKit/LayoutTests/http/tests/workers/resources/worker-importscripts-onerror-redirect-to-crossorigin.js
+++ third_party/WebKit/LayoutTests/http/tests/workers/resources/worker-importscripts-onerror-redirect-to-crossorigin.js
@@ -1,3 +1,7 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+self.onerror = function (message, filename, lineno, column) {
+    postMessage({ 'message': message, 'filename': filename, 'lineno': lineno, 'column': column });
+};
+
 var differentRedirectOrigin = "/resources/redirect.php?url=http://localhost:8000/workers/resources/worker-importScripts-throw.js";
 importScripts(differentRedirectOrigin)
