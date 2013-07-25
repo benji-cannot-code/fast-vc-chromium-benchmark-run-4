51ba5e6566e5d0d19b1b4ce1cf990e1123034720
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-25 12:21:13 +0000

    FileAPI: Clean up around FileSystemBackend
    Outdated comments and variable names still remain around FileSystemBackend.
    This change cleans them up.
    
    BUG=241701
    TEST=none
    R=kinuko@chromium.org
    
    Review URL: https://codereview.chromium.org/20277003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213616 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/fileapi/browser_file_system_helper.cc
+++ content/browser/fileapi/browser_file_system_helper.cc
@@ -64,12 +64,12 @@ scoped_refptr<fileapi::FileSystemContext> CreateFileSystemContext(
           BrowserThread::GetMessageLoopProxyForThread(BrowserThread::IO).get(),
           file_task_runner.get()));
 
-  // Setting up additional mount point providers.
-  ScopedVector<fileapi::FileSystemBackend> additional_providers;
+  // Setting up additional filesystem backends.
+  ScopedVector<fileapi::FileSystemBackend> additional_backends;
   GetContentClient()->browser()->GetAdditionalFileSystemBackends(
       browser_context,
       profile_path,
-      &additional_providers);
+      &additional_backends);
 
   scoped_refptr<fileapi::FileSystemContext> file_system_context =
       new fileapi::FileSystemContext(
@@ -77,7 +77,7 @@ scoped_refptr<fileapi::FileSystemContext> CreateFileSystemContext(
           BrowserContext::GetMountPoints(browser_context),
           browser_context->GetSpecialStoragePolicy(),
           quota_manager_proxy,
-          additional_providers.Pass(),
+          additional_backends.Pass(),
           profile_path,
           CreateBrowserFileSystemOptions(is_incognito));
 
@@ -105,9 +105,7 @@ bool CheckFileSystemPermissionsForProcess(
     return false;
   }
 
-  fileapi::FileSystemBackend* mount_point_provider =
-      context->GetFileSystemBackend(url.type());
-  if (!mount_point_provider) {
+  if (!context->GetFileSystemBackend(url.type())) {
     *error = base::PLATFORM_FILE_ERROR_INVALID_URL;
     return false;
   }
