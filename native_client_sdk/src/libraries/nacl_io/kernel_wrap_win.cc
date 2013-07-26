78678c009b87add3a29d44391fba98388d299baa
bradnelson@google.com <bradnelson@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 13:03:00 +0000

    Simplify ppapi_simple by use of environment variables.
    Converting all embed tag name, value attributes to environment variables.
    Converting attributes of the for ARGn="foo" to positional argv parameters to
    main.
    
    Dropping GetProperty in favor of getenv + setenv for defaults.
    
    Dropping argv[] passing to PPAPI_MAIN as this does not provide any additional
    opportunity for reconfigurability and is not used in any examples.
    
    BUG=None
    TEST=None
    R=noelallen@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/20083004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213875 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/kernel_wrap_win.cc
+++ native_client_sdk/src/libraries/nacl_io/kernel_wrap_win.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <errno.h>
 #include <fcntl.h>
 #include <stdarg.h>
+#include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>  // This must be included before <sys/stat.h>.
 #include <sys/stat.h>
@@ -202,6 +203,19 @@ int remove(const char* path) {
 
 int _rmdir(const char* path) {
   return ki_rmdir(path);
+}
+
+int setenv(const char* name, const char* value, int overwrite) {
+  if (0 == overwrite && NULL != getenv(name)) {
+    return 0;
+  }
+  errno_t result = _putenv_s(name, value);
+  if (result != 0) {
+    errno = result;
+    return -1;
+  } else {
+    return 0;
+  }
 }
 
 int _stat32(const char* path, struct _stat32* buf) {
--- native_client_sdk/src/libraries/ppapi_simple/ps_main.cc
+++ native_client_sdk/src/libraries/ppapi_simple/ps_main.cc
@@ -10,8 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi_simple/ps_main.h"
 
 
-void* PSMainCreate(PP_Instance inst, PSMainFunc_t func, const char* argv[]) {
-  PSInstance* pInst = new PSInstance(inst, argv);
+void* PSMainCreate(PP_Instance inst, PSMainFunc_t func) {
+  PSInstance* pInst = new PSInstance(inst);
   pInst->SetMain(func);
   return pInst;
 }
