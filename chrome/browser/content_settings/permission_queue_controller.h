dc3f6b9b6298e1a992db1c8690a713c7dd367b6a
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 20:32:09 +0000

    Fix a leak in permission_queue_controller_unittest.cc.
    PermissionsQueueController used to just drop all pending requests/infobars on
    the floor when destroyed.  After this change, it closes all open infobars on
    destruction.
    
    This also adds an explicit notification listener to the test harness that
    deletes infobars when they're removed, fixing the actual leak.  This is only
    necessary until my refactor lands, at which point these will be deleted
    automatically on removal.
    
    Removes some completely outdated suppressions, removes the suppressions covering
    the fixed leak, and temporarily comments out one remaining suppression of a
    different issue that has this same bug number in order to get an allocation
    stack from the bot.
    
    Cleans up the unittest file a bit.
    
    BUG=155530
    TEST=none
    R=markusheintz@chromium.org
    
    Review URL: https://codereview.chromium.org/20583002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213979 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/content_settings/permission_queue_controller.h
+++ chrome/browser/content_settings/permission_queue_controller.h
@@ -24,7 +24,7 @@ class Profile;
 // things listening for such notifications.
 // For the time being this class is self-contained and it doesn't seem pulling
 // the notification infrastructure would simplify.
-class PermissionQueueController : content::NotificationObserver {
+class PermissionQueueController : public content::NotificationObserver {
  public:
   typedef base::Callback<void(bool /* allowed */)> PermissionDecidedCallback;
 
@@ -86,6 +86,7 @@ class PermissionQueueController : content::NotificationObserver {
   Profile* const profile_;
   ContentSettingsType type_;
   PendingInfoBarRequests pending_infobar_requests_;
+  bool in_shutdown_;
 
   DISALLOW_COPY_AND_ASSIGN(PermissionQueueController);
 };
