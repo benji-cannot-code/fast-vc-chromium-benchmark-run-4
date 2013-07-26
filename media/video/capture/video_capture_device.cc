7772c81a66eca114884f407c7e2e02dffdfb9370
bemasc@google.com <bemasc@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 08:27:38 +0000

    Make Mac and Windows include the USB ID in the device name like Linux.
    Review URL: https://chromiumcodereview.appspot.com/19598004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213796 0039d316-1c4b-4281-b951-d872f2087c98

--- media/video/capture/video_capture_device.cc
+++ media/video/capture/video_capture_device.cc
@@ -7,6 +7,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace media {
 
+const std::string VideoCaptureDevice::Name::GetNameAndModel() const {
+// On Linux, the device name already includes the model identifier.
+#if !defined(OS_LINUX)
+  std::string model_id = GetModel();
+  if (!model_id.empty())
+    return device_name_ + " (" + model_id + ")";
+#endif  // if !defined(OS_LINUX)
+  return device_name_;
+}
+
 VideoCaptureDevice::Name*
 VideoCaptureDevice::Names::FindById(const std::string& id) {
   for (iterator it = begin(); it != end(); ++it) {
