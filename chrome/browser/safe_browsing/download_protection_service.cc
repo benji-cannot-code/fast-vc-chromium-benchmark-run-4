3f103c91d7a8db5051e8bb2741c671d21766c2bb
mattm@chromium.org <mattm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 03:08:45 +0000

    Add detailed histograms for DownloadProtectionService URLFetcher errors.
    BUG=263590
    R=isherman@chromium.org, mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/19599012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213747 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/safe_browsing/download_protection_service.cc
+++ chrome/browser/safe_browsing/download_protection_service.cc
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/weak_ptr.h"
 #include "base/metrics/histogram.h"
+#include "base/metrics/sparse_histogram.h"
 #include "base/sequenced_task_runner_helpers.h"
 #include "base/stl_util.h"
 #include "base/strings/string_number_conversions.h"
@@ -389,6 +390,14 @@ class DownloadProtectionService::CheckClientDownloadRequest
             << item_->GetUrlChain().back() << ": success="
             << source->GetStatus().is_success() << " response_code="
             << source->GetResponseCode();
+    if (source->GetStatus().is_success()) {
+      UMA_HISTOGRAM_SPARSE_SLOWLY(
+          "SBClientDownload.DownloadRequestResponseCode",
+          source->GetResponseCode());
+    }
+    UMA_HISTOGRAM_SPARSE_SLOWLY(
+        "SBClientDownload.DownloadRequestNetError",
+        -source->GetStatus().error());
     DownloadCheckResultReason reason = REASON_SERVER_PING_FAILED;
     DownloadCheckResult result = SAFE;
     if (source->GetStatus().is_success() &&
