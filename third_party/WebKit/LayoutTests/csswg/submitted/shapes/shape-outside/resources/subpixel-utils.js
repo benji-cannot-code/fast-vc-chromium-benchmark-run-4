0bbd84cb6423b5e374dcdd945024a07678044ed0
bjonesbe@adobe.com <bjonesbe@adobe.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-26 23:55:37 +0000

    New positioning model: support for rounded rectangle shape-outside
    Update the shape-outside rounded rectangle tests and import them from
    the CSS WG's repository. They have been moved to the CSS WG repository
    so they can be part of the spec test suite for CSS Shapes.
    
    This is a port from WebKit: http://trac.webkit.org/changeset/153063
    
    BUG=260357
    
    Review URL: https://chromiumcodereview.appspot.com/20786002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155029 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/csswg/submitted/shapes/shape-outside/resources/subpixel-utils.js
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+var SubPixelLayout = (function() {
+    var SUBPIXELS_PER_PIXEL = 64;
+    var enabled = undefined;
+
+    function isEnabled()
+    {
+        if (enabled === undefined) {
+            var elem = document.createElement('div');
+            elem.style.setProperty('width', '4.5px');
+            document.body.appendChild(elem);
+            var bounds = elem.getBoundingClientRect();
+            enabled = (bounds.width != Math.floor(bounds.width));
+            document.body.removeChild(elem);
+        }
+        return enabled;
+    }
+
+    return {
+        isEnabled: isEnabled,
+        snapToLayoutUnit: function(f) {
+            return isEnabled() ? Math.floor(f * SUBPIXELS_PER_PIXEL) / SUBPIXELS_PER_PIXEL : Math.floor(f); // as in LayoutUnit(f).toFloat()
+        },
+        ceilSnapToLayoutUnit: function(f) {
+            return isEnabled() ? Math.ceil(f * SUBPIXELS_PER_PIXEL) / SUBPIXELS_PER_PIXEL : Math.ceil(f); // see ceiledLayoutUnit(), LayoutUnit.h
+        }
+    }
+}());
