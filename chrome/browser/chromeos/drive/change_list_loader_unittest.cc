51737cc5c3b3c5b57826874ccbdfed5a53d812fd
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 08:14:02 +0000

    drive: Make ChangeListLoaderTest more comprehensive
    This should be the last piece.
    Verify that observers get notified about directory changes on fast fetch.
    
    BUG=161703
    TEST=unit_tests --gtest_filter="ChangeListLoaderTest.*"
    R=kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/20632004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213793 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/change_list_loader_unittest.cc
+++ chrome/browser/chromeos/drive/change_list_loader_unittest.cc
@@ -39,6 +39,8 @@ class TestChangeListLoaderObserver : public ChangeListLoaderObserver {
   const std::set<base::FilePath>& changed_directories() const {
     return changed_directories_;
   }
+  void clear_changed_directories() { changed_directories_.clear(); }
+
   int load_from_server_complete_count() const {
     return load_from_server_complete_count_;
   }
@@ -255,6 +257,8 @@ TEST_F(ChangeListLoaderTest, LoadIfNeeded_LocalMetadataAvailable) {
 }
 
 TEST_F(ChangeListLoaderTest, LoadIfNeeded_MyDrive) {
+  TestChangeListLoaderObserver observer(change_list_loader_.get());
+
   // Emulate the slowness of GetAllResourceList().
   drive_service_->set_never_return_all_resource_list(true);
 
@@ -265,6 +269,9 @@ TEST_F(ChangeListLoaderTest, LoadIfNeeded_MyDrive) {
       google_apis::test_util::CreateCopyResultCallback(&error));
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_EQ(1U, observer.changed_directories().count(
+      util::GetDriveGrandRootPath()));
+  observer.clear_changed_directories();
 
   // GetAllResourceList() was called.
   EXPECT_EQ(1, drive_service_->blocked_call_count());
@@ -289,6 +296,8 @@ TEST_F(ChangeListLoaderTest, LoadIfNeeded_MyDrive) {
       google_apis::test_util::CreateCopyResultCallback(&error));
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_EQ(1U, observer.changed_directories().count(
+      util::GetDriveMyDriveRootPath()));
 
   // Now the file is present.
   EXPECT_EQ(FILE_ERROR_OK,
@@ -319,12 +328,15 @@ TEST_F(ChangeListLoaderTest, LoadIfNeeded_NewDirectories) {
   EXPECT_TRUE(change_list_loader_->IsRefreshing());
 
   // Load My Drive.
+  TestChangeListLoaderObserver observer(change_list_loader_.get());
   change_list_loader_->LoadIfNeeded(
       DirectoryFetchInfo(drive_service_->GetRootResourceId(),
                          metadata_->GetLargestChangestamp()),
       google_apis::test_util::CreateCopyResultCallback(&error));
   base::RunLoop().RunUntilIdle();
   EXPECT_EQ(FILE_ERROR_OK, error);
+  EXPECT_EQ(1U, observer.changed_directories().count(
+      util::GetDriveMyDriveRootPath()));
 
   // The new file is present in the local metadata.
   base::FilePath file_path =
