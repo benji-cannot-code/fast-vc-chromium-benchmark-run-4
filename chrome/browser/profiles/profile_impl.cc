231c7cb9ece4c0dc848344524733b77e9c080100
atwilson@chromium.org <atwilson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 10:30:00 +0000

    Remove obsolete code to delay UPSS initialization on import.
    Import is now no longer done in parallel with a running profile, so we can get
    rid of the hacks to delay UserPolicySigninService startup until after import
    is completed.
    
    BUG=174864
    
    Review URL: https://chromiumcodereview.appspot.com/19773005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213824 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/profile_impl.cc
+++ chrome/browser/profiles/profile_impl.cc
@@ -573,12 +573,8 @@ void ProfileImpl::DoFinalInit() {
 #if !defined(OS_CHROMEOS)
   // Listen for bookmark model load, to bootstrap the sync service.
   // On CrOS sync service will be initialized after sign in.
-  if (!g_browser_process->profile_manager()->will_import()) {
-    // If |will_import()| is true we add the observer in
-    // ProfileManager::OnImportFinished().
-    BookmarkModel* model = BookmarkModelFactory::GetForProfile(this);
-    model->AddObserver(new BookmarkModelLoadedObserver(this));
-  }
+  BookmarkModel* model = BookmarkModelFactory::GetForProfile(this);
+  model->AddObserver(new BookmarkModelLoadedObserver(this));
 #endif
 }
 
@@ -1172,4 +1168,3 @@ PrefProxyConfigTracker* ProfileImpl::CreateProxyConfigTracker() {
   return ProxyServiceFactory::CreatePrefProxyConfigTrackerOfProfile(
       GetPrefs(), g_browser_process->local_state());
 }
-
