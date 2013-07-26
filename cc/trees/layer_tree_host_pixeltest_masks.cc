368cd3efd90106070a6d35fb967e5fd04c4c990d
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 12:30:17 +0000

    cc: A pixel test for readbacks of layers with CanClipSelf == true.
    TextureLayer is a self clipping layer, and this test verifies that
    we are able to correctly copy the entire layer when it is outside
    of the viewport.
    
    Also includes a couple unit tests for TextureMailboxes on layers and
    deleting the layer from the tree.
    
    R=piman
    BUG=263512
    
    Review URL: https://chromiumcodereview.appspot.com/20048004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213863 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/layer_tree_host_pixeltest_masks.cc
+++ cc/trees/layer_tree_host_pixeltest_masks.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "cc/layers/content_layer.h"
 #include "cc/layers/content_layer_client.h"
 #include "cc/layers/image_layer.h"
+#include "cc/layers/solid_color_layer.h"
 #include "cc/test/layer_tree_pixel_test.h"
 #include "cc/test/pixel_comparator.h"
 
