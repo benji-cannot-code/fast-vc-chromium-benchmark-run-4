d44e0155665e505793f0101ca81d4ed9bbce39f5
hclam@chromium.org <hclam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 01:19:53 +0000

    Make image_decoding_measurement more accurate
    The test sequence is now changed to:
    1. Load an image but not visible. Wait until fully loaded.
    2. Make the image visible. Wait for rAF.
    3. Make the invisible and go to (1).
    
    Waiting for the image to be fully loaded ensures there is no progressive
    decode. Timing of the decoding operation is better controlled by the
    visibility of the image.
    
    This change also makes the test runs faster by using only 5 iterations
    and less warm up time.
    
    BUG=260052
    R=nduca@chromium.org
    
    Review URL: https://codereview.chromium.org/20113003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213738 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/image_decoding.py
+++ tools/perf/measurements/image_decoding.py
@@ -50,3 +50,5 @@ class ImageDecoding(page_measurement.PageMeasurement):
       return
     image_decoding_avg = sum(durations) / len(durations)
     results.Add('ImageDecoding_avg', 'ms', image_decoding_avg)
+    results.Add('ImageLoading_avg', 'ms',
+                tab.EvaluateJavaScript('averageLoadingTimeMs()'))
