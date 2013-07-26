d51c392d947f37d9cd22570d21d1e732396e2d84
maksymb@chromium.org <maksymb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 13:47:28 +0000

    GCP2.0 Device: New parameters for command line: domain-name and service-name. Moved default variables to Printer.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/20150004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213893 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/gcp20/prototype/command_line_reader.h
+++ cloud_print/gcp20/prototype/command_line_reader.h
@@ -6,12 +6,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CLOUD_PRINT_GCP20_PROTOTYPE_COMMAND_LINE_READER_H_
 #define CLOUD_PRINT_GCP20_PROTOTYPE_COMMAND_LINE_READER_H_
 
+#include <string>
+
 #include "base/basictypes.h"
 
 namespace command_line_reader {
 
-uint16 ReadHttpPort();
-uint32 ReadTtl();
+uint16 ReadHttpPort(uint16 default_value);
+uint32 ReadTtl(uint32 default_value);
+std::string ReadServiceNamePrefix(const std::string& default_value);
+std::string ReadDomainName(const std::string& default_value);
 
 }  // namespace command_line_reader
 
