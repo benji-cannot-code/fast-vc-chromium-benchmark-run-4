e8755455c6435d6f6758c12bb6404b973beecf04
marja@chromium.org <marja@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-26 06:33:57 +0000

    Revert "Remove toV8FastForMainWorld."
    This reverts revision 154673.
    
    Reason: We want to see if the dromaeo regression goes away, see
    crbug.com/264056
    
    TBR=marja,abarth
    BUG=264056
    
    Review URL: https://codereview.chromium.org/20668002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154980 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/tests/results/V8TestTypedefs.cpp
+++ third_party/WebKit/Source/bindings/tests/results/V8TestTypedefs.cpp
@@ -23,7 +23,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "V8TestTypedefs.h"
 
 #include "RuntimeEnabledFeatures.h"
-#include "SerializedScriptValue.h"
 #include "V8SVGPoint.h"
 #include "V8SerializedScriptValue.h"
 #include "V8TestCallback.h"
