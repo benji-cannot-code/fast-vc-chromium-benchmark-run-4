3f0a53c7993c298897cc293a41173befd9c08ac1
epenner@chromium.org <epenner@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 11:51:52 +0000

    GPU: Use TLS pointer for GLApi.
    This is straight from:
    https://codereview.chromium.org/16894002/
    
    To support GL access on multiple threads, GL-API needs
    to be in TLS. It hasn't mattered previously in practice
    because our limited threaded GL calls were always forwarded
    to the right native calls. But as we improve virtual contexts
    and unset the API on ReleaseCurrent, this will no longer be
    the case.
    
    BUG=261264,263398
    
    Review URL: https://chromiumcodereview.appspot.com/20014006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213852 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_gl_api_implementation.cc
+++ ui/gl/gl_gl_api_implementation.cc
@@ -144,6 +144,7 @@ void DriverGL::InitializeExtensions(GLContext* context) {
 }
 
 void InitializeGLBindingsGL() {
+  g_current_gl_context_tls = new base::ThreadLocalPointer<GLApi>;
   g_driver_gl.Initialize();
   if (!g_real_gl) {
     g_real_gl = new RealGLApi();
@@ -159,11 +160,11 @@ void InitializeGLBindingsGL() {
 }
 
 GLApi* GetCurrentGLApi() {
-  return g_current_gl_context;
+  return g_current_gl_context_tls->Get();
 }
 
 void SetGLApi(GLApi* api) {
-  g_current_gl_context = api;
+  g_current_gl_context_tls->Set(api);
 }
 
 void SetGLToRealGLApi() {
@@ -188,8 +189,11 @@ void ClearGLBindingsGL() {
     g_trace_gl = NULL;
   }
   g_gl = NULL;
-  g_current_gl_context = NULL;
   g_driver_gl.ClearBindings();
+  if (g_current_gl_context_tls) {
+    delete g_current_gl_context_tls;
+    g_current_gl_context_tls = NULL;
+  }
 }
 
 GLApi::GLApi() {
@@ -254,7 +258,7 @@ void VirtualGLApi::Initialize(DriverGL* driver, GLContext* real_context) {
 }
 
 bool VirtualGLApi::MakeCurrent(GLContext* virtual_context, GLSurface* surface) {
-  bool switched_contexts = g_current_gl_context != this;
+  bool switched_contexts = g_current_gl_context_tls->Get() != this;
   GLSurface* current_surface = GLSurface::GetCurrent();
   if (switched_contexts || surface != current_surface) {
     // MakeCurrent 'lite' path that avoids potentially expensive MakeCurrent()
