6266619c346df68684dec05b359011ff8730fe59
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 17:07:28 +0000

    DevTools: crash on chrome://inspect in incognito.
    BUG=264199
    R=vsevik@chromium.org
    
    Review URL: https://codereview.chromium.org/20759002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213942 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/inspect_ui.cc
+++ chrome/browser/ui/webui/inspect_ui.cc
@@ -222,7 +222,8 @@ void InspectMessageHandler::HandleInspectCommand(const ListValue* args) {
         data->GetString(kAdbFrontendUrlField, &frontend_url)) {
       scoped_refptr<DevToolsAdbBridge> adb_bridge =
           DevToolsAdbBridge::Factory::GetForProfile(profile);
-      adb_bridge->Attach(serial, socket, debug_url, frontend_url);
+      if (adb_bridge)
+        adb_bridge->Attach(serial, socket, debug_url, frontend_url);
     }
     return;
   }
@@ -409,8 +410,10 @@ void InspectUI::StartListeningNotifications() {
   observer_->Init(this);
 
   Profile* profile = Profile::FromWebUI(web_ui());
-  adb_bridge_ = DevToolsAdbBridge::Factory::GetForProfile(profile);
-  adb_bridge_->AddListener(this);
+  DevToolsAdbBridge* adb_bridge =
+      DevToolsAdbBridge::Factory::GetForProfile(profile);
+  if (adb_bridge)
+    adb_bridge->AddListener(this);
 
   registrar_.Add(this,
                  content::NOTIFICATION_WEB_CONTENTS_CONNECTED,
@@ -427,8 +430,11 @@ void InspectUI::StopListeningNotifications()
 {
   if (!observer_.get())
     return;
-  adb_bridge_->RemoveListener(this);
-  adb_bridge_ = NULL;
+  Profile* profile = Profile::FromWebUI(web_ui());
+  DevToolsAdbBridge* adb_bridge =
+      DevToolsAdbBridge::Factory::GetForProfile(profile);
+  if (adb_bridge)
+    adb_bridge->RemoveListener(this);
   observer_->InspectUIDestroyed();
   observer_ = NULL;
   registrar_.RemoveAll();
