1fb1b20182c9245a0ffbcbf95b1bb5662a8bd148
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-26 22:09:32 +0000

    Revert "Fix form resubmissions happening silently."
    This reverts commit 4c142fe7f51fbfdd20b8551f4759cabb48bbcb82.
    
    This is suspected to have broken
    NavigationHistoryTest.testNavigateBackToNoncacheableLoginPage on android.
    
    TBR=abarth
    BUG=
    
    Review URL: https://codereview.chromium.org/20840002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155023 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/cache/ResourceFetcher.cpp
+++ third_party/WebKit/Source/core/loader/cache/ResourceFetcher.cpp
@@ -632,7 +632,7 @@ ResourceRequestCachePolicy ResourceFetcher::resourceRequestCachePolicy(const Res
             return ReturnCacheDataDontLoad;
         if (!m_documentLoader->overrideEncoding().isEmpty() || frameLoadType == FrameLoadTypeBackForward)
             return ReturnCacheDataElseLoad;
-        if (isReload || frameLoadType == FrameLoadTypeSame || request.isConditional() || request.httpMethod() == "POST")
+        if (isReload || frameLoadType == FrameLoadTypeSame || request.isConditional())
             return ReloadIgnoringCacheData;
         return UseProtocolCachePolicy;
     }
