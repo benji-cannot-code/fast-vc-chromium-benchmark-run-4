8db13ed564e437146f7c98907134e8c1c08c5e2f
abarth@chromium.org <abarth@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-26 19:54:51 +0000

    Optimize WebURL -> GURL conversions
    When rendering sina.com.cn, we spend 5% of our total time converting between
    WebURL and GURL. Prior to this CL, the conversion involved a number of copies:
    
    1) From the String to a temporary buffer in String::utf8.
    2) From the temporary buffer to a CString.
    3) From the CString to a std::string.
    
    After this CL, we are able to copy directly from the String into the GURL.
    Instead of WebURL storing the spec in a CString, we now store the spec as a
    String, which lets WebURL simply ref the same buffer used by KURL. When
    building the GURL, we copy directly out of the String (assume it's ASCII, which
    is usually the case for canonicalized URLs) into a temporary std::string, which
    we move all the way into the GURL.
    
    R=jyasskin@chromium.org
    BUG=261412
    
    Review URL: https://chromiumcodereview.appspot.com/20135002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155006 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/chromium/support/WebURL.cpp
+++ third_party/WebKit/Source/core/platform/chromium/support/WebURL.cpp
@@ -37,7 +37,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebKit {
 
 WebURL::WebURL(const WebCore::KURL& url)
-    : m_spec(url.string().utf8())
+    : m_string(url.string())
     , m_parsed(url.parsed())
     , m_isValid(url.isValid())
 {
@@ -45,7 +45,7 @@ WebURL::WebURL(const WebCore::KURL& url)
 
 WebURL& WebURL::operator=(const WebCore::KURL& url)
 {
-    m_spec = url.string().utf8();
+    m_string = url.string();
     m_parsed = url.parsed();
     m_isValid = url.isValid();
     return *this;
@@ -53,7 +53,7 @@ WebURL& WebURL::operator=(const WebCore::KURL& url)
 
 WebURL::operator WebCore::KURL() const
 {
-    return WebCore::KURL(m_spec, m_parsed, m_isValid);
+    return WebCore::KURL(m_string, m_parsed, m_isValid);
 }
 
 } // namespace WebKit
