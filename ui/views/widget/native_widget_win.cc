885a347b06c785de2ba291f2a7f8134c84517396
Yukawa@chromium.org <Yukawa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 16:32:02 +0000

    Enable InputMethodBridge for Views on Windows.
    With this CL, NativeWidgetWin starts using InputMethodBridge 
    instead of InputMethodWin to conform the expected design
    of Views framework.
    
    This CL only affects non-Aura Windows build because
    NativeWidgetWin is not used in Aura build.
    
    TBR=sky
    BUG=246534, 246516
    TEST=Manually done on Windows 7 with built-in Japanese MS-IME
    
    Review URL: https://chromiumcodereview.appspot.com/17413012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213929 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/widget/native_widget_win.cc
+++ ui/views/widget/native_widget_win.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/dragdrop/os_exchange_data.h"
 #include "ui/base/dragdrop/os_exchange_data_provider_win.h"
 #include "ui/base/events/event.h"
+#include "ui/base/ime/input_method_factory.h"
 #include "ui/base/keycodes/keyboard_code_conversion_win.h"
 #include "ui/base/l10n/l10n_util_win.h"
 #include "ui/base/theme_provider.h"
@@ -42,7 +43,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/views/focus/accelerator_handler.h"
 #include "ui/views/focus/view_storage.h"
 #include "ui/views/focus/widget_focus_manager.h"
-#include "ui/views/ime/input_method_win.h"
+#include "ui/views/ime/input_method_bridge.h"
 #include "ui/views/widget/aero_tooltip_manager.h"
 #include "ui/views/widget/drop_target_win.h"
 #include "ui/views/widget/monitor_win.h"
@@ -212,7 +213,8 @@ bool NativeWidgetWin::HasCapture() const {
 }
 
 InputMethod* NativeWidgetWin::CreateInputMethod() {
-  return new InputMethodWin(GetMessageHandler(), GetNativeWindow(), NULL);
+  return new InputMethodBridge(GetMessageHandler(), ui::GetSharedInputMethod(),
+                               true);
 }
 
 internal::InputMethodDelegate* NativeWidgetWin::GetInputMethodDelegate() {
