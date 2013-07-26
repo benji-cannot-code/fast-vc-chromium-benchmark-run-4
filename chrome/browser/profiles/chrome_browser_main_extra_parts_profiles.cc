a95cac82caf0e5541ff308b9e0dff2b331da14fe
kmadhusu@chromium.org <kmadhusu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 09:56:44 +0000

    [Clean up] Declare dependencies for InstantServiceFactory.
    Declear dependencies for InstantServiceFactory and instantiate the factory in ChromeBrowserMainExtraPartsProfiles.
    
    BUG=246351
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/19734011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213818 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
+++ chrome/browser/profiles/chrome_browser_main_extra_parts_profiles.cc
@@ -78,6 +78,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/prerender/prerender_manager_factory.h"
 #include "chrome/browser/printing/cloud_print/cloud_print_proxy_service_factory.h"
 #include "chrome/browser/profiles/gaia_info_update_service_factory.h"
+#include "chrome/browser/search/instant_service_factory.h"
 #include "chrome/browser/search_engines/template_url_fetcher_factory.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/sessions/session_service_factory.h"
@@ -258,6 +259,7 @@ EnsureBrowserContextKeyedServiceFactoriesBuilt() {
   GoogleURLTrackerFactory::GetInstance();
   HistoryServiceFactory::GetInstance();
   invalidation::InvalidationServiceFactory::GetInstance();
+  InstantServiceFactory::GetInstance();
 #if defined(ENABLE_MANAGED_USERS)
   ManagedUserServiceFactory::GetInstance();
 #endif
--- chrome/browser/search/instant_service_factory.cc
+++ chrome/browser/search/instant_service_factory.cc
@@ -8,6 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/profiles/incognito_helpers.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/instant_service.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
+#include "chrome/browser/themes/theme_service_factory.h"
 #include "components/browser_context_keyed_service/browser_context_dependency_manager.h"
 
 // static
@@ -25,7 +27,10 @@ InstantServiceFactory::InstantServiceFactory()
     : BrowserContextKeyedServiceFactory(
         "InstantService",
         BrowserContextDependencyManager::GetInstance()) {
-  // No dependencies.
+  DependsOn(TemplateURLServiceFactory::GetInstance());
+#if defined(ENABLE_THEMES)
+  DependsOn(ThemeServiceFactory::GetInstance());
+#endif
 }
 
 InstantServiceFactory::~InstantServiceFactory() {
