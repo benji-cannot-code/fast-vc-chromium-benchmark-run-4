b83da6bcd377d5cae1266516123100431dcd222f
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 08:48:59 +0000

    [win] Initialize the app list menu lazily, and allow invalidation.
    Currently the app launcher will show the old profile name, and no login
    identity, after signing in for the first time, until the browser is
    restarted (or the app list profile is switched).
    
    This CL makes populating of the app list menu lazy, and allows the items
    to be invalidated when the signin status changes. Simply setting the
    CURRENT_USER menu item to be 'dynamic' is insufficient, because the menu
    item uses a custom view which needs to be released and re-appended.
    
    BUG=262639
    TEST=Have Chrome not signed in, open a browser. Open app launcher, see
    signin page, and sign in, leave browser window open. Reopen app
    launcher, open menu: it should show email of the just-signed-in user.
    
    Review URL: https://chromiumcodereview.appspot.com/19675010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213807 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/views/app_list_view.cc
+++ ui/app_list/views/app_list_view.cc
@@ -180,6 +180,7 @@ void AppListView::OnSigninStatusChanged() {
 
   signin_view_->SetVisible(needs_signin);
   app_list_main_view_->SetVisible(!needs_signin);
+  app_list_main_view_->search_box_view()->InvalidateMenu();
 }
 
 views::View* AppListView::GetInitiallyFocusedView() {
--- ui/app_list/views/search_box_view.h
+++ ui/app_list/views/search_box_view.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ui/app_list/search_box_model_observer.h"
-#include "ui/app_list/views/app_list_menu_views.h"
 #include "ui/views/controls/button/menu_button_listener.h"
 #include "ui/views/controls/textfield/textfield_controller.h"
 #include "ui/views/view.h"
@@ -22,6 +21,7 @@ class Textfield;
 
 namespace app_list {
 
+class AppListMenuViews;
 class AppListViewDelegate;
 class SearchBoxModel;
 class SearchBoxViewDelegate;
@@ -43,6 +43,7 @@ class SearchBoxView : public views::View,
 
   bool HasSearch() const;
   void ClearSearch();
+  void InvalidateMenu();
 
   views::Textfield* search_box() { return search_box_; }
 
@@ -79,9 +80,10 @@ class SearchBoxView : public views::View,
   virtual void TextChanged() OVERRIDE;
 
   SearchBoxViewDelegate* delegate_;  // Not owned.
+  AppListViewDelegate* view_delegate_;  // Not owned.
   SearchBoxModel* model_;  // Owned by AppListModel.
 
-  AppListMenuViews menu_;
+  scoped_ptr<AppListMenuViews> menu_;
 
   views::ImageView* icon_view_;  // Owned by views hierarchy.
   views::MenuButton* menu_button_;  // Owned by views hierarchy.
