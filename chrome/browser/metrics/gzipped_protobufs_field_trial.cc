7283255a08ac3f1f8c1e6832300785b60c0ff9b8
asharif@chromium.org <asharif@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 11:38:43 +0000

    metrics: extend gzipped protobufs field trial by 1 month.
    Extend the gzipped protobufs field trial by 1 month to expire 2013/10/1.
    
    BUG=none
    TEST=git cl try
    
    Review URL: https://chromiumcodereview.appspot.com/20002012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213847 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/gzipped_protobufs_field_trial.cc
+++ chrome/browser/metrics/gzipped_protobufs_field_trial.cc
@@ -38,7 +38,7 @@ void CreateGzippedProtobufsFieldTrial() {
           kTrialDivisor,
           kControlGroupName,
           2013,
-          9,
+          10,
           1,
           NULL);
   gzipped_protobufs_trial->UseOneTimeRandomization();
