00e6295b79c3daf12f2be5dc61a82a8027984f0a
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-26 02:20:14 +0000

    Share the Custom Element registration context with imported documents.
    When a sub-document is imported with HTML Imports it should share and
    contribute to the Custom Element definitions of the master document
    through a shared registration context.
    
    BUG=247219
    
    Review URL: https://chromiumcodereview.appspot.com/20327002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@154956 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/custom/test-harness-utils.js
+++ third_party/WebKit/LayoutTests/fast/dom/custom/test-harness-utils.js
@@ -1,10 +1,13 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Creates a frame and invokes f when the frame is ready.
-window.withFrame = function (f) {
+window.withFrame = function (opt_url, f) {
+    var url = typeof opt_url == 'string' ? opt_url : 'data:text/html,';
+    f = f || opt_url;
+
     var frame = document.createElement('iframe');
     frame.onload = function () {
         f(frame);
     };
-    frame.src = 'data:text/html,';
+    frame.src = url;
     document.body.appendChild(frame);
 };
