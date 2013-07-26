d308b615c5ee721c287b56b9eaaaaa6c3c45fea3
jdduke@chromium.org <jdduke@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-26 10:54:55 +0000

    [Android] Release overscroll glow when the scroll gesture ends
    This matches stock Android browser overscroll behavior by releasing the effect
    immediately when the user ends the scroll gesture (e.g., lifts their finger).
    
    BUG=135975
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/19805006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@213831 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/overscroll_glow.h
+++ content/browser/android/overscroll_glow.h
@@ -21,7 +21,7 @@ class Layer;
 
 namespace content {
 
-/* |OverscrollGlow| mirrors its Android counterpart, EdgeEffect.java.
+/* |OverscrollGlow| mirrors its Android counterpart, OverscrollGlow.java.
  * Conscious tradeoffs were made to align this as closely as possible with the
  * original Android java version.
  */
@@ -47,6 +47,11 @@ class OverscrollGlow {
   void OnOverscrolled(base::TimeTicks current_time,
                       gfx::Vector2dF overscroll,
                       gfx::Vector2dF velocity);
+
+  // Triggers glow recession for any active edges.
+  // Note: This does not actually release any resources; the name mirrors that
+  //       in Android's OverscrollGlow class.
+  void Release(base::TimeTicks current_time);
 
   // Returns true if the effect still needs animation ticks.
   bool Animate(base::TimeTicks current_time);
@@ -84,8 +89,7 @@ class OverscrollGlow {
               gfx::Vector2dF overscroll,
               gfx::Vector2dF old_overscroll);
 
-  void Release(base::TimeTicks current_time);
-  void Release(Axis axis, base::TimeTicks current_time);
+  void ReleaseAxis(Axis axis, base::TimeTicks current_time);
 
   EdgeEffect* GetOppositeEdge(int edge_index);
 
