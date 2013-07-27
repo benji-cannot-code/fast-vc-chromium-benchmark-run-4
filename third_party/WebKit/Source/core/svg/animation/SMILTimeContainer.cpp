6f75ad6ab9984e5d86f3c7c1f66e038e5620dd47
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-27 12:22:35 +0000

    Introduce toSVGSMILElement, and use it
    As a step to change static_cast with toSVGXXX, static_cast<SVGSMILElement*> can
    be changed with toSVGSMILElement().
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19798009
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155048 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
+++ third_party/WebKit/Source/core/svg/animation/SMILTimeContainer.cpp
@@ -231,7 +231,7 @@ void SMILTimeContainer::updateDocumentOrderIndexes()
     unsigned timingElementCount = 0;
     for (Element* element = m_ownerSVGElement; element; element = ElementTraversal::next(element, m_ownerSVGElement)) {
         if (SVGSMILElement::isSMILElement(element))
-            static_cast<SVGSMILElement*>(element)->setDocumentOrderIndex(timingElementCount++);
+            toSVGSMILElement(element)->setDocumentOrderIndex(timingElementCount++);
     }
     m_documentOrderIndexesDirty = false;
 }
