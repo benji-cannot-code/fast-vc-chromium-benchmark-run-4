2468fe31f2e08d830d86e03f20fa9c6aaaa8525d
hubbe@chromium.org <hubbe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-27 00:50:31 +0000

    Make sure that gpu workarounds are enabled in content_gl_tests.
    Review URL: https://chromiumcodereview.appspot.com/20101005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214013 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/gl_helper_unittests.cc
+++ content/common/gpu/client/gl_helper_unittests.cc
@@ -24,6 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/common/gpu/client/gl_helper_scaling.h"
 #include "content/public/test/unittest_test_suite.h"
 #include "content/test/content_test_suite.h"
+#include "gpu/config/gpu_util.h"
 #include "media/base/video_frame.h"
 #include "testing/gtest/include/gtest/gtest.h"
 #include "third_party/skia/include/core/SkBitmap.h"
@@ -1256,6 +1257,7 @@ int main(int argc, char** argv) {
   gfx::GtkInitFromCommandLine(*CommandLine::ForCurrentProcess());
 #endif
   gfx::GLSurface::InitializeOneOff();
+  gpu::ApplyGpuDriverBugWorkarounds(CommandLine::ForCurrentProcess());
 
   content::UnitTestTestSuite runner(suite);
   base::MessageLoop message_loop;
