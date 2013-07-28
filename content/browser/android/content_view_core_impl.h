5586d1f6355671f8ff55f5300eda0d456f1b04d9
mkosiba@chromium.org <mkosiba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-28 08:25:17 +0000

    [android] Remove stale Add/RemoveFrameInfoCallback methods.
    This removes the Add/RemoveFrameInfoCallback from ContentViewCore.
    These were introduced for the android_webview and are no longer
    in use (they were missed during the browser compositor dependency
    removal).
    
    BUG=None
    TESTS=compile
    Android-only change, ran through Android trybots.
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/20574004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214101 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/android/content_view_core_impl.h
+++ content/browser/android/content_view_core_impl.h
@@ -61,10 +61,6 @@ class ContentViewCoreImpl : public ContentViewCore,
   virtual float GetDpiScale() const OVERRIDE;
   virtual void RequestContentClipping(const gfx::Rect& clipping,
                                       const gfx::Size& content_size) OVERRIDE;
-  virtual void AddFrameInfoCallback(
-      const UpdateFrameInfoCallback& callback) OVERRIDE;
-  virtual void RemoveFrameInfoCallback(
-      const UpdateFrameInfoCallback& callback) OVERRIDE;
 
   // --------------------------------------------------------------------------
   // Methods called from Java via JNI
@@ -366,8 +362,6 @@ class ContentViewCoreImpl : public ContentViewCore,
 
   // The owning window that has a hold of main application activity.
   ui::WindowAndroid* window_android_;
-
-  std::vector<UpdateFrameInfoCallback> update_frame_info_callbacks_;
 
   DISALLOW_COPY_AND_ASSIGN(ContentViewCoreImpl);
 };
