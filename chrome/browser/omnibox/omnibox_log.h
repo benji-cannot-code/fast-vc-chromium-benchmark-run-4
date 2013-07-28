600c360158414f227650b1fa5552ab64bc58bd19
mpearson@chromium.org <mpearson@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-28 18:35:52 +0000

    Make a copy of PageClassification enum outside the metrics code
    This will reduce in the future the number of metrics protocol buffers 
    that we need to #include in various omnibox code files.  It also 
    introduces better boundaries between disparate sections of the codebase.
    
    NOTRY=true
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/20747002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/omnibox/omnibox_log.h
+++ chrome/browser/omnibox/omnibox_log.h
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_provider.h"
 #include "chrome/browser/sessions/session_id.h"
-#include "chrome/common/metrics/proto/omnibox_event.pb.h"
 
 class AutocompleteResult;
 
@@ -26,8 +25,7 @@ struct OmniboxLog {
       AutocompleteInput::Type input_type,
       size_t selected_index,
       SessionID::id_type tab_id,
-      metrics::OmniboxEventProto::PageClassification
-          current_page_classification,
+      AutocompleteInput::PageClassification current_page_classification,
       base::TimeDelta elapsed_time_since_user_first_modified_omnibox,
       size_t completed_length,
       base::TimeDelta elapsed_time_since_last_change_to_default_match,
@@ -53,7 +51,7 @@ struct OmniboxLog {
 
   // The type of page (e.g., new tab page, regular web page) that the
   // user was viewing before going somewhere with the omnibox.
-  metrics::OmniboxEventProto::PageClassification current_page_classification;
+  AutocompleteInput::PageClassification current_page_classification;
 
   // The amount of time since the user first began modifying the text
   // in the omnibox.  If at some point after modifying the text, the
