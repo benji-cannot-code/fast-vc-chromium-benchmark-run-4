7690d00ef59f8a5749c0ec1ee6761245333802d2
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-28 08:46:00 +0000

    Change ConvertToResourceEntry to take output parameter and return bool.
    BUG=243195
    
    Review URL: https://chromiumcodereview.appspot.com/20636005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214108 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/resource_entry_conversion_unittest.cc
+++ chrome/browser/chromeos/drive/resource_entry_conversion_unittest.cc
@@ -25,8 +25,8 @@ TEST(ResourceEntryConversionTest, ConvertToResourceEntry_File) {
       google_apis::ResourceEntry::ExtractAndParse(*value));
   ASSERT_TRUE(gdata_resource_entry.get());
 
-  ResourceEntry entry =
-      ConvertToResourceEntry(*gdata_resource_entry);
+  ResourceEntry entry;
+  EXPECT_TRUE(ConvertToResourceEntry(*gdata_resource_entry, &entry));
 
   EXPECT_EQ("File 1.mp3",  entry.title());
   EXPECT_EQ("File 1.mp3",  entry.base_name());
@@ -101,8 +101,8 @@ TEST(ResourceEntryConversionTest,
       google_apis::ResourceEntry::ExtractAndParse(*value));
   ASSERT_TRUE(gdata_resource_entry.get());
 
-  ResourceEntry entry =
-      ConvertToResourceEntry(*gdata_resource_entry);
+  ResourceEntry entry;
+  EXPECT_TRUE(ConvertToResourceEntry(*gdata_resource_entry, &entry));
 
   EXPECT_EQ("Document 1",  entry.title());
   EXPECT_EQ("Document 1.gdoc",  entry.base_name());  // The suffix added.
@@ -182,8 +182,8 @@ TEST(ResourceEntryConversionTest,
       google_apis::ResourceEntry::ExtractAndParse(*value));
   ASSERT_TRUE(gdata_resource_entry.get());
 
-  ResourceEntry entry =
-      ConvertToResourceEntry(*gdata_resource_entry);
+  ResourceEntry entry;
+  EXPECT_TRUE(ConvertToResourceEntry(*gdata_resource_entry, &entry));
 
   EXPECT_EQ("Sub Directory Folder",  entry.title());
   EXPECT_EQ("Sub Directory Folder",  entry.base_name());
@@ -253,8 +253,8 @@ TEST(ResourceEntryConversionTest,
       google_apis::ResourceEntry::ExtractAndParse(*value));
   ASSERT_TRUE(gdata_resource_entry.get());
 
-  ResourceEntry entry =
-      ConvertToResourceEntry(*gdata_resource_entry);
+  ResourceEntry entry;
+  EXPECT_TRUE(ConvertToResourceEntry(*gdata_resource_entry, &entry));
 
   EXPECT_EQ("Deleted document",  entry.title());
   EXPECT_EQ("Deleted document.gdoc",  entry.base_name());
@@ -331,8 +331,8 @@ TEST(ResourceEntryConversionTest,
       google_apis::ResourceEntry::ExtractAndParse(*value));
   ASSERT_TRUE(gdata_resource_entry.get());
 
-  ResourceEntry entry =
-      ConvertToResourceEntry(*gdata_resource_entry);
+  ResourceEntry entry;
+  EXPECT_TRUE(ConvertToResourceEntry(*gdata_resource_entry, &entry));
 
   EXPECT_TRUE(entry.shared_with_me());
 }
