f56c7322589119846afe890fd7a211690fed3c82
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-29 21:52:24 +0000

    Move service_client to host/setup, since it's only used to register hosts.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/19789010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214233 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/setup/host_starter.h
+++ remoting/host/setup/host_starter.h
@@ -12,8 +12,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "google_apis/gaia/gaia_oauth_client.h"
 #include "remoting/base/rsa_key_pair.h"
 #include "remoting/base/url_request_context.h"
-#include "remoting/host/service_client.h"
 #include "remoting/host/setup/daemon_controller.h"
+#include "remoting/host/setup/service_client.h"
 
 namespace remoting {
 
