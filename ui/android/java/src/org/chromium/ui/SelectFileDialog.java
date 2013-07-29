1daa3a0f81850d4a5d22938f43b455c4075b320d
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-29 22:13:38 +0000

    Add method to show the error by resId.
    This avoid caller to use WindowAndroid's context to get the string resource.
    
    BUG=233440
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/20824004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214241 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/android/java/src/org/chromium/ui/SelectFileDialog.java
+++ ui/android/java/src/org/chromium/ui/SelectFileDialog.java
@@ -179,8 +179,7 @@ class SelectFileDialog implements WindowAndroid.IntentCallback{
         }
         if (!success) {
             onFileNotSelected();
-            String openingFileError = window.getContext().getString(R.string.opening_file_error);
-            window.showError(openingFileError);
+            window.showError(R.string.opening_file_error);
         }
     }
 
