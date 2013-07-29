7e5e40d6779164b01de2b287eea93cac785a029d
rvargas@google.com <rvargas@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-29 22:13:30 +0000

    Disk cache: make sure that stats are preserved between runs.
    There are two issues: 
    - For new caches the address of the stats was not saved. 
    - For existing caches the size of the stats was not correct. 
    
    BUG=263160
    TEST=net_unittests
    TBR=gavinp@chromium.org
    
    This is the same change as https://codereview.chromium.org/20892002/, for landing.
    
    Review URL: https://codereview.chromium.org/21150009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214240 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/backend_impl.cc
+++ net/disk_cache/backend_impl.cc
@@ -1368,6 +1368,8 @@ bool BackendImpl::InitStats() {
 
     if (!CreateBlock(file_type, num_blocks, &address))
       return false;
+
+    data_->header.stats = address.value();
     return stats_.Init(NULL, 0, address);
   }
 
