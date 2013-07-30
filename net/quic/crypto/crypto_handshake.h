74c79f045432b1797e96ff009305e65220bada4a
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 17:16:14 +0000

    net: make QUIC ProofVerifier more generic (reland).
    r213862 caused a crash when QUIC was enabled because, if a certificate
    verification completely immmediately, the details scoped_ptr wouldn't be set.
    This ended up crashing on a NULL pointer later in the code.
    
    BUG=none
    R=rtenneti@chromium.org
    
    Review URL: https://codereview.chromium.org/21086003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214380 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/crypto/crypto_handshake.h
+++ net/quic/crypto/crypto_handshake.h
@@ -16,6 +16,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/cert/cert_verify_result.h"
 #include "net/cert/x509_certificate.h"
 #include "net/quic/crypto/crypto_protocol.h"
+#include "net/quic/crypto/proof_verifier.h"
 #include "net/quic/quic_protocol.h"
 
 namespace net {
@@ -282,10 +283,12 @@ class NET_EXPORT_PRIVATE QuicCryptoClientConfig : public QuicCryptoConfig {
     const std::string& signature() const;
     bool proof_valid() const;
     uint64 generation_counter() const;
-    const CertVerifyResult* cert_verify_result() const;
+    const ProofVerifyDetails* proof_verify_details() const;
 
     void set_source_address_token(base::StringPiece token);
-    void SetCertVerifyResult(const CertVerifyResult& cert_verify_result);
+
+    // SetProofVerifyDetails takes ownership of |details|.
+    void SetProofVerifyDetails(ProofVerifyDetails* details);
 
    private:
     std::string server_config_id_;      // An opaque id from the server.
@@ -302,10 +305,7 @@ class NET_EXPORT_PRIVATE QuicCryptoClientConfig : public QuicCryptoConfig {
     // server_config_valid_ to false.
     uint64 generation_counter_;
 
-    // The result of certificate verification.
-    // TODO(rtenneti): should we change CertVerifyResult to be
-    // RefCountedThreadSafe object to avoid copying.
-    CertVerifyResult cert_verify_result_;
+    scoped_ptr<ProofVerifyDetails> proof_verify_details_;
 
     // scfg contains the cached, parsed value of |server_config|.
     mutable scoped_ptr<CryptoHandshakeMessage> scfg_;
