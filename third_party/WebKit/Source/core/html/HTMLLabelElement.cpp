97f7818e37122420562b1817239eeaaad148db8d
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-30 13:33:11 +0000

    Order of actions for labeled control should be focus then click.
    When we click on a checkbox or a radio button, event order is:
     - mousedown
     - focus, triggered by mousedown
     - mouseup
     - change, triggered by the following click <wkbug.com/93674>
     - click
    
    That is to say, we focus on it first and then click it. Click on an
    associated label should have same behavior.
    
    BUG=264567
    
    Review URL: https://chromiumcodereview.appspot.com/21005002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155162 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLLabelElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLLabelElement.cpp
@@ -132,12 +132,12 @@ void HTMLLabelElement::defaultEventHandler(Event* evt)
 
         processingClick = true;
 
-        // Click the corresponding control.
-        element->dispatchSimulatedClick(evt);
-
         document()->updateLayoutIgnorePendingStylesheets();
         if (element->isMouseFocusable())
             element->focus();
+
+        // Click the corresponding control.
+        element->dispatchSimulatedClick(evt);
 
         processingClick = false;
 
