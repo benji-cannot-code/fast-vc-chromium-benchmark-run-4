324e4efaa4dad9eb4a01b350d983795b67d37326
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 22:54:10 +0000

    Do not try to send an unregister message.
    This message was added in r99263.
    
    Otherwise in debug build when you click say in "Import bookmarks now..." from
    ntp it crashes with the stack below:
    
    [13156:13156:0728/185301:FATAL:web_ui_impl.cc(220)] Check failed: false. Unhandled chrome.send("introMessageDismissed");
     [0x7f5e6bdd3dbe] base::debug::StackTrace::StackTrace()
             [0x7f5e6be27d9f] logging::LogMessage::~LogMessage()
             [0x7f5e634bf887] content::WebUIImpl::ProcessWebUIMessage()
             [0x7f5e634beaa6] content::WebUIImpl::OnWebUISend()
             [0x7f5e634c1ad7] DispatchToMethod<>()
             [0x7f5e634bfc0b] ViewHostMsg_WebUISend::Dispatch<>()
             [0x7f5e634be8c5] content::WebUIImpl::OnMessageReceived()
             [0x7f5e634792c3] content::WebContentsImpl::OnMessageReceived()
             [0x7f5e6347c637] content::WebContentsImpl::OnMessageReceived()
             [0x7f5e633912bf] content::RenderViewHostImpl::OnMessageReceived()
             [0x7f5e6339707f] content::RenderViewHostImpl::OnMessageReceived()
             [0x7f5e63374510] content::RenderProcessHostImpl::OnMessageReceived()
             [0x7f5e6337480f] content::RenderProcessHostImpl::OnMessageReceived()
             [0x7f5e6ae99536] IPC::ChannelProxy::Context::OnDispatchMessage()
             [0x7f5e6ae9f9fa] base::internal::RunnableAdapter<>::Run()
             [0x7f5e6ae9f951] base::internal::InvokeHelper<>::MakeItSo()
             [0x7f5e6ae9f8ec] base::internal::Invoker<>::Run()
             [0x7f5e6bdc58ce] base::Callback<>::Run()
             [0x7f5e6be37cac] base::MessageLoop::RunTask()
             [0x7f5e6be37fbb] base::MessageLoop::DeferOrRunPendingTask()
             [0x7f5e6be381e5] base::MessageLoop::DoWork()
             [0x7f5e6bda495c] base::MessagePumpGlib::HandleDispatch()
             [0x7f5e6bda4d91] base::(anonymous namespace)::WorkSourceDispatch()
             [0x7f5e5e7bbd53] g_main_context_dispatch
              [0x7f5e5e7bc0a0] <unknown>
               [0x7f5e5e7bc164] g_main_context_iteration
                [0x7f5e6bda4400] base::MessagePumpGlib::RunWithDispatcher()
             [0x7f5e6bda4a79] base::MessagePumpGlib::Run()
             [0x7f5e6be376a7] base::MessageLoop::RunInternal()
             [0x7f5e6be37555] base::MessageLoop::RunHandler()
             [0x7f5e6be83f62] base::RunLoop::Run()
             [0x7f5e6f475749] ChromeBrowserMainParts::MainMessageLoopRun()
             [0x7f5e62f2760f] content::BrowserMainLoop::RunMainMessageLoopParts()
             [0x7f5e62f2ec57] content::BrowserMainRunnerImpl::Run()
             [0x7f5e62f237e1] content::BrowserMain()
             [0x7f5e62ee89fa] content::RunNamedProcessTypeMain()
             [0x7f5e6eb94d32] main
              [0x7f5e5b0d576d] __libc_start_main
               [0x7f5e6eb94c39] <unknown>
    
               Trace/breakpoint trap (core dumped)
    
    R=dbeam@chromium.org
    BUG=
    
    Review URL: https://codereview.chromium.org/20983002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214456 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/ntp4/nav_dot.js
+++ chrome/browser/resources/ntp4/nav_dot.js
@@ -133,7 +133,6 @@ cr.define('ntp', function() {
       if (this.ownerDocument.activeElement != this.input_)
         this.focus();
 
-      chrome.send('introMessageDismissed');
       e.stopPropagation();
     },
 
