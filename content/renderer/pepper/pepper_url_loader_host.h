26ebda43d5d5e4863dcdd31aea72a6cb628cb214
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 18:25:26 +0000

    Revert 214384 "Pepper: Make DataFromWebURLResponse async."
    > Pepper: Make DataFromWebURLResponse async.
    > 
    > Creating "new" FileRef resources in the renderer is an asynchronous operation.
    > Since DataFromWebURLResponse creates an external FileRef, that operation must
    > be asynchronous as well.
    > 
    > This requires that we add a small state machine to the PepperURLLoaderHost to
    > ensure that PpapiPluginMsg_URLLoader_ReceivedResponse is always sent to the
    > plugin before PpapiPluginMsg_URLLoader_FinishedLoading.
    > 
    > I've split this from the major FileRef refactor change in the interest of
    > making smaller patches (that are hopefully easier to review).
    > 
    > BUG=225441
    > R=bbudge@chromium.org, dmichael@chromium.org, yzshen@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/20122004
    
    TBR=teravest@chromium.org
    
    Review URL: https://codereview.chromium.org/21279002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214396 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_url_loader_host.h
+++ content/renderer/pepper/pepper_url_loader_host.h
@@ -8,8 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <vector>
 
-#include "base/memory/scoped_vector.h"
-#include "base/memory/weak_ptr.h"
 #include "content/common/content_export.h"
 #include "ppapi/host/resource_host.h"
 #include "ppapi/proxy/resource_message_params.h"
@@ -79,19 +77,12 @@ class PepperURLLoaderHost
       ppapi::host::HostMessageContext* context);
 
   // Sends or queues an unsolicited message to the plugin resource. This
-  // handles cases where messages must be reordered for the plugin and
-  // the case where we have created a pending host resource and the
-  // plugin has not connected to us yet.
+  // handles the case where we have created a pending host resource and the
+  // plugin has not connected to us yet. Such messages will be queued until the
+  // plugin resource connects.
   //
   // Takes ownership of the given pointer.
   void SendUpdateToPlugin(IPC::Message* msg);
-
-  // Sends or queues an unsolicited message to the plugin resource. This is
-  // used inside SendUpdateToPlugin for messages that are already ordered
-  // properly.
-  //
-  // Takes ownership of the given pointer.
-  void SendOrderedUpdateToPlugin(IPC::Message* msg);
 
   void Close();
 
@@ -104,7 +95,6 @@ class PepperURLLoaderHost
 
   // Converts a WebURLResponse to a URLResponseInfo and saves it.
   void SaveResponse(const WebKit::WebURLResponse& response);
-  void DidDataFromWebURLResponse(const ppapi::URLResponseInfoData& data);
 
   // Sends the UpdateProgress message (if necessary) to the plugin.
   void UpdateProgress();
@@ -139,15 +129,7 @@ class PepperURLLoaderHost
   // Messages sent while the resource host is pending. These will be forwarded
   // to the plugin when the plugin side connects. The pointers are owned by
   // this object and must be deleted.
-  ScopedVector<IPC::Message> pending_replies_;
-  ScopedVector<IPC::Message> out_of_order_replies_;
-
-  // True when there's a pending DataFromURLResponse call which will send a
-  // PpapiPluginMsg_URLLoader_ReceivedResponse to the plugin, which introduces
-  // ordering constraints on following messages to the plugin.
-  bool pending_response_;
-
-  base::WeakPtrFactory<PepperURLLoaderHost> weak_factory_;
+  std::vector<IPC::Message*> pending_replies_;
 
   DISALLOW_COPY_AND_ASSIGN(PepperURLLoaderHost);
 };
