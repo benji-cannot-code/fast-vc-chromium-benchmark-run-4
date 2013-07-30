61d827c3cd20947c2b3f20bf30028d4ae62e231c
benjhayden@chromium.org <benjhayden@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 15:20:46 +0000

    Modern Download Manager browser action extension
    Some of the features depend on https://codereview.chromium.org/16924017/
    After that CL and this are submitted, I'll upload this to the webstore and start getting the By Google tag.
    
    If you have a canary/dev/beta channel chrome installed or have a binary built from ToT and you're on the google network, go to
    http://www.corp.google.com/~benjhayden/
    right-click "download_manager.crx", Save Link As, open chrome://extensions and drag download_manager.crx from the shelf and drop it on chrome://extensions to install it.
    If you install it on the stable channel, it will invite you to install the beta channel.
    
    R=asanka@chromium.org, asargent@chromium.org, mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/17286017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214356 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/common/extensions/docs/examples/api/downloads/download_open/background.js
@@ -0,0 +1,49 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright (c) 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+function getOpeningIds() {
+  var ids = [];
+  try {
+    ids = JSON.parse(localStorage.openWhenComplete);
+  } catch (e) {
+    localStorage.openWhenComplete = JSON.stringify(ids);
+  }
+  return ids;
+}
+
+function setOpeningIds(ids) {
+  localStorage.openWhenComplete = JSON.stringify(ids);
+}
+
+chrome.downloads.onChanged.addListener(function(delta) {
+  if (!delta.state ||
+      (delta.state.current != 'complete')) {
+    return;
+  }
+  var ids = getOpeningIds();
+  if (ids.indexOf(delta.id) < 0) {
+    return;
+  }
+  chrome.downloads.open(delta.id);
+  ids.splice(ids.indexOf(delta.id), 1);
+  setOpeningIds(ids);
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  chrome.downloads.download({url: info.linkUrl}, function(downloadId) {
+    var ids = getOpeningIds();
+    if (ids.indexOf(downloadId) >= 0) {
+      return;
+    }
+    ids.push(downloadId);
+    setOpeningIds(ids);
+  });
+});
+
+chrome.contextMenus.create({
+  id: 'open',
+  title: chrome.i18n.getMessage('openContextMenuTitle'),
+  contexts: ['link'],
+});
