eb341560f6d0cba86fb04c6e092f5c56947d7c5f
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 19:26:40 +0000

    base: Re-re-apply WeakPtr support for SequencedWorkerPools
    This is a continuation of https://codereview.chromium.org/20163004/#ps55001 and identical to Patchset 4. I'm adding back some death test threadsafe flags that got dropped from the re-apply.
    
    In other words, this CL is:
    https://codereview.chromium.org/20163004/#ps55001 +
    https://codereview.chromium.org/19617002/
    
    Feel free to revert if it causes issues again.
    
    TBR=akalin,darin,dewittj,rnk
    
    BUG=165590
    
    Review URL: https://codereview.chromium.org/20754006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214411 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/audio_capturer_win.h
+++ remoting/host/audio_capturer_win.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
+#include "base/threading/thread_checker.h"
 #include "base/timer/timer.h"
 #include "base/win/scoped_co_mem.h"
 #include "base/win/scoped_comptr.h"
