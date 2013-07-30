bf965f596feebb6afd44f86174ceb42f2963465f
ppi@chromium.org <ppi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-30 16:49:57 +0000

    Android: distinguish between crashes and out-of-memory kills in content
    The process management logic in the content layer adjusts the renderer
    out-of-memory priority allowing the OS to kill inactive renderers when under
    memory pressure.
    
    Separately, content layer is also responsible for notyfing the ContentView
    clients about death of the renderer process that backs the ContentView, while
    the death can be caused either by the out-of-memory kill or an actual process
    crash.
    
    It would be useful to notify the clients not only that the process died, but
    also about the out-of-memory priority of the process upon its death - otherwise
    the clients have to employ heuristics to distinguish between crashes and
    out-of-memory kills which inevitably leads to tricky corner-cases.
    
    This patch makes ChildProcessLauncher an authoritative agent that keeps track
    of the bindings that raise the renderer process priority in order to protect it
    from out-of-memory killing.
    
    ChildProcessConnections use callbacks to notify ChildProcessLauncher each time
    a high priority connection is bound or unbound.
    
    Delegating this logic to ChildProcessLauncher allows to escape threading
    problems, as ChildProcessConnection might be notified about process
    disconnection (and hence closed) before as well as after the content layer
    detects a missing renderer and the ContentView client is notified.
    
    BUG=263047
    R=joth@chromium.org, yfriedman@chromium.org
    
    Review URL: https://codereview.chromium.org/19969003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214366 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -332,6 +332,9 @@ import java.util.Map;
 
     private boolean mAttachedToWindow = false;
 
+    // Pid of the renderer process backing this ContentViewCore.
+    private int mPid = 0;
+
     private ContentViewGestureHandler mContentViewGestureHandler;
     private PinchGestureStateListener mPinchGestureStateListener;
     private UpdateFrameInfoListener mUpdateFrameInfoListener;
@@ -701,6 +704,8 @@ import java.util.Map;
                 resetGestureDetectors();
             }
         };
+
+        mPid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
     }
 
     @CalledByNative
@@ -1413,13 +1418,13 @@ import java.util.Map;
     public void onAttachedToWindow() {
         mAttachedToWindow = true;
         if (mNativeContentViewCore != 0) {
-            int pid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
-            ChildProcessLauncher.bindAsHighPriority(pid);
-            // Normally the initial binding is removed in onRenderProcessSwap(), but it is
-            // possible to construct WebContents and spawn the renderer before passing it to
-            // ContentViewCore. In this case there will be no onRendererSwap() call and the
-            // initial binding will be removed here.
-            ChildProcessLauncher.removeInitialBinding(pid);
+            assert mPid == nativeGetCurrentRenderProcessId(mNativeContentViewCore);
+            ChildProcessLauncher.bindAsHighPriority(mPid);
+            // Normally the initial binding is removed in onRenderProcessSwap(), but it is possible
+            // to construct WebContents and spawn the renderer before passing it to ContentViewCore.
+            // In this case there will be no onRenderProcessSwap() call and the initial binding will
+            // be removed here.
+            ChildProcessLauncher.removeInitialBinding(mPid);
         }
         setAccessibilityState(mAccessibilityManager.isEnabled());
     }
@@ -1431,8 +1436,8 @@ import java.util.Map;
     public void onDetachedFromWindow() {
         mAttachedToWindow = false;
         if (mNativeContentViewCore != 0) {
-            int pid = nativeGetCurrentRenderProcessId(mNativeContentViewCore);
-            ChildProcessLauncher.unbindAsHighPriority(pid);
+            assert mPid == nativeGetCurrentRenderProcessId(mNativeContentViewCore);
+            ChildProcessLauncher.unbindAsHighPriority(mPid);
         }
         setInjectedAccessibility(false);
         hidePopupDialog();
@@ -1444,9 +1449,9 @@ import java.util.Map;
      * @see View#onVisibilityChanged(android.view.View, int)
      */
     public void onVisibilityChanged(View changedView, int visibility) {
-      if (visibility != View.VISIBLE) {
-          mZoomControlsDelegate.dismissZoomPicker();
-      }
+        if (visibility != View.VISIBLE) {
+            mZoomControlsDelegate.dismissZoomPicker();
+        }
     }
 
     /**
@@ -1814,6 +1819,11 @@ import java.util.Map;
     @SuppressWarnings("unused")
     @CalledByNative
     private void onTabCrash() {
+        assert mPid != 0;
+        getContentViewClient().onRendererCrash(ChildProcessLauncher.isOomProtected(mPid));
+        mPid = 0;
+
+        // Legacy method, to be dropped once all clients switch to onRendererCrash().
         getContentViewClient().onTabCrash();
     }
 
@@ -2389,6 +2399,7 @@ import java.util.Map;
     @SuppressWarnings("unused")
     @CalledByNative
     private void onRenderProcessSwap(int oldPid, int newPid) {
+        assert mPid == oldPid || mPid == newPid;
         if (mAttachedToWindow && oldPid != newPid) {
             ChildProcessLauncher.unbindAsHighPriority(oldPid);
             ChildProcessLauncher.bindAsHighPriority(newPid);
@@ -2397,6 +2408,7 @@ import java.util.Map;
         // We want to remove the initial binding even if the ContentView is not attached, so that
         // renderers for ContentViews loading in background do not retain the high priority.
         ChildProcessLauncher.removeInitialBinding(newPid);
+        mPid = newPid;
     }
 
     @SuppressWarnings("unused")
