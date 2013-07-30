657540f56147f458a681cff681832ad30b3750c4
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-30 05:19:48 +0000

    Add ContructorCallWith=Document to remove extra ::create methods
    Several DOM objects have an extra ::create(ScriptExecutionContext*) static method which
    internally calls toDocument() on the context. These can be removed by making the bindings
    generator smart enough to deal with the casting for you.
    
    Review URL: https://chromiumcodereview.appspot.com/21150011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155139 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/DocumentFragment.h
+++ third_party/WebKit/Source/core/dom/DocumentFragment.h
@@ -35,7 +35,6 @@ class ScriptExecutionContext;
 class DocumentFragment : public ContainerNode {
 public:
     static PassRefPtr<DocumentFragment> create(Document*);
-    static PassRefPtr<DocumentFragment> create(ScriptExecutionContext*);
 
     void parseHTML(const String&, Element* contextElement, ParserContentPolicy = AllowScriptingContent);
     bool parseXML(const String&, Element* contextElement, ParserContentPolicy = AllowScriptingContent);
