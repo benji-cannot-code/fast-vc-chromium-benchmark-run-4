868030d9d4945f070ceefd30744cfea5f41ddaf2
mtomasz@chromium.org <mtomasz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 02:57:29 +0000

    Fix flakyness in Files.app's test when commands are not attached to the document's body.
    The tests used to fail in the Debug build, since after closing the popup, focus was set to the body, instead of the previous focused element. This patch resolves this issue by locking the focus to the main element and also in tests, by waiting until the popup is removed from dom (and so the focus is set back to the main element).
    This change simplified the code, since there is no need to call refocus() manually anymore. This patch fixes some flakyness in KeyboardOperations/FileManagerBrowserTest.
    
    TEST=browser_test:*KeyboardOperations/FileManagerBrowserTest*
    BUG=252561
    R=yoshiki@chromium.org
    
    Review URL: https://codereview.chromium.org/21212003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214523 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/file_manager.js
+++ chrome/browser/resources/file_manager/js/file_manager.js
@@ -393,6 +393,11 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
         driveConnectionChangedHandler);
     driveConnectionChangedHandler();
 
+    // Set the initial focus and set it as a fallback.
+    this.document_.addEventListener('focusout', function(e) {
+      if (!e.relatedTarget)
+        this.refocus();
+    }.bind(this));
     this.refocus();
 
     this.initDataTransferOperations_();
@@ -2080,7 +2085,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
         this.filePopupCloseCallback_();
         this.filePopupCloseCallback_ = null;
       }
-      this.refocus();
 
       // These operations have to be in the end, otherwise v8 crashes on an
       // assert. See: crbug.com/224174.
@@ -2613,7 +2617,6 @@ var BOTTOM_MARGIN_FOR_PREVIEW_PANEL_PX = 52;
       parent.removeAttribute('renaming');
       parent.removeChild(this.renameInput_);
     }
-    this.refocus();
   };
 
   /**
