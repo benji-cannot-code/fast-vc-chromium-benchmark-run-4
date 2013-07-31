7df68e6f72fa7728bb3922fb9285acb14b8bcb20
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 01:26:56 +0000

    gn: Fixes some errors found while compiling with clang.
    ../../tools/gn/command_desc.cc:106:12: error: implicit conversion of NULL constant to 'int' [-Werror,-Wnull-conversion]
        return NULL;
        ~~~~~~ ^~~~
               0
    ../../tools/gn/command_desc.cc:161:12: error: implicit conversion of NULL constant to 'int' [-Werror,-Wnull-conversion]
        return NULL;
        ~~~~~~ ^~~~
               0
    ../../tools/gn/command_desc.cc:190:12: error: implicit conversion of NULL constant to 'int' [-Werror,-Wnull-conversion]
        return NULL;
        ~~~~~~ ^~~~
               0
    
    TBR=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/21312002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214496 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/command_desc.cc
+++ tools/gn/command_desc.cc
@@ -104,7 +104,7 @@ int RunDescCommand(const std::vector<std::string>& args) {
   if (args.size() != 1) {
     Err(Location(), "You're holding it wrong.",
         "Usage: \"gn desc <target_name>\"").PrintToStdout();
-    return NULL;
+    return 1;
   }
 
   const Target* target = GetTargetForDesc(args);
@@ -159,7 +159,7 @@ int RunDepsCommand(const std::vector<std::string>& args) {
   if (args.size() != 1) {
     Err(Location(), "You're holding it wrong.",
         "Usage: \"gn deps <target_name>\"").PrintToStdout();
-    return NULL;
+    return 1;
   }
 
   const Target* target = GetTargetForDesc(args);
@@ -188,7 +188,7 @@ int RunTreeCommand(const std::vector<std::string>& args) {
   if (args.size() != 1) {
     Err(Location(), "You're holding it wrong.",
         "Usage: \"gn tree <target_name>\"").PrintToStdout();
-    return NULL;
+    return 1;
   }
 
   const Target* target = GetTargetForDesc(args);
