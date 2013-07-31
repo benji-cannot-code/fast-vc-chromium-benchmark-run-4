0473acda104529fefb96c45ef1c94355e28c5dda
fgorski@chromium.org <fgorski@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 06:35:27 +0000

    Adding operation to remove a specific token from TokenWebData DB
    BUG=263943
    TBR=joi
    
    Review URL: https://chromiumcodereview.appspot.com/20568002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214590 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/webdata/token_web_data.h
+++ chrome/browser/webdata/token_web_data.h
@@ -49,6 +49,9 @@ class TokenWebData : public WebDataServiceBase {
   // Remove all tokens stored in the web database.
   void RemoveAllTokens();
 
+  // Removes a token related to |service| from the web database.
+  void RemoveTokenForService(const std::string& service);
+
   // Null on failure. Success is WDResult<std::vector<std::string> >
   virtual Handle GetAllTokens(WebDataServiceConsumer* consumer);
 
