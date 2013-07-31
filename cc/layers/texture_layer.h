e41271207e6c7826d6bce9f2b79c2ad5212cd5c6
alokp@chromium.org <alokp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 12:35:43 +0000

    Consider background-color opacity for TextureLayer opaque region.
    TextureLayer draws both - texture and background-color.
    So take background-color opacity into account when calculating
    opaque region.
    
    BUG=236982
    
    Review URL: https://chromiumcodereview.appspot.com/21084005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214682 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/texture_layer.h
+++ cc/layers/texture_layer.h
@@ -83,6 +83,7 @@ class CC_EXPORT TextureLayer : public Layer {
   virtual bool Update(ResourceUpdateQueue* queue,
                       const OcclusionTracker* occlusion) OVERRIDE;
   virtual void PushPropertiesTo(LayerImpl* layer) OVERRIDE;
+  virtual Region VisibleContentOpaqueRegion() const OVERRIDE;
   virtual bool BlocksPendingCommit() const OVERRIDE;
 
   virtual bool CanClipSelf() const OVERRIDE;
