7d484ed081ab8f18749c0fc755e05967a50a7e3b
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-31 08:00:53 +0000

    [HTML Imports] Make import loading in order.
    This change makes script loading in order from script viewpoint.
    That means, all imports and scripts inside them are now loaded and executed
    bofore following script.
    
    Similar facily has been partially implemented, where there is a guarantee that
    all subimports are loaded before following script execution in the parent
    import or the master document. This change extends it to support better ordering.
    
    For example, let's think about following import dependency:
    
    master - i0 - i1 - s0
           - i2 - i3 - s1
    
    where ix is an import and sx is a script.
    In this example, master imports i0 and i2, i0 imports i1, and i1 includes s0, etc.
    This change guarantees that s0 is executed before s1.
    
    For this purpose, documents need to block the parser.
    The blocking criteria is returned from HTMLImport::isBlocked().
    (It was HTMLImport::haveChildrenLoaded() before.)
    And this change is all about computing this "blocked" bit on each import.
    
    There are three notable changes:
    
    - Now the "blocked" bit is stored as HTMLImport::m_import flag. Originally it was
      computed for each query. It becomes too complex and expensive to do on demand.
      The flag is updated if a) new imports are requested via <link>, or b) an imported
      document is completely parsed. (a) turns on flags since such new imports can
      block existing import loading, then (b) is done for possibly unblocking them.
    
    - The update phase is now done in a timer callback instead of doing it immediately
      in notification from parsers and resources. This separation eliminates
      hidden callback reentrancy and deep callstack, both of which are hard to debug.
      See HTMLImportsController::unblockTimerFired() for more detail.
    
    - HTMLImports got m_children list to represent dependency tree.
      This list is used to traverse the dependnecy tree for updating the blocking flag.
    
    BUG=240592
    TEST=htmlimpots/import-blocking-*.html
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/21182004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155228 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImport.h
+++ third_party/WebKit/Source/core/html/HTMLImport.h
@@ -32,6 +32,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef HTMLImport_h
 #define HTMLImport_h
 
+#include "wtf/Vector.h"
+
 namespace WebCore {
 
 class Frame;
@@ -41,17 +43,43 @@ class HTMLImportsController;
 
 class HTMLImport {
 public:
+    static bool unblock(HTMLImport*);
+
     virtual ~HTMLImport() { }
 
-    bool haveChildrenLoaded();
     Frame* frame();
     Document* master();
 
+    bool isLoaded() const { return !isBlocked() && !isProcessing(); }
+    bool isBlocked() const { return m_blocked; }
+    void appendChild(HTMLImport*);
+
     virtual HTMLImportsController* controller() = 0;
-    virtual HTMLImport* parent() = 0;
-    virtual Document* document() = 0;
+    virtual HTMLImport* parent() const = 0;
+    virtual Document* document() const = 0;
     virtual void wasDetachedFromDocument() = 0;
     virtual void didFinishParsing() = 0;
+    virtual bool isProcessing() const = 0;
+
+protected:
+    HTMLImport()
+        : m_blocked(false)
+    { }
+
+private:
+    static void block(HTMLImport*);
+
+    void blockAfter(HTMLImport* child);
+    void block();
+    void unblock();
+    void didUnblock();
+
+    bool arePredecessorsLoaded() const;
+    bool areChilrenLoaded() const;
+    bool hasChildren() const { return !m_children.isEmpty(); }
+
+    Vector<HTMLImport*> m_children;
+    bool m_blocked; // If any of decendants or predecessors is in processing, it is blocked.
 };
 
 } // namespace WebCore
