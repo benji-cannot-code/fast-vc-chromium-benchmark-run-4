8fe677fae7bf25a73f8927d97e13e892ed7ef3e1
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 13:41:42 +0000

    Fix import crash when StartImportSettings is called before TemplateURLService is loaded.
    The SourceProfile has been passed around by pointer ever since http://crrev.com/48928 (then known as ProfileInfo); perhaps callers have changed since then, but it is now the case that  callers pass a SourceProfile allocated on the stack to this method. So if the TemplateURLService is not ready, the stack unwinds while the import is waiting. When the notification comes in, the ExternalProcessImporterHost is holding a pointer to a freed SourceProfile...
    
    Simply copy the SourceProfile which is a simple data holder, this is better and much safer than trying to keep a pointer around assuming the caller will keep it around for us...
    
    BUG=264666
    TEST=I can't reproduce the crash in my debug build (something else initializes TemplateURLService early on profile creation...) but I can repro on 30.0.1581.2 canary, so I'll try again on Canary after this lands...
    
    Review URL: https://chromiumcodereview.appspot.com/21133008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/importer/external_process_importer_host.cc
+++ chrome/browser/importer/external_process_importer_host.cc
@@ -31,7 +31,6 @@ ExternalProcessImporterHost::ExternalProcessImporterHost()
       installed_bookmark_observer_(false),
       is_source_readable_(true),
       client_(NULL),
-      source_profile_(NULL),
       items_(0),
       cancelled_(false),
       import_process_launched_(false) {
@@ -55,7 +54,7 @@ void ExternalProcessImporterHost::StartImportSettings(
 
   profile_ = target_profile;
   writer_ = writer;
-  source_profile_ = &source_profile;
+  source_profile_ = source_profile;
   items_ = items;
 
   if (!CheckForFirefoxLock(source_profile)) {
@@ -113,7 +112,7 @@ void ExternalProcessImporterHost::LaunchImportIfReady() {
   InProcessImporterBridge* bridge =
       new InProcessImporterBridge(writer_.get(),
                                   weak_ptr_factory_.GetWeakPtr());
-  client_ = new ExternalProcessImporterClient(this, *source_profile_, items_,
+  client_ = new ExternalProcessImporterClient(this, source_profile_, items_,
                                               bridge);
   import_process_launched_ = true;
   client_->Start();
