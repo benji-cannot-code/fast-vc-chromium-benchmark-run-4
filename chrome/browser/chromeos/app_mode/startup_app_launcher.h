1441d03052445b855c79eb0584cef46f45a8e666
fgorski@chromium.org <fgorski@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 08:08:53 +0000

    Removing TokenService notifications from StartupAppLauncher
    BUG=258107
    
    Review URL: https://chromiumcodereview.appspot.com/20815002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214628 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/app_mode/startup_app_launcher.h
+++ chrome/browser/chromeos/app_mode/startup_app_launcher.h
@@ -13,8 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "base/timer/timer.h"
 #include "chrome/browser/chromeos/app_mode/kiosk_app_launch_error.h"
-#include "content/public/browser/notification_observer.h"
-#include "content/public/browser/notification_registrar.h"
+#include "chrome/browser/signin/oauth2_token_service.h"
 #include "net/base/network_change_notifier.h"
 #include "ui/base/events/event_handler.h"
 
@@ -37,7 +36,7 @@ namespace chromeos {
 // If anything goes wrong, it exits app mode and goes back to login screen.
 class StartupAppLauncher
     : public base::SupportsWeakPtr<StartupAppLauncher>,
-      public content::NotificationObserver,
+      public OAuth2TokenService::Observer,
       public net::NetworkChangeNotifier::NetworkChangeObserver,
       public ui::EventHandler {
  public:
@@ -76,10 +75,9 @@ class StartupAppLauncher
   static void LoadOAuthFileOnBlockingPool(KioskOAuthParams* auth_params);
   void OnOAuthFileLoaded(KioskOAuthParams* auth_params);
 
-  // content::NotificationObserver overrides.
-  virtual void Observe(int type,
-                       const content::NotificationSource& source,
-                       const content::NotificationDetails& details) OVERRIDE;
+  // OAuth2TokenService::Observer overrides.
+  virtual void OnRefreshTokenAvailable(const std::string& account_id) OVERRIDE;
+  virtual void OnRefreshTokensLoaded() OVERRIDE;
 
   // net::NetworkChangeNotifier::NetworkChangeObserver overrides:
   virtual void OnNetworkChanged(
@@ -94,7 +92,6 @@ class StartupAppLauncher
   int64 launch_splash_start_time_;
 
   scoped_refptr<extensions::WebstoreStandaloneInstaller> installer_;
-  content::NotificationRegistrar registrar_;
   base::OneShotTimer<StartupAppLauncher> network_wait_timer_;
   KioskOAuthParams auth_params_;
 
