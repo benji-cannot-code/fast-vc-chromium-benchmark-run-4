db3b9a776685e20f9b468a8f0e52c069c3cb4d48
agl@chromium.org <agl@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 21:57:28 +0000

    Perform online revocation checks when EV certificates aren't covered by a fresh CRLSet.
    Previously a fresh CRLSet was sufficient to suppress online revocation checking
    for EV certificates because we aimed to have full EV coverage in the CRLSet.
    
    With this change, we'll only suppress online revocation checking for EV
    certificates when a fresh CRLSet actually covers the chain in question. We
    determine coverage by seeing if the CRLSet contains the issuer SPKI.
    
    There are no changes to the OS X certificate code as I believe that OS X
    already does online revocation checking for EV certs no matter what we do in
    Chrome.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/11260018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214825 0039d316-1c4b-4281-b951-d872f2087c98

--- net/test/spawned_test_server/base_test_server.cc
+++ net/test/spawned_test_server/base_test_server.cc
@@ -57,6 +57,7 @@ void GetCiphersList(int cipher, base::ListValue* values) {
 BaseTestServer::SSLOptions::SSLOptions()
     : server_certificate(CERT_OK),
       ocsp_status(OCSP_OK),
+      cert_serial(0),
       request_client_certificate(false),
       bulk_ciphers(SSLOptions::BULK_CIPHER_ANY),
       record_resume(false),
@@ -65,6 +66,8 @@ BaseTestServer::SSLOptions::SSLOptions()
 BaseTestServer::SSLOptions::SSLOptions(
     BaseTestServer::SSLOptions::ServerCertificate cert)
     : server_certificate(cert),
+      ocsp_status(OCSP_OK),
+      cert_serial(0),
       request_client_certificate(false),
       bulk_ciphers(SSLOptions::BULK_CIPHER_ANY),
       record_resume(false),
@@ -375,6 +378,10 @@ bool BaseTestServer::GenerateArguments(base::DictionaryValue* arguments) const {
     std::string ocsp_arg = ssl_options_.GetOCSPArgument();
     if (!ocsp_arg.empty())
       arguments->SetString("ocsp", ocsp_arg);
+
+    if (ssl_options_.cert_serial != 0)
+      arguments->Set("cert-serial",
+                     base::Value::CreateIntegerValue(ssl_options_.cert_serial));
 
     // Check bulk cipher argument.
     scoped_ptr<base::ListValue> bulk_cipher_values(new base::ListValue());
