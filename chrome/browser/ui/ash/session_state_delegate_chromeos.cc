b82ff32e74c27882d9a6e0f41c0ba7fb5c52f16f
dzhioev@chromium.org <dzhioev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 09:56:26 +0000

    Not block user session when login manager not enabled.
    BUG=264662
    
    Review URL: https://chromiumcodereview.appspot.com/21014005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214660 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/ash/session_state_delegate_chromeos.cc
+++ chrome/browser/ui/ash/session_state_delegate_chromeos.cc
@@ -6,11 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/ash/session_state_delegate_chromeos.h"
 
 #include "ash/session_state_observer.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/login/user.h"
 #include "chrome/browser/chromeos/login/user_adding_screen.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
+#include "chromeos/chromeos_switches.h"
 #include "chromeos/dbus/dbus_thread_manager.h"
 #include "chromeos/dbus/session_manager_client.h"
 
@@ -57,7 +59,10 @@ void SessionStateDelegateChromeos::UnlockScreen() {
 }
 
 bool SessionStateDelegateChromeos::IsUserSessionBlocked() const {
-  return !IsActiveUserSessionStarted() || IsScreenLocked() ||
+  bool has_login_manager = CommandLine::ForCurrentProcess()->HasSwitch(
+          chromeos::switches::kLoginManager);
+  return (has_login_manager && !IsActiveUserSessionStarted()) ||
+         IsScreenLocked() ||
          chromeos::UserAddingScreen::Get()->IsRunning();
 }
 
