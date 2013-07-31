9e4b0402cf255d50e7523f2d347758f4fdd608d1
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 15:11:06 +0000

    Adds unittests for RenderText (common baseline, etc.). Also applies the same fix to Mac OS X as http://crrev.com/19734003
    Adds unittests which check that RenderText respects FontList's metrics when it comes to font height and/or baseline.
    Also see http://crrev.com/19734003
    
    BUG=244323
    
    Review URL: https://chromiumcodereview.appspot.com/20287002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214744 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gfx/render_text_mac.cc
+++ ui/gfx/render_text_mac.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <ApplicationServices/ApplicationServices.h>
 
+#include <algorithm>
 #include <cmath>
 #include <utility>
 
@@ -141,6 +142,15 @@ void RenderTextMac::EnsureLayout() {
   CGFloat leading = 0;
   // TODO(asvitkine): Consider using CTLineGetBoundsWithOptions() on 10.8+.
   double width = CTLineGetTypographicBounds(line_, &ascent, &descent, &leading);
+  // Ensure ascent and descent are not smaller than ones of the font list.
+  // Keep them tall enough to draw often-used characters.
+  // For example, if a text field contains a Japanese character, which is
+  // smaller than Latin ones, and then later a Latin one is inserted, this
+  // ensures that the text baseline does not shift.
+  CGFloat font_list_height = font_list().GetHeight();
+  CGFloat font_list_baseline = font_list().GetBaseline();
+  ascent = std::max(ascent, font_list_baseline);
+  descent = std::max(descent, font_list_height - font_list_baseline);
   string_size_ = Size(width, ascent + descent + leading);
   common_baseline_ = ascent;
 }
