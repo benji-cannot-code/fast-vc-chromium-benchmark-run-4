284818185d6f57e077c8feb6742956c178ade4c3
olaru@adobe.com <olaru@adobe.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 08:09:35 +0000

    Fix Chromium crashing on early OS X 10.7 systems, when accessing NSWindowDidChangeBackingPropertiesNotification.
    This issue was originally fixed for OS X 10.6 versions, but still crashes on 10.7. According to comments in NSWindow.h and AppKit release notes, the NSWindowDidChangeBackingPropertiesNotification is available starting with OS X 10.7.3. Because of this, the same crash occurs on 10.7-10.7.2.
    
    R=thakis@chromium.org
    BUG=260595
    
    Review URL: https://chromiumcodereview.appspot.com/21088004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214629 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_widget_host_view_mac.mm
+++ content/browser/renderer_host/render_widget_host_view_mac.mm
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "content/browser/renderer_host/render_widget_host_view_mac.h"
 
+#import <objc/runtime.h>
 #include <QuartzCore/QuartzCore.h>
 
 #include "base/bind.h"
@@ -136,6 +137,29 @@ static inline int ToWebKitModifiers(NSUInteger flags) {
   if (flags & NSAlternateKeyMask) modifiers |= WebInputEvent::AltKey;
   if (flags & NSCommandKeyMask) modifiers |= WebInputEvent::MetaKey;
   return modifiers;
+}
+
+// This method will return YES for OS X versions 10.7.3 and later, and NO
+// otherwise.
+// Used to prevent a crash when building with the 10.7 SDK and accessing the
+// notification below. See: http://crbug.com/260595.
+static BOOL SupportsBackingPropertiesChangedNotification() {
+  // windowDidChangeBackingProperties: method has been added to the
+  // NSWindowDelegate protocol in 10.7.3, at the same time as the
+  // NSWindowDidChangeBackingPropertiesNotification notification was added.
+  // If the protocol contains this method description, the notification should
+  // be supported as well.
+  Protocol* windowDelegateProtocol = NSProtocolFromString(@"NSWindowDelegate");
+  struct objc_method_description methodDescription =
+      protocol_getMethodDescription(
+          windowDelegateProtocol,
+          @selector(windowDidChangeBackingProperties:),
+          NO,
+          YES);
+
+  // If the protocol does not contain the method, the returned method
+  // description is {NULL, NULL}
+  return methodDescription.name != NULL || methodDescription.types != NULL;
 }
 
 static float ScaleFactor(NSView* view) {
@@ -2453,7 +2477,8 @@ void RenderWidgetHostViewMac::FrameSwapped() {
 
   // Backing property notifications crash on 10.6 when building with the 10.7
   // SDK, see http://crbug.com/260595.
-  BOOL supportsBackingPropertiesNotification = base::mac::IsOSLionOrLater();
+  static BOOL supportsBackingPropertiesNotification =
+      SupportsBackingPropertiesChangedNotification();
 
   if (oldWindow) {
     if (supportsBackingPropertiesNotification) {
