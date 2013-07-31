54c5a97468105c274c035527661e297256e80efa
rosca@adobe.com <rosca@adobe.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 04:01:58 +0000

    Crash in HttpServer::DidClose
    This happens because DidClose is called twice for the same net::StreamListenSocket. The second time, the HttpConnection is not found and the browser process crashes.
    
    
    BUG=263730
    
    Review URL: https://chromiumcodereview.appspot.com/20086002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214551 0039d316-1c4b-4281-b951-d872f2087c98

--- net/socket/stream_listen_socket.cc
+++ net/socket/stream_listen_socket.cc
@@ -237,6 +237,12 @@ void StreamListenSocket::OnObjectSignaled(HANDLE object) {
     return;
   }
 
+  if (ev.lNetworkEvents & FD_CLOSE) {
+    Close();
+    // Close might have deleted this object. We should return immediately.
+    return;
+  }
+
   // The object was reset by WSAEnumNetworkEvents.  Watch for the next signal.
   watcher_.StartWatching(object, this);
 
@@ -253,10 +259,9 @@ void StreamListenSocket::OnObjectSignaled(HANDLE object) {
       has_pending_reads_ = true;
     } else {
       Read();
+      // Read() might call Close() internally and 'this' can be invalid here
+      return;
     }
-  }
-  if (ev.lNetworkEvents & FD_CLOSE) {
-    Close();
   }
 }
 #elif defined(OS_POSIX)
