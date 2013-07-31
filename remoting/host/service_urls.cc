e61cd4446e55337dcc20b7bccfe6cd3adcd7b50e
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 09:56:07 +0000

    Ignore client certificate requests by default.
    URLFetcher's current behavior when a certificate request is received is to immediately cancel the connection -- this is needed because doing anything else inside chrome could cause that decision to affect other requests using the same urlrequestcontext (that would otherwise have been able to request a certificate from the user).
    
    For the remoting host, however, that is irrelevant - there's no other way to request a certificate from the user, so we can consistently ignore client certificate requests - this at least allows the remoting host to successfully connect to servers with optional client certificate authentication (i.e. servers that request a client certificate, but will provide content even if one isn't provided).
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/21012007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214659 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/service_urls.cc
+++ remoting/host/service_urls.cc
@@ -21,8 +21,6 @@ const char kDirectoryBaseUrlSwitch[] = "directory-base-url";
 const char kXmppServerAddressSwitch[] = "xmpp-server-address";
 const char kXmppServerDisableTlsSwitch[] = "disable-xmpp-server-tls";
 const char kDirectoryBotJidSwitch[] = "directory-bot-jid";
-const char kIgnoreUrlFetcherCertRequestsSwitch[] =
-    "ignore-urlfetcher-cert-requests";
 
 // Non-configurable service paths.
 const char kDirectoryHostsSuffix[] = "/@me/hosts/";
@@ -35,8 +33,7 @@ ServiceUrls::ServiceUrls()
   : directory_base_url_(kDirectoryBaseUrl),
     xmpp_server_address_(kXmppServerAddress),
     xmpp_server_use_tls_(kXmppServerUseTls),
-    directory_bot_jid_(kDirectoryBotJid),
-    ignore_urlfetcher_cert_requests_(false) {
+    directory_bot_jid_(kDirectoryBotJid) {
 #if !defined(NDEBUG)
   // Allow debug builds to override urls via command line.
   CommandLine* command_line = CommandLine::ForCurrentProcess();
@@ -51,9 +48,6 @@ ServiceUrls::ServiceUrls()
   }
   if (command_line->HasSwitch(kXmppServerDisableTlsSwitch)) {
     xmpp_server_use_tls_ = false;
-  }
-  if (command_line->HasSwitch(kIgnoreUrlFetcherCertRequestsSwitch)) {
-    ignore_urlfetcher_cert_requests_ = true;
   }
   if (command_line->HasSwitch(kDirectoryBotJidSwitch)) {
     directory_bot_jid_ = command_line->GetSwitchValueASCII(
@@ -89,10 +83,6 @@ bool ServiceUrls::xmpp_server_use_tls() const {
 
 const std::string& ServiceUrls::directory_bot_jid() const {
   return directory_bot_jid_;
-}
-
-bool ServiceUrls::ignore_urlfetcher_cert_requests() const {
-  return ignore_urlfetcher_cert_requests_;
 }
 
 }  // namespace remoting
