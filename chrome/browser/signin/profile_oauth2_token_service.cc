b52e0b653d2cb6dd287252b29f511eebc0ca7a48
fgorski@chromium.org <fgorski@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 22:29:51 +0000

    Adding ability to cancel a OAuth2TokenService Request Adding testes to cover the cancel functionality
    BUG=263943
    
    Review URL: https://chromiumcodereview.appspot.com/20824006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214842 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/profile_oauth2_token_service.cc
+++ chrome/browser/signin/profile_oauth2_token_service.cc
@@ -76,6 +76,7 @@ void ProfileOAuth2TokenService::Initialize(Profile* profile) {
 }
 
 void ProfileOAuth2TokenService::Shutdown() {
+  CancelAllRequests();
   signin_global_error_->RemoveProvider(this);
   GlobalErrorServiceFactory::GetForProfile(profile_)->RemoveGlobalError(
       signin_global_error_.get());
@@ -114,6 +115,12 @@ void ProfileOAuth2TokenService::Observe(
           content::Details<TokenService::TokenAvailableDetails>(details).ptr();
       if (tok_details->service() ==
           GaiaConstants::kGaiaOAuth2LoginRefreshToken) {
+        // TODO(fgorski): Canceling all requests will not be correct in a
+        // multi-login environment. We should cancel only the requests related
+        // to the token being replaced (old token for the same account_id).
+        // Previous refresh token is not available at this point, but since
+        // there are no other refresh tokens, we cancel all active requests.
+        CancelAllRequests();
         ClearCache();
         UpdateAuthError(GoogleServiceAuthError::AuthErrorNone());
         FireRefreshTokenAvailable(GetAccountId(profile_));
@@ -127,6 +134,12 @@ void ProfileOAuth2TokenService::Observe(
       if (tok_details->service() == GaiaConstants::kLSOService ||
           tok_details->service() ==
               GaiaConstants::kGaiaOAuth2LoginRefreshToken) {
+        // TODO(fgorski): Canceling all requests will not be correct in a
+        // multi-login environment. We should cacnel only the requests related
+        // to the failed refresh token.
+        // Failed refresh token is not available at this point, but since
+        // there are no other refresh tokens, we cancel all active requests.
+        CancelAllRequests();
         ClearCache();
         UpdateAuthError(tok_details->error());
         FireRefreshTokenRevoked(GetAccountId(profile_), tok_details->error());
@@ -134,6 +147,7 @@ void ProfileOAuth2TokenService::Observe(
       break;
     }
     case chrome::NOTIFICATION_TOKENS_CLEARED: {
+      CancelAllRequests();
       ClearCache();
       UpdateAuthError(GoogleServiceAuthError::AuthErrorNone());
       FireRefreshTokensCleared();
