a53e08be245081da76df3b98c15c2b9428306d63
pavan.e@samsung.com <pavan.e@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-31 02:41:30 +0000

    [SVG] animateMotion is freezing at the end by default
    Need to mark the renderer for layout after doing a reset of the transform at the end of the animation.
    Otherwise transform will not be applied.
    
    R=pdr@chromium.org, schenney@chromium.org, dschulze@chromium.org, fmalita@chromium.org
    BUG=265835
    
    Review URL: https://chromiumcodereview.appspot.com/21262002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155209 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGAnimateMotionElement.cpp
+++ third_party/WebKit/Source/core/svg/SVGAnimateMotionElement.cpp
@@ -201,8 +201,17 @@ void SVGAnimateMotionElement::clearAnimatedType(SVGElement* targetElement)
 {
     if (!targetElement)
         return;
-    if (AffineTransform* transform = targetElement->supplementalTransform())
-        transform->makeIdentity();
+
+    AffineTransform* transform = targetElement->supplementalTransform();
+    if (!transform)
+        return;
+
+    transform->makeIdentity();
+
+    if (RenderObject* targetRenderer = targetElement->renderer()) {
+        targetRenderer->setNeedsTransformUpdate();
+        RenderSVGResource::markForLayoutAndParentResourceInvalidation(targetRenderer);
+    }
 }
 
 bool SVGAnimateMotionElement::calculateToAtEndOfDurationValue(const String& toAtEndOfDurationString)
