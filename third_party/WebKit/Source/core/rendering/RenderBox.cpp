85b5847115a917efeef5e749f22a026399052c18
ojan@chromium.org <ojan@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-07-31 23:34:47 +0000

    Revert "Revert 154194 "Manage FloatingObjects using hashmaps""
    This reverts commit r155275. It caused hundreds of layout test failures,
    e.g. http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux%2032/builds/12076.
    
    TBR=inferno@chromium.org
    
    Review URL: https://codereview.chromium.org/21438002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155282 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBox.cpp
+++ third_party/WebKit/Source/core/rendering/RenderBox.cpp
@@ -191,6 +191,7 @@ void RenderBox::removeFloatingOrPositionedChildFromBlockLists()
             parentBlock->markSiblingsWithFloatsForLayout(this);
             parentBlock->markAllDescendantsWithFloatsForLayout(this, false);
         }
+        RenderBlock::floatWillBeRemoved(this);
     }
 
     if (isOutOfFlowPositioned())
