3aa080ee4b6af68cfd64810442b4bcf85eabed04
gab@chromium.org <gab@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 13:40:12 +0000

    Do not CreateSentinel until after the process singleton has been grabbed by the current process.
    Move first_run::CreateSentinel to first_run::internal::CreateSentinel.
    
    This is based on top of https://codereview.chromium.org/20483002/
    
    BUG=264694
    TEST= 
    A) Launch chrome.exe with no First Run sentinel, make sure first run happens once (relaunch and it shouldn't happen twice). 
    B) Launch chrome.exe with First Run sentinel, but with --force-first-run, make sure first run happens anyways. 
    C) Launch chrome.exe with no First Run sentinel, but with --no-first-run, make sure first run doesn't happen (and doesn't happen in subsequent relaunches either without the flag).
    
    Review URL: https://chromiumcodereview.appspot.com/20743002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214709 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main.cc
+++ chrome/browser/chrome_browser_main.cc
@@ -1239,6 +1239,8 @@ int ChromeBrowserMainParts::PreMainMessageLoopRunImpl() {
         NOTREACHED();
     }
   }
+
+  first_run::CreateSentinelIfNeeded();
 #endif  // !defined(OS_ANDROID)
 
   // Desktop construction occurs here, (required before profile creation).
