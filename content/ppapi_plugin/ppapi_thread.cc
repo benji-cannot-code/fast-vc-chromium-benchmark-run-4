f957036a4f00019cd96b510a9873b8bcaba688ea
jschuh@chromium.org <jschuh@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 01:46:16 +0000

    Warm up dxva2.dll for pepflashplayer.dll
    BUG=265610
    R=piman@chromium.org
    
    Review URL: https://codereview.chromium.org/21186002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214505 0039d316-1c4b-4281-b951-d872f2087c98

--- content/ppapi_plugin/ppapi_thread.cc
+++ content/ppapi_plugin/ppapi_thread.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include "base/win/win_util.h"
+#include "base/win/windows_version.h"
 #include "sandbox/win/src/sandbox.h"
 #elif defined(OS_MACOSX)
 #include "content/common/sandbox_init_mac.h"
@@ -326,6 +327,13 @@ void PpapiThread::OnLoadPlugin(const base::FilePath& path,
   // can be loaded. TODO(cpu): consider changing to the loading style of
   // regular plugins.
   if (g_target_services) {
+    // Let Flash load DRM before lockdown on Vista+.
+    if (permissions.HasPermission(ppapi::PERMISSION_FLASH) &&
+        base::win::OSInfo::GetInstance()->version() >=
+        base::win::VERSION_VISTA ) {
+      LoadLibrary(L"dxva2.dll");
+    }
+
     // Cause advapi32 to load before the sandbox is turned on.
     unsigned int dummy_rand;
     rand_s(&dummy_rand);
