8614cef37c7db53690013a8951b4a39ea328e0e5
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 14:35:33 +0000

    Fix memory leak in UbertokenFetcher tests.
    Remove a line which spawns and immediatelly leaks a TestURLRequestContextGetter.
    
    BUG=264225
    R=atwilson@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/21106007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214732 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/signin/ubertoken_fetcher_unittest.cc
+++ chrome/browser/signin/ubertoken_fetcher_unittest.cc
@@ -65,7 +65,6 @@ class UbertokenFetcherTest : public TokenServiceTestHarness {
  public:
   virtual void SetUp() OVERRIDE {
     TokenServiceTestHarness::SetUp();
-    profile()->CreateRequestContext(NULL);
 
     ProfileOAuth2TokenServiceFactory::GetInstance()->
         SetTestingFactoryAndUse(profile(), Build);
