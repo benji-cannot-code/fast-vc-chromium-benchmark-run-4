c5b8a771e098c46cc0fc3eddaf91dbc23e4b75cc
crogers@google.com <crogers@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 05:16:49 +0000

    Support sending MIDI data with throttling
    The Web MIDI API allows sending of MIDI data, but we need to take
    care to not send too much data from the renderer to the browser.
    We implement a throttling mechanism where the browser process
    acknowledges the number of sent bytes back to the renderer,
    and the renderer only sends new data if it hasn't already sent
    too much unacknowledged data.
    
    BUG=163795
    TEST=none
    
    Review URL: https://chromiumcodereview.appspot.com/19612004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214570 0039d316-1c4b-4281-b951-d872f2087c98

--- media/midi/midi_manager.cc
+++ media/midi/midi_manager.cc
@@ -5,6 +5,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "media/midi/midi_manager.h"
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
+#include "base/threading/thread.h"
+
 namespace media {
 
 #if !defined(OS_MACOSX)
@@ -55,9 +59,26 @@ void MIDIManager::ReceiveMIDIData(
     double timestamp) {
   base::AutoLock auto_lock(clients_lock_);
 
-  // TODO(crogers): Filter out sysex.
   for (ClientList::iterator i = clients_.begin(); i != clients_.end(); ++i)
     (*i)->ReceiveMIDIData(port_index, data, length, timestamp);
-};
+}
+
+void MIDIManager::DispatchSendMIDIData(MIDIManagerClient* client,
+                                       int port_index,
+                                       const uint8* data,
+                                       size_t length,
+                                       double timestamp) {
+  // Lazily create the thread when first needed.
+  if (!send_thread_) {
+    send_thread_.reset(new base::Thread("MIDISendThread"));
+    send_thread_->Start();
+    send_message_loop_ = send_thread_->message_loop_proxy();
+  }
+
+  send_message_loop_->PostTask(
+     FROM_HERE,
+     base::Bind(&MIDIManager::SendMIDIData, base::Unretained(this),
+         client, port_index, data, length, timestamp));
+}
 
 }  // namespace media
