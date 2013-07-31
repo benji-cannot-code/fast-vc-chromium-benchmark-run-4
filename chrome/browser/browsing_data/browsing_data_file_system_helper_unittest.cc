44cdec606085ab7bdcc8519c4d4bb458e1d67fc5
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 02:26:33 +0000

    Remove unnecessary fileapi include from browsing_data_file_system_helper_unittest
    BUG=265770
    TEST=compile
    NOTRY=true
    R=bauerb@chromium.org
    
    Review URL: https://codereview.chromium.org/21221002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214516 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/browsing_data/browsing_data_file_system_helper_unittest.cc
+++ chrome/browser/browsing_data/browsing_data_file_system_helper_unittest.cc
@@ -19,7 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/test/test_browser_thread_bundle.h"
 #include "webkit/browser/fileapi/file_system_context.h"
 #include "webkit/browser/fileapi/file_system_url.h"
-#include "webkit/browser/fileapi/file_system_usage_cache.h"
 #include "webkit/common/fileapi/file_system_types.h"
 
 using content::BrowserContext;
