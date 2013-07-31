7c8d0b2e78d85fc7af1877db84812c13e459453e
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-07-31 06:36:04 +0000

    base: Added thread-specific CPU-time clock
    - TimeTicks::ThreadNow() measures the CPU time consumed by the calling thread
      (excluding time when the thread was de-scheduled). This features is
      needed for more accurate benchmarks.
    - Current implementation only supports Posix systems. Windows and Mac TBD.
    - Added unittest for new method.
    
    R=mark@chromium.org, nduca@chromium.org
    BUG=264306
    
    Review URL: https://chromiumcodereview.appspot.com/20576003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214591 0039d316-1c4b-4281-b951-d872f2087c98

--- base/time/time_mac.cc
+++ base/time/time_mac.cc
@@ -193,6 +193,12 @@ TimeTicks TimeTicks::HighResNow() {
 }
 
 // static
+TimeTicks TimeTicks::ThreadNow() {
+  NOTREACHED();
+  return TimeTicks();
+}
+
+// static
 TimeTicks TimeTicks::NowFromSystemTraceTime() {
   return HighResNow();
 }
