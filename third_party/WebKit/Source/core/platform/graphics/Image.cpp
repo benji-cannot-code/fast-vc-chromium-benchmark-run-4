5be51a8727139263935031b1a32dc5893bc5e5d4
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-01 12:34:32 +0000

    Collapse ImageSkia.cpp and ImageChromium.cpp into Image.cpp
    Also remove declaration of Image::create, which had no implementation in
    any of these files (or anywhere else!)
    
    BUG=242060
    
    Review URL: https://chromiumcodereview.appspot.com/21398002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155329 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/Image.cpp
+++ third_party/WebKit/Source/core/platform/graphics/Image.cpp
@@ -28,17 +28,24 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/platform/graphics/Image.h"
 
-#include <math.h>
 #include "core/platform/Length.h"
 #include "core/platform/MIMETypeRegistry.h"
 #include "core/platform/SharedBuffer.h"
+#include "core/platform/chromium/TraceEvent.h"
 #include "core/platform/graphics/BitmapImage.h"
+#include "core/platform/graphics/FloatPoint.h"
+#include "core/platform/graphics/FloatRect.h"
+#include "core/platform/graphics/FloatSize.h"
 #include "core/platform/graphics/GraphicsContext.h"
 #include "core/platform/graphics/GraphicsContextStateSaver.h"
 #include "core/platform/graphics/GraphicsTypes.h"
 #include "core/platform/graphics/IntRect.h"
+#include "public/platform/Platform.h"
+#include "public/platform/WebData.h"
 #include "wtf/MainThread.h"
 #include "wtf/StdLibExtras.h"
+
+#include <math.h>
 
 namespace WebCore {
 
@@ -56,6 +63,17 @@ Image* Image::nullImage()
     ASSERT(isMainThread());
     DEFINE_STATIC_LOCAL(RefPtr<Image>, nullImage, (BitmapImage::create()));;
     return nullImage.get();
+}
+
+PassRefPtr<Image> Image::loadPlatformResource(const char *name)
+{
+    const WebKit::WebData& resource = WebKit::Platform::current()->loadResource(name);
+    if (resource.isEmpty())
+        return Image::nullImage();
+
+    RefPtr<Image> image = BitmapImage::create();
+    image->setData(resource, true);
+    return image.release();
 }
 
 bool Image::supportsType(const String& type)
@@ -204,6 +222,14 @@ void Image::drawTiled(GraphicsContext* ctxt, const FloatRect& dstRect, const Flo
     }
 
     startAnimation();
+}
+
+void Image::drawPattern(GraphicsContext* context, const FloatRect& floatSrcRect, const FloatSize& scale,
+    const FloatPoint& phase, CompositeOperator compositeOp, const FloatRect& destRect, BlendMode blendMode)
+{
+    TRACE_EVENT0("skia", "Image::drawPattern");
+    if (RefPtr<NativeImageSkia> bitmap = nativeImageForCurrentFrame())
+        bitmap->drawPattern(context, adjustForNegativeSize(floatSrcRect), scale, phase, compositeOp, destRect, blendMode);
 }
 
 void Image::computeIntrinsicDimensions(Length& intrinsicWidth, Length& intrinsicHeight, FloatSize& intrinsicRatio)
