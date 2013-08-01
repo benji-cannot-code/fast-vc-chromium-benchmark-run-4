f979f6968206e35f6dadbd6b743a19f467ac714f
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 02:57:13 +0000

    Highlight items in the OSX App Launcher when install state changes.
    When install begins, ensure the page is shown. When install completes,
    ensure the page is shown, and select the item. This is to match CrOS/Win
    behavior.
    
    BUG=138633
    TEST=With OSX App launcher enabled, install a large item from the
    webstore. App launcher should show, with the item visible, with progress
    bars. Move to a different page and dismiss the app launcher. When
    install completes, app launcher should show again, and installed item
    should be visible, and selected.
    
    R=koz@chromium.org
    
    Review URL: https://codereview.chromium.org/21458002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214933 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/app_list/cocoa/apps_grid_view_item.mm
+++ ui/app_list/cocoa/apps_grid_view_item.mm
@@ -342,10 +342,12 @@ void ItemModelObserverBridge::ItemPercentDownloadedChanged() {
   if (!isInstalling == !progressIndicator_)
     return;
 
+  [self ensureVisible];
   if (!isInstalling) {
     [progressIndicator_ removeFromSuperview];
     progressIndicator_.reset();
     [self updateButtonTitle];
+    [self setSelected:YES];
     return;
   }
 
