5c86a9c64d77b47802d72c06a3effcfbd4adaf50
mvrable@chromium.org <mvrable@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 06:30:43 +0000

    Extension activity log database refactoring (step 4)
    This is a continuation of work in:
    https://codereview.chromium.org/18660004/
    https://codereview.chromium.org/19234003/
    https://codereview.chromium.org/19690003/
    
    Move all logic specific to a database schema to the ActivityLogPolicy
    classes, out of ActivityDatabase. This should largely finish the work
    of making it possible to try out different ways of shrinking the
    database size in different policies.
    
    Slightly change how incognito URLs are represented in Action objects:
    keep the real URL around but mark it as incognito, so policies can act
    as appropriate.
    
    Store activity log records in an Action object much closer to where the
    logs are generated, and remove several helper methods in ActivityLog for
    generating log records.
    
    BUG=255730, 253368
    
    Review URL: https://chromiumcodereview.appspot.com/21146003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214978 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/activity_log/stream_noargs_ui_policy.h
+++ chrome/browser/extensions/activity_log/stream_noargs_ui_policy.h
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define CHROME_BROWSER_EXTENSIONS_ACTIVITY_LOG_STREAM_NOARGS_UI_POLICY_H_
 
 #include <string>
+
 #include "base/containers/hash_tables.h"
 #include "chrome/browser/extensions/activity_log/fullstream_ui_policy.h"
 
@@ -19,12 +20,12 @@ class StreamWithoutArgsUIPolicy : public FullStreamUIPolicy {
   virtual ~StreamWithoutArgsUIPolicy();
 
  protected:
-  virtual void ProcessArguments(scoped_refptr<Action> action) const
-      OVERRIDE;
+  // Clears the args field of the action (except for some DOM events and a
+  // small whitelisted set of api_names).  For WEB_REQUEST actions, keeps keys
+  // but removes values from the "web_request" dictionary in other.
+  virtual scoped_refptr<Action> ProcessArguments(
+      scoped_refptr<Action> action) const OVERRIDE;
 
-  virtual void ProcessWebRequestModifications(
-      base::DictionaryValue& details,
-      std::string& details_string) const OVERRIDE;
  private:
   base::hash_set<std::string> arg_whitelist_api_;
 };
