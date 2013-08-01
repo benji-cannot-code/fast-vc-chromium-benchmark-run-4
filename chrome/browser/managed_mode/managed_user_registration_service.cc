56598953957b65250f8e409563f727289b55cf63
sergiu@chromium.org <sergiu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 12:14:56 +0000

    Add --no-supervised-user-acknowledgment-check flag
    This allows us to support servers which do not send supervised user acknowledgement on creation (e.g. the local server).
    
    This prevents the timeout from happening and forces completion as if the acknowledgment was received.
    
    R=pam@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/21094003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215011 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/managed_mode/managed_user_registration_service.cc
+++ chrome/browser/managed_mode/managed_user_registration_service.cc
@@ -104,7 +104,9 @@ void ManagedUserRegistrationService::Register(
   callback_ = callback;
 
   if (!CommandLine::ForCurrentProcess()->HasSwitch(
-        switches::kNoManagedUserRegistrationTimeout)) {
+        switches::kNoManagedUserRegistrationTimeout) &&
+      !CommandLine::ForCurrentProcess()->HasSwitch(
+        switches::kNoManagedUserAcknowledgmentCheck)) {
     registration_timer_.Start(
         FROM_HERE,
         base::TimeDelta::FromMilliseconds(kRegistrationTimeoutMS),
@@ -362,8 +364,10 @@ void ManagedUserRegistrationService::OnReceivedToken(
 }
 
 void ManagedUserRegistrationService::CompleteRegistrationIfReady() {
-  if (!pending_managed_user_acknowledged_ ||
-      pending_managed_user_token_.empty()) {
+  bool acknowledged = pending_managed_user_acknowledged_ ||
+                      CommandLine::ForCurrentProcess()->HasSwitch(
+                          switches::kNoManagedUserAcknowledgmentCheck);
+  if (!acknowledged || pending_managed_user_token_.empty()) {
     return;
   }
 
