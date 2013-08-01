314ab37274859459ff0bd4c1d0cc3499341b5c5d
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 16:06:15 +0000

    Add ASH_EXPORT to base class of exported WindowSelectorController.
    TBR=pkotwicz@chromium.org
    
    BUG=263477
    TEST=Win Aura Builder compiles.
    
    Review URL: https://codereview.chromium.org/21527003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215043 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_selector_delegate.h
+++ ash/wm/window_selector_delegate.h
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef ASH_WM_WINDOW_SELECTOR_DELEGATE_H_
 #define ASH_WM_WINDOW_SELECTOR_DELEGATE_H_
 
+#include "ash/ash_export.h"
 #include "base/compiler_specific.h"
 
 namespace aura {
@@ -15,7 +16,7 @@ class Window;
 namespace ash {
 
 // Implement this class to handle the selection event from WindowSelector.
-class WindowSelectorDelegate {
+class ASH_EXPORT WindowSelectorDelegate {
  public:
   // Invoked when a window is selected.
   virtual void OnWindowSelected(aura::Window* window) = 0;
