3f15e847ce292f0dfcb033f013093b15dfd0d75b
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-01 00:37:56 +0000

    Revert unnecessary WebNode includes from r155179
    Review URL: https://chromiumcodereview.appspot.com/20681005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155285 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebNode.h
+++ third_party/WebKit/public/web/WebNode.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WebNode_h
 
 #include "../platform/WebCommon.h"
-#include "../platform/WebImage.h"
 #include "../platform/WebPrivatePtr.h"
 #include "../platform/WebString.h"
 #include "WebExceptionCode.h"
