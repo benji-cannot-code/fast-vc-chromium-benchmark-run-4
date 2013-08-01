00627c2f05069c8e77c67cc32d4dc427320b3418
scherkus@chromium.org <scherkus@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 00:39:36 +0000

    Revert 214844 "Fix crash when reloading packaged app. "
    I believe it's causing AppShimQuitTest.QuitWithKeyEvent test to crash.
    
    It was recently added in r214672 and your try runs didn't get a chance to try it before your patch landed.
    
    > Fix crash when reloading packaged app. 
    > 
    > When a shell window was closed, it wasn't removed from the registry immediately 
    > (it was removed in a callback from OnNativeClose, which happens a bit later.) 
    > This meant that when the app was loaded again, the window was still in the 
    > registry with a stale pointer to the old extension. ExtensionSettingsHandler 
    > was tickling this by iterating shell windows on extension load.
    > 
    > BUG=174250
    > TEST=On chromeos, enable the 'Enable debugging for packed apps.' flag. Then
    > open chrome://extensions. Then open a packaged app, and reload it. Chrome
    > should not crash.
    > 
    > Review URL: https://chromiumcodereview.appspot.com/20243003
    
    TBR=benwells@chromium.org
    
    Review URL: https://codereview.chromium.org/20534003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214882 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
+++ chrome/browser/ui/gtk/extensions/native_app_window_gtk.cc
@@ -8,7 +8,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <gdk/gdkx.h>
 #include <vector>
 
-#include "base/message_loop/message_loop.h"
 #include "base/message_loop/message_pump_gtk.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/profiles/profile.h"
@@ -245,15 +244,12 @@ void NativeAppWindowGtk::Close() {
   // To help catch bugs in any event handlers that might get fired during the
   // destruction, set window_ to NULL before any handlers will run.
   window_ = NULL;
-  gtk_widget_destroy(window);
 
-  // On other platforms, the native window doesn't get destroyed synchronously.
-  // We simulate that here so that ShellWindow can assume that it doesn't get
-  // deleted immediately upon calling Close().
-  base::MessageLoop::current()->PostTask(
-      FROM_HERE,
-      base::Bind(&ShellWindow::OnNativeClose,
-                 base::Unretained(shell_window_)));
+  // OnNativeClose does a delete this so no other members should
+  // be accessed after. gtk_widget_destroy is safe (and must
+  // be last).
+  shell_window_->OnNativeClose();
+  gtk_widget_destroy(window);
 }
 
 void NativeAppWindowGtk::Activate() {
