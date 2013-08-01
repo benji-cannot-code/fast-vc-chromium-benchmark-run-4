a3d9bb9d978b02d80521ce2553f545ce861ab115
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 00:59:29 +0000

    ui: Fix some paths to WebKit.
    R=sky@chromium.org
    NOTRY=true
    
    Review URL: https://codereview.chromium.org/21120004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214892 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ime/composition_underline.h
+++ ui/base/ime/composition_underline.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace ui {
 
 // Intentionally keep sync with WebKit::WebCompositionUnderline defined in:
-// third_party/WebKit/Source/WebKit/chromium/public/WebCompositionUnderline.h
+// third_party/WebKit/public/web/WebCompositionUnderline.h
 struct CompositionUnderline {
   CompositionUnderline()
     : start_offset(0),
