5800add5b17fd50f2d8084d89c657b623781df8b
mmontgomery@chromium.org <mmontgomery@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-01 18:59:24 +0000

    Add "FAIL" to the set of possible results.
    The gtest results are migrating to the run-webkit-tests aka
    full_results format. Part of that change means there'll be a
    "FAIL" result, which TestResultServer needs to understand.
    
    This change is a prerequisite for the above change, https://chromiumcodereview.appspot.com/20492002/. 
    
    BUG=247192
    
    Review URL: https://chromiumcodereview.appspot.com/21090003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155353 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/TestResultServer/model/jsonresults_unittest.py
+++ third_party/WebKit/Tools/TestResultServer/model/jsonresults_unittest.py
@@ -88,6 +88,14 @@ FULL_RESULT_EXAMPLE = """ADD_RESULTS({
                 "expected": "PASS",
                 "actual": "SKIP"
             },
+            "unexpected-fail.html": {
+                "expected": "PASS",
+                "actual": "FAIL"
+            },
+            "flaky-failed.html": {
+                "expected": "PASS FAIL",
+                "actual": "FAIL"
+            },
             "media-document-audio-repaint.html": {
                 "expected": "IMAGE",
                 "actual": "IMAGE",
@@ -118,7 +126,7 @@ FULL_RESULT_EXAMPLE = """ADD_RESULTS({
         "SKIP": 2,
         "TIMEOUT": 16,
         "IMAGE+TEXT": 0,
-        "FAIL": 0,
+        "FAIL": 2,
         "AUDIO": 0
     },
     "has_wdiff": true,
@@ -337,8 +345,8 @@ class JsonResultsTest(unittest.TestCase):
             "builds": ["2", "1"],
             "tests": {
                 "001.html": {
-                    "results": [[100, TEXT]],
-                    "times": [[100, 0]],
+                    "results": [[100, TEXT], [100, FAIL]],
+                    "times": [[200, 0]],
                 }
             }
         }, json_string=JSON_RESULTS_OLD_TEMPLATE)
@@ -352,13 +360,13 @@ class JsonResultsTest(unittest.TestCase):
             }
         }, json_string=JSON_RESULTS_OLD_TEMPLATE)
         incremental_json, _ = JsonResults._get_incremental_json(self._builder, incremental_results, is_full_results_format=False)
-        merged_results, _ = JsonResults.merge(self._builder, aggregated_results, incremental_json, num_runs=200, sort_keys=True)
+        merged_results, _ = JsonResults.merge(self._builder, aggregated_results, incremental_json, num_runs=201, sort_keys=True)
         self.assert_json_equal(merged_results, self._make_test_json({
             "builds": ["3", "2", "1"],
             "tests": {
                 "001.html": {
-                    "results": [[101, TEXT]],
-                    "times": [[101, 0]],
+                    "results": [[101, TEXT], [100, FAIL]],
+                    "times": [[201, 0]],
                 }
             }
         }))
@@ -370,7 +378,7 @@ class JsonResultsTest(unittest.TestCase):
                 "buildNumbers": ["3"],
                 "chromeRevision": ["5678"],
                 "failure_map": CHAR_TO_FAILURE,
-                "num_failures_by_type": {"AUDIO": [0], "CRASH": [3], "IMAGE": [1], "IMAGE+TEXT": [0], "MISSING": [0], "PASS": [10], "SKIP": [2], "TEXT": [3], "TIMEOUT": [16]},
+                "num_failures_by_type": {"AUDIO": [0], "CRASH": [3], "FAIL": [2], "IMAGE": [1], "IMAGE+TEXT": [0], "MISSING": [0], "PASS": [10], "SKIP": [2], "TEXT": [3], "TIMEOUT": [16]},
                 "secondsSinceEpoch": [1368146629],
                 "tests": {
                     "media": {
@@ -406,7 +414,16 @@ class JsonResultsTest(unittest.TestCase):
                             "expected": "PASS FAIL IMAGE TIMEOUT CRASH MISSING",
                             "results": [[1, TIMEOUT]],
                             "times": [[1, 6]],
-                        }
+                        },
+                        "flaky-failed.html": {
+                            "expected": "PASS FAIL",
+                            "results": [[1, FAIL]],
+                            "times": [[1, 0]],
+                        },
+                        "unexpected-fail.html": {
+                            "results": [[1, FAIL]],
+                            "times": [[1, 0]],
+                        },
                     }
                 }
             },
@@ -575,13 +592,13 @@ class JsonResultsTest(unittest.TestCase):
             # Incremental results
             {"builds": ["4", "3"],
              "tests": {"001.html": {
-                           "results": [[2, IMAGE]],
-                           "times": [[2, 2]]}}},
+                           "results": [[2, IMAGE], [1, FAIL]],
+                           "times": [[3, 2]]}}},
             # Expected results
             {"builds": ["4", "3", "2", "1"],
              "tests": {"001.html": {
-                           "results": [[2, IMAGE], [200, TEXT]],
-                           "times": [[2, 2], [200, 0]]}}})
+                           "results": [[1, FAIL], [2, IMAGE], [200, TEXT]],
+                           "times": [[3, 2], [200, 0]]}}})
 
     def test_merge_multiple_tests_multiple_runs(self):
         self._test_merge(
@@ -1020,6 +1037,9 @@ class JsonResultsTest(unittest.TestCase):
                        "foo.bar2": {
                            "results": [[100, IMAGE]],
                            "times": [[100, 0]]},
+                       "test.failed": {
+                           "results": [[5, FAIL]],
+                           "times": [[5, 0]]},
                        },
              "version": 3},
             # Incremental results
@@ -1030,6 +1050,9 @@ class JsonResultsTest(unittest.TestCase):
                        "foo.bar3": {
                            "results": [[1, TEXT]],
                            "times": [[1, 0]]},
+                       "test.failed": {
+                           "results": [[5, FAIL]],
+                           "times": [[5, 0]]},
                        },
              "version": 4},
             # Expected results
@@ -1043,6 +1066,9 @@ class JsonResultsTest(unittest.TestCase):
                        "foo.bar3": {
                            "results": [[1, TEXT]],
                            "times": [[1, 0]]},
+                       "test.failed": {
+                           "results": [[10, FAIL]],
+                           "times": [[10, 0]]},
                        },
              "version": 4})
 
