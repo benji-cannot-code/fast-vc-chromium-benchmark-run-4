39d87110b294fcecfca8881ad56b48b4d1940ebd
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-01 15:08:01 +0000

    Remove unused methods in platform/FileSystem
    Will do the same cleanup in chrome side as well.
    
    BUG=none (remotely for 257852)
    TEST=compile
    
    Review URL: https://chromiumcodereview.appspot.com/21471003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155335 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/chromium/FileSystemChromium.cpp
+++ third_party/WebKit/Source/core/platform/chromium/FileSystemChromium.cpp
@@ -42,16 +42,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace WebCore {
 
-bool deleteFile(const String& path)
-{
-    return WebKit::Platform::current()->fileUtilities()->deleteFile(path);
-}
-
-bool deleteEmptyDirectory(const String& path)
-{
-    return WebKit::Platform::current()->fileUtilities()->deleteEmptyDirectory(path);
-}
-
 bool getFileSize(const String& path, long long& result)
 {
     FileMetadata metadata;
@@ -86,21 +76,6 @@ String directoryName(const String& path)
     return WebKit::Platform::current()->fileUtilities()->directoryName(path);
 }
 
-String pathByAppendingComponent(const String& path, const String& component)
-{
-    return WebKit::Platform::current()->fileUtilities()->pathByAppendingComponent(path, component);
-}
-
-bool makeAllDirectories(const String& path)
-{
-    return WebKit::Platform::current()->fileUtilities()->makeAllDirectories(path);
-}
-
-bool fileExists(const String& path)
-{
-    return WebKit::Platform::current()->fileUtilities()->fileExists(path);
-}
-
 PlatformFileHandle openFile(const String& path, FileOpenMode mode)
 {
     return WebKit::Platform::current()->fileUtilities()->openFile(path, mode);
@@ -111,24 +86,9 @@ void closeFile(PlatformFileHandle& handle)
     WebKit::Platform::current()->fileUtilities()->closeFile(handle);
 }
 
-long long seekFile(PlatformFileHandle handle, long long offset, FileSeekOrigin origin)
-{
-    return WebKit::Platform::current()->fileUtilities()->seekFile(handle, offset, origin);
-}
-
-bool truncateFile(PlatformFileHandle handle, long long offset)
-{
-    return WebKit::Platform::current()->fileUtilities()->truncateFile(handle, offset);
-}
-
 int readFromFile(PlatformFileHandle handle, char* data, int length)
 {
     return WebKit::Platform::current()->fileUtilities()->readFromFile(handle, data, length);
-}
-
-int writeToFile(PlatformFileHandle handle, const char* data, int length)
-{
-    return WebKit::Platform::current()->fileUtilities()->writeToFile(handle, data, length);
 }
 
 } // namespace WebCore
