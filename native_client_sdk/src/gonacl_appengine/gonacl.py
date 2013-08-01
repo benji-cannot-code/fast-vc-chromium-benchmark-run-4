33da3167bbf3f646ffe79e5b8cd11045f9d98c16
eliben@chromium.org <eliben@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 18:39:01 +0000

    Update uploading instructions in README and set copyright in the Python file
    BUG=None
    R=sbc@chromium.org
    
    Review URL: https://codereview.chromium.org/21544004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215073 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/gonacl_appengine/gonacl.py
+++ native_client_sdk/src/gonacl_appengine/gonacl.py
@@ -1,6 +1,8 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
 import webapp2
-
 
 application = webapp2.WSGIApplication([
   webapp2.Route('/', webapp2.RedirectHandler, defaults={
