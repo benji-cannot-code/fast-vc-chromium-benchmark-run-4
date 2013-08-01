035633018fddb939423b47baa30da073a5bbefb8
cjhopman@chromium.org <cjhopman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 00:48:35 +0000

    [Android] Hide text handles when displaying the popup zoomer
    Currently, text handles will appear on top of the popup zoomer.
    
    Instead, temporarily hide them while the popup zoomer is displayed.
    They will fade in after the popup zomer is no longer displayed.
    
    BUG=171548
    
    Review URL: https://chromiumcodereview.appspot.com/20868002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214886 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
+++ content/public/android/java/src/org/chromium/content/browser/ContentViewCore.java
@@ -2128,6 +2128,17 @@ import java.util.Map;
         scheduleTextHandleFadeIn();
     }
 
+    private boolean allowTextHandleFadeIn() {
+        if (mContentViewGestureHandler.isNativeScrolling() ||
+                mContentViewGestureHandler.isNativePinching()) {
+            return false;
+        }
+
+        if (mPopupZoomer.isShowing()) return false;
+
+        return true;
+    }
+
     // Cancels any pending fade in and schedules a new one.
     private void scheduleTextHandleFadeIn() {
         if (!isInsertionHandleShowing() && !isSelectionHandleShowing()) return;
@@ -2136,9 +2147,8 @@ import java.util.Map;
             mDeferredHandleFadeInRunnable = new Runnable() {
                 @Override
                 public void run() {
-                    if (mContentViewGestureHandler.isNativeScrolling() ||
-                            mContentViewGestureHandler.isNativePinching()) {
-                        // Delay fade in until no longer scrolling or pinching.
+                    if (!allowTextHandleFadeIn()) {
+                        // Delay fade in until it is allowed.
                         scheduleTextHandleFadeIn();
                     } else {
                         if (isSelectionHandleShowing()) {
@@ -2299,6 +2309,7 @@ import java.util.Map;
     private void showDisambiguationPopup(Rect targetRect, Bitmap zoomedBitmap) {
         mPopupZoomer.setBitmap(zoomedBitmap);
         mPopupZoomer.show(targetRect);
+        temporarilyHideTextHandles();
     }
 
     @SuppressWarnings("unused")
