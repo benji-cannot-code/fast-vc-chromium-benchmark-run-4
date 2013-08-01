9389e5d1e69c63f609140bcbf38ec9d3eaee976c
gkanwar@chromium.org <gkanwar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 16:59:22 +0000

    Changes argument passing to use options objects
    Options objects are specific to the test type, and are named tuples, so
    they are also immutable.
    
    This is a precursor to the host-driven refactor.
    
    BUG=176323
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/21008004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215054 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/pylib/gtest/test_options.py
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Defines the GTestOptions named tuple."""
+
+import collections
+
+GTestOptions = collections.namedtuple('GTestOptions', [
+    'build_type',
+    'tool',
+    'cleanup_test_files',
+    'push_deps',
+    'gtest_filter',
+    'test_arguments',
+    'timeout',
+    'suite_name'])
