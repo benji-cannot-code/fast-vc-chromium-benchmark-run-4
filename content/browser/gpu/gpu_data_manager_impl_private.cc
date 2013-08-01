af87218f4f7ad959a6ceb708637186afa7a6f041
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 06:17:21 +0000

    Send correct command line flags when using software compositor.
    The GPU, pepper3d, and accelerated 2d canvas don't work with software compositing, so disable them.
    
    BUG=261431
    R=apatrick@chromium.org
    
    Review URL: https://codereview.chromium.org/20777007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@214973 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/gpu_data_manager_impl_private.cc
+++ content/browser/gpu/gpu_data_manager_impl_private.cc
@@ -659,6 +659,17 @@ void GpuDataManagerImplPrivate::AppendRendererCommandLine(
   if (IsFeatureBlacklisted(gpu::GPU_FEATURE_TYPE_ACCELERATED_VIDEO_DECODE) &&
       !command_line->HasSwitch(switches::kDisableAcceleratedVideoDecode))
     command_line->AppendSwitch(switches::kDisableAcceleratedVideoDecode);
+
+  if (use_software_compositor_ &&
+      !command_line->HasSwitch(switches::kEnableSoftwareCompositing))
+    command_line->AppendSwitch(switches::kEnableSoftwareCompositing);
+
+#if defined(USE_AURA)
+  if (!CanUseGpuBrowserCompositor()) {
+    command_line->AppendSwitch(switches::kDisableGpuCompositing);
+    command_line->AppendSwitch(switches::kDisablePepper3d);
+  }
+#endif
 }
 
 void GpuDataManagerImplPrivate::AppendGpuCommandLine(
@@ -820,6 +831,11 @@ void GpuDataManagerImplPrivate::UpdateRendererWebPrefs(
     prefs->accelerated_compositing_for_3d_transforms_enabled = true;
     prefs->accelerated_compositing_for_plugins_enabled = true;
   }
+
+#if defined(USE_AURA)
+  if (!CanUseGpuBrowserCompositor())
+    prefs->accelerated_2d_canvas_enabled = false;
+#endif
 }
 
 gpu::GpuSwitchingOption
