974dc433a2bb6257dd10f6f39014944bc49ce620
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-01 19:20:29 +0000

    Support GData WAPI fallback for GetShareUrl on Drive API v2.
    Drive API v2 doesn't support share url yet. So, as a fallback, we need to
    use GData WAPI for share url even if we enable Drive API v2.
    
    BUG=266759
    TEST=Ran unit_tests and tested manually.
    
    Review URL: https://chromiumcodereview.appspot.com/20607006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215085 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/drive/drive_api_service.cc
+++ chrome/browser/drive/drive_api_service.cc
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/drive_api_requests.h"
 #include "chrome/browser/google_apis/gdata_errorcode.h"
 #include "chrome/browser/google_apis/gdata_wapi_parser.h"
+#include "chrome/browser/google_apis/gdata_wapi_requests.h"
 #include "chrome/browser/google_apis/request_sender.h"
 #include "content/public/browser/browser_thread.h"
 
@@ -43,11 +44,13 @@ using google_apis::GetContentCallback;
 using google_apis::GetFileRequest;
 using google_apis::GetFilelistRequest;
 using google_apis::GetResourceEntryCallback;
+using google_apis::GetResourceEntryRequest;
 using google_apis::GetResourceListCallback;
 using google_apis::GetShareUrlCallback;
 using google_apis::HTTP_NOT_IMPLEMENTED;
 using google_apis::HTTP_SUCCESS;
 using google_apis::InitiateUploadCallback;
+using google_apis::Link;
 using google_apis::ProgressCallback;
 using google_apis::RequestSender;
 using google_apis::ResourceEntry;
@@ -269,11 +272,13 @@ DriveAPIService::DriveAPIService(
     base::TaskRunner* blocking_task_runner,
     const GURL& base_url,
     const GURL& base_download_url,
+    const GURL& wapi_base_url,
     const std::string& custom_user_agent)
     : oauth2_token_service_(oauth2_token_service),
       url_request_context_getter_(url_request_context_getter),
       blocking_task_runner_(blocking_task_runner),
       url_generator_(base_url, base_download_url),
+      wapi_url_generator_(wapi_base_url, base_download_url),
       custom_user_agent_(custom_user_agent) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 }
@@ -290,6 +295,11 @@ void DriveAPIService::Initialize() {
   std::vector<std::string> scopes;
   scopes.push_back(kDriveScope);
   scopes.push_back(kDriveAppsReadonlyScope);
+
+  // GData WAPI token. These are for GetShareUrl().
+  scopes.push_back(util::kDocsListScope);
+  scopes.push_back(util::kDriveAppsScope);
+
   sender_.reset(new RequestSender(
      new google_apis::AuthService(
          oauth2_token_service_, url_request_context_getter_, scopes),
@@ -471,11 +481,17 @@ CancelCallback DriveAPIService::GetShareUrl(
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
   DCHECK(!callback.is_null());
 
-  // TODO(mtomasz): Implement this, once it is supported by the Drive API.
-  NOTIMPLEMENTED();
-  callback.Run(HTTP_NOT_IMPLEMENTED, GURL());
-
-  return CancelCallback();
+  // Unfortunately "share url" is not yet supported on Drive API v2.
+  // So, as a fallback, we use GData WAPI protocol for this method.
+  // TODO(hidehiko): Get rid of this implementation when share url is
+  // supported on Drive API v2.
+  return sender_->StartRequestWithRetry(
+      new GetResourceEntryRequest(sender_.get(),
+                                  wapi_url_generator_,
+                                  resource_id,
+                                  embed_origin,
+                                  base::Bind(&util::ParseShareUrlAndRun,
+                                             callback)));
 }
 
 CancelCallback DriveAPIService::GetAboutResource(
--- chrome/browser/drive/drive_api_service.h
+++ chrome/browser/drive/drive_api_service.h
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/google_apis/auth_service_interface.h"
 #include "chrome/browser/google_apis/auth_service_observer.h"
 #include "chrome/browser/google_apis/drive_api_url_generator.h"
+#include "chrome/browser/google_apis/gdata_wapi_url_generator.h"
 
 class GURL;
 class OAuth2TokenService;
@@ -46,6 +47,9 @@ class DriveAPIService : public DriveServiceInterface,
   // |base_url| is used to generate URLs for communication with the drive API.
   // |base_download_url| is used to generate URLs for downloading file from the
   // drive API.
+  // |wapi_base_url| is used to generate URLs for shared_url. Unfortunately,
+  // the share_url is not yet supported on Drive API v2, so as a fallback,
+  // we use GData WAPI.
   // |custom_user_agent| will be used for the User-Agent header in HTTP
   // requests issues through the service if the value is not empty.
   DriveAPIService(
@@ -54,6 +58,7 @@ class DriveAPIService : public DriveServiceInterface,
       base::TaskRunner* blocking_task_runner,
       const GURL& base_url,
       const GURL& base_download_url,
+      const GURL& wapi_base_url,
       const std::string& custom_user_agent);
   virtual ~DriveAPIService();
 
@@ -180,6 +185,7 @@ class DriveAPIService : public DriveServiceInterface,
   scoped_ptr<google_apis::RequestSender> sender_;
   ObserverList<DriveServiceObserver> observers_;
   google_apis::DriveApiUrlGenerator url_generator_;
+  google_apis::GDataWapiUrlGenerator wapi_url_generator_;
   const std::string custom_user_agent_;
 
   DISALLOW_COPY_AND_ASSIGN(DriveAPIService);
