1f741c7d57d78fdd090a24e1748d31cf8c408a7a
dbeam@chromium.org <dbeam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 20:46:26 +0000

    Remove restriction on number of times to show the generated card bubble for now while keeping the pref (to remember how many times the bubble has been shown) in place.
    TBR=estade@chromium.org
    BUG=266138
    
    Review URL: https://chromiumcodereview.appspot.com/20610003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215363 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_credit_card_bubble_controller.cc
+++ chrome/browser/ui/autofill/autofill_credit_card_bubble_controller.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/browser/ui/autofill/autofill_credit_card_bubble_controller.h"
 
+#include <climits>
 #include <string>
 
 #include "base/bind.h"
@@ -41,7 +42,9 @@ namespace autofill {
 
 namespace {
 
-static const int kMaxGeneratedCardTimesToShow = 5;
+// TODO(dbeam): add back a sensible limit once it's decided or remove
+// kMaxGeneratedCardTimesToShow if this behavior is finalized.
+static const int kMaxGeneratedCardTimesToShow = INT_MAX;
 static const char kWalletGeneratedCardLearnMoreLink[] =
     "http://support.google.com/wallet/bin/answer.py?hl=en&answer=2740044";
 
