f37a868fb55313849246b80da37f5764353ed31c
timvolodine@chromium.org <timvolodine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 04:44:01 +0000

    Enable Device Motion on Android by default.
    This CL:
    - enables Device Motion by default on the Android platform.
    - adds a disable-device-motion flag.
    
    BUG=135804
    
    Review URL: https://chromiumcodereview.appspot.com/21591002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215215 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/render_process_host_impl.cc
+++ content/browser/renderer_host/render_process_host_impl.cc
@@ -1030,6 +1030,9 @@ void RenderProcessHostImpl::PropagateBrowserCommandLineToRenderer(
     switches::kEnableWebGLDraftExtensions,
     switches::kTraceToConsole,
     switches::kEnableDeviceMotion,
+#if defined(OS_ANDROID)
+    switches::kDisableDeviceMotion,
+#endif
     // Please keep these in alphabetical order. Compositor switches here should
     // also be added to chrome/browser/chromeos/login/chrome_restart_request.cc.
     cc::switches::kBackgroundColorInsteadOfCheckerboard,
