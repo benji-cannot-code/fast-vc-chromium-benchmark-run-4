b349a3d3daab6577c1428942680a2121e79cd865
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-02 22:24:27 +0000

    Remove StringImpl::tryCreateUninitialized.
    Create or do not create. There is no try.
    
    The only client of tryCreateUninitialized is tryMakeString and the only client of _that_ does a RELEASE_ASSERT on the results anyway. So the cleanup cascades a little.
    
    It's also worth noting that our underlying system allocator is tcmalloc, which we have hard wired to crash on a failed allocation anyway.
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/21961002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155455 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/text/StringImpl.cpp
+++ third_party/WebKit/Source/wtf/text/StringImpl.cpp
@@ -26,7 +26,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "wtf/text/StringImpl.h"
 
-#include "wtf/ProcessID.h"
 #include "wtf/StdLibExtras.h"
 #include "wtf/WTFThreadData.h"
 #include "wtf/text/AtomicString.h"
@@ -37,6 +36,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifdef STRING_STATS
 #include "wtf/DataLog.h"
 #include "wtf/MainThread.h"
+#include "wtf/ProcessID.h"
 #include "wtf/RefCounted.h"
 #include <unistd.h>
 #endif
