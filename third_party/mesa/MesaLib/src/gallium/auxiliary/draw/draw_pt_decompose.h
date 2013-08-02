50aca95015ede6f7849ab2264b0739fe59c3c440
kbr@chromium.org <kbr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 01:27:44 +0000

    Deleted now-obsolete Mesa sources and removed exceptions from checklicenses.py.
    Tested with checklicenses.py run on Linux after removal.
        
    This CL is too large to upload. Committing by hand.
    
    BUG=238755
    TBR=phajdan.jr@chromium.org
    
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215185 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/mesa/MesaLib/src/gallium/auxiliary/draw/draw_pt_decompose.h
+++
@@ -1,8 +0,0 @@
-FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-#define LOCAL_VARS                           \
-   char *verts = (char *) vertices;          \
-   const boolean last_vertex_last =          \
-      !(draw->rasterizer->flatshade &&       \
-        draw->rasterizer->flatshade_first);
-
-#include "draw_decompose_tmp.h"
