b86b91fedcbc0bcc292b9065b18e06b252debdb6
petewil@chromium.org <petewil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 11:03:15 +0000

    Fix origin url for synced notifications
    BUG=236545
    
    Review URL: https://chromiumcodereview.appspot.com/20755003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215267 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/sync_notifier/synced_notification.cc
+++ chrome/browser/notifications/sync_notifier/synced_notification.cc
@@ -22,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/message_center/notification_types.h"
 
 namespace {
-const char kExtensionScheme[] = "chrome-extension://";
+const char kExtensionScheme[] = "synced-notification://";
 const char kDefaultSyncedNotificationScheme[] = "https:";
 
 // The name of our first synced notification service.
@@ -204,7 +204,7 @@ void SyncedNotification::Show(NotificationUIManager* notification_manager,
   string16 description = UTF8ToUTF16(GetDescription());
   string16 annotation = UTF8ToUTF16(GetAnnotation());
   // TODO(petewil): Eventually put the display name of the sending service here.
-  string16 display_source = UTF8ToUTF16(GetOriginUrl().spec());
+  string16 display_source = UTF8ToUTF16(GetAppId());
   string16 replace_key = UTF8ToUTF16(GetKey());
   string16 notification_heading = heading;
   string16 notification_text = text;
