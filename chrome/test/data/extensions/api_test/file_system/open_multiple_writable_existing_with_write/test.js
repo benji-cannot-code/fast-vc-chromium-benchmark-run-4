3a32629a9c628cdec063a558d17603a85bb70a12
sammc@chromium.org <sammc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 11:25:57 +0000

    Support choosing multiple files with fileSystem.chooseEntry.
    This adds acceptMultiple to ChooseEntryOptions, which when set to true,
    presents the user with a file chooser dialog that allows the selection
    of multiple files.
    
    BUG=159062
    
    Review URL: https://chromiumcodereview.appspot.com/18331017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215269 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/data/extensions/api_test/file_system/open_multiple_writable_existing_with_write/test.js
@@ -0,0 +1,17 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+chrome.test.runTests([
+  function openFile() {
+    chrome.fileSystem.chooseEntry(
+        {acceptsMultiple: true, type: 'openWritableFile'},
+        chrome.test.callbackPass(function(entries) {
+          chrome.test.assertEq(2, entries.length);
+          checkEntry(entries[0], 'open_existing1.txt', false, true);
+          checkEntry(entries[1], 'open_existing2.txt', false, true);
+        })
+    );
+  }
+]);
