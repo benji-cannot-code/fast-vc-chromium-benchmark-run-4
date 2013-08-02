6de8c110245f1c03bf1ad59af22431ea7aa117f7
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 08:55:55 +0000

    When quitting the last app, terminate asynchronously.
    This is because we check for shell windows before terminating, but
    closing shell windows will be asynchronous
    https://codereview.chromium.org/20243003/
    so we need to give it a chance to close before checking if they're all
    closed.
    
    BUG=168080
    
    Review URL: https://chromiumcodereview.appspot.com/21482002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215253 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/app_shim/extension_app_shim_handler_mac.cc
+++ apps/app_shim/extension_app_shim_handler_mac.cc
@@ -43,6 +43,13 @@ void ProfileLoadedCallback(base::Callback<void(Profile*)> callback,
   DCHECK_EQ(Profile::CREATE_STATUS_CREATED, status);
 }
 
+void TerminateIfNoShellWindows() {
+  bool shell_windows_left =
+      extensions::ShellWindowRegistry::IsShellWindowRegisteredInAnyProfile(0);
+  if (!shell_windows_left)
+    chrome::AttemptExit();
+}
+
 }  // namespace
 
 namespace apps {
@@ -114,10 +121,11 @@ void ExtensionAppShimHandler::Delegate::LaunchShim(
 }
 
 void ExtensionAppShimHandler::Delegate::MaybeTerminate() {
-  bool shell_windows_left =
-      extensions::ShellWindowRegistry::IsShellWindowRegisteredInAnyProfile(0);
-  if (!shell_windows_left)
-    chrome::AttemptExit();
+  // Post this to give ShellWindows a chance to remove themselves from the
+  // registry.
+  base::MessageLoop::current()->PostTask(
+      FROM_HERE,
+      base::Bind(&TerminateIfNoShellWindows));
 }
 
 ExtensionAppShimHandler::ExtensionAppShimHandler()
