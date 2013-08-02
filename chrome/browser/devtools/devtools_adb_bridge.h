746eddb29566af9bc6b3ef4a08cd0a53a28dc71d
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 09:05:40 +0000

    Extracted AdbWebSocket class into a separate file
    BUG=None
    
    TBR=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/21474002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215256 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/devtools_adb_bridge.h
+++ chrome/browser/devtools/devtools_adb_bridge.h
@@ -188,8 +188,6 @@ class DevToolsAdbBridge
   friend struct content::BrowserThread::DeleteOnThread<
       content::BrowserThread::UI>;
   friend class base::DeleteHelper<DevToolsAdbBridge>;
-  friend class AdbWebSocket;
-  friend class AgentHostDelegate;
 
   class RefCountedAdbThread : public base::RefCounted<RefCountedAdbThread> {
    public:
