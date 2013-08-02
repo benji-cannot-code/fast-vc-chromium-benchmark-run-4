60ae64fcf2e0a04d6d49b20d9ed0914e43fdf3bc
nhiroki@chromium.org <nhiroki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-02 04:14:54 +0000

    Revert 215174 "Create a refcounted usb context wrapper."
    Probably this causes UsbContextTest.GracefulShutdown failure on some bots.
    
    http://build.chromium.org/p/chromium.win/builders/XP%20Tests%20%283%29/builds/23652
    http://build.chromium.org/p/chromium.mac/builders/Mac10.6%20Tests%20%282%29/builds/41018
    
    > Create a refcounted usb context wrapper.
    > This will be also used when we introduce UsbDevice.
    > 
    > BUG=223817
    > 
    > Review URL: https://chromiumcodereview.appspot.com/20012002
    
    TBR=ikarienator@chromium.org
    
    Review URL: https://codereview.chromium.org/21720002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/usb/usb_api.h
+++ chrome/browser/extensions/api/usb/usb_api.h
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/base/io_buffer.h"
 
 class UsbDeviceHandle;
-class UsbService;
 
 namespace extensions {
 
@@ -73,21 +72,10 @@ class UsbFindDevicesFunction : public UsbAsyncApiFunction {
   virtual void AsyncWorkStart() OVERRIDE;
 
  private:
-  typedef scoped_ptr<std::vector<scoped_refptr<UsbDeviceHandle> > >
-      ScopedDeviceVector;
-
-  // Wait for GetDeviceService to return and start enumeration on FILE thread.
-  void EnumerateDevices(uint16_t vendor_id,
-                        uint16_t product_id,
-                        int interface_id,
-                        UsbService* service);
-  // Relay the result on IO thread to OnCompleted.
-  void OnEnumerationCompleted(ScopedDeviceVector devices);
-
-  // Create ApiResources and reply.
-  void OnCompleted(ScopedDeviceVector devices);
+  void OnCompleted();
 
   scoped_ptr<base::ListValue> result_;
+  std::vector<scoped_refptr<UsbDeviceHandle> > devices_;
   scoped_ptr<extensions::api::usb::FindDevices::Params> parameters_;
 };
 
