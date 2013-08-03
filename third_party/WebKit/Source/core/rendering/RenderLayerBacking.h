fcf680a33aceb5d94577d5e05db65c65b76807dd
shawnsingh@chromium.org <shawnsingh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-03 04:49:58 +0000

    Refactoring in preparation for Layer Squashing v2.
    A prerequisite for Layer Squashing version 2 is that multiple arbitrary
    subtrees of RenderLayers need to be able to contribute to the same
    GraphicsLayer. This patch creates a simple data structure that
    represents information needed for a partial RenderLayer subtree
    to paint into a GraphicsLayer.  This allows us to de-couple the
    RenderLayer tree from GraphicsLayer tree for more compositing
    flexibility.
    
    Subsequent patches (not required for layer squashing) will move the
    paintPhase from GraphicsLayer to RenderLayerBacking, and keep track
    of a "PaintTask" for each GraphicsLayer within RenderLayerBacking.
    
    BUG=261605
    
    Review URL: https://chromiumcodereview.appspot.com/18808005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155466 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderLayerBacking.h
+++ third_party/WebKit/Source/core/rendering/RenderLayerBacking.h
@@ -45,6 +45,20 @@ enum CompositingLayerType {
     ContainerCompositingLayer // layer with no backing store
 };
 
+
+// A GraphicsLayerPaintInfo contains all the info needed to paint a partial subtree of RenderLayers into a GraphicsLayer.
+struct GraphicsLayerPaintInfo {
+    RenderLayer* renderLayer;
+
+    IntRect compositedBounds;
+
+    IntSize offsetFromRenderer;
+
+    GraphicsLayerPaintingPhase paintingPhase;
+
+    bool isBackgroundLayer;
+};
+
 // RenderLayerBacking controls the compositing behavior for a single RenderLayer.
 // It holds the various GraphicsLayers, and makes decisions about intra-layer rendering
 // optimizations.
@@ -225,7 +239,7 @@ private:
 
     bool shouldClipCompositedBounds() const;
 
-    void paintIntoLayer(const GraphicsLayer*, GraphicsContext*, const IntRect& paintDirtyRect, PaintBehavior, GraphicsLayerPaintingPhase);
+    void doPaintTask(GraphicsLayerPaintInfo&, GraphicsContext*, const IntRect& clip);
 
     static CSSPropertyID graphicsLayerToCSSProperty(AnimatedPropertyID);
     static AnimatedPropertyID cssToGraphicsLayerProperty(CSSPropertyID);
