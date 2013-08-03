1afc429e9155e760fa46a96694b4e9908cf10846
sgurun@chromium.org <sgurun@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 08:16:31 +0000

    Combine ViewAndroidDelegate code.
    The ViewAndroidDelegate code in AutofillManagerDelegate and ContentViewCore
    has a lot of redundancies. Combine.
    
    Review URL: https://chromiumcodereview.appspot.com/21969002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215497 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwAutofillManagerDelegate.java
+++ android_webview/java/src/org/chromium/android_webview/AwAutofillManagerDelegate.java
@@ -12,7 +12,6 @@ import android.view.ViewGroup;
 import org.chromium.base.CalledByNative;
 import org.chromium.base.JNINamespace;
 import org.chromium.content.browser.ContentViewCore;
-import org.chromium.ui.ViewAndroidDelegate;
 import org.chromium.ui.autofill.AutofillPopup;
 import org.chromium.ui.autofill.AutofillSuggestion;
 
@@ -24,7 +23,6 @@ public class AwAutofillManagerDelegate {
     private final int mNativeAwAutofillManagerDelegate;
     private AutofillPopup mAutofillPopup;
     private ViewGroup mContainerView;
-    private double mDIPScale;
     private ContentViewCore mContentViewCore;
 
     @CalledByNative
@@ -36,10 +34,9 @@ public class AwAutofillManagerDelegate {
         mNativeAwAutofillManagerDelegate = nativeAwAutofillManagerDelegate;
     }
 
-    public void init(ContentViewCore contentViewCore, double DIPScale) {
+    public void init(ContentViewCore contentViewCore) {
         mContentViewCore = contentViewCore;
         mContainerView = contentViewCore.getContainerView();
-        mDIPScale = DIPScale;
     }
 
     @CalledByNative
@@ -51,7 +48,7 @@ public class AwAutofillManagerDelegate {
         if (mAutofillPopup == null) {
             mAutofillPopup = new AutofillPopup(
                 mContentViewCore.getContext(),
-                getViewAndroidDelegate(),
+                mContentViewCore.getViewAndroidDelegate(),
                 new AutofillPopup.AutofillPopupDelegate() {
                     public void requestHide() { }
                     public void suggestionSelected(int listIndex) {
@@ -69,38 +66,6 @@ public class AwAutofillManagerDelegate {
             return;
         mAutofillPopup.hide();
         mAutofillPopup = null;
-    }
-
-    private ViewAndroidDelegate getViewAndroidDelegate() {
-        return new ViewAndroidDelegate() {
-            @Override
-            public View acquireAnchorView() {
-                View anchorView = new View(mContentViewCore.getContext());
-                mContainerView.addView(anchorView);
-                return anchorView;
-            }
-
-            @Override
-            @SuppressWarnings("deprecation")  // AbsoluteLayout.LayoutParams
-            public void setAnchorViewPosition(
-                    View view, float x, float y, float width, float height) {
-                assert(view.getParent() == mContainerView);
-
-                int leftMargin = (int)Math.round(x * mDIPScale);
-                int topMargin = (int)mContentViewCore.getRenderCoordinates().getContentOffsetYPix()
-                        + (int)Math.round(y * mDIPScale);
-
-                android.widget.AbsoluteLayout.LayoutParams lp =
-                    new android.widget.AbsoluteLayout.LayoutParams((int)width,
-                        (int)height, leftMargin, topMargin);
-                view.setLayoutParams(lp);
-            }
-
-            @Override
-            public void releaseAnchorView(View anchorView) {
-                mContainerView.removeView(anchorView);
-            }
-        };
     }
 
     @CalledByNative
--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1648,7 +1648,7 @@ public class AwContents {
     @CalledByNative
     private void setAwAutofillManagerDelegate(AwAutofillManagerDelegate delegate) {
         mAwAutofillManagerDelegate = delegate;
-        delegate.init(mContentViewCore, mDIPScale);
+        delegate.init(mContentViewCore);
     }
 
     @CalledByNative
