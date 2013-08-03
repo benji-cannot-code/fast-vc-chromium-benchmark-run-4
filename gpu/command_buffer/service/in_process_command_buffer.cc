000361c52a851522a4252c78160a6864110e2208
etienneb@chromium.org <etienneb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 01:52:28 +0000

    Fix potential uninitialized variable. R=sievers@chromium.org, apatrick@chromium.org BUG=
    Review URL: https://chromiumcodereview.appspot.com/21914005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215451 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/command_buffer/service/in_process_command_buffer.cc
+++ gpu/command_buffer/service/in_process_command_buffer.cc
@@ -303,7 +303,7 @@ bool InProcessCommandBuffer::Initialize(
   share_group_id_ = share_group_id;
 
   base::WaitableEvent completion(true, false);
-  bool result;
+  bool result = false;
   base::Callback<bool(void)> init_task =
       base::Bind(&InProcessCommandBuffer::InitializeOnGpuThread,
                  base::Unretained(this),
@@ -454,7 +454,7 @@ bool InProcessCommandBuffer::InitializeOnGpuThread(
 
 void InProcessCommandBuffer::Destroy() {
   base::WaitableEvent completion(true, false);
-  bool result;
+  bool result = false;
   base::Callback<bool(void)> destroy_task = base::Bind(
       &InProcessCommandBuffer::DestroyOnGpuThread, base::Unretained(this));
   QueueTask(
