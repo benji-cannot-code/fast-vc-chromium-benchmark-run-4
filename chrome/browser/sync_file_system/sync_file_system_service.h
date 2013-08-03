b540be6968f66a0ccd89340f59a69d1e4d83dfa8
yoz@chromium.org <yoz@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 00:21:08 +0000

    Reland 213912 - Stop sending EXTENSION_UNLOADED notification when uninstalling disabled extensions.
    Most listeners don't want to know about uninstalling disabled extensions. Those that do can listen to EXTENSION_UNINSTALLED instead.
    
    Bugs involving some of these listeners were fixed in r214657 and https://codereview.chromium.org/21443002/
    
    BUG=263216
    TBR=kinuko@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/21428004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215419 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/sync_file_system_service.h
+++ chrome/browser/sync_file_system/sync_file_system_service.h
@@ -136,6 +136,8 @@ class SyncFileSystemService
   void HandleExtensionInstalled(const content::NotificationDetails& details);
   void HandleExtensionUnloaded(int type,
                                const content::NotificationDetails& details);
+  void HandleExtensionUninstalled(int type,
+                                  const content::NotificationDetails& details);
   void HandleExtensionEnabled(int type,
                               const content::NotificationDetails& details);
 
