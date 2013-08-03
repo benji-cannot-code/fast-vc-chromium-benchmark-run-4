3cf48965e517aadd83f1da627866c2f5478ee2e3
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 01:39:38 +0000

    [telemetry] Automatic hash file creation and upload to Cloud Storage.
    BUG=223667
    TEST=None.
    
    Review URL: https://chromiumcodereview.appspot.com/21684002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215445 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/PRESUBMIT.py
+++ tools/perf/PRESUBMIT.py
@@ -5,14 +5,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import os
 import sys
 
+
 PYLINT_BLACKLIST = []
 PYLINT_DISABLED_WARNINGS = ['R0923', 'R0201', 'E1101']
+
 
 def _CommonChecks(input_api, output_api):
   results = []
   old_sys_path = sys.path
   try:
-    sys.path = [os.path.join('..', 'telemetry')] + sys.path
+    sys.path = [os.path.join(os.pardir, 'telemetry')] + sys.path
     results.extend(input_api.canned_checks.RunPylint(
         input_api, output_api,
         black_list=PYLINT_BLACKLIST,
@@ -21,10 +23,12 @@ def _CommonChecks(input_api, output_api):
     sys.path = old_sys_path
   return results
 
+
 def CheckChangeOnUpload(input_api, output_api):
   report = []
   report.extend(_CommonChecks(input_api, output_api))
   return report
+
 
 def CheckChangeOnCommit(input_api, output_api):
   report = []
