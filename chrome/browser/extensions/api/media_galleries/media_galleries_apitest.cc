cfa2eac4050c5bd0d11bb224c310bbee17d3b120
tommycli@chromium.org <tommycli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 00:09:29 +0000

    Media Galleries API: Copy-to API test.
    Adds an end-to-end API test for the Copy-to capability in File API.
    
    R=vandebo, thestig
    BUG=NONE
    
    Review URL: https://chromiumcodereview.appspot.com/20574006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215412 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/media_galleries/media_galleries_apitest.cc
+++ chrome/browser/extensions/api/media_galleries/media_galleries_apitest.cc
@@ -3,13 +3,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/bind.h"
+#include "base/bind_helpers.h"
 #include "base/command_line.h"
+#include "base/files/scoped_temp_dir.h"
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
+#include "chrome/browser/browser_process.h"
 #include "chrome/browser/extensions/extension_apitest.h"
 #include "chrome/browser/extensions/extension_process_manager.h"
 #include "chrome/browser/extensions/extension_system.h"
 #include "chrome/browser/extensions/platform_app_browsertest_util.h"
+#include "chrome/browser/media_galleries/media_file_system_registry.h"
+#include "chrome/browser/media_galleries/media_galleries_preferences.h"
 #include "chrome/browser/media_galleries/media_galleries_test_util.h"
 #include "chrome/browser/storage_monitor/storage_info.h"
 #include "chrome/browser/storage_monitor/storage_monitor.h"
@@ -43,6 +49,31 @@ class MediaGalleriesExperimentalApiTest : public ExtensionApiTest {
 };
 
 }  // namespace
+
+// This function is to ensure at least one (fake) media gallery exists for
+// testing platforms with no default media galleries, such as CHROMEOS.
+void MakeFakeMediaGalleryForTest(Profile* profile, const base::FilePath& path) {
+  base::RunLoop runloop;
+  chrome::StorageMonitor::GetInstance()->EnsureInitialized(
+      runloop.QuitClosure());
+  runloop.Run();
+
+  chrome::MediaGalleriesPreferences* preferences =
+      g_browser_process->media_file_system_registry()->GetPreferences(profile);
+
+  chrome::MediaGalleryPrefInfo gallery_info;
+  ASSERT_FALSE(preferences->LookUpGalleryByPath(path, &gallery_info));
+  preferences->AddGallery(gallery_info.device_id,
+                          gallery_info.path,
+                          false /* user_added */,
+                          gallery_info.volume_label,
+                          gallery_info.vendor_name,
+                          gallery_info.model_name,
+                          gallery_info.total_size_in_bytes,
+                          gallery_info.last_attach_time);
+
+  content::RunAllPendingInMessageLoop();
+}
 
 class MediaGalleriesPlatformAppBrowserTest : public PlatformAppBrowserTest {
  protected:
@@ -86,7 +117,7 @@ IN_PROC_BROWSER_TEST_F(MediaGalleriesPlatformAppBrowserTest,
   chrome::EnsureMediaDirectoriesExists media_directories;
   ASSERT_TRUE(RunPlatformAppTest("api_test/media_galleries/no_access"))
       << message_;
-  RunSecondTestPhase(UTF8ToUTF16(base::StringPrintf(
+  RunSecondTestPhase(base::UTF8ToUTF16(base::StringPrintf(
       kTestGalleries, media_directories.num_galleries())));
 }
 
@@ -108,8 +139,29 @@ IN_PROC_BROWSER_TEST_F(MediaGalleriesPlatformAppBrowserTest,
   chrome::EnsureMediaDirectoriesExists media_directories;
   ASSERT_TRUE(RunPlatformAppTest("api_test/media_galleries/read_access"))
       << message_;
-  RunSecondTestPhase(UTF8ToUTF16(base::StringPrintf(
+  RunSecondTestPhase(base::UTF8ToUTF16(base::StringPrintf(
       kTestGalleries, media_directories.num_galleries())));
+}
+
+IN_PROC_BROWSER_TEST_F(MediaGalleriesPlatformAppBrowserTest,
+                       MediaGalleriesCopyTo) {
+  chrome::EnsureMediaDirectoriesExists media_directories;
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  MakeFakeMediaGalleryForTest(browser()->profile(), temp_dir.path());
+  ASSERT_TRUE(RunPlatformAppTest("api_test/media_galleries/copy_to_access"))
+      << message_;
+}
+
+IN_PROC_BROWSER_TEST_F(MediaGalleriesPlatformAppBrowserTest,
+                       MediaGalleriesCopyToNoAccess) {
+  chrome::EnsureMediaDirectoriesExists media_directories;
+  base::ScopedTempDir temp_dir;
+  ASSERT_TRUE(temp_dir.CreateUniqueTempDir());
+  MakeFakeMediaGalleryForTest(browser()->profile(), temp_dir.path());
+  ASSERT_TRUE(RunPlatformAppTest(
+      "api_test/media_galleries/copy_to_access/no_access"))
+      << message_;
 }
 
 IN_PROC_BROWSER_TEST_F(MediaGalleriesPlatformAppBrowserTest,
