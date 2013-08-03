78f42a59354c44be3c8bb8784830649f2eb2831c
sosa@chromium.org <sosa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 00:54:48 +0000

    Remove all traces of deprecated Get/SetReleaseTrack calls.
    In early M30 we deprecated this API and are planning on removing
    the calling end in the update engine. This CL removes the browser-side
    stubs/callbacks and any trace of them left in the code.
    
    BUG=chromium:221850
    TEST=try + build
    
    Review URL: https://chromiumcodereview.appspot.com/21015010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215431 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_policy_decoder_chromeos.cc
+++ chrome/browser/chromeos/policy/device_policy_decoder_chromeos.cc
@@ -400,7 +400,7 @@ void DecodeAutoUpdatePolicies(const em::ChromeDeviceSettingsProto& policy,
       // have to pass the channel in here, only ping the update engine to tell
       // it to fetch the channel from the policy.
       chromeos::DBusThreadManager::Get()->GetUpdateEngineClient()->
-          SetReleaseTrack(channel);
+          SetChannel(channel, false);
     }
     if (container.has_release_channel_delegated()) {
       policies->Set(key::kChromeOsReleaseChannelDelegated,
