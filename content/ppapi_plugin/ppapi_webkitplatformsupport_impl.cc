a6714ecc94d5bae33ee528b061f3e458233524fa
kjyoun@google.com <kjyoun@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-03 02:17:13 +0000

    Fix build breakage on chrome-for-tv
    Fixed a typo in an argument list.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/21700002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215460 0039d316-1c4b-4281-b951-d872f2087c98

--- content/ppapi_plugin/ppapi_webkitplatformsupport_impl.cc
+++ content/ppapi_plugin/ppapi_webkitplatformsupport_impl.cc
@@ -20,14 +20,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/WebKit/public/platform/win/WebSandboxSupport.h"
 #elif defined(OS_MACOSX)
 #include "third_party/WebKit/public/platform/mac/WebSandboxSupport.h"
+#elif defined(OS_ANDROID)
+#include "third_party/WebKit/public/platform/android/WebSandboxSupport.h"
 #elif defined(OS_POSIX)
-#if !defined(OS_ANDROID)
 #include "content/common/child_process_sandbox_support_impl_linux.h"
-#include "third_party/icu/source/common/unicode/utf16.h"
-#endif
 #include "third_party/WebKit/public/platform/linux/WebFontFamily.h"
 #include "third_party/WebKit/public/platform/linux/WebSandboxSupport.h"
-
+#include "third_party/icu/source/common/unicode/utf16.h"
 #endif
 
 using WebKit::WebSandboxSupport;
@@ -49,6 +48,8 @@ class PpapiWebKitPlatformSupportImpl::SandboxSupport
 #elif defined(OS_MACOSX)
   virtual bool loadFont(
       NSFont* srcFont, CGFontRef* out, uint32_t* fontID);
+#elif defined(OS_ANDROID)
+  // Empty class.
 #elif defined(OS_POSIX)
   virtual void getFontFamilyForCharacters(
       const WebKit::WebUChar* characters,
@@ -99,29 +100,7 @@ bool PpapiWebKitPlatformSupportImpl::SandboxSupport::loadFont(
 
 #elif defined(OS_ANDROID)
 
-// TODO(jrg): resolve (and implement?) PPAPI SandboxSupport for Android.
-
-void
-PpapiWebKitPlatformSupportImpl::SandboxSupport::getFontFamilyForCharacters(
-    const WebUChar* characters,
-    size_t num_characters,
-    const char* preferred_locale,
-    WebKit::WebFontFamily* family) {
-  NOTIMPLEMENTED();
-}
-
-void
-PpapiWebKitPlatformSupportImpl::SandboxSupport::getFontFamilyForCharacter(
-    WebUChar32* character,
-    const char* preferred_locale,
-    WebKit::WebFontFamily* family) {
-  NOTIMPLEMENTED();
-}
-
-void PpapiWebKitPlatformSupportImpl::SandboxSupport::getRenderStyleForStrike(
-    const char* family, int sizeAndStyle, WebKit::WebFontRenderStyle* out) {
-  NOTIMPLEMENTED();
-}
+// Empty class.
 
 #elif defined(OS_POSIX)
 
