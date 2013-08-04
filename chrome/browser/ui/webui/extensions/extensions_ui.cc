af77505b28a826e6f86bb7608779cd321ce4b75a
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-04 00:19:24 +0000

    When bookmarking some chrome:// urls display their associated favicons correctly.
    When you bookmark either chrome://settings or chrome://extensions today,
    we don't handle those two urls well and just display the default (blank document)
    favicon for them. But they have their own favicons, which we should display
    correctly, for example, on Bookmarks Bar.
    
    BUG=146613
    TEST=go to chrome://settings, bookmark it (ctrl+d or clicking on the star button),
    make sure you have Bookmarks Bar enabled (just for display purposes),
    observe that we now display the wrench icon for it. Do the same with
    chrome://extension and observe that the corresponding icon is also displayed
    correctly on Bookmarks Bar.
    R=dbeam@chromium.org
    
    Review URL: https://codereview.chromium.org/21679002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215522 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/extensions/extensions_ui.cc
+++ chrome/browser/ui/webui/extensions/extensions_ui.cc
@@ -15,6 +15,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_ui.h"
 #include "content/public/browser/web_ui_data_source.h"
 #include "grit/browser_resources.h"
+#include "grit/theme_resources.h"
+#include "ui/base/resource/resource_bundle.h"
 
 #if defined(OS_CHROMEOS)
 #include "chrome/browser/ui/webui/extensions/chromeos/kiosk_apps_handler.h"
@@ -74,7 +76,13 @@ ExtensionsUI::ExtensionsUI(content::WebUI* web_ui) : WebUIController(web_ui) {
   content::WebUIDataSource::Add(profile, source);
 }
 
-ExtensionsUI::~ExtensionsUI() {
+ExtensionsUI::~ExtensionsUI() {}
+
+// static
+base::RefCountedMemory* ExtensionsUI::GetFaviconResourceBytes(
+    ui::ScaleFactor scale_factor) {
+  ui::ResourceBundle& rb = ui::ResourceBundle::GetSharedInstance();
+  return rb.LoadDataResourceBytesForScale(IDR_EXTENSIONS_FAVICON, scale_factor);
 }
 
 }  // namespace extensions
--- chrome/browser/ui/webui/extensions/extensions_ui.h
+++ chrome/browser/ui/webui/extensions/extensions_ui.h
@@ -6,7 +6,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_UI_WEBUI_EXTENSIONS_EXTENSIONS_UI_H_
 #define CHROME_BROWSER_UI_WEBUI_EXTENSIONS_EXTENSIONS_UI_H_
 
+#include "base/basictypes.h"
 #include "content/public/browser/web_ui_controller.h"
+#include "ui/base/layout.h"
+
+namespace base {
+class RefCountedMemory;
+}
 
 namespace extensions {
 
@@ -14,6 +20,12 @@ class ExtensionsUI : public content::WebUIController {
  public:
   explicit ExtensionsUI(content::WebUI* web_ui);
   virtual ~ExtensionsUI();
+
+  static base::RefCountedMemory* GetFaviconResourceBytes(
+      ui::ScaleFactor scale_factor);
+
+ private:
+  DISALLOW_COPY_AND_ASSIGN(ExtensionsUI);
 };
 
 }  // namespace extensions
