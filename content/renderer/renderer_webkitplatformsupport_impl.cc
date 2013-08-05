e2759bbe01afaebd3cf4ff267ab6894c3e3f39ad
eseidel@chromium.org <eseidel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-05 23:59:40 +0000

    Remove SandboxSupport::getFontFamilyForCharacters which is now dead code
    There is no functional change in this CL, only removing dead code.
    
    This is the final part (4 of 4) for moving this API from a string of UTF16
    to a single UChar32 codepoint.
    
    Part 1: https://codereview.chromium.org/20732003
    Part 2: https://codereview.chromium.org/20883004
    Part 3: https://codereview.chromium.com/21513007
    
    BUG=266214
    
    Review URL: https://chromiumcodereview.appspot.com/22259003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215734 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/renderer_webkitplatformsupport_impl.cc
+++ content/renderer/renderer_webkitplatformsupport_impl.cc
@@ -183,11 +183,6 @@ class RendererWebKitPlatformSupportImpl::SandboxSupport
       CGFontRef* container,
       uint32* font_id);
 #elif defined(OS_POSIX)
-  virtual void getFontFamilyForCharacters(
-      const WebKit::WebUChar* characters,
-      size_t num_characters,
-      const char* preferred_locale,
-      WebKit::WebFontFamily* family);
   virtual void getFontFamilyForCharacter(
       WebKit::WebUChar32 character,
       const char* preferred_locale,
@@ -580,18 +575,6 @@ bool RendererWebKitPlatformSupportImpl::SandboxSupport::loadFont(
 // whole class for android.
 
 #elif defined(OS_POSIX)
-
-void
-RendererWebKitPlatformSupportImpl::SandboxSupport::getFontFamilyForCharacters(
-    const WebKit::WebUChar* characters,
-    size_t num_characters,
-    const char* preferred_locale,
-    WebKit::WebFontFamily* family) {
-  DCHECK(num_characters <= 2);
-  WebKit::WebUChar32 c;
-  U16_GET(characters, 0, 0, num_characters, c);
-  getFontFamilyForCharacter(c, preferred_locale, family);
-}
 
 void
 RendererWebKitPlatformSupportImpl::SandboxSupport::getFontFamilyForCharacter(
