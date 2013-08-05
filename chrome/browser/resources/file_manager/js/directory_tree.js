d1528005a0309d471f9870dbb5b42e5313550232
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-05 19:17:29 +0000

    Remove FileManager.resolvePath.
    This rewrites the code to handle file selection for saving to reduce the
    dependency to the FileSystem from FileManager.
    This is the preparation to support multi file system.
    
    BUG=268233
    TEST=Ran browser_tests --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*" and tested manually.
    
    Review URL: https://chromiumcodereview.appspot.com/21869004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215655 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_tree.js
+++ chrome/browser/resources/file_manager/js/directory_tree.js
@@ -89,15 +89,6 @@ DirectoryTreeUtil.updateSubElementsFromList = function(
 };
 
 /**
- * Returns true if the given directory entry is dummy.
- * @param {DirectoryEntry|Object} dirEntry DirectoryEntry to be checked.
- * @return {boolean} True if the given directory entry is dummy.
- */
-DirectoryTreeUtil.isDummyEntry = function(dirEntry) {
-  return !('createReader' in dirEntry);
-};
-
-/**
  * Finds a parent directory of the {@code path} from the {@code items}, and
  * invokes the DirectoryItem.selectPath() of the found directory.
  *
@@ -163,7 +154,7 @@ DirectoryTreeUtil.addAndRemoveDriveSpecialDirs = function(entries) {
 DirectoryTreeUtil.updateSubDirectories = function(
     item, dm, successCallback, opt_errorCallback) {
   // Tries to retrieve new entry if the cached entry is dummy.
-  if (DirectoryTreeUtil.isDummyEntry(item.entry)) {
+  if (util.isFakeDirectoryEntry(item.entry)) {
     // Fake Drive root.
     dm.resolveDirectory(
         item.fullPath,
@@ -171,7 +162,7 @@ DirectoryTreeUtil.updateSubDirectories = function(
           item.dirEntry_ = entry;
 
           // If the retrieved entry is dummy again, returns with an error.
-          if (DirectoryTreeUtil.isDummyEntry(entry)) {
+          if (util.isFakeDirectoryEntry(entry)) {
             if (opt_errorCallback)
               opt_errorCallback();
             return;
