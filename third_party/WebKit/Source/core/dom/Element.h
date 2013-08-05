1fa2183664d4b873582dbb268aa4089bb316b3ca
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-05 04:04:38 +0000

    Revert "Unify ways to cache named item in HTMLCollections"
    This reverts commit 5705911a4713a1370967ffe9630cb1a9ed6fd2b0.
    
    It makes fast/events/window-onerror-02.html crash.
    BUG=265009
    TBR=abarth
    
    Review URL: https://codereview.chromium.org/21679004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155503 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Element.h
+++ third_party/WebKit/Source/core/dom/Element.h
@@ -664,6 +664,9 @@ protected:
     virtual void didRecalcStyle(StyleChange);
     virtual PassRefPtr<RenderStyle> customStyleForRenderer();
 
+    virtual bool shouldRegisterAsNamedItem() const { return false; }
+    virtual bool shouldRegisterAsExtraNamedItem() const { return false; }
+
     void clearTabIndexExplicitlyIfNeeded();
     void setTabIndexExplicitly(short);
     virtual bool supportsFocus() const OVERRIDE;
@@ -712,7 +715,6 @@ private:
     void updateId(const AtomicString& oldId, const AtomicString& newId);
     void updateId(TreeScope*, const AtomicString& oldId, const AtomicString& newId);
     void updateName(const AtomicString& oldName, const AtomicString& newName);
-    void updateName(TreeScope*, const AtomicString& oldName, const AtomicString& newName);
     void updateLabel(TreeScope*, const AtomicString& oldForAttributeValue, const AtomicString& newForAttributeValue);
 
     void scrollByUnits(int units, ScrollGranularity);
@@ -754,6 +756,9 @@ private:
     unsigned rareDataChildIndex() const;
 
     SpellcheckAttributeState spellcheckAttributeState() const;
+
+    void updateNamedItemRegistration(const AtomicString& oldName, const AtomicString& newName);
+    void updateExtraNamedItemRegistration(const AtomicString& oldName, const AtomicString& newName);
 
     void unregisterNamedFlowContentNode();
 
