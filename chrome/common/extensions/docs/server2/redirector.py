14a1f1253ddd66e4db193340858888e81c4566b4
jaredshumway94@gmail.com <jaredshumway94@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 01:19:52 +0000

    docserver redirector bug fix
    the path to public/redirects.json is now correctly generated during cron
    runs.
    
    removed some logic related to channels that is now unneeded.
    
    BUG=268459
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22217004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215756 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/redirector.py
+++ chrome/common/extensions/docs/server2/redirector.py
@@ -6,7 +6,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 import posixpath
 from urlparse import urlsplit
 
-from branch_utility import BranchUtility
 from file_system import FileNotFoundError
 from third_party.json_schema_compiler.json_parse import Parse
 
@@ -15,7 +14,7 @@ class Redirector(object):
     self._root_path = root_path
     self._file_system = file_system
     self._cache = compiled_fs_factory.Create(
-      lambda _, rules: Parse(rules), Redirector)
+        lambda _, rules: Parse(rules), Redirector)
 
   def Redirect(self, host, path):
     ''' Check if a path should be redirected, first according to host
@@ -60,18 +59,12 @@ class Redirector(object):
     if path and path[0] == 'chrome':
       path.pop(0)
 
-    for channel in BranchUtility.GetAllChannelNames():
-      if channel in path:
-        path.remove(channel)
-        path.insert(0, channel)
-        break
-
     return 'https://developer.chrome.com/' + posixpath.join(*path)
 
   def Cron(self):
     ''' Load files during a cron run.
     '''
-    for root, dirs, files in self._file_system.Walk(self._root_path + '/'):
+    for root, dirs, files in self._file_system.Walk(self._root_path):
       if 'redirects.json' in files:
         self._cache.GetFromFile('%s/redirects.json' % posixpath.join(
-            self._root_path, root))
+            self._root_path, root).rstrip('/'))
