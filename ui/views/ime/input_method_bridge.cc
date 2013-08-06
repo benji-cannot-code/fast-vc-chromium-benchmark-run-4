7ead4bae45fc06ec0fcff969206f1923fc2c578a
yukishiino@chromium.org <yukishiino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 05:45:14 +0000

    Fixes twice-IME-committement on blur.
    This CL clears composition data in IME in InputMethodBridge::OnBlur.
    The cause was that ConfirmComposition called in InputMethodBridge::OnBlue doesn't clear IME's composition, and it will be committed again in host_->SetFocusedTextInputClient(NULL) in case of IMM32.
    
    BUG=265928
    TEST=Test manually.
    
    Review URL: https://chromiumcodereview.appspot.com/21874003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215803 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/ime/input_method_bridge.cc
+++ ui/views/ime/input_method_bridge.cc
@@ -46,7 +46,10 @@ void InputMethodBridge::OnFocus() {
 }
 
 void InputMethodBridge::OnBlur() {
-  ConfirmCompositionText();
+  if (HasCompositionText()) {
+    ConfirmCompositionText();
+    host_->CancelComposition(this);
+  }
 
   if (host_->GetTextInputClient() == this)
     host_->SetFocusedTextInputClient(NULL);
