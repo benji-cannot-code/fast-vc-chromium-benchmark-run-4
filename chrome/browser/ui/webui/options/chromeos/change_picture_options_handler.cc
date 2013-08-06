3f11df331e6a42ce71c02552be85d72c660997c6
kinaba@chromium.org <kinaba@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 17:26:45 +0000

    Change the meaning of SelectFileDialog.support_drive: it means Drive-aware callers.
    Previously, what the |support_drive| flag means is just to show
    Google Drive tab in the file dialog; it doesn't change other
    behaviors.
    
    Now, we are going to enable the "Google Drive" tab on Chrome OS
    for all call sites of the dialog, by adding a feature to Drive client
    to provide local snapshot so that callers need not care about Drive.
    
    On the other hand, the "create snapshot" trick is unnecessary for
    clients like Packaged app or extensions that implement fileBrowserHandler,
    which access files through HTML5 FileSystem API.
    So, for those clients I want to leave the |support_drive| flag to
    indicate that no special handling is needed. Here's the summary:
    
    Before:
    - support_drive, folder-dialog : show Drive tab. No special handling.
    - support_drive, open-dialog : show Drive tab. Create local snapshot.
    - support_drive, save-dialog : show Drive tab. No special handling.
    - !support_drive, folder-dialog : hide Drive tab.
    - !support_drive, open-dialog : hide Drive tab. (this mode is not used)
    - !support_drive, save-dialog : hide Drive tab.
    
    After:
    - support_drive, folder-dialog : show Drive tab. No special handling.
    - support_drive, open-dialog : show Drive tab. No special handling.
    - support_drive, save-dialog : show Drive tab. No special handling.
    - !support_drive, folder-dialog : *HIDE* Drive tab. (this is not supported yet)
    - !support_drive, open-dialog : Show Drive tab. Create local snapshot.
    - !support_drive, save-dialog : *HIDE* Drive tab. (VERY SOON switch to => "Show Drive tab. Create local snapshot.")
    
    BUG=140425
    
    Review URL: https://chromiumcodereview.appspot.com/22165002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215922 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
+++ chrome/browser/ui/webui/options/chromeos/change_picture_options_handler.cc
@@ -64,7 +64,6 @@ ui::SelectFileDialog::FileTypeInfo GetUserImageFileTypeInfo() {
   file_type_info.extension_description_overrides[0] =
       l10n_util::GetStringUTF16(IDS_IMAGE_FILES);
 
-  file_type_info.support_drive = true;
   return file_type_info;
 }
 
