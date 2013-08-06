54d14945c759e616974bb3501e5b9fc9721b36a0
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-06 02:01:51 +0000

    Revert "Implement DeferredAnimatableValue"
    This reverts commit 3d9f10fb57cc63ea5c96f092f67f8f517860c4e2 (https://codereview.chromium.org/19367002/)
    
    Since we now take the snapshot values outside of the style resolve stage
    we won't be needing DeferredAnimatableValue.
    
    BUG=232273
    
    Review URL: https://chromiumcodereview.appspot.com/22187002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155563 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimatableValue.cpp
+++ third_party/WebKit/Source/core/animation/AnimatableValue.cpp
@@ -32,7 +32,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "core/animation/AnimatableValue.h"
 #include "core/animation/AnimatableNeutral.h"
-#include "core/animation/DeferredAnimatableValue.h"
 #include <algorithm>
 
 namespace WebCore {
@@ -41,12 +40,6 @@ const AnimatableValue* AnimatableValue::neutralValue()
 {
     static AnimatableNeutral* neutralSentinelValue = AnimatableNeutral::create().leakRef();
     return neutralSentinelValue;
-}
-
-const AnimatableValue* AnimatableValue::deferredSnapshotValue()
-{
-    static DeferredAnimatableValue* deferredAnimatableValueSentinel = DeferredAnimatableValue::create().leakRef();
-    return deferredAnimatableValueSentinel;
 }
 
 PassRefPtr<AnimatableValue> AnimatableValue::interpolate(const AnimatableValue* left, const AnimatableValue* right, double fraction)
