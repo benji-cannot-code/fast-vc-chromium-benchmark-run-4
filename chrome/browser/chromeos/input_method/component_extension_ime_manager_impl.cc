e8112a15cc48003ed065f0a2d5f9b704bb2eac67
nona@chromium.org <nona@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 23:58:11 +0000

    Revive Chewing IME for traditional Chinese Input.
    We replaced ibus-mozc-chewing with new extension based input method but it is not acceptable for traditional Chinese users.
    This CL remove new input method and revive ibus-mozc-chewing.
    This CL only contains minimum changes because this is for M29 merges.
    
    BUG=266313
    TEST=Manually confirmed that the old input method is working on lumpy.
    
    Review URL: https://chromiumcodereview.appspot.com/20843010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216037 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/component_extension_ime_manager_impl.cc
+++ chrome/browser/chromeos/input_method/component_extension_ime_manager_impl.cc
@@ -44,11 +44,6 @@ struct WhitelistedComponentExtensionIME {
     "/usr/share/chromeos-assets/input_methods/nacl_mozc",
   },
   {
-    // Google Chinese Input (zhuyin)
-    "goedamlknlnjaengojinmfgpmdjmkooo",
-    "/usr/share/chromeos-assets/input_methods/zhuyin",
-  },
-  {
     // Google Chinese Input (pinyin)
     "nmblnjkfdkabgdofidlkienfnnbjhnab",
     "/usr/share/chromeos-assets/input_methods/pinyin",
@@ -73,11 +68,6 @@ struct WhitelistedComponentExtensionIME {
     // Open-sourced Pinyin Chinese Input Method.
     "cpgalbafkoofkjmaeonnfijgpfennjjn",
     "/usr/share/chromeos-assets/input_methods/pinyin",
-  },
-  {
-    // Open-sourced Zhuyin Chinese Input Method.
-    "ekbifjdfhkmdeeajnolmgdlmkllopefi",
-    "/usr/share/chromeos-assets/input_methods/zhuyin",
   },
   {
     // Open-sourced Cangjie Chinese Input Method.
