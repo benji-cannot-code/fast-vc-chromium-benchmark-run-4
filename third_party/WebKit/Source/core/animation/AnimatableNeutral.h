34af240cb55376e258300ce0858bfe65094adc10
timloh@chromium.org <timloh@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-06 09:36:40 +0000

    Minor cleanups in AnimatableValue code
    A few minor cleanups to the AnimatableValue classes. Basically this
    makes the interface more like the rest of Blink.
    
    - Make isNeutral non-virtual and add isNumber, isUnknown (note that we
        currently can only detect subclass type with dynamic_cast since
        m_type is protected)
    - Add toAnimatableNumber
    - Make AnimatableValue::m_type and AnimatableUnknown::m_value const
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22274006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/AnimatableNeutral.h
+++ third_party/WebKit/Source/core/animation/AnimatableNeutral.h
@@ -46,8 +46,6 @@ public:
         return 0;
     }
 
-    virtual bool isNeutral() const OVERRIDE { return true; }
-
 protected:
     static PassRefPtr<AnimatableNeutral> create() { return adoptRef(new AnimatableNeutral()); }
 
--- third_party/WebKit/Source/core/animation/AnimatableUnknown.h
+++ third_party/WebKit/Source/core/animation/AnimatableUnknown.h
@@ -66,7 +66,7 @@ private:
         ASSERT(m_value);
     }
 
-    RefPtr<CSSValue> m_value;
+    const RefPtr<CSSValue> m_value;
 };
 
 } // namespace WebCore
