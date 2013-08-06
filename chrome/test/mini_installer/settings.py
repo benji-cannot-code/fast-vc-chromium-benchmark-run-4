3ff79262b6e2fe603f4b45aa35ea5c8e6b1db6da
sukolsak@chromium.org <sukolsak@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 19:02:38 +0000

    Initial commit for the Automated Installer Testing Framework. The design documentation is available at https://docs.google.com/a/google.com/document/d/1iK0INUnaTbeEMXHLWpyRch2eyp-Uxb7ckppO7X-onLY/
    NOTRY=True
    BUG=264859
    TEST=
    1) Uninstall Chrome.
    2) Put mini_installer.exe in the same folder as test_installer.py.
    3) Run "python test_installer.py config\config.config".
    4) The script will install Chrome and then uninstall Chrome. In other words, it will go from clean -> installed -> clean. At each state, it will check that a registry entry for Chrome exists (or doesn't exist). You should see "Passed" at each state.
    
    Review URL: https://chromiumcodereview.appspot.com/20578004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215940 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/test/mini_installer/settings.py
@@ -0,0 +1,9 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+PRINT_TEST_PREFIX     = "Testing: "
+PRINT_COMMAND_PREFIX  = "  * Command: "
+PRINT_STATE_PREFIX    = "  * State: "
+PRINT_VERIFIER_PREFIX = "    - Verify that "
---
+++ chrome/test/mini_installer/verifier.py
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+import registry_verifier
+
+def Verify(property):
+  """Verifies that the current machine states match the property object."""
+  for verifier_name, value in property.iteritems():
+    if verifier_name == 'RegistryEntries':
+      registry_verifier.VerifyRegistryEntries(value)
+    else:
+      # TODO(sukolsak): Implement other verifiers
+      # TODO(sukolsak): Use unittest framework instead of exceptions.
+      raise Exception('Unknown verifier')
