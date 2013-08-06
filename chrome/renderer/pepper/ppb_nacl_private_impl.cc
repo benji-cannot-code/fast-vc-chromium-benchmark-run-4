e231fd5e9a5c2709bd50af6825542ebef71729cb
jvoung@chromium.org <jvoung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 07:46:18 +0000

    PNaCl on-demand installs: Make a separate async IPC to check if PNaCl is installed.
    Previously, there was a single synchronous IPC that would
    check if PNaCl is installed, then install if not installed,
    as well as open the individual files read-only.
    
    That sync IPC would block the UI if it needed to install PNaCl.
    
    Instead separate checking from the opening a file read-only.
    Have a separate IPC that is async, so that the UI is
    not blocked, and progress events could eventually be sent.
    Some basic plumbing of progress events is set up, but not
    yet wired up to JS.
    
    Order of events is:
    
    CheckInstalled
    * if Installed, reply w/ success
    * if not Installed {
        send progress event indicated install start
        try install
        (may send more progress events)
        send progress of install success or failure
      }
    
    
    BUG=252760
    TEST=unit_tests --gtest_filter=NaClFileHostTest*  (and manual)
    
    Review URL: https://chromiumcodereview.appspot.com/19863003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215844 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/pepper/ppb_nacl_private_impl.cc
+++ chrome/renderer/pepper/ppb_nacl_private_impl.cc
@@ -42,10 +42,11 @@ base::LazyInstance<scoped_refptr<PnaclTranslationResourceHost> >
     g_pnacl_resource_host = LAZY_INSTANCE_INITIALIZER;
 
 static bool InitializePnaclResourceHost() {
+  // Must run on the main thread.
+  content::RenderThread* render_thread = content::RenderThread::Get();
+  if (!render_thread)
+    return false;
   if (!g_pnacl_resource_host.Get()) {
-    content::RenderThread* render_thread = content::RenderThread::Get();
-    if (!render_thread)
-      return false;
     g_pnacl_resource_host.Get() = new PnaclTranslationResourceHost(
         render_thread->GetIOMessageLoopProxy());
     render_thread->AddFilter(g_pnacl_resource_host.Get());
@@ -202,6 +203,19 @@ int32_t BrokerDuplicateHandle(PP_FileHandle source_handle,
 #endif
 }
 
+int32_t EnsurePnaclInstalled(PP_Instance instance,
+                             PP_CompletionCallback callback) {
+  ppapi::thunk::EnterInstance enter(instance, callback);
+  if (enter.failed())
+    return enter.retval();
+  if (!InitializePnaclResourceHost())
+    return enter.SetResult(PP_ERROR_FAILED);
+  g_pnacl_resource_host.Get()->EnsurePnaclInstalled(
+      instance,
+      enter.callback());
+  return enter.SetResult(PP_OK_COMPLETIONPENDING);
+}
+
 PP_FileHandle GetReadonlyPnaclFD(const char* filename) {
   IPC::PlatformFileForTransit out_fd = IPC::InvalidPlatformFileForTransit();
   IPC::Sender* sender = content::RenderThread::Get();
@@ -211,11 +225,9 @@ PP_FileHandle GetReadonlyPnaclFD(const char* filename) {
           &out_fd))) {
     return base::kInvalidPlatformFileValue;
   }
-
   if (out_fd == IPC::InvalidPlatformFileForTransit()) {
     return base::kInvalidPlatformFileValue;
   }
-
   base::PlatformFile handle =
       IPC::PlatformFileForTransitToPlatformFile(out_fd);
   return handle;
@@ -256,8 +268,6 @@ int32_t GetNexeFd(PP_Instance instance,
   if (!InitializePnaclResourceHost())
     return enter.SetResult(PP_ERROR_FAILED);
 
-  IPC::Sender* sender = content::RenderThread::Get();
-  DCHECK(sender);
   base::Time last_modified_time;
   // If FromString fails, it doesn't touch last_modified_time and we just send
   // the default-constructed null value.
@@ -347,6 +357,7 @@ const PPB_NaCl_Private nacl_interface = {
   &UrandomFD,
   &Are3DInterfacesDisabled,
   &BrokerDuplicateHandle,
+  &EnsurePnaclInstalled,
   &GetReadonlyPnaclFD,
   &CreateTemporaryFile,
   &GetNexeFd,
