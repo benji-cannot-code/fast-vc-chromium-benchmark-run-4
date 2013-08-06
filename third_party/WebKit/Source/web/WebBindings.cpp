d783742b10ac1de4c1407e7188c9b23285069091
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-06 15:24:13 +0000

    Revert "Use V8 implementation of TypedArrays and DataView in Blink" (r155611).
    This change seems to be causing intermittent failures of webaudio tests. E.g.,
    http://build.chromium.org/p/chromium.webkit/builders/WebKit%20Linux/builds/15532
    
    BUG=259731
    TBR=dslomov@chromium.org,jochen,kbr@chromium.org
    
    Review URL: https://codereview.chromium.org/22267007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155615 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebBindings.cpp
+++ third_party/WebKit/Source/web/WebBindings.cpp
@@ -32,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "config.h"
 #include "WebBindings.h"
 
+#include "V8ArrayBufferView.h"
 #include "V8Element.h"
 #include "V8Range.h"
 #include "WebArrayBufferView.h"
@@ -42,7 +43,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/V8DOMWrapper.h"
 #include "bindings/v8/V8NPUtils.h"
 #include "bindings/v8/custom/V8ArrayBufferCustom.h"
-#include "bindings/v8/custom/V8ArrayBufferViewCustom.h"
 #include "bindings/v8/npruntime_impl.h"
 #include "bindings/v8/npruntime_priv.h"
 #include "core/dom/Range.h"
