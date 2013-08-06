13efad85e66a1a7a57c18a0c52a9b6ffc648e575
senorblanco@chromium.org <senorblanco@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-06 19:13:58 +0000

    Unprefix webkitImageSmoothingEnabled -> imageSmoothingEnabled.
    Most tests were modified to test the unprefixed version. One (canvas-imageSmoothingEnabled.html) was copied to a new name to test the prefixed version. Since the prefixed implementation is just a wrapper to the unprefixed implementation, that should be enough coverage.
    
    BUG=
    R=dglazkov@chromium.org
    
    Review URL: https://codereview.chromium.org/20527005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155624 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.h
+++ third_party/WebKit/Source/core/html/canvas/CanvasRenderingContext2D.h
@@ -223,8 +223,8 @@ public:
     LineCap getLineCap() const { return state().m_lineCap; }
     LineJoin getLineJoin() const { return state().m_lineJoin; }
 
-    bool webkitImageSmoothingEnabled() const;
-    void setWebkitImageSmoothingEnabled(bool);
+    bool imageSmoothingEnabled() const;
+    void setImageSmoothingEnabled(bool);
 
     PassRefPtr<Canvas2DContextAttributes> getContextAttributes() const;
 
