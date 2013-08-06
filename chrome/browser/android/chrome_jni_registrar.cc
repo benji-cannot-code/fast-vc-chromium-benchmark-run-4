5d6ba01e3a53d27561d97e189b2bff741007dc6a
aruslan@chromium.org <aruslan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 08:26:19 +0000

    [rAc Android] Remove the old dialog implementation.
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/21205007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215852 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/chrome_jni_registrar.cc
+++ chrome/browser/android/chrome_jni_registrar.cc
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/signin/android_profile_oauth2_token_service.h"
 #include "chrome/browser/speech/tts_android.h"
 #include "chrome/browser/sync/profile_sync_service_android.h"
-#include "chrome/browser/ui/android/autofill/autofill_dialog_view_android.h"
 #include "chrome/browser/ui/android/autofill/autofill_popup_view_android.h"
 #include "chrome/browser/ui/android/chrome_http_auth_handler.h"
 #include "chrome/browser/ui/android/javascript_app_modal_dialog_android.h"
@@ -56,8 +55,6 @@ static base::android::RegistrationMethod kChromeRegisteredMethods[] = {
   { "AndroidProfileOAuth2TokenService",
     AndroidProfileOAuth2TokenService::Register },
   { "ApplicationLifetime", RegisterApplicationLifetimeAndroid },
-  { "AutofillDialog",
-    autofill::AutofillDialogViewAndroid::RegisterAutofillDialogViewAndroid },
   { "AutofillPopup",
     autofill::AutofillPopupViewAndroid::RegisterAutofillPopupViewAndroid },
   { "CertificateViewer", RegisterCertificateViewer },
--- chrome/chrome_browser.gypi
+++ chrome/chrome_browser.gypi
@@ -3371,7 +3371,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
           'type': 'none',
           'sources': [
             'android/java/src/org/chromium/chrome/browser/ApplicationLifetime.java',
-            'android/java/src/org/chromium/chrome/browser/autofill/AutofillDialogGlue.java',
             'android/java/src/org/chromium/chrome/browser/autofill/AutofillPopupGlue.java',
             'android/java/src/org/chromium/chrome/browser/autofill/PersonalDataManager.java',
             'android/java/src/org/chromium/chrome/browser/CertificateViewer.java',
