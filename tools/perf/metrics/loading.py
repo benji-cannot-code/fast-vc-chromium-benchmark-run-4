dab50994b6069872b3256df9c8b292f8bb8bc980
qyearsley@chromium.org <qyearsley@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 22:14:07 +0000

    Make metrics/loading.py a subclass of Metric
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22336005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215995 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/metrics/loading.py
+++ tools/perf/metrics/loading.py
@@ -3,14 +3,25 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-def AddResultsForTab(tab, results):
-  load_timings = tab.EvaluateJavaScript('window.performance.timing')
-  load_time_ms = (
-    float(load_timings['loadEventStart']) -
-    load_timings['navigationStart'])
-  dom_content_loaded_time_ms = (
-    float(load_timings['domContentLoadedEventStart']) -
-    load_timings['navigationStart'])
-  results.Add('load_time', 'ms', load_time_ms)
-  results.Add('dom_content_loaded_time', 'ms',
-              dom_content_loaded_time_ms)
+from metrics import Metric
+
+class LoadingMetric(Metric):
+  """A metric for page loading time based entirely on window.performance"""
+
+  def Start(self, page, tab):
+    raise NotImplementedError()
+
+  def Stop(self, page, tab):
+    raise NotImplementedError()
+
+  def AddResults(self, tab, results):
+    load_timings = tab.EvaluateJavaScript('window.performance.timing')
+    load_time_ms = (
+      float(load_timings['loadEventStart']) -
+      load_timings['navigationStart'])
+    dom_content_loaded_time_ms = (
+      float(load_timings['domContentLoadedEventStart']) -
+      load_timings['navigationStart'])
+    results.Add('load_time', 'ms', load_time_ms)
+    results.Add('dom_content_loaded_time', 'ms',
+                dom_content_loaded_time_ms)
