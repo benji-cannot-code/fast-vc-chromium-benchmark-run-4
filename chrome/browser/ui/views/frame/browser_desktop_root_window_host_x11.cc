fae1c755a3f73b0b4065ab963bd43b466805430d
kochi@chromium.org <kochi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 03:25:01 +0000

    Revert 215769 "linux_aura: Implement the static part of the dbus..."
    > linux_aura: Implement the static part of the dbus menu for Unity.
    > 
    > In GTK, we get a menu in the Unity bar for free as long as we have a
    > GtkMenuBar object in our GtkWindow. linux_aura doesn't use gtk widgets
    > in the interface. So opportunistically dlopen libdbusmenu-glib and
    > manually construct a hidden DbusmenuMenuItem* object tree.
    > 
    > This only ports the static parts of the menus; it doesn't port the
    > History menu, which changes based on user actions.
    > 
    > BUG=265560
    > 
    > Review URL: https://chromiumcodereview.appspot.com/21187006
    
    TBR=erg@chromium.org
    
    Review URL: https://codereview.chromium.org/22326003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215780 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/frame/browser_desktop_root_window_host_x11.cc
+++ chrome/browser/ui/views/frame/browser_desktop_root_window_host_x11.cc
@@ -11,12 +11,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 BrowserDesktopRootWindowHostX11::BrowserDesktopRootWindowHostX11(
     views::internal::NativeWidgetDelegate* native_widget_delegate,
     views::DesktopNativeWidgetAura* desktop_native_widget_aura,
-    const gfx::Rect& initial_bounds,
-    BrowserView* browser_view)
+    const gfx::Rect& initial_bounds)
     : DesktopRootWindowHostX11(native_widget_delegate,
-                               desktop_native_widget_aura,
-                               initial_bounds),
-      browser_view_(browser_view) {
+                                 desktop_native_widget_aura,
+                                 initial_bounds) {
 }
 
 BrowserDesktopRootWindowHostX11::~BrowserDesktopRootWindowHostX11() {
@@ -40,28 +38,6 @@ bool BrowserDesktopRootWindowHostX11::UsesNativeSystemMenu() const {
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-// BrowserDesktopRootWindowHostX11,
-//     views::DesktopRootWindowHostX11 implementation:
-
-aura::RootWindow* BrowserDesktopRootWindowHostX11::Init(
-    aura::Window* content_window,
-    const views::Widget::InitParams& params) {
-  aura::RootWindow* root_window = views::DesktopRootWindowHostX11::Init(
-      content_window, params);
-
-  // We have now created our backing X11 window. We now need to (possibly)
-  // alert Unity that there's a menu bar attached to it.
-  global_menu_bar_x11_.reset(new GlobalMenuBarX11(browser_view_, this));
-
-  return root_window;
-}
-
-void BrowserDesktopRootWindowHostX11::CloseNow() {
-  global_menu_bar_x11_.reset();
-  DesktopRootWindowHostX11::CloseNow();
-}
-
-////////////////////////////////////////////////////////////////////////////////
 // BrowserDesktopRootWindowHost, public:
 
 // static
@@ -73,7 +49,6 @@ BrowserDesktopRootWindowHost*
         BrowserView* browser_view,
         BrowserFrame* browser_frame) {
   return new BrowserDesktopRootWindowHostX11(native_widget_delegate,
-                                             desktop_native_widget_aura,
-                                             initial_bounds,
-                                             browser_view);
+                                               desktop_native_widget_aura,
+                                               initial_bounds);
 }
