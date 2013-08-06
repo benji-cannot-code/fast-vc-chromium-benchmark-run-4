cf5a423979a3371691320b6716998f18d53e0e51
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 00:16:36 +0000

    Fix constants.
    TBR=rsleevi@chromium.org
    
    BUG=None.
    
    Review URL: https://codereview.chromium.org/22338003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215739 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/feedback/feedback_data.cc
+++ chrome/browser/feedback/feedback_data.cc
@@ -28,9 +28,9 @@ const char kMultilineIndicatorString[] = "<multiline>\n";
 const char kMultilineStartString[] = "---------- START ----------\n";
 const char kMultilineEndString[] = "---------- END ----------\n\n";
 
-std::string LogsToString(SystemLogsMap* sys_info) {
+std::string LogsToString(FeedbackData::SystemLogsMap* sys_info) {
   std::string syslogs_string;
-  for (SystemLogsMap::const_iterator it = sys_info->begin();
+  for (FeedbackData::SystemLogsMap::const_iterator it = sys_info->begin();
       it != sys_info->end(); ++it) {
     std::string key = it->first;
     std::string value = it->second;
@@ -51,7 +51,8 @@ std::string LogsToString(SystemLogsMap* sys_info) {
   return syslogs_string;
 }
 
-void ZipLogs(SystemLogsMap* sys_info, std::string* compressed_logs) {
+void ZipLogs(FeedbackData::SystemLogsMap* sys_info,
+             std::string* compressed_logs) {
   DCHECK(compressed_logs);
   std::string logs_string = LogsToString(sys_info);
   if (!feedback_util::ZipString(logs_string, compressed_logs)) {
@@ -60,6 +61,12 @@ void ZipLogs(SystemLogsMap* sys_info, std::string* compressed_logs) {
 }
 
 }  // namespace
+
+// static
+const char FeedbackData::kScreensizeHeightKey[] = "ScreensizeHeight";
+// static
+const char FeedbackData::kScreensizeWidthKey[] = "ScreensizeWidth";
+
 
 FeedbackData::FeedbackData() : profile_(NULL),
                                feedback_page_data_complete_(false),
@@ -85,18 +92,18 @@ void FeedbackData::OnFeedbackPageDataComplete() {
 }
 
 void FeedbackData::set_sys_info(
-    scoped_ptr<SystemLogsMap> sys_info) {
+    scoped_ptr<FeedbackData::SystemLogsMap> sys_info) {
   if (sys_info.get())
     CompressSyslogs(sys_info.Pass());
 }
 
 void FeedbackData::CompressSyslogs(
-    scoped_ptr<SystemLogsMap> sys_info) {
+    scoped_ptr<FeedbackData::SystemLogsMap> sys_info) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
   // We get the pointer first since base::Passed will nullify the scoper, hence
   // it's not safe to use <scoper>.get() as a parameter to PostTaskAndReply.
-  SystemLogsMap* sys_info_ptr = sys_info.get();
+  FeedbackData::SystemLogsMap* sys_info_ptr = sys_info.get();
   std::string* compressed_logs_ptr = new std::string;
   scoped_ptr<std::string> compressed_logs(compressed_logs_ptr);
   BrowserThread::PostBlockingPoolTaskAndReply(
@@ -111,7 +118,7 @@ void FeedbackData::CompressSyslogs(
 }
 
 void FeedbackData::OnCompressLogsComplete(
-    scoped_ptr<SystemLogsMap> sys_info,
+    scoped_ptr<FeedbackData::SystemLogsMap> sys_info,
     scoped_ptr<std::string> compressed_logs) {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::UI));
 
