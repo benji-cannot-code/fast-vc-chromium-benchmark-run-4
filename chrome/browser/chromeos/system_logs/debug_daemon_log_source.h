3089faefb5f8c0e51bc34df1a1d5ee07b7f97e9b
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-06 22:23:55 +0000

    Revert r215683, r215739 and r215736 The feedback component app is moved to M31; revert all related changes from M30. This CL reverts, https://codereview.chromium.org/22295003/ https://codereview.chromium.org/22338003/ https://codereview.chromium.org/22253004/
    TBR=rkc@chromium.org
    BUG=269113
    
    Review URL: https://codereview.chromium.org/22267012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@215999 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/system_logs/debug_daemon_log_source.h
+++ chrome/browser/chromeos/system_logs/debug_daemon_log_source.h
@@ -13,8 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/weak_ptr.h"
 #include "chrome/browser/chromeos/system_logs/system_logs_fetcher.h"
 
-class Profile;
-
 namespace chromeos {
 
 // Gathers log data from Debug Daemon.
@@ -43,15 +41,8 @@ class DebugDaemonLogSource : public SystemLogsSource {
                          const KeyValueMap& logs);
 
   // Read the contents of the specified user logs files and adds it to
-  // the response parameter.
-  static void ReadUserLogFiles(
-      const KeyValueMap& user_log_files,
-      const std::vector<Profile*>& last_used_profiles,
-      SystemLogsResponse* response);
-
-  // Merge the responses from ReadUserLogFiles into the main response dict and
-  // call RequestComplete to indicate that the user log files read is complete.
-  void MergeResponse(SystemLogsResponse* response);
+  // the response.
+  void ReadUserLogFiles(const KeyValueMap& user_log_files);
 
   // Sends the data to the callback_ when all the requests are completed
   void RequestCompleted();
--- chrome/browser/extensions/api/feedback_private/feedback_private_api.h
+++ chrome/browser/extensions/api/feedback_private/feedback_private_api.h
@@ -6,16 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_FEEDBACK_PRIVATE_FEEDBACK_PRIVATE_API_H_
 #define CHROME_BROWSER_EXTENSIONS_API_FEEDBACK_PRIVATE_FEEDBACK_PRIVATE_API_H_
 
+#include "chrome/browser/extensions/api/feedback_private/feedback_service.h"
 #include "chrome/browser/extensions/api/profile_keyed_api_factory.h"
 #include "chrome/browser/extensions/extension_function.h"
 #include "chrome/common/extensions/api/feedback_private.h"
-#include "ui/gfx/rect.h"
 
 namespace extensions {
 
 class FeedbackService;
-
-using extensions::api::feedback_private::SystemInformation;
 
 class FeedbackPrivateAPI : public ProfileKeyedAPI {
  public:
@@ -25,8 +23,7 @@ class FeedbackPrivateAPI : public ProfileKeyedAPI {
   FeedbackService* GetService() const;
   void RequestFeedback(const std::string& description_template,
                        const std::string& category_tag,
-                       const GURL& page_url,
-                       const gfx::Rect& screen_size);
+                       const GURL& page_url);
 
   // ProfileKeyedAPI implementation.
   static ProfileKeyedAPIFactory<FeedbackPrivateAPI>* GetFactoryInstance();
@@ -41,19 +38,6 @@ class FeedbackPrivateAPI : public ProfileKeyedAPI {
 
   Profile* const profile_;
   FeedbackService* service_;
-};
-
-// Feedback strings.
-class FeedbackPrivateGetStringsFunction : public SyncExtensionFunction {
- public:
-  DECLARE_EXTENSION_FUNCTION("feedbackPrivate.getStrings",
-                             FEEDBACKPRIVATE_GETSTRINGS)
-
- protected:
-  virtual ~FeedbackPrivateGetStringsFunction() {}
-
-  // SyncExtensionFunction overrides.
-  virtual bool RunImpl() OVERRIDE;
 };
 
 class FeedbackPrivateGetUserEmailFunction : public SyncExtensionFunction {
@@ -77,8 +61,7 @@ class FeedbackPrivateGetSystemInformationFunction
   virtual bool RunImpl() OVERRIDE;
 
  private:
-  void OnCompleted(
-      const std::vector<linked_ptr<SystemInformation> >& sys_info);
+  void OnCompleted(const SystemInformationList& sys_info);
 };
 
 class FeedbackPrivateSendFeedbackFunction : public AsyncExtensionFunction {
