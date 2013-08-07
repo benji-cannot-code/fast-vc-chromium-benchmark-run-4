f0cd2d5ef547f59a59387eebdaad99e1cdbe095f
hfung@chromium.org <hfung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 21:12:06 +0000

    Enable Google https URLs to get ZeroSuggestions.
    BUG=237284
    
    Review URL: https://chromiumcodereview.appspot.com/22311002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216274 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/zero_suggest_provider.cc
+++ chrome/browser/autocomplete/zero_suggest_provider.cc
@@ -20,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/autocomplete/history_url_provider.h"
 #include "chrome/browser/autocomplete/search_provider.h"
 #include "chrome/browser/autocomplete/url_prefix.h"
+#include "chrome/browser/google/google_util.h"
 #include "chrome/browser/metrics/variations/variations_http_header_provider.h"
 #include "chrome/browser/omnibox/omnibox_field_trial.h"
 #include "chrome/browser/profiles/profile.h"
@@ -187,12 +188,7 @@ ZeroSuggestProvider::~ZeroSuggestProvider() {
 }
 
 bool ZeroSuggestProvider::ShouldRunZeroSuggest(const GURL& url) const {
-  if (!url.is_valid())
-    return false;
-
-  // Do not query non-http URLs. There will be no useful suggestions for https
-  // or chrome URLs.
-  if (url.scheme() != chrome::kHttpScheme)
+  if (!ShouldSendURL(url))
     return false;
 
   // Don't run if there's no profile or in incognito mode.
@@ -216,6 +212,18 @@ bool ZeroSuggestProvider::ShouldRunZeroSuggest(const GURL& url) const {
     return false;
   }
   return true;
+}
+
+bool ZeroSuggestProvider::ShouldSendURL(const GURL& url) const {
+  if (!url.is_valid())
+    return false;
+
+  // Only allow HTTP URLs or Google HTTPS URLs (including Google search
+  // result pages).  For the latter case, Google was already sent the HTTPS
+  // URLs when requesting the page, so the information is just re-sent.
+  return (url.scheme() == chrome::kHttpScheme) ||
+      google_util::IsGoogleDomainUrl(url, google_util::ALLOW_SUBDOMAIN,
+                                     google_util::ALLOW_NON_STANDARD_PORTS);
 }
 
 void ZeroSuggestProvider::FillResults(
