55773d2ef35f34da97e798f0ba20fb5656848bf2
avayvod@chromium.org <avayvod@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 20:57:48 +0000

    Revert "A flag to enable Cast support lab for Android" Not launching in this release.
    This reverts commit 7ea5f2e765cc2d968e6879635e4b4e6f2f8e1359.
    
    Conflicts:
    	chrome/browser/about_flags.cc
    
    BUG=178307
    R=janx@chromium.org
    
    Review URL: https://codereview.chromium.org/22524006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216270 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/chrome_switches.cc
+++ chrome/common/chrome_switches.cc
@@ -1492,10 +1492,6 @@ const char kFakeCloudPolicyType[]           = "fake-cloud-policy-type";
 
 // Uses the tablet specific UI components when available.
 const char kTabletUI[]                      = "tablet-ui";
-
-// Enables support for playing videos on Google Cast devices.
-const char kEnableCast[]                    = "enable-cast";
-
 #endif
 
 #if defined(USE_ASH)
