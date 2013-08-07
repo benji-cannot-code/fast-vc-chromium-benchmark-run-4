24c72dc95c8eccf0c892d6bf922f6378308564b2
rsleevi@chromium.org <rsleevi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 00:28:59 +0000

    Add an enterprise policy preference for requiring revocation checks (hard fail) for local anchors
    BUG=258642
    
    Review URL: https://chromiumcodereview.appspot.com/18959003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216045 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/policy/configuration_policy_pref_store_unittest.cc
+++ chrome/browser/policy/configuration_policy_pref_store_unittest.cc
@@ -247,6 +247,8 @@ INSTANTIATE_TEST_CASE_P(
                       prefs::kDisableSSLRecordSplitting),
         PolicyAndPref(key::kEnableOnlineRevocationChecks,
                       prefs::kCertRevocationCheckingEnabled),
+        PolicyAndPref(key::kRequireOnlineRevocationChecksForLocalAnchors,
+                      prefs::kCertRevocationCheckingRequiredLocalAnchors),
         PolicyAndPref(key::kDisableAuthNegotiateCnameLookup,
                       prefs::kDisableAuthNegotiateCnameLookup),
         PolicyAndPref(key::kEnableAuthNegotiatePort,
