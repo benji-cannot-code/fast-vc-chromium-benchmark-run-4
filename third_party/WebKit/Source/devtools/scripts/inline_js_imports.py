2bafb34e5696cad65a88be09df8da0e70e1eaf84
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-07 14:52:53 +0000

    DevTools: Use rjsmin instead of jsmin for js minification
    This patch adds rjsmin instead of jsmin for js minification. The reason
    for this change is that both bundled jsmin implementation and its updated
    version seem to be failing on some tricky regexps. This results in
    invalid javascript. That was the reason why minification was not enabled for CodeMirrorTextEditor (this patch enables it)
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22393005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155695 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/scripts/inline_js_imports.py
+++ third_party/WebKit/Source/devtools/scripts/inline_js_imports.py
@@ -35,7 +35,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 from cStringIO import StringIO
 
-import jsmin
+import rjsmin
 import os.path
 import re
 import sys
@@ -78,7 +78,7 @@ def main(argv):
         os.remove(output_file_name)
     output_file = open(output_file_name, 'w')
     if not no_minify:
-        output_script = jsmin.jsmin(output_script)
+        output_script = rjsmin.jsmin(output_script)
     output_file.write(output_script)
     output_file.close()
 
