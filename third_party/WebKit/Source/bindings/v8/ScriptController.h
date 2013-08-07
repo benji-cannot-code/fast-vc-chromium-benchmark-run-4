7217e0c58669daeddfea1bafab7151212f80803a
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-07 09:26:20 +0000

    Allow sites to enable detailed 'window.onerror' handlers for cross-domain scripts.
    When triggering 'window.onerror', we currently sanitize the contents of
    the error if the script in which the error occurred isn't from the same
    origin as the document that loaded the script. Other major browsers (IE,
    Firefox[1], and WebKit[2]) bypass this sanitization step iff the script
    is served with appropriate 'Access-Control-Allow-Origin' headers that
    grant the loading document access to the script's contents. Clever
    developers agree[3] that this is a reasonable solution.
    
    This patch aligns our behavior with those browsers by passing the
    CORS state of a script through V8 so that it's available to us when
    exceptions are thrown.
    
    Note that this patch does not address the case of scripts imported
    into Workers. Our behavior there is already poor; it will require a bit
    more rework to correctly handle the basic case before moving on
    to implementing CORS support.
    
    Intent to Implement discussion at [4].
    
    [1]: https://bugzilla.mozilla.org/show_bug.cgi?id=696301
    [2]: https://bugs.webkit.org/show_bug.cgi?id=70574
    [3]: http://www.schemehostport.com/2011/10/x-script-origin-we-hardly-knew-ye.html
    [4]: https://groups.google.com/a/chromium.org/d/msg/blink-dev/Li61lfcbWws/NuUUNofRciMJ
    
    BUG=159566
    
    Review URL: https://chromiumcodereview.appspot.com/19596004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155670 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ScriptController.h
+++ third_party/WebKit/Source/bindings/v8/ScriptController.h
@@ -35,12 +35,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "bindings/v8/ScriptInstance.h"
 #include "bindings/v8/ScriptValue.h"
 
-#include <v8.h>
+#include "core/loader/CrossOriginAccessControl.h"
 #include "wtf/Forward.h"
 #include "wtf/HashMap.h"
 #include "wtf/RefCounted.h"
 #include "wtf/Vector.h"
 #include "wtf/text/TextPosition.h"
+#include <v8.h>
 
 struct NPObject;
 
@@ -82,7 +83,7 @@ public:
     ScriptValue executeScript(const String& script, bool forceUserGesture = false);
 
     // Evaluate JavaScript in the main world.
-    ScriptValue executeScriptInMainWorld(const ScriptSourceCode&);
+    ScriptValue executeScriptInMainWorld(const ScriptSourceCode&, AccessControlStatus = NotSharableCrossOrigin);
 
     // Executes JavaScript in an isolated world. The script gets its own global scope,
     // its own prototypes for intrinsic JavaScript objects (String, Array, and so-on),
@@ -97,7 +98,7 @@ public:
     // Returns true if argument is a JavaScript URL.
     bool executeScriptIfJavaScriptURL(const KURL&);
 
-    v8::Local<v8::Value> compileAndRunScript(const ScriptSourceCode&);
+    v8::Local<v8::Value> compileAndRunScript(const ScriptSourceCode&, AccessControlStatus = NotSharableCrossOrigin);
 
     v8::Local<v8::Value> callFunction(v8::Handle<v8::Function>, v8::Handle<v8::Object>, int argc, v8::Handle<v8::Value> argv[]);
     ScriptValue callFunctionEvenIfScriptDisabled(v8::Handle<v8::Function>, v8::Handle<v8::Object>, int argc, v8::Handle<v8::Value> argv[]);
--- third_party/WebKit/Source/bindings/v8/V8ScriptRunner.h
+++ third_party/WebKit/Source/bindings/v8/V8ScriptRunner.h
@@ -27,6 +27,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef V8ScriptRunner_h
 #define V8ScriptRunner_h
 
+#include "core/loader/CrossOriginAccessControl.h"
 #include "wtf/PassOwnPtr.h"
 #include "wtf/text/TextPosition.h"
 #include "wtf/text/WTFString.h"
@@ -42,7 +43,7 @@ public:
     // For the following methods, the caller sites have to hold
     // a HandleScope and a ContextScope.
     static PassOwnPtr<v8::ScriptData> precompileScript(v8::Handle<v8::String>, CachedScript*);
-    static v8::Local<v8::Script> compileScript(v8::Handle<v8::String>, const String&, const TextPosition&, v8::ScriptData*, v8::Isolate*);
+    static v8::Local<v8::Script> compileScript(v8::Handle<v8::String>, const String&, const TextPosition&, v8::ScriptData*, v8::Isolate*, AccessControlStatus = SharableCrossOrigin);
     static v8::Local<v8::Value> runCompiledScript(v8::Handle<v8::Script>, ScriptExecutionContext*);
     static v8::Local<v8::Value> compileAndRunInternalScript(v8::Handle<v8::String>, v8::Isolate*, const String& = String(), const TextPosition& = TextPosition(), v8::ScriptData* = 0);
     static v8::Local<v8::Value> callInternalFunction(v8::Handle<v8::Function>, v8::Handle<v8::Object>, int argc, v8::Handle<v8::Value> args[], v8::Isolate*);
