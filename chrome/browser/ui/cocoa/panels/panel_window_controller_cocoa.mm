0ce62765579bb4625822ec0c42bfdd6588dfccb1
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 00:16:22 +0000

    Add the logic to bypass the web contents view hiding logic for Mac panels.
    BUG=none
    TEST=none
    R=dimich@chromium.org
    
    Review URL: https://codereview.chromium.org/22500002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216042 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/panels/panel_window_controller_cocoa.mm
+++ chrome/browser/ui/cocoa/panels/panel_window_controller_cocoa.mm
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import <Cocoa/Cocoa.h>
 
 #include "base/auto_reset.h"
+#include "base/command_line.h"
 #include "base/logging.h"
 #include "base/mac/bundle_locations.h"
 #include "base/mac/mac_util.h"
@@ -31,6 +32,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/panels/panel_manager.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
 #include "chrome/browser/ui/toolbar/encoding_menu_controller.h"
+#include "chrome/common/chrome_version_info.h"
 #include "content/public/browser/render_widget_host_view.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
@@ -808,6 +810,12 @@ NSCursor* LoadWebKitCursor(WebKit::WebCursorInfo::Type type) {
 }
 
 - (void)windowDidResize:(NSNotification*)notification {
+  // This is a temporary check to track down a crash that occurs occasionally
+  // (http://crbug.com/265932).
+  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
+  if (channel == chrome::VersionInfo::CHANNEL_CANARY &&
+      CommandLine::ForCurrentProcess()->HasSwitch("enable-panel-experiment"))
+    return;
   // Hide the web contents view when the panel is not taller than the titlebar.
   // This is to ensure that the titlebar view is not overlapped with the web
   // contents view because the the web contents view assumes that its
