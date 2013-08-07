88bd88ab4bea3c80dc3f1022629dfae9154acf08
acolwell@chromium.org <acolwell@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-07 00:21:51 +0000

    Implement support for SourceBuffer.remove()
    BUG=166465
    TEST=http/tests/media/media-source/mediasource-remove.html
    
    Review URL: https://chromiumcodereview.appspot.com/22483002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155644 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/media/media-source/mediasource-util.js
+++ third_party/WebKit/LayoutTests/http/tests/media/media-source/mediasource-util.js
@@ -249,6 +249,20 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
                 testFunction(test, mediaTag, mediaSource);
             });
         }, description, options);
+    };
 
+    function timeRangesToString(ranges)
+    {
+        var s = "{";
+        for (var i = 0; i < ranges.length; ++i) {
+            s += " [" + ranges.start(i).toFixed(3) + ", " + ranges.end(i).toFixed(3) + ")";
+        }
+        return s + " }";
+    }
+
+    window['assertBufferedEquals'] = function(obj, expected, description)
+    {
+        var actual = timeRangesToString(obj.buffered);
+        assert_equals(actual, expected, description);
     };
 })(window);
