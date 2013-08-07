51fc5c7905f1c9c6d99e044b224c7bc758a05555
rtenneti@chromium.org <rtenneti@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 10:06:37 +0000

    Land Recent QUIC changes.
    Move calls to dispatcher->ProcessPacket into a unit-testable method
    inside QuicServer. Currently checks if GUID is readable (and drops
    packet if not).
    
    Further checks on packet validity can be added here in future if this
    refactor looks ok.
    
    Added quic_server_test.cc for QuicServerDispatchPacketTest.
    
    Merge internal change: 50037642
    
    QUIC - Added a comment to add getter/setter for Config class.
    
    Merge internal change: 50024203
    
    Minor fix to finish using the retransmission_it before inserting a new
    entry into the retransmission_map_.  Found when doing a code review for
    internal bug 10031572.
    
    Merge internal change: 49988308
    
    Minor change to improve readability of CreateStreamFrame's sizing test.
    
    Merge internal change: 49942445
    
    Added a comment to port LOG_IF_EVERY_N and control quic_packet_manager warnings.
    
    Merge internal change: 49834369
    
    R=rch@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22253008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216145 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ net/tools/quic/test_tools/mock_quic_dispatcher.cc
@@ -0,0 +1,22 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "net/tools/quic/test_tools/mock_quic_dispatcher.h"
+
+namespace net {
+namespace tools {
+namespace test {
+
+MockQuicDispatcher::MockQuicDispatcher(
+    const QuicConfig& config,
+    const QuicCryptoServerConfig& crypto_config,
+    QuicGuid guid,
+    EpollServer* eps)
+    : QuicDispatcher(config, crypto_config, guid, eps) { }
+MockQuicDispatcher::~MockQuicDispatcher() {}
+
+}  // namespace test
+}  // namespace tools
+}  // namespace net
