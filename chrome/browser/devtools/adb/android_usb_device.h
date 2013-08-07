3c3345b204085fa1370a4527cead9058e9b09724
pfeldman@chromium.org <pfeldman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 08:20:44 +0000

    DevTools: create usb devices on the handler thread.
    R=ikarienator@chromium.org
    
    Review URL: https://codereview.chromium.org/22301008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216129 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/adb/android_usb_device.h
+++ chrome/browser/devtools/adb/android_usb_device.h
@@ -77,6 +77,8 @@ class AndroidUsbDevice : public base::RefCountedThreadSafe<AndroidUsbDevice> {
                    int outbound_address,
                    int zero_mask);
 
+  void InitOnCallerThread();
+
   net::StreamSocket* CreateSocket(const std::string& command);
 
   void Send(uint32 command,
