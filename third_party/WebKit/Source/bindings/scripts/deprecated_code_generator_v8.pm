6e8dc79791114f05ccbbddf68993bc08c66e5f0a
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-07 18:19:40 +0000

    Use EventHandler type instead of EventListener for event handler attributes
    Distinguish in the IDL the HTML EventHandler attributes and DOM EventListener
    types added via addEventListener() method to match the specifications:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/webappapis.html#eventhandler
    http://dom.spec.whatwg.org/#eventlistener
    
    This makes our IDL closer to the specifications but there is no web-exposed
    behavior change with this patch. In the future, however, we may consider
    supporting only JS Functions (not objects with handleEvent() method) as
    input for EventHandler attributes to match the specification.
    
    R=haraken
    
    Review URL: https://chromiumcodereview.appspot.com/22581002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155702 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/scripts/deprecated_code_generator_v8.pm
+++ third_party/WebKit/Source/bindings/scripts/deprecated_code_generator_v8.pm
@@ -168,6 +168,7 @@ my %nonWrapperTypes = ("CompareHow" => 1,
                        "DOMTimeStamp" => 1,
                        "Dictionary" => 1,
                        "EventListener" => 1,
+                       "EventHandler" => 1,
                        "MediaQueryListListener" => 1,
                        "NodeFilter" => 1,
                        "SerializedScriptValue" => 1,
@@ -421,7 +422,7 @@ sub AddIncludesForType
     return if SkipIncludeHeader($type);
 
     # Default includes
-    if ($type eq "EventListener") {
+    if ($type eq "EventListener" or $type eq "EventHandler") {
         AddToImplIncludes("core/dom/EventListener.h");
     } elsif ($type eq "SerializedScriptValue") {
         AddToImplIncludes("bindings/v8/SerializedScriptValue.h");
@@ -913,12 +914,12 @@ END
     }
 }
 
-sub HasEventListenerAttribute
+sub HasEventHandlerAttribute
 {
     my $interface = shift;
 
     foreach my $attribute (@{$interface->attributes}) {
-        return 1 if $attribute->type eq "EventListener";
+        return 1 if $attribute->type eq "EventHandler";
     }
 
     return 0;
@@ -931,7 +932,7 @@ sub GetInternalFields
     my @customInternalFields = ();
     # Event listeners on DOM nodes are explicitly supported in the GC controller.
     if (!InheritsInterface($interface, "Node") &&
-        (InheritsInterface($interface, "EventTarget") || HasEventListenerAttribute($interface))) {
+        (InheritsInterface($interface, "EventTarget") || HasEventHandlerAttribute($interface))) {
         push(@customInternalFields, "eventListenerCacheIndex");
     }
     return @customInternalFields;
@@ -1435,7 +1436,7 @@ END
     $getterString = "${functionName}(" . join(", ", @arguments) . ")";
 
     my $expression;
-    if ($attribute->type eq "EventListener" && $interface->name eq "Window") {
+    if ($attribute->type eq "EventHandler" && $interface->name eq "Window") {
         $code .= "    if (!imp->document())\n";
         $code .= "        return;\n";
     }
@@ -1569,7 +1570,7 @@ END
     v8SetReturnValue(info, value);
     return;
 END
-    } elsif ($attribute->type eq "EventListener") {
+    } elsif ($attribute->type eq "EventHandler") {
         AddToImplIncludes("bindings/v8/V8AbstractEventListener.h");
         my $getterFunc = ToMethodName($attribute->name);
         # FIXME: Pass the main world ID for main-world-only getters.
@@ -1803,7 +1804,7 @@ END
     }
 
     my $nativeType = GetNativeType($attribute->type, $attribute->extendedAttributes, "parameter");
-    if ($attribute->type eq "EventListener") {
+    if ($attribute->type eq "EventHandler") {
         if ($interface->name eq "Window") {
             $code .= "    if (!imp->document())\n";
             $code .= "        return;\n";
@@ -1842,7 +1843,7 @@ END
         $code .= "    ExceptionState es(info.GetIsolate());\n";
     }
 
-    if ($attribute->type eq "EventListener") {
+    if ($attribute->type eq "EventHandler") {
         my $implSetterFunctionName = FirstLetterToUpperCase($attrName);
         AddToImplIncludes("bindings/v8/V8AbstractEventListener.h");
         if (!InheritsInterface($interface, "Node")) {
@@ -3904,7 +3905,7 @@ END
             next;
         }
 
-        if ($attrType eq "EventListener" && $interfaceName eq "Window") {
+        if ($attrType eq "EventHandler" && $interfaceName eq "Window") {
             $attrExt->{"OnProto"} = 1;
         }
 
