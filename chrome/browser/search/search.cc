e930e2c6c241e50ee8f2df2e4f4e0e47374d5eb0
kuan@chromium.org <kuan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-07 04:55:57 +0000

    instant extended: IsNTPURL should return false for search URL
    BUG=229009
    TEST=unit_tests --gtest_filter=SearchTest.IsNTPURL passes
    
    Review URL: https://chromiumcodereview.appspot.com/22457002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216074 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search/search.cc
+++ chrome/browser/search/search.cc
@@ -325,7 +325,8 @@ bool IsNTPURL(const GURL& url, Profile* profile) {
     return url == GURL(chrome::kChromeUINewTabURL);
 
   return profile &&
-      (IsInstantURL(url, profile) ||
+      ((IsInstantURL(url, profile) &&
+        GetSearchTermsFromURL(profile, url).empty()) ||
        url == GURL(chrome::kChromeSearchLocalNtpUrl));
 }
 
