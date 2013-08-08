c0dae31bcd6404bd5dd9a283d6976e1ad17b71ad
avi@chromium.org <avi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 23:01:30 +0000

    Remove NOTIFICATION_TASK_MANAGER_WINDOW_READY; it is not observed after r178323.
    BUG=268984
    TEST=no change
    
    Review URL: https://chromiumcodereview.appspot.com/22407011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216472 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/task_manager/task_manager_handler.cc
+++ chrome/browser/ui/webui/task_manager/task_manager_handler.cc
@@ -12,12 +12,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind_helpers.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/values.h"
-#include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/task_manager/task_manager.h"
 #include "chrome/browser/ui/host_desktop.h"
-#include "content/public/browser/notification_service.h"
-#include "content/public/browser/notification_source.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_ui.h"
@@ -185,11 +182,6 @@ void TaskManagerHandler::EnableTaskManager(const ListValue* indexes) {
 
   model_->AddObserver(this);
   model_->StartUpdating();
-
-  content::NotificationService::current()->Notify(
-      chrome::NOTIFICATION_TASK_MANAGER_WINDOW_READY,
-      content::Source<TaskManagerModel>(model_),
-      content::NotificationService::NoDetails());
 }
 
 void TaskManagerHandler::OpenAboutMemory(const ListValue* indexes) {
