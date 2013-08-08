e8c8ecdb73702403a406c767a03c8a671f26fd79
akuegel@chromium.org <akuegel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 17:23:47 +0000

    Create improved avatar label for mac.
    Update the avatar label button and make it look like in the mocks.
    Also support other themes by repainting the button if necessary.
    A screenshot can be found at the end of the bug description.
    
    BUG=241387,264968
    
    Review URL: https://chromiumcodereview.appspot.com/21036003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216408 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/browser/avatar_button_controller.h
+++ chrome/browser/ui/cocoa/browser/avatar_button_controller.h
@@ -11,15 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #import "base/mac/scoped_nsobject.h"
 #include "base/memory/scoped_ptr.h"
 
+@class AvatarLabelButton;
 @class AvatarMenuBubbleController;
 class Browser;
 
 namespace AvatarButtonControllerInternal {
 class Observer;
-}
-
-namespace ui {
-class ThemeProvider;
 }
 
 // This view controller manages the button/image that sits in the top of the
@@ -39,8 +36,8 @@ class ThemeProvider;
   // The avatar button.
   base::scoped_nsobject<NSButton> button_;
 
-  // The managed user avatar label. Only used for managed user profiles.
-  base::scoped_nsobject<NSButton> labelButton_;
+  // The managed user avatar label button. Only used for managed user profiles.
+  base::scoped_nsobject<AvatarLabelButton> labelButton_;
 }
 
 // The avatar button view.
