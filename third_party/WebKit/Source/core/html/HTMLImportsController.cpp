0c25a2e9d6e1723aa5732fa6b28f4d9633a8fbd9
morrita@chromium.org <morrita@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-08 02:14:38 +0000

    Remove wrong OVERRIDE to fix compilation failure.
    TBR=kochi
    BUG=
    
    Review URL: https://codereview.chromium.org/22470005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155731 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLImportsController.cpp
+++ third_party/WebKit/Source/core/html/HTMLImportsController.cpp
@@ -128,7 +128,7 @@ bool HTMLImportsController::isProcessing() const
     return m_master->parsing();
 }
 
-void HTMLImportsController::importWasDisposed() OVERRIDE
+void HTMLImportsController::importWasDisposed()
 {
     scheduleUnblock();
 }
