9a61304e8fbafc45aff6f2e55b11c2e438aff38f
hayato@chromium.org <hayato@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-08 06:14:02 +0000

    Rename getNodeInShadowTreeStack to getNodeInTreeOfTrees to utilize the term used in the Shadow DOM spec.
    BUG=None
    R=morrita@chromium.org, yutak@chromium.org
    
    Review URL: https://codereview.chromium.org/22300016
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155739 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/shadow/resources/event-dispatching.js
+++ third_party/WebKit/LayoutTests/fast/dom/shadow/resources/event-dispatching.js
@@ -132,7 +132,7 @@ function dumpComposedShadowTree(node, indent)
 function addEventListeners(nodes)
 {
     for (var i = 0; i < nodes.length; ++i) {
-        var node = getNodeInShadowTreeStack(nodes[i]);
+        var node = getNodeInTreeOfTrees(nodes[i]);
         node.addEventListener('click', recordEvent, false);
         node.addEventListener('dragstart', recordEvent, false);
         node.addEventListener('mouseout', recordEvent, false);
@@ -163,10 +163,10 @@ function moveMouse(oldElementId, newElementId)
 {
     clearEventRecords();
     debug('\n' + 'Moving mouse from ' + oldElementId + ' to ' + newElementId);
-    moveMouseOver(getNodeInShadowTreeStack(oldElementId));
+    moveMouseOver(getNodeInTreeOfTrees(oldElementId));
 
     clearEventRecords();
-    moveMouseOver(getNodeInShadowTreeStack(newElementId));
+    moveMouseOver(getNodeInTreeOfTrees(newElementId));
 
     debugDispatchedEvent('mouseout');
     debugDispatchedEvent('mouseover');
