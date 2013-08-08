2d55213025d03e135c44dd05331799ada0e126e9
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 19:44:34 +0000

    Loosen GetEngineTpe() checks to just do TLD+1.  Also check the alternate URLs, not just the main search URL.
    This is in preparation for trying to use this code to accurately identify
    various search and web page URLs associated with different providers.
    
    Also cleans up the .cc file to group the exported functions together.
    
    BUG=267137
    TEST=none
    R=mpearson@chromium.org
    
    Review URL: https://codereview.chromium.org/22651002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216428 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/search_engines/template_url_prepopulate_data_unittest.cc
+++ chrome/browser/search_engines/template_url_prepopulate_data_unittest.cc
@@ -227,7 +227,8 @@ TEST(TemplateURLPrepopulateDataTest, ClearProvidersFromPrefs) {
   EXPECT_FALSE(t_urls[default_index]->instant_url().empty());
   EXPECT_FALSE(t_urls[default_index]->image_url().empty());
   EXPECT_FALSE(t_urls[default_index]->image_url_post_params().empty());
-  EXPECT_EQ(SEARCH_ENGINE_GOOGLE,
+  EXPECT_EQ(
+      SEARCH_ENGINE_GOOGLE,
       TemplateURLPrepopulateData::GetEngineType(t_urls[default_index]->url()));
 }
 
@@ -264,7 +265,8 @@ TEST(TemplateURLPrepopulateDataTest, ProvidersFromPrepopulated) {
   EXPECT_GT(t_urls[default_index]->alternate_urls().size(), 1u);
   for (size_t i = 0; i < t_urls[default_index]->alternate_urls().size(); ++i)
     EXPECT_FALSE(t_urls[default_index]->alternate_urls()[i].empty());
-  EXPECT_EQ(SEARCH_ENGINE_GOOGLE,
+  EXPECT_EQ(
+      SEARCH_ENGINE_GOOGLE,
       TemplateURLPrepopulateData::GetEngineType(t_urls[default_index]->url()));
   EXPECT_FALSE(t_urls[default_index]->search_terms_replacement_key().empty());
 }
@@ -302,7 +304,9 @@ TEST_F(TemplateURLPrepopulateDataTest, GetEngineTypeAdvanced) {
   const char* kYahooURLs[] = {
       "http://search.yahoo.com/search?"
       "ei={inputEncoding}&fr=crmas&p={searchTerms}",
-      "http://search.yahoo.com/search?p={searchTerms}"
+      "http://search.yahoo.com/search?p={searchTerms}",
+      // Aggressively match types by checking just TLD+1.
+      "http://someothersite.yahoo.com/",
   };
   for (size_t i = 0; i < arraysize(kYahooURLs); ++i) {
     EXPECT_EQ(SEARCH_ENGINE_YAHOO,
