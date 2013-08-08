f64c2c6fa4afa68266b7daa82b4d9daedcd44b14
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 23:02:54 +0000

    Update search engine prepopulate data.
    The source data for this is already quite out of date, but it's better than not
    updating.  :/
    
    BUG=none
    TEST=none
    R=estade@chromium.org
    
    Review URL: https://codereview.chromium.org/20565004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216473 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/search_provider.cc
+++ chrome/browser/autocomplete/search_provider.cc
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/omnibox/omnibox_field_trial.h"
 #include "chrome/browser/profiles/profile.h"
 #include "chrome/browser/search/search.h"
-#include "chrome/browser/search_engines/search_engine_type.h"
 #include "chrome/browser/search_engines/template_url_prepopulate_data.h"
 #include "chrome/browser/search_engines/template_url_service.h"
 #include "chrome/browser/search_engines/template_url_service_factory.h"
