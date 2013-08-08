031e4666b463550a8c008bf5244506c254de4dc3
gkanwar@chromium.org <gkanwar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 01:08:14 +0000

    Converts monkey test to its own test type
    NOTRY=True
    BUG=223650, 263479
    
    Review URL: https://chromiumcodereview.appspot.com/22617003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216343 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ build/android/pylib/monkey/setup.py
@@ -0,0 +1,28 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+"""Generates test runner factory and tests for monkey tests."""
+
+import test_runner
+
+
+def Setup(test_options):
+  """Create and return the test runner factory and tests.
+
+  Args:
+    test_options: A MonkeyOptions object.
+
+  Returns:
+    A tuple of (TestRunnerFactory, tests).
+  """
+  # Token to replicate across devices as the "test". The TestRunner does all of
+  # the work to run the test.
+  tests = ['MonkeyTest']
+
+  def TestRunnerFactory(device, shard_index):
+    return test_runner.TestRunner(
+        test_options, device, shard_index)
+
+  return (TestRunnerFactory, tests)
