451660b7e1fd547c594410e32e17e16596886a12
jyasskin@chromium.org <jyasskin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 02:35:53 +0000

    Revert 216345 "Move ExtensionError to extensions/, add error limits"
    It leaks memory:
    http://build.chromium.org/p/chromium.memory.fyi/builders/Linux%20Tests%20%28valgrind%29%284%29/builds/24926/steps/memory%20test%3A%20unit/logs/stdio
    
    Leak_DefinitelyLost
    663 (264 direct, 399 indirect) bytes in 3 blocks are definitely lost in loss record 33,024 of 38,445
      operator new(unsigned long) (m_replacemalloc/vg_replace_malloc.c:1140)
      extensions::(anonymous namespace)::CreateNewRuntimeError(bool, std::basic_string<char, std::char_traits<char>, std::allocator<char> > const&, std::basic_string<unsigned short, base::string16_char_traits, std::allocator<unsigned short> > const&) (chrome/browser/extensions/error_console/error_console_unittest.cc:51)
      extensions::ErrorConsoleUnitTest_AddAndRemoveErrors_Test::TestBody() (chrome/browser/extensions/error_console/error_console_unittest.cc:80)
    
    
    > Move ExtensionError to extensions/, add error limits
    > 
    > Move ExtensionError class to extensions/browser/, since it doesn't need to be in chrome/.
    > Limit the number of errors stored per extension to 100.
    > Store errors in a map, keyed by Extension ID, since that is how we will likely be accessing them.
    > 
    > BUG=21734
    > 
    > Review URL: https://chromiumcodereview.appspot.com/21609003
    
    TBR=rdevlin.cronin@chromium.org
    
    Review URL: https://codereview.chromium.org/22585003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216355 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/error_console/extension_error.cc
+++ chrome/browser/extensions/error_console/extension_error.cc
@@ -3,14 +3,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "extensions/browser/extension_error.h"
+#include "chrome/browser/extensions/error_console/extension_error.h"
 
 #include "base/json/json_reader.h"
 #include "base/strings/string_number_conversions.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
+#include "chrome/common/extensions/extension.h"
 #include "extensions/common/constants.h"
-#include "url/gurl.h"
 
 using base::string16;
 
@@ -29,7 +29,7 @@ const char kStackTraceKey[] = "stackTrace";
 // populates |extension_id| with the ID. On failure, returns false and leaves
 // extension_id untouched.
 bool GetExtensionIDFromGURL(const GURL& url, std::string* extension_id) {
-  if (url.SchemeIs(kExtensionScheme)) {
+  if (url.SchemeIs(extensions::kExtensionScheme)) {
     *extension_id = url.host();
     return true;
   }
@@ -39,12 +39,10 @@ bool GetExtensionIDFromGURL(const GURL& url, std::string* extension_id) {
 }  // namespace
 
 ExtensionError::ExtensionError(Type type,
-                               const std::string& extension_id,
                                bool from_incognito,
                                const string16& source,
                                const string16& message)
     : type_(type),
-      extension_id_(extension_id),
       from_incognito_(from_incognito),
       source_(source),
       message_(message) {
@@ -61,13 +59,15 @@ std::string ExtensionError::PrintForTest() const {
          "\n  ID:      " + extension_id_;
 }
 
-ManifestParsingError::ManifestParsingError(const std::string& extension_id,
-                                           const string16& message)
+ManifestParsingError::ManifestParsingError(bool from_incognito,
+                                           const string16& source,
+                                           const string16& message,
+                                           size_t line_number)
     : ExtensionError(ExtensionError::MANIFEST_PARSING_ERROR,
-                     extension_id,
-                     false,  // extensions can't be installed while incognito.
-                     base::FilePath(kManifestFilename).AsUTF16Unsafe(),
-                     message) {
+                     from_incognito,
+                     source,
+                     message),
+      line_number_(line_number) {
 }
 
 ManifestParsingError::~ManifestParsingError() {
@@ -75,7 +75,8 @@ ManifestParsingError::~ManifestParsingError() {
 
 std::string ManifestParsingError::PrintForTest() const {
   return ExtensionError::PrintForTest() +
-         "\n  Type:    ManifestParsingError";
+         "\n  Type:    ManifestParsingError" +
+         "\n  Line:    " + base::IntToString(line_number_);
 }
 
 JavascriptRuntimeError::StackFrame::StackFrame() : line_number(-1),
@@ -101,7 +102,6 @@ JavascriptRuntimeError::JavascriptRuntimeError(bool from_incognito,
                                                logging::LogSeverity level,
                                                const string16& details)
     : ExtensionError(ExtensionError::JAVASCRIPT_RUNTIME_ERROR,
-                     std::string(),  // We don't know the id yet.
                      from_incognito,
                      source,
                      message),
