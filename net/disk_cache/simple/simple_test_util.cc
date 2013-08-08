00f2a60fbacf429ef2d678e9d2dbb3e3ac825247
pliard@chromium.org <pliard@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 08:52:08 +0000

    Make simple cache index take hashes in rather than key strings.
    The index was re-hashing keys that were already hashed.
    
    This CL also adds an ImmutableArray helper class for testing.
    
    BUG=257616
    R=gavinp@chromium.org, pasko@chromium.org
    
    Review URL: https://codereview.chromium.org/21037003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216372 0039d316-1c4b-4281-b951-d872f2087c98

--- net/disk_cache/simple/simple_test_util.cc
+++ net/disk_cache/simple/simple_test_util.cc
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "net/disk_cache/simple/simple_util.h"
 
 namespace disk_cache {
-
 namespace simple_util {
 
 bool CreateCorruptFileForTests(const std::string& key,
@@ -31,5 +30,4 @@ bool CreateCorruptFileForTests(const std::string& key,
 }
 
 }  // namespace simple_backend
-
 }  // namespace disk_cache
