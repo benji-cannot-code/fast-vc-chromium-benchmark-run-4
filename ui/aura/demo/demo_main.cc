829c562c222530dab6a0e77191330e7222669a08
tiago.vignatti@intel.com <tiago.vignatti@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 20:32:18 +0000

    aura_demo: Add a focus manager so that we don't crash on key press.
    Just like in aura_bench (commit f7af6e70).
    
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/21022013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216441 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/aura/demo/demo_main.cc
+++ ui/aura/demo/demo_main.cc
@@ -12,6 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/client/default_capture_client.h"
 #include "ui/aura/client/stacking_client.h"
 #include "ui/aura/env.h"
+#include "ui/aura/focus_manager.h"
 #include "ui/aura/root_window.h"
 #include "ui/aura/test/test_screen.h"
 #include "ui/aura/window.h"
@@ -117,6 +118,8 @@ int DemoMain() {
       test_screen->CreateRootWindowForPrimaryDisplay());
   scoped_ptr<DemoStackingClient> stacking_client(new DemoStackingClient(
       root_window.get()));
+  aura::FocusManager focus_manager;
+  aura::client::SetFocusClient(root_window.get(), &focus_manager);
 
   // Create a hierarchy of test windows.
   DemoWindowDelegate window_delegate1(SK_ColorBLUE);
