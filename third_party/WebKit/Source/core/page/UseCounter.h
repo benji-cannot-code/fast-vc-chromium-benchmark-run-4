10fd03b9bae9ef0294ec0d0d5433048d5adc214c
yutak@chromium.org <yutak@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-08 10:49:52 +0000

    Measure the usage of prefixed WebKitShadowRoot constructor.
    BUG=242153
    R=haraken@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22393003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155760 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/UseCounter.h
+++ third_party/WebKit/Source/core/page/UseCounter.h
@@ -161,6 +161,7 @@ public:
         SubFrameBeforeUnloadFired,
         CSSPseudoElementPrefixedDistributed,
         TextReplaceWholeText,
+        PrefixedShadowRootConstructor,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
