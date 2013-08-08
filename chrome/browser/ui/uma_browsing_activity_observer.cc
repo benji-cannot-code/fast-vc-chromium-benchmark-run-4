91f8aa1eb184b461ae4f75d5e088c7c39c67633a
rlp@chromium.org <rlp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 23:00:19 +0000

    [InstantExtended] Fixing how PageLoadSRP is emitted. Previously it was emitted only for Instant search results pages. Now it should be emitted for any search results page.
    These fixes include:
    - Pull out checking for search results page to its own function, IsSearchResultsPageFromDefaultSearchProvider(url) on the TemplateURLService
    - Add IsSearchResultsPage() to OmniboxCurrentPageDelegate to make checking within omnibox code easier
    - Add a test for TempalteURL::IsSearchURL which is the heart of the above functions
    - Move checking for search results pages from metrics_service.cc to uma_browsing_activity_observer.cc which is a more logical location.
    - Listen to notification for NAV_ENTRY_COMMITTED rather than LOAD_STARTED to capture more events. This includes adding a control RecordAction for NAV_ENTRY_COMMITTED so we can differentiate SRP from non-SRP.
    
    Incorporating some of the changes from https://codereview.chromium.org/20043003/ which utilize this functionality. These changes include:
    - Add a page classification category for SEARCH_RESULT_PAGE_NO_SEARCH_TERM_REPLACEMENT
    - Update the ClassifyPage function using the changes above to determine if search replacement is being done.
    - Update the omnibox_event.proto to account for the additional category as well.
    
    Also updates the name of the metrics since the definitions have changed.
    
    
    BUG=263644,264067
    
    Review URL: https://chromiumcodereview.appspot.com/21395002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216469 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/uma_browsing_activity_observer.cc
+++ chrome/browser/ui/uma_browsing_activity_observer.cc
@@ -7,14 +7,19 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/metrics/histogram.h"
 #include "chrome/browser/chrome_notification_types.h"
+#include "chrome/browser/search_engines/template_url_service.h"
+#include "chrome/browser/search_engines/template_url_service_factory.h"
 #include "chrome/browser/ui/browser.h"
 #include "chrome/browser/ui/browser_finder.h"
 #include "chrome/browser/ui/browser_iterator.h"
 #include "chrome/browser/ui/browser_window.h"
 #include "chrome/browser/ui/tabs/tab_strip_model.h"
+#include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_details.h"
+#include "content/public/browser/navigation_entry.h"
 #include "content/public/browser/notification_service.h"
 #include "content/public/browser/render_process_host.h"
+#include "content/public/browser/user_metrics.h"
 
 namespace chrome {
 namespace {
@@ -48,6 +53,20 @@ void UMABrowsingActivityObserver::Observe(
   if (type == content::NOTIFICATION_NAV_ENTRY_COMMITTED) {
     const content::LoadCommittedDetails& load =
         *content::Details<content::LoadCommittedDetails>(details).ptr();
+
+    content::NavigationController* controller =
+      content::Source<content::NavigationController>(source).ptr();
+    // Track whether the page loaded is a search results page (SRP). Track
+    // the non-SRP navigations as well so there is a control.
+    content::RecordAction(content::UserMetricsAction("NavEntryCommitted"));
+    if (TemplateURLServiceFactory::GetForProfile(
+            Profile::FromBrowserContext(controller->GetBrowserContext()))->
+            IsSearchResultsPageFromDefaultSearchProvider(
+                load.entry->GetURL())) {
+      content::RecordAction(
+          content::UserMetricsAction("NavEntryCommitted.SRP"));
+    }
+
     if (!load.is_navigation_to_different_page())
       return;  // Don't log for subframes or other trivial types.
 
