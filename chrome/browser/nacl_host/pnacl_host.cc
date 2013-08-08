b4e425e4ce92178065305bb18d3f375412d6e7ce
dschuff@chromium.org <dschuff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 22:15:41 +0000

    Handle incognito mode for PNaCl translation cache.
    This CL adds the incognito state to pending translations, and does not
    store them in the cache if they are off the record. Incognito translations
    can still hit on existing cache entries however.
    
    R=jvoung@chromium.org
    BUG= https://code.google.com/p/nativeclient/issues/detail?id=3372
    TEST= unit_tests --gtest_filter=PnaclHostTest*
    
    Review URL: https://codereview.chromium.org/22647006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216456 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/pnacl_host.cc
+++ chrome/browser/nacl_host/pnacl_host.cc
@@ -41,6 +41,7 @@ PnaclHost::PendingTranslation::PendingTranslation()
       got_nexe_fd(false),
       got_cache_reply(false),
       got_cache_hit(false),
+      is_incognito(false),
       callback(NexeFdCallback()),
       cache_info(nacl::PnaclCacheInfo()) {}
 PnaclHost::PendingTranslation::~PendingTranslation() {}
@@ -154,6 +155,7 @@ void PnaclHost::CreateTemporaryFile(TempFileCallback cb) {
 void PnaclHost::GetNexeFd(int render_process_id,
                           int render_view_id,
                           int pp_instance,
+                          bool is_incognito,
                           const nacl::PnaclCacheInfo& cache_info,
                           const NexeFdCallback& cb) {
   DCHECK(thread_checker_.CalledOnValidThread());
@@ -169,6 +171,7 @@ void PnaclHost::GetNexeFd(int render_process_id,
                                               render_process_id,
                                               render_view_id,
                                               pp_instance,
+                                              is_incognito,
                                               cache_info,
                                               cb),
                                    base::TimeDelta::FromMilliseconds(
@@ -196,6 +199,7 @@ void PnaclHost::GetNexeFd(int render_process_id,
   pt.callback = cb;
   pt.cache_info = cache_info;
   pt.cache_key = cache_key;
+  pt.is_incognito = is_incognito;
   pending_translations_[id] = pt;
   SendCacheQueryAndTempFileRequest(cache_key, id);
 }
@@ -255,9 +259,12 @@ void PnaclHost::OnTempFileReturn(const TranslationID& id,
   }
   if (fd == base::kInvalidPlatformFileValue) {
     std::string key(entry->second.cache_key);
+    bool is_incognito = entry->second.is_incognito;
     entry->second.callback.Run(fd, false);
     pending_translations_.erase(entry);
-    RequeryMatchingTranslations(key);
+    // No translations will be blocked waiting for an incongnito translation
+    if (!is_incognito)
+      RequeryMatchingTranslations(key);
     return;
   }
   PendingTranslation* pt = &entry->second;
@@ -283,6 +290,8 @@ void PnaclHost::CheckCacheQueryReady(
       if (it->second.cache_key == entry->second.cache_key &&
           // and it's not this translation,
           it->first != entry->first &&
+          // and it's not incognito,
+          !it->second.is_incognito &&
           // and if it's already gotten past this check and returned the miss.
           it->second.got_cache_reply &&
           it->second.got_nexe_fd) {
@@ -356,12 +365,15 @@ void PnaclHost::TranslationFinished(int render_process_id,
                << "," << pp_instance << " not found.";
     return;
   }
-  bool error = false;
+  bool store_nexe = true;
   // If this is a premature response (i.e. we haven't returned a temp file
-  // yet) or if it's an unsuccessful translation, don't store in the cache.
+  // yet) or if it's an unsuccessful translation, or if we are incognito,
+  // don't store in the cache.
+  // TODO(dschuff): use a separate in-memory cache for incognito
+  // translations.
   if (!entry->second.got_nexe_fd || !entry->second.got_cache_reply ||
-      !success) {
-    error = true;
+      !success || entry->second.is_incognito) {
+    store_nexe = false;
   } else if (!base::PostTaskAndReplyWithResult(
                  BrowserThread::GetBlockingPool(),
                  FROM_HERE,
@@ -370,10 +382,11 @@ void PnaclHost::TranslationFinished(int render_process_id,
                  base::Bind(&PnaclHost::StoreTranslatedNexe,
                             weak_factory_.GetWeakPtr(),
                             id))) {
-    error = true;
+    store_nexe = false;
   }
 
-  if (error) {
+  if (!store_nexe) {
+    // If store_nexe is true, the fd will be closed by CopyFileToBuffer.
     if (entry->second.got_nexe_fd) {
       BrowserThread::PostBlockingPoolTask(
           FROM_HERE,
@@ -497,8 +510,11 @@ void PnaclHost::RendererClosing(int render_process_id) {
           base::Bind(base::IgnoreResult(base::ClosePlatformFile),
                      to_erase->second.nexe_fd));
       std::string key(to_erase->second.cache_key);
+      bool is_incognito = to_erase->second.is_incognito;
       pending_translations_.erase(to_erase);
-      RequeryMatchingTranslations(key);
+      // No translations will be blocked waiting for an incongnito translation
+      if (!is_incognito)
+        RequeryMatchingTranslations(key);
     }
   }
   if (pending_translations_.empty()) {
