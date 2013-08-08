759f364f9b581ec5bc7bb239f54bcfe7a8446028
shashishekhar@chromium.org <shashishekhar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 22:42:35 +0000

    On session restore delete unassociated tab_ids.
    Old tab nodes are now added to free node pool, they can have 
    stale tab_id values from the old session. Instead of keeping old tab nodes with stale values simply delete them so that they do not accidentally 
    conflict with tab_id of the restarted session.
    
    BUG=263999
    TEST=Automated + Manual on Android.
    
    Review URL: https://chromiumcodereview.appspot.com/20244002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216464 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync/glue/tab_node_pool.h
+++ chrome/browser/sync/glue/tab_node_pool.h
@@ -33,7 +33,7 @@ namespace browser_sync {
 //                   restart. Nodes are only unassociated temporarily while the
 //                   model associator figures out which tabs belong to which
 //                   nodes. Eventually any remaining unassociated nodes are
-//                   freed.
+//                   deleted.
 // 3. Free         : Sync node is unused.
 
 class TabNodePool {
@@ -78,7 +78,7 @@ class TabNodePool {
   // Note: this should only be called when we discover tab sync nodes from
   // previous sessions, not for freeing tab nodes we created through
   // GetFreeTabNode (use FreeTabNode below for that).
-  void AddTabNode(int tab_node_id, const SessionID& tab_id);
+  void AddTabNode(int tab_node_id);
 
   // Returns the tab_id for |tab_node_id| if it is associated else returns
   // kInvalidTabID.
@@ -91,8 +91,8 @@ class TabNodePool {
   // Returns true if |tab_node_id| is an unassociated tab node.
   bool IsUnassociatedTabNode(int tab_node_id);
 
-  // Returns any unassociated nodes to the free node pool.
-  void FreeUnassociatedTabNodes();
+  // Deletes any unassociated nodes.
+  void DeleteUnassociatedTabNodes();
 
   // Clear tab pool.
   void Clear();
