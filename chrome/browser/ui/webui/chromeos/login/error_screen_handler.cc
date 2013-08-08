2dc1aca56bbd3e0b884341424d618e9bb5bfaa14
ygorshenin@chromium.org <ygorshenin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 23:22:55 +0000

    Simplified NetworkStateInformer. Now it doesn't contain any logic related to delay in notifications.
    BUG=244541
    TEST=manual tests on Lumpy
    
    Review URL: https://chromiumcodereview.appspot.com/22408002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216482 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/error_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/error_screen_handler.cc
@@ -60,6 +60,7 @@ void ErrorScreenHandler::Show(OobeDisplay::Screen parent_screen,
   ShowScreen(OobeUI::kScreenErrorMessage, params);
   NetworkErrorShown();
   EnableLazyDetection();
+  LOG(WARNING) << "Offline message is displayed";
 }
 
 void ErrorScreenHandler::Hide() {
@@ -69,6 +70,7 @@ void ErrorScreenHandler::Hide() {
   if (GetScreenName(parent_screen_, &screen_name))
     ShowScreen(screen_name.c_str(), NULL);
   DisableLazyDetection();
+  LOG(WARNING) << "Offline message is hidden";
 }
 
 void ErrorScreenHandler::FixCaptivePortal() {
