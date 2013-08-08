e55bebf0ace99be69dab645ce3826174a96b8e19
yoshiki@chromium.org <yoshiki@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-08 11:09:12 +0000

    Remove a 'content-updated' event in DirectoryTree on Files.app
    A 'content-updated' event was used to notify a change of the directory tree content, in order to hide the tree if the current volume doesn't have any folder. But now, the tree is only for Google Drive volume, which always has a folder (My Drive, Offline, Recent, ...), so that it's never hidden by 'content-update' event. It's unnecessary now, and we can remove it.
    
    BUG=269903
    TEST=manual
    R=mtomasz@chromium.org
    
    Review URL: https://codereview.chromium.org/22663002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216375 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/directory_tree.js
+++ chrome/browser/resources/file_manager/js/directory_tree.js
@@ -626,7 +626,6 @@ DirectoryTree.prototype.selectByEntryInternal_ = function(entry) {
         if (!DirectoryTreeUtil.searchAndSelectByEntry(
             this.items, this.currentEntry_))
           this.selectedItem = null;
-        cr.dispatchSimpleEvent(this, 'content-updated');
       }.bind(this),
       onError);
 };
@@ -678,7 +677,6 @@ DirectoryTree.prototype.onFilterChanged_ = function() {
     return;
 
   this.redraw(true /* recursive */);
-  cr.dispatchSimpleEvent(this, 'content-updated');
 };
 
 /**
@@ -726,10 +724,8 @@ DirectoryTree.prototype.clearTree_ = function(redraw) {
   this.selectedItem = null;
   this.entries_ = [];
 
-  if (redraw) {
+  if (redraw)
     this.redraw(false);
-    cr.dispatchSimpleEvent(this, 'content-updated');
-  }
 };
 
 /**
