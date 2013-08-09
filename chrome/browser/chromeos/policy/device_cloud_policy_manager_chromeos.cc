3bf5b39f5982b9282da5cd52daa2262188b925ec
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 20:10:42 +0000

    Shut off noisy device_cloud_policy_manager_chromeos.cc logging.
    These logs where useful at some point, but are now redundant and
    distractive, just remove them.
    
    BUG=None
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/22645012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216738 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
+++ chrome/browser/chromeos/policy/device_cloud_policy_manager_chromeos.cc
@@ -60,7 +60,7 @@ std::string GetMachineStatistic(const std::string& key) {
   chromeos::system::StatisticsProvider* provider =
       chromeos::system::StatisticsProvider::GetInstance();
   if (!provider->GetMachineStatistic(key, &value))
-    LOG(WARNING) << "Failed to get machine statistic " << key;
+    return std::string();
 
   return value;
 }
@@ -71,10 +71,8 @@ bool GetMachineFlag(const std::string& key, bool default_value) {
   bool value = default_value;
   chromeos::system::StatisticsProvider* provider =
       chromeos::system::StatisticsProvider::GetInstance();
-  if (!provider->GetMachineFlag(key, &value)) {
-    LOG(WARNING) << "Failed to get machine flag " << key;
+  if (!provider->GetMachineFlag(key, &value))
     return default_value;
-  }
 
   return value;
 }
