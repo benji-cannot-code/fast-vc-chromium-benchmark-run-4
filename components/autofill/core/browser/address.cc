c9e9ce82688f574d3712fbcfa4af6d9ec07b82b2
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 10:14:36 +0000

    [Autofill] Fill country codes for autocomplete type hint 'country'.
    BUG=254682
    TEST=unit_tests
    R=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22534002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216646 0039d316-1c4b-4281-b951-d872f2087c98

--- components/autofill/core/browser/address.cc
+++ components/autofill/core/browser/address.cc
@@ -63,7 +63,7 @@ base::string16 Address::GetRawInfo(ServerFieldType type) const {
       return zip_code_;
 
     case ADDRESS_HOME_COUNTRY:
-      return country_code_;
+      return ASCIIToUTF16(country_code_);
 
     default:
       return base::string16();
@@ -90,8 +90,9 @@ void Address::SetRawInfo(ServerFieldType type, const base::string16& value) {
       break;
 
     case ADDRESS_HOME_COUNTRY:
-      DCHECK(value.empty() || value.length() == 2u);
-      country_code_ = value;
+      DCHECK(value.empty() ||
+             (value.length() == 2u && IsStringASCII(value)));
+      country_code_ = UTF16ToASCII(value);
       break;
 
     case ADDRESS_HOME_ZIP:
@@ -105,9 +106,12 @@ void Address::SetRawInfo(ServerFieldType type, const base::string16& value) {
 
 base::string16 Address::GetInfo(const AutofillType& type,
                                 const std::string& app_locale) const {
+  if (type.html_type() == HTML_TYPE_COUNTRY_CODE)
+    return ASCIIToUTF16(country_code_);
+
   ServerFieldType storable_type = type.GetStorableType();
   if (storable_type == ADDRESS_HOME_COUNTRY && !country_code_.empty())
-    return AutofillCountry(UTF16ToASCII(country_code_), app_locale).name();
+    return AutofillCountry(country_code_, app_locale).name();
 
   return GetRawInfo(storable_type);
 }
@@ -115,10 +119,19 @@ base::string16 Address::GetInfo(const AutofillType& type,
 bool Address::SetInfo(const AutofillType& type,
                       const base::string16& value,
                       const std::string& app_locale) {
+  if (type.html_type() == HTML_TYPE_COUNTRY_CODE) {
+    if (!value.empty() && (value.size() != 2u || !IsStringASCII(value))) {
+      country_code_ = std::string();
+      return false;
+    }
+
+    country_code_ = StringToUpperASCII(UTF16ToASCII(value));
+    return true;
+  }
+
   ServerFieldType storable_type = type.GetStorableType();
   if (storable_type == ADDRESS_HOME_COUNTRY && !value.empty()) {
-    country_code_ =
-        ASCIIToUTF16(AutofillCountry::GetCountryCode(value, app_locale));
+    country_code_ = AutofillCountry::GetCountryCode(value, app_locale);
     return !country_code_.empty();
   }
 
@@ -133,7 +146,7 @@ void Address::GetMatchingTypes(const base::string16& text,
 
   // Check to see if the |text| canonicalized as a country name is a match.
   std::string country_code = AutofillCountry::GetCountryCode(text, app_locale);
-  if (!country_code.empty() && country_code_ == ASCIIToUTF16(country_code))
+  if (!country_code.empty() && country_code_ == country_code)
     matching_types->insert(ADDRESS_HOME_COUNTRY);
 }
 
