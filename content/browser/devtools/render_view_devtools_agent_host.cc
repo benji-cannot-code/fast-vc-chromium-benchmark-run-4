406bb39cb6bb83859589ab13d1ca3e42f8acc4f8
prybin@chromium.org <prybin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 19:27:03 +0000

    DevTools should not try to reattach in Render process is no actual client is connected
    Review URL: https://chromiumcodereview.appspot.com/22674008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216730 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/devtools/render_view_devtools_agent_host.cc
+++ content/browser/devtools/render_view_devtools_agent_host.cc
@@ -230,7 +230,7 @@ RenderViewDevToolsAgentHost::~RenderViewDevToolsAgentHost() {
 
 void RenderViewDevToolsAgentHost::AboutToNavigateRenderView(
     RenderViewHost* dest_rvh) {
-  if (!render_view_host_)
+  if (!render_view_host_ || !IsAttached())
     return;
 
   if (render_view_host_ == dest_rvh && static_cast<RenderViewHostImpl*>(
