14db0b3f8db9ae169b1808b4d34c7433d911ee96
tzik@chromium.org <tzik@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 12:46:03 +0000

    [SyncFS] Add ChangeList handling to MetadataDatabase
     * Add UpdateByChangeList, its test and helpers for them.
    
    BUG=240165
    
    Review URL: https://chromiumcodereview.appspot.com/22454004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216673 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/sync_file_system/drive_backend/tracker_set.h
+++ chrome/browser/sync_file_system/drive_backend/tracker_set.h
@@ -40,6 +40,7 @@ class TrackerSet {
   iterator end() { return tracker_set_.end(); }
   const_iterator begin() const { return tracker_set_.begin(); }
   const_iterator end() const { return tracker_set_.end(); }
+  bool empty() const { return tracker_set_.empty(); }
 
  private:
   FileTracker* active_tracker_;
