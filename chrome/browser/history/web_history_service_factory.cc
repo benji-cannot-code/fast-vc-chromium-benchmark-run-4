c0226888e0601f742f65b2b4f2235454d4e87513
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 23:01:47 +0000

    Cleanup: web history does not depend on TokenService, it depends on ProfileOAuth2TokenService.
    BUG=None
    
    Review URL: https://chromiumcodereview.appspot.com/22594008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216768 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/history/web_history_service_factory.cc
+++ chrome/browser/history/web_history_service_factory.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "chrome/browser/content_settings/cookie_settings.h"
 #include "chrome/browser/history/web_history_service.h"
-#include "chrome/browser/signin/token_service_factory.h"
+#include "chrome/browser/signin/profile_oauth2_token_service_factory.h"
 #include "chrome/browser/sync/profile_sync_service.h"
 #include "chrome/browser/sync/profile_sync_service_factory.h"
 #include "chrome/common/chrome_switches.h"
@@ -60,8 +60,8 @@ WebHistoryServiceFactory::WebHistoryServiceFactory()
     : BrowserContextKeyedServiceFactory(
         "WebHistoryServiceFactory",
         BrowserContextDependencyManager::GetInstance()) {
-  DependsOn(TokenServiceFactory::GetInstance());
   DependsOn(CookieSettings::Factory::GetInstance());
+  DependsOn(ProfileOAuth2TokenServiceFactory::GetInstance());
 }
 
 WebHistoryServiceFactory::~WebHistoryServiceFactory() {
