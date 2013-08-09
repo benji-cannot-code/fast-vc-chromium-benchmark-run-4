0a573892cffe977a40d91f44630d1b84b813f1fb
hfung@chromium.org <hfung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 07:39:13 +0000

    Add PageClassification to aqs again after ASAN test breakage (from https://chromiumcodereview.appspot.com/21625002).
    BUG=264773
    
    Review URL: https://chromiumcodereview.appspot.com/22712002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216636 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/autocomplete_input.h
+++ chrome/browser/autocomplete/autocomplete_input.h
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/basictypes.h"
+#include "base/gtest_prod_util.h"
 #include "base/strings/string16.h"
 #include "url/gurl.h"
 #include "url/url_parse.h"
@@ -222,6 +223,8 @@ class AutocompleteInput {
   void Clear();
 
  private:
+  FRIEND_TEST_ALL_PREFIXES(AutocompleteProviderTest, GetDestinationURL);
+
   // NOTE: Whenever adding a new field here, please make sure to update Clear()
   // method.
   string16 text_;
