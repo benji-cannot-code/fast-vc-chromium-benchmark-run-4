6f51f3b87a70c0a0ac712f9c9672e2bb02e69e1c
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 06:37:53 +0000

    Notify the display bounds change on secondary display when layout changes
    BUG=247059
    TEST=covered by unittest
    
    Review URL: https://chromiumcodereview.appspot.com/22645003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216607 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/display/display_controller.cc
+++ ash/display/display_controller.cc
@@ -405,6 +405,10 @@ void DisplayController::SetLayoutForCurrentDisplays(
     PreDisplayConfigurationChange();
     // TODO(oshima): Call UpdateDisplays instead.
     UpdateDisplayBoundsForLayout();
+    // Primary's bounds stay the same. Just notify bounds change
+    // on the secondary.
+    Shell::GetInstance()->screen()->NotifyBoundsChanged(
+        ScreenAsh::GetSecondaryDisplay());
     PostDisplayConfigurationChange();
   }
 }
