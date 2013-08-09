f25ded1cf8d2c10cc0419f4c4e2c8ddec94b53f3
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 00:30:39 +0000

    [Android WebView] Bail out of resource extracting when no paks specified.
    WebView doesn't extract any pak files, so short circuit the resource
    extractor machinery. Thisi is faster, and prevents cruft from being
    put onto the filesystem unnecesarily.
    
    Bug: b/8367699
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22466003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216509 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/ResourceExtractor.java
+++ content/public/android/java/src/org/chromium/content/browser/ResourceExtractor.java
@@ -53,11 +53,6 @@ public class ResourceExtractor {
 
         @Override
         protected Void doInBackground(Void... unused) {
-            if (sMandatoryPaks == null) {
-                assert false : "No pak files specified.  Call setMandatoryPaksToExtract before "
-                        + "beginning the resource extractions";
-                return null;
-            }
             if (!mOutputDir.exists() && !mOutputDir.mkdirs()) {
                 Log.e(LOGTAG, "Unable to create pak resources directory!");
                 return null;
@@ -243,7 +238,8 @@ public class ResourceExtractor {
     /**
      * Specifies the .pak files that should be extracted from the APK's asset resources directory
      * and moved to {@link #getOutputDirFromContext(Context)}.
-     * @param mandatoryPaks The list of pak files to be loaded.
+     * @param mandatoryPaks The list of pak files to be loaded. If no pak files are
+     *     required, pass a single empty string.
      */
     public static void setMandatoryPaksToExtract(String... mandatoryPaks) {
         assert (sInstance == null || sInstance.mExtractTask == null)
@@ -272,6 +268,10 @@ public class ResourceExtractor {
     }
 
     public void waitForCompletion() {
+        if (shouldSkipPakExtraction()) {
+            return;
+        }
+
         assert mExtractTask != null;
 
         try {
@@ -297,6 +297,10 @@ public class ResourceExtractor {
             return;
         }
 
+        if (shouldSkipPakExtraction()) {
+            return;
+        }
+
         mExtractTask = new ExtractTask();
         mExtractTask.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR);
     }
@@ -315,5 +319,15 @@ public class ResourceExtractor {
                 }
             }
         }
+    }
+
+    /**
+     * Pak extraction not necessarily required by the embedder; we allow them to skip
+     * this process if they call setMandatoryPaksToExtract with a single empty String.
+     */
+    private static boolean shouldSkipPakExtraction() {
+        // Must call setMandatoryPaksToExtract before beginning resource extraction.
+        assert sMandatoryPaks != null;
+        return sMandatoryPaks.length == 1 && "".equals(sMandatoryPaks[0]);
     }
 }
