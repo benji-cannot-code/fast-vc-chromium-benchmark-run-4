72d8820b16e11fe37bcdf58af9a15b76fe06b2b4
rtoy@google.com <rtoy@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 23:47:55 +0000

    Fix slow playback of 1-channel MP3 and AAC files for WebAudio on Android
    BUG=266006
    
    If the MediaFormat indicates 1 channel for the file but the MediaCodec decoder changes the output format to 2 channels, we remove the data from the second channel before sending to the receiver.  This makes the the transmitted data consistent with the expected number of channels.
    
    Review URL: https://chromiumcodereview.appspot.com/21618002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216779 0039d316-1c4b-4281-b951-d872f2087c98

--- media/base/android/java/src/org/chromium/media/WebAudioMediaCodecBridge.java
+++ media/base/android/java/src/org/chromium/media/WebAudioMediaCodecBridge.java
@@ -62,7 +62,14 @@ class WebAudioMediaCodecBridge {
 
         MediaFormat format = extractor.getTrackFormat(0);
 
-        int channelCount = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+        // Number of channels specified in the file
+        int inputChannelCount = format.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+
+        // Number of channels the decoder will provide. (Not
+        // necessarily the same as inputChannelCount.  See
+        // crbug.com/266006.)
+        int outputChannelCount = inputChannelCount;
+
         int sampleRate = format.getInteger(MediaFormat.KEY_SAMPLE_RATE);
         String mime = format.getString(MediaFormat.KEY_MIME);
 
@@ -78,13 +85,13 @@ class WebAudioMediaCodecBridge {
         if (DEBUG) {
             Log.d(LOG_TAG, "Tracks: " + extractor.getTrackCount()
                   + " Rate: " + sampleRate
-                  + " Channels: " + channelCount
+                  + " Channels: " + inputChannelCount
                   + " Mime: " + mime
                   + " Duration: " + durationMicroseconds + " microsec");
         }
 
         nativeInitializeDestination(nativeMediaCodecBridge,
-                                    channelCount,
+                                    inputChannelCount,
                                     sampleRate,
                                     durationMicroseconds);
 
@@ -140,7 +147,8 @@ class WebAudioMediaCodecBridge {
                 ByteBuffer buf = codecOutputBuffers[outputBufIndex];
 
                 if (info.size > 0) {
-                    nativeOnChunkDecoded(nativeMediaCodecBridge, buf, info.size);
+                    nativeOnChunkDecoded(nativeMediaCodecBridge, buf, info.size,
+                                         inputChannelCount, outputChannelCount);
                 }
 
                 buf.clear();
@@ -151,6 +159,10 @@ class WebAudioMediaCodecBridge {
                 }
             } else if (outputBufIndex == MediaCodec.INFO_OUTPUT_BUFFERS_CHANGED) {
                 codecOutputBuffers = codec.getOutputBuffers();
+            } else if (outputBufIndex == MediaCodec.INFO_OUTPUT_FORMAT_CHANGED) {
+                MediaFormat newFormat = codec.getOutputFormat();
+                outputChannelCount = newFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+                Log.d(LOG_TAG, "output format changed to " + newFormat);
             }
         }
 
@@ -164,11 +176,12 @@ class WebAudioMediaCodecBridge {
     }
 
     private static native void nativeOnChunkDecoded(
-        int nativeWebAudioMediaCodecBridge, ByteBuffer buf, int size);
+        int nativeWebAudioMediaCodecBridge, ByteBuffer buf, int size,
+        int inputChannelCount, int outputChannelCount);
 
     private static native void nativeInitializeDestination(
         int nativeWebAudioMediaCodecBridge,
-        int channelCount,
+        int inputChannelCount,
         int sampleRate,
         long durationMicroseconds);
 }
