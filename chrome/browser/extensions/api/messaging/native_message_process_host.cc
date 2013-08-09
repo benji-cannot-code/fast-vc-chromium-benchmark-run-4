8cc49cf38559a179ff37efaeedc8d44eb79004ec
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 22:29:32 +0000

    Use the blocking pool for async file operations in NativeMessageProcessHost.
    Remove dependency on deprecated WorkerPool and make tests shut down cleanly.
    
    BUG=248513
    R=sergeyu@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22638009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216767 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/messaging/native_message_process_host.cc
+++ chrome/browser/extensions/api/messaging/native_message_process_host.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/platform_file.h"
+#include "base/threading/sequenced_worker_pool.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/api/messaging/native_messaging_host_manifest.h"
 #include "chrome/browser/extensions/api/messaging/native_process_launcher.h"
@@ -138,10 +139,18 @@ void NativeMessageProcessHost::OnHostProcessLaunched(
   }
 
   read_file_ = read_file;
+
+  scoped_refptr<base::TaskRunner> task_runner(
+      content::BrowserThread::GetBlockingPool()->
+          GetTaskRunnerWithShutdownBehavior(
+              base::SequencedWorkerPool::SKIP_ON_SHUTDOWN));
+
   read_stream_.reset(new net::FileStream(
-      read_file, base::PLATFORM_FILE_READ | base::PLATFORM_FILE_ASYNC, NULL));
+      read_file, base::PLATFORM_FILE_READ | base::PLATFORM_FILE_ASYNC, NULL,
+      task_runner));
   write_stream_.reset(new net::FileStream(
-      write_file, base::PLATFORM_FILE_WRITE | base::PLATFORM_FILE_ASYNC, NULL));
+      write_file, base::PLATFORM_FILE_WRITE | base::PLATFORM_FILE_ASYNC, NULL,
+      task_runner));
 
   WaitRead();
   DoWrite();
