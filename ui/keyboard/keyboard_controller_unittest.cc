ffbe5529c13d0b349c256083d70ed0c9babcb67f
yoichio@chromium.org <yoichio@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 12:45:18 +0000

    Add GetTextInputMode function to TextInputClient.
    All implementation return TEXT_INPUT_MODE_DEFAULT for now.
    RenderWidgetHostViewWin/Aura will change implementation.
    
    This method will be used for Windows8 Metro inputscope.
    
    BUG=244688
    
    Review URL: https://chromiumcodereview.appspot.com/22401002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216671 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/keyboard_controller_unittest.cc
+++ ui/keyboard/keyboard_controller_unittest.cc
@@ -124,6 +124,9 @@ class TestTextInputClient : public ui::TextInputClient {
   virtual ui::TextInputType GetTextInputType() const OVERRIDE {
     return type_;
   }
+  virtual ui::TextInputMode GetTextInputMode() const OVERRIDE {
+    return ui::TEXT_INPUT_MODE_DEFAULT;
+  }
   virtual bool CanComposeInline() const OVERRIDE { return false; }
   virtual gfx::Rect GetCaretBounds() OVERRIDE { return gfx::Rect(); }
 
