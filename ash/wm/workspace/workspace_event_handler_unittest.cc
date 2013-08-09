119ce48fa2ebe9a3e486f481379ea6e9b973bc3b
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-09 22:14:58 +0000

    Remove unnecessary include.  This was the cause of linkage warning
    warning: hidden symbol 'void aura::Window::SetProperty<bool>(aura::WindowProperty<bool> const*, bool)' in obj/ash/wm/workspace/ash_unittests.workspace_event_handler_unittest.o is referenced by DSO lib/libash.so
    
    BUG=none
    TEST=no linkage warning
    
    Review URL: https://chromiumcodereview.appspot.com/22629008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216763 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_event_handler_unittest.cc
+++ ash/wm/workspace/workspace_event_handler_unittest.cc
@@ -17,7 +17,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/aura/test/event_generator.h"
 #include "ui/aura/test/test_window_delegate.h"
 #include "ui/aura/window.h"
-#include "ui/aura/window_property.h"
 #include "ui/base/hit_test.h"
 #include "ui/gfx/screen.h"
 
