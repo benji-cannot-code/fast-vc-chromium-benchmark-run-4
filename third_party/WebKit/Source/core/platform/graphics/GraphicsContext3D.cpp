4b91d0924e4486594c98e84882ae4d6e01626101
torne@chromium.org <torne@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-09 11:06:52 +0000

    Qualify skia gl includes properly in GraphicsContext3D.
    skia's public include path isn't supposed to include gpu/gl; update
    GraphicsContext3D.cpp to refer to skia headers with fully qualified
    paths so that Chromium doesn't need to include extra directories.
    
    BUG=
    R=jamesr@chromium.org
    
    Review URL: https://codereview.chromium.org/22582006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155836 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/GraphicsContext3D.cpp
+++ third_party/WebKit/Source/core/platform/graphics/GraphicsContext3D.cpp
@@ -30,8 +30,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "core/platform/graphics/GraphicsContext3D.h"
 
-#include "GrContext.h"
-#include "GrGLInterface.h"
 #include "core/html/ImageData.h"
 #include "core/html/canvas/CheckedInt.h"
 #include "core/platform/graphics/Extensions3D.h"
@@ -41,6 +39,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/platform/graphics/ImageObserver.h"
 #include "core/platform/graphics/gpu/DrawingBuffer.h"
 #include "core/platform/image-decoders/ImageDecoder.h"
+#include "third_party/skia/include/gpu/GrContext.h"
+#include "third_party/skia/include/gpu/gl/GrGLInterface.h"
 #include "wtf/CPU.h"
 #include "wtf/OwnArrayPtr.h"
 #include "wtf/PassOwnArrayPtr.h"
