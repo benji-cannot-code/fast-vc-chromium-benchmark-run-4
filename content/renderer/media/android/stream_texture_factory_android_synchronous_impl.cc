eea49a1556dd4fcb5214f59ec86983897ae210c5
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-10 15:10:38 +0000

    Fullscreen video in Android WebView
    Make StreamTextureFactory and StreamTextureProxy abstract
    base classes, and create a stub StreamTextureFactory for
    Android Webview using synchronous compositor.
    
    This provides enough plumbing to make full screen video
    work in Android WebView. In-page video still requires
    the in-process context to support streaming texture
    manager.
    
    BUG=239760
    
    Review URL: https://chromiumcodereview.appspot.com/22766004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216864 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ content/renderer/media/android/stream_texture_factory_android_synchronous_impl.cc
@@ -0,0 +1,35 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "content/renderer/media/android/stream_texture_factory_android_synchronous_impl.h"
+
+namespace content {
+
+StreamTextureFactorySynchronousImpl::StreamTextureFactorySynchronousImpl() {}
+StreamTextureFactorySynchronousImpl::~StreamTextureFactorySynchronousImpl() {}
+
+StreamTextureProxy* StreamTextureFactorySynchronousImpl::CreateProxy() {
+  return NULL;
+}
+
+void StreamTextureFactorySynchronousImpl::EstablishPeer(int32 stream_id,
+                                                        int player_id) {}
+
+unsigned StreamTextureFactorySynchronousImpl::CreateStreamTexture(
+    unsigned texture_target,
+    unsigned* texture_id,
+    gpu::Mailbox* texture_mailbox,
+    unsigned* texture_mailbox_sync_point) {
+  return 0;
+}
+
+void StreamTextureFactorySynchronousImpl::DestroyStreamTexture(
+    unsigned texture_id) {}
+
+void StreamTextureFactorySynchronousImpl::SetStreamTextureSize(
+    int32 texture_id,
+    const gfx::Size& size) {}
+
+}  // namespace content
