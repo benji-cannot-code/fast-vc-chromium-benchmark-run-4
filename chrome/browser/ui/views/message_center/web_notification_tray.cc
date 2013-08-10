8e17194e9a304e762fce28c5bba428c9b3fc6ff9
dewittj@chromium.org <dewittj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-10 08:22:00 +0000

    [Win] Only show bell tray icon after a notification.
    People who never see a notification should not have their desktop
    cluttered with extra status tray icons, so we'll hold off on
    creating it until we have a good reason to.
    
    R=jianli@chromium.org
    BUG=270688
    
    Review URL: https://chromiumcodereview.appspot.com/22394008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216827 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/message_center/web_notification_tray.cc
+++ chrome/browser/ui/views/message_center/web_notification_tray.cc
@@ -133,7 +133,6 @@ WebNotificationTray::WebNotificationTray()
       should_update_tray_content_(true) {
   message_center_tray_.reset(
       new MessageCenterTray(this, g_browser_process->message_center()));
-  OnMessageCenterTrayChanged();
 }
 
 WebNotificationTray::~WebNotificationTray() {
