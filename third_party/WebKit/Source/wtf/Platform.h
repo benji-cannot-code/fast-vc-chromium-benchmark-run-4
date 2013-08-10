644acd106627fe3becd101a8a00e2a97638ead7b
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-10 21:19:04 +0000

    Move all the HAVE() macros related to string manipulations to wtf/StringExtras.h. The goal of this refactoring is to reduce the scope of these macros and only make them visible to files where it is used.
    Review URL: https://chromiumcodereview.appspot.com/22118002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155905 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Platform.h
+++ third_party/WebKit/Source/wtf/Platform.h
@@ -139,7 +139,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if OS(UNIX)
 #define HAVE_MMAP 1
 #define HAVE_SIGNAL_H 1
-#define HAVE_STRINGS_H 1
 #define HAVE_SYS_TIME_H 1
 #define WTF_USE_PTHREADS 1
 #endif /* OS(UNIX) */
@@ -151,12 +150,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #if !defined(HAVE_VASPRINTF)
 #if !COMPILER(MSVC)
 #define HAVE_VASPRINTF 1
-#endif
-#endif
-
-#if !defined(HAVE_STRNSTR)
-#if OS(DARWIN) || (OS(FREEBSD) && !defined(__GLIBC__))
-#define HAVE_STRNSTR 1
 #endif
 #endif
 
--- third_party/WebKit/Source/wtf/StringExtras.h
+++ third_party/WebKit/Source/wtf/StringExtras.h
@@ -27,6 +27,16 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef WTF_StringExtras_h
 #define WTF_StringExtras_h
 
+#if OS(UNIX)
+#define HAVE_STRINGS_H 1
+#endif
+
+#if !defined(HAVE_STRNSTR)
+#if OS(DARWIN) || (OS(FREEBSD) && !defined(__GLIBC__))
+#define HAVE_STRNSTR 1
+#endif
+#endif
+
 #include <stdarg.h>
 #include <stdio.h>
 #include <string.h>
