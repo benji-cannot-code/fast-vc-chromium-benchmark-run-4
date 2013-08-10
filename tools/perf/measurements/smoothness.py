5d9fffe57aea13d770052ae5b950f852c530348a
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-10 08:33:28 +0000

    telemetry: separated stats collection from metrics calculation.
    - Split calculation of smoothness metrics into two parts:
      A) collection of stats (now from RenderingStats, in the future
      from traces),
      B) calculation of the actual benchmark metrics from the stats.
    - This allows modifying and testing both parts independently
    
    R=nduca@chromium.org
    BUG=264308
    
    Review URL: https://chromiumcodereview.appspot.com/22475007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216832 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/smoothness.py
+++ tools/perf/measurements/smoothness.py
@@ -4,6 +4,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 # found in the LICENSE file.
 from metrics import loading
 from metrics import smoothness
+from metrics.gpu_rendering_stats import GpuRenderingStats
 from telemetry.page import page_measurement
 
 class DidNotScrollException(page_measurement.MeasurementFailure):
@@ -60,11 +61,9 @@ class Smoothness(page_measurement.PageMeasurement):
     loading.LoadingMetric().AddResults(tab, results)
 
     smoothness.CalcFirstPaintTimeResults(results, tab)
-    smoothness.CalcScrollResults(rendering_stats_deltas, results)
-    smoothness.CalcTextureUploadResults(rendering_stats_deltas, results)
-    smoothness.CalcImageDecodingResults(rendering_stats_deltas, results)
-    smoothness.CalcAnalysisResults(rendering_stats_deltas, results)
-    smoothness.CalcLatencyResults(rendering_stats_deltas, results)
+
+    benchmark_stats = GpuRenderingStats(rendering_stats_deltas)
+    smoothness.CalcResults(benchmark_stats, results)
 
     if self.options.report_all_results:
       for k, v in rendering_stats_deltas.iteritems():
