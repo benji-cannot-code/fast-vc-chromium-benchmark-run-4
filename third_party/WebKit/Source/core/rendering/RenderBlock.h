601d9fb488b6d09277c634d7a30f6d507beab000
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-10 00:12:51 +0000

    Attempt to make it more clear what FloatIntervalSearchAdaptor::collectIfNeeded is doing.
    It seemed to me that template specifications would be clearer
    than an if.  They also allow for compile-time error checking
    were a 3rd type of float to come into existance in CSS4. :p
    
    For any unfamiliar with this method, this the object
    used for performing a search on a RedBlackTree in WTF.
    
    We create one of these adaptors, specifying that we want to
    search for values in a specific (logical) Y interval, and
    this adaptor is called back for any values in the RBTree
    cooresponding to that interval range.
    
    The job of this adaptor is to collect the various values
    we care about, including the left or right-most offset
    of the floats in that Y-range as well as what the last
    (document order) float seen in that range.
    
    It also collects the remaining available height for the block but I'm less clear on how that parameter is used.
    
    Review URL: https://chromiumcodereview.appspot.com/22493015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155885 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.h
+++ third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -1192,6 +1192,8 @@ protected:
         const FloatingObject* lastFloat() const { return m_last; }
 
     private:
+        bool updateOffsetIfNeeded(const FloatingObject*) const;
+
         const RenderBlock* m_renderer;
         int m_lowValue;
         int m_highValue;
