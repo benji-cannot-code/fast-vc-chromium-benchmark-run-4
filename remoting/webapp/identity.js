b299eb93f4e429679f3705f46f9f7d1a50965fa8
rmsousa@chromium.org <rmsousa@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-10 14:00:09 +0000

    s/experimental.identity/identity
    (identity is out of experimental)
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22339015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216846 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/webapp/identity.js
+++ remoting/webapp/identity.js
@@ -52,7 +52,7 @@ remoting.Identity = function(consentCallback) {
 remoting.Identity.prototype.callWithToken = function(onOk, onError) {
   this.pendingCallbacks_.push(new remoting.Identity.Callbacks(onOk, onError));
   if (this.pendingCallbacks_.length == 1) {
-    chrome.experimental.identity.getAuthToken(
+    chrome.identity.getAuthToken(
         { 'interactive': false },
         this.onAuthComplete_.bind(this, false));
   }
@@ -133,7 +133,7 @@ remoting.Identity.prototype.onAuthComplete_ = function(interactive, token) {
  * @private
  */
 remoting.Identity.prototype.onAuthContinue_ = function() {
-  chrome.experimental.identity.getAuthToken(
+  chrome.identity.getAuthToken(
       { 'interactive': true },
       this.onAuthComplete_.bind(this, true));
 };
