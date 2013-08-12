e4b7dd19499ef2619a169c8c292da88de3377713
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 21:20:35 +0000

    rAc: try really hard not to ellipsize addresses
    Change rAc suggestion text to return two string values, one which is preferred (but may not fit horizontally) and one which is a fallback (which has a better chance to fit horizontally).
    
    BUG=264973
    
    Review URL: https://chromiumcodereview.appspot.com/21724002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217076 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/data_model_wrapper.h
+++ chrome/browser/ui/autofill/data_model_wrapper.h
@@ -36,7 +36,7 @@ class DataModelWrapper {
   virtual ~DataModelWrapper();
 
   // Returns the data for a specific autocomplete type.
-  virtual string16 GetInfo(const AutofillType& type) const = 0;
+  virtual base::string16 GetInfo(const AutofillType& type) const = 0;
 
   // Returns the icon, if any, that represents this model.
   virtual gfx::Image GetIcon();
@@ -45,9 +45,15 @@ class DataModelWrapper {
   // out-param).
   virtual void FillInputs(DetailInputs* inputs);
 
-  // Returns text to display to the user to summarize this data source. The
-  // default implementation assumes this is an address.
-  virtual string16 GetDisplayText();
+  // Gets text to display to the user to summarize this data source. The
+  // default implementation assumes this is an address. Both params are required
+  // to be non-NULL and will be filled in with text that is vertically compact
+  // (but may take up a lot of horizontal space) and horizontally compact (but
+  // may take up a lot of vertical space) respectively. The return value will
+  // be true and the outparams will be filled in only if the data represented is
+  // complete and valid.
+  virtual bool GetDisplayText(base::string16* vertically_compact,
+                              base::string16* horizontally_compact);
 
   // Fills in |form_structure| with the data that this model contains. |inputs|
   // and |comparator| are used to determine whether each field in the
@@ -65,6 +71,10 @@ class DataModelWrapper {
   virtual void FillFormField(AutofillField* field) const;
 
  private:
+  // Formats address data into a single string using |separator| between
+  // fields.
+  base::string16 GetAddressDisplayText(const base::string16& separator);
+
   DISALLOW_COPY_AND_ASSIGN(DataModelWrapper);
 };
 
@@ -75,7 +85,7 @@ class EmptyDataModelWrapper : public DataModelWrapper {
   EmptyDataModelWrapper();
   virtual ~EmptyDataModelWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
 
  protected:
   virtual void FillFormField(AutofillField* field) const OVERRIDE;
@@ -89,7 +99,7 @@ class AutofillDataModelWrapper : public DataModelWrapper {
   AutofillDataModelWrapper(const AutofillDataModel* data_model, size_t variant);
   virtual ~AutofillDataModelWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
 
  protected:
   virtual void FillFormField(AutofillField* field) const OVERRIDE;
@@ -125,9 +135,10 @@ class AutofillCreditCardWrapper : public AutofillDataModelWrapper {
   explicit AutofillCreditCardWrapper(const CreditCard* card);
   virtual ~AutofillCreditCardWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
   virtual gfx::Image GetIcon() OVERRIDE;
-  virtual string16 GetDisplayText() OVERRIDE;
+  virtual bool GetDisplayText(base::string16* vertically_compact,
+                              base::string16* horizontally_compact) OVERRIDE;
 
  private:
   const CreditCard* card_;
@@ -141,8 +152,9 @@ class WalletAddressWrapper : public DataModelWrapper {
   explicit WalletAddressWrapper(const wallet::Address* address);
   virtual ~WalletAddressWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
-  virtual string16 GetDisplayText() OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual bool GetDisplayText(base::string16* vertically_compact,
+                              base::string16* horizontally_compact) OVERRIDE;
 
  private:
   const wallet::Address* address_;
@@ -157,9 +169,10 @@ class WalletInstrumentWrapper : public DataModelWrapper {
       const wallet::WalletItems::MaskedInstrument* instrument);
   virtual ~WalletInstrumentWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
   virtual gfx::Image GetIcon() OVERRIDE;
-  virtual string16 GetDisplayText() OVERRIDE;
+  virtual bool GetDisplayText(base::string16* vertically_compact,
+                              base::string16* horizontally_compact) OVERRIDE;
 
  private:
   const wallet::WalletItems::MaskedInstrument* instrument_;
@@ -173,8 +186,9 @@ class FullWalletBillingWrapper : public DataModelWrapper {
   explicit FullWalletBillingWrapper(wallet::FullWallet* full_wallet);
   virtual ~FullWalletBillingWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
-  virtual string16 GetDisplayText() OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual bool GetDisplayText(base::string16* vertically_compact,
+                              base::string16* horizontally_compact) OVERRIDE;
 
  private:
   wallet::FullWallet* full_wallet_;
@@ -188,7 +202,7 @@ class FullWalletShippingWrapper : public DataModelWrapper {
   explicit FullWalletShippingWrapper(wallet::FullWallet* full_wallet);
   virtual ~FullWalletShippingWrapper();
 
-  virtual string16 GetInfo(const AutofillType& type) const OVERRIDE;
+  virtual base::string16 GetInfo(const AutofillType& type) const OVERRIDE;
 
  private:
   wallet::FullWallet* full_wallet_;
--- chrome/browser/ui/autofill/data_model_wrapper_unittest.cc
+++ chrome/browser/ui/autofill/data_model_wrapper_unittest.cc
@@ -35,7 +35,8 @@ TEST(AutofillCreditCardWrapperTest, GetDisplayTextEmptyWhenExpired) {
   card.SetRawInfo(CREDIT_CARD_EXP_4_DIGIT_YEAR, ASCIIToUTF16("2010"));
   card.SetRawInfo(CREDIT_CARD_NUMBER, ASCIIToUTF16("4111111111111111"));
   AutofillCreditCardWrapper wrapper(&card);
-  EXPECT_TRUE(wrapper.GetDisplayText().empty());
+  base::string16 unused, unused2;
+  EXPECT_FALSE(wrapper.GetDisplayText(&unused, &unused2));
 }
 
 TEST(AutofillCreditCardWrapperTest, GetDisplayTextEmptyWhenInvalid) {
@@ -44,7 +45,8 @@ TEST(AutofillCreditCardWrapperTest, GetDisplayTextEmptyWhenInvalid) {
   card.SetRawInfo(CREDIT_CARD_EXP_4_DIGIT_YEAR, ASCIIToUTF16("9999"));
   card.SetRawInfo(CREDIT_CARD_NUMBER, ASCIIToUTF16("41111"));
   AutofillCreditCardWrapper wrapper(&card);
-  EXPECT_TRUE(wrapper.GetDisplayText().empty());
+  base::string16 unused, unused2;
+  EXPECT_FALSE(wrapper.GetDisplayText(&unused, &unused2));
 }
 
 TEST(AutofillCreditCardWrapperTest, GetDisplayTextNotEmptyWhenValid) {
@@ -53,7 +55,8 @@ TEST(AutofillCreditCardWrapperTest, GetDisplayTextNotEmptyWhenValid) {
   card.SetRawInfo(CREDIT_CARD_EXP_4_DIGIT_YEAR, ASCIIToUTF16("9999"));
   card.SetRawInfo(CREDIT_CARD_NUMBER, ASCIIToUTF16("4111111111111111"));
   AutofillCreditCardWrapper wrapper(&card);
-  EXPECT_FALSE(wrapper.GetDisplayText().empty());
+  base::string16 unused, unused2;
+  EXPECT_TRUE(wrapper.GetDisplayText(&unused, &unused2));
 }
 
 TEST(WalletInstrumentWrapperTest, GetInfoCreditCardExpMonth) {
@@ -73,7 +76,8 @@ TEST(WalletInstrumentWrapperTest, GetDisplayTextEmptyWhenExpired) {
       wallet::GetTestMaskedInstrument());
   instrument->status_ = wallet::WalletItems::MaskedInstrument::EXPIRED;
   WalletInstrumentWrapper wrapper(instrument.get());
-  EXPECT_TRUE(wrapper.GetDisplayText().empty());
+  base::string16 unused, unused2;
+  EXPECT_FALSE(wrapper.GetDisplayText(&unused, &unused2));
 }
 
 TEST(DataModelWrapperTest, GetDisplayTextEmptyWithoutPhone) {
@@ -81,10 +85,11 @@ TEST(DataModelWrapperTest, GetDisplayTextEmptyWithoutPhone) {
       wallet::GetTestMaskedInstrument());
 
   WalletInstrumentWrapper instrument_wrapper(instrument.get());
-  ASSERT_FALSE(instrument_wrapper.GetDisplayText().empty());
+  base::string16 unused, unused2;
+  ASSERT_TRUE(instrument_wrapper.GetDisplayText(&unused, &unused2));
 
   WalletAddressWrapper address_wrapper(&instrument->address());
-  ASSERT_FALSE(address_wrapper.GetDisplayText().empty());
+  ASSERT_TRUE(address_wrapper.GetDisplayText(&unused, &unused2));
 
   const_cast<wallet::Address*>(&instrument->address())->set_phone_number(
       string16());
@@ -92,11 +97,11 @@ TEST(DataModelWrapperTest, GetDisplayTextEmptyWithoutPhone) {
   ASSERT_TRUE(
       instrument_wrapper.GetInfo(
           AutofillType(PHONE_HOME_WHOLE_NUMBER)).empty());
-  EXPECT_TRUE(instrument_wrapper.GetDisplayText().empty());
+  EXPECT_FALSE(instrument_wrapper.GetDisplayText(&unused, &unused2));
 
   ASSERT_TRUE(
       address_wrapper.GetInfo(AutofillType(PHONE_HOME_WHOLE_NUMBER)).empty());
-  EXPECT_TRUE(address_wrapper.GetDisplayText().empty());
+  EXPECT_FALSE(address_wrapper.GetDisplayText(&unused, &unused2));
 }
 
 }  // namespace autofill
