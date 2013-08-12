f29119b2d99008c02c748cf60d8422608b6ec748
mvanouwerkerk@chromium.org <mvanouwerkerk@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 13:12:41 +0000

    Delete the Windows 7 Geolocation provider.
    This experimental feature is hidden behind the experimental-location-features flag and was never fully developed and has very little usage. Please refer to the bug for more details.
    
    BUG=134075
    
    Review URL: https://chromiumcodereview.appspot.com/22674006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216980 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/geolocation/location_arbitrator_impl.cc
+++ content/browser/geolocation/location_arbitrator_impl.cc
@@ -161,7 +161,11 @@ LocationProvider* GeolocationArbitratorImpl::NewNetworkLocationProvider(
 }
 
 LocationProvider* GeolocationArbitratorImpl::NewSystemLocationProvider() {
+#if defined(OS_WIN)
+  return NULL;
+#else
   return content::NewSystemLocationProvider();
+#endif
 }
 
 base::Time GeolocationArbitratorImpl::GetTimeNow() const {
