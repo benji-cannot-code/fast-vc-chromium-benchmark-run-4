3f6552d5941724269014fb063c1991c1286737a9
sl.ostapenko@samsung.com <sl.ostapenko@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-12 22:21:55 +0000

    Make HAVE(ISDEBUGGERPRESENT) a local macro (instead of a global macro) as it is only used in wtf/Assertions.cpp.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22472006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155957 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Assertions.cpp
+++ third_party/WebKit/Source/wtf/Assertions.cpp
@@ -61,6 +61,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if OS(WINDOWS)
 #include <windows.h>
+#define HAVE_ISDEBUGGERPRESENT 1
 #endif
 
 #if (OS(DARWIN) || (OS(LINUX) && !defined(__UCLIBC__))) && !OS(ANDROID)
