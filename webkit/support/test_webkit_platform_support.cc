75fc9ea59ef6bf0fe8bdb0a35efdf0ff614ed9b4
kinuko@chromium.org <kinuko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 13:13:00 +0000

    Implement UnitTestSupport::readFromFile
    This allows us to remove all PlatformFileHandle related code
    in Blink core.
    
    (Blink side patch: https://codereview.chromium.org/22645008/)
    
    BUG=267341
    
    Review URL: https://chromiumcodereview.appspot.com/22738002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216981 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/support/test_webkit_platform_support.cc
+++ webkit/support/test_webkit_platform_support.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/command_line.h"
 #include "base/file_util.h"
+#include "base/files/file_path.h"
 #include "base/files/scoped_temp_dir.h"
 #include "base/metrics/stats_counters.h"
 #include "base/path_service.h"
@@ -345,3 +346,12 @@ WebKit::WebLayerTreeView*
   return createLayerTreeViewForTesting();
 }
 
+WebKit::WebData TestWebKitPlatformSupport::readFromFile(
+    const WebKit::WebString& path) {
+  base::FilePath file_path = base::FilePath::FromUTF16Unsafe(path);
+
+  std::string buffer;
+  file_util::ReadFileToString(file_path, &buffer);
+
+  return WebKit::WebData(buffer.data(), buffer.size());
+}
