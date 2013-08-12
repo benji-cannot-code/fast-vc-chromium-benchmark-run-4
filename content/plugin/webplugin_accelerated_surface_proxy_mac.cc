d871b80afc5fe71ad2a12a1ed258397c779bb3cc
jam@chromium.org <jam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 17:09:16 +0000

    Move kDisableCoreAnimationPlugins to content_switches since it's only used in content now.
    TBR=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/22862003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217023 0039d316-1c4b-4281-b951-d872f2087c98

--- content/plugin/webplugin_accelerated_surface_proxy_mac.cc
+++ content/plugin/webplugin_accelerated_surface_proxy_mac.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/command_line.h"
 #include "content/plugin/webplugin_proxy.h"
-#include "ui/base/ui_base_switches.h"
+#include "content/public/common/content_switches.h"
 #include "ui/gl/io_surface_support_mac.h"
 #include "ui/surface/accelerated_surface_mac.h"
 #include "ui/surface/transport_dib.h"
