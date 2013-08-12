c8d2a0236f05eaa14165be1448e3fffcfba348dc
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 04:50:46 +0000

    Reland "216874 - Use overview mode for alt-tab cycling."
    Fixes valgrind failure and relands overview mode using alt-tab.
    
    BUG=263481, 264289
    TEST=WindowSelectorTest.Basic,WindowSelectorTest.BasicCycle
    TEST=Enable --ash-enable-overview-mode and use alt-tab to cycle through MRU windows. Cycling is done in overview mode on active display.
    
    Review URL: https://chromiumcodereview.appspot.com/22667003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216916 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/window_selector_controller.h
+++ ash/wm/window_selector_controller.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "ash/ash_export.h"
+#include "ash/wm/window_selector.h"
 #include "ash/wm/window_selector_delegate.h"
 #include "base/basictypes.h"
 #include "base/memory/scoped_ptr.h"
@@ -17,6 +18,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace aura {
 class Window;
+}
+
+namespace ui {
+class EventHandler;
 }
 
 namespace ash {
@@ -40,6 +45,15 @@ class ASH_EXPORT WindowSelectorController
   // or touch rather than keypresses.
   void ToggleOverview();
 
+  // Cycles between windows in the given |direction|. It is assumed that the
+  // alt key is held down and a key filter is installed to watch for alt being
+  // released.
+  void HandleCycleWindow(WindowSelector::Direction direction);
+
+  // Informs the controller that the Alt key has been released and it can
+  // terminate the existing multi-step cycle.
+  void AltKeyReleased();
+
   // Returns true if window selection mode is active.
   bool IsSelecting();
 
@@ -49,6 +63,7 @@ class ASH_EXPORT WindowSelectorController
 
  private:
   scoped_ptr<WindowSelector> window_selector_;
+  scoped_ptr<ui::EventHandler> event_handler_;
 
   DISALLOW_COPY_AND_ASSIGN(WindowSelectorController);
 };
