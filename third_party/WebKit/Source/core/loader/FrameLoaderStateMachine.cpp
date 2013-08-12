51b9ba51601265d464ef72978fc00169966b4b5f
japhet@chromium.org <japhet@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-12 23:23:33 +0000

    Loosen FrameLoaderStateMachine transition requirements
    Instead of requiring transitions of 1 state, allow skipping states as long
    as we never go backwards.
    
    This means we don't have to do several transitions one right after the other
    when we go out of the normal order (e.g., an explicit open in an initial
    empty document).
    
    BUG=264848
    
    Review URL: https://chromiumcodereview.appspot.com/22912002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@155963 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/loader/FrameLoaderStateMachine.cpp
+++ third_party/WebKit/Source/core/loader/FrameLoaderStateMachine.cpp
@@ -62,7 +62,7 @@ bool FrameLoaderStateMachine::isDisplayingInitialEmptyDocument() const
 
 void FrameLoaderStateMachine::advanceTo(State state)
 {
-    ASSERT(State(m_state + 1) == state);
+    ASSERT(m_state < state);
     m_state = state;
 }
 
