4278b0efe1b4bdb3f863871c963e76595e630f28
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 19:21:52 +0000

    [Android] Check native in finishComposingText
    If native side has been destroyed, then early out in
    ImeAdapter.finishComposingText.
    
    BUG=internal b/10143869
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22882002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217051 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/browser/input/ImeAdapter.java
+++ content/public/android/java/src/org/chromium/content/browser/input/ImeAdapter.java
@@ -363,6 +363,7 @@ public class ImeAdapter {
     }
 
     void finishComposingText() {
+        if (mNativeImeAdapterAndroid == 0) return;
         nativeFinishComposingText(mNativeImeAdapterAndroid);
     }
 
