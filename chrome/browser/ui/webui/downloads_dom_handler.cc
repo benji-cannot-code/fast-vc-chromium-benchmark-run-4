b44a5c3cdbb008f2c1cc7e1aa8eeca406ea57b5b
satorux@chromium.org <satorux@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 05:34:01 +0000

    webui: Remove an unnecessary include file
    file_manager_util.h is not used in downloads_dom_handler.cc
    
    BUG=269304
    TEST=none
    R=xiyuan@chromium.org
    
    Review URL: https://codereview.chromium.org/22588006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216939 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/downloads_dom_handler.cc
+++ chrome/browser/ui/webui/downloads_dom_handler.cc
@@ -52,10 +52,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ui/base/l10n/time_format.h"
 #include "ui/gfx/image/image.h"
 
-#if defined(OS_CHROMEOS)
-#include "chrome/browser/chromeos/extensions/file_manager/file_manager_util.h"
-#endif
-
 using content::BrowserContext;
 using content::BrowserThread;
 using content::UserMetricsAction;
