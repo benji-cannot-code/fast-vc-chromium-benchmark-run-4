0e1fdeec0dac98d847439fc9acf1dcddd4d7352f
earthdok@chromium.org <earthdok@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 05:11:11 +0000

    Use TestBrowserThreadBundle in MediaGalleriesPreferences tests.
    Simplify code and potentially plug a memory leak.
    
    BUG=270940
    R=thestig@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22625008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@216926 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media_galleries/media_galleries_preferences_unittest.cc
+++ chrome/browser/media_galleries/media_galleries_preferences_unittest.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/command_line.h"
 #include "base/memory/ref_counted.h"
 #include "base/memory/scoped_ptr.h"
-#include "base/message_loop/message_loop.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/values.h"
 #include "chrome/browser/extensions/extension_system.h"
@@ -23,7 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/extensions/background_info.h"
 #include "chrome/common/extensions/extension.h"
 #include "chrome/test/base/testing_profile.h"
-#include "content/public/test/test_browser_thread.h"
+#include "content/public/test/test_browser_thread_bundle.h"
 #include "grit/generated_resources.h"
 #include "sync/api/string_ordinal.h"
 #include "testing/gtest/include/gtest/gtest.h"
@@ -73,17 +72,11 @@ class MediaGalleriesPreferencesTest : public testing::Test {
       DeviceIdPrefIdsMap;
 
   MediaGalleriesPreferencesTest()
-      : ui_thread_(content::BrowserThread::UI, &loop_),
-        file_thread_(content::BrowserThread::FILE, &loop_),
-        profile_(new TestingProfile()),
+      : profile_(new TestingProfile()),
         default_galleries_count_(0) {
   }
 
   virtual ~MediaGalleriesPreferencesTest() {
-    // TestExtensionSystem uses DeleteSoon, so we need to delete the profile
-    // and then run the message queue to clean up.
-    profile_.reset();
-    base::MessageLoop::current()->RunUntilIdle();
   }
 
   virtual void SetUp() OVERRIDE {
@@ -241,9 +234,7 @@ class MediaGalleriesPreferencesTest : public testing::Test {
 
  private:
   // Needed for extension service & friends to work.
-  base::MessageLoop loop_;
-  content::TestBrowserThread ui_thread_;
-  content::TestBrowserThread file_thread_;
+  content::TestBrowserThreadBundle thread_bundle_;
 
 #if defined OS_CHROMEOS
   chromeos::ScopedTestDeviceSettingsService test_device_settings_service_;
