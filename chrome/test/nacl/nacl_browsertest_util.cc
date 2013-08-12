fadb44550bad55b962a7dd2918dc8389fddafd1c
sehr@chromium.org <sehr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-12 22:52:49 +0000

    disable pnacl by default
    BUG=none
    R=bbudge@chromium.org, bradnelson@chromium.org, bradnelson@google.com, darin@chromium.org, jhawkins@chromium.org, jvoung@chromium.org
    
    Review URL: https://codereview.chromium.org/22839005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217106 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/nacl/nacl_browsertest_util.cc
+++ chrome/test/nacl/nacl_browsertest_util.cc
@@ -188,15 +188,6 @@ static void AddPnaclParm(const base::FilePath::StringType& url,
   }
 }
 
-static void AddPnaclDisabledParm(const base::FilePath::StringType& url,
-                                 base::FilePath::StringType* url_with_parm) {
-  if (url.find(FILE_PATH_LITERAL("?")) == base::FilePath::StringType::npos) {
-    *url_with_parm = url + FILE_PATH_LITERAL("?pnacl_disabled=1");
-  } else {
-    *url_with_parm = url + FILE_PATH_LITERAL("&pnacl_disabled=1");
-  }
-}
-
 NaClBrowserTestBase::NaClBrowserTestBase() {
 }
 
@@ -220,11 +211,7 @@ bool NaClBrowserTestBase::GetDocumentRoot(base::FilePath* document_root) {
   return GetNaClVariantRoot(Variant(), document_root);
 }
 
-bool NaClBrowserTestBase::IsAPnaclTest() {
-  return false;
-}
-
-bool NaClBrowserTestBase::IsPnaclDisabled() {
+bool NaClBrowserTestBase::IsPnacl() {
   return false;
 }
 
@@ -247,15 +234,11 @@ bool NaClBrowserTestBase::RunJavascriptTest(const GURL& url,
 void NaClBrowserTestBase::RunLoadTest(
     const base::FilePath::StringType& test_file) {
   LoadTestMessageHandler handler;
-  base::FilePath::StringType test_file_with_pnacl = test_file;
-  if (IsAPnaclTest()) {
-    AddPnaclParm(test_file, &test_file_with_pnacl);
+  base::FilePath::StringType test_file_with_parm = test_file;
+  if (IsPnacl()) {
+    AddPnaclParm(test_file, &test_file_with_parm);
   }
-  base::FilePath::StringType test_file_with_both = test_file_with_pnacl;
-  if (IsPnaclDisabled()) {
-    AddPnaclDisabledParm(test_file_with_pnacl, &test_file_with_both);
-  }
-  bool ok = RunJavascriptTest(TestURL(test_file_with_both), &handler);
+  bool ok = RunJavascriptTest(TestURL(test_file_with_parm), &handler);
   ASSERT_TRUE(ok) << handler.error_message();
   ASSERT_TRUE(handler.test_passed()) << "Test failed.";
 }
@@ -263,15 +246,11 @@ void NaClBrowserTestBase::RunLoadTest(
 void NaClBrowserTestBase::RunNaClIntegrationTest(
     const base::FilePath::StringType& url_fragment) {
   NaClIntegrationMessageHandler handler;
-  base::FilePath::StringType url_fragment_with_pnacl = url_fragment;
-  if (IsAPnaclTest()) {
-    AddPnaclParm(url_fragment, &url_fragment_with_pnacl);
+  base::FilePath::StringType url_fragment_with_parm = url_fragment;
+  if (IsPnacl()) {
+    AddPnaclParm(url_fragment, &url_fragment_with_parm);
   }
-  base::FilePath::StringType url_fragment_with_both = url_fragment_with_pnacl;
-  if (IsPnaclDisabled()) {
-    AddPnaclDisabledParm(url_fragment_with_pnacl, &url_fragment_with_both);
-  }
-  bool ok = RunJavascriptTest(TestURL(url_fragment_with_both), &handler);
+  bool ok = RunJavascriptTest(TestURL(url_fragment_with_parm), &handler);
   ASSERT_TRUE(ok) << handler.error_message();
   ASSERT_TRUE(handler.test_passed()) << "Test failed.";
 }
@@ -300,25 +279,13 @@ base::FilePath::StringType NaClBrowserTestPnacl::Variant() {
   return FILE_PATH_LITERAL("pnacl");
 }
 
-bool NaClBrowserTestPnacl::IsAPnaclTest() {
+bool NaClBrowserTestPnacl::IsPnacl() {
   return true;
 }
 
-base::FilePath::StringType NaClBrowserTestPnaclDisabled::Variant() {
-  return FILE_PATH_LITERAL("pnacl");
-}
-
-bool NaClBrowserTestPnaclDisabled::IsAPnaclTest() {
-  return true;
-}
-
-bool NaClBrowserTestPnaclDisabled::IsPnaclDisabled() {
-  return true;
-}
-
-void NaClBrowserTestPnaclDisabled::SetUpCommandLine(CommandLine* command_line) {
+void NaClBrowserTestPnacl::SetUpCommandLine(CommandLine* command_line) {
   NaClBrowserTestBase::SetUpCommandLine(command_line);
-  command_line->AppendSwitch(switches::kDisablePnacl);
+  command_line->AppendSwitch(switches::kEnablePnacl);
 }
 
 NaClBrowserTestPnaclWithOldCache::NaClBrowserTestPnaclWithOldCache() {
