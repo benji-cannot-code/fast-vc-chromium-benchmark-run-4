56b42f25f9d450f85607311638217624e4fdb2e9
eseidel@chromium.org <eseidel@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-13 21:57:36 +0000

    RenderBlock should not reach inside FloatingObjects and walk it floating object set manually
    I also fixed the ordering of an if to avoid the expensive descendantChild check.
    
    BUG=271774
    R=bjonesbe@adobe.com, ojan@chromium.org
    
    Review URL: https://codereview.chromium.org/22890004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156050 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/rendering/RenderBlock.h
+++ third_party/WebKit/Source/core/rendering/RenderBlock.h
@@ -1215,6 +1215,8 @@ public:
     class FloatingObjects {
         WTF_MAKE_NONCOPYABLE(FloatingObjects); WTF_MAKE_FAST_ALLOCATED;
     public:
+        ~FloatingObjects();
+
         void clear();
         void add(FloatingObject*);
         void remove(FloatingObject*);
@@ -1230,6 +1232,7 @@ public:
             computePlacedFloatsTreeIfNeeded();
             return m_placedFloatsTree;
         }
+        void clearLineBoxTreePointers();
     private:
         FloatingObjects(const RenderBlock*, bool horizontalWritingMode);
         void computePlacedFloatsTree();
