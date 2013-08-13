86365983f4f6d955a9ee167b727326e521b77dcf
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 05:47:32 +0000

    Fix some clang warnings found by chromium-style plugin.
    This adds some missing OVERRIDE to virtual functions.
    
    BUG=229660
    TBR=brettw@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22927003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217211 0039d316-1c4b-4281-b951-d872f2087c98

--- url/url_canon_icu.h
+++ url/url_canon_icu.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 // ICU integration functions.
 
+#include "base/compiler_specific.h"
 #include "url/url_canon.h"
 #include "url/url_export.h"
 
@@ -28,7 +29,7 @@ class URL_EXPORT ICUCharsetConverter : public CharsetConverter {
 
   virtual void ConvertFromUTF16(const base::char16* input,
                                 int input_len,
-                                CanonOutput* output);
+                                CanonOutput* output) OVERRIDE;
 
  private:
   // The ICU converter, not owned by this class.
