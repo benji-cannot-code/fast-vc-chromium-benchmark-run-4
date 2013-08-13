49342365db94a9c67877e2919e8c01e3d09added
petewil@chromium.org <petewil@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 20:07:07 +0000

    Make mouse clicks dismiss the notification.
    The original design for synced notifications was that to allow for
    actions other than navigation, when we clicked on a synced notification,
    the notification would not be dismissed.
    
    However, we haven't implemented actions other than navigation, and it
    seems odd for synced notifications to behave differently than rich
    notifications, so we'll make navigation actions dismiss the synced
    notification, and when we add other actions later, we can have them not
    dismiss.
    
    BUG=268659
    
    Review URL: https://chromiumcodereview.appspot.com/22915002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217322 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/notifications/sync_notifier/chrome_notifier_delegate.cc
+++ chrome/browser/notifications/sync_notifier/chrome_notifier_delegate.cc
@@ -36,6 +36,7 @@ void ChromeNotifierDelegate::Click() {
 
   GURL destination = notification->GetDefaultDestinationUrl();
   NavigateToUrl(destination);
+  chrome_notifier_->MarkNotificationAsRead(notification_id_);
 }
 
 // TODO(petewil) Add the ability to do URL actions also.
@@ -45,6 +46,7 @@ void ChromeNotifierDelegate::ButtonClick(int button_index) {
   if (notification) {
     GURL destination = notification->GetButtonUrl(button_index);
     NavigateToUrl(destination);
+    chrome_notifier_->MarkNotificationAsRead(notification_id_);
   }
 }
 
