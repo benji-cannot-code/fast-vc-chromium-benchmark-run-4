b6e8ee93430904e6f17947aa6aad65577c382826
kaanb@chromium.org <kaanb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 05:14:36 +0000

    android: Always use physical_memory / 32 as GPU memory limit for low-end devices
    BUG=243703
    
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22905005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217197 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_context_android.cc
+++ ui/gl/gl_context_android.cc
@@ -5,6 +5,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ui/gl/gl_context.h"
 
+#include "base/android/sys_utils.h"
 #include "base/logging.h"
 #include "base/memory/ref_counted.h"
 #include "base/sys_info.h"
@@ -101,17 +102,21 @@ bool GLContextEGL::GetTotalGpuMemory(size_t* bytes) {
   // Droid Razr M(1GB)  91MB
   // Galaxy Nexus(1GB)  85MB
   // Xoom(1GB)          85MB
-  // Nexus S(512MB)     36MB
+  // Nexus S(low-end)   16MB
   static size_t limit_bytes = 0;
   if (limit_bytes == 0) {
-    if (physical_memory_mb >= 1536)
-      limit_bytes = physical_memory_mb / 8;
-    else if (physical_memory_mb >= 1152)
-      limit_bytes = physical_memory_mb / 10;
-    else if (physical_memory_mb >= 768)
-      limit_bytes = physical_memory_mb / 12;
-    else
-      limit_bytes = physical_memory_mb / 16;
+    if (!base::android::SysUtils::IsLowEndDevice()) {
+      if (physical_memory_mb >= 1536)
+        limit_bytes = physical_memory_mb / 8;
+      else if (physical_memory_mb >= 1152)
+        limit_bytes = physical_memory_mb / 10;
+      else if (physical_memory_mb >= 768)
+        limit_bytes = physical_memory_mb / 12;
+      else
+        limit_bytes = physical_memory_mb / 16;
+    } else {
+      limit_bytes = physical_memory_mb / 32;
+    }
     limit_bytes = limit_bytes * 1024 * 1024;
   }
   *bytes = limit_bytes;
