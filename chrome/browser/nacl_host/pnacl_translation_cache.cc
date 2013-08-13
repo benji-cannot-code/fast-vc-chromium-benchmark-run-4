2409b76e1e1df71ef952b23341d0e32b9a83c55c
dschuff@chromium.org <dschuff@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 04:21:13 +0000

    Clean up logging in PnaclHost
    This removes redundance in the error messages (since they are prepended
    with the file location) and includes system errno information where
    appropriate.
    
    R=jvoung@chromium.org
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/22985002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217190 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/nacl_host/pnacl_translation_cache.cc
+++ chrome/browser/nacl_host/pnacl_translation_cache.cc
@@ -200,8 +200,7 @@ void PnaclTranslationCacheEntry::ReadEntry(int offset, int len) {
 void PnaclTranslationCacheEntry::CloseEntry(int rv) {
   DCHECK(entry_);
   if (rv < 0) {
-    LOG(ERROR) << "PnaclTranslationCache: failed to close entry: "
-               << net::ErrorToString(rv);
+    LOG(ERROR) << "Failed to close entry: " << net::ErrorToString(rv);
     entry_->Doom();
   }
   BrowserThread::PostTask(
@@ -231,7 +230,7 @@ void PnaclTranslationCacheEntry::DispatchNext(int rv) {
 
   switch (step_) {
     case UNINITIALIZED:
-      LOG(ERROR) << "PnaclTranslationCache: DispatchNext called uninitialized";
+      LOG(ERROR) << "DispatchNext called uninitialized";
       break;
 
     case OPEN_ENTRY:
@@ -248,8 +247,7 @@ void PnaclTranslationCacheEntry::DispatchNext(int rv) {
       } else {
         if (rv != net::ERR_FAILED) {
           // ERROR_FAILED is what we expect if the entry doesn't exist.
-          LOG(ERROR) << "PnaclTranslationCache: OpenEntry failed: "
-                     << net::ErrorToString(rv);
+          LOG(ERROR) << "OpenEntry failed: " << net::ErrorToString(rv);
         }
         if (is_read_) {
           // Just a cache miss, not necessarily an error.
@@ -267,8 +265,7 @@ void PnaclTranslationCacheEntry::DispatchNext(int rv) {
         step_ = TRANSFER_ENTRY;
         WriteEntry(io_buf_->BytesConsumed(), io_buf_->BytesRemaining());
       } else {
-        LOG(ERROR) << "PnaclTranslationCache: Failed to Create Entry: "
-                   << net::ErrorToString(rv);
+        LOG(ERROR) << "Failed to Create Entry: " << net::ErrorToString(rv);
         Finish(rv);
       }
       break;
@@ -278,8 +275,7 @@ void PnaclTranslationCacheEntry::DispatchNext(int rv) {
         // We do not call DispatchNext directly if WriteEntry/ReadEntry returns
         // ERR_IO_PENDING, and the callback should not return that value either.
         LOG(ERROR)
-            << "PnaclTranslationCache: Failed to complete write to entry: "
-            << net::ErrorToString(rv);
+            << "Failed to complete write to entry: " << net::ErrorToString(rv);
         step_ = CLOSE_ENTRY;
         CloseEntry(rv);
         break;
@@ -353,8 +349,7 @@ int PnaclTranslationCache::Init(net::CacheType cache_type,
 
 void PnaclTranslationCache::OnCreateBackendComplete(int rv) {
   if (rv < 0) {
-    LOG(ERROR) << "PnaclTranslationCache: backend init failed:"
-               << net::ErrorToString(rv);
+    LOG(ERROR) << "Backend init failed:" << net::ErrorToString(rv);
   }
   // Invoke our client's callback function.
   if (!init_callback_.is_null()) {
