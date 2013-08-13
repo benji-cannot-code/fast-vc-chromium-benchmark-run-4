6a80ace3d270562dfd1aa759308d0a474351acc7
wangxianzhu@chromium.org <wangxianzhu@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-13 21:35:48 +0000

    Assert webkit platform in MemoryCache::prune().
    This helps debug bug 272334.
    
    BUG=272334
    
    Review URL: https://chromiumcodereview.appspot.com/23102002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156046 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fetch/MemoryCache.cpp
+++ third_party/WebKit/Source/core/fetch/MemoryCache.cpp
@@ -33,6 +33,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/workers/WorkerGlobalScope.h"
 #include "core/workers/WorkerLoaderProxy.h"
 #include "core/workers/WorkerThread.h"
+#include "public/platform/Platform.h"
 #include "weborigin/SecurityOrigin.h"
 #include "weborigin/SecurityOriginHash.h"
 #include "wtf/Assertions.h"
@@ -561,6 +562,8 @@ void MemoryCache::evictResources()
 
 void MemoryCache::prune()
 {
+    ASSERT(WebKit::Platform::current()); // This method should not be called after WebKit::shutdown().
+
     if (m_liveSize + m_deadSize <= m_capacity && m_maxDeadCapacity && m_deadSize <= m_maxDeadCapacity) // Fast path.
         return;
     if (m_inPruneResources)
