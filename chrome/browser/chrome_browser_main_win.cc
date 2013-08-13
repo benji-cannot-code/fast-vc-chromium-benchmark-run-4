4fa4e93435ad1b971903f1127c78e3f6a19385ce
simonjam@chromium.org <simonjam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-13 02:45:59 +0000

    Enable high resolution time for TimeTicks::Now on Windows Canary
    This should be unnoticeable, except for improved resolution in places such as window.performance.now().
    
    The feature is enabled if the user is running Canary channel or has manually specified the --enable-high-resolution-time flag. This will only work if the CPU has a non-stop TSC and isn't a broken Athlon processor. UMA data show this is a safe combination.
    
    The flag is propagated to renderer processes so that they know to enable it too.
    
    BUG=158234
    
    Review URL: https://chromiumcodereview.appspot.com/16896018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217172 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_browser_main_win.cc
+++ chrome/browser/chrome_browser_main_win.cc
@@ -31,6 +31,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_result_codes.h"
 #include "chrome/common/chrome_switches.h"
+#include "chrome/common/chrome_version_info.h"
 #include "chrome/common/env_vars.h"
 #include "chrome/installer/launcher_support/chrome_launcher_support.h"
 #include "chrome/installer/util/browser_distribution.h"
@@ -142,11 +143,26 @@ int DoUninstallTasks(bool chrome_still_running) {
   return result;
 }
 
+void MaybeEnableHighResolutionTimeEverywhere() {
+  chrome::VersionInfo::Channel channel = chrome::VersionInfo::GetChannel();
+  bool user_enabled = CommandLine::ForCurrentProcess()->HasSwitch(
+      switches::kEnableHighResolutionTime);
+  if (user_enabled || channel == chrome::VersionInfo::CHANNEL_CANARY) {
+    bool is_enabled = base::TimeTicks::SetNowIsHighResNowIfSupported();
+    if (is_enabled && !user_enabled) {
+      // Ensure that all of the renderers will enable it too.
+      CommandLine::ForCurrentProcess()->AppendSwitch(
+          switches::kEnableHighResolutionTime);
+    }
+  }
+}
+
 // ChromeBrowserMainPartsWin ---------------------------------------------------
 
 ChromeBrowserMainPartsWin::ChromeBrowserMainPartsWin(
     const content::MainFunctionParams& parameters)
     : ChromeBrowserMainParts(parameters) {
+  MaybeEnableHighResolutionTimeEverywhere();
   if (base::win::IsMetroProcess()) {
     typedef const wchar_t* (*GetMetroSwitches)(void);
     GetMetroSwitches metro_switches_proc = reinterpret_cast<GetMetroSwitches>(
--- content/browser/browser_child_process_host_impl.cc
+++ content/browser/browser_child_process_host_impl.cc
@@ -154,6 +154,9 @@ void BrowserChildProcessHostImpl::Launch(
 #if defined(OS_POSIX)
     switches::kChildCleanExit,
 #endif
+#if defined(OS_WIN)
+    switches::kEnableHighResolutionTime,
+#endif
   };
   cmd_line->CopySwitchesFrom(browser_command_line, kForwardSwitches,
                              arraysize(kForwardSwitches));
