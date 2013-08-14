65813e99881d967c4638e1117c0f941fe353f5ad
scottmg@chromium.org <scottmg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 05:46:58 +0000

    initialize trace_event hooks in angle on gpu startup
    Requires https://codereview.appspot.com/12699047/
    
    R=apatrick@chromium.org
    BUG=270179
    
    Review URL: https://chromiumcodereview.appspot.com/22744005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217487 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/gl/gl_implementation_win.cc
+++ ui/gl/gl_implementation_win.cc
@@ -9,6 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/base_paths.h"
 #include "base/bind.h"
 #include "base/command_line.h"
+#include "base/debug/trace_event.h"
 #include "base/files/file_path.h"
 #include "base/logging.h"
 #include "base/native_library.h"
@@ -58,6 +59,45 @@ bool LoadD3DXLibrary(const base::FilePath& module_path,
   }
   return true;
 }
+
+const unsigned char* AngleGetTraceCategoryEnabledFlag(const char* name) {
+  return TRACE_EVENT_API_GET_CATEGORY_GROUP_ENABLED(name);
+}
+
+void AngleAddTraceEvent(char phase,
+                        const unsigned char* category_group_enabled,
+                        const char* name,
+                        unsigned long long id,
+                        int num_args,
+                        const char** arg_names,
+                        const unsigned char* arg_types,
+                        const unsigned long long* arg_values,
+                        unsigned char flags) {
+  TRACE_EVENT_API_ADD_TRACE_EVENT(phase,
+                                  category_group_enabled,
+                                  name,
+                                  id,
+                                  num_args,
+                                  arg_names,
+                                  arg_types,
+                                  arg_values,
+                                  NULL,
+                                  flags);
+}
+
+typedef const unsigned char* (*GetCategoryEnabledFlagFunc)(const char* name);
+typedef void (*AddTraceEventFunc)(char phase,
+                                  const unsigned char* categoryGroupEnabled,
+                                  const char* name,
+                                  unsigned long long id,
+                                  int numArgs,
+                                  const char** argNames,
+                                  const unsigned char* argTypes,
+                                  const unsigned long long* argValues,
+                                  unsigned char flags);
+typedef void (*SetTraceFunctionPointersFunc)(
+    GetCategoryEnabledFlagFunc get_category_enabled_flag,
+    AddTraceEventFunc add_trace_event_func);
 
 }  // namespace
 
@@ -167,6 +207,17 @@ bool InitializeGLBindings(GLImplementation implementation) {
         SetupSoftwareRenderer(gles_library);
       }
 #endif
+
+      if (!using_swift_shader) {
+        SetTraceFunctionPointersFunc set_trace_function_pointers =
+            reinterpret_cast<SetTraceFunctionPointersFunc>(
+                base::GetFunctionPointerFromNativeLibrary(
+                    gles_library, "SetTraceFunctionPointers"));
+        if (set_trace_function_pointers) {
+          set_trace_function_pointers(&AngleGetTraceCategoryEnabledFlag,
+                                      &AngleAddTraceEvent);
+        }
+      }
 
       GLGetProcAddressProc get_proc_address =
           reinterpret_cast<GLGetProcAddressProc>(
