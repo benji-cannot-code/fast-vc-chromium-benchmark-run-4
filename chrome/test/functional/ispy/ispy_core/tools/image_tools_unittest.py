6629c141eefb2477106c1f6c05fb40d4e64ca360
cgrimm@chromium.org <cgrimm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 00:08:48 +0000

    [i-spy] Updated Image Tools.
    checkout from testing branch
    
    BUG=260424
    
    Review URL: https://chromiumcodereview.appspot.com/22824007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217395 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/functional/ispy/ispy_core/tools/image_tools_unittest.py
+++ chrome/test/functional/ispy/ispy_core/tools/image_tools_unittest.py
@@ -1,11 +1,15 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-# Copyright 2013 The Chromium Authors. All rights reserved.
+# Copyright (c) 2013 The Chromium Authors. All rights reserved.
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
 import unittest
+import sys
+import os
 from PIL import Image
-import image_tools
+
+sys.path.append(os.path.join(os.path.dirname(__file__), os.pardir))
+from tools import image_tools
 
 
 def _GenImage(size, color):
