45d3773881456026735741b64e0c88bdf7500462
varkha@chromium.org <varkha@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 21:01:16 +0000

    Animate phantom snap window across displays
    BUG=264024
    
    Review URL: https://chromiumcodereview.appspot.com/22429008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217644 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/wm/workspace/workspace_window_resizer.h
+++ ash/wm/workspace/workspace_window_resizer.h
@@ -70,6 +70,7 @@ class ASH_EXPORT WorkspaceWindowResizer : public WindowResizer {
  private:
   FRIEND_TEST_ALL_PREFIXES(WorkspaceWindowResizerTest, CancelSnapPhantom);
   FRIEND_TEST_ALL_PREFIXES(WorkspaceWindowResizerTest, PhantomSnapMaxSize);
+  FRIEND_TEST_ALL_PREFIXES(WorkspaceWindowResizerTest, PhantomWindowShow);
 
   // Type of snapping.
   enum SnapType {
