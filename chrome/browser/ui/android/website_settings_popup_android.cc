fc77d7fb3168175df2bb2ed5a60112d80d2cfad1
creis@chromium.org <creis@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 05:12:31 +0000

    Use visible entry for website settings dialog.
    BUG=265221
    TEST=Click page icon after clicking a link to a page with a 204 response.
    
    Review URL: https://chromiumcodereview.appspot.com/22831005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217485 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/android/website_settings_popup_android.cc
+++ chrome/browser/ui/android/website_settings_popup_android.cc
@@ -37,7 +37,7 @@ static jobjectArray GetCertificateChain(JNIEnv* env,
   content::WebContents* contents =
       content::ContentViewCore::GetNativeContentViewCore(env, view)->
           GetWebContents();
-  int cert_id = contents->GetController().GetActiveEntry()->GetSSL().cert_id;
+  int cert_id = contents->GetController().GetVisibleEntry()->GetSSL().cert_id;
   scoped_refptr<net::X509Certificate> cert;
   bool ok = CertStore::GetInstance()->RetrieveCert(cert_id, &cert);
   CHECK(ok);
@@ -78,7 +78,10 @@ WebsiteSettingsPopupAndroid::WebsiteSettingsPopupAndroid(
     jobject context,
     jobject java_content_view,
     WebContents* web_contents) {
-  if (web_contents->GetController().GetActiveEntry() == NULL)
+  // Important to use GetVisibleEntry to match what's showing in the omnibox.
+  content::NavigationEntry* nav_entry =
+      web_contents->GetController().GetVisibleEntry();
+  if (nav_entry == NULL)
     return;
 
   popup_jobject_.Reset(
@@ -90,8 +93,8 @@ WebsiteSettingsPopupAndroid::WebsiteSettingsPopupAndroid(
       Profile::FromBrowserContext(web_contents->GetBrowserContext()),
       TabSpecificContentSettings::FromWebContents(web_contents),
       InfoBarService::FromWebContents(web_contents),
-      web_contents->GetURL(),
-      web_contents->GetController().GetActiveEntry()->GetSSL(),
+      nav_entry->GetURL(),
+      nav_entry->GetSSL(),
       content::CertStore::GetInstance()));
 }
 
