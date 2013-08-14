666687c2846ab3d37ff02c0f92a73b58f87787f1
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-14 02:40:58 +0000

    Add toSVGClipPathElement(), and use it
    As a step to clean-up static_cast<SVGXXX>, static_cast<SVGClipPathElement*> also can
    be changed with toSVGClipPathElement().
    
    As minor clean-up, this cl changes to use toSVGSVGElement() instead of static_cast<SVGSVGElement*>.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23063002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156069 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGClipPathElement.h
+++ third_party/WebKit/Source/core/svg/SVGClipPathElement.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGClipPathElement_h
 #define SVGClipPathElement_h
 
+#include "SVGNames.h"
 #include "core/svg/SVGAnimatedBoolean.h"
 #include "core/svg/SVGAnimatedEnumeration.h"
 #include "core/svg/SVGExternalResourcesRequired.h"
@@ -55,6 +56,12 @@ private:
         DECLARE_ANIMATED_BOOLEAN(ExternalResourcesRequired, externalResourcesRequired)
     END_DECLARE_ANIMATED_PROPERTIES
 };
+
+inline SVGClipPathElement* toSVGClipPathElement(Node* node)
+{
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || node->hasTagName(SVGNames::clipPathTag));
+    return static_cast<SVGClipPathElement*>(node);
+}
 
 }
 
