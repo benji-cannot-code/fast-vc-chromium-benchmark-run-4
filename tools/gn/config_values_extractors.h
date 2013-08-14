84bebab8e82872139657da48c41ff61e23583334
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 03:54:11 +0000

    Fix a compilation error when compiling gn on Linux with clang.
    ../../tools/gn/escape.h:22:1: error: 'EscapeOptions' defined as a struct here but previously declared as a class [-Werror,-Wmismatched-tags]
    struct EscapeOptions {
    ^
    
    ../../tools/gn/config_values_extractors.h:16:1: note: did you mean struct here?
    class EscapeOptions;
    ^~~~~
    struct
    
    R=scottmg@chromium.org,brettw@chromium.org
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22826005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217459 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/config_values_extractors.h
+++ tools/gn/config_values_extractors.h
@@ -14,7 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "tools/gn/config_values.h"
 #include "tools/gn/target.h"
 
-class EscapeOptions;
+struct EscapeOptions;
 
 template<typename T, class Writer>
 inline void ConfigValuesToStream(
