0b10e5ca4761cc24f2ee955bb16d5d87dc283dd2
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-14 04:09:51 +0000

    minor autofill string cleanup
    BUG=260379
    
    Review URL: https://chromiumcodereview.appspot.com/22909002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217466 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autofill/autofill_cc_infobar_delegate.cc
+++ chrome/browser/autofill/autofill_cc_infobar_delegate.cc
@@ -14,9 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_delegate.h"
 #include "grit/generated_resources.h"
+#include "grit/google_chrome_strings.h"
 #include "grit/theme_resources.h"
 #include "ui/base/l10n/l10n_util.h"
-
 
 namespace autofill {
 
