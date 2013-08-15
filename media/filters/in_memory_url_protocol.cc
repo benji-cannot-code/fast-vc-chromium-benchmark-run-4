1f33354a4fd60733cb8002795f676f61a4a11cfa
chihchung@chromium.org <chihchung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 11:48:10 +0000

    Allow SetPosition to be called with the file size.
    When reading AVI files, ffmpeg uses SetPosition() to skip chunks it does
    not need (like an index chunk). If the last chunk of the file is skipped,
    SetPosition will be called with position equals to the file size, and
    an error message will be shown to the user.
    
    The meaning of the SetPosition call defined by the ffmpeg interface is
    the same as lseek() or fseek(), so it should be able to seek anywhere
    beyond end of file. But to be conservative we only allow seeking to eof
    but not more than that for now.
    
    BUG=chrome-os-partner:21699
    TEST=media_unittests --gtest_filter=BlockingUrlProtocolTest*
    TEST=manually check the avi file plays without error
    
    Review URL: https://chromiumcodereview.appspot.com/23088006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217789 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/in_memory_url_protocol.cc
+++ media/filters/in_memory_url_protocol.cc
@@ -36,7 +36,7 @@ bool InMemoryUrlProtocol::GetPosition(int64* position_out) {
 }
 
 bool InMemoryUrlProtocol::SetPosition(int64 position) {
-  if (position < 0 || position >= size_)
+  if (position < 0 || position > size_)
     return false;
   position_ = position;
   return true;
