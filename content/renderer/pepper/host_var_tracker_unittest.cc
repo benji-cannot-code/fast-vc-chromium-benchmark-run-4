f82b960a6df91796c668e90a733d09bafb6401eb
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 16:23:33 +0000

    Move ppapi_unittest to content/test.
    Code for supporting tests belongs in content/test, not content/renderer/pepper.
    
    BUG=
    R=jam@chromium.org
    
    Review URL: https://codereview.chromium.org/22662007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217800 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/host_var_tracker_unittest.cc
+++ content/renderer/pepper/host_var_tracker_unittest.cc
@@ -3,8 +3,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "content/renderer/pepper/ppapi_unittest.h"
-
 #include "base/memory/scoped_ptr.h"
 #include "content/renderer/pepper/host_globals.h"
 #include "content/renderer/pepper/host_var_tracker.h"
@@ -12,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/pepper/npapi_glue.h"
 #include "content/renderer/pepper/npobject_var.h"
 #include "content/renderer/pepper/pepper_plugin_instance_impl.h"
+#include "content/test/ppapi_unittest.h"
 #include "ppapi/c/pp_var.h"
 #include "ppapi/c/ppp_instance.h"
 #include "third_party/npapi/bindings/npruntime.h"
--- content/test/ppapi_unittest.h
+++ content/test/ppapi_unittest.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CONTENT_RENDERER_PEPPER_PPAPI_UNITTEST_H_
-#define CONTENT_RENDERER_PEPPER_PPAPI_UNITTEST_H_
+#ifndef CONTENT_TEST_PPAPI_UNITTEST_H_
+#define CONTENT_TEST_PPAPI_UNITTEST_H_
 
 #include "base/basictypes.h"
 #include "base/memory/ref_counted.h"
@@ -53,4 +53,4 @@ class PpapiUnittest : public testing::Test {
 
 }  // namespace content
 
-#endif  // CONTENT_RENDERER_PEPPER_PPAPI_UNITTEST_H_
+#endif  // CONTENT_TEST_PPAPI_UNITTEST_H_
