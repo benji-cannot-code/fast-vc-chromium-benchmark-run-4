ab25c228349d86081324ee091d5b841039cbaa1f
hclam@chromium.org <hclam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-15 19:03:47 +0000

    Revert "Throw an exception when denying access to 'Frame's 'location' setter."
    This reverts commit r156151. This change seems to be causing
    unit test failing on Win 7 (dbg) bot.
    
    TBR=mkwst@chromium.org
    
    Review URL: https://codereview.chromium.org/22887017
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156161 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/ExceptionStatePlaceholder.cpp
+++ third_party/WebKit/Source/bindings/v8/ExceptionStatePlaceholder.cpp
@@ -51,11 +51,6 @@ void NoExceptionStateAssertionChecker::throwTypeError(const String&)
     ASSERT_AT(false, m_file, m_line, "");
 }
 
-void NoExceptionStateAssertionChecker::throwSecurityError(const String&, const String&)
-{
-    ASSERT_AT(false, m_file, m_line, "");
-}
-
 #endif
 
 } // namespace WebCore
--- third_party/WebKit/Source/bindings/v8/custom/V8HTMLFrameElementCustom.cpp
+++ third_party/WebKit/Source/bindings/v8/custom/V8HTMLFrameElementCustom.cpp
@@ -34,10 +34,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "HTMLNames.h"
 #include "bindings/v8/BindingSecurity.h"
-#include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/V8Binding.h"
 #include "core/html/HTMLFrameElement.h"
-#include "core/html/parser/HTMLParserIdioms.h"
 
 namespace WebCore {
 
@@ -48,11 +46,8 @@ void V8HTMLFrameElement::locationAttrSetterCustom(v8::Local<v8::String> name, v8
     HTMLFrameElement* frame = V8HTMLFrameElement::toNative(info.Holder());
     String locationValue = toWebCoreStringWithNullCheck(value);
 
-    ExceptionState es(info.GetIsolate());
-    if (protocolIsJavaScript(stripLeadingAndTrailingHTMLSpaces(locationValue)) && !BindingSecurity::shouldAllowAccessToFrame(frame->contentFrame(), es)) {
-        es.throwIfNeeded();
+    if (!BindingSecurity::allowSettingFrameSrcToJavascriptUrl(frame, locationValue))
         return;
-    }
 
     frame->setLocation(locationValue);
 }
