e9561b3e3048850738961d370738b2137de06cce
arbesfeld@chromium.org <arbesfeld@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-15 23:31:15 +0000

    Expose createImageBitmap in worker threads.
    R=junov@chromium.org,senorblanco@chromium.org
    BUG=269517,166658
    
    Review URL: https://chromiumcodereview.appspot.com/22827005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156181 bbb929c8-8fbe-4397-9dbb-9b2b20218538

---
+++ third_party/WebKit/LayoutTests/fast/canvas/resources/canvas-createImageBitmap-invalid-blob-in-workers.js
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+importScripts('../../js/resources/js-test-pre.js');
+
+self.jsTestIsAsync = true;
+
+description('Test createImageBitmap with invalid blobs in workers.');
+
+self.addEventListener('message', function(e) {
+  createImageBitmap(e.data).then(function() {
+    testFailed('Promise fulfuilled.');
+    finishJSTest();
+  }, function() {
+    testPassed('Promise rejected.');
+    finishJSTest();
+  });
+});
--- third_party/WebKit/Source/modules/imagebitmap/ImageBitmapFactories.h
+++ third_party/WebKit/Source/modules/imagebitmap/ImageBitmapFactories.h
@@ -54,7 +54,7 @@ class ImageBitmap;
 class ImageData;
 class ScriptExecutionContext;
 
-class ImageBitmapFactories : public Supplement<DOMWindow> {
+class ImageBitmapFactories : public Supplement<DOMWindow>, public Supplement<ScriptExecutionContext> {
 
 class ImageBitmapLoader;
 
@@ -74,11 +74,9 @@ public:
     static ScriptObject createImageBitmap(EventTarget*, ImageBitmap*, ExceptionState&);
     static ScriptObject createImageBitmap(EventTarget*, ImageBitmap*, int sx, int sy, int sw, int sh, ExceptionState&);
 
-    void addLoader(PassRefPtr<ImageBitmapLoader>);
     void didFinishLoading(ImageBitmapLoader*);
 
     virtual ~ImageBitmapFactories() { }
-    static ImageBitmapFactories* from(DOMWindow*);
 
 private:
     class ImageBitmapLoader : public RefCounted<ImageBitmapLoader>, public FileReaderLoaderClient {
@@ -111,6 +109,12 @@ private:
     };
 
     static const char* supplementName();
+    static ImageBitmapFactories* from(EventTarget*);
+
+    template <class T>
+    static ImageBitmapFactories* fromInternal(T*);
+
+    void addLoader(PassRefPtr<ImageBitmapLoader>);
 
     HashSet<RefPtr<ImageBitmapLoader> > m_pendingLoaders;
 };
