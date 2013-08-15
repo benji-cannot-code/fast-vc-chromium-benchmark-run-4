f30a0ba5623f78053b6fdc2e8f7b5f90d19f3e48
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 05:45:56 +0000

    Fixing race conditions in chrome.alarms. This fix makes all alarms operations wait till alarms data is loaded from storage. Also fixing a bug in scheduling.
    BUG=236684
    
    Review URL: https://chromiumcodereview.appspot.com/22900005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217750 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/alarms/alarms_api.h
+++ chrome/browser/extensions/api/alarms/alarms_api.h
@@ -6,6 +6,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_EXTENSIONS_API_ALARMS_ALARMS_API_H__
 #define CHROME_BROWSER_EXTENSIONS_API_ALARMS_ALARMS_API_H__
 
+#include <vector>
+
 #include "chrome/browser/extensions/extension_function.h"
 
 namespace base {
@@ -13,8 +15,10 @@ class Clock;
 }  // namespace base
 
 namespace extensions {
+struct Alarm;
+typedef std::vector<Alarm> AlarmList;
 
-class AlarmsCreateFunction : public SyncExtensionFunction {
+class AlarmsCreateFunction : public AsyncExtensionFunction {
  public:
   AlarmsCreateFunction();
   // Use |clock| instead of the default clock. Does not take ownership
@@ -27,6 +31,8 @@ class AlarmsCreateFunction : public SyncExtensionFunction {
   virtual bool RunImpl() OVERRIDE;
   DECLARE_EXTENSION_FUNCTION("alarms.create", ALARMS_CREATE)
  private:
+  void Callback();
+
   base::Clock* const clock_;
   // Whether or not we own |clock_|. This is needed because we own it
   // when we create it ourselves, but not when it's passed in for
@@ -34,39 +40,48 @@ class AlarmsCreateFunction : public SyncExtensionFunction {
   bool owns_clock_;
 };
 
-class AlarmsGetFunction : public SyncExtensionFunction {
+class AlarmsGetFunction : public AsyncExtensionFunction {
  protected:
   virtual ~AlarmsGetFunction() {}
 
   // ExtensionFunction:
   virtual bool RunImpl() OVERRIDE;
+
+ private:
+  void Callback(const std::string& name, Alarm* alarm);
   DECLARE_EXTENSION_FUNCTION("alarms.get", ALARMS_GET)
 };
 
-class AlarmsGetAllFunction : public SyncExtensionFunction {
+class AlarmsGetAllFunction : public AsyncExtensionFunction {
  protected:
   virtual ~AlarmsGetAllFunction() {}
 
   // ExtensionFunction:
   virtual bool RunImpl() OVERRIDE;
+ private:
+  void Callback(const AlarmList* alarms);
   DECLARE_EXTENSION_FUNCTION("alarms.getAll", ALARMS_GETALL)
 };
 
-class AlarmsClearFunction : public SyncExtensionFunction {
+class AlarmsClearFunction : public AsyncExtensionFunction {
  protected:
   virtual ~AlarmsClearFunction() {}
 
   // ExtensionFunction:
   virtual bool RunImpl() OVERRIDE;
+ private:
+  void Callback(const std::string& name, bool success);
   DECLARE_EXTENSION_FUNCTION("alarms.clear", ALARMS_CLEAR)
 };
 
-class AlarmsClearAllFunction : public SyncExtensionFunction {
+class AlarmsClearAllFunction : public AsyncExtensionFunction {
  protected:
   virtual ~AlarmsClearAllFunction() {}
 
   // ExtensionFunction:
   virtual bool RunImpl() OVERRIDE;
+ private:
+  void Callback();
   DECLARE_EXTENSION_FUNCTION("alarms.clearAll", ALARMS_CLEARALL)
 };
 
