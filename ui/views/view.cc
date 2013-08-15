7572346a46415244f9002b74af83e9d11da03778
erg@chromium.org <erg@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 07:09:29 +0000

    linux_aura: Fix crash on drags from the omnibox.
    When we receive a mouse move event, there may be more events behind it
    in the queue. Prevent those further mouse events from also triggering a
    nested drag message loop.
    
    BUG=268797
    
    Review URL: https://chromiumcodereview.appspot.com/22934007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217757 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/view.cc
+++ ui/views/view.cc
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <cmath>
 
+#include "base/auto_reset.h"
 #include "base/debug/trace_event.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
@@ -186,6 +187,7 @@ View::View()
       accessibility_focusable_(false),
       context_menu_controller_(NULL),
       drag_controller_(NULL),
+      currently_dragging_(false),
       post_dispatch_handler_(new internal::PostEventDispatchHandler(this)),
       native_view_accessibility_(NULL) {
   AddPostTargetHandler(post_dispatch_handler_.get());
@@ -194,6 +196,10 @@ View::View()
 View::~View() {
   if (parent_)
     parent_->RemoveChildView(this);
+
+  // If we destroy the view during a drag, AutoReset will probably write to
+  // freed memory.
+  DCHECK(!currently_dragging_);
 
   for (Views::const_iterator i(children_.begin()); i != children_.end(); ++i) {
     (*i)->parent_ = NULL;
@@ -2319,10 +2325,14 @@ bool View::DoDrag(const ui::LocatedEvent& event,
                   const gfx::Point& press_pt,
                   ui::DragDropTypes::DragEventSource source) {
 #if !defined(OS_MACOSX)
+  if (currently_dragging_)
+    return false;
+
   int drag_operations = GetDragOperations(press_pt);
   if (drag_operations == ui::DragDropTypes::DRAG_NONE)
     return false;
 
+  base::AutoReset<bool> updating_focus(&currently_dragging_, true);
   OSExchangeData data;
   WriteDragData(press_pt, &data);
 
@@ -2332,6 +2342,7 @@ bool View::DoDrag(const ui::LocatedEvent& event,
   ConvertPointToWidget(this, &widget_location);
   GetWidget()->RunShellDrag(this, data, widget_location, drag_operations,
       source);
+
   return true;
 #else
   return false;
