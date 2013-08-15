3a8c009d110d8d956336ac7f4d4754acca42ffea
simonhatch@chromium.org <simonhatch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 22:36:17 +0000

    Getting a lot of failures syncing third_party/libjingle. Just delete the directory and resync it with gclient to get around this.
    BUG=266324
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22939006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217861 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/bisect_utils.py
+++ tools/bisect_utils.py
@@ -226,6 +226,23 @@ def RemoveThirdPartyWebkitDirectory():
   return True
 
 
+def RemoveThirdPartyLibjingleDirectory():
+  """Removes third_party/libjingle. At some point, libjingle was causing issues
+  syncing when using the git workflow (crbug.com/266324).
+
+  Returns:
+    True on success.
+  """
+  try:
+    path_to_dir = os.path.join(os.getcwd(), 'third_party', 'libjingle')
+    if os.path.exists(path_to_dir):
+      shutil.rmtree(path_to_dir)
+  except OSError, e:
+    if e.errno != errno.ENOENT:
+      return False
+  return True
+
+
 def RunGClientAndSync(cwd=None):
   """Runs gclient and does a normal sync.
 
@@ -266,6 +283,8 @@ def SetupGitDepot(opts):
         passed_deps_check = RemoveThirdPartyWebkitDirectory()
       else:
         passed_deps_check = True
+      if passed_deps_check:
+        passed_deps_check = RemoveThirdPartyLibjingleDirectory()
       os.chdir(cwd)
 
     if passed_deps_check:
