e098150a039f63109533564ec2ac768ed6fa42c4
kinuko@chromium.org <kinuko@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-15 06:05:19 +0000

    Switch WebFrameClient::openFileSystem to Platform::openFileSystem
    chrome side patch: https://codereview.chromium.org/22666004/
    
    BUG=257349
    
    Review URL: https://chromiumcodereview.appspot.com/22669002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156142 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/modules/filesystem/DOMWindowFileSystem.cpp
+++ third_party/WebKit/Source/modules/filesystem/DOMWindowFileSystem.cpp
@@ -70,7 +70,7 @@ void DOMWindowFileSystem::webkitRequestFileSystem(DOMWindow* window, int type, l
         return;
     }
 
-    LocalFileSystem::from(document)->requestFileSystem(document, fileSystemType, size, FileSystemCallbacks::create(successCallback, errorCallback, document, fileSystemType), AsynchronousFileSystem);
+    LocalFileSystem::from(document)->requestFileSystem(document, fileSystemType, size, FileSystemCallbacks::create(successCallback, errorCallback, document, fileSystemType));
 }
 
 void DOMWindowFileSystem::webkitResolveLocalFileSystemURL(DOMWindow* window, const String& url, PassRefPtr<EntryCallback> successCallback, PassRefPtr<ErrorCallback> errorCallback)
--- third_party/WebKit/Source/web/web.gypi
+++ third_party/WebKit/Source/web/web.gypi
@@ -268,8 +268,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
       'WebWorkerRunLoop.cpp',
       'WorkerAllowMainThreadBridgeBase.cpp',
       'WorkerAllowMainThreadBridgeBase.h',
-      'WorkerFileSystemCallbacksBridge.cpp',
-      'WorkerFileSystemCallbacksBridge.h',
       'WorkerFileSystemClient.cpp',
       'WorkerFileSystemClient.h',
       'android/WebInputEventFactory.cpp',
