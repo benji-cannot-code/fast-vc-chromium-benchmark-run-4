672808fe464ff3ec92c9310fea0fde8c3cec2648
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 00:37:33 +0000

    rAc: AutofillDialogViews performance improvements
    1) cache preferred size to avoid excessive recomputations
    2) reorganize GetPreferredSize() to do slightly less work
    3) don't react to each and every browser resize event. Coalesce these resizes.
    
    BUG=269674
    
    Review URL: https://chromiumcodereview.appspot.com/22984003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217711 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/autofill/autofill_dialog_views.h
+++ chrome/browser/ui/views/autofill/autofill_dialog_views.h
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_vector.h"
 #include "base/memory/weak_ptr.h"
 #include "base/scoped_observer.h"
+#include "base/timer/timer.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_types.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_view.h"
 #include "chrome/browser/ui/autofill/autofill_dialog_view_delegate.h"
@@ -484,6 +485,8 @@ class AutofillDialogViews : public AutofillDialogView,
 
   typedef std::map<DialogSection, DetailsGroup> DetailGroupMap;
 
+  gfx::Size CalculatePreferredSize();
+
   void InitChildViews();
 
   // Creates and returns a view that holds all detail sections.
@@ -561,6 +564,7 @@ class AutofillDialogViews : public AutofillDialogView,
 
   // Call this when the size of anything in |contents_| might've changed.
   void ContentsPreferredSizeChanged();
+  void DoContentsPreferredSizeChanged();
 
   // Gets the textfield view that is shown for the given DetailInput model, or
   // NULL.
@@ -576,9 +580,15 @@ class AutofillDialogViews : public AutofillDialogView,
   // The delegate that drives this view. Weak pointer, always non-NULL.
   AutofillDialogViewDelegate* const delegate_;
 
+  // The preferred size of the view, cached to avoid needless recomputation.
+  gfx::Size preferred_size_;
+
   // The window that displays |contents_|. Weak pointer; may be NULL when the
   // dialog is closing.
   views::Widget* window_;
+
+  // A timer used to coalesce re-layouts due to browser window resizes.
+  base::Timer browser_resize_timer_;
 
   // A DialogSection-keyed map of the DetailGroup structs.
   DetailGroupMap detail_groups_;
