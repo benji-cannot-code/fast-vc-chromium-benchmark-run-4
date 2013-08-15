c4dc95b26b9d82fd15b24e179c8ba5a9fdb069a3
estade@chromium.org <estade@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-15 17:34:12 +0000

    Revert 217770 "Landing https://codereview.chromium.org/18769010/."
    > Landing https://codereview.chromium.org/18769010/.
    > 
    > This turned out to be a tricky patch to land, so I'm landing https://codereview.chromium.org/18769010/ on behalf of elham@google.com. The original patch has all the requisite LGTMs so I'm just force-committing it here.
    > 
    > BUG=WebRTC bug 1198
    > TEST=Tested with Chrome and FireFox browser
    > 
    > Review URL: https://codereview.chromium.org/23172002
    
    TBR=phoglund@chromium.org
    
    Review URL: https://codereview.chromium.org/22840006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217808 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/data/webrtc/manual/constraints.js
+++ chrome/test/data/webrtc/manual/constraints.js
@@ -13,18 +13,18 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /**
  * Asks permission to use the webcam and mic from the browser.
  */
-function doGetUserMedia() {
-  // Call into getUserMedia via the polyfill (adapter.js).
+function getUserMedia() {
   var constraints = getConstraints_();
   var constraintsString = JSON.stringify(constraints, null, ' ');
   $('getusermedia-constraints').innerHTML = constraintsString;
-  if (!getUserMedia) {
+  if (!navigator.webkitGetUserMedia) {
     log_('Browser does not support WebRTC.');
     return;
   }
   log_('Requesting getUserMedia with constraints: ' + constraintsString);
-  getUserMedia(constraints, getUserMediaOkCallback_,
-               getUserMediaFailedCallback_);
+  navigator.webkitGetUserMedia(constraints,
+                               getUserMediaOkCallback_,
+                               getUserMediaFailedCallback_);
 }
 
 // Internals
@@ -105,7 +105,7 @@ function getConstraints_() {
 function getUserMediaOkCallback_(stream) {
   gLocalStream = stream;
   var videoTag = $('local-view');
-  attachMediaStream(videoTag, stream);
+  videoTag.src = webkitURL.createObjectURL(stream);
 
   // Due to crbug.com/110938 the size is 0 when onloadedmetadata fires.
   // videoTag.onloadedmetadata = updateVideoTagSize_(videoTag);
--- chrome/test/data/webrtc/manual/peerconnection.js
+++ chrome/test/data/webrtc/manual/peerconnection.js
@@ -12,7 +12,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function getUserMediaFromHere() {
   var constraints = $('getusermedia-constraints').value;
   try {
-    doGetUserMedia(constraints);
+    getUserMedia(constraints);
   } catch (exception) {
     print_('getUserMedia says: ' + exception);
   }
