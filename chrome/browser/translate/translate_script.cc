969e230218e8031c601d2762e5ff26c55171c153
toyoshim@chromium.org <toyoshim@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 13:40:29 +0000

    Translate: use server providing hooks to use custom loaders
    Remove code to hijack resource loading, and use server providing hooks
    to use callbacks to load resources.
    
    BUG=271956
    
    Review URL: https://chromiumcodereview.appspot.com/22783002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218009 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/translate/translate_script.cc
+++ chrome/browser/translate/translate_script.cc
@@ -34,6 +34,20 @@ const char kCallbackQueryName[] = "cb";
 const char kCallbackQueryValue[] =
     "cr.googleTranslate.onTranslateElementLoad";
 
+// Used in kTranslateScriptURL to specify using always ssl to load resources.
+const char kAlwaysUseSslQueryName[] = "aus";
+const char kAlwaysUseSslQueryValue[] = "true";
+
+// Used in kTranslateScriptURL to specify a CSS loader callback function name.
+const char kCssLoaderCallbackQueryName[] = "clc";
+const char kCssLoaderCallbackQueryValue[] = "cr.googleTranslate.onLoadCSS";
+
+// Used in kTranslateScriptURL to specify a JavaScript loader callback function
+// name.
+const char kJavascriptLoaderCallbackQueryName[] = "jlc";
+const char kJavascriptLoaderCallbackQueryValue[] =
+    "cr.googleTranslate.onLoadJavascript";
+
 }  // namespace
 
 TranslateScript::TranslateScript()
@@ -77,6 +91,18 @@ void TranslateScript::Request(const Callback& callback) {
       translate_script_url,
       kCallbackQueryName,
       kCallbackQueryValue);
+  translate_script_url = net::AppendQueryParameter(
+      translate_script_url,
+      kAlwaysUseSslQueryName,
+      kAlwaysUseSslQueryValue);
+  translate_script_url = net::AppendQueryParameter(
+      translate_script_url,
+      kCssLoaderCallbackQueryName,
+      kCssLoaderCallbackQueryValue);
+  translate_script_url = net::AppendQueryParameter(
+      translate_script_url,
+      kJavascriptLoaderCallbackQueryName,
+      kJavascriptLoaderCallbackQueryValue);
 
   translate_script_url =
       TranslateURLUtil::AddHostLocaleToUrl(translate_script_url);
