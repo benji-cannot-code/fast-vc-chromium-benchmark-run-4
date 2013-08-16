18c4708d5165bad261b33937095eb39672e29557
thestig@chromium.org <thestig@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 05:08:56 +0000

    Cleanup: Do not check for NULL pointers returned by new in printing code.
    See http://www.viva64.com/en/b/0205/
    
    BUG=271530
    R=vandebo
    
    Review URL: https://chromiumcodereview.appspot.com/22900002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217938 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/printing/print_preview_dialog_controller.cc
+++ chrome/browser/printing/print_preview_dialog_controller.cc
@@ -10,7 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/auto_reset.h"
-#include "base/command_line.h"
 #include "base/path_service.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chrome_notification_types.h"
@@ -28,7 +27,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/webui/print_preview/print_preview_ui.h"
 #include "chrome/common/chrome_content_client.h"
 #include "chrome/common/chrome_paths.h"
-#include "chrome/common/chrome_switches.h"
 #include "chrome/common/url_constants.h"
 #include "content/public/browser/navigation_controller.h"
 #include "content/public/browser/navigation_details.h"
@@ -397,17 +395,6 @@ WebContents* PrintPreviewDialogController::CreatePrintPreviewDialog(
   base::AutoReset<bool> auto_reset(&is_creating_print_preview_dialog_, true);
   Profile* profile =
       Profile::FromBrowserContext(initiator->GetBrowserContext());
-  if (CommandLine::ForCurrentProcess()->HasSwitch(switches::kChromeFrame)) {
-    // Chrome Frame only ever runs on the native desktop, so it is safe to
-    // create the popup on the native desktop.
-    Browser* current_browser = new Browser(
-        Browser::CreateParams(Browser::TYPE_POPUP, profile,
-                              chrome::GetActiveDesktop()));
-    if (!current_browser) {
-      NOTREACHED() << "Failed to create popup browser window";
-      return NULL;
-    }
-  }
 
   // |web_dialog_ui_delegate| deletes itself in
   // PrintPreviewDialogDelegate::OnDialogClosed().
