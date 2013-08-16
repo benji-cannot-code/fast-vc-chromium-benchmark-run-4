7e899ef382a84f85d66d4b9760e5dcb1c06cd79d
qyearsley@chromium.org <qyearsley@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 01:54:46 +0000

    Move memory-related histogram metrics into metrics/memory.py
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22934009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217911 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/perf/measurements/page_cycler.py
+++ tools/perf/measurements/page_cycler.py
@@ -19,18 +19,10 @@ cycling all pages.
 import os
 import sys
 
-from metrics import histogram
 from metrics import io
 from metrics import memory
 from telemetry.core import util
 from telemetry.page import page_measurement
-
-
-MEMORY_HISTOGRAMS = [
-    {'name': 'V8.MemoryExternalFragmentationTotal', 'units': 'percent'},
-    {'name': 'V8.MemoryHeapSampleTotalCommitted', 'units': 'kb'},
-    {'name': 'V8.MemoryHeapSampleTotalUsed', 'units': 'kb'}]
-
 
 class PageCycler(page_measurement.PageMeasurement):
   def __init__(self, *args, **kwargs):
@@ -41,7 +33,6 @@ class PageCycler(page_measurement.PageMeasurement):
       self._page_cycler_js = f.read()
 
     self._memory_metric = None
-    self._histograms = None
 
   def AddCommandLineOptions(self, parser):
     # The page cyclers should default to 10 iterations. In order to change the
@@ -55,10 +46,6 @@ class PageCycler(page_measurement.PageMeasurement):
   def DidStartBrowser(self, browser):
     """Initialize metrics once right after the browser has been launched."""
     self._memory_metric = memory.MemoryMetric(browser)
-    self._memory_metric.Start()
-    self._histograms = [histogram.HistogramMetric(
-                           h, histogram.RENDERER_HISTOGRAM)
-                       for h in MEMORY_HISTOGRAMS]
 
   def DidStartHTTPServer(self, tab):
     # Avoid paying for a cross-renderer navigation on the first page on legacy
@@ -69,8 +56,7 @@ class PageCycler(page_measurement.PageMeasurement):
     page.script_to_evaluate_on_commit = self._page_cycler_js
 
   def DidNavigateToPage(self, page, tab):
-    for h in self._histograms:
-      h.Start(page, tab)
+    self._memory_metric.Start(page, tab)
 
   def CustomizeBrowserOptions(self, options):
     options.AppendExtraBrowserArg('--enable-stats-collection-bindings')
@@ -85,21 +71,18 @@ class PageCycler(page_measurement.PageMeasurement):
       print 'typical_25 is currently disabled on mac. Skipping test.'
       sys.exit(0)
 
-
   def MeasurePage(self, page, tab, results):
     def _IsDone():
       return bool(tab.EvaluateJavaScript('__pc_load_time'))
     util.WaitFor(_IsDone, 60)
-
-    for h in self._histograms:
-      h.GetValue(page, tab, results)
-
     results.Add('page_load_time', 'ms',
                 int(float(tab.EvaluateJavaScript('__pc_load_time'))),
                 chart_name='times')
 
-  def DidRunTest(self, tab, results):
-    self._memory_metric.Stop()
+    self._memory_metric.Stop(page, tab)
     self._memory_metric.AddResults(tab, results)
+
+  def DidRunTest(self, tab, results):
+    self._memory_metric.AddSummaryResults(results)
     io.IOMetric().AddSummaryResults(tab, results)
 
