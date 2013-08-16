f6f23f245998b189eefdc7616fcccfd54f9906fd
tyoverby@chromium.org <tyoverby@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 07:34:52 +0000

    Logs the decoder used if the decoder used is GpuVideoDecoder.
    BUG=260005
    
    Review URL: https://chromiumcodereview.appspot.com/23171006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217964 0039d316-1c4b-4281-b951-d872f2087c98

--- media/filters/gpu_video_decoder.cc
+++ media/filters/gpu_video_decoder.cc
@@ -15,6 +15,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/task_runner_util.h"
 #include "media/base/bind_to_loop.h"
 #include "media/base/decoder_buffer.h"
+#include "media/base/media_log.h"
 #include "media/base/pipeline.h"
 #include "media/base/pipeline_status.h"
 #include "media/base/video_decoder_config.h"
@@ -53,12 +54,14 @@ GpuVideoDecoder::BufferData::BufferData(
 GpuVideoDecoder::BufferData::~BufferData() {}
 
 GpuVideoDecoder::GpuVideoDecoder(
-    const scoped_refptr<GpuVideoAcceleratorFactories>& factories)
+    const scoped_refptr<GpuVideoAcceleratorFactories>& factories,
+    const scoped_refptr<MediaLog>& media_log)
     : needs_bitstream_conversion_(false),
       gvd_loop_proxy_(factories->GetMessageLoop()),
       weak_factory_(this),
       factories_(factories),
       state_(kNormal),
+      media_log_(media_log),
       decoder_texture_target_(0),
       next_picture_buffer_id_(0),
       next_bitstream_buffer_id_(0),
@@ -182,6 +185,7 @@ void GpuVideoDecoder::Initialize(const VideoDecoderConfig& config,
   }
 
   DVLOG(3) << "GpuVideoDecoder::Initialize() succeeded.";
+  media_log_->SetStringProperty("video_decoder", "gpu");
   status_cb.Run(PIPELINE_OK);
 }
 
