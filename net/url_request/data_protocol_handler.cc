b6da2232a0924216a16864891d790fa1c8b7ab19
tsepez@chromium.org <tsepez@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 00:09:02 +0000

    Fix regression: don't redirect to data: URLs.
    BUG=272072
    
    Review URL: https://chromiumcodereview.appspot.com/22794009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217875 0039d316-1c4b-4281-b951-d872f2087c98

--- net/url_request/data_protocol_handler.cc
+++ net/url_request/data_protocol_handler.cc
@@ -17,4 +17,8 @@ URLRequestJob* DataProtocolHandler::MaybeCreateJob(
   return new URLRequestDataJob(request, network_delegate);
 }
 
+bool DataProtocolHandler::IsSafeRedirectTarget(const GURL& location) const {
+  return false;
+}
+
 }  // namespace net
