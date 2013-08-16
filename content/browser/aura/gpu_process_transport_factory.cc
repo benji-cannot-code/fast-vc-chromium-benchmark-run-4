f30c9ae4acf3c195a31a612b8d8a59ac1325ce9e
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 04:58:13 +0000

    Remove base::Callback from ContextProvider creation.
    Currently a Callback is given to ContextProvider's creation methods
    that is able to create a WebGraphicsContext3D and return it to the
    provider. This callback is called synchronously during the create
    method, and only one time.
    
    Instead of requiring this indirection through a Callback, we can instead
    just pass a WebGraphicsContext3D to the ContextProvider when it is
    created, directly.
    
    R=piman, sievers
    BUG=258625
    
    Review URL: https://chromiumcodereview.appspot.com/23247002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217934 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/aura/gpu_process_transport_factory.cc
+++ content/browser/aura/gpu_process_transport_factory.cc
@@ -225,10 +225,9 @@ scoped_ptr<cc::OutputSurface> GpuProcessTransportFactory::CreateOutputSurface(
   CommandLine* command_line = CommandLine::ForCurrentProcess();
   if (!command_line->HasSwitch(switches::kUIEnableSoftwareCompositing)) {
     context_provider = ContextProviderCommandBuffer::Create(
-        base::Bind(&GpuProcessTransportFactory::CreateContextCommon,
-                   base::Unretained(this),
-                   data->swap_client->AsWeakPtr(),
-                   data->surface_id));
+        GpuProcessTransportFactory::CreateContextCommon(
+            data->swap_client->AsWeakPtr(),
+            data->surface_id));
   }
   if (!context_provider.get()) {
     if (ui::Compositor::WasInitializedWithThread()) {
@@ -392,9 +391,7 @@ GpuProcessTransportFactory::OffscreenContextProviderForMainThread() {
   if (!shared_contexts_main_thread_.get() ||
       shared_contexts_main_thread_->DestroyedOnMainThread()) {
     shared_contexts_main_thread_ = ContextProviderCommandBuffer::Create(
-        base::Bind(&GpuProcessTransportFactory::
-                       CreateOffscreenCommandBufferContext,
-                   base::Unretained(this)));
+        GpuProcessTransportFactory::CreateOffscreenCommandBufferContext());
     if (shared_contexts_main_thread_) {
       shared_contexts_main_thread_->SetLostContextCallback(base::Bind(
           &GpuProcessTransportFactory::
@@ -413,9 +410,7 @@ GpuProcessTransportFactory::OffscreenContextProviderForCompositorThread() {
   if (!shared_contexts_compositor_thread_.get() ||
       shared_contexts_compositor_thread_->DestroyedOnMainThread()) {
     shared_contexts_compositor_thread_ = ContextProviderCommandBuffer::Create(
-        base::Bind(&GpuProcessTransportFactory::
-                       CreateOffscreenCommandBufferContext,
-                   base::Unretained(this)));
+        GpuProcessTransportFactory::CreateOffscreenCommandBufferContext());
   }
   return shared_contexts_compositor_thread_;
 }
