20023ffd0606c7c3f1ecd9765d1490e867387a69
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 01:43:07 +0000

    Fix deadlock and DCHECK issues when shutting down local discovery process
    1. Fix deadlock in local discovery process by introducing separate shutdown step
    for local discovery.
    
    2. Fix DCHECK error in local discovery process by shutting down MDns on the IO
    thread.
    
    3. Remove and then disable failing DHECKs requiring the service discovery
    prcoess host to be shut down on its owning thread, as we cannot guarantee that
    easily, and it is safe to shut it down on the IO thread so long as it has no
    listeners (for which there are DCHECKs).
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22021004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217910 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/local_discovery/service_discovery_host_client.cc
+++ chrome/browser/local_discovery/service_discovery_host_client.cc
@@ -128,9 +128,12 @@ ServiceDiscoveryHostClient::ServiceDiscoveryHostClient() : current_id_(0) {
 }
 
 ServiceDiscoveryHostClient::~ServiceDiscoveryHostClient() {
-  DCHECK(CalledOnValidThread());
+  // The ServiceDiscoveryHostClient may be destroyed from the IO thread or the
+  // owning thread.
+  DetachFromThread();
   DCHECK(service_watcher_callbacks_.empty());
   DCHECK(service_resolver_callbacks_.empty());
+  DCHECK(domain_resolver_callbacks_.empty());
 }
 
 scoped_ptr<ServiceWatcher> ServiceDiscoveryHostClient::CreateServiceWatcher(
@@ -231,8 +234,10 @@ void ServiceDiscoveryHostClient::StartOnIOThread() {
 
 void ServiceDiscoveryHostClient::ShutdownOnIOThread() {
   DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
-  if (utility_host_)
+  if (utility_host_) {
+    utility_host_->Send(new LocalDiscoveryMsg_ShutdownLocalDiscovery);
     utility_host_->EndBatchMode();
+  }
 }
 
 void ServiceDiscoveryHostClient::Send(IPC::Message* msg) {
--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.cc
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.cc
@@ -51,6 +51,8 @@ LocalDiscoveryUIHandler::LocalDiscoveryUIHandler(
 }
 
 LocalDiscoveryUIHandler::~LocalDiscoveryUIHandler() {
+  if (service_discovery_client_)
+    service_discovery_client_->Shutdown();
 }
 
 // static
