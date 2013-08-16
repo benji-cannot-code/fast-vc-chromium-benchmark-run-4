a9dfb41b8e6b6d8ffe0bcce7b6da8f9aefed0891
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 22:15:18 +0000

    Remove dependency on variation_ids.h in some variations code.
    This way, variation_ids.h can stay under chrome/ when a subset of this
    code will move to a new target that will also be used by the ChromeOS
    daemon process.
    
    BUG=266007
    TEST=Existing unit tests.
    
    Review URL: https://chromiumcodereview.appspot.com/22980009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218102 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/metrics/variations/uniformity_field_trials.cc
+++ chrome/common/metrics/variations/uniformity_field_trials.cc
@@ -5,9 +5,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "chrome/common/metrics/variations/uniformity_field_trials.h"
 
+#include <string>
+
 #include "base/metrics/field_trial.h"
 #include "base/strings/stringprintf.h"
 #include "base/time/time.h"
+#include "chrome/common/metrics/variations/variation_ids.h"
 #include "chrome/common/metrics/variations/variations_util.h"
 
 namespace chrome_variations {
--- chrome/common/metrics/variations/variations_associated_data.h
+++ chrome/common/metrics/variations/variations_associated_data.h
@@ -10,8 +10,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "base/metrics/field_trial.h"
-#include "base/strings/string16.h"
-#include "chrome/common/metrics/variations/variation_ids.h"
 
 // This file provides various helpers that extend the functionality around
 // base::FieldTrial.
@@ -44,6 +42,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // }
 
 namespace chrome_variations {
+
+typedef int VariationID;
+
+const VariationID EMPTY_ID = 0;
 
 // The Unique ID of a trial and its active group, where the name and group
 // identifiers are hashes of the trial and group name strings.
