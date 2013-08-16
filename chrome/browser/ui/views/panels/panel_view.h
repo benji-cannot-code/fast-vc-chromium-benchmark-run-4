a483308c2e217ceaac6b0034f844373d82974178
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 07:43:28 +0000

    Fix the problem that new panels do not get focus on win-aura
    The fix is to listen to activation change instead of focus change since the latter is not triggered on win-aura.
    
    BUG=269017
    TEST=Manual test by creating panels with initial focus
    
    Review URL: https://chromiumcodereview.appspot.com/23223005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217966 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/panels/panel_view.h
+++ chrome/browser/ui/views/panels/panel_view.h
@@ -9,7 +9,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "chrome/browser/ui/panels/native_panel.h"
 #include "ui/base/animation/animation_delegate.h"
-#include "ui/views/focus/widget_focus_manager.h"
 #include "ui/views/widget/widget_delegate.h"
 #include "ui/views/widget/widget_observer.h"
 
@@ -29,7 +28,6 @@ class WebView;
 class PanelView : public NativePanel,
                   public views::WidgetObserver,
                   public views::WidgetDelegateView,
-                  public views::WidgetFocusChangeListener,
 #if defined(OS_WIN)
                   public ui::HWNDMessageFilter,
 #endif
@@ -151,10 +149,6 @@ class PanelView : public NativePanel,
                                          bool active) OVERRIDE;
   virtual void OnWidgetBoundsChanged(views::Widget* widget,
                                      const gfx::Rect& new_bounds) OVERRIDE;
-
-  // Overridden from views::WidgetFocusChangeListener:
-  virtual void OnNativeFocusChange(gfx::NativeView focused_before,
-                                   gfx::NativeView focused_now) OVERRIDE;
 
   // Overridden from ui::HWNDMessageFilter:
 #if defined(OS_WIN)
