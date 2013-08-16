8b0e52c4df0e5a9ebff64bed88b137357f99f342
akalin@chromium.org <akalin@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 21:24:41 +0000

    Add a priority parameter for HostResolver::RequestInfo's constructor
    Use DEFAULT_PRIORITY when there isn't already a priority
    available.
    
    This has the net effect of changing the priority of
    host resolver requests that didn't specify one from
    MEDIUM to DEFAULT_PRIORITY (= LOWEST). This is okay,
    as those requests apparently didn't care what priority
    they wanted.
    
    BUG=166689
    TBR=mmenke@chromium.org
    
    Review URL: https://codereview.chromium.org/23146004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218093 0039d316-1c4b-4281-b951-d872f2087c98

--- net/http/http_network_transaction_unittest.cc
+++ net/http/http_network_transaction_unittest.cc
@@ -7283,8 +7283,12 @@ void HttpNetworkTransactionTest::BypassHostCacheOnRefreshHelper(
   AddressList addrlist;
   TestCompletionCallback callback;
   int rv = session_deps_.host_resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)), &addrlist,
-      callback.callback(), NULL, BoundNetLog());
+      HostResolver::RequestInfo(HostPortPair("www.google.com", 80),
+                                DEFAULT_PRIORITY),
+      &addrlist,
+      callback.callback(),
+      NULL,
+      BoundNetLog());
   EXPECT_EQ(ERR_IO_PENDING, rv);
   rv = callback.WaitForResult();
   EXPECT_EQ(OK, rv);
@@ -7292,8 +7296,12 @@ void HttpNetworkTransactionTest::BypassHostCacheOnRefreshHelper(
   // Verify that it was added to host cache, by doing a subsequent async lookup
   // and confirming it completes synchronously.
   rv = session_deps_.host_resolver->Resolve(
-      HostResolver::RequestInfo(HostPortPair("www.google.com", 80)), &addrlist,
-      callback.callback(), NULL, BoundNetLog());
+      HostResolver::RequestInfo(HostPortPair("www.google.com", 80),
+                                DEFAULT_PRIORITY),
+      &addrlist,
+      callback.callback(),
+      NULL,
+      BoundNetLog());
   ASSERT_EQ(OK, rv);
 
   // Inject a failure the next time that "www.google.com" is resolved. This way
@@ -10486,7 +10494,7 @@ WRAPPED_TEST_P(HttpNetworkTransactionTest, MAYBE_UseIPConnectionPooling) {
 
   // Preload www.gmail.com into HostCache.
   HostPortPair host_port("www.gmail.com", 443);
-  HostResolver::RequestInfo resolve_info(host_port);
+  HostResolver::RequestInfo resolve_info(host_port, DEFAULT_PRIORITY);
   AddressList ignored;
   rv = session_deps_.host_resolver->Resolve(resolve_info, &ignored,
                                            callback.callback(), NULL,
@@ -10731,7 +10739,8 @@ WRAPPED_TEST_P(HttpNetworkTransactionTest,
   EXPECT_EQ("hello!", response_data);
 
   // Preload cache entries into HostCache.
-  HostResolver::RequestInfo resolve_info(HostPortPair("www.gmail.com", 443));
+  HostResolver::RequestInfo resolve_info(HostPortPair("www.gmail.com", 443),
+                                         DEFAULT_PRIORITY);
   AddressList ignored;
   rv = host_resolver.Resolve(resolve_info, &ignored, callback.callback(),
                              NULL, BoundNetLog());
