5049316f67d0c62831d806955fd1db7d94b4fa89
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 20:11:44 +0000

    Add a lock to avoid races in FakeWindowCapturer
    DesktopCapturePickerModel uses WindowCapturer on a background 
    thread. In DesktopCapturePickerModel tests FakeWindowCapturer is
    also called on the main test thread to supply fake capture data.
    FakeWindowCapturer didn't synchronize threads which causes TSan 
    warnings (see attached bug) and may crash the test. Added a lock
    to avoid races.
    
    BUG=269965
    
    Review URL: https://chromiumcodereview.appspot.com/23012006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218074 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/media/desktop_media_picker_model_unittest.cc
+++ chrome/browser/media/desktop_media_picker_model_unittest.cc
@@ -75,7 +75,8 @@ class FakeWindowCapturer : public webrtc::WindowCapturer {
 
   void SetNextFrame(WindowId window_id,
                     scoped_ptr<webrtc::DesktopFrame> frame) {
-      frames_[window_id] = frame.release();
+    base::AutoLock lock(frames_lock_);
+    frames_[window_id] = frame.release();
   }
 
   // webrtc::WindowCapturer implementation.
@@ -85,6 +86,8 @@ class FakeWindowCapturer : public webrtc::WindowCapturer {
 
   virtual void Capture(const webrtc::DesktopRegion& region) OVERRIDE {
     DCHECK(callback_);
+
+    base::AutoLock lock(frames_lock_);
 
     webrtc::DesktopFrame* frame;
     std::map<WindowId, webrtc::DesktopFrame*>::iterator it =
@@ -119,6 +122,7 @@ class FakeWindowCapturer : public webrtc::WindowCapturer {
 
   // Frames to be captured per window.
   std::map<WindowId, webrtc::DesktopFrame*> frames_;
+  base::Lock frames_lock_;
 
   DISALLOW_COPY_AND_ASSIGN(FakeWindowCapturer);
 };
