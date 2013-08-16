e6c8c2fc7d9edfc3c5838a86e07a6eab59c50b37
pfeldman@chromium.org <pfeldman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-16 17:52:04 +0000

    Printed representation of SVG elements in Properties is wrong
    BUG=271242
    
    Review URL: https://chromiumcodereview.appspot.com/22865007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156230 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
+++ third_party/WebKit/Source/core/inspector/InjectedScriptSource.js
@@ -849,7 +849,7 @@ InjectedScript.prototype = {
             case 1 /* Node.ELEMENT_NODE */:
                 description += obj.id ? "#" + obj.id : "";
                 var className = obj.className;
-                description += className ? "." + className : "";
+                description += (className && typeof className === "string") ? "." + className : "";
                 break;
             case 10 /*Node.DOCUMENT_TYPE_NODE */:
                 description = "<!DOCTYPE " + description + ">";
