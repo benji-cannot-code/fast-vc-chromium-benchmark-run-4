b591eac693e6c3b2d26d107709dc283e14ca58cf
prybin@chromium.org <prybin@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-16 14:41:07 +0000

    Step into selection: accurately separate user selection with default highlight of first-to-execute position
    Review URL: https://chromiumcodereview.appspot.com/22859015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156221 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/ScriptsPanel.js
+++ third_party/WebKit/Source/devtools/front_end/ScriptsPanel.js
@@ -1003,7 +1003,7 @@ WebInspector.ScriptsPanel.prototype = {
             return;
         var stepIntoMarkup = this._executionSourceFrame.stepIntoMarkup();
         if (stepIntoMarkup)
-            stepIntoMarkup.moveSelectionTo(-1);
+            stepIntoMarkup.startIteratingSelection();
     },
 
     _onKeyUp: function(event)
@@ -1017,7 +1017,7 @@ WebInspector.ScriptsPanel.prototype = {
             return;
         var currentPosition = stepIntoMarkup.getSelectedItemIndex();
         if (typeof currentPosition === "undefined") {
-            stepIntoMarkup.selectFirstToExecute();
+            stepIntoMarkup.stopIteratingSelection();
         } else {
             var rawLocation = stepIntoMarkup.getRawPosition(currentPosition);
             this.doStepIntoSelection(rawLocation);
