aa2b08cfed7be490c512e6e020f8d1d3206b9c23
maksymb@chromium.org <maksymb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 10:22:58 +0000

    GCP2.0 Device: Local settings management.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22555003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217989 0039d316-1c4b-4281-b951-d872f2087c98

--- cloud_print/gcp20/prototype/cloud_print_response_parser.h
+++ cloud_print/gcp20/prototype/cloud_print_response_parser.h
@@ -10,6 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <vector>
 
 #include "base/callback.h"
+#include "cloud_print/gcp20/prototype/local_settings.h"
 
 namespace base {
 
@@ -35,8 +36,7 @@ struct Job {
 };
 
 // Parses CloudPrint register start response to out parameters.
-// Returns |true| on success. Callback is called with description as a parameter
-// when parsing is failed.
+// Returns |true| on success.
 bool ParseRegisterStartResponse(const std::string& response,
                                 std::string* error_description,
                                 std::string* polling_url,
@@ -45,19 +45,24 @@ bool ParseRegisterStartResponse(const std::string& response,
                                 std::string* device_id);
 
 // Parses CloudPrint register complete response to out parameters.
-// Returns |true| on success. Callback is called with description as a parameter
-// when parsing is failed.
+// Returns |true| on success.
 bool ParseRegisterCompleteResponse(const std::string& response,
                                    std::string* error_description,
                                    std::string* authorization_code,
                                    std::string* xmpp_jid);
 
 // Parses CloudPrint fetch response to out parameters.
-// Returns |true| on success. Callback is called with description as a parameter
-// when parsing is failed.
+// Returns |true| on success.
 bool ParseFetchResponse(const std::string& response,
                         std::string* error_description,
                         std::vector<Job>* list);
+
+// Parses CloudPrint printer response to get Local Settings.
+// Returns |true| on success.
+bool ParseLocalSettingsResponse(const std::string& response,
+                                std::string* error_description,
+                                LocalSettings::State* state,
+                                LocalSettings* settings);
 
 }  // namespace cloud_print_response_parser
 
