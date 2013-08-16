cd132df14919bb14436369faa3734ecfca680521
mek@chromium.org <mek@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 00:09:50 +0000

    Make sure that apps that are implementation details are not visible in the management API, similar to how these apps are hidden from the chrome://extensions page and the developer-private API. Also refactor code to have this check only in one spot instead of in the three different places that had similar logic.
    BUG=271788
    
    Review URL: https://chromiumcodereview.appspot.com/23072003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217877 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/extensions/api/management/management_api.cc
+++ chrome/browser/extensions/api/management/management_api.cc
@@ -210,8 +210,8 @@ void AddExtensionInfo(const ExtensionSet& extensions,
        iter != extensions.end(); ++iter) {
     const Extension& extension = *iter->get();
 
-    if (extension.location() == Manifest::COMPONENT)
-      continue;  // Skip built-in extensions.
+    if (extension.ShouldNotBeVisible())
+      continue;  // Skip built-in extensions/apps.
 
     extension_list->push_back(make_linked_ptr<management::ExtensionInfo>(
         CreateExtensionInfo(extension, system).release()));
@@ -460,7 +460,7 @@ bool ManagementSetEnabledFunction::RunImpl() {
   extension_id_ = params->id;
 
   const Extension* extension = service()->GetInstalledExtension(extension_id_);
-  if (!extension) {
+  if (!extension || extension->ShouldNotBeVisible()) {
     error_ = ErrorUtils::FormatErrorMessage(
         keys::kNoExtensionError, extension_id_);
     return false;
@@ -525,7 +525,7 @@ bool ManagementUninstallFunctionBase::Uninstall(
     bool show_confirm_dialog) {
   extension_id_ = extension_id;
   const Extension* extension = service()->GetExtensionById(extension_id_, true);
-  if (!extension) {
+  if (!extension || extension->ShouldNotBeVisible()) {
     error_ = ErrorUtils::FormatErrorMessage(
         keys::kNoExtensionError, extension_id_);
     return false;
@@ -676,6 +676,9 @@ void ManagementEventRouter::Observe(
   }
   DCHECK(event_name);
   DCHECK(extension);
+
+  if (extension->ShouldNotBeVisible())
+    return; // Don't dispatch events for built-in extensions.
 
   scoped_ptr<base::ListValue> args(new base::ListValue());
   if (event_name == events::kOnExtensionUninstalled) {
