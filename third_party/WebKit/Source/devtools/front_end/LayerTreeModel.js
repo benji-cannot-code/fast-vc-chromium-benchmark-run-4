007d4092abdb1bd4918fa87a0ebbd41ff0f3ed83
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-16 17:13:10 +0000

    DevTools: throttle layer tree requests to once per 100ms
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23235004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156229 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/LayerTreeModel.js
+++ third_party/WebKit/Source/devtools/front_end/LayerTreeModel.js
@@ -38,6 +38,7 @@ WebInspector.LayerTreeModel = function()
     this._layersById = {};
     InspectorBackend.registerLayerTreeDispatcher(new WebInspector.LayerTreeDispatcher(this));
     LayerTreeAgent.enable();
+    this._needsRefresh = true;
 }
 
 WebInspector.LayerTreeModel.Events = {
@@ -78,9 +79,10 @@ WebInspector.LayerTreeModel.prototype = {
      */
     requestLayers: function(callback)
     {
+        delete this._delayedRequestLayersTimer;
         if (!callback)
             callback = function() {}
-        if (typeof this._root !== "undefined") {
+        if (!this._needsRefresh) {
             callback();
             return;
         }
@@ -146,8 +148,10 @@ WebInspector.LayerTreeModel.prototype = {
 
     _layerTreeChanged: function()
     {
-        delete this._root;
-        this.requestLayers();
+        if (this._delayedRequestLayersTimer)
+            return;
+        this._needsRefresh = true;
+        this._delayedRequestLayersTimer = setTimeout(this.requestLayers.bind(this), 100);
     },
 
     __proto__: WebInspector.Object.prototype
