5bddca0db25251a0d2aec113383b72f3a76c42aa
simonjam@chromium.org <simonjam@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 00:48:17 +0000

    Revert 217824 "cc: RenderingStats overhaul"
    Speculative. May have broken smoothness_measurement on Mac 10.7.
    
    > cc: RenderingStats overhaul
    > - Split RenderingStats into MainThreadRenderingStats and
    >   ImplThreadRenderingStats
    > - Support accumulated stats and per-frame stats in RenderingStatsInstrumentation
    > - Dump MainThreadRenderingStats into trace event from 
    >   ThreadProxy::BeginFrameOnMainThread
    > - Dump ImplThreadRenderingStats into trace event from
    >   LayerTreeHostImpl::DrawLayers
    > - Made naming of RenderingStats members consistent
    > 
    > R=nduca@chromium.org,jamesr@chromium.org
    > BUG=266101,264308
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23088002
    
    TBR=ernstm@chromium.org
    
    Review URL: https://codereview.chromium.org/23155003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217892 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/trees/thread_proxy.cc
+++ cc/trees/thread_proxy.cc
@@ -696,7 +696,6 @@ void ThreadProxy::BeginFrameOnMainThread(
     scoped_ptr<BeginFrameAndCommitState> begin_frame_state) {
   TRACE_EVENT0("cc", "ThreadProxy::BeginFrameOnMainThread");
   DCHECK(IsMainThread());
-
   if (!layer_tree_host_)
     return;
 
@@ -818,7 +817,7 @@ void ThreadProxy::BeginFrameOnMainThread(
   // point of view, but asynchronously performed on the impl thread,
   // coordinated by the Scheduler.
   {
-    TRACE_EVENT_BEGIN0("cc", "ThreadProxy::BeginFrameOnMainThread::commit");
+    TRACE_EVENT0("cc", "ThreadProxy::BeginFrameOnMainThread::commit");
 
     DebugScopedSetMainThreadBlocked main_thread_blocked(this);
 
@@ -838,10 +837,6 @@ void ThreadProxy::BeginFrameOnMainThread(
 
     base::TimeDelta duration = stats_instrumentation->EndRecording(start_time);
     stats_instrumentation->AddCommit(duration);
-    TRACE_EVENT_END1("cc", "ThreadProxy::BeginFrameOnMainThread::commit",
-                     "data", stats_instrumentation->
-                     GetMainThreadRenderingStats().AsTraceableData());
-    stats_instrumentation->AccumulateAndClearMainThreadStats();
   }
 
   layer_tree_host_->CommitComplete();
