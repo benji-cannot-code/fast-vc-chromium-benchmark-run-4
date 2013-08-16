90066367475e160c6716a8e63f9d50b787915459
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-16 16:16:21 +0000

    Improve parallelization of CSP and XSSAuditor reporting tests.
    This patch passes the test name along with the report, meaning that each
    test will write its own temporary file. This has the happy effect of
    allowing us to run `http/tests/security` tests while providing the
    `--fully-parallel` flag to the test runner.
    
    Ah, speed.
    
    Review URL: https://chromiumcodereview.appspot.com/23172003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156226 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/report-only-from-header.php
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/report-only-from-header.php
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 <?php
-header("Content-Security-Policy-Report-Only: script-src 'self'; report-uri resources/save-report.php");
+header("Content-Security-Policy-Report-Only: script-src 'self'; report-uri resources/save-report.php?test=report-only-from-header.php");
 ?>
 <script>
 // This script block will trigger a violation report but shouldn't be blocked.
--- third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/save-report.php
+++ third_party/WebKit/LayoutTests/http/tests/security/contentSecurityPolicy/resources/save-report.php
@@ -6,7 +6,7 @@ function undoMagicQuotes($value) {
     return $value;
 }
 
-$reportFile = fopen("csp-report.txt.tmp", 'w');
+$reportFile = fopen("csp-report." . $_GET["test"] . ".tmp", 'w');
 $httpHeaders = $_SERVER;
 ksort($httpHeaders, SORT_STRING);
 foreach ($httpHeaders as $name => $value) {
@@ -25,5 +25,5 @@ foreach ($_COOKIE as $name => $value)
 fwrite($reportFile, "=== POST DATA ===\n");
 fwrite($reportFile, file_get_contents("php://input"));
 fclose($reportFile);
-rename("csp-report.txt.tmp", "csp-report.txt");
+rename("csp-report." . $_GET["test"] . ".tmp", "csp-report." . $_GET["test"] . ".txt");
 ?>
