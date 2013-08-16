46494716a64a10a2b7b884fc06edd24fe63aa80c
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-16 11:12:47 +0000

    Implement AppListControllerDelegateCocoa::CreateNewWindow for OSX.
    This fixes the context menu for the "Chrome" app on the OSX App
    Launcher.
    
    BUG=274057
    TEST=Context menu items shown for the "Chrome" app on the OSX App
    Launcher should now work as expected.
    
    Review URL: https://chromiumcodereview.appspot.com/23074004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@217994 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/app_list_service_mac.mm
+++ chrome/browser/ui/app_list/app_list_service_mac.mm
@@ -26,6 +26,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/app_list/app_list_service.h"
 #include "chrome/browser/ui/app_list/app_list_service_impl.h"
 #include "chrome/browser/ui/app_list/app_list_view_delegate.h"
+#include "chrome/browser/ui/browser_commands.h"
 #include "chrome/browser/ui/extensions/application_launch.h"
 #include "chrome/browser/ui/web_applications/web_app_ui.h"
 #include "chrome/browser/web_applications/web_app.h"
@@ -115,6 +116,7 @@ class AppListControllerDelegateCocoa : public AppListControllerDelegate {
   virtual gfx::NativeWindow GetAppListWindow() OVERRIDE;
   virtual bool CanPin() OVERRIDE;
   virtual bool CanDoCreateShortcutsFlow(bool is_platform_app) OVERRIDE;
+  virtual void CreateNewWindow(Profile* profile, bool incognito) OVERRIDE;
   virtual void DoCreateShortcutsFlow(Profile* profile,
                                      const std::string& extension_id) OVERRIDE;
   virtual void ActivateApp(Profile* profile,
@@ -274,6 +276,13 @@ void AppListControllerDelegateCocoa::DoCreateShortcutsFlow(
 
   web_app::UpdateShortcutInfoAndIconForApp(
       *extension, profile, base::Bind(&CreateShortcutsInDefaultLocation));
+}
+
+void AppListControllerDelegateCocoa::CreateNewWindow(
+    Profile* profile, bool incognito) {
+  Profile* window_profile = incognito ?
+      profile->GetOffTheRecordProfile() : profile;
+  chrome::NewEmptyWindow(window_profile, chrome::GetActiveDesktop());
 }
 
 void AppListControllerDelegateCocoa::ActivateApp(
