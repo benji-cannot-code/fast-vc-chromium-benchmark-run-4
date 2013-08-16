020764a1ecbf14e920a9dffdadf66b96f49fdf24
pstanek@opera.com <pstanek@opera.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-16 00:18:37 +0000

    Trigger spell check/remove markers if spell checker gets enabled/disabled.
    With this change when spell checker is turned off the misspelling markers
    disappear from the document (which is what a user expects and this currently
    works in chromium because UI code takes care of that - with this change it may
    stop). OTOH if spell checker is turned on again one would expect misspellings
    will appear right away - in the currently focused editable at least. This is how
    Gecko and Presto behave and this is what this patch does. Note it's not only
    about restoring old markers because content might have changed or language might
    have been switched. This is why new check is requested. This change may also
    come in handy if spell checking language is changed. It would require restarting
    spell checker on UI side on language change, though.
    
    Since there might be an awful lot of text in the focused editable when spell
    checker gets enabled, spell checking is chunked up. If asynchronous spell
    checker is enabled whole text is checked. Otherwise only one chunk is checked
    (the default chunk's size is set to 16KB but it might also be slightly bigger
    since all chunks are aligned to the sentence boundary).
    
    BUG=264737
    
    Review URL: https://chromiumcodereview.appspot.com/21130005
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156183 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/EditorClientImpl.cpp
+++ third_party/WebKit/Source/web/EditorClientImpl.cpp
@@ -47,9 +47,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "WebViewClient.h"
 #include "WebViewImpl.h"
 #include "core/dom/Document.h"
+#include "core/dom/DocumentMarkerController.h"
 #include "core/dom/EventNames.h"
 #include "core/dom/KeyboardEvent.h"
 #include "core/editing/Editor.h"
+#include "core/editing/SpellCheckRequester.h"
+#include "core/editing/TextCheckingHelper.h"
 #include "core/editing/UndoStep.h"
 #include "core/html/HTMLInputElement.h"
 #include "core/page/EventHandler.h"
@@ -139,10 +142,26 @@ bool EditorClientImpl::isContinuousSpellCheckingEnabled()
 
 void EditorClientImpl::toggleContinuousSpellChecking()
 {
-    if (isContinuousSpellCheckingEnabled())
+    if (isContinuousSpellCheckingEnabled()) {
         m_spellCheckThisFieldStatus = SpellCheckForcedOff;
-    else
+        if (Page* page = m_webView->page()) {
+            for (Frame* frame = page->mainFrame(); frame && frame->document(); frame = frame->tree()->traverseNext()) {
+                frame->document()->markers()->removeMarkers(DocumentMarker::Spelling | DocumentMarker::Grammar);
+            }
+        }
+    } else {
         m_spellCheckThisFieldStatus = SpellCheckForcedOn;
+        if (Frame* frame = m_webView->focusedWebCoreFrame()) {
+            if (unifiedTextCheckerEnabled(frame)) {
+                VisibleSelection frameSelection = frame->selection()->selection();
+                // If a selection is in an editable element spell check its content.
+                if (Element* rootEditableElement = frameSelection.rootEditableElement()) {
+                    VisibleSelection selection = VisibleSelection::selectionFromContentsOfNode(rootEditableElement);
+                    frame->editor()->markMisspellingsAndBadGrammar(selection);
+                }
+            }
+        }
+    }
 }
 
 bool EditorClientImpl::isGrammarCheckingEnabled()
