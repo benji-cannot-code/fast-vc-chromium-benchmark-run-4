c579032c5b4c494cf6624c0237c44ab5321cbd49
skuhne@chromium.org <skuhne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-17 14:03:14 +0000

    Autoclosing of tray bubbles when the mouse is over the bubble but not moved by the user
    The problem:
    
    The user watches a full screen movie and uses the keyboard to change the volume. At that time the cursor is at the location where the bubble shows up.
    
    The result is that the bubble will stay open until the user moves the mouse away.
    
    What should happen instead is that as long as the user does not actively move the mouse, it should automatically close again.
    
    The CL is looking at the Synthesize flag upon entry which indicates that this event was not a real user action. If that happens, the mouse will be monitored and if it is moved the event will be dispatched as a mouse entered event. Only when the entered event was sent, the exit event will be sent.
    
    BUG=271121
    TEST=visual
    
    Review URL: https://chromiumcodereview.appspot.com/23200007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218176 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/views/bubble/tray_bubble_view.h
+++ ui/views/bubble/tray_bubble_view.h
@@ -8,6 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/memory/scoped_ptr.h"
 #include "ui/views/bubble/bubble_delegate.h"
+#include "ui/views/mouse_watcher.h"
 #include "ui/views/views_export.h"
 
 // Specialized bubble view for bubbles associated with a tray icon (e.g. the
@@ -31,7 +32,8 @@ class TrayBubbleBorder;
 class TrayBubbleContentMask;
 }
 
-class VIEWS_EXPORT TrayBubbleView : public views::BubbleDelegateView {
+class VIEWS_EXPORT TrayBubbleView : public views::BubbleDelegateView,
+                                    public views::MouseWatcherListener {
  public:
   // AnchorType differentiates between bubbles that are anchored on a tray
   // element (ANCHOR_TYPE_TRAY) and display an arrow, or that are floating on
@@ -63,6 +65,8 @@ class VIEWS_EXPORT TrayBubbleView : public views::BubbleDelegateView {
     virtual void BubbleViewDestroyed() = 0;
 
     // Called when the mouse enters/exits the view.
+    // Note: This event will only be called if the mouse gets actively moved by
+    // the user to enter the view.
     virtual void OnMouseEnteredView() = 0;
     virtual void OnMouseExitedView() = 0;
 
@@ -160,6 +164,9 @@ class VIEWS_EXPORT TrayBubbleView : public views::BubbleDelegateView {
   virtual void OnMouseExited(const ui::MouseEvent& event) OVERRIDE;
   virtual void GetAccessibleState(ui::AccessibleViewState* state) OVERRIDE;
 
+  // Overridden from MouseWatcherListener
+  virtual void MouseMovedOutOfHost() OVERRIDE;
+
  protected:
   TrayBubbleView(gfx::NativeView parent_window,
                  views::View* anchor,
@@ -181,6 +188,13 @@ class VIEWS_EXPORT TrayBubbleView : public views::BubbleDelegateView {
   internal::TrayBubbleBorder* bubble_border_;
   scoped_ptr<internal::TrayBubbleContentMask> bubble_content_mask_;
   bool is_gesture_dragging_;
+
+  // True once the mouse cursor was actively moved by the user over the bubble.
+  // Only then the OnMouseExitedView() event will get passed on to listeners.
+  bool mouse_actively_entered_;
+
+  // Used to find any mouse movements.
+  scoped_ptr<MouseWatcher> mouse_watcher_;
 
   DISALLOW_COPY_AND_ASSIGN(TrayBubbleView);
 };
