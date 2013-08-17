6451e56c6f52f9fb49986ce0af76ee274800c44a
rch@chromium.org <rch@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-17 02:57:45 +0000

    Mark QUIC as broken when a QUIC session is closed without the crypto handshake finishing succesfully.
    Review URL: https://chromiumcodereview.appspot.com/23265003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218150 0039d316-1c4b-4281-b951-d872f2087c98

--- net/quic/quic_stream_factory.h
+++ net/quic/quic_stream_factory.h
@@ -26,6 +26,7 @@ namespace net {
 class CertVerifier;
 class ClientSocketFactory;
 class HostResolver;
+class HttpServerProperties;
 class QuicClock;
 class QuicClientSession;
 class QuicCryptoClientStreamFactory;
@@ -77,6 +78,7 @@ class NET_EXPORT_PRIVATE QuicStreamFactory
   QuicStreamFactory(
       HostResolver* host_resolver,
       ClientSocketFactory* client_socket_factory,
+      base::WeakPtr<HttpServerProperties> http_server_properties,
       QuicCryptoClientStreamFactory* quic_crypto_client_stream_factory,
       QuicRandom* random_generator,
       QuicClock* clock);
@@ -151,6 +153,7 @@ class NET_EXPORT_PRIVATE QuicStreamFactory
 
   HostResolver* host_resolver_;
   ClientSocketFactory* client_socket_factory_;
+  base::WeakPtr<HttpServerProperties> http_server_properties_;
   QuicCryptoClientStreamFactory* quic_crypto_client_stream_factory_;
   QuicRandom* random_generator_;
   scoped_ptr<QuicClock> clock_;
