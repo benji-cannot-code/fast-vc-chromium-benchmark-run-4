b46e2883dd2a3877b64566db3e47547fd79825fd
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-17 02:28:28 +0000

    Rename the Custom Elements runtime feature to "CustomElements".
    The name of the feature should match the name of the spec and the name
    used in the implementation. It used to be called CustomDOMElements.
    
    Refactoring; no change in behavior.
    
    BUG=234509
    
    Review URL: https://chromiumcodereview.appspot.com/23189010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156258 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
+++ third_party/WebKit/Source/web/WebRuntimeFeatures.cpp
@@ -385,7 +385,7 @@ bool WebRuntimeFeatures::isHTMLImportsEnabled()
 
 void WebRuntimeFeatures::enableCustomElements(bool enable)
 {
-    RuntimeEnabledFeatures::setCustomDOMElementsEnabled(enable);
+    RuntimeEnabledFeatures::setCustomElementsEnabled(enable);
 }
 
 void WebRuntimeFeatures::enableOverlayScrollbars(bool enable)
