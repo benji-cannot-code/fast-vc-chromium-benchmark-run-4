96f629ea328643492ae78a82b5ff56bfbcf51541
rogerta@chromium.org <rogerta@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-17 02:26:54 +0000

    When the user presses ESC in the sign in confirmation dialog, call the closure function so that sign in is cleaned up correctly.
    BUG=272701
    
    Review URL: https://chromiumcodereview.appspot.com/22977005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218147 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/gtk/one_click_signin_bubble_gtk.cc
+++ chrome/browser/ui/gtk/one_click_signin_bubble_gtk.cc
@@ -53,9 +53,11 @@ OneClickSigninBubbleGtk::OneClickSigninBubbleGtk(
 
 void OneClickSigninBubbleGtk::BubbleClosing(
     BubbleGtk* bubble, bool closed_by_escape) {
+  // If we get here and |start_sync_callback_| is not null, act like this is
+  // an undo.  All actions that start sign in are explicitly handled below.
   if (is_sync_dialog_ && !start_sync_callback_.is_null()) {
     base::ResetAndReturn(&start_sync_callback_).Run(
-        OneClickSigninSyncStarter::SYNC_WITH_DEFAULT_SETTINGS);
+        OneClickSigninSyncStarter::UNDO_SYNC);
   }
 
   // The bubble needs to close and remove the widgets from the window before
@@ -76,7 +78,7 @@ void OneClickSigninBubbleGtk::OnClickAdvancedLink(GtkWidget* link) {
           one_click_signin::HISTOGRAM_CONFIRM_ADVANCED);
 
     base::ResetAndReturn(&start_sync_callback_).Run(
-      OneClickSigninSyncStarter::CONFIGURE_SYNC_FIRST);
+        OneClickSigninSyncStarter::CONFIGURE_SYNC_FIRST);
   } else {
     Browser* browser = chrome::FindBrowserWithWindow(
       gtk_window_get_transient_for(bubble_->GetNativeWindow()));
@@ -97,7 +99,7 @@ void OneClickSigninBubbleGtk::OnClickOK(GtkWidget* link) {
           one_click_signin::HISTOGRAM_CONFIRM_OK);
 
     base::ResetAndReturn(&start_sync_callback_).Run(
-      OneClickSigninSyncStarter::SYNC_WITH_DEFAULT_SETTINGS);
+        OneClickSigninSyncStarter::SYNC_WITH_DEFAULT_SETTINGS);
   }
   bubble_->Close();
 }
@@ -136,11 +138,13 @@ void OneClickSigninBubbleGtk::OnClickLearnMoreLink(GtkWidget* link) {
 }
 
 void OneClickSigninBubbleGtk::OnClickCloseButton(GtkWidget* button) {
+  DCHECK(is_sync_dialog_);
   OneClickSigninHelper::LogConfirmHistogramValue(
       clicked_learn_more_ ?
           one_click_signin::HISTOGRAM_CONFIRM_LEARN_MORE_CLOSE :
           one_click_signin::HISTOGRAM_CONFIRM_CLOSE);
-  start_sync_callback_.Reset();
+  base::ResetAndReturn(&start_sync_callback_).Run(
+      OneClickSigninSyncStarter::UNDO_SYNC);
   bubble_->Close();
 }
 
