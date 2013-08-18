6b263f13271ab81772f0c4a51562ee6c81b577c9
pneubeck@chromium.org <pneubeck@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-18 12:44:29 +0000

    Migrate ONC import (from net-internals) to new NetworkHandlers.
    With this commit NetworkLibrary::LoadOncNetworks will be unused.
    
    BUG=273102
    TBR=eroman@chromium.org (for net-internals)
    
    Review URL: https://chromiumcodereview.appspot.com/23183002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218199 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/network_connect.cc
+++ chrome/browser/chromeos/options/network_connect.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/stringprintf.h"
 #include "base/strings/utf_string_conversions.h"
 #include "chrome/browser/chromeos/enrollment_dialog_view.h"
+#include "chrome/browser/chromeos/login/user.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/chromeos/options/network_config_view.h"
 #include "chrome/browser/profiles/profile_manager.h"
