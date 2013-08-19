f03696bf7b58b16953d1097aecf23047ed98fe16
tapted@chromium.org <tapted@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-19 10:44:53 +0000

    Various cleanups in web_app_mac.
     - audit #includes
     - Remove unused forward decs
     - Ensure ObjC headers are #imported
     - Remove unnecessary data member `chrome_bundle_id_` & constructor argument
     - Make GetAppLoaderPath() an anonymous/static function
     - Remove use of #if defined(GOOGLE_CHROME_BUILD)
     - Only the mocked-out methods need to be protected: (others to private)
     - rename member `app_data_path_` to `app_data_dir_`, and be consistent
    . . elsewhere to call a containing folder a `dir`
     - Decompose repeated pattern for getting the two destinations paths:
    . . -- Applications & Internal -- and appending the shortcut name
     - Declared function order: Getters, Creators, Updaters
     - DISALLOW_COPY_AND_ASSIGN
    
    BUG=None
    TEST=No functional changes
    
    Review URL: https://chromiumcodereview.appspot.com/23026002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218261 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/web_applications/web_app_mac.h
+++ chrome/browser/web_applications/web_app_mac.h
@@ -6,21 +6,14 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_MAC_H_
 #define CHROME_BROWSER_WEB_APPLICATIONS_WEB_APP_MAC_H_
 
+#include <string>
 #include <vector>
 
+#include "base/basictypes.h"
 #include "base/files/file_path.h"
 #include "base/gtest_prod_util.h"
-#include "base/strings/string16.h"
 #include "chrome/browser/shell_integration.h"
 #include "chrome/browser/web_applications/web_app.h"
-
-#ifdef __OBJC__
-@class NSDictionary;
-@class NSString;
-#else  // __OBJC__
-class NSDictionary;
-class NSString;
-#endif  // __OBJC__
 
 namespace web_app {
 
@@ -37,21 +30,26 @@ void MaybeLaunchShortcut(const ShellIntegration::ShortcutInfo& shortcut_info);
 class WebAppShortcutCreator {
  public:
   // Creates a new shortcut based on information in |shortcut_info|.
-  // A copy of the shortcut is placed in |app_data_path|.
+  // A copy of the shortcut is placed in |app_data_dir|.
   // |chrome_bundle_id| is the CFBundleIdentifier of the Chrome browser bundle.
   WebAppShortcutCreator(
-      const base::FilePath& app_data_path,
-      const ShellIntegration::ShortcutInfo& shortcut_info,
-      const std::string& chrome_bundle_id);
+      const base::FilePath& app_data_dir,
+      const ShellIntegration::ShortcutInfo& shortcut_info);
 
   virtual ~WebAppShortcutCreator();
 
   // Returns the base name for the shortcut.
-  base::FilePath GetShortcutName() const;
+  base::FilePath GetShortcutBasename() const;
 
   // Returns a path to the Chrome Apps folder in the relevant applications
   // folder. E.g. ~/Applications or /Applications.
-  virtual base::FilePath GetDestinationPath() const;
+  virtual base::FilePath GetApplicationsDirname() const;
+
+  // The full path to the app bundle under the relevant Applications folder.
+  base::FilePath GetApplicationsShortcutPath() const;
+
+  // The full path to the app bundle under the profile folder.
+  base::FilePath GetInternalShortcutPath() const;
 
   bool CreateShortcuts(ShortcutCreationReason creation_reason,
                        ShellIntegration::ShortcutLocations creation_locations);
@@ -59,24 +57,24 @@ class WebAppShortcutCreator {
   bool UpdateShortcuts();
 
  protected:
-  // Returns a path to the app loader.
-  base::FilePath GetAppLoaderPath() const;
-
-  // Updates the plist inside |app_path| with information about the app.
-  bool UpdatePlist(const base::FilePath& app_path) const;
-
-  // Updates the icon for the shortcut.
-  bool UpdateIcon(const base::FilePath& app_path) const;
-
   // Returns a path to an app bundle with the given id. Or an empty path if no
   // matching bundle was found.
   // Protected and virtual so it can be mocked out for testing.
   virtual base::FilePath GetAppBundleById(const std::string& bundle_id) const;
 
+  // Show the bundle we just generated in the Finder.
+  virtual void RevealAppShimInFinder() const;
+
  private:
   FRIEND_TEST_ALL_PREFIXES(WebAppShortcutCreatorTest, DeleteShortcuts);
   FRIEND_TEST_ALL_PREFIXES(WebAppShortcutCreatorTest, UpdateIcon);
   FRIEND_TEST_ALL_PREFIXES(WebAppShortcutCreatorTest, UpdateShortcuts);
+
+  // Returns the bundle identifier to use for this app bundle.
+  std::string GetBundleIdentifier() const;
+
+  // Returns the bundle identifier for the internal copy of the bundle.
+  std::string GetInternalBundleIdentifier() const;
 
   // Copies the app loader template into a temporary directory and fills in all
   // relevant information.
@@ -93,24 +91,20 @@ class WebAppShortcutCreator {
   // Updates the bundle id of the internal copy of the app shim bundle.
   bool UpdateInternalBundleIdentifier() const;
 
-  // Returns the bundle identifier to use for this app bundle.
-  std::string GetBundleIdentifier() const;
+  // Updates the plist inside |app_path| with information about the app.
+  bool UpdatePlist(const base::FilePath& app_path) const;
 
-  // Returns the bundle identifier for the internal copy of the bundle.
-  std::string GetInternalBundleIdentifier() const;
-
-  // Show the bundle we just generated in the Finder.
-  virtual void RevealAppShimInFinder() const;
+  // Updates the icon for the shortcut.
+  bool UpdateIcon(const base::FilePath& app_path) const;
 
   // Path to the data directory for this app. For example:
   // ~/Library/Application Support/Chromium/Default/Web Applications/_crx_abc/
-  base::FilePath app_data_path_;
+  base::FilePath app_data_dir_;
 
   // Information about the app.
   ShellIntegration::ShortcutInfo info_;
 
-  // The CFBundleIdentifier of the Chrome browser bundle.
-  std::string chrome_bundle_id_;
+  DISALLOW_COPY_AND_ASSIGN(WebAppShortcutCreator);
 };
 
 }  // namespace web_app
