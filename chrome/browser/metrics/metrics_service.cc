3c238045f94162f435348f787b56c277d11e754c
rogerm@chromium.org <rogerm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-19 15:04:58 +0000

    Add a smoke signal to count crash dump events on Windows.
    Hack Alert!
    
    This CL attempts to count the number of times the browser process generates a crash dump. It does so by sending up a smoke signal (creating a file in a smoke-signal subdirectory of the crash-dump directory) just after generating a minidump for the crash server. On browser startup, these files are scavenged and a crash dump counter is incremented for each.
    
    R= mad@chromium.org, cpu@chromium.org
    TBR= isherman@chromium.org
    BUG=
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22493007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218282 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/metrics/metrics_service.cc
+++ chrome/browser/metrics/metrics_service.cc
@@ -193,6 +193,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/ui/browser_otr_state.h"
 #include "chrome/browser/ui/search/search_tab_helper.h"
 #include "chrome/common/child_process_logging.h"
+#include "chrome/common/chrome_constants.h"
 #include "chrome/common/chrome_result_codes.h"
 #include "chrome/common/chrome_switches.h"
 #include "chrome/common/metrics/caching_permuted_entropy_provider.h"
@@ -226,6 +227,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #include <windows.h>  // Needed for STATUS_* codes
+#include "base/win/registry.h"
 #endif
 
 using base::Time;
@@ -794,6 +796,34 @@ void MetricsService::RecordBreakpadHasDebugger(bool has_debugger) {
     IncrementPrefValue(prefs::kStabilityDebuggerPresent);
 }
 
+#if defined(OS_WIN)
+void MetricsService::CountBrowserCrashDumpAttempts() {
+  base::string16 key_str(chrome::kBrowserCrashDumpAttemptsRegistryPath);
+  key_str += L"\\";
+  key_str += UTF8ToWide(chrome::kChromeVersion);
+
+  base::win::RegKey regkey;
+  if (regkey.Open(HKEY_CURRENT_USER,
+                  key_str.c_str(),
+                  KEY_ALL_ACCESS) != ERROR_SUCCESS) {
+    return;
+  }
+
+  base::string16 temp_name;
+  DWORD temp_value = 0;
+  int crash_dump_attempts = 0;
+  for (int i = regkey.GetValueCount() - 1; i >= 0; --i) {
+    if (regkey.GetValueNameAt(i, &temp_name) == ERROR_SUCCESS &&
+        regkey.ReadValueDW(temp_name.c_str(), &temp_value) == ERROR_SUCCESS) {
+      regkey.DeleteValue(temp_name.c_str());
+      if (temp_value != 0)
+        ++crash_dump_attempts;
+    }
+  }
+  UMA_HISTOGRAM_COUNTS("Chrome.BrowserCrashDumpAttempts", crash_dump_attempts);
+}
+#endif  // defined(OS_WIN)
+
 //------------------------------------------------------------------------------
 // private methods
 //------------------------------------------------------------------------------
@@ -842,6 +872,10 @@ void MetricsService::InitializeMetricsState() {
     // monitoring.
     pref->SetBoolean(prefs::kStabilityExitedCleanly, true);
   }
+
+#if defined(OS_WIN)
+  CountBrowserCrashDumpAttempts();
+#endif  // defined(OS_WIN)
 
   if (!pref->GetBoolean(prefs::kStabilitySessionEndCompleted)) {
     IncrementPrefValue(prefs::kStabilityIncompleteSessionEndCount);
