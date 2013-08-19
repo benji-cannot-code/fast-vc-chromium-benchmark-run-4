1f183b94aeaccfe9b47d0998372e1a167e0293d1
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-19 15:50:30 +0000

    Convert 'document.domain' exceptions to 'es.throwSecurityError()'.
    BUG=275282
    
    Review URL: https://chromiumcodereview.appspot.com/23321002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156312 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/LayoutTests/fast/dom/Document/script-tests/invalid-domain-change-throws-exception.js
+++ third_party/WebKit/LayoutTests/fast/dom/Document/script-tests/invalid-domain-change-throws-exception.js
@@ -1,4 +1,4 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 description("This test checks that a SECURITY_ERR exception is raised if an attempt is made to change document.domain to an invalid value.");
 
-shouldThrow('document.domain = "apple.com"', '"SecurityError: \'document.domain\' assignment failed: \'apple.com\' is not a suffix of \'\'."');
+shouldThrow('document.domain = "apple.com"', '"SecurityError: Failed to set the \'domain\' property on \'Document\': \'apple.com\' is not a suffix of \'\'."');
--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -38,6 +38,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "XMLNames.h"
 #include "bindings/v8/CustomElementConstructorBuilder.h"
 #include "bindings/v8/Dictionary.h"
+#include "bindings/v8/ExceptionMessages.h"
 #include "bindings/v8/ExceptionState.h"
 #include "bindings/v8/ExceptionStatePlaceholder.h"
 #include "bindings/v8/ScriptController.h"
@@ -3704,7 +3705,7 @@ String Document::domain() const
 void Document::setDomain(const String& newDomain, ExceptionState& es)
 {
     if (SchemeRegistry::isDomainRelaxationForbiddenForURLScheme(securityOrigin()->protocol())) {
-        es.throwDOMException(SecurityError, "'document.domain' assignment is forbidden for the '" + securityOrigin()->protocol() + "' scheme.");
+        es.throwSecurityError(ExceptionMessages::failedToSet("domain", "Document", "assignment is forbidden for the '" + securityOrigin()->protocol() + "' scheme."));
         return;
     }
 
@@ -3726,17 +3727,17 @@ void Document::setDomain(const String& newDomain, ExceptionState& es)
 
     int oldLength = domain().length();
     int newLength = newDomain.length();
-    String exceptionMessage =  "'document.domain' assignment failed: '" + newDomain + "' is not a suffix of '" + domain() + "'.";
+    String exceptionMessage =  ExceptionMessages::failedToSet("domain", "Document", "'" + newDomain + "' is not a suffix of '" + domain() + "'.");
     // e.g. newDomain = subdomain.www.example.com (25) and domain() = www.example.com (15)
     if (newLength >= oldLength) {
-        es.throwDOMException(SecurityError, exceptionMessage);
+        es.throwSecurityError(exceptionMessage);
         return;
     }
 
     String test = domain();
     // Check that it's a complete suffix, not e.g. "ample.com"
     if (test[oldLength - newLength - 1] != '.') {
-        es.throwDOMException(SecurityError, exceptionMessage);
+        es.throwSecurityError(exceptionMessage);
         return;
     }
 
@@ -3744,7 +3745,7 @@ void Document::setDomain(const String& newDomain, ExceptionState& es)
     // and we check that it's the same thing as newDomain
     test.remove(0, oldLength - newLength);
     if (test != newDomain) {
-        es.throwDOMException(SecurityError, exceptionMessage);
+        es.throwSecurityError(exceptionMessage);
         return;
     }
 
