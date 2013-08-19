e47415f9c39201b77275fc05a65ba3c3d4fd3d85
tfarina@chromium.org <tfarina@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-19 06:25:57 +0000

    ui/base/accelerators: Use base::string16 now that string16 was moved into base namespace.
    base/string16.h was moved into base namespace in r191198 by Brett.
    
    BUG=None
    TBR=ben@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23039010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218233 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/accessibility/accessible_text_utils.cc
+++ ui/base/accessibility/accessible_text_utils.cc
@@ -10,7 +10,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-size_t FindAccessibleTextBoundary(const string16& text,
+size_t FindAccessibleTextBoundary(const base::string16& text,
                                   const std::vector<int>& line_breaks,
                                   TextBoundaryType boundary,
                                   size_t start_offset,
