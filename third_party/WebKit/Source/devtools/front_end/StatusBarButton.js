0745043ad198bc3a5042bc3478508bd0d4857b42
aandrey@chromium.org <aandrey@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-19 16:44:35 +0000

    DevTools: Cancel async longClick timer when the button gets disabled.
    Extracted from https://codereview.chromium.org/23279005/
    
    BUG=
    R=caseq@chromium.org, pfeldman@chromium.org, prybin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23014004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156317 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/StatusBarButton.js
+++ third_party/WebKit/Source/devtools/front_end/StatusBarButton.js
@@ -130,8 +130,22 @@ WebInspector.StatusBarButton.prototype = {
     _clicked: function()
     {
         this.dispatchEventToListeners("click");
-        if (this._longClickInterval)
+        if (this._longClickInterval) {
             clearInterval(this._longClickInterval);
+            delete this._longClickInterval;
+        }
+    },
+
+    /**
+     * @override
+     */
+    _applyEnabledState: function()
+    {
+        this.element.disabled = !this._enabled;
+        if (this._longClickInterval) {
+            clearInterval(this._longClickInterval);
+            delete this._longClickInterval;
+        }
     },
 
     /**
@@ -228,8 +242,10 @@ WebInspector.StatusBarButton.prototype = {
         {
             if (e.which !== 1)
                 return;
-            if (this._longClickInterval)
+            if (this._longClickInterval) {
                 clearInterval(this._longClickInterval);
+                delete this._longClickInterval;
+            }
         }
 
         function longClicked()
