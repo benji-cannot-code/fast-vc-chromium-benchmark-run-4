929c66fd5135e44b7b3a09457de8fa8940a27c5e
enne@chromium.org <enne@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 01:39:25 +0000

    cc: Refactor scroll delta fixup out of push properties
    In impl-side painting, during push properties, all scrollable layers
    used to find their active twin to figure out what scrolling had occurred
    after the sent scroll delta was calculated.  As the default layer type
    is PictureLayer and there's no way to SetPushProperties on Layer for
    something happening on LayerImpl, when r214533 landed to skip
    PictureLayer::PushProperties this scroll delta fixup didn't
    happen.
    
    Ultimately, this meant that the pending tree was calculating priorities
    using the wrong transforms, causing activation to checkerboards for
    these onscreen tiles that were being considered offscreen.
    
    This is what caused r214533 (and r215188) to be reverted in r216218.
    This code is tested by existing scrolling unit tests.
    
    The fix is to move that code out into a separate pass so that skipping
    PushProperties doesn't cause misprioritization.
    
    R=danakj@chromium.org
    BUG=259402
    
    Review URL: https://chromiumcodereview.appspot.com/23295003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218399 0039d316-1c4b-4281-b951-d872f2087c98

--- cc/layers/layer_impl_unittest.cc
+++ cc/layers/layer_impl_unittest.cc
@@ -503,7 +503,7 @@ TEST_F(LayerImplScrollTest, ApplySentScrollsNoDelegate) {
   EXPECT_VECTOR_EQ(scroll_offset, layer()->scroll_offset());
   EXPECT_VECTOR_EQ(sent_scroll_delta, layer()->sent_scroll_delta());
 
-  layer()->ApplySentScrollDeltas();
+  layer()->ApplySentScrollDeltasFromAbortedCommit();
 
   EXPECT_VECTOR_EQ(scroll_offset + scroll_delta, layer()->TotalScrollOffset());
   EXPECT_VECTOR_EQ(scroll_delta - sent_scroll_delta, layer()->ScrollDelta());
@@ -528,7 +528,7 @@ TEST_F(LayerImplScrollTest, ApplySentScrollsWithIgnoringDelegate) {
   EXPECT_VECTOR_EQ(scroll_offset, layer()->scroll_offset());
   EXPECT_VECTOR_EQ(sent_scroll_delta, layer()->sent_scroll_delta());
 
-  layer()->ApplySentScrollDeltas();
+  layer()->ApplySentScrollDeltasFromAbortedCommit();
 
   EXPECT_VECTOR_EQ(fixed_offset, layer()->TotalScrollOffset());
   EXPECT_VECTOR_EQ(scroll_offset + sent_scroll_delta, layer()->scroll_offset());
@@ -552,7 +552,7 @@ TEST_F(LayerImplScrollTest, ApplySentScrollsWithAcceptingDelegate) {
   EXPECT_VECTOR_EQ(scroll_offset, layer()->scroll_offset());
   EXPECT_VECTOR_EQ(sent_scroll_delta, layer()->sent_scroll_delta());
 
-  layer()->ApplySentScrollDeltas();
+  layer()->ApplySentScrollDeltasFromAbortedCommit();
 
   EXPECT_VECTOR_EQ(scroll_offset + scroll_delta, layer()->TotalScrollOffset());
   EXPECT_VECTOR_EQ(scroll_offset + sent_scroll_delta, layer()->scroll_offset());
