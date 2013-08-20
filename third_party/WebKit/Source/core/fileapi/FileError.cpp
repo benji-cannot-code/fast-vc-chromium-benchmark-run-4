a59ede11a01b87eadf232695423fa9f65ab98cfc
mkwst@chromium.org <mkwst@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-20 09:51:47 +0000

    Convert 'FileError::throwDOMException' to use 'ExceptionState::throwSecurityError'
    The message is still uninteresting, but the sooner we get all
    SecurityError callsites migrated to the new API, the sooner we can start
    ASSERTing that it's the only way to throw SecurityError.
    
    BUG=275282
    R=kinuko@chromium.org
    
    Review URL: https://codereview.chromium.org/22813004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156384 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/fileapi/FileError.cpp
+++ third_party/WebKit/Source/core/fileapi/FileError.cpp
@@ -63,10 +63,6 @@ void FileError::throwDOMException(ExceptionState& es, ErrorCode code)
         ec = NotFoundError;
         message = FileError::notFoundErrorMessage;
         break;
-    case FileError::SECURITY_ERR:
-        ec = SecurityError;
-        message = FileError::securityErrorMessage;
-        break;
     case FileError::ABORT_ERR:
         ec = AbortError;
         message = FileError::abortErrorMessage;
@@ -105,6 +101,10 @@ void FileError::throwDOMException(ExceptionState& es, ErrorCode code)
         ec = PathExistsError;
         message = FileError::pathExistsErrorMessage;
         break;
+    // SecurityError is special-cased, as we want to route those exceptions through ExceptionState::throwSecurityError.
+    case FileError::SECURITY_ERR:
+        es.throwSecurityError(FileError::securityErrorMessage);
+        return;
     default:
         ASSERT_NOT_REACHED();
         return;
