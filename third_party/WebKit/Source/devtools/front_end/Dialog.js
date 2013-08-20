611f203d1f26a995cee1e5e6393a1ac96c48bee4
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-20 13:48:15 +0000

    Revert "Soft slide&fade-in transition for Dialog and Popover."
    This reverts r155427
    
    BUG=274458
    TBR=pfeldman
    
    Review URL: https://codereview.chromium.org/22926007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156397 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Dialog.js
+++ third_party/WebKit/Source/devtools/front_end/Dialog.js
@@ -59,7 +59,6 @@ WebInspector.Dialog = function(relativeToElement, delegate)
     this._windowResizeHandler = this._position.bind(this);
     window.addEventListener("resize", this._windowResizeHandler, true);
     this._delegate.focus();
-    this._element.classList.add("fx-appear");
 }
 
 /**
--- third_party/WebKit/Source/devtools/front_end/Popover.js
+++ third_party/WebKit/Source/devtools/front_end/Popover.js
@@ -107,7 +107,6 @@ WebInspector.Popover.prototype = {
             this._contentDiv.appendChild(this.contentElement);
 
         this._positionElement(anchor, preferredWidth, preferredHeight, arrowDirection);
-        this.element.classList.add("fx-appear");
 
         if (this._popoverHelper) {
             this._contentDiv.addEventListener("mousemove", this._popoverHelper._killHidePopoverTimer.bind(this._popoverHelper), true);
@@ -118,7 +117,6 @@ WebInspector.Popover.prototype = {
     hide: function()
     {
         this.detach();
-        this.element.classList.remove("fx-appear");
         delete WebInspector.Popover._popover;
     },
 
