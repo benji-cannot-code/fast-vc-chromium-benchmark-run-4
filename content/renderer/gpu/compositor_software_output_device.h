322645601588314d7e2f5f9b4944f025c8e0761d
jbauman@chromium.org <jbauman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 20:05:21 +0000

    Discard software renderer backbuffer when tab becomes invisible.
    We should delete the backbuffers to save memory.
    
    BUG=158487
    
    Review URL: https://chromiumcodereview.appspot.com/22801004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218543 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/gpu/compositor_software_output_device.h
+++ content/renderer/gpu/compositor_software_output_device.h
@@ -31,6 +31,8 @@ public:
 
   virtual SkCanvas* BeginPaint(gfx::Rect damage_rect) OVERRIDE;
   virtual void EndPaint(cc::SoftwareFrameData* frame_data) OVERRIDE;
+  virtual void EnsureBackbuffer() OVERRIDE;
+  virtual void DiscardBackbuffer() OVERRIDE;
 
   virtual void ReclaimSoftwareFrame(unsigned id) OVERRIDE;
 
