cc4fb3279e543f285a29978a3f23da806235dd3f
gyuyoung.kim@samsung.com <gyuyoung.kim@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-20 00:41:05 +0000

    Introduce toSVGEllipseElement(), and use it
    As a step to clean-up static_cast<SVGXXX>, static_cast<SVGEllipseElement*> also can
    be changed with toSVGEllipseElement().
    
    BUG=N/A
    
    Review URL: https://chromiumcodereview.appspot.com/22877029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156350 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/svg/SVGEllipseElement.h
+++ third_party/WebKit/Source/core/svg/SVGEllipseElement.h
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef SVGEllipseElement_h
 #define SVGEllipseElement_h
 
+#include "SVGNames.h"
 #include "core/svg/SVGAnimatedBoolean.h"
 #include "core/svg/SVGAnimatedLength.h"
 #include "core/svg/SVGExternalResourcesRequired.h"
@@ -56,6 +57,12 @@ private:
         DECLARE_ANIMATED_BOOLEAN(ExternalResourcesRequired, externalResourcesRequired)
     END_DECLARE_ANIMATED_PROPERTIES
 };
+
+inline SVGEllipseElement* toSVGEllipseElement(Node* node)
+{
+    ASSERT_WITH_SECURITY_IMPLICATION(!node || node->hasTagName(SVGNames::ellipseTag));
+    return static_cast<SVGEllipseElement*>(node);
+}
 
 } // namespace WebCore
 
