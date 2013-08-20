cb34ef096c72bd286ebf9237696944368e9b21c1
frankf@chromium.org <frankf@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 14:24:57 +0000

    [Android] Move getting the list of devices out of test_dispatcher.
    BUG=272486
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/23205011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218483 0039d316-1c4b-4281-b951-d872f2087c98

--- build/android/pylib/base/test_dispatcher_unittest.py
+++ build/android/pylib/base/test_dispatcher_unittest.py
@@ -168,7 +168,7 @@ class TestShard(unittest.TestCase):
   @staticmethod
   def _RunShard(runner_factory):
     return test_dispatcher.RunTests(
-        ['a', 'b', 'c'], runner_factory, False, None, shard=True)
+        ['a', 'b', 'c'], runner_factory, ['0', '1'], shard=True)
 
   def testShard(self):
     results, exit_code = TestShard._RunShard(MockRunner)
@@ -183,7 +183,7 @@ class TestShard(unittest.TestCase):
 
   def testNoTests(self):
     results, exit_code = test_dispatcher.RunTests(
-        [], MockRunner, False, None, shard=True)
+        [], MockRunner, ['0', '1'], shard=True)
     self.assertEqual(len(results.GetAll()), 0)
     self.assertEqual(exit_code, constants.ERROR_EXIT_CODE)
 
@@ -193,7 +193,7 @@ class TestReplicate(unittest.TestCase):
   @staticmethod
   def _RunReplicate(runner_factory):
     return test_dispatcher.RunTests(
-        ['a', 'b', 'c'], runner_factory, False, None, shard=False)
+        ['a', 'b', 'c'], runner_factory, ['0', '1'], shard=False)
 
   def testReplicate(self):
     results, exit_code = TestReplicate._RunReplicate(MockRunner)
@@ -209,7 +209,7 @@ class TestReplicate(unittest.TestCase):
 
   def testNoTests(self):
     results, exit_code = test_dispatcher.RunTests(
-        [], MockRunner, False, None, shard=False)
+        [], MockRunner, ['0', '1'], shard=False)
     self.assertEqual(len(results.GetAll()), 0)
     self.assertEqual(exit_code, constants.ERROR_EXIT_CODE)
 
