fbb98f0c044e9ff518916faa6314fbf8e1083733
sail@chromium.org <sail@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 12:32:29 +0000

    Fix omnibox repaint problem
    Just need to invalidate the popup when changing contents.
    
    BUG=274430
    TEST=Type "g" in the omnibox. Replaced "g" with "e". Verified that
    with my fix the popup correctly updated.
    
    Review URL: https://chromiumcodereview.appspot.com/22957007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218470 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.mm
+++ chrome/browser/ui/cocoa/omnibox/omnibox_popup_cell.mm
@@ -76,10 +76,12 @@ void DrawFadeTruncatingTitle(NSAttributedString* title,
 }
 
 - (void)setContentText:(NSAttributedString*)contentText {
+  [[self controlView] setNeedsDisplay:YES];
   contentText_.reset([contentText retain]);
 }
 
 - (void)setDescriptionText:(NSAttributedString*)descriptionText {
+  [[self controlView] setNeedsDisplay:YES];
   if (![descriptionText length]) {
     descriptionText_.reset();
     return;
