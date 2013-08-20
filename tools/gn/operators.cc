095ea9741caa99920108b9febd0c22e409b73b6a
brettw@chromium.org <brettw@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 17:23:15 +0000

    Add support for boolean values to GN.
    I removed support for implicit conversions of any type.
    
    BUG=
    R=scottmg@chromium.org
    
    Review URL: https://codereview.chromium.org/23033013
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218511 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/gn/operators.cc
+++ tools/gn/operators.cc
@@ -57,6 +57,7 @@ void RemoveMatchesFromList(const BinaryOpNode* op_node,
                            Err* err) {
   std::vector<Value>& v = list->list_value();
   switch (to_remove.type()) {
+    case Value::BOOLEAN:
     case Value::INTEGER:  // Filter out the individual int/string.
     case Value::STRING: {
       bool found_match = false;
@@ -343,8 +344,8 @@ Value ExecuteEqualsEquals(Scope* scope,
                           const Value& right,
                           Err* err) {
   if (left == right)
-    return Value(op_node, 1);
-  return Value(op_node, 0);
+    return Value(op_node, true);
+  return Value(op_node, false);
 }
 
 Value ExecuteNotEquals(Scope* scope,
@@ -358,10 +359,10 @@ Value ExecuteNotEquals(Scope* scope,
   return result;
 }
 
-Value FillNeedsToIntegersError(const BinaryOpNode* op_node,
-                               const Value& left,
-                               const Value& right,
-                               Err* err) {
+Value FillNeedsTwoIntegersError(const BinaryOpNode* op_node,
+                                const Value& left,
+                                const Value& right,
+                                Err* err) {
   *err = Err(op_node, "Comparison requires two integers.",
              "This operator can only compare two integers.");
   err->AppendRange(left.origin()->GetRange());
@@ -375,7 +376,7 @@ Value ExecuteLessEquals(Scope* scope,
                         const Value& right,
                         Err* err) {
   if (left.type() != Value::INTEGER || right.type() != Value::INTEGER)
-    return FillNeedsToIntegersError(op_node, left, right, err);
+    return FillNeedsTwoIntegersError(op_node, left, right, err);
   return Value(op_node, left.int_value() <= right.int_value());
 }
 
@@ -385,7 +386,7 @@ Value ExecuteGreaterEquals(Scope* scope,
                            const Value& right,
                            Err* err) {
   if (left.type() != Value::INTEGER || right.type() != Value::INTEGER)
-    return FillNeedsToIntegersError(op_node, left, right, err);
+    return FillNeedsTwoIntegersError(op_node, left, right, err);
   return Value(op_node, left.int_value() >= right.int_value());
 }
 
@@ -395,7 +396,7 @@ Value ExecuteGreater(Scope* scope,
                      const Value& right,
                      Err* err) {
   if (left.type() != Value::INTEGER || right.type() != Value::INTEGER)
-    return FillNeedsToIntegersError(op_node, left, right, err);
+    return FillNeedsTwoIntegersError(op_node, left, right, err);
   return Value(op_node, left.int_value() > right.int_value());
 }
 
@@ -405,7 +406,7 @@ Value ExecuteLess(Scope* scope,
                   const Value& right,
                   Err* err) {
   if (left.type() != Value::INTEGER || right.type() != Value::INTEGER)
-    return FillNeedsToIntegersError(op_node, left, right, err);
+    return FillNeedsTwoIntegersError(op_node, left, right, err);
   return Value(op_node, left.int_value() < right.int_value());
 }
 
@@ -416,8 +417,14 @@ Value ExecuteOr(Scope* scope,
                 const Value& left,
                 const Value& right,
                 Err* err) {
-  return Value(op_node,
-      static_cast<int64>(left.InterpretAsInt() || right.InterpretAsInt()));
+  if (left.type() != Value::BOOLEAN) {
+    *err = Err(left, "Left side of || operator is not a boolean.");
+    err->AppendRange(op_node->GetRange());
+  } else if (right.type() != Value::BOOLEAN) {
+    *err = Err(right, "Right side of || operator is not a boolean.");
+    err->AppendRange(op_node->GetRange());
+  }
+  return Value(op_node, left.boolean_value() || right.boolean_value());
 }
 
 Value ExecuteAnd(Scope* scope,
@@ -425,8 +432,14 @@ Value ExecuteAnd(Scope* scope,
                  const Value& left,
                  const Value& right,
                  Err* err) {
-  return Value(op_node,
-      static_cast<int64>(left.InterpretAsInt() && right.InterpretAsInt()));
+  if (left.type() != Value::BOOLEAN) {
+    *err = Err(left, "Left side of && operator is not a boolean.");
+    err->AppendRange(op_node->GetRange());
+  } else if (right.type() != Value::BOOLEAN) {
+    *err = Err(right, "Right side of && operator is not a boolean.");
+    err->AppendRange(op_node->GetRange());
+  }
+  return Value(op_node, left.boolean_value() && right.boolean_value());
 }
 
 }  // namespace
@@ -474,8 +487,14 @@ Value ExecuteUnaryOperator(Scope* scope,
                            const Value& expr,
                            Err* err) {
   DCHECK(op_node->op().type() == Token::BANG);
+
+  if (expr.type() != Value::BOOLEAN) {
+    *err = Err(expr, "Operand of ! operator is not a boolean.");
+    err->AppendRange(op_node->GetRange());
+    return Value();
+  }
   // TODO(scottmg): Why no unary minus?
-  return Value(op_node, !expr.InterpretAsInt());
+  return Value(op_node, !expr.boolean_value());
 }
 
 Value ExecuteBinaryOperator(Scope* scope,
--- tools/gn/value.cc
+++ tools/gn/value.cc
@@ -19,15 +19,31 @@ Value::Value(const ParseNode* origin, Type t)
       origin_(origin) {
 }
 
+Value::Value(const ParseNode* origin, bool bool_val)
+    : type_(BOOLEAN),
+      boolean_value_(bool_val),
+      int_value_(0),
+      origin_(origin) {
+}
+
 Value::Value(const ParseNode* origin, int64 int_val)
     : type_(INTEGER),
+      boolean_value_(false),
       int_value_(int_val),
       origin_(origin) {
 }
 
-Value::Value(const ParseNode* origin, const base::StringPiece& str_val)
+Value::Value(const ParseNode* origin, std::string str_val)
     : type_(STRING),
-      string_value_(str_val.as_string()),
+      string_value_(),
+      int_value_(0),
+      origin_(origin) {
+  string_value_.swap(str_val);
+}
+
+Value::Value(const ParseNode* origin, const char* str_val)
+    : type_(STRING),
+      string_value_(str_val),
       int_value_(0),
       origin_(origin) {
 }
@@ -40,6 +56,8 @@ const char* Value::DescribeType(Type t) {
   switch (t) {
     case NONE:
       return "none";
+    case BOOLEAN:
+      return "boolean";
     case INTEGER:
       return "integer";
     case STRING:
@@ -52,24 +70,12 @@ const char* Value::DescribeType(Type t) {
   }
 }
 
-int64 Value::InterpretAsInt() const {
-  switch (type_) {
-    case NONE:
-      return 0;
-    case INTEGER:
-      return int_value_;
-    case STRING:
-      return string_value_.empty() ? 0 : 1;
-    case LIST:
-      return list_value_.empty() ? 0 : 1;
-  }
-  return 0;
-}
-
 std::string Value::ToString(bool quote_string) const {
   switch (type_) {
     case NONE:
       return "<void>";
+    case BOOLEAN:
+      return boolean_value_ ? "true" : "false";
     case INTEGER:
       return base::Int64ToString(int_value_);
     case STRING:
@@ -103,6 +109,8 @@ bool Value::operator==(const Value& other) const {
     return false;
 
   switch (type_) {
+    case Value::BOOLEAN:
+      return boolean_value() == other.boolean_value();
     case Value::INTEGER:
       return int_value() == other.int_value();
     case Value::STRING:
