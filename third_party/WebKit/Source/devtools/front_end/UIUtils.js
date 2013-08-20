7fe7711e0768f63b35341af748e8a6c45e37dfc1
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-20 15:15:28 +0000

    DevTools: Fix edit-as-html behaviour
    This patch fixes the wierd behaviour which happens with "Edit as HTML"
    editor.
    
    BUG=272410
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/23068010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156400 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/UIUtils.js
+++ third_party/WebKit/Source/devtools/front_end/UIUtils.js
@@ -520,6 +520,7 @@ WebInspector.handleElementValueModifications = function(event, element, finishHa
 /** 
  * @param {Element} element
  * @param {WebInspector.EditingConfig=} config
+ * @return {?{cancel: function(), commit: function(), codeMirror: CodeMirror, setWidth: function(number)}}
  */
 WebInspector.startEditing = function(element, config)
 {
@@ -571,6 +572,16 @@ WebInspector.startEditing = function(element, config)
         if (typeof oldTabIndex !== "number" || oldTabIndex < 0)
             element.tabIndex = 0;
         WebInspector.setCurrentFocusElement(element);
+    }
+
+    /**
+     * @param {number} width
+     */
+    function setWidth(width)
+    {
+        const padding = 30;
+        codeMirror.getWrapperElement().style.width = (width - codeMirror.getWrapperElement().offsetLeft - padding) + "px";
+        codeMirror.refresh();
     }
 
     /**
@@ -693,7 +704,8 @@ WebInspector.startEditing = function(element, config)
     return {
         cancel: editingCancelled.bind(element),
         commit: editingCommitted.bind(element),
-        codeMirror: codeMirror // For testing.
+        codeMirror: codeMirror, // For testing.
+        setWidth: setWidth
     };
 }
 
