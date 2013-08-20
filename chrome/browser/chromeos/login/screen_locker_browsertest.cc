45ef5f26e861d65f0a75557686bee760d0c27f6e
flackr@chromium.org <flackr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 09:47:03 +0000

    Add ScreenLocker test to verify menus are closed on screen lock.
    BUG=135001
    TEST=ShellTest.LockScreenClosesActiveMenu
    
    Review URL: https://chromiumcodereview.appspot.com/20868003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218433 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/screen_locker_browsertest.cc
+++ chrome/browser/chromeos/login/screen_locker_browsertest.cc
@@ -3,12 +3,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "chrome/browser/chromeos/login/screen_locker.h"
+
 #include "base/command_line.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/message_loop/message_loop.h"
 #include "chrome/browser/chrome_notification_types.h"
 #include "chrome/browser/chromeos/login/mock_authenticator.h"
-#include "chrome/browser/chromeos/login/screen_locker.h"
 #include "chrome/browser/chromeos/login/screen_locker_tester.h"
 #include "chrome/browser/chromeos/login/user_manager.h"
 #include "chrome/browser/profiles/profile_manager.h"
