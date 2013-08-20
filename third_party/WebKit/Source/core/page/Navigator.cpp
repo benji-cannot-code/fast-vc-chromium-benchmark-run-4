efddc00edf9dde6e9d9f8c415abdede0820e05c8
ch.dumez@sisa.samsung.com <ch.dumez@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-20 13:42:05 +0000

    Introduce NavigatorID IDL interface
    Introduce NavigatorID IDL interface to avoid duplication and match the
    specification:
    http://www.whatwg.org/specs/web-apps/current-work/multipage/timers.html#navigatorid
    http://www.whatwg.org/specs/web-apps/current-work/multipage/timers.html#navigator
    http://www.whatwg.org/specs/web-apps/current-work/multipage/workers.html#workernavigator
    
    R=arv@chromium.org, haraken@chromium.org
    
    Review URL: https://codereview.chromium.org/22927015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156396 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/Navigator.cpp
+++ third_party/WebKit/Source/core/page/Navigator.cpp
@@ -29,6 +29,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/loader/CookieJar.h"
 #include "core/loader/FrameLoader.h"
 #include "core/page/Frame.h"
+#include "core/page/NavigatorID.h"
 #include "core/page/Page.h"
 #include "core/page/Settings.h"
 #include "core/platform/Language.h"
@@ -68,7 +69,7 @@ String Navigator::appVersion() const
 {
     if (!m_frame)
         return String();
-    String appVersion = NavigatorBase::appVersion();
+    String appVersion = NavigatorID::appVersion(this);
     if (shouldHideFourDot(m_frame))
         appVersion.replace("4.", "4_");
     return appVersion;
