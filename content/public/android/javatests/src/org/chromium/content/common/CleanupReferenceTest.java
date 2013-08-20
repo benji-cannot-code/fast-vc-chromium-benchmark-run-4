ff0ba1b7c5602e8afef9d6b5dd4d194df0b74eb3
gkanwar@chromium.org <gkanwar@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 22:25:26 +0000

    [Android] Small java changes for EMMA code coverage
    One piece of the overall java coverage change.
    See: https://codereview.chromium.org/20210002/ 
    
    NOTRY=True
    BUG=255644
    
    Review URL: https://chromiumcodereview.appspot.com/23253005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218559 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/javatests/src/org/chromium/content/common/CleanupReferenceTest.java
+++ content/public/android/javatests/src/org/chromium/content/common/CleanupReferenceTest.java
@@ -59,6 +59,8 @@ public class CleanupReferenceTest extends InstrumentationTestCase {
         assertEquals(1, sObjectCount.get());
 
         instance = null;
+        // Ensure compiler / instrumentation does not strip out the assignment.
+        assertTrue(instance == null);
         collectGarbage();
         assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
             @Override
@@ -82,6 +84,8 @@ public class CleanupReferenceTest extends InstrumentationTestCase {
         }
 
         instances = null;
+        // Ensure compiler / instrumentation does not strip out the assignment.
+        assertTrue(instances == null);
         collectGarbage();
         assertTrue(CriteriaHelper.pollForCriteria(new Criteria() {
             @Override
