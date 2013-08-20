f956c07219f150941aa5ed02941d543aa20b0dc3
haaawk@google.com <haaawk@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 09:58:59 +0000

    Android:Method to check if native library is initialized.
    It's required by ContentProvider in Android to avoid loading native library
    as a result of ContentProvider call. It allows ContentProvider to determine
    if native library is initialized and proceed only if so. ContentProvider.query
    method will drop the request if the library is not initialized.
    
    BUG= 176000
    
    Review URL: https://chromiumcodereview.appspot.com/23125004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218438 0039d316-1c4b-4281-b951-d872f2087c98

--- content/public/android/java/src/org/chromium/content/app/LibraryLoader.java
+++ content/public/android/java/src/org/chromium/content/app/LibraryLoader.java
@@ -65,6 +65,14 @@ public class LibraryLoader {
         }
     }
 
+    /**
+     * Checks if library is fully loaded and initialized.
+     */
+    public static boolean isInitialized() {
+        synchronized (sLock) {
+            return sInitialized;
+        }
+    }
 
     /**
      * Loads the library and blocks until the load completes. The caller is responsible
