a6279321c541415a94a3c05657a7daea22310b30
mnissler@chromium.org <mnissler@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-20 14:15:40 +0000

    Correctly canonicalize registration user on enterprise enrollment.
    The previous code failed to canonicalize the user received from the
    device management server before writing it to installation-time
    attributes.
    
    BUG=chromium:274473
    TEST=unit tests
    
    Review URL: https://chromiumcodereview.appspot.com/23064009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218480 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/policy/enterprise_install_attributes_unittest.cc
+++ chrome/browser/chromeos/policy/enterprise_install_attributes_unittest.cc
@@ -13,6 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/policy/proto/chromeos/install_attributes.pb.h"
 #include "chromeos/cryptohome/cryptohome_library.h"
 #include "chromeos/dbus/cryptohome_client.h"
+#include "google_apis/gaia/gaia_auth_util.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace policy {
@@ -29,6 +30,7 @@ void CopyLockResult(base::RunLoop* loop,
 }  // namespace
 
 static const char kTestUser[] = "test@example.com";
+static const char kTestUserCanonicalize[] = "UPPER.CASE@example.com";
 static const char kTestDomain[] = "example.com";
 static const char kTestDeviceId[] = "133750519";
 
@@ -101,6 +103,16 @@ TEST_F(EnterpriseInstallAttributesTest, Lock) {
                 "test@bluebears.com",
                 DEVICE_MODE_ENTERPRISE,
                 kTestDeviceId));
+}
+
+TEST_F(EnterpriseInstallAttributesTest, LockCanonicalize) {
+  EXPECT_EQ(EnterpriseInstallAttributes::LOCK_SUCCESS,
+            LockDeviceAndWaitForResult(
+                kTestUserCanonicalize,
+                DEVICE_MODE_ENTERPRISE,
+                kTestDeviceId));
+  EXPECT_EQ(gaia::CanonicalizeEmail(kTestUserCanonicalize),
+            install_attributes_.GetRegistrationUser());
 }
 
 TEST_F(EnterpriseInstallAttributesTest, IsEnterpriseDevice) {
