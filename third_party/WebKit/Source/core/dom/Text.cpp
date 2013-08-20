d183ec5b377ecf831c664a39fc0e000a4852dcda
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-20 00:11:30 +0000

    Text::splitText should use AttachLazily
    Don't use sync attach when splitting text nodes.
    
    Review URL: https://chromiumcodereview.appspot.com/22935015
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156345 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Text.cpp
+++ third_party/WebKit/Source/core/dom/Text.cpp
@@ -69,7 +69,7 @@ PassRefPtr<Text> Text::splitText(unsigned offset, ExceptionState& es)
     didModifyData(oldStr);
 
     if (parentNode())
-        parentNode()->insertBefore(newText.get(), nextSibling(), es);
+        parentNode()->insertBefore(newText.get(), nextSibling(), es, AttachLazily);
     if (es.hadException())
         return 0;
 
