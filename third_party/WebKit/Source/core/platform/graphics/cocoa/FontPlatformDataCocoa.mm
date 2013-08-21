1362f891a9a246c8c9a66e4933d659c3d37283b1
lushnikov@chromium.org <lushnikov@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-21 21:44:21 +0000

    DevTools: Add CSS.getPlatformFontsForNode in protocol.json
    This patch adds method CSS.getPlatformFontsForNode to the protocol.
    The method returns platform fonts that were used to render child text
    nodes for the given node.
    
    BUG=135489
    
    Review URL: https://chromiumcodereview.appspot.com/22923010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156505 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/graphics/cocoa/FontPlatformDataCocoa.mm
+++ third_party/WebKit/Source/core/platform/graphics/cocoa/FontPlatformDataCocoa.mm
@@ -235,6 +235,11 @@ static CTFontDescriptorRef cascadeToLastResortAndDisableSwashesFontDescriptor()
     return descriptor;
 }
 
+String FontPlatformData::fontFamilyName() const
+{
+    return String(CTFontCopyDisplayName(ctFont()));
+}
+
 CTFontRef FontPlatformData::ctFont() const
 {
     if (m_CTFont)
