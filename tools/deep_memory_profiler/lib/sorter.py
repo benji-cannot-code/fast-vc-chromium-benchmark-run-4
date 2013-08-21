9482b6f972b4a615c9310b49bb67f173f0b7a3f7
dmikurube@chromium.org <dmikurube@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 14:31:48 +0000

    Make 'dmprof cat' to dump 'orders' of rules and units for all rules even if empty.
    It also removes "no-buckets" from 'dmprof cat'.
    
    BUG=259206
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/22854031
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218732 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/deep_memory_profiler/lib/sorter.py
+++ tools/deep_memory_profiler/lib/sorter.py
@@ -9,6 +9,8 @@ import logging
 import os
 import re
 
+from lib.ordered_dict import OrderedDict
+
 
 LOGGER = logging.getLogger('dmprof')
 
@@ -314,17 +316,6 @@ class MallocRule(AbstractRule):
             (not self._typeinfo or self._typeinfo.match(typeinfo)))
 
 
-class NoBucketMallocRule(MallocRule):
-  """Represents a Rule that small ignorable units match with."""
-  def __init__(self):
-    super(NoBucketMallocRule, self).__init__({'name': 'tc-no-bucket'})
-    self._no_bucket = True
-
-  @property
-  def no_bucket(self):
-    return self._no_bucket
-
-
 class AbstractSorter(object):
   """An abstract class for classifying Units with a set of Rules."""
   def __init__(self, dct):
@@ -358,7 +349,7 @@ class AbstractSorter(object):
   @staticmethod
   def load(filename):
     with open(filename) as sorter_f:
-      sorter_dict = json.load(sorter_f)
+      sorter_dict = json.load(sorter_f, object_pairs_hook=OrderedDict)
     if sorter_dict['world'] == 'vm':
       return VMSorter(sorter_dict)
     elif sorter_dict['world'] == 'malloc':
@@ -378,6 +369,10 @@ class AbstractSorter(object):
   @property
   def root(self):
     return self._root
+
+  def iter_rule(self):
+    for rule in self._rules:
+      yield rule
 
   def find(self, unit):
     raise NotImplementedError()
@@ -408,11 +403,10 @@ class MallocSorter(AbstractSorter):
   def __init__(self, dct):
     assert dct['world'] == 'malloc'
     super(MallocSorter, self).__init__(dct)
-    self._no_bucket_rule = NoBucketMallocRule()
 
   def find(self, unit):
     if not unit.bucket:
-      return self._no_bucket_rule
+      return None
     assert unit.bucket.allocator_type == 'malloc'
 
     # TODO(dmikurube): Utilize component_cache again, or remove it.
@@ -434,7 +428,7 @@ class SorterTemplates(object):
   @staticmethod
   def load(filename):
     with open(filename) as templates_f:
-      templates_dict = json.load(templates_f)
+      templates_dict = json.load(templates_f, object_pairs_hook=OrderedDict)
     return SorterTemplates(templates_dict)
 
 
