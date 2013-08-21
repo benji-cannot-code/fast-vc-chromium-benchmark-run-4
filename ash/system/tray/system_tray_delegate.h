216a0afdd0c38a8b08e14f254756569cb72c4c7a
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 21:03:04 +0000

    Fixes a crash bug of TrayLocallyManagedUser notification in initialization (2nd)
    The first CL was reverted due to compile error on a windows builder.
    According to the error message, it seems that the parent class has
    to have ASH_EXPORT if its child has ASH_EXPORT.
    
    BUG=275697
    TBR=stevenjb@chromium.org, jamescook@chromium.org
    TEST=new test cases cover this
    
    Review URL: https://chromiumcodereview.appspot.com/23050017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218832 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/tray/system_tray_delegate.h
+++ ash/system/tray/system_tray_delegate.h
@@ -112,7 +112,7 @@ typedef std::vector<IMEInfo> IMEInfoList;
 
 class VolumeControlDelegate;
 
-class SystemTrayDelegate {
+class ASH_EXPORT SystemTrayDelegate {
  public:
   virtual ~SystemTrayDelegate() {}
 
