a0884619f2bb120884d867242b9109049cfc6270
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-21 08:20:16 +0000

    Fix XMLHttpRequest leak document when send() is called multiple times.
    XMLHttpRequest creates a ThreadableLoader which may call XHR async, so it setPendingActivity() to avoid being destroyed. However, before this patch, unsetPendingActivity() was called asynchronously after ThreadableLoader was destroyed, so it lead to multiple problems:
    a) When next m_loader was set in send() with pending unsetPendingActivity(), the pendingActivity may be dropped even when there exists new m_loader need protection.
    b) pendingActivity may be set multiple times from pending unsetPendingActivity(), but dropProtectionSoon() only decrements m_pendingActivityCount by one, leading to a leak.
    
    This patch fix the above problems by unsetPendingActivity() synchronously with m_loader destruction where possible. XMLHttpRequest::stop() still uses asynchronous unsetPendingActivity() to workaround issues mentioned in r152266.
    
    The file "leak-check.js" was moved from fast/dom to fast/js to enable access from http tests.
    
    BUG=270000
    
    Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=156078
    
    Review URL: https://chromiumcodereview.appspot.com/23005006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156456 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/xml/XMLHttpRequest.h
+++ third_party/WebKit/Source/core/xml/XMLHttpRequest.h
@@ -76,6 +76,11 @@ public:
         ResponseTypeArrayBuffer
     };
 
+    enum DropProtection {
+        DropProtectionSync,
+        DropProtectionAsync,
+    };
+
     virtual void contextDestroyed();
     virtual void didTimeout();
     virtual bool canSuspend() const;
@@ -170,7 +175,7 @@ private:
     void callReadyStateChangeListener();
     void dropProtectionSoon();
     void dropProtection(Timer<XMLHttpRequest>* = 0);
-    void internalAbort();
+    void internalAbort(DropProtection = DropProtectionSync);
     void clearResponse();
     void clearResponseBuffers();
     void clearRequest();
