df2a8ba5414683d4442dd3a6416a0cb0e892f4aa
tony@chromium.org <tony@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-21 17:39:23 +0000

    Make scrollbar thumb thickness and margin configurable.
    When using overlay scrollbars on desktop, remove the margin and
    increase the width.  This makes them easier to see on desktop.
    No visible changes to android.
    
    BUG=274010
    TEST=None
    
    Review URL: https://chromiumcodereview.appspot.com/22815011
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156482 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/ScrollbarThemeAndroid.cpp
+++ third_party/WebKit/Source/core/platform/ScrollbarThemeAndroid.cpp
@@ -33,7 +33,7 @@ namespace WebCore {
 
 ScrollbarTheme* ScrollbarTheme::nativeTheme()
 {
-    DEFINE_STATIC_LOCAL(ScrollbarThemeOverlay, theme, ());
+    DEFINE_STATIC_LOCAL(ScrollbarThemeOverlay, theme, (3, 4));
     return &theme;
 }
 
