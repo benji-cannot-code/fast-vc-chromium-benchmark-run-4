c926b20ee7dce849cebe9710d4e025a00dc5d732
jochen@chromium.org <jochen@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 06:11:56 +0000

    Revert 218654 "Fixes a crash bug of TrayLocallyManagedUser notif..."
    Doesn't compile on Win Aura:
    
    c:\b\build\slave\win_aura_builder\build\src\ash\system\tray\test_system_tray_delegate.h(18) : warning C4275: non dll-interface class 'ash::SystemTrayDelegate' used as base for dll-interface class 'ash::test::TestSystemTrayDelegate'
    
    > Fixes a crash bug of TrayLocallyManagedUser notification in initialization.
    > 
    > BUG=275697
    > R=stevenjb@chromium.org
    > TEST=new test cases cover this
    > 
    > Review URL: https://chromiumcodereview.appspot.com/23332005
    
    TBR=mukai@chromium.org
    
    Review URL: https://codereview.chromium.org/22831018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218657 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/ash.gyp
+++ ash/ash.gyp
@@ -723,7 +723,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
         'shell/window_type_launcher.cc',
         'shell/window_watcher.cc',
         'shell/window_watcher_unittest.cc',
-        'system/chromeos/managed/tray_locally_managed_user_unittest.cc',
         'system/chromeos/network/network_state_notifier_unittest.cc',
         'system/chromeos/power/power_status_unittest.cc',
         'system/chromeos/power/tray_power_unittest.cc',
--- ash/test/ash_test_helper.cc
+++ ash/test/ash_test_helper.cc
@@ -7,7 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "ash/ash_switches.h"
 #include "ash/shell.h"
-#include "ash/system/tray/test_system_tray_delegate.h"
 #include "ash/test/display_manager_test_api.h"
 #include "ash/test/shell_test_api.h"
 #include "ash/test/test_session_state_delegate.h"
@@ -98,9 +97,6 @@ void AshTestHelper::TearDown() {
 #endif
 
   aura::Env::DeleteInstance();
-
-  // Need to reset the initial login status.
-  TestSystemTrayDelegate::SetInitialLoginStatus(user::LOGGED_IN_USER);
 
   ui::ShutdownInputMethodForTesting();
   zero_duration_mode_.reset();
