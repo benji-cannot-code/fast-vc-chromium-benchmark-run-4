881352e52a7e314fd59e09e80e96b85f1f5302cc
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 20:00:56 +0000

    Also call Oobe.showSigninUI in loginForTesting
    BUG=273270
    TEST=manual
    R=nkostylev@chromium.org
    
    Review URL: https://codereview.chromium.org/23361005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218814 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/chromeos/login/login_common.js
+++ chrome/browser/resources/chromeos/login/login_common.js
@@ -272,6 +272,7 @@ cr.define('cr.ui', function() {
    */
   Oobe.loginForTesting = function(username, password) {
     chrome.send('skipToLoginForTesting');
+    Oobe.showSigninUI(username);
     chrome.send('completeLogin', [username, password]);
   };
 
