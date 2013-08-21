34f1db84c77b7156e88548621b43f0e8ff85d70b
etienneb@chromium.org <etienneb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 23:28:59 +0000

    Remove unused variable declaration.
    This issue was found by a linter.
    
    R=gauravsh@chromium.org
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23241007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218861 0039d316-1c4b-4281-b951-d872f2087c98

--- chromeos/network/network_profile.cc
+++ chromeos/network/network_profile.cc
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace chromeos {
 
 std::string NetworkProfile::ToDebugString() const {
-  std::string result;
   if (type() == NetworkProfile::TYPE_SHARED) {
     return base::StringPrintf("NetworkProfile(SHARED, %s)",
                               path.c_str());
