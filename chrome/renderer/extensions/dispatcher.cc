3377d376919cd0383fcb4b336068e186d121684f
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 17:28:10 +0000

    Don't expose document.register to Chrome Apps by default.
    This CL doesn't expose document.register by default to Chrome Apps.
    
    Instead, DocumentNatives.RegisterElement is exposed to web_view.js in
    order to register webview as a custom element.
    
    This CL depends on Dominic's CL: http://crrev.com/22854017
    
    BUG=273126
    Test=manually, <webview> still works.
    
    Review URL: https://chromiumcodereview.appspot.com/23292002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218769 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/extensions/dispatcher.cc
+++ chrome/renderer/extensions/dispatcher.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/renderer/extensions/chrome_v8_extension.h"
 #include "chrome/renderer/extensions/content_watcher.h"
 #include "chrome/renderer/extensions/context_menus_custom_bindings.h"
+#include "chrome/renderer/extensions/document_custom_bindings.h"
 #include "chrome/renderer/extensions/dom_activity_logger.h"
 #include "chrome/renderer/extensions/event_bindings.h"
 #include "chrome/renderer/extensions/extension_custom_bindings.h"
@@ -855,6 +856,9 @@ void Dispatcher::RegisterNativeHandlers(ModuleSystem* module_system,
   module_system->RegisterNativeHandler("context_menus",
       scoped_ptr<NativeHandler>(
           new ContextMenusCustomBindings(this, context)));
+  module_system->RegisterNativeHandler("document_natives",
+      scoped_ptr<NativeHandler>(
+          new DocumentCustomBindings(this, context)));
   module_system->RegisterNativeHandler("extension",
       scoped_ptr<NativeHandler>(
           new ExtensionCustomBindings(this, context)));
@@ -1292,11 +1296,11 @@ void Dispatcher::AddOrRemoveOriginPermissions(
 }
 
 void Dispatcher::EnableCustomElementWhiteList() {
-  WebKit::WebRuntimeFeatures::enableCustomElements(true);
-  WebKit::WebCustomElement::allowTagName("webview");
+  WebKit::WebRuntimeFeatures::enableEmbedderCustomElements(true);
+  WebKit::WebCustomElement::addEmbedderCustomElementName("webview");
   // TODO(fsamuel): Add <adview> to the whitelist once it has been converted
   // into a custom element.
-  WebKit::WebCustomElement::allowTagName("browser-plugin");
+  WebKit::WebCustomElement::addEmbedderCustomElementName("browser-plugin");
 }
 
 void Dispatcher::AddOrRemoveBindings(const std::string& extension_id) {
--- chrome/renderer/resources/extensions/web_view.js
+++ chrome/renderer/resources/extensions/web_view.js
@@ -11,6 +11,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 'use strict';
 
 var eventBindings = require('event_bindings');
+var DocumentNatives = requireNative('document_natives');
 var messagingNatives = requireNative('messaging_natives');
 var WebRequestEvent = require('webRequestInternal').WebRequestEvent;
 var webRequestSchema =
@@ -680,10 +681,6 @@ WebViewInternal.prototype.setupWebRequestEvents_ = function() {
   );
 };
 
-// Save document.register in a variable in case the developer attempts to
-// override it at some point.
-var register = document.register;
-
 // Registers browser plugin <object> custom element.
 function registerBrowserPluginElement() {
   var proto = Object.create(HTMLObjectElement.prototype);
@@ -704,7 +701,7 @@ function registerBrowserPluginElement() {
   };
 
   WebViewInternal.BrowserPlugin =
-      register.call(document, 'browser-plugin', {prototype: proto});
+      DocumentNatives.RegisterElement('browser-plugin', {prototype: proto});
 
   delete proto.createdCallback;
   delete proto.enteredDocumentCallback;
@@ -772,7 +769,8 @@ function registerWebViewElement() {
   };
   WebViewInternal.maybeRegisterExperimentalAPIs(proto, secret);
 
-  window.WebView = register.call(document, 'webview', {prototype: proto});
+  window.WebView =
+      DocumentNatives.RegisterElement('webview', {prototype: proto});
 
   // Delete the callbacks so developers cannot call them and produce unexpected
   // behavior.
