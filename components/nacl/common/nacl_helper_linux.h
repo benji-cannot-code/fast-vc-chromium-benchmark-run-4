4930543627709a5c17bebd28e22046be7de3c312
jln@chromium.org <jln@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 02:59:34 +0000

    Support a new remote IPC for for GetTerminationStatus.
    (Reland of https://chromiumcodereview.appspot.com/23020010/)
    
    This does three things:
    
    1. Clean-up a lot of the code in the Linux NaCl loader.
    2. Create a new IPC to the NaCl loader "Zygote" for the
    main Zygote to query the termination status of processes.
    3. Clean-up some code in the Zygote and fix "process tracking".
    
    zygote_linux.cc:
    * Split GetTerminationStatus() out from HandleGetTerminationStatus().
    * Handle the case where we need to perform a remote IPC for GetTerminationStatus()
    * Use the new GetTerminationStatus() to support the remote case for HandleReapRequest().
    * Replace real_pids_to_sandbox_pids mapping with process_info_map_.
    * Update shortcut case in ForkWithRealPid() to fill this out.
    * Update GetTerminationStatus() to remove existing entries.
    
    zygote_fork_delegate_linux.h:
    * Create a new GetTerminationStatus() interface.
    
    nacl_helper_linux.cc:
    * Split HandleZygoteRequest() out from main().
    * Split ChildNaClLoaderInit() to handle the child side of a fork().
    * Handle a new IPC in HandleGetTerminationStatusRequest().
    
    nacl_fork_delegate_linux.cc:
    * Implement the new GetTerminationStatus() interface.
    * Use Pickle for IPCs, make IPCs easier to write with SendIPCRequestAndReadReply().
    
    BUG=133453
    R=mseaborn@chromium.org, piman@chromium.org
    TBR=mseaborn@chromium.org
    
    Review URL: https://codereview.chromium.org/22875026
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218610 0039d316-1c4b-4281-b951-d872f2087c98

--- components/nacl/common/nacl_helper_linux.h
+++ components/nacl/common/nacl_helper_linux.h
@@ -10,10 +10,15 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // constants used to implement communication between the nacl_helper
 // process and the Chrome zygote.
 
+#define kNaClMaxIPCMessageLength 2048
+
 // Used by Helper to tell Zygote it has started successfully.
 #define kNaClHelperStartupAck "NACLHELPER_OK"
-// Used by Zygote to ask Helper to fork a new NaCl loader.
-#define kNaClForkRequest "NACLFORK"
+
+enum NaClZygoteIPCCommand {
+  kNaClForkRequest,
+  kNaClGetTerminationStatusRequest,
+};
 
 // The next set of constants define global Linux file descriptors.
 // For communications between NaCl loader and browser.
