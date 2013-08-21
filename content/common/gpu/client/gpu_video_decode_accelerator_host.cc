244afc743846cab3e7cc2611df87f84e46e1c440
fischman@chromium.org <fischman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 03:06:15 +0000

    Avoid use-after-free in GpuVideo{De,En}codeAcceleratorHost on error.
    BUG=265493
    
    Review URL: https://chromiumcodereview.appspot.com/23361002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218612 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/gpu/client/gpu_video_decode_accelerator_host.cc
+++ content/common/gpu/client/gpu_video_decode_accelerator_host.cc
@@ -38,11 +38,13 @@ GpuVideoDecodeAcceleratorHost::GpuVideoDecodeAcceleratorHost(
 
 void GpuVideoDecodeAcceleratorHost::OnChannelError() {
   DLOG(ERROR) << "GpuVideoDecodeAcceleratorHost::OnChannelError()";
-  OnErrorNotification(PLATFORM_FAILURE);
   if (channel_) {
     channel_->RemoveRoute(decoder_route_id_);
     channel_ = NULL;
   }
+  // See OnErrorNotification for why this needs to be the last thing in this
+  // function.
+  OnErrorNotification(PLATFORM_FAILURE);
 }
 
 bool GpuVideoDecodeAcceleratorHost::OnMessageReceived(const IPC::Message& msg) {
@@ -66,6 +68,8 @@ bool GpuVideoDecodeAcceleratorHost::OnMessageReceived(const IPC::Message& msg) {
     IPC_MESSAGE_UNHANDLED(handled = false)
   IPC_END_MESSAGE_MAP()
   DCHECK(handled);
+  // See OnErrorNotification for why |this| mustn't be used after
+  // OnErrorNotification might have been called above.
   return handled;
 }
 
@@ -166,6 +170,8 @@ void GpuVideoDecodeAcceleratorHost::Send(IPC::Message* message) {
     DLOG(ERROR) << "Send(" << message_type << ") failed";
     error = true;
   }
+  // See OnErrorNotification for why this needs to be the last thing in this
+  // function.
   if (error)
     OnErrorNotification(PLATFORM_FAILURE);
 }
@@ -220,9 +226,13 @@ void GpuVideoDecodeAcceleratorHost::OnErrorNotification(uint32 error) {
   DCHECK(CalledOnValidThread());
   if (!client_)
     return;
-  client_->NotifyError(
+
+  // Client::NotifyError() may Destroy() |this|, so calling it needs to be the
+  // last thing done on this stack!
+  media::VideoDecodeAccelerator::Client* client = NULL;
+  std::swap(client, client_);
+  client->NotifyError(
       static_cast<media::VideoDecodeAccelerator::Error>(error));
-  client_ = NULL;
 }
 
 }  // namespace content
