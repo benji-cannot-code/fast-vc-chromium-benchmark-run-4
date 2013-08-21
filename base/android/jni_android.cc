2a8c63ec16209717d6ddaac2952507b5fe74d368
clamy@chromium.org <clamy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 17:59:42 +0000

    SimpleCache: Android - skip registration when the JVM is not initialized
    This allows for standalone executable using the Simple Cache to be built on
    Android, without bundling them in an apk.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23135012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218778 0039d316-1c4b-4281-b951-d872f2087c98

--- base/android/jni_android.cc
+++ base/android/jni_android.cc
@@ -125,6 +125,10 @@ void InitVM(JavaVM* vm) {
   g_jvm = vm;
 }
 
+bool IsVMInitialized() {
+  return g_jvm != NULL;
+}
+
 void InitApplicationContext(const JavaRef<jobject>& context) {
   DCHECK(g_application_context.Get().is_null());
   g_application_context.Get().Reset(context);
