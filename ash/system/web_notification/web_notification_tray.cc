b6f192c6b22d0cd79cf5d7af7d114b8328125e38
mukai@chromium.org <mukai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 05:31:45 +0000

    Fixes a crash bug of TrayLocallyManagedUser notification in initialization.
    BUG=275697
    R=stevenjb@chromium.org
    TEST=new test cases cover this
    
    Review URL: https://chromiumcodereview.appspot.com/23332005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218654 0039d316-1c4b-4281-b951-d872f2087c98

--- ash/system/web_notification/web_notification_tray.cc
+++ ash/system/web_notification/web_notification_tray.cc
@@ -70,7 +70,7 @@ class WorkAreaObserver : public ShelfLayoutManagerObserver,
                          public ShellObserver {
  public:
   WorkAreaObserver(message_center::MessagePopupCollection* collection,
-                   ShelfLayoutManager* shelf);
+                   aura::RootWindow* root_window);
   virtual ~WorkAreaObserver();
 
   void SetSystemTrayHeight(int height);
@@ -82,7 +82,11 @@ class WorkAreaObserver : public ShelfLayoutManagerObserver,
   virtual void OnAutoHideStateChanged(ShelfAutoHideState new_state) OVERRIDE;
 
  private:
+  // Updates |shelf_| from |root_window_|.
+  void UpdateShelf();
+
   message_center::MessagePopupCollection* collection_;
+  aura::RootWindow* root_window_;
   ShelfLayoutManager* shelf_;
   int system_tray_height_;
 
@@ -91,18 +95,20 @@ class WorkAreaObserver : public ShelfLayoutManagerObserver,
 
 WorkAreaObserver::WorkAreaObserver(
     message_center::MessagePopupCollection* collection,
-    ShelfLayoutManager* shelf)
+    aura::RootWindow* root_window)
     : collection_(collection),
-      shelf_(shelf),
+      root_window_(root_window),
+      shelf_(NULL),
       system_tray_height_(0) {
   DCHECK(collection_);
-  shelf_->AddObserver(this);
+  UpdateShelf();
   Shell::GetInstance()->AddShellObserver(this);
 }
 
 WorkAreaObserver::~WorkAreaObserver() {
   Shell::GetInstance()->RemoveShellObserver(this);
-  shelf_->RemoveObserver(this);
+  if (shelf_)
+    shelf_->RemoveObserver(this);
 }
 
 void WorkAreaObserver::SetSystemTrayHeight(int height) {
@@ -123,6 +129,8 @@ void WorkAreaObserver::SetSystemTrayHeight(int height) {
 }
 
 void WorkAreaObserver::OnDisplayWorkAreaInsetsChanged() {
+  UpdateShelf();
+
   collection_->OnDisplayBoundsChanged(
       Shell::GetScreen()->GetDisplayNearestWindow(
           shelf_->shelf_widget()->GetNativeView()));
@@ -152,6 +160,15 @@ void WorkAreaObserver::OnAutoHideStateChanged(ShelfAutoHideState new_state) {
       work_area.set_y(work_area.y() + system_tray_height_);
   }
   collection_->SetDisplayInfo(work_area, display.bounds());
+}
+
+void WorkAreaObserver::UpdateShelf() {
+  if (shelf_)
+    return;
+
+  shelf_ = ShelfLayoutManager::ForLauncher(root_window_);
+  if (shelf_)
+    shelf_->AddObserver(this);
 }
 
 // Class to initialize and manage the WebNotificationBubble and
@@ -367,7 +384,7 @@ bool WebNotificationTray::ShowPopups() {
       message_center_tray_.get(),
       ash::switches::UseAlternateShelfLayout()));
   work_area_observer_.reset(new internal::WorkAreaObserver(
-      popup_collection_.get(), GetShelfLayoutManager()));
+      popup_collection_.get(), GetWidget()->GetNativeView()->GetRootWindow()));
   return true;
 }
 
