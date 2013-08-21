b6d961c9e15d9df3dd0169d319020d2bce0dff8e
jaredshumway94@gmail.com <jaredshumway94@gmail.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-21 22:12:59 +0000

    Docserver: Generate declare_permissions.html
    {apps,extensions}/declare_permissions.html is generated by the new
    PermissionsDataSource.  Tables use the new permissions.json file in
    conjunction with _permissions_features.json.  _api_features is used to
    determine api dependencies on permissions and generates simple
    descriptions.  More complex descriptions were pulled from the html
    template and moved to templates/private/permissions/.
    
    BUG=233967
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22911012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218843 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/common/extensions/docs/server2/server_instance.py
+++ chrome/common/extensions/docs/server2/server_instance.py
@@ -16,6 +16,7 @@ from host_file_system_creator import HostFileSystemCreator
 from intro_data_source import IntroDataSource
 from object_store_creator import ObjectStoreCreator
 from path_canonicalizer import PathCanonicalizer
+from permissions_data_source import PermissionsDataSource
 from redirector import Redirector
 from reference_resolver import ReferenceResolver
 from samples_data_source import SamplesDataSource
@@ -103,6 +104,13 @@ class ServerInstance(object):
         '/'.join((svn_constants.JSON_PATH, 'manifest.json')),
         '/'.join((svn_constants.API_PATH, '_manifest_features.json')))
 
+    self.permissions_data_source = PermissionsDataSource(
+        self.compiled_host_fs_factory,
+        self.host_file_system,
+        '/'.join((svn_constants.API_PATH, '_api_features.json')),
+        '/'.join((svn_constants.API_PATH, '_permission_features.json')),
+        '/'.join((svn_constants.JSON_PATH, 'permissions.json')))
+
     self.example_zipper = ExampleZipper(
         self.compiled_host_fs_factory,
         self.host_file_system,
@@ -117,7 +125,6 @@ class ServerInstance(object):
 
     self.strings_json_path = '/'.join((svn_constants.JSON_PATH, 'strings.json'))
 
-
     self.template_data_source_factory = TemplateDataSource.Factory(
         self.api_data_source_factory,
         self.api_list_data_source_factory,
@@ -127,6 +134,7 @@ class ServerInstance(object):
         self.compiled_host_fs_factory,
         self.ref_resolver_factory,
         self.manifest_data_source,
+        self.permissions_data_source,
         svn_constants.PUBLIC_TEMPLATE_PATH,
         svn_constants.PRIVATE_TEMPLATE_PATH,
         base_path,
@@ -135,6 +143,8 @@ class ServerInstance(object):
         DataSourceRegistry.AsTemplateData(self))
 
     self.api_data_source_factory.SetTemplateDataSource(
+        self.template_data_source_factory)
+    self.permissions_data_source.SetTemplateDataSource(
         self.template_data_source_factory)
 
   @staticmethod
