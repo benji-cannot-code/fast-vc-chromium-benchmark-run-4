b8c98c0dc237cf0a00ef2d5311df6d1a84f22721
michaelbai@chromium.org <michaelbai@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 21:41:51 +0000

    Start renderer in ContentViewCoreImpl::EvaluateJavaScript.
    In WebView, the app could evaluate the Javascript before loading any page. This patch start renderer if there is no existing one.
    
    Since renderer was created without SiteInstance, the RenderViewCreated callback will not be called, this patch also call it even without SiteInstance.
    
    
    
    
    BUG=273164
    
    Review URL: https://chromiumcodereview.appspot.com/23018005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219111 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/java/src/org/chromium/android_webview/AwContents.java
+++ android_webview/java/src/org/chromium/android_webview/AwContents.java
@@ -1368,7 +1368,7 @@ public class AwContents {
     }
 
     /**
-     * @see ContentViewCore.evaluateJavaScript(String, ContentViewCOre.JavaScriptCallback)
+     * @see ContentViewCore.evaluateJavaScript(String, ContentViewCore.JavaScriptCallback)
      */
     public void evaluateJavaScript(String script, final ValueCallback<String> callback) {
         ContentViewCore.JavaScriptCallback jsCallback = null;
@@ -1382,6 +1382,13 @@ public class AwContents {
         }
 
         mContentViewCore.evaluateJavaScript(script, jsCallback);
+    }
+
+    /**
+     * @see ContentViewCore.evaluateJavaScriptEvenIfNotYetNavigated(String)
+     */
+    public void evaluateJavaScriptEvenIfNotYetNavigated(String script) {
+        mContentViewCore.evaluateJavaScriptEvenIfNotYetNavigated(script);
     }
 
     //--------------------------------------------------------------------------------------------
--- content/browser/android/content_view_core_impl.h
+++ content/browser/android/content_view_core_impl.h
@@ -156,7 +156,8 @@ class ContentViewCoreImpl : public ContentViewCore,
   void EvaluateJavaScript(JNIEnv* env,
                           jobject obj,
                           jstring script,
-                          jobject callback);
+                          jobject callback,
+                          jboolean start_renderer);
   int GetNativeImeAdapter(JNIEnv* env, jobject obj);
   void SetFocus(JNIEnv* env, jobject obj, jboolean focused);
   void ScrollFocusedEditableNodeIntoView(JNIEnv* env, jobject obj);
