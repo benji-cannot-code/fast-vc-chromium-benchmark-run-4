ccef4beda4e0af917abb66f164a881862d1ed1d9
fsamuel@chromium.org <fsamuel@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 00:07:56 +0000

    Fix style: Method call can fit on one line.
    BUG=none
    TBR=jam@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22004004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218869 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chrome_content_browser_client.cc
+++ chrome/browser/chrome_content_browser_client.cc
@@ -853,9 +853,7 @@ void ChromeContentBrowserClient::GuestWebContentsAttached(
     NOTREACHED();
     return;
   }
-  guest->Attach(embedder_web_contents,
-                extension->id(),
-                extra_params);
+  guest->Attach(embedder_web_contents, extension->id(), extra_params);
 }
 
 void ChromeContentBrowserClient::RenderProcessHostCreated(
