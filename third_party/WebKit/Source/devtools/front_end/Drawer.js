f28f514e303ab717232aed818b76545660b2f19f
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 13:32:32 +0000

    DevTools: Introduce an experimental setting to open console with Ctrl+Tilde, not Esc.
    Drive-by: Fix console in drawer behavior so that it could be opened back while it is closing.
    
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/22586002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156559 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/Drawer.js
+++ third_party/WebKit/Source/devtools/front_end/Drawer.js
@@ -79,6 +79,11 @@ WebInspector.Drawer.prototype = {
         return Number.constrain(height, Preferences.minConsoleHeight, window.innerHeight - this._mainElement.totalOffsetTop() - Preferences.minConsoleHeight);
     },
 
+    isHiding: function()
+    {
+        return this._isHiding;
+    },
+
     show: function(view, animationType)
     {
         WebInspector.searchController.cancelSearch();
@@ -140,6 +145,7 @@ WebInspector.Drawer.prototype = {
         if (!this.visible)
             return;
 
+        this._isHiding = true;
         this._savedHeight = this.element.offsetHeight;
 
         WebInspector.restoreFocusFromElement(this.element);
@@ -171,6 +177,7 @@ WebInspector.Drawer.prototype = {
             document.body.removeStyleClass("drawer-visible");
             delete this._currentAnimation;
             this._elementToAdjust.style.bottom = 0;
+            delete this._isHiding;
         }
 
         this._currentAnimation = WebInspector.animateStyle(animations, this._animationDuration(animationType), animationCallback.bind(this));
