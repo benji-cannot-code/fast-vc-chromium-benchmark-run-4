6bd835c77e03e1b84e5b769ac1752ce612cec50c
dpranke@chromium.org <dpranke@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 23:47:22 +0000

    Remove all the code for --run-singly from run-webkit-tests
    The --run-singly flag only ever really made sense when we couldn't
    count on the driver implementation (DRT/content_shell) to enforce
    the timeout for a test. Once we implemented proper support in driver.run_test()
    for enforcing timeouts directly (and made that a requirement of the interface,
    --run-singly was just unnecessary overhead, and I don't think anyone really used
    it.
    
    This patch removes the code (and tests) and makes --run-singly a synonym for
    --batch-size=1 --verbose (which basically has the same effect of restarting
    the driver after every test).
    
    R=ojan@chromium.org
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22859045
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156598 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/test.py
+++ third_party/WebKit/Tools/Scripts/webkitpy/layout_tests/port/test.py
@@ -48,7 +48,6 @@ class TestInstance(object):
         self.crash = False
         self.web_process_crash = False
         self.exception = False
-        self.hang = False
         self.keyboard = False
         self.error = ''
         self.timeout = False
@@ -102,8 +101,8 @@ class TestList(object):
 #
 # These numbers may need to be updated whenever we add or delete tests.
 #
-TOTAL_TESTS = 107
-TOTAL_SKIPS = 27
+TOTAL_TESTS = 105
+TOTAL_SKIPS = 25
 
 UNEXPECTED_PASSES = 1
 UNEXPECTED_FAILURES = 23
@@ -113,7 +112,6 @@ def unit_test_list():
     tests.add('failures/expected/crash.html', crash=True)
     tests.add('failures/expected/exception.html', exception=True)
     tests.add('failures/expected/timeout.html', timeout=True)
-    tests.add('failures/expected/hang.html', hang=True)
     tests.add('failures/expected/missing_text.html', expected_text=None)
     tests.add('failures/expected/needsrebaseline.html', actual_text='needsrebaseline text')
     tests.add('failures/expected/needsmanualrebaseline.html', actual_text='needsmanualrebaseline text')
@@ -291,7 +289,6 @@ Bug(test) failures/expected/newlines_with_excess_CR.html [ Failure ]
 Bug(test) failures/expected/reftest.html [ ImageOnlyFailure ]
 Bug(test) failures/expected/text.html [ Failure ]
 Bug(test) failures/expected/timeout.html [ Timeout ]
-Bug(test) failures/expected/hang.html [ WontFix ]
 Bug(test) failures/expected/keyboard.html [ WontFix ]
 Bug(test) failures/expected/exception.html [ WontFix ]
 Bug(test) failures/unexpected/pass.html [ Failure ]
@@ -572,8 +569,6 @@ class TestDriver(Driver):
             raise KeyboardInterrupt
         if test.exception:
             raise ValueError('exception from ' + test_name)
-        if test.hang:
-            time.sleep((float(driver_input.timeout) * 4) / 1000.0 + 1.0)  # The 1.0 comes from thread_padding_sec in layout_test_runnery.
 
         audio = None
         actual_text = test.actual_text
