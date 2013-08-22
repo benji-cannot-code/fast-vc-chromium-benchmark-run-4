d1f13cf8b951e03745bd06c2ae992d0d440fb79d
caitkp@chromium.org <caitkp@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 20:16:08 +0000

    Kill NOTIFICATION_LOCK_BACKGROUND_DISPLAYED and NOTIFICATION_LOGIN_WEBUI_LOADED
    Incorporate functionality into existing delegate interface.
    
    BUG=268984
    TEST=no change
    TBR=darin@chromium.org
    
    (tbr'ed for changes to chrome_notifications_types.h)
    
    Review URL: https://chromiumcodereview.appspot.com/23264028
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219091 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
+++ chrome/browser/ui/webui/chromeos/login/signin_screen_handler.cc
@@ -1448,12 +1448,8 @@ void SigninScreenHandler::HandleAccountPickerReady() {
   is_account_picker_showing_first_time_ = true;
   MaybePreloadAuthExtension();
 
-  if (ScreenLocker::default_screen_locker()) {
-    content::NotificationService::current()->Notify(
-        chrome::NOTIFICATION_LOCK_WEBUI_READY,
-        content::NotificationService::AllSources(),
-        content::NotificationService::NoDetails());
-  }
+  if (ScreenLocker::default_screen_locker())
+    ScreenLocker::default_screen_locker()->delegate()->OnLockWebUIReady();
 
   if (delegate_)
     delegate_->OnSigninScreenReady();
@@ -1461,10 +1457,8 @@ void SigninScreenHandler::HandleAccountPickerReady() {
 
 void SigninScreenHandler::HandleWallpaperReady() {
   if (ScreenLocker::default_screen_locker()) {
-    content::NotificationService::current()->Notify(
-        chrome::NOTIFICATION_LOCK_BACKGROUND_DISPLAYED,
-        content::NotificationService::AllSources(),
-        content::NotificationService::NoDetails());
+    ScreenLocker::default_screen_locker()->delegate()->
+        OnLockBackgroundDisplayed();
   }
 }
 
