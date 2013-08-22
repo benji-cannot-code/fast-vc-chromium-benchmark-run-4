883be9628c04a2dd31229fbd4c864dae6972c207
dtu@chromium.org <dtu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 00:54:37 +0000

    [telemetry] Move telemetry/core/chrome/ to telemetry/core/backends/chrome/
    This is cleanup from the addition of Webdriver support.
    The new folder core/backends/ will have backend interfaces that must be implemented by both Chrome and WebDriver, e.g. browser_backend; as well as backends that are common to both, e.g. form_based_credentials_backend.
    
    I did something like sed -i 's/telemetry.core.chrome/telemetry.core.backends.chrome/g' *.py
    Luckily the import order should always be the same.
    
    
    BUG=None.
    TEST=./run_tests
    
    Review URL: https://chromiumcodereview.appspot.com/23072018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218886 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/core/backends/chrome/cros_browser_finder_unittest.py
@@ -0,0 +1,12 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+# TODO(nduca): Add basic unit test for cros_browser_finder.
+#
+# Here, we should mock the cros_interface module (assuming its working) and
+# verify that the finder does the right thing. Because the finder delegates most
+# of its work to the CRI, the test code here is going to be comparatively
+# simple.
+
