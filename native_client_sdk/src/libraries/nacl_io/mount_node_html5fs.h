b93686a6a5599ae6ae363ed208cb57bb4490ef9f
mgiuca@chromium.org <mgiuca@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 10:13:12 +0000

    [NaCl SDK] nacl_io MountNodeHtml5Fs GetType, IsaDir and IsaFile now work.
    Removed IsDirectory (use IsaDir instead).
    
    Also fixed GetSize so that it works for directories.
    
    BUG=271925
    
    Review URL: https://chromiumcodereview.appspot.com/23316002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218983 0039d316-1c4b-4281-b951-d872f2087c98

--- native_client_sdk/src/libraries/nacl_io/mount_node_html5fs.h
+++ native_client_sdk/src/libraries/nacl_io/mount_node_html5fs.h
@@ -31,7 +31,10 @@ class MountNodeHtml5Fs : public MountNode {
                       size_t count,
                       int* out_bytes);
 
+  virtual int GetType();
   virtual Error GetSize(size_t *out_size);
+  virtual bool IsaDir();
+  virtual bool IsaFile();
 
  protected:
   MountNodeHtml5Fs(Mount* mount, PP_Resource fileref);
@@ -43,11 +46,6 @@ class MountNodeHtml5Fs : public MountNode {
  private:
   PP_Resource fileref_resource_;
   PP_Resource fileio_resource_;  // 0 if the file is a directory.
-
-  // Returns true if this node is a directory.
-  bool IsDirectory() const {
-    return !fileio_resource_;
-  }
 
   friend class MountHtml5Fs;
 };
