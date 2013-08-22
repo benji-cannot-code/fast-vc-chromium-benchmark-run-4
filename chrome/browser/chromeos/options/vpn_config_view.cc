ffc264f0415ca653dca5b9b7130afb6669949f35
stevenjb@chromium.org <stevenjb@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 00:13:13 +0000

    Set kL2tpIpsecCaCertPemProperty as List
    BUG=266032
    
    Review URL: https://chromiumcodereview.appspot.com/23346004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218872 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/options/vpn_config_view.cc
+++ chrome/browser/chromeos/options/vpn_config_view.cc
@@ -825,8 +825,10 @@ void VPNConfigView::SetConfigProperties(
     case PROVIDER_TYPE_INDEX_L2TP_IPSEC_USER_CERT: {
       std::string ca_cert_pem = GetServerCACertPEM();
       if (!ca_cert_pem.empty()) {
-        properties->SetStringWithoutPathExpansion(
-            shill::kL2tpIpsecCaCertPemProperty, ca_cert_pem);
+        base::ListValue* pem_list = new base::ListValue;
+        pem_list->AppendString(ca_cert_pem);
+        properties->SetWithoutPathExpansion(
+            shill::kL2tpIpsecCaCertPemProperty, pem_list);
       }
       properties->SetStringWithoutPathExpansion(
           flimflam::kL2tpIpsecClientCertIdProperty, GetUserCertID());
@@ -848,7 +850,7 @@ void VPNConfigView::SetConfigProperties(
       std::string ca_cert_pem = GetServerCACertPEM();
       if (!ca_cert_pem.empty()) {
         base::ListValue* pem_list = new base::ListValue;
-        pem_list->AppendString(GetServerCACertPEM());
+        pem_list->AppendString(ca_cert_pem);
         properties->SetWithoutPathExpansion(
             shill::kOpenVPNCaCertPemProperty, pem_list);
       }
