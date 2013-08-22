8686ada6fc8f66a113e6ec5e7ef9bf0f7b56ee42
jbroman@chromium.org <jbroman@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 01:00:56 +0000

    Remove Pasteboard::clear, since it is a no-op.
    As the comment indicated, this is taken care of on the Chromium side.
    
    BUG=276565
    
    Review URL: https://chromiumcodereview.appspot.com/23173008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156523 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/platform/Pasteboard.cpp
+++ third_party/WebKit/Source/core/platform/Pasteboard.cpp
@@ -66,12 +66,6 @@ Pasteboard::Pasteboard()
 {
 }
 
-void Pasteboard::clear()
-{
-    // The ScopedClipboardWriter class takes care of clearing the clipboard's
-    // previous contents.
-}
-
 bool Pasteboard::isSelectionMode() const
 {
     return m_selectionMode;
