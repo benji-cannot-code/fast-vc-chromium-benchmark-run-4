acbeb41aefcd90dc7d4167fb7162da0dacb17fe2
esprehn@chromium.org <esprehn@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 19:54:46 +0000

    Switch users of DeprecatedAttachNow that were on detached subtrees to the default AttachLazily
    I had skipped over all of these in my previous patches because I knew it didn't
    matter what they passed, but now that Ojan has exposed them as users of
    DeprecatedAttachNow we should switch them to just pass the default arguments.
    
    Review URL: https://chromiumcodereview.appspot.com/22859048
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156572 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Range.cpp
+++ third_party/WebKit/Source/core/dom/Range.cpp
@@ -756,7 +756,7 @@ PassRefPtr<DocumentFragment> Range::processContents(ActionType action, Exception
     // (or just delete the stuff in between)
 
     if ((action == EXTRACT_CONTENTS || action == CLONE_CONTENTS) && leftContents)
-        fragment->appendChild(leftContents, es, DeprecatedAttachNow);
+        fragment->appendChild(leftContents, es);
 
     if (processStart) {
         NodeVector nodes;
@@ -766,7 +766,7 @@ PassRefPtr<DocumentFragment> Range::processContents(ActionType action, Exception
     }
 
     if ((action == EXTRACT_CONTENTS || action == CLONE_CONTENTS) && rightContents)
-        fragment->appendChild(rightContents, es, DeprecatedAttachNow);
+        fragment->appendChild(rightContents, es);
 
     return fragment.release();
 }
@@ -797,7 +797,7 @@ PassRefPtr<Node> Range::processContentsBetweenOffsets(ActionType action, PassRef
             deleteCharacterData(c, startOffset, endOffset, es);
             if (fragment) {
                 result = fragment;
-                result->appendChild(c.release(), es, DeprecatedAttachNow);
+                result->appendChild(c.release(), es);
             } else
                 result = c.release();
         }
@@ -811,7 +811,7 @@ PassRefPtr<Node> Range::processContentsBetweenOffsets(ActionType action, PassRef
             c->setData(c->data().substring(startOffset, endOffset - startOffset));
             if (fragment) {
                 result = fragment;
-                result->appendChild(c.release(), es, DeprecatedAttachNow);
+                result->appendChild(c.release(), es);
             } else
                 result = c.release();
         }
@@ -883,7 +883,7 @@ PassRefPtr<Node> Range::processAncestorsAndTheirSiblings(ActionType action, Node
         RefPtr<Node> ancestor = *it;
         if (action == EXTRACT_CONTENTS || action == CLONE_CONTENTS) {
             if (RefPtr<Node> clonedAncestor = ancestor->cloneNode(false)) { // Might have been removed already during mutation event.
-                clonedAncestor->appendChild(clonedContainer, es, DeprecatedAttachNow);
+                clonedAncestor->appendChild(clonedContainer, es);
                 clonedContainer = clonedAncestor;
             }
         }
@@ -906,15 +906,15 @@ PassRefPtr<Node> Range::processAncestorsAndTheirSiblings(ActionType action, Node
                 break;
             case EXTRACT_CONTENTS: // will remove child from ancestor
                 if (direction == ProcessContentsForward)
-                    clonedContainer->appendChild(child, es, DeprecatedAttachNow);
+                    clonedContainer->appendChild(child, es);
                 else
-                    clonedContainer->insertBefore(child, clonedContainer->firstChild(), es, DeprecatedAttachNow);
+                    clonedContainer->insertBefore(child, clonedContainer->firstChild(), es);
                 break;
             case CLONE_CONTENTS:
                 if (direction == ProcessContentsForward)
-                    clonedContainer->appendChild(child->cloneNode(true), es, DeprecatedAttachNow);
+                    clonedContainer->appendChild(child->cloneNode(true), es);
                 else
-                    clonedContainer->insertBefore(child->cloneNode(true), clonedContainer->firstChild(), es, DeprecatedAttachNow);
+                    clonedContainer->insertBefore(child->cloneNode(true), clonedContainer->firstChild(), es);
                 break;
             }
         }
