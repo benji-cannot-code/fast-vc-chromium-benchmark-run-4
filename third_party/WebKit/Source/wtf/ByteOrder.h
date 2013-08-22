e1e06466dc624254275861e64d66d414130fbf6a
l.gombos@samsung.com <l.gombos@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 04:56:35 +0000

    Do not include "wtf/Platform.h" directly.
    Follow the project coding style and include "config.h" instead of including "wtf/Platform.h". config.h includes "wtf/Platform.h".
    
    Review URL: https://chromiumcodereview.appspot.com/23187004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156542 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/ByteOrder.h
+++ third_party/WebKit/Source/wtf/ByteOrder.h
@@ -33,7 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define WTF_ByteOrder_h
 
 #include "wtf/CPU.h"
-#include "wtf/Platform.h"
 
 #if OS(UNIX)
 #include <arpa/inet.h>
--- third_party/WebKit/Source/wtf/ThreadSafeRefCounted.h
+++ third_party/WebKit/Source/wtf/ThreadSafeRefCounted.h
@@ -35,7 +35,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/DynamicAnnotations.h"
 #include "wtf/FastAllocBase.h"
 #include "wtf/Noncopyable.h"
-#include "wtf/Platform.h"
 #include "wtf/WTFExport.h"
 
 namespace WTF {
