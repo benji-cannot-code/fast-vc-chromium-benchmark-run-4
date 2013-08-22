e571aed3dc779f86d7c7523e405f1d10df65ed91
kouhei@chromium.org <kouhei@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 05:23:50 +0000

    Avoid Node references from AccessibilityImageMapLink from leaking.
    AccessibilityImageMapLink has two RefPtrs, m_areaElement and m_mapElement.
    However, they were not cleared when an AccessibilityImageMapLink instance detached.
    
    BUG=270000
    
    Review URL: https://chromiumcodereview.appspot.com/22893033
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156545 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/accessibility/AccessibilityMockObject.h
+++ third_party/WebKit/Source/core/accessibility/AccessibilityMockObject.h
@@ -47,12 +47,13 @@ public:
 protected:
     AccessibilityObject* m_parent;
 
+    // Must be called when the parent object clears its children.
+    virtual void detachFromParent() OVERRIDE { m_parent = 0; }
+
 private:
     virtual bool isMockObject() const OVERRIDE { return true; }
 
     virtual bool computeAccessibilityIsIgnored() const OVERRIDE;
-    // Must be called when the parent object clears its children.
-    virtual void detachFromParent() OVERRIDE { m_parent = 0; }
 };
 
 inline AccessibilityMockObject* toAccessibilityMockObject(AccessibilityObject* object)
