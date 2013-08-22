ff1d2eb38d050991647d407726064b7867b5d2fa
vasilii@chromium.org <vasilii@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 17:21:19 +0000

    Profile Reset dialog: new UMA histogram for Feedback checkbox.
    BUG=235037
    
    Review URL: https://chromiumcodereview.appspot.com/23360003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219041 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/options/reset_profile_settings_handler.cc
+++ chrome/browser/ui/webui/options/reset_profile_settings_handler.cc
@@ -7,6 +7,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include "base/bind.h"
 #include "base/bind_helpers.h"
+#include "base/metrics/histogram.h"
 #include "base/prefs/pref_service.h"
 #include "base/strings/string16.h"
 #include "base/values.h"
@@ -153,6 +154,7 @@ void ResetProfileSettingsHandler::ResetProfile(bool send_settings) {
       base::Bind(&ResetProfileSettingsHandler::OnResetProfileSettingsDone,
                  AsWeakPtr()));
   content::RecordAction(content::UserMetricsAction("ResetProfile"));
+  UMA_HISTOGRAM_BOOLEAN("ProfileReset.SendFeedback", send_settings);
 }
 
 }  // namespace options
