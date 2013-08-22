04d3840a5945d979c9f6095e31d9385c0162b82c
wolenetz@chromium.org <wolenetz@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-22 23:33:43 +0000

    Let WebMediaPlayerClient seek the MediaPlayerClient
    This Blink change is a prerequisite to fixing seek
    coordination in later CLs. This change lets
    WebMediaPlayers trigger seeks.
    
    BUG=267111
    TEST=all existing media and http/tests/media layout tests pass on desktop linux
    R=acolwell@chromium.org, jamesr@chromium.org, adamk@chromium.org, qinmin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23351006
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156595 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebMediaPlayerClientImpl.cpp
+++ third_party/WebKit/Source/web/WebMediaPlayerClientImpl.cpp
@@ -227,6 +227,11 @@ void WebMediaPlayerClientImpl::mediaSourceOpened(WebMediaSourceNew* webMediaSour
     m_mediaSource->setPrivateAndOpen(adoptPtr(new MediaSourcePrivateImpl(adoptPtr(webMediaSource))));
 }
 
+void WebMediaPlayerClientImpl::requestSeek(double time)
+{
+    m_client->mediaPlayerRequestSeek(time);
+}
+
 // MediaPlayer -------------------------------------------------
 
 void WebMediaPlayerClientImpl::load(const String& url)
