2e5442fc9a05d223c8080d22067a36d09ffe1b60
dgrogan@chromium.org <dgrogan@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-22 10:11:41 +0000

    Roll leveldb to r76.
    Main change that affects chrome is properly sanitizing options.max_open_files.
    
    There's also a large change in this roll that fixes a bug where a large
    contiguous keyspace of deleted data was not getting compacted. The fix
    improves performance for random seeks on such a database.
    
    BUG=268455
    
    Review URL: https://chromiumcodereview.appspot.com/23295021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218978 0039d316-1c4b-4281-b951-d872f2087c98

--- third_party/leveldatabase/port/port_chromium.h
+++ third_party/leveldatabase/port/port_chromium.h
@@ -23,6 +23,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #if defined(OS_WIN)
 #define snprintf _snprintf
+typedef SSIZE_T ssize_t;
 #if !defined(__clang__)
 # define va_copy(a, b) do { (a) = (b); } while (0)
 #endif
