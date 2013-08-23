61caab89042ba87287149ab7c445c46264863d53
caseq@chromium.org <caseq@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-23 08:57:58 +0000

    DevTools: misc layer panel polish
    - fix 3d layer view rotation;
    - deselect layer upon click outside of layer area in 3d view;
    - show empty view with a hint message if not in composited mode;
    
    BUG=
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/22859051
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156626 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/LayersPanel.js
+++ third_party/WebKit/Source/devtools/front_end/LayersPanel.js
@@ -49,6 +49,7 @@ WebInspector.LayersPanel = function()
     this.sidebarTreeElement.removeStyleClass("sidebar-tree");
 
     this._model = new WebInspector.LayerTreeModel();
+    this._model.addEventListener(WebInspector.LayerTreeModel.Events.LayerTreeChanged, this._onLayerTreeUpdated, this);
     this._currentlySelectedLayer = null;
     this._currentlyHoveredLayer = null;
 
@@ -81,12 +82,37 @@ WebInspector.LayersPanel.prototype = {
         WebInspector.Panel.prototype.willHide.call(this);
     },
 
+    _onLayerTreeUpdated: function()
+    {
+        if (this._currentlySelectedLayer && !this._model.layerById(this._currentlySelectedLayer.id()))
+            this._selectLayer(null);
+        if (this._currentlyHoveredLayer && !this._model.layerById(this._currentlyHoveredLayer.id()))
+            this._hoverLayer(null);
+    },
+
     /**
      * @param {WebInspector.Event} event
      */
     _onLayerSelected: function(event)
     {
         var layer = /** @type WebInspector.Layer */ (event.data);
+        this._selectLayer(layer);
+    },
+
+    /**
+     * @param {WebInspector.Event} event
+     */
+    _onLayerHovered: function(event)
+    {
+        var layer = /** @type WebInspector.Layer */ (event.data);
+        this._hoverLayer(layer);
+    },
+
+    /**
+     * @param {WebInspector.Layer?} layer
+     */
+    _selectLayer: function(layer)
+    {
         if (this._currentlySelectedLayer === layer)
             return;
         this._currentlySelectedLayer = layer;
@@ -100,11 +126,10 @@ WebInspector.LayersPanel.prototype = {
     },
 
     /**
-     * @param {WebInspector.Event} event
+     * @param {WebInspector.Layer?} layer
      */
-    _onLayerHovered: function(event)
+    _hoverLayer: function(layer)
     {
-        var layer = /** @type WebInspector.Layer */ (event.data);
         if (this._currentlyHoveredLayer === layer)
             return;
         this._currentlyHoveredLayer = layer;
@@ -116,7 +141,6 @@ WebInspector.LayersPanel.prototype = {
         this._layerTree.hoverLayer(layer);
         this._layers3DView.hoverLayer(layer);
     },
-
 
     __proto__: WebInspector.Panel.prototype
 }
