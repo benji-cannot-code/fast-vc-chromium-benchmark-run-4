38f67d5ca5df6912f3ece43118dea377aad965aa
oshima@chromium.org <oshima@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 21:51:49 +0000

    Use gfx::Screen to provide display information in WindowSizer
     I think this was necessary because gfx::Screen wasn't instance.
    
    BUG=272460
    
    Review URL: https://chromiumcodereview.appspot.com/22888006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219349 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/window_sizer/window_sizer.h
+++ chrome/browser/ui/window_sizer/window_sizer.h
@@ -14,23 +14,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 class Browser;
 
-// An interface implemented by an object that can retrieve information about
-// the monitors on the system.
-class MonitorInfoProvider {
- public:
-  virtual ~MonitorInfoProvider() {}
-
-  // Returns the bounds of the work area of the primary monitor.
-  virtual gfx::Rect GetPrimaryDisplayWorkArea() const = 0;
-
-  // Returns the bounds of the primary monitor.
-  virtual gfx::Rect GetPrimaryDisplayBounds() const = 0;
-
-  // Returns the bounds of the work area of the monitor that most closely
-  // intersects the provided bounds.
-  virtual gfx::Rect GetMonitorWorkAreaMatching(
-      const gfx::Rect& match_rect) const = 0;
-};
+namespace gfx {
+class Screen;
+}
 
 ///////////////////////////////////////////////////////////////////////////////
 // WindowSizer
@@ -51,10 +37,10 @@ class WindowSizer {
   // MonitorInfoProvider using the physical screen.
   WindowSizer(StateProvider* state_provider, const Browser* browser);
 
-  // WindowSizer owns |state_provider| and |monitor_info_provider|.
-  // It will use the supplied monitor info provider. Used only for testing.
+  // WindowSizer owns |state_provider| and will use the supplied |screen|.
+  // Used only for testing.
   WindowSizer(StateProvider* state_provider,
-              MonitorInfoProvider* monitor_info_provider,
+              gfx::Screen* screen,
               const Browser* browser);
 
   virtual ~WindowSizer();
@@ -186,7 +172,7 @@ class WindowSizer {
 
   // Providers for persistent storage and monitor metrics.
   scoped_ptr<StateProvider> state_provider_;
-  scoped_ptr<MonitorInfoProvider> monitor_info_provider_;
+  gfx::Screen* screen_;  // not owned.
 
   // Note that this browser handle might be NULL.
   const Browser* browser_;
