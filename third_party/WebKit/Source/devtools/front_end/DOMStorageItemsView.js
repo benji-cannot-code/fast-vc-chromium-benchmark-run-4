bf99eb7b844d995015e2bbfeb01d2ef6cdc7de85
vivek.vg@samsung.com <vivek.vg@samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-23 15:15:53 +0000

    DevTools: DOMStorage should notify the view about storage modifications instead of DOMStorageModel
    R=apavlov, pfeldman, vsevik
    
    Review URL: https://chromiumcodereview.appspot.com/22346003
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156635 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/devtools/front_end/DOMStorageItemsView.js
+++ third_party/WebKit/Source/devtools/front_end/DOMStorageItemsView.js
@@ -29,12 +29,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * @constructor
  * @extends {WebInspector.View}
  */
-WebInspector.DOMStorageItemsView = function(domStorage, domStorageModel)
+WebInspector.DOMStorageItemsView = function(domStorage)
 {
     WebInspector.View.call(this);
 
     this.domStorage = domStorage;
-    this.domStorageModel = domStorageModel;
 
     this.element.addStyleClass("storage-view");
     this.element.addStyleClass("table");
@@ -46,10 +45,10 @@ WebInspector.DOMStorageItemsView = function(domStorage, domStorageModel)
     this.refreshButton = new WebInspector.StatusBarButton(WebInspector.UIString("Refresh"), "refresh-storage-status-bar-item");
     this.refreshButton.addEventListener("click", this._refreshButtonClicked, this);
 
-    this.domStorageModel.addEventListener(WebInspector.DOMStorageModel.Events.DOMStorageItemsCleared, this._domStorageItemsCleared, this);
-    this.domStorageModel.addEventListener(WebInspector.DOMStorageModel.Events.DOMStorageItemRemoved, this._domStorageItemRemoved, this);
-    this.domStorageModel.addEventListener(WebInspector.DOMStorageModel.Events.DOMStorageItemAdded, this._domStorageItemAdded, this);
-    this.domStorageModel.addEventListener(WebInspector.DOMStorageModel.Events.DOMStorageItemUpdated, this._domStorageItemUpdated, this);
+    this.domStorage.addEventListener(WebInspector.DOMStorage.Events.DOMStorageItemsCleared, this._domStorageItemsCleared, this);
+    this.domStorage.addEventListener(WebInspector.DOMStorage.Events.DOMStorageItemRemoved, this._domStorageItemRemoved, this);
+    this.domStorage.addEventListener(WebInspector.DOMStorage.Events.DOMStorageItemAdded, this._domStorageItemAdded, this);
+    this.domStorage.addEventListener(WebInspector.DOMStorage.Events.DOMStorageItemUpdated, this._domStorageItemUpdated, this);
 }
 
 WebInspector.DOMStorageItemsView.prototype = {
@@ -145,7 +144,7 @@ WebInspector.DOMStorageItemsView.prototype = {
             if (children[i].data.key === storageData.key)
                 return;
 
-        var childNode = new WebInspector.DataGridNode({key: storageData.key, value: storageData.newValue}, false);
+        var childNode = new WebInspector.DataGridNode({key: storageData.key, value: storageData.value}, false);
         rootNode.insertChild(childNode, children.length - 1);
     },
 
@@ -172,8 +171,8 @@ WebInspector.DOMStorageItemsView.prototype = {
                     return;
                 }
                 keyFound = true;
-                if (childNode.data.value !== storageData.newValue) {
-                    childNode.data.value = storageData.newValue;
+                if (childNode.data.value !== storageData.value) {
+                    childNode.data.value = storageData.value;
                     childNode.refresh();
                     childNode.select();
                     childNode.reveal();
