5bfa098470f7d52704d9f8615c4eed83d909b740
mad@chromium.org <mad@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 21:49:03 +0000

    Outdated installs don't need a field trial anymore.
    TBR=ben@chromium.org
    BUG=265957
    
    Review URL: https://chromiumcodereview.appspot.com/23402006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219348 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/upgrade_detector_impl.cc
+++ chrome/browser/upgrade_detector_impl.cc
@@ -13,7 +13,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/files/file_path.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/memory/singleton.h"
-#include "base/metrics/field_trial.h"
 #include "base/path_service.h"
 #include "base/process/launch.h"
 #include "base/strings/string_number_conversions.h"
@@ -56,10 +55,6 @@ const int kNotifyCycleTimeForTestingMs = 500;  // Half a second.
 
 // The number of days after which we identify a build/install as outdated.
 const uint64 kOutdatedBuildAgeInDays = 12 * 7;
-
-// Finch Experiment strings to identify if we should check for outdated install.
-const char kOutdatedInstallCheckTrialName[] = "OutdatedInstallCheck";
-const char kOutdatedInstallCheck12WeeksGroupName[] = "12WeeksOutdatedInstall";
 
 std::string CmdLineInterval() {
   const CommandLine& cmd_line = *CommandLine::ForCurrentProcess();
@@ -323,11 +318,6 @@ bool UpgradeDetectorImpl::DetectOutdatedInstall() {
   static bool simulate_outdated = CommandLine::ForCurrentProcess()->HasSwitch(
       switches::kSimulateOutdated);
   if (!simulate_outdated) {
-    if (base::FieldTrialList::FindFullName(kOutdatedInstallCheckTrialName) !=
-            kOutdatedInstallCheck12WeeksGroupName) {
-      return false;
-    }
-
     // Also don't show the bubble if we have a brand code that is NOT organic.
     std::string brand;
     if (google_util::GetBrand(&brand) && !google_util::IsOrganic(brand))
