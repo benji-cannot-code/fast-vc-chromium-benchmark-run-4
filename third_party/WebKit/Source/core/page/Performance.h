dd342796691469501a7668c2211a8c3f221a7e88
simonjam@chromium.org <simonjam@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-23 04:03:30 +0000

    Record the reference time when Performance is constructed.
    BUG=264490
    
    Review URL: https://chromiumcodereview.appspot.com/23367007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156613 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/Performance.h
+++ third_party/WebKit/Source/core/page/Performance.h
@@ -104,6 +104,7 @@ private:
 
     Vector<RefPtr<PerformanceEntry> > m_resourceTimingBuffer;
     unsigned m_resourceTimingBufferSize;
+    double m_referenceTime;
 
     RefPtr<UserTiming> m_userTiming;
 };
