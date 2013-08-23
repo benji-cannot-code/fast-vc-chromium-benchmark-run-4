7c20b8c17da5f0842d4e459b6134174f0defc28f
vollick@chromium.org <vollick@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 00:44:30 +0000

    Add plumbing for scroll parent.
    Plumbs the scroll parent information into the cc layer trees. These relationships are not yet used. (That task is accomplished here https://codereview.chromium.org/18191009/).
    
    R=shawnsingh@chromium.org
    BUG=254435
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=219026
    
    Review URL: https://chromiumcodereview.appspot.com/18133004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219167 0039d316-1c4b-4281-b951-d872f2087c98

--- webkit/renderer/compositor_bindings/web_layer_impl.h
+++ webkit/renderer/compositor_bindings/web_layer_impl.h
@@ -127,6 +127,9 @@ class WebLayerImpl : public WebKit::WebLayer, public cc::LayerClient {
   // LayerClient implementation.
   virtual std::string DebugName() OVERRIDE;
 
+  virtual void setScrollParent(WebKit::WebLayer* parent);
+  virtual void setClipParent(WebKit::WebLayer* parent);
+
  protected:
   scoped_refptr<cc::Layer> layer_;
   WebKit::WebLayerClient* web_layer_client_;
