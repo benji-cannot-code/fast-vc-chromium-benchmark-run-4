67f9dbe41236381785e93d4e9d5cad730bb17b90
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 04:11:16 +0000

    sync: Improve testing of download functions
    Refactor the download functions so the commit to the server can be
    separated from the building of the commit message.  This allows us to
    test these functions in download_unittest.cc without needing to use the
    heavyweight MockConnectionManager.  It also lets us improve our testing
    of AppendClientDebugInfoIfNeeded by using less mocks and more real code.
    
    An important side benefit of this change is that it makes progress
    towards the removal of ModelTypeInvalidationMap, which is a step
    towards implementing crbug.com/233437.
    
    Also included in this change is a new TRACE_EVENT.  Though it's not
    really new, since it was removed (accidentally) in the Syncer refactor
    (r209867).  It's not related to this change, but it's a one-liner and I
    happen to be modifying nearby code.
    
    BUG=233437
    
    Review URL: https://chromiumcodereview.appspot.com/22849021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219209 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/test/engine/mock_connection_manager.h
+++ sync/test/engine/mock_connection_manager.h
@@ -235,10 +235,6 @@ class MockConnectionManager : public ServerConnectionManager {
     expected_filter_ = expected_filter;
   }
 
-  void ExpectGetUpdatesRequestStates(const ModelTypeInvalidationMap& states) {
-    expected_states_ = states;
-  }
-
   void SetServerReachable();
 
   void SetServerNotReachable();
@@ -394,8 +390,6 @@ class MockConnectionManager : public ServerConnectionManager {
   bool use_legacy_bookmarks_protocol_;
 
   ModelTypeSet expected_filter_;
-
-  ModelTypeInvalidationMap expected_states_;
 
   int num_get_updates_requests_;
 
