985d3f04ab78ab2471fa16132f3c6ae611e9812f
kaznacheev@chromium.org <kaznacheev@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 12:53:42 +0000

    Use the newest running version of Chrome for port forwarding
    While at it made PortForwardingController::Connection retain the DevToolsAdbBridge
    instance to make sure that the message loop exists for long enough.
    
    BUG=None
    R=pfeldman@chromium.org
    
    Review URL: https://codereview.chromium.org/22589006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219270 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/devtools/tethering_adb_filter.h
+++ chrome/browser/devtools/tethering_adb_filter.h
@@ -82,7 +82,7 @@ class TetheringAdbFilter : public base::RefCountedThreadSafe<
 class PortForwardingController {
  public:
   PortForwardingController(
-      base::MessageLoop* adb_message_loop,
+      scoped_refptr<DevToolsAdbBridge> bridge,
       PrefService* pref_service);
 
   virtual ~PortForwardingController();
@@ -93,7 +93,7 @@ class PortForwardingController {
   class Connection;
   typedef std::map<std::string, Connection*> Registry;
 
-  base::MessageLoop* adb_message_loop_;
+  scoped_refptr<DevToolsAdbBridge> bridge_;
   PrefService* pref_service_;
   Registry registry_;
 };
