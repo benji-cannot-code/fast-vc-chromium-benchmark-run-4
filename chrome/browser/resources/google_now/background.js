9787c69c2fc1af4c30d61f661905aae4629adb30
vadimt@chromium.org <vadimt@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 00:10:32 +0000

    Handling errors from chrome APIs. Some Chrome API calls can fail and return 'undefined' result. Making our code not send crashes and behave reasonably in such cases.
    Also, sending crashes if server sends malformed cards.
    
    + some smaller cleanups.
    
    BUG=164227
    
    Review URL: https://chromiumcodereview.appspot.com/22912021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219157 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/google_now/background.js
+++ chrome/browser/resources/google_now/background.js
@@ -24,8 +24,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // TODO(vadimt): Honor the flag the enables Google Now integration.
 // TODO(vadimt): Figure out the final values of the constants.
 // TODO(vadimt): Remove 'console' calls.
-// TODO(vadimt): Consider sending JS stacks for chrome.* API errors and
-// malformed server responses.
+// TODO(vadimt): Consider sending JS stacks for malformed server responses.
 
 /**
  * Standard response code for successful HTTP requests. This is the only success
@@ -347,6 +346,7 @@ function removeAllCards() {
   // notifications center and bug 260376 is fixed, the below clearing
   // code is no longer necessary.
   instrumented.notifications.getAll(function(notifications) {
+    notifications = notifications || {};
     for (var notificationId in notifications) {
       chrome.notifications.clear(notificationId, function() {});
     }
@@ -406,7 +406,7 @@ function requestLocation() {
   console.log('requestLocation');
   recordEvent(GoogleNowEvent.LOCATION_REQUEST);
   // TODO(vadimt): Figure out location request options.
-  chrome.metricsPrivate.getVariationParams('GoogleNow', function(params) {
+  instrumented.metricsPrivate.getVariationParams('GoogleNow', function(params) {
     var minDistanceInMeters =
         parseInt(params && params.minDistanceInMeters, 10) ||
         100;
@@ -583,15 +583,12 @@ function retryPendingDismissals() {
  */
 function onNotificationClicked(notificationId, selector) {
   instrumented.storage.local.get('notificationsData', function(items) {
-    items.notificationsData = items.notificationsData || {};
+    var notificationData = items &&
+        items.notificationsData &&
+        items.notificationsData[notificationId];
 
-    var notificationData = items.notificationsData[notificationId];
-
-    if (!notificationData) {
-      // 'notificationsData' in storage may not match the actual list of
-      // notifications.
+    if (!notificationData)
       return;
-    }
 
     var actionUrls = notificationData.actionUrls;
     if (typeof actionUrls != 'object') {
@@ -599,8 +596,7 @@ function onNotificationClicked(notificationId, selector) {
     }
 
     var url = selector(actionUrls);
-
-    if (typeof url != 'string')
+    if (!url)
       return;
 
     instrumented.tabs.create({url: url}, function(tab) {
@@ -887,7 +883,8 @@ function onStateChange() {
                   instrumented.storage.local.get(
                       'userRespondedToToast',
                       function(items) {
-                        var userRespondedToToast = !!items.userRespondedToToast;
+                        var userRespondedToToast =
+                            !items || !!items.userRespondedToToast;
                         updateRunningState(
                             signedIn,
                             geolocationEnabled,
@@ -975,10 +972,9 @@ instrumented.notifications.onButtonClicked.addListener(
         chrome.metricsPrivate.recordUserAction(
             'GoogleNow.ButtonClicked' + buttonIndex);
         onNotificationClicked(notificationId, function(actionUrls) {
-          if (!Array.isArray(actionUrls.buttonUrls))
-            return undefined;
-
-          return actionUrls.buttonUrls[buttonIndex];
+          var url = actionUrls.buttonUrls[buttonIndex];
+          verify(url, 'onButtonClicked: no url for a button');
+          return url;
         });
       }
     });
