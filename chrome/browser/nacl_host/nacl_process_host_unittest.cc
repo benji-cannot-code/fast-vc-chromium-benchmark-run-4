8c02d5b04b48ca4c7eff2071840c88abd6b140e4
mseaborn@chromium.org <mseaborn@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 00:40:08 +0000

    NaCl: Randomise the sandbox base address on 32-bit Windows
    For comparison, NaClPageAllocRandomized() in NaCl's win/sel_memory.c
    picks a random 0-2GB address and tries to allocate it, and retries
    this multiple times.  When I tried that approach, it often did
    multiple retries, which might reduce the entropy of the allocation if
    the retry loop often gives up.  It also doesn't take into account
    whether the kernel provides 2GB or 3GB of user address space (a boot
    time option).
    
    So instead of doing that, we query the available address space.  On
    32-bit Windows XP, the process should have about 1900MB of contiguous
    address space free.  So wherever the 1GB sandbox is allocated within
    that, there will be plenty of space either above or below it to load
    chrome.dll.
    
    Add a unit test for the allocation function so that it gets covered by
    the trybots, which only cover 64-bit Windows, and also because the
    production code path doesn't report an error if the allocation fails.
    
    BUG=https://code.google.com/p/nativeclient/issues/detail?id=3608
    TEST=test launching a NaCl plugin in a loop in a Windows XP VM
    
    Review URL: https://chromiumcodereview.appspot.com/23369002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219166 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ chrome/browser/nacl_host/nacl_process_host_unittest.cc
@@ -0,0 +1,16 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "chrome/browser/nacl_host/nacl_process_host.h"
+#include "testing/gtest/include/gtest/gtest.h"
+
+#if defined(OS_WIN)
+TEST(NaClProcessHostTest, AddressSpaceAllocation) {
+  size_t size = 1 << 20;  // 1 MB
+  void* addr = nacl::AllocateAddressSpaceASLR(GetCurrentProcess(), size);
+  bool success = VirtualFree(addr, 0, MEM_RELEASE);
+  ASSERT_TRUE(success);
+}
+#endif
