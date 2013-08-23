4730d0ed7cdbfebe441bd5c8db91ceef9cbbf7ea
sl.ostapenko@samsung.com <sl.ostapenko@samsung.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 17:35:56 +0000

    Remove double slash in include file path.
    Confuses rpmbuild on some systems: https://bugzilla.redhat.com/show_bug.cgi?id=304121
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23334004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219298 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/media/media_internals_handler.cc
+++ content/browser/media/media_internals_handler.cc
@@ -8,7 +8,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/bind.h"
 #include "base/bind_helpers.h"
 #include "base/values.h"
-#include "content/browser//media/media_internals_proxy.h"
+#include "content/browser/media/media_internals_proxy.h"
 #include "content/public/browser/browser_thread.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
