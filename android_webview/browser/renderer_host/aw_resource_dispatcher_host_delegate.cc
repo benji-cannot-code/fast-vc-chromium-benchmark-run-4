82561995c0328e6aaa483d293a9fe9722ab2cdb4
benm@chromium.org <benm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-23 18:21:56 +0000

    [Android WebView] Fix crash in AwResourceDispatcherHostDelegate::DownloadStarting
    The request may not have started, in which case we cannot ask for the
    MIME type or response headers.
    
    BUG=277744
    
    Review URL: https://chromiumcodereview.appspot.com/23075018
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219312 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.cc
+++ android_webview/browser/renderer_host/aw_resource_dispatcher_host_delegate.cc
@@ -22,6 +22,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/resource_throttle.h"
 #include "content/public/common/url_constants.h"
 #include "net/base/load_flags.h"
+#include "net/http/http_response_headers.h"
 #include "net/url_request/url_request.h"
 
 using android_webview::AwContentsIoThreadClient;
@@ -246,10 +247,16 @@ void AwResourceDispatcherHostDelegate::DownloadStarting(
   std::string mime_type;
   int64 content_length = request->GetExpectedContentSize();
 
-  request->GetResponseHeaderByName("content-disposition", &content_disposition);
   request->extra_request_headers().GetHeader(
       net::HttpRequestHeaders::kUserAgent, &user_agent);
-  request->GetMimeType(&mime_type);
+
+
+  net::HttpResponseHeaders* response_headers = request->response_headers();
+  if (response_headers) {
+    response_headers->GetNormalizedHeader("content-disposition",
+        &content_disposition);
+    response_headers->GetMimeType(&mime_type);
+  }
 
   request->Cancel();
 
