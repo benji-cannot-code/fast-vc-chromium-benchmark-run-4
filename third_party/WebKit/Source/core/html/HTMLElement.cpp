fe73b2834a9247b1dc9e753b4dfe0248af65dc24
igor.o@sisa.samsung.com <igor.o@sisa.samsung.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-24 00:49:23 +0000

    If an element has unicode-bidi: plaintext and no strong directional characters. By default it should be LTR.
    BUG=277687
    
    Review URL: https://chromiumcodereview.appspot.com/23112019
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156664 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/HTMLElement.cpp
+++ third_party/WebKit/Source/core/html/HTMLElement.cpp
@@ -46,6 +46,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "core/editing/markup.h"
 #include "core/html/HTMLBRElement.h"
 #include "core/html/HTMLFormElement.h"
+#include "core/html/HTMLInputElement.h"
 #include "core/html/HTMLTemplateElement.h"
 #include "core/html/HTMLTextFormControlElement.h"
 #include "core/html/parser/HTMLParserIdioms.h"
@@ -847,12 +848,12 @@ TextDirection HTMLElement::directionalityIfhasDirAutoAttribute(bool& isAuto) con
 
 TextDirection HTMLElement::directionality(Node** strongDirectionalityTextNode) const
 {
-    if (isHTMLTextFormControlElement(this)) {
-        HTMLTextFormControlElement* textElement = toHTMLTextFormControlElement(const_cast<HTMLElement*>(this));
+    if (hasTagName(inputTag)) {
+        HTMLInputElement* inputElement = toHTMLInputElement(const_cast<HTMLElement*>(this));
         bool hasStrongDirectionality;
-        Unicode::Direction textDirection = textElement->value().defaultWritingDirection(&hasStrongDirectionality);
+        Unicode::Direction textDirection = inputElement->value().defaultWritingDirection(&hasStrongDirectionality);
         if (strongDirectionalityTextNode)
-            *strongDirectionalityTextNode = hasStrongDirectionality ? textElement : 0;
+            *strongDirectionalityTextNode = hasStrongDirectionality ? inputElement : 0;
         return (textDirection == Unicode::LeftToRight) ? LTR : RTL;
     }
 
