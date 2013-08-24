benji-cannot-code/fast-vc-chromium-benchmark-run-4

commit f6705b248caec4652f2dd14eaaa08330bdd40ad8

Migrate WebMediaPlayer implementations to new load() method.
BUG=272993
TEST=All existing LayoutTests still pass.

Review URL: https://chromiumcodereview.appspot.com/23080003

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@218819 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/media/android/webmediaplayer_android.cc
@@ -196,25 +196,33 @@ WebMediaPlayerAndroid::~WebMediaPlayerAndroid() {
 #endif
 }
 
-void WebMediaPlayerAndroid::load(const WebURL& url, CORSMode cors_mode) {
-  load(url, NULL, cors_mode);
-}
-
-void WebMediaPlayerAndroid::load(const WebURL& url,
-                                 WebMediaSource* media_source,
+void WebMediaPlayerAndroid::load(LoadType load_type,
+                                 const WebKit::WebURL& url,
                                  CORSMode cors_mode) {
-  source_type_ = MediaPlayerAndroid::SOURCE_TYPE_URL;
+#if defined(GOOGLE_TV)
+  // TODO(acolwell): Remove this hack once Blink-side changes land.
+  if (load_type == LoadTypeURL && media_stream_client_)
+    load_type = LoadTypeMediaStream;
+#endif
+
+  switch (load_type) {
+    case LoadTypeURL:
+      source_type_ = MediaPlayerAndroid::SOURCE_TYPE_URL;
+      break;
+    case LoadTypeMediaSource:
+      source_type_ = MediaPlayerAndroid::SOURCE_TYPE_MSE;
+      break;
+    case LoadTypeMediaStream:
+#if defined(GOOGLE_TV)
+      source_type_ = MediaPlayerAndroid::SOURCE_TYPE_MSE;
+#else
+      source_type_ = MediaPlayerAndroid::SOURCE_TYPE_URL;
+#endif
+      break;
+  }
+
   has_media_metadata_ = false;
   has_media_info_ = false;
-
-  if (media_source)
-    source_type_ = MediaPlayerAndroid::SOURCE_TYPE_MSE;
-#if defined(GOOGLE_TV)
-  if (media_stream_client_) {
-    DCHECK(!media_source);
-    source_type_ = MediaPlayerAndroid::SOURCE_TYPE_STREAM;
-  }
-#endif
 
   media::SetDecryptorReadyCB set_decryptor_ready_cb;
   if (decryptor_) {  // |decryptor_| can be NULL is EME if not enabled.
@@ -229,7 +237,8 @@ void WebMediaPlayerAndroid::load(const WebURL& url,
     // |media_source_delegate_| is owned, so Unretained() is safe here.
     if (source_type_ == MediaPlayerAndroid::SOURCE_TYPE_MSE) {
       media_source_delegate_->InitializeMediaSource(
-          media_source,
+          base::Bind(&WebMediaPlayerAndroid::OnMediaSourceOpened,
+                     base::Unretained(this)),
           base::Bind(&WebMediaPlayerAndroid::OnNeedKey, base::Unretained(this)),
           set_decryptor_ready_cb,
           base::Bind(&WebMediaPlayerAndroid::UpdateNetworkState,
@@ -1114,6 +1123,11 @@ void WebMediaPlayerAndroid::OnKeyMessage(const std::string& session_id,
                       message.empty() ? NULL : &message[0],
                       message.size(),
                       destination_url_gurl);
+}
+
+void WebMediaPlayerAndroid::OnMediaSourceOpened(
+    WebKit::WebMediaSourceNew* web_media_source) {
+  client_->mediaSourceOpened(web_media_source);
 }
 
 void WebMediaPlayerAndroid::OnNeedKey(const std::string& session_id,

content/content_renderer.gypi
@@ -251,8 +251,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'renderer/media/webmediaplayer_params.h',
     'renderer/media/webmediaplayer_util.cc',
     'renderer/media/webmediaplayer_util.h',
-    'renderer/media/webmediasourceclient_impl.cc',
-    'renderer/media/webmediasourceclient_impl.h',
+    'renderer/media/webmediasource_impl.cc',
+    'renderer/media/webmediasource_impl.h',
     'renderer/media/websourcebuffer_impl.cc',
     'renderer/media/websourcebuffer_impl.h',
     'renderer/memory_benchmarking_extension.cc',

content/renderer/media/android/media_source_delegate.cc
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_number_conversions.h"
 #include "content/renderer/media/android/webmediaplayer_proxy_android.h"
 #include "content/renderer/media/webmediaplayer_util.h"
-#include "content/renderer/media/webmediasourceclient_impl.h"
+#include "content/renderer/media/webmediasource_impl.h"
 #include "media/base/android/demuxer_stream_player_params.h"
 #include "media/base/bind_to_loop.h"
 #include "media/base/demuxer_stream.h"
@@ -95,7 +95,7 @@ void MediaSourceDelegate::Destroy() {
 
   duration_change_cb_.Reset();
   update_network_state_cb_.Reset();
-  media_source_.reset();
+  media_source_opened_cb_.Reset();
   proxy_ = NULL;
 
   main_weak_this_.InvalidateWeakPtrs();
@@ -133,14 +133,14 @@ void MediaSourceDelegate::StopDemuxer() {
 }
 
 void MediaSourceDelegate::InitializeMediaSource(
-    WebKit::WebMediaSource* media_source,
+    const MediaSourceOpenedCB& media_source_opened_cb,
     const media::NeedKeyCB& need_key_cb,
     const media::SetDecryptorReadyCB& set_decryptor_ready_cb,
     const UpdateNetworkStateCB& update_network_state_cb,
     const DurationChangeCB& duration_change_cb) {
   DCHECK(main_loop_->BelongsToCurrentThread());
-  DCHECK(media_source);
-  media_source_.reset(media_source);
+  DCHECK(!media_source_opened_cb.is_null());
+  media_source_opened_cb_ = media_source_opened_cb;
   need_key_cb_ = need_key_cb;
   set_decryptor_ready_cb_ = set_decryptor_ready_cb;
   update_network_state_cb_ = media::BindToCurrentLoop(update_network_state_cb);
@@ -683,10 +683,10 @@ int MediaSourceDelegate::GetDurationMs() {
 
 void MediaSourceDelegate::OnDemuxerOpened() {
   DCHECK(main_loop_->BelongsToCurrentThread());
-  if (!media_source_)
+  if (media_source_opened_cb_.is_null())
     return;
 
-  media_source_->open(new WebMediaSourceClientImpl(
+  media_source_opened_cb_.Run(new WebMediaSourceImpl(
       chunk_demuxer_.get(), base::Bind(&LogMediaSourceError, media_log_)));
 }
 

content/renderer/media/android/media_source_delegate.h
@@ -36,6 +36,8 @@ class WebMediaPlayerProxyAndroid;
 
 class MediaSourceDelegate : public media::DemuxerHost {
  public:
+  typedef base::Callback<void(WebKit::WebMediaSourceNew*)>
+      MediaSourceOpenedCB;
   typedef base::Callback<void(WebKit::WebMediaPlayer::NetworkState)>
       UpdateNetworkStateCB;
   typedef base::Callback<void(const base::TimeDelta&)> DurationChangeCB;
@@ -57,7 +59,7 @@ class MediaSourceDelegate : public media::DemuxerHost {
   // Initialize the MediaSourceDelegate. |media_source| will be owned by
   // this object after this call.
   void InitializeMediaSource(
-      WebKit::WebMediaSource* media_source,
+      const MediaSourceOpenedCB& media_source_opened_cb,
       const media::NeedKeyCB& need_key_cb,
       const media::SetDecryptorReadyCB& set_decryptor_ready_cb,
       const UpdateNetworkStateCB& update_network_state_cb,
@@ -203,7 +205,6 @@ class MediaSourceDelegate : public media::DemuxerHost {
   DurationChangeCB duration_change_cb_;
 
   scoped_ptr<media::ChunkDemuxer> chunk_demuxer_;
-  scoped_ptr<WebKit::WebMediaSource> media_source_;
   media::Demuxer* demuxer_;
   bool is_demuxer_ready_;
 
@@ -220,6 +221,7 @@ class MediaSourceDelegate : public media::DemuxerHost {
   // Keep a list of buffered time ranges.
   WebKit::WebTimeRanges buffered_web_time_ranges_;
 
+  MediaSourceOpenedCB media_source_opened_cb_;
   media::NeedKeyCB need_key_cb_;
 
   // The currently selected key system. Empty string means that no key system

content/renderer/media/android/webmediaplayer_android.h
@@ -88,10 +88,9 @@ class WebMediaPlayerAndroid
   virtual bool canEnterFullscreen() const;
 
   // Resource loading.
-  virtual void load(const WebKit::WebURL& url, CORSMode cors_mode);
-  virtual void load(const WebKit::WebURL& url,
-                    WebKit::WebMediaSource* media_source,
-                    CORSMode cors_mode);
+  virtual void load(LoadType load_type,
+                    const WebKit::WebURL& url,
+                    CORSMode cors_mode) OVERRIDE;
 
   // Playback controls.
   virtual void play();
@@ -221,6 +220,8 @@ class WebMediaPlayerAndroid
   void OnKeyMessage(const std::string& session_id,
                     const std::vector<uint8>& message,
                     const std::string& destination_url);
+
+  void OnMediaSourceOpened(WebKit::WebMediaSourceNew* web_media_source);
 
   void OnNeedKey(const std::string& type,
                  const std::string& session_id,

content/renderer/media/webmediaplayer_impl.cc
@@ -28,7 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/renderer/media/webmediaplayer_delegate.h"
 #include "content/renderer/media/webmediaplayer_params.h"
 #include "content/renderer/media/webmediaplayer_util.h"
-#include "content/renderer/media/webmediasourceclient_impl.h"
+#include "content/renderer/media/webmediasource_impl.h"
 #include "content/renderer/pepper/pepper_webplugin_impl.h"
 #include "gpu/GLES2/gl2extchromium.h"
 #include "media/audio/null_audio_sink.h"
@@ -247,24 +247,18 @@ URLSchemeForHistogram URLScheme(const GURL& url) {
 
 }  // anonymous namespace
 
-void WebMediaPlayerImpl::load(const WebKit::WebURL& url, CORSMode cors_mode) {
-  load(url, NULL, cors_mode);
-}
-
-void WebMediaPlayerImpl::load(const WebKit::WebURL& url,
-                              WebKit::WebMediaSource* media_source,
+void WebMediaPlayerImpl::load(LoadType load_type, const WebKit::WebURL& url,
                               CORSMode cors_mode) {
   if (!defer_load_cb_.is_null()) {
     defer_load_cb_.Run(base::Bind(
-        &WebMediaPlayerImpl::DoLoad, AsWeakPtr(), url, media_source,
-        cors_mode));
+        &WebMediaPlayerImpl::DoLoad, AsWeakPtr(), load_type, url, cors_mode));
     return;
   }
-  DoLoad(url, media_source, cors_mode);
+  DoLoad(load_type, url, cors_mode);
 }
 
-void WebMediaPlayerImpl::DoLoad(const WebKit::WebURL& url,
-                                WebKit::WebMediaSource* media_source,
+void WebMediaPlayerImpl::DoLoad(LoadType load_type,
+                                const WebKit::WebURL& url,
                                 CORSMode cors_mode) {
   DCHECK(main_loop_->BelongsToCurrentThread());
 
@@ -273,6 +267,8 @@ void WebMediaPlayerImpl::DoLoad(const WebKit::WebURL& url,
 
   // Set subresource URL for crash reporting.
   base::debug::SetCrashKeyValue("subresource_url", gurl.spec());
+
+  load_type_ = load_type;
 
   // Handle any volume/preload changes that occurred before load().
   setVolume(GetClient()->volume());
@@ -283,9 +279,9 @@ void WebMediaPlayerImpl::DoLoad(const WebKit::WebURL& url,
   media_log_->AddEvent(media_log_->CreateLoadEvent(url.spec()));
 
   // Media source pipelines can start immediately.
-  if (media_source) {
+  if (load_type == LoadTypeMediaSource) {
     supports_save_ = false;
-    StartPipeline(media_source);
+    StartPipeline();
     return;
   }
 
@@ -913,10 +909,9 @@ void WebMediaPlayerImpl::OnPipelineBufferingState(
   Repaint();
 }
 
-void WebMediaPlayerImpl::OnDemuxerOpened(
-    scoped_ptr<WebKit::WebMediaSource> media_source) {
+void WebMediaPlayerImpl::OnDemuxerOpened() {
   DCHECK(main_loop_->BelongsToCurrentThread());
-  media_source->open(new WebMediaSourceClientImpl(
+  GetClient()->mediaSourceOpened(new WebMediaSourceImpl(
       chunk_demuxer_, base::Bind(&LogMediaSourceError, media_log_)));
 }
 
@@ -1012,7 +1007,7 @@ void WebMediaPlayerImpl::DataSourceInitialized(const GURL& gurl, bool success) {
     return;
   }
 
-  StartPipeline(NULL);
+  StartPipeline();
 }
 
 void WebMediaPlayerImpl::NotifyDownloading(bool is_downloading) {
@@ -1026,15 +1021,16 @@ void WebMediaPlayerImpl::NotifyDownloading(bool is_downloading) {
           "is_downloading_data", is_downloading));
 }
 
-void WebMediaPlayerImpl::StartPipeline(WebKit::WebMediaSource* media_source) {
+void WebMediaPlayerImpl::StartPipeline() {
   const CommandLine* cmd_line = CommandLine::ForCurrentProcess();
   bool increase_preroll_on_underflow = true;
 
   // Keep track if this is a MSE or non-MSE playback.
-  UMA_HISTOGRAM_BOOLEAN("Media.MSE.Playback", (media_source != NULL));
+  UMA_HISTOGRAM_BOOLEAN("Media.MSE.Playback",
+                        (load_type_ == LoadTypeMediaSource));
 
   // Figure out which demuxer to use.
-  if (!media_source) {
+  if (load_type_ != LoadTypeMediaSource) {
     DCHECK(!chunk_demuxer_);
     DCHECK(data_source_);
 
@@ -1053,10 +1049,8 @@ void WebMediaPlayerImpl::StartPipeline(WebKit::WebMediaSource* media_source) {
           base::Bind(&WebMediaPlayerImpl::OnTextTrack, base::Unretained(this));
     }
 
-    scoped_ptr<WebKit::WebMediaSource> ms(media_source);
     chunk_demuxer_ = new media::ChunkDemuxer(
-        BIND_TO_RENDER_LOOP_1(&WebMediaPlayerImpl::OnDemuxerOpened,
-                              base::Passed(&ms)),
+        BIND_TO_RENDER_LOOP(&WebMediaPlayerImpl::OnDemuxerOpened),
         BIND_TO_RENDER_LOOP_1(&WebMediaPlayerImpl::OnNeedKey, ""),
         add_text_track_cb,
         base::Bind(&LogMediaSourceError, media_log_));

content/renderer/media/webmediaplayer_impl.h
@@ -88,10 +88,9 @@ class WebMediaPlayerImpl
       const WebMediaPlayerParams& params);
   virtual ~WebMediaPlayerImpl();
 
-  virtual void load(const WebKit::WebURL& url, CORSMode cors_mode);
-  virtual void load(const WebKit::WebURL& url,
-                    WebKit::WebMediaSource* media_source,
-                    CORSMode cors_mode);
+  virtual void load(LoadType load_type,
+                    const WebKit::WebURL& url,
+                    CORSMode cors_mode) OVERRIDE;
 
   // Playback controls.
   virtual void play();
@@ -188,7 +187,7 @@ class WebMediaPlayerImpl
   void OnPipelineError(media::PipelineStatus error);
   void OnPipelineBufferingState(
       media::Pipeline::BufferingState buffering_state);
-  void OnDemuxerOpened(scoped_ptr<WebKit::WebMediaSource> media_source);
+  void OnDemuxerOpened();
   void OnKeyAdded(const std::string& session_id);
   void OnKeyError(const std::string& session_id,
                   media::MediaKeys::KeyError error_code,
@@ -208,8 +207,8 @@ class WebMediaPlayerImpl
  private:
   // Called after |defer_load_cb_| has decided to allow the load. If
   // |defer_load_cb_| is null this is called immediately.
-  void DoLoad(const WebKit::WebURL& url,
-              WebKit::WebMediaSource* media_source,
+  void DoLoad(LoadType load_type,
+              const WebKit::WebURL& url,
               CORSMode cors_mode);
 
   // Called after asynchronous initialization of a data source completed.
@@ -219,9 +218,7 @@ class WebMediaPlayerImpl
   void NotifyDownloading(bool is_downloading);
 
   // Finishes starting the pipeline due to a call to load().
-  //
-  // A non-null |media_source| will construct a Media Source pipeline.
-  void StartPipeline(WebKit::WebMediaSource* media_source);
+  void StartPipeline();
 
   // Helpers that set the network/ready state and notifies the client if
   // they've changed.
@@ -282,6 +279,9 @@ class WebMediaPlayerImpl
   // The currently selected key system. Empty string means that no key system
   // has been selected.
   WebKit::WebString current_key_system_;
+
+  // The LoadType passed in the |load_type| parameter of the load() call.
+  LoadType load_type_;
 
   // Playback state.
   //

content/renderer/media/webmediaplayer_ms.cc
@@ -92,9 +92,15 @@ WebMediaPlayerMS::~WebMediaPlayerMS() {
     delegate_->PlayerGone(this);
 }
 
-void WebMediaPlayerMS::load(const WebKit::WebURL& url, CORSMode cors_mode) {
+void WebMediaPlayerMS::load(LoadType load_type,
+                            const WebKit::WebURL& url,
+                            CORSMode cors_mode) {
   DVLOG(1) << "WebMediaPlayerMS::load";
   DCHECK(thread_checker_.CalledOnValidThread());
+
+  // TODO(acolwell): Change this to DCHECK_EQ(load_type,
+  // LoadTypeMediaStream) once Blink-side changes land.
+  DCHECK_NE(load_type, LoadTypeMediaSource);
 
   GURL gurl(url);
 
@@ -127,12 +133,6 @@ void WebMediaPlayerMS::load(const WebKit::WebURL& url, CORSMode cors_mode) {
   } else {
     SetNetworkState(WebMediaPlayer::NetworkStateNetworkError);
   }
-}
-
-void WebMediaPlayerMS::load(const WebKit::WebURL& url,
-                            WebKit::WebMediaSource* media_source,
-                            CORSMode cors_mode) {
-  NOTIMPLEMENTED();
 }
 
 void WebMediaPlayerMS::play() {

commit 381e77b0803f5bbcf4498d85c68f7787837b3606

To fix a bug introduced in r218819.
BUG=272993

Review URL: https://chromiumcodereview.appspot.com/23021019

git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219441 0039d316-1c4b-4281-b951-d872f2087c98

content/renderer/media/android/webmediaplayer_android.cc
@@ -212,7 +212,7 @@ void WebMediaPlayerAndroid::load(LoadType load_type,
       break;
     case LoadTypeMediaStream:
 #if defined(GOOGLE_TV)
-      source_type_ = MediaPlayerAndroid::SOURCE_TYPE_MSE;
+      source_type_ = MediaPlayerAndroid::SOURCE_TYPE_STREAM;
 #else
       source_type_ = MediaPlayerAndroid::SOURCE_TYPE_URL;
 #endif