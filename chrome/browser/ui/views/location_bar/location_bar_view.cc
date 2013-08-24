7db9a41e4e7e155dc5fbba36c20964d2f6750678
pkasting@chromium.org <pkasting@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-24 03:09:49 +0000

    Split OmniboxView::Update() into OnTabChanged() and Update().
    This also moves some functionality from OnTabChanged() into
    OmniboxEditModel::RestoreState().
    
    This shouldn't result in any visible change.  This split is to facilitate an
    upcoming change for the Show URL feature that requires properly setting and
    resetting the toolbar model's "force show URL" state when we update and/or
    change tabs.  This will require the OmniboxEditModel to jump through more hoops
    in the midst of these actions, which is why some more code has moved into it, so
    it can do so with ease.
    
    BUG=248287
    TEST=none
    R=msw@chromium.org
    
    Review URL: https://codereview.chromium.org/22866020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219404 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/location_bar/location_bar_view.cc
+++ chrome/browser/ui/views/location_bar/location_bar_view.cc
@@ -495,7 +495,7 @@ void LocationBarView::SetAnimationOffset(int offset) {
   animation_offset_ = offset;
 }
 
-void LocationBarView::Update(const WebContents* tab_for_state_restoring) {
+void LocationBarView::Update(const WebContents* contents) {
   mic_search_view_->SetVisible(
       !GetToolbarModel()->input_in_progress() && browser_ &&
       browser_->search_model()->voice_search_supported());
@@ -518,7 +518,10 @@ void LocationBarView::Update(const WebContents* tab_for_state_restoring) {
   if (star_view_)
     star_view_->SetVisible(star_enabled);
 
-  location_entry_->Update(tab_for_state_restoring);
+  if (contents)
+    location_entry_->OnTabChanged(contents);
+  else
+    location_entry_->Update();
 
   OnChanged();
 }
