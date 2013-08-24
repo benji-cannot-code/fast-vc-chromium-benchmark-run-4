c3ac4523693f4d944b487788c33ff05c8e7430c2
jnd@chromium.org <jnd@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-24 07:31:21 +0000

    Retire the WebContextMenuData::isImageBlocked as we already switched to use WebContextMenuData::hasImageContents instead.
    See related changes in https://codereview.chromium.org/22938006 and https://codereview.chromium.org/23200006.
    
    
    BUG=272076
    Test=None
    
    Review URL: https://chromiumcodereview.appspot.com/23037018
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156673 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/public/web/WebContextMenuData.h
+++ third_party/WebKit/public/web/WebContextMenuData.h
@@ -73,9 +73,6 @@ struct WebContextMenuData {
     // The absolute URL of the image/video/audio that is in context.
     WebURL srcURL;
 
-    // Whether the image in context has been blocked.
-    bool isImageBlocked;
-
     // Whether the image in context is a null.
     bool hasImageContents;
 
@@ -175,7 +172,6 @@ struct WebContextMenuData {
 
     WebContextMenuData()
         : mediaType(MediaTypeNone)
-        , isImageBlocked(false)
         , hasImageContents(true)
         , mediaFlags(MediaNone)
         , isSpeechInputEnabled(false)
