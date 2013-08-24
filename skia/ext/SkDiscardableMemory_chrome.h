bfa73a24e80ca08a7e2df85756fc506025017ceb
ernstm@chromium.org <ernstm@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-24 00:03:49 +0000

    skia: Added chrome implementation of SkDiscardableMemory.
    The implementation resembles that of WebDiscardableMemory. The skia part of
    this patch is here: https://codereview.chromium.org/22956004
    
    R=reed@google.com, scroggo@google.com, hclam@chromium.org, nduca@chromium.org, jamesr@chromium.org
    BUG=229120
    
    Review URL: https://chromiumcodereview.appspot.com/23206002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219386 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ skia/ext/SkDiscardableMemory_chrome.h
@@ -0,0 +1,34 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef SKIA_EXT_SK_DISCARDABLE_MEMORY_CHROME_H_
+#define SKIA_EXT_SK_DISCARDABLE_MEMORY_CHROME_H_
+
+#include "base/memory/discardable_memory.h"
+#include "base/memory/scoped_ptr.h"
+#include "third_party/skia/src/core/SkDiscardableMemory.h"
+
+// This class implements the SkDiscardableMemory interface using
+// base::DiscardableMemory.
+class SK_API SkDiscardableMemoryChrome : public SkDiscardableMemory {
+public:
+  SkDiscardableMemoryChrome();
+  virtual ~SkDiscardableMemoryChrome();
+
+  // Initialize the SkDiscardableMemoryChrome object and lock the memory.
+  // Returns true on success. No memory is allocated if this call returns
+  // false. This call should only be called once.
+  bool InitializeAndLock(size_t bytes);
+
+  // Implementation of SkDiscardableMemory interface.
+  virtual bool lock() OVERRIDE;
+  virtual void* data() OVERRIDE;
+  virtual void unlock() OVERRIDE;
+
+private:
+  scoped_ptr<base::DiscardableMemory> discardable_;
+};
+
+#endif  // SKIA_EXT_SK_DISCARDABLE_MEMORY_CHROME_H_
--- skia/skia_chrome.gypi
+++ skia/skia_chrome.gypi
@@ -62,6 +62,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
     'ext/recursive_gaussian_convolution.cc',
     'ext/recursive_gaussian_convolution.h',
     'ext/refptr.h',
+    'ext/SkDiscardableMemory_chrome.h',
+    'ext/SkDiscardableMemory_chrome.cc',
     'ext/SkMemory_new_handler.cpp',
     'ext/skia_trace_shim.h',
     'ext/skia_utils_base.cc',
