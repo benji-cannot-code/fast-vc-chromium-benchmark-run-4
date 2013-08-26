adee0d65971eb75d8e579331499ccf2292ecaa14
tkent@chromium.org <tkent@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-26 05:36:23 +0000

    Refactoring: Moe core/html/InputTypeUI.* to core/html/forms/InputTypeView.*
    Rename the class in a following patch.
    We're going to move all of *InputType.* to core/html/forms/.
    
    BUG=274205
    R=keishi@chromium.org
    
    Review URL: https://codereview.chromium.org/23193004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156690 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/html/InputType.h
+++ third_party/WebKit/Source/core/html/InputType.h
@@ -35,8 +35,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #define InputType_h
 
 #include "core/html/HTMLTextFormControlElement.h"
-#include "core/html/InputTypeUI.h"
 #include "core/html/StepRange.h"
+#include "core/html/forms/InputTypeView.h"
 #include "core/page/UseCounter.h"
 
 namespace WebCore {
--- third_party/WebKit/Source/core/html/forms/InputTypeView.h
+++ third_party/WebKit/Source/core/html/forms/InputTypeView.h
@@ -31,8 +31,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef InputTypeUI_h
-#define InputTypeUI_h
+#ifndef InputTypeView_h
+#define InputTypeView_h
 
 #include "core/page/FocusDirection.h"
 #include "wtf/FastAllocBase.h"
