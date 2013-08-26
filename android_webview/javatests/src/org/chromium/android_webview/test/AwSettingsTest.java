758e43e8ceecd86ba47d2d2f96d90970598e4a8d
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-26 19:30:52 +0000

    [Android] Fix device file push location
    On ICS, push location for Android WebView was:
    
    file:///mnt/sdcard/chrome/test/data/device_files/database_access.html
    
    And now is fixed to:
    
    file:///mnt/sdcard/chrome/test/data/webview/database_access.html
    
    BUG=277077
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/22903046
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219585 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
+++ android_webview/javatests/src/org/chromium/android_webview/test/AwSettingsTest.java
@@ -1571,12 +1571,8 @@ public class AwSettingsTest extends AwTestBase {
         }
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"AndroidWebView", "Preferences"})
-     * crbug.com/277077
-     */
-    @DisabledTest
+    @SmallTest
+    @Feature({"AndroidWebView", "Preferences"})
     public void testDomStorageEnabledWithTwoViews() throws Throwable {
         ViewPair views = createViews();
         runPerViewSettingsTest(
@@ -1622,12 +1618,8 @@ public class AwSettingsTest extends AwTestBase {
         helper.ensureSettingHasInitialValue();
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"AndroidWebView", "Preferences"})
-     * crbug.com/277077
-     */
-    @DisabledTest
+    @SmallTest
+    @Feature({"AndroidWebView", "Preferences"})
     public void testUniversalAccessFromFilesWithTwoViews() throws Throwable {
         ViewPair views = createViews();
         runPerViewSettingsTest(
@@ -1639,12 +1631,8 @@ public class AwSettingsTest extends AwTestBase {
 
     // This test verifies that local image resources can be loaded from file:
     // URLs regardless of file access state.
-    /**
-     * @SmallTest
-     * @Feature({"AndroidWebView", "Preferences"})
-     * crbug.com/277077
-     */
-    @DisabledTest
+    @SmallTest
+    @Feature({"AndroidWebView", "Preferences"})
     public void testFileAccessFromFilesImage() throws Throwable {
         final String imageContainerUrl = UrlUtils.getTestFileUrl("webview/image_access.html");
         final String imageHeight = "16";
@@ -1660,12 +1648,8 @@ public class AwSettingsTest extends AwTestBase {
         assertEquals(imageHeight, getTitleOnUiThread(awContents));
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"AndroidWebView", "Preferences"})
-     * crbug.com/277077
-     */
-    @DisabledTest
+    @SmallTest
+    @Feature({"AndroidWebView", "Preferences"})
     public void testFileAccessFromFilesIframeWithTwoViews() throws Throwable {
         ViewPair views = createViews();
         runPerViewSettingsTest(
@@ -1675,12 +1659,8 @@ public class AwSettingsTest extends AwTestBase {
                 views.getContents1(), views.getClient1()));
     }
 
-    /**
-     * @SmallTest
-     * @Feature({"AndroidWebView", "Preferences"})
-     * crbug.com/277077
-     */
-    @DisabledTest
+    @SmallTest
+    @Feature({"AndroidWebView", "Preferences"})
     public void testFileAccessFromFilesXhrWithTwoViews() throws Throwable {
         ViewPair views = createViews();
         runPerViewSettingsTest(
