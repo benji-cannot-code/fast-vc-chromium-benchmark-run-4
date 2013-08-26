e29b981bda4fea31a0f416288f22164804af44de
junjianx@chromium.org <junjianx@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-26 02:20:56 +0000

    Add tree menu for dmprof visualizer. Use jqTree as third-party plugin
    BUG=259206
    NOTRY=True
    
    Review URL: https://chromiumcodereview.appspot.com/23075010
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219490 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/deep_memory_profiler/visualizer/index.js
@@ -0,0 +1,18 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+// Copyright 2013 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+$(function() {
+  // Read original data and plot.
+  $.getJSON('data/result.json', function(jsonData) {
+    // Create model.
+    var profiler = new Profiler(jsonData);
+    // Create views subscribing model events.
+    var graphView = new GraphView(profiler);
+    var menuView = new MenuView(profiler);
+
+    // initialize categories according to roots information.
+    profiler.initializeCategories();
+  });
+});
