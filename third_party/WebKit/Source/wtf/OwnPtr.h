f609d91eb7d7ead5ed871d259e9017faea5b2bac
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-26 22:20:36 +0000

    Remove use of <memory> include from Blink headers.
    It's aspecific, pulls in a lot of heavy include work, and does not seem to be
    necessary.
    
    Review URL: https://chromiumcodereview.appspot.com/23443002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156728 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/OwnPtr.h
+++ third_party/WebKit/Source/wtf/OwnPtr.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/OwnPtrCommon.h"
 #include "wtf/TypeTraits.h"
 #include <algorithm>
-#include <memory>
 
 namespace WTF {
 
--- third_party/WebKit/Source/wtf/text/TextCodec.h
+++ third_party/WebKit/Source/wtf/text/TextCodec.h
@@ -28,7 +28,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef TextCodec_h
 #define TextCodec_h
 
-#include <memory>
 #include "wtf/Forward.h"
 #include "wtf/Noncopyable.h"
 #include "wtf/PassOwnPtr.h"
