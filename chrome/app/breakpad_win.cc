e14163b758c113099854ea94b933ba0a143ed476
bungeman@chromium.org <bungeman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-26 21:33:28 +0000

    Fix overwriting other data in InitBrowserCrashDumpsRegKey.
    This was causing random crashes depending on the layout of the linked objects.
    On Windows in Debug, swprintf appears to always write 'fe' to all bytes which
    could be written to. The second parameter of swprintf is the number of wide
    characters, not the number of bytes.
    
    R=cpu@chromium.org, rogerm@chromium.org
    
    Review URL: https://codereview.chromium.org/23300006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219614 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/app/breakpad_win.cc
+++ chrome/app/breakpad_win.cc
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <algorithm>
 #include <vector>
 
+#include "base/basictypes.h"
 #include "base/base_switches.h"
 #include "base/command_line.h"
 #include "base/debug/crash_logging.h"
@@ -152,7 +153,7 @@ void InitBrowserCrashDumpsRegKey() {
   // before consuming the signal (overwriting the signal with an identical one).
   // For now, we're willing to live with that risk.
   int length = swprintf(g_browser_crash_dump_value,
-                        sizeof(g_browser_crash_dump_value),
+                        arraysize(g_browser_crash_dump_value),
                         kBrowserCrashDumpValueFormatStr,
                         ::GetCurrentProcessId(),
                         ::GetTickCount());
