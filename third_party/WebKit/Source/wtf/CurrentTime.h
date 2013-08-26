f4b203ca3a1977f9b319fc4878b04e821b27bf09
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-26 20:47:52 +0000

    Remove unneccessary time.h include from CurrentTime.h
    Review URL: https://chromiumcodereview.appspot.com/22912024
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156724 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/CurrentTime.h
+++ third_party/WebKit/Source/wtf/CurrentTime.h
@@ -33,8 +33,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #ifndef CurrentTime_h
 #define CurrentTime_h
 
-#include <time.h>
-
 #include "wtf/WTFExport.h"
 
 namespace WTF {
