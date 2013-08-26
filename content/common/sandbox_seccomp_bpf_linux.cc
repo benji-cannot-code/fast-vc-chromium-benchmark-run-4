160f156160d1aed7fafb05494ba0f6da0e2e4466
mostynb@opera.com <mostynb@opera.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-26 22:54:56 +0000

    remove unused headers from sandbox_seccomp_bpf_linux.cc
    sandbox_seccomp_bpf_linux.cc includes linux/audit.h and linux/filter.h but does not seem to use any of the macros or types defined within.
    
    BUG=279413
    
    Review URL: https://chromiumcodereview.appspot.com/23226004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219630 0039d316-1c4b-4281-b951-d872f2087c98

--- content/common/sandbox_seccomp_bpf_linux.cc
+++ content/common/sandbox_seccomp_bpf_linux.cc
@@ -7,8 +7,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <dlfcn.h>
 #include <errno.h>
 #include <fcntl.h>
-#include <linux/audit.h>
-#include <linux/filter.h>
 #include <linux/net.h>
 #include <signal.h>
 #include <string.h>
