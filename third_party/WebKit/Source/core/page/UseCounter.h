d42988cf05874fdfbef39623551e01ef65f97fef
alexis.menard@intel.com <alexis.menard@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-27 17:00:14 +0000

    Implement dispatching of unprefixed animation events.
    This commit implements the dispatching to event listeners for
    animations DOM events. It uses the same mechanism as CSS Transitions
    which is the following :
    - If we have only listeners on prefixed events we dispatch prefixed events.
    - If we have only listeners on unprefixed events we dispatch unprefixed events.
    - If we have listeners on both unprefixed and prefixed events we dispatch
    unprefixed events only (they take precedence).
    
    This commit also monitors the usage of each CSS Animations events we dispatch.
    While it's a bit verbose I think it's important to have stats for each of them
    so we can make a decision in some years to remove the prefixed code path.
    
    Review URL: https://chromiumcodereview.appspot.com/23453002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156784 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/page/UseCounter.h
+++ third_party/WebKit/Source/core/page/UseCounter.h
@@ -188,6 +188,15 @@ public:
         NavigatorVendorSub,
         FileError,
         DocumentCharset, // Documented as IE extensions, from KHTML days.
+        PrefixedAnimationEndEvent,
+        UnprefixedAnimationEndEvent,
+        PrefixedAndUnprefixedAnimationEndEvent,
+        PrefixedAnimationStartEvent,
+        UnprefixedAnimationStartEvent,
+        PrefixedAndUnprefixedAnimationStartEvent,
+        PrefixedAnimationIterationEvent,
+        UnprefixedAnimationIterationEvent,
+        PrefixedAndUnprefixedAnimationIterationEvent,
         // Add new features immediately above this line. Don't change assigned
         // numbers of each items, and don't reuse unused slots.
         NumberOfFeatures, // This enum value must be last.
