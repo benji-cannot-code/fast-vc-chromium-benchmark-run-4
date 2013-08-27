a71a34feacf55b5929d1165bbf27dc976a128c72
jackhou@chromium.org <jackhou@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 04:16:20 +0000

    Quit shim when all windows are closed.
    BUG=263222
    TEST=Start an app with shims enabled.
    Close all windows of that app, the shim should close.
    TEST=Start an app with shims enabled.
    Uninstall the app, the shim should close.
    
    Review URL: https://chromiumcodereview.appspot.com/22802009
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219689 0039d316-1c4b-4281-b951-d872f2087c98

--- apps/app_shim/extension_app_shim_handler_mac.cc
+++ apps/app_shim/extension_app_shim_handler_mac.cc
@@ -143,10 +143,6 @@ ExtensionAppShimHandler::ExtensionAppShimHandler()
                  content::NotificationService::AllBrowserContextsAndSources());
   registrar_.Add(this, chrome::NOTIFICATION_PROFILE_DESTROYED,
                  content::NotificationService::AllBrowserContextsAndSources());
-  registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_UNINSTALLED,
-                 content::NotificationService::AllBrowserContextsAndSources());
-  registrar_.Add(this, chrome::NOTIFICATION_EXTENSION_PROCESS_TERMINATED,
-                 content::NotificationService::AllBrowserContextsAndSources());
   registrar_.Add(this, chrome::NOTIFICATION_BROWSER_OPENED,
                  content::NotificationService::AllBrowserContextsAndSources());
 }
@@ -313,12 +309,8 @@ void ExtensionAppShimHandler::OnShimQuit(Host* host) {
        it != windows.end(); ++it) {
     (*it)->GetBaseWindow()->Close();
   }
-
-  DCHECK_NE(0u, hosts_.count(make_pair(profile, app_id)));
-  host->OnAppClosed();
-
-  if (!browser_opened_ever_ && hosts_.empty())
-    delegate_->MaybeTerminate();
+  // Once the last window closes, flow will end up in OnAppDeactivated via
+  // AppLifetimeMonitor.
 }
 
 void ExtensionAppShimHandler::set_delegate(Delegate* delegate) {
@@ -358,20 +350,6 @@ void ExtensionAppShimHandler::Observe(
       }
       break;
     }
-    case chrome::NOTIFICATION_EXTENSION_PROCESS_TERMINATED:
-    case chrome::NOTIFICATION_EXTENSION_UNINSTALLED: {
-      std::string app_id;
-      if (type == chrome::NOTIFICATION_EXTENSION_PROCESS_TERMINATED) {
-        app_id = content::Details<extensions::ExtensionHost>(details).ptr()
-            ->extension_id();
-      } else {
-        app_id = content::Details<extensions::Extension>(details).ptr()->id();
-      }
-      Host* host = FindHost(profile, app_id);
-      if (host)
-        host->OnAppClosed();
-      break;
-    }
     default: {
       NOTREACHED();  // Unexpected notification.
       break;
@@ -400,7 +378,14 @@ void ExtensionAppShimHandler::OnAppActivated(Profile* profile,
 }
 
 void ExtensionAppShimHandler::OnAppDeactivated(Profile* profile,
-                                               const std::string& app_id) {}
+                                               const std::string& app_id) {
+  Host* host = FindHost(profile, app_id);
+  if (host)
+    host->OnAppClosed();
+
+  if (!browser_opened_ever_ && hosts_.empty())
+    delegate_->MaybeTerminate();
+}
 
 void ExtensionAppShimHandler::OnAppStop(Profile* profile,
                                         const std::string& app_id) {}
