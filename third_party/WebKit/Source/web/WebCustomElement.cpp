5ab929eaaf4559787076c555344eec03945fe91b
dominicc@chromium.org <dominicc@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-27 00:25:56 +0000

    Clean up WebCustomElement and other FIXMEs post Chromium r218769
    BUG=273126
    TEST=fast/dom/custom, browser_tests --gtest_filter=WebViewTest.*
    
    Review URL: https://chromiumcodereview.appspot.com/23129029
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156734 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/web/WebCustomElement.cpp
+++ third_party/WebKit/Source/web/WebCustomElement.cpp
@@ -40,13 +40,6 @@ using namespace WebCore;
 
 namespace WebKit {
 
-// FIXME: Remove this when all embedders switch to
-// addEmbedderCustomElementName.
-void WebCustomElement::allowTagName(const WebString& localName)
-{
-    addEmbedderCustomElementName(localName);
-}
-
 void WebCustomElement::addEmbedderCustomElementName(const WebString& name)
 {
     ASSERT(RuntimeEnabledFeatures::embedderCustomElementsEnabled());
