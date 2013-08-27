de4dd32ffa1a182706d2bdb0ab925d99d8df5fa1
sergeyu@chromium.org <sergeyu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 06:45:13 +0000

    Make XmppSignalStrategy accept ResolvingClientSocketFactory from outside.
    This is necessary to allow passing Pepper-based 
    ResolvingClientSocketFactory when using XMPP in the client plugin.
    
    BUG=274652
    
    Review URL: https://chromiumcodereview.appspot.com/23443004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219719 0039d316-1c4b-4281-b951-d872f2087c98

--- remoting/host/remoting_me2me_host.cc
+++ remoting/host/remoting_me2me_host.cc
@@ -28,6 +28,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ipc/ipc_channel.h"
 #include "ipc/ipc_channel_proxy.h"
 #include "ipc/ipc_listener.h"
+#include "jingle/glue/xmpp_client_socket_factory.h"
 #include "media/base/media.h"
 #include "net/base/network_change_notifier.h"
 #include "net/socket/client_socket_factory.h"
@@ -966,9 +967,12 @@ void HostProcess::StartHost() {
          state_ == HOST_STOPPED) << state_;
   state_ = HOST_STARTED;
 
+  scoped_ptr<jingle_glue::ResolvingClientSocketFactory> socket_factory(
+      new jingle_glue::XmppClientSocketFactory(
+          net::ClientSocketFactory::GetDefaultFactory(), net::SSLConfig(),
+          context_->url_request_context_getter(), false));
   signal_strategy_.reset(
-      new XmppSignalStrategy(net::ClientSocketFactory::GetDefaultFactory(),
-                             context_->url_request_context_getter(),
+      new XmppSignalStrategy(socket_factory.Pass(),
                              xmpp_server_config_));
 
   scoped_ptr<DnsBlackholeChecker> dns_blackhole_checker(
--- remoting/jingle_glue/xmpp_signal_strategy.h
+++ remoting/jingle_glue/xmpp_signal_strategy.h
@@ -22,10 +22,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "third_party/libjingle/source/talk/base/sigslot.h"
 #include "third_party/libjingle/source/talk/xmpp/xmppclient.h"
 
-namespace net {
-class ClientSocketFactory;
-class URLRequestContextGetter;
-}  // namespace net
+namespace jingle_glue {
+class ResolvingClientSocketFactory;
+}  // namespace jingle_glue
 
 namespace talk_base {
 class TaskRunner;
@@ -55,8 +54,7 @@ class XmppSignalStrategy : public base::NonThreadSafe,
   };
 
   XmppSignalStrategy(
-      net::ClientSocketFactory* socket_factory,
-      scoped_refptr<net::URLRequestContextGetter> request_context_getter,
+      scoped_ptr<jingle_glue::ResolvingClientSocketFactory> socket_factory,
       const XmppServerConfig& xmpp_server_config);
   virtual ~XmppSignalStrategy();
 
@@ -94,8 +92,7 @@ class XmppSignalStrategy : public base::NonThreadSafe,
 
   void SendKeepAlive();
 
-  net::ClientSocketFactory* socket_factory_;
-  scoped_refptr<net::URLRequestContextGetter> request_context_getter_;
+  scoped_ptr<jingle_glue::ResolvingClientSocketFactory> socket_factory_;
   std::string resource_name_;
   scoped_ptr<talk_base::TaskRunner> task_runner_;
   buzz::XmppClient* xmpp_client_;
