49ca08123009b29fca60784df79d93c486e97f08
waffles@chromium.org <waffles@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 01:07:58 +0000

    Refactor component installers.
    This change refactors the pnacl and widevine cdm component installers so that
    shared logic can be placed into a base class and installer-specific logic can
    be implemented by overriding private virtual methods.
    
    See Issue 23245016 for how this applies to Widevine.
    See Issue 22816025 for how this applies to PNaCl.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/21331004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219654 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/component_updater/component_updater_service.h
+++ chrome/browser/component_updater/component_updater_service.h
@@ -47,7 +47,6 @@ class ComponentInstaller {
   virtual bool GetInstalledFile(const std::string& file,
                                 base::FilePath* installed_file) = 0;
 
- protected:
   virtual ~ComponentInstaller() {}
 };
 
