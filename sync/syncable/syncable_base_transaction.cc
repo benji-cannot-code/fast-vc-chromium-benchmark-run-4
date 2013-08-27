4026d9877e492e4521928875ff0df735a46664d9
rlarocque@chromium.org <rlarocque@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 03:17:37 +0000

    sync: Expose BuildCommitItem helper function
    Refactor the part of BuildCommitCommand responsible for serializing a
    single syncable::Entry into a commit message protobuf into a separate
    function.  This will be useful when we try to refactor the sync engine
    to be more model-type aware.
    
    BUG=278484
    
    Review URL: https://chromiumcodereview.appspot.com/22859066
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219670 0039d316-1c4b-4281-b951-d872f2087c98

--- sync/syncable/syncable_base_transaction.cc
+++ sync/syncable/syncable_base_transaction.cc
@@ -11,12 +11,13 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace syncer {
 namespace syncable {
 
-Directory* BaseTransaction::directory() const {
-  return directory_;
+// static
+Id BaseTransaction::root_id() {
+  return Id();
 }
 
-Id BaseTransaction::root_id() const {
-  return Id();
+Directory* BaseTransaction::directory() const {
+  return directory_;
 }
 
 void BaseTransaction::Lock() {
