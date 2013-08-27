72a6599e91308d7692c862452c7302f0c981b447
hajimehoshi@chromium.org <hajimehoshi@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 10:42:39 +0000

    Reverse the label of 'Don't translate' checkbox to 'Offer to translate'
    The positive sentence 'offer to translate' is better in terms of UX.  This CL doesn't change the function but UX.
    
    BUG=243235
    
    Review URL: https://chromiumcodereview.appspot.com/22978017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219765 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/options/language_options.js
+++ chrome/browser/resources/options/language_options.js
@@ -182,9 +182,9 @@ cr.define('options', function() {
         this.initializeLanguageCodeToInputMethodIdsMap_();
       }
 
-      var checkbox = $('dont-translate-in-this-language');
+      var checkbox = $('offer-to-translate-in-this-language');
       checkbox.addEventListener('click',
-          this.handleDontTranslateCheckboxClick_.bind(this));
+          this.handleOfferToTranslateCheckboxClick_.bind(this));
 
       Preferences.getInstance().addEventListener(
           TRANSLATE_BLOCKED_LANGUAGES_PREF,
@@ -439,7 +439,7 @@ cr.define('options', function() {
         }
       }
 
-      this.updateDontTranslateCheckbox_(languageCode);
+      this.updateOfferToTranslateCheckbox_(languageCode);
 
       if (cr.isWindows || cr.isChromeOS)
         this.updateUiLanguageButton_(languageCode);
@@ -678,8 +678,8 @@ cr.define('options', function() {
      * @param {string} languageCode Language code (ex. "fr").
      * @private
      */
-    updateDontTranslateCheckbox_: function(languageCode) {
-      var div = $('language-options-dont-translate');
+    updateOfferToTranslateCheckbox_: function(languageCode) {
+      var div = $('language-options-offer-to-translate');
 
       if (!loadTimeData.getBoolean('enableTranslateSettings')) {
         div.hidden = true;
@@ -696,9 +696,9 @@ cr.define('options', function() {
         return;
       }
 
-      var dontTranslate = div.querySelector('div');
+      var offerToTranslate = div.querySelector('div');
       var cannotTranslate = $('cannot-translate-in-this-language');
-      var nodes = [dontTranslate, cannotTranslate];
+      var nodes = [offerToTranslate, cannotTranslate];
 
       var convertedLangCode = this.convertLangCodeForTranslation_(languageCode);
       if (this.translateSupportedLanguages_.indexOf(convertedLangCode) != -1) {
@@ -708,23 +708,23 @@ cr.define('options', function() {
         return;
       }
 
-      var checkbox = $('dont-translate-in-this-language');
+      var checkbox = $('offer-to-translate-in-this-language');
 
       // If the language corresponds to the default target language (in most
-      // cases, the user's locale language), "Don't Translate" checkbox should
-      // be always checked.
+      // cases, the user's locale language), "Offer to translate" checkbox
+      // should be always unchecked.
       var defaultTargetLanguage =
           loadTimeData.getString('defaultTargetLanguage');
       if (convertedLangCode == defaultTargetLanguage) {
         checkbox.disabled = true;
-        checkbox.checked = true;
+        checkbox.checked = false;
         return;
       }
 
       checkbox.disabled = false;
 
       var blockedLanguages = this.translateBlockedLanguages_;
-      var checked = blockedLanguages.indexOf(convertedLangCode) != -1;
+      var checked = blockedLanguages.indexOf(convertedLangCode) == -1;
       checkbox.checked = checked;
     },
 
@@ -823,11 +823,11 @@ cr.define('options', function() {
     },
 
     /**
-     * Handles don't-translate checkbox's click event.
+     * Handles offer-to-translate checkbox's click event.
      * @param {Event} e Click event.
      * @private
      */
-    handleDontTranslateCheckboxClick_: function(e) {
+    handleOfferToTranslateCheckboxClick_: function(e) {
       var checkbox = e.target;
       var checked = checkbox.checked;
 
@@ -835,9 +835,9 @@ cr.define('options', function() {
       var selectedLanguageCode = languageOptionsList.getSelectedLanguageCode();
 
       if (checked)
-        this.addBlockedLanguage_(selectedLanguageCode);
-      else
         this.removeBlockedLanguage_(selectedLanguageCode);
+      else
+        this.addBlockedLanguage_(selectedLanguageCode);
     },
 
     /**
@@ -916,7 +916,7 @@ cr.define('options', function() {
       var languageOptionsList = $('language-options-list');
       var selectedLanguageCode = languageOptionsList.getSelectedLanguageCode();
       this.translateBlockedLanguages_ = e.value.value;
-      this.updateDontTranslateCheckbox_(selectedLanguageCode);
+      this.updateOfferToTranslateCheckbox_(selectedLanguageCode);
     },
 
     /**
