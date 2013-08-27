6a78c41b1e653ef9d932c3015a2c0cee43a646ae
steveblock@chromium.org <steveblock@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-27 07:13:30 +0000

    Web Animations: Fix elpasedTime in animation events
    This addresses the FIXME in CSSAnimations::EventDelegate::onEventCondition()
    to pass the TimedItem to the event callback.
    
    Also tighten up a test such that it would now detect the FIXME.
    
    BUG=240653
    R=dstockwell
    
    Review URL: https://chromiumcodereview.appspot.com/23039013
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156768 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/animation/TimedItem.h
+++ third_party/WebKit/Source/core/animation/TimedItem.h
@@ -39,6 +39,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace WebCore {
 
 class Player;
+class TimedItem;
 
 static inline bool isNull(double value)
 {
@@ -64,7 +65,7 @@ public:
     class EventDelegate {
     public:
         virtual ~EventDelegate() { };
-        virtual void onEventCondition(bool isFirstSample, Phase previousPhase, Phase currentPhase, double previousIteration, double currentIteration) = 0;
+        virtual void onEventCondition(const TimedItem*, bool isFirstSample, Phase previousPhase, double previousIteration) = 0;
     };
 
     virtual ~TimedItem() { }
@@ -79,7 +80,9 @@ public:
     double currentIteration() const { return ensureCalculated().currentIteration; }
     double activeDuration() const { return ensureCalculated().activeDuration; }
     double timeFraction() const { return ensureCalculated().timeFraction; }
-    Player* player() const { return m_player; }
+    const Player* player() const { return m_player; }
+
+    const Timing& specified() const { return m_specified; }
 
 protected:
     TimedItem(const Timing&, PassOwnPtr<EventDelegate> = nullptr);
