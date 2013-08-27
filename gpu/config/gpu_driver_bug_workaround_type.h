0aaccb86b0aef714b4b684fcf89b567e6b662483
bajones@chromium.org <bajones@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-27 00:00:34 +0000

    Added gpu driver workaround for disabling ANGLE_instanced_arrays
    Needs to be disabled on Windows until the ANGLE corruption bug is corrected
    
    BUG=277817
    R=apatrick@chromium.org, kbr@chromium.org, piman@chromium.org, zmo@chromium.org
    
    Review URL: https://codereview.chromium.org/22909043
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219643 0039d316-1c4b-4281-b951-d872f2087c98

--- gpu/config/gpu_driver_bug_workaround_type.h
+++ gpu/config/gpu_driver_bug_workaround_type.h
@@ -63,6 +63,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
          disable_d3d11)                                      \
   GPU_OP(SWIZZLE_RGBA_FOR_ASYNC_READPIXELS,                  \
          swizzle_rgba_for_async_readpixels)                  \
+  GPU_OP(DISABLE_ANGLE_INSTANCED_ARRAYS,                     \
+         disable_angle_instanced_arrays)                     \
 
 namespace gpu {
 
