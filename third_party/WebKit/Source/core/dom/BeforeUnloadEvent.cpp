a6d44bcd396e77b9c00a695146a8bbdd3da1e511
jianli@chromium.org <jianli@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-28 00:28:32 +0000

    Revert 156774 "Add support for BeforeUnloadEvent"
    Speculative revert for Chromium OS test failures on browser_tests:
      KeyboardOperations/FileManagerBrowserTest.Test/3
      KeyboardOperations/FileManagerBrowserTest.Test/4
      KeyboardOperations/FileManagerBrowserTest.Test/5
    
    http://build.chromium.org/p/chromium.chromiumos/buildstatus?builder=Linux%20ChromiumOS%20Tests%20%282%29&number=17612
    http://build.chromium.org/p/chromium.memory/buildstatus?builder=Linux%20Chromium%20OS%20ASAN%20Tests%20%281%29&number=11554
    http://build.chromium.org/p/chromium.memory/buildstatus?builder=Linux%20Chromium%20OS%20ASAN%20Tests%20%282%29&number=11976
    http://build.chromium.org/p/chromium.memory/buildstatus?builder=Linux%20Chromium%20OS%20ASAN%20Tests%20%283%29&number=9424
    
    > Add support for BeforeUnloadEvent
    > 
    > Add support for BeforeUnloadEvent as per the specification:
    > http://www.whatwg.org/specs/web-apps/current-work/#beforeunloadevent
    > 
    > BeforeUnloadEvent has a returnValue attribute. Setting returnValue to a non-empty
    > string in an event handler causes the user agent should ask the user to confirm
    > that they wish to unload the document. This is equivalent to returning a non-empty
    > string in the EventHandler:
    > http://www.whatwg.org/specs/web-apps/current-work/#onbeforeunloadeventhandler
    > 
    > BeforeUnloadEvent and returnValue are already supported by IE and Firefox. Previously,
    > Blink was passing a base Event type to the beforeunload event handlers instead of
    > a BeforeUnloadEvent.
    > 
    > Note that this patch also removes support for Event.returnValue. This used to be
    > an IE extension but this is no longer supported by IE (nor Firefox) so the
    > compatibility risk should be low. Applications use preventDefault() nowadays to
    > do this and only fallback to Event.returnValue to support older versions of IE
    > (IE < 9).
    > 
    > BUG=277851
    > 
    > Committed: https://src.chromium.org/viewvc/blink?view=rev&revision=156654
    > 
    > Review URL: https://chromiumcodereview.appspot.com/22893051
    
    TBR=ch.dumez@sisa.samsung.com
    
    Review URL: https://codereview.chromium.org/23464008
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156802 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/BeforeUnloadEvent.cpp
+++ third_party/WebKit/Source/core/dom/BeforeUnloadEvent.cpp
@@ -1,10 +1,9 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
-/*
+/**
  * Copyright (C) 2001 Peter Kelly (pmk@post.com)
  * Copyright (C) 2001 Tobias Anton (anton@stud.fbi.fh-darmstadt.de)
  * Copyright (C) 2006 Samuel Weinig (sam.weinig@gmail.com)
  * Copyright (C) 2003, 2005, 2006 Apple Computer, Inc.
- * Copyright (C) 2013 Samsung Electronics.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
@@ -32,16 +31,20 @@ namespace WebCore {
 BeforeUnloadEvent::BeforeUnloadEvent()
     : Event(eventNames().beforeunloadEvent, false, true)
 {
-    ScriptWrappable::init(this);
 }
 
 BeforeUnloadEvent::~BeforeUnloadEvent()
 {
 }
 
-bool BeforeUnloadEvent::isBeforeUnloadEvent() const
+bool BeforeUnloadEvent::storesResultAsString() const
 {
     return true;
+}
+
+void BeforeUnloadEvent::storeResult(const String& s)
+{
+    m_result = s;
 }
 
 } // namespace WebCore
