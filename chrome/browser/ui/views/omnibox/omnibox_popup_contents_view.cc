e44cefbff0e74b9e57a5b8580dfca8466f83a4b5
dcblack@chromium.org <dcblack@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 13:24:37 +0000

    Calculate layout correctly when rendering a new set of omnibox suggestions.
    BUG=276715
    
    Review URL: https://chromiumcodereview.appspot.com/23496012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220007 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
+++ chrome/browser/ui/views/omnibox/omnibox_popup_contents_view.cc
@@ -248,7 +248,7 @@ void OmniboxPopupContentsView::UpdatePopupAppearance() {
     popup_->SetBounds(GetPopupBounds());
   }
 
-  SchedulePaint();
+  Layout();
 }
 
 gfx::Rect OmniboxPopupContentsView::GetTargetBounds() {
