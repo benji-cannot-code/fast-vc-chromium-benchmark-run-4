80e95f1df8e392147e922e502dab1cf245e809c3
qsr@chromium.org <qsr@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 20:13:36 +0000

    Remove unnecessary public typedef from AsyncSocketIoHandler
    TBR=darin@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/23437011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220077 0039d316-1c4b-4281-b951-d872f2087c98

--- base/async_socket_io_handler.h
+++ base/async_socket_io_handler.h
@@ -12,13 +12,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-// The message loop callback interface is different based on platforms.
-#if defined(OS_WIN)
-typedef base::MessageLoopForIO::IOHandler MessageLoopIOHandler;
-#elif defined(OS_POSIX)
-typedef base::MessageLoopForIO::Watcher MessageLoopIOHandler;
-#endif
-
 // Extends the CancelableSyncSocket class to allow reading from a socket
 // asynchronously on a TYPE_IO message loop thread.  This makes it easy to share
 // a thread that uses a message loop (e.g. for IPC and other things) and not
@@ -54,7 +47,12 @@ typedef base::MessageLoopForIO::Watcher MessageLoopIOHandler;
 //
 class BASE_EXPORT AsyncSocketIoHandler
     : public NON_EXPORTED_BASE(base::NonThreadSafe),
-      public NON_EXPORTED_BASE(MessageLoopIOHandler) {
+// The message loop callback interface is different based on platforms.
+#if defined(OS_WIN)
+      public NON_EXPORTED_BASE(base::MessageLoopForIO::IOHandler) {
+#else
+      public NON_EXPORTED_BASE(base::MessageLoopForIO::Watcher) {
+#endif
  public:
   AsyncSocketIoHandler();
   virtual ~AsyncSocketIoHandler();
