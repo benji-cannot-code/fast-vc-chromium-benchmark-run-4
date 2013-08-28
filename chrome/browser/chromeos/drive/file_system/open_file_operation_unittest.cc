23a91a848d8a97c4469927e4859c214c25973f18
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 10:44:08 +0000

    drive: Rename |resource_id| argument of OnCacheFileUploadNeededByOperation to |local_id|
    BUG=260514
    R=kinaba@chromium.org
    
    Review URL: https://codereview.chromium.org/23609007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219989 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system/open_file_operation_unittest.cc
+++ chrome/browser/chromeos/drive/file_system/open_file_operation_unittest.cc
@@ -60,7 +60,7 @@ TEST_F(OpenFileOperationTest, OpenExistingFile) {
   close_callback.Run();
   EXPECT_EQ(
       1U,
-      observer()->upload_needed_resource_ids().count(src_entry.resource_id()));
+      observer()->upload_needed_local_ids().count(src_entry.resource_id()));
 }
 
 TEST_F(OpenFileOperationTest, OpenNonExistingFile) {
@@ -127,7 +127,7 @@ TEST_F(OpenFileOperationTest, CreateNonExistingFile) {
   close_callback.Run();
   // Here we don't know about the resource id, so just make sure
   // OnCacheFileUploadNeededByOperation is called actually.
-  EXPECT_EQ(1U, observer()->upload_needed_resource_ids().size());
+  EXPECT_EQ(1U, observer()->upload_needed_local_ids().size());
 }
 
 TEST_F(OpenFileOperationTest, OpenOrCreateExistingFile) {
@@ -158,7 +158,7 @@ TEST_F(OpenFileOperationTest, OpenOrCreateExistingFile) {
   close_callback.Run();
   EXPECT_EQ(
       1U,
-      observer()->upload_needed_resource_ids().count(src_entry.resource_id()));
+      observer()->upload_needed_local_ids().count(src_entry.resource_id()));
 }
 
 TEST_F(OpenFileOperationTest, OpenOrCreateNonExistingFile) {
@@ -186,7 +186,7 @@ TEST_F(OpenFileOperationTest, OpenOrCreateNonExistingFile) {
   close_callback.Run();
   // Here we don't know about the resource id, so just make sure
   // OnCacheFileUploadNeededByOperation is called actually.
-  EXPECT_EQ(1U, observer()->upload_needed_resource_ids().size());
+  EXPECT_EQ(1U, observer()->upload_needed_local_ids().size());
 }
 
 TEST_F(OpenFileOperationTest, OpenFileTwice) {
@@ -236,14 +236,14 @@ TEST_F(OpenFileOperationTest, OpenFileTwice) {
 
   // There still remains a client opening the file, so it shouldn't be
   // uploaded yet.
-  EXPECT_TRUE(observer()->upload_needed_resource_ids().empty());
+  EXPECT_TRUE(observer()->upload_needed_local_ids().empty());
 
   close_callback2.Run();
 
   // Here, all the clients close the file, so it should be uploaded then.
   EXPECT_EQ(
       1U,
-      observer()->upload_needed_resource_ids().count(src_entry.resource_id()));
+      observer()->upload_needed_local_ids().count(src_entry.resource_id()));
 }
 
 }  // namespace file_system
