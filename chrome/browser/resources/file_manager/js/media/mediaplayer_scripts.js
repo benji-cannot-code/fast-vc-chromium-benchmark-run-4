d0ffb8152d9ddfd7bc7b3fae728b993e17cc5f69
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 13:50:08 +0000

    Fix path_util.js include dependency.
    VolumeManager starts to use RootType in path_util.js, but some dependency was
    lacked. Fix them.
    
    BUG=280403
    TEST=Ran browser_tests --gtest_filter="*FileSystemExtensionApiTest*:*FileManagerBrowserTest*:*FileBrowserPrivateApiTest*" and tested manually.
    
    Review URL: https://chromiumcodereview.appspot.com/23672003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220013 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
+++ chrome/browser/resources/file_manager/js/media/mediaplayer_scripts.js
@@ -18,6 +18,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 //<include src="../async_util.js"/>
 //<include src="../util.js"/>
+//<include src="../path_util.js"/>
 //<include src="../file_type.js"/>
 //<include src="../volume_manager.js">
 //<include src="../metadata/metadata_cache.js"/>
--- chrome/browser/resources/file_manager/js/photo/photo_import_scripts.js
+++ chrome/browser/resources/file_manager/js/photo/photo_import_scripts.js
@@ -34,10 +34,10 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 //<include src="../async_util.js"/>
 //<include src="../util.js"/>
+//<include src="../path_util.js"/>
 //<include src="../file_type.js"/>
 //<include src="../directory_contents.js"/>
 //<include src="../volume_manager.js"/>
-//<include src="../path_util.js"/>
 //<include src="../file_operation_manager_wrapper.js"/>
 //<include src="../metadata/metadata_cache.js"/>
 //<include src="../metrics.js"/>
