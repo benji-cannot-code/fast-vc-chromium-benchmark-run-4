0faef3e14f77f50258f91de75fcc2fa19ae15f6b
boliu@chromium.org <boliu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 15:17:30 +0000

    [Android WebView] Disable disambiguation popups
    This feature is not universally useful in webview. So disable
    it for now.
    
    Implemented by adding TAP_MULTIPLE_TARGETS_STRATEGY_NONE and
    setting this in AwContents constructor so there is no need to
    explicitly sync to renderer.
    
    BUG=280288, 280301
    
    Review URL: https://chromiumcodereview.appspot.com/23453011
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220032 0039d316-1c4b-4281-b951-d872f2087c98

--- android_webview/native/aw_contents.cc
+++ android_webview/native/aw_contents.cc
@@ -44,6 +44,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/render_process_host.h"
 #include "content/public/browser/render_view_host.h"
 #include "content/public/browser/web_contents.h"
+#include "content/public/common/renderer_preferences.h"
 #include "content/public/common/ssl_status.h"
 #include "jni/AwContents_jni.h"
 #include "net/cert/x509_certificate.h"
@@ -146,6 +147,9 @@ AwContents::AwContents(scoped_ptr<WebContents> web_contents)
       AwAutofillManagerDelegate::FromWebContents(web_contents_.get());
   if (autofill_manager_delegate)
     InitAutofillIfNecessary(autofill_manager_delegate->GetSaveFormData());
+
+  web_contents_->GetMutableRendererPrefs()->tap_multiple_targets_strategy =
+      content::TAP_MULTIPLE_TARGETS_STRATEGY_NONE;
 }
 
 void AwContents::SetJavaPeers(JNIEnv* env,
