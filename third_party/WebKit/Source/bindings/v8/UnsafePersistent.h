96676a5e213de42399d9aa9b12ad706a36256fd4
marja@chromium.org <marja@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-28 11:19:46 +0000

    Explicitly inline hot return value setter funcs in UnsafePersistent.
    It doesn't make a perf difference on Linux, but, as we use explicit inline
    directives on hot paths, it makes sense to explicitly inline these too.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23685002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156840 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/bindings/v8/UnsafePersistent.h
+++ third_party/WebKit/Source/bindings/v8/UnsafePersistent.h
@@ -66,7 +66,7 @@ public:
     }
 
     template<typename V8T, typename U>
-    bool setReturnValueWithSecurityCheck(v8::ReturnValue<v8::Value> returnValue, U* object)
+    inline bool setReturnValueWithSecurityCheck(v8::ReturnValue<v8::Value> returnValue, U* object)
     {
         v8::Handle<v8::Object> result = deprecatedHandle();
         // Security: always guard against malicious tampering.
@@ -75,7 +75,7 @@ public:
         return !result.IsEmpty();
     }
 
-    bool setReturnValue(v8::ReturnValue<v8::Value> returnValue)
+    inline bool setReturnValue(v8::ReturnValue<v8::Value> returnValue)
     {
         returnValue.Set(deprecatedHandle());
         return !isEmpty();
