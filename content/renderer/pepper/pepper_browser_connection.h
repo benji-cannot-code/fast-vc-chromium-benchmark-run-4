13a28fffac5b82da33052246e88f59cfa02c0573
teravest@chromium.org <teravest@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 04:20:37 +0000

    Pepper: Move FileRef to the "new" resource proxy.
    This change moves the FileRef implementation from the previous one in the "old"
    resource model (ppb_file_ref_impl.cc) to the "new" resource model
    (pepper_file_ref_host.cc), and from the renderer to the browser.
    
    As many as possible of the supporting changes were split off to other changes
    to minimize the size of this change. Unfortunately, a lot of changes for
    URLLoader had to be rolled into this change.
    
    The data structures for CreateInfo have changed, and all users of FileRef have
    to be moved over, which is what causes this change to be so large.
    
    TBR=dmichael@chromium.org, jschuh@chromium.org, yzshen@chromium.org
    BUG=225441
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=216744
    
    Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=218305
    
    Review URL: https://chromiumcodereview.appspot.com/21966004
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@219911 0039d316-1c4b-4281-b951-d872f2087c98

--- content/renderer/pepper/pepper_browser_connection.h
+++ content/renderer/pepper/pepper_browser_connection.h
@@ -17,6 +17,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "ppapi/c/pp_file_info.h"
 #include "ppapi/c/pp_instance.h"
 #include "ppapi/c/pp_resource.h"
+#include "ppapi/shared_impl/file_ref_detailed_info.h"
 
 namespace content {
 
@@ -31,10 +32,7 @@ class PepperBrowserConnection
   typedef base::Callback<void(const std::vector<int>&)>
       PendingResourceIDCallback;
   typedef base::Callback<void(
-      const std::vector<PP_Resource>&,
-      const std::vector<PP_FileSystemType>&,
-      const std::vector<std::string>&,
-      const std::vector<base::FilePath>&)> FileRefGetInfoCallback;
+      const std::vector<ppapi::FileRefDetailedInfo>&)> FileRefGetInfoCallback;
 
   explicit PepperBrowserConnection(RenderView* render_view);
   virtual ~PepperBrowserConnection();
@@ -75,10 +73,7 @@ class PepperBrowserConnection
       const std::vector<int>& pending_resource_host_ids);
   void OnMsgFileRefGetInfoReply(
       int32_t sequence_number,
-      const std::vector<PP_Resource>& resources,
-      const std::vector<PP_FileSystemType>& types,
-      const std::vector<std::string>& file_system_url_specs,
-      const std::vector<base::FilePath>& external_paths);
+      const std::vector<ppapi::FileRefDetailedInfo>& infos);
 
   // Return the next sequence number.
   int32_t GetNextSequence();
--- ppapi/shared_impl/url_response_info_data.h
+++ ppapi/shared_impl/url_response_info_data.h
@@ -9,7 +9,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 
 #include "ppapi/c/pp_stdint.h"
-#include "ppapi/shared_impl/ppb_file_ref_shared.h"
+#include "ppapi/shared_impl/file_ref_create_info.h"
 #include "ppapi/shared_impl/ppapi_shared_export.h"
 
 namespace ppapi {
@@ -24,8 +24,8 @@ struct PPAPI_SHARED_EXPORT URLResponseInfoData {
   std::string status_text;
   std::string redirect_url;
 
-  // Nonzero when streaming to a file.
-  PPB_FileRef_CreateInfo body_as_file_ref;
+  // Valid when streaming to a file.
+  FileRefCreateInfo body_as_file_ref;
 };
 
 }  // namespace ppapi
