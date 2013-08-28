9ea0745aa5f7b94d0134a741a00734276d982bc7
tyoshino@chromium.org <tyoshino@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 13:36:39 +0000

    Add an IPC to abort Stream construction.
    To be used by XHR to abort partially constructed Stream.
    https://codereview.chromium.org/18883002/
    
    BUG=169957
    
    Review URL: https://chromiumcodereview.appspot.com/23543002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220009 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/fileapi/fileapi_message_filter.cc
+++ content/browser/fileapi/fileapi_message_filter.cc
@@ -196,6 +196,7 @@ bool FileAPIMessageFilter::OnMessageReceived(
     IPC_MESSAGE_HANDLER(StreamHostMsg_SyncAppendSharedMemory,
                         OnAppendSharedMemoryToStream)
     IPC_MESSAGE_HANDLER(StreamHostMsg_FinishBuilding, OnFinishBuildingStream)
+    IPC_MESSAGE_HANDLER(StreamHostMsg_AbortBuilding, OnAbortBuildingStream)
     IPC_MESSAGE_HANDLER(StreamHostMsg_Clone, OnCloneStream)
     IPC_MESSAGE_HANDLER(StreamHostMsg_Remove, OnRemoveStream)
     IPC_MESSAGE_UNHANDLED(handled = false)
@@ -675,6 +676,13 @@ void FileAPIMessageFilter::OnFinishBuildingStream(const GURL& url) {
   scoped_refptr<Stream> stream(GetStreamForURL(url));
   if (stream.get())
     stream->Finalize();
+}
+
+void FileAPIMessageFilter::OnAbortBuildingStream(const GURL& url) {
+  DCHECK(BrowserThread::CurrentlyOn(BrowserThread::IO));
+  scoped_refptr<Stream> stream(GetStreamForURL(url));
+  if (stream.get())
+    stream->Abort();
 }
 
 void FileAPIMessageFilter::OnCloneStream(
