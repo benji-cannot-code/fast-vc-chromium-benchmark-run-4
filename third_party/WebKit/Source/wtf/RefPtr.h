bd46e2df350c6c0b15ed50d725c7b57fa434d040
mikhail.pozdnyakov@intel.com <mikhail.pozdnyakov@intel.com@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-28 20:00:18 +0000

    Cut down repeated code in RefPtr by using copy/move-and-swap for assignment operators
    Extract from WebKit patch by Darin Adler <darin@apple.com> r154519.
    
    RetainPtr is not modified because it requires some preliminary refactoring before that.
    
    Review URL: https://chromiumcodereview.appspot.com/23666004
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156871 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/RefPtr.h
+++ third_party/WebKit/Source/wtf/RefPtr.h
@@ -1,6 +1,6 @@
 FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 /*
- *  Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010 Apple Inc. All rights reserved.
+ *  Copyright (C) 2005, 2006, 2007, 2008, 2009, 2010, 2013 Apple Inc. All rights reserved.
  *
  *  This library is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Library General Public
@@ -93,52 +93,42 @@ namespace WTF {
         derefIfNotNull(ptr);
     }
 
-    template<typename T> inline RefPtr<T>& RefPtr<T>::operator=(const RefPtr<T>& o)
+    template<typename T> inline RefPtr<T>& RefPtr<T>::operator=(const RefPtr& o)
     {
-        T* optr = o.get();
-        refIfNotNull(optr);
-        T* ptr = m_ptr;
-        m_ptr = optr;
-        derefIfNotNull(ptr);
+        RefPtr ptr = o;
+        swap(ptr);
         return *this;
     }
 
     template<typename T> template<typename U> inline EnsurePtrConvertibleType(RefPtr<T>&, U, T) RefPtr<T>::operator=(const RefPtr<U>& o)
     {
-        T* optr = o.get();
-        refIfNotNull(optr);
-        T* ptr = m_ptr;
-        m_ptr = optr;
-        derefIfNotNull(ptr);
+        RefPtr ptr = o;
+        swap(ptr);
         return *this;
     }
 
     template<typename T> inline RefPtr<T>& RefPtr<T>::operator=(T* optr)
     {
-        refIfNotNull(optr);
-        T* ptr = m_ptr;
-        m_ptr = optr;
-        derefIfNotNull(ptr);
+        RefPtr ptr = optr;
+        swap(ptr);
         return *this;
     }
 
     template<typename T> inline RefPtr<T>& RefPtr<T>::operator=(const PassRefPtr<T>& o)
     {
-        T* ptr = m_ptr;
-        m_ptr = o.leakRef();
-        derefIfNotNull(ptr);
+        RefPtr ptr = o;
+        swap(ptr);
         return *this;
     }
 
     template<typename T> template<typename U> inline EnsurePtrConvertibleType(RefPtr<T>&, U, T) RefPtr<T>::operator=(const PassRefPtr<U>& o)
     {
-        T* ptr = m_ptr;
-        m_ptr = o.leakRef();
-        derefIfNotNull(ptr);
+        RefPtr ptr = o;
+        swap(ptr);
         return *this;
     }
 
-    template<class T> inline void RefPtr<T>::swap(RefPtr<T>& o)
+    template<class T> inline void RefPtr<T>::swap(RefPtr& o)
     {
         std::swap(m_ptr, o.m_ptr);
     }
