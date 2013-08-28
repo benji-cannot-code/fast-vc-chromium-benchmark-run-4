a9e63e9ba5a9629076e423dd2e7b690aac0fd937
wittman@chromium.org <wittman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-28 18:48:55 +0000

    Revert 220050 "Plumb user_gesture up from Blink to the JavaScrip..."
    > Plumb user_gesture up from Blink to the JavaScript dialog code, and don't offer to suppress dialogs if they're initiated by a user gesture.
    > 
    > BUG=263192
    > TEST=as in bug
    > 
    > Review URL: https://chromiumcodereview.appspot.com/22909052
    
    TBR=avi@chromium.org
    
    Review URL: https://codereview.chromium.org/23483008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220062 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_modal_dialogs/javascript_dialog_manager.cc
+++ chrome/browser/ui/app_modal_dialogs/javascript_dialog_manager.cc
@@ -48,7 +48,6 @@ class ChromeJavaScriptDialogManager : public JavaScriptDialogManager,
       content::JavaScriptMessageType message_type,
       const string16& message_text,
       const string16& default_prompt_text,
-      bool user_gesture,
       const DialogClosedCallback& callback,
       bool* did_suppress_message) OVERRIDE;
 
@@ -132,7 +131,6 @@ void ChromeJavaScriptDialogManager::RunJavaScriptDialog(
     content::JavaScriptMessageType message_type,
     const string16& message_text,
     const string16& default_prompt_text,
-    bool user_gesture,
     const DialogClosedCallback& callback,
     bool* did_suppress_message)  {
   *did_suppress_message = false;
@@ -149,11 +147,10 @@ void ChromeJavaScriptDialogManager::RunJavaScriptDialog(
       extra_data->last_javascript_message_dismissal_;
   bool display_suppress_checkbox = false;
   // Show a checkbox offering to suppress further messages if this message is
-  // being displayed within kJavaScriptMessageExpectedDelay of the last one and
-  // the dialog was not requested via an explicit user gesture.
+  // being displayed within kJavaScriptMessageExpectedDelay of the last one.
   if (time_since_last_message <
       base::TimeDelta::FromMilliseconds(
-          chrome::kJavaScriptMessageExpectedDelay) && !user_gesture) {
+          chrome::kJavaScriptMessageExpectedDelay)) {
     display_suppress_checkbox = true;
   } else {
     display_suppress_checkbox = false;
