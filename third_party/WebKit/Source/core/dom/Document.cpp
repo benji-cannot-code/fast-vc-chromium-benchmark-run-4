16cacbbf1d291dccbcd57819efba842e9c1caa34
jamesr@chromium.org <jamesr@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-29 00:34:11 +0000

    Move title encoding fixup code into Document
    BUG=277886
    
    Review URL: https://chromiumcodereview.appspot.com/23408002
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156884 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/dom/Document.cpp
+++ third_party/WebKit/Source/core/dom/Document.cpp
@@ -178,6 +178,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "wtf/StdLibExtras.h"
 #include "wtf/UnusedParam.h"
 #include "wtf/text/StringBuffer.h"
+#include "wtf/text/TextEncodingRegistry.h"
 
 using namespace std;
 using namespace WTF;
@@ -3945,6 +3946,26 @@ void Document::setDecoder(PassRefPtr<TextResourceDecoder> decoder)
 
 void Document::setEncoding(const WTF::TextEncoding& encoding)
 {
+    if (m_encoding == encoding)
+        return;
+
+    // It's possible for the encoding of the document to change while we're decoding
+    // data. That can only occur while we're processing the <head> portion of the
+    // document. There isn't much user-visible content in the <head>, but there is
+    // the <title> element. This function detects that situation and re-decodes the
+    // document's title so that the user doesn't see an incorrectly decoded title
+    // in the title bar.
+    if (m_titleElement
+        && !m_titleElement->firstElementChild()
+        && m_encoding == Latin1Encoding()
+        && m_titleElement->textContent().containsOnlyLatin1()) {
+
+        CString originalBytes = m_titleElement->textContent().latin1();
+        OwnPtr<TextCodec> codec = newTextCodec(encoding);
+        String correctlyDecodedTitle = codec->decode(originalBytes.data(), originalBytes.length(), true);
+        m_titleElement->setTextContent(correctlyDecodedTitle, IGNORE_EXCEPTION);
+    }
+
     m_encoding = encoding;
 }
 
