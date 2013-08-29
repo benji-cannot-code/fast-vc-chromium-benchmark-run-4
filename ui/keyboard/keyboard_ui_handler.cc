9647e153ccc20b821c85344d6352ecac8d6dbdeb
bshe@chromium.org <bshe@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 20:50:19 +0000

    To get input type of current input field, call chrome.send('getInputContext'). And the input type will be returned in a callback function named GetInputContextCallback.
    BUG=268949
    
    Review URL: https://chromiumcodereview.appspot.com/22642015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220359 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/keyboard/keyboard_ui_handler.cc
+++ ui/keyboard/keyboard_ui_handler.cc
@@ -13,7 +13,11 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/web_contents.h"
 #include "content/public/browser/web_contents_view.h"
 #include "content/public/browser/web_ui.h"
+#include "ui/aura/client/aura_constants.h"
+#include "ui/aura/root_window.h"
 #include "ui/aura/window.h"
+#include "ui/base/ime/input_method.h"
+#include "ui/base/ime/text_input_client.h"
 #include "ui/keyboard/keyboard_util.h"
 
 namespace keyboard {
@@ -28,6 +32,10 @@ void KeyboardUIHandler::RegisterMessages() {
   web_ui()->RegisterMessageCallback(
       "insertText",
       base::Bind(&KeyboardUIHandler::HandleInsertTextMessage,
+                 base::Unretained(this)));
+  web_ui()->RegisterMessageCallback(
+      "getInputContext",
+      base::Bind(&KeyboardUIHandler::HandleGetInputContextMessage,
                  base::Unretained(this)));
 }
 
@@ -47,6 +55,37 @@ void KeyboardUIHandler::HandleInsertTextMessage(const base::ListValue* args) {
 
   if (!keyboard::InsertText(text, root_window))
     LOG(ERROR) << "insertText failed";
+}
+
+void KeyboardUIHandler::HandleGetInputContextMessage(
+    const base::ListValue* args) {
+  int request_id;
+  if (!args->GetInteger(0, &request_id)) {
+    LOG(ERROR) << "getInputContext failed: bad argument";
+    return;
+  }
+  base::DictionaryValue results;
+  results.SetInteger("requestId", request_id);
+
+  aura::RootWindow* root_window =
+      web_ui()->GetWebContents()->GetView()->GetNativeView()->GetRootWindow();
+  if (!root_window) {
+    LOG(ERROR) << "getInputContext failed: no root window";
+    return;
+  }
+  ui::InputMethod* input_method =
+      root_window->GetProperty(aura::client::kRootWindowInputMethodKey);
+  if (!input_method) {
+    LOG(ERROR) << "getInputContext failed: no input method";
+    return;
+  }
+
+  ui::TextInputClient* tic = input_method->GetTextInputClient();
+  results.SetInteger("type",
+                     tic ? tic->GetTextInputType() : ui::TEXT_INPUT_TYPE_NONE);
+
+  web_ui()->CallJavascriptFunction("GetInputContextCallback",
+                                   results);
 }
 
 }  // namespace keyboard
--- ui/keyboard/resources/webui/api_adapter.js
+++ ui/keyboard/resources/webui/api_adapter.js
@@ -6,3 +6,59 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 function insertText(text) {
   chrome.send('insertText', [ text ]);
 }
+
+(function(exports) {
+  /**
+   * An array to save callbacks of each request.
+   * @type {Array.<function(Object)>}
+   */
+  var requestIdCallbackMap = [];
+
+  /**
+   * An incremental integer that represents a unique requestId.
+   * @type {number}
+   */
+  var requestId = 0;
+
+  /**
+   * Gets the context of the focused input field. The context is returned as a
+   * paramter in the |callback|.
+   * @param {function(Object)} callback The callback function after the webui
+   *     function finished.
+   * @return {number} The ID of the new request.
+   */
+  function GetInputContext(callback) {
+    var id = requestId;
+    requestIdCallbackMap[id] = callback;
+    chrome.send('getInputContext', [ id ]);
+    requestId++;
+    return id;
+  }
+
+  /**
+   * Cancel the callback specified by requestId.
+   * @param {number} requestId The requestId of the callback that about to
+   *     cancel.
+   */
+  function CancelRequest(requestId) {
+    requestIdCallbackMap[requestId] = undefined;
+  }
+
+  /**
+   * Webui function callback. Any call to chrome.send('getInputContext', [id])
+   * should trigger this function being called with the parameter
+   * inputContext.requestId == id.
+   * @param {Object} inputContext The context of focused input field. Note we
+   *     only have type(input box type) and requestId fields now.
+   */
+  function GetInputContextCallback(inputContext) {
+    var requestId = inputContext.requestId;
+    if (!requestIdCallbackMap[requestId])
+      return;
+    requestIdCallbackMap[requestId](inputContext);
+  }
+
+  exports.getInputContext = GetInputContext;
+  exports.cancelRequest = CancelRequest;
+  exports.GetInputContextCallback = GetInputContextCallback;
+})(this);
