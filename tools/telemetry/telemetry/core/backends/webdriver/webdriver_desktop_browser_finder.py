35d5f257e0fa5d67e7fdc44b771c3941210da15a
achuith@chromium.org <achuith@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 22:31:33 +0000

    Replace PossibleBrowser.options with PossibleBrowser.finder_options
    Just a simple find/replace of options with finder_options in these files.
    
    BUG=269131
    TEST=trybots, manual
    
    Review URL: https://chromiumcodereview.appspot.com/23566012
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220400 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/core/backends/webdriver/webdriver_desktop_browser_finder.py
+++ tools/telemetry/telemetry/core/backends/webdriver/webdriver_desktop_browser_finder.py
@@ -36,8 +36,8 @@ else:
 class PossibleWebDriverBrowser(possible_browser.PossibleBrowser):
   """A browser that can be controlled through webdriver API."""
 
-  def __init__(self, browser_type, options):
-    super(PossibleWebDriverBrowser, self).__init__(browser_type, options)
+  def __init__(self, browser_type, finder_options):
+    super(PossibleWebDriverBrowser, self).__init__(browser_type, finder_options)
 
   def CreateWebDriverBackend(self):
     raise NotImplementedError()
@@ -47,7 +47,7 @@ class PossibleWebDriverBrowser(possible_browser.PossibleBrowser):
     b = browser.Browser(backend, platform.CreatePlatformBackendForCurrentOS())
     return b
 
-  def SupportsOptions(self, options):
+  def SupportsOptions(self, finder_options):
     # TODO(chrisgao): Check if some options are not supported.
     return True
 
@@ -57,8 +57,8 @@ class PossibleWebDriverBrowser(possible_browser.PossibleBrowser):
 
 
 class PossibleDesktopIE(PossibleWebDriverBrowser):
-  def __init__(self, browser_type, options, architecture):
-    super(PossibleDesktopIE, self).__init__(browser_type, options)
+  def __init__(self, browser_type, finder_options, architecture):
+    super(PossibleDesktopIE, self).__init__(browser_type, finder_options)
     self._architecture = architecture
 
   def CreateWebDriverBackend(self):
@@ -68,12 +68,12 @@ class PossibleDesktopIE(PossibleWebDriverBrowser):
       # creating the webdriver instance. crbug.com/266170
       return webdriver.Ie()
     return webdriver_browser_backend.WebDriverBrowserBackend(
-        DriverCreator, False, self.options)
+        DriverCreator, False, self.finder_options)
 
 def SelectDefaultBrowser(_):
   return None
 
-def FindAllAvailableBrowsers(options):
+def FindAllAvailableBrowsers(finder_options):
   """Finds all the desktop browsers available on this machine."""
   browsers = []
   if not webdriver:
@@ -92,6 +92,6 @@ def FindAllAvailableBrowsers(options):
         continue
       if os.path.exists(os.path.join(ie_info['path'], ie_path)):
         browsers.append(
-            PossibleDesktopIE(ie_info['type'], options, architecture))
+            PossibleDesktopIE(ie_info['type'], finder_options, architecture))
 
   return browsers
--- tools/telemetry/telemetry/core/chrome/desktop_browser_finder_unittest.py
+++ tools/telemetry/telemetry/core/chrome/desktop_browser_finder_unittest.py
@@ -16,8 +16,8 @@ from telemetry.unittest import system_stub
 
 class FindTestBase(unittest.TestCase):
   def setUp(self):
-    self._options = browser_options.BrowserFinderOptions()
-    self._options.chrome_root = '../../../'
+    self._finder_options = browser_options.BrowserFinderOptions()
+    self._finder_options.chrome_root = '../../../'
     self._stubs = system_stub.Override(desktop_browser_finder,
                                        ['os', 'subprocess', 'sys'])
 
@@ -29,7 +29,7 @@ class FindTestBase(unittest.TestCase):
     return self._stubs.os.path.files
 
   def DoFindAll(self):
-    return desktop_browser_finder.FindAllAvailableBrowsers(self._options)
+    return desktop_browser_finder.FindAllAvailableBrowsers(self._finder_options)
 
   def DoFindAllTypes(self):
     browsers = self.DoFindAll()
@@ -135,7 +135,7 @@ class LinuxFindTest(FindTestBase):
              'content-shell-debug', 'content-shell-release']))
 
   def testFindWithProvidedExecutable(self):
-    self._options.browser_executable = '/foo/chrome'
+    self._finder_options.browser_executable = '/foo/chrome'
     self.assertTrue('exact' in self.DoFindAllTypes())
 
   def testFindUsingDefaults(self):
@@ -179,7 +179,7 @@ class WinFindTest(FindTestBase):
                            'system', 'canary']))
 
   def testFindAllWithExact(self):
-    self._options.browser_executable = 'c:\\tmp\\chrome.exe'
+    self._finder_options.browser_executable = 'c:\\tmp\\chrome.exe'
     types = self.DoFindAllTypes()
     self.assertEquals(
         set(types),
