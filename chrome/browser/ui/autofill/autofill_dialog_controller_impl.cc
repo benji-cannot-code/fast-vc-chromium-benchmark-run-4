b6e8a3a833309964a3f2bee8e26977f119392480
isherman@chromium.org <isherman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 09:38:18 +0000

    [Autofill] Simplify GetRawInfo() and SetRawInfo().
    Require clients of these methods to pre-convert the field type to a storable
    type if appropriate.  These two methods are really only intended to be used when
    fetching data directly from, or writing data directly to, a data model, which
    implies that the data should be being written to a storable type.
    
    Also, adds DCHECKs to verify that these methods are being invoked correctly.
    
    BUG=none
    TEST=none
    R=estade@chromium.org
    
    Review URL: https://chromiumcodereview.appspot.com/22320017
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220244 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
+++ chrome/browser/ui/autofill/autofill_dialog_controller_impl.cc
@@ -162,7 +162,8 @@ void FillFormGroupFromOutputs(const DetailOutputMap& detail_outputs,
                             iter->second,
                             g_browser_process->GetApplicationLocale());
       } else {
-        form_group->SetRawInfo(type, iter->second);
+        form_group->SetRawInfo(
+            AutofillType(type).GetStorableType(), iter->second);
       }
     }
   }
@@ -201,7 +202,8 @@ void GetBillingInfoFromOutputs(const DetailOutputMap& output,
         if (card)
           card->SetRawInfo(it->first->type, trimmed);
       } else if (profile) {
-        profile->SetRawInfo(it->first->type, trimmed);
+        profile->SetRawInfo(
+            AutofillType(it->first->type).GetStorableType(), trimmed);
       }
     }
   }
@@ -297,7 +299,7 @@ bool HasCompleteAndVerifiedData(const AutofillDataModel& data_model,
     ServerFieldType type = requested_fields[i].type;
     if (type != ADDRESS_HOME_LINE2 &&
         type != CREDIT_CARD_VERIFICATION_CODE &&
-        data_model.GetRawInfo(type).empty()) {
+        data_model.GetRawInfo(AutofillType(type).GetStorableType()).empty()) {
       return false;
     }
   }
