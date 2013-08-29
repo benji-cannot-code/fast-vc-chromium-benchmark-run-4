9a4febad88f2130f417931c0034b77c054456a93
hashimoto@chromium.org <hashimoto@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 04:03:13 +0000

    drive: Stop using resource ID to access local metadata in DownloadOperation
    Add OperationTestBase::GetLocalId
    Rename EnsureFileDownloadedByResourceId to EnsureFileDownloadedByLocalId
    Add ResourceEntry as a DownloadCallback member to workaround the base::Callback argument limit.
    
    BUG=260514
    TEST=unit_tests
    R=hidehiko@chromium.org
    
    Review URL: https://codereview.chromium.org/23503006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220181 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/file_system/download_operation_unittest.cc
+++ chrome/browser/chromeos/drive/file_system/download_operation_unittest.cc
@@ -119,7 +119,7 @@ TEST_F(DownloadOperationTest,
 
   FileError error = FILE_ERROR_FAILED;
   cache()->StoreOnUIThread(
-      "<resource_id>", "<md5>", tmp_file,
+      "<id>", "<md5>", tmp_file,
       internal::FileCache::FILE_OPERATION_COPY,
       google_apis::test_util::CreateCopyResultCallback(&error));
   test_util::RunBlockingPoolTask();
@@ -149,7 +149,7 @@ TEST_F(DownloadOperationTest,
   FileCacheEntry cache_entry;
   bool result = true;
   cache()->GetCacheEntryOnUIThread(
-      "<resource_id>",
+      "<id>",
       google_apis::test_util::CreateCopyResultCallback(&result,
                                                        &cache_entry));
   test_util::RunBlockingPoolTask();
@@ -198,7 +198,7 @@ TEST_F(DownloadOperationTest, EnsureFileDownloadedByPath_FromCache) {
   // Store something as cached version of this file.
   FileError error = FILE_ERROR_OK;
   cache()->StoreOnUIThread(
-      src_entry.resource_id(),
+      GetLocalId(file_in_root),
       src_entry.file_specific_info().md5(),
       temp_file,
       internal::FileCache::FILE_OPERATION_COPY,
@@ -248,17 +248,16 @@ TEST_F(DownloadOperationTest, EnsureFileDownloadedByPath_HostedDocument) {
   EXPECT_EQ(entry->resource_id(), util::ReadResourceIdFromGDocFile(file_path));
 }
 
-TEST_F(DownloadOperationTest, EnsureFileDownloadedByResourceId) {
+TEST_F(DownloadOperationTest, EnsureFileDownloadedByLocalId) {
   base::FilePath file_in_root(FILE_PATH_LITERAL("drive/root/File 1.txt"));
   ResourceEntry src_entry;
   ASSERT_EQ(FILE_ERROR_OK, GetLocalResourceEntry(file_in_root, &src_entry));
-  std::string resource_id = src_entry.resource_id();
 
   FileError error = FILE_ERROR_OK;
   base::FilePath file_path;
   scoped_ptr<ResourceEntry> entry;
-  operation_->EnsureFileDownloadedByResourceId(
-      resource_id,
+  operation_->EnsureFileDownloadedByLocalId(
+      GetLocalId(file_in_root),
       ClientContext(USER_INITIATED),
       GetFileContentInitializedCallback(),
       google_apis::GetContentCallback(),
@@ -351,7 +350,7 @@ TEST_F(DownloadOperationTest,
   }
 }
 
-TEST_F(DownloadOperationTest, EnsureFileDownloadedByResourceId_FromCache) {
+TEST_F(DownloadOperationTest, EnsureFileDownloadedByLocalId_FromCache) {
   base::FilePath temp_file;
   ASSERT_TRUE(file_util::CreateTemporaryFileInDir(temp_dir(), &temp_file));
 
@@ -362,7 +361,7 @@ TEST_F(DownloadOperationTest, EnsureFileDownloadedByResourceId_FromCache) {
   // Store something as cached version of this file.
   FileError error = FILE_ERROR_FAILED;
   cache()->StoreOnUIThread(
-      src_entry.resource_id(),
+      GetLocalId(file_in_root),
       src_entry.file_specific_info().md5(),
       temp_file,
       internal::FileCache::FILE_OPERATION_COPY,
@@ -374,11 +373,10 @@ TEST_F(DownloadOperationTest, EnsureFileDownloadedByResourceId_FromCache) {
   // Hence the downloading should work even if the drive service is offline.
   fake_service()->set_offline(true);
 
-  std::string resource_id = src_entry.resource_id();
   base::FilePath file_path;
   scoped_ptr<ResourceEntry> entry;
-  operation_->EnsureFileDownloadedByResourceId(
-      resource_id,
+  operation_->EnsureFileDownloadedByLocalId(
+      GetLocalId(file_in_root),
       ClientContext(USER_INITIATED),
       GetFileContentInitializedCallback(),
       google_apis::GetContentCallback(),
@@ -406,7 +404,7 @@ TEST_F(DownloadOperationTest, EnsureFileDownloadedByPath_DirtyCache) {
   // Store the file as a cache, marking it to be dirty.
   FileError error = FILE_ERROR_FAILED;
   cache()->StoreOnUIThread(
-      src_entry.resource_id(),
+      GetLocalId(file_in_root),
       src_entry.file_specific_info().md5(),
       dirty_file,
       internal::FileCache::FILE_OPERATION_COPY,
@@ -414,7 +412,7 @@ TEST_F(DownloadOperationTest, EnsureFileDownloadedByPath_DirtyCache) {
   test_util::RunBlockingPoolTask();
   EXPECT_EQ(FILE_ERROR_OK, error);
   cache()->MarkDirtyOnUIThread(
-      src_entry.resource_id(),
+      GetLocalId(file_in_root),
       google_apis::test_util::CreateCopyResultCallback(&error));
   test_util::RunBlockingPoolTask();
   EXPECT_EQ(FILE_ERROR_OK, error);
