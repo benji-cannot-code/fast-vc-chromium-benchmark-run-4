55ba3294a91798184e8bfcfda8ea3a54b6a99540
jianli@chromium.org <jianli@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 09:51:41 +0000

    [Mac] Do not minimize the stack if the titlebar of non-top panel is double-clicked.
    BUG=280822
    TEST=Manual test by launching a few panels in a stack and double-clicking on a non-top panel
    
    Review URL: https://chromiumcodereview.appspot.com/23710008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220252 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/cocoa/panels/panel_titlebar_view_cocoa.mm
+++ chrome/browser/ui/cocoa/panels/panel_titlebar_view_cocoa.mm
@@ -408,7 +408,7 @@ static NSEvent* MakeMouseEvent(NSEventType type,
   if ([event clickCount] == 1)
     [controller_ onTitlebarMouseClicked:[event modifierFlags]];
   else if ([event clickCount] == 2)
-    [controller_ minimizeButtonClicked:[event modifierFlags]];
+    [controller_ onTitlebarDoubleClicked:[event modifierFlags]];
 }
 
 - (void)mouseDragged:(NSEvent*)event {
