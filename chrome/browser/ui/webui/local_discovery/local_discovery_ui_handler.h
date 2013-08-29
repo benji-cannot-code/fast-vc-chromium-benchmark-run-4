b1fd9dd47440b30516538e7e508cafbf1882a556
noamsml@chromium.org <noamsml@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 08:48:41 +0000

    Prototype for multi-account privet registration
    Add a multi-account privet registration to the prototype devices page. Supports cookie-based multi-account as well as sync profile.
    
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/22834005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220222 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h
+++ chrome/browser/ui/webui/local_discovery/local_discovery_ui_handler.h
@@ -8,7 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 #include <map>
 #include <string>
+#include <vector>
 
+#include "chrome/browser/local_discovery/cloud_print_account_manager.h"
 #include "chrome/browser/local_discovery/privet_confirm_api_flow.h"
 #include "chrome/browser/local_discovery/privet_device_lister.h"
 #include "chrome/browser/local_discovery/privet_http.h"
@@ -18,6 +20,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/user_metrics.h"
 #include "content/public/browser/web_ui_message_handler.h"
 
+// TODO(noamsml): Factor out full registration flow into single class
 namespace local_discovery {
 
 // UI Handler for chrome://devices/
@@ -81,6 +84,7 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
   // Message handlers:
   // For registering a device.
   void HandleRegisterDevice(const base::ListValue* args);
+
   // For when the page is ready to recieve device notifications.
   void HandleStart(const base::ListValue* args);
 
@@ -90,8 +94,13 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
   // For when a visibility change occurs.
   void HandleIsVisible(const base::ListValue* args);
 
+  // For when a user choice is made.
+  void HandleChooseUser(const base::ListValue* args);
+
   // For when the IP address of the printer has been resolved for registration.
-  void StartRegisterHTTP(scoped_ptr<PrivetHTTPClient> http_client);
+  void StartRegisterHTTP(
+      const std::string& user,
+      scoped_ptr<PrivetHTTPClient> http_client);
 
   // For when the IP address of the printer has been resolved for registration.
   void StartInfoHTTP(scoped_ptr<PrivetHTTPClient> http_client);
@@ -99,6 +108,16 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
   // For when the confirm operation on the cloudprint server has finished
   // executing.
   void OnConfirmDone(PrivetConfirmApiCallFlow::Status status);
+
+  // For when the cloud print account list is resolved.
+  void OnCloudPrintAccountsResolved(const std::vector<std::string>& accounts,
+                                    const std::string& xsrf_token);
+
+  // For when XSRF token is received for a secondary account.
+  void OnXSRFTokenForSecondaryAccount(
+      const GURL& automated_claim_url,
+      const std::vector<std::string>& accounts,
+      const std::string& xsrf_token);
 
   // Log an error to the web interface.
   void LogRegisterErrorToWeb(const std::string& error);
@@ -111,6 +130,18 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
 
   // Set the visibility of the page.
   void SetIsVisible(bool visible);
+
+  // Get the sync account email.
+  std::string GetSyncAccount();
+
+  // Get the base cloud print URL for a given device.
+  const std::string& GetCloudPrintBaseUrl(const std::string& device_name);
+
+  // Start the confirm flow for a cookie based authentication.
+  void StartCookieConfirmFlow(
+      int user_index,
+      const std::string& xsrf_token,
+      const GURL& automatic_claim_url);
 
   // The current HTTP client (used for the current operation).
   scoped_ptr<PrivetHTTPClient> current_http_client_;
@@ -141,6 +172,19 @@ class LocalDiscoveryUIHandler : public content::WebUIMessageHandler,
 
   // Whether or not the page is marked as visible.
   bool is_visible_;
+
+  // Cloud print account manager to enumerate accounts and get XSRF token.
+  scoped_ptr<CloudPrintAccountManager> cloud_print_account_manager_;
+
+  // Device currently registering.
+  std::string current_register_device_;
+
+  // XSRF token.
+  std::string xsrf_token_for_primary_user_;
+
+  // Current user index (for multi-login), or kAccountIndexUseOAuth2 for sync
+  // credentials.
+  int current_register_user_index_;
 
   DISALLOW_COPY_AND_ASSIGN(LocalDiscoveryUIHandler);
 };
