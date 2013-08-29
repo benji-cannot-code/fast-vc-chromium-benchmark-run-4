7fd6ee8df3a57057ac5c741152c681cd952f4c3e
vsevik@chromium.org <vsevik@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-29 09:23:12 +0000

    DevTools: Send cookies when loading resources through backend.
    BUG=264673
    
    Review URL: https://chromiumcodereview.appspot.com/23532007
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@156907 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
+++ third_party/WebKit/Source/core/inspector/InspectorResourceAgent.cpp
@@ -732,7 +732,7 @@ void InspectorResourceAgent::loadResourceForFrontend(ErrorString* errorString, c
     }
 
     ThreadableLoaderOptions options;
-    options.allowCredentials = DoNotAllowStoredCredentials;
+    options.allowCredentials = AllowStoredCredentials;
     options.crossOriginRequestPolicy = AllowCrossOriginRequests;
     options.sendLoadCallbacks = SendCallbacks;
 
