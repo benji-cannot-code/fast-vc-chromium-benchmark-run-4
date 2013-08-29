be50b589f70f836a69be8d3448b9e6537b84cbd6
tiago.vignatti@intel.com <tiago.vignatti@intel.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 04:47:51 +0000

    views: Implement Ozone root window for desktop aura
    BUG=none
    
    Review URL: https://chromiumcodereview.appspot.com/22812021
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220204 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/base/ozone/surface_factory_ozone.h
+++ ui/base/ozone/surface_factory_ozone.h
@@ -13,7 +13,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 namespace gfx {
 class Screen;
 class VSyncProvider;
-} //  namespace gfx
+}  // namespace gfx
 
 namespace ui {
 
