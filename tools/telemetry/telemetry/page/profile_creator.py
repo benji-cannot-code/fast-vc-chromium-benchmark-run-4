36b717fe3eebaf81a6111f656f461470e197f708
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 01:34:04 +0000

    [Telemetry] Implement dirty profile generation
    Teach generation_profiles to actually generate a dirty profile and download it off a device.
    
    BUG=136664
    
    Review URL: https://chromiumcodereview.appspot.com/23604005
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220157 0039d316-1c4b-4281-b951-d872f2087c98

---
+++ tools/telemetry/telemetry/page/profile_creator.py
@@ -0,0 +1,20 @@
+FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
+# Copyright 2013 The Chromium Authors. All rights reserved.
+# Use of this source code is governed by a BSD-style license that can be
+# found in the LICENSE file.
+
+from telemetry.page import page_measurement
+
+class ProfileCreator(page_measurement.PageMeasurement):
+  """Base class for an object that constructs a Chrome profile."""
+
+  def __init__(self):
+    super(ProfileCreator, self).__init__()
+    self._page_set = None
+
+  @property
+  def page_set(self):
+    return self._page_set
+
+  def MeasurePage(self, _, tab, results):
+    pass
