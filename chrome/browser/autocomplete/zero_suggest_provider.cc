cb18901f1fdfe1604f8967bac624e60fc6641b03
hfung@chromium.org <hfung@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 09:50:21 +0000

    Fix bug that doesn't show verbatim query for ZeroSuggest in instant extended mode.
    BUG=280704
    
    Review URL: https://chromiumcodereview.appspot.com/23701007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220250 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/autocomplete/zero_suggest_provider.cc
+++ chrome/browser/autocomplete/zero_suggest_provider.cc
@@ -14,6 +14,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/strings/string_util.h"
 #include "base/strings/utf_string_conversions.h"
 #include "base/time/time.h"
+#include "chrome/browser/autocomplete/autocomplete_classifier.h"
+#include "chrome/browser/autocomplete/autocomplete_classifier_factory.h"
 #include "chrome/browser/autocomplete/autocomplete_input.h"
 #include "chrome/browser/autocomplete/autocomplete_match.h"
 #include "chrome/browser/autocomplete/autocomplete_provider_listener.h"
@@ -461,9 +463,9 @@ AutocompleteMatch ZeroSuggestProvider::MatchForCurrentURL() {
                           GURL(current_query_), current_page_classification_,
                           false, false, true, AutocompleteInput::ALL_MATCHES);
 
-  AutocompleteMatch match(
-      HistoryURLProvider::SuggestExactInput(this, input,
-                                            !HasHTTPScheme(input.text())));
+  AutocompleteMatch match;
+  AutocompleteClassifierFactory::GetForProfile(profile_)->Classify(
+      permanent_text_, false, true, &match, NULL);
   match.is_history_what_you_typed_match = false;
   match.allowed_to_be_default_match = true;
 
