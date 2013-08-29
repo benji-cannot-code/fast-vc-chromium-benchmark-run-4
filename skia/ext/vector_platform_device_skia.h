77305779194f105218186d60afc57a6e8e7a3f81
mtklein@google.com <mtklein@google.com@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 21:59:27 +0000

    remove unused include (a step towards removing SkTScopedPtr)
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23654006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220390 0039d316-1c4b-4281-b951-d872f2087c98

--- skia/ext/vector_platform_device_skia.h
+++ skia/ext/vector_platform_device_skia.h
@@ -11,7 +11,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/logging.h"
 #include "skia/ext/platform_device.h"
 #include "skia/ext/refptr.h"
-#include "third_party/skia/include/core/SkTScopedPtr.h"
 #include "third_party/skia/include/pdf/SkPDFDevice.h"
 
 class SkMatrix;
