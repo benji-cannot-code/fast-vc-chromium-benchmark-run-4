75b9d06940f3aab923989e325e64d2b814a6b3a0
asvitkine@chromium.org <asvitkine@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 18:35:20 +0000

    Fix build break following variations component move.
    Include wasn't updated.
    
    BUG=266007
    TBR=lambroslambrou@chromium.org
    
    Review URL: https://codereview.chromium.org/23629008
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220344 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/login/default_pinned_apps_field_trial_unittest.cc
+++ chrome/browser/chromeos/login/default_pinned_apps_field_trial_unittest.cc
@@ -8,8 +8,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/memory/scoped_ptr.h"
 #include "base/metrics/field_trial.h"
 #include "base/prefs/testing_pref_service.h"
-#include "chrome/common/metrics/entropy_provider.h"
 #include "chrome/test/base/testing_browser_process.h"
+#include "components/variations/entropy_provider.h"
 #include "testing/gtest/include/gtest/gtest.h"
 
 namespace chromeos {
