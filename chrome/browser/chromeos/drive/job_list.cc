552e29ba930034546459099ac7b75845d592d097
hidehiko@chromium.org <hidehiko@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 04:27:04 +0000

    Implement GetRemaining{File,Change}List on JobScheduler.
    The ContinueGetResourceList will be split into GetRemaining{File,Change}List.
    This CL supports them on JobScheduler.
    
    BUG=277253
    TEST=Ran unit_tests
    
    Review URL: https://chromiumcodereview.appspot.com/23676002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220191 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/drive/job_list.cc
+++ chrome/browser/chromeos/drive/job_list.cc
@@ -27,6 +27,10 @@ std::string JobTypeToString(JobType type) {
       return "TYPE_GET_CHANGE_LIST";
     case TYPE_CONTINUE_GET_RESOURCE_LIST:
       return "TYPE_CONTINUE_GET_RESOURCE_LIST";
+    case TYPE_GET_REMAINING_CHANGE_LIST:
+      return "TYPE_GET_REMAINING_CHANGE_LIST";
+    case TYPE_GET_REMAINING_FILE_LIST:
+      return "TYPE_GET_REMAINING_FILE_LIST";
     case TYPE_GET_RESOURCE_ENTRY:
       return "TYPE_GET_RESOURCE_ENTRY";
     case TYPE_GET_SHARE_URL:
@@ -119,6 +123,8 @@ bool IsActiveFileTransferJobInfo(const JobInfo& job_info) {
     case TYPE_SEARCH:
     case TYPE_GET_CHANGE_LIST:
     case TYPE_CONTINUE_GET_RESOURCE_LIST:
+    case TYPE_GET_REMAINING_CHANGE_LIST:
+    case TYPE_GET_REMAINING_FILE_LIST:
     case TYPE_GET_RESOURCE_ENTRY:
     case TYPE_GET_SHARE_URL:
     case TYPE_DELETE_RESOURCE:
