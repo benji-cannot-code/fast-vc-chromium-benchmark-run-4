36cb0dfa744807bcdb86f18aee8977fb5e582fdf
piman@chromium.org <piman@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 04:17:22 +0000

    Always FCM+compositor thread on all Aura platforms
    It's already on for Windows and Chrome OS, and there's no reason to leave it off
    on Linux. It's also a prerequisite for Übercompositor
    
    BUG=123444
    
    Review URL: https://chromiumcodereview.appspot.com/23672002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220189 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/gpu/compositor_util.cc
+++ content/browser/gpu/compositor_util.cc
@@ -45,8 +45,8 @@ bool IsForceCompositingModeBlacklisted() {
 }  // namespace
 
 bool IsThreadedCompositingEnabled() {
-#if defined(OS_WIN) && defined(USE_AURA)
-  // We always want compositing on Aura Windows.
+#if defined(USE_AURA)
+  // We always want threaded compositing on Aura.
   return true;
 #endif
 
@@ -59,12 +59,6 @@ bool IsThreadedCompositingEnabled() {
   if (command_line.HasSwitch(switches::kDisableForceCompositingMode) ||
       command_line.HasSwitch(switches::kDisableThreadedCompositing))
     return false;
-
-#if defined(OS_CHROMEOS)
-  // We always want threaded compositing on  ChromeOS unless it's explicitly
-  // disabled above.
-  return true;
-#endif
 
   if (command_line.HasSwitch(switches::kEnableThreadedCompositing))
     return true;
@@ -79,8 +73,8 @@ bool IsThreadedCompositingEnabled() {
 }
 
 bool IsForceCompositingModeEnabled() {
-#if defined(OS_WIN) && defined(USE_AURA)
-  // We always want compositing on Aura Windows.
+#if defined(USE_AURA)
+  // We always want compositing on Aura.
   return true;
 #endif
 
@@ -92,11 +86,6 @@ bool IsForceCompositingModeEnabled() {
   // Command line switches take precedence over blacklisting and field trials.
   if (command_line.HasSwitch(switches::kDisableForceCompositingMode))
     return false;
-
-#if defined(OS_CHROMEOS)
-  // We always want compositing ChromeOS unless it's explicitly disabled above.
-  return true;
-#endif
 
   if (command_line.HasSwitch(switches::kForceCompositingMode))
     return true;
