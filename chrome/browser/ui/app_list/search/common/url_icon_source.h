abb8af7d40114fba85522f93c7f44596a2a8433d
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 09:27:11 +0000

    Refactor WebstoreResultIconSource to a generic UrlIconSource class. Refactor the webstore result icon source into a common url icon source class that can be used by other classes too. This class will be used by the people search in addition to the webstore search. We've moved the CreateBadgedIcon to webstore_result instead, providing a pointer to it for the UrlIconSource class to use for post processing.
    R=xiyuan@chromium.org
    BUG=None
    TEST=Webstore search still functions correctly.
    
    Review URL: https://chromiumcodereview.appspot.com/23643007
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220241 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/search/common/url_icon_source.h
+++ chrome/browser/ui/app_list/search/common/url_icon_source.h
@@ -3,8 +3,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_WEBSTORE_RESULT_ICON_SOURCE_H_
-#define CHROME_BROWSER_UI_APP_LIST_SEARCH_WEBSTORE_RESULT_ICON_SOURCE_H_
+#ifndef CHROME_BROWSER_UI_APP_LIST_SEARCH_COMMON_URL_ICON_SOURCE_H_
+#define CHROME_BROWSER_UI_APP_LIST_SEARCH_COMMON_URL_ICON_SOURCE_H_
 
 #include "base/basictypes.h"
 #include "base/callback.h"
@@ -23,30 +23,27 @@ class URLRequestContextGetter;
 
 namespace app_list {
 
-// An ImageSkiaSource for the icon of web store result. It shows web store
-// icon before the app icon is fetched. When the app icon is fetched
-// successfully, it creates an representation using the app icon and web store
-// icon as a badge.
-class WebstoreResultIconSource : public gfx::ImageSkiaSource,
-                                 public net::URLFetcherDelegate,
-                                 public ImageDecoder::Delegate {
+// An ImageSkiaSource for icons fetched from a URL. Till the URL icon is
+// fetched, the default icon (specified by it's resource id) is shown.
+class UrlIconSource : public gfx::ImageSkiaSource,
+                      public net::URLFetcherDelegate,
+                      public ImageDecoder::Delegate {
  public:
   typedef base::Closure IconLoadedCallback;
 
-  WebstoreResultIconSource(const IconLoadedCallback& icon_loaded_callback,
-                           net::URLRequestContextGetter* context_getter,
-                           const GURL& icon_url,
-                           int icon_size);
-  virtual ~WebstoreResultIconSource();
+  // Create a URL Icon source with the given URL. The post_process parameter
+  // specifies a function to post-process the result icon before displaying it.
+  UrlIconSource(const IconLoadedCallback& icon_loaded_callback,
+                net::URLRequestContextGetter* context_getter,
+                const GURL& icon_url,
+                int icon_size,
+                int default_icon_resource_id);
+  virtual ~UrlIconSource();
 
  private:
   // Invoked from GetImageForScale to download the app icon when the hosting
   // ImageSkia gets painted on screen.
   void StartIconFetch();
-
-  // Creates the result icon by putting a small web store icon on the bottom
-  // right corner as a badge.
-  gfx::ImageSkiaRep CreateBadgedIcon(ui::ScaleFactor scale_factor);
 
   // gfx::ImageSkiaSource overrides:
   virtual gfx::ImageSkiaRep GetImageForScale(
@@ -64,6 +61,7 @@ class WebstoreResultIconSource : public gfx::ImageSkiaSource,
   net::URLRequestContextGetter* context_getter_;
   const GURL icon_url_;
   const int icon_size_;
+  const int default_icon_resource_id_;
 
   bool icon_fetch_attempted_;
   scoped_ptr<net::URLFetcher> icon_fetcher_;
@@ -72,9 +70,9 @@ class WebstoreResultIconSource : public gfx::ImageSkiaSource,
 
   gfx::ImageSkia icon_;
 
-  DISALLOW_COPY_AND_ASSIGN(WebstoreResultIconSource);
+  DISALLOW_COPY_AND_ASSIGN(UrlIconSource);
 };
 
 }  // namespace app_list
 
-#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_WEBSTORE_RESULT_ICON_SOURCE_H_
+#endif  // CHROME_BROWSER_UI_APP_LIST_SEARCH_COMMON_URL_ICON_SOURCE_H_
