23f0d5e88e81a0e224499eb0968f1c936ed5382b
rkc@chromium.org <rkc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 21:57:12 +0000

    Refactor Webstore search code. This CL refactors the current webstore search code to move out common code that will be used both by Webstore search and People search.
    R=xiyuan@chromium.org
    BUG=None
    TEST=Webstore search works as intended.
    
    Review URL: https://codereview.chromium.org/23666006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220388 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/ui/app_list/search/common/json_response_fetcher.cc
+++ chrome/browser/ui/app_list/search/common/json_response_fetcher.cc
@@ -3,21 +3,21 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include "chrome/browser/ui/app_list/search/webstore_search_fetcher.h"
+#include "chrome/browser/ui/app_list/search/common/json_response_fetcher.h"
 
 #include "base/bind.h"
 #include "base/values.h"
 #include "chrome/browser/safe_json_parser.h"
-#include "chrome/common/extensions/extension_constants.h"
 #include "net/base/load_flags.h"
 #include "net/url_request/url_fetcher.h"
 #include "net/url_request/url_request_status.h"
+#include "url/gurl.h"
 
 namespace app_list {
 
-const char kBadResponse[] = "Bad Web Store search response";
+const char kBadResponse[] = "Bad Web Service search response";
 
-WebstoreSearchFetcher::WebstoreSearchFetcher(
+JSONResponseFetcher::JSONResponseFetcher(
     const Callback& callback,
     net::URLRequestContextGetter* context_getter)
     : callback_(callback),
@@ -26,14 +26,13 @@ WebstoreSearchFetcher::WebstoreSearchFetcher(
   DCHECK(!callback_.is_null());
 }
 
-WebstoreSearchFetcher::~WebstoreSearchFetcher() {}
+JSONResponseFetcher::~JSONResponseFetcher() {}
 
-void WebstoreSearchFetcher::Start(const std::string& query,
-                                  const std::string& hl) {
+void JSONResponseFetcher::Start(const GURL& query_url) {
   Stop();
 
   fetcher_.reset(net::URLFetcher::Create(
-      extension_urls::GetWebstoreJsonSearchUrl(query, hl),
+      query_url,
       net::URLFetcher::GET,
       this));
   fetcher_->SetRequestContext(context_getter_);
@@ -42,12 +41,12 @@ void WebstoreSearchFetcher::Start(const std::string& query,
   fetcher_->Start();
 }
 
-void WebstoreSearchFetcher::Stop() {
+void JSONResponseFetcher::Stop() {
   fetcher_.reset();
   weak_factory_.InvalidateWeakPtrs();
 }
 
-void WebstoreSearchFetcher::OnJsonParseSuccess(
+void JSONResponseFetcher::OnJsonParseSuccess(
     scoped_ptr<base::Value> parsed_json) {
   if (!parsed_json->IsType(base::Value::TYPE_DICTIONARY)) {
     OnJsonParseError(kBadResponse);
@@ -58,11 +57,12 @@ void WebstoreSearchFetcher::OnJsonParseSuccess(
       static_cast<base::DictionaryValue*>(parsed_json.release())));
 }
 
-void WebstoreSearchFetcher::OnJsonParseError(const std::string& error) {
+void JSONResponseFetcher::OnJsonParseError(const std::string& error) {
   callback_.Run(scoped_ptr<base::DictionaryValue>());
 }
 
-void WebstoreSearchFetcher::OnURLFetchComplete(const net::URLFetcher* source) {
+void JSONResponseFetcher::OnURLFetchComplete(
+    const net::URLFetcher* source) {
   CHECK_EQ(fetcher_.get(), source);
 
   scoped_ptr<net::URLFetcher> fetcher(fetcher_.Pass());
@@ -78,10 +78,12 @@ void WebstoreSearchFetcher::OnURLFetchComplete(const net::URLFetcher* source) {
 
   scoped_refptr<SafeJsonParser> parser =
       new SafeJsonParser(webstore_json_data,
-                         base::Bind(&WebstoreSearchFetcher::OnJsonParseSuccess,
-                                    weak_factory_.GetWeakPtr()),
-                         base::Bind(&WebstoreSearchFetcher::OnJsonParseError,
-                                    weak_factory_.GetWeakPtr()));
+                         base::Bind(
+                             &JSONResponseFetcher::OnJsonParseSuccess,
+                             weak_factory_.GetWeakPtr()),
+                         base::Bind(
+                             &JSONResponseFetcher::OnJsonParseError,
+                             weak_factory_.GetWeakPtr()));
   // The parser will call us back via one of the callbacks.
   parser->Start();
 }
