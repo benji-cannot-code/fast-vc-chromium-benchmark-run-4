c5cd92b8844d657098a70f50f543b3d750d6a208
kkania@chromium.org <kkania@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 15:18:58 +0000

    [chromedriver] Always recognize the 'binary' and 'extensions' capabilities.
    These are passed always by the java webdriver client.
    Fixes the broken java android tests.
    BUG=none
    R=craigdh@chromium.org
    
    Review URL: https://codereview.chromium.org/23726003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220304 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/test/chromedriver/capabilities.cc
+++ chrome/test/chromedriver/capabilities.cc
@@ -54,6 +54,10 @@ Status IgnoreDeprecatedOption(
   return Status(kOk);
 }
 
+Status IgnoreCapability(const base::Value& option, Capabilities* capabilities) {
+  return Status(kOk);
+}
+
 Status ParseChromeBinary(
     const base::Value& option,
     Capabilities* capabilities) {
@@ -257,6 +261,10 @@ Status ParseChromeOptions(
   bool is_existing = chrome_options->HasKey("useExistingBrowser");
 
   std::map<std::string, Parser> parser_map;
+  // Ignore 'binary' and 'extensions' capability, since the Java client
+  // always passes them.
+  parser_map["binary"] = base::Bind(&IgnoreCapability);
+  parser_map["extensions"] = base::Bind(&IgnoreCapability);
   if (is_android) {
     parser_map["androidActivity"] =
         base::Bind(&ParseString, &capabilities->android_activity);
@@ -268,6 +276,7 @@ Status ParseChromeOptions(
         base::Bind(&ParseString, &capabilities->android_process);
     parser_map["args"] = base::Bind(&ParseArgs, true);
   } else if (is_existing) {
+    parser_map["args"] = base::Bind(&IgnoreCapability);
     parser_map["useExistingBrowser"] = base::Bind(&ParseUseExistingBrowser);
   } else {
     parser_map["forceDevToolsScreenshot"] = base::Bind(
