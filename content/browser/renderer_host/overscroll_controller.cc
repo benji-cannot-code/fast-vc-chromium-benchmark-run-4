4abba40c0c2bc7032fe4c687cfba371a60254a00
sadrul@chromium.org <sadrul@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 14:15:19 +0000

    overscroll: Use different thresholds to initiate overscroll from touch-pad and touchscreen.
    BUG=271519
    R=arv@chromium.org, joi@chromium.org, sky@chromium.org
    
    Review URL: https://codereview.chromium.org/23445003
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220283 0039d316-1c4b-4281-b951-d872f2087c98

--- content/browser/renderer_host/overscroll_controller.cc
+++ content/browser/renderer_host/overscroll_controller.cc
@@ -10,6 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "content/public/browser/overscroll_configuration.h"
 #include "content/public/browser/render_widget_host_view.h"
 
+using WebKit::WebInputEvent;
+
 namespace content {
 
 OverscrollController::OverscrollController(
@@ -235,14 +237,16 @@ void OverscrollController::ProcessEventForOverscroll(
         return;
 
       ProcessOverscroll(wheel.deltaX * wheel.accelerationRatioX,
-                        wheel.deltaY * wheel.accelerationRatioY);
+                        wheel.deltaY * wheel.accelerationRatioY,
+                        wheel.type);
       break;
     }
     case WebKit::WebInputEvent::GestureScrollUpdate: {
       const WebKit::WebGestureEvent& gesture =
           static_cast<const WebKit::WebGestureEvent&>(event);
       ProcessOverscroll(gesture.data.scrollUpdate.deltaX,
-                        gesture.data.scrollUpdate.deltaY);
+                        gesture.data.scrollUpdate.deltaY,
+                        gesture.type);
       break;
     }
     case WebKit::WebInputEvent::GestureFlingStart: {
@@ -277,13 +281,17 @@ void OverscrollController::ProcessEventForOverscroll(
   }
 }
 
-void OverscrollController::ProcessOverscroll(float delta_x, float delta_y) {
+void OverscrollController::ProcessOverscroll(float delta_x,
+                                             float delta_y,
+                                             WebKit::WebInputEvent::Type type) {
   if (scroll_state_ != STATE_CONTENT_SCROLLING)
     overscroll_delta_x_ += delta_x;
   overscroll_delta_y_ += delta_y;
 
   float horiz_threshold = GetOverscrollConfig(
-      OVERSCROLL_CONFIG_HORIZ_THRESHOLD_START);
+      WebInputEvent::isGestureEventType(type) ?
+          OVERSCROLL_CONFIG_HORIZ_THRESHOLD_START_TOUCHSCREEN :
+          OVERSCROLL_CONFIG_HORIZ_THRESHOLD_START_TOUCHPAD);
   float vert_threshold = GetOverscrollConfig(
       OVERSCROLL_CONFIG_VERT_THRESHOLD_START);
   if (fabs(overscroll_delta_x_) <= horiz_threshold &&
