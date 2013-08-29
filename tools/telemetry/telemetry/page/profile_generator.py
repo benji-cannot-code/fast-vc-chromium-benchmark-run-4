85a285ab92908d6a5eadbf34b386672b2929133d
jeremy@chromium.org <jeremy@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 19:23:46 +0000

    [Telemetry] some fixes for profile generation
    * For desktop copy profile directory out correctly.
    * Limit number of tabs opened by small_profile_creator to 5 to lower profile generation time on Android.
    * Change command line format for profile_generator to use --profile-type-to-generate argument to specify the type of profile we want to create.
    * generate_profile_experimental -> generate_profile
    
    BUG=136664
    NOTRY=true
    
    Review URL: https://chromiumcodereview.appspot.com/23592014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220353 0039d316-1c4b-4281-b951-d872f2087c98

--- tools/telemetry/telemetry/page/profile_generator.py
+++ tools/telemetry/telemetry/page/profile_generator.py
@@ -6,6 +6,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 """Handles generating profiles and transferring them to/from mobile devices."""
 
 import logging
+import optparse
 import os
 import shutil
 import sys
@@ -58,6 +59,9 @@ def GenerateProfiles(profile_creator_class, profile_creator_name, options):
   if not os.path.exists(generated_profiles_dir):
     os.makedirs(generated_profiles_dir)
   out_path = os.path.join(generated_profiles_dir, profile_creator_name)
+  if os.path.exists(out_path):
+    shutil.rmtree(out_path)
+
   shutil.move(temp_output_directory, out_path)
   sys.stderr.write("SUCCESS: Generated profile copied to: '%s'.\n" % out_path)
 
@@ -65,20 +69,29 @@ def GenerateProfiles(profile_creator_class, profile_creator_name, options):
 
 def Main():
   profile_creators = _DiscoverProfileCreatorClasses()
+  legal_profile_creators = '|'.join(profile_creators.keys())
 
   options = browser_options.BrowserFinderOptions()
-  parser = options.CreateParser("%%prog <profile_type> <--browser=...>")
+  parser = options.CreateParser(
+      "%%prog <--profile-type-to-generate=...> <--browser=...>")
   page_runner.AddCommandLineOptions(parser)
-  _, args = parser.parse_args()
+
+  group = optparse.OptionGroup(parser, 'Profile generation options')
+  group.add_option('--profile-type-to-generate',
+      dest='profile_type_to_generate',
+      default=None,
+      help='Type of profile to generate. '
+           'Supported values: %s' % legal_profile_creators)
+  parser.add_option_group(group)
+
+  _, _ = parser.parse_args()
 
   # Sanity check arguments.
-  legal_profile_creators = '|'.join(profile_creators.keys())
-  if len(args) != 1:
-    raise Exception("No profile type argument specified legal values are: %s" %
-        legal_profile_creators)
+  if not options.profile_type_to_generate:
+    raise Exception("Must specify --profile-type-to-generate option.")
 
-  if args[0] not in profile_creators.keys():
-    raise Exception("Invalid profile type, legal values are: %s" %
+  if options.profile_type_to_generate not in profile_creators.keys():
+    raise Exception("Invalid profile type, legal values are: %s." %
         legal_profile_creators)
 
   if not options.browser_type:
@@ -88,5 +101,6 @@ def Main():
     raise Exception("Can't use existing profile when generating profile.")
 
   # Generate profile.
-  profile_creator_class = profile_creators[args[0]]
-  return GenerateProfiles(profile_creator_class, args[0], options)
+  profile_creator_class = profile_creators[options.profile_type_to_generate]
+  return GenerateProfiles(profile_creator_class,
+      options.profile_type_to_generate, options)
