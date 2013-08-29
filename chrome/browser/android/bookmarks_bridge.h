1f7c737300c897379450df7064e0fc7504cec392
tedchoc@chromium.org <tedchoc@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-29 00:22:04 +0000

    Add support for editing/deleting/add to homescreening bookmarks from Android.
    BUG=247381
    
    Review URL: https://chromiumcodereview.appspot.com/23021020
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220132 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/android/bookmarks_bridge.h
+++ chrome/browser/android/bookmarks_bridge.h
@@ -14,6 +14,7 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "base/compiler_specific.h"
 #include "chrome/browser/bookmarks/base_bookmark_model_observer.h"
 
+class PrefService;
 class Profile;
 
 // The delegate to fetch bookmarks information for the Android native
@@ -38,6 +39,10 @@ class BookmarksBridge : public BaseBookmarkModelObserver {
                                  jobject j_callback_obj,
                                  jobject j_result_obj);
 
+  void DeleteBookmark(JNIEnv* env,
+                      jobject obj,
+                      jlong bookmark_id);
+
  private:
   virtual ~BookmarksBridge();
 
@@ -45,6 +50,8 @@ class BookmarksBridge : public BaseBookmarkModelObserver {
       const BookmarkNode* node,
       jobject j_result_obj);
   const BookmarkNode* GetFolderNodeFromId(jlong folder_id);
+  // Returns true if |node| can be modified by the user.
+  bool IsEditable(const BookmarkNode* node) const;
 
   // Override BaseBookmarkModelObserver.
   virtual void BookmarkModelChanged() OVERRIDE;
@@ -52,7 +59,8 @@ class BookmarksBridge : public BaseBookmarkModelObserver {
   virtual void BookmarkModelBeingDeleted(BookmarkModel* model) OVERRIDE;
 
   JavaObjectWeakGlobalRef weak_java_ref_;
-  BookmarkModel* bookmark_model_;
+  BookmarkModel* bookmark_model_;  // weak
+  PrefService* pref_service_;  // weak
 
   DISALLOW_COPY_AND_ASSIGN(BookmarksBridge);
 };
