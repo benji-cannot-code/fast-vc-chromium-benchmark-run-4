7218d199e907a2b7912f1418d42526e72a158c23
komatsu@chromium.org <komatsu@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 08:52:33 +0000

    Treat USE_ASH is always defined.
    BUG=
    
    Review URL: https://chromiumcodereview.appspot.com/23441015
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220535 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/browser/chromeos/input_method/candidate_window_controller_impl.cc
+++ chrome/browser/chromeos/input_method/candidate_window_controller_impl.cc
@@ -8,6 +8,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include <string>
 #include <vector>
 
+#include "ash/shell.h"
+#include "ash/shell_window_ids.h"
+#include "ash/wm/window_animations.h"
 #include "base/logging.h"
 #include "base/memory/scoped_ptr.h"
 #include "base/observer_list.h"
@@ -16,11 +19,6 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 #include "chrome/browser/chromeos/input_method/infolist_window_view.h"
 #include "ui/views/widget/widget.h"
 
-#if defined(USE_ASH)
-#include "ash/shell.h"
-#include "ash/shell_window_ids.h"
-#include "ash/wm/window_animations.h"
-#endif  // USE_ASH
 
 namespace chromeos {
 namespace input_method {
@@ -55,19 +53,14 @@ void CandidateWindowControllerImpl::CreateView() {
   // they should use WIDGET_OWNS_NATIVE_WIDGET ownership.
   params.ownership = views::Widget::InitParams::WIDGET_OWNS_NATIVE_WIDGET;
   // Show the candidate window always on top
-#if defined(USE_ASH)
   params.parent = ash::Shell::GetContainer(
       ash::Shell::GetActiveRootWindow(),
       ash::internal::kShellWindowId_InputMethodContainer);
-#else
-  params.keep_on_top = true;
-#endif
   frame_->Init(params);
-#if defined(USE_ASH)
+
   views::corewm::SetWindowVisibilityAnimationType(
       frame_->GetNativeView(),
       views::corewm::WINDOW_VISIBILITY_ANIMATION_TYPE_FADE);
-#endif  // USE_ASH
 
   // Create the candidate window.
   candidate_window_ = new CandidateWindowView(frame_.get());
@@ -80,11 +73,10 @@ void CandidateWindowControllerImpl::CreateView() {
   // Create the infolist window.
   infolist_window_.reset(new DelayableWidget);
   infolist_window_->Init(params);
-#if defined(USE_ASH)
+
   views::corewm::SetWindowVisibilityAnimationType(
       infolist_window_->GetNativeView(),
       views::corewm::WINDOW_VISIBILITY_ANIMATION_TYPE_FADE);
-#endif  // USE_ASH
 
   InfolistWindowView* infolist_view = new InfolistWindowView;
   infolist_view->Init();
