634e5691e56b866aa0d6bbc38bf33cbd49037cdc
danakj@chromium.org <danakj@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 02:59:50 +0000

    Revert 220479 "Add support for inverse transform animations."
    http://build.chromium.org/p/chromium.chromiumos/builders/Linux%20ChromiumOS%20Tests%20%281%29/builds/31905/steps/compositor_unittests/logs/InversesTransform
    
    > Add support for inverse transform animations.
    > 
    > This allows creation of a transform and its inverse that would be used
    > to counter-animate some child layer.
    > 
    > BUG=270857
    > 
    > Committed: https://src.chromium.org/viewvc/chrome?view=rev&revision=220124
    > 
    > R=vollick@chromium.org, wittman@chromium.org
    > 
    > Review URL: https://codereview.chromium.org/22861008
    
    TBR=danakj@chromium.org
    
    Review URL: https://codereview.chromium.org/23809002
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220492 0039d316-1c4b-4281-b951-d872f2087c98

--- ui/compositor/test/test_utils.cc
+++ ui/compositor/test/test_utils.cc
@@ -11,21 +11,12 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace ui {
 
-//TODO(avallee): Make this into a predicate and add some matrix pretty printing.
 void CheckApproximatelyEqual(const gfx::Transform& lhs,
                              const gfx::Transform& rhs) {
   for (int i = 0; i < 4; ++i) {
     for (int j = 0; j < 4; ++j) {
-      EXPECT_FLOAT_EQ(lhs.matrix().get(i, j), rhs.matrix().get(i, j))
-        << "(i, j) = (" << i << ", " << j <<")";
+      EXPECT_FLOAT_EQ(lhs.matrix().get(i, j), rhs.matrix().get(i, j));
     }
-  }
-
-  if (::testing::Test::HasFailure()) {
-    ADD_FAILURE() << "Expected matrix:\n"
-                  << lhs.ToString() << "\n"
-                  << "Actual matrix:\n"
-                  << rhs.ToString();
   }
 }
 
