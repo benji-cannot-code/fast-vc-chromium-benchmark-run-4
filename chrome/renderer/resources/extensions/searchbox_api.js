017d4dc5204f9851da4f23360af000c57ce91966
samarth@chromium.org <samarth@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 07:24:50 +0000

    InstantExtended: add back focus() API call.
    BUG=none
    TESTED=Open NTP, run chrome.embeddedSearch.searchBox.focus() in the DOM inspector.
    
    Review URL: https://chromiumcodereview.appspot.com/23462014
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220531 0039d316-1c4b-4281-b951-d872f2087c98

--- chrome/renderer/resources/extensions/searchbox_api.js
+++ chrome/renderer/resources/extensions/searchbox_api.js
@@ -14,6 +14,7 @@ if (!chrome.embeddedSearch) {
       // =======================================================================
       //                            Private functions
       // =======================================================================
+      native function Focus();
       native function GetDisplayInstantResults();
       native function GetFont();
       native function GetFontSize();
@@ -41,6 +42,10 @@ if (!chrome.embeddedSearch) {
       this.__defineGetter__('startMargin', GetStartMargin);
       this.__defineGetter__('suggestion', GetSuggestionToPrefetch);
       this.__defineGetter__('value', GetQuery);
+
+      this.focus = function() {
+        Focus();
+      };
 
       // This method is restricted to chrome-search://most-visited pages by
       // checking the invoking context's origin in searchbox_extension.cc.
