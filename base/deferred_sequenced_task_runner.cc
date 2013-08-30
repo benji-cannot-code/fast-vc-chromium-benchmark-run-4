9ed19571e93ec280fc87f3b2f1bdfcac4f2e16bf
finnur@chromium.org <finnur@chromium.org@0039d316-1c4b-4281-b951-d872f2087c98>
2013-08-30 12:59:25 +0000

    Init uninitialized member in ctor.
    BUG=NONE
    CID=108673
    R=msarda@chromium.org
    
    Review URL: https://codereview.chromium.org/23511006
    
    git-svn-id: svn://svn.chromium.org/chrome/trunk/src@220569 0039d316-1c4b-4281-b951-d872f2087c98

--- base/deferred_sequenced_task_runner.cc
+++ base/deferred_sequenced_task_runner.cc
@@ -10,7 +10,8 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 
 namespace base {
 
-DeferredSequencedTaskRunner::DeferredTask::DeferredTask() {
+DeferredSequencedTaskRunner::DeferredTask::DeferredTask()
+    : is_non_nestable(false) {
 }
 
 DeferredSequencedTaskRunner::DeferredTask::~DeferredTask() {
