dd6d81cb63dc4528eb720ac796d5607b2ddc769e
cevans@chromium.org <cevans@chromium.org@bbb929c8-8fbe-4397-9dbb-9b2b20218538>
2013-08-30 19:09:44 +0000

    Ensure that Vector destruction always clears the buffer pointer.
    This was previously the case, with the exception of inline-buffer Vectors in
    "inline storage" mode.
    
    In the process, some code cleanups were performed:
    - Removed unused releaseBuffer().
    - Added a little LIKELY / UNLIKELY.
    - Removed a lot of redundant branches.
    - Less work and two removed branches from the destructor path.
    
    Saves 100KB from a GCC-compiled Chrome binary. No real difference on clang,
    which seems to inline very differently.
    
    BUG=270545
    R=abarth@chromium.org, tsepez@chromium.org
    
    Review URL: https://codereview.chromium.org/23531010
    
    git-svn-id: svn://svn.chromium.org/blink/trunk@157005 bbb929c8-8fbe-4397-9dbb-9b2b20218538

--- third_party/WebKit/Source/wtf/Deque.h
+++ third_party/WebKit/Source/wtf/Deque.h
@@ -34,9 +34,9 @@ FASTVC-BENCH-CORPUS:chromium-main-v1-943b94ae-1c74-4335-94fa-ceb4d277cea8
 // FIXME: Could move what Vector and Deque share into a separate file.
 // Deque doesn't actually use Vector.
 
-#include <iterator>
 #include "wtf/PassTraits.h"
 #include "wtf/Vector.h"
+#include <iterator>
 
 namespace WTF {
 
@@ -350,6 +350,7 @@ namespace WTF {
         m_start = 0;
         m_end = 0;
         m_buffer.deallocateBuffer(m_buffer.buffer());
+        m_buffer.clearBufferPointer();
         checkValidity();
     }
 
